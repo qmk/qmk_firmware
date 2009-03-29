d83d87e1b8aa8c76e062f01588066d9ab4233828
Dean Camera <dean@fourwalledcubicle.com>
2009-03-29 06:31:24 +0000

    Fixed MagStripe keyboard interface polling interval, increased to 4ms from 2ms to maintain compatibility with Apple MacOS X. Fixed changelog comments on the size of the track buffers when compiling for the AT90USBXXX6/7.

--- Projects/Magstripe/Descriptors.c
+++ Projects/Magstripe/Descriptors.c
@@ -154,7 +154,7 @@ USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor =
 			EndpointAddress:        (ENDPOINT_DESCRIPTOR_DIR_IN | KEYBOARD_EPNUM),
 			Attributes:             EP_TYPE_INTERRUPT,
 			EndpointSize:           KEYBOARD_EPSIZE,
-			PollingIntervalMS:      0x02
+			PollingIntervalMS:      0x04
 		},
 };
 
