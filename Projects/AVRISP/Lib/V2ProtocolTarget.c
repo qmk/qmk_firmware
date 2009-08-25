244bba2097574fd9c4905eadac069c57796cbc28
Dean Camera <dean@fourwalledcubicle.com>
2009-08-25 13:26:34 +0000

    Make AVRISP project build again - fix incorrect preprocessor directive in V2Protocol.c, fix makefile LUFA_PATH.

--- Projects/AVRISP/Lib/V2ProtocolTarget.c
+++ Projects/AVRISP/Lib/V2ProtocolTarget.c
@@ -55,7 +55,7 @@ uint8_t V2Protocol_GetSPIPrescalerMask(void)
 		SPI_SPEED_FCPU_DIV_16,   // AVRStudio = 500KHz SPI, Actual = 500KHz SPI
 		SPI_SPEED_FCPU_DIV_32,   // AVRStudio = 250KHz SPI, Actual = 250KHz SPI
 		SPI_SPEED_FCPU_DIV_64    // AVRStudio = 125KHz SPI, Actual = 125KHz SPI	
-	#if (F_CPU == 16000000)
+	#elif (F_CPU == 16000000)
 		SPI_SPEED_FCPU_DIV_2,    // AVRStudio =   8MHz SPI, Actual =   8MHz SPI
 		SPI_SPEED_FCPU_DIV_4,    // AVRStudio =   4MHz SPI, Actual =   4MHz SPI
 		SPI_SPEED_FCPU_DIV_8,    // AVRStudio =   2MHz SPI, Actual =   2MHz SPI
