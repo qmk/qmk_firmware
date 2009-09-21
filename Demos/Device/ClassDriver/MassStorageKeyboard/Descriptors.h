f5c645296c837c85ff915a615cf1fd3193b91d7d
Dean Camera <dean@fourwalledcubicle.com>
2009-09-21 08:07:26 +0000

    Added new MassStorageKeyboard Device Class Driver demo (thanks to Matthias Hullin).

---
+++ Demos/Device/ClassDriver/MassStorageKeyboard/Descriptors.h
@@ -0,0 +1,96 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2009.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
+
+/*
+  Copyright 2009  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+  Copyright 2009  Matthias Hullin (matthias [at] hullin [dot] net)
+
+  Permission to use, copy, modify, and distribute this software
+  and its documentation for any purpose and without fee is hereby
+  granted, provided that the above copyright notice appear in all
+  copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Header file for Descriptors.c.
+ */
+ 
+#ifndef _DESCRIPTORS_H_
+#define _DESCRIPTORS_H_
+
+	/* Includes: */
+		#include <avr/pgmspace.h>
+
+		#include <LUFA/Drivers/USB/USB.h>
+		#include <LUFA/Drivers/USB/Class/MassStorage.h>
+		#include <LUFA/Drivers/USB/Class/HID.h>
+
+		#if (USE_INTERNAL_SERIAL == NO_DESCRIPTOR)
+			#warning USE_INTERNAL_SERIAL is not available on this AVR - please manually construct a device serial descriptor.
+		#endif
+
+	/* Macros: */
+		/** Endpoint number of the Keyboard HID reporting IN endpoint. */
+		#define KEYBOARD_EPNUM               1
+		
+		/** Size in bytes of the Keyboard HID reporting IN and OUT endpoints. */		
+		#define KEYBOARD_EPSIZE              8
+
+		/** Endpoint number of the Mass Storage device-to-host data IN endpoint. */
+		#define MASS_STORAGE_IN_EPNUM        3	
+
+		/** Endpoint number of the Mass Storage host-to-device data OUT endpoint. */
+		#define MASS_STORAGE_OUT_EPNUM       4	
+
+		/** Size in bytes of the Mass Storage data endpoints. */
+		#define MASS_STORAGE_IO_EPSIZE       64
+		
+	/* Type Defines: */		
+		/** Type define for the device configuration descriptor structure. This must be defined in the
+		 *  application code, as the configuration descriptor contains several sub-descriptors which
+		 *  vary between devices, and which describe the device's usage to the host.
+		 */
+		typedef struct
+		{
+			USB_Descriptor_Configuration_Header_t Config;
+			USB_Descriptor_Interface_t            Interface;
+			USB_Descriptor_Endpoint_t             DataInEndpoint;
+			USB_Descriptor_Endpoint_t             DataOutEndpoint;
+			USB_Descriptor_Interface_t            KeyboardInterface;
+			USB_HID_Descriptor_t                  KeyboardHID;
+	        USB_Descriptor_Endpoint_t             KeyboardEndpoint;
+		} USB_Descriptor_Configuration_t;
+
+/*		typedef struct
+		{
+			USB_Descriptor_Configuration_Header_t Config;
+			USB_Descriptor_Interface_t            Interface;
+			USB_HID_Descriptor_t                  KeyboardHID;
+	        USB_Descriptor_Endpoint_t             KeyboardEndpoint;
+		} USB_Descriptor_Configuration_keyboard_t;
+// */
+		
+	/* Function Prototypes: */
+		uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue, const uint8_t wIndex, void** const DescriptorAddress)
+											ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
+
+#endif
---
+++ Demos/Device/ClassDriver/MassStorageKeyboard/MassStorageKeyboard.c
@@ -0,0 +1,238 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2009.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
+
+/*
+  Copyright 2009  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+  Copyright 2009  Matthias Hullin (matthias [at] hullin [dot] net)
+
+  Permission to use, copy, modify, and distribute this software
+  and its documentation for any purpose and without fee is hereby
+  granted, provided that the above copyright notice appear in all
+  copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Main source file for the MassStorageKeyboard demo. This file contains the main tasks of
+ *  the demo and is responsible for the initial application hardware configuration.
+ */
+
+#include "MassStorageKeyboard.h"
+
+/** Buffer to hold the previously generated Keyboard HID report, for comparison purposes inside the HID class driver. */
+uint8_t PrevKeyboardHIDReportBuffer[sizeof(USB_KeyboardReport_Data_t)];
+
+/** LUFA Mass Storage Class driver interface configuration and state information. This structure is
+ *  passed to all Mass Storage Class driver functions, so that multiple instances of the same class
+ *  within a device can be differentiated from one another.
+ */
+USB_ClassInfo_MS_Device_t Disk_MS_Interface =
+	{
+		.Config =
+			{
+				.InterfaceNumber        = 0,
+
+				.DataINEndpointNumber   = MASS_STORAGE_IN_EPNUM,
+				.DataINEndpointSize     = MASS_STORAGE_IO_EPSIZE,
+
+				.DataOUTEndpointNumber  = MASS_STORAGE_OUT_EPNUM,
+				.DataOUTEndpointSize    = MASS_STORAGE_IO_EPSIZE,
+
+				.TotalLUNs              = TOTAL_LUNS,
+			},
+	};
+
+/** LUFA HID Class driver interface configuration and state information. This structure is
+ *  passed to all HID Class driver functions, so that multiple instances of the same class
+ *  within a device can be differentiated from one another.
+ */
+USB_ClassInfo_HID_Device_t Keyboard_HID_Interface =
+ 	{
+		.Config =
+			{
+				.InterfaceNumber         = 1,
+
+				.ReportINEndpointNumber  = KEYBOARD_EPNUM,
+				.ReportINEndpointSize    = KEYBOARD_EPSIZE,
+
+				.PrevReportINBuffer      = PrevKeyboardHIDReportBuffer,
+				.PrevReportINBufferSize  = sizeof(PrevKeyboardHIDReportBuffer),
+			},
+    };
+
+/** Main program entry point. This routine contains the overall program flow, including initial
+ *  setup of all components and the main program loop.
+ */
+int main(void)
+{
+	SetupHardware();
+
+	LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+
+	for (;;)
+	{
+		MS_Device_USBTask(&Disk_MS_Interface);
+		HID_Device_USBTask(&Keyboard_HID_Interface);
+		USB_USBTask();
+	}
+}
+
+/** Configures the board hardware and chip peripherals for the demo's functionality. */
+void SetupHardware(void)
+{
+	/* Disable watchdog if enabled by bootloader/fuses */
+	MCUSR &= ~(1 << WDRF);
+	wdt_disable();
+
+	/* Disable clock division */
+	clock_prescale_set(clock_div_1);
+
+	/* Hardware Initialization */
+	LEDs_Init();
+	Joystick_Init();
+	Buttons_Init();
+	SPI_Init(SPI_SPEED_FCPU_DIV_2 | SPI_SCK_LEAD_FALLING | SPI_SAMPLE_TRAILING | SPI_MODE_MASTER);
+	Dataflash_Init();
+	USB_Init();
+
+	/* Clear Dataflash sector protections, if enabled */
+	DataflashManager_ResetDataflashProtections();
+
+	/* Millisecond timer initialization, with output compare interrupt enabled for the HID idle timing */
+	OCR0A  = ((F_CPU / 64) / 1000);
+	TCCR0A = (1 << WGM01);
+	TCCR0B = ((1 << CS01) | (1 << CS00));
+	TIMSK0 = (1 << OCIE0A);
+}
+
+/** Event handler for the library USB Connection event. */
+void EVENT_USB_Device_Connect(void)
+{
+	LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
+}
+
+/** Event handler for the library USB Disconnection event. */
+void EVENT_USB_Device_Disconnect(void)
+{
+	LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+}
+
+/** Event handler for the library USB Configuration Changed event. */
+void EVENT_USB_Device_ConfigurationChanged(void)
+{
+	LEDs_SetAllLEDs(LEDMASK_USB_READY);
+
+	if (!(MS_Device_ConfigureEndpoints(&Disk_MS_Interface)))
+	  LEDs_SetAllLEDs(LEDMASK_USB_ERROR);
+	if (!(HID_Device_ConfigureEndpoints(&Keyboard_HID_Interface)))
+	  LEDs_SetAllLEDs(LEDMASK_USB_ERROR);
+}
+
+/** Event handler for the library USB Unhandled Control Request event. */
+void EVENT_USB_Device_UnhandledControlRequest(void)
+{
+	MS_Device_ProcessControlRequest(&Disk_MS_Interface);
+	HID_Device_ProcessControlRequest(&Keyboard_HID_Interface);
+}
+
+/** Mass Storage class driver callback function the reception of SCSI commands from the host, which must be processed.
+ *
+ *  \param[in] MSInterfaceInfo  Pointer to the Mass Storage class interface configuration structure being referenced
+ */
+bool CALLBACK_MS_Device_SCSICommandReceived(USB_ClassInfo_MS_Device_t* MSInterfaceInfo)
+{
+	bool CommandSuccess;
+	
+	LEDs_SetAllLEDs(LEDMASK_USB_BUSY);
+	CommandSuccess = SCSI_DecodeSCSICommand(MSInterfaceInfo);
+	LEDs_SetAllLEDs(LEDMASK_USB_READY);
+	
+	return CommandSuccess;
+}
+
+/** ISR to keep track of each millisecond interrupt, for determining the HID class idle period remaining when set. */
+ISR(TIMER0_COMPA_vect, ISR_BLOCK)
+{
+	HID_Device_MillisecondElapsed(&Keyboard_HID_Interface);
+}
+
+/** HID class driver callback function for the creation of HID reports to the host.
+ *
+ *  \param[in] HIDInterfaceInfo  Pointer to the HID class interface configuration structure being referenced
+ *  \param[in,out] ReportID  Report ID requested by the host if non-zero, otherwise callback should set to the generated report ID
+ *  \param[out] ReportData  Pointer to a buffer where the created report should be stored
+ *  \param[out] ReportSize  Number of bytes written in the report (or zero if no report is to be sent
+ *
+ *  \return Boolean true to force the sending of the report, false to let the library determine if it needs to be sent
+ */
+bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo, uint8_t* const ReportID,
+                                         void* ReportData, uint16_t* ReportSize)
+{
+	USB_KeyboardReport_Data_t* KeyboardReport = (USB_KeyboardReport_Data_t*)ReportData;
+	
+	uint8_t JoyStatus_LCL    = Joystick_GetStatus();
+	uint8_t ButtonStatus_LCL = Buttons_GetStatus();
+
+	if (JoyStatus_LCL & JOY_UP)
+	  KeyboardReport->KeyCode[0] = 0x04; // A
+	else if (JoyStatus_LCL & JOY_DOWN)
+	  KeyboardReport->KeyCode[0] = 0x05; // B
+
+	if (JoyStatus_LCL & JOY_LEFT)
+	  KeyboardReport->KeyCode[0] = 0x06; // C
+	else if (JoyStatus_LCL & JOY_RIGHT)
+	  KeyboardReport->KeyCode[0] = 0x07; // D
+
+	if (JoyStatus_LCL & JOY_PRESS)
+	  KeyboardReport->KeyCode[0] = 0x08; // E
+	  
+	if (ButtonStatus_LCL & BUTTONS_BUTTON1)
+	  KeyboardReport->KeyCode[0] = 0x09; // F
+	
+	*ReportSize = sizeof(USB_KeyboardReport_Data_t);
+	return false;
+}
+
+/** HID class driver callback function for the processing of HID reports from the host.
+ *
+ *  \param[in] HIDInterfaceInfo  Pointer to the HID class interface configuration structure being referenced
+ *  \param[in] ReportID  Report ID of the received report from the host
+ *  \param[in] ReportData  Pointer to a buffer where the created report has been stored
+ *  \param[in] ReportSize  Size in bytes of the received HID report
+ */
+void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo, const uint8_t ReportID,
+                                          const void* ReportData, const uint16_t ReportSize)
+{
+	uint8_t  LEDMask   = LEDS_NO_LEDS;
+	uint8_t* LEDReport = (uint8_t*)ReportData;
+
+	if (*LEDReport & 0x01) // NUM Lock
+	  LEDMask |= LEDS_LED1;
+	
+	if (*LEDReport & 0x02) // CAPS Lock
+	  LEDMask |= LEDS_LED3;
+
+	if (*LEDReport & 0x04) // SCROLL Lock
+	  LEDMask |= LEDS_LED4;
+	  
+	LEDs_SetAllLEDs(LEDMask);
+}
+
