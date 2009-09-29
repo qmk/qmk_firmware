7977663f62627a9561e6180e0548e5a9c83c97c3
Dean Camera <dean@fourwalledcubicle.com>
2009-09-29 07:15:09 +0000

    Add information on the new MouseHostDevice dual role demo to the library documentation. Add a 0x prefix to the LUFA_VERSION_INTEGER define to ensure that it is stored as BCD, and not an octal value.

--- LUFA/Version.h
+++ LUFA/Version.h
@@ -41,7 +41,7 @@
 	/* Public Interface - May be used in end-application: */
 		/* Macros: */
 			/** Indicates the version number of the library, as an integer. */
-			#define LUFA_VERSION_INTEGER     000000
+			#define LUFA_VERSION_INTEGER     0x000000
 
 			/** Indicates the version number of the library, as a string. */
 			#define LUFA_VERSION_STRING      "XXXXXX"
