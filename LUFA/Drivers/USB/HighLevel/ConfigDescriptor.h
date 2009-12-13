32b7762325829c6e7a4168bb7db5084a34673f20
Dean Camera <dean@fourwalledcubicle.com>
2009-12-13 14:16:29 +0000

    Oops - wrong casting on the Configuration Descriptor pointer advance routine.

--- LUFA/Drivers/USB/HighLevel/ConfigDescriptor.h
+++ LUFA/Drivers/USB/HighLevel/ConfigDescriptor.h
@@ -263,7 +263,7 @@
 			{
 				uint16_t CurrDescriptorSize = DESCRIPTOR_CAST(*CurrConfigLoc, USB_Descriptor_Header_t).Size;
 
-				*((uint8_t**)CurrConfigLoc) += CurrDescriptorSize;
+				*CurrConfigLoc += CurrDescriptorSize;
 				*BytesRem      -= CurrDescriptorSize;
 			}
 			
