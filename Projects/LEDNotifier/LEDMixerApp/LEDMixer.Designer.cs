901e26edd893a73ac38bdf95a7a72c48bdc1d07b
Dean Camera <dean@fourwalledcubicle.com>
2009-12-18 05:34:11 +0000

    Add LEDNotifier project to the Projects folder build list. Add new LEDMixer C# host application for the LEDNotifier project.

---
+++ Projects/LEDNotifier/LEDMixerApp/LEDMixer.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace LEDMixer
+{
+    partial class LEDMixer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbRed = new System.Windows.Forms.TrackBar();
+            this.tbGreen = new System.Windows.Forms.TrackBar();
+            this.tbBlue = new System.Windows.Forms.TrackBar();
+            this.serSerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbRed
+            // 
+            this.tbRed.Location = new System.Drawing.Point(54, 64);
+            this.tbRed.Maximum = 512;
+            this.tbRed.Name = "tbRed";
+            this.tbRed.Size = new System.Drawing.Size(230, 45);
+            this.tbRed.TabIndex = 0;
+            this.tbRed.Scroll += new System.EventHandler(this.tbRed_Scroll);
+            // 
+            // tbGreen
+            // 
+            this.tbGreen.Location = new System.Drawing.Point(54, 115);
+            this.tbGreen.Maximum = 512;
+            this.tbGreen.Name = "tbGreen";
+            this.tbGreen.Size = new System.Drawing.Size(230, 45);
+            this.tbGreen.TabIndex = 1;
+            this.tbGreen.Scroll += new System.EventHandler(this.tbGreen_Scroll);
+            // 
+            // tbBlue
+            // 
+            this.tbBlue.Location = new System.Drawing.Point(54, 163);
+            this.tbBlue.Maximum = 512;
+            this.tbBlue.Name = "tbBlue";
+            this.tbBlue.Size = new System.Drawing.Size(230, 45);
+            this.tbBlue.TabIndex = 2;
+            this.tbBlue.Scroll += new System.EventHandler(this.tbBlue_Scroll);
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(97, 12);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(99, 21);
+            this.cbPort.TabIndex = 3;
+            this.cbPort.SelectedIndexChanged += new System.EventHandler(this.cbPort_SelectedIndexChanged);
+            // 
+            // lblRed
+            // 
+            this.lblRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed.Location = new System.Drawing.Point(3, 64);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(48, 29);
+            this.lblRed.TabIndex = 4;
+            this.lblRed.Text = "Red";
+            this.lblRed.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlue.Location = new System.Drawing.Point(3, 115);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(50, 27);
+            this.lblBlue.TabIndex = 5;
+            this.lblBlue.Text = "Blue";
+            this.lblBlue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGreen.Location = new System.Drawing.Point(3, 163);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(50, 25);
+            this.lblGreen.TabIndex = 6;
+            this.lblGreen.Text = "Green";
+            this.lblGreen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // LEDMixer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 207);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.cbPort);
+            this.Controls.Add(this.tbBlue);
+            this.Controls.Add(this.tbGreen);
+            this.Controls.Add(this.tbRed);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LEDMixer";
+            this.Text = "LED Mixer";
+            this.Load += new System.EventHandler(this.LEDMixer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbRed;
+        private System.Windows.Forms.TrackBar tbGreen;
+        private System.Windows.Forms.TrackBar tbBlue;
+        private System.IO.Ports.SerialPort serSerialPort;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Label lblGreen;
+    }
+}
+
