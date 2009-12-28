d4ca7fb44c7d326b96cf391f0275dc323dbe24de
Dean Camera <dean@fourwalledcubicle.com>
2009-12-28 00:34:20 +0000

    Fix MIT license language to make its intent clearer.

--- Projects/AVRISP-MKII/Lib/XPROG/TINYNVM.h
+++ Projects/AVRISP-MKII/Lib/XPROG/TINYNVM.h
@@ -9,13 +9,13 @@
 /*
   Copyright 2009  Dean Camera (dean [at] fourwalledcubicle [dot] com)
 
-  Permission to use, copy, modify, and distribute this software
-  and its documentation for any purpose and without fee is hereby
-  granted, provided that the above copyright notice appear in all
-  copies and that both that the copyright notice and this
-  permission notice and warranty disclaimer appear in supporting
-  documentation, and that the name of the author not be used in
-  advertising or publicity pertaining to distribution of the
+  Permission to use, copy, modify, distribute, and sell this 
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in 
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting 
+  documentation, and that the name of the author not be used in 
+  advertising or publicity pertaining to distribution of the 
   software without specific, written prior permission.
 
   The author disclaim all warranties with regard to this
