4f9628376179238c8f08e5cc96a3b01080871049
Dean Camera <dean@fourwalledcubicle.com>
2010-01-25 13:07:54 +0000

    Add missing AVRISPDescriptors.c/.h files to the XPLAINBridge project.

---
+++ Projects/XPLAINBridge/AVRISPDescriptors.c
@@ -0,0 +1,219 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2010.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
+
+/*
+  Copyright 2010  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this 
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in 
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting 
+  documentation, and that the name of the author not be used in 
+  advertising or publicity pertaining to distribution of the 
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  USB Device Descriptors, for library use when in USB device mode. Descriptors are special 
+ *  computer-readable structures which the host requests upon device enumeration, to determine
+ *  the device's capabilities and functions.  
+ */
+
+#include "AVRISPDescriptors.h"
+
+/** Device descriptor structure. This descriptor, located in FLASH memory, describes the overall
+ *  device characteristics, including the supported USB version, control endpoint size and the
+ *  number of device configurations. The descriptor is read out by the USB host when the enumeration
+ *  process begins.
+ */
+USB_Descriptor_Device_t PROGMEM AVRISP_DeviceDescriptor =
+{
+	.Header                 = {.Size = sizeof(USB_Descriptor_Device_t), .Type = DTYPE_Device},
+		
+	.USBSpecification       = VERSION_BCD(01.10),
+	.Class                  = 0xFF,
+	.SubClass               = 0x00,
+	.Protocol               = 0x00,
+				
+	.Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
+		
+	.VendorID               = 0x03EB,
+	.ProductID              = 0x2104,
+	.ReleaseNumber          = VERSION_BCD(02.00),
+		
+	.ManufacturerStrIndex   = 0x01,
+	.ProductStrIndex        = 0x02,
+	.SerialNumStrIndex      = 0x03,
+		
+	.NumberOfConfigurations = FIXED_NUM_CONFIGURATIONS
+};
+
+/** Configuration descriptor structure. This descriptor, located in FLASH memory, describes the usage
+ *  of the device in one of its supported configurations, including information about any device interfaces
+ *  and endpoints. The descriptor is read out by the USB host during the enumeration process when selecting
+ *  a configuration so that the host may correctly communicate with the USB device.
+ */
+AVRISP_USB_Descriptor_Configuration_t PROGMEM AVRISP_ConfigurationDescriptor =
+{
+	.Config = 
+		{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Configuration_Header_t), .Type = DTYPE_Configuration},
+
+			.TotalConfigurationSize = sizeof(AVRISP_USB_Descriptor_Configuration_t),
+			.TotalInterfaces        = 1,
+				
+			.ConfigurationNumber    = 1,
+			.ConfigurationStrIndex  = NO_DESCRIPTOR,
+				
+			.ConfigAttributes       = USB_CONFIG_ATTR_SELFPOWERED,
+			
+			.MaxPowerConsumption    = USB_CONFIG_POWER_MA(100)
+		},
+
+	.AVRISPInterface = 
+		{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Interface_t), .Type = DTYPE_Interface},
+
+			.InterfaceNumber        = 0,
+			.AlternateSetting       = 0,
+			
+			.TotalEndpoints         = 2,
+				
+			.Class                  = 0xFF,
+			.SubClass               = 0x00,
+			.Protocol               = 0x00,
+				
+			.InterfaceStrIndex      = NO_DESCRIPTOR
+		},
+
+	.DataInEndpoint = 
+		{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Endpoint_t), .Type = DTYPE_Endpoint},
+			
+			.EndpointAddress        = (ENDPOINT_DESCRIPTOR_DIR_IN | AVRISP_DATA_EPNUM),
+			.Attributes             = (EP_TYPE_BULK | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+			.EndpointSize           = AVRISP_DATA_EPSIZE,
+			.PollingIntervalMS      = 0x00
+		},
+
+	.DataOutEndpoint =
+		{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Endpoint_t), .Type = DTYPE_Endpoint},
+			
+			.EndpointAddress        = (ENDPOINT_DESCRIPTOR_DIR_OUT | AVRISP_DATA_EPNUM),
+			.Attributes             = (EP_TYPE_BULK | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+			.EndpointSize           = AVRISP_DATA_EPSIZE,
+			.PollingIntervalMS      = 0x00
+		},
+};
+
+/** Language descriptor structure. This descriptor, located in FLASH memory, is returned when the host requests
+ *  the string descriptor with index 0 (the first index). It is actually an array of 16-bit integers, which indicate
+ *  via the language ID table available at USB.org what languages the device supports for its string descriptors.
+ */
+USB_Descriptor_String_t PROGMEM AVRISP_LanguageString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(1), .Type = DTYPE_String},
+		
+	.UnicodeString          = {LANGUAGE_ID_ENG}
+};
+
+/** Manufacturer descriptor string. This is a Unicode string containing the manufacturer's details in human readable
+ *  form, and is read out upon request by the host when the appropriate string ID is requested, listed in the Device
+ *  Descriptor.
+ */
+USB_Descriptor_String_t PROGMEM AVRISP_ManufacturerString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(11), .Type = DTYPE_String},
+		
+	.UnicodeString          = L"Dean Camera"
+};
+
+/** Product descriptor string. This is a Unicode string containing the product's details in human readable form,
+ *  and is read out upon request by the host when the appropriate string ID is requested, listed in the Device
+ *  Descriptor.
+ */
+USB_Descriptor_String_t PROGMEM AVRISP_ProductString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(22), .Type = DTYPE_String},
+		
+	.UnicodeString          = L"LUFA AVRISP MkII Clone"
+};
+
+/** Serial number string. This is a Unicode string containing the device's unique serial number, expressed as a
+ *  series of uppercase hexadecimal digits.
+ */
+USB_Descriptor_String_t PROGMEM AVRISP_SerialString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(13), .Type = DTYPE_String},
+		
+	.UnicodeString          = L"0000A00128255"
+};
+
+/** This function is called by the library when in device mode, and must be overridden (see library "USB Descriptors"
+ *  documentation) by the application code so that the address and size of a requested descriptor can be given
+ *  to the USB library. When the device receives a Get Descriptor request on the control endpoint, this function
+ *  is called so that the descriptor details can be passed back and the appropriate descriptor sent back to the
+ *  USB host.
+ */
+uint16_t AVRISP_GetDescriptor(const uint16_t wValue, const uint8_t wIndex, void** const DescriptorAddress)
+{
+	const uint8_t  DescriptorType   = (wValue >> 8);
+	const uint8_t  DescriptorNumber = (wValue & 0xFF);
+
+	void*    Address = NULL;
+	uint16_t Size    = NO_DESCRIPTOR;
+	
+	switch (DescriptorType)
+	{
+		case DTYPE_Device: 
+			Address = (void*)&AVRISP_DeviceDescriptor;
+			Size    = sizeof(USB_Descriptor_Device_t);
+			break;
+		case DTYPE_Configuration: 
+			Address = (void*)&AVRISP_ConfigurationDescriptor;
+			Size    = sizeof(AVRISP_USB_Descriptor_Configuration_t);
+			break;
+		case DTYPE_String: 
+			switch (DescriptorNumber)
+			{
+				case 0x00: 
+					Address = (void*)&AVRISP_LanguageString;
+					Size    = pgm_read_byte(&AVRISP_LanguageString.Header.Size);
+					break;
+				case 0x01: 
+					Address = (void*)&AVRISP_ManufacturerString;
+					Size    = pgm_read_byte(&AVRISP_ManufacturerString.Header.Size);
+					break;
+				case 0x02: 
+					Address = (void*)&AVRISP_ProductString;
+					Size    = pgm_read_byte(&AVRISP_ProductString.Header.Size);
+					break;
+				case 0x03:
+					Address = (void*)&AVRISP_SerialString;
+					Size    = pgm_read_byte(&AVRISP_SerialString.Header.Size);
+					break;					
+			}
+			
+			break;
+	}
+	
+	*DescriptorAddress = Address;
+	return Size;
+}
---
+++ Projects/XPLAINBridge/AVRISPDescriptors.h
@@ -0,0 +1,67 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2010.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
+
+/*
+  Copyright 2010  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this 
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in 
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting 
+  documentation, and that the name of the author not be used in 
+  advertising or publicity pertaining to distribution of the 
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Header file for AVRISPDescriptors.c.
+ */
+ 
+#ifndef _AVRISP_DESCRIPTORS_H_
+#define _AVRISP_DESCRIPTORS_H_
+
+	/* Includes: */
+		#include <avr/pgmspace.h>
+
+		#include <LUFA/Drivers/USB/USB.h>
+
+	/* Macros: */
+		/** Endpoint number of the AVRISP bidirectional data endpoint. */
+		#define AVRISP_DATA_EPNUM              2
+
+		/** Size in bytes of the AVRISP data endpoint. */
+		#define AVRISP_DATA_EPSIZE             64	
+
+	/* Type Defines: */
+		/** Type define for the device configuration descriptor structure. This must be defined in the
+		 *  application code, as the configuration descriptor contains several sub-descriptors which
+		 *  vary between devices, and which describe the device's usage to the host.
+		 */
+		typedef struct
+		{
+			USB_Descriptor_Configuration_Header_t    Config;
+			USB_Descriptor_Interface_t               AVRISPInterface;
+			USB_Descriptor_Endpoint_t                DataInEndpoint;
+			USB_Descriptor_Endpoint_t                DataOutEndpoint;
+		} AVRISP_USB_Descriptor_Configuration_t;
+
+	/* Function Prototypes: */
+		uint16_t AVRISP_GetDescriptor(const uint16_t wValue, const uint8_t wIndex, void** const DescriptorAddress);
+
+#endif
