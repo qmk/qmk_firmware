5f776f4785960c71f3ac0bd8784b33812a9abc90
Dean Camera <dean@fourwalledcubicle.com>
2010-01-28 13:17:36 +0000

    Fix up project documentation files' overview tables, so that multiple items occupy multiple lines in the same cell, rather than multiple cells.

--- Projects/Webserver/Descriptors.c
+++ Projects/Webserver/Descriptors.c
@@ -163,9 +163,9 @@ USB_Descriptor_String_t PROGMEM ManufacturerString =
  */
 USB_Descriptor_String_t PROGMEM ProductString =
 {
-	.Header                 = {.Size = USB_STRING_LEN(22), .Type = DTYPE_String},
+	.Header                 = {.Size = USB_STRING_LEN(14), .Type = DTYPE_String},
 		
-	.UnicodeString          = L"LUFA Mass Storage Demo"
+	.UnicodeString          = L"LUFA Webserver"
 };
 
 /** This function is called by the library when in device mode, and must be overridden (see library "USB Descriptors"
