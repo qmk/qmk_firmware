f979fd36be0a1d0047a46276226da7ff83fa13fe
Dean Camera <dean@fourwalledcubicle.com>
2010-02-02 12:56:29 +0000

    Remove now unused conf directory from the uIP stack in the Webserver project.

--- Projects/Webserver/Lib/uip/conf/global-conf.h
+++
@@ -1,8 +0,0 @@
-//Project specific configurations
-#ifndef __GLOBAL_CONF_H__
-#define __GLOBAL_CONF_H__
-
-//Include uip.h gives all the uip configurations in uip-conf.h
-#include "uip.h"
-
-#endif /*__GLOBAL_CONF_H__*/
