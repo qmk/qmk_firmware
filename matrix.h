82309deefc21f66d92df08b8eecae8466939e04d
tmk <nobody@nowhere>
2010-09-13 00:00:58 +0900

    add anti-ghost logic

--- matrix.h
+++ matrix.h
@@ -1,6 +1,10 @@
+#include <stdbool.h>
 
 extern uint8_t *matrix;
-extern uint8_t *prev_matrix;
+extern uint8_t *matrix_prev;
 
 void matrix_init(void);
 uint8_t matrix_scan(void);
+bool matrix_is_modified(void);
+bool matrix_has_ghost(void);
+bool matrix_has_ghost_in_row(uint8_t row);
