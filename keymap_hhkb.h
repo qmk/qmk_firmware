34a90769c9b29a24e39f6c2ca06106033dcb755f
tmk <nobody@nowhere>
2010-10-13 22:13:41 +0900

    ';' for Fn key: send ';' when key realease without using the layer

--- keymap_hhkb.h
+++ keymap_hhkb.h
@@ -4,10 +4,16 @@
 #include <stdint.h>
 #include "usb_keycodes.h"
 
-int get_layer(void);
-uint8_t get_keycode(int layer, int row, int col);
 
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 8
+
+
+extern int current_layer;
+extern bool key_sent;
+
+
+int get_layer(void);
+uint8_t get_keycode(int layer, int row, int col);
 
 #endif
