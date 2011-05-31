6d45e05ede8ea1a96df9a04d58a7d7ede51afd9b
tmk <nobody@nowhere>
2011-05-31 21:17:56 +0900

    Added PS/2 multimeda key support. HID Consumer page and System control are also supported now. merged mediakey branch: d53a356cd2011b461843a5c7c1527a61692893c1

--- pjrc/usb_extra.h
+++ pjrc/usb_extra.h
@@ -13,22 +13,11 @@
 
 #define EXTRA_INTERFACE		3
 #define EXTRA_ENDPOINT		4
-#define EXTRA_SIZE		2
+#define EXTRA_SIZE		8
 #define EXTRA_BUFFER		EP_DOUBLE_BUFFER
 
 
-// Consumer Page(0x0C) Consumer Control(0x01)
-#define AUDIO_VOL_UP		(1<<0)
-#define AUDIO_VOL_DOWN		(1<<1)
-#define AUDIO_MUTE		(1<<2)
-
-// Generic Desktop Page(0x01) System Control(0x80)
-#define SYSTEM_POWER_DOWN	(1<<0)
-#define SYSTEM_SLEEP		(1<<1)
-#define SYSTEM_WAKE_UP		(1<<2)
-
-
-int8_t usb_extra_audio_send(uint8_t bits);
-int8_t usb_extra_system_send(uint8_t bits);
+int8_t usb_extra_consumer_send(uint16_t bits);
+int8_t usb_extra_system_send(uint16_t bits);
 
 #endif
