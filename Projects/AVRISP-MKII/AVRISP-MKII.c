55283475d3e1c8191b3569524d35066566ceb0e7
Dean Camera <dean@fourwalledcubicle.com>
2012-05-21 20:45:16 +0000

    Oops - mask out the endpoint address direction when comparing the IN and OUT endpoint indexes in the AVRISP-MKII clone and XPLAINBridge projects.

--- Projects/AVRISP-MKII/AVRISP-MKII.c
+++ Projects/AVRISP-MKII/AVRISP-MKII.c
@@ -101,7 +101,7 @@ void EVENT_USB_Device_ConfigurationChanged(void)
 	ConfigSuccess &= Endpoint_ConfigureEndpoint(AVRISP_DATA_OUT_EPADDR, EP_TYPE_BULK, AVRISP_DATA_EPSIZE, 1);
 
 	/* Setup AVRISP Data IN endpoint if it is using a physically different endpoint */
-	if (AVRISP_DATA_IN_EPADDR != AVRISP_DATA_OUT_EPADDR)
+	if ((AVRISP_DATA_IN_EPADDR & ENDPOINT_EPNUM_MASK) != (AVRISP_DATA_OUT_EPADDR & ENDPOINT_EPNUM_MASK))
 	  ConfigSuccess &= Endpoint_ConfigureEndpoint(AVRISP_DATA_IN_EPADDR, EP_TYPE_BULK, AVRISP_DATA_EPSIZE, 1);
 
 	/* Indicate endpoint configuration success or failure */
