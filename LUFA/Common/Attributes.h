8f2e82bd61f7402c3566d7a0a10e6a19b18218c2
Dean Camera <dean@fourwalledcubicle.com>
2012-05-29 20:03:21 +0000

    Minor cleanup to Attributes.h spacing and rename old multiple-inclusion macro guard.

--- LUFA/Common/Attributes.h
+++ LUFA/Common/Attributes.h
@@ -52,8 +52,8 @@
  *  @{
  */
 
-#ifndef __LUFA_FUNCATTR_H__
-#define __LUFA_FUNCATTR_H__
+#ifndef __LUFA_ATTR_H__
+#define __LUFA_ATTR_H__
 
 	/* Preprocessor Checks: */
 		#if !defined(__INCLUDE_FROM_COMMON_H)
@@ -66,52 +66,52 @@
 				/** Indicates to the compiler that the function can not ever return, so that any stack restoring or
 				 *  return code may be omitted by the compiler in the resulting binary.
 				 */
-				#define ATTR_NO_RETURN              __attribute__ ((noreturn))
+				#define ATTR_NO_RETURN               __attribute__ ((noreturn))
 
 				/** Indicates that the function returns a value which should not be ignored by the user code. When
 				 *  applied, any ignored return value from calling the function will produce a compiler warning.
 				 */
-				#define ATTR_WARN_UNUSED_RESULT     __attribute__ ((warn_unused_result))
+				#define ATTR_WARN_UNUSED_RESULT      __attribute__ ((warn_unused_result))
 
 				/** Indicates that the specified parameters of the function are pointers which should never be \c NULL.
 				 *  When applied as a 1-based comma separated list the compiler will emit a warning if the specified
 				 *  parameters are known at compiler time to be \c NULL at the point of calling the function.
 				 */
-				#define ATTR_NON_NULL_PTR_ARG(...)  __attribute__ ((nonnull (__VA_ARGS__)))
+				#define ATTR_NON_NULL_PTR_ARG(...)   __attribute__ ((nonnull (__VA_ARGS__)))
 
 				/** Removes any preamble or postamble from the function. When used, the function will not have any
 				 *  register or stack saving code. This should be used with caution, and when used the programmer
 				 *  is responsible for maintaining stack and register integrity.
 				 */
-				#define ATTR_NAKED                  __attribute__ ((naked))
+				#define ATTR_NAKED                   __attribute__ ((naked))
 
 				/** Prevents the compiler from considering a specified function for in-lining. When applied, the given
 				 *  function will not be in-lined under any circumstances.
 				 */
-				#define ATTR_NO_INLINE              __attribute__ ((noinline))
+				#define ATTR_NO_INLINE               __attribute__ ((noinline))
 
 				/** Forces the compiler to inline the specified function. When applied, the given function will be
 				 *  in-lined under all circumstances.
 				 */
-				#define ATTR_ALWAYS_INLINE          __attribute__ ((always_inline))
+				#define ATTR_ALWAYS_INLINE           __attribute__ ((always_inline))
 
 				/** Indicates that the specified function is pure, in that it has no side-effects other than global
 				 *  or parameter variable access.
 				 */
-				#define ATTR_PURE                   __attribute__ ((pure))
+				#define ATTR_PURE                    __attribute__ ((pure))
 
 				/** Indicates that the specified function is constant, in that it has no side effects other than
 				 *  parameter access.
 				 */
-				#define ATTR_CONST                  __attribute__ ((const))
+				#define ATTR_CONST                   __attribute__ ((const))
 
 				/** Marks a given function as deprecated, which produces a warning if the function is called. */
-				#define ATTR_DEPRECATED             __attribute__ ((deprecated))
+				#define ATTR_DEPRECATED              __attribute__ ((deprecated))
 
 				/** Marks a function as a weak reference, which can be overridden by other functions with an
 				 *  identical name (in which case the weak reference is discarded at link time).
 				 */
-				#define ATTR_WEAK                   __attribute__ ((weak))
+				#define ATTR_WEAK                    __attribute__ ((weak))
 			#endif
 
 			/** Forces the compiler to not automatically zero the given global variable on startup, so that the
@@ -119,31 +119,31 @@
 			 *  behaviour of volatile memory once power is removed, but may be used in some specific circumstances,
 			 *  like the passing of values back after a system watchdog reset.
 			 */
-			#define ATTR_NO_INIT                    __attribute__ ((section (".noinit")))
+			#define ATTR_NO_INIT                     __attribute__ ((section (".noinit")))
 
 			/** Places the function in one of the initialization sections, which execute before the main function
 			 *  of the application. Refer to the avr-libc manual for more information on the initialization sections.
 			 *
 			 *  \param[in] SectionIndex  Initialization section number where the function should be placed.
 			 */
-			#define ATTR_INIT_SECTION(SectionIndex) __attribute__ ((used, naked, section (".init" #SectionIndex )))
+			#define ATTR_INIT_SECTION(SectionIndex)  __attribute__ ((used, naked, section (".init" #SectionIndex )))
 
 			/** Marks a function as an alias for another function.
 			 *
 			 *  \param[in] Func  Name of the function which the given function name should alias.
 			 */
-			#define ATTR_ALIAS(Func)               __attribute__ ((alias( #Func )))
+			#define ATTR_ALIAS(Func)                 __attribute__ ((alias( #Func )))
 
 			/** Marks a variable or struct element for packing into the smallest space available, omitting any
 			 *  alignment bytes usually added between fields to optimize field accesses.
 			 */
-			#define ATTR_PACKED                     __attribute__ ((packed))
+			#define ATTR_PACKED                      __attribute__ ((packed))
 
 			/** Indicates the minimum alignment in bytes for a variable or struct element.
 			 *
 			 *  \param[in] Bytes  Minimum number of bytes the item should be aligned to.
 			 */
-			#define ATTR_ALIGNED(Bytes)            __attribute__ ((aligned(Bytes)))
+			#define ATTR_ALIGNED(Bytes)              __attribute__ ((aligned(Bytes)))
 #endif
 
 /** @} */
