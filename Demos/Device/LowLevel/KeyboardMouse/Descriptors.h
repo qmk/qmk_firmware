579fbb68fc1aa437c23d85ed961a1d3bf6e6bf85
Dean Camera <dean@fourwalledcubicle.com>
2012-06-27 19:41:30 +0000

    Fixed invalid configuration descriptor in the low level KeyboardMouse device demo (thanks to Jun Wako).

--- Demos/Device/LowLevel/KeyboardMouse/Descriptors.h
+++ Demos/Device/LowLevel/KeyboardMouse/Descriptors.h
@@ -61,7 +61,6 @@
 			USB_Descriptor_Interface_t            HID2_MouseInterface;
 			USB_HID_Descriptor_HID_t              HID2_MouseHID;
 			USB_Descriptor_Endpoint_t             HID2_ReportINEndpoint;
-			USB_Descriptor_Endpoint_t             HID2_ReportOUTEndpoint;
 		} USB_Descriptor_Configuration_t;
 
 	/* Macros: */
