5087e6c3032e2f018c01a751961b94b65062208d
Dean Camera <dean@fourwalledcubicle.com>
2012-07-08 19:38:38 +0000

    Clean up HID EEPROM loader shim application code, simplify project makefile. Ensure shim application is properly rebuild by explicitly cleaning it before requesting a rebuild.

--- LUFA/Build/HID_EEPROM_Loader/HID_EEPROM_Loader.c
+++ LUFA/Build/HID_EEPROM_Loader/HID_EEPROM_Loader.c
@@ -28,14 +28,27 @@
   this software.
 */
 
+/** \file
+ *
+ *  Special application to extract an EEPROM image stored in FLASH memory, and
+ *  copy it to the device EEPROM. This application is designed to be used with
+ *  the HID build system module of LUFA to program the EEPROM of a target device
+ *  that uses the HID bootloader protocol, which does not have native EEPROM
+ *  programming support.
+ */
+
 #include <avr/io.h>
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
 
 /* References to the binary EEPROM data linked in the AVR's FLASH memory space */
-extern const char InputEEData[];
-extern const char InputEEData_size_sym[];
-#define InputEEData_size ((int)InputEEData_size_sym)
+extern const char _binary_InputEEData_bin_start[];
+extern const char _binary_InputEEData_bin_end[];
+extern const char _binary_InputEEData_bin_size[];
+
+/* Friendly names for the embedded binary data stored in FLASH memory space */
+#define InputEEData       _binary_InputEEData_bin_start
+#define InputEEData_size  ((int)_binary_InputEEData_bin_size)
 
 int main(void)
 {
