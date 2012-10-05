eee252603be67c539f9986cde76454f756e55d95
Dean Camera <dean@fourwalledcubicle.com>
2012-10-05 20:22:19 +0000

    Minor documentation improvements.

--- LUFA/Drivers/USB/Core/AVR8/PipeStream_AVR8.h
+++ LUFA/Drivers/USB/Core/AVR8/PipeStream_AVR8.h
@@ -18,7 +18,7 @@
   advertising or publicity pertaining to distribution of the 
   software without specific, written prior permission.
 
-  The author disclaim all warranties with regard to this
+  The author disclaims all warranties with regard to this
   software, including all implied warranties of merchantability
   and fitness.  In no event shall the author be liable for any
   special, indirect or consequential damages or any damages
