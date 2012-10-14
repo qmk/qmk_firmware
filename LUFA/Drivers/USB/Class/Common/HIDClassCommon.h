826a77e90c467bb5e3cf312267463fb645298252
Dean Camera <dean@fourwalledcubicle.com>
2012-10-14 13:58:08 +0000

    Fixed maximum allowed keyboard key code usage of 0x65 rather than 0xFF for the HID_DESCRIPTOR_KEYBOARD() macro (thanks to David Monro)

--- LUFA/Drivers/USB/Class/Common/HIDClassCommon.h
+++ LUFA/Drivers/USB/Class/Common/HIDClassCommon.h
@@ -428,10 +428,10 @@
 				HID_RI_REPORT_SIZE(8, 0x03),                \
 				HID_RI_OUTPUT(8, HID_IOF_CONSTANT),         \
 				HID_RI_LOGICAL_MINIMUM(8, 0x00),            \
-				HID_RI_LOGICAL_MAXIMUM(8, 0x65),            \
+				HID_RI_LOGICAL_MAXIMUM(8, 0xFF),            \
 				HID_RI_USAGE_PAGE(8, 0x07),                 \
 				HID_RI_USAGE_MINIMUM(8, 0x00),              \
-				HID_RI_USAGE_MAXIMUM(8, 0x65),              \
+				HID_RI_USAGE_MAXIMUM(8, 0xFF),              \
 				HID_RI_REPORT_COUNT(8, MaxKeys),            \
 				HID_RI_REPORT_SIZE(8, 0x08),                \
 				HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE), \
