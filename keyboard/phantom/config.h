305b4ee98df39b0d933327bfbc35bc88fa51fe74
tmk <nobody@nowhere>
2012-11-23 12:48:15 +0900

    Fix target name and product id of pahntom.

--- keyboard/phantom/config.h
+++ keyboard/phantom/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0xB919
+#define PRODUCT_ID      0x6057
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    t.m.k.
 #define PRODUCT         Phantom
