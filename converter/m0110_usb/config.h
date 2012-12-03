96951b259d08c37aaff9a847c48cec23b8af2a12
tmk <nobody@nowhere>
2012-12-03 15:08:23 +0900

    Fix debug pirnt and magic key.(M0110)

--- converter/m0110_usb/config.h
+++ converter/m0110_usb/config.h
@@ -36,9 +36,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking Caps Lock support */
 //#define MATRIX_HAS_LOCKING_CAPS
 
-/* key combination for command */
+/* Backspace for command key */
+#define BACKSPACE_CODE  0x33
 #define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LALT) | MOD_BIT(KC_LGUI)) \
+    matrix_is_on((BACKSPACE_CODE>>3)&0x0F, BACKSPACE_CODE&0x07) \
 )
 
 
