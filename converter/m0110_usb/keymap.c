8bf5fca44f2a1baeed87338b957c671efa12cd29
tmk <nobody@nowhere>
2012-12-05 17:49:08 +0900

    Fix keymap and build option.(M0110)

--- converter/m0110_usb/keymap.c
+++ converter/m0110_usb/keymap.c
@@ -27,8 +27,65 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap.h"
 
 
-#define KEYCODE(layer, row, col) (pgm_read_byte(&keymaps[(layer)][(row)][(col)]))
+/* 
+ * The keymap works with both M0110 and M0110A keyboards. As you can see, the M0110A is a superset
+ * of the M0110 keyboard, with only one exception: 'Enter' in M0110 does not exist
+ * on the M0110A, but since it generates a unique scan code which is not used for some other key in
+ * the M0110A, they are totally interchangeable.  In fact, the M0110A is functionally (almost)
+ * identical to the combination of the M0110 along with the M0120 keypad. The only difference
+ * (which is causing some problems as you will read below) is that the M0110+M0120 don't have
+ * dedicated arrow keys, while the M0110A does. However, the M0120 did have arrow keys, which
+ * doubled as the [comma], [/], [*] and [+] keys, when used with the [Shift] key. The M0110A has
+ * substituted the [comma] key with the [=] key, however its scancode is the same.
+ *
+ * Physical layout:
+ * M0110A
+ * ,---------------------------------------------------------. ,---------------.
+ * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Bcksp| |Clr|  =|  /|  *|
+ * |---------------------------------------------------------| |---------------|
+ * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|   | |  7|  8|  9|  -|
+ * |-----------------------------------------------------'   | |---------------|
+ * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return| |  4|  5|  6|  +|
+ * |---------------------------------------------------------| |---------------|
+ * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /|Shft|Up | |  1|  2|  3|   |
+ * |---------------------------------------------------------' |-----------|Ent|
+ * |Opt  |Mac    |           Space           |  \|Lft|Rgt|Dn | |      0|  .|   |
+ * `---------------------------------------------------------' `---------------'
+ *
+ * M0110                                                       M0120
+ * ,---------------------------------------------------------. ,---------------.
+ * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backs| |Clr|  -|  +|  *|
+ * |---------------------------------------------------------| |---------------|
+ * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \| |  7|  8|  9|  /|
+ * |---------------------------------------------------------| |---------------|
+ * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return| |  4|  5|  6|  ,|
+ * |---------------------------------------------------------| |---------------|
+ * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /|Shift   | |  1|  2|  3|   |
+ * `---------------------------------------------------------' |-----------|Ent|
+ *      |Opt|Mac |         Space               |Ent |Opt|      |      0|  .|   |
+ *      `-----------------------------------------------'      `---------------'
+ * With Shift keys on M0120 work as curosor.(-:Left *:Right /:Up ,:Down)
+ *
+ * NOTE: \ is located differently.
+ * NOTE: Enter on M0110 is different from Enter on keypad(M0120 and M0110A).
+ * NOTE: Left Shift and right Shift are logically same key.
+ * NOTE: Left Option and right Option are logically same key.
+ */
 
+/* Keymap definition Macro
+ * ,---------------------------------------------------------. ,---------------.
+ * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backs| |Clr|  =|  /|  *|
+ * |---------------------------------------------------------| |---------------|
+ * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|   | |  7|  8|  9|  -|
+ * |-----------------------------------------------------'   | |---------------|
+ * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return| |  4|  5|  6|  +|
+ * |---------------------------------------------------------| |---------------|
+ * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /|Shft|Up | |  1|  2|  3|   |
+ * |---------------------------------------------------------| |-----------|Ent|
+ * |Opt  |Mac    |         Space         |Ent|  \|Lft|Rgt|Dn | |      0|  .|   |
+ * `---------------------------------------------------------' `---------------'
+ * NOTE: Ent between Space and \ means Enter on M0110.
+ */
 #define KEYMAP( \
     K32,K12,K13,K14,K15,K17,K16,K1A,K1C,K19,K1D,K1B,K18,K33,  K47,K68,K6D,K62, \
     K30,K0C,K0D,K0E,K0F,K11,K10,K20,K22,K1F,K23,K21,K1E,      K59,K5B,K5C,K4E, \
@@ -52,10 +109,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { KC_##K68, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##K6D, KC_NO,    KC_NO    }, \
 }
 
+#define KEYCODE(layer, row, col) (pgm_read_byte(&keymaps[(layer)][(row)][(col)]))
+
 
 // Assign Fn key(0-7) to a layer to which switch with the Fn key pressed.
 static const uint8_t PROGMEM fn_layer[] = {
-#ifndef HASU
     1,              // Fn0
     0,              // Fn1
     0,              // Fn2
@@ -64,22 +122,11 @@ static const uint8_t PROGMEM fn_layer[] = {
     0,              // Fn5
     0,              // Fn6
     0               // Fn7
-#else
-    1,              // Fn0
-    2,              // Fn1
-    3,              // Fn2
-    1,              // Fn3
-    2,              // Fn4
-    0,              // Fn5
-    0,              // Fn6
-    0               // Fn7
-#endif
 };
 
 // Assign Fn key(0-7) to a keycode sent when release Fn key without use of the layer.
 // See layer.c for details.
 static const uint8_t PROGMEM fn_keycode[] = {
-#ifndef HASU
     KC_NO,          // Fn0
     KC_NO,          // Fn1
     KC_NO,          // Fn2
@@ -88,33 +135,12 @@ static const uint8_t PROGMEM fn_keycode[] = {
     KC_NO,          // Fn5
     KC_NO,          // Fn6
     KC_NO           // Fn7
-#else
-    KC_ENTER,       // Fn0
-    KC_SCOLON,      // Fn1
-    KC_SLASH,       // Fn2
-    KC_UP,          // Fn3
-    KC_NO,          // Fn4
-    KC_NO,          // Fn5
-    KC_NO,          // Fn6
-    KC_NO           // Fn7
-#endif
 };
 
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 
-     * The keymap works with both M0110 and M0110A keyboards. As you can see, the M0110A is a superset
-     * of the M0110 keyboard, with only one exception: 'Enter' in M0110 does not exist
-     * on the M0110A, but since it generates a unique scan code which is not used for some other key in
-     * the M0110A, they are totally interchangeable.  In fact, the M0110A is functionally (almost)
-     * identical to the combination of the M0110 along with the M0120 keypad. The only difference
-     * (which is causing some problems as you will read below) is that the M0110+M0120 don't have
-     * dedicated arrow keys, while the M0110A does. However, the M0120 did have arrow keys, which
-     * doubled as the [comma], [/], [*] and [+] keys, when used with the [Shift] key. The M0110A has
-     * substituted the [comma] key with the [=] key, however its scancode is the same.
-     *
-     * Default:
+    /* Default:
      * ,---------------------------------------------------------. ,---------------.
-     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backs| |Gui|  =|  /|  *|
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backs| |Ctl|  =|  /|  *|
      * |---------------------------------------------------------| |---------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|   | |  7|  8|  9|  -|
      * |-----------------------------------------------------'   | |---------------|
@@ -122,79 +148,36 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------| |---------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /|Shft|Up | |  1|  2|  3|   |
      * |---------------------------------------------------------| |-----------|Ent|
-     * |Ctrl |Alt    |         Space         |Gui|  \|Lft|Rgt|Dn | |      0|  .|   |
+     * |Alt  |Gui    |         Space         |Ctl|  \|Lft|Rgt|Dn | |      0|  .|   |
      * `---------------------------------------------------------' `---------------'
-     *
-     * HHKB/WASD/Mouse Layer(Fn0):
-     * ,---------------------------------------------------------. ,---------------.
-     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delet| |Nlk|Mb1|Mb3|Mb2|
-     * |---------------------------------------------------------| |---------------|
-     * |Caps |Hom| Up|PgU|   |   |   |   |Psc|Slk|Pau|Up |Ins|   | |MwD|McU|MwU|MwU|
-     * |-----------------------------------------------------'   | |---------------|
-     * |Fn0   |Lef|Dow|Rig|   |   |   |   |Hom|PgU|Lef|Rig|Return| |McL|McD|McR|MwD|
-     * |---------------------------------------------------------| |---------------|
-     * |Shift   |End|   |PgD|   |VoD|VoU|Mut|End|PgD|Dow|Shif|Up | |MwL|McD|MwR|   |
-     * |---------------------------------------------------------| |-----------|Mb2|
-     * |Ctrl |Alt    |         Space        |Gui |  \|Lft|Rgt|Dn | |    Mb1|Mb3|   |
-     * `---------------------------------------------------------' `---------------'
-     * Mb: Mouse Button / Mc: Mouse Cursor / Mw: Mouse Wheel
-     *
-     * NOTE: M0110 has no arrow and keypad keys.
-     * NOTE: \ is located next to ] on M0110.
-     * NOTE: Gui between Space and \ is Enter on M0110 not exists on M0110A.
-     * NOTE: LShift and RShift are logically same key. (M0110, M0110A)
-     * NOTE: LOption and ROption are logically same key. (M0110)
      */
-#ifndef HASU
     KEYMAP(
-    GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,    LGUI,EQL, PSLS,PAST,
+    GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,    LCTL,EQL, PSLS,PAST,
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,         P7,  P8,  P9,  PMNS,
     FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,     P4,  P5,  P6,  PPLS,
     LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          UP,      P1,  P2,  P3,  PENT,
-    LCTL,LALT,               SPC,                LGUI,BSLS,LEFT,RGHT,DOWN,    P0,       PDOT
+    LALT,LGUI,               SPC,                LCTL,BSLS,LEFT,RGHT,DOWN,    P0,       PDOT
     ),
-    // HHKB & WASD
+    /* Cursor Layer(WASD, IJKL)
+     * ,---------------------------------------------------------. ,---------------.
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delet| |Nlk|  =|  /|  *|
+     * |---------------------------------------------------------| |---------------|
+     * |Caps |Hom| Up|PgU|   |   |   |PgU| Up|Hom|Psc|Slk|Pau|   | |  7|  8|  9|  -|
+     * |-----------------------------------------------------'   | |---------------|
+     * |Fn0   |Lef|Dow|Rig|   |   |   |Lef|Dow|Rig|   |   |Return| |  4|  5|  6|  +|
+     * |---------------------------------------------------------| |---------------|
+     * |Shift   |End|   |PgD|   |   |   |PgD|   |End|   |Shif|PgU| |  1|  2|  3|   |
+     * |---------------------------------------------------------| |-----------|Ent|
+     * |Alt  |Gui    |         Space        |Gui |Ins|Hom|End|PgD| |      0|  .|   |
+     * `---------------------------------------------------------' `---------------'
+     */
     KEYMAP(
     ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,     NLCK,EQL, PSLS,PAST,
-    CAPS,HOME,UP,  PGUP,NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,UP,  NO,           P7,  P8,  P9,  PMNS,
-    FN0, LEFT,DOWN,RGHT,NO,  NO,  NO,  NO,  HOME,PGUP,LEFT,RGHT,     ENT,     P4,  P5,  P6,  PPLS,
-    LSFT,END, NO,  PGDN,NO,  VOLD,VOLU,MUTE,END, PGDN,DOWN,          PGUP,    P1,  P2,  P3,  PENT,
-    LCTL,LALT,               SPC,                LGUI,INS, HOME,END, PGDN,    P0,       PDOT
+    CAPS,HOME,UP,  PGUP,NO,  NO,  NO,  PGUP,UP,  HOME,PSCR,SLCK,PAUS,         P7,  P8,  P9,  PMNS,
+    FN0, LEFT,DOWN,RGHT,NO,  NO,  NO,  LEFT,DOWN,RGHT,NO,  NO,       ENT,     P4,  P5,  P6,  PPLS,
+    LSFT,END, NO,  PGDN,NO,  NO,  NO,  PGDN,NO,  END, NO,            PGUP,    P1,  P2,  P3,  PENT,
+    LALT,LGUI,               SPC,                LCTL,INS, HOME,END, PGDN,    P0,       PDOT
     ),
-#else
-    // hasu's keymap(To enable this use 'make hasu' to add option flag: EXTRAFLAGS=-DHASU)
-    KEYMAP(
-    GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,    LGUI,EQL, PSLS,PAST,
-    TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,         P7,  P8,  P9,  PMNS,
-    LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN1, QUOT,     FN0,     P4,  P5,  P6,  PPLS,
-    LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2,           FN3,     P1,  P2,  P3,  PENT,
-    FN4, LALT,               SPC,                LGUI,BSLS,LEFT,RGHT,DOWN,    LGUI,     PDOT
-    ),
-    // HHKB & WASD
-    KEYMAP(
-    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,     NLCK,EQL, PSLS,PAST,
-    CAPS,HOME,UP,  PGUP,NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS,UP,  NO,           P7,  P8,  P9,  PMNS,
-    LCTL,LEFT,DOWN,RGHT,NO,  NO,  NO,  NO,  HOME,PGUP,LEFT,RGHT,     FN0,     P4,  P5,  P6,  PPLS,
-    LSFT,END, NO,  PGDN,NO,  VOLD,VOLU,MUTE,END, PGDN,DOWN,          FN3,     P1,  P2,  P3,  PENT,
-    LCTL,LALT,               SPC,                LGUI,INS, LEFT,RGHT,DOWN,    P0,       PDOT
-    ),
-    // vi mousekeys
-    KEYMAP(
-    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,     NLCK,EQL, PSLS,PAST,
-    CAPS,NO,  NO,  NO,  NO,  NO,  WH_L,WH_D,WH_U,WH_R,PSCR,SLCK,PAUS,         P7,  P8,  P9,  PMNS,
-    LCTL,VOLD,VOLU,MUTE,NO,  NO,  MS_L,MS_D,MS_U,MS_R,FN1, NO,       ENT,     P4,  P5,  P6,  PPLS,
-    LSFT,NO,  NO,  NO,  NO,  BTN3,BTN2,BTN1,NO,  NO,  NO,            UP,      P1,  P2,  P3,  PENT,
-    FN4, LALT,               BTN1,               LGUI,NO,  LEFT,RGHT,DOWN,    P0,       PDOT
-    ),
-    // vi cusorkeys
-    KEYMAP(
-    ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,     NLCK,EQL, PSLS,PAST,
-    CAPS,NO,  NO,  NO,  NO,  NO,  HOME,PGDN,PGUP,END, PSCR,SLCK,PAUS,         P7,  P8,  P9,  PMNS,
-    LCTL,NO,  NO,  NO,  NO,  NO,  LEFT,DOWN,UP,  RGHT,NO,  NO,       ENT,     P4,  P5,  P6,  PPLS,
-    LSFT,NO,  NO,  NO,  NO,  NO,  HOME,PGDN,PGUP,END, FN2,           UP,      P1,  P2,  P3,  PENT,
-    LCTL,LALT,               SPC,                LGUI,INS, LEFT,RGHT,DOWN,    P0,       PDOT
-    ),
-#endif
 };
 
 
