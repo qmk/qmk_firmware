e6836c3f6df23eb82188bacca15bb0614a0f8b45
tmk <nobody@nowhere>
2013-01-12 10:16:18 +0900

    Fix PWM control of LED in phantom.

--- keyboard/phantom/led.c
+++ keyboard/phantom/led.c
@@ -23,12 +23,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 void led_set(uint8_t usb_led)
 {
     if (!(usb_led & (1<<USB_LED_CAPS_LOCK)))
-        PORTB &= ~(1<<6);
+        DDRB &= ~(1<<6);
     else
-        PORTB |= (1<<6);
+        DDRB |= (1<<6);
 
     if (!(usb_led & (1<<USB_LED_SCROLL_LOCK)))
-        PORTB &= ~(1<<7);
+        DDRB &= ~(1<<7);
     else
-        PORTB |= (1<<7);
+        DDRB |= (1<<7);
 }
