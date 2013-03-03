0c40ef8897448c56709bd26fb766c6e30635eea9
Dean Camera <dean@fourwalledcubicle.com>
2013-03-03 14:34:07 +0000

    Add documentation to the Printer class bootloader, add LED flash when the bootloader is active.

--- Bootloaders/Printer/BootloaderPrinter.c
+++ Bootloaders/Printer/BootloaderPrinter.c
@@ -297,6 +297,16 @@ void SetupHardware(void)
 	/* Hardware Initialization */
 	LEDs_Init();
 	USB_Init();
+
+	/* Bootloader active LED toggle timer initialization */
+	TIMSK1 = (1 << TOIE1);
+	TCCR1B = ((1 << CS11) | (1 << CS10));
+}
+
+/** ISR to periodically toggle the LEDs on the board to indicate that the bootloader is active. */
+ISR(TIMER1_OVF_vect, ISR_BLOCK)
+{
+	LEDs_ToggleLEDs(LEDS_LED1 | LEDS_LED2);
 }
 
 /** Event handler for the USB_Connect event. This indicates that the device is enumerating via the status LEDs. */
