d5cff25cd14053965fcf072a7109493155a531c3
Dean Camera <dean@fourwalledcubicle.com>
2013-03-09 11:11:05 +0000

    Update Bootloader buld test to generate bootloaders using the new Printer class bootloader. Optimize incomplete Mass Storage bootloader LUFA configuration.

--- Bootloaders/Incomplete/MassStorage/Config/LUFAConfig.h
+++ Bootloaders/Incomplete/MassStorage/Config/LUFAConfig.h
@@ -57,13 +57,13 @@
 //		#define NO_CLASS_DRIVER_AUTOFLUSH
 
 		/* General USB Driver Related Tokens: */
-//		#define ORDERED_EP_CONFIG
+		#define ORDERED_EP_CONFIG
 		#define USE_STATIC_OPTIONS               (USB_DEVICE_OPT_FULLSPEED | USB_OPT_REG_ENABLED | USB_OPT_AUTO_PLL)
 		#define USB_DEVICE_ONLY
 //		#define USB_HOST_ONLY
 //		#define USB_STREAM_TIMEOUT_MS            {Insert Value Here}
 //		#define NO_LIMITED_CONTROLLER_CONNECT
-//		#define NO_SOF_EVENTS
+		#define NO_SOF_EVENTS
 
 		/* USB Device Mode Driver Related Tokens: */
 		#define USE_RAM_DESCRIPTORS
@@ -71,7 +71,7 @@
 //		#define USE_EEPROM_DESCRIPTORS
 //		#define NO_INTERNAL_SERIAL
 		#define FIXED_CONTROL_ENDPOINT_SIZE      8
-//		#define DEVICE_STATE_AS_GPIOR            {Insert Value Here}
+		#define DEVICE_STATE_AS_GPIOR            0
 		#define FIXED_NUM_CONFIGURATIONS         1
 //		#define CONTROL_ONLY_DEVICE
 		#define INTERRUPT_CONTROL_ENDPOINT
