1385058fc21f80347c7afd2b818e38795ce9d55a
tmk <nobody@nowhere>
2013-03-09 22:23:32 +0900

    Fix gh60/matrix.c and Makefile

--- keyboard/gh60/matrix.c
+++ keyboard/gh60/matrix.c
@@ -120,7 +120,7 @@ bool matrix_has_ghost(void)
 inline
 bool matrix_is_on(uint8_t row, uint8_t col)
 {
-    return (matrix[row] & (1<<col));
+    return (matrix[row] & ((matrix_row_t)1<<col));
 }
 
 inline
