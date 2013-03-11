a0ef9fade23170bf569e037e2452fb01330fdaf5
tmk <nobody@nowhere>
2013-03-11 15:28:14 +0900

    Add build option BOOTMAGIC_ENABLE

--- common.mk
+++ common.mk
@@ -10,12 +10,16 @@ SRC +=	$(COMMON_DIR)/host.c \
 	$(COMMON_DIR)/print.c \
 	$(COMMON_DIR)/debug.c \
 	$(COMMON_DIR)/bootloader.c \
-	$(COMMON_DIR)/bootmagic.c \
-	$(COMMON_DIR)/eeconfig.c \
 	$(COMMON_DIR)/util.c
 
 
 # Option modules
+ifdef BOOTMAGIC_ENABLE
+    SRC += $(COMMON_DIR)/bootmagic.c
+    SRC += $(COMMON_DIR)/eeconfig.c
+    OPT_DEFS += -DBOOTMAGIC_ENABLE
+endif
+
 ifdef MOUSEKEY_ENABLE
     SRC += $(COMMON_DIR)/mousekey.c
     OPT_DEFS += -DMOUSEKEY_ENABLE
