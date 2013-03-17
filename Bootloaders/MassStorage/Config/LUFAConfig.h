f429975e01c4a7b76e5bafccf4e8e41489c58340
Dean Camera <dean@fourwalledcubicle.com>
2013-03-17 16:37:00 +0000

    Tweak the LUFA config for the Mass Storage class bootloader to reduce the compiled size slightly by removing unused features.

--- Bootloaders/MassStorage/Config/LUFAConfig.h
+++ Bootloaders/MassStorage/Config/LUFAConfig.h
@@ -75,8 +75,8 @@
 		#define FIXED_NUM_CONFIGURATIONS         1
 //		#define CONTROL_ONLY_DEVICE
 		#define INTERRUPT_CONTROL_ENDPOINT
-//		#define NO_DEVICE_REMOTE_WAKEUP
-//		#define NO_DEVICE_SELF_POWER
+		#define NO_DEVICE_REMOTE_WAKEUP
+		#define NO_DEVICE_SELF_POWER
 
 		/* USB Host Mode Driver Related Tokens: */
 //		#define HOST_STATE_AS_GPIOR              {Insert Value Here}
