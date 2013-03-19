ecb7550a91a7fb9a2e7e717f3c0d40d30fd92121
tmk <nobody@nowhere>
2013-03-20 00:37:32 +0900

    Add NO_ACTION_FUNCTION config option

--- keyboard/gh60/config.h
+++ keyboard/gh60/config.h
@@ -64,5 +64,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_OVERLAY
 #define NO_ACTION_TAPPING
 #define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
 
 #endif
