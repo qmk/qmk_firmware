54e46d7818ad14f4a00a9df905f26f135a462e6d
Dean Camera <dean@fourwalledcubicle.com>
2013-03-20 20:03:00 +0000

    Add smaller AVR8 targets to the Mass Storage bootloader target list in the Bootloader build test.

--- Bootloaders/MassStorage/BootloaderMassStorage.c
+++ Bootloaders/MassStorage/BootloaderMassStorage.c
@@ -69,11 +69,13 @@ void Application_Jump_Check(void)
 		/* Enable pull-up on the IO13 pin so we can use it to select the mode */
 		PORTC |=  (1 << 7);
 		Delay_MS(10);
-		JumpToApplication |= ((PINC & (1 << 7)) != 0);
-		PORTC &= ~(1 << 7);
-	#endif
 
-	#if ((BOARD == BOARD_XPLAIN) || (BOARD == BOARD_XPLAIN_REV1))
+		/* If IO13 is not jumpered to ground, start the user application instead */
+		JumpToApplication |= ((PINC & (1 << 7)) != 0);
+
+		/* Disable pull-up after the check has completed */
+		PORTC &= ~(1 << 7);
+	#elif ((BOARD == BOARD_XPLAIN) || (BOARD == BOARD_XPLAIN_REV1))
 		/* Disable JTAG debugging */
 		JTAG_DISABLE();
 
