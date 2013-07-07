8d1376bbe92123465bbad9881832404e762d6342
Dean Camera <dean@fourwalledcubicle.com>
2013-07-07 13:00:44 +0200

    Clean up Atmel Studio integration Python script according to PEP8 rules.

--- LUFA/StudioIntegration/VSIX/generate_caches.py
+++ LUFA/StudioIntegration/VSIX/generate_caches.py
@@ -11,27 +11,27 @@ sys.path.append("ProjectGenerator")
 
 
 def show_message(message):
-	print "[Project Generator] %s" % message
-	sys.stdout.flush()
+    print("[Project Generator] %s" % message)
+    sys.stdout.flush()
 
 
 def main(lufa_root_path):
-	try:
-		from asf_avrstudio5_interface import PythonFacade
-	except ImportError:
-		print "Fatal Error: The ASF project generator is missing."
-		return 1
+    try:
+        from asf_avrstudio5_interface import PythonFacade
+    except ImportError:
+        print("Fatal Error: The ASF project generator is missing.")
+        return 1
 
-	p = PythonFacade(lufa_root_path)
+    p = PythonFacade(lufa_root_path)
 
-	show_message("Checking database sanity...")
-	p.check_extension_database_sanity(lufa_root_path)
+    show_message("Checking database sanity...")
+    p.check_extension_database_sanity(lufa_root_path)
 
-	show_message("Building cache files...")
-	p.generate_extension_cache_files(lufa_root_path)
+    show_message("Building cache files...")
+    p.generate_extension_cache_files(lufa_root_path)
 
-	show_message("Cache files created.")
-	return 0
+    show_message("Cache files created.")
+    return 0
 
 
 if __name__ == "__main__":
