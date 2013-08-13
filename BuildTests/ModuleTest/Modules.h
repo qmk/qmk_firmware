23910fa09e3d71dcd091b5ebcc1ce6a7f6fa9411
Dean Camera <dean@fourwalledcubicle.com>
2013-08-13 21:35:21 +0200

    Add XMEGA TWI to the ModuleTest build test.

--- BuildTests/ModuleTest/Modules.h
+++ BuildTests/ModuleTest/Modules.h
@@ -30,6 +30,8 @@
 
 #include <LUFA/Common/Common.h>
 
+#include <LUFA/Platform/Platform.h>
+
 #include <LUFA/Drivers/USB/USB.h>
 #include <LUFA/Drivers/Misc/RingBuffer.h>
 #include <LUFA/Drivers/Misc/TerminalCodes.h>
@@ -43,16 +45,12 @@
 		#include <LUFA/Drivers/Peripheral/ADC.h>
 	#endif
 
-	#include <LUFA/Drivers/Peripheral/Serial.h>	
-	#include <LUFA/Drivers/Peripheral/SPI.h>
-	#include <LUFA/Drivers/Peripheral/SerialSPI.h>
-#elif (ARCH == ARCH_XMEGA)
 	#include <LUFA/Drivers/Peripheral/Serial.h>
 	#include <LUFA/Drivers/Peripheral/SPI.h>
 	#include <LUFA/Drivers/Peripheral/SerialSPI.h>
-	
-	#include <LUFA/Platform/XMEGA/ClockManagement.h>
-#elif (ARCH == ARCH_UC3)
-	#include <LUFA/Platform/UC3/ClockManagement.h>
-	#include <LUFA/Platform/UC3/InterruptManagement.h>
+#elif (ARCH == ARCH_XMEGA)
+	#include <LUFA/Drivers/Peripheral/TWI.h>
+	#include <LUFA/Drivers/Peripheral/Serial.h>
+	#include <LUFA/Drivers/Peripheral/SPI.h>
+	#include <LUFA/Drivers/Peripheral/SerialSPI.h>
 #endif
