fd7833388cdf8b15728d951b63e78571ba2edbc9
tmk <nobody@nowhere>
2013-11-29 08:42:40 +0900

    Add new keymap framework

---
+++ keyboard/hhkb/keymap_common.h
@@ -0,0 +1,57 @@
+/*
+Copyright 2012,2013 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef KEYMAP_COMMON_H
+#define KEYMAP_COMMON_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "keycode.h"
+#include "action.h"
+#include "action_code.h"
+#include "action_layer.h"
+#include "action_macro.h"
+#include "action_util.h"
+#include "report.h"
+#include "host.h"
+#include "print.h"
+#include "debug.h"
+#include "keymap.h"
+
+
+extern const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS];
+extern const uint16_t fn_actions[];
+
+
+#define KEYMAP( \
+    K31, K30, K00, K10, K11, K20, K21, K40, K41, K60, K61, K70, K71, K50, K51, \
+    K32, K01, K02, K13, K12, K23, K22, K42, K43, K62, K63, K73, K72, K52, \
+    K33, K04, K03, K14, K15, K24, K25, K45, K44, K65, K64, K74, K53, \
+    K34, K05, K06, K07, K16, K17, K26, K46, K66, K76, K75, K55, K54, \
+         K35, K36,           K37,                K57, K56 \
+) \
+{ \
+    { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07 }, \
+    { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17 }, \
+    { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_NO    }, \
+    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37 }, \
+    { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K46, KC_NO    }, \
+    { KC_##K50, KC_##K51, KC_##K52, KC_##K53, KC_##K54, KC_##K55, KC_##K56, KC_##K57 }, \
+    { KC_##K60, KC_##K61, KC_##K62, KC_##K63, KC_##K64, KC_##K65, KC_##K66, KC_NO    }, \
+    { KC_##K70, KC_##K71, KC_##K72, KC_##K73, KC_##K74, KC_##K75, KC_##K76, KC_NO    } \
+}
+
+#endif
--- keyboard/hhkb/keymap_hasu.c
+++ keyboard/hhkb/keymap_hasu.c
@@ -1,57 +1,13 @@
-/*
-Copyright 2011,2012,2013 Jun Wako <wakojun@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 /* 
- * Keymap for PFU HHKB Pro
+ * Hasu: my personal keymap
  */
-#include <stdint.h>
-#include <stdbool.h>
-#include <avr/pgmspace.h>
-#include "keycode.h"
-#include "action.h"
-#include "action_macro.h"
-#include "report.h"
-#include "host.h"
-#include "debug.h"
-#include "keymap.h"
+#include "keymap_common.h"
 
-
-#define KEYMAP( \
-    K31, K30, K00, K10, K11, K20, K21, K40, K41, K60, K61, K70, K71, K50, K51, \
-    K32, K01, K02, K13, K12, K23, K22, K42, K43, K62, K63, K73, K72, K52, \
-    K33, K04, K03, K14, K15, K24, K25, K45, K44, K65, K64, K74, K53, \
-    K34, K05, K06, K07, K16, K17, K26, K46, K66, K76, K75, K55, K54, \
-         K35, K36,           K37,                K57, K56 \
-) \
-{ \
-    { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07 }, \
-    { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17 }, \
-    { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_NO    }, \
-    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37 }, \
-    { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K46, KC_NO    }, \
-    { KC_##K50, KC_##K51, KC_##K52, KC_##K53, KC_##K54, KC_##K55, KC_##K56, KC_##K57 }, \
-    { KC_##K60, KC_##K61, KC_##K62, KC_##K63, KC_##K64, KC_##K65, KC_##K66, KC_NO    }, \
-    { KC_##K70, KC_##K71, KC_##K72, KC_##K73, KC_##K74, KC_##K75, KC_##K76, KC_NO    } \
-}
 
 #ifdef KEYMAP_SECTION_ENABLE
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
 #else
-static const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
+const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
     /* Layer 0: Default Layer
      * ,-----------------------------------------------------------.
@@ -69,7 +25,7 @@ static const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, \
            TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
            LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN3, QUOT,FN4, \
-            FN5,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2, RSFT,FN1, \
+           FN5, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, FN2, RSFT,FN1, \
                 LGUI,LALT,          FN6,                RALT,RGUI),
 
     /* Layer 1: HHKB mode (HHKB Fn)
@@ -87,7 +43,7 @@ static const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      */ 
     KEYMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
            CAPS,NO,  NO,  NO,  NO,  NO,  NO,  NO,  PSCR,SLCK,PAUS, UP,  NO,  BSPC, \
-           LCTL,VOLD,VOLU,MUTE,NO,  NO,  PAST,PSLS,HOME,PGUP,LEFT,RGHT,ENT, \
+           LCTL,VOLD,VOLU,MUTE,NO,  FN9, PAST,PSLS,HOME,PGUP,LEFT,RGHT,ENT, \
            LSFT,NO,  NO,  NO,  NO,  NO,  PPLS,PMNS,END, PGDN,DOWN,RSFT,TRNS, \
                 LGUI,LALT,          SPC,                RALT,RGUI),
 
@@ -192,7 +148,7 @@ enum macro_id {
 #ifdef KEYMAP_SECTION_ENABLE
 const uint16_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
 #else
-static const uint16_t fn_actions[] PROGMEM = {
+const uint16_t fn_actions[] PROGMEM = {
 #endif
     [0] = ACTION_DEFAULT_LAYER_SET(0),                // Default layer(not used)
     [1] = ACTION_LAYER_TAP_TOGGLE(1),                 // HHKB layer(toggle with 5 taps)
@@ -203,11 +159,12 @@ static const uint16_t fn_actions[] PROGMEM = {
     [6] = ACTION_LAYER_TAP_KEY(5, KC_SPC),            // Mousekey layer with Space
     [7] = ACTION_LAYER_TOGGLE(3),                     // Mousekey layer(toggle)
     [8] = ACTION_MODS_KEY(MOD_LCTL, KC_W),            // Close Tab
+    [9] = ACTION_MODS_KEY(MOD_LSFT, KC_4),            // Shift stuck test
 
 //  [8] = ACTION_LMOD_TAP_KEY(KC_LCTL, KC_BSPC),       // LControl with tap Backspace
 //  [9] = ACTION_LMOD_TAP_KEY(KC_LCTL, KC_ESC),        // LControl with tap Esc
-//  [11] = ACTION_FUNCTION_TAP(LSHIFT_LPAREN),         // Function: LShift with tap '('
-//  [12] = ACTION_FUNCTION_TAP(RSHIFT_RPAREN),         // Function: RShift with tap ')'
+  [11] = ACTION_FUNCTION_TAP(LSHIFT_LPAREN),         // Function: LShift with tap '('
+  [12] = ACTION_FUNCTION_TAP(RSHIFT_RPAREN),         // Function: RShift with tap ')'
 //  [13] = ACTION_MACRO_TAP(LSHIFT_PAREN),             // Macro: LShift with tap '('
 //  [14] = ACTION_MACRO_TAP(RSHIFT_PAREN),             // Macro: RShift with tap ')'
 //  [15] = ACTION_MACRO(HELLO),                        // Macro: say hello
@@ -268,18 +225,20 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
             // NOTE: cant use register_code to avoid conflicting with magic key bind
             if (event.pressed) {
                 if (tap.count == 0 || tap.interrupted) {
-                    add_mods(MOD_BIT(KC_LSHIFT));
+                    //add_mods(MOD_BIT(KC_LSHIFT));
+                    layer_on(1);
                 } else {
-                    host_add_mods(MOD_BIT(KC_LSHIFT));
-                    host_add_key(KC_9);
-                    host_send_keyboard_report();
-                    host_del_mods(MOD_BIT(KC_LSHIFT));
-                    host_del_key(KC_9);
-                    host_send_keyboard_report();
+                    add_mods(MOD_BIT(KC_LSHIFT));
+                    add_key(KC_9);
+                    send_keyboard_report();
+                    del_mods(MOD_BIT(KC_LSHIFT));
+                    del_key(KC_9);
+                    send_keyboard_report();
                 }
             } else {
                 if (tap.count == 0 || tap.interrupted) {
-                    del_mods(MOD_BIT(KC_LSHIFT));
+                    //del_mods(MOD_BIT(KC_LSHIFT));
+                    layer_off(1);
                 }
             }
             break;
@@ -289,12 +248,12 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
                 if (tap.count == 0 || tap.interrupted) {
                     add_mods(MOD_BIT(KC_RSHIFT));
                 } else {
-                    host_add_mods(MOD_BIT(KC_RSHIFT));
-                    host_add_key(KC_0);
-                    host_send_keyboard_report();
-                    host_del_mods(MOD_BIT(KC_RSHIFT));
-                    host_del_key(KC_0);
-                    host_send_keyboard_report();
+                    add_mods(MOD_BIT(KC_RSHIFT));
+                    add_key(KC_0);
+                    send_keyboard_report();
+                    del_mods(MOD_BIT(KC_RSHIFT));
+                    del_key(KC_0);
+                    send_keyboard_report();
                 }
             } else {
                 if (tap.count == 0 || tap.interrupted) {
@@ -303,20 +262,4 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
             }
             break;
     }
-}
-
-
-
-/* translates key to keycode */
-uint8_t keymap_key_to_keycode(uint8_t layer, key_t key)
-{
-    return pgm_read_byte(&keymaps[(layer)][(key.row)][(key.col)]);
-}
-
-/* translates Fn index to action */
-action_t keymap_fn_to_action(uint8_t keycode)
-{
-    action_t action;
-    action.code = pgm_read_word(&fn_actions[FN_INDEX(keycode)]);
-    return action;
 }
