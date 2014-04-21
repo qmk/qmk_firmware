c6902681e72411fbe293657a9d75a2168021a6d8
tmk <nobody@nowhere>
2014-04-21 12:18:12 +0900

    Change PollingIntervalMS to 10ms(Fix #114)

--- protocol/lufa/descriptor.c
+++ protocol/lufa/descriptor.c
@@ -306,7 +306,7 @@ const USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor =
             .EndpointAddress        = (ENDPOINT_DIR_IN | KEYBOARD_IN_EPNUM),
             .Attributes             = (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
             .EndpointSize           = KEYBOARD_EPSIZE,
-            .PollingIntervalMS      = 0x01
+            .PollingIntervalMS      = 0x0A
         },
 
     /*
