8f78affa27a49ecfee9ece4fe4a30ebd7bbc6dcb
tmk <nobody@nowhere>
2014-07-01 04:29:45 +0900

    Add RN-42 Bluetooth module support

--- keyboard/hhkb_rn42/rn42.h
+++ keyboard/hhkb_rn42/rn42.h
@@ -1,6 +1,15 @@
 #ifndef RN42_H
 #define RN42_H
 
+#include <stdbool.h>
+
 host_driver_t rn42_driver;
+host_driver_t rn42_config_driver;
+
+void rn42_init(void);
+void rn42_putc(uint8_t c);
+void rn42_autoconnect(void);
+void rn42_disconnect(void);
+bool rn42_ready(void);
 
 #endif
