e2b42d25a35ad9fe04282266d610ffd83dc498f1
tmk <nobody@nowhere>
2014-08-20 13:36:15 +0900

    Add support for HHKB JP

--- keyboard/hhkb_rn42/matrix.c
+++ keyboard/hhkb_rn42/matrix.c
@@ -20,20 +20,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <stdint.h>
 #include <stdbool.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "print.h"
 #include "debug.h"
 #include "util.h"
 #include "timer.h"
 #include "matrix.h"
-
-
-// Timer resolution check
-#if (1000000/TIMER_RAW_FREQ > 20)
-#   error "Timer resolution(>20us) is not enough for HHKB matrix scan tweak on V-USB."
-#endif
+#include "hhkb_avr.h"
 
 
 // matrix state buffer(1:on, 0:off)
@@ -41,122 +34,6 @@ static matrix_row_t *matrix;
 static matrix_row_t *matrix_prev;
 static matrix_row_t _matrix0[MATRIX_ROWS];
 static matrix_row_t _matrix1[MATRIX_ROWS];
-
-
-// Matrix I/O ports
-//
-// row:     HC4051[A,B,C]  selects scan row0-7
-// col:     LS145[A,B,C,D] selects scan col0-7 and enable(D)
-// key:     on: 0/off: 1
-// prev:    unknown: output previous key state(negated)?
-
-#if defined(__AVR_AT90USB1286__)
-// Ports for Teensy++
-// row:     PB0-2
-// col:     PB3-5,6
-// key:     PE6(pull-uped)
-// prev:    PE7
-#define KEY_INIT()              do {    \
-    DDRB |= 0x7F;                       \
-    DDRE |=  (1<<7);                    \
-    DDRE &= ~(1<<6);                    \
-    PORTE |= (1<<6);                    \
-} while (0)
-#define KEY_SELECT(ROW, COL)    (PORTB = (PORTB & 0xC0) |       \
-                                         (((COL) & 0x07)<<3) |  \
-                                         ((ROW) & 0x07))
-#define KEY_ENABLE()            (PORTB &= ~(1<<6))
-#define KEY_UNABLE()            (PORTB |=  (1<<6))
-#define KEY_STATE()             (PINE & (1<<6))
-#define KEY_PREV_ON()           (PORTE |=  (1<<7))
-#define KEY_PREV_OFF()          (PORTE &= ~(1<<7))
-#define KEY_POWER_ON()
-#define KEY_POWER_OFF()
-
-#elif defined(__AVR_ATmega32U4__)
-// Ports for my designed Alt Controller PCB
-// row:     PB0-2
-// col:     PB3-5,6
-// key:     PD7(pull-uped)
-// prev:    PB7
-// power:   PD4(L:off/H:on)
-#define KEY_INIT()              do {    \
-    DDRB  = 0xFF;                       \
-    PORTB = 0x00;                       \
-    DDRD  &= ~0x80;                     \
-    PORTD |= 0x80;                      \
-    /* keyswitch board power on */      \
-    DDRD  |=  (1<<4);                   \
-    PORTD |=  (1<<4);                   \
-    KEY_UNABLE();                       \
-    KEY_PREV_OFF();                     \
-} while (0)
-#define KEY_SELECT(ROW, COL)    (PORTB = (PORTB & 0xC0) |       \
-                                         (((COL) & 0x07)<<3) |  \
-                                         ((ROW) & 0x07))
-#define KEY_ENABLE()            (PORTB &= ~(1<<6))
-#define KEY_UNABLE()            (PORTB |=  (1<<6))
-#define KEY_STATE()             (PIND & (1<<7))
-#define KEY_PREV_ON()           (PORTB |=  (1<<7))
-#define KEY_PREV_OFF()          (PORTB &= ~(1<<7))
-#define KEY_POWER_ON()
-#define KEY_POWER_OFF()
-/*
-#define KEY_POWER_ON()          do {    \
-    KEY_INIT();                         \
-    PORTD |=  (1<<4);                   \
-    _delay_ms(1);                       \
-} while (0)
-#define KEY_POWER_OFF()         do {    \
-    PORTD &= ~(1<<4);                   \
-    DDRB  &= ~0xFF;                     \
-    PORTB &= ~0xFF;                     \
-    DDRB  &= ~0x80;                     \
-    PORTB &= ~0x80;                     \
-} while (0)
-*/
-
-
-#elif defined(__AVR_ATmega328P__)
-// Ports for V-USB
-// key:     PB0(pull-uped)
-// prev:    PB1
-// row:     PB2-4
-// col:     PC0-2,3
-// power:   PB5(Low:on/Hi-z:off)
-#define KEY_INIT()              do {    \
-    DDRB  |= 0x3E;                      \
-    DDRB  &= ~(1<<0);                   \
-    PORTB |= 1<<0;                      \
-    DDRC  |= 0x0F;                      \
-    KEY_UNABLE();                       \
-    KEY_PREV_OFF();                     \
-} while (0)
-#define KEY_SELECT(ROW, COL)    do {    \
-    PORTB = (PORTB & 0xE3) | ((ROW) & 0x07)<<2; \
-    PORTC = (PORTC & 0xF8) | ((COL) & 0x07);    \
-} while (0)
-#define KEY_ENABLE()            (PORTC &= ~(1<<3))
-#define KEY_UNABLE()            (PORTC |=  (1<<3))
-#define KEY_STATE()             (PINB & (1<<0))
-#define KEY_PREV_ON()           (PORTB |=  (1<<1))
-#define KEY_PREV_OFF()          (PORTB &= ~(1<<1))
-// Power supply switching
-#define KEY_POWER_ON()          do {    \
-    KEY_INIT();                         \
-    PORTB &= ~(1<<5);                   \
-    _delay_ms(1);                       \
-} while (0)
-#define KEY_POWER_OFF()         do {    \
-    DDRB  &= ~0x3F;                     \
-    PORTB &= ~0x3F;                     \
-    DDRC  &= ~0x0F;                     \
-    PORTC &= ~0x0F;                     \
-} while (0)
-
-#else
-#   error "define code for matrix scan"
-#endif
 
 
 inline
@@ -199,13 +76,13 @@ uint8_t matrix_scan(void)
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
         for (uint8_t col = 0; col < MATRIX_COLS; col++) {
             KEY_SELECT(row, col);
-            _delay_us(40);
+            _delay_us(5);
 
             // Not sure this is needed. This just emulates HHKB controller's behaviour.
             if (matrix_prev[row] & (1<<col)) {
                 KEY_PREV_ON();
             }
-            _delay_us(7);
+            _delay_us(10);
 
             // NOTE: KEY_STATE is valid only in 20us after KEY_ENABLE.
             // If V-USB interrupts in this section we could lose 40us or so
@@ -241,11 +118,13 @@ uint8_t matrix_scan(void)
                 matrix[row] = matrix_prev[row];
             }
 
+            _delay_us(5);
             KEY_PREV_OFF();
             KEY_UNABLE();
+
             // NOTE: KEY_STATE keep its state in 20us after KEY_ENABLE.
             // This takes 25us or more to make sure KEY_STATE returns to idle state.
-            _delay_us(150);
+            _delay_us(75);
         }
     }
     KEY_POWER_OFF();
