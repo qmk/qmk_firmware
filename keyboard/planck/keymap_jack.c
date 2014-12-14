ed8d9a099d90d0a367d3887a26a1efa9129bc345
Jack Humbert <jack.humb@gmail.com>
2014-12-14 10:30:27 -0500

    added planck folder

--- keyboard/planck/keymap_jack.c
+++ keyboard/planck/keymap_jack.c
@@ -5,7 +5,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
   ESC,  A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,
   LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, ENT,
-  RSFT, LCTL, LALT, LGUI, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
+  FN26, LCTL, LALT, LGUI, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
 [1] = KEYMAP( /* Jack RAISE */
   GRV,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    BSPC,
   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MINS, EQL,  LBRC, RBRC, BSLS,
@@ -13,7 +13,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TRNS, TRNS, TRNS, TRNS, TRNS,    TRNS,   FN1,  MNXT, VOLD, VOLU, MPLY),
 [2] = KEYMAP( /* Jack LOWER */
   FN22, FN10, FN11, FN12, FN13, FN14, FN15, FN16, FN17, FN18, FN19, BSPC,
-  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, FN20, FN21, FN23, FN24, FN28,
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, FN20, FN21, FN23, FN24, FN25,
   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
   TRNS, TRNS, TRNS, TRNS, FN2,    TRNS,    TRNS, MNXT, VOLD, VOLU, MPLY),
 };
@@ -36,5 +36,7 @@ const uint16_t PROGMEM fn_actions[] = {
     [22] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),
     [23] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC),
     [24] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),
-    [28] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
+    [25] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
+
+    [26] = ACTION_MODS_KEY(MOD_LSFT, MOD_RSFT),
 };
