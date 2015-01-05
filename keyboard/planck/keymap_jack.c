62c2149152c1e6e7a5b3880cc7a2045a3b8daf40
Jack Humbert <jack.humb@gmail.com>
2015-01-05 13:11:34 -0500

    Update keymap_jack.c

--- keyboard/planck/keymap_jack.c
+++ keyboard/planck/keymap_jack.c
@@ -5,21 +5,30 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
   ESC,  A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,
   LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, ENT,
-  FN26, LCTL, LALT, LGUI, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
-[1] = KEYMAP( /* Jack RAISE */
+  RSFT, LCTL, LALT, LGUI, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
+[1] = KEYMAP( /* Jack colemak */
+  TAB,  Q,    W,    F,    P,    G,    J,    L,    U,    Y,    SCLN, BSPC,
+  ESC,  A,    R,    S,    T,    D,    H,    N,    E,    I,    O,     QUOT,
+  LSFT, Z,    X,    C,    V,    B,    K,    M,    COMM, DOT,  SLSH, ENT,
+  FN3, LCTL, LALT, LGUI, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
+[2] = KEYMAP( /* Jack RAISE */
   GRV,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    BSPC,
-  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MINS, EQL,  LBRC, RBRC, BSLS,
-  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MINS, TRNS,
+  TRNS, FN3, FN4, TRNS, TRNS, TRNS, TRNS, MINS, EQL,  LBRC, RBRC, BSLS,
+  TRNS, F11,  F12,  F13,  F14,  F15,  F16,  F17,  F18,  F19,  F20, TRNS,
   TRNS, TRNS, TRNS, TRNS, TRNS,    TRNS,   FN1,  MNXT, VOLD, VOLU, MPLY),
-[2] = KEYMAP( /* Jack LOWER */
+[3] = KEYMAP( /* Jack LOWER */
   FN22, FN10, FN11, FN12, FN13, FN14, FN15, FN16, FN17, FN18, FN19, BSPC,
-  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, FN20, FN21, FN23, FN24, FN25,
-  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
+  TRNS, FN3, FN4, TRNS, TRNS, TRNS, TRNS, FN20, FN21, FN23, FN24, FN28,
+  TRNS, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  TRNS,
   TRNS, TRNS, TRNS, TRNS, FN2,    TRNS,    TRNS, MNXT, VOLD, VOLU, MPLY),
 };
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
-    [2] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
+    [1] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
+    [2] = ACTION_LAYER_MOMENTARY(3),  // to Fn overlay 
+    // [1] = ACTION_LAYER_ON_OFF(1),
+    // [2] = ACTION_LAYER_ON_OFF(2),
+    [3] = ACTION_DEFAULT_LAYER_SET(0), 
+    [4] = ACTION_DEFAULT_LAYER_SET(1),  
 
     [10] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
     [11] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
@@ -36,7 +45,5 @@ const uint16_t PROGMEM fn_actions[] = {
     [22] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),
     [23] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC),
     [24] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),
-    [25] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
-
-    [26] = ACTION_MODS_KEY(MOD_LSFT, MOD_RSFT),
+    [28] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
 };
