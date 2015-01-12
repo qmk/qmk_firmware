42fe42274fe0ff2f10698100f28ab686822652ff
Jack Humbert <jack.humb@gmail.com>
2015-01-12 12:37:16 -0500

    names

--- keyboard/planck/keymap_jack.c
+++ keyboard/planck/keymap_jack.c
@@ -2,13 +2,13 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = KEYMAP( /* Jack */
-  TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
-  ESC,  A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,
+  ESC,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
+  TAB,  A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,
   LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, ENT,
   RSFT, LCTL, LALT, LGUI, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
 [1] = KEYMAP( /* Jack colemak */
-  TAB,  Q,    W,    F,    P,    G,    J,    L,    U,    Y,    SCLN, BSPC,
-  ESC,  A,    R,    S,    T,    D,    H,    N,    E,    I,    O,     QUOT,
+  ESC,  Q,    W,    F,    P,    G,    J,    L,    U,    Y,    SCLN, BSPC,
+  TAB,  A,    R,    S,    T,    D,    H,    N,    E,    I,    O,     QUOT,
   LSFT, Z,    X,    C,    V,    B,    K,    M,    COMM, DOT,  SLSH, ENT,
   FN3, LCTL, LALT, LGUI, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
 [2] = KEYMAP( /* Jack RAISE */
