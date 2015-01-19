507e0636f03f7d30f90782353f6f3509006ec696
tmk <hasu@tmk-kbd.com>
2015-01-19 10:20:20 +0900

    Revert "Changed hotkey for bootloader jump"
    This reverts commit f31cbc7219c9a1903331771c10056302817e68ca.

--- common/command.c
+++ common/command.c
@@ -216,7 +216,7 @@ static bool command_common(uint8_t code)
             print("C> ");
             command_state = CONSOLE;
             break;
-        case KC_B:
+        case KC_PAUSE:
             clear_keyboard();
             print("\n\nJump to bootloader... ");
             _delay_ms(1000);
