84760b098e8cb916f4516d165a0b1fa351188e1b
Charlie Andrews <charlieandrews.cwa@gmail.com>
2015-03-13 10:07:51 -0500

    added my keymap

--- keyboard/planck/keymap_charlie.c
+++ keyboard/planck/keymap_charlie.c
@@ -3,9 +3,9 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = KEYMAP( /* Charlie workman */
   ESC,   Q,    D,    R,    W,    B,    J,    F,    U,    P,    SCLN, BSLS,
-  LCTL,  A,    S,    H,    T,    G,    Y,    N,    E,    O,    QUOT, BSPC,
+  LCTL,  A,    S,    H,    T,    G,    Y,    N,    E,    O,    I,    BSPC,
   LSFT,  Z,    X,    M,    C,    V,    K,    L,    COMM, DOT,  SLSH, ENT,
-  RSFT, LALT, LGUI, RCTL, FN2,    SPC,     FN1, LEFT, DOWN, UP,  RGHT),
+  RSFT, LALT, LGUI, RCTL, FN2,   SPC,  FN1, LEFT, DOWN, UP,  RGHT),
 [1] = KEYMAP( /* Charlie RAISE */
   TAB,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    BSPC,
   TRNS, FN3, FN4, PAUSE, TRNS, TRNS, TRNS, MINS, EQL,  LBRC, RBRC, BSLS,
@@ -13,7 +13,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TRNS, TRNS, TRNS, TRNS, TRNS,    TRNS,   FN1,  MNXT, VOLD, VOLU, MPLY),
 [2] = KEYMAP( /* Charlie LOWER */
   GRV, FN10, FN11, FN12, FN13, FN14, FN15, FN16, FN17, FN18, FN19, BSPC,
-  TRNS, FN3, FN4, PAUSE, TRNS, TRNS, TRNS, FN20, FN21, FN23, FN24, FN28,
+  TRNS, FN3, FN4, PAUSE, TRNS, TRNS, TRNS, FN20, FN21, FN23, QUOT, FN28,
   TRNS, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  TRNS,
   TRNS, TRNS, TRNS, TRNS, FN2,    TRNS,    TRNS, MNXT, VOLD, VOLU, MPLY),
 };
