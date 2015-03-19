e9df959c131932f7d149b8c0fa07d7fea845f0fc
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-03-18 23:58:21 -0400

    Update keymap_nathan.c
    Remove macro for KEYMAP_GRID that is in keymap_common.h.
    Add comments with ASCII art for the two layouts in keymap_common.h.

--- keyboard/planck/keymap_nathan.c
+++ keyboard/planck/keymap_nathan.c
@@ -3,31 +3,6 @@
 
 #include "keymap_common.h"
 
-#ifndef KEYMAP_GRID
-   /* Planck keymap definition macro for the 'grid' layout
-    * ,-----------------------------------------------------------------------.
-    * |     |     |     |     |     |     |     |     |     |     |     |     |
-    * |-----------------------------------------------------------------------|
-    * |     |     |     |     |     |     |     |     |     |     |     |     |
-    * |-----------------------------------------------------------------------|
-    * |     |     |     |     |     |     |     |     |     |     |     |     |
-    * |-----------------------------------------------------------------------|
-    * |     |     |     |     |     |     |     |     |     |     |     |     |
-    * `-----------------------------------------------------------------------'
-    */
-#define KEYMAP_GRID( \
-       K00,  K01,  K02,  K03,  K04,  K05,  K06,  K07,  K08,  K09,  K0A,  K0B, \
-       K10,  K11,  K12,  K13,  K14,  K15,  K16,  K17,  K18,  K19,  K1A,  K1B, \
-       K20,  K21,  K22,  K23,  K24,  K25,  K26,  K27,  K28,  K29,  K2A,  K2B, \
-       K30,  K31,  K32,  K33,  K34,  K35,  K36,  K37,  K38,  K39,  K3A,  K3B  \
-) { \
-    { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B }, \
-    { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B }, \
-    { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K2B }, \
-    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B }  \
-}
-#endif
-
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /* 0: main layer
     * ,-----------------------------------------------------------------------.
@@ -50,9 +25,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * The top row are Visual Studio combos:
     *   'Run', 'Breakpoint', 'Step over', 'Step into', 'Set cursor to line'
     * 2nd row are key combos:
-    *   'ctrl-alt-delete', 'ctrl-shift-escape' 
+    *   'ctrl-alt-delete', 'ctrl-shift-escape'
     * 3rd row are macros keys:
-    *   'P0' - 'P5' execute a script on Windows machines 
+    *   'P0' - 'P5' execute a script on Windows machines
     * ,-----------------------------------------------------------------------.
     * | ESC | F5   | F9 | F10 | F11 |S+F11|CSF10|NLock|Num7 |Num8 |Num9 | Del |
     * |-----------------------------------------------------------------------|
@@ -113,7 +88,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [11] = ACTION_MODS_KEY(MOD_LSFT, KC_LBRC), // {
   [12] = ACTION_MODS_KEY(MOD_LSFT, KC_COMMA), // <
   [13] = ACTION_MODS_KEY(MOD_LSFT, KC_0),  // )
-  [14] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC), // {
+  [14] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC), // }
   [15] = ACTION_MODS_KEY(MOD_LSFT, KC_DOT), // >
   // Combo
   [16] = ACTION_MODS_KEY(MOD_LALT | MOD_LCTL, KC_DEL), // Ctrl+Alt+Delete
@@ -127,7 +102,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [23] = ACTION_MODS_KEY(MOD_LSFT, KC_6), // ^
   [24] = ACTION_MODS_KEY(MOD_LSFT, KC_7), // &
   [25] = ACTION_MODS_KEY(MOD_LSFT, KC_8), // *
-  [26] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL), // + 
+  [26] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL), // +
   [27] = ACTION_MODS_KEY(MOD_LSFT, KC_MINUS), // _
   [28] = ACTION_MODS_KEY(MOD_LSFT, KC_QUOTE), // "
   [29] = ACTION_MODS_KEY(MOD_LSFT, KC_GRAVE), // ~
@@ -142,39 +117,37 @@ const uint16_t PROGMEM fn_actions[] = {
 // *return*
 #define RUN_PYTHON_PROGRAM_ON_WIN MACRO( T(ENT), END )
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
     keyevent_t event = record->event;
     switch (id) {
         case M_P0:
             return event.pressed ?
-                ADD_PYTHON_PROGRAM_ON_WIN( 0 ) : 
+                ADD_PYTHON_PROGRAM_ON_WIN( 0 ) :
                 RUN_PYTHON_PROGRAM_ON_WIN;
         case M_P1:
             return event.pressed ?
-                ADD_PYTHON_PROGRAM_ON_WIN( 1 ) : 
+                ADD_PYTHON_PROGRAM_ON_WIN( 1 ) :
                 RUN_PYTHON_PROGRAM_ON_WIN;
         case M_P2:
             return event.pressed ?
-                ADD_PYTHON_PROGRAM_ON_WIN( 2 ) : 
+                ADD_PYTHON_PROGRAM_ON_WIN( 2 ) :
                 RUN_PYTHON_PROGRAM_ON_WIN;
         case M_P3:
             return event.pressed ?
-                ADD_PYTHON_PROGRAM_ON_WIN( 3 ) : 
+                ADD_PYTHON_PROGRAM_ON_WIN( 3 ) :
                 RUN_PYTHON_PROGRAM_ON_WIN;
         case M_P4:
             return event.pressed ?
-                ADD_PYTHON_PROGRAM_ON_WIN( 4 ) : 
+                ADD_PYTHON_PROGRAM_ON_WIN( 4 ) :
                 RUN_PYTHON_PROGRAM_ON_WIN;
         case M_P5:
             return event.pressed ?
-                ADD_PYTHON_PROGRAM_ON_WIN( 5 ) : 
+                ADD_PYTHON_PROGRAM_ON_WIN( 5 ) :
                 RUN_PYTHON_PROGRAM_ON_WIN;
         case M_USERNAME:
             return event.pressed ?
                 MACRO( T(N), T(A), T(T), T(H), T(A), T(N), T(R), T(O), T(S), T(S), T(P), T(O), T(W), T(E), T(L), T(L), END ) :
                 MACRO_NONE;
-
     }
     return MACRO_NONE;
 }
