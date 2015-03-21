2afab26b72d255ab4f525a0eb1e4a1218cfefa4b
Jack Humbert <jack.humb@gmail.com>
2015-03-21 01:01:09 -0400

    colemak shortcuts

--- keyboard/planck/extended_keymap_common.h
+++ keyboard/planck/extended_keymap_common.h
@@ -70,9 +70,46 @@ extern const uint16_t fn_actions[];
 #define RALT(kc) kc | 0x1400
 #define RGUI(kc) kc | 0x1800
 
+// Alias for function layers than expand past FN31
 #define FUNC(kc) kc | 0x2000
 
+// Aliases
 #define S(kc) LSFT(kc)
 #define F(kc) FUNC(kc)
+
+// For software implementation of colemak
+#define CM_Q    KC_Q
+#define CM_W    KC_W
+#define CM_F    KC_E
+#define CM_P    KC_R
+#define CM_G    KC_T
+#define CM_J    KC_Y
+#define CM_L    KC_U
+#define CM_U    KC_I
+#define CM_Y    KC_O
+#define CM_SCLN KC_P
+
+#define CM_A    KC_A
+#define CM_R    KC_S
+#define CM_S    KC_D
+#define CM_T    KC_F
+#define CM_D    KC_G
+#define CM_H    KC_H
+#define CM_N    KC_J
+#define CM_E    KC_K
+#define CM_I    KC_L
+#define CM_O    KC_SCLN
+
+#define CM_Z    KC_Z
+#define CM_X    KC_X
+#define CM_C    KC_C
+#define CM_V    KC_V
+#define CM_B    KC_B
+#define CM_K    KC_N
+#define CM_M    KC_M
+#define CM_COMM KC_COMM
+#define CM_DOT  KC_DOT
+#define CM_SLSH KC_SLSH
+
 
 #endif
