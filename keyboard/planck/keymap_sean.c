b5615e950d0db65fe87c1be84353f298479d3bd6
Sean Hunter <shunter@palantir.com>
2015-04-02 09:11:20 +0100

    Latest custom map with lower + raise

--- keyboard/planck/keymap_sean.c
+++ keyboard/planck/keymap_sean.c
@@ -2,34 +2,41 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = KEYMAP( /* Matrix Dvorak */
-      QUOT, COMM, DOT,  P,    Y,    SLSH, EQL,  F,    G,    C,    R,   L,
-      A,    O,    E,    U,    I,    ESC,  BSPC, D,    H,    T,    N,   S,
-      SCLN, Q,    J,    K,    X,    TAB,  ENT,  B,    M,    W,    V,   Z,
-      LSFT, LCTL, LALT, LGUI, FN1,  SPC,        FN2,  LEFT, DOWN, UP,  RGHT),
+      QUOT, COMM,  DOT,    P,    Y, SLSH,  EQL,    F,    G,    C,    R,    L,
+         A,    O,    E,    U,    I,  ESC, BSPC,    D,    H,    T,    N,    S,
+      SCLN,    Q,    J,    K,    X,  TAB,  ENT,    B,    M,    W,    V,    Z,
+      LSFT, LCTL, LALT, LGUI,  FN1,  SPC,        FN2, LEFT, DOWN,   UP, RGHT),
 
     [1] = KEYMAP( /* Matrix Qwerty */
-      Q,    W,    E,    R,    T,    SLSH, EQL,  Y,    U,    I,    O,    P,
-      A,    S,    D,    F,    G,    ESC,  BSPC, H,    J,    K,    L,    SCLN,
-      Z,    X,    C,    V,    B,    TAB,  ENT,  N,    M,    COMM, DOT,  SLSH,
-      LSFT, LCTL, LALT, LGUI, FN1,  SPC,        FN2,  LEFT, DOWN, UP,  RGHT),
+         Q,    W,    E,    R,    T, QUOT,  EQL,    Y,    U,    I,    O,    P,
+         A,    S,    D,    F,    G,  ESC, BSPC,    H,    J,    K,    L, SCLN,
+         Z,    X,    C,    V,    B,  TAB,  ENT,    N,    M, COMM,  DOT, SLSH,
+      LSFT, LCTL, LALT, LGUI,  FN1,  SPC,        FN2, LEFT, DOWN,   UP, RGHT),
       
     [2] = KEYMAP( /* fn1 lower */
-      F1,   F2,   F3,   F4,   F5,   TRNS, TRNS, F6,   F7,   F8,   F9,   F10,
-      1,    2,    3,    4,    5,    ESC,  DEL,  6,    7,    8,    9,    0,
-      FN3,  FN4,  TRNS,   GRV,  MINS, TRNS, TRNS, BSLS, LBRC, RBRC, TRNS, TRNS,
-      TRNS, TRNS, TRNS, TRNS, FN1,  TRNS,       TRNS, HOME, PGDN, PGUP, END),
+        F1,   F2,   F3,   F4,   F5, TRNS, TRNS,   F6,   F7,   F8,   F9,  F10,
+         1,    2,    3,    4,    5,  F18,  DEL,    6,    7,    8,    9,    0,
+       FN3,  FN4, TRNS,  GRV, MINS, TRNS, TRNS, BSLS, LBRC, RBRC, TRNS, TRNS,
+      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,        FN5, HOME, PGDN, PGUP,  END),
 
     [3] = KEYMAP( /* fn2 raise */
-      F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,
-      FN10, FN11, FN12, FN13, FN14, ESC,  DEL,  FN15, FN16, FN17, FN18, FN19,
-      FN3,  FN4,  TRNS,   FN22, FN20, TRNS, TRNS, FN28, FN23, FN24, TRNS, TRNS,
-      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       FN2,  MNXT, VOLD, VOLU, MPLY),
+        F1,   F2,   F3,   F4,   F5, TRNS, TRNS,   F6,   F7,   F8,   F9,  F10,
+       F11,  F12,  F13,  F14,  F15,  F18,  DEL,  F16,  F17,  F18,  F19,  F20,
+       FN3,  FN4, TRNS, TRNS, TRNS, TRNS, TRNS, EJCT,  PWR, LSFT,PAUSE, RSFT,
+      LEFT, DOWN,   UP, RGHT,  FN5, TRNS,       TRNS, MNXT, VOLD, VOLU, MUTE),
+
+    [4] = KEYMAP( /* lower + raise */
+      LEFT, DOWN,   UP, RGHT, TRNS, TRNS, TRNS, TRNS, LEFT, DOWN,   UP, RGHT,
+      HOME, PGDN, PGUP,  END, TRNS,  F18,  DEL, TRNS, HOME, PGDN, PGUP,  END,
+      FN3,  FN4,  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, EJCT,  PWR, MPLY, RSFT,
+      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, MNXT, VOLD, VOLU, MUTE),
 };
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
-    [2] = ACTION_LAYER_MOMENTARY(3),  // to Fn overlay 
+    [1] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay LOWER
+    [2] = ACTION_LAYER_MOMENTARY(3),  // to Fn overlay RAISE
     [3] = ACTION_DEFAULT_LAYER_SET(0), 
     [4] = ACTION_DEFAULT_LAYER_SET(1),  
+    [5] = ACTION_LAYER_MOMENTARY(4),  // to Fn overlay LOWER + RAISE
 
     [10] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
     [11] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
