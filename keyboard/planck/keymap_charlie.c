b2b94cd8497a8d24789137e189878cafd9a597dd
Charlie Andrews <charlieandrews.cwa@gmail.com>
2015-09-26 13:32:42 -0500

    Use reverse instead of swap and don't reverse bottom

--- keyboard/planck/keymap_charlie.c
+++ keyboard/planck/keymap_charlie.c
@@ -5,27 +5,27 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[0] = KEYMAP_AND_SWAP( /* Charlie workman */
-  ESC,   Q,    D,    R,    W,    B,    J,    F,    U,    P,    SCLN, BSPC,
+[0] = KEYMAP_AND_REVERSE( /* Charlie workman */
+  TAB,   Q,    D,    R,    W,    B,    J,    F,    U,    P,    SCLN, BSPC,
   LCTL,  A,    S,    H,    T,    G,    Y,    N,    E,    O,    I,    ENT,
   LSFT,  Z,    X,    M,    C,    V,    K,    L,    COMM, DOT,  SLSH, RSFT,
-  FN3, LCTL, LALT, LGUI, FN1,   FN17,  FN2, LEFT, DOWN, UP,  RGHT),
-[2] = KEYMAP_AND_SWAP( /* Charlie querty */
-  ESC,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
+  FN3, LCTL, LALT, LGUI, FN1,   FN17,  FN2, RGUI, RALT, RCTL,  FN3),
+[2] = KEYMAP_AND_REVERSE( /* Charlie querty */
+  TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
   LCTL, A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, ENT,
   LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, RSFT,
-  FN3, LCTL, LALT, LGUI, FN1,    FN17,     FN2, LEFT, DOWN, UP,  RGHT),
-[4] = KEYMAP_AND_SWAP( /* Charlie RAISE */
-  TAB,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    TRNS,
+  FN3, LCTL, LALT, LGUI, FN1,    FN17,     FN2, RGUI, RALT, RCTL,  FN3),
+[4] = KEYMAP_AND_REVERSE( /* Charlie RAISE */
+  ESC,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    TRNS,
   TRNS, FN4,  FN5,  FN6,  FN7,  FN8,  FN9,  FN10, FN11, FN12, FN13, TRNS,
   TRNS, F11,  F12,  F13,  F14,  F15,  F16,  F17,  TRNS,  TRNS,  TRNS,  TRNS,
   TRNS, TRNS, TRNS, TRNS, TRNS,    TRNS,  FN16,  MNXT, VOLD, VOLU, MPLY),
-[6] = KEYMAP_AND_SWAP( /* Charlie LOWER */
+[6] = KEYMAP_AND_REVERSE( /* Charlie LOWER */
   GRV, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, BSLS,
   TRNS, FN14, FN15, TRNS, TRNS, TRNS, TRNS, MINS, EQL, LBRC, RBRC, QUOT,
   TRNS, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  TRNS,
   TRNS, TRNS, TRNS, TRNS, FN16,    TRNS,    TRNS, MNXT, VOLD, VOLU, MPLY),
-[8] = KEYMAP_AND_SWAP( /* Charlie MOUSE */
+[8] = KEYMAP_AND_REVERSE( /* Charlie MOUSE */
   GRV, FN15, TRNS, TRNS, TRNS, TRNS, TRNS, MS_BTN1, MS_BTN2, MS_BTN3, TRNS, BSLS,
   TRNS, FN14, MS_ACCEL0, MS_ACCEL1, MS_ACCEL2, TRNS, TRNS, MS_LEFT, MS_DOWN, MS_UP, MS_RIGHT, QUOT,
   TRNS, TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   MS_WH_LEFT,   MS_WH_DOWN,   MS_WH_UP,   MS_WH_RIGHT,  TRNS,
@@ -68,8 +68,8 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 }
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
-    [2] = ACTION_LAYER_MOMENTARY(3),  // to Fn overlay
+    [1] = ACTION_LAYER_MOMENTARY(4),  // to Fn overlay
+    [2] = ACTION_LAYER_MOMENTARY(6),  // to Fn overlay
 
     [3] = ACTION_MODS_KEY(MOD_LGUI, KC_TAB), // switch windows
     [4] = ACTION_MODS_KEY(MOD_LSFT, KC_1), // !
