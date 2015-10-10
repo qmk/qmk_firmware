19bb28d102b8c9b226aae38e2d8d9e53e574d6ec
flabbergast <s3+flabbergast@sdfeu.org>
2015-10-10 15:20:55 +0100

    Add ARM Teensies bootloader code.

--- tmk_core/common/chibios/bootloader.c
+++ tmk_core/common/chibios/bootloader.c
@@ -1,8 +1,13 @@
 #include "bootloader.h"
 
 #include "ch.h"
+#include "hal.h"
 
 #ifdef STM32_BOOTLOADER_ADDRESS
+/* STM32 */
+
+#if defined(STM32F0XX)
+/* This code should be checked whether it runs correctly on platforms */
 #define SYMVAL(sym) (uint32_t)(((uint8_t *)&(sym)) - ((uint8_t *)0))
 extern uint32_t __ram0_end__;
 
@@ -10,7 +15,19 @@ void bootloader_jump(void) {
   *((unsigned long *)(SYMVAL(__ram0_end__) - 4)) = 0xDEADBEEF; // set magic flag => reset handler will jump into boot loader
    NVIC_SystemReset();
 }
-#else /* STM32_BOOTLOADER_ADDRESS */
-void bootloader_jump(void) {}
-#endif /* STM32_BOOTLOADER_ADDRESS */
 
+#else /* defined(STM32F0XX) */
+#error Check that the bootloader code works on your platform and add it to bootloader.c!
+#endif /* defined(STM32F0XX) */
+
+#elif defined(KL2x) || defined(K20x) /* STM32_BOOTLOADER_ADDRESS */
+/* Kinetis */
+
+void bootloader_jump(void) {
+	chThdSleepMilliseconds(100);
+	__BKPT(0);
+}
+
+#else /* neither STM32 nor KINETIS */
+void bootloader_jump(void) {}
+#endif
