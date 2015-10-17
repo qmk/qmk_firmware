0914c583518b3d80b668d1bc6265c15c92642047
flabbergast <s3+flabbergast@sdfeu.org>
2015-10-17 18:20:12 +0100

    Add correct chibios/bootloader_jump for infinity KB.

--- tmk_core/common/chibios/bootloader.c
+++ tmk_core/common/chibios/bootloader.c
@@ -23,10 +23,24 @@ void bootloader_jump(void) {
 #elif defined(KL2x) || defined(K20x) /* STM32_BOOTLOADER_ADDRESS */
 /* Kinetis */
 
+#if defined(KIIBOHD_BOOTLOADER)
+/* Kiibohd Bootloader (MCHCK and Infinity KB) */
+#define SCB_AIRCR   (*(volatile uint32_t *)0xE000ED0C) // Application Interrupt and Reset Control
+#define VBAT        (*(volatile uint8_t  *)0x4003E000) // VBAT register file (32 bytes)
+const uint8_t sys_reset_to_loader_magic[] = "\xff\x00\x7fRESET TO LOADER\x7f\x00\xff";
+void bootloader_jump(void) {
+  __builtin_memcpy((void *)&VBAT, (const void *)sys_reset_to_loader_magic, sizeof(sys_reset_to_loader_magic));
+  // request reset
+  SCB_AIRCR = 0x05FA0004;
+}
+
+#else /* defined(KIIBOHD_BOOTLOADER) */
+/* Default for Kinetis - expecting an ARM Teensy */
 void bootloader_jump(void) {
 	chThdSleepMilliseconds(100);
 	__BKPT(0);
 }
+#endif /* defined(KIIBOHD_BOOTLOADER) */
 
 #else /* neither STM32 nor KINETIS */
 void bootloader_jump(void) {}
