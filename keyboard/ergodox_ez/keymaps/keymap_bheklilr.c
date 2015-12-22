941402c9d03e7c5af9bdb846965e9493bc28b13d
Aaron Stevens <bheklilr2@gmail.com>
2015-12-21 23:41:08 -0600

    Got layer 1 defined how I want it, tomorrow night will be layer two and possibly layer 3.  Then we'll see about extra layers

--- keyboard/ergodox_ez/keymaps/keymap_bheklilr.c
+++ keyboard/ergodox_ez/keymaps/keymap_bheklilr.c
@@ -6,49 +6,52 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
+#define SUBL_PROJ LCTL(LALT(KC_P))
+#define SUBL_PAL RCTL(LSFT(KC_P))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Del    |   1  |   2  |   3  |   4  |   5  |  6   |           |   7  |   8  |   9  |   0  |   -  |   =  |  BkSp  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | BkSp   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | Esc    |   A  |   S  |   D  | F/L3 |   G  |------|           |------|   H  |J / L2|   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------| Ctrl+|           | Ctrl+|------+------+------+------+------+--------|
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  | Alt+P|           | Sft+P|   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |Grv/L1|  '"  |  Alt | Home | End  |                                       | PgUp | PgDn |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |      |      |CtlSft|       | Meh  |        |      |
+ *                                 | Space| Tab  |------|       |------| Enter  |Space |
+ *                                 |      |      |CtlAlt|       |AltSft|        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
-        // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
-        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
-        // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(1),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+    // left hand
+    KC_DELT,         KC_1,         KC_2,    KC_3,    KC_4,           KC_5,          KC_6,
+    KC_BSPC,         KC_Q,         KC_W,    KC_E,    KC_R,           KC_T,          TG(1),
+    KC_ESC,          KC_A,         KC_S,    KC_D,    LT(MDIA, KC_F), KC_G,
+    KC_LSFT,         CTL_T(KC_Z),  KC_X,    KC_C,    KC_V,           KC_B,          SUBL_PROJ,
+    LT(SYMB,KC_GRV), KC_QUOT,      KC_LALT, KC_HOME, KC_END,
+                                                                     ALT_T(KC_APP), KC_LGUI,
+                                                                                    LCTL(KC_LSFT),
+                                                     KC_SPC,         KC_TAB,        LCTL(KC_LALT),
+    // right hand
+    KC_7,          KC_8,          KC_9,           KC_0,    KC_MINS, KC_EQL,         KC_BSPC,
+    TG(1),         KC_Y,          KC_U,           KC_I,    KC_O,    KC_P,           KC_BSLS,
+                   KC_H,          LT(MDIA, KC_J), KC_K,    KC_L,    KC_SCLN,        KC_QUOT,
+    SUBL_PAL,      KC_N,          KC_M,           KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
+                                  KC_PGUP,        KC_PGDN, KC_LBRC, KC_RBRC,        KC_FN1,
+    KC_LALT,       CTL_T(KC_ESC),
+    MEH_T(KC_NO),
+    RALT(KC_RSFT), KC_ENT,        KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
