78dfe35872ada5f2fb629416d5975ac524d80adc
Joe Cridge <joe.cridge@me.com>
2015-12-27 11:46:36 +0000

    Complete function layer

--- keyboard/ergodox_ez/keymaps/keymap_jeo_ez.c
+++ keyboard/ergodox_ez/keymaps/keymap_jeo_ez.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_DOWN,  KC_UP,    KC_RIGHT,  KC_LEFT,   KC_F19,
 
    KC_F20,      KC_F21,
-   KC_DELETE,
+   KC_DELT,
    TG(FPAD),    KC_ENT,  KC_SPC
 ),
 
@@ -159,10 +159,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /**
    * SYMB: Left Hand
    *
-   * Blank keys are transparent.
+   * Blank keys are transparent. Hashed keys are disabled.
    *
    *        ,--------------------------------------------------.
-   *        |        |      |@     |GBP   |EUR   |      |      |
+   *        |        | #### |@     |GBP   |EUR   | #### |      |
    *        |--------+------+------+------+------+-------------|
    *        |        |...   |_     |[     |]     |^     |      |
    *        |--------+------+------+------+------+------|      |
@@ -170,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *        |--------+------+------+------+------+------|      |
    *        |        |#     |$     ||     |~     |`     |      |
    *        `--------+------+------+------+------+-------------'
-   *          |      |      |      |      |      |
+   *          | #### |      |      |      |      |
    *          `----------------------------------'
    *                                                    ,-------------.
    *                                                    |      |      |
@@ -180,11 +180,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                             |      |      |      |
    *                                             `--------------------'
    */
-   KC_TRNS,  KC_TRNS,     KC_AT,    UC(0x00A3),  UC(0x20AC),  KC_TRNS,  KC_TRNS,
+   KC_TRNS,  KC_NO,       KC_AT,    UC(0x00A3),  UC(0x20AC),  KC_NO,    KC_TRNS,
    KC_TRNS,  UC(0x2026),  KC_UNDS,  KC_LBRC,     KC_RBRC,     KC_CIRC,  KC_TRNS,
    KC_TRNS,  KC_BSLS,     KC_SLSH,  KC_LCBR,     KC_RCBR,     KC_ASTR,
    KC_TRNS,  KC_HASH,     KC_DLR,   KC_PIPE,     KC_TILD,     KC_GRV,   KC_TRNS,
-   KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,     KC_TRNS,
+   KC_NO,    KC_TRNS,     KC_TRNS,  KC_TRNS,     KC_TRNS,
 
                                                               KC_TRNS,  KC_TRNS,
                                                                         KC_TRNS,
@@ -196,7 +196,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Blank keys are transparent.
    *
    *               ,--------------------------------------------------.
-   *               |      |      |LDQUO |RDQUO |LSQUO |LDQUO |        |
+   *               |      | #### |LDQUO |RDQUO |LSQUO |LDQUO |        |
    *               |------+------+------+------+------+------+--------|
    *               |      |!     |<     |>     |=     |&     |        |
    *               |      |------+------+------+------+------+--------|
@@ -215,7 +215,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *        `--------------------'
    *
    */
-   KC_TRNS,  KC_TRNS,     UC(0x201C),  UC(0x201D),  UC(0x2018),  UC(0x2019),  KC_TRNS,
+   KC_TRNS,  KC_NO,       UC(0x201C),  UC(0x201D),  UC(0x2018),  UC(0x2019),  KC_TRNS,
    KC_TRNS,  KC_EXLM,     S(KC_COMM),  S(KC_DOT),   KC_EQL,      KC_AMPR,     KC_TRNS,
              S(KC_SLSH),  KC_LPRN,     KC_RPRN,     KC_MINS,     KC_COLN,     KC_TRNS,
    KC_TRNS,  KC_PLUS,     KC_PERC,     S(KC_QUOT),  KC_QUOT,     KC_SCLN,     KC_TRNS,
@@ -226,46 +226,80 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TRNS,  KC_TRNS,     KC_TRNS
 ),
 
-/* Keymap 2: Media and mouse keys
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-// MEDIA AND MOUSE
-KEYMAP(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
-    // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_WBAK
+[FPAD] = KEYMAP(
+  /**
+   * FPAD: Left Hand
+   *
+   * This half-layer is designed to function as an independent navigation pad.
+   *
+   * Blank keys are transparent. Hashed keys are disabled.
+   *
+   *        ,--------------------------------------------------.
+   *        |Escape  |F1    |F2    |F3    |F4    |F5    |      |
+   *        |--------+------+------+------+------+-------------|
+   *        |Tab     |PgUp  |BackSp|Up    |Delete|PgDn  |      |
+   *        |--------+------+------+------+------+------|      |
+   *        |Enter   |Home  |Left  |Down  |Right |End   |------|
+   *        |--------+------+------+------+------+------|      |
+   *        |TAB     |Close |Back  |NewTab|Fwd   |NewWin|      |
+   *        `--------+------+------+------+------+-------------'
+   *          |LOCK  | #### | #### | #### | #### |
+   *          `----------------------------------'
+   *                                                    ,-------------.
+   *                                                    |      |      |
+   *                                             ,------|------|------|
+   *                                             |      |      |      |
+   *                                             |      |      |------|
+   *                                             |      |      |      |
+   *                                             `--------------------'
+   */
+   KC_ESC,     KC_F1,       KC_F2,    KC_F3,       KC_F4,    KC_F5,       KC_TRNS,
+   KC_TAB,     KC_PGUP,     KC_BSPC,  KC_UP,       KC_DELT,  KC_PGDN,     KC_TRNS,
+   KC_ENT,     KC_HOME,     KC_LEFT,  KC_DOWN,     KC_RGHT,  KC_END,
+   S(KC_TAB),  LGUI(KC_W),  KC_WBAK,  LGUI(KC_T),  KC_WFWD,  LGUI(KC_N),  KC_TRNS,
+   TG(LOCK),   KC_NO,       KC_NO,    KC_NO,       KC_NO,
+
+                                                             KC_TRNS,     KC_TRNS,
+                                                                          KC_TRNS,
+                                                   KC_TRNS,  KC_TRNS,     KC_TRNS,
+
+  /**
+   * FPAD: Right Hand
+   *
+   * This half-layer is designed to function as an independent keypad for
+   * calculator and hex input.
+   *
+   * Blank keys are transparent. Hashed keys are disabled.
+   *
+   *               ,--------------------------------------------------.
+   *               |:     |F6    |F7    |F8    |F9    |F10   |/       |
+   *               |------+------+------+------+------+------+--------|
+   *               |0x    |A     |7     |8     |9     |D     |*       |
+   *               |      |------+------+------+------+------+--------|
+   *               |------|B     |4     |5     |6     |E     |-       |
+   *               |0b    |------+------+------+------+------+--------|
+   *               |      |C     |1     |2     |3     |F     |+       |
+   *               `-------------+------+------+------+------+--------'
+   *                             |#     |0     |.     |e     |,     |
+   *                             `----------------------------------'
+   *        ,-------------.
+   *        |Left  |Right |
+   *        |------+------+------.
+   *        |BackSp|Escape|Enter |
+   *        |------|      |      |
+   *        |      |      |      |
+   *        `--------------------'
+   *
+   */
+   KC_COLN,  KC_F6,    KC_F7,    KC_F8,  KC_F9,    KC_F10,   KC_PSLS,
+   KC_NO,    S(KC_A),  KC_P7,    KC_P8,  KC_P9,    S(KC_D),  KC_PAST,
+             S(KC_B),  KC_P4,    KC_P5,  KC_P6,    S(KC_E),  KC_PMNS,
+   KC_NO,    S(KC_C),  KC_P1,    KC_P2,  KC_P3,    S(KC_F),  KC_PPLS,
+                       KC_HASH,  KC_P0,  KC_PDOT,  KC_E,     KC_PCMM,
+
+   KC_LEFT,  KC_RGHT,
+   KC_BSPC,
+   KC_TRNS,  KC_ESC,   KC_PENT
 ),
 };
 
