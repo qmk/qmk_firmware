be824541e71356ab40e78171a6b1b737bc183967
Didier Loiseau <didierloiseau+git@gmail.com>
2015-12-29 21:36:17 +0100

    Initial TypeMatrix keymap: Layer 0

--- keyboard/ergodox_ez/keymaps/keymap_typematrix.c
+++ keyboard/ergodox_ez/keymaps/keymap_typematrix.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|ace   |           |ace   |------+------+------+------+------+--------|
  * | LShift |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| '/Shift|
  * |--------+------+------+------+------+------|Enter |           |Enter |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |/Hyper|           |/ Meh |   N  |   M  |   ,  |   .  |   /  | \/Shift|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | \/Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCtrl | ~L1  | LGui | Play | App  |                                       | Left |Right |   -  |   =  |RCtrl |
  *   `----------------------------------'                                       `----------------------------------'
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_DELT,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_BSPC,
         KC_LSFT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_ENT),
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_ENT,
         KC_LCTL,        KC_FN1,       KC_LGUI,KC_MPLY,KC_APP,
 
                                               ALT_T(KC_ESC),  TG(1),
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_DELT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_RBRC,
              KC_BSPC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),SFT_T(KC_QUOT),
-            MEH_T(KC_ENT),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_BSLS),
+             KC_ENT,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(KC_BSLS),
                                   KC_LEFT,KC_RGHT,KC_MINS,KC_EQL,           KC_RCTL,
 
              KC_HOME, CTL_T(KC_END),
