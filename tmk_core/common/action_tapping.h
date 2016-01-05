e3847b9e4be5e3271a45ad1058bdf1bd6e115887
Noah Andrews <NoahAndrews@users.noreply.github.com>
2016-01-05 15:45:45 -0500

    Improved tap/hold functionality

--- tmk_core/common/action_tapping.h
+++ tmk_core/common/action_tapping.h
@@ -19,9 +19,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
-/* period of tapping(ms) */
+/* period of tapping(ms)*/
+/* default is 200       */
 #ifndef TAPPING_TERM
-#define TAPPING_TERM    200
+#define TAPPING_TERM    350
 #endif
 
 /* tap count needed for toggling a feature */
