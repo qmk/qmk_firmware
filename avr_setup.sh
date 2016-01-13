71ee3eb2e7fdddbfac4443c5f8522a2f620f86e2
dragon788 <dragon788@users.noreply.github.com>
2016-01-12 21:41:43 -0600

    All dfu dependencies in place, multi-line syntax to make it easy to add/read

--- avr_setup.sh
+++ avr_setup.sh
@@ -11,21 +11,54 @@ if [[ -n "$(type -P pacman )" ]]; then
   # Always run the pacman mirror update script if possible when vagrant comes up
   # This will ensure that users never get stalled on a horribly slow mirror
   pacman -Syyu --needed --noconfirm
-  pacman -S --needed --noconfirm base-devel avr-gcc avr-binutils avr-libc dfu-util
+  pacman -S --needed --noconfirm \
+    base-devel \
+    avr-gcc \
+    avr-binutils \
+    avr-libc \
+    dfu-util
 
 elif [[ -n "$(type -P apt-get)" ]]; then
   # Debian and derivatives
   apt-get update -y && apt-get upgrade -y
-  apt-get install -y build-essential gcc unzip wget zip gcc-avr binutils-avr avr-libc
+  apt-get install -y \
+    build-essential \
+    gcc \
+    unzip \
+    wget \
+    zip \
+    gcc-avr \
+    binutils-avr \
+    avr-libc \
+    dfu-util
   
 elif [[ -n "$(type -P yum)" ]]; then
   # Fedora, CentOS or RHEL and derivatives
   yum -y makecache && yum -y update
-  yum -y install gcc glibc-headers kernel-devel kernel-headers make perl git wget
+  yum -y install \
+    gcc \
+    glibc-headers \
+    kernel-devel \
+    kernel-headers \
+    make \
+    perl \
+    git \
+    wget \
+    avr-binutils \
+    avr-gcc \
+    avr-libc \
+    dfu-util
 
 elif [[ -n "$(type -P zypper)" ]]; then
   # openSUSE
   zypper refresh --non-interactive && zypper update --non-interactive
-  zypper --non-interactive install git make gcc kernel-devel patch wget
+  zypper --non-interactive install \
+    git \
+    make \
+    gcc \
+    kernel-devel \
+    patch \
+    wget \
+    dfu-util
 
 fi
