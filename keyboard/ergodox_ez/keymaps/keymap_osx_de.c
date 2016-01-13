8c338391ba2e25d0a25f91b19a4d10fe9ace432e
Stephan Bösebeck <stephan.boesebeck@holidayinsider.com>
2016-01-13 13:30:01 +0100

    typo fix

--- keyboard/ergodox_ez/keymaps/keymap_osx_de.c
+++ keyboard/ergodox_ez/keymaps/keymap_osx_de.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,                  KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   CTL_T(LGUI(KC_V)),
+        KC_ESC,                  KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   LGUI(KC_V),
         KC_TAB,                  KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LGUI,
         TG(3),                   KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,                 CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_LALT,
