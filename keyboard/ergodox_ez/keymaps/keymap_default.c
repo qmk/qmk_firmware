e04464c7ab455dcbee35be9f16c4e5a33a7261e7
dragon788 <dragon788@users.noreply.github.com>
2016-01-16 12:31:13 -0600

    Having media layer defined causes toggles to 'stick' rather than be momentary in my experience

--- keyboard/ergodox_ez/keymaps/keymap_default.c
+++ keyboard/ergodox_ez/keymaps/keymap_default.c
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 // MEDIA AND MOUSE
-[MDIA] = KEYMAP(
+KEYMAP(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
