6cbfeaacb0499aef2e14098b5cb7fe08cc4b1660
Noah Andrews <NoahAndrews@users.noreply.github.com>
2016-01-20 10:59:34 -0500

    Added basic Windows layout
    Swaps CMD and CTRL

--- keyboard/planck/keymaps/keymap_noah.c
+++ keyboard/planck/keymaps/keymap_noah.c
@@ -9,9 +9,10 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QW 0
-#define _MK 1
-#define _LW 2
-#define _RS 3
+#define _WI 1
+#define _MK 2
+#define _LW 3
+#define _RS 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = { /* Qwerty */
@@ -20,6 +21,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT },
   {M(0),    KC_LCTL, KC_LALT, KC_LGUI, MO(_LW), KC_LSFT,  LT(_MK,KC_SPC),  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
+[_WI] = { /* Qwerty-Windows */
+  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT },
+  {M(0),    KC_LGUI, KC_LALT, KC_LCTL, MO(_LW), KC_LSFT,  LT(_MK,KC_SPC),  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+/*MT(KC_LCTL,M(0))*/
 },
 [_MK] = { /* Mouse Keys */
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_BTN1,   KC_MS_UP,  KC_MS_BTN2, KC_SLCK, KC_PAUS},
@@ -30,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RS] = { /* RAISE */
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE},
   {KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS},
+  {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_WI), KC_TRNS, KC_TRNS,   KC_TRNS},
   {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LCTL(LSFT(KC_N)), KC_VOLD, KC_VOLU, LCTL(LSFT(KC_P)) }
 },
 [_LW] = { /* LOWER */
