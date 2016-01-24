60af5c2173f920b13f33ac5f26fac34444f18f45
Yang Liu <i@yangliu.name>
2016-01-24 13:14:50 -0800

    Add support for WS2812 LEDs

--- keyboard/planck/config.h
+++ keyboard/planck/config.h
@@ -58,6 +58,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
+/* ws2812 RGB LED */
+#define ws2812_PORTREG  PORTD
+#define ws2812_DDRREG   DDRD
+#define ws2812_pin PD1
+#define RGBLED_NUM 28     // Number of LEDs
+#ifndef RGBLIGHT_HUE_STEP
+#define RGBLIGHT_HUE_STEP 10
+#endif
+#ifndef RGBLIGHT_SAT_STEP
+#define RGBLIGHT_SAT_STEP 17
+#endif
+#ifndef RGBLIGHT_VAL_STEP
+#define RGBLIGHT_VAL_STEP 17
+#endif
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
