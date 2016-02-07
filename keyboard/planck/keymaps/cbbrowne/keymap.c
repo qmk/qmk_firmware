6105fe54da4540eee537ea2faf06c42cacf8c256
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2016-02-07 22:48:59 +0100

    Simplified the random letter and digit generator

--- keyboard/planck/keymaps/cbbrowne/keymap.c
+++ keyboard/planck/keymaps/cbbrowne/keymap.c
@@ -1,3 +1,4 @@
+#include "stdlib.h"
 #include "planck.h"
 #ifdef BACKLIGHT_ENABLE
   #include "backlight.h"
@@ -80,41 +81,41 @@ enum macro_id {
 /* Note that Planck has dimensions 4 rows x 12 columns */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_QW] = { /* Qwerty */
-  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT },
-  {KC_TAB,  M(M_LED), KC_LALT, KC_LGUI, MO(_LW), KC_SPC,  KC_SPC,  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
-},
-[_RS] = { /* RAISE */
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
-  {KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_KP), DF(_KP), RESET,   KC_TRNS},
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
-},
-[_LW] = { /* LOWER */
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-  {KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-  {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_KP), DF(_KP), RESET,   KC_TRNS},
-  {KC_TRNS, DF(_KP), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
-},
-[_KP] = { /* Key Pad */
-  {KC_ESC,  M(M_USERNAME),    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_KP_ENTER, KC_KP_PLUS, KC_KP_PLUS, KC_KP_ENTER, KC_BSPC},
-  {KC_LCTL, M(M_RANDDIGIT),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_KP_MINUS, KC_7, KC_8,    KC_9,  KC_ENT},
-  {KC_LSFT, M(M_RANDLETTER),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_KP_PLUS,    KC_4, KC_5,  KC_6, KC_DOT},
-  {BL_STEP, M(M_LED), KC_LALT, KC_LGUI, KC_NO, KC_SPC,  KC_SPC,  DF(_QW),   KC_1, KC_2, KC_3,  KC_0}
-}
+  [_QW] = { /* Qwerty */
+    {KC_ESC,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+    {KC_LCTL, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
+    {KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT},
+    {KC_TAB,  M(M_LED), KC_LALT, KC_LGUI, MO(_LW), KC_SPC,  KC_SPC,  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  },
+  [_RS] = { /* RAISE */
+    {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
+    {KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
+    {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_KP), DF(_KP), RESET,   KC_TRNS},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  },
+  [_LW] = { /* LOWER */
+    {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
+    {KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
+    {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_KP), DF(_KP), RESET,   KC_TRNS},
+    {KC_TRNS, DF(_KP), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  },
+  [_KP] = { /* Key Pad */
+    {KC_ESC,  M(M_USERNAME),   KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_KP_ENTER,   KC_KP_PLUS, KC_KP_PLUS, KC_KP_ENTER, KC_BSPC},
+    {KC_LCTL, M(M_RANDDIGIT),  KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_KP_MINUS,   KC_7,       KC_8,       KC_9,        KC_ENT},
+    {KC_LSFT, M(M_RANDLETTER), KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_KP_PLUS,    KC_4,       KC_5,       KC_6,        KC_DOT},
+    {BL_STEP, M(M_LED),        KC_LALT, KC_LGUI, KC_NO, KC_SPC,  KC_SPC,  DF(_QW),       KC_1,       KC_2,       KC_3,        KC_0}
+  }
 };
 
 const uint16_t PROGMEM fn_actions[] = {
 };
 
-/* This bit of logic seeds a wee linear congruential random number generator */
-
-static uint16_t random_value = 157;
-#define randadd 53
-#define randmul 181
-#define randmod 167
+const uint16_t digit_codes[] = {KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0};
+const uint16_t letter_codes[] = {KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J, KC_K,
+                                 KC_L, KC_M, KC_N, KC_O, KC_P, KC_Q, KC_R, KC_S, KC_T, KC_U, KC_V,
+                                 KC_W, KC_X, KC_Y, KC_Z};
+static uint16_t random_digit;
+static uint16_t random_letter;
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
@@ -123,13 +124,13 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
   case M_LED:
     if (record->event.pressed) {
       register_code(KC_RSFT);
-#ifdef BACKLIGHT_ENABLE
-      backlight_step();
-#endif
+      #ifdef BACKLIGHT_ENABLE
+        backlight_step();
+      #endif
     } else {
       unregister_code(KC_RSFT);
     }
-    break;	    
+    break;
   case M_USERNAME:
     if (record->event.pressed) {
       return MACRO( I(1), T(C), T(B), T(B), T(R), T(O), T(W), T(N), T(E));
@@ -138,163 +139,21 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     }
     break;
   case M_RANDDIGIT:
-    /* Generate, based on random number generator, a keystroke for
-       a numeric digit chosen at random */
-    random_value = ((random_value + randadd) * randmul) % randmod;
-    if (record->event.pressed) 
-    switch(random_value % 10) {
-    case 0:
-      register_code (KC_0);
-      unregister_code (KC_0);
-      break;
-    case 1:
-      register_code (KC_1);
-      unregister_code (KC_1);
-      break;
-    case 2:
-      register_code (KC_2);
-      unregister_code (KC_2);
-      break;
-    case 3:
-      register_code (KC_3);
-      unregister_code (KC_3);
-      break;
-    case 4:
-      register_code (KC_4);
-      unregister_code (KC_4);
-      break;
-    case 5:
-      register_code (KC_5);
-      unregister_code (KC_5);
-      break;
-    case 6:
-      register_code (KC_6);
-      unregister_code (KC_6);
-      break;
-    case 7:
-      register_code (KC_7);
-      unregister_code (KC_7);
-      break;
-    case 8:
-      register_code (KC_8);
-      unregister_code (KC_8);
-      break;
-    case 9:
-      register_code (KC_9);
-      unregister_code (KC_9);
-      break;
+    /* Generate a keystroke for a digit chosen at random */
+    if (record->event.pressed) {
+      random_digit = digit_codes[rand()%(sizeof(digit_codes) / sizeof(uint16_t))];
+      register_code(random_digit);
+    } else {
+      unregister_code(random_digit);
     }
     break;
   case M_RANDLETTER:
-    /* Generate, based on random number generator, a keystroke for
-       a letter chosen at random */
-    random_value = ((random_value + randadd) * randmul) % randmod;
-    if (record->event.pressed) 
-    switch(random_value % 26) {
-    case 0:
-      register_code(KC_A);
-      unregister_code(KC_A);
-      break;
-    case 1:
-      register_code(KC_B);
-      unregister_code(KC_B);
-      break;
-    case 2:
-      register_code(KC_C);
-      unregister_code(KC_C);
-      break;
-    case 3:
-      register_code(KC_D);
-      unregister_code(KC_D);
-      break;
-    case 4:
-      register_code(KC_E);
-      unregister_code(KC_E);
-      break;
-    case 5:
-      register_code(KC_F);
-      unregister_code(KC_F);
-      break;
-    case 6:
-      register_code(KC_G);
-      unregister_code(KC_G);
-      break;
-    case 7:
-      register_code(KC_H);
-      unregister_code(KC_H);
-      break;
-    case 8:
-      register_code(KC_I);
-      unregister_code(KC_I);
-      break;
-    case 9:
-      register_code(KC_J);
-      unregister_code(KC_J);
-      break;
-    case 10:
-      register_code(KC_K);
-      unregister_code(KC_K);
-      break;
-    case 11:
-      register_code(KC_L);
-      unregister_code(KC_L);
-      break;
-    case 12:
-      register_code(KC_M); 
-      unregister_code(KC_M); 
-      break;
-    case 13:
-      register_code(KC_N);
-      unregister_code(KC_N);
-      break;
-    case 14:
-      register_code(KC_O);
-      unregister_code(KC_O);
-      break;
-    case 15:
-      register_code(KC_P);
-      unregister_code(KC_P);
-      break;
-    case 16:
-      register_code(KC_Q);
-      unregister_code(KC_Q);
-      break;
-    case 17:
-      register_code(KC_R);
-      unregister_code(KC_R);
-      break;
-    case 18:
-      register_code(KC_S);
-      unregister_code(KC_S);
-      break;
-    case 19:
-      register_code(KC_T);
-      unregister_code(KC_T);
-      break;
-    case 20:
-      register_code(KC_U);
-      unregister_code(KC_U);
-      break;
-    case 21:
-      register_code(KC_V);
-      unregister_code(KC_V);
-      break;
-    case 22:
-      register_code(KC_W);
-      unregister_code(KC_W);
-      break;
-    case 23:
-      register_code(KC_X);
-      unregister_code(KC_X);
-      break;
-    case 24:
-      register_code(KC_Y);
-      unregister_code(KC_Y);
-      break;
-    case 25:
-      register_code(KC_Z);
-      unregister_code(KC_Z);
-      break;
+    /* Generate a keystroke for a letter chosen at random */
+    if (record->event.pressed) {
+      random_letter = letter_codes[rand()%(sizeof(letter_codes) / sizeof(uint16_t))];
+      register_code(random_letter);
+    } else {
+      unregister_code(random_letter);
     }
     break;
   }
