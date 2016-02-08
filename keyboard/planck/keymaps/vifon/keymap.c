da8b5b0e1a0d8862afb87a2aa3bfdd9dd7aa73fb
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2016-02-09 00:04:04 +0100

    Holding both modifiers enables the third layer

--- keyboard/planck/keymaps/vifon/keymap.c
+++ keyboard/planck/keymaps/vifon/keymap.c
@@ -29,6 +29,8 @@ enum {
     _REC_KEY,
     _REC_STOP,
     _MACRO_PLAY,
+
+    _MC1,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,19 +56,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_TILD, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,        KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
     {KC_TRNS, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), M(_REC_START),  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
     {KC_TRNS, LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0), M(_MACRO_PLAY), KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS},
-    {KC_TRNS, BL_TOGG,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_TRNS, BL_TOGG,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        MO(_SP), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 [_RS]= { /* RAISE */
     {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
     {KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
     {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_CM), DF(_PP), RESET,   KC_TRNS},
-    {KC_TRNS, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_VOLD, KC_VOLU, KC_TRNS}
+    {KC_TRNS, BL_STEP, KC_TRNS, KC_TRNS, MO(_SP), KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_VOLD, KC_VOLU, KC_TRNS}
 },
 [_SP]= { /* special */
     {KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_PSCR, KC_BSPC},
     {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SLCK, KC_QUOT},
     {KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-    {KC_LCTL, KC_TRNS, KC_LGUI, KC_LALT, KC_TRNS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+    {KC_LCTL, KC_TRNS, KC_LGUI, KC_LALT, M(_MC1), KC_SPC,  KC_SPC,  M(_MC1), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 [_REC] = { /* Macro recording */
     {M(_REC_KEY), M(_REC_KEY),  M(_REC_KEY), M(_REC_KEY), M(_REC_KEY), M(_REC_KEY), M(_REC_KEY), M(_REC_KEY), M(_REC_KEY), M(_REC_KEY), M(_REC_KEY), M(_REC_KEY)},
@@ -125,6 +127,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
              * recording so we just play the first key now.
              */
             process_action(&macro_buffer[0]);
+        }
+        break;
+
+    case _MC1:
+        if (!record->event.pressed) {
+            layer_off(_LW);
+            layer_off(_RS);
+            layer_off(_SP);
         }
         break;
     }
