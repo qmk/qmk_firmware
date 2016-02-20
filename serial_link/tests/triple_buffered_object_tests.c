50edb3d90c0076eb088f79564e38382452a21d03
Fred Sundvik <fsundvik@gmail.com>
2016-02-20 22:22:42 +0200

    Add first version of triple buffered object

---
+++ serial_link/tests/triple_buffered_object_tests.c
@@ -0,0 +1,44 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Fred Sundvik
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <cgreen/cgreen.h>
+#include "protocol/triple_buffered_object.c"
+
+Describe(TripleBufferedObject);
+BeforeEach(TripleBufferedObject) {}
+AfterEach(TripleBufferedObject) {}
+
+typedef struct {
+    uint8_t state;
+    uint32_t buffer[3];
+}test_object_t;
+
+Ensure(TripleBufferedObject, writes_and_and_reads_object) {
+    test_object_t test_object;
+    uint32_t src = 0x3456ABCC;
+    uint32_t dst;
+    triple_buffer_write(4, (triple_buffer_object_t*)&test_object, &src);
+    triple_buffer_read(4, (triple_buffer_object_t*)&test_object, &dst);
+    assert_that(dst, is_equal_to(src));
+}
