2b9420a4c993eb9be43910bc6d5549f0e5b63007
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2016-02-20 23:29:02 +0100

    Separate the raise+lower layer from the special layer

--- keyboard/planck/keymaps/vifon/keymap.c
+++ keyboard/planck/keymaps/vifon/keymap.c
@@ -17,6 +17,7 @@ enum {
     _PP,
     _LW,
     _RS,
+    _DL,
     _SP,
 };
 
@@ -50,19 +51,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_TILD, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
     {KC_ESC,  LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
     {KC_TRNS, LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0), KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT },
-    {KC_TRNS, BL_TOGG,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, MO(_SP), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_TRNS, BL_TOGG,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, MO(_DL), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 [_RS]= { /* RAISE */
     {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
     {KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
     {KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_CM), DF(_PP), RESET,   KC_ENT },
-    {KC_TRNS, BL_STEP, KC_TRNS, KC_TRNS, MO(_SP), KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_VOLD, KC_VOLU, KC_TRNS}
+    {KC_TRNS, BL_STEP, KC_TRNS, KC_TRNS, MO(_DL), KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_VOLD, KC_VOLU, KC_TRNS}
+},
+[_DL]= { /* DUAL */
+    {KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_PSCR, KC_BSPC},
+    {KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SLCK, KC_QUOT},
+    {KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+    {KC_LCTL, KC_TRNS, KC_LGUI, KC_LALT, M(_MC1), KC_SPC,  KC_SPC,  M(_MC2), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 [_SP]= { /* special */
     {KC_TAB,  M(_REC_START1), M(_MACRO_PLAY1), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_PSCR, KC_BSPC},
     {KC_TRNS, M(_REC_START2), M(_MACRO_PLAY2), KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SLCK, KC_QUOT},
     {KC_LSFT, KC_TRNS,        KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-    {KC_LCTL, KC_TRNS,        KC_LGUI,         KC_LALT, M(_MC1), KC_SPC,  KC_SPC,  M(_MC2), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+    {KC_LCTL, KC_TRNS,        KC_LGUI,         KC_LALT, KC_TRNS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 MACRO_RECORD_KEYMAPS,
 };
@@ -83,14 +90,14 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         if (!record->event.pressed) {
             layer_off(_LW);
             layer_on(_RS);
-            layer_off(_SP);
+            layer_off(_DL);
         }
         break;
     case _MC2:
         if (!record->event.pressed) {
             layer_on(_LW);
             layer_off(_RS);
-            layer_off(_SP);
+            layer_off(_DL);
         }
         break;
     }
