37c8a232764d8c614d6e1183e80bab89d901cbee
Noah Andrews <NoahAndrews@users.noreply.github.com>
2016-02-26 00:29:55 -0500

    Include MSYS directory in Windows path

--- setup-path-win.bat
+++ setup-path-win.bat
@@ -1,5 +1,5 @@
 @echo off
-setx /M path "%PATH%;C:\MinGW\bin" > nul 2>&1
+setx /M path "%PATH%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin" > nul 2>&1
 if NOT ["%errorlevel%"]==["0"] (
 	echo FAILED. Rerun with administrator privileges.
 	pause
