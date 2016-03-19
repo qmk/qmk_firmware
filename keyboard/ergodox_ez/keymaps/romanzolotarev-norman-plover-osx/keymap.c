7138b1e6fce97dbe73d307b576cbbb2676059058
Roman Zolotarev <hi@romanzolotarev.com>
2016-03-19 17:43:02 +0800

    Update layout

--- keyboard/ergodox_ez/keymaps/romanzolotarev-norman-plover-osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/romanzolotarev-norman-plover-osx/keymap.c
@@ -9,53 +9,46 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP(
-    //
-    KC_TILD,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
-    KC_TAB,        KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_BSLS,
+    KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_F14,
+    KC_TAB,        KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    TG(PLVR),
     CTL_T(KC_ESC), KC_A,    KC_S,    KC_E,    KC_T,    KC_G,
     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,
-    TG(PLVR),      KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI,
+    KC_F1,         KC_F2,   KC_F3,   KC_LALT, KC_LGUI,
     /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    KC_VOLD, KC_MUTE,
     /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_VOLU,
-    /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    KC_BSPC, KC_ESC,  KC_FN1,
+    /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    KC_BSPC, CTL_T(KC_ESC), KC_FN1,
     //
-    /*-*/          KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-    /*-*/          KC_SLSH, KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN, KC_MINS,
-    /*-*/          /*-*/    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,    KC_ENT,
-    /*-*/          KC_RBRC, KC_P,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    /*-*/          /*-*/    /*-*/    KC_RCTL, KC_RALT, KC_TRNS, KC_TRNS, KC_TRNS,
+    /*-*/          KC_F15,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_EQL,
+    /*-*/          KC_BSLS, KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN,       KC_MINS,
+    /*-*/          /*-*/    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,          KC_ENT,
+    /*-*/          KC_RBRC, KC_P,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,       KC_RSFT,
+    /*-*/          /*-*/    /*-*/    KC_RGUI, KC_RALT, KC_F4,   KC_F5,         KC_F6,
     KC_MPLY,       KC_MNXT,
     KC_MPRV,
     KC_FN3,        KC_QUOT, KC_SPC
-    //
   ),
-  //
   [SYMB] = KEYMAP(
-    //
-    KC_TRNS,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
-    KC_TRNS,       KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
-    KC_TRNS,       KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
-    KC_TRNS,       KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
+    KC_TRNS,       KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS,
     /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS,
     /*-*/          /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS, KC_TRNS,
     //
-    /*-*/          KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    /*-*/          KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-    /*-*/          /*-*/    KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-    /*-*/          KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-    /*-*/          /*-*/    /*-*/    KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
+    /*-*/          KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_TRNS,
+    /*-*/          /*-*/    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,       KC_TRNS,
+    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
+    /*-*/          /*-*/    /*-*/    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
     KC_TRNS,       KC_TRNS,
     KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_TRNS
-    //
   ),
-  //
   [PLVR] = KEYMAP(
-    //
-    KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
+    KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+    KC_NO,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
     KC_NO,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
     KC_NO,         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_NO,   KC_NO,
@@ -63,37 +56,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS,
     /*-*/          /*-*/    /*-*/    /*-*/    KC_C,    KC_V,    KC_NO,
     //
-    /*-*/          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    /*-*/          KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
-    /*-*/          /*-*/    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-    /*-*/          KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    /*-*/          /*-*/    /*-*/    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    /*-*/          KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,
+    /*-*/          KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_NO,
+    /*-*/          /*-*/    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_LBRC,
+    /*-*/          KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,       KC_QUOT,
+    /*-*/          /*-*/    /*-*/    KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,
     KC_TRNS,       KC_TRNS,
     KC_TRNS,
     KC_NO,         KC_N,    KC_M
-    //
   ),
-  //
   [ARRW] = KEYMAP(
-    //
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,       KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS,
     /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS,
     /*-*/          /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS, KC_TRNS,
     //
-    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,
-    /*-*/          /*-*/    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
-    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
+    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS,       KC_TRNS,
+    /*-*/          /*-*/    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,       KC_TRNS,
+    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
     /*-*/          /*-*/    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS,       KC_TRNS,
     KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_TRNS
-    //
   ),
 };
 
--- keyboard/ergodox_ez/keymaps/romanzolotarev-norman-plover-osx/readme.md
+++ keyboard/ergodox_ez/keymaps/romanzolotarev-norman-plover-osx/readme.md
@@ -3,22 +3,32 @@
 There are four layers:
 
 - **BASE** is [Norman layout](https://normanlayout.info/).
-- **PLVR** is optimized for [Plover](http://www.openstenoproject.org).
 - **SYMB** for numbers and symbols.
+- **PLVR** is optimized for [Plover](http://www.openstenoproject.org).
 - **ARRW** for navigation.
+
+[![keyboard-layout](romanzolotarev-norman-plover-osx.png)]()
 
 ## Switching
 
-- Tap `~L1` to toggle **SYMB**.
-- Tap `~L3` to toggle **ARRW**.
-- Hold `~L1` (or `~L3`) to activate **SYMB** (or **ARRW**) while holding.
-- Tap `L2` to toggle **PLVR**.
+- Tap `SYMB` to toggle **SYMB**.
+- Tap `ARRW` to toggle **ARRW**.
+- Hold `SYMB` (or `ARRW`) to activate **SYMB** (or **ARRW**) while holding.
+- Tap `PLVR` to toggle **PLVR**.
 
 ## LEDs
 
 - Red: SYMB is on.
 - Green: PLVR is on.
 - Blue: ARRW is on.
+
+## Functional Keys
+
+- Tap `F1` to mute microphone via [Shush](http://mizage.com/shush/).
+- Tap `F2` to copy screenshot to the clipboard.
+- Hold `SHIFT` and tap `F2` to save screenshot as a file.
+- Tap `F3`, `F4`, `F5`, `F6` to resize a window via [Divvy](http://mizage.com/divvy/).
+- Tap `F14`, `F15` to adjust display brightness.
 
 ## CTRL/ESC
 
@@ -31,21 +41,3 @@ Both of those keys are frequently used in Vim.
 
 - While in **BASE** hold left `SHIFT` and right `SHIRT` and then tap `N`.
 - Then you can activate **PLVR** and use ErgoDox EZ for steno.
-
-## Layers
-
-BASE
-
-![base](keyboard-layout-editor/base.png)
-
-SYMB
-
-![symb](keyboard-layout-editor/symb.png)
-
-PLVR
-
-![plvr](keyboard-layout-editor/plvr.png)
-
-ARRW
-
-![arrw](keyboard-layout-editor/arrw.png)
