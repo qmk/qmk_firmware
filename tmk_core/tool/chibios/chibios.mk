f02cca91f1e501fd8eff0105882be113b367b5aa
flabbergast <s3+flabbergast@sdfeu.org>
2016-03-25 19:44:37 +0000

    Chibios: disable LTO (link-time optimisation).
    With LTO enabled, sometimes things fail for mysterious reasons
    (e.g. bootloader jump on WF with LEDs enabled), just because the
    linker optimisation is too aggressive.

--- tmk_core/tool/chibios/chibios.mk
+++ tmk_core/tool/chibios/chibios.mk
@@ -35,7 +35,7 @@ endif
 
 # Enable this if you want link time optimizations (LTO)
 ifeq ($(USE_LTO),)
-  USE_LTO = yes
+  USE_LTO = no
 endif
 
 # If enabled, this option allows to compile the application in THUMB mode.
