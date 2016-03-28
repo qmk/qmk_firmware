2181be029e01d9cf46ae3cadcdf25f5bca02c631
Damien Pollet <damien.pollet@gmail.com>
2016-03-28 16:13:37 +0200

    Add action-preprocessing hook to keyboard template

--- quantum/template/template.c
+++ quantum/template/template.c
@@ -11,6 +11,11 @@ void matrix_scan_user(void) {
 }
 
 __attribute__ ((weak))
+void process_action_user(keyrecord_t *record) {
+	// leave this function blank - it can be defined in a keymap file
+}
+
+__attribute__ ((weak))
 void led_set_user(uint8_t usb_led) {
 	// leave this function blank - it can be defined in a keymap file
 }
@@ -18,15 +23,22 @@ void led_set_user(uint8_t usb_led) {
 void matrix_init_kb(void) {
 	// put your keyboard start-up code here
 	// runs once when the firmware starts up
-	
+
 	matrix_init_user();
 }
 
 void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
+	// put your looping keyboard code here
+	// runs every cycle (a lot)
 
 	matrix_scan_user();
+}
+
+void process_action_kb(keyrecord_t *record) {
+	// put your per-action keyboard code here
+	// runs for every action, just before processing by the firmware
+
+	process_action_user(record);
 }
 
 void led_set_kb(uint8_t usb_led) {
