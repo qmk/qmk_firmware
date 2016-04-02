680301e3e3f837aa4f8bda403af3fc42156516fa
eltang <etang2099@gmail.com>
2016-04-02 06:48:44 -0700

    Update action.c

--- tmk_core/common/action.c
+++ tmk_core/common/action.c
@@ -91,7 +91,8 @@ action_t store_or_get_action(bool pressed, keypos_t key)
         for (uint8_t bit_number = 0; bit_number <= 4; bit_number++) {
             source_layers_cache[bit_number][storage_row] ^= (-(bool)((layer & (1U << bit_number)) != 0) ^ source_layers_cache[bit_number][storage_row])) & (1U << storage_bit);
         }
-    } else {
+    }
+    else {
         layer = 0;
         for (uint8_t bit_number = 0; bit_number <= 4; bit_number++) {
             layer |= (uint8_t)(!!(source_layers_cache[bit_number][storage_row] & (1U << storage_bit))) << bit_number;
