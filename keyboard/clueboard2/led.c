34dd96187c5ea2fe6e806f662e6ab0c320d0df1b
skullY <skullydazed@gmail.com>
2016-04-05 20:27:24 -0700

    Fix the clueboard2 led code

--- keyboard/clueboard2/led.c
+++ keyboard/clueboard2/led.c
@@ -25,7 +25,7 @@ void led_init_ports() {
     DDRB |= (1<<4);
 }
 
-void led_set(uint8_t usb_led) {
+void led_set_kb(uint8_t usb_led) {
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
         // Turn capslock on
         PORTB |= (1<<4);
