5f5da5e133562bfb3c0267ef70897d827e4fc2a1
Jack Humbert <jack.humb@gmail.com>
2016-04-16 23:07:50 -0400

    changes to play_notes, goodbye

--- keyboard/preonic/keymaps/default/keymap.c
+++ keyboard/preonic/keymaps/default/keymap.c
@@ -242,7 +242,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         case _QWERTY:
           if (record->event.pressed) {
             #ifdef AUDIO_ENABLE
-              play_notes(&tone_qwerty, 4, false, 0);
+              play_notes(&tone_qwerty, false, 0);
             #endif
             persistant_default_layer_set(1UL<<_QWERTY);
           }
@@ -250,7 +250,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         case _COLEMAK:
           if (record->event.pressed) {
             #ifdef AUDIO_ENABLE
-              play_notes(&tone_colemak, 6, false, 1);
+              play_notes(&tone_colemak, false, 0);
             #endif
             persistant_default_layer_set(1UL<<_COLEMAK);
           }
@@ -258,7 +258,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         case _DVORAK:
           if (record->event.pressed) {
             #ifdef AUDIO_ENABLE
-              play_notes(&tone_dvorak, 8, false, 10);
+              play_notes(&tone_dvorak, false, 0);
             #endif
             persistant_default_layer_set(1UL<<_DVORAK);
           }
@@ -302,7 +302,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
           if (record->event.pressed) {
             #ifdef AUDIO_ENABLE
               audio_on();
-              play_notes(&start_up, 4, false, 0);
+              play_notes(&start_up, false, 0);
             #endif
           }
         break;
@@ -317,7 +317,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         case 9:
           if (record->event.pressed) {
             #ifdef AUDIO_ENABLE
-              play_notes(&tone_music, 8, false, 0);
+              play_notes(&tone_music, false, 0);
               layer_on(_MUSIC);
             #endif
           }
@@ -344,6 +344,6 @@ void process_action_user(keyrecord_t *record) {
 void matrix_init_user(void) {
   #ifdef AUDIO_ENABLE
     init_notes();
-    play_notes(&start_up, 4, false, 0);
+    play_notes(&start_up, false, 0);
   #endif
 }
