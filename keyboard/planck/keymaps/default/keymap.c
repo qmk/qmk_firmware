6136c95b746032c469ed5bc5b4ce9934359094fb
Jack Humbert <jack.humb@gmail.com>
2016-04-28 23:10:40 -0400

    adds condition to one last audio ref in planck default

--- keyboard/planck/keymaps/default/keymap.c
+++ keyboard/planck/keymaps/default/keymap.c
@@ -325,7 +325,9 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
             layer_off(_LOWER);
             layer_off(_ADJUST);
             layer_off(_MUSIC);
-            stop_all_notes();
+            #ifdef AUDIO_ENABLE
+              stop_all_notes();
+            #endif
             layer_on(_PLOVER);
             if (!eeconfig_is_enabled()) {
                 eeconfig_init();
