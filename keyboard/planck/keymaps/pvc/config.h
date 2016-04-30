b23bebbeb348bb7b24f4b54565a019a4b75f8322
IBNobody <ibnobody@gmail.com>
2016-04-30 10:08:10 -0500

    tweaked personal

--- keyboard/planck/keymaps/pvc/config.h
+++ keyboard/planck/keymaps/pvc/config.h
@@ -73,7 +73,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* disable debug print */
-//#define NO_DEBUG
+#define NO_DEBUG
 
 /* disable print */
 //#define NO_PRINT
