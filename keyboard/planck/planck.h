42902e320eaddcb1a8527e2232fd9b44df6a23b0
François Marlier <cino@loop.local>
2016-05-14 12:10:30 +0200

    Unicode
    to have unicode input you need to:
    
    - set your OS input method to UNICODE if needed
    - enable unicode in your makefile
    - copy the action_function from
    keyboard/planck/keymaps/unicode/unicode.c to your keymap.c
    set the target OS method in your keymap.c: void matrix_init_user() {
    set_unicode_mode(UC_OSX); } you can then switch when you want with:
    set_unicode_mode(UC_OSX); set_unicode_mode(UC_LNX);
    set_unicode_mode(UC_WIN);
    put some unicode codes in your keymap like so: UC(0x0061)
    I did change the bit mask in quantum/keymap_common.c and .h
    I’m afraid we will need uint32 to get a total support for all unicode
    tables or relocate the handler as @mbarkhau did.

--- keyboard/planck/planck.h
+++ keyboard/planck/planck.h
@@ -14,6 +14,9 @@
 #ifdef MIDI_ENABLE
 	#include <keymap_midi.h>
 #endif
+#ifdef UNICODE_ENABLE
+	#include <unicode.h>
+#endif
 
 #define PLANCK_MIT( \
 	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
