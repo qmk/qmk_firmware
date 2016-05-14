1ad2097a4aabb2d1e7c19a93b638ea1b37e64e3d
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2016-05-14 23:54:48 +0200

    Secure the reset button

--- keyboard/planck/keymaps/vifon/keymap.c
+++ keyboard/planck/keymaps/vifon/keymap.c
@@ -6,6 +6,7 @@
 #  include "backlight.h"
 #endif
 #include "timer.h"
+#include <bootloader.h>
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -29,6 +30,7 @@ enum usermacro {
     _MLW,
     _MRS,
     _SHLK,                      /* ShiftLock */
+    _RST,                       /* Reset */
 };
 
 #define _______ KC_TRNS
@@ -61,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RS]= { /* RAISE */
     {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
     {KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-    {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_CM), DF(_PP), RESET,   KC_ENT },
+    {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DF(_QW), DF(_CM), DF(_PP), M(_RST), KC_ENT },
     {_______, BL_STEP, _______, _______, M(_MLW), KC_BTN2, KC_BTN2, M(_MRS), KC_MPLY, KC_VOLD, KC_VOLU, _______}
 },
 [_DL]= { /* DUAL */
@@ -116,6 +118,19 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         break;
     case _SHLK:
         register_code(KC_LSFT);
+        break;
+    case _RST:
+        if (record->event.pressed) {
+            key_timer = timer_read();
+        } else {
+            if (timer_elapsed(key_timer) >= 500) {
+                clear_keyboard();
+                backlight_toggle();
+                _delay_ms(250);
+                backlight_toggle();
+                bootloader_jump();
+            }
+        }
         break;
     }
     return MACRO_NONE;
