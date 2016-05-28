ef445bbf15fa751ed0ac31397b6b0496c6fabd92
Samuel Goodwin <samuel@roundwallsoftware.com>
2016-05-28 18:16:06 +0200

    Trying again to start over.

---
+++ keyboard/kc60v2/kc60v2.h
@@ -0,0 +1,30 @@
+#ifndef KC60V2_H
+#define KC60V2_H
+
+#include "matrix.h"
+#include "keymap_common.h"
+#ifdef BACKLIGHT_ENABLE
+	#include "backlight.h"
+#endif
+#include <avr/io.h>
+#include <stddef.h>
+
+// This a shortcut to help you visually see your layout.
+// The following is an example using the Planck MIT layout
+// The first section contains all of the arguements
+// The second converts the arguments into a two-dimensional array
+#define KEYMAP( \
+    k00, k01, k02, \
+      k10,  k11   \
+) \
+{ \
+    { k00, k01,   k02 }, \
+    { k10, KC_NO, k11 }, \
+}
+
+void matrix_init_user(void);
+void matrix_scan_user(void);
+bool process_action_user(keyrecord_t *record);
+void led_set_user(uint8_t usb_led);
+
+#endif
