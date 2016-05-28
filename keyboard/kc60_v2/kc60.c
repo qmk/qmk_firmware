5342fef81db7eccbea206edbd5d215f05a6c9244
Samuel Goodwin <samuel@roundwallsoftware.com>
2016-05-28 20:21:00 +0200

    Fix last port changes and fix LED control in keymap.

--- keyboard/kc60_v2/kc60.c
+++ keyboard/kc60_v2/kc60.c
@@ -66,12 +66,12 @@ void backlight_init_ports()
     // And this: http://blog.saikoled.com/post/43165849837/secret-konami-cheat-code-to-high-resolution-pwm-on
     
     // Pin PB7 = OCR1C (Timer 1, Channel C)
-    // Compare Output Mode = Clear on compare match, Channel C = COM1C1=1 COM1C0=0
+    // Compare Output Mode = Clear on compare match, Channel C = COM1B1=1 COM1C0=0
     // (i.e. start high, go low when counter matches.)
     // WGM Mode 14 (Fast PWM) = WGM13=1 WGM12=1 WGM11=1 WGM10=0
     // Clock Select = clk/1 (no prescaling) = CS12=0 CS11=0 CS10=1
     
-    TCCR1A = _BV(COM1C1) | _BV(WGM11); // = 0b00001010;
+    TCCR1A = _BV(COM1B1) | _BV(WGM11); // = 0b00001010;
     TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10); // = 0b00011001;
 
     backlight_init();
@@ -82,7 +82,7 @@ void backlight_set(uint8_t level)
     if ( level == 0 )
     {
         // Turn off PWM control on PB6, revert to output low.
-        TCCR1A &= ~(_BV(COM1C1));
+        TCCR1A &= ~(_BV(COM1B1));
         CHANNEL = 0x0;
         // Prevent backlight blink on lowest level
         PORTB &= ~(_BV(PORTB6));
@@ -91,8 +91,8 @@ void backlight_set(uint8_t level)
     {
         // Prevent backlight blink on lowest level
         PORTB &= ~(_BV(PORTB6));
-        // Turn on PWM control of PB7
-        TCCR1A |= _BV(COM1C1);
+        // Turn on PWM control of PB6
+        TCCR1A |= _BV(COM1B1);
         // Set the brightness
         CHANNEL = 0xFFFF;
     }
@@ -100,8 +100,8 @@ void backlight_set(uint8_t level)
     {
         // Prevent backlight blink on lowest level
         PORTB &= ~(_BV(PORTB6));
-        // Turn on PWM control of PB7
-        TCCR1A |= _BV(COM1C1);
+        // Turn on PWM control of PB6
+        TCCR1A |= _BV(COM1B1);
         // Set the brightness
         CHANNEL = 0xFFFF >> ((BACKLIGHT_LEVELS - level) * ((BACKLIGHT_LEVELS + 1) / 2));
     }
