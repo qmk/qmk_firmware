4114483c6f6c3805941bb8b208d9854d8083ad96
Samuel Goodwin <samuel@roundwallsoftware.com>
2016-05-28 17:03:32 +0200

    Don't need this for now.

--- keyboard/kc60_v2/keymaps/default.c
+++ keyboard/kc60_v2/keymaps/default.c
@@ -20,14 +20,5 @@ const uint16_t PROGMEM fn_actions[] = {
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-      } else {
-        unregister_code(KC_RSFT);
-      }
-      break;
-  }
   return MACRO_NONE;
 };
