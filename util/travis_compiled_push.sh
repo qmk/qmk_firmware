4111800a76e8ddc18422275012fd182fab63e358
Jack Humbert <jack.humb@gmail.com>
2016-06-19 13:46:44 -0400

    script update

--- util/travis_compiled_push.sh
+++ util/travis_compiled_push.sh
@@ -9,7 +9,7 @@ git config user.email "jack.humb+travis.ci@gmail.com"
 git remote rm origin
 git remote add origin "https://$GH_TOKEN@github.com/jackhumbert/qmk_firmware.git"
 git fetch origin
-git branch -D $TRAVIS_BRANCH-automated-build
+[[ $(git branch | grep $TRAVIS_BRANCH-automated-build) != "" ]] && git branch -D $TRAVIS_BRANCH-automated-build
 git checkout -b $TRAVIS_BRANCH-automated-build
 git commit -Am "adds compiled files from $TRAVIS_BRANCH@${rev}" 
 git push origin $TRAVIS_BRANCH-automated-build
