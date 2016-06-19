41bfd5830e41666cc01633f5025192d9a997f548
Jack Humbert <jack.humb@gmail.com>
2016-06-18 20:02:13 -0400

    script for travis

---
+++ util/travis_compiled_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+# if [ "$TRAVIS_BRANCH" != "master" ]
+# then
+#   echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+#   exit 0
+# fi
+
+rev=$(git rev-parse --short HEAD)
+
+git config user.name "Travis CI"
+git config user.email "jack.humb+travis.ci@gmail.com"
+git remote add origin "https://$GH_TOKEN@github.com/jackhumbert/qmk_firmware.git"
+
+git commit -m "compiling .hex files at ${rev}"
+git push origin/$TRAVIS_BRANCH
