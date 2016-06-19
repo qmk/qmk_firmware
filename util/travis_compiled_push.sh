b970f8816325794466290a37c0b22b69090de25a
Jack Humbert <jack.humb@gmail.com>
2016-06-19 15:20:48 -0400

    throw an add in there

--- util/travis_compiled_push.sh
+++ util/travis_compiled_push.sh
@@ -11,5 +11,6 @@ git remote add origin "https://$GH_TOKEN@github.com/jackhumbert/qmk_firmware.git
 git fetch origin
 [[ $(git branch | grep $TRAVIS_BRANCH-automated-build) != "" ]] && git branch -D $TRAVIS_BRANCH-automated-build
 git checkout -b $TRAVIS_BRANCH-automated-build
-git commit -am "adds compiled files from $TRAVIS_BRANCH@${rev}" 
+git add -A
+git commit -m "adds compiled files from $TRAVIS_BRANCH@${rev}" 
 git push origin -f $TRAVIS_BRANCH-automated-build
