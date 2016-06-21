49ef8c9ef90a509969dcf37bbc3075f2cadcfd38
Zhuochun <zhuochun@hotmail.com>
2016-06-22 02:25:54 +0800

    Adjust SYMB layout

--- keyboard/ergodox_ez/keymaps/zhuochun/keymap.c
+++ keyboard/ergodox_ez/keymaps/zhuochun/keymap.c
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,-------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           | XXXX |  F6  |  F7  |  F8  |  F9  |  F10 |  F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+-------|
- * |        |   !  |   @  |   {  |   }  |   |  | XXXX |           | XXXX |      |   +  |   {  |   }  |   _  |  F12  |
+ * |        |      |  Up  |      |      |      | XXXX |           | XXXX |      |   -  |   {  |   }  |   _  |  F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+-------|
- * |        |   #  |   $  |   [  |   ]  |   `  |------|           |------|      |   =  |   [  |   ]  |   -  | Enter |
+ * |        | Left | Down | Rght |      |      |------|           |------|      |   =  |   [  |   ]  |   +  |       |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+-------|
- * |        |   %  |   ^  |   (  |   )  |   ~  |      |           |      |      |   '  |   (  |   )  |   "  |       |
+ * |        |      |      |      |      |      |      |           |      |      |   '  |   (  |   )  |   "  |       |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+-------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      | Calc |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |Delete|      |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -77,17 +77,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
         // left hand
         KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-        KC_TRNS,KC_HASH,KC_DLR, KC_LBRC,KC_RBRC,KC_GRV,
-        KC_TRNS,KC_PERC,KC_CIRC,KC_LPRN,KC_RPRN,KC_TILD,KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_UP,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,KC_TRNS,KC_TRNS,
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                         KC_TRNS,KC_TRNS,
                                                 KC_TRNS,
-                                KC_TRNS,KC_TRNS,KC_TRNS,
+                                KC_DELT,KC_TRNS,KC_TRNS,
         // right hand
         KC_TRNS,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
-        KC_TRNS,KC_TRNS,KC_PLUS,KC_LCBR,KC_RCBR,KC_UNDS,KC_F12,
-                KC_TRNS,KC_EQL, KC_LBRC,KC_RBRC,KC_MINS,KC_ENT,
+        KC_TRNS,KC_TRNS,KC_MINS,KC_LCBR,KC_RCBR,KC_UNDS,KC_F12,
+                KC_TRNS,KC_EQL, KC_LBRC,KC_RBRC,KC_PLUS,KC_TRNS,
         KC_TRNS,KC_TRNS,KC_QUOT,KC_LPRN,KC_RPRN,SFT_T(KC_QUOT),KC_TRNS,
                         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
         KC_TRNS,KC_TRNS,
