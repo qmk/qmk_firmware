b5890df6d02c5461b30b0f94065eac579d03eb4f
Fred Sundvik <fsundvik@gmail.com>
2016-06-22 12:35:56 +0300

    Add ChibiOS and ChibiOS-Contrib submodules
    Also fix the makefile path for them.

--- tmk_core/tool/chibios/chibios.mk
+++ tmk_core/tool/chibios/chibios.mk
@@ -83,8 +83,8 @@ endif
 #
 
 # Imported source files and paths
-CHIBIOS ?= $(TMK_DIR)/tool/chibios/chibios
-CHIBIOS_CONTRIB ?= $(TMK_DIR)/tool/chibios/chibios-contrib
+CHIBIOS ?= $(TMK_DIR)/../lib/chibios
+CHIBIOS_CONTRIB ?= $(TMK_DIR)/../lib/chibios-contrib
 # Startup files. Try a few different locations, for compability with old versions and 
 # for things hardware in the contrib repository
 STARTUP_MK = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/mk/startup_$(MCU_STARTUP).mk
