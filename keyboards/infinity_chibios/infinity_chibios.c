6c1964ddb4e516645ae6523b595677a109ac5fbf
Fred Sundvik <fsundvik@gmail.com>
2016-06-29 21:15:02 +0300

    Fix the infinity_chibios compilation

--- keyboards/infinity_chibios/infinity_chibios.c
+++ keyboards/infinity_chibios/infinity_chibios.c
@@ -14,17 +14,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "keymap_common.h"
-
-
-/* translates key to keycode */
-uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
-{
-    return keymaps[(layer)][(key.row)][(key.col)];
-}
-
-/* translates Fn keycode to action */
-action_t keymap_fn_to_action(uint8_t keycode)
-{
-    return (action_t){ .code = fn_actions[FN_INDEX(keycode)] };
-}
+#include "infinity_chibios.h"
--- keyboards/infinity_chibios/infinity_chibios.h
+++ keyboards/infinity_chibios/infinity_chibios.h
@@ -17,22 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef KEYMAP_COMMON_H
 #define KEYMAP_COMMON_H
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "keycode.h"
-#include "action.h"
-#include "action_macro.h"
-#include "action_util.h"
-#include "report.h"
-#include "host.h"
-#include "print.h"
-#include "debug.h"
-#include "keymap.h"
-
-
-extern const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS];
-extern const uint16_t fn_actions[];
-
+#include "quantum.h"
 
 #ifdef INFINITY_PROTOTYPE
 
