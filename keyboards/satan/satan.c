13f852e28c5d3106670741238fd5cabf29e0de79
TerryMathews <terry@terrymathews.net>
2016-06-29 22:12:44 -0400

    Fix CAPS_LED logic in Satan
    Needed to invert for proper operation. Previous code had LED on when off
    and vice versa.

--- keyboards/satan/satan.c
+++ keyboards/satan/satan.c
@@ -22,9 +22,9 @@ void led_init_ports(void) {
 void led_set_kb(uint8_t usb_led) {
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
         // Turn capslock on
-        PORTB |= (1<<2);
+        PORTB &= ~(1<<2);
     } else {
         // Turn capslock off
-        PORTB &= ~(1<<2);
+        PORTB |= (1<<2);
     }
 }
