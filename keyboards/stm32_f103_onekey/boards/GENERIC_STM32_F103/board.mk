386f83261b7e5f6716be11d2c4e551c1a69f6030
Fred Sundvik <fsundvik@gmail.com>
2016-07-01 12:53:12 +0300

    Fix the board path of stm32_f103_onekey

--- keyboards/stm32_f103_onekey/boards/GENERIC_STM32_F103/board.mk
+++ keyboards/stm32_f103_onekey/boards/GENERIC_STM32_F103/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = ./boards/GENERIC_STM32_F103/board.c
+BOARDSRC = $(KEYBOARD_PATH)/boards/GENERIC_STM32_F103/board.c
 
 # Required include directories
-BOARDINC = ./boards/GENERIC_STM32_F103
+BOARDINC = $(KEYBOARD_PATH)/boards/GENERIC_STM32_F103
