b11657d1c8a3360779b6b412806d89ca3175fc5f
Fred Sundvik <fsundvik@gmail.com>
2016-07-05 13:48:12 +0300

    Suprojects for ChibiOS testing

--- keyboards/chibios_test/config.h
+++ keyboards/chibios_test/config.h
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include "config_common.h"
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
@@ -25,11 +26,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 /* in python2: list(u"whatever".encode('utf-16-le')) */
 /*   at most 32 characters or the ugly hack in usb_main.c borks */
-#define MANUFACTURER "TMK"
+#define MANUFACTURER "QMK"
 #define USBSTR_MANUFACTURER    'T', '\x00', 'M', '\x00', 'K', '\x00', ' ', '\x00', '\xc6', '\x00'
-#define PRODUCT "ChibiOS TMK test"
-#define USBSTR_PRODUCT         'C', '\x00', 'h', '\x00', 'i', '\x00', 'b', '\x00', 'i', '\x00', 'O', '\x00', 'S', '\x00', ' ', '\x00', 'T', '\x00', 'M', '\x00', 'K', '\x00', ' ', '\x00', 't', '\x00', 'e', '\x00', 's', '\x00', 't', '\x00'
-#define DESCRIPTION "TMK keyboard firmware over ChibiOS"
+#define PRODUCT "ChibiOS QMK test"
+#define USBSTR_PRODUCT         'C', '\x00', 'h', '\x00', 'i', '\x00', 'b', '\x00', 'i', '\x00', 'O', '\x00', 'S', '\x00', ' ', '\x00', 'Q', '\x00', 'M', '\x00', 'K', '\x00', ' ', '\x00', 't', '\x00', 'e', '\x00', 's', '\x00', 't', '\x00'
+#define DESCRIPTION "QMK keyboard firmware test for ChibiOS"
 
 /* key matrix size */
 #define MATRIX_ROWS 1
--- keyboards/chibios_test/stm32_f072_onekey/stm32_f072_onekey.h
+++ keyboards/chibios_test/stm32_f072_onekey/stm32_f072_onekey.h
@@ -1,5 +1,5 @@
 #ifndef STM32_F072_ONEKEY_H
 #define STM32_F072_ONEKEY_H
-#include "quantum.h"
+#include "chibios_test.h"
 #endif
 
