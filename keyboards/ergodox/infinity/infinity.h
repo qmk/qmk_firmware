e137f11889a6cd8ac2ae9eaa4782e7e956fcceef
Fred Sundvik <fsundvik@gmail.com>
2016-07-09 01:38:58 +0300

    Add empty led control functions to Infinity Ergodox
    So that most keymaps compiles and links

--- keyboards/ergodox/infinity/infinity.h
+++ keyboards/ergodox/infinity/infinity.h
@@ -3,6 +3,72 @@
 
 #include "quantum.h"
 
+void ergodox_board_led_on(void);
+void ergodox_right_led_1_on(void);
+void ergodox_right_led_2_on(void);
+void ergodox_right_led_3_on(void);
+void ergodox_right_led_on(uint8_t led);
+
+void ergodox_board_led_off(void);
+void ergodox_right_led_1_off(void);
+void ergodox_right_led_2_off(void);
+void ergodox_right_led_3_off(void);
+void ergodox_right_led_off(uint8_t led);
+
+inline void ergodox_led_all_on(void)
+{
+    ergodox_board_led_on();
+    ergodox_right_led_1_on();
+    ergodox_right_led_2_on();
+    ergodox_right_led_3_on();
+}
+
+inline void ergodox_led_all_off(void)
+{
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+}
+
+inline void ergodox_right_led_1_set(uint8_t n){
+	if (n) {
+		ergodox_right_led_1_on();
+	} else {
+		ergodox_right_led_1_off();
+	}
+}
+
+inline void ergodox_right_led_2_set(uint8_t n){
+	if (n) {
+		ergodox_right_led_2_on();
+	} else {
+		ergodox_right_led_2_off();
+	}
+}
+
+inline void ergodox_right_led_3_set(uint8_t n){
+	if (n) {
+		ergodox_right_led_3_on();
+	} else {
+		ergodox_right_led_3_off();
+	}
+}
+
+inline void ergodox_right_led_set(uint8_t led, uint8_t n){
+	if (n) {
+		ergodox_right_led_on(led);
+	} else {
+		ergodox_right_led_off(led);
+	}
+}
+
+inline void ergodox_led_all_set(uint8_t n) {
+    ergodox_right_led_1_set(n);
+    ergodox_right_led_2_set(n);
+    ergodox_right_led_3_set(n);
+}
+
 #define KEYMAP( \
     A80, A70, A60, A50, A40, A30, A20,  \
     A81, A71, A61, A51, A41, A31, A21,  \
