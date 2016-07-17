08851da429afcbaf6d23b44e989c897c3a32835a
Mazin Bokhari <mazin.bokhari@gmail.com>
2016-07-17 14:29:37 -0500

    Remove more unnecessary buttons

--- keyboard/ergodox_ez/keymaps/maz/keymap.c
+++ keyboard/ergodox_ez/keymaps/maz/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |Grv/L1|      |      |      |*ARRW*|                                       |      |      |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,                 KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_TAB,                 KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_ESC,                 KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,                KC_Z,  KC_X,  KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),        KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,LT(ARRW, KC_RGHT),
+        KC_LSFT,                KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        LT(SYMB,KC_GRV),        KC_TRNS,      KC_TRNS,KC_TRNS,MO(ARRW),
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                CTL_T(KC_SPC),GUI_T(KC_BSPC),KC_LALT,
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(SYMB),     KC_Y,   KC_U,             KC_I,    KC_O,   KC_P,             KC_BSLS,
                       KC_H,   KC_J,             KC_K,    KC_L,   KC_SCLN,          KC_QUOT,
         MEH_T(KC_NO), KC_N,   KC_M,             KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT,
-                              LT(MDIA, KC_UP),  KC_DOWN, KC_LBRC, KC_RBRC,          KC_FN1,
+                              MO(MDIA),         KC_TRNS, KC_LBRC,KC_RBRC,          KC_FN1,
         KC_LALT, KC_ESC,
         KC_PGUP,
         KC_RALT,GUI_T(KC_TAB), CTL_T(KC_ENT)
