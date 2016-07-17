42a78e1d756f95abdbe37481d0a622e70911dcac
Mazin Bokhari <mazin.bokhari@gmail.com>
2016-07-17 14:56:32 -0500

    Remove more keys

--- keyboard/ergodox_ez/keymaps/maz/keymap.c
+++ keyboard/ergodox_ez/keymaps/maz/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |      |  Grv |      |*SYMB*|*ARRW*|                                       |*MDIA*|*SYMB*|   [  |  ]   |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 |      |      |      |       |      |        |      |
  *                                 | Space| Bksp |------|       |------|  Tab   |Enter |
  *                                 | ctrl | gui  | Alt  |       | Alt  |  gui   | ctrl |
  *                                 `--------------------'       `----------------------'
@@ -39,22 +39,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,                 KC_1,             KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_EQL,                 KC_1,             KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
         KC_TAB,                 KC_Q,             KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_ESC,                 KC_A,             KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,                KC_Z,             KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         KC_TRNS,                KC_GRV,  KC_TRNS,MO(SYMB),MO(ARRW),
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
+                                                    KC_TRNS,  KC_TRNS,
+                                                              KC_TRNS,
                                                CTL_T(KC_SPC),GUI_T(KC_BSPC),KC_LALT,
         // right hand
-        KC_RGHT,      KC_6,   KC_7,             KC_8,    KC_9,   KC_0,             KC_MINS,
+        KC_TRNS,      KC_6,   KC_7,             KC_8,    KC_9,   KC_0,             KC_MINS,
         TG(SYMB),     KC_Y,   KC_U,             KC_I,    KC_O,   KC_P,             KC_BSLS,
                       KC_H,   KC_J,             KC_K,    KC_L,   KC_SCLN,          KC_QUOT,
         MEH_T(KC_NO), KC_N,   KC_M,             KC_COMM, KC_DOT, KC_SLSH,          KC_RSFT,
         MO(MDIA),                  MO(SYMB), KC_LBRC,KC_RBRC, KC_TRNS,
-        KC_LALT, KC_ESC,
-        KC_PGUP,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS,
         KC_RALT,GUI_T(KC_TAB), CTL_T(KC_ENT)
     ),
 /* Keymap 1: Symbol Layer
