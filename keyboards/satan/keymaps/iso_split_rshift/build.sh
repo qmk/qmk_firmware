4b32a161d04c9efe650479b89d4c8f323dd41aa3
Toni <jeder@mail1a.de>
2016-07-18 10:50:45 +0200

    renamed keymap

--- keyboards/satan/keymaps/iso_split_rshift/build.sh
+++ keyboards/satan/keymaps/iso_split_rshift/build.sh
@@ -2,15 +2,16 @@
 # adjust for cpu 
 # -j 16 gave best result on a hyperthreaded quad core core i7
 THREADS="-j 16"
+KMAP=iso_split_rschift
 echo "We need sudo later"
 sudo ls 2>&1 /dev/null
 make clean
-make KEYMAP=toni77 ${THREADS}
+make KEYMAP=iso_split_rschift ${THREADS}
 if [[ $? -eq 0 ]]
 then
     echo "please trigger flashing! you have 5 seconds"
     sleep 5
-    sudo make KEYMAP=toni77 dfu ${THREADS}
+    sudo make KEYMAP=iso_split_rschift dfu ${THREADS}
 else
     echo "make failed"
     exit 77
