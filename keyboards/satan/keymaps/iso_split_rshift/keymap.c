1024c4e19cc251300c583f6c08bbae23817cdaf2
Toni <jeder@mail1a.de>
2016-07-22 08:13:06 +0200

    purely cosmetic changes

--- keyboards/satan/keymaps/iso_split_rshift/keymap.c
+++ keyboards/satan/keymaps/iso_split_rshift/keymap.c
@@ -14,7 +14,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap _DEF: Default Layer
-     * ,-----------------------------------------------------------.-------------------------------------
+     * ,-----------------------------------------------------------.
      * |Grv|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
      * |-----------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|   \ | Tab is Fn1 !!
@@ -26,14 +26,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Ctrl|Win |Alt |      Space/Fn0         |Alt |Win |Menu|RCtl| Gui Menu, RCtrl is 
      * `-----------------------------------------------------------'   LEFT DWN RIGHT
      */
-[_DEF] = KEYMAP_ISO_SPLITRSHIFT(
-         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  \
-         F(2),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
-         F(1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, F(17),   \
-         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TG(_SFX),F(4),  \
-         KC_LCTL, KC_LGUI, KC_LALT,             LT(_SPC,KC_SPACE),               KC_RALT, F(5),    F(6),    F(7)) ,
+    [_DEF] = KEYMAP_ISO_SPLITRSHIFT(
+            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+            F(2),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
+            F(1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, F(17),   \
+            KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TG(_SFX),F(4),    \
+            KC_LCTL, KC_LGUI, KC_LALT,                    LT(_SPC,KC_SPACE),                          KC_RALT, F(5),    F(6),    F(7)) ,
+
     /* Keymap 1: F-and-vim Layer, modified with Space (Fn0)
- * ,-----------------------------------------------------------.-----------------------------------------
+     * ,-----------------------------------------------------------.
      * |PrSc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete|
      * |-----------------------------------------------------------|
      * |    |Paus| Up|   |   |   |   |   |   |   |   |   |   |     |
@@ -45,15 +46,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                        |Alt |Prev|Vol-|Next|
      * `-----------------------------------------------------------'
      */
-[_SPC] = KEYMAP_ISO_SPLITRSHIFT(
-         KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-         KC_TRNS, KC_PAUS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-         KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, \
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPACE,M(0),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, \
-         KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,                         KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT) ,
-    
+    [_SPC] = KEYMAP_ISO_SPLITRSHIFT(
+            KC_PSCR, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
+            KC_TRNS, KC_PAUS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+            KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPACE,M(0),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, \
+            KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT),
+
     /* Keymap 2: Tab Layer w/ vim pageup, modified with Tab (Fn1)
-     * ,-----------------------------------------------------------.-----------------------------------------
+     * ,-----------------------------------------------------------.
      * |WAKE|   |   |   |   |   |   |   |   |   |   |   |   |Insert|  TAB+GRC = WAKE
      * |-----------------------------------------------------------|
      * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
@@ -65,15 +66,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                        |Alt |Pos1|PgDn|End |
      * `-----------------------------------------------------------'
      */
-[_TAB] = KEYMAP_ISO_SPLITRSHIFT(
-         KC_WAKE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  \
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT,  \
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M(1),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, \
-         KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,                         KC_TRNS, KC_HOME, KC_PGDN, KC_END) ,
-    
+    [_TAB] = KEYMAP_ISO_SPLITRSHIFT(
+            KC_WAKE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT,  \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M(1),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, \
+            KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_HOME, KC_PGDN, KC_END),
+
     /* Keymap 3: Split right shift Numpad toggle Layer (Fn2)
-     * ,-----------------------------------------------------------.-----------------------------------------
+     * ,-----------------------------------------------------------.
      * |RSET|  |   |   |   |   |   |  7|  8|  9|   |   |   |Backsp |
      * |-----------------------------------------------------------|
      * |     |   |   |   |   |   |   | 4 | 5 | 6 |   |   |   |   \ |
@@ -85,13 +86,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Ctrl|Win |Alt |                        |Alt |Left|Down|Right|
      * `-----------------------------------------------------------'
      */
-[_SFX] = KEYMAP_ISO_SPLITRSHIFT(
-         RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, \
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, \
-         KC_TRNS, F(9),    F(10),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS, KC_NO,   KC_ENT,  \
-         KC_TRNS, F(11),   F(12),   F(13),   F(14),   F(15),   F(16),   KC_TRNS, KC_TRNS, KC_0,    KC_TRNS, KC_SLSH, KC_TRNS, KC_UP,   \
-         KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,                         KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT) ,
-
+    [_SFX] = KEYMAP_ISO_SPLITRSHIFT(
+            RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, \
+            KC_TRNS, F(9),    F(10),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS, KC_NO,   KC_ENT,  \
+            KC_TRNS, F(11),   F(12),   F(13),   F(14),   F(15),   F(16),   KC_TRNS, KC_TRNS, KC_0,    KC_TRNS, KC_SLSH, KC_TRNS, KC_UP,   \
+            KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT),
 };
 
 enum function_id {
@@ -129,19 +129,19 @@ const uint16_t PROGMEM fn_actions[] = {
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-  // MACRODOWN only works in this function
-      switch(id) {
+    // MACRODOWN only works in this function
+    switch(id) {
         case 0:
-          return (record->event.pressed ? 
-                  MACRO( D(RALT), T(SPC), U(RALT), END )
-                  :MACRO( END ));
-          break;
+            return (record->event.pressed ? 
+                    MACRO( D(RALT), T(SPC), U(RALT), END )
+                    :MACRO( END ));
+            break;
         case 1:
-          return (record->event.pressed ? 
-                  MACRO( D(LALT), T(F2), U(LALT), END )
-                  :MACRO( END ));
-          break;
-      }
+            return (record->event.pressed ? 
+                    MACRO( D(LALT), T(F2), U(LALT), END )
+                    :MACRO( END ));
+            break;
+    }
     return MACRO_NONE;
 };
 
