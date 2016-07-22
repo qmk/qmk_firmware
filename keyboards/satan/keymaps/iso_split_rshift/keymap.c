5631f1436c356fb9e09138f186981dc9b6cb43a2
Toni <jeder@mail1a.de>
2016-07-22 11:17:57 +0200

    converted funtions to QMK aliases

--- keyboards/satan/keymaps/iso_split_rshift/keymap.c
+++ keyboards/satan/keymaps/iso_split_rshift/keymap.c
@@ -27,13 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'   LEFT DWN RIGHT
      */
     [_DEF] = KEYMAP_ISO_SPLITRSHIFT(
-            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-            F(2),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-            F(1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, F(17),   \
-            KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TG(_SFX),F(4),    \
-            KC_LCTL, KC_LGUI, KC_LALT,                    LT(_SPC,KC_SPACE),                          KC_RALT, F(5),    F(6),    F(7)) ,
-
-    /* Keymap 1: F-and-vim Layer, modified with Space (Fn0)
+            KC_GRV,          KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS,        KC_EQL,          KC_BSPC,         \
+            LT(_TAB,KC_TAB), KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,        KC_RBRC,         KC_BSLS,         \
+            CTL_T(KC_ESC),   KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,        KC_NUHS,         CTL_T(KC_ENT),   \
+            KC_LSFT,         KC_NUBS, KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,        TG(_SFX),        SFT_T(KC_UP),    \
+            KC_LCTL,         KC_LGUI, KC_LALT,             LT(_SPC,KC_SPACE),               KC_RALT, ALT_T(KC_LEFT), GUI_T(KC_DOWN),  CTL_T(KC_RIGHT)),
+    /* Keymap 1: F-and-vim Layer, modified with Space (by holding space)
      * ,-----------------------------------------------------------.
      * |PrSc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete|
      * |-----------------------------------------------------------|
@@ -53,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPACE,M(0),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, \
             KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT),
 
-    /* Keymap 2: Tab Layer w/ vim pageup, modified with Tab (Fn1)
+    /* Keymap 2: Tab Layer w/ vim pageup, modified with Tab (by holding tab)
      * ,-----------------------------------------------------------.
      * |WAKE|   |   |   |   |   |   |   |   |   |   |   |   |Insert|  TAB+GRC = WAKE
      * |-----------------------------------------------------------|
@@ -73,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M(1),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, \
             KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_HOME, KC_PGDN, KC_END),
 
-    /* Keymap 3: Split right shift Numpad toggle Layer (Fn2)
+    /* Keymap 3: Split right shift Numpad toggle Layer (by tapping the split rshift key)
      * ,-----------------------------------------------------------.
      * |RSET|  |   |   |   |   |   |  7|  8|  9|   |   |   |Backsp |
      * |-----------------------------------------------------------|
@@ -87,11 +86,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      */
     [_SFX] = KEYMAP_ISO_SPLITRSHIFT(
-            RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, \
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, \
-            KC_TRNS, F(9),    F(10),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS, KC_NO,   KC_ENT,  \
-            KC_TRNS, F(11),   F(12),   F(13),   F(14),   F(15),   F(16),   KC_TRNS, KC_TRNS, KC_0,    KC_TRNS, KC_SLSH, KC_TRNS, KC_UP,   \
-            KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT),
+            RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, \
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, \
+            KC_TRNS, F(2),    F(3),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3, KC_TRNS, KC_TRNS, KC_NO,   KC_ENT,  \
+            KC_TRNS, F(4),    F(5),    F(6),    F(7),    F(8),    F(9),    KC_TRNS, KC_TRNS, KC_0, KC_TRNS, KC_SLSH, KC_TRNS, KC_UP,   \
+            KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                         KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT),
 };
 
 enum function_id {
@@ -107,24 +106,16 @@ enum function_id {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0]  = ACTION_LAYER_TAP_KEY(_SPC, KC_SPACE),
-    [1]  = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
-    [2]  = ACTION_LAYER_TAP_KEY(_TAB, KC_TAB),
-    [3]  = ACTION_LAYER_TOGGLE(_SFX),
-    [4]  = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_UP),
-    [5]  = ACTION_MODS_TAP_KEY(MOD_RALT, KC_LEFT),
-    [6]  = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_DOWN),
-    [7]  = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_RIGHT),
-    [8]  = ACTION_FUNCTION(LAUNCH),
- purely cosmetic changes   [9]  = ACTION_FUNCTION(RGBLED_TOGGLE),
-    [10] = ACTION_FUNCTION(RGBLED_STEP_MODE),
-    [11] = ACTION_FUNCTION(RGBLED_INCREASE_HUE),
-    [12] = ACTION_FUNCTION(RGBLED_DECREASE_HUE),
-    [13] = ACTION_FUNCTION(RGBLED_INCREASE_SAT),
-    [14] = ACTION_FUNCTION(RGBLED_DECREASE_SAT),
-    [15] = ACTION_FUNCTION(RGBLED_INCREASE_VAL),
-    [16] = ACTION_FUNCTION(RGBLED_DECREASE_VAL),
-    [17] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ENT),
+    [1] = ACTION_FUNCTION(LAUNCH),
+    [2] = ACTION_FUNCTION(RGBLED_TOGGLE),
+    [3] = ACTION_FUNCTION(RGBLED_STEP_MODE),
+    [4] = ACTION_FUNCTION(RGBLED_INCREASE_HUE),
+    [5] = ACTION_FUNCTION(RGBLED_DECREASE_HUE),
+    [6] = ACTION_FUNCTION(RGBLED_INCREASE_SAT),
+    [7] = ACTION_FUNCTION(RGBLED_DECREASE_SAT),
+    [8] = ACTION_FUNCTION(RGBLED_INCREASE_VAL),
+    [9] = ACTION_FUNCTION(RGBLED_DECREASE_VAL),
+    [10] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ENT),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
