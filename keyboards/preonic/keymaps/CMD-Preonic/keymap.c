317b3e3227bf39f7bc6e7bd4b65913c7feb8d2ce
commandlinedesign <commandlinedesign@gmail.com>
2016-07-23 20:02:45 -0700

    Tested, resolved Mouse issue, resolved numpad issue

--- keyboards/preonic/keymaps/CMD-Preonic/keymap.c
+++ keyboards/preonic/keymaps/CMD-Preonic/keymap.c
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC},
-  {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLASH},
-  {LT(_RAISE, KC_ENT), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT},
-  {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT },
-  {M(M_BL),   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_MINUS, KC_EQUAL, KC_LBRC,  KC_RBRC}
+  {KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC},
+  {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLASH},
+  {LT(_RAISE, KC_ENT), KC_A,    KC_S,    KC_D,    KC_F,                KC_G,    KC_H,    KC_J,     KC_K,     KC_L,     KC_SCLN, KC_QUOT},
+  {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT },
+  {M(M_BL),   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   LT(_LOWER, KC_SPC),  KC_SPC,  RAISE,   KC_MINUS, KC_EQUAL, KC_LBRC,  KC_RBRC}
 },
 
 /* Colemak
@@ -134,11 +134,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] = {
-  {KC_ESC,  KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, _______, _______, _______, _______, _______, _______, _______, _______},
-  {KC_KP_7, KC_KP_8,     KC_KP_9,        KC_KP_PLUS,  _______, _______, _______, _______, _______, _______, _______, _______},
-  {KC_KP_4, KC_KP_5,     KC_KP_6,        KC_KP_PLUS,  _______, _______, _______, _______, _______, _______, _______, _______},
-  {KC_KP_1, KC_KP_2,     KC_KP_3,        KC_KP_ENTER, _______, _______, _______, _______, _______, _______, _______, _______},
-  {KC_KP_0, KC_KP_0,     KC_KP_DOT,      KC_KP_ENTER, _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_ESC, KC_PSLS, KC_PAST, KC_PMNS,  _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_P7,  KC_P8,   KC_P9,   KC_PPLS,  _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_P4,  KC_P5,   KC_P6,   KC_PPLS,  _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_P1,  KC_P2,   KC_P3,   KC_PENT,  _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_P0,  KC_P0,   KC_PDOT, KC_PENT,  _______, _______, _______, _______, _______, _______, _______, _______},
 },
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -154,11 +154,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_GRV,  KC_F1,    KC_F2,   KC_F3,      KC_F4,          KC_F5,         KC_F6,       KC_F7,       KC_F8,          KC_F9,       KC_F10,  KC_DEL},
-  {KC_MUTE, KC_BSPC,  KC_UP,   KC_DEL,     KC_WWW_FORWARD, KC_MS_BTN1,    KC_MS_UP,    KC_MS_BTN2,  KC_KP_SLASH,    KC_KP_7,     KC_KP_8, KC_KP_9},
-  {_______, KC_LEFT,  KC_DOWN, KC_RIGHT,   KC_WWW_REFRESH, KC_MS_LEFT,    KC_MS_DOWN,  KC_MS_RIGHT, KC_KP_ASTERISK, KC_KP_4,      KC_KP_5, KC_KP_6},
-  {KC_VOLU, KC_MPRV,  KC_MPLY, KC_MNXT,    KC_WWW_BACK,    KC_MS_WH_DOWN, KC_MS_BTN3,  KC_MS_WH_UP, KC_KP_MINUS,    KC_KP_1,     KC_KP_2, KC_KP_3},
-  {KC_VOLD, KC_PAUSE, KC_INS,  KC_PSCREEN, _______,        _______,       _______,     _______,     KC_KP_PLUS,     KC_KP_ENTER, KC_KP_0, KC_KP_DOT}
+  {KC_GRV,  KC_F1,    KC_F2,   KC_F3,      KC_F4,          KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10, KC_DEL},
+  {KC_MUTE, KC_BSPC,  KC_UP,   KC_DEL,     KC_WWW_FORWARD, KC_BTN1, KC_MS_U, KC_BTN2, KC_PSLS,  KC_P7,   KC_P8,  KC_P9},
+  {_______, KC_LEFT,  KC_DOWN, KC_RIGHT,   KC_WWW_REFRESH, KC_MS_L, KC_MS_D, KC_MS_R, KC_PAST,  KC_P4,   KC_P5,  KC_P6},
+  {KC_VOLU, KC_MPRV,  KC_MPLY, KC_MNXT,    KC_WWW_BACK,    KC_WH_D, KC_BTN3, KC_WH_U, KC_PMNS,  KC_P1,   KC_P2,  KC_P3},
+  {KC_VOLD, KC_PAUSE, KC_INS,  KC_PSCREEN, _______,        _______, _______, _______, KC_PPLS, KC_PENT, KC_P0,  KC_PDOT}
 },
 
 /* Raise
