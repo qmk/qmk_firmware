6b4f0e49583da5444e05b19ce8fb0ea7280156ca
JeeBak Kim <jeebak.kim@gmail.com>
2016-07-26 13:34:01 -0700

    Tap for Enter, hold for Shift

--- keyboards/planck/keymaps/jeebak/keymap.c
+++ keyboards/planck/keymaps/jeebak/keymap.c
@@ -42,60 +42,61 @@ enum planck_keycodes {
 #define CTL_ESC     CTL_T(KC_ESC)               // Tap for Esc, hold for Ctrl
 #define LT_TC       LT(_TOUCHCURSOR, KC_SPC)    // L-ayer T-ap T-ouch C-ursor
 //                  ^-- Requires KC_TRNS / _______ for the trigger key in the destination layer
+#define SFT_ENT     SFT_T(KC_ENT)               // Tap for Enter, hold for Shift
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,---------------------------------------------------------------------------------------.
- * | Tab      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |----------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl/Esc |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |----------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift    |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |----------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite    | Ctrl | Alt  | GUI  |Lower |  TC/Space   |Raise | Left | Down |  Up  |Right |
- * `---------------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------------------------.
+ * | Tab      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
+ * |----------+------+------+------+------+-------------+------+------+------+------+--------|
+ * | Ctrl/Esc |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   "    |
+ * |----------+------+------+------+------+------|------+------+------+------+------+--------|
+ * | Shift    |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft/Ent |
+ * |----------+------+------+------+------+------+------+------+------+------+------+--------|
+ * | Brite    | Ctrl | Alt  | GUI  |Lower |  TC/Space   |Raise | Left | Down |  Up  | Right  |
+ * `-----------------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
   {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   LT_TC,   LT_TC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Colemak
- * ,---------------------------------------------------------------------------------------.
- * | Tab      |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
- * |----------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl/Esc |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
- * |----------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift    |   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |----------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite    | Ctrl | Alt  | GUI  |Lower |  TC/Space   |Raise | Left | Down |  Up  |Right |
- * `---------------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------------------------.
+ * | Tab      |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  |  Bksp  |
+ * |----------+------+------+------+------+-------------+------+------+------+------+--------|
+ * | Ctrl/Esc |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |   "    |
+ * |----------+------+------+------+------+------|------+------+------+------+------+--------|
+ * | Shift    |   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Sft/Ent |
+ * |----------+------+------+------+------+------+------+------+------+------+------+--------|
+ * | Brite    | Ctrl | Alt  | GUI  |Lower |  TC/Space   |Raise | Left | Down |  Up  | Right  |
+ * `-----------------------------------------------------------------------------------------'
  */
 [_COLEMAK] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
   {CTL_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
   {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   LT_TC,   LT_TC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Dvorak
- * ,---------------------------------------------------------------------------------------.
- * | Tab      |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
- * |----------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl/Esc |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
- * |----------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift    |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
- * |----------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite    | Ctrl | Alt  | GUI  |Lower |  TC/Space   |Raise | Left | Down |  Up  |Right |
- * `---------------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------------------------.
+ * | Tab      |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  |  Bksp  |
+ * |----------+------+------+------+------+-------------+------+------+------+------+--------|
+ * | Ctrl/Esc |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |   /    |
+ * |----------+------+------+------+------+------|------+------+------+------+------+--------|
+ * | Shift    |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Sft/Ent |
+ * |----------+------+------+------+------+------+------+------+------+------+------+--------|
+ * | Brite    | Ctrl | Alt  | GUI  |Lower |  TC/Space   |Raise | Left | Down |  Up  | Right  |
+ * `-----------------------------------------------------------------------------------------'
  */
 [_DVORAK] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
   {CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH},
-  {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT },
+  {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    SFT_ENT},
   {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   LT_TC,  LT_TC,    RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
