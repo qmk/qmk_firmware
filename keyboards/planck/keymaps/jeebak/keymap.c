6d7b74b682f60c048cf6dbc276c1d5d4913a5743
JeeBak Kim <jeebak.kim@gmail.com>
2016-08-06 00:08:42 -0700

    Move KC_ACL1 button

--- keyboards/planck/keymaps/jeebak/keymap.c
+++ keyboards/planck/keymaps/jeebak/keymap.c
@@ -186,9 +186,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Mouse Layer
  * ,-----------------------------------------------------------------------------------.
- * |      |      |ACCL0 |      |ACCL1 |      |      |WHL_L |  Up  |WHL_R | BTN2 |      |
+ * |      |      |ACCL0 |      |      |      |      |WHL_L |  Up  |WHL_R | BTN2 |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |ACCL2 | BTN2 |      | BTN1 |      |WHL_Up| Left | Down |Right | BTN4 | BTN5 |
+ * |      |ACCL2 | BTN2 |      | BTN1 |ACCL1 |WHL_Up| Left | Down |Right | BTN4 | BTN5 |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      | BTN3 |      |WHL_Dn| BTN1 |      |      | BTN3 |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -197,8 +197,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_MOUSECURSOR] = {
-  {_______, _______, KC_ACL0, _______, KC_ACL1, _______, _______, KC_WH_L, KC_MS_U, KC_WH_R, KC_BTN2, _______},
-  {_______, KC_ACL2, KC_BTN2, _______, KC_BTN1, _______, KC_WH_U, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN4, KC_BTN5},
+  {_______, _______, KC_ACL0, _______, _______, _______, _______, KC_WH_L, KC_MS_U, KC_WH_R, KC_BTN2, _______},
+  {_______, KC_ACL2, KC_BTN2, _______, KC_BTN1, KC_ACL1, KC_WH_U, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN4, KC_BTN5},
   {_______, _______, _______, _______, KC_BTN3, _______, KC_WH_D, KC_BTN1, _______, _______, KC_BTN3, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
