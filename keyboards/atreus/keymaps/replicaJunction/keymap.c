6a24bac9c40ea22e92421f7f9bb7c7af6f562fca
Joshua T <joshua@sonofone.net>
2016-08-09 22:16:21 -0500

    Moved Shift keys to Z and ?
    Instead of using a thumb shift, I was given the idea of using the pinky keys as dual-role keys that also send the Shift keypress.
    
    This gives me a Shift key on each hand again, but it will make me learn to Shift with opposite hands after all.

--- keyboards/atreus/keymaps/replicaJunction/keymap.c
+++ keyboards/atreus/keymaps/replicaJunction/keymap.c
@@ -46,17 +46,17 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_CO] = KEYMAP(
-  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                          KC_J,            KC_L,    KC_U,    KC_Y,    KC_SCLN,
-  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                          KC_M,            KC_N,    KC_E,    KC_I,    KC_O,
-  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                          KC_K,            KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-  KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, SFT_T(KC_BSPC), CTL_T(KC_DEL), ALT_T(KC_ENT),  LT(_NU, KC_SPC), MO(_EX), KC_MINS, KC_QUOT, KC_EQL
+  KC_Q,        KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,            KC_L,    KC_U,    KC_Y,    KC_SCLN,
+  KC_A,        KC_R,    KC_S,    KC_T,    KC_G,                                   KC_M,            KC_N,    KC_E,    KC_I,    KC_O,
+  SFT_T(KC_Z), KC_X,    KC_C,    KC_D,    KC_V,                                   KC_K,            KC_H,    KC_COMM, KC_DOT,  SFT_T(KC_SLSH),
+  KC_ESC,      KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, CTL_T(KC_DEL), ALT_T(KC_ENT),  LT(_NU, KC_SPC), MO(_EX), KC_MINS, KC_QUOT, KC_EQL
 ),
 
 [_QW] = KEYMAP( /* Qwerty */
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,            KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,            KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,            KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-  KC_ESC,  KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, CTL_T(KC_BSPC), ALT_T(KC_ENT),  LT(_NU, KC_SPC), MO(_NU), TG(_GA), KC_QUOT, KC_EQL
+  KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,            KC_U,    KC_I,    KC_O,    KC_P,
+  KC_A,        KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,            KC_J,    KC_K,    KC_L,    KC_SCLN,
+  SFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,            KC_M,    KC_COMM, KC_DOT,  SFT_T(KC_SLSH),
+  KC_ESC,      KC_TAB,  KC_LGUI, KC_LSFT, KC_BSPC, CTL_T(KC_BSPC), ALT_T(KC_ENT),  LT(_NU, KC_SPC), MO(_NU), TG(_GA), KC_QUOT, KC_EQL
 ),
 
 [_EX] = KEYMAP( /* Extend */
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______,                   _______, KC_WH_U, KC_MS_U, KC_WH_D, _______,
   _______, _______, _______, _______, _______,                   _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,
   _______, _______, _______, _______, _______,                   KC_BTN3, _______, KC_MS_D, _______, _______,
-  _______, _______, _______, _______, KC_SPC,  KC_BSPC, KC_BTN2, KC_BTN1, _______, _______, _______, _______
+  _______, _______, _______, KC_LSFT, KC_SPC,  KC_BSPC, KC_BTN2, KC_BTN1, _______, _______, _______, _______
 )};
 
 const uint16_t PROGMEM fn_actions[] = {
