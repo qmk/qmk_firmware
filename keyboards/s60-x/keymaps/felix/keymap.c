38766dbc82cd0765cd88a47bb88b57d58b4b6723
Felix Uhl <felix.uhl@outlook.de>
2016-08-11 09:36:37 +0200

    Minor changes to keymaps Clarified extra keys Made ISO-keys non-dead in default layout Added non-US backslash on felix layout

--- keyboards/s60-x/keymaps/felix/keymap.c
+++ keyboards/s60-x/keymaps/felix/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |-----------------------------------------------------------|
 	* |Ctrl  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
 	* |-----------------------------------------------------------|
-	* |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| Up  |Fn1 |
+	* |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| Up  | <> |
 	* |-----------------------------------------------------------|
 	* |Fn2 |Gui |Alt |        SpaceFn        |Alt |Left|Down|Right|
 	* `-----------------------------------------------------------'
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL, \
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         \
 		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,          \
-		KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_UP,   KC_FN1, \
+		KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_UP,  KC_NUBS, \
 		KC_FN2,  KC_LGUI, KC_LALT,                   KC_FN0,                             KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT),
 
 	/* Layout 1: Gaming Layer, SpaceFn disabled
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |-----------------------------------------------------------|
 	* |      |Vl-|Mut|Vl+|   |   |Hom|Lef|Dow|Rig|End|   |PEnt    |
 	* |-----------------------------------------------------------|
-	* |        |Prt|   |   |   |Cal|   |   |   |   |Pau |PUp |Rsft|
+	* |        |Prt|   |   |   |Cal|   |   |   |   |Pau |PUp |Fn1 |
 	* |-----------------------------------------------------------|
 	* |    |    |    |                        |    |Home|PDn |End |
 	* `-----------------------------------------------------------'
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DEBUG,   RESET,   \
 		KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_TRNS, KC_TRNS, KC_PGUP, KC_UP,   KC_PGDN, KC_TRNS, KC_SLCK, KC_PAUS, KC_INS,           \
 		KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_TRNS, KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_TRNS, KC_TRNS, KC_PENT,          \
-		KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_CALC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PAUS, KC_PGUP, KC_RSFT, \
+		KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_CALC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PAUS, KC_PGUP, KC_FN1,  \
 		KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS,                         KC_TRNS, KC_HOME, KC_PGDN, KC_END),
 };
 
