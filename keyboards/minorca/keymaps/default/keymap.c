1a3559e07a8328365440ae842b13e43a18616d59
messiach <sholtis.26@gmail.com>
2016-08-17 11:16:50 -0400

    fixed F11 in keymap.

--- keyboards/minorca/keymaps/default/keymap.c
+++ keyboards/minorca/keymaps/default/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FN1] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSLS           },
   {_______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_GRV,  KC_PLUS,   KC_MINS, S(KC_SCLN), KC_SCLN, XXXXXXX, S(KC_QUOT) },
-  {_______, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11, F12, _______, KC_SCLN                 },
+  {_______, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______, KC_SCLN                 },
   {_______, XXXXXXX, _______, _______, KC_VOLU,  XXXXXXX, XXXXXXX, KC_VOLD, XXXXXXX, _______, KC_MNXT, KC_MPLY        }
 },
 
