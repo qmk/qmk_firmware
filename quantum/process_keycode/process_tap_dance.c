0d28787c5cf2173d12f57b397515f91cffaa820a
Gergely Nagy <algernon@madhouse-project.org>
2016-08-18 11:29:53 +0200

    Add a register/unregister_code16 pair of functions
    These functions register not only the 8bit keycode, but the modifiers
    too. It doesn't handle the full range of the upper 8bits, just the mods,
    but that's a good start.
    
    Changed the tap-dance pair functions to use these, so one can do:
    
      `ACTION_TAP_DANCE_DOUBLE (KC_COLN, KC_SCLN)`
    
    ...and that will do the right thing.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- quantum/process_keycode/process_tap_dance.c
+++ quantum/process_keycode/process_tap_dance.c
@@ -8,9 +8,9 @@ void qk_tap_dance_pair_finished (qk_tap_dance_state_t *state, void *user_data) {
   qk_tap_dance_pair_t *pair = (qk_tap_dance_pair_t *)user_data;
 
   if (state->count == 1) {
-    register_code (pair->kc1);
+    register_code16 (pair->kc1);
   } else if (state->count == 2) {
-    register_code (pair->kc2);
+    register_code16 (pair->kc2);
   }
 }
 
@@ -18,9 +18,9 @@ void qk_tap_dance_pair_reset (qk_tap_dance_state_t *state, void *user_data) {
   qk_tap_dance_pair_t *pair = (qk_tap_dance_pair_t *)user_data;
 
   if (state->count == 1) {
-    unregister_code (pair->kc1);
+    unregister_code16 (pair->kc1);
   } else if (state->count == 2) {
-    unregister_code (pair->kc2);
+    unregister_code16 (pair->kc2);
   }
 }
 
