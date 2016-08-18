f5a10ad041e7c2ddb7a88c621f9e36db29ed248c
Xyverz <xyverz@gmail.com>
2016-08-18 00:05:24 -0700

    Returned RAISE and LOWER buttons to their original functions - too many inopportune ENTER inputs. :-(

--- keyboards/planck/keymaps/xyverz/keymap.c
+++ keyboards/planck/keymaps/xyverz/keymap.c
@@ -26,7 +26,7 @@ enum planck_keycodes {
   COLEMAK,
   DVORAK,
   LOWER,
-  RAISE = LT(_RAISE, KC_ENT),
+  RAISE,
   BACKLIT
 };
 
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH},
   {KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS},
-  {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT},
+  {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    SFT_T(KC_ENT)},
   {KC_LCTL, KC_LALT, KC_LEFT, KC_RGHT, LOWER,   KC_BSPC, KC_SPC,  RAISE,   KC_UP,   KC_DOWN, KC_RGUI, KC_ENT }
 },
 
@@ -211,7 +211,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
-      //return false;
+      return false;
       break;
     case BACKLIT:
       if (record->event.pressed) {
