f7042556806ed89ace7b7efa4875033531fc1751
Xyverz <xyverz@gmail.com>
2016-08-19 22:45:20 -0700

    Changed the Ergodox keymap to use the KEYMAP() macro, updated readme.md.

--- keyboards/ergodox/keymaps/xyverz/keymap.c
+++ keyboards/ergodox/keymaps/xyverz/keymap.c
@@ -1,8 +1,4 @@
 /*
- * PLEASE NOTE: This keymap has been updated to work with the Infinity ErgoDox. To build for the Infinity,
- * simply append `SUBPROJECT=infinity` to the end of your `make` statement. This keymap file is untested
- * on the Infinity ErgoDox.
- * 
  * About this keymap:
  * 
  * The Dvorak layout shown herestems from my early Kinesis years, using the Contour PS/2 with a Dvorak
@@ -14,9 +10,6 @@
  *
  * I've enabled persistent keymaps for Qwerty, Dvorak and Colemak layers, similar to the default Planck
  * layouts.
- * 
- * For a better explanation of the layout matrices shown below, take a look at ../../ez/ez.h where the
- * keymap is defined, beginning on line 81.
  *
  */
 
@@ -197,7 +190,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
         _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
         _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, DVORAK,  QWERTY,  COLEMAK, _______, _______,
         _______, _______, _______, _______, _______,
                                             _______, _______,
                                                      _______,
@@ -224,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      | |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      | |      |      | KP 1 | KP 2 | KP 3 |KP Ent|        |
  * `--------+------+------+------+------+-------------' `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                             | KP 0 |      |      |KP Ent|      |
+ *   |      |      |      |      |      |                             | KP 0 |      | KP . |KP Ent|      |
  *   `----------------------------------'                             `----------------------------------'
  *                                      ,-------------. ,-------------.
  *                                      |      |      | |      |      |
@@ -249,7 +242,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
                  _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
         _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______,
-                          _______, _______, _______, _______, _______,
+                          KC_P0,   _______, KC_PDOT, KC_PENT, _______,
         _______, _______,
         _______,
         _______, _______, _______
