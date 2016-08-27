2f568a53d005fe79138138edbbf0eedb96a02541
Fred Sundvik <fsundvik@gmail.com>
2016-08-27 13:43:46 +0300

    Convert frame_router_tests to GTest

--- quantum/serial_link/tests/rules.mk
+++ quantum/serial_link/tests/rules.mk
@@ -1,3 +1,9 @@
 serial_link_byte_stuffer_SRC :=\
 	$(SERIAL_PATH)/tests/byte_stuffer_tests.cpp \
 	$(SERIAL_PATH)/protocol/byte_stuffer.c
+
+serial_link_frame_router_SRC := \
+	$(SERIAL_PATH)/tests/frame_router_tests.cpp \
+	$(SERIAL_PATH)/protocol/byte_stuffer.c \
+    $(SERIAL_PATH)/protocol/frame_validator.c \
+	$(SERIAL_PATH)/protocol/frame_router.c
