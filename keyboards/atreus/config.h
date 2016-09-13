3668b295200c53e7131551bc906264ecc54e6b04
Austin Erlandson <austin.erlandson@metova.com>
2016-09-13 11:16:53 -0500

    Shorten TAPPING_TERM for better responsiveness of overloaded keys...

--- keyboards/atreus/config.h
+++ keyboards/atreus/config.h
@@ -49,6 +49,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define UNUSED_PINS
 #endif
 
+/* Make Overloaded Keys switch faster */
+#define TAPPING_TERM 175
+
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
