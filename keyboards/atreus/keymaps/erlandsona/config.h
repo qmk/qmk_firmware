fd05b5dfd468188bf032969804e5060882512954
Austin Erlandson <austin.erlandson@metova.com>
2016-09-15 11:19:54 -0500

    Revert because the IGNORE_MOD_TAP_INTERRUPT flag didn't feel right.

--- keyboards/atreus/keymaps/erlandsona/config.h
+++ keyboards/atreus/keymaps/erlandsona/config.h
@@ -22,15 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* Make Overloaded Keys switch faster */
-#define TAPPING_TERM 175
-
-/* this makes it possible to do rolling combos (zx)
- * with keys that convert to other keys on hold
- * (z becomes ctrl when you hold it,
- * and when this option isn't enabled,
- * z rapidly followed by x actually sends Ctrl-x. That's bad.)
- */
-#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 150
 
 /* USB Device descriptor parameter */
 
