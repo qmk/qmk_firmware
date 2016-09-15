29084d0de2a217feefa6dab348325a13fd1fb1b6
Austin Erlandson <austin.erlandson@metova.com>
2016-09-15 10:58:28 -0500

    Add IGNORE_MOD_TAP_INTERRUPT to fix overloaded mod keys sending mod on diads...

--- keyboards/atreus/keymaps/erlandsona/config.h
+++ keyboards/atreus/keymaps/erlandsona/config.h
@@ -21,13 +21,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 
-
-
 /* Make Overloaded Keys switch faster */
 #define TAPPING_TERM 175
 
-
-
+/* this makes it possible to do rolling combos (zx)
+ * with keys that convert to other keys on hold
+ * (z becomes ctrl when you hold it,
+ * and when this option isn't enabled,
+ * z rapidly followed by x actually sends Ctrl-x. That's bad.)
+ */
+#define IGNORE_MOD_TAP_INTERRUPT
 
 /* USB Device descriptor parameter */
 
--- keyboards/atreus/keymaps/erlandsona/keymap.c
+++ keyboards/atreus/keymaps/erlandsona/keymap.c
@@ -18,16 +18,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP( /* Qwerty */
-  KC_Q       , KC_W   , KC_E   , KC_R   ,  KC_T   ,                               KC_Y  ,  KC_U   , KC_I   , KC_O   , KC_P          ,
-  KC_A       , KC_S   , KC_D   , KC_F   ,  KC_G   ,                               KC_H  ,  KC_J   , KC_K   , KC_L   , KC_SCLN       ,
-  SFT_T(KC_Z), KC_X   , KC_C   , KC_V   ,  KC_B   ,                               KC_N  ,  KC_M   , KC_COMM, KC_DOT , SFT_T(KC_QUOT),
-  KC_LCTL    , KC_LALT, KC_LALT, KC_LGUI,  KC_BSPC, ALT_T(KC_ESC), CTL_T(KC_ENT), KC_SPC,  F(NUMS), KC_RALT, KC_SLSH, KC_BSLS      ),
+  KC_Q       , KC_W   , KC_E   , KC_R   ,  KC_T   ,                 KC_Y  ,  KC_U   , KC_I   , KC_O   , KC_P          ,
+  ALT_T(KC_A), KC_S   , KC_D   , KC_F   ,  KC_G   ,                 KC_H  ,  KC_J   , KC_K   , KC_L   , KC_SCLN       ,
+  SFT_T(KC_Z), KC_X   , KC_C   , KC_V   ,  KC_B   ,                 KC_N  ,  KC_M   , KC_COMM, KC_DOT , SFT_T(KC_QUOT),
+  KC_LCTL    , KC_LALT, KC_LALT, KC_LGUI,  KC_BSPC, KC_ESC, KC_ENT, KC_SPC,  F(NUMS), KC_RALT, KC_SLSH, KC_BSLS      ),
 
 [NUMS] = KEYMAP( /* Numbers / Arrows / Symbols */
-  KC_GRV , KC_7   , KC_8   ,  KC_9   , KC_0  ,                   KC_LPRN, KC_RPRN, KC_MINS, KC_EQL , KC_LBRC,
-  KC_TAB , KC_4   , KC_5   ,  KC_6   , KC_DLR,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_RBRC,
-  _______, KC_1   , KC_2   ,  KC_3   , KC_DOT,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
-  _______, _______, _______,  _______, KC_DEL, F(MOUS), _______, _______, _______, _______, _______, _______),
+  KC_GRV       , KC_1   , KC_2   , KC_3   , KC_4   ,                   KC_LPRN, KC_RPRN, KC_MINS, KC_EQL , KC_LBRC,
+  ALT_T(KC_TAB), KC_5   , KC_6   , KC_7   , KC_8   ,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_RBRC,
+  _______      , KC_9   , KC_0   , KC_DOT , KC_COMM,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
+  _______      , _______, _______, _______, KC_DEL , F(MOUS), _______, _______, _______, _______, _______, _______),
 
 [MOUS] = KEYMAP( /* Mouse and Media Keys */
   KC_SLCK, KC_PAUSE, KC_F11 , KC_F10 , KC_F9  ,                   KC_F8  , KC_F7  , KC_F6  , KC_F5  , KC_F4,
