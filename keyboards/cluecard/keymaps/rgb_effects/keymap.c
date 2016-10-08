2d7b549e2172b6ffadcaf852055864e6213cfa72
Fred Sundvik <fsundvik@gmail.com>
2016-10-09 01:38:55 +0300

    Add keymap with audio disabled but RGB effects enabled

---
+++ keyboards/cluecard/keymaps/rgb_effects/keymap.c
@@ -0,0 +1,25 @@
+#include "cluecard.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = KEYMAP(
+		RGB_TOG,        RGB_SAI,        RGB_VAI, \
+		        RGB_HUD,        RGB_HUI,         \
+		RGB_MOD,        RGB_SAD,        RGB_VAD, \
+		BL_STEP,                                 \
+		KC_NO,   KC_NO,   KC_NO                  \
+	)
+};
+
+void matrix_init_user(void) {
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+}
