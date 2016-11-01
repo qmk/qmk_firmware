ced96f27dea84090e0b13c618ba77394ae535435
xyverz <xyverz@gmail.com>
2016-10-31 18:27:05 -0700

    Updates to keymap file.

--- keyboards/gh60/keymaps/xyverz/keymap.c
+++ keyboards/gh60/keymaps/xyverz/keymap.c
@@ -6,9 +6,6 @@
 extern keymap_config_t keymap_config;
 
 
-// Used for SHIFT_ESC
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -29,6 +26,20 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+    /*
+     *  _QW: Qwerty Layer
+     * ,-----------------------------------------------------------.
+     * |ESC | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| Backsp|
+     * |-----------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
+     * |-----------------------------------------------------------|
+     * |Fn     |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '| Return|
+     * |-----------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|     Shift|
+     * |-----------------------------------------------------------|
+     * |Ctrl|Alt |Gui |      Space             |Gui |Alt |Ctrl|  Fn|
+     * `-----------------------------------------------------------'
+     */
 [_QW] = { /* Layer 0: Qwerty */
     {KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC},
     {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS},
@@ -37,6 +48,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGUI, KC_RALT, KC_RCTL, MO(_FL)}
   },
 
+    /*
+     *  _DV: Dvorak Layer
+     * ,-----------------------------------------------------------.
+     * |ESC | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  [|  ]| Backsp|
+     * |-----------------------------------------------------------|
+     * |Tab  |  '|  ,|  .|  P|  Y|  F|  G|  C|  R|  L|  /|  =|    \|
+     * |-----------------------------------------------------------|
+     * |Fn     |  A|  O|  E|  U|  I|  D|  H|  T|  N|  S|  -| Return|
+     * |-----------------------------------------------------------|
+     * |Shift   |  ;|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|     Shift|
+     * |-----------------------------------------------------------|
+     * |Ctrl|Alt |Gui |      Space             |Gui |Alt |Ctrl|  Fn|
+     * `-----------------------------------------------------------'
+     */
 [_DV] = { /* Layer 1: Dvorak */
     {KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC},
     {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS},
@@ -45,6 +70,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGUI, KC_RALT, KC_RCTL, MO(_FL)}
   },
 
+    /*
+     *  _CM: Colemak Layer
+     * ,-----------------------------------------------------------.
+     * |ESC | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =| Backsp|
+     * |-----------------------------------------------------------|
+     * |Tab  |  Q|  W|  F|  P|  G|  J|  L|  U|  Y|  ;|  [|  ]|    \|
+     * |-----------------------------------------------------------|
+     * |Fn     |  A|  R|  S|  T|  D|  H|  N|  E|  I|  O|  '| Return|
+     * |-----------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  K|  M|  ,|  .|  /|     Shift|
+     * |-----------------------------------------------------------|
+     * |Ctrl|Alt |Gui |      Space             |Gui |Alt |Ctrl|  Fn|
+     * `-----------------------------------------------------------'
+     */
 [_CM] = { /* Layer 2: Colemak */
     {KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC},
     {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS},
@@ -53,21 +92,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGUI, KC_RALT, KC_RCTL, MO(_FL)}
   },
 
+    /*
+     *  _FL: Function Layer
+     * ,-----------------------------------------------------------.
+     * |    |F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|    Del|
+     * |-----------------------------------------------------------|
+     * |     |   |   |   |   |   |   |PgU| Up|PgD|PSc|SLk|Pau|     |
+     * |-----------------------------------------------------------|
+     * |       |   |MPr|MPl|MNx|   |Hom| Lt| Dn| Rt|   |   |       |
+     * |-----------------------------------------------------------|
+     * |CAPS    |   |Mut|VlD|VlU|   |End|   |   |   |   |          |
+     * |-----------------------------------------------------------|
+     * |_QW |_DV |_CM |                        |    |    |    |    |
+     * `-----------------------------------------------------------'
+     */
 [_FL] = { /* Layer 3: Functions */
     {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL },
     {_______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, _______},
     {_______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, XXXXXXX, _______},
-    {KC_CAPS, XXXXXXX, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_END,  _______, _______, _______, _______, _______, XXXXXXX, _______},
+    {KC_CAPS, XXXXXXX, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_END,  _______, _______, _______, _______, XXXXXXX, _______},
     {QWERTY,  DVORAK,  COLEMAK, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______}
   },
 
 
 };
-
-enum function_id {
-    SHIFT_ESC,
-};
-
 
 const uint16_t PROGMEM fn_actions[] = {
 
