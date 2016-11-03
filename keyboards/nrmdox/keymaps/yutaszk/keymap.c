57620e16ac82da4c7c91791148e258d705795370
yutaszk <yutaszk23@gmail.com>
2016-11-04 02:30:23 +0900

    Initialize nrmdox yutaszk keymap

--- keyboards/nrmdox/keymaps/yutaszk/keymap.c
+++ keyboards/nrmdox/keymaps/yutaszk/keymap.c
@@ -16,13 +16,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * +---+---+---+---+---+---+            +---+---+---+---+---+---+
- * |ESC| q | w | e | r | t |            | y | u | i | o | p |B S|
+ * |TAB| q | w | e | r | t |            | y | u | i | o | p |BS |
  * +---+---+---+---+---+---+            +---+---+---+---+---+---+
  * |CTL| a | s | d | f | g |            | h | j | k | l |: ;|RET|
  * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
- * |TAB|SFT| z | x | c | v |<E>|    |<J>| b | n | m |, <|. >|/ ?|
+ * |LT1| z | x | c | v | b |ALT|    |ALT| n | m |, <|. >|/ ?|LT2|
  * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
- *             |ALT|SYM|SPC|NUM|    |NUM|SPC|SYM|GUI|
+ *             |TG3|GUI|S/S| ] |    | [ |S/S|LT1|TG1|
  *             +---+---+---+---+    +---+---+---+---+
  *
  */
@@ -30,68 +30,96 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-            KC_ESC   , KC_Q    , KC_W    , KC_E    , KC_R   , KC_T   ,
-            KC_LCTRL , KC_A    , KC_S    , KC_D    , KC_F   , KC_G   ,
-            SFT_T(KC_TAB), KC_LSFT  , KC_Z    , KC_X    , KC_C    , KC_V   , KC_LANG2 ,
-                                           KC_LALT ,MO(SYMB), KC_SPC , MO(NUM),
+            KC_TAB                , KC_Q      , KC_W      , KC_E      , KC_R      , KC_T                  ,
+            KC_LCTL               , KC_A      , KC_S      , KC_D      , KC_F      , KC_G                  ,
+            LT(SYMB, KC_F15)      , KC_Z      , KC_X      , KC_C      , KC_V      , KC_B                  , KC_LALT   ,
+                                                            TG(SWAP)  , KC_LGUI   , SFT_T(KC_SPC)         , KC_RBRC   ,
         // right hand
-                      KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC ,
-                      KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_ENT  ,
-            KC_LANG1 , KC_B   , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH ,
-            MO(NUM), KC_SPC , MO(SYMB) , KC_LGUI
+                        KC_Y                  , KC_U                  , KC_I      , KC_O      , KC_P      , KC_BSPC   ,
+                        KC_H                  , KC_J                  , KC_K      , KC_L      , KC_SCLN   , KC_ENT    ,
+            KC_RALT   , KC_N                  , KC_M                  , KC_COMM   , KC_DOT    , KC_SLSH   , LT(FUNC, KC_F16)      ,
+            KC_LBRC   , SFT_T(KC_SPC)         , LT(SYMB, KC_ENT)      , TG(SYMB)
     ),
 
-/* Keymap 1: Symbol layer
+
+/* Keymap 1: Cursor layer
  *
  * +---+---+---+---+---+---+            +---+---+---+---+---+---+
- * |   | ! | @ | # | $ | % |            | ^ | & | * | ( | ) |DEL|
+ * | 1 | 2 | 3 | 4 | 5 | 6 |            | 7 | 8 | 9 | 0 | - | = |
  * +---+---+---+---+---+---+            +---+---+---+---+---+---+
- * |   | _ | \ | | | ' | " |            |A_L|A_D|A_U|A_R|INS|   |
+ * |ESC|INS|HOM|PgU|PgD|END|            |A_L|A_D|A_U|A_R| ` |' "|
  * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
- * |   |   | + | - | = | ` |   |    |   |CAP| { | } | [ | ] |4SP|
+ * |LT1|   |   |   |   |   |   |    |   |   |   |   |   | \ |LT2|
  * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
- *             |ALT|   |SPC|NUM|    |NUM|SPC|   |GUI|
+ *             |TG3|   |   |   |    |   |   |LT1|TG0|
  *             +---+---+---+---+    +---+---+---+---+
  *
  */
 [SYMB] = KEYMAP(  // layer 1 : symbol
         // left hand
-            KC_TRNS  , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC       ,
-            KC_TRNS , KC_UNDS , KC_BSLS , KC_PIPE , KC_QUOT , LSFT(KC_QUOT) ,
-            KC_TRNS  , KC_TRNS , KC_PLUS , KC_MINS , KC_EQL  , KC_GRV        , KC_TRNS ,
-                                           KC_LALT , KC_TRNS,  KC_SPC ,KC_TRNS,
+            KC_1      , KC_2      , KC_3      , KC_4      , KC_5      , KC_6      ,
+            KC_ESC    , KC_INS    , KC_HOME   , KC_PGUP   , KC_PGDN   , KC_END    ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+                                                KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
         // right hand
-                      KC_CIRC , KC_AMPR , KC_ASTR      , KC_LPRN  , KC_RPRN , KC_DEL    ,
-                      KC_LEFT , KC_DOWN , KC_UP        , KC_RIGHT , KC_INS  , KC_TRNS   ,
-            KC_TRNS , KC_CAPS , KC_LCBR , KC_RCBR      , KC_LBRC  , KC_RBRC , M(M_4SPC) ,
-             KC_TRNS ,KC_SPC  , KC_TRNS  , KC_LGUI
+                        KC_7      , KC_8      , KC_9      , KC_0      , KC_MINS   , KC_EQL    ,
+                        KC_LEFT   , KC_DOWN   , KC_UP     , KC_RIGHT  , KC_GRV    , KC_QUOT   ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
     ),
 
 
-/* Keymap 2: Number layer
+/* Keymap 2: Function layer
  *
  * +---+---+---+---+---+---+            +---+---+---+---+---+---+
- * |F 1|F 2|F 3|F 4|F 5|F 6|            |F 7|F 8|F 9|F10|F11|F12|
+ * |F1 |F2 |F3 |F4 |F5 |F6 |            |F7 |F8 |F9 |F10|F11|F12|
  * +---+---+---+---+---+---+            +---+---+---+---+---+---+
- * |   | 1 | 2 | 3 | 4 | 5 |            | 6 | 7 | 8 | 9 | 0 |RET|
+ * |   |   |   |   |   |   |            |   |   |   |   |   |   |
  * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
- * |   |   |NOP|NOP|NOP|NOP|   |    |   |HOM|PGD|PGU|END|DBG|RST|
+ * |LT1|   |   |   |   |   |   |    |RST|   |   |   |   |   |LT2|
  * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
- *             |ALT|SYM|SPC|   |    |   |SPC|SYM|GUI|
+ *             |TG3|   |   |   |    |   |   |LT1|TG1|
  *             +---+---+---+---+    +---+---+---+---+
  *
  */
-[NUM] = KEYMAP(  // layer 2 : number
+[FUNC] = KEYMAP(  // layer 2 : function
         // left hand
-            KC_F1    , KC_F2  , KC_F3  , KC_F4   , KC_F5  , KC_F6  ,
-            KC_TRNS , KC_1   , KC_2   , KC_3    , KC_4   , KC_5   ,
-            KC_TRNS  , KC_TRNS  , KC_NO  , KC_NO   , KC_NO  , KC_NO  , KC_TRNS ,
-                                         KC_LALT , KC_TRNS, KC_SPC , KC_TRNS ,
+            KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     , KC_F6     ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+                                                KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
         // right hand
-                       KC_F7   , KC_F8   , KC_F9        , KC_F10 , KC_F11     , KC_F12 ,
-                       KC_6    , KC_7    , KC_8         , KC_9   , KC_0       , KC_ENT ,
-            KC_TRNS , KC_HOME , KC_PGDN , KC_PGUP      , KC_END , M(M_DEBUG)  , RESET  ,
-            KC_TRNS , KC_SPC  , KC_TRNS , KC_LGUI
+                        KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    ,
+                        KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_RESET  , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+    ),
+
+
+/* Keymap 3: Swap layer
+ *
+ * +---+---+---+---+---+---+            +---+---+---+---+---+---+
+ * |   |   |   |   |   |   |            |   |   |   |   |   |   |
+ * +---+---+---+---+---+---+            +---+---+---+---+---+---+
+ * |GUI|   |   |   |   |   |            |   |   |   |   |   |   |
+ * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
+ * |LT1|   |   |   |   |   |   |    |   |   |   |   |   |   |LT2|
+ * +---+---+---+---+---+---+---+    +---+---+---+---+---+---+---+
+ *             |TG0|CTL|   |   |    |   |   |LT1|TG1|
+ *             +---+---+---+---+    +---+---+---+---+
+ *
+ */
+[SWAP] = KEYMAP(  // layer 3 : swap
+        // left hand
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_LGUI   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+                                                KC_TRNS   , KC_LCTL   , KC_TRNS   , KC_TRNS   ,
+        // right hand
+                        KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+                        KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
+            KC_TRNS   , KC_TRNS   , KC_TRNS   , KC_TRNS   ,
     ),
 
 };
