748432b480c5939af3b952243072049464861011
Daniel <danielallendeutsch@gmail.com>
2016-11-18 11:11:08 -0600

    moves brackets to top row.

--- keyboards/ergodox/keymaps/common-nighthawk/keymap.c
+++ keyboards/ergodox/keymaps/common-nighthawk/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   x  |   x  |   x  |   _  | Hypr | Home |           | End  | Hypr |  Esc |   x  |   x  |   x  |   `~   |
+ * | Esc    |   x  |   x  |   x  |  Esc | Hypr | Home |           | End  |  Esc |   [  |   ]  |   x  |  `~  |   `~   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |Tab/Ctrl|   Q  |   W  |   E  |   R  |   T  |  Up  |           | PgUp |Y 2Esc| U 2[ | I 2] |   O  |   P  |Bks/Ctrl|
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,          KC_1,          KC_1,          KC_1,            KC_UNDS,  ALL_T(KC_NO),      KC_HOME,
+        KC_ESC,          KC_1,          KC_1,          KC_1,            KC_ESC,   ALL_T(KC_NO),      KC_HOME,
         CTL_T(KC_TAB),   KC_Q,          KC_W,          KC_E,            KC_R,     KC_T,              KC_UP,
         GUI_T(KC_QUOT),  KC_A,          KC_S,          KC_D,            KC_F,     KC_G,
         M(1),            KC_Z,          KC_X,          KC_C,            KC_V,     KC_B,              KC_DOWN,
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                         KC_VOLD,
                                                                         LT(SYMB, KC_SPC),  TG(MDIA),  RESET,
         // right hand
-        KC_END,          ALL_T(KC_NO),  KC_ESC,         KC_1,           KC_1,     KC_1,              KC_GRV,
+        KC_END,          KC_ESC,        KC_LBRC,        KC_RBRC,        KC_1,     KC_GRV,            KC_GRV,
         KC_PGUP,         TD(TD_Y_ESC),  TD(TD_U_LBRC),  TD(TD_I_RBRC),  KC_O,     KC_P,              CTL_T(KC_BSPC),
                          KC_H,          KC_J,           KC_K,           KC_L,     KC_SCOLON,         GUI_T(KC_ENT),
         KC_PGDN,         KC_N,          KC_M,           KC_COMM,        KC_DOT,   KC_SLSH,           M(1),
@@ -71,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   ^  |   {  |   (  |   [  |      |           |      |   =  |   7  |   8  |   9  |   0  |        |
+ * |        |   !  |   ^  |   [  |   (  |   {  |      |           |      |   =  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   &  |   $  |   }  |   )  |   ]  |------|           |------|   +  |   4  |   5  |   6  |   *  |        |
+ * |        |   &  |   $  |   ]  |   )  |   }  |------|           |------|   +  |   4  |   5  |   6  |   *  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   \  |   #  |   @  |   %  |   |  |      |           |      |   -  |   1  |   2  |   3  |   /  |        |
+ * |        |   \  |   @  |   #  |   %  |   |  |      |           |      |   -  |   1  |   2  |   3  |   /  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |   0  |   0  |   .  |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -91,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,
-       KC_TRNS,  KC_EXLM,  KC_CIRC,  KC_LCBR,  KC_LPRN,  KC_LBRC,  KC_TRNS,
-       KC_TRNS,  KC_AMPR,  KC_DLR,   KC_RCBR,  KC_RPRN,  KC_RBRC,
-       KC_TRNS,  KC_BSLS,  KC_HASH,  KC_AT,    KC_PERC,  KC_PIPE,  KC_TRNS,
+       KC_TRNS,  KC_EXLM,  KC_CIRC,  KC_LBRC,  KC_LPRN,  KC_LCBR,  KC_TRNS,
+       KC_TRNS,  KC_AMPR,  KC_DLR,   KC_RBRC,  KC_RPRN,  KC_RCBR,
+       KC_TRNS,  KC_BSLS,  KC_AT,    KC_HASH,  KC_PERC,  KC_PIPE,  KC_TRNS,
        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                KC_TRNS,  KC_TRNS,
                                                KC_TRNS,
