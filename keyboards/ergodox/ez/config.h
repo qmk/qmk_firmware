06a2677b7eedbf58532fa1a673ba1277e756174d
Jack Humbert <jack.humb@gmail.com>
2016-11-21 20:17:32 -0500

    fix infinity

--- keyboards/ergodox/ez/config.h
+++ keyboards/ergodox/ez/config.h
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../config.h"
 
+#include "config_common.h"
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x1307
