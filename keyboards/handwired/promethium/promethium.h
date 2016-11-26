124b494d031b9887ac2816d5199440a1d5842d05
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2016-11-26 13:44:50 +0700

    change init delay to 2000ms

--- keyboards/handwired/promethium/promethium.h
+++ keyboards/handwired/promethium/promethium.h
@@ -3,6 +3,8 @@
 
 #include "quantum.h"
 
+#define PS2_INIT_DELAY 2000
+
 #define KEYMAP( \
     k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, \
     k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, \
