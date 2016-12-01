c53cb606db36703263bd7a06b0d0e7b218dc0695
Erez Zukerman <ezukerman@godaddy.com>
2016-11-30 22:22:57 -0500

    Better layout

--- keyboards/ergodox/keymaps/robot_test_layout/keymap.c
+++ keyboards/ergodox/keymaps/robot_test_layout/keymap.c
@@ -9,15 +9,41 @@ enum custom_keycodes {
   RGB_00FF00,
   RGB_0000FF,
   RGB_FFFFFF,
-  RGB_TOGGLE
+  RGB_TOGGLE,
+  LED1,
+  LED2,
+  LED3
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = KEYMAP(
-    RGB_TOGGLE,RGB_FF0000,RGB_00FF00,RGB_0000FF,RGB_FFFFFF,RGB_TOGGLE,KC_6,
-    KC_A,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_E,KC_A,KC_A,KC_S,KC_D,KC_F,KC_G,KC_A,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_L,KC_Z,KC_QUOTE,KC_N,KC_U,KC_C,KC_E,KC_8,KC_9,KC_Y,KC_COMMA,KC_6,KC_7,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_J,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,KC_J,KC_K,KC_Z,KC_N,KC_M,KC_COMMA,KC_DOT,KC_E,KC_QUOTE,KC_8,KC_7,KC_LBRACKET,KC_RBRACKET,KC_H,KC_9,KC_7,KC_8,KC_7,KC_6,KC_9),
+        RGB_TOGGLE, RGB_FF0000, RGB_00FF00, RGB_0000FF, RGB_FFFFFF, KC_5, KC_LPRN,
+        KC_GRAVE,   KC_A,       KC_B,       KC_C,       KC_D,       KC_E, KC_EXLM,
+        KC_HASH,    KC_J,       KC_K,       KC_L,       KC_M,       KC_N,
+        KC_AMPR,    KC_T,       KC_U,       KC_V,       KC_W,       KC_X, KC_DLR,
+        KC_PIPE,    KC_R,       KC_PLUS,    KC_LCBR,    KC_RCBR,
+
+                                            KC_F, KC_G,
+                                                  KC_H,
+                                     KC_P,  KC_O, KC_I,
+
+
+
+                                               // RIGHT HAND
+                                               KC_RPRN,       KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,
+                                               KC_AT,         KC_F, KC_G, KC_H,    KC_I,   KC_COLN, KC_BSLS,
+                                                              KC_O, KC_P, KC_Q,    KC_R,   KC_S,    KC_QUOT,
+                                               LSFT(KC_COMM), KC_Y, KC_Z, KC_COMM, KC_DOT, KC_SLSH, KC_ASTR,
+                                                              KC_A, KC_B, KC_C,    KC_D,   KC_PIPE,
+
+                                               LED1, KC_E,
+                                               LED2,
+                                               LED3, KC_J, KC_K
+    )
 };
+
+
 
 const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_TAP_TOGGLE(1)
@@ -35,7 +61,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     return MACRO_NONE;
 };
 
-
+bool status_led1_on = false, status_led2_on = false, status_led3_on = false;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     // dynamically generate these.
@@ -43,7 +69,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         #ifdef RGBLIGHT_ENABLE
           EZ_RGB(0xff0000);
-          register_code(KC_A); unregister_code(KC_A);
+          register_code(KC_1); unregister_code(KC_1);
         #endif
       }
       return false;
@@ -52,7 +78,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         #ifdef RGBLIGHT_ENABLE
           EZ_RGB(0x00ff00);
-          register_code(KC_B); unregister_code(KC_B);
+          register_code(KC_2); unregister_code(KC_2);
         #endif
       }
       return false;
@@ -61,7 +87,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         #ifdef RGBLIGHT_ENABLE
           EZ_RGB(0x0000ff);
-          register_code(KC_C); unregister_code(KC_C);
+          register_code(KC_3); unregister_code(KC_3);
         #endif
       }
       return false;
@@ -70,7 +96,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         #ifdef RGBLIGHT_ENABLE
           EZ_RGB(0xffffff);
-          register_code(KC_D); unregister_code(KC_D);
+          register_code(KC_4); unregister_code(KC_4);
         #endif
       }
       return false;
@@ -79,52 +105,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         #ifdef RGBLIGHT_ENABLE
           rgblight_toggle();
-          register_code(KC_F); unregister_code(KC_F);
+          register_code(KC_EQL); unregister_code(KC_EQL);
         #endif
+      }
+      return false;
+      break;
+    case LED1:
+      if (record->event.pressed) {
+        if(status_led1_on) {
+        ergodox_right_led_1_off();
+        status_led1_on = false;
+        } else {
+        ergodox_right_led_1_on();
+        status_led1_on = true;
+        }
+      }
+      return false;
+      break;
+    case LED2:
+      if (record->event.pressed) {
+        if(status_led2_on) {
+        ergodox_right_led_2_off();
+        status_led2_on = false;
+        } else {
+        ergodox_right_led_2_on();
+        status_led2_on = true;
+        }
+      }
+      return false;
+      break;
+    case LED3:
+      if (record->event.pressed) {
+        if(status_led3_on) {
+        ergodox_right_led_3_off();
+        status_led3_on = false;
+        } else {
+        ergodox_right_led_3_on();
+        status_led3_on = true;
+        }
       }
       return false;
       break;
   }
   return true;
 }
-
-void matrix_scan_user(void) {
-
-    uint8_t layer = biton32(layer_state);
-
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
-    switch (layer) {
-        case 1:
-            ergodox_right_led_1_on();
-            break;
-        case 2:
-            ergodox_right_led_2_on();
-            break;
-        case 3:
-            ergodox_right_led_3_on();
-            break;
-        case 4:
-            ergodox_right_led_1_on();
-            ergodox_right_led_2_on();
-            break;
-        case 5:
-            ergodox_right_led_1_on();
-            ergodox_right_led_3_on();
-            break;
-        case 6:
-            ergodox_right_led_2_on();
-            ergodox_right_led_3_on();
-            break;
-        case 7:
-            ergodox_right_led_1_on();
-            ergodox_right_led_2_on();
-            ergodox_right_led_3_on();
-            break;
-        default:
-            break;
-    }
-
-};
