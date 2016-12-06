1f52dbcac01c37157c29038334e1f7b5be9bd4aa
Daniel <danielallendeutsch@gmail.com>
2016-12-06 14:15:23 -0600

    removes double-tab brackets and adds back quot to left cmd.

--- keyboards/ergodox/keymaps/common-nighthawk/keymap.c
+++ keyboards/ergodox/keymaps/common-nighthawk/keymap.c
@@ -26,37 +26,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  _     |   x  |   x  |   _  |  Esc | Hypr | Home |           | End  | Hypr |   [  |   ]  |   x  |  `~  |  Bks   |
+ * |  _     |   x  |   x  |   _  |  Esc | Hypr | Home |           | End  | Hypr |   [  |   ]  |  `~  |  `~  |  Bks   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |Ctrl/Tab|   Q  |   W  |   E  |   R  |   T  |  Up  |           | PgUp |   Y  | U 2[ | I 2] |   O  |   P  |  Ctrl  |
+ * |Ctrl/Tab|   Q  |   W  |   E  |   R  |   T  |  Up  |           | PgUp |   Y  |   U  |   I  |   O  |   P  |  Ctrl  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Cmd   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |Entr/Cmd|
+ * | Cmd/"' |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |Cmd/Entr|
  * |--------+------+------+------+------+------| Down |           | PgDn |------+------+------+------+------+--------|
  * |  Shft  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |  Shft  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Alt  |   x  |   x  |   x  | Left |                                       |Right |  Bks |   x  |   x  |  Alt |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Mute | VolUp|       | Play |  Del   |
+ *                                        |Teensy| VolUp|       | Play |  Del   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | VolDn|       | Next |        |      |
  *                                 |Sp/~L1|  L2  |------|       |------|   L2   |'"/~L1|
- *                                 |      |      |Teensy|       | Prev |        |      |
+ *                                 |      |      | Mute |       | Prev |        |      |
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_UNDS,         KC_1,          KC_1,          KC_UNDS,         KC_ESC,   ALL_T(KC_NO),      KC_HOME,
         CTL_T(KC_TAB),   KC_Q,          KC_W,          KC_E,            KC_R,     KC_T,              KC_UP,
-        KC_LGUI,         KC_A,          KC_S,          KC_D,            KC_F,     KC_G,
+        GUI_T(KC_QUOT),  KC_A,          KC_S,          KC_D,            KC_F,     KC_G,
         KC_LSFT,         KC_Z,          KC_X,          KC_C,            KC_V,     KC_B,              KC_DOWN,
         KC_LALT,         KC_1,          KC_1,          KC_1,            KC_LEFT,
-                                                                        KC_MUTE,  KC_VOLU,
+                                                                        RESET,    KC_VOLU,
                                                                         KC_VOLD,
-                                                                        LT(SYMB, KC_SPC),  TG(MDIA),  RESET,
+                                                                        LT(SYMB, KC_SPC),  TG(MDIA), KC_MUTE,
         // right hand
-        KC_END,          ALL_T(KC_NO),  KC_LBRC,        KC_RBRC,        KC_1,     KC_GRV,            KC_BSPC,
-        KC_PGUP,         KC_Y,          TD(TD_U_LBRC),  TD(TD_I_RBRC),  KC_O,     KC_P,              KC_LCTRL,
+        KC_END,          ALL_T(KC_NO),  KC_LBRC,        KC_RBRC,        KC_GRV,   KC_GRV,            KC_BSPC,
+        KC_PGUP,         KC_Y,          KC_U,           KC_I,           KC_O,     KC_P,              KC_LCTRL,
                          KC_H,          KC_J,           KC_K,           KC_L,     KC_SCOLON,         GUI_T(KC_ENT),
         KC_PGDN,         KC_N,          KC_M,           KC_COMM,        KC_DOT,   KC_SLSH,           KC_RSFT,
                                         KC_RGHT,        KC_BSPC,        KC_1,     KC_1,              KC_RALT,
