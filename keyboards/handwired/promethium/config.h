0b54b32361ca0bb32086f0a8f74a9beedd3b6fa1
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2016-12-10 19:39:27 +0700

    Enable Adafruit BLE support

--- keyboards/handwired/promethium/config.h
+++ keyboards/handwired/promethium/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Priyadi
 #define PRODUCT         Promethium Keyboard
-#define DESCRIPTION     Promethium Keyboard
+#define DESCRIPTION
 
 /* key matrix size */
 #define MATRIX_ROWS 8
--- keyboards/handwired/promethium/rules.mk
+++ keyboards/handwired/promethium/rules.mk
@@ -1,4 +1,3 @@
-
 # MCU name
 #MCU = at90usb1287
 MCU = atmega32u4
@@ -66,6 +65,7 @@ BLUETOOTH_ENABLE ?= no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE ?= no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 PS2_MOUSE_ENABLE ?= yes
 PS2_USE_INT ?= yes
+ADAFRUIT_BLE_ENABLE ?= yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE ?= no    # Breathing sleep LED during USB suspend
