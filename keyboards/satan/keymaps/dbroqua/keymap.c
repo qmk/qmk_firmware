61d4137e3f84d46d399ff09ddced49f141538dda
dbroqua <dbroqua@mousur.org>
2016-12-24 14:53:15 +0100

    - Updated dbroqua layout for Satan keyboard

--- keyboards/satan/keymaps/dbroqua/keymap.c
+++ keyboards/satan/keymaps/dbroqua/keymap.c
@@ -28,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *         `-----------------------------------------------------------------'
  */
   [_DEFAULT] = KEYMAP_HHKB( /* Basic QWERTY */
-      KC_ESC,   KC_1,       KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,       KC_MINS,    KC_EQL, KC_BSLS,    KC_GRV, \
-      KC_TAB,   KC_Q,       KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,       KC_LBRC,    KC_RBRC,KC_BSPC,  \
-      KC_LCTL,  KC_A,       KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,   \
-      KC_LSFT,  KC_Z,       KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,    KC_RSFT,    MO(_FN), \
-      ______,   KC_LGUI,    KC_LALT,                KC_SPC,                         KC_RALT,KC_RGUI,    ______,     ______ \
+      KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,   KC_0,       KC_MINS,    KC_EQL, KC_BSLS,    KC_GRV, \
+      KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,       KC_LBRC,    KC_RBRC,KC_BSPC,  \
+      KC_LCTL,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,   \
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,    KC_RSFT,    MO(_FN), \
+      ______,   KC_LGUI,KC_LALT,                KC_SPC,                          KC_RALT,KC_RGUI,    ______,     ______ \
       ),
 
 /* FN Layer
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *         `-----------------------------------------------------------------'
  */
   [_FN] = KEYMAP_HHKB( /* Layer 1 */
-      ______,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS,  KC_DEL, \
+      TG(_SFX), KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS,  KC_DEL, \
       KC_CAPS,  ______, ______, ______, ______, ______, ______, ______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   ______, ______,  \
       ______,   KC_VOLD,KC_VOLU,KC_MUTE,______, ______, KC_PAST,KC_PSLS,KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, ______,   \
       ______,   KC_MPRV,KC_MPLY,KC_MNXT,______, ______, KC_PPLS,KC_PMNS,KC_END,  KC_PGDN, KC_DOWN, ______,  ______,  \
-      ______,   ______, ______,                 TG(_SFX),                 KC_MSTP, ______,  ______,  ______ \
+      ______,   ______, ______,                 ______,                 KC_MSTP, ______,  ______,  ______ \
       ),
 
 /* SFX Layer
@@ -72,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SFX] = KEYMAP_HHKB(
       ______,   ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, \
       ______,   BL_DEC, BL_INC, BL_TOGG,______, ______, ______, ______, ______, ______, ______, ______, ______, ______,  \
-      ______,   F(2),   F(3),   ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,   \
-      ______,   F(4),   F(5),   F(6),   F(7),   F(8),   F(9),   ______, ______, ______, ______, ______, ______,  \
+      ______,   F(0),   F(1),   ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,   \
+      ______,   F(2),   F(3),   F(4),   F(5),   F(6),   F(7),   ______, ______, ______, ______, ______, ______,  \
       ______,   ______, ______,                 ______,                 ______, ______, ______, ______ \
       )
 };
@@ -96,16 +96,14 @@ enum function_id {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
-  [1] = ACTION_LAYER_TOGGLE(2),     // toggle wasd overlay
-  [2]  = ACTION_FUNCTION(RGBLED_TOGGLE),
-  [3]  = ACTION_FUNCTION(RGBLED_STEP_MODE),
-  [4]  = ACTION_FUNCTION(RGBLED_INCREASE_HUE),
-  [5]  = ACTION_FUNCTION(RGBLED_DECREASE_HUE),
-  [6]  = ACTION_FUNCTION(RGBLED_INCREASE_SAT),
-  [7]  = ACTION_FUNCTION(RGBLED_DECREASE_SAT),
-  [8]  = ACTION_FUNCTION(RGBLED_INCREASE_VAL),
-  [9]  = ACTION_FUNCTION(RGBLED_DECREASE_VAL)
+  [0]  = ACTION_FUNCTION(RGBLED_TOGGLE),
+  [1]  = ACTION_FUNCTION(RGBLED_STEP_MODE),
+  [2]  = ACTION_FUNCTION(RGBLED_INCREASE_HUE),
+  [3]  = ACTION_FUNCTION(RGBLED_DECREASE_HUE),
+  [4]  = ACTION_FUNCTION(RGBLED_INCREASE_SAT),
+  [5]  = ACTION_FUNCTION(RGBLED_DECREASE_SAT),
+  [6]  = ACTION_FUNCTION(RGBLED_INCREASE_VAL),
+  [7]  = ACTION_FUNCTION(RGBLED_DECREASE_VAL)
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
