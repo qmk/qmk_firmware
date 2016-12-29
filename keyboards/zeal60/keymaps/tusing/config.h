a45d11b7ff84f53b2f66e15f9f4bd34c426ca810
Tushar Singal <tusharsingal@gmail.com>
2016-12-29 12:44:03 -0500

    Adding RGBW strip software-based current-limiting functionality.

--- keyboards/zeal60/keymaps/tusing/config.h
+++ keyboards/zeal60/keymaps/tusing/config.h
@@ -3,22 +3,6 @@
 
 #include "../../config.h"
 
-/* enable/disable LEDs based on layout */
-#undef USE_SPLIT_BACKSPACE
-#define USE_SPLIT_BACKSPACE 0
-
-#undef USE_SPLIT_LEFT_SHIFT
-#define USE_SPLIT_LEFT_SHIFT 0
-
-#undef USE_SPLIT_RIGHT_SHIFT
-#define USE_SPLIT_RIGHT_SHIFT 0
-
-#undef USE_7U_SPACEBAR
-#define USE_7U_SPACEBAR 0
-
-#undef USE_ISO_ENTER
-#define USE_ISO_ENTER 0
-
 // RGB underglow stuff.
 #define RGBLIGHT_ANIMATIONS
 #define RGB_DI_PIN B0          // The pin your RGB strip is wired to
@@ -27,5 +11,9 @@
 #define RGBLIGHT_HUE_STEP 5    // How much each press of rgb_hue changes hue
 #define RGBLIGHT_SAT_STEP 10   // How much each press of rgb_sat changes sat
 #define RGBLIGHT_VAL_STEP 10   // How much each press of rgb_val changes val
+
+// Current limiting.
+#define RGBSTRIP_CURRENT_LIMIT 400        // Strip current limit in mA.
+#define RGBSTRIP_MAX_CURRENT_PER_LIGHT 60 // mA per light when at max brightness.
 
 #endif //CONFIG_USER_H 
