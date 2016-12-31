e8640ef821513196f282dff78aa130447ca29702
Roben Kleene <robenk@gmail.com>
2016-12-31 14:29:12 -0500

    Start defining some keys

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -35,21 +35,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
 
+
+// KC_EQL, KC_LEFT, KC_DELT, KC_BSPC
+
 // Left Keyboard
-KC_EQL,           KC_1,         KC_2,           KC_3,             KC_4,  KC_5,  KC_LEFT,
-KC_DELT,          KC_Q,         KC_W,           KC_E,             KC_R,  KC_T,  TG(SYMB),
-KC_BSPC,          KC_A,         KC_S,           KC_D,             KC_F,  KC_G,
-KC_LSFT,          CTL_T(KC_Z),  KC_X,           KC_C,             KC_V,  KC_B,  ALL_T(KC_NO),
-LT(SYMB,KC_GRV),  KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+KC_GRV,         KC_1,         KC_2,     KC_3,     KC_4,     KC_5,  KC_MINS,
+KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,     KC_T,  KC_NO,
+KC_CAPS,        KC_A,         KC_S,     KC_D,     KC_F,     KC_G,
+KC_LSFT,        CTL_T(KC_Z),  KC_X,     KC_C,     KC_V,     KC_B,  MO(MDIA),
+KC_LEFT,        KC_RIGHT,     KC_LCTL,  KC_LALT,  KC_LGUI,
 // Left Cluster
 ALT_T(KC_APP),  KC_LGUI,
 KC_HOME,
-KC_SPC,         KC_BSPC,  KC_END,
+KC_SPC,         KC_BSPC,      KC_END,
 
 // Right Cluster
 KC_RGHT,            KC_7,                     KC_7,            KC_8,            KC_9,     KC_0,              KC_MINS,
 TG(SYMB),           KC_Y,                     KC_U,            KC_I,            KC_O,     KC_P,              KC_BSLS,
-KC_H,               KC_J,                     KC_K,            KC_L,            LT(MDIA,  KC_SCLN),KC_LGUI,
+KC_H,               KC_J,                     KC_K,            KC_L,            LT(MDIA,KC_SCLN) ,KC_LGUI,
 MEH_T(KC_NO),KC_N,  KC_M,                     KC_COMM,KC_DOT,  CTL_T(KC_SLSH),  KC_RSFT,
 KC_UP,              KC_DOWN,KC_LBRC,KC_RBRC,  KC_FN1,
 // Right Cluster
