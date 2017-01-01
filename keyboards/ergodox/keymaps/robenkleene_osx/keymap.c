721f9d24d25726f69dd41c5ade6a2bc7c22dd72d
Roben Kleene <robenk@gmail.com>
2017-01-01 13:14:00 -0500

    Finish base keymap

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -1,15 +1,15 @@
-// Netable differences vs. the default firmware for the ErgoDox EZ:
-// 1. The Cmd key is now on the right side, making Cmd+Space easier.
-// 2. The media keys work on OSX (But not on Windows).
 #include "ergodox.h"
 #include "debug.h"
 #include "action_layer.h"
 
-#define BASE 0 // default layer
+#define BASE 0 // Base
+// TODO: Delete unused
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+#define FCTN 3 // media keys
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  Fn  |           |  Fn  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Left |Right |LCTRL | LALT | LGUI |                                       | RGUI | ROPT |RCTRL |  Up  | Down |
+ *   | Left |Right |LCTRL | LALT | LGUI |                                       | RGUI | RALT |RCTRL |  Up  | Down |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | ESC  |      |       |      |  ESC   |
@@ -31,34 +31,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
-// If it accepts an argument (i.e, is a function), it doesn't need KC_.
-// Otherwise, it needs KC_*
+
 [BASE] = KEYMAP(  // layer 0 : default
 
-
-// KC_EQL, KC_LEFT, KC_DELT, KC_BSPC
-
 // Left Keyboard
-KC_GRV,         KC_1,         KC_2,     KC_3,     KC_4,     KC_5,  KC_MINS,
-KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,     KC_T,  KC_NO,
-KC_CAPS,        KC_A,         KC_S,     KC_D,     KC_F,     KC_G,
-KC_LSFT,        CTL_T(KC_Z),  KC_X,     KC_C,     KC_V,     KC_B,  MO(MDIA),
-KC_LEFT,        KC_RIGHT,     KC_LCTL,  KC_LALT,  KC_LGUI,
+KC_GRV,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_MINS,
+KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,  KC_NO,
+KC_CAPS,  KC_A,      KC_S,     KC_D,     KC_F,     KC_G,
+KC_LSFT,  KC_Z,      KC_X,     KC_C,     KC_V,     KC_B,  MO(FCTN),
+KC_LEFT,  KC_RIGHT,  KC_LCTL,  KC_LALT,  KC_LGUI,
 // Left Cluster
-ALT_T(KC_APP),  KC_LGUI,
+KC_ESC,   KC_NO,
 KC_HOME,
-KC_SPC,         KC_BSPC,      KC_END,
+KC_BSPC,  KC_DEL,  KC_END,
 
+// Right Keyboard
+KC_EQL,    KC_7,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS,
+KC_RBRC    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,
+KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+MO(FCTN),  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+KC_RGUI,   KC_RALT,  KC_RCTL,  KC_UP,    KC_DOWN,
 // Right Cluster
-KC_RGHT,            KC_7,                     KC_7,            KC_8,            KC_9,     KC_0,              KC_MINS,
-TG(SYMB),           KC_Y,                     KC_U,            KC_I,            KC_O,     KC_P,              KC_BSLS,
-KC_H,               KC_J,                     KC_K,            KC_L,            LT(MDIA,KC_SCLN) ,KC_LGUI,
-MEH_T(KC_NO),KC_N,  KC_M,                     KC_COMM,KC_DOT,  CTL_T(KC_SLSH),  KC_RSFT,
-KC_UP,              KC_DOWN,KC_LBRC,KC_RBRC,  KC_FN1,
-// Right Cluster
-KC_LALT,            CTL_T(KC_ESC),
+KC_NO,    KC_ESC,
 KC_PGUP,
-KC_PGDN,            KC_TAB,                   KC_ENT
+KC_PGDN,  KC_ENT,  KC_SPC
 
 ),
 
