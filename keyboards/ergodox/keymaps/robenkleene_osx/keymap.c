dc215fae1cf58c826c1b011b824abd78dcadcaf4
Roben Kleene <robenk@gmail.com>
2017-01-02 22:18:20 -0500

    Clean up

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -112,7 +112,6 @@ KC_TRNS,  KC_TRNS,  KC_TRNS
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
