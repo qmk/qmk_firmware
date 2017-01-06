7e19b51e3365c3578f77be4272cdd2e4552cd0ed
Joshua T <joshua@sonofone.net>
2017-01-06 17:08:14 -0600

    Increased TAPPING_TERM to 200

--- keyboards/ergodox/keymaps/replicaJunction/config.h
+++ keyboards/ergodox/keymaps/replicaJunction/config.h
@@ -28,7 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
-#define TAPPING_TERM    150
+// MS the button needs to be held before a tap becomes a hold (default: 200)
+#define TAPPING_TERM    200
+
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
 // I don't have any locking keys, so I don't need these features
