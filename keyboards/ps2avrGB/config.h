7be7529b0597a32d8652ec795ad69e0f30702c91
Luiz Ribeiro <luizribeiro@gmail.com>
2017-01-21 17:09:17 -0500

    Simplified and polished a bit the code changes on tmk_core

--- keyboards/ps2avrGB/config.h
+++ keyboards/ps2avrGB/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BACKLIGHT_COLOR 0x110000
 
 #define NO_UART 1
-#define EEPROM_BOOTLOADER_START 1
+#define BOOTLOADHID_BOOTLOADER 1
 
 /* key combination for command */
 #define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
