6774f18228e704ccece40805f4860cac8090e070
TerryMathews <terry@terrymathews.net>
2017-01-22 15:57:30 -0500

    Missed commenting out notes at end of line

--- keyboards/lets_split/rev2/config.h
+++ keyboards/lets_split/rev2/config.h
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // wiring of each half
 #define MATRIX_ROW_PINS { D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-// #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6 } uncomment this line and comment line above if you need to reverse left-to-right key order
+// #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6 } //uncomment this line and comment line above if you need to reverse left-to-right key order
 
 #define CATERINA_BOOTLOADER
 
