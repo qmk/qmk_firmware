8bd3c099e690160c3f9eec386cfdfc4b7363ed06
Ryan MacLean <ryan@ryanmaclean.com>
2017-01-24 13:24:26 -0800

    Update keymap.c

--- keyboards/zeal60/keymaps/ryanmaclean/keymap.c
+++ keyboards/zeal60/keymaps/ryanmaclean/keymap.c
@@ -1,4 +1,7 @@
 // Ryan MacLean's layout for Zeal60
+// Note that LGUI and RGUI are swapped with LALT and RALT respectively, for use with Macs
+// Also note that Control has replaced Caps Lock, and that pressing left or right shift once
+// will output left parenthese and right parenthese respectively. 
 
 #include "zeal60.h"
 
