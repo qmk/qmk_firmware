6f826836be435428158eddc511959d6fc38ada98
TerryMathews <terry@terrymathews.net>
2017-01-29 22:22:32 -0500

    Let's Split: establish rev2fliphalf subproject
    Subproject to accomodate second half pad built in opposite orientation
    (places TRRS jacks on opposing edges for shorter cable runs)

--- keyboards/lets_split/keymaps/i2c/config.h
+++ keyboards/lets_split/keymaps/i2c/config.h
@@ -25,3 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef SUBPROJECT_rev2
     #include "../../rev2/config.h"
 #endif
+#ifdef SUBPROJECT_rev2fliphalf
+	#include "../../rev2fliphalf/config.h"
+#endif
