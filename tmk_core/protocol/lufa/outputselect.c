1436f37151a3c28078087bdbd76fada259ed1fb9
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2017-02-01 20:18:52 +0700

    Fix wrong include

--- tmk_core/protocol/lufa/outputselect.c
+++ tmk_core/protocol/lufa/outputselect.c
@@ -12,7 +12,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "quantum.h"
+#include "lufa.h"
 #include "outputselect.h"
 #ifdef ADAFRUIT_BLE_ENABLE
     #include "adafruit_ble.h"
