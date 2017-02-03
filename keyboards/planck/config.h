8a7daf34a26fddcf1ddc5331cce7e160beea76c3
johannes <developer.johannes.larsson@gmail.com>
2017-02-03 10:47:37 +0100

    initial keymap

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -32,10 +32,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 12
 
 /* Planck PCB default pin-out */
-#define MATRIX_ROW_PINS { D0, D5, B5, B6 }
-#define MATRIX_COL_PINS { F1, F0, B0, C7, F4, F5, F6, F7, D4, D6, B4, D7 }
+#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
+#define MATRIX_COL_PINS { B1, B3, B2, B6, B5, B4, D2, D7, C6, D4, D0, D1 }
 #define UNUSED_PINS
-
 #define AUDIO_VOICES
 
 #define BACKLIGHT_PIN B7
