43ab09a31dfaed53059863e36c312dd95e96a96b
milestogo <milestogo@users.noreply.github.com>
2017-02-04 20:06:28 -0800

    moving back out of handwired

--- keyboards/microsoft-sculpt-mobile/keymaps/milestogo/keymap.c
+++ keyboards/microsoft-sculpt-mobile/keymaps/milestogo/keymap.c
@@ -11,9 +11,9 @@
 
 
 // Custom macros
-#define SFT_ENT   SFT_T(KC_ENT) // tap for enter, hold for shift
-#define TT_SYM   ACTION_LAYER_TAP_TOGGLE(_SYM) // on when held, toggle with taps
-
+#define SFT_ENT KC_FN0 
+#define TT_SYM  KC_FN1
+#define TO_CDH KC_FN2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*  QWERTY
@@ -33,21 +33,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 [_QWR] = KEYMAP( \
-   KC_ESC,   KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,   KC_9, KC_0,    KC_F11,   KC_F12, KC_VOLD, KC_VOLU, KC_MUTE,\
-   KC_GRAVE, KC_1, KC_2, KC_3 ,KC_4, KC_5, KC_6, KC_7, KC_8,   KC_9, KC_0,    KC_MINUS, KC_EQL, KC_BSPC, KC_DEL,\
+   KC_ESC,   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10,    KC_F11,   KC_F12, KC_VOLD, KC_VOLU, TO_CDH,\
+   KC_ESC,   KC_1, KC_2, KC_3 ,KC_4, KC_5, KC_6, KC_7, KC_8,   KC_9, KC_0,    KC_MINUS, KC_EQL, KC_BSPC, KC_DEL,\
    KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,   KC_O, KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,\
-   KC_CAPS,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L, KC_SCLN, KC_QUOT,  KC_ENT, KC_PGUP,\
+   TT_SYM,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L, KC_SCLN, KC_QUOT,  KC_ENT, KC_PGUP,\
    KC_LSFT,  KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT,KC_SLSH,KC_RSFT,  KC_UP,  KC_PGDN,\
-   KC_LCTL,  KC_LGUI, KC_LALT, KC_SPACE, KC_RALT, RSFT(KC_1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
+   KC_LCTL,  KC_LGUI, KC_LALT, KC_SPACE, TT_SYM,TT_SYM,KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 [_CDH] = KEYMAP (\
    ____,     ____, ____, ____, ____, ____, ____, ____, ____,   ____, ____,    ____,     ____,   ____,    ____,     ____,  \
-   ____,     ____, ____, ____, ____, ____, ____, ____, ____,   ____, ____,    ____,     ____,   ____,    ____,   \
+   KC_ESC,     ____, ____, ____, ____, ____, ____, ____, ____,   ____, ____,    ____,     ____,   ____,    ____,   \
    KC_TAB,  KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN, ____,    ____,   ____,\
    TT_SYM,  KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT, KC_ENT, ____,\
    KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, ____,   ____,\
-  ____,____,____,____,TT_SYM,TT_SYM,____,____,____,____
+  ____,____,____,____,TT_SYM,TT_SYM,TT_SYM,____,____,____
 
 ),
 
@@ -70,7 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 const uint16_t PROGMEM fn_actions[] = {
-
+ [0]= SFT_T(KC_ENT), // tap for enter, hold for shift
+ [1] = ACTION_LAYER_TAP_TOGGLE(_SYM), // on when held, toggle with taps
+ [2] = ACTION_LAYER_TOGGLE(_CDH),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
