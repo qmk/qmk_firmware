19d63ad72c012ee3dfb9375724067b278593df8a
Jack Humbert <jack.humb@gmail.com>
2017-02-07 01:35:10 -0500

    try building only keyboards needed

---
+++ util/travis_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TRAVIS_COMMIT_MESSAGE="${TRAVIS_COMMIT_MESSAGE:-none}"
+TRAVIS_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE:-HEAD~1..HEAD}"
+
+if [[ "$TRAVIS_COMMIT_MESSAGE" != *"[skip build]"* ]] ; then 
+	NEFM=$(git diff --name-only -n 1 ${TRAVIS_COMMIT_RANGE} | grep -Ev '^(keyboards/)' | wc -l)
+	if [[ $NEFM -gt 0 ]] ; then
+		echo "Making all keymaps for all keyboards"
+		make all-keyboards AUTOGEN=true
+	else
+		MKB=$(git diff --name-only -n 1 ${TRAVIS_COMMIT_RANGE} | grep -oP '(?<=keyboards\/)([a-zA-Z_]+)' | sort -u)
+		for KB in $MKB ; do
+			echo "Making all keymaps for $KB"
+			make "$KB" AUTOGEN=true
+		done
+	fi
+fi
--- util/travis_compiled_push.sh
+++ util/travis_compiled_push.sh
@@ -38,9 +38,9 @@ if [[ "$TRAVIS_COMMIT_MESSAGE" != *"[skip build]"* ]] ; then
 	git clone https://$GH_TOKEN@github.com/jackhumbert/qmk.fm.git
 	cd qmk.fm
 	git submodule update --init --recursive
-	rm -rf keyboard
-	rm -rf keyboards
-	cp -r ../qmk_firmware/keyboards .
+	#rm -rf keyboard
+	#rm -rf keyboards
+	yes | cp -rf ../qmk_firmware/keyboards .
 	mkdir keyboards/ergodox_ez/
 	cp ../qmk_firmware/util/ergodox_ez.html keyboards/ergodox_ez/index.html
 	cp ../qmk_firmware/readme.md qmk_readme.md
