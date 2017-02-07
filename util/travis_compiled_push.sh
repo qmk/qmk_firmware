f14e9fdd59619626b52eb21e454ab06fc9ac7930
Jack Humbert <jack.humb@gmail.com>
2017-02-07 12:36:25 -0500

    pass through travis errors

--- util/travis_compiled_push.sh
+++ util/travis_compiled_push.sh
@@ -41,7 +41,7 @@ if [[ "$TRAVIS_COMMIT_MESSAGE" != *"[skip build]"* ]] ; then
 	#rm -rf keyboard
 	#rm -rf keyboards
 	yes | cp -rf ../qmk_firmware/keyboards .
-	mkdir keyboards/ergodox_ez/
+	#mkdir keyboards/ergodox_ez/
 	cp ../qmk_firmware/util/ergodox_ez.html keyboards/ergodox_ez/index.html
 	cp ../qmk_firmware/readme.md qmk_readme.md
 	./generate.sh
