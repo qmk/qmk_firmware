5944ab246a981d6ceca94b0972345277a746c2d3
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2017-02-10 21:28:46 +0700

    Implement battery level indicator

--- keyboards/handwired/promethium/promethium.c
+++ keyboards/handwired/promethium/promethium.c
@@ -1,6 +1,36 @@
 #include "promethium.h"
+#include "analog.h"
+#include "timer.h"
+#include "matrix.h"
+
+float battery_percentage(void) {
+    float voltage = analogRead(BATTERY_PIN) * 2 * 3.3 / 1024;
+    float percentage = (voltage - 3.5) * 143;
+    if (percentage > 100) {
+        return 100;
+    } else if (percentage < 0) {
+        return 0;
+    } else {
+        return percentage;
+    }
+}
+
+__attribute__ ((weak))
+void battery_poll(float percentage) {
+}
 
 void matrix_init_kb(void) {
-
 	matrix_init_user();
 }
+
+void matrix_scan_kb(void) {
+    static uint16_t counter = BATTERY_POLL;
+    counter++;
+
+    if (counter > BATTERY_POLL) {
+        counter = 0;
+        battery_poll(battery_percentage());
+    }
+}
+
+
