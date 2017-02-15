e73270ec6f0f311b0490d33236cab90b8ce6a086
skullY <skullydazed@gmail.com>
2017-02-15 11:12:09 -0800

    Clarify the license for files we have signoff on

--- quantum/template/config.h
+++ quantum/template/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2017 REPLACE_WITH_YOUR_NAME
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
