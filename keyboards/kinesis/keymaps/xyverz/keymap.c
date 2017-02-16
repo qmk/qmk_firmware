d3451739b3efcf195a5fafc2308965f7e006b511
Xyverz <xyverz@gmail.com>
2017-02-15 21:56:48 -0800

    minor adjustments.

--- keyboards/kinesis/keymaps/xyverz/keymap.c
+++ keyboards/kinesis/keymaps/xyverz/keymap.c
@@ -54,25 +54,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DV] = KEYMAP(
            // Left Hand
            KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
-           KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+           KC_RBRC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
            KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,
            KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
            KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,
                     KC_GRV,  KC_INS,  KC_LEFT, KC_RGHT,
            // Left Thumb
-                    KC_LCTL, KC_LALT,
+                    CTL_T(KC_ESC), KC_LALT,
                              KC_HOME,
            KC_BSPC, LT(_LO, KC_DEL),  KC_END,
 
            // Right Hand
            KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, MO(_LO),  MO(_RA),
-           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-           KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,
+           KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSLS,
            KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
            KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-                    KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
+                    KC_UP,   KC_DOWN, KC_SLSH, KC_EQL,
            // Right Thumb
-           KC_RGUI, KC_RCTL,
+           KC_RGUI, CTL_T(KC_ESC),
            KC_PGUP,
            KC_PGDN, LT(_RA, KC_ENT),  KC_SPC
     ),
