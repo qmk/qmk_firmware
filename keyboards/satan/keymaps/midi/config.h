5d7a9297f91a71a00b5e258c03b5b1629632f468
Gabriel Young <gabeplaysdrums@live.com>
2017-02-19 17:18:05 -0800

    Allow customization of the number of tone keycodes and add example keymap

---
+++ keyboards/satan/keymaps/midi/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// place overrides here
+
+/* override number of MIDI tone keycodes (each octave adds 12 bytes) */
+#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+#endif
