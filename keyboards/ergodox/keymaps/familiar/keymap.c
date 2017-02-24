72522ed2fbdfe71b5409c603a86f8ed441fd548a
nstickney <nstickney@gmail.com>
2017-02-24 17:11:19 -0600

    first two layers of ErgoDox Familiar completeC

--- keyboards/ergodox/keymaps/familiar/keymap.c
+++ keyboards/ergodox/keymaps/familiar/keymap.c
@@ -4,7 +4,7 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-//#define FUNC 1 // function, media, arrow keys
+#define ARRW 1 // function, media, arrow keys
 //#define NUMP 2 // numpad
 //#define INTL 3 // international symbols
 
@@ -16,53 +16,95 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | ESC    |   1  |   2  |   3  |   4  |   5  |   -  |           |  =   |   6  |   7  |   8  |   9  |   0  | SYSREQ |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | DEL    |   Q  |   W  |   E  |   R  |   T  | HOME |           | PGUP |   Y  |   U  |   I  |   O  |   P  |  BKSP  |
+ * | DEL    |   Q  |   W  |   E  |   R  |   T  | HOME |           | PGUP |   Y  |   U  |   I  |   O  |   P  |   BKSP |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | TAB    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  ENTER |
  * |--------+------+------+------+------+------|  END |           | PGDN |------+------+------+------+------+--------|
  * | (/LSFT |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | )/RSFT |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- * |LGUI/LCTRL|MENU|   `  |   '  | CAPS |                                       |   [  |   ]  |   \  |MENU|RGUI/RCTRL|
+ * | LCTRL  | LGUI |   `  |   '  |PRTSCR|                                       |   [  |   ]  |   \  | RGUI |  RCTRL |
  * `------------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | VOL- | VOL+ |       | BRT- | BRT+ |
  *                                 ,------|------|------|       |------+------+------.
- *                                 | ALT/ |      | MUTE |       | NUM  |      | ALT/ |
+ *                                 | ALT/ |      | MUTE |       | NUMLK|      | ALT/ |
  *                                 | SPC  | LAY1 |------|       |------| LAY1 | SPC  |
- *                                 |      |      | LAY3 |       | LAY2 |      |      |
+ *                                 |      |      | LAY3 |       |TTLAY2|      |      |
  *                                 `--------------------'       `--------------------'
  */
-// If it accepts an argument (i.e, is a function), it doesn't need KC_.
-// Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,         KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,
         KC_DEL,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_HOME,
         KC_TAB,         KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSPO,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_END,
-        KC_LCTL,        KC_LGUI,KC_GRV, KC_QUOT,KC_CAPS,
-                                                KC_VOLD, KC_VOLU,
+        KC_LCTL,        KC_LGUI,KC_GRV, KC_QUOT,KC_PSCR,
+                                                KC_VOLD,    KC_VOLU,
                                                             KC_MUTE,
-                                ALT_T(KC_SPC),  MO(BASE),   MO(BASE),
+                                ALT_T(KC_SPC),  MO(ARRW),   TT(BASE),
         // right hand
             KC_EQL,     KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_SYSREQ,
             KC_PGUP,    KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
                         KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENTER,
             KC_PGDN,    KC_N,   KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSPC,
                                 KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_RGUI,    KC_RCTL,
-            KC_VOLD, KC_VOLU,
+            KC_VOLD,    KC_VOLU,
             KC_NLCK,
-            TT(BASE),   MO(BASE),   ALT_T(KC_SPC)
+            TT(BASE),   MO(ARRW),   ALT_T(KC_SPC)
+    ),
+
+/* Keymap 0: Basic layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      |  UP  |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      | LEFT | DOWN |RIGHT |      |------|           |------| LEFT | DOWN |  UP  | RIGHT|      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |M_PREV|M_STOP|M_PLPS|M_NEXT|      |      |           |      |      |      |      |      |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ * |        |      |      |      |      |                                       |      |      |      |      |        |
+ * `------------------------------------'                                       `------------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+[ARRW] = KEYMAP(// layer 1 : functions and arrows
+        // left hand
+        KC_TRNS,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F11,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_UP,      KC_TRNS,    KC_TRNS,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                                            KC_TRNS,    KC_TRNS,
+                                                        KC_TRNS,
+                            KC_TRNS,        KC_TRNS,    KC_TRNS,
+        // right hand
+            KC_F12,     KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_TRNS,
+            KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                        KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_TRNS,    KC_TRNS,
+            KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                                    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+            KC_TRNS,    KC_TRNS,
+            KC_TRNS,
+            KC_TRNS,    KC_TRNS,    KC_TRNS
     ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(BASE)                // FN1 - Momentary Layer 1 (Function Keys)
+    [1] = ACTION_LAYER_TAP_TOGGLE(ARRW)                // FN1 - Momentary Layer 1 (Function Keys)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
