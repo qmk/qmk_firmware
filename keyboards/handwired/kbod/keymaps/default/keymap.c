e7592f9ec6bd9a5a711ec54d3b608af35f05eff9
Nurahmadie <nurahmadie@gmail.com>
2017-02-24 12:06:10 +0700

    Remove lead keys, fix some typos, rearrange layers

--- keyboards/handwired/kbod/keymaps/default/keymap.c
+++ keyboards/handwired/kbod/keymaps/default/keymap.c
@@ -7,29 +7,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQUAL, KC_BSPC,
       KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLASH,
       KC_CAPS,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOT, KC_ENTER,
-      KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSPO,
-      LT(1, KC_LEAD), KC_LCTL, KC_LALT, KC_SPACE, KC_RALT, KC_RGUI, KC_APP, KC_RCTL 
+      KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSPC,
+      MO(1), KC_LCTL, KC_LALT, KC_SPACE, KC_RALT, KC_RGUI, KC_APP, KC_RCTL 
   ),
   KEYMAP( /* Cursor layer */
       KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-      _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_INSERT,
-      _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_HOME, KC_END, _____,
+      TO(3),  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_INSERT,
+      TO(2),  _____, _____, _____, _____, _____, _____, _____, _____, _____, KC_HOME, KC_END, _____,
       _____,  _____, _____, _____, _____, _____, _____, _____, KC_PGUP, KC_PGDN, KC_UP, _____,
-      _____,  _____, _____, _____, _____, KC_LEFT, KC_DOWN, KC_RIGHT, _____
+      _____,  _____, _____, _____, KC_LEFT, KC_DOWN, KC_RIGHT, _____
   ),
   KEYMAP( /* Multimedia layer */
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, KC_MS_BTN1, KC_MS_UP, _____,
-      LT(3, KC_LEAD),  _____, _____, _____, _____, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_BTN3
+      TO(0),  _____, _____, _____, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_BTN3
   ),
   KEYMAP( /* Multimedia layer */
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____,
       _____,  _____, _____, _____, _____, _____, _____, _____, _____, KC_MS_BTN1, KC_MS_WH_UP, _____,
-      _____,  _____, _____, _____, _____, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_MS_BTN3
+      TO(0),  _____, _____, _____, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_MS_BTN3
   ),
 };
 
@@ -37,6 +37,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [0]  = ACTION_FUNCTION(0),
 };
 
+#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
   static uint8_t shift_esc_shift_mask;
   switch (id) {
@@ -63,31 +64,11 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
   }
 }
 
-
-LEADER_EXTERNS();
-
-void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
-
-    SEQ_ONE_KEY(KC_B) {
-      layer_switch(0);
-    }
-    SEQ_ONE_KEY(KC_C) {
-      layer_switch(1);
-    }
-    SEQ_ONE_KEY(KC_M) {
-      layer_switch(2);
-    }
-  }
-}
-
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
     return MACRO_NONE;
 };
 
 void led_set_user(uint8_t usb_led) {
-
+  
 }
