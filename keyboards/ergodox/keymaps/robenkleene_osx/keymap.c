38486fc21bfd3b3e7f5700436c1c53e536c3c34c
Roben Kleene <robenk@gmail.com>
2017-02-28 22:17:27 -0500

    Update keymap to place minus key

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -10,9 +10,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ~    |   1  |   2  |   3  |   4  |   5  |   -  |           |   =  |   6  |   7  |   8  |   9  |   0  |   \    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |   ~  |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |   }  |   Y  |   U  |   I  |   O  |   P  |   {    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |   {  |           |   }  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   "    |
  * |--------+------+------+------+------+------|  Fn  |           |  Fn  |------+------+------+------+------+--------|
@@ -32,9 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Default
 [DFLT] = KEYMAP(
 
+
 // Left Keyboard
-KC_GRV,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_MINS,
-KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,  KC_NO,
+KC_EQL,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_GRV,
+KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,  KC_LBRC,
 KC_CAPS,  KC_A,      KC_S,     KC_D,     KC_F,     KC_G,
 KC_LSFT,  KC_Z,      KC_X,     KC_C,     KC_V,     KC_B,  MO(FCTN),
 KC_LEFT,  KC_RIGHT,  KC_LCTL,  KC_LALT,  KC_LGUI,
@@ -45,8 +46,8 @@ KC_HOME,
 KC_BSPC,  KC_DEL,  KC_END,
 
 // Right Keyboard
-KC_EQL,    KC_7,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS,
-KC_RBRC,   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,
+KC_NO,     KC_7,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+KC_RBRC,   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
 KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
 MO(FCTN),  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
 KC_RGUI,   KC_RALT,  KC_RCTL,  KC_UP,    KC_DOWN,
