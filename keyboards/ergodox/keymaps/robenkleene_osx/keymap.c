74112bd4c03cc65aba28d5882e0488623c77ad16
Roben Kleene <robenk@gmail.com>
2017-02-28 22:39:13 -0500

    Swap fn and esc

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -15,13 +15,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   {  |           |   }  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   "    |
- * |--------+------+------+------+------+------|  Fn  |           |  Fn  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| ESC  |           | ESC  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Left |Right |LCTRL | LALT | LGUI |                                       | RGUI | RALT |RCTRL |  Up  | Down |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | ESC  |      |       |      |  ESC   |
+ *                                        | Fn   |      |       |      |  Fn  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | BS   | DEL  |------|       |------|  Ret   |Space |
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_GRV,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_EQL,
 KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,  KC_LBRC,
 KC_CAPS,  KC_A,      KC_S,     KC_D,     KC_F,     KC_G,
-KC_LSFT,  KC_Z,      KC_X,     KC_C,     KC_V,     KC_B,  MO(FCTN),
+KC_LSFT,  KC_Z,      KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
 KC_LEFT,  KC_RIGHT,  KC_LCTL,  KC_LALT,  KC_LGUI,
 
 // Left Cluster
-KC_ESC,   KC_NO,
+MO(FCTN),   KC_NO,
 KC_HOME,
 KC_BSPC,  KC_DEL,  KC_END,
 
@@ -49,11 +49,11 @@ KC_BSPC,  KC_DEL,  KC_END,
 KC_NO,     KC_7,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
 KC_RBRC,   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
 KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-MO(FCTN),  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+KC_ESC,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
 KC_RGUI,   KC_RALT,  KC_RCTL,  KC_UP,    KC_DOWN,
 
 // Right Cluster
-KC_NO,    KC_ESC,
+KC_NO,    MO(FCTN),
 KC_PGUP,
 KC_PGDN,  KC_ENT,  KC_SPC
 
