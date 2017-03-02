d11962aeb27c73b87f8154d7f2cee747c8858d09
Gabriel Young <gabeplaysdrums@live.com>
2017-03-02 11:40:06 -0800

    fix 'stop_all_notes' naming to be more consistent

--- quantum/process_keycode/process_audio.h
+++ quantum/process_keycode/process_audio.h
@@ -4,7 +4,7 @@
 bool process_audio(uint16_t keycode, keyrecord_t *record);
 void process_audio_noteon(uint8_t note);
 void process_audio_noteoff(uint8_t note);
-void process_audio_stop_all_notes(void);
+void process_audio_all_notes_off(void);
 
 void audio_on_user(void);
 
