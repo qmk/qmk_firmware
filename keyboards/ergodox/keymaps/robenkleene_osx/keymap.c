b8c892d5724b41d2ae1b7cc1cb97f66ec6525a93
Roben Kleene <robenk@gmail.com>
2017-03-01 22:46:30 -0500

    Give full arrow keys on the right side

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -38,7 +38,7 @@ KC_GRV,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_EQL,
 KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,  KC_LBRC,
 KC_CAPS,  KC_A,      KC_S,     KC_D,     KC_F,     KC_G,
 KC_LSFT,  KC_Z,      KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
-KC_LEFT,  KC_RIGHT,  KC_LCTL,  KC_LALT,  KC_LGUI,
+KC_NO,    KC_NO,     KC_LCTL,  KC_LALT,  KC_LGUI,
 
 // Left Cluster
 MO(FCTN),   KC_NO,
@@ -49,8 +49,8 @@ KC_BSPC,  KC_DEL,  KC_END,
 KC_NO,     KC_7,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
 KC_RBRC,   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
 KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-KC_ESC,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-KC_RGUI,   KC_RALT,  KC_RCTL,  KC_UP,    KC_DOWN,
+KC_ESC,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+KC_RGUI,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,
 
 // Right Cluster
 KC_NO,    MO(FCTN),
