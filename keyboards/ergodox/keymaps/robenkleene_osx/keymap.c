35bea74476e67dd761cb293dcbe458ca211af95a
Roben Kleene <robenk@gmail.com>
2017-03-02 21:59:26 -0500

    Add second brace location

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -50,7 +50,7 @@ KC_NO,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
 KC_RBRC,   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
 KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
 KC_ESC,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-KC_RGUI,   KC_RALT,  KC_RCTL,  KC_NO,    MO(FCTN),
+KC_RGUI,   KC_RALT,  KC_LBRC,  KC_RBRC,  MO(FCTN),
 
 // Right Cluster
 KC_NO,    MO(FCTN),
