66a757f3e5445b1548cf0aaff1429b566e574079
Roben Kleene <robenk@gmail.com>
2017-03-06 20:51:07 -0500

    Add Kinesis style control keys

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |   =  |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |   {  |   Y  |   U  |   I  |   O  |   P  |   |    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -18,10 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| ESC  |           |   }  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  ~   |   |  |LCTRL | LALT | LGUI |                                       | RGUI | RALT |RCTRL |      | Fn   |
+ *   |  ~   |   |  |LCTRL | LALT | LGUI |                                       | RGUI | RALT |   {  |   }  | Fn   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | Fn   |      |       |      |  Fn  |
+ *                                        | CTRL |  Fn  |       |  Fn  | CTRL |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | BS   | DEL  |------|       |------|  Ret   |Space |
@@ -34,14 +34,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 // Left Keyboard
-KC_EQL,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_EQL,
+KC_EQL,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
 KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
 KC_LCTL,  KC_A,      KC_S,     KC_D,     KC_F,     KC_G,
 KC_LSFT,  KC_Z,      KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
-KC_GRV,    KC_BSLS,   KC_LCTL,  KC_LALT,  KC_LGUI,
+KC_GRV,   KC_BSLS,   KC_LCTL,  KC_LALT,  KC_LGUI,
 
 // Left Cluster
-MO(FCTN),   KC_NO,
+KC_LCTL,  MO(FCTN),
 KC_HOME,
 KC_BSPC,  KC_DEL,  KC_END,
 
@@ -53,7 +53,7 @@ KC_RBRC,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
 KC_RGUI,   KC_RALT,  KC_LBRC,  KC_RBRC,  MO(FCTN),
 
 // Right Cluster
-KC_NO,    MO(FCTN),
+MO(FCTN), KC_RCTL,
 KC_PGUP,
 KC_PGDN,  KC_ENT,  KC_SPC
 
@@ -64,11 +64,11 @@ KC_PGDN,  KC_ENT,  KC_SPC
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | TNSY   |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           | VolD | VolU |  RW  | Play |  FF  | Mute |        |
+ * |        |      |      |      |      |      |      |           | VolU |  FF  |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |      |      |  UP  |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      | LEFT | DOWN |RIGHT |        |
+ * |        |      |      |      |      |      |------|           |------| Play | Mute |      |  UP  |      |        |
+ * |--------+------+------+------+------+------|      |           | VolD |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |  RW  |      | LEFT | DOWN |RIGHT |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -98,9 +98,9 @@ KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Right Keyboard
 KC_TRNS,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
+KC_VOLU,  KC_MNXT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_MPLY,  KC_MUTE,  KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,
+KC_VOLD,  KC_MPRV,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Right Cluster
