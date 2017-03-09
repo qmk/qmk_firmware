1140b40fab9d2a9986de1cf3c1a2553a7d5275e9
nstickney <nstickney@gmail.com>
2017-03-08 22:28:18 -0600

    found LT(layer, kc) so I could take out TapDance code

--- keyboards/ergodox/keymaps/familiar/keymap.c
+++ keyboards/ergodox/keymaps/familiar/keymap.c
@@ -12,12 +12,6 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-// Tap Dance Codes
-enum {
-    SLASH,
-    WHACK,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
@@ -28,30 +22,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | TAB    |   Q  |   W  |   E  |   R  |   T  | HOME |           | PGUP |   Y  |   U  |   I  |   O  |   P  | DELETE |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | NOOP   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  ENTER |
+ * | CLEAR  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  ENTER |
  * |--------+------+------+------+------+------|  END |           | PGDN |------+------+------+------+------+--------|
  * | (/LSFT |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  UP  | )/RSFT |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- * | LCTRL  | LGUI | MENU |   '  | NOOP |                                       |   [  |   ]  | LEFT | DOWN |  RIGHT |
+ * | LCTRL  | LGUI | MENU |   '  |   "  |                                       |   [  |   ]  | LEFT | DOWN |  RIGHT |
  * `------------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |PRTSCR| ESC  |       | VOL- | VOL+ |
  *                                 ,------|------|------|       |------+------+------.
  *                                 | ALT/ |SLASH/| NUMLK|       | MUTE |WHACK/| ALT/ |
- *                                 | SPC  |MO(1)/|------|       |------|MO(1)/| SPC  |
- *                                 |      |TG(1) | LAY3 |       | LAY2 |TG(1) |      |
+ *                                 | SPC  | MO(1)|------|       |------|MO(1) | SPC  |
+ *                                 |      |      | LAY3 |       | LAY2 |      |      |
  *                                 `--------------------'       `--------------------'
  */
 [BASE] = KEYMAP(
         // left hand
         KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,
-        XXXXXXX,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
+        KC_CLEAR,   KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
         KC_LSPO,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_END,
-        KC_LCTL,    KC_LGUI,    KC_MENU,    KC_QUOT,    XXXXXXX,
+        KC_LCTL,    KC_LGUI,    KC_MENU,    KC_QUOT,    S(KC_QUOT),
                                                                     KC_INS,     KC_ESC,
                                                                                 KC_NLCK,
-                                                    ALT_T(KC_SPC),  TD(SLASH),  TG(INTL),
+                                            ALT_T(KC_SPC),  LT(ARRW,KC_SLSH),   TG(INTL),
         // right hand
             KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
             KC_PGUP,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,
@@ -60,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     KC_LBRC,    KC_RBRC,    KC_LEFT,    KC_DOWN,    KC_RGHT,
             KC_VOLD,    KC_VOLU,
             KC_MUTE,
-            TG(NUMP),   TD(WHACK),   ALT_T(KC_SPC)
+            TG(NUMP),   LT(ARRW,KC_BSLS),   ALT_T(KC_SPC)
     ),
 
 /* layer 1 : functions and arrows
@@ -70,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |  UP  |      |      |      |           |      |      |      |      |      |      | INSERT |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      | LEFT | DOWN |RIGHT |      |------|           |------| LEFT | DOWN |  UP  | RIGHT|      |        |
+ * |CAPSLOCK|      | LEFT | DOWN |RIGHT |      |------|           |------| LEFT | DOWN |  UP  | RIGHT|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |M_PREV|M_STOP|M_PLPS|M_NEXT|      |      |           |      |      |      |      |      | PGUP |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -88,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,
         _______,    _______,    _______,    KC_UP,      _______,    _______,    _______,
-        _______,    _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,
+        KC_CAPS,    _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,    _______,
         _______,    _______,    _______,    _______,    _______,
                                                                     KC_SYSREQ,  KC_PAUSE,
@@ -108,15 +102,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* layer 2 : numberpad
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |   (  |   )  |   ÷  |  ×   |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |   7  |   8  |   9  |  -   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
+ * |        |      |      |      |      |      |------|           |------|      |   4  |   5  |   6  |  +   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |   1  |   2  |   3  |  =   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- * |        |      |      |      |      |                                       |      |      |      |      |        |
+ * |        |      |      |      |      |                                       |   0  |   .  |   ,  | ENTER|        |
  * `------------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -137,11 +131,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         _______,
                             _______,        _______,    _______,
         // right hand
-            _______,    _______,    _______,    _______,    _______,    _______,    _______,
-            _______,    _______,    _______,    _______,    _______,    _______,    _______,
-                        _______,    _______,    _______,    _______,    _______,    _______,
-            _______,    _______,    _______,    _______,    _______,    _______,    _______,
-                                    _______,    _______,    _______,    _______,    _______,
+            _______,    _______,    S(KC_9),    S(KC_0),    KC_PSLS,    KC_PAST,    _______,
+            _______,    _______,    KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_PMNS,    _______,
+                        _______,    KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_PPLS,    _______,
+            _______,    _______,    KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_PEQL,    _______,
+                                    KC_KP_0,    KC_KP_DOT,  KC_PCMM,    KC_PENT,    _______,
             _______,    _______,
             _______,
             _______,    _______,    _______
@@ -188,87 +182,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______,
             _______,    _______,    _______
     ),
-};
-
-// See https://github.com/algernon/ergodox-layout/blob/master/keymap.c
-// When holding the SLASH or WHACK key, the ARRW layer activates while the key is held.
-// Tapping the key produces the / or \ key. Double-tapping it toggles the ARRW layer
-// on until a third tap.
-
-typedef struct {
-    bool layer_toggle;
-    bool sticky;
-} td_ta_state_t;
-
-static void slash_finished (qk_tap_dance_state_t *state, void *user_data) {
-    td_ta_state_t *td_ta = (td_ta_state_t *) user_data;
-
-    if (td_ta->sticky) {
-        td_ta->sticky = false;
-        td_ta->layer_toggle = false;
-        layer_off (ARRW);
-        return;
-    }
-
-    if (state->count == 1 && !state->pressed) {
-        register_code (KC_SLSH);
-        td_ta->sticky = false;
-        td_ta->layer_toggle = false;
-    } else {
-        td_ta->layer_toggle = true;
-        layer_on (ARRW);
-        td_ta->sticky = (state->count == 2);
-    }
-}
-
-static void slash_reset (qk_tap_dance_state_t *state, void *user_data) {
-    td_ta_state_t *td_ta = (td_ta_state_t *) user_data;
-
-    if (!td_ta->layer_toggle)
-        unregister_code (KC_SLSH);
-    if (!td_ta->sticky)
-        layer_off (ARRW);
-}
-
-static void whack_finished (qk_tap_dance_state_t *state, void *user_data) {
-    td_ta_state_t *td_ta = (td_ta_state_t *) user_data;
-
-    if (td_ta->sticky) {
-        td_ta->sticky = false;
-        td_ta->layer_toggle = false;
-        layer_off (ARRW);
-        return;
-    }
-
-    if (state->count == 1 && !state->pressed) {
-        register_code (KC_BSLS);
-        td_ta->sticky = false;
-        td_ta->layer_toggle = false;
-    } else {
-        td_ta->layer_toggle = true;
-        layer_on (ARRW);
-        td_ta->sticky = (state->count == 2);
-    }
-}
-
-static void whack_reset (qk_tap_dance_state_t *state, void *user_data) {
-    td_ta_state_t *td_ta = (td_ta_state_t *) user_data;
-
-    if (!td_ta->layer_toggle)
-        unregister_code (KC_BSLS);
-    if (!td_ta->sticky)
-        layer_off (ARRW);
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [SLASH] = {
-        .fn = { NULL, slash_finished, slash_reset },
-        .user_data = (void *)&((td_ta_state_t) { false, false })
-    },
-    [WHACK] = {
-        .fn = { NULL, whack_finished, whack_reset },
-        .user_data = (void *)&((td_ta_state_t) { false, false })
-    }
 };
 
 // Runs just one time when the keyboard initializes.
