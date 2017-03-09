a7e9704c4b513f753a4e30c6c77f31ea7516f080
Roben Kleene <robenk@gmail.com>
2017-03-08 22:06:45 -0500

    Allow special modifier keys to pass through

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 // Left Keyboard
-KC_EQL,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
-KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
-KC_LCTL,  KC_A,      KC_S,     KC_D,     KC_F,     KC_G,
-KC_LSFT,  KC_Z,      KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
-KC_NO,    KC_GRV,   KC_LCTL,  KC_LALT,  KC_LGUI,
+KC_EQL,   KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
+KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
+KC_LCTL,  KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
+KC_LSFT,  KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
+KC_NO,    KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
 
 // Left Cluster
 KC_LCTL,  MO(FCTN),
@@ -85,30 +85,31 @@ KC_PGDN,  KC_ENT,  KC_SPC
 [FCTN] = KEYMAP(
 
 // Left Keyboard
-RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_NO,
-KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+RESET,    KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_NO,
+KC_NO,    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,
+KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
+KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,
+KC_NO,    KC_NO,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Left Cluster
-KC_NO,  KC_TRNS,
+KC_TRNS,  KC_TRNS,
 KC_NO,
-KC_NO,  KC_NO,  KC_NO,
+KC_NO,    KC_NO,    KC_NO,
 
 // Right Keyboard
 KC_NO,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,  KC_F11,
 KC_VOLU,  KC_MNXT,  KC_NO,    KC_NO,    KC_UP,     KC_NO,   KC_NO,
 KC_MPLY,  KC_MUTE,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_NO,
-KC_VOLD,  KC_MPRV,  KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_NO,
-KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+KC_VOLD,  KC_MPRV,  KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_NO,    KC_NO,    KC_TRNS,
 
 // Right Cluster
-KC_TRNS,  KC_NO,
+KC_TRNS,  KC_TRNS,
 KC_NO,
-KC_NO,  KC_NO,  KC_NO
+KC_NO,    KC_NO,    KC_NO
 
 ),
+
 
 };
 
