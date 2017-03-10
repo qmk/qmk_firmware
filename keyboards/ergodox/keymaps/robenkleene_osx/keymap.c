eee18bcacdfccf58bd41db0706f30d9ddd399f40
Roben Kleene <robenk@gmail.com>
2017-03-09 21:22:21 -0500

    Add left hand side function key

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 // Left Keyboard
-KC_EQL,   KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
-KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
-KC_LCTL,  KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
-KC_LSFT,  KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
-KC_NO,    KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
+KC_EQL,    KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
+KC_TAB,    KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
+KC_LCTL,   KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
+KC_LSFT,   KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
+MO(FCTN),  KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
 
 // Left Cluster
 KC_LCTL,  MO(FCTN),
@@ -89,7 +89,7 @@ RESET,    KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_NO,
 KC_NO,    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,
 KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,
 KC_TRNS,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,
-KC_NO,    KC_NO,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,    KC_NO,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Left Cluster
 KC_TRNS,  KC_TRNS,
