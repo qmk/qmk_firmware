0521e82003ed9f46b58845f9ffb39ed818c9233c
Roben Kleene <robenk@gmail.com>
2017-03-09 21:10:26 -0500

    Use esc as thumb key

--- keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
+++ keyboards/ergodox/keymaps/robenkleene_osx/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   1  |   2  |   3  |   4  |   5  |  =   |           |   =  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |   {  |   Y  |   U  |   I  |   O  |   P  |   |    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | CTRL |  Fn  |       |  Fn  | CTRL |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | BS   | DEL  |------|       |------|  Ret   |Space |
+ *                                 | BS   | ESC  |------|       |------|  Ret   |Space |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 // Left Keyboard
-KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_EQL,
+KC_EQL,   KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
 KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
 KC_LCTL,  KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
 KC_LSFT,  KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_ESC,
@@ -43,10 +43,10 @@ KC_NO,    KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
 // Left Cluster
 KC_LCTL,  MO(FCTN),
 KC_HOME,
-KC_BSPC,  KC_DEL,  KC_END,
+KC_BSPC,  KC_ESC,  KC_END,
 
 // Right Keyboard
-KC_EQL,   KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,
+KC_NO,   KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,
 KC_LBRC,  KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSLS,
 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
 KC_RBRC,  KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,
