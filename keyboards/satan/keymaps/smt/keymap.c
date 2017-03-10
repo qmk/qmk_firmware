3fd9fdc3022a12a34da1bd8ad549ce435fc2a78f
Stephen Tudor <smt@smt.io>
2017-03-10 16:10:05 -0500

    Forsake spacefn in favor of left shift tapdance. Move media controls.

--- keyboards/satan/keymaps/smt/keymap.c
+++ keyboards/satan/keymaps/smt/keymap.c
@@ -24,10 +24,11 @@ enum planck_keycodes {
 #define XXXXXXX KC_NO
 
 // Custom macros
-#define CTL_ESC     CTL_T(KC_ESC)               // Tap for Esc, hold for Ctrl
-#define SFT_ENT     SFT_T(KC_ENT)               // Tap for Enter, hold for Shift
-#define HPR_TAB     ALL_T(KC_TAB)               // Tap for Tab, hold for Hyper (Super+Ctrl+Shift+Alt)
 #define ALT_GRV     ALT_T(KC_GRV)               // Tap for Backtick, hold for Alt
+#define CTL_ESC     CTL_T(KC_ESC)               // Tap for Esc, hold for Ctrl
+#define HPR_TAB     ALL_T(KC_TAB)               // Tap for Tab, hold for Hyper (Super+Ctrl+Shift+Alt)
+#define SFT_ENT     SFT_T(KC_ENT)               // Tap for Enter, hold for Shift
+#define SFT_RSE     TD(KC_LSFT, OS(_RAISE))     // Double-tap for RAISE one-shot, otherwise Left Shift
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _QWERTY: (Base Layer) Default Layer
@@ -40,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space/Fn         |Alt |Gui |Fn  |Ctrl |
+   * |Ctrl|Gui |Alt |         Space         |Alt |Gui |Fn  |Ctrl |
    * `-----------------------------------------------------------'
    */
 [_QWERTY] = KEYMAP_ANSI(
   F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
   HPR_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   CTL_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         SFT_ENT, \
-  KC_LCTL, KC_LGUI,KC_LALT,                LT(_RAISE, KC_SPC),                     KC_RALT,KC_RGUI,MO(_LOWER),KC_RCTL),
+  SFT_RSE,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         SFT_ENT, \
+  KC_LCTL, KC_LGUI,KC_LALT,                      KC_SPC,                           KC_RALT,KC_RGUI,MO(_LOWER),KC_RCTL),
 
   /* Keymap _COLEMAK: (Base Layer) Default Layer
    * ,-----------------------------------------------------------.
@@ -60,15 +61,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  K|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space/Fn         |Alt |Gui |Fn  |Ctrl |
+   * |Ctrl|Gui |Alt |         Space         |Alt |Gui |Fn  |Ctrl |
    * `-----------------------------------------------------------'
    */
 [_COLEMAK] = KEYMAP_ANSI(
   F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
   HPR_TAB, KC_Q,   KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,KC_LBRC, KC_RBRC,KC_BSLS, \
   CTL_ESC, KC_A,   KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,   KC_I,   KC_O   ,KC_QUOT,         KC_ENT,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         SFT_ENT, \
-  KC_LCTL, KC_LGUI,KC_LALT,                LT(_RAISE, KC_SPC),                     KC_RALT,KC_RGUI,MO(_LOWER),KC_RCTL),
+  SFT_RSE,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         SFT_ENT, \
+  KC_LCTL, KC_LGUI,KC_LALT,                      KC_SPC,                           KC_RALT,KC_RGUI,MO(_LOWER),KC_RCTL),
 
   /* Keymap _DVORAK: (Base Layer) Default Layer
    * ,-----------------------------------------------------------.
@@ -80,35 +81,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Shift   |  ;|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|ShiftEnter|
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space/Fn         |Alt |Gui |Fn  |Ctrl |
+   * |Ctrl|Gui |Alt |         Space         |Alt |Gui |Fn  |Ctrl |
    * `-----------------------------------------------------------'
    */
 [_DVORAK] = KEYMAP_ANSI(
   F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_LBRC, KC_RBRC,KC_BSPC, \
   HPR_TAB, KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLSH, KC_EQL, KC_BSLS, \
   CTL_ESC, KC_A,   KC_O,   KC_E,   KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   KC_MINS,         KC_ENT,  \
-  KC_LSFT,         KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,            SFT_ENT, \
-  KC_LCTL, KC_LGUI,KC_LALT,                LT(_RAISE, KC_SPC),                     KC_RALT,KC_RGUI,MO(_LOWER),KC_RCTL),
+  SFT_RSE,         KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,            SFT_ENT, \
+  KC_LCTL, KC_LGUI,KC_LALT,                      KC_SPC,                           KC_RALT,KC_RGUI,MO(_LOWER),KC_RCTL),
 
   /* Keymap _LOWER: Function Layer
    * ,-----------------------------------------------------------.
    * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete |
    * |-----------------------------------------------------------|
-   * |     |Hom| UP|End|   |   |   |   |   |   |   |Vo-|Vo+|     |
+   * |     |Hom| UP|End|   |   |   |   |   |   |   |BL-|BL+|BL   |
    * |-----------------------------------------------------------|
-   * |      |LFT| DN| RT|   |   |LFT| DN| UP| RT|   |Pg+|        |
+   * |      |LFT| DN| RT|   |   |LFT| DN| UP| RT|Vo+|Pg+|        |
    * |-----------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |   |   |Pg-|          |
+   * |        |   |   |Prv|Ply|Nxt|   |   |   |Vo-|Pg-|          |
    * |-----------------------------------------------------------|
-   * |    |    |Prev|         Play           |Next|    |    |    |
+   * |    |    |    |                        |    |    |    |    |
    * `-----------------------------------------------------------'
    */
 [_LOWER] = KEYMAP_ANSI(
   KC_GRV ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_DEL,  \
-  _______,KC_HOME,KC_UP  ,KC_END ,_______,_______,_______,_______,_______,_______,_______,KC_VOLD,KC_VOLU,_______, \
-  _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,KC_PGUP        ,_______, \
-  _______        ,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PGDN        ,_______, \
-  _______,_______,KC_MPRV                        ,KC_MPLY                        ,KC_MNXT,_______,_______,_______),
+  _______,KC_HOME,KC_UP  ,KC_END ,_______,_______,_______,_______,_______,_______,_______,BL_DEC ,BL_INC ,BL_TOGG, \
+  _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_VOLU,KC_PGUP        ,_______, \
+  KC_LSFT        ,_______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,_______,KC_VOLD,KC_PGDN        ,_______, \
+  _______,_______,_______                        ,_______                        ,_______,_______,_______,_______),
 
   /* Keymap _RAISE: Function Layer
    * ,-----------------------------------------------------------.
@@ -126,15 +127,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = KEYMAP_ANSI(
   #ifdef RGBLIGHT_ENABLE
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET  , \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,BL_DEC ,BL_INC ,BL_TOGG, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
   _______,_______,_______,_______,_______,_______,_______,QWERTY ,COLEMAK,DVORAK ,_______,_______        ,_______, \
-  _______        ,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,_______,_______        ,_______, \
-  _______,_______,_______                        ,_______                        ,_______,_______,_______,_______
+  KC_LSFT        ,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,_______,_______        ,_______, \
+  _______,_______,_______                        ,_______                        ,_______,_______,XXXXXXX,_______
   #else
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET  , \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,BL_DEC ,BL_INC ,BL_TOGG, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
   _______,_______,_______,_______,_______,_______,_______,QWERTY ,COLEMAK,DVORAK ,_______,_______        ,_______, \
-  _______        ,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______        ,_______, \
+  KC_LSFT        ,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______        ,_______, \
   _______,_______,_______                        ,_______                        ,_______,_______,XXXXXXX,_______
   #endif
   )
