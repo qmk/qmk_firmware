72eb975790d0bd7fd7352768cfa09a66b63a2a41
Nurahmadie <nurahmadie@gmail.com>
2017-03-11 20:52:52 +0700

    Fix layer stuck, and add numpad support

--- keyboards/handwired/kbod/config.h
+++ keyboards/handwired/kbod/config.h
@@ -159,4 +159,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+#define PREVENT_STUCK_MODIFIERS
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
 #endif
