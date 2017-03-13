3a1ee48cda5f185ebb3a4f989b389edf029992bb
Osamu Aoki <osamu@debian.org>
2017-03-13 17:11:05 +0000

    Typo fix

--- quantum/template/template.h
+++ quantum/template/template.h
@@ -20,7 +20,7 @@
 
 // This a shortcut to help you visually see your layout.
 // The following is an example using the Planck MIT layout
-// The first section contains all of the arguements
+// The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
 #define KEYMAP( \
     k00, k01, k02, \
