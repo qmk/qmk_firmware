4b1d0c585977884faac28f1e7210bf528af46c9a
Dylan Khor <thekhord@gmail.com>
2017-03-16 17:36:31 +0000

    remove const

--- keyboards/planck/keymaps/khord/keymap.c
+++ keyboards/planck/keymaps/khord/keymap.c
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-const qk_tap_dance_action_t tape_dance_actions[] = {
+qk_tap_dance_action_t tape_dance_actions[] = {
   [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
 };
 
