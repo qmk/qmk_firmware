3c2c489768cf77cd5573a39305198c4c4b2027f1
jprMesh <mrachhh@gmail.com>
2017-03-17 09:11:20 -0400

    Atreus52- Fixed spacing and changed to KC_KEYMAP macro.

--- keyboards/atreus62/keymaps/atreus52/keymap.c
+++ keyboards/atreus62/keymaps/atreus52/keymap.c
@@ -6,21 +6,21 @@
 #define RAISE 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[QWERTY] = KEYMAP(
-	NO, 1,    2,    3,    4,    5,                6,    7,    8,    9,    0,    NO, \
-	NO, Q,    W,    E,    R,    T,                Y,    U,    I,    O,    P,    NO, \
-	NO, A,    S,    D,    F,    G,                H,    J,    K,    L,    SCLN, NO, \
-	NO, Z,    X,    C,    V,    B,                N,    M,    COMM, DOT,  SLSH, NO, \
-	NO, ESC,  TAB,  LCTL, FN0,  FN10, BSPC, ENT,  SPC,  LALT, LGUI, LEFT, RGHT, NO  ),
+[QWERTY] = KC_KEYMAP(
+    NO, 1,    2,    3,    4,    5,                6,    7,    8,    9,    0,    NO, \
+    NO, Q,    W,    E,    R,    T,                Y,    U,    I,    O,    P,    NO, \
+    NO, A,    S,    D,    F,    G,                H,    J,    K,    L,    SCLN, NO, \
+    NO, Z,    X,    C,    V,    B,                N,    M,    COMM, DOT,  SLSH, NO, \
+    NO, ESC,  TAB,  LCTL, FN0,  FN10, BSPC, ENT,  SPC,  LALT, LGUI, LEFT, RGHT, NO  ),
 
-[DVORAK] = KEYMAP(
-	NO, 1,    2,    3,    4,    5,                6,    7,    8,    9,    0,    NO, \
-	NO, QUOT, COMM, DOT,  P,    Y,                F,    G,    C,    R,    L,    NO, \
-	NO, A,    O,    E,    U,    I,                D,    H,    T,    N,    S,    NO, \
-	NO, SCLN, Q,    J,    K,    X,                B,    M,    W,    V,    Z,    NO, \
-	NO, ESC,  TAB,  LCTL, FN0,  FN10, BSPC, ENT,  SPC,  LALT, LGUI, LEFT, RGHT, NO  ),
+[DVORAK] = KC_KEYMAP(
+    NO, 1,    2,    3,    4,    5,                6,    7,    8,    9,    0,    NO, \
+    NO, QUOT, COMM, DOT,  P,    Y,                F,    G,    C,    R,    L,    NO, \
+    NO, A,    O,    E,    U,    I,                D,    H,    T,    N,    S,    NO, \
+    NO, SCLN, Q,    J,    K,    X,                B,    M,    W,    V,    Z,    NO, \
+    NO, ESC,  TAB,  LCTL, FN0,  FN10, BSPC, ENT,  SPC,  LALT, LGUI, LEFT, RGHT, NO  ),
 
-[RAISE] = KEYMAP(
+[RAISE] = KC_KEYMAP(
     NO, ESC,  TRNS, MPRV, MPLY, MNXT,             TRNS, MUTE, VOLD, VOLU, DEL,  NO, \
     NO, TRNS, TRNS, GRV,  FN13, FN14,             BSLS, FN16, FN18, LBRC, QUOT, NO, \
     NO, FN12, MINS, FN11, EQL,  FN15,             SLSH, LEFT, DOWN, UP,   RGHT, NO, \
@@ -46,18 +46,18 @@ const uint16_t PROGMEM fn_actions[] = {
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-	// MACRODOWN only works in this function
-	switch (id) {
-	case 0:
-		if (record->event.pressed) {
-			register_code(KC_RSFT);
-		}
-		else {
-			unregister_code(KC_RSFT);
-		}
-		break;
-	}
-	return MACRO_NONE;
+    // MACRODOWN only works in this function
+    switch (id) {
+    case 0:
+        if (record->event.pressed) {
+            register_code(KC_RSFT);
+        }
+        else {
+            unregister_code(KC_RSFT);
+        }
+        break;
+    }
+    return MACRO_NONE;
 };
 
 static uint8_t qw_dv_swap_state = 0;
