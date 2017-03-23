a9d686cbef1ea1bf4e5f8ffb991f44973e282ee0
Danny Nguyen <danny@80pct.com>
2017-03-23 14:46:58 -0400

    Add RGB support

--- keyboards/atreus50/config.h
+++ keyboards/atreus50/config.h
@@ -61,7 +61,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN C6
-#define RGBLIGHT_TIMER
+#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 12     // Number of LEDs
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
