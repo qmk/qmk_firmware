5fbaf31dc26a7bb27eb276ec419255e92b293d60
Fred Sundvik <fsundvik@gmail.com>
2017-04-02 02:29:50 +0300

    Brightness for Ergodox Infinity emulated LEDs

--- keyboards/ergodox/infinity/config.h
+++ keyboards/ergodox/infinity/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* number of backlight levels */
 #define BACKLIGHT_LEVELS 3
 
-#define LED_BRIGHTNESS_LO       15
+#define LED_BRIGHTNESS_LO       100
 #define LED_BRIGHTNESS_HI       255
 
 /* define if matrix has ghost */
