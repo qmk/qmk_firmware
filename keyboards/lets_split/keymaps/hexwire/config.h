ed9c18e86650496627e3a94c29c370e76a6e8313
Danny Nguyen <danny@80pct.com>
2017-04-03 14:00:33 -0400

    Remove flip-half from keymap

--- keyboards/lets_split/keymaps/hexwire/config.h
+++ keyboards/lets_split/keymaps/hexwire/config.h
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 
 #define TAPPING_TERM 150
-#define FLIP_HALF
 
 #ifdef SUBPROJECT_rev1
     #include "../../rev1/config.h"
