66abffa26bd4243451d8b1ef808cdafb13edddf4
Stephan Bösebeck <sb@caluga.de>
2017-04-04 16:14:10 +0200

    playing a bit

---
+++ keyboards/ergodox/keymaps/osx_de_experimental/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+/* using UK layout for space-cadet-shift */
+#define LSPO_KEY KC_8
+#define RSPC_KEY KC_9
+
+#define LEADER_TIMEOUT 400 // leader key sequence timeout in millis
+
+#define TAPPING_TERM 200
+
+#endif
+
+
