454812034a68305c1fa1fc69a1af0b9f79abb7de
Joel Petersen <tibcmhhn@gmail.com>
2017-04-05 17:51:21 -0700

    make led_controller_init available in header, change led const to enum for easier expansion of led page maps

--- keyboards/infinity60/led_controller.c
+++ keyboards/infinity60/led_controller.c
@@ -43,16 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *Unused in Alphabet Layout
 */
 
-/* WF LED MAP
-    - digits mean "row" and "col", i.e. 45 means C4-5 in the IS31 datasheet, matrix A
-
-  11 12 13 14 15 16 17 18 21 22 23 24 25 26 27  28
-   31 32 33 34 35 36 37 38 41 42 43 44 45  46   47
-   48 51 52 53 54 55 56 57 58 61 62 63 64   65  66
-    67 68 71 72 73 74 75 76 77 78 81 82  83  84 85
-  86  87  88       91        92  93 (94)  95 96 97
-*/
-
 /*
   each page has 0xB4 bytes
   0 - 0x11: LED control (on/off):
