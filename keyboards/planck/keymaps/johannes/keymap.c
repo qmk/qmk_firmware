835adb7848f4a2d6feb848bdfab1e9990f5f430a
Johannes Larsson <developer.johannes.larsson@gmail.com>
2017-04-12 23:11:51 +0200

    colemak layout maybe

--- keyboards/planck/keymaps/johannes/keymap.c
+++ keyboards/planck/keymaps/johannes/keymap.c
@@ -11,22 +11,40 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/* Colemak
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[0] = {
+  {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
+  {KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
+  {KC_LCTRL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {TG(1), XXXXXXX, XXXXXXX, KC_LGUI, KC_LSHIFT,   KC_SPC,  M(2),   M(1),   KC_LALT, KC_RALT, XXXXXXX,   M(3)}
+},
+
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Ctrl |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | super|shift | Space|Raise | lower|  alt | altgr|      |fn    |
  * `-----------------------------------------------------------------------------------'
  */
-[0] = {
+[1] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LCTRL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, KC_LSHIFT,    KC_SPC,  M(2),   M(1),   KC_LALT, KC_RALT, XXXXXXX,   M(3)}
+  {_______, XXXXXXX, XXXXXXX, KC_LGUI, KC_LSHIFT,    KC_SPC,  M(2),   M(1),   KC_LALT, KC_RALT, XXXXXXX,   M(3)}
 },
 
 /* Lower
@@ -40,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Reset |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[1] = {
+[2] = {
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-  {_______, _______,   KC_LCBR,   KC_RCBR,   KC_PLUS,   _______,   _______,   M(4), M(5), M(6), KC_UNDS, _______},
+  {_______, _______,   KC_LCBR,   KC_RCBR,   KC_PLUS,   _______,   _______,   RALT(KC_P), RALT(KC_Q), RALT(KC_P), KC_UNDS, _______},
   {_______, _______,   _______,   _______,   _______,  _______,  _______,_______, _______,_______, KC_BSLS, _______},
   {_______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
@@ -58,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[2] = {
+[3] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
   {_______,  _______,   KC_LBRC,   KC_RBRC,   KC_EQL,   _______,   KC_LEFT,   KC_DOWN, KC_UP,  KC_RIGHT, KC_MINS, _______},
   {_______, _______,   _______,   _______,   _______,  _______,  _______,  _______, _______, _______, KC_PIPE, _______},
@@ -66,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 
 // Fn-layer
-[3] = {
+[4] = {
   {KC_F1,  KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12},
   {_______,  _______,   _______,   _______,   _______,   _______,   _______,   _______, _______,  _______, _______, _______},
   {_______, _______,   _______,   _______,   _______,  _______,  _______,  _______, _______, _______, _______, _______},
@@ -84,23 +102,23 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
   switch(id) {
     case 1:
       if (record->event.pressed) {
-        layer_on(1);
-      } else {
-        layer_off(1);
-      }
-      break;
-    case 2:
-      if (record->event.pressed) {
         layer_on(2);
       } else {
         layer_off(2);
       }
       break;
-	  case 3:
+    case 2:
       if (record->event.pressed) {
         layer_on(3);
       } else {
         layer_off(3);
+      }
+      break;
+	  case 3:
+      if (record->event.pressed) {
+        layer_on(4);
+      } else {
+        layer_off(4);
       }
       break;
     case 4: // å
