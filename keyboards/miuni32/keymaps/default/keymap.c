5c78ed0a62b42a3a4b5a7f30c2c8456089d38e81
htang156 <h.tang.156@gmail.com>
2017-04-16 13:40:03 -0700

    Added keymap commets to default layout.

--- keyboards/miuni32/keymaps/default/keymap.c
+++ keyboards/miuni32/keymaps/default/keymap.c
@@ -1,21 +1,57 @@
 #include "miuni32.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Level 0: Default Layer
+     * ,---------------------------------------------------------------------------------------.
+     * |   Q   |   W   |   E   |   R   |   T   |   Y   |   U   |   I   |   O   |   P   |  BSP  |
+     * |---------------------------------------------------------------------------------------|
+     * |   A   |   S   |   D   |   F   |   G   |   H   |   J   |   K   |   L   |  ENT  |LT(1|,)|
+     * |---------------------------------------------------------------------------------------|
+     * |LT(3|Z)|   X   |   C   |   V   |   NO  |  SPC  |   B   |   N   |   M   |  RSFT |LT(2|.)|
+     * |---------------------------------------------------------------------------------------|
+     */
 	[0] ={
 			{KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC}, 
 			{KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT, LT(1, KC_COMMA)}, 
 			{LT(3, KC_Z), KC_X, KC_C, KC_V, KC_NO, KC_SPC, KC_B, KC_N, KC_M, KC_RSFT, LT(2, KC_DOT)}
 	},
+	/* Level 1: Numbers Layer
+     * ,---------------------------------------------------------------------------------------.
+     * |  ESC  |   7   |   8   |   9   |   /   |   *   |   .   |   ,   |   (   |   )   |  BSP  |
+     * |---------------------------------------------------------------------------------------|
+     * |  TAB  |   4   |   5   |   6   |   -   |   +   |   =   |   UP  |  HOME |  PGUP |  TRNS |
+     * |---------------------------------------------------------------------------------------|
+     * |  LATL |   1   |   2   |   3   |   0   |   NO  |  LEFT |  DOWN |  RGHT |  PGDN |  RALT |
+     * |---------------------------------------------------------------------------------------|
+     */
 	[1] ={
 			{KC_ESC, KC_7, KC_8, KC_9, KC_SLSH, KC_ASTR, KC_DOT, KC_COMM, KC_LPRN, KC_RPRN, KC_BSPC}, 
 			{KC_TAB, KC_4, KC_5, KC_6, KC_MINS, KC_PLUS, KC_EQL, KC_UP, KC_HOME, KC_PGUP, KC_TRNS},
 			{KC_LALT, KC_1, KC_2, KC_3, KC_0, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_RALT}
 	},
+	/* Level 2: Symbols Layer
+     * ,---------------------------------------------------------------------------------------.
+     * |   !   |   @   |   #   |   $   |   %   |   ^   |   &   |   *   |   -   |   +   |   =   |
+     * |---------------------------------------------------------------------------------------|
+     * |   {   |   }   |   [   |   ]   |  TRNS |  TRNS |   \   |   ;   |   :   |   `   |   ?   |
+     * |---------------------------------------------------------------------------------------|
+     * |  LSFT |  LCTL |   L   |   T   |  TRNS |  TAB  |   N   |  TRNS |  TRNS |  RCTL |  TRNS |
+     * |---------------------------------------------------------------------------------------|
+     */
 	[2] ={
 			{KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_EQL},
 			{KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_BSLS, KC_SCLN, KC_COLN, KC_GRV, KC_QUES},
 			{KC_LSFT, KC_LCTL, KC_L, KC_T, KC_TRNS, KC_TAB, KC_N, KC_TRNS, KC_TRNS, KC_RCTL, KC_TRNS}
 	},
+	/* Level 3: RGB Layer
+     * ,---------------------------------------------------------------------------------------.
+     * | RESET |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |
+     * |---------------------------------------------------------------------------------------|
+     * |RGB_TOG|RGB_MOD|RGB_HUI|RGB_HUD|   NO  |RGB_SAI|RGB_SAD|RGB_VAI|RGB_VAD|  TRNS |  TRNS |
+     * |---------------------------------------------------------------------------------------|
+     * |  TRNS |  TRNS |  TRNS |  TRNS |   NO  |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |
+     * |---------------------------------------------------------------------------------------|
+     */
 	[3] ={
 			{RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}, 
 			{RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, KC_NO, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS},
