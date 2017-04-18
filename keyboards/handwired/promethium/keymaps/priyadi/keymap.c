3cbb904a6a5ad756930ddf6ef39273484293c0e8
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2017-04-19 02:55:19 +0700

    Fix Planck compatibility

--- keyboards/handwired/promethium/keymaps/priyadi/keymap.c
+++ keyboards/handwired/promethium/keymaps/priyadi/keymap.c
@@ -924,7 +924,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void persistant_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
+#ifdef RGBSPS_ENABLE
   led_set_default_layer_indicator();
+#endif
 }
 
 #ifdef DOUBLESPACE_LAYER_ENABLE
