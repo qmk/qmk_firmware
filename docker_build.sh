1eb8499ec440169a542403ba98fae74e1404709f
geoemon2k <keiji.ue@gmail.com>
2017-04-21 10:12:44 +0900

    add docker_build.sh for building on mac

---
+++ docker_build.sh
@@ -0,0 +1,1 @@
+docker run -e keymap=ijikeman-osx -e subproject=ez -e keyboard=ergodox --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
--- keyboards/ergodox/keymaps/ijikeman-osx/keymap.c
+++ keyboards/ergodox/keymaps/ijikeman-osx/keymap.c
@@ -28,17 +28,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \|   |
  * |--------+------+------+------+------+------|      |           |  ]}  |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '"   |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:   | Enter |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |*(LANG)|           |  [{  |   N  |   M  |   ,< |   .> |   /? | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |  [{  |   N  |   M  |   ,< |   .> |   /? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      | Alt  |  Cmd | Fn(1)|                         |  BS  | Fn(2)  | Alt |  \|  |      |
+ *   |  Esc |      | Alt  |  Cmd | Fn(1)|                         |  BS  |  Cmd |   \| |  '"  |      |
  *   `----------------------------------'                         `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | PGDN | PGUP |       | LEFT | RIGHT|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | UP   |        |      |
- *                                 | Space| ESC  |------|       |------|        |Enter |
+ *                                 | Spc  |*(LANG)|-----|       |------|        |  Spc |
  *                                 |      |      | End  |       | DOWN |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -48,21 +48,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         M_TESC,         KC_1,         KC_2,       KC_3,       KC_4,       KC_5,     KC_6,
         KC_TAB,         KC_Q,         KC_W,       KC_E,       KC_R,       KC_T,     KC_NO,
         KC_LCTL,        KC_A,         KC_S,       KC_D,       KC_F,       KC_G,
-        KC_LSFT,        KC_Z,         KC_X,       KC_C,       KC_V,       KC_B,     M_TLANG,
-        KC_NO,          KC_NO,        KC_LALT,    KC_LGUI,    LT(1,KC_NO),
+        KC_LSFT,        KC_Z,         KC_X,       KC_C,       KC_V,       KC_B,     KC_NO,
+        KC_ESC,         KC_NO,        KC_LALT,    KC_LGUI,   LT(1,KC_NO),
                                                   KC_PGDN,    KC_PGUP,
                                                               KC_HOME,
-                                      KC_SPC,     KC_ESC,     KC_END,
+                                      KC_SPC,     M_TLANG,     KC_END,
 
         // right hand
         KC_6,           KC_7,         KC_8,       KC_9,       KC_0,        KC_MINS,   KC_EQL,
         KC_RBRC,        KC_Y,         KC_U,       KC_I,       KC_O,        KC_P,      KC_BSLS,
-                        KC_H,         KC_J,       KC_K,       KC_L,        KC_SCLN,   KC_QUOT,
+                        KC_H,         KC_J,       KC_K,       KC_L,        KC_SCLN,   KC_ENT,
         KC_LBRC,        KC_N,         KC_M,       KC_COMM,    KC_DOT,      KC_SLSH,   KC_RSFT,
-        KC_BSPC,        LT(2,KC_NO),  KC_RALT,    KC_BSLS,    KC_NO,
+        KC_BSPC,        KC_RGUI,      KC_BSLS,    KC_QUOT,    KC_NO,
         KC_LEFT,        KC_RIGHT,
         KC_UP,
-        KC_DOWN,        KC_NO,      KC_ENT
+        KC_DOWN,        KC_NO,      KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  *  |        |      |      |      |      |      |      |           |      |      | CP   | CUT  | PST  |      |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |       |      |      |      | Fn(1)|                                       | DEL  |      |      |      |      |
+ *   |       |      |      |      | Fn(1)|                                       |  DEL |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
