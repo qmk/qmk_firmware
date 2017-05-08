eeaade2dc166fb620d679b603fe6e4c6532dc996
jpetermans <tibcmhhm@gmail.com>
2017-05-08 14:35:08 -0700

    CLeaned out debug code

--- keyboards/infinity60/led.c
+++ keyboards/infinity60/led.c
@@ -29,26 +29,22 @@ void led_set(uint8_t usb_led) {
     msg_t msg;
 
     if (usb_led & (1<<USB_LED_NUM_LOCK)) {
-        // signal the LED control thread
         chSysUnconditionalLock();
         msg=(TOGGLE_NUM_LOCK << 8) | 1;
         chMBPostI(&led_mailbox, msg);
         chSysUnconditionalUnlock();
     } else {
-        // signal the LED control thread
         chSysUnconditionalLock();
         msg=(TOGGLE_NUM_LOCK << 8) | 0;
         chMBPostI(&led_mailbox, msg);
         chSysUnconditionalUnlock();
     }
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // signal the LED control thread
         chSysUnconditionalLock();
         msg=(TOGGLE_CAPS_LOCK << 8) | 1;
         chMBPostI(&led_mailbox, msg);
         chSysUnconditionalUnlock();
     } else {
-        // signal the LED control thread
         chSysUnconditionalLock();
         msg=(TOGGLE_CAPS_LOCK << 8) | 0;
         chMBPostI(&led_mailbox, msg);
