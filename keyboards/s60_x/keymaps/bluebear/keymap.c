60c068d72edcaf95f2c5fdb412e0e5fbe6a13399
Ante Laurijssen <antelaurijssen@gmail.com>
2017-05-21 08:40:42 -0400

    Continuing work on keymap.c

--- keyboards/s60_x/keymaps/bluebear/keymap.c
+++ keyboards/s60_x/keymaps/bluebear/keymap.c
@@ -4,62 +4,140 @@
 
 enum keyboard_layers {
   BASE = 0, //Base Layer
-  ARROW,    //Arrow/FN Layer
+  ARROWFN,  //Arrow/FN Layer
   MOUSE,    //Mouse Layer
   MIDI,     //Midi Layer
+  MORSE,    //Morse Code Layer
 };
 
+// Midi Chords
 
-// Midi Chord Keycodes
-/*
-bool midi_chord(uint8_t mode, uint16_t root);
-
-enum midi_chord_modes {
+enum midi_chord_modes { //ACTION_FUNCTION id
   major = 0,
   minor,
 };
-*/
+
+enum midi_chord_root { //ACTION_FUNCTION opt
+  C = 0,
+  Cs,
+  Db = Cs,
+  D,
+  Ds,
+  Eb = Ds,
+  E,
+  F,
+  Fs,
+  Gb = Fs,
+  G,
+  Gs,
+  Ab = Gs,
+  A,
+  As,
+  Bb = As,
+  B,
+};
+  
 enum midi_chord_keycodes {
 
   //Major Chords
 
-  MI_CH_C = SAFE_RANGE,
-  MI_CH_Cs,
+  MI_CH_C = ACTION_FUNCTION(0, 0),
+  MI_CH_Cs = ACTION_FUNCTION(0, 1),
   MI_CH_Db = MI_CH_Cs,
-  MI_CH_D,
-  MI_CH_Ds,
+  MI_CH_D = ACTION_FUNCTION(0, 2),
+  MI_CH_Ds = ACTION_FUNCTION(0, 3),
   MI_CH_Eb = MI_CH_Ds,
-  MI_CH_E,
-  MI_CH_F,
-  MI_CH_Fs,
+  MI_CH_E = ACTION_FUNCTION(0, 4),
+  MI_CH_F = ACTION_FUNCTION(0, 5),
+  MI_CH_Fs = ACTION_FUNCTION(0, 6),
   MI_CH_Gb = MI_CH_Fs,
-  MI_CH_G,
-  MI_CH_Gs,
+  MI_CH_G = ACTION_FUNCTION(0, 7),
+  MI_CH_Gs = ACTION_FUNCTION(0, 8),
   MI_CH_Ab = MI_CH_Gs,
-  MI_CH_A,
-  MI_CH_As,
+  MI_CH_A = ACTION_FUNCTION(0, 9),
+  MI_CH_As = ACTION_FUNCTION(0, 10),
   MI_CH_Bb = MI_CH_As,
-  MI_CH_B,
+  MI_CH_B = ACTION_FUNCTION(0, 11),
 
   //Minor Chords
 
-  MI_CH_Cm,
-  MI_CH_Csm,
+  MI_CH_Cm = ACTION_FUNCTION(1, 0),
+  MI_CH_Csm = ACTION_FUNCTION(1, 1),
   MI_CH_Dbm = MI_CH_Csm,
-  MI_CH_Dm,
-  MI_CH_Dsm,
+  MI_CH_Dm = ACTION_FUNCTION(1, 2),
+  MI_CH_Dsm = ACTION_FUNCTION(1, 3),
   MI_CH_Ebm = MI_CH_Dsm,
-  MI_CH_Em,
-  MI_CH_Fm,
-  MI_CH_Fsm,
+  MI_CH_Em = ACTION_FUNCTION(1, 4),
+  MI_CH_Fm = ACTION_FUNCTION(1, 5),
+  MI_CH_Fsm = ACTION_FUNCTION(1, 6),
   MI_CH_Gbm = MI_CH_Fsm,
-  MI_CH_Gm,
-  MI_CH_Gsm,
+  MI_CH_Gm = ACTION_FUNCTION(1, 7),
+  MI_CH_Gsm= ACTION_FUNCTION(1, 8),
   MI_CH_Abm = MI_CH_Gsm,
-  MI_CH_Am,
-  MI_CH_Asm,
+  MI_CH_Am = ACTION_FUNCTION(1, 9),
+  MI_CH_Asm = ACTION_FUNCTION(1, 10),
   MI_CH_Bbm = MI_CH_Asm,
-  MI_CH_Bm,
+  MI_CH_Bm = ACTION_FUNCTION(1, 11),
+};
+
+//Morse Code Macro Keys
+
+enum morse_macros {
+  MC_0 = M(0),
+  MC_1 = M(1),
+  MC_2 = M(2),
+  MC_3 = M(3),
+  MC_4 = M(4),
+  MC_5 = M(5),
+  MC_6 = M(6),
+  MC_7 = M(7),
+  MC_8 = M(8),
+  MC_9 = M(9),
+  MC_A = M(10),
+  MC_B = M(11),
+  MC_C = M(12),
+  MC_D = M(13),
+  MC_E = M(14),
+  MC_F = M(15),
+  MC_G = M(16),
+  MC_H = M(17),
+  MC_I = M(18),
+  MC_J = M(19),
+  MC_K = M(20),
+  MC_L = M(21),
+  MC_M = M(22),
+  MC_N = M(23),
+  MC_O = M(24),
+  MC_P = M(25),
+  MC_Q = M(26),
+  MC_R = M(27),
+  MC_S = M(28),
+  MC_T = M(29),
+  MC_U = M(30),
+  MC_V = M(31),
+  MC_W = M(32),
+  MC_X = M(33),
+  MC_Y = M(34),
+  MC_Z = M(35),
+  MC_DOT = M(36),
+  MC_COMM = M(37),
+  MC_QUES = M(38),
+  MC_APOS = M(39),
+  MC_EXCL = M(40),
+  MC_SLSH = M(41),
+  MC_PARO = M(42),
+  MC_PARC = M(43),
+  MC_AMP = M(44),
+  MC_COL = M(45),
+  MC_SCLN = M(46),
+  MC_EQL = M(47),
+  MC_PLUS = M(48),
+  MC_MINS = M(49),
+  MC_QUOT = M(50),
+  MC_DOLR = M(51),
+  MC_AT = M(52),
+  MC_UNDS = M(53),
+  MC_SPACE = M(54),
 };
 
 //Keymaps
@@ -89,7 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 				  MO(2), KC_LGUI, KC_LALT,  LT(1, KC_SPACE),  ALGR_T(KC_APP),  KC_RGUI,  OSM(MOD_LCTL | MOD_LSFT),  OSM(MOD_LCTL | MOD_LALT)
 				  ),
   
-  /* 1: Arrow/Missing Keys Layer
+  /* 1: Arrow/FN Layer
 
 	 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
 	 │TG(3)│ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │     │     │
@@ -104,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
   */
 
-  [ARROW] = KEYMAP(
+  [ARROWFN] = KEYMAP(
 				   TG(3),  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,  KC_F11,  KC_F12,  KC_NO,  KC_NO,  \
 				   KC_CAPS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PGUP,  KC_UP,  KC_HOME,  KC_END,  KC_NO,  KC_INS,  KC_DEL,  \
 				   KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS,  \
@@ -159,12 +237,316 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 				  KC_NO,  KC_NO,  KC_NO,  MI_ALLOFF,  KC_NO, KC_NO,  KC_NO,  KC_NO
 				  ),
 
+  /* 4: Morse Code Layer
+	 
+	 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+	 │     │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │     │     │     │     │
+	 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+	 │     │  '  │  ,  │  .  │  P  │  Y  │  F  │  G  │  C  │  R  │  L  │  /  │  =  │BSPC │█████│
+	 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+	 │     │  A  │  O  │  E  │  U  │  I  │  D  │  H  │  T  │  N  │  S  │  -  │▒▒▒▒▒│ENTER│█████│
+	 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+	 │SHIFT│▒▒▒▒▒│  ;  │  Q  │  J  │  K  │  X  │  B  │  M  │  W  │  V  │  Z  │▒▒▒▒▒│SHIFT│▒▒▒▒▒│
+	 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+	 │     │     │     │█████│█████│█████│SPACE│█████│█████│█████│     │     │     │     │█████│
+	 └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+  */
+  
+  [MORSE] = KEYMAP(
+				  KC_NO,  MC_1,  MC_2,  MC_3,  MC_4,  MC_5,  MC_6,  MC_7,  MC_8,  MC_9,  MC_0,  KC_NO,  KC_NO,  KC_NO,  KC_NO, \
+				  KC_NO,  MC_APOS,  MC_COMM, MC_DOT,  MC_P,  MC_Y,  MC_F,  MC_G,  MC_C,  MC_R,  MC_L,  MC_SLSH,  MC_EQL,  KC_BSPC, \
+				  KC_NO,  MC_A,  MC_O,  MC_E,  MC_U,  MC_I,  MC_D,  MC_H,  MC_T,  MC_N,  MC_S,  MC_MINS,  KC_NO,  KC_ENT,	\
+				  KC_LSFT,  KC_NO,  MC_SCLN,  MC_Q,  MC_J,  MC_K,  MC_X,  MC_B,  MC_M,  MC_W,  MC_V,  MC_Z,  KC_NO,  KC_RSFT,  KC_NO, \
+				  KC_NO, KC_NO, KC_NO,   MC_SPACE,  KC_NO,  KC_NO,  KC_NO, KC_NO
+				  ),
+
+};
+
+// Morse Code Macros
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  switch(id) {
+  case 0: //Number 0
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(MINS), T(MINS), T(MINS), T(SPACE), END); //-----
+	}
+	break;
+  case 1: //Number 1
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(MINS), T(MINS), T(MINS), T(SPACE), END); //.----
+	}
+	break;
+  case 2: //Number 2
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(MINS), T(MINS), T(MINS), T(SPACE), END); //..---
+	}
+	break;
+  case 3: // Number 3
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(DOT), T(MINS), T(MINS), T(SPACE), END); //...--
+	}
+	break;
+  case 4: //Number 4
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(DOT), T(DOT), T(MINS), T(SPACE), END); //....-
+	}
+	break;
+  case 5: //Number 5
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(DOT), T(DOT), T(DOT), T(SPACE), END); //.....
+	}
+	break;
+  case 6: //Number 6
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(DOT), T(DOT), T(DOT), T(SPACE), END); //-....
+	}
+	break;
+  case 7: //Number 7
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(DOT), T(DOT), T(DOT), T(SPACE), END); //--...
+	}
+	break;
+  case 8: //Number 8
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(MINS), T(DOT), T(DOT), T(SPACE), END); //---..
+	}
+	break;
+  case 9: //Number 9
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(MINS), T(MINS), T(DOT), T(SPACE), END); //----.
+	}
+	break;
+  case 10: //Letter A
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(SPACE), END); //.-
+	}
+	break;
+  case 11: //Letter B
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(DOT), T(DOT), T(SPACE), END); //-...
+	}
+	break;
+  case 12: //Letter C
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(MINS), T(DOT), T(SPACE), END); //-.-.
+	}
+	break;
+  case 13: //Letter D
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(DOT), T(SPACE), END); //-..
+	}
+	break;
+  case 14: //Letter E
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(SPACE), END); //.
+	}
+	break;
+  case 15: //Letter F
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(MINS), T(DOT), T(SPACE), END); //..-.
+	}
+	break;
+  case 16: //Letter G
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(DOT), T(SPACE), END); //--.
+	}
+	break;
+  case 17: //Letter H
+	  if (record->event.pressed) {
+		return MACRO(T(DOT), T(DOT), T(DOT), T(DOT), T(SPACE), END); //....
+	  }
+	break; 
+  case 18: //Letter I
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(SPACE), END); //..
+	}
+	break;
+  case 19: //Letter J
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(MINS), T(MINS), T(SPACE), END); //.---
+	}
+	break;
+  case 20: //Letter K
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(MINS), T(SPACE), END); //-.-
+	}
+	break;
+  case 21: //Letter L
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(DOT), T(DOT), T(SPACE), END); //.-..
+	}
+	break;
+  case 22: //Letter M
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(SPACE), END); //--
+	}
+	break;
+  case 23: //Letter N
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(SPACE), END); //-.
+	}
+	break;
+  case 24: //Letter O
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(MINS), T(SPACE), END); //---
+	}
+	break;
+  case 25: //Letter P
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(MINS), T(DOT), T(SPACE), END); //.--.
+	}
+	break;
+  case 26: //Letter Q
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(DOT), T(MINS), T(SPACE), END); //--.-
+	}
+	break;
+  case 27: //Letter R
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(DOT), T(SPACE), END); //.-.
+	}
+	break;
+  case 28: //Letter S
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(DOT), T(SPACE), END); //...
+	}
+	break;
+  case 29: //Letter T
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(SPACE), END); //-
+	}
+	break;
+  case 30: //Letter U
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(MINS), T(SPACE), END); //..-
+	}
+	break;
+  case 31: //Letter V
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(DOT), T(MINS), T(SPACE), END); //...-
+	}
+	break;
+  case 32: //Letter W
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(MINS), T(SPACE), END); //.--
+	}
+	break;
+  case 33: //Letter X
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(DOT), T(MINS), T(SPACE), END); //-..-
+	}
+	break;
+  case 34: //Letter Y
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(MINS), T(MINS), T(SPACE), END); //-.--
+	}
+	break;
+  case 35: //Letter Z
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(DOT), T(DOT), T(SPACE), END); //--..
+	}
+	break;
+  case 36: //Punctuation .
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(DOT), T(MINS), T(DOT), T(MINS), T(SPACE), END); //.-.-.-
+	}
+	break;
+  case 37: //Punctuation ,
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(DOT), T(DOT), T(MINS), T(MINS), T(SPACE), END); //--..--
+	}
+	break;
+  case 38: //Punctuation ?
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(MINS), T(MINS), T(DOT), T(DOT), T(SPACE), END); //..--..
+	}
+	break;
+  case 39: //Punctuation '
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(MINS), T(MINS), T(MINS), T(DOT), T(SPACE), END); //-....-
+	}
+	break;
+  case 40: //Punctuation !
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(MINS), T(DOT), T(MINS), T(MINS), T(SPACE), END); //-.-.--
+	}
+	break;
+  case 41: //Punctuation /
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(DOT), T(MINS), T(DOT), T(SPACE), END); //-..-.
+	}
+	break;
+  case 42: //Punctuation (
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(MINS), T(MINS), T(DOT), T(SPACE), END); //-.--.
+	}
+	break;
+  case 43: //Punctuation )
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(MINS), T(MINS), T(DOT), T(MINS), T(SPACE), END); //-.--.-
+	}
+	break;
+  case 44: //Punctuation &
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(DOT), T(DOT), T(DOT), T(SPACE), END); //.-...
+	}
+	break;
+  case 45: //Punctuation :
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(MINS), T(MINS), T(DOT), T(DOT), T(DOT), T(SPACE), END); //---...
+	}
+	break;
+  case 46: //Punctuation ;
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(MINS), T(DOT), T(MINS), T(DOT), T(SPACE), END); //-.-.-.
+	}
+	break;
+  case 47: //Punctuation =
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(DOT), T(DOT), T(MINS), T(SPACE), END); //-...-
+	}
+	break;
+  case 48: //Punctuation +
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(DOT), T(MINS), T(DOT), T(SPACE), END); //.-.-.
+	}
+	break;
+  case 49: //Punctuation -
+	if (record->event.pressed) {
+	  return MACRO(T(MINS), T(DOT), T(DOT), T(DOT), T(DOT), T(MINS), T(SPACE), END); //-....-
+	}
+	break;
+  case 50: //Punctuation "
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(DOT), T(DOT), T(MINS), T(DOT), T(SPACE), END); //.-..-.
+	}
+	break;
+  case 51: //Punctuation $
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(DOT), T(MINS), T(DOT), T(DOT), T(MINS), T(SPACE), END); //...-..-
+	}
+	break;
+  case 52: //Punctuation @
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(MINS), T(MINS), T(DOT), T(MINS), T(DOT), T(SPACE), END); //.--.-.
+	}
+	break;		
+  case 53: //Punctuation _
+	if (record->event.pressed) {
+	  return MACRO(T(DOT), T(DOT), T(MINS), T(MINS), T(DOT), T(MINS), T(SPACE), END); //..--.-
+  case 54: //Morse Space
+	if (record->event.pressed) {
+	  return MACRO(T(BSLS), T(SPACE), END); //When pressed, this sends a slash followed by a space, making it easier to distinguish words in Morse
+	}
+	break;
+  }
+  return MACRO_NONE;
 };
 
 /*
 // Midi Chord Function
 
-bool midi_chord(uint8_t mode, uint16_t root) {
   switch (mode) {
   case 0:
 	uint8_t channel = midi_config.channel;
