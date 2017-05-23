eb13f95de16b1041584384b5f6d0f7a50e7a97cf
geoemon2k <keiji.ue@gmail.com>
2017-05-23 16:36:24 +0900

    fixed MAP text

--- keyboards/ergodox/keymaps/ijikeman-osx/keymap.c
+++ keyboards/ergodox/keymaps/ijikeman-osx/keymap.c
@@ -66,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,< |   .> |   /? |   '"   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  Esc |      | Alt  |  Cmd |LOWER(EI)|                        |RAISE(KANA)|  \|  |      |      |      |
- *   `-------------------------------------'                        `---------------------------------------'
+ *   |  Esc |LANG2 | Alt  |  Cmd |LOWER |                           |RAISE |  \|  |      |LANG1 |      |
+ *   `----------------------------------'                            `--------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+--------+------.
@@ -216,8 +216,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,< |   .> |   /? |   '"   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  Esc |      | Alt  |  Cmd | Spc  |                         |  BS  |  \|  |      |      |      |
- *   `----------------------------------'                         `----------------------------------'
+ *   |  Esc | LANG2| Alt  |  Cmd | Spc  |                         |  BS  |  \|  |      |      |LANG1 |      |
+ *   `----------------------------------'                         `----------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | PGDN | PGUP |       | LEFT | RIGHT|
  *                                 ,------|------|------|       |------+--------+------.
@@ -233,7 +233,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,       KC_E,       KC_R,       KC_T,     KC_NO,
         KC_LCTL,        KC_A,         KC_S,       KC_D,       KC_F,       KC_G,
         KC_LSFT,        KC_Z,         KC_X,       KC_C,       KC_V,       KC_B,     KC_NO,
-        KC_ESC,         KC_NO,        KC_LALT,    MT(KC_LGUI,KC_LANG2),   LT(5,KC_NO),
+        KC_ESC,         KC_LANG2,        KC_LALT,    KC_LGUI, LT(5,KC_NO),
                                                   KC_TRNS,    KC_TRNS,
                                                               KC_TRNS,
                                       KC_SPC,     LGUI(KC_SPC),     KC_TRNS,
@@ -243,7 +243,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,        KC_Y,         KC_U,       KC_I,       KC_O,        KC_P,    M_BRC,
                       KC_H,         KC_J,       KC_K,       KC_L,        KC_SCLN, KC_ENT,
         KC_NO,        KC_N,         KC_M,       KC_COMM,    KC_DOT,      KC_SLSH, KC_QUOT,
-        LT(6,KC_NO),  MT(KC_LEFT,KC_LANG1),      KC_DOWN,    KC_UP,      KC_RIGHT,
+        LT(6,KC_NO),  KC_LEFT,      KC_DOWN,    KC_UP,      KC_RIGHT,
         KC_TRNS,        KC_TRNS,
         KC_TRNS,
         TG(4),        KC_NO,        KC_BSPC
