9d52cea64f52ac49a7ecd027c99308c6fbc1d2f4
Nick Choi <iinikchi@gmail.com>
2017-05-24 12:36:36 -0400

    moved unicodemap rules to localer make

--- keyboards/frosty_flake/rules.mk
+++ keyboards/frosty_flake/rules.mk
@@ -63,7 +63,6 @@ NKRO_ENABLE ?= no            # USB Nkey Rollover
 BACKLIGHT_ENABLE ?= no       # Enable keyboard backlight functionality on B7 by default
 MIDI_ENABLE ?= no            # MIDI controls
 UNICODE_ENABLE ?= no         # Unicode
-UNICODEMAP_ENABLE ?= yes
 BLUETOOTH_ENABLE ?= no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE ?= no           # Audio output on port C6
 FAUXCLICKY_ENABLE ?= no      # Use buzzer to emulate clicky switches
