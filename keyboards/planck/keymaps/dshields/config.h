8ccb53882c8400084d7eba47f506fec4e4ece254
Daniel Shields <dshields@bats.com>
2017-05-25 11:08:17 +0100

    Tweak reported power consumption to work with iOS

--- keyboards/planck/keymaps/dshields/config.h
+++ keyboards/planck/keymaps/dshields/config.h
@@ -6,14 +6,15 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define ONESHOT_TAP_TOGGLE      2
-#define ONESHOT_TIMEOUT         500
+#define USB_MAX_POWER_CONSUMPTION 100
+#define ONESHOT_TAP_TOGGLE        2
+#define ONESHOT_TIMEOUT           500
 
-#define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    60
-#define MOUSEKEY_MAX_SPEED      7
-#define MOUSEKEY_WHEEL_DELAY    0
+#define MOUSEKEY_INTERVAL         20
+#define MOUSEKEY_DELAY            0
+#define MOUSEKEY_TIME_TO_MAX      40
+#define MOUSEKEY_MAX_SPEED        7
+#define MOUSEKEY_WHEEL_DELAY      0
 
 #define DM_PLAY DYN_MACRO_PLAY1
 #define DM_STRT DYN_REC_START1
