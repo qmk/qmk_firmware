9c582fc797c3fbddd753791aab279dd2ad27b9c4
Fred Sundvik <fsundvik@gmail.com>
2017-05-28 18:26:38 +0300

    Add install dependencies

--- util/wsl_install.sh
+++ util/wsl_install.sh
@@ -40,10 +40,6 @@ function install_drivers {
     popd > /dev/null
 }
 
-echo "Installing dependencies (p7zip-full, wget)"
-echo "This will ask for the sudo password"
-sudo apt-get install p7zip-full wget
-
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 if [[ $dir != /mnt/* ]];
@@ -55,6 +51,24 @@ then
 fi
 
 pushd "$dir"
+
+while true; do
+    echo
+    echo "Do you want to install all toolchain dependencies needed for compiling QMK?"
+    echo "This will run install_dependencies.sh, which calls apt-get upgrade."
+    echo "If you don't want that, you can install the dependencies manually."
+    read -p "(Y/N) " res
+    case $res in
+        [Yy]* ) sudo ./install_dependencies.sh; break;;
+        [Nn]* ) break;;
+        * ) echo "Invalid answer";;
+    esac
+done
+
+echo "Installing dependencies needed for the installation (p7zip-full, wget)"
+echo "This will ask for the sudo password"
+sudo apt-get install p7zip-full wget
+
 
 if [ ! -d "$download_dir" ]; then
     install_utils
