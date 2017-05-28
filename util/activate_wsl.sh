3d3999d8635d1c9ca5a0276545ce4bdc173aa9a4
Fred Sundvik <fsundvik@gmail.com>
2017-05-28 20:36:27 +0300

    Actually install flip instead of just extracting

--- util/activate_wsl.sh
+++ util/activate_wsl.sh
@@ -6,7 +6,7 @@ download_dir=$util_dir/wsl_downloaded
 export DFU_PROGRAMMER=$download_dir/dfu-programmer/dfu-programmer.exe
 export DFU_UTIL=$download_dir/dfu-util-0.9-win64/dfu-util.exe
 export TEENSY_LOADER_CLI=$download_dir/teensy_loader_cli.exe
-export BATCHISP=$download_dir/Flip/bin/batchisp.exe
+export BATCHISP=batchisp.exe
 
 
 
