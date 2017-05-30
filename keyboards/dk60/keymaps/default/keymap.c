e0beaecb36c923cdbe44a4da0f56194b1f01e48c
Damien Broqua <dbroqua@mousur.org>
2017-05-30 22:13:34 +0200

    - Added Capslock/Esc led

--- keyboards/dk60/keymaps/default/keymap.c
+++ keyboards/dk60/keymaps/default/keymap.c
@@ -1,8 +1,20 @@
 #include "dk60.h"
 #include "action_layer.h"
 
-#define _DEFAULT 0
-#define _FN 1
+enum planck_layers {
+  _QWERTY,
+  _FN,
+  _DVORAK,
+  _LOWER,
+  _RAISE,
+  _PLOVER,
+  _ADJUST
+};
+
+enum planck_keycodes {
+  QWERTY = SAFE_RANGE,
+  FN
+};
 
 // Fillers to make layering more clear
 #define ______ KC_TRNS
@@ -21,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *         |LGUI | LAlt  |               Space                | RAlt   |RGUI |
  *         `-----------------------------------------------------------------'
  */
-  [_DEFAULT] = KEYMAP( /* Basic QWERTY */
+  [_QWERTY] = KEYMAP( /* Basic QWERTY */
       KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,   KC_0,       KC_MINS,    KC_EQL, KC_BSLS,    KC_GRV, \
       KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,       KC_LBRC,    KC_RBRC,KC_BSPC,  \
       KC_LCTL,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,   \
-      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,    KC_RSFT,    MO(_FN), \
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,    KC_RSFT,    FN, \
                 KC_LGUI,KC_LALT,                KC_SPC,                          KC_RALT,KC_RGUI \
       ),
 
@@ -50,3 +62,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 ______, ______,                 ______,                 KC_MSTP, ______ \
       )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case FN:
+      if (record->event.pressed) {
+        layer_on(_FN);
+        dk60_esc_led_on();
+      } else {
+        layer_off(_FN);
+        dk60_esc_led_off();
+      }
+      return false;
+      break;
+  }
+  return true;
+}
