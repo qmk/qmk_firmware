7a433a7f3deea8e4d04a469e31e8b47047eacc99
Danny Nguyen <danny@80pct.com>
2017-05-30 16:52:02 -0400

    Update copyright info

--- keyboards/nyquist/keymaps/i2c/config.h
+++ keyboards/nyquist/keymaps/i2c/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2017 Danny Nguyen <danny@hexwire.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
