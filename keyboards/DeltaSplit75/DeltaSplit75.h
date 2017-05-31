758307447473852e400f9fd204f684b8b01b5f61
xyxjj <junjie0605@hotmail.com>
2017-05-31 14:34:28 +0800

    Add files via upload

---
+++ keyboards/DeltaSplit75/DeltaSplit75.h
@@ -0,0 +1,13 @@
+#ifndef DeltaSplit75_H
+#define DeltaSplit75_H
+
+#ifdef SUBPROJECT_V2
+    #include "V2.h"
+#endif
+#ifdef SUBPROJECT_ProtoSplit
+    #include "ProtoSplit.h"
+#endif
+
+#include "quantum.h"
+
+#endif
