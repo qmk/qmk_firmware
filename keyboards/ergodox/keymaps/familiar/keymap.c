6897b4c2ac04d302ad2df59c65b04b887898b964
Stick <nstickney@gmail.com>
2017-06-02 19:51:29 -0500

    moved alt out from under space

--- keyboards/ergodox/keymaps/familiar/keymap.c
+++ keyboards/ergodox/keymaps/familiar/keymap.c
@@ -28,13 +28,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  END |           | PGDN |------+------+------+------+------+--------|
  * | (/LSFT |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  UP  | )/RSFT |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- * | LCTRL  | LGUI | MENU |   '  |   "  |                                       |   [  |   ]  | LEFT | DOWN |  RIGHT |
+ * | LCTRL  | LGUI | MENU |   '  | LALT |                                       |[/RALT|   ]  | LEFT | DOWN |  RIGHT |
  * `------------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | VOL- | VOL+ |       |PRTSCR|      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 | SPC/ |SLASH/| MUTE |       |NUMLCK|WHACK/| SPC/ |
- *                                 | ALT  | MO(1)|------|       |------|MO(1) | ALT  |
+ *                                 |      |SLASH/| MUTE |       |NUMLCK|WHACK/|      |
+ *                                 | SPC  | MO(1)|------|       |------|MO(1) | SPC  |
  *                                 |      |      | LAY3 |       | LAY2 |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -44,19 +44,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,
         KC_GRV,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
         KC_LSPO,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_END,
-        KC_LCTL,    KC_LGUI,    KC_MENU,    KC_QUOT,    S(KC_QUOT),
+        KC_LCTL,    KC_LGUI,    KC_MENU,    KC_QUOT,    KC_LALT,
                                                                     KC_VOLD,    KC_VOLU,
                                                                                 KC_MUTE,
-                                            ALT_T(KC_SPC),  LT(ARRW,KC_SLSH),   TG(INTL),
+                                                KC_SPC,     LT(ARRW, KC_SLSH),  TG(INTL),
         // right hand
             KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
             KC_PGUP,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,
                         KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENTER,
             KC_PGDN,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_UP,      KC_RSPC,
-                                    KC_LBRC,    KC_RBRC,    KC_LEFT,    KC_DOWN,    KC_RGHT,
+                        MT(MOD_RALT, KC_LBRC),  KC_RBRC,    KC_LEFT,    KC_DOWN,    KC_RGHT,
             KC_PSCR,     _______,
             KC_NLCK,
-            TG(NUMP),   LT(ARRW,KC_BSLS),   ALT_T(KC_SPC)
+            TG(NUMP),   LT(ARRW, C_BSLS),   KC_SPC
     ),
 
 /* layer 1: International symbols, etc
