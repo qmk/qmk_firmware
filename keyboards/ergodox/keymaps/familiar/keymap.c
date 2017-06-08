4be4f61f191623486c4bfb342754553f443fb691
Stick <nstickney@gmail.com>
2017-06-08 08:59:30 -0500

    trying to fix LEDs

--- keyboards/ergodox/keymaps/familiar/keymap.c
+++ keyboards/ergodox/keymaps/familiar/keymap.c
@@ -242,26 +242,26 @@ void matrix_init_user(void) {
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
 
-    // uint8_t layer = biton32(layer_state);
-    //
-    // ergodox_board_led_off();
-    // ergodox_right_led_1_off();
-    // ergodox_right_led_2_off();
-    // ergodox_right_led_3_off();
-    // switch (layer) {
-    //     case INTL:
-    //     case INSF:
-    //         ergodox_right_led_1_on();
-    //         break;
-    //     case NUMP:
-    //         ergodox_right_led_2_on();
-    //         break;
-    //     case ARRW:
-    //         ergodox_right_led_3_on();
-    //         break;
-    //     default:
-    //         // none
-    //         break;
-    // }
+    uint8_t layer = biton32(layer_state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+        case INTL:
+        case INSF:
+            ergodox_right_led_1_on();
+            break;
+        case NUMP:
+            ergodox_right_led_2_on();
+            break;
+        case ARRW:
+            ergodox_right_led_3_on();
+            break;
+        default:
+            // none
+            break;
+    }
 
 };
