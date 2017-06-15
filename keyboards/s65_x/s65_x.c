422d02e2383e240ccb2d3fab6401410e5eafcc93
Geoff Lane <geoff@zorched.net>
2017-06-15 13:42:14 -0400

    LED init works with just pin declared

--- keyboards/s65_x/s65_x.c
+++ keyboards/s65_x/s65_x.c
@@ -5,7 +5,6 @@ void matrix_init_kb(void) {
   // put your keyboard start-up code here
   // runs once when the firmware starts up
   matrix_init_user();
-  led_init_ports();
 };
 
 void matrix_scan_kb(void) {
@@ -13,11 +12,6 @@ void matrix_scan_kb(void) {
   // runs every cycle (a lot)
   matrix_scan_user();
 };
-
-void led_init_ports(void) {
-  // * Set our LED pins as output
-  DDRB |= (1<<7);
-}
 
 void led_set_kb(uint8_t usb_led) {
   if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
