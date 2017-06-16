a86996a03fe35bf43998dd6ee039bff7b64cf635
Fred Sundvik <fsundvik@gmail.com>
2017-06-16 21:51:40 +0300

    Add empty timer functions

--- tmk_core/common/native/timer.c
+++ tmk_core/common/native/timer.c
@@ -14,4 +14,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "timer.h"
+
+void timer_init(void) {}
+
+void timer_clear(void) {}
+
+uint16_t timer_read(void) { return 0; }
+uint32_t timer_read32(void) { return 0; }
+uint16_t timer_elapsed(uint16_t last) { return 0; }
+uint32_t timer_elapsed32(uint32_t last) { return 0; }
 
