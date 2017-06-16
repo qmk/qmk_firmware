5910174984a6a692b32e8a4ec999f592b577709b
Fred Sundvik <fsundvik@gmail.com>
2017-06-16 21:34:01 +0300

    Try to build tmk_common

--- build_full_test.mk
+++ build_full_test.mk
@@ -12,3 +12,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+include tests/$(TEST)/rules.mk
+#include $(TMK_PATH)/protocol.mk
+
+TEST_PATH=tests/$(TEST)
+
+$(TEST)_SRC=$(TEST_PATH)/test.c $(TMK_COMMON_SRC)
+$(TEST)_DEFS=$(TMK_COMMON_DEFS)
