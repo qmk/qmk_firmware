617578399cf1a0fff70ceeb21d63a06524c801a3
Fred Sundvik <fsundvik@gmail.com>
2017-06-16 22:39:56 +0300

    Add keymap

--- tests/basic/test.c
+++ tests/basic/test.c
@@ -14,4 +14,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "quantum.h"
 
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = {
+	    {KC_A, KC_B},
+	    {KC_C, KC_D}
+	},
+};
