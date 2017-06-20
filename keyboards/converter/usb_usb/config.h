119fa28d9759faefcecab1f961c00399dcd66610
Balz Guenat <balz.guenat@gmail.com>
2017-06-20 02:19:15 +0200

    start working on usb-usb converter port

---
+++ keyboards/converter/usb_usb/config.h
@@ -0,0 +1,37 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x005B
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    QMK
+#define PRODUCT         QMK USB-USB Converter
+#define DESCRIPTION     USB to USB Keyboard Converter with QMK
+
+
+/* matrix size */
+#define MATRIX_ROWS 16
+#define MATRIX_COLS 16
+
+/* key combination for command */
+#define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))) 
+
+#endif
