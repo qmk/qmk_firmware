bf0188104e1776d4260d0f3cf316cde0a6ff1397
Jack Humbert <jack.humb@gmail.com>
2017-06-25 15:55:57 -0400

    fix binary comps

--- quantum/keycode_config.c
+++ quantum/keycode_config.c
@@ -92,10 +92,10 @@ uint16_t keycode_config(uint16_t keycode) {
 uint8_t mod_config(uint8_t mod) {
     keymap_config.raw = eeconfig_read_keymap();
     if (keymap_config.swap_lalt_lgui) {
-        if ((mod & MOD_LGUI) && !(mod & MOD_RGUI)) {
+        if ((mod & MOD_LGUI) && (mod & MOD_RGUI) != MOD_RGUI) {
             mod &= ~MOD_LGUI;
             mod |= MOD_LALT;
-        } else if ((mod & MOD_LALT) && !(mod & MOD_RALT)) {
+        } else if ((mod & MOD_LALT) && (mod & MOD_RALT) != MOD_RALT) {
             mod &= ~MOD_LALT;
             mod |= MOD_LGUI;
         }
