36cc4fdd91d46a469c219a737b09b486ff762ec7
Jack Humbert <jack.humb@gmail.com>
2017-06-26 13:54:18 -0400

    solid logic

--- quantum/keycode_config.c
+++ quantum/keycode_config.c
@@ -92,19 +92,19 @@ uint16_t keycode_config(uint16_t keycode) {
 uint8_t mod_config(uint8_t mod) {
     keymap_config.raw = eeconfig_read_keymap();
     if (keymap_config.swap_lalt_lgui) {
-        if ((mod & MOD_LGUI) && (mod & MOD_RGUI) != MOD_RGUI) {
+        if ((mod & MOD_RGUI) == MOD_LGUI) {
             mod &= ~MOD_LGUI;
             mod |= MOD_LALT;
-        } else if ((mod & MOD_LALT) && (mod & MOD_RALT) != MOD_RALT) {
+        } else if ((mod & MOD_RALT) == MOD_LALT) {
             mod &= ~MOD_LALT;
             mod |= MOD_LGUI;
         }
     }
     if (keymap_config.swap_ralt_rgui) {
-        if (mod & MOD_RGUI) {
+        if ((mod & MOD_RGUI) == MOD_RGUI) {
             mod &= ~MOD_RGUI;
             mod |= MOD_RALT;
-        } else if (mod & MOD_RALT) {
+        } else if ((mod & MOD_RALT) == MOD_RALT) {
             mod &= ~MOD_RALT;
             mod |= MOD_RGUI;
         }
