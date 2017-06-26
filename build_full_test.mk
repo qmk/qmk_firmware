acc94cf50aa4bfc221e59ce20a3d13711dbc78ba
Fred Sundvik <fsundvik@gmail.com>
2017-06-27 00:01:27 +0300

    Build all cpp files in test directories

--- build_full_test.mk
+++ build_full_test.mk
@@ -18,7 +18,6 @@
 TEST_PATH=tests/$(TEST)
 
 $(TEST)_SRC= \
-	$(TEST_PATH)/test.cpp \
 	$(TEST_PATH)/keymap.c \
 	$(TMK_COMMON_SRC) \
 	$(QUANTUM_SRC) \
@@ -26,6 +25,8 @@ $(TEST)_SRC= \
 	tests/test_common/test_driver.cpp \
 	tests/test_common/keyboard_report_util.cpp \
 	tests/test_common/test_fixture.cpp
+$(TEST)_SRC += $(patsubst $(ROOTDIR)/%,%,$(wildcard $(TEST_PATH)/*.cpp))
+
 $(TEST)_DEFS=$(TMK_COMMON_DEFS)
 $(TEST)_CONFIG=$(TEST_PATH)/config.h
 VPATH+=$(TOP_DIR)/tests/test_common
