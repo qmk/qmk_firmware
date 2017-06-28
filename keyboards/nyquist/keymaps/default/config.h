884d29e75098850329bf33243472a304d4742f07
Jack Humbert <jack.humb@gmail.com>
2017-06-28 10:38:18 -0400

    cleanup nyquist

--- keyboards/nyquist/keymaps/default/config.h
+++ keyboards/nyquist/keymaps/default/config.h
@@ -15,12 +15,20 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define USE_I2C
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+/* Use I2C or Serial, not both */
+
+#define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
 
 #define MASTER_LEFT
 // #define _MASTER_RIGHT
 // #define EE_HANDS
 
-#ifdef SUBPROJECT_rev1
-    #include "../../rev1/config.h"
 #endif
