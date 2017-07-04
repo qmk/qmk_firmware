7d7c945bcbb858a4958ac2fb43f6a1e87d27302e
That-Canadian <poole.chris.11@gmail.com>
2017-07-04 15:26:25 -0400

    Updated ECO files to follow proper formatting. Also updated that-canadian keymap to take out extraneous characters.

--- keyboards/eco/rev1/config.h
+++ keyboards/eco/rev1/config.h
@@ -15,65 +15,16 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef REV1_CONFIG_H
+#define REV1_CONFIG_H
 
-#include "config_common.h"
+#include "../config.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x1337
-#define PRODUCT_ID      0x6006
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Bishop Keyboards
-#define PRODUCT         The ECO Keyboard
-#define DESCRIPTION     An economical ortholinear keyboard
 
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 14
-
-/* Planck PCB default pin-out */
+/* ECO V1 pin-out */
 #define MATRIX_ROW_PINS { B1, B6, B2, B3 }
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B5, B4, E6, D7, C6, D4, D0, D1, D2, D3 }
 #define UNUSED_PINS
-
-#define CATERINA_BOOTLOADER
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
 
 #endif
