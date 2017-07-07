3382aec611a2e8e1f1cfc30b60c187bbd270bdf4
Mike Terhar <michael.terhar@pararray.com>
2017-07-07 14:17:46 -0400

    Fixed the lower-left side of the keymap, which needed more spacers due to the matrix being directly put into the array rather than using the keymap function.

--- keyboards/jd45/keymaps/mjt6u/keymap-debug.c
+++ keyboards/jd45/keymaps/mjt6u/keymap-debug.c
@@ -40,43 +40,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {FKEYTAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS, KC_BSPC},
    {CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  LT(2, KC_QUOT)},
      {KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SFT_T(KC_ENT)},
-  { XXXXXXX, KC_LALT, KC_LGUI, XXXXXXX,  XXXXXXX,  XXXXXXX, FNSPC,  MO(_FUNCTION),  MO(_ADJUST),   XXXXXXX }
+  { XXXXXXX, KC_LALT, KC_LGUI, XXXXXXX,  XXXXXXX,  XXXXXXX, FNSPC,  XXXXXXX, XXXXXXX, MO(_FUNCTION),  MO(_ADJUST),   XXXXXXX }
 },
 [_QWERTYNUMMODS] = {
 {  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-  {XXXXXXX, _______, _______, XXXXXXX,  XXXXXXX,  XXXXXXX,_______, _______, _______, XXXXXXX }
+  {XXXXXXX, _______, _______, XXXXXXX,  XXXXXXX,  XXXXXXX,_______, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX }
 },
 [_NUMSYM] = {
 {KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0, _______, KC_DEL},
  {__MOD__, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______},
   {_______, _______, _______, _______, _______, KC_SPC, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, _______},
-{XXXXXXX, __MOD__, __MOD__, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, __MOD__, __MOD__, XXXXXXX}
+{XXXXXXX, __MOD__, __MOD__, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, XXXXXXX, XXXXXXX, __MOD__, __MOD__, XXXXXXX}
 },
 [_FUNCTION] = {
 {__MOD__,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, KC_F12},
 {__MOD__, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
 {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-{XXXXXXX, __MOD__, __MOD__, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, __MOD__, __MOD__, XXXXXXX }
+{XXXXXXX, __MOD__, __MOD__, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, XXXXXXX, XXXXXXX, __MOD__, __MOD__, XXXXXXX }
 },
 [_NUMPAD] = {
 {KC_GRV,  _______,   KC_UP, _______,   KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_PSCR, KC_LBRC, KC_RBRC},
 {_______, KC_LEFT, KC_DOWN, KC_RGHT,   KC_4,    KC_5,    KC_6,  KC_INS, KC_HOME, KC_PGUP, _______, _______},
 {_______, _______, _______, _______,   KC_1,    KC_2,    KC_3,  KC_DEL,  KC_END, KC_PGDN, _______, _______},
-{XXXXXXX, _______, _______, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, _______, _______, XXXXXXX }
+{XXXXXXX, _______, _______, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX }
 },
 [_FKEYNUMPAD] = {
 {_______, _______, KC_VOLU, _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_PSCR, _______, _______},
 {_______, KC_MPRV, KC_VOLD, KC_MNXT,   KC_F4,   KC_F5,   KC_F6,    KC_J,    KC_K,    KC_L, KC_SCLN, _______},
 {_______, _______, _______, _______,   KC_F1,   KC_F2,   KC_F3, KC_MUTE, KC_MPRV, KC_MNXT, KC_MSTP, _______},
-{XXXXXXX, _______, _______, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, __MOD__, KC_PAUS, XXXXXXX }
+{XXXXXXX, _______, _______, XXXXXXX,  XXXXXXX,  XXXXXXX, _______, XXXXXXX, XXXXXXX, __MOD__, KC_PAUS, XXXXXXX }
 },
 [_ADJUST] = {
   {_______, RESET, _______, _______, _______, _______, _______, DYN_REC_START1, DYN_REC_START2, _______, KC_PSCR, _______, _______},
   { _______, _______, _______, _______, USEFNMODS, _______, _______, DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, MACSLEEP, _______, _______},
   {  _______, _______, _______, _______, _______, _______, USENUMMODS, _______, _______, _______, _______, _______},
-  {XXXXXXX, _______, _______,XXXXXXX,  XXXXXXX,  XXXXXXX, _______, __MOD__, __MOD__, XXXXXXX}
+  {XXXXXXX, _______, _______,XXXXXXX,  XXXXXXX,  XXXXXXX, _______, XXXXXXX, XXXXXXX, __MOD__, __MOD__, XXXXXXX}
  }
 };
 
