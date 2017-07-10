56c0fe6fbc9b4fcead24f2d8a53bf9b8d9934bb3
Fred Sundvik <fsundvik@gmail.com>
2017-07-10 18:16:35 +0300

    Install unzip

--- util/msys2_install.sh
+++ util/msys2_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+echo "Installing dependencies needed for the installation (quazip)"
+pacman -S msys/unzip
+
 source "$dir/win_shared_install.sh"
 
 echo
