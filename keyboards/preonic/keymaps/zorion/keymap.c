10e47d576f02fea0b22e1759bf0c1633eba1b38a
Michael Atkinson <michael@philomathery.com>
2017-07-13 10:33:18 -0500

    standardize indentation because I'm not a monster

--- keyboards/preonic/keymaps/zorion/keymap.c
+++ keyboards/preonic/keymaps/zorion/keymap.c
@@ -358,42 +358,42 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-      case QWERTY:
-        if (record->event.pressed) {
-          #ifdef AUDIO_ENABLE
-            PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
-          #endif
-          persistent_default_layer_set(1UL<<_QWERTY);
-        }
-        return false;
-        break;
-      case COLEMAK:
-        if (record->event.pressed) {
-          #ifdef AUDIO_ENABLE
-            PLAY_NOTE_ARRAY(tone_colemak, false, 0);
-          #endif
-          persistent_default_layer_set(1UL<<_COLEMAK);
-        }
-        return false;
-        break;
-      case DVORAK:
-        if (record->event.pressed) {
-          #ifdef AUDIO_ENABLE
-            PLAY_NOTE_ARRAY(tone_dvorak, false, 0);
-          #endif
-          persistent_default_layer_set(1UL<<_DVORAK);
-        }
-        return false;
-        break;
-      case NUMFUN:
-        if (record->event.pressed) {
-          #ifdef AUDIO_ENABLE
-            PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
-          #endif
-          persistent_default_layer_set(1UL<<_NUMFUN);
-        }
-        return false;
-        break;
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
+        #endif
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case COLEMAK:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_NOTE_ARRAY(tone_colemak, false, 0);
+        #endif
+        persistent_default_layer_set(1UL<<_COLEMAK);
+      }
+      return false;
+      break;
+    case DVORAK:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_NOTE_ARRAY(tone_dvorak, false, 0);
+        #endif
+        persistent_default_layer_set(1UL<<_DVORAK);
+      }
+      return false;
+      break;
+    case NUMFUN:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
+        #endif
+        persistent_default_layer_set(1UL<<_NUMFUN);
+      }
+      return false;
+      break;
     }
     return true;
 };
@@ -420,25 +420,25 @@ void matrix_init_user(void) {
 
 void startup_user()
 {
-    _delay_ms(20); // gets rid of tick
-    PLAY_NOTE_ARRAY(tone_startup, false, 0);
+  _delay_ms(20); // gets rid of tick
+  PLAY_NOTE_ARRAY(tone_startup, false, 0);
 }
 
 void shutdown_user()
 {
-    PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
-    _delay_ms(150);
-    stop_all_notes();
+  PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
+  _delay_ms(150);
+  stop_all_notes();
 }
 
 void music_on_user(void)
 {
-    music_scale_user();
+  music_scale_user();
 }
 
 void music_scale_user(void)
 {
-    PLAY_NOTE_ARRAY(music_scale, false, 0);
+  PLAY_NOTE_ARRAY(music_scale, false, 0);
 }
 
 #endif
