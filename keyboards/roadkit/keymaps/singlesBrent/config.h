e5f606558e3ec8ec9b969584aaaf528308042c16
rootuseralpha <brent.markus@gmail.com>
2017-07-19 18:54:40 -0500

    Add files via upload

---
+++ keyboards/roadkit/keymaps/singlesBrent/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// place overrides here
+#define TAPPING_TERM 175
+
+#ifdef BACKLIGHT_ENABLE
+    #define BACKLIGHT_PIN B5
+	#define BACKLIGHT_LEVELS 3
+    #define BACKLIGHT_ON_STATE 0
+	backlight_set(3);
+#endif	
+	
+#endif
