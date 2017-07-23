d1c7cf7ae450270a21b3e6168322d9e52dfdcfaf
Donald <itsaferbie@gmail.com>
2017-07-23 16:37:31 -0400

    Changed case of deltasplit75 to be lowercase.
    To follow convention.

---
+++ keyboards/deltasplit75/deltasplit75.h
@@ -0,0 +1,13 @@
+#ifndef deltasplit75_H
+#define deltasplit75_H
+
+#ifdef SUBPROJECT_v2
+    #include "v2.h"
+#endif
+#ifdef SUBPROJECT_ProtoSplit
+    #include "ProtoSplit.h"
+#endif
+
+#include "quantum.h"
+
+#endif
