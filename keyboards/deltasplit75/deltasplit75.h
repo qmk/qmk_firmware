8169873136ff45a12b0abc9447b3a3b8c005b49f
Donald <itsaferbie@gmail.com>
2017-07-23 16:43:35 -0400

    Changed case of protosplit
    Also updated readme to have correct capitalization.

--- keyboards/deltasplit75/deltasplit75.h
+++ keyboards/deltasplit75/deltasplit75.h
@@ -4,8 +4,8 @@
 #ifdef SUBPROJECT_v2
     #include "v2.h"
 #endif
-#ifdef SUBPROJECT_ProtoSplit
-    #include "ProtoSplit.h"
+#ifdef SUBPROJECT_protosplit
+    #include "protosplit.h"
 #endif
 
 #include "quantum.h"
