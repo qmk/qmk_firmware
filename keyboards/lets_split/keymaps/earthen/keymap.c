93e604d0157b9c6200d215bedce5ea973a44b760
Joan Lustenberger <lustenberger.joan@outlook.com>
2017-07-27 17:26:22 +0200

    korrected.

--- keyboards/lets_split/keymaps/earthen/keymap.c
+++ keyboards/lets_split/keymaps/earthen/keymap.c
@@ -152,9 +152,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [LOWER] = KEYMAP( \
-  _______, KC_F4,   KC_F3,   KC_F2,   KC_F1,   KC_1,    KC_DELT, KC_7,    KC_8,    KC_9,   KC_PAST, _______, \
+  _______, KC_F4,   KC_F3,   KC_F2,   KC_F1,   KC_0,    KC_DELT, KC_7,    KC_8,    KC_9,   KC_PAST, _______, \
   _______, KC_F8,   KC_F7,   KC_F6,   KC_F5,   KC_5,    XXXXXXX, KC_4,    KC_5,    KC_6,   KC_PPLS, KC_PMNS, \
-  _______, KC_F12,  KC_F11,  KC_F10,  KC_F9,   KC_0,    KC_0,    KC_1,    KC_2,    KC_3,   KC_PSLS, _______, \
+  _______, KC_F12,  KC_F11,  KC_F10,  KC_F9,   KC_1,    KC_0,    KC_1,    KC_2,    KC_3,   KC_PSLS, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_PCMM, KC_DOT, CH_EQL,  _______ \
 ),
 
