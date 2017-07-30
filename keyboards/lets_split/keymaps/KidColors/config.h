0a1de2de0c33b1e831c94a2369158641618d957d
Joan Lustenberger <lustenberger.joan@outlook.com>
2017-07-30 07:55:02 +0200

    corrected main hand.

--- keyboards/lets_split/keymaps/KidColors/config.h
+++ keyboards/lets_split/keymaps/KidColors/config.h
@@ -33,22 +33,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8 // units to step when in/decreasing hue
 #define RGBLIGHT_SAT_STEP 8 // units to step when in/decresing saturation
 #define RGBLIGHT_VAL_STEP 8 // units to step when in/decreasing value (brightness)
-/* Mouse configuration */
-#define MOUSEKEY_INTERVAL 20
-#define MOUSEKEY_DELAY 0
-#define MOUSEKEY_TIME_TO_MAX 60
-#define MOUSEKEY_MAX_SPEED 7
-#define MOUSEKEY_WHEEL_DELAY 0
 /* Select hand configuration */
 
-// Tap Toogle layer
-#define TAPPING_TOGGLE 1
-
-// #define MASTER_LEFT
-#define _MASTER_RIGHT
+#define MASTER_LEFT
+// #define _MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_TERM    150
-
-#define PERMISSIVE_HOLD
 #endif
