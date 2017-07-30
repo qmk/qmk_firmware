6bcf399dc056fb326d873830de192defb207c90d
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-07-30 17:22:08 +0100

    Add Dvorak and gui-key-switch support.

--- keyboards/kbd66/keymaps/default/keymap.c
+++ keyboards/kbd66/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2017 REPLACE_WITH_YOUR_NAME
+/* Copyright 2017 Mark Smith
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,28 +15,35 @@
  */
 #include "kbd66.h"
 
+#include "eeconfig.h"
+
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define _QWERTY 0
+enum kbd66_layers {
+  _QWERTY,
+  _DVORAK,
+  _FUNC
+};
 
 enum kbd66_keycodes {
-  QWERTY = SAFE_RANGE //,
-  //DVORAK
+  QWERTY = SAFE_RANGE,
+  DVORAK,
+  FUNC
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* Keymap 0: Default Layer
+    /* Qwerty
      * ,----------------------------------------------------------.   ,---.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  +|  Bs  |   |INS|
      * |----------------------------------------------------------|   |---|
      * |Tab |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     |   |DEL|
      * |-----------------------------------------------------| Ent|   `---'
      * |Cap  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  \|    |
-     * |-------------------------------------------------------------.
-     * |Shift |  `|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  Shift | UP|
+     * |---------------------------------------------------------------.
+     * |Shft|  `|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  Shift   | UP|
      * |-------------------------------------------------------------------.
      * |Ctl |Alt |Win |         Space           |Win |Alt | Fn |LFT|DWN|RIG|
      * `-------------------------------------------------------------------'
@@ -45,9 +52,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_HOME,
   KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_DEL,
   KC_CAPS,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS,
-  KC_LSFT, KC_NONUS_BSLASH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-  KC_LCTL, KC_LALT, KC_LGUI,   KC_SPC,KC_SPC, KC_RGUI, KC_RALT, KC_NO,      KC_LEFT, KC_DOWN,   KC_RGHT
+  KC_LSPO, KC_NONUS_BSLASH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,
+  KC_LCTL, KC_LALT, KC_LGUI,   KC_SPC,KC_SPC, KC_RGUI, KC_RALT, FUNC,      KC_LEFT, KC_DOWN,   KC_RGHT
 ),
+
+    /* Dvorak
+     * ,----------------------------------------------------------.   ,---.
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  +|  Bs  |   |INS|
+     * |----------------------------------------------------------|   |---|
+     * |Tab |  "|  ,|  .|  P|  Y|  F|  G|  C|  R|  L|  /|  =|     |   |DEL|
+     * |-----------------------------------------------------| Ent|   `---'
+     * |Cap  |  A|  O|  E|  U|  I|  D|  H|  T|  N|  S|  -|  \|    |
+     * |-------------------------------------------------------------.
+     * |Shft|  `|  ;|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|  Shift | UP|
+     * |-------------------------------------------------------------------.
+     * |Ctl |Alt |Win |         Space           |Win |Alt | Fn |LFT|DWN|RIG|
+     * `-------------------------------------------------------------------'
+     */
+[_DVORAK] = KEYMAP(
+  KC_ESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,   KC_7, KC_8, KC_9, KC_0,  KC_LBRC, KC_RBRC, KC_BSPC, KC_HOME,
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH, KC_EQL, KC_ENT, KC_DEL,
+  KC_CAPS,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS, KC_BSLS,
+  KC_LSPO, KC_NONUS_BSLASH, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSPC, KC_UP,
+  KC_LCTL, KC_LALT, KC_LGUI,   KC_SPC,KC_SPC, KC_RGUI, KC_RALT, FUNC,      KC_LEFT, KC_DOWN,   KC_RGHT
+),
+
+[_FUNC] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______,  QWERTY, DVORAK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______, _______,  _______, _______, _______,
+  _______, _______, _______,   _______,_______, _______, _______, FUNC,      _______, _______,   _______
+)
 
 };
 
@@ -79,7 +115,35 @@ void matrix_scan_user(void) {
 
 }
 
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case DVORAK:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_DVORAK);
+      }
+      return false;
+      break;
+    case FUNC:
+      if (record->event.pressed) {
+        layer_on(_FUNC);
+      } else {
+        layer_off(_FUNC);
+      }
+      return false;
+      break;
+  }
+
   return true;
 }
 
