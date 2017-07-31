b523415917ef81ce265eb6c12aae0f8f07e2b0f8
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-07-31 08:33:03 +0100

    Disable space cadet rollover - not sure if I'll want to reverse this.

--- keyboards/kbd66/config.h
+++ keyboards/kbd66/config.h
@@ -182,4 +182,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 1
 
+#define DISABLE_SPACE_CADET_ROLLOVER
+
 #endif
