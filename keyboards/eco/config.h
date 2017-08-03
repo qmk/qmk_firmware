db2fbce14c566665747ddc5c6d31213688a9102d
That-Canadian <poole.chris.11@gmail.com>
2017-08-03 11:02:52 -0400

    Added V2 files for the ECO board, also changed default from rev1 to rev2.

--- keyboards/eco/config.h
+++ keyboards/eco/config.h
@@ -73,5 +73,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef SUBPROJECT_rev1
     #include "rev1/config.h"
 #endif
+#ifdef SUBPROJECT_rev2
+    #include "rev2/config.h"
+#endif
 
 #endif
