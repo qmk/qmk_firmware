5fb6fa9f03f2a8e39d87645eebf557457e32f26a
CarVac <c.lo.to.da.down.lo@gmail.com>
2017-08-06 20:53:32 -0400

    move backspace, etc, and fix tab

--- keyboards/mitosis/keymaps/carvac_dv/keymap.c
+++ keyboards/mitosis/keymaps/carvac_dv/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
   {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
   {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
-  {XXXXXXX, KC_PGUP, KC_PSCR, KC_LCTL, KC_SPC,         KC_LSFT, KC_ENT,  KC_UP,   KC_PGUP, XXXXXXX },
+  {XXXXXXX, KC_PGUP, KC_PSCR, KC_LCTL, KC_SPC,         KC_LSFT, KC_ENT,  KC_UP,   KC_BSPC, XXXXXXX },
   {XXXXXXX, KC_PGDN, KC_LGUI, KC_LALT, MO(_FN),       TT(_NUM), KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX }
 },
 
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_NUM] = { /* Number layout along the home row for maximum speed*/
-  {_______, _______, _______, KC_MINS, KC_EQL,        KC_RBRC, KC_PAST, KC_PPLS, KC_PMNS, KC_LBRC },
+  {KC_TAB,  _______, _______, KC_MINS, KC_EQL,        KC_RBRC, KC_PAST, KC_PPLS, KC_PMNS, KC_LBRC },
   {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0    },
   {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10  },
   {XXXXXXX, KC_F11,  KC_F12,  _______, _______,       _______, _______, _______, _______, XXXXXXX },
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
  * | ESC    | MS_L   | MS_D   | MS_R   |        || BTN5   | BTN1   | BTN2   |        | ' (-_) |
  * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
- * | APP    | MPRV   | MPLY   | MSTP   | MNXT   || BSPC   | DEL    | INS    |        | \      |
+ * | APP    | MPRV   | MPLY   | MSTP   | MNXT   ||        | BSPC   | DEL    | INS    | \      |
  * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
  *          | VOLU   |        |        |        ||        |        | PGUP   |        |
  *          |--------+--------+--------+--------||--------+--------+--------+--------|
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    cursor keys on left hand, and all symbols*/
   {KC_GRV,  _______, KC_MS_U, _______, _______,       KC_BTN4, _______, KC_BTN3, _______, KC_LBRC },
   {KC_ESC,  KC_MS_L, KC_MS_D, KC_MS_R, _______,       KC_BTN5, KC_BTN1, KC_BTN2, _______, KC_QUOT },
-  {KC_APP,  KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,       KC_BSPC, KC_DEL,  KC_INS,  _______, KC_BSLS },
+  {KC_APP,  KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,       _______, KC_BSPC, KC_DEL,  KC_INS,  KC_BSLS },
   {XXXXXXX, KC_VOLU, _______, _______, _______,       _______, _______, KC_PGUP, _______, XXXXXXX },
   {XXXXXXX, KC_VOLD, _______, _______, _______,       _______, KC_HOME, KC_PGDN, KC_END,  XXXXXXX }
 },
