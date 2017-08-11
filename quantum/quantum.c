18cc25de92010dd6774ad40825eac51ccc063192
Balz Guenat <balz.guenat@gmail.com>
2017-08-11 21:43:49 +0200

    add short comment to grave_esc_ctrl_override

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -477,6 +477,8 @@ bool process_record_quantum(keyrecord_t *record) {
                                       |MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)));
       
 #ifdef GRAVE_ESC_CTRL_OVERRIDE
+      // if CTRL is pressed, ESC is always read as ESC, even if SHIFT or GUI is pressed.
+      // this is handy for the ctrl+shift+esc shortcut on windows, among other things.
       if (get_mods() & (MOD_BIT(KC_LCTL) | MOD_BIT(KC_RCTL)))
         shifted = 0;
 #endif
