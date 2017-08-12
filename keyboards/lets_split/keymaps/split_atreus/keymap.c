196c545355f6e7385a42adadbeeb2247b436e87e
Hugh Enxing <henxing@draper.com>
2017-08-11 21:11:51 -0400

    Added keys for testing

--- keyboards/lets_split/keymaps/split_atreus/keymap.c
+++ keyboards/lets_split/keymaps/split_atreus/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,   KC_NO,   KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,   KC_NO,   KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL, KC_LALT, KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, \
-  KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT,  KC_NO,   KC_BSPC, KC_SPC,  KC_NO,  MO(_L2),  KC_MINS, KC_QUOT, KC_ENT   \
+  KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT,  KC_DLR,  KC_BSPC, KC_SPC,  KC_EQL,  MO(_L2),  KC_MINS, KC_QUOT, KC_ENT   \
 ),
 
 [_L2] = KEYMAP( \
