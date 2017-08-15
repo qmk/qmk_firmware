60588c8e8eb25797b36da0db7af3ee795a731b74
Balz Guenat <balz.guenat@gmail.com>
2017-08-16 01:34:46 +0200

    cleanup usb-usb converter code

--- keyboards/converter/usb_usb/usb_usb.h
+++ keyboards/converter/usb_usb/usb_usb.h
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #ifndef USB_USB_H
 #define USB_USB_H
 
