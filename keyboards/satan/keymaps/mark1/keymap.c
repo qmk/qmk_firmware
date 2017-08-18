0be483c762fa770185b7ff4ed49fd9736f7e309b
Mark Spanbroek <mark@spanbroek.net>
2017-08-18 23:33:06 +0200

    Fix power; use keyboard shortcut for Mac

--- keyboards/satan/keymaps/mark1/keymap.c
+++ keyboards/satan/keymaps/mark1/keymap.c
@@ -5,6 +5,7 @@
 #define KC_FNLK TG(1) // Fn lock
 #define KC_HOME LCTL(KC_A) // Home, or ctrl-a
 #define KC_END LCTL(KC_E) // End, or ctrl-e
+#define KC_SLP LGUI(LALT(KC_POWER)) // sleep, or cmd-option-power
 
 // When using this split right shift layout, k3c and k3d are no longer reversed
 // as in other layouts
@@ -38,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,____,
         ____,VOLU,____,____,____,____,____,HOME,UP  ,END ,PGUP,____,____,____,
         ____,VOLD,MRWD,MPLY,MFFD,____,____,LEFT,DOWN,RGHT,PGDN,____,____,____,
-        ____,____,MUTE,____,____,____,____,____,____,____,SLEP,____,____,____,
+        ____,____,MUTE,____,____,____,____,____,____,____,SLP ,____,____,____,
         ____,____,____,          ____,                    ____,____,____,____
     )
 };
