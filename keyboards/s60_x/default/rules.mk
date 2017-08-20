b9d670fca606ff3aa16b1c3068b728c448181a10
Daniel Khodabakhsh <d.khodabakhsh@gmail.com>
2017-08-19 23:50:29 -0700

    Update s60_x/default/rule.mk for original Sentraq S60-X Fix and issue with the original Sentraq S60-X not being compatible with 'default'. If 'default' shouldn't be changed, perhaps I can create an 'original' revision.

--- keyboards/s60_x/default/rules.mk
+++ keyboards/s60_x/default/rules.mk
@@ -1,7 +1,7 @@
 CONSOLE_ENABLE = no        # Console for debug(+400)
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = no          # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes     # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no     # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no
 
 ifndef QUANTUM_DIR
