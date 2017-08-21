8fd534805f56a461b8f4be91bf5ef3713ad90be6
Jack Humbert <jack.humb@gmail.com>
2017-08-20 23:28:43 -0400

    touch-up default layouts

--- keyboards/nyquist/keymaps/default/config.h
+++ keyboards/nyquist/keymaps/default/config.h
@@ -22,12 +22,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Use I2C or Serial, not both */
 
-#define USE_SERIAL
+// #define USE_SERIAL
 // #define USE_I2C
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define _MASTER_RIGHT
 // #define EE_HANDS
 
