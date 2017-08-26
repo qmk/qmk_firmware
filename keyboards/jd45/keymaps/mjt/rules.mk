ef078686fb9db7a71e7fd3069fdb651274a1a31d
Michael Terhar <michael.terhar@pararray.com>
2017-08-26 12:50:43 -0400

    Moved custom rules.mk to apropriate keymap
    Reset the shared rules.mk file.

--- keyboards/jd45/keymaps/mjt/rules.mk
+++ keyboards/jd45/keymaps/mjt/rules.mk
@@ -59,9 +59,9 @@ EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE = yes	# Console for debug(+400)
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-# SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-# NKRO_ENABLE = yes		# USB Nkey Rollover - not yet supported in LUFA
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes		# USB Nkey Rollover - not yet supported in LUFA
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-MIDI_ENABLE = YES 		# MIDI controls
+MIDI_ENABLE = no 		# MIDI controls
 # UNICODE_ENABLE = YES 		# Unicode
-BLUETOOTH_ENABLE = yes # Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = no # Enable Bluetooth with the Adafruit EZ-Key HID
