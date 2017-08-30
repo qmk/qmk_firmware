e10ee8018d56e52164a857e79dd0e7f8b2791601
Shay Merrill <shaymdev@gmail.com>
2017-08-29 21:50:02 -0600

    orthodox shaymdev: add reset and grv

--- keyboards/orthodox/keymaps/shaymdev/keymap.c
+++ keyboards/orthodox/keymaps/shaymdev/keymap.c
@@ -74,14 +74,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = KEYMAP( \
   _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR,                                                                _______, KC_P7, KC_P8, KC_P9, KC_PMNS, _______, \
-  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,   _______, XXXXXXX, _______,          _______, XXXXXXX, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, _______, \
-  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, _______, _______, _______, _______,          _______, _______, KC_KP_0, _______, KC_P1, KC_P2, KC_P3, KC_PENT, _______ \
+  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,	_______, XXXXXXX, _______,          _______, XXXXXXX, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, _______, \
+  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV,	_______, _______, _______,          _______, _______, KC_KP_0, _______, KC_P1, KC_P2, KC_P3, KC_PENT, _______ \
 ),
 
 [_RAISE] = KEYMAP( \
   _______, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP,                                                                  KC_VOLU, KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,\
   KC_CAPS, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,   _______, XXXXXXX, _______,          _______, XXXXXXX, _______, KC_VOLD, KC_F4,   KC_F5,   KC_F6,   KC_F11,  XXXXXXX,\
-  XXXXXXX, KC_NLCK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______, _______, _______,          _______, _______, _______,  KC_MUTE, KC_F1,   KC_F2,   KC_F3,   KC_F12,  XXXXXXX\
+  RESET,   KC_NLCK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______, _______, _______,          _______, _______, _______,  KC_MUTE, KC_F1,   KC_F2,   KC_F3,   KC_F12,  XXXXXXX\
 ),
 
 [_ADJUST] =  KEYMAP( \
