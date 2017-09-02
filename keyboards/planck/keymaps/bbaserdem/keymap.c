0821c3eea2df406fd7dc64ffbdef7b1013a5f4d0
Batuhan Baserdem <baserdem.batuhan@gmail.com>
2017-09-02 07:38:38 -0400

    Adding keymap

--- keyboards/planck/keymaps/bbaserdem/keymap.c
+++ keyboards/planck/keymaps/bbaserdem/keymap.c
@@ -15,36 +15,21 @@
  */
 
 #include "planck.h"
-#include "action_layer.h"
 
 #define _______ KC_TRNS
 #define XXX KC_NO
 
-#define DV 0
-#define TD 1
-#define QW 2
-#define TQ 3
-#define GM 4
-#define MO 5
-#define SY 6
-#define FN 7
-#define MS 8
+#define _DV 0
+#define _TD 1
+#define _QW 2
+#define _TQ 3
+#define _GM 4
+#define _MO 5
+#define _SY 6
+#define _FN 7
+#define _MS 8
 
-extern keymap_config_t keymap_config;
-
-enum planck_layers {
-  _DVO,
-  _TRD,
-  _QWE,
-  _TRQ
-  _GAM,
-  _MOU,
-  _SYM,
-  _FUN,
-  _MUS,
-};
-
-enum planck_keycodes {
+enum custom_keycodes {
     TUR_A = SAFE_RANGE,
     TUR_C,
     TUR_G,
@@ -57,17 +42,17 @@ enum planck_keycodes {
     UTF_WN,
 };
 
-// Tap dance definitions
+// Tap dance
 enum {
-    ATRD = 0,
-    ATRA,
+    TD_ATD = 0,
+    TD_ATQ,
 };
 
 // Tap dance feature
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Left Ctrl, second one is momentory switch to layer TUR
-    [ATRD] =  ACTION_TAP_DANCE_DOUBLE( KC_LALT, MO(TRD) )
-    ,[ATRQ] = ACTION_TAP_DANCE_DOUBLE( KC_LALT, MO(TRQ) )
+    [TD_ATD] =  ACTION_TAP_DANCE_DOUBLE( KC_LALT, MO(_TD) )
+    ,[TD_ATQ] = ACTION_TAP_DANCE_DOUBLE( KC_LALT, MO(_TQ) )
 };
 
 
@@ -84,13 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Shift| Ctrl |  Alt | Meta | Symb | Enter| Space| Func | Left | Down |  Up  | Right|
  * `-----------------------------------------------------------------------------------'
  */
-[_DVO] = {
+[_DV] = {
   {BL_STEP, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,  KC_BSPC },
   {KC_ESC , KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,  KC_DEL  },
-  {KC_TAB , KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,  MO(MO)  },
-  {KC_LSFT, KC_LCTL,TD(TD),   KC_LGUI, MO(SY),  KC_ENT,  KC_SPC,  MO(FN),  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT }
+  {KC_TAB , KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,  MO(_MO) },
+  {KC_LSFT, KC_LCTL, TD_ATD,  KC_LGUI, MO(_SY), KC_ENT,  KC_SPC,  MO(_FN), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT }
 },
-[_TRD] = {
+[_TD] = {
   {_______,_______,_______,_______,_______,_______,_______, TUR_G, TUR_C,  _______,_______,_______},
   {_______, TUR_A,  TUR_O, _______, TUR_U,  TUR_I, _______,_______,_______,_______, TUR_S, _______},
   {_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______},
@@ -98,56 +83,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },
 
 // Qwerty layover
-[_QWE] = {
+[_QW] = {
   {_______, KC_Q,  KC_W,  KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   _______},
   {_______, KC_A,  KC_S,  KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,_______},
   {_______, KC_Z,  KC_X,  KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_QUOT,_______},
-  {_______,_______,TD(TQ),_______,_______,_______,_______,_______,_______,_______,_______,_______}
+  {_______,_______,TD_ATQ,_______,_______,_______,_______,_______,_______,_______,_______,_______}
 },
-[_TRD] = {
+[_TQ] = {
   {_______,_______,_______,_______,_______,_______,_______, TUR_U, TUR_I,  TUR_O,  _______,_______},
   {_______, TUR_A, TUR_S,  _______,_______, TUR_G, _______,_______,_______,_______,_______,_______},
   {_______,_______,_______, TUR_C, _______,_______,_______,_______,_______,_______,_______,_______},
   {_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______}
 },
 // Game layer
-[_GAM] = {
-  { TG(GM),    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_UP,    KC_GRAVE, KC_7,    KC_8, KC_9,    KC_ESC  },
+[_GM] = {
+  { TG(_GM),   KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_UP,    KC_GRAVE, KC_7,    KC_8, KC_9,    KC_ESC  },
   { KC_TILDE,  KC_A,    KC_S,    KC_D,    KC_F,   KC_LEFT, KC_DOWN,  KC_RIGHT, KC_4,    KC_5, KC_6,    KC_ENT  },
   { KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,   KC_F1,   KC_COMMA, KC_DOT,   KC_1,    KC_2, KC_3,    KC_RSFT },
   { KC_LALT,   KC_LCTL, KC_EXLM, KC_QUES, _______,KC_SPC,  KC_SPC,   _______,  KC_SLSH, KC_0, KC_RALT, KC_RCTL }
 },
 
 // Mouse control layer
-[_MOU] = {
-  { TG(MO),  XXX,           KC_MS_BTN1,    KC_MS_UP,   KC_MS_BTN2,  XXX,            XXX, XXX,     XXX, XXX,          XXX,          XXX        },
+[_MO] = {
+  { TG(_MO), XXX,           KC_MS_BTN1,    KC_MS_UP,   KC_MS_BTN2,  XXX,            XXX, XXX,     XXX, XXX,          XXX,          XXX        },
   { XXX,     KC_MS_WH_LEFT, KC_MS_LEFT,    KC_MS_BTN3, KC_MS_RIGHT, KC_MS_WH_RIGHT, XXX, XXX,     XXX, XXX,          XXX,          XXX        },
   { XXX,     KC_MS_BTN4,    KC_MS_WH_DOWN, KC_MS_DOWN, KC_MS_WH_UP, KC_MS_BTN5,     XXX, XXX,     XXX, XXX,          XXX,         _______     },
   { XXX,     XXX,           XXX,           XXX,        _______,     XXX,            XXX, _______, XXX, KC_MS_ACCEL0, KC_MS_ACCEL1, KC_MS_ACCEL2}
 },
 
 // Symbols layer
-[_SYM] = {
-  {TG(SY), KC_QUES,KC_LCBR,KC_LBRC,KC_LPRN,KC_BSLS,KC_SLSH,KC_RPRN,KC_RBRC,KC_RCBR,KC_PIPE,TUR_TL },
+[_SY] = {
+  {TG(_SY),KC_QUES,KC_LCBR,KC_LBRC,KC_LPRN,KC_BSLS,KC_SLSH,KC_RPRN,KC_RBRC,KC_RCBR,KC_PIPE,TUR_TL },
   {KC_GRV ,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS},
   {KC_TILD,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_PLUS,KC_EQL ,KC_UNDS},
   {_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______}
 },
 
 // Function layer
-[_FUN] = {
-  {TG(FN), TG(QW),TG(GM),MU_ON,  _______,_______,_______,   UTF_LI, UTF_WN ,KC_WAKE,KC_SLEP,KC_PWR },
+[_FN] = {
+  {TG(_FN),TG(_QW),TG(_GM),MU_ON,  _______,_______,_______,   UTF_LI, UTF_WN ,KC_WAKE,KC_SLEP,KC_PWR },
   {KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12 },
   {_______,LSFT(KC_Z),LSFT(KC_Y),LSFT(KC_X),LSFT(KC_C),LSFT(KC_V),KC_PSCR,KC_INS, KC_HOME,KC_PGDN,KC_PGUP,KC_END },
   {_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,_______,_______,_______,KC_MPRV,KC_MSTP,KC_MPLY,KC_MNXT}
 },
 
 // Music layer
-[_MUS] = {
+[_MS] = {
     { MU_OFF, KC_LCTL, KC_LALT, KC_LGUI, KC_UP, KC_DOWN, MU_MOD, XXX, XXX, XXX, XXX, XXX },
-    { XXX,  XXX,   XXX,   XXX,   XXX, XXX,   XXX,  XXX, XXX, XXX, XXX, XXX },
-    { XXX,  XXX,   XXX,   XXX,   XXX, XXX,   XXX,  XXX, XXX, XXX, XXX, XXX },
-    { XXX,  XXX,   XXX,   XXX,   XXX, XXX,   XXX,  XXX, XXX, XXX, XXX, XXX }
+    { XXX,    XXX,     XXX,     XXX,     XXX,   XXX,     XXX,    XXX, XXX, XXX, XXX, XXX },
+    { XXX,    XXX,     XXX,     XXX,     XXX,   XXX,     XXX,    XXX, XXX, XXX, XXX, XXX },
+    { XXX,    XXX,     XXX,     XXX,     XXX,   XXX,     XXX,    XXX, XXX, XXX, XXX, XXX }
 }
 };
 
@@ -164,13 +149,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case MU_ON:
       if (record->event.pressed) {
-          layer_on(MS);
+          layer_on(_MS);
       }
       return true;
       break;
     case MU_OFF:
       if (record->event.pressed) {
-          layer_off(MS);
+          layer_off(_MS);
       }
       return true;
       break;
