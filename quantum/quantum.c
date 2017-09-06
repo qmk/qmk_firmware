e1abe16341273d2e5fbfe858c9a48bb5ae140108
skullY <skullydazed@gmail.com>
2017-09-06 13:31:15 -0700

    Remove orphaned break statements

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -250,33 +250,28 @@ bool process_record_quantum(keyrecord_t *record) {
         reset_keyboard();
       }
     return false;
-      break;
     case DEBUG:
       if (record->event.pressed) {
           print("\nDEBUG: enabled.\n");
           debug_enable = true;
       }
     return false;
-      break;
   #ifdef FAUXCLICKY_ENABLE
   case FC_TOG:
     if (record->event.pressed) {
       FAUXCLICKY_TOGGLE;
     }
     return false;
-    break;
   case FC_ON:
     if (record->event.pressed) {
       FAUXCLICKY_ON;
     }
     return false;
-    break;
   case FC_OFF:
     if (record->event.pressed) {
       FAUXCLICKY_OFF;
     }
     return false;
-    break;
   #endif
   #ifdef RGBLIGHT_ENABLE
   case RGB_TOG:
@@ -284,49 +279,41 @@ bool process_record_quantum(keyrecord_t *record) {
       rgblight_toggle();
     }
     return false;
-    break;
   case RGB_MOD:
     if (record->event.pressed) {
       rgblight_step();
     }
     return false;
-    break;
   case RGB_HUI:
     if (record->event.pressed) {
       rgblight_increase_hue();
     }
     return false;
-    break;
   case RGB_HUD:
     if (record->event.pressed) {
       rgblight_decrease_hue();
     }
     return false;
-    break;
   case RGB_SAI:
     if (record->event.pressed) {
       rgblight_increase_sat();
     }
     return false;
-    break;
   case RGB_SAD:
     if (record->event.pressed) {
       rgblight_decrease_sat();
     }
     return false;
-    break;
   case RGB_VAI:
     if (record->event.pressed) {
       rgblight_increase_val();
     }
     return false;
-    break;
   case RGB_VAD:
     if (record->event.pressed) {
       rgblight_decrease_val();
     }
     return false;
-    break;
   case RGB_MODE_PLAIN:
     if (record->event.pressed) {
       rgblight_mode(1);
@@ -374,20 +361,17 @@ bool process_record_quantum(keyrecord_t *record) {
         set_output(OUTPUT_AUTO);
       }
       return false;
-      break;
     case OUT_USB:
       if (record->event.pressed) {
         set_output(OUTPUT_USB);
       }
       return false;
-      break;
     #ifdef BLUETOOTH_ENABLE
     case OUT_BT:
       if (record->event.pressed) {
         set_output(OUTPUT_BLUETOOTH);
       }
       return false;
-      break;
     #endif
     #endif
     case MAGIC_SWAP_CONTROL_CAPSLOCK ... MAGIC_TOGGLE_NKRO:
@@ -494,7 +478,6 @@ bool process_record_quantum(keyrecord_t *record) {
         unregister_mods(MOD_BIT(KC_LSFT));
       }
       return false;
-      // break;
     }
 
     case KC_RSPC: {
@@ -517,7 +500,6 @@ bool process_record_quantum(keyrecord_t *record) {
         unregister_mods(MOD_BIT(KC_RSFT));
       }
       return false;
-      // break;
     }
     case GRAVE_ESC: {
       uint8_t shifted = get_mods() & ((MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT)
