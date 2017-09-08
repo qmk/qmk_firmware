d0e58df857c2bbe55122ba67a2edd1120792906a
cbbrowne <cbbrowne@cbbrowne.tor.int-afilias.info>
2017-09-08 12:15:42 -0400

    Add a few more main layer keys, and modify LOWER to shift things outwards to conform with main layer

--- keyboards/xd75/keymaps/cbbrowne/keymap.c
+++ keyboards/xd75/keymaps/cbbrowne/keymap.c
@@ -148,9 +148,9 @@ special things...
 
    [_QCENT2] = { /* QWERTY, with keypad in the centre */
      { KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_ESC,  KC_PLUS, KC_MINS, RESET,     KC_ESC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0 },
-     { QALT,    KC_W,    KC_E,    KC_R,    KC_T,   KC_7,    KC_8,    KC_9,    RANDDIG,  _______, KC_Y,    KC_U,    KC_I,    KC_O,    PALT },
+     { QALT,    KC_W,    KC_E,    KC_R,    KC_T,   KC_7,    KC_8,    KC_9,    RANDDIG, USERNAME, KC_Y,    KC_U,    KC_I,    KC_O,    PALT },
      { ACTL,    KC_S,    KC_D,    KC_F,    KC_G,   KC_4,    KC_5,    KC_6,    RANDALP,  _______, KC_H,    KC_J,    KC_K,    KC_L,    SCTL },
-     { ZSHF,    KC_X,    KC_C,    KC_V,    KC_B,   KC_1,    KC_2,    KC_3,    _______,  _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  SCTL },
+     { ZSHF,    KC_X,    KC_C,    KC_V,    KC_B,   KC_1,    KC_2,    KC_3,    MVERSION,  _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  SCTL },
      { KC_TAB,  ROT_LED, MRAISE,  KC_BSPC, KC_SPC, KC_ENT,  KC_0,    _______,  KC_ENT,  KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT }
    },
 
@@ -170,10 +170,10 @@ special things...
  */
  
  [_LOWER] = { /* LOWERED */
-  { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ___T___, ___T___  },
-  { _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, _______, KC_INS   },
-  { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, ___T___, ___T___, _______  },
-  { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, ___T___, ___T___, _______, _______  },
+  { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, },
+  { KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, KC_INS   },
+  { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, ___T___, ___T___  },
+  { KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, ___T___, ___T___, _______, _______  },
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  },
  },
 
