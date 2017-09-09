8efdf668a41bbadbf8da2d99351980255a6e9c35
jd <dohmain@users.noreply.github.com>
2017-09-09 17:58:28 -0400

    turn off tx/rx LEDs

--- keyboards/lets_split/rev2/rev2.c
+++ keyboards/lets_split/rev2/rev2.c
@@ -19,13 +19,12 @@ void matrix_init_kb(void) {
         PLAY_SONG(tone_startup);
     #endif
 
-    // // green led on
-    // DDRD |= (1<<5);
-    // PORTD &= ~(1<<5);
+    // // turn off tx/rx LEDs
+     DDRD &= ~(1<<5);
+     PORTD &= ~(1<<5);
 
-    // // orange led on
-    // DDRB |= (1<<0);
-    // PORTB &= ~(1<<0);
+     DDRB &= ~(1<<0);
+     PORTB &= ~(1<<0);
 
 	matrix_init_user();
 };
