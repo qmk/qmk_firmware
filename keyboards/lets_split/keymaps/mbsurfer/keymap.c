6722019ebd2c09a818a32c9a45a5411990faf8c6
Cory Shaw <cory.shaw.dev@gmail.com>
2017-09-11 14:38:17 -0400

    Update mbsurfer let's split keymap with new RGB key codes for modes

--- keyboards/lets_split/keymaps/mbsurfer/keymap.c
+++ keyboards/lets_split/keymaps/mbsurfer/keymap.c
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,------------------------------------------     ------------------------------------------.
- * |      | Reset|      |      |      |      |     |      |      |      |      |      |  Del |
+ * | Reset|      | Plain|Breath|Rainbw| Swirl|     | Snake|Knight| Xmas |Grdent|      |  Del |
  * |------+------+------+------+------+-------     -------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm|     |AGswap|Qwerty|Colemk|Dvorak|      |      |
  * |------+------+------+------+------+-------     -------+------+------+------+------+------|
@@ -146,10 +146,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------     ------------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
-  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
-  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-  _______, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, C_S_ESC, C_A_DEL \
+  RESET,   _______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, _______, KC_DEL,  \
+  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,  AG_SWAP,  QWERTY,  COLEMAK, DVORAK,  _______, _______, \
+  _______, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD,  RGB_SAI,  RGB_VAD, RGB_VAI, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, C_S_ESC, C_A_DEL \
 )
 
 
