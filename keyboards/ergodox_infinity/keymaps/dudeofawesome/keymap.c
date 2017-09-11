0aa853a47fa3dc668824b1cb7403723891385092
Louis Orleans <louis@orleans.io>
2017-09-11 11:09:12 -0700

    ✨ Updating keymap

--- keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
+++ keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
- * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |---------+------+------+------+------+------|  L2  |           |  L2  |------+------+------+------+------+--------|
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | EPRM  |      |      |      |      |                                       |      |    . |   0  |   =  |      |
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
       KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
       KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
-      KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
+      KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_FN2,
          EPRM, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
                                        KC_TRNS, KC_TRNS,
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       // right hand
       KC_F12,  KC_F6,   KC_F7,   KC_F8,  KC_F9, KC_F10,  KC_TRNS,
-      KC_TRNS, KC_UP,   KC_7,    KC_8,   KC_9,  KC_ASTR, KC_TRNS,
+      KC_FN2,  KC_UP,   KC_7,    KC_8,   KC_9,  KC_ASTR, KC_TRNS,
                KC_DOWN, KC_4,    KC_5,   KC_6,  KC_PLUS, KC_TRNS,
       KC_TRNS, KC_AMPR, KC_1,    KC_2,   KC_3,  KC_BSLS, KC_TRNS,
                         KC_TRNS, KC_DOT, KC_0,  KC_EQL,  KC_TRNS,
@@ -130,15 +130,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
       // left hand
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_BTN2, KC_MS_U, KC_BTN1, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_BTN2, KC_MS_U,  KC_BTN1, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D,  KC_MS_R, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN_3, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS,  KC_TRNS,
 
-                                          KC_TRNS, KC_TRNS,
-                                                   KC_TRNS,
-                                 KC_TRNS, KC_TRNS, KC_TRNS,
+                                           KC_TRNS, KC_TRNS,
+                                                    KC_TRNS,
+                                  KC_TRNS, KC_TRNS, KC_TRNS,
 
       // right hand
       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
