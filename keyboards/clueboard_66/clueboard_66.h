0838461a5865c64d55f39b30791a8d56ab4a90d5
Jack Humbert <jack.humb@gmail.com>
2017-09-14 00:46:29 -0400

    start converting keyboards to new system

--- keyboards/clueboard_66/clueboard_66.h
+++ keyboards/clueboard_66/clueboard_66.h
@@ -1,13 +1,13 @@
 #ifndef CLUEBOARD_H
 #define CLUEBOARD_H
 
-#ifdef SUBPROJECT_rev1
+#ifdef KEYBOARD_clueboard_rev1
     #include "rev1.h"
 #endif
-#ifdef SUBPROJECT_rev2
+#ifdef KEYBOARD_clueboard_rev2
     #include "rev2.h"
 #endif
-#ifdef SUBPROJECT_rev3
+#ifdef KEYBOARD_clueboard_rev3
     #include "rev3.h"
 #endif
 
