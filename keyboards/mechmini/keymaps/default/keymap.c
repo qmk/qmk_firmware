34fa26821fe242f6044783623458394b6bac5d3b
krusli <rusli.kenneth@gmail.com>
2017-09-15 23:56:16 +1000

    Update Mechmini keymap, reduce reported power consumption for iOS Camera Adapter compatibility

--- keyboards/mechmini/keymaps/default/keymap.c
+++ keyboards/mechmini/keymaps/default/keymap.c
@@ -14,15 +14,48 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "mechmini.h"
+#define _BL 0
+#define _FN1 1
+#define _FN2 2
+#define _FN3 3
+#define _____ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    KEYMAP(
-        TAB,  Q,    W,    E,    R,    T,    Y,    U,   I,    O,    P,    BSLS,
-        LCTL, A,    S,    D,    F,    G,    H,    J,   K,    L,    SCLN,
-        LSFT, Z,    X,    C,    V,    B,    N,    M,   COMM, DOT,  SLSH,
-        GRV,  LALT, LGUI,       SPC,        ENT,       RGUI, RALT, RCTL
-    )
+   [_BL] = KEYMAP(
+     KC_ESC,     KC_Q,       KC_W,       KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+     KC_TAB,     KC_A,       KC_S,       KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_ENT,
+     KC_LSFT,    KC_Z,       KC_X,       KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   MO(_FN2),
+     KC_LCTL,    KC_LGUI,    KC_LALT,         _____,         KC_SPC,        _____,                   MO(_FN1), MO(_FN3)
+   ),
+   [_FN1] = KEYMAP(
+     _____,      _____,      KC_UP,      KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_SLCK,  KC_PAUS,  KC_DEL,
+     KC_CAPS,    KC_LEFT,    KC_DOWN,    KC_RIGHT, _____,    _____,    _____,    KC_INS,   KC_HOME,  KC_PGUP,  KC_PSCR,
+     _____,      _____,      _____,      _____,    _____,    _____,    _____,    KC_END,   KC_PGDN,  _____,    _____,
+     _____,      _____,      _____,            _____,        _____,         _____,                   _____,    _____
+   ),
+   [_FN2] = KEYMAP(
+     KC_GRAVE,   KC_1,       KC_2,       KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
+     KC_CAPS,    _____,      _____,      _____,    _____,    KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_MINS,  KC_EQL,   _____,
+     _____,      _____,      _____,      _____,    _____,    _____,    _____,    KC_SCLN,  KC_QUOT,  KC_SLSH,  _____,
+     _____,      _____,      _____,            _____,        _____,         _____,                   _____,    _____
+   ),
+   [_FN3] = KEYMAP(
+     KC_F1,      KC_F2,      KC_F3,      KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+     _____,      _____,      _____,      _____,    _____,    _____,    _____,    _____,    _____,    _____,    _____,
+     _____,      _____,      _____,      _____,    _____,    _____,    _____,    _____,    _____,    _____,    _____,
+     _____,      _____,      _____,            _____,        _____,         _____,                   _____,    _____
+   )
 };
+
+/**
+ * Blank keymap
+ [0] = KEYMAP(
+   _____,      _____,      _____,      _____,    _____,    _____,    _____,    _____,    _____,    _____,    _____,    _____
+   _____,      _____,      _____,      _____,    _____,    _____,    _____,    _____,    _____,    _____,    _____,
+   _____,      _____,      _____,      _____,    _____,    _____,    _____,    _____,    _____,    _____,    _____,
+   _____,      _____,      _____,            _____,        _____,         _____,                   _____,    _____
+ )
+ */
 
 const uint16_t PROGMEM fn_actions[] = {
 };
