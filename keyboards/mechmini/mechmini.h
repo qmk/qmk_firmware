eec7d6b7dbecf99a9b8f35eed2f809da0bb0ca1c
krusli <rusli.kenneth@gmail.com>
2017-09-15 23:56:16 +1000

    Update Mechmini keymap, reduce reported power consumption for iOS Camera Adapter compatibility

--- keyboards/mechmini/mechmini.h
+++ keyboards/mechmini/mechmini.h
@@ -20,18 +20,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "keycode.h"
 #include "action.h"
+#include "quantum.h"
 
 #define KEYMAP( \
     K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, \
     K02, K12, K22, K32, K42, K52, K36, KD6, KD7, K37, KA2, \
     K01, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, \
-    K00, K10, K20,      K56,      K57,      KB0, KC0, K66  \
+    K00, K10, K20,      K56, K57, KB0,           KC0, K66  \
 ) \
 { \
-    { KC_##K00, KC_##K10, KC_##K20, KC_##K56, KC_NO,    KC_NO,    KC_##K57, KC_NO,    KC_##KB0, KC_##KC0, KC_##K66, KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
-    { KC_##K01, KC_##K11, KC_##K21, KC_##K31, KC_##K41, KC_##K51, KC_##K46, KC_##KE6, KC_##KE7, KC_##K47, KC_##KA1, KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
-    { KC_##K02, KC_##K12, KC_##K22, KC_##K32, KC_##K42, KC_##K52, KC_##K36, KC_##KD6, KC_##KD7, KC_##K37, KC_##KA2, KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
-    { KC_##K03, KC_##K13, KC_##K23, KC_##K33, KC_##K43, KC_##K53, KC_##K26, KC_##KC6, KC_##KC7, KC_##K27, KC_##KA3, KC_##KB3, KC_NO,    KC_NO,    KC_NO }, \
+    { K00, K10, K20, K56, KC_NO,    K57,    KC_NO, KC_NO,    KB0, KC0, K66, KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
+    { K01, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
+    { K02, K12, K22, K32, K42, K52, K36, KD6, KD7, K37, KA2, KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
+    { K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, KC_NO,    KC_NO,    KC_NO }, \
     { KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
     { KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
     { KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO }, \
