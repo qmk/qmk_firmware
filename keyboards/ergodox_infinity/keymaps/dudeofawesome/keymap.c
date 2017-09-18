2a8c804f8c3c86c3aaf9758ef92ff2e9cf7b11aa
Louis Orleans <louis@orleans.io>
2017-09-18 13:06:03 -0700

    💄 Changing numpad layout

--- keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
+++ keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
@@ -97,11 +97,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 KC_TRNS, KC_TRNS, KC_TRNS,
 
     // right hand
-    KC_F12,  KC_F6,   KC_F7,   KC_F8,  KC_F9, KC_F10,  KC_TRNS,
-    KC_TRNS, KC_UP,   KC_7,    KC_8,   KC_9,  KC_ASTR, KC_TRNS,
-             KC_DOWN, KC_4,    KC_5,   KC_6,  KC_PLUS, KC_TRNS,
-    KC_TRNS, KC_AMPR, KC_1,    KC_2,   KC_3,  KC_BSLS, KC_TRNS,
-                      KC_TRNS, KC_DOT, KC_0,  KC_EQL,  TO(MDIA),
+    KC_F12,  KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_TRNS,
+    KC_TRNS, KC_UP,   KC_7,    KC_8,   KC_9,   KC_ASTR, KC_TRNS,
+             KC_DOWN, KC_4,    KC_5,   KC_6,   KC_PLUS, KC_TRNS,
+    KC_TRNS, KC_AMPR, KC_1,    KC_2,   KC_3,   KC_BSLS, KC_TRNS,
+                      KC_0,    KC_0,   KC_DOT, KC_EQL,  TO(MDIA),
 
     KC_TRNS, KC_TRNS,
     KC_TRNS,
