3eeab279146684c337e18cc2a69d7fdfd7787c84
Maxr1998 <max.rumpf1998@gmail.com>
2017-09-18 14:00:35 +0200

    Maxr1998's GH60 layout

---
+++ keyboards/gh60/keymaps/maxr1998/rules.mk
@@ -0,0 +1,10 @@
+# Build Options
+#   comment out to disable the options.
+#
+RGBLIGHT_ENABLE = yes # Enable RGB Underglow
+KEY_LOCK_ENABLE = yes # Enable key lock option
+SKIP_GIT = yes
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
