8e207dc9b071e3f9b96abb0f7b42fb4035571f5e
Danny Nguyen <danny@keeb.io>
2017-09-21 15:56:27 -0400

    Implement sticky modifiers

--- keyboards/viterbi/keymaps/dwallace/keymap.c
+++ keyboards/viterbi/keymaps/dwallace/keymap.c
@@ -9,7 +9,12 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  FN,
+  STK_SHIFT,
+  STK_CTRL,
+  STK_ALT,
+  STK_GUI,
+  STK_META,
+  STK_CLEAR,
 };
 
 #define KC_ KC_TRNS
@@ -23,8 +28,13 @@ enum custom_keycodes {
 #define KC_RMOD RGB_MOD
 #define KC_RVAD RGB_VAD
 #define KC_RVAI RGB_VAI
-#define KC_CLRM KC_NO           // TODO: Clear sticky modifiers
 #define KC_RST RESET
+#define KC_SSFT STK_SHIFT
+#define KC_SCTL STK_CTRL
+#define KC_SALT STK_ALT
+#define KC_SGUI STK_GUI
+#define KC_SMTA STK_META
+#define KC_SCLR STK_CLEAR
 
 // TODO: Make modifiers sticky
 
@@ -38,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
      ESC , A  , S  , D  , F  , G  ,RBRC,      H  , J  , K  , L  ,SCLN,QUOT,ENT ,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,SWIN,      N  , M  ,COMM,DOT , UP ,SLSH,RSFT,
+     SSFT, Z  , X  , C  , V  , B  ,SWIN,      N  , M  ,COMM,DOT , UP ,SLSH,RSFT,
   //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
-     LCTL,LCAG,LALT,LGUI,SPC ,CLRM,SAPP,     FN1 ,SPC ,RGUI,LEFT,DOWN,RGHT,PGDN
+     SCTL,SMTA,SALT,SGUI,SPC ,SCLR,SAPP,     FN1 ,SPC ,RGUI,LEFT,DOWN,RGHT,PGDN
   //`----+----+----+----+----+----+----'    `----+----+----+----+----+----+----'
   ),
 
@@ -69,6 +79,47 @@ void persistent_default_layer_set(uint16_t default_layer) {
   default_layer_set(default_layer);
 }
 
+bool modifier_already_applied = false;
+uint8_t physically_held_modifiers = 0;
+
+void update_underglow_level(void) {
+  if (1 == 0) {
+    // TODO: Dim underglow
+    return;
+  }
+  // TODO: Set underglow level high
+}
+
+void add_sticky_modifier(uint16_t keycode) {
+  add_mods(MOD_BIT(keycode));
+  register_code(keycode);
+  modifier_already_applied = false;
+}
+
+void clear_sticky_modifiers(void) {
+  unregister_code(KC_LSFT);
+  unregister_code(KC_LCTL);
+  unregister_code(KC_LALT);
+  unregister_code(KC_LGUI);
+  update_underglow_level();
+}
+
+void handle_sticky_modifier_event(uint16_t keycode, bool pressed) {
+  if (pressed) {
+    add_sticky_modifier(keycode);
+    physically_held_modifiers |= MOD_BIT(keycode);
+  } else {
+    del_mods(MOD_BIT(keycode));
+    physically_held_modifiers &= ~MOD_BIT(keycode);
+    if (modifier_already_applied) {
+      clear_sticky_modifiers();
+    } else {
+      add_sticky_modifier(keycode);
+    }
+  }
+  update_underglow_level();
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
@@ -80,6 +131,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case STK_SHIFT:
+      handle_sticky_modifier_event(KC_LSFT, record->event.pressed);
+      return false;
+      break;
+    case STK_CTRL:
+      handle_sticky_modifier_event(KC_LCTL, record->event.pressed);
+      return false;
+      break;
+    case STK_ALT:
+      handle_sticky_modifier_event(KC_LALT, record->event.pressed);
+      return false;
+      break;
+    case STK_GUI:
+      handle_sticky_modifier_event(KC_LGUI, record->event.pressed);
+      return false;
+      break;
+    case STK_META:
+      handle_sticky_modifier_event(KC_LCTL, record->event.pressed);
+      handle_sticky_modifier_event(KC_LALT, record->event.pressed);
+      handle_sticky_modifier_event(KC_LGUI, record->event.pressed);
+      return false;
+      break;
+    case STK_CLEAR:
+      if (record->event.pressed) {
+        clear_sticky_modifiers();
+      }
+      return false;
+      break;
+  }
+  if (!record->event.pressed && IS_KEY(keycode)) {
+    modifier_already_applied = true;
+    if (physically_held_modifiers == 0)
+      clear_sticky_modifiers();
   }
   return true;
 }
