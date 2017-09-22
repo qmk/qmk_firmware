246ea00ab70b2a8e6fc9ca55a2ba346e62e24519
Louis Orleans <louis@orleans.io>
2017-09-22 01:09:16 -0700

    perf(planck): remove plover
    saves 256 bytes of memory

--- keyboards/planck/keymaps/dudeofawesome/keymap.c
+++ keyboards/planck/keymaps/dudeofawesome/keymap.c
@@ -26,7 +26,6 @@
    _WORKMAN,
    _LOWER,
    _RAISE,
-   _PLOVER,
    _ADJUST
  };
 
@@ -35,11 +34,9 @@
    COLEMAK,
    DVORAK,
    WORKMAN,
-   PLOVER,
    LOWER,
    RAISE,
-   BACKLIT,
-   EXT_PLV
+   BACKLIT
  };
 
  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -152,30 +149,11 @@
    {_______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT}
  },
 
- /* Plover layer (http://opensteno.org)
-  * ,-----------------------------------------------------------------------------------.
-  * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
-  * |------+------+------+------+------+-------------+------+------+------+------+------|
-  * |      |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
-  * |------+------+------+------+------+------|------+------+------+------+------+------|
-  * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
-  * |------+------+------+------+------+------+------+------+------+------+------+------|
-  * | Exit |      |      |   A  |   O  |             |   E  |   U  |      |      |      |
-  * `-----------------------------------------------------------------------------------'
-  */
-
- [_PLOVER] = {
-   {KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   },
-   {XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC},
-   {XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-   {EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX}
- },
-
  /* Adjust (Lower + Raise)
   * ,-----------------------------------------------------------------------------------.
   * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
   * |------+------+------+------+------+-------------+------+------+------+------+------|
-  * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Wrkman|Dvorak|Plover|      |
+  * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Wrkman|Dvorak|      |      |
   * |------+------+------+------+------+------|------+------+------+------+------+------|
   * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
   * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -184,18 +162,13 @@
   */
  [_ADJUST] = {
    {_______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL },
-   {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  WORKMAN, DVORAK,  PLOVER,  _______},
+   {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  WORKMAN, DVORAK,  _______, _______},
    {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
  }
 
 
  };
-
- #ifdef AUDIO_ENABLE
-   float plover_song[][2]     = SONG(PLOVER_SOUND);
-   float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
- #endif
 
  bool process_record_user(uint16_t keycode, keyrecord_t *record) {
    switch (keycode) {
@@ -252,34 +225,6 @@
          #endif
        } else {
          unregister_code(KC_RSFT);
-       }
-       return false;
-       break;
-     case PLOVER:
-       if (record->event.pressed) {
-         #ifdef AUDIO_ENABLE
-           stop_all_notes();
-           PLAY_SONG(plover_song);
-         #endif
-         layer_off(_RAISE);
-         layer_off(_LOWER);
-         layer_off(_ADJUST);
-         layer_on(_PLOVER);
-         if (!eeconfig_is_enabled()) {
-             eeconfig_init();
-         }
-         keymap_config.raw = eeconfig_read_keymap();
-         keymap_config.nkro = 1;
-         eeconfig_update_keymap(keymap_config.raw);
-       }
-       return false;
-       break;
-     case EXT_PLV:
-       if (record->event.pressed) {
-         #ifdef AUDIO_ENABLE
-           PLAY_SONG(plover_gb_song);
-         #endif
-         layer_off(_PLOVER);
        }
        return false;
        break;
