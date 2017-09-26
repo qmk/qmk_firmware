edcf3c76a839794d032f31a58d89b7eee09c16fc
Snipeye <Snipeye@gmail.com>
2017-09-26 17:56:42 -0600

    Fixed typo on POINTING_DEVICE_ENABLE
    It read "POITNING_DEVICE_ENABLE", which would not be terribly helpful.

--- keyboards/dichotemy/rules.mk
+++ keyboards/dichotemy/rules.mk
@@ -64,7 +64,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 #
 #BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
 #MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
-POITNING_DEVICE_ENABLE = yes # Generic Pointer, not as big as mouse keys hopefully.
+POINTING_DEVICE_ENABLE = yes # Generic Pointer, not as big as mouse keys hopefully.
 EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE = yes	# Console for debug(+400)
 COMMAND_ENABLE = yes   # Commands for debug and configuration
