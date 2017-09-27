7b1fb76e013ad8367f7c543b93b267ce524824f0
Snipeye <Snipeye@gmail.com>
2017-09-26 18:34:52 -0600

    Overriding weak attributes in generic pointing_device.c

--- keyboards/dichotemy/dichotemy.c
+++ keyboards/dichotemy/dichotemy.c
@@ -1,7 +1,45 @@
 #include "dichotemy.h"
+#include "pointing_device.h"
 
 void uart_init(void) {
 	SERIAL_UART_INIT();
+}
+
+void pointing_device_task(void){
+    SERIAL_UART_INIT();
+    uint32_t timeout = 0;
+
+    //the m character requests the RF slave to send the mouse report
+    SERIAL_UART_DATA = 'm';
+
+    //trust the external inputs completely, erase old data
+    uint8_t uart_data[11] = {0};
+
+    //there are 10 bytes corresponding to 10 columns, and an end byte
+    for (uint8_t i = 0; i < 11; i++) {
+        //wait for the serial data, timeout if it's been too long
+        //this only happened in testing with a loose wire, but does no
+        //harm to leave it in here
+        while(!SERIAL_UART_RXD_PRESENT){
+            timeout++;
+            if (timeout > 10000){
+                break;
+            }
+        } 
+        uart_data[i] = SERIAL_UART_DATA;
+    }
+
+    //check for the end packet, the key state bytes use the LSBs, so 0xE0
+    //will only show up here if the correct bytes were recieved
+    if (uart_data[10] == 0xE0)
+    {
+        //shifting and transferring the info to the mouse report varaible
+        //mouseReport.x = 127 max -127 min
+        //mouseReport.y = 127 max -127 min
+        //mouseReport.v = 127 max -127 min (scroll vertical)
+        //mouseReport.h = 127 max -127 min (scroll horizontal)
+        //mouseReport.buttons = 0x31 max (bitmask for mouse buttons 1-5) 0x00 min
+    }
 }
 
 void led_init(void) {
