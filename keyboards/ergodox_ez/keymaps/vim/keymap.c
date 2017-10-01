c0e5b6a845c68f09fb7bf4b2c1a720d45180e726
Benny Powers <benny.p@elasticmedia.io>
2017-10-01 07:14:04 +0300

    Move project to new directory structure

--- keyboards/ergodox_ez/keymaps/vim/keymap.c
+++ keyboards/ergodox_ez/keymaps/vim/keymap.c
@@ -1,6 +1,4 @@
-#include "ergodox.h"
-#include "ez.h"
-#include "config.h"
+#include QMK_KEYBOARD_H
 #include "quantum_keycodes.h"
 #include "action_layer.h"
 #include "version.h"
@@ -89,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                                 KC_HOME,KC_END,
                                                                         _______,
-                                                GUI_T(KC_SPC),  NOR_MOD,_______,
+                                                        _______,_______,TT(NOR_MOD),
 
     // Right Hand
                               KC_ATP , KC_6, KC_7,   KC_8,    KC_9,   KC_0,   KC_MINS,
@@ -103,6 +101,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_ENT, GUI_T(KC_BSPC)
   ),
 
+  [SYMB] = KEYMAP(
+    // Left Hand
+    NOR_MOD,KC_1,   KC_2,   KC_3,    KC_4,   KC_5, KC_ATM,
+    KC_TAB ,KC_Q,   KC_W,   KC_E,    KC_R,   KC_T, KC_GRV,
+    _______,KC_A,   KC_S,   KC_D,    KC_F,   KC_G,
+    KC_LSFT,KC_Z,   KC_X,   KC_C,    KC_V,   KC_B, KC_EQL,
+    KC_LCTL,KC_LALT,KC_LPRN,KC_LBRC, KC_LCBR,
+
+                                                                _______,_______,
+                                                                        _______,
+                                                        _______,_______,TT(NOR_MOD),
+
+    // Right Hand
+                              KC_ATP , KC_6, KC_7,   KC_8,    KC_9,   KC_0,   KC_MINS,
+                              KC_MINS, KC_Y, KC_U,   KC_I,    KC_O,   KC_P,   KC_BSLS,
+                                       KC_H, KC_J,   KC_K,    KC_L,   KC_SCLN,KC_QUOT,
+                              KC_PLUS, KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLSH,KC_RSFT,
+                                             KC_RCBR,KC_RBRC, KC_RPRN,KC_RALT,KC_RCTL,
+
+    _______,_______,
+    _______,
+    _______,_______,_______
+  ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
