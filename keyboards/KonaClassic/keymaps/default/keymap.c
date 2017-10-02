1dd7cb463238dfb0e47a6af30383a20e1d5b62b8
Maximillian Hill <mhill@cpay.com>
2017-10-02 10:08:03 -0700

    Fixed KonaClassic config

--- keyboards/KonaClassic/keymaps/default/keymap.c
+++ keyboards/KonaClassic/keymaps/default/keymap.c
@@ -33,126 +33,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_F9, KC_F10, KC_LCTL, KC_LGUI, KC_LALT,  KC_SPC,          KC_RALT, KC_ENT,                   KC_RGUI, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	[_FN] = KEYMAP(
-		F(4),F(5), KC_TILD, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET, \
+		RGB_TOG, RGB_MOD, KC_TILD, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET, \
 		______,  ______,  ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, \
-		F(6),F(7), ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, \
-		F(8),F(9), ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, \
-		F(10),F(11), ______,  ______, ______, ______,         ______, ______,                 ______, ______, ______, ______, ______
+		RGB_HUI, RGB_HUD, ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, \
+		RGB_SAI, RGB_SAD, ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, \
+		RGB_VAI, RGB_VAD, ______,  ______, ______, ______,         ______, ______,                 ______, ______, ______, ______, ______
 	)
-};
-
-enum function_id {
-    RGBLED_TOGGLE,
-    RGBLED_STEP_MODE,
-    RGBLED_INCREASE_HUE,
-    RGBLED_DECREASE_HUE,
-    RGBLED_INCREASE_SAT,
-    RGBLED_DECREASE_SAT,
-    RGBLED_INCREASE_VAL,
-    RGBLED_DECREASE_VAL,
-    SHIFT_ESC,
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-    [0]  = ACTION_LAYER_MOMENTARY(2),  // Momentary Fn overlay
-    [1]  = ACTION_LAYER_TOGGLE(1),     // Toggle Arrow Layer overlay
-    [2]  = ACTION_LAYER_TAP_KEY(2, KC_CAPS), // Tap to toggle caps lock and hold to activate function layer
-    [3]  = ACTION_LAYER_TOGGLE(3),     // Toggle Underglow Layer overlay
-    [4]  = ACTION_FUNCTION(RGBLED_TOGGLE), //Turn on/off underglow
-    [5]  = ACTION_FUNCTION(RGBLED_STEP_MODE), // Change underglow mode
-    [6]  = ACTION_FUNCTION(RGBLED_INCREASE_HUE),
-    [7]  = ACTION_FUNCTION(RGBLED_DECREASE_HUE),
-    [8]  = ACTION_FUNCTION(RGBLED_INCREASE_SAT),
-    [9]  = ACTION_FUNCTION(RGBLED_DECREASE_SAT),
-    [10] = ACTION_FUNCTION(RGBLED_INCREASE_VAL),
-    [11] = ACTION_FUNCTION(RGBLED_DECREASE_VAL),
-    [12] = ACTION_FUNCTION(SHIFT_ESC),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-  // MACRODOWN only works in this function
-      switch(id) {
+    // MACRODOWN only works in this function
+    switch(id) {
         case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
+            if (record->event.pressed) {
+                register_code(KC_RSFT);
+            } else {
+                unregister_code(KC_RSFT);
+            }
+            break;
+    }
     return MACRO_NONE;
 };
 
+
+void matrix_init_user(void) {
+
+}
+
 void matrix_scan_user(void) {
-};
 
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+}
 
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  switch (id) {
-    case RGBLED_TOGGLE:
-      //led operations
-      if (record->event.pressed) {
-        rgblight_toggle();
-        }
-        break;
-    case RGBLED_INCREASE_HUE:
-      if (record->event.pressed) {
-        rgblight_increase_hue();
-        }
-        break;
-    case RGBLED_DECREASE_HUE:
-      if (record->event.pressed) {
-        rgblight_decrease_hue();
-        }
-        break;
-    case RGBLED_INCREASE_SAT:
-      if (record->event.pressed) {
-        rgblight_increase_sat();
-        }
-        break;
-    case RGBLED_DECREASE_SAT:
-      if (record->event.pressed) {
-        rgblight_decrease_sat();
-        }
-        break;
-    case RGBLED_INCREASE_VAL:
-        if (record->event.pressed) {
-          rgblight_increase_val();
-        }
-        break;
-    case RGBLED_DECREASE_VAL:
-        if (record->event.pressed) {
-          rgblight_decrease_val();
-        }
-        break;
-    case RGBLED_STEP_MODE:
-        if (record->event.pressed) {
-          rgblight_step();
-        }
-        break;
-    static uint8_t shift_esc_shift_mask;
-    // Shift + ESC = ~
-    case SHIFT_ESC:
-      shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
-      if (record->event.pressed) {
-        if (shift_esc_shift_mask) {
-          add_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          add_key(KC_ESC);
-          send_keyboard_report();
-        }
-      } else {
-        if (shift_esc_shift_mask) {
-          del_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          del_key(KC_ESC);
-          send_keyboard_report();
-        }
-      }
-      break;
-    }
-};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
