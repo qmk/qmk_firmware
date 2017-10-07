1305bc284ea2f0794815388901378c9a23ea11ab
Masayuki Sunahara <tamanishi@gmail.com>
2017-10-07 23:26:16 +0900

    swap LGUI and Lower

--- keyboards/nyquist/keymaps/tamanishi/keymap.c
+++ keyboards/nyquist/keymaps/tamanishi/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Adjust| Ctrl | Alt  | GUI  |Lower |Space |Space | GUI  |   -  |   =  |   [  |   ]  |
+ * |Adjust| Ctrl | Alt  | Lower| GUI  |Space |Space | GUI  |   -  |   =  |   [  |   ]  |
  * `-----------------------------------------------------------------------------------'
  */
         [_QWERTY] = KEYMAP(
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
             KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
             KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-            ADJUST, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, KC_RGUI, KC_MINS, KC_EQL, KC_LBRC, KC_RBRACKET),
+            ADJUST, KC_LCTL, KC_LALT, LOWER, KC_LGUI, KC_SPC, KC_SPC, KC_RGUI, KC_MINS, KC_EQL, KC_LBRC, KC_RBRACKET),
 
         /* Colemak
  * ,-----------------------------------------------------------------------------------.
