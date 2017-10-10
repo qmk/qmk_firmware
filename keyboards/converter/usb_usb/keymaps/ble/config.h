a2754289e63b862d2587e1eaf0142abd31e8899c
Benjamin Dengler <benjamin.dengler@gmail.com>
2017-10-10 19:09:19 +0200

    Added BLE support to USB-USB converter

---
+++ keyboards/converter/usb_usb/keymaps/ble/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// Turn off the mode leds on the BLE module
+#define ADAFRUIT_BLE_ENABLE_MODE_LEDS 0
+#define ADAFRUIT_BLE_ENABLE_POWER_LED 0
+
+#endif
