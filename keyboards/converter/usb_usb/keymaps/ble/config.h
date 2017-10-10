e9476674c914853ade6a8d4411e470c3ae53d3fa
Benjamin Dengler <benjamin.dengler@gmail.com>
2017-10-10 20:30:54 +0200

    Added readme

--- keyboards/converter/usb_usb/keymaps/ble/config.h
+++ keyboards/converter/usb_usb/keymaps/ble/config.h
@@ -3,6 +3,11 @@
 
 #include "../../config.h"
 
+#undef PRODUCT
+#define PRODUCT         QMK BLE Adapter
+#undef DESCRIPTION
+#define DESCRIPTION  
+
 // Turn off the mode leds on the BLE module
 #define ADAFRUIT_BLE_ENABLE_MODE_LEDS 0
 #define ADAFRUIT_BLE_ENABLE_POWER_LED 0
