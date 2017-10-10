b15920af6e85d129294006ffe92c20dfb3621802
Kyle Montag <thekylemontag@gmail.com>
2017-10-10 00:38:30 -0400

    return to base config for xd60.c

--- keyboards/xd60/xd60.c
+++ keyboards/xd60/xd60.c
@@ -11,15 +11,12 @@ extern inline void xd60_bl_led_off(void);
 void led_set_kb(uint8_t usb_led) {
 	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
 
-  /* if (usb_led & (1<<USB_LED_CAPS_LOCK)) { */
-	/* 	xd60_caps_led_on(); */
-  /*   xd60_bl_led_on(); */
-	/* } else { */
-	/* 	xd60_caps_led_off(); */
-  /*   xd60_bl_led_off(); */
-  /* } */
+  if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+		xd60_caps_led_on();
+	} else {
+		xd60_caps_led_off();
+  }
 
-  xd60_bl_led_off();
   // if (usb_led & (1<<USB_LED_NUM_LOCK)) {
 	// xd60_esc_led_on();
 	// } else {
