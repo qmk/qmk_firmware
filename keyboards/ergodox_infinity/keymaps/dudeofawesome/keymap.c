c8793c3d417d206a5ca64503517a4daeeaf86d49
Louis Orleans <louis@orleans.io>
2017-10-14 14:15:41 -0700

    🐛 fix numpad

--- keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
+++ keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
@@ -430,7 +430,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
     KC_F12,  KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,         KC_F11,
     _______, _______, KC_KP_7,   KC_KP_8, KC_KP_9, KC_KP_ASTERISK, _______,
-             _______, KC_KP_4,   KC_KP_5, KC_KP_7, KC_KP_PLUS,     _______,
+             _______, KC_KP_4,   KC_KP_5, KC_KP_6, KC_KP_PLUS,     _______,
     _______, _______, KC_KP_1,   KC_KP_2, KC_KP_3, KC_KP_SLASH,    _______,
                       KC_KP_DOT, KC_KP_0, KC_KP_0, KC_KP_EQUAL,    _______,
 
