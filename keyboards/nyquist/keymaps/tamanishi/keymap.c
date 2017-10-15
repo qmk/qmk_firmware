c2061e2b6852978435579957e4d2bd0ddeb8d3a9
Masayuki Sunahara <tamanishi@gmail.com>
2017-10-15 09:32:31 +0900

    add mouse moving to adjust layer.

--- keyboards/nyquist/keymaps/tamanishi/keymap.c
+++ keyboards/nyquist/keymaps/tamanishi/keymap.c
@@ -17,12 +17,12 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes
 {
-  QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
-  LOWER,
-  RAISE,
-  ADJUST,
+    QWERTY = SAFE_RANGE,
+    COLEMAK,
+    DVORAK,
+    LOWER,
+    RAISE,
+    ADJUST,
 };
 
 // Fillers to make layering more clear
@@ -137,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |MsLeft|MsDown| MsUp |MsRght|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_ADJUST] = KEYMAP(
             KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)
 
@@ -161,81 +161,81 @@ float tone_colemak[][2] = SONG(COLEMAK_SOUND);
 
 void persistent_default_layer_set(uint16_t default_layer)
 {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
+    eeconfig_update_default_layer(default_layer);
+    default_layer_set(default_layer);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
-  switch (keycode)
-  {
-  case QWERTY:
-    if (record->event.pressed)
+    switch (keycode)
     {
+    case QWERTY:
+        if (record->event.pressed)
+        {
 #ifdef AUDIO_ENABLE
-      PLAY_SONG(tone_qwerty);
+            PLAY_SONG(tone_qwerty);
 #endif
-      persistent_default_layer_set(1UL << _QWERTY);
-    }
-    return false;
-    break;
-  case COLEMAK:
-    if (record->event.pressed)
-    {
+            persistent_default_layer_set(1UL << _QWERTY);
+        }
+        return false;
+        break;
+    case COLEMAK:
+        if (record->event.pressed)
+        {
 #ifdef AUDIO_ENABLE
-      PLAY_SONG(tone_colemak);
+            PLAY_SONG(tone_colemak);
 #endif
-      persistent_default_layer_set(1UL << _COLEMAK);
-    }
-    return false;
-    break;
-  case DVORAK:
-    if (record->event.pressed)
-    {
+            persistent_default_layer_set(1UL << _COLEMAK);
+        }
+        return false;
+        break;
+    case DVORAK:
+        if (record->event.pressed)
+        {
 #ifdef AUDIO_ENABLE
-      PLAY_SONG(tone_dvorak);
+            PLAY_SONG(tone_dvorak);
 #endif
-      persistent_default_layer_set(1UL << _DVORAK);
+            persistent_default_layer_set(1UL << _DVORAK);
+        }
+        return false;
+        break;
+    case LOWER:
+        if (record->event.pressed)
+        {
+            layer_on(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        else
+        {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        return false;
+        break;
+    case RAISE:
+        if (record->event.pressed)
+        {
+            layer_on(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        else
+        {
+            layer_off(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        return false;
+        break;
+    case ADJUST:
+        if (record->event.pressed)
+        {
+            layer_on(_ADJUST);
+        }
+        else
+        {
+            layer_off(_ADJUST);
+        }
+        return false;
+        break;
     }
-    return false;
-    break;
-  case LOWER:
-    if (record->event.pressed)
-    {
-      layer_on(_LOWER);
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    }
-    else
-    {
-      layer_off(_LOWER);
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    }
-    return false;
-    break;
-  case RAISE:
-    if (record->event.pressed)
-    {
-      layer_on(_RAISE);
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    }
-    else
-    {
-      layer_off(_RAISE);
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-    }
-    return false;
-    break;
-  case ADJUST:
-    if (record->event.pressed)
-    {
-      layer_on(_ADJUST);
-    }
-    else
-    {
-      layer_off(_ADJUST);
-    }
-    return false;
-    break;
-  }
-  return true;
+    return true;
 }
