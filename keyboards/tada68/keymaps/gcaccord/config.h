23b923e8a31d769fc08c0aa667dcf3663b0cbbb6
giuseppe <g.caferra@outlook.com>
2017-10-17 21:04:16 +0200

    Mods added to home row WIP
    Ctrl Mod on F and J
    Shift Mod on D and K
    ALt Mod on S and L

---
+++ keyboards/tada68/keymaps/gcaccord/config.h
@@ -0,0 +1,8 @@
+#include "../../config.h"
+
+#define COMBO_COUNT 1
+
+#define BACKLIGHT_BREATHING
+
+#define TAPPING_TERM 200
+
--- keyboards/tada68/keymaps/gcaccord/keymap.c
+++ keyboards/tada68/keymaps/gcaccord/keymap.c
@@ -1,5 +1,4 @@
 #include "tada68.h"
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -10,7 +9,15 @@
 
 #define _______ KC_TRNS
 
-#define SOFT_CTRL 
+enum process_combo_event {
+  WF_CTRL 
+  };
+
+const uint16_t PROGMEM ctrl_combo[] = {KC_J, KC_K, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [WF_CTRL] = COMBO_ACTION(ctrl_combo),
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
@@ -69,7 +76,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_TL] = KEYMAP_ANSI(
   KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  KC_LCTL, KC_A,   KC_S,   KC_D,   CTL_T(KC_F),   KC_G,   KC_H,   CTL_T(KC_J),KC_K,KC_L,KC_SCLN,KC_QUOT,KC_ENT,KC_PGUP,  \
+  KC_LCTL, KC_A,   ALT_T(KC_S),   SFT_T(KC_D),   CTL_T(KC_F),   KC_G,   KC_H,   CTL_T(KC_J),SFT_T(KC_K),ALT_T(KC_L),KC_SCLN,KC_QUOT,KC_ENT,KC_PGUP,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT, KC_UP, KC_PGDN, \
   KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                        TO(_BL), MO(_FL), KC_RCTRL, KC_LEFT, KC_DOWN, KC_RGHT)
 };
+
+
+void process_combo_event(uint8_t combo_index, bool pressed) {
+  switch(combo_index) {
+  case WF_CTRL:
+      if (pressed) {
+            register_code(KC_LCTRL);
+              send_keyboard_report();
+              unregister_code(KC_LCTRL);
+              
+      }
+      break;
+    }
+}
