6191c7aac34da5c38976c9c2a2d8ae914c9dc08b
mogranjm <ja.morgan1@outlook.com>
2017-10-17 15:40:36 +1100

    Updated rules.mk
    Inadvertantly removed important code from the rules.mk in previous commit. This has been restored.
    
    Also disabled Tap_Dance in both rules.mk files

--- keyboards/nyquist/keymaps/DivergeJM/rules.mk
+++ keyboards/nyquist/keymaps/DivergeJM/rules.mk
@@ -31,4 +31,4 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-TAP_DANCE_ENABLE = yes      # Enable Tap Dance
+TAP_DANCE_ENABLE = no      # Enable Tap Dance
