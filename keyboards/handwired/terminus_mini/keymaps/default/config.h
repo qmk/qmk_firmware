470440984aebb613710ea1a38dc9e5ce1326b677
mogranjm <ja.morgan1@outlook.com>
2017-10-27 16:53:10 +1100

    README fix & keymap update for 3 keyboards
    Fixed the markdown for the handwired/terminus_mini:default, Nyqyist:DivergeJM & frosty_flake:QFR_JM.
    
    Added TAPPING_TERM =  150 to config.h for all keyboards
    
    Switched LT(LOWER) and LAlt on the mod row for ortholinear boards.

--- keyboards/handwired/terminus_mini/keymaps/default/config.h
+++ keyboards/handwired/terminus_mini/keymaps/default/config.h
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define TAPPING_TERM 150 //reduce time required to register a held key
 
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
