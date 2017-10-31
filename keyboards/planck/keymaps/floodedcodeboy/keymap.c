f61e4a2fc22b5e851176f592b77f651ac509b863
Jacob Gabriel <floodedcodeboy@gmail.com>
2017-10-31 21:51:04 +0000

    add in iterm toggle key, move enter back to rhs as MT with shift

--- keyboards/planck/keymaps/floodedcodeboy/keymap.c
+++ keyboards/planck/keymaps/floodedcodeboy/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | TBC1 | Ctrl | Alt  | GUI  |Lower |Enter |Space |Raise |   /  | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  *
- * TBC1 - possible switch to windows control layer / terminal/amythest controls
+ * TBC1 - toggle Iterm
  * LShift - Shift when held / Caps when tapped
  * Enter - MOD TAP with ENTER / R.SHIFT If held
  * Esc - Esc then ~ when pressed with Shift
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_QWERTY] = {
             {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
             {KC_GESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
-            {KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_RSPC},
-            {_______, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_ENTER, KC_SPC, RAISE, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT}},
+            {KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, MT(MOD_RSFT, KC_ENTER)},
+            {LCTL(KC_TILDE), KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT}},
 
         /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -60,7 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * CMD+~ to bring up iterm
  * AMY - Amythest Layer
  */
-        [_LOWER] = {{KC_MUTE, KC_F1, KC_F2, KC_F3, _______, KC_LCBR, KC_RCBR, KC_SLASH, KC_ASTERISK, KC_KP_7, KC_KP_8, KC_KP_9}, {KC_VOLU, KC_F4, KC_F5, KC_F6, _______, KC_LPRN, KC_RPRN, KC_MINUS, KC_PLUS, KC_KP_4, KC_KP_5, KC_KP_6}, {KC_VOLD, KC_F7, KC_F8, KC_F9, _______, KC_PIPE, KC_BSLASH, _______, KC_KP_0, KC_KP_1, KC_KP_3, KC_KP_4}, {_______, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}},
+        [_LOWER] = {
+            {KC_MUTE, KC_F1, KC_F2, KC_F3, _______, KC_LCBR, KC_RCBR, KC_SLASH, KC_ASTERISK, KC_KP_7, KC_KP_8, KC_KP_9},
+            {KC_VOLU, KC_F4, KC_F5, KC_F6, _______, KC_LPRN, KC_RPRN, KC_MINUS, KC_PLUS, KC_KP_4, KC_KP_5, KC_KP_6},
+            {KC_VOLD, KC_F7, KC_F8, KC_F9, _______, KC_PIPE, KC_BSLASH, _______, KC_KP_0, KC_KP_1, KC_KP_3, KC_KP_4},
+            {_______, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}
+        },
 
         /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -73,7 +78,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Acc2 |      |      |      |      |             |      | Prev | Vol- | Vol+ | Next |
  * `-----------------------------------------------------------------------------------'
  */
-        [_RAISE] = {{KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL}, {KC_MS_ACCEL0, KC_BTN2, KC_MS_UP, KC_BTN1, KC_MS_WH_UP, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS}, {KC_MS_ACCEL1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______}, {KC_MS_ACCEL2, _______, _______, _______, _______, _______, _______, _______, KC_MEDIA_PREV_TRACK, KC_VOLD, KC_VOLU, KC_MEDIA_NEXT_TRACK}},
+        [_RAISE] = {
+            {KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL},
+            {KC_MS_ACCEL0, KC_BTN2, KC_MS_UP, KC_BTN1, KC_MS_WH_UP, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS},
+            {KC_MS_ACCEL1, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
+            {KC_MS_ACCEL2, _______, _______, _______, _______, _______, _______, _______, KC_MEDIA_PREV_TRACK, KC_VOLD, KC_VOLU, KC_MEDIA_NEXT_TRACK}
+        },
 
         /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
@@ -86,7 +96,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-        [_ADJUST] = {{_______, RESET, DEBUG, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_DEL}, {_______, _______, MU_MOD, AU_ON, AU_OFF, AG_NORM, AG_SWAP, QWERTY, _______, _______, _______, _______}, {_______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, _______, _______, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}},
+        [_ADJUST] = {
+            {_______, RESET, DEBUG, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_DEL},
+            {_______, _______, MU_MOD, AU_ON, AU_OFF, AG_NORM, AG_SWAP, QWERTY, _______, _______, _______, _______},
+            {_______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, _______, _______, _______, _______},
+            {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+        },
 
         /* Amythest ()
  * ,-----------------------------------------------------------------------------------.
