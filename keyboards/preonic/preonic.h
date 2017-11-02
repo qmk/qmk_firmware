d894cde3d5194e2c67c2236db9f1570350ff67dc
Jack Humbert <jack.humb@gmail.com>
2017-11-02 12:18:18 -0400

    update preonic, add info.json

--- keyboards/preonic/preonic.h
+++ keyboards/preonic/preonic.h
@@ -3,7 +3,7 @@
 
 #include "quantum.h"
 
-#define PREONIC_MIT( \
+#define LAYOUT_preonic_mit( \
 	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
 	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, \
 	k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, \
@@ -18,7 +18,7 @@
 	{ k40, k41, k42, k43, k44, k45, k45, k47, k48, k49, k4a, k4b } \
 }
 
-#define PREONIC_GRID( \
+#define LAYOUT_preonic_grid( \
 	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \
 	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, \
 	k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, \
@@ -33,7 +33,7 @@
 	{ k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b } \
 }
 
-#define KEYMAP PREONIC_MIT
-#define LAYOUT_ortho_5x12 PREONIC_GRID
+#define KEYMAP LAYOUT_preonic_mit
+#define LAYOUT_ortho_5x12 LAYOUT_preonic_grid
 
 #endif
