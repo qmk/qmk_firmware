697b86cdb0030e0ad7931dbec8dc94e1ef733af4
mogranjm <ja.morgan1@outlook.com>
2017-11-03 10:04:11 +1100

    Update DivergeJM
    Switched master to Left hand,
    Moved Reset key to a different location

--- keyboards/nyquist/keymaps/DivergeJM/config.h
+++ keyboards/nyquist/keymaps/DivergeJM/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #ifdef SUBPROJECT_rev1
--- keyboards/nyquist/keymaps/DivergeJM/keymap.c
+++ keyboards/nyquist/keymaps/DivergeJM/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'    `-----------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  _______, XXXXXXX,  XXXXXXX,  XXXXXXX,  RESET,   XXXXXXX,  KC_PEQL, KC_PSLS, KC_PAST, KC_MINS,  XXXXXXX, _______, \
+  _______, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, RESET,    KC_PEQL, KC_PSLS, KC_PAST, KC_MINS,  XXXXXXX, _______, \
   _______, KC_PGUP,  KC_HOME,  KC_UP,    KC_END,  XXXXXXX,  KC_7,    KC_8,    KC_9,    KC_PPLS,  XXXXXXX, _______,\
   _______, KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT, XXXXXXX,  KC_4,    KC_5,    KC_6,    KC_PENT,  XXXXXXX, _______,\
   _______, _______,  _______,  _______,  _______, XXXXXXX,  KC_1,    KC_2,    KC_3,    KC_PENT,  XXXXXXX, _______,\
