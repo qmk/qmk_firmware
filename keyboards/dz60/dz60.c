392c3e0d243417b02666cefeb2443d6a02e54708
Jennifer <me@nowhere>
2017-11-03 01:31:33 -0700

    have led default to off on start up

--- keyboards/dz60/dz60.c
+++ keyboards/dz60/dz60.c
@@ -16,7 +16,9 @@ void matrix_scan_kb(void) {
 
 void led_init_ports(void) {
   // Set caps lock LED pin as output
-  DDRB |= (1<<2);
+  DDRB |= (1 << 2);
+  // Default to off
+  PORTB |= (1 << 2);
 }
 
 void led_set_kb(uint8_t usb_led) {
