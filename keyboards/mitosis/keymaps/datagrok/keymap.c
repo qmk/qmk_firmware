24a4f6f9198e20751f44b3fdee6e3c6bdfe60de6
Michael F. Lamb <mike@datagrok.org>
2017-11-03 02:26:52 -0700

    mitosis/datagrok: apply same punctuation hack to qwerty layer

--- keyboards/mitosis/keymaps/datagrok/keymap.c
+++ keyboards/mitosis/keymaps/datagrok/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_xQ] = {
     {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P},
     {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
-    {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
+    {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOT},
     {XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX},
     {XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX}
   },
