6c1d2b78a2631251de2b61c3eeb5759118696e65
Michael F. Lamb <mike@datagrok.org>
2017-11-03 02:26:31 -0700

    mitosis/datagrok: a minor layout improvement simplifies custom-shifted code
    instead of [, .] [? !], using [, ?] [. !] greatly simplifies the code
    needed to perform the shifted-key switching. (And keeps , and . on the
    same keys that they are under qwerty.)
    
    also: layout improvements for symbols

--- keyboards/mitosis/keymaps/datagrok/keymap.c
+++ keyboards/mitosis/keymaps/datagrok/keymap.c
@@ -9,12 +9,6 @@ enum mitosis_layers
     _xF  // functions
 };
 
-enum mitosis_keycodes
-{
-  COMMADOT = SAFE_RANGE,
-  QUESBANG
-};
-
 // Fillers to make layering more clear
 #define XXXXXXX KC_NO   // No-op (no key in this location on Mitosis' fake matrix)
 #define _______ KC_TRNS // Transparent, because I haven't decided a mapping yet
@@ -34,11 +28,11 @@ enum mitosis_keycodes
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_xW] = {
-        {KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,     KC_P,     KC_SCLN},
-        {KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,     KC_O,     KC_I},
-        {KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    COMMADOT, QUESBANG, KC_QUOT},
-        {XXXXXXX, KC_LSUP, KC_LCTL, MO(_xN), KC_LSFT, KC_RSFT, MO(_xN), KC_RCTL,  KC_RSUP,  XXXXXXX},
-        {XXXXXXX, KC_LHYP, KC_LMTA, KC_BSPC, MO(_xS), MO(_xS), KC_SPC,  KC_RMTA,  KC_RHYP,  XXXXXXX}
+        {KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN},
+        {KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I},
+        {KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_QUOT},
+        {XXXXXXX, KC_LSUP, KC_LCTL, MO(_xN), KC_LSFT, KC_RSFT, MO(_xN), KC_RCTL, KC_RSUP, XXXXXXX},
+        {XXXXXXX, KC_LHYP, KC_LMTA, MO(_xS), KC_BSPC, KC_SPC,  MO(_xS), KC_RMTA, KC_RHYP, XXXXXXX}
     },
     [_xQ] = {
         {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P},
@@ -48,31 +42,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX}
     },
     [_xS] = {
-        {KC_ESC,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_PIPE, KC_GRV,  KC_TILD},
+        {KC_ESC,  _______, KC_UP,   _______, _______, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TILD},
+        {KC_TAB,  KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_CIRC, KC_AMPR, KC_PIPE, KC_GRV,  KC_UNDS},
         {KC_BSLS, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_SLSH},
-        {KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______},
         {XXXXXXX, _______, _______, MO(_xF), _______, _______, MO(_xF), _______, _______, XXXXXXX},
-        {XXXXXXX, _______, _______, KC_DEL,  ___M___, ___M___, KC_ENT,  _______, _______, XXXXXXX},
+        {XXXXXXX, _______, _______, ___M___, KC_DEL,  KC_ENT,  ___M___, _______, _______, XXXXXXX},
     },
     [_xN] = {
-        {_______, _______, KC_UP,   _______, _______, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_P0},
-        {_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS},
+        {_______, _______, _______, _______, _______, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_P0},
+        {_______, _______, _______, _______, _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS},
         {_______, _______, _______, _______, _______, KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_PEQL},
         {XXXXXXX, _______, _______, ___M___, _______, _______, ___M___, _______, _______, XXXXXXX},
-        {XXXXXXX, _______, _______, _______, MO(_xF), MO(_xF), KC_PENT, _______, _______, XXXXXXX},
+        {XXXXXXX, _______, _______, MO(_xF), _______, KC_PENT, MO(_xF), _______, _______, XXXXXXX},
     },
     [_xF] = {
-        {KC_PWR,  _______, KC_PGUP, _______, KC_VOLU, KC_NLCK, KC_F7,   KC_F8,   KC_F9,   KC_F10},
-        {KC_CAPS, KC_HOME, KC_PGDN, KC_END,  KC_VOLD, KC_SLCK, KC_F4,   KC_F5,   KC_F6,   KC_F11},
-        {_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_MENU, KC_F1,   KC_F2,   KC_F3,   KC_F12},
+        {KC_PWR,  _______, KC_PGUP, _______, KC_VOLU, KC_F13,  KC_F7,   KC_F8,   KC_F9,   KC_F10},
+        {_______, KC_HOME, KC_PGDN, KC_END,  KC_VOLD, KC_F14,  KC_F4,   KC_F5,   KC_F6,   KC_F11},
+        {_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_F15,  KC_F1,   KC_F2,   KC_F3,   KC_F12},
         {XXXXXXX, _______, _______, ___M___, _______, _______, ___M___, _______, _______, XXXXXXX},
         {XXXXXXX, _______, _______, _______, ___M___, ___M___, _______, _______, _______, XXXXXXX},
     },
 };
 
-/* This pile of ugly code is a hack to place <period> on <shift-comma>, have
- * <question mark> accessible unshifted, and place <exclaimation mark> on
- * shift+<question mark>, when using an operating system configured for a
+/* This is a hack to place <question mark> on <shift-comma> and <exclaimation
+ * mark> on <shift-period>, when using an operating system configured for a
  * US/qwerty layout. I expect that once I get it working I'll promptly stash it
  * away in a layer or an IFDEF because it's way less hacky to do this by
  * configuring my keymap in Linux.
@@ -80,59 +73,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool comm_shifted = false;
 bool ques_shifted = false;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    uint8_t lshift;
-    uint8_t rshift;
-
-    if (!(keycode == COMMADOT || keycode == QUESBANG)) return true;
-
-    lshift = get_mods() & MOD_BIT(KC_LSHIFT);
-    rshift = get_mods() & MOD_BIT(KC_RSHIFT);
+    uint8_t shifted;
+    uint16_t s_keycode;
+    bool *k_shifted;
 
     switch (keycode) {
-        case COMMADOT:
-            if (record->event.pressed) {
-                if (lshift || rshift) {
-                    // unshift for dot then restore shift
-                    comm_shifted = true;
-                    if (lshift) unregister_code(KC_LSHIFT);
-                    if (rshift) unregister_code(KC_RSHIFT);
-                    register_code(KC_DOT);
-                    if (lshift) register_code(KC_LSHIFT);
-                    if (rshift) register_code(KC_RSHIFT);
-                } else {
-                    register_code(KC_COMM);
-                }
-            } else {
-                if (comm_shifted) {
-                    comm_shifted = false;
-                    unregister_code(KC_DOT);
-                } else {
-                    unregister_code(KC_COMM);
-                }
-            }
+        case KC_COMM:
+            s_keycode = KC_SLSH;
+            k_shifted = &comm_shifted;
             break;
-        case QUESBANG:
-            if (record->event.pressed) {
-                if (lshift || rshift) {
-                    // ! is shifted, no need to unshift
-                    ques_shifted = true;
-                    register_code(KC_1); // will be shifted to !
-                } else {
-                    register_code(KC_LSHIFT);
-                    register_code(KC_SLSH);
-                    unregister_code(KC_LSHIFT);
-                }
-            } else {
-                if (ques_shifted) {
-                    ques_shifted = false;
-                    unregister_code(KC_1);
-                } else {
-                    unregister_code(KC_SLSH);
-                }
-            }
+        case KC_DOT:
+            s_keycode = KC_1;
+            k_shifted = &ques_shifted;
             break;
+        default:
+            return true;
     }
-    return false;
+
+    shifted = get_mods() & (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT));
+
+    // Keydown. If shift is currently pressed, register its alternate keycode.
+    if (record->event.pressed && shifted) {
+        *k_shifted = true;
+        register_code(s_keycode);
+        return false;
+    // Keyup. If shift was pressed back when the key was pressed, unregister
+    // its alternate keycode.
+    } else if (!(record->event.pressed) && *k_shifted) {
+        *k_shifted = false;
+        unregister_code(s_keycode);
+        return false;
+    // Otherwise, behave as normal.
+    } else {
+        return true;
+    }
 }
 
 // Set the bits of A selected by MASK to the corresponding bits of B
