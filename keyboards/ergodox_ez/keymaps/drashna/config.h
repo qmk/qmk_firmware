8bfd750699950c03fe16bca10e1c4743ee2dd8ba
Drashna Jael're <drashna@live.com>
2017-11-04 21:57:04 -0700

    Revert "Rename ez keymaps for userspace"
    This reverts commit c25425911852e41711a5f0273b5741adb16e5bd4.

--- keyboards/ergodox_ez/keymaps/drashna/config.h
+++ keyboards/ergodox_ez/keymaps/drashna/config.h
@@ -1,19 +1,11 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
+
 #include "../../config.h"
 
 
-#define TAPPING_TERM 200
 
-#ifdef RGBLIGHT_ENABLE
-#undef RGBLIGHT_SAT_STEP 
-#define RGBLIGHT_SAT_STEP 12
-#define RGBLIGHT_EFFECT_KNIGHT_LENGTH 7
-#define RGBLIGHT_EFFECT_SNAKE_LENGTH 7
-#define RGBLIGHT_EFFECT_BREATHE_CENTER 1
-#endif // RGBLIGHT_ENABLE
+// make this easy to toggle behavior, so that it can be more easily toggled
+#define LAYER_UNDERGLOW_LIGHTING  
 
-#undef DEBOUNCE
-#define DEBOUNCE    5
-
+#ifdef LAYER_UNDERGLOW_LIGHTING
+#undef RGBLIGHT_ANIMATIONS
 #endif
--- users/drashna/drashna.c
+++
@@ -1,5 +0,0 @@
-#include "drashna.h"
-
-void my_custom_function(void) {
-    
-}
