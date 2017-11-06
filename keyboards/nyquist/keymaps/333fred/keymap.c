f360115c69bb5ed90e4e8aa5bd4a2e18247d2461
Fredric Silberberg <fred@silberberg.xyz>
2017-11-05 22:19:05 -0800

    Set up tap dance for layers on the lower button.

--- keyboards/nyquist/keymaps/333fred/keymap.c
+++ keyboards/nyquist/keymaps/333fred/keymap.c
@@ -1,5 +1,6 @@
 #include "nyquist.h"
 #include "action_layer.h"
+#include "action_util.h"
 #include "eeconfig.h"
 
 extern keymap_config_t keymap_config;
@@ -15,6 +16,68 @@ enum custom_macros {
     PSCREEN_APP
 };
 
+// Tap dance for layer transitions
+enum tap_dance_declarations {
+    TD_LWR_VIM = 0
+};
+
+typedef enum {
+    SINGLE_TAP, SINGLE_HOLD, DOUBLE
+} tap_dance_state_enum;
+
+static tap_dance_state_enum tap_dance_state;
+static bool tap_dance_active = false;
+
+void tap_dance_layer_finished(qk_tap_dance_state_t *state, void *user_data) {
+    // Determine the current state
+    if (state->count == 1) {
+        if (state->interrupted || state->pressed == 0) tap_dance_state = SINGLE_TAP;
+        else tap_dance_state = SINGLE_HOLD;
+    } else if (state->count == 2) {
+        tap_dance_state = DOUBLE;
+    } else {
+        // We don't handle this.
+        return;
+    }
+
+    switch (tap_dance_state) {
+        case SINGLE_TAP:
+            if (tap_dance_active) {
+                reset_oneshot_layer();
+                layer_off(_LOWER);
+                tap_dance_active = false;
+            } else {
+                set_oneshot_layer(_LOWER, ONESHOT_START);
+                tap_dance_active = true;
+            }
+            break;
+        case SINGLE_HOLD:
+            layer_on(_LOWER);
+            break;
+        case DOUBLE:
+            layer_on(_VIM);
+    }
+}
+
+
+void tap_dance_layer_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch(tap_dance_state) {
+        case SINGLE_TAP:
+            clear_oneshot_layer_state(ONESHOT_PRESSED);
+            break;
+        case SINGLE_HOLD:
+            layer_off(_LOWER);
+            break;
+        case DOUBLE:
+            layer_off(_VIM);
+            break;
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_LWR_VIM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, tap_dance_layer_finished, tap_dance_layer_reset)
+};
+
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -27,19 +90,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |LTVIMF|   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |/ Ctrl| Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |  F4  | GUI  |Lower | Bksp | Spc  | Ent  |  Alt |   +  | Game | Del  |
+ * | Ctrl |  Alt |  F4  | GUI  |Lwr/VM| Bksp | Spc  | Ent  |  Alt |   +  | Game | Del  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
     KC_GRV,        KC_1,         KC_2,  KC_3,    KC_4,           KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,            KC_MINUS, \
     KC_TAB,        KC_Q,         KC_W,  KC_E,    KC_R,           KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,            KC_BSLASH, \
-    KC_ESC,        KC_A,         KC_S,  KC_D,    LT(_VIM, KC_F), KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,         KC_QUOT, \
+    KC_ESC,        KC_A,         KC_S,  KC_D,    KC_F,           KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,         KC_QUOT, \
     OSM(MOD_LSFT), LCTL_T(KC_Z), KC_X,  KC_C,    KC_V,           KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RCTL_T(KC_SLSH), OSM(MOD_RSFT), \
-    KC_LCTL,       KC_LALT,      KC_F4, KC_LGUI, OSL(_LOWER),    KC_BSPC, KC_SPC,  KC_ENT,  KC_LOCK, KC_EQL, TG(_GAME),       KC_DEL \
+    KC_LCTL,       KC_LALT,      KC_F4, KC_LGUI, TD(TD_LWR_VIM), KC_BSPC, KC_SPC,  KC_ENT,  KC_LOCK, KC_EQL, TG(_GAME),       KC_DEL \
 ),
 
 /* Lower
@@ -73,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      | LShft|      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_VIM] =  KEYMAP( \
@@ -81,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______,  _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, M(DLEFT), M(DRIGHT), KC_LCTL, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
     _______, _______,  _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______,  _______,   _______, KC_LSFT, _______, _______, _______, _______, _______, _______, _______ \
+    _______, _______,  _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 /* Gaming mode (Raise)
@@ -132,4 +195,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
             break;
     }
     return MACRO_NONE;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (tap_dance_state == SINGLE_TAP && keycode != TD(TD_LWR_VIM)) {
+        tap_dance_active = false;
+    }
+    return true;
 }
