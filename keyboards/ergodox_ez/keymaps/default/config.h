07f61ece01991a10deaaf7fc97c8ee5133c8ed83
Jack Humbert <jack.humb@gmail.com>
2017-11-06 09:15:30 -0500

    restore default mode/color if no 0 color

--- keyboards/ergodox_ez/keymaps/default/config.h
+++ keyboards/ergodox_ez/keymaps/default/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define RGBLIGHT_COLOR_LAYER_0 0x00, 0x00, 0xFF
+// #define RGBLIGHT_COLOR_LAYER_0 0x00, 0x00, 0xFF
 #define RGBLIGHT_COLOR_LAYER_1 0xFF, 0x00, 0x00
 #define RGBLIGHT_COLOR_LAYER_2 0x00, 0xFF, 0x00
 #define RGBLIGHT_COLOR_LAYER_3 0xFF, 0xFF, 0x00
