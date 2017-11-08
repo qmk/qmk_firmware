e6639ec2d28d43b72e67f1a77ef4d8ed663814b7
Mathias Kende <mathias@kende.fr>
2017-11-08 23:44:58 +0100

    Initial version of the Ergodox EZ Bépo keymap, TypeMatrix style.

---
+++ keyboards/ergodox_ez/keymaps/bepo_tm_style/keymap.c
@@ -0,0 +1,119 @@
+#include QMK_KEYBOARD_H
+#include "debug.h"
+#include "action_layer.h"
+#include "version.h"
+
+
+#include "keymap_bepo.h"
+
+#define VERSION M(0)
+
+
+// Define layer names 
+#define BASE 0
+#define FN 1
+#define MOUSE 2
+#define ARROWS 2
+
+enum custom_keycodes {
+  SWAP_HAND = SAFE_RANGE, // make this a layer so that it can have the TT mode?
+};
+
+#define ___ KC_TRANSPARENT
+#define XXX KC_NOP
+
+#define ESC_FN    LT(FN, KC_ESC)
+#define M_RSFT    MT(MOD_RSFT, BP_M)
+#define CCED_RCTL MT(MOD_RCTL, BP_CCED)
+#define PERC_FN   LT(FN, BP_PERC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE] = KEYMAP(  
+    /* left hand */
+    BP_DLR,   BP_DQOT, BP_LGIL, BP_RGIL, BP_LPRN, BP_RPRN, KC_DEL,
+    KC_TAB,   BP_B,    BP_ECUT, BP_P,    BP_O,    BP_EGRV, KC_BSPC,
+    KC_LSFT,  BP_A,    BP_U,    BP_I,    BP_E,    BP_COMM,
+    KC_LCTRL, BP_AGRV, BP_Y,    BP_X,    BP_DOT,  BP_K,    KC_ENT,
+    ESC_FN,   BP_ECRC, KC_LOCK, KC_LGUI, KC_LALT,
+                                                          KC_LEFT,    KC_DOWN,
+                                                                    SWAP_HAND,
+                                                  KC_SPC,  TT(FN), TT(ARROWS),
+    /* right hand */
+        KC_DEL,  BP_AT,   BP_PLUS, BP_MINS, BP_SLSH, BP_ASTR, BP_EQL,
+        KC_BSPC, BP_DCRC, BP_V,    BP_D,    BP_L,    BP_J,    BP_Z,
+                 BP_C,    BP_T,    BP_S,    BP_R,    BP_N,    M_RSFT,
+        KC_ENT,  BP_APOS, BP_Q,    BP_G,    BP_H,    BP_F,    CCED_RCTL,
+                          KC_MPRV, KC_MPLY, KC_MNXT, BP_W,    PERC_FN,
+    KC_UP,     KC_RIGHT,
+    SWAP_HAND,
+    TT(MOUSE), KC_RALT, KC_SPC),
+
+  [FN] = KEYMAP(  
+    /* left hand */
+    KC_SLEP, KC_F1, KC_F2,  KC_F3,   KC_F4,    KC_F5, KC_INS,
+    ___,     ___,   ___,    ___,     ___,      ___,   KC_SLCK,
+    ___,     ___,   ___,    ___,     ___,      ___,
+    ___,     ___,   KC_CUT, KC_COPY, KC_PASTE, ___,   KC_NLCK,
+    ___,     ___,   ___,    ___,     ___,
+                                                      KC_HOME, KC_PGDN,
+                                                                   ___,
+                                                  ___,    ___,     ___,
+    /* right hand */
+        KC_PSCR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_PAUS, KC_PEQL, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_F12,
+                 KC_PCMM, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, ___,
+        KC_PENT, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PAST, ___,
+                          KC_MUTE, KC_VOLD, KC_VOLU, KC_PSLS, ___,
+    KC_PGUP, KC_END,
+    ___,
+    ___,     ___,    ___),
+
+  [MOUSE] = KEYMAP(  
+    /* left hand */
+    ___,     ___, ___,     KC_BTN3, ___,     ___,     ___,
+    KC_ACL2, ___, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN4, ___,
+    KC_ACL1, ___, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN5,
+    KC_ACL0, ___, ___,     ___,     ___,     ___,     ___,
+    ___,     ___, ___,     ___,     ___,
+                                                      ___, ___,
+                                                           ___,
+                                                 ___, ___, ___,
+    /* right hand */
+         ___, ___, ___,     ___,     ___,     ___, ___,
+         ___, ___, ___,     KC_WH_U, ___,     ___, ___,
+              ___, KC_WH_L, KC_WH_D, KC_WH_R, ___, ___,
+         ___, ___, ___,     ___,     ___,     ___, ___,
+                   ___,     ___,     ___,     ___, ___,
+    ___, ___,
+    ___,
+    ___, ___, ___),
+
+  [ARROWS] = KEYMAP(  
+    /* left hand */
+    ___, ___, ___, ___, ___, ___, ___,
+    ___, ___, ___, ___, ___, ___, ___,
+    ___, ___, ___, ___, ___, ___,
+    ___, ___, ___, ___, ___, ___, ___,
+    ___, ___, ___, ___, ___,
+                                  ___, ___,
+                                       ___,
+                             ___, ___, ___,
+    /* right hand */
+         ___, ___, ___,     ___,     ___,     ___, ___,
+         ___, ___, KC_HOME,     KC_UP, KC_END,     KC_PGUP, ___,
+              ___, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, ___,
+         ___, ___, ___,     ___,     ___,     ___, ___,
+                   ___,     ___,     ___,     ___, ___,
+    ___, ___,
+    ___,
+    ___, ___, ___),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case SWAP_HAND:
+      swap_hands = record->event.pressed;
+      return false;  // Skip all further processing of this key
+  }
+  return true; // Let QMK send the enter press/release events
+}
