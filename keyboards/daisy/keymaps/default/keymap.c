73da2621bb8405aa183b73d85756ea810c5d5d71
Max <17062872+westfoxtrot@users.noreply.github.com>
2017-11-08 14:02:28 +0000

    Add files via upload

--- keyboards/daisy/keymaps/default/keymap.c
+++ keyboards/daisy/keymaps/default/keymap.c
@@ -1,28 +1,16 @@
-/* Copyright 2017
-
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include "daisy.h"
+#include "action_layer.h"
+
+//extern keymap_config_t keymap_config;
 
 // Layer shorthand
-#define _QW 0
-#define _FN1 1
-#define _FN2 2
+#define _BL 0
+#define _LW 1
+#define _RS 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
+/* Base Layer
  * .-----------------------------------------------------------------------.
  * | ESC | Q   | W   | E   | R   | T   | Y   | U   | I   | O   | P   | \|  |
  * |-----------------------------------------------------------------------|
@@ -34,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '-----------------------------------------------------------------------'
  */
 
- [_QW] = { /* QWERTY */
-  {GRAVE_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS },
-  {KC_TAB,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_ENT    },
-  {KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,     KC_SLSH  },
-  {KC_LCTL, KC_LGUI, KC_LALT,        KC_SPC,              KC_BSPC,             MO(_FN1),   MO(_FN2),   KC_RALT },
- },
+	[_BL] = KEYMAP(
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS,
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,   KC_K,    KC_L,    KC_ENT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH,
+  KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_BSPC, MO(_LW), MO(_RS), KC_RALT ),
+ 
 
-/* FN1
+/* Function Layer
  * .-----------------------------------------------------------------------.
  * | GRV | 1   | 2   | 3   | 4   | 5   | 6   | 7   | 8   | 9   | 0   | -_  |
  * |-----------------------------------------------------------------------|
@@ -53,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '-----------------------------------------------------------------------'
  */
  
- [_FN1] = { /* FUNCTION */
-  {KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS },
-  {KC_TRNS,    KC_EQL,  KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT,   KC_TRNS    },
-  {KC_TRNS,     KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS  },
-  {KC_LCTL, KC_LGUI, KC_LALT,        KC_TRNS,             KC_DEL,              KC_TRNS,   KC_TRNS,   KC_TRNS },
- },
+	[_LW] = KEYMAP(
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_TRNS, KC_EQL,  KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, KC_TRNS,
+  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS, KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS ),
  
- /* FN2
+ 
+ /* Second Function Layer
  * .-----------------------------------------------------------------------.
  * | RST | F1  | F2  | F3  | F4  | F5  | F6  |     | HOME| PGUP|     |     |
  * |-----------------------------------------------------------------------|
@@ -72,12 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '-----------------------------------------------------------------------'
  */
  
- [_FN2] = { /* FUNCTION */
-  {RESET,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_HOME, KC_PGUP,KC_TRNS,KC_TRNS },
-  {KC_TRNS,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_END,  KC_PGDN,    KC_TRNS    },
-  {KC_TRNS,     RGB_TOG, RGB_SMOD, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, RGB_SAD, RGB_SAI, BL_STEP,    KC_TRNS  },
-  {KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,            KC_TRNS,              KC_TRNS,  KC_TRNS,    KC_TRNS },
- },
+	[_RS] = KEYMAP(
+  RESET,   KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_HOME, KC_PGUP,  KC_TRNS,  KC_TRNS,
+  KC_TRNS, KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_END,  KC_PGDN,  KC_TRNS,
+  KC_TRNS, RGB_TOG, RGB_SMOD, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, RGB_SAD, RGB_SAI, BL_STEP,  KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
  
 };
 
