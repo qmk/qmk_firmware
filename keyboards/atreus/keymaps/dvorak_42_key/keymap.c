dd60038eeb1c6eca994d587a6b16c23c93ccafe3
lucwastiaux <luc.wastiaux@xsmail.com>
2017-11-14 07:50:39 +0800

    Changes to Atreus and Ergodox EZ Dvorak 42key layout (#1997)
    * importing 42 key dvorak layout
    
    * added comment for build instructions
    
    * adding atreus dvorak 42 key layout
    
    * added readme
    
    * add readme
    
    * build instructions
    
    * additional MEH shortcuts
    
    * added shifted symbols on symbols layer
    
    * working extra symbols on COMBINED layer
    
    * bring atreus layout inline with the ergodox one
    
    * add necessary macros
    
    * working ls macro
    
    * added more shell macros
    
    * added screen rename / screen number macros
    
    * add ctrl-a key in shell-nav to use screen more easily
    
    * added shell screen layer
    
    * assign screen switching macros to screen layer
    
    * define all screen switching macros
    
    * more screen-related shortcuts added on shell screen layer
    
    * change shell nav bottom right row to match base layer (backspace / delete)
    
    * remove some mappings on SHELL_NAV layer as they are now in the screen layer
    
    * added more screen macros
    
    * changes to COMBINED layer (pipe on the right) and modified shell nav
    
    * moved pipe/backslash to then right
    
    * documented SHELL_SCREEN layer
    
    * put backspace/delete on SHELL_NAV layer

--- keyboards/atreus/keymaps/dvorak_42_key/keymap.c
+++ keyboards/atreus/keymaps/dvorak_42_key/keymap.c
@@ -44,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 },	
 
 [COMBINED] = { 
-  {KC_EXLM, KC_AT,      KC_HASH,     KC_DLR,      KC_PERC, KC_TRNS,    KC_PLUS,   KC_7, KC_8,    KC_9,    KC_ASTR, },
-  {KC_LPRN, KC_RPRN,    KC_LBRACKET, KC_RBRACKET, KC_UNDS,  KC_TRNS,    KC_MINS,  KC_4, KC_5,    KC_6,    KC_SLSH, },
-  {KC_COLN, KC_DQUO,    KC_LCBR,     KC_RCBR,     KC_TRNS,  KC_TRNS,    KC_EQUAL, KC_1, KC_2,    KC_3,    KC_QUES, },
-  {KC_PIPE, KC_BSLS,    KC_CIRC,     KC_AMPR,     KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_0, KC_DOT,  KC_TILD, KC_GRAVE, }
+  {KC_EXLM, KC_AT,      KC_HASH,     KC_DLR,      KC_PERC, KC_TRNS,  KC_PLUS,  KC_7, KC_8,    KC_9,    KC_ASTR, },
+  {KC_LPRN, KC_RPRN,    KC_LBRACKET, KC_RBRACKET, KC_UNDS, KC_TRNS,  KC_MINS,  KC_4, KC_5,    KC_6,    KC_SLSH, },
+  {KC_COLN, KC_DQUO,    KC_LCBR,     KC_RCBR,     KC_AMPR, KC_TRNS,  KC_EQUAL, KC_1, KC_2,    KC_3,    KC_QUES, },
+  {KC_TRNS, KC_TILD,    KC_GRAVE,    KC_CIRC,     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_0, KC_DOT,  KC_PIPE, KC_BSLS, }
 },	
 
 [MOUSE] = { 
