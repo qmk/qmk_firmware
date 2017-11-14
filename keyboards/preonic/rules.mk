ec3e065f0d2c65175384699cb11fa388250fa914
Jack Humbert <jack.humb@gmail.com>
2017-11-14 16:11:29 -0500

    QMK DFU bootloader generation (#2009)
    * adds :bootloader target
    
    * update planck and preonic revisions
    
    * remove references to .h files for planck
    
    * update preonic keymap
    
    * only add keyboard.h files that exist
    
    * add production target
    
    * hook things up with the new lufa variables
    
    * update rules for planck/preonic
    
    * back backlight key turn of status led when pressed
    
    * add manufacturer/product strings to bootloader

--- keyboards/preonic/rules.mk
+++ keyboards/preonic/rules.mk
@@ -55,7 +55,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+CONSOLE_ENABLE = yes         # Console for debug(+400)
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
@@ -70,3 +70,5 @@ API_SYSEX_ENABLE = no
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 LAYOUTS = ortho_5x12
+
+DEFAULT_FOLDER = preonic/rev2
