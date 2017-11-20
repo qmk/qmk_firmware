5f86b65d1f121acd8d9499958ad53653606db636
Mathias Kende <mathias@kende.fr>
2017-11-20 01:06:55 +0100

    Slightly slow down the mouse and mouse wheel.

--- keyboards/ergodox_ez/keymaps/bepo_tm_style/config.h
+++ keyboards/ergodox_ez/keymaps/bepo_tm_style/config.h
@@ -1,11 +1,24 @@
 #include "../../config.h"
 
+// Sets good default for the speed of the mouse.
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_DELAY
+#undef MOUSEKEY_TIME_TO_MAX
+#undef MOUSEKEY_MAX_SPEED
+
 #define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_DELAY          100
 #define MOUSEKEY_TIME_TO_MAX    60
 #define MOUSEKEY_MAX_SPEED      7
-#define MOUSEKEY_WHEEL_DELAY 0
-// MOUSEKEY_WHEEL_MAX_SPEED;
-// MOUSEKEY_WHEEL_TIME_TO_MAX;
 
-// #define TAPPING_TOGGLE  2
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#undef MOUSEKEY_WHEEL_DELAY
+
+#define MOUSEKEY_WHEEL_MAX_SPEED    5
+#define MOUSEKEY_WHEEL_TIME_TO_MAX  60
+#define MOUSEKEY_WHEEL_DELAY        100
+
+#undef TAPPING_TOGGLE
+
+#define TAPPING_TOGGLE  1
