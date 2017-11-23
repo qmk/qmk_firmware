9df414bdc608c5f9b2a6069e14aad0c1689a0076
Callum Oakley <c.oakley108@gmail.com>
2017-11-23 17:46:13 +0000

    qwerty layer for SO, general cleanup

--- keyboards/planck/keymaps/callum/keymap.c
+++ keyboards/planck/keymaps/callum/keymap.c
@@ -1,17 +1,8 @@
 #include "planck.h"
 #include "action_layer.h"
-#ifdef AUDIO_ENABLE
-  #include "audio.h"
-#endif
-#include "eeconfig.h"
 
 extern keymap_config_t keymap_config;
 
-#define _BASE 0
-#define _MOVE 1
-#define _SYMB 2
-#define _MORE 3
-#define _FUNC 4
 #define G(X) LGUI(X)
 #define A(X) LALT(X)
 #define C(X) LCTL(X)
@@ -20,15 +11,25 @@ extern keymap_config_t keymap_config;
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+enum planck_layers {
+    _COLEMAK,
+    _QWERTY,
+    _SYMB,
+    _MOVE,
+    _FUNC
+};
+
 enum planck_keycodes {
-  MOVE = SAFE_RANGE,
+  COLEMAK = SAFE_RANGE,
+  QWERTY,
   SYMB,
+  MOVE,
   FUNC
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* BASE
+/* COLEMAK
  * ,-----------------------------------------------------------------------.
  * |Tab  |  Q  |  W  |  F  |  P  |  G  |  J  |  L  |  U  |  Y  |  ;  |  -  |
  * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Func |Super| Alt |Ctrl |Symb |Enter|Space|Move |Ctrl | Alt |Super|Func |
  * `-----------------------------------------------------------------------'
  */
-[_BASE] = {
+[_COLEMAK] = {
   {
     KC_TAB,       KC_Q,         KC_W,         KC_F,
     KC_P,         KC_G,         KC_J,         KC_L,
@@ -62,29 +63,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   }
 },
 
-[_MOVE] = {
+/* QWERTY
+ * ,-----------------------------------------------------------------------.
+ * |Tab  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  -  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Bksp |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Shift|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Shift|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Func |Super| Alt |Ctrl |Symb |Enter|Space|Move |Ctrl | Alt |Super|Func |
+ * `-----------------------------------------------------------------------'
+ */
+[_QWERTY] = {
   {
-    GC(KC_UP),    GAC(KC_1),    G(KC_6),      G(KC_5),
-    G(KC_4),      GAC(KC_UP),   GAC(KC_RGHT), KC_HOME,
-    KC_UP,        KC_END,       C(KC_SPC),    KC_ESC
+    KC_TAB,       KC_Q,         KC_W,         KC_E,
+    KC_R,         KC_T,         KC_Y,         KC_U,
+    KC_I,         KC_O,         KC_P,         KC_MINS
   },
   {
-    GC(KC_DOWN),  GAC(KC_2),    G(KC_3),      G(KC_2),
-    G(KC_1),      G(KC_F),      G(KC_C),      KC_LEFT,
-    KC_DOWN,      KC_RGHT,      KC_CAPS,      KC_DEL
+    KC_BSPC,      KC_A,         KC_S,         KC_D,
+    KC_F,         KC_G,         KC_H,         KC_J,
+    KC_K,         KC_L,         KC_SCLN,      KC_QUOT
   },
   {
-    _______,      GAC(KC_3),    G(KC_9),      G(KC_8),
-    G(KC_7),      GAC(KC_LEFT), GAC(KC_DOWN), KC_PGDN,
-    KC_PGUP,      GC(KC_LEFT),  GC(KC_RGHT),  _______
+    KC_LSFT,      KC_Z,         KC_X,         KC_C,
+    KC_V,         KC_B,         KC_N,         KC_M,
+    KC_COMM,      KC_DOT,       KC_SLSH,      KC_RSFT
   },
   {
-    _______,      _______,      _______,      _______,
-    _______,      GC(KC_ENT),   _______,      _______,
-    _______,      _______,      _______,      _______
+    FUNC,         KC_LGUI,      KC_LALT,      KC_LCTL,
+    SYMB,         KC_ENT,       KC_SPC,       MOVE,
+    KC_RCTL,      KC_RALT,      KC_RGUI,      FUNC
   }
 },
 
+/* SYMB
+ * ,-----------------------------------------------------------------------.
+ * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * | Del |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |  ~  |  `  |  +  |  =  |  |  |  \  |  [  |  ]  |  {  |  }  |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |     |     |     |     |
+ * `-----------------------------------------------------------------------'
+ */
 [_SYMB] = {
   {
     KC_ESC,       KC_1,         KC_2,         KC_3,
@@ -108,21 +131,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   }
 },
 
-[_MORE] = {
+/* MOVE
+ * ,-----------------------------------------------------------------------.
+ * |     |     |     |     |     |     |     |Home | Up  | End |     | Esc |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |Left |Down |Right|Caps | Del |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |PgDn |PgUp |     |     |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |     |     |     |     |
+ * `-----------------------------------------------------------------------'
+ */
+[_MOVE] = {
   {
-    XXXXXXX,      A(KC_1),      A(KC_2),      A(KC_3),
-    A(KC_4),      A(KC_5),      A(KC_6),      A(KC_7),
-    A(KC_8),      A(KC_9),      A(KC_0),      XXXXXXX
+    GC(KC_UP),    GAC(KC_1),    G(KC_6),      G(KC_5),
+    G(KC_4),      GAC(KC_UP),   GAC(KC_RGHT), KC_HOME,
+    KC_UP,        KC_END,       C(KC_SPC),    KC_ESC
   },
   {
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+    GC(KC_DOWN),  GAC(KC_2),    G(KC_3),      G(KC_2),
+    G(KC_1),      G(KC_F),      G(KC_C),      KC_LEFT,
+    KC_DOWN,      KC_RGHT,      KC_CAPS,      KC_DEL
   },
   {
-    _______,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      _______
+    _______,      GAC(KC_3),    G(KC_9),      G(KC_8),
+    G(KC_7),      GAC(KC_LEFT), GAC(KC_DOWN), KC_PGDN,
+    KC_PGUP,      GC(KC_LEFT),  GC(KC_RGHT),  _______
   },
   {
     _______,      _______,      _______,      _______,
@@ -131,6 +165,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   }
 },
 
+/* FUNC
+ * ,-----------------------------------------------------------------------.
+ * |Reset| F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 |VolUp|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Power| F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |VolDn|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     | F21 | F22 | F23 | F24 |     |     |     |     |Clmak|Qwrty|     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |Prev |Mute |Play |Next |     |     |     |     |
+ * `-----------------------------------------------------------------------'
+ */
 [_FUNC] = {
   {
     RESET,        KC_F1,        KC_F2,        KC_F3,
@@ -145,7 +190,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {
     _______,      KC_F21,       KC_F22,       KC_F23,
     KC_F24,       XXXXXXX,      XXXXXXX,      XXXXXXX,
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      _______
+    XXXXXXX,      COLEMAK,      QWERTY,       _______
   },
   {
     _______,      _______,      _______,      _______,
@@ -156,25 +201,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+#ifdef AUDIO_ENABLE
+  float colemak_song[][2] = SONG(COLEMAK_SOUND);
+  float qwerty_song[][2] = SONG(QWERTY_SOUND);
+#endif
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case MOVE:
+    case COLEMAK:
       if (record->event.pressed) {
-        layer_on(_MOVE);
-        update_tri_layer(_MOVE, _SYMB, _MORE);
-      } else {
-        layer_off(_MOVE);
-        update_tri_layer(_MOVE, _SYMB, _MORE);
+        #ifdef AUDIO_ENABLE
+          stop_all_notes();
+          PLAY_SONG(colemak_song);
+        #endif
+        set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+      break;
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          stop_all_notes();
+          PLAY_SONG(qwerty_song);
+        #endif
+        set_single_persistent_default_layer(_QWERTY);
       }
       return false;
       break;
     case SYMB:
       if (record->event.pressed) {
         layer_on(_SYMB);
-        update_tri_layer(_MOVE, _SYMB, _MORE);
       } else {
         layer_off(_SYMB);
-        update_tri_layer(_MOVE, _SYMB, _MORE);
+      }
+      return false;
+      break;
+    case MOVE:
+      if (record->event.pressed) {
+        layer_on(_MOVE);
+      } else {
+        layer_off(_MOVE);
       }
       return false;
       break;
--- keyboards/planck/keymaps/callum/readme.md
+++ keyboards/planck/keymaps/callum/readme.md
@@ -4,8 +4,8 @@ This is a layout for the grid planck, built with a few ideals in mind:
 
 - Consistent and minimal response times should be maintained. Keys that react
   differently depending on whether they are tapped or held, keys that react
-  differently if they are double tapped, etc. should be avoided – they
-  inevitably send their keycode later than a normal key – interrupting the
+  differently if they are double tapped, etc. should be avoided -- they
+  inevitably send their keycode later than a normal key -- interrupting the
   immediate feedback from the screen. Therefore we restrict ourselves to
   chording as our only means of getting more than one symbol out of a single
   physical key.
@@ -17,8 +17,68 @@ This is a layout for the grid planck, built with a few ideals in mind:
 - There should be two of every modifier (one on each side), otherwise certain
   long key combinations become hard to make.
 
-We have five layers. A `BASE` layer, in colemak; a `MOVE` layer, with an arrow
-cluster, other movement keys, and hotkeys; a `SYMB` layer, with numbers and
-symbols; a `FUNC` layer, with function keys and media keys; and a `MORE` layer,
-with stuff that doesn’t fit anywhere else. The `MORE` layer is activated by
-holding the Move and Symb keys simultaniously.
+Keys are arranged as follows. Blank keys below may actually contain
+undocumented shortcuts specific to my workflow. The base layer can be set to
+colemak or qwerty (from the function layer).
+
+```
+/* COLEMAK
+ * ,-----------------------------------------------------------------------.
+ * |Tab  |  Q  |  W  |  F  |  P  |  G  |  J  |  L  |  U  |  Y  |  ;  |  -  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Bksp |  A  |  R  |  S  |  T  |  D  |  H  |  N  |  E  |  I  |  O  |  '  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Shift|  Z  |  X  |  C  |  V  |  B  |  K  |  M  |  ,  |  .  |  /  |Shift|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |FUNC |Super| Alt |Ctrl |SYMB |Enter|Space|MOVE |Ctrl | Alt |Super|FUNC |
+ * `-----------------------------------------------------------------------'
+ */
+
+/* QWERTY
+ * ,-----------------------------------------------------------------------.
+ * |Tab  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  -  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Bksp |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Shift|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Shift|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |FUNC |Super| Alt |Ctrl |SYMB |Enter|Space|MOVE |Ctrl | Alt |Super|FUNC |
+ * `-----------------------------------------------------------------------'
+ */
+
+/* SYMB
+ * ,-----------------------------------------------------------------------.
+ * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * | Del |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |  ~  |  `  |  +  |  =  |  |  |  \  |  [  |  ]  |  {  |  }  |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |     |     |     |     |
+ * `-----------------------------------------------------------------------'
+ */
+
+/* MOVE
+ * ,-----------------------------------------------------------------------.
+ * |     |     |     |     |     |     |     |Home | Up  | End |     | Esc |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |Left |Down |Right|Caps | Del |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |PgDn |PgUp |     |     |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |     |     |     |     |
+ * `-----------------------------------------------------------------------'
+ */
+
+/* FUNC
+ * ,-----------------------------------------------------------------------.
+ * |Reset| F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 |VolUp|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Power| F11 | F12 | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 |VolDn|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     | F21 | F22 | F23 | F24 |     |     |     |     |COLMK|QWRTY|     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |Prev |Mute |Play |Next |     |     |     |     |
+ * `-----------------------------------------------------------------------'
+ */
+ ```
