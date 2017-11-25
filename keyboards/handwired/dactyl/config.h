12f0aac6187002b01661a408899ee49e31e7ff3c
Erin Call <hello@erincall.com>
2017-11-25 13:24:52 -0800

    [whitespace] realign some constants

--- keyboards/handwired/dactyl/config.h
+++ keyboards/handwired/dactyl/config.h
@@ -37,11 +37,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 6
 
-#define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    60
-#define MOUSEKEY_MAX_SPEED      7
-#define MOUSEKEY_WHEEL_DELAY 0
+#define MOUSEKEY_INTERVAL     20
+#define MOUSEKEY_DELAY        0
+#define MOUSEKEY_TIME_TO_MAX  60
+#define MOUSEKEY_MAX_SPEED    7
+#define MOUSEKEY_WHEEL_DELAY  0
 
 #define TAPPING_TOGGLE  1
 
