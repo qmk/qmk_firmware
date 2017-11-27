2d26eee303e7bcca32d5fe3c6d784a51c3189a44
rominronin <me@baristosun.com>
2017-11-27 21:32:38 +0100

    katana60 defaults, initial pull-request

--- keyboards/katana60/config.h
+++ keyboards/katana60/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    RominRonin CandyKeys
 #define PRODUCT         Katana60
-#define DESCRIPTION     Ergonomic 60% Keyboard
+#define DESCRIPTION     QMK keyboard firmware for Katana60
 
 /* key matrix size */
 #define MATRIX_ROWS 5
--- keyboards/katana60/keymaps/default/readme.md
+++ keyboards/katana60/keymaps/default/readme.md
@@ -1,1 +1,3 @@
 # The default keymap for katana60
+
+With Windows OS and QWERTY layout in mind.
