b7b74aa3b7b298cf8190c9388d810b1e3cb0505a
Jason Yamada-Hanff <jyamada-hanff@chromologic.com>
2017-11-27 12:47:43 -0800

    Keymap: add jyh levinson keymaps
    remove tap dance number keys due to sluggish responses
    
    simplify I2C configuration
    
    reverse hyper and meh positions
    
    reduce tapping term to allow fast typing with mod tap space keys
    
    remove Alt hold on right space key. too many false touches.
    
    add vim-style arrows (hjkl) on asdf; semicolon/quote on raise
    
    correct documentation string for jyh levinson layout
    
    add alternative levinson layout for jyh
    
    more inspiration from atreus keyboard. keeps shift in the expected
    places. reduces number of layers. puts numpad layout easily
    accessible. places important keys on palms and meta keys on top
    corners for xmonad.
    
    correct layer switching in jyh2 levinson layout
    
    I was getting stuck on the numpad layer because the numpad hold key
    was remapped once the layer was activated. Bad idea. Minor other
    refinements.
    
    use curly bracket { instead of brace [ on raise layer
    
    add RGB keys on adjust layer
    
    bootmagic key is confusing. turn it off.
    
    try out permissive hold setting to make mod-tap better
    
    remove hold for right control on quote key. too many mistakes
    
    turn off permissive hold. it was causing unexpected behavior
    
    enable rgb underglow animations
    
    add make it pink/blue key to levinson layout
    
    prevent stuck modifiers in jyh2 levinson keymap

---
+++ keyboards/levinson/keymaps/jyh/rules.mk
@@ -0,0 +1,6 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+BACKLIGHT_ENABLE=yes
+USE_I2C=yes
---
+++ keyboards/levinson/keymaps/jyh2/rules.mk
@@ -0,0 +1,6 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+RGBLIGHT_ENABLE=yes
+BOOTMAGIC_ENABLE=no
