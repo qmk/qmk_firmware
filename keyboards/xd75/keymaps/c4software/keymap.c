840c8411d27a83068e9be0aea847cf5f062d5aaa
Valentin Brosseau <c4software@gmail.com>
2017-11-29 22:11:53 +0100

    Move ENTER / BACKSP / DEL

--- keyboards/xd75/keymaps/c4software/keymap.c
+++ keyboards/xd75/keymaps/c4software/keymap.c
@@ -23,23 +23,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | E      | R      | T      | [      | \      | ]      | Y      | U      | I      | O      | P      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | RALT   | A      | S      | D      | F      | G      | ENTER  | DEL    | ENTER  | H      | J      | K      | L      | ;      | PG UP  |
+ * | RALT   | A      | S      | D      | F      | G      | ENTER  | DEL    | ENTER  | H      | J      | K      | L      | ;      | HOME   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | HOME   | UP     | END    | N      | M      | ,      | .      | /      | PG DN  |
+ * | LSHIFT | Z      | X      | C      | V      | B      | BACKSP | UP     | BACKSP | N      | M      | ,      | .      | /      | END    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
  * | LCTRL  | LALT   | LGUI   | FN     | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | FN     | RGUI   | RALT   | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_MINS, KC_GRV,  KC_EQL,  KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
+  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_MINS, KC_GRV,  KC_EQL,  KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_NO   },
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_QUOT },
-  { KC_RALT, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_ENT,  KC_DEL,  KC_ENT,  KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_PGUP  },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_HOME, KC_UP,   KC_END,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_PGDN },
+  { KC_RALT, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_ENT,  KC_DEL,  KC_ENT,  KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_HOME },
+  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_BSPC, KC_UP,   KC_BSPC, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_END  },
   { KC_LCTL, KC_LALT, KC_LGUI, MO(_FN), KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC, KC_SPC, MO(_FN), KC_RGUI, KC_RALT, KC_RCTL },
  },
 
