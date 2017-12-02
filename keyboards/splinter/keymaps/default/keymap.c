046e8a65ae3d19bded66b5b49a7902be716eecd0
Gilbert Consellado <gilbertconsellado@gmail.com>
2017-12-02 16:30:30 +0800

    initial caps rainbow

--- keyboards/splinter/keymaps/default/keymap.c
+++ keyboards/splinter/keymaps/default/keymap.c
@@ -22,6 +22,53 @@ enum custom_keycodes {
 
 #define TOTAL_MATRIX_POINTS (MATRIX_ROWS * MATRIX_COLS)
 
+const uint8_t lights[360]={
+  0,   0,   0,   0,   0,   1,   1,   2,
+  2,   3,   4,   5,   6,   7,   8,   9,
+ 11,  12,  13,  15,  17,  18,  20,  22,
+ 24,  26,  28,  30,  32,  35,  37,  39,
+ 42,  44,  47,  49,  52,  55,  58,  60,
+ 63,  66,  69,  72,  75,  78,  81,  85,
+ 88,  91,  94,  97, 101, 104, 107, 111,
+114, 117, 121, 124, 127, 131, 134, 137,
+141, 144, 147, 150, 154, 157, 160, 163,
+167, 170, 173, 176, 179, 182, 185, 188,
+191, 194, 197, 200, 202, 205, 208, 210,
+213, 215, 217, 220, 222, 224, 226, 229,
+231, 232, 234, 236, 238, 239, 241, 242,
+244, 245, 246, 248, 249, 250, 251, 251,
+252, 253, 253, 254, 254, 255, 255, 255,
+255, 255, 255, 255, 254, 254, 253, 253,
+252, 251, 251, 250, 249, 248, 246, 245,
+244, 242, 241, 239, 238, 236, 234, 232,
+231, 229, 226, 224, 222, 220, 217, 215,
+213, 210, 208, 205, 202, 200, 197, 194,
+191, 188, 185, 182, 179, 176, 173, 170,
+167, 163, 160, 157, 154, 150, 147, 144,
+141, 137, 134, 131, 127, 124, 121, 117,
+114, 111, 107, 104, 101,  97,  94,  91,
+ 88,  85,  81,  78,  75,  72,  69,  66,
+ 63,  60,  58,  55,  52,  49,  47,  44,
+ 42,  39,  37,  35,  32,  30,  28,  26,
+ 24,  22,  20,  18,  17,  15,  13,  12,
+ 11,   9,   8,   7,   6,   5,   4,   3,
+  2,   2,   1,   1,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0,
+  0,   0,   0,   0,   0,   0,   0,   0};
+
 const uint8_t base = 5;
 const uint32_t _PC = 0xFF0000; // LED Red, pressed LED.
 const uint32_t _LC[5] = { \
@@ -36,7 +83,7 @@ const uint32_t _LC[5] = { \
 };
 
 uint8_t cur_lyr = 0; // current selected layer.
-uint8_t dim = 7; // rgb dimming level.
+uint8_t dim = 0; // rgb dimming level.
 
 #define _baseLayer KEYMAP( \
   KC_QUOTE,    KC_COMMA,    KC_DOT,          KC_P,            KC_Y,              KC_F,           KC_G,            KC_C,            KC_R,           KC_L,        \
@@ -154,11 +201,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+void sine_led(uint8_t angle) {
+  setrgb(lights[(angle + 120)%360] >> dim, lights[angle] >> dim, lights[(angle + 240)%360] >> dim, (LED_TYPE *)&led[0]);
+  rgblight_set();
+}
+
+void test_loop(void){
+  static uint16_t last_timer = 0;
+  static int k = 0;
+
+  if(k>=260) {
+    k = 0;
+  }
+
+  if(timer_elapsed(last_timer)<8) {
+    return;
+  }
+  last_timer = timer_read();
+  xprintf("k: %d\n", k);
+  sine_led(k);
+  k++;
+}
+
 void matrix_scan_user(void) {
   cur_lyr = biton32(layer_state);
   shifted_layer();
   layer_led();
+  test_loop();
 }
+
 
 void matrix_init_user(void) {
   init_tap_dance();
