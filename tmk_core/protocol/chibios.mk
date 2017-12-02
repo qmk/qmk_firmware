0fefb3c40361bc13c4a54602f730647a03810e01
Fred Sundvik <fsundvik@gmail.com>
2017-12-02 21:05:17 +0200

    Try to compile usb_descriptor on ChibiOS

--- tmk_core/protocol/chibios.mk
+++ tmk_core/protocol/chibios.mk
@@ -4,6 +4,7 @@ CHIBIOS_DIR = $(PROTOCOL_DIR)/chibios
 
 SRC += $(CHIBIOS_DIR)/usb_main.c
 SRC += $(CHIBIOS_DIR)/main.c
+SRC += usb_descriptor.c
 
 VPATH += $(TMK_PATH)/$(PROTOCOL_DIR)
 VPATH += $(TMK_PATH)/$(CHIBIOS_DIR)
