b42950febc6bfa8dfd9344e557605f09d074950f
Valentin Brosseau <c4software@gmail.com>
2017-12-03 12:08:57 +0100

    Commit my bepo layout instead of the qwerty version since i will not use it

---
+++ keyboards/xd75/keymaps/c4software_bepo/README.md
@@ -0,0 +1,9 @@
+# Ressources for the XD75Re
+
+Set of ressources for build and customizing the XD75Re keyboard
+
+- [BuildLog](https://rawgit.com/c4software/xd75re/master/buildlog/index.html)
+- [Default Layout](./layout/default)
+- [C4software Layout](./layout/c4software_qwerty)
+- [C4software Bépo Layout](./layout/c4software_bepo)
+- [Original Flashtools (Windows)](./flash_xd75_officiel_tools_windows/)
--- keyboards/xd75/keymaps/c4software_bepo/keymap.c
+++ keyboards/xd75/keymaps/c4software_bepo/keymap.c
@@ -14,33 +14,34 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "xd75.h"
+#include "keymap_bepo.h"
 
 // Layer shorthand
-#define _QW 0
+#define _BP 0
 #define _FN 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
+/* Native BepoDev for compatible computer
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      |        |
+ * | ESC    | "      | <      | >      | (      | )      | %      |        |        | @      | +      | -      | /      | *      | =      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | [      | \      | ]      | Y      | U      | I      | O      | P      | '      |
+ * | TAB    | B      | E_ACUT | P      | O      | $      | E_CIRC | DEL    | ^      | V      | D      | L      | J      | Z      | C_CEDIL|
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | RALT   | A      | S      | D      | F      | G      | ENTER  | DEL    | ENTER  | H      | J      | K      | L      | ;      | HOME   |
+ * | RALT   | A      | U      | I      | E      | ,      | ENTER  | ENTER  | C      | T      | S      | R      | N      | M      | W      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | BACKSP | UP     | BACKSP | N      | M      | ,      | .      | /      | END    |
+ * | LSHIFT | A_GRAV | Y      | X      | .      | K      | BACKSP | BACKSP | ’      | Q      | G      | H      | F      | UP     | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LALT   | LGUI   | FN     | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | FN     | RGUI   | RALT   | RCTRL  |
+ * | LCTRL  | LALT   | LGUI   | FN     | SPACE  | SPACE  | RALT   | RALT   | SPACE  | SPACE  | FN     | RGUI   | LEFT   | DOWN   | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
- [_QW] = { /* QWERTY */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_MINS, KC_GRV,  KC_EQL,  KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_NO   },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_QUOT },
-  { KC_RALT, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_ENT,  KC_DEL,  KC_ENT,  KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_HOME },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_BSPC, KC_UP,   KC_BSPC, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_END  },
-  { KC_LCTL, KC_LALT, KC_LGUI, MO(_FN), KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC, KC_SPC, MO(_FN), KC_RGUI, KC_RALT, KC_RCTL },
+ [_BP] = { /* BepoDev */
+  { KC_ESC,  BP_DQOT,    BP_LGIL,    BP_RGIL, BP_LPRN, BP_RPRN,    TODO,    TODO,    TODO,    BP_AT,   BP_PLUS, BP_MINUS, BP_SLASH, BP_ASTR, BP_EQUAL },
+  { KC_TAB,  BP_B,       BP_E_ACUTE, BP_P,    BP_O,    BP_DOLLAR,  BP_ECRC, KC_DEL,  BP_DCRC, BP_V,    BP_D,    BP_L,     BP_J,     BP_Z,    BP_CCED  },
+  { KC_RALT, BP_A,       BP_U,       BP_I,    BP_E,    BP_COMMA,   KC_ENT,  KC_ENT,  BP_C,    BP_T,    BP_S,    BP_R,     BP_N,     BP_M,    BP_W     },
+  { KC_LSFT, BP_A_GRAVE, BP_Y,       BP_X,    BP_DOT,  BP_K,       KC_BSPC, KC_BSPC, BP_APOS, BP_Q,    BP_G,    BP_H,     BP_F,     KC_UP,   KC_RSFT  },
+  { KC_LCTL, KC_LALT,    KC_LGUI,    MO(_FN), KC_SPC,  KC_SPC,     KC_RALT, KC_RALT, KC_SPC,  KC_SPC,  MO(_FN), KC_RGUI,  KC_LEFT,  KC_DOWN, KC_RGHT  },
  },
 
 /* FUNCTION
@@ -51,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        | PREV   | PLAY   | NEXT   | STOP   |        | P4     | P5     | P6     | +      | RGB_SAI| RGB_SAD| RGB_VAI| RGB_VAD|        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | VOL-   | MUTE   | VOL+   |        |        | P1     | P2     | P3     | PENT   |        |        |        |        |        |
+ * |        | VOL-   | MUTE   | VOL+   |        |        | P1     | P2     | P3     | PENT   |        |        |        | Home   |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | RESET  |        |        | FN     |        |        | P0     | P0     | P.     | PENT   |        | FN     |        |        |        |
+ * | RESET  |        |        | FN     |        |        | P0     | P00    | P.     | PENT   |        |        | FN     | End    |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
  
@@ -61,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
   { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_MINS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, KC_TRNS },
   { KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS },
-  { KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
-  { RESET,   KC_TRNS, KC_TRNS, MO(_FN), KC_TRNS, KC_TRNS, KC_KP_0, KC_KP_0, KC_PDOT, KC_PENT, KC_TRNS, MO(_FN), KC_TRNS, KC_TRNS, KC_TRNS },
+  { KC_TRNS, KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_TRNS },
+  { RESET,   KC_TRNS, KC_TRNS, MO(_FN), KC_TRNS, KC_TRNS, KC_KP_0, KP_00, KC_PDOT, KC_PENT, KC_TRNS,   KC_TRNS, MO(_FN), KC_END,  KC_TRNS },
  }
 };
 
