9c2864f236df4b99e370372ad735d99d39316702
Dan McGregor <dan.mcgregor@usask.ca>
2017-12-05 07:36:04 -0600

    grep -> $(GREP)
    Some UNIXy systems (FreeBSD for example) don't use GNU grep by default.
    Allow the user to specify which grep implementation to use so that
    GNU grep can be specified.

--- keyboards/handwired/woodpad/rules.mk
+++ keyboards/handwired/woodpad/rules.mk
@@ -71,7 +71,7 @@ RGBLIGHT_ENABLE ?= no
 #    while [ -z $$USB ]; do \
 #      sleep 1; \
 #      ls /dev/tty* > /tmp/2; \
-#      USB=`diff /tmp/1 /tmp/2 | grep -o '/dev/tty.*'`; \
+#      USB=`diff /tmp/1 /tmp/2 | $(GREP) -o '/dev/tty.*'`; \
 #    done; \
 #    avrdude -p $(MCU) -c avr109 -P $$USB -U flash:w:$(BUILD_DIR)/$(TARGET).hex
 #
