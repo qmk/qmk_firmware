ddf9e14be7ac5bbeed3a9ad29b35c948495ed7db
Mathias Kende <mathias@kende.fr>
2017-12-05 19:10:17 +0100

    shorten a comment.

--- keyboards/ergodox_ez/keymaps/bepo_tm_style/rules.mk
+++ keyboards/ergodox_ez/keymaps/bepo_tm_style/rules.mk
@@ -1,7 +1,7 @@
 MOUSEKEY_ENABLE  = yes # Mouse keys
 EXTRAKEY_ENABLE  = yes # Audio control and System control
 COMMAND_ENABLE   = yes # Commands for debug and configuration
-NKRO_ENABLE      = yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE      = yes # USB Nkey Rollover - for issues, see github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 ONEHAND_ENABLE   = yes # Allow swapping hands of keyboard
 KEY_LOCK_ENABLE  = yes # Enable the KC_LOCK key
 TAP_DANCE_ENABLE = yes # Enable the tap dance feature.
