19edbe83253c1c8e0f4b23785af9dad3e2402745
mechmerlin <mechmerlin@gmail.com>
2017-12-11 20:57:22 -0800

    Port xauser keymap from: https://github.com/xauser/tmk_keyboard/blob/xauser/keyboard/octagon_v2/keymap_common.h

--- keyboards/octagon_v2/octagon_v2.h
+++ keyboards/octagon_v2/octagon_v2.h
@@ -20,21 +20,21 @@
 
 #define NO KC_NO
 
-#define KEYMAP( \
-    K5A, K5B, K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O,   K5P, K5Q, K5R, K5S, \
-    K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M,      K4O,   K4P, K4Q, K4R, K4S, \
-    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,      K3O,   K3P, K3Q, K3R, K3S, \
-    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L,           K2O,   K2P, K2Q, K2R, K2S, \
-    K1A,      K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K,      K1M, K1N, K1O,   K1P, K1Q, K1R, K1S, \
-    K0A, K0B, K0C,                          K0I,      K0K,      K0M, K0N, K0O,   K0P, K0Q, K0R \
-) { \
-/*           0    1    2    3    4    5    6    7    8    9    10   11   12   13   14     15   16   17   18  */ \
-/* 0 */   { K5A, K5B, K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O,   K5P, K5Q, K5R, K5S, }, \
-/* 1 */   { K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, NO,  K4O,   K4P, K4Q, K4R, K4S, }, \
-/* 2 */   { K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, NO,  K3O,   K3P, K3Q, K3R, K3S, }, \
-/* 3 */   { K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, NO,  NO,  K2O,   K2P, K2Q, K2R, K2S, }, \
-/* 4 */   { K1A, NO,  K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, NO,  K1M, K1N, K1O,   K1P, K1Q, K1R, K1S, }, \
-/* 5 */   { K0A, K0B, K0C, NO,  NO,  NO,  NO,  NO,  K0I, NO,  K0K, NO,  K0M, K0N, K0O,   K0P, K0Q, K0R } \
-}
+//Octagon V2
 
+#define KEYMAP( \
+    K5A, K5B, K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5Q, \
+    K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, K4O, K4P, \
+    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3O, K3P, \
+    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2O, K2P, \
+    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1M, K1N, K1O, K1P, \
+    K0A, K0B, K0C,                K0J,           K0K, K0L, K0M, K0N, K0O, K0P  \
+) { \
+  { KC_##K5A, KC_##K5B, KC_##K5C, KC_##K5D, KC_##K5E, KC_##K5F, KC_##K5G, KC_##K5H, KC_##K5I, KC_##K5J, KC_##K5K, KC_##K5L, KC_##K5M, KC_##K5N, KC_##K5O, KC_NO,    KC_##K5Q }, \
+  { KC_##K4A, KC_##K4B, KC_##K4C, KC_##K4D, KC_##K4E, KC_##K4F, KC_##K4G, KC_##K4H, KC_##K4I, KC_##K4J, KC_##K4K, KC_##K4L, KC_##K4M, KC_NO,    KC_##K4O, KC_##K4P, KC_NO    }, \
+  { KC_##K3A, KC_##K3B, KC_##K3C, KC_##K3D, KC_##K3E, KC_##K3F, KC_##K3G, KC_##K3H, KC_##K3I, KC_##K3J, KC_##K3K, KC_##K3L, KC_##K3M, KC_NO,    KC_##K3O, KC_##K3P, KC_NO    }, \
+  { KC_##K2A, KC_##K2B, KC_##K2C, KC_##K2D, KC_##K2E, KC_##K2F, KC_##K2G, KC_##K2H, KC_##K2I, KC_##K2J, KC_##K2K, KC_##K2L, KC_##K2M, KC_NO,    KC_##K2O, KC_##K2P, KC_NO    }, \
+  { KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F, KC_##K1G, KC_##K1H, KC_##K1I, KC_##K1J, KC_##K1K, KC_NO,    KC_##K1M, KC_##K1N, KC_##K1O, KC_##K1P, KC_NO    }, \
+  { KC_##K0A, KC_##K0B, KC_##K0C, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##K0J, KC_NO,    KC_##K0K, KC_##K0L, KC_##K0M, KC_##K0N, KC_##K0O, KC_##K0P, KC_NO    }  \
+}
 #endif
