7dac2b25f28176a4890beb04a797d2bd323eb455
mechmerlin <mechmerlin@gmail.com>
2017-12-13 07:05:29 -0800

    Intermediate fix to get LEDs working

--- keyboards/octagon_v2/indicator_leds.c
+++ keyboards/octagon_v2/indicator_leds.c
@@ -30,6 +30,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NS_PER_CYCLE (NS_PER_SEC / CYCLES_PER_SEC)
 #define NS_TO_CYCLES(n) ((n) / NS_PER_CYCLE)
 
+enum Device {
+  Device_PCBRGB,
+  Device_STATUSLED
+};
+
 void send_bit_d4(bool bitVal) {
   if(bitVal) {
     asm volatile (
@@ -64,28 +69,68 @@ void send_bit_d4(bool bitVal) {
   }
 }
 
+void send_bit_d6(bool bitVal)
+{
+  if(bitVal) {
+    asm volatile (
+        "sbi %[port], %[bit] \n\t"
+        ".rept %[onCycles] \n\t"
+        "nop \n\t"
+        ".endr \n\t"
+        "cbi %[port], %[bit] \n\t"
+        ".rept %[offCycles] \n\t"
+        "nop \n\t"
+        ".endr \n\t"
+        ::
+        [port]      "I" (_SFR_IO_ADDR(PORTD)),
+        [bit]       "I" (6),
+        [onCycles]  "I" (NS_TO_CYCLES(T1H) - 2),
+        [offCycles] "I" (NS_TO_CYCLES(T1L) - 2));
+  } else {
+    asm volatile (
+        "sbi %[port], %[bit] \n\t"
+        ".rept %[onCycles] \n\t"
+        "nop \n\t"
+        ".endr \n\t"
+        "cbi %[port], %[bit] \n\t"
+        ".rept %[offCycles] \n\t"
+        "nop \n\t"
+        ".endr \n\t"
+        ::
+        [port]      "I" (_SFR_IO_ADDR(PORTD)),
+        [bit]       "I" (6),
+        [onCycles]  "I" (NS_TO_CYCLES(T0H) - 2),
+        [offCycles] "I" (NS_TO_CYCLES(T0L) - 2));
+  }
+}
+
 void show(void) {
   _delay_us((RES / 1000UL) + 1);
 }
 
-void send_value(uint8_t byte) {
+void send_value(uint8_t byte, enum Device device) {
   for(uint8_t b = 0; b < 8; b++) {
-    send_bit_d4(byte & 0b10000000);
+    if(device == Device_STATUSLED) {
+      send_bit_d4(byte & 0b10000000);
+    }
+    if(device == Device_PCBRGB) {
+      send_bit_d6(byte & 0b10000000);
+    }
     byte <<= 1;
   }
 }
 
-void send_color(uint8_t r, uint8_t g, uint8_t b) {
-  send_value(g);
-  send_value(r);
-  send_value(b);
+void send_color(uint8_t r, uint8_t g, uint8_t b, enum Device device) {
+  send_value(g, device);
+  send_value(r, device);
+  send_value(b, device);
 }
 
 void indicator_leds_set(bool leds[8]) {
   cli();
-  send_color(leds[1] ? 255 : 0, leds[2] ? 255 : 0, leds[0] ? 255 : 0);
-  send_color(leds[4] ? 255 : 0, leds[5] ? 255 : 0, leds[3] ? 255 : 0);
-  send_color(leds[6] ? 255 : 0, leds[7] ? 255 : 0, 0);
+  send_color(leds[1] ? 255 : 0, leds[2] ? 255 : 0, leds[0] ? 255 : 0, Device_STATUSLED);
+  send_color(leds[4] ? 255 : 0, leds[3] ? 255 : 0, leds[5] ? 255 : 0, Device_STATUSLED);
+  leds[6] ? (PORTD &= ~0b10000000) : (PORTD |= 0b10000000);
   sei();
   show();
 }
