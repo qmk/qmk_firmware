9bf83380ade52b5b0fabd58d3c73c47d1e7a2998
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-12-15 22:39:46 -0700

    Dupe my Planck layout to the Niu for now TODO: start abstracting common keymap code accross my boards

---
+++ keyboards/niu_mini/keymaps/jrolfs/keymap.c
@@ -0,0 +1,112 @@
+#include "niu_mini.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+// Layer names
+#define _BASE     0
+#define _LOWER    1
+#define _RAISE    2
+#define _BOTH     3
+#define _FUNCTION 4
+
+// Layer keycodes
+#define LOWER    M(_LOWER)
+#define RAISE    M(_RAISE)
+#define FUNCTION M(_FUNCTION)
+
+// Function macro keycodes
+#define ESC_CTL FUNC(0) // Tap for Esc, hold for Ctrl
+#define SFT_ENT FUNC(1) // Tap for Enter, hold for Shift
+
+// Function macros
+const uint16_t PROGMEM fn_actions[] = {
+    [0] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
+    [1] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT)
+};
+
+#define PEE_KEY KC_P
+#define PEE_KEY_LOWER KC_RPRN
+#define PEE_KEY_RAISE KC_0
+
+// Layer definitions
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = {
+    {KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,   PEE_KEY,   KC_BSPC},
+    {ESC_CTL,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT},
+    {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, SFT_ENT},
+    {FUNCTION, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_RGUI, KC_DQT, PEE_KEY,   KC_COLN}
+  },
+
+  [_LOWER] = {
+    {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, PEE_KEY_LOWER, _______},
+    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,       KC_PIPE},
+    {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______,       _______},
+    {_______, _______, _______, _______, _______, KC_PGDN, KC_PGDN, _______, _______, _______, PEE_KEY_LOWER, _______}
+  },
+
+  [_RAISE] = {
+    {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    PEE_KEY_RAISE, _______},
+    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,       KC_BSLS},
+    {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, _______, _______, _______,       _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, PEE_KEY_RAISE, KC__MUTE}
+  },
+
+  [_BOTH] = {
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, BL_DEC,  BL_INC,  _______, BL_TOGG}
+  },
+
+  [_FUNCTION] = {
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+    {_______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  }
+};
+
+// Set a layer persistently.
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+// Macro actions for each corresponding ID
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  switch(id) {
+    case _RAISE: // Raised layer
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _BOTH);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _BOTH);
+      }
+      break;
+
+    case _LOWER: // Lowered layer
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _BOTH);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _BOTH);
+      }
+      break;
+
+    case _FUNCTION:
+      if (record->event.pressed) {
+        layer_on(_FUNCTION);
+      } else {
+        layer_off(_FUNCTION);
+      }
+      break;
+  }
+
+
+  return MACRO_NONE;
+}
