a9e7754eb9e0c57c93180b7c2e93553fda6f6701
Jamie Rolfs <jamie.rolfs@gmail.com>
2017-12-18 00:29:40 -0800

    Add Scroll Lock/Pause for Mac brightness adjustment

--- keyboards/planck/keymaps/jrolfs/keymap.c
+++ keyboards/planck/keymaps/jrolfs/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
     {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
     {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______},
-    {_______, _______, _______, _______, _______, KC_PGDN, KC_PGDN, _______, _______, _______, _______, _______}
+    {_______, _______, _______, _______, _______, KC_PGDN, KC_PGDN, _______, KC_SLCK, KC_PAUS, _______, _______}
   },
 
   [_RAISE] = {
