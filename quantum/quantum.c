4f291f12e279f73ece0deef814bfd85b14841c98
Balz Guenat <balz.guenat@gmail.com>
2017-12-22 05:52:13 +0100

    use cie lightness for non-breathing backlight and make breathing able to reach true max brightness

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -912,7 +912,23 @@ void backlight_task(void) {
 
 #else // pwm through timer
 
-#define TIMER_TOP 0xFFFF
+#define TIMER_TOP 0xFFFFU
+
+// See http://jared.geek.nz/2013/feb/linear-led-pwm
+static uint16_t cie_lightness(uint16_t v) {
+  if (v <= 5243) // if below 8% of max
+    return v / 9; // same as dividing by 900%
+  else {
+    uint32_t y = (((uint32_t) v + 10486) << 8) / (10486 + 0xFFFFUL); // add 16% of max and compare
+    // to get a useful result with integer division, we shift left in the expression above
+    // and revert what we've done again after squaring.
+    y = y * y * y >> 8;
+    if (y > 0xFFFFUL) // prevent overflow
+      return 0xFFFFU;
+    else
+      return (uint16_t) y;
+  }
+}
 
 // range for val is [0..TIMER_TOP]. PWM pin is high while the timer count is below val.
 static inline void set_pwm(uint16_t val) {
@@ -932,7 +948,7 @@ void backlight_set(uint8_t level) {
     TCCR1A |= _BV(COM1x1);
   }
   // Set the brightness
-  set_pwm(TIMER_TOP * (uint32_t)level / BACKLIGHT_LEVELS);
+  set_pwm(cie_lightness(TIMER_TOP * (uint32_t)level / BACKLIGHT_LEVELS));
 }
 
 void backlight_task(void) {}
@@ -1022,22 +1038,6 @@ void breathing_period_dec(void)
  */
 static const uint8_t breathing_table[BREATHING_STEPS] PROGMEM = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 17, 20, 24, 28, 32, 36, 41, 46, 51, 57, 63, 70, 76, 83, 91, 98, 106, 113, 121, 129, 138, 146, 154, 162, 170, 178, 185, 193, 200, 207, 213, 220, 225, 231, 235, 240, 244, 247, 250, 252, 253, 254, 255, 254, 253, 252, 250, 247, 244, 240, 235, 231, 225, 220, 213, 207, 200, 193, 185, 178, 170, 162, 154, 146, 138, 129, 121, 113, 106, 98, 91, 83, 76, 70, 63, 57, 51, 46, 41, 36, 32, 28, 24, 20, 17, 15, 12, 10, 8, 6, 5, 4, 3, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-// See http://jared.geek.nz/2013/feb/linear-led-pwm
-static uint16_t cie_lightness(uint16_t v) {
-  if (v <= 5243) // if below 8% of max
-    return v / 9; // same as dividing by 900%
-  else {
-    uint32_t y = (((uint32_t) v + 10486) << 8) / (10486 + 0xFFFFUL); // add 16% of max and compare
-    // to get a useful result with integer division, we shift left in the expression above
-    // and revert what we've done again after squaring.
-    y = y * y * y >> 8;
-    if (y > 0xFFFFUL) // prevent overflow
-      return 0xFFFFU;
-    else
-      return (uint16_t) y;
-  }
-}
-
 // Use this before the cie_lightness function.
 static inline uint16_t scale_backlight(uint16_t v) {
   return v / BACKLIGHT_LEVELS * get_backlight_level();
@@ -1059,7 +1059,7 @@ ISR(TIMER1_OVF_vect)
       breathing_interrupt_disable();
   }
 
-  set_pwm(cie_lightness(scale_backlight((uint16_t) pgm_read_byte(&breathing_table[index]) << 8)));
+  set_pwm(cie_lightness(scale_backlight((uint16_t) pgm_read_byte(&breathing_table[index]) * 0x0101U)));
 }
 
 #endif // BACKLIGHT_BREATHING
