538097d9a06e02142f14e2ea8cce3e5f8b7dab24
U-LANDSRAAD\drashna <drashna@live.com>
2017-12-22 10:44:45 -0800

    Add One Shot Mod tap toggle

--- keyboards/ergodox_ez/keymaps/drashna/config.h
+++ keyboards/ergodox_ez/keymaps/drashna/config.h
@@ -18,5 +18,6 @@
 #define TAPPING_TERM 150
 #undef PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+#define ONESHOT_TAP_TOGGLE 2
 
 #endif
--- keyboards/orthodox/keymaps/drashna/config.h
+++ keyboards/orthodox/keymaps/drashna/config.h
@@ -41,6 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 150
 #undef PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+#define ONESHOT_TAP_TOGGLE 2
 
 
 
