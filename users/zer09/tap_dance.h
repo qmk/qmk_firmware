73d9732ae10e67e5acbebc676a00598e8ebb9644
Gilbert Consellado <gilbertconsellado@gmail.com>
2017-12-24 15:16:35 +0800

    add new enums for tap dance

--- users/zer09/tap_dance.h
+++ users/zer09/tap_dance.h
@@ -1,6 +1,19 @@
 #include "splinter.h"
 
 enum {
+  TP_SAFE_START,
+  TP_LCTL,
+  TP_LSFT,
+  TP_LALT,
+  TP_RCTL,
+  TP_RSFT,
+  TP_RALT,
+  TP_TILD,
+  TP_EGRV,
+  TP_SAFET_END
+};
+
+enum {
   CT_SAFE_START = 0,
   CT_N_TILDE,
   CT_ESC_GRV,
@@ -26,6 +39,30 @@ typedef struct {
 
 void init_tap_dance(void);
 int cur_dance(qk_tap_dance_state_t *);
+
+void dance_lctl_finished(qk_tap_dance_state_t *, void *);
+void dance_lctl_reset(qk_tap_dance_state_t *, void *);
+
+void dance_lsft_finished(qk_tap_dance_state_t *, void *);
+void dance_lsft_reset(qk_tap_dance_state_t *, void *);
+
+void dance_lalt_finished(qk_tap_dance_state_t *, void *);
+void dance_lalt_reset(qk_tap_dance_state_t *, void *);
+
+void dance_rctl_finished(qk_tap_dance_state_t *, void *);
+void dance_rctl_reset(qk_tap_dance_state_t *, void *);
+
+void dance_rsft_finished(qk_tap_dance_state_t *, void *);
+void dance_rsft_reset(qk_tap_dance_state_t *, void *);
+
+void dance_ralt_finished(qk_tap_dance_state_t *, void *);
+void dance_ralt_reset(qk_tap_dance_state_t *, void *);
+
+void dance_tild_finished(qk_tap_dance_state_t *, void *);
+void dance_tild_reset(qk_tap_dance_state_t *, void *);
+
+void dance_egrv_finished(qk_tap_dance_state_t *, void *);
+void dance_egrv_reset(qk_tap_dance_state_t *, void *);
 
 void n_tilde_finished(qk_tap_dance_state_t *, void *);
 void n_tilde_reset(qk_tap_dance_state_t *, void *);
