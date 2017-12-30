841954d0b2747ce057d7cc72eac4f0f0fc10329f
monsieurluge <monsieurluge@gmail.com>
2017-12-30 22:43:02 +0100

    move the arrows, home, end, page-up and page-down keys

--- keyboards/planck/keymaps/monsieurluge/keymap.c
+++ keyboards/planck/keymaps/monsieurluge/keymap.c
@@ -51,16 +51,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+-------------+------+------+------+------+------|
    * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Shift |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Ctrl | GUI  | Fn   | Alt  |Lower |    Space    |Raise | Alt  | Left | Down |Right |
+   * | Ctrl | GUI  | Fn   | Alt  |Lower |    Space    |Raise | Left | Up   | Down |Right |
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = {
     {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
     {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT},
-    {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_RSFT},
-    {KC_LCTL, KC_LGUI, MO(_FN), KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT}
+    {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
+    {KC_LCTL, KC_LGUI, MO(_FN), KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT}
   },
 
   /* Lower
@@ -69,16 +69,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+-------------+------+------+------+------+------|
    * |  `   |   '  |   {  |   [  |   (  |   !  |   ?  |   )  |   ]  |   }  |   "  |   =  |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      |      |      |   |  |   /  |   _  |   -  |   \  |   +  |      | PgUp |      |
+   * |      |      |      |   |  |   /  |   _  |   -  |   \  |   +  |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |Lower |    Space    |Raise |      | Home |PgDwn | End  |
+   * |      |      |      |      |Lower |    Space    |Raise | Home | PgUp |PgDwn | End  |
    * `-----------------------------------------------------------------------------------'
    */
   [_LOWER] = {
     {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DELETE},
     {KC_GRV,  KC_QUOT, KC_LCBR, KC_LBRC, KC_LPRN, KC_EXLM, KC_QUES, KC_RPRN, KC_RBRC, KC_RCBR, KC_DQT,  KC_EQUAL},
-    {XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_SLSH, KC_UNDS, KC_MINS, KC_BSLS, KC_PLUS, XXXXXXX, KC_PGUP, XXXXXXX},
-    {XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______, _______, _______, XXXXXXX, KC_HOME, KC_PGDN, KC_END}
+    {XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_SLSH, KC_UNDS, KC_MINS, KC_BSLS, KC_PLUS, XXXXXXX, XXXXXXX, XXXXXXX},
+    {XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}
   },
 
   /* Raise
@@ -87,16 +87,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+-------------+------+------+------+------+------|
    * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      | PgUp |      |
+   * |      |      |      |      |      |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |Lower |    Space    |Raise |      | Home |PgDwn | End  |
+   * |      |      |      |      |Lower |    Space    |Raise | Home | PgUp |PgDwn | End  |
    * `-----------------------------------------------------------------------------------'
    */
   [_RAISE] = {
     {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
     {XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX},
-    {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX},
-    {XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______, _______, _______, XXXXXXX, KC_HOME, KC_PGDN, KC_END}
+    {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX},
+    {XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}
   },
 
   /* Adjust (Lower + Raise)
