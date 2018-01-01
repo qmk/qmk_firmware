230f9d59c3e0055cb3c3293bfd10290b4d9f73b2
Daniel Hertz <daniel@dhertz.com>
2018-01-01 19:17:07 +0000

    Use qmk shortcuts rather than tmk functions

--- keyboards/whitefox/keymaps/dan/keymap.c
+++ keyboards/whitefox/keymaps/dan/keymap.c
@@ -30,9 +30,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = KEYMAP_ISO( \
         KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,    KC_DEL, \
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,             KC_PGUP,\
-        KC_FN5, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,     KC_ENT, KC_PGDN,\
+        CTL_T(KC_F19), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,     KC_ENT, KC_PGDN,\
         KC_LSFT,KC_FN1, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP,  KC_HOME,\
-        KC_FN2,KC_LALT, KC_FN6,                         KC_FN0,                 KC_FN7, KC_FN8, KC_FN10,     KC_LEFT,KC_DOWN,KC_RGHT \
+        CTL_T(KC_F18),KC_LALT, KC_FN6,                     LT(1, KC_SPC),              KC_FN7, KC_FN8, KC_FN10,     KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /* Layer 1: HHKB mode (Space)
      * ,---------------------------------------------------------------.
@@ -74,12 +74,12 @@ enum function_id {
  * Fn action definition
  */
 const uint16_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
-    [0]  = ACTION_LAYER_TAP_KEY(1, KC_SPC),            // function layer with Space
+ //   [0]  = ACTION_LAYER_TAP_KEY(1, KC_SPC),            // function layer with Space
     [1]  = ACTION_MACRO(HASH_TILDE),                   // hash / tilde
-    [2]  = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_F18),      // notification centre
-    [3]  = ACTION_LAYER_TAP_KEY(2, MOD_RCTL),          // wasd mouse mode
-    [4]  = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),          // tilde
-    [5]  = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_F19),      // alfred
+ //   [2]  = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_F18),      // notification centre
+ //   [3]  = ACTION_LAYER_TAP_KEY(2, MOD_RCTL),          // wasd mouse mode
+ //   [4]  = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),          // tilde
+ //   [5]  = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_F19),      // alfred
     [6]  = ACTION_FUNCTION_TAP(CMD_TAB_CMD),           // tap cmd tab or cmd
     [7]  = ACTION_FUNCTION_TAP(CMD_GRV_CMD),                    // grave tab
     [8]  = ACTION_MACRO(CTRL_A),                       // ctrl a
