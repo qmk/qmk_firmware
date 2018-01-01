b2c38a170fbee95137bf0520e6e1d4b4bebd7a93
Stacy Devino <stacydevino@gmail.com>
2018-01-01 17:55:18 -0600

    JC65 - Adding in CapsLock LED support and my personal keymap
    Signed-off-by: Stacy Devino <stacydevino@gmail.com>

--- keyboards/jc65/jc65.c
+++ keyboards/jc65/jc65.c
@@ -23,6 +23,14 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 
 void led_set_kb(uint8_t usb_led) {
 	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+        // output low
+        DDRB |= (1<<2);
+        PORTB &= ~(1<<2);
+    } else {
+        // Hi-Z
+        DDRB &= ~(1<<2);
+        PORTB &= ~(1<<2);
+    }
 	led_set_user(usb_led);
 }
