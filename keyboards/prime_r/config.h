b3390f10752854a97c36f7e4f6d7bf1ad5fcbbba
rooski15 <34818505+rooski15@users.noreply.github.com>
2018-01-03 22:49:49 -0800

    Update config.h
    Added device information

--- keyboards/prime_r/config.h
+++ keyboards/prime_r/config.h
@@ -24,9 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define MANUFACTURER    PrimeKB
+#define PRODUCT         Prime_R
+#define DESCRIPTION     A compact layout with mini-numpad.
 
 /* key matrix size */
 #define MATRIX_ROWS 5
