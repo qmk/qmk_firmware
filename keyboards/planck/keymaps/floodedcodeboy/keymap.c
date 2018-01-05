ad3ec287fa984386c4985267f761431646386028
Jacob Gabriel <jacob.gabriel@essencedigital.com>
2018-01-05 10:58:12 +0000

    cleanup the lower layer

--- keyboards/planck/keymaps/floodedcodeboy/keymap.c
+++ keyboards/planck/keymaps/floodedcodeboy/keymap.c
@@ -32,7 +32,7 @@ enum planck_keycodes
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-        /* Qwerty
+    /* Qwerty
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -40,21 +40,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |(LShft|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |RShft)|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | TBC1 | Ctrl | Alt  | GUI  |Lower |Space |Space |Raise |   /  | Left | Down |Right |
+ * | TBC1 | Ctrl | Alt  | GUI  |Lower |    Space    |Raise |   /  | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
- * 
+ *
  * TBC1 - toggle Iterm
  * LShift - Shift when held / Caps when tapped
  * Enter - MOD TAP with ENTER / R.SHIFT If held
  * Esc - Esc then ~ when pressed with Shift
  */
-        [_QWERTY] = {
-            {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
-            {KC_GESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
-            {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, MT(MOD_RSFT, KC_ENTER)},
-            {LCTL(KC_GRAVE), KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT}},
+    [_QWERTY] = {
+        {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
+        {KC_GESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
+        {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, MT(MOD_RSFT, KC_ENTER)},
+        {LCTL(KC_GRAVE), KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT}},
 
-        /* Lower
+    /* Lower
  * ,-----------------------------------------------------------------------------------.
  * | Mute |  F1  |  F2  |  F3  |   [  |   *  |   +  |   ]  |      |  7   |  8   |  9   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -66,35 +66,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  *
  * Proposed Additions:
- * CMD+~ to bring up iterm
  * AMY - Amythest Layer
+ *
  */
-        [_LOWER] = {
-            {KC_MUTE, KC_F1, KC_F2, KC_F3, KC_LBRC, KC_ASTERISK, KC_PLUS, KC_RBRC, _______, KC_KP_7, KC_KP_8, KC_KP_9}, 
-            {KC_VOLU, KC_F4, KC_F5, KC_F6, KC_LCBR, KC_SLSH, KC_MINUS, KC_RCBR, _______, KC_KP_4, KC_KP_5, KC_KP_6}, 
-            {KC_VOLD, KC_F7, KC_F8, KC_F9, KC_LPRN, KC_PIPE, KC_BSLASH, KC_RPRN, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3}, 
-            {_______, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}
-        },
+    [_LOWER] = {
+        {KC_MUTE, KC_F1, KC_F2, KC_F3, _______, KC_LCBR, KC_RCBR, KC_SLASH, KC_ASTERISK, KC_KP_7, KC_KP_8, KC_KP_9},
+        {KC_VOLU, KC_F4, KC_F5, KC_F6, _______, KC_LPRN, KC_RPRN, KC_MINUS, KC_PLUS, KC_KP_4, KC_KP_5, KC_KP_6},
+        {KC_VOLD, KC_F7, KC_F8, KC_F9, _______, KC_PIPE, KC_BSLASH, _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3},
+        {_______, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}
+    },
 
-        /* Raise
+    /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | DEL  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      | PgUp |PgDwn |   -  |   =  |   [  |   ]  |  \   |
+ * |      |      |      |      |      |      |      |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      | Home | End  |   _  |   {  |   }  |   (  |  )   |
+ * |      |      |      |      |      |      |      |   _  |   {  |   }  |   (  |  )   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Prev | Vol- | Vol+ | Next |
+ * |      |      |      |      |      |             |      |      | Vol- | Vol+ |      |
  * `-----------------------------------------------------------------------------------'
  */
-        [_RAISE] = {
-            {KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL}, 
-            {_______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS}, 
-            {_______, _______, _______, _______, _______, KC_HOME, KC_END, KC_UNDERSCORE, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN}, 
-            {_______, _______, _______, _______, _______, _______, _______, _______, KC_MEDIA_PREV_TRACK, KC_VOLD, KC_VOLU, KC_MEDIA_NEXT_TRACK}
-        },
+    [_RAISE] = {
+        {KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL},
+        {_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS},
+        {_______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______}
+    },
 
-        /* Adjust (Lower + Raise)
+    /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -105,14 +105,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-        [_ADJUST] = {
-            {_______, RESET, DEBUG, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_DEL}, 
-            {_______, _______, MU_MOD, AU_ON, AU_OFF, AG_NORM, AG_SWAP, QWERTY, _______, _______, _______, _______}, 
-            {_______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, BL_TOGG, BL_STEP, _______, _______}, 
-            {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-        },
+    [_ADJUST] = {
+        {_______, RESET, DEBUG, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_DEL}, 
+        {_______, _______, MU_MOD, AU_ON, AU_OFF, AG_NORM, AG_SWAP, QWERTY, _______, _______, _______, _______}, 
+        {_______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, BL_TOGG, BL_STEP, _______, _______}, 
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+    },
 
-        /* Amythest ()
+    /* Amythest ()
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Increase Main Pane Count : Alt +  Shift + , : LALT(LSFT(KC_COMMA))
  * Decrease Main Pane Count : Alt + Shift + . : LALT(LSFT(KC_DOT))
  */
-        [_AMYTHEST] = {{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}}
+    [_AMYTHEST] = {{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}}
 
 };
 
@@ -154,7 +154,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         break;
     case LOWER:
         if (record->event.pressed)
-        {   
+        {
             layer_on(_LOWER);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
         }
