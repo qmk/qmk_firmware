4cb99b14f149b01fe25b4a3ce489041367587e63
Jamie Rolfs <jamie.rolfs@gmail.com>
2018-01-08 01:25:51 -0800

    Add jrolfs Let's Split implementation

---
+++ keyboards/lets_split/keymaps/jrolfs/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define USE_SERIAL
+
+#undef RGBLED_NUM
+#define RGBLED_NUM 8
+#define RGBLIGHT_ANIMATIONS
+
+#define EE_HANDS
+
+#endif
---
+++ keyboards/lets_split/keymaps/jrolfs/keymap.c
@@ -0,0 +1,4 @@
+#include "lets_split.h"
+
+#include "jrolfs.h"
+#include "jrolfs_layers.h"
