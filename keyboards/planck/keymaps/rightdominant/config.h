f29fa85d410ffdd64ecb1dcd9350a8123ea01df2
Jon Arintok <jon.Arintok@Gmail.com>
2018-01-08 22:56:43 -0500

    added rightdominant keymap

---
+++ keyboards/planck/keymaps/rightdominant/config.h
@@ -0,0 +1,16 @@
+
+#define MK_MOMENTARY_ACCEL // comment this out for tap-to-select acceleration
+// medium values will be used as default
+#define MK_C_OFFSET_SLOW     1 // pixels
+#define MK_C_INTERVAL_SLOW 100 // milliseconds
+#define MK_C_OFFSET_MED      4
+#define MK_C_INTERVAL_MED   16
+#define MK_C_OFFSET_FAST    12
+#define MK_C_INTERVAL_FAST  16
+
+#define MK_W_OFFSET_SLOW     1 // wheel clicks
+#define MK_W_INTERVAL_SLOW 400 // milliseconds
+#define MK_W_OFFSET_MED      1
+#define MK_W_INTERVAL_MED  200
+#define MK_W_OFFSET_FAST     1
+#define MK_W_INTERVAL_FAST 100
