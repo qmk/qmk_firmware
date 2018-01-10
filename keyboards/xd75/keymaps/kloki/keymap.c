36d4fb24e233de21949ec3b4384457a1d63c3f98
Koen Klinkers <koen.klinkers@devmob.com>
2018-01-10 22:35:48 +0100

    bslash

--- keyboards/xd75/keymaps/kloki/keymap.c
+++ keyboards/xd75/keymaps/kloki/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_WO] = { /* WORKMAN */
   { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   MO(_FUN),TG(_NUM),TG(_QW),KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
-  { KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,   KC_B,   KC_GRV,  KC_LGUI, KC_QUOT, KC_J,   KC_F,   KC_U,    KC_P,    KC_SCLN, KC_QUOT },
+  { KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,   KC_B,   KC_GRV,  KC_LGUI, KC_QUOT, KC_J,   KC_F,   KC_U,    KC_P,    KC_SCLN, KC_BSLS },
   { CTL_T(KC_ESC),KC_A,KC_S,   KC_H,    KC_T,   KC_G,   KC_EQL,  KC_UP,   KC_MINS, KC_Y,   KC_N,   KC_E,    KC_O,    KC_I,    KC_ENT  },
   { KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,   KC_V,   KC_LEFT, KC_DOWN, KC_RGHT, KC_K,   KC_L,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
   { KC_MEH,KC_LGUI,KC_LALT,KC_LEFT,KC_RGHT,LT(_SYM,KC_SPC),LGUI_T(KC_ENT),KC_SPC,LT(_SYM,KC_TAB),KC_BSPC,KC_DOWN,KC_UP,KC_LBRC,KC_RBRC,MO(_SYM)},
