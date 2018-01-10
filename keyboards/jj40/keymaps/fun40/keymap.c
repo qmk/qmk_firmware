b8e53366a14dcb2ce8f24c682f0d7856f751754d
randtrawick <randtrawick@gmail.com>
2018-01-10 07:36:56 -0700

    updated keymap

--- keyboards/jj40/keymaps/fun40/keymap.c
+++ keyboards/jj40/keymaps/fun40/keymap.c
@@ -53,14 +53,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Shift|PrScr |ISO ~ |ISO | |      |      |      |      |      |bl_on |bl_stp| Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | nkro | Alt  | NKRO |Lower | PgDn | PgUp |Raise | Next | Vol- | Vol+ | Play |
+ * | Ctrl | nkro | Alt  | NKRO |Lower | Home | End  |Raise | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, \
   KC_LSFT, KC_PSCR, S(KC_NUHS), S(KC_NUBS), _______, _______, _______, KC_POWER, _______, BL_ON, BL_STEP, KC_ENT, \
-  KC_LCTL, MAGIC_TOGGLE_NKRO, KC_LALT, TG_NKRO, _______, KC_PGDN, KC_PGUP, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_LCTL, MAGIC_TOGGLE_NKRO, KC_LALT, TG_NKRO, _______, KC_HOME, KC_END, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Raise
@@ -71,14 +71,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Ins  |  `   |   \  |   |  |   [  |   ]  |      |   1  |   2  |   3  |   .  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |      | Alt  |      |Lower |      |      |Raise |   0  |      | Home | End  |
+ * | Ctrl |      | Alt  |      |Lower | PgDn | PgUp |Raise |   0  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
   KC_CAPS, KC_MINS, KC_PLUS, KC_EQL,  KC_LPRN, KC_RPRN, _______, KC_7,  KC_8,    KC_9, KC_BSPC, KC_DEL, \
   KC_LOCK,  KC_TILD, KC_UNDS, _______, KC_LCBR, KC_RCBR, _______, KC_4,  KC_5,    KC_6, _______, _______, \
-  KC_INS, KC_GRV, KC_BSLS, KC_PIPE, KC_LBRC, KC_RBRC,_______, KC_1,  KC_2,    KC_3, KC_DOT, _______, \
-  KC_LCTL, _______, KC_LALT, _______, _______, _______, _______, _______, KC_0, _______, KC_HOME, KC_END \
+  KC_INS, KC_GRV, KC_BSLS, KC_PIPE, KC_LBRC, KC_RBRC,_______, KC_1,  KC_2,    KC_3, KC_DOT, KC_ENT, \
+  KC_LCTL, _______, KC_LALT, _______, _______, KC_PGDN, KC_PGUP, _______, KC_0, _______, _______, _______ \
 )
 
 /* Adjust
