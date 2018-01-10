e862cd0f3fd046a7a7f4ff4293f112a60e5c81f6
Koen Klinkers <koen.klinkers@devmob.com>
2018-01-10 22:01:01 +0100

    added rgb controls

--- keyboards/xd75/keymaps/kloki/keymap.c
+++ keyboards/xd75/keymaps/kloki/keymap.c
@@ -20,13 +20,14 @@
 #define _QW 1
 #define _NUM 2
 #define _SYM 3
+#define _FUN 4
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
  [_WO] = { /* WORKMAN */
-  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_LSFT, TG(_NUM),TG(_QW),KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
+  { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   MO(_FUN),TG(_NUM),TG(_QW),KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
   { KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,   KC_B,   KC_GRV,  KC_LGUI, KC_QUOT, KC_J,   KC_F,   KC_U,    KC_P,    KC_SCLN, KC_QUOT },
   { CTL_T(KC_ESC),KC_A,KC_S,   KC_H,    KC_T,   KC_G,   KC_EQL,  KC_UP,   KC_MINS, KC_Y,   KC_N,   KC_E,    KC_O,    KC_I,    KC_ENT  },
   { KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,   KC_V,   KC_LEFT, KC_DOWN, KC_RGHT, KC_K,   KC_L,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
@@ -49,10 +50,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { _______,  _______, _______, _______, _______,_______,_______, KC_0, _______, _______,_______,_______, _______, _______, _______ },
  },
  [_SYM] = { /* SYM */
-  { _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  _______, KC_F11,  KC_F12,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_DEL, },
-  { _______,  KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR,KC_ASTR,_______, _______, _______, KC_LABK,KC_HOME,KC_UP,   KC_PGUP, KC_ASTR, _______ },
+  { _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  _______, _______, _______, KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_DEL, },
+  { _______,  KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR,KC_ASTR,_______, KC_F11,  KC_F12,  KC_LABK,KC_HOME,KC_UP,   KC_PGUP, KC_ASTR, _______ },
   { _______,  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN,KC_GRV, _______, _______, _______, KC_RABK,KC_LEFT,KC_DOWN, KC_RIGHT,KC_PLUS, _______ },
   { _______,  KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC,KC_TILD,_______, _______, _______, KC_AMPR,KC_END, KC_DOWN, KC_PGDN, KC_BSLS, _______ },
+  { _______,  _______, _______, _______, _______,_______,_______, _______, _______, _______,_______,_______, _______, _______, _______ },
+ },
+ [_FUN] = { /* FUN */
+  { _______,  RGB_RMOD,RGB_MOD, RGB_TOG, _______,_______,_______, _______, _______, _______,_______,_______, _______, _______, RESET   },
+  { _______,  RGB_HUD, RGB_HUI, _______, _______,_______,_______, _______, _______, _______,_______,_______, _______, _______, _______ },
+  { _______,  RGB_SAD, RGB_SAI, _______, _______,_______,_______, _______, _______, _______,_______,_______, _______, _______, _______ },
+  { _______,  RGB_VAD, RGB_VAI, _______, _______,_______,_______, _______, _______, _______,_______,_______, _______, _______, _______ },
   { _______,  _______, _______, _______, _______,_______,_______, _______, _______, _______,_______,_______, _______, _______, _______ },
  }
 };
