f1d9a71c3a0a712b8c57042484eed5e6ad5ab109
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-01-12 18:57:48 +0100

    Updated files to better support sockets version (#2255)

--- keyboards/lets_split/matrix.c
+++ keyboards/lets_split/matrix.c
@@ -124,6 +124,12 @@ uint8_t matrix_cols(void)
 
 void matrix_init(void)
 {
+#ifdef DISABLE_JTAG
+  // JTAG disable for PORT F. write JTD bit twice within four cycles.
+  MCUCR |= (1<<JTD);
+  MCUCR |= (1<<JTD);
+#endif
+
     debug_enable = true;
     debug_matrix = true;
     debug_mouse = true;
