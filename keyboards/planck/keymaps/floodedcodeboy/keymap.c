b45c2c67c4b95f948afe4c830237213a0f81e58f
Jacob Gabriel <floodedcodeboy@gmail.com>
2018-01-13 11:20:38 +0000

    move amythest commands to defined variabls

--- keyboards/planck/keymaps/floodedcodeboy/keymap.c
+++ keyboards/planck/keymaps/floodedcodeboy/keymap.c
@@ -21,6 +21,17 @@ enum planck_keycodes
     AMYTHEST
 };
 
+// Amythest Window Management
+#define AM_SET_FOCUS_MAIN LALT(LSFT(KC_ENTER))
+#define AM_CYCLE_FWD LALT(LSFT(KC_SPACE))
+#define AM_CYCLE_BK LCTL(LALT(LSFT(KC_SPACE)))
+#define AM_SHRINK_PANE LALT(LSFT(KC_H))
+#define AM_EXPAND_PANE LALT(LSFT(KC_L))
+#define AM_FOCUS_CCW LALT(LSFT(KC_J))
+#define AM_FOCUS_CW LALT(LSFT(KC_K))
+#define AM_INC_PANE_COUNT LALT(LSFT(KC_COMMA))
+#define AM_DEC_PANE_COUNT LALT(LSFT(KC_DOT))
+
 // enum
 // {
 //     TD_SPC_SFT = 0
@@ -64,17 +75,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F10 |  F11 |  F12 | |||| |             | |||| | Home |PG Up |PG Dn | End  |
  * `-----------------------------------------------------------------------------------'
+ * Proposed
+ * * ,-----------------------------------------------------------------------------------.
+ * | Mute |  F1  |  F2  |  F3  |   [  |   *  |   +  |   ]  |      |  7   |  8   |  9   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Vol+ |  F4  |  F5  |  F6  |   {  |   /  |   -  |   }  |      |  4   |  5   |  6   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Vol- |  F7  |  F8  |  F9  |   (  |   |  |   \  |   )  |   0  |  1   |  2   |  3   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |  F10 |  F11 |  F12 | |||| |             | |||| | Home |PG Up |PG Dn | End  |
+ * `-----------------------------------------------------------------------------------'
+ *
  *
  * Proposed Additions:
  * AMY - Amythest Layer
  *
  */
-    [_LOWER] = {
-        {KC_MUTE, KC_F1, KC_F2, KC_F3, KC_LCBR, KC_ASTERISK, KC_PLUS, KC_RCBR, KC_KP_7, KC_KP_8, KC_KP_9},
-        {KC_VOLU, KC_F4, KC_F5, KC_F6, KC_LPRN, KC_BSLASH, KC_SLASH, KC_RPRN, KC_KP_4, KC_KP_5, KC_KP_6},
-        {KC_VOLD, KC_F7, KC_F8, KC_F9, _______, KC_PIPE, KC_MINUS, _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3},
-        {_______, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}
-    },
+    [_LOWER] = {{KC_MUTE, KC_F1, KC_F2, KC_F3, KC_LCBR, KC_ASTERISK, KC_PLUS, KC_RCBR, KC_KP_7, KC_KP_8, KC_KP_9}, {KC_VOLU, KC_F4, KC_F5, KC_F6, KC_LPRN, KC_BSLASH, KC_SLASH, KC_RPRN, KC_KP_4, KC_KP_5, KC_KP_6}, {KC_VOLD, KC_F7, KC_F8, KC_F9, _______, KC_PIPE, KC_MINUS, _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3}, {_______, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END}},
 
     /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -87,12 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      | Vol- | Vol+ |      |
  * `-----------------------------------------------------------------------------------'
  */
-    [_RAISE] = {
-        {KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL},
-        {_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS},
-        {_______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
-        {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______}
-    },
+    [_RAISE] = {{KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL}, {_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS}, {_______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______}},
 
     /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
@@ -105,12 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-    [_ADJUST] = {
-        {_______, RESET, DEBUG, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_DEL}, 
-        {_______, _______, MU_MOD, AU_ON, AU_OFF, AG_NORM, AG_SWAP, QWERTY, _______, _______, _______, _______}, 
-        {_______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, BL_TOGG, BL_STEP, _______, _______}, 
-        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-    },
+    [_ADJUST] = {{_______, RESET, DEBUG, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_DEL}, {_______, _______, MU_MOD, AU_ON, AU_OFF, AG_NORM, AG_SWAP, QWERTY, _______, _______, _______, _______}, {_______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, _______, BL_TOGG, BL_STEP, _______, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}},
 
     /* Amythest ()
  * ,-----------------------------------------------------------------------------------.
