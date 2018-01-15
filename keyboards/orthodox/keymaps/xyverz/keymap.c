dc88242594f51f145a36e1401cfdb1d23aed5951
Xyverz <xyverz@gmail.com>
2018-01-15 13:14:51 -0800

    Added Readme
    I finally got around to adding a readme to this keymap. I've also added minor changes to the layout.

--- keyboards/orthodox/keymaps/xyverz/keymap.c
+++ keyboards/orthodox/keymaps/xyverz/keymap.c
@@ -70,13 +70,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = KEYMAP ( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                                KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                                KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
   KC_CAPS, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, XXXXXXX, KC_HOME,          KC_PGUP, XXXXXXX, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, _______, KC_LEFT, KC_RGHT, _______, _______, _______, KC_DEL,  KC_END,           KC_PGDN, KC_INS,  _______, _______, _______, KC_UP,   KC_DOWN, _______, _______ \
 ),
 
 [_RAISE] = KEYMAP ( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   KC_CAPS, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, XXXXXXX, KC_HOME,          KC_PGUP, XXXXXXX, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, _______, KC_LEFT, KC_RGHT, _______, _______, _______, KC_DEL,  KC_END,           KC_PGDN, KC_INS,  _______, _______, _______, KC_UP,   KC_DOWN, _______, _______ \
 ),
