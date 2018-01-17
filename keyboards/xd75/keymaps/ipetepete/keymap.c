68f579bd78a4264e2269846c1ce190aaf4126476
Peter Peterson <peter.peterson.iv@gmail.com>
2018-01-17 13:51:10 -0700

    tweaking keymap

--- keyboards/xd75/keymaps/ipetepete/keymap.c
+++ keyboards/xd75/keymaps/ipetepete/keymap.c
@@ -18,14 +18,13 @@
 #define ESCCTRL CTL_T(KC_ESC)
 #define KC_SPFN LT(_SPCFN, KC_SPC) // press for space, hold for function layer (aka spacefn)
 #define HYPER MT(MOD_HYPR, KC_ENT)
+#define KC_FUNC MO(_FUNC)
 #define ENTSFT SFT_T(KC_ENT)
 #define APPTOG LGUI(KC_TAB) // toggles last two apps used
 #define APPWIN LGUI(KC_GRV) // toggles app windows (Mac/Linux)
 #define SPCRGT LCTL(KC_RGHT) // Switch workspaces
 #define SPCLFT LCTL(KC_LEFT)
 
-#define _______ KC_TRANS
-#define XXXXXXX KC_NO
 
 // Layer shorthand
 enum layers{
@@ -45,8 +44,7 @@ enum keycodes{
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    /* QWERTY
+ /* QWERTY SPLIT
  * .--------------------------------------------------------------------------------------------------------------------------------.
  * | F10  | F11  | F12  | `      | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | BACKSP |
  * |------+------+------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
@@ -62,12 +60,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = {
         /* QWERTY */
-        {KC_F10,  KC_F11,  KC_F12,  KC_GRV,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
-        {KC_F7,   KC_F8,   KC_F9,   KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS},
-        {KC_F4,   KC_F5,   KC_F6,   ESCCTRL, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-        {KC_F1,   KC_F2,   KC_F3,   KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENTSFT},
-        {KC_PGDN, KC_PGUP, KC_HOME, FUNC,    HYPER, KC_LCTL, KC_LGUI, KC_LALT, SPCFN,  SPCFN,  KC_RALT, KC_RGUI, KC_RCTL, HYPER,   FUNC},
+        {KC_GRV,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5, KC_MINS, KC_EQL, KC_BSLS,    KC_6,   KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC},
+        {KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC, KC_PGUP, KC_RBRC,    KC_Y,   KC_U, KC_I,    KC_O,    KC_P,    KC_BSLS},
+        {ESCCTRL, KC_A,  KC_S,    KC_D,    KC_F,    KC_G, KC_LCBR, KC_PGDN, KC_RCBR,    KC_H,   KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+        {KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN, KC_HOME, KC_RPRN,    KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, ENTSFT},
+        {KC_FUNC,MOD_HYPR,KC_LGUI, KC_LALT,KC_LCTL, KC_SPFN, _______, KC_END, _______, KC_SPFN,KC_RALT, KC_RGUI, KC_RCTL, HYPER, KC_FUNC},
     },
+
+
 
 /* SPCFN
  * .-----------------------------------------------------------------------------------------------------------------------------------------.
@@ -85,11 +85,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SPCFN] = {
         /* SPCFN */
-        {_______, KC_SLSH, KC_ASTR, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, KC_UP,   KC_MINS,  KC_EQL, KC_DEL,
-        {KC_P7,  KC_P8,   KC_P9,    _______, _______, SPCLFT,  KC_PGDN, SPCRGT,  KC_LBRC, KC_RBRC, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,
-        {KC_P4,   KC_P5,   KC_P6,   _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______, _______,
-        {KC_P1,   KC_P2,   KC_P3,   _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______,
-        {KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        {_______, _______, _______, _______, _______, _______, KC_F10,  KC_F11,  KC_F12,  _______, _______,_______,KC_MINS,  KC_EQL, KC_DEL},
+        {_______, _______, _______, _______, _______, KC_LBRC, KC_F7,   KC_F8,   KC_F9,   KC_RBRC, _______, KC_UP,   _______, _______, _______},
+        {_______, _______, _______, _______, _______, KC_LCBR, KC_F4,   KC_F5,   KC_F6,   KC_RCBR, KC_LEFT, KC_DOWN, KC_RIGHT,_______, _______},
+        {_______, _______, _______, _______, _______, KC_LPRN, KC_F1,   KC_F2,   KC_F3,   KC_RPRN, _______, _______, _______, _______, _______},
+        {_______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______},
+    },
+
+    [_FUNC] = {
+        /* SPCFN */
+        {RESET,   _______,   _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, _______, _______, _______, _______, _______, _______},
+        {_______,DF(_QWERTY),_______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______},
+        {_______, _______,   _______, _______, _______, _______, _______, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______},
+        {_______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+        {_______, _______,   _______, _______, _______, _______, AG_NORM, AG_SWAP, _______, _______, _______, _______, _______, _______, _______},
     }
 };
 
