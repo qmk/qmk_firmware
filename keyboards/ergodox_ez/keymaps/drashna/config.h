c52c0faa31fdfecae1332df87fa518387b8ad255
Drashna Jaelre <drashna@live.com>
2018-01-17 11:28:23 -0800

    Update to drashna keymaps (#2281)
    * Move faux clicky into userspace
    
    * Get Audio and RGB enabled on Orthodox-rev1
    
    * Add faux click to userspace
    
    * Add Orthodox Rev3 check to macros
    
    * Hack Orthodox Name for drashna keymap
    
    * No more One Shots
    
    * Ergodox product name hack
    
    * Enable Audio on Orthodox by default

--- keyboards/ergodox_ez/keymaps/drashna/config.h
+++ keyboards/ergodox_ez/keymaps/drashna/config.h
@@ -20,4 +20,7 @@
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 #define ONESHOT_TAP_TOGGLE 2
 
+#undef PRODUCT
+#define PRODUCT         DrashnaDox - Hacked ErgoDox EZ Shine
+
 #endif
