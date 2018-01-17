9db7fef084759cef373a2eb5a79b6a632488d37c
monsieurluge <monsieurluge@gmail.com>
2018-01-17 23:22:41 +0100

    remove the unicode characters and update the Fn disposition

--- keyboards/planck/keymaps/monsieurluge/rules.mk
+++ keyboards/planck/keymaps/monsieurluge/rules.mk
@@ -1,9 +1,5 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
-
-UNICODE_ENABLE = yes
+AUDIO_ENABLE = no
+# UNICODE_ENABLE = yes
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
