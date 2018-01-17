59477983a82c80e7924a2242fbb05146ab901b4f
Peter Peterson <ppeterson@noao.edu>
2018-01-17 15:02:47 -0700

    updated keymap

--- keyboards/xd75/keymaps/ipetepete/keymap.c
+++ keyboards/xd75/keymaps/ipetepete/keymap.c
@@ -18,8 +18,8 @@
 #define ESCCTRL CTL_T(KC_ESC)
 #define KC_SPFN LT(_SPCFN, KC_SPC) // press for space, hold for function layer (aka spacefn)
 #define HYPER MT(MOD_HYPR, KC_ENT)
-#define KC_FUNC MO(_FUNC)
 #define ENTSFT SFT_T(KC_ENT)
+#define KC_FUN MO(_FUN)
 #define APPTOG LGUI(KC_TAB) // toggles last two apps used
 #define APPWIN LGUI(KC_GRV) // toggles app windows (Mac/Linux)
 #define SPCRGT LCTL(KC_RGHT) // Switch workspaces
@@ -29,17 +29,17 @@
 // Layer shorthand
 enum layers{
   _QWERTY = 0,
-  _FUNC,
-  _UPPER,
   _LOWER,
-  _SPCFN
+  _GAMR,
+  _SPCFN,
+  _FUN,
 };
 
 enum keycodes{
   QWERTY = 0,
   FUNC,
-  UPPER,
-  LOWER,
+  LO,
+  UP,
   SPCFN
 };
 
@@ -64,10 +64,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC, KC_PGUP, KC_RBRC,    KC_Y,   KC_U, KC_I,    KC_O,    KC_P,    KC_BSLS},
         {ESCCTRL, KC_A,  KC_S,    KC_D,    KC_F,    KC_G, KC_LCBR, KC_PGDN, KC_RCBR,    KC_H,   KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
         {KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN, KC_HOME, KC_RPRN,    KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, ENTSFT},
-        {KC_FUNC,MOD_HYPR,KC_LGUI, KC_LALT,KC_LCTL, KC_SPFN, _______, KC_END, _______, KC_SPFN,KC_RALT, KC_RGUI, KC_RCTL, HYPER, KC_FUNC},
+        {KC_FUN,KC_LGUI,KC_LALT,KC_LCTL,MO(_LOWER), KC_SPFN, _______, KC_END, _______, KC_SPFN, KC_RCTL, KC_RALT, KC_RGUI, MOD_HYPR,KC_FUN},
     },
 
+    [_LOWER] = {
+        /* SPCFN */
+        {_______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______},
+        {APPTOG,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, XXXXXXX, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  XXXXXXX},
+        {APPWIN,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX, XXXXXXX, XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX},
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+    },
 
+    [_GAMR] = {
+        /* QWERTY */
+        {KC_GRV,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5, KC_F7, KC_F8,  KC_F9,    KC_6,   KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC},
+        {KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T, KC_F4, KC_F5,  KC_F6,    KC_Y,   KC_U, KC_I,    KC_O,    KC_P,    KC_BSLS},
+        {KC_ESC,  KC_A,  KC_S,    KC_D,    KC_F,    KC_G, KC_F1, KC_F2,  KC_F3,    KC_H,   KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+        {KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX, KC_UP,  XXXXXXX,    KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, ENTSFT},
+        {KC_FUN,KC_LGUI,KC_LALT,KC_LCTL,  KC_SPC,KC_SPFN,KC_LEFT, KC_DOWN, KC_LEFT, KC_SPFN, KC_RCTL, KC_RALT, KC_RGUI, MOD_HYPR,KC_FUN},
+    },
 
 /* SPCFN
  * .-----------------------------------------------------------------------------------------------------------------------------------------.
@@ -92,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {_______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______},
     },
 
-    [_FUNC] = {
+    [_FUN] = {
         /* SPCFN */
         {RESET,   _______,   _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, _______, _______, _______, _______, _______, _______},
         {_______,DF(_QWERTY),_______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______},
-        {_______, _______,   _______, _______, _______, _______, _______, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______},
+        {_______, _______,   _______, _______, _______, DF(_GAMR), _______, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______},
         {_______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
         {_______, _______,   _______, _______, _______, _______, AG_NORM, AG_SWAP, _______, _______, _______, _______, _______, _______, _______},
     }
