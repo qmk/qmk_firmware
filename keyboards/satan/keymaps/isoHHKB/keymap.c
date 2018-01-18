4f796ce35e69225abe8b438b2e5a4b0dfa69a12b
Alex Vogel <avogelm@gmail.com>
2018-01-19 00:50:01 +0100

    First version of my HHKB ISO Spanish Keymap

--- keyboards/satan/keymaps/isoHHKB/keymap.c
+++ keyboards/satan/keymaps/isoHHKB/keymap.c
@@ -11,11 +11,10 @@
 #define _BL 0
 #define _FL 1
 
-#define _______ KC_TRNS
+#define KC_ENYE M(0)
+#define KC_CEDL M(1)
 
-// Special characters 'ç' and 'ñ' for spanish users
-#define KC_CEDL LALT(KC_KP_0, KC_KP_2, KC_KP_3, KC_KP_1)
-#define KC_ENYE LALT(KC_KP_0, KC_KP_2, KC_KP_4, KC_KP_1)
+#define _______ KC_TRNS
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -25,48 +24,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |-----------------------------------------------------------|
     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]| Ret |
     * |-----------------------------------------------------------|
-    * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|   |urn|
+    * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  Ñ|  ;| ' |urn|
     * |-----------------------------------------------------------|
     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Fn|
     * |-----------------------------------------------------------|
     * |    |Alt|WinK  |      Space            |WinK  |Alt|        |
     * `-----------------------------------------------------------'
     *
-    *    INPUT LAYOUT
-    *    {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d}, \
-    *  	 {k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, ___}, \
-    *  	 {k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c TODO, k2d}, \
-    *  	 {k30, ___, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3d, k3c}, \
-    *  	 {___, k41, k42, ___, ___, ___, ___, k45, ___, ___, ___, k4a, k4b, ___}  \
-    *
     */
-[_BL] = KEYMAP_ISO_HHKB(
-  F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,_______, \
-  KC_LCTRL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,_____,KC_ENT,  \
-  KC_LSFT,_______, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT, MO(_FL), \
-  _______,KC_LGUI,KC_LALT,_______,_______,_______,_______,KC_SPC,_______,_______,_______, KC_RALT,KC_RGUI,_______),
 
-  /* Keymap _FL: (Function Layer) Second Layer
-   * ,-----------------------------------------------------------.
-   * |    |F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|PSR|  RESET|
-   * |-----------------------------------------------------------|
-   * |     |PLY|VUP|MUT|   |   |   |   |   |   |   |   |   |     |
-   * |-----------------------------------------------------------|
-   * |       |   |VDN|   |   |   |   |   |   |   |   |   |   |   |
-   * |-----------------------------------------------------------|
-   * |        |   |   |  Ç|   |   |  Ñ|   |   |   |   |      |   |
-   * |-----------------------------------------------------------|
-   * |    |   |      |                       |      |   |        |
-   * `-----------------------------------------------------------'
-   */
-[_FL] = KEYMAP_ISO_HHKB(
+    [_BL] = KEYMAP_ISO_HHKB( \
+    F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
+    KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,_______, \
+    KC_LCTRL, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L, KC_ENYE,KC_SCLN,KC_QUOT,KC_ENT,  \
+    KC_LSFT,_______, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT, MO(_FL), \
+    _______, KC_LGUI, KC_LALT,             KC_SPC,                 KC_RALT, KC_RGUI,_______, _______),
 
-  _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_PSCR, RESET,  \
-  _______,KC_MPLY,KC_VOLU,KC_MUTE,_______,_______,_______,_______,_______,_______,_______, _______,_______, _______, \
-  _______,_______,KC_VOLD,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
-  _______,_______,_______,KC_CEDL,_______,_______,KC_ENYE,_______,_______,_______,_______,_______, \
-  _______,_______,_______,                 _______,                       _______,_______,_______, _______),
+    /* Keymap _FL: (Function Layer) Second Layer
+     * ,-----------------------------------------------------------.
+     * |    |F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|PSR|  RESET|
+     * |-----------------------------------------------------------|
+     * |     |PLY|VUP|MUT|   |   |   |   |   |   |   |   |   |     |
+     * |-----------------------------------------------------------|
+     * |       |   |VDN|   |   |   |   |   |   |   |   |   |   |   |
+     * |-----------------------------------------------------------|
+     * |        |   |   |  Ç|   |   |   |   |   |   |   |      |   |
+     * |-----------------------------------------------------------|
+     * |    |   |      |                       |      |   |        |
+     * `-----------------------------------------------------------'
+     */
+    [_FL] = KEYMAP_ISO_HHKB(\
+
+    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_PSCR, RESET, \
+    _______,KC_MPLY,KC_VOLU,KC_MUTE,_______,_______,_______,_______,_______,_______,_______, _______,_______, _______, \
+    _______,_______,KC_VOLD,_______,_______,_______,_______,_______,_______,_______,_______,_______, _______, _______, \
+    _______,_______,_______,_______,KC_CEDL,_______,_______,_______,_______,_______,_______,_______,_______, _______, \
+    _______,_______,_______,                 _______,                       _______,_______,_______, _______)
 
 };
 
@@ -102,4 +95,16 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
       }
       break;
   }
-}
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    if (record->event.pressed) {
+        switch(id) {
+            case 0: // Ñ
+                return MACRO(D(LALT), T(KP_0), T(KP_2), T(KP_4), T(KP_1), U(LALT), END);
+            case 1: // Ç
+                return MACRO(D(LALT), T(KP_0), T(KP_2), T(KP_3), T(KP_1), U(LALT), END);
+        }
+    }
+    return MACRO_NONE;
+};
--- keyboards/satan/keymaps/isoHHKB/readme.md
+++ keyboards/satan/keymaps/isoHHKB/readme.md
@@ -1,1 +1,3 @@
 # HKKB-ISO Satan Layout by Stunny
+
+This layout combines the split Right Shift ISO layout with the popular HHKB layout. The Caps Lock key will be replaced in the default layout by the Control key, which is more convenient for some people.
