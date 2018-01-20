2063fdcb01a0b0819681a772f6827b77709f253d
Masayuki Sunahara <tamanishi@gmail.com>
2018-01-20 10:28:53 +0900

    tweak constants

--- keyboards/nyquist/keymaps/tamanishi/config.h
+++ keyboards/nyquist/keymaps/tamanishi/config.h
@@ -34,8 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 10
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
 
 #endif
