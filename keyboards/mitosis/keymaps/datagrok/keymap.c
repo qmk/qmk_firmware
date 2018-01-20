e89e579e26bd5a8d6c107d9d2cadbc90ae337306
Michael F. Lamb <mike@datagrok.org>
2018-01-20 08:57:38 -0800

    mitosis:datagrok: add num lock on Blue + QWERTY T

--- keyboards/mitosis/keymaps/datagrok/keymap.c
+++ keyboards/mitosis/keymaps/datagrok/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {XXXXXXX, _______, _______, _______, KC_DEL,  KC_ENT,  _______, _______, _______, XXXXXXX},
   },
   [_xN] = {
-    {_______, _______, _______, _______, _______, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_P0},
+    {_______, _______, _______, _______, KC_NLCK, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_P0},
     {_______, _______, _______, _______, _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS},
     {_______, _______, _______, _______, _______, KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_PEQL},
     {XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX},
