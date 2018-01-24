673620a5881fb5123863f9574cf59414a04b361d
LEdiodes <mkkerfoot@gmail.com>
2018-01-23 18:55:36 -0600

    Update keymap.c

--- keyboards/xd60/keymaps/default/keymap.c
+++ keyboards/xd60/keymaps/default/keymap.c
@@ -8,8 +8,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_GRV,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
-      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_DEL,      \
-      KC_LCTL, KC_LGUI, KC_LALT,                 TD(TD_ESC_CAPS),                          KC_RGUI, F(0),     KC_LEFT, KC_DOWN,  KC_RIGHT),
+      TD(TD_KC_RSFT_CAPS), KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  TD(TD_KC_RSFT_CAPS), KC_UP,    KC_DEL,      \
+      KC_LCTL, KC_LGUI, KC_LALT,                 TD(TD_SPC_ENT),                          KC_RGUI, F(0),     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
   // 1: Function Layer
   KEYMAP(
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,   KC_WH_U, KC_UP,   KC_WH_D, KC_BSPC,KC_HOME,KC_CALC,KC_NO,  KC_INS, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           KC_DEL,    \
       KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL, KC_END, KC_PGDN,KC_NO,  KC_NO,  KC_NO,   KC_HOME, KC_PGUP,  KC_NO,             KC_ENT,    \
       KC_LSFT, KC_NO,   KC_NO,   KC_APP,  BL_STEP,KC_NO,  KC_NO,  KC_VOLD,KC_VOLU,KC_MUTE, KC_END,  KC_PGDN,  KC_RSFT, KC_PGUP,  KC_INS,      \
-      KC_LCTL, KC_LGUI, KC_LALT,                 TD(TD_ESC_CAPS),                          KC_RGUI, F(0),     KC_HOME, KC_PGDOWN,KC_END),
+      KC_LCTL, KC_LGUI, KC_LALT,                 TD(TD_SPC_ENT),                          KC_RGUI, F(0),     KC_HOME, KC_PGDOWN,KC_END),
 
 };
 
@@ -61,7 +61,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Left Shift, twice for Caps Lock
   [TD_KC_LSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
   //Tap once for Right Shift, twice for Caps Lock
-  [TD_KC_RSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)  
+  [TD_KC_RSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
     
     
 // Other declarations would go here, separated by commas, if you have them
