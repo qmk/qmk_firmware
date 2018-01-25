6622b955514716a302a27ae0d816acc14576c412
MakotoKurauchi <pluis@me.com>
2018-01-25 23:16:47 +0900

    Disable LOCKING_SUPPORT_ENABLE

--- keyboards/helix/rev2/config.h
+++ keyboards/helix/rev2/config.h
@@ -61,9 +61,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEBOUNCING_DELAY 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+//#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+//#define LOCKING_RESYNC_ENABLE
 
 /* key combination for command */
 #define IS_COMMAND() ( \
