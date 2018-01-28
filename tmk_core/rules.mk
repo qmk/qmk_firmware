9bfaf66792310fae2d525633b280b354babca026
skullydazed <skullydazed@users.noreply.github.com>
2018-01-28 11:11:28 -0800

    Always copy the firmware file to the root qmk_firmware directory (#2314)
    * Always copy the firmware file to the root qmk_firmware directory
    
    * remove circular dependency

--- tmk_core/rules.mk
+++ tmk_core/rules.mk
@@ -216,9 +216,9 @@ MOVE_DEP = mv -f $(patsubst %.o,%.td,$@) $(patsubst %.o,%.d,$@)
 
 elf: $(BUILD_DIR)/$(TARGET).elf
 hex: $(BUILD_DIR)/$(TARGET).hex
-cphex: hex
-	$(SILENT) || printf "Copying $(TARGET).hex to qmk_firmware folder" | $(AWK_CMD)
-	$(COPY) $(BUILD_DIR)/$(TARGET).hex $(TARGET).hex && $(PRINT_OK)
+cpfirmware: $(FIRMWARE_FORMAT)
+	$(SILENT) || printf "Copying $(TARGET).$(FIRMWARE_FORMAT) to qmk_firmware folder" | $(AWK_CMD)
+	$(COPY) $(BUILD_DIR)/$(TARGET).$(FIRMWARE_FORMAT) $(TARGET).$(FIRMWARE_FORMAT) && $(PRINT_OK)
 eep: $(BUILD_DIR)/$(TARGET).eep
 lss: $(BUILD_DIR)/$(TARGET).lss
 sym: $(BUILD_DIR)/$(TARGET).sym
