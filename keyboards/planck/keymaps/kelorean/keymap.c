bbe47e4cb456a49a8e4169fa55d931d19fec12e8
kelorean <regor2526@gmail.com>
2018-02-02 16:12:50 -0500

    planck-swap up and down arrows. s65x-added dvorak

--- keyboards/planck/keymaps/kelorean/keymap.c
+++ keyboards/planck/keymaps/kelorean/keymap.c
@@ -12,7 +12,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ */ 
 
 #include "planck.h"
 #include "action_layer.h"
--- keyboards/planck/keymaps/kelorean/readme.md
+++ keyboards/planck/keymaps/kelorean/readme.md
@@ -1,2 +1,1 @@
 # The Default Planck Layout
-added
