5e8177719195f499b6b5ac089d6456e4dee7b51a
kelorean <regor2526@gmail.com>
2018-02-02 15:20:00 -0500

    Added Dvorak as first layer of default keymap

--- keyboards/planck/keymaps/kelorean/keymap.c
+++ keyboards/planck/keymaps/kelorean/keymap.c
@@ -12,11 +12,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-<<<<<<< HEAD
- */ 
-=======
  */
->>>>>>> Planck-swapped up/down arrows,s65x-add dvorak
 
 #include "planck.h"
 #include "action_layer.h"
--- keyboards/planck/keymaps/kelorean/readme.md
+++ keyboards/planck/keymaps/kelorean/readme.md
@@ -1,6 +1,2 @@
 # The Default Planck Layout
-<<<<<<< HEAD
-Swapped up and down arrows.
-=======
-
->>>>>>> Planck-swapped up/down arrows,s65x-add dvorak
+added
