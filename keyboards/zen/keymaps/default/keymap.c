05f08d999c2eca27194efba9f1c624d7d893e2ef
Legonut <legonut3@gmail.com>
2018-02-06 19:46:14 -0500

    Update keymap

--- keyboards/zen/keymaps/default/keymap.c
+++ keyboards/zen/keymaps/default/keymap.c
@@ -49,19 +49,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.             .-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * |      |RGBSAI|RGBVAI|RGBSAD|      |      |             |      | Pgup |  Up  | Pgdn |Insert| Home |
+ * |      |RGBSAI|RGBVAI|RGBSAD| RESET|  [   |             |   ]  | Pgup |  Up  | Pgdn |Insert| Home |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * |      |RGBHUD|RGBVAD|RGBHUI|      |      |             |      | Left | Down | Right|Delete| End  |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * | Shift|      |      |      |      |      |             | NKRO |      |      | Pause| Back | Next |
+ * |Shift |      |      |      |      |      |             | NKRO |      |      | Pause| Back | Next |
  * |------+------+------+------+------+------+------..-----+------+------+------+------+------+------|
  * | Ctrl | GUI  | Alt  |RGBMOD|      |      |      ||     |      |      |      | Mute | VOLUP| VOLDN|
  * `------------------------------------------------''-----------------------------------------------'
  */
 [_NAV] = KEYMAP( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,             KC_F8,    KC_F9,   KC_F10,   KC_F11,  KC_F12 ,  \
-  KC_TRNS, RGB_SAI, RGB_VAI, RGB_SAD, RESET,   KC_TRNS,                   KC_TRNS,           KC_PGUP,  KC_UP,   KC_PGDN, KC_TRNS, KC_TRNS , \
-  KC_TRNS, RGB_HUD, RGB_VAD, RGB_HUI, KC_TRNS, KC_TRNS,                   KC_TRNS,           KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS , \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,             KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12 ,  \
+  KC_TRNS, RGB_SAI, RGB_VAI, RGB_SAD, RESET,   KC_LBRC,                   KC_RBRC,           KC_PGUP,  KC_UP,   KC_PGDN, KC_INS,  KC_HOME , \
+  KC_TRNS, RGB_HUD, RGB_VAD, RGB_HUI, KC_TRNS, KC_TRNS,                   KC_TRNS,           KC_LEFT,  KC_DOWN, KC_RGHT, KC_DEL,  KC_END , \
   KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   MAGIC_TOGGLE_NKRO, KC_TRNS,  KC_TRNS, KC_MPLY, KC_MPRV, KC_MNXT , \
   KC_LCTL, KC_LGUI, KC_LALT, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS, KC_MUTE, KC_VOLU, KC_VOLD \
 ),
