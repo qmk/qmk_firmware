17b543e711f36ada0685cf5f3117853be1e3e797
Naut <jrbarnachea@gmail.com>
2018-02-06 19:45:44 -0800

    Backlight On/off support
    Migrated code from the BFake. Functionality only on BL_ON, BL_OFF,
    BL_TOGG.

--- keyboards/jc65p/config.h
+++ keyboards/jc65p/config.h
@@ -29,6 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 16
 
+#define NO_BACKLIGHT_CLOCK
+#define BACKLIGHT_LEVELS 3
 #define RGBLED_NUM 16
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 8
