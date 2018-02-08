b7c060f50a69697de99d12c9418df2fce3560588
Naut <jrbarnachea@gmail.com>
2018-02-07 23:54:19 -0800

    Renamed jc65 ps2avrgb directory
    Renamed directory for more clarity.

--- keyboards/jc65_32a/config.h
+++ keyboards/jc65_32a/config.h
@@ -15,8 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef JC65P_CONFIG_H
-#define JC65P_CONFIG_H
+#ifndef CONFIG_H
+#define CONFIG_H
 
 #include "config_common.h"
 
--- keyboards/jc65_32a/jc65_32a.c
+++ keyboards/jc65_32a/jc65_32a.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "jc65p.h"
+#include "jc65_32a.h"
 #ifdef BACKLIGHT_ENABLE
 #include "backlight.h"
 #endif
