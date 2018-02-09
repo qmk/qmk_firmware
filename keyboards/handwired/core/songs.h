ce9afd7b631f041fa782a09e2d2411b0f58bc2d1
Mitchell van Manen <ma.van.manen@live.nl>
2018-02-09 08:58:32 +0100

    remove files that were moved

--- keyboards/handwired/core/songs.h
+++
@@ -1,8 +0,0 @@
-#include "../../../quantum/audio/musical_notes.h"
-
-#define IMPERIAL_MARCH_HALF \
-  HD_NOTE(_A4), HD_NOTE(_A4), HD_NOTE(_A4), QD_NOTE(_F4), QD_NOTE(_C5), \
-  HD_NOTE(_A4), QD_NOTE(_F4), QD_NOTE(_C5), WD_NOTE(_A4)
-
-#define STARTUP \
-  Q__NOTE(_A3), Q__NOTE(_A4)
