f05591d70504abcb488383373020afff0b72de23
U-LANDSRAAD\drashna <drashna@live.com>
2018-02-11 08:15:00 -0800

    Disable unused action features

--- users/drashna/drashna.h
+++ users/drashna/drashna.h
@@ -147,6 +147,9 @@ enum {
 
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
 #ifdef FAUXCLICKY_ENABLE
 #define AUD_ON  FC_ON
 #define AUD_OFF FC_OFF
