7a9abac8600baf03e8bd470460d73531f67b5f89
U-CLSCORPORATE\cbaetzel <cbaetzel@L001513.ContractLandStaff.local>
2018-02-12 13:44:28 -0600

    steno keys added to plover layout

--- keyboards/ergodox_infinity/keymaps/halfkeyboard/keymap.c
+++ keyboards/ergodox_infinity/keymaps/halfkeyboard/keymap.c
@@ -2,6 +2,7 @@
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
+#include "keymap_steno.h"
 #ifndef MIDI_ENABLE
 #error "Midi is not enabled"
 #endif
@@ -306,22 +307,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [PLVR] = LAYOUT_ergodox(  // layout: layer 4: Steno for Plover
         // left hand
         KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-        KC_NO,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
-        KC_NO,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
-        KC_NO,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_NO,
+        STN_N1, STN_N2, STN_N3, STN_N4, STN_N5, STN_N6, KC_NO,
+        STN_FN, STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1,
+        KC_NO,  STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,   KC_NO,
         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                                      KC_FN4, KC_NO,
+                                      KC_TRNS, KC_TRNS,
                                            KC_TRNS,
-                                 KC_C,   KC_V,   KC_NO,
+                                 STN_A,   STN_O,   KC_NO,
         // right hand
-             KC_TRNS,  KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_TRNS,
-             KC_TRNS,   KC_6,    KC_7,    KC_8,   KC_9,    KC_0,   KC_TRNS,
-                        KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
-               KC_NO,   KC_H,    KC_J,    KC_K,   KC_L, KC_SCLN,   KC_QUOT,
+             KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+             KC_TRNS, STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,   STN_NC,
+                     STN_ST3,  STN_FR,  STN_PR,  STN_LR,  STN_TR,   STN_DR,
+               KC_NO,STN_ST4,  STN_RR,  STN_BR,  STN_GR,  STN_SR,   STN_ZR,
                               KC_TRNS, KC_TRNS,  KC_NO,   KC_NO,     KC_NO,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
-        KC_NO,  KC_N,   KC_M
+        KC_NO,  STN_E,   STN_U
 ),
 /* Keymap 8: shortcuts
  *
@@ -506,7 +507,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-
+  steno_set_mode(STENO_MODE_GEMINI); // or STENO_MODE_BOLT
 };
 
 
