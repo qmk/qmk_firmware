10bc0a725815269fb3acd69c6f3e169a91752a5b
Oryhara <turomar@gmail.com>
2018-02-14 09:59:06 -0600

    visualizer decided to have a conflict again.  fixed.

--- keyboards/ergodox_infinity/keymaps/halfkeyboard/visualizer.c
+++ keyboards/ergodox_infinity/keymaps/halfkeyboard/visualizer.c
@@ -170,20 +170,12 @@ static void get_visualizer_layer_and_color(visualizer_state_t* state) {
     /* if (state->status.leds & (1u << USB_LED_CAPS_LOCK)) {
         saturation = 255;
     } */
-<<<<<<< HEAD
-<<<<<<< HEAD
     if (state->status.layer & 0x400) {
         state->target_lcd_color = LCD_COLOR(OCEAN, saturation, 0xFF);
         state->layer_text = "STENOGRAPHY";
         start_keyframe_animation(&KITT_Scanner_animation);
     }
     else if (state->status.layer & 0x200) {
-=======
-    if (state->status.layer & 0x200) {
->>>>>>> 43592fa51c7f592735c0d54f6d67a69d018d5338
-=======
-    if (state->status.layer & 0x200) {
->>>>>>> b89e318d35b295465a14679f27d71077a51daa3b
         state->target_lcd_color = LCD_COLOR(GREEN, saturation, 0xFF);
         state->layer_text = "MOUSE";
     }
@@ -195,14 +187,6 @@ static void get_visualizer_layer_and_color(visualizer_state_t* state) {
         state->target_lcd_color = LCD_COLOR(VIOLET, saturation, 0xFF);
         state->layer_text = "Plover";
         start_keyframe_animation(&KITT_Scanner_animation);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 43592fa51c7f592735c0d54f6d67a69d018d5338
-=======
-
->>>>>>> b89e318d35b295465a14679f27d71077a51daa3b
     }
     else if (state->status.layer & 0x40) {
         state->target_lcd_color = LCD_COLOR(RASPBERRY, saturation, 0xFF);
