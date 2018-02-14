573834cf81a09c0ff75b9f5099d8c67033674293
William <wrwilson@smu.edu>
2018-02-14 10:54:09 -0600

    Fixed KEYMAP vs KC_KEYMAP

--- keyboards/chimera_ortho/chimera_ortho.h
+++ keyboards/chimera_ortho/chimera_ortho.h
@@ -49,7 +49,7 @@
 // This a shortcut to help you visually see your layout.
 // The first section contains all of the arguements
 // The second converts the arguments into a two-dimensional array
-#define KEYMAP( \
+#define KC_KEYMAP( \
   k00, k01, k02, k03, k04, k05, k06,      k07, k08, k09, k10, k11, k12, k13, \
   k14, k15, k16, k17, k18, k19, k20,      k21, k22, k23, k24, k25, k26, k27, \
   k28, k29, k31, k32, k33, k34, k35,      k36, k37, k38, k41, k42, k43, k44,\
@@ -63,7 +63,7 @@
 	{ KC_NO,      KC_##k28, KC_##k14, KC_##k00, KC_##k45,      KC_##k48, KC_##k13, KC_##k27, KC_##k44, KC_NO }, \
 }
 
-#define KC_KEYMAP( \
+#define KEYMAP( \
   k00, k01, k02, k03, k04, k05, k06,      k07, k08, k09, k10, k11, k12, k13, \
   k14, k15, k16, k17, k18, k19, k20,      k21, k22, k23, k24, k25, k26, k27, \
   k28, k29, k31, k32, k33, k34, k35,      k36, k37, k38, k41, k42, k43, k44,\
