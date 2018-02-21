4fbd9f809cca132771929cc9ebbb7bc567f75723
Graham Heath <heathg@google.com>
2018-02-20 18:06:38 -0600

    Fixes missing key

--- keyboards/meira/keymaps/grahampheath/keymap.c
+++ keyboards/meira/keymaps/grahampheath/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_X0,   KC_A,     KC_S,    KC_D,    KC_F,   KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_X1, \
-    KC_EMOJ, MOD_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_X3,    KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    KC_EMOJ, KC_LCTL,  KC_LALT, KC_LGUI, LOWER,  KC_X3,    KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   /* Colemak
