2d74c04a539637605d52897e098e59c8b4649a30
mechmerlin <mechmerlin@gmail.com>
2018-02-22 21:32:38 -0800

    Enable backlight LEDs, and change pin for RGB

--- keyboards/amj96/config.h
+++ keyboards/amj96/config.h
@@ -189,6 +189,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 16
-#define RGB_DI_PIN D6
+#define RGB_DI_PIN D3
 
 #endif
