0f6c3b97761354ca78254bbd0422320ed2ccab8b
skullY <skullydazed@gmail.com>
2018-02-25 17:41:43 -0800

    remove duplicate #define

--- keyboards/clueboard/66/keymaps/skully/keymap.c
+++ keyboards/clueboard/66/keymaps/skully/keymap.c
@@ -4,9 +4,6 @@ enum custom_keycodes {
    M_SHRUG = SAFE_RANGE
 };
 
-// Helpful defines
-#define _______ KC_TRNS
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
