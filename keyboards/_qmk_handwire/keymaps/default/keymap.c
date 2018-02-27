da32068f489e94eff7ebf57b83c6a55d405ca81f
Jack Humbert <jack.humb@gmail.com>
2018-02-26 21:29:07 -0500

    update to qwerty

--- keyboards/_qmk_handwire/keymaps/default/keymap.c
+++ keyboards/_qmk_handwire/keymaps/default/keymap.c
@@ -34,20 +34,35 @@ enum custom_keycodes {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = KEYMAP(
-      RESET,  KC_1,    KC_2,    KC_3,    KC_4,    KC_0,    KC_DEL,
-      KC_TAB,  DEBUG,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME,
       KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_END,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_N,
-      KC_RCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_LGUI,
-                                          KC_SPC, MO(2), MO(1),
+      KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, MO(1),
+                                          KC_SPC, MO(1), RESET,
 
-      KC_INS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    RESET,
-      KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    DEBUG,    KC_DEL,
+      KC_INS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+      KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
       KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_QUOT,
                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                         KC_RCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-      MO(2), MO(1), KC_SPC
+      RESET, MO(2), KC_SPC
     )
+    // [0] = KEYMAP(
+    //   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,
+    //   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_HOME,
+    //   KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_END,
+    //   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+    //   KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, MO(1),
+    //                                       KC_SPC, MO(1), RESET,
+
+    //   KC_INS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    //   KC_PGUP, KC_J,    KC_L,    KC_U,    KC_Y,    KC_COLN, KC_DEL,
+    //   KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    //            KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    //                     KC_RGUI, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    //   RESET, MO(2), KC_SPC
+    // )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
