de8ba8f22260dab2c46a09be2d08d6b4ab8d2bfa
Louis Orleans <louis@orleans.io>
2018-02-28 00:39:49 -0800

    💄 add Workman layer sound

--- keyboards/preonic/keymaps/dudeofawesome/config.h
+++ keyboards/preonic/keymaps/dudeofawesome/config.h
@@ -8,6 +8,7 @@
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(PREONIC_SOUND), \
                                   SONG(COLEMAK_SOUND), \
                                   SONG(DVORAK_SOUND) \
                                 }
