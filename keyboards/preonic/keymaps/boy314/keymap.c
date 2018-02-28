30b9a8546725b0789643620a511f88364d834da1
Willie Yee <boy.314.yee.willie@gmail.com>
2018-02-28 18:47:59 -0500

    updated osu layout

--- keyboards/preonic/keymaps/boy314/keymap.c
+++ keyboards/preonic/keymaps/boy314/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |XXXXXX|XXXXXX|NUM 7 |NUM 8 |NUM 9 |XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX| Bksp |
+ * | Esc  |  \   |XXXXXX|NUM 7 |NUM 8 |NUM 9 |XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX| Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |XXXXXX|XXXXXX|XXXXXX|NUM 4 |XXXXXX|NUM 6 |XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|XXXXXX|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_OSU] = {
   {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
-  {KC_ESC , XXXXXXX, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC},
+  {KC_ESC,KC_BSLASH, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC},
   {XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_4, XXXXXXX, KC_KP_6,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX},
   {_______,   KC_UP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX},
   {KC_LEFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,KC_KP_DOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,TO(_QWERTY)}
