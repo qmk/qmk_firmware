52bba9c95690c8aca3e098f58bacf9c8fdf4af6e
Louis Orleans <louis@orleans.io>
2018-02-28 00:26:33 -0800

    💄 use Planck startup sound

--- keyboards/preonic/keymaps/dudeofawesome/config.h
+++ keyboards/preonic/keymaps/dudeofawesome/config.h
@@ -4,7 +4,7 @@
 #include "config_common.h"
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    #define STARTUP_SONG SONG(PLANCK_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
