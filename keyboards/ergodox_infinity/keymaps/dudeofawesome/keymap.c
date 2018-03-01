ab185d673431539996d4c9cbe2ce9f1b5140a825
Louis Orleans <louis@orleans.io>
2018-03-01 13:37:38 -0800

    ✨ use more standard numpad layout

--- keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
+++ keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
@@ -25,7 +25,6 @@ enum custom_keycodes {
   RAISE,
   GAME,
   MOUSE,
-  NUM,
   EPRM,
   VRSN,
   RGB_SLD,
@@ -55,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_ergodox(
   // left hand
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5, KC_ESC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T, KC_LBRACKET,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,
-  KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B, KC_FN1,
-  NUM,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,
+  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,  KC_5, KC_ESC,
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T, KC_LBRACKET,
+  KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,
+  KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B, KC_FN1,
+  TT(_NUM), KC_LCTL, KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
                                                             KC_LALT,
@@ -72,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_FN1,      KC_N, KC_M,  KC_COMM, KC_DOT,  LT(_MOUSE, KC_SLSH), RSFT_T(KC_ENT),
                      RAISE, KC_LEFT, KC_DOWN, KC_UP,               KC_RIGHT,
 
-  NUM,     TG(_MOUSE),
+  TT(_NUM), TG(_MOUSE),
   KC_LALT,
-  KC_LGUI, RAISE,      KC_SPACE
+  KC_LGUI,  RAISE,      KC_SPACE
 ),
 
 /* Keymap 0: Basic Workman layer
@@ -100,11 +99,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_WORKMAN] = LAYOUT_ergodox(
   // left hand
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
-  KC_TAB,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B, KC_LBRACKET,
-  KC_ESC,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,
-  KC_LSPO, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V, KC_FN1,
-  NUM,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,
+  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
+  KC_TAB,   KC_Q,    KC_D,    KC_R,    KC_W,    KC_B, KC_LBRACKET,
+  KC_ESC,   KC_A,    KC_S,    KC_H,    KC_T,    KC_G,
+  KC_LSPO,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V, KC_FN1,
+  TT(_NUM), KC_LCTL, KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
                                                             KC_LALT,
@@ -117,9 +116,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_FN1,      KC_K, KC_L,  KC_COMM, KC_DOT,  LT(_MOUSE, KC_SLSH), RSFT_T(KC_ENT),
                      RAISE, KC_LEFT, KC_DOWN, KC_UP,               KC_RIGHT,
 
-  NUM,     TG(_MOUSE),
+  TT(_NUM), TG(_MOUSE),
   KC_LALT,
-  KC_LGUI, RAISE,      KC_SPACE
+  KC_LGUI,  RAISE,      KC_SPACE
 ),
 
 /* Keymap 0: Basic Dvorak layer
@@ -145,11 +144,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_DVORAK] = LAYOUT_ergodox(
   // left hand
-  KC_GRV,  KC_1,      KC_2,    KC_3,    KC_4,  KC_5, KC_ESC,
-  KC_TAB,  KC_QUOT,   KC_COMM, KC_DOT,  KC_P,  KC_Y, KC_LBRACKET,
-  KC_ESC,  KC_A,      KC_O,    KC_E,    KC_U,  KC_I,
-  KC_LSPO, KC_SCOLON, KC_Q,    KC_J,    KC_K,  KC_X, KC_FN1,
-  NUM,     KC_LCTL,   KC_LALT, KC_LGUI, LOWER,
+  KC_GRV,   KC_1,      KC_2,    KC_3,    KC_4,  KC_5, KC_ESC,
+  KC_TAB,   KC_QUOT,   KC_COMM, KC_DOT,  KC_P,  KC_Y, KC_LBRACKET,
+  KC_ESC,   KC_A,      KC_O,    KC_E,    KC_U,  KC_I,
+  KC_LSPO,  KC_SCOLON, KC_Q,    KC_J,    KC_K,  KC_X, KC_FN1,
+  TT(_NUM), KC_LCTL,   KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
                                                             KC_LALT,
@@ -162,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_FN1,      KC_B, KC_M,  KC_W,    KC_V,    KC_Z,  RSFT_T(KC_ENT),
                      RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
 
-  TG(_NUM), TG(_MOUSE),
+  TT(_NUM), TG(_MOUSE),
   KC_LALT,
   KC_LGUI,  RAISE,      KC_SPACE
 ),
@@ -190,11 +189,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_COLEMAK] = LAYOUT_ergodox(
   // left hand
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, KC_LBRACKET,
-  KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,
-  KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_FN1,
-  NUM,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,
+  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
+  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, KC_LBRACKET,
+  KC_ESC,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,
+  KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_FN1,
+  TT(_NUM), KC_LCTL, KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
                                                             KC_LALT,
@@ -207,9 +206,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_FN1,      KC_K, KC_M,  KC_COMM, KC_DOT,  LT(_MOUSE, KC_SLSH), RSFT_T(KC_ENT),
                      RAISE, KC_LEFT, KC_DOWN, KC_UP,               KC_RIGHT,
 
-  NUM,     TG(_MOUSE),
+  TT(_NUM), TG(_MOUSE),
   KC_LALT,
-  KC_LGUI, RAISE,      KC_SPACE
+  KC_LGUI,  RAISE,      KC_SPACE
 ),
 
 /* Lower
@@ -433,24 +432,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______,
                     _______, _______, _______, _______, _______,
 
-  TG(_MOUSE), TG(_NUM),
+  TG(_MOUSE), TT(_NUM),
   _______,
   _______,    _______,  _______
 ),
 
-
 /* Keymap 2: NUMPAD
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           | F12  |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |        |   ✗  |   ✗  |   ✗  |   ✗  |   ✗  |   ✗  |           |   ✗  |   ✗  |   ✗  |   /  |   *  |   -  |        |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |        |  !   |  @   |  {   |  }   |  |   |      |           |      |      |  7   |  8   |  9   |  *   |        |
+ * |        |   ✗  | Home |  Up  |  End | PgUp |      |           |      |   ✗  |   7  |   8  |   9  |   +  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  #   |  $   |  (   |  )   |  `   |------|           |------|      |  4   |  5   |  6   |  +   |        |
+ * |        |   ✗  | Left | Down | Right| PgDn |------|           |------|   ✗  |   4  |   5  |   6  |   +  |    ✗   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  %   |  ^   |  [   |  ]   |  ~   |      |           |      |      |  1   |  2   |  3   |  \   |        |
+ * |        |   /  |   *  |   -  |   +  | Enter|      |           |      |   ✗  |   1  |   2  |   3  | Enter|  Enter |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |  .   |  0   |  0   |  =   |      |
+ *   |      |      |      |      |   ✗  |                                       |   0  |   0  |   .  | Enter|   =  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                      ,-------------.           ,-------------.
  *                                      |      |      |           |      |      |
@@ -462,22 +460,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NUM] = LAYOUT_ergodox(
   // left hand
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
-  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,
-  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
-  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______,
-  _______, _______, _______, _______, _______,
+  _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  _______, KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_NO,
+  _______, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
+  _______, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_PENT, KC_NO,
+  _______, _______, _______, _______, KC_NO,
 
                                                _______, _______,
                                                         _______,
                                       _______, _______, _______,
 
   // right hand
-  KC_F12,  KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,         KC_F11,
-  _______, _______, KC_KP_7,   KC_KP_8, KC_KP_9, KC_KP_ASTERISK, _______,
-           _______, KC_KP_4,   KC_KP_5, KC_KP_6, KC_KP_PLUS,     _______,
-  _______, _______, KC_KP_1,   KC_KP_2, KC_KP_3, KC_KP_SLASH,    _______,
-                    KC_KP_DOT, KC_KP_0, KC_KP_0, KC_KP_EQUAL,    _______,
+  KC_NO, KC_NO, KC_NO, KC_PSLS, KC_PAST, KC_PMNS, _______,
+  KC_NO, KC_NO, KC_P7, KC_P8,   KC_P9,   KC_PPLS, _______,
+         KC_NO, KC_P4, KC_P5,   KC_P6,   KC_PPLS, KC_NO,
+  KC_NO, KC_NO, KC_P1, KC_P2,   KC_P3,   KC_PENT, KC_PENT,
+                KC_P0, KC_P0,   KC_PDOT, KC_PENT, KC_PEQL,
 
   _______, _______,
   _______,
@@ -557,13 +555,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_off(_ADJUST);
           layer_on(_LOWER);
         }
-      }
-      return false;
-    case NUM:
-      if (record->event.pressed) {
-        layer_on(_NUM);
-      } else {
-        layer_off(_NUM);
       }
       return false;
     case EPRM:
