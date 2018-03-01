02bed681f7ca0f2821df7d61f10ec9275261fb13
Louis Orleans <louis@orleans.io>
2018-03-01 14:09:18 -0800

    🔥 disable space cadet

--- keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
+++ keyboards/ergodox_infinity/keymaps/dudeofawesome/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,  KC_5, KC_ESC,
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T, KC_LBRACKET,
   KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,  KC_G,
-  KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B, KC_FN1,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B, KC_FN1,
   TT(_NUM), KC_LCTL, KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
   KC_TAB,   KC_Q,    KC_D,    KC_R,    KC_W,    KC_B, KC_LBRACKET,
   KC_ESC,   KC_A,    KC_S,    KC_H,    KC_T,    KC_G,
-  KC_LSPO,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V, KC_FN1,
+  KC_LSFT,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V, KC_FN1,
   TT(_NUM), KC_LCTL, KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,      KC_2,    KC_3,    KC_4,  KC_5, KC_ESC,
   KC_TAB,   KC_QUOT,   KC_COMM, KC_DOT,  KC_P,  KC_Y, KC_LBRACKET,
   KC_ESC,   KC_A,      KC_O,    KC_E,    KC_U,  KC_I,
-  KC_LSPO,  KC_SCOLON, KC_Q,    KC_J,    KC_K,  KC_X, KC_FN1,
+  KC_LSFT,  KC_SCOLON, KC_Q,    KC_J,    KC_K,  KC_X, KC_FN1,
   TT(_NUM), KC_LCTL,   KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
@@ -192,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
   KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, KC_LBRACKET,
   KC_ESC,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,
-  KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_FN1,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_FN1,
   TT(_NUM), KC_LCTL, KC_LALT, KC_LGUI, LOWER,
 
                                                   KC_MPLY,  TG(_MOUSE),
