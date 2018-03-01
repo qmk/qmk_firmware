728423a5d5e119758abf42f4eaca8a915c759acd
Louis Orleans <louis@orleans.io>
2018-02-28 18:39:08 -0800

    ✨ reworking numpad layer to match a real numpad

--- keyboards/preonic/keymaps/dudeofawesome/keymap.c
+++ keyboards/preonic/keymaps/dudeofawesome/keymap.c
@@ -190,23 +190,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Numpad
  * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |      |   ✗  |   ✗  |   ✗  |   ✗  |   ✗  |   ✗  |   ✗  |   /  |   *  |   -  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   !  |   @  |   {  |   }  |   |  |      |   7  |   8  |   9  |   *  |      |
+ * |      |   ✗  | Home |  Up  |  End | PgUp |   ✗  |   7  |   8  |   9  |   +  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   #  |   $  |   (  |   )  |   `  |      |   4  |   5  |   6  |   +  |      |
+ * |      |   ✗  | Left | Down | Right| PgDn |   ✗  |   4  |   5  |   6  |   +  |   ✗  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   %  |   ^  |   [  |   ]  |   ~  |      |   1  |   2  |   3  |   /  |      |
+ * |      |   ✗  |   ✗  |   ✗  |   ✗  |   ✗  |   ✗  |   1  |   2  |   3  |   ⏎  |   ⏎  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |   .  |   0  |   0  |   =  |      |
+ * |      |      |      |      |   ✗  |             |   0  |   0  |   .  |   ⏎  |   =  |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] = {
-  {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,         KC_F12},
-  {_______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______, KC_KP_7,   KC_KP_8, KC_KP_9, KC_KP_ASTERISK, _______},
-  {_______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  _______, KC_KP_4,   KC_KP_5, KC_KP_6, KC_KP_PLUS,     _______},
-  {_______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______, KC_KP_1,   KC_KP_2, KC_KP_3, KC_KP_SLASH,    _______},
-  {_______, _______, _______, _______, _______, _______, _______, KC_KP_DOT, KC_KP_0, KC_KP_0, KC_KP_EQUAL,    _______}
+  {_______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, _______},
+  {_______, KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_NO,   KC_KP_7,    KC_KP_8,     KC_KP_9,        KC_KP_PLUS,  _______},
+  {_______, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,   KC_KP_4,    KC_KP_5,     KC_KP_6,        KC_KP_PLUS,  KC_NO},
+  {_______, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_KP_1,    KC_KP_2,     KC_KP_3,        KC_KP_ENTER, KC_KP_ENTER},
+  {_______, _______, _______, _______, KC_NO,   _______, _______, KC_KP_0,    KC_KP_0,     KC_KP_DOT,      KC_KP_ENTER, KC_KP_EQUAL}
 }
 
 
