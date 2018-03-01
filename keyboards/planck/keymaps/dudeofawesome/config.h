b3e522fb1f6b7c0001149fd0486ad0e469688ebb
Louis Orleans <louis@orleans.io>
2018-03-01 11:16:31 -0800

    🔥 remove workman sound def

--- keyboards/planck/keymaps/dudeofawesome/config.h
+++ keyboards/planck/keymaps/dudeofawesome/config.h
@@ -4,8 +4,6 @@
 #include "../../config.h"
 #include "dudeofawesome.h"
 
-#define WORKMAN_SOUND COLEMAK_SOUND
-
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PLANCK_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
