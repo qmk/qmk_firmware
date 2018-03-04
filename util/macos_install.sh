4672d4e10ff44a46b274dc4b0c67758d8267e79c
skullY <skullydazed@gmail.com>
2018-03-04 14:09:53 -0800

    flesh out the macos install script

--- util/macos_install.sh
+++ util/macos_install.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! brew --version 2>&1 > /dev/null; then
+if brew --version 2>&1 > /dev/null; then
 	echo "Error! Homebrew not installed or broken!"
-	exit 1
+	echo -n "Would you like to install homebrew now? [y/n] "
+	while read ANSWER; do
+		case $ANSWER in
+			y|Y)
+				/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+				break
+			;;
+			n|N)
+				exit 1
+			;;
+			*)
+				echo -n "Would you like to install homebrew now? [y/n] "
+			;;
+                esac
+	done
 fi
 
 brew tap osx-cross/avr
