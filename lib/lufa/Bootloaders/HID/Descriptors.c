efb5a9b2c199eb485285674288879215c0d8d489
Scott Wilson <scott.t.wilson@gmail.com>
2018-03-05 22:49:41 -0500

    Set USB vid/pid so that it can be found by teensyloader

--- lib/lufa/Bootloaders/HID/Descriptors.c
+++ lib/lufa/Bootloaders/HID/Descriptors.c
@@ -73,8 +73,8 @@ const USB_Descriptor_Device_t DeviceDescriptor =
 
 	.Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
 
-	.VendorID               = 0x03EB,
-	.ProductID              = 0x2067,
+	.VendorID               = 0x16C0,
+	.ProductID              = 0x0478,
 	.ReleaseNumber          = VERSION_BCD(0,0,1),
 
 	.ManufacturerStrIndex   = NO_DESCRIPTOR,
