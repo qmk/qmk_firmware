082f58f11d727c56b23ff2f437686eb24741ee95
U-LANDSRAAD\drashna <drashna@live.com>
2018-03-10 08:05:52 -0800

    Add different layouts

--- keyboards/contra/rules.mk
+++ keyboards/contra/rules.mk
@@ -57,4 +57,4 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 
-LAYOUTS = ortho_4x12 
+LAYOUTS = ortho_4x12 contra_grid contra_2USPC
