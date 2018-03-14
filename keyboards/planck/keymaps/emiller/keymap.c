0c49998fea2f332c47776610043167e4ef705b3e
Edmund Miller <eman0088@gmail.com>
2018-03-14 00:34:33 -0500

    Update QWERTY

--- keyboards/planck/keymaps/emiller/keymap.c
+++ keyboards/planck/keymaps/emiller/keymap.c
@@ -54,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI | Alt  | VIM  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | GUI  | Alt  | VIM  | Lower|    Space    |Raise | NUM  | Vol- | Vol+ | Del  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, VIM , LOWER,   KC_SPC,  KC_SPC,  RAISE,  BACKLIT, BL_DEC, BL_INC,  KC_DEL}
+  {KC_LCTL, KC_LGUI, KC_LALT, TT(VIM), LOWER,   KC_SPC,  KC_SPC,  RAISE,  TT(NUM), KC_VOLD, KC_VOLU,  KC_DEL}
 },
 
 /* Colemak
