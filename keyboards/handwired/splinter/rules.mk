cf7faeb26708ec9a5f090b8019b33c2ef432ee2a
Gilbert Consellado <gilbertconsellado@gmail.com>
2018-03-20 15:51:28 +0800

    Got no love from i2c, serial to the rescue

--- keyboards/handwired/splinter/rules.mk
+++ keyboards/handwired/splinter/rules.mk
@@ -1,6 +1,8 @@
 SRC += matrix.c \
 	   i2c.c \
-	   split_util.c
+	   split_util.c \
+     serial.c \
+     ssd1306.c
 
 # MCU name
 #MCU = at90usb1287
--- keyboards/handwired/splinter/split_util.h
+++ keyboards/handwired/splinter/split_util.h
@@ -1,10 +1,10 @@
 #ifndef SPLIT_KEYBOARD_UTIL_H
 #define SPLIT_KEYBOARD_UTIL_H
 
-#include "eeconfig.h"
 #include <stdbool.h>
+#include "eeconfig.h"
 
-#define SLAVE_I2C_ADDRESS 0x32
+#define SLAVE_I2C_ADDRESS           0x32
 
 extern volatile bool isLeftHand;
 
@@ -14,5 +14,7 @@ void matrix_slave_scan(void);
 void split_keyboard_setup(void);
 bool has_usb(void);
 void keyboard_slave_loop(void);
+
+void matrix_master_OLED_init (void);
 
 #endif
