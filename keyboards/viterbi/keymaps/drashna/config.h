607d807d24bdceb218a64783cee77331ab646cca
U-LANDSRAAD\drashna <drashna@live.com>
2018-03-24 10:50:55 -0700

    Convert Viterbi to LAYOUT macro

--- keyboards/viterbi/keymaps/drashna/config.h
+++ keyboards/viterbi/keymaps/drashna/config.h
@@ -59,7 +59,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_I2C
 #define NO_MUSIC_MODE
-#define half_KEYMAP( \
+#define LAYOUT_ortho_5x7( \
     L00, L01, L02, L03, L04, L05, L06, \
     L10, L11, L12, L13, L14, L15, L16, \
     L20, L21, L22, L23, L24, L25, L26, \
