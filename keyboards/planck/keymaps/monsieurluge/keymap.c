78f39c94cb5bc807f272bdef75a63f4e786e13c8
monsieurluge <monsieurluge@gmail.com>
2018-03-26 18:45:42 +0200

    restore the left shift key

--- keyboards/planck/keymaps/monsieurluge/keymap.c
+++ keyboards/planck/keymaps/monsieurluge/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    * │ tab │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │  ;  │ ent │
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   * │ bsp │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │ up  │ del │
+   * │shift│  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │ up  │ del │
    * ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
    * │ctrl │ os  │media│ alt │lower│   raise   │space│ alt │left │down │right│
    * └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = {
     {KC_ESC,  KC_Q,    KC_W,   KC_E,    KC_R,  KC_T,  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC},
     {KC_TAB,  KC_A,    KC_S,   KC_D,    KC_F,  KC_G,  KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT},
-    {KC_BSPC, KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_DEL },
+    {KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_DEL },
     {KC_LCTL, KC_LGUI, MO(_MEDIA), KC_LALT, LOWER, RAISE, RAISE, SP_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT}
   },
 
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    * │ tab │  T  │  N  │  I  │  E  │  ,  │  .  │  A  │  S  │  R  │  O  │ ent │
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   * │ bsp │  ;  │  W  │  J  │  F  │  X  │  Y  │  Q  │  K  │  Z  │ up  │ del │
+   * │shift│  ;  │  W  │  J  │  F  │  X  │  Y  │  Q  │  K  │  Z  │ up  │ del │
    * ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
    * │ctrl │ os  │media│ alt │lower│   raise   │space│ alt │left │down │right│
    * └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CUSTOM] = {
     {KC_ESC,  KC_G,    KC_M,   KC_D,    KC_L,  KC_V,    KC_H,   KC_U,   KC_C,    KC_P,    KC_B,    KC_BSPC},
     {KC_TAB,  KC_T,    KC_N,   KC_I,    KC_E,  KC_COMM, KC_DOT, KC_A,   KC_S,    KC_R,    KC_O,    KC_ENT},
-    {KC_BSPC, KC_SCLN, KC_W,   KC_J,    KC_F,  KC_X,    KC_Y,   KC_Q,   KC_K,    KC_Z,    KC_UP,   KC_DEL },
+    {KC_LSFT, KC_SCLN, KC_W,   KC_J,    KC_F,  KC_X,    KC_Y,   KC_Q,   KC_K,    KC_Z,    KC_UP,   KC_DEL },
     {KC_LCTL, KC_LGUI, MO(_MEDIA), KC_LALT, LOWER, RAISE,   RAISE,  SP_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT}
   },
 
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    * │ tab │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │  ;  │ ent │
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   * │ bsp │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │ up  │ del │
+   * │shift│  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │ up  │ del │
    * ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
    * │ctrl │ os  │ alt │space│lower│   raise   │space│media│left │down │right│
    * └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAMING] = {
     {KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,  KC_T,  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC},
     {KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,  KC_G,  KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT },
-    {KC_BSPC, KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_DEL },
+    {KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_DEL },
     {KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, LOWER, RAISE, RAISE, SP_SPC, MO(_MEDIA),  KC_LEFT, KC_DOWN, KC_RGHT}
   },
 
