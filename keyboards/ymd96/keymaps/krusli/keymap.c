977040a63fc81e93cbc754897f76af783f852504
harshit goel <harshitgoel96@yahoo.com>
2018-03-26 22:28:12 +0530

    enable led and backlight on ymd96, caps lock key is an issue

--- keyboards/ymd96/keymaps/krusli/keymap.c
+++ keyboards/ymd96/keymaps/krusli/keymap.c
@@ -1,25 +1,28 @@
-/*
-Base Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
-Modified 2017 Andrew Novak <ndrw.nvk@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public LicensezZZ
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "ymd96.h"
 #include "action_layer.h"
-#include "rgblight.h"
 
+/*
+#define _QWERTY 0
+#define _LOWER  1
+#define _RAISE  2*/
+/*
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+};*/
+
+void matrix_scan_user(void) {
+  // runs at every matrix scan.
+}
+
+enum {
+  TD_H_E = 0
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_H_E] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END)
+};
 #define ______ KC_TRNS
 #define _DEFLT 0
 #define _RAISE 1
@@ -37,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
     [_DEFLT] = KEYMAP(
 			KC_ESC,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,       KC_F12,      KC_PSCR, KC_HOME,    KC_END,     KC_INSERT,   KC_DELETE,      KC_PGUP, \
-			KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,      KC_EQL,          KC_BSPC,         KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_PGDN, \
-			KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,      KC_RBRC,         KC_BSLS,         KC_P7,      KC_P8,       KC_P9,          KC_PMNS, \
+			KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,      KC_EQL,          KC_BSPC,         KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_PMNS, \
+			KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,      KC_RBRC,         KC_BSLS,         KC_P7,      KC_P8,       KC_P9,          KC_NO, \
 			KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,                       KC_ENT,          KC_P4,      KC_P5,       KC_P6,          KC_PPLS, \
 			KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,                               KC_RSFT,         KC_P1,      KC_P2,       KC_P3,          KC_PENT, \
-			KC_LCTL, KC_LGUI,KC_LALT,                              KC_SPC,                        MO(_RAISE),      KC_RGUI,     KC_LEFT, KC_DOWN,    KC_UP,      KC_RGHT,     KC_P0,          KC_PDOT  \
+			KC_LCTL, KC_LGUI,KC_LALT,                              KC_SPC,                   KC_RGUI     ,          KC_LEFT, KC_DOWN,    KC_UP,      KC_RGHT, MO(_RAISE),     KC_P0,          KC_PDOT  \
 	),
 	/* Layer 1, raise layer
 	*  |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
@@ -52,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*  |       |       |       |                                             |      |      |MPrev |      |      | MNext|      |      |      |
 	*/ 
 	[_RAISE] = KEYMAP(
-			______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______, \
-			______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,           ______,  ______,  ______,  ______, \
-			______,  ______, RGB_VAI, RGB_VAD, RGB_MOD,  ______,  ______,  ______,  ______,  ______,  KC_F22,  KC_F23,  KC_F24,  ______,           ______,  ______,  ______,  ______, \
-			______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,           ______,           ______,  ______,  ______,  ______, \
-			______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_VOLD,KC_VOLU, KC_MUTE,                   KC_MPLY,           ______,  ______,  ______,  ______, \
-		    ______,  ______,  ______,                                  ______,                                 ______,  ______, KC_MPRV,  ______,  ______, KC_MNXT,  ______,  ______  \
+			RESET,RGB_TOG,  BL_TOGG,  BL_STEP,    ______,  ______,  ______,  ______,  ______,  ______,  ______, ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______, \
+			______,  BL_INC,  BL_DEC,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______, \
+			______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_F22,  KC_F23,  KC_F24,  ______,  ______,  ______,  ______,  ______, \
+			______,  RGB_HUI,  RGB_SAI,  RGB_VAI,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______, \
+			______,  RGB_HUD,  RGB_SAD,  RGB_VAD,  ______,  ______,  ______,  ______,  KC_VOLD,KC_VOLU, KC_MUTE,      KC_MPLY,  ______,  ______,  ______,  ______, \
+		    ______,  ______,  BL_OFF,                              BL_ON,                        ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______  \
 	)
 };
---
+++ keyboards/ymd96/keymaps/krusli/readme.md
@@ -0,0 +1,2 @@
+# krusli
+Default JJ40 keymap, adapted with RGB underglow support. GUI and LAlt is also swapped.
