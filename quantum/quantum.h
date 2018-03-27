30e9517e3f83d3af2bfade216091ca3c5ade7539
U-LANDSRAAD\drashna <drashna@live.com>
2018-03-27 15:45:29 -0700

    Force require TAPPING_TERM if Tap Dance is enabled

--- quantum/quantum.h
+++ quantum/quantum.h
@@ -104,7 +104,12 @@ extern uint32_t default_layer_state;
     #include "process_unicodemap.h"
 #endif
 
-#include "process_tap_dance.h"
+#ifdef TAP_DANCE_ENABLE
+#ifndef TAPPING_TERM
+#error TAPPING_TERM not defined
+#endif
+  #include "process_tap_dance.h"
+#endif
 
 #ifdef PRINTING_ENABLE
     #include "process_printer.h"
