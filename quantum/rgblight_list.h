5ab10fed64f6b752376e2589781f0bcabc0fc316
U-LANDSRAAD\drashna <drashna@live.com>
2018-03-28 13:00:22 -0700

    add RGB commands as global functions

---
+++ quantum/rgblight_list.h
@@ -0,0 +1,44 @@
+#ifndef RGBLIGHT_LIST_H
+#define RGBLIGHT_LIST_H
+
+
+#define rgblight_setrgb_white()       rgblight_setrgb (0xFF, 0xFF, 0xFF);
+#define rgblight_setrgb_red()         rgblight_setrgb (0xFF, 0x00, 0x00);
+#define rgblight_setrgb_coral()       rgblight_setrgb (0xFF, 0x7C, 0x4D);
+#define rgblight_setrgb_orange()      rgblight_setrgb (0xFF, 0x80, 0x00);
+#define rgblight_setrgb_goldenrod()   rgblight_setrgb (0xD9, 0xA5, 0x21);
+#define rgblight_setrgb_gold()        rgblight_setrgb (0xFF, 0xD9, 0x00);
+#define rgblight_setrgb_yellow()      rgblight_setrgb (0xFF, 0xFF, 0x00);
+#define rgblight_setrgb_chartreuse()  rgblight_setrgb (0x80, 0xFF, 0x00);
+#define rgblight_setrgb_green()       rgblight_setrgb (0x00, 0xFF, 0x00);
+#define rgblight_setrgb_springgreen() rgblight_setrgb (0x00, 0xFF, 0x80);
+#define rgblight_setrgb_turquoise()   rgblight_setrgb (0x47, 0x6E, 0x6A);
+#define rgblight_setrgb_teal()        rgblight_setrgb (0x00, 0x80, 0x80);
+#define rgblight_setrgb_cyan()        rgblight_setrgb (0x00, 0xFF, 0xFF);
+#define rgblight_setrgb_azure()       rgblight_setrgb (0x99, 0xf5, 0xFF);
+#define rgblight_setrgb_blue()        rgblight_setrgb (0x00, 0x00, 0xFF);
+#define rgblight_setrgb_purple()      rgblight_setrgb (0x7A, 0x00, 0xFF);
+#define rgblight_setrgb_magenta()     rgblight_setrgb (0xFF, 0x00, 0xFF);
+#define rgblight_setrgb_pink()        rgblight_setrgb (0xFF, 0x80, 0xBF);
+
+
+#define rgblight_sethsv_white()       rgblight_sethsv (0,  0x00, 255);
+#define rgblight_sethsv_red()         rgblight_sethsv (0,  255, 255);
+#define rgblight_sethsv_coral()       rgblight_sethsv (16, 176, 255);
+#define rgblight_sethsv_orange()      rgblight_sethsv (39,  255, 255);
+#define rgblight_sethsv_goldenrod()   rgblight_sethsv (43,  218, 218);
+#define rgblight_sethsv_gold()        rgblight_sethsv (51,  255, 255);
+#define rgblight_sethsv_yellow()      rgblight_sethsv (60,  255, 255);
+#define rgblight_sethsv_chartreuse()  rgblight_sethsv (90, 255, 255);
+#define rgblight_sethsv_green()       rgblight_sethsv (120,  255, 255);
+#define rgblight_sethsv_springgreen() rgblight_sethsv (150,  255, 255);
+#define rgblight_sethsv_turquoise()   rgblight_sethsv (174,  90, 112);
+#define rgblight_sethsv_teal()        rgblight_sethsv (180,  255, 128);
+#define rgblight_sethsv_cyan()        rgblight_sethsv (180,  255, 255);
+#define rgblight_sethsv_azure()       rgblight_sethsv (186,  102, 255);
+#define rgblight_sethsv_blue()        rgblight_sethsv (240,  255, 255);
+#define rgblight_sethsv_purple()      rgblight_sethsv (270, 255, 255);
+#define rgblight_sethsv_magenta()     rgblight_sethsv (300, 255, 255);
+#define rgblight_sethsv_pink()        rgblight_sethsv (330, 128, 255);
+
+#endif
