59f068f4aec72bbae0c263c1a9d8e87e4dd8a4f0
Michael L. Walker <michael.l.walker@intel.com>
2018-03-29 16:07:12 -0700

    Updating my keymap

--- keyboards/contra/keymaps/walker-ws2812/keymap.c
+++ keyboards/contra/keymaps/walker-ws2812/keymap.c
@@ -78,58 +78,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
-/* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
 [_QWERTY] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_LALT, KC_BSLASH, LOWER, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_RGHT, KC_DOWN, KC_UP}
+  {KC_LCTL, KC_LGUI, KC_LALT, KC_DEL, LOWER, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_RGHT, KC_DOWN, KC_UP}
 },
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_MINUS},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_INSERT,   KC_HOME,   KC_PGUP,   KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PLUS, _______, KC_PIPE},
-  {_______, KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_END,  KC_PGDOWN, KC_SCOLON, _______, _______, KC_DOUBLE_QUOTE,  RGB_MOD},
-  {_______, KC_F7,   KC_F8,   KC_F9, _______, _______, _______, KC_F10, KC_F11, KC_F12, KC_PGDOWN, KC_PGUP}
+  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_UNDERSCORE},
+  {KC_CAPSLOCK,  KC_F1,   KC_F2,   KC_F3,   KC_INSERT,   KC_HOME,   KC_PGUP,   KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PLUS, _______, KC_PIPE},
+  {_______, KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_END,  KC_PGDOWN, KC_SCOLON, KC_LT, KC_GT, KC_DOUBLE_QUOTE, KC_PSCREEN},
+  {_______, KC_F7,   KC_F8,   KC_F9, _______, _______, _______, _______, KC_HOME, KC_END, KC_PGDOWN, KC_PGUP}
 },
 
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
 [_RAISE] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_INS,  KC_HOME, KC_PGUP, KC_LBRACKET, KC_RBRACKET,  KC_EQUAL, _______, KC_BSLS},
-  {_______, KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_END,  KC_PGDOWN, KC_COLON, KC_NUBS, _______, KC_QUOTE, RGB_TOG},
-  {_______, KC_F7,   KC_F8,   KC_F9, _______, _______, _______, _______, RGB_SLOWER, RGB_FASTER, KC_END, KC_HOME}
+  {KC_SCROLLLOCK,  KC_F1,   KC_F2,   KC_F3,   KC_INS,  KC_HOME, KC_PGUP, KC_LBRACKET, KC_RBRACKET,  KC_EQUAL, _______, KC_BSLS},
+  {_______, KC_F4,   KC_F5,   KC_F6,   KC_DEL,  KC_END,  KC_PGDOWN, KC_COLON, KC_NUBS, _______, KC_QUOTE, KC_PAUSE},
+  {_______, KC_F7,   KC_F8,   KC_F9, _______, _______, _______, _______, _______, KC_F10, KC_F11, KC_F12}
 },
 
 /* Plover layer (http://opensteno.org)
@@ -151,22 +118,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX}
 },
 
-/* Adjust (Lower + Raise)
- * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [_ADJUST] = {
   {_______, RESET,   DEBUG,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL },
   {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,  _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, RGB_TOG},
+  {_______, _______, _______, _______, _______, _______, _______, _______, RGB_RMOD, RGB_MOD, RGB_SLOWER,RGB_FASTER}
 }
 
 
