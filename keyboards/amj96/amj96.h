343b5ce50a83f2c66bb608064bc6c843cea2a3d2
mechmerlin <mechmerlin@gmail.com>
2018-03-30 20:55:50 -0700

    Fix keymap
    - Change KEYMAP to LAYOUT
    - Adjust formatting of table

--- keyboards/amj96/amj96.h
+++ keyboards/amj96/amj96.h
@@ -22,13 +22,13 @@
 // The following is an example using the Planck MIT layout
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
-#define KEYMAP( \
+#define LAYOUT( \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E,   K1F, K09, K0A, K0C, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E,   K2F, K0B, K0F, K6F,\
-    K30,   K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,  K3C,  K3D,    K3E, K3F, K0D, K6D,\
-    K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B,     K4C,         K4D, K4E, K4F, K6E,\
-    K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, K5A,      K5B, K5C,K5D,    K5E, K5F, K0E, K07,\
-    K60, K61, K62,           K63,           K64, K65,           K66,      K68,   K69, K6A, K6B, K6C\
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E,   K3F, K0D,      K6D,\
+    K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4E,   K4F,           K6E,\
+    K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, K5A, K5B, K5C, K5D, K5E,   K5F, K0E, K07,\
+    K60, K61, K62, K63, K64, K65, K66,      K68, K69, K6A, K6B, K6C\
 ) { \
     { KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##K07, KC_NO,    KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D, KC_##K0E, KC_##K0F}, \
     { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F}, \
--- keyboards/amj96/keymaps/default/keymap.c
+++ keyboards/amj96/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-     KEYMAP(
+     LAYOUT(
         ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, PSCR,PAUS,  INS, DEL, PGUP,PGDN, \
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,NO,    NLCK,PSLS,PAST,PMNS, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     BSLS,  P7,  P8,  P9,  PPLS, \
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT,NO, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,      RSFT,UP,    P1,  P2,  P3,  PENT,\
         LCTL,LGUI,LALT,               SPC,                RALT,FN0,      LEFT,DOWN,  RGHT,P0, PDOT, PENT),
 
-     KEYMAP(
+     LAYOUT(
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,\
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,    TRNS,TRNS,TRNS,TRNS,\
         TRNS,TRNS,TRNS,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,TRNS,     TRNS,  TRNS,TRNS,TRNS,TRNS,\
