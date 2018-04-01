782acdd73e979609cdd660f55bb73cb77b39c785
Drashna Jaelre <drashna@live.com>
2018-03-31 21:02:40 -0700

    Add userspace config.h handling to build script (#2640)
    * Add userspace 'config.h' file
    
    * Add more robust docs
    
    * Remove config.h code from drashna userspace
    
    * Spelling error
    
    * Include links to Config Options page
    
    * Remove config.h documentation from userspace doc, as it's no longer needed

--- users/drashna/rules.mk
+++ users/drashna/rules.mk
@@ -2,10 +2,4 @@
 SRC += drashna.c
 EXTRAFLAGS        += -flto
 
-ifneq ("$(wildcard users/$(KEYMAP)/config.h)","")
-    CONFIG_H += users/$(KEYMAP)/config.h
-endif
 
-ifeq ($(strip $(NO_SECRETS)), yes)
-    OPT_DEFS += -DNO_SECRETS
-endif
