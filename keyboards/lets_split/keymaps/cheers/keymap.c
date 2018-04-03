cab743715377c33ed4cbf6ccedb84a696499d865
Porras, Jorge A <jorge.porras@thermofisher.com>
2018-04-02 17:49:26 -0700

    cheers keymap for let's split redefinition

--- keyboards/lets_split/keymaps/cheers/keymap.c
+++ keyboards/lets_split/keymaps/cheers/keymap.c
@@ -25,64 +25,61 @@ enum custom_keycodes {
 #define XXXXXXX KC_NO
 
 // Custom macros
-#define CTL_ESC     CTL_T(KC_ESC)               // Tap for Esc, hold for Ctrl
 #define SFT_ENT     SFT_T(KC_ENT)               // Tap for Enter, hold for Shift
-#define HPR_TAB     ALL_T(KC_TAB)               // Tap for Tab, hold for Hyper (Super+Ctrl+Alt+Shift)
-#define MEH_GRV     MEH_T(KC_GRV)               // Tap for Backtick, hold for Meh (Ctrl+Alt+Shift)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Norman
 ,-----------------------------------------. ,-----------------------------------------.
-| Tab  |   Q  |   W  |   D  |   F  |   K  | |   J  |   U  |   R  |   L  |   ;  |  `   |
+| Esc  |   Q  |   W  |   D  |   F  |   K  | |   J  |   U  |   R  |   L  |   ;  | Bksp |
 |------+------+------+------+------+------| |------+------+------+------+------+------|
-| Esc  |   A  |   S  |   E  |   T  |   G  | |   Y  |   N  |   I  |   O  |   H  |  '   |
+| Tab  |   A  |   S  |   E  |   T  |   G  | |   Y  |   N  |   I  |   O  |   H  |  '   |
 |------+------+------+------+------+------| |------+------+------+------+------+------|
-| Shift|   Z  |   X  |   C  |   V  |   B  | |   P  |   M  |   ,  |   .  |   /  |Enter |
+| Shift|   Z  |   X  |   C  |   V  |   B  | |   P  |   M  |   ,  |   .  |   /  | Enter|
 |------+------+------+------+------+------| |------+------+------+------+------+------|
-| Home | End  | Alt  | GUI  |Lower | Bksp | |Space |Raise | Left | Down |  Up  |Right |
+|      |      | Ctrl | Alt  | Lower| GUI  | | Space| Raise| Left | Down |  Up  | Right|
 `-----------------------------------------' `-----------------------------------------'
  */
 [_NORMAN] = KEYMAP( \
-  HPR_TAB, KC_Q,   KC_W,    KC_D,    KC_F,  KC_K,    KC_J,   KC_U,  KC_R,    KC_L,    KC_SCLN, KC_GRV,  \
-  CTL_ESC, KC_A,   KC_S,    KC_E,    KC_T,  KC_G,    KC_Y,   KC_N,  KC_I,    KC_O,    KC_H,    KC_QUOT, \
-  KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,  KC_B,    KC_P,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
-  KC_HOME, KC_END, KC_LALT, KC_LGUI, LOWER, KC_BSPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_ESC,  KC_Q,    KC_W,    KC_D,    KC_F,  KC_K,    KC_J,   KC_U,  KC_R,    KC_L,    KC_SCLN, KC_BSPC, \
+  KC_TAB,  KC_A,    KC_S,    KC_E,    KC_T,  KC_G,    KC_Y,   KC_N,  KC_I,    KC_O,    KC_H,    KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_P,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
+  , , KC_LCTL, KC_LALT, LOWER, KC_LGUI, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
  * ,-----------------------------------------. ,-----------------------------------------.
- * |      |   1  |   2  |   3  |   4  |   5  | |   6  |   7  |   8  |   9  |   0  |  `   |
+ * | Esc  |      |      |      |      |      | |   7  |   8  |   9  |   /  |   =  |      |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  | |  F6  |   4  |   5  |   6  |   -  |  '   |
+ * | Tab  |      |      |      |      |      | |   4  |   5  |   6  |   *  |      |      |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 | |  F12 |   1  |   2  |   3  |   =  |      |
+ * | Shift|      |      |      |      |      | |   1  |   2  |   3  |   -  |      |      |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      |      |      |      | Del  | |Enter |      |Pause | Vol- | Vol+ | Play |
+ * |      |      | Ctrl | Alt  |      | GUI  | |Enter |   0  |   .  |   +  |      |      |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,    KC_MINS, _______, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,    KC_3,    KC_EQL,  _______, \
-  _______, _______, _______, _______, _______, KC_DEL,  SFT_ENT, _______, KC_PAUS, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, , , , , , KC_P7,   KC_P8, KC_P9,   KC_PSLS, KC_PEQL, , \
+  _______, , , , , , KC_P4,   KC_P5, KC_P6,   KC_PAST, XXXXXXX, , \
+  _______, , , , , , KC_P1,   KC_P2, KC_P3,   KC_PMNS, XXXXXXX, , \
+  _______, _______, _______, _______, _______, _______, KC_PENT, KC_P0, KC_PDOT, KC_PPLS, , 
 ),
 
 /* Raise
  * ,-----------------------------------------. ,-----------------------------------------.
- * |      |   !  |   @  |   #  |   $  |   %  | |   ^  |   &  |   *  |   (  |   )  |  ~   |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  | |   ^  |   &  |   *  |   (  |   )  |  Del |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      |      |      |      |      | |   _  |   ?  |   +  |   {  |   }  |  |   |
+ * |   _  |   ?  |   +  |   {  |   }  |  |   | |   -  |   /  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      |      |      |      |      | |   -  |   /  |   =  |   [  |   ]  |  \   |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      |      |      |      | Del  | |Enter |      | Home |PageDn|PageUp| End  |
+ * |      |      |      |      |      |  Del | | Enter|      | Home |PageDn|PageUp|  End |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, ALL_T(KC_TILD), \
-  _______, _______, _______, _______, _______, _______, KC_UNDS, KC_QUES, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,        \
-  _______, _______, _______, _______, _______, _______, KC_MINS, KC_SLSH, KC_EQL,  KC_LBRC, KC_RBRC, SFT_T(KC_BSLS), \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+  KC_UNDS, KC_QUES, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_MINS, KC_SLSH, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   _______, _______, _______, _______, _______, KC_DEL,  SFT_ENT, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
