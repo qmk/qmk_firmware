8c5c18fd1df6359e3db206f5e287da604d1ae136
Jordan Garcia <arickho@gmail.com>
2018-04-04 10:12:53 -0400

    Fix Layer LED flashing issue

--- keyboards/dz60/dz60.c
+++ keyboards/dz60/dz60.c
@@ -23,28 +23,15 @@ void matrix_scan_kb(void)
   switch (layer)
   {
   case 1:
-    led[15].r = 191;
-    led[15].g = 216;
-    led[15].b = 0;
-    ws2812_setleds(led, 16);
+    rgblight_setrgb_at(191, 216, 0, 15);
     break;
   case 2:
-    led[15].r = 107;
-    led[15].g = 91;
-    led[15].b = 0;
-    ws2812_setleds(led, 16);
+    rgblight_setrgb_at(107, 91, 0, 15);
     break;
   case 3:
-    led[15].r = 233;
-    led[15].g = 75;
-    led[15].b = 0;
-    ws2812_setleds(led, 16);
+    rgblight_setrgb_at(233, 75, 0, 15);
     break;
   default:
-    led[15].r = 0;
-    led[15].g = 0;
-    led[15].b = 0;
-    ws2812_setleds(led, 16);
     break;
   }
 };
@@ -55,36 +42,4 @@ void led_init_ports(void)
   DDRB |= (1 << 2);
   // Default to off
   PORTB |= (1 << 2);
-}
-
-void led_set_kb(uint8_t usb_led)
-{
-  // Code for caps lock LED as reported by the OS
-  // Set this per keymap, instead of globally
-  led_set_user(usb_led);
-
-
-    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        led[0].r = 255;
-        ws2812_setleds(led, 16);
-    } else {
-        led[0].r = 0;
-        ws2812_setleds(led, 16);
-    }
-
-    if (usb_led & (1<<USB_LED_SCROLL_LOCK)) {
-        led[0].g = 255;
-        ws2812_setleds(led, 16);
-    } else {
-        led[0].g = 0;
-        ws2812_setleds(led, 16);
-    }
-
-    if (usb_led & (1<<USB_LED_NUM_LOCK)) {
-        led[0].b = 255;
-        ws2812_setleds(led, 16);
-    } else {
-        led[0].b = 0;
-        ws2812_setleds(led, 16);
-    }
 }
