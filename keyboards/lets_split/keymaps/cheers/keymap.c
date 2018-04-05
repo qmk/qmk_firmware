b7d5eb6f62ef24aa3a2b855e62d473b255ae5b59
Porras, Jorge A <jorge.porras@thermofisher.com>
2018-04-04 17:36:14 -0700

    updated Cheers keymap for let's split

--- keyboards/lets_split/keymaps/cheers/keymap.c
+++ keyboards/lets_split/keymaps/cheers/keymap.c
@@ -11,94 +11,114 @@ extern keymap_config_t keymap_config;
 #define _NORMAN 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
+#define _ADJUST 3
 
+// To be used in process_record_user
 enum custom_keycodes {
   NORMAN = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
+  SH_HOME,
+  SH_END,
+  SH_KWS,
+  SH_CLR,
+  SH_CLRL,
+  SH_CLRR,
+  SH_SUDO
 };
 
-// Fillers to make layering more clear
+// Fillers (to make layering more clear)
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+// Mod taps
+#define SFT_ENT    SFT_T(KC_ENT)	// Tap for Enter, hold for Shift
+#define CTL_ESC    CTL_T(KC_ESC)	// Tap for Escape, hold for Control
+
 // Custom macros
-#define SFT_ENT     SFT_T(KC_ENT)               // Tap for Enter, hold for Shift
+#define SCN_SHT    LSFT(LGUI(KC_4))	// Take a screenshot (MacOS)
+#define SCN_LCK    LCTL(LGUI(KC_Q))	// Lock screen (MacOS)
+#define SH_REC     LALT(LGUI(KC_B))	// Show history record
+#define SH_HOME    LCTL(KC_HOME)	// Go to start of line
+#define SH_END     LCTl(KC_END)		// Go to end of line
+#define SH_KWS     LCTl(KC_R)		// Search command history
+#define SH_CLRL    LCTl(KC_U)		// Clear line
+#define SH_CLRW    LCTl(KC_W)		// Clear word behind cursor
+#define SH_CLRC    LCTl(KC_K)		// Clear line from cursor
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Norman
 ,-----------------------------------------. ,-----------------------------------------.
-| Esc  |   Q  |   W  |   D  |   F  |   K  | |   J  |   U  |   R  |   L  |   ;  | Bksp |
+|  Tab |   Q  |   W  |   D  |   F  |   K  | |   J  |   U  |   R  |   L  |   ;  | Bksp |
 |------+------+------+------+------+------| |------+------+------+------+------+------|
-| Tab  |   A  |   S  |   E  |   T  |   G  | |   Y  |   N  |   I  |   O  |   H  |  '   |
+|CtlEsc|   A  |   S  |   E  |   T  |   G  | |   Y  |   N  |   I  |   O  |   H  |  '   |
 |------+------+------+------+------+------| |------+------+------+------+------+------|
-| Shift|   Z  |   X  |   C  |   V  |   B  | |   P  |   M  |   ,  |   .  |   /  | Enter|
+| Shift|   Z  |   X  |   C  |   V  |   B  | |   P  |   M  |   ,  |   .  |   /  |SftEnt|
 |------+------+------+------+------+------| |------+------+------+------+------+------|
-|      |      | Ctrl | Alt  | Lower| GUI  | | Space| Raise| Left | Down |  Up  | Right|
+| Lock | Home |  End | Alt  | Lower| GUI  | | Space| Raise| Left | Down |  Up  | Right|
 `-----------------------------------------' `-----------------------------------------'
  */
 [_NORMAN] = KEYMAP( \
-  KC_ESC,  KC_Q,    KC_W,    KC_D,    KC_F,  KC_K,    KC_J,   KC_U,  KC_R,    KC_L,    KC_SCLN, KC_BSPC, \
-  KC_TAB,  KC_A,    KC_S,    KC_E,    KC_T,  KC_G,    KC_Y,   KC_N,  KC_I,    KC_O,    KC_H,    KC_QUOT, \
+  KC_TAB,  KC_Q,    KC_W,    KC_D,    KC_F,  KC_K,    KC_J,   KC_U,  KC_R,    KC_L,    KC_SCLN, KC_BSPC, \
+  CTL_ESC, KC_A,    KC_S,    KC_E,    KC_T,  KC_G,    KC_Y,   KC_N,  KC_I,    KC_O,    KC_H,    KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_P,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
-  , , KC_LCTL, KC_LALT, LOWER, KC_LGUI, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  SCN_LCK, KC_HOME, KC_END,  KC_LALT, LOWER, KC_LGUI, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
  * ,-----------------------------------------. ,-----------------------------------------.
- * | Esc  |      |      |      |      |      | |   7  |   8  |   9  |   /  |   =  |      |
+ * |  Tab |  F1  |  F2  |  F3  |  F4  |  Ins | |   7  |   8  |   9  |   =  | Calc | Bksp |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Tab  |      |      |      |      |      | |   4  |   5  |   6  |   *  |      |      |
+ * |CtlEsc|  F5  |  F6  |  F7  |  F8  |      | |   4  |   5  |   6  |   -  |   +  |  Del |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Shift|      |      |      |      |      | |   1  |   2  |   3  |   -  |      |      |
+ * | Shift|  F9  |  F10 |  F11 |  F12 |      | |   1  |   2  |   3  |   /  |   *  |SftEnt|
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      | Ctrl | Alt  |      | GUI  | |Enter |   0  |   .  |   +  |      |      |
+ * | Lock | Home |  End |  Alt | Lower|  GUI | |   0  | Raise|   .  |  Cut | Copy | Paste|
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  _______, , , , , , KC_P7,   KC_P8, KC_P9,   KC_PSLS, KC_PEQL, , \
-  _______, , , , , , KC_P4,   KC_P5, KC_P6,   KC_PAST, XXXXXXX, , \
-  _______, , , , , , KC_P1,   KC_P2, KC_P3,   KC_PMNS, XXXXXXX, , \
-  _______, _______, _______, _______, _______, _______, KC_PENT, KC_P0, KC_PDOT, KC_PPLS, , 
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_INS,  KC_P7, KC_P8,   KC_P9,   KC_PEQL, KC_CALC, _______, \
+  _______, KC_F5,   KC_F6,   KC_F8,   KC_F9,   XXXXXXX, KC_P4, KC_P5,   KC_P6,   KC_PMNS, KC_PPLS, KC_DEL,  \
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_P1, KC_P2,   KC_P3,   KC_PSLS, KC_PAST, _______, \
+  _______, _______, _______, _______, _______, _______, KC_P0, _______, KC_PDOT, KC_CUT,  KC_COPY, KC_PASTE
 ),
 
 /* Raise
  * ,-----------------------------------------. ,-----------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  | |   ^  |   &  |   *  |   (  |   )  |  Del |
+ * |  Tab |   !  |   @  |   #  |   $  |   %  | |   ^  |   &  |   *  |   (  |   )  |  Del |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |   _  |   ?  |   +  |   {  |   }  |  |   | |   -  |   /  |   =  |   [  |   ]  |  \   |
+ * |CtlEsc| Mute | VolUp| VolDn| Play |ScnSht| |   ~  |   _  |   |  |   [  |   ]  |   '  |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * | Shift|ClearL|ClearW|ClearC|ShHome| ShEnd| |Search| UpDir| Sudo | Clear|Record|SftEnt|
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      |      |      |      |  Del | | Enter|      | Home |PageDn|PageUp|  End |
+ * | Lock | Home |  End |  Alt | Lower|  GUI | | Space| Raise| Left | Down |  Up  | Right|
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  KC_UNDS, KC_QUES, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_MINS, KC_SLSH, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  _______, _______, _______, _______, _______, KC_DEL,  SFT_ENT, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+  _______, KC_MUTE, KC_VOLU, KC_VOLD, KC_MPLY, SCN_SHT, KC_TILD, KC_UNDS, KC_PIPE, KC_LBRC, KC_RBRC, _______, \
+  _______, SH_CLRL,  SH_CLRW, SH_CLRC, SH_HOME, SH_END,  SH_KWS, SH_UP,   SH_SUDO, SH_CLR,  SH_REC,  _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-/* Adjust (Lower + Raise)
+/* Adjust (Raise + Lower)
  * ,-----------------------------------------. ,-----------------------------------------.
- * |      |Reset |      |      |      |      | |      |      |      |      |      |Reset |
+ * |      |      |      |      | Reset|      | |      | Reset|      |      |      |      |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm| |AGswap|Norman|      |      |      |      |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * |      |      |      |      |      |      | |      |      |      |      |      |      |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      |      |      |      |      | |      |      |      |      |      |      |
+ * |      |      |      |      | Lower|      | |      | Raise|      |      |      |      |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_ADJUST] = KEYMAP( \
-  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   \
-  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, NORMAN,  _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, NORMAN,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 )
 
 };
@@ -122,7 +142,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         persistent_default_layer_set(1UL<<_NORMAN);
       }
       return false;
-      break;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
@@ -132,7 +151,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
@@ -142,7 +160,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     case ADJUST:
       if (record->event.pressed) {
         layer_on(_ADJUST);
@@ -150,7 +167,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_ADJUST);
       }
       return false;
-      break;
+    case SH_SUDO:
+      SEND_STRING("sudo !!"SS_TAP(X_ENT));	// Elevate last terminal command (MacOS)
+      return false;
+    case SH_UP:
+      SEND_STRING("cd .."SS_TAP(X_ENT));	// Go up a directory (MacOS)
+      return false;
+    case SH_CLR:
+      SEND_STRING("clear"SS_TAP(X_ENT));	// Clear screen (MacOS)
+      return false;
   }
   return true;
 }
