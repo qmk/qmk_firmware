cbd7ba5dde11011f2592880d7975cd83603924b4
Daniel H Klein <kledan@amazon.com>
2018-04-05 17:49:17 -0700

    shift left controls

--- keyboards/nyquist/keymaps/danielhklein/keymap.c
+++ keyboards/nyquist/keymaps/danielhklein/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----+------+------+------+------+------|     |------+------+------+------+------+------|
  * |Lshft|   Z  |   X  |   C  |   V  |   B  |     |   N  |   M  |   ,  |   .  |  /   |Enter |
  * |-----+------+------+------+------+------|     |------+------+------+------+------+------|
- * | Ctrl| LAlt | LGui |      |  Bspc/Raise |     |   Spc/Lower | Left | Down |  Up  |Right |
+ * |     | Ctrl | LAlt | LGui |  Bspc/Raise |     |   Spc/Lower | Left | Down |  Up  |Right |
  * `----------------------------------------'     '-----------------------------------------'
  */
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,    KC_RBRACKET, \
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,      KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_SLSH, KC_ENT, \
-  KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, BSP_RSE, BSP_RSE, SPC_LWR, SPC_LWR, KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT \
+  XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, BSP_RSE, BSP_RSE, SPC_LWR, SPC_LWR, KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT \
   ),  
 
 /* Raise
