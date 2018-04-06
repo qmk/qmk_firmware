2075de623a1f6e1812026c8767edb29af525a7ef
Phil <baldonp1@tcnjedu>
2018-04-06 14:01:24 -0400

    Beginning implementation of Pearl backlight. Levels and breathing not working yet.

--- keyboards/pearl/config.h
+++ keyboards/pearl/config.h
@@ -31,6 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 12
 
+#define BACKLIGHT_LEVELS 1
+
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 13
 
