1d9e6302624e02aac3eee26d461a1ed260a53bf9
pnrnc485 <kenta.suzuki1014@gmail.com>
2018-04-10 19:39:06 +0900

    Add: Functions

--- keyboards/jj40/keymaps/suzuken/keymap.c
+++ keyboards/jj40/keymaps/suzuken/keymap.c
@@ -4,13 +4,15 @@
 enum jj40_layers {
   _QWERTY,
   _LOWER,
-  _RAISE
+  _RAISE,
+  _FUNC
 };
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
-  RAISE
+  RAISE,
+  FUNC
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -22,14 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  |  '"  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | GUI  |Space |Lower |Raise |Space | Left | Down |  Up  |Right |
+ * |  Fn  | Ctrl | Alt  | GUI  |Space |Lower |Raise |Space | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-  _______, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  MO(_LOWER),  MO(_RAISE),  KC_SPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  MO(_FUNC), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  MO(_LOWER),  MO(_RAISE),  KC_SPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
@@ -66,10 +68,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ENT, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, KC_BSLS, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+),
+    
+/* Functions
+ *  Note: Terminal uses Calculator shortcut, change your OS keyboard shortcut appropriately.
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      | RGB  | RGB  | RGB  | RGB  |  BL  |  BL  |  BL  |      |      |      |      |
+ * |      | Togl | Hue+ | Sat+ | Brt+ | Togl |Breath| Brt+ |      |      |      |VolUp |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      | RGB  | RGB  | RGB  | WWW  | WWW  |  BL  |      |      |      |      |
+ * |      | Term | Hue- | Sat- | Brt- | Back | Fwd  | Brt- |      |      |PrScr |VolDn |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |||||||| RGB  |      |      |      |      |      |      |      |      |      |
+ * |      |||Fn||| Mode |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FUNC] = KEYMAP( \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
+    _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, BL_TOGG, BL_BRTG, BL_INC,  _______, _______, _______, KC_VOLU, \
+    _______, KC_CALC, RGB_HUD, RGB_SAD, RGB_VAD, KC_WBAK, KC_WFWD, BL_DEC,  _______, _______, KC_PSCR, KC_VOLD, \
+    _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 )
 };
 
 // Loop
 void matrix_scan_user(void) {
-  // Empty
+    // Empty
 };
