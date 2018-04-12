1761c73271d71600f76db753b208cf0bb8113d2a
lbibass <notlbibass@gmail.com>
2018-04-12 12:33:55 -0400

    changed mousekeys settings.

--- keyboards/lets_split/keymaps/lbibass/config.h
+++ keyboards/lets_split/keymaps/lbibass/config.h
@@ -37,4 +37,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 //#define EE_HANDS
 #define TAPPING_TERM 200
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_INTERVAL          0
+#define MOUSEKEY_MAX_SPEED         2
+#define MOUSEKEY_TIME_TO_MAX       0
+#define MOUSEKEY_WHEEL_MAX_SPEED   .5
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 200
 #endif
