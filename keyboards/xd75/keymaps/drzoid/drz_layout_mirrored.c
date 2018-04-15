3a5b1d3067e79295bc2206a608c907fee95430f8
DrZoid <drzoid-github@tppt.eu>
2018-04-15 22:07:35 +0200

    keymap fixes

--- keyboards/xd75/keymaps/drzoid/drz_layout_mirrored.c
+++ keyboards/xd75/keymaps/drzoid/drz_layout_mirrored.c
@@ -71,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY_ON_INTL_QWERTY] =
     KEYMAP(
       DRZ_LCK,  KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     _______,  KC_6,    KC_7,      KC_8,     KC_9,    KC_0,      KC_ESC,   DRZ_LCK,
-      MO(_FN),  KC_LGUI,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     _______,  KC_Y,    KC_U,      KC_I,     KC_O,    KC_P,      KC_LGUI,  MO(_FN),
-      MO(_L3),  KC_LALT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     _______,  KC_H,    KC_J,      KC_K,     KC_L,    KC_SCLN,   KC_LALT,  MO(_L3),
+      MO(_FN),  KC_LGUI,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     _______,  KC_Y,    KC_U,      KC_I,     KC_O,    KC_P,      KC_RGUI,  MO(_FN),
+      MO(_L3),  KC_LALT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     _______,  KC_H,    KC_J,      KC_K,     KC_L,    KC_SCLN,   KC_RALT,  MO(_L3),
       MO(_L2),  DRZ__LS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     _______,  KC_N,    KC_M,      _______,  KC_UP,   KC_COLN,   DRZ__RS,  MO(_L2),
       DRZ__SH,  KC_LCTL,  DRZ__AC,  DRZ__EM,  KC_MENU,  KC_SPC,   KC_SPC,   KC_ENT,   KC_SPC,  KC_SPC,    KC_LEFT,  KC_DOWN, KC_RGHT,   KC_RCTL,  DRZ__SH
     ),
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,  _______,
       _______,  _______,  KC_F11,   KC_F12,   KC_F13,   KC_F14,   KC_F15,   _______,  KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   _______,  _______,
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
-      _______,  _______,  KC_PSCR,  KC_CALC,  KC_PAUS,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
+      _______,  _______,  KC_PSCR,  KC_PAUS,  KC_CALC,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, KC_LBRC,    _______,   KC_RBRC,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  _______,  _______,
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, KC_LPRN,    _______,   KC_RPRN,  XXXXXXX,  XXXXXXX,  XXXXXXX, KC_EXLM,  _______,  _______,
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, KC_LCBR,    _______,   KC_RCBR,  XXXXXXX,  KC_PIPE,  KC_PGUP, KC_QUES,  _______,  _______,
-      _______,  DRZ__CA,  _______,  _______,  _______, KC_BSPC, DRZ_ST,     _______,   KC_TAB,   KC_DELT,  KC_HOME,  KC_PGDN, KC_END,   DRZ__CA,  _______
+      _______,  DRZ__CA,  _______,  _______,  _______, DRZ__ST, KC_TAB,     _______,   KC_BSPC,  KC_DELT,  KC_HOME,  KC_PGDN, KC_END,   DRZ__CA,  _______
     ),
 
   [_L3] =
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define DRZ_SBR RALT(FR_RPAR) //  ] (square bracket right)
 #define DRZ_CBL RALT(FR_APOS) //  { (curly bracket left)
 #define DRZ_CBR RALT(FR_EQUA) //  } (curly bracket right)
-#define DRZ_PIP RALT(FR_MOIN) //  | (pipe)
+#define DRZ_PIP RALT(KC_6)    //  | (pipe)
 #define DRZ_INT LSFT(FR_COMM) //  ? (interrogation mark)
 #define DRZ_SQT RALT(FR_EGRV) //  ` (special quote ?)
 #define DRZ_ASL LALT(FR_UNDS) //  \ (antislash)
@@ -129,8 +129,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY_ON_FR_AZERTY] =
     KEYMAP(
       DRZ_LCK,  KC_ESC,   FR_1,     FR_2,     FR_3,     FR_4,     FR_5,     _______,  FR_6,     FR_7,     FR_8,     FR_9,     FR_0,     KC_ESC,   DRZ_LCK,
-      MO(_FN),  KC_LGUI,  FR_Q,     FR_W,     FR_E,     FR_R,     FR_T,     _______,  FR_Y,     FR_U,     FR_I,     FR_O,     FR_P,     KC_LGUI,  MO(_FN),
-      MO(_L3),  KC_LALT,  FR_A,     FR_S,     FR_D,     FR_F,     FR_G,     _______,  FR_H,     FR_J,     FR_K,     FR_L,     FR_SCLN,  KC_LALT,  MO(_L3),
+      MO(_FN),  KC_LGUI,  FR_Q,     FR_W,     FR_E,     FR_R,     FR_T,     _______,  FR_Y,     FR_U,     FR_I,     FR_O,     FR_P,     KC_RGUI,  MO(_FN),
+      MO(_L3),  KC_LALT,  FR_A,     FR_S,     FR_D,     FR_F,     FR_G,     _______,  FR_H,     FR_J,     FR_K,     FR_L,     FR_SCLN,  KC_RALT,  MO(_L3),
       MO(_L2),  DRZ__LS,  FR_Z,     FR_X,     FR_C,     FR_V,     FR_B,     _______,  FR_N,     FR_M,     _______,  KC_UP,    FR_COLN,  DRZ__RS,  MO(_L2),
       DRZ__SH,  KC_LCTL,  DRZ__AC,  DRZ__EM,  KC_MENU,  KC_SPC,   KC_SPC,   KC_ENT,   KC_SPC,   KC_SPC,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_RCTL,  DRZ__SH
     ),
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,  _______,  FR_F1,    FR_F2,    FR_F3,    FR_F4,    FR_F5,    _______,  FR_F6,    FR_F7,    FR_F8,    FR_F9,    FR_F10,   _______,  _______,
       _______,  _______,  FR_F11,   FR_F12,   FR_F13,   FR_F14,   FR_F15,   _______,  FR_F16,   FR_F17,   FR_F18,   FR_F19,   FR_F20,   _______,  _______,
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
-      _______,  _______,  KC_PSCR,  KC_CALC,  KC_PAUS,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
+      _______,  _______,  KC_PSCR,  KC_PAUS,  KC_CALC,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, DRZ_SBL,    _______,  DRZ_SBL,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, FR_LPAR,    _______,  FR_RPAR,  XXXXXXX,  XXXXXXX,  XXXXXXX,  FR_EXCL,  _______,  _______,
       _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, DRZ_CBL,    _______,  DRZ_CBR,  XXXXXXX,  DRZ_PIP,  FR_PGUP,  DRZ_INT,  _______,  _______,
-      _______,  DRZ__CA,  _______,  _______,  _______, FR_BSPC, DRZ__ST,    _______,  FR_TAB,   FR_DEL,   FR_HOME,  FR_PGDN,  FR_END,   DRZ__CA,  _______
+      _______,  DRZ__CA,  _______,  _______,  _______, DRZ__ST, FR_TAB ,    _______,  FR_BSPC,  FR_DEL,   FR_HOME,  FR_PGDN,  FR_END,   DRZ__CA,  _______
     ),
 
   [_L3] =
