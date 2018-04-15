0ea526b73a306b1ab085cca36c432c7cb95558bd
Gilbert Consellado <gilbertconsellado@gmail.com>
2018-04-15 14:05:22 +0800

    set the tapping_term to 100

--- users/zer09/config.h
+++ users/zer09/config.h
@@ -15,6 +15,11 @@
 
 #define FORCE_NKRO
 
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#endif
+#define TAPPING_TERM 100
+
 // Disable action_get_macro and fn_actions, since we don't use these
 // and it saves on space in the firmware.
 #define NO_ACTION_MACRO
