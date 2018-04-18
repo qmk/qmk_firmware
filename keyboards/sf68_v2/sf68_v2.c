e3488adadb08d574030c8a46d070a8b2e942cab1
Lei Yu <leiyu@ceasia.com.cn>
2018-04-18 17:12:39 +0800

    update keymap

--- keyboards/sf68_v2/sf68_v2.c
+++ keyboards/sf68_v2/sf68_v2.c
@@ -3,24 +3,25 @@
 
 void matrix_init_kb(void)
 {
-
-    // set pd0&pd1 as output
-    DDRF |= _BV(1);
-
-    //PORTD |= 1;
     matrix_init_user();
+    led_init_ports();
 }
+
 void matrix_scan_kb(void)
 {
     matrix_scan_user();
 }
 
-void led_set_kb(uint8_t usb_led) {
-    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-      // Turn capslock on
-      PORTF |= (1<<1);
-    } else {
-      // Turn capslock off
-      PORTF &= (1<<1);
-    }
-  }
+void led_init_ports(void)
+{
+    // led setting
+    DDRF |= (1<<1);
+    PORTF |= (1<<1);
+
+    // rgb setting
+    //DDRF |= _BV(1);
+}
+
+void led_set_kb(uint8_t usb_led) { 
+    led_set_user(usb_led);
+}
