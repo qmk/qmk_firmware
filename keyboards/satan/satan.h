a1377344732a7df3e7554b04614d21d31c4f1a15
Christian Hartlage <c@dende.de>
2018-04-20 18:39:46 +0200

    fixed missing newline

--- keyboards/satan/satan.h
+++ keyboards/satan/satan.h
@@ -175,4 +175,5 @@ void matrix_scan_user(void);
 #define LAYOUT_60_iso KEYMAP_ISO
 #define LAYOUT_60_ansi_split_bs_rshift KEYMAP_HHKB
 #define LAYOUT_60_iso_split_bs_rshift KEYMAP_ISO_SPLITRSHIFT_SPLITBACKSPACE
+
 #endif
