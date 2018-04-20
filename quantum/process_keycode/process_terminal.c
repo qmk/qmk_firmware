4784fc85e3015a2e8f1f4a1c9487bce005fbe4e4
Michael Pressburg <mikepress88@gmail.com>
2018-04-21 00:16:31 +0100

    Fixed bad copyright header

--- quantum/process_keycode/process_terminal.c
+++ quantum/process_keycode/process_terminal.c
@@ -1,4 +1,6 @@
-s program is free software: you can redistribute it and/or modify
+/* Copyright 2017 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
