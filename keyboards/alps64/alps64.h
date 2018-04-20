ad74c9026e77e945cdd5cf174ac7c80ee993b628
mechmerlin <mechmerlin@gmail.com>
2018-04-19 19:53:02 -0700

    Change KEYMAP to LAYOUT

--- keyboards/alps64/alps64.h
+++ keyboards/alps64/alps64.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Alps64 keymap definition macro */
 
-#define KEYMAP( \
+#define LAYOUT( \
     K36, K37, K46, K47, K56, K57, K66, K67, K76, K77, K06, K07, K17, K26, K27, \
     K34, K35, K44, K45, K54, K55, K64, K65, K75, K05, K15, K16, K25, K24, \
     K32, K33, K43, K52, K53, K63, K73, K74, K03, K04, K13, K14, K23, \
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { K70, K71, K72, K73, K74, K75, K76, K77 } \
 }
 
-#define KC_KEYMAP( \
+#define LAYOUT_kc( \
     K36, K37, K46, K47, K56, K57, K66, K67, K76, K77, K06, K07, K17, K26, K27, \
     K34, K35, K44, K45, K54, K55, K64, K65, K75, K05, K15, K16, K25, K24, \
     K32, K33, K43, K52, K53, K63, K73, K74, K03, K04, K13, K14, K23, \
