652b9d89a7bca3d977dfbca87264bd561ef90445
Carlos Filoteo <c.filoteo@live.com>
2018-04-21 11:11:42 -0600

    Disable mousekeys

--- keyboards/ut47/rules.mk
+++ keyboards/ut47/rules.mk
@@ -55,7 +55,7 @@ SRC = matrix.c led.c protocol/serial_uart.c
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
