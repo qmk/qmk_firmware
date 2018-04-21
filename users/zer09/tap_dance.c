b891c2ad342159f8e64b1af1e67b09530d5712b6
Gilbert Consellado <gilbertconsellado@gmail.com>
2018-04-21 15:52:36 +0800

    Remove the interrupted state of the tap dance

--- users/zer09/tap_dance.c
+++ users/zer09/tap_dance.c
@@ -31,25 +31,25 @@ void layer_switcher_tap(uint8_t new_layer) {
 int cur_dance(qk_tap_dance_state_t *state) {
   switch (state->count) {
   case 1:
-    if (state->interrupted || state->pressed == 0) {
+    if (state->pressed == 0) {
       return SINGLE_TAP;
     } else {
       return SINGLE_HOLD;
     }
   case 2:
-    if (state->interrupted || state->pressed == 0) {
+    if (state->pressed == 0) {
       return DOUBLE_TAP;
     } else {
       return DOUBLE_HOLD;
     }
   case 3:
-    if (state->interrupted || state->pressed == 0) {
+    if (state->pressed == 0) {
       return TRIPLE_TAP;
     } else {
       return TRIPLE_HOLD;
     }
   default:
-    if (state->interrupted || state->pressed == 0) {
+    if (state->pressed == 0) {
       return DEFAULT_TAP;
     } else {
       return DEFAULT_HOLD;
