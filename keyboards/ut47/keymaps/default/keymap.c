5886d5cea7b7a8aee56c51990a040b838b0b421d
Carlos Filoteo <c.filoteo@live.com>
2018-04-22 09:24:47 -0600

    Remove extra file

--- keyboards/ut47/keymaps/default/keymap.c
+++ keyboards/ut47/keymaps/default/keymap.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "ut47.h"
-#include "keymap_common.h"
 #include "protocol/serial.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
