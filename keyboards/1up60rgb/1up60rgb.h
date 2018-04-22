e132d5fe8fba7c9424c2c0baa2afe63658ca1c15
Joe Wasson <jwasson+github@gmail.com>
2018-04-21 23:16:25 -0700

    Add layouts to 1up60rgb. (#2789)
    * Add layouts to 1up60rgb.
    
    * Fix build break in header file for 1up60rgb.

--- keyboards/1up60rgb/1up60rgb.h
+++ keyboards/1up60rgb/1up60rgb.h
@@ -19,7 +19,7 @@
 
 
 /* ANSI variant. No extra keys for ISO */
-#define LAYOUT_60_ansi ( \
+#define LAYOUT_60_ansi( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012,       K013, \
 	K100,       K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, K114, \
 	K200,       K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212,       K214, \
@@ -34,7 +34,7 @@
 )
 
 /* ISO variant. Remove useless ANSI keys */
-#define LAYOUT_60_iso ( \
+#define LAYOUT_60_iso( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012,       K013, \
 	K100,       K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113,       \
 	K200,       K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213, K214, \
@@ -49,7 +49,7 @@
 )
 
 /* HHKB Variant */
-#define LAYOUT_60_ansi_split_bs_rshift ( \
+#define LAYOUT_60_ansi_split_bs_rshift( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, K014, \
 	K100,       K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, K114, \
 	K200,       K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212,       K214, \
