714ddf8ff2c4ab398339c487ee0c432b84f16497
Carlos Filoteo <c.filoteo@live.com>
2018-04-22 23:56:29 -0600

    Fix fn layer keys

--- keyboards/ut47/keymaps/default/keymap.c
+++ keyboards/ut47/keymaps/default/keymap.c
@@ -33,7 +33,7 @@ LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT_RSFT_ENT,
-  KC_LCTL, KC_LALT, KC_LGUI, KC_APP,  MO(1),      KC_SPC,        KC_FN0,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LCTL, KC_LALT, KC_LGUI, KC_APP,  MO(2),      KC_SPC,        MO(1),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 LAYOUT(
