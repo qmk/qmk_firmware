dca944b04d596d89e53c0ac32d140963ade52acd
monsieurluge <monsieurluge@gmail.com>
2018-04-24 21:38:32 +0200

    restore the left shift key in the "gaming" layout

--- keyboards/planck/keymaps/monsieurluge/keymap.c
+++ keyboards/planck/keymaps/monsieurluge/keymap.c
@@ -73,16 +73,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    * │ tab │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │  ;  │ ent │
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   * │media│  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │ up  │raise│
+   * │shift│  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │ up  │raise│
    * ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
-   * │ctrl │ os  │ alt │space│lower│   shift   │space│ alt │left │down │right│
+   * │ctrl │ os  │ alt │space│lower│   shift   │space│media│left │down │right│
    * └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
    */
   [_GAMING] = {
     {KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,  KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC},
     {KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,  KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT},
-    {SP_MEDI, KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   RAISE},
-    {KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, LOWER, KC_LSFT, KC_LSFT, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT}
+    {KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   RAISE},
+    {KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, LOWER, KC_LSFT, KC_LSFT, KC_SPC, SP_MEDI, KC_LEFT, KC_DOWN, KC_RGHT}
   },
 
   /* lower
