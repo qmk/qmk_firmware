676080372c8218f7f6c23edd35dd8c2f51884d51
Jack Humbert <jack.humb@gmail.com>
2018-04-24 11:58:36 -0400

    try mouse wheel again

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -291,11 +291,11 @@ void encoder_update(bool clockwise) {
     }
   } else {
     if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
+      register_code(KC_MS_WH_DOWN);
+      unregister_code(KC_MS_WH_DOWN);
     } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
+      register_code(KC_MS_WH_UP);
+      unregister_code(KC_MS_WH_UP);
     }
   }
 }
