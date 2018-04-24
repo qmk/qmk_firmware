1763a1757e24b854e5203f4fb9337687b81154ca
mechmerlin <mechmerlin@gmail.com>
2018-04-23 19:53:21 -0700

    Fix that row by column

--- keyboards/meme/meme.h
+++ keyboards/meme/meme.h
@@ -18,17 +18,19 @@
 
 #include "quantum.h"
 
-// This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
-// The first section contains all of the arguments
-// The second converts the arguments into a two-dimensional array
-#define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k11   \
-) \
-{ \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k11 }, \
+#define LAYOUT_all( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      K1F, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K2F, \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A,           K3D, K3E, K3F, \
+    K40, K41, K42,           K45,                     K4A, K4B, K4C, K4D, K4E, K4F \
+)\
+{\
+  {K00, K01, K02, K03,   K04,   K05, K06,   K07,   K08,   K09,   K0A, K0B,   K0C,   K0D, K0E,   K0F}, \
+  {K10, K11, K12, K13,   K14,   K15, K16,   K17,   K18,   K19,   K1A, K1B,   K1C,   K1D, KC_NO, K1F}, \
+  {K20, K21, K22, K23,   K24,   K25, K26,   K27,   K28,   K29,   K2A, K2B,   KC_NO, K2D, KC_NO, K2F}, \
+  {K30, K31, K32, K33,   K34,   K35, K36,   K37,   K38,   K39,   K3A, KC_NO, KC_NO, K3D, K3E,   K3F}, \
+  {K40, K41, K42, KC_NO, KC_NO, K45, KC_NO, KC_NO, KC_NO, KC_NO, K4A, K4B,   K4C,   K4D, K4E,   K4F} \
 }
 
 #endif
