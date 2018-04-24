7bae922365892ae5c27eff1d45840660b2319390
mechmerlin <mechmerlin@gmail.com>
2018-04-23 20:02:59 -0700

    work in progress commit

--- keyboards/meme/meme.h
+++ keyboards/meme/meme.h
@@ -33,4 +33,25 @@
   {K40, K41, K42, KC_NO, KC_NO, K45, KC_NO, KC_NO, KC_NO, KC_NO, K4A, K4B,   K4C,   K4D, K4E,   K4F} \
 }
 
+
+#define LAYOUT_real( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      K1F, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K2F, \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A,           K3D, K3E, K3F, \
+    K40, K41, K42,           K45,                     K4A, K4B, K4C, K4D, K4E, K4F \
+)\
+{\
+  {K00, K01, K02, K03, K04, K05, K06, K07}, \
+  {K10, K11, K12, K13, K14, K15, K16, K17}, \
+  {K20, K21, K22, K23, K24, K25, K26, K27}, \
+  {K30, K31, K32, K33, K34, K35, K36, K37}, \
+  {K40, K41, K42, K43, K44, K45, K46, K47}, \
+  {K50, K51, K52, K53, K54, K55, K56, K57}, \
+  {K60, K61, K62, K63, K64, K65, K66, K67}, \
+  {K70, K71, K72, K73, K74, K75, K76, K77}, \
+  {K80, K81, K82, K83, K84, K85, K86, K87}, \
+  {K90, K91, K92, K93, K94, K95, K96, K97}, \
+}
+
 #endif
