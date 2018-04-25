5f252b09c6f3cd310e5d243229d627350d90d74b
mechmerlin <mechmerlin@gmail.com>
2018-04-25 08:22:05 -0700

    Stopping point at creating targets for new_project script

--- util/new_project.sh
+++ util/new_project.sh
@@ -3,7 +3,9 @@
 # Jack Humbert 2015
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 <keyboard_name>"
+	echo "Usage: $0 <keyboard_name> <firmware_type>"
+	echo "Example: $0 gh60 avr"
+	echo "Example: $0 bfake ps2avrgb"
 	exit 1
 fi
 
@@ -15,11 +17,20 @@ fi
 cd "$(dirname "$0")/.."
 
 KEYBOARD=$1
+FIRMWARE_TYPE=$2
+
+if [ -z "$2" ]; then
+	echo "Defaulting to avr"
+	FIRMWARE_TYPE = "avr"
+fi
+
 KEYBOARD_UPPERCASE=$(echo $1 | awk '{print toupper($0)}')
 KEYBOARD_NAME=$(basename $1)
 KEYBOARD_NAME_UPPERCASE=$(echo $KEYBOARD_NAME | awk '{print toupper($0)}')
 
-cp -r quantum/template keyboards/$KEYBOARD
+cp -r quantum/template/base keyboards/$KEYBOARD
+cp -r quantum/template/$FIRMWARE_TYPE / keyboards/$KEYBOARD/
+
 mv keyboards/${KEYBOARD}/template.c keyboards/${KEYBOARD}/${KEYBOARD_NAME}.c
 mv keyboards/${KEYBOARD}/template.h keyboards/${KEYBOARD}/${KEYBOARD_NAME}.h
 find keyboards/${KEYBOARD} -type f -exec sed -i '' -e "s;%KEYBOARD%;${KEYBOARD_NAME};g" {} \;
