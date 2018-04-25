e543d77e9bedecface616051f1697ea439bad9c7
foostan <ks@fstn.jp>
2018-04-26 00:29:13 +0900

    Remove unused settings

--- keyboards/crkbd/rev1/config.h
+++ keyboards/crkbd/rev1/config.h
@@ -29,21 +29,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT         Crkbd
 #define DESCRIPTION     A split keyboard with 3x6 vertically staggered keys and 3 thumb keys
 
-#define CRKBD_ROWS 5
-
 /* key matrix size */
 // Rows are doubled-up
-#if  CRKBD_ROWS == 4
-  #define MATRIX_ROWS 8
-  #define MATRIX_COLS 7
-  #define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-#elif CRKBD_ROWS == 5
-  #define MATRIX_ROWS 10
-  #define MATRIX_COLS 7
-  #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
-#else
-  #error "expected CRKBD_ROWS 4 or 5"
-#endif
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 7
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
 
 // wiring of each half
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2 }
--- keyboards/crkbd/rev1/keymaps/default/keymap.c
+++ keyboards/crkbd/rev1/keymaps/default/keymap.c
@@ -55,7 +55,6 @@ enum macro_keycodes {
 //Macros
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 
-#if CRKBD_ROWS == 5
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = KEYMAP( \
@@ -114,11 +113,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`-----------------------------------------------------------------------------------'  `-----------------------------------------------------------------------------------'
   )
 };
-
-#elif CRKBD_ROWS == 4
-#else
-#error "undefined keymaps"
-#endif
 
 #ifdef AUDIO_ENABLE
 
