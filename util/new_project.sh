083d43827fb862c65bead4a4281626bf0897f94f
mechmerlin <mechmerlin@gmail.com>
2018-04-25 22:38:45 -0700

    consider the case where the firmware type target is not valid

--- util/new_project.sh
+++ util/new_project.sh
@@ -25,6 +25,11 @@ cd "$(dirname "$0")/.."
 KEYBOARD=$1
 FIRMWARE_TYPE=$2
 
+if [ $FIRMWARE_TYPE != "avr" -a $FIRMWARE_TYPE != "ps2avrgb" ]; then
+  echo "Invalid firmware type target"
+  exit 1
+fi
+
 KEYBOARD_UPPERCASE=$(echo $1 | awk '{print toupper($0)}')
 KEYBOARD_NAME=$(basename $1)
 KEYBOARD_NAME_UPPERCASE=$(echo $KEYBOARD_NAME | awk '{print toupper($0)}')
