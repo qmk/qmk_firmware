119a3b617788f386d59d495a07f0bef6b060bfa6
lbibass <notlbibass@gmail.com>
2018-04-27 13:48:35 -0400

    Revised my keymap a bit more.

--- keyboards/ut47/keymaps/lbibass/keymap.c
+++ keyboards/ut47/keymaps/lbibass/keymap.c
@@ -30,7 +30,6 @@ extern keymap_config_t keymap_config;
 #define _RAISE 3
 #define _ADJUST 16
 
-
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   WINDOWS,
@@ -92,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LGUI,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_ESC, RGB_TOG, KC_LALT, KC_LCTL,  LOWER,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_ESC, RGB_TOG, KC_LALT, KC_LCTL,LOWER, KC_SPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Windows
@@ -110,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_ESC ,RGB_TOG, KC_LGUI, KC_LALT,  LOWER,     KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_ESC ,RGB_TOG, KC_LGUI, KC_LALT,  LOWER,     KC_SPC,        RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* raise
@@ -128,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
   KC_LGUI,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  KC_ESC, RGB_TOG, KC_LALT, KC_LCTL, LOWER,  KC_SPC, _______, TD(CT_media), KC_VOLD, KC_VOLU, KC_MFFD \
+  KC_ESC, RGB_TOG, KC_LALT, KC_LCTL, LOWER,     KC_SPC,         _______, TD(CT_media), KC_VOLD, KC_VOLU, KC_MFFD \
 ),
 
 /* Lower
