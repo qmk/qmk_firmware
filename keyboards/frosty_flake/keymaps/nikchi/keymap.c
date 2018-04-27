b0b06fb569597c5b846025067261939d1750a116
Nick Choi <iinikchi@gmail.com>
2018-04-27 16:09:07 -0400

    ok

--- keyboards/frosty_flake/keymaps/nikchi/keymap.c
+++ keyboards/frosty_flake/keymaps/nikchi/keymap.c
@@ -15,6 +15,7 @@ void cycleEmojis(qk_tap_dance_state_t *state, void *user_data);
 void cycleAnimals(qk_tap_dance_state_t *state, void *user_data);
 void cycleHands(qk_tap_dance_state_t *state, void *user_data);
 void cycleMemes(qk_tap_dance_state_t *state, void *user_data);
+void cycleAll(qk_tap_dance_state_t *state, void *user_data);
 
 void tap(uint16_t keycode){
     register_code(keycode);
@@ -28,7 +29,8 @@ enum taps{
   ANIMAL,
   HAND,
   MEMES,
-  COPA
+  COPA,
+  ALLS
 };
 
 enum unicode_name { // split every five emojis
@@ -76,7 +78,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [EMOJIS] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleEmojis, NULL, NULL, 800),
   [ANIMAL] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleAnimals, NULL, NULL, 800),
   [HAND] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleHands, NULL, NULL, 800),
-  [MEMES] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleMemes, NULL, NULL, 800)
+  [MEMES] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleMemes, NULL, NULL, 800),
+  [ALLS] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleAll, NULL, NULL, 800)
 // Other declarations would go here, separated by commas, if you have them
 };
 
@@ -106,6 +109,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   }
   return MACRO_NONE;
 };
+
 
 // emojis in unicode
 const uint32_t PROGMEM unicode_map[] = {
@@ -141,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LSPO,KC_NUBS,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,        KC_RSPC,            KC_UP,            KC_P1,  KC_P2,  KC_P3,KC_PENT, \
  TD(TD_CTCPS),KC_LGUI,KC_LALT,                 KC_SPC,                                KC_LEAD,KC_RGUI, KC_APP,MO(1)  ,  KC_LEFT,KC_DOWN,KC_RGHT,    KC_P0,KC_PDOT),
 [1] = KEYMAP(\
-      KC_ESC,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,           KC_PSCR,KC_SLCK,KC_PAUS,                        \
+    TD(ALLS),  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,           KC_PSCR,KC_SLCK,KC_PAUS,                        \
    TD(EMOJIS),TD(ANIMAL),TD(HAND),TD(MEMES),X(WEARY),X(UNAMU),   KC_6,   KC_7,   KC_8,   KC_9,   KC_0, KC_MINS, KC_EQL,KC_BSPC,   KC_MPRV,KC_MPLY,KC_MNXT,  KC_NLCK,KC_PSLS,KC_PAST,KC_PMNS, \
       KC_TAB,   KC_Q,   M(0),   KC_E,   KC_R,X(EGGPL),X(WATER),   KC_U,   KC_I,   KC_O,   KC_P, KC_UP  ,KC_RBRC,KC_BSLS,   KC_MUTE,KC_VOLD,KC_VOLU,    KC_P7,  KC_P8,  KC_P9,KC_PPLS, \
       KC_LCTL,   M(1),   M(3),   M(2),   KC_F,   X(LIT), X(SNEK),   KC_J,   KC_K,   KC_L,KC_LEFT,KC_RGHT,         KC_ENT,                              KC_P4,  KC_P5,  KC_P6,      \
@@ -153,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC,KC_BSLS,   KC_MUTE,KC_VOLD,KC_VOLU,    KC_P7,  KC_P8,  KC_P9,KC_PPLS, \
       KC_LCTL,   KC_D,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,         KC_ENT,                              KC_P4,  KC_P5,  KC_P6,      \
       KC_LSFT,KC_NUBS,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,        KC_RSFT,          KC_MS_U,            KC_P1,  KC_P2,  KC_P3,KC_PENT, \
-      KC_BTN1,KC_BTN3,KC_BTN2,                 KC_SPC,                                KC_RALT,KC_RGUI, _______, _______,  KC_MS_L,KC_MS_D,KC_MS_R,    KC_P0,KC_PDOT),
+      KC_BTN1,KC_BTN3,KC_BTN2,                 KC_SPC,                                KC_RALT,KC_RGUI, TG(2) , KC_NO ,  KC_MS_L,KC_MS_D,KC_MS_R,    KC_P0,KC_PDOT),
 };
 
 LEADER_EXTERNS();
@@ -169,7 +173,7 @@ void matrix_scan_user(void) {
       tap(KC_C);
       unregister_code(KC_LCTL);
     }
-    SEQ_THREE_KEYS(KC_L,KC_I,KC_T) { // 🔥🔥
+    SEQ_THREE_KEYS(KC_L,KC_I,KC_T) { // 🔥🔥 Leader key then type lit
       unicode_input_start();
       register_hex32(pgm_read_dword(&unicode_map[LIT]));
       unicode_input_finish();
@@ -194,6 +198,20 @@ void cycleEmojis(qk_tap_dance_state_t *state, void *user_data) {
     tap(KC_BSPC);
     unicode_input_start();
     register_hex32(pgm_read_dword(&unicode_map[state->count]));
+    unicode_input_finish();
+  }
+};
+
+void cycleAll(qk_tap_dance_state_t *state, void *user_data) {
+  if(state->count == 1) {
+    unicode_input_start();
+    register_hex32(pgm_read_dword(&unicode_map[2]));
+    unicode_input_finish();
+  }
+  else if(state->count <= 1642) {
+    tap(KC_BSPC);
+    unicode_input_start();
+    register_hex32(pgm_read_dword(&unicode_map[2])+state->count);
     unicode_input_finish();
   }
 };
@@ -239,5 +257,3 @@ void cycleMemes(qk_tap_dance_state_t *state, void *user_data) {
     unicode_input_finish();
   }
 };
-
-
