25a768208d7866240770a2b5887810cdb4a76c09
ericywl <midnight2903@gmail.com>
2018-04-30 18:33:53 +0800

    Missed out one lightsaver.h

--- keyboards/lightsaver/keymaps/rasmus/keymap.c
+++ keyboards/lightsaver/keymaps/rasmus/keymap.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "lightsaver.h"
+#include QMK_KEYBOARD_H
 
 #define KC_F1GL TD(TD_F1_GAME)
 #define KC_CLFN TD(TD_CAPS_FN)
