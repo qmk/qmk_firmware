7db422aa1bcf2f01dd1c505e182885503917af19
hailbreno <contato@breno.co>
2018-04-30 15:51:06 -0300

    Qwerty layout done

--- keyboards/dz60/keymaps/hailbreno/keymap.c
+++ keyboards/dz60/keymaps/hailbreno/keymap.c
@@ -2,11 +2,13 @@
 * @Author: hailbreno
 * @Date:   2018-04-30 14:32:13
 * @Last Modified by:   hailbreno
-* @Last Modified time: 2018-04-30 15:25:58
+* @Last Modified time: 2018-04-30 15:50:11
 */
 
 #include "dz60.h"
 #include "action_layer.h"
+
+#define X KC_NO
 
 extern keymap_config_t keymap_config;
 
@@ -56,22 +58,24 @@ enum keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------------------------.
+ * |     |     |     |     |     |     |     |     |     |     |     |     |     |     |     |
+ * |-----------------------------------------------------------------------------------------+
+ * |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
+ * |-----------------------------------------------------------------------------------------+
+ * |          |    |     |     |     |     |     |     |     |     |     |     |             |
+ * |-----------------------------------------------------------------------------------------+
+ * |           |     |     |     |     |     |     |     |     |     |     |     |     |     |
+ * |-----------------------------------------------------------------------------------------+
+ * |      |      |      |             |      |                 |     |     |     |     |     |
+ * `-----------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_2_shifts(
-  KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
-  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-  KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RGUI, KC_LEFT, KC_DOWN, KC_RIGHT),
+  KC_GESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,  KC_GRV,  KC_DEL,
+  KC_TAB,      KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,      KC_BSLS,
+  KC_CAPS,     KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,              KC_ENT,
+  KC_LSFT, X,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,  KC_DEL,
+  KC_LCTL,     KC_LALT,  KC_LGUI,  KC_BSPC,    KC_SPC,  KC_SPC,     KC_RALT,  KC_APP,  KC_LEFT,  KC_DOWN,  KC_RIGHT),
 
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
