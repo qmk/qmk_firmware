03f572dcc04762d2b2b26bfaa394dafdfa79700d
amesa0 <amesa062@fiu.edu>
2018-04-30 01:14:44 -0400

    add keymap

--- keyboards/dz60/keymaps/helpful - hhkb/keymap.c
+++ keyboards/dz60/keymaps/helpful - hhkb/keymap.c
@@ -8,31 +8,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------------.
- * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | Bkspc| Del|
+ * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | ` ~  | Del|
  * |-----------------------------------------------------------------------------------------+
- * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
+ * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  | Bksp   |
  * |-----------------------------------------------------------------------------------------+
  * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
  * |-----------------------------------------------------------------------------------------+
- * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | ` ~ |  U  | FN  |
+ * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  \  |  U  | FN  |
  * |-----------------------------------------------------------------------------------------+
  * | Ctrl |  Alt  |  Cmd  |              Space                | Cmd | RAlt |  L  |  D  |  R  |
  * `-----------------------------------------------------------------------------------------'
  */
 
   KEYMAP_2_SHIFTS(
-      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_DEL,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
       KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSFT, KC_UP, MO(1),
-      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RGUI, KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_BSLASH, KC_UP, MO(1),
+      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
 /* FN Layer
  * ,-----------------------------------------------------------------------------------------.
  * | Esc | F1  | F2 | F3  | F4  | F5  | F6   | F7  | F8  | F9 | F10 | F11 | F12 |     |      |
  * |-----------------------------------------------------------------------------------------+
- * |        |VOL-  | MUTE |VOL+  |     |     |     |     |     |     |     |      |      |      |
+ * |        |VOL-  | MUTE |VOL+  |     |     |     |     |     |     |     |      |      |    |
  * |-----------------------------------------------------------------------------------------+
  * |          | REV| PAUSE |  FWD|     |     |     |     |     |     |     |     |            |
  * |-----------------------------------------------------------------------------------------+
