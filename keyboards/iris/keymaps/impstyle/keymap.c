0082ace190c55d37894a11c7ffa344823199d211
David Friedrich <david.friedrich@digimapas.cl>
2018-04-30 14:31:20 -0300

    mouse layer added to impstyle keymap

--- keyboards/iris/keymaps/impstyle/keymap.c
+++ keyboards/iris/keymaps/impstyle/keymap.c
@@ -7,13 +7,13 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
+#define _MOUSE 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
+  MOUSE,
 };
 
 #define KC_ KC_TRNS
@@ -25,6 +25,7 @@ enum custom_keycodes {
 #define KC_BL_S BL_STEP
 #define KC_TGLW TG(_LOWER)
 #define KC_TGRS TG(_RAISE)
+#define KC_MOUS MOUSE
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -45,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                    KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
      KC_TAB , KC_Q  , KC_W  ,KC_E  , KC_R  , KC_T  ,                     KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS ,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                    KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_TGRS,     KC_TGLW , KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_PLUS,     KC_MOUS , KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                                   KC_LGUI,KC_RASE,KC_SPC ,         KC_ENT ,KC_LOWR,KC_LALT
   ),
   
@@ -67,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,_______,KC_PGUP,_______,_______,KC_LBRC,               KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,_______,
         _______ ,KC_HOME,KC_PGDN,KC_END,_______,KC_LPRN,               KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
     _______,_______,_______,_______,_______,_______,_______,        _______ ,_______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS ,
-                          _______ ,_______,KC_DEL ,        KC_BSPC ,_______, KC_P0
+                          _______ ,_______,KC_PSCR ,        KC_DEL ,_______, KC_P0
 ),
   // RAISE LAYER
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
@@ -90,21 +91,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      _______ ,_______,_______,            KC_EQL,_______,_______
   ),
 
-  [_ADJUST] = LAYOUT(
-  //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
-      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RESET  , DEBUG  , RGB_HUD, RGB_SAD, RGB_VAD, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
-      BL_STEP, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
-  //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
-                                      _______, _______, _______,                  _______, _______, _______
-  //                                `--------+--------+--------'                `--------+--------+--------'
-  )
+  [_MOUSE] = LAYOUT(
+     KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                KC_F6 , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+        _______ ,_______, KC_MS_UP ,_______,_______,_______,               KC_MS_WH_UP,_______,_______,_______ ,_______,_______,
+        _______ ,KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,_______,_______,               KC_MS_WH_DOWN,KC_MS_BTN1,KC_MS_BTN2,KC_MS_BTN3,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______,        _______,_______,_______,_______,KC_MS_ACCEL0,KC_MS_ACCEL2,_______,
+                                     _______ ,_______,_______,            _______,_______,_______
+  ),
 
 };
+
+
 
 #ifdef AUDIO_ENABLE
 float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
@@ -129,31 +126,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _MOUSE);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _MOUSE);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _MOUSE);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _MOUSE);
       }
       return false;
       break;
-    case ADJUST:
+    case MOUSE:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on(_MOUSE);
+        update_tri_layer(_LOWER, _RAISE, _MOUSE);
       } else {
-        layer_off(_ADJUST);
+        layer_off(_MOUSE);
+        update_tri_layer(_LOWER, _RAISE, _MOUSE);
       }
       return false;
       break;
+
   }
   return true;
 }
