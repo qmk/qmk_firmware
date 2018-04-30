d0db0b7fad53e90681656e9f37bd9f2aba619f75
Lei Yu <yuleiz@gmail.com>
2018-04-30 21:31:54 +0800

    add wm7

---
+++ keyboards/wm7/config.h
@@ -0,0 +1,83 @@
+/*
+Copyright 2017 Lei Yu <yuleiz@gmail.com>
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+
+#define VENDOR_ID       0xBEEF
+#define PRODUCT_ID      0x1949
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    WolfMark 
+#define PRODUCT         WM7
+#define DESCRIPTION     qmk keyboard firmware for wm7 
+
+/* key matrix size */
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 9
+
+// Change this to how you wired your keyboard
+// COLS: Left to right, ROWS: Top to bottom
+#define MATRIX_ROW_PINS { B5, B6, C6, C7, F7, F6, F5, F4}
+#define MATRIX_COL_PINS { D0, D1, D2, D3, D4, D5, D6, D7, B4}
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+//#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL 
+
+/* define if matrix has ghost */
+//#define MATRIX_HAS_GHOST
+
+/* number of backlight levels */
+//#define BACKLIGHT_PIN F1
+//#define BACKLIGHT_LEVELS 3
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+//#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+//#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+// rgb led
+
+//#define RGBLIGHT_ANIMATION
+#define RGB_DI_PIN B0
+#define RGBLED_NUM 3
+//#define RGBLIGHT_HUE_STEP 8
+//#define RGBLIGHT_SAT_STEP 8
+//#define RGBLIGHT_VAL_STEP 8
+//#define RGB_LIGHT_TIMER
+//#define ws2812_PORTREG PORTF
+//#define ws2812_DDRREG DDRF
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
+
+#endif
---
+++ keyboards/wm7/keymaps/hoq/keymap.c
@@ -0,0 +1,32 @@
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include "wm7.h"
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _NML 0
+#define _FUN 1
+
+#define _______ KC_TRNS
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_NML] = KEYMAP(
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,            KC_BSPC, KC_ESC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC,           KC_BSLS, KC_INS, \
+  KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,  KC_DEL, \
+  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(_FUN), KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT, KC_UP, \
+  KC_LCTL,          KC_LALT, KC_SPC,  MO(_FUN),                           KC_SPC, KC_RALT,                   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT),
+
+[_FUN] = KEYMAP(
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           _______, _______, \
+  _______, RGB_TOG, RGB_MOD, RGB_VAI, RESET,   _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+  _______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, _______, \
+  _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+  _______,          _______, _______, _______,                            _______, _______,                   _______, _______, _______, _______),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+};
