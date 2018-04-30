26a0404f43eb2cf38590d4aa55e8005d7bc3608a
Doug McCaughan <juggler@gmail.com>
2018-04-30 12:16:55 -0400

    Adding bootloader definition

--- keyboards/minidox/keymaps/djuggler/rules.mk
+++ keyboards/minidox/keymaps/djuggler/rules.mk
@@ -1,3 +1,9 @@
+# Bootloader
+#     This definition is optional, and if your keyboard supports multiple bootloaders of
+#     different sizes, comment this out, and the correct address will be loaded
+#     automatically (+60). See bootloader.mk for all options.
+BOOTLOADER = caterina
+
 RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 
 ifndef QUANTUM_DIR
