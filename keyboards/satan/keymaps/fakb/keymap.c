cd194786b69d27c3c2d22014bd6698c56987b79e
Mobi <mobi@couch.db>
2018-05-01 21:03:02 +0200

    removed duplicate layout KEYMAP_FAKB and pointed keymap.c to default on

--- keyboards/satan/keymaps/fakb/keymap.c
+++ keyboards/satan/keymaps/fakb/keymap.c
@@ -20,14 +20,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  LSFT_MA,KC_NUBS,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,RSFT_MA,MO(1)  ,\
  XXXXXXX,KC_LALT,KC_LGUI,                          SPCE_MA,                      KC_RGUI,KC_RALT,XXXXXXX,XXXXXXX),
 
-[1] = KEYMAP_FAKB(
+[1] = KEYMAP(
  KC_PSCR,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_INS ,_______,\
  _______,_______,_______,KC_PGUP,KC_PGDN,KC_HOME,KC_END ,KC_PGDN,KC_PGUP,_______,_______,_______,_______,KC_DEL ,\
  KC_CAPS,KC_NUBS,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,_______,_______,\
  _______,_______,BL_TOGG,BL_INC ,BL_DEC ,_______,_______,KC_VOLD,KC_VOLU,KC_MUTE,_______,_______,_______,_______,\
  _______,_______,_______,                _______,                                _______,_______,_______,_______),
 
-[2] = KEYMAP_FAKB(
+[2] = KEYMAP(
  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET  ,\
  KC_BTN5,KC_BTN4,KC_BTN3,KC_BTN2,KC_BTN1,KC_HOME,KC_END ,_______,_______,_______,_______,_______,_______,_______,\
  _______,_______,KC_WH_L,KC_WH_U,KC_WH_D,KC_WH_R,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,_______,_______,\
