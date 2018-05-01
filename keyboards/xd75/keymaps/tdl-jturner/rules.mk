a24ac9c85d7febab18fa9901836c43b4f9f9519e
Josh Turner <turner.josh@gmail.com>
2018-05-01 08:37:43 -0700

    Corrected header

--- keyboards/xd75/keymaps/tdl-jturner/rules.mk
+++ keyboards/xd75/keymaps/tdl-jturner/rules.mk
@@ -1,4 +1,4 @@
-# Copyright 2013 Jun Wako <wakojun@gmail.com>
+# Copyright 2018 Josh Turner (/u/tdl-jturner)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
