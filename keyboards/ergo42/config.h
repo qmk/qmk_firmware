3c0178c2e81fcc0c34d15e850223a0bdfeac2a23
p <_>
2018-05-02 18:52:35 +0900

    Tweak mouse movement speed

--- keyboards/ergo42/config.h
+++ keyboards/ergo42/config.h
@@ -25,9 +25,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MOUSEKEY_DELAY             32
 #define MOUSEKEY_INTERVAL          16
-#define MOUSEKEY_MAX_SPEED         9
-#define MOUSEKEY_TIME_TO_MAX       9
-#define MOUSEKEY_WHEEL_MAX_SPEED   1
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 160
+
+#define MOUSEKEY_MOVE_DELTA        3
+#define MOUSEKEY_MAX_SPEED         10 /* times faster than MOVE_DELTA */
+#define MOUSEKEY_TIME_TO_MAX       10
+
+#define MOUSEKEY_WHEEL_DELTA       1
+#define MOUSEKEY_WHEEL_MAX_SPEED   1 /* times faster */
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
 
 #endif
