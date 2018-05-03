26d48a2c5bb359f6e4a0d29dd832db2359d33419
lbibass <notlbibass@gmail.com>
2018-05-02 21:07:59 -0400

    changed some more

--- keyboards/ut47/keymaps/lbibass/config.h
+++ keyboards/ut47/keymaps/lbibass/config.h
@@ -20,5 +20,12 @@
 #include "config_common.h"
 
 // place overrides here
+#undef TAPPING_TERM
 #define TAPPING_TERM 200
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_INTERVAL          0
+#define MOUSEKEY_MAX_SPEED   2
+#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_WHEEL_MAX_SPEED   1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 120
 #endif
--- keyboards/ut47/keymaps/lbibass/keymap.c
+++ keyboards/ut47/keymaps/lbibass/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LGUI,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   KC_ESC, RGB_TOG, KC_LALT, KC_LCTL,LOWER, KC_SPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_WINDOWS] = LAYOUT( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  TD(TD_SHIFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   KC_ESC ,RGB_TOG, KC_LGUI, KC_LALT,  LOWER,     KC_SPC,        RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
@@ -150,9 +150,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, TD(CT_media), KC_VOLD, KC_VOLU, KC_MFFD \
 ),
 [_MOUSE] = LAYOUT(\
-  KC_ESC,  KC_MS_BTN1, KC_MS_U, KC_MS_BTN2, KC_MYCM, _______, _______, _______, _______, _______, KC_PSCR, _______,\
-  _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, _______, _______, _______, _______,\
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+  KC_ESC,  KC_MS_BTN1, KC_MS_U, KC_MS_BTN2, KC_MS_WH_UP, _______, _______, _______, _______, _______, KC_PSCR, _______,\
+  _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______,\
+  _______, KC_MS_ACCEL0, KC_MS_ACCEL1, KC_MS_ACCEL2, _______, _______, _______, _______, _______, _______, _______, _______,\
   _______, _______, _______, _______, _______,     _______,      _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
 ),
 /* Adjust (Lower + Raise)
@@ -248,5 +248,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-  return true;
-};
+    }
+      return true;
+}
