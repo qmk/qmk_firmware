c631bdf59fdc6ccfc169639b4bd24208a8dc3f7d
David Friedrich <david.friedrich@digimapas.cl>
2018-05-03 12:32:49 -0300

    still testing double tab

--- keyboards/iris/keymaps/impstyle/keymap.c
+++ keyboards/iris/keymaps/impstyle/keymap.c
@@ -9,8 +9,6 @@ extern keymap_config_t keymap_config;
 #define _LOWER 1
 #define _RAISE 2
 #define _MOUSE 3
-#define ONESHOT_TAP_TOGGLE 2 // double tab
-
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
