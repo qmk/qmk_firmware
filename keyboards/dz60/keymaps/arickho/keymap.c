b0515039b720d0374d9bf1aae70959751526d5d7
Jordan Garcia <arickho@gmail.com>
2018-05-04 09:59:26 -0400

    Light layer on middle space bar

--- keyboards/dz60/keymaps/arickho/keymap.c
+++ keyboards/dz60/keymaps/arickho/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
         MO(_UTIL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSHIFT, _DEAD_, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSHIFT, MT(MOD_RSFT, KC_UP), KC_RSHIFT,
-        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, MO(_MOUSE), MO(_LIGHTS), KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_LIGHTS), KC_SPC, MO(_MOUSE), MO(_LIGHTS), KC_LEFT, KC_DOWN, KC_RGHT),
 
     /* Utility Layer
     * ,-----------------------------------------------------------------------------------------.
