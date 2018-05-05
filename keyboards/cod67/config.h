c10d6e39c404f0fa871336600d9ed0e73501a45e
mechmerlin <mechmerlin@gmail.com>
2018-05-05 14:20:51 -0700

    mock the pins and keymap for now

--- keyboards/cod67/config.h
+++ keyboards/cod67/config.h
@@ -42,8 +42,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { D0, D1, D2, D3, D4 }
+#define MATRIX_COL_PINS { F0, F1, F2, F3, F4, F5, F6, F7, B0, B2, A0, A1, A1, A2 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
