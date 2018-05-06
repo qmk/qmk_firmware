89ab5b9e66a5bb30109f36d8be90b0e1233db098
Daniel Levett <dlevett@hotmail.co.uk>
2018-05-06 16:47:28 +0100

    Update Iris DVORAK keymap with lighting

--- keyboards/iris/keymaps/dvorak/keymap.c
+++ keyboards/iris/keymaps/dvorak/keymap.c
@@ -4,68 +4,7 @@
 
 extern keymap_config_t keymap_config;
 
-// TODO: UNICODEMAP_ENABLE , make emoji layer
-// TODO: Leader keys
-// TODO: shift to be held on tap
-
-#define _DVORAK 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
-
-enum custom_keycodes {
-	DVORAK = SAFE_RANGE,
-	LOWER,
-	RAISE,
-	ADJUST
-};
-
-#include "dist/_keymap.c"
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-
-    case DVORAK:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_DVORAK);
-      }
-      return false;
-
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-      } else {
-        layer_off(_LOWER);
-      }
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      return false;
-
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-      } else {
-        layer_off(_RAISE);
-      }
-      update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      return false;
-
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-
-  }
-  return true;
-}
-
+// Colours
 #define rgblight_set_blue        rgblight_sethsv (0xFF,  0xFF, 0xFF);
 #define rgblight_set_red         rgblight_sethsv (0x00,  0xFF, 0xFF);
 #define rgblight_set_green       rgblight_sethsv (0x78,  0xFF, 0xFF);
@@ -76,31 +15,98 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define rgblight_set_purple      rgblight_sethsv (0x10E, 0xFF, 0xFF);
 #define rgblight_set_white       rgblight_sethsv (0x00,  0x00, 0xFF);
 
-void matrix_init_user(void) {
-#ifdef RGBLIGHT_ENABLE
-  rgblight_disable();
-#endif
-}
+// Layers
+#define _DVORAK 0
+#define _LOWER 1
+#define _RAISE 2
+
+enum custom_keycodes {
+	DVORAK = SAFE_RANGE,
+	LOWER,
+	RAISE,
+	ADJUST
+};
+
+// Keycodes
+#define KC_LOWERSPACE LT(_LOWER, KC_SPACE)
+#define KC_RAISESPACE LT(_RAISE, KC_SPACE)
+
+// Tap dance sequences
+enum {
+  SHIFT_CAPS = 0
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSHIFT, KC_CAPSLOCK)
+};
+
+// State variables
+bool state_is_shift_held = false;
+bool state_is_caps_lock = false;
+static uint32_t state_current = 0;
+
+#include "dist/_keymap.c"
 
 uint32_t layer_state_set_user(uint32_t state) {
-#ifdef RGBLIGHT_ENABLE
-    switch (biton32(state)) {
-    case _RAISE:
-      rgblight_set_blue;
-      rgblight_enable();
-      break;
+  state_current = state;
+
+  if (state_is_caps_lock) {
+    rgblight_set_red;
+    return state;
+  }
+
+  if (state_is_shift_held) {
+    rgblight_set_yellow;
+    return state;
+  }
+
+  switch (biton32(state)) {
+
     case _LOWER:
       rgblight_set_orange;
-      rgblight_enable();
       break;
-    case _ADJUST:
-      rgblight_set_green;
-      rgblight_enable();
+
+    case _RAISE:
+      rgblight_set_teal;
       break;
-    default: //  for any other layers, or the default layer
-      rgblight_disable();
+
+    default:
+      //rgblight_disable();
+      rgblight_set_white;
+      rgblight_mode(1);
       break;
-    }
-#endif
+
+  }
+
   return state;
+}
+
+void led_set_kb(uint8_t usb_led) {
+  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+    state_is_caps_lock = true;
+  }else{
+    state_is_caps_lock = false;
+  }
+  layer_state_set_user(state_current);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+
+    case KC_LSHIFT:
+    case TD(SHIFT_CAPS):
+      if (record->event.pressed) {
+        state_is_shift_held = true;
+      } else {
+        state_is_shift_held = false;
+      }
+      break;
+
+  }
+
+  // Trigger layer state change for all keys, so modifier keys
+  // and caps lock can trigger backlight changes.
+  layer_state_set_user(state_current);
+
+  return true;
 }
