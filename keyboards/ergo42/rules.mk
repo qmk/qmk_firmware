36fb754399d382c33ea5b0723b98ad980dfe24bd
p <_>
2018-05-07 19:21:42 +0900

    Add KC_LOCK key

--- keyboards/ergo42/rules.mk
+++ keyboards/ergo42/rules.mk
@@ -59,6 +59,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+KEY_LOCK_ENABLE = yes       # KC_LOCK to auto-hold a key
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
