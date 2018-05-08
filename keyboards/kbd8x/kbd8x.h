970c137791eec9115c54ada6bcaea3fe76968e0c
mechmerlin <mechmerlin@gmail.com>
2018-05-07 20:26:08 -0700

    map out the non confusing part of the matrix

--- keyboards/kbd8x/kbd8x.h
+++ keyboards/kbd8x/kbd8x.h
@@ -19,16 +19,15 @@
 #include "quantum.h"
 
 // This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
 #define LAYOUT( \
-    K00, K01, K02, K03,  \
-    K10, K11  K12, K13,  \
-    K20, K21, K22, K23, \
-    K30, K31, K32, K33, \
-    K40, K41, K42, K43, \
-    K50, K51, K52, K53, \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A,   \
+    K10, K11  K12, K13, K14, K15, K16, K17, K18, K19, K1A,  \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A,  \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A,     \
+    K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A,  \
+    K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, K5A, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
 ) \
 { \
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E }, \
