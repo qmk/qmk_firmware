e1598a5a4c95334f968fde8d782f20bf3003431e
mechmerlin <mechmerlin@gmail.com>
2018-05-07 21:59:25 -0700

    fix some stupid compile errors

--- keyboards/kbd8x/config.h
+++ keyboards/kbd8x/config.h
@@ -60,6 +60,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+#endif
 
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
