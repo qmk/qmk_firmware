41195b326b520943b9339c8480eabca12b6a1a09
Erez Zukerman <bulk@ezuk.org>
2018-05-09 09:43:36 -0400

    a quick attempt

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -82,6 +82,7 @@ UNICODE_ENABLE   = yes # Unicode
 SWAP_HANDS_ENABLE= yes # Allow swapping hands of keyboard
 SLEEP_LED_ENABLE = no
 API_SYSEX_ENABLE = no
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 
 LAYOUTS = ergodox
