afacd42368e0dc7627a695508f15598b38429c63
yiancar <yiangosyiangou@cytanet.com.cy>
2018-05-09 04:23:21 +0100

    Add effect speed support for RGB Matrix *No EEPROM yet*  (#2922)
    * Added Modular keyboards L,R and NUM
    
    Created code modules for the 3 modules of the modular keyboard.
    Original idea by MechboardsUK. Uses i2c implementation similar to lets
    split
    
    * Remove modular from master
    
    This is to fix incorrect branching
    
    * Add effect speed support for RGB Matrix *No eeprom yet*
    
    Keycodes RGB_SPI and RGB_SPD have been added to increase and decrease effect speed.
    
    Speed is not saved in EEPROM yet as per Jack's request.
    
    * Update rgb_matrix.c
    
    * RGB Matrix speed fix rgblight.h
    
    * More fixes for rgb speed. Speed functions declared but not used in rgblight
    
    * More travis fixes..
    
    * Another one for travis..

--- quantum/rgb_matrix.h
+++ quantum/rgb_matrix.h
@@ -58,6 +58,7 @@ typedef union {
     uint16_t hue     :9;
     uint8_t  sat     :8;
     uint8_t  val     :8;
+    uint8_t  speed   :8;//EECONFIG needs to be increased to support this
   };
 } rgb_config_t;
 
@@ -129,6 +130,8 @@ void rgblight_increase_sat(void);
 void rgblight_decrease_sat(void);
 void rgblight_increase_val(void);
 void rgblight_decrease_val(void);
+void rgblight_increase_speed(void);
+void rgblight_decrease_speed(void);
 void rgblight_mode(uint8_t mode);
 uint32_t rgblight_get_mode(void);
 
