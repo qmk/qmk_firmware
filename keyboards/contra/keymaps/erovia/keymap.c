64b038796eb7cb0d1453b90f72a354cbb2d39af3
Erovia <erovia@users.noreply.github.com>
2018-05-09 22:15:16 +0200

    Enable tap dancing and implement Space Cadet-like shift with LSHIFT and ENTER

--- keyboards/contra/keymaps/erovia/keymap.c
+++ keyboards/contra/keymaps/erovia/keymap.c
@@ -34,6 +34,16 @@ enum planck_keycodes {
   RAISE
 };
 
+enum {
+  TD_SPACE_CADET_SHIFT = 0,
+  TD_SPACE_CADET_ENTER = 1
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_SPACE_CADET_SHIFT] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_LPRN),
+  [TD_SPACE_CADET_ENTER] = ACTION_TAP_DANCE_DOUBLE(KC_ENT, KC_RPRN)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -50,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
   {KC_GRV,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_ESC, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {TD(TD_SPACE_CADET_SHIFT), KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_SPACE_CADET_ENTER) },
   {KC_LCTL,  KC_LGUI, KC_LALT, KC_LGUI, LOWER,   KC_TAB,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
