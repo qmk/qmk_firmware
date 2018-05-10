45dd6ff26458c0d0c6df39a274cfce84febc2aea
Sameeh Jubran <sameeh.j@gmail.com>
2018-05-11 00:18:09 +0300

    Dactylmanuform: dvorak: Add page up and down buttons
    The buttons are supposedly in the commented graphical keymap but
    not in the actual array.
    
    Signed-off-by: Sameeh Jubran <sameeh.j@gmail.com>

--- keyboards/handwired/dactyl_manuform/keymaps/dvorak/keymap.c
+++ keyboards/handwired/dactyl_manuform/keymaps/dvorak/keymap.c
@@ -67,13 +67,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_UPPER] = LAYOUT( \
-  KC_TRNS,  KC_TRNS,    KC_MS_UP,   KC_TRNS,     KC_TRNS, KC_VOLU, KC_TRNS,  KC_UP,      KC_TRNS,    KC_TRNS, \
-  KC_TRNS,  KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS, KC_MUTE, KC_LEFT,  KC_DOWN,    KC_RIGHT,   KC_TRNS, \
-  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS, KC_VOLD, KC_SLSH,  KC_BSLS,    KC_QUES,    KC_PIPE, \
-            KC_TRNS,    KC_TRNS,                                             KC_MS_BTN1, KC_MS_BTN2,          \
-                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            \
-                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            \
-                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS                                             \
+  KC_TRNS,  KC_TRNS,    KC_MS_UP,   KC_TRNS,     KC_TRNS, KC_VOLU, KC_TRNS,  KC_UP,      KC_TRNS,    KC_PGUP,   \
+  KC_TRNS,  KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS, KC_MUTE, KC_LEFT,  KC_DOWN,    KC_RIGHT,   KC_PGDOWN, \
+  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS, KC_VOLD, KC_SLSH,  KC_BSLS,    KC_QUES,    KC_PIPE,   \
+            KC_TRNS,    KC_TRNS,                                             KC_MS_BTN1, KC_MS_BTN2,            \
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                              \
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                              \
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS                                               \
 ),
 /* Lower
  * ,----------------------------------,                             ,----------------------------------,
