e903a01dce67f324ba0e9d22311c2410d75b223a
Josh McFadden <josh@visionistinc.com>
2018-05-11 15:53:12 -0400

    Turning on backlight support and mapping backlight keys in the adjust layer.

--- keyboards/planck/keymaps/josh/keymap.c
+++ keyboards/planck/keymaps/josh/keymap.c
@@ -3,7 +3,6 @@
 
 #include "planck.h"
 #include "action_layer.h"
-//#define AUDIO_ENABLE
 #ifdef AUDIO_ENABLE
   #include "audio.h"
 #endif
@@ -123,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = {
   {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL },
   {_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
+  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, BL_TOGG, BL_DEC,  BL_INC,  BL_BRTG},
   {_______, _______, _______, _______, _______, _______, _______, _______, CLICKY_TOGGLE, CLICKY_DOWN, CLICKY_UP, CLICKY_RESET}
 },
 
