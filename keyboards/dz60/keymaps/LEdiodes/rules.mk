fd3bb8ab17a94613a7eddab969205ba57e23f4ec
matthew.kerfoot@virteva.com <matthew.kerfoot@virteva.com>
2018-05-11 11:47:48 -0500

    updated formating and sorting of enabled vs disabled build options.

--- keyboards/dz60/keymaps/LEdiodes/rules.mk
+++ keyboards/dz60/keymaps/LEdiodes/rules.mk
@@ -36,23 +36,19 @@ F_USB = $(F_CPU)
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
-
 # Boot Section Size in *bytes*
 OPT_DEFS += -DBOOTLOADER_SIZE=4096
 
-
 # Build Options
-#   comment out to disable the options.
-#
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
-CONSOLE_ENABLE = no	# Console for debug(+400)
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+MOUSEKEY_ENABLE = yes	  # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes  	# Audio control and System control(+450)
+NKRO_ENABLE = yes	    	# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = yes   # enabled RGB underglow
 AUTO_SHIFT_ENABLE = yes # If the time depressed is greater than or equal to the AUTO_SHIFT_TIMEOUT, then a shifted version of the key is emitted. If the time is less than the AUTO_SHIFT_TIMEOUT time, then the normal state is emitted
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = yes  # enabled tap dancing
+AUDIO_ENABLE = no       # not applicable for dz60
+SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
+CONSOLE_ENABLE = no	    # Console for debug(+400)
+COMMAND_ENABLE = no     # Commands for debug and configuration
