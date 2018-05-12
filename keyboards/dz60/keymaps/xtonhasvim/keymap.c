1f44d0bf320a99811243ba3988e03416795a0c42
Xton <cdewan@apple.com>
2018-05-12 14:05:46 -0700

    don't actually need the second move trigger and it makes typing less responsive.

--- keyboards/dz60/keymaps/xtonhasvim/keymap.c
+++ keyboards/dz60/keymaps/xtonhasvim/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
 	  KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-		LCTL_T(KC_ESC), LT(_MOVE,KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(_MOVE,KC_SCLN), KC_QUOT, KC_ENT,
+		LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(_MOVE,KC_SCLN), KC_QUOT, KC_ENT,
 		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_NO,
 		KC_LCTL, KC_LALT, KC_LGUI, VIM_START, TG(_MOUSE), KC_SPC, KC_RGUI, KC_RALT, X_____X, KC_RCTL, MO(_FUN)),
 
@@ -56,15 +56,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		TO(_QWERTY), _______, _______, TO(_QWERTY), X_____X, X_____X, _______, _______, _______, _______, _______),
 
 };
-
-void led_set_user(uint8_t usb_led) {
-    // if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-    //     DDRB |= (1 << 2); PORTB &= ~(1 << 2);
-    // } else {
-    //     DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
-    // }
-}
-
 
 #define LED_BIT 1 << 2
 #define LED_MASK ~(1 << 2)
