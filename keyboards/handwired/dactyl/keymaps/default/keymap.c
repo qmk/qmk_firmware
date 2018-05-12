c50ab89c7057f3e73255ecef59dbd116f4580ee5
Brian Dawn <brian.t.dawn@gmail.com>
2018-05-11 20:23:04 -0500

    More dactyl changes.

--- keyboards/handwired/dactyl/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl/keymaps/default/keymap.c
@@ -34,12 +34,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               |      |ace   | End  |  | PgDn |      |      |
  *                               `--------------------'  `--------------------'
  */
+	
 [BASE] = LAYOUT_dactyl(  // layer 0 : default
         // left hand
           KC_1,           KC_2,     KC_3,     KC_4,  KC_5,          KC_EQL,
           KC_Q,           KC_W,     KC_E,     KC_R,  KC_T,         KC_TAB,
-          KC_A,           KC_S,     KC_D,     KC_F,  KC_G,         KC_BSPC,
-   CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,         KC_LSFT,
+          KC_A,           KC_S,     KC_D,     KC_F,  KC_G,         CTL_T(KC_ESC),
+          KC_Z,           KC_X,     KC_C,     KC_V,  KC_B,         KC_LSFT,
        KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT,                KC_TILD,
                                                                       ALT_T(KC_APP),  KC_LGUI,
                                                                                       KC_SPC,
@@ -48,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
                              KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
                              KC_H,   KC_J,     KC_K,     KC_L,  LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
-                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSFT,
+                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_GRV,
                                     KC_UP,  KC_DOWN,  KC_LBRC,            KC_RBRC,          KC_FN1,
           KC_RALT,  CTL_T(KC_ESC),
           KC_PGUP,
