5636ba45e5db975b0e4fd86792618b918398a081
yiancar <yiangosyiangou@cytanet.com.cy>
2018-05-15 15:19:26 +0100

    More Cleanup for HS60

--- keyboards/hs60/hs60.h
+++ keyboards/hs60/hs60.h
@@ -21,9 +21,7 @@
 #include "quantum.h"
 
 // This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
-// The first section contains all of the arguments
-// The second converts the arguments into a two-dimensional array
+
 #define KEYMAP( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      \
