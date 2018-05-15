c097998b3a97a07a0577a0915ae5ee6b98f37083
yiancar <yiangosyiangou@cytanet.com.cy>
2018-05-15 16:16:30 +0100

    HS60 modifications to work with configurator

--- keyboards/hs60/rules.mk
+++ keyboards/hs60/rules.mk
@@ -58,8 +58,8 @@ OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -73,6 +73,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+LAYOUTS = 60_ansi 60_iso    # Define standard layouts
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
 
 # Experimental features for zealcmd please do no enable
