19da26cfafc44d1d8131b06f4e18d63dc46a007c
yiancar <yiangosyiangou@cytanet.com.cy>
2018-05-15 15:08:39 +0100

    Update config.h

--- keyboards/hs60/config.h
+++ keyboards/hs60/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0257
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Yiancar-Designs
-#define PRODUCT         Sixty Sockets
+#define PRODUCT         HS60
 #define DESCRIPTION     GH60 compatible, tool free RGB keyboard
 
 /* key matrix size */
