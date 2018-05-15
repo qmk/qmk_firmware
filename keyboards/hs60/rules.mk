a367a1dc66c53fe75d0df2a83eb5927b0581c0ec
yiancar <yiangosyiangou@cytanet.com.cy>
2018-05-15 16:31:41 +0100

    More HS60 cleanup

--- keyboards/hs60/rules.mk
+++ keyboards/hs60/rules.mk
@@ -73,7 +73,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-LAYOUTS = 60_ansi 60_iso    # Define standard layouts
+LAYOUTS = 60_iso            # Define standard layouts
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
 
 # Experimental features for zealcmd please do no enable
