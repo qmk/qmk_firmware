12bdbd442df1d693d0e3766e8e998ec750071fd3
Dan Herrera <Daniel_Herrera@ultimatesoftware.com>
2018-05-15 18:47:37 -0400

    Implement majority of DAD layout

--- keyboards/chimera_ortho/keymaps/dad/keymap.c
+++ keyboards/chimera_ortho/keymaps/dad/keymap.c
@@ -1,191 +1,115 @@
 #include "chimera_ortho.h"
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
+#define TAPPING_TOGGLE 2
+
 enum chimera_ortho_layers
 {
 	_BASE,
-	_CAPS,
-	_NUMPAD,
-	_SYMBOLS,
-	_MACROS,
-	_NAV
+	_NAV,
+	_SYM,
+	_FUNC,
+	_MOUSE
 };
 
-#define KC_NMPD TG(_NUMPAD)
-#define KC_SYMB TG(_SYMBOLS)
-#define KC_SPFN LT(_NAV,KC_EQL)
-#define KC_SCTL MT(MOD_LCTL, KC_LBRC)
-#define KC_SCTR MT(MOD_LCTL, KC_RBRC)
-#define KC_SPLT MT(MOD_LALT, KC_MINS)
-#define KC_SPRT MT(MOD_LALT, KC_1)
-#define KC_GBRC MT(MOD_RGUI, KC_8)
-#define KC_GQOT MT(MOD_LGUI, KC_QUOT)
-#define KC_MESC LT(_MACROS, KC_ESC)
-#define KC_INCL M(0)
-#define KC_PULL M(1)
-#define KC_PUSH M(2)
-#define KC_SCAP M(3)
-#define KC_SCOF M(4)
-#define KC_CAD LALT(LCTL(KC_DEL))
+#define KC_NAV MO(_NAV)
+#define KC_SYM MO(_SYM)
+#define KC_FUNC MO(_FUNC)
+#define KC_MOUSE TT(_MOUSE)
 
 #define KC_DELSHFT SFT_T(KC_DEL)
 
-#define LONGPRESS_DELAY 150
-//#define LAYER_TOGGLE_DELAY 300
+#define KC_WK_LEFT LCA(KC_LEFT)
+#define KC_WK_DOWN LCA(KC_DOWN)
+#define KC_WK_UP LCA(KC_UP)
+#define KC_WK_RGHT LCA(KC_RGHT)
 
-// Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+#define KC_QUAKE LCTL(KC_GRAVE)
+
+#define LONGPRESS_DELAY 150
+
+#define KC_RESET RESET
 #define KC_ KC_TRNS 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = KC_KEYMAP(
       //,-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------.
-         LALT   ,TAB    ,QUOT   ,COMM   ,DOT    ,P      ,Y           ,F      ,G      ,C      ,R      ,L      ,SLSH   ,       
+         LALT   ,TAB    ,QUOT   ,COMM   ,DOT    ,P      ,Y           ,F      ,G      ,C      ,R      ,L      ,SLSH   ,FUNC   
       //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
-        ,LGUI   ,ESC    ,A      ,O      ,E      ,U      ,I           ,D      ,H      ,T      ,N      ,S      ,MINS   ,       
+        ,LGUI   ,ESC    ,A      ,O      ,E      ,U      ,I           ,D      ,H      ,T      ,N      ,S      ,MINS   ,MOUSE  
       //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
         ,LCTL   ,DELSHFT,SCLN   ,Q      ,J      ,K      ,X           ,B      ,M      ,W      ,V      ,Z      ,BSPC   ,ENTER
       //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
-                                        ,       ,LSHIFT                      ,SPC    ,    
+                                        ,NAV    ,LSHIFT                      ,SPC    ,SYM 
       //\-------------------------------+-------+-------+-------/    \-------+-------+---------------------------------------/
   ),
 
-  [_CAPS] = KC_KEYMAP(  
-  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
-         ,    ,    ,    ,    ,    ,    ,          ,    ,    ,    ,    ,    ,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,UNDS,          ,    ,    ,    ,    ,COLN,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-     SCOF,    ,    ,    ,    ,    ,    ,          ,    ,    ,    ,    ,    ,SCOF,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-                             ,    ,                    ,    
-  // \------------------+----+----+---/       \---+----+----+-------------------/
+  [_NAV] = KC_KEYMAP(
+      //,-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------.
+                ,       ,       ,       ,       ,       ,            ,       ,HOME   ,PGDOWN ,PGUP   ,END    ,       ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,PSCR   ,MENU   ,       ,       ,       ,            ,       ,LEFT   ,DOWN   ,UP     ,RIGHT  ,       ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,CAPS   ,NLCK   ,INS    ,       ,       ,            ,       ,WK_LEFT,WK_DOWN,WK_UP  ,WK_RGHT,       ,     
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+                                        ,       ,                            ,       ,    
+      //\-------------------------------+-------+-------+-------/    \-------+-------+---------------------------------------/
   ),
 
-  [_NUMPAD] = KC_KEYMAP(  
-  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
-         ,    ,COLN ,    ,    ,    ,    ,          ,    , 7  , 8  , 9  ,ASTR,MINS,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         ,    ,DOT ,    ,    ,    ,    ,          ,    , 4  , 5  , 6  ,PLUS,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,    ,          ,    , 1  , 2  , 3  ,SLSH,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-                             ,    ,                     , 0  
-  // \------------------+----+----+---/       \---+----+----+-------------------/
+  [_SYM] = KC_KEYMAP(
+      //,-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------.
+                ,QUAKE  ,GRAVE  ,TILDE  ,BSLASH ,PIPE   ,LPRN        ,RPRN   ,7      ,8      ,9      ,SLSH   ,EQUAL  ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,       ,EXLM   ,AT     ,HASH   ,DLR    ,LCBR        ,RCBR   ,4      ,5      ,6      ,ASTR   ,PLUS   ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,       ,PERC   ,CIRC   ,AMPR   ,ASTR   ,LBRC        ,RBRC   ,1      ,2      ,3      ,MINUS  ,       ,     
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+                                        ,       ,                            ,0      ,    
+      //\-------------------------------+-------+-------+-------/    \-------+-------+---------------------------------------/
   ),
 
-  [_SYMBOLS] = KC_KEYMAP(  
-  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
-         ,EXLM, AT ,HASH,DLR ,PERC,    ,          ,CIRC,AMPR,ASTR,LPRN,RPRN,BSLS,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         , F1 , F2 , F3 , F4 , F5 ,    ,          ,TILD,COLN,UNDS,LCBR,RCBR,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         , F6 , F7 , F8 , F9 ,F10 ,    ,          ,GRV ,SCLN,MINS,LBRC,RBRC,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-                         PIPE,    ,                    ,
-  // \------------------+----+----+---/       \---+----+----+-------------------/
+  [_FUNC] = KC_KEYMAP(
+      //,-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------.
+                ,RESET  ,SLEP   ,MRWD   ,MPLY   ,MFFD   ,            ,       ,F9     ,F10    ,F11    ,F12    ,       ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,       ,PWR    ,MUTE   ,VOLD   ,VOLU   ,            ,       ,F5     ,F6     ,F7     ,F8     ,       ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,       ,WAKE   ,MPRV   ,MPLY   ,MNXT   ,            ,       ,F1     ,F2     ,F3     ,F4     ,       ,     
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+                                        ,       ,                            ,       ,    
+      //\-------------------------------+-------+-------+-------/    \-------+-------+---------------------------------------/
   ),
 
-  [_NAV] = KC_KEYMAP(  
-  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
-         ,    ,    ,    ,    ,    ,    ,          ,    ,    , UP ,    ,PSCR,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,    ,          ,    ,LEFT,DOWN,RGHT,    ,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,    ,          ,    ,PGUP,PGDN,    ,    ,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-                             ,DEL ,                    ,    
-  // \------------------+----+----+---/       \---+----+----+-------------------/
+  [_MOUSE] = KC_KEYMAP(
+      //,-------+-------+-------+-------+-------+-------+-------.    ,-------+-------+-------+-------+-------+-------+-------.
+                ,       ,       ,       ,       ,       ,            ,       ,       ,       ,       ,       ,       ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,       ,       ,       ,       ,       ,            ,       ,       ,       ,       ,       ,       ,       
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+        ,       ,       ,       ,       ,       ,       ,            ,       ,       ,       ,       ,       ,       ,     
+      //|-------+-------+-------+-------+-------+-------+-------|    |-------+-------+-------+-------+-------+-------+-------|
+                                        ,       ,                            ,       ,    
+      //\-------------------------------+-------+-------+-------/    \-------+-------+---------------------------------------/
   ),
-
-  [_MACROS] = KC_KEYMAP(  
-  //,----+----+----+----+----+----+----.     ,----+----+----+----+----+----+----.
-         ,    ,    ,    ,    ,    ,    ,          ,    ,    ,INCL,    ,    ,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-         ,    ,    ,CAD ,    ,    ,    ,          ,    ,    ,    ,    ,    ,    ,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-     SCAP,    ,    ,    ,    ,    ,    ,          ,    ,    ,PULL,PUSH,    ,SCAP,
-  //|----+----+----+----+----+----+----|     |----+----+----+----+----+----+----|
-                             ,    ,                    ,    
-  // \------------------+----+----+---/       \---+----+----+-------------------/
-  )
-
 };
-
-
-const uint16_t PROGMEM fn_actions[] = {
-
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  switch(id) {
-    /* include some kind of library or header */
-    case 0:
-      if (record->event.pressed) {
-        SEND_STRING("#include <>");
-        return MACRO( T(LEFT), END);
-      }
-      break;
-    case 1:
-      if (record->event.pressed) {
-        SEND_STRING("git pull");
-        return MACRO( T(ENT), END );
-      }
-      break;
-    case 2:
-      if (record->event.pressed){
-        SEND_STRING("git push");
-        return MACRO( T(ENT), END );
-      }
-      break;
-    case 3:
-      if (record->event.pressed){
-        layer_on(_CAPS);
-        register_code(KC_CAPSLOCK);
-        unregister_code(KC_CAPSLOCK);
-      }
-      break;
-    case 4:
-      if (record->event.pressed){
-        layer_off(_CAPS);
-        register_code(KC_CAPSLOCK);
-        unregister_code(KC_CAPSLOCK);
-      }
-      break;
-  }
-  return MACRO_NONE;
-};
- 
 
 void matrix_scan_user(void) {
     uint8_t layer = biton32(layer_state);
     
     switch (layer) {
-    	case _BASE:
-    	    set_led_green;
-    	    break;
-        case _CAPS:
-	    set_led_white;
-	    break;
-        case _NUMPAD:
-            set_led_blue;
-            break;
-        case _SYMBOLS:
-            set_led_red;
+        case _BASE:
+            set_led_green;
             break;
         case _NAV:
-	    set_led_magenta;
-	    break;
-        case _MACROS:
-	    set_led_cyan;
-	    break;
-       default:
+            set_led_white;
+            break;
+        case _SYM:
+            set_led_blue;
+            break;
+        default:
+            set_led_red;
+            set_led_magenta;
+            set_led_cyan;
             set_led_green;
             break;
     }
