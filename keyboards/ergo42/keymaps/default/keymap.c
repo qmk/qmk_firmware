b53a2a05cbdbfb0cfbbcbef7604acec5770a1af6
p <_>
2018-05-17 14:53:04 +0900

    Replace duplicate T with EQL

--- keyboards/ergo42/keymaps/default/keymap.c
+++ keyboards/ergo42/keymaps/default/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE
  * ,------------------------------------------------.   ,------------------------------------------------.
- * |   `  |   Q  |   W  |   E  |   R  |   T  |  Y   |   |  T   |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |   `  |   Q  |   W  |   E  |   R  |   T  |  Y   |   |  =   |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
  * | Tab/ |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  '   |
  * | Ctrl |      |      |      |      |      |      |   |      |      |      |      |      |      |      |
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = KEYMAP( \
-  KC_JYEN,        KC_Q,    KC_W,   KC_E,            KC_R,             KC_T,   KC_Y,    /**/ KC_T,              KC_Y,   KC_U,                KC_I,    KC_O,    KC_P,    KC_MINS,   \
+  KC_JYEN,        KC_Q,    KC_W,   KC_E,            KC_R,             KC_T,   KC_Y,    /**/ KC_EQL,            KC_Y,   KC_U,                KC_I,    KC_O,    KC_P,    KC_MINS,   \
   LCTL_T(KC_TAB), KC_A,    KC_S,   LT(MOUSE, KC_D), KC_F,             KC_G,   S(KC_9), /**/ S(KC_0),           KC_H,   KC_J,                KC_K,    KC_L,    KC_SCLN, KC_QUOT,   \
   KC_LSFT,        KC_Z,    KC_X,   KC_C,            KC_V,             KC_B,   KC_N,    /**/ KC_B,              KC_N,   KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_BSLASH, \
   KC_LOCK,        KC_PSCR, KC_APP, KC_LGUI,         LALT_T(KC_LANG2), KC_SPC, KC_ESC,  /**/ LT(SWAP, KC_BSPC), KC_ENT, LT(RAISE, KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT   \
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,------------------------------------------------.   ,------------------------------------------------.
- * |Reest |   1  |   2  |   3  |   4  |   5  |   6  |   |   5  |   6  |   7  |   8  |   9  |   0  |   =  |
+ * |Reest |   1  |   2  |   3  |   4  |   5  |   6  |   |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * |      |   !  |   @  |   #  |   $  |   %  |   [  |   |   ]  |   ^  |   &  |   *  |   (  |   )  |   +  |
+ * |      |   !  |   @  |   #  |   $  |   %  |   [  |   |   ]  |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |   |   }  |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [RAISE] = KEYMAP( \
-  RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       /**/ KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
-  _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LBRC,    /**/ KC_RBRC,    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_EQL), \
-  _______, KC_F1,    KC_F2,  KC_F3,   KC_F4,   KC_F5,   S(KC_LBRC), /**/ S(KC_RBRC), KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    \
-  _______, _______, _______, _______, _______, _______, _______,    /**/ _______,    _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY    \
+  RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       /**/ KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LBRC,    /**/ KC_RBRC,    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, \
+  _______, KC_F1,    KC_F2,  KC_F3,   KC_F4,   KC_F5,   S(KC_LBRC), /**/ S(KC_RBRC), KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+  _______, _______, _______, _______, _______, _______, _______,    /**/ _______,    _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Mouse
