8fb45cab15a9e328ef8d7b2ff901dc70ea5aa78f
mechmerlin <mechmerlin@gmail.com>
2018-05-16 20:37:38 -0700

    Unsupport community layouts but use standard layouts

--- keyboards/hs60/hs60.h
+++ keyboards/hs60/hs60.h
@@ -22,7 +22,7 @@
 
 // This a shortcut to help you visually see your layout.
 
-#define LAYOUT_iso( \
+#define LAYOUT_60_iso( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, \
@@ -36,7 +36,7 @@
     { K40, K41, K42, XXX, XXX, XXX, K46, XXX, XXX, XXX, K4A, K4B, K4C, K4D }  \
 }
 
-#define LAYOUT_ansi( \
+#define LAYOUT_60_ansi( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D, \
