6012834ac65ff5a6c7cec6f56b5fe62cecbce256
Xyverz <xyverz@gmail.com>
2018-05-18 15:32:50 -0700

    Updating my maps in upstream master (#2987)
    * Adding Rama M10-A Macropad
    
    * ch-ch-ch changes...
    
    * Major overhaul based on SMT's keymap.
    
    * more changes.
    
    * Moved the FKeys to the ADJUST layer.
    
    * More rearranging.
    
    * Alias in Atreus62 keymap to make it more legible
    Added config.h to fix tapping_term issue for Caps Lock key in OSX
    
    * Added OrthoDox layout.
    
    * More layout changes.
    
    * Fixing things with the keyboard.
    
    * Finishing touches.
    
    Set left-hand master in config.h
    Embedded the arrow keys in keymap.c
    
    * Revised keymap making this easier to use.
    
    * additions and changes.
    
    * changes to various keymaps.
    
    * Minor adjustments to OrthoDox layout.
    
    * Added Eco keymap. Updated Let's Split keymap.
    
    * Added gherkin
    
    * Removed my M10A keymap
    
    * Planck Keymap Updates
    
    Updated my Planck keymap and created a simple keymap for Seph's Preonic.
    
    * Added readme
    
    * readme fixes
    
    * Update readme.md
    
    more clarification
    
    * Keymap Tweaks
    
    Removed the Power button setting from the keymap. It was in a
    horrible location. I'll work on getting it setup somewhere else
    sometime later.
    
    * Added Readme
    
    I finally got around to adding a readme to this keymap. I've also added minor changes to the layout.
    
    * Fixed Keymap Error
    
    * Fixed Readme
    
    * adding iris and levinson keymaps
    
    * Tweaks to keymap
    
    * added youngJZ keymap
    
    * Changes to keymap
    Added a readme.md
    
    * Levinson changes
    
    Added the readme.md and rules.mk files.
    Configured RGB underglow and backlighting.
    
    * fixed readme
    
    * changes to keymaps
    
    * Updated keymap
    
    * Updated readme.md
    
    * Updated Readme (again)
    
    * Updated Readme
    
    Fixed formatting. Again.
    
    * Updated readme
    
    This is the last readme update for this keyboard update. I hope.
    
    * Added Contra keymap
    
    * Kinesis Keymap Update
    
    * Updated Keymaps
    
    I've updated my Kinesis (Stapelberg) layout and my Clueboard 66 layout.
    I've also updated my Kinesis Readme.
    
    * Clueboard Keymap update
    
    Added media keys to my Clueboard 66 Rev2 layout.
    
    * Added keymap
    
    Added Minidox keymap & rules.
    Added user function to Let's Split keymap that turns off the red
     LEDs on the Pro Micros.
    
    * New Zen keymap
    
    Added Zen keyboard to my list of keyboards, so had to generate a new
    keymap for it.
    
    Also adding some changes to my MiniDox keymap and config.h, as well
    as my Levinson's config.h.
    
    The config.h file changes enable ee_hands.
    
    * A few changes for useability
    
    I made a few changes to the Minidox keymap to see if I can't make it more useable.
    
    I'm also working on streamlining the Zen keyboard keymap to reduce layers.
    
    * Re-vamped Iris keymap.
    
    * changes
    
    * minor keymap change
    
    This was a minor keymap change to use mod_tap for the backspace key:
    ALT when held, BSPC when tapped.
    
    * Added Fourier keymap
    
    * Keymap Cleanup
    
    Moved KC_ESC to KC_CAPS, and changed KC_ESC to KC_GRV
    This is because of muscle memory, I kept hitting ESC when trying to hit TAB.
    
    * Keymap Adjustments
    
    Swapped Caps/Esc, put Caps in Raise/Lower layers, put Grv in normal
    Esc position. Adjusted the readme.md to reflect these changes.
    
    * minor tweaks
    
    Added code to disable red ProMicro LEDs after flashing.
    
    * Clean-up
    
    * Corrections to keymap.
    
    Fixed a foul-up in the Zen keymap where the lctrl was where the LOWER
    should have been.
    
    * Changes to make this fall in line with the new Layout features
    
    * Moving to LAYOUTs for 4x12 boards
    
    * fixed config.h file
    
    * standardization changes
    
    * Reverted Atreus62 keymap to LAYOUT format
    
    * Switch Preonic and Nyquist to ortho_5x12
    
    * Corrections to config.h
    
    * config.h file tweaks
    
    * config.h file tweaks

--- keyboards/iris/keymaps/xyverz/config.h
+++ keyboards/iris/keymaps/xyverz/config.h
@@ -1,8 +1,5 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+Copyright 2017 Danny Nguyen <danny@hexwire.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,25 +18,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#include "../../config.h"
-
-/* Use I2C or Serial, not both */
+#include "config_common.h"
 
 #define USE_SERIAL
-// #define USE_I2C
+#define EE_HANDS
 
-/* Select hand configuration */
+#define PREVENT_STUCK_MODIFIERS
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
+#undef PRODUCT
+#define PRODUCT Iris Keyboard
 
+#undef MANUFACTURER
+#define MANUFACTURER Ian Sterling
 
-#ifdef SUBPROJECT_rev2
-    /* RGB Underglow */
-    #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 8
-#endif
+#undef DESCRIPTION
+#define DESCRIPTION A column-staggered split ortholinear keyboard
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
 
 #endif
