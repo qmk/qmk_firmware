d5b33fb345b8413b1fcd146a4aa1733d7d4baadb
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-18 05:27:22 -0700

    Configurator updates for AMJ Pad
    - Add `LAYOUT_all` to info.json
    - `KEYMAP` to `LAYOUT` throughout
    - `amjpad.h` to `QMK_KEYBOARD_H` throughout

--- keyboards/amjpad/amjpad.h
+++ keyboards/amjpad/amjpad.h
@@ -23,7 +23,7 @@
  */
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
-#define KEYMAP( \
+#define LAYOUT( \
     k00, k01, k02, k03, \
 	k10, k11, k12, k13, \
 	k20, k21, k22, k23, \
@@ -39,7 +39,7 @@
 	{k40, k41, k42, k43}, \
 	{k50, XXX, k52, XXX} \
 }
-#define MAXKEYMAP( \
+#define LAYOUT_all( \
     k00, k01, k02, k03, \
 	k10, k11, k12, k13, \
 	k20, k21, k22, k23, \
