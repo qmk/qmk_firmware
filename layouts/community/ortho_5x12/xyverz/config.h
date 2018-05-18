cd444b75757055315fb0c77e17a81197a0368ae1
Ian Sterling <xyverz@Paeroa.hsd1.ca.comcast.net>
2018-05-17 21:33:43 -0700

    Corrections to config.h

--- layouts/community/ortho_5x12/xyverz/config.h
+++ layouts/community/ortho_5x12/xyverz/config.h
@@ -11,8 +11,11 @@
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 11     // Number of LEDs
+#undef RGBLIGHT_HUE_STEP
 #define RGBLIGHT_HUE_STEP 8
+#undef RGBLIGHT_SAT_STEP
 #define RGBLIGHT_SAT_STEP 8
+#undef RGBLIGHT_VAL_STEP
 #define RGBLIGHT_VAL_STEP 8
 
 // for the split boards //
