94670e49f4fff90102952a964117c29f6f921482
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-19 15:04:52 -0700

    Configurator updates for M10-A
    - Add `info.json`
    - `<keyboard>.h` to `QMK_KEYBOARD_H` throughout
    - Redid the keymaps to use the `LAYOUT` macro

--- keyboards/m10a/m10a.h
+++ keyboards/m10a/m10a.h
@@ -3,17 +3,19 @@
 
 #include "quantum.h"
 
+#define XXX KC_NO
+
 #define LAYOUT( \
       k00, k01, k02, \
       k10, k11, k12, \
       k20, k21, k22, \
-      k30, k31, k32  \
+              k32  \
 ) \
 { \
     { k00, k01, k02 }, \
     { k10, k11, k12 }, \
     { k20, k21, k22 }, \
-    { k30, k31, k32 } \
+    { XXX, XXX, k32 } \
 }
 
 #endif
