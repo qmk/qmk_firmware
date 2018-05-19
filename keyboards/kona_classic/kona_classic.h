d11d79bba72cd65f71c4b845014006f8754f5950
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-19 02:31:12 -0700

    Configurator updates for Kona Classic
    - Add `info.json`
    - `KEYMAP` to `LAYOUT` syntax throughout
    - `<keyboard>.h` to `QMK_KEYBOARD_H` throughout

--- keyboards/kona_classic/kona_classic.h
+++ keyboards/kona_classic/kona_classic.h
@@ -18,7 +18,7 @@
 
 #include "quantum.h"
 
-#define KEYMAP( \
+#define LAYOUT( \
     KA0, KA1, K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
     KA2, KA3, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
     KA4, KA5, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, \
