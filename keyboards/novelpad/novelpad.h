fff0c29e9eba8369d4a0b097ec7c8cc141d4154c
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-19 23:27:45 -0700

    Configurator update for NovelPad
    - Add `info.json`
    - `KEYMAP` to `LAYOUT` syntax

--- keyboards/novelpad/novelpad.h
+++ keyboards/novelpad/novelpad.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-#define KEYMAP( \
+#define LAYOUT( \
 	K00, K01, K02, K03, \
 	K10, K11, K12, K13, \
 	K20, K21, K22, K23, \
