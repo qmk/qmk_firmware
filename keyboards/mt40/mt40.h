30cb115d7c92d17664ea8013a1022bf342570238
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-19 22:17:54 -0700

    Configurator updates for MT40
    - Add `info.json`
    - `KEYMAP` to `LAYOUT` syntax
    - `<keyboard>.h` to `QMK_KEYBOARD_H`

--- keyboards/mt40/mt40.h
+++ keyboards/mt40/mt40.h
@@ -24,7 +24,7 @@
 // The following is an example using the Planck MIT layout
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
-#define KEYMAP( \
+#define LAYOUT( \
     K00,  K01,  K02,  K03,  K04,  K05,  K06,  K07,  K08,  K09,  K0A,  K0B, \
     K10,  K11,  K12,  K13,  K14,  K15,  K16,  K17,  K18,  K19,  K1A,  K1B, \
     K20,  K21,  K22,  K23,  K24,  K25,  K26,  K27,  K28,  K29,  K2A,  K2B, \
@@ -40,7 +40,7 @@
     { KC_NO, KC_NO, K09,   K19,   K29,   KC_NO, K2B,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, K08,   K18,   K28   }  \
 }
 
-/* #define KC_KEYMAP( \ */
+/* #define LAYOUT_kc( \ */
 /*     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, \ */
 /*     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, \ */
 /*     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, \ */
