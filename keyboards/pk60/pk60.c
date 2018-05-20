03a206611b0836636c1ee52c849c7b02f42cd44e
Barry Huang <yj7272098@hotmail.com>
2018-05-20 10:28:00 +0800

    name correction

--- keyboards/pk60/pk60.c
+++ keyboards/pk60/pk60.c
@@ -1,6 +1,6 @@
 #include "pk60.h"
 
-void led_set_user(uint8_t usb_led) {
+void led_set_kb(uint8_t usb_led) {
     
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
         PORTF |= (1<<4);
@@ -8,5 +8,5 @@ void led_set_user(uint8_t usb_led) {
         PORTF &= ~(1<<4);
     }
     led_set_user(usb_led);
-    
+
 }
