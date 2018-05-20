f698bbcd65426cf7144b886f7fc7ec29d6d7c41f
Barry Huang <yj7272098@hotmail.com>
2018-05-21 01:28:33 +0800

    Capslock indicator add (#3007)
    * Capslock indicator add
    
    * edit
    
    * name correction
    
    * led_set_user(usb_led); delete

--- keyboards/pk60/pk60.c
+++ keyboards/pk60/pk60.c
@@ -1,1 +1,12 @@
 #include "pk60.h"
+
+void led_set_kb(uint8_t usb_led) {
+    
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+        PORTF |= (1<<4);
+    } else {
+        PORTF &= ~(1<<4);
+    }
+    led_set_user(usb_led);
+
+}
