0b239dc540966d5cc073cd119566204732442b4e
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-19 18:10:27 -0700

    Configurator updates for Mitosis
    - Add `info.json`
    - `KEYMAP` to `LAYOUT` syntax throughout
    - `<keyboard>.h` to `QMK_KEYBOARD_H` throughout

--- keyboards/mitosis/keymaps/carvac_dv/keymap.c
+++ keyboards/mitosis/keymaps/carvac_dv/keymap.c
@@ -3,7 +3,7 @@
 //  number layer with all the numbers on the home row, and a function layer
 //  that provides mouse keys among other things.
 
-#include "mitosis.h"
+#include QMK_KEYBOARD_H
 
 enum mitosis_layers
 {
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          '-----------------------------------''-----------------------------------'
  */
 
-[_STD] = { /* Standard; as compatible with dvorak and qwerty as possible */
-  {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
-  {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
-  {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
-  {XXXXXXX, KC_PGUP, KC_TAB,  KC_LCTL, KC_SPC,         KC_LSFT, KC_ENT,  KC_UP,   KC_PSCR, XXXXXXX },
-  {XXXXXXX, KC_PGDN, KC_LGUI, KC_LALT, MO(_FN),       MO(_NUM), KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX }
-},
+[_STD] = LAYOUT( /* Standard; as compatible with dvorak and qwerty as possible */
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P   , \
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+           KC_PGUP, KC_TAB,  KC_LCTL, KC_SPC,         KC_LSFT, KC_ENT,  KC_UP,   KC_PSCR, \
+           KC_PGDN, KC_LGUI, KC_LALT, MO(_FN),       MO(_NUM), KC_LEFT, KC_DOWN, KC_RGHT
+),
 
 /* Number layout, for data entry and programming purposes (Dvorak result in parens)
  * .--------------------------------------------..--------------------------------------------.
@@ -63,13 +63,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          '-----------------------------------''-----------------------------------'
  */
 
-[_NUM] = { /* Number layout along the home row for maximum speed*/
-  {KC_TAB,  _______, _______, KC_MINS, KC_EQL,        KC_RBRC, KC_PAST, KC_PPLS, KC_PMNS, KC_LBRC },
-  {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0    },
-  {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10  },
-  {XXXXXXX, KC_F11,  KC_F12,  _______, _______,       _______, _______, _______, _______, XXXXXXX },
-  {XXXXXXX, _______, _______, _______, _______,       _______, _______, _______, _______, XXXXXXX }
-},
+[_NUM] = LAYOUT( /* Number layout along the home row for maximum speed*/
+  KC_TAB,  _______, _______, KC_MINS, KC_EQL,        KC_RBRC, KC_PAST, KC_PPLS, KC_PMNS, KC_LBRC, \
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0   , \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10 , \
+           KC_F11,  KC_F12,  _______, _______,       _______, _______, _______, _______, \
+           _______, _______, _______, _______,       _______, _______, _______, _______
+),
 
 
 /* Fn layout, for typing purposes (Dvorak result in parens)
@@ -86,14 +86,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          '-----------------------------------''-----------------------------------'
  */
 
-[_FN] = { /* Function Layer, primary alternative layer featuring numpad on right hand,
+[_FN] = LAYOUT( /* Function Layer, primary alternative layer featuring numpad on right hand,
                                    cursor keys on left hand, and all symbols*/
-  {KC_GRV,  _______, KC_MS_U, _______, _______,       KC_WH_U, KC_WH_L, KC_BTN3, KC_WH_R, KC_LBRC },
-  {KC_ESC,  KC_MS_L, KC_MS_D, KC_MS_R, _______,       KC_WH_D, KC_BTN1, KC_BTN2, _______, KC_QUOT },
-  {KC_APP,  KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,       _______, KC_BSPC, KC_DEL,  KC_INS,  KC_BSLS },
-  {XXXXXXX, KC_VOLU, _______, _______, _______,       _______, _______, KC_PGUP, _______, XXXXXXX },
-  {XXXXXXX, KC_VOLD, _______, _______, _______,       _______, KC_HOME, KC_PGDN, KC_END,  XXXXXXX }
-},
+  KC_GRV,  _______, KC_MS_U, _______, _______,       KC_WH_U, KC_WH_L, KC_BTN3, KC_WH_R, KC_LBRC, \
+  KC_ESC,  KC_MS_L, KC_MS_D, KC_MS_R, _______,       KC_WH_D, KC_BTN1, KC_BTN2, _______, KC_QUOT, \
+  KC_APP,  KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,       _______, KC_BSPC, KC_DEL,  KC_INS,  KC_BSLS, \
+           KC_VOLU, _______, _______, _______,       _______, _______, KC_PGUP, _______, \
+           KC_VOLD, _______, _______, _______,       _______, KC_HOME, KC_PGDN, KC_END
+),
 
 /* blank key layout template
  * .--------------------------------------------..--------------------------------------------.
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void matrix_scan_user(void) {
     uint8_t layer = biton32(layer_state);
-    
+
     switch (layer) {
     	case _STD:
     		set_led_off;
