7bff9ec223b79a1321b00b1b9cfbc90cc54863a6
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-20 22:26:20 -0700

    Configurator update for YMD96
    - Add `info.json`
    - Keymaps updated to `LAYOUT` syntax
    - `<keyboard>.h` to `QMK_KEYBOARD_H`

--- keyboards/ymd96/keymaps/JJ50/keymap.c
+++ keyboards/ymd96/keymaps/JJ50/keymap.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public LicensezZZ
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ymd96.h"
+#include QMK_KEYBOARD_H
 #include "action_layer.h"
 #include "rgblight.h"
 
