d4271ff65651f24ab56db2e7182d5156f3ea6bf2
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-20 22:09:28 -0700

    Configurator update for TKC1800
    - `info.json` has some white space added for easier reading, and a big block comment for if someone wants to "fork" the `LAYOUT` macro
    - `<keyboard>.h` to `QMK_KEYBOARD_H`

--- keyboards/tkc1800/keymaps/wkl/keymap.c
+++ keyboards/tkc1800/keymaps/wkl/keymap.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "tkc1800.h"
+#include QMK_KEYBOARD_H
 #include "LUFA/Drivers/Peripheral/TWI.h"
 #include "i2c.h"
 #include "ssd1306.h"
