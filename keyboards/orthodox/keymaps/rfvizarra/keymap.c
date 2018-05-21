700a04fbd6f6542cccecd69ca47d2e3463ce1bb0
Rodrigo Fernandez-Vizarra <rfvizarra@gmail.com>
2018-05-21 23:26:35 +0200

    Consolidated inclues with #include QMK_KEYBOARD_H

--- keyboards/orthodox/keymaps/rfvizarra/keymap.c
+++ keyboards/orthodox/keymaps/rfvizarra/keymap.c
@@ -19,9 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "orthodox.h"
-#include "action_layer.h"
-#include "eeconfig.h"
+#include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
 
