8ae2d217093ee63c7012e1191a3fd901d940c557
epaew <epaew.333@gmail.com>
2018-05-23 08:23:56 +0900

    deleted the verbose line of rule.mk

--- keyboards/iris/keymaps/edvorakjp/rules.mk
+++ keyboards/iris/keymaps/edvorakjp/rules.mk
@@ -14,15 +14,3 @@ AUDIO_ENABLE = no      # Audio output on port C6
 UNICODE_ENABLE = no    # Unicode
 BLUETOOTH_ENABLE = no  # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes  # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
-SUBPROJECT_rev1 = yes
-USE_I2C = yes
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-
-CUSTOM_MATRIX = yes
-
-DEFAULT_FOLDER = iris/rev2
-
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
