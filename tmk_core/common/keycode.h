bcf5ea7e4e53a875cfc06742290253f4fe9225d9
Leo Wzukw <leowzukw@users.noreply.github.com>
2018-05-25 19:35:01 +0200

    Fix dead link to USB keycodes doc
    Link was dead and the fresher version I could find on usb.org is still older than this one.
    
    Thus, WaybackMachine seems the best option.

--- tmk_core/common/keycode.h
+++ tmk_core/common/keycode.h
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /*
  * Keycodes based on HID Usage Keyboard/Keypad Page(0x07) plus special codes
- * http://www.usb.org/developers/devclass_docs/Hut1_12.pdf
+ * https://web.archive.org/web/20060218214400/http://www.usb.org/developers/devclass_docs/Hut1_12.pdf
  */
 #ifndef KEYCODE_H
 #define KEYCODE_H
