eb900b95a3c76201c24c6ab36a90dc6de39742ca
noroadsleft <xxiinophobia@yahoo.com>
2018-05-25 04:36:49 -0700

    Update personal KC60 keymap

--- keyboards/kc60/keymaps/noroadsleft/config.h
+++ keyboards/kc60/keymaps/noroadsleft/config.h
@@ -1,3 +1,5 @@
+#define PREVENT_STUCK_MODIFIERS
+
 // Enable single-color backlighting
 #define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 5
--- keyboards/kc60/keymaps/noroadsleft/keymap.c
+++ keyboards/kc60/keymaps/noroadsleft/keymap.c
@@ -1,24 +1,28 @@
 
 #include QMK_KEYBOARD_H
+#include <sendstring_dvorak.h>
 
 // Layer Definitions
-#define   _QW   0
-#define   _DV   1
-#define   _FW   2
-#define   _FM   3
-#define   _NP   4
-#define   _MA   5
-//#define   _Q2   6
+#define _QW 0
+#define _DV 1
+#define _FW 2
+#define _FM 3
+#define _NP 4
+#define _MA 5
+//#define _Q2 6
 
 
 // Keycode Definitions
 #define _______ KC_TRNS
+#define NO_CHNG KC_TRNS // Note for me for keys I need to leave as Pass-through
 #define XXXXXXX KC_NO
 
 #define FW_CAPS LT(_FW, KC_CAPS) // _FW when held, Caps when tapped
 #define FM_CAPS LT(_FM, KC_CAPS) // _FM when held, Caps when tapped
 
-#define MC_PSCR LGUI(LSFT(KC_3)) // MacOS Print Screen
+#define MC_PSCR LGUI(LSFT(KC_3)) // MacOS Print Screen (Command + Shift + 3)
+#define MC_HOME LGUI(KC_LEFT)    // MacOS Home (Command + Left Arrow)
+#define MC_END  LGUI(KC_RGHT)    // MacOS End (Command + Right Arrow)
 #define MC_UNDO LGUI(KC_Z)       // MacOS Undo
 #define MC_CUT  LGUI(KC_X)       // MacOS Cut
 #define MC_COPY LGUI(KC_C)       // MacOS Copy
@@ -49,13 +53,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 };
 
-
 // Keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* QWERTY */
   [_QW] = LAYOUT_60_ansi(
-    //       .        .        .        .        .        .        .        .        .        .        .        .        .        .        .
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     FW_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           \
@@ -64,12 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   /* Dvorak */
-  /*
-   * Use this to have access to US Dvorak layout when connecting your
-   * keyboard to a computer that only has US QWERTY.
-   */
   [_DV] = LAYOUT_60_ansi(
-    //       .        .        .        .        .        .        .        .        .        .        .        .        .        .        .
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC, \
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS, \
     FM_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, KC_ENT,           \
@@ -79,54 +78,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Windows Fn layer */
   [_FW] = LAYOUT_60_ansi(
-    //       .        .        .        .        .        .        .        .        .        .        .        .        .        .        .
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-    _______, KC_CALC, KC_UP,   _______, _______, _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PSCR, KC_SLCK, KC_PAUS, \
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, KC_PENT,          \
-    _______, _______, KC_APP,  BL_DEC,  BL_TOGG, BL_INC,  BL_BRTG, KC_MUTE, KC_VOLD, KC_VOLU, TG(_DV), _______,                   \
-    _______, _______, _______,                   TG(_NP),                                     _______, MO(_MA), _______, _______  \
+    _______, KC_CALC, _______, _______, _______, _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PSCR, KC_SLCK, KC_PAUS, \
+    NO_CHNG, _______, _______, _______, _______, _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, KC_PENT,          \
+    _______, _______, KC_APP,  BL_DEC,  BL_TOGG, BL_INC,  BL_BRTG, KC_MUTE, KC_VOLD, KC_VOLU, DF(_DV), _______,                   \
+    _______, _______, _______,                   TG(_NP),                                     _______, MO(_MA), NO_CHNG, _______  \
   ),
 
   /* MacOS Fn layer */
   [_FM] = LAYOUT_60_ansi(
-    //       .        .        .        .        .        .        .        .        .        .        .        .        .        .        .
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MC_PSCR, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          \
-    _______, MC_UNDO, MC_CUT,  MC_COPY, MC_PSTE, BL_TOGG, _______, _______, _______, _______, TG(_DV), _______,                   \
-    _______, _______, _______,                   TG(_NP),                                     _______, _______, _______, _______  \
+    _______, _______, _______, _______, _______, _______, _______, MC_HOME, KC_UP,   MC_END,  _______, MC_PSCR, _______, _______, \
+    NO_CHNG, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_BRTG, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,          \
+    _______, MC_UNDO, MC_CUT,  MC_COPY, MC_PSTE, _______, _______, _______, _______, _______, DF(_QW), _______,                   \
+    _______, _______, _______,                   TG(_NP),                                     _______, _______, NO_CHNG, _______  \
   ),
 
   /* Numpad layer */
   [_NP] = LAYOUT_60_ansi(
-    //       .        .        .        .        .        .        .        .        .        .        .        .        .        .        .
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
     _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_PSLS, KC_PEQL, _______, \
     _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PPLS, KC_PMNS, KC_PENT,          \
     _______, _______, _______, _______, _______, _______, _______, KC_P0,   _______, KC_PDOT, _______, _______,                   \
-    _______, _______, _______,                   TO(_NP),                                     _______, _______, _______, _______  \
+    _______, _______, _______,                   TG(_NP),                                     _______, _______, NO_CHNG, _______  \
   ),
 
   /* Macro layer */
   [_MA] = LAYOUT_60_ansi(
-    //       .        .        .        .        .        .        .        .        .        .        .        .        .        .        .
+    //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, M_GITUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, M_GITDN, _______, _______, _______, _______, _______, _______, M_L3DED, _______, _______, _______,          \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   \
-    _______, _______, _______,                   TO(_NP),                                     _______, _______, _______, _______  \
+    RESET,   _______, _______,                   _______,                                     _______, NO_CHNG, NO_CHNG, _______  \
   ),
 
-  /* Quake 2 layer
-  [_Q2] = LAYOUT_60_ansi(
-    //       .        .        .        .        .        .        .        .        .        .        .        .        .        .        .
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   \
-    _______, _______, _______,                   TO(_NP),                                     _______, _______, _______, _______  \
-  )
-  */
 };
 
 
