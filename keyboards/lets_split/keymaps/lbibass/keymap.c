3146913e600c65244efd5a70730391009b0b0eb8
lbibass <notlbibass@gmail.com>
2018-05-27 15:15:12 -0400

    fixed conflicts

--- keyboards/lets_split/keymaps/lbibass/keymap.c
+++ keyboards/lets_split/keymaps/lbibass/keymap.c
@@ -149,22 +149,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
-<<<<<<< HEAD
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
-=======
  * |      |      |      |      |      |             |      | media| Vol- | Vol+ | skip |
->>>>>>> 79fa9b78b9a78a187680f9fd06cf2e365e55fcac
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_LGUI,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-<<<<<<< HEAD
   _______, _______, _______, _______, _______, _______, _______, _______, TD_MEDIA, KC_VOLD, KC_VOLU, KC_MFFD \
-=======
-  _______, _______, _______, _______, _______, _______, _______, _______, TD(CT_CLN), KC_VOLD, KC_VOLU, KC_MFFD \
->>>>>>> 79fa9b78b9a78a187680f9fd06cf2e365e55fcac
 ),
 
 /* Adjust (Lower + Raise)
