a4d2fdac78ab1a0eba6b9ae76bceda5363911062
lbibass <notlbibass@gmail.com>
2018-05-27 10:00:23 -0400

    modified rules a bit.

--- keyboards/mechmicro/rules.mk
+++ keyboards/mechmicro/rules.mk
@@ -54,7 +54,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
-CONSOLE_ENABLE = no	# Console for debug(+400)
+CONSOLE_ENABLE = yes	# Console for debug(+400)
 COMMAND_ENABLE = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
