373147f80b8317c69844d3e9ef4f79d9e3a961f6
Daniel Hertz <daniel@dhertz.com>
2018-05-28 02:06:12 +0100

    Add layer colour indicator

--- keyboards/canoe/keymaps/dhertz/keymap.c
+++ keyboards/canoe/keymaps/dhertz/keymap.c
@@ -17,3 +17,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDOWN, KC_END
       ),
 };
+
+void matrix_scan_keymap(void) {
+    uint8_t layer = biton32(layer_state);
+
+    switch (layer) {
+        case 0:
+            rgblight_setrgb_teal();
+            break;
+        case 1:
+            rgblight_setrgb_magenta();
+            break;
+        default:
+            // none
+            break;
+    }
+};
