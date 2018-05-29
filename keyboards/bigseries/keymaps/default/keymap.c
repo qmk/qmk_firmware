eb8388b31e5c0101c1e99e3babb72a47546df156
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-05-29 03:53:03 -0700

    Refactor and Configurator updates for Woodkeys/NovelKeys Big Series (#3073)
    * Refactor for Woodkeys Big Series
    
    * Configurator update for Woodkeys Big Series

--- keyboards/bigseries/keymaps/default/keymap.c
+++ keyboards/bigseries/keymaps/default/keymap.c
@@ -15,11 +15,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../../bigseries.h"
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-KEYMAP(
+LAYOUT(
     KC_A),
 
 
