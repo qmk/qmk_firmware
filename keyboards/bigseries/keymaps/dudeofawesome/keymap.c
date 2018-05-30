f35424d3a842fce7d8c42ce9c51ca11ec940d45b
Louis Orleans <louis@orleans.io>
2018-05-29 22:24:29 -0700

    🔥 remove unnecesarry code

--- keyboards/bigseries/keymaps/dudeofawesome/keymap.c
+++ keyboards/bigseries/keymaps/dudeofawesome/keymap.c
@@ -25,23 +25,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT(TD(TD_PLAY)),
 };
 
-bool initialized = false;
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  return MACRO_NONE;
-}
-
-void matrix_init_user(void) {
-  if (!initialized){
-      dprintf("Initializing in matrix_scan_user");
-      rgblight_enable();
-      rgblight_mode(7);
-      rgblight_sethsv(0,255,255);
-      rgblight_setrgb(0x00, 0x00, 0xFF);
-      initialized = true;
-    }
-}
-
 void tap_dance (qk_tap_dance_state_t *state, void *user_data) {
   switch (state->count) {
     case 0 ... 1:
@@ -61,37 +44,3 @@ void tap_dance (qk_tap_dance_state_t *state, void *user_data) {
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_PLAY] = ACTION_TAP_DANCE_FN(tap_dance),
 };
-
-void led_set_user(uint8_t usb_led) {
-
-  if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_KANA)) {
-
-  } else {
-
-  }
-
-}
