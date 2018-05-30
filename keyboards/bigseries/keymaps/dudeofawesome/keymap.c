6f36813e17bc627d594723a4f765a9055e26c17f
Louis Orleans <louis@orleans.io>
2018-05-29 22:24:01 -0700

    👽 update for new keymap format

--- keyboards/bigseries/keymaps/dudeofawesome/keymap.c
+++ keyboards/bigseries/keymaps/dudeofawesome/keymap.c
@@ -15,14 +15,14 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../../bigseries.h"
+#include QMK_KEYBOARD_H
 
 enum TAP_DANCE {
   TD_PLAY = 0,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  KEYMAP(TD(TD_PLAY)),
+  LAYOUT(TD(TD_PLAY)),
 };
 
 bool initialized = false;
