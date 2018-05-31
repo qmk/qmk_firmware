5d091c06fd084d1b57adab7ba5ab009a30e23adc
Kousuke Tada <pekaso34@gmail.com>
2018-05-31 23:34:25 +0900

    Commit including suggestions

--- keyboards/fortitude60/rev1/config.h
+++ keyboards/fortitude60/rev1/config.h
@@ -37,9 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { D1, D0, D3, B7, B6 }
 #define MATRIX_COL_PINS { F7, F6, F5, B3, B2, B1 }
 
-//#define MATRIX_ROW_PINS { D4, D7, E6, B4, B5 }
-//#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
