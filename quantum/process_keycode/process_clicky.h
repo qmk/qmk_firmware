781c464f84f32743858890f824da245959690359
Drashna Jaelre <drashna@live.com>
2018-05-31 12:24:10 -0700

    Add functions so you can configure programatically

--- quantum/process_keycode/process_clicky.h
+++ quantum/process_keycode/process_clicky.h
@@ -4,4 +4,13 @@
 void clicky_play(void);
 bool process_clicky(uint16_t keycode, keyrecord_t *record);
 
+void clicky_freq_up(void);
+void clicky_freq_down(void);
+void clicky_freq_reset(void);
+void clicky_freq_toggle(void);
+void clicky_freq_on(void);
+void clicky_freq_off(void);
+
+bool is_clicky_on(void);
+
 #endif
