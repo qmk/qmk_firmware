b2398ecbe7047d6bcd39a6c8039b1bfbdcc51ad2
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2018-05-30 19:44:52 -0700

    CA66 Backlighting FIx (#3089)
    * Enable backlighting using pin F0 and changing it in the rules.mk file as well
    
    * Change KB_H to CA66_H

--- keyboards/ca66/rules.mk
+++ keyboards/ca66/rules.mk
@@ -51,6 +51,6 @@ CONSOLE_ENABLE ?= no	# Console for debug(+400)
 COMMAND_ENABLE ?= no    # Commands for debug and configuration
 SLEEP_LED_ENABLE ?= no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE ?= yes  # Enable keyboard backlight functionality
 AUDIO_ENABLE ?= no
 RGBLIGHT_ENABLE ?= yes
