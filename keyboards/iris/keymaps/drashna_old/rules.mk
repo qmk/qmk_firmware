3d1349b280d9c0a57e37b5347a405426de6dba10
Drashna Jaelre <drashna@live.com>
2018-05-31 22:11:06 -0700

    Big overhaul to Drashna keymaps and userspace (#3097)
    * Optimize secrets code
    
    * Orthodox tweaks
    
    * rules.mk features
    
    * Minor cleanup
    
    * Revert mod bits
    
    * Force Hold breaks One Shot Tap Toggle
    
    * Cleanup
    
    * Moke keymaps more consistent
    
    * minor ergodox tweak
    
    * More OSM for the Orthodox
    
    * Cleanup of userspace
    
    * Toggle Secrets
    
    * Add hidden process record for super secret macros
    
    * Make sure secret macros always compiles
    
    * finish up making them super secret
    
    * Add ColinTA's rgb twinkle as WIP
    
    * Optimize RGB Twinkling for typing
    
    Also, tweak RGB indicators.
    
    AND WTF, I HAVE NO IDEA WHY THE INDICATORS ONLY WORK AS IS.  The logical method for getting them working doesn't ... and it's beyond bizarre
    
    * Make console logging more configurable
    
    * Indicator travisty
    
    * Clean up userspace rgb code
    
    * Optimize RGB Twinking to work on default layer only, and to base it's color on the curent hue
    
    * Eff it... rgblight_sethsv_at runs at every matrix scan
    
    * RGB Twinkle cleanup
    
    * Update Iris code for new board
    
    * Move RGB Indicator and RGB Twinkle into userspace
    
    * Move RGB Indicator code to rgb_stuff.c
    
    * Major cleanup of RGB Code in userspace
    
    * Additional cleanup of RGB code in userspace
    
    * Use noeeprom functions to save my boards!
    
    * Enable RGB Sleep on all boards now
    
    * Add old iris board
    
    * tapping tweak
    
    * Use byte 19 for eeprom to prepare for possible merge of eeconfig function pr
    
    * Add code to fix default layer after eeprom reset

--- keyboards/iris/keymaps/drashna_old/rules.mk
+++ keyboards/iris/keymaps/drashna_old/rules.mk
@@ -1,3 +1,5 @@
+USER_NAME := drashna
+
 BOOTMAGIC_ENABLE  = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE   = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE   = yes       # Audio control and System control(+450)
