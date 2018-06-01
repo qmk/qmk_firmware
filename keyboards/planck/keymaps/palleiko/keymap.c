54ab2ace07c007241d6c2e8bd9022fef6bb9a0c8
palleiko <ian.palleiko@gmail.com>
2018-05-31 20:23:43 -0400

    Removed Layer Tap functionality

--- keyboards/planck/keymaps/palleiko/keymap.c
+++ keyboards/planck/keymaps/palleiko/keymap.c
@@ -61,10 +61,8 @@ enum planck_keycodes {
 // NOP
 #define ___x___ KC_NO
 
-// MT mods
-#define NAV_SC LT(NAV, KC_SCLN)
+// MT Shift & Enter
 #define S_ENT MT(MOD_RSFT, KC_ENT)
-#define ESC_NUM LT(NUM, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -76,14 +74,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  WM  | GUI  | Alt  |Lower |    Space    |Raise | Alt  | GUI  |  WM  | NUM  |
+ * | Ctrl |  NUM | NAV  | Alt  |Lower |    Space    |Raise | NAV  | GUI  |  NUM |  WM  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {ESC_NUM, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NAV_SC,  KC_QUOT},
+  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, S_ENT  },
-  {KC_LCTL, WM,      NUM,     KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, WM,      NUM}
+  {KC_LCTL, NUM,     NAV,     KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   NAV,     KC_RGUI, NUM,     WM}
 },
 
 /* Colemak
@@ -94,14 +92,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  WM  | GUI  | Alt  |Lower |    Space    |Raise | Alt  | GUI  |  WM  | NUM  |
+ * | Ctrl |  NUM | NAV  | Alt  |Lower |    Space    |Raise | NAV  | GUI  |  NUM |  WM  |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
-  {ESC_NUM, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
+  {KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, S_ENT  },
-  {KC_LCTL, WM,      NUM,     KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, WM,      NUM}
+  {KC_LCTL, NUM,     NAV,     KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   NAV,     KC_RGUI, NUM,     WM}
 },
 
 /* Dvorak
@@ -112,14 +110,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  WM  | GUI  | Alt  |Lower |    Space    |Raise | Alt  | GUI  |  WM  | NUM  |
+ * | Ctrl |  NUM | NAV  | Alt  |Lower |    Space    |Raise | NAV  | GUI  |  NUM |  WM  |
  * `----------------------------------------------------------------------------------'
  */
 [_DVORAK] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
-  {ESC_NUM, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH},
+  {KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH},
   {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT },
-  {KC_LCTL, WM,      NUM,     KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, WM,      NUM}
+  {KC_LCTL, NUM,     NAV,     KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   NAV,     KC_RGUI, NUM,     WM}
 },
 
 /* Lower
@@ -157,6 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
+
 /* Hex Numpad
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |   {  |   }  |   A  |  B   |   7  |   8  |   9  |      |      |
