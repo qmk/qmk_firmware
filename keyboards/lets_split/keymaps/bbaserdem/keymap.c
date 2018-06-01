8eaf23ae8146766615cc1cec3fc8e04111e8ef49
Batuhan Baserdem <bbaserdem@users.noreply.github.com>
2018-05-31 21:55:45 -0400

    User space fixed (#3095)
    * Put in my keymaps
    
    * Fixed all but weird lets split issue
    
    * Organized and tried to trobleshoot lets split
    
    * Organized and tried to trobleshoot lets split
    
    * Added bbaserdem keymaps
    
    * Added bbaserdem keymaps
    
    * Fixed stuff
    
    * FIxed a filename error

--- keyboards/lets_split/keymaps/bbaserdem/keymap.c
+++ keyboards/lets_split/keymaps/bbaserdem/keymap.c
@@ -1,13 +1,22 @@
 /*
- * PLANCK - Dvorak turkish
+ * Let's Split - Dvorak turkish
  * Keymap by @bbaserdem
  * Dvorak layout with multiple features
  * Most of the code is in the "user" directory.
  * Check qmk_firmware/users/bbaserdem for the main part of the code
  */
+#define KEYMAP(...) LAYOUT_ortho_4x12(__VA_ARGS__)
 
-#include "planck.h"
+#include "lets_split.h"
 #include "bbaserdem.h"
+
+
+void matrix_init_keymap (void) {
+}
+
+uint32_t layer_state_set_keymap(uint32_t state) {
+    return state;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Main Dvorak layer
@@ -22,8 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SE] = SETTINGS,
     // Mouse emulation layer
     [_MO] = MOUSE,
-#ifdef MUSIC_ENABLE
-    // Music mode
+#ifdef AUDIO_ENABLE
     [_MU] = MUSIC,
 #endif
 };
