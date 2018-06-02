f29d09d61ce1e09c6356876eb8cb0f106d178400
premek <premek@users.noreply.github.com>
2018-06-02 13:45:39 +0200

    planck premek - left thumb shift

--- keyboards/planck/keymaps/premek/keymap.c
+++ keyboards/planck/keymaps/premek/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P   },
   {KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN},
   {KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
-  {KC_LGUI, _______, _______, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  KC_SPC,  RAISE,   KC_RALT, _______, KC_RGUI}
+  {KC_LGUI, _______, _______, KC_LALT, LOWER,   KC_RSFT, KC_ENT,  KC_SPC,  RAISE,   KC_RALT, _______, KC_RGUI}
 },
 [_COLEMAK] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
