1bb4b7780b203b1ff065e2d8727ae46d4f59f8ca
wanleg <32079073+wanleg@users.noreply.github.com>
2018-06-04 14:40:59 -0700

    cleanup prior to Gherkin Pull

--- keyboards/jj40/keymaps/wanleg/rules.mk
+++
@@ -1,7 +0,0 @@
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
-
-AUDIO_ENABLE = no
-TAP_DANCE_ENABLE = yes
-MOUSEKEY_ENABLE = yes
--- keyboards/planck/keymaps/wanleg/rules.mk
+++
@@ -1,6 +0,0 @@
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
-
-TAP_DANCE_ENABLE = yes
-MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
