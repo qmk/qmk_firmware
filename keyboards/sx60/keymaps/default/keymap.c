19443c6bce3485daf952825437fde4a05761ad91
Jason Gorman <amnesia0287@gmail.com>
2018-06-04 18:51:49 -0700

    Changing includes to QMK_KEYBOARD_H

--- keyboards/sx60/keymaps/default/keymap.c
+++ keyboards/sx60/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-#include "sx60.h"
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_ansi_split_bs_rshift( KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
