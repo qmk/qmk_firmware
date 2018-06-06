8384d09323b0db291dce47fad5ca3afa95975dd2
noroadsleft <xxiinophobia@yahoo.com>
2018-06-06 11:03:38 -0700

    Refactor Dilly to use LAYOUT_ortho_3x10

--- keyboards/dilly/keymaps/default/keymap.c
+++ keyboards/dilly/keymaps/default/keymap.c
@@ -30,37 +30,37 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_BASE] = LAYOUT(
+  [_BASE] = LAYOUT_ortho_3x10(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_ASFT, KC_S,    KC_D,    KC_F_L3, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ESCS,
     KC_ZCTL, KC_XALT, KC_CGUI, KC_V_L4, KC_SPL2, KC_B_L1, KC_N_L5, KC_MALT, KC_BSCT, KC_ENTS
   ),
 
-  [_FN1] = LAYOUT(
+  [_FN1] = LAYOUT_ortho_3x10(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     _______, _______, _______, _______, KC_BSPC, _______, _______, _______, _______, _______
   ),
 
-  [_FN2] = LAYOUT(
+  [_FN2] = LAYOUT_ortho_3x10(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, KC_GRV,
     _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______
   ),
 
-  [_FN3] = LAYOUT(
+  [_FN3] = LAYOUT_ortho_3x10(
     _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_TAB,  _______, _______, _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN, KC_QUOT,
     _______, _______, _______, _______, KC_BSPC, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
-  [_FN4] = LAYOUT(
+  [_FN4] = LAYOUT_ortho_3x10(
     _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     KC_TAB,  _______, _______, _______, _______, KC_LT,   KC_GT,   KC_QUES, KC_COLN, KC_DQUO,
     _______, _______, KC_GUIC, _______, KC_BSPC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
 
-  [_FN5] = LAYOUT(
+  [_FN5] = LAYOUT_ortho_3x10(
     RGB_TOG, RGB_MOD, _______, RESET,   RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______,
     _______, _______, DEBUG,   _______, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______,
     BL_STEP, _______, KC_GUIC, _______, _______, _______, _______, _______, _______, _______
