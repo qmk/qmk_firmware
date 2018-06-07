89e56e2201cde83e3019ec96f8540ce66854d138
noroadsleft <xxiinophobia@yahoo.com>
2018-06-07 04:39:54 -0700

    Added git branch-name macro

--- keyboards/kc60/keymaps/noroadsleft/keymap.c
+++ keyboards/kc60/keymaps/noroadsleft/keymap.c
@@ -40,6 +40,7 @@ enum custom_keycodes {
     G_ORIG,
     G_COMM,
     G_C10R,
+    G_BRCH,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -66,9 +67,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             case G_COMM:
                 SEND_STRING("git commit -m \"\"" SS_TAP(X_LEFT));
+                layer_off(_MA);
                 return false;
             case G_C10R:
                 SEND_STRING("c10r-");
+                layer_off(_MA);
+                return false;
+            case G_BRCH:
+                SEND_STRING("$(git branch-name)");
+                layer_off(_MA);
                 return false;
         }
     }
@@ -134,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, G_ORIG,  G_PUSH,  G_UPS,   _______, _______, _______, _______, _______, T_PEEKR, _______, _______, _______, \
     _______, _______, _______, G_PULL,  G_COMM,  _______, _______, _______, _______, T_L3DED, _______, _______, _______,          \
-    _______, _______, _______, G_C10R,  _______, _______, _______, _______, _______, _______, _______, _______,                   \
+    _______, _______, _______, G_C10R,  _______, G_BRCH,  _______, _______, _______, _______, _______, _______,                   \
     RESET,   _______, _______,                   _______,                                     _______, TG(_MA), NO_CHNG, _______  \
   ),
 
