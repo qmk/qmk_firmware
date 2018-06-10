df7ad9d468ca052c51cc0d9305c7030c55f19ab3
Breno Reis <breno.dk@gmail.com>
2018-06-09 23:37:12 -0300

    added my niu mini layout

---
+++ keyboards/niu_mini/keymaps/framtava/rules.mk
@@ -0,0 +1,7 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+CONSOLE_ENABLE = no         # Console for debug(+400)
+AUDIO_ENABLE = no           # Audio output on port C6
+UNICODE_ENABLE = no         # Unicode
