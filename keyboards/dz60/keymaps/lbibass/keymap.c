30aa787cd840c46ec23ed665797a0d7912a207bd
lbibass <notlbibass@gmail.com>
2018-06-12 22:12:10 -0400

    revised my lay

--- keyboards/dz60/keymaps/lbibass/keymap.c
+++ keyboards/dz60/keymaps/lbibass/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][5][15] = {
 * `-----------------------------------------------------------------------------------------'
 */
 
-LAYOUT(	
+LAYOUT(
 		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_DEL,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
 		KC_LGUI, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
@@ -29,7 +29,7 @@ LAYOUT(KC_GRV, KC_F14, KC_F15, KC_TRNS, KC_TRNS, BL_DEC, BL_INC, KC_MRWD, KC_MPL
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_LEFT, KC_RIGHT, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC, BL_TOGG, BL_INC, BL_STEP, KC_TRNS, KC_MRWD, KC_MPLY, KC_MFFD, KC_DOWN, KC_TRNS, KC_TRNS,
        KC_CAPS, KC_LALT, KC_LCTL, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RGUI, KC_RALT, KC_RCTL),
-/* Layer 0 (Basically an HHKB MacOS layout)
+/* Layer 0 (Basically an HHKB windows layout)
 * ,-----------------------------------------------------------------------------------------.
 * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  \  |  ~  |
 * |-----------------------------------------------------------------------------------------+
@@ -52,7 +52,7 @@ LAYOUT( KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_M
 
     // layer 3
     LAYOUT(
-           KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_MINS, KC_EQL, KC_TRNS, KC_TRNS,
+           KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,  KC_TRNS, KC_TRNS,
            TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, RESET,
            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_RIGHT, KC_TRNS,
            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_TRNS, KC_TRNS,
