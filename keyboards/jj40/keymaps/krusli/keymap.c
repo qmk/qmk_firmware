59643b360f073873ead72ada6db257d2e454d3d3
Kenneth <14309096+krusli@users.noreply.github.com>
2018-06-16 00:48:00 +1000

    Remove Numpad layer (easy to accidentally toggle)

--- keyboards/jj40/keymaps/krusli/keymap.c
+++ keyboards/jj40/keymaps/krusli/keymap.c
@@ -3,7 +3,6 @@
 #define _QWERTY 0
 #define _LOWER  1
 #define _RAISE  2
-#define _NUMPAD 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -14,14 +13,6 @@ enum custom_keycodes {
 void matrix_scan_user(void) {
   // runs at every matrix scan.
 }
-
-// enum {
-//   TD_H_E = 0
-// };
-//
-// qk_tap_dance_action_t tap_dance_actions[] = {
-//   [TD_H_E] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END)
-// };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty
@@ -36,10 +27,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_2U_space( \
-    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_ESC,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-    TO(_NUMPAD),KC_LCTL, KC_LGUI, KC_LALT, MO(_LOWER),  KC_SPC,  MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+    _______, KC_LCTL, KC_LGUI, KC_LALT, MO(_LOWER),  KC_SPC,  MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   /* Lower
@@ -76,23 +67,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  KC_PGUP, KC_PGDN, _______, \
     _______, KC_MRWD, KC_MPLY, KC_MNXT, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
-  ),
-
-  /* Numpad
-   * ,-----------------------------------------------------------------------------------.
-   * | Esc  |  7   |  8   |  9   |  *   |  /   |      |      |      |      |      |      |
-   * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * | <--  |  4   |  5   |  6   |  +   |  -   |      |      |      |      |      |      |
-   * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      |  1   |  2   |  3   |Enter |Enter |      |      |      |      |      |      |
-   * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Qwerty|  0   |  .   |  .   |Enter |Enter        |      |      |      |      |      |
-   * `-----------------------------------------------------------------------------------'
-   */
-  [_NUMPAD] = LAYOUT_2U_space( \
-    KC_ESC,     KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_PSLS, _______, _______, _______, _______, _______, _______, \
-    KC_BSPC,    KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PMNS, _______, _______, _______, _______, _______, _______, \
-    _______,    KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_PENT, _______, _______, _______, _______, _______, _______, \
-    TO(_QWERTY),KC_P0,   KC_PDOT, KC_PDOT, KC_PENT, _______, _______, _______, _______, _______, _______ \
   )
 };
