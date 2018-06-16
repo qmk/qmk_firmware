404e73421576aa60ce6118ae78c1b119fa8ee071
noroadsleft <xxiinophobia@yahoo.com>
2018-06-15 17:50:43 -0700

    Fixes per drashna

--- keyboards/contra/keymaps/ryanm101/keymap.c
+++ keyboards/contra/keymaps/ryanm101/keymap.c
@@ -1,4 +1,5 @@
-#include QMK_KEYBOARD_H
+#include "contra.h"
+#include "action_layer.h"
 
 extern keymap_config_t keymap_config;
 
@@ -17,13 +18,13 @@ extern keymap_config_t keymap_config;
 #define _L2  3
 #define _L3  4
 
-enum custom_keycodes
+enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   QWERTYMAC,
   L1,
   L2,
   L3
-;
+};
 
 //Tap Dance Declarations
 #define COLON TD(CLN)
@@ -36,7 +37,7 @@ enum custom_keycodes
 #define CADCAE TD(CAD_CAE)
 #define SHIFTSLASHPIPE TD(TD_SHIFTSLASHPIPE)
 
-enum
+enum {
   CLN = 0,
   QUOT,
   CAD_CAE,
@@ -46,9 +47,10 @@ enum
   ANG,
   TD_TMUX,
   TD_SHIFTSLASHPIPE,
-;
+};
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
   /* Qwerty
    * ,-----------------------------------------------------------------------------------.
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
@@ -115,10 +117,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
    * `-----------------------------------------------------------------------------------'
    */
   [_L2] = LAYOUT_ortho_4x12(
-    KC_SYSREQ, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_NUMLOCK, KC_KP_7, KC_KP_8, KC_KP_9,   KC_KP_ASTERISK, KC_BSPC,
-    XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX,    KC_KP_4, KC_KP_5, KC_KP_6,   KC_KP_SLASH,    _______,
-    XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX,    KC_KP_1, KC_KP_2, KC_KP_3,   KC_KP_PLUS,     KC_KP_ENTER,
-    _______,   _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,     XXXXXXX, KC_KP_0, KC_KP_DOT, KC_KP_MINUS,    _______
+    KC_SYSREQ, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_NLCK, KC_P7,   KC_P8,    KC_P9,   KC_PAST, KC_BSPC,
+    XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_P4,   KC_P5,    KC_P6,   KC_PSLS, _______,
+    XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, KC_P1,   KC_P2,    KC_P3,   KC_PPLS, KC_PENT,
+    _______,   _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, KC_P0,    KC_PDOT, KC_PMNS, _______
   ),
 
   /* L3
@@ -139,115 +141,115 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   )
 
-;
+);
 
 // Parantheses
-void paranthesis_dance (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
+void paranthesis_dance (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
     SEND_STRING("()"); register_code(KC_LEFT); unregister_code(KC_LEFT);
-   else if (state->count == 2)
+  } else if (state->count == 2) {
     SEND_STRING("(");
-   else if (state->count == 3)
+  } else if (state->count == 3) {
     SEND_STRING(")");
+  }
+}
 
+void curly_dance (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    SEND_STRING("{}"); register_code(KC_LEFT); unregister_code(KC_LEFT);
+  } else if (state->count == 2) {
+    SEND_STRING("{");
+  } else if (state->count == 3) {
+    SEND_STRING("}");
+  }
+}
 
-
-void curly_dance (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
-    SEND_STRING(""); register_code(KC_LEFT); unregister_code(KC_LEFT);
-   else if (state->count == 2)
-    SEND_STRING("");
-   else if (state->count == 3)
-    SEND_STRING("");
-
-
-
-void square_dance (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
+void square_dance (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
     SEND_STRING("[]"); register_code(KC_LEFT); unregister_code(KC_LEFT);
-   else if (state->count == 2)
+  } else if (state->count == 2) {
     SEND_STRING("[");
-   else if (state->count == 3)
+  } else if (state->count == 3) {
     SEND_STRING("]");
+  }
+}
 
-
-
-void angular_dance (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
+void angular_dance (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
     SEND_STRING("<>"); register_code(KC_LEFT); unregister_code(KC_LEFT);
-   else if (state->count == 2)
+  } else if (state->count == 2) {
     SEND_STRING("<");
-   else if (state->count == 3)
+  } else if (state->count == 3) {
     SEND_STRING(">");
+  }
+}
 
-
-
-void tmux_dance (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
+void tmux_dance (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
     SEND_STRING("tmux"); register_code(KC_ENT); unregister_code(KC_ENT);
-   else if (state->count == 2)
+  } else if (state->count == 2) {
     register_mods(MOD_BIT(KC_LCTRL));
     register_code(KC_B); unregister_code(KC_B);
     unregister_mods(MOD_BIT(KC_LCTRL));
     register_mods(MOD_BIT(KC_LSHIFT));
     register_code(KC_5); unregister_code(KC_5);
     unregister_mods(MOD_BIT(KC_LSHIFT));
+  }
+}
 
-
-
-void cmd_dance (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
+void cmd_dance (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
     register_mods(MOD_BIT(KC_LCTRL));
     register_mods(MOD_BIT(KC_LALT));
     register_code(KC_DELETE);
     unregister_mods(MOD_BIT(KC_LCTRL));
     unregister_mods(MOD_BIT(KC_LALT));
     unregister_code(KC_DELETE);
-   else if (state->count == 2)
+  } else if (state->count == 2) {
     register_mods(MOD_BIT(KC_LGUI));
     register_mods(MOD_BIT(KC_LALT));
     register_code(KC_ESC);
     unregister_mods(MOD_BIT(KC_LGUI));
     unregister_mods(MOD_BIT(KC_LALT));
     unregister_code(KC_ESC);
-   else if (state->count == 3)
+  } else if (state->count == 3) {
     register_mods(MOD_BIT(KC_LGUI));
     register_mods(MOD_BIT(KC_LSHIFT));
     register_code(KC_4);
     unregister_mods(MOD_BIT(KC_LGUI));
     unregister_mods(MOD_BIT(KC_LSHIFT));
     unregister_code(KC_4);
+  }
+}
 
-
-
-void cmd_sft_slash_pipe_down (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
-    if (state->interrupted || state->pressed==0)
+void cmd_sft_slash_pipe_down (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    if (state->interrupted || state->pressed==0) {
       register_code (KC_NONUS_BSLASH);
-     else
+    } else {
       register_code (KC_LSFT);
-
-   else if (state->count == 2)
+    }
+  } else if (state->count == 2) {
     register_mods(MOD_BIT(KC_LSFT));
     register_code(KC_NONUS_BSLASH);
+  }
+}
 
-
-
-void cmd_sft_slash_pipe_up (qk_tap_dance_state_t *state, void *user_data)
-  if (state->count == 1)
-    if (keyboard_report->mods & MOD_BIT(KC_LSFT))
+void cmd_sft_slash_pipe_up (qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    if (keyboard_report->mods & MOD_BIT(KC_LSFT)) {
       unregister_code (KC_LSFT);
-     else
+    } else {
       unregister_code (KC_NONUS_BSLASH);
-
-   else if (state->count == 2)
+    }
+  } else if (state->count == 2) {
     unregister_mods(MOD_BIT(KC_LSFT));
     unregister_code(KC_NONUS_BSLASH);
-
-
+  }
+}
 
  //All tap dance functions would go here. Only showing this one.
- qk_tap_dance_action_t tap_dance_actions[] =
+ qk_tap_dance_action_t tap_dance_actions[] = {
    [CLN] = ACTION_TAP_DANCE_DOUBLE (KC_SCLN, S(KC_SCLN ))
    ,[QUOT] = ACTION_TAP_DANCE_DOUBLE (KC_QUOT, S(KC_2))
    ,[CAD_CAE] = ACTION_TAP_DANCE_FN_ADVANCED( NULL, NULL, cmd_dance )
@@ -257,4 +259,4 @@ void cmd_sft_slash_pipe_up (qk_tap_dance_state_t *state, void *user_data)
    ,[ANG] = ACTION_TAP_DANCE_FN_ADVANCED( NULL, NULL, angular_dance )
    ,[TD_TMUX] = ACTION_TAP_DANCE_FN_ADVANCED( NULL, NULL, tmux_dance )
    ,[TD_SHIFTSLASHPIPE] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, cmd_sft_slash_pipe_down, cmd_sft_slash_pipe_up)
- ;
+ };
