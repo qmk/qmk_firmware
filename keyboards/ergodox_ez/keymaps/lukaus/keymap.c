052e4cd9acbfcb893a2872e4d8097f2050513af8
Luke Stanley <lukewgs@gmail.com>
2018-06-18 10:55:02 -0500

    updated ergodox_ez keymap and initial commit for IBM Model M 122 key

--- keyboards/ergodox_ez/keymaps/lukaus/keymap.c
+++ keyboards/ergodox_ez/keymaps/lukaus/keymap.c
@@ -75,15 +75,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,   KC_W,     KC_E,         KC_R,       KC_T,         KC_F23,
     MO(3),      KC_A,   KC_S,     KC_D,         KC_F,       KC_G,
     KC_LSHIFT,  KC_Z,   KC_X,     KC_C,         KC_V,       KC_B,         KC_F24,
-    KC_LCTL,    KC_F8,  KC_LALT,  KC_LGUI,      KC_F13,
+    KC_LCTL,    KC_F8,  KC_LALT,  KC_F14,      KC_F13,
     
-      KC_HOME,  TO(0),  MEH_T(KC_NO), KC_SPACE, KC_LCTL,  KC_LALT,
+      KC_HOME,  TO(0),  KC_F15,  KC_SPACE, KC_LCTL,  KC_LALT,
     
     KC_6,       KC_7,   KC_8,     KC_9,         KC_0,       KC_MINUS,   KC_EQUAL,
     KC_F24,     KC_Y,   KC_U,     KC_I,         KC_O,       KC_P,       KC_BSLASH,
                 KC_H,   KC_J,     KC_K,         KC_L,       KC_SCOLON,  KC_QUOTE,
     KC_F17,     KC_N,   KC_M,     KC_COMMA,     KC_DOT,     KC_SLASH,   KC_RSHIFT,
-            KC_DELETE,  KC_F19,   KC_F20,       KC_F21,     KC_F22,
+            KC_DELETE,  KC_F19,   KC_LGUI,       KC_F21,     KC_F22,
 
       KC_F17,   KC_F18, KC_PGUP,  KC_PGDOWN,    KC_ENTER,   KC_BSPACE
   ),
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      TO(2),KC_TRANSPARENT,KC_TRANSPARENT,LALT(KC_F10),KC_ENTER,KC_BSPACE
   ),
   // Keypad, Lighting, and Mouse emulation layer
- ///* 
+ /* 
     [4] = KEYMAP(
     KC_ESCAPE,  KC_NO,      KC_NO,        KC_MS_BTN3, KC_NO,        KC_NO,          KC_NO,
     KC_TAB,     KC_NO,      KC_MS_BTN2,   KC_MS_UP,   KC_MS_BTN1,   KC_MS_WH_UP,    TO(0),
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_F17, KC_F18, KC_PGUP,  KC_PGDOWN,  KC_ENTER, KC_BSPACE
   )
   //*/
-  /*
+  ///*
   // Keypad, Lighting, and Mouse emulation layer
   [4] = KEYMAP(
     KC_ESCAPE,  KC_NO,      KC_NO,        KC_MS_BTN3, KC_NO,        KC_NO,          KC_NO,
