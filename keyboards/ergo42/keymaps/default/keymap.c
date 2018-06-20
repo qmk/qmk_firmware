fa47da89ab199361eaaced0ed507ab338f11e735
p <_>
2018-06-20 12:26:01 +0900

    Shift the bottom row of the right half

--- keyboards/ergo42/keymaps/default/keymap.c
+++ keyboards/ergo42/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |   Z  |   X  |   C  |   V  |   B  |  N   |   |  B   |   N  |   M  |   ,  |   .  |   /  |  \   |
+ * | Sft  |   Z  |   X  |   C  |   V  |   B  |  N   |   |      |  B   |   N  |   M  |   ,  |   .  |  /   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * |  BS  |PrtSc | App  | GUI  |Lang2/|Space | ESC  |   | Tab  |Enter |Lang1/| Left | Down |  Up  |Right |
  * |      |      |      |      |Alt   |      |      |   |      |      |Raise |      |      |      |      |
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = KEYMAP( \
-  KC_JYEN,  KC_Q,    KC_W,   KC_E,            KC_R,             KC_T,   KC_Y,    /**/ KC_EQL,  KC_Y,   KC_U,                KC_I,    KC_O,    KC_P,    KC_MINS,   \
+  KC_JYEN,  KC_Q,    KC_W,   KC_E,            KC_R,             KC_T,   KC_Y,    /**/ KC_EQL,  KC_Y,   KC_U,                KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LCTRL, KC_A,    KC_S,   LT(MOUSE, KC_D), KC_F,             KC_G,   S(KC_9), /**/ S(KC_0), KC_H,   KC_J,                KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,   KC_C,            KC_V,             KC_B,   KC_N,    /**/ KC_B,    KC_N,   KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_BSLASH, \
-  KC_BSPC,  KC_PSCR, KC_APP, KC_LGUI,         LALT_T(KC_LANG2), KC_SPC, KC_ESC,  /**/ KC_TAB,  KC_ENT, LT(RAISE, KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT   \
+  KC_LSFT,  KC_Z,    KC_X,   KC_C,            KC_V,             KC_B,   KC_N,    /**/ _______, KC_B,   KC_N,                KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+  KC_BSPC,  KC_PSCR, KC_APP, KC_LGUI,         LALT_T(KC_LANG2), KC_SPC, KC_ESC,  /**/ KC_TAB,  KC_ENT, LT(RAISE, KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT \
 ),
 
 /* Raise
@@ -41,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
  * |      |   !  |   @  |   #  |   $  |   %  |   [  |   |   ]  |   ^  |   &  |   *  |   (  |   )  |  +   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |   |   }  |Left  |Down  |  Up  |Right | F10  | F11  |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |   |   }  |Left  |Down  |  Up  |Right |   |  |  \   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * |      |      |      |      | GUI  |  BS  |      |   |      |      |      | Next | Vol- | Vol+ | Play |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [RAISE] = KEYMAP( \
-  RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    /**/ _______,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_EQL,    \
-  _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LBRC,    /**/ KC_RBRC,    S(KC_6), S(KC_7), S(KC_8), S(KC_9),  S(KC_0), S(KC_EQL), \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   S(KC_LBRC), /**/ S(KC_RBRC), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_F10,  KC_F11,    \
-  _______, _______, _______, _______, KC_LGUI, KC_BSPC, _______,    /**/ _______,    _______, _______, KC_MNXT, KC_VOLD,  KC_VOLU, KC_MPLY    \
+  RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    /**/ _______,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,         KC_EQL,    \
+  _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LBRC,    /**/ KC_RBRC,    S(KC_6), S(KC_7), S(KC_8), S(KC_9),  S(KC_0),      S(KC_EQL), \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   S(KC_LBRC), /**/ S(KC_RBRC), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, S(KC_BSLASH), KC_BSLASH, \
+  _______, _______, _______, _______, KC_LGUI, KC_BSPC, _______,    /**/ _______,    _______, _______, KC_MNXT, KC_VOLD,  KC_VOLU,      KC_MPLY    \
 ),
 
 /* Mouse
