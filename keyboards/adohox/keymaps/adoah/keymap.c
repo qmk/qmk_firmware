f53d671dede795a492aa8bd86eea525acf119d4b
Alex <xadoahx@gmail.com>
2018-06-21 09:36:16 +0300

    debugging keyboard problems

--- keyboards/adohox/keymaps/adoah/keymap.c
+++ keyboards/adohox/keymaps/adoah/keymap.c
@@ -47,11 +47,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                            |      |     |       |      |      |
  *                            `------------'       `------------'
  */
-[_QWERTY] = KEYMAP( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-          LT(KC_LCTRL, KC_LGUI), MO(LOWER), KC_SPC, KC_ENT,                     KC_BSPC, KC_LSHIFT, MO(RAISE), LT(KC_LALT, KC_TAB)               \
+  /*Check the raise layer*/
+  [_QWERTY] = KEYMAP( \
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+          LT(LOWER, KC_LGUI), KC_LCTRL, KC_SPC, KC_ENT,           KC_LALT, KC_BSPC, KC_LSHIFT, LT(RAISE, KC_TAB)\
 ),
 
 /* Raise
