bc8e59c9f7bf7e73fcc8bf27e133ebcbd61c24ab
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-06-21 10:41:51 -0700

    DZ60 Configurator fix and refactor (#3205)
    * Bugfix refactor
    
    * Added 60_iso layout to rules.mk
    
    * Added sample ISO-UK keymap
    
    * Keymap refactor
    
    * Fixes per @andys8

--- keyboards/dz60/keymaps/jkbone/keymap.c
+++ keyboards/dz60/keymaps/jkbone/keymap.c
@@ -12,27 +12,27 @@ enum layers_idx {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_BL] = LAYOUT_ISO(
-		KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,    KC_BSPC,
-		KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_ENT,
-		FL_ESC,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS,
-		KC_LSFT,    KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,        KC_RSFT,
-        KC_LCTL,     KC_LGUI,     KC_LALT,                      KC_SPC,                         FL, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT
-    ),
-	[_ML] = LAYOUT_ISO(
-		____,      ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,         ____,
-		____,        ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,     ____,     ____,
-		____,         ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,     ____,
-		____,       ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,          ____,
-        KC_LGUI,     KC_LALT,     KC_LCTL,                      ____,                             ____,    ____,    ____,    ____,    ____
-    ),
-	[_FL] = LAYOUT_ISO(
-		RESET,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,     KC_DEL,
-		____,        ____,   DF(_BL),   DF(_ML),    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,     ____,    ____,
-		____,         ____,    ____,    ____,    ____,    ____,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,    ____,     ____,     ____,
-		____,       F(3),     F(2),    F(1),    F(0),    ____,    ____,    ____, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY,          ____,
-        ____,        ____,      ____,                          ____,                             ____,    ____,    ____,    ____,    ____
-    )
+  [_BL] = LAYOUT_60_iso_5x1u(
+    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,    KC_BSPC,
+    KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_ENT,
+    FL_ESC,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS,
+    KC_LSFT,    KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,        KC_RSFT,
+    KC_LCTL,     KC_LGUI,     KC_LALT,                      KC_SPC,                         FL, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT
+  ),
+  [_ML] = LAYOUT_60_iso_5x1u(
+    ____,      ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,         ____,
+    ____,        ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,     ____,     ____,
+    ____,         ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,     ____,
+    ____,       ____,     ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,          ____,
+    KC_LGUI,     KC_LALT,     KC_LCTL,                      ____,                             ____,    ____,    ____,    ____,    ____
+  ),
+  [_FL] = LAYOUT_60_iso_5x1u(
+    RESET,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,     KC_DEL,
+    ____,        ____,   DF(_BL),   DF(_ML),    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,     ____,    ____,
+    ____,         ____,    ____,    ____,    ____,    ____,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,    ____,     ____,     ____,
+    ____,       F(3),     F(2),    F(1),    F(0),    ____,    ____,    ____, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY,          ____,
+    ____,        ____,      ____,                          ____,                             ____,    ____,    ____,    ____,    ____
+  )
 };
 
 enum function_id {
