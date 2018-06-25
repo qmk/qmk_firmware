2d04061201e79b5edf8790028455994a8475dbec
mechmerlin <mechmerlin@gmail.com>
2018-06-25 07:46:58 -0700

    Add QMK Configurator support

--- keyboards/org60/org60.h
+++ keyboards/org60/org60.h
@@ -20,7 +20,7 @@ inline void org60_caps_led_off(void)   { DDRB &= ~(1<<2); PORTB &= ~(1<<2); }
 inline void org60_bl_led_off(void)   	{ DDRF &= ~(1<<5); PORTF &= ~(1<<5); }
 
 /* Org60 Keymap Definition Macro */
-#define KEYMAP( \
+#define LAYOUT( \
       K00,  K01,  K02,  K03,  K04,  K05,  K06,  K07,  K08,  K09,  K0A,  K0B,  K0C,  K0D,  K49, \
       K10,  K11,  K12,  K13,  K14,  K15,  K16,  K17,  K18,  K19,  K1A,  K1B,  K1C,  K1D,       \
       K20,  K21,  K22,  K23,  K24,  K25,  K26,  K27,  K28,  K29,  K2A,  K2B,  K2C,  K2D,       \
