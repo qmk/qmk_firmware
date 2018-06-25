f7d4ecd1cdadacc5de9d7b94be4a1b1883294673
fordmr <mrford17@gmail.com>
2018-06-25 17:56:48 -0500

    Remove the RGB call
    Make would fail because of the RGB call that wasn't anywhere but here, cleaned that up from the default config.

--- keyboards/iris/keymaps/s1carii/config.h
+++ keyboards/iris/keymaps/s1carii/config.h
@@ -31,12 +31,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#undef RGBLED_NUM
-/*
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-*/
 #endif
