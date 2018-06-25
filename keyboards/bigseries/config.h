6ccd45c477d5a7563e9ddd698184689c963f8ab1
Cole Markham <github@ccmcomputing.net>
2018-06-24 23:59:37 -0500

    LED Testing

--- keyboards/bigseries/config.h
+++ keyboards/bigseries/config.h
@@ -54,6 +54,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D3
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 5
+#ifdef RGBLIGHT_LIMIT_VAL
+#undef RGBLIGHT_LIMIT_VAL
+#endif
+#define RGBLIGHT_LIMIT_VAL 128
 #endif
 
 #endif
