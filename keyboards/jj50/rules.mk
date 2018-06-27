b04e4ba72594b888ae3dec24e2cf3e834eb4d84f
noroadsleft <xxiinophobia@yahoo.com>
2018-06-26 23:59:46 -0700

    Rules update: add LAYOUTS = ortho_5x12

--- keyboards/jj50/rules.mk
+++ keyboards/jj50/rules.mk
@@ -27,7 +27,7 @@ F_CPU = 12000000
 
 # Bootloader
 #     This definition is optional, and if your keyboard supports multiple bootloaders of
-#     different sizes, comment this out, and the correct address will be loaded 
+#     different sizes, comment this out, and the correct address will be loaded
 #     automatically (+60). See bootloader.mk for all options.
 BOOTLOADER = bootloadHID
 
@@ -63,3 +63,5 @@ endif
 
 # programming options
 PROGRAM_CMD = ./keyboards/ps2avrGB/program $(TARGET).hex
+
+LAYOUTS = ortho_5x12
