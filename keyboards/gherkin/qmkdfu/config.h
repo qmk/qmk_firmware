8e916150f09fc8cba550bd6f1ce4e200d6ee0d5d
admin <admin@mbp.local>
2018-06-28 02:57:06 -0700

    add QMK DFU make option, bootloader replacement instructions
    in wanleg readme: update flashing instructions, add bootloader replacement instructions (replace default Caterina with QMK DFU).
    
    add make option for QMK DFU

---
+++ keyboards/gherkin/qmkdfu/config.h
@@ -0,0 +1,12 @@
+#ifndef QMKDFU_CONFIG_H
+#define QMKDFU_CONFIG_H
+
+#include "config_common.h"
+
+/* for QMK DFU */
+/* set top left key as ESC */
+#define QMK_ESC_OUTPUT B4 // usually COL
+#define QMK_ESC_INPUT F7 // usually ROW
+#define QMK_LED B0
+
+#endif
