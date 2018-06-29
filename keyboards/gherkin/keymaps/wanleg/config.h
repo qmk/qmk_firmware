9c458a494cca2682a0600d55702646a820722c45
wanleg <32079073+wanleg@users.noreply.github.com>
2018-06-29 13:20:44 -0700

    suggested revisions made

--- keyboards/gherkin/keymaps/wanleg/config.h
+++ keyboards/gherkin/keymaps/wanleg/config.h
@@ -4,10 +4,10 @@
 #include "../../config.h"
 #endif
 
-//#define SFTENT_KEY KC_A
-
-#define TAPPING_TERM 200
 #define PREVENT_STUCK_MODIFIERS
+
+//Tap Dance Prerequisite
+#define TAPPING_TERM 200
 
 //Mousekeys Settings
 #define MOUSEKEY_INTERVAL 16
@@ -15,3 +15,10 @@
 #define MOUSEKEY_TIME_TO_MAX 60
 #define MOUSEKEY_MAX_SPEED 7
 #define MOUSEKEY_WHEEL_DELAY 0
+
+/* for QMK DFU bootloader */
+/* not required if using default ProMicro bootloader */
+/* set top left key as bootloader mode escape key */
+#define QMK_ESC_OUTPUT B4 // usually COL
+#define QMK_ESC_INPUT F7 // usually ROW
+#define QMK_LED B0
