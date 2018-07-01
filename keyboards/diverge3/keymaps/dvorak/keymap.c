2ccf8c067b3b81f5e6a06c71c5da91b45d5864ef
Daniel Levett <dlevett@hotmail.co.uk>
2018-07-01 17:18:28 +0100

    Update diverge3 Dvorak keymap

--- keyboards/diverge3/keymaps/dvorak/keymap.c
+++ keyboards/diverge3/keymaps/dvorak/keymap.c
@@ -2,9 +2,6 @@
 #include "action_layer.h"
 #include "eeconfig.h"
 
-extern keymap_config_t keymap_config;
-qk_tap_dance_action_t tap_dance_actions[] = {};
-
 #define _DVORAK 0
 #define _LOWER 1
 #define _RAISE 2
