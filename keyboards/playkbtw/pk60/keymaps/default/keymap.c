3ac59263132b932e6b84feb9d514bd84205eef5b
noroadsleft <xxiinophobia@yahoo.com>
2018-07-02 01:00:21 -0700

    Keymap bugfix: deleted two extra commas

--- keyboards/playkbtw/pk60/keymaps/default/keymap.c
+++ keyboards/playkbtw/pk60/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,           \
     KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT, MO(1),   \
-    KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  MO(1),   KC_RALT, MO(1),   KC_NO,   KC_APP,  KC_RCTL,                                     \
+    KC_LCTL, KC_LGUI, KC_LALT, MO(1),                              KC_SPC,  MO(1),   KC_RALT, MO(1),   KC_NO,   KC_APP,  KC_RCTL           \
   ),
 
   [1] = LAYOUT_all( \
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,   _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PSCR, KC_CALC,          \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_SLCK, _______,          \
     _______, _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                     \
+    _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______           \
   ),
 
 };
