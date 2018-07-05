31bf387b00246e2f9c35aaa44265aca7c271283d
Kyle Brown <kdb424@gmail.com>
2018-07-05 01:37:44 -0700

    Having an L key is also nice...

--- keyboards/levinson/keymaps/kdb424/keymap.c
+++ keyboards/levinson/keymaps/kdb424/keymap.c
@@ -57,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = LAYOUT_ortho_4x12( \
-  KC_GESC,       KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,    KC_F,      KC_G,    KC_C,     KC_R,    KC_SLSH,  KC_BSPC, \
-  KC_TAB,        KC_A,    KC_O,    KC_E,    KC_U,   KC_I,    KC_D,      KC_H,    KC_T,     KC_N,    KC_S,     TD(TD_EQUAL_ENTER),  \
-  SFT_T(KC_BSPC),KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,    KC_B,      KC_M,    KC_W,     KC_V,    KC_Z,     TD(TD_DASH_SLASH),  \
-  KC_LCTL,       KC_LGUI, KC_LALT, MO(2),   MO(3),  KC_SPC,  KC_SPC,    MO(4),   KC_LEFT,  KC_DOWN, KC_UP,    CTL_T(KC_RGHT)  \
+  KC_GESC,       KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,    KC_F,      KC_G,    KC_C,     KC_R,    KC_L,    KC_BSPC, \
+  KC_TAB,        KC_A,    KC_O,    KC_E,    KC_U,   KC_I,    KC_D,      KC_H,    KC_T,     KC_N,    KC_S,    TD(TD_EQUAL_ENTER),  \
+  SFT_T(KC_BSPC),KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,    KC_B,      KC_M,    KC_W,     KC_V,    KC_Z,    TD(TD_DASH_SLASH),  \
+  KC_LCTL,       KC_LGUI, KC_LALT, MO(2),   MO(3),  KC_SPC,  KC_SPC,    MO(4),   KC_LEFT,  KC_DOWN, KC_UP,   CTL_T(KC_RGHT)  \
 ),
 
 /* Gaming
@@ -75,10 +75,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_GAMING] = LAYOUT_ortho_4x12( \
-  KC_GESC,       KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,    KC_F,      KC_G,    KC_C,     KC_R,    KC_SLSH,  KC_BSPC, \
-  KC_TAB,        KC_A,    KC_O,    KC_E,    KC_U,   KC_I,    KC_D,      KC_H,    KC_T,     KC_N,    KC_S,     TD(TD_EQUAL_ENTER),  \
-  SFT_T(KC_BSPC),KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,    KC_B,      KC_M,    KC_W,     KC_V,    KC_Z,     TD(TD_DASH_SLASH),  \
-  KC_LCTL,       KC_LBRC, KC_LALT, KC_SLSH, KC_RBRC,KC_SPC,  KC_SPC,    MO(4),   KC_LEFT,  KC_DOWN, KC_UP,    CTL_T(KC_RGHT)  \
+  KC_GESC,       KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,    KC_F,      KC_G,    KC_C,     KC_R,    KC_L,    KC_BSPC, \
+  KC_TAB,        KC_A,    KC_O,    KC_E,    KC_U,   KC_I,    KC_D,      KC_H,    KC_T,     KC_N,    KC_S,    TD(TD_EQUAL_ENTER),  \
+  SFT_T(KC_BSPC),KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,    KC_B,      KC_M,    KC_W,     KC_V,    KC_Z,    TD(TD_DASH_SLASH),  \
+  KC_LCTL,       KC_LBRC, KC_LALT, KC_SLSH, KC_RBRC,KC_SPC,  KC_SPC,    MO(4),   KC_LEFT,  KC_DOWN, KC_UP,   CTL_T(KC_RGHT)  \
 ),
 
 /* Raise1
