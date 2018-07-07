2ea995aaf296dfaeef610bf5e70e70c904f5b674
hinnebusch <josh@hinnebus.ch>
2018-07-06 22:08:56 -0400

    Update files to match new QMK structure

--- keyboards/h87a/h87a.h
+++ keyboards/h87a/h87a.h
@@ -22,12 +22,13 @@
 // The following is an example using the Planck MIT layout
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
-#define LAYOUT( K000,       K001, K011, K002, K012,    K003, K013, K004, K014,    K015, K006, K016, K007,    K017, K008, K018,  \
-				K020, K030, K021, K031, K022, K032, K023, K033, K024, K034, K025, K035, K026, K036, K027,    K037, K028, K038,  \
-				K040, K050, K041, K051, K042, K052, K043, K044, K054, K045, K055, K046, K056,                K057, K048, K058,  \
-				K060, K070, K061, K071, K062, K072, K063, K073, K064, K074, K065, K075, K066, K076,                             \
- 				K080, K090, K081, K091, K082, K092, K083, K093, K084, K094, K085, K095, K086, K096,                K088,        \
-				K100, K110, K101,                   K113,                   K105, K115, K106, K116,          K117, K108, K118   \
+#define LAYOUT(\
+	K000,       K001, K011, K002, K012,    K003, K013, K004, K014,    K015, K006, K016, K007,    K017, K008, K018,  \
+	K020, K030, K021, K031, K022, K032, K023, K033, K024, K034, K025, K035, K026, K036, K027,    K037, K028, K038,  \
+	K040, K050, K041, K051, K042, K052, K043, K053, K044, K054, K045, K055, K046, K056,                K057, K048, K058,  \
+	K060, K070, K061, K071, K062, K072, K063, K073, K064, K074, K065, K075, K066, K076,                             \
+ 	K080, K090, K081, K091, K082, K092, K083, K093, K084, K094, K085, K095, K086, K096,                K088,        \
+	K100, K110, K101,                   K113,                   K105, K115, K106, K116,          K117, K108, K118   \
 ) { \
 	{ K000,  K001,  K002,  K003,  K004,  KC_NO, K006,  K007,  K008 }, \
 	{ KC_NO, K011,  K012,  K013,  K014,  K015,  K016,  K017,  K018 }, \
--- keyboards/h87a/rules.mk
+++ keyboards/h87a/rules.mk
@@ -67,3 +67,4 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+EXTRAFLAGS += -flto
