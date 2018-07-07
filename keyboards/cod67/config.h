0f443be71901c5925a2627e5f8dd09f9814ba0e9
mechmerlin <mechmerlin@gmail.com>
2018-07-06 18:17:21 -0700

    fix comments

--- keyboards/cod67/config.h
+++ keyboards/cod67/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 REPLACE_WITH_YOUR_NAME
+Copyright 2018 MechMerlin
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -50,7 +50,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 #define BACKLIGHT_PIN D4
-#define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 3
 
 
