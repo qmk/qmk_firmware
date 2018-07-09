9acba6b0c6ea07b166adeec9c0c6779902ad39b0
Josh Hinnebusch <josh@hinnebus.ch>
2018-07-09 13:56:40 +0000

    Update config.h

--- keyboards/h87a/config.h
+++ keyboards/h87a/config.h
@@ -15,11 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
