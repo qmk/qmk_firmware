2c1a075c5995c1d20de50b069911998f21f3234a
Legonut <legonut3@gmail.com>
2018-07-09 19:29:24 -0400

    remove old helix stuff

--- keyboards/sol/rev2/keymaps/default/keymap.c
+++ keyboards/sol/rev2/keymaps/default/keymap.c
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   RGB_TOG, RGB_TOG, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   RGB_MOD, RGB_MOD, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   RGB_VAI,   RESET,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   RGB_TOG, BL_ON  , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   RGB_MOD, BL_STEP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   RGB_VAI,   RESET, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   RGB_VAD, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
       KC_LCTL, KC_LGUI, KC_LALT, RGB_SAI, RGB_SAD, LOWER,  KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
                                                    LOWER,  KC_SPC,  KC_SPC,  RAISE \
