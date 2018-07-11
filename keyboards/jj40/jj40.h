f3b45e8f7a452239e467d9836678163deea6b45b
noroadsleft <xxiinophobia@yahoo.com>
2018-07-11 01:38:21 -0700

    Refactor: rename LAYOUT_offset_right macro as LAYOUT_planck_1x2uR

--- keyboards/jj40/jj40.h
+++ keyboards/jj40/jj40.h
@@ -58,7 +58,7 @@ void matrix_init_user(void);  // TODO port this to other PS2AVRGB boards
 }
 
 
-#define LAYOUT_offset_right( \
+#define LAYOUT_planck_1x2uR( \
     K01, K02, K03, K04, K05, K06, K07, K08, K09, K010, K011, K012, \
     K11, K12, K13, K14, K15, K16, K17, K18, K19, K110, K111, K112, \
     K21, K22, K23, K24, K25, K26, K27, K28, K29, K210, K211, K212, \
