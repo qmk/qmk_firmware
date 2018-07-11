0487ac082435fed8b572230691cf91e61909cddd
Danny Nguyen <danny@keeb.io>
2018-07-11 12:13:24 -0400

    Add macro to play All Star chorus and print lyrics, fixes issues #3345, #3346, #3347, #3348, #3349, #3350

--- quantum/audio/song_list.h
+++ quantum/audio/song_list.h
@@ -435,4 +435,14 @@
     Q__NOTE(_AS5), \
     Q__NOTE(_C6), \
 
+#define ALL_STAR \
+    E__NOTE(_AS4), Q__NOTE(_FS4), S__NOTE(_FS4), S__NOTE(_DS4), E__NOTE(_FS4), Q__NOTE(_FS4), S__NOTE(_FS4), S__NOTE(_DS4), \
+    E__NOTE(_FS4), Q__NOTE(_FS4), Q__NOTE(_FS4), QD_NOTE(_AS4), \
+    E__NOTE(_AS4), Q__NOTE(_FS4), S__NOTE(_FS4), S__NOTE(_DS4), E__NOTE(_FS4), Q__NOTE(_FS4), S__NOTE(_FS4), S__NOTE(_DS4), \
+    E__NOTE(_FS4), Q__NOTE(_FS4), Q__NOTE(_FS4), Q__NOTE(_AS4), E__NOTE(_REST),\
+    Q__NOTE(_AS4), Q__NOTE(_CS5), E__NOTE(_B4), E__NOTE(_CS5), E__NOTE(_DS5), Q__NOTE(_FS5), \
+    E__NOTE(_GS5), Q__NOTE(_GS5), E__NOTE(_FS4), E__NOTE(_FS4), E__NOTE(_GS4), E__NOTE(_FS4), \
+    E__NOTE(_AS4), Q__NOTE(_GS4), Q__NOTE(_GS4), Q__NOTE(_FS4), Q__NOTE(_GS4), \
+    E__NOTE(_AS4), HD_NOTE(_DS4)
+
 #endif
