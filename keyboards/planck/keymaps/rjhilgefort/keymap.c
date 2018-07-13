88d034d5f9fca70a6859634286c2f6ca2133b854
Rob Hilgefort <rjhilgefort@gmail.com>
2018-07-13 10:16:29 -0600

    removed refs to bad layers, formatting comments

--- keyboards/planck/keymaps/rjhilgefort/keymap.c
+++ keyboards/planck/keymaps/rjhilgefort/keymap.c
@@ -23,13 +23,11 @@ enum planck_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
-  _PLOVER,
   _ADJUST
 };
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  PLOVER,
   BACKLIT,
   EXT_PLV
 };
@@ -40,21 +38,21 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,---------------------------------------------------------------------------------------.
- * | Tab      |   Q  |   W  |   E   |  R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |----------+------+------+-------+-----+-------------+------+------+------+------+------|
- * | Ctrl/Esc |   A  |   S  |   D   |  F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |----------+------+------+-------+-----+------|------+------+------+------+------+------|
- * | Shift    |   Z  |   X  |   C   |  V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |----------+------+------+-------+-----+------+------+------+------+------+------+------|
- * |          | Ctrl | Alt  | Lower | GUI |    Space    |Raise | Left | Down |  Up  |Right |
- * `---------------------------------------------------------------------------------------'
+ * ,-------------------------------------------------------------------------------------------------------.
+ * | Tab               |   Q    |   W    |   E   |   R    |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |-------------------+--------+--------+-------+--------+-------------+------+------+------+------+------|
+ * | Ctrl/Esc          |   A    |   S    |   D   |   F    |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |-------------------+--------+--------+-------+--------+------|------+------+------+------+------+------|
+ * | Shift             |   Z    |   X    |   C   |   V    |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |-------------------+--------+--------+-------+--------+------+------+------+------+------+------+------|
+ * |                   | Ctrl   |  Alt   | Lower |  GUI   |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,               KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT,              KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {____________________, KC_LCTL, KC_LALT, LOWER, KC_LGUI, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_TAB,               KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT,              KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {____________________, KC_LCTL, KC_LALT, LOWER,  KC_LGUI, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
@@ -93,30 +91,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
-/* Plover layer (http://opensteno.org)
- * ,-----------------------------------------------------------------------------------.
- * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Exit |      |      |   A  |   O  |             |   E  |   U  |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-
-[_PLOVER] = {
-  {KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   },
-  {XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC},
-  {XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX}
-},
-
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -124,10 +103,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   DEBUG,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL },
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,  _______},
+  {_______, RESET,   _____,   _______, _______, _______, _______, _______, _______,  _______, _______, KC_DEL },
+  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  ________, _______, _______, _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, _______, _______, _______, _______, _______, _______, _______, ________, _______, _______, _______}
 }
 
 
