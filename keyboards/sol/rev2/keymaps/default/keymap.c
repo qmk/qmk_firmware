b8efab3d65cdede2ac96d930923783c066389a07
Legonut <legonut3@gmail.com>
2018-07-13 16:54:03 -0400

    keymap changes and updates to rgblight.c

--- keyboards/sol/rev2/keymaps/default/keymap.c
+++ keyboards/sol/rev2/keymaps/default/keymap.c
@@ -80,8 +80,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   RGB_MOD, BL_STEP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
       ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   RGB_VAI,   RESET, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   RGB_VAD, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      KC_LCTL, KC_LGUI, KC_LALT, RGB_SAI, RGB_SAD, LOWER,  KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
-                                                   LOWER,  KC_SPC,  KC_SPC,  RAISE \
+      KC_LCTL, KC_LGUI, KC_LALT, RGB_SAI, RGB_SAD, KC_SPC, KC_DEL,  LOWER,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
+                                                   KC_SPC, KC_DEL,  KC_SPC, KC_SPC\
       ),
 
   /* Colemak
@@ -131,7 +131,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
                                                    LOWER,   KC_SPC,  KC_SPC,  RAISE \
       ),
-
+  /* Lower
+   * ,------------------------------------------------.  ,------------------------------------------------.
+   * |   ~  |   !  |   @  |   #  |   $  |   %  |      |  |      |   ^  |   &  |   *  |   (  |   )  |      |
+   * |------+------+------+------+------+------|------|  |------|------+------+------+------+------+------|
+   * |   ~  |   !  |   @  |   #  |   $  |   %  |      |  |      |   ^  |   &  |   *  |   (  |   )  |      |
+   * |------+------+------+------+------+------|------|  |------|------+------+------+------+------+------|
+   * | Ctrl |   A  |   O  |   E  |   U  |   I  |      |  |      |   D  |   H  |   T  |   N  |   S  |  /   |
+   * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+   * | Shift|   ;  |   Q  |   J  |   K  |   X  |   [  |  |   ]  |   B  |   M  |   W  |   V  |   Z  |Enter |
+   * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+   * |Adjust| Esc  | Alt  | GUI  | EISU |Lower |Space |  |Space |Raise | KANA | Left | Down |  Up  |Right |
+   * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+   *                                    |Lower |Space |  |Space |Raise |
+   *                                    `-------------'  `------------'
+   */
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
