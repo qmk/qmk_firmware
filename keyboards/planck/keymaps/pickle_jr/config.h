2a0ba7970b64e0583e271b21008b8fa2c6aff5d1
Taylor Gonzalez <Taylor.Gonzalez@aero.bombardier.com>
2018-07-13 07:57:58 -0500

    Updated mouse key speed

--- keyboards/planck/keymaps/pickle_jr/config.h
+++ keyboards/planck/keymaps/pickle_jr/config.h
@@ -7,12 +7,11 @@
     #define STARTUP_SONG SONG(PLANCK_SOUND)
 #endif
 
-#define MOUSEKEY_DELAY             1
-#define MOUSEKEY_INTERVAL          1
-#define MOUSEKEY_MAX_SPEED         2
-#define MOUSEKEY_TIME_TO_MAX       100
-#define MOUSEKEY_WHEEL_MAX_SPEED   5
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+#define MOUSEKEY_DELAY       0
+#define MOUSEKEY_INTERVAL    16
+#define MOUSEKEY_MAX_SPEED   7
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_WHEEL_DELAY 0
 
 /*
  * MIDI options
