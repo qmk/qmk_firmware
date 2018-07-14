6c6178a33f14e0153336fd6899a4eb07a2ba6f6b
Rob Hilgefort <rjhilgefort@gmail.com>
2018-07-13 23:40:51 -0600

    First version of my ergodox ported layout!

--- keyboards/planck/keymaps/rjhilgefort/keymap.c
+++ keyboards/planck/keymaps/rjhilgefort/keymap.c
@@ -38,21 +38,21 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-------------------------------------------------------------------------------------------------------.
- * | Tab               |   Q    |   W    |   E   |   R    |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |-------------------+--------+--------+-------+--------+-------------+------+------+------+------+------|
- * | Ctrl/Esc          |   A    |   S    |   D   |   F    |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |-------------------+--------+--------+-------+--------+------|------+------+------+------+------+------|
- * | Shift             |   Z    |   X    |   C   |   V    |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |-------------------+--------+--------+-------+--------+------+------+------+------+------+------+------|
- * |                   | Ctrl   |  Alt   | Lower |  GUI   |    Space    |Raise | Left | Down |  Up  |Right |
- * `-------------------------------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------------------------------------.
+ * | Tab               |   Q    |   W  |   E   |  R  |   T  |   Y  |   U  |   I    |   O  |   P  | Bksp  |
+ * |-------------------+--------+------+-------+-----+-------------+------+--------+------+------+-------|
+ * | Ctrl/Esc          |   A    |   S  |   D   |  F  |   G  |   H  |   J  |   K    |   L  |   ;  |  "    |
+ * |-------------------+--------+------+-------+-----+------|------+------+--------+------+------+-------|
+ * | Shift             |   Z    |   X  |   C   |  V  |   B  |   N  |   M  |   ,    |   .  |   /  | Enter |
+ * |-------------------+--------+------+-------+-----+------+------+------+--------+------+------+-------|
+ * | Raise             | Ctrl   | Alt  | Lower | GUI |    Space    | Enter | Lower |      |      | Raise |
+ * `-----------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,               KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT,              KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {____________________, KC_LCTL, KC_LALT, LOWER,  KC_LGUI, KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_TAB,               KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT,              KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
+  {RAISE,                KC_LCTL, KC_LALT, LOWER, KC_LGUI, KC_SPC,  KC_SPC,  KC_ENT,  LOWER,   _______, _______, RAISE  }
 },
 
 /* Lower
@@ -67,11 +67,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY}
+  {_______, KC_BSLS, KC_SLSH, KC_LBRC, KC_RBRC, KC_TILD, _______, KC_EQL,  KC_PLUS, KC_MINS,  KC_UNDS, KC_BSPC},
+  {_______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_GRV,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______},
+  {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______}
 },
+/* [_LOWER] = { */
+/*   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC}, */
+/*   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE}, */
+/*   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______}, */
+/*   {_______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY} */
+/* }, */
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -85,11 +91,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  {_______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9,   _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_4, KC_5, KC_6,   _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3,   _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_0, KC_0, KC_DOT, _______, _______}
 },
+/* [_RAISE] = { */
+/*   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC}, */
+/*   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS}, */
+/*   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______}, */
+/*   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY} */
+/* }, */
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
@@ -103,10 +115,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   _____,   _______, _______, _______, _______, _______, _______,  _______, _______, KC_DEL },
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  ________, _______, _______, _______},
+  {_______, RESET,   _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_DEL },
+  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______, _______, _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, ________, _______, _______, _______}
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______}
 }
 
 
@@ -127,63 +139,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         print("mode just switched to qwerty and this is a huge string\n");
         set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-      break;
-    case DVORAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
-      }
-      return false;
-      break;
-    case BACKLIT:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-        #ifdef BACKLIGHT_ENABLE
-          backlight_step();
-        #endif
-        #ifdef KEYBOARD_planck_rev5
-          PORTE &= ~(1<<6);
-        #endif
-      } else {
-        unregister_code(KC_RSFT);
-        #ifdef KEYBOARD_planck_rev5
-          PORTE |= (1<<6);
-        #endif
-      }
-      return false;
-      break;
-    case PLOVER:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          stop_all_notes();
-          PLAY_SONG(plover_song);
-        #endif
-        layer_off(_RAISE);
-        layer_off(_LOWER);
-        layer_off(_ADJUST);
-        layer_on(_PLOVER);
-        if (!eeconfig_is_enabled()) {
-            eeconfig_init();
-        }
-        keymap_config.raw = eeconfig_read_keymap();
-        keymap_config.nkro = 1;
-        eeconfig_update_keymap(keymap_config.raw);
-      }
-      return false;
-      break;
-    case EXT_PLV:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(plover_gb_song);
-        #endif
-        layer_off(_PLOVER);
       }
       return false;
       break;
