9025cc18f7edcb1a061e70dceeac730338cedf7f
u/andylikescandy <andylikescandy@gmail.com>
2018-07-15 12:30:34 -0700

    Swapped RALT and RGUI

--- keyboards/bfo9000/keymaps/andylikescandy6x18/keymap.c
+++ keyboards/bfo9000/keymaps/andylikescandy6x18/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,     KC_W,     KC_F,    KC_P,     KC_G,          KC_END,         KC_P7,            KC_P8,        KC_P9,          KC_PMNS,    KC_PGUP,              KC_J,    KC_L,        KC_U,       KC_Y,       KC_SCLN,        KC_BSPC,         \
     KC_ESC,     KC_A,     KC_R,     KC_S,    KC_T,     KC_D,          KC_DEL,         KC_P4,            KC_P5,        KC_P6,          KC_PPLS,    KC_PGDN,              KC_H,    KC_N,        KC_E,       KC_I,       KC_O,           KC_QUOT,          \
     KC_LSFT,    KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,          SFT_T(KC_ENT),  KC_P1,            KC_P2,        KC_P3,          KC_UP,      KC_BSLASH,            KC_K,    KC_M,        KC_COMM,    KC_DOT,     KC_SLSH,        SFT_T(KC_ENT),      \
-    KC_LCTL,    CTRLSFT,  KC_LGUI,  KC_LALT, RSESPC,   NAVSPC,        KC_LCTL,        KC_P0,            KC_PDOT,      KC_LEFT,        KC_DOWN,    KC_RGHT,              NAVSPC,  RSESPC,      KC_RGUI,    KC_RALT,    KC_APPLICATION, KC_RCTL       \
+    KC_LCTL,    CTRLSFT,  KC_LGUI,  KC_LALT, RSESPC,   NAVSPC,        KC_LCTL,        KC_P0,            KC_PDOT,      KC_LEFT,        KC_DOWN,    KC_RGHT,              NAVSPC,  RSESPC,      KC_RALT,    KC_RGUI,    KC_APPLICATION, KC_RCTL       \
 ),
 [_QWERTY] = LAYOUT( \
     CTLALTSFT,  KC_F1,    KC_F2,    KC_F3,   KC_F4,    KC_F5,         KC_F6,          KC_F7,            KC_F8,        KC_F9,          KC_F10,     KC_F11,               KC_F12,  KC_PSCREEN,  KC_PAUSE,   KC_CAPS,    KC_INSERT,      KC_EQL,     \
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,          KC_END,         KC_P7,            KC_P8,        KC_P9,          KC_PMNS,    KC_PGUP,              KC_Y,    KC_U,        KC_I,       KC_O,       KC_P,        KC_BSPC,         \
     KC_ESC,     KC_A,     KC_S,     KC_D,    KC_F,     KC_G,          KC_DEL,         KC_P4,            KC_P5,        KC_P6,          KC_PPLS,    KC_PGDN,              KC_H,    KC_J,        KC_K,       KC_L,       KC_SCLN,           KC_QUOT,          \
     KC_LSFT,    KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,          SFT_T(KC_ENT),  KC_P1,            KC_P2,        KC_P3,          KC_UP,      KC_BSLASH,            KC_N,    KC_M,        KC_COMM,    KC_DOT,     KC_SLSH,        SFT_T(KC_ENT),      \
-    KC_LCTL,    CTRLSFT,  KC_LGUI,  KC_LALT, RSESPC,   NAVSPC,        KC_LCTL,        KC_P0,            KC_PDOT,      KC_LEFT,        KC_DOWN,    KC_RGHT,              NAVSPC,  RSESPC,      KC_RGUI,    KC_RALT,    KC_APPLICATION, KC_RCTL       \
+    KC_LCTL,    CTRLSFT,  KC_LGUI,  KC_LALT, RSESPC,   NAVSPC,        KC_LCTL,        KC_P0,            KC_PDOT,      KC_LEFT,        KC_DOWN,    KC_RGHT,              NAVSPC,  RSESPC,      KC_RALT,    KC_RGUI,    KC_APPLICATION, KC_RCTL       \
 ),
 [_RAISE] = LAYOUT( \
     CTLALTDEL,  SWBASE,     SWQWERTY,   XXXXXXX,    XXXXXXX,        XXXXXXX,    XXXXXXX,      XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,                   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
