96cb9f4661faa80e795b1e6731b7a8e8a50bd0cb
Evan Travers <evantravers@gmail.com>
2018-07-16 11:44:16 -0400

    Update template config.h with `#pragma once` (#3415)
    * Update template config.h with `#pragma once`
    
    Making sure this is consistent with #3404
    
    * Move to `#pragma once` for the other templates

--- quantum/template/ps2avrgb/config.h
+++ quantum/template/ps2avrgb/config.h
@@ -15,10 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#include "config_common.h"
+#pragma once
 
 #define VENDOR_ID       0x20A0
 #define PRODUCT_ID      0x422D
@@ -45,5 +42,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key combination for command */
 #define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
-
-#endif
