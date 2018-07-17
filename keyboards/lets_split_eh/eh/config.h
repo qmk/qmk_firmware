fca0249b3308618c9c26fd660b072328d7c5fa63
That-Canadian <Poole.Chris.11@gmail.com>
2018-07-16 19:25:02 -0700

    Lets split eh (#3120)
    * Line ending stuff again
    
    * Added Let's Split Eh? Files and updated #USE_IC2 checks to also include th EH revision (can only be used in I2C)
    
    * Added personal keymap, updated some of the EH files
    
    * Created new keyboard file for testing "lets_split_eh" will merge into lets_split once fully functional
    
    * Added split code from lets_split, removed pro micro imports and LED code
    
    THIS IS WORKING CODE, WITHOUT RGB AND BACKLIGHT
    
    * Took back original Lets Slit files for the lets_split keyboard, working in the lets_split_eh folder for now
    
    * Updated eh.c
    
    * More rework of the I2C code, added global flags for split boards.
    
    * Introduced RGB over I2C, having weird edge case issues at the moment though
    
    * Fixed weird I2C edgecase with RGB, although still would like to track down route cause..
    
    * Changed RGB keycodes (static ones) to activate on key-up instead of key-down to elimate weird ghosting issue over I2C
    
    * Lots of changes, mainly externalized the Split keyboard code and added logic for only including when needed.
    
    - Added makefile option "SPLIT_KEYBOARD" that when = yes will include the split keyboard files and custom matrix
    - Split keyboard files placed into quantum/split_common/
    - Added define option for config files "SPLIT_HAND_PIN" FOr using high/low pin to determine handedness, low = right hand, high = left hand
    - Cleaned up split logic for RGB and Backlight so it is only exectuted / included when needed
    
    * Updated documentation for the new makefile options and #defines specific to split keyboards
    
    * Added a bit more info to docs, so people aren't confused
    
    * Modifed Let's Split to use externalized code, also added left and right hand eeprom files to the split_common folder
    
    * Removed some debugging from eh.c
    
    * Small changes to keyboard configs. Also added a default keymap (just a copy of my that_canadian keymap).
    
    * Added a README file to the Let's Split Eh?
    
    * Changed it so RGB static updates are done on key-up ONLY for split boards rather than all boards. Also fixed leftover un-used variable in rgblight.c
    
    * Updated default keymap and my keymap for Let's Split Eh? Updated the comments so it reflects RGB control, and removed audio functions.
    
    * Fixed lets_split_eh not having a default version
    
    * Removed "eh" references from lets_split folder for now
    
    * Took lets_split folder from master to fix travis build errors, weird my local was overriding.
    
    * Changed LAYOUT_ortho_4x12_kc -> LAYOUT_kc_ortho_4x12 to match bakingpy and others
    
    * Removed rules.mk from my lets_split keymap, not needed
    
    * Updated the config_options doc to better explain the usage of "#define SPLIT_HAND_PIN"

---
+++ keyboards/lets_split_eh/eh/config.h
@@ -0,0 +1,47 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef EH_CONFIG_H
+#define EH_CONFIG_H
+
+// To let configuration know this is of type EH, which will force I2C irregardless of user config
+#define EH
+
+#include "config_common.h"
+
+/* Let's Split EH? pin-out */
+#define MATRIX_ROW_PINS { B1, B3, D7, B4 }
+#define MATRIX_COL_PINS { F4, F5, C6, B6, B5, D5 }
+#define UNUSED_PINS
+
+#define DIODE_DIRECTION COL2ROW
+
+#define BACKLIGHT_PIN B7
+
+/* number of backlight levels */
+#define BACKLIGHT_LEVELS 3
+
+/* ws2812 RGB LED */
+#define RGB_DI_PIN B2
+#define RGBLED_NUM 6    // Number of LEDs (each hand)
+
+/* Split Defines */
+#define SPLIT_HAND_PIN D3
+
+#endif
+
---
+++ quantum/split_common/split_util.c
@@ -0,0 +1,145 @@
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/eeprom.h>
+#include "split_util.h"
+#include "matrix.h"
+#include "keyboard.h"
+#include "config.h"
+#include "timer.h"
+#include "split_flags.h"
+
+#ifdef RGBLIGHT_ENABLE
+#   include "rgblight.h"
+#endif
+#ifdef BACKLIGHT_ENABLE
+#   include "backlight.h"
+#endif
+
+#ifdef SPLIT_HAND_PIN
+#   include "pincontrol.h"
+#endif
+
+#if defined(USE_I2C) || defined(EH)
+#  include "i2c.h"
+#else
+#  include "serial.h"
+#endif
+
+volatile bool isLeftHand = true;
+
+volatile uint8_t setTries = 0;
+
+static void setup_handedness(void) {
+  #ifdef SPLIT_HAND_PIN
+    // Test pin SPLIT_HAND_PIN for High/Low, if low it's right hand
+    pinMode(SPLIT_HAND_PIN, PinDirectionInput);
+    isLeftHand = digitalRead(SPLIT_HAND_PIN);
+  #else
+    #ifdef EE_HANDS
+      isLeftHand = eeprom_read_byte(EECONFIG_HANDEDNESS);
+    #else
+      // I2C_MASTER_RIGHT is deprecated, use MASTER_RIGHT instead, since this works for both serial and i2c
+      #if defined(I2C_MASTER_RIGHT) || defined(MASTER_RIGHT)
+        isLeftHand = !has_usb();
+      #else
+        isLeftHand = has_usb();
+      #endif
+    #endif
+  #endif
+}
+
+static void keyboard_master_setup(void) {
+#if defined(USE_I2C) || defined(EH)
+  i2c_master_init();
+  #ifdef SSD1306OLED
+    matrix_master_OLED_init ();
+  #endif
+#else
+  serial_master_init();
+#endif
+
+    // For master the Backlight info needs to be sent on startup
+    // Otherwise the salve won't start with the proper info until an update
+    BACKLIT_DIRTY = true;
+}
+
+static void keyboard_slave_setup(void) {
+  timer_init();
+#if defined(USE_I2C) || defined(EH)
+    i2c_slave_init(SLAVE_I2C_ADDRESS);
+#else
+    serial_slave_init();
+#endif
+}
+
+bool has_usb(void) {
+   USBCON |= (1 << OTGPADE); //enables VBUS pad
+   _delay_us(5);
+   return (USBSTA & (1<<VBUS));  //checks state of VBUS
+}
+
+void split_keyboard_setup(void) {
+   setup_handedness();
+
+   if (has_usb()) {
+      keyboard_master_setup();
+   } else {
+      keyboard_slave_setup();
+   }
+   sei();
+}
+
+void keyboard_slave_loop(void) {
+   matrix_init();
+   
+   //Init RGB
+   #ifdef RGBLIGHT_ENABLE
+      rgblight_init();
+   #endif
+
+   while (1) {
+      matrix_slave_scan();
+      
+      // read backlight info
+    #ifdef BACKLIGHT_ENABLE
+        if (BACKLIT_DIRTY) {
+            backlight_set(i2c_slave_buffer[I2C_BACKLIT_START]);
+            BACKLIT_DIRTY = false;
+        }
+    #endif
+    #ifdef RGBLIGHT_ENABLE
+        if (RGB_DIRTY) {
+            cli();
+            uint32_t dword;
+            
+            /*dword = i2c_slave_buffer[I2C_RGB_START + 3];
+            dword = (dword << 8) + i2c_slave_buffer[I2C_RGB_START + 2];
+            dword = (dword << 8) + i2c_slave_buffer[I2C_RGB_START + 1];
+            dword = (dword << 8) + i2c_slave_buffer[I2C_RGB_START];*/
+            
+            
+            uint8_t *dword_dat = (uint8_t *)(&dword);
+            for (int i = 0; i < 4; i++) {
+                dword_dat[i] = i2c_slave_buffer[I2C_RGB_START+i];
+            }
+
+            rgblight_update_dword(dword);
+            RGB_DIRTY = false;
+            sei();
+        }
+    #endif
+   }
+}
+
+// this code runs before the usb and keyboard is initialized
+void matrix_setup(void) {
+    split_keyboard_setup();
+
+    if (!has_usb()) {
+        //rgblight_init();
+        keyboard_slave_loop();
+    }
+}
