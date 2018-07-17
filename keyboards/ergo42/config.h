cee28296933daed5aec60f9d39726e5dd566347c
p <_>
2018-07-17 12:18:45 +0900

    Move user settings

--- keyboards/ergo42/config.h
+++ keyboards/ergo42/config.h
@@ -21,30 +21,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define CATERINA_BOOTLOADER     /* Use caterina bootloader to make RESET keycode available */
-#define PREVENT_STUCK_MODIFIERS /* Auto-restore layer when the layer key is released */
-
-#define MOUSEKEY_DELAY             32
-#define MOUSEKEY_INTERVAL          16
-#define MOUSEKEY_MOVE_DELTA        3
-#define MOUSEKEY_MAX_SPEED         11 /* times faster than MOVE_DELTA */
-#define MOUSEKEY_TIME_TO_MAX       10
-
-#define MOUSEKEY_WHEEL_DELAY       48
-#define MOUSEKEY_WHEEL_INTERVAL    48
-#define MOUSEKEY_WHEEL_DELTA       1
-#define MOUSEKEY_WHEEL_MAX_SPEED   1 /* times faster */
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 1
-
-/* Prefer "hold" when "down -> up -> down" (recognized immediately as "tap twice" by default) */
-#define TAPPING_FORCE_HOLD
-
-/* Prefer "hold" when "SandS Dn -> A Dn -> A Up -> SandS Up" within the TAPPING_TERM */
-#define PERMISSIVE_HOLD
-#define PERMISSIVE_HOLD_SHIFT_ONLY
-
-/* Prefer "tap" when "SandS Dn -> A Dn -> Sands Up -> A Up" within the TAPPING_TERM */
-/* (Like IGNORE_MOD_TAP_INTERRUPT but only ignores SHIFT) */
-#define IGNORE_SHIFT_TAP_INTERRUPT
-
 #endif
--- keyboards/ergo42/keymaps/phi/config.h
+++ keyboards/ergo42/keymaps/phi/config.h
@@ -23,15 +23,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../../config.h"
 
-/* Use I2C or Serial, not both */
+/* split keyboard setup */
+#define USE_SERIAL              /* SERIAL or I2C */
+#define MASTER_RIGHT            /* RIGHT or LEFT (or EE_HANDS) */
 
-#define USE_SERIAL
-// #define USE_I2C
+#define CATERINA_BOOTLOADER     /* Use caterina bootloader to make RESET keycode available */
+#define PREVENT_STUCK_MODIFIERS /* Auto-restore layer when the layer key is released */
 
-/* Select hand configuration */
+#define MOUSEKEY_DELAY             32
+#define MOUSEKEY_INTERVAL          16
+#define MOUSEKEY_MOVE_DELTA        3
+#define MOUSEKEY_MAX_SPEED         11 /* times faster than MOVE_DELTA */
+#define MOUSEKEY_TIME_TO_MAX       10
 
-/* #define MASTER_LEFT */
-#define MASTER_RIGHT
-// #define EE_HANDS
+#define MOUSEKEY_WHEEL_DELAY       48
+#define MOUSEKEY_WHEEL_INTERVAL    48
+#define MOUSEKEY_WHEEL_DELTA       1
+#define MOUSEKEY_WHEEL_MAX_SPEED   1 /* times faster */
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 1
+
+/* Prefer "hold" when "down -> up -> down" (recognized immediately as "tap twice" by default) */
+#define TAPPING_FORCE_HOLD
+
+/* Prefer "hold" when "SandS Dn -> A Dn -> A Up -> SandS Up" within the TAPPING_TERM */
+#define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD_SHIFT_ONLY
+
+/* Prefer "tap" when "SandS Dn -> A Dn -> Sands Up -> A Up" within the TAPPING_TERM */
+/* (Like IGNORE_MOD_TAP_INTERRUPT but only ignores SHIFT) */
+#define IGNORE_SHIFT_TAP_INTERRUPT
 
 #endif
