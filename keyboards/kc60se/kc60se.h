c8ea268f5fe5baabd79407283456a0c17098c092
noroadsleft <xxiinophobia@yahoo.com>
2018-07-19 09:31:06 -0700

    Insert KC_NO (___) element into bottom row of LAYOUT_60_ansi matrix
    Reported by @NimMooMoo on QMK Discord
    
    Right-hand modifiers on bottom row were shifted one key to the right. After debugging, discovered that the "electrical matrix" (the part of the matrix macro that determines which row and column a key uses) was one element short on the last row at K49, causing the rest of the row to be offset by one column.
    
    This commit corrects that issue.

--- keyboards/kc60se/kc60se.h
+++ keyboards/kc60se/kc60se.h
@@ -55,7 +55,7 @@ inline void kc60se_caps_led_off(void) { DDRB &= ~(1<<2); PORTB &= ~(1<<2); }
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D }, \
     { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, ___, K2D }, \
     { K30, ___, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, ___, K3D }, \
-    { K40, K41, K42, ___, ___, K45, ___, ___, ___, K4A, K4B, K4C, K4D }  \
+    { K40, K41, K42, ___, ___, K45, ___, ___, ___, ___, K4A, K4B, K4C, K4D }  \
 }
 
 /* HHKB
