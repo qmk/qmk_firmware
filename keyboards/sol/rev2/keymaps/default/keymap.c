20368a2c1e6b57e3e4e96e4d2c59c804d31335c9
Legonut <legonut3@gmail.com>
2018-07-19 14:50:16 -0400

    Redo keymap a bit

--- keyboards/sol/rev2/keymaps/default/keymap.c
+++ keyboards/sol/rev2/keymaps/default/keymap.c
@@ -76,9 +76,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                    `-------------'  `------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   RGB_TOG, BL_ON  , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   RGB_TOG, BL_ON  , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   RGB_MOD, BL_STEP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
-      ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   RGB_VAI,   RESET, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   RGB_VAI,   RESET, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   RGB_VAD, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
       KC_LCTL, KC_LGUI, KC_LALT, RGB_SAI, RGB_SAD, KC_SPC, KC_DEL,  LOWER,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
                                                    KC_SPC, KC_DEL,  KC_SPC, KC_SPC\
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   RGB_MOD, RGB_MOD, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL, \
       KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   RESET,   RESET,   KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC, KC_RBRC, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,  KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
+      KC_LCTL,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,  KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
                                                    LOWER,  KC_SPC,  KC_SPC,  RAISE \
       ),
 
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    RGB_MOD, RGB_MOD, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_DEL, \
       KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    RESET,   RESET,   KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
       KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_LBRC, KC_RBRC, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT , \
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
+      KC_LCTL,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, \
                                                    LOWER,   KC_SPC,  KC_SPC,  RAISE \
       ),
   /* Lower
