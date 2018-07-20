b162db72ff350791c28da6dd8101b551cb6cae3a
Sterling <503326@MC02VP210HV2H.tld>
2018-07-20 16:56:04 -0700

    Rules for vitamins_included
    Added a section to disable RGB underglow for the Let's Split
       Vitamins Included board.

--- layouts/community/ortho_4x12/xyverz/rules.mk
+++ layouts/community/ortho_4x12/xyverz/rules.mk
@@ -2,9 +2,9 @@ BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output on port C6
 
 ifeq ("$(KEYBOARD)","vitamins_included")
-  RGBLIGHT_ENABLE = no
-else
   RGBLIGHT_ENABLE = yes
+else
+  RGBLIGHT_ENABLE = no
 endif
 
 ifndef QUANTUM_DIR
