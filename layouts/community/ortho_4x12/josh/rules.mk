57717758cec15aa6f15e26956c433f30e8310e34
Josh McFadden <josh@visionistinc.com>
2018-07-21 00:50:31 -0400

    Adding vitamins_included makefile options to my ortho_4x12 layout.

--- layouts/community/ortho_4x12/josh/rules.mk
+++ layouts/community/ortho_4x12/josh/rules.mk
@@ -3,7 +3,16 @@ BACKLIGHT_ENABLE = no
 BACKLIGHT_BREATHING = no
 AUDIO_ENABLE = no
 USE_I2C = yes
-else
+endif
+
+ifneq (,$(findstring vitamins_included,$(KEYBOARD)))
+BACKLIGHT_ENABLE = no
+BACKLIGHT_BREATHING = no
+AUDIO_ENABLE = no
+USE_I2C = no
+endif
+
+ifneq (,$(findstring planck,$(KEYBOARD)))
 BACKLIGHT_ENABLE = yes
 BACKLIGHT_BREATHING = yes
 AUDIO_ENABLE = yes
