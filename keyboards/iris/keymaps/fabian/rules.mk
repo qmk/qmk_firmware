b4a93f677d7633e9e5009fd416f377ac0f13a62f
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2018-07-22 11:19:06 +0200

    deleted obsolete rules.mk (was part of the old makefile system)

--- keyboards/iris/keymaps/fabian/rules.mk
+++
@@ -1,9 +0,0 @@
-// AUTO_SHIFT_ENABLE = yes
-// AUTO_SHIFT_MODIFIERS = yes
-BACKLIGHT_ENABLE = yes
-MOUSEKEY_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
