27dc21d9fe114defc34db385e08ed082230856c3
Ian Sterling <xyverz@gmail.com>
2018-07-22 01:49:12 -0700

    fixing ortho_4x12 configs

--- layouts/community/ortho_4x12/xyverz/rules.mk
+++ layouts/community/ortho_4x12/xyverz/rules.mk
@@ -2,9 +2,9 @@ BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output on port C6
 
 ifeq ("$(KEYBOARD)","vitamins_included")
-  RGBLIGHT_ENABLE = yes
-else
   RGBLIGHT_ENABLE = no
+else
+  RGBLIGHT_ENABLE = yes
 endif
 
 ifndef QUANTUM_DIR
