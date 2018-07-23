546ba911aedef24bc777c1b04b9f904aadaf70d0
Peioris <mfmayol@up.edu.ph>
2018-07-24 06:54:11 +0800

    replaced 'qc60/rev1' with 'qc60/proto'

--- keyboards/handwired/qc60/rules.mk
+++ keyboards/handwired/qc60/rules.mk
@@ -65,4 +65,4 @@ SPLIT_KEYBOARD = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-DEFAULT_FOLDER = qc60/rev1
+DEFAULT_FOLDER = qc60/proto
