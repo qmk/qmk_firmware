2d5493d1912b5c17d129100f6f5026b399132d6a
Legonut <legonut3@gmail.com>
2018-07-25 12:54:26 -0400

    more OLED stuff

--- keyboards/sol/rev2/keymaps/default/rules.mk
+++ keyboards/sol/rev2/keymaps/default/rules.mk
@@ -29,7 +29,7 @@ endef
 # you can edit follows 7 Variables
 #  jp: 以下の7つの変数を必要に応じて編集します。
 HELIX_ROWS = 5              # Helix Rows is 4 or 5
-OLED_ENABLE = yes            # OLED_ENABLE
+OLED_ENABLE = no            # OLED_ENABLE
 LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" insted of "common/glcdfont.c"
 LED_BACK_ENABLE = yes        # LED backlight (Enable WS2812 RGB underlight.)
 LED_UNDERGLOW_ENABLE = no   # LED underglow (Enable WS2812 RGB underlight.)
@@ -120,3 +120,5 @@ endif
 # $(info -- RGBLIGHT_ENABLE=$(RGBLIGHT_ENABLE))
 # $(info -- OPT_DEFS=$(OPT_DEFS))
 # $(info )
+
+OLED_ENABLE = yes
