3cc3d84fcbb43a1c0eefe41b05978463f81491ac
Guido Bartolucci <guido@bartolucci.org>
2018-07-25 11:31:03 -0400

    removed old code

--- layouts/community/ortho_4x12/guidoism/generate_c.py
+++ layouts/community/ortho_4x12/guidoism/generate_c.py
@@ -21,45 +21,15 @@ def unicode(k, only_unicode=True):
 
 d = json.load(open('layouts/community/ortho_4x12/guidoism/guidoism.json'))
 
-def truthy(s):
-    return [a for a in s if a]
-
-def grouper(iterable, n):
-    args = [iter(iterable)] * n
-    return (truthy(r) for r in itertools.zip_longest(*args))
-
-def just(s, n=5):
-    return [a.center(n*2+1 if len(s) == 11 and i == 5 else n) for i, a in enumerate(s)]
-
 def surround(s, a, b, c):
     return a + b.join(s) + c
 
 def pattern(cell, table):
     return ['─'*cell for i in range(table)]
 
-def layer(i, l, rename):
-    return [just(list(map(rename, row))) for row in grouper(l, 12)]
-
 top    = surround(pattern(5, 12), '┌', '┬', '┐')
 mid    = surround(pattern(5, 12), '├', '┼', '┤')
 bottom = surround(pattern(5, 12), '└', '┴', '┘')
-
-if False:
-    for i, l in enumerate(d['layers']):
-        print('/*')
-        print(top)
-        for n, row in enumerate(layer(i, l, unicode)):
-            print(surround(row, '│', '│', '│'))
-            if n < 3:
-                print(surround(pattern(5, 12), '├', '┼', '┤'))
-            else:
-                print(bottom)
-        print('*/')
-        print('  [%s] = %s(' % (layer_names[i], d['layout']))
-        for n, row in enumerate(layer(i, l, lambda k: layer_name.get(k, k))):
-            print(surround(row, '    ', ', ', ','))
-        print('  ),\n')
-    quit()
 
 from more_itertools import chunked, intersperse, interleave_longest
 
@@ -94,12 +64,4 @@ def c_layout(i, definition):
 
 print(',\n\n'.join(c_layout(i, l) for i, l in enumerate(d['layers'])))
 
-# What I really want I think is a state machine that knows whether it's on the top,
-# beginning of line, end of line, middle or bottom.
-# 
-# - If it's on the top then it's 1-12               [k for k in keys if k <= 12]
-# - If it's on the left end then it's 1, 13, etc    [k for k in keys if k % 12 == 1]
-# - If it's on the right end then it's 12, 24, etc  [k for k in keys if k % 12 == 0]
-# - If it's on the bottom then it's the last eight  [k for k in keys if k > 48-12]
-# 
 
