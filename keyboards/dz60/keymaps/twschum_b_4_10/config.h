4cdec9a3f1b5fa6c4bf3ca6ceb310fd81b4ca123
Tim <tim.schumacher@nokia.com>
2018-07-26 15:03:12 -0400

    Add custom config file for keyboard; TT and mousekey settings

---
+++ keyboards/dz60/keymaps/twschum_b_4_10/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef TAPPING_TOGGLE
+#undef TAPPING_TOGGLE
+#endif
+#define TAPPING_TOGGLE 2
+
+// https://docs.qmk.fm/features/mouse-keys#configuring-the-behavior-of-mousekeys
+#define MOUSEKEY_DELAY        0
+#define MOUSEKEY_INTERVAL     20
+#define MOUSEKEY_MAX_SPEED    10
+#define MOUSEKEY_TIME_TO_MAX  15
+#define MOUSEKEY_WHEEL_MAX_SPEED   1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 255
