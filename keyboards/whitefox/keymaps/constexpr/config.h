0bfda74261029a53ecfa497bffbab2bff03466b4
Konstantin Đorđević <vomindoraan@gmail.com>
2018-07-30 00:25:06 +0200

    Add keyboards/whitefox/keymaps/constexpr

---
+++ keyboards/whitefox/keymaps/constexpr/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define FORCE_NKRO
+
+#define TAPPING_TOGGLE 2
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+
+#define MOUSEKEY_DELAY 50
+#define MOUSEKEY_INTERVAL 15
+#define MOUSEKEY_MAX_SPEED 4
+#define MOUSEKEY_TIME_TO_MAX 50
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 50
+
+#endif
---
+++ keyboards/whitefox/keymaps/constexpr/rules.mk
@@ -0,0 +1,8 @@
+BOOTMAGIC_ENABLE = no
+CONSOLE_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+UNICODE_ENABLE = yes
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
