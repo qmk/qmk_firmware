5215f5d74aed1887e5a5e4a2e142185ee80558f5
Manassarn Manoonchai <manassarn@gmail.com>
2018-07-31 19:33:02 +0700

    Enable midi & audio on planck/light

--- keyboards/planck/keymaps/narze/rules.mk
+++ keyboards/planck/keymaps/narze/rules.mk
@@ -11,8 +11,6 @@ CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
@@ -21,3 +19,11 @@ RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this 
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 COMBO_ENABLE = yes
+
+ifeq ($(strip $(KEYBOARD)), planck/light)
+  MIDI_ENABLE = yes
+  AUDIO_ENABLE = yes
+else
+  MIDI_ENABLE = no
+  AUDIO_ENABLE = no
+endif
