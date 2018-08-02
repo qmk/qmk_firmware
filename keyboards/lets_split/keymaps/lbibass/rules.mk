fc68f5767c49946da61018f298adbe1081f3d2b9
lbibass <notlbibass@gmail.com>
2018-08-02 18:06:57 -0400

    Here we go.

---
+++ keyboards/lets_split/keymaps/lbibass/rules.mk
@@ -0,0 +1,10 @@
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
+AUDIO_ENABLE = yes
+CONSOLE_ENABLE = no
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
