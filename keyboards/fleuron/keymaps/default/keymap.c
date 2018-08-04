6533cf8c2a2bbba4bf0e08f2796ca4df12ea97e6
James Underwood <james@jlunderwood.com>
2018-08-04 16:48:03 -0700

    updates to default layout

--- keyboards/fleuron/keymaps/default/keymap.c
+++ keyboards/fleuron/keymaps/default/keymap.c
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP(
   /* Qwerty
    * ,---------------------------------------------------------------------------------------------------------------.
-   * |  Esc |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  |  F12 | Home | End  | M1   |
+   * |  Esc |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  |  F12 | Home | PgUp | Bksp |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |  Del | PgUp | PgDn |  /   |
+   * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |  Del | End  | PgDn |  /   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |  7   |  8   |  9   |  *   |
    * |------+------+------+------+------+-------------+------+------+------+------+------+------+------+------+------|
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | RGB  | Ctrl | Alt  | GUI  |Lower |Shift |Space |Raise | Left | Down |  Up  |Right |  0   |  00  |  .   |Enter |
    * `---------------------------------------------------------------------------------------------------------------'
    */
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_HOME,  KC_END,  KC_SPC, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_DEL, KC_PGUP,  KC_PGDN, KC_PSLS, \
+  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_HOME,  KC_PGUP, KC_BSPC, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_DEL, KC_END,   KC_PGDN, KC_PSLS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_P7,  KC_P8,    KC_P9,   KC_PAST, \
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_P4,  KC_P5,    KC_P6,   KC_PMNS, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  KC_P1,  KC_P2,    KC_P3,   KC_PPLS, \
