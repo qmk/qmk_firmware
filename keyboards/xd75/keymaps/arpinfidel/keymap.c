1b38ee50807509c0246e5a3c1ed4b3a95896f218
ArpinFidel <richardputra2910@gmail.com>
2018-08-06 00:31:31 +0700

    added caps lock led indicator

--- keyboards/xd75/keymaps/arpinfidel/keymap.c
+++ keyboards/xd75/keymaps/arpinfidel/keymap.c
@@ -112,3 +112,11 @@ uint32_t layer_state_set_user(uint32_t state) {
   
   return state;
 }
+
+void led_set_user(uint8_t usb_led) {
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+        capslock_led_on();
+    } else {
+        capslock_led_off();
+    }
+}
