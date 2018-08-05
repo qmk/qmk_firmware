b536c5e2ce1c94ae6a9d5bf593e188b694857974
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2018-08-05 00:55:15 -0300

    vim fix

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -10,7 +10,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
-		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,   KC_5,    KC_6,     KC_7,     KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_GRV,   KC_BSLS,
+		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,   KC_5,    KC_6,     KC_7,     KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,
 		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,   KC_T,    KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
 		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,   KC_G,    KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
 		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,   KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_GRV,
@@ -19,14 +19,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN1] = LAYOUT(
 		KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,  KC_F11,   KC_F12,   X___X,   KC_EJCT,
 		X___X,   RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  X___X,     KC_UP,   KC_MRWD,  KC_MFFD,  KC_DEL,
-		X___X,   X___X,    X___X,    X___X,    X___X,    X___X,    KC_LEFT,  KC_UP,    KC_DOWN,  KC_RIGHT,  X___X,   X___X,    KC_PENT,
+		X___X,   X___X,    X___X,    X___X,    X___X,    X___X,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  X___X,   X___X,    KC_PENT,
 		X___X,   X___X,    X___X,    X___X,    X___X,    X___X,    KC_LEFT,  KC_DOWN,  KC_ENT,   X___X,     X___X,   X___X,    X___X,    X___X,
 		X___X,   X___X,    X___X,    X___X,    X___X,    X___X,    X___X,    X___X,    X___X,    X___X,     TO(0)),
 
   [_FN2] = LAYOUT(
 		KC_GRV,  X___X,    X___X,    MISCTRL,  X___X,     RGB_VAD,  RGB_VAI,  KC_MRWD,  KC_MPLY,  KC_MFFD,   KC_MUTE,  KC__VOLDOWN,  KC__VOLUP,  X___X,   KC_EJCT,
 		X___X,   KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_ACL0,   KC_ACL1,  KC_ACL2,  X___X,    X___X,    X___X,     KC_UP,    X___X,        X___X,      KC_DEL,
-		X___X,   KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_RIGHT,  X___X,    KC_LEFT,  KC_UP,    KC_DOWN,  KC_RIGHT,  X___X,    X___X,        KC_PENT,
+		X___X,   KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_RIGHT,  X___X,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  X___X,    X___X,        KC_PENT,
 		X___X,   X___X,    X___X,    X___X,    X___X,     X___X,    KC_LEFT,  KC_DOWN,  KC_ENT,   X___X,     X___X,    X___X,        X___X,      X___X,
 		X___X,   X___X,    X___X,    X___X,    X___X,     X___X,    X___X,    X___X,    X___X,    X___X,     RESET)
 };
