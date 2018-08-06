276d6da20aa22b591f675a82e1e7be5f4b3df88d
Yan-Fa Li <yanfali@gmail.com>
2018-08-05 22:38:59 -0700

    Use the layer define value

--- keyboards/catch22/keymaps/default/keymap.c
+++ keyboards/catch22/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT( /* Base */
            KC_A,    KC_B,  KC_C,  KC_D,  \
            KC_E,    KC_F,  KC_G,  KC_H,  \
-  MO(1),   KC_I,    KC_J,  KC_K,  KC_L,  \
+  MO(_FN), KC_I,    KC_J,  KC_K,  KC_L,  \
   KC_1,    KC_M,    KC_N,  KC_O,  KC_P,  \
            KC_Q,    KC_R,  KC_S,  KC_T   \
 ),
