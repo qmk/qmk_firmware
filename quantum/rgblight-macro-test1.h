b9e8dfd3825c952bf06888307d3b5eca7f1bc803
mtei <2170248+mtei@users.noreply.github.com>
2018-08-06 19:19:05 +0900

    add temporary test code rgblight-macro-test1.[ch]

---
+++ quantum/rgblight-macro-test1.h
@@ -0,0 +1,44 @@
+// temporary code for test
+
+#ifdef _RGBM_SINGLE
+  _RGBM_SINGLE( STATIC_LIGHT )
+  _RGBM_MULTI( BREATHING )
+  _RGBM_TMP( breathing_3 )
+  _RGBM_TMP( breathing_4 )
+  _RGBM_TMP( BREATHING_end )
+  _RGBM_MULTI( RAINBOW_MOOD )
+  _RGBM_TMP( rainbow_mood_7 )
+  _RGBM_TMP( RAINBOW_MOOD_end )
+  _RGBM_MULTI( RAINBOW_SWIRL )
+  _RGBM_TMP( rainbow_swirl_10 )
+  _RGBM_TMP( rainbow_swirl_11 )
+  _RGBM_TMP( rainbow_swirl_12 )
+  _RGBM_TMP( rainbow_swirl_13 )
+  _RGBM_TMP( RAINBOW_SWIRL_end )
+  _RGBM_MULTI( SNAKE )
+  _RGBM_TMP( snake_16 )
+  _RGBM_TMP( snake_17 )
+  _RGBM_TMP( snake_18 )
+  _RGBM_TMP( snake_19 )
+  _RGBM_TMP( SNAKE_end )
+  _RGBM_MULTI( KNIGHT )
+  _RGBM_TMP( knight_22 )
+  _RGBM_TMP( KNIGHT_end )
+  _RGBM_SINGLE( CHRISTMAS )
+  _RGBM_MULTI( STATIC_GRADIENT )
+  _RGBM_TMP( static_gradient_26 )
+  _RGBM_TMP( static_gradient_27 )
+  _RGBM_TMP( static_gradient_28 )
+  _RGBM_TMP( static_gradient_29 )
+  _RGBM_TMP( static_gradient_30 )
+  _RGBM_TMP( static_gradient_31 )
+  _RGBM_TMP( static_gradient_32 )
+  _RGBM_TMP( static_gradient_33 )
+  _RGBM_TMP( STATIC_GRADIENT_end )
+  _RGBM_SINGLE( RGB_TEST )
+  _RGBM_SINGLE( ALTERNATING )
+#endif
+
+#undef _RGBM_SINGLE
+#undef _RGBM_MULTI
+#undef _RGBM_TMP  
