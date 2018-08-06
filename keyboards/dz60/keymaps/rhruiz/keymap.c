e6072be67028e6669f760d102e902ad2fa5a039a
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2018-08-05 22:24:40 -0300

    s/fn2/tap to backspace, hold for fn2/

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -3,6 +3,7 @@
 #define X___X KC_TRNS
 #define MISCTRL LCTL(KC_UP)
 #define CTRLESC LCTL_T(KC_ESC)
+#define FN2BSPC LT(_FN2, KC_BSPC)
 
 #define _BL 0
 #define _FN1 1
@@ -11,11 +12,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
-		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,      KC_5,    KC_6,     KC_7,     KC_8,   KC_9,     KC_0,       KC_MINS,  KC_EQL,   KC_BSLS,   KC_GRV,
-		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,      KC_T,    KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,       KC_LBRC,  KC_RBRC,  KC_BSPC,
-		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,      KC_G,    KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,    KC_QUOT,  KC_ENT,
-		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,      KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,  TG(_FN3),
-		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  MO(_FN2),  KC_ENT,  KC_LGUI,  KC_LALT,  KC_NO,  KC_APP,   TO(_FN1)),
+		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,    KC_6,     KC_7,     KC_8,   KC_9,     KC_0,       KC_MINS,  KC_EQL,   KC_BSLS,   KC_GRV,
+		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,       KC_LBRC,  KC_RBRC,  KC_BSPC,
+		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,    KC_QUOT,  KC_ENT,
+		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,  TG(_FN3),
+		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  FN2BSPC,  KC_ENT,  KC_LGUI,  KC_LALT,  KC_NO,  KC_APP,   TO(_FN1)),
 
   [_FN1] = LAYOUT(
 		KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_F11,   KC_F12,   X___X,   KC_EJCT,
