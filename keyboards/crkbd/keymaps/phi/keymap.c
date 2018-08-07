caec0d0e52f722b5f636387d8770575817a1b0c1
p <_>
2018-08-07 14:32:44 +0900

    Refactoring: Remove unused code

--- keyboards/crkbd/keymaps/phi/keymap.c
+++ keyboards/crkbd/keymaps/phi/keymap.c
@@ -119,8 +119,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     oled_record_event(keycode, record);
     return true;
 }
-
-/* void persistent_default_layer_set(uint16_t default_layer) { */
-/*   eeconfig_update_default_layer(default_layer); */
-/*   default_layer_set(default_layer); */
-/* } */
