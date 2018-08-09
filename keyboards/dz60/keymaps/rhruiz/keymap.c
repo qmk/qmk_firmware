2f0c4fe55c46c5c83ed088c95f908a5327813b53
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2018-08-09 12:02:31 -0300

    GUI_T(KC_ENT) on right spacebar

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #define MISCTRL LCTL(KC_UP)
+#define CMDENT LGUI_T(KC_ENT)
 #define CTRLESC LCTL_T(KC_ESC)
 #define FN2BSPC LT(_FN2, KC_BSPC)
 
@@ -22,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,       KC_LBRC,  KC_RBRC,  KC_BSPC,
 		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,    KC_QUOT,  KC_ENT,
 		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,  TG(_FN3),
-		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  FN2BSPC,  KC_ENT,  KC_LGUI,  KC_LALT,  KC_NO,  KC_APP,   TO(_FN1)),
+		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  FN2BSPC,  CMDENT,  KC_LGUI,  KC_LALT,  KC_NO,  KC_APP,   TO(_FN1)),
 
   [_FN1] = LAYOUT(
 		KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_F11,   KC_F12,   _______,  KC_EJCT,
