9d4193fe44dcaed66d9f3d6a494a058999dc6cc3
David Friedrich <david.friedrich@digimapas.cl>
2018-08-09 12:48:49 -0400

    manuform 5x6 added

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -31,8 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 // Rows are doubled-up
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 5
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 6
 
 // wiring of each half
 #define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
