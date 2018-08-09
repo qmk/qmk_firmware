bdbcd7874fa966b52ef5ebed379329ee20096c43
Erik Thorsell <erik.thorsell@consultant.volvo.com>
2018-08-09 14:39:40 +0200

    Update Svorak A5 keymap

--- layouts/community/ergodox/dvorak_svorak_a5/keymap.c
+++ layouts/community/ergodox/dvorak_svorak_a5/keymap.c
@@ -1,9 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "debug.h"
 #include "action_layer.h"
-#include "version.h"
-#include "keymap_nordic.h"
-#include "keymap_norwegian.h"
+#include "keymap_swedish.h"
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
@@ -13,18 +11,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |   1  |   2  |   3  |   4  |   5  | ~MOUS|           |      |   6  |   7  |   8  |   9  |   0  |   \    |
+ * |        |   1  |   2  |   3  |   4  |   5  | ~MOUS|           |      |   6  |   7  |   8  |   9  |   0  |    +   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   Ã…  |   Ã„  |   Ã–  |   P  |   Y  |      |           |      |   F  |   G  |   C  |   R  |   L  |   ,    |
+ * |        |   Å  |   Ä  |   Ö  |   P  |   Y  |      |           |      |   F  |   G  |   C  |   R  |   L  |   ,    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Escape |   A  |   O  |   E  |   U  |   I  |------|           |------|   D  |   H  |   T  |   N  |   S  |  -/_   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   .  |   Q  |   J  |   K  |   X  |      |           |      |   B  |   M  |   W  |   V  |   Z  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *  | LCTRL |      |      |      | Super|                                       | ~SYMB| Left | Down |  Up  | Right |
+ *  | LCTRL |      |      | LAlt | Super|                                       | ~SYMB| Left | Down |  Up  | Right |
  *  `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
+ *                                        |  Ins |  Del |       |      |      |
  *                                 ,------|------|------|       |------+--------+-------.
  *                                 |      |      |      |       |      |        |       |
  *                                 | BSP  | Tab  |------|       |------|  Enter | Space |
@@ -33,20 +31,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_NO,    KC_1,   KC_2,     KC_3,    KC_4,    KC_5,   MO(MOUS),
-        KC_NO,    NO_AA,  NO_AE,    NO_OSLH, KC_P,    KC_Y,   KC_NO,
+        KC_NO,    KC_1,   KC_2,     KC_3,    KC_4,    KC_5,      MO(MOUS),
+        KC_TAB,   NO_AA,  NO_AE,    NO_OSLH, KC_P,    KC_Y,      KC_NO,
         KC_ESC,   KC_A,   KC_O,     KC_E,    KC_U,    KC_I,
-        KC_LSFT,  KC_DOT, KC_Q,     KC_J,    KC_K,    KC_X,   KC_NO,
-        KC_LCTRL, KC_NO,  KC_NO,    KC_NO,   KC_LGUI,
-                                                      KC_NO,  KC_NO,
-                                                              KC_NO,
-                                             KC_BSPC, KC_TAB, KC_NO,
+        KC_LSFT,  KC_DOT, KC_Q,     KC_J,    KC_K,    KC_X,      KC_NO,
+        KC_LCTRL, KC_NO,  KC_NO,    KC_LALT, KC_LCMD,
+                                                      KC_INSERT, KC_DEL,
+                                                                 KC_NO,
+                                             KC_BSPC, KC_TAB,    KC_NO,
         // right hand
-        KC_NO,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0,   KC_BSLASH,
-        KC_NO,    KC_F,   KC_G,     KC_C,    KC_R,    KC_L,   KC_COMM,
-                  KC_D,   KC_H,     KC_T,    KC_N,    KC_S,   KC_MINUS,
-        KC_NO,    KC_B,   KC_M,     KC_W,    KC_V,    KC_Z,   KC_RSFT,
-                          MO(SYMB), KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT,
+        KC_NO,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0,      NO_PLUS,
+        KC_NO,    KC_F,   KC_G,     KC_C,    KC_R,    KC_L,      KC_COMM,
+                  KC_D,   KC_H,     KC_T,    KC_N,    KC_S,      NO_MINS,
+        KC_NO,    KC_B,   KC_M,     KC_W,    KC_V,    KC_Z,      KC_RSFT,
+                          MO(SYMB), KC_LEFT, KC_DOWN, KC_UP,     KC_RGHT,
         KC_NO,    KC_NO,
         KC_NO,
         KC_NO,    KC_ENT, KC_SPACE
@@ -60,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   ;  |   /  |   (  |   )  |   |  |------|           |------|   #  |   ^  |   #  |   "  |   ~  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   :  |   =  |   @  |   !  |   \  |      |           |      |   %  |   `  |   '  |   *  |      |        |
+ * |        |   :  |   =  |   @  |   !  |   \  |      |           |      |   %  |   ´  |   '  |   *  |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |       |
  *   `----------------------------------'                                       `----------------------------------'
@@ -68,30 +66,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |  BSP |      |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       KC_NO, KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,
-       KC_NO, KC_LCBR, KC_RCBR,     KC_LBRC, KC_RBRC, KC_DLR,  KC_NO,
-       KC_NO, KC_SCLN, KC_KP_SLASH, KC_LPRN, KC_RPRN, KC_PIPE,
-       KC_NO, KC_COLN, KC_PEQL,     KC_AT,   KC_EXLM, KC_BSLS, KC_NO,
-       KC_NO, KC_NO,   KC_NO,       KC_NO,   KC_NO,
+       KC_NO, KC_NO,         KC_NO,       KC_NO,      KC_NO,      KC_NO,            KC_NO,
+       KC_NO, ALGR(KC_7),    ALGR(KC_0),  ALGR(KC_8), ALGR(KC_9), ALGR(KC_4),       KC_NO,
+       KC_NO, LSFT(KC_COMM), KC_KP_SLASH, LSFT(KC_8), LSFT(KC_9), ALGR(KC_NUBS),
+       KC_NO, LSFT(KC_DOT),  LSFT(KC_0),  ALGR(KC_2), KC_EXLM,    ALGR(KC_MINS),    KC_NO,
+       KC_NO, KC_NO,         KC_NO,       KC_NO,      KC_NO,
                                                       KC_NO,   KC_NO,
                                                                KC_NO,
-                                             KC_NO,   KC_NO,   KC_NO,
+                                             KC_TRNS, KC_NO,   KC_NO,
        // right hand
-       KC_NO, KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,
-       KC_NO, KC_DQT,  KC_AMPR,     KC_LT,   KC_GT,   KC_NO,   KC_NO,
-              KC_HASH, KC_CIRC,     KC_HASH, KC_DQT,  KC_TILD, KC_NO,
-       KC_NO, KC_PERC, KC_GRV,      KC_QUOT, KC_ASTR, KC_NO,   KC_NO,
-                       KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,
+       KC_NO, KC_NO,         KC_NO,       KC_NO,       KC_NO,       KC_NO,   KC_NO,
+       KC_NO, LSFT(KC_2),    LSFT(KC_6),  NO_LESS,     NO_GRTR,     KC_NO,   KC_NO,
+              KC_HASH,       NO_CIRC,     KC_HASH,     LSFT(KC_2),  NO_TILD, KC_NO,
+       KC_NO, KC_PERC,       NO_ACUT,     NO_APOS,     NO_ASTR,     KC_NO,   KC_NO,
+                             KC_NO,       KC_NO,       KC_NO,       KC_NO,   KC_NO,
        KC_NO, KC_NO,
        KC_NO,
-       KC_NO, KC_NO,   KC_NO
+       KC_NO, KC_NO,         KC_NO
 ),
 /* Keymap 2: Media and mouse keys
  *
