f6aab9925968dfb9ce917964e872ebb9728cf9ba
wanleg <wanleg@hotmail.ca>
2018-08-10 03:06:41 -0700

    additional layout support

--- layouts/community/ortho_4x12/wanleg/config.h
+++ layouts/community/ortho_4x12/wanleg/config.h
@@ -1,3 +1,11 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
+
+//split keyboard settings
+#if defined(KEYBOARD_lets_split_rev2)
+#define USE_SERIAL
+#undef USE_I2C
+#define EE_HANDS
+#endif
+
 #endif
--- users/wanleg/wanleg.c
+++ users/wanleg/wanleg.c
@@ -28,6 +28,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case ONEHAND:
+    if (record->event.pressed) {
+      set_single_persistent_default_layer(ONE);
+      }
+      return false;
+      break;
     case SUBTER:
       if (record->event.pressed) {
         layer_on(SUB);
