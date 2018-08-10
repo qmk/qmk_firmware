9e20d8175d4ef0bb9752bdef7f649001c38aef31
p <_>
2018-08-10 20:27:51 +0900

    Make MOUSEKEY a bit slower

--- keyboards/crkbd/keymaps/phi/config.h
+++ keyboards/crkbd/keymaps/phi/config.h
@@ -36,13 +36,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PREVENT_STUCK_MODIFIERS /* Auto-restore layer when the layer key is released */
 
 #define MOUSEKEY_DELAY             32
-#define MOUSEKEY_INTERVAL          16
+#define MOUSEKEY_INTERVAL          24
 #define MOUSEKEY_MOVE_DELTA        3
 #define MOUSEKEY_MAX_SPEED         11 /* times faster than MOVE_DELTA */
 #define MOUSEKEY_TIME_TO_MAX       10
 
 #define MOUSEKEY_WHEEL_DELAY       48
-#define MOUSEKEY_WHEEL_INTERVAL    48
+#define MOUSEKEY_WHEEL_INTERVAL    72
 #define MOUSEKEY_WHEEL_DELTA       1
 #define MOUSEKEY_WHEEL_MAX_SPEED   1 /* times faster */
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 1
