ba91725c2295b2f345fcda2514b346dcb886806f
Andy Holder <andy.m.holder@gmail.com>
2018-08-10 23:38:36 +0100

    Change Keymap to layout

--- keyboards/dz60/dz60.h
+++ keyboards/dz60/dz60.h
@@ -162,7 +162,7 @@
 	{ K400,  K401, KC_NO,  K403, KC_NO, KC_NO,  K406, KC_NO, KC_NO, KC_NO,  K410,  K411,  K412,  K413,  K414 }  \
 }
 
-#define KEYMAP_60_ansi_split_bs_rshift_5x1u( \
+#define LAYOUT_60_ansi_split_bs_rshift_5x1u( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, K014, \
 	K100,       K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, K114, \
 	K200,       K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213,       \
