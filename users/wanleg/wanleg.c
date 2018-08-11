b4ea2bb58e42caeb2bcaa00462acb31e9ae1170d
wanleg <32079073+wanleg@users.noreply.github.com>
2018-08-10 20:29:30 -0700

    add additional keymaps

--- users/wanleg/wanleg.c
+++ users/wanleg/wanleg.c
@@ -34,6 +34,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case QWERTY75:
+    if (record->event.pressed) {
+      set_single_persistent_default_layer(QW75);
+      }
+      return false;
+      break;
+    case GHERKIN75:
+    if (record->event.pressed) {
+      set_single_persistent_default_layer(GK75);
+      }
+      return false;
+      break;
     case SUBTER:
       if (record->event.pressed) {
         layer_on(SUB);
@@ -98,7 +110,55 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-
+//////////////////////////////////////////////////////////////////
+	case SUBTER75:
+      if (record->event.pressed) {
+        layer_on(SUB75);
+      } else {
+        layer_off(SUB75);
+      }
+      return false;
+      break;
+	case SUPRA75:
+      if (record->event.pressed) {
+        layer_on(SUP75);
+      } else {
+        layer_off(SUP75);
+      }
+      return false;
+      break;
+	case NUMBER75:
+      if (record->event.pressed) {
+        layer_on(NUM75);
+      } else {
+        layer_off(NUM75);
+      }
+      return false;
+      break;
+	case DIRECTION75:
+      if (record->event.pressed) {
+        layer_on(DIR75);
+      } else {
+        layer_off(DIR75);
+      }
+      return false;
+      break;
+	case ETCETERA75:
+      if (record->event.pressed) {
+        layer_on(ETC75);
+      } else {
+        layer_off(ETC75);
+      }
+      return false;
+      break;
+	case FUNCTION75:
+      if (record->event.pressed) {
+        layer_on(FN75);
+      } else {
+        layer_off(FN75);
+      }
+      return false;
+      break;
   }
   return true;
 }
