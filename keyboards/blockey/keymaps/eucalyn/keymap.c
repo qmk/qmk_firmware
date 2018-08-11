b30c516c755ae15fc04f4805b19569de8759e052
eucalyn <hi.eucalyn@gmail.com>
2018-08-12 04:20:48 +0900

    change sources by reviews

--- keyboards/blockey/keymaps/eucalyn/keymap.c
+++ keyboards/blockey/keymaps/eucalyn/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+/* Copyright 2018 Eucalyn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,8 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-
-#include "../../blockey.h"
 
 #ifdef RGBLIGHT_ENABLE
 //Following line allows macro to read current RGB settings
