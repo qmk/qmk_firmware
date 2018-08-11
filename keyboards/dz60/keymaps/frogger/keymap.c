836ea9be7c52a5d9512ded725ceb22a00de8db89
Dave Brown <davebrown.dev@gmail.com>
2018-08-10 22:10:11 -0400

    adding my alt'd arrows

--- keyboards/dz60/keymaps/frogger/keymap.c
+++ keyboards/dz60/keymaps/frogger/keymap.c
@@ -7,6 +7,7 @@
 #define LAYER_1 1
 #define LAYER_2 2
 #define LAYER_3 3
+#define LAYER_4 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -23,7 +24,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Ctrl |  Cmd  |  Alt  |   LAY1    | LAY2 |      SPACE     | RAlt | LAY1 |  L  |  D  |  R |
    * `-----------------------------------------------------------------------------------------'
    */
-
   LAYOUT_2_shifts(
       KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
 
@@ -33,8 +33,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(LAYER_1), KC_UP, MO(LAYER_2),
 
-      KC_LCTL, KC_LGUI, KC_LALT, MO(LAYER_1),  KC_RALT, KC_SPC, KC_RALT, MO(LAYER_1), KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LCTL, KC_LGUI, KC_LALT, MO(LAYER_1), LM(LAYER_1, 0x04), KC_SPC, KC_RALT, MO(LAYER_1), KC_LEFT, KC_DOWN, KC_RIGHT
       ),
+
+
 
   /* Layer 1
    * ,-----------------------------------------------------------------------------------------.
@@ -51,7 +53,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_VOLD, KC_VOLU, KC_MUTE,
    KC_MPRV, KC_MPLY, KC_MNXT,
    */
-
   LAYOUT_2_shifts(
       KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, ______, KC_DEL,
 
@@ -63,6 +64,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_VOLD, KC_MUTE
       ),
+
+
 
   /* Layer 2
    * ,-----------------------------------------------------------------------------------------.
@@ -77,7 +80,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |       |       |                                   |     |      |     |     |     |
    * `-----------------------------------------------------------------------------------------'
    */
-
   LAYOUT_2_shifts(
       RESET, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, ______, RESET,
 
