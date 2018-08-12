346e7038ccf50b59c5a83cc9c7ad05306dd6e776
zwnk <david@impstyle.com>
2018-08-12 19:59:37 -0300

    removed the i2c, serial, split_util and matrix files and inserted SPLIT_KEYBOARD

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/impstyle/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/impstyle/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT, 
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH, 
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,                    
-                                      KC_SPC, RAISE  ,                        LOWER, KC_ENT,                                          
+                                      RAISE,KC_SPC,                        KC_ENT, LOWER,                                          
                                       KC_TAB,KC_HOME,                         KC_END,  KC_DEL,                                            
                                       KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT 
   ),   
