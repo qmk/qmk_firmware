9d0a24b3ec76d9441c22ba01eca947eece6d2c8e
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2018-08-11 23:38:16 -0300

    fn3 is no more

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -8,7 +8,6 @@
 #define _BL 0
 #define _FN1 1
 #define _FN2 2
-#define _FN3 3
 
 void change_leds_to(uint16_t, rgblight_config_t);
 bool state_changed = false;
@@ -22,8 +21,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,    KC_6,     KC_7,     KC_8,   KC_9,     KC_0,       KC_MINS,  KC_EQL,   KC_BSLS,   KC_GRV,
 		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,     KC_I,   KC_O,     KC_P,       KC_LBRC,  KC_RBRC,  KC_BSPC,
 		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,     KC_J,     KC_K,   KC_L,     KC_SCLN,    KC_QUOT,  KC_ENT,
-		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  KC_RSFT,  TG(_FN3),
-		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  FN2BSPC,  CMDENT,  KC_LGUI,  KC_LALT,  KC_NO,  KC_APP,   TO(_FN1)),
+		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,     KC_SLSH,  MT(MOD_RSFT, KC_UP),  TG(_FN2),
+		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  FN2BSPC,  CMDENT,  KC_LGUI,  MT(MOD_LALT, KC_LEFT),  KC_NO,  MT(MOD_RCTL, KC_DOWN),   LT(_FN1, KC_RIGHT)),
 
   [_FN1] = LAYOUT(
 		KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_F11,   KC_F12,   _______,  KC_EJCT,
@@ -37,14 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_ACL0,   KC_ACL1,  KC_ACL2,  _______,  _______,  _______,   KC_UP,    _______,      _______,    KC_DEL,
 		_______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_RIGHT,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  _______,  _______,      KC_PENT,
 		_______,  _______,  _______,  _______,  _______,   _______,  KC_LEFT,  KC_DOWN,  KC_ENT,   _______,   _______,  _______,      _______,    _______,
-		_______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,   RESET),
-
-  [_FN3] = LAYOUT(
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  KC_K8s,    _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_UP,    TG(_FN3),
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  _______,  KC_DOWN,  KC_RIGHT)
+		_______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,   RESET)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
@@ -65,10 +57,6 @@ void matrix_scan_user(void) {
 
     case _FN2:
       hue = 0;
-      break;
-
-    case _FN3:
-      hue = 120;
       break;
   }
 
@@ -129,7 +117,6 @@ uint32_t layer_state_set_user(uint32_t state) {
 
       case _FN1:
       case _FN2:
-      case _FN3:
         state_changed = true;
         break;
 
