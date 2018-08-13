1940599e0e74ea23ae951aff0bbecc51fe6d71b1
Danny <nooges@users.noreply.github.com>
2018-08-13 15:38:49 -0400

    Keyboard: Clean up Iris files (#3640)

--- keyboards/iris/iris.h
+++ keyboards/iris/iris.h
@@ -1,5 +1,4 @@
-#ifndef IRIS_H
-#define IRIS_H
+#pragma once
 
 #ifdef KEYBOARD_iris_rev1
     #include "rev1.h"
@@ -26,5 +25,3 @@
         KC_##L30, KC_##L31, KC_##L32, KC_##L33, KC_##L34, KC_##L35, KC_##LT4, KC_##RT4, KC_##R30, KC_##R31, KC_##R32, KC_##R33, KC_##R34, KC_##R35, \
                                                 KC_##LT1, KC_##LT2, KC_##LT3, KC_##RT3, KC_##RT2, KC_##RT1 \
     )
-
-#endif
