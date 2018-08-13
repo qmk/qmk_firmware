77ba6cef101ca841770c5310202696fd82f9e11d
Drashna Jaelre <drashna@live.com>
2018-08-13 16:02:44 -0700

    Remove NO_PRINT and NO_DEBUG from userspace config.h
    Since these are automatically disabled if the console isn't enabled.

--- users/drashna/config.h
+++ users/drashna/config.h
@@ -61,12 +61,6 @@
 
 // Disable action_get_macro and fn_actions, since we don't use these
 // and it saves on space in the firmware.
-#if !defined(NO_DEBUG) && !defined(CONSOLE_ENABLE)
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
