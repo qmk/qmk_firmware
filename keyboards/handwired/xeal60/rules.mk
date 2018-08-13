ef21e2063c8f07593827e10796bf0da8733f76a9
Alex Ong <alex.ong@unsw.edu.au>
2018-08-13 15:25:57 +1000

    Updated rules.mk

--- keyboards/handwired/xeal60/rules.mk
+++ keyboards/handwired/xeal60/rules.mk
@@ -64,13 +64,12 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SUBPROJECT_rev1 = yes
-USE_I2C = no
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 CUSTOM_MATRIX = yes
 
-LAYOUTS = ortho_4x12
+LAYOUTS = split60
 
-DEFAULT_FOLDER = lets_split/rev2
+DEFAULT_FOLDER = handwired/xeal60/rev1
