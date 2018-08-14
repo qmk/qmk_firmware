61b61161478ebe1a65fc4b9a9efc0f887a342767
zwnk <david@impstyle.com>
2018-08-14 00:23:11 -0300

    Keyboard: Dactyl manuform 5x6 added and working (#3627)
    * mouse layer keys shifted
    
    * mouse layer keys shifted
    
    * manuform 5x6 added
    
    * mouse layer keys shifted
    
    * manuform 5x6 added
    
    * dactyl_manuform 5x6 keymap added
    
    * reorg. dactyl manuform folder
    
    * removed LAYOUTS = ortho_4x12 for 4x5
    
    * Rows and Cols in config.h fixed
    
    * MASTER_LEFT
    
    * 5x6 matrix fixed
    
    * keymap updated
    
    * removed the i2c, serial, split_util and matrix files and inserted SPLIT_KEYBOARD

--- keyboards/handwired/dactyl_manuform/5x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/rules.mk
@@ -1,9 +1,3 @@
-SRC += matrix.c \
-	   i2c.c \
-	   split_util.c \
-	   serial.c \
-	   ssd1306.c
-
 # MCU name
 #MCU = at90usb1287
 MCU = atmega32u4
@@ -70,6 +64,4 @@ USE_I2C = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-CUSTOM_MATRIX = yes
-
-LAYOUTS = ortho_4x12
+SPLIT_KEYBOARD = yes
