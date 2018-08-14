feec8ad4694814ed8953c6f72798f51f39724af5
zwnk <david@impstyle.com>
2018-08-14 14:54:22 -0300

    Keyboard: Add default keymap for dactyl_manuform 5x6 (#3649)
    * mouse layer keys shifted
    
    * mouse layer keys shifted
    
    * manuform 5x6 added
    
    * mouse layer keys shifted
    
    * manuform 5x6 added
    
    * dactyl_manuform 5x6 keymap added
    
    * reorg. dactyl manuform folder
    
    * removed LAYOUTS = ortho_4x12 for 4x5
    
    * Rows and Cols in config.h fixed
    
    * MASTER_LEFT
    
    * 5x6 matrix fixed
    
    * keymap updated
    
    * removed the i2c, serial, split_util and matrix files and inserted SPLIT_KEYBOARD
    
    * default keymap for dactyl_manuform 5x6
    
    * Fixes for Dactyl Manuform
    
    * Add/update default keymap

--- keyboards/handwired/dactyl_manuform/4x5/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/config.h
@@ -16,8 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #include "config_common.h"
 
@@ -26,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x3060
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    tshort
-#define PRODUCT         Dactyl-Manuform
 #define DESCRIPTION     A split keyboard for the cheap makers
 
 /* key matrix size */
@@ -45,53 +43,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* number of backlight levels */
 // #define BACKLIGHT_LEVELS 3
 
-/* mouse config */
-#define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    60
-#define MOUSEKEY_MAX_SPEED      7
-#define MOUSEKEY_WHEEL_DELAY 0
 
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
-/* Enables This makes it easier for fast typists to use dual-function keys */
-#define PERMISSIVE_HOLD
-
-/* ws2812 RGB LED */
-#define RGB_DI_PIN D3
-#define RGBLIGHT_TIMER
-#define RGBLED_NUM 12    // Number of LEDs
-#define ws2812_PORTREG  PORTD
-#define ws2812_DDRREG   DDRD
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-// #define NO_DEBUG
-
-/* disable print */
-// #define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
-
-
-#endif
