a42b65095c9219f893faa791c65d492e63e3583a
Alex Ong <alex.ong@unsw.edu.au>
2018-08-17 16:52:57 +1000

    Disabled a whole bunch of features. TODO: Test that keyboard still works fine.

--- keyboards/handwired/xeal60/rev1/config.h
+++ keyboards/handwired/xeal60/rev1/config.h
@@ -82,9 +82,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* disable action features */
 //#define NO_ACTION_LAYER
-//define NO_ACTION_TAPPING
+#define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
-
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define IGNORE_MOD_TAP_INTERRUPT
 #endif
