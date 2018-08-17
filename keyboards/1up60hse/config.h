a880d994c11452a1ee170b15b2da91d6d600f075
Mark John Merin <v-mamer@microsoft.com>
2018-08-17 06:55:47 -0700

    add the rest of my name

--- keyboards/1up60hse/config.h
+++ keyboards/1up60hse/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Mech
+Copyright 2018 MechMerlin
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
