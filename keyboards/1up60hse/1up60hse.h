622c4d2a97b7a6b2b02f6cb9bc2fc64b20479177
mechmerlin <mechmerlin@gmail.com>
2018-08-16 22:42:10 -0700

    can't use numbers for defines

--- keyboards/1up60hse/1up60hse.h
+++ keyboards/1up60hse/1up60hse.h
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef 1UP60HSE_H
-#define 1UP60HSE_H
+#ifndef KB_H
+#define KB_H
 
 #include "quantum.h"
 
