37d596a2bcb2869ed1b1f63fe60bb5fb5dd417b7
Kyle Brown <kdb424@gmail.com>
2018-08-17 05:06:08 -0700

    Enable RGB and leader key

--- layouts/community/ortho_4x12/kdb424/rules.mk
+++ layouts/community/ortho_4x12/kdb424/rules.mk
@@ -1,10 +1,11 @@
-RGBLIGHT_ENABLE   = no
 AUDIO_ENABLE      = no
 BOOTLOADER        = qmk-dfu # caterina is bad and it should feel bad. I don't use anything but DFU
 BACKLIGHT_ENABLE  = no      # Enable keyboard backlight functionality
 NKRO_ENABLE       = no      # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 TAP_DANCE_ENABLE  = yes     # Enable tap dance.
 AUTO_SHIFT_ENABLE = no      # Auto hold shift on longer keypress
+RGBLIGHT_ENABLE   = yes     # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+USE_IC2           = no
 
-EXTRAFLAGS        += -flto
+#EXTRAFLAGS        += -flto
 
