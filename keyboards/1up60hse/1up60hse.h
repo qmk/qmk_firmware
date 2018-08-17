f71f90732ec686328dba4ee8c2a250d1a7349700
mechmerlin <mechmerlin@gmail.com>
2018-08-16 23:08:24 -0700

    put in a default keymap, use the correct pins, and put in a switch matrix

--- keyboards/1up60hse/1up60hse.h
+++ keyboards/1up60hse/1up60hse.h
@@ -19,18 +19,21 @@
 #include "quantum.h"
 
 // This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
 // The first section contains all of the arguments representing the physical
 // layout of the board and position of the keys
 // The second converts the arguments into a two-dimensional array which 
 // represents the switch matrix. 
-#define LAYOUT( \
-    K00, K01, K02, \
-      K10,  K11   \
-) \
-{ \
-    { K00, K01,   K02 }, \
-    { K10, KC_NO, K11 }, \
+#define LAYOUT_60_ansi( \
+	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, \
+	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, \
+	K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211,       K213, \
+	K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310,             K313, \
+	K400, K401, K402,             K405,                   K409, K410, K411,       K413  \
+) { \
+	{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011,  K012,  K013 }, \
+	{ K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111,  K112,  K113 }, \
+	{ K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211,  KC_NO, K213 }, \
+	{ K300,  K301,  K302,  K303,  K304,  K305,  K306,  K307,  K308,  K309,  K310,  KC_NO, KC_NO, K313 }, \
+	{ K400,  K401,  K402,  KC_NO, KC_NO, K405,  KC_NO, KC_NO, KC_NO, K409,  K410,  K411,  KC_NO, K413 }  \
 }
-
 #endif
--- keyboards/1up60hse/config.h
+++ keyboards/1up60hse/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 MechMerlin
+Copyright 2018 Mech
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -28,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
 
 /*
  * Keyboard Matrix Assignments
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { B3, B2, B1, B0, D4 }
+#define MATRIX_COL_PINS { C7, F7, F6, F5, F4, F1, E6, D1, D0, D2, D3, D5, D6, D7 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
@@ -50,12 +50,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BACKLIGHT_PIN B7
 #define BACKLIGHT_BREATHING
-#define BACKLIGHT_LEVELS 3
+#define BACKLIGHT_LEVELS 5
 
-#define RGB_DI_PIN E2
+#define RGB_DI_PIN F0
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 16
+#define RGBLED_NUM 14
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
