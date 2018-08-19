a56c51580453ebaa94594816df1ff30339969ada
Christoffer Holmberg <christoffer.holmberg@ksfmedia.fi>
2018-08-19 10:10:59 +0300

    Update README.md

--- keyboards/ergodox_ez/keymaps/skug/keymap.c
+++ keyboards/ergodox_ez/keymaps/skug/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2017 Andreas Lindhé
+/* Copyright 2018 Andreas Lindhé, Christoffer Holmberg
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
