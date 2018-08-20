5e9a837554037f77d2d70f62de5d28d922f556f0
Pavlos Vinieratos <pvinis@gmail.com>
2018-08-20 08:25:01 +0200

    add some modifiers on the home row

--- users/pvinis/config.h
+++ users/pvinis/config.h
@@ -3,3 +3,7 @@
 #ifdef AUDIO_ENABLE
 // #define STARTUP_SONG SONG(SONIC_RING)
 #endif
+
+// allow rolling when keys have hold functionality
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 100
