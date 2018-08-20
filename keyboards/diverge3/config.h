9320edd2fd97d438fa4943e3131926faef62c913
Levi Olson <olson.levi@gmail.com>
2018-08-20 08:36:28 -0500

    Diverge3: Updates per comments in PR

--- keyboards/diverge3/config.h
+++ keyboards/diverge3/config.h
@@ -152,25 +152,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Feature disable options
  *  These options are also useful to firmware size reduction.
  */
+
 /* disable debug print */
 //#define NO_DEBUG
+
 /* disable print */
 //#define NO_PRINT
+
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
+
 /*
  * MIDI options
  */
+
 /* Prevent use of disabled MIDI features in the keymap */
 //#define MIDI_ENABLE_STRICT 1
+
 /* enable basic MIDI features:
    - MIDI notes can be sent when in Music mode is on
 */
 //#define MIDI_BASIC
+
 /* enable advanced MIDI features:
    - MIDI notes can be added to the keymap
    - Octave shift and transpose
@@ -178,6 +185,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
    - etc.
 */
 //#define MIDI_ADVANCED
+
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 1
 
