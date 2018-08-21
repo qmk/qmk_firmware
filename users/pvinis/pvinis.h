9892d599d7f245f2ad7e590e0f650bb5c104839d
Pavlos Vinieratos <pvinis@gmail.com>
2018-08-21 11:59:47 +0200

    apparently im not using z all that much..

--- users/pvinis/pvinis.h
+++ users/pvinis/pvinis.h
@@ -70,7 +70,7 @@ enum {
 // ,-----+-----+-----+-----+-----,   ,-----+-----+-----+-----+-----,
 #define _________________QWERTY_L1_________________  KC_Q   , KC_W   , KC_E   , KC_R   , KC_T
 #define _________________QWERTY_L2_________________  KC_A   , KC_S   , KC_D   , KC_F   , KC_G
-#define _________________QWERTY_L3_________________  KC_1   , KC_X   , KC_C   , KC_V   , KC_B
+#define _________________QWERTY_L3_________________  KC_Z   , KC_X   , KC_C   , KC_V   , KC_B
 
 #define _________________QWERTY_R1_________________  KC_Y   , KC_U   , KC_I   , KC_O   , KC_P
 #define _________________QWERTY_R2_________________  KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN
