e0b323c197157d7db1d13b3e45932d0d9ffc5085
noroadsleft <xxiinophobia@yahoo.com>
2018-08-22 01:21:11 -0700

    Swapped `___` for `XXX` in amj96.h per @mechmerlin

--- keyboards/amj96/amj96.h
+++ keyboards/amj96/amj96.h
@@ -19,7 +19,7 @@
 #include "quantum.h"
 
 // readability
-#define ___ KC_NO
+#define XXX KC_NO
 
 // This a shortcut to help you visually see your layout.
 // The following is an example using the Planck MIT layout
@@ -33,13 +33,13 @@
     K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, K5A, K5B, K5C, K5D,        K5E, K5F, K0E, K07, \
     K60, K61, K62,                K63,                     K64, K65, K66, K68,   K69, K6A, K6B, K6C  \
 ) { \
-    { ___, ___, ___, ___, ___, ___, ___, K07, ___, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
+    { XXX, XXX, XXX, XXX, XXX, XXX, XXX, K07, XXX, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F }, \
     { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F }, \
     { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, K3F }, \
     { K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
     { K50, K51, K52, K53, K54, K55, K56, K57, K58, K59, K5A, K5B, K5C, K5D, K5E, K5F }, \
-    { K60, K61, K62, K63, K64, K65, K66, ___, K68, K69, K6A, K6B, K6C, K6D, K6E, K6F }  \
+    { K60, K61, K62, K63, K64, K65, K66, XXX, K68, K69, K6A, K6B, K6C, K6D, K6E, K6F }  \
 }
 
 
