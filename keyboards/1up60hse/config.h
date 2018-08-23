7a67de8c2a8ee6523be5dad0a29f010bb30c15fb
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2018-08-22 20:14:52 -0700

    Keyboard: Finish up the HSE now that I have a board to play with! (#3728)
    * fix up readmes
    
    * update keymap to match configurator default
    
    * add more info

--- keyboards/1up60hse/config.h
+++ keyboards/1up60hse/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    You
+#define MANUFACTURER    1upkeyboards
 #define PRODUCT         1up60hse
-#define DESCRIPTION     A custom keyboard
+#define DESCRIPTION     A custom 60% keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
