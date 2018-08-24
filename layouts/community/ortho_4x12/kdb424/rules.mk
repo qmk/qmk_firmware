0297b289ab609c007319e421bb762cecd95c2141
Kyle Brown <kdb424@gmail.com>
2018-08-23 23:09:21 -0700

    (ノಠ痊ಠ)ノ彡┻━┻

--- layouts/community/ortho_4x12/kdb424/rules.mk
+++ layouts/community/ortho_4x12/kdb424/rules.mk
@@ -1,15 +1,9 @@
 BOOTLOADER        = qmk-dfu # caterina is bad and it should feel bad. I don't use anything but DFU
 BACKLIGHT_ENABLE  = no      # Enable keyboard backlight functionality
 TAP_DANCE_ENABLE  = yes     # Enable tap dance.
-AUTO_SHIFT_ENABLE = no      # Auto hold shift on longer keypress
 USE_IC2           = no
-UNICODEMAP_ENABLE = yes
-
-ifneq ("$(KEYBOARD)","levinson")
-  RGBLIGHT_ENABLE = no      # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
-else
-  RGBLIGHT_ENABLE = yes
-endif
+AUDIO_ENABLE      = no
+RGBLIGHT_ENABLE   = no
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
@@ -22,11 +16,4 @@ else
   NKRO_ENABLE = yes
 endif
 
-ifneq ("$(KEYBOARD)","planck")
-  AUDIO_ENABLE = no
-else
-  AUDIO_ENABLE = yes
-endif
-
-
-EXTRAFLAGS        += -flto
+#EXTRAFLAGS        += -flto
