5ca4c19e880847cd3a11ca7b5e58742b8007b5c3
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2018-08-25 14:44:07 -0400

    Basic layer 0

--- keyboards/whitefox/keymaps/manlycode/keymap.c
+++ keyboards/whitefox/keymaps/manlycode/keymap.c
@@ -34,18 +34,18 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [0] = LAYOUT_truefox( \
-        KC_ESC,        KC_1,       KC_2,       KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,   KC_0,       KC_MINS,    KC_EQL,     KC_GRV,        KC_BSPC, KC_DEL, \
-        KC_TAB,          KC_Q,       KC_W,       KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,       KC_LBRC,    KC_RBRC,   KC_BSLS,       TD(TD_PGDOWN_UP), \
-        KC_LCTL,          KC_A,       KC_S,       KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,                  KC_HOME,\
-        KC_LSPO,           KC_Z,       KC_X,       KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,    KC_RSPC,             KC_UP,         KC_END,\
-        KC_LCTL, KC_LALT,   KC_LGUI,                  KC_SPC,                                      KC_RALT, KC_FN0,                KC_LEFT,  KC_DOWN,       KC_RGHT \
+        KC_GESC,        KC_1,    KC_2,    KC_3,   KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV,          LGUI(KC_BSLS), \
+        KC_TAB,         KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC__MUTE, \
+        KC_LCTL,        KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  TO(0), \
+        KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   TO(1),  \
+        KC_LCTL,        KC_LALT, KC_LGUI,                  KC_SPC,                   KC_RGUI, KC_FN0,           KC_LEFT, KC_DOWN, KC_RGHT \
     ),
   [1] = LAYOUT_truefox( \
-        KC_GESC,     KC_1,       KC_2,       KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,       KC_MINS,    KC_EQL,     KC_GRV,     KC_BSPC, KC_DEL, \
-        KC_TAB,     KC_Q,       KC_W,       KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,             TD(TD_PGDOWN_UP), \
-        CTL_T(KC_BSPC),    KC_A,       KC_S,       KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,                          KC_HOME,\
-        KC_LSPO,    KC_Z,       KC_X,       KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT,     KC_SLSH,    KC_RSPC,    KC_UP,                       KC_END,\
-        KC_LCTL,    KC_LGUI,    KC_LALT,               KC_SPC,                  KC_RALT, KC_FN0,                                     KC_LEFT,KC_DOWN,KC_RGHT \
+        KC_GESC,         KC_1,    KC_2,    KC_3,   KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV,          LGUI(KC_BSLS), \
+        KC_TAB,         KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL, \
+        KC_LCTL,        KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  KC__VOLUP, \
+        KC_LSPO,        KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_UP,   KC__VOLDOWN,  \
+        KC_LCTL,        KC_LALT, KC_LGUI,                  KC_SPC,                   KC_RGUI, KC_FN0,           KC_LEFT, KC_DOWN, KC_RGHT \
     ),
 };
 
