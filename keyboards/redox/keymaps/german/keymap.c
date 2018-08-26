5df6bbbdbe839d189425cfd33422a43bd5be39fc
frederik-h <frederik-h@users.noreply.github.com>
2018-08-26 19:14:28 +0200

    Review changes
    Remove unnecessary code fragments that
    were pointed out in the review
    of PR #3695 by @drashna.

--- keyboards/redox/keymaps/german/keymap.c
+++ keyboards/redox/keymaps/german/keymap.c
@@ -106,13 +106,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
-
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-#endif
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
