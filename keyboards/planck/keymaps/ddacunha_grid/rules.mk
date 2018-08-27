0c6961819708228c67796412dadf00c713fecca9
Daniel Da Cunha <daniel@ddc.im>
2018-08-27 11:54:36 +0800

    Change to rules.mk

--- keyboards/planck/keymaps/ddacunha_grid/rules.mk
+++ keyboards/planck/keymaps/ddacunha_grid/rules.mk
@@ -1,7 +1,7 @@
 
 
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in 
+#   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
