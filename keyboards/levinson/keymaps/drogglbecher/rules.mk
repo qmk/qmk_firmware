d191506006a8101fcb9ed57128f3972ce152d21b
Sebastian Spiess <spiess.sebastian@web.de>
2018-08-27 21:52:37 +0200

    Add levinson layout for drogglbecher

---
+++ keyboards/levinson/keymaps/drogglbecher/rules.mk
@@ -0,0 +1,10 @@
+AUTO_SHIFT_ENABLE = yes
+AUTO_SHIFT_MODIFIERS = yes
+RGBLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = no
+UNICODE_ENABLE = yes
+DEFAULT_FOLDER = levinson/rev2
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
