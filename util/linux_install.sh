265eca0f7c473a5b6f4b2ab6cec6f751506cf49c
Thomas Vandaele <thomas.vandaele+github@gmail.com>
2018-08-27 21:07:30 +0200

    Add manjaro asn arch-based distro

--- util/linux_install.sh
+++ util/linux_install.sh
@@ -9,7 +9,7 @@ elif grep ID /etc/os-release | grep -q debian; then
 	sudo apt-get install gcc unzip wget zip gcc-avr binutils-avr avr-libc \
 	    dfu-programmer dfu-util gcc-arm-none-eabi binutils-arm-none-eabi \
 	    libnewlib-arm-none-eabi
-elif grep ID /etc/os-release | grep -q arch; then
+elif grep ID /etc/os-release | grep -q 'arch\|manjaro'; then
 	sudo pacman -S gcc unzip wget zip avr-gcc avr-binutils avr-libc \
 	    dfu-util arm-none-eabi-gcc arm-none-eabi-binutils \
 	    arm-none-eabi-newlib
