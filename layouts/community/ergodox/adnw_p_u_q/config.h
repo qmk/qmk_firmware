2ede8eb1ed0eb9cc8dcb336aa25291bebcc5b922
Robert Schardt <robert.schardt@gmx.de>
2018-08-27 10:02:58 +0200

    3723 - outsource definitions to config.h

---
+++ layouts/community/ergodox/adnw_p_u_q/config.h
@@ -0,0 +1,12 @@
+#include QMK_KEYBOARD_H
+#include "debug.h"
+#include "action_layer.h"
+#include "keymap_german.h"
+
+// Note:
+// You need to set a Tapping-Value:
+// TAPPING_TERM-Variable in keyboards/ergodox_infinity/config.h 
+// My ideal value is 145 but find your own
+
+// more options here: https://docs.qmk.fm/config_options.html
+#define FORCE_NKRO
--- layouts/community/ergodox/adnw_p_u_q/keymap.c
+++ layouts/community/ergodox/adnw_p_u_q/keymap.c
@@ -1,15 +1,3 @@
-#include QMK_KEYBOARD_H
-#include "debug.h"
-#include "action_layer.h"
-#include "keymap_german.h"
-
-// Note:
-// You need to set a Tapping-Value:
-// TAPPING_TERM-Variable in keyboards/ergodox_infinity/config.h 
-// My ideal value is 145 but find your own
-
-// more options here: https://docs.qmk.fm/config_options.html
-#define FORCE_NKRO
 #define PERMISSIVE_HOLD // tab/hold-Keys should work better with that
 
 #define BASE 0 // default layer / VIM
