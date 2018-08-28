b79ec59dce6e6903e64d76547749d67558cc6008
Ben Busby <ben.busby7@gmail.com>
2018-08-28 00:00:35 -0600

    Added mouse controls for raised layer

--- keyboards/nyquist/keymaps/busby222/keymap.c
+++ keyboards/nyquist/keymaps/busby222/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| |------+-------+------+------+------+-------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  | |   N  |   M   |   ,  |   .  |  Up  | Enter |
  * |------+------+------+------+------+------| |------+-------+------+------+------+-------|
- * | Ctrl |Adjust| Alt  | GUI  |Lower |Space | | Space |Raise |  /   | Left | Down | Right |
+ * | Ctrl |Adjust| GUI  | Alt  |Lower |Space | | Space |Raise |  /   | Left | Down | Right |
  * `-----------------------------------------| |-------------------------------------------|
  */
 
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT , \
-  KC_LCTL,  ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT \
+  KC_LCTL,  ADJUST,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 /* Lower
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------| |-----------------------------------------.
- * |   `  |   7  |   8  |   9  |   *  |   5  | |   6  |   7  |   8  |   9  |  *  | Bksp |
+ * |   `  |   7  |   8  |   9  |   *  |   5  | |   6  |   7  |   8  |   9  |  -  | Bksp |
  * |------+------+------+------+------+------| |------+------+------+------+------+-----|
  * |   `  |   4  |   5  |   6  |   +  |   5  | |  6   |  4   |   5  |  6  |   +  |  Del |
  * |------+------+------+------+------+------| |-----+------+------+------+------+------|
- * | Del  |   1  |   2  |   3  |   -  |  F5  | | F6  |   1  |   2  |   3  |   -  |  \   |
+ * | Del  |   1  |   2  |   3  |   -  |  F5  | | F6  |   1  |   2  |   3  |   *  |  \   |
  * |------+------+------+------+------+------| |-----+------+------+------+------+------|
  * |      |   0  |   .  |   =  |   /  |  F11 | | F12 |   0  |  .   |   =  |   /  |Enter |
  * |------+------+------+------+------+------| |-----+------+------+------+------+------|
@@ -81,10 +81,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------| |----------------------------------------'
  */
 [_RAISE] = LAYOUT( \
-  KC_GRV,  KC_P7,   KC_P8,   KC_P9,   KC_4,    KC_5,    KC_6,    KC_P7,   KC_P8,   KC_P9,   KC_ASTR, KC_BSPC, \
-  KC_GRV,  KC_P4,   KC_P5,   KC_P6,   KC_4,    KC_5,    KC_6,    KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_DEL, \
-  KC_DEL,  KC_P1,   KC_P2,   KC_P3,   KC_F4,   KC_F5,   KC_F6,   KC_P1,   KC_P2,   KC_P3,   KC_MINS, KC_BSLS, \
-  _______, KC_P0,   KC_PDOT, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P0,   KC_DOT,  KC_EQL,  KC_SLSH, _______, \
+  KC_GRV,  KC_MS_ACCEL0,  KC_MS_ACCEL1, KC_MS_ACCEL2, KC_4,    KC_5,    KC_6,    KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_BSPC, \
+  KC_GRV,  KC_MS_WH_DOWN, KC_MS_UP,     KC_MS_WH_UP,  KC_4,    KC_5,    KC_6,    KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_DEL, \
+  KC_DEL,  KC_MS_LEFT,    KC_MS_DOWN,   KC_MS_RIGHT,  KC_F4,   KC_F5,   KC_F6,   KC_P1,   KC_P2,   KC_P3,   KC_ASTR, KC_BSLS, \
+  _______, KC_P0,         KC_MS_BTN1,   KC_MS_BTN1,   KC_MS_BTN2,  KC_F11,  KC_F12,  KC_P0,   KC_DOT,  KC_EQL,  KC_SLSH, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
