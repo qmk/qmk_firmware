7f90c4e11f81330facd8fd129c19746de99f6fb6
noroadsleft <xxiinophobia@yahoo.com>
2018-08-28 13:47:51 -0700

    Added BOOTLOADER rule to avr/rules.mk per @drashna

--- quantum/template/avr/rules.mk
+++ quantum/template/avr/rules.mk
@@ -48,6 +48,13 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 OPT_DEFS += -DBOOTLOADER_SIZE=4096
 
 
+# Bootloader
+#     This definition is optional, and if your keyboard supports multiple bootloaders of
+#     different sizes, comment this out, and the correct address will be loaded
+#     automatically (+60). See bootloader.mk for all options.
+BOOTLOADER = atmel-dfu
+
+
 # Build Options
 #   change yes to no to disable
 #
