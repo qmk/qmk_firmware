e2c85ac64e577a2ca9873ca2a8823b8e1b4575b2
Xton <cdewan@apple.com>
2018-08-29 21:41:17 -0400

    enable power return

--- keyboards/v60_type_r/keymaps/xtonhasvim/keymap.c
+++ keyboards/v60_type_r/keymaps/xtonhasvim/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FUN] = LAYOUT_all(
 	       KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  X_____X, KC_DEL, \
 	       X_____X,   RGB_M_P,  KC_UP,    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,   X_____X, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,    X_____X,          KC_INS, \
-	       X_____X,   KC_LEFT,  KC_DOWN,  KC_RIGHT, RGB_HUD,  RGB_SAD,  RGB_VAD,   X_____X, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT,          X_____X,          \
+	       X_____X,   KC_LEFT,  KC_DOWN,  KC_RIGHT, RGB_HUD,  RGB_SAD,  RGB_VAD,   X_____X, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT,          FIREY_RETURN,          \
 	       X_____X,   X_____X,  BL_DEC,   BL_STEP,  BL_INC,   X_____X,  KC_VOLD,   KC_VOLU, KC_MUTE, KC_END,  KC_PGDN,  KC_DOWN,X_____X, X_____X,  \
 	       X_____X,   X_____X,  X_____X,            X_____X,                                                           X_____X,  X_____X, X_____X, _______
     ),
