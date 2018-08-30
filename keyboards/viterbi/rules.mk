c9101fdb9cd0ae708bf4e43482123f27343d25a9
Drashna Jaelre <drashna@live.com>
2018-08-30 10:59:28 -0700

    Convert Viterbi to Split Common code

--- keyboards/viterbi/rules.mk
+++ keyboards/viterbi/rules.mk
@@ -1,8 +1,3 @@
-SRC += matrix.c \
-	   i2c.c \
-	   split_util.c \
-	   serial.c
-
 # MCU name
 #MCU = at90usb1287
 MCU = atmega32u4
@@ -63,13 +58,11 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
-SUBPROJECT_rev1 = yes
-USE_I2C = yes
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-CUSTOM_MATRIX = yes
+SPLIT_KEYBOARD = yes
 
 DEFAULT_FOLDER = viterbi/rev1
 
