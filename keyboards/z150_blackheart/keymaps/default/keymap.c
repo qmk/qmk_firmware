45c48e08abdbc6322aeaf6fce1e66e4c87195feb
blindassassin111 <38090555+blindassassin111@users.noreply.github.com>
2018-08-29 21:46:11 -0500

    Fixing Z-150 Issues
    Still unsure where the extra "\" is that through the error.

--- keyboards/z150_blackheart/keymaps/default/keymap.c
+++ keyboards/z150_blackheart/keymaps/default/keymap.c
@@ -21,22 +21,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		______, ______, 	______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, 		 		______, ______, ______, ______,
 		______, ______, 	______, ______, 								______, 										______,				 		______,  ______
 		),
-/* Not sure how to setup dual layout support so for now the code is commented out.
-	Z150_TKL(
+
+	LAYOUT_Z150_TKL(
 		KC_F1, 	KC_F2, 		KC_ESC,  KC_1, 	  KC_2,   KC_3, 	 KC_4, 	KC_5, 	KC_6, KC_7, KC_8, 	 KC_9, 	 KC_0, 	  KC_MINS, KC_EQL, 	KC_BSPC, 		KC_INS,  KC_HOME, KC_PGUP, 
 		KC_F3, 	KC_F4, 		KC_TAB,  KC_Q, 	  KC_W,   KC_E, 	 KC_R, 	KC_T, 	KC_Y, KC_U, KC_I, 	 KC_O, 	 KC_P, 	  KC_LBRC, KC_RBRC, KC_BSLS, 		KC_DEL,  KC_END,  KC_PGDN, 
 		KC_F5, 	KC_F6, 		KC_CAPS, KC_A, 	  KC_S,   KC_D, 	 KC_F, 	KC_G, 	KC_H, KC_J, KC_K, 	 KC_L, 	 KC_SCLN, KC_QUOT, KC_ENT, 			 		XXXXXX,  XXXXXX,  XXXXXX, 
-		KC_F7, 	KC_F8, 		KC_LSFT, KC_Z, 	  KC_X,   KC_C, 	 KC_V, 	KC_B, 	KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(1), 			 		XXXXXX,  KC_UP,   XXXXXX, 
+		KC_F7, 	KC_F8, 		KC_LSFT, KC_BSLS, KC_Z,   KC_X,      KC_C, 	KC_V, 	KC_B, KC_N, KC_M,    KC_COMM,KC_DOT,  KC_SLSH, KC_RSFT, MO(1), 			XXXXXX,  KC_UP,   XXXXXX, 
 		KC_F9, 	KC_F10,		KC_LCTL, KC_LGUI, KC_LALT,KC_LSFT, 		KC_SPC, 							 KC_RALT, KC_RGUI, KC_MENU, KC_RCTL, 			KC_LEFT, KC_DOWN, KC_RGHT
 		),
 
-	Z150_TKL(
+	LAYOUT_Z150_TKL(
 		______, ______, 	RESET,  ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, 			______, ______, ______, 
 		______, ______, 	______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, 			______, ______, ______, 
 		______, ______, 	______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, 					______, ______, ______, 
 		______, ______, 	______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, 					______, ______, ______, 
 		______, ______, 	______, ______, ______, ______, 		______,                            		______, ______, ______, ______, 			______, ______, ______
-		),*/
+		),
 };
 
 void matrix_init_user(void) {
