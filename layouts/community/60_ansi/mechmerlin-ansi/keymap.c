9d5bc2a19b58e4813c43f0df5cc65b256fee2f73
mechmerlin <mechmerlin@gmail.com>
2018-08-31 08:22:04 -0700

    put the FNX into a userspace incase I ever want to use it for something else

--- layouts/community/60_ansi/mechmerlin-ansi/keymap.c
+++ layouts/community/60_ansi/mechmerlin-ansi/keymap.c
@@ -24,22 +24,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,                               KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT)
 
  };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    static uint16_t fnx_layer_timer;
-
-    switch (keycode){
-        case KC_FNX:                                    
-            if(record->event.pressed){
-                fnx_layer_timer = timer_read();
-                layer_on(_FL);
-            } else {
-                layer_off(_FL);
-                if (timer_elapsed(fnx_layer_timer) < 150) {  
-                    layer_invert(_AL);
-                }
-            }
-        return false;
-    }
-    return true;
-}
