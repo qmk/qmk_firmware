d217bb43fb625080294b588961f30956d01599b1
mechmerlin <mechmerlin@gmail.com>
2018-08-31 08:10:03 -0700

    spread userspace love to 75 layout

--- layouts/community/75_ansi/mechmerlin-75_ansi/keymap.c
+++ layouts/community/75_ansi/mechmerlin-75_ansi/keymap.c
@@ -1,10 +1,5 @@
 #include QMK_KEYBOARD_H
-
-#define _BL   0 // Base Layer
-#define _FL   1 // Function Layer
-
-// Custom #defined keycodes
-#define KC_CTCP MT(KC_LCTL, KC_CAPS)
+#include "mechmerlin.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
