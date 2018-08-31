d756591531df46204d059307b0be5b6b1da6c442
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2018-08-30 23:04:42 -0300

    source cosmetics

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -1,13 +1,19 @@
 #include QMK_KEYBOARD_H
 
+#define _BL 0
+#define _FN1 1
+#define _FN2 2
+
 #define MISCTRL LCTL(KC_UP)
 #define CMDENT LGUI_T(KC_ENT)
 #define CTRLESC LCTL_T(KC_ESC)
 #define FN2BSPC LT(_FN2, KC_BSPC)
 
-#define _BL 0
-#define _FN1 1
-#define _FN2 2
+// arrow cluster
+#define CLLEFT MT(MOD_LGUI, KC_LEFT)
+#define CLDOWN MT(MOD_LALT, KC_DOWN)
+#define CLUP MT(MOD_RCTL, KC_UP)
+#define CLRIGHT LT(_FN1, KC_RIGHT)
 
 void change_leds_to(uint16_t, uint8_t, rgblight_config_t);
 bool state_changed = false;
@@ -19,11 +25,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
-		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,    KC_6,         KC_7,      KC_8,         KC_9,      KC_0,     KC_MINS,      KC_EQL,   KC_BSLS,   KC_GRV,
-		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,         KC_U,      KC_I,         KC_O,      KC_P,     KC_LBRC,      KC_RBRC,  KC_BSPC,
-		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,         KC_J,      KC_K,         KC_L,      KC_SCLN,  KC_QUOT,      KC_ENT,
-		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,         KC_N,      KC_M,         KC_COMM,   KC_DOT,   KC_SLSH,      KC_RSFT,  TG(_FN2),
-		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  FN2BSPC,  CMDENT,  MT(MOD_LGUI,  KC_LEFT),  MT(MOD_LALT,  KC_DOWN),  KC_NO,    MT(MOD_RCTL,  KC_UP),   LT(_FN1,   KC_RIGHT)),
+		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_GRV,
+		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
+		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  TG(_FN2),
+		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  FN2BSPC,  CMDENT,  CLLEFT,  CLDOWN,  KC_NO,  CLUP,     CLRIGHT),
 
   [_FN1] = LAYOUT(
 		KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_F11,   KC_F12,   _______,  KC_EJCT,
