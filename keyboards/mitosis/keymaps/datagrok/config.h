1bb29eee032bd5ba6d0ba67b109c64e865e64ccb
Michael F. Lamb <mike@datagrok.org>
2018-09-01 23:08:21 -0700

    mitosis:datagrok: add makefile argument MITOSIS_DATAGROK_SLOWUART

--- keyboards/mitosis/keymaps/datagrok/config.h
+++ keyboards/mitosis/keymaps/datagrok/config.h
@@ -3,6 +3,17 @@
 
 #include "../../config.h"
 
+#ifdef MITOSIS_DATAGROK_SLOWUART
+// This is the highest possible baud rate that a pro micro clocked at 8Mhz can
+// support without errors. I don't notice any difference in behavior at this
+// slower speed. (So I think it should maybe be the default, to allow a single
+// codebase to support both available flavors of pro micro.) This requires a
+// corresponding change to the wireless module firmware; see
+// https://github.com/reversebias/mitosis/pull/10
+#undef SERIAL_UART_BAUD // avoids redefinition warning
+#define SERIAL_UART_BAUD 250000
+#endif
+
 // I want to place an underscore as tap behavior on the right shift key. But
 // RSFT_T(KC_UNDS) doesn't work; mod-tap doesn't work with pre-shifted keys. So
 // instead we take advantage of Space Cadet Shift that does something similar
