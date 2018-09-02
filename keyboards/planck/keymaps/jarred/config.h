b683809946eaa0e04901c683a27fdb6e38446b2f
Jarred Steenvoorden <jazzjarred@gmail.com>
2018-09-02 10:57:13 +1000

    Update userspace with common config.h

--- keyboards/planck/keymaps/jarred/config.h
+++ keyboards/planck/keymaps/jarred/config.h
@@ -19,21 +19,4 @@
 
 #include "config_common.h"
 
-#define PREVENT_STUCK_MODIFIERS
-
-#ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
-#endif
-
-#define TAPPING_TERM    200
-
-#define MOUSEKEY_DELAY 0
-#define MOUSEKEY_INTERVAL 16
-#define MOUSEKEY_TIME_TO_MAX 40
-#define MOUSEKEY_MAX_SPEED 5
-
-#define MOUSEKEY_WHEEL_DELAY 0
-#define MOUSEKEY_WHEEL_MAX_SPEED   4
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 255	
-
 #endif
