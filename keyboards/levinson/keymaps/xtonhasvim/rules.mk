47c817bdf8ced173a0f67f6c1155ba80040749ae
Xton <cdewan@apple.com>
2018-09-02 14:53:11 -0700

    pulled in levinson from staging.

---
+++ keyboards/levinson/keymaps/xtonhasvim/rules.mk
@@ -0,0 +1,9 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+# MOUSEKEY_ENABLE = yes
+# BACKLIGHT_ENABLE = yes
+# AUDIO_ENABLE = no
+
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
