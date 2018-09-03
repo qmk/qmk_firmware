9478dab1781b21fc9f6a9cd89c9452dbf221797f
antonio miranda <antsmiranda@gmail.com>
2018-09-03 16:36:56 +0200

    Enable sound and freeup some media keys on layers

--- keyboards/planck/keymaps/am/keymap.c
+++ keyboards/planck/keymaps/am/keymap.c
@@ -85,14 +85,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   \  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   _  |   =  |   [  |   ]  |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Sleep |      |      |      |      |             |      | Prev | Stop | Slct | Mute |
+ * |Sleep |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
   {KC_PIPE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV},
   {KC_BSLS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS, KC_EQL,  KC_LBRC, KC_RBRC, KC_SLSH},
-  {KC_SLEP, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MSTP, KC_MSEL, KC_MUTE}
+  {KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 /* Raise
@@ -103,14 +103,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   =  |   7  |   8  |   9  |   0  |   -  |   /  |   1  |   2  |   3  |   .  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Sleep |      |      |      |      |             |      | Prev | Stop | Slct | Mute |
+ * |Sleep |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
   {KC_0,    KC_1,    KC_2,    KC_3,    KC_4,   KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
   {KC_CIRC,  KC_4,    KC_5,    KC_6,    KC_DOT, KC_PLUS,  KC_ASTR, KC_4,    KC_5,    KC_6,    KC_MINS, KC_DLR},
   {KC_EQL,  KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS,  KC_SLSH, KC_1,    KC_2,    KC_3,    KC_DOT,  KC_ENT},
-  {KC_SLEP, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MSTP, KC_MSEL, KC_MUTE}
+  {KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
 /* TouchCursor layer (http://martin-stone.github.io/touchcursor/) plus personal customizations
