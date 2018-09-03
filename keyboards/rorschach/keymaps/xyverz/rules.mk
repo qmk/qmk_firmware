5c317a64e4fcf011501e408b24ab58f0d1812dd5
Ian Sterling <xyverz@gmail.com>
2018-09-03 12:32:37 -0700

    Moved keymap to separate branch
    Moved the Rorschach keymap I was working on to a separate branch
    for development purposes. Not ready to push it up to upstream
    master.

--- keyboards/rorschach/keymaps/xyverz/rules.mk
+++
@@ -1,7 +0,0 @@
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output on port C6
-RGBLIGHT_ENABLE = yes       # Enable RGB Underglow lighting
-
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
