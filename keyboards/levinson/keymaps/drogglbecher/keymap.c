b6753f1998dda19a84a5eefae0a5251e1c66398c
Sebastian Spiess <spiess.sebastian@web.de>
2018-09-03 20:30:44 +0200

    Add tab to layer 0

--- keyboards/levinson/keymaps/drogglbecher/keymap.c
+++ keyboards/levinson/keymaps/drogglbecher/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * ├──────┼──────┼──────┼──────┼──────┼──────┤ ├──────┼──────┼──────┼──────┼──────┼──────┤
     * │ SHFT │  z   │  x   │  c   │  v   │  b   │ │  n   │  m   │ [ {  │ ] }  │ ; :  │ SHFT │
     * ├──────┼──────┼──────┼──────┼──────┼──────┤ ├──────┼──────┼──────┼──────┼──────┼──────┤
-    * │ L1   │ L2   │ OS   │ \ |  │ SPC  │ SPC  │ │ SPC  │ SPC  │ , <  │ . >  │ - _  │  / ? │
+    * │ L1   │ OS   │  L2  │ TAB  | SPC  │ SPC  │ │ SPC  │ SPC  │ , <  │ . >  │ - _  │ / ?  │
     * └──────┴──────┴──────┴──────┴──────┴──────┘ └──────┴──────┴──────┴──────┴──────┴──────┘
     */
 
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,   KC_Q,      KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC, \
      KC_LCTL,  KC_A,      KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_QUOT,    KC_ENT, \
      KC_LSFT,  KC_Z,      KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_LBRC,    KC_RBRC,    KC_SCLN,    KC_RSFT, \
-     KC_X1,    KC_X2,     KC_LGUI,    KC_BSLS,    KC_SPC,     KC_SPC,     KC_SPC,     KC_SPC,     KC_COMM,    KC_DOT,     KC_MINS,    KC_SLSH \
+     KC_X1,    KC_RGUI,   KC_X2,      KC_TAB,     KC_SPC,     KC_SPC,     KC_SPC,     KC_SPC,     KC_COMM,    KC_DOT,     KC_MINS,    KC_SLSH \
   ),
 
    /* ┌──────┬──────┬──────┬──────┬──────┬──────┐ ┌──────┬──────┬──────┬──────┬──────┬──────┐
@@ -41,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * ├──────┼──────┼──────┼──────┼──────┼──────┤ ├──────┼──────┼──────┼──────┼──────┼──────┤
     * │      │  Ä   │  Ö   │  Ü   │  €   │      │ │      │      │  (   │  )   │      │      │
     * ├──────┼──────┼──────┼──────┼──────┼──────┤ ├──────┼──────┼──────┼──────┼──────┼──────┤
-    * │      │      │ ALT  │      │ SPC  │ SPC  │ │ SPC  │ SPC  │      │      │      │      │
+    * │      | ALT  │      │      | SPC  │ SPC  │ │ SPC  │ SPC  │      │      │      │  \ | │
     * └──────┴──────┴──────┴──────┴──────┴──────┘ └──────┴──────┴──────┴──────┴──────┴──────┘
     */
 
   [_FN1] = LAYOUT_ortho_4x12( \
     _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL, \
     _______,    UC(L'ä'),   UC(L'ö'),   UC(L'ü'),   UC(L'ß'),   _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    KC_PIPE, \
-    _______,    UC(L'Ä'),   UC(L'Ö'),   UC(L'Ü'),   UC(L'€'),   _______,    _______,    KC_LPRN,    KC_RPRN,    _______,    _______,    _______, \
-    _______,    _______,    KC_LALT,    _______,    KC_SPC,     KC_SPC,     KC_SPC,     KC_SPC,     _______,    _______,    _______,    _______ \
+    _______,    UC(L'Ä'),   UC(L'Ö'),   UC(L'Ü'),   UC(L'€'),   _______,    _______,    _______,    KC_LPRN,    KC_RPRN,    _______,    _______, \
+    _______,    KC_LALT,    _______,    KC_TAB,     KC_SPC,     KC_SPC,     KC_SPC,     KC_SPC,     _______,    _______,    _______,    KC_BSLS \
   ),
 
    /* ┌──────┬──────┬──────┬──────┬──────┬──────┐ ┌──────┬──────┬──────┬──────┬──────┬──────┐
