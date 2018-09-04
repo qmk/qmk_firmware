08657c139f8a9d01a6c7df22ca38199b543a302e
antonio miranda <antsmiranda@gmail.com>
2018-09-04 23:07:30 +0200

    Fix config.h unescaped line

--- keyboards/planck/keymaps/am/config.h
+++ keyboards/planck/keymaps/am/config.h
@@ -7,7 +7,7 @@
     #define STARTUP_SONG SONG(PLANCK_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND),   \
-                                  SONG(COLEMAK_SOUND)
+                                  SONG(COLEMAK_SOUND) \
                                 }
 #endif
 
