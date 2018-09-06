d7a0445c943ceee00b167b96331d43f76266fdd7
mechmerlin <mechmerlin@gmail.com>
2018-09-06 13:09:19 -0700

    create default keymap

--- util/new_keymap.sh
+++ util/new_keymap.sh
@@ -16,8 +16,20 @@ if [ -z "$USERNAME" ]; then
     exit 1
 fi
 
+cd "$(dirname "$0")/.."
 
 if [ ! -d "keyboards/$1" ]; then
 	printf "Error! keyboards/%s does not exist!\n" "$1"
 	exit 1
 fi
+
+if [ -d "keyboards/$1/keymaps/$2" ]; then
+	printf "Error! keyboards/%s/%s already exists!\n" "$1" "$2"
+	exit 1
+fi
+
+cp -r keyboards/$1/keymaps/default keyboards/$1/keymaps/$2
+
+printf "Make your new keymap by typing \n"
+printf "   make %s:%s\n" "$1" "$2"
+printf "from qmk_firmware\n"
