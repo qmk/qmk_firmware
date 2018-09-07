a0e8c63125901d0ab62002460dad349e88eecbc8
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2018-09-07 00:36:08 -0300

    Introduce a "config" layer

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -3,6 +3,7 @@
 #define _BL 0
 #define _FN1 1
 #define _FN2 2
+#define _CFG 3
 
 #define MISCTRL LCTL(KC_UP)
 #define CTRLESC LCTL_T(KC_ESC)
@@ -23,25 +24,32 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
-		KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_GRV,
+		KC_GRV,    KC_1,     KC_2,     KC_3,    KC_4,     KC_5,    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_GRV,
 		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
-		CTRLESC ,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+		CTRLESC,   KC_A,     KC_S,     KC_D,    KC_F,     KC_G,    KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
 		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  MO(_FN2),
 		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  MO(_FN2), KC_BSPC, CLLEFT,  CLDOWN,  KC_NO,  CLUP,     CLRIGHT),
 
   [_FN1] = LAYOUT(
-		KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,    KC_F11,   KC_F12,   _______,  KC_EJCT,
-		_______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,   RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,   KC_UP,     KC_MRWD,  KC_MFFD,  _______,
-		_______,  _______,  _______,  _______,  KC_RIGHT,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  _______,   _______,  KC_PENT,
-		_______,  _______,  _______,  _______,  _______,   _______,  KC_LEFT,  KC_DOWN,  KC_ENT,   _______,   _______,   _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,   KC_DEL,   _______,  _______,  _______,  _______,   RESET),
+		KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,   KC_F12,   _______,  KC_EJCT,
+		_______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_ACL0,   KC_ACL1,  KC_ACL2,  _______,  _______,  _______,   KC_UP,    _______,  _______,   _______,
+		_______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_RIGHT,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  _______,  _______,  KC_PENT,
+		_______,  _______,  _______,  _______,  _______,   _______,  KC_LEFT,  KC_DOWN,  KC_ENT,   _______,   _______,  _______,  _______,  MO(_CFG),
+		_______,  _______,  _______,  _______,  MO(_CFG),  KC_DEL,   _______,  _______,  _______,  _______,   RESET),
 
   [_FN2] = LAYOUT(
-		KC_GRV,   _______,  _______,  MISCTRL,  _______,   RGB_VAD,  RGB_VAI,  KC_MRWD,  KC_MPLY,  KC_MFFD,   KC_MUTE,  KC__VOLDOWN,  KC__VOLUP,  KC_K8s,  KC_EJCT,
-		_______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_ACL0,   KC_ACL1,  KC_ACL2,  _______,  _______,  _______,   KC_UP,    _______,      _______,    _______,
-		_______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_RIGHT,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  _______,  _______,      KC_PENT,
-		_______,  _______,  _______,  _______,  _______,   _______,  KC_LEFT,  KC_DOWN,  KC_ENT,   _______,   _______,  _______,      _______,    _______,
-		_______,  _______,  _______,  _______,  _______,   KC_DEL,   _______,  _______,  _______,  _______,   RESET)
+		KC_GRV,    _______,  _______,  MISCTRL,  _______,   RGB_VAD,  RGB_VAI,  KC_MRWD,  KC_MPLY,  KC_MFFD,   KC_MUTE,  KC__VOLDOWN,  KC__VOLUP,  _______,  KC_EJCT,
+		_______,   KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_ACL0,   KC_ACL1,  KC_ACL2,  _______,  _______,  _______,   KC_UP,    _______,      _______,    _______,
+		_______,   KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_RIGHT,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  _______,  _______,      KC_PENT,
+		_______,   _______,  _______,  _______,  _______,   _______,  KC_LEFT,  KC_DOWN,  KC_ENT,   _______,   _______,  _______,      _______,    _______,
+		MO(_CFG),  _______,  _______,  _______,  _______,   KC_DEL,   _______,  _______,  _______,  _______,   MO(_CFG)),
+
+  [_CFG] = LAYOUT(
+		_______,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,  RGB_M_SN,  RGB_M_K,  RGB_M_X,  RGB_M_G,  RGB_M_T,  _______,  _______,  _______,  _______,  RESET,
+		_______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,   RGB_SAI,   RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,  RESET,
+		_______,  _______,  _______,  _______,  _______,   _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,
+		_______,  _______,  _______,  _______,  _______,   _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+		_______,  _______,  _______,  _______,  _______,   _______,   _______,  _______,  _______,  _______,  _______),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
@@ -59,12 +67,17 @@ void matrix_scan_user(void) {
   switch(biton32(layer_state)) {
     case _FN1:
       hue = 2;
-      sat = 178;
+      sat = 225;
       break;
 
     case _FN2:
-      hue = 254;
-      sat = 120;
+      hue = 275;
+      sat = 255;
+      break;
+
+    case _CFG:
+      hue = 110;
+      sat = 255;
       break;
   }
 
@@ -77,14 +90,6 @@ void matrix_scan_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch(keycode) {
-    case KC_K8s: {
-      if (record->event.pressed) {
-        SEND_STRING("kubectl ");
-        return false;
-      }
-      break;
-    }
-
     case GRAVE_ESC: {
       uint8_t shifted = get_mods() & ((MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT)
                                       |MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)
@@ -145,6 +150,7 @@ uint32_t layer_state_set_user(uint32_t state) {
 
       case _FN1:
       case _FN2:
+      case _CFG:
         state_changed = true;
         break;
 
