2a768921c2d42c7dc2a8ec1139b4432f9942757b
mechmerlin <mechmerlin@gmail.com>
2018-09-07 18:17:03 -0700

    figure out the column pins

--- keyboards/panc60/config.h
+++ keyboards/panc60/config.h
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 15
 
 #define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_COL_PINS { A0, A1, A2, A3, A4, A5, A6, A7, C7, C6, C5, C4, C3, C2, D7 }
 #define UNUSED_PINS
 
 #define DIODE_DIRECTION COL2ROW
