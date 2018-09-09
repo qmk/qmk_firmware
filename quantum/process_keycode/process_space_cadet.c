3f4ae0920db83eccf75d27600929f8b49831277c
leico <yoshitaka_12@iamas.ac.jp>
2018-09-10 00:35:46 +0900

    refactored space cadet process

--- quantum/process_keycode/process_space_cadet.c
+++ quantum/process_keycode/process_space_cadet.c
@@ -16,6 +16,8 @@
 
 #include "quantum.h"
 
+
+// Shift / paren setup
 #ifndef LSPO_KEY
   #define LSPO_KEY KC_9
 #endif
@@ -35,15 +37,177 @@
   #define SFTENT_KEY KC_ENT
 #endif
 
+/*
 static bool     shift_interrupted[2] = {0, 0};
 static uint16_t scs_timer        [2] = {0, 0};
+*/
+
+static bool     shift_pressed[ 2 ] = { false, false };
+static uint16_t scs_time     [ 2 ] = { 0, 0 };
+
+
+
+
+
+
+
+
+
+static void stop_SCS( uint8_t pos ){ 
+
+  shift_pressed[ pos ] = false;
+//  scs_time     [ pos ] = UINT16_C( 0 ); // + 120 bytes
+
+  return;
+}
+
+
+static void start_SCS( uint8_t pos, uint16_t time ){
+
+  shift_pressed[ pos ] = true;
+  scs_time     [ pos ] = time;
+
+  return;
+}
+
+
+
+
+
+
+static bool is_while_tap( uint8_t pos ){ 
+  return timer_elapsed( scs_time[ pos ] ) < TAPPING_TERM;
+}
+
+static bool is_pressed( uint8_t pos ){ 
+  return shift_pressed[ pos ];
+}
+
+# ifdef DISABLE_SPACE_CADET_ROLLOVER
+static bool is_tap_enable( uint8_t pos ){
+  return is_pressed( pos ) && is_while_tap( pos );
+}
+
+# endif
+
+
+
+
+static void pressed_SCS ( uint16_t time, uint8_t pos, uint8_t brother, uint8_t shift ){
+
+  register_mods( MOD_BIT( shift ) );
+ 
+# ifdef DISABLE_SPACE_CADET_ROLLOVER
+    if( is_tap_enable( brother ) ) {
+      stop_SCS ( brother );
+      return;
+    }
+# endif
+
+  start_SCS( pos, time );
+
+  return;
+}
+
+
+static void released_SCS ( uint8_t pos, uint8_t shift, uint8_t mod, uint16_t key, bool is_sftent ){
+
+    if( ! is_pressed( pos ) ) {
+      unregister_mods( MOD_BIT( shift ) );
+      return;
+    }
+
+    bool mod_changed = false;
+
+      if( is_sftent ){                          // is this shift enter ?
+        unregister_mods( MOD_BIT( shift ) );
+      }
+    /* -------------------- ↑ Shift Enter ↓ Shift Parenthesis -------------- */
+#   ifdef DISABLE_SPACE_CADET_MODIFIER
+      else {
+        unregister_mods( MOD_BIT( shift ) );
+#   else
+      else if( mod != shift ) {
+        unregister_mods( MOD_BIT( shift ) );
+          register_mods( MOD_BIT( mod   ) );
+        mod_changed = true;
+#   endif
+      }
+
+
+  if( is_while_tap( pos ) ) {
+      register_code( key );
+    unregister_code( key );
+  }
+
+  stop_SCS ( pos );
+
+
+  unregister_mods(
+      ( mod_changed ) ? MOD_BIT( mod   ) :      // Shift Parenthesis and Mod changed
+      ( ! is_sftent ) ? MOD_BIT( shift ) : 0    // Shift Parenthesis not Mod changed, or other, nothing happen
+  );
+  return;
+}
+
+
+
 
 
 
 bool process_space_cadet( uint16_t keycode, keyrecord_t *record ){ 
 
-  switch( keycode ) { 
-  
+  switch( keycode ) {
+
+    case KC_LSPO : {
+      if( record -> event.pressed ) { 
+        pressed_SCS( record -> event.time, 0, 1, KC_LSFT );
+      }
+      else{
+        released_SCS( 0, KC_LSFT, LSPO_MOD, LSPO_KEY, false );
+      }
+
+      return false;
+    } 
+    break;
+
+    case KC_RSPC : {
+      if( record -> event.pressed ) {
+        pressed_SCS( record -> event.time, 1, 0, KC_RSFT );
+      }
+      else {
+        released_SCS( 1, KC_RSFT, RSPC_MOD, RSPC_KEY, false );
+      }
+
+      return false;
+    }
+    break;
+
+    case KC_SFTENT : {
+      if( record -> event.pressed ) {
+        pressed_SCS( record -> event.time, 1, 0, KC_RSFT );
+      }
+      else {
+        released_SCS( 1, KC_RSFT, RSPC_MOD, SFTENT_KEY, true );
+      }
+
+      return false;
+    }
+    break;
+
+    default: {
+/*
+      shift_interrupted[0] = true;
+      shift_interrupted[1] = true;
+*/
+      stop_SCS( 0 );
+      stop_SCS( 1 );
+
+    }
+    break;
+
+
+/*
     case KC_LSPO: {
       if (record->event.pressed) {
         shift_interrupted[0] = false;
@@ -113,7 +277,6 @@ bool process_space_cadet( uint16_t keycode, keyrecord_t *record ){
       }
       return false;
     }
-
     case KC_SFTENT: {
       if (record->event.pressed) {
         shift_interrupted[1] = false;
@@ -130,12 +293,7 @@ bool process_space_cadet( uint16_t keycode, keyrecord_t *record ){
       }
       return false;
     }
-
-    default: {
-      shift_interrupted[0] = true;
-      shift_interrupted[1] = true;
-      break;
-    }
+*/
 
 
   }
