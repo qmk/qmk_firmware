0afd217d4ededd1c0805bf24e1345b49091eb93d
CarReq <c94rivera@gmail.com>
2018-09-10 20:05:09 -0400

    altered keymap to add missing keys and fix issues with space

--- keyboards/planck/keymaps/carreq/keymap.c
+++ keyboards/planck/keymaps/carreq/keymap.c
@@ -30,8 +30,7 @@ enum planck_layers {
   _RAISE,
   _PLOVER,
   _ADJUST,
-  _FN,
-  _SPACE_FN
+  _FN
 };
 
 enum planck_keycodes {
@@ -52,8 +51,6 @@ enum planck_keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define FN MO(_FN)
-#define SPACE_FN LT(_SPACE_FN, KC_SPC)
-
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -73,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, FN,   KC_LALT, KC_LGUI, LOWER,   SPACE_FN,  SPACE_FN,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, FN,	  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  	KC_SPC,	   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Workman
@@ -149,25 +146,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,        KC_F11,  KC_F12,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
     XXXXXXX,        XXXXXXX, ROLL,    XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, SHRUG,   XXXXXXX, 
     XXXXXXX,        _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_SPC,   KC_SPC,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-),
-
-
-/* Space fn
- * ,-----------------------------------------------------------------------------------.
- * |PRNT SC|      |  UP  |      |      |      |      |      |      |      |      |SLEEP |
- * |-------+------+------+------+------+-------------+------+------+------+------+------|
- * |       | LEFT | DOWN | RIGHT|      |      |      | LEFT | DOWN |  UP  | RIGHT|      |
- * |-------+------+------+------+------+------|------+------+------+------+------+------| 
- * |       |      |      |      |      |      |      |      |      |      |      |      |
- * |-------+------+------+------+------+------+------+------+------+------+------+------|
- * |       |      |      |      |      |             |      | MUTE |VOLDWN|VOL UP|      |
- * `-----------------------------------------------------------------------------------'
- */
-[_SPACE_FN] = LAYOUT_planck_grid(
-    KC_PSCR,  XXXXXXX, KC_UP,    XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    KC_SLEP, 
-    XXXXXXX,  KC_LEFT, KC_DOWN,  KC_RIGHT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT, KC_DOWN,  KC_UP,       KC_RIGHT,   XXXXXXX, 
-    XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX, 
-    XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  _______,  _______,  XXXXXXX, KC__MUTE, KC__VOLDOWN, KC__VOLUP,  XXXXXXX
 ),
 
 
