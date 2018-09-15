8ef6d342bad9f4c54113018fff4b7218c1923dd9
James Laird-Wah <james@laird-wah.net>
2018-09-15 21:55:14 +1000

    model01: use fast banked LED setting commands

--- keyboards/model01/leds.c
+++ keyboards/model01/leds.c
@@ -17,6 +17,7 @@
 #include <i2c_master.h>
 #include <led_tables.h>
 #include <rgb_matrix.h>
+#include <string.h>
 #include "model01.h"
 
 #define I2C_TIMEOUT     1000
@@ -111,7 +112,22 @@ const rgb_led g_rgb_leds[DRIVER_LED_TOTAL] = {
   {{0x07}, {220, 35}, 0},
 };
 
+static struct {
+  uint8_t b;
+  uint8_t g;
+  uint8_t r;
+} __attribute__((packed)) led_state[64];
 
+static void set_color(int index, uint8_t r, uint8_t g, uint8_t b) {
+  led_state[index].r = r;
+  led_state[index].g = g;
+  led_state[index].b = b;
+}
+
+static void set_color_all(uint8_t r, uint8_t g, uint8_t b) {
+  for (int i=0; i<DRIVER_LED_TOTAL; i++)
+    set_color(i, r, g, b);
+}
 
 static void init(void) {
   // Enable high current pathway to LEDs - this does violate the USB spec though! (1.6 amps...)
@@ -124,13 +140,27 @@ static void init(void) {
 }
 
 static void flush(void) {
+  uint8_t *bank_data = (uint8_t*)&led_state[0];
+  uint8_t command[1 + 8*3];
+  for (int hand=0; hand<2; hand++) {
+    int addr = I2C_ADDR(hand);
+
+    for (int bank=0; bank<4; bank++) {
+      command[0] = TWI_CMD_LED_BASE + bank;
+      memcpy(&command[1], bank_data, 8*3);
+      i2c_transmit(addr, command, sizeof(command), I2C_TIMEOUT);
+      _delay_us(100);
+
+      bank_data += 8*3;
+    }
+  }
 }
 
 const rgb_matrix_driver_t rgb_matrix_driver = {
   .init = init,
   .flush = flush,
-  .set_color = set_led_to,
-  .set_color_all = set_all_leds_to
+  .set_color = set_color,
+  .set_color_all = set_color_all
 };
 
 #endif
