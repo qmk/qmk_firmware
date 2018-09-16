d178bc6e775c239c72ae93f468529178d9076f85
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-09-16 12:30:24 +0100

    Remove unecessary import and change to pragma once
     - (instead of repeated ifdef)

--- keyboards/ymd96/keymaps/AnthonyWharton/config.h
+++ keyboards/ymd96/keymaps/AnthonyWharton/config.h
@@ -17,10 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "config_common.h"
-
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #define VENDOR_ID       0x20A0
 #define PRODUCT_ID      0x422D
@@ -64,4 +61,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* key combination for command */
 #define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
 
-#endif
