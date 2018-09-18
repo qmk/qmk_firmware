9ef3899bbbc9695c7d0bf736bc2fef838650ec90
mechmerlin <mechmerlin@gmail.com>
2018-09-17 21:47:59 -0700

    add missing routines to matrix.c of unloved_bastard

--- keyboards/bpiphany/unloved_bastard/matrix.c
+++ keyboards/bpiphany/unloved_bastard/matrix.c
@@ -25,6 +25,24 @@
 #include "util.h"
 #include "matrix.h"
 
+__attribute__ ((weak))
+void matrix_init_kb(void) {
+    matrix_init_user();
+}
+
+__attribute__ ((weak))
+void matrix_scan_kb(void) {
+    matrix_scan_user();
+}
+
+__attribute__ ((weak))
+void matrix_init_user(void) {
+}
+
+__attribute__ ((weak))
+void matrix_scan_user(void) {
+}
+
 #ifndef DEBOUNCING_DELAY
 #   define DEBOUNCING_DELAY 5
 #endif
