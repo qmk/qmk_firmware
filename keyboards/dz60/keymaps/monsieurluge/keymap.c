0e96e7fd5097b309703cf870d92fe65941201b3d
monsieurluge <monsieurluge@gmail.com>
2018-09-19 09:39:04 +0200

    revert the left-ctrl <-> fn3 change

--- keyboards/dz60/keymaps/monsieurluge/keymap.c
+++ keyboards/dz60/keymaps/monsieurluge/keymap.c
@@ -29,17 +29,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴────────┤
      * │   tab   │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │  ;  │  '  │    enter    │
      * ├─────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬─────┬─────┤
-     * │   ctrl    │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │shift│XXXXX│ up  │ fn2 │
+     * │    fn3    │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │shift│XXXXX│ up  │ fn2 │
      * ├──────┬────┴─┬───┴──┬──┴─────┴─────┼─────┴┬────┴─────┴─────┼─────┼─────┼─────┼─────┼─────┤
-     * │ fn3  │  os  │ alt  │     fn1      │ fn4  │  space/shift   │ alt │ctrl │left │down │right│
+     * │ ctrl │  os  │ alt  │     fn1      │ fn4  │  space/shift   │ alt │ctrl │left │down │right│
      * └──────┴──────┴──────┴──────────────┴──────┴────────────────┴─────┴─────┴─────┴─────┴─────┘
     */
     LAYOUT(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_SLSH, KC_DEL, KC_BSPC,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
         KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LCTL, XXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT, KC_UP, MO(_MED),
-        MO(_NUM), KC_LGUI, KC_LALT, MO(_SYM), MO(_ARR), SP_SHSP, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        MO(_NUM), XXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT, KC_UP, MO(_MED),
+        KC_LCTL, KC_LGUI, KC_LALT, MO(_SYM), MO(_ARR), SP_SHSP, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* custom layout
@@ -50,17 +50,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴────────┤
      * │   tab   │  A  │  U  │  I  │  E  │  O  │  L  │  T  │  S  │  R  │  N  │  '  │    enter    │
      * ├─────────┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬───┴─┬─────┬─────┤
-     * │   ctrl    │  W  │  Y  │  G  │  ,  │  Z  │  X  │  .  │  F  │  H  │shift│XXXXX│ up  │ fn2 │
+     * │    fn3    │  W  │  Y  │  G  │  ,  │  Z  │  X  │  .  │  F  │  H  │shift│XXXXX│ up  │ fn2 │
      * ├──────┬────┴─┬───┴──┬──┴─────┴─────┼─────┴┬────┴─────┴─────┼─────┼─────┼─────┼─────┼─────┤
-     * │ fn3  │  os  │ alt  │     fn1      │ fn4  │  space/shift   │ alt │ctrl │left │down │right│
+     * │ ctrl │  os  │ alt  │     fn1      │ fn4  │  space/shift   │ alt │ctrl │left │down │right│
      * └──────┴──────┴──────┴──────────────┴──────┴────────────────┴─────┴─────┴─────┴─────┴─────┘
     */
     LAYOUT(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_SLSH, KC_DEL, KC_BSPC,
         KC_TAB, KC_B, KC_M, KC_P, KC_Q, KC_SCLN, KC_K, KC_V, KC_C, KC_D, KC_J, KC_LBRC, KC_RBRC, KC_BSLS,
         KC_TAB, KC_A, KC_U, KC_I, KC_E, KC_O, KC_L, KC_T, KC_S, KC_R, KC_N, KC_QUOT, KC_ENT,
-        KC_LCTL, XXXXXX, KC_W, KC_Y, KC_G, KC_COMM, KC_Z, KC_X, KC_DOT, KC_F, KC_H, KC_RSFT, KC_UP, MO(_MED),
-        MO(_NUM), KC_LGUI, KC_LALT, MO(_SYM), MO(_ARR), SP_SHSP, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        MO(_NUM), XXXXXX, KC_W, KC_Y, KC_G, KC_COMM, KC_Z, KC_X, KC_DOT, KC_F, KC_H, KC_RSFT, KC_UP, MO(_MED),
+        KC_LCTL, KC_LGUI, KC_LALT, MO(_SYM), MO(_ARR), SP_SHSP, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* symbols (fn1)
