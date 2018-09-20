5c5086dc540e21555b490716a9ebf16006e9e0fb
jetpactuxedo <maddene@madden.ninja>
2018-09-19 19:22:43 -0500

    RGB Inidcator example for new van pcbs

--- keyboards/thevankeyboards/minivan/keymaps/jetpacktuxedo/config.h
+++ keyboards/thevankeyboards/minivan/keymaps/jetpacktuxedo/config.h
@@ -1,7 +1,9 @@
 #ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "../../config.h"
-
-#define PERMISSIVE_HOLD
+  #define CONFIG_USER_H
+  #include "../../config.h"
+  #define PERMISSIVE_HOLD
+  #ifdef RGBLIGHT_ENABLE
+    // This is the only effect I care about for this board
+    #define RGBLIGHT_EFFECT_BREATHING
+  #endif
 #endif
