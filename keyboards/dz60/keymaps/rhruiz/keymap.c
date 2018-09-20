20d7324d0ef373c0323f850949dd99908e10de2e
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2018-09-19 23:39:18 -0300

    Add caps lock to fn+tab

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -25,7 +25,7 @@ enum rhruiz_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
-		KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,
+		KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_DEL,
 		KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
 		CTRLESC,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
 		KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_FN2,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_KEY_OVERRIDE] = LAYOUT(
 		KC_GRV,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_EJCT,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_DEL,
+		KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_DEL,
 		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PENT,
 		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
 		_______,  _______,  _______,  _______,  _______,  KC_DEL,   _______,  _______,  _______,  _______,  _______
@@ -181,6 +181,7 @@ void change_leds_to(uint16_t hue, uint8_t sat, rgblight_config_t eeprom_config) 
       rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
       for (uint8_t i = RGBLED_NUM ; i-- > 0 ; ) {
         if (i == 8 || i == 15) {
+          rgblight_sethsv_at(hue, sat, eeprom_config.val, i);
           continue;
         }
 
@@ -189,10 +190,10 @@ void change_leds_to(uint16_t hue, uint8_t sat, rgblight_config_t eeprom_config) 
     }
 
     state_changed = false;
+  } else {
+    rgblight_sethsv_at(hue, sat, eeprom_config.val, 8);
+    rgblight_sethsv_at(hue, sat, eeprom_config.val, 15);
   }
-
-  rgblight_sethsv_at(hue, sat, eeprom_config.val, 8);
-  rgblight_sethsv_at(hue, sat, eeprom_config.val, 15);
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
