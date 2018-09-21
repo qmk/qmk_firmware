0b8ed1c3cb70a047966ae7de196cdf7bf36f034c
Ben <benlewis1994@gmail.com>
2018-09-21 10:05:01 +0100

    Added pragma back

--- keyboards/v60_type_r/keymaps/followingghosts/config.h
+++ keyboards/v60_type_r/keymaps/followingghosts/config.h
@@ -13,5 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
 #define V60_POLESTAR
 #define RGBLIGHT_EFFECT_BREATHING
