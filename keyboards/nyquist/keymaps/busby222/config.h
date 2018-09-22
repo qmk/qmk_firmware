fa932e137fc5c109e3f390baeb3561c80f4503bc
busby222 <33362396+busby222@users.noreply.github.com>
2018-09-22 13:25:08 -0600

    Lowering max mouse speed (personal preference)

--- keyboards/nyquist/keymaps/busby222/config.h
+++ keyboards/nyquist/keymaps/busby222/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
 #define MOUSEKEY_TIME_TO_MAX 60
-#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_MAX_SPEED 6
 #define MOUSEKEY_WHEEL_DELAY 0
 
 #endif
