ddb30b30909822253371c083da62f4a33bd20578
listofoptions <listofoptions@gmail.com>
2018-09-23 13:15:37 -0400

    updated matrix and default layout a bit

--- keyboards/converter/HP_46010A/keymaps/default/keymap.c
+++ keyboards/converter/HP_46010A/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------------------------------------------------------------------------||---------|    |-------------------|
      * |caps|ctrl|   a|   s|   d|   f|   g|   h|   j|   k|   l|   ;|   '|   retr||xxxx|pgup|    |   4|   5|   6|   ,|
      * |------------------------------------------------------------------------------------    |-------------------|
-     * |del |lsft  |   z|   x|   c|   v|   b|   n|   m|   ,|   .|   /|rsft  |xxxx|  up|pgdn|    |   1|   2|   3| tab|
+     * |del |lsft  |   z|   x|   c|   v|   b|   n|   m|   ,|   .|   /|rsft  | app|  up|pgdn|    |   1|   2|   3| tab|
      * |-------------------------------------------------------------------------|---------|    |---------------    |
      * | gui|      |lalt|              space                    |ralt|      |left|down|rght|    |        0|   .|    |
      * `-----------------------------------------------------------------------------------'    `-------------------'
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE,     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPC,     KC_INS,  KC_HOME,   KC_PAST, KC_PSLS, KC_PPLS, KC_PMNS, \
         KC_TAB,          KC_Q,KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL,  KC_END,    KC_P7,   KC_P8,   KC_P9,   KC_PENT, \
         KC_CAPS, KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,        KC_ENT,  KC_NO,   KC_PGUP,   KC_P4,   KC_P5,   KC_P6,   KC_PCMM, \
-        KC_DEL,  KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_RSFT, KC_NO,   KC_UP,   KC_PGDN,   KC_P1,   KC_P2,   KC_P3,   KC_TAB,  \
+        KC_DEL,  KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_RSFT, KC_APP,  KC_UP,   KC_PGDN,   KC_P1,   KC_P2,   KC_P3,   KC_TAB,  \
         KC_LGUI,               KC_LALT,              KC_SPACE                    ,               KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT,        KC_P0,       KC_PDOT           \
     )
 } ;
