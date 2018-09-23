2716d888926f999f416bec56021704f9de48bb40
James Laird-Wah <james@laird-wah.net>
2018-09-23 18:27:31 +1000

    fixup: missing #ifdef EXTRAKEY_ENABLEs
    broke build on AVR with EXTRAKEY disabled

--- tmk_core/protocol/lufa/lufa.c
+++ tmk_core/protocol/lufa/lufa.c
@@ -722,6 +722,7 @@ static void send_mouse(report_mouse_t *report)
  */
 static void send_system(uint16_t data)
 {
+#ifdef EXTRAKEY_ENABLE
     uint8_t timeout = 255;
 
     if (USB_DeviceState != DEVICE_STATE_Configured)
@@ -739,6 +740,7 @@ static void send_system(uint16_t data)
 
     Endpoint_Write_Stream_LE(&r, sizeof(report_extra_t), NULL);
     Endpoint_ClearIN();
+#endif
 }
 
 /** \brief Send Consumer
@@ -747,6 +749,7 @@ static void send_system(uint16_t data)
  */
 static void send_consumer(uint16_t data)
 {
+#ifdef EXTRAKEY_ENABLE
     uint8_t timeout = 255;
     uint8_t where = where_to_send();
 
@@ -798,6 +801,7 @@ static void send_consumer(uint16_t data)
 
     Endpoint_Write_Stream_LE(&r, sizeof(report_extra_t), NULL);
     Endpoint_ClearIN();
+#endif
 }
 
 
