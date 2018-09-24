f60bbac22edfac1a759a780b3c91fe7d62e0b629
Wilba6582 <Jason.S.Wiliams@gmail.com>
2018-09-24 20:18:39 +1000

    Improved dynamic keymaps

--- keyboards/zeal60/zeal60_api.h
+++ keyboards/zeal60/zeal60_api.h
@@ -24,7 +24,7 @@ enum zeal60_command_id
 	id_set_keyboard_value,
 	id_dynamic_keymap_get_keycode,
 	id_dynamic_keymap_set_keycode,
-	id_dynamic_keymap_clear_all,
+	id_dynamic_keymap_reset,
 	id_backlight_config_set_value,
 	id_backlight_config_get_value,
 	id_backlight_config_save,
