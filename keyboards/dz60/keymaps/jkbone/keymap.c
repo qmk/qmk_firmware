ab443b3466182916377fefeaf5e9e86194f06594
noroadsleft <xxiinophobia@yahoo.com>
2018-09-27 12:23:10 -0700

    DZ60: refactor jkbone keymap to use process_record_user
    Replaced TMK action_function keycodes with QMK process_record_user equivalents.

--- keyboards/dz60/keymaps/jkbone/keymap.c
+++ keyboards/dz60/keymaps/jkbone/keymap.c
@@ -10,6 +10,13 @@ enum layers_idx {
     _FL  // Functions
 };
 
+enum custom_keycodes {
+    RGB_VAZ = SAFE_RANGE,
+    RGB_OAZ,
+    RGB_WAN,
+    RGB_000
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT_60_iso_5x1u(
     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,    KC_BSPC,
@@ -29,41 +36,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,     KC_DEL,
     ____,        ____,   DF(_BL),   DF(_ML),    ____,    ____,    ____,    ____,    ____,    ____,    ____,     ____,     ____,    ____,
     ____,         ____,    ____,    ____,    ____,    ____,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,    ____,     ____,     ____,
-    ____,       F(3),     F(2),    F(1),    F(0),    ____,    ____,    ____, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY,          ____,
+    ____,       RGB_000,  RGB_WAN, RGB_OAZ, RGB_VAZ, ____,    ____,    ____, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY,          ____,
     ____,        ____,      ____,                          ____,                             ____,    ____,    ____,    ____,    ____
   )
 };
 
-enum function_id {
-    RGB_VAZ,
-    RGB_OAZ,
-    RGB_WAN,
-    RGB_000
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-  [0]  = ACTION_FUNCTION(RGB_VAZ),
-  [1]  = ACTION_FUNCTION(RGB_OAZ),
-  [2]  = ACTION_FUNCTION(RGB_WAN),
-  [3]  = ACTION_FUNCTION(RGB_000)
-};
-
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  switch (id) {
-    case RGB_VAZ:
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch(keycode) {
+      case RGB_VAZ:
         rgblight_enable();
         rgblight_setrgb(0, 204, 33);
         break;
-    case RGB_OAZ:
+      case RGB_OAZ:
         rgblight_enable();
         rgblight_setrgb(229, 65, 0);
         break;
-    case RGB_WAN:
+      case RGB_WAN:
         rgblight_enable();
         rgblight_setrgb(255, 255, 255);
         break;
-    case RGB_000:
+      case RGB_000:
         rgblight_disable();
         break;
+    }
   }
-}
+  return true;
+};
