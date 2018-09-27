03de3ce0560ab00d675815e154c0b4f28cbae921
Shay Merrill <shaymdev@gmail.com>
2018-09-26 22:15:45 -0600

    remove rgb on lower layer

--- keyboards/orthodox/keymaps/shaymdev/keymap.c
+++ keyboards/orthodox/keymaps/shaymdev/keymap.c
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = KEYMAP( \
   _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR,                                                                _______, KC_P7, KC_P8, KC_P9, KC_PMNS, _______, \
-  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,	_______, XXXXXXX, _______,          _______, XXXXXXX, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, RGB_VAI, \
-  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV,	_______, _______, _______,          _______, _______, KC_KP_0, _______, KC_P1, KC_P2, KC_P3, KC_PENT, RGB_VAD \
+  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR, _______, XXXXXXX, _______,          _______, XXXXXXX, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, _______, \
+  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV,	_______, _______, _______,          _______, _______, KC_KP_0, _______, KC_P1, KC_P2, KC_P3, KC_PENT, _______ \
 ),
 
 [_RAISE] = KEYMAP( \
