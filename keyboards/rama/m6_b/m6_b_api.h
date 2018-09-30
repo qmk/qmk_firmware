0b456ba7e78c4cd49b611d16732d95208a82b3a4
Wilba6582 <Wilba6582@users.noreply.github.com>
2018-10-01 01:35:10 +1000

    RAMA M6-B and IS31FL3218 driver (#4021)
    * Initial RAMA M6-B commit.
    
    * Moved IS31FL3218 driver, minor cleanups
    
    * Refactor, added dynamic keymap.
    
    * Added dynamic keymaps to RAMA M6-A
    
    * Refactor M6-A and M6-B to use common code.
    
    * Formatting
    
    * Cleanup
    
    * Cleanup
    
    * Changes from review

--- keyboards/rama/m6_b/m6_b_api.h
+++ keyboards/rama/m6_b/m6_b_api.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 Wilba
+/* Copyright 2017 Jason Williams (Wilba)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,12 +13,27 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
 
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
+#define PROTOCOL_VERSION 0x0001
 
-#include "../../config.h"
+enum m6_b_command_id
+{
+	id_get_protocol_version = 0x01, // always 0x01
+	id_get_keyboard_value,
+	id_set_keyboard_value,
+	id_dynamic_keymap_get_keycode,
+	id_dynamic_keymap_set_keycode,
+	id_dynamic_keymap_reset,
+	id_backlight_config_set_value,
+	id_backlight_config_get_value,
+	id_backlight_config_save,
+	id_eeprom_reset,
+	id_bootloader_jump,
+	id_unhandled = 0xFF,
+};
 
-// place overrides here
-
-#endif
+enum m6_b_keyboard_value_id
+{
+	id_uptime = 0x01
+};
