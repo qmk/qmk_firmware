c011e60673616f6ed8daf46982f3b11ef97f2179
Konstantin Đorđević <konstantin.djordjevic@tradecore.com>
2018-10-01 19:22:00 +0200

    Swap Fn and RCtrl

--- keyboards/whitefox/keymaps/konstantin/keymap.c
+++ keyboards/whitefox/keymaps/konstantin/keymap.c
@@ -2,8 +2,8 @@
 
 #define FN      MO(L_FN)
 #define FN_CAPS LT(L_FN, KC_CAPS)
-#define FN_RCTL TD(TD_FN_RCTL) // Unused
-#define RLALT   TD(TD_RLALT)   // Unused
+#define FN_RCTL TD(TD_FN_RCTL)    // Unused
+#define RLALT   TD(TD_RLALT)      // Unused
 
 #define TOP     LCTL(KC_HOME)
 #define BOTTOM  LCTL(KC_END)
@@ -89,17 +89,17 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |---------------------------------------------------------------|
    * |FnCaps| A | S | D | F | G | H | J | K | L | ; | ' | Enter  |PgU|
    * |---------------------------------------------------------------|
-   * | LShift | Z | X | C | V | B | N | M | , | . | / |  Fn  | ↑ |PgD|
+   * | LShift | Z | X | C | V | B | N | M | , | . | / |RCtrl | ↑ |PgD|
    * |---------------------------------------------------------------|
-   * |LCtl|LGui|LAlt|         Space          |RAlt|RCtl| | ← | ↓ | → |
+   * |LCtl|LGui|LAlt|         Space          |RAlt| Fn | | ← | ↓ | → |
    * `---------------------------------------------------------------'
    */
   [L_BASE] = LAYOUT_truefox( \
     KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV, KC_PSCR, \
     KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,        KC_DEL,  \
     FN_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,        KC_ENT,         KC_PGUP, \
-    KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,FN,             KC_UP,  KC_PGDN, \
-    KC_LCTL,KC_LGUI,KC_LALT,                        KC_SPC,                 KC_RALT,KC_RCTL,                KC_LEFT,KC_DOWN,KC_RGHT  \
+    KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RCTL,        KC_UP,  KC_PGDN, \
+    KC_LCTL,KC_LGUI,KC_LALT,                        KC_SPC,                 KC_RALT,FN,                     KC_LEFT,KC_DOWN,KC_RGHT  \
   ),
 
   /* Function layer
@@ -110,17 +110,17 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |---------------------------------------------------------------|
    * |      |M← |M↓ |M→ |MW↑|   |   |   |   |   |   |   |        |Top|
    * |---------------------------------------------------------------|
-   * |        |MA0|MA2|MW←|MW→|   |   |   |VoD|VoU|Mut|      |PgU|Btm|
+   * |        |MA0|MA2|MW←|MW→|   |   |   |VoD|VoU|Mut| App  |PgU|Btm|
    * |---------------------------------------------------------------|
-   * |    |    |    |          MW↓           |    |Menu| |Hom|PgD|End|
+   * |    |    |    |          MW↓           |    |    | |Hom|PgD|End|
    * `---------------------------------------------------------------'
    */
   [L_FN] = LAYOUT_truefox( \
     _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, NUMPAD, KC_SLCK,KC_PAUS, \
     KC_BTN4,KC_BTN2,KC_MS_U,KC_BTN1,KC_BTN3,KC_BTN5,_______,_______,_______,KC_MSTP,KC_MPLY,KC_MPRV,KC_MNXT,CLEAR,          KC_INS,  \
     _______,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_U,_______,_______,_______,_______,_______,_______,_______,        _______,        TOP,     \
-    _______,        KC_ACL0,KC_ACL2,KC_WH_L,KC_WH_R,_______,_______,_______,KC_VOLD,KC_VOLU,KC_MUTE,_______,        KC_PGUP,BOTTOM,  \
-    _______,_______,_______,                        KC_WH_D,                _______,KC_APP,                 KC_HOME,KC_PGDN,KC_END   \
+    _______,        KC_ACL0,KC_ACL2,KC_WH_L,KC_WH_R,_______,_______,_______,KC_VOLD,KC_VOLU,KC_MUTE,KC_APP,         KC_PGUP,BOTTOM,  \
+    _______,_______,_______,                        KC_WH_D,                _______,_______,                KC_HOME,KC_PGDN,KC_END   \
   ),
 
   /* Numpad layer
