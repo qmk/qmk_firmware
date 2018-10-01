f648743b1ea4eeb22b793c4d44a616e35e6e3e8e
noroadsleft <xxiinophobia@yahoo.com>
2018-10-01 12:41:04 -0700

    DZ60: refactor LAYOUT_60_iso_5x1u matrix
    Updated matrix so `KC_ENT` belongs on the home row, which is consistent with LAYOUT_60_iso for the DZ60 as well as the ISO community layouts.

--- keyboards/dz60/dz60.h
+++ keyboards/dz60/dz60.h
@@ -212,8 +212,8 @@
 */
 #define LAYOUT_60_iso_5x1u( \
 	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,      k0e, \
-	k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k2d, \
-	k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k1e,      \
+	k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,      \
+	k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k1e, k2d, \
 	k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b,      k3d,      \
 	k40, k41,      k43,           k46,                k4a, k4b, k4c, k4d, k4e  \
 ) { \
