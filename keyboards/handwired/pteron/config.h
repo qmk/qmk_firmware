8d3a6fcac71c50377a51ffe1fdde3f4eedd2e390
Filip Sund <filip.sund@gmail.com>
2018-10-02 12:35:57 +0200

    Made some progress on Pteron keymap.

--- keyboards/handwired/pteron/config.h
+++ keyboards/handwired/pteron/config.h
@@ -7,9 +7,9 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+// #define MANUFACTURER    qmkbuilder
+#define PRODUCT         Pteron Keyboard
+#define DESCRIPTION     Pteron Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
@@ -23,20 +23,8 @@
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
 
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 0
-#endif
-
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+#define DEBOUNCING_DELAY 5  // 5 is default
 
 /* key combination for command */
 #define IS_COMMAND() ( \
@@ -45,14 +33,5 @@
 
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
-
-
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
 
 #endif
--- keyboards/handwired/pteron/rules.mk
+++ keyboards/handwired/pteron/rules.mk
@@ -44,7 +44,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE ?= yes	# Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE ?= no	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
 CONSOLE_ENABLE ?= no	# Console for debug(+400)
@@ -52,5 +52,6 @@ COMMAND_ENABLE ?= no    # Commands for debug and configuration
 SLEEP_LED_ENABLE ?= no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
-AUDIO_ENABLE ?= no
 RGBLIGHT_ENABLE ?= no
+UNICODE_ENABLE ?= no
+UNICODEMAP_ENABLE ?= no
