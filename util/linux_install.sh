2d671a7e1a75bb9d03a40c0e440f897c7e52a15e
Chris Johnston <chris@thejohnstons.net>
2018-10-02 22:16:49 -0700

    Enable support for Linux Mint 18 with linux_install.sh (#4042)
    Enable support for ubuntu-derived linux-es in installer
    
    Checks /etc/os-release for mention of ubuntu and debian.
    
    Linux Mint uses ID_LIKE=ubuntu, not ID_LIKE=debian (as seen in
    Ubuntu 16.04, unsure about others)

--- util/linux_install.sh
+++ util/linux_install.sh
@@ -4,7 +4,7 @@ if grep ID /etc/os-release | grep -qE "rhel|fedora"; then
 	sudo dnf install gcc unzip wget zip dfu-util dfu-programmer avr-gcc \
 	    avr-libc binutils-avr32-linux-gnu arm-none-eabi-gcc-cs \
 	    arm-none-eabi-binutils-cs arm-none-eabi-newlib
-elif grep ID /etc/os-release | grep -q debian; then
+elif grep ID /etc/os-release | grep -qE 'debian|ubuntu'; then
 	sudo apt-get update
 	sudo apt-get install gcc unzip wget zip gcc-avr binutils-avr avr-libc \
 	    dfu-programmer dfu-util gcc-arm-none-eabi binutils-arm-none-eabi \
@@ -38,5 +38,5 @@ fi
 else
 	echo "Sorry, we don't recognize your OS. Help us by contributing support!"
 	echo
-	echo "https://docs.qmk.fm/contributing.html"
+	echo "https://docs.qmk.fm/#/contributing"
 fi
