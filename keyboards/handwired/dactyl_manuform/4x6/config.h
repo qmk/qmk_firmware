d55468c18d666870cfe095d32153e5e503e50649
Filip Sund <filip.sund@gmail.com>
2018-10-03 21:04:01 +0200

    Keyboard: Added dactyl-manuform 4x6 and keymap. (#4058)
    * Added dactyl-manuform 4x6 and keymap.
    
    * Did changes recommended by drashna.

--- keyboards/handwired/dactyl_manuform/4x6/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/config.h
@@ -1,6 +1,4 @@
 /*
-This is the c configuration file for the keymap
-
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
 
@@ -20,15 +18,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "../../config.h"
+#include "config_common.h"
 
-/* Use I2C or Serial, not both */
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x3060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    tshort
+#define DESCRIPTION     A split keyboard for the cheap makers
 
-#define USE_SERIAL
-// #define USE_I2C
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 6
 
-/* Select hand configuration */
+// row-driven
+#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
 
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
-#define EE_HANDS
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
