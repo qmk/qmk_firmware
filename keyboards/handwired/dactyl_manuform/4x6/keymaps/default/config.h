02a5e26721d1c227f3ae4595ee03c51bf8d9afdf
Filip Sund <filip.sund@gmail.com>
2018-10-03 21:04:01 +0200

    Keyboard: Added dactyl-manuform 4x6 and keymap. (#4058)
    * Added dactyl-manuform 4x6 and keymap.
    
    * Did changes recommended by drashna.

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/config.h
@@ -20,10 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "../../config.h"
-
 /* Use I2C or Serial, not both */
-
 #define USE_SERIAL
 // #define USE_I2C
 
