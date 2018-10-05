f1003aaccfdd4210103e796206e6ac40285dab5b
Takeshi ISHII <2170248+mtei@users.noreply.github.com>
2018-10-06 06:51:34 +0900

    Keyboard: Helix five_rows keymap reduced size (#4082)
    * add PERMISSIVE_HOLD in helix:five_rows/config.h
    
    * add Eucalyn char layout into helix:five_rows
    
    * Helix five_rows keymap: make rgblight modes selectable.
    
    No change in build result.
    
    * fix keymap.c map comment, add console compile option
    
    No change in build result.
    
    * Helix five_rows keymap: reduced the size.

--- keyboards/helix/config.h
+++ keyboards/helix/config.h
@@ -48,17 +48,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define DISABLE_LEADER
 #endif // USE_Link_Time_Optimization
 
-#if defined(LED_ANIMATIONS) || defined(RGBLIGHT_ANIMATIONS)
-   #undef RGBLIGHT_ANIMATIONS
-   #define RGBLIGHT_EFFECT_BREATHING
-   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-   #define RGBLIGHT_EFFECT_SNAKE
-   #define RGBLIGHT_EFFECT_KNIGHT
-   #define RGBLIGHT_EFFECT_CHRISTMAS
-   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-   #define RGBLIGHT_EFFECT_RGB_TEST
-   #define RGBLIGHT_EFFECT_ALTERNATING
-#endif
-
 #endif /* CONFIG_H */
