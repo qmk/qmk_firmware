38949797524cf6b260f6d7738b333f6a668d190d
Grady Sigler <gradysigler@gmail.com>
2018-10-05 07:44:58 -0600

    tweaks

--- keyboards/iris/keymaps/gsigler/keymap.c
+++ keyboards/iris/keymaps/gsigler/keymap.c
@@ -36,18 +36,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|  TAB   |   Q    |   W    |   E    |    R   |    T   |                          |    Y   |   U    |   I    |   O    |   P    |   +/=  |
      KC_TAB,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-  //|  {/[   |   A    |   S    |   D    |    F   |    G   |                          |    H   |   J    |   K    |   L    |   :/;  |   "/'  |
-     KC_LBRC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  //| RAISE  |   A    |   S    |   D    |    F   |    G   |                          |    H   |   J    |   K    |   L    |   :/;  |   "/'  |
+      RAISE,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-  //|  }/]   |   Z    |   X    |   C    |    V   |    B     LOWER              RAISE |    N   |   M    |   </,  |  >/.   |  ?-/   |   |-\  | 
-     KC_RBRC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,             RAISE,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+  //|  LOWER |   Z    |   X    |   C    |    V   |    B     {/[                }/] |    N   |   M    |   </,  |  >/.   |  ?-/   |   |-\  | 
+      LOWER,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,           KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-  //                               |  CTRL  | COMMAND| SPACE                    | BKSPACE|  ENTER |   ALT 
-                                    KC_LCTL, KC_LGUI,  KC_SPC,                   KC_BSPC,  KC_ENT,  KC_LALT
+  //                               |  CTRL  | COMMAND| BKSPACE                    | SPACE|  ENTER |   ALT 
+                                    KC_LCTL, KC_LGUI,  KC_BSPC,                   KC_SPC,  KC_ENT,  KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LOWER] = LAYOUT(
+// KC_LBRC
+// KC_RBRC
+
+  [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
   //|        |   F1   |   F2   |   F3   |    F4  |   F5   |                          |   F6   |   F7   |   F8   |   F9   |   F10  |   F11  |
      KC_TRNS,  KC_F1,    KC_F2,  KC_F3,   KC_F4,   KC_F5,                              KC_F6,    KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,
@@ -55,32 +58,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|        |    !   |    @   |   {    |   }    |    |   |                          |        |        |   ↑    |        |        |   F12  |
      KC_TRNS, KC_EXLM,   KC_AT,  KC_LCBR, KC_RCBR, KC_PIPE,                           XXXXXXX, XXXXXXX,  KC_UP,  XXXXXXX, XXXXXXX, KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-  //|        |    #   |    $   |   (    |   )    |    `   |                          |        |    ←   |   ↓    |    →   |        |        | 
-     XXXXXXX,  KC_HASH, KC_DLR, KC_LPRN, KC_RPRN,   KC_GRV,                            XXXXXXX, KC_LEFT,KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+  //|  RAISE |    #   |    $   |   (    |   )    |    `   |                          |        |    ←   |   ↓    |    →   |        |        | 
+      TRNS,  KC_HASH, KC_DLR, KC_LPRN, KC_RPRN,   KC_GRV,                            XXXXXXX, KC_LEFT,KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-  //|    &   |    %   |    ^   |   [    |   ]    |    ~   |                          |        |  MUTE  | VOLUP  | VOLDOWN|        |        |    
-     KC_AMPR, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   TRNS,           XXXXXXX, XXXXXXX, KC__MUTE, KC__VOLUP, KC__VOLDOWN, XXXXXXX, XXXXXXX,
+  //|        |    %   |    ^   |   [    |   ]    |    ~   |    &                     |        |  MUTE  | VOLUP  | VOLDOWN|        |        |    
+     XXXXXXX, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,   KC_AMPR,        XXXXXXX, XXXXXXX, KC__MUTE, KC__VOLUP, KC__VOLDOWN, XXXXXXX, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-  //                               |  CTRL  | COMMAND| SPACE                    | BKSPACE|  ENTER |   ALT 
+  //                               |  CTRL  | COMMAND| BKSPACE                  | SPACE|  ENTER |   ALT 
                                     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,  KC_TRNS, KC_TRNS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_RAISE] = LAYOUT(
+  [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
   //|        |        |        |        |        |        |                          |        |   7    |    8   |    9   |    *   |    /   | 
-     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX,   KC_P7,   KC_P8,   KC_P9,  KC_PAST, KC_PSLS,
+     KC_ASUP, KC_ASON, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX,   KC_P7,   KC_P8,   KC_P9,  KC_PAST, KC_PSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
   //|        |        |        |        |        |        |                          |        |    4   |    5   |    6   |    +   |    -    | 
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                            XXXXXXX,  KC_P4,   KC_P5,   KC_P6,  KC_PPLS, KC_PMNS,
+     KC_ASDN, KC_ASOFF, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                            XXXXXXX,  KC_P4,   KC_P5,   KC_P6,  KC_PPLS, KC_PMNS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
   //|        |        |        |        |        |        |                          |        |    1   |    2   |    3   |        |        | 
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                             XXXXXXX,  KC_P1,   KC_P2,   KC_P3,  XXXXXXX, XXXXXXX,
+     KC_ASRP, KC_ASTG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                             XXXXXXX,  KC_P1,   KC_P2,   KC_P3,  XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-  //|        |        |        |        |        |        |                          |        |   0    |    .   | ENTER  |   =    |        | 
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            TRNS,  XXXXXXX, KC_P0,   KC_PDOT, KC_PENT, KC_PEQL, XXXXXXX,
+  //| LOWER  |        |        |        |        |        |                          |        |   0    |    .   | ENTER  |   =    |        | 
+       TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX,  XXXXXXX, KC_P0,   KC_PDOT, KC_PENT, KC_PEQL, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-  //                               |  CTRL  | COMMAND| SPACE                    | BKSPACE|  ENTER |   ALT 
+  //                               |  CTRL  | COMMAND| BKSPACE                  | SPACE|  ENTER |   ALT 
                                     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
