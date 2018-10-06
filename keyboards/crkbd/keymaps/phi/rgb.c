f26b2254147e160b7e1b50d511cbb9ee8df4e989
p <_>
2018-10-07 04:00:56 +0900

    Add utility fns and improve gradient

--- keyboards/crkbd/keymaps/phi/rgb.c
+++ keyboards/crkbd/keymaps/phi/rgb.c
@@ -1,15 +1,29 @@
-void rgb_render_gradient (uint16_t h, uint8_t s, uint8_t v, uint16_t h2, uint8_t s2, uint8_t v2) {
-    rgblight_sethsv_at(h, s, v, 0);
-    rgblight_sethsv_at((h * 3 + h2) / 4, (s * 3 + s2) / 4, (v * 3 + v2) / 4, 1);
-    rgblight_sethsv_at((h + h2) / 2, (s + s2) / 2, (v + v2) / 2, 2);
-    rgblight_sethsv_at((h + h2 * 3) / 4, (s + s2 * 3) / 4, (v + v2 * 3) / 4, 3);
-    rgblight_sethsv_at(h2, s2, v2, 4);
+typedef struct rgb_color { uint8_t r; uint8_t g; uint8_t b; } rgb_color_t;
+typedef rgb_color_t rgb_image_t[RGBLED_NUM];
+
+void rgb_send_image (rgb_image_t *image) {
+    for (int i = 0; i < RGBLED_NUM; i++) {
+        rgblight_setrgb_at(image[i]->r, image[i]->g, image[i]->b, i);
+    }
+}
+
+void rgb_render_gradient (rgb_image_t *image, uint16_t h, uint8_t s, uint16_t h2, uint8_t s2) {
+    LED_TYPE from, to;
+    sethsv(h, s, 255, &from);
+    sethsv(h2, s2, 255, &to);
+    for (int i = 0; i < RGBLED_NUM; i++) {
+        image[i]->r = (to.r * i + from.r * (RGBLED_NUM - 1 - i)) / (RGBLED_NUM - 1);
+        image[i]->g = (to.g * i + from.g * (RGBLED_NUM - 1 - i)) / (RGBLED_NUM - 1);
+        image[i]->b = (to.b * i + from.b * (RGBLED_NUM - 1 - i)) / (RGBLED_NUM - 1);
+    }
 }
 
 void update_rgblight (void) {
     static bool rgb_initialized = false;
+    static rgb_image_t image;
     if (!rgb_initialized) {
-        rgb_render_gradient(341, 201, 223, 285, 145, 135);
+        rgb_render_gradient(&image, 250, 135, 1, 130);
+        rgb_send_image(&image);
         rgb_initialized = true;
     }
 }
