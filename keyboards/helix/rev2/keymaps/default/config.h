289e93c0b17eee75041c505b59dea7ba4b0e3076
mtei <2170248+mtei@users.noreply.github.com>
2018-10-07 01:56:22 +0900

    Deselect RGB_TEST and ALTERNATING in Helix rev2,pico keymaps config.h.

--- keyboards/helix/rev2/keymaps/default/config.h
+++ keyboards/helix/rev2/keymaps/default/config.h
@@ -34,8 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
    #define RGBLIGHT_EFFECT_KNIGHT
    #define RGBLIGHT_EFFECT_CHRISTMAS
    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-   #define RGBLIGHT_EFFECT_RGB_TEST
-   #define RGBLIGHT_EFFECT_ALTERNATING
+   //#define RGBLIGHT_EFFECT_RGB_TEST
+   //#define RGBLIGHT_EFFECT_ALTERNATING
 #endif
 
 #endif /* CONFIG_USER_H */
