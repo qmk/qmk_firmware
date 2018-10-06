60f5ce8890453751a00ab63b150dded25c39db42
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-10-06 16:49:04 +0200

    Keymap: Update keyboards/planck/keymaps/vifon (#4084)
    * Implement macro recording
    
    * Remove off-by-one from the macro recorder
    
    * Holding both modifiers enables the third layer
    
    * Exit the 3rd layer into one of the secondary ones
    
    * Regular Enter and Escape on the secondary layers
    
    Useful for the macro recording because the "tap" modifiers always act as
    modifiers there.
    
    * Remove the buffer overflow from the macro recorder
    
    When the limit is reached, the backlight blinks with each keypress.
    
    * More robust macro recording implementation
    
    * Macro record refactoring and improvement
    
    - support for a second macro (saved using the end of the macro buffer)
    - improved reliability
    
    * Add comments to my macros
    
    * Macro recording: remove a redundant code
    
    * Separate the raise+lower layer from the special layer
    
    * Add back/forward keys
    
    * Bind mouse movement
    
    * More readable KC_TRNS
    
    * Improve the mouse bindings
    
    * Use a convenience macro instead of the low-level ACTION macro
    
    * Add mouse to the Dual layer; make most of the layer transparent
    
    * New "mouse" layer; clean up the "special" layer
    
    * CapsLock, NumLock and ScrollLock on the Special layer
    
    * Enable my fix for the stuck modifiers and adjust the dual layer to it
    
    * Fix the macro recording after enabling the stuck mods fix
    
    * Click with Lower/Raise+Space
    
    * Reduce the memory used by the macro recording
    
    Previously the auxiliary layers were numbered 30 and 31 which resulted
    in all 32 array elements being allocated regardless of the actually
    used layers in the user code.
    
    * Optimize source_layers_cache for the cache memory
    
    Swapping the array indices should increase the locality of the memory access.
    
    * Remove the mouse layer
    
    * Use update_tri_layer to simplify the code
    
    * Add some custom per-keymap build options
    
    * Add ShiftLock
    
    * Secure the reset button
    
    * Add numeric layer
    
    * Remove explicit layer keys on LW and RS layers
    
    * Replace Alt+left/right with Back/Forward keys
    
    * Sleep/delay key (for macro recording)
    
    * App/menu key
    
    * Fix stuff after merging to master
    
    * Local per-keymap config.h; minor cleanup
    
    * Remove the old macro_record.h
    
    Replaced with dynamic_macro.h
    
    * Adjust my keymap to the current API
    
    * Pure Pro Gaming layer
    
    Accessible by double tapping the Pure Pro layer. It features switched
    Lower and Raise layers, for one-handed access to numbers.
    
    * Pure Pro Gaming layer: no Mod4, persistent number layer under RShift
    
    * Pure Pro Gaming layer activated with long press, not double press
    
    * Reduce the size of the vifon keymap
    
    See: issue #930
    
    * Increase the dynamic macro buffer
    
    The default was actually decreased. The actual used value didn't change.
    
    * Add the context menu key
    
    * No more strange ideas in the gaming layer
    
    * Reduce the firmware size by disabling API_SYSEX_ENABLE
    
    * Make slash available on the PP layer (replaces the RESET key)
    
    * Add a generic "hold this key" macro
    
    * Add a missing undef
    
    * Turn LGUI in the gaming layer into LW instead of disabling it
    
    It's not as disruptive as LGUI and it makes the LW/RS order kind of
    more consistent (LW on the left, RS on the right).
    
    * Refactor the Pure Pro Gaming layer
    
    * Try out the 1st party qmk KC_LOCK (vs my own KM_HOLD)
    
    * Alternative way to switch keyboard layouts with the left hand
    
    * Add dot & slash to the Pure Pro layer (but not to the Gaming layer)
    
    * Now that there is a second KM_LW, we can simplify the layout a bit
    
    It was confusing to have the modifiers completely swapped.  Just
    moving them is much more natural.
    
    * Disable the console now that qmk got bigger
    
    * Update the keymap for the modern qmk standards

--- keyboards/planck/keymaps/vifon/rules.mk
+++ keyboards/planck/keymaps/vifon/rules.mk
@@ -16,6 +16,8 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+API_SYSEX_ENABLE = no       # https://www.reddit.com/r/olkb/comments/5swhij/_/ddie6zq/
+KEY_LOCK_ENABLE = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
