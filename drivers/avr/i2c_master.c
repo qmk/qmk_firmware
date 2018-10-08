c84fe3c8a0468a5a4296128469b2f282777747fb
yiancar <yiangosyiangou@cytanet.com.cy>
2018-10-08 22:55:56 +0100

    Update drivers/avr/i2c_master.c

--- drivers/avr/i2c_master.c
+++ drivers/avr/i2c_master.c
@@ -7,10 +7,9 @@
 
 #include "i2c_master.h"
 #include "timer.h"
-#include "config.h"
 
 #ifndef F_SCL
-#define F_SCL 300000UL // SCL frequency
+#define F_SCL 400000UL // SCL frequency
 #endif
 #define Prescaler 1
 #define TWBR_val ((((F_CPU / F_SCL) / Prescaler) - 16 ) / 2)
