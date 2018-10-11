75f362f99648e479d9cd74f2e215360dbf4c9361
wanleg <wanleg@github.com>
2018-10-10 17:57:00 -0700

    keymap change

--- layouts/community/ortho_5x15/wanleg/keymap.c
+++ layouts/community/ortho_5x15/wanleg/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [GK50] = LAYOUT_ortho_5x10_wrapper(
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+  KC_ESC,    _______, _______, _______, _______, _______, _______, _______, _______,    KC_BSPC,
   _______________Gherkin_Row_0_______________,
   _______________Gherkin_Row_1_______________,
   _______________Gherkin_Row_2_______________,
