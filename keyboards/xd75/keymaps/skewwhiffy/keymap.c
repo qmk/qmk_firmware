d7428ed9375ae6575346208398931a69ebf6c821
Kenny Hung <kennysubs+github@outlook.com>
2018-10-12 19:54:37 +0100

    Some review markups

--- keyboards/xd75/keymaps/skewwhiffy/keymap.c
+++ keyboards/xd75/keymaps/skewwhiffy/keymap.c
@@ -13,10 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "xd75.h"
+#include QMK_KEYBOARD_H
 #include "keymap_uk.h"
-
-#define _______ KC_TRNS
 
 // Layer shorthand
 #define _CM 0 // Colemak
