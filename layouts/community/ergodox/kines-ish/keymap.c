dd5a88e1323a0c18a5ef7076a6683a8bbd05e72e
Andre Cato <andrecato@gmail.com>
2018-10-13 21:05:49 -0400

    add tarmak4

--- layouts/community/ergodox/kines-ish/keymap.c
+++ layouts/community/ergodox/kines-ish/keymap.c
@@ -259,7 +259,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_TARMAK] = LAYOUT_ergodox(  // layer 4 : colemak
         // left hand
         KC_EQL,            KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  LALT(KC_LEFT), 
-        KC_TAB,            KC_Q,    KC_W,    KC_F,     KC_J,     KC_G,  TG(_SYMB),
+        KC_TAB,            KC_Q,    KC_W,    KC_F,     KC_P,     KC_G,  TG(_SYMB),
         TD(TD_ESC_CAPS),   KC_A,    KC_R,    KC_S,     KC_T,     KC_D,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  KC_FN3,
         KC_FN1,            KC_GRV,  KC_INS,  KC_LEFT,  KC_RGHT,
@@ -268,8 +268,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_BSPC,KC_DEL,KC_END,
         // right hand
              LALT(KC_RGHT), KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
-             TG(_MDIA_MOUSE),      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
-                            KC_H,   KC_N,   KC_E,   KC_L,   KC_SCLN,  KC_QUOT,
+             TG(_MDIA_MOUSE),      KC_J,   KC_U,   KC_I,   KC_Y,   KC_SCLN,     KC_BSLS,
+                            KC_H,   KC_N,   KC_E,   KC_L,   KC_O,     KC_QUOT,
              KC_TRNS,       KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
                                     KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,  KC_FN2,
              KC_LGUI, KC_RCTL,
