4bad9d8543c51500bce9c7e03068173e8cce91ad
CarReq <c94rivera@gmail.com>
2018-10-14 22:14:23 -0400

    fixed fn layer so fn and letters can be pressed simultaneously for MC

--- keyboards/planck/keymaps/carreq/keymap.c
+++ keyboards/planck/keymaps/carreq/keymap.c
@@ -171,9 +171,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_FN] = LAYOUT_planck_grid(
     LALT(KC_BSPC),  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-    XXXXXXX,        KC_F11,  KC_F12,  XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,        _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_SPC,   KC_SPC,   XXXXXXX, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    XXXXXXX,        KC_F11,  KC_F12,  _______, _______,  _______,  _______,  _______, _______, _______, _______, _______,
+    _______,        _______, _______, _______, _______,  _______,  _______,  _______, _______, _______, _______, _______,
+    _______,        _______, _______, _______, _______,  KC_SPC,   KC_SPC,   _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Plover layer (http://opensteno.org)
