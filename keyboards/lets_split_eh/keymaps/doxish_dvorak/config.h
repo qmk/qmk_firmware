2587024137210901c11858c8705cc302cf340a23
ROMUSKING <38523153+ROMUSKING@users.noreply.github.com>
2018-10-16 23:29:52 +0100

    code cleanup
    Changes requested by Drashna + Tap Dance enabled

--- keyboards/lets_split_eh/keymaps/doxish_dvorak/config.h
+++ keyboards/lets_split_eh/keymaps/doxish_dvorak/config.h
@@ -18,9 +18,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "../../config.h"
-
-#endif
+#pragma once
