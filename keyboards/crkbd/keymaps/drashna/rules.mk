76b5ac621d3f5f5e809de2bc9479d54b99a2f89f
Drashna Jaelre <drashna@live.com>
2018-10-16 20:33:55 -0700

    Update to Corne Keyboard (crkbd) keymap

--- keyboards/crkbd/keymaps/drashna/rules.mk
+++ keyboards/crkbd/keymaps/drashna/rules.mk
@@ -26,6 +26,6 @@ SRC +=  ./lib/glcdfont.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
+        ./lib/host_led_state_reader.c \
         # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
