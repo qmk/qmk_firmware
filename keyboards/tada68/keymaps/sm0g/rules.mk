5c2332a082003f4d396903ac042888ec288c4446
sm0g <sm0g@protonmail.com>
2018-10-18 16:13:00 -0400

    Removed unneeded code and the sm0gunix layout.

--- keyboards/tada68/keymaps/sm0g/rules.mk
+++ keyboards/tada68/keymaps/sm0g/rules.mk
@@ -15,7 +15,4 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = yes       # Breathing sleep LED during USB suspend
-TAP_DANCE_ENABLE = yes      # Tappa Dance Bitch
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
+TAP_DANCE_ENABLE = yes      # Tappa Dance
