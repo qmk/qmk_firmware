ce11a0a4b51a5de3b95ae5b5e8763fdf22388442
fpazos <30479687+fpazos@users.noreply.github.com>
2018-10-18 15:20:04 +0200

    Revert last commit
    I moddified by mistake a file from another keyboard

--- keyboards/handwired/bluepill/bluepill70/bluepill70.h
+++ keyboards/handwired/bluepill/bluepill70/bluepill70.h
@@ -1,6 +1,4 @@
-#ifndef BLUEPILL70_H
-#define BLUEPILL70_H
+#pragma once
 #include "bluepill.h"
 //  Modified by Xydane
 //	#define LAYOUT(k00) {{ k00 }}
-#endif
--- keyboards/handwired/bluepill/config.h
+++ keyboards/handwired/bluepill/config.h
@@ -15,8 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
@@ -57,5 +56,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_ONESHOT
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
-
-#endif
