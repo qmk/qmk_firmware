397066188a5b78e77805328a43bd29349efc66bc
Brian Arnold <barnold@redlobster.com>
2018-10-18 00:13:15 -0400

    removed non-relavent keymaps.

--- flash.sh
+++
@@ -1,5 +0,0 @@
-echo "Sleeping for 5 seconds for keyboard to enter bootloader..."
-sleep 5
-dfu-programmer atmega32u4 erase
-dfu-programmer atmega32u4 flash dz60_marianas.hex
-dfu-programmer atmega32u4 reset
