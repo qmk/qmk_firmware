2f932b832830f7ca78f1e993f98b3e45be989b80
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-10-20 13:06:03 -0500

    Update keyboards/iris/keymaps/vyolle_steno/rules.mk
    Co-Authored-By: Vyolle <dchamberlin.ax@gmail.com>

--- keyboards/iris/keymaps/vyolle_steno/rules.mk
+++ keyboards/iris/keymaps/vyolle_steno/rules.mk
@@ -1,5 +1,5 @@
 EXTRAKEY_ENABLE = yes       # Disable if you run out of usb endpoints. Media keys need this to work.
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700) Uses extra usb enpoints. 
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700) Uses extra usb endpoints. 
 STENO_ENABLE = yes          # Additional protocols for Stenography(+1700), requires VIRTSER.
 AUDIO_ENABLE = no           # Audio output on port C6
 MIDI_ENABLE = no            # MIDI controls
