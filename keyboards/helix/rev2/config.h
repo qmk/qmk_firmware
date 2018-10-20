cdbaecdfba3cf5d7ee1543a619d816e936cc50be
kanarne <kanarne@gmail.com>
2018-10-21 01:57:53 +0900

    chenge TAPPING_TERM

--- keyboards/helix/rev2/config.h
+++ keyboards/helix/rev2/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 
 /* Use I2C or Serial */
 #define USE_I2C
