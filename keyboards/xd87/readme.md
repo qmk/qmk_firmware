ab6d5338bb7477acd3f48b89d86fc4fedc86d0e8
Alexander Fougner <fougner89@gmail.com>
2018-10-21 15:44:02 +0200

    Add support for community layouts
    * Add TKL ANSI layout
    
    * Enable TKL ISO/ANSI community layout
    
    * Fix LAYOUT_all

--- keyboards/xd87/readme.md
+++ keyboards/xd87/readme.md
@@ -3,12 +3,20 @@
 ![XD87](https://i.imgur.com/qVyOies.jpg)
 
 
-Keyboard Maintainer: [Alexander Fougner](https://github.com/fougner)
-
-Hardware Supported: ATmega 32u4 based 80% keyboard kit
-
-Hardware Availability: [KPrepublic.com](https://kprepublic.com/products/xd87-xd87-xd80-custom-mechanical-keyboard-kit80-supports-tkg-tools-support-underglow-rgb-pcb-programmed-gh80-kle)
+- Keyboard Maintainer: [Alexander Fougner](https://github.com/fougner)
+- Hardware Supported: XD87 PCB v1.1
+- Hardware Availability: 
+	- [KPrepublic.com](https://kprepublic.com/products/xd87-xd87-xd80-custom-mechanical-keyboard-kit80-supports-tkg-tools-support-underglow-rgb-pcb-programmed-gh80-kle)
 
 Make example for this keyboard (after setting up your build environment):
 
     make xd87:default
+
+It also supports the tkl_ansi and tkl_iso community layouts:
+
+	  # Build firmware with ANSI layout
+	  make xd87:default_tkl_ansi
+	  # Build firmware with ISO layout
+	  make xd87:default_tkl_iso
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
--- keyboards/xd87/xd87.h
+++ keyboards/xd87/xd87.h
@@ -18,32 +18,38 @@
 
 #include "quantum.h"
 
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-
-// Keymap utilizing all the possible keys on the PCB.
-#define LAYOUT_7bit( \
-    K00, K01, K02, K03, K04, K05, K56, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, K0G, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      K1E, K1F, K1G, \
+#define LAYOUT_all( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K0E, K0F, K0G, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K3E, K1E, K1F, K1G, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,      K2E, K2F, K2G, \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,      K3E, K3F, K3G, \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,                     \
     K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D,      K4E, K4F, K4G, \
-    K50, K51, K52, K53,      K55, K57, K58,           K5A, K5B, K5C, K5D,      K5E, K5F, K5G  \
+    K50, K51, K52,           K55,      K58,           K5A, K5B, K5C, K5D,      K5E, K5F, K5G  \
 ) { \
 /*          0      1      2      3      4      5      6      7      8      9      A      B      C      D      E      F      G    */  \
 /* 0 */   { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D,   K0E,   K0F,   K0G   }, \
 /* 1 */   { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D,   K1E,   K1F,   K1G   }, \
 /* 2 */   { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   K2D,   K2E,   K2F,   K2G   }, \
-/* 3 */   { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,   K3E,   K3F,   K3G   }, \
+/* 3 */   { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,   KC_NO, KC_NO, KC_NO }, \
 /* 4 */   { K40,   K41,   K42,   K43,   K44,   K45,   K46,   K47,   K48,   K49,   K4A,   K4B,   K4C,   K4D,   K4E,   K4F,   K4G   }, \
-/* 5 */   { K50,   K51,   K52,   K53,   KC_NO, K55,   K56,   K57,   K58,   KC_NO, K5A,   K5B,   K5C,   K5D,   K5E,   K5F,   K5G   }  \
+/* 5 */   { K50,   K51,   K52,   KC_NO, KC_NO, K55,   KC_NO, KC_NO, K58,   KC_NO, K5A,   K5B,   K5C,   K5D,   K5E,   K5F,   K5G   }  \
 }
+
+#define LAYOUT_tkl_ansi( \
+    K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K0E, K0F, K0G, \
+    K01, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1D,      K1E, K1F, K1G, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,      K2E, K2F, K2G, \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3D,                     \
+    K40,      K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C,                K4F,      \
+    K50, K51, K52,                K58,                K5A, K5B, K5C, K5D,      K5E, K5F, K5G  \
+) LAYOUT_all( \
+    K00,   KC_NO, K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D,          K0E,   K0F,   K0G,   \
+    K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   KC_NO, K1D, KC_NO,   K1E,   K1F,   K1G,   \
+    K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   K2D,          K2E,   K2F,   K2G,   \
+    K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   KC_NO, K3D,                               \
+    K40,   KC_NO, K42,   K43,   K44,   K45,   K46,   K47,   K48,   K49,   K4A,   K4B,   K4C,   KC_NO,        KC_NO, K4F,   KC_NO, \
+    K50,   K51,   K52,                 KC_NO,        K58,                 K5A,   K5B,   K5C,   K5D,          K5E,   K5F,   K5G    \
+)
 
 #define LAYOUT_tkl_iso( \
     K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K0E, K0F, K0G, \
@@ -52,14 +58,13 @@
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,                     \
     K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C,                K4F,      \
     K50, K51, K52,                K58,                K5A, K5B, K5C, K5D,      K5E, K5F, K5G  \
-) LAYOUT_7bit( \
-    K00,   KC_NO, K02,   K03,   K04,   K05,   KC_NO, K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D,   K0E,   K0F,   K0G,   \
-    K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D,          K1E,   K1F,   K1G,   \
+) LAYOUT_all( \
+    K00,   KC_NO, K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D,          K0E,   K0F,   K0G,   \
+    K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D, KC_NO,   K1E,   K1F,   K1G,   \
     K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   KC_NO,        K2E,   K2F,   K2G,   \
-    K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,          KC_NO, KC_NO, KC_NO, \
+    K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,                               \
     K40,   K41,   K42,   K43,   K44,   K45,   K46,   K47,   K48,   K49,   K4A,   K4B,   K4C,   KC_NO,        KC_NO, K4F,   KC_NO, \
-    K50,   K51,   K52,   KC_NO,        KC_NO, KC_NO, K58,                 K5A,   K5B,   K5C,   K5D,          K5E,   K5F,   K5G    \
+    K50,   K51,   K52,                 KC_NO,        K58,                 K5A,   K5B,   K5C,   K5D,          K5E,   K5F,   K5G    \
 )
-
 
 #endif
