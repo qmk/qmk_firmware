faa3458702ced5e47692790f00257f1a595d75ea
monksoffunk <osamu@sho-k.com>
2018-10-22 23:36:45 +0900

    Fix error handling in split_scomm.c : mtei works

--- keyboards/zinc/rev1/serial_config.h
+++ keyboards/zinc/rev1/serial_config.h
@@ -1,14 +1,18 @@
 #ifndef SOFT_SERIAL_CONFIG_H
 #define SOFT_SERIAL_CONFIG_H
 
+#ifndef SOFT_SERIAL_PIN
 /* Soft Serial defines */
-#define SERIAL_PIN_DDR DDRD
-#define SERIAL_PIN_PORT PORTD
-#define SERIAL_PIN_INPUT PIND
-#define SERIAL_PIN_MASK _BV(PD2)
-#define SERIAL_PIN_INTERRUPT INT2_vect
+#define SOFT_SERIAL_PIN D2
+//  OPTIONAL: #define SELECT_SOFT_SERIAL_SPEED ? // ? = 1,2,3,4,5
+//                                               //  1: about 137kbps (default)
+//                                               //  2: about 75kbps
+//                                               //  3: about 39kbps
+//                                               //  4: about 26kbps
+//                                               //  5: about 20kbps
+#endif
 
+//// USE flexible API (using multi-type transaction function)
 #define SERIAL_USE_MULTI_TRANSACTION
-
 
 #endif /* SOFT_SERIAL_CONFIG_H */
