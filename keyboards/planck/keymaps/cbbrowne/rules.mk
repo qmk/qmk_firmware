c99e94d2c6fc3d576321f3b9ed6791f5f60f460c
Drashna Jaelre <drashna@live.com>
2018-10-22 08:57:37 -0700

    Replace outdated RGB/Audio information

--- keyboards/planck/keymaps/cbbrowne/rules.mk
+++ keyboards/planck/keymaps/cbbrowne/rules.mk
@@ -13,7 +13,7 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
 API_SYSEX_ENABLE = no      # Enable SYSEX API (+5390)
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
