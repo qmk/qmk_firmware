00b6f14821f44ead75504e28d7fed26791cb2875
Drashna Jaelre <drashna@live.com>
2018-10-22 08:57:37 -0700

    Replace outdated RGB/Audio information

--- keyboards/lets_split/keymaps/xk/rules.mk
+++ keyboards/lets_split/keymaps/xk/rules.mk
@@ -11,7 +11,7 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = yes         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 
 ifndef QUANTUM_DIR
