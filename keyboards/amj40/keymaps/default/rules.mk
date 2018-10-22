7e99d869deb57251dc15620beff34d5fd53066e4
Drashna Jaelre <drashna@live.com>
2018-10-22 10:26:19 -0700

    Remove all of the deprecated RGB defines
    Fixes #3641

--- keyboards/amj40/keymaps/default/rules.mk
+++ keyboards/amj40/keymaps/default/rules.mk
@@ -18,9 +18,6 @@ RGBLIGHT_ENABLE  = yes  # Enable WS2812 RGB underlight.
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-#define ws2812_PORTREG  PORTD
-#define ws2812_DDRREG   DDRD
-
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
--- keyboards/amj40/keymaps/myee/rules.mk
+++ keyboards/amj40/keymaps/myee/rules.mk
@@ -1,5 +1,5 @@
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in 
+#   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no  # Virtual DIP switch configuration(+1000)
@@ -17,11 +17,3 @@ RGBLIGHT_ENABLE  = yes  # Enable WS2812 RGB underlight.
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-
-#define ws2812_PORTREG  PORTD
-#define ws2812_DDRREG   DDRD
-
-
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
