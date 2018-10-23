aa01835d023282003a3497078349990614e4e668
Konstantin Đorđević <konstantin.djordjevic@tradecore.com>
2018-10-23 23:56:36 +0200

    Add usage and error messages

--- util/docker_build.sh
+++ util/docker_build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ $# -gt 2 ]; then
+    echo "Usage: $0 [KEYBOARD [KEYMAP]]"
+    exit 1
+elif [ ! "$(command -v docker)" ]; then
+    echo "Error: docker not found"
+    echo "See https://docs.docker.com/install/#supported-platforms for installation instructions"
+    exit 2
+fi
+
 path=$(pwd -W 2>/dev/null) || path=$PWD  # Use Windows path if on Windows
 
 docker run --rm \
