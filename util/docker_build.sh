6d3a2ff7bce334505db73a8ff0a031efde692e89
Konstantin Đorđević <vomindoraan@gmail.com>
2018-10-24 02:34:03 +0200

    Add keyboard:keymap form, improve script

--- util/docker_build.sh
+++ util/docker_build.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-if [ $# -gt 2 ]; then
-    echo "Usage: $0 [KEYBOARD [KEYMAP]]"
+IMAGE=edasque/qmk_firmware
+IMAGE_DIR=/qmk
+
+# Check preconditions
+if [[ $# -gt 2 ]]; then
+    echo "Usage: $0 [keyboard [keymap]]" >&2
+    echo "       $0 keyboard:keymap" >&2
     exit 1
-elif [ ! "$(command -v docker)" ]; then
-    echo "Error: docker not found"
-    echo "See https://docs.docker.com/install/#supported-platforms for installation instructions"
+elif [[ ! $(command -v docker) ]]; then
+    echo "Error: docker not found" >&2
+    echo "See https://docs.docker.com/install/#supported-platforms for installation instructions" >&2
     exit 2
 fi
 
-path=$(pwd -W 2>/dev/null) || path=$PWD  # Use Windows path if on Windows
+# Compute arguments
+if [[ $# -eq 1 && $1 == *:* ]]; then
+    # keyboard:keymap form
+    IFS=':' read -ra args <<< "$1"
+    keyboard=${args[0]}
+    keymap=${args[1]}
+else
+    # [keyboard [keymap]] form
+    keyboard=${1-$(read -rp "keyboard=" kb; echo $kb)}
+    keymap=${2-$(read -rp "keymap=" km; echo $km)}
+fi
+dir=$(pwd -W 2>/dev/null) || dir=$PWD  # Use Windows path if on Windows
 
-docker run --rm \
-    -e keyboard=${1-$(read -rp "keyboard=" kb; echo $kb)} \
-    -e keymap=${2-$(read -rp "keymap=" km; echo $km)} \
-    -v $path:/qmk:rw \
-    edasque/qmk_firmware
+# Run container and build firmware
+docker run --rm -e keyboard=$keyboard -e keymap=$keymap -v $dir:$IMAGE_DIR:rw $IMAGE
