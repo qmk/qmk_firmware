852129fbceb1c93f1fef6ccb586dff68f4ce9a7c
Konstantin Đorđević <vomindoraan@gmail.com>
2018-10-24 23:20:46 +0200

    Add target argument, change usage forms in script

--- util/docker_build.sh
+++ util/docker_build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-IMAGE=edasque/qmk_firmware
-IMAGE_DIR=/qmk
+read -rd '' USAGE << EOF
+Usage: $0
+       $0 keyboard:keymap[:target]
+       $0 keyboard [keymap] [target]
+EOF
 
 # Check preconditions
-if [[ $# -gt 2 ]]; then
-    echo "Usage: $0 [keyboard [keymap]]" >&2
-    echo "       $0 keyboard:keymap" >&2
+if [[ $* =~ "--help" ]]; then
+    echo "$USAGE"
+    exit 0
+elif [[ $# -gt 3 ]]; then
+    echo "$USAGE"
     exit 1
 elif [[ ! $(command -v docker) ]]; then
     echo "Error: docker not found" >&2
@@ -14,18 +19,31 @@ elif [[ ! $(command -v docker) ]]; then
     exit 2
 fi
 
-# Compute arguments
-if [[ $# -eq 1 && $1 == *:* ]]; then
-    # keyboard:keymap form
+# Determine arguments
+if [[ $# -eq 0 ]]; then
+    # Empty form
+    echo "Input build arguments (leave empty for defaults):"
+    read -rp "keyboard=" keyboard
+    read -rp "keymap=" keymap
+    read -rp "target=" target
+elif [[ $# -eq 1 && $1 == *:* ]]; then
+    # keyboard:keymap[:target] form
     IFS=':' read -ra args <<< "$1"
     keyboard=${args[0]}
     keymap=${args[1]}
+    target=${args[2]}
 else
-    # [keyboard [keymap]] form
-    keyboard=${1-$(read -rp "keyboard=" kb; echo $kb)}
-    keymap=${2-$(read -rp "keymap=" km; echo $km)}
+    # keyboard [keymap] [target] form
+    keyboard=$1
+    keymap=$2
+    target=$3
 fi
 dir=$(pwd -W 2>/dev/null) || dir=$PWD  # Use Windows path if on Windows
 
 # Run container and build firmware
-docker run --rm -e keyboard=$keyboard -e keymap=$keymap -v $dir:$IMAGE_DIR:rw $IMAGE
+docker run --rm \
+    ${keyboard:+-e keyboard="$keyboard"} \
+    ${keymap:+-e keymap="$keymap"} \
+    ${target:+-e target="$target"} \
+    -v "$dir":/qmk:rw \
+    edasque/qmk_firmware
