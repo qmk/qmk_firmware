b23fd4fa4dd90fb1ef839cfdc8547c55e993de52
Konstantin Đorđević <vomindoraan@gmail.com>
2018-10-25 07:03:27 +0200

    Add USB pass-through for Linux and docker-machine hosts

--- util/docker_build.sh
+++ util/docker_build.sh
@@ -30,8 +30,17 @@ else
     keymap=${args[1]}
     target=${args[2]}
 fi
+if [[ -n $target ]]; then
+    if [[ $(uname) == "Linux" || $(docker-machine active 2>/dev/null) ]]; then
+        usb_args="--privileged -v /dev/bus/usb:/dev/bus/usb"
+    else
+        echo "target requires docker-machine to run on your platform" >&2
+        echo "See http://gw.tnode.com/docker/docker-machine-with-usb-support-on-windows-macos" >&2
+        exit 3
+    fi
+fi
 dir=$(pwd -W 2>/dev/null) || dir=$PWD  # Use Windows path if on Windows
 
 # Run container and build firmware
-docker run --rm -v "$dir":/qmk_firmware qmkfm/qmk_firmware \
+docker run --rm $usb_args -v "$dir":/qmk_firmware qmkfm/qmk_firmware \
     make "$keyboard${keymap:+:$keymap}${target:+:$target}"
