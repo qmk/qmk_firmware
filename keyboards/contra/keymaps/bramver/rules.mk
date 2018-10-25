694bafbbbf07f0630aefcb7ecf66f04b6cc86164
Bramver <Bramvereertbrugghen@outlook.com>
2018-10-25 18:23:38 +0200

    Added personal Contra layout.

---
+++ keyboards/contra/keymaps/bramver/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+NKRO_ENABLE = yes           # USB Nkey Rollover
+UNICODEMAP_ENABLE = yes         # Unicode
+BOOTLOADER = atmel-dfu
+
+LAYOUTS = ortho_4x12 planck_mit planck_grid
