49c12e83b57f5c4c7a387355e4445e78db03a36f
lbibass <notlbibass@gmail.com>
2018-10-26 10:48:14 -0400

    Added locking caps lock.

--- keyboards/clueboard/60/keymaps/lbibass_aek/keymap.c
+++ keyboards/clueboard/60/keymaps/lbibass_aek/keymap.c
@@ -1,6 +1,5 @@
 #include "60.h"
 
-#define _______ KC_TRNS
 
 enum keyboard_layers {
   _BL,
@@ -27,13 +26,13 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
         MT(MOD_LGUI, KC_ESC ), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LT(KC_ENTER, _FL),
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LCTL, KC_SPC, KC_RGUI, KC_RALT, KC_RCTL),
+        KC_LOCKING_CAPS, KC_LALT, KC_LCTL, KC_SPC, KC_RGUI, KC_RALT, KC_RCTL),
     [_FL] = KEYMAP_AEK(
         KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        RESET, _______, _______, _______, _______, _______, _______),
+        KC_LOCKING_CAPS, _______, _______, _______, _______, _______, _______),
     [_CL] = KEYMAP_AEK(
         BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______,
