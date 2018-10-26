07413541ddccec4496ca20ad7e1acb4648c9ca22
wanleg <wanleg@github.com>
2018-10-26 13:44:05 -0700

    jj40 backlighting setup

--- layouts/community/ortho_4x12/wanleg/rules.mk
+++ layouts/community/ortho_4x12/wanleg/rules.mk
@@ -8,9 +8,3 @@ endif
 ifeq ($(strip $(KEYBOARD)), planck/rev6)
 	AUDIO_ENABLE = yes
 endif
-
-ifeq ($(strip $(KEYBOARD)), jj40)
-	BACKLIGHT_ENABLE = yes
-	# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-	SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-endif
