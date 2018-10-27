3d175f2340c14250a71af78afec5a1e890d9f4e7
comaid <a@b.c>
2018-10-27 17:22:54 +0900

    fix about screen off timer
    * Fix Up ScreenOffInterval exceeded uint16_t
    
    * Fix Up never waking up once screen off if in case of matrix are not dirty.

--- keyboards/crkbd/crkbd.c
+++ keyboards/crkbd/crkbd.c
@@ -1,1 +1,7 @@
 #include "crkbd.h"
+#include "ssd1306.h" 
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    process_record_gfx(keycode,record);
+	return process_record_user(keycode, record);
+}
--- keyboards/crkbd/ssd1306.h
+++ keyboards/crkbd/ssd1306.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "pincontrol.h"
+#include "action.h"
 
 enum ssd1306_cmds {
   DisplayOff = 0xAE,
@@ -86,3 +87,5 @@ void matrix_write(struct CharacterMatrix *matrix, const char *data);
 void matrix_write_ln(struct CharacterMatrix *matrix, const char *data);
 void matrix_write_P(struct CharacterMatrix *matrix, const char *data);
 void matrix_render(struct CharacterMatrix *matrix);
+
+bool process_record_gfx(uint16_t keycode, keyrecord_t *record);
