9913c7b2def48cb67858ea7d7ca88c37c127f352
jshuf <jshuf@puppyfish.com>
2018-10-28 21:00:28 -0400

    Change layout name from default to ansi

--- keyboards/kbd19x/kbd19x.h
+++ keyboards/kbd19x/kbd19x.h
@@ -82,7 +82,7 @@ inline void kbd19x_nmlk_led_off(void)   { DDRB &= ~(1<<2); PORTB &= ~(1<<2); }
   {k1d, k1e, k0e, k2e, k4e, k37, k38, k39, k45, k46, k47, k48, XXX},\
 }
 
-#define LAYOUT_default( \
+#define LAYOUT_ansi( \
   k00,  k01, k02, k03, k04,  k05, k06, k07, k08,  k09, k0a, k0b, k0c, k0e,     k0f, k0g, k0h, k0i, \
   k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c,    k1e,     k1f, k1g, k1h, k1i, \
   k20,  k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c,   k2e,     k2f, k2g, k2h, k2i, \
