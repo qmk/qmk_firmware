52080a23834d31312b36ab7ea1e08bff78efa014
Michael Pio <mfmayol@up.edu.ph>
2018-10-30 06:47:39 +0800

    updated rules.mk to enable more commands

--- keyboards/vinta/rules.mk
+++ keyboards/vinta/rules.mk
@@ -38,9 +38,9 @@ DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000
 #   comment out to disable the options.
 #
 BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = no				# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no				# Mouse keys
-EXTRAKEY_ENABLE = no				# Audio control and System control
+BOOTMAGIC_ENABLE = yes				# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes				# Mouse keys
+EXTRAKEY_ENABLE = yes				# Audio control and System control
 CONSOLE_ENABLE = no					# Console for debug
 COMMAND_ENABLE = no    				# Commands for debug and configuration
 NKRO_ENABLE = yes					# USB Nkey Rollover
