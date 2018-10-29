4f0ba55fe5796f25870ca4cc744a97c1e06b2b68
Bocaj <jacob.jerrell@gmail.com>
2018-10-29 12:59:00 -0500

    Adds pretty_osx and _win wrappers

--- layouts/community/ergodox/bocaj/keymap.c
+++ layouts/community/ergodox/bocaj/keymap.c
@@ -17,58 +17,93 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include "bocaj.h"
 
+#define LAYOUT_ergodox_pretty_osx( \
+    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, \
+    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
+    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A  \
+  ) \
+  LAYOUT_ergodox_pretty_wrapper( \
+      KC_ESC,  _______________________NUMBER_LEFT_______________________, JJ_ARRW,          KC_MINUS, _______________________NUMBER_RIGHT______________________, KC_EQL, \
+      KC_DEL,     K01,      K02,       K03,        K04,       K05,        KC_LPRN,          KC_RPRN,     K06,       K07,       K08,       K09,       K0A,       KC_BSLS, \
+      KC_NUMS,    K11, SFT_T(K12), GUI_T(K13),  ALT_T(K14),   K15,                                       K16,  ALT_T(K17),  GUI_T(K18), SFT_T(K19),  K1A,       KC_QUOT, \
+      KC_LSFT, CTL_T(K21),  K22,       K23,        K24,       K25,        HYP_LBK,          MEH_RBK,     K26,       K27,       K28,       K29,    CTL_T(K2A),   KC_RSFT, \
+          ___________________ERGODOX_BOTTOM_LEFT___________________,                                       ___________________ERGODOX_BOTTOM_RIGHT__________________,    \
+                                                         ______________________ERGODOX_THUMBS_____________________
+    )
+
+#define LAYOUT_ergodox_pretty_win( \
+    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, \
+    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
+    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A  \
+  ) \
+  LAYOUT_ergodox_pretty_wrapper( \
+      KC_ESC,  _______________________NUMBER_LEFT_______________________, JJ_ARRW,          KC_MINUS, _______________________NUMBER_RIGHT______________________, KC_EQL, \
+      KC_DEL,     K01,      K02,       K03,        K04,       K05,        KC_LPRN,          KC_RPRN,     K06,       K07,       K08,       K09,       K0A,       KC_BSLS, \
+      KC_NUMS,    K11, SFT_T(K12), CTL_T(K13),  ALT_T(K14),   K15,                                       K16,  ALT_T(K17),  CTL_T(K18), SFT_T(K19),  K1A,       KC_QUOT, \
+      KC_LSFT, GUI_T(K21),  K22,       K23,        K24,       K25,        HYP_LBK,          MEH_RBK,     K26,       K27,       K28,       K29,    GUI_T(K2A),   KC_RSFT, \
+          ___________________ERGODOX_BOTTOM_LEFT___________________,                                       ___________________ERGODOX_BOTTOM_RIGHT__________________,    \
+                                                         ______________________ERGODOX_THUMBS_____________________
+    )
+
+#define LAYOUT_ergodox_pretty_osx_wrapper(...)       LAYOUT_ergodox_pretty_osx(__VA_ARGS__)
+#define LAYOUT_ergodox_pretty_win_wrapper(...)       LAYOUT_ergodox_pretty_win(__VA_ARGS__)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Hardware Workman - http://www.keyboard-layout-editor.com/#/gists/7a07cb982ec3597ba3e3d947554225f1
-            .---------------------------------------------.                                              .---------------------------------------------.
-            |  ESC  |  1  |  2  |  3  |  4  |  5  |   ->  |                                              !   -   |  6  |  7  |  8  |  9  |  0  |   =   |
-            !-------+-----+-----+-----+-----+-------------!                                              !-------+-----+-----+-----+-----+-----+-------!
-            |  DEL  |  Q  |  D  |  R  |  W  |  B  |   (   |                                              !   )   |  J  |  F  |  U  |  P  |  ;  |   \   |
-            !-------+-----+-----+-----x-----x-----!       !                                              !       !-----x-----x-----+-----+-----+-------!
-            |  NUMS |  A  |  S  |  H  |  T  |  G  |-------!                                              !-------!  Y  |  N  |  E  |  O  |  I  |   '   |
-            !-------+-----+-----+-----x-----x-----!  HYP  !                                              !  MEH  !-----x-----x-----+-----+-----+-------!
-            | SHIFT |  Z  |  X  |  M  |  C  |  V  |   [   |                                              !   ]   |  K  |  L  |  ,  |  .  |  /  | SHIFT |
-            '-------+-----+-----+-----+-----+-------------'                                              '-------------+-----+-----+-----+-----+-------'
-             | GAME | NUM | LWR |  UP | LFT | .---------------.                                      .---------------. ! RGT | DWN |     |     | ADJ  |
-             '------------------------------' |   APP | HOME  |                                      !  PGUP |  ESC  | '------------------------------'
-                                      .-------+-------+-------!                                      !-------+-------+-------.
-                                      !       !       |  END  |                                      !  PGDN |       ! ENTER !
-                                      ! SPACE ! BSPCE !-------!                                      !-------!  TAB  !   /   !
-                                      |       |       | COPY  |                                      ! PASTE |       | LOWER |
-                                      '-----------------------'                                      '-----------------------'
+  /* Workman - http://www.keyboard-layout-editor.com/#/gists/7a07cb982ec3597ba3e3d947554225f1
+                .---------------------------------------------.                                 .---------------------------------------------.
+                |  ESC  |  1  |  2  |  3  |  4  |  5  |   ->  |                                 !   -   |  6  |  7  |  8  |  9  |  0  |   =   |
+                !-------+-----+-----+-----+-----+-------------!                                 !-------+-----+-----+-----+-----+-----+-------!
+                |  DEL  |  Q  |  D  |  R  |  W  |  B  |   (   |                                 !   )   |  J  |  F  |  U  |  P  |  ;  |   \   |
+                !-------+-----+-----+-----x-----x-----!       !                                 !       !-----x-----x-----+-----+-----+-------!
+                |  NUMS |  A  |  S  |  H  |  T  |  G  |-------!                                 !-------!  Y  |  N  |  E  |  O  |  I  |   '   |
+                !-------+-----+-----+-----x-----x-----!  HYP  !                                 !  MEH  !-----x-----x-----+-----+-----+-------!
+                | SHIFT |  Z  |  X  |  M  |  C  |  V  |   [   |                                 !   ]   |  K  |  L  |  ,  |  .  |  /  | SHIFT |
+                '-------+-----+-----+-----+-----+-------------'                                 '-------------+-----+-----+-----+-----+-------'
+                 | GAME | NUM | LWR |  UP | LFT | .---------------.                         .---------------. ! RGT | DWN |     |     | ADJ  |
+                 '------------------------------' |   APP | HOME  |                         !  PGUP |  ESC  | '------------------------------'
+                                          .-------+-------+-------!                         !-------+-------+-------.
+                                          !       !       |  END  |                         !  PGDN |       ! ENTER !
+                                          ! SPACE ! BSPCE !-------!                         !-------!  TAB  !   /   !
+                                          |       |       | COPY  |                         ! PASTE |       | LOWER |
+                                          '-----------------------'                         '-----------------------'
 */
-  [_HWRKMN] = LAYOUT_ergodox_pretty_wrapper(
-    KC_ESC, _______________________NUMBER_LEFT_______________________, JJ_ARRW,   KC_MINUS,_______________________NUMBER_RIGHT______________________, KC_EQUAL,
-    KC_DEL, _______________________HWORKMAN_L1_______________________, KC_LPRN,   KC_RPRN, _______________________HWORKMAN_R1_______________________, KC_BSLS,
-    KC_NUMS,_______________________HWORKMAN_L2_______________________,                     _______________________HWORKMAN_R2_______________________, KC_QUOTE,
-    KC_LSFT,_______________________HWORKMAN_L3_______________________, HYP_LBK,   MEH_RBK, _______________________HWORKMAN_R3_______________________, KC_RSFT,
-      ___________________ERGODOX_BOTTOM_LEFT___________________,                                  ___________________ERGODOX_BOTTOM_RIGHT__________________,
-                                                   ______________________ERGODOX_THUMBS_____________________
+  [_OSXWRKMN] = LAYOUT_ergodox_pretty_osx_wrapper(
+                 _______________________HWORKMAN_L1_______________________,         _______________________HWORKMAN_R1_______________________,
+                 _______________________HWORKMAN_L2_______________________,         _______________________HWORKMAN_R2_______________________,
+                 _______________________HWORKMAN_L3_______________________,         _______________________HWORKMAN_R3_______________________
   ),
-/* Software Workman / QWERTY - http://www.keyboard-layout-editor.com/#/gists/b6c016a22a9d31381a276a603a42fe5f
-            .---------------------------------------------.                                              .---------------------------------------------.
-            |  ESC  |  1  |  2  |  3  |  4  |  5  |   ->  |                                              !   -   |  6  |  7  |  8  |  9  |  0  |   =   |
-            !-------+-----+-----+-----+-----+-------------!                                              !-------+-----+-----+-----+-----+-----+-------!
-            |  DEL  |  Q  |  W  |  E  |  R  |  T  |   (   |                                              !   )   |  Y  |  U  |  I  |  O  |  P  |   \   |
-            !-------+-----+-----+-----x-----x-----!       !                                              !       !-----x-----x-----+-----+-----+-------!
-            |  NUMS |  A  |  S  |  D  |  F  |  G  |-------!                                              !-------!  H  |  J  |  K  |  L  |  ;  |   '   |
-            !-------+-----+-----+-----x-----x-----!  HYP  !                                              !  MEH  !-----x-----x-----+-----+-----+-------!
-            | SHIFT |  Z  |  X  |  C  |  V  |  B  |   [   |                                              !   ]   |  N  |  M  |  ,  |  .  |  /  | SHIFT |
-            '-------+-----+-----+-----+-----+-------------'                                              '-------------+-----+-----+-----+-----+-------'
-             | GAME | NUM | LWR |  UP | LFT | .---------------.                                      .---------------. ! RGT | DWN |     |     | ADJ  |
-             '------------------------------' |   APP | HOME  |                                      !  PGUP |  ESC  | '------------------------------'
-                                      .-------+-------+-------!                                      !-------+-------+-------.
-                                      !       !       |  END  |                                      !  PGDN |       ! ENTER !
-                                      ! SPACE ! BSPCE !-------!                                      !-------!  TAB  !   /   !
-                                      |       |       | COPY  |                                      ! PASTE |       | LOWER |
-                                      '-----------------------'                                      '-----------------------'
+  [_WINWRKMN] = LAYOUT_ergodox_pretty_win_wrapper(
+                 _______________________HWORKMAN_L1_______________________,         _______________________HWORKMAN_R1_______________________,
+                 _______________________HWORKMAN_L2_______________________,         _______________________HWORKMAN_R2_______________________,
+                 _______________________HWORKMAN_L3_______________________,         _______________________HWORKMAN_R3_______________________
+  ),
+/* QWERTY - http://www.keyboard-layout-editor.com/#/gists/b6c016a22a9d31381a276a603a42fe5f
+                 .---------------------------------------------.                               .---------------------------------------------.
+                 |  ESC  |  1  |  2  |  3  |  4  |  5  |   ->  |                               !   -   |  6  |  7  |  8  |  9  |  0  |   =   |
+                 !-------+-----+-----+-----+-----+-------------!                               !-------+-----+-----+-----+-----+-----+-------!
+                 |  DEL  |  Q  |  W  |  E  |  R  |  T  |   (   |                               !   )   |  Y  |  U  |  I  |  O  |  P  |   \   |
+                 !-------+-----+-----+-----x-----x-----!       !                               !       !-----x-----x-----+-----+-----+-------!
+                 |  NUMS |  A  |  S  |  D  |  F  |  G  |-------!                               !-------!  H  |  J  |  K  |  L  |  ;  |   '   |
+                 !-------+-----+-----+-----x-----x-----!  HYP  !                               !  MEH  !-----x-----x-----+-----+-----+-------!
+                 | SHIFT |  Z  |  X  |  C  |  V  |  B  |   [   |                               !   ]   |  N  |  M  |  ,  |  .  |  /  | SHIFT |
+                 '-------+-----+-----+-----+-----+-------------'                               '-------------+-----+-----+-----+-----+-------'
+                  | GAME | NUM | LWR |  UP | LFT | .---------------.                       .---------------. ! RGT | DWN |     |     | ADJ  |
+                  '------------------------------' |   APP | HOME  |                       !  PGUP |  ESC  | '------------------------------'
+                                           .-------+-------+-------!                       !-------+-------+-------.
+                                           !       !       |  END  |                       !  PGDN |       ! ENTER !
+                                           ! SPACE ! BSPCE !-------!                       !-------!  TAB  !   /   !
+                                           |       |       | COPY  |                       ! PASTE |       | LOWER |
+                                           '-----------------------'                       '-----------------------'
 */
-  [_SWRKMN] = LAYOUT_ergodox_pretty_wrapper(
-    KC_ESC, _______________________NUMBER_LEFT_______________________, JJ_ARRW,   KC_MINUS,_______________________NUMBER_RIGHT______________________, KC_EQUAL,
-    KC_DEL, _______________________SWORKMAN_L1_______________________, KC_LPRN,   KC_RPRN, _______________________SWORKMAN_R1_______________________, KC_BSLS,
-    KC_NUMS,_______________________SWORKMAN_L2_______________________,                     _______________________SWORKMAN_R2_______________________, KC_QUOTE,
-    KC_LSFT,_______________________SWORKMAN_L3_______________________, HYP_LBK,   MEH_RBK, _______________________SWORKMAN_R3_______________________, KC_RSFT,
-      ___________________ERGODOX_BOTTOM_LEFT___________________,                                  ___________________ERGODOX_BOTTOM_RIGHT__________________,
-                                                   ______________________ERGODOX_THUMBS_____________________
+  [_OSXQWERTY] = LAYOUT_ergodox_pretty_osx_wrapper(
+                 _______________________SWORKMAN_L1_______________________,         _______________________SWORKMAN_R1_______________________,
+                 _______________________SWORKMAN_L2_______________________,         _______________________SWORKMAN_R2_______________________,
+                 _______________________SWORKMAN_L3_______________________,         _______________________SWORKMAN_R3_______________________
+  ),
+  [_WINQWERTY] = LAYOUT_ergodox_pretty_win_wrapper(
+                 _______________________SWORKMAN_L1_______________________,         _______________________SWORKMAN_R1_______________________,
+                 _______________________SWORKMAN_L2_______________________,         _______________________SWORKMAN_R2_______________________,
+                 _______________________SWORKMAN_L3_______________________,         _______________________SWORKMAN_R3_______________________
   ),
 /* Lower - http://www.keyboard-layout-editor.com/#/gists/f1d745a88d1c48ab55e095efd9e7a43a
             .---------------------------------------------.                                              .---------------------------------------------.
@@ -146,15 +181,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DIABLO] = LAYOUT_ergodox_pretty_wrapper(
     KC_ESC , XXXXXXX  , XXXXXXX   ,XXXXXXX   ,XXXXXXX   ,XXXXXXX   ,XXXXXXX,    XXXXXXX,  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX,
     KC_TAB , KC_Q     ,ALT_T(KC_S), KC_I     , KC_F     , KC_J     ,KC_MINS,    XXXXXXX,  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX,
-<<<<<<< HEAD
     KC_Q   , KC_1     , KC_2      , KC_3     , KC_4     ,KC_DCLR   ,                      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX,
     KC_LSFT, KC_D3_1  , KC_D3_2   , KC_D3_3  , KC_D3_4  , KC_T     ,KC_LOCK,    XXXXXXX,  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX  , XXXXXXX   , XXXXXXX  , KC_M     ,                                             XXXXXXX,    XXXXXXX,    XXXXXXX,     _______, XXXXXXX,
-=======
-    KC_LOCK, KC_1     , KC_2      , KC_3     , KC_4     ,KC_DCLR   ,                      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX,
-    KC_LSFT, KC_D3_1  , KC_D3_2   , KC_D3_3  , KC_D3_4  , KC_T     ,KC_MINS,    XXXXXXX,  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX,
-    _______, XXXXXXX  , XXXXXXX   , XXXXXXX  , KC_M     ,                                             XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX, XXXXXXX,
->>>>>>> upstream/master
                                                               XXXXXXX, XXXXXXX,    XXXXXXX,XXXXXXX,
                                                                        XXXXXXX,    XXXXXXX,
                                                       KC_BTN1,KC_BTN2,KC_ENTER,    XXXXXXX,XXXXXXX,XXXXXXX
@@ -162,46 +191,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
-<<<<<<< HEAD
   // Keycodes using ErgoDox specific functions
   // MC_LOCK is here because default_layer is here
-=======
->>>>>>> upstream/master
   uint8_t default_layer = 0;
   default_layer = eeconfig_read_default_layer();
   switch (keycode) {
     case KC_SWRK:
       if (!record->event.pressed) {
         set_single_persistent_default_layer(_SWRKMN);
-<<<<<<< HEAD
         layer_move(_SWRKMN);
-=======
-        layer_move(default_layer);
->>>>>>> upstream/master
-        ergodox_blink_all_leds();
-        ergodox_blink_all_leds();
-      }
       return false;
       break;
     case KC_HWRK:
       if (!record->event.pressed) {
         set_single_persistent_default_layer(_HWRKMN);
-<<<<<<< HEAD
         layer_move(_HWRKMN);
-=======
-        layer_move(default_layer);
->>>>>>> upstream/master
         ergodox_blink_all_leds();
-        ergodox_blink_all_leds();
-      }
-      return false;
-      break;
-    case KC_EPRM:
-      if (!record->event.pressed) {
-        ergodox_blink_all_leds();
-        eeconfig_init();
-      }
-      return false;
       break;
     case MC_LOCK:
       if (!record->event.pressed) {
