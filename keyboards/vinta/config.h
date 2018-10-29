465aa70ca289b3fe2d8b8f16829bf864ee432a38
Michael Pio <mfmayol@up.edu.ph>
2018-10-30 06:46:40 +0800

    changed product id and device ver

--- keyboards/vinta/config.h
+++ keyboards/vinta/config.h
@@ -1,27 +1,11 @@
-/* Copyright 2018 Peioris
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0258
-#define DEVICE_VER      0x0001
+#define PRODUCT_ID      0x0C61
+#define DEVICE_VER      0x00C6
 #define MANUFACTURER    PeiorisBoards
 #define PRODUCT         Vinta R1
 #define DESCRIPTION     Tada68 Replacement PCB
