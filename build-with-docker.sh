c620f79c266ff5a221ff4261f75f0f64797bcfc7
danielo515 <rdanielo@gmail.com>
2018-10-31 17:19:27 +0100

    feat(build): added script for compiling with docker easily

---
+++ build-with-docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This script requires that you have docker installed.
+# To use it just provide the keyboard name as the first argument and the keymap as the second argument.
+# The docker container is deleted after the compilation, so this does not leave any trace.
+KEYBOARD=${1:-ergodox_ez}
+KEYMAP=${2:-default}
+docker run --rm -v $(pwd):/qmk -e keyboard="$KEYBOARD" -e subproject=" " -e keymap="$KEYMAP" qmk:latest
