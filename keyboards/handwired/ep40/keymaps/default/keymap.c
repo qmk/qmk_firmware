16566a7e5a4fbca77bcb172cb277a9924975c79b
Elliot Powell <johnp8727@gmail.com>
2018-11-02 23:16:40 +0000

    Updated defualt keympap to have a Backspace mod del key

--- keyboards/handwired/ep40/keymaps/default/keymap.c
+++ keyboards/handwired/ep40/keymaps/default/keymap.c
@@ -15,12 +15,19 @@
  */
 #include QMK_KEYBOARD_H
 
+static bool bsdel_mods = false;
+
+enum custom_keycodes {
+  PLACEHOLDER = SAFE_RANGE, // ensure these codes start after the highest keycode defined in Quantum
+  M_BSDEL,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-    KC_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,\
+    KC_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, M_BSDEL,\
     KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,       KC_ENT, \
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_RSFT,\
-    LCTL_T(KC_LBRC), KC_LGUI, KC_LALT,  KC_SPC,  LT(1, KC_SPC), KC_RGUI, MO(2), RCTL_T(KC_RBRC),  KC_DEL \
+    LCTL_T(KC_LBRC), KC_LGUI, KC_LALT,  KC_SPC,  LT(1, KC_SPC), KC_RALT, MO(2), RCTL_T(KC_RBRC),  KC_F5 \
   ),
 
   [1] = LAYOUT( /* Base */
@@ -37,3 +44,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, RESET \
   ),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+      case M_BSDEL: {
+        uint8_t kc = KC_BSPC;
+
+        if (record->event.pressed) {
+            if (keyboard_report->mods) {
+                kc = KC_DEL;
+            }
+            register_code (kc);
+            bsdel_mods = keyboard_report->mods;
+        }
+        else {
+            if (bsdel_mods) {
+                kc = KC_DEL;
+            }
+            unregister_code (kc);
+        }
+
+        return false;
+        break;
+    }
+  }
+  return true;
+}
