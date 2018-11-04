380b645889ad3908b8925ec4ee1e1818adea8c3d
comaid <a@b.c>
2018-11-04 20:42:58 +0900

    Fix up screen off timer of helix
    * Fix Up ScreenOffInterval exceeded uint16_t
    
    * Fix Up never waking up once screen off if in case of matrix are not dirty

--- keyboards/helix/helix.c
+++ keyboards/helix/helix.c
@@ -1,1 +1,9 @@
 #include "helix.h"
+#include "ssd1306.h" 
+
+#ifdef OLED_ENABLED
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    process_record_gfx(keycode,record);
+	return process_record_user(keycode, record);
+}
+#endif
