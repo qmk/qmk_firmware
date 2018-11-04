e47b5ed27f3cc68883e76dc544f1448e5761e96e
comaid <a@b.c>
2018-11-05 08:16:20 +0900

    Improve internal processing of process_record_kb()
    * Use the return value of process_record_gfx()

--- keyboards/crkbd/crkbd.c
+++ keyboards/crkbd/crkbd.c
@@ -2,6 +2,5 @@
 #include "ssd1306.h" 
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    process_record_gfx(keycode,record);
-	return process_record_user(keycode, record);
+	return process_record_gfx(keycode,record) && process_record_user(keycode, record);
 }
