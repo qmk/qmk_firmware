6ede8b07c17a8720c63906981b5a3607a7eafcbe
comaid <a@b.c>
2018-11-04 21:55:38 +0900

    Changing referenIng incorrect constant name
    * OLED_ENABLED => SSD1306OLED

--- keyboards/helix/helix.c
+++ keyboards/helix/helix.c
@@ -1,7 +1,7 @@
 #include "helix.h"
 #include "ssd1306.h" 
 
-#ifdef OLED_ENABLED
+#ifdef SSD1306OLED
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     process_record_gfx(keycode,record);
 	return process_record_user(keycode, record);
