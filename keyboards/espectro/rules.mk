a8093ad283791b3b2eb9a935dcd5e9341ee3078c
Drashna Jaelre <drashna@live.com>
2018-11-04 10:02:03 -0500

    Update keyboards/espectro/rules.mk
    Co-Authored-By: TurboMech <32869044+TurboMech@users.noreply.github.com>

--- keyboards/espectro/rules.mk
+++ keyboards/espectro/rules.mk
@@ -50,7 +50,7 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE ?= no	# Mouse keys(+4700)
+MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
 CONSOLE_ENABLE ?= no	# Console for debug(+400)
 COMMAND_ENABLE ?= yes    # Commands for debug and configuration
