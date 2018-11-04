dc24a1f9d3d5ceb648fe40281b57c4978e87eb6b
comaid <a@b.c>
2018-11-05 08:28:10 +0900

    Improve internal processing of process_record_kb()
    * Use the return value of process_record_gfx()

--- keyboards/helix/helix.c
+++ keyboards/helix/helix.c
@@ -3,7 +3,6 @@
 
 #ifdef SSD1306OLED
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    process_record_gfx(keycode,record);
-	return process_record_user(keycode, record);
+	return process_record_gfx(keycode,record) && process_record_user(keycode, record);
 }
 #endif
