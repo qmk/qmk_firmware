801a7aed2a25ba0ea7d1a7ec12c1e1a7bf95a792
zvecr <z.zvecr@gmail.com>
2018-11-06 00:25:07 +0000

    Switch to bootmagic lite

--- keyboards/6lit/keymaps/default/keymap.c
+++ keyboards/6lit/keymaps/default/keymap.c
@@ -31,11 +31,7 @@ void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
-  // jump to bootloaer when all keys are pressed
-  if (matrix_get_row(0) == 0b111 && matrix_get_row(1) == 0b111) {
-    clear_keyboard();
-    bootloader_jump();
-  }
+
 }
 
 void led_set_user(uint8_t usb_led) {
