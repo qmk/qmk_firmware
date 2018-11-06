496b17b4a130e36b02521df4f487505a3746a458
mechmerlin <mechmerlin@gmail.com>
2018-11-06 11:40:07 -0800

    set rgb underglow color

--- keyboards/dz60/keymaps/mechmerlin/keymap.c
+++ keyboards/dz60/keymaps/mechmerlin/keymap.c
@@ -32,3 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
+
+// Set underglow RGB leds to yellow
+// Find the list of available colors in quantum/rgblight_list.h
+void matrix_init_user(void) {
+  rgblight_sethsv_noeeprom_yellow();
+}
