c2ed4bc5341eb116b12ba397fa530bd890929497
mechmerlin <mechmerlin@gmail.com>
2018-11-05 16:43:03 -0800

    cleanup keymap

--- keyboards/dz60/keymaps/mechmerlin/keymap.c
+++ keyboards/dz60/keymaps/mechmerlin/keymap.c
@@ -1,15 +1,21 @@
 #include QMK_KEYBOARD_H
 
-#define _BL 0 // Base Layer
-#define _FL 1 // Function Layer
-#define _CL 2 // Control Layer
+enum keyboard_layers {
+  _BL = 0, // Base Layer
+  _FL,     // Function Layer
+  _AL      // Arrow Layer
+};
+
+// Custom #defined keycodes (shorter macros for readability)
+#define KC_CTCP LCTL_T(KC_CAPS)
+#define KC_RSSH RSFT_T(KC_SLSH)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT_60_b_ansi(
-      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      LCTL_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_LSFT,
-      KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, KC_DEL,
+      KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+      KC_CTCP, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_LSFT,
+      KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_RSSH, KC_UP,   KC_DEL,
       KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, MO(_FL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   [_FL] = LAYOUT_60_b_ansi(
