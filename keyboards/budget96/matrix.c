ca8071e0b8f7135bb736355ffd74e5e730153952
mechmerlin <mechmerlin@gmail.com>
2018-11-08 12:31:55 -0800

    update matrix.c

--- keyboards/budget96/matrix.c
+++ keyboards/budget96/matrix.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
+Copyright 2018 Jack Humbert <jack.humb@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,33 +17,58 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <avr/io.h>
 #include <util/delay.h>
-
+#include <string.h>
 #include "matrix.h"
 
 #ifndef DEBOUNCE
 #   define DEBOUNCE	5
 #endif
 
+__attribute__ ((weak))
+void matrix_init_kb(void) {
+  matrix_init_user();
+}
+
+__attribute__ ((weak))
+void matrix_scan_kb(void) {
+  matrix_scan_user();
+}
+
+__attribute__ ((weak))
+void matrix_init_user(void) { }
+
+__attribute__ ((weak))
+void matrix_scan_user(void) { }
+
+// #define MATRIX_ROW_PINS { A0, A1, A2, A3, A4, A5, A6, A7, C7, C6, C5, C4, C3, C2, D7 }
+// #define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7 }
+
 static uint8_t debouncing = DEBOUNCE;
 
 static matrix_row_t matrix[MATRIX_ROWS];
 static matrix_row_t matrix_debouncing[MATRIX_ROWS];
 
-void matrix_set_row_status(uint8_t row);
-uint8_t bit_reverse(uint8_t x);
-
 void matrix_init(void) {
-    // all outputs for rows high
-    DDRB = 0xFF;
-    PORTB = 0xFF;
-    // all inputs for columns
-    DDRA = 0x00;
-    DDRC &= ~(0x111111<<2);
-    DDRD &= ~(1<<PIND7);
-    // all columns are pulled-up
-    PORTA = 0xFF;
-    PORTC |= (0b111111<<2);
-    PORTD |= (1<<PIND7);
+
+    // disables JTAG so we can use them as columns
+    MCUCSR = (1<<JTD);
+    MCUCSR = (1<<JTD);
+
+    // rows (output)
+    DDRA |= ((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
+    PORTA |= ((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
+
+    DDRC |= ((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2));
+    PORTC |= ((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2));
+
+    DDRD |= (1 << 7);
+    PORTD |= (1 << 7);
+
+    // cols (input)
+    DDRB &= ~((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
+
+    // pull-up cols
+    PORTB |= ((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
 
     // initialize matrix state: all keys off
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
@@ -55,23 +80,60 @@ void matrix_init(void) {
 }
 
 uint8_t matrix_scan(void) {
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        matrix_set_row_status(row);
+
+    // actual matrix scan
+    for (uint8_t c = 0; c < MATRIX_ROWS; c++) {
+        switch (c) {
+          case 0:   PORTA &= ~(1 << 0); break;
+          case 1:   PORTA &= ~(1 << 1); break;
+          case 2:   PORTA &= ~(1 << 2); break;
+          case 3:   PORTA &= ~(1 << 3); break;
+          case 4:   PORTA &= ~(1 << 4); break;
+          case 5:   PORTA &= ~(1 << 5); break;
+          case 6:   PORTA &= ~(1 << 6); break;
+          case 7:   PORTA &= ~(1 << 7); break;
+          case 8:   PORTC &= ~(1 << 2); break;
+          case 9:   PORTC &= ~(1 << 3); break;
+          case 10:  PORTC &= ~(1 << 4); break;
+          case 11:  PORTC &= ~(1 << 5); break;
+          case 12:  PORTC &= ~(1 << 6); break;
+          case 13:  PORTC &= ~(1 << 7); break;
+          case 14:  PORTD &= ~(1 << 7); break;
+        }
         _delay_us(5);
 
-        matrix_row_t cols = (
-            // cols 0..7, PORTA 0 -> 7
-            (~PINA) & 0xFF
-        ) | (
-            // cols 8..13, PORTC 7 -> 0
-            bit_reverse((~PINC) & 0xFF) << 8
-        ) | (
-            // col 14, PORTD 7
-            ((~PIND) & (1 << PIND7)) << 7
+  //columns
+        matrix_row_t current_row = (
+          (((PINB & (1 << 0)) ? 0 : 1 ) << 0)  |
+          (((PINB & (1 << 1)) ? 0 : 1 ) << 1)  |
+          (((PINB & (1 << 2)) ? 0 : 1 ) << 2)  |
+          (((PINB & (1 << 3)) ? 0 : 1 ) << 3)  |
+          (((PINB & (1 << 4)) ? 0 : 1 ) << 4)  |
+          (((PINB & (1 << 5)) ? 0 : 1 ) << 5)  |
+          (((PINB & (1 << 6)) ? 0 : 1 ) << 6)  |
+          (((PINB & (1 << 7)) ? 0 : 1 ) << 7)
         );
 
-        if (matrix_debouncing[row] != cols) {
-            matrix_debouncing[row] = cols;
+        switch (c) {
+          case 0:   PORTA |= (1 << 0); break;
+          case 1:   PORTA |= (1 << 1); break;
+          case 2:   PORTA |= (1 << 2); break;
+          case 3:   PORTA |= (1 << 3); break;
+          case 4:   PORTA |= (1 << 4); break;
+          case 5:   PORTA |= (1 << 5); break;
+          case 6:   PORTA |= (1 << 6); break;
+          case 7:   PORTA |= (1 << 7); break;
+          case 8:   PORTC |= (1 << 2); break;
+          case 9:   PORTC |= (1 << 3); break;
+          case 10:  PORTC |= (1 << 4); break;
+          case 11:  PORTC |= (1 << 5); break;
+          case 12:  PORTC |= (1 << 6); break;
+          case 13:  PORTC |= (1 << 7); break;
+          case 14:  PORTD |= (1 << 7); break;
+        }
+
+        if (matrix_debouncing[c] != current_row) {
+            matrix_debouncing[c] = current_row;
             debouncing = DEBOUNCE;
         }
     }
@@ -89,19 +151,6 @@ uint8_t matrix_scan(void) {
     matrix_scan_quantum();
 
     return 1;
-}
-
-// declarations
-void matrix_set_row_status(uint8_t row) {
-    DDRB = (1 << row);
-    PORTB = ~(1 << row);
-}
-
-uint8_t bit_reverse(uint8_t x) {
-    x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
-    x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
-    x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0);
-    return x;
 }
 
 inline matrix_row_t matrix_get_row(uint8_t row) {
