5e5d4ec8e64a986401e5dd977f87a641fc978f31
mechmerlin <mechmerlin@gmail.com>
2018-11-08 21:05:25 -0800

    put KC_NO where they belong

--- keyboards/budget96/budget96.h
+++ keyboards/budget96/budget96.h
@@ -32,20 +32,20 @@
 ) \
 { \
     { k00, k01, k02, k03, k04, k05, k06, k07 }, \
-    { k10, k11, k12, k13, k14, k15, k16, k17 }, \
+    { k10, k11, k12, k13, k14, KC_NO, k16, k17 }, \
     { k20, k21, k22, k23, k24, k25, k26, k27 }, \
     { k30, k31, k32, k33, k34, k35, k36, k37 }, \
-    { k40, k41, k42, k43, k44, k45, k46, k47 }, \
-    { k50, k51, k52, k53, k54, k55, k56, k57 }, \
-    { k60, k61, k62, k63, k64, k65, k66, k67 }, \
-    { k70, k71, k72, k73, k74, k75, k76, k77 }, \
-    { k80, k81, k82, k83, k84, k85, k86, k87 }, \
-    { k90, k91, k92, k93, k94, k95, k96, k97 }, \
-    { kA0, kA1, kA2, kA3, kA4, kA5, kA6, kA7 }, \
+    { KC_NO, k41, k42, k43, k44, k45, k46, k47 }, \
+    { KC_NO, k51, k52, k53, k54, k55, k56, k57 }, \
+    { k60, k61, k62, k63, k64, KC_NO, k66, k67 }, \
+    { KC_NO, k71, k72, k73, k74, KC_NO, k76, KC_NO }, \
+    { k80, k81, k82, k83, k84, KC_NO, k86, k87 }, \
+    { k90, k91, k92, k93, k94, KC_NO, k96, k97 }, \
+    { kA0, kA1, kA2, kA3, kA4, KC_NO, kA6, kA7 }, \
     { kB0, kB1, kB2, kB3, kB4, kB5, kB6, kB7 }, \
-    { kC0, kC1, kC2, kC3, kC4, kC5, kC6, kC7 }, \
-    { kD0, kD1, kD2, kD3, kD4, kD5, kD6, kD7 }, \
-    { kE0, kE1, kE2, kE3, kE4, kE5, kE6, kE7 }, \
+    { KC_NO, KC_NO, kC2, kC3, kC4, kC5, kC6, kC7 }, \
+    { kD0, kD1, KC_NO, kD3, kD4, kD5, kD6, kD7 }, \
+    { KC_NO, kE1, KC_NO, KC_NO, kE4, kE5, kE6, KC_NO }, \
 }
 
 #endif
