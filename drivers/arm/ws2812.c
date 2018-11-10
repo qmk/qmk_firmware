f90ebb5f67f629f8eb205d0381ebb5dfdc9bf894
Andrew Kannan <andrew.kannan@gmail.com>
2018-11-10 10:04:02 -0500

    Save progress for now

--- drivers/arm/ws2812.c
+++ drivers/arm/ws2812.c
@@ -8,154 +8,91 @@
 #include "ws2812.h"
 #include "stdlib.h"
 
-static uint8_t *fb;
-static int sLeds;
-static stm32_gpio_t *sPort;
-static uint32_t sMask;
-uint8_t* dma_source;
-static LED_TYPE led_array[RGBLED_NUM];
+#define BYTES_FOR_LED_BYTE 4
+#define NB_COLORS 3
+#define BYTES_FOR_LED BYTES_FOR_LED_BYTE*NB_COLORS
+#define DATA_SIZE BYTES_FOR_LED*NB_LEDS
+#define RESET_SIZE 200
+#define PREAMBLE_SIZE 4
+// Define the spi your LEDs are plugged to here
+#define WS2812_SPI SPID2
+// Define the number of LEDs you wish to control in your LED strip
+#define NB_LEDS RGBLED_NUM
 
-void setColor(uint8_t color, uint8_t *buf,uint32_t mask){
-  int i;
-  for (i=0;i<8;i++){
-    buf[i]=((color<<i)&0b10000000?0x0:mask);
+#define LED_SPIRAL 1
+
+static uint8_t txbuf[PREAMBLE_SIZE + DATA_SIZE + RESET_SIZE];
+static uint8_t get_protocol_eq(uint8_t data, int pos);
+
+/*
+ * This lib is meant to be used asynchronously, thus the colors contained in
+ * the txbuf will be sent in loop, so that the colors are always the ones you
+ * put in the table (the user thus have less to worry about)
+ *
+ * Since the data are sent via DMA, and the call to spiSend is a blocking one,
+ * the processor ressources are not used to much, if you see your program being
+ * too slow, simply add a:
+ * chThdSleepMilliseconds(x);
+ * after the spiSend, where you increment x untill you are satisfied with your
+ * program speed, another trick may be to lower this thread priority : your call
+ */
+static THD_WORKING_AREA(LEDS_THREAD_WA, 128);
+static THD_FUNCTION(ledsThread, arg) {
+  (void) arg;
+  while(1){
+    spiSend(&LEDS_SPI, PREAMBLE_SIZE + DATA_SIZE + RESET_SIZE, txbuf);
   }
 }
 
-void setColorRGB(Color c, uint8_t *buf, uint32_t mask) {
-  setColor(c.G,buf, mask);
-  setColor(c.R,buf+8, mask);
-  setColor(c.B,buf+16, mask);
+static const SPIConfig spicfg = {
+  NULL,
+  PORT_WS2812,
+  PIN_WS2812,
+  SPI_CR1_BR_1|SPI_CR1_BR_0 // baudrate : fpclk / 8 => 1tick is 0.32us
+};
+
+/*
+ * Function used to initialize the driver.
+ *
+ * Starts by shutting off all the LEDs.
+ * Then gets access on the LED_SPI driver.
+ * May eventually launch an animation on the LEDs (e.g. a thread setting the
+ * txbuff values)
+ */
+void leds_init(void){
+  for(int i = 0; i < RESET_SIZE; i++)
+    txbuf[DATA_SIZE+i] = 0x00;
+  for (int i=0; i<PREAMBLE_SIZE; i++)
+    txbuf[i] = 0x00;
+  spiAcquireBus(&LEDS_SPI);              /* Acquire ownership of the bus.    */
+  spiStart(&LEDS_SPI, &spicfg);          /* Setup transfer parameters.       */
+  spiSelect(&LEDS_SPI);                  /* Slave Select assertion.          */
+  chThdCreateStatic(LEDS_THREAD_WA, sizeof(LEDS_THREAD_WA),NORMALPRIO, ledsThread, NULL);
 }
 
-/**
- * @brief   Initialize Led Driver
- * @details Initialize the Led Driver based on parameters.
- *          Following initialization, the frame buffer would automatically be
- *          exported to the supplied port and pins in the right timing to drive
- *          a chain of WS2812B controllers
- * @note    The function assumes the controller is running at 72Mhz
- * @note    Timing is critical for WS2812. While all timing is done in hardware
- *          need to verify memory bandwidth is not exhausted to avoid DMA delays
- *
- * @param[in] leds      length of the LED chain controlled by each pin
- * @param[in] port      which port would be used for output
- * @param[in] mask      Which pins would be used for output, each pin is a full chain
- * @param[out] o_fb     initialized frame buffer
- *
+/*
+ * As the trick here is to use the SPI to send a huge pattern of 0 and 1 to
+ * the ws2812b protocol, we use this helper function to translate bytes into
+ * 0s and 1s for the LED (with the appropriate timing).
  */
+static uint8_t get_protocol_eq(uint8_t data, int pos){
+  uint8_t eq = 0;
+  if (data & (1 << (2*(3-pos))))
+    eq = 0b1110;
+  else
+    eq = 0b1000;
+  if (data & (2 << (2*(3-pos))))
+    eq += 0b11100000;
+  else
+    eq += 0b10000000;
+  return eq;
+}
+
 
 void WS2812_init(void) {
-  static uint8_t * p;
-  //uint32_t port = RGBLED_PORT;
-  //ledDriverInit(RGBLED_NUM, (stm32_gpio_t *)(port & 0xFFF0), 1 << (port & 0xF), &p);
-  ledDriverInit(RGBLED_NUM, GPIOA, 0b00000010, &p);
-}
-
-void ledDriverInit(int leds, stm32_gpio_t *port, uint32_t mask, uint8_t **o_fb) {
-  sLeds=leds;
-  sPort=port;
-  sMask=mask;
-  palSetGroupMode(port, sMask, 0, PAL_MODE_OUTPUT_PUSHPULL|PAL_STM32_OSPEED_HIGHEST|PAL_STM32_PUPDR_FLOATING);
-
-  // maybe don't do whole port?
-  // palSetPadMode(port, 8, PAL_MODE_OUTPUT_PUSHPULL|PAL_STM32_OSPEED_HIGHEST|PAL_STM32_PUPDR_FLOATING);
-
-
-  // configure pwm timers -
-  // timer 2 as master, active for data transmission and inactive to disable transmission during reset period (50uS)
-  // timer 3 as slave, during active time creates a 1.25 uS signal, with duty cycle controlled by frame buffer values
-  static PWMConfig pwmc2 = {72000000 / 90, /* 800Khz PWM clock frequency. 1/90 of PWMC3   */
-                            (72000000 / 90) * 0.05, /*Total period is 50ms (20FPS), including sLeds cycles + reset length for ws2812b and FB writes  */
-                            NULL,
-                            { {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-                              {PWM_OUTPUT_DISABLED, NULL},
-                              {PWM_OUTPUT_DISABLED, NULL},
-                              {PWM_OUTPUT_DISABLED, NULL}},
-                              TIM_CR2_MMS_2, /* master mode selection */
-                              0, };
-  /* master mode selection */
-  static PWMConfig pwmc3 = {72000000,/* 72Mhz PWM clock frequency.   */
-                            90, /* 90 cycles period (1.25 uS per period @72Mhz       */
-                            NULL,
-                            { {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-                              {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-                              {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-                              {PWM_OUTPUT_ACTIVE_HIGH, NULL}},
-                              0,
-                              0,
-  };
-  dma_source = chHeapAlloc(NULL, 1);
-  fb = chHeapAlloc(NULL, ((sLeds) * 24)+10);
-  *o_fb=fb;
-  int j;
-  for (j = 0; j < (sLeds) * 24; j++) fb[j] = 0;
-  dma_source[0] = sMask;
-  // DMA stream 2, triggered by channel3 pwm signal. if FB indicates, reset output value early to indicate "0" bit to ws2812
-  dmaStreamAllocate(STM32_DMA1_STREAM2, 10, NULL, NULL);
-  dmaStreamSetPeripheral(STM32_DMA1_STREAM2, &(sPort->BSRR.H.clear));
-  dmaStreamSetMemory0(STM32_DMA1_STREAM2, fb);
-  dmaStreamSetTransactionSize(STM32_DMA1_STREAM2, (sLeds) * 24);
-  dmaStreamSetMode(
-      STM32_DMA1_STREAM2,
-      STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_MINC | STM32_DMA_CR_PSIZE_BYTE
-      | STM32_DMA_CR_MSIZE_BYTE | STM32_DMA_CR_CIRC | STM32_DMA_CR_PL(2));
-  // DMA stream 3, triggered by pwm update event. output high at beginning of signal
-  dmaStreamAllocate(STM32_DMA1_STREAM3, 10, NULL, NULL);
-  dmaStreamSetPeripheral(STM32_DMA1_STREAM3, &(sPort->BSRR.H.set));
-  dmaStreamSetMemory0(STM32_DMA1_STREAM3, dma_source);
-  dmaStreamSetTransactionSize(STM32_DMA1_STREAM3, 1);
-  dmaStreamSetMode(
-      STM32_DMA1_STREAM3, STM32_DMA_CR_TEIE |
-      STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MSIZE_BYTE
-      | STM32_DMA_CR_CIRC | STM32_DMA_CR_PL(3));
-  // DMA stream 6, triggered by channel1 update event. reset output value late to indicate "1" bit to ws2812.
-  // always triggers but no affect if dma stream 2 already change output value to 0
-  dmaStreamAllocate(STM32_DMA1_STREAM6, 10, NULL, NULL);
-  dmaStreamSetPeripheral(STM32_DMA1_STREAM6, &(sPort->BSRR.H.clear));
-  dmaStreamSetMemory0(STM32_DMA1_STREAM6, dma_source);
-  dmaStreamSetTransactionSize(STM32_DMA1_STREAM6, 1);
-  dmaStreamSetMode(
-      STM32_DMA1_STREAM6,
-      STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MSIZE_BYTE
-      | STM32_DMA_CR_CIRC | STM32_DMA_CR_PL(3));
-  pwmStart(&PWMD2, &pwmc2);
-  pwmStart(&PWMD3, &pwmc3);
-  // set pwm3 as slave, triggerd by pwm2 oc1 event. disables pwmd2 for synchronization.
-  PWMD3.tim->SMCR |= TIM_SMCR_SMS_0 | TIM_SMCR_SMS_2 | TIM_SMCR_TS_0;
-  PWMD2.tim->CR1 &= ~TIM_CR1_CEN;
-  // set pwm values.
-  // 28 (duty in ticks) / 90 (period in ticks) * 1.25uS (period in S) = 0.39 uS
-  pwmEnableChannel(&PWMD3, 2, 28);
-  // 58 (duty in ticks) / 90 (period in ticks) * 1.25uS (period in S) = 0.806 uS
-  pwmEnableChannel(&PWMD3, 0, 58);
-  // active during transfer of 90 cycles * sLeds * 24 bytes * 1/90 multiplier
-  pwmEnableChannel(&PWMD2, 0, 90 * sLeds * 24 / 90);
-  // stop and reset counters for synchronization
-  PWMD2.tim->CNT = 0;
-  // Slave (TIM3) needs to "update" immediately after master (TIM2) start in order to start in sync.
-  // this initial sync is crucial for the stability of the run
-  PWMD3.tim->CNT = 89;
-  PWMD3.tim->DIER |= TIM_DIER_CC3DE | TIM_DIER_CC1DE | TIM_DIER_UDE;
-  dmaStreamEnable(STM32_DMA1_STREAM3);
-  dmaStreamEnable(STM32_DMA1_STREAM6);
-  dmaStreamEnable(STM32_DMA1_STREAM2);
-  // all systems go! both timers and all channels are configured to resonate
-  // in complete sync without any need for CPU cycles (only DMA and timers)
-  // start pwm2 for system to start resonating
-  PWMD2.tim->CR1 |= TIM_CR1_CEN;
-}
-
-void ledDriverWaitCycle(void){
-  while (PWMD2.tim->CNT < 90 * sLeds * 24 / 90){chThdSleepMicroseconds(1);};
-}
-
-void testPatternFB(uint8_t *fb){
-  int i;
-  Color tmpC = {rand()%256, rand()%256, rand()%256};
-  for (i=0;i<sLeds;i++){
-    setColorRGB(tmpC,fb+24*i, sMask);
-  }
+  /* MOSI pin*/
+  palSetPadMode(PORT_WS2812, PIN_WS2812, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
+  leds_init();
 }
 
 void ws2812_setleds(LED_TYPE *ledarray, uint16_t number_of_leds) {
--- drivers/arm/ws2812.h
+++ drivers/arm/ws2812.h
@@ -1,30 +1,23 @@
-/*
- * LEDDriver.h
- *
- *  Created on: Aug 26, 2013
- *      Author: Omri Iluz
- */
-
-#ifndef WS2812_H_
-#define WS2812_H_
+#pragma once
 
 #include "hal.h"
 #include "rgblight_types.h"
 
 #define sign(x) (( x > 0 ) - ( x < 0 ))
 
-typedef struct Color Color;
-struct Color {
-  uint8_t R;
-  uint8_t G;
-  uint8_t B;
-};
+typedef struct {
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+} rgb_color;
 
-void ledDriverInit(int leds, stm32_gpio_t *port, uint32_t mask, uint8_t **o_fb);
-void setColorRGB(Color c, uint8_t *buf, uint32_t mask);
-void testPatternFB(uint8_t *fb);
-void ledDriverWaitCycle(void);
 
+void set_leds_color_rgb(rgb_color color);
+void set_led_color_rgb(rgb_color color, int pos);
+void leds_init(void);
+
+
+// This is what users will use to interface with this
 void ws2812_setleds(LED_TYPE *ledarray, uint16_t number_of_leds);
 void ws2812_setleds_rgbw(LED_TYPE *ledarray, uint16_t number_of_leds);
 
@@ -34,4 +27,3 @@ void WS2812_set_color( uint8_t index, uint8_t red, uint8_t green, uint8_t blue )
 void WS2812_set_color_all( uint8_t red, uint8_t green, uint8_t blue );
 void WS2812_send_colors(void);
 
-#endif /* LEDDRIVER_H_ */
