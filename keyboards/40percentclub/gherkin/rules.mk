e6ddc971d0873f741f9a36f6fe82c5e01fd1930e
zvecr <z.zvecr@gmail.com>
2018-11-12 20:02:28 +0000

    Tidy 40percentclub boards (#4403)
    * Remove unnecessary re-definitions for XXXXXXX and _______
    
    * Update 6lit to use KC_NO in layout macros
    
    * Update rules.mk to follow current template
    
    * Remove use of deprecated function action_get_macro - unused and contents were from old template
    
    * Remove use of deprecated function action_get_macro - all code was commented out
    
    * Remove use of deprecated function action_get_macro - convert macro to use process_record_user
    
    * Convert keymap to consistent use of _______
    
    * fix use of old style header guards
    
    * Swap KC_NO for XXXXXXX macro since the swap to the macro for KC_TRANS
    
    * Resolve use of ?= in rules.mk files
    
    * Remove duplication of rules.mk and config.h in gherkin mjt keymap
    
    * Remove unnecessary #includes
    
    * Align layout macros so foobar can use gherkin keymaps
    
    * Align 6lit layout macros with foobar
    
    * Remove ifndef QUANTUM_DIR from keymaps

--- keyboards/40percentclub/gherkin/rules.mk
+++ keyboards/40percentclub/gherkin/rules.mk
@@ -33,25 +33,40 @@ ARCH = AVR8
 #     CPU clock adjust registers or the clock division fuses), this will be equal to F_CPU.
 F_USB = $(F_CPU)
 
-# Bootloader
-#     This definition is optional, and if your keyboard supports multiple bootloaders of
-#     different sizes, comment this out, and the correct address will be loaded
-#     automatically (+60). See bootloader.mk for all options.
-BOOTLOADER = caterina
-
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
+
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   atmega32a    bootloadHID
+BOOTLOADER = caterina
+
+
+# If you don't know the bootloader type, then you can specify the
+# Boot Section Size in *bytes* by uncommenting out the OPT_DEFS line
+#   Teensy halfKay      512
+#   Teensy++ halfKay    1024
+#   Atmel DFU loader    4096
+#   LUFA bootloader     4096
+#   USBaspLoader        2048
+# OPT_DEFS += -DBOOTLOADER_SIZE=4096
+
 
 # Build Options
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
-CONSOLE_ENABLE = no	# Console for debug(+400)
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+MOUSEKEY_ENABLE = yes	  # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes	  # Audio control and System control(+450)
+CONSOLE_ENABLE = no	    # Console for debug(+400)
+COMMAND_ENABLE = no     # Commands for debug and configuration
+SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes		    # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = no
--- keyboards/40percentclub/ut47/config.h
+++ keyboards/40percentclub/ut47/config.h
@@ -15,8 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #include "config_common.h"
 
@@ -82,6 +81,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
         } while(0)
 #   else
 #       error "USART configuration is needed."
-#endif
-
 #endif
