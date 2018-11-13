67d1f93e680c092e1a7a7f5f617b93d9cd8b6968
Drashna Jaelre <drashna@live.com>
2018-11-13 10:45:26 -0800

    Update bjohnson keymaps

--- keyboards/quefrency/keymaps/bjohnson/config.h
+++ keyboards/quefrency/keymaps/bjohnson/config.h
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 #define FORCE_NKRO
+#define RETRO_TAPPING
 
 #define TAPPING_TOGGLE 1
 
