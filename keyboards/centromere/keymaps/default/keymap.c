b060de3ad6d689e560560ef209f88f3f183d2623
SouthPawEngineer <33986522+SouthPawEngineer@users.noreply.github.com>
2018-11-17 11:21:56 -0700

    Update keymap.c
    Update to new v2 Centromere

--- keyboards/centromere/keymaps/default/keymap.c
+++ keyboards/centromere/keymaps/default/keymap.c
@@ -25,36 +25,39 @@ enum centromere_keycodes
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = { /*QWERTY*/
+	{XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,     XXXXXXX,       XXXXXXX },
 	{KC_ESC,    KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,   XXXXXXX,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSPC },
 	{KC_TAB,    KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,   XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT },
 	{KC_LSFT,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,   XXXXXXX,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_ENT  },
-	{XXXXXXX,   XXXXXXX,   KC_LALT, LOWER,  XXXXXXX,  KC_ENT,  XXXXXXX,   XXXXXXX,   KC_SPC,  XXXXXXX,   RAISE, KC_LCTL, XXXXXXX,      XXXXXXX},
-	{XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,     XXXXXXX,       XXXXXXX }
+	{XXXXXXX,   XXXXXXX,   KC_LALT, LOWER,  XXXXXXX,  KC_ENT,  XXXXXXX,   XXXXXXX,   KC_SPC,  XXXXXXX,   RAISE, KC_LCTL, XXXXXXX,      XXXXXXX}
+
 }, 
 
 
 [_LOWER] = {/*LOWER*/
+  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,   XXXXXXX},
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX,   XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL   },
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX,   XXXXXXX,  KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE },
   {KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  XXXXXXX,   XXXXXXX, KC_F12,S(KC_NUHS),S(KC_NUBS),KC_VOLD, KC_VOLU, KC_MPLY},
-  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , KC_DEL, XXXXXXX,   XXXXXXX, KC_DEL, XXXXXXX , XXXXXXX,    XXXXXXX, XXXXXXX,   XXXXXXX},
-  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,   XXXXXXX}
+  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , KC_DEL, XXXXXXX,   XXXXXXX, KC_DEL, XXXXXXX , XXXXXXX,    XXXXXXX, XXXXXXX,   XXXXXXX}
+ 
 }, 
 
 [_RAISE] = {/*RAISE*/
+  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,   XXXXXXX},
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   XXXXXXX,   XXXXXXX,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL },
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  XXXXXXX,   XXXXXXX,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS },
   {KC_F7, KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12, XXXXXXX,   XXXXXXX,  KC_NUHS,  KC_NUBS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT  },
-  {XXXXXXX, XXXXXXX, _______, _______, KC_HOME, _______,XXXXXXX,   XXXXXXX, _______, KC_END, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX   },
-  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,   XXXXXXX}
+  {XXXXXXX, XXXXXXX, _______, _______, KC_HOME, _______,XXXXXXX,   XXXXXXX, _______, KC_END, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX   }
+  
 },
 
 [_ADJUST] = {/*ADJUST*/
+  {_______, _______, _______, _______, _______, _______,  XXXXXXX,   XXXXXXX,_______, _______, _______, _______, _______, _______   },
   {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    XXXXXXX,   XXXXXXX,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
   {_______, RESET  , _______, _______, _______, _______,  XXXXXXX,   XXXXXXX, _______, _______, _______, _______, _______,   KC_DEL },
   {_______, _______, _______, AU_ON,   AU_OFF,  _______,  XXXXXXX,   XXXXXXX, _______, _______,  _______, _______,  _______, _______},
-  {_______, _______, _______, _______, _______, _______,  XXXXXXX,   XXXXXXX, _______, _______, _______,  KC_HOME, XXXXXXX, XXXXXXX  },
-  {_______, _______, _______, _______, _______, _______,  XXXXXXX,   XXXXXXX,_______, _______, _______, _______, _______, _______   }
+  {_______, _______, _______, _______, _______, _______,  XXXXXXX,   XXXXXXX, _______, _______, _______,  KC_HOME, XXXXXXX, XXXXXXX  }
 },
 
 
