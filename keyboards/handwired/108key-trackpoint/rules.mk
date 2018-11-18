639757a10ac13573ec07e7d09137bd82553a6f0d
mkem114 <20897849+mkem114@users.noreply.github.com>
2018-11-18 21:08:54 +1300

    Merge rules

--- keyboards/handwired/108key-trackpoint/rules.mk
+++ keyboards/handwired/108key-trackpoint/rules.mk
@@ -1,22 +1,23 @@
-MCU = atmega32u4
+MCU = at90usb1286
 F_CPU = 16000000
 ARCH = AVR8
 F_USB = $(F_CPU)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
-OPT_DEFS += -DBOOTLOADER_SIZE=512
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
+OPT_DEFS += -DBOOTLOADER_SIZE=4096
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = yes        # Console for debug(+400)
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+CONSOLE_ENABLE ?= no        # Console for debug(+400)
+COMMAND_ENABLE ?= no        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-NKRO_ENABLE = no            # USB Nkey Rollover
+NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 MIDI_ENABLE = no            # MIDI controls
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
+RGBLIGHT_ENABLE ?= no
 
 PS2_MOUSE_ENABLE = yes
 PS2_USE_USART = yes
-
+k
