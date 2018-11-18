09ed7799f7bd704bc54583369bcf5365405a9372
Charlie McMackin <charliemac@gmail.com>
2018-11-18 07:42:06 -0600

    Remove legacy fn_actions in favor of MO(1)

--- keyboards/bpiphany/pegasushoof/keymaps/default_jis/keymap.c
+++ keyboards/bpiphany/pegasushoof/keymaps/default_jis/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
                              KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,                   KC_DEL, KC_END, KC_PGDN, \
                              KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,KC_ENT, \
                              KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RO,  KC_RSFT,                           KC_UP, \
-                             KC_LCTL, KC_LGUI,KC_LALT,KC_MHEN,        KC_SPC,                 KC_HENK,KC_KANA,KC_RALT,KC_FN0, KC_RCTL,                   KC_LEFT,KC_DOWN,KC_RGHT),
+                             KC_LCTL, KC_LGUI,KC_LALT,KC_MHEN,        KC_SPC,                 KC_HENK,KC_KANA,KC_RALT,MO(1),  KC_RCTL,                   KC_LEFT,KC_DOWN,KC_RGHT),
    /* Layer 1: Function layer */
    [KM_MEDIA] = LAYOUT_tkl_jis(_______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,           KC_WAKE,KC_PWR, KC_SLEP, \
                                _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   _______,_______,KC_VOLU, \
@@ -38,10 +38,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
                                _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,                           KC_MPLY, \
                                _______,_______,_______,_______,        _______,                _______,_______,_______,_______,RESET  ,                   KC_MPRV,KC_MSTP,KC_MNXT)
   };
-
-const uint16_t PROGMEM fn_actions[] = {
-  [0] = ACTION_LAYER_MOMENTARY(KM_MEDIA)
-};
 
 void led_set_user(uint8_t usb_led) {
   if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
