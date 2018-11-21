a0322fbe165565742d94cf2f0ed2baf85b879101
wanleg <wanleg@github.com>
2018-11-21 14:20:04 -0800

    commenting out unused placeholders

--- layouts/community/ortho_5x15/wanleg/rules.mk
+++ layouts/community/ortho_5x15/wanleg/rules.mk
@@ -2,10 +2,10 @@
 #or
 #ifeq (,$(findstring xd75,$(KEYBOARD)))
 #something
-endif
+#endif
 
-ifeq ($(strip $(KEYBOARD)), 40percentclub/5x5)
+#ifeq ($(strip $(KEYBOARD)), 40percentclub/5x5)
 #or
 #ifeq (,$(findstring 40percentclub/5x5,$(KEYBOARD)))
 #something
-endif
+#endif
