b84be39a4cda503eec9ddd3daa446c17005bf600
U-DESKTOP-7JNCQ96\Wilfred <wilfredpinos@yahoo.com>
2018-11-20 23:24:42 -0500

    adding changes from the last 2 weeks to my fork

---
+++ keyboards/melody96/keymaps/wilfred96/config.h
@@ -0,0 +1,14 @@
+
+#pragma once
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+
+#include "../../config.h"
+
+//overrides
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+#define TAPPING_TERM 175
+
+#endif
