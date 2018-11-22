2436cd546fca917ac42387a26db61f01cf0d34a4
Nico Hormazabal <nicolas.hormazabal@schibsted.com>
2018-11-22 09:50:04 +0100

    moved F keys

--- keyboards/planck/keymaps/dr_notsokind/keymap.c
+++ keyboards/planck/keymaps/dr_notsokind/keymap.c
@@ -129,14 +129,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |LShift|  F6  |  F7  |  F8  |  F9  |  F10 |      |ISO # |ISO / | NULL | PgUp | Enter|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |  F11 |      |      F12    |      |      | Home | PgDn |  End |
+ * |      |  F11 |  F12 |      |      |             |      |      | Home | PgDn |  End |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {KC_GRV ,  KC_1,    KC_2,   KC_3,     KC_4,  KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL},
-  {KC_DEL ,  KC_F1,   KC_F2,  KC_F3,    KC_F4, KC_F5,  _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {KC_LSFT,  KC_F6,   KC_F7,  KC_F8,    KC_F9, KC_F10, _______, KC_NUHS, KC_NUBS, XXXXXXX, KC_PGUP, KC_ENT },
-  {_______, _______, _______, KC_F11, _______, KC_F12,  KC_F12, _______, _______, KC_HOME, KC_PGDN, KC_END }
+  {KC_GRV ,  KC_1,   KC_2,      KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL},
+  {KC_DEL ,  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
+  {KC_LSFT,  KC_F6,  KC_F7,    KC_F8,   KC_F9,  KC_F10, _______, KC_NUHS, KC_NUBS, XXXXXXX, KC_PGUP, KC_ENT },
+  {_______,  KC_F11, KC_F12, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END }
 },
 
 /* LOWER
