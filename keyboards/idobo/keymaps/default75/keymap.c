f566ccc211a379f898f126095a99431792a557c9
Milton Griffin <griffinmilton3@gmail.com>
2018-11-21 20:43:06 -0500

    Added default75. No lighting.

--- keyboards/idobo/keymaps/default75/keymap.c
+++ keyboards/idobo/keymaps/default75/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 GriffinMJ
+/* Copyright 2018 Milton Griffin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
