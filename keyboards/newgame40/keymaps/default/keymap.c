d3d5939a0892f05b2aade31b936cb38c782efa75
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2018-11-23 21:32:42 +0900

    Update keyboards/newgame40/keymaps/default/keymap.c
    Co-Authored-By: GoTakigawa <44800276+GoTakigawa@users.noreply.github.com>

--- keyboards/newgame40/keymaps/default/keymap.c
+++ keyboards/newgame40/keymaps/default/keymap.c
@@ -148,7 +148,7 @@
   * |Shift |  GUI |Lower | Ctrl |Space | Bksp |Enter |Raise | Left |Right |
   * `---------------------------------------------------------------------'
   */
- [_EUCALYN] = LAYOUT( \
+ [_EUCALYN] = LAYOUT_ortho_4x10( \
    KC_Q,     KC_W,      KC_COMM,   KC_DOT,    KC_SCLN,     KC_M,     KC_R,     KC_D,     KC_Y,      KC_P, \
    KC_A,     KC_O,      KC_E,      KC_I,      KC_U,        KC_G,     KC_T,     KC_K,     KC_S,      KC_N, \
    KC_Z,     KC_X,      KC_C,      KC_V,      KC_F,        KC_B,     KC_H,     KC_J,     KC_L,      KC_SLSH, \
