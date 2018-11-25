907e6640bfb0e618095236f4a51c6930e3cd4d77
listofoptions <listofoptions@gmail.com>
2018-11-25 16:17:22 -0500

    fixed a but in the layout (reversed key order in row)

--- keyboards/converter/5291/5291.h
+++ keyboards/converter/5291/5291.h
@@ -40,30 +40,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     KEY_F7,KEY_F8,  KEY_LSFS,KEY_BSLS,KEY_Z,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_M,KEY_COMM,KEY_DOT, KEY_SLS,KEY_RSFS, KEY_PAST,KEY_PAD1,KEY_PAD2, KEY_PAD3,KEY_PPLS,  \
     KEY_F9,KEY_F10, KEY_LALT,                              KEY_SPACE,                                     KEY_CLOCK,         KEY_PAD0,          KEY_PDOT            \
 ) \
-{ {KEY_3,    KEY_W,   KEY_S,   KEY_Z     }\
-, {KEY_4,    KEY_E,   KEY_D,   KEY_X     }\
-, {KEY_5,    KEY_R,   KEY_F,   KEY_C     }\
-, {KEY_6,    KEY_T,   KEY_G,   KEY_V     }\
-, {KEY_7,    KEY_Y,   KEY_H,   KEY_B     }\
-, {KEY_8,    KEY_U,   KEY_J,   KEY_N     }\
-, {KEY_NLOCK,KEY_PAD8,KEY_PAD5,KEY_PAD2  }\
-, {KEY_9,    KEY_I,   KEY_K,   KEY_M     }\
-, {KEY_0,    KEY_O,   KEY_L,   KEY_COMM  }\
-, {KEY_MIN,  KEY_P,   KEY_SEMI,KEY_DOT   }\
-, {KEY_EQU,  KEY_LBRC,KEY_QUOT,KEY_SLS   }\
-, {KEY_RBRC, KEY_TICK,KEY_RSFS,KEY_SPACE }\
-, {KEY_BACK, KEY_ENTR,KEY_PAST,KEY_CLOCK }\
-, {KEY_PAD7, KEY_PAD4,KEY_PAD1,KEY_PAD0  }\
-, {KEY_SLOCK,KEY_PMIN,KC_NO,   KEY_PPLS  }\
-, {KEY_PAD9, KEY_PAD6,KEY_PAD3,KEY_PDOT  }\
-, {KEY_2,    KEY_Q,   KEY_A,   KEY_BSLS  }\
-, {KEY_1,    KC_NO,   KC_NO,   KEY_LALT  }\
-, {KEY_F1,   KEY_F3,  KEY_F5,  KEY_F7    }\
-, {KEY_F2,   KEY_F4,  KEY_F6,  KEY_F8    }\
-, {KC_NO,    KC_NO,   KC_NO,   KEY_F10   }\
-, {KC_NO,    KC_NO,   KC_NO,   KEY_F9    }\
-, {KEY_ESC,  KEY_TAB, KEY_LCTR,KEY_LSFS  }\
-, {KC_NO,    KC_NO,   KC_NO,   KC_NO     }\
+{ {KEY_Z,     KEY_S,   KEY_W,   KEY_3     }\
+, {KEY_X,     KEY_D,   KEY_E,   KEY_4     }\
+, {KEY_C,     KEY_F,   KEY_R,   KEY_5     }\
+, {KEY_V,     KEY_G,   KEY_T,   KEY_6     }\
+, {KEY_B,     KEY_H,   KEY_Y,   KEY_7     }\
+, {KEY_N,     KEY_J,   KEY_U,   KEY_8     }\
+, {KEY_PAD2,  KEY_PAD5,KEY_PAD8,KEY_NLOCK }\
+, {KEY_M,     KEY_K,   KEY_I,   KEY_9     }\
+, {KEY_COMM,  KEY_L,   KEY_O,   KEY_0     }\
+, {KEY_DOT,   KEY_SEMI,KEY_P,   KEY_MIN   }\
+, {KEY_SLS,   KEY_QUOT,KEY_LBRC,KEY_EQU   }\
+, {KEY_SPACE, KEY_RSFS,KEY_TICK,KEY_RBRC  }\
+, {KEY_CLOCK, KEY_PAST,KEY_ENTR,KEY_BACK  }\
+, {KEY_PAD0,  KEY_PAD1,KEY_PAD4,KEY_PAD7  }\
+, {KEY_PPLS,  KC_NO,   KEY_PMIN,KEY_SLOCK }\
+, {KEY_PDOT,  KEY_PAD3,KEY_PAD6,KEY_PAD9  }\
+, {KEY_BSLS,  KEY_A,   KEY_Q,   KEY_2     }\
+, {KEY_LALT,  KC_NO,   KC_NO,   KEY_1     }\
+, {KEY_F7,    KEY_F5,  KEY_F3,  KEY_F1    }\
+, {KEY_F8,    KEY_F6,  KEY_F4,  KEY_F2    }\
+, {KC_F10,    KC_NO,   KC_NO,   KC_NO     }\
+, {KC_F9,     KC_NO,   KC_NO,   KC_NO     }\
+, {KEY_LSFS,  KEY_LCTR,KEY_TAB, KEY_ESC   }\
+, {KC_NO,     KC_NO,   KC_NO,   KC_NO     }\
 }
 
 #define LAYOUT LAYOUT_5291
