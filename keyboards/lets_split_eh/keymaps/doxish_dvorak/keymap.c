e81a9ec1fdd98d177591330766c5e08e7871852a
zvecr <z.zvecr@gmail.com>
2018-11-26 18:34:55 +0000

    Keymap build fixes now that ortho_4x12 is enabled

--- keyboards/lets_split_eh/keymaps/doxish_dvorak/keymap.c
+++ keyboards/lets_split_eh/keymaps/doxish_dvorak/keymap.c
@@ -1,7 +1,4 @@
 #include QMK_KEYBOARD_H
-// #include "eeconfig.h"
-//
-// extern keymap_config_t keymap_config;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
