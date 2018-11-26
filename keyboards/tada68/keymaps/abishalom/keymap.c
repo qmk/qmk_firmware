a4322965b93ab5db2a7e48197c9b907748dbf65a
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-11-26 18:40:40 -0500

    Update keyboards/tada68/keymaps/abishalom/keymap.c
    Co-Authored-By: abishalom <abishalom1997@gmail.com>

--- keyboards/tada68/keymaps/abishalom/keymap.c
+++ keyboards/tada68/keymaps/abishalom/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[_BL] = KEYMAP_ANSI(
+[_BL] = LAYOUT_ansi(
   KC_GESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
   LT(_FL, KC_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   SEMI_FN,KC_QUOT,         KC_ENT,KC_PGUP,  \
