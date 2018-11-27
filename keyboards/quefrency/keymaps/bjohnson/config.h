ecd21b44a8efd5c7241ebf48c65fc7b30134865f
Drashna Jaelre <drashna@live.com>
2018-11-26 16:45:24 -0800

    Update to drashna userspace and keymaps (#4459)
    * Fix reversed bool check in layer_state_set
    
    * Add Quefrency 65 for a friend
    
    * Add Ergodox EZ Glow keymap
    
    * Add RGB Matrix Code
    
    * Further changes to rgb matrix ErgoDox EZ
    
    * Update bjohnson keymaps
    
    * Fix CRKBD display
    
    * Overhaul to corne keyboard
    
    * Narrow scope for keylogger
    
    * Minor layout tweaks to Corne Keyboard
    
    * additional CRKBD tweaks
    
    * Minor tweaks to CRKBD
    
    * Add all characters for keylogger
    
    * Ergodox EZ Glow overhaul
    
    * Fix Ergodox EZ Glow layer colors
    
    * Increase Tapping Term for Corne Keyboard
    
    * Fix unicode-ish
    
    * Revert some changes
    
    * Add layer specific lighting effects
    
    * Some minor tweaks to ergodox glow config
    
    * revert changes to ergodox files
    
    * Update Glow readme
    
    * Add more tapping term defines
    
    * Fix changes
    
    * Fix ergodox keymap
    
    * Hopefully fix sleeping
    
    * Disable layer indications if rgb matrix is disabled
    
    * Add support for sleeping and rgb layer change toggle to ergodox ez glow
    
    * Make RGB Layer Indication Great Again
    
    * Make Unicode Great Again
    
    * Remove placeholder define
    
    Co-Authored-By: drashna <drashna@live.com>
    
    * Remove placeholder define
    
    Co-Authored-By: drashna <drashna@live.com>
    
    * Remove old EEPROM Reset keycode

--- keyboards/quefrency/keymaps/bjohnson/config.h
+++ keyboards/quefrency/keymaps/bjohnson/config.h
@@ -3,6 +3,7 @@ This is the c configuration file for the keymap
 
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
+Copyright 2018 Danny Nguyen <danny@keeb.io>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,31 +21,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+/* Use I2C or Serial, not both */
 
-/* Select hand configuration */
+#define USE_SERIAL
+// #define USE_I2C
 
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
-#define EE_HANDS
+#define TAPPING_TERM 500
 
-#define SSD1306OLED
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#define FORCE_NKRO
+#define RETRO_TAPPING
 
-#define USE_SERIAL_PD2
+#define TAPPING_TOGGLE 1
 
-// #define TAPPING_FORCE_HOLD
-// #define TAPPING_TERM 100
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
 
-#ifdef RGBLIGHT_ENABLE
-#undef RGBLED_NUM
-#define RGBLED_NUM 27
-
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#define RGBLIGHT_LIMIT_VAL 100
-#endif
-
-#ifdef AUDIO_ENABLE
-#define B6_AUDIO
-// #define NO_MUSIC_MODE
-#endif
+#define RGBLIGHT_SLEEP
