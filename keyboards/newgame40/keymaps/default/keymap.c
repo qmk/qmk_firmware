2f9bce2824b1b8b27d0c40bc5151a3e3fa6473b5
GO <44800276+GoTakigawa@users.noreply.github.com>
2018-11-27 21:21:04 +0900

    Update keymap.c

--- keyboards/newgame40/keymaps/default/keymap.c
+++ keyboards/newgame40/keymaps/default/keymap.c
@@ -18,17 +18,15 @@
 
  extern keymap_config_t keymap_config;
 
- // Each layer gets a name for readability, which is then used in the keymap matrix below.
- // The underscores don't mean anything - you can have a layer called STUFF or any other name.
- // Layer names don't all need to be of the same length, obviously, and you can also skip them
- // entirely and just use numbers.
- #define _QWERTY 0
- #define _COLEMAK 1
- #define _DVORAK 2
- #define _LOWER 3
- #define _RAISE 4
- #define _EUCALYN 5
- #define _ADJUST 16
+enum layers {
+  _QWERTY,
+  _COLEMAK,
+  _DVORAK,
+  _EUCALYN,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
 
  enum custom_keycodes {
    QWERTY = SAFE_RANGE,
