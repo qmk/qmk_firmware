c1326d6daf9a18228139b7578df24728962bf883
jtcarrasco <5018598+jtcarrasco@users.noreply.github.com>
2018-11-28 15:16:21 -0800

    Update config.h

--- keyboards/idobo/keymaps/Revok75/config.h
+++ keyboards/idobo/keymaps/Revok75/config.h
@@ -13,14 +13,3 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#pragma once
-
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "../../config.h"
-// place overrides here
-#undef BACKLIGHT_BREATHING
-
-#endif
