78e0e1983e1cb78ad29cff5029626928f81c74d3
Eric Setiawan <ricky.ason@gmail.com>
2018-11-30 19:36:48 +0700

    asd

--- keyboards/tada68/keymaps/angelbirth/keymap.c
+++ keyboards/tada68/keymaps/angelbirth/keymap.c
@@ -8,7 +8,8 @@
 #define _FL 1
 
 qk_tap_dance_action_t tap_dance_actions[]={
-		[0]=ACTION_TAP_DANCE_DOUBLE(KC_GRV,KC_ESC)
+		[0]=ACTION_TAP_DANCE_DOUBLE(KC_GRV,KC_ESC),
+    [1]=ACTION_TAP_DANCE_DOUBLE(KC_LSFT,KC_CAPS)
 };
 
 
@@ -31,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TD(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_HOME, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_PGUP, \
   KC_BSPC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGDN,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_END, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,MO(_FL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LSFT,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_END, \
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,LT(_FL,KC_APP),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -49,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = LAYOUT_ansi(
   KC_ESC, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_PSCREEN ,  \
-  KC_TRNS,XXX, XXX,XXX,XXX, XXX,XXX,XXX,XXX,XXX,XXX,KC_MPRV,KC_MNXT, KC_MPLY,KC_PAUSE, \
-  KC_TRNS,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,        KC_TRNS,KC_MSTP, \
+  KC_TRNS,XXX, KC_UP,XXX,XXX, XXX,XXX,XXX,XXX,XXX,XXX,KC_MPRV,KC_MNXT, KC_MPLY,KC_PAUSE, \
+  KC_TRNS,KC_LEFT,KC_DOWN,KC_RIGHT,XXX,XXX,XXX,XXX,XXX,XXX,XXX,XXX,        KC_TRNS,KC_MSTP, \
   KC_TRNS,XXX,XXX,BL_DEC, BL_TOGG,BL_INC, XXX,KC_MUTE,KC_VOLD,KC_VOLU,XXX,KC_BTN1, KC_WH_U, KC_BTN2, \
-  KC_TRNS,KC_TRNS,KC_TRNS,                 KC_TRNS,               KC_TRNS,KC_TRNS,KC_TRNS,KC_WH_L,KC_WH_D, KC_WH_R),
+  KC_TRNS,RESET,KC_TRNS,                 KC_TRNS,               KC_TRNS,KC_TRNS,KC_APP,KC_WH_L,KC_WH_D, KC_WH_R)
 };
