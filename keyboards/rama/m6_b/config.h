65839cdf7e1e593b9afb0f78f95402e689cc5cdf
Wilba <Jason.S.Williams@gmail.com>
2018-12-01 03:43:34 +1100

    Added macros to Dynamic Keymaps, Zeal60 RGB backlight improvements (#4520)
    * Refactored M6-B to use Zeal60 RGB backlight code
    
    * Fixed M6-B LED co-ordinates
    
    * Minor changes to RGB config for Zeal65
    
    * Added dynamic keymaps to WT80-A, WT60-A, WT-80A, U80-A
    
    * Macro implementation
    
    * Implemented macros, API protocol version 8, RGB backlight fixes
    
    * Improved radial effects for M6-B
    
    * Fixed undefined references when building an RGB keyboard after M6-A

--- keyboards/rama/m6_b/config.h
+++ keyboards/rama/m6_b/config.h
@@ -150,11 +150,14 @@
 // Bump this every time we change what we store
 // This will automatically reset the EEPROM with defaults
 // and avoid loading invalid data from the EEPROM
-#define EEPROM_VERSION 0x07
+#define EEPROM_VERSION 0x08
 #define EEPROM_VERSION_ADDR 34
 
 // Backlight config starts after EEPROM version
 #define RGB_BACKLIGHT_CONFIG_EEPROM_ADDR 35
-// Dynamic keymap starts after backlight config (35+37)
-#define DYNAMIC_KEYMAP_EEPROM_ADDR 72
-
+// Dynamic keymap starts after backlight config (35+43)
+#define DYNAMIC_KEYMAP_EEPROM_ADDR 78
+// Dynamic macro starts after dynamic keymaps (78+(4*6*2)) = (78+48)
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_ADDR 126
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 898
+#define DYNAMIC_KEYMAP_MACRO_COUNT 16
