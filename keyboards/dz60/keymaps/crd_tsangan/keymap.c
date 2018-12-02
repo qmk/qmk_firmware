6ad23f976ab187c8d0747fbac4c50740e6ad0dbe
crd <jason@jobrapp.com>
2018-12-01 23:56:06 -0800

    Resort to native key aliases when they exist

--- keyboards/dz60/keymaps/crd_tsangan/keymap.c
+++ keyboards/dz60/keymaps/crd_tsangan/keymap.c
@@ -10,8 +10,7 @@ enum keyboard_layers {
 #define KC_RSUP RSFT_T(KC_UP)
 #define KC_RGLT RGUI_T(KC_LEFT)
 #define KC_RADN RALT_T(KC_DOWN)
-#define KC_RITE KC_RIGHT
-#define KC_PAWS KC_PAUSE
+#define KC_RCRT RCTL_T(KC_RIGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
@@ -19,12 +18,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 		KC_CTES, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSUP, MO(_FL),
-		KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGLT, KC_RADN, KC_RCTL
+		KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGLT, KC_RADN, KC_RCRT
 	),
 	[_FL] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
 		RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
 		_______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, KC_MUTE, _______, _______, KC_PGDN, KC_PGUP, KC_DEL,
-		_______, KC_LEFT, KC_DOWN, KC_RITE, _______, _______, KC_SLCK, KC_VOLD, KC_VOLU, KC_PAWS, _______, _______, _______,
+		_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_SLCK, KC_VOLD, KC_VOLU, KC_PAUS, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______,                            _______,                            _______, _______, _______
 	)
