41717ea08f417edabcb2e0ec1e894a3492b91a00
Elijah Scheele <eli@zanycadence.com>
2018-12-05 20:05:28 -0800

    basic config of Hannah's keyboard setup!

--- keyboards/melody96/config.h
+++ keyboards/melody96/config.h
@@ -4,20 +4,26 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    YMDK
-#define PRODUCT         Melody96
-#define DESCRIPTION     Melody96
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x6060
+#define DEVICE_VER 0x0001
+#define MANUFACTURER YMDK
+#define PRODUCT Melody96
+#define DESCRIPTION Melody96
 
 /* key matrix size */
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 9
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { B7, B3, B2, B1, B0, E6, F0, F1, F4, F5, F6, F7 }
-#define MATRIX_COL_PINS { D0, D1, D2, D3, D5, D4, D6, D7, B4 }
+#define MATRIX_ROW_PINS                                \
+    {                                                  \
+        B7, B3, B2, B1, B0, E6, F0, F1, F4, F5, F6, F7 \
+    }
+#define MATRIX_COL_PINS                    \
+    {                                      \
+        D0, D1, D2, D3, D5, D4, D6, D7, B4 \
+    }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
@@ -27,6 +33,8 @@
 #define BACKLIGHT_PIN B6
 #ifdef BACKLIGHT_PIN
 #define BACKLIGHT_LEVELS 5
+#define BACKLIGHT_BREATHING
+#define BREATHING_PERIOD 6
 #endif
 
 /* Set 0 if debouncing isn't needed */
@@ -40,16 +48,15 @@
 
 /* key combination for command */
 #define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
 
-#define RGB_DI_PIN E2
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 18
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
+// #define RGB_DI_PIN E2
+// #ifdef RGB_DI_PIN
+// #define RGBLIGHT_ANIMATIONS
+// #define RGBLED_NUM 18
+// #define RGBLIGHT_HUE_STEP 8
+// #define RGBLIGHT_SAT_STEP 8
+// #define RGBLIGHT_VAL_STEP 8
+// #endif
 
 #endif
