1efb73668221f68a9e349d44a74dca3c7a65737a
milestogo <milestogo@users.noreply.github.com>
2018-12-06 15:53:54 -0800

    more include cleanup

--- keyboards/converter/palm_usb/config.h
+++ keyboards/converter/palm_usb/config.h
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #pragma once
-#define CONFIG_H
 
 #define CUSTOM_MATRIX 2
 
--- keyboards/converter/palm_usb/stowaway/stowaway.h
+++ keyboards/converter/palm_usb/stowaway/stowaway.h
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#define STOWAWAY_H
 
 #include "quantum.h"
 
