c9159effc0b07b44d90ea92de625b9d338938eb2
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2018-12-07 16:48:03 -0800

    handwired/MS_sculpt_mobile Refactor, Configurator and cleanup (#4576)
    * handwired/MS_sculpt_mobile: refactor
    
    - layout macro KEYMAP renamed to LAYOUT
    - layout macro MATRIX_TESTING_KEYMAP renamed to MATRIX_TESTING_LAYOUT
    
    * handwired/MS_sculpt_mobile: Configurator support
    
    * handwired/MS_sculpt_mobile: readme cleanup
    
    * handwired/MS_sculpt_mobile: rename folder
    
    Renamed the directory to fully lowercase.
    
    * handwired/MS_sculpt_mobile: rename keyboard files
    
    Renamed the keyboard core files to fully lowercase.
    
    * handwired/ms_sculpt_mobile: keymap readme cleanup
    
    Capitalization fixes because I'm that guy.
    
    * handwired/ms_sculpt_mobile: keymap config.h fixes
    
    Keymap config.h files updated to use #pragma once
    
    * handwired/ms_sculpt_mobile: updated paths
    
    Fixing path references due to rename.

--- keyboards/handwired/ms_sculpt_mobile/ms_sculpt_mobile.h
+++ keyboards/handwired/ms_sculpt_mobile/ms_sculpt_mobile.h
@@ -4,7 +4,7 @@
 #include "quantum.h"
 
 
-#define KEYMAP( \
+#define LAYOUT( \
                 k7Q, k6O, k6P,  k6Q, k5O, k5P, k5Q, k7A, k7B, k7C, k7D, k7E, k7F, k1O, k1K, k1L, \
                 k5A,  k5K,  k5L, k5M, k5N, k5H, k5I, k2A, k2B, k2C, k2D, k5B, k5C, k5J, k2E, \
                 k6R,  k6D,  k6E, k6F, k7I, k7J, k4A, k4B, k4C, k4D, k3A, k3B, k3C, k3D, \
@@ -24,7 +24,7 @@
 }
 
 
-#define MATRIX_TESTING_KEYMAP( \
+#define MATRIX_TESTING_LAYOUT( \
         k0A, k0B, k0C, k0D, k0E, k0F, k0G, k0H, k0I, k0, k0J, k0K, k0L, k0M, k0N, k0O, k0P, k0Q,\
         k1A, k1B, k1C, k1D, k1E, k1F, k1G, k1H, k1I, k1, k1J, k1K, k1L, k1M, k1N, k1O, k1P, k1Q,\
         k2A, k2B, k2C, k2D, k2E, k2F, k2G, k2H, k2I, k2, k2J, k2K, k2L, k2M, k2N, k2O, k2P, k2Q,\
