b0e99f50f6a056d07a90df5cf935c5e29f774d1b
noroadsleft <xxiinophobia@yahoo.com>
2018-12-13 18:04:10 -0800

    Cluecard: refactor
    - renamed layout macro KEYMAP to LAYOUT
    - reformatted layout macro to more closely resemble physical device layout
    - keymaps now use #include QMK_KEYBOARD_H
    - config.h files updated to use #pragma once method
    - deleted outdated QUANTUM_DIR code blocks from rules.mk files
    - white space changes on rules.mk files (alignment/readability)

--- keyboards/clueboard/card/card.h
+++ keyboards/clueboard/card/card.h
@@ -6,12 +6,12 @@
 // This a shortcut to help you visually see your layout.
 // The first section contains all of the arguements
 // The second converts the arguments into a two-dimensional array
-#define KEYMAP( \
-	k00, k01, k02, \
-	k10, k12, \
-	k20, k21, k22, \
-	k11, \
-	k30, k31, k32 \
+#define LAYOUT( \
+	k00,      k01,      k02, \
+	     k10,      k12,      \
+	k20,      k21,      k22, \
+	     k11,                \
+	          k30, k31, k32  \
 ) { \
 	{ k00, k01, k02, }, \
 	{ k10, k11, k12, }, \
