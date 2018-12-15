d850eda7ec391ec7e1d18cf850944f13bb523fdc
noroadsleft <xxiinophobia@yahoo.com>
2018-12-14 20:07:30 -0800

    Coding conventions fix in default keymap

--- keyboards/chimera_ortho/keymaps/default/keymap.c
+++ keyboards/chimera_ortho/keymaps/default/keymap.c
@@ -7,8 +7,7 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-enum chimera_ortho_layers
-{
+enum chimera_ortho_layers {
   _QWERTY,
   _CAPS,
   _NUMPAD,
