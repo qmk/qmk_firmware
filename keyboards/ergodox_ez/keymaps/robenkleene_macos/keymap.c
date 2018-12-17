0c36111afe0483c128509ee7902713439311a943
Roben Kleene <contact@robenkleene.com>
2018-12-17 11:07:42 -0500

    Finish layout

--- keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
+++ keyboards/ergodox_ez/keymaps/robenkleene_macos/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |  -   |           |  =   |   6  |   7  |   8  |   9  |   0  |  BS    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   |    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | CTRL |  Fn  |       |  Fn  | CTRL |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | BS   | ESC  |------|       |------|  Ret   |Space |
+ *                                 |Space | RET  |------|       |------|  Ret   |Space |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
 
 // Left Keyboard
-KC_EQL,    KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_NO,
+KC_ESC,    KC_1,    KC_2,     KC_3,     KC_4,     KC_5,  KC_MINS,
 KC_TAB,    KC_Q,    KC_W,     KC_E,     KC_R,     KC_T,  KC_RBRC,
 KC_LCTL,   KC_A,    KC_S,     KC_D,     KC_F,     KC_G,
 KC_LSFT,   KC_Z,    KC_X,     KC_C,     KC_V,     KC_B,  KC_LCTL,
@@ -43,10 +43,10 @@ MO(FCTN),  KC_GRV,  KC_LCTL,  KC_LALT,  KC_LGUI,
 // Left Cluster
 KC_LCTL,  MO(FCTN),
 KC_HOME,
-KC_BSPC,  KC_ESC,  KC_END,
+KC_SPC,   KC_ENT,    KC_END,
 
 // Right Keyboard
-KC_NO,    KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_MINS,
+KC_EQL,   KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_BSPC,
 KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSLS,
 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
 KC_RCTL,  KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,
