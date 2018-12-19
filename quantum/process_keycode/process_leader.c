5143005c75bfc69c6e230c6d798d41dd58f00bd0
Drashna Jaelre <drashna@live.com>
2018-12-18 18:03:20 -0800

    Remove customization

--- quantum/process_keycode/process_leader.c
+++ quantum/process_keycode/process_leader.c
@@ -38,29 +38,28 @@ uint8_t leader_sequence_size = 0;
 bool process_leader(uint16_t keycode, keyrecord_t *record) {
   // Leader key set-up
   if (record->event.pressed) {
-    if (leading) {
-      if (timer_elapsed(leader_time) < LEADER_TIMEOUT) {
-        leader_sequence[leader_sequence_size] = keycode;
-        leader_sequence_size++;
-        return false;
-      }
 #ifdef LEADER_PER_KEY_TIMING
-      else { leader_time = timer_read(); }
+    leader_time = timer_read();
 #endif
-    } else {
-      if (keycode == KC_LEAD) {
-        leader_start();
-        leading = true;
-        leader_time = timer_read();
-        leader_time = timer_read();
-        leader_sequence_size = 0;
-        leader_sequence[0] = 0;
-        leader_sequence[1] = 0;
-        leader_sequence[2] = 0;
-        leader_sequence[3] = 0;
-        leader_sequence[4] = 0;
-      }
-      break;
+    if (!leading && keycode == KC_LEAD) {
+      leader_start();
+      leading = true;
+#ifndef LEADER_PER_KEY_TIMING
+      leader_time = timer_read();
+#endif
+      leader_time = timer_read();
+      leader_sequence_size = 0;
+      leader_sequence[0] = 0;
+      leader_sequence[1] = 0;
+      leader_sequence[2] = 0;
+      leader_sequence[3] = 0;
+      leader_sequence[4] = 0;
+      return false;
+    }
+    if (leading && timer_elapsed(leader_time) < LEADER_TIMEOUT) {
+      leader_sequence[leader_sequence_size] = keycode;
+      leader_sequence_size++;
+      return false;
     }
   }
   return true;
