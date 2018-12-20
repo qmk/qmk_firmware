5dc2cf936e17aed71a54295e01eb9b90f2e610c3
takashiski <takashiskibb@gmail.com>
2018-12-20 23:22:09 +0900

    include split_common.h

--- keyboards/hecomi_alpha/hecomi_alpha.c
+++ keyboards/hecomi_alpha/hecomi_alpha.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "hecomi_alpha.h"
+#include "split_common.h"
 
 void matrix_init_kb(void) {
 	// put your keyboard start-up code here
