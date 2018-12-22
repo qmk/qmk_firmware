2f93922125e133deec3fb0db22585844e9b07c1e
wanleg <wanleg@github.com>
2018-12-21 17:49:50 -0800

    fixed 5x5 with cleave

--- layouts/community/ortho_5x15/wanleg/keymap.c
+++ layouts/community/ortho_5x15/wanleg/keymap.c
@@ -7,40 +7,118 @@
 
 #if defined(CLEAVE)
 #define LAYOUT_75_base( \
-    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
-    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, \
-    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, \
-	K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C  \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1a, K1b, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, \
+	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3a, K3b  \
   ) \
   LAYOUT_wrapper( \
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NLCK,  QWERTY75, GHERKIN75, FUNCTION75, \
-     K01,	K02,	K03,	K04,	K05,	K06, KC_7, KC_8, KC_9,		K07,	K08,	K09,	K0A,	K0B,	K0C, \
-     K11,	K12,	K13,	K14,	K15,	K16, KC_4, KC_5, KC_6,		K17,	K18,	K19,	K1A,	K1B,	K1C, \
-     K21,	K22,	K23,	K24,	K25,	K26, KC_1, KC_2, KC_3,		K27,	K28,	K29,	K2A,	K2B,	K2C, \
-     K31,	K32,	K33,	K34,	K35,	K36, KC_0, KC_0, KC_DOT,	K37,	K38,	K39,	K3A,	K3B,	K3C  \
+     K00, K01, K02, K03, K04, K05, KC_7, KC_8, KC_9,		K06, K07, K08, K09, K0a, K0b, \
+     K10, K11, K12, K13, K14, K15, KC_4, KC_5, KC_6,		K16, K17, K18, K19, K1a, K1b, \
+     K20, K21, K22, K23, K24, K25, KC_1, KC_2, KC_3,		K26, K27, K28, K29, K2a, K2b, \
+     K30, K31, K32, K33, K34, K35, KC_0, KC_0, KC_DOT,	    K36, K37, K38, K39, K3a, K3b  \
   )
   
 #else
 #define LAYOUT_75_base( \
-    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
-    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, \
-    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, \
-	K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C  \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1a, K1b, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, \
+	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3a, K3b  \
   ) \
   LAYOUT_wrapper( \
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NLCK,  QWERTY75, GHERKIN75, FUNCTION75, \
-     K01,	K02,	K03,	K04,	K05,	K06,	K07,	K08,	K09,	K0A,	K0B,	K0C, KC_7, KC_8, KC_9, \
-     K11,	K12,	K13,	K14,	K15,	K16,	K17,	K18,	K19,	K1A,	K1B,	K1C, KC_4, KC_5, KC_6, \
-     K21,	K22,	K23,	K24,	K25,	K26,	K27,	K28,	K29,	K2A,	K2B,	K2C, KC_1, KC_2, KC_3, \
-     K31,	K32,	K33,	K34,	K35,	K36,	K37,	K38,	K39,	K3A,	K3B,	K3C, KC_0, KC_0, KC_DOT\
+     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, KC_7, KC_8, KC_9, \
+     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1a, K1b, KC_4, KC_5, KC_6, \
+     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, KC_1, KC_2, KC_3, \
+     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3a, K3b, KC_0, KC_0, KC_DOT\
   )
 #endif  
 
 #define LAYOUT_75_base_wrapper(...)       LAYOUT_75_base(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+#if defined(KEYBOARD_40percentclub_5x5)
+[GK75] = LAYOUT_ortho_5x15_wrapper(
+  _________________Num_Row_75________________, QWERTY75, XXXXXXX, FUNCTION75,
+  _______________GherkinLike_0_______________, KC_KP_7, KC_KP_8, KC_KP_9,
+  _______________GherkinLike_1_______________, KC_KP_4, KC_KP_5, KC_KP_6,
+  TD(TD_SFT_CAPS), SFT_T(KC_Z), KC_X, KC_C, LT(NUM75, KC_V), LT(ETC75, KC_B), KC_N, LT(DIR75, KC_M), GUI_T(KC_COMM), ALT_T(KC_DOT), CTL_T(KC_BSPC), SFT_T(KC_ENT), KC_KP_1, KC_KP_2, KC_KP_3,
+  TD(RST_TAP_DANCE), GHERKIN50, KC_LALT, NUMPAD, NUMBER, ETCETERA, KC_SPC,DIRECTION, KC_RGUI, KC_RALT, KC_DEL, KC_RCTL, KC_KP_0, KC_KP_0, KC_KP_DOT
+),
 
- /* QWERTY 75
+[GK50] = LAYOUT_ortho_5x10_wrapper(
+  KC_ESC,    _______, _______, _______, _______, _______, _______, _______, _______,    KC_BSPC,
+  _______________Gherkin_Row_0_______________,
+  _______________Gherkin_Row_1_______________,
+  _______________Gherkin_Row_2_______________,
+  TD(RST_TAP_DANCE), GHERKIN75, NUMPAD, gNUMBER, gETCETERA, KC_SPC,gDIRECTION, KC_RGUI, KC_RALT, KC_RGUI
+),
+
+[gNUM] = LAYOUT_ortho_5x10_wrapper(
+  _________________BLANK_50__________________,
+  _______________Gherkin_NUM_0_______________,
+  _______________Gherkin_NUM_1_______________,
+  _______________Gherkin_NUM_2_______________,
+  _______, _______, _______, _______, _______, KC_ENT, KC_RSFT, KC_RGUI, _______, _______
+),
+
+[gDIR] = LAYOUT_ortho_5x10_wrapper(
+  _________________BLANK_50__________________,
+  _______________Gherkin_DIR_0_______________,
+  _______________Gherkin_DIR_1_______________,
+  _______________Gherkin_DIR_2_______________,
+  _________________BLANK_50__________________
+),
+
+[gETC] = LAYOUT_ortho_5x10_wrapper(
+  _________________BLANK_50__________________,
+  _______________Gherkin_ETC_0_______________,
+  _______________Gherkin_ETC_1_______________,
+  _______________Gherkin_ETC_2_______________,
+  _______, KC_CAPS, _______, _______, _______, LALT(LCTL(KC_DEL)), _______, _______, _______, _______
+),
+
+#if defined(BLUEFRUIT)
+[PAD] = LAYOUT_ortho_5x5_wrapper(
+  _______, _______, _______, OUT_BT, OUT_USB,
+  _______________NUMPAD_Row__0_______________, _______,
+  _______________NUMPAD_Row__1_______________, _______,
+  _______________NUMPAD_Row__2_______________, _______,
+  KC_KP_0, TD(LYR50_TAP_DANCE), KC_KP_DOT, KC_PMNS, _______
+),
+#else
+ [PAD] = LAYOUT_ortho_5x5_wrapper(
+  _______, _______, _______, _______, _______,
+  _______________NUMPAD_Row__0_______________, _______,
+  _______________NUMPAD_Row__1_______________, _______,
+  _______________NUMPAD_Row__2_______________, _______,
+  KC_KP_0, TD(LYR50_TAP_DANCE), KC_KP_DOT, KC_PMNS, _______
+),
+#endif
+#else
+ /* Gherkin 75
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | NUMLOCK| qwerty | gherkin| FN     |
+ * |-----------------------------------------------------------------------------------------------------------+--------+--------+--------|
+ * |                                                                                                           |   7    |   8    |   9    |
+ * |                                                                                                           |--------+--------+--------|
+ * |                                            4x12 GHERKIN LAYOUT                                            |   4    |   5    |   6    |
+ * |                                                                                                           |--------+--------+--------|
+ * |                                                                                                           |   1    |   2    |   3    |
+ * |                                                                                                           |--------+--------+--------|
+ * |                                                                                                           |   0    |   0    |   .    |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+[GK75] = LAYOUT_75_base_wrapper(
+  _______________GherkinLike_0_______________, 
+  _______________GherkinLike_1_______________, 
+  _______________GherkinLike_2_______________,
+  _______________GherkinLike_3_______________
+),
+#endif
+/* QWERTY 75
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | NUMLOCK| gherkin|        | FN     |
  * |-----------------------------------------------------------------------------------------------------------+--------+--------+--------|
