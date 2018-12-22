35e5631d4e43bfdd3c5980cf04af37052f338935
Gam3cat <tmk3ith@hotmail.com>
2018-12-21 17:37:01 -0800

    Added h87a keymap, updated m10a/jc65/gonnerd

---
+++ keyboards/h87a/keymaps/gam3cat/config.h
@@ -0,0 +1,9 @@
+#include "../../config.h"
+//Force NKRO to be enabled.
+#define FORCE_NKRO
+
+//GRAVE_ESC override for CTRL+SHIFT+ESC Windows task manager shortcut.
+//#define GRAVE_ESC_CTRL_OVERRIDE
+
+//Delay matrix scan for tap dance, reduce to activate modifier keys faster.
+//#define TAPPING_TERM 200
