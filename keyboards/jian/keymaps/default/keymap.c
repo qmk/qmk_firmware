676963b24e0632c8239151c1b3c769500acd1e0a
Юрий Махоткин <kgofhedgehogs@gmail.com>
2018-12-24 10:34:20 +0200

    removedrise lower toggles

--- keyboards/jian/keymaps/default/keymap.c
+++ keyboards/jian/keymaps/default/keymap.c
@@ -140,14 +140,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_UNDS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  F12_RGU, \
            PLS_LCT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, MIN_RCT, \
            EQL_LAL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-                                      _______, _______, TG_LWR,       TG_LWR,  _______, _______\
+                                      _______, _______, _______,      _______,  _______, _______\
 ),
 
 [_RAISE] = LAYOUT(\
   _______, KC_NLCK, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,      KC_VOLU, KC_HOME, KC_PSCR, KC_PGUP, KC_SLCK, KC_CAPS, _______, \
            EQL_LCT, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_UP,   KC_RGHT, KC_INS,  APP_RCT, \
            _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      KC_VOLD, KC_END,  KC_DOWN, KC_PGDN, KC_PAUS, _______, \
-                                      TG_RSE,  _______, _______,      _______, _______, TG_RSE\
+                                      _______, _______, _______,      _______, _______, _______\
 ),
 
 [_ADJUST] = SYMM_LAYOUT(\
