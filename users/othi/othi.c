e1e225c90514d0232131afc8b2cb78714335f720
Othi <mnpq.raven@gmail.com>
2018-12-26 14:26:49 +0000

    small update to othi userspace Issues: make issue regarding tap_dance.h and it not catching the variables inside and can't pipe to quantum TODO: fix tap_dance.h, check for proper inclusions

--- users/othi/othi.c
+++ users/othi/othi.c
@@ -1,12 +1,9 @@
 #include "othi.h"
 
+void eeconfig_init_users(void) {
 #if (defined(UNICODE_ENABLE) || defined(UNICODEMAP_ENABLE) || defined(UCIS_ENABLE))
   #define OTHI_UNICODE_MODE UC_LNX
 #else
   #define OTHI_UNICODE_MODE 1
 #endif
-
-//#if (defined(UNICODE_ENABLE) || defined(UNICODEMAP_ENABLE) || defined(UCIS_ENABLE))
-//set_unicode_input_mode(OTHI_UNICODE_MODE);
-//#endif //UNICODE_ENABLE
-
+}
--- users/othi/othi.h
+++ users/othi/othi.h
@@ -8,6 +8,7 @@
 #include "rgblight.h"
 #endif
 
+//Layers Definition
 #define CL 0
 #define NM_MODE 2
 #define VI_MODE 3
@@ -15,6 +16,7 @@
 #define ACCENT 5
 #define ACCENT_CAP 6
 
+//Unicode Definitions
 #define DE_AE     UC(0x00E4)
 #define DE_SS     UC(0x00DF)
 #define DE_OE     UC(0x00F6)
