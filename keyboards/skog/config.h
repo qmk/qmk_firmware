2eb5dfa347bde26bc2d83bad87e792da84ec06f6
Jumail <mundekkat@hotmail.com>
2018-12-26 12:40:19 +1100

    Updated manufacturer/product name

--- keyboards/skog/config.h
+++ keyboards/skog/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0x20A0
 #define PRODUCT_ID      0x422D
 
-#define MANUFACTURER    Fox Lab
-#define PRODUCT         Leaf 80
+#define MANUFACTURER    Percent
+#define PRODUCT         Skog TKL
 
 /* matrix size */
 #define MATRIX_ROWS 8
--- keyboards/skog/usbconfig.h
+++ keyboards/skog/usbconfig.h
@@ -241,7 +241,7 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x00, 0x02
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'F','o','x',' ','L','a','b'
+#define USB_CFG_VENDOR_NAME     'P','e','r','c','e','n','t'
 #define USB_CFG_VENDOR_NAME_LEN 7
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
@@ -251,8 +251,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'L','e','a','f',' ','8','0'
-#define USB_CFG_DEVICE_NAME_LEN 7
+#define USB_CFG_DEVICE_NAME     'S','k','o','g',' ','T','K','L'
+#define USB_CFG_DEVICE_NAME_LEN 8
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
