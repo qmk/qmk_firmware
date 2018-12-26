8264432df0659b2881f520f116c519f4ec4e5afb
Othi <mnpq.raven@gmail.com>
2018-12-26 23:31:32 +0100

    solved the issue from (ee3a9ab), compiles properly now

---
+++ keyboards/kbd6x/keymaps/othi/rules.mk
@@ -0,0 +1,4 @@
+RGBLIGHT_ENABLE = yes
+NKRO_ENABLE = yes            # USB Nkey Rollover
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+TAP_DANCE_ENABLE = yes
--- users/othi/rules.mk
+++ users/othi/rules.mk
@@ -1,26 +1,9 @@
 SRC += othi.c
 
-#ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
-#	SRC += tap_dance.c
-#endif
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+	SRC += tap_dance.c
+endif
 
 ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
 	SRC += rgb_light.c
 endif
-
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no        # Console for debug(+400)
-COMMAND_ENABLE = no        # Commands for debug and configuration
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
-UNICODE_ENABLE = yes         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = yes
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
-TAP_DANCE_ENABLE=yes
