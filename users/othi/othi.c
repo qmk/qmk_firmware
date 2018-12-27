9670fa78a486664f41fdf7474eeed294750a7fad
Othi <mnpq.raven@gmail.com>
2018-12-27 17:15:24 +0100

    added dz60 normal 60% keymap, proper unicode setter

--- users/othi/othi.c
+++ users/othi/othi.c
@@ -6,7 +6,6 @@
   #define OTHI_UNICODE_MODE 1
 #endif
 
-
 void eeconfig_init_user(void) {
   #if (defined(UNICODE_ENABLE) || defined(UNICODEMAP_ENABLE) || defined(UCIS_ENABLE))
     set_unicode_input_mode(OTHI_UNICODE_MODE);
