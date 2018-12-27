b4eaa82ad6a1fdfc90bb4940aa72fe555d25e467
Gergely Peidl <gergely@peidl.net>
2018-12-27 23:22:44 +0100

    Update keymap.

--- keyboards/lets_split/keymaps/geripgeri/config.h
+++ keyboards/lets_split/keymaps/geripgeri/config.h
@@ -23,13 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../../config.h"
 
-/* Use I2C or Serial, not both */
-
 #define USE_SERIAL
-// #define USE_I2C
 
 /* Select hand configuration */
-
 #define MASTER_LEFT
 
 #define AUTO_SHIFT_TIMEOUT 130
