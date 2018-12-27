cd88d595544cb9f4c3ad56b725ed68bf65418b31
Othi <mnpq.raven@gmail.com>
2018-12-27 17:02:13 +0100

    index on master: ee1be3e5b Improve diagnostics for build hashes

---
+++ users/othi/rgb_light.c
@@ -0,0 +1,21 @@
+#include "rgb_light.h"
+uint32_t layer_state_set_user(uint32_t state) {
+    switch (biton32(state)) {
+    case NM_MODE:
+        rgblight_setrgb (0x00,  0x66, 0x00);
+        break;
+    case VI_MODE:
+        rgblight_setrgb (0x66,  0x66, 0x00);
+        break;
+    case ACCENT:
+        rgblight_setrgb (0x7A,  0x00, 0xFF);
+        break;
+    case ACCENT_CAP:
+        rgblight_setrgb (0x7A,  0xFF, 0xFF);
+        break;
+    default: //  for any other layers, or the default layer
+        rgblight_setrgb (0xFF,  0x00, 0x00);
+        break;
+    }
+  return state;
+}
