9ec49745974533ce4a5d01f67b1f924918b836c1
Max <17062872+westfoxtrot@users.noreply.github.com>
2018-12-29 00:07:47 +0100

    refactored cyclops keyboard project (#4734)
    * refactored cyclops keyboard project
    
    Moved the files into a subfolder to allow consistency in preparation for additional commits to come in the future.
    
    made some small adjustments to the configuration and keymap
    
    * Update keyboards/westfoxtrot/cyclops/keymaps/default/keymap.c
    
    Co-Authored-By: westfoxtrot <17062872+westfoxtrot@users.noreply.github.com>
    
    * Update keyboards/westfoxtrot/cyclops/readme.md
    
    Co-Authored-By: westfoxtrot <17062872+westfoxtrot@users.noreply.github.com>
    
    * Update keyboards/westfoxtrot/cyclops/readme.md
    
    Co-Authored-By: westfoxtrot <17062872+westfoxtrot@users.noreply.github.com>
    
    * Update keyboards/westfoxtrot/cyclops/readme.md
    
    Co-Authored-By: westfoxtrot <17062872+westfoxtrot@users.noreply.github.com>

--- keyboards/westfoxtrot/cyclops/cyclops.h
+++ keyboards/westfoxtrot/cyclops/cyclops.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 'mechmerlin'
+/* Copyright 2018 westfoxtrot
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
