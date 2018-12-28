ffd0018fb1dcf3d5f90d4a226c437d4e85430cce
crd <darwin@senet.us>
2018-12-27 23:50:41 -0800

    Temporarily remove failing community layout from zeal60 make rules

--- keyboards/zeal60/rules.mk
+++ keyboards/zeal60/rules.mk
@@ -76,4 +76,6 @@ RAW_ENABLE = yes
 DYNAMIC_KEYMAP_ENABLE = yes
 CIE1931_CURVE = yes
 
-LAYOUTS = 60_ansi 60_iso 60_hhkb 60_ansi_split_bs_rshift
+# 60_iso  removed because unixmaal's community layout won't compile
+# due to duplicate function definition https://travis-ci.org/qmk/qmk_firmware/builds/472014965
+LAYOUTS = 60_ansi 60_hhkb 60_ansi_split_bs_rshift
