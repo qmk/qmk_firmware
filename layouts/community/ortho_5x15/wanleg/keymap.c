0667b1c59743dc63daaf6871b602fe823795d1c8
Drashna Jaelre <drashna@live.com>
2018-12-29 13:01:06 -0800

    Update layouts/community/ortho_5x15/wanleg/keymap.c
    Co-Authored-By: wanleg <32079073+wanleg@users.noreply.github.com>

--- layouts/community/ortho_5x15/wanleg/keymap.c
+++ layouts/community/ortho_5x15/wanleg/keymap.c
@@ -25,7 +25,7 @@
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, K2c, K2d, K2e, \
 	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3a, K3b, K3c, K3d, K3e  \
   ) \
-  LAYOUT_wrapper( \
+   LAYOUT_ortho_5x15_wrapper( \
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NLCK,  QWERTY, GHERKIN, FUNCTION, \
      K0c,  K0d,  K0e,		K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, \
      K1c,  K1d,  K1e, 		K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1a, K1b, \
