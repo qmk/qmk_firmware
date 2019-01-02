e74a316a9d8f43b22c11dac527a2461bffaf896b
ishtob <ishtob@gmail.com>
2019-01-02 13:04:54 -0500

    update ishtob keymap and fix config settings

--- keyboards/hadron/ver3/config.h
+++ keyboards/hadron/ver3/config.h
@@ -192,14 +192,11 @@
 #define ZC_DET_TIME 0
 #define AUTO_CAL_TIME 3
 
-//#define WS2812_LED_N 2
-//#define RGBLED_NUM WS2812_LED_N
-//#define WS2812_TIM_N 2
-//#define WS2812_TIM_CH 2
-//#define PORT_WS2812     GPIOA
-//#define PIN_WS2812      15
-//#define WS2812_DMA_STREAM STM32_DMA1_STREAM2  // DMA stream for TIMx_UP (look up in reference manual under DMA Channel selection)
-//#define WS2812_DMA_CHANNEL 7                  // DMA channel for TIMx_UP
-//#define WS2812_EXTERNAL_PULLUP
+//#define RGBLIGHT_ANIMATIONS
 
+//#define RGBLED_NUM 10
+//#define RGB_DI_PIN B5
+//#define DRIVER_LED_TOTAL RGBLED_NUM
+
+//#define RGB_MATRIX_KEYPRESSES
 #endif
