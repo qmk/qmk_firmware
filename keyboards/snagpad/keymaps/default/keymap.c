952c97af5eb58a2705bc30e9817d9266e51a8fd1
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-01-02 16:26:00 +1100

    Update keyboards/snagpad/keymaps/default/keymap.c
    Co-Authored-By: flehrad <20124416+flehrad@users.noreply.github.com>

--- keyboards/snagpad/keymaps/default/keymap.c
+++ keyboards/snagpad/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_P7, KC_P8, KC_P9, KC_PPLS, 
  		KC_P4, KC_P5, KC_P6, 
 		KC_P1, KC_P2, KC_P3, KC_PENT, 
-		KC_P0, KC_NO, KC_PDOT, KC_NO),
+ 		KC_P0,        KC_PDOT),
 };
 
 	switch (id) {
