6ad1f9da9b22fdfff441cecacfda24dd6d65f931
Michael Walker <walkerstop@gmail.com>
2019-01-04 01:05:31 -0700

    Add layout for use with ws2812b and such

---
+++ keyboards/wheatfield/blocked65/keymaps/walker/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define RGB_DI_PIN E2
+#define RGBLIGHT_ANIMATIONS
+#undef RGBLED_NUM
+#define RGBLED_NUM 72
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+#endif
