def35817d41e9868e14827198791c53125163a3b
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-01-05 08:23:40 +1100

    Update keyboards/handwired/numbrero/numbrero.h
    Co-Authored-By: flehrad <20124416+flehrad@users.noreply.github.com>

--- keyboards/handwired/numbrero/numbrero.h
+++ keyboards/handwired/numbrero/numbrero.h
@@ -18,7 +18,7 @@
 #define LAYOUT_numbrero_ortho(\
   K00, K01, K02, K03, K04, \
 	K10, K11, K12, K13, K14, \
-	K20, K21, K22, K23, K24  \
+	K20, K21, K22, K23, K24,  \
 	K30, K31, K32, K33, K34, \
 	     K41, K42, K43, K44  \
 ){ \
