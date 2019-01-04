5f2a3c26fefae2730051009c68c602341e462072
noroadsleft <xxiinophobia@yahoo.com>
2019-01-04 15:46:05 -0800

    S65-X: enable 65_ansi and 65_iso community layouts
    Thi commit allows the Sentraq S65-X to use the 65_ansi and 65_iso community layouts.
    
    - LAYOUT_ansi renamed to LAYOUT_65_ansi
    - LAYOUT_iso renamed to LAYOUT_65_iso
    - added LAYOUTS rule to rules.mk

--- keyboards/s65_x/keymaps/iso/keymap.c
+++ keyboards/s65_x/keymaps/iso/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   /* 0: ISO qwerty */
-  [_BL] = LAYOUT_iso(
+  [_BL] = LAYOUT_65_iso(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_END,  \
     FL_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP, \
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
    */
 
-  [_AL] = LAYOUT_iso(
+  [_AL] = LAYOUT_65_iso(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
    */
 
-  [_FL] = LAYOUT_iso(
+  [_FL] = LAYOUT_65_iso(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, \
     _______, TG(_AL), KC_UP,   _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS,          _______, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______, _______, \
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
    */
 
-  [_UL] = LAYOUT_iso(
+  [_UL] = LAYOUT_65_iso(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
     _______, RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
