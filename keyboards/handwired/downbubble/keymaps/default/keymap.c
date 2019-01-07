3be4022590d72b836b6a365fae6c9121dc680ce3
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-01-07 18:11:15 +1100

    Update keyboards/handwired/downbubble/keymaps/default/keymap.c
    Co-Authored-By: flehrad <20124416+flehrad@users.noreply.github.com>

--- keyboards/handwired/downbubble/keymaps/default/keymap.c
+++ keyboards/handwired/downbubble/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_P4,   KC_P5,   KC_P6,   KC_PPLS,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_P1,   KC_P2,   KC_P3,                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,          KC_UP,
     KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC,          KC_SPC,               KC_P0,            KC_PDOT, KC_PENT,         KC_BSPC,          KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
-  )
+  ),
 
   LAYOUT_downbubble_splitrightshift(\
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                                   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_NLCK, KC_HOME, KN_TRNS
