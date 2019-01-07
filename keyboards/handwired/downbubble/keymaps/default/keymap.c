b031b531c1e61ab2a55c7ba663c3e93643d2b089
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-01-07 18:10:58 +1100

    Update keyboards/handwired/downbubble/keymaps/default/keymap.c
    Co-Authored-By: flehrad <20124416+flehrad@users.noreply.github.com>

--- keyboards/handwired/downbubble/keymaps/default/keymap.c
+++ keyboards/handwired/downbubble/keymaps/default/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                 KC_P7,   KC_P8,   KC_P9,   KC_PPLS,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_PGDN,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_P4,   KC_P5,   KC_P6,   KC_TRNS,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_P1,   KC_P2,   KC_P3,   KC_PENT,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_TRNS, KC_UP,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC,          KC_SPC,               KC_P0,   KC_TRNS, KC_PDOT, KC_TRNS,         KC_BSPC,          KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC,          KC_SPC,               KC_P0,   KC_TRNS, KC_PDOT, KC_TRNS,         KC_BSPC,          KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
   ),
 };
 
