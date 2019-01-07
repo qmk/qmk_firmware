bde4c014a4f3aaa2d3b1c03d2a8fb287a7ed3be1
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-01-07 18:10:51 +1100

    Update keyboards/handwired/downbubble/keymaps/default/keymap.c
    Co-Authored-By: flehrad <20124416+flehrad@users.noreply.github.com>

--- keyboards/handwired/downbubble/keymaps/default/keymap.c
+++ keyboards/handwired/downbubble/keymaps/default/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_P4,   KC_P5,   KC_P6,   KC_TRNS,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_P1,   KC_P2,   KC_P3,   KC_PENT,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_TRNS, KC_UP,
     KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC,          KC_SPC,               KC_P0,   KC_TRNS, KC_PDOT, KC_TRNS,         KC_BSPC,          KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT,
-  )
+  ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
