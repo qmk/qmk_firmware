61654b9e5374dba7c51da0b119500331614f3c16
reasonsandreasons <walterphanley@gmail.com>
2019-01-09 14:01:22 -0500

    Cleaned up comments, other things.

--- keyboards/primekb/prime_o/keymaps/reasonsandreasons/keymap.c
+++ keyboards/primekb/prime_o/keymaps/reasonsandreasons/keymap.c
@@ -17,7 +17,6 @@
 #include QMK_KEYBOARD_H
 
 #define L1BS LT(2, KC_BSPC)
-#define TDSE KC_SPC //TD(TD_SPC_ENT)
 #define LSUP LSFT_T(KC_UP)
 #define CTLL LCTL_T(KC_LEFT)
 #define ECTL LCTL_T(KC_ESC)
@@ -63,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [0] = LAYOUT(
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,  KC_5,  KC_6,  KC_7,  KC_8,     KC_9,     KC_0,     KC_DEL,   KC_NLCK,   KC_PSLS,  KC_PAST,  KC_PMNS,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,     KC_P,     KC_BSLS,  KC_7,      KC_8,     KC_9,     KC_PPLS,
-    ECTL,     KC_A,     KC_S,     KC_D,     KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,     SCLS,     KC_QUOT,  KC_4,      KC_5,     KC_6,     KC_PEQL,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  LSUP,     KC_1,      KC_2,     KC_3,     KC_ENT,
-    ALLM,     KC_LCTL,  KC_LALT,  KC_LGUI,  L1BS,  L1BS,  TDSE,  TDSE,  KC_RGUI,  KC_RALT,  CTLL,     KC_DOWN,  KC_RIGHT,  KC_0,     KC_PDOT,  KC_ENT
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,  KC_5,  KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_DEL,   KC_NLCK,   KC_PSLS,  KC_PAST,  KC_PMNS,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,  KC_T,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,  KC_7,      KC_8,     KC_9,     KC_PPLS,
+    ECTL,     KC_A,     KC_S,     KC_D,     KC_F,  KC_G,  KC_H,    KC_J,    KC_K,     KC_L,     SCLS,     KC_QUOT,  KC_4,      KC_5,     KC_6,     KC_PEQL,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,  KC_B,  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  LSUP,     KC_1,      KC_2,     KC_3,     KC_ENT,
+    ALLM,     KC_LCTL,  KC_LALT,  KC_LGUI,  L1BS,  L1BS,  KC_SPC,  KC_SPC,  KC_RGUI,  KC_RALT,  CTLL,     KC_DOWN,  KC_RIGHT,  KC_0,     KC_PDOT,  KC_ENT
   ),
 
 /* GAME
@@ -111,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------|------+------+------+------+------+------+------+------+------+------+------+------+------+      |
- * |      |      |      |      |             |    Space    |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |             |    Enter    |      |      |      |      |      |      |      |      |
  * |      |      |      |      |             |             |      |      |      |      |      |      |      |      |
  * `---------------------------------------------------------------------------------------------------------------'
  */
