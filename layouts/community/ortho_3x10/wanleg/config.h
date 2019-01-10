a9e4fc2d078e5b5db3ccaa8a84c57a456d84fe30
wanleg <wanleg@github.com>
2019-01-09 17:54:16 -0800

    edit based on 40percentclub refactor change

--- layouts/community/ortho_3x10/wanleg/config.h
+++ layouts/community/ortho_3x10/wanleg/config.h
@@ -2,7 +2,7 @@
 
 //QMK DFU settings (ProMicro boards)
 // set top left key as bootloader mode escape key on Gherkin
-#if defined(KEYBOARD_gherkin)
+#if defined(KEYBOARD_40percentclub_gherkin)
 #define QMK_LED B0
 #define QMK_ESC_OUTPUT B4 // usually COL
 #define QMK_ESC_INPUT F7 // usually ROW
