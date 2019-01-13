8dcdfc38cfb51e8bc1ac07acc0fc20e666c50926
James Churchill <pelrun@gmail.com>
2019-01-13 18:28:09 +1000

    Enable LTO to get lets_split/sockets under the line

--- keyboards/lets_split/sockets/rules.mk
+++ keyboards/lets_split/sockets/rules.mk
@@ -2,3 +2,5 @@ BACKLIGHT_ENABLE = no
 AUDIO_ENABLE = yes
 RGBLIGHT_ENABLE = yes #Don't enable this along with I2C
 SPLIT_TRANSPORT = serial #leave out the i2c code to save space
+
+EXTRAFLAGS += -flto -DUSE_Link_Time_Optimization
