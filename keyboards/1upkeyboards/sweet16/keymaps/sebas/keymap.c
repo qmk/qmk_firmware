9b27094b5fadd71c935613b503a5e31b339af9e3
Spaceman <rionlion100@gmail.com>
2019-01-13 17:31:12 -0500

    Update keymap.c

--- keyboards/1upkeyboards/sweet16/keymaps/sebas/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/sebas/keymap.c
@@ -8,13 +8,13 @@ enum layers {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_KEYP]=LAYOUT_ortho_4x4(
+ [_KEYP]=LAYOUT_ortho_4x4(
         KC_P7, KC_P8,   KC_P9,   ALT,
         KC_P4, KC_P5,   KC_P6,   KC_PAST,
         KC_P1, KC_P2,   KC_P3,   KC_PMNS,
         KC_P0, KC_PDOT, KC_PENT, KC_PPLS
-        
-    [_FUNC]=LAYOUT_ortho_4x4(
+     ),
+ [_FUNC]=LAYOUT_ortho_4x4(
         KC_PSCR, KC_SLCK, KC_PAUS, _______,
         KC_INS,  KC_HOME, KC_PGUP, XXXXXXX,
         KC_DEL,  KC_END,  KC_PGDN, XXXXXXX,
