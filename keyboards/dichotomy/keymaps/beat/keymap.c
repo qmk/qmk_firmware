eedc0ca9db736d7c59b58ff646fccb22fbd692ff
Peter Grüner <dr-BEat@users.noreply.github.com>
2019-01-13 20:08:20 +0100

    Added Numpad to apply layer

--- keyboards/dichotomy/keymaps/beat/keymap.c
+++ keyboards/dichotomy/keymaps/beat/keymap.c
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_ADJUST] = LAYOUT( /* Shifted number/function layout, for per-key control.  Only active when shift is held, and number is toggled or held */
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          _______, _______, KC_UP,   _______, _______, _______,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,         _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______,        _______, _______, _______,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_PSLS, KC_P7,   KC_P8,   KC_P9, KC_PMNS, _______,
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,         KC_PAST, KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
+  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,         KC_PCMM, KC_P1,   KC_P2,   KC_P3, KC_PEQL, _______,
+                             _______, _______, _______,        _______, KC_P0,   KC_PDOT,
                     _______, _______, _______, _______,        _______, _______, _______, _______
   ),
 
