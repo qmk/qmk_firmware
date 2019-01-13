7bcd4ec87f64f39701d6d1ebbf15c527d9cd3ec2
Peter Grüner <dr-BEat@users.noreply.github.com>
2019-01-13 20:07:40 +0100

    Removed extra layer buttons

--- keyboards/dichotomy/keymaps/beat/keymap.c
+++ keyboards/dichotomy/keymaps/beat/keymap.c
@@ -16,11 +16,11 @@ enum dichotomy_keycodes
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT( /* Base layout, nearly qwerty but with modifications because it's not a full keyboard. Obviously. */
-  KC_ESCAPE,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TT(_MOUSE),
-                                 KC_LCTL, KC_LALT, TT(_MOUSE),     KC_RGUI, KC_RALT, KC_RCTL,
-              MS_BTN3, SFT_T(KC_ESCAPE), KC_LGUI, KC_BSPC,         KC_SPC,  MO(_RAISE), TT(_LOWER),   MS_BTN3
+  KC_ESCAPE,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TT(_MOUSE),
+                                 KC_LCTL, KC_LALT, KC_LGUI,    KC_RGUI, KC_RALT, KC_RCTL,
+               MS_BTN3, SFT_T(KC_ESCAPE), KC_DEL,  KC_SPC,     KC_SPC,  MO(_RAISE), TT(_LOWER),   MS_BTN3
   ),
 
   [_RAISE] = LAYOUT( /* Shifted layout, small changes (because angle brackets have been moved to thumb cluster buttons) */
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, KC_VOLD,  KC_VOLU, KC_MPLY, KC_MNXT,       KC_MUTE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
   _______, _______, KC_TILD,  _______, _______, KC_MPRV,       _______, _______, _______, KC_PIPE, KC_DQT,  _______,
                               _______, _______, _______,       _______, _______, _______,
-                 _______,  MO(_RAISE), _______, _______,       _______, _______, _______, _______
+                     _______, _______, _______, _______,       _______, _______, _______, _______
   ),
 
   [_ADJUST] = LAYOUT( /* Shifted number/function layout, for per-key control.  Only active when shift is held, and number is toggled or held */
