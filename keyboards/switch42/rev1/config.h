4d9764620f6044f848e92a34c31f9e523ef70750
p <_>
2019-01-15 02:04:45 +0900

    Define RGB_LIGHT_LIMIT_VAL and USB_MAX_POWER_CONSUMPTION

--- keyboards/switch42/rev1/config.h
+++ keyboards/switch42/rev1/config.h
@@ -57,8 +57,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
-
 #define RGBLED_NUM 21    // Number of LEDs
+#define RGBLIGHT_LIMIT_VAL 120
+
+#if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
+// USB_MAX_POWER_CONSUMPTION value
+//  120  RGBoff
+//  330  RGB 6
+//  300  RGB 32
+#define USB_MAX_POWER_CONSUMPTION 400
+#else
+// fix iPhone and iPad power adapter issue
+// iOS device need lessthan 100
+#define USB_MAX_POWER_CONSUMPTION 100
+#endif
 
 /*
  * Feature disable options
