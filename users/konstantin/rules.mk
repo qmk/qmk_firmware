e9ff622b906075f85ead2087df3113a0326d56b3
Konstantin Đorđević <vomindoraan@gmail.com>
2019-01-14 15:24:38 +0100

    Disable Bootmagic, enable KEYBOARD_SHARED_EP with Command

--- users/konstantin/rules.mk
+++ users/konstantin/rules.mk
@@ -1,11 +1,12 @@
-BOOTMAGIC_ENABLE = full # TODO: Disable once Command is fixed
-COMMAND_ENABLE   = no
-CONSOLE_ENABLE   = yes
-EXTRAKEY_ENABLE  = yes
-MOUSEKEY_ENABLE  = yes
-NKRO_ENABLE      = yes
-TAP_DANCE_ENABLE = yes
-UNICODE_ENABLE   = yes
+BOOTMAGIC_ENABLE   = no
+COMMAND_ENABLE     = yes
+CONSOLE_ENABLE     = yes
+EXTRAKEY_ENABLE    = yes
+KEYBOARD_SHARED_EP = yes # TODO: Disable once Command is fixed
+MOUSEKEY_ENABLE    = yes
+NKRO_ENABLE        = yes
+TAP_DANCE_ENABLE   = yes
+UNICODE_ENABLE     = yes
 
 SRC += konstantin.c
 ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
