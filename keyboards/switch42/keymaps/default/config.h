de4d7576313ae52078e0083a5d204657948e5502
p <_>
2019-01-14 18:44:42 +0900

    Fix RGBLED_NUM

--- keyboards/switch42/keymaps/default/config.h
+++ keyboards/switch42/keymaps/default/config.h
@@ -33,9 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
-#undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
 #define RGBLIGHT_LIMIT_VAL 120
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
--- keyboards/switch42/rev1/config.h
+++ keyboards/switch42/rev1/config.h
@@ -58,7 +58,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 
-#define RGBLED_NUM 12    // Number of LEDs
+#define RGBLED_NUM 21    // Number of LEDs
 
 /*
  * Feature disable options
