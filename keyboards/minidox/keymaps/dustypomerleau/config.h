a70eb66f081219b532f488586001727695b76076
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2019-01-15 21:38:47 +1100

    add CTPC to config

--- keyboards/minidox/keymaps/dustypomerleau/config.h
+++ keyboards/minidox/keymaps/dustypomerleau/config.h
@@ -5,6 +5,7 @@
 
 #include "config_common.h"
 
+#define CONVERT_TO_PROTON_C
 #define EE_HANDS
 #define IGNORE_MOD_TAP_INTERRUPT
 #define ONESHOT_TIMEOUT 1000
@@ -14,13 +15,16 @@
 #define TAPPING_TERM 150
 #define USE_SERIAL
 
-// #define MOUSEKEY_DELAY 0 //  before cursor movement (high feels sluggish, low makes fine movement difficult)
-// #define MOUSEKEY_INTERVAL 20 // between movement reports - low settings feel like high mouse speed
+// optional configuration:
+
+// #define MOUSEKEY_DELAY 0 //  delay before cursor movement (high feels sluggish, low makes fine movement difficult)
+// #define MOUSEKEY_INTERVAL 20 // time between movement reports - low settings feel like high mouse speed
 // #define MOUSEKEY_MAX_SPEED 10
 // #define MOUSEKEY_TIME_TO_MAX 60
 // #define MOUSEKEY_WHEEL_DELAY 0
 // #define MOUSEKEY_WHEEL_MAX_SPEED 8
 // #define MOUSEKEY_WHEEL_TIME_TO_MAX 4
+
 // #define ONESHOT_TAP_TOGGLE 2 // not compatible with TAPPING_FORCE_HOLD
 
 #endif
