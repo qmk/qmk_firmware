079fbc41da0ccbc77f2e1302b2700188a0406c0f
Codetector <codetector@codetector.cn>
2019-01-16 20:09:41 -0500

    test

--- keyboards/ergodox_stm32/config.h
+++ keyboards/ergodox_stm32/config.h
@@ -19,5 +19,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef ERGODOX_STM32_CONFIG_H
 #define ERGODOX_STM32_CONFIG_H
 
+#define VENDOR_ID 0xFFCD
+#define PRODUCT_ID 0xBFCE
+#define DEVICE_VER 0x0001
+#define MANUFACTURER ErgoDox
+#define PRODUCT ErgoDox_STM
+#define DESCRIPTION ErgoDox STM32 Keyboard
+
+#define MATRIX_ROWS 14
+#define MATRIX_ROWS_PER_SIDE (MATRIX_ROWS / 2)
+#define MATRIX_COLS 6
 
 #endif
