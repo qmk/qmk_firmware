51509c82cbda6c123efbdc3a91308df5b6ec6c84
asamwow <asamwow@github.com>
2019-01-18 19:40:14 -0800

    Added new literate config

--- keyboards/planck/keymaps/samuel/keymap.c
+++ keyboards/planck/keymaps/samuel/keymap.c
@@ -1,111 +1,52 @@
-/**
- * Sam's Dvorak Layout Optimized for programming
- **/
-
 #include "planck.h"
 #include "action_layer.h"
 
-#define _______ KC_TRNS
+#define TAPPING_TERM 250
 
 extern keymap_config_t keymap_config;
 
+static uint16_t alt_timers[2]  = { 0, 0};
+
+bool altInterupted = false;
+
 enum planck_layers {
   _DVORAK,
-  _LOWER,
-  _RAISE,
-  _SYMBOLS,
+  _RISE
 };
 
 enum planck_keycodes {
   DVORAK = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  SYMBOLS,
+  RISE,
+  LALT,
+  RALT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Dvorak
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   -  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |Enter |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Shift |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | LGUI | Tab  | Alt  |Raise |Lower |Space |Symbol| Alt  | Tab  | RGUI | Ctrl | I never use this right tab sadly
- * `-----------------------------------------------------------------------------------'
- */
-[_DVORAK] = {
-  { KC_ESC, KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,   KC_BSPC},
-  {KC_MINS,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT},
-  {KC_LSFT, KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,   KC_RSFT},
-  {KC_LCTL, KC_LGUI,  KC_TAB, KC_LALT,   RAISE,   LOWER,  KC_SPC, SYMBOLS, KC_RALT,  KC_TAB, KC_RGUI,   KC_RCTL}
+[_DVORAK] =
+
+{{ LT(_RISE, KC_DEL), KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, LT(_RISE, KC_SLSH)},
+{ LSFT_T(KC_EQL), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, RSFT_T(KC_MINUS)},
+{ LCTL_T(KC_LBRC), KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, RCTL_T(KC_RBRC)},
+{ LGUI_T(KC_GRAVE), LALT, KC_LEFT, KC_RGHT, KC_BSPC, KC_TAB, KC_ENT, KC_SPC, KC_UP, KC_DOWN, RALT, RGUI_T(KC_BSLASH)}
 },
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |      |Delete|Pg Up |  Up  |Pg Dn |   (  |      | F1   | F2   | F3   | F4   |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | Bksp | Left | Down |Right |   )  |      | F5   | F6   | F7   | F8   |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Insert|ctrl-c|ctrl-x|ctrl-v|   /  |      | F9   | F10  | F11  | F12  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
+[_RISE] =
 
-[_LOWER] = {
-	    {_______,  KC_DEL,     KC_PGUP,       KC_UP,     KC_PGDN, KC_LPRN, _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______},
-	    {_______, KC_BSPC,     KC_LEFT,     KC_DOWN,    KC_RIGHT, KC_RPRN, _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______},
-	    {_______,  KC_INS,  LCTL(KC_C),  LCTL(KC_X),  LCTL(KC_V), KC_SLSH, _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______},
-	    {_______, _______,     _______,     _______,     _______, _______, _______, _______, _______, _______, _______, _______}
+{{ KC_TRNS, LCTL(KC_Z), LCTL(KC_C), LCTL(KC_X), LCTL(KC_V), KC_INS, KC_EQL, KC_7, KC_8, KC_9, KC_F11, KC_TRNS},
+{ KC_TRNS, KC_ESC, KC_CAPS, KC_PGUP, KC_PGDN, KC_F4, KC_ASTR, KC_4, KC_5, KC_6, KC_0, KC_TRNS},
+{ KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_1, KC_2, KC_3, KC_F12, KC_TRNS},
+{ KC_TRNS, KC_TRNS, LCTL(LSFT(KC_LEFT)), LCTL(LSFT(KC_RGHT)), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LCTL(LSFT(KC_UP)), LCTL(LSFT(KC_DOWN)), KC_TRNS, KC_TRNS}
 },
-
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |      |   ^  |   1  |   2  |   3  |   *  |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   0  |   4  |   5  |   6  |   =  |      |      |      |      |      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   +  |   7  |   8  |   9  |Enter |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_RAISE] = {
-	    {_______, KC_CIRC,    KC_1,    KC_2,     KC_3, KC_ASTR, _______, _______, _______, _______, _______, _______},
-	    {_______,    KC_0,    KC_4,    KC_5,     KC_6,  KC_EQL, _______, _______, _______, _______, _______, _______},
-	    {_______, KC_PLUS,    KC_7,    KC_8,     KC_9,  KC_ENT, _______, _______, _______, _______, _______, _______},
-	    {_______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______}
-},
-
-/* Symbols
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |   !  |   ?  |   @  |   $  |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   [  |   ]  |   &  |   |  |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   \  |   #  |   `  |   %  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-
-[_SYMBOLS] = {
-	      {_______, _______, _______, _______, _______, _______, _______, KC_EXLM, KC_QUES,   KC_AT,  KC_DLR, _______},
-	      {_______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_AMPR, KC_PIPE, _______},
-	      {_______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_HASH,  KC_GRV, KC_PERC, _______},
-	      {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-}
-
 
 };
 
-#ifdef AUDIO_ENABLE
-  float plover_song[][2]     = SONG(PLOVER_SOUND);
-  float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
-#endif
+void mod_type(uint16_t modcode, uint16_t keycode) {
+  register_mods(MOD_BIT(modcode));
+  register_code(keycode);
+  unregister_code(keycode);
+  unregister_mods(MOD_BIT(modcode));
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -115,36 +56,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case LOWER:
+    case RISE:
       if (record->event.pressed) {
-        layer_on(_LOWER);
-        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_on(_RISE);
       } else {
-        layer_off(_LOWER);
-        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        layer_off(_RISE);
       }
       return false;
       break;
-    case RAISE:
+    case LALT:
       if (record->event.pressed) {
-        layer_on(_RAISE);
-        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        alt_timers[0] = timer_read();
+        altInterupted = false;
+        register_mods(MOD_BIT(KC_LALT));
       } else {
-        layer_off(_RAISE);
-        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        unregister_mods(MOD_BIT(KC_LALT));
+        if (!altInterupted && timer_elapsed(alt_timers[0]) < TAPPING_TERM) {
+          mod_type(KC_LSHIFT, KC_9);
+        }
       }
       return false;
       break;
-    case SYMBOLS:
+    case RALT:
       if (record->event.pressed) {
-        layer_on(_SYMBOLS);
-        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        alt_timers[1] = timer_read();
+        altInterupted = false;
+        register_mods(MOD_BIT(KC_RALT));
       } else {
-        layer_off(_SYMBOLS);
-        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        unregister_mods(MOD_BIT(KC_RALT));
+        if (!altInterupted && timer_elapsed(alt_timers[1]) < TAPPING_TERM) {
+          mod_type(KC_LSHIFT, KC_0);
+        }
       }
       return false;
       break;
   }
+  altInterupted = true;
   return true;
 }
--- keyboards/planck/keymaps/samuel/readme.md
+++
@@ -1,9 +0,0 @@
-# Sam's Lefthanded Dvorak
-
-I like to type a lot, but coding gives me some strain. I made this layout to help me type faster and with less movement. Since I like to use the mouse, I placed a lot of focus onto the left hand for as much as I could. I am also left handed so this feels natural to me.
-
-To look at the layout look at the keymap c file. There are 3 layers not including the base, dvorak layer. Besides these layers, most of the bottom row and side columns are symetrical; basic shift, tab, control, alt, and gui (windows) keys. Your thumbs control either the alt keys, all 3 of the layer keys, and a space key. The layer below the "h" key on the right side is where I placed symbols that I would only probably need to type if I was using my right hand. To the left of the space are two more layer keys, one below the "i" key and one below the "e" key. One of these layers creats a number pad under your left hand, while the other layer gives a lot of data manipulation keys and arrow keys without having to lift a finger.
-
-Now, when I type, my fingers need to travel much less distance, and I do not need to move from mouse to keyboard or back nearly as often.
-
-Thank you QMK!
