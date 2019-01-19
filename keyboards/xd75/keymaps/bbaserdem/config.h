170e4537a782d36afb7622aa722f639bb4b73996
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2019-01-19 15:38:33 -0500

    XD75re

--- keyboards/xd75/keymaps/bbaserdem/config.h
+++ keyboards/xd75/keymaps/bbaserdem/config.h
@@ -1,4 +1,9 @@
 #ifndef CONFIG_KEYMAP_H
 #define CONFIG_KEYMAP_H
 #include "../../config.h"
+
+#define BACKLIGHT_LEVELS 9
+#define BACKLIGHT_BREATHING
+#define BREATHING_PERIOD 5
+
 #endif
--- keyboards/xd75/keymaps/bbaserdem/keymap.c
+++ keyboards/xd75/keymaps/bbaserdem/keymap.c
@@ -14,56 +14,30 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "xd75.h"
+#include "bbaserdem.h"
 
 // Layer shorthand
 #define _BASE 0
 #define _FUNC 1
 #define FUNCT MO(_FUNC)
 #define XXX KC_NO
+#define RGB_RMD RGB_MODE_REVERSE
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY: but turkish f
- * .-----------------------------------------------------------------------------------------------------------------------.
- * | * +   | 1 "   | 2 "   | 3 ^   | 4 $   | 5 %   | 6 &   | 7 / { | 8 ( [ | 9 ) ] | 0 = } | / ? \ | - _   |BackSpc| Delete|
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * | Escape| F   @ | G     |       | I     | O     | D     | R     | N     | H     | P     | Q     | W   ~ |  Home | Pg Up |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |  Tab  | U     |       | E     | A     |       | T     | K     | M     | L     | Y     |     # | X   ` |  End  | Pg Dn |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+---------------+-------+-------|
- * | Shift | J     |       | V     | C     |       | Z     | S     | B     | . :   | , ;   | < > | | Shift |   Up  |CapsLck|
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * | Funct |  Ctrl |  Meta |  Alt  |     Space     |     Enter     |      AltGr    |   OS  |  Ctrl |  Left |  Down | Right |
- * '-----------------------------------------------------------------------------------------------------------------------'
- */
-
- [_BASE] = { /* QWERTY */
+[_BASE] = { // QWERTY
   { KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,KC_DEL },
   { KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_HOME,KC_PGUP},
   { KC_TAB, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,KC_END, KC_PGDN},
   { KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_NUHH,KC_RSFT,KC_UP,  KC_INS },
-  { KC_LCTL,KC_LGUI,KC_LALT,FUNCT,  KC_SPC, XXX,    KC_ENT, XXX,    KC_RALT,KC_RGUI,KC_RCTL,KC_CAPS,KC_LEFT,KC_DOWN,KC_RGHT},
+  { KC_LCTL,KC_LGUI,KC_LALT,KC_SPC, XXX,    KC_ENT, XXX,    KC_SPC, XXX,    KC_RALT,FUNCT,  KC_CAPS,KC_LEFT,KC_DOWN,KC_RGHT},
  },
 
-/* FUNCTION
- * .-----------------------------------------------------------------------------------------------------------------------.
- * |BootKbd|   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |PrntScr|  Ins  |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       | Sleep |       | Led - | Led + |       |       |       |       |       | Vol X | Vol - | Vol + |       |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |       |       |       |       |       |       |       |       |       |       |       |       |  Wake |       |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+---------------+-------+-------|
- * |       |       |       |       |       |       |       |       |       |       |       |       |       | MuStop|       |
- * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |       |       |       |       |       |       |       |       |       |       |       | MuPrev| MuTog | MuNext|
- * '-----------------------------------------------------------------------------------------------------------------------'
- */
- 
- [_FUNC] = { /* FUNCTION */
+[_FUNC] = { // FUNCTION
   { RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_PSCR,KC_INS },
-  { _______,KC_SLEP,KC_F13, BL_DEC, BL_INC, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_MUTE,KC_VOLD,KC_VOLU,KC_F19, KC_F20 },
-  { _______,KC_F21 ,KC_F22, KC_F23, KC_F24, XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    KC_WAKE,_______},
-  { _______,XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    _______,KC_MSTP,_______},
-  { _______,_______,_______,_______,XXX,    _______,XXX,    _______,SECRET1,SECRET2,SECRET3,SECRET4,KC_MPRV,KC_MPLY,KC_MNXT},
+  { BL_BRTG,RGB_RMD,RGB_HUI,RGB_SAI,RGB_VAI,XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX    },
+  { BL_STEP,RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    KC_MUTE,XXX    },
+  { BL_TOGG,RGB_TOG,XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    KC_VOLD,KC_MSTP,KC_VOLU},
+  { XXX,    XXX,    XXX,    XXX,    SECRET1,XXX,    SECRET2,XXX,    SECRET3,XXX,    _______,XXX,    KC_MPRV,KC_MPLY,KC_MNXT},
  }
 };
