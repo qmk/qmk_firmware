dd31eeab420f9ec2c25615e2d01adfb858f07800
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-01-18 21:43:24 -0800

    handwired/qc60: refactor, Configurator update, and readme update (#4878)
    * handwired/qc60: refactor and Configurator update
    
    - correct layout macro name (LAYOUT_ANSI_DEFAULT to LAYOUT_ansi_default)
    - add layout data for remaining layout macros
    - correct ISO layout macros (neither had a split left Shift)
    - refactor LAYOUT_iso_alt (place KC_NUHS key on home row; consistent with LAYOUT_iso_default)
    - proto.h refactored to use #pragma once include guard
    
    * handwired/qc60: keymap refactor
    
    - delete redundant KC_TRNS and KC_NO aliases
    
    * handwired/qc60: readme update
    
    - update header (made consistent with QMK template)
    - update Docs links (Newbs Guide; grammar)

--- keyboards/handwired/qc60/proto/proto.h
+++ keyboards/handwired/qc60/proto/proto.h
@@ -14,8 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef PROTO_H
-#define PROTO_H
+#pragma once
 
 #include "qc60.h"
 
@@ -26,10 +25,10 @@
 
 /* Split Backspace
  * {R07, XXX, R05, R04, R03, R02, R01, R00}
- * 
+ *
  * Split Right Shift
  * {R37, R36, R35, R34, R33, R32, R31, XXX}
- * 
+ *
  * Split Left Shift
  * {L30, L31, L32, L33, L34, L35, L36, XXX}
  *
@@ -101,14 +100,14 @@
     L00, L01, L02, L03, L04, L05, L06, R00, R01, R02, R03, R04, R05,           R07, \
     L10, L11, L12, L13, L14, L15,      R10, R11, R12, R13, R14, R15, R16,           \
     L20, L21, L22, L23, L24, L25,           R21, R22, R23, R24, R25, R26, R17, R27, \
-         L31, L32, L33, L34, L35, L36,      R31, R32, R33, R34, R35, R36,           \
+    L30, L31, L32, L33, L34, L35, L36,      R31, R32, R33, R34, R35, R36,           \
     L40, L41, L42, L43, L44,                     R43, R44, R42, R45, R46,      R47  \
 ) \
 { \
     {L00, L01, L02, L03, L04, L05, L06, XXX}, \
     {L10, L11, L12, L13, L14, L15, XXX, XXX}, \
     {L20, L21, L22, L23, L24, L25, XXX, XXX}, \
-    {XXX, L31, L32, L33, L34, L35, L36, XXX}, \
+    {L30, L31, L32, L33, L34, L35, L36, XXX}, \
     {L40, L41, L42, L43, L44, XXX, XXX, XXX}, \
     {R07, XXX, R05, R04, R03, R02, R01, R00}, \
     {R17, R16, R15, R14, R13, R12, R11, R10}, \
@@ -118,17 +117,17 @@
 }
 
 #define LAYOUT_iso_alt( \
-    L00, L01, L02, L03, L04, L05, L06, R00, R01, R02, R03, R04, R05,      R07, \
-    L10, L11, L12, L13, L14, L15,      R10, R11, R12, R13, R14, R15, R16, R17, \
-    L20, L21, L22, L23, L24, L25,           R21, R22, R23, R24, R25, R26, R27, \
-         L31, L32, L33, L34, L35, L36,      R31, R32, R33, R34, R35, R36,      \
-    L40, L41, L42, L43, L44,                     R43, R44,      R45, R46, R47  \
+    L00, L01, L02, L03, L04, L05, L06, R00, R01, R02, R03, R04, R05,           R07, \
+    L10, L11, L12, L13, L14, L15,      R10, R11, R12, R13, R14, R15, R16,           \
+    L20, L21, L22, L23, L24, L25,           R21, R22, R23, R24, R25, R26, R17, R27, \
+    L30, L31, L32, L33, L34, L35, L36,      R31, R32, R33, R34, R35, R36,           \
+    L40, L41, L42, L43, L44,                     R43, R44,      R45, R46,      R47  \
 ) \
 { \
     {L00, L01, L02, L03, L04, L05, L06, XXX}, \
     {L10, L11, L12, L13, L14, L15, XXX, XXX}, \
     {L20, L21, L22, L23, L24, L25, XXX, XXX}, \
-    {XXX, L31, L32, L33, L34, L35, L36, XXX}, \
+    {L30, L31, L32, L33, L34, L35, L36, XXX}, \
     {L40, L41, L42, L43, L44, XXX, XXX, XXX}, \
     {R07, XXX, R05, R04, R03, R02, R01, R00}, \
     {R17, R16, R15, R14, R13, R12, R11, R10}, \
@@ -196,6 +195,3 @@
     {XXX, R36, R35, R34, R33, R32, R31, XXX}, \
     {R47, XXX, R45, R44, R43, XXX, XXX, XXX}, \
 }
-
-
-#endif
