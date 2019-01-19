af5d030f9c174de695ee90bf7a4e635cc1879ad7
Peter Grüner <dr-BEat@users.noreply.github.com>
2019-01-19 21:13:00 +0100

    Moved buttons and added toggle for raise button

--- keyboards/dichotomy/keymaps/beat/keymap.c
+++ keyboards/dichotomy/keymaps/beat/keymap.c
@@ -20,15 +20,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TT(_MOUSE),
                                  KC_LCTL, KC_LALT, KC_LGUI,    KC_RGUI, KC_RALT, KC_RCTL,
-               MS_BTN3, SFT_T(KC_ESCAPE), KC_DEL,  KC_SPC,     KC_SPC,  MO(_RAISE), TT(_LOWER),   MS_BTN3
+               MS_BTN3, SFT_T(KC_ESCAPE), KC_DEL,  KC_SPC,     KC_SPC,  TT(_RAISE), TT(_LOWER),   MS_BTN3
   ),
 
   [_RAISE] = LAYOUT( /* Shifted layout, small changes (because angle brackets have been moved to thumb cluster buttons) */
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT,        KC_MUTE, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,  _______,
-  _______, _______, KC_GRV,  _______, _______, KC_MPRV,        _______, _______, _______, KC_BSLS, KC_QUOT, _______,
+  _______, _______, KC_GRV,  _______, _______, KC_MPRV,        _______, KC_LABK, KC_RABK, KC_BSLS, KC_QUOT, _______,
                              _______, _______, _______,        _______, _______, _______,
-                    _______, _______, KC_LABK, _______,        _______, KC_RABK, _______, _______
+                    _______, _______, _______, _______,        _______, _______, _______, _______
   ),
 
   [_LOWER] = LAYOUT( /* Number layout, basically the main function layer */
