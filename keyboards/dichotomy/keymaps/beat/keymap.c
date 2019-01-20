2c54892c1e52040657c1a619b6001addeb113679
Peter Grüner <dr-BEat@users.noreply.github.com>
2019-01-20 18:01:16 +0100

    Added Menu,PrintScreen and Windowslock buttons, and left handmouse

--- keyboards/dichotomy/keymaps/beat/keymap.c
+++ keyboards/dichotomy/keymaps/beat/keymap.c
@@ -19,13 +19,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESCAPE,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TT(_MOUSE),
-                                 KC_LCTL, KC_LALT, KC_LGUI,    KC_RGUI, KC_RALT, KC_RCTL,
+                                 KC_LGUI, KC_LALT, KC_LCTL,    KC_RCTL, KC_RALT, KC_RGUI,
                MS_BTN3, SFT_T(KC_ESCAPE), KC_DEL,  KC_SPC,     KC_SPC,  TT(_RAISE), TT(_LOWER),   MS_BTN3
   ),
 
   [_RAISE] = LAYOUT( /* Shifted layout, small changes (because angle brackets have been moved to thumb cluster buttons) */
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT,        KC_MUTE, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,  _______,
+  _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT,        KC_MUTE, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
   _______, _______, KC_GRV,  _______, _______, KC_MPRV,        _______, KC_LABK, KC_RABK, KC_BSLS, KC_QUOT, _______,
                              _______, _______, _______,        _______, _______, _______,
                     _______, _______, _______, _______,        _______, _______, _______, _______
@@ -42,17 +42,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT( /* Shifted number/function layout, for per-key control.  Only active when shift is held, and number is toggled or held */
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_PSLS, KC_P7,   KC_P8,   KC_P9, KC_PMNS, _______,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,         KC_PAST, KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
-  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,         KC_PCMM, KC_P1,   KC_P2,   KC_P3, KC_PEQL, _______,
+  _______, _______, _______, LGUI(KC_L), KC_PSCR, KC_APP,      KC_PCMM, KC_P1,   KC_P2,   KC_P3, KC_PEQL, _______,
                              _______, _______, _______,        _______, KC_P0,   KC_PDOT,
                     _______, _______, _______, _______,        _______, _______, _______, _______
   ),
 
   [_MOUSE] = LAYOUT( /* Mouse layer, including buttons for clicking. */
-  _______, _______, _______, _______, _______, _______,        KC_VOLU, KC_HOME, KC_PGUP, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,        _______, MS_BTN1, MS_BTN2, MS_BTN3, _______, _______,
-  _______, _______, _______, _______, _______, _______,        KC_VOLD, KC_END,  KC_PGDN, _______, _______, _______,
-                             _______, _______, _______,        _______, KC_UP,   _______,
-                    _______, _______, _______, _______,        KC_LEFT, KC_DOWN, KC_RGHT, _______
+  _______, _______, _______, _______, _______, _______,        KC_VOLU, KC_HOME, KC_WH_U, KC_PGUP, _______, _______,
+  _______, _______, MS_BTN2, MS_BTN3, MS_BTN1, _______,        _______, MS_BTN1, MS_BTN3, MS_BTN2, _______, _______,
+  _______, _______, _______, _______, _______, _______,        KC_VOLD, KC_END,  KC_WH_D, KC_PGDN, _______, _______,
+                             _______, KC_UP,   _______,        _______, KC_UP,   _______,
+                    _______, KC_LEFT, KC_DOWN, KC_RGHT,        KC_LEFT, KC_DOWN, KC_RGHT, _______
   )
 };
 
