c4f1d740fd69522fdcaf4fa9a6ab8b0d7ea4a045
Drashna Jaelre <drashna@live.com>
2019-01-20 01:38:04 +0100

    Apply suggestions from code review
    Co-Authored-By: schaefer-dev <schaefer.dev@gmail.com>

--- keyboards/hhkb/keymaps/schaeferdev/config.h
+++ keyboards/hhkb/keymaps/schaeferdev/config.h
@@ -1,10 +1,8 @@
 // Based off of this section:
 // https://github.com/qmk/qmk_firmware/blob/master/doc/BUILD_GUIDE.md#the-configh-file
-#ifndef CONFIG_SCHAEFERDEV_H
-#define CONFIG_SCHAEFERDEV_H
+#pragma once
 
 // Bring in original defaults
-#include "../../config.h"
 
 // Define mousekey settings
 #define MOUSEKEY_DELAY          0
@@ -15,5 +13,3 @@
 #define MOUSEKEY_WHEEL_MAX_SPEED   160
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 200
 
-
-#endif
