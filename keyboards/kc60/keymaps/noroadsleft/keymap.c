67bd632c38a0ce4b659e179980af5d41a4862fe1
noroadsleft <xxiinophobia@yahoo.com>
2019-01-20 01:52:33 -0800

    NUBS_Z: initial version
    Create a keycode that is normally Z, but KC_NUBS when tapped while Alt is being held.
    
    This removes the possibility of using an Alt+Z shortcut.

--- keyboards/kc60/keymaps/noroadsleft/keymap.c
+++ keyboards/kc60/keymaps/noroadsleft/keymap.c
@@ -84,7 +84,8 @@ enum custom_keycodes {
   Q2_ESC,
   Q2_GRV,
   MC_UNDO,
-  MC_PSTE
+  MC_PSTE,
+  NUBS_Z
 };
 
 
@@ -234,6 +235,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       };
       return false;
       break;
+    case NUBS_Z:
+      if (record->event.pressed) {
+        if ( modifiers & MODS_ALT_MASK ) {
+          SEND_STRING( SS_TAP(X_NONUS_BSLASH) );
+        } else {
+          SEND_STRING( SS_TAP(X_Z) );
+        }
+      };
+      return false;
+      break;
   } // switch()
   return true;
 };
@@ -252,7 +263,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     FW_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   \
+    KC_LSFT, NUBS_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   \
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, MO(_FW), KC_RCTL  \
   ),
 
