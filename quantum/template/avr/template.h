97896a7877d925dcbff8ccb00113cfb7296e91aa
mechmerlin <mechmerlin@gmail.com>
2019-01-23 12:48:50 -0800

    use pragma once in our templates

--- quantum/template/avr/template.h
+++ quantum/template/avr/template.h
@@ -13,8 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef %KEYBOARD_UPPERCASE%_H
-#define %KEYBOARD_UPPERCASE%_H
+#pragma once
 
 #include "quantum.h"
 
@@ -34,5 +33,3 @@
     { K00, K01,   K02 }, \
     { K10, KC_NO, K11 }, \
 }
-
-#endif
--- quantum/template/ps2avrgb/template.h
+++ quantum/template/ps2avrgb/template.h
@@ -13,8 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef %KEYBOARD_UPPERCASE%_H
-#define %KEYBOARD_UPPERCASE%_H
+#pragma once
 
 #include "quantum.h"
 
@@ -34,5 +33,3 @@
     { k00, k01,   k02 }, \
     { k10, KC_NO, k11 }, \
 }
-
-#endif
