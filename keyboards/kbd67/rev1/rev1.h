607aaffebb3b0cce81c377981663433e15b7030d
mechmerlin <mechmerlin@gmail.com>
2019-01-22 22:05:56 -0800

    rename kbd67 due to compile error

--- keyboards/kbd67/rev1/rev1.h
+++ keyboards/kbd67/rev1/rev1.h
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef KBD67_H
-#define KBD67_H
+#ifndef REV1_H
+#define REV1_H
 
 #include "quantum.h"
 
