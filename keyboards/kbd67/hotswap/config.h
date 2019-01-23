7883870149b16bd8eeacdae6f6476d66dd7d44e6
mechmerlin <mechmerlin@gmail.com>
2019-01-22 20:00:04 -0800

    get rid of vanilla cookie cutter code

--- keyboards/kbd67/hotswap/config.h
+++ keyboards/kbd67/hotswap/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    You
-#define PRODUCT         hotswap
-#define DESCRIPTION     A custom keyboard
+#define MANUFACTURER    KBDFans
+#define PRODUCT         KBD67 Hotswap
+#define DESCRIPTION     A custom 65% keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 2
