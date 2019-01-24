9cba69905286bd81674b507c9bfb4665e5f8ae9f
Sterling <503326@MC02VP210HV2H.tld>
2019-01-24 08:52:26 -0800

    Standardizing keymap `include` lines.

--- keyboards/orthodox/keymaps/xyverz/keymap.c
+++ keyboards/orthodox/keymaps/xyverz/keymap.c
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-#include "action_layer.h"
-#include "eeconfig.h"
 
 extern keymap_config_t keymap_config;
 
