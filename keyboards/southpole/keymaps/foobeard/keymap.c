891bfae74e0f0900dc43dfda33df67f715bc3b7d
Eric <ericduns@gmail.com>
2019-01-26 12:39:01 -0700

    made changes fixing outdated lines and added readme.md

--- keyboards/southpole/keymaps/foobeard/keymap.c
+++ keyboards/southpole/keymaps/foobeard/keymap.c
@@ -13,7 +13,7 @@ You should have received a copy of the GNU General Public LicensezZZ
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "southpole.h"
+#include QMK_KEYBOARD_H
 
 enum layers {
   _QWERTY,
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*  |      0      |   .  |  En  | | Ctrl  |  Win  |  Alt  |                  Space                            |  Fn  |    | Left |  Down  | Right|
 	*  `------+------+------+------+-+-------------------------------------------------------------------------------------------------------+------'
 	*/
-    [_QWERTY] = KEYMAP(
+    [_QWERTY] = LAYOUT(
 		KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_DELETE, KC_INS,
 		KC_P7, KC_P8, KC_P9, KC_PPLS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_VOLU,
 		KC_P4, KC_P5, KC_P6, KC_PENT, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_VOLD,
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*  |             |      |      | |       |       |       |                                                   |      |    |      |        |      |
 	*  `------+------+------+------+-+-------------------------------------------------------------------------------------------------------+------'
 	*/
-	[_RAISE] = KEYMAP(
+	[_RAISE] = LAYOUT(
 		RESET,  ______, ______, ______, KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, ______, ______, KC_CALC,
 		______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
 		______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
---
+++ keyboards/southpole/readme.md
@@ -0,0 +1,9 @@
+Southpole
+[Southpole](https://imgur.com/a/MZ2wdYt)
+
+PCB designed by u/waxpoetic for a southpaw style board with an hhkb style layout.
+
+Keyboard Maintainer: ericduns
+Hardware Supported: Teensy 2.0
+
+Make southpole:default
