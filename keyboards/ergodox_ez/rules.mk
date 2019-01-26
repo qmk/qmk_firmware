49e1f1a40a815970e9b3dcdb880357ea5cde8398
Seebs <seebs@seebs.net>
2019-01-26 12:24:42 -0600

    Automatically include build.mk after rules.mk.
    In some cases, you might want to have rules which are conditional
    on settings, but to allow later files to override the settings.
    
    For instance:
    
        FOO_ENABLE = no
        ifeq $($(strip $(FOO_ENABLE)),yes)
    	SRC += foo.c
        endif
    
    if you override FOO_ENABLE elsewhere, nothing happens.
    
    So we split this into a rules.mk containing the setting, and
    a build.mk containing the build rules, then include every
    build.mk file *after* all the rules.mk files have been
    included. I made this change to ergodox_ez as an example.
    
    A call to include with no filename in an eval works
    harmlessly, so we don't have to do existence checks.
    
    Signed-off-by: seebs <seebs@seebs.net>

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -93,28 +93,4 @@ LCDSTENO_ENABLE  = no  # use i2c display for steno
 # For instance, if you solder the A0 jumper, you use 1 here.
 I2CLCD_PORT    = 0
 
-ifeq ($(strip $(I2CLCD_AF_ENABLE)), yes)
-    SRC += i2c_lcd_af.c
-    OPT_DEFS += -DI2CLCD_PORT=$(I2CLCD_PORT) -DI2CLCD_ENABLE
-endif
-
-ifeq ($(strip $(I2CLCD_SB_ENABLE)), yes)
-    SRC += i2c_lcd_sb.c
-    OPT_DEFS += -DI2CLCD_PORT=$(I2CLCD_PORT) -DI2CLCD_ENABLE
-endif
-
-ifeq ($(strip $(I2CLCD_ENABLE)), yes)
-  SRC += i2c_lcd.c
-  OPT_DEFS += -DI2CLCD_PORT=$(I2CLCD_PORT)
-endif
-
-ifeq ($(strip $(LCDSTENO_ENABLE)), yes)
-  SRC += lcd_steno.c
-endif
-
-ifeq ($(strip $(RGB_MATRIX_ENABLE)), no)
-  SRC += i2c_master.c
-endif
-
-
 LAYOUTS = ergodox
