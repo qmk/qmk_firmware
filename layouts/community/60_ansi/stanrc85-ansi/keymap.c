a779a4958d62eea79931ffe9be4165eb2c3b3672
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2019-01-25 22:38:35 -0500

    Changed _____ to _______ as requested to meet QMK standards.

--- layouts/community/60_ansi/stanrc85-ansi/keymap.c
+++ layouts/community/60_ansi/stanrc85-ansi/keymap.c
@@ -16,7 +16,6 @@
 #include QMK_KEYBOARD_H
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-#define _____ KC_TRNS //readability
 #define DEFAULT 0 	//Custom ANSI
 #define LAYER1 1 	//Default ANSI
 #define LAYER2 2 	//F keys, arrows, custom shortcuts, volume control
@@ -38,12 +37,12 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_60_ansi(
-		TD(TD_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, 
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
-		LT(2, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
+		TD(TD_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+		LT(2, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
 		KC_LCTL, KC_LGUI, KC_LALT, LT(2, KC_SPC), KC_RALT, TD(TD_WIN), MO(3), KC_RCTL),
-		
+
 	[1] = LAYOUT_60_ansi(
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
 		KC_TAB,KC_Q,KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
@@ -52,18 +51,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , KC_RALT, MO(2), MO(3), KC_RCTL),
 
 	[2] = LAYOUT_60_ansi(
-		KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, 
-		_____, _____, LCA(KC_QUOT), KC_VOLU, LCA(KC_SCLN), _____, _____, KC_HOME, KC_UP, KC_END, _____, _____, _____, RESET,
-		KC_CAPS, _____, KC_MUTE, KC_VOLD, KC_MPLY, _____, _____, KC_LEFT, KC_DOWN, KC_RGHT, _____, _____, _____,
-		_____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, 
-		_____, _____, _____, _____, _____, _____, _____, _____),
-		
+		KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+		_______, _______, LCA(KC_QUOT), KC_VOLU, LCA(KC_SCLN), _______, _______, KC_HOME, KC_UP, KC_END, _______, _______, _______, RESET,
+		KC_CAPS, _______, KC_MUTE, KC_VOLD, KC_MPLY, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______),
+
 	[3] = LAYOUT_60_ansi(
-		RESET, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, 
-		_____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, _____, RESET, 
-		KC_CAPS, _____, _____, TG(1), _____, _____, BL_STEP, BL_DEC, BL_INC, BL_TOGG, _____, _____, _____, 
-		_____, _____, _____, RGB_VAD, RGB_VAI, RGB_SAI, RGB_HUD, RGB_HUI, RGB_MOD, RGB_TOG, _____, _____, 
-		_____, _____, _____, _____, _____, _____, _____, _____)
+		RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
+		KC_CAPS, _______, _______, TG(1), _______, _______, BL_STEP, BL_DEC, BL_INC, BL_TOGG, _______, _______, _______,
+		_______, _______, _______, RGB_VAD, RGB_VAI, RGB_SAI, RGB_HUD, RGB_HUI, RGB_MOD, RGB_TOG, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
