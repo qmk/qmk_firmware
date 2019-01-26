123608fb318a42500d64d29aa46c7d08140033fd
alex-ong <the.onga@gmail.com>
2019-01-26 17:10:27 +1100

    DO NOT USE Revert back to original API to support split_keyboards.

--- quantum/matrix.c
+++ quantum/matrix.c
@@ -122,7 +122,7 @@ void matrix_init(void) {
         raw_matrix[i] = 0;
         matrix[i] = 0;
     }
-    debounce_init();
+    debounce_init(MATRIX_ROWS);
 
     matrix_init_quantum();
 }
@@ -143,7 +143,7 @@ uint8_t matrix_scan(void)
   }
 #endif
 
-  debounce(raw_matrix, matrix, changed);
+  debounce(raw_matrix, matrix, MATRIX_ROWS, changed);
 
   matrix_scan_quantum();
   return 1;
