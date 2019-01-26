945049ca99a5fb84dbdb806b2e3f1f9de2a64a12
Eric <ericduns@gmail.com>
2019-01-26 00:55:17 -0700

    fixed keymap files

--- keyboards/southpole/keymaps/default/keymap.c
+++ keyboards/southpole/keymaps/default/keymap.c
@@ -18,51 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-	keyevent_t event = record->event;
-
-	switch (id) {
-
-	}
-	return MACRO_NONE;
-}
-
 void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
-}
-
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-		
-	} else {
-		
-	}
-
 }
