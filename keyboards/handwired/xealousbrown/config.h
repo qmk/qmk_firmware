65d6ae10b2a325cedc9fed8a6df1697bf7b6885d
alex-ong <the.onga@gmail.com>
2019-01-27 07:59:00 +1100

    Changed to pragma once in config.h

--- keyboards/handwired/xealousbrown/config.h
+++ keyboards/handwired/xealousbrown/config.h
@@ -14,9 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #include "config_common.h"
 
@@ -51,5 +49,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key combination for magic key command */
 #define IS_COMMAND() ( keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
-
-#endif
