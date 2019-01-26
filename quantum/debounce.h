eb4359f9295d6c793f359fd812ce1b473a406e96
alex-ong <the.onga@gmail.com>
2019-01-26 17:46:27 +1100

    Restored debounce.h since there wasnt any real change.

--- quantum/debounce.h
+++ quantum/debounce.h
@@ -1,26 +1,13 @@
 #pragma once
-/*
-Copyright 2017 Alex Ong<the.onga@gmail.com>
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 
 #include "matrix.h"
 
-void debounce_init(uint8_t num_rows); //every debounce algorithm will have unique storage needs.
-
 // raw is the current key state
-// cooked is the debounced input/output key state
+// on entry cooked is the previous debounced state
+// on exit cooked is the current debounced state
 // changed is true if raw has changed since the last call
 void debounce(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, bool changed);
 
 bool debounce_active(void);
+
+void debounce_init(uint8_t num_rows);
