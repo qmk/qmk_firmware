743d60cfeec887cb8851397d5d41faef1a04465b
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-01-26 02:30:01 -0700

    Update keyboards/southpole/keymaps/foobeard/keymap.c
    Co-Authored-By: ericduns <ericduns@gmail.com>

--- keyboards/southpole/keymaps/foobeard/keymap.c
+++ keyboards/southpole/keymaps/foobeard/keymap.c
@@ -13,7 +13,7 @@ You should have received a copy of the GNU General Public LicensezZZ
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "southpole.h"
+#include QMK_KEYBOARD_H
 
 enum layers {
   _QWERTY,
