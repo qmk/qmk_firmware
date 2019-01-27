9900c0e142ba7cba2155fc37ddfe27cfe3ae471e
Andrew Kannan <andrew.kannan@gmail.com>
2019-01-27 15:21:17 -0500

    EEPROM implementation fix and updated p60 code

--- keyboards/handwired/practice60/practice60.c
+++ keyboards/handwired/practice60/practice60.c
@@ -3,8 +3,6 @@
 
 #include "ch.h"
 #include "hal.h"
-
-// #include "underglow.h"
 #include "led_custom.h"
 #include "print.h"
 #include "debug.h"
@@ -17,14 +15,13 @@
 #define LED_TGL()   do { palTogglePad(GPIOC, 13); } while (0)
 #endif
 
+// static uint8_t writeBL = 0;
+
 void matrix_init_kb(void){
       /* MOSI pin*/
     palSetPadMode(GPIOB, 15, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
-
     LED_ON();
-    palSetPad(GPIOA, 8);
     wait_ms(500);
-    palClearPad(GPIOA, 8);
     LED_OFF();
 
 #ifdef RGBLIGHT_ENABLE
@@ -32,7 +29,8 @@ void matrix_init_kb(void){
 #endif
 }
 
-void matrix_scan_kb(void){
+void matrix_scan_kb(void)
+{
   #ifdef RGBLIGHT_ENABLE
     rgblight_task();
   #endif
