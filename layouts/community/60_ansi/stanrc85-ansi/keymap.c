6c2ceb9e4bb2c10c816d18e7ae8323c31aa5ee41
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2019-01-27 11:01:46 -0500

    Modified Copyright information

--- layouts/community/60_ansi/stanrc85-ansi/keymap.c
+++ layouts/community/60_ansi/stanrc85-ansi/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 MechMerlin
+/* Copyright 2018 Stanrc85
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 #define DEFAULT 0 	//Custom ANSI
-#define LAYER1 1 	//Default ANSI
+#define LAYER1 1 	//Default ANSI (enable with Fn2+D)
 #define LAYER2 2 	//F keys, arrows, custom shortcuts, volume control
 #define LAYER3 3 	//Backlight controls and RESET
 
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
 		KC_TAB,KC_Q,KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , KC_RALT, MO(2), MO(3), KC_RCTL),
 
 	[2] = LAYOUT_60_ansi(
