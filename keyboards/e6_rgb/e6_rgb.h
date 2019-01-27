5bac4cffef93319e48d0fee612efc9b2e76626bd
Lei Yu <yuleiz@gmail.com>
2019-01-27 22:08:40 +0800

    rename 60_split_bs_shift to 60_ansi_split_bs_shift

--- keyboards/e6_rgb/e6_rgb.h
+++ keyboards/e6_rgb/e6_rgb.h
@@ -67,7 +67,7 @@
   {k30, k32, k33, k34, k35, k36, k37, k38, KC_NO, k39, k3a, k3b, k3c, k3d}, \
   {k40, k42, k43, KC_NO, KC_NO, KC_NO, k47, KC_NO, KC_NO, KC_NO, KC_NO, k49, k4a, k4b}  \
 }
-#define LAYOUT_60_split_bs_shift( \
+#define LAYOUT_60_ansi_split_bs_shift( \
   k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, \
 	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1e, \
 	k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2e, \
