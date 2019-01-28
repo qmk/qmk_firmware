b09e5509b636a6393e08f1fef9f29ad832d62fff
Konstantin Đorđević <vomindoraan@gmail.com>
2019-01-28 22:32:23 +0100

    Remove #define IS_COMMAND line in keyboards/handwired/xealousbrown/config.h

--- keyboards/handwired/xealousbrown/config.h
+++ keyboards/handwired/xealousbrown/config.h
@@ -46,6 +46,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-/* key combination for magic key command */
-// #define IS_COMMAND() ( keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
