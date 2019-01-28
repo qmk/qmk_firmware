0721c596842d965b79dc2f392c22987dd94d2e52
sai2791 <sai2791@googlemail.com>
2019-01-28 23:17:27 +0100

    Update to rules. Experimenting to save more space

--- keyboards/ergodox_ez/keymaps/sai2791/rules.mk
+++ keyboards/ergodox_ez/keymaps/sai2791/rules.mk
@@ -3,3 +3,9 @@
 SRC = matrix.c
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
+
+# Trying stuff  5950
+RGBLIGHT_ANIMATION = no  ## No difference
+UNICODE_ENABLE   = no ## saves 598 buyes
+#SLEEP_LED_ENABLE = no ## No Difference
+#API_SYSEX_ENABLE = no ## uses 3274 bytes
