e5b1f6f60b83659342f325610b9947770100dd8f
Harshit Goel <harshitgoel96@yahoo.com>
2019-01-29 18:07:23 +0530

    align layout name, added maintainer username

--- keyboards/ymd96/keymaps/hgoel89/keymap.c
+++ keyboards/ymd96/keymaps/hgoel89/keymap.c
@@ -27,7 +27,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define _DEFLT 0
 #define _RAISE 1
 
-#define KEYMAP KEYMAP_CUSTOM
+#define KEYMAP LAYOUT_CUSTOM
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
