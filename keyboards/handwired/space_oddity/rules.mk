02180c87f2618d6be33f0c171b48418e2b88c800
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-01-28 17:09:58 -0800

    handwired/space_oddity: refactor, Configurator support and readme cleanup (#4977)
    * handwired/space_oddity: refactor
    
    - config.h: updated to use #pragma once include guard
    - rules.mk: replace `?=` with `=`
    - space_oddity.h:
      - updated to use #pragma once include guard
      - layout macro KEYMAP renamed to LAYOUT
    - default keymap:
      - updated to use #include QMK_KEYBOARD_H
      - replace references to Planck keycodes
      - replace fn_actions-powered layer switch keycode
      - rebuild keymap block comments
      - keymap layers now use short-form keycodes
      - refactor action_get_macro keycodes into process_record_user
    
    * handwired/space_oddity: Configurator support
    
    * handwired/space_oddity: readme cleanup
    
    - update readme to better conform with QMK template
    - fixed the make command.

--- keyboards/handwired/space_oddity/rules.mk
+++ keyboards/handwired/space_oddity/rules.mk
@@ -35,7 +35,7 @@ F_USB = $(F_CPU)
 
 # Bootloader
 #     This definition is optional, and if your keyboard supports multiple bootloaders of
-#     different sizes, comment this out, and the correct address will be loaded 
+#     different sizes, comment this out, and the correct address will be loaded
 #     automatically (+60). See bootloader.mk for all options.
 BOOTLOADER = caterina
 
@@ -45,14 +45,14 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE ?= yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
-EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
-CONSOLE_ENABLE ?= no	# Console for debug(+400)
-COMMAND_ENABLE ?= no    # Commands for debug and configuration
-SLEEP_LED_ENABLE ?= no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
-AUDIO_ENABLE ?= no
-RGBLIGHT_ENABLE ?= no
+BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = yes      # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes      # Audio control and System control(+450)
+CONSOLE_ENABLE = no        # Console for debug(+400)
+COMMAND_ENABLE = no        # Commands for debug and configuration
+SLEEP_LED_ENABLE = no      # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes          # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
 TAP_DANCE_ENABLE = yes
