37d678304db4ea8efff817ca8eb25b69765af52d
Andrew Kannan <andrew.kannan@klaviyo.com>
2019-01-29 23:04:20 -0500

    Moving cannonkeys boards to one place, eeprom fix (#4999)
    * Move boards to cannonkeys and share resources
    
    * Share common files between cannonkey boards
    
    * Fix ortho60 keymap
    
    * update LED numbers
    
    * Add RGB keys to Ortho60 and Ortho48
    
    * Add Backlight control to default layout Ortho60 and 48
    
    * Remove unnecessary ws2812.c SRC from rules.mk

--- keyboards/cannonkeys/bluepill/keyboard.c
+++ keyboards/cannonkeys/bluepill/keyboard.c
@@ -1,15 +1,9 @@
-
-#include "ortho60.h"
-
 #include "ch.h"
 #include "hal.h"
-
-#include "underglow.h"
-#include "print.h"
-#include "debug.h"
+#include "led_custom.h"
 #include "util.h"
+#include "quantum.h"
 
-/* generic STM32F103C8T6 board */
 #ifdef BOARD_GENERIC_STM32_F103
 #define LED_ON()    do { palClearPad(GPIOC, 13) ;} while (0)
 #define LED_OFF()   do { palSetPad(GPIOC, 13); } while (0)
@@ -19,14 +13,18 @@
 void matrix_init_kb(void){
       /* MOSI pin*/
     palSetPadMode(GPIOB, 15, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
-
     LED_ON();
-    palSetPad(GPIOA, 8);
     wait_ms(500);
-    palClearPad(GPIOA, 8);
     LED_OFF();
 
+#ifdef RGBLIGHT_ENABLE
     leds_init();
+#endif
+}
 
-
+void matrix_scan_kb(void)
+{
+  #ifdef RGBLIGHT_ENABLE
+    rgblight_task();
+  #endif
 }
