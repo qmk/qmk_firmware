18c8c58af28e36dc05d241a9a473cdffbeb78706
Boy-314 <boy.314.yee.willie@gmail.com>
2019-01-30 14:06:41 -0500

    fixed .h file

--- keyboards/40percentclub/half_n_half/half_n_half.h
+++ keyboards/40percentclub/half_n_half/half_n_half.h
@@ -36,13 +36,13 @@
     K70, K71, K72, K73, K74, K75, K76, \
               K82 \
 ) { \
-    { AC_##K00, AC_##K01, AC_##K02, AC_##K03, AC_##K04, AC_##K05, AC_##K06 }, \
-    { AC_##K10, AC_##K11, AC_##K12, AC_##K13, AC_##K14, AC_##K15, AC_##K16 }, \
-    { AC_##K20, AC_##K21, AC_##K22, AC_##K23, AC_##K24, AC_##K25, AC_##K26 }, \
-    { KC_NO,    KC_NO,    KC_NO,    KC_NO,    AC_##K34, KC_NO,    KC_NO    }, \
+    { K00,   K01,   K02,   K03,   K04, K05,   K06   }, \
+    { K10,   K11,   K12,   K13,   K14, K15,   K16   }, \
+    { K20,   K21,   K22,   K23,   K24, K25,   K26   }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, K34, KC_NO, KC_NO }, \
 \
-    { AC_##K56, AC_##K55, AC_##K54, AC_##K53, AC_##K52, AC_##K51, AC_##K50 }, \
-    { AC_##K66, AC_##K65, AC_##K64, AC_##K63, AC_##K62, AC_##K61, AC_##K60 }, \
-    { AC_##K76, AC_##K75, AC_##K74, AC_##K73, AC_##K72, AC_##K71, AC_##K70 }, \
-    { KC_NO,    KC_NO,    KC_NO,    KC_NO,    AC_##K82, KC_NO,    KC_NO    } \
+    { K56,   K55,   K54,   K53,   K52, K51,   K50   }, \
+    { K66,   K65,   K64,   K63,   K62, K61,   K60   }, \
+    { K76,   K75,   K74,   K73,   K72, K71,   K70   }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, K82, KC_NO, KC_NO }  \
 }
