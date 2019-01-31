e54f1e0e46557147d1235f47ba411c60df18899f
Tracy Meade <meade.tracy@gmail.com>
2019-01-31 15:06:58 -0800

    update auto detect connection logic

--- keyboards/laptreus/laptreus.c
+++ keyboards/laptreus/laptreus.c
@@ -2,12 +2,5 @@
 
 void matrix_init_kb() {
   // auto detect output on init
-  #ifdef MODULE_ADAFRUIT_BLE
-    uint8_t output = auto_detect_output();
-    if (output == OUTPUT_USB) {
-      set_output(OUTPUT_USB);
-    } else {
-      set_output(OUTPUT_BLUETOOTH);
-    }
-  #endif
+  set_output(OUTPUT_AUTO);
 }
