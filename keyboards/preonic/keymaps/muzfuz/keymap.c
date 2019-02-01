f452f0a119d2482362baa1204f3997c450e5d152
Mazin Power <muzfuz@users.noreply.github.com>
2019-02-01 20:44:57 +0000

    [Keymap] Adds Preonic keymap for muzfuz. Small Planck addition. (#5019)
    * Adds keymaps for muzfuz
    
    * Remove unused keys from adjust layer
    
    * Bring in line with current QMK standards.
    
    * Adds Preonic and adjusted Planck keymaps
    
    * Make changes per review request

--- keyboards/preonic/keymaps/muzfuz/keymap.c
+++ keyboards/preonic/keymaps/muzfuz/keymap.c
@@ -17,21 +17,19 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-extern keymap_config_t keymap_config;
-
-enum planck_layers
-{
+enum preonic_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
   _ADJUST
 };
 
-enum planck_keycodes
+enum preonic_keycodes
 {
   QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
   BACKLIT,
-  EXT_PLV,
   HASHRKT,
   CLNEQLS
 };
@@ -44,16 +42,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Qwerty
  * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   \  |
+ * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | ESCCTL  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |    Enter    |     Space   | Left | Down |  Up  |Right |
+ * | Brite| Ctrl | Alt  | GUI  |Lower | Enter|Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-    [_QWERTY] = LAYOUT_planck_grid(
+    [_QWERTY] = LAYOUT_preonic_grid(
+        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         ESC_CTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
@@ -61,40 +62,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
+ * ,-----------------------------------------------------------------------------------.
+ * |      |  =>  |      |   [  |   ]  |      |      |   _  |   +  |   |   |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |ESCCTL|  :=  |      |  {   |  }   |   (  |   )  |   -  |   =  |   \  |   ~  | GUI |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  =>  |      |  [   |  ]   |     |     |   _  |   +  |  |   | Home  |  End  |
+ * |      |      |      |  [   |  ]   |     |     |      |      |      | Home  |  End  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |            | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-
-    [_LOWER] = LAYOUT_planck_grid(
-        KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    [_LOWER] = LAYOUT_preonic_grid(
+        KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        _______, HASHRKT, _______, KC_LBRC, KC_RBRC, _______, _______, KC_UNDS, KC_PLUS, KC_PIPE, _______, _______,
         ESC_CTL, CLNEQLS, _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_BSLS, KC_TILD, KC_RGUI,
-        _______, HASHRKT, _______, KC_LBRC, KC_RBRC, _______, _______, KC_UNDS, KC_PLUS, KC_PIPE, KC_HOME, KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
 
     /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |      |      |  Up  |      |      |      |   7  |   8  |   9  |   0  | Bksp |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |  Up  |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |ESCCTL|     | Left | Down | Right |      |      |   4  |   5  |   6  |      | GUI  |
+ * |ESCCTL|     | Left | Down | Right |      |      |      |      |      |      | GUI  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |  1   |  2   |  3   |     |      |
+ * |      |      |      |      |      |      |      |      |      |      |     |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |             | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-    [_RAISE] = LAYOUT_planck_grid(
-        KC_GRV, _______, _______, KC_UP, _______, _______, _______, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-        ESC_CTL, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_4, KC_5, KC_6, _______, KC_RGUI,
-        _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3, _______, _______,
+    [_RAISE] = LAYOUT_preonic_grid(
+        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______,
+        _______, _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, _______, _______,
+        ESC_CTL, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, KC_RGUI,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
 
     /* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -102,10 +110,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |             |             |      |      |      |      |
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-    [_ADJUST] = LAYOUT_planck_grid(
+    [_ADJUST] = LAYOUT_preonic_grid(
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
         _______, RESET, DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
         _______, _______, MU_MOD, AU_ON, AU_OFF, _______, _______, QWERTY, _______, _______, _______, _______,
         _______, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, TERM_ON, TERM_OFF, _______, _______, _______,
@@ -113,20 +122,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-#ifdef AUDIO_ENABLE
-float plover_song[][2] = SONG(PLOVER_SOUND);
-float plover_gb_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
-#endif
-
-uint32_t layer_state_set_user(uint32_t state)
-{
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record)
-{
-  switch (keycode)
-  {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
   case HASHRKT:
     if (record->event.pressed)
     {
@@ -142,8 +139,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
   case QWERTY:
     if (record->event.pressed)
     {
-      print("mode just switched to qwerty and this is a huge string\n");
       set_single_persistent_default_layer(_QWERTY);
+    }
+    return false;
+    break;
+  case LOWER:
+    if (record->event.pressed)
+    {
+      layer_on(_LOWER);
+      update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    }
+    else
+    {
+      layer_off(_LOWER);
+      update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    }
+    return false;
+    break;
+  case RAISE:
+    if (record->event.pressed)
+    {
+      layer_on(_RAISE);
+      update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    }
+    else
+    {
+      layer_off(_RAISE);
+      update_tri_layer(_LOWER, _RAISE, _ADJUST);
     }
     return false;
     break;
@@ -151,25 +173,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     if (record->event.pressed)
     {
       register_code(KC_RSFT);
-#ifdef BACKLIGHT_ENABLE
-      backlight_step();
-#endif
-#ifdef KEYBOARD_planck_rev5
-      PORTE &= ~(1 << 6);
-#endif
-    }
-    else
-    {
-      unregister_code(KC_RSFT);
-#ifdef KEYBOARD_planck_rev5
-      PORTE |= (1 << 6);
-#endif
-    }
-    return false;
-    break;
-  }
-  return true;
-}
+            #ifdef BACKLIGHT_ENABLE
+              backlight_step();
+            #endif
+            #ifdef __AVR__
+            PORTE &= ~(1<<6);
+            #endif
+          } else {
+            unregister_code(KC_RSFT);
+            #ifdef __AVR__
+            PORTE |= (1<<6);
+            #endif
+          }
+          return false;
+          break;
+      }
+    return true;
+};
 
 bool muse_mode = false;
 uint8_t last_muse_note = 0;
@@ -177,121 +197,75 @@ uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
 
-void encoder_update(bool clockwise)
-{
-  if (muse_mode)
-  {
-    if (IS_LAYER_ON(_RAISE))
-    {
-      if (clockwise)
-      {
+void encoder_update(bool clockwise) {
+  if (muse_mode) {
+    if (IS_LAYER_ON(_RAISE)) {
+      if (clockwise) {
         muse_offset++;
-      }
-      else
-      {
+      } else {
         muse_offset--;
       }
-    }
-    else
-    {
-      if (clockwise)
-      {
-        muse_tempo += 1;
-      }
-      else
-      {
-        muse_tempo -= 1;
+    } else {
+      if (clockwise) {
+        muse_tempo+=1;
+      } else {
+        muse_tempo-=1;
       }
     }
-  }
-  else
-  {
-    if (clockwise)
-    {
-#ifdef MOUSEKEY_ENABLE
-      register_code(KC_MS_WH_DOWN);
-      unregister_code(KC_MS_WH_DOWN);
-#else
+  } else {
+    if (clockwise) {
       register_code(KC_PGDN);
       unregister_code(KC_PGDN);
-#endif
-    }
-    else
-    {
-#ifdef MOUSEKEY_ENABLE
-      register_code(KC_MS_WH_UP);
-      unregister_code(KC_MS_WH_UP);
-#else
+    } else {
       register_code(KC_PGUP);
       unregister_code(KC_PGUP);
-#endif
     }
   }
 }
 
-void dip_update(uint8_t index, bool active)
-{
-  switch (index)
-  {
-  case 0:
-    if (active)
-    {
-#ifdef AUDIO_ENABLE
-      PLAY_SONG(plover_song);
-#endif
-      layer_on(_ADJUST);
-    }
-    else
-    {
-#ifdef AUDIO_ENABLE
-      PLAY_SONG(plover_gb_song);
-#endif
-      layer_off(_ADJUST);
-    }
-    break;
-  case 1:
-    if (active)
-    {
-      muse_mode = true;
-    }
-    else
-    {
-      muse_mode = false;
-#ifdef AUDIO_ENABLE
-      stop_all_notes();
-#endif
-    }
-  }
-}
-
-void matrix_scan_user(void)
-{
-#ifdef AUDIO_ENABLE
-  if (muse_mode)
-  {
-    if (muse_counter == 0)
-    {
-      uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-      if (muse_note != last_muse_note)
-      {
-        stop_note(compute_freq_for_midi_note(last_muse_note));
-        play_note(compute_freq_for_midi_note(muse_note), 0xF);
-        last_muse_note = muse_note;
+void dip_update(uint8_t index, bool active) {
+  switch (index) {
+    case 0:
+      if (active) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
       }
-    }
-    muse_counter = (muse_counter + 1) % muse_tempo;
-  }
-#endif
+      break;
+    case 1:
+      if (active) {
+        muse_mode = true;
+      } else {
+        muse_mode = false;
+        #ifdef AUDIO_ENABLE
+          stop_all_notes();
+        #endif
+      }
+   }
 }
 
-bool music_mask_user(uint16_t keycode)
-{
-  switch (keycode)
-  {
-  case RAISE:
-  case LOWER:
-    return false;
-  default:
-    return true;
+void matrix_scan_user(void) {
+  #ifdef AUDIO_ENABLE
+    if (muse_mode) {
+      if (muse_counter == 0) {
+        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+        if (muse_note != last_muse_note) {
+          stop_note(compute_freq_for_midi_note(last_muse_note));
+          play_note(compute_freq_for_midi_note(muse_note), 0xF);
+          last_muse_note = muse_note;
+        }
+      }
+      muse_counter = (muse_counter + 1) % muse_tempo;
+    }
+  #endif
+}
+
+bool music_mask_user(uint16_t keycode) {
+  switch (keycode) {
+    case RAISE:
+    case LOWER:
+      return false;
+    default:
+      return true;
   }
 }
