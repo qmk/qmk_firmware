3b287a82349735788e8f374ac89c10a9975122b8
p <_>
2019-02-02 01:46:04 +0900

    Remove IS_COMMAND macro from switch42 firmware
    see b05c0e46c6c62972b68518635104fb58927d1a13

--- keyboards/switch42/rev1/config.h
+++ keyboards/switch42/rev1/config.h
@@ -50,11 +50,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 //#define LOCKING_RESYNC_ENABLE
 
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 #define RGBLED_NUM ((MATRIX_ROWS >> 1) * MATRIX_COLS - (MATRIX_COLS >> 1)) // Number of LEDs
