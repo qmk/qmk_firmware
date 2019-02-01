8c5c1fd7fe8866f61081ec4cf953f48352d7c088
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-01-31 16:37:13 -0800

    Space65 Updates (#5008)
    * update reset sequence info
    
    * fix my rgb underglow led number flub
    
    * add a more sensible keymap
    
    * add caps lock led support
    
    * remove unused #define
    
    * swap the spacebar sizes around to match the space65
    
    * use vomindoraan's suggestion in PR to use IS_LED_ON

--- keyboards/gray_studio/space65/space65.c
+++ keyboards/gray_studio/space65/space65.c
@@ -38,6 +38,11 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 
 void led_set_kb(uint8_t usb_led) {
 	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+  if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+    writePinHigh(E6);
+  } else {
+    writePinLow(E6);
+  }
 
 	led_set_user(usb_led);
 }
