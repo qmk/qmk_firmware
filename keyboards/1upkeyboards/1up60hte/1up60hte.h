67e83a2701069cf50b75f4e4f0480706fc6ae8e1
Braydon Bubnick <braydon.bubnick@gmail.com>
2019-02-04 11:56:38 -0600

    Fixed errant KC_NO in layout

--- keyboards/1upkeyboards/1up60hte/1up60hte.h
+++ keyboards/1upkeyboards/1up60hte/1up60hte.h
@@ -39,6 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, \
 	K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212,       \
 	K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311, K312,       \
-	KC_NO, K401, K402,                  K406,                  K410, K411, K412, KC_NO  \
+	KC_NO, K401, K402,                  K406,                  K410, K411, K412, K413  \
 )
 
