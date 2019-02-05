3d93c10003c217922d6c6ad9537a021a73d42774
p <_>
2019-02-05 09:32:12 +0900

    Bugfix: Make wheel accel also respect the MOUSEKEY_ACCEL*_SPEED macros

--- tmk_core/common/mousekey.c
+++ tmk_core/common/mousekey.c
@@ -87,11 +87,11 @@ static uint8_t wheel_unit(void)
 {
     uint16_t unit;
     if (mousekey_accel & (1<<0)) {
-        unit = (MOUSEKEY_WHEEL_DELTA * mk_wheel_max_speed)/4;
+        unit = MOUSEKEY_ACCEL0_SPEED((MOUSEKEY_WHEEL_DELTA * mk_wheel_max_speed));
     } else if (mousekey_accel & (1<<1)) {
-        unit = (MOUSEKEY_WHEEL_DELTA * mk_wheel_max_speed)/2;
+        unit = MOUSEKEY_ACCEL1_SPEED((MOUSEKEY_WHEEL_DELTA * mk_wheel_max_speed));
     } else if (mousekey_accel & (1<<2)) {
-        unit = (MOUSEKEY_WHEEL_DELTA * mk_wheel_max_speed);
+        unit = MOUSEKEY_ACCEL2_SPEED((MOUSEKEY_WHEEL_DELTA * mk_wheel_max_speed));
     } else if (mousekey_repeat == 0) {
         unit = MOUSEKEY_WHEEL_DELTA;
     } else if (mousekey_repeat >= mk_wheel_time_to_max) {
