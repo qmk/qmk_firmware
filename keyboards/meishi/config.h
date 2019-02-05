e3c1d13855339c8fad7cb978fb8f30a14175068c
Biacco42 <biacco42@gmail.com>
2019-02-06 00:28:09 +0900

    Fix copyright year

--- keyboards/meishi/config.h
+++ keyboards/meishi/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Biacco42
+Copyright 2019 Biacco42
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
