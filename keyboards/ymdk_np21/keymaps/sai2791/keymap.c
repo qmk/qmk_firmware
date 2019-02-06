7d510950fdb6d19a60023cee1829adf2f1d99aac
Steve Inglis <sai2791@googlemail.com>
2019-02-06 15:38:35 +0000

    Sorted Key layout ASCII maps, also figured out which keymaps need to be set for the 2u buttons

--- keyboards/ymdk_np21/keymaps/sai2791/keymap.c
+++ keyboards/ymdk_np21/keymaps/sai2791/keymap.c
@@ -11,66 +11,81 @@ enum custom_keycodes {
 
 //Tap Dance Config
 enum {
-  TD_A_B = 0
+  TD_A_B = 0,
+  TD_C_D
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap Enter for A, twice for B
-  [TD_A_B] = ACTION_TAP_DANCE_DOUBLE(KC_A,KC_B)
+  [TD_A_B] = ACTION_TAP_DANCE_DOUBLE(KC_A,KC_B),
   // Other declarations would go here, separated by commas, if you have them
+  [TD_C_D] = ACTION_TAP_DANCE_DOUBLE(KC_C,KC_D)
 };
 
+
+/* Note the Enter and Plus(+) key only need the second key
+ * in the sequence to work, if only the first is set then it
+ * will not work.
+ *
+ * for the zero (0) key, the upper level key needs to be set
+*/
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
- * ,-----------------------------------------.
- * |   0  |   1  |   4  |   7  | NUM  | ESC  |
- * |------+------+------+------+------+------|
- * |   .  |   2  |   5  |   8  |   /  | TAB  |---,
- * |------+------+------+------+------+------|USB|
- * |  DOT |   3  |   6  |   9  |   *  | BS   |---'
- * |------+------+------+------+------+------|
- * | Enter| Enter|   +  |   +  |   -  | FN   |
- * `-----------------------------------------'
+ * ,----------------------------------------------.
+ * |      |   1  |   4  |   7  | NUM  | ESC       |
+ * |  0   +------+------+------+------+-----------|
+ * |      |   2  |   5  |   8  |   /  | TAB       |---,
+ * |------+------+------+------+------+-----------|USB|
+ * |  DOT |   3  |   6  |   9  |   *  | BS        |---'
+ * |------+------+------+------+------+-----------|
+ * |     Enter   |      +      |   -  | TO(_BL)   |
+ * `----------------------------------------------'
  */
 [_NP] = LAYOUT_numpad_6x4( \
   KC_KP_0,     KC_KP_1,     KC_KP_4,    KC_KP_7,    KC_NUMLOCK,     KC_ESC,    \
-  KC_DOT,      KC_KP_2,     KC_KP_5,    KC_KP_8,    KC_KP_SLASH,    KC_TAB,    \
+  KC_NO,       KC_KP_2,     KC_KP_5,    KC_KP_8,    KC_KP_SLASH,    KC_TAB,    \
   KC_KP_DOT,   KC_KP_3,     KC_KP_6,    KC_KP_9,    KC_KP_ASTERISK, KC_BSPACE, \
-  KC_KP_ENTER, KC_ENTER, KC_KP_PLUS, KC_PLUS, KC_KP_MINUS,    TO(_BL)     \
+  KC_KP_ENTER, KC_KP_ENTER, KC_KP_PLUS, KC_PLUS,    KC_KP_MINUS,    TO(_BL)    \
 ),
-/* Qwerty
- * ,---------------------------------------------.
- * | BL_BRTG |        |         |        |   |   |
- * |---------+--------+---------+--------+---+---|
- * |         | BL_OFF | BL_TOGG | BL_ON  |   |   |---,
- * |---------+--------+---------+--------+---+---|USB|
- * |         |        |         |        |   |   |---'
- * |---------+--------+---------+--------+---+---|
- * | BL_DEC  | BL_DEC | BL_INC  | BL_INC |   |   |
- * `---------------------------------------------'
+/* Backlight
+ * ,----------------------------------------------------.
+ * | BL_BRTG |        |         |        |   | //////// |
+ * |---------+--------+---------+--------+---+----------|
+ * |         | BL_OFF | BL_TOGG | BL_ON  |   | TO(_MP)  |---,
+ * |---------+--------+---------+--------+---+----------|USB|
+ * |         |        |         |        |   | //////// |---'
+ * |---------+--------+---------+--------+---+----------|
+ * | BL_DEC  | BL_DEC | BL_INC  | BL_INC |   | //////// |
+ * `----------------------------------------------------'
  */
 [_BL] = LAYOUT_numpad_6x4( \
-  BL_BRTG, _______, _______, _______, _______, KC_NO, \
-  _______, BL_OFF,  BL_TOGG, BL_ON,   _______, TO(_NP), \
-  _______, _______, _______, _______, _______, TO(_MP), \
-  BL_DEC,  BL_DEC,  BL_INC,  BL_INC,  _______, _______  \
+  BL_BRTG, _______, _______, _______, _______, KC_NO,   \
+  _______, BL_OFF,  BL_TOGG, BL_ON,   _______, TO(_MP), \
+  _______, _______, _______, _______, _______, KC_NO,   \
+  BL_DEC,  BL_DEC,  BL_INC,  BL_INC,  _______, KC_NO    \
 ),
-/* Qwerty
- * ,---------------------------------------------.
- * | BL_BRTG |        |         |        |   |   |
- * |-------+--------+---------+--------+---+---|
- * |         | BL_OFF | BL_TOGG | BL_ON  |   |   |---,
- * |---------+--------+---------+--------+---+---|USB|
- * |         |        |         |        |   |   |---'
- * |---------+--------+---------+--------+---+---|
- * | BL_DEC  | BL_DEC | BL_INC  | BL_INC |   |   |
- * `---------------------------------------------'
+/* Macropad
+ * ,----------------------------------------------------.
+ * |         |        |         |        |   | TO(_NP)  |
+ * |         +--------+---------+--------+---+----------|
+ * |         |        |         |        |   | //////   |---,
+ * |---------+--------+---------+--------+---+----------|USB|
+ * |         |        |         |        |   |          |---'
+ * |---------+--------+---------+--------+---+----------|
+ * |     TD(A,B)      |      TD(C,D)     |   |          |
+ * `----------------------------------------------------'
  */
+/* Note the Enter and Plus(+) key only need the second key
+ * in the sequence to work, if only the first is set then it
+ * will not work.
+ *
+ * for the zero (0) key, the lower level key needs to be set
+*/
 [_MP] = LAYOUT_numpad_6x4( \
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(_NP), \
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(_BL),  \
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  \
-  TD(TD_A_B), TD(TD_A_B), KC_NO, KC_NO,  KC_NO, KC_NO  \
+  TD(TD_C_D), KC_NO, KC_NO, KC_NO, KC_NO, TO(_NP),                     \
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            \
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            \
+  KC_NO, TD(TD_A_B), KC_NO, TD(TD_C_D),  KC_NO, KC_NO                  \
 )
 };
 
