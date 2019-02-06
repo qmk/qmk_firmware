0d318692f67a91ddb9bd25ed595803c3926f2cbe
Andrew Kannan <andrew.kannan@gmail.com>
2019-02-05 20:47:42 -0500

    eeprom changes

--- keyboards/handwired/satisfaction75/rules.mk
+++ keyboards/handwired/satisfaction75/rules.mk
@@ -47,6 +47,6 @@ NKRO_ENABLE = yes	    # USB Nkey Rollover
 CUSTOM_MATRIX = no # Custom matrix file
 ENCODER_ENABLE = yes
 QWIIC_ENABLE += MICRO_OLED
-# BACKLIGHT_ENABLE = yes
+BACKLIGHT_ENABLE = yes
 
 DEFAULT_FOLDER = handwired/satisfaction75
