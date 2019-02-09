239f3898b9bf1e51c1770ed3bf7e094047e02a0a
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-02-09 18:00:25 -0500

    Update keyboards/m0lly/m0lly.h
    Co-Authored-By: TerryMathews <terry@terrymathews.net>

--- keyboards/m0lly/m0lly.h
+++ keyboards/m0lly/m0lly.h
@@ -22,7 +22,7 @@
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      K1F, K1G, K1H, K1I, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,      K2F, K2G, K2H, K2I, \
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,    K3D,   K3F, K3G, K3H, K3I, \
-    K40, K41, K42,           K45,                K49, K4B,    K4C, K4D, K4F,   K4G, K4H, K4I  \
+    K40, K41, K42,           K45,                        K49, K4B, K4C, K4D,   K4F, K4G, K4H, K4I  \
 ) { \
     { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D,   K0E,   K0F,   K0G, K0H, K0I }, \
     { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D,   KC_NO, K1F,   K1G, K1H, K1I }, \
