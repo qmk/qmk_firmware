1a1c73386582ab0ac44b81f248720ac6bab741f0
Danny Nguyen <danny@keeb.io>
2019-02-11 15:43:08 -0500

    Update BDN9 files

--- keyboards/keebio/bdn9/config.h
+++ keyboards/keebio/bdn9/config.h
@@ -32,7 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 9
 
 /* Keyboard Matrix Assignments */
-#define NO_PIN (~0)
 #define MATRIX_ROW_PINS { NO_PIN }
 #define MATRIX_COL_PINS { D2, D4, F4, D7, B1, B3, E6, B4, B2 }
 
