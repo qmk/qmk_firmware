ddb0f39ebf317c7a23c69ed45efb1882746a01b0
Konstantin Đorđević <vomindoraan@gmail.com>
2019-02-11 23:21:41 +0100

    Generate UNICODE and UNICODEMAP constants using macros

--- users/konstantin/rules.mk
+++ users/konstantin/rules.mk
@@ -5,5 +5,8 @@ endif
 ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
   SRC += tap_dance.c
 endif
+ifneq (,$(filter yes,$(UNICODE_ENABLE) $(UNICODEMAP_ENABLE)))  # if either is yes
+  SRC += unicode.c
+endif
 
 EXTRAFLAGS += -flto
--- users/konstantin/unicode.h
+++ users/konstantin/unicode.h
@@ -2,10 +2,33 @@
 
 #include "quantum.h"
 
-#define COMMA   UC(0x002C)
-#define L_PAREN UC(0x0028)
-#define R_PAREN UC(0x0029)
-#define EQUALS  UC(0x003D)
-#define TIMES   UC(0x00D7)
-#define DIVIDE  UC(0x00F7)
-#define MINUS   UC(0x2212)
+#define FOREACH_UNICODE(M) \
+  M(COMMA,   0x002C)       \
+  M(L_PAREN, 0x0028)       \
+  M(R_PAREN, 0x0029)       \
+  M(EQUALS,  0x003D)       \
+  M(TIMES,   0x00D7)       \
+  M(DIVIDE,  0x00F7)       \
+  M(MINUS,   0x2212)
+
+#define UC_KEYCODE(name, code)  name = UC(code),
+
+#define X_NAME(name, code)    X_ ## name,
+#define X_ENTRY(name, code)   [X_ ## name] = code,
+#define X_KEYCODE(name, code) name = X(X_ ## name),
+
+#if defined(UNICODE_ENABLE)
+  enum unicode_keycodes {
+    FOREACH_UNICODE(UC_KEYCODE)
+  };
+#elif defined(UNICODEMAP_ENABLE)
+  enum unicode_names {
+    FOREACH_UNICODE(X_NAME)
+  };
+
+  extern const uint32_t PROGMEM unicode_map[];
+
+  enum unicode_keycodes {
+    FOREACH_UNICODE(X_KEYCODE)
+  };
+#endif
