0e054f031001681e7d9dc364d8500a8a2d05bb44
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-02-12 08:26:54 -0200

    Back with regular ctrl and gesc for now

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -33,9 +33,9 @@ const hue_sat_pair hue_sat_pairs[][2] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
-		KC_GRV,    KC_1,     KC_2,     KC_3,    KC_4,      KC_5,    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_DEL,
+		KC_GESC,   KC_1,     KC_2,     KC_3,    KC_4,      KC_5,    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_GRV,
 		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,      KC_T,    KC_Y,    KC_U,    KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
-		CTRLESC,   KC_A,     KC_S,     KC_D,    KC_F,      KC_G,    KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+		KC_LCTL,   KC_A,     KC_S,     KC_D,    KC_F,      KC_G,    KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
 		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,      KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  MO(_FN2),
 		MO(_FN1),  KC_LALT,  KC_LGUI,  SPCFN2,  MO(_FN2),  BKSFN1,  CLLEFT,  CLDOWN,  KC_NO,  CLUP,     CLRIGHT
   ),
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_KEY_OVERRIDE] = LAYOUT(
-		KC_ESC,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,  KC_EJCT,
+		KC_GRV,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,  KC_EJCT,
 		KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_DEL,
 		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PENT,
 		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
