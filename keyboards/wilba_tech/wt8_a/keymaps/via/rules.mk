c5e10b7203f140e747ac3a95b6964c52110e07df
Wilba <Jason.S.Williams@gmail.com>
2019-02-13 07:59:56 +1100

    Added WT75-A, refactor wilba.tech PCBs (#5100)
    * Added WT75-A, added "via" keymaps to wilba.tech
    
    * Missed file last commit

--- keyboards/wilba_tech/wt8_a/keymaps/via/rules.mk
+++ keyboards/wilba_tech/wt8_a/keymaps/via/rules.mk
@@ -1,8 +1,5 @@
 # project specific files
-SRC =	drivers/issi/is31fl3736.c \
-		drivers/avr/i2c_master.c \
-		keyboards/wilba_tech/wt_mono_backlight.c \
-		keyboards/wilba_tech/wt_main.c
+SRC =	keyboards/wilba_tech/wt_main.c
 		
 # MCU name
 MCU = atmega32u4
@@ -66,3 +63,6 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+
+RAW_ENABLE = yes
+DYNAMIC_KEYMAP_ENABLE = yes
