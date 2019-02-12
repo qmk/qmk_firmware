66b6bb1084d01f3245553e54218a63bf24c6b088
Steve Inglis <sai2791@googlemail.com>
2019-02-12 19:52:59 +0000

    Changed KC_NUMLOCK to KC_NLCK and it now shows up in keyboard viewer

--- keyboards/ymdk_np21/keymaps/sai2791/keymap.c
+++ keyboards/ymdk_np21/keymaps/sai2791/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_NP] = LAYOUT_numpad_6x4( \
-  KC_KP_0,     KC_KP_1,     KC_KP_4,    KC_KP_7,    KC_NUMLOCK,     KC_ESC,    \
+  KC_KP_0,     KC_KP_1,     KC_KP_4,    KC_KP_7,    KC_NLCK,     KC_ESC,    \
   KC_NO,       KC_KP_2,     KC_KP_5,    KC_KP_8,    KC_KP_SLASH,    KC_TAB,    \
   KC_KP_DOT,   KC_KP_3,     KC_KP_6,    KC_KP_9,    KC_KP_ASTERISK, KC_BSPACE, \
   KC_KP_ENTER, KC_KP_ENTER, KC_KP_PLUS, KC_PLUS,    KC_KP_MINUS,    TO(_BL)    \
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_BL] = LAYOUT_numpad_6x4( \
   BL_BRTG, _______, _______, _______, _______, KC_NO,   \
-  _______, BL_OFF,  BL_TOGG, BL_ON,   _______, TO(_MP), \
+  KC_NO, BL_OFF,  BL_TOGG, BL_ON,   _______, TO(_MP), \
   _______, _______, _______, _______, _______, KC_NO,   \
-  BL_DEC,  BL_DEC,  BL_INC,  BL_INC,  _______, KC_NO    \
+  KC_NO,  BL_DEC,  KC_NO,  BL_INC,  _______, KC_NO    \
 ),
 /* Macropad
  * ,----------------------------------------------------.
