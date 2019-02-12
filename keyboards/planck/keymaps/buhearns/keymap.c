42f85187a6b579c93e44628f84ae120f847cebfb
buhearns <47539568+buhearns@users.noreply.github.com>
2019-02-12 02:56:26 +0100

    Removing deprecated function
    with fauxpark's help

--- keyboards/planck/keymaps/buhearns/keymap.c
+++ keyboards/planck/keymaps/buhearns/keymap.c
@@ -3,8 +3,6 @@
 
 extern keymap_config_t keymap_config;
 
-#define B_ENT FUNC(0) // Tap for enter, hold for right shift
-
 enum custom_keycodes {
   B_UPD = SAFE_RANGE,
   B_SSH,
@@ -30,16 +28,12 @@ enum custom_keycodes {
   B_DE_3
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT)
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT_planck_grid(
     KC_GESC,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LT(5,KC_TAB),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(4,KC_BSLS),
-    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, B_ENT,
+    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_ENT),
     KC_LCTL,     KC_LGUI, KC_RALT, MO(3),   MO(2),   KC_SPC,  KC_SPC,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
