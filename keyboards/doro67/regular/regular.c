791bf689d413bc2eb27f1042a20421cec68ee7c0
mechmerlin <mechmerlin@gmail.com>
2019-02-12 20:36:40 -0800

    moved it all to a regular directory. It's called regular as that's what it's called in the group buy. In the future we will have other directories for the other PCBs that are delivered to customers.

--- keyboards/doro67/regular/regular.c
+++ keyboards/doro67/regular/regular.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 'mechmerlin'
+/* Copyright 2019 MechMerlin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "doro67.h"
+#include "regular.h"
 
 void matrix_init_kb(void) {
 	// put your keyboard start-up code here
