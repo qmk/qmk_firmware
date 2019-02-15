6cece19ab5d5c1cda3ab78a2fafdf72615c734ac
zvecr <git@zvecr.com>
2019-02-15 14:47:47 +0000

    Remove duplication of ssh keys and fix usage on PR builds (#5013)

--- util/travis_compiled_push.sh
+++ util/travis_compiled_push.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-TRAVIS_BRANCH="${TRAVIS_BRANCH:master}"
-TRAVIS_PULL_REQUEST="${TRAVIS_PULL_REQUEST:false}"
-TRAVIS_COMMIT_MESSAGE="${TRAVIS_COMMIT_MESSAGE:-none}"
-TRAVIS_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE:-HEAD~1..HEAD}"
+source util/travis_push.sh
 
 set -o errexit -o nounset
 
@@ -11,17 +8,6 @@ rev=$(git rev-parse --short HEAD)
 echo "Using git hash ${rev}"
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]] ; then
-
-git config --global user.name "QMK Bot"
-git config --global user.email "hello@qmk.fm"
-
-openssl aes-256-cbc -K $encrypted_b0ee987fd0fc_key -iv $encrypted_b0ee987fd0fc_iv -in secrets.tar.enc -out secrets.tar -d
-tar xvf secrets.tar
-
-chmod 600 id_rsa_qmk_firmware
-chmod 600 id_rsa_qmk.fm
-eval `ssh-agent -s`
-ssh-add id_rsa_qmk_firmware
 
 # convert to unix line-endings
 git checkout master
