59f2216e2fb7f0eed5d533c4455a878458b4cdab
Drashna Jaelre <drashna@live.com>
2019-02-17 11:22:51 +0900

    Update keyboards/hecomi/hecomi.h
    Co-Authored-By: takashiski <takashiskibb@gmail.com>

--- keyboards/hecomi/hecomi.h
+++ keyboards/hecomi/hecomi.h
@@ -69,7 +69,7 @@ Right hand:
 	{KC_NO,    KC_NO,    K92, K93, K94, K95, K96, K97}\
 }
 
-#define LAYOUT_KC(\
+#define LAYOUT_kc(\
 K00, K01, K02, K03, K04, K05, K06,        K50, K51, K52, K53, K54, K55, K56, K57, \
 K10,   K11, K12, K13, K14, K15, K16,    K60, K61, K62, K63, K64, K65, K66,   K67, \
 K20,    K21, K22, K23, K24, K25,          K71, K72, K73, K74, K75, K76,      K77,\
