6238a9c680fb1241c8ee41158d9809c1561ebefc
Konstantin Đorđević <vomindoraan@gmail.com>
2019-02-18 03:16:21 +0100

    Add GODSPEED_YELLOW color

--- users/konstantin/rgb.c
+++ users/konstantin/rgb.c
@@ -1,3 +1,4 @@
 #include "rgb.h"
 
-const hsv_t GODSPEED_BLUE = { .h = 300, .s = 68, .v = RGBLIGHT_LIMIT_VAL };
+const hsv_t GODSPEED_BLUE   = { .h = 300, .s = 68,  .v = RGBLIGHT_LIMIT_VAL };
+const hsv_t GODSPEED_YELLOW = { .h = 35,  .s = 165, .v = RGBLIGHT_LIMIT_VAL };
