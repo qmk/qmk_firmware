b0c01e29a433feba3c775f4184e359f732789bb0
Mike Roberts <miker@miker.org>
2019-02-18 16:35:52 -0500

    fix ble

--- keyboards/nek_type_a/rules.mk
+++ keyboards/nek_type_a/rules.mk
@@ -12,7 +12,7 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 BOOTLOADER = caterina
 
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
