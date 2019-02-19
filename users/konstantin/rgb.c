2f34a1f4d5174a76371050f024fd030c59ec82f7
Konstantin Đorđević <vomindoraan@gmail.com>
2019-02-19 15:20:11 +0100

    Update Godspeed colors

--- users/konstantin/rgb.c
+++ users/konstantin/rgb.c
@@ -20,5 +20,5 @@ const uint8_t RGBLED_SNAKE_INTERVALS[] PROGMEM = {20, 50, 100};
 const uint8_t RGBLED_KNIGHT_INTERVALS[] PROGMEM = {20, 50, 100};
 #endif
 
-const hsv_t GODSPEED_BLUE   = { .h = 300, .s = 68,  .v = RGBLIGHT_LIMIT_VAL };
-const hsv_t GODSPEED_YELLOW = { .h = 35,  .s = 165, .v = RGBLIGHT_LIMIT_VAL };
+const hsv_t GODSPEED_BLUE   = { .h = 280, .s = 68,  .v = RGBLIGHT_LIMIT_VAL };
+const hsv_t GODSPEED_YELLOW = { .h = 38,  .s = 153, .v = RGBLIGHT_LIMIT_VAL };
