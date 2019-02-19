9a64c6b82ef7fc46e35f7d8c35eeccf3961ba0bf
zvecr <git@zvecr.com>
2019-02-19 00:48:52 +0000

    [Keyboard] XD84 fixes for caps lock led, backlight and firmware size (#5178)
    * Enable backlight
    
    * Enable Link Time Optimization
    
    * Enable caps lock led

--- keyboards/xd84/rules.mk
+++ keyboards/xd84/rules.mk
@@ -70,7 +70,7 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality on B7 by default
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 UNICODE_ENABLE = no         # Unicode
@@ -82,5 +82,7 @@ HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
 # custom matrix setup
 CUSTOM_MATRIX = yes
 SRC = i2c_master.c custom_matrix_helper.c pca9555.c matrix.c
+
+EXTRAFLAGS += -flto
 
 LAYOUTS = 75_ansi 75_iso
--- keyboards/xd84/xd84.c
+++ keyboards/xd84/xd84.c
@@ -15,29 +15,18 @@
  */
 #include "xd84.h"
 
-void matrix_init_kb(void) {
-	// put your keyboard start-up code here
-	// runs once when the firmware starts up
+void keyboard_pre_init_kb(void) {
+  setPinOutput(B6);
 
-	matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-	// put your looping keyboard code here
-	// runs every cycle (a lot)
-
-	matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-	// put your per-action keyboard code here
-	// runs for every action, just before processing by the firmware
-
-	return process_record_user(keycode, record);
+  keyboard_pre_init_user();
 }
 
 void led_set_kb(uint8_t usb_led) {
-	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+  if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+    writePinLow(B6);
+  } else {
+    writePinHigh(B6);
+  }
 
-	led_set_user(usb_led);
+  led_set_user(usb_led);
 }
