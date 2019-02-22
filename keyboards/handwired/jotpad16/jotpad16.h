4b887b79ea8d57d7c3cf2d7d158e9cbe81db5315
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-02-22 09:00:03 -0300

    Update keyboards/handwired/jotpad16/jotpad16.h
    Co-Authored-By: jotix <47826561+jotix@users.noreply.github.com>

--- keyboards/handwired/jotpad16/jotpad16.h
+++ keyboards/handwired/jotpad16/jotpad16.h
@@ -3,7 +3,7 @@
 #include "quantum.h"
 
 // Used to create a keymap using only KC_ prefixed keys
-#define LAYOUT_jotpad16( \
+#define LAYOUT_ortho_4x4( \
 	k00, k01, k02, k03, \
 	k10, k11, k12, k13, \
 	k20, k21, k22, k23, \
