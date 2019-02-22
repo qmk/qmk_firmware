eccfb75f2ed25b9d4972043b3abef6b7bd112a14
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-02-22 09:00:15 -0300

    Update keyboards/handwired/jotpad16/keymaps/default/keymap.c
    Co-Authored-By: jotix <47826561+jotix@users.noreply.github.com>

--- keyboards/handwired/jotpad16/keymaps/default/keymap.c
+++ keyboards/handwired/jotpad16/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   0   |   .   |   /   |   *   |
  * +-------+-------+-------+-------+
  */
-[_NUMPAD] = LAYOUT_jotpad16 ( 
+[_NUMPAD] = LAYOUT_ortho_4x4( 
     KC_P7,  KC_P8,  KC_P9,  KC_MINS,
     KC_P4,  KC_P5,  KC_P6,  KC_PLUS,
     KC_P1,  KC_P2,  KC_P3,  KC_PENT,
