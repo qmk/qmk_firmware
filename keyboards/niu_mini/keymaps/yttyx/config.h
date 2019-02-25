1cf4293820bce44004ad0e60bc6fe4c19cfbb275
yttyx <yttyx@picasa.org.uk>
2019-02-25 09:50:14 +0000

    niu - Changes following review.

--- keyboards/niu_mini/keymaps/yttyx/config.h
+++ keyboards/niu_mini/keymaps/yttyx/config.h
@@ -1,12 +1,4 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-// Disable some options to reduce firmware size
-#define NO_ACTION_TAPPING
+#pragma once
 #define NO_ACTION_ONESHOT
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
-#define NO_DEBUG
-#define NO_PRINT
-
-#endif
