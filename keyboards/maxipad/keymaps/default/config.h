a965dda433458e5f94cb09c620da6325a6e10a77
zvecr <git@zvecr.com>
2019-02-25 00:33:03 +0000

    Initial refactor of maxipad to conform to current standards

--- keyboards/maxipad/keymaps/default/config.h
+++ keyboards/maxipad/keymaps/default/config.h
@@ -13,4 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "maxipad.h"
+
+#pragma once
+
+// place overrides here
