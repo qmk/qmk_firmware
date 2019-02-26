778bac55046d913b388889f8d9ab6968fc2a9062
Juno Nguyen <juno.ngx@gmail.com>
2019-02-26 21:35:19 +0800

    Further codes clean up.

--- keyboards/vitamins_included/keymaps/juno/keymap.c
+++ keyboards/vitamins_included/keymaps/juno/keymap.c
@@ -3,10 +3,6 @@
 
 extern keymap_config_t keymap_config;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _QWERTY 0
 #define _COLEMAK 1
 #define _DVORAK 2
@@ -31,7 +27,6 @@ enum custom_keycodes {
   DP_OFF
 };
 
-// Fillers to make layering more clear
 #define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -199,8 +194,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 											   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX   \
 )
 
-// underscore to allow modifiers holding and exiting the layer
-
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
@@ -257,11 +250,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
 	  
-	  
-	  
-	  
-	  
-	  
 	 // Additional personal custom functions	  
 
 	case KC_CAPS:
@@ -300,8 +288,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			//layer_off(_FN1); // do not put in this line
 			layer_off(_DPAD);	
 		}
-
-
   }
   return true;
 }
