2e5da5259997b30314d59c1fa4af2c65c5dd659e
Sterling <503326@MC02VP210HV2H.tld>
2019-02-27 08:51:38 -0800

    Tweaks to Atreus62 Keymap
    Added a layer for FPS RPG Loot Shooters.

--- keyboards/atreus62/keymaps/xyverz/keymap.c
+++ keyboards/atreus62/keymaps/xyverz/keymap.c
@@ -49,8 +49,9 @@ TODO:
 #define _QWERTY 1
 #define _COLEMAK 2
 #define _WOW 3
-#define _LOWER 4
-#define _RAISE 5
+#define _DESTINY 4
+#define _LOWER 5
+#define _RAISE 6
 #define _ADJUST 16
 
 enum atreus52_keycodes {
@@ -58,6 +59,7 @@ enum atreus52_keycodes {
   QWERTY,
   COLEMAK,
   WOW,
+  DESTINY,
   LOWER,
   RAISE
 };
@@ -102,6 +104,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LALT, KC_LEFT, KC_RGHT, LOWER,   KC_BSPC, KC_LALT, CTL_ENT, KC_SPC,  RAISE,   KC_UP,   KC_DOWN, KC_RGUI, KC_ENT
   ),
 
+  [_DESTINY] = LAYOUT ( /* Dvorak with minor modifications for playing Destiny 2 and other FPS Looters */
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+    KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+    KC_LCTL, KC_LALT, KC_LEFT, KC_RGHT, LOWER,   KC_BSPC, KC_DEL,  KC_ENT,  KC_SPC,  RAISE,   KC_UP,   KC_DOWN, KC_RGUI, KC_ENT
+  ),
+
   [_LOWER] = LAYOUT (
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
     KC_TILD, KC_GRV,  _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_PIPE,
@@ -119,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT (
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, RESET,   _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,                   _______, QWERTY,  COLEMAK, DVORAK,  _______, WOW,
+    _______, _______, _______, _______, _______, _______,                   _______, QWERTY,  COLEMAK, DVORAK,  DESTINY, WOW,
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
@@ -164,6 +174,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case WOW:
           if (record->event.pressed) {
             persistent_default_layer_set(1UL<<_WOW);
+          }
+          return false;
+          break;
+        case DESTINY:
+          if (record->event.pressed) {
+            persistent_default_layer_set(1UL<<_DESTINY);
           }
           return false;
           break;
