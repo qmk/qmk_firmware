93ed4ba4892976d49718b85d3a43031cb1d230c1
Yuya.N <root@eureka.jpn.com>
2019-02-28 23:58:45 +0900

    Fix keymap f12

--- keyboards/dozen0/keymaps/f12/keymap.c
+++ keyboards/dozen0/keymaps/f12/keymap.c
@@ -18,8 +18,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_UP, KC_DEL, KC_BSPC, \
-    KC_LCTRL, KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_ENT \
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, \
+    KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12 \
   ),
 };
 
