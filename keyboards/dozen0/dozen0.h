f1c2762c93f9a2317d91aaf7ed1ad032590bbf0b
Yuya.N <root@eureka.jpn.com>
2019-03-01 01:45:49 +0900

    Fix copyright and replace include guards

--- keyboards/dozen0/dozen0.h
+++ keyboards/dozen0/dozen0.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+/* Copyright 2019 yynmt
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,8 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef DOZEN0_H
-#define DOZEN0_H
+#pragma once
 
 #include "quantum.h"
 
@@ -33,5 +32,3 @@
 { \
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11 }, \
 }
-
-#endif
