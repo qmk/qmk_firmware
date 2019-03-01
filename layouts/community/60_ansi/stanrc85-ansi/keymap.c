882deabdbcc7a384f1be0739b6175a05fc64d4bb
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2019-03-01 12:53:48 -0500

    Move RGB code to layer_rgb.c

--- layouts/community/60_ansi/stanrc85-ansi/keymap.c
+++ layouts/community/60_ansi/stanrc85-ansi/keymap.c
@@ -57,27 +57,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______,                   _______,                   _______, _______, _______, _______)
 };
 
-uint32_t layer_state_set_user(uint32_t state) {
-    switch (biton32(state)) {
-    case 0:
-        rgblight_setrgb (0xFF,  0x00, 0x00);
-        break;
-    case 1:
-        rgblight_setrgb (0x00,  0xFF, 0x00);
-        break;
-    case 2:
-        rgblight_setrgb (0x00,  0x00, 0xFF);
-        break;
-    case 3:
-        rgblight_setrgb (0xFF,  0xFF, 0xFF);
-        break;
-    default: //  for any other layers, or the default layer
-        rgblight_setrgb (0xFF,  0x00, 0x00);
-        break;
-    }
-  return state;
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
