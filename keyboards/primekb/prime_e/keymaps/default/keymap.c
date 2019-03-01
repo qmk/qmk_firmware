6924cde04c1c374ec7f5a1c47190c6142c57c41d
Drashna Jaelre <drashna@live.com>
2019-03-01 11:48:46 -0800

    Update keyboards/primekb/prime_e/keymaps/default/keymap.c
    Co-Authored-By: holtenc <holtenc@gmail.com>

--- keyboards/primekb/prime_e/keymaps/default/keymap.c
+++ keyboards/primekb/prime_e/keymaps/default/keymap.c
@@ -51,7 +51,7 @@ void led_set_user(uint8_t usb_led) {
   if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
     PORTB |= (1<<1);
   } else {
-    PORTB &= ~(1<<1);
+    writePinHigh(B1);
   }
 /*  
   if (usb_led & (1<<USB_LED_SCROLL_LOCK)) {
