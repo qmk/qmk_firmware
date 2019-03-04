da9580b27382b1afbf67d3875a415464790840cb
Ryan Caltabiano <rcalt2vt@gmail.com>
2019-03-03 21:03:25 -0600

    Last changes due to framework update

--- quantum/rgb_matrix_animations/solid_splash_anim.h
+++ quantum/rgb_matrix_animations/solid_splash_anim.h
@@ -1,47 +1,48 @@
 #pragma once
 #ifdef RGB_MATRIX_KEYPRESSES
 #include "rgb_matrix_types.h"
-#if defined(DISABLE_RGB_MATRIX_SOLID_SPLASH) || defined(DISABLE_RGB_MATRIX_SOLID_MULTISPLASH)
+#if !defined(DISABLE_RGB_MATRIX_SOLID_SPLASH) || !defined(DISABLE_RGB_MATRIX_SOLID_MULTISPLASH)
+
+extern rgb_counters g_rgb_counters;
+extern const rgb_led g_rgb_leds[DRIVER_LED_TOTAL];
+extern rgb_config_t rgb_matrix_config;
 
 #ifndef MAX
-    #define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
 #endif
 
 #ifndef MIN
-    #define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
+#define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
 #endif
 
-void rgb_matrix_solid_multisplash(const rgb_counters* counters, const rgb_led* leds, const rgb_config_t* config) {
-    // if (g_rgb_counters.any_key_hit < 0xFF) {
-        HSV hsv = { .h = rgb_matrix_config.hue, .s = rgb_matrix_config.sat, .v = rgb_matrix_config.val };
-        RGB rgb;
-        Point point;
-        Point last_point;
-        for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++) {
-            point = leds[i].point;
-            uint16_t d = 0;
-            // if (g_rgb_counters.last_led_count) {
-                for (uint8_t last_i = 0; last_i < g_rgb_counters.last_led_count; last_i++) {
-                    last_point = leds[g_rgb_counters.last_led_hit[last_i]].point;
-                    uint16_t dist = (uint16_t)sqrt(pow(point.x - last_point.x, 2) + pow(point.y - last_point.y, 2));
-                    uint16_t effect = (g_rgb_counters.key_hit[g_rgb_counters.last_led_hit[last_i]] << 2) - dist;
-                    d += 255 - MIN(MAX(effect, 0), 255);
-                }
-            // } else {
-            //     d = 255;
-            // }
-            hsv.v = MAX(MIN(d, 255), 0);
-            rgb = hsv_to_rgb( hsv );
-            rgb_matrix_set_color( i, rgb.r, rgb.g, rgb.b );
-        }
+void rgb_matrix_solid_multisplash(void) {
+  // if (g_rgb_counters.any_key_hit < 0xFF) {
+  HSV hsv = { rgb_matrix_config.hue, rgb_matrix_config.sat, rgb_matrix_config.val };
+  for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++) {
+    Point point = g_rgb_leds[i].point;
+    uint16_t d = 0;
+    // if (g_rgb_counters.last_led_count) {
+    for (uint8_t last_i = 0; last_i < g_rgb_counters.last_led_count; last_i++) {
+      Point last_point = g_rgb_leds[g_rgb_counters.last_led_hit[last_i]].point;
+      uint16_t dist = (uint16_t)sqrt(pow(point.x - last_point.x, 2) + pow(point.y - last_point.y, 2));
+      uint16_t effect = (g_rgb_counters.key_hit[g_rgb_counters.last_led_hit[last_i]] << 2) - dist;
+      d += 255 - MIN(MAX(effect, 0), 255);
+    }
     // } else {
-        // rgb_matrix_set_color_all( 0, 0, 0 );
+    //     d = 255;
     // }
+    hsv.v = MAX(MIN(d, 255), 0);
+    RGB rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+  }
+  // } else {
+      // rgb_matrix_set_color_all( 0, 0, 0 );
+  // }
 }
 
-void rgb_matrix_solid_splash(rgb_counters* counters, const rgb_led* leds, const rgb_config_t* config) {
-    g_rgb_counters.last_led_count = MIN(g_rgb_counters.last_led_count, 1);
-    rgb_matrix_solid_multisplash(counters, leds, config);
+void rgb_matrix_solid_splash(void) {
+  g_rgb_counters.last_led_count = MIN(g_rgb_counters.last_led_count, 1);
+  rgb_matrix_solid_multisplash();
 }
 
 #endif // !defined(DISABLE_RGB_MATRIX_SPLASH) && !defined(DISABLE_RGB_MATRIX_MULTISPLASH)
