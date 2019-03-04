4c4b364932563d5fb2f3c4cd6ca484ea40c17a7c
ai03 <sarcaphx510@gmail.com>
2019-03-03 17:13:46 -0800

    Get things working except indicators

---
+++ keyboards/ai03/orbit/keymaps/default/keymap.c
@@ -0,0 +1,138 @@
+/* Copyright 2018 Ryota Goto
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  MANUAL = SAFE_RANGE,
+  DBLZERO
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT( /* Base */
+		BL_STEP,   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_BSPC, \
+		TO(1),   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, KC_BSLS, \
+		KC_NO,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
+		KC_NO,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PSCR, KC_DEL,  \
+			     KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_GRV,  KC_MENU, KC_MINS, KC_EQL 
+	),
+	[1] = LAYOUT( /* Fn, Arrowkeys, Media control, Backlight */
+		BL_STEP,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLU, _______, \
+		TO(2),   _______, _______, KC_PGUP, _______, _______, KC_F11,  KC_F12,  _______, KC_UP,   _______, _______, KC_VOLD, BL_STEP, \
+		TO(0),   _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_MPLY, _______, \
+		TO(0),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  \
+			     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+	),
+	[2] = LAYOUT( /* Mousekeys and Numpad */
+		BL_STEP,   _______, _______, _______, _______, _______, _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______, _______, \
+		KC_NO,   _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______, _______, \
+		TO(1),   _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______, _______, \
+		TO(1),   _______, KC_ACL0, KC_ACL1, KC_ACL2, KC_BTN3, _______, DBLZERO, KC_P0,   KC_PDOT, KC_PENT, KC_PPLS, _______, MANUAL,  \
+			     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+	)
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) {
+		case MANUAL:
+			if (record->event.pressed) 
+			{
+				// Keypress
+				SEND_STRING("https://kb.ai03.me/projects/orbit.html");
+			} 
+			else 
+			{
+				// Key release
+			}
+			break;
+		case DBLZERO:
+			if (record->event.pressed) 
+			{
+				// Keypress
+				SEND_STRING("00");
+			} 
+			else 
+			{
+				// Key release
+			}
+			break;
+  }
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+	
+	// Right hand LEDs as simple indicator LEDs
+    led_toggle(3, (usb_led & (1 << USB_LED_NUM_LOCK)));
+    led_toggle(4, (usb_led & (1 << USB_LED_CAPS_LOCK)));
+	led_toggle(5, (usb_led & (1 << USB_LED_SCROLL_LOCK)));
+
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+	
+	// Left hand LEDs as layer indicator
+	//
+	// O X X - Layer 0
+	// O O X - Layer 1
+	// O O O - Layer 2
+	// X O O - Layer 3
+	// X X O - Layer 4
+	// O X O - Anything else. Please teach me how you memorize more than 5 layers.
+	
+	switch (biton32(state))
+	{
+		case 0:
+			led_toggle(0, true);
+			led_toggle(1, false);
+			led_toggle(2, false);
+			break;
+		case 1:
+			led_toggle(0, true);
+			led_toggle(1, true);
+			led_toggle(2, false);
+			break;
+		case 2:
+			led_toggle(0, true);
+			led_toggle(1, true);
+			led_toggle(2, true);
+			break;
+		case 3:
+			led_toggle(0, false);
+			led_toggle(1, true);
+			led_toggle(2, true);
+			break;
+		case 4:
+			led_toggle(0, false);
+			led_toggle(1, false);
+			led_toggle(2, true);
+			break;
+		default:
+			led_toggle(0, true);
+			led_toggle(1, false);
+			led_toggle(2, true);
+			break;
+	}
+	return state;
+}
---
+++ keyboards/ai03/orbit/orbit.c
@@ -0,0 +1,153 @@
+/* Copyright 2018 Ryota Goto
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "orbit.h"
+#include "split_util.h"
+
+// Call led_toggle to set LEDs easily
+// LED IDs:
+// 
+// (LEFT) 0 1 2   |   3 4 5 (RIGHT)
+
+void led_toggle(int id, bool on) {
+	
+	if (isLeftHand) {
+		switch(id) {
+			case 0:
+				// Left hand C6
+				if (on)
+					PORTC |= (1<<6);
+				else
+					PORTC &= ~(1<<6);
+				break;
+			case 1:
+				// Left hand B6
+				if (on)
+					PORTB |= (1<<6);
+				else
+					PORTB &= ~(1<<6);
+				break;
+			case 2:
+				// Left hand B5
+				if (on)
+					PORTB |= (1<<5);
+				else
+					PORTB &= ~(1<<5);
+				break;
+			default:
+				break;
+		}
+	} else {
+		switch(id) {
+			case 3:
+				// Right hand F6
+				if (on)
+					PORTF |= (1<<6);
+				else
+					PORTF &= ~(1<<6);
+				break;
+			case 4:
+				// Right hand F7
+				if (on)
+					PORTF |= (1<<7);
+				else
+					PORTF &= ~(1<<7);
+				break;
+			case 5:
+				// Right hand C7
+				if (on)
+					PORTC |= (1<<7);
+				else
+					PORTC &= ~(1<<7);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+// Set all LEDs at once using an array of 6 booleans
+// LED IDs:
+// 
+// (LEFT) 0 1 2   |   3 4 5 (RIGHT)
+// 
+// Ex. set_all_leds({ false, false, false, true, true, true }) would turn off left hand, turn on right hand
+
+void set_all_leds(bool leds[6]) {
+	for (int i = 0; i < 6; i++) {
+		led_toggle(i, leds[i]);
+	}
+}
+
+void matrix_init_kb(void) {
+	// put your keyboard start-up code here
+	// runs once when the firmware starts up
+	
+	// Initialize indicator LEDs to output
+	if (isLeftHand)
+	{
+		DDRC |= (1<<6);
+		DDRB |= (1<<6);
+		DDRB |= (1<<5);
+	}
+	else
+	{
+		DDRF |= (1<<6);
+		DDRF |= (1<<7);
+		DDRC |= (1<<7);
+	}
+	
+	if(is_keyboard_master()) 
+	{
+		bool allLeds[] = { true, true, true, true, true, true };
+		set_all_leds(allLeds);
+	}
+
+	matrix_init_user();
+}
+
+uint8_t get_current_layer(void) {
+	return current_layer;
+}
+
+void matrix_scan_kb(void) {
+	// put your looping keyboard code here
+	// runs every cycle (a lot)
+
+	matrix_scan_user();
+}
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+	// put your per-action keyboard code here
+	// runs for every action, just before processing by the firmware
+
+	return process_record_user(keycode, record);
+}
+
+void led_set_kb(uint8_t usb_led) {
+	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+
+	led_set_user(usb_led);
+}
+
+uint32_t layer_state_set_kb(uint32_t state) {
+	// keymap-unrelated LED toggles
+	
+	current_layer = biton32(state);
+	
+	return layer_state_set_user(state);
+}
+
+
