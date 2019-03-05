336f0047936fa5627adbb8b88a1058a5f75d2d4b
SpacebarRacecar <42380065+SpacebarRacecar@users.noreply.github.com>
2019-03-05 15:51:03 +0100

    changes to userspace

--- users/spacebarracecar/spacebarracecar.c
+++ users/spacebarracecar/spacebarracecar.c
@@ -340,3 +340,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return process_record_keymap(keycode, record);
   }
 }
+
+__attribute__((weak))
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
