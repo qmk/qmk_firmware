27834f7a1c272e0d94427676b012ec3573c7cd9d
mechmerlin <mechmerlin@gmail.com>
2019-03-05 20:14:21 -0800

    remove a comma for compilation errors

--- keyboards/mechkeys/mk60/mk60.h
+++ keyboards/mechkeys/mk60/mk60.h
@@ -30,7 +30,7 @@
     k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, \
     k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D,      \
     k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k3E, \
-    k40,      k42, k43, k44,           k47,      k49, k4A, k4B, k4C, k4D, k4E, \
+    k40,      k42, k43, k44,           k47,      k49, k4A, k4B, k4C, k4D, k4E \
 ) \
 { \
     { k00, k01,   k02, k03, k04, k05,   k06,   k07, k08,   k09, k0A, k0B, k0C, k0D, k0E},   \
