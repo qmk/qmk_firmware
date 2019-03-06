17bb8326b285a66ba193f9a1367896abf4e0f0f7
mechmerlin <mechmerlin@gmail.com>
2019-03-05 20:08:07 -0800

    create the appropriate LAYOUT macro

--- keyboards/mechkeys/mk60/mk60.h
+++ keyboards/mechkeys/mk60/mk60.h
@@ -26,10 +26,16 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k11   \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, \
+    k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, \
+    k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D,      \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k3E, \
+    k40,      k42, k43, k44,           k47,      k49, k4A, k4B, k4C, k4D, k4E, \
 ) \
 { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k11 }, \
+    { k00, k01,   k02, k03, k04, k05,   k06,   k07, k08,   k09, k0A, k0B, k0C, k0D, k0E},   \
+    { k10, KC_NO, k12, k13, k14, k15,   k16,   k17, k18,   k19, k1A, k1B, k1C, k1D, k1E},   \
+    { k20, KC_NO, k22, k23, k24, k25,   k26,   k27, k28,   k29, k2A, k2B, k2C, k2D, KC_NO}, \
+    { k30, k31,   k32, k33, k34, k35,   k36,   k37, k38,   k39, k3A, k3B, k3C, k3D, k3E},   \
+    { k40, KC_NO, k42, k43, k44, KC_NO, KC_NO, k47, KC_NO, k49, k4A, k4B, k4C, k4D, k4E},   \
 }
