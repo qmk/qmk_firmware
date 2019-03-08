7c1fb5dfa4c4cfc787e0d016842eafe6eaef1a78
Eric Peterson <elpeterson16@gmail.com>
2019-03-08 01:10:00 -0500

    missed an underscore

--- keyboards/launchpad/keymaps/default_rgb/keymap.c
+++ keyboards/launchpad/keymaps/default_rgb/keymap.c
@@ -67,10 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------'
  */
 [_FUNC] = LAYOUT( \
-    KC_Q, CALTDEL, \
-    KC_A, TSKMGR, \
-    KC_Z, KC_X, \
-    ____, KC_C \
+    KC_Q,    CALTDEL, \
+    KC_A,    TSKMGR, \
+    KC_Z,    KC_X, \
+    _______, KC_C \
 )
 
 };
