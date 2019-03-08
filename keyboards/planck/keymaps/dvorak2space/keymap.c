92ceda2d81d37454b9793d3dc42cb77d78ae6954
IsaacElenbaas <isaacelenbaas@gmail.com>
2019-03-08 17:47:58 -0500

    Ctrl+Bksp after first tap

--- keyboards/planck/keymaps/dvorak2space/keymap.c
+++ keyboards/planck/keymaps/dvorak2space/keymap.c
@@ -16,13 +16,22 @@ enum {
 enum {
 	FB = 0,
 	LPN,
-	RPN
+	RPN,
+	BCK
 };
+
+void backspace(qk_tap_dance_state_t *state, void *user_data) {
+  if(state->count == 1)
+    SEND_STRING(SS_TAP(X_BSPACE));
+  else
+    SEND_STRING(SS_LCTRL(SS_TAP(X_BSPACE)));
+}
 
 qk_tap_dance_action_t tap_dance_actions[] = {
 	[FB] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_NUBS),
 	[LPN] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_LCBR),
-	[RPN] = ACTION_TAP_DANCE_DOUBLE(KC_RPRN, KC_RCBR)
+	[RPN] = ACTION_TAP_DANCE_DOUBLE(KC_RPRN, KC_RCBR),
+	[BCK] = ACTION_TAP_DANCE_FN(backspace)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 	[0] = LAYOUT_planck_2x2u(
-		KC_TAB,	KC_QUOT,KC_COMM,KC_DOT,	KC_P,	KC_Y,	KC_F,	KC_G,	KC_C,	KC_R,	KC_L,	KC_BSPC,
+		KC_TAB,	KC_QUOT,KC_COMM,KC_DOT,	KC_P,	KC_Y,	KC_F,	KC_G,	KC_C,	KC_R,	KC_L,	TD(BCK),
 		TT(3),	KC_A,	KC_O,	KC_E,	KC_U,	KC_I,	KC_D,	KC_H,	KC_T,	KC_N,	KC_S,	KC_ENT,
 		KC_LSFT,KC_SCLN,KC_Q,	KC_J,	KC_K,	KC_X,	KC_B,	KC_M,	KC_W,	KC_V,	KC_Z,	KC_RSFT,
 		KC_ESC,	MOUSER,	MOUSEL,	KC_LCTL,		KC_SPC,	TT(2),			KC_LGUI,KC_VOLD,KC_VOLU,OSL(4)
@@ -72,13 +81,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 	[2] = LAYOUT_planck_2x2u(
-		KC_TAB,	KC_PLUS,KC_PMNS,KC_ASTR,TD(FB),HK_IF,	HK_ELSE,TD(LPN),TD(RPN),KC_LCBR,KC_RCBR,KC_BSPC,
+		KC_TAB,	KC_PLUS,KC_PMNS,KC_ASTR,TD(FB),HK_IF,	HK_ELSE,TD(LPN),TD(RPN),KC_LCBR,KC_RCBR,TD(BCK),
 		KC_PEQL,KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_ENT,
 		KC_NO,	KC_LT,	KC_GT,	KC_AMPR,KC_PIPE,KC_UNDS,KC_DLR,	KC_AT,	KC_HASH,KC_PERC,KC_CIRC,KC_NO,
 		KC_NO,	KC_NO,	KC_NO,	KC_NO,			KC_SPC,	KC_TRNS,		KC_NO,	KC_NO,	KC_NO,	KC_NO
 	),
 	[3] = LAYOUT_planck_2x2u( //Nav
-		KC_TAB,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_HOME,KC_UP,	KC_END,	KC_NO,	KC_BSPC,
+		KC_TAB,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_HOME,KC_UP,	KC_END,	KC_NO,	TD(BCK),
 		KC_TRNS,KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	CTRLL,	KC_LEFT,KC_DOWN,KC_RGHT,CTRLR,	KC_ENT,
 		KC_LSFT,KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,
 		KC_NO,	KC_NO,	KC_NO,	KC_NO,			KC_SPC,	KC_NO,			KC_NO,	KC_NO,	KC_NO,	KC_NO
