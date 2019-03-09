e0c3b70f30c30abb776a8e186c8001052a8d4d06
Ethan Beyer <ethan@ethanbeyer.com>
2019-03-09 14:46:16 -0500

    I think this is the layout I will try to use to start

---
+++ keyboards/dz60/keymaps/_bonfire/README.md
@@ -0,0 +1,10 @@
+# Todo
+
+1. OMG I need arrrow keys to be very accessible, i use them constantly: https://docs.qmk.fm/#/faq_keymap?id=arrow-on-right-modifier-keys-with-dual-role
+2. Backspace is a little tacky, not a fan
+3. what keys are going to be used for what on modifiers? I need to make them the same as all my other keyboards
+    programming
+    keycaps
+4. backlight controls
+5. what are my layers going to be?
+6. Norman on a layer?
--- keyboards/dz60/keymaps/_bonfire/keymap.c
+++ keyboards/dz60/keymaps/_bonfire/keymap.c
@@ -1,19 +1,18 @@
 #include QMK_KEYBOARD_H
 
 /*
-* Each layer gets a name for readability.
-* The underscores don't mean anything - you can
-* have a layer called STUFF or any other name.
-* Layer names don't all need to be of the same
-* length, and you can also skip them entirely
-* and just use numbers.
+* Each layer gets a name for readability. The underscores don't mean anything - you can
+* have a layer called STUFF or any other name. Layer names don't all need to be of the same
+* length, and you can also skip them entirely and just use numbers.
 *
-* based on zvecr
+* Based on `dz60/zvecr`
 *
 */
-#define BL 0     // Base Layer
-#define FL 1     // Function Layer
-#define ML 2     // Macro Layer
+#define BASE    0    // Layer: Base
+#define ACTN    1    // Layer: Action
+#define FUNC    2    // Layer: Function
+#define KEYB    3    // Layer: Keyboard
+
 
 /* Bonfire's Keyboard
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
@@ -27,44 +26,52 @@
  * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬┴───┤
  * │40  │41  │43  │46                      │4a  │4b  │4d  │4e  │        8
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
-
-#define LAYOUT__bonfire( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,      k0e, \
-    k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, \
-    k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d,      \
-    k30,      k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b,      k3d, k3e, \
-    k40, k41,      k43,           k46,                k4a, k4b,      k4d, k4e \
-) { \
-    { k00, k01,   k02,   k03, k04,   k05,   k06, k07,   k08,   k09,   k0a, k0b, k0c,   KC_NO, k0e   }, \
-    { k10, KC_NO, k12,   k13, k14,   k15,   k16, k17,   k18,   k19,   k1a, k1b, k1c,   k1d,   k1e   }, \
-    { k20, KC_NO, k22,   k23, k24,   k25,   k26, k27,   k28,   k29,   k2a, k2b, k2c,   k2d,   KC_NO }, \
-    { k30, KC_NO, k32,   k33, k34,   k35,   k36, k37,   k38,   k39,   k3a, k3b, KC_NO, k3d,   k3e   }, \
-    { k40, k41,   KC_NO, k43, KC_NO, KC_NO, k46, KC_NO, KC_NO, KC_NO, k4a, k4b, KC_NO, k4d,   k4e   }  \
-}
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[BL] = LAYOUT__bonfire(
-		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
-		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(FL),
-		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                               KC_RALT, KC_RGUI, KC_RCTL, MO(ML)),
+	[BASE] = LAYOUT__bonfire(
+		KC_GRV,   KC_1,    KC_2,    KC_3,   KC_4,    KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,   KC_BSPC,
+		KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,    KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
+		MO(ACTN), KC_A,    KC_S,    KC_D,   KC_F,    KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,    KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, FN0,     MO(FUNC),
+		KC_LCTL,  KC_LGUI, KC_LALT, KC_SPC, KC_RALT, FN2,  FN1,  FN3,
+    ),
 
-	[FL] = LAYOUT__bonfire(
-		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-		_______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
-		_______,          _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_HOME, KC_PGUP, _______,
-		_______,          _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, KC_DEL,  KC_END,  KC_PGDN,          _______,
-		_______, _______,          _______,          _______, _______, _______,                            _______, _______,          _______),
+    [ACTN] = LAYOUT__bonfire(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, FN4,     _______,
+        _______, _______, _______, _______, _______, _______, FN5,     FN6,     FN7
+    ),
+
+	[FUNC] = LAYOUT__bonfire(
+		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,
+		_______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,  _______, _______,
+		KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_HOME, KC_PGUP,  _______,
+		_______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, KC_DEL,  KC_END,  KC_PGDN, MO(KEYB), _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______,
+    ),
+
+	[KEYB] = LAYOUT__bonfire(
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, 
+		_______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______
+    )
+
+};
 
 
-	[ML] = LAYOUT__bonfire(
-		_______, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,           _______,
-		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-		_______, _______,          _______,          _______, _______, _______,                            _______, _______,          _______),
-
+const uint16_t PROGMEM fn_actions[] = {
+    [0] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_UP),
+    [1] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_DOWN),
+    [2] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_LEFT),
+    [3] = ACTION_MODS_TAP_KEY(MOD_APP, KC_RIGHT),
+    [4] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_PGUP),
+    [6] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_PGDN),
+    [7] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_HOME),
+    [8] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_END),
 };
