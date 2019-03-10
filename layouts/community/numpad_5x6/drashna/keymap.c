c41253a8d327c1e3df21c1f4fbab362b84e8a2db
Drashna Jaelre <drashna@live.com>
2019-03-10 12:16:16 -0700

    Add Semi-colon to numpad

--- layouts/community/numpad_5x6/drashna/keymap.c
+++ layouts/community/numpad_5x6/drashna/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_F3,   KC_F4,    KC_P7,   KC_P8,   KC_P9,
 		KC_F5,   KC_F6,    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		KC_F7,   KC_F8,    KC_P1,   KC_P2,   KC_P3,
-		KC_F9,   KC_F10,        KC_P0,       KC_PDOT, KC_PENT
+		KC_F9,   KC_COLN,       KC_P0,       KC_PDOT, KC_PENT
   ),
 
   [_GAMEPAD] = LAYOUT_numpad_5x6(
