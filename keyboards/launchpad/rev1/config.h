b6cf2ec5b82feacbde3286e03298553872f871b2
Eric Peterson <elpeterson16@gmail.com>
2019-03-10 04:31:10 +0000

    reverted rev1/config.h

--- keyboards/launchpad/rev1/config.h
+++ keyboards/launchpad/rev1/config.h
@@ -15,56 +15,18 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef REV1_CONFIG_H
+#define REV1_CONFIG_H
 
-#include "config_common.h"
+#include "../config.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID 0x1337
-#define PRODUCT_ID 0x6007
-#define MANUFACTURER Maple Computing
-#define PRODUCT Launch Pad
-#define DESCRIPTION An all - in - on macropad
+#define DEVICE_VER      0x0001
 
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 2
+/* Let's Macro V2 pin-out */
+#define MATRIX_ROW_PINS { C6, B1, B3, D7 }
+#define MATRIX_COL_PINS { D2, F7 }
+#define UNUSED_PINS
 
-/* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
-
-#ifdef SUBPROJECT_rev1
-#  include "rev1/config.h"
-#endif
 
 #endif
