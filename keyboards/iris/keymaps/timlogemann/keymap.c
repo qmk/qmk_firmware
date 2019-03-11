573bb5da0958380126ea71f4df1647bfad59942a
Tim Logemann <tim@logemann.nu>
2019-03-11 15:34:02 +0100

    turn used-to-be-broken key into alt

--- keyboards/iris/keymaps/timlogemann/keymap.c
+++ keyboards/iris/keymaps/timlogemann/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_EQL,
         TD(TD_SHIFT_CAPS), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_SLSH,
         KC_LCTL, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_NO, KC_NO, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_BSLS,
-        KC_H, KC_LGUI, KC_SFTENT, KC_SPC, TT(1), KC_LALT),
+        KC_LALT, KC_LGUI, KC_SFTENT, KC_SPC, TT(1), KC_LALT),
     [1] = LAYOUT(
         KC_GRV, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO, TO(0), TO(2), TO(3), KC_NO, KC_DEL,
         KC_NO, KC_MINUS, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_MFFD, KC__VOLUP,
