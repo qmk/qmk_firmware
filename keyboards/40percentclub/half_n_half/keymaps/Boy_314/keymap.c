33dc0962f88f98f58db036edef5cccaf4e96292e
Boy-314 <boy.314.yee.willie@gmail.com>
2019-03-12 18:20:46 -0400

    update keymap with qwerty layout, and enable tap dancing

--- keyboards/40percentclub/half_n_half/keymaps/Boy_314/keymap.c
+++ keyboards/40percentclub/half_n_half/keymaps/Boy_314/keymap.c
@@ -32,18 +32,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * ,-------------------------------------------------------------------------------------------------.
 	 * |Tab   |'     |,     |.     |P     |Y     |Brght+|=     |F     |G     |C     |R     |L     |Bksp  |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |CtlCps|A     |O     |E     |U     |I     |Brght-|Ctrl+F|D     |H     |T     |N     |S     |Enter |
+	 * |LCtrl |A     |O     |E     |U     |I     |Brght-|Ctrl+F|D     |H     |T     |N     |S     |Enter |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
 	 * |LShift|;     |Q     |J     |K     |X     |LAlt  |-     |B     |M     |W     |V     |Z     |RShift|
 	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
 	 *                             |TD Swap Layer|             |Space        |
-	 *                             `-------------'             `-------------'
+	 *                             `-------------'			   `-------------'
 	 */
 	[DVORAK] = LAYOUT(/* Base Dvorak */
-	  KC_TAB,           KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_BRIU, KC_EQL,     KC_F, KC_G, KC_C,    KC_R,   KC_L,   KC_BSPC,
-	  LCTL_T(KC_CAPS),  KC_A,    KC_O,    KC_E,   KC_U, KC_I, KC_BRID, LCTL(KC_F), KC_D, KC_H, KC_T,    KC_N,   KC_S,   KC_ENT,
-	  KC_LSPO,          KC_SCLN, KC_Q,    KC_J,   KC_K, KC_X, KC_LALT, KC_MINS,    KC_B, KC_M, KC_W,    KC_V,   KC_Z,   KC_RSPC,
-	                                      TD(TD_SWAP_LAYERS),                      KC_SPC
+	  KC_TAB,   KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_BRIU, KC_EQL,     KC_F, KC_G, KC_C,    KC_R,   KC_L,   KC_BSPC,
+	  KC_LCTL,  KC_A,    KC_O,    KC_E,   KC_U, KC_I, KC_BRID, LCTL(KC_F), KC_D, KC_H, KC_T,    KC_N,   KC_S,   KC_ENT,
+	  KC_LSPO,  KC_SCLN, KC_Q,    KC_J,   KC_K, KC_X, KC_LALT, KC_MINS,    KC_B, KC_M, KC_W,    KC_V,   KC_Z,   KC_RSPC,
+	                                      TD(TD_SWAP_LAYERS),                    KC_SPC
 	),
 
 	/* QWERTY
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |LShift|Z     |X     |C     |V     |B     |LAlt  |-     |N     |M     |,     |.     |/     |RShift|
 	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
 	 *                             |TD Swap Layer|             |Space        |
-	 *                             `-------------'             `-------------'
+	 *                             `-------------'			   `-------------'
 	 */
 	[QWERTY] = LAYOUT(/* Base Qwerty */
 	  KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R, KC_T, KC_BRIU, KC_QUOT,    KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
