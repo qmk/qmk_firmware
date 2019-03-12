1208f4414efb8fa5a48e576ac7315c33ce4a0ff2
Drashna Jaelre <drashna@live.com>
2019-03-12 12:01:36 -0600

    Update keyboards/georgi/keymaps/default/keymap.c
    Co-Authored-By: germ <jeremythegeek@gmail.com>

--- keyboards/georgi/keymaps/default/keymap.c
+++ keyboards/georgi/keymaps/default/keymap.c
@@ -213,7 +213,7 @@ STN_PWR, STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,       STN_ST4, STN_RR, STN_BR,
 						 STN_N1, STN_A,  STN_O,			STN_E,   STN_U,  STN_N1)
 ,
 // Gaming layer with Numpad, Very limited
-[GAMING] = LAYOUT_GEORGI(  
+[GAMING] = LAYOUT_georgi(  
 KC_LSFT, KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y,   KC_U, KC_I, KC_O, KC_P,    KC_ENT,
 KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,       KC_H,   KC_J, KC_K, KC_L, KC_SCLN, KC_DQUO,
 KC_LALT, KC_SPC, LT(GAMING_2, KC_ENT),	     KC_DEL, KC_ASTR,  TO(STENO_LAYER)), 
