9013b94d60690cf7bd4022887371cbb3c1b95a89
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-03-12 15:11:09 -0700

    Update keyboards/mt980/mt980.c
    Co-Authored-By: walkerstop <walkerstop@gmail.com>

--- keyboards/mt980/mt980.c
+++ keyboards/mt980/mt980.c
@@ -35,7 +35,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void led_set_user(uint8_t usb_led) {
 
-  if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+  if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
     writePinLow(C6);
   }
   else {
