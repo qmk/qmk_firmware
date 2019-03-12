48328e08b429ecd626f268eaf849afb43a385e41
Michael L. Walker <michael.l.walker@intel.com>
2019-03-12 13:08:06 -0700

    Update manufacturer name

--- keyboards/mt980/config.h
+++ keyboards/mt980/config.h
@@ -6,7 +6,7 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    dztech
+#define MANUFACTURER    Wheat Field Peripherals
 #define PRODUCT         MT980
 #define DESCRIPTION     Keyboard
 
