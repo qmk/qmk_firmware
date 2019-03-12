1ee9af44cb83049522f4d4c5c1fff06eb2cdeb07
monsieurluge <monsieurluge@gmail.com>
2019-03-12 22:51:54 +0100

    fix: missing "left shift" key in the gaming layout

--- keyboards/planck/keymaps/monsieurluge/keymap.c
+++ keyboards/planck/keymaps/monsieurluge/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAMING] = {
     {KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
     {KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
-    {SP_NUMB, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SP_MEDI},
+    {KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SP_MEDI},
     {KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, SP_SYMB, SP_NUMB, SP_SPSH, SP_SPSH, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT}
   },
 
