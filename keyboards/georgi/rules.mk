4e8c7efadaf08d94a6a47d8fe5af7b94b5d50e60
Drashna Jaelre <drashna@live.com>
2019-03-13 00:44:45 -0600

    Update keyboards/georgi/rules.mk
    Co-Authored-By: germ <jeremythegeek@gmail.com>

--- keyboards/georgi/rules.mk
+++ keyboards/georgi/rules.mk
@@ -17,6 +17,6 @@ EXTRAFLAGS += -flto
 CUSTOM_MATRIX 		= yes
 STENO_ENABLE 		= yes
 EXTRAKEY_ENABLE     = yes
-CONSOLE_ENABLE      = yes
+CONSOLE_ENABLE      = no
 COMMAND_ENABLE      = no
 NKRO_ENABLE			= yes
