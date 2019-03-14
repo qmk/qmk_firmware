d7ba9b939814cd47d2e534e28774c2f54b128d3f
itou <hsginjake123@gmail.com>
2019-03-15 03:35:23 +0900

    fix layout define

--- keyboards/handwired/lovelive9/lovelive9.h
+++ keyboards/handwired/lovelive9/lovelive9.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include "quantum.h"
-#define LAYOUT_ortho_1x9( \
+#define LAYOUT( \
 	k00, k01, k02, k03, k04, k05, k06, k07, k08 \
 ) \
 { \
 	{ k00, k01, k02, k03, k04, k05, k06, k07, k08 } \
 }
-#define LAYOUT LAYOUT_ortho_1x9
