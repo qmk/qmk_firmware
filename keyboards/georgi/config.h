5a6d03fce2d6e45e20e3a0d99aeb899883396dbc
Jeremy Bernhardt <bernhardtjeremy@gmail.com>
2019-03-14 02:46:02 -0600

    Fixed info.json

--- keyboards/georgi/config.h
+++ keyboards/georgi/config.h
@@ -29,7 +29,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define FORCE_NKRO
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
+#define NO_DEBUG
 #define NO_ACTION_ONESHOT
+#define NO_ACTION_FUNCTION
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
