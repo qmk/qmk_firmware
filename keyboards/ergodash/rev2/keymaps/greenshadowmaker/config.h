ee10caa8057005b865e9936e2ad4c71ebfe93a99
Drashna Jaelre <drashna@live.com>
2019-03-14 21:39:10 -0700

    Update keyboards/ergodash/rev2/keymaps/greenshadowmaker/config.h
    Ah!  Thanks fort the clarification, it was a blind copy/paste
    
    Co-Authored-By: GreenShadowMaker <39226745+GreenShadowMaker@users.noreply.github.com>

--- keyboards/ergodash/rev2/keymaps/greenshadowmaker/config.h
+++ keyboards/ergodash/rev2/keymaps/greenshadowmaker/config.h
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_USER_H
+#pragma once
 #define CONFIG_USER_H
 
 #include "../../config.h"
