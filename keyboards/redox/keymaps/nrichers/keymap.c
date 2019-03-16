ff09098f0bf6565e8f9cad7b452e5aa7d706b1e4
Nik Richers <nrichers@elastic.co>
2019-03-15 19:39:58 -0700

    Additional keymap tweaks, add readme info

--- keyboards/redox/keymaps/nrichers/keymap.c
+++ keyboards/redox/keymaps/nrichers/keymap.c
@@ -23,6 +23,7 @@ enum custom_keycodes {
 
 #define KC_ALAS LALT_T(KC_PAST)
 #define KC_CTES LCTL_T(KC_ESC)
+#define KC_CTQU RCTL_T(KC_QUOT)
 
 #define KC_NAGR LT(_NAV, KC_GRV)
 #define KC_NAMI LT(_NAV, KC_MINS)
@@ -38,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,SYM_L   ,                          SYM_L   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_EQL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_CTES ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_LBRC ,                          KC_RBRC ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
+     KC_CTES ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_LBRC ,                          KC_RBRC ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_CTQU ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADPU ,KC_PGDN ,        KC_HOME ,KC_ADEN ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_BSLS ,KC_PPLS ,KC_PMNS ,KC_ALAS ,     KC_LGUI ,    KC_BSPC ,KC_DEL  ,        KC_ENT  ,KC_SPC  ,    KC_RGUI ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT 
+     KC_BSLS ,KC_PPLS ,KC_PMNS ,KC_ALAS ,     KC_LGUI ,    KC_SPC  ,KC_BSPC ,        KC_DEL  ,KC_ENT  ,    KC_RGUI ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
--- keyboards/redox/keymaps/nrichers/readme.md
+++ keyboards/redox/keymaps/nrichers/readme.md
@@ -1,1 +1,13 @@
 # A tweaked keymap for Redox
+
+Changes:
+
+- Move GUI (SUPER) key to where default left CTRL and right ALT are
+- Move /? into usual location next to right SHIFT
+- Put \| into bottom-left spot where GUI (SUPER) was
+- Change default ESC into ESC on tap, CTRL on hold
+- Change default ' above right SHIFT into ' on tap, CTRL on hold
+- Add F11 and F12 keys to two layers
+- On thumb clusters: 
+  - Left: Move SPACE closest to left thumb and move BACKSPACE to the right of that
+  - Right: Move ENTER closest to right thumb and move DEL to the left of that 
