1e5c99879b7f5dcac0ee83671e27397c4afdca81
asamwow <asamwow@github.com>
2019-03-17 09:00:38 -0700

    Added command and qwerty layers

--- keyboards/planck/keymaps/samuel/keymap.c
+++ keyboards/planck/keymaps/samuel/keymap.c
@@ -2,21 +2,10 @@
 
 extern keymap_config_t keymap_config;
 
-static uint16_t tap_timer = 0;
-
-char last_mod = 10;
-
-uint16_t keys_pressed = 0;
-
-enum planck_layers {
-_DVORAK,
-_RISE
-};
-
 // where the 'T_' communicates how the key does something different when tapped.
 enum planck_keycodes {
-DVORAK = SAFE_RANGE,
-KC_T_LALT,
+//DVORAK = SAFE_RANGE,
+KC_T_LALT = SAFE_RANGE,
 KC_T_RALT,
 KC_T_LGUI,
 KC_T_RGUI,
@@ -37,7 +26,21 @@ KC_EZPSTE,
 KC_EZSHFT,
 KC_EZCTRL,
 KC_EZGUI,
-KC_EZALT
+KC_EZALT,
+KC_DVORAK,
+KC_QWERTY,
+KC_USER,
+KC_EMAIL,
+KC_NAME,
+KC_DIR,
+KC_COM
+};
+
+enum planck_layers {
+_DVORAK,
+_QWERTY,
+_RISE,
+_COMMAND
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,17 +53,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 { KC_T_LGUI, KC_T_LALT, KC_UP, KC_DOWN, KC_BSPC, KC_TAB, KC_ENT, KC_SPC, KC_LEFT, KC_RIGHT, KC_T_RALT, KC_T_RGUI}
 },
 
+[_QWERTY] =
+
+{{ KC_T_LRSE, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_T_RRSE},
+{ KC_T_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_T_RSFT},
+{ KC_T_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_QUOT, KC_T_RCTL},
+{ KC_T_LGUI, KC_T_LALT, KC_UP, KC_DOWN, KC_BSPC, KC_TAB, KC_ENT, KC_SPC, KC_LEFT, KC_RIGHT, KC_T_RALT, KC_DVORAK}
+},
+
 [_RISE] =
 
 {{ KC_TRANSPARENT, KC_EZUNDO, KC_EZCOPY, KC_EZCUT, KC_EZPSTE, KC_INS, KC_EQL, KC_7, KC_8, KC_9, KC_TRANSPARENT, KC_TRANSPARENT},
 { KC_EZSHFT, KC_ESC, KC_CAPS, KC_PGUP, KC_PGDN, KC_HOME, KC_ASTR, KC_4, KC_5, KC_6, KC_0, KC_EZSHFT},
-{ KC_EZCTRL, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_1, KC_2, KC_3, KC_TRANSPARENT, KC_EZCTRL},
+{ KC_EZCTRL, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_1, KC_2, KC_3, KC_COM, KC_EZCTRL},
 { KC_EZGUI, KC_EZALT, KC_EZUP, KC_EZDOWN, KC_SPC, KC_AMPR, KC_PIPE, KC_END, KC_EZLEFT, KC_EZRGHT, KC_EZALT, KC_EZGUI}
+},
+
+[_COMMAND] =
+
+{{ KC_TRANSPARENT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRANSPARENT},
+{ KC_TRANSPARENT, KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_TRANSPARENT},
+{ KC_TRANSPARENT, KC_F21, KC_F22, KC_F23, KC_F24, KC_QWERTY, KC_DVORAK, KC_USER, KC_EMAIL, KC_NAME, KC_TRANSPARENT, KC_TRANSPARENT},
+{ KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_DIR, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT}
 },
 
 };
 
+static uint16_t tap_timer = 0;
+char last_mod = 10;
+
 void mod_press(uint16_t tap_code, uint16_t hold_code, int id) {
+    // this first if body makes double modified keys impossible, but stops the
+    // delay when modifying a tap key which would result in the tap key not
+    // getting modified.
     if (last_mod != id && last_mod != 10) {
         tap_code16(tap_code);
         last_mod = 10;
@@ -81,12 +106,20 @@ void mod_lift(uint16_t tap_code, uint16_t hold_code, int id) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case DVORAK:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_DVORAK);
-            }
-            return false;
-            break;
+
+case KC_DVORAK:
+if (record->event.pressed) {
+    set_single_persistent_default_layer(_DVORAK);
+}
+return false;
+break;
+
+case KC_QWERTY:
+if (record->event.pressed) {
+    set_single_persistent_default_layer(_QWERTY);
+}
+return false;
+break;
 
 case KC_T_LALT:
 if (record->event.pressed) {
@@ -275,6 +308,45 @@ break;
 case KC_EZPSTE:
 if (record->event.pressed) {
     tap_code16(C(KC_V));
+    last_mod = 10;
+}
+return false;
+break;
+
+case KC_COM:
+if (record->event.pressed) {
+    set_oneshot_layer(_COMMAND, ONESHOT_START);
+    last_mod = 10;
+} else {
+    set_oneshot_layer(_COMMAND, ONESHOT_PRESSED);
+}
+return false;
+break;
+
+case KC_USER:
+if (record->event.pressed) {
+    send_string("mhostley");
+    last_mod = 10;
+}
+return false;
+break;
+case KC_EMAIL:
+if (record->event.pressed) {
+    send_string("mhostley@gmail.com");
+    last_mod = 10;
+}
+return false;
+break;
+case KC_NAME:
+if (record->event.pressed) {
+    send_string("Samuel Jahnke");
+    last_mod = 10;
+}
+return false;
+break;
+case KC_DIR:
+if (record->event.pressed) {
+    send_string("home/mhostley/");
     last_mod = 10;
 }
 return false;
