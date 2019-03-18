3645e18170f5b7aedcc801b90256db05fbb629de
monsieurluge <monsieurluge@gmail.com>
2019-03-18 21:47:47 +0100

    invert the minus and underscore keys

--- keyboards/planck/keymaps/monsieurluge/keymap.c
+++ keyboards/planck/keymaps/monsieurluge/keymap.c
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    * │     │home │pgdwn│pgup │ end │  '  │  "  │left │down │ up  │right│     │
    * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   * │     │  `  │  {  │  }  │  =  │  +  │  -  │  _  │  [  │  ]  │  \  │     │
+   * │     │  `  │  {  │  }  │  =  │  +  │  _  │  -  │  [  │  ]  │  \  │     │
    * ├─────┼─────┼─────┼─────┼─────┴─────┼─────┴─────┼─────┼─────┼─────┼─────┤
    * │     │     │     │     │▒▒▒▒▒▒▒▒▒▒▒│           │     │     │     │     │
    * └─────┴─────┴─────┴─────┴───────────┴───────────┴─────┴─────┴─────┴─────┘
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SYMB] = {
     {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL },
     {_______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_QUOT, KC_DQT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______},
-    {_______, KC_GRV,  KC_LCBR, KC_RCBR, KC_EQUAL, KC_PLUS, KC_MINS, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, _______},
+    {_______, KC_GRV,  KC_LCBR, KC_RCBR, KC_EQUAL, KC_PLUS, KC_UNDS, KC_MINS, KC_LBRC, KC_RBRC, KC_BSLS, _______},
     {_______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______}
   },
 
