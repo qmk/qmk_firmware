fae5863a200f4aeb2abfa20c1c22073ab1308a0a
Drashna Jaelre <drashna@live.com>
2019-03-18 19:02:02 -0700

    Revert some changes

--- keyboards/handwired/traveller/keymaps/default/keymap.c
+++ keyboards/handwired/traveller/keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "mousekey.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
