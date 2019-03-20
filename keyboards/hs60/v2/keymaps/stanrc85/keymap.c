7bbf629f61324fed3416ff841a977892af4c8cfe
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2019-03-19 20:33:02 -0400

    Remove tap dance code from keymap.c

--- keyboards/hs60/v2/keymaps/stanrc85/keymap.c
+++ keyboards/hs60/v2/keymaps/stanrc85/keymap.c
@@ -16,17 +16,6 @@
 #include QMK_KEYBOARD_H
 #include "stanrc85.h"
 
-//Tap Dance Declarations
-enum {
-	TD_WIN = 0,
-	TD_ESC
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-	[TD_WIN] = ACTION_TAP_DANCE_DOUBLE(KC_CAD, KC_LOCK),
-	[TD_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV)
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_60_ansi(
 		TD_TESC, KC_1,    KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
