64ea6794ddf96362f12c358046e5a697e3bad92f
Reagan Mitchell <reaganmitchell01@gmail.com>
2019-03-20 17:52:48 -0500

    added Vorstellungklavier custom keyboard

---
+++ keyboards/handwired/vorstellungsklavier/readme.md
@@ -0,0 +1,1 @@
+Custom ergonomic keyboard by Reagan Mitchell. More details and keymaps to come.
---
+++ keyboards/handwired/vorstellungsklavier/vorstellungsklavier.c
@@ -0,0 +1,7 @@
+#include "vorstellungsklavier.h"
+
+
+void matrix_init_kb(void) {
+
+	matrix_init_user();
+};
