ea3a71a2c2d11827b22808ef0748d4d4085665cf
Drashna Jaelre <drashna@live.com>
2019-03-22 14:48:29 -0700

    Remove user keymap changes

--- keyboards/orthodox/keymaps/oscillope/config.h
+++ keyboards/orthodox/keymaps/oscillope/config.h
@@ -19,8 +19,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
 
+#include "config_common.h"
 
 /* Use I2C or Serial, not both */
 
@@ -35,3 +37,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 #define TAPPING_TOGGLE 3
+
+#endif
