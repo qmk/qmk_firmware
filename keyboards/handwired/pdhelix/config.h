914e41338bcf240487a0a7f10aec095ca96d7656
mtei <2170248+mtei@users.noreply.github.com>
2019-03-22 15:46:12 +0900

    Finish testing rgblight.c with quantum/split_common code.
    Revert "Add temporary code to split_common/transport.c to test rgblight.c."
    
    This reverts commit 71db3e24eef40d4c455fb9fd1664e4487c9d927a.

--- keyboards/handwired/pdhelix/config.h
+++ keyboards/handwired/pdhelix/config.h
@@ -61,7 +61,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_LIMIT_VAL 120 /* The maximum brightness level */
   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-  #define RGBLIGHT_SPLIT
 //  /*== all animations enable ==*/
 //   #define RGBLIGHT_ANIMATIONS
 // /*== or choose animations ==*/
