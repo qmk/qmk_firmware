488fca95c37fd9e78e8331e664cb323cbe17ec65
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2019-03-22 20:37:00 -0400

    Adding new tap dance code

--- users/stanrc85/stanrc85.h
+++ users/stanrc85/stanrc85.h
@@ -16,8 +16,23 @@ enum custom_keycodes {
 //Tap Dance Declarations
 enum {
 	TD_WIN = 0,
-	TD_ESC
+	TD_ESC,
+  TD_RCTL
 };
+
+// define a type containing as many tapdance states as you need
+typedef enum {
+  SINGLE_TAP,
+  SINGLE_HOLD,
+  DOUBLE_TAP
+} td_state_t;
+
+// function to determine the current tapdance state
+int cur_dance (qk_tap_dance_state_t *state);
+
+// `finished` and `reset` functions for each tapdance keycode
+void ctl_copy_finished (qk_tap_dance_state_t *state, void *user_data);
+void ctl_copy_reset (qk_tap_dance_state_t *state, void *user_data);
 
 //Aliases for longer keycodes
 #define KC_CAD	LALT(LCTL(KC_DEL))
@@ -28,3 +43,4 @@ enum {
 #define LT_SPCF LT(2, KC_SPC)
 #define TD_TESC TD(TD_ESC)
 #define TD_TWIN TD(TD_WIN)
+#define TD_CTL  TD(TD_RCTL)
