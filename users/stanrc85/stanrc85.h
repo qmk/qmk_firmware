2e4df12a89fe4b045b3ad36008977204e90da8cc
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2019-03-22 23:10:26 -0400

    Fix enums

--- users/stanrc85/stanrc85.h
+++ users/stanrc85/stanrc85.h
@@ -8,15 +8,24 @@
 #define LAYER2 2   //Function keys, arrows, custom shortcuts, volume control
 #define LAYER3 3   //RGB Underglow controls and RESET
 
-enum custom_keycodes {
-  KC_MAKE = SAFE_RANGE,
-  NEW_SAFE_RANGE  //use "NEW_SAFE_RANGE" for keymap specific codes
+//Aliases for longer keycodes
+#define KC_CAD	LALT(LCTL(KC_DEL))
+#define KC_LOCK	LGUI(KC_L)
+#define CA_QUOT LCA(KC_QUOT)
+#define CA_SCLN LCA(KC_SCLN)
+#define KC_CTLE LCTL_T(KC_ESC)
+#define LT_SPCF LT(2, KC_SPC)
+#define TD_TESC TD(TD_ESC)
+#define TD_TWIN TD(TD_WIN)
+#define TD_TCTL TD(TD_RCTL)
+
+enum cust_keys {
+	KC_MAKE = SAFE_RANGE
 };
 
-//Tap Dance Declarations
 enum tap_dance {
-	TD_ESC = 0,
   TD_WIN,
+  TD_ESC,
   TD_RCTL
 };
 
@@ -33,14 +42,3 @@ int cur_dance (qk_tap_dance_state_t *state);
 // `finished` and `reset` functions for each tapdance keycode
 void ctl_copy_finished (qk_tap_dance_state_t *state, void *user_data);
 void ctl_copy_reset (qk_tap_dance_state_t *state, void *user_data);
-
-//Aliases for longer keycodes
-#define KC_CAD	LALT(LCTL(KC_DEL))
-#define KC_LOCK	LGUI(KC_L)
-#define CA_QUOT LCA(KC_QUOT)
-#define CA_SCLN LCA(KC_SCLN)
-#define KC_CTLE LCTL_T(KC_ESC)
-#define LT_SPCF LT(2, KC_SPC)
-#define TD_TESC TD(TD_ESC)
-#define TD_TWIN TD(TD_WIN)
-#define TD_CTL  TD(TD_RCTL)
