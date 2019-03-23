e2f9261f3fd2ab43c291b0b217ad35149ab4496e
Drashna Jaelre <drashna@live.com>
2019-03-22 20:03:05 -0500

    Update keyboards/tetris/rules.mk
    Co-Authored-By: YCF <65446+YCF@users.noreply.github.com>

--- keyboards/tetris/rules.mk
+++ keyboards/tetris/rules.mk
@@ -55,6 +55,6 @@ SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
 AUDIO_ENABLE ?= yes
-RGBLIGHT_ENABLE ?= yes
+RGBLIGHT_ENABLE = yes
 EXTRAFLAGS        = -flto 
 ENCODER_ENABLE = yes
