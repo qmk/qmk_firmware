b4169eb254fd36129f13eb35dfea1cdfcc5ea338
hed854 <hed854k@gmail.com>
2019-03-24 21:01:46 +0100

    Enable I2C on the Quefrency for direct connection to the ESP32!

--- keyboards/keebio/quefrency/keymaps/monaka/config.h
+++ keyboards/keebio/quefrency/keymaps/monaka/config.h
@@ -23,5 +23,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Use I2C or Serial, not both */
 
-#define USE_SERIAL
-// #define USE_I2C
+//#define USE_SERIAL
+#define USE_I2C
