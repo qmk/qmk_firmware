36fd770dc068e2cdf80073980aae49cc6dc69220
Bryan Arendt <barendt@covermymeds.com>
2019-03-26 16:13:11 -0400

    update keys

--- keyboards/redox/keymaps/ctrlshiftbryan/keymap.c
+++ keyboards/redox/keymaps/ctrlshiftbryan/keymap.c
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_NO   ,                          KC_NO   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_NO ,                            KC_NO   ,KC_H    ,KC_J    ,KC_K    ,  KC_L ,KC_SCLN ,  MT(MOD_RGUI, KC_QUOT) ,
+     KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_NO ,                            KC_MEH  ,KC_H    ,KC_J    ,KC_K    ,  KC_L ,KC_SCLN ,  MT(MOD_RGUI, KC_QUOT) ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT , MT(MOD_LCTL, KC_Z), MT(MOD_LGUI, KC_X) ,KC_C ,KC_V , KC_B, KC_HOME, KC_END, KC_PGUP ,KC_PGDN ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_BKSL , KC_RSFT ,
+     KC_LSFT , MT(MOD_LCTL, KC_Z), MT(MOD_LGUI, KC_X) ,KC_C ,KC_V , KC_B, KC_HOME, KC_END, KC_PGUP ,KC_PGDN ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH , KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      KC_GRV  ,KC_QUOT ,KC_LEFT ,KC_RGHT ,     KC_SPC  ,    KC_SPC , KC_BSPC,         KC_RALT  ,KC_ENT  ,    KC_ENT  ,     KC_UP ,  KC_DOWN, KC_LBRC,KC_RBRC
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
