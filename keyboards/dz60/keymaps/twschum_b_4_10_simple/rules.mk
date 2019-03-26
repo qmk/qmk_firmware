19343461e1ce978cc9cbde4026a5bf57b4ef4378
Tim <tim.schumacher@nokia.com>
2019-03-26 13:42:52 -0400

    Add simplified version of keymap to help debug issues

---
+++ keyboards/dz60/keymaps/twschum_b_4_10_simple/rules.mk
@@ -0,0 +1,7 @@
+# Build Options
+# override the options specified in dz60/rules.mk
+
+BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
+BACKLIGHT_ENABLE = no  # Enable per-key backlight LEDs
+RGBLIGHT_ENABLE = no # Enable the RGB underglow LEDs
+KEY_LOCK_ENABLE = yes  # Enables use of KC_LOCK
