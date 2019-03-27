7cf897651f72a9052571c85d1ea11ca178f237e7
Alex Ong <alex.ong@unsw.edu.au>
2019-03-28 10:01:26 +1100

    Accidental merge with ergodox_ez

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -83,7 +83,6 @@ SLEEP_LED_ENABLE = no
 API_SYSEX_ENABLE = no
 RGBLIGHT_ENABLE = yes
 RGB_MATRIX_ENABLE = no # enable later
-DEBOUNCE_TYPE = eager_pr
 
 ifeq ($(strip $(RGB_MATRIX_ENABLE)), no)
   SRC += i2c_master.c
