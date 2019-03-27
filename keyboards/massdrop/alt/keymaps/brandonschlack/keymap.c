9a6d7cde3d02c57a18f507bd62306a7218e3004b
brandonschlack <brandonschlack@gmail.com>
2019-03-26 23:24:23 -0700

    Added media, macOS keycodes to massdrop/alt

--- keyboards/massdrop/alt/keymaps/brandonschlack/keymap.c
+++ keyboards/massdrop/alt/keymaps/brandonschlack/keymap.c
@@ -50,22 +50,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     /* Layer 1: Function Layer
       * ,---------------------------------------------------------------.
-      * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|       |Mut|
+      * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   SLPD|Mut|
       * |---------------------------------------------------------------|
-      * |LBr  |LS-|LB+|LS+|   |   |   |UAT|GCR|   |F13|F14|F15|     |End|
+      * |LBr  |LS-|LB+|LS+|   |   |   |UAT|GCR|   |F13|F14|F15|  LHP|End|
       * |---------------------------------------------------------------|
       * |LPD   |LP-|LB-|LP+|   |   |   |   |   |   |   |   |        |VlU|
       * |---------------------------------------------------------------|
-      * |        |LTM|LTO|   |   |DFU|KRO|   |   |   |   |      |PgU|VlD|
+      * |        |LTM|LTO|   |   |DFU|KRO|   |Prv|Nxt|Ply|      |PgU|VlD|
       * |---------------------------------------------------------------|
       * |    |    |    |                       |    |    |  |Hom|PgD|End|
       * `---------------------------------------------------------------'
       */
     [1] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_MUTE, \
-        L_T_BR,  L_PSD,   L_BRI,   L_PSI,   KC_TRNS, KC_TRNS, KC_TRNS, U_T_AUTO,U_T_AGCR,KC_TRNS, KC_F13,  KC_F14,  KC_F15,  KC_TRNS, KC_END, \
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  MC_SLPD, KC_MUTE, \
+        L_T_BR,  L_PSD,   L_BRI,   L_PSI,   KC_TRNS, KC_TRNS, KC_TRNS, U_T_AUTO,U_T_AGCR,KC_TRNS, KC_F13,  KC_F14,  KC_F15,  MC_LHPD, KC_END, \
         L_T_PTD, L_PTP,   L_BRD,   L_PTN,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_VOLU, \
-        KC_TRNS, L_T_MD,  L_T_ONF, KC_TRNS, KC_TRNS, MD_BOOT, TG_NKRO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_PGUP, KC_VOLD, \
+        KC_TRNS, L_T_MD,  L_T_ONF, KC_TRNS, KC_TRNS, MD_BOOT, TG_NKRO, KC_TRNS, KC_MRWD, KC_MFFD, KC_MPLY, KC_TRNS,          KC_PGUP, KC_VOLD, \
         KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END  \
     ),
     /*
