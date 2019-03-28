f62d0814813b04b62ad89c5a742e087b91e4954b
Jeremy Bernhardt <bernhardtjeremy@gmail.com>
2019-03-28 12:07:14 -0600

    ErgoTaco support

---
+++ keyboards/ergotaco/keymaps/default/rules.mk
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------------------
+# make gergo:germ:dfu
+# Make sure you have dfu-programmer installed!
+#----------------------------------------------------------------------------
+# Firmware options
+
+#Debug options
+VERBOSE 		 		= yes
+DEBUG_MATRIX_SCAN_RATE  = no
+DEBUG_MATRIX		 	= yes
+
+# A bunch of stuff that you shouldn't touch unless you
+# know what you're doing.
+#
+# No touchy, capiche?
+SRC += matrix.c i2c_master.c
+ifeq ($(strip $(DEBUG_MATRIX)), yes)
+    OPT_DEFS += -DDEBUG_MATRIX
+endif
