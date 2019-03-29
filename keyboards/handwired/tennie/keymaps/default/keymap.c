a4826e5cd20ba309e247374bfd482aef47033b1e
Jack H <jack@jackhil.de>
2019-03-28 19:56:27 -0500

    Fix broken keymaps

--- keyboards/handwired/tennie/keymaps/default/keymap.c
+++ keyboards/handwired/tennie/keymaps/default/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   //    ├────────┼────────┼────────┼────────┼
 
-               KC_SHRK,  KCOGRE, KC_TCP
+               KC_SHRK, KC_OGRE, KC_TCP
 
   //         └────────┴────────┴────────┘
   ),
--- keyboards/handwired/tennie/keymaps/test/keymap.c
+++ keyboards/handwired/tennie/keymaps/test/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [base] = LAYOUT(
   //         ┌────────┬────────┬────────┐
 
-                KC_1  ,  KC_2  ,  LC_3  ,
+                KC_1  ,  KC_2  ,  KC_3  ,
 
   //    ├────────┼────────┼────────┼────────┼
 
