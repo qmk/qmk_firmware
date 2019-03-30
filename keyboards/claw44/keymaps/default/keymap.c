e3b2d6808386ddaa8bdff04ff7f206f2f2525e9e
Yuya Fukumoto <fuku@rockme.co.jp>
2019-03-30 11:00:34 +0900

    add claw44 keyboard

---
+++ keyboards/claw44/keymaps/default/keymap.c
@@ -0,0 +1,180 @@
+#include QMK_KEYBOARD_H
+#include "bootloader.h"
+#ifdef PROTOCOL_LUFA
+  #include "lufa.h"
+  #include "split_util.h"
+#endif
+#ifdef SSD1306OLED
+  #include "ssd1306.h"
+#endif
+
+extern keymap_config_t keymap_config;
+
+extern uint8_t is_master;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layer_number {
+  _QWERTY = 0,
+  _LOWER,
+  _RAISE,
+};
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE
+};
+
+enum macro_keycodes {
+  KC_SAMPLEMACRO,
+};
+
+#define KC_ KC_TRNS
+#define KC_RST RESET
+#define KC_L_SPC LT(_LOWER, KC_SPC) // lower
+#define KC_R_ENT LT(_RAISE, KC_ENT) // raise
+#define KC_G_JA LGUI_T(KC_LANG1) // cmd or win
+#define KC_G_EN LGUI_T(KC_LANG2) // cmd or win
+#define KC_C_BS LCTL_T(KC_BSPC) // ctrl
+#define KC_A_DEL ALT_T(KC_DEL) // alt
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT_kc( \
+  //,----+----+----+----+----+----.     ,----+----+----+----+----+----.
+     ESC , Q  , W  , E  , R  , T  ,       Y  , U  , I  , O  , P  ,MINS,
+  //|----+----+----+----+----+----|     |----+----+----+----+----+----|
+     TAB , A  , S  , D  , F  , G  ,       H  , J  , K  , L  ,SCLN,QUOT,
+  //|----+----+----+----+----+----+     |----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,       N  , M  ,COMM,DOT ,SLSH,RSFT,
+  //`----+----+----+----+----+----/     \----+----+----+----+----+----'
+             A_DEL,G_EN,L_SPC,C_BS,      C_BS,R_ENT,G_JA,A_DEL
+  //          `----+----+----+----'     `----+----+----+----'
+  ),
+
+  //   \ ^ ! & |  @ = + * % -
+  // ( # $ " ' ~  ← ↓ ↑ → ` )
+  //         { [  ] }
+
+  [_RAISE] = LAYOUT_kc( \
+  //,----+----+----+----+----+----.     ,----+----+----+----+----+----.
+         ,BSLS,CIRC,EXLM,AMPR,PIPE,      AT  ,EQL ,PLUS,ASTR,PERC,MINS,
+  //|----+----+----+----+----+----|     |----+----+----+----+----+----|
+     LPRN,HASH,DLR ,DQT ,QUOT,TILD,      LEFT,DOWN, UP ,RGHT,GRV ,RPRN,
+  //|----+----+----+----+----+----|     |----+----+----+----+----+----|
+        ,     ,    ,    ,LCBR,LBRC,      RBRC,RCBR,    ,    ,    ,    ,
+  //`----+----+----+----+----+----/     \----+----+----+----+----+----'
+                   ,    ,    ,    ,          ,    ,    ,RST
+  //          `----+----+----+----'     `----+----+----+----'
+  ),
+
+  [_LOWER] = LAYOUT_kc( \
+  //,----+----+----+----+----+----.     ,----+----+----+----+----+----.
+      F1 , F2 , F3 , F4 , F5 , F6 ,          ,EQL ,PLUS,ASTR,PERC,MINS,
+  //|----+----+----+----+----+----|     |----+----+----+----+----+----|
+         , 1  , 2  , 3  , 4  , 5  ,       6  , 7  , 8  , 9  , 0  ,    ,
+  //|----+----+----+----+----+----|     |----+----+----+----+----+----|
+      F7 , F8 , F9 , F10, F11, F12,          ,    ,COMM,DOT ,SLSH,    ,
+  //`----+----+----+--+-+----+----/     \----+----+----+----+----+----'
+               RST ,    ,    ,    ,          ,    ,    ,
+  //          `----+----+----+----'     `----+----+----+----'
+  ),
+};
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+void matrix_init_user(void) {
+  //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+  #ifdef SSD1306OLED
+    iota_gfx_init(!has_usb());   // turns on the display
+  #endif
+}
+
+//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
+#ifdef SSD1306OLED
+
+// When add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+void matrix_scan_user(void) {
+  iota_gfx_task();
+}
+
+void matrix_render_user(struct CharacterMatrix *matrix) {
+  if (is_master) {
+    // If you want to change the display of OLED, you need to change here
+    matrix_write_ln(matrix, read_layer_state());
+    matrix_write_ln(matrix, read_keylog());
+    matrix_write_ln(matrix, read_keylogs());
+    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+    //matrix_write_ln(matrix, read_host_led_state());
+    //matrix_write_ln(matrix, read_timelog());
+  } else {
+    matrix_write(matrix, read_logo());
+  }
+}
+
+void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
+  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+    memcpy(dest->display, source->display, sizeof(dest->display));
+    dest->dirty = true;
+  }
+}
+
+void iota_gfx_task_user(void) {
+  struct CharacterMatrix matrix;
+  matrix_clear(&matrix);
+  matrix_render_user(&matrix);
+  matrix_update(&display, &matrix);
+}
+#endif//SSD1306OLED
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+#ifdef SSD1306OLED
+    set_keylog(keycode, record);
+#endif
+    // set_timelog();
+  }
+
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+      } else {
+        layer_off(_LOWER);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+      } else {
+        layer_off(_RAISE);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
---
+++ keyboards/claw44/readme.md
@@ -0,0 +1,14 @@
+# Planck
+
+![Claw44](https://i.imgur.com/5a8iogl.jpg)
+
+A split keyboard with 3x6 vertically staggered keys and 4 thumb keys.
+
+Keyboard Maintainer: [@yfuku_](https://twitter.com/yfuku_) 
+
+Make example for this keyboard (after setting up your build environment):
+
+    make claw44:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+There needs to be two spaces at the end of the Keyboard Maintainer and Hardware Supported lines for it to render correctly with Markdown.
