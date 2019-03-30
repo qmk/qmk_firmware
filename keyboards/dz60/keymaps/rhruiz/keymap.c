5e582aecfd0ee0830d6c62cdfaa1d09b792c9952
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-03-29 23:18:24 -0300

    fn-less left space on the dz60

--- keyboards/dz60/keymaps/rhruiz/keymap.c
+++ keyboards/dz60/keymaps/rhruiz/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,      KC_T,    KC_Y,    KC_U,    KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
 		KC_LCTL,   KC_A,     KC_S,     KC_D,    KC_F,      KC_G,    KC_H,    KC_J,    KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
 		KC_LSFT,   KC_NO,    KC_Z,     KC_X,    KC_C,      KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  MO(_FN2),
-		MO(_FN1),  KC_LALT,  KC_LGUI,  SPCFN2,  MO(_FN2),  BKSFN1,  CLLEFT,  CLDOWN,  KC_NO,  CLUP,     CLRIGHT
+		MO(_FN1),  KC_LALT,  KC_LGUI,  KC_SPC,  MO(_FN2),  BKSFN1,  CLLEFT,  CLDOWN,  KC_NO,  CLUP,     CLRIGHT
   ),
 
   [_VIM_EMACS] = LAYOUT(
