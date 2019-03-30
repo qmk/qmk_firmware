9cf994d5308e9a3344aead08454446701551a91d
Yuya Fukumoto <fuku@rockme.co.jp>
2019-03-30 17:50:02 +0900

    Move layer definition

--- keyboards/claw44/keymaps/yfuku/keymap.c
+++ keyboards/claw44/keymaps/yfuku/keymap.c
@@ -16,12 +16,6 @@ extern uint8_t is_master;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-enum layer_number {
-  _QWERTY = 0,
-  _LOWER,
-  _RAISE,
-};
-
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
