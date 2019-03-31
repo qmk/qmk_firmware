c070e966e7c22d2973901243c8a1c6e41cf62944
Nicholas Shaff <nick@shadedream.com>
2019-03-30 21:49:34 -0500

    Renamed split_space to multi.

--- keyboards/doro67/multi/multi.h
+++ keyboards/doro67/multi/multi.h
@@ -31,7 +31,7 @@
   {   K40,   K41,   K42,   KC_NO, K44,   KC_NO, KC_NO, KC_NO, KC_NO, K49,   K4A,   KC_NO, K4C,   K4D,   K4E   }, \
 }
 
-#define LAYOUT_split_space( \
+#define LAYOUT_multi( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K48, K0E, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2D, K2E, \
