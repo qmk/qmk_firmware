bc00f104b576630e1e14629462e2f55ae7ba2326
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2019-04-01 12:25:06 -0500

    Update keymap.c

--- keyboards/romac/keymaps/default/keymap.c
+++ keyboards/romac/keymaps/default/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_1, KC_2, KC_3, \
 		MO(_FN1), KC_0, KC_ENT) \
 	),
-	[_1] = LAYOUT_ortho_3x4(
+	[_FN1] = LAYOUT_ortho_3x4(
 		KC_TRNS, KC_HOME, KC_PGUP, \
 		KC_TRNS, KC_END, KC_PGDN, \
 		KC_TRNS, KC_TRNS, KC_TRNS, \
