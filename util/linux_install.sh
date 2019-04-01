1bc886b32f234631f8f7587d18834f9389b25cba
Sorixelle <srxl@protonmail.com>
2019-04-01 20:42:54 +1100

    Fix typos + grammatical edits in comments

--- util/linux_install.sh
+++ util/linux_install.sh
@@ -183,9 +183,9 @@ elif grep ID /etc/os-release | grep -q solus; then
 elif grep ID /etc/os-release | grep -q void; then
 	# musl Void systems don't have glibc cross compilers avaliable in their repos.
 	# glibc Void systems do have musl cross compilers though, for some reason.
-	# So, default to musl, and switch to glibc is glibc is detected.
+	# So, default to musl, and switch to glibc if it is installed.
 	CROSS_ARM=cross-arm-linux-musleabi
-	if xbps-query glibc > /dev/null; then # Check is glibc is installed
+	if xbps-query glibc > /dev/null; then # Check is glibc if installed
 		CROSS_ARM=cross-arm-linux-gnueabi
 	fi
 
