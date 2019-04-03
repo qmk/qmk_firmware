c98247e3dd2958bd2d8969dc75170e7e2757b895
XScorpion2 <rcalt2vt@gmail.com>
2019-04-02 19:24:14 -0500

    RGB Matrix Overhaul (#5372)
    * RGB Matrix overhaul
    Breakout of animations to separate files
    Integration of optimized int based math lib
    Overhaul of rgb_matrix.c and animations for performance
    
    * Updating effect function api for future extensions
    
    * Combined the keypresses || keyreleases define checks into a single define so I stop forgetting it where necessary
    
    * Moving define RGB_MATRIX_KEYREACTIVE_ENABLED earlier in the include chain

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -109,7 +109,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DRIVER_1_LED_TOTAL 24
 #define DRIVER_2_LED_TOTAL 24
 #define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL
-#define RGB_MATRIX_SKIP_FRAMES 10
 
 // #define RGBLIGHT_COLOR_LAYER_0 0x00, 0x00, 0xFF
 /* #define RGBLIGHT_COLOR_LAYER_1 0x00, 0x00, 0xFF */
