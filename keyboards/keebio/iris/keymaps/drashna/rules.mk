5ad19cda087c58fa103bc86712c8d7769b1d26bb
Drashna Jaelre <drashna@live.com>
2019-04-03 11:36:41 -0700

    Cries in AVR

--- keyboards/keebio/iris/keymaps/drashna/rules.mk
+++ keyboards/keebio/iris/keymaps/drashna/rules.mk
@@ -5,14 +5,14 @@ CONSOLE_ENABLE    = no         # Console for debug(+400)
 COMMAND_ENABLE    = no        # Commands for debug and configuration
 TAP_DANCE_ENABLE  = no
 RGBLIGHT_ENABLE   = yes
-AUDIO_ENABLE      = yes
+AUDIO_ENABLE      = no
 NKRO_ENABLE       = yes
 BACKLIGHT_ENABLE  = no
 SWAP_HANDS_ENABLE = no
 
-INDICATOR_LIGHTS  = no
+INDICATOR_LIGHTS  = yes
 MACROS_ENABLED    = no
-RGBLIGHT_TWINKLE  = no
+RGBLIGHT_TWINKLE  = yes
 RGBLIGHT_STARTUP_ANIMATION = yes
 
 BOOTLOADER        = qmk-dfu
