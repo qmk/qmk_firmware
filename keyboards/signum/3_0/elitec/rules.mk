e01a92360613eff4c7d8b3063d6ea5803730a3ec
Jan Christoph Ebersbach <jceb@e-jc.de>
2019-04-05 21:06:24 +0200

    Use bootmagic lite instead of full

--- keyboards/signum/3_0/elitec/rules.mk
+++ keyboards/signum/3_0/elitec/rules.mk
@@ -48,7 +48,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE = no	# Console for debug(+400)
