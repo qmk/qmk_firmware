01bf084b4e444e088573870338d4f44f9c96aec9
zvecr <git@zvecr.com>
2019-04-06 22:03:23 +0100

    Add backlight pwm bodge till #4324 lands

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -1125,6 +1125,13 @@ static const uint8_t backlight_pin = BACKLIGHT_PIN;
 #  define COMxx1 COM1A1
 #  define OCRxx  OCR3A
 #  define ICRx   ICR3
+#elif defined(__AVR_ATmega32A__) && BACKLIGHT_PIN == D4
+#  define TCCRxA TCCR1A
+#  define TCCRxB TCCR1B
+#  define COMxx1 COM1B1
+#  define OCRxx  OCR1B
+#  define ICRx   ICR1
+#  define TIMSK1 TIMSK
 #else
 #  define NO_HARDWARE_PWM
 #endif
