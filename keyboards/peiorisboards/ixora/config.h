13f733eeb2eb0c4d958066cd75f266c7e4e0fb05
Peioris <mfmayol@up.edu.ph>
2019-04-09 09:15:40 +0800

    added ixora files

---
+++ keyboards/peiorisboards/ixora/config.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0C61
+#define DEVICE_VER      0x00C6
+#define MANUFACTURER    PeiorisBoards
+#define PRODUCT         Ixora Rev1
+#define DESCRIPTION     6key Macropad
+
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 6
+
+#define MATRIX_ROW_PINS { A0 }
+#define MATRIX_COL_PINS { B4, A15, B3, A1, B6, B5 }
+
+/* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 0
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is userful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+// #define GRAVE_ESC_CTRL_OVERRIDE
+
+/*
+ * Force NKRO
+ *
+ * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
+ * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
+ * makefile for this to work.)
+ *
+ * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
+ * until the next keyboard reset.
+ *
+ * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
+ * fully operational during normal computer usage.
+ *
+ * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
+ * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
+ * bootmagic, NKRO mode will always be enabled until it is toggled again during a
+ * power-up.
+ *
+ */
+//#define FORCE_NKRO
+
+/*
+ * Magic Key Options
+ *
+ * Magic keys are hotkey commands that allow control over firmware functions of
+ * the keyboard. They are best used in combination with the HID Listen program,
+ * found here: https://www.pjrc.com/teensy/hid_listen.html
+ *
+ * The options below allow the magic key functionality to be changed. This is
+ * useful if your keyboard/keypad is missing keys and you want magic key support.
+ *
+ */
+
+/* key combination for magic key command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
---
+++ keyboards/peiorisboards/ixora/ixora.h
@@ -0,0 +1,42 @@
+#ifndef IXORA_H
+#define IXORA_H
+
+#define XXX KC_NO
+
+#include "quantum.h"
+
+// This a shortcut to help you visually see your layout.
+
+#define LAYOUT_FULL( \
+    K00, K01, K02, \
+    K03, K04, K05  \
+) \
+{ \
+    { K00, K01, K02, K03, K04, K05 } \
+}
+
+#define LAYOUT_BLOCKER_RIGHT( \
+    K00, K01,      \
+    K03, K04, K05  \
+) \
+{ \
+    { K00, K01, XXX, K03, K04, K05 } \
+}
+
+#define LAYOUT_BLOCKER_LEFT( \
+         K01, K02, \
+    K03, K04, K05  \
+) \
+{ \
+    { XXX, K01, K02, K03, K04, K05 } \
+}
+
+#define LAYOUT_ARROWS( \
+         K01,      \
+    K03, K04, K05  \
+) \
+{ \
+    { XXX, K01, XXX, K03, K04, K05 } \
+}
+
+#endif
