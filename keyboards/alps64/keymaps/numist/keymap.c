8cc9ea410b0524949992a60b99e0f0fc8c252c02
Scott Perry <dev@numist.net>
2019-04-08 19:38:53 -0700

    Remove unused bindings, add some more experiments

--- keyboards/alps64/keymaps/numist/keymap.c
+++ keyboards/alps64/keymaps/numist/keymap.c
@@ -11,20 +11,22 @@ enum keyboard_layers {
 #define KC_RADN RALT_T(KC_DOWN)
 #define KC_RGLT RCMD_T(KC_LEFT)
 #define KC_RCRT RCTL_T(KC_RIGHT)
+#define KC_LSUP LSFT_T(KC_PGUP)
+#define KC_LCDN LCTL_T(KC_PGDN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT_aek_103(
     KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     KC_CTES, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSUP,
-    KC_LCTL, KC_LALT, KC_LGUI,                   LT(_FL ,KC_SPC),                              KC_RGLT, KC_RADN, KC_RCRT
+    KC_LSUP, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSUP,
+    KC_LCDN, KC_LALT, KC_LGUI,                   LT(_FL ,KC_SPC),                              KC_RGLT, KC_RADN, KC_RCRT
   ),
   [_FL] = LAYOUT_aek_103(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    _______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, RESET,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_PAUS, _______, KC_PGUP,
+    _______, _______, _______, _______, _______, _______, KC_PAUS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______, _______, RESET,
+    _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, KC_PGUP,
     _______, _______, _______,                            _______,                            KC_HOME, KC_PGDN, KC_END
   )
 };
