fedc1ab9f50adf3693bad0e4cc4b509360325492
Brandon Schlack <brandonschlack@gmail.com>
2019-04-10 13:02:48 -0700

    [Keymap] Move LED controls to Magic layer for Massdrop/Alt

--- keyboards/massdrop/alt/keymaps/brandonschlack/keymap.c
+++ keyboards/massdrop/alt/keymaps/brandonschlack/keymap.c
@@ -45,27 +45,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(FL),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(FL),  KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     /* Layer 1: Function Layer
       * ,---------------------------------------------------------------.
       * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   SLPD|Mut|
       * |---------------------------------------------------------------|
-      * |LBr  |LS-|LB+|LS+|   |   |   |   |GCR|   |F13|F14|F15|  LHP|End|
+      * |     |   |   |   |DFU|   |   |   |   |   |F13|F14|F15|  LHP|End|
       * |---------------------------------------------------------------|
-      * |LPD   |LP-|LB-|LP+|   |   |   |   |   |   |   |   |        |VlU|
+      * |      |   |   |   |   |   |   |   |   |   |   |   |        |VlU|
       * |---------------------------------------------------------------|
-      * |        |LTM|LTO|   |   |DFU|KRO|   |Prv|Nxt|Ply|      |PgU|VlD|
+      * |        |   |   |   |   | ML|   |   |Prv|Nxt|Ply|      |PgU|VlD|
       * |---------------------------------------------------------------|
       * |    |    |    |                       |    |    |  |Hom|PgD|End|
       * `---------------------------------------------------------------'
       */
     [FL] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  MC_SLPD, MC_MUTE, \
-        L_T_BR,  L_PSD,   L_BRI,   L_PSI,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, U_T_AGCR,KC_TRNS, KC_F13,  KC_F14,  KC_F15,  MC_LHPD, KC_END, \
-        L_T_PTD, L_PTP,   L_BRD,   L_PTN,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, MC_VOLU, \
-        KC_TRNS, L_T_MD,  L_T_ONF, KC_TRNS, KC_TRNS, MD_BOOT, TG_NKRO, KC_TRNS, KC_MRWD, KC_MFFD, KC_MPLY, KC_TRNS,          KC_PGUP, MC_VOLD, \
-        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END  \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MD_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F13,  KC_F14,  KC_F15,  MC_LHPD, KC_END,  \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, MC_VOLU, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DF(ML),  KC_TRNS, KC_TRNS, KC_MRWD, KC_MFFD, KC_MPLY, KC_TRNS,          KC_PGUP, MC_VOLD, \
+        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END   \
+    ),
+    /* Layer 2: Magic Layer
+      * ,---------------------------------------------------------------.
+      * | BL|   |   |   |   |   |   |   |   |   |   |   |   |       |   |
+      * |---------------------------------------------------------------|
+      * |LBr  |LS-|LB+|LS+|   |   |   |   |GCR|   |   |   |   |     |   |
+      * |---------------------------------------------------------------|
+      * |LPD   |LP-|LB-|LP+|   |   |   |   |   |   |   |   |        |   |
+      * |---------------------------------------------------------------|
+      * |        |LTM|LTO|   |   |   |KRO|   |   |   |   |      |   |   |
+      * |---------------------------------------------------------------|
+      * |    |    |    |                       |    |    |  |   |   |   |
+      * `---------------------------------------------------------------'
+      */
+    [ML] = LAYOUT(
+        DF(BL),  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+        L_T_BR,  L_PSD,   L_BRI,   L_PSI,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, U_T_AGCR,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+        L_T_PTD, L_PTP,   L_BRD,   L_PTN,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, \
+        XXXXXXX, L_T_MD,  L_T_ONF, XXXXXXX, XXXXXXX, XXXXXXX, TG_NKRO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, \
+        XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  \
     ),
     /*
     [X] = LAYOUT(
