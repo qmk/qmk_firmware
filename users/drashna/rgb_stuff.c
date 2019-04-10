abd1615e5735004325086b3f9f241036d31fed89
Drashna Jaelre <drashna@live.com>
2019-04-09 19:06:42 -0700

    Add some additional handling for rgb init

--- users/drashna/rgb_stuff.c
+++ users/drashna/rgb_stuff.c
@@ -248,6 +248,7 @@ bool process_record_user_rgb(uint16_t keycode, keyrecord_t *record) {
 
 void keyboard_post_init_rgb(void) {
 #if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_STARTUP_ANIMATION)
+  if (userspace_config.rgb_layer_change) { rgblight_enable_noeeprom(); }
 	if (rgblight_config.enable) {
     layer_state_set_user(layer_state);
     uint16_t old_hue = rgblight_config.hue;
