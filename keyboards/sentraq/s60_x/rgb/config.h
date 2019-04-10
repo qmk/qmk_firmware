47c1817267375b2a5e79b5346fa1abf97a196739
mechmerlin <mechmerlin@gmail.com>
2019-04-09 19:55:51 -0700

    fix up rgb pcb rules and config that previously depended on rules and config in a parent directory

--- keyboards/sentraq/s60_x/rgb/config.h
+++ keyboards/sentraq/s60_x/rgb/config.h
@@ -3,19 +3,40 @@
 
 #include "config_common.h"
 
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Massdrop
 #define PRODUCT         S60-X-RGB
 #define DESCRIPTION     q.m.k. keyboard firmware for S60-X RGB
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
 
 /* key matrix pins */
 #define MATRIX_ROW_PINS { B5, B4, D7, D6, D4 }
 #define MATRIX_COL_PINS { D0, D1, D2, D3, D5, B6, C6, C7, F1, F0, E6, B3, B2, B1, B0 }
 #define UNUSED_PINS
 
+/* number of backlight levels */
+#define BACKLIGHT_PIN B7
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 3
+#endif
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCING_DELAY 5
 
 #define RGB_DI_PIN F6
 #ifdef RGB_DI_PIN
