147e9090a5323f3e23a0c5b49a81f7092f94c386
James <bronzegears@gmail.com>
2019-04-09 22:52:07 -0400

    updating the Corne OLED

--- keyboards/crkbd/keymaps/klackygears/rules.mk
+++ keyboards/crkbd/keymaps/klackygears/rules.mk
@@ -4,7 +4,7 @@
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -18,14 +18,15 @@ RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 TAP_DANCE_ENABLE = yes
 
-LED_ANIMATIONS = yes
-LED_BACK_ENABLE = yes
+#LED_ANIMATIONS = yes
+#LED_BACK_ENABLE = yes
 
 #EXTRAFLAGS += -flto
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
+OLED_ENABLE = yes
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
         ./lib/rgb_state_reader.c \
