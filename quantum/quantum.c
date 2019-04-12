1dbd951322b164e928df1519b171fffc57565cfe
Mike Roberts <miker@miker.org>
2019-04-12 17:00:47 -0400

    switch ifdef name

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -1058,7 +1058,7 @@ void matrix_init_quantum() {
   #ifdef HAPTIC_ENABLE
     haptic_init();
   #endif
-  #ifdef MODULE_ADAFRUIT_BLE
+  #ifdef OUTPUT_AUTO_ENABLE
     set_output(OUTPUT_AUTO);
   #endif
   matrix_init_kb();
