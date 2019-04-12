c9c9d3f23a4127394b1e733a24277bec5eefcdd5
roguepullrequest <roguepullrequest@users.noreply.github.com>
2019-04-12 23:47:03 +0100

    Removed layer songs
    Removed layer songs for cleanliness. Will use them later.

--- keyboards/planck/keymaps/roguepullrequest/config.h
+++ keyboards/planck/keymaps/roguepullrequest/config.h
@@ -5,11 +5,4 @@
 
 #ifdef AUDIO_ENABLE
 	#define STARTUP_SONG SONG(RICK_ROLL)
-	
-	#define DEFAULT_LAYER_SONGS { \
-/*		SONG(ZELDA_PUZZLE),   \
-	       	SONG(MARIO_MUSHROOM), \
-		SONG(MARIO_GAMEOVER), \
-		SONG(RICK_ROLL)       \
-*/	}
 #endif
