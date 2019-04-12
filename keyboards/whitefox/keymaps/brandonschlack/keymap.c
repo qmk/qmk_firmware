f7befec1340f650a4bd4b9572d9e0032ca57f8cd
brandonschlack <brandonschlack@gmail.com>
2019-04-12 10:54:33 -0700

    [Keymap] Update whitefox with kbd67/hotswap keymap

--- keyboards/whitefox/keymaps/brandonschlack/keymap.c
+++ keyboards/whitefox/keymaps/brandonschlack/keymap.c
@@ -29,17 +29,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          * `---------------------------------------------------------------'
          */
     [BL] = LAYOUT_truefox(\
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_BSLS, KC_HOME,\
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_END,\
-        HY_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP,\
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,\
-        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, TT(FL), KC_LEFT, KC_DOWN, KC_RGHT\
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_HOME, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_END,  \
+        HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT, KC_UP,   KC_PGDN, \
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, MO(FL),                    KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
         /* Keymap FL: Function Layer
          * ,---------------------------------------------------------------.
-         * |RST| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|MSN|LHP|SLP|
+         * |MKE| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|MSN|LHP|SLP|
          * |---------------------------------------------------------------|
-         * |     |   |   |   |   |   |   |   |   |   |F13|F14|F15|  Del|Mut|
+         * |     |   |   |   |RST|   |   |   |   |   |F13|F14|F15|  Del|Mut|
          * |---------------------------------------------------------------|
          * |      |   |   |   |   |   |   |   |   |   |   |   |        |VlU|
          * |---------------------------------------------------------------|
@@ -49,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          * `---------------------------------------------------------------'
          */
     [FL] = LAYOUT_truefox(\
-        RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, MC_MSSN, MC_LHPD, MC_SLPD,\
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_F13,  KC_F14,  KC_F15, KC_DEL, MC_MUTE,\
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MC_VOLU,\
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_MRWD, KC_MFFD, KC_MPLY, _______, _______, MC_VOLD,\
-        _______, _______, _______, _______, _______, _______, _______, _______, _______\
+        KC_MAKE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, MC_MSSN, MC_LHPD, MC_SLPD, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F13,  KC_F14,   KC_F15, KC_DEL,           MC_MUTE, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          MC_VOLU, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MRWD, KC_MFFD, KC_MPLY,                   KC_TRNS, KC_PGUP, MC_VOLD, \
+        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,                   KC_HOME, KC_PGDN, KC_END   \
     ),
 };
