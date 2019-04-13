83a45609c61ce63616790ff477fc135291ace4eb
zk-phi <zerokay.phi@gmail.com>
2019-04-13 23:12:04 +0900

    Update switch42 default keymap

--- keyboards/switch42/keymaps/default/keymap.c
+++ keyboards/switch42/keymaps/default/keymap.c
@@ -1,186 +1,204 @@
-#include QMK_KEYBOARD_H
-#include "bootloader.h"
-#ifdef PROTOCOL_LUFA
-  #include "lufa.h"
-  #include "split_util.h"
-#endif
-
-extern keymap_config_t keymap_config;
-
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
+#include "switch42.h"
 
 extern uint8_t is_master;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 3
-#define _RAISE 4
-#define _ADJUST 16
+/* GLOBAL VARS */
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-  BACKLIT,
-  RGBRST
-};
+#define BASE      0
+#define RAISE     1
+#define FUNCTION  2
+#define GARAKE    3
+#define GARARAISE 4
+#define TENKEY    5
+#define MOUSE     6
+#define WHEEL     7
 
-enum macro_keycodes {
-  KC_SAMPLEMACRO,
-};
+#define L_BASE      0
+#define L_RAISE     (2 << (RAISE - 1))
+#define L_FUNCTION  (2 << (FUNCTION - 1))
+#define L_GARAKE    (2 << (GARAKE - 1))
+#define L_GARARAISE (2 << (GARARAISE - 1))
+#define L_TENKEY    (2 << (TENKEY - 1))
+#define L_MOUSE     (2 << (MOUSE - 1))
+#define L_WHEEL     (2 << (WHEEL - 1))
 
-#define KC______ KC_TRNS
-#define KC_XXXXX KC_NO
-#define KC_LOWER LOWER
-#define KC_RAISE RAISE
-#define KC_RST   RESET
-#define KC_LRST  RGBRST
-#define KC_LTOG  RGB_TOG
-#define KC_LHUI  RGB_HUI
-#define KC_LHUD  RGB_HUD
-#define KC_LSAI  RGB_SAI
-#define KC_LSAD  RGB_SAD
-#define KC_LVAI  RGB_VAI
-#define KC_LVAD  RGB_VAD
-#define KC_LMOD  RGB_MOD
-#define KC_CTLTB CTL_T(KC_TAB)
-#define KC_GUIEI GUI_T(KC_LANG2)
-#define KC_ALTKN ALT_T(KC_LANG1)
+/* FEATURES */
+
+#ifdef RGBLIGHT_ENABLE
+#include "./rgb.c"
+#endif
+#ifdef TAP_DANCE_ENABLE
+#include "./dance.c"
+#endif
+
+/* KEYCODE DEFINITIONS */
+
+#define KC_____ KC_TRNS
+#define KC_XXXX KC_NO
+
+#define KC_D_MOUS  LT(MOUSE, KC_D)
+#define KC_L1_RAI  LT(RAISE, KC_LANG1)
+#define KC_ESC_FN  LT(FUNCTION, KC_ESC)
+#define KC_L2_ALT  LALT_T(KC_LANG2)
+#define KC_WEEL    MO(WHEEL)
+#define KC_BASE    TO(BASE)
+#define KC_GARAKE  TG(GARAKE)
+#define KC_TENKEY  TG(TENKEY)
+#define KC_ALT_EN  LALT_T(KC_ENT)
+#define KC_CTL_TB  LCTL_T(KC_TAB)
+
+#ifdef TAP_DANCE_ENABLE
+#define KC_GK1     TD(TD_GARAKE1)
+#define KC_GK2     TD(TD_GARAKE2)
+#define KC_GK3     TD(TD_GARAKE3)
+#define KC_GK4     TD(TD_GARAKE4)
+#define KC_GK5     TD(TD_GARAKE5)
+#define KC_GK6     TD(TD_GARAKE6)
+#define KC_GK7     TD(TD_GARAKE7)
+#define KC_GK8     TD(TD_GARAKE8)
+#define KC_GK9     TD(TD_GARAKE9)
+#define KC_GK0_RA  TD(TD_GARAKE0_RAISE)
+#define KC_CTL_L1  TD(TD_CTL_LANG1)
+#define KC_SFT_L2  TD(TD_SFT_LANG2)
+#else
+#define KC_GK1     KC_1
+#define KC_GK2     KC_2
+#define KC_GK3     KC_3
+#define KC_GK4     KC_4
+#define KC_GK5     KC_5
+#define KC_GK6     KC_6
+#define KC_GK7     KC_7
+#define KC_GK8     KC_8
+#define KC_GK9     KC_9
+#define KC_GK0_RA  LT(RAISE, KC_0)
+#define KC_CTL_L1  LCTL_T(KC_LANG1)
+#define KC_SFT_L2  LSFT_T(KC_LANG2)
+#endif
+
+#define KC_RST  RESET
+#define KC_MUP  KC_MS_U
+#define KC_MDN  KC_MS_D
+#define KC_MLFT KC_MS_L
+#define KC_MRGT KC_MS_R
+#define KC_WUP  KC_WH_U
+#define KC_WDN  KC_WH_D
+#define KC_WLFT KC_WH_L
+#define KC_WRGT KC_WH_R
+#define KC_ZNHN A(KC_ZKHK)
+
+/* KEYMAPS */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT_kc( \
-  //,-----------------------------------------. ,-----------------------------------------.
-        ESC,     Q,     W,     E,     R,     T,       Y,     U,     I,     O,     P,  BSPC,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-      CTLTB,     A,     S,     D,     F,     G,       H,     J,     K,     L,  SCLN,  QUOT,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-       LSFT,     Z,     X,     C,     V,     B,       N,     M,  COMM,   DOT,  SLSH,  RSFT,\
-  //`------+------+------+------+------+------| |------+------+------+------+------+------'
-                           GUIEI, LOWER,   SPC,     ENT, RAISE, ALTKN \
-                       //`--------------------' `--------------------'
-  ),
 
-  [_LOWER] = LAYOUT_kc( \
-  //,-----------------------------------------. ,-----------------------------------------.
-        ESC,     1,     2,     3,     4,     5,      6,     7,     8,     9,     0,  BSPC,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-      CTLTB,    F1,    F2,    F3,    F4,    F5,     F6,    F7,    F8,    F9,   F10, XXXXX,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-       LSFT,   F11,   F12,   F13,   F14,   F15,     F16,   F17,   F18,   F19,   F20, XXXXX,\
-  //`------+------+------+------+------+------| |------+------+------+------+------+------'
-                           GUIEI, LOWER,   SPC,     ENT, RAISE, ALTKN \
-                       //`--------------------' `--------------------'
-  ),
+[BASE] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    JYEN , Q    , W    , E    , R    , T    ,   Y    , U    , I    , O    , P    , MINS , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    LCTL , A    , S    ,D_MOUS, F    , G    ,   H    , J    , K    , L    , SCLN , QUOT , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    LSFT , Z    , X    , C    , V    , B    ,   N    , M    , COMM , DOT  , SLSH , BSLS , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                        L2_ALT, SPC  ,ESC_FN,   TAB  , ENT  ,L1_RAI \
+//                     `--------------------' `--------------------'
+),
 
-  [_RAISE] = LAYOUT_kc( \
-  //,-----------------------------------------. ,-----------------------------------------.
-        ESC,  EXLM,    AT,  HASH,   DLR,  PERC,    CIRC,  AMPR,  ASTR,  LPRN,  RPRN,  BSPC,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-      CTLTB, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,    MINS,   EQL,  LCBR,  RCBR,  PIPE,   GRV,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-       LSFT, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,    UNDS,  PLUS,  LBRC,  RBRC,  BSLS,  TILD,\
-  //`------+------+------+------+------+------| |------+------+------+------+------+------'
-                           GUIEI, LOWER,   SPC,     ENT, RAISE, ALTKN \
-                       //`--------------------' `--------------------'
-  ),
+[RAISE] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    ZNHN , 1    , 2    , 3    , 4    , 5    ,   6    , 7    , 8    , 9    , 0    , EQL  , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , LCBR , LPRN , LBRC ,   RBRC , RPRN , RCBR , ____ , UP   , PLUS , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , EXLM , AT   , HASH , DLR  , PERC ,   CIRC , AMPR , ASTR , LEFT , DOWN , RGHT , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                         ____ , BSPC , RST  ,  GARAKE, ____ , XXXX  \
+//                     `--------------------' `--------------------'
+),
 
-  [_ADJUST] = LAYOUT_kc( \
-  //,-----------------------------------------. ,-----------------------------------------.
-        RST,  LRST, XXXXX, XXXXX, XXXXX, XXXXX,   XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-       LTOG,  LHUI,  LSAI,  LVAI, XXXXX, XXXXX,   XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-  //|------+------+------+------+------+------| |------+------+------+------+------+------|
-       LMOD,  LHUD,  LSAD,  LVAD, XXXXX, XXXXX,   XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
-  //`------+------+------+------+------+------| |------+------+------+------+------+------'
-                           GUIEI, LOWER,   SPC,     ENT, RAISE, ALTKN \
-                       //`--------------------' `--------------------'
-  )
+[FUNCTION] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    ____ , F1   , F2   , F3   , F4   , F5   ,   F6   , F7   , F8   , F9   , PSCR , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , LGUI ,   ____ , ____ , ____ , ____ , PGUP , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ____ , ____ , ____ , HOME , PGDN , END , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                         ____ , ____ , XXXX ,   ____ , ____ , ____  \
+//                     `--------------------' `--------------------'
+),
+
+[GARAKE] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ESC  , GK7  , GK8  , GK9  , BSPC , MINS , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,  CTL_TB, GK4  , GK5  , GK6  , SCLN , QUOT , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,   LSFT , GK1  , GK2  , GK3  , DOT  , SLSH , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                         ____ , ____ , BASE ,   SPC  ,ALT_EN,GK0_RA\
+//                     `--------------------' `--------------------'
+),
+
+[GARARAISE] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    ____ , ____ , ____ , ____ , ____ , ____ ,  TENKEY, BTN1 , MUP  , BTN2 , ZNHN , EQL  , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,  CTL_L1, MLFT , MDN  , MRGT , UP   , PLUS , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,  SFT_L2, WUP  , WDN  , LEFT , DOWN , RGHT , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                         ____ , ____ , ____ ,   ____ , ____ , XXXX  \
+//                     `--------------------' `--------------------'
+),
+
+[TENKEY] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ____ , 7    , 8    , 9    , ____ , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ____ , 4    , 5    , 6    , ____ , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ____ , 1    , 2    , 3    , ____ , ____ , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                         ____ , ____ , ____ ,   ____ , 0    , ____ \
+//                     `--------------------' `--------------------'
+),
+
+[MOUSE] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ____ , ____ , ____ , ____ , ____ , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , WEEL , ____ , ACL0 , ____ ,   MLFT , MDN  , MUP  , MRGT , ____ , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , BTN1 , BTN2 ,   ____ , ____ , ____ , ____ , ____ , ____ , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                         ____ , ____ , ____ ,   ____ , ____ , ____  \
+//                     `--------------------' `--------------------'
+),
+
+[WHEEL] = LAYOUT_kc( \
+//,-----------------------------------------. ,-----------------------------------------.
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ____ , ____ , ____ , ____ , ____ , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,   WRGT , WUP  , WDN  , WLFT , ____ , ____ , \
+//|------+------+------+------+------+------| |------+------+------+------+------+------|
+    ____ , ____ , ____ , ____ , ____ , ____ ,   ____ , ____ , ____ , ____ , ____ , ____ , \
+//`------+------+------+------+------+------| |------+------+------+------+------+------'
+                         ____ , ____ , ____ ,   ____ , ____ , ____  \
+//                     `--------------------' `--------------------'
+),
+
 };
 
-int RGB_current_mode;
+/* USER TASKS */
 
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
-
-void matrix_init_user(void) {
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
+void rgblight_task_user (void) {
+  #ifdef RGBLIGHT_ENABLE
+    rgb_update(false);
+  #endif
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-        break;
-    case RGB_MOD:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          rgblight_mode(RGB_current_mode);
-          rgblight_step();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
-      return false;
-      break;
-    case RGBRST:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgblight_default();
-          rgblight_enable();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
-      break;
-  }
-  return true;
+  #ifdef RGBLIGHT_ENABLE
+    rgb_process_record(keycode, record);
+  #endif
+    return true;
 }
