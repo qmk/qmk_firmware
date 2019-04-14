847d2deb45ed5f7df2144d840fb6dcf119d15c69
Jeremy Bernhardt <jeremythegeek@gmail.com>
2019-04-14 12:01:52 -0600

    [Keyboard] Georgi FW updates (#5609)
    * Working on chording
    
    * Working on chording
    
    * Got layouts in order
    
    * Initial Georgi support
    
    * forgot to add keymaps
    
    * Updated readme
    
    * Update keyboards/georgi/keymaps/template/readme.md
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/georgi.h
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/keymaps/default/keymap.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/keymaps/default/keymap.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/rules.mk
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/rules.mk
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/matrix.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/georgi.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/georgi.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/rules.mk
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/keymaps/default/keymap.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/keymaps/template/keymap.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/matrix.c
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Disabled features, updated info
    
    * Update keyboards/georgi/config.h
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Update keyboards/georgi/config.h
    
    Co-Authored-By: germ <jeremythegeek@gmail.com>
    
    * Fixed info.json
    
    * Split the number button and fixed gaming mode.
    
    * started work on history feature
    
    * Working history/multikeyfuckery
    
    * type
    
    * inital code reduction refactor
    
    * Got multikey patched up, optimizing for size
    
    * Forgot to remove stuff
    
    * fixed key repeat
    
    * Key repeat added.
    
    * Symshift locking
    
    * Midchord Sym shenanigans.
    
    * Added only QWERTY mode
    
    * Split out header
    
    * Added stickybits, minimal layour
    
    * Fixing user layout
    
    * Whitespace fixing
    
    * Fixing Version name

--- keyboards/georgi/keymaps/norman/rules.mk
+++ keyboards/georgi/keymaps/norman/rules.mk
@@ -1,18 +1,20 @@
 #----------------------------------------------------------------------------
-# make georgi:extrakey:dfu
+# make georgi:default:dfu
 # Make sure you have dfu-programmer installed!
 #----------------------------------------------------------------------------
 
-#Debug options
+NO_REPEAT				 = yes
 VERBOSE					 = yes
-CONSOLE_ENABLE			 = yes
-DEBUG_MATRIX_SCAN_RATE   = no
-DEBUG_MATRIX			 = no
 KEYBOARD_SHARED_EP       = yes
 CUSTOM_MATRIX			 = yes
-MOUSEKEY_ENABLE			 = no
-SINGLE_LAYER			 = no
+MOUSEKEY_ENABLE			 = yes
 
+#Debug options
+CONSOLE_ENABLE			 = no
+DEBUG_MATRIX_SCAN_RATE   = no
+DEBUG_MATRIX			 = no
+NO_PRINT				 = yes
+ONLY_QWERTY				 = no
 
 # A bunch of stuff that you shouldn't touch unless you
 # know what you're doing.
@@ -22,6 +24,12 @@ SRC += matrix.c i2c_master.c
 ifeq ($(strip $(DEBUG_MATRIX)), yes)
     OPT_DEFS += -DDEBUG_MATRIX
 endif
-ifeq ($(strip $(SINGLE_LAYER)), yes)
-    OPT_DEFS += -DSINGLE_LAYER
+ifeq ($(strip $(NO_REPEAT)), yes)
+    OPT_DEFS += -DNO_REPEAT
+endif
+ifeq ($(strip $(NO_PRINT)), yes)
+    OPT_DEFS += -DNO_PRINT -DNO_DEBUG
+endif
+ifeq ($(strip $(ONLY_QWERTY)), yes)
+    OPT_DEFS += -DONLYQWERTY
 endif
