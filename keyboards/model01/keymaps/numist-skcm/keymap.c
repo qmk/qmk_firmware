4a194c0f10d54ccb8bf43199998d315caa5e96c0
Scott Perry <dev@numist.net>
2019-04-13 22:26:37 -0700

    Add steno layer and dynamic macro bindings--both work great!

--- keyboards/model01/keymaps/numist-skcm/keymap.c
+++ keyboards/model01/keymaps/numist-skcm/keymap.c
@@ -14,6 +14,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "keymap_steno.h"
+
+enum keycodes {
+  NONE = SAFE_RANGE,
+  DYNAMIC_MACRO_RANGE
+};
+
+#include "dynamic_macro.h"
+
+void matrix_init_user() {
+  steno_set_mode(STENO_MODE_GEMINI); // or STENO_MODE_BOLT
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_record_dynamic_macro(keycode, record)) {
+    return false;
+  }
+  return true;
+}
 
 enum macro_keycodes {
   KC_M_FIND,
@@ -59,6 +78,7 @@ enum {
   DEF = 0,
   NUM,
   FUN,
+  PLV,
 };
 
 #define M_FIND     M(KC_M_FIND)
@@ -68,9 +88,13 @@ enum {
 #define M_COPY     M(KC_M_COPY)
 #define M_PSTE     M(KC_M_PASTE)
 
+#define M_RECD1  DYN_REC_START1
+#define M_STOP1 DYN_REC_STOP
+#define M_PLAY1 DYN_MACRO_PLAY1
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [DEF] = LAYOUT(
-  KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , MO(NUM),
+  TG(PLV), KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , MO(NUM),
   KC_GRV , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LBRC,    KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_EQL ,
   KC_PGUP, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_TAB ,    KC_ENT , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
   KC_PGDN, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ESC ,    KC_BSLS, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,
@@ -93,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 [FUN] = LAYOUT(
   RESET  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-  _______, _______, _______, _______, _______, _______, _______,    KC_MPRV, KC_MNXT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_F12 ,
+  _______, M_RECD1, M_STOP1, M_PLAY1, _______, _______, _______,    KC_MPRV, KC_MNXT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_F12 ,
   KC_HOME, M_CUT  , M_COPY , M_PSTE , M_FIND , M_AGAIN, _______,    KC_MPLY, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
   KC_END , _______, _______, _______, _______, _______, M_UNDO ,    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_BSLS, KC_PIPE,
                                          _______,                                 _______,
@@ -101,6 +125,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______,                     _______,
                                                   _______,               KC_DEL ,
                                       _______,                                        _______
+  ),
+[PLV] = LAYOUT(
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  STN_N1 , STN_N2 , STN_N3 , STN_N4 , STN_N5 , STN_N6 , XXXXXXX,    XXXXXXX, STN_N7 , STN_N8 , STN_N9 , STN_NA , STN_NB , STN_NC ,
+  XXXXXXX, STN_S1 , STN_TL , STN_PL , STN_HL , STN_ST1, XXXXXXX,    XXXXXXX, STN_ST3, STN_FR , STN_PR , STN_LR , STN_TR , STN_DR ,
+  XXXXXXX, STN_S2 , STN_KL , STN_WL , STN_RL , STN_ST2, XXXXXXX,    XXXXXXX, STN_ST4, STN_RR , STN_BR , STN_GR , STN_SR , STN_ZR,
+                                         XXXXXXX,                                 XXXXXXX,
+                                            STN_A  ,                           STN_U  ,
+                                               STN_O  ,                     STN_E  ,
+                                                  XXXXXXX,               XXXXXXX,
+                                      XXXXXXX,                                        XXXXXXX
   )
 };
 
--- keyboards/model01/keymaps/numist-skcm/rules.mk
+++ keyboards/model01/keymaps/numist-skcm/rules.mk
@@ -1,5 +1,6 @@
 # used by default keymap
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no
 RGB_MATRIX_ENABLE = no
 NKRO_ENABLE = yes
 COMMAND_ENABLE = yes
+STENO_ENABLE = yes
