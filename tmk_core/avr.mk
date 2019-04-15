b18f2d8f2f64547563d1f5543d11e835f7a30368
mtei <2170248+mtei@users.noreply.github.com>
2019-04-16 00:32:34 +0900

    fix LIB_SRC and QUANTUM_LIB_SRC for ARM(chibios)

--- tmk_core/avr.mk
+++ tmk_core/avr.mk
@@ -6,13 +6,15 @@ CC = avr-gcc
 OBJCOPY = avr-objcopy
 OBJDUMP = avr-objdump
 SIZE = avr-size
-AR = avr-ar rcs
+AR = avr-ar
 NM = avr-nm
 HEX = $(OBJCOPY) -O $(FORMAT) -R .eeprom -R .fuse -R .lock -R .signature
 EEP = $(OBJCOPY) -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 --no-change-warnings -O $(FORMAT)
 BIN =
 
 COMMON_VPATH += $(DRIVER_PATH)/avr
+
+ARFLAGS =  rcs
 
 COMPILEFLAGS += -funsigned-char
 COMPILEFLAGS += -funsigned-bitfields
