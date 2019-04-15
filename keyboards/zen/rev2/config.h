8ff9e3c355354d39711ff9a015320be6fbce08dd
XScorpion2 <rcalt2vt@gmail.com>
2019-04-15 00:29:50 -0400

    [Keyboard] Zen keyboard update for Rev2 (#5522)
    * Updated Zen keyboard for rev2 support
    
    * Fixing r1 compile errors
    
    * PR feedback and changes for Proton-C compile errors

--- keyboards/zen/rev2/config.h
+++ keyboards/zen/rev2/config.h
@@ -19,25 +19,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x3060
-#define DEVICE_VER      0x0001
+#define PRODUCT_ID      0x3061
+#define DEVICE_VER      0x0002
 #define MANUFACTURER    Legonut
-#define PRODUCT         Project Zen
-#define DESCRIPTION     Split gaming keyboard
+#define PRODUCT         "Project Zen"
+#define DESCRIPTION     "Split gaming keyboard"
 
 /* key matrix size */
 // Rows are doubled-up
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 7
 
 // wiring of each half
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { B2, B3, B1, F7, F6, D4, B6 }
+#ifdef CONVERT_TO_PROTON_C
+  #define MATRIX_ROWS 5
+  #define MATRIX_COLS 7
+#else
+  #define MATRIX_ROWS 10
+  #define MATRIX_COLS 7
+#endif
+
+// Proton-C does pin conversion
+#define MATRIX_ROW_PINS { C6, E6, B5, D7, B4 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B3, B1, B2 }
+
+#define NUMBER_OF_ENCODERS 1
+
+#define ENCODERS_PAD_A  { D4 }
+#define ENCODERS_PAD_B  { D2 }
+
+#define RGB_DI_PIN         B6
+#define SOFT_SERIAL_PIN    D3
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-#define SOFT_SERIAL_PIN   D0
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
@@ -51,9 +64,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_RESYNC_ENABLE
 
 /* ws2812 RGB LED */
-#define RGB_DI_PIN D1
+#define RGBLED_NUM 34    // Number of LEDs
 
-#define RGBLED_NUM 16    // Number of LEDs
+// If using 90 Degree rotation, increase block cout
+#ifdef OLED_ROTATE90
+  #define OLED_DISPLAY_CUSTOM
+  #define OLED_DISPLAY_WIDTH 128
+  #define OLED_DISPLAY_HEIGHT 32
+  #define OLED_MATRIX_SIZE (OLED_DISPLAY_HEIGHT / 8 * OLED_DISPLAY_WIDTH) // 512 (compile time mathed)
+  #define OLED_BLOCK_TYPE uint16_t // Type to use for segmenting the oled display for smart rendering, use unsigned types only
+  #define OLED_BLOCK_COUNT (sizeof(OLED_BLOCK_TYPE) * 8) // 8 (compile time mathed)
+  #define OLED_BLOCK_SIZE (OLED_MATRIX_SIZE / OLED_BLOCK_COUNT) // 32 (compile time mathed)
+  #define OLED_SOURCE_MAP { 0, 8, 16, 24 }
+  #define OLED_TARGET_MAP { 24, 16, 8, 0 }
+#endif
 
 /*
  * Feature disable options
