410d4f76bf3fdc84b69cfc1bd4412e766a110b8d
Rob Rogers <r2d2rogers@gmail.com>
2019-04-16 00:29:16 -0500

    Crkbd RGB Matrix config with WS2812

--- keyboards/crkbd/rev1/config.h
+++ keyboards/crkbd/rev1/config.h
@@ -53,7 +53,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 
+#ifdef RGBLIGHT_ENABLE
 #define RGBLED_NUM 12    // Number of LEDs
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#define RGBLED_NUM 54    // Number of LEDs
+#define DRIVER_LED_TOTAL RGBLED_NUM
+#endif
 
 /*
  * Feature disable options
