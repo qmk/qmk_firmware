bf4266b4becad6f03a78a881873ce63457d20404
ishtob <ishtob@gmail.com>
2019-04-16 17:18:40 -0400

    refactor layer define with enum

--- keyboards/boston_meetup/2019/keymaps/default/keymap.c
+++ keyboards/boston_meetup/2019/keymaps/default/keymap.c
@@ -4,19 +4,19 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
 
-enum preonic_keycodes {
+enum custom_layers {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
+
+enum custom_keycodes {
   BASE = SAFE_RANGE,
   LOWER,
   RAISE,
-};
-
-enum macro_keycodes {
-    KC_DEMOMACRO,
+  KC_DEMOMACRO
 };
 
 // Custom macros
