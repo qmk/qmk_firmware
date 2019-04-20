32513ce820a78f9eb2ad9a1f1fdd19458261c227
zvecr <git@zvecr.com>
2019-04-20 14:05:23 +0100

    Add skip linting logic

--- util/travis_analysis.sh
+++ util/travis_analysis.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+TRAVIS_COMMIT_MESSAGE="${TRAVIS_COMMIT_MESSAGE:-none}"
+TRAVIS_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE:-HEAD~1..HEAD}"
+
+# test force push
+#TRAVIS_COMMIT_RANGE="c287f1bfc5c8...81f62atc4c1d"
+
+CHANGED_FILES=$(git diff --name-only -n 1 ${TRAVIS_COMMIT_RANGE})
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[skip lint]"* ]]; then
+    echo "Skipping due to commit message"
+    exit 0
+fi
+
 # Lint markdown - output is only warnings till code base is up to scratch
 echo "Linting markdown..."
 markdownlint **/*.md --ignore='tmk_core/*' | true
