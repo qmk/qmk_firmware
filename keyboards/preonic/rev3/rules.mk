2f5dbfca634c725085c17293ced3b992ab4e0e3f
noroadsleft <xxiinophobia@yahoo.com>
2019-04-19 17:32:40 -0700

    Preonic: Concatenate duplicate rules
    Concatenate rules that are set and then overridden into one setting.

--- keyboards/preonic/rev3/rules.mk
+++ keyboards/preonic/rev3/rules.mk
@@ -1,8 +1,10 @@
 
 
-# MCU name
-#MCU = at90usb1287
-MCU = atmega32u4
+# project specific files
+SRC = matrix.c
+
+# Cortex version
+MCU  = STM32F303
 
 # Processor frequency.
 #     This will define a symbol, F_CPU, in all source code files equal to the
@@ -54,13 +56,14 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #   change to "no" to disable the options, or define them in the Makefile in 
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
+BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration(+1000)
+## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
+MOUSEKEY_ENABLE = yes      # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes         # Console for debug(+400)
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+COMMAND_ENABLE = yes       # Commands for debug and configuration
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = yes           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
@@ -70,29 +73,10 @@ API_SYSEX_ENABLE = no
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+#SLEEP_LED_ENABLE = yes
 
-LAYOUTS = ortho_5x12
-# project specific files
-SRC = matrix.c
-LAYOUTS += ortho_5x12
-
-# Cortex version
-MCU  = STM32F303
-
-# Build Options
-#   comment out to disable the options.
-#
-BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration
-## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
-MOUSEKEY_ENABLE = yes # Mouse keys
-EXTRAKEY_ENABLE = yes # Audio control and System control
-CONSOLE_ENABLE = yes  # Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
-#SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes     # USB Nkey Rollover
 CUSTOM_MATRIX = yes # Custom matrix file
-AUDIO_ENABLE = yes
-RGBLIGHT_ENABLE = no
 # SERIAL_LINK_ENABLE = yes
 ENCODER_ENABLE = yes
+
+LAYOUTS = ortho_5x12
