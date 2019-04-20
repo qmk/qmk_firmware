caa03e234de53d615acd8a8e200d14db65e65f44
Drashna Jaelre <drashna@live.com>
2019-04-20 00:11:35 -0700

    Re-alight startup animation to use new HUE range

--- users/drashna/rgb_stuff.c
+++ users/drashna/rgb_stuff.c
@@ -250,15 +250,13 @@ bool process_record_user_rgb(uint16_t keycode, keyrecord_t *record) {
 
 void keyboard_post_init_rgb(void) {
 #if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_STARTUP_ANIMATION)
-    if (userspace_config.rgb_layer_change) {
-        rgblight_enable_noeeprom();
-    }
+    if (userspace_config.rgb_layer_change) { rgblight_enable_noeeprom(); }
     if (rgblight_config.enable) {
         layer_state_set_user(layer_state);
         uint16_t old_hue = rgblight_config.hue;
         rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-        for (uint16_t i = 360; i > 0; i--) {
-            rgblight_sethsv_noeeprom( ( i + old_hue) % 360, 255, 255);
+        for (uint16_t i = 255; i > 0; i--) {
+            rgblight_sethsv_noeeprom( ( i + old_hue) % 255, 255, 255);
             matrix_scan();
             wait_ms(10);
         }
