a3f7179cd8b7dc75f806dca19163bd2def2da5b2
mechmerlin <mechmerlin@gmail.com>
2019-04-21 15:51:48 -0700

    get consistent naming in config file

--- keyboards/doro67/rgb/config.h
+++ keyboards/doro67/rgb/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    BackProp Studios
-#define PRODUCT         Doro 67 RGB 
-#define DESCRIPTION     A custom 65% keyboard with blocker
+#define MANUFACTURER    BackProp Studio
+#define PRODUCT         Doro67 RGB PCB
+#define DESCRIPTION     65% custom board
 
 /* key matrix size */
 #define MATRIX_ROWS 5
