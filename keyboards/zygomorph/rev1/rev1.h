aaa51d17c04bd53e31aa8faa7cd2603af09ed598
Ryan Caltabiano <rcalt2vt@gmail.com>
2019-04-20 21:49:48 -0500

    Removal of old encoder / oled driver, fix for layout macros

--- keyboards/zygomorph/rev1/rev1.h
+++ keyboards/zygomorph/rev1/rev1.h
@@ -21,10 +21,10 @@
     { L20, L21, L22, L23, L24, L25 }, \
     { L30, L31, L32, L33, L34, L35 }, \
     { L40, L41, L42, L43, L44, L45 }, \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
     { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
   }
 
@@ -59,11 +59,11 @@
     { L10, L11, L12, L13, L14, L15 }, \
     { L20, L21, L22, L23, L24, L25 }, \
     { L30, L31, L32, L33, L34, L35 }, \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
     { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
   }
 
@@ -78,7 +78,7 @@
     { L10, L11, L12, L13, L14, L15 }, \
     { L20, L21, L22, L23, L24, L25 }, \
     { L30, L31, L32, L33, L34, L35 }, \
-    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
     { R00, R01, R02, R03, R04, R05, REL }, \
     { R10, R11, R12, R13, R14, R15, RER }, \
     { R20, R21, R22, R23, R24, R25 }, \
