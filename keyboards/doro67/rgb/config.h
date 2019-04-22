d77b917c38d7130a66c79f784042c856c55bbace
mechmerlin <mechmerlin@gmail.com>
2019-04-21 17:41:50 -0700

    fix some inconsistencies

--- keyboards/doro67/rgb/config.h
+++ keyboards/doro67/rgb/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    BackProp Studio
+#define MANUFACTURER    Backprop Studio
 #define PRODUCT         Doro67 RGB PCB
-#define DESCRIPTION     65% custom board
+#define DESCRIPTION     65% custom keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
