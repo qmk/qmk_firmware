c6475cf0648c871dea227cd3a06e741675b04a7c
kit-bto <42862131+kit-bto@users.noreply.github.com>
2019-04-24 13:02:30 +0900

    Update rules.mk

--- keyboards/adkb96/rules.mk
+++ keyboards/adkb96/rules.mk
@@ -1,8 +1,3 @@
-SRC += matrix.c \
-	   i2c.c \
-	   split_util.c \
-	   serial.c \
-
 # MCU name
 #MCU = at90usb1287
 MCU = atmega32u4
@@ -78,6 +73,6 @@ RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-CUSTOM_MATRIX = yes
+SPLIT_KEYBOARD = yes
 
 DEFAULT_FOLDER = adkb96/rev1
