f1fabf9fba9af3beef3ced2fa3e904095d3dc3c9
kit-bto <42862131+kit-bto@users.noreply.github.com>
2019-04-24 11:40:12 +0900

    Update config.h

--- keyboards/adkb96/rev1/config.h
+++ keyboards/adkb96/rev1/config.h
@@ -16,10 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
-
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x00a5
 #define PRODUCT_ID      0xad96
