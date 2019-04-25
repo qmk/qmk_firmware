6475cc01e9bb6f8b5f0f5433c95771c4fda168a9
Drashna Jaelre <drashna@live.com>
2019-04-25 11:02:25 +0900

    Update keyboards/adkb96/rev1/config.h
    Co-Authored-By: kit-bto <42862131+kit-bto@users.noreply.github.com>

--- keyboards/adkb96/rev1/config.h
+++ keyboards/adkb96/rev1/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5, F4 }
 
-#define SPLIT_HAND_PIN D0
+#define SOFT_SERIAL_PIN D0
 
 /* define tapping term */
 #define TAPPING_TERM 100
