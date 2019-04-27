e8f31cd88f05ff706aa56dcb40516ac5148ce8e3
Drashna Jaelre <drashna@live.com>
2019-04-27 12:19:51 -0700

    Scrolling OLED key logger!

--- keyboards/crkbd/keymaps/drashna/keymap.c
+++ keyboards/crkbd/keymaps/drashna/keymap.c
@@ -133,8 +133,10 @@ void render_crkbd_logo(void) {
   oled_write_P(crkbd_logo, false);
 }
 
-char keylogs_str[6] = {};
-int keylogs_str_idx = 0;
+#define KEYLOG_LEN (int)(32 / OLED_FONT_WIDTH)
+char keylog_str[KEYLOG_LEN] = {};
+uint8_t keylogs_str_idx = 0;
+uint16_t log_timer = 0;
 
 const char code_to_name[60] = {
     ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
@@ -144,30 +146,28 @@ const char code_to_name[60] = {
     'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
     '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
 
-void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
-  if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) || (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-  if (keycode < 60) {
-    name = code_to_name[keycode];
-  } else { return; }
+void add_keylog(uint16_t keycode) {
+    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
+        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
 
-
-  // update keylogs
-  if (keylogs_str_idx == sizeof(keylogs_str) - 1) {
-    keylogs_str_idx = 0;
-    for (int i = 0; i < sizeof(keylogs_str) - 1; i++) {
-      keylogs_str[i] = ' ';
+    for (uint8_t i = KEYLOG_LEN - 1; i > 0; i--) {
+        keylog_str[i] = keylog_str[i - 1];
     }
-  }
+    if (keycode < 60) {
+        keylog_str[0] = code_to_name[keycode];
+    }
+    log_timer = timer_read();
+}
 
-  keylogs_str[keylogs_str_idx] = name;
-  keylogs_str_idx++;
+void update_log(void) {
+    if (timer_elapsed(log_timer) > 750) {
+        add_keylog(0);
+    }
 }
 
 
-
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) { set_keylog(keycode, record); }
+    if (record->event.pressed) {  add_keylog(keycode); }
     return true;
 }
 
@@ -243,7 +243,7 @@ void render_status(void) {
   oled_write_P(led_usb_state & (1<<USB_LED_CAPS_LOCK)   ? PSTR(" CAPS") : PSTR("     "), false);
   oled_write_P(led_usb_state & (1<<USB_LED_SCROLL_LOCK) ? PSTR(" SCRL") : PSTR("     "), false);
 
-  oled_write(keylogs_str, false);
+  oled_write(keylog_str, false);
 }
 
 
@@ -255,6 +255,8 @@ void oled_task_user(void) {
     oled_scroll_left();  // Turns on scrolling
   }
 }
+
+void matrix_scan_keymap(void) { update_log(); }
 #endif
 
 uint16_t get_tapping_term(uint16_t keycode) {
