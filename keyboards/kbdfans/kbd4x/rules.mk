aca1b250b60a0b181505e728961afedbb3a1a2f1
newtonapple <david.github@gmail.com>
2019-04-29 00:34:05 -0700

    Turn off console for KBD4X so firmware size falls within limit.

--- keyboards/kbdfans/kbd4x/rules.mk
+++ keyboards/kbdfans/kbd4x/rules.mk
@@ -65,7 +65,7 @@ BOOTLOADER = atmel-dfu
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = yes        # Console for debug(+400)
+CONSOLE_ENABLE = no        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
