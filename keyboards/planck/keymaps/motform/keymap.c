48b5465d214a685d75f5e68fa986d6ef811bfde4
motform <love.lagerkvist@protonmail.com>
2019-04-30 10:27:57 +0200

    Add readme and remove unused enums (thanks @drasha!)

--- keyboards/planck/keymaps/motform/keymap.c
+++ keyboards/planck/keymaps/motform/keymap.c
@@ -14,15 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-/* ABOUT THIS LAYOUT
- * 
- * Implements colemak-se: github.com/motform/colemak-se
- * Optimized for use with Emacs and Evil-mode
- * Only vetted for the MacOS implementation of Swedish QWERTY
- * Don’t forget to turn of dead keys!
- */
-
 #include QMK_KEYBOARD_H
 #include "muse.h"
 #include "keymap_swedish.h"
@@ -30,15 +21,10 @@
 extern keymap_config_t keymap_config;
 
 enum planck_layers {
-  _COLEMAK,
-  _LOWER,
-  _RAISE,
-  _ADJUST
-};
-
-enum planck_keycodes {
-  COLEMAK = SAFE_RANGE,
-  BACKLIT,
+                    _COLEMAK,
+                    _LOWER,
+                    _RAISE,
+                    _ADJUST
 };
 
 #define LOWER MO(_LOWER)
@@ -46,7 +32,6 @@ enum planck_keycodes {
 
 /* These definitions can be removed once keymap_swe is properly vetted against MacOS */
 #define NO_DLR_MAC_V  ALGR(LALT(KC_4))
-#define NO_EUR_MAC_V  ALGR(LSFT(KC_4))
 #define NO_AT_MAC_V   ALGR(LALT(KC_2))
 
 /* Esc when pressed, ctrl when used as a modifier */
@@ -75,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
 * ,-----------------------------------------------------------------------------------.
-* |      | PIPE |   '  |   @  |   €  |   &  | DOWN |      | RIGHT|   `  |   ´  |  ~   |
+* |      | PIPE |   '  |   @  |   &  |      | DOWN |      | RIGHT|   `  |   ´  |  ~   |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
 * |      |   !  |   "  |   #  |   ?  |   %  | LEFT |   /  |   \  |   =  |   +  |  *   |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -86,10 +71,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 [_LOWER] = LAYOUT_planck_grid
 (
- _______, NO_PIPE_MAC, NO_APOS, NO_AT_MAC_V, NO_EUR_MAC_V,  NO_AMPR, KC_DOWN, KC_NO,       KC_RGHT,     NO_GRV,  NO_GRV,  NO_TILD,
- _______, KC_EXLM,     NO_QUO2, KC_HASH,     NO_QUES,       KC_PERC, KC_LEFT, NO_SLSH,     NO_BSLS_MAC, NO_EQL,  KC_PPLS, KC_PAST,
+ _______, NO_PIPE_MAC, NO_APOS, NO_AT_MAC_V, NO_AMPR,     _______, KC_DOWN, KC_NO,       KC_RGHT,     NO_GRV,  NO_GRV,  NO_TILD,
+ _______, KC_EXLM,     NO_QUO2, KC_HASH,     NO_QUES,     KC_PERC, KC_LEFT, NO_SLSH,     NO_BSLS_MAC, NO_EQL,  KC_PPLS, KC_PAST,
  _______, KC_NO,       KC_NO,   NO_LBRC,     NO_LCBR_MAC, KC_NO,   KC_UP,   NO_RCBR_MAC, NO_RBRC,     KC_NO,   KC_NO,   _______,
- _______, _______,     _______, _______,     _______,       _______, KC_DEL,  _______,     KC_MNXT,     KC_VOLD, KC_VOLU, KC_MPLY
+ _______, _______,     _______, _______,     _______,     _______, KC_DEL,  _______,     KC_MNXT,     KC_VOLD, KC_VOLU, KC_MPLY
  ),
 
 /* Raise
@@ -115,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset| Debug|      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Colmak|      |      |      |      |
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TRMon |TRMoff|      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -125,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid
 (
  _______, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______,  _______, _______, _______,
- _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, COLEMAK, _______,  _______, _______, _______,
+ _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______,  _______, _______, _______,
  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
  )
@@ -134,19 +119,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 uint32_t layer_state_set_user(uint32_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case COLEMAK:
-      if (record->event.pressed) {
-        print("mode just switched to colemak and this is a huge string\n");
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-      break;
-  }
-  return true;
 }
 
 bool muse_mode = false;
