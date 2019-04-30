d6936f16d10dca063d56a5ff2979f6b262d505dc
Jeremy Bernhardt <bernhardtjeremy@gmail.com>
2019-04-30 02:34:06 -0600

    Forgot to roll version

--- keyboards/butterstick/config.h
+++ keyboards/butterstick/config.h
@@ -4,7 +4,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define PRODUCT_ID      0x1337
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    g Heavy Industries
 #define PRODUCT         Butter Stick
