4bda0e174c7f8bb66192e66c62ec7e0f62a37a49
fauxpark <fauxpark@gmail.com>
2019-05-03 20:21:45 +0800

    Update keyboards/dztech/dz65rgb/keymaps/default/keymap.c
    Co-Authored-By: moyi4681 <moyi4681@users.noreply.github.com>

--- keyboards/dztech/dz65rgb/keymaps/default/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/default/keymap.c
@@ -19,7 +19,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void rgb_matrix_indicators_user(void)
 {
-	  uint8_t this_led = host_keyboard_leds();
 	    if ( this_led & (1<<USB_LED_CAPS_LOCK))
 	    {
 	      rgb_matrix_set_color(8, 0xFF, 0xFF, 0xFF);
