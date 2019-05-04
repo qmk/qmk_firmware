cd9a061d8cc513ed985e74310faba9bf87203b84
noroadsleft <18669334+noroadsleft@users.noreply.github.com>
2019-05-04 03:48:43 -0400

    Update keyboards/kbdfans/kbd67/rev2/config.h
    Co-Authored-By: Vorror <paul@dgtlrepublic.com>

--- keyboards/kbdfans/kbd67/rev2/config.h
+++ keyboards/kbdfans/kbd67/rev2/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    dztech
+#define MANUFACTURER    KBDFans
 #define PRODUCT         KBD67v2
 #define DESCRIPTION     65% Keyboard
 
