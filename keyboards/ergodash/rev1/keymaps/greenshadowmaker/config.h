eaa1aee6b9c193dce5d9ab87cb1e3fdec227a3fe
omkbd <takuya.electronics@gmail.com>
2019-05-06 08:19:07 +0900

    [Keybooard] Update of Ergodash Keyboard (#5772)
    * Simpler coding
    
    use SPLIT_KEYBOARD
    delete rev1 (rev2→rev1)  because rev1is not sold
    
    * delete USE_I2C = no
    
    * Update readme.md

--- keyboards/ergodash/rev1/keymaps/greenshadowmaker/config.h
+++ keyboards/ergodash/rev1/keymaps/greenshadowmaker/config.h
@@ -31,11 +31,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
-
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 24
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
