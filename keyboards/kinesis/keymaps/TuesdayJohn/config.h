55afeba13253570e4da73443844f9f2bb00a5774
Salt-Peanuts <hajimeokuda@gmail.com>
2019-05-05 19:28:45 -0400

    Update config.h

--- keyboards/kinesis/keymaps/TuesdayJohn/config.h
+++ keyboards/kinesis/keymaps/TuesdayJohn/config.h
@@ -1,13 +1,6 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-
-#include "../../config.h"
+#pragma once
 
 // place overrides here
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 175
 #define TAPPING_TOGGLE 2
-
-
-#endif
