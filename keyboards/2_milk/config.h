d18127befb9f6284a8d92c62ccff63e075c85a3b
Spaceman <rionlion100@gmail.com>
2019-05-05 17:09:30 -0400

    Update config.h

--- keyboards/2_milk/config.h
+++ keyboards/2_milk/config.h
@@ -1,1 +1,12 @@
+/* key matrix size */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 1
 
+/* Milk defualt pinout*/
+#define MATRIX_ROW_PINS { D0, D5 }
+#define MATRIX_COL_PINS { F1 }
+
+
+#ifdef RGBLIGHT_ENABLE
+#define RGB_DI_PIN D3
+#endif
