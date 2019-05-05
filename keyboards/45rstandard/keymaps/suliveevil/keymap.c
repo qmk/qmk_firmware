46836fee90cd8a8f174a67db524276eede1ea531
fauxpark <fauxpark@gmail.com>
2019-05-05 21:05:43 +0800

    Update keyboards/45rstandard/keymaps/suliveevil/keymap.c
    Co-Authored-By: suliveevil <35763237+suliveevil@users.noreply.github.com>

--- keyboards/45rstandard/keymaps/suliveevil/keymap.c
+++ keyboards/45rstandard/keymaps/suliveevil/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	KEYMAP(
+	LAYOUT(
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
 		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT, 
 		KC_LSFT, LT(4, KC_NO), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_SLSH, KC_UP, KC_RCTL, 
