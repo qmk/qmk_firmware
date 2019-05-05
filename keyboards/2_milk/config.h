0f15475c76d6210ac645747d4f6ce18a82d31b7c
Spaceman <rionlion100@gmail.com>
2019-05-05 19:12:34 -0400

    Add files via upload

--- keyboards/2_milk/config.h
+++ keyboards/2_milk/config.h
@@ -1,12 +1,24 @@
+#pragma once
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x1209
+#define PRODUCT_ID      0xB195
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    rionlion100
+#define PRODUCT         2% Milk
+#define DESCRIPTION     A milk themed 2% Keyboard
+
 /* key matrix size */
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 1
 
 /* Milk defualt pinout*/
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1 }
+#define MATRIX_ROW_PINS { B4, B5 }
+#define MATRIX_COL_PINS { D7 }
 
 
 #ifdef RGBLIGHT_ENABLE
-#define RGB_DI_PIN D3
+#define RGB_DI_PIN B6
+#define RGBLED_NUM 1
 #endif
