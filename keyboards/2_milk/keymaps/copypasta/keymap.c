34ca3b8598494a555de8c378fc8b609254922517
Spaceman <rionlion100@gmail.com>
2019-05-05 20:48:05 -0400

    Update keymap.c

--- keyboards/2_milk/keymaps/copypasta/keymap.c
+++ keyboards/2_milk/keymaps/copypasta/keymap.c
@@ -3,7 +3,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [0] = LAYOUT(
-          KC_COPY,
-          KC_PASTE
+          RCTL(KC_C), 
+          RCTL(KC_V)
           )
 };
