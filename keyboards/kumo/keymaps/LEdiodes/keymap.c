bd9f7a3e5626ba75a0ae4b49e9cad3b951a6f7b0
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-05-06 07:37:40 -0500

    Update keyboards/kumo/keymaps/LEdiodes/keymap.c
    Co-Authored-By: LEdiodes <14025111+LEdiodes@users.noreply.github.com>

--- keyboards/kumo/keymaps/LEdiodes/keymap.c
+++ keyboards/kumo/keymaps/LEdiodes/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Ctrl+# | Win+Exp | Alt  |    L3(DEL)  | (L3)SPACE |  L2 | [+( | ]+)  | "+' |
  * `---------------------------------------------------------------------------'
  */
-[L0] = KEYMAP(LT(MO(L4),TD(TD_KC_ESC_F5)), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, \
+[L0] = LAYOUT(LT(MO(L4),TD(TD_KC_ESC_F5)), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, \
               LT(MO(L2), KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, LT(MO(L3),KC_ENT), \
               KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSHIFT, MO(L1), \
               TD(TD_LCTL_HASH), TD(TD_KC_WIN_WIN_E), KC_LALT, KC_NO, LT(MO(L3), KC_DEL), LT(MO(L3), KC_SPC), MO(L2), TD(TD_LBRC_LPRN), TD(TD_RBRC_RPRN), TD(TD_DQT_QUOT) \
