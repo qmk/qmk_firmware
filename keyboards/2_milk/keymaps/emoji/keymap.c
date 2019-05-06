e3fce97ce36deff5d8c2f884cfb292ab65f57a30
Spaceman <rionlion100@gmail.com>
2019-05-05 21:03:44 -0400

    Update keymap.c

--- keyboards/2_milk/keymaps/emoji/keymap.c
+++ keyboards/2_milk/keymaps/emoji/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       [0] = LAYOUT(
-          SEND_STRING("¯\\_(ツ)_/¯"),
-          SEND_STRING("༼ つ ◕◡◕ ༽つ")
+          send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF"),
+          send_unicode_hex_string("0CA0 005F 0CA0")
           )
 };
