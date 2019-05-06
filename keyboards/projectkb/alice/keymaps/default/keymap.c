31b6ab37543efdee92770bd8c2e70d0bc93cd734
Andrew Kannan <andrew.kannan@gmail.com>
2019-05-05 23:44:00 -0400

    remove unused enum

--- keyboards/projectkb/alice/keymaps/default/keymap.c
+++ keyboards/projectkb/alice/keymaps/default/keymap.c
@@ -25,11 +25,6 @@ extern keymap_config_t keymap_config;
 #define _BASE 0
 #define _FN1 1
 
-// enum custom_keycodes {
-//   QWERTY = SAFE_RANGE,
-// };
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_default(
     KC_ESC,  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
