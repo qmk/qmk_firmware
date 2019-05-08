2a4781eca9085b986ecc54e02bfb8e33c51db506
eswai <>
2019-05-08 07:54:48 +0200

    矢印キー

--- keyboards/miniaxe/keymaps/naginata/keymap.c
+++ keyboards/miniaxe/keymaps/naginata/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* 薙刀式
  *
  * ,----------------------------------.           ,----------------------------------.
- * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
+ * |   Q  |   W  |   E  |   R  |      |           |      |   U  |   I  |   O  |   P  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |   ;  |
  * |------+------+------+------+------|           |------+------+------+------+------|
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_NAGINATA] = LAYOUT( \
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+  KC_Q,    KC_W,    KC_E,    KC_R, KC_LEFT,      KC_RGHT,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
                     KC_LGUI, LOWER, NGSHFT,       NGSHFT, RAISE, MT(MOD_LALT, KC_BSPC)  \
