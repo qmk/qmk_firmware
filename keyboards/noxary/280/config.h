b5ef360aa6b6630acb0277514018c983c261029c
mechmerlin <mechmerlin@gmail.com>
2019-05-07 20:30:32 -0700

    define pins used and create an appropriate LAYOUT macro

--- keyboards/noxary/280/config.h
+++ keyboards/noxary/280/config.h
@@ -20,16 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x4E58
+#define PRODUCT_ID      0x0050
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    MechMerlin
-#define PRODUCT         %KEYBOARD%
-#define DESCRIPTION     A custom keyboard
+#define MANUFACTURER    Noxary
+#define PRODUCT         280
+#define DESCRIPTION     A custom TKL
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 9
 
 /*
  * Keyboard Matrix Assignments
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { F0, E6, D6, D4, F6, F5, F4, F1, B2, D3, D2, D1}
+#define MATRIX_COL_PINS { F7, C7, C6, B6, B5, B4, D7, B0, B3}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
@@ -53,9 +53,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
 
-// #define BACKLIGHT_PIN B7
-// #define BACKLIGHT_BREATHING
-// #define BACKLIGHT_LEVELS 3
+#define BACKLIGHT_PIN B7
+#define BACKLIGHT_BREATHING
+#define BACKLIGHT_LEVELS 3
 
 // #define RGB_DI_PIN E2
 // #ifdef RGB_DI_PIN
