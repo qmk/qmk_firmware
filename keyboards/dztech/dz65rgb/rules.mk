dc7b922a81eb418943b891ac882592cbeba62db5
moyi4681 <moyi4681@users.noreply.github.com>
2019-05-09 10:35:54 +0800

    Update rules.mk

--- keyboards/dztech/dz65rgb/rules.mk
+++ keyboards/dztech/dz65rgb/rules.mk
@@ -1,4 +1,6 @@
 MCU  = STM32F303
+DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
+DFU_SUFFIX_ARGS = -p DF11 -v 0483
 BACKLIGHT_ENABLE = no
 BOOTMAGIC_ENABLE = lite           # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes              # Mouse keys
@@ -8,5 +10,4 @@ COMMAND_ENABLE = no                # Commands for debug and configuration
 NKRO_ENABLE = yes                 # USB Nkey Rollover
 AUDIO_ENABLE = no
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
-
 LAYOUTS = 65_ansi
