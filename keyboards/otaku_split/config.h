c43d72349dfaa344d48f107e45ef6fccae6b8964
takashiski <takashiskibb@gmail.com>
2019-05-11 03:38:18 +0900

    update

--- keyboards/otaku_split/config.h
+++ keyboards/otaku_split/config.h
@@ -28,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 10 
+#define MATRIX_COLS 8
 
 /*
  * Keyboard Matrix Assignments
@@ -41,8 +41,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { B5,B4,E6,D7,C6 }
+#define MATRIX_COL_PINS { B6,B2,B3,B1,F7,F6,F5,F4 }
+//#define MATRIX_COL_PINS { F4,F5,F6,F7,B1,B3,B2,B6 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
