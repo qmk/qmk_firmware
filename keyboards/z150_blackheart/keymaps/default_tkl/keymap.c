b263d26efbc7165c9ef84e2ac27ea3fe5f7fdb54
noroadsleft <xxiinophobia@yahoo.com>
2019-05-10 12:29:25 -0700

    Elevate Indicator LED set-up and toggling to keyboard level

--- keyboards/z150_blackheart/keymaps/default_tkl/keymap.c
+++ keyboards/z150_blackheart/keymaps/default_tkl/keymap.c
@@ -45,25 +45,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void led_set_user(uint8_t usb_led) {
-    setPinOutput(B0);
-    setPinOutput(E6);
-    setPinOutput(E7);
-
-    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
-        writePinHigh(E7);
-    } else {
-        writePinLow(E7);
-    }
-
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        writePinHigh(B0);
-    } else {
-        writePinLow(B0);
-    }
-
-    if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
-        writePinHigh(E6);
-    } else {
-        writePinLow(E6);
-    }
 }
