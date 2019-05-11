4f6f1350a574e2e77ef80843cbd5b2744deb6af7
Croktopus <39040552+Croktopus@users.noreply.github.com>
2019-05-11 08:44:12 -0700

    fixed LEDs and keymaps

--- keyboards/handwired/daishi/keymaps/default/keymap.c
+++ keyboards/handwired/daishi/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
   KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,          KC_P4  , KC_P5  , KC_P6  , KC_EQL ,
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          KC_UP  , KC_P1  , KC_P2  , KC_P3  , KC_PENT,
-  KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC ,                            KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0  , KC_PDOT         ,
+  KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC ,                            KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0  , KC_PDOT         
  ),
 
 /* FN
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______,
-  _______, _______,  _______,                  _______,                            _______, _______,          _______, _______, _______, _______, _______         ,
+  _______, _______,  _______,                  _______,                            _______, _______,          _______, _______, _______, _______, _______         
  )
 };
 
@@ -105,18 +105,18 @@ void matrix_init_user(void) {
   setPinOutput(C6);
 }
 
-void led_set_user(uint8_t usb_led) {
-    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+void led_set_kb(uint8_t usb_led) {
+    if (IS_LED_OFF(usb_led, USB_LED_NUM_LOCK)) {
         writePinLow(C4);
     } else {
-        writePinHigh(C44);
+        writePinHigh(C4);
     }
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+    if (IS_LED_OFF(usb_led, USB_LED_CAPS_LOCK)) {
         writePinLow(C5);
     } else {
         writePinHigh(C5);
     }
-    if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
+    if (IS_LED_OFF(usb_led, USB_LED_SCROLL_LOCK)) {
         writePinLow(C6);
     } else {
         writePinHigh(C6);
