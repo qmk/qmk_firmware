da56c6a6dcc9fa38747407aae8b190f538a8f935
Ryan Caltabiano <rcalt2vt@gmail.com>
2019-05-11 08:14:19 -0500

    Updating defaults

--- keyboards/zygomorph/rules.mk
+++ keyboards/zygomorph/rules.mk
@@ -46,7 +46,7 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
