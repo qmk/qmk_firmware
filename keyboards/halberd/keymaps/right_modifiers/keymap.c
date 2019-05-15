05e746fe36ff54935354b11c1cab9d8d560de39f
Katsuhiro Endo <ka2hiro@curlybracket.co.jp>
2019-05-15 19:59:23 +0900

    Remove dead code.

--- keyboards/halberd/keymaps/right_modifiers/keymap.c
+++ keyboards/halberd/keymaps/right_modifiers/keymap.c
@@ -116,7 +116,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         set_single_persistent_default_layer(_QWERTY);
       }
       return false;
-      break;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
@@ -126,7 +125,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
@@ -136,7 +134,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-      break;
     case ADJUST:
       if (record->event.pressed) {
         layer_on(_ADJUST);
@@ -144,7 +141,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_ADJUST);
       }
       return false;
-      break;
   }
   return true;
 }
