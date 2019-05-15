f7de66258d39c309ed122b5d1d61ee7814dc8aef
coseyfannitutti <bryanwilson437@gmail.com>
2019-05-15 08:28:18 -0400

    Update config.h

--- keyboards/coseyfannitutti/mulletpad/config.h
+++ keyboards/coseyfannitutti/mulletpad/config.h
@@ -114,7 +114,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* key combination for magic key command */
-#define IS_COMMAND() ( \
+ //#define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
