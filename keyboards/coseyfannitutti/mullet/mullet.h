da6936790dac367976c3545d6b51b8f97397e51e
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2019-05-15 05:31:31 -0400

    Update keyboards/coseyfannitutti/mullet/mullet.h
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/coseyfannitutti/mullet/mullet.h
+++ keyboards/coseyfannitutti/mullet/mullet.h
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef MULLET_H
+#pragma once
 #define MULLET_H
 
 #include "quantum.h"
