da833a3273be6ca61498858262f788e15167787f
kingwangwong <kingngngco@gmail.com>
2019-05-16 23:32:00 -0500

    edited some more files

--- keyboards/thevankeyboards/minivan/keymaps/king/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/king/keymap.c
@@ -12,7 +12,7 @@ enum layer_names {
 
 
 #define ______ KC_TRNS
-#define SPC_NUM LT(_NL, KC_SPC)
+#define SPC_FUN LT(_FL, KC_SPC)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       /* 0: Main Layer
@@ -23,15 +23,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * │ TAB  │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │ ;   │'       │
    * ├──────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬─────┤
    * │ LSHFT   │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │MO(SL│
-   * ├─────┬───┴─┬───┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
-   * │LCTRL│LGUI │LALT │MO(SL│SPACE(_NL)  │BKSPC           │MO(_F│     │MO(RL    │
-   * └─────┴─────┴─────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
+   * ├────────┬┴─────┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
+   * │LCTRL   │   LGUI │ LALT│SPACE(_NL)  │MO(_NL)         │BKSPC│     │MO(RL    │
+   * └────────┴────────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
    */
     [_ML] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,     KC_P,     KC_ENT,  
-        KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,     KC_SCLN,  KC_QUOT,         
-        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, MO(_SL),     
-        KC_LCTL, KC_LGUI, KC_LALT,         SPC_NUM,        KC_BSPC,        MO(_FL), XXXXXXX, MO(_RL)
+        KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,     KC_ENT,  
+        KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,  KC_QUOT,         
+        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, MO(_SL),     
+        KC_LCTL, KC_LGUI, KC_LALT,         SPC_FUN,        MO(_NL),        KC_BSPC, XXXXXXX, MO(_RL)
     ),
 
 
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * │ CAPS │     │LEFT │DOWN │RIGHT│PGDN │     │ F5  │ F6  │ F7  │ F8  │        │
    * ├──────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬─────┤
    * │         │     │     │ DEL │ INS │     │     │ F9  │ F10 │ F11 │ F12 │     │
-   * ├─────┬───┴─┬───┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
-   * │     │     │     │MO(SL│SPACE(_FL)  │BACKSPACE(_NL)  │     │     │         │
-   * └─────┴─────┴─────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
+   * ├────────┬┴─────┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
+   * │        │        │     │            │                │     │     │         │
+   * └────────┴────────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
    */
     [_FL] = LAYOUT(
         ______,   XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX, KC_F1, KC_F2,  KC_F3,  KC_F4,   ______,  
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * │   `  │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  8  │ 0   │        │
    * ├──────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬─────┤
    * │         │     │     │     │     │     │     │     │     │     │     │     │
-   * ├─────┬───┴─┬───┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
-   * │     │     │     │MO(SL│SPACE(_FL)  │BACKSPACE(_NL)  │     │     │         │
-   * └─────┴─────┴─────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
+   * ├────────┬┴─────┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
+   * │        │        │     │            │                │     │     │         │
+   * └────────┴────────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
    */
     [_NL] = LAYOUT(
         KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  ______,  
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├──────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬─────┤
    * │         │     │     │     │     │     │     │     │     │     │     │     │
    * ├─────┬───┴─┬───┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
-   * │     │     │     │MO(SL│SPACE(_FL)  │BACKSPACE(_NL)  │     │     │         │
+   * │     │     │     │     │            │                │     │     │         │
    * └─────┴─────┴─────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
    */
     [_SL] = LAYOUT(
