560cc96c4d7a7cfb3447bc9dec25c2ce2633bb26
Ryan Caltabiano <rcalt2vt@gmail.com>
2019-05-19 08:34:25 -0500

    Reduce rgb matrix firmware size

--- quantum/rgb_matrix_animations/colorband_sat_anim.h
+++ quantum/rgb_matrix_animations/colorband_sat_anim.h
@@ -2,19 +2,13 @@
 RGB_MATRIX_EFFECT(BAND_SAT)
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-bool BAND_SAT(effect_params_t* params) {
-  RGB_MATRIX_USE_LIMITS(led_min, led_max);
-
-  HSV hsv = { rgb_matrix_config.hue, 0, rgb_matrix_config.val };
-  uint8_t time = scale16by8(g_rgb_counters.tick, rgb_matrix_config.speed / 4);
-  for (uint8_t i = led_min; i < led_max; i++) {
-    RGB_MATRIX_TEST_LED_FLAGS();
+static void BAND_SAT_math(HSV* hsv, uint8_t i, uint8_t time) {
     int16_t s = rgb_matrix_config.sat - abs(scale8(g_led_config.point[i].x, 228) + 28 - time) * 8;
-    hsv.s = s < 0 ? 0 : s;
-    RGB rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-  }
-  return led_max < DRIVER_LED_TOTAL;
+    hsv->s = s < 0 ? 0 : s;
+}
+
+bool BAND_SAT(effect_params_t* params) {
+    return effect_runner_i(params, &BAND_SAT_math);
 }
 
 #endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
