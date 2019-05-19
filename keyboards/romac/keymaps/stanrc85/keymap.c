56e97265d6268491fa4c1bcb2b791e213288d864
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2019-05-18 21:57:37 -0400

    add cycle layer code

--- keyboards/romac/keymaps/stanrc85/keymap.c
+++ keyboards/romac/keymaps/stanrc85/keymap.c
@@ -19,7 +19,9 @@
 
 #define _NUM 0
 #define _NAV 1
-#define _FN1 2
+#define _MED 2
+#define _RGB 3
+#define _FN1 4
 
 enum keys {
   C_LAYR = SAFE_RANGE
@@ -33,29 +35,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	C_LAYR, KC_0, KC_ENT),
 
   [_NAV] = LAYOUT(
-	KC_INS,  KC_PGUP, KC_PGDN,
-	KC_HOME, KC_UP,   KC_END,
+	KC_HOME, KC_INS,  KC_PGUP,
+	KC_END,  KC_UP,   KC_PGDN,
 	KC_LEFT, KC_DOWN, KC_RGHT,
 	C_LAYR,  TD_TWIN, KC_DOT),
 
+  [_MED] = LAYOUT(
+	KC_MUTE, KC_VOLD, KC_VOLU,
+	CA_QUOT, KC_MPLY, CA_SCLN,
+	LCTL(KC_C), LCTL(KC_V), KC_TRNS,
+	C_LAYR,  KC_TRNS, KC_TRNS),
+
+  [_RGB] = LAYOUT(
+	RGB_SAI, RGB_VAI, RGB_HUI,
+	RGB_SAD, RGB_VAD, RGB_HUD,
+	RGB_TOG, RGB_MOD, KC_TRNS,
+	C_LAYR,  KC_TRNS, KC_TRNS),
+
   [_FN1] = LAYOUT(
-	KC_TRNS, KC_HOME, KC_PGUP,
-	KC_TRNS, KC_END,  KC_PGDN,
+	KC_TRNS, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS, RESET,
 	C_LAYR,  KC_LSFT, KC_MAKE)
 };
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+  keypos_t key;
+  uint8_t current_layer;
+  uint8_t next_layer;
+  uint8_t max_layer;
+  max_layer = sizeof(keymaps) / MATRIX_ROWS / MATRIX_COLS / 2;
   switch (keycode) {
-  case C_LAYR: //Cycles layers up to max_layer variable
+  case C_LAYR: //Cycles layers from 0 to max_layer
     if (!record->event.pressed) {
-      keypos_t key;
-      uint8_t current_layer;
-      uint8_t next_layer;
-      uint8_t max_layer = 2;
       current_layer = layer_switch_get_layer(key);
       next_layer = current_layer + 1;
-      if (next_layer <= max_layer) {
+      if (current_layer < max_layer) {
         layer_on(next_layer);
       }
       else {
