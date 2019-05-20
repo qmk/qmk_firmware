11122b239497a8d1357002c6dfddc5542253e2a3
Jonathan Rascher <jon@bcat.name>
2019-05-19 23:19:30 -0500

    Switch Quefrency from flaky I2C back to serial

--- keyboards/keebio/quefrency/keymaps/bcat/config.h
+++ keyboards/keebio/quefrency/keymaps/bcat/config.h
@@ -1,7 +1,18 @@
 #pragma once
 
-/* Use I2C rather than serial communicaiton to reduce latency. */
-#define USE_I2C
+/*
+ * I2C seems to randomly drop keystrokes. Not sure why. It seems a bit like
+ * https://github.com/qmk/qmk_firmware/issues/5037, but that issue is closed,
+ * and our problems happen even with underglow disabled.
+ *
+ * This issue occurs with multiple TRRS cables of different lengths from
+ * different companies, so it's most likely not a cable issue.  It may be that
+ * we are running into issues with long I2C runs, in which case stronger
+ * pull-up resistors might help:
+ * https://hackaday.com/2017/02/08/taking-the-leap-off-board-an-introduction-to-i2c-over-long-wires/.
+ * For now, just don't use I2C.
+ */
+#define USE_SERIAL
 
 /* Turn off RGB lighting when the host goes to sleep. */
 #define RGBLIGHT_SLEEP
