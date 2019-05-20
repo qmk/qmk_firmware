d53797139833a37ef424e966f1fecb7dfd269b45
Michael Pio <mfmayol@up.edu.ph>
2019-05-20 19:32:57 +0800

    Update keyboards/peiorisboards/ixora/keymaps/wntrmln/keymap.c
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/peiorisboards/ixora/keymaps/wntrmln/keymap.c
+++ keyboards/peiorisboards/ixora/keymaps/wntrmln/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [0] = LAYOUT_FULL(
   KC_PSCR, KC_MUTE, LGUI(KC_1),
-  KC_MPRV, KC_MPLY, KC_MNXT),
+  KC_MPRV, KC_MPLY, KC_MNXT)
 };
 
 void matrix_init_user(void) {
