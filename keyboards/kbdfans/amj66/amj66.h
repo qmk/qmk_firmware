2ca6bd8c316865ccae84c8926ce8354a25191abc
noroadsleft <xxiinophobia@yahoo.com>
2019-05-21 13:51:39 -0700

    Add LAYOUT_66_ansi and LAYOUT_66_iso macros
    - include QMK Configurator data
    - enable Community Layout support

--- keyboards/kbdfans/amj66/amj66.h
+++ keyboards/kbdfans/amj66/amj66.h
@@ -45,3 +45,31 @@
     {  k30,  k31,  k32,  k33,  k34,  k35,  k36,  k37,  k38,  k39,  k3A,  k3B,    k3C,    k3D,    k3E,    KC_NO  }, \
     {  k40,  k41,  k42,  k43,  k44,  k45,  k46,  k47,  k48,  k49,  k4A,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO  } \
 }
+
+#define LAYOUT_66_ansi( \
+    k00,      k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,   k0F, \
+    k10,      k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D,   k1E, \
+    k20,      k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B,      k2C, \
+    k30,      k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C,      k3E, \
+    k40, k41, k42,                k44,           k45, k46, k47,      k48, k49, k4A \
+) { \
+    {  k00,  k01,    k02,  k03,    k04,  k05,  k06,  k07,  k08,  k09,  k0A,  k0B,    k0C,    k0D,    KC_NO,  k0F    }, \
+    {  k10,  k11,    k12,  k13,    k14,  k15,  k16,  k17,  k18,  k19,  k1A,  k1B,    k1C,    k1D,    k1E,    KC_NO  }, \
+    {  k20,  k21,    k22,  k23,    k24,  k25,  k26,  k27,  k28,  k29,  k2A,  k2B,    k2C,    KC_NO,  KC_NO,  KC_NO  }, \
+    {  k30,  KC_NO,  k32,  k33,    k34,  k35,  k36,  k37,  k38,  k39,  k3A,  k3B,    k3C,    KC_NO,  k3E,    KC_NO  }, \
+    {  k40,  k41,    k42,  KC_NO,  k44,  k45,  k46,  k47,  k48,  k49,  k4A,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO  } \
+}
+
+#define LAYOUT_66_iso( \
+    k00,      k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,   k0F, \
+    k10,      k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C,        k1E, \
+    k20,      k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k1D, k2C, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C,      k3E, \
+    k40, k41, k42,                k44,           k45, k46, k47,      k48, k49, k4A \
+) { \
+    {  k00,  k01,  k02,  k03,    k04,  k05,  k06,  k07,  k08,  k09,  k0A,  k0B,    k0C,    k0D,    KC_NO,  k0F    }, \
+    {  k10,  k11,  k12,  k13,    k14,  k15,  k16,  k17,  k18,  k19,  k1A,  k1B,    k1C,    k1D,    k1E,    KC_NO  }, \
+    {  k20,  k21,  k22,  k23,    k24,  k25,  k26,  k27,  k28,  k29,  k2A,  k2B,    k2C,    KC_NO,  KC_NO,  KC_NO  }, \
+    {  k30,  k31,  k32,  k33,    k34,  k35,  k36,  k37,  k38,  k39,  k3A,  k3B,    k3C,    KC_NO,  k3E,    KC_NO  }, \
+    {  k40,  k41,  k42,  KC_NO,  k44,  k45,  k46,  k47,  k48,  k49,  k4A,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO  } \
+}
