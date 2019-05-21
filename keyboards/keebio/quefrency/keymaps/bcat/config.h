eff63a1223b7de4c60aeed911a4319bf24148795
Jonathan Rascher <jon@bcat.name>
2019-05-21 18:36:35 -0500

    Lower mouse wheel speed on Quefrency slightly

--- keyboards/keebio/quefrency/keymaps/bcat/config.h
+++ keyboards/keebio/quefrency/keymaps/bcat/config.h
@@ -31,5 +31,5 @@
 /* Lower mouse speed to adjust for reduced MOUSEKEY_INTERVAL. */
 #define MOUSEKEY_MAX_SPEED 7
 #define MOUSEKEY_TIME_TO_MAX 150
-#define MOUSEKEY_WHEEL_MAX_SPEED 4
+#define MOUSEKEY_WHEEL_MAX_SPEED 3
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 150
