7d35025310cd8d9200700c28d0a0dded54051e72
Brandon Schlack <brandonschlack@gmail.com>
2019-05-22 11:39:19 -0700

    [Keymap] Swap Grave,Backslash

--- keyboards/kbdfans/kbd67/hotswap/keymaps/brandonschlack/keymap.c
+++ keyboards/kbdfans/kbd67/hotswap/keymaps/brandonschlack/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ***********/
 /* Base Layer: Mac
  * ,---------------------------------------------------------------.
- * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|Hom|
+ * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  `|  \|Hom|
  * |---------------------------------------------------------------|
  * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|PgU|
  * |---------------------------------------------------------------|
@@ -48,14 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------'
  */
 [LB_MAC] = LAYOUT(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_HOME, \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_HOME, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGUP, \
   HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   KC_END,  \
   KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, MM_FN_M,                   KC_LEFT, KC_DOWN, KC_RGHT),
 /* Function Layer: Mac
  * ,---------------------------------------------------------------.
- * |MKE| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|MSN|LHP|SLP|
+ * |MKE| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|LHP|MSN|SLP|
  * |---------------------------------------------------------------|
  * |     |   |   |   |RST|   |   |   |   |   |F13|F14|F15|  Del|VlU|
  * |---------------------------------------------------------------|
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------'
  */
 [LF_MAC] = LAYOUT(
-  KC_MAKE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  MC_MSSN, MC_LHPD, MC_SLPD, \
+  KC_MAKE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  MC_LHPD, MC_MSSN, MC_SLPD, \
   _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, KC_F13,  KC_F14,  KC_F15,  KC_DEL,           KC_VOLU, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TG_MAGC,          KC_VOLD, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MRWD, KC_MFFD, MC_PLYR, MM_RSFT,                   KC_PGUP, _______, \
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ***********/
 /* Base Layer: Windows
  * ,---------------------------------------------------------------.
- * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|Hom|
+ * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  `|  \|Hom|
  * |---------------------------------------------------------------|
  * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|PgU|
  * |---------------------------------------------------------------|
@@ -90,14 +90,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------'
  */
 [LB_WIN] = LAYOUT(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_HOME, \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_HOME, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGUP, \
   HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   KC_END,  \
   KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MM_FN_W,                   KC_LEFT, KC_DOWN, KC_RGHT),
 /* Function Layer: Windows
  * ,---------------------------------------------------------------.
- * |MKE| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |TSK|LCK|
+ * |MKE| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|TSK|   |LCK|
  * |---------------------------------------------------------------|
  * |     |   |   |   |RST|   |   |   |   |   |PtS|SLk|Pau|  Del|VlU|
  * |---------------------------------------------------------------|
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------'
  */
 [LF_WIN] = LAYOUT(
-  KC_MAKE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, WN_TSKM, WN_LOCK, \
+  KC_MAKE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  WN_TSKM, _______, WN_LOCK, \
   _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL,           KC_VOLU, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TG_MAGC,          KC_VOLD, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MSTP, MM_RSFT,                   KC_PGUP, _______, \
