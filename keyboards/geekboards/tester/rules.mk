428bc83ecd6689b21f420e59d01cd9b8f01c6346
moyi4681 <moyi4681@users.noreply.github.com>
2019-05-23 14:01:13 +0800

    Update keyboards/geekboards/tester/rules.mk
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/geekboards/tester/rules.mk
+++ keyboards/geekboards/tester/rules.mk
@@ -46,7 +46,6 @@ BOOTLOADER = qmk-dfu
 # Do not put the microcontroller into power saving mode
 # when we get USB suspend event. We want it to keep updating
 # backlight effects.
-OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
 
 # Build Options
 #   change yes to no to disable
