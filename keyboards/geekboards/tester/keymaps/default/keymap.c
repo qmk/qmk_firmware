efab70106451b67e67dc9802ed68a3a7bb82c26e
moyi4681 <moyi4681@users.noreply.github.com>
2019-05-23 14:01:36 +0800

    Update keyboards/geekboards/tester/keymaps/default/keymap.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/geekboards/tester/keymaps/default/keymap.c
+++ keyboards/geekboards/tester/keymaps/default/keymap.c
@@ -6,7 +6,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 [1] = LAYOUT( /* Base */
     KC_ESC,  KC_F1,     KC_F2,     KC_F3,
-    KC_F4,   KC_F5,     KC_F6,     KC_F7),
+    KC_F4,   KC_F5,     KC_F6,     KC_F7
+    ),
 };
 
 void matrix_init_user(void) {
