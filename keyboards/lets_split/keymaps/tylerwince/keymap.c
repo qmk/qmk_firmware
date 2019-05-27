b9a278811141a66b4a98680d29c17745d1579f32
Tyler Wince <tyler@wince.dev>
2019-05-27 12:55:27 -0700

    fix layers

--- keyboards/lets_split/keymaps/tylerwince/keymap.c
+++ keyboards/lets_split/keymaps/tylerwince/keymap.c
@@ -2,19 +2,8 @@
 
 extern keymap_config_t keymap_config;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _COLEMAK 1
-#define _DVORAK 2
-#define _LOWER 3
-#define _RAISE 4
-#define _ADJUST 16
-
 enum layer_names {
-  _QWERTY,
+  _QWERTY = 0,
   _COLEMAK,
   _DVORAK,
   _LOWER,
