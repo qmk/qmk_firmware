5918dcd4793f3f3b41ede139c15e8115ac4b9caa
William Sturgiss <william.sturgiss@cordatahealth.com>
2019-05-28 13:15:00 -0400

    added ctrl_esc

--- keyboards/mechmini/v2/keymaps/wsturgiss/keymap.c
+++ keyboards/mechmini/v2/keymaps/wsturgiss/keymap.c
@@ -27,12 +27,14 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_SLASH_BACKSLASH]  = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, KC_BSLS)
 };
 
+#define CTRL_ESC CTL_T(KC_ESC)
+
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[base] = LAYOUT_2u_space_ortho(
 		KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-		KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT,
+		CTRL_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT,
 		KC_LSPO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     TD(1),    TD(2),    TD(3),    KC_RSPC,
 		KC_LCTL,    _______,  KC_LALT,  KC_LGUI,  MO(1),        KC_SPC,         MO(2),    KC_VOLD,  KC_MPLY,  KC_VOLU,  KC_GRV),
 
