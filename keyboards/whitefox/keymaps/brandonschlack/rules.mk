252c56baeb7de664bdaad344819bf473a546b67c
Brandon Schlack <brandonschlack@gmail.com>
2019-05-28 10:41:07 -0700

    [Keymap] Remove whitefox keymap

--- keyboards/whitefox/keymaps/brandonschlack/rules.mk
+++
@@ -1,8 +0,0 @@
-BOOTMAGIC_ENABLE  = yes
-COMMAND_ENABLE    = yes # Commands for debug and configuration
-CONSOLE_ENABLE    = yes
-EXTRAKEY_ENABLE   = yes # Audio control and System control
-MOUSEKEY_ENABLE   = yes # Mouse keys
-NKRO_ENABLE       = yes # USB Nkey Rollover
-BACKLIGHT_ENABLE  = no  # No backlights installed
-VISUALIZER_ENABLE = no  # No backlights installed
