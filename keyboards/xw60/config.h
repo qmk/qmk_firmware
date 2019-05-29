42f18f1054e51d327f8d0caa59db812c60b46eef
itsdrdick <itsdrdick@gmail.com>
2019-05-29 18:53:29 +0800

    Update keyboards/xw60/config.h
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/xw60/config.h
+++ keyboards/xw60/config.h
@@ -22,7 +22,7 @@
 #define DIODE_DIRECTION COL2ROW
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
