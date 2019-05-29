65677ccf9499e16542b7671d34a1b0ce93c1b4f3
Mack Woodie <j.mack.woodie@gmail.com>
2019-05-28 20:12:50 -0400

    Update keymap.c

--- keyboards/planck/keymaps/_MackNew/keymap.c
+++ keyboards/planck/keymaps/_MackNew/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-  KC_TILD,                    ,                    ,        ,        ,        ,        ,        , KC_AMPR, KC_ASTR,        ,  KC_DEL,
+  KC_TILD,             _______,             _______, _______, _______, _______, _______, _______, KC_AMPR, KC_ASTR, _______,  KC_DEL,
   _______, LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RGHT)), _______, _______, KC_PGUP, KC_HOME, KC_UNDS,  KC_DLR, KC_PERC, KC_CIRC, KC_PIPE,
   _______,             _______,             _______, _______, _______, KC_PGDN,  KC_END,  KC_EQL, KC_EXLM,  KC_AT,  KC_HASH, _______,
   _______,             _______,             _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-  KC_GRV,         ,        ,        ,        , KC_LPRN, KC_RPRN, KC_PLUS,    KC_7,    KC_8,    KC_9, KC_BSPC,
-  _______,        ,        ,        ,        , KC_LCBR, KC_RCBR, KC_MINS,    KC_4,    KC_5,    KC_6, KC_BSLS,
-  _______,        ,        ,        ,        , KC_LBRC, KC_RBRC,  KC_EQL,    KC_1,    KC_2,    KC_3, _______,
+  KC_GRV,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_PLUS,    KC_7,    KC_8,    KC_9, KC_BSPC,
+  _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_MINS,    KC_4,    KC_5,    KC_6, KC_BSLS,
+  _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC,  KC_EQL,    KC_1,    KC_2,    KC_3, _______,
   _______, _______, _______, _______, _______, _______, _______, _______,    KC_0,  KC_DOT, KC_SLSH, KC_ASTR
 ),
 
@@ -103,10 +103,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid(
   _______,   RESET, _______, _______, _______, _______, _______, _______,  _______, _______, KC_PSCR, KC_DEL ,
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______,  _______, _______, _______, _______,
-  _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,         ,        ,        , _______,
-  _______, _______, _______, _______, _______, _______, _______, _______,         ,        ,        , _______
+  _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______
 )
-
 
 };
 
