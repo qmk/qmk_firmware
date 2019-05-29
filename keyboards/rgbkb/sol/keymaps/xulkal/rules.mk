6f4e42b06cc042d0bc49ef51ce70791b030d8fa0
Ryan Caltabiano <rcalt2vt@gmail.com>
2019-05-28 19:46:23 -0500

    Updated LED flags and fixed my keymap

--- keyboards/rgbkb/sol/keymaps/xulkal/rules.mk
+++ keyboards/rgbkb/sol/keymaps/xulkal/rules.mk
@@ -15,29 +15,5 @@ OLED_DRIVER_ENABLE      = yes       # Enable the OLED Driver
 
 # Do not edit past here
 
-ifeq ($(strip $(IOS_DEVICE_ENABLE)), yes)
-    OPT_DEFS += -DIOS_DEVICE_ENABLE
-else ifeq ($(strip $(RGBLIGHT_FULL_POWER)), yes)
-    OPT_DEFS += -DRGBLIGHT_FULL_POWER
-endif
-
-ifeq ($(strip $(RGB_MATRIX_KEYPRESSES)), yes)
-    OPT_DEFS += -DRGB_MATRIX_KEYPRESSES
-endif
-
-ifeq ($(strip $(RGB_MATRIX_FRAMEBUFFER)), yes)
-    OPT_DEFS += -RGB_MATRIX_FRAMEBUFFER_EFFECTS
-endif
-
-ifeq ($(strip $(LED_MIRRORED)), yes)
-    OPT_DEFS += -DLED_MIRRORED
-endif
-
-ifeq ($(strip $(FULLHAND_ENABLE)), yes)
-    OPT_DEFS += -DFULLHAND_ENABLE
-endif
-
-ifeq ($(strip $(EXTRA_ENCODERS_ENABLE)), yes)
-    OPT_DEFS += -DEXTRA_ENCODERS_ENABLE
-endif
+include keyboards/$(KEYBOARD)/post_rules.mk
 
