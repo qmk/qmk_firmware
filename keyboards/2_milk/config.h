2356aba609deddf75d0a29aafd1de3d2ffcf1972
Spaceman <rionlion100@gmail.com>
2019-05-31 12:11:15 -0400

    fix copyright

--- keyboards/2_milk/config.h
+++ keyboards/2_milk/config.h
@@ -1,13 +1,28 @@
+/* Copyright 2019 Sebastian Williams
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #pragma once
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x1209
+#define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0xB195
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    rionlion100
 #define PRODUCT         2% Milk
-#define DESCRIPTION     A milk themed 2% Keyboard
+#define DESCRIPTION     A m ilk themed 2% Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 2
