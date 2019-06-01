bebe9d3e9eba9c4529ebef51f3de20b93510d092
ai03 <sarcaphx510@gmail.com>
2019-05-31 18:53:25 -0700

    Apply suggestions from code review
    Co-Authored-By: Elliot Powell <32494740+e11i0t23@users.noreply.github.com>
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/kbdfans/kbdpad_mk2/keymaps/default/keymap.c
+++ keyboards/kbdfans/kbdpad_mk2/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
+  [0] = LAYOUT_ortho_6x4( /* Base */
     KC_ESC,  KC_LCTL, KC_LALT, KC_BSPC, \
 	KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
 	KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
--- keyboards/kbdfans/kbdpad_mk2/keymaps/tester/keymap.c
+++ keyboards/kbdfans/kbdpad_mk2/keymaps/tester/keymap.c
@@ -17,7 +17,7 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
+  [0] = LAYOUT_ortho_6x4( /* Base */
     BL_TOGG, BL_STEP, RGB_TOG, RGB_MOD, \
 	KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
 	KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
