db8b7507b79dc23229247d683c8cc7f262374b87
jotix <jujodeve@gmail.com>
2019-06-01 00:31:57 -0300

    leds working

--- layouts/community/ortho_4x12/jotix/keymap.c
+++ layouts/community/ortho_4x12/jotix/keymap.c
@@ -62,6 +62,7 @@ uint32_t layer_state_set_user(uint32_t state) {
   case _LOWER:
     writePinHigh(JOTANCK_LED1);
     writePinLow(JOTANCK_LED2);
+    break;
   case _RAISE:
     writePinLow(JOTANCK_LED1);
     writePinHigh(JOTANCK_LED2);
