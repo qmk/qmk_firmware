8b5474b513bbd77d689e070008036b811d85924b
jotix <jujodeve@gmail.com>
2019-05-31 22:18:27 -0300

    set custom leds on layers

--- layouts/community/ortho_4x12/jotix/keymap.c
+++ layouts/community/ortho_4x12/jotix/keymap.c
@@ -55,22 +55,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 uint32_t layer_state_set_user(uint32_t state) {
+
+  #ifdef JOTANCK_LEDS
   switch (biton32(state)) { 
   case _RAISE:
-    writePinLow(JOTANCK_LED1);
+    writePinHigh(JOTANCK_LED1);
     break;
   case _LOWER:
-    writePinLow(JOTANCK_LED2);
+    writePinHigh(JOTANCK_LED2);
     break;
 /*  case _ADJUST:
     writePinLow(JOTANCK_LED1);
     writePinLow(JOTANCK_LED2);
     break; */
   default:
-    writePinHigh(JOTANCK_LED1);
-    writePinHigh(JOTANCK_LED2);
+    writePinLow(JOTANCK_LED1);
+    writePinLow(JOTANCK_LED2);
     break; 
   }
+  #endif
+
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
