5fa73831f0f4dad3d8517e38e63a4b9d20886708
jotix <jujodeve@gmail.com>
2019-05-31 23:41:09 -0300

    bug fix

--- layouts/community/ortho_4x12/jotix/keymap.c
+++ layouts/community/ortho_4x12/jotix/keymap.c
@@ -9,8 +9,9 @@ enum layers {
     _ADJUST,
 };
 
-#define LOWER TT(_LOWER)
-#define RAISE TT(_RAISE)
+#define LOWER   TT(_LOWER)
+#define RAISE   TT(_RAISE)
+#define TAB_ADJ LT(_ADJUST, KC_TAB)
 
 #define FN_LAYER LAYOUT_ortho_4x12 (\
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,\
@@ -25,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
  * |   esc  |    Q   |    W   |    E   |    R   |    T   |    Y   |    U   |    I   |    O   |    P   |  bksp  |
  * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
- * |   tab  |    A   |    S   |    D   |    F   |    G   |    H   |    J   |    K   |    L   |    ;   |  enter |
+ * |tab/adj |    A   |    S   |    D   |    F   |    G   |    H   |    J   |    K   |    L   |    ;   |  enter |
  * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
  * | lshift |    Z   |    X   |    C   |    V   |    B   |    N   |    M   |    ,   |    .   |   up   |    /   |
  * +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
@@ -34,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_ortho_4x12 ( 
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    TAB_ADJ, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
     KC_LCTL, KC_LGUI, KC_LALT, KC_RALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT
 ),
@@ -47,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Adjust 
  */
 [_ADJUST] = LAYOUT_ortho_4x12 (
-    _______, RESET,   _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_PSCR, KC_SLCK, KC_PAUS, _______,
-    _______, _______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______, _______,
-    _______, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, _______, RESET,
+    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-),    
+),     
 };
 
 uint32_t layer_state_set_user(uint32_t state) {
