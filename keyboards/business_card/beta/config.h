34d200e12caa10e8366e2b3490072ab7a1908cf8
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-06-01 20:19:16 +0900

    Update keyboards/business_card

--- keyboards/business_card/beta/config.h
+++ keyboards/business_card/beta/config.h
@@ -80,12 +80,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
--- keyboards/business_card/beta/keymaps/default/keymap.c
+++ keyboards/business_card/beta/keymaps/default/keymap.c
@@ -19,8 +19,8 @@ const uint16_t PROGMEM
 keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] =
 LAYOUT(/* Base */
-        KC_1, KC_2, \
-        KC_3, KC_4, \
+        KC_1, KC_2,
+        KC_3, KC_4,
         KC_5, KC_6
 ),
 };
