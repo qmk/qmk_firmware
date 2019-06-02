d0ec2f10cc65bfc81b84a2aa67215f380cf310a7
Peter Tillemans <pti@snamellit.com>
2019-06-03 00:58:56 +0200

    Remove unused code in my keymap and modernized tap hold keys

--- keyboards/atreus/keymaps/ptillemans/keymap.c
+++ keyboards/atreus/keymaps/ptillemans/keymap.c
@@ -11,12 +11,15 @@
 #define _RS 1
 #define _LW 2
 
+#define MY_SHEN MT(MOD_LSFT, KC_ENT)
+#define MY_CTES MT(MOD_LCTL, KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    TT(_LW), KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,     F(2), KC_LALT, KC_SPC,  TT(_RS), KC_MINS, KC_QUOT, F(1)
+    TT(_LW), KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC, MY_CTES, KC_LALT, KC_SPC,  TT(_RS), KC_MINS, KC_QUOT, MY_SHEN
   ),
 
   /*
@@ -43,27 +46,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
     KC_NO,   KC_VOLD, _______, _______, KC_MNXT, _______, _______, _______, TO(_QW), KC_PSCR, KC_SLCK, KC_MPLY
   )
-};
-
-
-const uint16_t PROGMEM fn_actions[] = {
-  [1] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_ENT),
-  [2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
-  [3] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_SPC)
-};
-
-
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  // MACRODOWN only works in this function
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-      } else {
-        unregister_code(KC_RSFT);
-      }
-    break;
-  }
-  return MACRO_NONE;
 };
