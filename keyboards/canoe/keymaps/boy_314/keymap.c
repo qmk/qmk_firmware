8808af2e0d043bc8cb1ad4446929e9a43b588527
Boy-314 <boy.314.yee.willie@gmail.com>
2019-06-03 00:42:29 -0400

    fixed copyright comments

--- keyboards/canoe/keymaps/boy_314/keymap.c
+++ keyboards/canoe/keymaps/boy_314/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
+Copyright 2019 Boy_314
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
