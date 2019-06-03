03b421f4de334f22362fcc32ac7a1e75ed1347b9
GitWellBack <48095880+GitWellBack@users.noreply.github.com>
2019-06-03 19:20:25 +1000

    Separate default keymap into proper default, iso and ansi versions

--- keyboards/ducky/one2mini/config.h
+++ keyboards/ducky/one2mini/config.h
@@ -167,3 +167,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
+
+/* RGB configuration */
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
