36616cb265186eec98a99be36c49c5bf530d51dc
Drashna Jaelre <drashna@live.com>
2019-06-02 19:44:49 -0700

    Update keyboard for Iris Rev 3 conflicts

--- keyboards/keebio/iris/keymaps/drashna/config.h
+++ keyboards/keebio/iris/keymaps/drashna/config.h
@@ -29,8 +29,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 #ifdef RGBLIGHT_ENABLE
-#   undef RGBLED_NUM
+#   undef  RGBLED_NUM
 #   define RGBLED_NUM 18     // Number of LEDs
+#   undef  RGBLED_SPLIT
 #   define RGBLED_SPLIT { 9, 9 }
 #   define RGBLIGHT_HUE_STEP 12
 #   define RGBLIGHT_SAT_STEP 12
@@ -48,14 +49,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   endif //RGBLIGHT_ENABLE
 #endif //AUDIO_ENABLE
 
-#define QMK_ESC_OUTPUT F6 // usually COL
-#define QMK_ESC_INPUT D7 // usually ROW
-#define QMK_LED B0
-#define QMK_SPEAKER C6
+#ifndef KEYBOARD_keebio_iris_rev3
+#   define QMK_ESC_OUTPUT F6 // usually COL
+#   define QMK_ESC_INPUT D7 // usually ROW
+#   define QMK_LED B0
+#   define QMK_SPEAKER C6
+#endif
 
 #undef PRODUCT
 #ifdef KEYBOARD_keebio_iris_rev2
 #   define PRODUCT         Drashna Hacked Iris Rev.2
+#elif defined(KEYBOARD_keebio_iris_rev3)
+#   define PRODUCT         Drashna Hacked Iris Rev.3
 #endif
 
 #define SHFT_LED1 6
