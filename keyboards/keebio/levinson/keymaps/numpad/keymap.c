aa0f6f44f03e4537ad7f0cd6c2c9814a13afb2ec
Tim <tim.schumacher@nokia.com>
2019-06-04 14:52:16 -0400

    Adds basi numpad configuration for levinson keyboard half

---
+++ keyboards/keebio/levinson/keymaps/numpad/keymap.c
@@ -0,0 +1,28 @@
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Numpad
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |   0  |   1  |   4  |   7  |  Esc | Mute |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |   ,  |   2  |   5  |   8  |   /  | Vol- |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |   .  |   3  |   6  |   9  |   *  | Vol+ |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |    Enter    |   +  |   -  |   ?  |   ~  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[0] = LAYOUT_ortho_4x12( \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_KP_0,     KC_KP_1,     KC_KP_4,    KC_KP_7,     KC_ESC,         KC_BSPC, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_KP_COMMA, KC_KP_2,     KC_KP_5,    KC_KP_8,     KC_KP_SLASH,    KC_A,    \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_KP_DOT,   KC_KP_3,     KC_KP_6,    KC_KP_9,     KC_KP_ASTERISK, KC_B,    \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_KP_ENTER, KC_KP_ENTER, KC_KP_PLUS, KC_KP_MINUS, KC_QUESTION,    KC_C     \
+)
+
+};
+
---
+++ keyboards/keebio/levinson/keymaps/numpad/readme.md
@@ -0,0 +1,5 @@
+# Numpad Layout
+
+Uses a single half of the Levinson a vertical 4x6 numpad/macropad
+
+[Example Layout]()
