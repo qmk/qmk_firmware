8652efa94ca55c14d8422bd29e204d44a9633206
zk-phi <zerokay.phi@gmail.com>
2019-06-06 15:04:35 +0900

    Move keymap-specific rules to keymaps/<map>/rules.mk

--- keyboards/ergo42/rules.mk
+++ keyboards/ergo42/rules.mk
@@ -43,13 +43,13 @@ F_USB = $(F_CPU)
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
+
 # Boot Section Size in *bytes*
 #   Teensy halfKay   512
 #   Teensy++ halfKay 1024
 #   Atmel DFU loader 4096
 #   LUFA bootloader  4096
 #   USBaspLoader     2048
-BOOTLOADER = caterina
 OPT_DEFS += -DBOOTLOADER_SIZE=4096
 
 # Build Options
@@ -59,9 +59,8 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-KEY_LOCK_ENABLE = no        # KC_LOCK to auto-hold a key
 CONSOLE_ENABLE = no         # Console for debug(+400)
-COMMAND_ENABLE = no         # Commands for debug and configuration
+COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
