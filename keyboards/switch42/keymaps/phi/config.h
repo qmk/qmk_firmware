44f31cb7fbd21ef69df808325344795a877841a1
zk-phi <zerokay.phi@gmail.com>
2019-06-06 15:18:44 +0900

    Remove use of the enhanced mousekey feature (Switch42)

--- keyboards/switch42/keymaps/phi/config.h
+++ keyboards/switch42/keymaps/phi/config.h
@@ -50,8 +50,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_MAX_SPEED   1 /* times faster */
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 1
 
-#define MOUSEKEY_ACCEL0_SPEED 0.66
-#define MOUSEKEY_ACCEL0_WHEEL_SPEED 0.25
+#define MOUSEKEY_ACL_MAX_SPEED 16
+#define MOUSEKEY_ACL_WHEEL_MAX_SPEED 4
 
 #define TAPPING_TERM 180
 
