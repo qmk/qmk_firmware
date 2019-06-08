89db4081f68591179753511c2d8fa9c4441396b9
Yan-Fa Li <yanfali@gmail.com>
2019-06-07 19:35:10 -0700

    Update keymap for incorrect keys
     - update docs

--- keyboards/plain60/keymaps/yanfali/README.md
+++ keyboards/plain60/keymaps/yanfali/README.md
@@ -2,7 +2,7 @@
 - Tsangan bottom row
 - Split Backspace
 - Split Right shift
-- RESET on layer and F keys
+- RESET, F keys, and Cursors (WASD), DEL, Capslock on layer
 - Capslock -> LCTL_T(KC_ESC)
 - Bootmagic lite
 - Command enabled
--- keyboards/plain60/keymaps/yanfali/keymap.c
+++ keyboards/plain60/keymaps/yanfali/keymap.c
@@ -10,9 +10,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FN] = LAYOUT(
-  _______,  KC_F1,    KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,    KC_F11,  KC_F12,  _______, _______, \
-  _______,  _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______, _______, RESET,         \
-  _______,  _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______, _______, _______,          \
+  _______,  KC_F1,    KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,    KC_F11,  KC_F12,  _______, KC_DEL, \
+  _______,  _______,  KC_UP,     _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______, _______, RESET,         \
+  KC_CAPS,  KC_LEFT,  KC_DOWN,   KC_RIGHT,  _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______, _______, _______,          \
   _______,  _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______, _______, _______,       \
   _______,  _______,  _______,                         _______,                         _______,   _______,   _______,  _______),
 
@@ -21,5 +21,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,           KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         \
   LCTL_T(KC_ESC),   KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,          \
   KC_LSFT,          KC_NO,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN)  ,       \
-  KC_LCTL,          KC_LALT, KC_LGUI,                        KC_SPC,                          KC_RGUI, KC_RALT, KC_NO,  KC_RCTRL)
+  KC_LCTL,          KC_LALT, KC_LGUI,                        KC_SPC,                          KC_NO,   KC_RGUI, KC_RALT,  KC_RCTRL)
 };
