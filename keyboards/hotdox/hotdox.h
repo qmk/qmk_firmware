773724d2b98224952362e1280e86fed851838ed9
imchipwood <imchipwood@gmail.com>
2019-06-09 14:27:02 -0700

    KC-less hotdox layout

--- keyboards/hotdox/hotdox.h
+++ keyboards/hotdox/hotdox.h
@@ -185,3 +185,42 @@ void ergodox_blink_all_leds(void);
     { KC_NO,L51,L52,L53,L54,L55,L56,          R50,R51,R52,R53,R54,R55,KC_NO }  \
    }
 
+#define LAYOUT_ergodox_kc(                                                              \
+                                                                                        \
+                                                                \
+    k00,k01,k02,k03,k04,k05,k06,                                \
+    k10,k11,k12,k13,k14,k15,k16,                                \
+    k20,k21,k22,k23,k24,k25,                                    \
+    k30,k31,k32,k33,k34,k35,k36,                                \
+    k40,k41,k42,k43,k44,                                        \
+                            k55,k56,                            \
+                                k54,                            \
+                        k51,k52,k53,                            \
+                                                                \
+        k07,k08,k09,k0A,k0B,k0C,k0D,                            \
+        k17,k18,k19,k1A,k1B,k1C,k1D,                            \
+            k28,k29,k2A,k2B,k2C,k2D,                            \
+        k37,k38,k39,k3A,k3B,k3C,k3D,                            \
+                k49,k4A,k4B,k4C,k4D,                            \
+    k57,k58,                                                    \
+    k59,                                                        \
+    k5A,k5B,k5C )                                               \
+    LAYOUT_ergodox( \
+        KC_##k00,   KC_##k01,   KC_##k02,   KC_##k03,   KC_##k04,   KC_##k05,   KC_##k06,               \
+        KC_##k10,   KC_##k11,   KC_##k12,   KC_##k13,   KC_##k14,   KC_##k15,   KC_##k16,               \
+        KC_##k20,   KC_##k21,   KC_##k22,   KC_##k23,   KC_##k24,   KC_##k25,                           \
+        KC_##k30,   KC_##k32,   KC_##k32,   KC_##k33,   KC_##k34,   KC_##k35,   KC_##k36,               \
+        KC_##k40,   KC_##k41,   KC_##k42,   KC_##k43,   KC_##k44,                                       \
+                                                                                KC_##k55,   KC_##k56,   \
+                                                                                            KC_##k54,   \
+                                                                    KC_##k51,   KC_##k52,   KC_##k53,   \
+                                                                                                        \
+                    KC_##k07,   KC_##k08,   KC_##k09,   KC_##k0A,   KC_##k0B,   KC_##k0C,   KC_##k0D,   \
+                    KC_##k17,   KC_##k18,   KC_##k19,   KC_##k1A,   KC_##k1B,   KC_##k1C,   KC_##k1D,   \
+                                KC_##k28,   KC_##k29,   KC_##k2A,   KC_##k2B,   KC_##k2C,   KC_##k2D,   \
+                    KC_##k37,   KC_##k38,   KC_##k39,   KC_##k3A,   KC_##k3B,   KC_##k3C,   KC_##k3D,   \
+                                            KC_##k49,   KC_##k4A,   KC_##k4B,   KC_##k4C,   KC_##k4D,   \
+        KC_##k57,   KC_##k58,                                                                           \
+        KC_##k59,                                                                                       \
+        KC_##k5A,   KC_##k5B,   KC_##k5C                                                                \
+    )
--- keyboards/hotdox/keymaps/imchipwood/keymap.c
+++ keyboards/hotdox/keymaps/imchipwood/keymap.c
@@ -1,12 +1,25 @@
 #include QMK_KEYBOARD_H
+#include "hotdox.h"
 #include "quantum.h"
+#include "action_layer.h"
+#include "eeconfig.h"
 #include "version.h"
 
-// Layer names
-#define BASE 0 // default layer
-#define SYMB 1 // symbols
-#define MDIA 2 // media keys
+extern keymap_config_t keymap_config;
 
+#define KC____  KC_TRNS // three underscores "___" for transparent
+
+// Layer names
+#define _QWERTY 0 // default layer
+#define _NUMB   1 // symbols
+#define _MDIA   2 // media keys
+
+
+enum custom_keycodes {
+	QWERTY = SAFE_RANGE,
+	NUMB,
+	MDIA,
+};
 
 // Tap Dance Declarations 
 enum {
@@ -25,6 +38,12 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define KC_REND TD(TD_RIGHT_END)
 #define KC_LOME TD(TD_LEFT_HOME)
 #define KC_RCAP TD(TD_RSFT_CAPS)
+
+#define KC_TGNB TG(_NUMB)
+#define KC_TGMD TG(_MDIA)
+#define KC_MONB MO(_NUMB)
+#define KC_MOMD MO(_MDIA)
+#define KC_RESET RESET
 
 
 // Custom per-key tapping term
@@ -59,34 +78,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
-[BASE] = LAYOUT_ergodox(
+[_QWERTY] = LAYOUT_ergodox_kc(
 	// Left hand
-	KC_EQL, 			KC_1, 		KC_2, 		KC_3, 				KC_4, 				KC_5, 		KC_ESC, 
-	KC_TAB, 			KC_Q, 		KC_W, 		KC_E, 				KC_R, 				KC_T, 		TG(1), 
-	KC_BSLS, 			KC_A, 		KC_S, 		KC_D, 				KC_F, 				KC_G, 
-	KC_LSFT, 			KC_Z, 		KC_X, 		KC_C, 				KC_V, 				KC_B, 		MO(2), 
-	KC_LCTL, 			KC_LALT, 	KC_LGUI, 	KC_LOME, 			KC_REND, 	
-									KC_DEL, 	KC_INS, 			KC_HOME, 
-									KC_ENT, 	MO(1), 				KC_END, 
+	EQL, 	1, 		2, 		3, 		4, 		5, 		ESC, 
+	TAB, 	Q, 		W, 		E, 		R, 		T, 		TGNB, 
+	BSLS, 	A, 		S, 		D, 		F, 		G, 
+	LSFT, 	Z, 		X, 		C, 		V, 		B, 		MOMD, 
+	LCTL, 	LALT, 	LGUI, 	LOME, 	REND, 	
+											DEL, 	INS, 	
+													HOME, 
+									ENT, 	MONB, 	
+													END, 
 	// Right hand
-	KC_GRV, 			KC_6, 		KC_7, 		KC_8, 				KC_9, 				KC_0, 		KC_MINS, 
-	TG(2), 				KC_Y, 		KC_U, 		KC_I, 				KC_O, 				KC_P, 		KC_LBRC, 
-						KC_H, 		KC_J, 		KC_K, 				KC_L, 				KC_SCLN, 	KC_QUOT, 
-	MO(1), 				KC_N, 		KC_M, 		KC_COMM, 			KC_DOT, 			KC_SLSH, 	KC_RCAP, 
-									KC_UP, 		KC_DOWN, 			KC_LEFT, 			KC_RGHT, 	KC_PSCR, 
-									KC_LCTL, 	KC_LALT, 			KC_PGUP, 	
-									KC_PGDN, 	KC_BSPC, 			KC_SPC
+	GRV, 	6, 		7, 		8, 		9, 		0, 		MINS, 
+	TGMD,	Y, 		U, 		I, 		O, 		P, 		LBRC, 
+			H, 		J, 		K, 		L, 		SCLN, 	QUOT, 
+	MONB, 	N, 		M, 		COMM, 	DOT, 	SLSH, 	RCAP, 
+					UP, 	DOWN, 	LEFT, 	RGHT, 	PSCR, 
+	LCTL, 	LALT, 	
+	PGUP, 	
+	PGDN, 	BSPC, 	SPC
 	),
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   7  |   8  |   9  |   *  |   /  |    ]   |
+ * |         |      |      |      |      |      |      |           |      |   7  |   8  |   9  |   *  |   /  |    ]   |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   #  |   $  |   (  |   )  |   `  |------|           |------|   4  |   5  |   6  |   +  |   -  |        |
+ * |         |      |      |      |      |      |------|           |------|   4  |   5  |   6  |   +  |   -  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   1  |   2  |   3  |   =  |  up  |  enter |
+ * |         |      |      |      |      |      |      |           |      |   1  |   2  |   3  |   =  |  up  |  enter |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  * |         |      |      | PgUp | PgDn |                                       |   0  |   .  | left | down |right |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -98,23 +120,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-[SYMB] = LAYOUT_ergodox(
+[_NUMB] = LAYOUT_ergodox_kc(
 	// Left hand
-	KC_TRNS, 			KC_F1, 		KC_F2, 		KC_F3, 				KC_F4, 				KC_F5, 		KC_TRNS, 
-	KC_TRNS, 			KC_EXLM, 	KC_AT, 		KC_LCBR, 			KC_RCBR, 			KC_PIPE, 	KC_TRNS, 
-	KC_TRNS, 			KC_HASH, 	KC_DLR, 	KC_LPRN, 			KC_RPRN, 			KC_GRV, 
-	KC_TRNS, 			KC_PERC, 	KC_CIRC, 	KC_LBRC, 			KC_RBRC, 			KC_TILD, 	KC_TRNS,
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_PGUP, 			KC_PGDN, 
-									KC_TRNS, 	KC_TRNS, 			KC_TRNS, 
-									KC_ENT, 	KC_TRNS, 			KC_TRNS, 
+	___, 	F1, 	F2, 	F3, 	F4, 	F5, 	___, 
+	___, 	___, 	___, 	___, 	___, 	___, 	___, 
+	___, 	___, 	___, 	___, 	___, 	___, 
+	___, 	___, 	___, 	___, 	___, 	___, 	___,
+	___, 	___, 	___, 	PGUP, 	PGDN, 
+					___, 	___, 	___, 
+					ENT, 	___, 	___, 
 	// Right hand
-	KC_F12, 			KC_F6, 		KC_F7, 		KC_F8, 				KC_F9, 				KC_F10, 	KC_F11,
-	KC_TRNS, 			KC_7, 		KC_8, 		KC_9, 				KC_PAST, 			KC_PSLS, 	KC_RBRC, 
-						KC_4, 		KC_5, 		KC_6, 				KC_PPLS, 			KC_PMNS, 	KC_TRNS, 
-	KC_TRNS, 			KC_1, 		KC_2, 		KC_3, 				KC_PEQL, 			KC_UP, 		KC_PENT, 
-									KC_0, 		KC_PDOT, 			KC_LEFT, 			KC_DOWN, 	KC_RGHT, 
-									KC_TRNS, 	KC_TRNS, 			KC_TRNS, 
-									KC_TRNS, 	KC_DEL, 			KC_ESC
+	F12, 	F6, 	F7, 	F8, 	F9, 	F10, 	F11,
+	___, 	7, 		8, 		9, 		PAST, 	PSLS, 	RBRC, 
+			4, 		5, 		6, 		PPLS, 	PMNS, 	___, 
+	___, 	1, 		2, 		3, 		PEQL, 	UP,		PENT, 
+					0, 		PDOT, 	LEFT, 	DOWN, 	RGHT, 
+					___, 	___, 	___, 
+					___, 	DEL, 	ESC
 	),
 /* Keymap 2: Media and mouse keys
  *
@@ -137,22 +159,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-[MDIA] = LAYOUT_ergodox(
+[_MDIA] = LAYOUT_ergodox_kc(
 	// Left hand
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_TRNS, 			KC_TRNS, 			KC_TRNS, 	KC_TRNS, 
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_MS_U, 			KC_TRNS, 			KC_TRNS, 	KC_TRNS, 
-	KC_TRNS, 			KC_TRNS, 	KC_MS_L, 	KC_MS_D, 			KC_MS_R, 			KC_TRNS, 
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_TRNS, 			KC_TRNS, 			KC_TRNS, 	KC_TRNS, 
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_BTN1, 			KC_BTN2, 
-									RESET, 		KC_TRNS, 			KC_TRNS, 
-									KC_TRNS, 	KC_TRNS, 			KC_TRNS, 
+	___, 	___, 	___, 	___, 	___, 	___, 	___, 
+	___, 	___, 	___, 	MS_U, 	___, 	___, 	___, 
+	___, 	___, 	MS_L, 	MS_D, 	MS_R, 	___, 
+	___, 	___, 	___, 	___, 	___, 	___, 	___, 
+	___, 	___, 	___, 	BTN1, 	BTN2, 
+					RESET, 	___, 	___, 
+					___, 	___, 	___, 
 	// Right hand								
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_TRNS, 			KC_TRNS, 			KC_TRNS, 	KC_TRNS, 
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_TRNS, 			KC_TRNS, 			KC_TRNS, 	KC_TRNS, 
-						KC_TRNS, 	KC_TRNS, 	KC_TRNS, 			KC_TRNS, 			KC_TRNS, 	KC_MPLY, 
-	KC_TRNS, 			KC_TRNS, 	KC_TRNS, 	KC_MPRV, 			KC_MNXT, 			KC_TRNS, 	KC_TRNS, 
-									KC_VOLU, 	KC_VOLD, 			KC_MUTE, 			KC_TRNS, 	KC_TRNS, 
-									KC_TRNS, 	KC_TRNS, 			KC_TRNS, 
-									KC_TRNS, 	KC_TRNS, 			KC_WBAK
+	___, 	___, 	___, 	___, 	___, 	___, 	___, 
+	___, 	___, 	___, 	___, 	___, 	___, 	___, 
+			___, 	___, 	___, 	___, 	___, 	MPLY, 
+	___, 	___, 	___, 	MPRV, 	MNXT, 	___, 	___, 
+					VOLU, 	VOLD, 	MUTE, 	___, 	___, 
+					___, 	___, 	___, 
+					___, 	___, 	WBAK
 	)
 };
