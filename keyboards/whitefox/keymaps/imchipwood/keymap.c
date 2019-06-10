20687bb1c8f257dd4f40f224e58bd5a09a5de56e
imchipwood <imchipwood@gmail.com>
2019-06-09 17:20:09 -0700

    added LAYOUT_kc to whitefox, updated my whitefox layout to use it, other minor tweaks

--- keyboards/whitefox/keymaps/imchipwood/keymap.c
+++ keyboards/whitefox/keymaps/imchipwood/keymap.c
@@ -1,39 +1,23 @@
-/*
-Copyright 2015 Jun Wako <wakojun@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include QMK_KEYBOARD_H
 
+#include "whitefox.h"
 
-/*
-enum {
-	TD_RIGHT_END = 0,
-	TD_LEFT_HOME,
-	TD_RSFT_CAPS,
-};
-*/
+// tap dance is enabled for imchipwood, gotta define something here...
+qk_tap_dance_action_t tap_dance_actions[] = {};
 
-// Tap Dance Definitions - none for now
-qk_tap_dance_action_t tap_dance_actions[] = {
-	/*
-	[TD_RIGHT_END] = ACTION_TAP_DANCE_DOUBLE(KC_RIGHT, KC_END),
-	[TD_LEFT_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_HOME),
-	[TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
-	*/
-};
+// Layer names
+#define _QWERTY     0 // default layer
+#define _FN_MEDIA   1 // media, function, and other keys
 
+// KC_-prefixed macros for quantum actions to work with KC_-less layouts
+// Layers
+#define KC_MONB     MO(_FN_MEDIA)
+
+// Reset to bootloader
+#define KC_RESET    RESET
+
+// three underscores "___" for transparent
+#define KC____      KC_TRNS
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
@@ -46,21 +30,33 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------------------------------------------------------------|
      * |Shif|   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up | END|
      * |----------------------------------------------------------------|
-     * |Ctrl|Gui |Alt |         Space    |Fn0 |Alt |Gui |  |Lef|Dow| Rig|
+     * |Ctrl|Gui |Alt |         Space          |Alt |Fn0 | |Lef|Dow| Rig|
      * `----------------------------------------------------------------'
      */
-    [0] = LAYOUT( \
-        KC_ESC, 	KC_1,   	KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   	KC_8,   KC_9,   	KC_0,   	KC_MINS,	KC_EQL,		KC_BSPC,KC_TILD,	KC_DEL,\
-        KC_TAB, 	KC_Q,   	KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   	KC_I,   KC_O,   	KC_P,   	KC_LBRC,	KC_RBRC,			KC_BSLS,    KC_PSCR,\
-        KC_CAPS,	KC_A,   	KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   	KC_K,   KC_L,   	KC_SCLN,	KC_QUOT,	KC_NUHS,			KC_ENT,     KC_HOME,\
-        KC_LSFT,	KC_NUBS,	KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   	KC_M,   KC_COMM,	KC_DOT, 	KC_SLSH,	KC_RSFT,    		KC_UP,  	KC_END,\
-        KC_LCTL,	KC_LGUI,	KC_LALT,               KC_SPC,           KC_RALT,	MO(1), 	KC_RCTL,     						KC_LEFT,			KC_DOWN,	KC_RGHT \
+    [_QWERTY] = LAYOUT_kc( 
+//    ,--------------------------------------------------------------------------------.
+        ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSPC,TILD, DEL,
+//    |--------------------------------------------------------------------------------|
+          TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,   BSLS,PSCR,
+//    |--------------------------------------------------------------------------------|
+          CAPS,   A,   S,   D,   F,   G,   H,   J,    K,    L, SCLN,QUOT,NUHS,ENT,HOME,
+//    |--------------------------------------------------------------------------------|
+        LSFT,NUBS,  Z,   X,   C,   V,   B,   N,   M, COMM, DOT, SLSH,    RSFT, UP, END,
+//    |--------------------------------------------------------------------------------|
+        LCTL,LGUI,LALT,            SPC,                   RALT, MONB,___,LEFT,DOWN,RGHT 
+//    `--------------------------------------------------------------------------------'
     ),
-    [1] = LAYOUT( \
-        RESET,	KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS,KC_TRNS,KC_NLCK,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PSCR,KC_SLCK,KC_PAUS,KC_TRNS,     KC_SLCK,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_VOLU,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MPRV,KC_MNXT,     KC_PGUP,KC_VOLD,\
-        KC_TRNS,KC_TRNS,KC_TRNS,               KC_TRNS,          KC_TRNS,KC_TRNS,KC_TRNS,     KC_HOME,KC_PGDN,KC_END  \
+    [_FN_MEDIA] = LAYOUT_kc( 
+//    ,--------------------------------------------------------------------------------.
+        RESET,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, INS, ___,NLCK,
+//    |--------------------------------------------------------------------------------|
+          ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,PSCR,SLCK,PAUS,   ___, SLCK,
+//    |--------------------------------------------------------------------------------|
+           ___, ___, ___, ___, ___, ___, ___, ___,  ___,  ___,  ___, ___, ___,___,VOLU,
+//    |--------------------------------------------------------------------------------|
+         ___, ___,___, ___, ___, ___, ___, ___, ___,  ___, ___, MPRV,   MNXT,PGUP,VOLD,
+//    |--------------------------------------------------------------------------------|
+         ___, ___, ___,            ___,                    ___,  ___,___,HOME,PGDN,END
+//    `--------------------------------------------------------------------------------' 
     ),
 };
--- keyboards/whitefox/whitefox.h
+++ keyboards/whitefox/whitefox.h
@@ -102,4 +102,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { K80, K81,   K82, K83,   K84, K85, K86, KC_NO }  \
 }
 
+
+#define LAYOUT_kc( \
+        K00, K10, K20, K30, K40, K50, K60, K70, K80, K01, K11, K21, K31, K41, K51, K61, \
+        K71, K81, K02, K12, K22, K32, K42, K52, K62, K72, K82, K03, K13, K23,      K33, \
+        K43, K53, K63, K73, K83, K04, K14, K24, K34, K44, K54, K64, K74, K84,      K05, \
+        K15, K25, K35, K45, K55, K65, K75, K85, K06, K16, K26, K36, K46,      K56, K66, \
+        K76, K86, K07,                K17,           K27, K37, K47,      K57, K67, K77  \
+    )   \
+    LAYOUT( \
+        KC_##K00, KC_##K10, KC_##K20, KC_##K30, KC_##K40, KC_##K50, KC_##K60, KC_##K70, KC_##K80, KC_##K01, KC_##K11, KC_##K21, KC_##K31, KC_##K41, KC_##K51, KC_##K61, \
+        KC_##K71, KC_##K81, KC_##K02, KC_##K12, KC_##K22, KC_##K32, KC_##K42, KC_##K52, KC_##K62, KC_##K72, KC_##K82, KC_##K03, KC_##K13, KC_##K23,           KC_##K33, \
+        KC_##K43, KC_##K53, KC_##K63, KC_##K73, KC_##K83, KC_##K04, KC_##K14, KC_##K24, KC_##K34, KC_##K44, KC_##K54, KC_##K64, KC_##K74, KC_##K84,           KC_##K05, \
+        KC_##K15, KC_##K25, KC_##K35, KC_##K45, KC_##K55, KC_##K65, KC_##K75, KC_##K85, KC_##K06, KC_##K16, KC_##K26, KC_##K36, KC_##K46,           KC_##K56, KC_##K66, \
+        KC_##K76, KC_##K86, KC_##K07,                               KC_##K17,                     KC_##K27, KC_##K37, KC_##K47,           KC_##K57, KC_##K67, KC_##K77  \
+    )
+
 #endif
