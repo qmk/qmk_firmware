c4ddcbefbf98b2fb367fe10e81f07d0c96f1e167
calbatr0ss <calvinmcm@gmail.com>
2019-06-10 19:34:02 -0400

    swap right ctrl and menu

--- keyboards/dz60/keymaps/calbatr0ss/keymap.c
+++ keyboards/dz60/keymaps/calbatr0ss/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
  * │ SHIFT  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ SHIFT│LYR│
  * ├────┬───┴┬──┴─┬─┴───┴──┬┴───┼───┴───┴──┬┴───┼───┴┬────┬┴───┤
- * │CAPS│ OS │ ALT│ SPACE  │ FN │  SPACE   │ ALT│ OS │CTRL│MENU│
+ * │CAPS│ OS │ ALT│ SPACE  │ FN │  SPACE   │ ALT│ OS │MENU│CTRL│
  * └────┴────┴────┴────────┴────┴──────────┴────┴────┴────┴────┘
  */
   LAYOUT_60_calbatr0ss(
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,           KC_Q,    KC_W,    KC_E,   KC_R,  KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       KC_LCTL,          KC_A,    KC_S,    KC_D,   KC_F,  KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(3),
-      KC_CAPS, KC_LGUI,          KC_LALT, KC_SPC,        MO(2),            KC_SPC, KC_RALT,         KC_RGUI, KC_RCTL, KC_APP),
+      KC_CAPS, KC_LGUI,          KC_LALT, KC_SPC,        MO(2),            KC_SPC, KC_RALT,         KC_RGUI, KC_APP,  KC_RCTL),
 
 /*  LAYER 1
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
  * │ SHIFT  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ SHIFT│LYR│
  * ├────┬───┴┬──┴─┬─┴───┴──┬┴───┼───┴───┴──┬┴───┼───┴┬────┬┴───┤
- * │CAPS│ ALT│ OS │ SPACE  │ FN │  SPACE   │ OS │ ALT│CTRL│MENU│
+ * │CAPS│ ALT│ OS │ SPACE  │ FN │  SPACE   │ OS │ ALT│MENU│CTRL│
  * └────┴────┴────┴────────┴────┴──────────┴────┴────┴────┴────┘
  */
   LAYOUT_60_calbatr0ss(
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,           KC_Q,    KC_W,    KC_E,   KC_R,  KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       KC_LCTL,          KC_A,    KC_S,    KC_D,   KC_F,  KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(3),
-      KC_CAPS, KC_LALT,          KC_LGUI, KC_SPC,        MO(2),            KC_SPC, KC_RGUI,         KC_RALT, KC_RCTL, KC_APP),
+      KC_CAPS, KC_LALT,          KC_LGUI, KC_SPC,        MO(2),            KC_SPC, KC_RGUI,         KC_RALT, KC_APP,  KC_RCTL),
 
 /*  LAYER 2
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
