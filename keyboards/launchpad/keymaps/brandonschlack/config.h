ad1e6bbe190dce7365d1ee8d1ece096d0c2a431f
Brandon Schlack <brandonschlack@gmail.com>
2019-06-11 12:48:36 -0700

    [Keymap] Add launchpad mouse key config

--- keyboards/launchpad/keymaps/brandonschlack/config.h
+++ keyboards/launchpad/keymaps/brandonschlack/config.h
@@ -7,3 +7,8 @@
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+
+/* Mouse Keys Configuration */
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
