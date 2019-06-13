f84911776fd560b5e2ce5b91f3ab99edaa1c25fc
Brandon Kirisaki <b.k.lu@ieee.org>
2019-06-13 15:15:43 -0700

    Added tritium_numpad
    Adding tritium numpad handwired 6x4 numpad design from thingiverse
    user tritiumfusion.

---
+++ keyboards/handwired/tritium_numpad/readme.md
@@ -0,0 +1,12 @@
+Tritium Numpad
+===
+
+Keyboard Maintainer: QMK Community  
+Hardware Supported: Handwired 6x4 numpads  
+Hardware Availability: https://www.thingiverse.com/thing:2855938
+
+Make example for this keyboard (after setting up your build environment):
+
+    make tritium_numpad:default
+
+See [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) then the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information.
---
+++ keyboards/handwired/tritium_numpad/tritium_numpad.c
@@ -0,0 +1,30 @@
+#include "tritium_numpad.h"
+#include "led.h"
+
+void matrix_init_kb(void) {
+	// put your keyboard start-up code here
+	// runs once when the firmware starts up
+	matrix_init_user();
+	led_init_ports();
+};
+
+void matrix_scan_kb(void) {
+	// put your looping keyboard code here
+	// runs every cycle (a lot)
+	matrix_scan_user();
+};
+
+void led_init_ports(void) {
+    // * Set our LED pins as output
+    DDRD |= (1<<5);
+}
+
+void led_set_kb(uint8_t usb_led) {
+    if (usb_led & (1<<USB_LED_NUM_LOCK)) {
+        // Turn numlock on
+        PORTD &= ~(1<<5);
+    } else {
+        // Turn numlock off
+        PORTD |= (1<<5);
+    }
+}
