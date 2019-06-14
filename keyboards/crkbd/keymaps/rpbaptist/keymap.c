654f68de3680d31c52cfbc644978794e17774e21
Richard Baptist <rpbaptist@gmail.com>
2019-06-14 11:26:21 +0200

    Fix white space

--- keyboards/crkbd/keymaps/rpbaptist/keymap.c
+++ keyboards/crkbd/keymaps/rpbaptist/keymap.c
@@ -155,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_UTIL] = LAYOUT_kc( \
   //,-----------------------------------------.                ,-----------------------------------------.
-      ESC,   XXXXX,  MSTP,  VOLU,  MNXT,  PAUS,                   NLCK,  KP_7,  KP_8,  KP_9,  PSLS,  PAST,\
+        ESC, XXXXX,  MSTP,  VOLU,  MNXT,  PAUS,                   NLCK,  KP_7,  KP_8,  KP_9,  PSLS,  PAST,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
       _____, XXXXX,  MPRV,  VOLD,  MPLY,   INS,                   CAPS,  KP_4,  KP_5,  KP_6,  PMNS,  PPLS,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
