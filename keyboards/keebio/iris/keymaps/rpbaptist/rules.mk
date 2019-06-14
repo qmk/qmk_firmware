2fd58038be7d869164412008c2127e73203c9d95
Richard Baptist <rpbaptist@gmail.com>
2019-06-14 10:30:06 +0200

    Add Iris keymaps

---
+++ keyboards/keebio/iris/keymaps/rpbaptist/rules.mk
@@ -0,0 +1,10 @@
+AUDIO_ENABLE = no           # Audio output on port C6
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+COMMAND_ENABLE = no         # Commands for debug and configuration
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+MIDI_ENABLE = no            # MIDI controls
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+UNICODE_ENABLE = no         # Unicode
+
+RGBLIGHT_ENABLE = yes
+BACKLIGHT_ENABLE = yes
