2041833e46411b2f472064699905a485fe929906
Henrik Sørensen <henrik.sorensen@gmail.com>
2019-06-15 11:49:01 +0200

    PCB Rev 1.1 moved Row5's pin to E6, because the teensy++ hangs an onboard LED off D6.

--- keyboards/omnikeyish/config.h
+++ keyboards/omnikeyish/config.h
@@ -3,16 +3,14 @@
 
 #include "config_common.h"
 
-#define DYNAMIC_MACRO_COUNT 12
-#define DYNAMIC_MACRO_SIZE 48
-
+#define KEYBOARD_PCB_REV 10
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0666
 #define DEVICE_VER      0x1337
 #define MANUFACTURER    Henrik O. Sørensen
-#define PRODUCT         Omnikey(-ish) PCB
+#define PRODUCT         Omnikey(-ish) Keyboard
 #define DESCRIPTION     Replacement PCB for Omnikey keyboards
 
 /* key matrix size */
@@ -20,10 +18,16 @@
 #define MATRIX_COLS 23
 
 /* key matrix pins */
+#if KEYBOARD_PCB_REV == 10
 #define MATRIX_ROW_PINS { D2, D3, D4, D5, D6, D7 }
-//#define MATRIX_ROW_PINS { D6 }
+#else
+#define MATRIX_ROW_PINS { D2, D3, D4, D5, E6, D7 }
+#endif 
 #define MATRIX_COL_PINS { F0, F1, F2, F3, F4, F5, F6, F7, C7, C6, C5, C4, C3, C2, C1, C0, B0, B1, B2, B3, B4, B5, B6 }
-#define UNUSED_PINS
+
+#define NUMLOCKLEDPIN E0
+#define CAPSLOCKLEDPIN E1
+#define SCROLLLOCKLEDPIN B7
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
@@ -53,12 +57,11 @@
 #define RGBLIGHT_VAL_STEP 8
 #endif
 
+#define DYNAMIC_MACRO_RANGE SAFE_RANGE + 1
+#define DYNAMIC_MACRO_COUNT 12
+#define DYNAMIC_MACRO_SIZE 48
+
 #endif
 
-#define NUMLOCKLEDPIN E0
-#define CAPSLOCKLEDPIN E1
-#define SCROLLLOCKLEDPIN B7
-
-#define DYNAMIC_MACRO_RANGE SAFE_RANGE + 1
 
 
