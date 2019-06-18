407953dabf78289f9c1d5192d55b0eb8030ecf0b
Callum Oakley <c.oakley108@gmail.com>
2019-06-18 22:29:37 +0100

    mods on symbol layer

--- keyboards/planck/keymaps/callum/keymap.c
+++ keyboards/planck/keymaps/callum/keymap.c
@@ -3,9 +3,8 @@
 
 extern keymap_config_t keymap_config;
 
-#define XXXX KC_NO
 #define ____ KC_TRNS
-#define a KC_A
+#define a LSFT_T(KC_A)
 #define b KC_B
 #define back G(KC_LBRC)
 #define bspc KC_BSPC
@@ -18,8 +17,7 @@ extern keymap_config_t keymap_config;
 #define del KC_DEL
 #define dot KC_DOT
 #define down KC_DOWN
-#define e KC_E
-#define e_ra RALT_T(KC_E)
+#define e RALT_T(KC_E)
 #define end KC_END
 #define ent KC_ENT
 #define esc KC_ESC
@@ -48,13 +46,13 @@ extern keymap_config_t keymap_config;
 #define f7 KC_F7
 #define f8 KC_F8
 #define f9 KC_F9
+#define func MO(FUNC)
 #define fwd G(KC_RBRC)
 #define g KC_G
 #define gbp A(KC_3)
 #define h KC_H
 #define home KC_HOME
-#define i KC_I
-#define i_rc RCTL_T(KC_I)
+#define i RCTL_T(KC_I)
 #define j KC_J
 #define k KC_K
 #define l KC_L
@@ -65,8 +63,9 @@ extern keymap_config_t keymap_config;
 #define lsft KC_LSFT
 #define m KC_M
 #define mins KC_MINS
+#define move MO(MOVE)
 #define mute KC_MUTE
-#define n KC_N
+#define n RGUI_T(KC_N)
 #define n0 KC_0
 #define n1 KC_1
 #define n2 KC_2
@@ -77,9 +76,8 @@ extern keymap_config_t keymap_config;
 #define n7 KC_7
 #define n8 KC_8
 #define n9 KC_9
-#define n_rg RGUI_T(KC_N)
 #define next KC_MNXT
-#define o KC_O
+#define o LSFT_T(KC_O)
 #define p KC_P
 #define pgdn KC_PGDN
 #define pgup KC_PGUP
@@ -88,23 +86,21 @@ extern keymap_config_t keymap_config;
 #define prev KC_MPRV
 #define q KC_Q
 #define quot KC_QUOT
-#define r KC_R
-#define r_lc LCTL_T(KC_R)
+#define r LCTL_T(KC_R)
 #define ralt KC_RALT
 #define rctl KC_RCTL
 #define rght KC_RGHT
 #define rgui KC_RGUI
 #define rset RESET
 #define rsft KC_RSFT
-#define s KC_S
-#define s_la LALT_T(KC_S)
+#define s LALT_T(KC_S)
 #define scln KC_SCLN
 #define sldn S(A(KC_DOWN))
 #define slsh KC_SLSH
 #define slup S(A(KC_UP))
 #define spc KC_SPC
-#define t KC_T
-#define t_lg LGUI_T(KC_T)
+#define symb MO(SYMB)
+#define t LGUI_T(KC_T)
 #define tab KC_TAB
 #define u KC_U
 #define up KC_UP
@@ -113,98 +109,104 @@ extern keymap_config_t keymap_config;
 #define volu KC_F12
 #define w KC_W
 #define x KC_X
+#define xxxx KC_NO
 #define y KC_Y
 #define z KC_Z
-
+#define hash TD(_hash)
+#define astr TD(_astr)
+#define eql TD(_eql)
+#define rprn TD(_rprn)
+#define lprn TD(_lprn)
+#define dlr TD(_dlr)
 
 enum planck_layers {
-    _BASE,
-    _SYMB,
-    _MOVE,
-    _FUNC,
+    BASE,
+    SYMB,
+    MOVE,
+    FUNC,
 };
 
 enum planck_keycodes {
-    SYMB = SAFE_RANGE,
-    MOVE,
-    FUNC,
-
-    ampr,
-    astr,
+    ampr = SAFE_RANGE,
     at,
     bsls,
     circ,
-    dlr,
-    eql,
     exlm,
     grv,
-    hash,
     lbrc,
     lcbr,
-    lprn,
     perc,
     pipe,
     plus,
     rbrc,
     rcbr,
-    rprn,
     tild,
 };
 
+enum td_keycodes {
+    _hash,
+    _astr,
+    _eql,
+    _rprn,
+    _lprn,
+    _dlr,
+};
+
+typedef enum {
+  SINGLE_TAP,
+  SINGLE_HOLD,
+  DOUBLE_SINGLE_TAP,
+} td_state_t;
+
+static td_state_t td_state;
+
+int cur_dance (qk_tap_dance_state_t *state);
+
+void hash_finished(qk_tap_dance_state_t *state, void *user_data);
+void hash_reset(qk_tap_dance_state_t *state, void *user_data);
+void astr_finished(qk_tap_dance_state_t *state, void *user_data);
+void astr_reset(qk_tap_dance_state_t *state, void *user_data);
+void eql_finished(qk_tap_dance_state_t *state, void *user_data);
+void eql_reset(qk_tap_dance_state_t *state, void *user_data);
+void rprn_finished (qk_tap_dance_state_t *state, void *user_data);
+void rprn_reset(qk_tap_dance_state_t *state, void *user_data);
+void lprn_finished(qk_tap_dance_state_t *state, void *user_data);
+void lprn_reset(qk_tap_dance_state_t *state, void *user_data);
+void dlr_finished(qk_tap_dance_state_t *state, void *user_data);
+void dlr_reset(qk_tap_dance_state_t *state, void *user_data);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_planck_grid(
+    [BASE] = LAYOUT_planck_grid(
          tab,    q,    w,    f,    p,    g,    j,    l,    u,    y, scln, mins,
-        bspc,    a, r_lc, s_la, t_lg,    d,    h, n_rg, e_ra, i_rc,    o, quot,
+        bspc,    a,    r,    s,    t,    d,    h,    n,    e,    i,    o, quot,
         lsft,    z,    x,    c,    v,    b,    k,    m, comm,  dot, slsh, rsft,
-        XXXX, XXXX, XXXX, FUNC, MOVE,  ent,  spc, SYMB, FUNC, XXXX, XXXX, XXXX
+        xxxx, xxxx, xxxx, func, move,  ent,  spc, symb, func, xxxx, xxxx, xxxx
     ),
 
-    [_SYMB] = LAYOUT_planck_grid(
+    [SYMB] = LAYOUT_planck_grid(
          esc,   n7,   n5,   n3,   n1,   n9,   n8,   n0,   n2,   n4,   n6, dash,
          del, bsls, hash, astr,  eql, pipe,   at, rprn, lprn,  dlr, ampr,  gbp,
         caps,  grv, exlm, lbrc, rbrc, circ, tild, rcbr, lcbr, plus, perc, caps,
         ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____
     ),
 
-    [_MOVE] = LAYOUT_planck_grid(
-         esc, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, home,   up,  end, XXXX, XXXX,
-         del, XXXX, lctl, lalt, lgui, XXXX, XXXX, left, down, rght, XXXX, XXXX,
-        ____, XXXX, XXXX, back,  fwd, XXXX, XXXX, pgdn, pgup, XXXX, XXXX, ____,
+    [MOVE] = LAYOUT_planck_grid(
+         esc, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, home,   up,  end, xxxx, xxxx,
+         del, lsft, lctl, lalt, lgui, xxxx, xxxx, left, down, rght, xxxx, xxxx,
+        ____, xxxx, xxxx, back,  fwd, xxxx, xxxx, pgdn, pgup, xxxx, xxxx, ____,
         ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____
     ),
 
-    [_FUNC] = LAYOUT_planck_grid(
+    [FUNC] = LAYOUT_planck_grid(
         rset,   f1,   f2,   f3,   f4,   f5,   f6,   f7,   f8,   f9,  f10, volu,
         powr,  f11,  f12,  f13,  f14,  f15,  f16,  f17,  f18,  f19,  f20, vold,
-        ____, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, ____,
-        ____, ____, ____, ____, prev, mute, play, next, ____, ____, ____, ____
+        ____, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, play, next, prev, mute, ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____
     ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case SYMB:
-            if (record->event.pressed) {
-                layer_on(_SYMB);
-            } else {
-                layer_off(_SYMB);
-            }
-            return false;
-        case MOVE:
-            if (record->event.pressed) {
-                layer_on(_MOVE);
-            } else {
-                layer_off(_MOVE);
-            }
-            return false;
-        case FUNC:
-            if (record->event.pressed) {
-                layer_on(_FUNC);
-            } else {
-                layer_off(_FUNC);
-            }
-            return false;
-
         // Override the defualt auto shifted symbols to use SEND_STRING
         // See https://github.com/qmk/qmk_firmware/issues/4072
         case exlm:
@@ -212,12 +214,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case at:
             if (record->event.pressed) { SEND_STRING("@"); }
-            return false;
-        case hash:
-            if (record->event.pressed) { SEND_STRING("#"); }
-            return false;
-        case dlr:
-            if (record->event.pressed) { SEND_STRING("$"); }
             return false;
         case perc:
             if (record->event.pressed) { SEND_STRING("%"); }
@@ -228,15 +224,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case ampr:
             if (record->event.pressed) { SEND_STRING("&"); }
             return false;
-        case astr:
-            if (record->event.pressed) { SEND_STRING("*"); }
-            return false;
-        case lprn:
-            if (record->event.pressed) { SEND_STRING("("); }
-            return false;
-        case rprn:
-            if (record->event.pressed) { SEND_STRING(")"); }
-            return false;
         case tild:
             if (record->event.pressed) { SEND_STRING("~"); }
             return false;
@@ -245,9 +232,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case plus:
             if (record->event.pressed) { SEND_STRING("+"); }
-            return false;
-        case eql:
-            if (record->event.pressed) { SEND_STRING("="); }
             return false;
         case pipe:
             if (record->event.pressed) { SEND_STRING("|"); }
@@ -270,3 +254,188 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+int cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) {
+            return SINGLE_TAP;
+        } else {
+            return SINGLE_HOLD;
+        }
+    } else if (state->count == 2) {
+        return DOUBLE_SINGLE_TAP;
+    } else {
+        return 3;
+    }
+}
+
+void hash_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            SEND_STRING("#");
+            break;
+        case SINGLE_HOLD:
+            register_mods(MOD_BIT(KC_LCTL));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            SEND_STRING("##");
+            break;
+    }
+}
+
+void hash_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            break;
+        case SINGLE_HOLD:
+            unregister_mods(MOD_BIT(KC_LCTL));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            break;
+    }
+}
+
+void astr_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            SEND_STRING("*");
+            break;
+        case SINGLE_HOLD:
+            register_mods(MOD_BIT(KC_LALT));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            SEND_STRING("**");
+            break;
+    }
+}
+
+void astr_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            break;
+        case SINGLE_HOLD:
+            unregister_mods(MOD_BIT(KC_LALT));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            break;
+    }
+}
+
+void eql_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            SEND_STRING("=");
+            break;
+        case SINGLE_HOLD:
+            register_mods(MOD_BIT(KC_LGUI));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            SEND_STRING("==");
+            break;
+    }
+}
+
+void eql_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            break;
+        case SINGLE_HOLD:
+            unregister_mods(MOD_BIT(KC_LGUI));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            break;
+    }
+}
+
+void rprn_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            SEND_STRING(")");
+            break;
+        case SINGLE_HOLD:
+            register_mods(MOD_BIT(KC_RGUI));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            SEND_STRING("))");
+            break;
+    }
+}
+
+void rprn_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            break;
+        case SINGLE_HOLD:
+            unregister_mods(MOD_BIT(KC_RGUI));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            break;
+    }
+}
+
+void lprn_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            SEND_STRING("(");
+            break;
+        case SINGLE_HOLD:
+            register_mods(MOD_BIT(KC_RALT));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            SEND_STRING("((");
+            break;
+    }
+}
+
+void lprn_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            break;
+        case SINGLE_HOLD:
+            unregister_mods(MOD_BIT(KC_RALT));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            break;
+    }
+}
+
+void dlr_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case SINGLE_TAP:
+            SEND_STRING("$");
+            break;
+        case SINGLE_HOLD:
+            register_mods(MOD_BIT(KC_RCTL));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            SEND_STRING("$$");
+            break;
+    }
+}
+
+void dlr_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_TAP:
+            break;
+        case SINGLE_HOLD:
+            unregister_mods(MOD_BIT(KC_RCTL));
+            break;
+        case DOUBLE_SINGLE_TAP:
+            break;
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [_hash] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, hash_finished, hash_reset),
+    [_astr] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, astr_finished, astr_reset),
+    [_eql] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, eql_finished, eql_reset),
+    [_rprn] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rprn_finished, rprn_reset),
+    [_lprn] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lprn_finished, lprn_reset),
+    [_dlr] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dlr_finished, dlr_reset),
+};
