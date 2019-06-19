ee3ce06b5fbc4f806150f217b795782d00377d59
Richard Baptist <rpbaptist@gmail.com>
2019-06-19 22:23:08 +0200

    Enable qmk-dfu bootloader

--- keyboards/crkbd/keymaps/rpbaptist/rules.mk
+++ keyboards/crkbd/keymaps/rpbaptist/rules.mk
@@ -23,6 +23,6 @@ SWAP_HANDS_ENABLE  = no      # Enable one-hand typing
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE   = no    # Breathing sleep LED during USB suspend
 
-# BOOTLOADER       = qmk-dfu
+BOOTLOADER       = qmk-dfu
 
 OLED_DRIVER_ENABLE = yes
