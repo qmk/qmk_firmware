e385d9450139a6efaecb9c24e9c832398aa78e5d
Ki Finnsson <kifinnsson@gmail.com>
2019-06-19 11:18:41 -0600

    Keymap Cleanup
    Further clean up of my keymap
    Updates to readme.md to explain how my nonstandard layout works. Note: I really don't recommend anyone base their layout on my work as it's a nonstandard mix of ISO and ANSI.

--- keyboards/dz60/keymaps/kifinnsson/keymap.c
+++ keyboards/dz60/keymaps/kifinnsson/keymap.c
@@ -34,32 +34,32 @@ enum my_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	LAYOUT_all(
-		KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,     KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC,
-		KC_TAB,  KC_Q,    KC_W,    KC_F,   KC_P,   KC_G,   KC_J,    KC_L,     KC_U,   KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-		MO(1),   KC_A,    KC_R,    KC_S,   KC_T,   KC_D,   KC_H,    KC_N,     KC_E,   KC_I,    KC_O,    KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   MO(2),   KC_K,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSFT, KC_NO,
-		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, MO(1),    KC_NO,  MO(3),   KC_RCTL),
+		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
+		KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,   KC_U,     KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
+		MO(1),   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,   KC_E,     KC_I,    KC_O,    KC_QUOT, KC_ENT,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(2),   KC_K,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, KC_RSFT, XXXXXXX,
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, MO(1),  XXXXXXX,  MO(3),   KC_RCTL),
 
 	LAYOUT_all(
-		KC_GRV,  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL,
-		KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,   KC_NO,   KC_NO,
-		KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PENT, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,
-		KC_TRNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-		KC_TRNS, KC_P0,   KC_PDOT, KC_PENT, KC_PENT, KC_PENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET),
+		KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
+		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		_______, KI_ININ, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
+		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,,
+		_______, _______, XXXXXXX, KC_ENT,  KC_ENT,  KC_ENT,  _______, _______, _______, _______, RESET),
 
 	LAYOUT_all(
 		KI_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  WG_PHON, KI_BKSP,
-		KC_NO,   KI_1,    KI_2,    KI_3,    KI_4,    KI_5,    KI_6,    KI_7,    KI_8,    KI_9,    KI_10,   KI_11,   KI_12,   KI_BSLS,
-		KC_CAPS, KC_NO,   KC_NO,   KI_SIG,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KI_KI2,  KC_TRNS, KI_KI,   KC_TRNS, KI_WLFT, KI_WRGT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO,  KC_NO),
+		XXXXXXX, KI_1,    KI_2,    KI_3,    KI_4,    KI_5,    KI_6,    KI_7,    KI_8,    KI_9,    KI_10,   KI_11,   KI_12,   KI_BSLS,
+		KC_CAPS, XXXXXXX, XXXXXXX, KI_SIG,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,,
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KI_KI2,  _______, KI_KI,   _______, KI_WLFT, KI_WRGT, _______, _______, _______, _______,
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX,),
 
 	LAYOUT_all(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -80,7 +80,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false; // Skip all further processing of this key
     case KI_ESC:
       if (record->event.pressed) {
-        SEND_STRING("PUSH-OVER-SNOW-SEEDS");
+        SEND_STRING("");
       } else {
         
       }
--- keyboards/dz60/keymaps/kifinnsson/readme.md
+++ keyboards/dz60/keymaps/kifinnsson/readme.md
@@ -1,4 +1,8 @@
-# DZ60
+# DZ60:kifinnsson
+
+kifinnsson layout
+-----------------
+Note: This is a constant work in progress for my daily driver, I do not recommend you use it as this is a ISO keyboard on the left and an ANSI keyboard on the right. On this mod-shifted colemak layout row 4 has an extra key which is placed between "B" and "K" which is used for a macro layer switch. This is a mod-shift and the dead key was a perfect use for another dedicated layer key. If you build one with my wonkyness feel free to use it as a template. If you didn't build as I did you are not going to have a good time. I may do a cleaned up version to publish to upstream but it will be a while. 
 
 ![dz60](https://cdn.shopify.com/s/files/1/1473/3902/files/1_6525343b-ee62-47e8-882a-05e316136a3f.jpg?v=1501657073)
 
