24876c3112c5084e4450707429445cd08c1c7fa0
Adam Price <komidore64@gmail.com>
2019-06-19 16:01:38 -0400

    planck - version 3
    - add tap versus hold mechanism for enter and right shift, respectively

--- keyboards/planck/keymaps/komidore64/keymap.c
+++ keyboards/planck/keymaps/komidore64/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-#define K64_VERSION "2"
+#define K64_VERSION "3"
 
 enum custom_keycodes {
     VER_STR = SAFE_RANGE,
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_planck_1x2uC(
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
             KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
             KC_ESC,  KC_CAPS, KC_LGUI, KC_LALT, MO(1),   KC_SPC,           MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [1] = LAYOUT_planck_1x2uC(
