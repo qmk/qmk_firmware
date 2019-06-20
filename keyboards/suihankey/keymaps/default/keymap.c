3f0f74d45af1a1f750b1d32fec9e822b498b1d6a
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-06-20 13:29:51 +0900

    fix keymap

--- keyboards/suihankey/keymaps/default/keymap.c
+++ keyboards/suihankey/keymaps/default/keymap.c
@@ -33,6 +33,7 @@ enum layers{
 #define KC_CMD_SP    LT(COMMAND,KC_SPC)
 #define KC_CMD_ET    LT(COMMAND,KC_ENTER)
 #define KC_NUM_ALT   LT(NUMBER,KC_LALT)
+#define KC_NUM_BS   LT(NUMBER,KC_BSPC)
 #define KC_SET_CTRL  LT(SETTING,KC_LCTRL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -40,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_Q,  KC_W,  KC_E, KC_R, KC_T,                 KC_Y,  KC_U,  KC_I, KC_O, KC_P,\
           KC_A,  KC_S,  KC_D, KC_F, KC_G,                 KC_H,  KC_J,  KC_K, KC_L, KC_SCLN,\
           KC_Z,  KC_X,  KC_C, KC_V, KC_B,                 KC_N,  KC_M,  KC_COMMA, KC_DOT, KC_SLASH,\
-    KC_SET_CTRL,  KC_NUM_ALT,  KC_CMD_SP,                 KC_CMD_ET,  KC_NUM_ALT,  KC_SET_CTRL   \
+    KC_SET_CTRL,  KC_NUM_ALT,  KC_CMD_SP,                 KC_CMD_ET,  KC_BSPC,  KC_LSFT   \
   ),
   [COMMAND] = LAYOUT( /* Base */
           KC_Q,  KC_W,  KC_E, KC_R, KC_T,                           KC_Y,  KC_U,  KC_I, KC_O, KC_P,\
           KC_A,  KC_S,  KC_D, KC_F, KC_G,                           KC_H,  KC_J,  KC_K, KC_L, KC_SCLN,\
     LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V), LCTL(KC_B),   KC_N,  KC_M,  KC_COMMA, KC_DOT, KC_SLASH,\
-          KC_NO,  KC_NUM_ALT,  KC_CMD_SP,                           KC_CMD_ET,  KC_NUM_ALT,  KC_SET_CTRL   \
+                   KC_NO,  KC_NO,  KC_NO,                           KC_NO,  KC_NO,  KC_NO   \
   ),
   [NUMBER] = LAYOUT( /* Base */
           KC_F1,  KC_F2,  KC_F3, KC_F4, KC_F5,                 KC_P7,  KC_P8,  KC_P9, KC_NO, KC_NO,\
