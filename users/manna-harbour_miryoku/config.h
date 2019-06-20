e398fc21f65faf06fa6f285bbad73c8c4bfb10e7
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2019-06-20 17:07:29 +1000

    First release

---
+++ users/manna-harbour_miryoku/config.h
@@ -0,0 +1,11 @@
+
+// generated from users/manna-harbour_miryoku/miryoku.org
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Recommended for heavy chording.
+#define QMK_KEYS_PER_SCAN 4
