d81fbd190bb323c8693491f29c7f5fe31ad6cc55
Adam Price <komidore64@gmail.com>
2019-06-24 11:07:34 -0400

    planck - version 5
    delete on right pinky for all layers other than base

--- keyboards/planck/keymaps/komidore64/keymap.c
+++ keyboards/planck/keymaps/komidore64/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-#define K64_VERSION "4"
+#define K64_VERSION "5"
 
 enum custom_keycodes {
     VER_STR = SAFE_RANGE,
@@ -31,13 +31,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, XXXXXXX, _______, _______, _______, _______,          MO(3),   KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
     [2] = LAYOUT_planck_1x2uC(
-            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
             _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
             XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
             _______, XXXXXXX, _______, _______, MO(3),   _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
     [3] = LAYOUT_planck_1x2uC(
-            RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, XXXXXXX,
+            RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, KC_DEL,
             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, VER_STR,
             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX,          _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
