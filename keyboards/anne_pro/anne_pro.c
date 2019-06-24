10a40b1ff15ae99b7e4e6ef80fd4c88d8b2442af
Michiel Visser <info@webmichiel.nl>
2019-06-24 22:20:30 +0200

    Remove excess callbacks for LED UART

--- keyboards/anne_pro/anne_pro.c
+++ keyboards/anne_pro/anne_pro.c
@@ -18,52 +18,12 @@
 #include "ch.h"
 #include "hal.h"
 
-/*
- * This callback is invoked when a transmission buffer has been completely
- * read by the driver.
- */
-static void txend1(UARTDriver *uartp) {
-    (void)uartp;
-}
-
-/*
- * This callback is invoked when a transmission has physically completed.
- */
-static void txend2(UARTDriver *uartp) {
-    (void)uartp;
-}
-
-/*
- * This callback is invoked on a receive error, the errors mask is passed
- * as parameter.
- */
-static void rxerr(UARTDriver *uartp, uartflags_t e) {
-    (void)uartp;
-    (void)e;
-}
-
-/*
- * This callback is invoked when a character is received but the application
- * was not ready to receive it, the character is passed as parameter.
- */
-static void rxchar(UARTDriver *uartp, uint16_t c) {
-    (void)uartp;
-    (void)c;
-}
-
-/*
- * This callback is invoked when a receive buffer has been completely written.
- */
-static void rxend(UARTDriver *uartp) {
-    (void)uartp;
-}
-
 static UARTConfig uart_cfg = {
-    .txend1_cb = txend1,
-    .txend2_cb = txend2,
-    .rxend_cb = rxend,
-    .rxchar_cb = rxchar,
-    .rxerr_cb = rxerr,
+    .txend1_cb = NULL,
+    .txend2_cb = NULL,
+    .rxend_cb = NULL,
+    .rxchar_cb = NULL,
+    .rxerr_cb = NULL,
     .speed = 38400,
     .cr1 = 0,
     .cr2 = USART_CR2_LINEN,
