ed3124f4c996cf4dc75bd3650dfc85409b20d2a8
Sterling <503326@MC02VP210HV2H.tld>
2019-06-25 10:07:02 -0700

    Fixed orthodox keymap config.h file
    I hope this one fixes the problem. *sigh*

--- keyboards/orthodox/keymaps/xyverz/config.h
+++ keyboards/orthodox/keymaps/xyverz/config.h
@@ -22,8 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#include "../../rev1/config.h"
-
 /* Use I2C or Serial, not both */
 
 #define USE_SERIAL
