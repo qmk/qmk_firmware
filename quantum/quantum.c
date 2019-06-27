f066d9d2c77c1939cde92dc3be6b7036ff7dae69
Drashna Jaelre <drashna@live.com>
2019-06-27 10:11:53 -0700

    Extend Magic range check to include new magic codes

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -571,7 +571,8 @@ bool process_record_quantum(keyrecord_t *record) {
       return false;
     #endif
     #endif
-    case MAGIC_SWAP_CONTROL_CAPSLOCK ... MAGIC_TOGGLE_NKRO:
+    case MAGIC_SWAP_CONTROL_CAPSLOCK ... MAGIC_TOGGLE_ALT_GUI:
+    case MAGIC_SWAP_LCTL_LGUI ... MAGIC_TOGGLE_CTL_GUI:
       if (record->event.pressed) {
         // MAGIC actions (BOOTMAGIC without the boot)
         if (!eeconfig_is_enabled()) {
