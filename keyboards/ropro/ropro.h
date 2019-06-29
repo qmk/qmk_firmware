712deb9bffeee797da2b97860ac08a41b06d8df2
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2019-06-28 23:06:34 -0500

    major transfer swap of ropro.h and config.h

--- keyboards/ropro/ropro.h
+++ keyboards/ropro/ropro.h
@@ -2,31 +2,18 @@
 
 #include "quantum.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0002
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Kingly_keys
-#define PRODUCT         The_RoPro
-#define DESCRIPTION     Keyboard
-
-/* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 13
-
-/* key matrix pins */
-#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, F1, }
-#define MATRIX_COL_PINS { F0, D1, D0, D4, C6, D7, E6, B4, B5, B3, B2, B6, D2 }
-#define UNUSED_PINS
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+#define LAYOUT( \
+       K10, K20, K30, K40, K50, K60, K70, K80, K90, K100, K110, K120 \
+       K11, K21, K31, K41, K51, K61, K71, K81, K91, K101, K111, K121 \
+	K02, K12, K22, K32, K42, K52, K62, K72, K82, K92, K102, K112, K122 \
+	K03, K13, K23, K33, K43, K53, K63, K73, K83, K93, K103, K113, K123 \
+	K04, K14, K24, K34, K44, K54, K64, K74, K84, K94, K104, K114, K124 \
+	K05, K15, K25, K35, K45, K55, K65, K75, K85, K95, K105, K115, K125 \
+) { \
+       K10, K20, K30, K40, K50, K60, K70, K80, K90, K100, K110, K120 \
+       K11, K21, K31, K41, K51, K61, K71, K81, K91, K101, K111, K121 \
+	K02, K12, K22, K32, K42, K52, K62, K72, K82, K92, K102, K112, K122 \
+	K03, K13, K23, K33, K43, K53, K63, K73, K83, K93, K103, K113, K123 \
+	K04, K14, K24, K34, K44, K54, K64, K74, K84, K94, K104, K114, K124 \
+	K05, K15, K25, K35, K45, K55, K65, K75, K85, K95, K105, K115, K125 \
+}
