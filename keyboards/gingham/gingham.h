096d2b1b6b17170503fdad7746a0f0995af0bc4e
yiancar <yiangosyiangou@cytanet.com.cy>
2019-06-29 19:32:22 +0100

    Update keyboards/gingham/gingham.h
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/gingham/gingham.h
+++ keyboards/gingham/gingham.h
@@ -34,7 +34,7 @@
     { K40, K41, K42, XXX, XXX, XXX, K46, XXX, XXX, XXX, K4A, K4B, K4C, K4D }  \
 }
 
-#define LAYOUT_60_ansi( \
+#define LAYOUT_60_ansi_split_bs_rshift( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K1D, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K2C, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D, \
