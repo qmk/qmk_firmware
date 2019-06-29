0f162741aae264289974cf5c8e58478c5ae88296
jotix <47826561+jotix@users.noreply.github.com>
2019-06-29 08:27:00 -0300

    Update layouts/community/ortho_4x12/jotix/keymap.c
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- layouts/community/ortho_4x12/jotix/keymap.c
+++ layouts/community/ortho_4x12/jotix/keymap.c
@@ -94,7 +94,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void led_set_user(uint8_t usb_led) {
   if (!(IS_LED_ON(usb_led, USB_LED_NUM_LOCK))) {
-    register_code(KC_NUMLOCK);
+    tap_code(KC_NUMLOCK);
     unregister_code(KC_NUMLOCK);
   }
 }
