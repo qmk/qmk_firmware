4428dca87bf4da706521c0b198c63d97f2161693
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2019-06-29 04:14:51 -0500

    Omit Layout for Clarity

--- keyboards/ropro/rules.mk
+++ keyboards/ropro/rules.mk
@@ -63,5 +63,4 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 
-LAYOUTS = LAYOUT
 LAYOUTS_HAS_RGB = no
