6190eafe4f74c79faaafb588997562fe5266382a
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2019-06-28 22:45:37 -0500

    Update rules.mk

--- keyboards/ropro/rules.mk
+++ keyboards/ropro/rules.mk
@@ -57,5 +57,5 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 
-LAYOUTS = default
+LAYOUTS = ortho_6x13
 LAYOUTS_HAS_RGB = no
