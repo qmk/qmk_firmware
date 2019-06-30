af4abec49b7ef2e3591026e3454061250a093d8c
TuCZnak <tucznak@brambora.net>
2019-06-30 10:00:22 +0200

    TuCZnak's KBD75 layout

---
+++ keyboards/kbdfans/kbd75/keymaps/tucznak/config.h
@@ -0,0 +1,46 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#ifdef MANUFACTURER
+#undef MANUFACTURER
+#endif
+#ifdef PRODUCT
+#undef PRODUCT
+#endif
+#ifdef DESCRIPTION
+#undef DESCRIPTION
+#endif
+
+#define MANUFACTURER    Potato Inc.
+#define PRODUCT         Qt3.14
+#define DESCRIPTION     Look, a keyboard!
+
+/* send tap key if no layer key was used even after tap delay */
+#define TAPPING_TERM 250
+#define RETRO_TAPPING
+
+/* turn off RGB when computer sleeps */
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_SLEEP
+#endif
+
+/* number of backlight levels */
+#ifdef BACKLIGHT_LEVELS
+#undef BACKLIGHT_LEVELS
+#endif
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 8
+#endif
+
+/* turn off RGB when computer sleeps */
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_SLEEP
+#endif
+
+/* send tap key if no layer key was used even after tap delay */
+#define TAPPING_TERM 250
+#define RETRO_TAPPING
+
+#endif
