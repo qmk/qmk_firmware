8c498179ed39c027be078150a3c89a1eedbd304d
yiancar <yiangosyiangou@cytanet.com.cy>
2019-07-01 21:11:38 +0100

    [Keyboard] Gingham (#6212)
    * Initial
    
    * Prepare for final release
    
    * Final
    
    * Update keyboards/gingham/matrix.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/gingham/rules.mk
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/gingham/rules.mk
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Update keyboards/gingham/config.h
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/gingham.h
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/hs60/v1/rules.mk
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/info.json
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/info.json
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/info.json
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/gingham.h
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/gingham.h
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Fixing copypastas
    
    * Update keyboards/gingham/info.json
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/info.json
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/info.json
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/gingham/info.json
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * More thingies

--- keyboards/gingham/rules.mk
+++ keyboards/gingham/rules.mk
@@ -1,9 +1,9 @@
-# MCU name
-#MCU = at90usb1286
-MCU = atmega32u4
+SRC = matrix.c \
+      i2c_master.c
 
-# project specific files
-#SRC =
+# MCU name
+MCU = atmega328p
+PROTOCOL = VUSB
 
 # Processor frequency.
 #     This will define a symbol, F_CPU, in all source code files equal to the
@@ -17,7 +17,6 @@ MCU = atmega32u4
 #     reflect the processor speed set externally so that the code can use accurate
 #     software delays.
 F_CPU = 16000000
-
 
 #
 # LUFA specific
@@ -41,37 +40,56 @@ F_USB = $(F_CPU)
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
-# Boot Section
-BOOTLOADER = qmk-dfu
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   atmega32a    bootloadHID
+#
+# This uses usbaspbootloader
+# BOOTLOADER = atmel-dfu
 
-# Do not put the microcontroller into power saving mode
-# when we get USB suspend event. We want it to keep updating
-# backlight effects.
-OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
+# If you don't know the bootloader type, then you can specify the
+# Boot Section Size in *bytes* by uncommenting out the OPT_DEFS line
+#   Teensy halfKay      512
+#   Teensy++ halfKay    1024
+#   Atmel DFU loader    4096
+#   LUFA bootloader     4096
+#   USBaspLoader        2048
+# OPT_DEFS += -DBOOTLOADER_SIZE=4096
+OPT_DEFS += -DBOOTLOADER_SIZE=2048
+
+# Flash program via avrdude, but default command is not suitable.
+# You can use plaid:default:program
+PROGRAM_CMD = avrdude -c usbasp -p m328p -U flash:w:$(BUILD_DIR)/$(TARGET).hex
+
+# disable debug code
+OPT_DEFS = -DDEBUG_LEVEL=0
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
-COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = no        # Console for debug(+400)
+COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
+NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-RGB_MATRIX_ENABLE = yes     # Use RGB matrix
-RAW_ENABLE = yes
+HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
 
-LAYOUTS = 60_ansi_split_bs_rshift
-
-# Experimental features for zealcmd please do no enable
-#RAW_ENABLE = yes
-#USE_KEYMAPS_IN_EEPROM = yes
+# unsupported features for now
+NO_UART = yes
+NO_SUSPEND_POWER_DOWN = yes
+CUSTOM_MATRIX           = yes
