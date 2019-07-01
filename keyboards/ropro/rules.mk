72782ce2d948ed4f3f2b22d1c97ff2188eef2669
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2019-07-01 11:20:48 -0500

    update rules.mk
    add RGB "underglow" function

--- keyboards/ropro/rules.mk
+++ keyboards/ropro/rules.mk
@@ -63,4 +63,5 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 ENCODER_ENABLE = yes
-LAYOUTS_HAS_RGB = no
+RGBLIGHT_ENABLE = yes
+LAYOUTS_HAS_RGB = yes
