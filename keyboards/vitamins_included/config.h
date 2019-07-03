caec28462b54975e311811141d6ea7659c87ab6b
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2019-07-03 23:55:13 +0200

    Add some space saving defines

--- keyboards/vitamins_included/config.h
+++ keyboards/vitamins_included/config.h
@@ -20,5 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
+#ifndef NO_ACTION_MACRO
+    #define NO_ACTION_MACRO
+#endif
+#ifndef NO_ACTION_FUNCTION
+    #define NO_ACTION_FUNCTION
+#endif
+#ifndef NO_DEBUG
+    #define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+    #define NO_PRINT
+#endif // !NO_PRINT
