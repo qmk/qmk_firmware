55b5f9aaca732361e2e27e37a3b2beab602e06d1
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2019-07-03 02:51:15 +0200

    moved rev2 to use split_common

--- keyboards/vitamins_included/rev2/config.h
+++ keyboards/vitamins_included/rev2/config.h
@@ -21,6 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+#define SPLIT_HAND_PIN B4
+#define SOFT_SERIAL_PIN D0
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xBEE5
 #define PRODUCT_ID      0xBEEB
--- keyboards/vitamins_included/rev2/rev2.c
+++ keyboards/vitamins_included/rev2/rev2.c
@@ -8,6 +8,22 @@ void led_set_kb(uint8_t usb_led) {
 }
 #endif
 
+bool is_keyboard_left(void) {
+  #if defined(SPLIT_HAND_PIN)
+    // Test pin SPLIT_HAND_PIN for High/Low, if low it's right hand
+    setPinInputHigh(SPLIT_HAND_PIN);
+    bool x = !readPin(SPLIT_HAND_PIN);
+    setPinInput(SPLIT_HAND_PIN);
+    return x;
+  #elif defined(EE_HANDS)
+    return eeprom_read_byte(EECONFIG_HANDEDNESS);
+  #elif defined(MASTER_RIGHT)
+    return !is_keyboard_master();
+  #endif
+
+  return is_keyboard_master();
+}
+
 void matrix_init_kb(void) {
 	matrix_init_user();
 };
