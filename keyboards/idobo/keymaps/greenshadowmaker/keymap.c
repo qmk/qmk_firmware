a2f42391c3782f7a8846500e72661174fa09b24a
GreenShadowMaker <shadowgit@greenghost.net>
2019-07-04 09:51:58 -0700

    fixed errors and added arrows bottom right to match my other layouts

--- keyboards/idobo/keymaps/greenshadowmaker/keymap.c
+++ keyboards/idobo/keymaps/greenshadowmaker/keymap.c
@@ -18,23 +18,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      | -      |        | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | =      |        | -      | 6      | 7      | 8      | 9      | 0      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | E      | R      | T      |        |        | [      | Y      | U      | I      | O      | P      | ]      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | RAISE  | A      | S      | D      | F      | G      |        | UP     |        | H      | J      | K      | L      | ;      | '      |
+ * | RAISE  | A      | S      | D      | F      | G      |        | UP     | PrtScr | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | LEFT   | DOWN   | RIGHT  | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | `      | \      | LALT   | LCTRL  | LOWER  | SPACE  | LGUI   | DEL    | ENTER  | SPACE  | RAISE  |        |        |        | PrtScr |
+ * | `      | \      | LALT   | LCTRL  | LOWER  | SPACE  | LGUI   | DEL    | ENTER  | SPACE  | RAISE  |  LEFT  |  DOWN  |  UP    | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_5x15( \
-    KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, XXXXXXX,    KC_EQL,   KC_6,    KC_7,    KC_8,       KC_9,     KC_0, KC_BSPC,  \
+    KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQL,  XXXXXXX,   KC_MINS,   KC_6,    KC_7,    KC_8,       KC_9,     KC_0, KC_BSPC,  \
     KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, XXXXXXX,   KC_LBRC,   KC_Y,    KC_U,    KC_I,       KC_O,     KC_P, KC_RBRC,  \
-     RAISE,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,   KC_UP,   XXXXXXX,   KC_H,    KC_J,    KC_K,       KC_L,  KC_SCLN, KC_QUOT, \
+     RAISE,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX,   KC_UP,   KC_PSCR,   KC_H,    KC_J,    KC_K,       KC_L,  KC_SCLN, KC_QUOT, \
    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT, KC_DOWN,   KC_RGHT,   KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_GRAVE, KC_GRAVE, KC_LALT, KC_LCTL,   LOWER,  KC_SPC,    KC_LGUI,  KC_DEL,    KC_ENT, KC_SPC,   RAISE,    XXXXXXX, XXXXXXX,  XXXXXXX, KC_PSCR  \
+  KC_GRAVE,  KC_BSLS, KC_LALT, KC_LCTL,   LOWER,  KC_SPC,    KC_LGUI,  KC_DEL,    KC_ENT, KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,   KC_UP, KC_RIGHT  \
 ),
 
 
