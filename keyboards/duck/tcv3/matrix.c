5628a2140aa45caef386d6c5a7b2d8d439f07178
mechmerlin <mechmerlin@gmail.com>
2019-07-04 13:20:42 -0700

    missed a column in the matrix

--- keyboards/duck/tcv3/matrix.c
+++ keyboards/duck/tcv3/matrix.c
@@ -163,23 +163,24 @@ uint8_t read_fwkey(void)
  *
  * col 0: B4
  * col 1: D7
- * col G: B5
+ * col H: B5
  *
  * col / pin:    PC6  PB6  PF1  PF4  PC7
  * 2:             1    0    0    0    0
- * 3:             1    0    0    1    0
- * 4:             1    0    1    1    0
- * 5:             1    0    0    0    1
- * 6:             1    0    1    0    1
- * 7:             1    0    1    1    1
- * 8:             0    1    0    0    0
- * 9:             0    1    1    0    0
- * A:             0    1    0    1    0
- * B:             0    1    1    1    0
- * C:             0    1    0    0    1
- * D:             0    1    1    0    1
- * E:             0    1    0    1    1
- * F:             0    1    1    1    1
+ * 3:             1    0    1    0    0
+ * 4:             1    0    0    1    0
+ * 5:             1    0    1    1    0
+ * 6:             1    0    0    0    1
+ * 7:             1    0    1    0    1
+ * 8:             1    0    1    1    1
+ * 9:             0    1    0    0    0
+ * A:             0    1    1    0    0
+ * B:             0    1    0    1    0
+ * C:             0    1    1    1    0
+ * D:             0    1    0    0    1
+ * E:             0    1    1    0    1
+ * F:             0    1    0    1    1
+ * G:             0    1    1    1    1
  *
  */
 static void unselect_cols(void) {
@@ -207,58 +208,62 @@ static void select_col(uint8_t col) {
             break;
         case 3:
             PORTC |= 0b01000000;
-            PORTF |= 0b00010000;
+            PORTF |= 0b00000010;
             break;
         case 4:
             PORTC |= 0b01000000;
-            PORTF |= 0b00010010;
+            PORTF |= 0b00010000;
             break;
         case 5:
-            PORTC |= 0b11000000;
+            PORTC |= 0b01000000;
+            PORTF |= 0b00010010;
             break;
         case 6:
             PORTC |= 0b11000000;
-            PORTF |= 0b00000010;
             break;
         case 7:
             PORTC |= 0b11000000;
-            PORTF |= 0b00010010;
+            PORTF |= 0b00000010;
             break;
         case 8:
-            PORTB |= 0b01000000;
+            PORTC |= 0b11000000;
+            PORTF |= 0b00010010;
             break;
         case 9:
             PORTB |= 0b01000000;
-            PORTF |= 0b00000010;
             break;
         case 10:
             PORTB |= 0b01000000;
-            PORTF |= 0b00010000;
+            PORTF |= 0b00000010;
             break;
         case 11:
             PORTB |= 0b01000000;
-            PORTF |= 0b00010010;
+            PORTF |= 0b00010000;
             break;
         case 12:
             PORTB |= 0b01000000;
-            PORTC |= 0b10000000;
+            PORTF |= 0b00010010;
             break;
         case 13:
             PORTB |= 0b01000000;
             PORTC |= 0b10000000;
-            PORTF |= 0b00000010;
             break;
         case 14:
             PORTB |= 0b01000000;
             PORTC |= 0b10000000;
-            PORTF |= 0b00010000;
+            PORTF |= 0b00000010;
             break;
         case 15:
             PORTB |= 0b01000000;
             PORTC |= 0b10000000;
-            PORTF |= 0b00010010;
+            PORTF |= 0b00010000;
             break;
         case 16:
+            PORTB |= 0b01000000;
+            PORTC |= 0b10000000;
+            PORTF |= 0b00010010;
+            break;
+        case 17:
             PORTB |= 0b00100000;
             break;
     }
