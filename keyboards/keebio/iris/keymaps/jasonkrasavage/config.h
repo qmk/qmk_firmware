e628ef429240cffe952047bbd1c908f4ee45ea1f
jasonkrasavage <jkrasavage@colum.edu>
2019-07-05 08:51:05 -0500

    Update config.h

--- keyboards/keebio/iris/keymaps/jasonkrasavage/config.h
+++ keyboards/keebio/iris/keymaps/jasonkrasavage/config.h
@@ -23,6 +23,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef RGBLED_NUM
 
 #define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 90
-#define RGBLIGHT_SAT_STEP 20
-#define RGBLIGHT_VAL_STEP 100
