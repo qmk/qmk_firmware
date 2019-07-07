3a9ee55e7c23ad60b4495b583396fbe43acbfc4d
Peter Grüner <dr-BEat@users.noreply.github.com>
2019-07-07 21:12:00 +0200

    Switched Del and Ctrl on left hand

--- keyboards/dichotomy/keymaps/beat/keymap.c
+++ keyboards/dichotomy/keymaps/beat/keymap.c
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESCAPE,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TT(_MOUSE),
-                                 KC_LGUI, KC_LALT, KC_LCTL,    KC_RCTL, KC_RALT, KC_RGUI,
-               MS_BTN3, SFT_T(KC_ESCAPE), KC_DEL,  KC_SPC,     KC_SPC,  TT(_RAISE), TT(_LOWER),   MS_BTN3
+                                 KC_LGUI, KC_LALT, KC_DEL,     KC_RCTL, KC_RALT, KC_RGUI,
+               MS_BTN3, SFT_T(KC_ESCAPE), KC_LCTL, KC_SPC,     KC_SPC,  TT(_RAISE), TT(_LOWER),   MS_BTN3
   ),
 
   [_RAISE] = LAYOUT( /* Shifted layout, small changes (because angle brackets have been moved to thumb cluster buttons) */
