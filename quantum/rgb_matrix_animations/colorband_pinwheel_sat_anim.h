635c9c7121179ece1c2777655be62e9f854ca65b
Ryan Caltabiano <rcalt2vt@gmail.com>
2019-07-07 15:51:09 -0500

    Fixing effects to respect user sat and val levels

--- quantum/rgb_matrix_animations/colorband_pinwheel_sat_anim.h
+++ quantum/rgb_matrix_animations/colorband_pinwheel_sat_anim.h
@@ -3,7 +3,7 @@ RGB_MATRIX_EFFECT(BAND_PINWHEEL_SAT)
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 static void BAND_PINWHEEL_SAT_math(HSV* hsv, int16_t dx, int16_t dy, uint8_t time) {
-    hsv->s = rgb_matrix_config.sat - time - atan2_8(dy, dx) * 3;
+    hsv->s = scale8(rgb_matrix_config.sat - time - atan2_8(dy, dx) * 3, rgb_matrix_config.sat);
 }
 
 bool BAND_PINWHEEL_SAT(effect_params_t* params) {
