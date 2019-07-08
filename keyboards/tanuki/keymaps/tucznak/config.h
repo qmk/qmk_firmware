62f0e8ff2e2f297175d20ce53dbc2672cfcf4c19
TuCZnak <tucznak@brambora.net>
2019-07-08 22:59:02 +0200

    initial commit

---
+++ keyboards/tanuki/keymaps/tucznak/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#undef MANUFACTURER
+#undef PRODUCT
+#undef DESCRIPTION
+
+#define MANUFACTURER    Potato Inc.
+#define PRODUCT         Trash Panda
+#define DESCRIPTION     Qt3.14 smolkeeb
+
+/* turn off RGB when computer sleeps */
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_SLEEP
+#endif
+
+/* send tap key if no layer key was used even after tap delay */
+#define TAPPING_TERM 50
+#define RETRO_TAPPING
+
+/* disable debug print */
+#define NO_DEBUG
