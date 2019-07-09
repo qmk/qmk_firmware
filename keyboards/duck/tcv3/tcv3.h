202a672c484ae3934ddb4d998b797a656589edda
mechmerlin <mechmerlin@gmail.com>
2019-07-08 22:46:13 -0700

    better layout macro that matches physical layout

--- keyboards/duck/tcv3/tcv3.h
+++ keyboards/duck/tcv3/tcv3.h
@@ -17,34 +17,20 @@
 
 #include "quantum.h"
 
-#define LAYOUT( \
-    K5A, K5B, K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O,     K5Q, \
-    K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M,      K4O, K4P, \
-    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,      K3O, K3P, \
-    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M,      K2O, K2P, \
-    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K,      K1M, K1N, K1O, K1P, \
-    K0A, K0B, K0C,                               K0J, K0K, K0L, K0M, K0N, K0O, K0P  \
-) { \
-  { K5A, K5B, K5C, K5D,   K5E,   K5F,   K5G,   K5H,   K5I,   K5J,   K5K, K5L,   K5M, K5N,   K5O, KC_NO, K5Q }, \
-  { K4A, K4B, K4C, K4D,   K4E,   K4F,   K4G,   K4H,   K4I,   K4J,   K4K, K4L,   K4M, KC_NO, K4O, K4P,   KC_NO    }, \
-  { K3A, K3B, K3C, K3D,   K3E,   K3F,   K3G,   K3H,   K3I,   K3J,   K3K, K3L,   K3M, KC_NO, K3O, K3P,   KC_NO    }, \
-  { K2A, K2B, K2C, K2D,   K2E,   K2F,   K2G,   K2H,   K2I,   K2J,   K2K, K2L,   K2M, KC_NO, K2O, K2P,   KC_NO    }, \
-  { K1A, K1B, K1C, K1D,   K1E,   K1F,   K1G,   K1H,   K1I,   K1J,   K1K, KC_NO, K1M, K1N,   K1O, K1P,   KC_NO    }, \
-  { K0A, K0B, K0C, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, K0J,   KC_NO, K0K, K0L,   K0M, K0N,   K0O, K0P,   KC_NO    }  \
-}
+#define XXX KC_NO
 
-#define LAYOUT_75_ansi( \
-    K5A, K5B, K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O,     K5Q, \
-    K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M,      K4O, K4P, \
-    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,      K3O, K3P, \
-    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L,           K2O, K2P, \
-    K1A, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K,      K1M, K1N, K1O, K1P, \
-    K0A, K0B, K0C,                               K0J, K0K, K0L, K0M, K0N, K0O, K0P  \
+#define LAYOUT( \
+    K00, K01, K02, K04, K05, K06, K07, K09, K0A, K0B, K0C,           K0D, K0E, K0F, K0G, K0H, K0I, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E,      K1G, K1H, K1I, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E,      K2G, K2H, K2I, \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,           K3G, K3H, K3I, \
+    K40, K41, K42,      K44, K45, K46, K47, K48, K49, K4A, K4B, K4C,      K4E, K4F,      K4H,      \
+    K50, K51, K52, K53, K54,                          K5A,      K5C,      K5E, K5F, K5G, K5H, K5I  \
 ) { \
-  { K5A, K5B, K5C, K5D,   K5E,   K5F,   K5G,   K5H,   K5I,   K5J,   K5K, K5L,   K5M, K5N,   K5O, KC_NO, K5Q }, \
-  { K4A, K4B, K4C, K4D,   K4E,   K4F,   K4G,   K4H,   K4I,   K4J,   K4K, K4L,   K4M, KC_NO, K4O, K4P,   KC_NO    }, \
-  { K3A, K3B, K3C, K3D,   K3E,   K3F,   K3G,   K3H,   K3I,   K3J,   K3K, K3L,   K3M, KC_NO, K3O, K3P,   KC_NO    }, \
-  { K2A, K2B, K2C, K2D,   K2E,   K2F,   K2G,   K2H,   K2I,   K2J,   K2K, K2L,   KC_NO, KC_NO, K2O, K2P,   KC_NO    }, \
-  { K1A, KC_NO, K1C, K1D,   K1E,   K1F,   K1G,   K1H,   K1I,   K1J,   K1K, KC_NO, K1M, K1N,   K1O, K1P,   KC_NO    }, \
-  { K0A, K0B, K0C, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, K0J,   KC_NO, K0K, K0L,   K0M, K0N,   K0O, K0P,   KC_NO    }  \
+  { K00, K01, K02, XXX, K04, K05, K06, K07, XXX, K09, K0A, K0B, K0C, K0D, K0E, K0F, K0G, K0H, K0I }, \
+  { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, XXX, K1G, K1H, K1I }, \
+  { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, XXX, K2G, K2H, K2I }, \
+  { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, XXX, XXX, K3G, K3H, K3I }, \
+  { K40, K41, K42, XXX, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, XXX, K4E, K4F, XXX, K4H, XXX }, \
+  { K50, K51, K52, K53, K54, XXX, XXX, XXX, XXX, XXX, K5A, XXX, K5C, XXX, K5E, K5F, K5G, K5H, K5I }, \
 }
