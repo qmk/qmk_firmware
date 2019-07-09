c6e787a46555d68ff4c0946f5b2085fa186d5994
Richard Baptist <rpbaptist@gmail.com>
2019-07-09 16:35:25 +0200

    Apply fixes to own keymap

--- keyboards/crkbd/keymaps/rpbaptist/keymap.c
+++ keyboards/crkbd/keymaps/rpbaptist/keymap.c
@@ -1,12 +1,12 @@
 #include QMK_KEYBOARD_H
-#include "bootloader.h"
-#ifdef PROTOCOL_LUFA
-  #include "lufa.h"
-  #include "split_util.h"
-#endif
-#ifdef SSD1306OLED
-  #include "ssd1306.h"
-#endif
+// #include "bootloader.h"
+// #ifdef PROTOCOL_LUFA
+//   #include "lufa.h"
+//   #include "split_util.h"
+// #endif
+// #ifdef SSD1306OLED
+//   #include "ssd1306.h"
+// #endif
 
 extern keymap_config_t keymap_config;
 
