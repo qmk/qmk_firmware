ffca8a40fc6a04707c3117fcfaacb231ef23d772
Chris Scheib <chris@scheib.io>
2019-07-10 18:32:48 -0400

    format rgbmatrix_layers

--- keyboards/dztech/dz60rgb/rgbmatrix_layers.c
+++ keyboards/dztech/dz60rgb/rgbmatrix_layers.c
@@ -7,20 +7,20 @@ void rgb_matrix_layer_helper (uint8_t red, uint8_t green, uint8_t blue, bool def
 }
 
 void rgb_matrix_indicators_user(void) {
-      uint8_t this_led = host_keyboard_leds();
-      if (!g_suspend_state) {
-        switch (biton32(layer_state)) {
-          case _LAYER1:
-            rgb_matrix_layer_helper(0xFF, 0x00, 0x00, false); break;
-          case _LAYER2:
-            rgb_matrix_layer_helper(0x00, 0xFF, 0x00, false); break;
-          case _LAYER3:
-            rgb_matrix_layer_helper(0x00, 0x00, 0xFF, false); break;
-          case _LAYER4:
-            rgb_matrix_layer_helper(0xFF, 0xFF, 0x00, false); break;
-                                      }
-                            }
-      if ( this_led & (1<<USB_LED_CAPS_LOCK)) {
-            rgb_matrix_set_color(40, 0xFF, 0xFF, 0xFF);
-      }
+    uint8_t this_led = host_keyboard_leds();
+    if (!g_suspend_state) {
+    switch (biton32(layer_state)) {
+        case _LAYER1:
+        rgb_matrix_layer_helper(0xFF, 0x00, 0x00, false); break;
+        case _LAYER2:
+        rgb_matrix_layer_helper(0x00, 0xFF, 0x00, false); break;
+        case _LAYER3:
+        rgb_matrix_layer_helper(0x00, 0x00, 0xFF, false); break;
+        case _LAYER4:
+        rgb_matrix_layer_helper(0xFF, 0xFF, 0x00, false); break;
+                                    }
+                        }
+    if ( this_led & (1<<USB_LED_CAPS_LOCK)) {
+        rgb_matrix_set_color(40, 0xFF, 0xFF, 0xFF);
+    }
 }
