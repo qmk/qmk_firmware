4db966b5854e01537ec03b96c50f6a8930cbd949
Martin Ekberg <martin@mekberg.se>
2019-07-10 16:53:25 +0200

    Add PERMISSIVE_HOLD option, add sleep/lock shortcuts, minor fixes

--- keyboards/dz60/keymaps/mekberg/keymap.c
+++ keyboards/dz60/keymaps/mekberg/keymap.c
@@ -1,10 +1,5 @@
 #include QMK_KEYBOARD_H
 
-// Fillers to make layering more clear
-#define XXXXXX KC_TRNS
-#define ______ KC_NO
-
-
 /*
 DZ60 ISO physical layout
 1u == 8chars
@@ -21,22 +16,47 @@ DZ60 ISO physical layout
 `-------------------------------------------------------------------------------------------------------------------------------------'
 */
 
+// Fillers to make layering more clear
+#define XXXXXX KC_TRNS
+#define ______ KC_NO
+
+// ISO keycodes with Swedish layout names
+#define SE_PLUS KC_MINS         // +
+#define SE_TCK KC_EQL           // Fronttick (´)
+#define SE_DASH KC_SLSH         // -
+#define SE_UML KC_RBRC          // Umlaut (¨)
+#define SE_QUT KC_NUHS          // Singlequote (')
+#define SE_AUML KC_QUOT     	// Ä
+#define SE_ARNG KC_LBRC     	// Å
+#define SE_OUML KC_SCLN     	// Ö
+#define SE_LT KC_NUBS       	// <
+
+// My common shortcuts
+#define MY_PREV KC_MRWD         // Media: Previous
+#define MY_NEXT KC_MFFD         // Media: Next
+#define MY_PLAY KC_MPLY         // Media: Play/Pause
+#define MY_VOLU KC__VOLUP       // Media: Volume Up
+#define MY_VOLD KC__VOLDOWN     // Media: Volume Down
+#define MY_MUTE KC__MUTE        // Media: Mute
+#define MY_SLEP G(A(KC_POWER))  // Mac: Sleep
+#define MY_LOCK C(A(KC_L))      // Mac: Lock
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_60_iso(
 // Default Layer
 //  ,-------------------------------------------------------------------------------------------------------------------------------------.
 //  |  Esc   |    1   |    2   |    3   |    4   |    5   |    6   |    7   |    8   |    9   |    0   |    +   |   ´    |   Backspace    |
-      KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,      KC_BSPC,
+      KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    SE_PLUS, SE_TCK,     KC_BSPC,
 //  |-------------------------------------------------------------------------------------------------------------------------------------|
 //  |    Tab     |    Q    |    W   |    E   |    R   |    T   |    Y   |    U   |    I   |    O   |    P   |   Å    |    ¨   |           |
-     LT(2,KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+     LT(2,KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG, SE_UML,
 //  |--------------------------------------------------------------------------------------------------------------------------|          |
-//  |    LCtrl     |    A   |    S   |     D  |    F   |    G   |    H   |    J   |    K   |    L   |    Ö   |    Å   |    '   |   Enter  |
-      KC_LCTL,       KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_NUHS,  KC_ENT,
+//  |    LCtrl     |    A   |    S   |     D  |    F   |    G   |    H   |    J   |    K   |    L   |    Ö   |    Ä   |    '   |   Enter  |
+      KC_LCTL,       KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SE_OUML, SE_AUML, SE_QUT,   KC_ENT,
 //  |-------------------------------------------------------------------------------------------------------------------------------------|
 //  |  Shift   |   <>   |    Z   |    X   |    C   |    V   |    B   |    N   |    M   |    ,   |    .    |    -   |        RShift        |
-      KC_LSFT,  KC_NUBS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,   KC_SLSH,        KC_RSFT,
+      KC_LSFT,  SE_LT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,   SE_DASH,        KC_RSFT,
 //  |-------------------------------------------------------------------------------------------------------------------------------------|
 //  |  LCtrl   |   LAlt   |  Command |                     Space                              |  Command |   RAlt   |    Fn    |   RCtrl  |
       KC_LCTL,   KC_LALT,   KC_LGUI,                       KC_SPC,                              KC_LGUI,   KC_RALT,    MO(1),    KC_RCTL),
@@ -48,17 +68,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |  Esc   |   F1   |   F2   |   F3   |   F4   |   F5   |   F6   |   F7   |   F8   |   F9   |   F10  |  F11   |  F12   |     Delete     |
       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,      KC_DEL,
 //  |-------------------------------------------------------------------------------------------------------------------------------------|
-//  |            |        |        |        |        |        |        |        |        |        |        |        |        |            |
-        ______,    ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,
-//  |--------------------------------------------------------------------------------------------------------------------------|          |
-//  |   Caps Lock  |        |        |        |        |        |        |        |        |        |        |        |        |          |
-        KC_CAPS,     ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,    ______,
+//  |            |        |        |        |        |        |        |        |        |        | VolUp  |        |        |            |
+        ______,    ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______, MY_VOLU,  ______,  ______,
+//  |--------------------------------------------------------------------------------------------------------------------------|   Lock   |
+//  |   Caps Lock  |        | Sleep  |        |        |        |        |  Prev  |  Play  |  Next  | VolDn  |        |        |  Screen  |
+        KC_CAPS,     ______, MY_SLEP,  ______,  ______,  ______,  ______, MY_PREV, MY_PLAY, MY_NEXT, MY_VOLD,  ______,  ______,  MY_LOCK,
 //  |-------------------------------------------------------------------------------------------------------------------------------------|
 //  |          |        |        |        |        |        |        |        |        |        |        |        |                       |
       ______,   ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,          ______,
 //  |-------------------------------------------------------------------------------------------------------------------------------------|
-//  |          |          |          |                                                        |          |          |          |          |
-      ______,    ______,    ______,                      ______,                                ______,     ______,    XXXXXX,    ______),
+//  |          |          |          |                     Mute                               |          |          |          |          |
+      ______,    ______,    ______,                      MY_MUTE,                               ______,     ______,    XXXXXX,    ______),
 //  `-------------------------------------------------------------------------------------------------------------------------------------'
 
 	[2] = LAYOUT_60_iso(
