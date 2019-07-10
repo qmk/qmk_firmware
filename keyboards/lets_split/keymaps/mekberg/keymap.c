38447a43559093faaf050d01e5b3352c97bc2624
Martin Ekberg <martin@mekberg.se>
2019-07-10 15:24:24 +0200

    Add lock and sleep shortcuts, formatting

--- keyboards/lets_split/keymaps/mekberg/keymap.c
+++ keyboards/lets_split/keymaps/mekberg/keymap.c
@@ -1,72 +1,78 @@
 #include QMK_KEYBOARD_H
 
-#define SYMBOL MO(1)
-#define NUMBER MO(2)
-#define SPECIAL MO(3)
-// Esc => MOVEMENT layer when held
-#define MOVE LT(4, KC_ESC)
-
-// Enter => Shift when held
-#define SFTENT RSFT_T(KC_ENT)
-// Tab => Ctrl when held
-#define CTLTAB LCTL_T(KC_TAB)
-
-// ISO keycodes with Swedish layout names
-#define SE_PLUS KC_MINS     // +
-#define SE_QSTM S(SE_PLUS)  // Question mark
-#define SE_TCK KC_EQL       // Fronttick
-#define SE_BTCK S(SE_TCK)   // Backtick
-#define SE_UML KC_RBRC      // Umlaut (¨)
-#define SE_TAK S(SE_UML)    // Caret (^)
-#define SE_TILD A(SE_UML)   // Tilde (~)
-#define SE_QUT KC_NUHS      // Singlequote
-#define SE_DQUT S(KC_2)     // Doublequote
-#define SE_AST S(SE_QUT)    // Asterisk
-#define SE_AUML KC_QUOT     // Ä
-#define SE_ARNG KC_LBRC     // Å
-#define SE_OUML KC_SCLN     // Ö
-#define SE_LT KC_NUBS       // <
-#define SE_GT S(SE_LT)      // >
-#define SE_DASH KC_SLSH     // -
-#define SE_USCR S(SE_DASH)  // _
-#define AP_SLS S(KC_7)      // /
-#define AP_PIPE LALT(KC_7)  // |
-#define AP_BSLS A(AP_SLS)   // Backslash
-#define AP_LCBR A(AP_LPAR)  // {
-#define AP_RCBR A(AP_RPAR)  // }
-#define AP_LBR A(KC_8)      // [
-#define AP_RBR A(KC_9)      // ]
-#define AP_LPAR S(KC_8)     // (
-#define AP_RPAR S(KC_9)     // )
-#define SE_AT RALT(KC_2)    // @
-#define SE_EXCL S(KC_1)     // !
-#define SE_HASH S(KC_3)     // #
-#define SE_USD RALT(KC_4)   // $
-#define SE_PCNT S(KC_5)     // %
-#define SE_AMP S(KC_6)      // &
-#define SE_EQLS S(KC_0)     // =
-
-// My common shortcuts
-#define MY_PREV KC_MRWD
-#define MY_NEXT KC_MFFD
-#define MY_PLAY KC_MPLY
-#define MY_VOLU KC__VOLUP
-#define MY_VOLD KC__VOLDOWN
-
 /*
 Let's Split physical layout
 1u == 8chars
+* == Homing keys
                              LEFT                                                     RIGHT
    ,-----------------------------------------------------.   ,-----------------------------------------------------.
    |        |        |        |        |        |        |   |        |        |        |        |        |        |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-   |        |        |        |        |*       |        |   |        |*       |        |        |        |        |
+   |        |        |        |        |   *    |        |   |        |   *    |        |        |        |        |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
    |        |        |        |        |        |        |   |        |        |        |        |        |        |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
    |        |        |        |        |        |        |   |        |        |        |        |        |        |
    `-----------------------------------------------------´   `-----------------------------------------------------'
 */
+
+// Layers
+#define SYMBOL MO(1)
+#define NUMBER MO(2)
+#define SPECIAL MO(3)
+#define MOVE LT(4, KC_ESC)      // Esc => MOVEMENT layer when held
+
+// Modifiers
+#define SFTENT RSFT_T(KC_ENT)   // Enter => Shift when held
+#define CTLTAB LCTL_T(KC_TAB)   // Tab => Ctrl when held
+
+// ISO keycodes with Swedish layout names
+#define SE_PLUS KC_MINS         // +
+#define SE_QSTM S(SE_PLUS)      // Question mark
+#define SE_TCK KC_EQL           // Fronttick (´)
+#define SE_BTCK S(SE_TCK)       // Backtick (`)
+#define SE_UML KC_RBRC          // Umlaut (¨)
+#define SE_TAK S(SE_UML)        // Caret (^)
+#define SE_TILD A(SE_UML)       // Tilde (~)
+#define SE_QUT KC_NUHS          // Singlequote (')
+#define SE_DQUT S(KC_2)         // Doublequote (")
+#define SE_AST S(SE_QUT)        // Asterisk
+#define SE_AUML KC_QUOT         // Ä
+#define SE_ARNG KC_LBRC         // Å
+#define SE_OUML KC_SCLN         // Ö
+#define SE_LT KC_NUBS           // <
+#define SE_GT S(SE_LT)          // >
+#define SE_DASH KC_SLSH         // -
+#define SE_USCR S(SE_DASH)      // _
+#define SE_AT RALT(KC_2)        // @
+#define SE_EXCL S(KC_1)         // !
+#define SE_HASH S(KC_3)         // #
+#define SE_USD RALT(KC_4)       // $
+#define SE_PCNT S(KC_5)         // %
+#define SE_AMP S(KC_6)          // &
+#define SE_EQLS S(KC_0)         // =
+
+// Apple-specific layout keys
+#define AP_SLS S(KC_7)          // /
+#define AP_PIPE LALT(KC_7)      // |
+#define AP_BSLS A(AP_SLS)       // Backslash
+#define AP_LCBR A(AP_LPAR)      // {
+#define AP_RCBR A(AP_RPAR)      // }
+#define AP_LBR A(KC_8)          // [
+#define AP_RBR A(KC_9)          // ]
+#define AP_LPAR S(KC_8)         // (
+#define AP_RPAR S(KC_9)         // )
+
+// My common shortcuts
+#define MY_PREV KC_MRWD         // Media: Previous
+#define MY_NEXT KC_MFFD         // Media: Next
+#define MY_PLAY KC_MPLY         // Media: Play/Pause
+#define MY_VOLU KC__VOLUP       // Media: Volume Up
+#define MY_VOLD KC__VOLDOWN     // Media: Volume Down
+#define MY_MUTE KC__MUTE        // Media: Mute
+#define MY_SLEP G(A(KC_POWER)) // Mac: Sleep
+#define MY_LOCK C(A(KC_L))      // Mac: Lock
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -103,11 +109,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
    |   |    |   #    |   %    |   [    |   ]    |   "    |   |        |        |        |        |        |        |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-   |        |   ?    |   \    |   <    |   >    |   /    |   |  Del   |        |        |        |        |        |
+   |        |   ?    |   \    |   <    |   >    |   /    |   |  Del   |XXXXXXXX|        |        |        |        |
    `-----------------------------------------------------´   `-----------------------------------------------------'
 */
 [1] = LAYOUT_ortho_4x12(
-    SE_EQLS, SE_EXCL, SE_AT,   AP_LCBR, AP_RCBR, SE_AMP,      _______, _______, _______, _______, _______, SE_TCK,
+    SE_BTCK, SE_EXCL, SE_AT,   AP_LCBR, AP_RCBR, SE_AMP,      _______, _______, _______, _______, _______, SE_TCK,
     SE_TILD, SE_TAK,  SE_USD,  AP_LPAR, AP_RPAR, SE_QUT,      _______, _______, _______, _______, _______, SE_UML,
     AP_PIPE, SE_HASH, SE_PCNT, AP_LBR,  AP_RBR,  SE_DQUT,     _______, _______, _______, _______, _______, _______,
     _______, SE_QSTM, AP_BSLS, SE_LT,   SE_GT,   AP_SLS,      KC_DEL,  _______, _______, _______, _______, _______
@@ -123,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
    |        |  F9    |  F10   |  F11   |  F12   |        |   |        |   1    |   2    |   3    |   =    |   '    |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-   |        |        |        |        |        |        |   |  Del   |        |   0    |        |        |        |
+   |        |        |        |        |XXXXXXXX|        |   |  Del   |        |   0    |        |        |        |
    `-----------------------------------------------------´   `-----------------------------------------------------'
 */
 [2] = LAYOUT_ortho_4x12(
@@ -137,27 +143,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 3 SPECIAL
                              LEFT                                                     RIGHT
    ,-----------------------------------------------------.   ,-----------------------------------------------------.
-   | Reset  |EEPReset|        |        |        |        |   |        |        | VolUp  |        |        | Backspc|
+   | Reset  |EEPReset|        |        |        |        |   |  Lock  |        | VolUp  |        |        | Backspc|
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
    |        |        |        |        |*       |        |   |        |* Prev  |  Play  |  Next  |        |        |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
    |        |        |        |        |        |        |   |        |        | VolDn  |        |        |        |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-   |        |        |        |        |        |        |   |  Mute  |        |        |        |        |        |
+   |XXXXXXXX|        |        |        |        |        |   |  Mute  |        |        |        |        | Sleep  |
    `-----------------------------------------------------´   `-----------------------------------------------------'
 */
-[3] =  LAYOUT_ortho_4x12(
-    RESET,   EEP_RST, _______, _______, _______, _______,     _______, _______, MY_VOLU, _______, _______, KC_BSPC,
+[3] = LAYOUT_ortho_4x12(
+    RESET,   EEP_RST, _______, _______, _______, _______,     MY_LOCK, _______, MY_VOLU, _______, _______, KC_BSPC,
     _______, _______, _______, _______, _______, _______,     _______, MY_PREV, MY_PLAY, MY_NEXT, _______, _______,
     _______, _______, _______, _______, _______, _______,     _______, _______, MY_VOLD, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,     KC_MUTE, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______,     MY_MUTE, _______, _______, _______, _______, MY_SLEP
 ),
 
 /*
 4 MOVEMENT
                              LEFT                                                     RIGHT
    ,-----------------------------------------------------.   ,-----------------------------------------------------.
-   |        |        |        |        |        |        |   |  Ins   |  Home  |   Up   |  End   |  PgUp  | Backspc|
+   |XXXXXXXX|        |        |        |        |        |   |  Ins   |  Home  |   Up   |  End   |  PgUp  | Backspc|
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
    |        |        |        |        |*       |        |   |        |* Left  |  Down  | Right  |  PgDn  |        |
    |--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
@@ -166,7 +172,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    |        |        |        |        |        |        |   |  Del   |        |        |        |        |        |
    `-----------------------------------------------------´   `-----------------------------------------------------'
 */
-[4] =  LAYOUT_ortho_4x12(
+[4] = LAYOUT_ortho_4x12(
     _______, _______, _______, _______, _______, _______,     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_BSPC,
     _______, _______, _______, _______, _______, _______,     _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
