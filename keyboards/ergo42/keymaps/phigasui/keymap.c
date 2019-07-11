fb0388b2927281412402ef43c8547326309aa2d7
Yuta Ohigashi <ohigashi.yuta@qiita.com>
2019-07-11 11:50:48 +0900

    Change tab & delete key

--- keyboards/ergo42/keymaps/phigasui/keymap.c
+++ keyboards/ergo42/keymaps/phigasui/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_TAB,   KC_Q,     KC_W,   KC_E,    KC_R,     KC_T,    XXXXXXX,    XXXXXXX, KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_BSPC, \
+    XXXXXXX,  KC_Q,     KC_W,   KC_E,    KC_R,     KC_T,    KC_TAB,     KC_BSPC, KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    XXXXXXX, \
     KC_LCTL,  KC_A,     KC_S,   KC_D,    KC_F,     KC_G,    KC_ESC,     KC_ESC,  KC_H,    KC_J,     KC_K,    KC_L,   KC_MINS, KC_ENT,  \
     KC_LSFT,  KC_Z,     KC_X,   KC_C,    KC_V,     KC_B,    KC_ESC,     KC_ESC,  KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
     MO(META), MO(GAME), KC_SPC, KC_RALT, MO(SYMB), KC_LGUI, KC_SPC,     KC_SPC,  KC_RGUI, MO(SYMB), KC_RALT, KC_SPC, KC_SPC,  MO(META) \
