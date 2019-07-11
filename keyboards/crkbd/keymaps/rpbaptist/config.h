96f9fea80771e781f9f66896989fce9caa110120
Richard Baptist <rpbaptist@gmail.com>
2019-07-11 10:14:30 +0200

    Decrease tapping term

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -60,7 +60,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_VAL_STEP 16
 #endif
 
-#define TAPPING_TERM 170
+#define TAPPING_TERM 150
 // #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
