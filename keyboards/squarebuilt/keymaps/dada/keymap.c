d31703804570d052092e4af107ac680fc9b40426
Lance Mercado <Lance@SquareBuilt.com>
2019-07-11 12:05:05 -0400

    key update

--- keyboards/squarebuilt/keymaps/dada/keymap.c
+++ keyboards/squarebuilt/keymaps/dada/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,          KC_W,    KC_E,    KC_R,          KC_T,                      KC_Y,         KC_U,          KC_I,    KC_O,   KC_P,
 	    KC_A,          KC_S,    KC_D,    KC_F,          KC_G,                      KC_H,         KC_J,          KC_K,    KC_L,   KC_SCLN,
 	    KC_Z,          KC_X,    KC_C,    KC_V,          KC_B,                      KC_N,         KC_M,          KC_COMM, KC_DOT, KC_SLSH,
-	    KC_ESC,        KC_LGUI, KC_LALT, LT(2,KC_CAPS), LCTL_T(KC_BSPC),           RSFT(KC_SPC), LT(1,KC_TAB),  TG(4),   KC_DEL, KC_ENT
+	    KC_ESC,        KC_LGUI, KC_LALT, LT(2,KC_CAPS), CTL_T(KC_BSPC),           SFT_T(KC_SPC), LT(1,KC_TAB),  TG(4),   KC_DEL, KC_ENT
 	),
 	[1] = LAYOUT(
         KC_EXLM,       KC_AT,   KC_HASH, KC_DLR,        KC_PERC,                   KC_PLUS,      KC_7,          KC_8,    KC_9,    KC_0,
