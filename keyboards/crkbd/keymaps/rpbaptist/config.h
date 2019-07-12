8d71846372f56e7ca7013d79b56b816574c311ab
Richard Baptist <rpbaptist@gmail.com>
2019-07-12 13:47:23 +0200

    Increase tapping term and enable permissive hold

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -60,8 +60,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_VAL_STEP 16
 #endif
 
-#define TAPPING_TERM 150
-// #define PERMISSIVE_HOLD
+#define TAPPING_TERM 160
+#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #define TAPPING_TOGGLE 2
