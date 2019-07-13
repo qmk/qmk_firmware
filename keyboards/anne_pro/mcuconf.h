09260691e65fe105b62bcfbf62367238c0c0a000
Michiel Visser <info@webmichiel.nl>
2019-07-13 13:50:00 +0200

    Add basic Bluetooth support

--- keyboards/anne_pro/mcuconf.h
+++ keyboards/anne_pro/mcuconf.h
@@ -177,7 +177,7 @@
  * UART driver system settings.
  */
 #define STM32_UART_USE_USART1               FALSE
-#define STM32_UART_USE_USART2               FALSE
+#define STM32_UART_USE_USART2               TRUE
 #define STM32_UART_USE_USART3               TRUE
 #define STM32_UART_USART1_IRQ_PRIORITY      12
 #define STM32_UART_USART2_IRQ_PRIORITY      12
