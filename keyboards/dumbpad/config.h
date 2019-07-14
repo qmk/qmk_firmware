8e9e4da70b59071116ad0aa2d19be2d0bf1e9c18
Chip <imchipwood@gmail.com>
2019-07-14 08:29:21 -0700

    rotary encoder button now connected into column 0, row 3

--- keyboards/dumbpad/config.h
+++ keyboards/dumbpad/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 4
-#define MATRIX_COLS 4
+#define MATRIX_COLS 5
 
 /*
  * Keyboard Matrix Assignments
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS { F4, F5, F6, F7 }
-#define MATRIX_COL_PINS { D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
--- keyboards/dumbpad/keymaps/default/keymap.c
+++ keyboards/dumbpad/keymaps/default/keymap.c
@@ -20,16 +20,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT( /* Base */
-    KC_7,    KC_8,    KC_9,    KC_BSPC, 
-    KC_4,    KC_5,    KC_6,    KC_ESC, 
-    KC_1,    KC_2,    KC_3,    KC_TAB, 
-    MO(SUB), KC_0,    KC_DOT,  KC_ENT
+                KC_7,    KC_8,    KC_9,    KC_BSPC, 
+                KC_4,    KC_5,    KC_6,    KC_ESC, 
+                KC_1,    KC_2,    KC_3,    KC_TAB, 
+    KC_MS_BTN1, MO(SUB), KC_0,    KC_DOT,  KC_ENT
   ),
   [SUB] = LAYOUT( /* Base */
-    KC_7,    KC_8,    KC_9,    RESET, 
-    KC_4,    KC_5,    KC_6,    KC_ESC, 
-    KC_1,    KC_2,    KC_3,    KC_TAB, 
-    KC_TRNS, KC_0,    KC_DOT,  KC_ENT
+                KC_7,    KC_8,    KC_9,    RESET, 
+                KC_4,    KC_5,    KC_6,    KC_ESC, 
+                KC_1,    KC_2,    KC_3,    KC_TAB, 
+    KC_MS_BTN2, KC_TRNS, KC_0,    KC_DOT,  KC_ENT
   ),
 };
 
