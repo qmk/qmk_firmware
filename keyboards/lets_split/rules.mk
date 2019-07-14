f90def4616871f3f21103e8897eae8cc2feaa05e
Andre Cato <andrecato@gmail.com>
2019-07-14 17:09:33 -0400

    Get Lets Split working with new Elite-C controller

--- keyboards/lets_split/rules.mk
+++ keyboards/lets_split/rules.mk
@@ -44,7 +44,8 @@ F_USB = $(F_CPU)
 #     This definition is optional, and if your keyboard supports multiple bootloaders of
 #     different sizes, comment this out, and the correct address will be loaded 
 #     automatically (+60). See bootloader.mk for all options.
-BOOTLOADER = caterina
+#BOOTLOADER = caterina
+BOOTLOADER = atmel-dfu
 
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
