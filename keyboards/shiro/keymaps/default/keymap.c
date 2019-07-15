a658c183cef0a27ba4dbd29636433488d93c95e6
Takuma Shinohara <takuma1201@gmail.com>
2019-07-16 00:43:52 +0900

    Remove unnecessary code

--- keyboards/shiro/keymaps/default/keymap.c
+++ keyboards/shiro/keymaps/default/keymap.c
@@ -24,28 +24,27 @@ enum layer_number {
 #define NUMBER TO(_NUMBER)
 #define CURSOL TO(_CURSOL)
 #define MOUSE  TO(_MOUSE)
-#define CP     M(0)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NUMBER] = LAYOUT( 
-    NUMBER,   CURSOL,   MOUSE,   \
-    KC_P7,    KC_P8,    KC_P9,   \
-    KC_P4,    KC_P5,    KC_P6,   \
-    KC_P1,    KC_P2,    KC_P3,   \
-    KC_P0,    KC_BSPC,  KC_ENT   \
+    NUMBER,   CURSOL,   MOUSE,
+    KC_P7,    KC_P8,    KC_P9,
+    KC_P4,    KC_P5,    KC_P6,
+    KC_P1,    KC_P2,    KC_P3,
+    KC_P0,    KC_BSPC,  KC_ENT
   ),
   [_CURSOL] = LAYOUT( 
-    NUMBER,   CURSOL,   MOUSE,   \
-    KC_HOME,  KC_UP,    KC_PGUP, \
-    KC_LEFT,  KC_ESC,   KC_RIGHT, \
-    KC_END,   KC_DOWN,  KC_PGDN, \
-    KC_DEL,   KC_BSPC,  KC_ENT   \
+    NUMBER,   CURSOL,   MOUSE,
+    KC_HOME,  KC_UP,    KC_PGUP,
+    KC_LEFT,  KC_ESC,   KC_RIGHT,
+    KC_END,   KC_DOWN,  KC_PGDN,
+    KC_DEL,   KC_BSPC,  KC_ENT
   ), 
   [_MOUSE] = LAYOUT( 
-    NUMBER,   CURSOL,   MOUSE, \
-    KC_CUT,   KC_COPY,  KC_PSTE, \
-    KC_UNDO,  KC_FIND,  KC_AGIN, \
-    XXXXXXX,  XXXXXXX,  XXXXXXX, \
-    KC_DEL,   KC_BSPC,  KC_ENT   \
+    NUMBER,   CURSOL,   MOUSE,
+    KC_CUT,   KC_COPY,  KC_PSTE,
+    KC_UNDO,  KC_FIND,  KC_AGIN,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,
+    KC_DEL,   KC_BSPC,  KC_ENT
   ),
 };
