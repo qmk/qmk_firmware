7e1a9151fa16b7ad3778e2aec54e8cd541d9f694
Takuma Shinohara <takuma1201@gmail.com>
2019-07-16 01:33:51 +0900

    removed back slash and not used functions.

--- keyboards/shiro/keymaps/check/keymap.c
+++ keyboards/shiro/keymaps/check/keymap.c
@@ -36,11 +36,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */ 
-    TEST_A1,  TEST_A2,  TEST_A3, \
-    TEST_B1,  TEST_B2,  TEST_B3, \
-    TEST_C1,  TEST_C2,  TEST_C3, \
-    TEST_D1,  TEST_D2,  TEST_D3, \
-    TEST_E1,  TEST_E2,  TEST_E3 \
+    TEST_A1,  TEST_A2,  TEST_A3,
+    TEST_B1,  TEST_B2,  TEST_B3,
+    TEST_C1,  TEST_C2,  TEST_C3,
+    TEST_D1,  TEST_D2,  TEST_D3,
+    TEST_E1,  TEST_E2,  TEST_E3
   )
 };
 
@@ -63,16 +63,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case TEST_E3: if(record->event.pressed){ SEND_STRING("E3"); } break;
   }
   return true;
-}
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
 }
