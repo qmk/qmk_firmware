51d7ddc9c65cf709675fcd8410f1fb694bffbc1d
Michiel Visser <info@webmichiel.nl>
2019-07-15 19:29:38 +0200

    Add the Windows and Arrows layouts

--- keyboards/anne_pro/keymaps/arrow/keymap.c
+++ keyboards/anne_pro/keymaps/arrow/keymap.c
@@ -16,20 +16,20 @@
 
 #include QMK_KEYBOARD_H
 
-/* Default Anne Pro layout, windows style */
+/* Default Anne Pro layout, arrows style */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,
-      KC_LCTL, KC_LWIN, KC_LALT,                   KC_SPACE,                                    KC_RALT, MO(1),   MO(2),   KC_RCTL
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,                     KC_RSFT,
+      KC_LCTL, MO(2),   KC_LALT,                   KC_SPACE,                                    KC_LEFT, KC_DOWN, KC_RGHT, MO(1)
   ),
   [1] = LAYOUT(
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
       XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, APL_RGB, APL_RAT, APL_BRT, APL_MOD, KC_UP,   KC_SLCK, KC_PAUS, KC_HOME, KC_END,  KC_PSCR,
       XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN,          XXXXXXX,
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_DEL,                    _______,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_SLSH,                   _______,
       _______, _______, _______,                   XXXXXXX,                                     _______, _______, _______, _______
   ),
   [2] = LAYOUT(
