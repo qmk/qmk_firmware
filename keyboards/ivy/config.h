1afde3ca330a825636b2e1af4e3d418766c0b9a3
That-Canadian <poole.chris.11@gmail.com>
2019-07-14 17:43:04 -0700

    Removed unneeded define block from SUBPROJECT_rev1 as it is redundant (Thanks drashna ;) )

--- keyboards/ivy/config.h
+++ keyboards/ivy/config.h
@@ -65,7 +65,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_ONESHOT
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
-
-#ifdef SUBPROJECT_rev1
-    #include "rev1/config.h"
-#endif
