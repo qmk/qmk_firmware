b1b7d263d8dc645a97ba3ab3ff03aa1567b2c0a9
Richard Baptist <rpbaptist@gmail.com>
2019-07-16 10:30:45 +0200

    Revert changes
    The eternal battle

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -61,8 +61,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define TAPPING_TERM 160
-#define PERMISSIVE_HOLD
-// #define IGNORE_MOD_TAP_INTERRUPT
+// #define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #define TAPPING_TOGGLE 2
 
