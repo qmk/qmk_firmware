9b89e4ee2c53119298e36eb396129caa1e254aba
Jarrett Drouillard <jarrett@thestyl.us>
2019-07-17 14:44:29 -0400

    kuatsure-planck: add &! keys to lower

--- keyboards/planck/keymaps/kuatsure/keymap.c
+++ keyboards/planck/keymaps/kuatsure/keymap.c
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |  Up  |      |   `  |   <  |   {  |   }  |  _   |      |  |   |
+ * |      |   !  |   &  |  Up  |      |   `  |   <  |   {  |   }  |  _   |      |  |   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | Del  | Left | Down | Right|   ~  |   =  |   (  |   )  |  +   |  :   |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid_wrapper(
-    _______, _______, _______, KC_UP,   _______,  KC_GRV,  _______, ____CRBRACES____, KC_UNDS, _______, KC_PIPE,
+    _______, KC_EXLM, KC_AMPR, KC_UP,   _______,  KC_GRV,  _______, ____CRBRACES____, KC_UNDS, _______, KC_PIPE,
     _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_TILD, KC_EQL,  _____PARENS_____, KC_PLUS, KC_COLN, KC_DQT ,
     _______, KC_VOLD, KC_MRWD, KC_MPLY, KC_MFFD,  KC_VOLU, KC_MINS, ___SQBRACKETS___, _______, KC_QUES, _______,
     _______, KC_MUTE, _______, _______, _______,  _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
