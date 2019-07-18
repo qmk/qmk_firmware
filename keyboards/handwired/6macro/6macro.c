17d382048f8f5bec49c76725b8720915e352cb5d
joaofbmaia <jm@joao-maia.com>
2019-07-18 18:32:04 +0100

    Initial commit for 6macro firmware

---
+++ keyboards/handwired/6macro/6macro.c
@@ -0,0 +1,30 @@
+/* Copyright 2019 joaofbmaia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "6macro.h"
+
+/*led_config_t g_led_config = { {
+  // Key Matrix to LED Index
+  {0, 1, 2},
+  {3, 4, 5}
+}, {
+  // LED Index to Physical Position
+  {0, 0}, {112, 0}, {224, 0}, 
+  {0, 64}, {112,  64 }, {224, 64},
+  {224, 32}, {150, 64}, {75, 64}, {0, 32}
+}, {
+  // LED Index to Flag
+  4, 4, 4, 4, 4, 4, 2, 2, 2, 2
+} };*/
