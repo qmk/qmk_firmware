8b177a6392e6698060edc4e566c5dd6df39edbc2
Collin Diekvoss <cdiekvoss@gmail.com>
2019-07-20 07:32:13 -0500

    Suggested changes

--- keyboards/zj68/zj68.h
+++ keyboards/zj68/zj68.h
@@ -45,7 +45,7 @@
     { K40,  K41,   K42,   KC_NO, KC_NO, KC_NO, K46,  KC_NO, K48,   K49,   K4A,   K4B,   K4C,  K4D,   K4E }  \
 }
 
-#define LAYOUT_65_all( \
+#define LAYOUT_all( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K4E, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K2C, K1E, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K1D, K2E, \
