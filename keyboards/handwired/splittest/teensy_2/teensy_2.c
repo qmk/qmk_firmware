1ccb2270239fbbaab0e4d957a69a32760e67645d
zvecr <git@zvecr.com>
2019-07-20 20:22:51 +0100

    Refactor splittest to support multiple dev boards - update teensy master logic

--- keyboards/handwired/splittest/teensy_2/teensy_2.c
+++ keyboards/handwired/splittest/teensy_2/teensy_2.c
@@ -2,8 +2,13 @@
 
 bool is_keyboard_master(void) {
     // TODO: replace this override once USB host detection is implemented
-    // SPLIT_HAND_PIN Combined with MASTER_LEFT or MASTER_RIGHT, it gives a crude
+    // SPLIT_HAND_PIN Combined with MASTER_LEFT or MASTER_RIGHT, gives a crude
     // way to force teensy to run as slave/master
     setPinInput(SPLIT_HAND_PIN);
+
+#if defined(MASTER_RIGHT)
+    return !readPin(SPLIT_HAND_PIN);
+#else
     return readPin(SPLIT_HAND_PIN);
+#endif
 }
