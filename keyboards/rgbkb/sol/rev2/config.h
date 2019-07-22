40b0ddd425bf09ddbb15a414b5147e98256f10a8
XScorpion2 <rcalt2vt@gmail.com>
2019-07-22 12:43:52 -0500

    Sol Rev 2 Keyboard (#6389)
    * Basic Rev 2 implementation
    
    * Updated LED defines and added Extra encoder support
    
    * Fixed rgb pin assignment
    
    * Physically accurate LED positions
    
    * Single Color Band scrolling left to right effects
    
    * Spirals, Pinwheels, and Documentation....Oh My!
    
    * Spiral effect band thickness adjustments
    
    * Fixing animation spin directions
    
    * Full hand LED positions
    
    * Basic Rev 2 implementation
    
    Updated LED defines and added Extra encoder support
    
    Fixed rgb pin assignment
    
    Physically accurate LED positions
    
    Full hand LED positions
    
    Moving rev2 folder
    
    * RGB Center Point LED position update
    
    * Fixing led config commas
    
    * Fixing led config commas
    
    * fix enter key
    
    * fix enter
    
    * Small changes to default
    
    * update default
    
    * typo fix
    
    * update default
    
    * Fixing defines & led config, turned full hand & extra encoders into rules.mk feature
    
    * Refactored rules.mk to have a post_rules.mk
    
    * Forgot to offset the matrix to led map due to the edge led additions
    
    * Updated LED flags and fixed my keymap
    
    * Update keymap.c
    
    include speed controls for RGB
    
    * Fixing more rules.mk and adding keymap like encoders functionality
    
    * Sol Rev 2 Implementation
    
    * Minor fixes
    
    * Keymap fixes
    
    * Fix Colemak, add lock keys

--- keyboards/rgbkb/sol/rev2/config.h
+++ keyboards/rgbkb/sol/rev2/config.h
@@ -21,23 +21,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x3060
-#define DEVICE_VER      0x0001
+#define DEVICE_VER      0x0002
 #define MANUFACTURER    RGBKB
 #define PRODUCT         Sol
 #define DESCRIPTION     "An RGB, split, ortho-esque keyboard"
 
 /* ws2812 RGB LED */
-#define RGB_DI_PIN B3
+#define RGB_DI_PIN B7
+
+#define BACKLIGHT_LEDS 124
+
+#ifdef FULLHAND_ENABLE
+  #define FULLHAND_LEDS 24
+#else
+  #define FULLHAND_LEDS 0
+#endif
+
+// Underglow / DIY Tent Glow are parallel to the top row leds, no separate define
 
 #ifdef LED_MIRRORED
-  #define RGBLED_NUM 35
+  #define RGBLED_NUM ((BACKLIGHT_LEDS + FULLHAND_LEDS) / 2)
 #else
-  #define RGBLED_NUM 70
+  #define RGBLED_NUM (BACKLIGHT_LEDS + FULLHAND_LEDS)
 #endif
 #define DRIVER_LED_TOTAL  RGBLED_NUM
 
-#define RGB_MATRIX_CENTER { 112, 35 }
+#define RGB_MATRIX_CENTER { 112, 37 }
 
 // Encoder support
+#ifndef EXTRA_ENCODERS_ENABLE
 #define ENCODERS_PAD_A { D2 }
 #define ENCODERS_PAD_B { D6 }
+#else
+#ifdef OLED_DRIVER_ENABLE
+    #error Extra encoders cannot be enabled at the same time as the OLED Driver as they use the same pins.
+#endif
+#define ENCODERS_PAD_A { D2, D1, B0 }
+#define ENCODERS_PAD_B { D6, B1, D0 }
+#endif
