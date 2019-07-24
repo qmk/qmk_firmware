39a4efffd0ac29af89194c8d1bea593db60ddeb2
noroadsleft <xxiinophobia@yahoo.com>
2019-07-24 11:02:28 -0700

    Correct Manufacturer in USB Device Descriptor

--- keyboards/amjkeyboard/amj66/config.h
+++ keyboards/amjkeyboard/amj66/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0xBD66
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    KBDFans
+#define MANUFACTURER    AMJKeyboard
 #define PRODUCT         AMJ66
 #define DESCRIPTION     QMK keyboard firmware for AMJ66
 
