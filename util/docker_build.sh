a1793c78c7efc47851c39d36d6a126538a102609
zvecr <git@zvecr.com>
2019-07-25 21:56:24 +0100

    Fix line escape

--- util/docker_build.sh
+++ util/docker_build.sh
@@ -47,7 +47,7 @@ dir=$(pwd -W 2>/dev/null) || dir=$PWD  # Use Windows path if on Windows
 
 # Run container and build firmware
 docker run --rm -it $usb_args \
-    -w /qmk_firmware/
+    -w /qmk_firmware/ \
 	-v "$dir":/qmk_firmware \
 	-e ALT_GET_KEYBOARDS=true \
 	-e SKIP_GIT="$SKIP_GIT" \
