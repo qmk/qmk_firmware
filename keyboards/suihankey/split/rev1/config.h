312162239a8da4b9987e64377ecb02fb4e75197f
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-07-27 02:51:25 +0900

    fix split

--- keyboards/suihankey/split/rev1/config.h
+++ keyboards/suihankey/split/rev1/config.h
@@ -28,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 5
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 4
 
 /*
  * Keyboard Matrix Assignments
