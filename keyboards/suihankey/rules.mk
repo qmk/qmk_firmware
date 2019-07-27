392227ec7a8f95326df524f72536db2b310ec63f
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-07-27 18:18:41 +0900

    set BOOTMAGIC_ENABLE lite

--- keyboards/suihankey/rules.mk
+++ keyboards/suihankey/rules.mk
@@ -61,7 +61,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = lite    # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
 CONSOLE_ENABLE = no        # Console for debug(+400)
