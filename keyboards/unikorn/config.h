e862371b5dd748d99b4b43f56e51b1569a3bc897
fauxpark <fauxpark@gmail.com>
2019-07-27 10:37:33 +1000

    Remove NO_BACKLIGHT_CLOCK (#6418)

--- keyboards/unikorn/config.h
+++ keyboards/unikorn/config.h
@@ -35,7 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 #define DEBOUNCE 5
 
-#define NO_BACKLIGHT_CLOCK
 #define BACKLIGHT_LEVELS 1
 #ifdef RGBLIGHT_ENABLE
 #define RGBLED_NUM 17
