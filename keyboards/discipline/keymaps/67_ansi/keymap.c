f53c2254863efc692b965bce5268e0cf10a41cc6
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2019-07-28 03:26:24 -0400

    initial commit

--- keyboards/discipline/keymaps/67_ansi/keymap.c
+++ keyboards/discipline/keymaps/67_ansi/keymap.c
@@ -28,7 +28,7 @@
    * |---------------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  Shift  |  U  | Pgdn|
    * |---------------------------------------------------------------------------------------------+
-   * | Ctrl |  Win  |  Alt  |              Space                | RAlt | FN |RCtrl|  L |  D  |  R  |
+   * | Ctrl |  Win  |  Alt  |              Space                | RAlt | FN |||||||  L |  D  |  R  |
    * '---------------------------------------------------------------------------------------------'
    */
 
@@ -36,25 +36,25 @@
    * .---------------------------------------------------------------------------------------------.
    * | ` ~ |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE |PNTSC|
    * |---------------------------------------------------------------------------------------------+
-   * | Tab    |STATC|BRTHE|RNBOW|RESET|     |     |     |     |     |     |PAUSE|     |  \   |     |
+   * | Tab    |     |     |     |RESET|     |     |     |     |     |     |PAUSE|     |  \   |     |
    * |---------------------------------------------------------------------------------------------+
-   * | Caps    |RGBH+|RGBS+|RGBB+|     |     |     |     |     |     |     | INS |    Enter  | HOME|
+   * | Caps    |     |     |     |     |     |     |     |     |     |     | INS |    Enter  | HOME|
    * |---------------------------------------------------------------------------------------------+
-   * | Shift     |RGBH-|RGBS-|RGBB-|     |     |     |     |RGBM-|RGBM+|RGBTG|  Shift  |VOLUP| END |
+   * | Shift     |     |     |     |     |     |     |     |     |     |     |  Shift  |VOLUP| END |
    * |---------------------------------------------------------------------------------------------+
-   * | Ctrl |  Win  |  Alt  |                                   | RAlt| FN |RCtrl|  L  |VOLDN|  R  |
+   * | Ctrl |  Win  |  Alt  |                                   | RAlt| FN |||||||  L  |VOLDN|  R  |
    * '---------------------------------------------------------------------------------------------'
    */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BL] = LAYOUT_68_ansi(
+  [_BL] = LAYOUT_67_ansi(
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_GRV,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,  KC_DEL,
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGUP,
       KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_PGDN,
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                 KC_RALT, MO(_FL), KC_RCTL,  KC_LEFT, KC_DOWN,  KC_RIGHT),
+      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                 KC_RALT, MO(_FL), KC_NO,    KC_LEFT, KC_DOWN,  KC_RIGHT),
 
-  [_FL] = LAYOUT_68_ansi(
+  [_FL] = LAYOUT_67_ansi(
      /* esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc       `~  */
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_PSCR,
      /*  tab      Q       W        E        R        T      Y        U      I        O        P        [         ]        \      delete*/ 
@@ -64,5 +64,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* shift             Z         X        C       V       B       N      M        ,        .        /        shift     up      pg dn*/
       KC_LSFT,          KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS,  KC_RSFT, KC_VOLU,  KC_END,
      /* ctrl     win      alt                              space                    alt      fn       ctrl      left     down     right*/
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_TRNS,                KC_RALT, KC_TRNS, KC_RCTL,  KC_TRNS, KC_VOLD,  KC_TRNS)
+      KC_LCTL, KC_LGUI, KC_LALT,                          KC_TRNS,                KC_RALT, KC_TRNS,  KC_NO,    KC_TRNS, KC_VOLD,  KC_TRNS)
 };
--- keyboards/discipline/keymaps/wkl_ansi_3_right_mods/keymap.c
+++ keyboards/discipline/keymaps/wkl_ansi_3_right_mods/keymap.c
@@ -28,7 +28,7 @@
    * |---------------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  Shift  |  U  | Pgdn|
    * |---------------------------------------------------------------------------------------------+
-   * | Ctrl |  Win  |  Alt  |              Space                | RAlt | FN |RCtrl|  L |  D  |  R  |
+   * |   Ctrl   |    Alt    |              Space                | RAlt | FN |RCtrl|  L |  D  |  R  |
    * '---------------------------------------------------------------------------------------------'
    */
 
@@ -42,19 +42,19 @@
    * |---------------------------------------------------------------------------------------------+
    * | Shift     |RGBH-|RGBS-|RGBB-|     |     |     |     |RGBM-|RGBM+|RGBTG|  Shift  |VOLUP| END |
    * |---------------------------------------------------------------------------------------------+
-   * | Ctrl |  Win  |  Alt  |                                   | RAlt| FN |RCtrl|  L  |VOLDN|  R  |
+   * |   Ctrl   |     Alt   |                                   | RAlt| FN |RCtrl|  L  |VOLDN|  R  |
    * '---------------------------------------------------------------------------------------------'
    */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BL] = LAYOUT_68_ansi(
+  [_BL] = LAYOUT_wkl_ansi_3_right_mods(
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_GRV,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,  KC_DEL,
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGUP,
       KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_PGDN,
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                 KC_RALT, MO(_FL), KC_RCTL,  KC_LEFT, KC_DOWN,  KC_RIGHT),
+      KC_LCTL, KC_NO,   KC_LALT,                          KC_SPC,                 KC_RALT, MO(_FL), KC_RCTL,  KC_LEFT, KC_DOWN,  KC_RIGHT),
 
-  [_FL] = LAYOUT_68_ansi(
+  [_FL] = LAYOUT_wkl_ansi_3_right_mods(
      /* esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc       `~  */
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_PSCR,
      /*  tab      Q       W        E        R        T      Y        U      I        O        P        [         ]        \      delete*/ 
@@ -63,6 +63,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS,  KC_INS,            KC_HOME,
      /* shift             Z         X        C       V       B       N      M        ,        .        /        shift     up      pg dn*/
       KC_LSFT,          KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS,  KC_RSFT, KC_VOLU,  KC_END,
-     /* ctrl     win      alt                              space                    alt      fn       ctrl      left     down     right*/
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_TRNS,                KC_RALT, KC_TRNS, KC_RCTL,  KC_TRNS, KC_VOLD,  KC_TRNS)
+     /* ctrl              alt                              space                    alt      fn       ctrl      left     down     right*/
+      KC_LCTL, KC_NO,   KC_LALT,                          KC_TRNS,                KC_RALT, KC_TRNS, KC_RCTL,  KC_TRNS, KC_VOLD,  KC_TRNS)
 };
