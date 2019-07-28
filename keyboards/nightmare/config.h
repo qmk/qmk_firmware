67503b54245b50fec79b7bd5822557c0f0f0d8cb
codybender <codyfbender@gmail.com>
2019-07-27 23:32:22 -0600

    fixed pins

--- keyboards/nightmare/config.h
+++ keyboards/nightmare/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { B4, B5, D0, D1, D2, D3, F4, F5, F6, F7, B1, B3, B2, B6}
+#define MATRIX_COL_PINS { B4, B5, D3, D2, D1, D0, F4, F5, F6, F7, B1, B3, B2, B6}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
