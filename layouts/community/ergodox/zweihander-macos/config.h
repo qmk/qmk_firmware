6546a6c90dcf6fa898cb6a76267109525b1bdd8b
Nathan Galt <adiabatic@users.noreply.github.com>
2019-07-27 18:35:33 -0700

    zweihander: Add increasingly high DEBOUNCE

---
+++ layouts/community/ergodox/zweihander-macos/config.h
@@ -0,0 +1,6 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef  DEBOUNCE    // I reject your debounce value and substitute my own
+#define DEBOUNCE 20 // 15 (the default) is too low for Kailh Coppers; I get double presses
