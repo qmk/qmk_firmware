e420e76260e47af58b0c4dc44d43b8fd3cf8ee28
fauxpark <fauxpark@gmail.com>
2019-07-30 12:00:38 +1000

    32A has differently named register

--- tmk_core/common/keyboard.c
+++ tmk_core/common/keyboard.c
@@ -133,10 +133,12 @@ void disable_jtag(void) {
 // To use PF4-7, disable JTAG by writing JTD bit twice within four cycles.
 #if (defined(__AVR_AT90USB646__)  || defined(__AVR_AT90USB647__) \
      defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB1287__) \
-     defined(__AVR_ATmega16U4__)  || defined(__AVR_ATmega32U4__)) \
-     defined(__AVR_ATmega32A__)
+     defined(__AVR_ATmega16U4__)  || defined(__AVR_ATmega32U4__))
     MCUCR |= _BV(JTD);
     MCUCR |= _BV(JTD);
+#elif defined(__AVR_ATmega32A__)
+    MCUCSR |= _BV(JTD);
+    MCUCSR |= _BV(JTD);
 #endif
 }
 
