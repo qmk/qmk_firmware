b16c254b03d47f04528ee354b46b461d38629544
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2019-07-30 00:25:54 -0400

    update layouts and keymaps

--- keyboards/discipline/keymaps/wkl_ansi_2_right_mods/keymap.c
+++ keyboards/discipline/keymaps/wkl_ansi_2_right_mods/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,  KC_DEL,
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGUP,
       KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_PGDN,
-      KC_LCTL, KC_NO,   KC_LALT,                          KC_SPC,                 KC_RALT, MO(_FL), KC_NO,    KC_LEFT, KC_DOWN,  KC_RIGHT),
+      KC_LCTL,          KC_LALT,                          KC_SPC,                 KC_RALT, MO(_FL),           KC_LEFT, KC_DOWN,  KC_RIGHT),
 
   [_FL] = LAYOUT_wkl_ansi_2_right_mods(
      /* esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc       `~  */
@@ -64,5 +64,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* shift             Z         X        C       V       B       N      M        ,        .        /        shift     up      pg dn*/
       KC_LSFT,          KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS,  KC_RSFT, KC_VOLU,  KC_END,
      /* ctrl              alt                              space                    alt      fn                 left     down     right*/
-      KC_LCTL, KC_NO,   KC_LALT,                          KC_TRNS,                KC_RALT, KC_TRNS,  KC_NO,    KC_TRNS, KC_VOLD,  KC_TRNS)
+      KC_LCTL,          KC_LALT,                          KC_TRNS,                KC_RALT, KC_TRNS,            KC_TRNS, KC_VOLD,  KC_TRNS)
 };
