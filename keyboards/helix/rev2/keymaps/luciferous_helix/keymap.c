2343198388df025a082052246ac02140e9897dd8
sinofseven <em.s.00001@gmail.com>
2019-07-30 16:08:55 +0900

    fix

--- keyboards/helix/rev2/keymaps/luciferous_helix/keymap.c
+++ keyboards/helix/rev2/keymaps/luciferous_helix/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    JA_CLON, KC_ENT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  JA_ENUN, KC_RSFT, \
-      KC_LALT, KC_LGUI, KC_LGUI, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_LALT, KC_LGUI, KC_LGUI, KC_LGUI, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LGUI, _______, _______, _______, _______ \
       ),
 
   /* Colemak
