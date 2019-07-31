53802dc18654c4055ed9ce1a0b935351a9b1b11a
Yan-Fa Li <yanfali@gmail.com>
2019-07-31 16:57:39 -0700

    Initial Commit

---
+++ keyboards/wonderland/wonderland.c
@@ -0,0 +1,40 @@
+#include "wonderland.h"
+
+void matrix_init_kb(void) {
+	// put your keyboard start-up code here
+	// runs once when the firmware starts up
+	matrix_init_user();
+	led_init_ports();
+};
+
+void matrix_scan_kb(void) {
+	// put your looping keyboard code here
+	// runs every cycle (a lot)
+	matrix_scan_user();
+};
+
+void led_init_ports(void) {
+    // * Set our LED pins as output
+    DDRB |= (1 << 1) | (1 << 2) | (1 << 3);
+}
+
+void led_set_kb(uint8_t usb_led) {
+	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+        DDRB |= (1 << 1);
+    } else {
+        DDRB &= ~(1 << 1);
+    }
+
+    if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+        DDRB |= (1 << 2);
+    } else {
+        DDRB &= ~(1 << 2);
+    }
+
+    if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
+        DDRB |= (1 << 3);
+    } else {
+        DDRB &= ~(1 << 3);
+    }
+	led_set_user(usb_led);
+}
