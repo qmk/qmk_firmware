bbc23f5a5e8431e6f3c4e42d153f38b71bb86fc9
x1- <viva008@gmail.com>
2019-08-01 17:53:06 +0900

    Remove unnecessary codes.

--- keyboards/kudox/keymaps/default/keymap.c
+++ keyboards/kudox/keymaps/default/keymap.c
@@ -1,7 +1,5 @@
 #include QMK_KEYBOARD_H
 
-extern keymap_config_t keymap_config;
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -9,12 +7,6 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _SYMB 1
 #define _NUMB 2
-
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  SYMB,
-  NUMB,
-};
 
 // Shortcut to make keymap more readable
 #define SYM_L   MO(_SYMB)
