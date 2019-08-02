378406b6ec45e0129268ae76af2916eb6188985c
x1- <viva008@gmail.com>
2019-08-02 13:25:01 +0900

    Modified RGB_LED_NUM on kudox_game/rev1/config.h.

--- keyboards/kudox_game/rev1/config.h
+++ keyboards/kudox_game/rev1/config.h
@@ -59,7 +59,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 
-#define RGBLED_NUM 14    // Number of LEDs
+#undef RGBLED_NUM
+#define RGBLED_NUM 7    // Number of LEDs
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
 
 /*
  * Feature disable options
