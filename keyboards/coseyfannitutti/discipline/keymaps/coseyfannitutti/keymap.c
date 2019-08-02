93d2a58b92632f7c1d9f794a71ce88e0d8023baa
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2019-08-02 00:54:56 -0400

    Update keyboards/coseyfannitutti/discipline/keymaps/coseyfannitutti/keymap.c
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/coseyfannitutti/discipline/keymaps/coseyfannitutti/keymap.c
+++ keyboards/coseyfannitutti/discipline/keymaps/coseyfannitutti/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    MO(_FL),
       KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                 KC_RALT, MO(_FL),           KC_LEFT, KC_DOWN,  KC_RIGHT),
 
-  [_FL] = LAYOUT_67_ansi(
+  [_FL] = LAYOUT_65_ansi_2_right_mods(
      /* esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc       `~  */
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_PSCR,
      /*  tab      Q       W        E        R        T      Y        U      I        O        P        [         ]        \      delete*/ 
