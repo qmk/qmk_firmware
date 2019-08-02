72f382fc02b3c5c81c54fd2d239d653fe2f8083c
fauxpark <fauxpark@gmail.com>
2019-08-02 13:47:52 +1000

    Remove unused _BOOTLOADER defines

--- keyboards/winkeyless/bface/rules.mk
+++ keyboards/winkeyless/bface/rules.mk
@@ -37,7 +37,7 @@ RGBLIGHT_ENABLE = yes
 RGBLIGHT_CUSTOM_DRIVER = yes
 
 OPT_DEFS = -DDEBUG_LEVEL=0
-OPT_DEFS += -DBOOTLOADER_SIZE=2048
+BOOTLOADER = bootloadHID
 
 # custom matrix setup
 SRC = i2c_master.c
