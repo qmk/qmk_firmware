9b06d24b030e17982186c8331a2a84fde114814a
Branden Byers <branden@brandenbyers.com>
2019-08-01 22:58:38 -0700

    Remove the slash

--- keyboards/ckeys/thedora/keymaps/default/keymap.c
+++ keyboards/ckeys/thedora/keymaps/default/keymap.c
@@ -8,7 +8,6 @@ enum layers {
   _ADMIN                // admin duties
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //           ┌─────────┬─────────┬─────────┬─────────┬─────────┐
@@ -30,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // └─────────┴─────────┴─────────┴─────────┴─────────┴─────────┘
 
   [_BASE] = LAYOUT(
-    KC_KP_7, KC_KP_8,   KC_KP_9,     KC_PSLS, MO(_ADMIN), MU_TOG, \
-    KC_KP_4, KC_KP_5,   KC_KP_6,     KC_PAST, TG(_MOUSE),             \
-    KC_KP_1, KC_KP_2,   KC_KP_3,     KC_PMNS, TG(_MIDI),              \
-    KC_KP_0, KC_KP_DOT, KC_KP_EQUAL, KC_PPLS, MO(_MUSIC)              \
+    KC_KP_7, KC_KP_8,   KC_KP_9,     KC_PSLS, MO(_ADMIN), MU_TOG,
+    KC_KP_4, KC_KP_5,   KC_KP_6,     KC_PAST, TG(_MOUSE),
+    KC_KP_1, KC_KP_2,   KC_KP_3,     KC_PMNS, TG(_MIDI),
+    KC_KP_0, KC_KP_DOT, KC_KP_EQUAL, KC_PPLS, MO(_MUSIC)
   ),
 
 // MUSIC LAYER
@@ -56,10 +55,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //└─────────┴─────────┴─────────┴─────────┴─────────┴─────────┘
 
   [_MUSIC] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,            \
-    _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, MU_MOD,            \
-    KC_LCTL, KC_LALT, _______, KC_LGUI, _______ \
+    _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, MU_MOD,
+    KC_LCTL, KC_LALT, _______, KC_LGUI, _______
   ),
 
 // MIDI LAYER
@@ -82,10 +81,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // └─────────┴─────────┴─────────┴─────────┴─────────┴─────────┘
 
   [_MIDI] = LAYOUT(
-    MI_Ds_3, MI_E_3,  MI_F_3, MI_Fs_3, MI_G_3, _______,            \
-    MI_As_2, MI_B_2,  MI_C_3, MI_Cs_3, MI_D_3, \
-    MI_F_2,  MI_Fs_2, MI_G_2, MI_Gs_2, TG(_MIDI),            \
-    MI_C_2,  MI_Cs_2, MI_D_2, MI_Ds_2, MI_E_2 \
+    MI_Ds_3, MI_E_3,  MI_F_3, MI_Fs_3, MI_G_3, _______,
+    MI_As_2, MI_B_2,  MI_C_3, MI_Cs_3, MI_D_3,
+    MI_F_2,  MI_Fs_2, MI_G_2, MI_Gs_2, TG(_MIDI),
+    MI_C_2,  MI_Cs_2, MI_D_2, MI_Ds_2, MI_E_2
   ),
 
 // MOUSE LAYER
@@ -108,10 +107,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // └─────────┴─────────┴─────────┴─────────┴─────────┴─────────┘
 
   [_MOUSE] = LAYOUT(
-    KC_MS_BTN5, _______,       KC_MS_WH_UP,   _______, _______, _______,        \
-    _______,    KC_MS_BTN1,    KC_MS_UP,      KC_MS_BTN2, TG(_MOUSE),   \
-    KC_MS_BTN4, KC_MS_LEFT,    KC_MS_DOWN,    KC_MS_RIGHT,_______,   \
-    KC_MS_BTN3, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, _______ \
+    KC_MS_BTN5, _______,       KC_MS_WH_UP,   _______,        _______,    _______,
+    _______,    KC_MS_BTN1,    KC_MS_UP,      KC_MS_BTN2,     TG(_MOUSE),
+    KC_MS_BTN4, KC_MS_LEFT,    KC_MS_DOWN,    KC_MS_RIGHT,    _______,
+    KC_MS_BTN3, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, _______
   ),
 
 // ADMIN LAYER
@@ -134,10 +133,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // └─────────┴─────────┴─────────┴─────────┴─────────┴─────────┘
 
   [_ADMIN] = LAYOUT(
-    RESET,   _______, _______, _______, TG(_ADMIN), _______, \
-    _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______,  _______, \
-    _______, _______, _______, _______,   _______ \
+    RESET,   _______, _______, _______, TG(_ADMIN), _______,
+    _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______
   ),
 };
 
