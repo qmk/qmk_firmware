78eeb6cd16f53ff04e8a3add98802123e774afa1
Joel Challis <git@zvecr.com>
2019-08-02 21:35:35 +0100

    Update Atreus to current code conventions (#5849)
    * Update atreus to current code conventions - add multi revision instead of defines
    
    * Remove config.h duplication from user keymaps
    
    * Add breaking change log
    
    * Add missing pragma once

--- keyboards/atreus/teensy2/config.h
+++ keyboards/atreus/teensy2/config.h
@@ -14,4 +14,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "atreus.h"
+#pragma once
+
+#include "config_common.h"
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+*/
+#define MATRIX_ROW_PINS { D0, D1, D2, D3 }
+#define MATRIX_COL_PINS { F6, F5, F4, B7, B6, B5, B4, B3, B2, B1, B0 }
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
