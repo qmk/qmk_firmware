177af104c353cc810d3ab3db402b9dff08ececd9
Chip <imchipwood@gmail.com>
2019-08-04 01:22:08 -0700

    changing default keymap slightly, added config.h for default layout

--- keyboards/dumbpad/keymaps/default/keymap.c
+++ keyboards/dumbpad/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_7,      KC_8,    KC_9,     KC_BSPC, 
                    KC_4,      KC_5,    KC_6,     KC_ESC, 
                    KC_1,      KC_2,    KC_3,     KC_TAB, 
-    KC_BTN1,       MO(_SUB),  KC_0,    KC_DOT,   KC_ENTER
+    KC_BTN1,       TT(_SUB),  KC_0,    KC_DOT,   KC_ENTER
   ),
   /*
         SUB LAYER
@@ -111,9 +111,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
       default:
         // other layers - volume up (CW) and down (CCW)
         if (clockwise) {
-          tap_code(KC_VOLU);
+          tap_code(KC_EQL);
         } else {
-          tap_code(KC_VOLD);
+          tap_code(KC_MINS);
         }
         break;
     }
