b54064df374e85746ac4efa9c450cc8248bf4534
Brandon Schlack <brandonschlack@gmail.com>
2019-08-05 12:24:07 -0700

    [Keymap] Initial RoMac keymap layer setup

--- keyboards/romac/keymaps/brandonschlack/keymap.c
+++ keyboards/romac/keymaps/brandonschlack/keymap.c
@@ -17,22 +17,45 @@
 #include QMK_KEYBOARD_H
 #include "brandonschlack.h"
 
-#define _BASE 0
-#define _FN1 1
+#define _NUMPAD 0
+#define _FN 1
+#define _NUMLOCK 2
+#define _MACRO 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[_BASE] = LAYOUT(
+	[_NUMPAD] = LAYOUT(
 		KC_P7, KC_P8, KC_P9, \
 		KC_P4, KC_P5, KC_P6, \
 		KC_P1, KC_P2, KC_P3, \
-		LT(_FN1, KC_NLCK), KC_P0, KC_PENT \
+		OSL(_FN), KC_P0, KC_PDOT \
 	),
 
-	[_FN1] = LAYOUT(
-		KC_TRNS, KC_HOME, KC_PGUP, \
-		KC_TRNS, KC_END, KC_PGDN, \
-		KC_TRNS, KC_TRNS, KC_TRNS, \
-		KC_TRNS, KC_TRNS, KC_PDOT \
+	[_FN] = LAYOUT(
+		KC_PSLS, KC_PAST, KC_BSPC, \
+		KC_TRNS, KC_TRNS, KC_PMNS, \
+		TG(_NUMLOCK), DF(_MACRO), KC_PPLS, \
+		KC_TRNS, DF(_MAGIC), KC_PENT \
+	),
+
+    [_NUMLOCK] = LAYOUT(
+		KC_HOME, KC_UP, KC_PGUP, \
+		KC_LEFT, CMD_TAB, KC_RGHT, \
+		KC_END, KC_DOWN, KC_PGDN, \
+		DF(_NUMPAD), KC_INS, KC_DEL  \
+	),
+
+    [_MACRO] = LAYOUT(
+		KC_F13, KC_F14, KC_F15, \
+		KC_F16, KC_F17, KC_F18, \
+		KC_F19, KC_F20, KC_F21, \
+		LT(_MAGIC,KC_F22), KC_F23, KC_F24  \
+	),
+
+    [_MAGIC] = LAYOUT(
+		KC_MAKE, XXXXXXX, XXXXXXX, \
+		XXXXXXX, XXXXXXX, XXXXXXX, \
+		DF(_NUMPAD), DF(_NUMLOCK), DF(_MACRO), \
+		KC_TRNS, XXXXXXX, XXXXXXX  \
 	)
 };
