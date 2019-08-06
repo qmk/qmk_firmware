c03294bddc5d6c8bc7255067cb5eee48a27f458b
kuchosauronad0 <22005492+kuchosauronad0@users.noreply.github.com>
2019-08-06 15:45:34 -0700

    added comments, restructure

--- users/kuchosauronad0/leader.c
+++ users/kuchosauronad0/leader.c
@@ -20,47 +20,58 @@ void matrix_scan_user(void){
   LEADER_DICTIONARY() {
     leading = false;
     leader_end();
+    // T is for TMUX
+    // Q is for OS related things
+    // other single key sequences are mostly for terminals and vim
 
-    SEQ_TWO_KEYS(KC_Q, KC_Q) {
-      // switch to layer L_A Adjust
-      // layer_on(L_A);
-    }
-    SEQ_TWO_KEYS(KC_Q, KC_C) {
-      // switch to layer L_A Adjust
-      // layer_on(L_C);
-    }
-    SEQ_TWO_KEYS(KC_Q, KC_D) {
-      // switch to layer L_A Adjust
-      // layer_on(L_D);
-    }
-    SEQ_ONE_KEY(KC_T) {
-      tmux_prefix();
-      register_code(KC_RIGHT);
-      unregister_code(KC_RIGHT);
-    }
     SEQ_ONE_KEY(KC_W) {
       // Vim + Tmux Macro, when in command mode in vim: write to file, change to the other pane in the current session and repeat the last command
       SEND_STRING(":w" SS_TAP(X_ENTER));
       tmux_pane_switch_repeat();
       }
-    SEQ_THREE_KEYS(KC_D, KC_D, KC_S) {
-      SEND_STRING("https://start.duckduckgo.com"SS_TAP(X_ENTER));
+    
+    SEQ_ONE_KEY(KC_T) {
+      // Send the Tmux Prefix and press 'right' arrow
+      tmux_prefix();
+      register_code(KC_RIGHT);
+      unregister_code(KC_RIGHT);
     }
-    SEQ_THREE_KEYS(KC_BSPC, KC_BSPC, KC_BSPC){
-        reset_keyboard();
+
+    SEQ_TWO_KEYS(KC_T, KC_T) {
+      // Send the Tmux Prefix to a nested session
+      tmux_prefix();
+      tmux_prefix();
     }
-    SEQ_TWO_KEYS(KC_S, KC_P){
+
+    SEQ_TWO_KEYS(KC_Q, KC_A) {
+      // Switch to layer L_A Adjust
+      layer_on(L_A);
+    }
+
+    SEQ_TWO_KEYS(KC_Q, KC_P){
+      // Press windows key, send string 'plex' and press enter
       register_code(KC_LGUI);
       register_code(KC_S);
       unregister_code(KC_S);
       unregister_code(KC_LGUI);
       SEND_STRING("plex" SS_TAP(X_ENTER));
     }
-    SEQ_TWO_KEYS(KC_A, KC_S) {
+
+    SEQ_TWO_KEYS(KC_Q, KC_S) {
+      // Open a search
       register_code(KC_LGUI);
       register_code(KC_S);
       unregister_code(KC_S);
       unregister_code(KC_LGUI);
+    }
+
+    SEQ_THREE_KEYS(KC_Q, KC_Q, KC_S) {
+      SEND_STRING("https://start.duckduckgo.com"SS_TAP(X_ENTER));
+    }
+
+    SEQ_THREE_KEYS(KC_BSPC, KC_BSPC, KC_BSPC){
+        // Reset the keyboard
+        reset_keyboard();
     }
   }
 
