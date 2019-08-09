55cfb65d583cf12315ca2dd4043e75788098f946
Konstantin Đorđević <konstantin.djordjevic@tradecore.com>
2019-08-09 03:48:35 +0200

    Remove placeholder comments in regular.h and rgb.h

--- keyboards/doro67/rgb/rgb.h
+++ keyboards/doro67/rgb/rgb.h
@@ -17,14 +17,6 @@
 
 #include "quantum.h"
 
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
 #define LAYOUT( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, \
