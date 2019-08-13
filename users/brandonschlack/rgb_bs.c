08e5865b784c93efd2e0b4b5b2b3018b2c4a210b
Brandon Schlack <brandonschlack@gmail.com>
2019-08-13 10:56:34 -0700

    [User] Initial rgb_theme test

--- users/brandonschlack/rgb_bs.c
+++ users/brandonschlack/rgb_bs.c
@@ -10,12 +10,17 @@ extern led_config_t g_led_config;
 #endif
 
 void keyboard_post_init_rgb(void) {
+    set_rgb_theme(rgb_granite);
     layer_state_set_user(layer_state);
 #if defined(RGBLIGHT_ENABLE)
     rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 #elif defined(RGB_MATRIX_ENABLE)
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
 #endif
+}
+
+void set_rgb_theme(rgb_theme_t theme) {
+    user_rgb_theme = theme;
 }
 
 #ifdef RGB_MATRIX_ENABLE
@@ -58,41 +63,98 @@ void rgb_layer_helper(uint8_t hue, uint8_t sat, uint8_t val) {
 }
 #endif
 
+HSV get_rgb_theme_color(uint8_t index, rgb_theme_t theme) {
+    // uint8_t MAX = sizeof(theme.colors)/sizeof(theme.colors[0]);
+    // if (index < MAX) {
+    //     rgb_theme_color_t color = theme.colors[index];
+    //     return color;
+    // }
+    rgb_theme_color_t color = theme.colors[index];
+    return color;
+};
+
+void rgb_theme_layer(layer_state_t state, rgb_theme_t rgb_theme) {
+    uint8_t rgb_color_index = biton32(state);
+    // uint8_t MAX = sizeof(rgb_theme.colors)/sizeof(rgb_theme.colors[0]);
+    HSV color = get_rgb_theme_color(rgb_color_index, rgb_theme);
+
+    // if (index < MAX) {
+    //     color = *(rgb_theme.colors[index]);
+    // } else if (rgb_color_index == _MAGIC) {
+    //     color = { HSV_THEME_M };
+    // }
+    rgb_layer_helper( color.h, color.s, color.v );
+//     switch (biton32(state)) {
+// #ifndef IS_MACROPAD
+//         case _MACFN:
+//             rgb_layer_helper(HSV_THEME_1);
+//             break;
+// #else
+//         case _REEDER:
+//             rgb_layer_helper(HSV_THEME_1);
+//             break;
+//         case _MEDIA:
+//             rgb_layer_helper(HSV_THEME_2);
+//             break;
+//         case _KEYPAD:
+//             rgb_layer_helper(HSV_THEME_3);
+//             break;
+// #endif
+//         case _MAGIC:
+//             rgb_layer_helper(HSV_THEME_M);
+//             break;
+//         default:
+//             switch (biton32(default_layer_state)) {
+//     #ifndef IS_MACROPAD
+//                 case _MAC:
+//                     rgb_layer_helper(HSV_THEME_0);
+//                     break;
+//     #else
+//                 case _NAVI:
+//                     rgb_layer_helper(HSV_THEME_0);
+//                     break;
+//     #endif
+//             }
+//             break;
+//     }
+}
+
 layer_state_t layer_state_set_rgb(layer_state_t state) {
     #if defined(RGBLIGHT_ENABLE) || defined(RGB_MATRIX_ENABLE)
-    switch (biton32(state)) {
-#ifndef IS_MACROPAD
-        case _MACFN:
-            rgb_layer_helper(HSV_THEME_1);
-            break;
-#else
-        case _REEDER:
-            rgb_layer_helper(HSV_THEME_1);
-            break;
-        case _MEDIA:
-            rgb_layer_helper(HSV_THEME_2);
-            break;
-        case _KEYPAD:
-            rgb_layer_helper(HSV_THEME_3);
-            break;
-#endif
-        case _MAGIC:
-            rgb_layer_helper(HSV_THEME_M);
-            break;
-        default:
-            switch (biton32(default_layer_state)) {
-    #ifndef IS_MACROPAD
-                case _MAC:
-                    rgb_layer_helper(HSV_THEME_0);
-                    break;
-    #else
-                case _NAVI:
-                    rgb_layer_helper(HSV_THEME_0);
-                    break;
-    #endif
-            }
-            break;
-    }
+//     switch (biton32(state)) {
+// #ifndef IS_MACROPAD
+//         case _MACFN:
+//             rgb_layer_helper(HSV_THEME_1);
+//             break;
+// #else
+//         case _REEDER:
+//             rgb_layer_helper(HSV_THEME_1);
+//             break;
+//         case _MEDIA:
+//             rgb_layer_helper(HSV_THEME_2);
+//             break;
+//         case _KEYPAD:
+//             rgb_layer_helper(HSV_THEME_3);
+//             break;
+// #endif
+//         case _MAGIC:
+//             rgb_layer_helper(HSV_THEME_M);
+//             break;
+//         default:
+//             switch (biton32(default_layer_state)) {
+//     #ifndef IS_MACROPAD
+//                 case _MAC:
+//                     rgb_layer_helper(HSV_THEME_0);
+//                     break;
+//     #else
+//                 case _NAVI:
+//                     rgb_layer_helper(HSV_THEME_0);
+//                     break;
+//     #endif
+//             }
+//             break;
+//     }
+    rgb_theme_layer(state, user_rgb_theme);
 #endif // RGBLIGHT_ENABLE
     return state;
 }
--- users/brandonschlack/rgb_theme.h
+++ users/brandonschlack/rgb_theme.h
@@ -45,4 +45,38 @@
 #if !defined(HSV_THEME_M)
 #   define HSV_THEME_M   HSV_SPRINGGREEN
 #endif
+
+
+typedef enum {
+    LASER = 0,
+    GRANITE
+} rgb_theme_name_t;
+
+typedef HSV rgb_theme_color_t;
+
+typedef struct {
+    rgb_theme_name_t theme;
+    rgb_theme_color_t colors[];
+} rgb_theme_t;
+
+rgb_theme_t user_rgb_theme;
+
+static const HSV laser_purple =  { HSV_LSR_PURPLE };
+static const HSV laser_pink =    { HSV_LSR_PINK };
+static const HSV laser_blue =    { HSV_LSR_BLUE };
+static const HSV laser_cyan =    { HSV_LSR_CYAN };
+static const HSV laser_magenta = { HSV_LSR_MAGENTA };
+static const rgb_theme_t rgb_laser = { LASER, { laser_purple, laser_pink, laser_blue, laser_cyan, laser_magenta } };
+
+static const HSV granite_white =  { HSV_GNT_WHITE };
+static const HSV granite_blue =   { HSV_GNT_BLUE };
+static const HSV granite_red =    { HSV_GNT_RED };
+static const HSV granite_green =  { HSV_GNT_GREEN };
+static const HSV granite_yellow = { HSV_GNT_YELLOW };
+static const rgb_theme_t rgb_granite = { GRANITE, { granite_white, granite_blue, granite_red, granite_green, granite_yellow } };
+
+void set_rgb_theme(rgb_theme_t theme);
+rgb_theme_t get_rgb_theme(void);
+
+HSV get_rgb_theme_color(uint8_t index, rgb_theme_t theme);
 #endif
