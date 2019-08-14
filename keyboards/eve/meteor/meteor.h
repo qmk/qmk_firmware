afc0b2a5eafa13d03faa5bf51aa6112106a78abf
mechmerlin <mechmerlin@gmail.com>
2019-08-13 21:46:18 -0700

    add an appropriate LAYOUT macro

--- keyboards/eve/meteor/meteor.h
+++ keyboards/eve/meteor/meteor.h
@@ -22,18 +22,17 @@
 // The second converts the arguments into a two-dimensional array
 
 #define LAYOUT( \
-  k00, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,  \
-  k10, k11, k12, k13,   k14, k15, k16,   k17, k18, k19, k1A,   k1B, k1C, k1D, \
-  k20, k21, k22, k23,   k24, k25, k26,   k27, k28, k29, k2A,   k2B, k2C,   k2D, \
-  k30, k31, k32, k33,   k34, k35, k36,   k37, k38, k39, k3A,   k3B, k3C,    \
-  k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4A, k4B, k4D, \
-  k50, k51, k52,  k55, k59, k5A, k5B, k5C, k5D \
-
+  k00, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,      \
+  k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, \
+  k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D, \
+  k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C,      \
+  k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4A, k4B, k4D,      \
+  k50, k51, k52, k55, k59, k5A, k5B, k5C, k5D                           \
 ){ \
-  { k00, KC_NO, k02, k03,   k04, k05, k06,   k07, k08, k09, k0A,   k0B, k0C,   k0D  }, \
-  { k10, k11, k12, k13,   k14, k15, k16,   k17, k18, k19, k1A,   k1B, k1C,   k1D }, \
-  { k20, k21, k22, k23,   k24, k25, k26,   k27, k28, k29, k2A,   k2B, k2C,   k2D }, \
-  { k30, k31, k32, k33,   k34, k35, k36,   k37, k38, k39, k3A,   k3B, k3C,   KC_NO }, \
-  { k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4A, k4B, KC_NO, k4D }, \
-  { k50, k51, k52, KC_NO, KC_NO, k55, KC_NO, KC_NO, KC_NO, k59, k5A, k5B, k5C, k5D }, \
+  { k00, KC_NO, k02, k03,   k04,   k05, k06,   k07,   k08,   k09, k0A, k0B, k0C,   k0D   }, \
+  { k10, k11,   k12, k13,   k14,   k15, k16,   k17,   k18,   k19, k1A, k1B, k1C,   k1D   }, \
+  { k20, k21,   k22, k23,   k24,   k25, k26,   k27,   k28,   k29, k2A, k2B, k2C,   k2D   }, \
+  { k30, k31,   k32, k33,   k34,   k35, k36,   k37,   k38,   k39, k3A, k3B, k3C,   KC_NO }, \
+  { k40, k41,   k42, k43,   k44,   k45, k46,   k47,   k48,   k49, k4A, k4B, KC_NO, k4D   }, \
+  { k50, k51,   k52, KC_NO, KC_NO, k55, KC_NO, KC_NO, KC_NO, k59, k5A, k5B, k5C,   k5D   }, \
 }
