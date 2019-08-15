b1bdc5a83d285ea9743ce63583958021aff59eba
kuchosauronad0 <22005492+kuchosauronad0@users.noreply.github.com>
2019-08-14 21:29:35 -0700

    disabled tap dance

--- keyboards/c39/keymaps/kuchosauronad0/rules.mk
+++ keyboards/c39/keymaps/kuchosauronad0/rules.mk
@@ -5,7 +5,7 @@ EXTRAKEY_ENABLE   = yes      # Audio control and System control(+450)
 CONSOLE_ENABLE    = no       # Console for debug(+400)
 COMMAND_ENABLE    = no       # Commands for debug and configuration
 LEADER_ENABLE     = yes      # default is yes
-TAP_DANCE_ENABLE  = yes      #(+1254)
+TAP_DANCE_ENABLE  = no      #(+1254)
 UNICODE_ENABLE    = no       #(+1134)
 UNICODEMAP_ENABLE = yes
 RGBLIGHT_ENABLE   = no
@@ -17,4 +17,4 @@ MACROS_ENABLED    = no
 RGBLIGHT_TWINKLE  = no
 RGBLIGHT_STARTUP_ANIMATION = no
 
-NO_SECRETS = no
+NO_SECRETS = yes
