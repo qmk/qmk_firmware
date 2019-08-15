6593ce26138f7cd88dd13ea65dff1385813c9fc4
George Koenig <gkoenig@coredial.com>
2019-08-15 19:10:19 -0400

    move 'LAYOUT to keymap.c; remove atreus.h

--- keyboards/atreus/keymaps/ridingqwerty/keymap.c
+++ keyboards/atreus/keymaps/ridingqwerty/keymap.c
@@ -30,6 +30,25 @@
                     MODS                   //              LAYERS              //                  MODS           //
 */
 
+#ifdef LAYOUT
+#undef LAYOUT
+#define LAYOUT( \
+    K00,   K01,   K02,   K03,   K04,                 K40,   K41,   K42,   K43,   K44, \
+    K10,   K11,   K12,   K13,   K14,                 K50,   K51,   K52,   K53,   K54, \
+    K20,   K21,   K22,   K23,   K24,                 K60,   K61,   K62,   K63,   K64, \
+    K30,   K31,   K32,   K33,   K34,   K35,   K70,   K71,   K72,   K73,   K74,   K75  \
+) { \
+  { K00, K01, K02, K03, K04, KC_NO }, \
+  { K10, K11, K12, K13, K14, KC_NO }, \
+  { K20, K21, K22, K23, K24, KC_NO }, \
+  { K30, K31, K32, K33, K34, K35   }, \
+  { K44, K43, K42, K41, K40, KC_NO }, \
+  { K54, K53, K52, K51, K50, KC_NO }, \
+  { K64, K63, K62, K61, K60, KC_NO }, \
+  { K75, K74, K73, K72, K71, K70   }  \
+}
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
