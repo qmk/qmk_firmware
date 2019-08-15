5f2b255bb162271e5b6a4b597b60edfcc02dc96f
Florian Didron <florian@111studio.jp>
2019-08-15 15:24:20 +0900

    chore: set rgb suspend to default

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -113,6 +113,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_MATRIX_LED_PROCESS_LIMIT 5
 #define RGB_MATRIX_LED_FLUSH_LIMIT 26
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true
 
 // #define RGBLIGHT_COLOR_LAYER_0 0x00, 0x00, 0xFF
 /* #define RGBLIGHT_COLOR_LAYER_1 0x00, 0x00, 0xFF */
