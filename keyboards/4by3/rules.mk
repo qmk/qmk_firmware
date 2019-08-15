1963abcfa34a7759367618435fcd88fed9885fa0
eliassjogreen <eliassjogreen1@gmail.com>
2019-08-16 00:41:54 +0200

    Added 4by3 keyboard

---
+++ keyboards/4by3/rules.mk
@@ -0,0 +1,11 @@
+MCU 				= atmega32u4
+F_CPU 				= 16000000
+ARCH				= AVR8
+F_USB 				= $(F_CPU)
+OPT_DEFS 			+= -DINTERRUPT_CONTROL_ENDPOINT
+BOOTLOADER 			= caterina
+
+EXTRAKEY_ENABLE 	= yes
+NKRO_ENABLE 		= yes
+CONSOLE_ENABLE		= yes
+COMMAND_ENABLE		= yes
