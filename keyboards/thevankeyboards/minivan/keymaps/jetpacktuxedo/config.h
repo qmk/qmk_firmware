f408a8a102c286feaae863f3852e9772e3d96eb4
Ethan Madden <maddene@madden.ninja>
2019-08-15 14:01:13 -0700

    simplify config.h
    As per @drashna in CR

--- keyboards/thevankeyboards/minivan/keymaps/jetpacktuxedo/config.h
+++ keyboards/thevankeyboards/minivan/keymaps/jetpacktuxedo/config.h
@@ -1,9 +1,4 @@
-#ifndef CONFIG_USER_H
-  #define CONFIG_USER_H
-  #include "../../config.h"
-  #define PERMISSIVE_HOLD
-  #ifdef RGBLIGHT_ENABLE
-    // This is the only effect I care about for this board
-    #define RGBLIGHT_EFFECT_BREATHING
-  #endif
-#endif
+#pragma once
+
+#define PERMISSIVE_HOLD
+#define RGBLIGHT_EFFECT_BREATHING
