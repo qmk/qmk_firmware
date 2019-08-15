0d6b3fb38be9615db09711b8b070ba028b43678a
Richard Baptist <rpbaptist@gmail.com>
2019-08-15 14:07:27 +0200

    Add RETRO_TAPPING

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -54,6 +54,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 135
 // #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
+#define RETRO_TAPPING
 
 #define TAPPING_TOGGLE 2
 
