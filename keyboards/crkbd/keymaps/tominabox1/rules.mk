2a17e3f895e7f0aedc672a57f191e98b62b25cb4
TJ Campie <tom.campie@gmail.com>
2019-08-16 15:34:59 -0500

    Update rules.mk

--- keyboards/crkbd/keymaps/tominabox1/rules.mk
+++ keyboards/crkbd/keymaps/tominabox1/rules.mk
@@ -24,6 +24,8 @@ EXTRAFLAGS += -flto
 
 BOOTLOADER = qmk-dfu
 
+#LOCAL_GLCDFONT = yes
+OLED_DRIVER_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./cornedoug.c \
