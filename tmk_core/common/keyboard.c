a3b68babbd2511843be27693382b77663a061cfb
fauxpark <fauxpark@gmail.com>
2019-08-17 12:43:59 +1000

    Wrap disable_jtag() in an ifndef

--- tmk_core/common/keyboard.c
+++ tmk_core/common/keyboard.c
@@ -130,15 +130,17 @@ static inline bool has_ghost_in_row(uint8_t row, matrix_row_t rowdata)
 #endif
 
 void disable_jtag(void) {
-// To use PF4-7 (PC2-5 on ATmega32A), disable JTAG by writing JTD bit twice within four cycles.
-#if (defined(__AVR_AT90USB646__)  || defined(__AVR_AT90USB647__)  || \
-     defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB1287__) || \
-     defined(__AVR_ATmega16U4__)  || defined(__AVR_ATmega32U4__))
+#ifndef JTAG_ENABLE
+    // To use PF4-7 (PC2-5 on ATmega32A), disable JTAG by writing JTD bit twice within four cycles.
+    #if (defined(__AVR_AT90USB646__)  || defined(__AVR_AT90USB647__)  || \
+         defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB1287__) || \
+         defined(__AVR_ATmega16U4__)  || defined(__AVR_ATmega32U4__))
     MCUCR |= _BV(JTD);
     MCUCR |= _BV(JTD);
-#elif defined(__AVR_ATmega32A__)
+    #elif defined(__AVR_ATmega32A__)
     MCUCSR |= _BV(JTD);
     MCUCSR |= _BV(JTD);
+    #endif
 #endif
 }
 
