00202fb960492ee201e90ba3dfff9c8a41ad0212
kuchosauronad0 <22005492+kuchosauronad0@users.noreply.github.com>
2019-08-16 17:08:14 -0700

    removed unnecessary include

--- keyboards/c39/keymaps/kuchosauronad0/keymap.c
+++ keyboards/c39/keymaps/kuchosauronad0/keymap.c
@@ -19,7 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "quantum.h"
 #include QMK_KEYBOARD_H
 #include "kuchosauronad0.h"
 
