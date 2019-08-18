bfbf918f3c7deb6be9283b4b62704f982ec76db1
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-08-18 15:01:36 +0900

    set LAYOUT_numpad_5x4

--- keyboards/angel17/angel17.h
+++ keyboards/angel17/angel17.h
@@ -25,12 +25,12 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT( \
+#define LAYOUT_numpad_5x4( \
     k00, k01, k02, k03, \
-    k10, k11, k12, k13, \
-    k20, k21, k22, \
-    k30, k31, k32, k33, \
-    k40, k41 \
+    k10, k11, k12, \
+    k20, k21, k22, k13, \
+    k30, k31, k32, \
+    k40, k41, k33 \
 ) \
 { \
     { k00, k10, k20, k31, k41 }, \
