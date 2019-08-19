d40bef5cda246863d20156f9711f37e2943d887b
jonathan.liu <jonathan.liu@massdrop.com>
2019-08-18 21:20:31 -0700

    Fix held key getting stuck when NKRO is toggled

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -647,6 +647,7 @@ bool process_record_quantum(keyrecord_t *record) {
             #endif
             break;
           case MAGIC_TOGGLE_NKRO:
+            clear_keyboard(); // clear first buffer to prevent stuck keys
             keymap_config.nkro = !keymap_config.nkro;
             break;
           default:
