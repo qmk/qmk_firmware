1784d1bfac44a63bf343b6e2098f0cba81d58cb2
skullY <skullydazed@gmail.com>
2019-08-22 13:30:50 -0700

    Add support for passing files at the command line

--- lib/python/qmk/cli/cformat.py
+++ lib/python/qmk/cli/cformat.py
@@ -6,22 +6,24 @@ import subprocess
 from milc import cli
 
 
+@cli.argument('files', nargs='*', help='Filename(s) to format.')
 @cli.entrypoint("Format C code according to QMK's style.")
 def main(cli):
     """Format C code according to QMK's style.
     """
     clang_format = ['clang-format', '-i']
-    code_files = []
-    for dir in ['drivers', 'quantum', 'tests', 'tmk_core']:
-        for dirpath, dirnames, filenames in os.walk(dir):
-            if 'tmk_core/protocol/usb_hid' in dirpath:
-                continue
-            for name in filenames:
-                if name.endswith('.c') or name.endswith('.h') or name.endswith('.cpp'):
-                    code_files.append(os.path.join(dirpath, name))
+    if not cli.args.files:
+        for dir in ['drivers', 'quantum', 'tests', 'tmk_core']:
+            for dirpath, dirnames, filenames in os.walk(dir):
+                if 'tmk_core/protocol/usb_hid' in dirpath:
+                    continue
+                for name in filenames:
+                    if name.endswith('.c') or name.endswith('.h') or name.endswith('.cpp'):
+                        cli.args.files.append(os.path.join(dirpath, name))
 
     try:
-        subprocess.run(clang_format + code_files, check=True)
+        subprocess.run(clang_format + cli.args.files, check=True)
         cli.log.info('Successfully formatted the C code.')
     except subprocess.CalledProcessError:
         cli.log.error('Error formatting C code!')
+        return False
