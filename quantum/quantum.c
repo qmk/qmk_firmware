b62e160a8950f451b08f1fee0109e60a58c5ddaa
Drashna Jaelre <drashna@live.com>
2019-08-21 17:07:49 -0700

    Additional changes for Layer State typedef compatibility (#5906)
    * Additional changes for Layer State typedef compatibility
    
    * Replace biton32 with get_highest_layer in docs
    
    * Change additional layer structure code
    
    * Fix uGFX reference issue
    
    * Remove dynamic_keymap check
    
    * Where did all these extra spaces come from
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -931,9 +931,9 @@ void set_single_persistent_default_layer(uint8_t default_layer) {
   default_layer_set(1U<<default_layer);
 }
 
-uint32_t update_tri_layer_state(uint32_t state, uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  uint32_t mask12 = (1UL << layer1) | (1UL << layer2);
-  uint32_t mask3 = 1UL << layer3;
+layer_state_t update_tri_layer_state(layer_state_t state, uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+  layer_state_t mask12 = (1UL << layer1) | (1UL << layer2);
+  layer_state_t mask3 = 1UL << layer3;
   return (state & mask12) == mask12 ? (state | mask3) : (state & ~mask3);
 }
 
