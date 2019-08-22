c7eede2249d22dd4fabed83043dcf4cc7408cf27
skullY <skullydazed@gmail.com>
2019-08-21 23:46:51 -0700

    run yapf on the code

--- lib/python/qmk/tests/test_qmk_errors.py
+++ lib/python/qmk/tests/test_qmk_errors.py
@@ -1,7 +1,8 @@
 from qmk.errors import NoSuchKeyboardError
 
+
 def test_NoSuchKeyboardError():
     try:
-        raise(NoSuchKeyboardError("test message"))
+        raise NoSuchKeyboardError("test message")
     except NoSuchKeyboardError as e:
         assert e.message == 'test message'
