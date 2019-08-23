f2a05549c4f4799c5ed1bdedfcecb674edd59767
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2019-08-23 02:53:34 +0200

    Removed rev2 check in matrix.c as rev2 uses split_common

--- keyboards/vitamins_included/matrix.c
+++ keyboards/vitamins_included/matrix.c
@@ -131,12 +131,6 @@ uint8_t matrix_cols(void) {
 }
 
 bool has_usb(void) {
-    if(is_rev2()) 
-    {
-        USBCON |= (1 << OTGPADE); //enables VBUS pad
-        _delay_us(5);
-        return (USBSTA & (1<<VBUS));  //checks state of VBUS
-    }
     return UDADDR & _BV(ADDEN); // This will return true if a USB connection has been established
 }
 
