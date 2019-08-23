b5a1e8cfce986ec49171fb27260415486e4a944d
Jarrett Drouillard <jarrett@thestyl.us>
2019-08-23 10:02:07 -0400

    planck-kuatsure: mute > play

--- keyboards/planck/keymaps/kuatsure/keymap.c
+++ keyboards/planck/keymaps/kuatsure/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KT_MTAB, _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_BSPC,
     KT_CESC, _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, KC_QUOT,
     KT_LSFT, _________________QWERTY_L3_________________, _________________QWERTY_R3_M_______________, KC_ENT,
-    KC_MUTE, KC_LEAD, KC_LALT, KC_LGUI, LOWER,    KC_SPC, KC_SPC,    RAISE, KT_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_MPLY, KC_LEAD, KC_LALT, KC_LGUI, LOWER,    KC_SPC, KC_SPC,    RAISE, KT_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Lower
