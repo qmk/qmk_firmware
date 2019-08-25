1b58ae1c757b13066f1cb8665aa50cc607c5485c
mikethetiger <mikepitre@outlook.com>
2019-08-25 18:09:50 -0500

    mikethetiger's milk keymap

--- keyboards/2_milk/keymaps/mikethetiger/rules.mk
+++ keyboards/2_milk/keymaps/mikethetiger/rules.mk
@@ -24,7 +24,7 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #
 BOOTMAGIC_ENABLE  = no	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE  = no	# Mouse keys(+4700)
-EXTRAKEY_ENABLE  = no	# Audio control and System control(+450)
+EXTRAKEY_ENABLE  = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE  = yes	# Console for debug(+400)
 COMMAND_ENABLE  = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE  = no  # Breathing sleep LED during USB suspend
