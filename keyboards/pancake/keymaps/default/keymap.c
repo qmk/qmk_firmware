ed2bab8b06ca635c324d931c22e519d29ef2500e
Spaceman <rionlion100@gmail.com>
2019-08-26 08:05:08 -0400

    Update keyboards/pancake/keymaps/default/keymap.c
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/pancake/keymaps/default/keymap.c
+++ keyboards/pancake/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |   ?  |      | Vol- |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_grid(
+[_LOWER] = LAYOUT_ortho_4x12(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, _______, KC_PIPE,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_VOLU, _______,
