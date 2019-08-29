7efcf4db78475115ca74e50849444641dbd0184e
Drashna Jael're <drashna@live.com>
2019-08-28 17:51:30 -0700

    Fix up based on feedback

--- quantum/process_keycode/process_tap_dance.h
+++ quantum/process_keycode/process_tap_dance.h
@@ -68,7 +68,7 @@ typedef struct {
 #    define ACTION_TAP_DANCE_TOGGLE_LAYER(kc, layer) \
         { .fn = { NULL, qk_tap_dance_dual_role_finished, qk_tap_dance_dual_role_reset }, .user_data = (void *)&((qk_tap_dance_dual_role_t) { kc, layer, layer_invert }), }
 
-#    define ACTION_TAP_DANCE_MOVE_LAYER(kc, layer) ACTION_TAP_DANCE_DUAL_ROLE(kc, layer)
+#    define ACTION_TAP_DANCE_LAYER_MOVE(kc, layer) ACTION_TAP_DANCE_DUAL_ROLE(kc, layer)
 
 #    define ACTION_TAP_DANCE_FN(user_fn) \
         { .fn = {NULL, user_fn, NULL}, .user_data = NULL, }
@@ -78,6 +78,8 @@ typedef struct {
 
 #    define ACTION_TAP_DANCE_FN_ADVANCED_TIME(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, tap_specific_tapping_term) \
         { .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, .user_data = NULL, .custom_tapping_term = tap_specific_tapping_term, }
+
+
 
 extern qk_tap_dance_action_t tap_dance_actions[];
 
