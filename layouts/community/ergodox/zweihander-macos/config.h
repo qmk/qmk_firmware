10ab9d7b0b6f0d1b4a56df76092cdb12e56761d6
Nathan Galt <adiabatic@users.noreply.github.com>
2019-08-29 12:09:46 -0700

    Adjust debounce for Kailh Coppers

--- layouts/community/ergodox/zweihander-macos/config.h
+++ layouts/community/ergodox/zweihander-macos/config.h
@@ -3,4 +3,4 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #undef  DEBOUNCE    // I reject your debounce value and substitute my own
-#define DEBOUNCE 20 // 15 (the default) is too low for Kailh Coppers; I get double presses
+#define DEBOUNCE 22 // 15 (the default) is too low for Kailh Coppers; I get double presses
