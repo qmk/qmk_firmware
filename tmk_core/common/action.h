b624f32f944acdc59dcb130674c09090c5c404cb
skullY <skullydazed@gmail.com>
2019-08-30 11:19:03 -0700

    clang-format changes

--- tmk_core/common/action.h
+++ tmk_core/common/action.h
@@ -24,23 +24,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "action_code.h"
 #include "action_macro.h"
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /* tapping count and state */
 typedef struct {
-    bool    interrupted :1;
-    bool    reserved2   :1;
-    bool    reserved1   :1;
-    bool    reserved0   :1;
-    uint8_t count       :4;
+    bool    interrupted : 1;
+    bool    reserved2 : 1;
+    bool    reserved1 : 1;
+    bool    reserved0 : 1;
+    uint8_t count : 4;
 } tap_t;
 
 /* Key event container for recording */
 typedef struct {
-    keyevent_t  event;
+    keyevent_t event;
 #ifndef NO_ACTION_TAPPING
     tap_t tap;
 #endif
@@ -68,17 +67,17 @@ extern bool disable_action_cache;
 
 /* Code for handling one-handed key modifiers. */
 #ifdef SWAP_HANDS_ENABLE
-extern bool swap_hands;
+extern bool           swap_hands;
 extern const keypos_t hand_swap_config[MATRIX_ROWS][MATRIX_COLS];
-#if (MATRIX_COLS <= 8)
-typedef  uint8_t    swap_state_row_t;
-#elif (MATRIX_COLS <= 16)
-typedef  uint16_t   swap_state_row_t;
-#elif (MATRIX_COLS <= 32)
-typedef  uint32_t   swap_state_row_t;
-#else
-#error "MATRIX_COLS: invalid value"
-#endif
+#    if (MATRIX_COLS <= 8)
+typedef uint8_t swap_state_row_t;
+#    elif (MATRIX_COLS <= 16)
+typedef uint16_t swap_state_row_t;
+#    elif (MATRIX_COLS <= 32)
+typedef uint32_t swap_state_row_t;
+#    else
+#        error "MATRIX_COLS: invalid value"
+#    endif
 
 void process_hand_swap(keyevent_t *record);
 #endif
@@ -91,7 +90,7 @@ void unregister_code(uint8_t code);
 void tap_code(uint8_t code);
 void register_mods(uint8_t mods);
 void unregister_mods(uint8_t mods);
-//void set_mods(uint8_t mods);
+// void set_mods(uint8_t mods);
 void clear_keyboard(void);
 void clear_keyboard_but_mods(void);
 void clear_keyboard_but_mods_and_keys(void);
@@ -112,4 +111,4 @@ void debug_action(action_t action);
 }
 #endif
 
-#endif  /* ACTION_H */
+#endif /* ACTION_H */
