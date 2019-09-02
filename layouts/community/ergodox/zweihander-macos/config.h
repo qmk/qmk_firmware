d35b760dcb268c01010650e7422c132a3ec68140
Nathan Galt <adiabatic@users.noreply.github.com>
2019-09-02 15:45:23 -0700

    Disable DEBOUNCE change for Gateron Clears

--- layouts/community/ergodox/zweihander-macos/config.h
+++ layouts/community/ergodox/zweihander-macos/config.h
@@ -5,8 +5,8 @@
 
 // 15,  the default,  is too low for Kailh Coppers. I get lots of double presses.
 // (ba-dum tish)
-#undef  DEBOUNCE
-#define DEBOUNCE 22
+// #undef  DEBOUNCE
+// #define DEBOUNCE 22
 
 // Reduce USB power consumption to 100 mA to make it work with a Lightning to USB 3 Camera Adapter (MK0W2AM/A). ZSA says 100 mA is high enough for the keyboard if you disable the big Shine/Glow LEDs first.
 #undef  USB_MAX_POWER_CONSUMPTION
