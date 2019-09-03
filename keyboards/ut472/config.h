09e5f8ea9f1222bc91d25768eab316028c7d8a93
Annihilator6000 <8xovermsoctober@gmail.com>
2019-09-02 22:54:49 -0400

    UT47.2 keymap/updates for Planck style layout switching

--- keyboards/ut472/config.h
+++ keyboards/ut472/config.h
@@ -57,4 +57,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+#define TAPPING_TERM 175
 #endif
