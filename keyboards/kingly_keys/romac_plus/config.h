80d1c1343638abf22b556d41495113fdbd84a101
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2019-09-03 11:35:32 -0500

    add defined tap_code_delay value

--- keyboards/kingly_keys/romac_plus/config.h
+++ keyboards/kingly_keys/romac_plus/config.h
@@ -33,6 +33,8 @@
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
+#define TAP_CODE_DELAY 10
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
