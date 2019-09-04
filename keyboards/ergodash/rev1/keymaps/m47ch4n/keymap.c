10ad99799eb038eeb905f47d94b51eeaa20a4932
M47CH4N <m47ch4n@gmail.com>
2019-09-04 23:53:48 +0900

    Fix wrong key repeating bug

--- keyboards/ergodash/rev1/keymaps/m47ch4n/keymap.c
+++ keyboards/ergodash/rev1/keymaps/m47ch4n/keymap.c
@@ -98,11 +98,11 @@ void persistent_default_layer_set(uint16_t default_layer) {
 
 #define SEND_STRING_RESTORE(STR) (SHIFT_PRESSED ? SEND_STRING(STR SS_DOWN(X_LSHIFT)) : SEND_STRING(STR SS_UP(X_LSHIFT)))
 
-#define KEY(CODE) (record->event.pressed ? SEND_STRING_RESTORE(SS_DOWN(X_##CODE)) : SEND_STRING_RESTORE(SS_UP(X_##CODE)))
+#define KEY(CODE) (record->event.pressed ? SEND_STRING(SS_DOWN(X_##CODE)) : SEND_STRING_RESTORE(SS_UP(X_##CODE)))
 
-#define KEY_SHIFT(CODE) (record->event.pressed ? SEND_STRING_RESTORE(SS_DOWN(X_LSHIFT) SS_DOWN(X_##CODE)) : SEND_STRING_RESTORE(SS_UP(X_##CODE)))
+#define KEY_SHIFT(CODE) (record->event.pressed ? SEND_STRING(SS_DOWN(X_LSHIFT) SS_DOWN(X_##CODE)) : SEND_STRING_RESTORE(SS_UP(X_##CODE)))
 
-#define KEY_UPSHIFT(CODE) (record->event.pressed ? SEND_STRING_RESTORE(SS_UP(X_LSHIFT) SS_DOWN(X_##CODE)) : SEND_STRING_RESTORE(SS_UP(X_##CODE)))
+#define KEY_UPSHIFT(CODE) (record->event.pressed ? SEND_STRING(SS_UP(X_LSHIFT) SS_DOWN(X_##CODE)) : SEND_STRING_RESTORE(SS_UP(X_##CODE)))
 
 #define SHIFT_DU(CODE_DOWN, CODE_UP) (SHIFT_PRESSED ? CODE_DOWN : CODE_UP)
 #define CASE_US(CODE, US, JP)                   \
