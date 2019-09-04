266b96fac0f3724f3a5fb339cceba003c4bd9b77
Annihilator6000 <8xovermsoctober@gmail.com>
2019-09-04 00:03:46 -0400

    Undo breaking change in config.h

--- keyboards/ut472/config.h
+++ keyboards/ut472/config.h
@@ -57,5 +57,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
-#define TAPPING_TERM 175
 #endif
