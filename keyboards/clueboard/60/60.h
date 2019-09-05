35ffdd7cc885ea4508303a858be2667c62f87fe4
lbibass <notlbibass@gmail.com>
2019-09-05 09:46:09 -0400

    actually fixed the glitch.

--- keyboards/clueboard/60/60.h
+++ keyboards/clueboard/60/60.h
@@ -45,7 +45,7 @@
  * 3 mods on either side of the spacebar.
  */
 #define LAYOUT_aek(\
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,   k0e, \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09,  k0b, k0a, k0c,   k0e, \
      k10,   k11, k12, k13, k14, k15, k16, k17, k18, k19, k1b,k1a,  k1c,   k1e, \
      k20,    k21, k22, k23, k24, k25, k26, k27, k28, k29, k2b,k2a,     k2e, \
        k30,   k32, k33, k34, k35, k36, k37, k38, k39, k3b, k3a,       k3c, \
