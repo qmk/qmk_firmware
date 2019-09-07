3947142fc414fe697839413c2796e2aa43fc62bd
Drashna Jael're <drashna@live.com>
2019-09-07 15:49:20 -0700

    Add QMK DFU bootloader info

--- layouts/community/ortho_4x12/drashna/config.h
+++ layouts/community/ortho_4x12/drashna/config.h
@@ -17,7 +17,7 @@
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_MATRIX_KEYPRESSES  // reacts to keypresses (will slow down matrix scan by a lot)
 // #define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (not recommened)
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 // #define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 // #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
@@ -32,8 +32,12 @@
 #if !defined(KEYBOARD_planck_light)
 #    ifdef RGBLIGHT_ENABLE
 #        define NO_MUSIC_MODE
-#    endif  // RGBLIGHT_ENABLE
-#endif      // KEYBOARD_planck_light
+#    endif                     // RGBLIGHT_ENABLE
+#    define QMK_ESC_OUTPUT E6  // usually COL
+#    define QMK_ESC_INPUT B0   // usually ROW
+#    define QMK_LED D6
+#    define QMK_SPEAKER C6
+#endif  // KEYBOARD_planck_light
 
 #if defined(KEYBOARD_planck)
 #    undef PRODUCT
