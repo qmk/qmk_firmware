1293d0f33eadd5f56a60d7d7aa5ee8438c7a46e0
Annihilator6000 <8xovermsoctober@gmail.com>
2019-09-07 10:56:56 -0400

    Code cleanup

--- keyboards/ut472/keymaps/annihilator6000/keymap.c
+++ keyboards/ut472/keymaps/annihilator6000/keymap.c
@@ -17,8 +17,6 @@
 
 #define LT3_TAB LT(3, KC_TAB)
 
-extern keymap_config_t keymap_config;
-
 enum ut472_layers {
   _QWERTY,
   _WORKMAN,
@@ -56,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Ctrl| Alt | Gui | App |  L2  |   Space   |  L1  | Left| Down|  Up |Right|
    * `-------------------------------------------------------------------------'
    */
-[_QWERTY] = LAYOUT_ut472(
+[_QWERTY] = LAYOUT(
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LT3_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
