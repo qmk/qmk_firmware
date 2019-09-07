e1fffccb0f20e881ee95b284c61a33fed5e0b346
Annihilator6000 <8xovermsoctober@gmail.com>
2019-09-07 12:04:31 -0400

    [Keymap] UT47.2 Planck-style layout switching (#6669)
    * UT47.2 keymap/updates for Planck style layout switching
    
    * UT47.2 keymap for Planck-style layout switching / code clean up
    
    * UT47.2 keymap for Planck-style layout switching: Qwerty, Workman, Colemak, Colemak Mod-DH, and Dvorak added / code clean up
    
    * Change the layout info to match the keymap
    
    * Edit readme to contain relevant info for layout switching
    
    * Edit readme to contain relevant info for layout switching
    
    * Edit readme to contain relevant info for layout switching
    
    * Edit readme to contain relevant info for layout switching. Add QMK Configurator file.
    
    * Update readme.md with make information
    
    * Undo breaking change in config.h
    
    * Code cleanup
    
    * Code cleanup
    
    * Code cleanup
    
    * More code cleanup

--- keyboards/ut472/keymaps/annihilator6000/config.h
+++ keyboards/ut472/keymaps/annihilator6000/config.h
@@ -14,20 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- #pragma once
+#pragma once
 
-#include "quantum.h"
-
-#define LAYOUT( \
-  K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, \
-  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1a, K1b, \
-  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, \
-  K30, K31, K32, K33, K34,    K35,   K37, K38, K39, K3a, K3b  \
-) \
-  { \
-    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1a, K1b }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b }, \
-    { K30, K31, K32, K33, K34, K35, K35, K37, K38, K39, K3a, K3b }  \
-  }
-
+// place overrides here
