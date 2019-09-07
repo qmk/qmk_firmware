284dab92f57a25213d9c8f1dc5d106770c6dad13
mechmerlin <mechmerlin@gmail.com>
2019-09-06 20:30:41 -0700

    strip out the VIA enabling from default rules.mk

--- keyboards/dp60/rules.mk
+++ keyboards/dp60/rules.mk
@@ -1,42 +1,6 @@
 # MCU name
 MCU = atmega32u4
 
-# project specific files
-#SRC =
-
-# Processor frequency.
-#     This will define a symbol, F_CPU, in all source code files equal to the
-#     processor frequency in Hz. You can then use this symbol in your source code to
-#     calculate timings. Do NOT tack on a 'UL' at the end, this will be done
-#     automatically to create a 32-bit value in your source code.
-#
-#     This will be an integer division of F_USB below, as it is sourced by
-#     F_USB after it has run through any CPU prescalers. Note that this value
-#     does not *change* the processor frequency - it should merely be updated to
-#     reflect the processor speed set externally so that the code can use accurate
-#     software delays.
-F_CPU = 16000000
-
-
-#
-# LUFA specific
-#
-# Target architecture (see library "Board Types" documentation).
-ARCH = AVR8
-
-# Input clock frequency.
-#     This will define a symbol, F_USB, in all source code files equal to the
-#     input clock frequency (before any prescaling is performed) in Hz. This value may
-#     differ from F_CPU if prescaling is used on the latter, and is required as the
-#     raw input clock is fed directly to the PLL sections of the AVR for high speed
-#     clock generation for the USB and other AVR subsections. Do NOT tack on a 'UL'
-#     at the end, this will be done automatically to create a 32-bit value in your
-#     source code.
-#
-#     If no clock division is performed on the input clock inside the AVR (via the
-#     CPU clock adjust registers or the clock division fuses), this will be equal to F_CPU.
-F_USB = $(F_CPU)
-
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
@@ -59,7 +23,7 @@ BOOTLOADER = atmel-dfu
 #
 BOOTMAGIC_ENABLE = lite # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no # Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
@@ -75,12 +39,7 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 #RGBLIGHT_ENABLE = yes       # Use RGB bottom light
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
 
-RAW_ENABLE = yes
-#WEBUSB_ENABLE = yes
-DYNAMIC_KEYMAP_ENABLE = yes
-
 LAYOUTS = 60_ansi 60_hhkb 60_iso 60_ansi_split_bs_rshift
 
 CUSTOM_MATRIX = yes
-SRC += matrix.c keyboards/wilba_tech/wt_main.c
-#keyboards/stm60/webusb.c
+SRC += matrix.c 
