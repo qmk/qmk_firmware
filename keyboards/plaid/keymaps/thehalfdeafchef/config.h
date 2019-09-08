5871d2dbe59aa9dbedd89ed94d1fbb6b7a7444ec
Canino Developments <ian@thecommittedbug.io>
2019-09-08 19:18:44 -0400

    update copyright notice

--- keyboards/plaid/keymaps/thehalfdeafchef/config.h
+++ keyboards/plaid/keymaps/thehalfdeafchef/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 Takuya Urakawa (dm9records.com)
+/* Copyright 2019 Ian Canino (ian@thecommittedbug.io)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
