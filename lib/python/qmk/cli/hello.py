dde3522c0aa37e3aa86628727a61ac3bb2a82974
skullY <skullydazed@gmail.com>
2019-09-08 21:22:47 -0700

    pyformat

--- lib/python/qmk/cli/hello.py
+++ lib/python/qmk/cli/hello.py
@@ -4,6 +4,7 @@ This is an example QMK CLI script.
 """
 from milc import cli
 
+
 @cli.argument('-n', '--name', default='World', help='Name to greet.')
 @cli.subcommand('QMK Hello World.')
 def hello(cli):
