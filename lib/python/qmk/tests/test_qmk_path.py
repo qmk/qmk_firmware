b415d3f4a44c0f3bb308c1ceaa3d69be70303697
skullY <skullydazed@gmail.com>
2019-09-08 21:27:37 -0700

    Add a test for subcommand_modules

--- lib/python/qmk/tests/test_qmk_path.py
+++ lib/python/qmk/tests/test_qmk_path.py
@@ -11,3 +11,12 @@ def test_keymap_onekey_pytest():
 def test_normpath():
     path = qmk.path.normpath('lib/python')
     assert path == os.environ['ORIG_CWD'] + '/lib/python'
+
+
+def test_subcommand_modules():
+    count = 0
+    for module in qmk.path.subcommand_modules():
+        count += 1
+        assert module.startswith('qmk.cli.')
+
+    assert count > 9
