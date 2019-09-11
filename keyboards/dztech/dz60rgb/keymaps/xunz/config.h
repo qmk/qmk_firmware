0127e5fd7316377c378d6d90f4c53fa45cb169a1
Papop Chattongchaikul <dpprachaya@gmail.com>
2019-09-11 14:47:35 +0700

    [Update] Delete some unnecessary code in config.h

--- keyboards/dztech/dz60rgb/keymaps/xunz/config.h
+++ keyboards/dztech/dz60rgb/keymaps/xunz/config.h
@@ -50,17 +50,6 @@
 
 // #define RGB_MATRIX_KEYRELEASES
 
-// some speed shit
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
-
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
-
 #define NO_ACTION_ONESHOT
 #define TAPPING_FORCE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
