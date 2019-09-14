7bf8565ff7c7099f8968552d1af052e0cacfe43b
Xiao Yu <xiao@xster.net>
2019-09-14 15:13:49 -0700

    permissive hold

--- keyboards/xd75/keymaps/xo/config.h
+++ keyboards/xd75/keymaps/xo/config.h
@@ -1,8 +1,6 @@
-/* Copyright 2017 Benjamin Kesselring
- *
- * This program is free software: you can redistribute it and/or modify
+/* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -11,10 +9,11 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
 
-// place overrides here
 #define TAPPING_TOGGLE 2
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 150
