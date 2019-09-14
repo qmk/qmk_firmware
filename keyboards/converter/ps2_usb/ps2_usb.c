81a43b368e31f16f936824f66d41ae13e69c5ff1
lbibass <ewdries02@gmail.com>
2019-09-14 16:21:13 -0400

    Update keyboards/converter/ps2_usb/ps2_usb.c
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/converter/ps2_usb/ps2_usb.c
+++ keyboards/converter/ps2_usb/ps2_usb.c
@@ -3,7 +3,7 @@
 
 #include "led.h"
 
-void led_set(uint8_t usb_led)
+void led_set_kb(uint8_t usb_led)
 {
   uint8_t ps2_led = 0;
   if (usb_led & (1 << USB_LED_SCROLL_LOCK)) ps2_led |= (1 << PS2_LED_SCROLL_LOCK);
