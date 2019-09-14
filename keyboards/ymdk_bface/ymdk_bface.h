5ee56dc4b6795858aeb42b3bf5fe0eb6ad88aaa2
Ethan Durrant <5387347+emdarcher@users.noreply.github.com>
2019-09-13 23:05:48 -0600

    fixed typos in layout

--- keyboards/ymdk_bface/ymdk_bface.h
+++ keyboards/ymdk_bface/ymdk_bface.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   K03, K13, K23, K33, K43, K53, K63, K73, K83, K93, KA3, KB3,  KC3,  KD3, \
   K02, K12, K22, K32, K42, K52, K62, K72, K82, K92, KA2, KB2,  KC2,    KD2, \
   K01, K11,   K21, K31, K41, K51, K61, K71, K81, K91, KA1, KB1,  KC1, KD1, KE1, \
-  K00,    K10,     K20,  K30         K70,          K80   K90, KA0, KB0, KC0, KD0  \
+  K00,    K10,     K20,  K30,         K70,          K80,   K90, KA0, KB0, KC0, KD0  \
 ){ \
   { K00,   K10,   K20,   K30,   KC_NO, KC_NO, KC_NO,K70,    K80,    K90,    KA0,   KB0,   KC0,   KD0,   KC_NO }, \
   { K01,   K11,   K21,   K31,   K41,   K51,   K61,  K71,    K81,    K91,    KA1,   KB1,   KC1,   KD1,   KE1 }, \
