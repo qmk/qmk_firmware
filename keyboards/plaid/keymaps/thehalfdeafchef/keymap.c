89d17f7cfaf74e0553f57aa43239c3ee2695bb30
IanGC <ian@thecommittedbug.io>
2019-09-15 12:36:54 -0400

    Apply suggestions from code review
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/plaid/keymaps/thehalfdeafchef/keymap.c
+++ keyboards/plaid/keymaps/thehalfdeafchef/keymap.c
@@ -103,8 +103,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FUNCTION] = LAYOUT_planck_mit(RESET, LCA_T(KC_F1), LCA_T(KC_F2), LCA_T(KC_F3), LCA_T(KC_F4), LCA_T(KC_F5), LCA_T(KC_F6), LCA_T(KC_F7), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QWERTY, DVORAK, COLEMAK, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LED, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)};
 
 // constants to toggle LED behavior
-#define LED_ON 1
-#define LED_OFF 0
 
 // Setup config struct for LED
 typedef union {
