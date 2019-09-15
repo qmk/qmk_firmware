ef91fe94c343ce443e3892f3c71a32e3f6aa4814
mizuhof <moc.liam0000@gmail.com>
2019-09-15 23:39:58 +0900

    [update]
    update crkbd:custom

--- keyboards/crkbd/keymaps/custom/keymap.c
+++ keyboards/crkbd/keymaps/custom/keymap.c
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,-----------------------------------------.
      KC_TAB,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                   KC_6,  KC_7,  KC_8,  KC_9,  KC_0,KC_DEL,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,KC_NO,KC_NO,\
+    KC_LCTL, KC_NO, KC_NO, KC_HOME, KC_END, KC_NO,                KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,KC_MINS,KC_EQL,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_END, KC_HOME, KC_NO, KC_NO, KC_ENT,\
+    KC_LALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
                                 KC_LGUI, KC_RSHIFT, KC_SPC,  LOWER, RAISE,KC_ESC\
                               //`--------------------'  `--------------------'
@@ -62,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,-----------------------------------------.
      KC_TAB,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_GRV,KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,\
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_TILD,KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,\
+    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_GRV,KC_LCBR,KC_RCBR,KC_MINS,KC_EQL,KC_PIPE,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+-----|
+    KC_LALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_TILD,KC_LBRC,KC_RBRC,KC_UNDS,KC_PLUS,KC_BSLS,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
                                 KC_LGUI, KC_RSHIFT, KC_SPC,   LOWER, RAISE,KC_ESC\
                               //`--------------------'  `--------------------'
