da6a51f4f438f2724b48ff5956e54592d19b31e1
Jonas Avellana <jonas.avellana@rallyteam.com>
2019-09-15 20:04:20 -0600

    [chore(crkbd)] adding SLEEP_LED_ENABLE on rules.mk

--- keyboards/crkbd/keymaps/ninjonas/rules.mk
+++ keyboards/crkbd/keymaps/ninjonas/rules.mk
@@ -1,5 +1,9 @@
 RGBLIGHT_ENABLE = yes
 MOUSEKEY_ENABLE = no
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+
 EXTRAFLAGS += -flto
 
 # If you want to change the display of OLED, you need to change here
