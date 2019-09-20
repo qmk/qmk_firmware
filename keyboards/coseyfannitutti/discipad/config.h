5d22faf7248a6c0431121eb043e842262d574fab
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2019-09-20 01:25:59 -0400

    Update config.h

--- keyboards/coseyfannitutti/discipad/config.h
+++ keyboards/coseyfannitutti/discipad/config.h
@@ -243,7 +243,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 */
 
-/* Bootmagic Lite key configuration */
+/* Bootmagic Lite key configuration
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
-
+*/
