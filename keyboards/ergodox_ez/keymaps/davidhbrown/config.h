cdb0dc25983e3d1880f7c0ed58467a4a4a88bd39
David H. Brown <dave@davidhbrown.us>
2019-09-22 11:47:20 -0400

    Requested / suggested changes
    keymap.c: Removed unused process_record_user function; added linebreaks to help format keymap
    config.h: added #pragma once

--- keyboards/ergodox_ez/keymaps/davidhbrown/config.h
+++ keyboards/ergodox_ez/keymaps/davidhbrown/config.h
@@ -2,6 +2,8 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#pragma once
+
 #undef RGBLIGHT_HUE_STEP
 #define RGBLIGHT_HUE_STEP 24
 
