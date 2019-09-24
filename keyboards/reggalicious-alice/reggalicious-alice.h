387d345f5c826bb9f7c8c668180834fe3e52861c
cfbender <codyfbender@gmail.com>
2019-09-23 22:18:43 -0600

    Initialize config

---
+++ keyboards/reggalicious-alice/reggalicious-alice.h
@@ -0,0 +1,34 @@
+/* Copyright 2019 cfbender
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "quantum.h"
+
+#define KNO KC_NO
+
+#define LAYOUT_default( \
+  K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, K07, K17, \
+  K20, K30, K21, K31, K22, K32, K23,      K33, K24, K34, K25, K35, K26, K36, K27,   \
+  K40, K50, K41, K51, K42, K52, K43,      K53, K44, K54, K45, K55, K46,      K57, \
+       K60, K61, K71, K62, K72, K63,      K73, K64, K74, K65, K75, K66, K76, K67, \
+       K80,      K81, K82,      K83,           K84,      K85,                K87  \
+) { \
+  {  K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, K07, K17  }, \
+  {  K20, K30, K21, K31, K22, K32, K23, KNO, K33, K24, K34, K25, K35, K26, K36, K27  }, \
+  {  K40, K50, K41, K51, K42, K52, K43, KNO, K53, K44, K54, K45, K55, K46, KNO, K57  }, \
+  {  KNO, K60, K61, K71, K62, K72, K63, KNO, K73, K64, K74, K65, K75, K66, K76, K67  }, \
+  {  KNO, K80, KNO, K81, K82, KNO, K83, KNO, KNO, K84, KNO, K85, KNO, KNO, KNO, K87  }  \
+}
