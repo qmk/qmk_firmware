61b5d0e0c5921d9c3ee1cbbc1f80a9b3dcc6b880
mikethetiger <30720424+mikethetiger@users.noreply.github.com>
2019-09-23 20:14:55 -0500

    [Keymap] mikethetiger's milk keymap (#6611)
    * Added my Preonic keymap
    
    * Update keyboards/preonic/keymaps/mikethetiger/keymap.c
    
    Co-Authored-By: mikethetiger <30720424+mikethetiger@users.noreply.github.com>
    
    * Update keyboards/preonic/keymaps/mikethetiger/keymap.c
    
    Co-Authored-By: mikethetiger <30720424+mikethetiger@users.noreply.github.com>
    
    * Added my Preonic keymap
    
    * Added my Preonic keymap
    
    * mikethetigers lets slpit eh keymap
    
    * mikethetiger's milk keymap
    
    * Update rules.mk
    
    * Update keyboards/thevankeyboards/minivan/keymaps/mikethetiger/keymap.c
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Update rules.mk
    
    Better?

---
+++ keyboards/2_milk/keymaps/mikethetiger/keymap.c
@@ -0,0 +1,8 @@
+﻿#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+      [0] = LAYOUT(
+          KC_VOLU,
+          KC_VOLD
+          )
+};
--- keyboards/2_milk/keymaps/mikethetiger/rules.mk
+++ keyboards/2_milk/keymaps/mikethetiger/rules.mk
@@ -1,30 +1,9 @@
-﻿# MCU name
-MCU = atmega32u4
-
-# Processor frequency.
-F_CPU = 16000000
-
-#
-# LUFA specific
-#
-# Target architecture (see library "Board Types" documentation).
-ARCH = AVR8
-
-# Input clock frequency.
-F_USB = $(F_CPU)
-
-# Bootloader
-BOOTLOADER = caterina
-
-# Interrupt driven control endpoint task(+60)
-OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
-
 # Build Options
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE  = no	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE  = no	# Mouse keys(+4700)
-EXTRAKEY_ENABLE  = no	# Audio control and System control(+450)
+EXTRAKEY_ENABLE  = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE  = yes	# Console for debug(+400)
 COMMAND_ENABLE  = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE  = no  # Breathing sleep LED during USB suspend
