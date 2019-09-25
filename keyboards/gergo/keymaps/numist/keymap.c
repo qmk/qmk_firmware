b661dfd91f25ee4b447ea084acaa0ea74cb68a3d
Scott Perry <dev@numist.net>
2019-09-25 12:52:56 -0700

    Adopt new layout macro for gergo and sync up to default map

--- keyboards/gergo/keymaps/numist/keymap.c
+++ keyboards/gergo/keymaps/numist/keymap.c
@@ -24,129 +24,30 @@
 #define M_PLAY1 DYN_MACRO_PLAY1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
- *
- * ,-------------------------------------------.                         ,-------------------------------------------.
- * | L1/ESC |   Q  |   W  |   E  |   R  |   T  |                         |   Y  |   U  |   I  |   O  |   P  |  | \   |
- * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |Ctrl/BS |   A  |   S  |  D   |   F  |   G  | RMB  |           |      |   H  |   J  |   K  |   L  | ;  : |  ' "   |
- * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | LMB  |           |      |   N  |   M  | ,  < | . >  | /  ? |  - _   |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *                    .----------.   .-------.                                 .------.   .-----.
- *                    | Super/Del|   |Ent/ALT|                                 | Tab  |   |BKSP |
- *                    '----------'   '-------'                                 `------.   '-----'
- *                                        ,-------.                      ,-------.
- *                                        | MMB   |                      | PgDn  |
- *                                 ,------|-------|                      |-------|------.
- *                                 | SYMB | NUMB  |                      | SYMB  | NUMB |
- *                                 | Space| Escape|                      | Mod   |Space |
- *                                 |      |       |                      |       |      |
- *                                 `--------------'                      `--------------'
- */
-[BASE] = LAYOUT_GERGO(
-KC_GRAVE,              KC_Q,  KC_W,   KC_F,   KC_P, KC_B,                        KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN, KC_EQL,
-LT(NUMB, KC_PGUP),     KC_A,  KC_R,   KC_S,   KC_T, KC_G, KC_TAB,       KC_ENT,  KC_M, KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT,
-MT(MOD_LSFT, KC_PGDN), KC_Z,  KC_X,   KC_C,   KC_D, KC_V, KC_ESC,       KC_BSLS, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+// Keymap 0: Base layer
+[BASE] = LAYOUT_gergo(
+KC_GRAVE,              KC_Q,  KC_W,   KC_F,   KC_P, KC_B,                  /**/                   KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN, KC_EQL,
+LT(NUMB, KC_PGUP),     KC_A,  KC_R,   KC_S,   KC_T, KC_G, KC_TAB,          /**/          KC_ENT,  KC_M, KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT,
+MT(MOD_LSFT, KC_PGDN), KC_Z,  KC_X,   KC_C,   KC_D, KC_V, KC_ESC, KC_LBRC, /**/ KC_RBRC, KC_BSLS, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
 
-	                KC_LCTL, KC_LALT,                                                       LT(FN, KC_SPC), LT(KC_RCTL, KC_DEL),
-                                                         KC_LBRC,       KC_RBRC,
-                                                KC_LCMD, KC_LSFT,       KC_BSPC, MT(MOD_RSFT, KC_CAPS)),
-/* Keymap 1: Symbols layer
- *
- * ,-------------------------------------------.                         ,-------------------------------------------.
- * |        |  !   |  @   |  {   |  }   |  |   |                         |      |      |      |      |      |  \ |   |
- * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |        |  #   |  $   |  (   |  )   |  `   |      |           |      |   +  |  -   |  /   |  *   |  %   |  ' "   |
- * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |        |  %   |  ^   |  [   |  ]   |  ~   |      |           |      |   &  |  =   |  ,   |  .   |  / ? | - _    |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *                        .------.   .------.                                 .------.   .-----.
- *                        |      |   |      |                                 |      |   | DEL |
- *                        '------'   '------'                                 `------.   '-----'
- *                                        ,-------.                     ,-------.
- *                                        |       |                     | PgUp  |
- *                                 ,------|-------|                     |-------|------.
- *                                 |      |       |                     |       |      |
- *                                 |   ;  |   =   |                     |   =   |   ;  |
- *                                 |      |       |                     |       |      |
- *                                 `--------------'                     `--------------'
- */
-[FN] = LAYOUT_GERGO(
-KC_TRNS, M_RECD1, M_STOP1, M_PLAY1, KC_TRNS, KC_TRNS,                          KC_MNXT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_TRNS,
-KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_MPLY,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
-KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_MPRV,  KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_BSLS, KC_PIPE,
+                                     KC_LCTL,  KC_LALT,  KC_LCMD, KC_LSFT, /**/ KC_BSPC, MOD_RSFT,  LT(FN, KC_SPC),  LT(KC_RCTL, KC_DEL)
+// TODO: MT taps KC_CAPS too quickly for the key hysteresis imposed by macOS; need to find a different solution for MT(MOD_RSFT, KC_CAPS).
+),
 
-                            KC_TRNS, KC_TRNS,                                           _______, KC_TRNS,
-                                                      KC_TRNS,       KC_TRNS,
-                                             KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS),
-/* Keymap 2: Pad/Function layer
- *
- * ,-------------------------------------------.                         ,-------------------------------------------.
- * |        |   1  |  2   |  3   |  4   |  5   |                         |  6   |  7   |  8   |  9   |  0   |        |
- * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |  F1    |  F2  | F3   | F4   | F5   | F6   | BTN1 |           |      | LEFT | DOWN |  UP  | RIGHT|VolDn | VolUp  |
- * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |  F7    |  F8  | F9   | F10  | F11  | F12  | BTN2 |           |      | MLFT | MDWN | MUP  | MRGHT|Ply/Pa|  Skip  |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *                        .------.   .------.                                 .------.   .-----.
- *                        |      |   |      |                                 |      |   |     |
- *                        '------'   '------'                                 `------.   '-----'
- *                                        ,-------.                     ,-------.
- *                                        |       |                     | PgUp  |
- *                                 ,------|-------|                     |-------|------.
- *                                 |      |       |                     |       |      |
- *                                 |      |       |                     |       |      |
- *                                 |      |       |                     |       |      |
- *                                 `--------------'                     `--------------'
- */
-[NUMB] = LAYOUT_GERGO(
-KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_TRNS,                         KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
-_______, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
-                        KC_TRNS, KC_TRNS,                               KC_0, KC_DOT,
-                                              KC_TRNS,       KC_TRNS,
-                                     KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS)
+// Keymap 1: Fn layer
+[FN] = LAYOUT_gergo(
+KC_TRNS, M_RECD1, M_STOP1, M_PLAY1, KC_TRNS, KC_TRNS,                   /**/                   KC_MNXT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_TRNS,
+KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          /**/          KC_MPLY, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
+KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_MPRV, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_BSLS, KC_PIPE,
+
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS,  _______,  KC_TRNS
+),
+// Keymap 2: Numpad/F-key layer
+[NUMB] = LAYOUT_gergo(
+KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_TRNS,                   /**/                   KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
+_______, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_TRNS,          /**/          KC_TRNS, KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,
+KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
+
+                            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS,  KC_0,  KC_DOT,
+)
 };
-
-/* Keymap template
- *
- * ,-------------------------------------------.                         ,-------------------------------------------.
- * |        |      |      |      |      |      |                         |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *                        .------.   .------.                                 .------.   .-----.
- *                        |      |   |      |                                 |      |   |     |
- *                        '------'   '------'                                 `------.   '-----'
- *                                        ,-------.       ,-------.
- *                                        |       |       |       |
- *                                 ,------|-------|       |-------|------.
- *                                 |      |       |       |       |      |
- *                                 |      |       |       |       |      |
- *                                 |      |       |       |       |      |
- *                                 `--------------'       `--------------'
-[SYMB] = LAYOUT_GERGO(
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-
-                                                  KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS,
-                                                           KC_TRNS,       KC_TRNS,
-                                             	  KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS),
- */
-
-// Runs just one time when the keyboard initializes.
-void matrix_init_user(void) {
-
-};
-
-// Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
-    //uint8_t layer = biton32(layer_state);
-    biton32(layer_state);
-};
-
-
