bcdf03ee87a490a437ad848e9221dd5565d26918
Jonas Avellana <jonas.avellana@rallyteam.com>
2019-09-24 19:04:37 -0600

    [fix(#6797)] cleaning up rules.mk

--- keyboards/crkbd/keymaps/ninjonas/rules.mk
+++ keyboards/crkbd/keymaps/ninjonas/rules.mk
@@ -8,12 +8,12 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 EXTRAFLAGS += -flto
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
-        ./lib/rgb_state_reader.c \
+SRC +=  ./lib/rgb_state_reader.c \
         oled.c \
         rgb.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+        # ./lib/glcdfont.c \
+        # ./lib/logo_reader.c \
+        # ./lib/keylogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
