7b24bae509582bbbd632d1f8aea0b099e4a0eda2
Jonas Avellana <jonas.avellana@rallyteam.com>
2019-09-25 07:54:09 -0600

    [fix(#6797)] using default glcdfont.c for lily58 & crkbd

--- keyboards/crkbd/keymaps/ninjonas/config.h
+++ keyboards/crkbd/keymaps/ninjonas/config.h
@@ -44,5 +44,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_VAL_STEP 17
 #endif 
 
-#define OLED_FONT_H "keyboards/crkbd/keymaps/ninjonas/glcdfont.c"
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
 #define OLED_DISABLE_TIMEOUT
--- keyboards/lily58/keymaps/ninjonas/config.h
+++ keyboards/lily58/keymaps/ninjonas/config.h
@@ -29,5 +29,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 #define TAPPING_FORCE_HOLD
 
-#define OLED_FONT_H "keyboards/lily58/keymaps/ninjonas/glcdfont.c"
+#define OLED_FONT_H "keyboards/lily58/lib/glcdfont.c"
 #define OLED_DISABLE_TIMEOUT
