013a52f7bddb5e8d8494e5933c21d4b1bfd791c1
Jonas Avellana <jonas.avellana@workday.com>
2019-09-25 16:55:15 -0600

    [fix(#6797)] Using LINK_TIME_OPTIMIZATION_ENABLE rather than EXTRAFLAGS as per code review

--- keyboards/crkbd/keymaps/ninjonas/rules.mk
+++ keyboards/crkbd/keymaps/ninjonas/rules.mk
@@ -1,18 +1,11 @@
 RGBLIGHT_ENABLE = yes
 MOUSEKEY_ENABLE = no
 OLED_DRIVER_ENABLE = yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-EXTRAFLAGS += -flto
-
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
-        rgb.c \
-        # ./lib/glcdfont.c \
-        # ./lib/logo_reader.c \
-        # ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+        rgb.c
