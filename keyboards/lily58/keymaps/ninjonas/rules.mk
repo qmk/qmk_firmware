9ce235b08baa8544d3be88dd7eb3fd97698994f3
Jonas Avellana <jonas.avellana@rallyteam.com>
2019-09-24 19:35:58 -0600

    [fix(#6797)] Using OLED Driver for Lily58

--- keyboards/lily58/keymaps/ninjonas/rules.mk
+++ keyboards/lily58/keymaps/ninjonas/rules.mk
@@ -1,5 +1,6 @@
+OLED_DRIVER_ENABLE = yes
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
-        layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+SRC +=  oled.c \
+        # ./lib/glcdfont.c \
+        # ./lib/logo_reader.c \
+        # ./lib/keylogger.c \
