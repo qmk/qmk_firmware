d143ddc06265652b2e9eb01e454ead135169712d
Yan-Fa Li <yanfali@gmail.com>
2019-09-25 21:35:54 -0700

    [Keymap] Port personal keymap to 60_tsangan_hhkb (#6820)
    * Port personal keymap to 60_tsangan_hhkb
    
     - add 60_tsangan_hhkb layout to plain60
     - Fix bug in split rs in plain60
     - use community and user based layout for 60_tsangan_hhkb
       - set up audio for plain60 only
    
    * Add LAYOUT_60_ansi_split_bs_rshift

--- keyboards/maartenwut/plain60/rules.mk
+++ keyboards/maartenwut/plain60/rules.mk
@@ -65,4 +65,4 @@ BLUETOOTH_ENABLE = no # Enable Bluetooth with the Adafruit EZ-Key HID
 RAW_ENABLE = yes
 DYNAMIC_KEYMAP_ENABLE = yes
 
-LAYOUTS = 60_ansi 60_ansi_split_bs_rshift 60_hhkb 60_iso
+LAYOUTS = 60_ansi 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_tsangan_hhkb
