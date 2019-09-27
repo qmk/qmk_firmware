8714a488bd82db3fb56461b2adc2c1b197eb8a23
Alexander Fougner <fougner89@gmail.com>
2019-09-27 20:29:00 +0200

    fix comments from review

--- keyboards/bpiphany/unloved_bastard/unloved_bastard.h
+++ keyboards/bpiphany/unloved_bastard/unloved_bastard.h
@@ -18,8 +18,6 @@
 
 #include "quantum.h"
 
-#define LAYOUT_all LAYOUT
-
 #define LAYOUT( \
   KG6,      KH4, KI4, KI2, KI6, KP5, KL6, KM2, KM4, KO4, KO5, KO6, KO0,   KN5, KN7, KP7, \
   KG4, KG5, KH5, KI5, KJ5, KJ4, KK4, KK5, KL5, KM5, KF5, KF4, KL4, KO2,   KR4, KC4, KE4, \
@@ -61,8 +59,8 @@ LAYOUT_all( \
 #define LAYOUT_tkl_iso( \
   KG6,      KH4, KI4, KI2, KI6, KP5, KL6, KM2, KM4, KO4, KO5, KO6, KO0,   KN5, KN7, KP7, \
   KG4, KG5, KH5, KI5, KJ5, KJ4, KK4, KK5, KL5, KM5, KF5, KF4, KL4, KO2,   KR4, KC4, KE4, \
-  KG2, KG7, KH7, KI7, KJ7, KJ2, KK2, KK7, KL7, KM7, KF7, KF2, KL2, KO3,   KQ4, KC5, KE5, \
-  KH2, KG3, KH3, KI3, KJ3, KJ6, KK6, KK3, KL3, KM3, KF3, KF6,      KO1,                  \
+  KG2, KG7, KH7, KI7, KJ7, KJ2, KK2, KK7, KL7, KM7, KF7, KF2, KL2,        KQ4, KC5, KE5, \
+  KH2, KG3, KH3, KI3, KJ3, KJ6, KK6, KK3, KL3, KM3, KF3, KF6, KO3, KO1,                  \
   KB2, KH6, KG1, KH1, KI1, KJ1, KJ0, KK0, KK1, KL1, KM1, KF0,      KB3,        KC6,      \
   KP4, KN2, KN6,                KQ6,                KN0, KN3, KM0, KP1,   KC0, KQ0, KR0  \
 ) \
@@ -74,5 +72,7 @@ LAYOUT_all( \
   KB2, KH6, KG1, KH1, KI1, KJ1, KJ0, KK0, KK1, KL1, KM1, KF0,      KB3,        KC6,      \
   KP4, KN2, KN6,                KQ6,                KN0, KN3, KM0, KP1,   KC0, KQ0, KR0  \
 )
+
+#define LAYOUT_all LAYOUT
 
 #endif
