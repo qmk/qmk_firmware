cd42efb43468f3c7e7acd67b650f816394b98543
Drashna Jael're <drashna@live.com>
2019-09-29 08:19:43 -0700

    Tweak feature settings for Ergodox EZ Glow

--- layouts/community/ergodox/drashna_glow/rules.mk
+++ layouts/community/ergodox/drashna_glow/rules.mk
@@ -7,7 +7,16 @@ SRC += $(CORRECTED_LAYOUT)/keymap.c
 -include $(CORRECTED_LAYOUT)/rules.mk
 
 ifeq ($(strip $(KEYBOARD)), ergodox_ez)
-	RGBLIGHT_ENABLE = no
+	RGBLIGHT_ENABLE = yes
 	RGB_MATRIX_ENABLE = yes
-#   TAP_DANCE_ENABLE  = no
+	TAP_DANCE_ENABLE  = no
+	CONSOLE_ENABLE     = no
+	BOOTMAGIC_ENABLE   = lite
+
+	UNICODE_ENABLE     = yes
+	UNICDOEMAP_ENABLE  = no
+
+	RGBLIGHT_TWINKLE   = no
+	INDICATOR_LIGHTS   = no
+	RGBLIGHT_STARTUP_ANIMATION = no
 endif
