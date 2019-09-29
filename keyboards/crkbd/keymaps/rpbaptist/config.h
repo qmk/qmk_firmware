08fe0dcea5b8377b0f03764b2ec5c7e1cf54b88c
Richard Baptist <rpbaptist@gmail.com>
2019-09-29 12:27:51 +0200

    Put RGB MATRIX in config and rules

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -47,9 +47,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_HUE_STEP 10
   #define RGBLIGHT_SAT_STEP 16
   #define RGBLIGHT_VAL_STEP 16
+  #define RGBLIGHT_SLEEP
 #endif
 
-#define RGBLIGHT_SLEEP
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_MATRIX_KEYPRESSES  // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+// #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150  // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#    define RGB_MATRIX_HUE_STEP 10
+#    define RGB_MATRIX_SAT_STEP 15
+#    define RGB_MATRIX_VAL_STEP 16
+#    define RGB_MATRIX_SPD_STEP 10
+#endif
 
 #define TAPPING_TERM 150
 // #define PERMISSIVE_HOLD
--- keyboards/crkbd/keymaps/rpbaptist/rules.mk
+++ keyboards/crkbd/keymaps/rpbaptist/rules.mk
@@ -8,8 +8,8 @@ EXTRAFLAGS        += -flto
 MOUSEKEY_ENABLE    = yes  # Mouse keys(+4700)
 EXTRAKEY_ENABLE    = yes  # Audio control and System control(+450)
 NKRO_ENABLE        = yes  # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-RGBLIGHT_ENABLE    = yes  # Enable WS2812 RGB underlight.
 
+RGBLIGHT_ENABLE    = no  # Enable WS2812 RGB underlight.
 BOOTMAGIC_ENABLE   = no   # Virtual DIP switch configuration(+1000)
 CONSOLE_ENABLE     = no   # Console for debug(+400)
 COMMAND_ENABLE     = no   # Commands for debug and configuration
@@ -20,12 +20,12 @@ UNICODE_ENABLE     = no   # Unicode
 BLUETOOTH_ENABLE   = no   # Enable Bluetooth with the Adafruit EZ-Key HID
 SWAP_HANDS_ENABLE  = no   # Enable one-hand typing
 
+RGB_MATRIX_ENABLE = WS2812
+
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE   = no    # Breathing sleep LED during USB suspend
 
 BOOTLOADER       = qmk-dfu
-
-# RGB_MATRIX_ENABLE = WS2812
 
 OLED_DRIVER_ENABLE = yes
 
