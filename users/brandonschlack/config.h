a0a80f6a1f73e6ef2e0f86109b2e9dc4390bd0e4
Brandon Schlack <brandonschlack@gmail.com>
2019-09-30 15:36:43 -0700

    [User] Update TAP_CODE_DELAY for encoders

--- users/brandonschlack/config.h
+++ users/brandonschlack/config.h
@@ -12,7 +12,11 @@
 #   define RGB_THEME_ENABLE
 #endif
 
-#define TAP_CODE_DELAY 5
+#ifdef ENCODER_ENABLE
+#    define TAP_CODE_DELAY 10
+#else
+#    define TAP_CODE_DELAY 5
+#endif
 
 /* Disable unused and unneeded features to reduce on firmware size */
 #ifdef LOCKING_SUPPORT_ENABLE
