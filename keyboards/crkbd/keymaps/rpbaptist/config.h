3b86679d6b4d66f784744690126afac39a49abcb
Richard Baptist <rpbaptist@gmail.com>
2019-10-01 10:07:20 +0200

    Remove disabling of NO_ACTION_MACRO and NO_ACTION_FUNCTION

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -106,8 +106,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define OLED_FONT_H "keyboards/crkbd/keymaps/rpbaptist/glcdfont.c"
 
-#define NO_ACTION_FUNCTION
-#define NO_ACTION_MACRO
 #define NO_ACTION_ONESHOT
 
 #undef PRODUCT
