cc9fd321f386cc2172d351a62e0e182ef4558dcf
nrtkbb <nrtkbb@gmail.com>
2019-10-02 01:37:32 +0900

    Update keyboards/uzu42/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/uzu42/rules.mk
+++ keyboards/uzu42/rules.mk
@@ -14,7 +14,6 @@ MCU = atmega32u4
 #     automatically (+60). See bootloader.mk for all options.
 BOOTLOADER = caterina
 
-
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
