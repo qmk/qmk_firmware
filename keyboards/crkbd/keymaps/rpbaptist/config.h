d967087ab82a3d89a9fa0a41887b5676c2d075be
Richard Baptist <rpbaptist@gmail.com>
2019-10-01 09:46:44 +0200

    Remove redundant config

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -110,14 +110,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_MACRO
 #define NO_ACTION_ONESHOT
 
-
-#ifndef NO_DEBUG
-  #define NO_DEBUG
-#endif // !NO_DEBUG
-
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-  #define NO_PRINT
-#endif // !NO_PRINT
-
 #undef PRODUCT
 #define PRODUCT Corne Keyboard
