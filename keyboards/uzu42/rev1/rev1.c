e1ff889b15007f409b80aa23eba6b813f083c4be
nrtkbb <nrtkbb@gmail.com>
2019-10-03 06:52:00 +0900

    Remove code overwritten by the core.

--- keyboards/uzu42/rev1/rev1.c
+++ keyboards/uzu42/rev1/rev1.c
@@ -2,19 +2,6 @@
 
 
 void matrix_init_kb(void) {
-
-    #ifdef AUDIO_ENABLE
-        _delay_ms(20); // gets rid of tick
-        PLAY_SONG(tone_startup);
-    #endif
-
 	matrix_init_user();
 };
 
-void shutdown_kb(void) {
-    #ifdef AUDIO_ENABLE
-        PLAY_SONG(tone_goodbye);
-      	_delay_ms(150);
-      	stop_all_notes();
-    #endif
-}
