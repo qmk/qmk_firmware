1548f510fef387edf7d89e57413cff03a4331c9f
nrtkbb <nrtkbb@gmail.com>
2019-10-02 21:41:30 +0900

    fix setting for RGBLED

--- keyboards/uzu42/rev1/config.h
+++ keyboards/uzu42/rev1/config.h
@@ -53,7 +53,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 
-#define RGBLED_NUM 12    // Number of LEDs
+#ifdef RGBLIGHT_ENABLE
+#   define RGBLED_NUM 54    // Number of LEDs
+#   define RGBLED_SPLIT { 27, 27 }
+#endif
 
 /*
  * Feature disable options
