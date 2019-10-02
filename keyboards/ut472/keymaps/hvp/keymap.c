041260cec1e0fe31d95612f49c0062fafb75359b
Hampus <hampus.andersson@gmail.com>
2019-10-03 01:02:11 +0200

    Move tap dance over to user space

--- keyboards/ut472/keymaps/hvp/keymap.c
+++ keyboards/ut472/keymaps/hvp/keymap.c
@@ -14,17 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "hvp.c"
 
 #define LT3_ESC LT(3, KC_ESC)
-
-// Tap Dance Declarations
-enum tapdance_id
-{
-  TD1 = 0,
-  TD2,
-  TD3
-};
-
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -105,103 +97,3 @@ LAYOUT( /* Tab */
   RESET, _______, _______, _______, KC_MUTE,     KC_MPLY,      KC_MSTP, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 };
-
-// Tap dance 1
-void dance_1_finished(qk_tap_dance_state_t *state, void *user_data)
-{
-  if (state->count == 2)
-  {
-    register_code(KC_SCLN);
-    unregister_code(KC_SCLN);
-  }
-  else
-  {
-    register_code(KC_RALT);
-    register_code(KC_O);
-    unregister_code(KC_RALT);
-    unregister_code(KC_O);
-  }
-}
-
-void dance_1_reset(qk_tap_dance_state_t *state, void *user_data)
-{
-  if (state->count == 2)
-  {
-    unregister_code(KC_SCLN);
-  }
-  else
-  {
-    unregister_code(KC_RALT);
-    unregister_code(KC_O);
-  }
-}
-
-// Tap dance 2
-void dance_2_finished(qk_tap_dance_state_t *state, void *user_data)
-{
-  if (state->count == 2)
-  {
-    register_code(KC_QUOT);
-    unregister_code(KC_QUOT);
-  }
-  else
-  {
-    register_code(KC_RALT);
-    register_code(KC_A);
-    unregister_code(KC_RALT);
-    unregister_code(KC_A);
-    }
-}
-
-void dance_2_reset(qk_tap_dance_state_t *state, void *user_data)
-{
-  if (state->count == 2)
-  {
-    unregister_code(KC_QUOT);
-  }
-  else
-  {
-    unregister_code(KC_RALT);
-    unregister_code(KC_A);
-  }
-}
-
-// Tap dance 3
-void dance_3_finished(qk_tap_dance_state_t *state, void *user_data)
-{
-  // if (state->count == 2)
-  if (state->count == 2)
-  {
-    register_code(KC_SLSH);
-    unregister_code(KC_SLSH);
-  }
-  else
-  {
-    register_code(KC_RALT);
-    register_code(KC_W);
-    unregister_code(KC_RALT);
-    unregister_code(KC_W);
-  }
-}
-
-void dance_3_reset(qk_tap_dance_state_t *state, void *user_data)
-{
-  if (state->count == 2)
-  {
-    unregister_code(KC_SLSH);
-  }
-  else
-  {
-    unregister_code(KC_RALT);
-    unregister_code(KC_W);
-  }
-}
-
-// Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // simple tap dance
-    [TD1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_1_finished, dance_1_reset),
-
-    [TD2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_2_finished, dance_2_reset),
-
-    [TD3] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_3_finished, dance_3_reset)};
