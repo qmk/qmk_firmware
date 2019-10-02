e5a85e053c28210c504dd6f6ba516af57a3e53f1
nrtkbb <nrtkbb@gmail.com>
2019-10-03 07:07:24 +0900

    Removed unuse variable.

--- keyboards/uzu42/keymaps/default/keymap.c
+++ keyboards/uzu42/keymaps/default/keymap.c
@@ -14,8 +14,6 @@ extern keymap_config_t keymap_config;
 extern rgblight_config_t rgblight_config;
 #endif
 
-extern volatile bool isLeftHand;
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
