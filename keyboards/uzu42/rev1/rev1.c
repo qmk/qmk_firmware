ad61e53968ab9df3a297f6f3879c9119d43da10b
nrtkbb <nrtkbb@gmail.com>
2019-10-03 06:39:11 +0900

    Remove not needed code.

--- keyboards/uzu42/rev1/rev1.c
+++ keyboards/uzu42/rev1/rev1.c
@@ -1,11 +1,6 @@
 #include "uzu42.h"
 
 
-#ifdef AUDIO_ENABLE
-    float tone_startup[][2] = SONG(STARTUP_SOUND);
-    float tone_goodbye[][2] = SONG(GOODBYE_SOUND);
-#endif
-
 #ifdef SSD1306OLED
 void led_set_kb(uint8_t usb_led) {
     // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
