78fc6b430b7e661b8c5350531e05c9ba6a7eee16
Richard Baptist <rpbaptist@users.noreply.github.com>
2019-10-02 08:46:12 +0200

    Use get_highest_layer instead of biton32
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/crkbd/keymaps/rpbaptist/keymap.c
+++ keyboards/crkbd/keymaps/rpbaptist/keymap.c
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  switch (biton32(default_layer_state)) {
+  switch (get_highest_layer(default_layer_state)) {
     case _COLEMAKDHM:
       state = update_tri_layer_state(state, _NUM, _NAV, _UTIL);
       break;
