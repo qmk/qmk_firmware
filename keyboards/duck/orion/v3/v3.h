7a7cecd58f92c4387886c3127af2130a113e4a2a
mechmerlin <mechmerlin@gmail.com>
2019-10-03 09:04:31 -0700

    backslash and backspace row left to fix

--- keyboards/duck/orion/v3/v3.h
+++ keyboards/duck/orion/v3/v3.h
@@ -19,21 +19,18 @@
 
 #define ___ KC_NO
 
-#define LAYOUT( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k3b,  \
-    k10, k1f, k11, k12, k13, k14, k110, k15, k16, k17, k18, k19, k1a, k1b, k1d, k1e, k1c,  \
-    k20, k2f, k21, k22, k23, k24, k210, k25, k26, k27, k28, k29, k2a, k2b, k2d, k2e, k2c,  \
-    k30, k3f, k31, k32, k33, k34, k310, k35, k36, k37, k38, k39, k3a,  \
-    k40, k4f, k41, k42, k43, k44, k410, k45, k46, k47, k48, k49, k4e,  \
-    k50, k5f, k51, k53, k56, k57, k58, k59, k5d, k5e, k5c \
-){ \
-    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, ___, ___ },  \
-    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f, k110 },  \
-    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f, k210 },  \
-    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, ___, ___, ___, k3f, k310 },  \
-    { k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, ___, ___, ___, ___, k4e, k4f, k410 },  \
-    { k50, k51, ___, k53, ___, ___, k56, k57, k58, k59, ___, ___, k5c, k5d, k5e, k5f, ___ }, \
+#define LAYOUT_tkl_ansi( \
+    K5A,      K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q,  \
+    K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M,      K4O, K4N, K40, K4P, \
+    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,      K3O, K3N, K3Q, K3P, \
+    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L,           K2N, \
+    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J,      K1K, K1N, K1P, \
+    K0A, K0B, K0C,                     K0F, K0I,      K0K,      K0M, K0N, K0O, K0P, K0Q \
+) { \
+  { K5A, ___, K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q }, \
+  { K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, ___, K4O, K4P, ___ }, \
+  { K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, ___, K3O, K3P, K3Q }, \
+  { K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, ___, K2N, ___, ___, ___ }, \
+  { K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, ___, ___, K1N, ___, K1P, ___ }, \
+  { K0A, K0B, K0C, ___, ___, K0F, ___, ___, K0I, ___, K0K, ___, K0M, K0N, K0O, K0P, K0Q }  \
 }
-
-
-
