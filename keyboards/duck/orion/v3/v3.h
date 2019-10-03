634b964f9f5bcaca5d2db5492ad7e5ba1f0d9f50
mechmerlin <mechmerlin@gmail.com>
2019-10-03 09:07:59 -0700

    reorg from backslash to pgdn

--- keyboards/duck/orion/v3/v3.h
+++ keyboards/duck/orion/v3/v3.h
@@ -22,14 +22,14 @@
 #define LAYOUT_tkl_ansi( \
     K5A,      K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q,  \
     K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M,      K4O, K4N, K40, K4P, \
-    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,      K3O, K3N, K3Q, K3P, \
+    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,      K3N, K3O, K3P, K3Q, \
     K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L,           K2N, \
     K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J,      K1K, K1N, K1P, \
     K0A, K0B, K0C,                     K0F, K0I,      K0K,      K0M, K0N, K0O, K0P, K0Q \
 ) { \
   { K5A, ___, K5C, K5D, K5E, K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q }, \
   { K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, ___, K4O, K4P, ___ }, \
-  { K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, ___, K3O, K3P, K3Q }, \
+  { K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3O, K3P, K3Q }, \
   { K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, ___, K2N, ___, ___, ___ }, \
   { K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, ___, ___, K1N, ___, K1P, ___ }, \
   { K0A, K0B, K0C, ___, ___, K0F, ___, ___, K0I, ___, K0K, ___, K0M, K0N, K0O, K0P, K0Q }  \
