c0e885888da5ff0335a9ed3053c9daecf956526a
Xiao Yu <xiao@xster.net>
2019-10-02 23:38:33 -0700

    different rgb modes

--- keyboards/xiudi/xd75/keymaps/xo/keymap.c
+++ keyboards/xiudi/xd75/keymaps/xo/keymap.c
@@ -15,7 +15,7 @@
 
 // You might want to alter this to something that matches the keycap LED colors
 // you installed.
-#define BASE_COLOR RGB_BLUE
+#define BASE_COLOR 154, 255, 255
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho_5x15(
@@ -26,16 +26,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TT(1), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC, KC_ENT, KC_RGUI, KC_RALT, KC_RCTL, TT(1)),
   [1] = LAYOUT_ortho_5x15(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_P7, KC_P8, KC_P9, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-    LCTL(KC_GRV), KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P4, KC_P5, KC_P6, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18,
-    KC_CAPS, KC_TRNS, KC_TRNS, KC_MSTP, RGB_SAD, RGB_SAI, KC_P1, KC_P2, KC_P3, KC_PLUS, KC_TRNS, QK_BOOT, KC_MUTE, KC_VOLD, KC_VOLU,
-    MO(2), KC_PSCR, KC_PAUS, KC_APP, RGB_VAD, RGB_VAI, KC_P0, KC_SLSH, KC_PDOT, KC_PENT, KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT,
-    TG(1), KC_TRNS, RGB_TOG, KC_SCRL, RGB_RMOD, RGB_MOD, KC_NUM, KC_TRNS, KC_ASTR, KC_PENT, KC_PENT, MO(1), KC_TRNS, KC_TRNS, TG(1)),
+    LCTL(KC_GRV), KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_PMNS, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PAST, KC_PSLS, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU,
+    MO(2), KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_P0, KC_SLSH, KC_PDOT, KC_PENT, KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,
+    TG(1), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ASTR, KC_PENT, KC_PENT, MO(1), KC_TRNS, KC_TRNS, TG(1)),
   [2] = LAYOUT_ortho_5x15(
     QK_BOOT, KC_SLEP, KC_WAKE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_DOWN,BL_UP,  BL_TOGG,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, RGB_TOG)
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_DOWN, BL_UP, BL_TOGG,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_RMOD, RGB_MOD, RGB_SAD, RGB_SAI, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, RGB_TOG)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -47,10 +47,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   if (IS_MOD(keycode)) {
     if (record->event.pressed) {
-      rgblight_setrgb(RGB_RED);
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+      rgblight_sethsv_noeeprom(HSV_RED);
     } else {
-      rgblight_setrgb(BASE_COLOR);
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_KNIGHT + 1);
+      rgblight_sethsv_noeeprom(BASE_COLOR);
     }
+    rgblight_enable();
   }
 
   return true;
@@ -59,14 +62,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
     case 0:
-      rgblight_setrgb(BASE_COLOR);
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_KNIGHT + 1);
+      rgblight_sethsv_noeeprom(BASE_COLOR);
       break;
     case 1:
-      rgblight_setrgb(RGB_GREEN);
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+      rgblight_sethsv_noeeprom(HSV_GREEN);
       break;
     case 2:
-      rgblight_setrgb(RGB_PURPLE);
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+      rgblight_sethsv_noeeprom(HSV_PURPLE);
       break;
     }
+  rgblight_enable();
   return state;
 }
