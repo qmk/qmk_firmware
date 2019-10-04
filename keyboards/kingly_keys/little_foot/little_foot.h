7fa569f584aaa9fa0d0b2b8c97be8bc7e46a4c94
TheRoyalSweatshirt <TheRoyalSweatshirt@github.com>
2019-10-04 15:57:51 -0500

    update little_foot.h

--- keyboards/kingly_keys/little_foot/little_foot.h
+++ keyboards/kingly_keys/little_foot/little_foot.h
@@ -16,7 +16,7 @@
     { k11, k12, k13, k14, k15, k16, k17, k18, k19, k110 }, \
     { k21, k22, k23, k24, k25, k26, k27, k28, k29, k210 }, \
     { k31, k32, k33, k34, k35, k36, k37, k38, k39, k310 }, \
-    { XXX, k42, XXX, XXX, k45, k46, XXX, XXX, k49,  XXX } \
+    { XXX, k42, XXX, XXX, k45, k46, XXX, XXX, k49, XXX  } \
 }
 
 #define LAYOUT_big_space_base( \
