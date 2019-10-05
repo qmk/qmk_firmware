d0b2b9b452f66061601caddb056b73f47b9082ce
jorgemanzo <jmanzo203689@gmail.com>
2019-10-05 12:22:33 -0700

    flash now has arguments that make sense
    arguments now look like qmk flash [-h] [-kb KEYBOARD] [-km KEYMAP] [filename] bootloader
    
    where bootloader is always required, and you specify either -kb and -km or filename.
    
    the bootloader list would be better off listen when -h is specified I think.

--- lib/python/qmk/cli/flash.py
+++ lib/python/qmk/cli/flash.py
@@ -1,6 +1,7 @@
-"""Compile a QMK Firmware.
+"""Compile and flash QMK Firmware
 
 You can compile a keymap already in the repo or using a QMK Configurator export.
+You can also specify makefile options.
 """
 import json
 import os
@@ -13,22 +14,45 @@ from milc import cli
 import qmk.keymap
 import qmk.path
 
+def print_bootloader_help():
+    cli.log.error('You must supply a configuration file and bootloader,')
+    cli.echo('or a -kb keyboard, -km keymap, and bootloader.')
+    cli.echo('For example: qmk flash -kb planck/rev6 -km default dfu-util')
+    cli.echo('or qmk flash 1upkeyboards_60hse_default.json dfu-util')
+    cli.echo('For more info, visit https://docs.qmk.fm/#/flashing')
+    cli.echo('Bootloaders:')
+    cli.echo('dfu')
+    cli.echo('dfu-ee')
+    cli.echo('dfu-split-left')
+    cli.echo('dfu-split-right')
+    cli.echo('avrdude')
+    cli.echo('BootloadHID')
+    cli.echo('dfu-util')
+    cli.echo('dfu-util-split-left')
+    cli.echo('dfu-util-split-right')
+    cli.echo('st-link-cli')
 
-@cli.argument('filename', nargs='?', arg_only=True, type=FileType('r'), help='The configurator export to flash')
-@cli.argument('-kb', '--keyboard', help='The keyboard to build a firmware for. Ignored when a configurator export is supplied.')
-@cli.argument('-km', '--keymap', help='The keymap to build a firmware for. Ignored when a configurator export is supplied.')
-@cli.argument('-tg', '--target', help='The flash command, corresponding to qmk\'s make options (dfu, dfu-util, teensy, etc).')
+@cli.argument('bootloader', arg_only=True, help='The flash command, corresponding to qmk\'s make options of bootloaders.')
+@cli.argument('filename', nargs='?', arg_only=True, help='The configurator export JSON to compile. Use this if you dont want to specify a keymap and keyboard.')
+@cli.argument('-km', '--keymap', help='The keymap to build a firmware for. Use this if you dont have a configurator file. Ignored when a configurator file is supplied.')
+@cli.argument('-kb', '--keyboard', help='The keyboard to build a firmware for. Use this if you dont have a configurator file. Ignored when a configurator file is supplied.')
 @cli.subcommand('QMK FLash.')
 def flash(cli):
-    """Log a friendly greeting.
-    """
-    target = 'flash'
-    if cli.config.flash.target:
-    	target = cli.config.flash.target
+    """Compile and or flash QMK Firmware or keyboard/layout
 
+    If a Configurator JSON export is supplied this command will create a new keymap. Keymap and Keyboard arguments
+    will be ignored.
+
+    If no file is supplied, keymap and keyboard are expected.
+
+    A bootloader is always required
+
+    """
     if cli.args.filename:
         # Parse the configurator json
-        user_keymap = json.load(cli.args.filename)
+        file = open(cli.args.filename)
+        user_keymap = json.load(file)
+        file.close()
 
         # Generate the keymap
         keymap_path = qmk.path.keymap(user_keymap['keyboard'])
@@ -36,15 +60,15 @@ def flash(cli):
         qmk.keymap.write(user_keymap['keyboard'], user_keymap['keymap'], user_keymap['layout'], user_keymap['layers'])
         cli.log.info('Wrote keymap to {fg_cyan}%s/%s/keymap.c', keymap_path, user_keymap['keymap'])
 
-        # Flash the keymap
-        command = ['make', ':'.join((user_keymap['keyboard'], user_keymap['keymap'], target))]
+        # Compile the keymap
+        command = ['make', ':'.join((user_keymap['keyboard'], user_keymap['keymap'], cli.args.bootloader))]
 
-    elif cli.config.flash.keyboard and cli.config.flash.keymap:
+    elif cli.args.keyboard and cli.args.keymap:
         # Generate the make command for a specific keyboard/keymap.
-        command = ['make', ':'.join((cli.config.flash.keyboard, cli.config.flash.keymap, target))]
+        command = ['make', ':'.join((cli.args.keyboard, cli.args.keymap, cli.args.bootloader))]
 
     else:
-        cli.log.error('You must supply a configurator export or both `--keyboard` and `--keymap`. Optionally, a --target make option.')
+        print_bootloader_help()
         return False
 
     cli.log.info('Flashing keymap with {fg_cyan}%s\n\n', ' '.join(command))
