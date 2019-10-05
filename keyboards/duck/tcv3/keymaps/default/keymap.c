314ddbc6937598418cfd86f774acddc0bb1d00f3
mechmerlin <mechmerlin@gmail.com>
2019-10-04 18:44:59 -0700

    change the dates over to 2019

--- keyboards/duck/tcv3/keymaps/default/keymap.c
+++ keyboards/duck/tcv3/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 MechMerlin <mechmerlin@gmail.com>
+/* Copyright 2019 MechMerlin <mechmerlin@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
