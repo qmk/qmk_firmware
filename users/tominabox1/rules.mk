acc5acf3b7f6a6cf725e71452f93bf5afcfabe70
TJ Campie <tom.campie@gmail.com>
2019-10-05 11:38:55 -0500

    cleaning up a few maps and rules

--- users/tominabox1/rules.mk
+++ users/tominabox1/rules.mk
@@ -10,10 +10,17 @@ NKRO_ENABLE = yes
 ifeq ($(strip $(KEYBOARD)), crkbd/rev1)
 RGB_MATRIX_ENABLE = WS2812
 EXTRAFLAGS += -flto
-
 BOOTLOADER = qmk-dfu
-
 OLED_DRIVER_ENABLE = yes
 endif
+
+ifeq ($(strip $(KEYBOARD)), lazydesigners/dimple)
+BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+endif
+
 
 SRC += tominabox1.c
