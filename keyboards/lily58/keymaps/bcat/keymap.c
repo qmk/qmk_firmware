bc300b4fba88efce2d0c6c55a3f64e7cebcd09b0
Jonathan Rascher <jon@bcat.name>
2019-10-05 01:22:21 -0500

    Rebinding shift on Raise is stupid

--- keyboards/lily58/keymaps/bcat/keymap.c
+++ keyboards/lily58/keymaps/bcat/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_LOWER] = LAYOUT(
         _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
         KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        _______,  _______,  _______,  _______,  _______,  _______,
+        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        KC_INS,   KC_DEL,   _______,  _______,  _______,  _______,
         KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  KC_LBRC,  KC_RBRC,  KC_GRV,   KC_BSLS,  _______,  _______,
                                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
         KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,
         _______,  _______,  _______,  _______,  _______,  _______,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_HOME,  KC_PGUP,
-        KC_INS,   KC_DEL,   KC_CAPS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_TILD,  KC_PIPE,  KC_END,   KC_PGDN,
+        _______,  _______,  KC_CAPS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_TILD,  KC_PIPE,  KC_END,   KC_PGDN,
                                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
