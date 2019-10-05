ac2978ec1cc8b3197e4a8702e926b16298b1afb1
Jonathan Rascher <jon@bcat.name>
2019-10-05 01:09:00 -0500

    Move nav keys to more consistent locations

--- keyboards/lily58/keymaps/bcat/keymap.c
+++ keyboards/lily58/keymaps/bcat/keymap.c
@@ -8,10 +8,7 @@ enum layer {
 };
 
 #define LY_LWR MO(LAYER_LOWER)
-
 #define LY_RSE MO(LAYER_RAISE)
-
-#define LY_ADJST LT(LAYER_ADJUST, KC_APP)
 
 #define KY_CESC LCTL_T(KC_ESC)
 
@@ -21,13 +18,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
         KY_CESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_LBRC,  KC_RBRC,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-                                      KC_LGUI,  KC_LALT,  LY_LWR,   KC_SPC,   KC_ENT,   LY_RSE,   KC_RALT,  LY_ADJST
+                                      KC_LGUI,  KC_LALT,  LY_LWR,   KC_SPC,   KC_ENT,   LY_RSE,   KC_RALT,  KC_APP
     ),
 
     [LAYER_LOWER] = LAYOUT(
         _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
         KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_INS,   _______,
+        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        _______,  _______,  _______,  _______,  _______,  _______,
         KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  KC_LBRC,  KC_RBRC,  KC_GRV,   KC_BSLS,  _______,  _______,
                                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
@@ -35,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_RAISE] = LAYOUT(
         _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
         KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,
-        _______,  _______,  _______,  _______,  _______,  _______,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_DEL,   _______,
-        _______,  _______,  KC_CAPS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_TILD,  KC_PIPE,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_HOME,  KC_PGUP,
+        KC_INS,   KC_DEL,   KC_CAPS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_TILD,  KC_PIPE,  KC_END,   KC_PGDN,
                                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
@@ -48,3 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, LAYER_LOWER, LAYER_RAISE, LAYER_ADJUST);
+}
