38aefaf78e3d9f17ef561f031679a02c9fba869c
Joel Challis <git@zvecr.com>
2019-10-05 16:57:00 +0100

    ARM - Initial backlight support (#6487)
    * Move AVR backlight to own file, add borrowed ARM implementation
    
    * Tiny fix for backlight custom logic
    
    * Remove duplicate board from rebase
    
    * Fix f303 onekey example
    
    * clang-format
    
    * clang-format
    
    * Remove backlight keymap debug
    
    * Initial pass of ARM backlight docs
    
    * Initial pass of ARM backlight docs - resolve todos
    
    * fix rules validation logic
    
    * Add f072 warning
    
    * Add f072 warning
    
    * tidy up breathing in backlight keymap
    
    * tidy up breathing in backlight keymap
    
    * add missing break to backlight keymap

--- keyboards/handwired/onekey/bluepill/config.h
+++ keyboards/handwired/onekey/bluepill/config.h
@@ -21,3 +21,7 @@
 #define MATRIX_COL_PINS { B0 }
 #define MATRIX_ROW_PINS { A7 }
 #define UNUSED_PINS
+
+#define BACKLIGHT_PIN           A0
+#define BACKLIGHT_PWM_DRIVER    PWMD2
+#define BACKLIGHT_PWM_CHANNEL   1
