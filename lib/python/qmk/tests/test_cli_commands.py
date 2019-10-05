897888db419239f013561b155de5993b1966820e
jorgemanzo <jmanzo203689@gmail.com>
2019-10-04 23:38:34 -0700

    Add CLI command for flashing a keyboard
    A new CLI subcommand was added, flash, which behaves very similar to the already present compile CLI comamnd, but with the added ability to target a bootloader. The command is used like so: qmk flash [-h] [-b] [-kb KEYBOARD] [-km KEYMAP] [-bl BOOTLOADER] [filename].
    
    A -kb <keyboard> and -km <keymap> is expected, or a configurator export JSON filename. A bootloader can be specified using -bl <target>, and if left unspecified, the target is assumed to be :flash. -bl can be used to list the available bootloaders.
    
    If -km <keymap> is provided, but no -kb <keyboard>, then a message is printed suggesting the user to run qmk list_keyboards.

--- lib/python/qmk/tests/test_cli_commands.py
+++ lib/python/qmk/tests/test_cli_commands.py
@@ -13,6 +13,9 @@ def test_cformat():
 def test_compile():
     assert check_subcommand('compile', '-kb', 'handwired/onekey/pytest', '-km', 'default').returncode == 0
 
+def test_flash():
+    assert check_subcommand('flash', '-b').returncode == 1
+    assert check_subcommand('flash').returncode == 1
 
 def test_config():
     result = check_subcommand('config')
