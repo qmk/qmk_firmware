7803e47a9fcf73c8abe25f7669d82e6058459681
joooosh <bricsio.benavides@uqconnect.edu.au>
2019-10-05 18:22:04 +1000

    Renamed directory joooosh to joooosh_hhkb... Removed redundant KC_TRNS alias #define... Updated to use KC_TRNS alias defined in QMK_KEYBOARD_H.

--- keyboards/dz60/keymaps/joooosh_hhkb/keymap.c
+++ keyboards/dz60/keymaps/joooosh_hhkb/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#define ______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -42,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------------------------------------------------------------'
  */
 	LAYOUT_60_hhkb(
-		KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,    KC_INS,   RESET,
-		RGB_TOG,         ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_PSCR, KC_SLCK, KC_PAUS,  KC_UP,     ______,   KC_CLR,
-		KC_CAPS,         KC_VOLD, KC_VOLU, KC_MUTE, ______,  ______,  KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT,  KC_RIGHT,  KC_RETURN,
-		______,          ______,  ______,  ______,  ______,  ______,  KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN,  ______,     ______,
-		        ______,           ______,                    ______,                             KC_APP,            ______),
+		KC_GRV, KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_F11,   KC_F12,    KC_INS,   RESET,
+		RGB_TOG,         _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR, KC_SLCK, KC_PAUS,  KC_UP,     _______,   KC_CLR,
+		KC_CAPS,         KC_VOLD, KC_VOLU,   KC_MUTE,  _______,  _______,  KC_PAST,  KC_PSLS,  KC_HOME, KC_PGUP, KC_LEFT,  KC_RIGHT,  KC_RETURN,
+		_______,         _______,  _______,  _______,  _______,  _______,  KC_PPLS,  KC_PMNS,  KC_END,  KC_PGDN, KC_DOWN,  _______,   _______,
+		        _______,           _______,                    _______,                             KC_APP,            _______),
 };
