6cb2dc4b2849b2073fbea3dba10a419a748e997e
Pittyolo <kovacspitti@gmail.com>
2019-10-05 19:03:48 +0200

    Update keyboards/xd75/keymaps/pitty/keymap.c
    Thanks!
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/xd75/keymaps/pitty/keymap.c
+++ keyboards/xd75/keymaps/pitty/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "keymap_extras/keymap_hungarian.h" 
+#include "keymap_hungarian.h" 
 
 enum XD75_layers {
   _QWERTY,
