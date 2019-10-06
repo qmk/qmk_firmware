98c1091e67d106cd6548064f95d45196233130f6
Developer <anotherdeveloper@icloud.com>
2019-10-06 21:36:42 +0800

    Add userspace layers

--- users/yet-another-developer/yet-another-developer.h
+++ users/yet-another-developer/yet-another-developer.h
@@ -1,2 +1,19 @@
 #pragma once
+// #include "quantum.h"
+// #include "version.h"
+// #include "eeprom.h"
 #include "wrappers.h"
+#include "process_records.h"
+
+/* Define layer names */
+enum userspace_layers {
+    _QWERTY  = 0,
+    _NUMLOCK = 0,
+    _COLEMAK,
+    _MODS, /* layer 8 */
+    _MACROS,
+    _MEDIA,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
