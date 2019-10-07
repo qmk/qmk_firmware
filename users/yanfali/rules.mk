6bed2394866f0b98ac9b13a82999c875abc9e44a
Yan-Fa Li <yanfali@gmail.com>
2019-10-07 13:31:11 -0700

    [Keymap] Community layout for hhkb (#6961)

--- users/yanfali/rules.mk
+++ users/yanfali/rules.mk
@@ -2,5 +2,11 @@ BOOTMAGIC = lite
 DYNAMIC_KEYMAP_ENABLE = no
 CONSOLE_ENABLE = yes
 COMMAND_ENABLE = yes
-AUDIO_ENABLE = yes
 LINK_TIME_OPTIMIZATION_ENABLE = yes
+
+ifneq (, $(findstring tokyo60, $(KEYBOARD)))
+    AUDIO_ENABLE = no
+else
+    AUDIO_ENABLE = yes
+endif
+
