966f5100147fe8b9f24d9e6b9341bc644a1a5776
mechmerlin <mechmerlin@gmail.com>
2019-10-06 22:08:59 -0700

    formatting and whitespace changes to better see where the keys are in the keymap

--- keyboards/duck/tcv3/tcv3.h
+++ keyboards/duck/tcv3/tcv3.h
@@ -20,7 +20,7 @@
 #define XXX KC_NO
 
 #define LAYOUT_all( \
-    K00, K01,   K02, K04, K05, K06, K07, K09, K0A, K0B, K0C,           K0D, K0E, K0F, K0G,   K0J, K0I, \
+    K00, K01,   K02,      K04, K05, K06, K07,   K09, K0A, K0B, K0C,    K0D, K0E, K0F, K0G,   K0J, K0I, \
     K10, K11,   K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F, K1G,   K1H, K1I, \
     K20, K21,   K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E,      K2G,   K2H, K2I, \
     K30, K31,   K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E,      K3G,   K3H, K3I, \
@@ -37,7 +37,7 @@
 
 
 #define LAYOUT( \
-    K00, K01,   K02, K04, K05, K06, K07, K09, K0A, K0B, K0C,           K0D, K0E, K0F, K0G,   K0J, K0I, \
+    K00, K01,   K02,      K04, K05, K06, K07,   K09, K0A, K0B, K0C,    K0D, K0E, K0F, K0G,   K0J, K0I, \
     K10, K11,   K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E,      K1G,   K1H, K1I, \
     K20, K21,   K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E,      K2G,   K2H, K2I, \
     K30, K31,   K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,           K3G,   K3H, K3I, \
