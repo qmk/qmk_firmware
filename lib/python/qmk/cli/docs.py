f8e19dead1e7fc5d2bd5a58f1e02028ade809d6b
fauxpark <fauxpark@gmail.com>
2019-10-09 01:58:53 +1100

    Apply suggestions from code review
    Co-Authored-By: skullydazed <skullydazed@users.noreply.github.com>

--- lib/python/qmk/cli/docs.py
+++ lib/python/qmk/cli/docs.py
@@ -10,13 +10,13 @@ class DocsHandler(http.server.SimpleHTTPRequestHandler):
         super().__init__(*args, directory='docs', **kwargs)
 
 
-@cli.argument('-p', '--port', default=8080, help='Port number to use.')
-@cli.subcommand('Local instance of QMK docs')
+@cli.argument('-p', '--port', default=8936, type=int, help='Port number to use.')
+@cli.subcommand('Run a local webserver for QMK documentation.')
 def docs(cli):
     """Spin up a local HTTPServer instance for the QMK docs.
     """
     with http.server.HTTPServer(('', cli.config.docs.port), DocsHandler) as httpd:
-        cli.log.info("Serving QMK docs locally on port %d.", cli.config.docs.port)
+        cli.log.info("Serving QMK docs at http://localhost:%d/", cli.config.docs.port)
         cli.log.info("Press Control+C to exit.")
 
         httpd.serve_forever()
