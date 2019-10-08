8efe2aaef1fb3b59e2edc0690891dd9f46c65ca4
Jono Warren <34277641+Micro-Biology@users.noreply.github.com>
2019-10-08 12:18:13 +0100

    Update keyboards/dz60/keymaps/iso_vim_arrow_split_rs/keymap.c
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/dz60/keymaps/iso_vim_arrow_split_rs/keymap.c
+++ keyboards/dz60/keymaps/iso_vim_arrow_split_rs/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl |GUI|Alt  |         Space         |Lft|Dwn|Up |Rht|vD |
    * `-----------------------------------------------------------'
    */
-  LAYOUT_60_stand_stag_all(
+  LAYOUT_60_iso_5x1u_split_bs_rshift_spc(
 //QWERTY Base layer:
 //  1        2        3        4        5        6        7        8        9        10       11       12       13       14       15
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
