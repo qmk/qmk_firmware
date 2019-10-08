9fe7b406cb43e334d588e69b14c3fa41ae4c4925
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2019-10-08 13:24:20 -0500

    [Keyboard] Move existing boards to Kingly_Keys and add more boards (#6879)
    * try to fix and orginize to Kingly_Keys subfolder and add various keyboard support
    
    * fixed layout nomenclature and rules.mk pref
    
    * modified readme for smd_milk
    
    * fixed layout name in little_foot.h
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * remove old stand-alone keyboard folders
    
    * Fixed missing comma in littlefoot keymap
    
    * remove OLED code in romac_plus.c
    
    * Update rules.mk
    
    * Update readme.md
    
    * Apply suggestions from code review
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Update rules.mk
    
    * Update rules.mk
    
    * Update keymap.c
    
    * Update keymap.c
    
    * Update keymap.c
    
    * fix little_foot.h layouts, delete smd_milk readme.md
    
    * Fix ALpha Edits
    
    * Fix ALpha Edits p.2
    
    * update little_foot.h
    
    * fix little_foot.h p.2
    
    * Update keyboards/kingly_keys/little_foot/little_foot.h
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>
    
    * Update keyboards/kingly_keys/little_foot/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/kingly_keys/romac_plus/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Modify config.h for cleaned up PCB.

--- keyboards/kingly_keys/romac/keymaps/default/keymap.c
+++ keyboards/kingly_keys/romac/keymaps/default/keymap.c
@@ -25,13 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_7, KC_8, KC_9, \
 		KC_4, KC_5, KC_6, \
 		KC_1, KC_2, KC_3, \
-		MO(_FN1), KC_0, KC_ENT \
+		MO(1), KC_0, KC_DOT \
 	),
   
 	[_FN1] = LAYOUT(
 		KC_TRNS, KC_HOME, KC_PGUP, \
 		KC_TRNS, KC_END, KC_PGDN, \
 		KC_TRNS, KC_TRNS, KC_TRNS, \
-		KC_TRNS, KC_TRNS, KC_DOT \
+		KC_TRNS, KC_TRNS, KC_ENT \
 	)
 };
