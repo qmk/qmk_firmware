45617d706bfbf92958efaefbd2c3a70d64727ed4
cfbender <codyfbender@gmail.com>
2019-10-07 20:04:17 -0600

    Rename in __init__.py

--- lib/python/qmk/cli/__init__.py
+++ lib/python/qmk/cli/__init__.py
@@ -9,7 +9,7 @@ from . import doctor
 from . import hello
 from . import json
 from . import list
-from . import kle2qmk
+from . import kle2json
 from . import new
 from . import pyformat
 from . import pytest
