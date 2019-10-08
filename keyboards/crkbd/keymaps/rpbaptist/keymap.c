20d49e270238e1cb18bc84c241efb1acd30de743
Richard Baptist <rpbaptist@gmail.com>
2019-10-08 16:43:59 +0200

    Swap colors

--- keyboards/crkbd/keymaps/rpbaptist/keymap.c
+++ keyboards/crkbd/keymaps/rpbaptist/keymap.c
@@ -270,13 +270,13 @@ void rgb_matrix_indicators_user(void) {
                 rgb_matrix_layer_helper(HSV_ORANGE, LED_FLAG_UNDERGLOW);
                 break;
             case _FN:
-                rgb_matrix_layer_helper(HSV_BLUE, LED_FLAG_UNDERGLOW);
+                rgb_matrix_layer_helper(HSV_GOLDENROD, LED_FLAG_UNDERGLOW);
                 break;
             case _NUM:
-                rgb_matrix_layer_helper(HSV_SPRINGGREEN, LED_FLAG_UNDERGLOW);
+                rgb_matrix_layer_helper(HSV_GREEN, LED_FLAG_UNDERGLOW);
                 break;
             case _NAV:
-                rgb_matrix_layer_helper(HSV_CHARTREUSE, LED_FLAG_UNDERGLOW);
+                rgb_matrix_layer_helper(HSV_BLUE, LED_FLAG_UNDERGLOW);
                 break;
             case _UTIL:
                 rgb_matrix_layer_helper(HSV_MAGENTA, LED_FLAG_UNDERGLOW);
