bb68bc3c35c802ae3d87aba7c2deb400aaa71e3d
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-10-09 06:50:07 -0700

    Update keyboards/exent/keymaps/default/keymap.c
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/exent/keymaps/default/keymap.c
+++ keyboards/exent/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,          KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                                  MO(1), KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT), 
 
-    [1] = LAYOUT_65_ansi(\
+    [1] = LAYOUT_65_ansi(
         KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_TRNS,
         BL_TOGG, BL_STEP,  BL_INC,  BL_DEC,  RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         RGB_TOG, RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
