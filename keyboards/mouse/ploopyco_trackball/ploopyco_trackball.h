4f67e50408fc56753cf2f553814763ab4cd31a3c
Drashna Jael're <drashna@live.com>
2019-10-12 18:20:33 -0700

    Convert SPI commands from arduino to LUFA
    But I have no idea if this is actually correct or not

--- keyboards/mouse/ploopyco_trackball/ploopyco_trackball.h
+++ keyboards/mouse/ploopyco_trackball/ploopyco_trackball.h
@@ -18,6 +18,8 @@
 
 #include "quantum.h"
 
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
 /* This a shortcut to help you visually see your layout.
  *
  * The first section contains all of the arguments representing the physical
