eba04ad0a6b4b67d98acd6cf274f4f12592647f1
fauxpark <fauxpark@gmail.com>
2019-10-15 00:31:02 +1100

    Change some boards incorrectly assumed to be halfkay

--- keyboards/handwired/gamenum/rules.mk
+++ keyboards/handwired/gamenum/rules.mk
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = halfkay
+BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
