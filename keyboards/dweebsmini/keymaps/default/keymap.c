7c1e9adeeab273954e18ec3d264bfd2a733276c2
doesntfazer <39163867+doesntfazer@users.noreply.github.com>
2019-10-14 11:40:14 -0400

    Update keymap.c

--- keyboards/dweebsmini/keymaps/default/keymap.c
+++ keyboards/dweebsmini/keymaps/default/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT( \
    KC_GRAVE, KC_1,    KC_2,     KC_3,       KC_4,      KC_5,       KC_LBRC,           KC_RBRC,  KC_6,     KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC, \
-   KC_DEL,  KC_F1,    KC_F2,    KC_F3,      KC_F4,     KC_F5,      KC_PGUP,           KC_PGDN,  KC_F6,    KC_UNDS,    KC_PLUS,    KC_LCBR,    KC_RCBR,    KC_PIPE, \
+   KC_DEL,  KC_F1,    KC_F2,    KC_F3,      KC_F4,     KC_F5,      KC_PGUP,           KC_PGDN,  KC_F6,    KC_MINS,    KC_PLUS,    KC_LCBR,    KC_RCBR,    KC_BSLS, \
    KC_TRNS, KC_F7,    KC_F8,    KC_F9,      KC_F10,    KC_F11,                                  KC_F12,   KC_M,       KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT, \
                                 ADJUST,     KC_LGUI,   RAISE,      KC_SPACE,          KC_ENTER, LOWER,    KC_RALT,     KC_RCTL  \
 ),
