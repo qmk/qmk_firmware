347ae48ad57e89fa21ed2641c035146f7880846a
Jonathan Rascher <jon@bcat.name>
2019-10-15 21:33:37 -0500

    Move function keys to Raise layer

--- keyboards/lily58/keymaps/bcat/keymap.c
+++ keyboards/lily58/keymaps/bcat/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_LOWER] = LAYOUT(
         _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
         KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_UNDS,
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F11,   KC_F12,   KC_INS,   KC_DEL,   _______,  _______,
-        _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,  _______,  KC_TILD,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,                      KC_TILD,  KC_LCBR,  KC_RCBR,  KC_PIPE,  _______,  _______,
+        _______,  _______,  KC_CAPS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  KC_GRV,   KC_LBRC,  KC_RBRC,  KC_BSLS,  _______,  _______,
                                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_RAISE] = LAYOUT(
         _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
         KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-        _______,  _______,  _______,  _______,  _______,  _______,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_HOME,  KC_PGUP,
-        _______,  _______,  KC_CAPS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,  _______,  KC_GRV,   KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_END,   KC_PGDN,
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_HOME,  KC_PGUP,
+        _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,  _______,  KC_F11,   KC_F12,   KC_INS,   KC_DEL,   KC_END,   KC_PGDN,
                                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
