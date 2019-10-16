0fb38908d775ae4bd10faa882a78f539c34521ef
Ethan Madden <emadden@dell.com>
2019-10-16 14:10:03 -0700

    Double backspace + changed layer detection

--- keyboards/primekb/prime_e/keymaps/jetpacktuxedo/keymap.c
+++ keyboards/primekb/prime_e/keymaps/jetpacktuxedo/keymap.c
@@ -17,14 +17,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_ESC,        KC_Q,    KC_W, KC_E,     KC_R,    KC_T,    KC_Y,        KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_DEL,
+    KC_ESC,        KC_Q,    KC_W, KC_E,     KC_R,    KC_T,    KC_Y,        KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_BSPC,
     LT(2, KC_TAB), KC_A,    KC_S, KC_D,     KC_F,    KC_G,    KC_H,        KC_J,    KC_K,    KC_L,    KC_SCLN, LT(2, KC_ENT),
     KC_LSFT,       KC_Z,    KC_X, KC_C,     KC_V,    KC_B,    LT(3, KC_B), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_LCTL,       KC_LGUI,       KC_LALT,  LT(1, KC_SPC),    LT(1, KC_SPC),        KC_RALT,          KC_RGUI, KC_RCTL
   ),
 
   [1] = LAYOUT(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  KC_TRNS,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  KC_DEL,
     KC_TRNS, KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_TRNS,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
     KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,              KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS
@@ -60,17 +60,17 @@ void matrix_init_user(void) {
 //function for layer indicator LED
 uint32_t layer_state_set_user(uint32_t state)
 {
-    if (state & (1<<1)) {
+    if (layer_state_cmp(state, 1)) {
     writePinHigh(B1);
     } else {
         writePinLow(B1);
     }
-    if (state & (1<<2)) {
+    if (layer_state_cmp(state, 2)) {
     writePinHigh(B2);
     } else {
         writePinLow(B2);
     }
-    if (state & (1<<3)) {
+    if (layer_state_cmp(state, 3)) {
     writePinHigh(B3);
     } else {
         writePinLow(B3);
