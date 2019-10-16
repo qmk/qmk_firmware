a4b93c051e929c4a8f31ad2e55b4fca164de11da
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-10-16 19:14:46 -0300

    disable hand swap

--- keyboards/keebio/viterbi/keymaps/rhruiz/config.h
+++ keyboards/keebio/viterbi/keymaps/rhruiz/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* #define MASTER_RIGHT */
 // #define EE_HANDS
 
-#define SWAP_HANDS_ENABLE
+/* #define SWAP_HANDS_ENABLE */
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
