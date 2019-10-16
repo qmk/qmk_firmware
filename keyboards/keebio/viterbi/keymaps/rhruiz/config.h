fc3b424950f341eecb07908f405d0f630e4f68f4
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-10-16 17:38:38 -0300

    enable swap hands

--- keyboards/keebio/viterbi/keymaps/rhruiz/config.h
+++ keyboards/keebio/viterbi/keymaps/rhruiz/config.h
@@ -21,8 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-// #define MASTER_RIGHT
+/* #define MASTER_RIGHT */
 // #define EE_HANDS
+
+#define SWAP_HANDS_ENABLE
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
--- keyboards/keebio/viterbi/keymaps/rhruiz/keymap.c
+++ keyboards/keebio/viterbi/keymaps/rhruiz/keymap.c
@@ -11,13 +11,32 @@
 
 // clang-format off
 
+#ifdef SWAP_HANDS_ENABLE
+__attribute__ ((weak))
+const keypos_t hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
+    {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}},
+    {{0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1}, {6, 1}},
+    {{0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}, {6, 2}},
+    {{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}, {6, 3}},
+    {{0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}, {6, 4}},
+
+    {{6, 0}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
+    {{6, 1}, {5, 1}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}},
+    {{6, 2}, {5, 2}, {4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}},
+    {{6, 3}, {5, 3}, {4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}},
+    {{6, 4}, {5, 4}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}}
+};
+#endif
+
+#define KC_CESC CTRLESC
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT_ortho_5x14(
-    KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,   KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS,
+    KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_MINS,   KC_EQL , KC_6,    KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS,
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LBRC,   KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_QUOT,
-    KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_PGUP,   KC_BSLS, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+    KC_CESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_PGUP,   KC_BSLS, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_PGDN,   KC_ENT,  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-    KC__FN1, KC_LCTL, KC_LALT, KC_LGUI, KC__FN1, KC_SPC , KC_SPC ,   KC_BSPC, KC__FN2, KC_RGUI, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
+    KC__FN2, KC_SPC , KC_LCTL, KC_LALT, KC_LGUI, KC__FN1, KC_SPC ,   KC_BSPC, KC__FN2, KC_RGUI, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
   ),
 
   [_VIM_EMACS] = LAYOUT_ortho_5x14(
@@ -37,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_KEY_OVERRIDE] = LAYOUT_ortho_5x14(
-    KC_TILD, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, KC_PIPE,
+    KC_TILD, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, SH_TG,
     _______, _______, _______, _______, _______, _______, KC_LCBR,   KC_RCBR, _______, _______, _______, _______, _______, KC_DQUO,
     _______, _______, _______, _______, _______, _______, KC_PGUP,   KC_PIPE, _______, _______, _______, _______, KC_SCLN, KC_DQUO,
     _______, _______, _______, _______, _______, _______, KC_PGDN,   KC_PENT, _______, _______, KC_LT  , KC_GT  , KC_QUES, _______,
@@ -53,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN2] = LAYOUT_ortho_5x14(
-    _______, KC_SLCK, KC_PAUS, MISCTRL, _______, RGB_VAD, RGB_VAI,   KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC__VDN, KC__VUP, _______,
+    _______, KC_SLCK, KC_PAUS, MISCTRL, RGB_VAD, RGB_VAI, SH_TG  ,   KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC__VDN, KC__VUP, _______,
     _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
