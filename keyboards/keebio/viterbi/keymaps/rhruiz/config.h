8a7a2239d2d84d55d0d110e025f7bfdb1d501961
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-10-19 14:49:37 -0300

    use eeprom to set the master

--- keyboards/keebio/viterbi/keymaps/rhruiz/config.h
+++ keyboards/keebio/viterbi/keymaps/rhruiz/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
 /* #define MASTER_RIGHT */
-// #define EE_HANDS
+#define EE_HANDS
 
 /* #define SWAP_HANDS_ENABLE */
 
