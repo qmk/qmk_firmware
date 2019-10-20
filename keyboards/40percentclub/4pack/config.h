180110216394c04ef37560b7e8ec9db1b1bb342e
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2019-10-20 03:04:15 +0300

    pinout changed with direct pins instead of matrix

--- keyboards/40percentclub/4pack/config.h
+++ keyboards/40percentclub/4pack/config.h
@@ -41,8 +41,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { B0 } // B0 equivalents the ground pin
-#define MATRIX_COL_PINS { E6, D7, C6, D4}
+// #define MATRIX_ROW_PINS { B0 } // B0 equivalents the ground pin
+// #define MATRIX_COL_PINS { E6, D7, C6, D4 }
+#define DIRECT_PINS { \
+    { E6, D7, C6, D4 } \
+}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
