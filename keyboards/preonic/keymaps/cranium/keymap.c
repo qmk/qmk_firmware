4a21f56dbb3393fa59084370de63751526881f74
Romain Gehrig <romain.gehrig@gmail.com>
2019-10-20 11:43:05 +0200

    Bring back the _QWERTY layer code
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/preonic/keymaps/cranium/keymap.c
+++ keyboards/preonic/keymaps/cranium/keymap.c
@@ -18,7 +18,8 @@
 #include "muse.h"
 
 enum preonic_layers {
-  _LOWER = 1,
+  _QWERTY,
+  _LOWER,
   _RAISE,
   _ADJUST
 };
@@ -43,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl |  F12 |  GUI | Alt  |Lower |    Space    |Raise |AltGr |   [  |   ]  |Enter |
  * `-----------------------------------------------------------------------------------'
  */
-LAYOUT_preonic_grid(
+[_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC,
   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
