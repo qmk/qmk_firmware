88ad365062f52067790ed2aedfb7d2ce92192b52
Pentti Laitinen <pentti.laitinen@protonmail.com>
2019-10-21 22:25:02 +0300

    Fix indentation to 4 spaces

--- keyboards/ergodox_infinity/keymaps/nordic_ergo/keymap.c
+++ keyboards/ergodox_infinity/keymaps/nordic_ergo/keymap.c
@@ -44,25 +44,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
-  // left hand
-  KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,  KC_5,   KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,  KC_T,   TG(1),
-  KC_CAPS,  KC_A,   KC_S,   KC_D,   KC_F,  KC_G,
-  KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,  KC_B,   MO(2),
-  KC_LCTRL, MO(3),  KC_EQL, KC_LGUI, KC_LALT,
-                                          KC_NLCK,  KC_SLCK,
-                                          KC_HOME,
-                                          KC_SPC,   KC_DEL, KC_END,
+    // left hand
+    KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,  KC_5,   KC_GRV,
+    KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,  KC_T,   TG(1),
+    KC_CAPS,  KC_A,   KC_S,   KC_D,   KC_F,  KC_G,
+    KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,  KC_B,   MO(2),
+    KC_LCTRL, MO(3),  KC_EQL, KC_LGUI, KC_LALT,
+                                            KC_NLCK,  KC_SLCK,
+                                            KC_HOME,
+                                            KC_SPC,   KC_DEL, KC_END,
 
-  // right hand
-  KC_BSLS,  KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
-  TG(1),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,
-            KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-  MO(2),    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_NONUS_BSLASH,
-  KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT, KC_RBRC,
-  KC_RGUI,  KC_RALT,
-  KC_PGUP,
-  KC_PGDN,  KC_ENT, KC_BSPC
+    // right hand
+    KC_BSLS,  KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
+    TG(1),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,
+              KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
+    MO(2),    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_NONUS_BSLASH,
+    KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT, KC_RBRC,
+    KC_RGUI,  KC_RALT,
+    KC_PGUP,
+    KC_PGDN,  KC_ENT, KC_BSPC
 ),
 /* Keymap 1: Basic layer with functions
  *
@@ -89,24 +89,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [FUNCL] = LAYOUT_ergodox(  // layer 1 : functions
-  // left hand
-  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,
-                                          _______,  _______,
-                                          _______,
-                                          _______,  _______,  _______,
-  // right hand
-  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,
-  _______,
-  _______,  _______,  _______
+    // left hand
+    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,
+                                            _______,  _______,
+                                            _______,
+                                            _______,  _______,  _______,
+    // right hand
+    KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,
+    _______,  _______,
+    _______,
+    _______,  _______,  _______
 ),
 
 /* Keymap 2: Symbol Layer
@@ -132,24 +132,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
-  // left hand
-  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,
-  _______,  DE_EXLM,  DE_AT,    DE_LCBR,  DE_RCBR,  DE_PIPE,  _______,
-  _______,  DE_HASH,  DE_DLR,   DE_LPRN,  DE_RPRN,  DE_GRV,
-  _______,  DE_PERC,  DE_CIRC,  DE_LBRC,  DE_RBRC,  DE_TILD,  _______,
-  _______,  _______,  _______,  _______,  _______,
-                                          RGB_MOD,  BL_TOGG,
-                                          BL_INC,
-                                          RGB_VAD,  RGB_VAI,  BL_DEC,
-  // right hand
-  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-  _______,  KC_UP,    KC_7,     KC_8,     KC_9,     DE_ASTR,  _______,
-            KC_DOWN,  KC_4,     KC_5,     KC_6,     DE_PLUS,  _______,
-  _______,  DE_AMPR,  KC_1,     KC_2,     KC_3,     DE_BSLS,  _______,
-  _______,  KC_DOT,   KC_0,     DE_EQL,   _______,
-  RGB_TOG,  RGB_SLD,
-  _______,
-  _______,  RGB_HUD,  RGB_HUI
+   // left hand
+   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,
+   _______,  DE_EXLM,  DE_AT,    DE_LCBR,  DE_RCBR,  DE_PIPE,  _______,
+   _______,  DE_HASH,  DE_DLR,   DE_LPRN,  DE_RPRN,  DE_GRV,
+   _______,  DE_PERC,  DE_CIRC,  DE_LBRC,  DE_RBRC,  DE_TILD,  _______,
+   _______,  _______,  _______,  _______,  _______,
+                                           RGB_MOD,  BL_TOGG,
+                                           BL_INC,
+                                           RGB_VAD,  RGB_VAI,  BL_DEC,
+   // right hand
+   KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+   _______,  KC_UP,    KC_7,     KC_8,     KC_9,     DE_ASTR,  _______,
+             KC_DOWN,  KC_4,     KC_5,     KC_6,     DE_PLUS,  _______,
+   _______,  DE_AMPR,  KC_1,     KC_2,     KC_3,     DE_BSLS,  _______,
+   _______,  KC_DOT,   KC_0,     DE_EQL,   _______,
+   RGB_TOG,  RGB_SLD,
+   _______,
+   _______,  RGB_HUD,  RGB_HUI
 ),
 
 /* Keymap 3: Media and mouse keys
@@ -176,30 +176,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
-  RESET,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,
-  _______,  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  _______,  _______,
-  _______,  KC_BTN4,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN5,
-  _______,  KC_WH_L,  KC_WH_D,  KC_WH_U,  KC_WH_R,  KC_BTN3,  _______,
-  _______,  _______,  KC_ACL0,  KC_ACL1,  KC_ACL2,
-                                          _______,  _______,
-                                          _______,
-                                          KC_BTN1,  KC_BTN2,  _______,
-  // right hand
-  KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,
-  _______, _______,   KC_VOLD,  KC_MUTE,  KC_VOLU,  _______,  _______,
-           _______,   KC_MPRV,  KC_MSTP,  KC_MPLY,  KC_MNXT,  _______,
-  _______, KC_WSCH,   KC_WBAK,  KC_WHOM,  KC_WREF,  KC_WFWD,  _______,
-  _______, _______,   _______,  _______,  _______,
-  _______, _______,
-  _______,
-  _______, KC_WBAK, KC_WFWD
+   RESET,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,
+   _______,  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  _______,  _______,
+   _______,  KC_BTN4,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN5,
+   _______,  KC_WH_L,  KC_WH_D,  KC_WH_U,  KC_WH_R,  KC_BTN3,  _______,
+   _______,  _______,  KC_ACL0,  KC_ACL1,  KC_ACL2,
+                                           _______,  _______,
+                                           _______,
+                                           KC_BTN1,  KC_BTN2,  _______,
+   // right hand
+   KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,
+   _______, _______,   KC_VOLD,  KC_MUTE,  KC_VOLU,  _______,  _______,
+            _______,   KC_MPRV,  KC_MSTP,  KC_MPLY,  KC_MNXT,  _______,
+   _______, KC_WSCH,   KC_WBAK,  KC_WHOM,  KC_WREF,  KC_WFWD,  _______,
+   _______, _______,   _______,  _______,  _______,
+   _______, _______,
+   _______,
+   _______, KC_WBAK, KC_WFWD
 ),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-  // MACRODOWN only works in this function
-      switch(id) {
+    // MACRODOWN only works in this function
+    switch(id) {
         case 0:
         if (record->event.pressed) {
           SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
@@ -210,7 +210,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
           eeconfig_init();
         }
         break;
-      }
+    }
     return MACRO_NONE;
 };
 
