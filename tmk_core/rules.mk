968fcd9d3f5ed2840928948566cdd762190c9d26
mtei <2170248+mtei@users.noreply.github.com>
2019-10-22 04:51:59 +0900

    comment update in tmk_core/rules.mk

--- tmk_core/rules.mk
+++ tmk_core/rules.mk
@@ -52,7 +52,8 @@ $(foreach OUTPUT,$(OUTPUTS),\
 
 #    Listing objects to be stored in library (*.a)
 #    The current binutils 'ar' command cannot support lto objects, so force the -fno-lto option.
-#    If the binutils 'ar' command supports lto objects, remove the following line:
+#    This is a workaround for the 'ar' command not supporting lto objects.
+#    If the 'ar' command supports lto objects, remove the following line:
 $(foreach OUTPUT,$(OUTPUTS),$(eval NO_LTO_OBJ +=$(call NO_LTO_OBJ_FROM_SRC,LIB,$(OUTPUT))))
 
 # Define a list of all objects
