b9fcc6b71dea63ddb3eb39e53de564a26c3316cf
Drashna Jael're <drashna@live.com>
2019-10-21 12:23:43 -0700

    Limit brightness when both RGB features are enabled

--- layouts/community/ergodox/drashna_glow/config.h
+++ layouts/community/ergodox/drashna_glow/config.h
@@ -3,7 +3,12 @@
 #include "../drashna/config.h"
 
 #undef PRODUCT
-#define PRODUCT         DrashnaDox - Hacked ErgoDox EZ Glow
+#define PRODUCT DrashnaDox - Hacked ErgoDox EZ Glow
 
 #undef RGB_MATRIX_LED_PROCESS_LIMIT
 #undef RGB_MATRIX_LED_FLUSH_LIMIT
+
+#if defined(RGBLIGHT_ENABLE) && defined(RGB_MATRIX_ENABLE)
+#    define RGBLIGHT_LIMIT_VAL 175
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS (RGBLIGHT_LIMIT_VAL + 25)
+#endif
