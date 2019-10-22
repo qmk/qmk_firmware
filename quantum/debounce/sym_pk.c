323f422d9406c4d832b0ad48fbb92f42949c62e0
Scott Perry <dev@numist.net>
2019-10-22 13:34:38 -0700

    New non-eager per-key symmetric debouncer

---
+++ quantum/debounce/sym_pk.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2009-2019 Benjamin Holt & Scott Perry <qmk@numist.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "matrix.h"
+#include "timer.h"
+#include "quantum.h"
+#include <stdlib.h>
+
+#ifndef DEBOUNCE
+#    define DEBOUNCE 5
+#endif
+
+/*
+ * This debouncer uses an overflowing stacked counter cribbed with permission
+ * from https://github.com/numist/project-typewriter/blob/master/debounce.h
+ */
+typedef struct {
+    matrix_row_t db0;    // counter bit 0
+    matrix_row_t db1;    // counter bit 1
+    matrix_row_t state;  // debounced state
+} debounce_t;
+
+static inline matrix_row_t debounce_single_row(debounce_t *d, matrix_row_t sample) {
+    matrix_row_t changes, delta = sample ^ d->state;
+    d->db1  = ((d->db1) ^ (d->db0)) & delta;
+    d->db0  = ~(d->db0) & delta;
+    changes = ~(~delta | (d->db0) | (d->db1));
+    d->state ^= changes;
+    return changes;
+}
+
+static debounce_t *debounce_state = 0;
+static uint16_t    timer;
+static bool        primed = false;
+
+void debounce_init(uint8_t num_rows) {
+    if (debounce_state) {
+        free(debounce_state);
+    }
+    debounce_state = (debounce_t *)calloc(num_rows, sizeof(debounce_t));
+}
+
+void debounce(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, bool changed) {
+    if (!primed) {
+        // Initialize debouncer state with current values
+        for (int i = 0; i < num_rows; i++) {
+            cooked[i] = debounce_state[i].state = raw[i];
+        }
+        timer  = timer_read();
+        primed = true;
+    }
+
+    // 2-bit debouncer overflows after 4 samples
+    if (timer_elapsed(timer) > DEBOUNCE / 4) {
+        for (int i = 0; i < num_rows; i++) {
+            debounce_t *d = &debounce_state[i];
+            cooked[i] ^= debounce_single_row(d, raw[i]);
+        }
+        timer = timer_read();
+    }
+}
