77ab20bf5aa76296875277f751448455f6376df1
Eriq M. Adams <eriq.adams@gmail.com>
2019-10-23 12:22:26 +0700

    change  DEBOUNCING_DELAY to DEBOUNCE, remove IS_COMMAND()

--- keyboards/blackplum/config.h
+++ keyboards/blackplum/config.h
@@ -20,7 +20,7 @@
 #define UNUSED_PINS
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -28,10 +28,6 @@
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN C7
