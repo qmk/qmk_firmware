b1c426fa1a96d608e5f9cb80f1580bd75d798f90
skullydazed <skullydazed@users.noreply.github.com>
2019-10-23 14:08:02 -0700

    Update keyboards/1upkeyboards/sweet16/rev1/rules.mk
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/1upkeyboards/sweet16/rev1/rules.mk
+++ keyboards/1upkeyboards/sweet16/rev1/rules.mk
@@ -1,5 +1,5 @@
 MCU = atmega32u4
 BOOTLOADER = caterina
-EXTRAFLAGS += -flto
+LINK_TIME_OPTIMIZATION_ENABLE=yes
 RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
