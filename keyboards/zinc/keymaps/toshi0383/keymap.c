b2c24d62b2286f2c4257104c9a86e0247cd26c86
toshi0383 <t.suzuki326@gmail.com>
2019-10-24 22:23:21 +0900

    cleanup

--- keyboards/zinc/keymaps/toshi0383/keymap.c
+++ keyboards/zinc/keymaps/toshi0383/keymap.c
@@ -19,8 +19,6 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  DLOWER,
-  DRAISE,
   ADJUST,
 };
 
@@ -48,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    KC_ESC, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,             KC_Y,  KC_U,  KC_I, KC_O,   KC_P, KC_BSPACE, \
    KC_TAB, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,             KC_H,  KC_J,  KC_K, KC_L,   KC_RGUI, KC_RSFT, \
    KC_SPC, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,             KC_N,  KC_M,  KC_RCTL, KC_RGUI, KC_RALT, RGUI(KC_RSFT), \
-   _______, _______, _______, _______, LOWER, KC_ENT,     KC_ENT, RAISE, _______, _______, _______, _______ \
+   _______, _______, _______, _______, LOWER, KC_ENT,    KC_ENT, RAISE, _______, _______, _______, _______ \
    ),
 
   /* Lower
@@ -65,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
     [_LOWER] =  LAYOUT_ortho_4x12( \
       KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______,\
       _______, KC_LT, KC_GT, KC_LPRN, KC_RPRN, KC_GRV,       KC_MINS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_PIPE, \
-      _______, _______, _______, _______, KC_EQL, KC_QUES,  KC_UNDS, KC_PLUS, KC_COMM, KC_DOT, KC_SLSH, _______, \
+      _______, _______, _______, _______, KC_EQL, KC_QUES,   KC_UNDS, KC_PLUS, KC_COMM, KC_DOT, KC_SLSH, _______, \
       _______, _______, _______, _______, _______, KC_TRNS,  ADJUST, KC_RGUI, _______, _______, _______, _______ \
       ),
 
@@ -82,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    */
   [_RAISE] = LAYOUT_ortho_4x12( \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  _______, \
-  _______, KC_COLN, KC_SCLN, KC_DQT, KC_QUOT, KC_BSLS,        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, \
+  _______, KC_COLN, KC_SCLN, KC_DQT, KC_QUOT, KC_BSLS,         KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, \
   _______, _______, _______, _______, _______, _______,        ADJUST, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, KC_LGUI, KC_LSFT,        KC_LSFT, _______, _______, _______, _______, _______ \
   ),
