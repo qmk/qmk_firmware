bda18dac1896f9280ac45abd0208921515799687
Brandon Schlack <brandonschlack@gmail.com>
2019-10-24 18:05:57 -0700

    [User] TAP DELAY updates
    * Remove PERMISSIVE_HOLD
    * Add TAP_HOLD_CAPS_DELAY
    * Set TAP_CODE_DELAY only for USBasp boards

--- users/brandonschlack/config.h
+++ users/brandonschlack/config.h
@@ -2,7 +2,7 @@
 
 #define TAPPING_TOGGLE 2
 #define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
+#define TAP_HOLD_CAPS_DELAY 200
 
 #ifdef RGBLIGHT_ENABLE
 #   define RGBLIGHT_SLEEP
@@ -12,9 +12,7 @@
 #   define RGB_THEME_ENABLE
 #endif
 
-#ifdef ENCODER_ENABLE
-#    define TAP_CODE_DELAY 10
-#else
+#ifdef BOOTLOADER_USBASP
 #    define TAP_CODE_DELAY 5
 #endif
 
