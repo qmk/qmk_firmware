d87cf5f9ff564c6ecd68465a39d8cd2576736a6b
J.Flanagan <jrfhoutx@comcast.net>
2019-10-27 11:39:01 -0500

    Update keyboards/sck/m0116b/keymaps/default/keymap.c
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/sck/m0116b/keymaps/default/keymap.c
+++ keyboards/sck/m0116b/keymaps/default/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |   |    M0116B       |   |   |    |    |    | |         |    |    |
    * `-----------------------------------------------------------' `-------------------'
    */
-[1] = LAYOUT_M0116( /* Layer 1 */
+[1] = LAYOUT_m0116_ansi( /* Layer 1 */
 	                                                          RESET,
 		KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
