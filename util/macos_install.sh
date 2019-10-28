bcf442d1739b6e9f2de548fd68f76842835d61ce
Yadunand Prem <yadunut@gmail.com>
2019-10-29 09:13:07 +1100

    Clean macos install script (#7186)

--- util/macos_install.sh
+++ util/macos_install.sh
@@ -2,20 +2,20 @@
 
 util_dir=$(dirname "$0")
 
-if ! brew --version 2>&1 > /dev/null; then
+if ! brew --version >/dev/null 2>&1; then
 	echo "Error! Homebrew not installed or broken!"
 	echo -n "Would you like to install homebrew now? [y/n] "
-	while read ANSWER; do
+	while read -r ANSWER; do
 		case $ANSWER in
-			y|Y)
-				/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-				break
+		y | Y)
+			/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+			break
 			;;
-			n|N)
-				exit 1
+		n | N)
+			exit 1
 			;;
-			*)
-				echo -n "Would you like to install homebrew now? [y/n] "
+		*)
+			echo -n "Would you like to install homebrew now? [y/n] "
 			;;
 		esac
 	done
@@ -26,4 +26,4 @@ brew tap PX4/homebrew-px4
 brew update
 brew install avr-gcc@8 gcc-arm-none-eabi dfu-programmer avrdude clang-format dfu-util python3
 brew link --force avr-gcc@8
-pip3 install -r ${util_dir}/../requirements.txt
+pip3 install -r "${util_dir}/../requirements.txt"
