83c2684d41dc20152e970385384c744028ac4a7c
Jos Boersema <joshb@xs4all.nl>
2019-10-29 13:12:54 +0100

    Updating the comment for two mouse buttons right hand.

--- keyboards/thevankeyboards/minivan/keymaps/josjoha/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/josjoha/keymap.c
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[ _MOV ] = LAYOUT (
 
-	/* _LTR WLft WUp  WDn  WRht  xxx  xxx  Home PgUp PgDn End    Bksp
-	 * LCtl  MLft MUp  MDn  MRht  xxx  xxx  Left Up   Down Right RCtl
-	 * LSht   Btn5 Btn4 Btn3 Butn2 Btn1 xxx  Acc0 Acc1 Acc2 xxx  RSht
+	/* _LTR WLft WUp  WDn  WRht  xxx  Btn3 Home PgUp PgDn End    Bksp
+	 * LCtl  MLft MUp  MDn  MRht  xxx  Btn1 Left Up   Down Right RCtl
+	 * LSht   Btn5 Btn4 Btn3 Butn2 Btn1 Btn2 Acc0 Acc1 Acc2 xxx  RSht
 	 * LAlt   _LTR   ___     ___      ___      ___       _LTR    RAlt
 	 */
 			
