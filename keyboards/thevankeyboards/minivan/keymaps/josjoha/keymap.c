feddfe24ba454a50635efd15ba1a70e9ed9de880
Jos Boersema <joshb@xs4all.nl>
2019-10-29 17:42:53 +0100

    Minor whitespace adjustment

--- keyboards/thevankeyboards/minivan/keymaps/josjoha/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/josjoha/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          * ,+LAlt    Left   ___    ___     ___     ___ Right  Ent+RAlt
          * */
                 
-        TO ( _LTR )     , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_DEL           ,
+        TO ( _LTR )       , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_DEL             ,
         LCTL_T ( KC_BSPC ) , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_BSLS , KC_EQL  , KC_SLSH , KC_LBRC , KC_RBRC , RCTL_T ( KC_GRV )  , 
         LSFT_T ( KC_DOT )   , KC_0    , KC_9    , KC_8    , KC_7    , KC_6    , KC_PIPE , KC_PLUS , KC_QUES , KC_LCBR , KC_RCBR , RSFT_T ( KC_TILD ) , 
         LALT_T ( KC_COMM )   , KC_LEFT , _______ , _______ , _______ , _______ , KC_RGHT  , RALT_T ( KC_ENT )
