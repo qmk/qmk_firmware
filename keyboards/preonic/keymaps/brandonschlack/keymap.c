797cb85f22096523bdb5c732c818fa8f1eb0ca3e
Brandon Schlack <brandonschlack@gmail.com>
2019-10-30 15:27:13 -0700

    [Keymap] Update preonic encoder

--- keyboards/preonic/keymaps/brandonschlack/keymap.c
+++ keyboards/preonic/keymaps/brandonschlack/keymap.c
@@ -130,11 +130,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else {
         if (clockwise) {
-            register_code(KC_PGDN);
-            unregister_code(KC_PGDN);
+            tap_code(KC_VOLU);
         } else {
-            register_code(KC_PGUP);
-            unregister_code(KC_PGUP);
+            tap_code(KC_VOLD);
         }
     }
 }
