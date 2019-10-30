44513d7d7b86785e89eb00f4fbc7c17981a295e8
ai03 <sarcaphx510@gmail.com>
2019-10-29 21:59:25 -0700

    Move board files to match MKII RGB files

--- keyboards/kbdfans/kbd67/mkii_soldered/mkii_soldered.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/mkii_soldered.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "kbd67_mk2_soldered.h"
+#include "mkii_soldered.h"
 
 // Optional override functions below.
 // You can leave any or all of these undefined.
