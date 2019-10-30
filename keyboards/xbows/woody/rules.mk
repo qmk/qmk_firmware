4af5529e2c4ceaaa4a59a54a875668faa23b76d8
moyi4681 <moyi4681@users.noreply.github.com>
2019-10-30 17:47:33 +0800

    Update keyboards/xbows/woody/rules.mk
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/xbows/woody/rules.mk
+++ keyboards/xbows/woody/rules.mk
@@ -1,6 +1,5 @@
 MCU = atmega32u4
 BOOTLOADER = atmel-dfu
-OPT_DEFS += -DBOOTLOADER_SIZE=4096
 BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration(+1000)
 BACKLIGHT_ENABLE = no
 MOUSEKEY_ENABLE = yes              # Mouse keys
