1e7aa8a04240bb2ee7b8edb7fd3268fa654ad1a3
Brandon Schlack <brandonschlack@gmail.com>
2019-10-30 15:29:16 -0700

    [Keymap] Update jnao features
    * Add Mouse Keys
    * Set Command to default (enabled)

--- keyboards/jnao/keymaps/brandonschlack/rules.mk
+++ keyboards/jnao/keymaps/brandonschlack/rules.mk
@@ -1,4 +1,4 @@
 # Build Options
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration(+1000)
-COMMAND_ENABLE = no         # Don't need Command
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 BACKLIGHT_ENABLE = no       # No backlights installed
