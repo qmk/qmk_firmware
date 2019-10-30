52502f56ec7c60b8c7a48e8ede2c0738494102f8
Stephen Hogsten <stephenhogsten@gmail.com>
2019-10-29 20:09:44 -0700

    disable deprecated actions

--- quantum/template/ps2avrgb/config.h
+++ quantum/template/ps2avrgb/config.h
@@ -44,6 +44,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define NO_UART 1
 
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
 /* key combination for magic key command */
 /* defined by default; to change, uncomment and set to the combination you want */
 // #define IS_COMMAND() (get_mods() == MOD_MASK_SHIFT)
