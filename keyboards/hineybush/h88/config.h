b38d1f945c141ebbae36961fe3f64ca847c26283
hineybush <hineybushkeyboards@gmail.com>
2019-10-31 16:18:22 -0400

    update copyright year in files

--- keyboards/hineybush/h88/config.h
+++ keyboards/hineybush/h88/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Josh Hinnebusch
+Copyright 2019 Josh Hinnebusch
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once 
+#pragma once
 
 #include "config_common.h"
 
