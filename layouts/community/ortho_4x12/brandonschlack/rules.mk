19824e5b5f6521aac6000dc7d308f6ce9ddcff1b
brandonschlack <brandonschlack@gmail.com>
2019-10-30 22:26:34 -0700

    [Keymap] Remove preonic keymap, use ortho_5x12

--- layouts/community/ortho_4x12/brandonschlack/rules.mk
+++ layouts/community/ortho_4x12/brandonschlack/rules.mk
@@ -1,3 +1,7 @@
+ifeq ($(strip $(KEYBOARD)), planck/rev6)
+	SRC += muse.c
+endif
+
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 BACKLIGHT_ENABLE = no       # No backlights installed
 
--- layouts/community/ortho_5x12/brandonschlack/rules.mk
+++ layouts/community/ortho_5x12/brandonschlack/rules.mk
@@ -1,3 +1,7 @@
+ifeq ($(strip $(KEYBOARD)), preonic/rev3)
+	SRC += muse.c
+endif
+
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 BACKLIGHT_ENABLE = no       # No backlights installed
 
