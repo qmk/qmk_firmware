a05c095702be992d2b06e3e67fa282cfccc0f51e
moyi4681 <moyi4681@users.noreply.github.com>
2019-10-31 19:51:55 +0800

    Update dz65rgb.c

--- keyboards/dztech/dz65rgb/dz65rgb.c
+++ keyboards/dztech/dz65rgb/dz65rgb.c
@@ -124,6 +124,6 @@ void rgb_matrix_indicators_user(void)
 {
     if (IS_LED_ON(host_keyboard_leds(), USB_LED_CAPS_LOCK))
     {
-        rgb_matrix_set_color(8, 0xFF, 0xFF, 0xFF);
+        rgb_matrix_set_color(30, 0xFF, 0xFF, 0xFF);
     }
 }
