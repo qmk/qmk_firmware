58b4ff213447ee6d9486b67db18cb0b5e81483b1
Ben Weakley <2173281+Defying@users.noreply.github.com>
2019-11-02 19:25:21 -0400

    Update keyboards/redscarf_i/keymaps/4x6_ortho/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/redscarf_i/keymaps/4x6_ortho/keymap.c
+++ keyboards/redscarf_i/keymaps/4x6_ortho/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LEFT, KC_DOWN, KC_RGHT, MO(1)
   ),
 
-  [1] = LAYOUT_4x6_ortho( // Function 1
+  [1] = LAYOUT_ortho_6x4( // Function 1
     _______, _______, _______, _______,
     _______, _______, _______, _______,
     _______, _______, _______, _______,
