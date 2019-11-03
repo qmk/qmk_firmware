81b52ce733cff11d494361562954fc1a01c28651
Jos Boersema <joshb@xs4all.nl>
2019-11-03 14:58:35 +0100

    Updated layer _REV comment with its map (forgot something as usual). (Deletion was added to movement layer not numbers layer where it already is, as mentioned in previous commit.)

--- keyboards/thevankeyboards/minivan/keymaps/josjoha/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/josjoha/keymap.c
@@ -120,10 +120,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [ _REV ] = LAYOUT (
 
-        /* _LTR    WLft WDn  WUp  WRht  xxx  Btn3 Home PgUp PgDn End    Bksp
-         * LCtl     MLft MDn  MUp  MRht  xxx  Btn1 Left Up   Down Right RCtl
-         * LSht      Btn5 Btn4 Btn3 Butn2 Btn1 Btn2 Acc0 Acc1 Acc2 xxx  RSht
-         * Del+LAlt  Left     ___     ___      ___      ___     Right   RAlt
+        /* _LTR    End  PgDn PgUp Home  xxx  xxx  xxx  xxx  xxx  xxx     Del
+         * LCtl     Left Down Up   Right xxx  5    4    3    2    1     RCtl
+         * LSht      xxx  xxx  xxx  xxx   xxx  6    7    8    9    0    RSht
+         * Bspc+LAlt Left   Sht(Tab)  ___      ___      Tab     Right   RAlt
          */
                 
         TO ( _LTR )       , KC_END  , KC_PGDN      , KC_PGUP , KC_HOME , XXXXXXX , XXXXXXX , XXXXXXX           , XXXXXXX , XXXXXXX , XXXXXXX , KC_DEL  ,
