5cf7cbba527259e9a183054f076106faf1f2c7e1
zvecr <git@zvecr.com>
2019-11-04 13:44:25 +0000

    Remove debug flags from keymap

--- keyboards/handwired/onekey/keymaps/rgb/keymap.c
+++ keyboards/handwired/onekey/keymaps/rgb/keymap.c
@@ -5,10 +5,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-
   rgblight_enable_noeeprom();
   rgblight_sethsv_noeeprom_cyan();
   rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL);
