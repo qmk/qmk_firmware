0e3739a2130dc50d93c4f2ed53ea4e53fa4a1cc4
Florian Didron <fd@librem.one>
2019-11-05 20:50:31 +0900

    fix: adds webusb include on test keymaps

--- keyboards/ergodox_ez/keymaps/webusb/keymap.c
+++ keyboards/ergodox_ez/keymaps/webusb/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "webusb.h"
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox(
   // left hand
-  VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+  WEBUSB_PAIR,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
   KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
   KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
   KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
