9a8e218aa382881d0d4110541e7e1195b10ed3b6
noroadsleft <xxiinophobia@yahoo.com>
2019-11-04 23:58:01 -0800

    enable dynamic macros

--- keyboards/kc60/keymaps/noroadsleft/keymap.c
+++ keyboards/kc60/keymaps/noroadsleft/keymap.c
@@ -285,7 +285,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
         FW_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           \
         KC_LSFT, NUBS_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   \
-        CTL_GRV, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, MO(_FW), KC_RCTL  \
+        CTL_GRV, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, MO(_MA), MO(_FW), KC_RCTL  \
     ),
 
     /* Dvorak */
@@ -295,7 +295,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS, \
         FW_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, KC_ENT,           \
         KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,                   \
-        CTL_GRV, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, MO(_FW), KC_RCTL  \
+        CTL_GRV, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, MO(_MA), MO(_FW), KC_RCTL  \
     ),
 
     /* Colemak */
@@ -305,7 +305,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, \
         FW_CAPS, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,           \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   \
-        CTL_GRV, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, MO(_FW), KC_RCTL  \
+        CTL_GRV, KC_LGUI, KC_LALT,                   KC_SPC,                                      KC_RALT, MO(_MA), MO(_FW), KC_RCTL  \
     ),
 
     /****************
@@ -405,9 +405,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Macro layer */
     [_MACROS] = LAYOUT_60_ansi(
         //       2        3        4        5        6        7        8        9        10       11       12       13       14       15       16
-        TG(_MA), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______, G_PUSH,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______, G_FTCH,  G_COMM,  _______, _______, _______, _______, T_L3DED, _______, _______, _______,          \
+        TG(_MA), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DM_REC1, DM_REC2, _______, \
+        _______, _______, _______, G_PUSH,  _______, _______, _______, _______, _______, _______, _______, DM_PLY1, DM_PLY2, DM_RSTP, \
+        _______, _______, _______, G_FTCH,  G_COMM,  _______, _______, _______, _______, _______, _______, _______, _______,          \
         _______, _______, _______, _______, _______, G_BRCH,  SIGNA,   _______, _______, _______, _______, _______,                   \
         _______, _______, _______,                   _______,                                     _______, _______, NO_CHNG, _______  \
     ),
--- keyboards/kc60/keymaps/noroadsleft/rules.mk
+++ keyboards/kc60/keymaps/noroadsleft/rules.mk
@@ -5,3 +5,4 @@ LINK_TIME_OPTIMIZATION_ENABLE = yes
 MOUSEKEY_ENABLE = no        # Mouse keys
 # COMMAND_ENABLE = no         # Commands for debug and configuration
 SPACE_CADET_ENABLE = no     # Space Cadet
+DYNAMIC_MACRO_ENABLE = yes
