dc1137129d063975b3191853dc96d0095fcf94d6
James Young <18669334+noroadsleft@users.noreply.github.com>
2019-11-04 23:05:51 -0800

    [Keyboard] Add LAYOUT_60_iso to XD60 (#7263)
    * refactor iso keymap
    
    - remove redundant action_layer.h include
    - use enum for layer management
    - QMK coding conventions (four-space indent)
    
    * add LAYOUT_60_iso macro
    
    * enable 60_iso community layout for both revisions
    
    * swap KC_HOME to KC_RSFT
    
    * update ISO keymap readme
    
    * update default keymap readme
    
    `make` command no longer valid.
    
    * refactor keyboard readme
    
    Update to reflect current QMK template.
    
    * add LAYOUT_60_iso data to info.json
    
    * use #pragma once in header files

--- keyboards/xd60/rev2/config.h
+++ keyboards/xd60/rev2/config.h
@@ -15,8 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #include "config_common.h"
 
@@ -70,5 +69,3 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
-#endif
