c8d2ccc9760e55bc2034ef078eba861e8dddee01
Richard Baptist <rpbaptist@gmail.com>
2019-11-06 10:46:04 +0100

    Revert "Additional cleanup and updating"
    This reverts commit e0da9b63f31accb27e341cda7910a2d700cb1f66.

--- keyboards/crkbd/rev1/rev1.h
+++ keyboards/crkbd/rev1/rev1.h
@@ -5,12 +5,20 @@
 #include "quantum.h"
 
 #ifdef PROTOCOL_LUFA
-#    include "lufa.h"
-#    include "split_util.h"
+    #include "lufa.h"
+    #include "split_util.h"
 #endif
 
 #ifdef SSD1306OLED
-#    include "ssd1306.h"
+    #include "ssd1306.h"
+#endif
+
+#ifdef USE_I2C
+    #include <stddef.h>
+    #ifdef __AVR__
+        #include <avr/interrupt.h>
+        #include <avr/io.h>
+    #endif
 #endif
 
 // clang-format off
