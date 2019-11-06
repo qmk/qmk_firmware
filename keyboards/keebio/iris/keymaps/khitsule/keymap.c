b39f7c17ec0443ddf0519d219ef780bfb35a5161
Mysti Kruger <khitsule@gmail.com>
2019-11-06 14:20:00 +1000

    sync with upstream

--- keyboards/keebio/iris/keymaps/khitsule/keymap.c
+++ keyboards/keebio/iris/keymaps/khitsule/keymap.c
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LCTL,          KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_ENT,          KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, LC_ENT, KC_SPC,              SFT_T(KC_BSPC),  RAISE,   KC_LGUI
+                                    KC_LALT, LC_LCTL, KC_SPC,              SFT_T(KC_BSPC),  RAISE,   KC_LGUI
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 };
