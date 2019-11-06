51b8af42d46e14a5614be3f3c46933bf72c12194
Mysti Kruger <khitsule@gmail.com>
2019-11-06 15:42:23 +1000

    update to iris keymap

--- keyboards/keebio/iris/keymaps/khitsule/config.h
+++ keyboards/keebio/iris/keymaps/khitsule/config.h
@@ -18,8 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // #define USE_I2C
-// #define EE_HANDS
-#define MASTER_LEFT
+#define EE_HANDS
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #undef RGBLED_NUM
