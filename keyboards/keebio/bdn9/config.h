f62b3a7895272b82343adbf91cb5f376fe4d0fe5
CodeCoffeeCode <lyharris8@gmail.com>
2019-11-07 20:31:02 -0800

    bdn9 and some other stuff

--- keyboards/keebio/bdn9/config.h
+++ keyboards/keebio/bdn9/config.h
@@ -61,6 +61,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
+#define TAP_CODE_DELAY 10
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
