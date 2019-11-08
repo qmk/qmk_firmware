42816c6da4a1d7b91a15019a56d310551a1e6ddd
alittlepeace <mandyskittie@Gmail.com>
2019-11-08 13:07:26 -0600

    Update config.h

--- keyboards/redscarf_iiplus/verd/config.h
+++ keyboards/redscarf_iiplus/verd/config.h
@@ -1,6 +1,6 @@
 /*
 Copyright 2019 Maarten Dekkers <maartenwut@gmail.com>
-
+Port done by Andy Lee <alee@alittlepeacemusic.com>
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
