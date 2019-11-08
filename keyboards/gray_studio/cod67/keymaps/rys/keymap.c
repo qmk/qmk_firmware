0dc6a5e0e59c8d56e5a2153540ca4d6fa9e065c3
Rys Sommefeldt <rys@sommefeldt.com>
2019-11-08 21:40:09 +0000

    Remove empty function defs in keymap

--- keyboards/gray_studio/cod67/keymaps/rys/keymap.c
+++ keyboards/gray_studio/cod67/keymaps/rys/keymap.c
@@ -21,9 +21,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______,   RESET, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
-
-void matrix_init_user(void) {}
-
-void matrix_scan_user(void) {}
-
-void led_set_user(uint8_t usb_led) {}
