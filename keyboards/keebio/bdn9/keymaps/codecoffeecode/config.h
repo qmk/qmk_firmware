690bb0aa0d5f71c7ee83b1b972582c169da3aea5
CodeCoffeeCode <lyharris8@gmail.com>
2019-11-07 22:42:12 -0800

    removing foobar, moving config changes to my folder only

--- keyboards/keebio/bdn9/keymaps/codecoffeecode/config.h
+++ keyboards/keebio/bdn9/keymaps/codecoffeecode/config.h
@@ -61,7 +61,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
+#define TAP_CODE_DELAY 10
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+/* For constant mouse speed */
+#define MOUSEKEY_DELAY 300
+#define MOUSEKEY_INTERVAL 50
+#define MOUSEKEY_MAX_SPEED 1
+#define MOUSEKEY_TIME_TO_MAX 0
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
