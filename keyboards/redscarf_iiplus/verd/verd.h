edc274bdf7bcdec6d11b77b2650efd246554fc5a
alittlepeace <mandyskittie@Gmail.com>
2019-11-08 13:11:01 -0600

    Update verd.h

--- keyboards/redscarf_iiplus/verd/verd.h
+++ keyboards/redscarf_iiplus/verd/verd.h
@@ -27,7 +27,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT_SPLIT0( \
+#define LAYOUT_60_numpad( \
 	  K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, K014, K015, K016, K017, \
 	  K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, K114, K115, K116,  \
 	  K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211,       K213, K214, K215, K216, K217, \
