8e499d81630fc8d9c65685410f6a882330d2ff2a
alittlepeace <mandyskittie@Gmail.com>
2019-11-08 20:43:57 -0600

    Update keymap.c

--- keyboards/redscarf_iiplus/verd/keymaps/alittlepeace/keymap.c
+++ keyboards/redscarf_iiplus/verd/keymaps/alittlepeace/keymap.c
@@ -18,8 +18,7 @@
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
     KC_P00 = SAFE_RANGE,
-    CTRLMA,
-
+    
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, TG(1), KC_PSLS, KC_PAST, KC_PMNS, 
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_INS, KC_HOME, KC_PGUP, 
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_DEL, KC_END, KC_PGDN, KC_PPLS, 
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-        CTRLMA, KC_UP, LCTL(KC_TAB), KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, MO(2), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PENT
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, C(S(KC_TAB)), KC_UP, LCTL(KC_TAB), 
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, MO(2), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PENT
     ),
 
     [1] = LAYOUT_60_numpad_split0(
@@ -54,12 +53,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 SEND_STRING("00");
                  }
-            break;
-        case CTRLMA:
-            if (record->event.pressed) {
-		tap_code16(C(S(KC_TAB)));
-		 }
-	    break;
+        break;
 	    case TG(1):
            if (record->event.pressed && host_keyboard_led_state().num_lock) {
                 tap_code(KC_NUMLOCK);
