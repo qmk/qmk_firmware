10430a5c5c17b967632d2e1d34dca4ef210bba64
alittlepeace <mandyskittie@Gmail.com>
2019-11-08 20:39:10 -0600

    Update keyboards/redscarf_iiplus/verd/verd.c
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/redscarf_iiplus/verd/verd.c
+++ keyboards/redscarf_iiplus/verd/verd.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 Maarten Dekkers <maartenwut@gmail.com>
+/* Copyright 2019 Andy Lee <alee@alittlepeacemusic.com>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
