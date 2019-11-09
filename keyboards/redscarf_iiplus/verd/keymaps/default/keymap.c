f03b656cd19ae6a36dbcdd8ace00668f1e27449f
alittlepeace <mandyskittie@Gmail.com>
2019-11-08 20:57:12 -0600

    Update keymap.c

--- keyboards/redscarf_iiplus/verd/keymaps/default/keymap.c
+++ keyboards/redscarf_iiplus/verd/keymaps/default/keymap.c
@@ -16,40 +16,26 @@
 #include QMK_KEYBOARD_H
 
 // Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    KC_P00 = SAFE_RANGE,
-  
-
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT_60_numpad_split0(
+    [0] = LAYOUT_60_numpad(
         KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_P7, KC_P8, KC_P9, 
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_P4, KC_P5, KC_P6, KC_PPLS, 
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_P1, KC_P2, KC_P3, 
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, MO(1), KC_RCTL, KC_P0, KC_P00, KC_PDOT, KC_PENT
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, MO(1), KC_RCTL, KC_P0, KC_PDOT, KC_PENT
     ),
     
-    [1] = LAYOUT_60_numpad_split0(
+    [1] = LAYOUT_60_numpad(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _____,  _____,  _____,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _____,  _____,  _____,    _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _____,  _____,  _____,
-        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _____,  _____,  _____,  _______
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _____,  _____,  _______
     ),
 };
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_P00:
-            if (record->event.pressed) {
-                SEND_STRING("00");
-                 }
-            break;
-    }
-    return true;
-}
+
 
 void matrix_init_user (void) {
   if (!get_host_led_state().num_lock) {
