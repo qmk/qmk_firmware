b5d0e698b1677abb96ba60ba0e2444d4198601dd
Zach Little <jzachlittle@gmail.com>
2019-11-09 16:36:50 -0600

    update keymaps, build dactyl manuform keymap

--- keyboards/crkbd/keymaps/zlittle/keymap.c
+++ keyboards/crkbd/keymaps/zlittle/keymap.c
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_VOLU,
     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD,
     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
-                            KC_F11, KC_F12, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_TRNS
+                            KC_F11, KC_F12, KC_SCROLLLOCK,                        KC_TRNS, KC_TRNS, KC_TRNS
     ),
     /* Keymap 2: Pad/Function layer
      *
