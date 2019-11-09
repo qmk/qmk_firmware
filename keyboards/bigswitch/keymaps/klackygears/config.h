d9c23aca2cffd9d765815c8c14f50515e5600d40
James <bronzegears@gmail.com>
2019-11-09 16:47:18 -0500

    adding bigswitch keymap

---
+++ keyboards/bigswitch/keymaps/klackygears/config.h
@@ -0,0 +1,29 @@
+/*
+Copyright 2018 QMK Contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+#define TAPPING_TERM 200
+
+#define MANUFACTURER    klackygears
+#define PRODUCT         BigSwitch
+#define DESCRIPTION     A single key board for Novelkeys Big Switch
+
+
+#define RGBLIGHT_ANIMATIONS
+#endif
