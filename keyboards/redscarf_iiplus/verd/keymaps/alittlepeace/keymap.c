f7468bde6effc4c1dbea798ed73af3bf72953065
alittlepeace <mandyskittie@Gmail.com>
2019-11-08 20:25:06 -0600

    Update keymap.c

--- keyboards/redscarf_iiplus/verd/keymaps/alittlepeace/keymap.c
+++ keyboards/redscarf_iiplus/verd/keymaps/alittlepeace/keymap.c
@@ -25,11 +25,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_60_numpad_split0(
-        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, TG(1), KC_PSLS, KC_PAST, KC_PMNS, 
+        KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, TG(1), KC_PSLS, KC_PAST, KC_PMNS, 
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_INS, KC_HOME, KC_PGUP, 
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_DEL, KC_END, KC_PGDN, KC_PPLS, 
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-        CTRLMA, KC_UP, LCTL(KC_TAB), KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PENT
+        CTRLMA, KC_UP, LCTL(KC_TAB), KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, MO(2), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PENT
     ),
 
     [1] = LAYOUT_60_numpad_split0(
@@ -39,7 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_1,    KC_2,    KC_3,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, KC_0,    KC_P00,  KC_DOT,  _______
     ),
-
+    
+    [2] = LAYOUT_60_numpad_split0(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _____,  _____,  _____,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _____,  _____,  _____,    _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _____,  _____,  _____,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _____,  _____,  _____,  _______
+    ),
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -59,12 +66,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		return true;
 		}
 	    break;
-	    case LAYOFF:
-	    if (record->event.pressed) {
-		tap_code(KC_NUMLOCK);
-		layer_invert(1);
-		}
-	    break;
     }
     return true;
 }
@@ -75,10 +76,11 @@ void matrix_init_user (void) {
   }
 }
 
-void led_set_user(uint8_t usb_led) {
-    if(IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+bool led_update_user(led_t led_state) {
+    if (led_state.num_lock) {
         layer_on(1);
     } else {
-        layer_off(0);
+        layer_off(1);
     }
+    return true;
 }
