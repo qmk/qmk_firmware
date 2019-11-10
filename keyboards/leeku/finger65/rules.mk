f08df8bde18f5543bad0963355a393675e28b3a0
Sid Carter <sidcarter@users.noreply.github.com>
2019-11-10 17:40:44 -0500

    Update keyboards/leeku/finger65/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/leeku/finger65/rules.mk
+++ keyboards/leeku/finger65/rules.mk
@@ -17,7 +17,7 @@ BOOTLOADER = bootloadHID
 #
 BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control(+450)
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
