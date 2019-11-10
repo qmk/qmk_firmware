cef02b0db6e378c6808546b1e8a747c0959b600a
Sid Carter <sidcarter@users.noreply.github.com>
2019-11-10 17:41:24 -0500

    Update keyboards/leeku/finger65/keymaps/madhatter/keymap.c
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/leeku/finger65/keymaps/madhatter/keymap.c
+++ keyboards/leeku/finger65/keymaps/madhatter/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LGUI,KC_LALT,              KC_NO,        KC_SPC,       KC_NO,             FNMS,    KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [_FNMS] = LAYOUT_65_ANSI(
+  [_FNMS] = LAYOUT_65_ansi(
      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, KC_DEL,  KC_MPLY,
      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         RESET,   KC_VOLU,
      AG_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         EEP_RST, KC_VOLD,
