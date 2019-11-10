837bc7358e118d07d20066f3cf2f6acd15961e5d
Sid Carter <geek@sidcarter.com>
2019-11-10 12:45:55 -0500

    update USB information

--- keyboards/leeku/finger65/usbconfig.h
+++ keyboards/leeku/finger65/usbconfig.h
@@ -240,7 +240,7 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'L', 'e', 'e', 'K', 'u'
+#define USB_CFG_VENDOR_NAME     'L', 'e', 'e', 'k', 'u'
 #define USB_CFG_VENDOR_NAME_LEN 5
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
