cdc25e02bdee38ec1a1c221182daabeac724c625
Sid Carter <sidcarter@users.noreply.github.com>
2019-11-10 12:38:31 -0500

    Update keyboards/leeku/finger65/usbconfig.h
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/leeku/finger65/usbconfig.h
+++ keyboards/leeku/finger65/usbconfig.h
@@ -251,7 +251,7 @@ section at the end of this file).
  * details.
  */
 #define USB_CFG_DEVICE_NAME     'L', '3', ' ', 'k', 'e', 'y', 'b', 'o', 'a', 'r', 'd'
-#define USB_CFG_DEVICE_NAME_LEN 11
+#define USB_CFG_DEVICE_NAME_LEN 8
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
