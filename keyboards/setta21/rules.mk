6d05ed42da3c9f5b8241d80fa21589726959b632
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-11-11 08:44:17 +0900

    Update keyboards/setta21/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/setta21/rules.mk
+++ keyboards/setta21/rules.mk
@@ -16,7 +16,7 @@ BOOTLOADER = caterina
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control(+450)
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
