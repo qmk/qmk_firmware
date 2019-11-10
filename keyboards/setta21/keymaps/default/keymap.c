cee94ed008d794c5484c4f8cacc5f544c44b031f
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-11-11 02:29:32 +0900

    Update keyboards/setta21/keymaps/default/keymap.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/setta21/keymaps/default/keymap.c
+++ keyboards/setta21/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ enum layer_number {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT(
+  [_BASE] = LAYOUT_numpad_6x4(
       //,-----------------------------------|
 LT(_ADJUST,KC_ESC),  KC_F2,  KC_EQL,  KC_DEL,
       //|--------+--------+--------+--------|
