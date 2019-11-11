4313e3de6d5d41ffa49ef4d822dfcbc675e6f876
Vince Gellár <gellar.vince@gmail.com>
2019-11-11 20:53:16 +0100

    Use suggested condensed rules.mk

--- keyboards/keebio/bfo9000/keymaps/deepbluealchemy/rules.mk
+++ keyboards/keebio/bfo9000/keymaps/deepbluealchemy/rules.mk
@@ -3,20 +3,11 @@
 #   the appropriate keymap folder that will get included automatically
 #
 
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = yes        # Console for debug(+400)
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = yes        # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-TAP_DANCE_ENABLE = no       # Enable Tap Dancing function
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = yes
+COMMAND_ENABLE = no
+NKRO_ENABLE = yes
+UNICODE_ENABLE = yes
 
 # make keebio/bfo9000:deepbluealchemy:avrdude SPLIT_LEFT=yes
 
