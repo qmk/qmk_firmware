5d21b90bb30f5d2f30ac5d97e3b793cfc1c99339
yiancar <yiangosyiangou@cytanet.com.cy>
2019-11-11 19:25:47 +0000

    Return fix

--- drivers/issi/is31fl3733.c
+++ drivers/issi/is31fl3733.c
@@ -82,24 +82,22 @@ bool    g_led_control_registers_update_required[DRIVER_COUNT] = {false};
 
 uint8_t IS31FL3733_write_register(uint8_t addr, uint8_t reg, uint8_t data) {
     // If the transaction fails function returns 1.
-    uint8_t output = 0;
     g_twi_transfer_buffer[0] = reg;
     g_twi_transfer_buffer[1] = data;
 
 #if ISSI_PERSISTENCE > 0
     for (uint8_t i = 0; i < ISSI_PERSISTENCE; i++) {
-        if (i2c_transmit(addr << 1, g_twi_transfer_buffer, 2, ISSI_TIMEOUT) == 0) {
+        if (!(i2c_transmit(addr << 1, g_twi_transfer_buffer, 2, ISSI_TIMEOUT) == 0)) {
+            return 1;
             break;
-        } else {
-            output = 1;
         }
     }
 #else
     if (i2c_transmit(addr << 1, g_twi_transfer_buffer, 2, ISSI_TIMEOUT)) {
-        output = 1;
+        return 1;
     }
 #endif
-    return output;
+    return 0;
 }
 
 uint8_t IS31FL3733_write_pwm_buffer(uint8_t addr, uint8_t *pwm_buffer) {
