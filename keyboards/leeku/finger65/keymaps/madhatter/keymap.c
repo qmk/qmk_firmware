dca368feff1ee993c33791ba856784ea7d248670
Sid Carter <geek@sidcarter.com>
2019-11-10 23:10:03 -0500

    Monica Bellucci ruined my life

--- keyboards/leeku/finger65/keymaps/madhatter/keymap.c
+++ keyboards/leeku/finger65/keymaps/madhatter/keymap.c
@@ -22,7 +22,7 @@ enum layers {
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT_65_ansi(
+  [_QWERTY] = LAYOUT_65_ansi_split_bs(
     KC_GESC, KC_1,   KC_2,   KC_3,  KC_4, KC_5,  KC_6,  KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSLS, KC_HOME,
     KC_TAB,  KC_Q,   KC_W,   KC_E,  KC_R, KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC, KC_PGUP,
     KC_GRV,  KC_A,   KC_S,   KC_D,  KC_F, KC_G,  KC_H,  KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LGUI,KC_LALT,                    KC_SPC,                       FNMS,             KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [_FNMS] = LAYOUT_65_ansi(
+  [_FNMS] = LAYOUT_65_ansi_split_bs(
      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, KC_DEL,  KC_MPLY,
      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         RESET,   KC_VOLU,
      AG_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         EEP_RST,          KC_VOLD,
