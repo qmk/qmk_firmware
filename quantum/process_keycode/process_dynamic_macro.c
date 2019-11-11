9d4feecd05bb3db95661d7d58a3a0aff9eb7898e
Jan Kolkmeier <jankolkmeier@gmail.com>
2019-11-11 15:33:49 +0100

    prevent macro from being replayed immediately when recording ended with play key

--- quantum/process_keycode/process_dynamic_macro.c
+++ quantum/process_keycode/process_dynamic_macro.c
@@ -205,6 +205,10 @@ bool process_dynamic_macro(uint16_t keycode, keyrecord_t *record) {
                     dynamic_macro_record_start(&macro_pointer, r_macro_buffer);
                     macro_id = 2;
                     return false;
+            }
+        }
+        if (record->event.pressed) {
+            switch (keycode) {
                 case DYN_MACRO_PLAY1:
                     dynamic_macro_play(macro_buffer, macro_end, +1);
                     return false;
