c0567df59a336da0d86e2627c9c816fb513530bc
James Young <xxiinophobia@yahoo.com>
2019-11-12 00:55:08 -0800

    ~echoes

--- util/travis_git_setup.sh
+++ util/travis_git_setup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Git Setup
+echo -e "\nTRAVIS_REPO_SLUG = $TRAVIS_REPO_SLUG\nTRAVIS_BRANCH = $TRAVIS_BRANCH"
 
 # Add QMK repository as `upstream`
+echo "$ git remote add upstream https://github.com/qmk/qmk_firmware.git"
 git remote add upstream https://github.com/qmk/qmk_firmware.git
 
 # Confirm the remote was added
+echo "$ git remote -v"
 git remote -v
 
 # Fetch the branch being targeted by the pull request (The "base" branch)
+echo "git fetch upstream $TRAVIS_BRANCH"
 git fetch upstream $TRAVIS_BRANCH
 
 # How many commits are exclusive to the "base" branch and to the "head" branch
 git rev-list --left-right --count $TRAVIS_BRANCH...$TRAVIS_PULL_REQUEST_SHA
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+	echo "This is a pull request."
+else
 	echo "This is not a pull request."
 fi
 
