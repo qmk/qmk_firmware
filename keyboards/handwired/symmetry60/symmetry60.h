2e9a999f559b240ab91cc32b26c482c242e0b74e
marhalloweenvt <marhalloweenvt@gmail.com>
2019-11-12 09:43:14 +0700

    Update keyboards/handwired/symmetry60/symmetry60.h
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/symmetry60/symmetry60.h
+++ keyboards/handwired/symmetry60/symmetry60.h
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef SYMMETRY60_H
+#pragma once
 #define SYMMETRY60_H
 
 #include "quantum.h"
