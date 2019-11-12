978b21cc32e26e455107d3c7b27da9ef085aec5b
James Young <xxiinophobia@yahoo.com>
2019-11-12 02:54:27 -0800

    small fixes

--- util/travis_git_setup.sh
+++ util/travis_git_setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Git Setup
-echo -e "\n"
 echo -e "TRAVIS_REPO_SLUG = $TRAVIS_REPO_SLUG"
 echo -e "TRAVIS_BRANCH = $TRAVIS_BRANCH"
 echo -e "CURRENT COMMIT HASH = $(git rev-parse --short HEAD)"
 echo -e "CURRENT BRANCH = $(git rev-parse --abbrev-ref HEAD)"
 
 # Add QMK repository as `upstream`
-echo -e "\nAdd QMK repository as `upstream`..."
+echo -e "\nAdd QMK repository as \`upstream\`..."
 git remote add upstream https://github.com/qmk/qmk_firmware.git
 
 # Confirm the remote was added
@@ -18,7 +17,7 @@ git remote -v
 # Fetch the branch being targeted by the pull request (The "base" branch)
 echo -e "\nFetching refs for qmk:master..."
 git fetch upstream master
-CURRENT_MASTER=$(git log -n 1 --pretty=format:"%H")
+CURRENT_MASTER=$(git log -n 1 --pretty=format:"%H" upstream/master)
 echo -e "\nCurrent SHA of qmk:master is $CURRENT_MASTER"
 
 # How many commits are exclusive to the "base" branch $CURRENT_MASTER
@@ -30,7 +29,7 @@ git rev-list --left-right --count $CURRENT_MASTER...$TRAVIS_PULL_REQUEST_SHA
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
 	# Triggered by pull request
-
+	echo "This is a pull request."
 	# How many commits are exclusive to the "base" branch
 	# $CURRENT_MASTER and to the "head" branch
 	#git rev-list --left-right --count $TRAVIS_BRANCH...$TRAVIS_PULL_REQUEST_SHA
