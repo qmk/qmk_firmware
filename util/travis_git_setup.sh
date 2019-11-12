0442eb248ab0e7c2fea8dccc9871d960ce54bf93
James Young <xxiinophobia@yahoo.com>
2019-11-12 02:33:56 -0800

    more git tracing...

--- util/travis_git_setup.sh
+++ util/travis_git_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Git Setup
-echo -e "\nTRAVIS_REPO_SLUG = $TRAVIS_REPO_SLUG\nTRAVIS_BRANCH = $TRAVIS_BRANCH"
+echo -e "\n"
+echo -e "TRAVIS_REPO_SLUG = $TRAVIS_REPO_SLUG"
+echo -e "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+echo -e "CURRENT COMMIT HASH = $(git rev-parse --short HEAD)"
+echo -e "CURRENT BRANCH = $(git rev-parse --abbrev-ref HEAD)"
 
 # Add QMK repository as `upstream`
 echo "$ git remote add upstream https://github.com/qmk/qmk_firmware.git"
@@ -12,12 +16,14 @@ echo "$ git remote -v"
 git remote -v
 
 # Fetch the branch being targeted by the pull request (The "base" branch)
-echo "git fetch upstream $TRAVIS_BRANCH"
-git fetch upstream $TRAVIS_BRANCH
+echo "Fetching refs for qmk:master..."
+git fetch upstream master
 
 # How many commits are exclusive to the "base" branch and to the "head" branch
 git rev-list --left-right --count $TRAVIS_BRANCH...$TRAVIS_PULL_REQUEST_SHA
 
+# If $TRAVIS_PULL_REQUEST is not false, then the build was triggered by
+# a pull request. Otherwise, the build was triggered by a push.
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
 	echo "This is a pull request."
@@ -25,4 +31,4 @@ else
 	echo "This is not a pull request."
 fi
 
-echo -e $TRAVIS_REPO_SLUG / $TRAVIS_BRANCH
+# echo -e $TRAVIS_REPO_SLUG / $TRAVIS_BRANCH
