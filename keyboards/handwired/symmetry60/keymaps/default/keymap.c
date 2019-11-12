088f8f08c1af62f878270abb5c7fbd6d5d83bb71
marhalloweenvt <marhalloweenvt@gmail.com>
2019-11-12 09:52:38 +0700

    Update keymap.c

--- keyboards/handwired/symmetry60/keymaps/default/keymap.c
+++ keyboards/handwired/symmetry60/keymaps/default/keymap.c
@@ -24,15 +24,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_DEL, KC_LGUI,  KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL 
   ),
 };
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
