e69b504094a52d8cc8badfdface62767774b6ced
fauxpark <fauxpark@gmail.com>
2019-11-13 08:56:49 +1100

    Use 1ms as default for now

--- tmk_core/protocol/vusb/vusb.c
+++ tmk_core/protocol/vusb/vusb.c
@@ -349,8 +349,9 @@ const PROGMEM uchar mouse_hid_report[] = {
 #    define USB_MAX_POWER_CONSUMPTION 500
 #endif
 
+// TODO: change this to 10ms to match LUFA
 #ifndef USB_POLLING_INTERVAL_MS
-#    define USB_POLLING_INTERVAL_MS 10
+#    define USB_POLLING_INTERVAL_MS 1
 #endif
 
 /*
