fc09cdbbd130fdb14a0400617a5ea91ff7bff6a0
marhalloweenvt <marhalloweenvt@gmail.com>
2019-11-12 09:51:07 +0700

    Update keymap.c

--- keyboards/handwired/symmetry60/keymaps/default/keymap.c
+++ keyboards/handwired/symmetry60/keymaps/default/keymap.c
@@ -15,8 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines the keycodes used by our macros in process_record_user
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [0] = LAYOUT_ortho_5x14( \
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
