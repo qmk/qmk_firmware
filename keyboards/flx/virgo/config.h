f02483412f17e9c3d1ef6ffe42c0abf2de92075f
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-11-12 22:00:25 -0800

    Update keyboards/flx/virgo/config.h
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/flx/virgo/config.h
+++ keyboards/flx/virgo/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    [EDIT_THIS]
+#define MANUFACTURER    FLX
 #define PRODUCT         [EDIT_THIS] FLX-V90 Default
 #define DESCRIPTION     [EDIT_THIS] A QMK-powered custom keyboard
 
