f49b40d1ec22bfb99806cea203e3e626cce50ab2
moyi4681 <moyi4681@users.noreply.github.com>
2019-11-15 03:55:46 +0800

    [Keyboard] add kbdmini; dztech, kbdfans keyboards cleanup (#7223)
    * add kbdmini, dztech and kbdfans keyboards small fix
    
    * Caps lock indicator moved from keymap
    
    * Update dz65rgb.c
    
    * fix error
    
    * fix matthewrobo keymap error
    
    * Update keyboards/kbdfans/kbd67/mkiirgb/mkiirgb.c
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Update keyboards/dztech/dz60rgb/dz60rgb.c
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Update keyboards/dztech/dz60rgb_ansi/dz60rgb_ansi.c
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Update keyboards/dztech/dz60rgb_wkl/dz60rgb_wkl.c
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Update keyboards/dztech/dz65rgb/dz65rgb.c
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * wrap all of the g_led_config stuff and rgb matrix functions in ifdefs
    
    wrap all of the g_led_config stuff and rgb matrix functions in ifdefs

--- keyboards/dztech/dz60rgb_ansi/rules.mk
+++ keyboards/dztech/dz60rgb_ansi/rules.mk
@@ -2,7 +2,7 @@ MCU  = STM32F303
 DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
 DFU_SUFFIX_ARGS = -p DF11 -v 0483
 BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = lite            # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite           # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes              # Mouse keys
 EXTRAKEY_ENABLE = yes              # Audio control and System control
 CONSOLE_ENABLE = no                # Console for debug
@@ -11,4 +11,6 @@ COMMAND_ENABLE = no                # Commands for debug and configuration
 NKRO_ENABLE = no                 # USB Nkey Rollover
 AUDIO_ENABLE = no
 RGB_MATRIX_ENABLE = IS31FL3733     # Use RGB matrix
-NO_USB_STARTUP_CHECK = no          # Disable initialization only when usb is plugged in66666666666666666666666666
+NO_USB_STARTUP_CHECK = no          # Disable initialization only when usb is plugged in
+
+LAYOUTS = 60_ansi
