d05debc32d389f744bbfdcc8fe20b42c65e90ea3
Carlos Filoteo <c.filoteo12@gmail.com>
2019-11-14 09:07:03 -0700

    Move rev1 specific configs up

--- keyboards/lily58/rev1/config.h
+++ keyboards/lily58/rev1/config.h
@@ -54,6 +54,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 12    // Number of LEDs
 
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
