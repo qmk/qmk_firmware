8986b1ea52c7529cd01db8addf38dcf0a76eded2
Drashna Jael're <drashna@live.com>
2019-11-13 23:20:56 -0800

    Fix RGB Light layer indication

--- users/drashna/rgb_stuff.c
+++ users/drashna/rgb_stuff.c
@@ -337,67 +337,70 @@ void matrix_scan_rgb(void) {
 #endif
 }
 
+#ifdef RGBLIGHT_ENABLE
+void rgblight_set_hsv_and_mode(uint8_t hue, uint8_t sat, uint8_t val, uint8_t mode) {
+    rgblight_sethsv_noeeprom(hue, sat, val);
+    wait_us(175); // Add a slight delay between color and mode to ensure it's processed correctly
+    rgblight_mode_noeeprom(mode);
+}
+#endif
+
 layer_state_t layer_state_set_rgb(layer_state_t state) {
 #ifdef RGBLIGHT_ENABLE
     if (userspace_config.rgb_layer_change) {
-        switch (biton32(state)) {
+        switch (get_highest_layer(state)) {
             case _MACROS:
-                rgblight_sethsv_noeeprom_orange();
-                userspace_config.is_overwatch ? rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE + 2) : rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE + 3);
+                rgblight_set_hsv_and_mode(HSV_ORANGE, userspace_config.is_overwatch ? RGBLIGHT_MODE_SNAKE + 2 : RGBLIGHT_MODE_SNAKE + 3);
                 break;
             case _MEDIA:
-                rgblight_sethsv_noeeprom_chartreuse();
-                rgblight_mode_noeeprom(RGBLIGHT_MODE_KNIGHT + 1);
+                rgblight_set_hsv_and_mode(HSV_CHARTREUSE, RGBLIGHT_MODE_KNIGHT + 1);
                 break;
             case _GAMEPAD:
-                rgblight_sethsv_noeeprom_orange();
-                rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE + 2);
+                rgblight_set_hsv_and_mode(HSV_ORANGE, RGBLIGHT_MODE_SNAKE + 2);
                 break;
             case _DIABLO:
-                rgblight_sethsv_noeeprom_red();
-                rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
+                rgblight_set_hsv_and_mode(HSV_RED, RGBLIGHT_MODE_BREATHING + 3);
                 break;
             case _RAISE:
-                rgblight_sethsv_noeeprom_yellow();
-                rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
+                rgblight_set_hsv_and_mode(HSV_YELLOW, RGBLIGHT_MODE_BREATHING + 3);
                 break;
             case _LOWER:
-                rgblight_sethsv_noeeprom_green();
-                rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
+                rgblight_set_hsv_and_mode(HSV_GREEN, RGBLIGHT_MODE_BREATHING + 3);
                 break;
             case _ADJUST:
-                rgblight_sethsv_noeeprom_red();
-                rgblight_mode_noeeprom(RGBLIGHT_MODE_KNIGHT + 2);
+                rgblight_set_hsv_and_mode(HSV_RED, RGBLIGHT_MODE_KNIGHT + 2);
                 break;
             default:  //  for any other layers, or the default layer
-                switch (biton32(default_layer_state)) {
+            {
+                uint8_t mode = get_highest_layer(state) == _MODS ? RGBLIGHT_MODE_BREATHING : RGBLIGHT_MODE_STATIC_LIGHT;
+                switch (get_highest_layer(default_layer_state)) {
                     case _COLEMAK:
-                        rgblight_sethsv_noeeprom_magenta();
+                        rgblight_set_hsv_and_mode(HSV_MAGENTA, mode);
                         break;
                     case _DVORAK:
-                        rgblight_sethsv_noeeprom_springgreen();
+                        rgblight_set_hsv_and_mode(HSV_SPRINGGREEN, mode);
                         break;
                     case _WORKMAN:
-                        rgblight_sethsv_noeeprom_goldenrod();
+                        rgblight_set_hsv_and_mode(HSV_GOLDENROD, mode);
                         break;
                     case _NORMAN:
-                        rgblight_sethsv_noeeprom_coral();
+                        rgblight_set_hsv_and_mode(HSV_CORAL, mode);
                         break;
                     case _MALTRON:
-                        rgblight_sethsv_noeeprom_yellow();
+                        rgblight_set_hsv_and_mode(HSV_YELLOW, mode);
                         break;
                     case _EUCALYN:
-                        rgblight_sethsv_noeeprom_pink();
+                        rgblight_set_hsv_and_mode(HSV_PINK, mode);
                         break;
                     case _CARPLAX:
-                        rgblight_sethsv_noeeprom_blue();
+                        rgblight_set_hsv_and_mode(HSV_BLUE, mode);
                         break;
                     default:
-                        rgblight_sethsv_noeeprom_cyan();
+                        rgblight_set_hsv_and_mode(HSV_CYAN, mode);
                         break;
                 }
-                biton32(state) == _MODS ? rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING) : rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);  // if _MODS layer is on, then breath to denote it
                 break;
+            }
         }
     }
 #endif  // RGBLIGHT_ENABLE
