26de32ffd4a8f28c09a8a903fc03fbe6777a4572
mtei <2170248+mtei@users.noreply.github.com>
2019-11-14 16:27:32 +0900

    add HELIX=scan option into {rev2/pico}/local_features.mk
    Made DEBUG_MATRIX_SCAN_RATE easy to use.

--- keyboards/helix/pico/local_features.mk
+++ keyboards/helix/pico/local_features.mk
@@ -10,7 +10,7 @@ define HELIX_CUSTOMISE_MSG
   $(info -  OLED_ENABLE          = $(OLED_ENABLE))
   $(info -  LED_BACK_ENABLE      = $(LED_BACK_ENABLE))
   $(info -  LED_UNDERGLOW_ENABLE = $(LED_UNDERGLOW_ENABLE))
-  $(info -  LED_ANIMATION        = $(LED_ANIMATIONS))
+  $(info -  LED_ANIMATIONS       = $(LED_ANIMATIONS))
   $(info -  IOS_DEVICE_ENABLE    = $(IOS_DEVICE_ENABLE))
   $(info )
 endef
@@ -43,8 +43,15 @@ endef
     ifeq ($(findstring ios,$(HELIX)), ios)
       IOS_DEVICE_ENABLE = yes
     endif
+    ifeq ($(findstring scan,$(HELIX)), scan)
+      # use DEBUG_MATRIX_SCAN_RATE
+      # see docs/newbs_testing_debugging.md
+      OPT_DEFS +=  -DDEBUG_MATRIX_SCAN_RATE
+      CONSOLE_ENABLE = yes
+      SHOW_VERBOSE_INFO = yes
+    endif
     ifeq ($(findstring verbose,$(HELIX)), verbose)
-       SHOW_VERBOSE_INFO = yes
+      SHOW_VERBOSE_INFO = yes
     endif
     SHOW_HELIX_OPTIONS = yes
   endif
@@ -92,8 +99,10 @@ endif
 ifneq ($(strip $(SHOW_HELIX_OPTIONS)),)
   $(eval $(call HELIX_CUSTOMISE_MSG))
   ifneq ($(strip $(SHOW_VERBOSE_INFO)),)
-     $(info -- RGBLIGHT_ENABLE = $(RGBLIGHT_ENABLE))
-     $(info -- OPT_DEFS        = $(OPT_DEFS))
+     $(info -- RGBLIGHT_ENABLE    = $(RGBLIGHT_ENABLE))
+     $(info -- OLED_DRIVER_ENABLE = $(OLED_DRIVER_ENABLE))
+     $(info -- CONSOLE_ENABLE     = $(CONSOLE_ENABLE))
+     $(info -- OPT_DEFS           = $(OPT_DEFS))
      $(info -- LINK_TIME_OPTIMIZATION_ENABLE = $(LINK_TIME_OPTIMIZATION_ENABLE))
      $(info )
   endif
