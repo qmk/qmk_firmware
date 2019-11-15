db3eb3260c8f025a8d2a25018bfb16fe7af64c55
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-11-16 02:50:04 +0900

    Update keyboards/nafuda/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/nafuda/rules.mk
+++ keyboards/nafuda/rules.mk
@@ -16,7 +16,7 @@ BOOTLOADER = caterina
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
