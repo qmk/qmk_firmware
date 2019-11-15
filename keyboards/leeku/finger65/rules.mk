bf59216d1be5d07e9fab8089c95792469f68f4eb
Sid Carter <geek@sidcarter.com>
2019-11-14 21:24:41 -0500

    USB power changes

--- keyboards/leeku/finger65/rules.mk
+++ keyboards/leeku/finger65/rules.mk
@@ -23,4 +23,3 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 MIDI_ENABLE = no            # MIDI support
-
