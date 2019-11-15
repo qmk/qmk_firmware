4af7cf91364d06b48565939eaad8a07e16ecfe7e
Luiz Correia <luiz@luizcorreia.eti.br>
2019-11-15 19:34:56 -0300

    Change key names to capitals

--- keyboards/gh60/revc/revc.h
+++ keyboards/gh60/revc/revc.h
@@ -130,15 +130,15 @@ inline void gh60_wasd_leds_off(void)    { setPinInput(F7); writePinLow(F7); }
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
 */
 #define LAYOUT_60_abnt2( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,      k0e, \
-    k10,      k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,      \
-    k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k1e, k2d, \
-    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d,      \
-    k40, k41,      k43,           k46,                k4a, k4b,      k4d, k4e  \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C,      K0E, \
+    K10,      K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      \
+    K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K1E, K2D, \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,      \
+    K40, K41,      K43,           K46,                K4A, K4B,      K4D, K4E  \
 ) { \
-    { k00,  k01,   k02,   k03,  k04,   k05,   k06,  k07,   k08,   k09,   k0a,  k0b,  k0c,   KC_NO, k0e   }, \
-    { k10,  KC_NO, k12,   k13,  k14,   k15,   k16,  k17,   k18,   k19,   k1a,  k1b,  k1c,   k1d,   k1e   }, \
-    { k20,  KC_NO, k22,   k23,  k24,   k25,   k26,  k27,   k28,   k29,   k2a,  k2b,  k2c,   k2d,   KC_NO }, \
-    { k30,  k31,   k32,   k33,  k34,   k35,   k36,  k37,   k38,   k39,   k3a,  k3b,  k3c,   k3d,   KC_NO }, \
-    { k40,  k41,   KC_NO, k43,  KC_NO, KC_NO, k46,  KC_NO, KC_NO, KC_NO, k4a,  k4b,  KC_NO, k4d,   k4e   }  \
+    { K00,  K01,   K02,   K03,  K04,   K05,   K06,  K07,   K08,   K09,   K0A,  K0B,  K0C,   KC_NO, K0E   }, \
+    { K10,  KC_NO, K12,   K13,  K14,   K15,   K16,  K17,   K18,   K19,   K1A,  K1B,  K1C,   K1D,   K1E   }, \
+    { K20,  KC_NO, K22,   K23,  K24,   K25,   K26,  K27,   K28,   K29,   K2A,  K2B,  K2C,   K2D,   KC_NO }, \
+    { K30,  K31,   K32,   K33,  K34,   K35,   K36,  K37,   K38,   K39,   K3A,  K3B,  K3C,   K3D,   KC_NO }, \
+    { K40,  K41,   KC_NO, K43,  KC_NO, KC_NO, K46,  KC_NO, KC_NO, KC_NO, K4A,  K4B,  KC_NO, K4D,   K4E   }  \
 }
