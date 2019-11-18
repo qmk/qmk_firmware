530eb354ec8c279353f8c53e26360014f470ef0c
moyi4681 <moyi4681@live.cn>
2019-11-17 22:54:13 -0800

    Update knight.c

--- keyboards/xbows/knight/knight.c
+++ keyboards/xbows/knight/knight.c
@@ -1,4 +1,5 @@
 #include "knight.h"
+
 void matrix_init_kb(void) {
     // put your keyboard start-up code here
     // runs once when the firmware starts up
@@ -6,6 +7,7 @@ void matrix_init_kb(void) {
     matrix_init_user();
     led_init_ports();
 }
+
 void led_init_ports(void) {
     setPinOutput(D1);
     writePinHigh(D1);
@@ -14,16 +16,8 @@ void led_init_ports(void) {
 }
 bool led_update_kb(led_t led_state) {
     if(led_update_user(led_state)) {
-        if (led_state.num_lock) {
-            writePinLow(D1);
-        } else {
-            writePinHigh(D1);
-        }
-        if (led_state.caps_lock) {
-            writePinLow(E2);
-        } else {
-            writePinHigh(E2);
-        }
-        return true;
+        writePin(E2, !led_state.caps_lock);
+        writePin(D1, !led_state.num_lock);
     }
+    return true;
 }
