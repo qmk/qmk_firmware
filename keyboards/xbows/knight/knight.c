7824e2b4ff05c94c3250428ec3b4e5bb535ff0b8
moyi4681 <moyi4681@live.cn>
2019-11-17 19:13:10 -0800

    knight Indicator light fix

--- keyboards/xbows/knight/knight.c
+++ keyboards/xbows/knight/knight.c
@@ -6,14 +6,14 @@ void matrix_init_kb(void) {
 };
 void led_set_kb(uint8_t usb_led) {
     if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
-        writePinHigh(D1);
-    } else {
         writePinLow(D1);
+    } else {
+        writePinHigh(D1);
     }
     if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        writePinHigh(E2);
-    } else {
         writePinLow(E2);
+    } else {
+        writePinHigh(E2);
     }
     led_set_user(usb_led);
 }
