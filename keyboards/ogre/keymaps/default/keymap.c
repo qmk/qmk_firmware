1e726e0c259c22f21a93d0322b39b016693d7a54
Bryan Arendt <barendt@covermymeds.com>
2019-11-18 16:23:17 -0500

    fix braces

--- keyboards/ogre/keymaps/default/keymap.c
+++ keyboards/ogre/keymaps/default/keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
     KC_ESC,               KC_EQL,             KC_1,               KC_2,               KC_3,          KC_4,    KC_5,    KC_GRV, \
     KC_PGUP,              KC_6,               KC_7,               KC_8,               KC_9,          KC_0,    KC_MINS, KC_DEL, \
-    KC_TAB,               KC_Q,               KC_W,               KC_E,               KC_R,          KC_T,    KC_LBRC, \
-    KC_RBRC,              KC_Y,               KC_U,               KC_I,               KC_O,          KC_P,    KC_BSLS, \
+    KC_TAB,               KC_Q,               KC_W,               KC_E,               KC_R,          KC_T,    KC_RBRC, \
+    KC_LBRC,              KC_Y,               KC_U,               KC_I,               KC_O,          KC_P,    KC_BSLS, \
     KC_ESC,               KC_A,               KC_S,               KC_D,               KC_F,          KC_G,    KC_PGDN, \
     KC_QUOT,              KC_H,               KC_J,               KC_K,               KC_L,          KC_SCLN, MT(MOD_RGUI, KC_QUOT),\
     KC_LSFT,              MT(MOD_LCTL, KC_Z), MT(MOD_LGUI, KC_X), KC_C,               KC_V,          KC_B, \
@@ -37,8 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT( /* Base */
     RESET,                KC_EQL,             KC_F1,              KC_F2,              KC_F3,         KC_F4,   KC_F5,    KC_GRV, \
     KC_PGUP,              KC_F6,              KC_F7,              KC_F8,              KC_F9,         KC_F10,  KC_F11,   KC_F12, \
-    RGB_MOD,              RGB_HUI,            RGB_SAI,            RGB_VAI,            KC_R,          KC_T,    KC_LBRC, \
-    KC_RBRC,              KC_Y,               KC_U,               KC_MPLY,            KC_O,          KC_P,    KC_BSLS, \
+    RGB_MOD,              RGB_HUI,            RGB_SAI,            RGB_VAI,            KC_R,          KC_T,    KC_RBRC, \
+    KC_LBRC,              KC_Y,               KC_U,               KC_MPLY,            KC_O,          KC_P,    KC_BSLS, \
     RGB_MODE_PLAIN,       RGB_HUD,            RGB_SAD,            RGB_VAD,            KC_F,          KC_G,    KC_PGDN, \
     RGB_MODE_BREATHE,     KC_H,               KC_J,               KC_K,               KC_L,          KC_SCLN, MT(MOD_RGUI, KC_QUOT), \
     RGB_TOG,              MT(MOD_LCTL, KC_Z), MT(MOD_LGUI, KC_X), KC_C,               KC_V,          KC_B, \
