bf620eb705a348a85e3f2e3da3f5f532b5a65f01
kevin-b-lockwood <57274670+kevin-b-lockwood@users.noreply.github.com>
2019-11-25 10:04:56 -0800

    Update config.h

--- keyboards/handwired/plonk/config.h
+++ keyboards/handwired/plonk/config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0000
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Kevin Lockwood
-#define PRODUCT plonk
-#define DESCRIPTION A custom keyboard
+#define PRODUCT Plonk
+#define DESCRIPTION A Planck Ripoff
 
 /* key matrix size */
 #define MATRIX_ROWS 4
@@ -52,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+// #define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
