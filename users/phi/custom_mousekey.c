f1b548e9f9ce3325abfe05ba04578018d1bb265d
zk-phi <zk-phi@users.noreply.github.com>
2019-11-25 14:00:29 +0900

    Fix wheel-key speed handling

--- users/phi/custom_mousekey.c
+++ users/phi/custom_mousekey.c
@@ -2,6 +2,8 @@
 
 extern uint8_t mk_time_to_max, mk_wheel_time_to_max, mk_max_speed, mk_wheel_max_speed, mk_delay, mk_interval;
 
+uint8_t pressed_wheel_key_count = 0;
+
 bool process_mousekey_accel (uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
       case KC_MS_ACCEL0:
@@ -19,11 +21,17 @@ bool process_mousekey_accel (uint16_t keycode, keyrecord_t *record) {
         return false;
       case KC_MS_WH_UP: case KC_MS_WH_DOWN: case KC_MS_WH_LEFT: case KC_MS_WH_RIGHT:
         if (record->event.pressed) {
-            mk_delay = MOUSEKEY_WHEEL_DELAY / 10;
-            mk_interval = MOUSEKEY_WHEEL_INTERVAL;
+            if (!pressed_wheel_key_count) {
+                mk_delay = MOUSEKEY_WHEEL_DELAY / 10;
+                mk_interval = MOUSEKEY_WHEEL_INTERVAL;
+            }
+            pressed_wheel_key_count++;
         } else {
-            mk_delay = MOUSEKEY_DELAY / 10;
-            mk_interval = MOUSEKEY_INTERVAL;
+            pressed_wheel_key_count--;
+            if (!pressed_wheel_key_count) {
+                mk_delay = MOUSEKEY_DELAY / 10;
+                mk_interval = MOUSEKEY_INTERVAL;
+            }
         }
         return true;
     }
