4ba17556960b5715abdde8133c8b92709b2033e3
kevin-b-lockwood <57274670+kevin-b-lockwood@users.noreply.github.com>
2019-11-25 15:55:34 -0800

    Update rules.mk

--- keyboards/handwired/plonk/rules.mk
+++ keyboards/handwired/plonk/rules.mk
@@ -1,27 +1,5 @@
 # MCU name
-# MCU = atmega32u4
 MCU = STM32F303
-ALLOW_WARNINGS = yes
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
-# BOOTLOADER = dfu-util
-
-# If you don't know the bootloader type, then you can specify the
-# Boot Section Size in *bytes* by uncommenting out the OPT_DEFS line
-# Otherwise, delete this section
-#   Teensy halfKay      512
-#   Teensy++ halfKay    1024
-#   Atmel DFU loader    4096
-#   LUFA bootloader     4096
-#   USBaspLoader        2048
-# OPT_DEFS += -DBOOTLOADER_SIZE=4096
 
 # Build Options
 #   change yes to no to disable
