7080346175e2b595b1cd5d0cfcb19ab8b5fb3400
关凯 <guankai1@corp.netease.com>
2019-11-26 09:02:15 +0800

    add a if statement to support user override func

--- keyboards/heliar/wm1_hotswap/wm1_hotswap.c
+++ keyboards/heliar/wm1_hotswap/wm1_hotswap.c
@@ -28,30 +28,12 @@ void keyboard_pre_init_kb(void)
 }
 
 bool led_update_kb(led_t led_state) {
-    writePin(D7, !led_state.num_lock);
-    writePin(D6, !led_state.caps_lock);
-    writePin(D4, !led_state.scroll_lock);
+
+    if (led_update_user(led_state)){
+        writePin(D7, !led_state.num_lock);
+        writePin(D6, !led_state.caps_lock);
+        writePin(D4, !led_state.scroll_lock);
+    }
 
     return true;
-}
-
-void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
-
-    matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
-
-    matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    // put your per-action keyboard code here
-    // runs for every action, just before processing by the firmware
-
-    return process_record_user(keycode, record);
 }
