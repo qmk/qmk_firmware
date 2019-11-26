900d9686a16532c3640c519c4b0c1028d1e11455
Constantine Chen <csc_dev@protonmail.com>
2019-11-25 23:54:17 -0800

    Moved the git_macros variable out of the process_record_user function.

--- users/csc027/csc027.c
+++ users/csc027/csc027.c
@@ -1,35 +1,35 @@
 #include "csc027.h"
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    static const char* git_macros[] = {
-        // Make sure that the macro strings match the order they are declared
-        // in the custom_keycodes enum.
-        "git add ",
-        "git branch ",
-        "git checkout ",
-        "git cherry-pick ",
-        "git commit -m \"\""SS_TAP(X_LEFT),
-        "git diff ",
-        "git fetch ",
-        "git grep ",
-        "git log --decorate --oneline --graph ",
-        "git init ",
-        "git mv ",
-        "git merge ",
-        "git push ",
-        "git pull ",
-        "git rebase ",
-        "git remote ",
-        "git reset ",
-        "git show ",
-        "git stash ",
-        "git status ",
-        "git tag ",
-        SS_LCTRL(SS_LALT(SS_TAP(X_HOME)))"\t ",
-        SS_LCTRL(SS_LALT(SS_TAP(X_HOME)))"\t\t\t ",
-        SS_LCTRL(SS_LALT(SS_TAP(X_HOME)))SS_LALT("\t")
-    };
+static const char* git_macros[] = {
+    // Make sure that the macro strings match the order they are declared
+    // in the custom_keycodes enum.
+    "git add ",
+    "git branch ",
+    "git checkout ",
+    "git cherry-pick ",
+    "git commit -m \"\""SS_TAP(X_LEFT),
+    "git diff ",
+    "git fetch ",
+    "git grep ",
+    "git log --decorate --oneline --graph ",
+    "git init ",
+    "git mv ",
+    "git merge ",
+    "git push ",
+    "git pull ",
+    "git rebase ",
+    "git remote ",
+    "git reset ",
+    "git show ",
+    "git stash ",
+    "git status ",
+    "git tag ",
+    SS_LCTRL(SS_LALT(SS_TAP(X_HOME)))"\t ",
+    SS_LCTRL(SS_LALT(SS_TAP(X_HOME)))"\t\t\t ",
+    SS_LCTRL(SS_LALT(SS_TAP(X_HOME)))SS_LALT("\t")
+};
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
         case LOWER:
             if(record->event.pressed) {
