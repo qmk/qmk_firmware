28260c8534cecf999910a81daa6dba7fec49ff44
John Helveston <john.helveston@gmail.com>
2019-11-26 07:06:13 -0500

    Removed unnecessary rules

--- keyboards/lily58/keymaps/jhelvy/rules.mk
+++ keyboards/lily58/keymaps/jhelvy/rules.mk
@@ -4,21 +4,6 @@
 #
 AUTO_SHIFT_ENABLE = yes     # Autoshift by holding down a key
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
