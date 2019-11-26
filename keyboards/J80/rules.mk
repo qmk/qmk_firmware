bbfe0928b13f5d74f9f964802a1694c02a09b80a
Øyvind Wilhelmsen <mail@oew.no>
2019-11-26 23:42:56 +0100

    set bootmagic to lite

--- keyboards/J80/rules.mk
+++ keyboards/J80/rules.mk
@@ -14,13 +14,12 @@ BOOTLOADER = bootloadHID
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
-WS2812_DRIVER = i2c
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE  = no        # Mouse keys
+EXTRAKEY_ENABLE  = yes       # Audio control and System control
+CONSOLE_ENABLE   = yes        # Console for debug
+COMMAND_ENABLE   = yes        # Commands for debug and configuration
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE  = no        # Enable keyboard RGB underglow
 
 OPT_DEFS = -DDEBUG_LEVEL=0
