a6edc100619d1d478b802676859531882a46465d
onefiftynine <49580373+projectkeyboard@users.noreply.github.com>
2019-11-26 03:39:47 -0600

    Update signature87.c
    Disabled LED indicators to revisit later

--- keyboards/projectkb/signature87/signature87.c
+++ keyboards/projectkb/signature87/signature87.c
@@ -1,9 +1,10 @@
 #include "signature87.h"
 
-bool led_update_kb(led_t led_state) {
-    if (led_update_user(led_state)) {
-        writePin(B11, !led_state.caps_lock);
-        writePin(B5, !led_state.scroll_lock);
-    }
-    return true;
-}
+// Need to revise so build passes
+//bool led_update_kb(led_t led_state) {
+//    if (led_update_user(led_state)) {
+//        writePin(B11, !led_state.caps_lock);
+//        writePin(B5, !led_state.scroll_lock);
+//    }
+//    return true;
+//}
