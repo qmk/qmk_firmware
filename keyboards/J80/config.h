29dae58a1055aa7e181e52a38260ef607208730c
Øyvind Wilhelmsen <mail@oew.no>
2019-11-26 23:43:53 +0100

    removed comment

--- keyboards/J80/config.h
+++ keyboards/J80/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { A1, A2, A3, A4, A5, A6, A7, C7, C6, C5, C4, C3, C2, A0 }
 #define UNUSED_PINS { B4, C1, C0, D0, D1, D2, D3, D4, D5, D6, D7 }
 
-#define DIODE_DIRECTION COL2ROW // ????
+#define DIODE_DIRECTION COL2ROW
 #define DEBOUNCE 5
 
 #define BACKLIGHT_LEVELS 1
