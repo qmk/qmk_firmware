26c7ed87687fa10321ea465e3853786f10972b88
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-11-26 21:52:42 -0800

    Update keyboards/jae/j01/config.h
    Changing it to use the Esc key for bootmagic lite
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/jae/j01/config.h
+++ keyboards/jae/j01/config.h
@@ -222,4 +222,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Bootmagic Lite key configuration */
 #define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 1
+#define BOOTMAGIC_LITE_COLUMN 2
