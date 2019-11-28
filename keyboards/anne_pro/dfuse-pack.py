5e10d7ae40df79794e6f507dc87c6950c8b28b3b
Michiel Visser <info@webmichiel.nl>
2019-11-28 18:04:28 +0100

    Fix DFU packer python script for python3 (fixes #10)

--- keyboards/anne_pro/dfuse-pack.py
+++ keyboards/anne_pro/dfuse-pack.py
@@ -140,7 +140,7 @@ if __name__=="__main__":
           try:
             address = address & 0xFFFFFFFF
           except ValueError:
-            print "Address %s invalid." % address
+            print("Address %s invalid." % address)
             sys.exit(1)
           target.append({ 'address': address, 'data': ih.tobinstr(start=address, end=end-1)})
 
