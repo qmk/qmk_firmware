0929b7218977ba64459c623aaa7a951ed463016b
klackygears <bronzegears@gmail.com>
2019-12-03 13:04:56 -0500

    adding LaunchPad

---
+++ keyboards/launchpad/keymaps/klackygears/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* Underlight Configuration */
+#define RGB_DI_PIN F4
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10  // Number of LEDs
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
