afe58b8addac8680156227dba5f91410f1c6c857
Fatty Mink <marhalloweenvt@gmail.com>
2019-12-03 20:48:44 +0700

    Update ColorLice
    - Adjust info.json to support Configurator
    - Adjust layout name to fit with info.json
    - Seperate 2 feature: RGB Switch and RGB Underglow

--- keyboards/helix/rev2/config.h
+++ keyboards/helix/rev2/config.h
@@ -39,8 +39,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SERIAL_USE_MULTI_TRANSACTION
 
 /* Select hand configuration */
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 // Helix keyboard OLED support
--- keyboards/helix/rev2/keymaps/default/rules.mk
+++ keyboards/helix/rev2/keymaps/default/rules.mk
@@ -10,8 +10,8 @@ LINK_TIME_OPTIMIZATION_ENABLE = no  # if firmware size over limit, try this opti
 # Helix Spacific Build Options
 # you can uncomment and edit follows 7 Variables
 #  jp: 以下の7つの変数を必要に応じて編集し、コメントアウトをはずします。
-# HELIX_ROWS = 5              # Helix Rows is 4 or 5
-# OLED_ENABLE = no            # OLED_ENABLE
+HELIX_ROWS = 5              # Helix Rows is 4 or 5
+OLED_ENABLE = yes            # OLED_ENABLE
 # LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" insted of "common/glcdfont.c"
 # LED_BACK_ENABLE = no        # LED backlight (Enable WS2812 RGB underlight.)
 # LED_UNDERGLOW_ENABLE = no   # LED underglow (Enable WS2812 RGB underlight.)
