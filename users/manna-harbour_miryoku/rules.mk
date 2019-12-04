1b1c4f89785c6db85dae62138e696af10b1c20f4
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2019-12-04 13:29:56 +1100

    use LTO_ENABLE instead of EXTRAFLAGS
    This enables the same option, and disables action_macros, and action_functions, both of which seem to break when LTO is enabled. And this is a lot simpler to use.
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -2,4 +2,4 @@
 
 MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
-EXTRAFLAGS += -flto	     # Link Time Optimization to reduce code size, 31358->28034/28672
+LTO_ENABLE = yes
