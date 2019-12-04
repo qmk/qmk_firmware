92fa70c8a2528d96157f38708dbf34c6356074d7
mechmerlin <mechmerlin@gmail.com>
2019-12-03 20:51:03 -0800

    used misterkeeb's tool to convert my matrix tracing into a LAYOUT macro

--- keyboards/foxlab/orange75/32a/32a.h
+++ keyboards/foxlab/orange75/32a/32a.h
@@ -26,11 +26,20 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
+#define ___ KC_NO
+
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
-) \
-{ \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,  \
+    k10, k1d, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1e, k0e,  \
+    k20, k2d, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2e,  \
+    k30, k3d, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3e,  \
+    k40, k4d, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4c, k4e,  \
+    k50, k5d, k51, k54, k58, k59, k5a, k5c, k5e \
+){ \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, ___, k0e },  \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e },  \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e },  \
+    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, ___, k3d, k3e },  \
+    { k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a, ___, k4c, k4d, k4e },  \
+    { k50, k51, ___, ___, k54, ___, ___, ___, k58, k59, k5a, ___, k5c, k5d, k5e } \
 }
