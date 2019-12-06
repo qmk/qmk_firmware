42c2af383e063daa2d8ef93dbae65db1f731ece2
Alper Çugun <github@alper.nl>
2019-12-06 15:06:37 +0100

    Port changes to other layers

--- keyboards/contra/keymaps/alper/keymap.c
+++ keyboards/contra/keymaps/alper/keymap.c
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_planck_mit(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,                  KC_O,    KC_P,    KC_BSPC,
+    MT(MOD_LCTL, KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,                  KC_L,    KC_SCLN, KC_QUOT,
+    MT(MOD_LSFT, KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,               KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
+    KC_TAB,               KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,   MT(MOD_RGUI, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   /* Colemak
@@ -68,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_COLEMAK] = LAYOUT_planck_mit(
-    KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,               KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,                  KC_Y,    KC_SCLN, KC_BSPC,
+    MT(MOD_LCTL, KC_TAB), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,                  KC_I,    KC_O,    KC_QUOT,
+    MT(MOD_LSFT, KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM,               KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
+    KC_TAB,               KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,   MT(MOD_RGUI, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   /* Dvorak
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,               KC_QUOT, KC_COMM, KC_DOT,  KC_P,  KC_Y,    KC_F,    KC_G,    KC_C,                  KC_R,    KC_L,    KC_BSPC,
     MT(MOD_LCTL, KC_TAB), KC_A,    KC_O,    KC_E,    KC_U,  KC_I,    KC_D,    KC_H,    KC_T,                  KC_N,    KC_S,    KC_SLSH,
     MT(MOD_LSFT, KC_ESC), KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,    KC_B,    KC_M,    KC_W,                  KC_V,    KC_Z,    MT(MOD_RSFT, KC_ENT),
-    KC_TAB,               KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC,  RAISE,   MT(MOD_RGUI, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,               KC_LCTL, KC_LALT, KC_LGUI, LOWER,      KC_SPC,      RAISE,   MT(MOD_RGUI, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT
   ),
 // 0,0 is TAB in this layout because my key for Ctrl/Tab seems to be broken.
 // If yours isn't, it would be free.
--- keyboards/contra/keymaps/alper/readme.md
+++ keyboards/contra/keymaps/alper/readme.md
@@ -10,4 +10,4 @@ TODO
 
 * Port changes in Dvorak to the other keyboards for consistency
 * Try out tapdance for brackets (see ryanm101/keymap.c)
-* REMAP ESC To LT() so I can use it to select numbers (_NUMB) and functions more easily? (see msiu/keymap.c)
+* REMAP ESC To LT() so I can use it to select numbers (_NUMB) and functions more easily? (see msiu/keymap.c) https://docs.qmk.fm/#/feature_advanced_keycodes?id=switching-and-toggling-layers
