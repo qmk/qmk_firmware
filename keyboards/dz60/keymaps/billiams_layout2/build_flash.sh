e7541faadc27b53efc8421757a6354654f1a22b4
William Chang <william@factual.com>
2019-12-06 17:05:36 -0800

    update flash script per pull request review

--- keyboards/dz60/keymaps/billiams_layout2/build_flash.sh
+++ keyboards/dz60/keymaps/billiams_layout2/build_flash.sh
@@ -3,7 +3,4 @@
 # dfu-programmer atmega32u4 reset
 
 # run this in the qmk_firmware directory
-make dz60:billiams_layout2
-dfu-programmer atmega32u4 erase --force && \
-dfu-programmer atmega32u4 flash dz60_billiams_layout2.hex && \
-dfu-programmer atmega32u4 reset
+make dz60:billiams_layout2:flash
