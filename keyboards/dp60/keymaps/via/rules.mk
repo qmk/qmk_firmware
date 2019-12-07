0391d3868f1bc94e9c0a47109c886a670370397f
Lei Yu <yuleiz@gmail.com>
2019-12-07 23:24:36 +0800

    update dp60 configuration

--- keyboards/dp60/keymaps/via/rules.mk
+++ keyboards/dp60/keymaps/via/rules.mk
@@ -35,13 +35,11 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-#RGBLIGHT_ENABLE = yes       # Use RGB bottom light
+RGBLIGHT_ENABLE = yes       # Use RGB bottom light
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
 
 RAW_ENABLE = yes
-#WEBUSB_ENABLE = yes
 DYNAMIC_KEYMAP_ENABLE = yes
 
 CUSTOM_MATRIX = yes
 SRC += matrix.c keyboards/wilba_tech/wt_main.c
-#keyboards/stm60/webusb.c
--- keyboards/dp60/rules.mk
+++ keyboards/dp60/rules.mk
@@ -34,10 +34,10 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-#RGBLIGHT_ENABLE = yes       # Use RGB bottom light
+RGBLIGHT_ENABLE = yes       # Use RGB bottom light
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
 
 LAYOUTS = 60_ansi 60_hhkb 60_iso 60_ansi_split_bs_rshift
 
 CUSTOM_MATRIX = yes
-SRC += matrix.c 
+SRC += matrix.c
