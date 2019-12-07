ff877f173a72aeb26c9f46b0b7f77f80d85f25c0
Gergely Peidl <gergely@peidl.net>
2019-12-07 18:14:44 +0100

    Add tap dance for hungarian letters, multimedia control, slash/backslash.

--- keyboards/lets_split/keymaps/geripgeri/keymap.c
+++ keyboards/lets_split/keymaps/geripgeri/keymap.c
@@ -43,35 +43,34 @@ enum custom_keycodes {
 };
 
 
-enum td_hungarian_letters {
-  U,
-  O
+enum td_extra_keys {
+  U = 0 ,
+  O = 1,
+  NEXTPREV = 2,
+  SLASH = 3
 };
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  DOUBLE_SINGLE_TAP = 5, //send two single taps
+  TRIPLE_TAP = 6,
+  TRIPLE_HOLD = 7
+};
+
+int cur_dance (qk_tap_dance_state_t *state);
+
+void u_finished (qk_tap_dance_state_t *state, void *user_data);
+void o_finished (qk_tap_dance_state_t *state, void *user_data);
 
 uint32_t rgb_mode = RGBLIGHT_MODE_BREATHING + 1;
-
-
-void tap_dance_u_finished (qk_tap_dance_state_t *state, void *user_data) {
-  switch(state->count) {
-    case 1: SEND_STRING(SS_RALT("]")); break;
-    case 2: SEND_STRING(SS_RALT("-")); break;
-    case 3: SEND_STRING(SS_RALT("\\")); break;
-  }
-}
-
-void tap_dance_o_finished (qk_tap_dance_state_t *state, void *user_data) {
-  switch(state->count) {
-    case 1: SEND_STRING(SS_RALT("=")); break;
-    case 2: SEND_STRING(SS_RALT("0")); break;
-    case 3: SEND_STRING(SS_RALT("[")); break;
-  }
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [U] = ACTION_TAP_DANCE_FN(tap_dance_u_finished),
-  [O] = ACTION_TAP_DANCE_FN(tap_dance_o_finished)
-};
-
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -92,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12(
    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS, \
    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  TD(SLASH), KC_ENT , \
    KC_LCTL, KC_LALT, NUMPAD,  KC_LGUI, LOWER,   KC_SPC,       KC_BSPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
@@ -109,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'                `-----------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL, \
+  KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL, \
   SLIGHTLY, SMILE, JOY, RELAXED, HEART, _______,              KC_HOME, KC_PGUP, _______, KC_LCBR, KC_RCBR, KC_PIPE, \
   SAD, CRY, NETRURAL, SCREAM, THUMBSUP, _______,              KC_END, KC_PGDOWN,_______, KC_LBRC, KC_RBRC, KC_PSCREEN, \
   _______, _______, _______, _______, _______, KC_INSERT,     KC_DEL, _______,  TD(NEXTPREV), KC_VOLD, KC_VOLU, KC_MPLY \
@@ -129,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,\
   _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______ \
 ),
 
@@ -341,7 +340,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	SEND_STRING(":+1:");
       }    
       break;
-
   }
   return true;
 }
@@ -372,3 +370,65 @@ void led_set_user(uint8_t usb_led) {
   rgblight_mode_noeeprom(rgb_mode);
 }
 
+
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
+    else return SINGLE_HOLD;
+  }
+  else if (state->count == 2) {
+    /*
+     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+     * keystrokes of the key, and not the 'double tap' action/macro.
+    */
+    if (state->interrupted) return DOUBLE_SINGLE_TAP;
+    else if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  //Assumes no one is trying to type the same letter three times (at least not quickly).
+  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
+  if (state->count == 3) {
+    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
+    else return TRIPLE_HOLD;
+  }
+  else return 8; //magic number. At some point this method will expand to work for more presses
+}
+
+static tap utap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+static tap otap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+
+void u_finished (qk_tap_dance_state_t *state, void *user_data) {
+  utap_state.state = cur_dance(state);
+  switch(utap_state.state) {
+    case SINGLE_TAP: SEND_STRING(SS_RALT("]")); break;
+    case DOUBLE_TAP: SEND_STRING(SS_RALT("-")); break;
+    case TRIPLE_TAP: SEND_STRING(SS_RALT("\\")); break;
+  }
+}
+
+void o_finished (qk_tap_dance_state_t *state, void *user_data) {
+  otap_state.state = cur_dance(state);
+  switch(otap_state.state) {
+    case SINGLE_TAP: SEND_STRING(SS_RALT("=")); break;
+    case DOUBLE_TAP: SEND_STRING(SS_RALT("0")); break;
+    case TRIPLE_TAP: SEND_STRING(SS_RALT("[")); break;
+  }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [U] = ACTION_TAP_DANCE_FN(u_finished),
+  [O] = ACTION_TAP_DANCE_FN(o_finished),
+  [NEXTPREV] = ACTION_TAP_DANCE_DOUBLE(KC_MNXT, KC_MPRV),
+  [SLASH] = ACTION_TAP_DANCE_DOUBLE(KC_SLASH, KC_BSLS)
+ };
