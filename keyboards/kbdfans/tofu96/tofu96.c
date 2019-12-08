5a5e8ca20b4eb79a491b2a8784c29a48a231ba25
moyi4681 <moyi4681@users.noreply.github.com>
2019-12-08 15:39:57 +0800

    Update tofu96.c

--- keyboards/kbdfans/tofu96/tofu96.c
+++ keyboards/kbdfans/tofu96/tofu96.c
@@ -1,76 +1,26 @@
-/* Copyright 2018 @TurboMech /u/TurboMech <discord> @A9entOran9e#6134
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+#include "tofu96.h"
 
+void matrix_init_kb(void) {
+    // put your keyboard start-up code here
+    // runs once when the firmware starts up
 
-#include QMK_KEYBOARD_H
-#include "quantum.h"
-
-
-void matrix_scan_kb(void) {
-	// put your looping keyboard code here
-	// runs every cycle (a lot)
-
-	matrix_scan_user();
+    matrix_init_user();
+    led_init_ports();
 }
 
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-	// put your per-action keyboard code here
-	// runs for every action, just before processing by the firmware
-
-	return process_record_user(keycode, record);
+void led_init_ports(void) {
+    setPinOutput(B2);
+    writePinHigh(B2);
+    setPinOutput(B0);
+    writePinHigh(B0);
+	setPinOutput(B1);
+    writePinHigh(B1);
 }
-
-void led_set_kb(uint8_t usb_led) {
-	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-	led_set_user(usb_led);
+bool led_update_kb(led_t led_state) {
+    if(led_update_user(led_state)) {
+        writePin(B2, !led_state.caps_lock);
+        writePin(B0, !led_state.num_lock);
+        writePin(B1, !led_state.scroll_lock);		
+    }
+    return true;
 }
-
-__attribute__ ((weak))
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-		DDRB |= (1 << 0); PORTB &= ~(1 << 0);
-	} else {
-		DDRB &= ~(1 << 0); PORTB &= ~(1 << 0);
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		DDRB |= (1 << 2); PORTB &= ~(1 << 2);
-	} else {
-		DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-		DDRB |= (1 << 1); PORTB &= ~(1 << 1);
-	} else {
-		DDRB &= ~(1 << 1); PORTB &= ~(1 << 1);
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-
-	} else {
-
-	}
-
-}
-
