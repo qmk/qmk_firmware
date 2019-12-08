3dad9109f6fe54e3a931ce4a97722530214968d1
Yan-Fa Li <yanfa.li@fireeye.com>
2019-12-08 15:33:36 -0800

    Automatic mouse detection and enablement idea

--- lib/python/qmk/commands.py
+++ lib/python/qmk/commands.py
@@ -4,7 +4,7 @@ import json
 import qmk.keymap
 
 
-def create_make_command(keyboard, keymap, target=None):
+def create_make_command(keyboard, keymap, special=None, target=None):
     """Create a make compile command
 
     Args:
@@ -14,15 +14,29 @@ def create_make_command(keyboard, keymap, target=None):
         keymap
             The name of the keymap, for example 'algernon'
 
+        special
+            optional features to enable
+
         target
             Usually a bootloader.
 
     Returns:
         A command that can be run to make the specified keyboard and keymap
     """
+
+    command = ['make']
+    if special is not None and len(special) > 0:
+        options = ''
+        for value, key in enumerate(special):
+            options += '%s=%s ' (key, special[key])
+        command.append(options)
+
     if target is None:
-        return ['make', ':'.join((keyboard, keymap))]
-    return ['make', ':'.join((keyboard, keymap, target))]
+        command.append(':'.join((keyboard, keymap)))
+    else:
+        command.append(':'.join((keyboard, keymap, target)))
+
+    return command
 
 
 def parse_configurator_json(configurator_file):
@@ -45,10 +59,43 @@ def compile_configurator_json(user_keymap, bootloader=None):
     Returns:
         A command to run to compile and flash the C file.
     """
+
+    # scan keymap for special codes
+    special = {'MOUSE_ENABLE': 'no'}
+    for layer_num, layer in enumerate(user_keymap['layers']):
+        # check each key for special keys of interest
+        for key_pos, key in enumerate(layer):
+            ret = special_keys(key)
+            if (ret != 'IGNORE'):
+                special[ret] = 'yes'
+
     # Write the keymap C file
     qmk.keymap.write(user_keymap['keyboard'], user_keymap['keymap'], user_keymap['layout'], user_keymap['layers'])
 
     # Return a command that can be run to make the keymap and flash if given
     if bootloader is None:
-        return create_make_command(user_keymap['keyboard'], user_keymap['keymap'])
-    return create_make_command(user_keymap['keyboard'], user_keymap['keymap'], bootloader)
+        return create_make_command(user_keymap['keyboard'], user_keymap['keymap'], special)
+    return create_make_command(user_keymap['keyboard'], user_keymap['keymap'], special, bootloader)
+
+
+# used to turn out features based on presence of codes in keymap
+def special_keys(kc):
+    switcher = {
+        'KC_MS_U': 'MOUSE_ENABLE',
+        'KC_MS_D': 'MOUSE_ENABLE',
+        'KC_MS_L': 'MOUSE_ENABLE',
+        'KC_MS_R': 'MOUSE_ENABLE',
+        'KC_BTN1': 'MOUSE_ENABLE',
+        'KC_BTN2': 'MOUSE_ENABLE',
+        'KC_BTN3': 'MOUSE_ENABLE',
+        'KC_BTN4': 'MOUSE_ENABLE',
+        'KC_BTN5': 'MOUSE_ENABLE',
+        'KC_WH_U': 'MOUSE_ENABLE',
+        'KC_WH_D': 'MOUSE_ENABLE',
+        'KC_WH_L': 'MOUSE_ENABLE',
+        'KC_WH_R': 'MOUSE_ENABLE',
+        'KC_ACL0': 'MOUSE_ENABLE',
+        'KC_ALC1': 'MOUSE_ENABLE',
+        'KC_ACL2': 'MOUSE_ENABLE'
+    }
+    return switcher.get(kc, "IGNORE")
