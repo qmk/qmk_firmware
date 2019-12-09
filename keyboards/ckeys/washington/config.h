bd30d245000e0742cf895ba0c5cec8f9f068c8e7
merlin04 <Merlin04@users.noreply.github.com>
2019-12-08 17:49:08 -0800

    Fix encoder direction

--- keyboards/ckeys/washington/config.h
+++ keyboards/ckeys/washington/config.h
@@ -46,8 +46,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-#define ENCODERS_PAD_A { C6 }
-#define ENCODERS_PAD_B { D4 }
+#define ENCODERS_PAD_A { D4 }
+#define ENCODERS_PAD_B { C6 }
 
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
