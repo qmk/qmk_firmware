34cdeda194c0c436836ab7133f4eadbb7a877bd3
Yan-Fa Li <yanfa.li@fireeye.com>
2019-12-09 10:45:37 -0800

    Updates

--- keyboards/ergotravel/keymaps/yanfali/rules.mk
+++ keyboards/ergotravel/keymaps/yanfali/rules.mk
@@ -4,3 +4,4 @@ BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration(+1000)
 CONSOLE_ENABLE = yes         # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+LINK_TIME_OPTIMIZATION_ENABLE = yes
