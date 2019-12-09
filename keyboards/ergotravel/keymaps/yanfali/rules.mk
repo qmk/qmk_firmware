fd23fe9daeef7813fb55532128fcf76540fa6f37
Yan-Fa Li <yanfa.li@fireeye.com>
2019-12-09 10:25:55 -0800

    Update rules.mk so it compiles again

--- keyboards/ergotravel/keymaps/yanfali/rules.mk
+++ keyboards/ergotravel/keymaps/yanfali/rules.mk
@@ -1,1 +1,6 @@
 BOOTLOADER = qmk-dfu
+
+BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration(+1000)
+CONSOLE_ENABLE = yes         # Console for debug(+400)
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
