38a8cbbad0b931e7612dd53e90e1a1ada34b3336
moyi4681 <moyi4681@users.noreply.github.com>
2019-12-08 16:32:54 -0800

    Update keyboards/kbdfans/tofu96/tofu96.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/kbdfans/tofu96/tofu96.c
+++ keyboards/kbdfans/tofu96/tofu96.c
@@ -9,7 +9,7 @@ void matrix_init_kb(void) {
 void led_init_ports(void) {
     setPinOutput(B2);
     setPinOutput(B0);
-	setPinOutput(B1);
+    setPinOutput(B1);
 }
 bool led_update_kb(led_t led_state) {
     if(led_update_user(led_state)) {
