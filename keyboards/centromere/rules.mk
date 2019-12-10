618232f78993bdcdf8f1222460e9e7cb1d7fdc09
spe2 <falkenbergb@gmail.com>
2019-12-09 19:26:24 -0700

    Update keyboards/centromere/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/centromere/rules.mk
+++ keyboards/centromere/rules.mk
@@ -19,7 +19,7 @@ F_CPU = 8000000
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
+MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE = yes	# Console for debug(+400)
 COMMAND_ENABLE = yes   # Commands for debug and configuration
