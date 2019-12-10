562848b585b72cd1466fe776bd5884d47c5f77f8
spe2 <falkenbergb@gmail.com>
2019-12-09 20:15:33 -0700

    Update keyboards/centromere/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/centromere/rules.mk
+++ keyboards/centromere/rules.mk
@@ -19,7 +19,7 @@ BOOTLOADER = caterina
 #
 BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug(+400)
 COMMAND_ENABLE = yes   # Commands for debug and configuration
 CUSTOM_MATRIX = yes    # Remote matrix from the wireless bridge
