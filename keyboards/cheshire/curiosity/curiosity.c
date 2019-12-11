7bd85578331f588e630fd352535ad53755672bce
Khader Syed <35927972+chapter63@users.noreply.github.com>
2019-12-11 00:33:39 -0500

    confirm config that works

--- keyboards/cheshire/curiosity/curiosity.c
+++ keyboards/cheshire/curiosity/curiosity.c
@@ -1,27 +1,17 @@
 #include "curiosity.h"
 
 void matrix_init_board(void){
-    setPinOutput(A0);
-    setPinOutput(A1);
-    setPinOutput(A2);
+    setPinOutput(A8);
+    setPinOutput(A9);
+    setPinOutput(A10);
 }
 
-
-void led_set_kb(uint8_t usb_led) {
-    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
-        writePinLow(A0);
-    } else {
-        writePinHigh(A0);
+bool led_update_kb(led_t led_state) {
+    bool runDefault = led_update_user(led_state);
+    if (runDefault) {
+      writePin(A8, !led_state.num_lock);
+      writePin(A9, !led_state.caps_lock);
+      writePin(A10, !led_state.scroll_lock);
     }
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        writePinLow(A1);
-    } else {
-        writePinHigh(A1);
-    }
-    if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
-        writePinLow(A2);
-    } else {
-        writePinHigh(A2);
-    }
-    led_set_user(usb_led);
+    return runDefault;
 }
