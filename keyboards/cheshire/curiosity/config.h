a29b007681943f0db9ace3476c5081d60ba1ab5c
Khader Syed <35927972+chapter63@users.noreply.github.com>
2019-12-11 00:02:14 -0500

    update config to use

--- keyboards/cheshire/curiosity/config.h
+++ keyboards/cheshire/curiosity/config.h
@@ -23,16 +23,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 /* in python2: list(u"whatever".encode('utf-16-le')) */
 /*   at most 32 characters or the ugly hack in usb_main.c borks */
-#define MANUFACTURER Cheshire Designs LLC
+#define MANUFACTURER Cheshire Designs
 #define PRODUCT Curiosity
-#define DESCRIPTION Curiosity
+#define DESCRIPTION "Cheshire Designs Curiosity"
 
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 16
 
-#define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, A5, A6, A7 }
-#define MATRIX_ROW_PINS { B13, B14, A1, A2, A4 }
+#define MATRIX_COL_PINS { B11, B10, B2, B1, B0, A7, A6, A5, B9, B8, B7, B6, B5, B4, B3, A15 }
+#define MATRIX_ROW_PINS { B13, B14, A4, A2, A1 }
 #define DIODE_DIRECTION COL2ROW
 
 #define BACKLIGHT_LEVELS 6
