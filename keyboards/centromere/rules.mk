e46fb7c85e49f3dd187bc25811843e61c8da878b
spe2 <falkenbergb@gmail.com>
2019-12-10 19:22:35 -0700

    Apply suggestions from code review
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/centromere/rules.mk
+++ keyboards/centromere/rules.mk
@@ -20,14 +20,14 @@ BOOTLOADER = caterina
 BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug(+400)
+CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = yes   # Commands for debug and configuration
 CUSTOM_MATRIX = yes    # Remote matrix from the wireless bridge
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-# SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes		# USB Nkey Rollover - not yet supported in LUFA
-# BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-# MIDI_ENABLE = yes 		# MIDI controls
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes		# USB Nkey Rollover
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+MIDI_ENABLE = no 		# MIDI controls
 UNICODE_ENABLE = yes 		# Unicode
 BLUETOOTH_ENABLE = no # Enable Bluetooth with the Adafruit EZ-Key HID
 
