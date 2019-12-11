6318baa9489fdfd74185567ed93b479bad8d5dec
starcalleramethyst <toppleld@gmail.com>
2019-12-11 14:03:47 -0600

    tap dance enumeration and timing
    added a TD key to send space on press and . on double tap. added tapping term to set tap detection time.

--- keyboards/rgbkb/zen/rev1/keymaps/starcaller_v1.1/keymap.c
+++ keyboards/rgbkb/zen/rev1/keymaps/starcaller_v1.1/keymap.c
@@ -66,6 +66,18 @@ break;
 
   }
   return true;
+};  
+
+//Tap Dance Declarations
+enum {
+  TD_SPC_DOT = 0
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_SPC_DOT]  = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_PDOT)
+// Other declarations would go here, separated by commas, if you have them
 };
 
 
@@ -77,7 +89,7 @@ KC_GESC, KC_1, 	KC_2, 	KC_3, 	KC_4, 	KC_5, 				KC_6, 	KC_7, 	KC_8, 	KC_9, 	KC_0,
 KC_TAB,  KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 				KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, KC_BSPC, 
 KC_LCTL, KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 				KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, KC_QUOT, 
 KC_LSFT, KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 				KC_N, 	KC_M, 	KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT, 
-TO(2),   KC_LGUI, KC_LALT, RGB_TOG, MO(1), KC_SPC, KC_BSPC, KC_ENT, KC_SPC, MO(3), KC_MINS, KC_EQL, KC_PGUP, KC_PGDN),
+TO(2),   KC_LGUI, KC_LALT, RGB_TOG, MO(1), TD(TD_SPC_DOT), KC_BSPC, KC_ENT, TD(TD_SPC_DOT), MO(3), KC_MINS, KC_EQL, KC_PGUP, KC_PGDN),
 
 	[1] = LAYOUT(
 KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6, 			KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
