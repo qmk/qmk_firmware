4c381de4c0b3ff8dedcde0e4e0d77d4d7e0a31b1
Richard Baptist <rpbaptist@gmail.com>
2019-12-12 15:13:26 +0100

    Reduce tapping term slighly

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -96,7 +96,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGB_MATRIX_ANIMATION_SPEED_SLOWER RGB_MATRIX_ANIMATION_SPEED_SLOW / 3
 #endif
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 140
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
