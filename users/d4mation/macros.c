7a58080d10b51081c0738dd26a6deeeced681819
Eric Defore <eric.defore@gmail.com>
2019-12-16 12:24:50 -0500

    OBetter check for Unicode Enabled. Moved some checks into macros.c

--- users/d4mation/macros.c
+++ users/d4mation/macros.c
@@ -43,105 +43,113 @@ bool process_record_user( uint16_t keycode, keyrecord_t *record ) {
       return false;
       break;
 
-    case AMENO:  /* ༼ つ ◕_◕ ༽つ */
+    #ifdef UNICODE_ENABLE
 
-      if ( record->event.pressed ) {
-
-        send_unicode_hex_string( "0F3C 0020 3064 0020 25D5 005F 25D5 0020 0F3D 3064" );
-
-      }
-
-      return false;
-      break;
-
-    case MAGIC:  /* (∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. * */
-
-      if ( record->event.pressed ) {
-
-        send_unicode_hex_string( "0028 2229 0020 0361 00B0 0020 035C 0296 0020 0361 00B0 0029 2283 2501 2606 FF9F 002E 0020 002A" );
-
-      }
-
-      return false;
-      break;
-
-    case LENNY:  /* ( ͡° ͜ʖ ͡°) */
-
-      if ( record->event.pressed ) {
-
-        send_unicode_hex_string( "0028 0020 0361 00B0 0020 035C 0296 0020 0361 00b0 0029" );
-
-      }
-
-      return false;
-      break;
-
-    case DISFACE:  /* ಠ_ಠ */
-
-      if ( record->event.pressed ) {
-        send_unicode_hex_string( "0CA0 005F 0CA0" );
-      }
-
-      return false;
-      break;
-
-    case TFLIP:  /* (╯°□°)╯ ︵ ┻━┻ */
-
-      if ( record->event.pressed ) {
-
-        send_unicode_hex_string( "0028 256F 00b0 25A1 00B0 0029 256F FE35 253B 2501 253B" );
-
-      }
-
-      return false;
-      break;
-
-    case TPUT:  /* ┬──┬ ノ( ゜-゜ノ) */
-
-      if ( record->event.pressed ) {
-
-        send_unicode_hex_string( "252C 2500 2500 252C 0020 30CE 0028 0020 309C 002D 309C 30CE 0029" );
-
-      }
-
-      return false;
-      break;
-
-    case SHRUG:  /* ¯\_(ツ)_/¯ */
-
-      if ( record->event.pressed ) {
-
-        send_unicode_hex_string( "00AF 005C 005F 0028 30C4 0029 005F 002F 00AF" );
-
-      }
-
-      return false;
-      break;
-
-    case ZALGO:  /* Toggles Zalgo Text mode */
-
-      if ( record->event.pressed ) {
-        zalgo_enabled = ! zalgo_enabled;
-      }
-
-      return false;
-      break;
-
-    default:
-
-      if ( zalgo_enabled ) {
-
-        if ( keycode < KC_A || ( keycode > KC_0 && keycode < KC_MINUS ) || keycode > KC_SLASH ) {
-          process_record_keymap( keycode, record );
-          return true;
-        }
+      case AMENO:  /* ༼ つ ◕_◕ ༽つ */
 
         if ( record->event.pressed ) {
-          zalgo_text( keycode );
+
+          send_unicode_hex_string( "0F3C 0020 3064 0020 25D5 005F 25D5 0020 0F3D 3064" );
+
         }
 
         return false;
-      }
+        break;
+
+      case MAGIC:  /* (∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. * */
+
+        if ( record->event.pressed ) {
+
+          send_unicode_hex_string( "0028 2229 0020 0361 00B0 0020 035C 0296 0020 0361 00B0 0029 2283 2501 2606 FF9F 002E 0020 002A" );
+
+        }
+
+        return false;
+        break;
+
+      case LENNY:  /* ( ͡° ͜ʖ ͡°) */
+
+        if ( record->event.pressed ) {
+
+          send_unicode_hex_string( "0028 0020 0361 00B0 0020 035C 0296 0020 0361 00b0 0029" );
+
+        }
+
+        return false;
+        break;
+
+      case DISFACE:  /* ಠ_ಠ */
+
+        if ( record->event.pressed ) {
+          send_unicode_hex_string( "0CA0 005F 0CA0" );
+        }
+
+        return false;
+        break;
+
+      case TFLIP:  /* (╯°□°)╯ ︵ ┻━┻ */
+
+        if ( record->event.pressed ) {
+
+          send_unicode_hex_string( "0028 256F 00b0 25A1 00B0 0029 256F FE35 253B 2501 253B" );
+
+        }
+
+        return false;
+        break;
+
+      case TPUT:  /* ┬──┬ ノ( ゜-゜ノ) */
+
+        if ( record->event.pressed ) {
+
+          send_unicode_hex_string( "252C 2500 2500 252C 0020 30CE 0028 0020 309C 002D 309C 30CE 0029" );
+
+        }
+
+        return false;
+        break;
+
+      case SHRUG:  /* ¯\_(ツ)_/¯ */
+
+        if ( record->event.pressed ) {
+
+          send_unicode_hex_string( "00AF 005C 005F 0028 30C4 0029 005F 002F 00AF" );
+
+        }
+
+        return false;
+        break;
+
+      case ZALGO:  /* Toggles Zalgo Text mode */
+
+        if ( record->event.pressed ) {
+          zalgo_enabled = ! zalgo_enabled;
+        }
+
+        return false;
+        break;
+
+    #endif
+
+    default:
+
+      #ifdef UNICODE_ENABLE
+
+        if ( zalgo_enabled ) {
+
+          if ( keycode < KC_A || ( keycode > KC_0 && keycode < KC_MINUS ) || keycode > KC_SLASH ) {
+            process_record_keymap( keycode, record );
+            return true;
+          }
+
+          if ( record->event.pressed ) {
+            zalgo_text( keycode );
+          }
+
+          return false;
+        }
+
+      #endif
 
       break;
   }
--- users/d4mation/rules.mk
+++ users/d4mation/rules.mk
@@ -1,13 +1,13 @@
-SRC += d4mation.c
+SRC += d4mation.c \
+  tap-hold.c \
+  macros.c
 
 BOOTMAGIC_ENABLE = no
 LTO_ENABLE = yes
 MOUSEKEY_ENABLE = no
 
 ifeq ($(strip $(UNICODE_ENABLE)), yes)
-  SRC += zalgo.c \
-         tap-hold.c \
-         macros.c
+  SRC += zalgo.c
 endif
 
 ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
