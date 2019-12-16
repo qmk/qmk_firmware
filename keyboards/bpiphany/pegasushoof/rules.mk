59bfbe83883fbf2337b9541aa2875e935064a1f4
nygel <github.com@lyndley.com>
2019-12-16 13:14:52 +0000

    - add rules definition

--- keyboards/bpiphany/pegasushoof/rules.mk
+++ keyboards/bpiphany/pegasushoof/rules.mk
@@ -27,6 +27,6 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 
-LAYOUTS = tkl_ansi
+LAYOUTS = tkl_ansi tkl_iso
 
 DEFAULT_FOLDER=bpiphany/pegasushoof/2013
