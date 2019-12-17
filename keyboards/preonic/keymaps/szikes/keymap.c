3ab2d9b320d0216ac6000c8d5dd79dbb0c72cd27
szikes-adam <szikes.adam@gmail.com>
2019-12-17 19:05:01 +0100

    Added shifted lower + swap z and y

--- keyboards/preonic/keymaps/szikes/keymap.c
+++ keyboards/preonic/keymaps/szikes/keymap.c
@@ -6,6 +6,7 @@ enum preonic_layers {
   _QWERTY,
   _SHIFT,
   _LOWER,
+  _SHIFTED_LOWER,
   _RAISE,
   _SHIFTED_RAISE,
   _ADJUST
@@ -69,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
   KC_ESC,   HU_1,        HU_2,    HU_3,        HU_4,    HU_5,    HU_6,    HU_7,        HU_8,        HU_9,        HU_0,    KC_BSPC, \
-  KC_TAB,   HU_Q,        HU_W,    TD(TD_E_EE), HU_R,    HU_T,    HU_Y,    TD(TD_U_UU), TD(TD_I_II), TD(TD_O_OO), HU_P,    HU_BSLS, \
+  KC_TAB,   HU_Q,        HU_W,    TD(TD_E_EE), HU_R,    HU_T,    HU_Z,    TD(TD_U_UU), TD(TD_I_II), TD(TD_O_OO), HU_P,    HU_BSLS, \
   KC_LCTL,  TD(TD_A_AA), HU_S,    HU_D,        HU_F,    HU_G,    HU_H,    HU_J,        HU_K,        HU_L,        HU_SCLN, KC_ENT,  \
-  SHIFT,    HU_Z,        HU_X,    HU_C,        HU_V,    HU_B,    HU_N,    HU_M,        HU_COMM,     HU_DOT,      KC_UP,   HU_SLSH, \
+  SHIFT,    HU_Y,        HU_X,    HU_C,        HU_V,    HU_B,    HU_N,    HU_M,        HU_COMM,     HU_DOT,      KC_UP,   HU_SLSH, \
   HU_EQL,   HU_QUOT,     KC_LGUI, KC_LALT,     LOWER,   KC_SPC,  KC_SPC,  RAISE,       HU_MINS,     KC_LEFT,     KC_DOWN, KC_RGHT  \
 ),
 
@@ -90,9 +91,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 @ */
 [_SHIFT] = LAYOUT_preonic_grid( \
   S(KC_ESC),  HU_EXLM,      HU_AT,      HU_HASH,      HU_DLR,  HU_PERC,  HU_CIRC,    HU_AMPR,      HU_ASTR,      HU_LPRN,       HU_RPRN,    S(KC_BSPC), \
-  S(KC_TAB),  S(HU_Q),      S(HU_W),    TD(TD_S_E_EE),S(HU_R), S(HU_T),  S(HU_Y),    TD(TD_S_U_UU),TD(TD_S_I_II),TD(TD_S_O_OO), S(HU_P),    HU_PIPE,    \
+  S(KC_TAB),  S(HU_Q),      S(HU_W),    TD(TD_S_E_EE),S(HU_R), S(HU_T),  S(HU_Z),    TD(TD_S_U_UU),TD(TD_S_I_II),TD(TD_S_O_OO), S(HU_P),    HU_PIPE,    \
   S(KC_LCTL), TD(TD_S_A_AA),S(HU_S),    S(HU_D),      S(HU_F), S(HU_G),  S(HU_H),    S(HU_J),      S(HU_K),      S(HU_L),       HU_COLN,    S(KC_ENT),  \
-  _______,    S(HU_Z),      S(HU_X),    S(HU_C),      S(HU_V), S(HU_B),  S(HU_N),    S(HU_M),      HU_LESS,      HU_MORE,       S(KC_UP),   HU_QST,     \
+  _______,    S(HU_Y),      S(HU_X),    S(HU_C),      S(HU_V), S(HU_B),  S(HU_N),    S(HU_M),      HU_LESS,      HU_MORE,       S(KC_UP),   HU_QST,     \
   HU_PLUS,    HU_DQOT,      S(KC_LGUI), S(KC_LALT),   _______, S(KC_SPC), S(KC_SPC), _______,      HU_UNDS,      S(KC_LEFT),    S(KC_DOWN), S(KC_RGHT) \
 ),
 
@@ -117,6 +118,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______,   KC_MNXT,   KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
+/* Lower + Shift
+ * ,-----------------------------------------------------------------------------------.
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_SHIFTED_LOWER] = LAYOUT_preonic_grid( \
+  HU_TILD,   HU_EXLM,  HU_AT,    HU_HASH,  HU_DLR,    HU_PERC,   HU_CIRC,   HU_AMPR, HU_ASTR, HU_LPRN, HU_RPRN, S(KC_DEL), \
+  HU_TILD,   HU_EXLM,  HU_AT,    HU_HASH,  HU_DLR,    HU_PERC,   HU_CIRC,   HU_AMPR, HU_ASTR, HU_LPRN, HU_RPRN, S(KC_DEL), \
+  S(KC_DEL), S(KC_F1), S(KC_F2), S(KC_F3), S(KC_F4),  S(KC_F5),  S(KC_F6),  HU_UNDS, HU_PLUS, HU_LCBR, HU_RCBR, HU_PIPE,   \
+  _______,   S(KC_F7), S(KC_F8), S(KC_F9), S(KC_F10), S(KC_F11), S(KC_F12), KC_NUHS, KC_NUBS, _______, _______, _______,   \
+  _______,   _______,  _______,  _______,  _______,   _______,   _______,   _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY    \
+),
+
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
@@ -134,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   HU_GRV,  HU_1,    HU_2,    HU_3,    HU_4,    HU_5,    HU_6,    HU_7,          HU_8,    HU_9,          HU_0,    KC_DEL,     \
   HU_GRV,  HU_1,    HU_2,    HU_3,    HU_4,    HU_5,    HU_6,    TD(TD_UE_UEE), HU_8,    TD(TD_OE_OEE), HU_0,    KC_DEL,     \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   HU_MINS,       HU_EQL,  HU_LBRC,       HU_RBRC, HU_BSLS,    \
-  KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS,       KC_NUBS, S(KC_INSERT),  KC_PGUP, KC_PSCREEN, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS,       KC_NUBS, KC_INSERT,     KC_PGUP, KC_PSCREEN, \
   _______, _______, _______, _______, _______, _______, _______, _______,       _______, KC_HOME,       KC_PGDN, KC_END      \
 ),
 
@@ -207,6 +229,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           update_tri_layer(_LOWER, _RAISE, _ADJUST);
           update_tri_layer(_SHIFT, _RAISE, _SHIFTED_RAISE);
+          update_tri_layer(_SHIFT, _LOWER, _SHIFTED_LOWER);
           return false;
           break;
         case RAISE:
@@ -217,6 +240,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           update_tri_layer(_LOWER, _RAISE, _ADJUST);
           update_tri_layer(_SHIFT, _RAISE, _SHIFTED_RAISE);
+          update_tri_layer(_SHIFT, _LOWER, _SHIFTED_LOWER);
           return false;
           break;
       }
