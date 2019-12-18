821f1d03169f1ac3c344a54eea5143df1ba677d1
hineybush <hineybushkeyboards@gmail.com>
2019-12-18 01:06:08 -0500

    update info.json and default keymap per LZ

--- keyboards/hineybush/physix/keymaps/default/keymap.c
+++ keyboards/hineybush/physix/keymaps/default/keymap.c
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,                 KC_PGUP,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,    KC_ENT,                              KC_PGDN,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,    KC_RSFT,                KC_UP,
-    KC_LCTL,          KC_LALT,          KC_SPC,  KC_LGUI,          KC_SPC,  MO(_FN),            KC_RCTL,                           KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL,          KC_LALT,          KC_SPC,  MO(_FN),          KC_SPC,  KC_RALT,            KC_RGUI,                           KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN] = LAYOUT_split_bksp_275_rspace(
-    KC_TRNS, RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_TOGG, BL_DEC,  BL_INC,  KC_TRNS, KC_TRNS, KC_INS,
+    KC_TRNS, RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_BRTG, BL_TOGG, BL_INC,  BL_DEC,  KC_TRNS, KC_TRNS, KC_INS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_HOME,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_END,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
