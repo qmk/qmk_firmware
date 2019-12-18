9b3b4e19460c584c69a42a925638daf19fd89539
fauxpark <fauxpark@gmail.com>
2019-12-19 08:30:51 +1100

    Missed these LTO blocks

--- keyboards/dztech/dz60rgb_wkl/v1/config.h
+++ keyboards/dztech/dz60rgb_wkl/v1/config.h
@@ -32,6 +32,12 @@
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 3
 
+/* disable these deprecated features by default */
+#ifndef LINK_TIME_OPTIMIZATION_ENABLE
+#    define NO_ACTION_MACRO
+#    define NO_ACTION_FUNCTION
+#endif
+
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
