bec83d50ac9677d9d6bd1af23af5f660859414ab
Nick Brassel <nick@tzarc.org>
2019-12-20 07:15:34 +1100

    Attempt to sort out 32-bit timer overflow arithmetic for 16-bit timers.

--- tmk_core/common/chibios/timer.c
+++ tmk_core/common/chibios/timer.c
@@ -9,8 +9,25 @@ void timer_clear(void) {}
 uint16_t timer_read(void) { return (uint16_t)timer_read32(); }
 
 uint32_t timer_read32(void) {
+#if CH_CFG_ST_RESOLUTION == 32
+
     uint32_t systime = (uint32_t)chVTGetSystemTime();
     return TIME_I2MS(systime);
+
+#elif CH_CFG_ST_RESOLUTION == 16
+
+    static uint32_t last_systime = (uint32_t)chVTGetSystemTime();
+    static uint32_t overflow     = 0;
+
+    uint32_t systime = (uint32_t)chVTGetSystemTime();
+    if (systime < last_systime) {
+        overflow += (1 + (uint32_t)UINT16_MAX);
+    }
+
+    last_systime = systime;
+    return TIME_I2MS(systime + overflow);
+
+#endif
 }
 
 uint16_t timer_elapsed(uint16_t last) { return TIMER_DIFF_16(timer_read(), last); }
