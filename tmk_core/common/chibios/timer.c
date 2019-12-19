49deb18b061ee4017a4d58b4b594764ebebb45d3
Nick Brassel <nick@tzarc.org>
2019-12-20 07:08:07 +1100

    Timer simplification, 100k back working.

--- tmk_core/common/chibios/timer.c
+++ tmk_core/common/chibios/timer.c
@@ -2,26 +2,15 @@
 
 #include "timer.h"
 
-static systime_t last_systime    = 0;
-static systime_t overflow        = 0;
-static uint32_t  current_time_ms = 0;
-
 void timer_init(void) { timer_clear(); }
 
-void timer_clear(void) {
-    last_systime    = chVTGetSystemTime();
-    overflow        = 0;
-    current_time_ms = 0;
-}
+void timer_clear(void) {}
 
 uint16_t timer_read(void) { return (uint16_t)timer_read32(); }
 
 uint32_t timer_read32(void) {
-    systime_t     systime = chVTGetSystemTime();
-    sysinterval_t elapsed = chTimeDiffX(last_systime, systime);
-    last_systime          = systime;
-    current_time_ms += chTimeI2MS(elapsed);
-    return current_time_ms;
+    uint32_t systime = (uint32_t)chVTGetSystemTime();
+    return TIME_I2MS(systime);
 }
 
 uint16_t timer_elapsed(uint16_t last) { return TIMER_DIFF_16(timer_read(), last); }
