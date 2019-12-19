223d2ebbd8f07dae26dfd68337d2d8b3754500df
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2019-12-19 10:57:26 -0600

    Update keyboards/lily58/keymaps/mikefightsbears/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/mikefightsbears/rules.mk
+++ keyboards/lily58/keymaps/mikefightsbears/rules.mk
@@ -4,7 +4,7 @@
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
