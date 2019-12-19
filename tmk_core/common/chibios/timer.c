93b3764b9b97579eca42e1b0aa869654df4e66b0
Nick Brassel <nick@tzarc.org>
2019-12-20 07:28:15 +1100

    Another attempt at fixing 16-bit timers.

--- tmk_core/common/chibios/timer.c
+++ tmk_core/common/chibios/timer.c
@@ -2,30 +2,37 @@
 
 #include "timer.h"
 
+#if CH_CFG_ST_RESOLUTION < 32
+static uint32_t last_systime = 0;
+static uint32_t overflow     = 0;
+#endif
+
 void timer_init(void) { timer_clear(); }
 
-void timer_clear(void) {}
+void timer_clear(void) {
+#if CH_CFG_ST_RESOLUTION < 32
+    last_systime = (uint32_t)chVTGetSystemTime();
+    overflow     = 0;
+#endif
+}
 
 uint16_t timer_read(void) { return (uint16_t)timer_read32(); }
 
 uint32_t timer_read32(void) {
-#if CH_CFG_ST_RESOLUTION == 32
-
     uint32_t systime = (uint32_t)chVTGetSystemTime();
-    return TIME_I2MS(systime);
 
-#elif CH_CFG_ST_RESOLUTION == 16
+#if CH_CFG_ST_RESOLUTION < 32
 
-    static uint32_t last_systime = (uint32_t)chVTGetSystemTime();
-    static uint32_t overflow     = 0;
-
-    uint32_t systime = (uint32_t)chVTGetSystemTime();
     if (systime < last_systime) {
-        overflow += (1 + (uint32_t)UINT16_MAX);
+        overflow += ((uint32_t)1) << CH_CFG_ST_RESOLUTION;
     }
 
     last_systime = systime;
-    return TIME_I2MS(systime + overflow);
+    return (uint32_t)TIME_I2MS(systime + overflow);
+
+#else
+
+    return (uint32_t)TIME_I2MS(systime);
 
 #endif
 }
