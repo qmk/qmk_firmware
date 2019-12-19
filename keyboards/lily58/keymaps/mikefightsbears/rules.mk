18544c6bf729243179a2e7ee73b5f6c914d73446
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2019-12-19 10:57:19 -0600

    Update keyboards/lily58/keymaps/mikefightsbears/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/mikefightsbears/rules.mk
+++ keyboards/lily58/keymaps/mikefightsbears/rules.mk
@@ -3,7 +3,7 @@
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
