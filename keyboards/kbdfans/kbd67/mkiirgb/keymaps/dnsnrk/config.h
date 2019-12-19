bfbbf7114ae354250678922adeb4d5a2983b0d19
Denis Nareike <denis.nareike@gmail.com>
2019-12-19 11:22:24 +0100

    lowered debounce

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/dnsnrk/config.h
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/dnsnrk/config.h
@@ -1,5 +1,4 @@
-/* Copyright 2018 MechMerlin
- *
+/*
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -17,8 +16,9 @@
 #pragma once
 
 // place overrides here
+#undef QMK_KEYS_PER_SCAN
 #define QMK_KEYS_PER_SCAN 4
 #undef DEBOUNCE
-#define DEBOUNCE 15
+#define DEBOUNCE 10
 
 
