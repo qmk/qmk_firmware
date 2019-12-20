cd24cc3ebae0e9d292eaf5b9e034fb467e4a819e
Jeremy Soller <jeremy@system76.com>
2019-12-19 20:36:55 -0700

    Debug keypresses

--- keyboards/system76/virgo/virgo.c
+++ keyboards/system76/virgo/virgo.c
@@ -4,6 +4,6 @@ void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
   debug_matrix=true;
-  //debug_keyboard=true;
+  debug_keyboard=true;
   //debug_mouse=true;
 }
