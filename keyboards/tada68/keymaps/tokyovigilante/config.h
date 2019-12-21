960093da0cb0a1cef15f19a966efeb0196f83a31
Ryan <tokyovigilante@users.noreply.github.com>
2019-12-21 13:46:56 +1100

    Remove obselete #endif
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/tada68/keymaps/tokyovigilante/config.h
+++ keyboards/tada68/keymaps/tokyovigilante/config.h
@@ -23,5 +23,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QMK_ESC_OUTPUT F0 // usually COL
 #define QMK_ESC_INPUT D0 // usually ROW
 #define QMK_LED B6
-
-#endif
