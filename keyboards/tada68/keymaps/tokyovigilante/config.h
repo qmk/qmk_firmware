2a5adb118e19eaf25faa022d50a9bd64358fa598
Ryan <tokyovigilante@users.noreply.github.com>
2019-12-21 13:47:54 +1100

    Remove obselete #define
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/tada68/keymaps/tokyovigilante/config.h
+++ keyboards/tada68/keymaps/tokyovigilante/config.h
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#define CONFIG_H
 
 #undef DESCRIPTION
 #define DESCRIPTION     QMK keyboard firmware for TADA68 with QMK bootloader
