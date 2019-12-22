df79264ffc6687545a6f938909c9951a68380a5c
yiancar <yiangosyiangou@cytanet.com.cy>
2019-12-22 16:36:57 +0000

    Update keyboards/doppelganger/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/doppelganger/rules.mk
+++ keyboards/doppelganger/rules.mk
@@ -27,7 +27,7 @@ BOOTLOADER = atmel-dfu
 #
 BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug(+400)
 COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
