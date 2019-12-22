750941d9fb428a4acc34fac995aad29003ac65b0
yiancar <yiangosyiangou@cytanet.com.cy>
2019-12-22 16:42:40 +0000

    Update keyboards/doppelganger/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/doppelganger/rules.mk
+++ keyboards/doppelganger/rules.mk
@@ -25,7 +25,7 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug(+400)
 COMMAND_ENABLE = no        # Commands for debug and configuration
