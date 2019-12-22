40ea4f503faf7014e58e523e8f8f8721a61c317d
yiancar <yiangosyiangou@cytanet.com.cy>
2019-12-22 16:41:27 +0000

    More fixes my g:)

--- keyboards/doppelganger/doppelganger.c
+++ keyboards/doppelganger/doppelganger.c
@@ -15,35 +15,6 @@
  */
 #include "doppelganger.h"
 
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
-
-/*
-
-void matrix_init_kb(void) {
-  // put your keyboard start-up code here
-  // runs once when the firmware starts up
-
-  matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-  // put your looping keyboard code here
-  // runs every cycle (a lot)
-
-  matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-  // put your per-action keyboard code here
-  // runs for every action, just before processing by the firmware
-
-  return process_record_user(keycode, record);
-}
-
-*/
-
 bool led_update_kb(led_t led_state) {
   bool res = led_update_user(led_state);
   if(res) {
@@ -58,10 +29,6 @@ bool led_update_kb(led_t led_state) {
 }
 
 __attribute__((weak)) layer_state_t layer_state_set_user(layer_state_t state) {
-  if( IS_LAYER_ON(1) ){
-    writePinLow(B0);
-  } else {
-    writePinHigh(B0);
-  }
+  writePin(B0, (state & (1UL << 1)));
   return state;
 }
