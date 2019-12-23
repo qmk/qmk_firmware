d17a743ff635a9fdce990324c79b735fdcdd9df5
toshi0383 <t.suzuki326@gmail.com>
2019-12-23 11:50:32 +0900

    update zinc:toshi0383

--- keyboards/zinc/keymaps/toshi0383/keymap.c
+++ keyboards/zinc/keymaps/toshi0383/keymap.c
@@ -18,18 +18,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    * ,-----------------------------------------.             ,-----------------------------------------.
    * | Esc  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |BSpace|
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Tab  |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   :  |Shift |
+   * | Tab  |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |Shift | GUI  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |Space |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  | Ctrl | GUI  | Alt  | SCMD |
+   * |Space |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  | Ctrl |  .   | Alt  | SCMD |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |Lower |Enter |             |Enter |Raise |      |      |      |      |
+   * | GUI  |      |      |      |Lower |Enter |             |Enter |Raise |      |      |      |      |
    * `-----------------------------------------'             `-----------------------------------------'
    */
   [_BASE] = LAYOUT_ortho_4x12(
    KC_ESC, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,             KC_Y,  KC_U,  KC_I, KC_O,   KC_P, KC_BSPACE, \
-   KC_TAB, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,             KC_H,  KC_J,  KC_K, KC_L,   KC_RGUI, KC_RSFT, \
-   KC_SPC, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,             KC_N,  KC_M,  KC_RCTL, KC_RGUI, KC_RALT, RGUI(KC_RSFT), \
-   _______, _______, _______, _______, LOWER, KC_ENT,    KC_ENT, RAISE, _______, _______, _______, _______ \
+   KC_TAB, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,             KC_H,  KC_J,  KC_K, KC_L,   SFT_T(KC_SCLN), KC_RGUI,
+   KC_SPC, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,             KC_N,  KC_M,  KC_RCTL, KC_DOT, KC_RALT, RGUI(KC_RSFT), \
+   KC_RGUI, _______, _______, _______, LOWER, KC_ENT,    KC_ENT, RAISE, _______, _______, _______, _______ \
    ),
 
   /* Lower
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
     [_LOWER] =  LAYOUT_ortho_4x12( \
       KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______,\
       _______, KC_LT, KC_GT, KC_LPRN, KC_RPRN, KC_GRV,       KC_MINS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_PIPE, \
-      _______, _______, _______, _______, KC_EQL, KC_QUES,   KC_UNDS, KC_PLUS, KC_COMM, KC_DOT, KC_SLSH, _______, \
-      _______, _______, _______, _______, _______, KC_TRNS,  ADJUST, KC_RGUI, _______, _______, _______, _______ \
+      KC_LALT, _______, _______, _______, KC_EQL, KC_QUES,   KC_UNDS, KC_PLUS, KC_COMM, _______, KC_SLSH, _______, \
+      _______, _______, _______, KC_TRNS, _______, KC_TRNS,  ADJUST, KC_RGUI, _______, _______, _______, _______ \
       ),
 
   /* Raise
