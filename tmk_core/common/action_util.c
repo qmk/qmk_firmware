0babacd64f9f628357208eb2f4d057b218ea57c9
Damien Merenne <damien@bloom-life.com>
2019-12-23 10:20:26 +0100

    fixup! Update tmk_core/common/action_util.c

--- tmk_core/common/action_util.c
+++ tmk_core/common/action_util.c
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "timer.h"
 #include "keycode_config.h"
 #include <quantum_keycodes.h>
+#include <quantum/quantum.h>
 
 extern keymap_config_t keymap_config;
 
