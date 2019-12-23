95ce5905f83530133134771bde62813b3b6fb644
Jeremy Soller <jeremy@system76.com>
2019-12-23 14:17:29 -0700

    Fix some more whitespace

--- keyboards/system76/virgo/keymaps/default/keymap.c
+++ keyboards/system76/virgo/keymaps/default/keymap.c
@@ -30,8 +30,8 @@ ________________________________________________________________________________
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
           KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
               KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-          KC_LCTL, MO(1),   KC_LALT, KC_LGUI,      KC_SPC,           KC_SPC,      MO(1),   KC_RALT, KC_RCTL,      KC_LEFT, KC_DOWN, KC_RIGHT
-               ),
+          KC_LCTL, MO(1),   KC_LALT, KC_LGUI,     KC_SPC,           KC_SPC,       MO(1),   KC_RALT, KC_RCTL,     KC_LEFT, KC_DOWN, KC_RIGHT
+  ),
 
     /* Layer 1, function layer
 ________________________________________________________________________________________________________________________________________
@@ -62,7 +62,7 @@ ________________________________________________________________________________
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PGUP,
-          KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,      KC_TRNS,          KC_TRNS,      KC_TRNS,   KC_TRNS, KC_TRNS,   KC_HOME, KC_PGDN, KC_END
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,     KC_HOME,  KC_PGDN, KC_END
   )
 };
 
