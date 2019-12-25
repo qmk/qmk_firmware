895468773ce6af6237859547872112227736028e
OctopusZ <928281509@qq.com>
2019-12-25 17:34:54 +0800

    remove the wkl version, update codes
    remove the wkl version,and add the WKL layout, update some old style codes.

---
+++ keyboards/pitta/sq80/config.h
@@ -0,0 +1,35 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#pragma once
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    PITTA
+#define PRODUCT         SQ68
+#define DESCRIPTION     SQ68 Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 15
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { B0, B1, B2, F6, B5, D4 }
+#define MATRIX_COL_PINS { C7, C6, B6, B3, B4, D7, D6, E6, F0, F1, F4, F5, F7, D5, D3 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* number of backlight levels */
+#define BACKLIGHT_PIN B7
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 5
+#endif
+
+#endif
