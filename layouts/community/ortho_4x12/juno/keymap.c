5cd44f0bd792b4604418ff2669426d9f8a3183d9
Juno Nguyen <juno.ngx@gmail.com>
2019-12-26 13:51:45 +0800

    Update layouts/community/ortho_4x12/juno/keymap.c
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- layouts/community/ortho_4x12/juno/keymap.c
+++ layouts/community/ortho_4x12/juno/keymap.c
@@ -240,7 +240,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 layer_state_t layer_state_set_user(layer_state_t state) {
   // LED control, lighting up when Fn layer is activated
   state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-  switch (biton32(state)) {
+  switch (get_highest_layer(state)) {
     case _QWERTY:
       backlight_set(0);
       break;
