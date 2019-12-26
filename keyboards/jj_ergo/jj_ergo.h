e31a487a56e359fc3c3801e45d9b9ac1eab9cdd7
Neil Kettle <mu-b@openwrt-build.home.local>
2019-12-26 20:06:05 +0000

    fix bug in the keymap.

--- keyboards/jj_ergo/jj_ergo.h
+++ keyboards/jj_ergo/jj_ergo.h
@@ -78,7 +78,7 @@ uint8_t init_mcp23018(void);
    {                                                            \
     { k00,   k01, k02, k03, k04, k05,   k06,      k07,   k08,   k09, k0A, k0B, k0C, k0D    }, \
     { k10,   k11, k12, k13, k14, k15,   k16,      k17,   k18,   k19, k1A, k1B, k1C, k1D    }, \
-    { k20,   k21, k22, k23, k24, k25,   KC_NO,    KC_NO, k28,   k29, k2A, k2B, k2C, k2D    }, \
+    { k20,   k21, k22, k23, k24, k25,   KC_NO,    k28,   k29, k2A, k2B, k2C, k2D, KC_NO     }, \
     { k30,   k31, k32, k33, k34, k35,   k36,      k37,   k38,   k39, k3A, k3B, k3C, k3D    }, \
     { k40,   k41, k42, k43, k44, k53, k52,   k49, k4A, k4B, k4C, k4D, k57, k58    }, \
     { KC_NO, KC_NO, k56, k51, k54, KC_NO,   k55,      KC_NO, KC_NO,   k59, k5C, k5A, KC_NO, k5B  }  \
