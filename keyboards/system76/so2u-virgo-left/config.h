b3c656fffc4ab06c25cc4bfb1d1a10766381b7b8
leviport <leviport377@gmail.com>
2019-12-27 12:18:24 -0700

    More work on split ortho 2U... ...but still not complete

--- keyboards/system76/so2u-virgo-left/config.h
+++ keyboards/system76/so2u-virgo-left/config.h
@@ -8,16 +8,16 @@
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    System76
-#define PRODUCT         Virgo
-#define DESCRIPTION     Virgo Keyboard
+#define PRODUCT         Split Ortho 2U Virgo Left
+#define DESCRIPTION     Virgo Left
 
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 7
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { A3, A2, A1, A0, D15, D14 }
-#define MATRIX_COL_PINS { D2, D3, D4, D5, D6, D7, D8 }
+#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3 }
+#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
