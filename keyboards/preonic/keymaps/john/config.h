8b11920caf07d33642808bc9e032900619578252
John Garrett <infantrygarrett@icloud.com>
2019-12-27 23:08:04 -0600

    finaly

--- keyboards/preonic/keymaps/john/config.h
+++ keyboards/preonic/keymaps/john/config.h
@@ -32,7 +32,6 @@
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
                                   SONG(COLEMAK_SOUND), \
-                                  SONG(WORKMAN_SOUND), \                    
                                   SONG(DVORAK_SOUND) \
                                 }
 #endif
