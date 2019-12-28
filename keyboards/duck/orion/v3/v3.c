5b1fc457f31b8423f800ec99275b81ef533d9441
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2019-12-28 08:23:37 -0800

    Update keyboards/duck/orion/v3/v3.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/duck/orion/v3/v3.c
+++ keyboards/duck/orion/v3/v3.c
@@ -32,7 +32,7 @@
 bool led_update_kb(led_t led_state) {
     if(led_update_user(led_state)) {
         writePin(B0, !led_state.caps_lock);
-        writePin(B4, !led_state.scroll_lock);
+        writePin(B4, !led_state.num_lock);
         writePin(D7, !led_state.scroll_lock);
     }
     return true;
