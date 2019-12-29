817765b7e6b75978e9b94a5eea1daaea2265434c
Tom Saleeba <tom.saleeba@gmail.com>
2019-12-29 12:20:59 +1030

    fix: address comment from @drashna

--- keyboards/ergoslab/config.h
+++ keyboards/ergoslab/config.h
@@ -19,15 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-// Sets good default for the speed of the mouse.
-#undef MOUSEKEY_INTERVAL
-#undef MOUSEKEY_DELAY
-#undef MOUSEKEY_TIME_TO_MAX
-#undef MOUSEKEY_MAX_SPEED
-#undef MOUSEKEY_WHEEL_MAX_SPEED
-#undef MOUSEKEY_WHEEL_TIME_TO_MAX
-#undef MOUSEKEY_WHEEL_DELAY
-
 #define MOUSEKEY_INTERVAL           16
 #define MOUSEKEY_DELAY              0
 #define MOUSEKEY_TIME_TO_MAX        40
