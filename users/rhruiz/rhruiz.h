2c525ee017677eeaed173831491845df8e47b74d
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2019-12-29 01:00:50 -0300

    add a starcraft layer

--- users/rhruiz/rhruiz.h
+++ users/rhruiz/rhruiz.h
@@ -1,6 +1,6 @@
 #pragma once
 
-enum rhruiz_layers { _BL, _VIM_EMACS, _MOUSE, _KEY_OVERRIDE, _FN1, _FN2, _CFG, _NUM };
+enum rhruiz_layers { _BL, _GAME, _VIM_EMACS, _MOUSE, _KEY_OVERRIDE, _FN1, _FN2, _CFG, _NUM };
 
 enum custom_keycodes { KC_MAKE = SAFE_RANGE, KC_EPIP, NEW_SAFE_RANGE };
 
