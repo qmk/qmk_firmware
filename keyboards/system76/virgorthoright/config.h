40a98452a8a011d8e44edc51fcc7e51b214a78b9
leviport <leviport377@gmail.com>
2019-12-30 11:55:49 -0700

    Update config for right side

--- keyboards/system76/virgorthoright/config.h
+++ keyboards/system76/virgorthoright/config.h
@@ -5,19 +5,19 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x1776
-#define PRODUCT_ID      0x1776
+#define PRODUCT_ID      0x1777
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    System76
-#define PRODUCT         Virgortholeft
-#define DESCRIPTION     Left half of 2U Split Ortho
+#define PRODUCT         Virgorthoright
+#define DESCRIPTION     Right half of 2U Split Ortho
 
 /* key matrix size */
 #define MATRIX_ROWS 6
-#define MATRIX_COLS 7
+#define MATRIX_COLS 9
 
 /* key matrix pins */
 #define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3 }
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { D3, D2, D1, D0, D4, C6, D7, E6, B4 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
