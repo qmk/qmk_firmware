ffa528c36a4295d4abd33255a4a32ce2c5e33322
Josh Hinnebusch <joshhinnebusch@gmail.com>
2020-01-01 14:14:58 -0500

    Update keyboards/hineybush/h87a/h87a.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/hineybush/h87a/h87a.c
+++ keyboards/hineybush/h87a/h87a.c
@@ -46,4 +46,6 @@ void eeconfig_init_kb(void) {  // EEPROM is getting reset!
   rgblight_enable(); // Enable RGB by default
   rgblight_sethsv(0, 255, 128);  // Set default HSV - red hue, full saturation, medium brightness
   rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL + 2); // set to RGB_RAINBOW_SWIRL by default
+  
+  eeconfig_init_user();
 }
