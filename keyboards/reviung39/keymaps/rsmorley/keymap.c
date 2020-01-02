8fa40fc2dd07aff2aff4b07564284048ba8a6737
Scott Morley <smorley@sofi.com>
2020-01-01 19:33:25 -0700

    Move tab to Left Shift. Ctrl tap is left (

--- keyboards/reviung39/keymaps/rsmorley/keymap.c
+++ keyboards/reviung39/keymaps/rsmorley/keymap.c
@@ -29,8 +29,8 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung39(
     KC_GESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_LSPO,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_RSPC,
-    CTL_T(KC_TAB),  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  ALT_T(KC_F6),
+    LSFT_T(KC_TAB),  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_RSPC,
+    KC_LCPO,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  ALT_T(KC_F6),
                                                        LT(_RAISE, KC_ENT),    RCMD_T(KC_F3),   LT(_LOWER, KC_SPC)
   ),
   
