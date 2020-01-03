fde1ef707d18bacf52830b6a4c7d7b2518927d1b
Ben Weakley <2173281+Defying@users.noreply.github.com>
2020-01-02 16:09:20 -0800

    final touches! i think we're there

--- keyboards/redscarf_i/rules.mk
+++ keyboards/redscarf_i/rules.mk
@@ -12,10 +12,10 @@ MCU = atmega32u4
 BOOTLOADER = atmel-dfu
 
 # build options
-BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration
 MOUSEKEY_ENABLE  = no   # Mouse keys(+4700)
 EXTRAKEY_ENABLE  = yes  # Audio control and System control(+450)
-CONSOLE_ENABLE   = no   # Console for debug(+400)
+CONSOLE_ENABLE   = no   # Console for debug
 COMMAND_ENABLE   = no   # Commands for debug and configuration
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
 UNICODE_ENABLE   = no   # Unicode
