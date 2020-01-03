9a68dcf827d5b9c3fb52dff85df24294bacd5492
James Young <xxiinophobia@yahoo.com>
2020-01-02 16:53:06 -0800

    rules.mk templating

--- keyboards/redscarf_i/rules.mk
+++ keyboards/redscarf_i/rules.mk
@@ -11,13 +11,23 @@ MCU = atmega32u4
 #   ATmega328P   USBasp
 BOOTLOADER = atmel-dfu
 
-# build options
-BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration
-MOUSEKEY_ENABLE  = no   # Mouse keys
-EXTRAKEY_ENABLE  = yes  # Audio control and System control
-CONSOLE_ENABLE   = no   # Console for debug
-COMMAND_ENABLE   = no   # Commands for debug and configuration
-BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-UNICODE_ENABLE   = no   # Unicode
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE  = no      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE   = no      # Mouse keys
+EXTRAKEY_ENABLE   = yes     # Audio control and System control
+CONSOLE_ENABLE    = no      # Console for debug
+COMMAND_ENABLE    = no      # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE  = no      # Breathing sleep LED during USB suspend
+BACKLIGHT_ENABLE  = yes     # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE   = no      # Enable keyboard RGB underglow
+MIDI_ENABLE       = no      # MIDI support
+BLUETOOTH_ENABLE  = no      # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE      = no      # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+HD44780_ENABLE    = no      # Enable support for HD44780 based LCDs
+UNICODE_ENABLE    = no      # Unicode
 
 LAYOUTS = ortho_5x4 ortho_6x4 numpad_5x4 numpad_6x4
