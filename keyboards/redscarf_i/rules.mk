90bb88d340de63c57280a4c5adc4b4f5aea4c314
Ben Weakley <2173281+Defying@users.noreply.github.com>
2020-01-02 16:08:41 -0800

    Update keyboards/redscarf_i/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/redscarf_i/rules.mk
+++ keyboards/redscarf_i/rules.mk
@@ -10,3 +10,5 @@ CONSOLE_ENABLE   = no   # Console for debug(+400)
 COMMAND_ENABLE   = no   # Commands for debug and configuration
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
 UNICODE_ENABLE   = no   # Unicode
+
+LAYOUTS = ortho_5x4 ortho_6x4 numpad_5x4 numpad_6x4
