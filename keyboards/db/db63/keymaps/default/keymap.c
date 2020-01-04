814635d111f2db5ad1311d8b36396a7a6c1641ff
Hung DO <hungdohuy@gmail.com>
2020-01-04 18:40:08 +0700

    editing contact

--- keyboards/db/db63/keymaps/default/keymap.c
+++ keyboards/db/db63/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Sebastian Kaim <sebastian.kaim@sebb767.de>
+Copyright 2020 Hung DO <hungdohuy@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
