4c9a2d45a897cc40396c0109272038c5dd89a274
Yan-Fa Li <yanfa.li@fireeye.com>
2020-01-03 17:11:19 -0800

    protect receive too

--- tmk_core/protocol/chibios/usb_shim.c
+++ tmk_core/protocol/chibios/usb_shim.c
@@ -7,3 +7,9 @@ void usbStartTransmitIGuard(USBDriver *usbp, usbep_t ep, const uint8_t *buf, siz
     usbStartTransmitI(usbp, ep, buf, n);
   }
 }
+
+void usbStartReceiveIGuard(USBDriver *usbp, usbep_t ep, uint8_t *buf, size_t n) {
+  if (usbp->epc[ep] != NULL) {
+      usbStartReceiveI(usbp, ep, buf, n);
+  }
+}
