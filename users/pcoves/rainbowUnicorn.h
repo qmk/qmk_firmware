730dc788ec976c283b92ee10f1a57bc4b6bd3ed5
Pablo COVES <pablo.coves@protonmail.com>
2020-01-04 17:00:12 +0100

    RainbowUnicorn refactoring

--- users/pcoves/rainbowUnicorn.h
+++ users/pcoves/rainbowUnicorn.h
@@ -2,9 +2,4 @@
 
 #include "quantum.h"
 
-bool rainbowUnicornIsEnabled(void);
-void rainbowUnicornEnable(void);
-void rainbowUnicornDisable(void);
-void rainbowUnicornToggle(void);
-
-void rainbowUnicorn(uint16_t keycode, keyrecord_t *keyrecord);
+__attribute__((weak)) bool process_record_rainbowUnicorn(uint16_t keycode, keyrecord_t* keyrecord);
