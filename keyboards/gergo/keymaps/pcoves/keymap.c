4bbe574e75661da750c319a63efb85f12721fdb1
Pablo COVES <pablo.coves@protonmail.com>
2020-01-05 12:52:34 +0100

    Move tab

--- keyboards/gergo/keymaps/pcoves/keymap.c
+++ keyboards/gergo/keymaps/pcoves/keymap.c
@@ -21,7 +21,7 @@
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_0] = LAYOUT_gergo(
-            KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,      KC_T,                                                KC_Y,      KC_U,     KC_I,      KC_O,     KC_P,     _______,
+            _______,    KC_Q,      KC_W,        KC_E,      KC_R,      KC_T,                                                KC_Y,      KC_U,     KC_I,      KC_O,     KC_P,     _______,
             _______,    CTL_A,     KC_S,        KC_D,      KC_F,      KC_G,    _______,                         _______,   KC_H,      KC_J,     KC_K,      KC_L,     CTL_SCLN, _______,
             _______,    SFT_Z,     KC_X,        KC_C,      KC_V,      KC_B,    _______,   _______,   _______,   _______,   KC_N,      KC_M,     KC_COMM,   KC_DOT,   SFT_SLSH, _______,
                                                         TD(ALT),   SFT_ENT,   TD(LEFT),   _______,   _______,   TD(RIGHT), L1_SPC,    KC_RGUI
