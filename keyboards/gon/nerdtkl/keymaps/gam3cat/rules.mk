4b3b26abb69dcc7c8298c6248cee0a892fea3acf
James Young <xxiinophobia@yahoo.com>
2020-01-04 18:12:35 -0800

    nerdtkl: cleanup rules.mk files
    nerdtkl/rules.mk has the following changes:
    
    - NKRO_ENABLE flipped from no to yes
    - BACKLIGHT_ENABLE flipped from yes to no
    
    The default keymap's rules.mk used the above settings, so using that as the keyboard-level default so that keymaps/default/rules.mk can be deleted.
    
    The gam3cat keymap's rules.mk file has been updated to more closely match the AVR template and remove the firmware size impact notes, but without any settings changes or deletions.

--- keyboards/gon/nerdtkl/keymaps/gam3cat/rules.mk
+++ keyboards/gon/nerdtkl/keymaps/gam3cat/rules.mk
@@ -1,24 +1,26 @@
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
+#   change yes to no to disable
 #
-TAP_DANCE_ENABLE = no       # Enable TapDance functionality
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no        # Mouse keys(+1500)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes           # Nkey Rollover - If this doesn't work, add this to config.h: #define FORCE_NKRO
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover - If this doesn't work, add this to config.h: #define FORCE_NKRO
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 UNICODEMAP_ENABLE = no      # Enable extended unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Uses buzzer to emulate clicky switches.  By default, uses the C6 pin, same as AUDIO_ENABLE.
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 #VARIABLE_TRACE = no         # Use this to debug changes to variable values
-API_SYSEX_ENABLE = no       # This enables using the Quantum SYSEX API to send strings(+5390)
-KEY_LOCK_ENABLE = no        # This enables key lock(+260)
+API_SYSEX_ENABLE = no       # This enables using the Quantum SYSEX API to send strings
+KEY_LOCK_ENABLE = no        # This enables key lock
 SPLIT_KEYBOARD = no         # This enables split keyboard support and includes all necessary files located at quantum/split_common
+TAP_DANCE_ENABLE = no       # Enable TapDance functionality
