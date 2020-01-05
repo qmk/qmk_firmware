b764be2a4e782c5479fd45d1f460d33740ee7fb6
Yan-Fa Li <yanfa.li@fireeye.com>
2020-01-04 16:10:00 -0800

    cleanups

--- layouts/community/65_ansi/yanfali/keymap.c
+++ layouts/community/65_ansi/yanfali/keymap.c
@@ -7,36 +7,28 @@ enum _layers {
 
 // shim wilba_tech
 #ifndef EF_DEC
-#define EF_DEC RGB_RMOD
-#define EF_INC RGB_MOD
-#define H1_DEC RGB_HUD
-#define H1_INC RGB_HUI
-#define H2_DEC RGB_HUD
-#define H2_INC RGB_HUI
-#define S1_DEC RGB_SAD
-#define S1_INC RGB_SAI
-#define S2_DEC RGB_SAD
-#define S2_INC RGB_SAI
-#define BR_DEC RGB_VAD
-#define BR_INC RGB_VAI
-#define ES_DEC KC_TRNS
-#define ES_INC KC_TRNS
+    #define EF_DEC RGB_RMOD
+    #define EF_INC RGB_MOD
+    #define H1_DEC RGB_HUD
+    #define H1_INC RGB_HUI
+    #define H2_DEC RGB_HUD
+    #define H2_INC RGB_HUI
+    #define S1_DEC RGB_SAD
+    #define S1_INC RGB_SAI
+    #define S2_DEC RGB_SAD
+    #define S2_INC RGB_SAI
+    #define BR_DEC RGB_VAD
+    #define BR_INC RGB_VAI
+    #define ES_DEC KC_TRNS
+    #define ES_INC KC_TRNS
 #endif
-
-// Zeal60-specific keys:
-//  EF_INC, EF_DEC,   // next/previous backlight effect
-//  H1_INC, H1_DEC,   // Color 1 hue increase/decrease
-//  S1_INC, S1_DEC,   // Color 1 saturation increase/decrease
-//  H2_INC, H2_DEC,   // Color 2 hue increase/decrease
-//  S2_INC, S2_DEC,   // Color 2 saturation increase/decrease
-//  BR_INC, BR_DEC,   // backlight brightness increase/decrease
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BS] = LAYOUT_65_ansi( /* Base */
-    KC_GRV, KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,\
-    KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,\
-    LCTL_T(KC_ESC), KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,\
-    KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END, \
+    KC_GRV,          KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,\
+    KC_TAB,          KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,\
+    LCTL_T(KC_ESC),  KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,\
+    KC_LSFT,         KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END, \
     KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC,                          KC_RGUI, MO(FN),   KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT),
 
 [FN] = LAYOUT_65_ansi( /* FN */
