b839e463adad5a27574d14141d4b883fdf1fe0ac
Pablo COVES <pablo.coves@protonmail.com>
2020-01-05 19:30:29 +0100

    Combos update

--- users/pcoves/combo.c
+++ users/pcoves/combo.c
@@ -8,20 +8,22 @@ enum {
     BSP,
     DEL,
 
+    TAB,
     BSL,
 
     CUT,
     GRA,
 };
 
-const uint16_t PROGMEM min[] = {KC_V, KC_B, COMBO_END};
-const uint16_t PROGMEM eql[] = {KC_N, KC_M, COMBO_END};
+const uint16_t PROGMEM min[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM eql[] = {KC_M, KC_COMM, COMBO_END};
 
 const uint16_t PROGMEM esc[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM bsp[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM del[] = {KC_DOWN, KC_UP, COMBO_END};
 
-const uint16_t PROGMEM bsl[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM tab[] = {KC_S, KC_F, COMBO_END};
+const uint16_t PROGMEM bsl[] = {KC_J, KC_L, COMBO_END};
 
 const uint16_t PROGMEM cut[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM gra[] = {KC_S, KC_D, COMBO_END};
@@ -34,6 +36,7 @@ combo_t key_combos[COMBO_COUNT] = {
     [BSP] = COMBO(bsp, KC_BSPC),
     [DEL] = COMBO(del, KC_DEL),
 
+    [TAB] = COMBO(tab, KC_TAB),
     [BSL] = COMBO(bsl, KC_BSLS),
 
     [CUT] = COMBO(cut, KC_QUOT),
