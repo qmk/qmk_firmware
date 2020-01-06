dc8f23e8fa87c21c8e5413234d828c77146c5052
Rossman360 <rmontsinger@gmail.com>
2020-01-06 00:18:41 -0500

    tragicforce working with userspace

--- keyboards/keebio/tragicforce68/keymaps/rossman360/keymap.c
+++ keyboards/keebio/tragicforce68/keymaps/rossman360/keymap.c
@@ -1,19 +1,9 @@
 #include QMK_KEYBOARD_H
+#include "rossman360.h"
 
 #define PGMOD LT(_MOD, KC_PGDN)
 #define SPCMOD LT(_MOD, KC_SPACE)
 #define VOLMOD LT(_MOD, KC__MUTE)
-#define CMDBSP MT(MOD_LGUI, KC_BSPC)
-#define ALTDEL MT(MOD_LALT, KC_DEL)
-#define BWORD LCTL(KC_BSPC)
-#define LWORD LCTL(KC_LEFT)
-#define RWORD LCTL(KC_RIGHT)
-#define JUMPBACK LSFT(KC_TAB)
-#define UNDO LCTL(KC_Z)
-#define XPANDR LCTL(LSFT(KC_X))
-#define NTAB LCTL(KC_T)
-#define CTAB LCTL(KC_W)
-#define XPANDR LCTL(LSFT(KC_X))
 
 enum layer_names {
   _BASE,
@@ -22,66 +12,8 @@ enum layer_names {
   _DEL,
 };
 
-enum custom_keycodes {
-  LAUNCHCLOCK = SAFE_RANGE,
-  CSPEAK,
-  SPEAK1,
-  SPEAK2,
-  SPEAK3,
-  SPEAK4,
-  PARADOWN,
-  WREFRESH,
-  PMERGE,
-  REMCAPS,
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
 switch (keycode) {
-	case CSPEAK:
-		if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_PGDOWN) SS_TAP(X_ENTER) SS_TAP(X_ENTER) SS_TAP(X_PGDOWN));
-	break;
-	}
-	case SPEAK1:
-		if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_PGDOWN) SS_TAP(X_ENTER) SS_TAP(X_ENTER) SS_TAP(X_PGDOWN) SS_LCTRL(SS_TAP(X_1)));
-	break;
-	}
-	case SPEAK2:
-		if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_PGDOWN) SS_TAP(X_ENTER) SS_TAP(X_ENTER) SS_TAP(X_PGDOWN) SS_LCTRL(SS_TAP(X_2)));
-	}
-	break;
-	case SPEAK3:
-		if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_PGDOWN) SS_TAP(X_ENTER) SS_TAP(X_ENTER) SS_TAP(X_PGDOWN) SS_LCTRL(SS_TAP(X_3)));
-	}
-	break;
-	case SPEAK4:
-		if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_PGDOWN) SS_TAP(X_ENTER) SS_TAP(X_ENTER) SS_TAP(X_PGDOWN) SS_LCTRL(SS_TAP(X_4)));
-	}
-	break;
-	case PARADOWN:
-		if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_PGDOWN) SS_TAP(X_ENTER) SS_TAP(X_PGDOWN));
-    }
-    break;
-    case PMERGE:
-    if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_HOME) SS_TAP(X_BSPACE) SS_TAP(X_SPACE) SS_LCTRL(SS_TAP(X_BSPACE)) SS_TAP(X_SPACE));
-    }
-    break;
-    case WREFRESH:
-    if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_SPACE) SS_TAP(X_BSPACE));
-    }
-    break;
-    case REMCAPS:
-    if (record->event.pressed) {
-		SEND_STRING(SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_LCTRL(SS_TAP(X_LEFT)) SS_TAP(X_DELETE));
-    }
-    break;
  };  
  return true; 
 };
