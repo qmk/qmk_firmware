5c7fcac1c9acbdaaad78718659498ca02225f632
Pablo COVES <pablo.coves@anatoscope.com>
2020-01-06 10:27:48 +0100

    Fix double write

--- users/pcoves/unicode.c
+++ users/pcoves/unicode.c
@@ -4,16 +4,16 @@
 bool process_record_unicode(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case EMOTE0:
-            send_unicode_hex_string("0028 256f 00B0 25A1 00B0 FF09 256F FE35 253B 2501 253B");
+            if (record->event.pressed) send_unicode_hex_string("0028 256f 00B0 25A1 00B0 FF09 256F FE35 253B 2501 253B");
             return false;
         case EMOTE1:
-            send_unicode_hex_string("0028 30D8 FF65 005F FF65 0029 30D8 2533 2501 2533");
+            if (record->event.pressed) send_unicode_hex_string("0028 30D8 FF65 005F FF65 0029 30D8 2533 2501 2533");
             return false;
         case EMOTE2:
-            send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
+            if (record->event.pressed) send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
             return false;
         case EMOTE3:
-            send_unicode_hex_string("0CA0 005F 0CA0");
+            if (record->event.pressed) send_unicode_hex_string("0CA0 005F 0CA0");
             return false;
     }
     return true;
