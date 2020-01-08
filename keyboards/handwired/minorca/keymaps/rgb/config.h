4edfe1656eee66c9bfcccd0cf983be73b9da1e94
James Young <xxiinophobia@yahoo.com>
2020-01-08 11:58:41 -0800

    update keymaps/rgb/config.h
    - use #pragma once include guard
    - remove redundant config.h include

--- keyboards/handwired/minorca/keymaps/rgb/config.h
+++ keyboards/handwired/minorca/keymaps/rgb/config.h
@@ -1,7 +1,4 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "../../config.h"
+#pragma once
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D5
@@ -10,5 +7,3 @@
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-
-#endif
