3d6bce3069de2df7205f9264e358a1619d922433
James Young <xxiinophobia@yahoo.com>
2020-01-08 11:55:30 -0800

    elevate Bootmagic configurations to keyboard level

--- keyboards/handwired/minorca/keymaps/rgb/config.h
+++ keyboards/handwired/minorca/keymaps/rgb/config.h
@@ -3,12 +3,6 @@
 
 #include "../../config.h"
 
-/* bootmagic salt key */
-#define BOOTMAGIC_KEY_SALT              KC_ESC
-
-/* skip bootmagic and eeconfig */
-#define BOOTMAGIC_KEY_SKIP              KC_SPACE
-
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D5
 #define RGBLIGHT_ANIMATIONS
