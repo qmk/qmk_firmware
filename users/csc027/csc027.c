3b704479a4449e85dab0ac1d84d156de4593e393
Constantine Chen <csc_dev@protonmail.com>
2020-01-08 22:20:06 -0800

    Updated comments to explain that for non-AVR MCUs, the PROGMEM macro doesn't do anything.

--- users/csc027/csc027.c
+++ users/csc027/csc027.c
@@ -30,17 +30,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case (MC_first + 1)...(MC_last - 1):
             if(record->event.pressed) {
+                send_string_P(
 #if defined(__AVR__)
-                // The accessor here first reads from the pointer array that is located
-                // in PROGMEM.  The pointer is taken and passed to the send_string_P
-                // function, which is aware of the difference between RAM and PROGMEM
-                // pointers.
-                send_string_P((char*)pgm_read_word(&git_macros[keycode - MC_first - 1]));
+                    // The accessor here first reads from the pointer array that is located
+                    // in PROGMEM.  The pointer is taken and passed to the send_string_P
+                    // function, which is aware of the difference between RAM and PROGMEM
+                    // pointers.
+                    (char*)pgm_read_word(&git_macros[keycode - MC_first - 1])
 #else
-                // For some reason, compiling using ChibiOS for STM boards works without explicitly
-                // grabbing the PROGMEM pointer.  This is a hack until a better solution is found.
-                send_string_P(git_macros[keycode - MC_first - 1]);
+                    // For non-AVR MCUs, the PROGMEM macro is defined as nothing.  So, the strings are
+                    // declared in RAM instead of flash.  The send_string_P uses a different definition
+                    // of pgm_read_byte internally, which uses RAM pointers instead.  This is why the
+                    // raw pointer should be passed for non-AVR MCUs.
+                    git_macros[keycode - MC_first - 1]
 #endif
+                );
                 return true;
             }
             return false;
