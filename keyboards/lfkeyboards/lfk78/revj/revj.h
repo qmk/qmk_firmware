4d23c8e6351aa05e9c98209d3c6aafe33f6d086a
fauxpark <fauxpark@gmail.com>
2020-01-10 10:04:10 +1100

    Split info.json into revision folders

--- keyboards/lfkeyboards/lfk78/revj/revj.h
+++ keyboards/lfkeyboards/lfk78/revj/revj.h
@@ -64,7 +64,7 @@
 #define LAYOUT_iso( \
     k11, k12,   k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, k1F, k1G,   k1H, k1I, \
     k21, k22,   k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D, k2E, k2F, k2G,   k2H, k2I, \
-    k31, k32,   k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k3E, k3F,     \
+    k31, k32,   k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k3E, k3F, \
     k41, k42,   k43, k44, k45, k46, k47, k48, k49, k4A, k4B, k4C, k4D, k4E,      k4F,   k4H, \
     k51, k52,   k53, k54, k55,              k59,                  k5D, k5E, k5F,   k5G, k5H, k5I \
 ) { \
