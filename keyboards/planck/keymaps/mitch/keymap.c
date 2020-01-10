a6f6d4cc8a011ccdb0195d14eb3ecd097fd1c0f3
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-01-09 22:45:22 -0800

    [Keymap] Refactor Planck keymaps to use Layout Macros (#7172)
    * refactor ab keymap
    
    * refactor alexey keymap
    
    * refactor altgr keymap
    
    * refactor andylikescandy keymap
    
    * refactor aviator keymap
    
    * refactor basic keymap
    
    * refactor bone2planck keymap
    
    * refactor brandon keymap
    
    * refactor cbbrowne keymap
    
    * refactor chance keymap
    
    * refactor circuit keymap
    
    * refactor coloneljesus keymap
    
    * refactor corvec keymap
    
    * refactor danielhklein keymap
    
    * refactor dbroqua keymap
    
    * refactor dc keymap
    
    * refactor dcompact keymap
    
    * refactor dlaroe keymap
    
    * refactor dodger keymap
    
    * refactor dr0ck keymap
    
    * refactor dudeofawesome keymap
    
    * refactor emilyh keymap
    
    * refactor espynn keymap
    
    * refactor experimental keymap
    
    * refactor gabriel keymap
    
    * refactor grahampheath keymap
    
    * refactor gunp keymap
    
    * refactor handwired_binaryplease keymap
    
    * refactor hiea keymap
    
    * refactor hieax keymap
    
    * refactor impossible keymap
    
    * refactor jacob keymap
    
    * refactor jeebak keymap
    
    * refactor jeremy-dev keymap
    
    * refactor jhenahan keymap
    
    * refactor jirgn keymap
    
    * refactor johannes keymap
    
    * refactor kelorean keymap
    
    * refactor khord keymap
    
    * refactor kloki keymap
    
    * refactor kmontag42 keymap
    
    * refactor lae3 keymap
    
    * refactor lukas keymap
    
    * refactor luke keymap
    
    * refactor mason keymap
    
    * refactor matrixman keymap
    
    * refactor mjt keymap
    
    * refactor mjtnumsym keymap
    
    * refactor mollat keymap
    
    * refactor myoung34 keymap
    
    * refactor neo2planck keymap
    
    * refactor nico keymap
    
    * refactor not-quite-neo keymap
    
    * refactor originerd keymap
    
    * refactor paget keymap
    
    * refactor palleiko keymap
    
    * refactor pete keymap
    
    * refactor phreed keymap
    
    * refactor piemod keymap
    
    * refactor pok3r keymap
    
    * refactor premek keymap
    
    * refactor pvc keymap
    
    * refactor rai-suta keymap
    
    * refactor rodhaene keymap
    
    * refactor sdothum keymap
    
    * refactor sean keymap
    
    * refactor sgoodwin keymap
    
    * refactor smt keymap
    
    * refactor steno keymap
    
    * refactor tak3over keymap
    
    * refactor tehwalris keymap
    
    * refactor that_canadian keymap
    
    * refactor thermal_printer keymap
    
    * refactor unicode keymap
    
    * refactor yale keymap
    
    * refactor yang keymap
    
    * refactor zach keymap
    
    * refactor zrichard keymap
    
    * refactor angerthosenear keymap
    
    * refactor austin keymap
    
    * refactor charlie keymap
    
    * refactor daniel keymap
    
    * refactor david keymap
    
    * refactor dzobert keymap
    
    * refactor joe keymap
    
    * refactor kyle keymap
    
    * refactor leo keymap
    
    * refactor lucas keymap
    
    * refactor max keymap
    
    * refactor mitch keymap
    
    * refactor samuel keymap
    
    * refactor tong92 keymap
    
    * Add Changelog entry
    
    * move changelog (new breaking changes target date)

--- keyboards/planck/keymaps/mitch/keymap.c
+++ keyboards/planck/keymaps/mitch/keymap.c
@@ -1,5 +1,4 @@
-#include "keymap.h"
-#include "quantum.h"
+#include QMK_KEYBOARD_H
 
 #define QWERTY 0
 #define LOWER 1
@@ -21,12 +20,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Esc  | Del  | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT) },
-  {KC_ESC,  KC_DEL,  KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
-},
+[QWERTY] = LAYOUT_planck_grid(
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT) ,
+  KC_ESC,  KC_DEL,  KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -39,12 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |    Enter    |      |      | PgUp | PgUn |      |
  * `-----------------------------------------------------------------------------------'
  */
-[LOWER] = {
-  {GUI_GRV, _______, _______, _______, _______, _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
-  {KC_CAPS, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, KC_PIPE, KC_PIPE},
-  {_______, _______, _______, _______, _______, _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_BSLS, KC_ENT},
-  {_______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, KC_PGDN, KC_PGUP, _______}
-},
+[LOWER] = LAYOUT_planck_grid(
+  GUI_GRV, _______, _______, _______, _______, _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+  KC_CAPS, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, KC_PIPE, KC_PIPE,
+  _______, _______, _______, _______, _______, _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_BSLS, KC_ENT,
+  _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, KC_PGDN, KC_PGUP, _______
+),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -58,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 
-[RAISE] = {
-  {KC_GRV,  KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______},
-  {_______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, _______, KC_4,    KC_5,    KC_6,    _______, KC_BSLS},
-  {_______, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, _______, _______, KC_1,    KC_2,    KC_3,    _______, KC_ENT},
-  {_______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, KC_PGDN, KC_PGUP, _______}
-}
+[RAISE] = LAYOUT_planck_grid(
+  KC_GRV,  KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______,
+  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, _______, KC_4,    KC_5,    KC_6,    _______, KC_BSLS,
+  _______, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, _______, _______, KC_1,    KC_2,    KC_3,    _______, KC_ENT,
+  _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, KC_PGDN, KC_PGUP, _______
+)
 };
