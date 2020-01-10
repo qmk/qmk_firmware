5e06b0bd999c2b9d9ec88596789ec4e69f9b1556
ehz <ptg0317@gmail.com>
2020-01-10 13:49:40 +0900

    test scroll, push/lower

--- keyboards/preonic/keymaps/ehztot/keymap.c
+++ keyboards/preonic/keymaps/ehztot/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | '    |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ESC  | ESC  | GUI  | ALT  |    SPACE    |   bksp/Rais | Left | Down |  Up  |Right |
+ * | LOWER| ESC  | GUI  | ALT  |    SPACE    |   bksp/Rais | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,  \
-  KC_ESC, KC_ESC, KC_LGUI, KC_LALT, KC_SPC,   KC_SPC,  KC_BSPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  LOWER, KC_ESC, KC_LGUI, KC_LALT, KC_SPC,   KC_SPC,  KC_BSPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Lower
@@ -192,11 +192,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     }
   } else {
     if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
+      tap_code(KC_MS_WH_DOWN);
     } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
+      tap_code(KC_MS_WH_UP);
     }
   }
 }
--- keyboards/preonic/keymaps/ehztot/rules.mk
+++ keyboards/preonic/keymaps/ehztot/rules.mk
@@ -1,1 +1,2 @@
 SRC += muse.c
+ENCODER_ENABLE = yes 
