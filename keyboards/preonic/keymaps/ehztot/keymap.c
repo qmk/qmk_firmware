58ad38a0b74d5987d10b419afbacd91b6b31c6ed
ehz <ptg0317@gmail.com>
2020-01-11 01:31:10 +0900

    remake the path of remote

--- keyboards/preonic/keymaps/ehztot/keymap.c
+++ keyboards/preonic/keymaps/ehztot/keymap.c
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPS, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,  \
-  LOWER, KC_ESC, KC_LGUI, KC_LALT, KC_SPC,   KC_SPC,  KC_BSPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  LOWER, KC_ESC, KC_LGUI, KC_LALT, KC_SPC,   KC_SPC,  KC_DEL,  KC_DEL,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Lower
@@ -192,9 +192,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     }
   } else {
     if (clockwise) {
-      tap_code(KC_MS_WH_DOWN);
-    } else {
       tap_code(KC_MS_WH_UP);
+    } else {
+      tap_code(KC_MS_WH_DOWN);
     }
   }
 }
