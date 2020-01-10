21940f126d10f7d34eeec928b47d59350399619a
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-01-09 22:45:22 -0800

    [Keymap] Refactor Planck keymaps to use Layout Macros (#7172)
    * refactor ab keymap
    
    * refactor alexey keymap
    
    * refactor altgr keymap
    
    * refactor andylikescandy keymap
    
    * refactor aviator keymap
    
    * refactor basic keymap
    
    * refactor bone2planck keymap
    
    * refactor brandon keymap
    
    * refactor cbbrowne keymap
    
    * refactor chance keymap
    
    * refactor circuit keymap
    
    * refactor coloneljesus keymap
    
    * refactor corvec keymap
    
    * refactor danielhklein keymap
    
    * refactor dbroqua keymap
    
    * refactor dc keymap
    
    * refactor dcompact keymap
    
    * refactor dlaroe keymap
    
    * refactor dodger keymap
    
    * refactor dr0ck keymap
    
    * refactor dudeofawesome keymap
    
    * refactor emilyh keymap
    
    * refactor espynn keymap
    
    * refactor experimental keymap
    
    * refactor gabriel keymap
    
    * refactor grahampheath keymap
    
    * refactor gunp keymap
    
    * refactor handwired_binaryplease keymap
    
    * refactor hiea keymap
    
    * refactor hieax keymap
    
    * refactor impossible keymap
    
    * refactor jacob keymap
    
    * refactor jeebak keymap
    
    * refactor jeremy-dev keymap
    
    * refactor jhenahan keymap
    
    * refactor jirgn keymap
    
    * refactor johannes keymap
    
    * refactor kelorean keymap
    
    * refactor khord keymap
    
    * refactor kloki keymap
    
    * refactor kmontag42 keymap
    
    * refactor lae3 keymap
    
    * refactor lukas keymap
    
    * refactor luke keymap
    
    * refactor mason keymap
    
    * refactor matrixman keymap
    
    * refactor mjt keymap
    
    * refactor mjtnumsym keymap
    
    * refactor mollat keymap
    
    * refactor myoung34 keymap
    
    * refactor neo2planck keymap
    
    * refactor nico keymap
    
    * refactor not-quite-neo keymap
    
    * refactor originerd keymap
    
    * refactor paget keymap
    
    * refactor palleiko keymap
    
    * refactor pete keymap
    
    * refactor phreed keymap
    
    * refactor piemod keymap
    
    * refactor pok3r keymap
    
    * refactor premek keymap
    
    * refactor pvc keymap
    
    * refactor rai-suta keymap
    
    * refactor rodhaene keymap
    
    * refactor sdothum keymap
    
    * refactor sean keymap
    
    * refactor sgoodwin keymap
    
    * refactor smt keymap
    
    * refactor steno keymap
    
    * refactor tak3over keymap
    
    * refactor tehwalris keymap
    
    * refactor that_canadian keymap
    
    * refactor thermal_printer keymap
    
    * refactor unicode keymap
    
    * refactor yale keymap
    
    * refactor yang keymap
    
    * refactor zach keymap
    
    * refactor zrichard keymap
    
    * refactor angerthosenear keymap
    
    * refactor austin keymap
    
    * refactor charlie keymap
    
    * refactor daniel keymap
    
    * refactor david keymap
    
    * refactor dzobert keymap
    
    * refactor joe keymap
    
    * refactor kyle keymap
    
    * refactor leo keymap
    
    * refactor lucas keymap
    
    * refactor max keymap
    
    * refactor mitch keymap
    
    * refactor samuel keymap
    
    * refactor tong92 keymap
    
    * Add Changelog entry
    
    * move changelog (new breaking changes target date)

--- keyboards/planck/keymaps/altgr/colemak.h
+++ keyboards/planck/keymaps/altgr/colemak.h
@@ -13,19 +13,19 @@
   // | Ctrl |  GUI |  Alt |  Esc | Space|  Tab | Bksp |  Ent | Left | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_BASE] = {
-    {KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    CNTR_TL, CNTR_TR, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN},
-    {KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    CNTR_HL, CNTR_HR, KC_M,    KC_N,    KC_E,    KC_I,    KC_O   },
-    {KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    CNTR_BL, CNTR_BR, KC_K,    KC_H,    KC_COMM, KC_DOT,  TD_QUOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_BASE] = LAYOUT_planck_grid(
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    CNTR_TL, CNTR_TR, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    CNTR_HL, CNTR_HR, KC_M,    KC_N,    KC_E,    KC_I,    KC_O   ,
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    CNTR_BL, CNTR_BR, KC_K,    KC_H,    KC_COMM, KC_DOT,  TD_QUOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT
+  ),
 
-  [_SHIFT] = {
-    {S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), KC_SCLN},
-    {S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), CNTR_HL, CNTR_HR, S(KC_M), S(KC_N), S(KC_E), S(KC_I), S(KC_O)},
-    {S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), CNTR_BL, CNTR_BR, S(KC_K), S(KC_H), KC_COMM, KC_DOT,  TD_QUOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_SHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), KC_SCLN,
+    S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), CNTR_HL, CNTR_HR, S(KC_M), S(KC_N), S(KC_E), S(KC_I), S(KC_O),
+    S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), CNTR_BL, CNTR_BR, S(KC_K), S(KC_H), KC_COMM, KC_DOT,  TD_QUOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT
+  ),
 
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   W  |   F  |   P  |   B  | ^Alt | ^GUI |   J  |   L  |   U  |   Y  |   :  |
@@ -37,12 +37,12 @@
   // | Ctrl |  GUI |  Alt |  Esc |  f() |  Tab |  Del |   -  | Left | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_LSHIFT] = {
-    {S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN},
-    {S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), CNTR_HL, CNTR_HR, S(KC_M), S(KC_N), S(KC_E), S(KC_I), S(KC_O)},
-    {S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), CNTR_BL, CNTR_BR, S(KC_K), S(KC_H), KC_SLSH, KC_QUES, TD_DQOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  ___fn__, LT_TAB,  KC_DEL,  KC_MINS, SL_LEFT, S_DOWN,  S_UP,    S_RGHT },
-  },
+  [_LSHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN,
+    S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), CNTR_HL, CNTR_HR, S(KC_M), S(KC_N), S(KC_E), S(KC_I), S(KC_O),
+    S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), CNTR_BL, CNTR_BR, S(KC_K), S(KC_H), KC_SLSH, KC_QUES, TD_DQOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  ___fn__, LT_TAB,  KC_DEL,  KC_MINS, SL_LEFT, S_DOWN,  S_UP,    S_RGHT
+  ),
 
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   W  |   F  |   P  |   B  | ^Alt | ^GUI |   J  |   L  |   U  |   Y  |   :  |
@@ -54,12 +54,12 @@
   // | Ctrl |  GUI |  Alt | Caps |   _  | ↑Tab | Bksp |  f() | Left | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_RSHIFT] = {
-    {S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN},
-    {S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), CNTR_HL, CNTR_HR, S(KC_M), S(KC_N), S(KC_E), S(KC_I), S(KC_O)},
-    {S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), CNTR_BL, CNTR_BR, S(KC_K), S(KC_H), TD_TILD, TD_GRV,  TD_DQOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  KC_CAPS, KC_UNDS, SL_TAB,  LT_BSPC, ___fn__, SL_LEFT, S_DOWN,  S_UP,    S_RGHT },
-  },
+  [_RSHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN,
+    S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), CNTR_HL, CNTR_HR, S(KC_M), S(KC_N), S(KC_E), S(KC_I), S(KC_O),
+    S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), CNTR_BL, CNTR_BR, S(KC_K), S(KC_H), TD_TILD, TD_GRV,  TD_DQOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  KC_CAPS, KC_UNDS, SL_TAB,  LT_BSPC, ___fn__, SL_LEFT, S_DOWN,  S_UP,    S_RGHT
+  ),
 #endif
 
 // ..................................................................... ColemaX
@@ -74,19 +74,19 @@
   // | Ctrl |  GUI |  Alt |  Esc | Space|  Tab | Bksp |  Ent | Left | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_BASE] = {
-    {KC_Q,    KC_W,    KC_C,    KC_G,    KC_Z,    CNTR_TL, CNTR_TR, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN},
-    {KC_A,    KC_R,    KC_S,    KC_T,    KC_B,    CNTR_HL, CNTR_HR, KC_K,    KC_N,    KC_E,    KC_I,    KC_O   },
-    {KC_X,    KC_V,    KC_F,    KC_D,    KC_P,    CNTR_BL, CNTR_BR, KC_M,    KC_H,    KC_COMM, KC_DOT,  TD_QUOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_BASE] = LAYOUT_planck_grid(
+    KC_Q,    KC_W,    KC_C,    KC_G,    KC_Z,    CNTR_TL, CNTR_TR, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+    KC_A,    KC_R,    KC_S,    KC_T,    KC_B,    CNTR_HL, CNTR_HR, KC_K,    KC_N,    KC_E,    KC_I,    KC_O   ,
+    KC_X,    KC_V,    KC_F,    KC_D,    KC_P,    CNTR_BL, CNTR_BR, KC_M,    KC_H,    KC_COMM, KC_DOT,  TD_QUOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT
+  ),
 
-  [_SHIFT] = {
-    {S(KC_Q), S(KC_W), S(KC_C), S(KC_G), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), KC_SCLN},
-    {S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_B), CNTR_HL, CNTR_HR, S(KC_K), S(KC_N), S(KC_E), S(KC_I), S(KC_O)},
-    {S(KC_X), S(KC_V), S(KC_F), S(KC_D), S(KC_P), CNTR_BL, CNTR_BR, S(KC_M), S(KC_H), KC_COMM, KC_DOT,  TD_QUOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_SHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_W), S(KC_C), S(KC_G), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), KC_SCLN,
+    S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_B), CNTR_HL, CNTR_HR, S(KC_K), S(KC_N), S(KC_E), S(KC_I), S(KC_O),
+    S(KC_X), S(KC_V), S(KC_F), S(KC_D), S(KC_P), CNTR_BL, CNTR_BR, S(KC_M), S(KC_H), KC_COMM, KC_DOT,  TD_QUOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_TAB,  LT_BSPC, TD_ENT,  LT_LEFT, AT_DOWN, GT_UP,   CT_RGHT
+  ),
 
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   W  |   C  |   G  |   Z  | ^Alt | ^GUI |   J  |   L  |   U  |   Y  |   :  |
@@ -98,12 +98,12 @@
   // | Ctrl |  GUI |  Alt |  Esc |  f() |  Tab |  Del |   -  | Left | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_LSHIFT] = {
-    {S(KC_Q), S(KC_W), S(KC_C), S(KC_G), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN},
-    {S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_B), CNTR_HL, CNTR_HR, S(KC_K), S(KC_N), S(KC_E), S(KC_I), S(KC_O)},
-    {S(KC_X), S(KC_V), S(KC_F), S(KC_D), S(KC_P), CNTR_BL, CNTR_BR, S(KC_M), S(KC_H), KC_SLSH, KC_QUES, TD_DQOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  ___fn__, LT_TAB,  KC_DEL,  KC_MINS, SL_LEFT, S_DOWN,  S_UP,    S_RGHT },
-  },
+  [_LSHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_W), S(KC_C), S(KC_G), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN,
+    S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_B), CNTR_HL, CNTR_HR, S(KC_K), S(KC_N), S(KC_E), S(KC_I), S(KC_O),
+    S(KC_X), S(KC_V), S(KC_F), S(KC_D), S(KC_P), CNTR_BL, CNTR_BR, S(KC_M), S(KC_H), KC_SLSH, KC_QUES, TD_DQOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  ___fn__, LT_TAB,  KC_DEL,  KC_MINS, SL_LEFT, S_DOWN,  S_UP,    S_RGHT
+  ),
 
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   W  |   C  |   G  |   Z  | ^Alt | ^GUI |   J  |   L  |   U  |   Y  |   :  |
@@ -115,10 +115,10 @@
   // | Ctrl |  GUI |  Alt | Caps |   _  | ↑Tab | Bksp |  f() | Left | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_RSHIFT] = {
-    {S(KC_Q), S(KC_W), S(KC_C), S(KC_G), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN},
-    {S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_B), CNTR_HL, CNTR_HR, S(KC_K), S(KC_N), S(KC_E), S(KC_I), S(KC_O)},
-    {S(KC_X), S(KC_V), S(KC_F), S(KC_D), S(KC_P), CNTR_BL, CNTR_BR, S(KC_M), S(KC_H), TD_TILD, TD_GRV,  TD_DQOT},
-    {OS_CTL,  OS_GUI,  OS_ALT,  KC_CAPS, KC_UNDS, SL_TAB,  LT_BSPC, ___fn__, SL_LEFT, S_DOWN,  S_UP,    S_RGHT },
-  },
+  [_RSHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_W), S(KC_C), S(KC_G), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_J), S(KC_L), S(KC_U), S(KC_Y), TD_COLN,
+    S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_B), CNTR_HL, CNTR_HR, S(KC_K), S(KC_N), S(KC_E), S(KC_I), S(KC_O),
+    S(KC_X), S(KC_V), S(KC_F), S(KC_D), S(KC_P), CNTR_BL, CNTR_BR, S(KC_M), S(KC_H), TD_TILD, TD_GRV,  TD_DQOT,
+    OS_CTL,  OS_GUI,  OS_ALT,  KC_CAPS, KC_UNDS, SL_TAB,  LT_BSPC, ___fn__, SL_LEFT, S_DOWN,  S_UP,    S_RGHT
+  ),
 #endif
--- keyboards/planck/keymaps/hieax/hiea.h
+++ keyboards/planck/keymaps/hieax/hiea.h
@@ -13,19 +13,19 @@
   // | Ctrl |  GUI |  Alt |  Esc | Space|  Ins | Left |  Ent | Bksp | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_BASE] = {
-    {KC_Q,    KC_Y,    KC_O,    KC_U,    KC_Z,    CNTR_TL, CNTR_TR, KC_G,    KC_D,    KC_N,    KC_M,    KC_X   },
-    {HOME_H,  HOME_I,  HOME_E,  HOME_A,  KC_DOT,  CNTR_HL, CNTR_HR, KC_C,    HOME_T,  HOME_R,  HOME_S,  HOME_W },
-    {KC_J,    KC_SCLN, TD_QUOT, KC_K,    KC_COMM, CNTR_BL, CNTR_BR, KC_B,    KC_P,    KC_L,    KC_F,    KC_V   },
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_INS,  SP_LEFT, TD_ENT,  SP_BSPC, AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_BASE] = LAYOUT_planck_grid(
+    KC_Q,    KC_Y,    KC_O,    KC_U,    KC_Z,    CNTR_TL, CNTR_TR, KC_G,    KC_D,    KC_N,    KC_M,    KC_X   ,
+    HOME_H,  HOME_I,  HOME_E,  HOME_A,  KC_DOT,  CNTR_HL, CNTR_HR, KC_C,    HOME_T,  HOME_R,  HOME_S,  HOME_W ,
+    KC_J,    KC_SCLN, TD_QUOT, KC_K,    KC_COMM, CNTR_BL, CNTR_BR, KC_B,    KC_P,    KC_L,    KC_F,    KC_V   ,
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_INS,  SP_LEFT, TD_ENT,  SP_BSPC, AT_DOWN, GT_UP,   CT_RGHT
+  ),
 
-  [_SHIFT] = {
-    {S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X)},
-    {S(KC_H), S(KC_I), S(KC_E), S(KC_A), KC_DOT,  CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W)},
-    {S(KC_J), KC_SCLN, TD_QUOT, S(KC_K), KC_COMM, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V)},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_INS,  SP_LEFT, TD_ENT,  SP_BSPC, AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_SHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X),
+    S(KC_H), S(KC_I), S(KC_E), S(KC_A), KC_DOT,  CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W),
+    S(KC_J), KC_SCLN, TD_QUOT, S(KC_K), KC_COMM, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V),
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_INS,  SP_LEFT, TD_ENT,  SP_BSPC, AT_DOWN, GT_UP,   CT_RGHT
+  ),
 
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   Y  |   O  |   U  |   Z  | Shift| Caps |   G  |   D  |   N  |   M  |   X  |
@@ -37,12 +37,12 @@
   // | Ctrl |  GUI |  Alt | ↑Tab |  f() |  Ins | Left |   _  |  Del | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_LSHIFT] = {
-    {S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X)},
-    {S(KC_H), S(KC_I), S(KC_E), S(KC_A), KC_QUES, CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W)},
-    {S(KC_J), TD_COLN, TD_DQOT, S(KC_K), KC_SLSH, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V)},
-    {OS_CTL,  OS_GUI,  OS_ALT,  S_TAB,   ___fn__, LT_INS,  S_LEFT,  KC_UNDS, SP_DEL,  S_DOWN,  S_UP,    S_RGHT },
-  },
+  [_LSHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X),
+    S(KC_H), S(KC_I), S(KC_E), S(KC_A), KC_QUES, CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W),
+    S(KC_J), TD_COLN, TD_DQOT, S(KC_K), KC_SLSH, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V),
+    OS_CTL,  OS_GUI,  OS_ALT,  S_TAB,   ___fn__, LT_INS,  S_LEFT,  KC_UNDS, SP_DEL,  S_DOWN,  S_UP,    S_RGHT
+  ),
 
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   Y  |   O  |   U  |   Z  | Shift| Caps |   G  |   D  |   N  |   M  |   X  |
@@ -54,12 +54,12 @@
   // | Ctrl |  GUI |  Alt |  Tab |   -  |  Ins | Left |  f() | Bksp | Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_RSHIFT] = {
-    {S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X)},
-    {S(KC_H), S(KC_I), S(KC_E), S(KC_A), KC_QUES, CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W)},
-    {S(KC_J), TD_COLN, TD_DQOT, S(KC_K), KC_SLSH, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V)},
-    {OS_CTL,  OS_GUI,  OS_ALT,  KC_TAB,  KC_MINS, LT_INS,  S_LEFT,  ___fn__, SP_BSPC, S_DOWN,  S_UP,    S_RGHT },
-  },
+  [_RSHIFT] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X),
+    S(KC_H), S(KC_I), S(KC_E), S(KC_A), KC_QUES, CNTR_HL, CNTR_HR, S(KC_C), S(KC_T), S(KC_R), S(KC_S), S(KC_W),
+    S(KC_J), TD_COLN, TD_DQOT, S(KC_K), KC_SLSH, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V),
+    OS_CTL,  OS_GUI,  OS_ALT,  KC_TAB,  KC_MINS, LT_INS,  S_LEFT,  ___fn__, SP_BSPC, S_DOWN,  S_UP,    S_RGHT
+  ),
 #ifdef HOME_MODS
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   Y  |   O  |   U  |   Z  |  Fn  | Caps |   `  |   [  |   ]  |   ^  |   X  |
@@ -71,12 +71,12 @@
   // | Ctrl |  GUI |  Alt |  Esc | Space|  Ins | Left |   +  | Space| Down |  Up  | Right|
   // `-----------------------------------------------------------------------------------'
 
-  [_LSYMBOL] = {
-    {S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, TD_GRV,  KC_LBRC, KC_RBRC, KC_CIRC, S(KC_X)},
-    {HOME_H,  HOME_I,  HOME_E,  ___fn__, KC_QUES, CNTR_HL, CNTR_HR, KC_PIPE, KC_LPRN, KC_RPRN, KC_DLR,  S(KC_W)},
-    {S(KC_J), TD_COLN, TD_DQOT, S(KC_K), KC_SLSH, CNTR_BL, CNTR_BR, KC_BSLS, KC_LCBR, KC_RCBR, KC_HASH, S(KC_V)},
-    {OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_INS,  SP_LEFT, KC_PLUS, SL_SPC,  AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_LSYMBOL] = LAYOUT_planck_grid(
+    S(KC_Q), S(KC_Y), S(KC_O), S(KC_U), S(KC_Z), CNTR_TL, CNTR_TR, TD_GRV,  KC_LBRC, KC_RBRC, KC_CIRC, S(KC_X),
+    HOME_H,  HOME_I,  HOME_E,  ___fn__, KC_QUES, CNTR_HL, CNTR_HR, KC_PIPE, KC_LPRN, KC_RPRN, KC_DLR,  S(KC_W),
+    S(KC_J), TD_COLN, TD_DQOT, S(KC_K), KC_SLSH, CNTR_BL, CNTR_BR, KC_BSLS, KC_LCBR, KC_RCBR, KC_HASH, S(KC_V),
+    OS_CTL,  OS_GUI,  OS_ALT,  LT_ESC,  TD_SPC,  LT_INS,  SP_LEFT, KC_PLUS, SL_SPC,  AT_DOWN, GT_UP,   CT_RGHT
+  ),
 
   // ,-----------------------------------------------------------------------------------.
   // |   Q  |   .  |   *  |   &  |   ~  |  Fn  | Caps |   G  |   D  |   N  |   M  |   X  |
@@ -89,11 +89,11 @@
   // |                           | ↑Tab |   =  |  Ent | Bksp |                           |
   // `-----------------------------------------------------------------------------------'
 
-  [_RSYMBOL] = {
-    {S(KC_Q), KC_DOT,  KC_ASTR, KC_AMPR, TD_TILD, CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X)},
-    {S(KC_H), TD_LT,   KC_PERC, TD_GT,   KC_QUES, CNTR_HL, CNTR_HR, S(KC_C), ___fn__, HOME_R,  HOME_S,  HOME_W },
-    {S(KC_J), TD_COLN, KC_AT,   KC_EXLM, KC_SLSH, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V)},
-    {OS_CTL,  OS_GUI,  OS_ALT,  S_TAB,   TD_EQL,  LT_INS,  SP_LEFT, TD_ENT,  LT_BSPC, AT_DOWN, GT_UP,   CT_RGHT},
-  },
+  [_RSYMBOL] = LAYOUT_planck_grid(
+    S(KC_Q), KC_DOT,  KC_ASTR, KC_AMPR, TD_TILD, CNTR_TL, CNTR_TR, S(KC_G), S(KC_D), S(KC_N), S(KC_M), S(KC_X),
+    S(KC_H), TD_LT,   KC_PERC, TD_GT,   KC_QUES, CNTR_HL, CNTR_HR, S(KC_C), ___fn__, HOME_R,  HOME_S,  HOME_W ,
+    S(KC_J), TD_COLN, KC_AT,   KC_EXLM, KC_SLSH, CNTR_BL, CNTR_BR, S(KC_B), S(KC_P), S(KC_L), S(KC_F), S(KC_V),
+    OS_CTL,  OS_GUI,  OS_ALT,  S_TAB,   TD_EQL,  LT_INS,  SP_LEFT, TD_ENT,  LT_BSPC, AT_DOWN, GT_UP,   CT_RGHT
+  ),
 #endif
 #endif
