46733c9ec52ffb3d0e374e21c63e558cb369e207
Ilya Zhuravlev <whatever@xyz.is>
2020-01-09 19:06:42 -0500

    Change vid/pid

--- keyboards/xiaomi/mk02/config.h
+++ keyboards/xiaomi/mk02/config.h
@@ -18,8 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x0483
-#define PRODUCT_ID      0x5710
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0B91
 #define DEVICE_VER      0x0001
 /* in python2: list(u"whatever".encode('utf-16-le')) */
 /*   at most 32 characters or the ugly hack in usb_main.c borks */
