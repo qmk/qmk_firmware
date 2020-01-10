5c1b30768c97a9824fecc28b0ea4ac8a7a5eff47
Lei Yu <yuleiz@gmail.com>
2020-01-10 11:28:17 +0800

    fixed incorrect matrix setting

--- keyboards/matrix/pjf/config.h
+++ keyboards/matrix/pjf/config.h
@@ -21,7 +21,7 @@
 #define MATRIX_COL_PINS { C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15}
 #define UNUSED_PINS
 
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
