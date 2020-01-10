ade891b6ace8b0926cac9fc95d257282aadfb6bc
fauxpark <fauxpark@gmail.com>
2020-01-10 11:19:19 +1100

    Migrate `ACTION_LAYER_TAP_KEY()` to `LT()` (#7277)
    * Branch point for 2019 Nov 30 Breaking Change.
    
    * Migrate `ACTION_LAYER_TAP_KEY()` to `LT()`
    
    * Apply suggestions from code review
    
    * Add ChangeLog entry
    
    * New breaking changes target date

--- keyboards/gh60/revc/keymaps/robotmaxtron/keymap.c
+++ keyboards/gh60/revc/keymaps/robotmaxtron/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
         F(12),KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINS,KC_EQL,KC_BSPC, \
         KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_LBRC,KC_RBRC,KC_BSLS, \
-        F(2),KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_J,KC_K,KC_L,KC_SCLN,KC_QUOT,KC_NO,KC_ENT,  \
+        LT(2, KC_CAPS),KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_J,KC_K,KC_L,KC_SCLN,KC_QUOT,KC_NO,KC_ENT,  \
         KC_LSFT,KC_NO,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_N,KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_NO,KC_RSFT, \
         KC_LCTL,KC_LGUI,KC_LALT,        KC_SPC,      KC_NO,KC_RALT,MO(2),KC_APP,KC_RCTL),
 
@@ -107,7 +107,6 @@ enum function_id {
 
 const uint16_t PROGMEM fn_actions[] = {
     [1]  = ACTION_LAYER_TOGGLE(1),     // Toggle Arrow Layer overlay
-    [2]  = ACTION_LAYER_TAP_KEY(2, KC_CAPS), // Tap to toggle caps lock and hold to activate function layer
     [3]  = ACTION_LAYER_TOGGLE(3),     // Toggle Underglow Layer overlay
     [4]  = ACTION_FUNCTION(RGBLED_TOGGLE), //Turn on/off underglow
     [5]  = ACTION_FUNCTION(RGBLED_STEP_MODE), // Change underglow mode
@@ -118,21 +117,6 @@ const uint16_t PROGMEM fn_actions[] = {
     [10] = ACTION_FUNCTION(RGBLED_INCREASE_VAL),
     [11] = ACTION_FUNCTION(RGBLED_DECREASE_VAL),
     [12] = ACTION_FUNCTION(SHIFT_ESC),
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
-    return MACRO_NONE;
 };
 
 void matrix_scan_user(void) {
