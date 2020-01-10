fcf707ec8c6e5332c77d254b4fb7b6c0e81064df
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-01-09 22:45:22 -0800

    [Keymap] Refactor Planck keymaps to use Layout Macros (#7172)
    * refactor ab keymap
    
    * refactor alexey keymap
    
    * refactor altgr keymap
    
    * refactor andylikescandy keymap
    
    * refactor aviator keymap
    
    * refactor basic keymap
    
    * refactor bone2planck keymap
    
    * refactor brandon keymap
    
    * refactor cbbrowne keymap
    
    * refactor chance keymap
    
    * refactor circuit keymap
    
    * refactor coloneljesus keymap
    
    * refactor corvec keymap
    
    * refactor danielhklein keymap
    
    * refactor dbroqua keymap
    
    * refactor dc keymap
    
    * refactor dcompact keymap
    
    * refactor dlaroe keymap
    
    * refactor dodger keymap
    
    * refactor dr0ck keymap
    
    * refactor dudeofawesome keymap
    
    * refactor emilyh keymap
    
    * refactor espynn keymap
    
    * refactor experimental keymap
    
    * refactor gabriel keymap
    
    * refactor grahampheath keymap
    
    * refactor gunp keymap
    
    * refactor handwired_binaryplease keymap
    
    * refactor hiea keymap
    
    * refactor hieax keymap
    
    * refactor impossible keymap
    
    * refactor jacob keymap
    
    * refactor jeebak keymap
    
    * refactor jeremy-dev keymap
    
    * refactor jhenahan keymap
    
    * refactor jirgn keymap
    
    * refactor johannes keymap
    
    * refactor kelorean keymap
    
    * refactor khord keymap
    
    * refactor kloki keymap
    
    * refactor kmontag42 keymap
    
    * refactor lae3 keymap
    
    * refactor lukas keymap
    
    * refactor luke keymap
    
    * refactor mason keymap
    
    * refactor matrixman keymap
    
    * refactor mjt keymap
    
    * refactor mjtnumsym keymap
    
    * refactor mollat keymap
    
    * refactor myoung34 keymap
    
    * refactor neo2planck keymap
    
    * refactor nico keymap
    
    * refactor not-quite-neo keymap
    
    * refactor originerd keymap
    
    * refactor paget keymap
    
    * refactor palleiko keymap
    
    * refactor pete keymap
    
    * refactor phreed keymap
    
    * refactor piemod keymap
    
    * refactor pok3r keymap
    
    * refactor premek keymap
    
    * refactor pvc keymap
    
    * refactor rai-suta keymap
    
    * refactor rodhaene keymap
    
    * refactor sdothum keymap
    
    * refactor sean keymap
    
    * refactor sgoodwin keymap
    
    * refactor smt keymap
    
    * refactor steno keymap
    
    * refactor tak3over keymap
    
    * refactor tehwalris keymap
    
    * refactor that_canadian keymap
    
    * refactor thermal_printer keymap
    
    * refactor unicode keymap
    
    * refactor yale keymap
    
    * refactor yang keymap
    
    * refactor zach keymap
    
    * refactor zrichard keymap
    
    * refactor angerthosenear keymap
    
    * refactor austin keymap
    
    * refactor charlie keymap
    
    * refactor daniel keymap
    
    * refactor david keymap
    
    * refactor dzobert keymap
    
    * refactor joe keymap
    
    * refactor kyle keymap
    
    * refactor leo keymap
    
    * refactor lucas keymap
    
    * refactor max keymap
    
    * refactor mitch keymap
    
    * refactor samuel keymap
    
    * refactor tong92 keymap
    
    * Add Changelog entry
    
    * move changelog (new breaking changes target date)

--- keyboards/planck/keymaps/altgr/keymap.c
+++ keyboards/planck/keymaps/altgr/keymap.c
@@ -66,17 +66,10 @@
 // sudo CPATH=<keymap.c directory>/common make ...
 
 
-#include "config.h"
-#pragma message "You may need to add LAYOUT_planck_grid to your keymap layers - see default for an example"
-#include "planck.h"
-#include "action_layer.h"
+#include QMK_KEYBOARD_H
 #ifdef STENO_ENABLE
 #include "keymap_steno.h"
 #endif
-#ifdef AUDIO_ENABLE
-#include "audio.h"
-#endif
-#include "eeconfig.h"
 
 extern keymap_config_t keymap_config;
 
@@ -183,7 +176,7 @@ enum planck_keycodes {
 #define S_TAB   S    (KC_TAB)
 #define S_UP    S    (KC_UP)
 
-#include "tapdance.h"
+#include "common/tapdance.h"
 
 // keycodes
 #define ___x___ KC_TRNS
@@ -256,35 +249,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #include "beakl.h"
 #include "colemak.h"
 #include "qwerty.h"
-#include "steno_layout.h"
+#include "common/steno_layout.h"
 
 // ...................................................... Number / Function Keys
 
-#include "number_fkey_layout.h"
+#include "common/number_fkey_layout.h"
 
 // ......................................................... Symbol / Navigation
 
-#include "symbol_guifn_layout.h"
+#include "common/symbol_guifn_layout.h"
 
 // ............................................................... Toggle Layers
 
 #ifdef CENTER_TT
-#include "toggle_layout.h"
+#include "common/toggle_layout.h"
 #endif
 
 // ......................................................... Short Cuts / Adjust
 
-#include "chord_layout.h"
+#include "common/chord_layout.h"
 
 };
 
 // ...................................................................... Sounds
 
-#include "sounds.h"
+#include "common/sounds.h"
 
 // ........................................................... User Keycode Trap
 
-#include "keycode_functions.h"
+#include "common/keycode_functions.h"
 
 #define BASE_1  1
 #define BASE_2  2
@@ -486,4 +479,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
   return true;
 }
 
-#include "init.h"
+#include "common/init.h"
