e6abfd4a19fdbdfe1b86e30c891c8dff7afa93f4
ehz <ptg0317@gmail.com>
2020-01-11 17:40:26 +0900

    start song imperal march

--- keyboards/preonic/keymaps/ehztot/config.h
+++ keyboards/preonic/keymaps/ehztot/config.h
@@ -1,12 +1,10 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    #define STARTUP_SONG SONG(IMPERIAL_MARCH)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
                                 }
 #endif
 
