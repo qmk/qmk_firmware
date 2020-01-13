0bf2b4cbecf34b783e169c7ed0dae5fd4d494792
Koichi Katano <36572567+kkatano@users.noreply.github.com>
2020-01-13 17:02:53 +0900

    Update config.h

--- keyboards/yurei/config.h
+++ keyboards/yurei/config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0000
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Koichi Katano
-#define PRODUCT yurei
-#define DESCRIPTION A custom keyboard
+#define PRODUCT Yurei
+#define DESCRIPTION A Tenkeyless PCB
 
 /* key matrix size */
 #define MATRIX_ROWS 6
