92c4a44d5b635e91bfb7cc78f316152e92156810
Rossman360 <rmontsinger@gmail.com>
2020-01-13 17:15:41 -0500

    line spacing and such

--- keyboards/montsinger/rebound/rebound.h
+++ keyboards/montsinger/rebound/rebound.h
@@ -19,9 +19,7 @@
 #include "quantum.h"
 
 /* This is a shortcut to help you visually see your layout.
- *
  * The first section contains all of the arguments representing the 
-physical
  * layout of the board and position of the keys.
  *
  * The second converts the arguments into a two-dimensional array which
