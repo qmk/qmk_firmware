0b7734bb7473196dce59d64bd935577866f09029
Koichi Katano <36572567+kkatano@users.noreply.github.com>
2020-01-13 21:48:10 +0900

    Update yurei.c

--- keyboards/yurei/yurei.c
+++ keyboards/yurei/yurei.c
@@ -16,48 +16,10 @@
 
 #include "yurei.h"
 
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
-
-void led_set_user(uint8_t usb_led) {
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        writePinHigh(B6);
-    } else {
-        writePinLow(B6);
-    }
-    if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
-        writePinHigh(B7);
-    } else {
-        writePinLow(B7);
-    }
-}
-
-/*
-void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
-
-    matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
-
-    matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    // put your per-action keyboard code here
-    // runs for every action, just before processing by the firmware
-
-    return process_record_user(keycode, record);
-}
-
 bool led_update_kb(led_t led_state) {
-    // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-    return led_update_user(led_state);
+    if (led_update_user(led_state)) {
+        writePin(B6, led_state.caps_lock);
+        writePin(B7, led_state.scroll_lock);
+    }
+    return true;
 }
-*/
