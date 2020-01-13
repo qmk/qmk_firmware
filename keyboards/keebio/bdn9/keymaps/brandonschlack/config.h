569cc80a0221972784ba61fc9d8a40c298c826b6
Brandon Schlack <brandonschlack@gmail.com>
2020-01-13 13:34:46 -0800

    [Keymap] Add Constant Mouse Keys to BDN9 keymap

--- keyboards/keebio/bdn9/keymaps/brandonschlack/config.h
+++ keyboards/keebio/bdn9/keymaps/brandonschlack/config.h
@@ -6,5 +6,5 @@
 #define RGBLIGHT_EFFECT_BREATHING // Only have Breathing Animation
 
 /* Mouse Keys Configuration */
-// #define MK_3_SPEED
-// #define MK_W_INTERVAL_UNMOD 20
+#define MK_3_SPEED
+#define MK_W_INTERVAL_UNMOD 20
