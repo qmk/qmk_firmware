06da46d820fc2fdbfa334f8f0da27f2f606320e6
BenRoe <mail@benjaminroesner.com>
2020-01-14 20:07:33 +0100

    Move ENCODER_ENABLE under Build Options

--- keyboards/keycapsss/plaid_pad/rules.mk
+++ keyboards/keycapsss/plaid_pad/rules.mk
@@ -14,7 +14,6 @@ BOOTLOADER = USBasp
 # disable debug code
 OPT_DEFS = -DDEBUG_LEVEL=0
 
-ENCODER_ENABLE = yes
 
 # Build Options
 #   change yes to no to disable
@@ -36,6 +35,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
+ENCODER_ENABLE = yes
 
 LAYOUTS = numpad_4x4
 LAYOUTS_HAS_RGB = no
