87c030ef200de4405eadd6cdc0f8dd1de0dd088a
Neil Feagan <neilfeagan@gmail.com>
2020-01-14 17:46:46 -0600

    Update rules.mk

--- keyboards/kv/revT/rules.mk
+++ keyboards/kv/revT/rules.mk
@@ -2,8 +2,6 @@
 MCU = STM32F303
 
 # Build Options
-<<<<<<< HEAD
-<<<<<<< HEAD
 #   comment out to disable the options.
 BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
@@ -21,6 +19,3 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
-=======
-=======
->>>>>>> e050e59ec79b49332222157d2f74968b49066e58
