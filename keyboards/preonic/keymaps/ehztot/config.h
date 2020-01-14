98698f0742435496628982796bbb1375d01dddeb
ehz <ptg0317@gmail.com>
2020-01-14 21:16:56 +0900

    o2j

--- keyboards/preonic/keymaps/ehztot/config.h
+++ keyboards/preonic/keymaps/ehztot/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(MARIO_THEME)
+    #define STARTUP_SONG SONG(Ode to Joy)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
