1a6361b3b04e9f8a70ef0147957282fb1148c632
ehz <ptg0317@gmail.com>
2020-01-14 20:49:18 +0900

    change start song campanella

--- keyboards/preonic/keymaps/ehztot/config.h
+++ keyboards/preonic/keymaps/ehztot/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(MARIO_THEME)
+    #define STARTUP_SONG SONG(CAMPANELLA)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
