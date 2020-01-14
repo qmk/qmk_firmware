b2aa9b109d19041f019ff7716c1bb1cc0d3db78d
Brandon Schlack <brandonschlack@gmail.com>
2020-01-13 18:15:31 -0800

    [Keymap] Fix 60_ansi layout
    * Add Media Mod Tap for RCmd

--- layouts/community/60_ansi/brandonschlack-ansi/keymap.c
+++ layouts/community/60_ansi/brandonschlack-ansi/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
     HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          MT_RSUP, \
-    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MT_RALT, MT_RFDN, MT_RCRT  \
+    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             MM_RGPY, MT_RALT, MT_RFDN, MT_RCRT  \
 ),
 
 /* Function Layer
