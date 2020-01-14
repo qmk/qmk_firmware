bd817aa76731987d8c69c4f07875c78c1609f909
Neil Feagan <neilfeagan@gmail.com>
2020-01-14 17:43:12 -0600

    Update rules.mk

--- keyboards/kv/revT/rules.mk
+++ keyboards/kv/revT/rules.mk
@@ -24,18 +24,3 @@ HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 =======
 =======
 >>>>>>> e050e59ec79b49332222157d2f74968b49066e58
-#   change yes to no to disable
-#
-BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration
-## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
-MOUSEKEY_ENABLE = yes # Mouse keys
-EXTRAKEY_ENABLE = yes # Audio control and System control
-CONSOLE_ENABLE = yes  # Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
-#SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes     # USB Nkey Rollover
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = no
-# SERIAL_LINK_ENABLE = yes
->>>>>>> 7f1ef1ab3ebbf7a64af4901a5ccc65415533d4eb
