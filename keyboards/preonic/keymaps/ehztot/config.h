71c0440827f23db9965b3f1e0ccc87dc0bbdb56c
ehz <ptg0317@gmail.com>
2020-01-14 21:06:31 +0900

    mario

--- keyboards/preonic/keymaps/ehztot/config.h
+++ keyboards/preonic/keymaps/ehztot/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(NOCTURNE_OP_9_NO_1)
+    #define STARTUP_SONG SONG(MARIO_THEME)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
