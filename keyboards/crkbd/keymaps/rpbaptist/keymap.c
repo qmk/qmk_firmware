2e37e19cd6ea96ec89c33193e1d8d27154da3959
Richard Baptist <rpbaptist@gmail.com>
2020-01-14 14:40:27 +0100

    Disable RGB layer indicator by default

--- keyboards/crkbd/keymaps/rpbaptist/keymap.c
+++ keyboards/crkbd/keymaps/rpbaptist/keymap.c
@@ -397,7 +397,7 @@ void rgb_matrix_set_defaults(void) {
     rgb_matrix_config.enable = 1;
     rgb_matrix_config.hsv    = (HSV){128, 255, 128};  // TEAL
 
-    user_config.rgb_layer_change        = true;
+    user_config.rgb_layer_change        = false;
     user_config.rgb_matrix_idle_anim    = true;
     user_config.rgb_matrix_idle_timeout = 60000;
 
