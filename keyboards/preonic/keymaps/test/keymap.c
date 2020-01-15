070668c51cd86691bbd7f99ddcc0c75f78e41a5d
ehz <ptg0317@gmail.com>
2020-01-16 00:32:37 +0900

    rm err

--- keyboards/preonic/keymaps/test/keymap.c
+++ keyboards/preonic/keymaps/test/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_preonic_2x2u(KC_GRV, LT(1,KC_1), LT(1,KC_2), LT(1,KC_3), LT(1,KC_4), LT(1,KC_5), LT(1,KC_6), LT(1,KC_7), LT(1,KC_8), LT(1,KC_9), LT(1,KC_0), KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_E, KC_R, KC_T, KC_Y, KC_U, LT(1,KC_O), LT(1,KC_P), LT(1,KC_MINS), KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, LT(1,KC_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT, KC_NO, LT(1,KC_ESC), KC_LGUI, KC_LALT, LT(1,KC_SPC), LT(1,KC_DEL), LT(1,KC_LEFT), LT(1,KC_DOWN), LT(1,KC_UP), LT(1,KC_RGHT)),
 
-};/* Lower
+/* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
