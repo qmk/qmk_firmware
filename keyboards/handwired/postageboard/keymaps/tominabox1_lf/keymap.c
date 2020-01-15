1fc19ae41e5249b6606cd92ab046518be0d1555b
tominabox1 <tom.campie@gmail.com>
2020-01-15 15:33:09 -0600

    add grv key to LF, tweak taps

--- keyboards/handwired/postageboard/keymaps/tominabox1_lf/keymap.c
+++ keyboards/handwired/postageboard/keymaps/tominabox1_lf/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,
     TD(TD_Q_ESC),TD(TD_WTAB),     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,  TD(TD_QCOL), //Y+QUOTE = KC_BSPC
     TD(TD_CTRL_A),     KC_R,     KC_S,     KC_T,     KC_D,     KC_H,     KC_N,     KC_E,     KC_I, KC_O,
-    LSFT_T(KC_Z),     KC_X,  KC_C,     KC_V,     KC_B,  KC_K, KC_M,  KC_COMM,  RSFT_T(KC_DOT), TD(TD_QUES_ENT),
+    LSFT_T(KC_Z),     KC_X,  KC_C,     KC_V,     KC_B,  KC_K, KC_M,  KC_COMM,  RSFT_T(KC_DOT), KC_SLSH,
                        KC_LALT, KC_LGUI, LT(_RAISE, KC_SPC), LT(_LOWER, KC_ENT)
   ),
   [_RAISE] = LAYOUT_split_space_base(
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_split_space_base(
   KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-      RESET,   RGBRST,  AG_NORM,  AG_SWAP,  DEBUG,  XXXXXXX,KC_PGDN,    KC_UP,  KC_PGUP,    XXXXXXX,
+      RESET,   RGBRST,  AG_NORM,  AG_SWAP,  DEBUG,  KC_GRV ,KC_PGDN,    KC_UP,  KC_PGUP,    XXXXXXX,
      RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  SPONGEBOB,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,
      RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  AESTHETIC,  KC_MINS,    KC_RO,  KC_COMM,   KC_DOT,  KC_BSLS,
      KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS
