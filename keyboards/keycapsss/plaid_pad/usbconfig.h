0fc159eacff65f86f0869c3d6d21f67a15f3981d
Ben <BenRoe@users.noreply.github.com>
2020-01-16 15:39:35 +0100

    Update keyboards/keycapsss/plaid_pad/usbconfig.h
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/keycapsss/plaid_pad/usbconfig.h
+++ keyboards/keycapsss/plaid_pad/usbconfig.h
@@ -241,7 +241,7 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'P', 'l', 'a', 'i', 'd'
+#define USB_CFG_DEVICE_NAME     'P', 'l', 'a', 'i', 'd', '-', 'P', 'a', 'd'
 #define USB_CFG_DEVICE_NAME_LEN 5
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
