40075ab87679a391454bcfc6b9382a2bedd786f1
Ben <BenRoe@users.noreply.github.com>
2020-01-16 15:15:06 +0100

    Update keyboards/keycapsss/plaid_pad/keymaps/default/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/keycapsss/plaid_pad/keymaps/default/keymap.c
+++ keyboards/keycapsss/plaid_pad/keymaps/default/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  0  |  .  |  =  |  +  |
  * `-----------------------'
  */
-LAYOUT_numpad_4x4(
+LAYOUT_ortho_4x4(
 		KC_P7, KC_P8,   KC_P9,      KC_PSLS,
 		KC_P4, KC_P5,   KC_P6,      KC_PAST,
 		KC_P1, KC_P2,   KC_P3,      KC_PMNS,
