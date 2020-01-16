5915978d3a1393904fefb7d037260a0e73ca0a1c
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-01-16 13:11:46 +0100

    Update keyboards/keebwerk/nano_slider/config.h
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/keebwerk/nano_slider/config.h
+++ keyboards/keebwerk/nano_slider/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID 0xFEED
 #define PRODUCT_ID 0x6060
 #define DEVICE_VER 0x0001
-#define MANUFACTURER KEEBWERK
+#define MANUFACTURER keebwerk.
 #define PRODUCT nano_slider
 #define DESCRIPTION A small eight key macropad
 
