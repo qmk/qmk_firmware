805b505cb551d31798b2acd2fa0777b774f2c3ba
Rys Sommefeldt <rys@sommefeldt.com>
2020-01-16 09:09:09 +0000

    Rename to 65_iso_blocker and remove uk designation

--- keyboards/gray_studio/space65/space65.h
+++ keyboards/gray_studio/space65/space65.h
@@ -55,7 +55,7 @@
     { k40, k41,   KC_NO, k43, KC_NO, KC_NO, KC_NO, k47, KC_NO, KC_NO, k4A, k4B, KC_NO, k4D,   k4E,   k4F }, \
 }
 
-#define LAYOUT_65_iso( \
+#define LAYOUT_65_iso_blocker( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C,    K0D,   K0F,   \
     K10,   K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,         K1F,   \
     K20,    K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K1E, K2D,   K2F,   \
