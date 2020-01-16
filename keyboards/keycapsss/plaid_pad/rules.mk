bea62801669b129c2c8c82cfb3a84fb32e9a7f47
Ben <BenRoe@users.noreply.github.com>
2020-01-16 15:16:11 +0100

    Update keyboards/keycapsss/plaid_pad/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/keycapsss/plaid_pad/rules.mk
+++ keyboards/keycapsss/plaid_pad/rules.mk
@@ -18,7 +18,7 @@ OPT_DEFS = -DDEBUG_LEVEL=0
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no        # Console for debug(+400)
