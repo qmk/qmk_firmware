39a79cbc480cae16788f8435452787913e7b69f4
Ben <BenRoe@users.noreply.github.com>
2020-01-16 15:17:17 +0100

    Update keyboards/keycapsss/plaid_pad/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/keycapsss/plaid_pad/rules.mk
+++ keyboards/keycapsss/plaid_pad/rules.mk
@@ -34,7 +34,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
+HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 ENCODER_ENABLE = yes
 
 LAYOUTS = ortho_4x4
