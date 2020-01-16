b1654b15d97d693c150dde2674c37d34950f8318
Rob Haswell <me@robhaswell.co.uk>
2020-01-16 17:06:22 +0000

    Apply the alt config to the left right gradient.

--- quantum/rgb_matrix_animations/gradient_left_right_anim.h
+++ quantum/rgb_matrix_animations/gradient_left_right_anim.h
@@ -5,14 +5,29 @@ RGB_MATRIX_EFFECT(GRADIENT_LEFT_RIGHT)
 bool GRADIENT_LEFT_RIGHT(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
 
-    HSV     hsv   = rgb_matrix_config.hsv;
-    uint8_t scale = scale8(64, rgb_matrix_config.speed);
+    HSV led_hsv;
+    HSV hsv       = rgb_matrix_config.hsv;
+    HSV alt_hsv   = rgb_matrix_config.hsv;
+    alt_hsv.h    += rgb_matrix_alt_config.hsv.h;
+    alt_hsv.s    += rgb_matrix_alt_config.hsv.s;
+    alt_hsv.v    += rgb_matrix_alt_config.hsv.v;
+
+    uint8_t led_scale;
+    uint8_t scale     = scale8(64, rgb_matrix_config.speed);
+    uint8_t alt_scale = scale8(64, rgb_matrix_config.speed + rgb_matrix_alt_config.speed);
     for (uint8_t i = led_min; i < led_max; i++) {
         RGB_MATRIX_TEST_LED_FLAGS();
         // The x range will be 0..224, map this to 0..7
         // Relies on hue being 8-bit and wrapping
-        hsv.h   = rgb_matrix_config.hsv.h + (scale * g_led_config.point[i].x >> 5);
-        RGB rgb = hsv_to_rgb(hsv);
+        if (RGB_MATRIX_IS_UNDERGLOW()) {
+            led_hsv = alt_hsv;
+            led_scale = alt_scale;
+        } else {
+            led_hsv = hsv;
+            led_scale = scale;
+        }
+        led_hsv.h   = led_hsv.h + (led_scale * g_led_config.point[i].x >> 5);
+        RGB rgb = hsv_to_rgb(led_hsv);
         rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
     }
     return led_max < DRIVER_LED_TOTAL;
