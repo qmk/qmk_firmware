5108a48d8eaa8dbe35a604f24a4e899b206b4e16
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-01-16 05:33:27 +0100

    Added LTO

--- keyboards/keebwerk/nano_slider/rules.mk
+++ keyboards/keebwerk/nano_slider/rules.mk
@@ -6,18 +6,19 @@ BOOTLOADER = atmel-dfu
 
 
 # Build Options
-BOOTMAGIC_ENABLE		= lite		# Virtual DIP switch configuration
-MOUSEKEY_ENABLE			= yes		# Mouse keys
-EXTRAKEY_ENABLE			= yes		# Audio control and System control
-CONSOLE_ENABLE			= yes		# Console for debug
-COMMAND_ENABLE			= yes		# Commands for debug and configuration
+BOOTMAGIC_ENABLE				= lite		# Virtual DIP switch configuration
+MOUSEKEY_ENABLE					= yes		# Mouse keys
+EXTRAKEY_ENABLE					= yes		# Audio control and System control
+CONSOLE_ENABLE					= yes		# Console for debug
+COMMAND_ENABLE					= yes		# Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE		= no		# Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE				= no		# Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE				= yes		# USB Nkey Rollover
-BACKLIGHT_ENABLE		= no		# Enable keyboard backlight functionality
-RGBLIGHT_ENABLE			= yes		# Enable keyboard RGB underglow
-MIDI_ENABLE				= yes		# MIDI support
-BLUETOOTH_ENABLE		= no		# Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE			= no		# Audio output on port C6
+NKRO_ENABLE						= yes		# USB Nkey Rollover
+BACKLIGHT_ENABLE				= no		# Enable keyboard backlight functionality
+RGBLIGHT_ENABLE					= yes		# Enable keyboard RGB underglow
+MIDI_ENABLE						= yes		# MIDI support
+BLUETOOTH_ENABLE				= no		# Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE					= no		# Audio output on port C6
+LINK_TIME_OPTIMIZATION_ENABLE	= yes
 
