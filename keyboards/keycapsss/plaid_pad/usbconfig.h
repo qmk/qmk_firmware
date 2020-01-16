e2a94266de2fc22623fb9297bf36b1d66a412b02
Ben <BenRoe@users.noreply.github.com>
2020-01-16 15:41:15 +0100

    Update keyboards/keycapsss/plaid_pad/usbconfig.h
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/keycapsss/plaid_pad/usbconfig.h
+++ keyboards/keycapsss/plaid_pad/usbconfig.h
@@ -232,7 +232,7 @@ section at the end of this file).
 /* Version number of the device: Minor number first, then major number.
  */
 #define USB_CFG_VENDOR_NAME     'K','e','y','c','a','p','s','s','s'
-#define USB_CFG_VENDOR_NAME_LEN 10
+#define USB_CFG_VENDOR_NAME_LEN 9
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
