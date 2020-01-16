1df6d26d4f55f95815fb9a18d63a729fe09c4ceb
ehz <ptg0317@gmail.com>
2020-01-16 21:06:22 +0900

    rm lt ()

--- keyboards/preonic/keymaps/test/keymap.c
+++ keyboards/preonic/keymaps/test/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LOWER| ESC  | GUI  | ALT  |    SPACE    |  del        | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_preonic_2x2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, LT(KC_BSLS,KC_MINS), KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_ESC, LT(KC_LGUI,KC_ESC), KC_LGUI, KC_LALT, KC_SPC, LT(1,KC_DEL),KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+[_QWERTY] = LAYOUT_preonic_2x2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_ESC, KC_ESC, KC_LGUI, KC_LALT, KC_SPC, LT(1,KC_DEL),KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      | bd   |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_preonic_2x2u(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_EQL, KC_PSCR, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_INS, KC_HOME, KC_PGUP, KC_BRIU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_DEL, KC_END, KC_PGDN, KC_LGUI, KC_BRID, KC_NO, KC_TRNS, KC_MPLY, TO(1), KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD),
+[_LOWER] = LAYOUT_preonic_2x2u(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_EQL, KC_PSCR, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_INS, KC_HOME, KC_PGUP, KC_BRIU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_DEL, KC_END, KC_PGDN, KC_LGUI, KC_BRID, KC_NO, KC_TRNS, KC_MPLY, TO(1), KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD,
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
