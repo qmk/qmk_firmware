29bcf49bb276c29ca51f39d59cf00907e7d4b423
Takuya Urakawa <urkwtky@gmail.com>
2020-01-17 13:17:48 +0900

    Update keyboards/tartan/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/tartan/rules.mk
+++ keyboards/tartan/rules.mk
@@ -36,7 +36,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
+HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
 # unsupported features for now
 NO_UART = yes
