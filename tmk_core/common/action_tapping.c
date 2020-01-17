e5890845a727313005fde7fe3e324e2b658c6d21
Drashna Jaelre <drashna@live.com>
2020-01-17 12:33:08 -0800

    Cleanup check for PERMISSIVE_HOLD (#7861)
    * Fix bug in PERMISSIVE_HOLD check
    
    caused by #5009 (aka, me)
    
    * Remove check for per key

--- tmk_core/common/action_tapping.c
+++ tmk_core/common/action_tapping.c
@@ -111,7 +111,7 @@ bool process_tapping(keyrecord_t *keyp) {
                  * This can register the key before settlement of tapping,
                  * useful for long TAPPING_TERM but may prevent fast typing.
                  */
-#    if defined(TAPPING_TERM_PER_KEY) || (!defined(PER_KEY_TAPPING_TERM) && TAPPING_TERM >= 500) || defined(PERMISSIVE_HOLD)
+#    if defined(TAPPING_TERM_PER_KEY) || (TAPPING_TERM >= 500) || defined(PERMISSIVE_HOLD)
 #        ifdef TAPPING_TERM_PER_KEY
                 else if ((get_tapping_term(get_event_keycode(tapping_key.event)) >= 500) && IS_RELEASED(event) && waiting_buffer_typed(event))
 #        else
