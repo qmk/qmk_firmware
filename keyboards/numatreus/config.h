86a1adfab27d328d00e5124557d22b7ca28ddb66
yohewi <yohewi@gmail.com>
2020-01-19 03:19:25 +0900

    update

--- keyboards/numatreus/config.h
+++ keyboards/numatreus/config.h
@@ -22,9 +22,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6062
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Profet
-#define PRODUCT         Atreus
-#define DESCRIPTION     q.m.k. keyboard firmware for Atreus
+#define MANUFACTURER    yohewi
+#define PRODUCT         NumAtreus
+#define DESCRIPTION     q.m.k. keyboard firmware for NumAtreus
 
 /* key matrix size */
 #define MATRIX_ROWS 4
