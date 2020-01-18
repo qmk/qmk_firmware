b4c072a790518eb400a5d904ae070effdb596b72
Drashna Jael're <drashna@live.com>
2020-01-18 15:53:59 -0800

    last minutue cleanup

--- keyboards/mouse/ploopyco_trackball/config.h
+++ keyboards/mouse/ploopyco_trackball/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0xFEED
-#define DEVICE_VER   0x1776
+#define PRODUCT_ID   0x1776
+#define DEVICE_VER   0x0001
 #define MANUFACTURER Ploopyco
 #define PRODUCT      Trackball
 #define DESCRIPTION  A QMK Powered Trackball
--- keyboards/mouse/ploopyco_trackball/rules.mk
+++ keyboards/mouse/ploopyco_trackball/rules.mk
@@ -15,10 +15,10 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
