32c1254e024303289b6ebe31d584d3abd1fee25f
Jeremy Bernhardt <jeremythegeek@gmail.com>
2020-01-19 16:57:11 -0600

    Update keyboards/faunchpad/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/faunchpad/rules.mk
+++ keyboards/faunchpad/rules.mk
@@ -13,7 +13,7 @@ BOOTLOADER = atmel-dfu
 
 MOUSEKEY_ENABLE 	= yes		# Mouse keys
 EXTRAKEY_ENABLE 	= yes       # Audio control and System control(+450)
-CONSOLE_ENABLE 		= yes       # Console for debug(+400)
+CONSOLE_ENABLE 		= yes       # Console for debug
 COMMAND_ENABLE 		= no		# Commands for debug and configuration
 NKRO_ENABLE 		= yes       # USB Nkey Rollover
 STENO_ENABLE		= yes 		# Needed for chording
