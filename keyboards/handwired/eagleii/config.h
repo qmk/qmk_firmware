0c41e2de4b66839932ea26a3db7cadd69edcd1c2
Spaceman <rionlion100@gmail.com>
2020-01-19 20:47:44 -0500

    Update keyboards/handwired/eagleii/config.h
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/handwired/eagleii/config.h
+++ keyboards/handwired/eagleii/config.h
@@ -4,7 +4,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define PRODUCT_ID      0x9789
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Eagle
 #define PRODUCT         II
