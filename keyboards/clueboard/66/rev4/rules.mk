66aaa09621d54cf85a2b3ac8e161d8b64637e35d
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2020-01-19 22:01:50 -0800

    Update keyboards/clueboard/66/rev4/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/clueboard/66/rev4/rules.mk
+++ keyboards/clueboard/66/rev4/rules.mk
@@ -6,7 +6,7 @@ MCU = STM32F303
 #
 BOOTMAGIC_ENABLE = lite   # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes   # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes   # Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes   # Audio control and System control
 CONSOLE_ENABLE = yes    # Console for debug(+400)
 COMMAND_ENABLE = yes     # Commands for debug and configuration
 NKRO_ENABLE = yes       # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
