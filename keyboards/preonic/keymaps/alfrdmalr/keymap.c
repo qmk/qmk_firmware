c75f0a31b7464940019623a47e630da9b5aa490a
alfrdmalr <alfrdmalr@gmail.com>
2020-01-21 08:48:54 -0500

    restore settings layer

--- keyboards/preonic/keymaps/alfrdmalr/keymap.c
+++ keyboards/preonic/keymaps/alfrdmalr/keymap.c
@@ -36,6 +36,7 @@ enum preonic_keycodes {
 #define NUMSPACE LT(_NUMPAD, KC_SPC)
 #define NAVLAYER MO(_NAVIGATION)
 #define SYMLAYER MO(_SYMBOLS)
+#define SETLAYER MO(_SETTINGS)
 #define CTRLSHFT C(KC_LSFT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -58,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,      KC_Q,     KC_W,    KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,    KC_DEL,  
   NAVLAYER,    KC_A,     KC_S,    KC_D,    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,    KC_L,     KC_SCLN, KC_QUOT, 
   KC_LSFT,     KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, 
-  KC_LCTL,     CTRLSHFT, KC_LGUI, KC_LALT, SYMLAYER, NUMSPACE, NUMSPACE, SYMLAYER, KC_RALT, SETTINGS, MU_TOG,  KC_LEAD
+  KC_LCTL,     CTRLSHFT, KC_LGUI, KC_LALT, SYMLAYER, NUMSPACE, NUMSPACE, SYMLAYER, KC_RALT, SETLAYER, MU_TOG,  KC_LEAD
 ),
 
 /* SYMBOLS
