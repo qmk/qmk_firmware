1c83064e890188f4d2de7b93c8509fa4f57c66ee
yohewi <yohewi@gmail.com>
2020-01-21 21:52:44 +0900

    Update keyboards/numatreus/keymaps/hdbx/keymap.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/numatreus/keymaps/hdbx/keymap.c
+++ keyboards/numatreus/keymaps/hdbx/keymap.c
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       )
 };
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
   state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
 return state;
 }
