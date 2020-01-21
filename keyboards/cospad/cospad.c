c89ea2dbca9709745a5cb0cfd02877ea0bfcb358
fauxpark <fauxpark@gmail.com>
2020-01-22 10:50:56 +1100

    Add num lock indicator code

--- keyboards/cospad/cospad.c
+++ keyboards/cospad/cospad.c
@@ -14,3 +14,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "cospad.h"
+
+bool led_update_kb(led_t led_state) {
+    if (led_update_user(led_state)) {
+        writePin(B2, !led_state.num_lock);
+    }
+    return true;
+}
