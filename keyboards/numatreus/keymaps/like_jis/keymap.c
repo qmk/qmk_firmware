1bed98f4b5f984787f3a47f57f7e0de4f7d73d34
yohewi <yohewi@gmail.com>
2020-01-22 12:42:38 +0900

    Update keyboards/numatreus/keymaps/like_jis/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/numatreus/keymaps/like_jis/keymap.c
+++ keyboards/numatreus/keymaps/like_jis/keymap.c
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------|             |------+------+------+------+------|
    RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_SLCK,      XXXXXXX, KC_WH_R, KC_WH_D,  KC_END, KC_PGDN,
   //|------+------+------+------+------|             |------+------+------+------+------|
-   RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_NLCK,      KC_XXXXXXX, KC_BTN1, KC_BTN2, KC_MS_U, KC_XXXXXXX,
+   RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_NLCK,      XXXXXXX, KC_BTN1, KC_BTN2, KC_MS_U, XXXXXXX,
   //|------+------+------+------+------|------+------+------+------+------+------+------|
    KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R
   //|------+------+------+------+-------------+------+------+------+------+------+------|
