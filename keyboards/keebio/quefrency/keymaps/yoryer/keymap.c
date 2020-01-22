f81977c1b5c80510973ee2c87d4c515ca2d3d5d6
Jorge Noguera <yoryer@gmail.com>
2020-01-21 23:13:20 -0300

    - Updated the layer list as an enum - Removed .gitignore file from keymap folder

--- keyboards/keebio/quefrency/keymaps/yoryer/keymap.c
+++ keyboards/keebio/quefrency/keymaps/yoryer/keymap.c
@@ -4,9 +4,12 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _FN1 1
-#define _RGB2 2
+
+enum layer {
+    _BASE,
+    _FN1,
+    _RGB2,
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
