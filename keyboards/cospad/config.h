28b02e5e467945ae42172b1d5dd1f57493706b82
fauxpark <fauxpark@gmail.com>
2020-01-22 12:25:29 +1100

    Define BACKLIGHT_ON_STATE explicitly, in anticipation of PR6749

--- keyboards/cospad/config.h
+++ keyboards/cospad/config.h
@@ -56,6 +56,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BACKLIGHT_PIN F7
 // #define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 3
+#define BACKLIGHT_ON_STATE 0
 
 #define RGB_DI_PIN F6
 #ifdef RGB_DI_PIN
