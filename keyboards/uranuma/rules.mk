bef0589ee082e9174eede4ecdf9c3ee374183f79
yohewi <yohewi@gmail.com>
2020-01-22 12:39:55 +0900

    Update keyboards/uranuma/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/uranuma/rules.mk
+++ keyboards/uranuma/rules.mk
@@ -39,7 +39,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
+HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
 #COMBO_ENABLE = yes
 #SRC +=  .nicola.c \
