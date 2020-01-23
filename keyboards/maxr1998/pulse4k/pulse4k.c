6f0d4cf7b01c029b1d0db42a80174228c27e4b16
Maxr1998 <max.rumpf1998@gmail.com>
2020-01-23 09:58:46 +0100

    Add missing functions to fix configurator
    Add empty encoder functions to the keyboard code, as the configurator doesn't see the keymap defined functions at first.
    To be able to override these functions, they are marked as 'weak'.

--- keyboards/maxr1998/pulse4k/pulse4k.c
+++ keyboards/maxr1998/pulse4k/pulse4k.c
@@ -59,3 +59,11 @@ void encoder_update_kb(uint8_t index, bool clockwise) {
         } else encoder_two_update(clockwise);
     }
 }
+
+__attribute__((weak)) void encoder_one_update(bool clockwise) {
+    // override in keymap
+}
+
+__attribute__((weak)) void encoder_two_update(bool clockwise) {
+    // override in keymap
+}
