5afcffb85cf768391db6ac7b1c975696ae86d939
worldspawn00 <mcmancuso@gmail.com>
2020-01-22 20:21:35 -0600

    adding boards

---
+++ keyboards/wsk/gothic50/config.h
@@ -0,0 +1,69 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0000
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Worldspawn
+#define PRODUCT         Gothic50
+#define DESCRIPTION     Expanded Ergo Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* key matrix pins */
+
+#define MATRIX_ROW_PINS { B5, B4, D7, D6 }
+#define MATRIX_COL_PINS { E6, F0, F1, C7, C6, B6, D4, D5, D3, D2, D1, D0, B7, B0 }
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+
+
+#define RGB_DI_PIN F7
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#endif
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCING_DELAY 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+
+#define LOCKING_RESYNC_ENABLE
+
+/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is userful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+
+ #define GRAVE_ESC_CTRL_OVERRIDE
+
+#endif
+
+
+
+
+
+
+
+
+
---
+++ keyboards/wsk/gothic50/gothic50.c
@@ -0,0 +1,3 @@
+
+#include "gothic50.h"
+
