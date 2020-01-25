3756ebc21b794c7a1a9b179fe26cecafc4634aa2
brandonschlack <brandonschlack@gmail.com>
2020-01-25 15:12:04 -0800

    [Keymap] Fix FN_LYR layer define refactor

--- layouts/community/ortho_5x12/brandonschlack/keymap.c
+++ layouts/community/ortho_5x12/brandonschlack/keymap.c
@@ -206,7 +206,7 @@ bool music_mask_user(uint16_t keycode) {
     switch (keycode) {
         case RAISE:
         case LOWER:
-        case FN_LYR:
+        case MCO_LYR:
             return false;
         default:
             return true;
