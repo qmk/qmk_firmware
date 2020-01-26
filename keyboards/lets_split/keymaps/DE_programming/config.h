d9ad6ba93cc803afde14144cc3b3ed3a5cceab3e
moseschmiedel <58808540+moseschmiedel@users.noreply.github.com>
2020-01-26 01:09:30 +0100

    Update copyright in config.h

--- keyboards/lets_split/keymaps/DE_programming/config.h
+++ keyboards/lets_split/keymaps/DE_programming/config.h
@@ -1,8 +1,7 @@
 /*
 This is the c configuration file for the keymap
 
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+Copyright 2020 Mose Schmiedel <mose.schmiedel@web.de>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
