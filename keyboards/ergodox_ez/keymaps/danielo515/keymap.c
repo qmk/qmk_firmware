0c23fb1370ada6a2a4cfafe891645c4dce07a0c9
Daniel Rodríguez Rivero <rdanielo@gmail.com>
2020-01-26 12:05:59 +0100

    fix: some more @drashna improvements
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/ergodox_ez/keymaps/danielo515/keymap.c
+++ keyboards/ergodox_ez/keymaps/danielo515/keymap.c
@@ -229,10 +229,10 @@ void matrix_scan_kb(void){
     matrix_scan_user();
 };
 
-uint32_t layer_state_set_user(uint32_t state)
+layer_state_t layer_state_set_user(layer_state_t state)
 {
 
-  uint8_t layer = biton32(state);
+  uint8_t layer = get_highest_layer(state);
 
   ergodox_board_led_off();
   ergodox_right_led_1_off();
