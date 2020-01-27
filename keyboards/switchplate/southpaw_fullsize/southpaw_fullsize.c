37d38edba1e7503b62992c6f7697de837a7523cc
ai03 <sarcaphx510@gmail.com>
2020-01-27 03:16:36 -0800

    Make indicator LEDs work

--- keyboards/switchplate/southpaw_fullsize/southpaw_fullsize.c
+++ keyboards/switchplate/southpaw_fullsize/southpaw_fullsize.c
@@ -20,14 +20,36 @@
 // You can leave any or all of these undefined.
 // These are only required if you want to perform custom actions.
 
-/*
+#define INDICATOR_NUM D3
+#define INDICATOR_CAPS D4
+#define INDICATOR_SCR D5
+
+
 void matrix_init_kb(void) {
     // put your keyboard start-up code here
     // runs once when the firmware starts up
 
+    // D3 Numlock, D4 Capslock, D5 Scrlock
+	setPinOutput(INDICATOR_NUM);
+    setPinOutput(INDICATOR_CAPS);
+    setPinOutput(INDICATOR_SCR);
+
     matrix_init_user();
 }
 
+bool led_update_kb(led_t led_state) {
+    // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+    bool res = led_update_user(led_state);
+    if (res)
+    {
+        writePin(INDICATOR_NUM, !led_state.num_lock);
+        writePin(INDICATOR_CAPS, !led_state.caps_lock);
+        writePin(INDICATOR_SCR, !led_state.scroll_lock);
+    }
+    return res;
+}
+
+/*
 void matrix_scan_kb(void) {
     // put your looping keyboard code here
     // runs every cycle (a lot)
@@ -40,11 +62,5 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     // runs for every action, just before processing by the firmware
 
     return process_record_user(keycode, record);
-}
-
-bool led_update_kb(led_t led_state) {
-    // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-    return led_update_user(led_state);
 }
 */
