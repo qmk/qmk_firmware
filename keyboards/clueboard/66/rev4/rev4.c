212aeee202f1031395f56a087441c6da030afdd4
skullydazed <skullydazed@users.noreply.github.com>
2020-01-26 22:32:11 -0800

    Revert "Clueboard Rev4 Updates aka Volcano 660 (#7942)"
    This reverts commit e0a0430c31bd9fabab93e8933e9988e0d3f66434.

--- keyboards/clueboard/66/rev4/rev4.c
+++ keyboards/clueboard/66/rev4/rev4.c
@@ -1,4 +1,5 @@
 #include "rev4.h"
+//#include "backlight.h"
 
 void matrix_init_kb(void) {
     // put your keyboard start-up code here
@@ -7,19 +8,49 @@ void matrix_init_kb(void) {
     led_init_ports();
 }
 
+void matrix_scan_kb(void) {
+    matrix_scan_user();
+}
+
+void backlight_init_ports(void) {
+    print("init_backlight_pin()\n");
+    // Set our LED pins as output
+    //DDRD |= (1<<6); // Esc
+    //DDRB |= (1<<7); // Page Up
+    //DDRD |= (1<<4); // Arrows
+
+    // Set our LED pins low
+    //PORTD &= ~(1<<6); // Esc
+    //PORTB &= ~(1<<7); // Page Up
+    //PORTD &= ~(1<<4); // Arrows
+}
+
+void backlight_set(uint8_t level) {
+/*
+    if ( level == 0 ) {
+        // Turn off light
+        PORTD |= (1<<6); // Esc
+        PORTB |= (1<<7); // Page Up
+        PORTD |= (1<<4); // Arrows
+    } else {
+        // Turn on light
+        PORTD &= ~(1<<6); // Esc
+        PORTB &= ~(1<<7); // Page Up
+        PORTD &= ~(1<<4); // Arrows
+    }
+*/
+}
+
 void led_init_ports() {
     // Set our LED pins as output
-    setPinOutput(B13); // LED1
-    writePinLow(B13);
-
-    setPinOutput(B14); // LED2
-    writePinLow(B14);
-
-    setPinOutput(B8); // LED3
-    writePinLow(B8);
-
-    setPinOutput(B0); // Capslock LED
-    writePinLow(B0);
+    palSetPadMode(GPIOB, 13, PAL_MODE_OUTPUT_PUSHPULL); // LED1
+    palClearPad(GPIOB, 13);
+    palSetPadMode(GPIOB, 14, PAL_MODE_OUTPUT_PUSHPULL); // LED2
+    palClearPad(GPIOB, 14);
+    palSetPadMode(GPIOA, 8, PAL_MODE_OUTPUT_PUSHPULL); // LED3
+    palClearPad(GPIOA, 8);
+    palSetPadMode(GPIOA, 0, PAL_MODE_OUTPUT_PUSHPULL); // Capslock LED
+    palClearPad(GPIOA, 0);
 }
 
 void led_set_kb(uint8_t usb_led) {
