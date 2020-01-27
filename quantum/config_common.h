d4f504b70d0825b4ef5a9d0e59344a944b7f2938
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-01-27 05:04:39 +0100

    Ensure setPinInput actually sets input high-Z (#6237)
    * Ensure setPinInput actually sets input high-z
    
    * Fixed _PIN_ADDRESS Macro arguments
    as recommended by vomindoraan
    
    * Fixed instances of setInput to use new behavour
    
    * Changed kmac matrix to use input with pullups
    
    * Update keyboards/gh60/revc/revc.h
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Fixed input state for unselect_rows
    
    * fixed merge conflict
    
    * Updated all instances of older uses of setPinInput()
    
    * Fixed naming mistake
    
    Co-authored-by: fauxpark <fauxpark@gmail.com>

--- quantum/config_common.h
+++ quantum/config_common.h
@@ -133,7 +133,7 @@
 #    endif
 
 #    ifndef __ASSEMBLER__
-#        define _PIN_ADDRESS(p, offset) _SFR_IO8(ADDRESS_BASE + (p >> PORT_SHIFTER) + offset)
+#        define _PIN_ADDRESS(p, offset) _SFR_IO8(ADDRESS_BASE + ((p) >> PORT_SHIFTER) + (offset))
 // Port X Input Pins Address
 #        define PINx_ADDRESS(p) _PIN_ADDRESS(p, 0)
 // Port X Data Direction Register,  0:input 1:output
