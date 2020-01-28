8f5c5fa327ed5809bf1d8e5fc45e8331a382837a
worldspawn00 <mcmancuso@gmail.com>
2020-01-28 00:36:26 -0600

    Update keyboards/wsk/gothic50/rules.mk
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/wsk/gothic50/rules.mk
+++ keyboards/wsk/gothic50/rules.mk
@@ -63,7 +63,7 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
