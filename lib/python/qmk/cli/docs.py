edbb0ad1d9a1ce03edb3940a78b614fd6782df6d
zvecr <git@zvecr.com>
2020-01-28 18:53:47 +0000

    update to cli command

--- lib/python/qmk/cli/docs.py
+++ lib/python/qmk/cli/docs.py
@@ -2,16 +2,16 @@
 """
 import http.server
 import os
+import shutil
+import subprocess
+from pathlib import Path
 
 from milc import cli
 
-
-@cli.argument('-p', '--port', default=8936, type=int, help='Port number to use.')
-@cli.subcommand('Run a local webserver for QMK documentation.', hidden=False if cli.config.user.developer else True)
-def docs(cli):
+def serve(dir):
     """Spin up a local HTTPServer instance for the QMK docs.
     """
-    os.chdir('docs')
+    os.chdir(dir)
 
     with http.server.HTTPServer(('', cli.config.docs.port), http.server.SimpleHTTPRequestHandler) as httpd:
         cli.log.info("Serving QMK docs at http://localhost:%d/", cli.config.docs.port)
@@ -23,3 +23,36 @@ def docs(cli):
             cli.log.info("Stopping HTTP server...")
         finally:
             httpd.shutdown()
+
+def build():
+    """
+    Invoke the docs build process
+
+    TODO(unclaimed):
+        * [ ] Add a real build step... something static docs
+    """
+
+    input_path = Path('docs')
+    output_path = Path('.build/docs')
+
+    if output_path.exists():
+        shutil.rmtree(output_path)
+
+    shutil.copytree(input_path, output_path)
+
+    #Generate internal docs
+    doxygen = subprocess.run(['doxygen', 'Doxyfile'])
+    moxygen = subprocess.run(['npx', 'moxygen', '-q', '-a', '-g', '-o', '.build/docs/internals_%s.md', 'doxygen/xml'])
+
+@cli.argument('-b', '--build', arg_only=True, action='store_true', help='Build.')
+@cli.argument('-s', '--serve', arg_only=True, action='store_true', help='Serve docs locally.')
+@cli.argument('-p', '--port', default=8936, type=int, help='Port number to use.')
+@cli.subcommand('Local interactins wth QMK documentation.', hidden=False if cli.config.user.developer else True)
+def docs(cli):
+    if cli.args.build:
+        build()
+        if cli.args.serve:
+            serve('.build/docs')
+    else:
+        # default to just serve the docs folder
+        serve('docs')
