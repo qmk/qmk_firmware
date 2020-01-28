ef1676001ddce84c5360af8fdf06e3fa7b22a06a
worldspawn00 <mcmancuso@gmail.com>
2020-01-28 00:41:19 -0600

    Update keyboards/walletburner/neuron/rules.mk
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/walletburner/neuron/rules.mk
+++ keyboards/walletburner/neuron/rules.mk
@@ -45,7 +45,7 @@ BOOTLOADER = atmel-dfu
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE ?= yes	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
+MOUSEKEY_ENABLE ?= yes	# Mouse keys
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
 CONSOLE_ENABLE ?= no	# Console for debug(+400)
 COMMAND_ENABLE ?= no    # Commands for debug and configuration
