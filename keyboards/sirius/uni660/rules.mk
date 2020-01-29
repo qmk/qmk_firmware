2afce44f0ff28d4aa95ab4171eb0c75613c0ec76
Daniel H Klein <dhklein92@gmail.com>
2020-01-28 16:19:58 -0800

    Update keyboards/sirius/uni660/rules.mk
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/sirius/uni660/rules.mk
+++ keyboards/sirius/uni660/rules.mk
@@ -45,7 +45,7 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE = yes	# Console for debug(+400)
