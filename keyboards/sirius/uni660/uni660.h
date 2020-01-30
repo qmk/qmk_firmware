70768e99576a668c97d2ce0301683210793992b7
Daniel H Klein <dhklein92@gmail.com>
2020-01-30 14:46:50 -0800

    Update keyboards/sirius/uni660/uni660.h
    Co-Authored-By: fauxpark <fauxpark@gmail.com>

--- keyboards/sirius/uni660/uni660.h
+++ keyboards/sirius/uni660/uni660.h
@@ -2,7 +2,7 @@
 
 #include "quantum.h"
 
-#define red_led_off   PORTF |= (1<<5)
+#define red_led_off   writePinHigh(F5)
 #define red_led_on    PORTF &= ~(1<<5)
 #define blu_led_off   PORTF |= (1<<4)
 #define blu_led_on    PORTF &= ~(1<<4)
