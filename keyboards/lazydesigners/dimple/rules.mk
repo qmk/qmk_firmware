4139de3cc9c56a157d78e1e418893d08e3f23bde
Joel Challis <git@zvecr.com>
2020-01-30 23:44:40 +0000

    Fix AVR boards which were overriding backlight without setting… (#8044)
    * Add backlight custom driver to all boards not declaring it but overriding backlight
    
    * Apply suggestions from code review
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: fauxpark <fauxpark@gmail.com>
    
    * Remove old tmk references from show_options.mk
    
    Co-authored-by: fauxpark <fauxpark@gmail.com>

--- keyboards/lazydesigners/dimple/rules.mk
+++ keyboards/lazydesigners/dimple/rules.mk
@@ -18,5 +18,5 @@ BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes        # Audio control and System control
 NKRO_ENABLE = yes            # USB Nkey Rollover
-BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality on B7 by default
+BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
