c11469583bab39549a01c2be6b6c172e40f3182c
mechlovin <teammechlovin@gmail.com>
2020-01-31 09:23:33 +0700

    update

--- keyboards/mechlovin/hannah910/config.h
+++ keyboards/mechlovin/hannah910/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x4D4C // ML-MechLovin'                                                   
+#define VENDOR_ID       0x4D4C // ML-MechLovin'
 #define MANUFACTURER    Mechlovin
 #define PRODUCT         Mechlovin Hannah910
 #define DESCRIPTION     Mechlovin Hannah910
