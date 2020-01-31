833001baa7bc2fcfb51b1310a7d00b695aac8b0f
ENDO Katsuhiro <ka2hiro@curlybracket.co.jp>
2020-02-01 05:32:06 +0900

    Update keyboards/chidori/keymaps/extended/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/chidori/keymaps/extended/keymap.c
+++ keyboards/chidori/keymaps/extended/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |ADJUST| Esc  | Alt  | GUI  |LOWER |Space |             | Space| RAISE| Left | Down |  Up  | Right|             |      0      |   .  |      |   -  | Bksp |
    * `-----------------------------------------'             `-----------------------------------------'             `-----------------------------------------'
    */
-    [_COLEMAK] = LAYOUT3(
+    [_COLEMAK] = LAYOUT_extended(
       KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,              KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,       KC_P7,   KC_P8,   KC_P9,  KC_PPLS, KC_NLCK, KC_ESC,
       KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,              KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,       KC_P4,   KC_P5,   KC_P6,  _______, KC_PSLS, KC_TAB,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,       KC_P1,   KC_P2,   KC_P3,  KC_PENT, KC_PAST, KC_PEQL,
