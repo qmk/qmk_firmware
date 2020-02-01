2f9dbd9e7d7c4d349572ae13efe3c9205e28cb5d
Katsuhiro Endo <ka2hiro@curlybracket.co.jp>
2020-02-01 09:04:10 +0900

    Update rules.mk

--- keyboards/chidori/rules.mk
+++ keyboards/chidori/rules.mk
@@ -1,11 +1,5 @@
-SRC += matrix.c
-SRC += board.c
-SRC += i2c_master.c
-
 # MCU name
 MCU = atmega328p
-PROTOCOL = VUSB
-F_CPU = 16000000
 ARCH = AVR8
 F_USB = $(F_CPU)
 
@@ -20,17 +14,6 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
 BOOTLOADER = USBasp
-
-
-# If you don't know the bootloader type, then you can specify the
-# Boot Section Size in *bytes* by uncommenting out the OPT_DEFS line
-#   Teensy halfKay      512
-#   Teensy++ halfKay    1024
-#   Atmel DFU loader    4096
-#   LUFA bootloader     4096
-#   USBaspLoader        2048
-# OPT_DEFS += -DBOOTLOADER_SIZE=4096
-
 
 # Build Options
 #   change yes to no to disable
@@ -54,3 +37,8 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no        # Enable support for HD44780 based LCDs
 
 CUSTOM_MATRIX = yes
+
+# project specific files
+SRC += matrix.c
+SRC += board.c
+QUANTUM_LIB_SRC += i2c_master.c
