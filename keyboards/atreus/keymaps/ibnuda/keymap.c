0d6c0fc8621116f2c12dd14fbf85817ac60caad1
Ibnu D. Aji <iaji@siskam.link>
2020-02-02 09:49:44 +0700

    forgot to replace some underscores and keycodes.

--- keyboards/atreus/keymaps/ibnuda/keymap.c
+++ keyboards/atreus/keymaps/ibnuda/keymap.c
@@ -134,7 +134,7 @@ combo_t key_combos[COMBO_COUNT] = {
     // left hand combinations.
     [COLON_COMMA]   = COMBO(colon_comma_combo,  KC_TAB),
     [COMMA_DOT]     = COMBO(comma_dot_combo,    KC_QUES),
-    [DOT_P]         = COMBO(dot_p_combo,        KC_UNDR),
+    [DOT_P]         = COMBO(dot_p_combo,        KC_UNDS),
     [Q_J]           = COMBO(q_j_combo,          LCTL(KC_W)),
     [J_K]           = COMBO(j_k_combo,          KC_DELT),
 
@@ -244,31 +244,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     SM_CLN, KC_COMM,KC_DOT, KC_P,   KC_Y,                       KC_F,   KC_G,   KC_C,   KC_R,   KC_L,
     SH_A,   CT_O,   AL_E,   KC_U,   GU_I,                       GU_D,   KC_H,   AL_T,   CT_N,   SH_S,
     MU_QUOT,KC_Q,   KC_J,   KC_K,   KC_X,                       KC_B,   KC_M,   KC_W,   KC_V,   MU_Z,
-    _______,_______,_______,_______,LW_BSPC,SFT_ESC,    ALT_ENT,RS_SPC, _______,_______,_______,____
+    _______,_______,_______,_______,LW_BSPC,SFT_ESC,    ALT_ENT,RS_SPC, _______,_______,_______,_______
 ),
 
 [_RAISE] = LAYOUT(
     KC_EXLM,KC_AT,  KC_UP,  KC_LCBR,KC_RCBR,                    KC_BSLS,KC_7,   KC_8,   KC_9,   KC_ASTR ,
     KC_HASH,KC_LEFT,KC_DOWN,KC_RGHT,KC_DLR,                     KC_EQL, KC_4,   KC_5,   KC_6,   KC_TILD ,
     KC_LBRC,KC_RBRC,KC_LPRN,KC_RPRN,KC_AMPR,                    KC_GRV, KC_1,   KC_2,   KC_3,   KC_PLUS ,
-    _______,_______,_______,_______,ADDDD,  _______,    ALT_ENT,RS_SPC, _______,_______,_______,____
+    _______,_______,_______,_______,ADDDD,  _______,    ALT_ENT,RS_SPC, _______,_______,_______,_______
 ),
 [_LOWER] = LAYOUT(
     KC_ESC, KC_QUES,KC_UNDS,KC_F1,  KC_F2,                      KC_F3,  KC_F4,  KC_MINS,KC_SLSH,KC_BSPC ,
     KC_LSFT,KC_TAB, KC_PGUP,KC_F5,  KC_F6,                      KC_F7,  KC_F8,  KC_HOME,KC_LALT,KC_ENT  ,
     KC_CLCK,KC_SLCK,KC_PGDN,KC_F9,  KC_F10,                     KC_F11, KC_F12, KC_END, KC_INS, KC_SLSH ,
-    _______,_______,_______,_______,ADDDD,  _______,    KC_DELT,ADDDD,  _______,_______,_______,____
+    _______,_______,_______,_______,ADDDD,  _______,    KC_DELT,ADDDD,  _______,_______,_______,_______
 ),
 [_ADJUST] = LAYOUT(
     _______,EXPLR,  KC_UP,  PRVTAB, PRVWIN,                     NXTWIN, NXTTAB, _______,_______,LCKGUI,
     TSKMGR, KC_LEFT,KC_DOWN,KC_RGHT,UPTAB,                      DNTAB,  KC_ENT, KC_LGUI,_______,CALDL,
-    _______,CLSGUI, _______,CONPST, RESET,                      _______,_______,_______,_______,____,
-    _______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,____
+    _______,CLSGUI, _______,CONPST, RESET,                      _______,_______,_______,_______,_______,
+    _______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______
 ),
 [_MUIS] = LAYOUT(
-    _______,KC_BTN2,KC_MS_U,KC_BTN1,_______,                    _______,KC_BTN1,KC_MS_U,KC_BTN2,____,
-    _______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                    _______,KC_MS_L,KC_MS_D,KC_MS_R,____,
-    _______,_______,KC_WH_D,KC_WH_U,_______,                    _______,KC_WH_U,KC_WH_D,_______,____,
-    _______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,____
+    _______,KC_BTN2,KC_MS_U,KC_BTN1,_______,                    _______,KC_BTN1,KC_MS_U,KC_BTN2,_______,
+    _______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                    _______,KC_MS_L,KC_MS_D,KC_MS_R,_______,
+    _______,_______,KC_WH_D,KC_WH_U,_______,                    _______,KC_WH_U,KC_WH_D,_______,_______,
+    _______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______
 ),
 };
