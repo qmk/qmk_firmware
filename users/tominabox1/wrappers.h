5db3d95b42b9417b8b0483614a970d720e0aaa96
tominabox1 <tom.campie@gmail.com>
2020-02-03 16:14:20 -0600

    Update wrappers.h

--- users/tominabox1/wrappers.h
+++ users/tominabox1/wrappers.h
@@ -17,7 +17,7 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
 /* Colemak */
 #define _______________COLEMAK1____________________       KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN
 #define _______________COLEMAK2____________________       KC_A,    KC_R,    KC_S,    KC_T,    KC_D, KC_H,    KC_N,    KC_E,    KC_I,    KC_O
-#define _______________COLEMAK3____________________       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_QUES
+#define _______________COLEMAK3____________________       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
 
 /* Num/Symbol Layer */
 #define __________________NUM_1____________________       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0
