828434691e3bd929e499a5aaf2b361ff85a2241c
FKML <fkml520@yahoo.co.jp>
2020-02-03 21:05:55 +0900

    Delete adjust

--- keyboards/silverbullet44/keymaps/FKML/keymap.c
+++ keyboards/silverbullet44/keymaps/FKML/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV , KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_PIPE, 
   _______, _______, _______, _______, _______, _______,                     KC_1,     KC_2,     KC_3,     KC_4,    KC_5, _______, 
   _______, _______, _______, _______, _______, _______,                     KC_6,     KC_7,     KC_8,     KC_9,    KC_0, _______, 
-                             _______, _______, _______, ADJUST,  _______, _______, _______, _______ 
+                             _______, _______, _______, _______,  _______, _______, _______, _______ 
   ),
 
 /* RAISE
