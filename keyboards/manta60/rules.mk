e8ce10df03d4a5845d87fe0bb6fbd4014939fda8
KamoNanban <35567743+KamoNanban@users.noreply.github.com>
2020-02-05 00:22:00 +0900

    Fixed rules.mk

--- keyboards/manta60/rules.mk
+++ keyboards/manta60/rules.mk
@@ -32,3 +32,9 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
 SPLIT_KEYBOARD = yes
+
+IOS_DEVICE_ENABLE = yes      # connect to IOS device (iPad,iPhone)
+
+ifeq ($(strip $(IOS_DEVICE_ENABLE)), yes)
+    OPT_DEFS += -DIOS_DEVICE_ENABLE
+endif
