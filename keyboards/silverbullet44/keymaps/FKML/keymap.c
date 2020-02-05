acfc83d0ebbf5c307c7c1d262a0971653b212bc6
swanmatch <swan_match@yahoo.co.jp>
2020-02-05 09:40:34 +0900

    I got god's keymap...hahaha:)

--- keyboards/silverbullet44/keymaps/FKML/keymap.c
+++ keyboards/silverbullet44/keymaps/FKML/keymap.c
@@ -23,9 +23,9 @@ enum layer {
 };
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
+    QWERTY = SAFE_RANGE,
+    LOWER,
+    RAISE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,14 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
  * | CMD  |   Z  |   X  |   C  |   V  |   B  |-------.-------.  ,---------------|   N  |   M  |   ,  |   .  |   /  |  -   |
  * `-----------------------------------------/       /       /   \       \      \-----------------------------------------'
-                              | CAPS  | LOWER| Space/  Bcsp /     \    .  \ Ent | RAISE|LEDOff|
-                              `----------------------------'       '--------------------------'
+ *                            | CAPS  | LOWER| Space/  Bcsp /     \    .  \ Ent | RAISE|LEDOff|
+ *                            `----------------------------'       '--------------------------'
  */
- [_QWERTY] = LAYOUT( 
-     KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                      KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_EQL,  
-    KC_LSFT,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, 
-    KC_LCMD,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                      KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS, 
-                            KC_CLCK,  LOWER, KC_SPC, KC_BSPC,    KC_DOT,  KC_ENT,  RAISE, RGB_TOG 
+  [_QWERTY] = LAYOUT(
+     KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                      KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_EQL,
+    KC_LSFT,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                      KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LCMD,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                      KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+                            KC_CLCK,  LOWER, KC_SPC, KC_BSPC,    KC_DOT,  KC_ENT,  RAISE, RGB_TOG
   ),
 
 /* LOWER
@@ -59,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                             |      |      |      /       /     \       \     |      |      |
  *                             `---------------------------'       '--------------------------'
  */
- [_LOWER] = LAYOUT( 
-  KC_GRV , KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_PIPE, 
-  _______, _______, _______, _______, _______, _______,                     KC_1,     KC_2,     KC_3,     KC_4,    KC_5, _______, 
-  _______, _______, _______, _______, _______, _______,                     KC_6,     KC_7,     KC_8,     KC_9,    KC_0, _______, 
-                             _______, _______, _______, _______,  _______, _______, _______, _______ 
+  [_LOWER] = LAYOUT(
+    KC_GRV , KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_PIPE,
+    _______, _______, _______, _______, _______, _______,                     KC_1,     KC_2,     KC_3,     KC_4,    KC_5, _______,
+    _______, _______, _______, _______, _______, _______,                     KC_6,     KC_7,     KC_8,     KC_9,    KC_0, _______,
+                               _______, _______, _______, _______,  _______, _______, _______, _______
   ),
 
 /* RAISE
@@ -74,14 +74,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
  * |      |  F11 |  F12 |      |      |      |-------.-------.  ,---------------|      |      | Left |  Up  | Down |Right |
  * `-----------------------------------------/       /       /   \       \      \-----------------------------------------'
-  *                            |      |      |      /       /     \       \     |      |      |
+ *                            |      |      |      /       /     \       \     |      |      |
  *                             `---------------------------'       '--------------------------'
  */
-    [_RAISE] = LAYOUT( 
-  _______,   KC_F1,   KC_F2,    KC_F3,   KC_F4,  KC_F5,                     _______, _______, _______, _______,  KC_MINS, KC_PLUS, 
-  _______,   KC_F6,   KC_F7,    KC_F8,   KC_F9,  KC_F10,                    _______, _______, _______, _______,  KC_LBRC, KC_RBRC, 
-  _______,  KC_F11,   KC_F12, _______, _______, _______,                    _______, _______, KC_LEFT,   KC_UP,  KC_DOWN, KC_RGHT, 
-                              _______, _______, _______, _______,  _______, _______, _______, _______ 
+  [_RAISE] = LAYOUT(
+    _______,   KC_F1,   KC_F2,    KC_F3,   KC_F4,  KC_F5,                     _______, _______, _______, _______,  KC_MINS, KC_PLUS,
+    _______,   KC_F6,   KC_F7,    KC_F8,   KC_F9,  KC_F10,                    _______, _______, _______, _______,  KC_LBRC, KC_RBRC,
+    _______,  KC_F11,   KC_F12, _______, _______, _______,                    _______, _______, KC_LEFT,   KC_UP,  KC_DOWN, KC_RGHT,
+                                _______, _______, _______, _______,  _______, _______, _______, _______
   ),
 
 /*   ADJUST
@@ -94,13 +94,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------/       /       /   \       \      \-----------------------------------------'
  *                             |      |      |      /       /     \       \     |      |      |
  *                             `---------------------------'       '--------------------------'
-*/
+ */
 
- [_ADJUST] = LAYOUT( 
-  _______, XXXXXXX, XXXXXXX, CK_RST,  XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, _______, 
-  XXXXXXX, XXXXXXX, MU_TOG,  CK_UP,   AU_TOG,  XXXXXXX,                      RGB_SPI, RGB_MOD,  RGB_VAI, RGB_SAI, RGB_HUI, XXXXXXX, 
-  XXXXXXX, XXXXXXX, MU_MOD,  CK_DOWN, XXXXXXX, XXXXXXX,                      RGB_SPD, RGB_RMOD, RGB_VAD, RGB_SAD, RGB_HUD, XXXXXXX, 
-                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX 
+ [_ADJUST] = LAYOUT(
+    _______, XXXXXXX, XXXXXXX, CK_RST,  XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    XXXXXXX, XXXXXXX, MU_TOG,  CK_UP,   AU_TOG,  XXXXXXX,                      RGB_SPI, RGB_MOD,  RGB_VAI, RGB_SAI, RGB_HUI, XXXXXXX,
+    XXXXXXX, XXXXXXX, MU_MOD,  CK_DOWN, XXXXXXX, XXXXXXX,                      RGB_SPD, RGB_RMOD, RGB_VAD, RGB_SAD, RGB_HUD, XXXXXXX,
+                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   )
 };
 
@@ -112,33 +112,33 @@ extern rgblight_config_t rgblight_config;
 int RGB_current_mode;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-      case LOWER:
-        if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        }
-        return false;
-        break;
-      case RAISE:
-        if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        }
-        return false;
-        break;
-  }
-  return true;
+    switch (keycode) {
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+    }
+    return true;
 }
 
 void matrix_init_user(void) {
-  #ifdef RGBLIGHT_ENABLE
+#ifdef RGBLIGHT_ENABLE
     RGB_current_mode = rgblight_config.mode;
-  #endif
+#endif
 }
