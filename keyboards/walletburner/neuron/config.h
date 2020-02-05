430cfbce4861253ed31abea79de7b50df3602aa6
George Koenig <george.g.koenig@gmail.com>
2020-02-05 13:36:21 -0500

    One last tweak to correct MANUFACTURER

--- keyboards/walletburner/neuron/config.h
+++ keyboards/walletburner/neuron/config.h
@@ -4,9 +4,9 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define PRODUCT_ID      0x62AC
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    worldspawn
+#define MANUFACTURER    Walletburner
 #define PRODUCT         Neuron
 #define DESCRIPTION     Keyboard
 
