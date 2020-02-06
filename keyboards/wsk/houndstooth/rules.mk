1271e0a1ffc90631c281a810e98bd0616dbec1b5
George Koenig <george.g.koenig@gmail.com>
2020-02-06 14:06:45 -0500

    Correct RGBLIGHT_ENABLE to no

--- keyboards/wsk/houndstooth/rules.mk
+++ keyboards/wsk/houndstooth/rules.mk
@@ -23,4 +23,4 @@ SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes      # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
