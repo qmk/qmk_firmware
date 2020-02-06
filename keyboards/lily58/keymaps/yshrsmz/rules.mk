94bd4ff75531ae2f56b1fdbae367b9637def23f5
yshrsmz <the.phantom.bane@gmail.com>
2020-02-07 01:24:10 +0900

    remove more backslashes

--- keyboards/lily58/keymaps/yshrsmz/rules.mk
+++ keyboards/lily58/keymaps/yshrsmz/rules.mk
@@ -17,11 +17,11 @@ RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
-        ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+SRC += ./lib/glcdfont.c
+SRC += ./lib/rgb_state_reader.c
+SRC += ./lib/layer_state_reader.c
+SRC += ./lib/logo_reader.c
+SRC += ./lib/keylogger.c
+#SRC += ./lib/mode_icon_reader.c
+#SRC += ./lib/host_led_state_reader.c
+#SRC += ./lib/timelogger.c
