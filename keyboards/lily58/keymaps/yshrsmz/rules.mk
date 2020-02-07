2d207aa188272bedb4847a054984caeedc33f67a
Yasuhiro SHIMIZU <the.phantom.bane@gmail.com>
2020-02-07 14:23:02 +0900

    update rules.mk

--- keyboards/lily58/keymaps/yshrsmz/rules.mk
+++ keyboards/lily58/keymaps/yshrsmz/rules.mk
@@ -1,27 +1,11 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 
 # If you want to change the display of OLED, you need to change here
-SRC += ./lib/glcdfont.c
-SRC += ./lib/rgb_state_reader.c
-SRC += ./lib/layer_state_reader.c
-SRC += ./lib/logo_reader.c
-SRC += ./lib/keylogger.c
-#SRC += ./lib/mode_icon_reader.c
-#SRC += ./lib/host_led_state_reader.c
-#SRC += ./lib/timelogger.c
+SRC +=  ./lib/glcdfont.c \
+        ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
