a1a88b8ac778b2b22be127eb2409ae7b338f7e7e
worldspawn00 <mcmancuso@gmail.com>
2020-02-07 21:34:37 -0600

    [Keyboard] adding keyboard: gothic70 (#7982)
    * gothic70
    
    * Update keyboards/wsk/gothic70/readme.md
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/wsk/gothic70/readme.md
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/wsk/gothic70/readme.md
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/wsk/gothic70/rules.mk
    
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>
    
    * Update keyboards/wsk/gothic70/rules.mk
    
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>
    
    * Update keyboards/wsk/gothic70/rules.mk
    
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>
    
    * Update keyboards/wsk/gothic70/rules.mk
    
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>
    
    * Update keyboards/wsk/gothic70/rules.mk
    
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>
    
    * Update keyboards/wsk/gothic70/rules.mk
    
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>
    
    * Update keyboards/wsk/gothic70/config.h
    
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/config.h
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Update keyboards/wsk/gothic70/keymaps/default/keymap.c
    
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
    
    * Committed review suggestions
    
    * Small indicator LED layer function tweak
    
    * Fixing layout for configurator
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>
    Co-authored-by: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>

---
+++ keyboards/wsk/gothic70/config.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x63CC
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Worldspawn00
+#define PRODUCT         Gothic70
+#define DESCRIPTION     Expanded Ergo Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* key matrix pins */
+
+#define MATRIX_ROW_PINS { D0, D1, D2, D3, D5 }
+#define MATRIX_COL_PINS { F0, F1, E6, C7, C6, B6, D4, B1, B0, B7, B5, B4, D7, D6, B3 }
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+#define RGB_DI_PIN F4
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 7
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#endif
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
