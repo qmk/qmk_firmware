1ee32431e2f78848ef9d793f79d50ea501cbe8a9
eswai <>
2020-02-08 18:03:41 +0100

    LTO

--- keyboards/eswai/anhedral67/keymaps/naginata_v12/rules.mk
+++ keyboards/eswai/anhedral67/keymaps/naginata_v12/rules.mk
@@ -1,7 +1,9 @@
 BOOTLOADER = qmk-dfu
 COMBO_ENABLE = yes
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+# RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 UNICODE_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 
 SRC +=  naginata_v12.c
+
+EXTRAFLAGS += -flto
