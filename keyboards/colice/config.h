b312799aaa4ba0f875a589df6c1ef587bbe6e8c4
Takeshi Nishio <kkeennnn@gmail.com>
2020-02-09 22:25:59 +0900

    Update スプリットキーボード対応

--- keyboards/colice/config.h
+++ keyboards/colice/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 5
+#define MATRIX_ROWS 5*2 // Doubled for split keyboard
 #define MATRIX_COLS 10
 
 /*
@@ -51,7 +51,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D1  // or D1, D2, D3, E6
+#define SOFT_SERIAL_PIN D2  // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
--- keyboards/colice/keymaps/default/keymap.c
+++ keyboards/colice/keymaps/default/keymap.c
@@ -29,45 +29,37 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   STR_LOWER,
-  STR_RAISE
+  STR_RAISE,
+  STR_ADJUST
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    /* 動作確認済み
     [_QWERTY] = LAYOUT(
-            KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,           KC_PSLS,KC_PAST,KC_PMNS, \
-                    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS, \
-                    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, \
-                    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV, KC_P1,  KC_P2,  KC_P3,  KC_PENT, \
-      KC_ESC, KC_TAB, KC_LCTL,KC_LSFT,LOWER,  KC_LALT,KC_LGUI,KC_SPC, KC_P0,          KC_PDOT \
+      KC_ESC, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,               KC_MUTE,KC_VOLU,KC_VOLD,                 KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC, \
+              KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,       KC_P7,  KC_MRWD,KC_MPLY,KC_MFFD,                 KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS, \
+              KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,       KC_P4,  KC_P5,  KC_P6,  KC_PPLS,                 KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT, \
+              KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LOWER,      KC_P1,  KC_P2,  KC_P3,  KC_PENT,         RAISE,  KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_UP,  KC_RSFT, \
+              RAISE,                  KC_LALT,KC_LGUI,KC_SPC,             KC_P0,          KC_PDOT,                 LOWER,  KC_SPC, KC_LGUI,KC_LALT,KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     [_LOWER] = LAYOUT(
-            KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,           KC_PSLS,KC_PAST,KC_PMNS, \
-                    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS, \
-                    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, \
-                    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV, KC_P1,  KC_P2,  KC_P3,  KC_PENT, \
-      KC_ESC, KC_TAB, KC_LCTL,KC_LSFT,_______,  KC_LALT,KC_LGUI,KC_SPC, KC_P0,          KC_PDOT \
-    )*/
-    [_QWERTY] = LAYOUT(
-      KC_ESC, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,           KC_PSLS,KC_PAST,KC_PMNS, \
-              KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS, \
-              KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, \
-              KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   RAISE,  KC_P1,  KC_P2,  KC_P3,  KC_PENT, \
-              LOWER,                  KC_LALT,KC_LGUI,KC_SPC, KC_P0,          KC_PDOT \
-    ),
-    [_LOWER] = LAYOUT(
-      STR_LOWER,  KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,           KC_PSLS,KC_PAST,KC_PMNS, \
-              KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS, \
-              KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, \
-              KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   RAISE,  KC_P1,  KC_P2,  KC_P3,  KC_PENT, \
-              _______,                KC_LALT,KC_LGUI,KC_SPC, KC_P0,          KC_PDOT \
+    STR_LOWER,_______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,         _______,_______,_______,                    KC_F7,  KC_F8,  KC_F9,KC_F10,   KC_F11, KC_F12, KC_DEL, \
+              _______,_______,_______,LCTL(KC_E),_______,_______,_______,    _______,_______,_______,_______,            _______,_______,_______,_______,KC_UP,  _______,_______,_______, \
+              _______,LCTL(KC_A),_______,KC_DEL, KC_RGHT,KC_ESC, KC_BSPC,    _______,_______,_______,_______,            KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,_______,_______, \
+              _______,_______,_______,_______,_______,KC_LEFT,_______,       _______,_______,_______,_______,    _______,KC_DOWN,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,_______, \
+              _______,                _______,_______,_______,               _______,        _______,            _______,_______,_______,_______,_______,_______,_______ \
     ),
     [_RAISE] = LAYOUT(
-      STR_RAISE,  KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,           KC_PSLS,KC_PAST,KC_PMNS, \
-              KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_P7,  KC_P8,  KC_P9,  KC_PMNS, \
-              KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_P4,  KC_P5,  KC_P6,  KC_PPLS, \
-              KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   RAISE,  KC_P1,  KC_P2,  KC_P3,  KC_PENT, \
-              _______,                KC_LALT,KC_LGUI,KC_SPC, KC_P0,          KC_PDOT \
+      STR_RAISE, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,               KC_PSLS,KC_PAST,KC_PMNS,              KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC, \
+              KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,          KC_P7,  KC_P8,  KC_P9,  KC_PMNS,                 KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS, \
+              KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,          KC_P4,  KC_P5,  KC_P6,  KC_PPLS,                 KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT, \
+              KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   _______,       KC_P1,  KC_P2,  KC_P3,  KC_PENT,         _______,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_UP,  KC_RSFT, \
+              _______,                  KC_LALT,KC_LGUI,KC_SPC,              KC_P0,          KC_PDOT,                 _______,KC_SPC, KC_LGUI,KC_LALT,KC_LEFT,KC_DOWN,KC_RGHT \
+    ),
+    [_ADJUST] = LAYOUT(
+      STR_ADJUST, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,              KC_PSLS,KC_PAST,KC_PMNS,             KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC, \
+              KC_TAB, KC_Q,   KC_W,   KC_E,   RESET,   KC_T,   KC_Y,          KC_P7,  KC_P8,  KC_P9,  KC_PMNS,                 KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS, \
+              KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,          KC_P4,  KC_P5,  KC_P6,  KC_PPLS,                 KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT, \
+              KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   _______,       KC_P1,  KC_P2,  KC_P3,  KC_PENT,         _______,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_UP,  KC_RSFT, \
+              _______,                  KC_LALT,KC_LGUI,KC_SPC,              KC_P0,          KC_PDOT,                 _______,KC_SPC, KC_LGUI,KC_LALT,KC_LEFT,KC_DOWN,KC_RGHT \
     )
 };
 
@@ -95,6 +87,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // when keycode QMKBEST is released
       }
       break;
+      case STR_ADJUST:
+        if (record->event.pressed) {
+          // when keycode QMKBEST is pressed
+          SEND_STRING("Adjust");
+        } else {
+          // when keycode QMKBEST is released
+        }
+        break;
       case LOWER:
       if (record->event.pressed) {
         lower_pressed = true;
