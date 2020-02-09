37a69de0a590da213d511651bad6cc6b6c72ea91
Takeshi Nishio <kkeennnn@gmail.com>
2020-02-09 22:25:59 +0900

    Update スプリットキーボード対応

--- keyboards/colice/rules.mk
+++ keyboards/colice/rules.mk
@@ -24,6 +24,8 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
+SPLIT_KEYBOARD = yes
+
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
