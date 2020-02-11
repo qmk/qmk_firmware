2e8050ce3304636b82faecd8859610e52787adb1
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2020-02-11 18:10:39 +0100

    breaking more stuff

--- lib/python/qmk/cli/pytest.py
+++ lib/python/qmk/cli/pytest.py
@@ -3,8 +3,8 @@
 QMK script to run unit and integration tests against our python code.
 """
 import subprocess
-#import os
-#import sys
+import os
+import sys
 
 from milc import cli
 
@@ -13,9 +13,9 @@ from milc import cli
 def pytest(cli):
     """Run several linting/testing commands.
     """
-    #parrent_env = os.environ.copy()
-    #parrent_env["PYTHONPATH"] = ":".join(sys.path[1:])
+    parrent_env = os.environ.copy()
+    parrent_env["PYTHONPATH"] = ":".join(sys.path[1:])
 
     flake8 = subprocess.run(['flake8', 'lib/python', 'bin/qmk'])
-    nose2 = subprocess.run(['nose2', '-v'])#, env=parrent_env)
+    nose2 = subprocess.run(['nose2', '-v'], env=parrent_env)
     return flake8.returncode | nose2.returncode
