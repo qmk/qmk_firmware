8fa4ab6eba8caf0c0e71a493d2b0e5f289e9c745
zvecr <git@zvecr.com>
2020-02-11 23:03:11 +0000

    rename default f072 board file to GENERIC_STM32_F072XB

---
+++ drivers/boards/GENERIC_STM32_F072XB/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = $(CHIBIOS)/os/hal/boards/GENERIC_STM32_F072XB/board.c
+
+# Required include directories
+BOARDINC = $(CHIBIOS)/os/hal/boards/GENERIC_STM32_F072XB
--- drivers/boards/STM32_F103_STM32DUINO/board.mk
+++
@@ -1,5 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/boards/STM32_F103_STM32DUINO/board.c
-
-# Required include directories
-BOARDINC = $(BOARD_PATH)/boards/STM32_F103_STM32DUINO
