8a2cb6c481fb705a3282e7933af076ab281a847e
Rys Sommefeldt <rys@sommefeldt.com>
2020-02-11 17:14:31 +0000

    Update Think6.5 PRODUCT and remove VIA JSON defs

--- keyboards/gray_studio/think65/hotswap/config.h
+++ keyboards/gray_studio/think65/hotswap/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x4001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Graystudio
-#define PRODUCT         Graystudio Think6.5 Hotswap
+#define PRODUCT         Think6.5° Hotswap
 #define DESCRIPTION     Graystudio Think6.5 Hotswap
 
 /* key matrix size */
--- keyboards/gray_studio/think65/solder/config.h
+++ keyboards/gray_studio/think65/solder/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x4000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Graystudio
-#define PRODUCT         Graystudio Think6.5
+#define PRODUCT         Think6.5°
 #define DESCRIPTION     Graystudio Think6.5
 
 /* key matrix size */
