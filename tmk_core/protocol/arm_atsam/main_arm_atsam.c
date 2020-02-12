2f13946a2ec92daa69dadf741ef6cfac7cbb45c3
fauxpark <fauxpark@gmail.com>
2020-02-12 23:09:25 +1100

    Improvements to extrakey HID descriptors

--- tmk_core/protocol/arm_atsam/main_arm_atsam.c
+++ tmk_core/protocol/arm_atsam/main_arm_atsam.c
@@ -130,7 +130,6 @@ void send_extra(uint8_t report_id, uint16_t data) {
 
 void send_system(uint16_t data) {
 #ifdef EXTRAKEY_ENABLE
-    if (data != 0) data = data - SYSTEM_POWER_DOWN + 1;
     send_extra(REPORT_ID_SYSTEM, data);
 #endif  // EXTRAKEY_ENABLE
 }
