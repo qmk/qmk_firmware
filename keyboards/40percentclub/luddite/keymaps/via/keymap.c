f98d86fb8d3a62230c90acef77e4418acda2fd2c
morbetter <60626808+morbetter@users.noreply.github.com>
2020-02-13 08:28:29 -0800

    Removing block and comment as suggested

--- keyboards/40percentclub/luddite/keymaps/via/keymap.c
+++ keyboards/40percentclub/luddite/keymaps/via/keymap.c
@@ -1,14 +1,5 @@
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_60_ansi(
     KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,
