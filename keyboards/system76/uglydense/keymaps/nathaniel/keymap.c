cccab92a1fab0b84f031207126101fa730db651d
leviport <leviport377@gmail.com>
2020-02-14 13:51:55 -0700

    Add nathaniel & shpurk layouts

--- keyboards/system76/uglydense/keymaps/nathaniel/keymap.c
+++ keyboards/system76/uglydense/keymaps/nathaniel/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Layer 0, qwerty layer
+  /* Layer 0, default layer
 ___________________________________________________________________________________________________________________________________
 |        |        |        |        |        |        |        |        |        |        |        |        |        |            |
 |  ESC   |   F1   |   F2   |   F3   |   F4   |   F5   |   F6   |   F7   |   F8   |   F9   |  F10   |  F11   |  F12   |  DELETE    |
@@ -20,7 +20,7 @@ ________________________________________________________________________________
       |   SHIFT    |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    | SHIFT  |   UP   |
   ____|____________|________|________|________|________|________|________|________|________|________|________|________|________|_________
   |            |        |       |        |                 |                 |        |        |             |        |        |        |
-  |    CTRL    |  FN    | LGUI  | LALT   |    SPACE        |   Backspace     | RCTRL  |  RALT  |     FN      |  LEFT  |  DOWN  | RIGHT  |
+  |    LGUI    |  FN    | LCTL  | LALT   |    SPACE        |      SPACE      | RCTRL  |  RALT  |     FN      |  LEFT  |  DOWN  | RIGHT  |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|________|________|________|
 */
 
@@ -30,7 +30,7 @@ ________________________________________________________________________________
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
           KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
               KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-          KC_LCTL, MO(1),   KC_LGUI, KC_LALT,     KC_SPC,            KC_BSPC,     KC_RCTL,   KC_RALT,   MO(1),     KC_LEFT, KC_DOWN, KC_RIGHT
+          KC_LGUI, MO(1),   KC_LCTL, KC_LALT,     KC_SPC,           KC_SPC,     KC_RCTL,   KC_RALT,   MO(1),     KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
     /* Layer 1, function layer
@@ -42,28 +42,28 @@ ________________________________________________________________________________
 |        |        |        |        |        |        |        |        |        |        |        |        |        |            |
 |________|________|________|________|________|________|________|________|________|________|________|________|________|____________|
 |            |        |        |        |        |        |        |        |        |        |        |        |        |        |
-|            |  Home  |   Up   |  End   |  PgUp  |        |  PgUp  |  Home  |   Up   |  End   |        |        |        |        |
+|            |        |        |        |        |        |        |        |        |        |        |        |        |        |
 |____________|________|________|________|________|________|________|________|________|________|________|________|________|________|
   |            |        |        |        |        |        |        |        |        |        |        |        |            |
-  |            |  Left  |  Down  |  Right |  PgDn  |        |  PgDn  |  Left  |  Down  |  Right |        |        |            |
+  |            |        |        |        |        |        |        |        |        |        |        |        |            |
   |____________|________|________|________|________|________|________|________|________|________|________|________|____________|
       |            |        |        |        |        |        |        |        |        |        |        |        |        |
-      |            |PlayPaus| Prev   | Next   | VolDn  | VolUp  | VolMute|        |        |        |        |        |  PgUp  |
+      |            |        |        |        |        |        |        |        |        |        |        |        |  PgUp  |
   ____|____________|________|________|________|________|________|________|________|________|________|________|________|________|_________
   |            |        |       |        |                 |                 |        |        |             |        |        |        |
-  |            |        |       |        |    Backspace    |                 |        |        |             |  Home  |  PgDn  |  End   |
+  |            |        |       |        |                 |                 |        |        |             |  Home  |  PgDn  |  End   |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|________|________|________|
 * 'RESET' resets the controller and puts the board into firmware flashing mode. If this key is hit accidentally, just unplug the board
 *        and plug it back in.
 */
 
   [1] = LAYOUT(
-      RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,   TO(0),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-          KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
-              KC_TRNS, KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PGUP,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_BSPC,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,     KC_HOME,  KC_PGDN, KC_END
+    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PGUP,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,     KC_HOME,  KC_PGDN, KC_END
   )
 };
 
