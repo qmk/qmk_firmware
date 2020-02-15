72f4c761ff7e65aa4aa096e657ac8f44a0d39d44
mechmerlin <mechmerlin@gmail.com>
2020-02-15 07:53:25 -0800

    update super16 vid and pid

--- keyboards/1upkeyboards/super16/config.h
+++ keyboards/1upkeyboards/super16/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x2010
+#define VENDOR_ID       0x6F75 // OU
+#define PRODUCT_ID      0x5355 // SU
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    1upkeyboards
 #define PRODUCT         super16
