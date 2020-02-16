b589034b9175a837dc47644650278ee258975e38
buzzlighter1 <mikhail.evtushenko@gmail.com>
2020-02-16 22:26:13 +0300

    Update keyboards/xd75/keymaps/buzzlighter1/keymap.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/xd75/keymaps/buzzlighter1/keymap.c
+++ keyboards/xd75/keymaps/buzzlighter1/keymap.c
@@ -97,7 +97,7 @@ void led_set_user(uint8_t usb_led) {
 
 // Runs whenever there is a layer state change.
 layer_state_t layer_state_set_user(layer_state_t state) {
-  uint8_t layer = biton32(state);
+  uint8_t layer = get_highest_layer(state);
     
   gp100_led_off();
   //gp103_led_off();
