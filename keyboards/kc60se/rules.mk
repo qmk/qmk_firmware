2f9c5657f49f848821f9c87488fc5b040ba6c38d
Blake C. Lewis <blake.lewis@gmail.com>
2020-02-15 19:02:51 -0600

    Update keyboards/kc60se/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/kc60se/rules.mk
+++ keyboards/kc60se/rules.mk
@@ -19,4 +19,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
-LAYOUTS = all 60_ansi 60_ansi_split_bs_rshift
+LAYOUTS = 60_ansi 60_ansi_split_bs_rshift
