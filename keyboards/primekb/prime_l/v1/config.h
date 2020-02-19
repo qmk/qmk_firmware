42d6270f28831e95d1cb9c14a7423d5b1d864d67
holtenc <holtenc@gmail.com>
2020-02-19 13:12:11 -0600

    Merge /prime_l and /prime_l_v2 (#8194)
    * correct indicator light states.
    
    function of indicator lights was inverted. these changes correct that.
    
    * flesh out keymaps pre production
    
    * Enable extrakey in rules
    
    * Prime_BLE initial commit
    
    * Initial commit for Prime_L V2
    
    * Update info.json
    
    correct key spacing.
    
    * update copyright
    
    * Update readme.md
    
    * Inital commit
    
    * updates before PR into QMK master
    
    * Drop Prime_EXL Plus from PR. Make requested changes to Prime_L V2
    
    * Rename keyboards/primekb/Prime_l_v2/config.h to keyboards/primekb/prime_l_v2/config.h
    
    * Rename keyboards/primekb/prime_l_v2/config.h to keyboards/primekb/Prime_l_v2/config.h
    
    * remove directory Prime_l_v2
    
    * re-submit with proper folder name.
    
    * Restructure /primekb directory to merge /prime_l and /prime_l_v2
    
    * made changes requested by QMK reviewers
    
    * Update keyboards/primekb/prime_l/v1/readme.md
    
    * Update keyboards/primekb/prime_l/v1/readme.md
    
    * Update keyboards/primekb/prime_l/v1/readme.md

--- keyboards/primekb/prime_l/v1/config.h
+++ keyboards/primekb/prime_l/v1/config.h
@@ -21,22 +21,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x4024
-#define MANUFACTURER    PrimeKB
-#define DESCRIPTION     Custom layout keyboard
+#define DEVICE_VER      0x0001
+#define PRODUCT         Prime_L V1
 
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 16
+/* Keyboard Matrix Assignments */
+#define MATRIX_ROW_PINS { D1, D0, B7, B3, B2 }
+#define MATRIX_COL_PINS { D2, D3, D5, D4, D6, D7, B4, B5, C7, C6, F7, F6, F5, F4, F1, F0 }
+#define UNUSED_PINS
 
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+#define BACKLIGHT_PIN B6
+/*#define BACKLIGHT_BREATHING*/
+#define BACKLIGHT_LEVELS 4
