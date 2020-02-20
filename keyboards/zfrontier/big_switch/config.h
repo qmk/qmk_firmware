026d49f2fe756659452a222a3ece1362e42e935f
carlxia <xiacarl@gmail.com>
2020-02-20 11:59:05 +0800

    Update keyboards/zfrontier/big_switch/config.h
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/zfrontier/big_switch/config.h
+++ keyboards/zfrontier/big_switch/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID 0xFEED
 #define PRODUCT_ID 0x0A01
 #define DEVICE_VER 0x0005
-#define MANUFACTURER zfrontier
+#define MANUFACTURER zFrontier
 #define PRODUCT S01_Gaint_Key
 #define DESCRIPTION Big Switch
 
@@ -64,4 +64,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* long tapping config */
 #define TAPPING_TERM 500
-
