af5cf98064655c6ff0d02af952828c81e68cfb32
Andrew Kannan <andrew.kannan@gmail.com>
2020-02-20 02:23:40 -0500

    backlight enable

--- keyboards/cannonkeys/db60/rules.mk
+++ keyboards/cannonkeys/db60/rules.mk
@@ -15,7 +15,7 @@ COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
 CUSTOM_MATRIX = no # Custom matrix file
-BACKLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 WS2812_DRIVER = spi
 
