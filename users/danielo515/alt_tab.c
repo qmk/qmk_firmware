fa7ee919189452ebc270a0021d012b488e7e022f
Danielo Rodriguez <rdanielo@gmail.com>
2020-02-23 16:39:00 +0100

    refactor: early return

--- users/danielo515/alt_tab.c
+++ users/danielo515/alt_tab.c
@@ -1,5 +1,6 @@
 #include "danielo515.h"
 #include "alt_tab.h"
+
 bool                       altPressed = false;
 __attribute__((weak)) void alt_tab_activated(void){};
 __attribute__((weak)) void alt_tab_deactivated(void){};
@@ -9,17 +10,17 @@ extern bool                onMac;
 bool process_alt_tab(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case ALT_TAB:
-            if (record->event.pressed) {
-                if (altPressed) {
-                    tap_code(KC_TAB);
-                } else {
-                    altPressed = true;
-                    onMac ? register_code(KC_LGUI) : register_code(KC_LALT);
-                    tap_code(KC_TAB);
-                    alt_tab_activated();
-                }
+            if (!record->event.pressed) {
+                return false;
             }
-            return false;
+            if (altPressed) {
+                tap_code(KC_TAB);
+            } else {
+                altPressed = true;
+                onMac ? register_code(KC_LGUI) : register_code(KC_LALT);
+                tap_code(KC_TAB);
+                alt_tab_activated();
+            }
         // avoid alt releasing if the key is of movement
         case KC_RIGHT ... KC_UP:
             if (altPressed) {
