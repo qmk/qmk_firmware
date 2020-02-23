a96665a7c3107fb50dae00b26146820a6f3801eb
ai03 <sarcaphx510@gmail.com>
2020-02-22 17:15:40 -0800

    Update keyboards/ai03/equinox/keymaps/proto_via/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/ai03/equinox/keymaps/proto_via/keymap.c
+++ keyboards/ai03/equinox/keymaps/proto_via/keymap.c
@@ -22,7 +22,7 @@
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
+  [0] = LAYOUT_all( /* Base */
     MO(2),   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, 
     MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, 
     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, 
@@ -47,4 +47,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,          _______,          _______,     _______,      _______, _______, _______
   )
 };
-
