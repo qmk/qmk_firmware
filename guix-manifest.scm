8a9757cdba0509bff59becf7b08f72a54893709f
Ekaitz Zárraga <ekaitz@elenq.tech>
2020-02-23 20:22:49 +0100

    Add Guix manifest
    Add guix manifest to make guix environments like `guix environment --manifest=guix-manifest.scm`
    
    - It's not configurable at the moment.
    - AVR-Toolchain is 4.9 version because 5.5 package is broken, see [help-guix mailing list](https://lists.gnu.org/archive/html/help-guix/2020-02/msg00195.html)

---
+++ guix-manifest.scm
@@ -0,0 +1,10 @@
+(specifications->manifest
+  '("unzip"
+    "zip"
+    "git"
+    "wget"
+    "dfu-programmer"
+    "dfu-util"
+    "avr-toolchain@4.9"
+    "gcc-toolchain"
+    "arm-none-eabi-toolchain"))
