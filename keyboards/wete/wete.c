e375af184b2bdc5450c62b7aa7a286a2d5e9c480
Ramon Imbao <ramonimbao@gmail.com>
2020-02-23 16:39:27 +0800

    [Keyboard] Add Wete

---
+++ keyboards/wete/wete.c
@@ -0,0 +1,39 @@
+#include "wete.h"
+#include "print.h"
+#include "debug.h"
+
+#include "ch.h"
+#include "hal.h"
+
+backlight_config_t kb_backlight_config = {
+    .enable = true,
+    .breathing = true,
+    .level = BACKLIGHT_LEVELS
+};
+
+void keyboard_pre_init_user(void) {
+    // Initialize indicator LED pins
+    setPinOutput(A14); // Num Lock
+    setPinOutput(A15); // Scroll Lock
+    setPinOutput(B3); // Caps Lock
+}
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if (res) {
+        writePin(A14, !led_state.num_lock);
+        writePin(A15, !led_state.scroll_lock);
+        writePin(B3, !led_state.caps_lock);
+    }
+
+    return res;
+}
+
+void matrix_init_kb(void) {
+    backlight_init_ports();
+    matrix_init_user();
+}
+
+void matrix_scan_kb(void) {
+    matrix_scan_user();
+}
