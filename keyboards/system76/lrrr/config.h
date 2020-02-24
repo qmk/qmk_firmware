780c951b360a7cff1cba07bcc052a6e3b73616ab
leviport <leviport377@gmail.com>
2020-02-24 15:41:27 -0700

    Add Lrrr keyboard, ruler of Omicron Persei 8

---
+++ keyboards/system76/lrrr/config.h
@@ -0,0 +1,35 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x1776
+#define PRODUCT_ID      0x1776
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    System76
+#define PRODUCT         Lrrr
+#define DESCRIPTION     Lrrr Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 15
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { F5, F4, F3, F2, B3, B1 }
+#define MATRIX_COL_PINS { B2, B0, D2, D3, D1, D0, D4, C6, D7, E6, B4, B5, B6, B7, D6 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#endif // CONFIG_H
