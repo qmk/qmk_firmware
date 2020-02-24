d06d03b7d10aa460ea64490d4b1cd403cc7a3e34
ai03 <sarcaphx510@gmail.com>
2020-02-23 17:28:42 -0800

    Update keyboards/ai03/equinox/keymaps/proto_via/rules.mk
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/ai03/equinox/keymaps/proto_via/rules.mk
+++ keyboards/ai03/equinox/keymaps/proto_via/rules.mk
@@ -1,5 +1,5 @@
 VIA_ENABLE = yes
 
 # Fix for prototype missing COL0, COL1, using backlight and RGB underglow I/O pins
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
