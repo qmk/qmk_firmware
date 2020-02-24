4dbf2fa02fed2de048ec1fb63ab0480dd4c4d6e6
moyi4681 <moyi4681@users.noreply.github.com>
2020-02-24 08:53:02 +0800

    Update keyboards/miller/gm862/gm862.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/miller/gm862/gm862.c
+++ keyboards/miller/gm862/gm862.c
@@ -99,7 +99,7 @@ void suspend_wakeup_init_kb(void) {
 __attribute__ ((weak))
 void rgb_matrix_indicators_user(void)
 {
-    if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK))
+    if (host_keyboard_led_state().caps_lock)
     {
         rgb_matrix_set_color(28, 0xFF, 0xFF, 0xFF);
     }
