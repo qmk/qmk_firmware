6d183806ec83f6961828913fc19beb011b736d26
Dustin L. Howett <dustin@howett.net>
2020-02-25 08:05:18 -0800

    Keyboard: Remove meaningless weak redefinitions from frosty
    These are not necessary (and all of them already live somewhere in
    Quantum).

--- keyboards/bpiphany/frosty_flake/frosty_flake.c
+++ keyboards/bpiphany/frosty_flake/frosty_flake.c
@@ -11,27 +11,6 @@ void keyboard_pre_init_kb() {
     keyboard_pre_init_user();
 }
 
-void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
-
-    matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
-
-    matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    // put your per-action keyboard code here
-    // runs for every action, just before processing by the firmware
-
-    return process_record_user(keycode, record);
-}
-
 bool led_update_kb(led_t usb_led) {
     // user requests no further processing
     if (!led_update_user(usb_led))
@@ -41,27 +20,5 @@ bool led_update_kb(led_t usb_led) {
     writePin(B7, !usb_led.num_lock);
     writePin(C6, !usb_led.scroll_lock);
 
-    return true;
-}
-
-__attribute__ ((weak))
-void matrix_init_user(void) {
-}
-
-__attribute__ ((weak))
-void matrix_scan_user(void) {
-}
-
-__attribute__ ((weak))
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-__attribute__ ((weak))
-void led_set_user(uint8_t usb_led) {
-}
-
-__attribute__ ((weak))
-bool led_update_user(led_t usb_led) {
     return true;
 }
