71e702bea91548f21e697374a01ef39acad1780b
leviport <leviport377@gmail.com>
2020-02-25 08:39:24 -0700

    Update Lrrr it uses Caterina bootloader Also B1 wasn't working for Row 6, so I changed that to F6

--- keyboards/system76/lrrr/config.h
+++ keyboards/system76/lrrr/config.h
@@ -16,8 +16,8 @@
 #define MATRIX_COLS 15
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { F5, F4, F3, F2, B3, B1 }
-#define MATRIX_COL_PINS { B2, B0, D2, D3, D1, D0, D4, C6, D7, E6, B4, B5, B6, B7, D6 }
+#define MATRIX_ROW_PINS { F5, F4, F1, F0, B3, F6 }
+#define MATRIX_COL_PINS { B2, B0, D3, D2, D1, D0, D4, C6, D7, E6, B4, B5, B6, B7, D6 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
