99038cfbea295058dd460fc492d9a58cd7f35935
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-02-26 01:42:44 -0300

    give us some more time to press esc

--- keyboards/lily58/keymaps/rhruiz/config.h
+++ keyboards/lily58/keymaps/rhruiz/config.h
@@ -32,8 +32,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-#undef TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_FORCE_HOLD
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
