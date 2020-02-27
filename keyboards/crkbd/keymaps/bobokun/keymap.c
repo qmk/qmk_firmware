9b95e331aa9b60793cd16a9a223958989439ddaa
bobokun <jon.cy.lee98@gmail.com>
2020-02-26 21:52:08 -0500

    Git Update Script

--- keyboards/crkbd/keymaps/bobokun/keymap.c
+++ keyboards/crkbd/keymaps/bobokun/keymap.c
@@ -36,8 +36,6 @@ static const char PROGMEM code_to_name[0xFF] = {
 void add_keylog(uint16_t keycode);
 #endif
 
-
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
