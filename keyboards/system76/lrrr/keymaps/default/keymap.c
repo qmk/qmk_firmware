1b979a89ed06f32a4e77c8b5ee84ba4f40441510
leviport <leviport377@gmail.com>
2020-02-27 09:26:51 -0700

    Un-swap RCTL and RALT, making RCTL closer to right thumb

--- keyboards/system76/lrrr/keymaps/default/keymap.c
+++ keyboards/system76/lrrr/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ ________________________________________________________________________________
       |   SHIFT    |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    |   SHIFT    |   UP   |
   ____|____________|________|________|________|________|________|________|________|________|________|________|____________|________|_________
   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
-  |    CTRL    |  FN    | LGUI  | LALT   |    SPACE        |      SPACE      | RALT   |  RCTRL |     FN      |   |  LEFT  |  DOWN  | RIGHT  |
+  |    CTRL    |  FN    | LGUI  | LALT   |    SPACE        |      SPACE      | RCTRL  |  RALT  |     FN      |   |  LEFT  |  DOWN  | RIGHT  |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|   |________|________|________|
 */
 
@@ -30,7 +30,7 @@ ________________________________________________________________________________
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGDN,
           KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,       KC_END,
               KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
-          KC_LCTL, MO(1),   KC_LGUI, KC_LALT,     KC_SPC,           KC_SPC,     KC_RALT,   KC_RCTL,   MO(1),         KC_LEFT, KC_DOWN, KC_RIGHT
+          KC_LCTL, MO(1),   KC_LGUI, KC_LALT,     KC_SPC,           KC_SPC,     KC_RCTL,   KC_RALT,   MO(1),         KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
     /* Layer 1, function layer
