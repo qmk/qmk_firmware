ad91b06ddeadeea05fa33fe8a719a88441feee14
Brandon Schlack <brandonschlack@gmail.com>
2020-02-27 12:50:52 -0800

    [Keymap] Fixes for nightmare keymap
    * Fix Lower/Raise Layers to match ortho maps
    * Add Play/Pause to Lower/Raise
    * Add Arrow Mod-Taps
    * Add Next-/Prev-Tab, Next-Window to Macro Layer

--- keyboards/nightmare/keymaps/brandonschlack/keymap.c
+++ keyboards/nightmare/keymaps/brandonschlack/keymap.c
@@ -32,43 +32,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_split( \
     KC_HOME,   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
     KC_PGUP,   HY_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
-    KC_PGDN,            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MCO_LYR,  \
-    KC_END,                      KC_LCTL, KC_LALT, KC_LGUI,     SPC_LWR,              SPC_LWR,           KC_RGUI, KC_RALT, KC_RCTL  \
+    KC_PGDN,            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, UP_RSFT, MCO_LYR,  \
+    KC_END,                      KC_LCTL, KC_LALT, KC_LGUI,     SPC_LWR,              SPC_RAI,           LFT_GUI, DWN_ALT, RGT_CTL  \
 ),
 /**
  * Lower
  * ┌───┬┬┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
- * │SlD│││` ~│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│Del│
+ * │SlD│││ ` │ ! │ @ │ # │ $ │ % │ ^ │ & │ * │ ( │ ) │ _ │ + │   │
  * ├───┼┼┼───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │VlU│││    │ ← │ ↓ │ ↑ │ → │   │ F1│ F2│ F3│ F4│ F5│ F6│ |  \ │
+ * │VlU│││    │ ← │ ↓ │ ↑ │ → │   │ F1│ F2│ F3│ F4│ F5│ F6│   |  │
  * ├───┼┼┼────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┤
- * │VlD│││      │   │   │   │   │ F7│ F8│ F9│F10│F11│F12│    │   │
+ * │VlD│││      │   │   │   │   │ F7│ F8│ F9│F10│F11│F12│    │Ply│
  * ├───┼┼┼┬┬┬┬┬─┴─┬─┴──┬┴──┬┴───┴───┼───┴───┴──┬┴──┬┴───┼───┬┼┬┬┬┤
  * │Mut││││││││   │    │   │        │          │   │    │   ││││││
  * └───┴┴┴┴┴┴┴┴───┴────┴───┴────────┴──────────┴───┴────┴───┴┴┴┴┴┘
  */
 [_LOWER] = LAYOUT_split( \
-    MC_SLPD,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DELT, \
-    KC_VOLU,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_BSLS, \
-    KC_VOLD,            _______, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, \
+    MC_SLPD,   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, \
+    KC_VOLU,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_PIPE, \
+    KC_VOLD,            _______, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MPLY, \
     KC_MUTE,                     _______, _______, _______,     _______,              _______,           _______, _______, _______  \
 ),
 /**
  * Raise
  * ┌───┬┬┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
- * │SlD│││   │   │   │   │   │   │   │   │   │   │   │ ↑ │   │   │
+ * │SlD│││ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │   │
  * ├───┼┼┼───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │Nxt│││    │ F1│ F2│ F3│ F4│ F5│ F6│   │   │   │ ← │ → │      │
+ * │Nxt│││    │ ← │ ↓ │ ↑ │ → │   │ F1│ F2│ F3│ F4│ F5│ F6│   \  │
  * ├───┼┼┼────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┤
- * │Prv│││      │ F7│ F8│ F9│F10│F11│F12│   │   │   │ ↓ │    │   │
+ * │Prv│││      │   │   │   │   │ F7│ F8│ F9│F10│F11│F12│    │Ply│
  * ├───┼┼┼┬┬┬┬┬─┴─┬─┴──┬┴──┬┴───┴───┼───┴───┴──┬┴──┬┴───┼───┬┼┬┬┬┤
  * │Ply││││││││   │    │   │        │          │   │    │   ││││││
  * └───┴┴┴┴┴┴┴┴───┴────┴───┴────────┴──────────┴───┴────┴───┴┴┴┴┴┘
  */
 [_RAISE] = LAYOUT_split( \
-    MC_SLPD,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, \
-    KC_MFFD,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_LEFT, KC_RGHT,          _______, \
-    KC_MRWD,            _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_DOWN, _______, _______, \
+    MC_SLPD,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, \
+    KC_MFFD,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_BSLS, \
+    KC_MRWD,            _______, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MPLY, \
     KC_MPLY,                     _______, _______, _______,     _______,              _______,           _______, _______, _______  \
 ),
 /**
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_MACRO] = LAYOUT_split( \
     _______,   _______, KC_BRMD, KC_BRMU, MC_MSSN, MC_LHPD, _______, _______, KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, _______, \
-    _______,   _______, _______, _______, MC_BACK, MC_FWRD, _______, _______, _______, _______, _______, _______, _______,          _______, \
+    _______,   NXT_WIN, PRV_TAB, NXT_TAB, MC_BACK, MC_FWRD, _______, _______, _______, _______, _______, _______, _______,          _______, \
     _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______,                     _______, _______, _______,     _______,              _______,           _______, _______, _______  \
 ),
