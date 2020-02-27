421fc1e5404825e9fb5c86be4c9751fa568db9fd
aholland909 <andrewholland909@gmail.com>
2020-02-27 16:15:17 +0000

    Update keyboards/cu80/config.h
    Remove "Custom matrix"
    
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/cu80/config.h
+++ keyboards/cu80/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { F6, F5, F4, F1, F0, C7, C6, B6, B0, E6, B7, B3, B2, D2, D3, D5, D4}
 #define UNUSED_PINS
 
-/* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
+/* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
