224ca812192c1ccff08854a9bc44fc5104888e38
Jon <jon.cy.lee98@gmail.com>
2020-02-27 19:44:47 -0500

    Adjust RGB based on layers

--- keyboards/crkbd/keymaps/bobokun/config.h
+++ keyboards/crkbd/keymaps/bobokun/config.h
@@ -71,15 +71,15 @@ along with this program.  If not see <http://www.gnu.org/licenses/>.
 // #    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN        // Full gradient scrolling out to in
 // #    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL   // Full dual gradients scrolling out to in
 // #    define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON  // Full gradent Chevron shapped scrolling left to right
-// #    define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL      // Full gradient spinning pinwheel around center of keyboard
+#    define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL      // Full gradient spinning pinwheel around center of keyboard
 // #    define DISABLE_RGB_MATRIX_CYCLE_SPIRAL        // Full gradient spinning spiral around center of keyboard
 // #    define DISABLE_RGB_MATRIX_DUAL_BEACON         // Full gradient spinning around center of keyboard
-// #    define DISABLE_RGB_MATRIX_RAINBOW_BEACON      // Full tighter gradient spinning around center of keyboard
+#    define DISABLE_RGB_MATRIX_RAINBOW_BEACON      // Full tighter gradient spinning around center of keyboard
 // #    define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS   // Full dual gradients spinning two halfs of keyboard
 #    define DISABLE_RGB_MATRIX_RAINDROPS           // Randomly changes a single key's hue
 #    define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS // Randomly changes a single key's hue and saturation
 // #    define DISABLE_RGB_MATRIX_TYPING_HEATMAP      // How hot is your WPM!
-// #    define DISABLE_RGB_MATRIX_DIGITAL_RAIN        // That famous computer simulation
+#    define DISABLE_RGB_MATRIX_DIGITAL_RAIN        // That famous computer simulation
 #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE   // Pulses keys hit to hue & value then fades value out
 #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE      // Static single hue, pulses keys hit to shifted hue then fades to current hue
 #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE       // Hue & value pulse near a single key hit then fades value out
@@ -89,7 +89,7 @@ along with this program.  If not see <http://www.gnu.org/licenses/>.
 #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS      // Hue & value pulse away on the same column and row of a single key hit then fades value out
 #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS // Hue & value pulse away on the same column and row of multiple key hits then fades value out
 // #    define DISABLE_RGB_MATRIX_SPLASH              // Full gradient & value pulse away from a single key hit then fades value out
-// #    define DISABLE_RGB_MATRIX_MULTISPLASH         // Full gradient & value pulse away from multiple key hits then fades value out
+#    define DISABLE_RGB_MATRIX_MULTISPLASH         // Full gradient & value pulse away from multiple key hits then fades value out
 #    define DISABLE_RGB_MATRIX_SOLID_SPLASH        // Hue & value pulse away from a single key hit then fades value out
 #    define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
 
