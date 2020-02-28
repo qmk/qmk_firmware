ae2d5f21f219e238b7d84d9baee0c9dfa5422f12
Alex Daigre <alexdaigre@gmail.com>
2020-02-28 01:45:34 -0600

    Updated copyright attribuatation to include the author of the file I modified.

--- keyboards/preonic/keymaps/programmer-dongato/keymap.c
+++ keyboards/preonic/keymaps/programmer-dongato/keymap.c
@@ -1,4 +1,5 @@
 /* Copyright 2020 Alex Daigre
+ * Modified from layout provided by Jack Humbert
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
