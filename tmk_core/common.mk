94e109391889f5137c94da793968e84f28649706
zvecr <git@zvecr.com>
2020-02-29 00:43:21 +0000

    Enable syscalls all the time

--- tmk_core/common.mk
+++ tmk_core/common.mk
@@ -27,21 +27,11 @@ TMK_COMMON_SRC +=	$(COMMON_DIR)/host.c \
 
 ifeq ($(PLATFORM),AVR)
   TMK_COMMON_SRC += $(PLATFORM_COMMON_DIR)/xprintf.S
-endif
-
-ifeq ($(PLATFORM),CHIBIOS)
+else ifeq ($(PLATFORM),CHIBIOS)
   TMK_COMMON_SRC += $(PLATFORM_COMMON_DIR)/printf.c
-  ifeq ($(strip $(AUTO_SHIFT_ENABLE)), yes)
-    TMK_COMMON_SRC += $(CHIBIOS)/os/various/syscalls.c
-  else ifeq ($(strip $(TERMINAL_ENABLE)), yes)
-    TMK_COMMON_SRC += $(CHIBIOS)/os/various/syscalls.c
-  endif
-endif
-
-ifeq ($(PLATFORM),ARM_ATSAM)
+else ifeq ($(PLATFORM),ARM_ATSAM)
   TMK_COMMON_SRC += $(PLATFORM_COMMON_DIR)/printf.c
 endif
-
 
 # Option modules
 BOOTMAGIC_ENABLE ?= no
