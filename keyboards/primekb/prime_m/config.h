42d210cfa2737a1062a466b7d3985350d71dcfe9
holtenc <holtenc@gmail.com>
2020-02-29 09:57:53 -0600

    Update keyboards/primekb/prime_m/config.h
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/primekb/prime_m/config.h
+++ keyboards/primekb/prime_m/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x5052
-#define PRODUCT_ID      0x504D
+#define VENDOR_ID       0x5052 // "PR"
+#define PRODUCT_ID      0x504D // "PM"
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    PrimeKB
 #define PRODUCT         Prime_M
