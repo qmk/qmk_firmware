b6b3bc3f0beeb819c345cb2c1032806366b25b4b
James Young <xxiinophobia@yahoo.com>
2020-02-29 11:03:43 -0800

    clang-format the future branch via `qmk cformat` again
    Don't know why the previous pass didn't fully format.

--- quantum/stm32/chconf.h
+++ quantum/stm32/chconf.h
@@ -595,8 +595,9 @@
  * @details User initialization code added to the @p chSysInit() function
  *          just before interrupts are enabled globally.
  */
-#    define CH_CFG_SYSTEM_INIT_HOOK() \
-        { /* Add threads initialization code here.*/ }
+#    define CH_CFG_SYSTEM_INIT_HOOK()                \
+        { /* Add threads initialization code here.*/ \
+        }
 
 /**
  * @brief   Threads descriptor structure extension.
@@ -611,34 +612,39 @@
  * @note    It is invoked from within @p _thread_init() and implicitly from all
  *          the threads creation APIs.
  */
-#    define CH_CFG_THREAD_INIT_HOOK(tp) \
-        { /* Add threads initialization code here.*/ }
+#    define CH_CFG_THREAD_INIT_HOOK(tp)              \
+        { /* Add threads initialization code here.*/ \
+        }
 
 /**
  * @brief   Threads finalization hook.
  * @details User finalization code added to the @p chThdExit() API.
  */
-#    define CH_CFG_THREAD_EXIT_HOOK(tp) \
-        { /* Add threads finalization code here.*/ }
+#    define CH_CFG_THREAD_EXIT_HOOK(tp)            \
+        { /* Add threads finalization code here.*/ \
+        }
 
 /**
  * @brief   Context switch hook.
  * @details This hook is invoked just before switching between threads.
  */
 #    define CH_CFG_CONTEXT_SWITCH_HOOK(ntp, otp) \
-        { /* Context switch code here.*/ }
+        { /* Context switch code here.*/         \
+        }
 
 /**
  * @brief   ISR enter hook.
  */
 #    define CH_CFG_IRQ_PROLOGUE_HOOK() \
-        { /* IRQ prologue code here.*/ }
+        { /* IRQ prologue code here.*/ \
+        }
 
 /**
  * @brief   ISR exit hook.
  */
 #    define CH_CFG_IRQ_EPILOGUE_HOOK() \
-        { /* IRQ epilogue code here.*/ }
+        { /* IRQ epilogue code here.*/ \
+        }
 
 /**
  * @brief   Idle thread enter hook.
@@ -647,7 +653,8 @@
  * @note    This macro can be used to activate a power saving mode.
  */
 #    define CH_CFG_IDLE_ENTER_HOOK() \
-        { /* Idle-enter code here.*/ }
+        { /* Idle-enter code here.*/ \
+        }
 
 /**
  * @brief   Idle thread leave hook.
@@ -656,22 +663,25 @@
  * @note    This macro can be used to deactivate a power saving mode.
  */
 #    define CH_CFG_IDLE_LEAVE_HOOK() \
-        { /* Idle-leave code here.*/ }
+        { /* Idle-leave code here.*/ \
+        }
 
 /**
  * @brief   Idle Loop hook.
  * @details This hook is continuously invoked by the idle thread loop.
  */
 #    define CH_CFG_IDLE_LOOP_HOOK() \
-        { /* Idle loop code here.*/ }
+        { /* Idle loop code here.*/ \
+        }
 
 /**
  * @brief   System tick event hook.
  * @details This hook is invoked in the system tick handler immediately
  *          after processing the virtual timers queue.
  */
-#    define CH_CFG_SYSTEM_TICK_HOOK() \
-        { /* System tick event code here.*/ }
+#    define CH_CFG_SYSTEM_TICK_HOOK()       \
+        { /* System tick event code here.*/ \
+        }
 
 /**
  * @brief   System halt hook.
@@ -679,7 +689,8 @@
  *          the system is halted.
  */
 #    define CH_CFG_SYSTEM_HALT_HOOK(reason) \
-        { /* System halt code here.*/ }
+        { /* System halt code here.*/       \
+        }
 
 /**
  * @brief   Trace hook.
@@ -687,7 +698,8 @@
  *          trace buffer.
  */
 #    define CH_CFG_TRACE_HOOK(tep) \
-        { /* Trace code here.*/ }
+        { /* Trace code here.*/    \
+        }
 
 /** @} */
 
