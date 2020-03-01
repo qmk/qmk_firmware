65067455027ded794592f9fa0dbbd6dcb45b83c0
skullY <skullydazed@gmail.com>
2020-02-29 21:12:20 -0800

    Fix model01

--- keyboards/model01/model01.c
+++ keyboards/model01/model01.c
@@ -25,3 +25,11 @@ void matrix_init_kb(void) {
 
 void matrix_scan_kb(void) {
 }
+
+__attribute__ ((weak))
+void matrix_scan_user(void) {
+}
+
+__attribute__ ((weak))
+void matrix_init_user(void) {
+}
