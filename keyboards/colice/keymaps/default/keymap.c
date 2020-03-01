945d9504f7a8f949126cfdc097df4a991bacfc2b
Takeshi Nishio <kkeennnn@gmail.com>
2020-03-01 21:35:16 +0900

    Update 右手側、画像ソフトなどで使うため、矢印キーの左をレイヤーキーからCTRLへ変更。

--- keyboards/colice/keymaps/default/keymap.c
+++ keyboards/colice/keymaps/default/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,       KC_P7,  KC_P8,  KC_P9,  KC_PMNS,                 KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS, \
               KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,       KC_MPRV,KC_MPLY,KC_MNXT,KC_PPLS,                 KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT, \
               KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   RAISE,      KC_MUTE,KC_VOLD,KC_VOLU,KC_PENT,                 KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_UP, \
-              RAISE,                  KC_LALT,KC_LGUI,SP_SFT,             KC_P0,          KC_PDOT,                         LOWER,  SP_GUI, KC_LALT,        LOWER,KC_LEFT,KC_DOWN,KC_RGHT \
+              RAISE,                  KC_LALT,KC_LGUI,SP_SFT,             KC_P0,          KC_PDOT,                         LOWER,  SP_GUI, KC_LALT,        KC_LCTL,KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     [_NUM] = LAYOUT(
       _______, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,               KC_PSLS,KC_PAST,TG_NUM,              KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC, \
