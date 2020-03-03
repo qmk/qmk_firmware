ed6586e25bd854f445193566150439b25853f831
Alfred Maler <alfrdmalr@gmail.com>
2020-03-03 13:13:42 -0500

    [Keymap] Feature/alfrdmalr/keymap update (#8174)
    * WIP do not merge
    
    * first pass at custom preonic layout
    
    * add auto shift and reset via leader key
    
    * Update readme
    
    * update copyright notice
    
    * formatting changes
    
    * fix: use MO instead of process_record_user
    
    * added backslash and moved grave position
    
    * remove extraneous 'j' characer in NUMPAD template
    
    * update template formatting
    
    * remove process_record_user
    
    * swap "!" with "@"
    
    * fix readme formatting
    
    * update readme layout image
    
    * restore settings layer
    
    * add windows minimize sequence
    
    * fix: switch to correct seq function for three-key sequence
    
    * fix: missing semicolon
    
    * refactor: move keymap to userspace and generic 5x12 layout
    
    * add numlock to numpad layer
    
    * add readme
    
    * update readme formatting
    
    * remove unused wrappers from layout keymap
    
    * update readme title to reflect new location
    
    * remove alfrdmalr directory from preonic/keymaps
    
    * add ortho 4x12 support
    
    * add 'trilayer' settings and update keymap
    
    * update SYMBOLS layer to SYMBOL
    
    * remove minimize sequence
    
    * clean up user config
    
    * add brightness controls
    
    * update settings ascii
    
    * moved some symbols around to make vim/linux smoother

--- layouts/community/ortho_4x12/alfrdmalr/keymap.c
+++ layouts/community/ortho_4x12/alfrdmalr/keymap.c
@@ -3,17 +3,15 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // QWERTY 
-[_QWERTY] = LAYOUT_ortho_5x12_wrapper(
-  K00,     ____NUMROW_L0____, ____NUMROW_R0____, K0B,
-  K10,     ____QWERTY_L1____, ____QWERTY_R1____, K1B_ALT,
+[_QWERTY] = LAYOUT_ortho_4x12_wrapper(
+  K10,     ____QWERTY_L1____, ____QWERTY_R1____, K1B,
   K20,     ____QWERTY_L2____, ____QWERTY_R2____, K2B,
   K30,     ____QWERTY_L3____, ____QWERTY_R3____, K3B,
   K40,     _____BASE_L4_____, _____BASE_R4_____, K4B
 ),
 
 // SYMBOL 
-[_SYMBOL] = LAYOUT_ortho_5x12_wrapper(
-  K00_SYM, ____NUMROW_L0____, ____NUMROW_R0____, K0B_SYM,
+[_SYMBOL] = LAYOUT_ortho_4x12_wrapper(
   K10_SYM, ____SYMBOL_L1____, ____SYMBOL_R1____, K1B_SYM,
   K20_SYM, ____SYMBOL_L2____, ____SYMBOL_R2____, K2B_SYM,
   K30_SYM, ____SYMBOL_L3____, ____SYMBOL_R3____, K3B_SYM,
@@ -21,8 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 // NAVIGATION 
-[_NAVIGATION] = LAYOUT_ortho_5x12_wrapper(
-  K00_NAV, ____NUMROW_L0____, ____NUMROW_R0____, K0B_NAV,
+[_NAVIGATION] = LAYOUT_ortho_4x12_wrapper(
   K10_NAV, __NAVIGATION_L1__, __NAVIGATION_R1__, K1B_NAV,
   K20_NAV, __NAVIGATION_L2__, __NAVIGATION_R2__, K2B_NAV,
   K30_NAV, __NAVIGATION_L3__, __NAVIGATION_R3__, K3B_NAV,
@@ -30,8 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 // NUMPAD 
-[_NUMPAD] = LAYOUT_ortho_5x12_wrapper( 
-  K00_NUM, ____NUMROW_L0____, ____NUMROW_R0____, K0B_NUM,
+[_NUMPAD] = LAYOUT_ortho_4x12_wrapper( 
   K10_NUM, ____NUMPAD_L1____, ____NUMPAD_R1____, K1B_NUM,
   K20_NUM, ____NUMPAD_L2____, ____NUMPAD_R2____, K2B_NUM,
   K30_NUM, ____NUMPAD_L3____, ____NUMPAD_R3____, K3B_NUM,
@@ -39,8 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 // SETTINGS 
-[_SETTINGS] = LAYOUT_ortho_5x12_wrapper( 
-  K00_SET, ______TRANS______,  ______TRANS______, K0B_SET,
+[_SETTINGS] = LAYOUT_ortho_4x12_wrapper( 
   K10_SET, ___SETTINGS_L1___,  ___SETTINGS_R1___, K1B_SET,
   K20_SET, ___SETTINGS_L2___,  ___SETTINGS_R2___, K2B_SET,
   K30_SET, ___SETTINGS_L3___,  ___SETTINGS_R3___, K3B_SET,
