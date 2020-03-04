ccbe9d256b85ae674b7462fc4c3016434f71ae5d
James Young <xxiinophobia@yahoo.com>
2020-03-04 11:07:17 -0800

    refactor yd60mq.c to use led_update_kb()

--- keyboards/yd60mq/yd60mq.c
+++ keyboards/yd60mq/yd60mq.c
@@ -1,12 +1,14 @@
 #include "yd60mq.h"
 
-void led_set_kb(uint8_t usb_led) {
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        setPinOutput(F4);
-        writePinLow(F4);
-    } else {
-        setPinInput(F4);
-	}
-
-    led_set_user(usb_led);
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if (res) {
+        // writePin sets the pin high for 1 and low for 0.
+        // In this example the pins are inverted, setting
+        // it low/0 turns it on, and high/1 turns the LED off.
+        // This behavior depends on whether the LED is between the pin
+        // and VCC or the pin and GND.
+        writePin(F4, !led_state.caps_lock);
+    }
+    return res;
 }
