d9e62a5cc69d76ee13b4a7a83d2f107b81d88dfe
Art <blah>
2020-03-05 19:48:54 +0000

    added visual map

--- keyboards/split75/keymaps/mike_acrylic/keymap.c
+++ keyboards/split75/keymaps/mike_acrylic/keymap.c
@@ -1,23 +1,9 @@
-/*
-Copyright 2017 Luiz Ribeiro <luizribeiro@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "split75.h"
 
 #include QMK_KEYBOARD_H
+
+#define QWERTY 0
+#define MOD 1
 
 typedef struct {
   bool is_press_action;
@@ -42,23 +28,43 @@ int cur_dance (qk_tap_dance_state_t *state);
 void alt_finished (qk_tap_dance_state_t *state, void *user_data);
 void alt_reset (qk_tap_dance_state_t *state, void *user_data);
 
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = KEYMAP(
-        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_INS, KC_HOME, \
-	KC_P8, KC_P9, KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSPC, KC_DEL, \
-	KC_P6, KC_P7, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-	KC_P4, KC_P5, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENTER, KC_PGUP, \
-	KC_P2, KC_P3, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, \
-	KC_P0, KC_P1, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT \
-    ),
-    [1] = KEYMAP(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    \
-        KC_TRNS, KC_TRNS, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_PSCR,   \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_NO,   RGB_TOG,RGB_VAI,RGB_HUI,RGB_SAI,KC_INS, RESET,   KC_PSCR, KC_SLCK,  KC_PAUS, KC_BSLS,  KC_SLCK,   \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,RGB_MOD,RGB_VAD,RGB_HUD,RGB_SAD,KC_NO,  KC_NO,   KC_F14,  KC_F15,   KC_INS,  KC_HOME,   \
-        KC_TRNS, KC_TRNS, KC_LSFT, KC_MPRV, KC_MPLY, KC_MNXT,KC_NO,  BL_TOGG,KC_NO,  KC_MUTE,KC_VOLD, KC_VOLU, KC_NO,    KC_RSFT, RGB_MOD,  KC_END,    \
-        KC_TRNS, KC_TRNS, KC_LCTL, KC_LGUI, TD(ALT_OSL1),     RESET, RESET, RESET,                  KC_TRNS,   KC_RCTL, KC_TRNS,  KC_HOME, BL_STEP,  KC_END) \
-};
+
+  /* Base QWERTY   ,-----------------------------------------.     ,-----------------------------------------------------.
+  *                | ESC | F1  | F2  | F3  | F4  | F5  | F6  |     | F7  | F8  | F9  | F10 | F11 | F12 |Print| Ins | Del |
+  * ,-----------.  |-----+-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----+-----------+-----|
+  * |  8  |  9  |  |  ~  |  1  |  2  |  3  |  4  |  5  |  6  |     |  7  |  8  |  9  |  0  |  -  |  =  | Backspac  | Home|
+  * |-----+-----|  |-----------------------------------------'  ,--------------------------------------------------+-----|
+  * |  6  |  7  |  | Tab   |  Q  |  W  |  E  |  R  |  T  |     |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \    | PgUp|
+  * |-----+-----|  |---------------------------------------.    `--------------------------------------------------+-----|
+  * |  4  |  5  |  | Caps    |  A  |  S  |  D  |  F  |  G  |     |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    | PgDn|
+  * |-----+-----|  |-----------------------------------------.   `-------------------------------------------------+-----|
+  * |  2  |  3  |  | Shift     |  Z  |  X  |  C  |  V  |  B  |     |  N  |  M  |  ,  |  .  |   /   |  Shift  | Up  | End |
+  * |-----+-----|  |-----------------------------------------'   ,-------------------------------------------+-----+-----|
+  * |  0  |  1  |  | Ctrl  |  GUI |  Alt |   Space   |     |     | Space         |  Alt  |     | Ctrl  | Left| Down|Right|
+  * `-----------'  `---------------------------------------'     `-------------------------------------------------------'
+  */
+  [QWERTY] = KEYMAP(
+//--------------------------------Left Hand-----------------------------------| |--------------------------------Right Hand------------------------------------------------
+                KC_ESC,   KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,                KC_F7,  KC_F8,   KC_F9,  KC_F10,  KC_F11,   KC_F12,   KC_PSCR, KC_INS,   KC_DEL, \
+KC_8,  KC_9,    KC_GRAVE, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,                 KC_7,   KC_8,    KC_9,   KC_0,    KC_MINUS, KC_EQUAL, KC_BSPC,           KC_HOME, \
+KC_6,  KC_7,    KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,                 KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,           KC_PGUP, \
+KC_4,  KC_5,    KC_CAPS,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,                 KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,  KC_ENTER,                    KC_PGDN, \
+KC_2,  KC_3,    KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,                 KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,                      KC_RSFT, KC_UP,   KC_END, \
+KC_0,  KC_1,    KC_LCTL,  KC_LGUI, KC_LALT, MO(MOD),KC_SPC,                       KC_SPC,         KC_RALT, MO(MOD),KC_RCTL,                      KC_LEFT, KC_DOWN, KC_RIGHT \
+  ),
+
+  [MOD] = KEYMAP(
+//--------------------------------Left Hand---------------------------------------|    |--------------------------------Right Hand------------------------------------------------
+                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, \
+KC_TRNS, KC_TRNS,   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                 KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,            KC_PSCR, \
+KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_UP,   KC_NO,   RGB_TOG, RGB_VAI,               RGB_HUI, RGB_SAI, KC_INS,  RESET,   KC_PSCR, KC_SLCK,  KC_PAUS, KC_BSLS,           KC_SLCK, \
+KC_TRNS, KC_TRNS,   KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,RGB_MOD, RGB_VAD,               RGB_HUD, RGB_SAD, KC_NO,   KC_NO,   KC_F14,  KC_F15,   KC_INS,                     KC_HOME, \
+KC_TRNS, KC_TRNS,   KC_LSFT, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   BL_TOGG,               KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,                      KC_RSFT, RGB_MOD,  KC_END, \
+KC_TRNS, KC_TRNS,   KC_LCTL, KC_LGUI, TD(ALT_OSL1),     RESET, RESET,                   RESET,            KC_TRNS, KC_RCTL, KC_TRNS,                    KC_HOME, BL_STEP,  KC_END \
+  )};
 
 int cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
