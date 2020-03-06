e0ec1082eeae41af96595ef3fd76f65a21c0cd39
Nick Brassel <nick@tzarc.org>
2020-03-07 10:55:23 +1100

    Add support for hardware and board initialisation overrides.

--- tmk_core/protocol/chibios/main.c
+++ tmk_core/protocol/chibios/main.c
@@ -101,6 +101,40 @@ void midi_ep_task(void);
 //   }
 // }
 
+/* Early initialisation
+ */
+__attribute__((weak)) void early_hardware_init_pre(void) {}
+
+__attribute__((weak)) void early_hardware_init_post(void) {
+#ifdef STM32_BOOTLOADER_ADDRESS
+    void enter_bootloader_mode_if_requested(void);
+    enter_bootloader_mode_if_requested();
+#endif
+}
+
+__attribute__((weak)) void board_init(void) {
+}
+
+// This overrides what's normally in ChibiOS board definitions
+void __early_init(void) {
+    early_hardware_init_pre();
+
+    // This is the renamed equivalent of __early_init in the board.c file
+    void __chibios_override___early_init(void);
+    __chibios_override___early_init();
+
+    early_hardware_init_post();
+}
+
+// This overrides what's normally in ChibiOS board definitions
+void boardInit(void) {
+    // This is the renamed equivalent of boardInit in the board.c file
+    void __chibios_override_boardInit(void);
+    __chibios_override_boardInit();
+
+    board_init();
+}
+
 /* Main thread
  */
 int main(void) {
