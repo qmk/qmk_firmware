5126c838d31b65854851a7e918af17eb1dcf193c
Derek <dschmell91@gmail.com>
2020-03-07 13:53:07 -0500

    Update keymap.c

--- keyboards/splitreus62/keymaps/default/keymap.c
+++ keyboards/splitreus62/keymaps/default/keymap.c
@@ -37,7 +37,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO,                         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, _______, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-        KC_LCTRL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_NO, KC_NO,      KC_ENT, KC_SPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+        KC_LCTRL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_NO, KC_NO,      KC_ENT, KC_SPC, KC_NO, KC_NO, KC_NO, KC_NO, TG(3)
+    ),
+        //game layer
+    [3] = LAYOUT(
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                          KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                          KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+        KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G,                        KC_H, LT(2, KC_J), KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
+        KC_LCTRL, KC_NO, KC_LALT, KC_GRV, KC_SPC, KC_BSPC, KC_DEL,      KC_ENT, KC_SPC, KC_LBRC, KC_RBRC, KC_HOME, KC_END, TO(0)
     )
 };
 
