d7a2b3e887e34335918104324e81bc80ea51fe6c
nickolaij <ni.jones999@gmail.com>
2020-03-07 11:09:37 +1000

    Update keyboards/abacus/rules.mk
    Co-Authored-By: Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>

--- keyboards/abacus/rules.mk
+++ keyboards/abacus/rules.mk
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = atmel-dfu
+BOOTLOADER = qmk-dfu
 
 # Build Options
 #   change yes to no to disable
