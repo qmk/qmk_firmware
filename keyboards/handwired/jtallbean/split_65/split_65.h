6cadba93a0e673ad9190cbcbf7fdaf695d65fc71
samlli <samuelli.pk@gmail.com>
2020-03-07 14:34:38 -0500

    Renamed LAYOUT to LAYOUT_all

--- keyboards/handwired/jtallbean/split_65/split_65.h
+++ keyboards/handwired/jtallbean/split_65/split_65.h
@@ -26,7 +26,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
- #define LAYOUT( \
+ #define LAYOUT_all( \
    L00, L01, L02, L03, L04, L05, L06, L07, R00, R01, R02, R03, R04, R05, R06, R07, R08, \
    L10, L11,      L13, L14, L15, L16, L17, R10, R11, R12, R13, R14, R15, R16, R17, R18, \
    L20, L21,      L23, L24, L25, L26, L27, R20, R21, R22, R23, R24, R25,      R27, R28, \
