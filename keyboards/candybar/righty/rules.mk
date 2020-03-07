fc0e370d8d86b8e8e6f2d7952fea76b23d352575
U-TERRY-T450S\Terry <terry@terrymathews.net>
2020-03-07 13:19:34 -0500

    Candybar: split lefty and righty into subprojects.

---
+++ keyboards/candybar/righty/rules.mk
@@ -0,0 +1,25 @@
+# MCU name
+MCU = STM32F072
+BOARD = ST_STM32F072B_DISCOVERY
+
+# Build Options
+#   comment out to disable the options.
+#
+# EXTRAFLAGS+=-flto
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+BACKLIGHT_ENABLE = no
+BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration
+## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
+MOUSEKEY_ENABLE = no # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+CONSOLE_ENABLE = no  # Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes     # USB Nkey Rollover
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
+SERIAL_LINK_ENABLE = no
+
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
