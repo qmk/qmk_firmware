60b20238a8015b8dfe43558125b575ed5b2d4e60
Christian Gurholt <kifinnsson@gmail.com>
2020-03-07 09:54:56 -0700

    updated uinit32()_t to layer_state_t() for update_tri_layer_state
    as suggested
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/planck/keymaps/kifinnsson/keymap.c
+++ keyboards/planck/keymaps/kifinnsson/keymap.c
@@ -183,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         )
     };
 
-uint32_t layer_state_set_user(uint32_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); }
+layer_state_t layer_state_set_user(layer_state_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         //
