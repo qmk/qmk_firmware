7aafd0a9681a008eaa0776b9bf3f86cac9d708f3
yohewi <yohewi@gmail.com>
2020-03-08 16:49:15 +0900

    20200308_1

--- keyboards/uranuma/uranuma.h
+++ keyboards/uranuma/uranuma.h
@@ -2,15 +2,6 @@
 
 #include "quantum.h"
 
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-
 #define LAYOUT( \
   k00, k01, k02, k03, k04,     k05, k06, k07, k08, k09, \
   k10, k11, k12, k13, k14,     k15, k16, k17, k18, k19, \
