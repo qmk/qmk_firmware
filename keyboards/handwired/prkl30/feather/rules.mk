a9d7b894db3ac0ea6597f98e637962db962766e3
ErkHal <erkkihalinen@gmail.com>
2020-03-08 10:16:47 +0200

    Remove false comments from rules.mk

--- keyboards/handwired/prkl30/feather/rules.mk
+++ keyboards/handwired/prkl30/feather/rules.mk
@@ -26,6 +26,6 @@ CONSOLE_ENABLE  = yes       # Console for debug
 COMMAND_ENABLE  = yes       # Commands for debug and configuration
 SLEEP_LED_ENABLE  = no      # Breathing sleep LED during USB suspend
 NKRO_ENABLE  = yes          # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE  = no      # Custom backlighting code is used, so this should not be enabled
+BACKLIGHT_ENABLE  = no
 AUDIO_ENABLE  = no          # This can be enabled if a speaker is connected to the expansion port. Not compatible with RGBLIGHT below
 RGBLIGHT_ENABLE  = no       # This can be enabled if a ws2812 strip is connected to the expansion port.
--- keyboards/handwired/prkl30/promicro/rules.mk
+++ keyboards/handwired/prkl30/promicro/rules.mk
@@ -22,6 +22,6 @@ CONSOLE_ENABLE  = yes       # Console for debug
 COMMAND_ENABLE  = yes       # Commands for debug and configuration
 SLEEP_LED_ENABLE  = no      # Breathing sleep LED during USB suspend
 NKRO_ENABLE  = yes          # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE  = no      # Custom backlighting code is used, so this should not be enabled
+BACKLIGHT_ENABLE  = no
 AUDIO_ENABLE  = no          # This can be enabled if a speaker is connected to the expansion port. Not compatible with RGBLIGHT below
 RGBLIGHT_ENABLE  = yes      # This can be enabled if a ws2812 strip is connected to the expansion port.
