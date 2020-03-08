dde973097de7b07e32206a86907d4e76d9b0e50e
TerryMathews <terry@terrymathews.net>
2020-03-08 10:42:01 -0400

    Update keyboards/wheatfield/blocked65/keymaps/default/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/wheatfield/blocked65/keymaps/default/keymap.c
+++ keyboards/wheatfield/blocked65/keymaps/default/keymap.c
@@ -12,7 +12,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_BL] = LAYOUT(
+  [_BL] = LAYOUT_65_ansi_blocker(
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_INSERT,    \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,  KC_DEL,  \
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGUP,    \
