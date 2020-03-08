32d27e66e8ab4e065d1c225211aa4775605961f9
elmo-space <lorwel@mailbox.org>
2020-03-08 14:01:54 +0100

    Update keyboards/kira80/usbconfig.h
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/kira80/usbconfig.h
+++ keyboards/kira80/usbconfig.h
@@ -241,7 +241,7 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'p', 's', '2', 'a', 'v', 'r', 'G', 'B'
+#define USB_CFG_DEVICE_NAME     'K', 'i', 'r', 'a', '8', '0'
 #define USB_CFG_DEVICE_NAME_LEN 8
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
