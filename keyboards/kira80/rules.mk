f63148e8cfa4507ef21bc3b02ba4ef57b2497be4
elmo-space <lorwel@mailbox.org>
2020-03-08 15:30:38 +0100

    fix weird double commit

--- keyboards/kira80/rules.mk
+++ keyboards/kira80/rules.mk
@@ -9,13 +9,6 @@ MCU = atmega32a
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = bootloadHID
 
 # Build Options
