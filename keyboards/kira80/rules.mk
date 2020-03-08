aa05491761e0d0bbc0d06cab45c6f0b306ac2f7a
elmo-space <lorwel@mailbox.org>
2020-03-08 14:01:29 +0100

    Update keyboards/kira80/rules.mk
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/kira80/rules.mk
+++ keyboards/kira80/rules.mk
@@ -2,6 +2,13 @@
 MCU = atmega32a
 
 # Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
 BOOTLOADER = bootloadHID
 
 # Build Options
