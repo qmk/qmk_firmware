97f607c4179a0e5d0a64cf287682aa643550b0df
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-03-09 12:01:00 -0300

    disable mouse keys to allow mac to sleep
    See https://github.com/qmk/qmk_firmware/issues/8323

--- keyboards/crkbd/keymaps/rhruiz/rules.mk
+++ keyboards/crkbd/keymaps/rhruiz/rules.mk
@@ -1,5 +1,5 @@
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
