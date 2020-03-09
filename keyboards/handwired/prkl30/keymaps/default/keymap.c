119aa56708ce254c6d042812d90b7ab1c627e35d
Erkki Halinen <erkkihalinen@gmail.com>
2020-03-09 10:25:35 +0200

    Apply suggestions from code review
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/handwired/prkl30/keymaps/default/keymap.c
+++ keyboards/handwired/prkl30/keymaps/default/keymap.c
@@ -28,10 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------'
  */
 [_DEFAULT] = LAYOUT_all(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
-    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    LT(_LOWER, KC_SPC),  _______,    KC_B,    KC_N,    KC_M,
-    KC_P,    NO_QUOT, KC_BSPC, NO_AE,   NO_OSLH, LT(_FN, KC_ENT), KC_COMM, LT(_RAISE, KC_DOT)
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    NO_QUOT, KC_BSPC,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    NO_AE,   NO_OSLH, LT(_FN, KC_ENT),
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    LT(_LOWER, KC_SPC), _______, KC_B,    KC_N,    KC_M,    KC_COMM, LT(_RAISE, KC_DOT)
 ),
 
 /* Lower - Numbers and some symbols
