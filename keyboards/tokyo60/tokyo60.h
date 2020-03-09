9a26722309e5bbc673b295de0b2cf62e8933885c
Zambumon <carlosalmuvar@gmail.com>
2020-03-09 17:38:08 +0100

    VIA_ENABLE Tokyo60 PCB

--- keyboards/tokyo60/tokyo60.h
+++ keyboards/tokyo60/tokyo60.h
@@ -1,7 +1,9 @@
-#ifndef TOKYO60_H
-#define TOKYO60_H
+#pragma once
 
 #include "quantum.h"
+#include "via.h"
+
+#define XXX KC_NO
 
 #define LAYOUT_60_hhkb( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K1D,\
@@ -12,9 +14,7 @@
 ) { \
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D }, \
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, KC_NO, K2D }, \
-    { K30, KC_NO, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D }, \
-    { KC_NO, K42, K43, KC_NO, KC_NO, K46, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, K4B, K4C, K4D }  \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, XXX, K2D }, \
+    { K30, XXX, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D }, \
+    { XXX, K42, K43, XXX, XXX, K46, XXX, XXX, XXX, XXX, XXX, K4B, K4C, K4D }  \
 }
-
-#endif
