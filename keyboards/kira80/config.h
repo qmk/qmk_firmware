e7d44e953178a99e3e788769cfb3e8981319a1e7
elmo-space <lorwel@mailbox.org>
2020-03-09 11:54:55 +0100

    Apply suggestions from code review
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/kira80/config.h
+++ keyboards/kira80/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0x20A0
-#define PRODUCT_ID 0x422D
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0xC583
 #define DEVICE_VER 0x0102
 #define MANUFACTURER EVE
 #define PRODUCT Kira80
