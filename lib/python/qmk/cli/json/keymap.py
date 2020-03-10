520e1ebd297af8ea89b99a9a0d1825e3e5f0ef5b
skullY <skullydazed@gmail.com>
2020-03-10 07:44:30 -0700

    add missing blank line

--- lib/python/qmk/cli/json/keymap.py
+++ lib/python/qmk/cli/json/keymap.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from milc import cli
 
+
 @cli.argument('-o', '--output', arg_only=True, type=Path, help='File to write to')
 @cli.argument('-q', '--quiet', arg_only=True, action='store_true', help="Quiet mode, only output error messages")
 @cli.argument('filename', arg_only=True, help='Configurator JSON file')
