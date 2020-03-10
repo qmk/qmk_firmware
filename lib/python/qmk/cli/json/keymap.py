4dd8bef8752d4e432668f711d2ef666784cf8e78
skullY <skullydazed@gmail.com>
2020-03-10 07:24:08 -0700

    Rename qmk json-keymap to qmk json2c

--- lib/python/qmk/cli/json/keymap.py
+++ lib/python/qmk/cli/json/keymap.py
@@ -18,39 +18,5 @@ def json_keymap(cli):
 
     This command uses the `qmk.keymap` module to generate a keymap.c from a configurator export. The generated keymap is written to stdout, or to a file if -o is provided.
     """
-    cli.args.filename = qmk.path.normpath(cli.args.filename)
-
-    # Error checking
-    if not cli.args.filename.exists():
-        cli.log.error('JSON file does not exist!')
-        cli.print_usage()
-        exit(1)
-
-    if str(cli.args.filename) == '-':
-        # TODO(skullydazed/anyone): Read file contents from STDIN
-        cli.log.error('Reading from STDIN is not (yet) supported.')
-        cli.print_usage()
-        exit(1)
-
-    # Environment processing
-    if cli.args.output == ('-'):
-        cli.args.output = None
-
-    # Parse the configurator json
-    with cli.args.filename.open('r') as fd:
-        user_keymap = json.load(fd)
-
-    # Generate the keymap
-    keymap_c = qmk.keymap.generate(user_keymap['keyboard'], user_keymap['layout'], user_keymap['layers'])
-
-    if cli.args.output:
-        cli.args.output.parent.mkdir(parents=True, exist_ok=True)
-        if cli.args.output.exists():
-            cli.args.output.replace(cli.args.output.name + '.bak')
-        cli.args.output.write_text(keymap_c)
-
-        if not cli.args.quiet:
-            cli.log.info('Wrote keymap to %s.', cli.args.output)
-
-    else:
-        print(keymap_c)
+    cli.log.error('This command has been renamed to `qmk json2c`.')
+    exit(1)
