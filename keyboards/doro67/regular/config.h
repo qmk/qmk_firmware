cd0b7164e582d88839d18b8034a4e931d910aec3
mechmerlin <mechmerlin@gmail.com>
2020-03-12 07:57:45 -0700

    apply the same VIA changes to the regular PCB

--- keyboards/doro67/regular/config.h
+++ keyboards/doro67/regular/config.h
@@ -3,8 +3,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x4250 // BP for Backprop
+#define PRODUCT_ID      0x5247 // RG for Regular
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Backprop Studio
 #define PRODUCT         Doro67 Regular PCB
