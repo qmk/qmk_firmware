4af5ce9dc4abd89dd46ee871138d21239e55d41b
2Moons-JP <nevans10@hotmail.com>
2020-03-14 14:07:46 +0900

    Adding Slice Keyboard

---
+++ keyboards/basekeys/slice/slice.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "quantum.h"
+
+#ifdef KEYBOARD_basekeys_slice_rev0
+  #include "rev0.h"
+#endif
+
+#ifdef KEYBOARD_basekeys_slice_rev0_rgb
+  #include "rev0.h"
+#endif
+
+#ifdef KEYBOARD_basekeys_slice_rev1
+  #include "rev1.h"
+#endif
+
+#ifdef KEYBOARD_basekeys_slice_rev1_rgb
+  #include "rev1.h"
+#endif
+
