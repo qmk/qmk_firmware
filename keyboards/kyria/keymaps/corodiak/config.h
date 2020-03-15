354c3894553170878c44f3b618ecaa2d26242f4c
Corodiak <jann-niklas@familiezimmermann.de>
2020-03-15 16:39:49 +0100

    Clean up

--- keyboards/kyria/keymaps/corodiak/config.h
+++ keyboards/kyria/keymaps/corodiak/config.h
@@ -33,8 +33,9 @@
 
 #define TAPPING_TERM 200
 
-// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 #define WAIT_FOR_USB
+
+// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 #define SPLIT_USB_DETECT
 #define NO_USB_STARTUP_CHECK
 
