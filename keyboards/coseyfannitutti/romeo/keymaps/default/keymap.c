2a9fe5855d626093a46066a6128592137e201add
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2020-03-15 18:56:47 -0400

    Update keyboards/coseyfannitutti/romeo/keymaps/default/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/coseyfannitutti/romeo/keymaps/default/keymap.c
+++ keyboards/coseyfannitutti/romeo/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
       KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT,
-      KC_LSFT, KC_SLSH, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT,           
+      KC_LSFT, KC_SLSH, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT,
       KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, MO(1), KC_RCTL ),
 
   [1] = LAYOUT_all(
