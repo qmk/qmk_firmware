2ece4cdc6470d774a2dd14a280462d4fa72cce32
mechmerlin <mechmerlin@gmail.com>
2020-03-15 11:40:31 -0700

    comment the vendor ID

--- keyboards/pdxkbc/config.h
+++ keyboards/pdxkbc/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x5044
+#define VENDOR_ID       0x5044 // PD
 #define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Franklin Harding
