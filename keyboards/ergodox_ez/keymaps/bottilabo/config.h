d3fbefd1d8f39ffc45172edf50a7e2de0b780994
Bottilabo <mitamex4u@gmail.com>
2020-03-15 10:55:08 +0900

    deprecated.

--- keyboards/ergodox_ez/keymaps/bottilabo/config.h
+++
@@ -1,9 +0,0 @@
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-#define NO_ACTION_TAPPING
-#define NO_ACTION_ONESHOT
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
-//#define DEBUG_MATRIX_SCAN_RATE
-
--- keyboards/ergodox_ez/keymaps/bottilabo/rules.mk
+++
@@ -1,7 +0,0 @@
-MOUSEKEY_ENABLE  = no  # Mouse keys(+4700)
-EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
-CONSOLE_ENABLE   = no  # Console for debug(+400)
-COMMAND_ENABLE   = no  # Commands for debug and configuration
-CUSTOM_MATRIX    = yes # Custom matrix file for the ErgoDox EZ
-NKRO_ENABLE      = yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-UNICODE_ENABLE   = yes # Unicode
