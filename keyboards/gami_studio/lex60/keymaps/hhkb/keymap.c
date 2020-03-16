227a2ef97e7bd3da238b2fa61dacd696c1db8005
Lukas Alexandre <lukas@codelogic.me>
2020-03-16 10:12:08 +0100

    Fixing UHKB layout and adding possibility of split right shit for HHKB and UHKB

--- keyboards/gami_studio/lex60/keymaps/hhkb/keymap.c
+++ keyboards/gami_studio/lex60/keymaps/hhkb/keymap.c
@@ -17,26 +17,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,               KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,          KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,          KC_K,    KC_L,     KC_SCLN, KC_QUOT,          KC_ENT,
       KC_LSFT,                       KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,          KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, MO(3),
-                                     KC_LALT, KC_LGUI,           KC_SPC,  LT(2, KC_ENT),          KC_RGUI, KC_RALT),
+                                     KC_LALT, KC_LGUI,           KC_SPC,           LT(2, KC_ENT),          KC_RGUI,  KC_RALT, MO(3)),
 
   [_GAMING] = LAYOUT_hhkb(
       KC_GRV,               KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,          KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSLS,   KC_ESC,
       KC_TAB,               KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,          KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       KC_LCTL,              KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,          KC_K,    KC_L,     KC_SCLN, KC_QUOT,          KC_ENT,
       KC_LSFT,                       KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,          KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, MO(3),
-                                     KC_LALT, KC_LGUI,           KC_SPC,  LT(2, KC_ENT),          KC_RGUI, KC_RALT),
+                                     KC_LALT, KC_LGUI,           KC_SPC,           LT(2, KC_ENT),          KC_RGUI,  KC_RALT, MO(3)),
 
   [_FUNCTION] = LAYOUT_hhkb(
       KC_GRV,               KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,         KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_BSLS,   KC_DEL,
       KC_TAB,               XXXXXXX, KC_UP,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,  KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX,
       KC_LCTL,              KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,       KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX,          XXXXXXX,
-      KC_LSFT,                       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_MPRV,  KC_MNXT, KC_MPLY, XXXXXXX, MO(3),
-                                     KC_LALT, KC_LGUI,           KC_SPC,  LT(2, KC_ENT),          KC_RGUI, KC_RALT),
+      KC_LSFT,                       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, KC_MPRV,  KC_MNXT, KC_MPLY, XXXXXXX, XXXXXXX,
+                                     KC_LALT, KC_LGUI,           KC_SPC,           LT(2, KC_ENT),          KC_RGUI,  KC_RALT, XXXXXXX),
 
   [_FIRMWARE] = LAYOUT_hhkb(
-      RESET,                RGB_TOG, RGB_MOD, RGB_HUD,  RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD,       RGB_VAI, BL_DEC,   BL_TOGG, BL_INC,  XXXXXXX, XXXXXXX,   TG(1),
-      XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX,              XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,          XXXXXXX,
+      RESET,                XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   TG(1),
+      RGB_TOG,              RGB_HUI, RGB_SAI, RGB_VAI,  BL_INC,  RGB_MOD, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      BL_TOGG,              RGB_HUD, RGB_SAD, RGB_VAD,  BL_DEC,  XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,          XXXXXXX,
       XXXXXXX,                       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                     XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX),
+                                     XXXXXXX, XXXXXXX,           XXXXXXX,          XXXXXXX,                XXXXXXX,  XXXXXXX, XXXXXXX),
 };
--- keyboards/gami_studio/lex60/lex60.h
+++ keyboards/gami_studio/lex60/lex60.h
@@ -20,19 +20,19 @@
     {k40, k41, k42, XXX, XXX, k45, k46, XXX, XXX, XXX, k4a, k4b, k4c, k4d, XXX}  \
 }
 
-#define LAYOUT_uhhkb( \
+#define LAYOUT_uhkb( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,      \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b,      k2d,      \
-    k30,      k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,           \
-         k41, k42,           k45, k46,           k49, k4a, k4b                 \
+    k30,      k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d,      \
+         k41, k42,           k45, k46,           k49, k4a                      \
 ) \
 { \
     {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e}, \
     {k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, XXX}, \
     {k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, XXX, k2d, XXX}, \
-    {k30, XXX, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, XXX, XXX}, \
-    {XXX, k41, k42, XXX, XXX, k45, k46, XXX, XXX, k49, k4a, k4b, XXX, XXX, XXX}  \
+    {k30, XXX, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, XXX}, \
+    {XXX, k41, k42, XXX, XXX, k45, k46, XXX, XXX, k49, k4a, XXX, XXX, XXX, XXX}  \
 }
 
 #define LAYOUT_hhkb( \
@@ -40,14 +40,14 @@
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,      \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b,      k2d,      \
     k30,      k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d,      \
-         k41, k42,           k45, k46,                     k4b, k4c            \
+         k41, k42,           k45, k46,                k4a, k4b, k4c            \
 ) \
 { \
     {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e}, \
     {k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, XXX}, \
     {k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, XXX, k2d, XXX}, \
     {k30, XXX, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, XXX}, \
-    {XXX, k41, k42, XXX, XXX, k45, k46, XXX, XXX, XXX, XXX, k4b, k4c, XXX, XXX}  \
+    {XXX, k41, k42, XXX, XXX, k45, k46, XXX, XXX, XXX, k4a, k4b, k4c, XXX, XXX}  \
 }
 
 #define LAYOUT_iso( \
