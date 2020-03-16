071368614038ffc6cc1056cc7ce39aaa9fe590d6
mtei <2170248+mtei@users.noreply.github.com>
2020-03-17 01:35:50 +0900

    Changed how to define avr port

--- quantum/config_common.h
+++ quantum/config_common.h
@@ -29,7 +29,66 @@
 #    endif
 #    define PORT_SHIFTER 4  // this may be 4 for all AVR chips
 
-#    include "avr_portaddress_defs.h"
+// If you want to add more to this list, copy from avr_full_port_address_defs.text
+#ifdef __AVR_ATmega32U4__
+#define ADDRESS_BASE (0x03 & 0xf0)
+#define PINB_ADDRESS (0x03 & 0xf)
+#define PINC_ADDRESS (0x06 & 0xf)
+#define PIND_ADDRESS (0x09 & 0xf)
+#define PINE_ADDRESS (0x0C & 0xf)
+#define PINF_ADDRESS (0x0F & 0xf)
+#endif
+#ifdef __AVR_ATmega16U4__
+#define ADDRESS_BASE (0x03 & 0xf0)
+#define PINB_ADDRESS (0x03 & 0xf)
+#define PINC_ADDRESS (0x06 & 0xf)
+#define PIND_ADDRESS (0x09 & 0xf)
+#define PINE_ADDRESS (0x0C & 0xf)
+#define PINF_ADDRESS (0x0F & 0xf)
+#endif
+#ifdef __AVR_ATmega32U2__
+#define ADDRESS_BASE (0x03 & 0xf0)
+#define PINB_ADDRESS (0x03 & 0xf)
+#define PINC_ADDRESS (0x06 & 0xf)
+#define PIND_ADDRESS (0x09 & 0xf)
+#endif
+#ifdef __AVR_ATmega16U2__
+#define ADDRESS_BASE (0x03 & 0xf0)
+#define PINB_ADDRESS (0x03 & 0xf)
+#define PINC_ADDRESS (0x06 & 0xf)
+#define PIND_ADDRESS (0x09 & 0xf)
+#endif
+#ifdef __AVR_AT90USB1286__
+#define ADDRESS_BASE (0X00 & 0xf0)
+#define PINA_ADDRESS (0X00 & 0xf)
+#define PINB_ADDRESS (0X03 & 0xf)
+#define PINC_ADDRESS (0x06 & 0xf)
+#define PIND_ADDRESS (0x09 & 0xf)
+#define PINE_ADDRESS (0x0C & 0xf)
+#define PINF_ADDRESS (0x0F & 0xf)
+#endif
+#ifdef __AVR_AT90USB646__
+#define ADDRESS_BASE (0X00 & 0xf0)
+#define PINA_ADDRESS (0X00 & 0xf)
+#define PINB_ADDRESS (0X03 & 0xf)
+#define PINC_ADDRESS (0x06 & 0xf)
+#define PIND_ADDRESS (0x09 & 0xf)
+#define PINE_ADDRESS (0x0C & 0xf)
+#define PINF_ADDRESS (0x0F & 0xf)
+#endif
+#ifdef __AVR_ATmega32A__
+#define ADDRESS_BASE (0x19 & 0xf0)
+#define PINA_ADDRESS (0x19 & 0xf)
+#define PINB_ADDRESS (0x16 & 0xf)
+#define PINC_ADDRESS (0x13 & 0xf)
+#define PIND_ADDRESS (0x10 & 0xf)
+#endif
+#ifdef __AVR_ATmega328P__
+#define ADDRESS_BASE (0x03 & 0xf0)
+#define PINB_ADDRESS (0x03 & 0xf)
+#define PINC_ADDRESS (0x06 & 0xf)
+#define PIND_ADDRESS (0x09 & 0xf)
+#endif
 
 /* I/O pins */
 #    define PINDEF(port, pin) ((PIN##port##_ADDRESS << PORT_SHIFTER) | pin)
