8e130ae460122bb949d65e4099b2a1c7affe67a4
Takeshi Nishio <kkeennnn@gmail.com>
2020-03-17 05:48:31 +0900

    Update Fix migrate errors from test_duplex_dp to test_col2col

--- keyboards/test_col2col/config.h
+++ keyboards/test_col2col/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0000
 #define DEVICE_VER 0x0001
 #define MANUFACTURER jpskenn
-#define PRODUCT test_duplex_dp
+#define PRODUCT test_col2col2
 #define DESCRIPTION A custom keyboard
 
 /* key matrix size */
