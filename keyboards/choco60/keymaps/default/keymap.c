01cd2421e3907f702f11e6c94fcbdba98b6522dd
erugis0345 <tl091264@gmail.com>
2020-03-16 22:41:42 +0900

    Synchronize with HHKB keymap
    The key layout has been adjusted to the HHKB key map (Lite extension mode).
    Reference: https://www.pfu.fujitsu.com/hhkeyboard/leaflet/hairetu.html

--- keyboards/choco60/keymaps/default/keymap.c
+++ keyboards/choco60/keymaps/default/keymap.c
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_FN] = LAYOUT(
     _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INSERT, KC_DELETE,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, KC_UP, KC_RBRACKET, KC_BSPACE,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_ENTER,
-    RESET, _______, _______, _______, _______, _______, _______, _______, KC_END, KC_PGDOWN, KC_DOWN, KC_RSHIFT, KC_FN,
+    KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, KC_UP, KC_RBRACKET, KC_BSPACE,
+    _______, _______, _______, _______, _______, _______, KC_ASTR, KC_SLSH, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_ENTER,
+    RESET, _______, _______, _______, _______, _______, KC_PLUS, KC_MINS, KC_END, KC_PGDOWN, KC_DOWN, KC_RSHIFT, KC_FN,
     _______, _______, _______, _______, _______, KC_STOP, _______
   )
 };
