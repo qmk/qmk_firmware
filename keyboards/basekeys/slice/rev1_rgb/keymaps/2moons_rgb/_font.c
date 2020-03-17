dd873a0165ae4e3d2c6889359c1c805bdc6661a3
2Moons-JP <57225836+2Moons-JP@users.noreply.github.com>
2020-03-18 00:13:03 +0900

    Update keyboards/basekeys/slice/rev1_rgb/keymaps/2moons_rgb/_font.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/basekeys/slice/rev1_rgb/keymaps/2moons_rgb/_font.c
+++ keyboards/basekeys/slice/rev1_rgb/keymaps/2moons_rgb/_font.c
@@ -1,17 +1,7 @@
 // This is the 'classic' fixed-space bitmap font for Adafruit_GFX since 1.0.
 // See gfxfont.h for newer custom bitmap font info.
 
-#ifndef FONT5X7_H
-#define FONT5X7_H
-
-#ifdef __AVR__
- #include <avr/io.h>
- #include <avr/pgmspace.h>
-#elif defined(ESP8266)
- #include <pgmspace.h>
-#else
- #define PROGMEM
-#endif
+#include "progmem.h"
 
 // Standard ASCII 5x7 font
 const unsigned char font[] PROGMEM = {
