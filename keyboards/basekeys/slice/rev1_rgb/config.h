9ef449ded817bd86cc9fa1330727f5786a2cb11b
2Moons-JP <nevans10@hotmail.com>
2020-03-17 18:02:09 +0900

    Slice Keyboard Update

--- keyboards/basekeys/slice/rev1_rgb/config.h
+++ keyboards/basekeys/slice/rev1_rgb/config.h
@@ -59,4 +59,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_ANIMATIONS
 #endif
 
-#define OLED_FONT_H "keyboards/basekeys/slice/keymaps/2moons_rgb/slice_font.c"
+#define OLED_FONT_H "keyboards/basekeys/slice/rev1_rgb/keymaps/2moons/slice_font.c"
