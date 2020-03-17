e12a803462be064deaa63e47a7aa199cc65f0862
2Moons-JP <57225836+2Moons-JP@users.noreply.github.com>
2020-03-17 23:55:24 +0900

    Update keyboards/basekeys/slice/slice.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/basekeys/slice/slice.h
+++ keyboards/basekeys/slice/slice.h
@@ -1,15 +1,6 @@
 #pragma once
 
 #include "quantum.h"
-
-#ifdef KEYBOARD_basekeys_slice_rev0
-  #include "rev0.h"
-#endif
-
-#ifdef KEYBOARD_basekeys_slice_rev0_rgb
-  #include "rev0.h"
-#endif
-
 #ifdef KEYBOARD_basekeys_slice_rev1
   #include "rev1.h"
 #endif
@@ -17,4 +8,3 @@
 #ifdef KEYBOARD_basekeys_slice_rev1_rgb
   #include "rev1.h"
 #endif
-
