bbe44053798bafd5af7bfe5f35cfe50523ae6fc9
2Moons-JP <57225836+2Moons-JP@users.noreply.github.com>
2020-03-18 17:12:58 +0900

    Update keyboards/basekeys/slice/rev1_rgb/rules.mk
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/basekeys/slice/rev1_rgb/rules.mk
+++ keyboards/basekeys/slice/rev1_rgb/rules.mk
@@ -17,10 +17,10 @@ SPLIT_KEYBOARD = yes
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
-CONSOLE_ENABLE = no        # Console for debug(+400)
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
