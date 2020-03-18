5aec6cdb0927c1cd1b63273bed01c3c18ad7ad1e
Joshua Diamond <josh@windowoffire.com>
2020-03-18 14:14:59 -0400

    Rename users/spidey3/rgblight.c to layer_rgb.c per suggestion

--- users/spidey3/rules.mk
+++ users/spidey3/rules.mk
@@ -3,7 +3,7 @@ SRC += init.c
 SRC += spidey3.c
 
 ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
-  SRC += rgblight.c
+  SRC += layer_rgb.c
 endif
 ifeq ($(strip $(UNICODEMAP_ENABLE)), yes)
   SRC += unicode.c
