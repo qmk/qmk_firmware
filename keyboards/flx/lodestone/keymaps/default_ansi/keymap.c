c9d0c211b41420a4363af92806330f037f53c8f7
James Young <xxiinophobia@yahoo.com>
2020-03-18 16:50:14 -0700

    rename layout macros
    LAYOUT_ansi -> LAYOUT_65_ansi_blocker_split_bs
    LAYOUT_iso -> LAYOUT_65_iso_blocker_split_bs

--- keyboards/flx/lodestone/keymaps/default_ansi/keymap.c
+++ keyboards/flx/lodestone/keymaps/default_ansi/keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	// Default layer
-	[0] = LAYOUT_ansi(
+	[0] = LAYOUT_65_ansi_blocker_split_bs(
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_HOME,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
 		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),            KC_LEFT, KC_DOWN, KC_RGHT),
 
 	// Fn1 Layer
-	[1] = LAYOUT_ansi(
+	[1] = LAYOUT_65_ansi_blocker_split_bs(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,  KC_TRNS,
 		KC_CAPS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS,          KC_TRNS,
 		KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP,          KC_TRNS,          KC_TRNS,
--- keyboards/flx/lodestone/lodestone.h
+++ keyboards/flx/lodestone/lodestone.h
@@ -36,7 +36,7 @@
     { K40, K41, K42, ___, ___, ___, K46, ___, ___, ___, K4A, K4B, ___, K4D, K4E, K4F }, \
 }
 
-#define LAYOUT_ansi( \
+#define LAYOUT_65_ansi_blocker_split_bs( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F,   \
      K10,  K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,  K1D,   K1F,   \
       K20,   K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,  K2F,   \
@@ -50,7 +50,7 @@
     { K40, K41, K42, ___, ___, ___, K46, ___, ___, ___, K4A, K4B, ___, K4D, K4E, K4F }, \
 }
 
-#define LAYOUT_iso( \
+#define LAYOUT_65_iso_blocker_split_bs( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F,   \
      K10,  K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,         K1F,   \
       K20,   K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,  K2F,   \
