cc7e7449e3a664d8ca2138c166a53dc63f2eb50e
CryptoKid91 <12398829+CryptoKid91@users.noreply.github.com>
2020-03-18 12:39:16 +0200

    Update keyboards/xd75/keymaps/cryptokid91/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/xd75/keymaps/cryptokid91/keymap.c
+++ keyboards/xd75/keymaps/cryptokid91/keymap.c
@@ -51,13 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 							      
- [_QWERTY] = { /* SPLIT QWERTY */
-  { KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSCR, KC_SLCK, KC_LOCK, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS  },
-  { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC  },
-  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT  },
-  { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_N,   KC_M,    KC_COMM, KC_DOT,  MS_US,   KC_SLSH  },
-  { KC_APP,  KC_NUBS, KC_LGUI, KC_LALT, MS_LW,   KC_SPC,  MS_P00,  KC_P0,   KC_PDOT, KC_ENT, MS_RS,   KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT },
- },
+ [_QWERTY] = LAYOUT_ortho_5x15( /* SPLIT QWERTY */
+   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PSCR, KC_SLCK, KC_LOCK, KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_N,   KC_M,    KC_COMM, KC_DOT,  MS_US,   KC_SLSH,
+   KC_APP,  KC_NUBS, KC_LGUI, KC_LALT, MS_LW,   KC_SPC,  MS_P00,  KC_P0,   KC_PDOT, KC_ENT, MS_RS,   KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+ ),
 
 /* LOWERED
  * .--------------------------------------------------------------------------------------------------------------------------------------.
