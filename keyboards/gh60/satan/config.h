55cb8a58097d221751c6ce02a053d8a91ecfa5e9
Diego Song <diego.m.song@gmail.com>
2020-03-18 14:57:01 -0400

    Removed inline comment next to VID for GH60 Satan

--- keyboards/gh60/satan/config.h
+++ keyboards/gh60/satan/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x4335 // GEEKhack
+#define VENDOR_ID       0x4335
 #define PRODUCT_ID      0x0002
 #define DEVICE_VER      0x0003
 #define MANUFACTURER    SATAN
