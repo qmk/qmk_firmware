28005bc70cbc23c25a4e88d3a54c0b061ad31974
Xelus22 <lovlots@hotmail.com>
2020-03-18 21:30:30 +1100

    remove define and add 3rd layer for VIA keymap

--- keyboards/xelus/snap96/keymaps/default/keymap.c
+++ keyboards/xelus/snap96/keymaps/default/keymap.c
@@ -1,8 +1,6 @@
 // Default layout for Snap96
 #include QMK_KEYBOARD_H
 
-#define _______ KC_TRNS
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,  KC_PSCR, KC_SLCK, KC_HOME, KC_END,
