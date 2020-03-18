254fad9f5bf745d32fe65cccd76ba1ac8813067d
Flexerm <51996324+Flexerm@users.noreply.github.com>
2020-03-18 08:38:26 +0000

    Update keyboards/flx/lodestone/rules.mk
    Changed Link_Time_Optimization to LTO didn't know this was a thing :)
    
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/flx/lodestone/rules.mk
+++ keyboards/flx/lodestone/rules.mk
@@ -30,4 +30,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
-LINK_TIME_OPTIMIZATION_ENABLE = yes
+LTO_ENABLE = yes
