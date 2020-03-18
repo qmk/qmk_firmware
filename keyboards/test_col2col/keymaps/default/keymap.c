d66984ddc12b15563463cb3de7bbe5cd422f3c13
Takeshi Nishio <kkeennnn@gmail.com>
2020-03-18 21:58:55 +0900

    Testing

--- keyboards/test_col2col/keymaps/default/keymap.c
+++ keyboards/test_col2col/keymaps/default/keymap.c
@@ -30,10 +30,12 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_1,    KC_2,   KC_3,    KC_4, KC_5, KC_6, KC_A,KC_B,KC_C,KC_D,KC_E,KC_F
+        KC_1,   KC_2,   KC_3,   KC_4, \
+        KC_A,   KC_B,   KC_C,   KC_D
     ),
     [_FN] = LAYOUT(
-      KC_1,    KC_2,   KC_3,    KC_4, KC_5, KC_6, KC_A,KC_B,KC_C,KC_D,KC_E,KC_F
+        KC_1,   KC_2,   KC_3,   KC_4, \
+        KC_A,   KC_B,   KC_C,   KC_D
     )
 };
 
