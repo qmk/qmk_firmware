88f7f0bf19ff4e658ce40f29dcdf0919825c77e3
Álvaro A. Volpato <alvaro.volpato@usp.br>
2020-03-19 16:28:53 -0300

    Add botmagic support and remoce console_enable
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/acheron/elongate/rules.mk
+++ keyboards/acheron/elongate/rules.mk
@@ -13,10 +13,10 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
