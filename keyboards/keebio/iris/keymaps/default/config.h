6ad3328b8310475caac160801a4de5a6abd162bd
zvecr <git@zvecr.com>
2020-03-19 00:43:20 +0000

    Allow RGBLIGHT_ANIMATIONS to work on keebio/iris configurator builds

--- keyboards/keebio/iris/keymaps/default/config.h
+++ keyboards/keebio/iris/keymaps/default/config.h
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 #undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 12
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
