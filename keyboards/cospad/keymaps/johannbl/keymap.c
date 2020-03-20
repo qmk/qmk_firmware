0e326cc585012a9f9f23e2b1181f805e25b52470
johannbl <jblanteigne@gmail.com>
2020-03-20 17:03:10 -0400

    Update keymap.c
    Added a macro for the "00" key.

--- keyboards/cospad/keymaps/johannbl/keymap.c
+++ keyboards/cospad/keymaps/johannbl/keymap.c
@@ -6,6 +6,27 @@
  * edit it directly.
  */
 
+enum custom_keycodes {
+  DOUBLEZERO = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case DOUBLEZERO:
+      if (record->event.pressed) {
+        // when keycode DOUBLEZERO is pressed
+        SEND_STRING("00");
+      } else {
+        // when keycode DOUBLEZERO is released
+      }
+      break;
+
+  }
+  return true;
+};
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /* Layer 0
  		* ,-------------------.
@@ -28,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         	KC_P7, KC_P8, KC_P9, KC_PMNS, 
         	KC_P4, KC_P5, KC_P6, KC_PPLS, 
         	KC_P1, KC_P2, KC_P3,
-        	KC_F24, KC_P0, KC_PDOT, KC_PENT
+        	DOUBLEZERO, KC_P0, KC_PDOT, KC_PENT
         ),
         
         [1] = LAYOUT_numpad2_6x4(
