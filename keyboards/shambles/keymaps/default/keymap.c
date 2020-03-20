13e5ed482eedca5492d765cefcb6e2c532641e61
OsciX <ryrutdavis@gmail.com>
2020-03-20 14:00:13 -0400

    Fix Shambles again

--- keyboards/shambles/keymaps/default/keymap.c
+++ keyboards/shambles/keymaps/default/keymap.c
@@ -8,28 +8,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_RSFT, KC_UP, 
 		KC_Z, KC_X, KC_C, KC_V, KC_SPC, KC_B, KC_N, KC_M, KC_LEFT, KC_DOWN, KC_RGHT)
 };
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-	keyevent_t event = record->event;
-
-	switch (id) {
-
-	}
-	return MACRO_NONE;
-}
-
-void matrix_init_user(void) {
-	
-}
-
-void matrix_scan_user(void) {
-	
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
