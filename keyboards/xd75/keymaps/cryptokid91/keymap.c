7bb01fbbcc9b7ccac9b7f66248af0533dfdbc26c
CryptoKid91 <12398829+CryptoKid91@users.noreply.github.com>
2020-03-21 03:10:28 +0200

    Update keyboards/xd75/keymaps/cryptokid91/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/xd75/keymaps/cryptokid91/keymap.c
+++ keyboards/xd75/keymaps/cryptokid91/keymap.c
@@ -157,7 +157,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       _delay_ms(100);
       SEND_STRING("www.io/tech.fi");
       _delay_ms(100);
-      SEND_STRING(SS_TAP(X_ENTER));
+      tap_code(KC_ENT);
     }
     return false;
   case TERM:
