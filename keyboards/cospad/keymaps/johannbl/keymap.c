e9751b44aef6a28bdea720d904113fcea56be0d1
johannbl <jblanteigne@gmail.com>
2020-03-21 15:50:36 -0400

    Update keyboards/cospad/keymaps/johannbl/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/cospad/keymaps/johannbl/keymap.c
+++ keyboards/cospad/keymaps/johannbl/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  		* | 00 | 0  | .  |    |
  		* `-------------------'
  		*/
-        [0] = LAYOUT_numpad2_6x4(
+        [0] = LAYOUT_numpad_6x4_split_plus_zero(
         	KC_MUTE, KC_VOLD, KC_VOLU, TT(2), 
         	KC_TAB, KC_PSLS, KC_PAST, KC_BSPC, 
         	KC_P7, KC_P8, KC_P9, KC_PMNS, 
