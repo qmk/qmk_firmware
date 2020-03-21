5c0f59d9496d6d20dd85f9cb6395f4d21175a60b
artjomsR <artjomsR@users.noreply.github.com>
2020-03-21 16:14:39 +0000

    Update keyboards/split75/config.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/split75/config.h
+++ keyboards/split75/config.h
@@ -14,9 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #define VENDOR_ID       0x20A0
 #define PRODUCT_ID      0x422D
