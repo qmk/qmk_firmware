002a0da0e34df9e1b3ad6db4e45e2663dcaef3f7
artjomsR <artjomsR@users.noreply.github.com>
2020-03-21 16:14:30 +0000

    Update keyboards/split75/config.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/split75/config.h
+++ keyboards/split75/config.h
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define VENDOR_ID       0x20A0
 #define PRODUCT_ID      0x422D
-// TODO: share these strings with usbconfig.h
-// Edit usbconfig.h to change these.
 #define MANUFACTURER    Wheatfield
 #define PRODUCT         Split75   
 
