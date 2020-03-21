89fbe36fea2ff4d029db1454162f1d1dceb1cd62
spidey3 <josh@windowoffire.com>
2020-03-21 14:27:54 -0400

    Update users/spidey3/layer_rgb.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- users/spidey3/layer_rgb.c
+++ users/spidey3/layer_rgb.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
-#include <spidey3.h>
-#include <velocikey.h>
+#include "spidey3.h"
+#include "velocikey.h"
 
 extern rgblight_config_t rgblight_config;
 
@@ -81,4 +81,3 @@ bool process_record_user_rgb(uint16_t keycode, keyrecord_t *record) {
 
   return true;
 }
-
