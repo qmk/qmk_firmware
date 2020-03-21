e15daa4ecc1dc2bad5f8e7865e2854cb74e9a0ea
Art <blah>
2020-03-21 18:21:23 +0000

    defined tapping toggle as double tap

--- keyboards/split75/config.h
+++ keyboards/split75/config.h
@@ -31,3 +31,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_UART 1
 /* key combination for command */
 #define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
+
+#define TAPPING_TOGGLE  2
