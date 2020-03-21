1f36a794e2baca6362bf4486a0d788258d97d228
artjomsR <artjomsR@users.noreply.github.com>
2020-03-21 17:11:22 +0000

    Update keyboards/split75/usbconfig.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/split75/usbconfig.h
+++ keyboards/split75/usbconfig.h
@@ -251,8 +251,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'p', 's', '2', 'a', 'v', 'r', 'G', 'B'
-#define USB_CFG_DEVICE_NAME_LEN 8
+#define USB_CFG_DEVICE_NAME     's', 'p', 'l', 'i', 't', '7', '5'
+#define USB_CFG_DEVICE_NAME_LEN 7
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
