44996eb3d19e99733dfc774255e322c5e6282ace
Lauren <codecoffeecode@pop-os.localdomain>
2020-03-21 17:10:32 -0700

    Updating tests: removing keyboard name prefix

--- lib/python/qmk/tests/test_cli_commands.py
+++ lib/python/qmk/tests/test_cli_commands.py
@@ -62,35 +62,35 @@ def test_list_keymaps_short():
     # check short arg format
     result = check_subcommand('list-keymaps', '-kb', 'planck/ez')
     assert result.returncode == 0
-    assert 'planck/ez:default' and 'planck/ez:drashna' in result.stdout
+    assert 'default' and 'drashna' in result.stdout
 
 
 def test_list_keymaps_long():
     # check long arg format
     result = check_subcommand('list-keymaps', '--keyboard', 'planck/ez')
     assert result.returncode == 0
-    assert 'planck/ez:default' and 'planck/ez:drashna' in result.stdout
+    assert 'default' and 'drashna' in result.stdout
 
 
 def test_list_keymaps_kb_only():
     # check keyboard param
     result = check_subcommand('list-keymaps', '-kb', 'niu_mini')
     assert result.returncode == 0
-    assert 'niu_mini:default' in result.stdout
+    assert 'default' in result.stdout
 
 
 def test_list_keymaps_vendor_kb():
     # check vendor/keyboard param
     result = check_subcommand('list-keymaps', '-kb', 'ai03/lunar')
     assert result.returncode == 0
-    assert 'ai03/lunar:default' and 'ai03/lunar:via' in result.stdout
+    assert 'default' and 'via' in result.stdout
 
 
 def test_list_keymaps_vendor_kb_rev():
     # check vendor/keyboard/revision param
     result = check_subcommand('list-keymaps', '-kb', 'kbdfans/kbd67/mkiirgb/v2')
     assert result.returncode == 0
-    assert 'kbdfans/kbd67/mkiirgb/v2:default' and 'kbdfans/kbd67/mkiirgb/v2:kemmeldev' in result.stdout
+    assert 'default' and 'kemmeldev' in result.stdout
 
 
 def test_list_keymaps_no_such_keyboard():
