59b1f993abd2c3e5a7ed58e35f3be35b584de981
hsgw <urkwtky@gmail.com>
2020-03-24 17:09:06 +0900

    fix ergoinu rules.mk

--- keyboards/dm9records/ergoinu/rules.mk
+++ keyboards/dm9records/ergoinu/rules.mk
@@ -26,7 +26,7 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SUBPROJECT_rev1 = no
 USE_I2C = no               # i2c is not supported
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
@@ -34,8 +34,6 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 CUSTOM_MATRIX = yes
 SRC += matrix.c serial.c split_util.c
-
-DEFAULT_FOLDER = ergoinu
 
 # ergoinu configs
 DISABLE_PROMICRO_LEDs = yes
