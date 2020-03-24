cdb779626e9f84e9ef002085ab7e0a3c15902a99
2Moons-JP <57225836+2Moons-JP@users.noreply.github.com>
2020-03-24 12:13:07 +0900

    Update keyboards/basekeys/slice/rev1/config.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/basekeys/slice/rev1/config.h
+++ keyboards/basekeys/slice/rev1/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // wiring of each half
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5}
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5 }
 
 #define DIODE_DIRECTION COL2ROW
 
