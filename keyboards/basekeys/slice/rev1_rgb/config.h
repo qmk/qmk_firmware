1301538bb88df9c949f1e4cab330aa60b0c2cc0d
2Moons-JP <57225836+2Moons-JP@users.noreply.github.com>
2020-03-24 12:18:29 +0900

    Update keyboards/basekeys/slice/rev1_rgb/config.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/basekeys/slice/rev1_rgb/config.h
+++ keyboards/basekeys/slice/rev1_rgb/config.h
@@ -34,12 +34,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5 }
 
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
 #define DIODE_DIRECTION COL2ROW
 
-#define RGB_KEYBOARD
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
 
 /* serial.c configuration for split keyboard */
 #define SOFT_SERIAL_PIN D2
