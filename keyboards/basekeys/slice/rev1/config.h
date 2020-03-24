577341f113921aab144508f5a5ee24c3adb43836
2Moons-JP <57225836+2Moons-JP@users.noreply.github.com>
2020-03-24 12:10:19 +0900

    Update keyboards/basekeys/slice/rev1/config.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/basekeys/slice/rev1/config.h
+++ keyboards/basekeys/slice/rev1/config.h
@@ -38,9 +38,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
-
-#define DIODE_DIRECTION COL2ROW
-
 /* serial.c configuration for split keyboard */
 #define SOFT_SERIAL_PIN D2
 
