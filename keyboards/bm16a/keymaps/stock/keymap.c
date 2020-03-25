64e7a95f0d8633d46a1b0ab4559028092cbed009
Jakub Darowski <jakubdarowski@gmail.com>
2020-03-25 09:06:16 +0100

    Update keyboards/bm16a/keymaps/stock/keymap.c
    Removed trailing backslashes from layouts
    
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/bm16a/keymaps/stock/keymap.c
+++ keyboards/bm16a/keymaps/stock/keymap.c
@@ -23,15 +23,15 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_ortho_4x4(
-    KC_P7,    KC_P8,    KC_P9,    KC_PMNS,  \
-    KC_P4,    KC_P5,    KC_P6,    KC_PPLS,  \
-    KC_P1,    KC_P2,    KC_P3,    KC_PENT,  \
-    KC_P0,    KC_PDOT,  KC_SPC,   MO(_FN1)  \
+    KC_P7,    KC_P8,    KC_P9,    KC_PMNS,
+    KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+    KC_P0,    KC_PDOT,  KC_SPC,   MO(_FN1)
   ),
   [_FN1] = LAYOUT_ortho_4x4(
-    RESET,    KC_PAST,  KC_PSLS,  _______,  \
-    BL_TOGG,  BL_DEC,   BL_INC,   BL_STEP,  \
-    RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  \
-    RGB_SAI,  RGB_SAD,  _______,  _______   \
+    RESET,    KC_PAST,  KC_PSLS,  _______,
+    BL_TOGG,  BL_DEC,   BL_INC,   BL_STEP,
+    RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,
+    RGB_SAI,  RGB_SAD,  _______,  _______
   )
 };
