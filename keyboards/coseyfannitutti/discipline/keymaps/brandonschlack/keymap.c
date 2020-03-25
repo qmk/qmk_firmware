c5eccfdf85e670a32ec248c9cda4f2ac25050831
brandonschlack <brandonschlack@gmail.com>
2020-03-25 10:02:55 -0700

    [Keymap] Update discipline keymap to 2_right_mods

--- keyboards/coseyfannitutti/discipline/keymaps/brandonschlack/keymap.c
+++ keyboards/coseyfannitutti/discipline/keymaps/brandonschlack/keymap.c
@@ -26,16 +26,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * │HyCaps│ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│   Enter│PgD│
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
  * │Shift   │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│ Shift│ Up│End│
- * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
- * │Ctrl│ Opt│ Cmd│          Space         │Cmd│Opt│Fn │Lef│Dow│Rig│
- * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+ * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴──┬───┼───┼───┤
+ * │Ctrl│ Opt│ Cmd│          Space         │ Cmd │FnPly│Lef│Dow│Rig│
+ * └────┴────┴────┴────────────────────────┴─────┴─────┴───┴───┴───┘
  */
-[_BASE] = LAYOUT_65_ansi( \
+[_BASE] = LAYOUT_65_ansi_2_right_mods( \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, \
-    HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_PGDN, \
+    HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN, \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,  \
-    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                    RWD_GUI, FFD_ALT, PLY_FN1, KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RGUI, PLY_FN1,          KC_LEFT, KC_DOWN, KC_RGHT  \
 ),
 /* Function Layer
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
@@ -45,16 +45,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
  * │      │   │   │   │   │   │   │   │   │   │   │   │        │VlD│
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
- * │        │   │   │   │   │   │   │   │   │   │   │      │PgU│Mut│
- * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
- * │    │    │    │                        │   │   │   │Hom│PgD│End│
- * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+ * │        │   │   │   │   │RST│   │Mke│Prv│Nxt│Ply│      │PgU│Mut│
+ * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴──┬───┼───┼───┤
+ * │    │    │    │                        │     │     │Hom│PgD│End│
+ * └────┴────┴────┴────────────────────────┴─────┴─────┴───┴───┴───┘
  */
-[_FN1] = LAYOUT_65_ansi(
+[_FN1] = LAYOUT_65_ansi_2_right_mods( \
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DELT, MC_SLPD, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F13,  KC_F14,  KC_F15,  MC_LHPD, KC_VOLU, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLD, \
-    _______,          _______, _______, _______, _______, _______, _______, QM_MAKE, _______, _______, _______, _______, KC_PGUP, KC_MUTE, \
-    _______, _______, _______,                            _______,                   _______, _______, _______, KC_HOME, KC_PGDN, KC_END   \
+    _______,          _______, _______, _______, _______, RESET,   _______, QM_MAKE, KC_MPRV, KC_MNXT, KC_MPLY, _______, KC_PGUP, KC_MUTE, \
+    _______, _______, _______,                            _______,                   _______, _______,          KC_HOME, KC_PGDN, KC_END   \
 )
 };
