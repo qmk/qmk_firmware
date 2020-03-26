662aaae861c8605be26a1462566d48e608f85738
cfbender <codyfbender@gmail.com>
2020-03-26 14:42:57 -0600

    Flip encoder pins

--- keyboards/absinthe/config.h
+++ keyboards/absinthe/config.h
@@ -48,6 +48,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENCODERS_PAD_A { F1 }
 #define ENCODERS_PAD_B { F0 }
 
+#define ENCODER_DIRECTION_FLIP
+
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
