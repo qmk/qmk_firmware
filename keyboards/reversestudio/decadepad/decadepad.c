757a279566ab9357ce05cea4cc8f7e9550983c82
‭huajijam <strhuaji@gmail.com>
2020-03-27 03:13:07 +0800

    fix bug

--- keyboards/reversestudio/decadepad/decadepad.c
+++ keyboards/reversestudio/decadepad/decadepad.c
@@ -1,4 +1,4 @@
-#include "DecadePad.h"
+#include "decadepad.h"
 bool led_update_kb(led_t led_state) {
     if (led_update_user(led_state)){
         // writePin sets the pin high for 1 and low for 0.
