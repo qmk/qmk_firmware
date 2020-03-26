634fde6b343c25f7e2399225ea1f0dac0d6df4f8
‭huajijam <strhuaji@gmail.com>
2020-03-27 03:47:51 +0800

    Final Version
    Fix all bugs

--- keyboards/reversestudio/decadepad/keymaps/default/keymap.c
+++ keyboards/reversestudio/decadepad/keymaps/default/keymap.c
@@ -1,12 +1,12 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_numpad_6x4(
-  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_M_R,
-  KC_NLCK,  KC_PSLS,  KC_PAST,  KC_PMNS,
-  KC_P7,    KC_P8,    KC_P9,        
-  KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
-  KC_P1,    KC_P2,    KC_P3,    
-  KC_P0,              KC_PDOT,  KC_PENT        
-) 
+  [0] = LAYOUT_numpad_6x4(
+    RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_M_R,
+    KC_NLCK,  KC_PSLS,  KC_PAST,  KC_PMNS,
+    KC_P7,    KC_P8,    KC_P9,        
+    KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+    KC_P1,    KC_P2,    KC_P3,    
+    KC_P0,              KC_PDOT,  KC_PENT        
+  ) 
 };
