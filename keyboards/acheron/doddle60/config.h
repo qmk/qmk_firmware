5e1e0894965dd860dadd1dc995cd31476fca27eb
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2020-03-26 14:50:21 -0300

    Rename PRODUCT_ID to 4436

--- keyboards/acheron/doddle60/config.h
+++ keyboards/acheron/doddle60/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4150 // AP for Acheron Project
-#define PRODUCT_ID      0x4433 // D6 for Doddle60 
+#define PRODUCT_ID      0x4436 // D6 for Doddle60 
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AcheronProject
 #define PRODUCT         Doddle60 rev. pre-Alpha
