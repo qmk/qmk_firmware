059f7f147d446f090453e09f813882070627b5ea
Croktopus <39040552+Croktopus@users.noreply.github.com>
2020-03-26 02:48:02 -0700

    first try

--- keyboards/timberwolf/config.h
+++ keyboards/timberwolf/config.h
@@ -20,16 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x0000
+#define VENDOR_ID 0x6D6D
+#define PRODUCT_ID 0x5754
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Croktopus
-#define PRODUCT timberwolf
+#define PRODUCT Timber Wolf
 #define DESCRIPTION A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 17
+#define MATRIX_COLS 6
 
 /*
  * Keyboard Matrix Assignments
@@ -41,17 +41,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { B2, B3, B7, D0, D1, D2, B5, B6, C6, C7, F7, F6, F5, F4, F1, F0, E6 }
+#define MATRIX_COL_PINS { D5, D3, D4, D6, D7, B4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
+/* Wncoswe pins */
+#define ENCODERS_PAD_A { B12 }
+#define ENCODERS_PAD_B { B13 }
+
+/* Encoder resolution, lower number = more sensitive */
+#define ENCODER_RESOLUTION 4
+
+/* Reverse encoder direction */
+// #define ENCODER_DIRECTION_FLIP
+
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+// #define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
@@ -92,9 +102,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MATRIX_HAS_GHOST
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+// #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+// #define LOCKING_RESYNC_ENABLE
 
 /* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
  * This is userful for the Windows task manager shortcut (ctrl+shift+esc).
