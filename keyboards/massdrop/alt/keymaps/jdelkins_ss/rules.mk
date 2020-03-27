d6f57257cdd97178397629dfaa3828aaa61d8531
Joel D. Elkins <joel@elkins.co>
2020-03-27 16:08:49 -0500

    [Keymap] merge jdelkins userspace and associated keymaps

---
+++ keyboards/massdrop/alt/keymaps/jdelkins_ss/rules.mk
@@ -0,0 +1,9 @@
+ifeq ($(OS),Windows_NT)
+	MDLOADER = bin/mdloader_windows
+else
+	MDLOADER = sudo bin/mdloader_linux
+endif
+
+doflash: .build/massdrop_alt_jdelkins_ss.hex
+	$(MDLOADER) --first --download $< --restart
+
