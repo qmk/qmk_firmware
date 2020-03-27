d686be4b17f54b7d1624b36f85b52fd661bc5964
artjomsR <artjomsR@users.noreply.github.com>
2020-03-27 10:55:49 +0000

    Update keyboards/split75/config.h
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/split75/config.h
+++ keyboards/split75/config.h
@@ -29,7 +29,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGBLED_NUM 8
 #define RGBLIGHT_ANIMATIONS
-
-#define NO_UART 1
-/* key combination for command */
-#define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
