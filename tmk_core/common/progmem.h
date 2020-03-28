02b1c7e1d09b3c44d9ef812d2d69d83799510ca6
Casper Weiss Bang <master@thecdk.net>
2020-03-29 00:35:11 +0100

    fixed problem with implicit declaration in quantum/rgblight.c (#8406)
    * Update tmk_core/common/progmem.h
    
    Co-Authored-By: Ryan <fauxpark@gmail.com>
    
    * Update quantum/rgblight.c
    
    Co-Authored-By: Ryan <fauxpark@gmail.com>
    
    * fixed problem with implicit declaration in quantum/rgblight.c (#8381)
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- tmk_core/common/progmem.h
+++ tmk_core/common/progmem.h
@@ -4,7 +4,12 @@
 #    include <avr/pgmspace.h>
 #else
 #    define PROGMEM
-#    define pgm_read_byte(p) *((unsigned char*)(p))
-#    define pgm_read_word(p) *((uint16_t*)(p))
-#    define pgm_read_dword(p) *((uint32_t*)(p))
+#    define memcpy_P(dest, src, n) memcpy(dest, src, n)
+#    define pgm_read_byte(address_short) *((uint8_t*)(address_short))
+#    define pgm_read_word(address_short) *((uint16_t*)(address_short))
+#    define pgm_read_dword(address_short) *((uint32_t*)(address_short))
+#    define pgm_read_ptr(address_short) *((void*)(address_short))
+#    define strcmp_P(s1, s2) strcmp(s1, s2)
+#    define strcpy_P(dest, src) strcpy(dest, src)
+#    define strlen_P(src) strlen(src)
 #endif
