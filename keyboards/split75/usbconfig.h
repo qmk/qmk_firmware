c4afee57d75fe6cd4424a6b236ac8b9ad97e343c
artjomsR <artjomsR@users.noreply.github.com>
2020-03-28 14:07:52 +0000

    Update keyboards/split75/usbconfig.h
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/split75/usbconfig.h
+++ keyboards/split75/usbconfig.h
@@ -315,7 +315,6 @@ section at the end of this file).
 #define USB_CFG_DESCR_PROPS_STRING_SERIAL_NUMBER    USB_PROP_IS_DYNAMIC
 #define USB_CFG_DESCR_PROPS_HID                     USB_PROP_IS_DYNAMIC
 #define USB_CFG_DESCR_PROPS_HID_REPORT              USB_PROP_IS_DYNAMIC
-//#define USB_CFG_DESCR_PROPS_HID_REPORT              0
 #define USB_CFG_DESCR_PROPS_UNKNOWN                 0
 
 #define usbMsgPtr_t unsigned short
