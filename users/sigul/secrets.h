9c50d874eda9e65d0416689c077ca461193b0184
sigul <desk@silviogulizia.com>
2020-03-28 16:33:19 +0100

    remove secrets

--- users/sigul/secrets.h
+++
@@ -1,7 +0,0 @@
-static const char *  secrets[] = {
-  "3931044785",
-  "desk" SS_LALT(";") "silviogulizia.com",
-  "https" SS_LSFT(".") SS_LSFT("7") SS_LSFT("7") "silviogulizia.com",
-  "silvio" SS_LALT(";") "vivereintenzionalmente.com",
-  "https" SS_LSFT(".") SS_LSFT("7") SS_LSFT("7") "vivereintenzionalmente.com",
-};
