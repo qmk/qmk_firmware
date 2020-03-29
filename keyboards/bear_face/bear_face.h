b9e294517976316b076be95185e3acb4dff5e5e4
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2020-03-28 22:56:43 -0500

    bear_face initial commit
    Added bear_face PCB
    
    * ATMega32u4-based replacement PCB for the Vortex Race 3
    
    * Default keymap emulates stock board keymap (with a few changes), including layer toggles for QWERTY, COLEMAK, and DVORAK layouts

---
+++ keyboards/bear_face/bear_face.h
@@ -0,0 +1,36 @@
+/*
+Copyright 2020 Will Hedges <wshedges@outlook.com> @chemicalwill
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "quantum.h"
+
+#define LAYOUT_83_ansi( \
+    K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, K014, \
+    K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, K114, \
+    K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213, K214, \
+    K300, K301, K302, K303, K304, K305, K306, K307, K308, K309, K310, K311,       K313, K314, \
+    K400, K401, K402, K403, K404, K405, K406, K407, K408, K409, K410,       K412, K413, K414, \
+    K500, K501, K502,             K505,                   K509, K510, K511, K512, K513, K514  \
+) { \
+    { K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011,  K012,  K013,  K014 }, \
+    { K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111,  K112,  K113,  K114 }, \
+    { K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211,  K212,  K213,  K214 }, \
+    { K300,  K301,  K302,  K303,  K304,  K305,  K306,  K307,  K308,  K309,  K310,  K311,  KC_NO, K313,  K314 }, \
+    { K400,  K401,  K402,  K403,  K404,  K405,  K406,  K407,  K408,  K409,  K410,  KC_NO, K412,  K413,  K414 }, \
+    { K500,  K501,  K502,  KC_NO, KC_NO, K505,  KC_NO, KC_NO, KC_NO, K509,  K510,  K511,  K512,  K513,  K514 }  \
+}
---
+++ keyboards/bear_face/readme.md
@@ -0,0 +1,16 @@
+# bear_face
+
+![bear_face](https://imgur.com/a/11fNXvS)
+===
+
+Vortex Race 3 with replacement QMK-compatible PCB designed by [chemicalwill](https://github.com/chemicalwill)
+
+* Keyboard Maintainer: [Will Hedges](https://github.com/chemicalwill)
+* Hardware Supported: bear_face v1.0, atmega32u4
+* Hardware Availability: [PCB files](https://github.com/chemicalwill/bear_face)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make bear_face:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
