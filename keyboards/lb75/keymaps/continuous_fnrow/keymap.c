a3a9522fbe8d1e09a913ff243ba71bef7fd2bb09
ai03-2725 <ai03.public@gmail.com>
2020-03-30 17:31:09 +0900

    Update keymaps as necessary

--- keyboards/lb75/keymaps/continuous_fnrow/keymap.c
+++ keyboards/lb75/keymaps/continuous_fnrow/keymap.c
@@ -24,7 +24,7 @@
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
+  [0] = LAYOUT_continuous_fnrow( /* Base */
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS,  \
 	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_HOME,  \
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_END,   \
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,  \
 	KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT   \
   ),
-  [1] = LAYOUT( /* Fn */
+  [1] = LAYOUT_continuous_fnrow( /* Fn */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 	KC_CAPS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, BL_TOGG, BL_STEP, _______, _______,          _______, _______, \
@@ -41,19 +41,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______  \
   )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
--- keyboards/lb75/keymaps/divided_fnrow/keymap.c
+++ keyboards/lb75/keymaps/divided_fnrow/keymap.c
@@ -15,25 +15,26 @@
  */
 #include QMK_KEYBOARD_H
 
-/*	K000, K010, K001, K011, K002, K012, K003, K013, K004, K014, K005, K015, K006, K016, K007, K017, \
- *	K020, K030, K021, K031, K022, K032, K023, K033, K024, K034, K025, K035, K026, K036, K027, K037, \
- *	K040, K050, K041, K051, K042, K052, K043, K053, K044, K054, K045, K055, K046,       K047, K057, \
- *	K060, K070, K061, K071, K062, K072, K063, K073, K064, K074, K065, K075,       K076,       K077, \
- *	K080, K090, K081, K091, K082, K092, K083, K093, K084, K094, K085, K095, K086,       K087, K097, \
- *	K100, K110, K111,                   K103,                   K105, K115, K106, K116, K107, K117  \
- */
+/*
+    K000,    K010, K001, K011, K002,    K003, K013, K004, K014,    K005, K015, K006, K016,    K017, \
+    K020, K030, K021, K031, K022, K032, K023, K033, K024, K034, K025, K035, K026, K036, K027, K037, \
+    K040, K050, K041, K051, K042, K052, K043, K053, K044, K054, K045, K055, K046,       K047, K057, \
+    K060, K070, K061, K071, K062, K072, K063, K073, K064, K074, K065, K075,       K076,       K077, \
+    K080, K090, K081, K091, K082, K092, K083, K093, K084, K094, K085, K095, K086,       K087, K097, \
+    K100, K110, K101,                   K103,                   K105, K115, K106, K116, K107, K117  \
+*/
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_NO,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_PSCR,  \
+  [0] = LAYOUT_divided_fnrow( /* Base */
+    KC_ESC,       KC_F1,   KC_F2,   KC_F3,   KC_F4,       KC_F5,   KC_F6,   KC_F7,   KC_F8,       KC_F9,   KC_F10,  KC_F11,  KC_F12,       KC_PSCR,  \
 	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_HOME,  \
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_END,   \
 	MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,  \
 	KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,  \
 	KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT   \
   ),
-  [1] = LAYOUT( /* Fn */
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  [1] = LAYOUT_divided_fnrow( /* Fn */
+    RESET,       _______, _______, _______, _______,      _______, _______, _______, _______,      _______, _______, _______, _______,     _______, \
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 	KC_CAPS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, BL_TOGG, BL_STEP, _______, _______,          _______, _______, \
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______, \
@@ -42,18 +43,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
