7a74976a191e0ec7becb737bb484732f5336cc66
ai03-2725 <ai03.public@gmail.com>
2020-03-30 17:22:24 +0900

    Delete unnecessary functions in lb75.c

--- keyboards/lb75/lb75.c
+++ keyboards/lb75/lb75.c
@@ -44,24 +44,4 @@ void led_set_kb(uint8_t usb_led) {
   led_set_user(usb_led);
 }
 
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
 
-/*
-
-void matrix_scan_kb(void) {
-  // put your looping keyboard code here
-  // runs every cycle (a lot)
-
-  matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-  // put your per-action keyboard code here
-  // runs for every action, just before processing by the firmware
-
-  return process_record_user(keycode, record);
-}
-
-*/
