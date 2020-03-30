c7ac981714ec6fc7eea35e6a0d91d606ded22eec
ai03-2725 <ai03.public@gmail.com>
2020-03-30 17:50:53 +0900

    Remove split config

--- keyboards/quad_h/lb75/config.h
+++ keyboards/quad_h/lb75/config.h
@@ -49,11 +49,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-/*
- * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
- */
-#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
-
 #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 5
