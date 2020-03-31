c67e30459390982f4f1b47f46c62322f1fe9ba87
Drashna Jaelre <drashna@live.com>
2020-03-31 16:26:43 -0700

    [Keymap] Drashna's Cleanup and RGB Divide (#8506)
    * Enable External EEPROM on Planck Rev6
    
    * Update KC_MAKE macro to use qmk cli util
    
    * Disable additional gradients for rgb matrix
    
    * Update analog code for newer methods
    
    * Update ergodox layout
    
    * Disable Grave Escape
    
    * Cleanup OLED code a bit
    
    * Remove old unicode code
    
    * Seperate RGB Matrix code from RGB Light code in userspace
    
    * Massive overhaul an generalization of personal OLED code
    
    Now lets hope I NEVER get a keyboard using a 128x32 in a normal orientation.
    
    * Super tiny cleanup
    
    * Enable Diablo layer on kyria
    
    * clang format pass
    
    * Additional OLED cleanup

--- users/drashna/config.h
+++ users/drashna/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Use custom magic number so that when switching branches, EEPROM always gets reset
-#define EECONFIG_MAGIC_NUMBER (uint16_t)0x1337
+#define EECONFIG_MAGIC_NUMBER (uint16_t)0x1338
 
 /* Set Polling rate to 1000Hz */
 #define USB_POLLING_INTERVAL_MS 1
@@ -52,6 +52,7 @@
 #    if defined(__AVR__) && !defined(__AVR_AT90USB1286__)
 #        define DISABLE_RGB_MATRIX_ALPHAS_MODS
 #        define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#        define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
 #        define DISABLE_RGB_MATRIX_BREATHING
 #        define DISABLE_RGB_MATRIX_BAND_SAT
 #        define DISABLE_RGB_MATRIX_BAND_VAL
