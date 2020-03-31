c1aca44a246930e412305650aeb0342eb9a5e05d
Danny <nooges@users.noreply.github.com>
2020-03-31 10:50:49 -0400

    Remove unneeded extern
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/keebio/quefrency/keymaps/via/keymap.c
+++ keyboards/keebio/quefrency/keymaps/via/keymap.c
@@ -1,7 +1,5 @@
 #include QMK_KEYBOARD_H
 
-extern keymap_config_t keymap_config;
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
     KC_F1,   KC_F2,   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
