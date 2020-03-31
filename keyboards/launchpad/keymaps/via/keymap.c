95315e6b4aa98db57707089a073b2d498716956f
mcmadhatter <mcmadhatter@hotmail.com>
2020-03-31 06:53:01 +0100

    Update keyboards/launchpad/keymaps/via/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/launchpad/keymaps/via/keymap.c
+++ keyboards/launchpad/keymaps/via/keymap.c
@@ -28,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | FUNC |  8   |
  * `-------------'
  */
-[_QWERTY] = LAYOUT( \
-    KC_1,      KC_2, \
-    KC_3,      KC_4, \
-    KC_5,      KC_6, \
-    MO(_FUNC), KC_8  \
+[_QWERTY] = LAYOUT(
+    KC_1,      KC_2,
+    KC_3,      KC_4,
+    KC_5,      KC_6,
+    MO(_FUNC), KC_8
 ),
 
 /* Function
