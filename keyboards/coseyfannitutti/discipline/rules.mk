2b708d064476e4035cb6c90385a20d9a27ca2523
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2020-03-31 17:08:34 -0400

    Update keyboards/coseyfannitutti/discipline/rules.mk
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/coseyfannitutti/discipline/rules.mk
+++ keyboards/coseyfannitutti/discipline/rules.mk
@@ -21,7 +21,7 @@ OPT_DEFS = -DDEBUG_LEVEL=0
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
