04e5f253d6370e2726144e0bda8219e767f358dc
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2020-03-30 23:19:06 -0500

    Updated license
    * Changed license name to username
    
    * Changed license email to github profile url

--- keyboards/bear_face/config.h
+++ keyboards/bear_face/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Will Hedges <wshedges@outlook.com> @chemicalwill
+Copyright 2020 chemicalwill <https://github.com/chemicalwill>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
