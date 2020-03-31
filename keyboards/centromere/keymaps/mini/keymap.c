4e01e8c3f1782e490bb2145ea220216fc91fd458
spe2 <falkenbergb@gmail.com>
2020-03-31 15:44:09 -0600

    Update keyboards/centromere/keymaps/mini/keymap.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/centromere/keymaps/mini/keymap.c
+++ keyboards/centromere/keymaps/mini/keymap.c
@@ -112,10 +112,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
-void persistant_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
