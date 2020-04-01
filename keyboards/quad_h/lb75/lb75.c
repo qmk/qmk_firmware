361995abbd70b897edef8344ae3dd5ccfcb4cd13
ai03 <sarcaphx510@gmail.com>
2020-04-01 16:38:34 +0900

    Update keyboards/quad_h/lb75/lb75.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/quad_h/lb75/lb75.c
+++ keyboards/quad_h/lb75/lb75.c
@@ -26,22 +26,11 @@ void matrix_init_kb(void) {
   matrix_init_user();
 }
 
-void led_set_kb(uint8_t usb_led) {
-  // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-  if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-	writePinHigh(B1);
-  } else {
-	writePinLow(B1);
-  }
-  
-  if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
-	writePinHigh(B2);
-  } else {
-	writePinLow(B2);
-  }
-  
-  led_set_user(usb_led);
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        writePin(B1, !led_state.caps_lock);
+        writePin(B2, !led_state.scroll_lock);
+    }
 }
-
 
