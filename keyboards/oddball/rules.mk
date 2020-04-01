0ae4563dcaf05efbed38376d3e6cd897780280a9
Alexander Tulloh <>
2020-04-01 21:12:08 +1100

    Removed unused headers from adns code

--- keyboards/oddball/rules.mk
+++ keyboards/oddball/rules.mk
@@ -17,7 +17,7 @@ BOOTLOADER = halfkay
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no        # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
