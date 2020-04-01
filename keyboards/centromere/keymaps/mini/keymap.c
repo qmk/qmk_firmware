272b8ed612cc49d3e0ff5e0ef3a7bce40771bf32
spe2 <falkenbergb@gmail.com>
2020-03-31 21:20:00 -0600

    Update keyboards/centromere/keymaps/mini/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/centromere/keymaps/mini/keymap.c
+++ keyboards/centromere/keymaps/mini/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_QWERTY] = LAYOUT( \
-  KC_NO,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_NO, \
-  KC_NO,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_NO, \
-  KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_NO, \
-                    KC_LCTL, LOWER, KC_SPC,         KC_BSPC, RAISE, OSM(MOD_LSFT)                 \
+[_QWERTY] = LAYOUT(
+  KC_NO,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_NO,
+  KC_NO,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_NO,
+  KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_NO,
+                    KC_LCTL, LOWER, KC_SPC,         KC_BSPC, RAISE, OSM(MOD_LSFT)
 ),
 
 /* Raise
