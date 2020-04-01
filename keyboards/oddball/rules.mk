b9c2ce624cac073b10c86999f8e57598cdbf2354
Alexander Tulloh <>
2020-04-01 16:26:18 +1100

    Disable MOUSEKEY_ENABLE in oddball

--- keyboards/oddball/rules.mk
+++ keyboards/oddball/rules.mk
@@ -15,7 +15,7 @@ BOOTLOADER = halfkay
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
