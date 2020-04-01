b4101b15ba10526d26617cb7f6307f3da8792350
hsgw <urkwtky@gmail.com>
2020-04-01 17:22:39 +0900

    add status led feature

--- keyboards/plaid/keymaps/via/config.h
+++ keyboards/plaid/keymaps/via/config.h
@@ -1,2 +1,3 @@
+// These VID/PID is not verified. Use at your own risk.
 #define VENDOR_ID 0x0d39
 #define PRODUCT_ID 0x0001
--- keyboards/plaid/keymaps/via/keymap.c
+++ keyboards/plaid/keymaps/via/keymap.c
@@ -1,3 +1,19 @@
+/* Copyright 2020 Takuya Urakawa (dm9records.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
 #define LOWER MO(1)
@@ -45,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | DEL  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -78,3 +94,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 };
+
+uint32_t layer_state_set_user(uint32_t state) {
+  return update_tri_layer_state(state, 1, 2, 3);
+}
+
+bool led_update_user(led_t led_state) {
+    writePin(LED_RED, led_state.caps_lock);
+    writePin(LED_GREEN, led_state.scroll_lock);
+    return false;
+}
