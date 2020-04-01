89ffa63bf45db6b57941f5d52142426682d8649d
Alexander Tulloh <>
2020-04-01 21:28:12 +1100

    Update oddball copyright notices to be the same

--- keyboards/oddball/keymaps/default/keymap.c
+++ keyboards/oddball/keymaps/default/keymap.c
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include QMK_KEYBOARD_H
 
 enum layer_names {
