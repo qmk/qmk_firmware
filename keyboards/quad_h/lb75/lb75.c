c460a2b434051d5164a6dc68599b7040cf9ed853
ai03-2725 <ai03.public@gmail.com>
2020-04-01 16:52:00 +0900

    Update lb75.c

--- keyboards/quad_h/lb75/lb75.c
+++ keyboards/quad_h/lb75/lb75.c
@@ -28,9 +28,12 @@ void matrix_init_kb(void) {
 
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
+
     if(res) {
         writePin(B1, !led_state.caps_lock);
         writePin(B2, !led_state.scroll_lock);
     }
+    
+    return res;
 }
 
