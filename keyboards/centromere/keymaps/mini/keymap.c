ffcda61f737f76ff8068665690468f4bf3c7bf0b
spe2 <falkenbergb@gmail.com>
2020-03-31 21:19:51 -0600

    Update keyboards/centromere/keymaps/mini/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/centromere/keymaps/mini/keymap.c
+++ keyboards/centromere/keymaps/mini/keymap.c
@@ -60,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_RAISE] = LAYOUT( \
-  KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    	KC_NO,    \
-  KC_NO,   KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,	KC_NO,    \
-  KC_NO,   KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS,  KC_QUOT,	KC_NO,    \
-                    _______, _______, _______,      _______, _______, _______                    \
+[_RAISE] = LAYOUT(
+  KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    	KC_NO,
+  KC_NO,   KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,	KC_NO,
+  KC_NO,   KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS,  KC_QUOT,	KC_NO,
+                    _______, _______, _______,      _______, _______, _______
 ),
 
 /* Lower
