391faac1c20da4a07b10f678400f4bb1f568ce0c
Alan Wu <XrXr@users.noreply.github.com>
2020-04-02 12:41:23 -0400

    Turn off stuff I don't need

--- keyboards/cannonkeys/instant60/rules.mk
+++ keyboards/cannonkeys/instant60/rules.mk
@@ -12,10 +12,10 @@ SRC =	keyboard.c \
       led.c
 
 BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
+MOUSEKEY_ENABLE = no	# Mouse keys
+EXTRAKEY_ENABLE = no	# Audio control and System control
+CONSOLE_ENABLE = no	# Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
 CUSTOM_MATRIX = no # Custom matrix file
