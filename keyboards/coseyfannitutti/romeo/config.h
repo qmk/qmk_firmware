2fbbac04bb0c84323e3bb16b4d164d726046967c
coseyfannitutti <43188488+coseyfannitutti@users.noreply.github.com>
2020-04-05 01:38:25 -0400

    [Keyboard] romeo - add via layout (#8667)

--- keyboards/coseyfannitutti/romeo/config.h
+++ keyboards/coseyfannitutti/romeo/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x6B62
 #define PRODUCT_ID      0x4069
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    coseyfannitutti
