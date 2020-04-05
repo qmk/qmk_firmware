d6cb19b39feeaa72870d1f33102a27feaa9da3b3
foxx1337 <foxx1337@yahoo.com>
2020-04-05 19:21:49 +0200

    Add rgbmatrix to the raw HID protocol

--- keyboards/massdrop/ctrl/keymaps/foxx1337/hid_protocol.h
+++ keyboards/massdrop/ctrl/keymaps/foxx1337/hid_protocol.h
@@ -1,13 +1,63 @@
 #pragma once
 
+#include <color.h>
 #include <raw_hid.h>
 
+#define CTRL_HID_GREETING_VERSION "CTRLHID 1.0.0"
+
 enum ctrl_hid_codes {
+    // Signals end of message. Not really that useful.
     CTRL_HID_EOM = 0x00,
+    CTRL_HID_OK,
+    CTRL_HID_NOK,
+
+    /**
+     * to hid: CTRL_HID_HELLO
+     * from hid: CTRL_HID_HELLO, "CTRLHID 1.0.0", CTRL_HID_EOM
+     **/
     CTRL_HID_HELLO,
-    CTRL_HID_LIGHTS_TOGGLE
+
+    /**
+     * to hid: CTRL_HID_LIGHTS_TOGGLE
+     * from hid: CTRL_HID_LIGHTS_TOGGLE, CTRL_HID_OK, is_led_timeout, CTRL_HID_EOM
+     *
+     * Toggles all the leds on the keyboard. is_led_timeout will be 1 if the new
+     * state is off, 0 if leds are on.
+     **/
+    CTRL_HID_LIGHTS_TOGGLE,
+
+    /**
+     * to hid: CTRL_HID_LED, led_id, r, g, b
+     * from hid: CTRL_HID_LED, CTRL_HID_OK, CTRL_HID_EOM
+     * on error: CTRL_HID_LED, CTRL_HID_NOK, num_leds, CTRL_HID_EOM
+     *
+     * Sets the specific led to r, g, b. It's only visible when the hid_effect mode is active.
+     **/
+    CTRL_HID_LED,
+
+    /**
+     * to hid: CTRL_HID_LEDS, start, count, r0, g0, b0, ..., r[count-1], g[count-1], b[count-1]
+     * from hid: CTRL_HID_LEDS, CTRL_HID_OK, affected_leds, CTRL_HID_EOM
+     *
+     * Sets affected_leds leds following start to the corresponding r, g, b value.
+     * It's only visible when the hid_effect mode is active.
+     **/
+    CTRL_HID_LEDS,
+
+    /**
+     * to hid: CTRL_HID_RGBMATRIX_MODE, mode
+     * from hid: CTRL_HID_RGBMATRIX_MODE, CTRL_HID_OK, CTRL_HID_EOM
+     * on error: CTRL_HID_RGBMATRIX_MODE, CTRL_HID_NOK, mode_max, CTRL_HID_EOM
+     *
+     * Changes light mode. mode_max is hid_effect for CTRL_HID_LED and CTRL_HID_LEDS.
+     */
+    CTRL_HID_RGBMATRIX_MODE
 };
 
 extern uint8_t raw_hid_buffer[RAW_EPSIZE];
+
+// Defined in rgb_matrix_user.inc
+// It's 119 for Massdrop CTRL, 0 - 118.
+extern RGB rgb_matrix_led_state[DRIVER_LED_TOTAL];
 
 void raw_hid_perform_send(void);
--- keyboards/massdrop/ctrl/keymaps/foxx1337/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/foxx1337/keymap.c
@@ -161,24 +161,71 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void raw_hid_say_hello(void) {
-    raw_hid_buffer[0] = CTRL_HID_HELLO;
-    raw_hid_buffer[1] = 'C';
-    raw_hid_buffer[2] = 'T';
-    raw_hid_buffer[3] = 'R';
-    raw_hid_buffer[4] = 'L';
-    raw_hid_buffer[5] = CTRL_HID_EOM;
-
-    raw_hid_perform_send();
+    const char *ctrl = CTRL_HID_GREETING_VERSION;
+    uint8_t i = 0;
+    while (ctrl[i] != 0 && i + 2 < RAW_EPSIZE) {
+        raw_hid_buffer[1 + i] = ctrl[i];
+        i++;
+    }
+    raw_hid_buffer[i] = CTRL_HID_EOM;
 }
 
 void raw_hid_lights_toggle(void) {
     change_led_state(!is_led_timeout);
 
-    raw_hid_buffer[0] = CTRL_HID_LIGHTS_TOGGLE;
-    raw_hid_buffer[1] = (uint8_t) is_led_timeout;
-    raw_hid_buffer[2] = CTRL_HID_EOM;
+    raw_hid_buffer[1] = CTRL_HID_OK;
+    raw_hid_buffer[2] = (uint8_t) is_led_timeout;
+    raw_hid_buffer[3] = CTRL_HID_EOM;
+}
 
-    raw_hid_perform_send();
+void raw_hid_led(uint8_t *data) {
+    //rgb_matrix_set_color(data[1], data[2], data[3], data[4]);
+    const uint8_t led = data[1];
+
+    if (led >= DRIVER_LED_TOTAL) {
+        raw_hid_buffer[1] = CTRL_HID_NOK;
+        raw_hid_buffer[2] = DRIVER_LED_TOTAL;
+        raw_hid_buffer[3] = CTRL_HID_EOM;
+        return;
+    }
+
+    rgb_matrix_led_state[led].r = data[2];
+    rgb_matrix_led_state[led].g = data[3];
+    rgb_matrix_led_state[led].b = data[4];
+
+    raw_hid_buffer[1] = CTRL_HID_OK;
+    raw_hid_buffer[2] = CTRL_HID_EOM;
+}
+
+void raw_hid_leds(uint8_t *data) {
+    const uint8_t first_led = data[1];
+    const uint8_t number_leds = data[2];
+
+    uint8_t i = 0;
+    while (i < number_leds && first_led + i < DRIVER_LED_TOTAL && i * 3 + 5 < RAW_EPSIZE) {
+        rgb_matrix_led_state[first_led + i].r = data[3 + i * 3 + 0];
+        rgb_matrix_led_state[first_led + i].g = data[3 + i * 3 + 1];
+        rgb_matrix_led_state[first_led + i].b = data[3 + i * 3 + 2];
+        i++;
+    }
+
+    raw_hid_buffer[1] = CTRL_HID_OK;
+    raw_hid_buffer[2] = i;
+    raw_hid_buffer[3] = CTRL_HID_EOM;
+}
+
+void raw_hid_rgbmatrix_mode(uint8_t *data) {
+    const uint8_t mode = data[1];
+    if (mode >= RGB_MATRIX_EFFECT_MAX) {
+        raw_hid_buffer[1] = CTRL_HID_NOK;
+        raw_hid_buffer[2] = RGB_MATRIX_EFFECT_MAX - 1;
+        raw_hid_buffer[3] = CTRL_HID_EOM;
+        return;
+    }
+    rgb_matrix_mode_noeeprom(mode);
+
+    raw_hid_buffer[1] = CTRL_HID_OK;
+    raw_hid_buffer[2] = CTRL_HID_EOM;
 }
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
@@ -189,5 +236,16 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
         case CTRL_HID_LIGHTS_TOGGLE:
             raw_hid_lights_toggle();
             break;
+        case CTRL_HID_LED:
+            raw_hid_led(data);
+            break;
+        case CTRL_HID_LEDS:
+            raw_hid_leds(data);
+            break;
+        case CTRL_HID_RGBMATRIX_MODE:
+            raw_hid_rgbmatrix_mode(data);
+            break;
     }
+
+    raw_hid_perform_send();
 }
