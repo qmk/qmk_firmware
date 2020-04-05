c0cad318197be0c168df1a6920b3880eb0658ad7
stanrc85 <stanrc@gmail.com>
2020-04-05 17:39:19 -0400

    Extra spaces removed

--- users/stanrc85/rgblight_layers.c
+++ users/stanrc85/rgblight_layers.c
@@ -1,30 +1,30 @@
 #include "stanrc85.h"
 
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {3, 2, HSV_RED},       
+    {3, 2, HSV_RED},
     {10, 2, HSV_RED}
 );
 
 const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {3, 1, HSV_GREEN},       
+    {3, 1, HSV_GREEN},
     {11, 1, HSV_GREEN}
 );   
 
 const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {3, 1, HSV_BLUE},       
+    {3, 1, HSV_BLUE},
     {11, 1, HSV_BLUE} 
 );
 
 const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {3, 1, HSV_WHITE},       
+    {3, 1, HSV_WHITE},
     {11, 1, HSV_WHITE}
 );
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer,
-    my_layer1_layer,    // Overrides caps lock layer          
-    my_layer2_layer,    // Overrides other layers
+    my_layer1_layer,
+    my_layer2_layer,
     my_layer3_layer
 );
 
