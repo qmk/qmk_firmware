5b555eb2cd6b571e828716b3e4b5b849865ed7d8
halfenergized <half_energized@yahoo.co.uk>
2020-04-05 11:28:42 +0700

    Update 910.h

--- keyboards/tgr/910/910.h
+++ keyboards/tgr/910/910.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 mechmerlin
+/* Copyright 2020 halfenergized/poisonking
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -40,7 +40,7 @@
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k52,  \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2d, k51,       \
     k30, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k50,       \
-    k40, k41, k42, k45, k47, k48, k49, k4b, k4c, k4d                            \
+    k40, k41, k42, k45, k47, k48, k4b, k4c, k4d                                 \
 ){ \
     { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d },  \
     { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d },  \
@@ -56,7 +56,7 @@
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k52,  \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2d, k51,       \
     k30, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k50,       \
-    k40, k41, k42, k45, k47, k48, k4b, k4c, k4d                            \
+    k40, k41, k42, k45, k47, k48, k4b, k4c, k4d                                 \
 ){ \
     { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d },  \
     { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d },  \
@@ -72,7 +72,7 @@
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c,  k52,      \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k51,  \
     k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k50,  \
-    k40, k41, k42, k45, k47, k48, k49, k4b, k4c, k4d                            \
+    k40, k41, k42, k45, k47, k48, k4b, k4c, k4d                                 \
 ){ \
     { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d },  \
     { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, ___ },  \
