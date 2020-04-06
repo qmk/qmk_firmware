fb3ffade02e4e54727b4f1d461925d16ea616a1d
Danny Nguyen <danny@keeb.io>
2020-04-06 12:57:08 -0400

    Add Choconum

---
+++ keyboards/keebio/choconum/choconum.h
@@ -0,0 +1,49 @@
+/* Copyright 2020 Keebio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
+
+#define LAYOUT_5x4( \
+    KA1, KA2, KA3, KA4, \
+    KB1, KB2, KB3, KB4, \
+    KC1, KC2, KC3, KC4, \
+    KD1, KD2, KD3, KD4, \
+    KE1, KE2, KE3, KE4 \
+) \
+{ \
+    { KA1, KA2, KA3, KA4 }, \
+    { KB1, KB2, KB3, KB4 }, \
+    { KC1, KC2, KC3, KC4 }, \
+    { KD1, KD2, KD3, KD4 }, \
+    { KE1, KE2, KE3, KE4 } \
+}
+
+#define LAYOUT_numpad( \
+    KA1, KA2, KA3, KA4, \
+    KB1, KB2, KB3, KB4, \
+    KC1, KC2, KC3, \
+    KD1, KD2, KD3, KD4, \
+    KE1,      KE3 \
+) \
+{ \
+    { KA1, KA2, KA3, KA4 }, \
+    { KB1, KB2, KB3, KB4 }, \
+    { KC1, KC2, KC3, KC_NO }, \
+    { KD1, KD2, KD3, KD4 }, \
+    { KE1, KC_NO, KE3, KC_NO } \
+}
---
+++ keyboards/keebio/choconum/keymaps/default/keymap.c
@@ -0,0 +1,40 @@
+/* Copyright 2020 Keebio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT_numpad(
+        KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,            KC_PDOT
+    ),
+    [_FN] = LAYOUT_numpad(
+        _______, _______, _______, _______,
+        KC_HOME, KC_UP,   KC_PGUP, _______,
+        KC_LEFT, _______, KC_RGHT,
+        KC_END,  KC_DOWN, KC_PGDN, KC_PEQL,
+        KC_INS,           KC_DEL
+    )
+};
