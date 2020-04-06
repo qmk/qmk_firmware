dd318cec4b52361a5238b984e63a97c5b856edbe
x1- <viva008@gmail.com>
2020-04-06 19:55:12 +0900

    Modified default revision of kudox rev3.

--- keyboards/kudox/rev3/keymaps/jis/keymap.c
+++ keyboards/kudox/rev3/keymaps/jis/keymap.c
@@ -62,3 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
+
+#ifdef RGBLIGHT_ENABLE
+void keyboard_post_init_user(void) {
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL);
+};
+#endif
