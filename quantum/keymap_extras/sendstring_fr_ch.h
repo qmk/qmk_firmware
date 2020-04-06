7c699709eda3176a9b0cea28773fb767ed07953f
fauxpark <fauxpark@gmail.com>
2020-04-06 13:03:49 +1000

    Fix copyrights

--- quantum/keymap_extras/sendstring_fr_ch.h
+++ quantum/keymap_extras/sendstring_fr_ch.h
@@ -1,4 +1,4 @@
-/* Copyright 2016 Jack Humbert
+/* Copyright 2020
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- quantum/keymap_extras/sendstring_german_ch.h
+++ quantum/keymap_extras/sendstring_german_ch.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 Patrick Hener
+/* Copyright 2020
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
