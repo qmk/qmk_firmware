93142bdc8bad31736a57708c50d687cbf250a017
Andrew Kannan <andrew.kannan@gmail.com>
2020-04-07 00:42:15 -0400

    Add revision 1 and revision 2 to ProjectKB Alice PCB

--- keyboards/projectkb/alice/rev2/rules.mk
+++ keyboards/projectkb/alice/rev2/rules.mk
@@ -17,7 +17,6 @@ BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 WS2812_DRIVER = spi
 
-DEFAULT_FOLDER = projectkb/alice/rev2
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
