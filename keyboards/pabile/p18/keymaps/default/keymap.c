a81a7fd6b8f35085c2a71214dc6915430c39ddee
pabile <1162412+pabile@users.noreply.github.com>
2020-04-07 15:32:29 +0800

    modified:   keyboards/pabile/p18/keymaps/default/keymap.c

--- keyboards/pabile/p18/keymaps/default/keymap.c
+++ keyboards/pabile/p18/keymaps/default/keymap.c
@@ -18,17 +18,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
-    if (clockwise) {
-      tap_code(KC_WH_U); /*mouse wheel up*/
-    } else {
-      tap_code(KC_WH_D); /*mouse wheel down */
-    }
-  } else if (index == 1) { /* Second encoder */  
+  if (index == 0) { /* First encoder below the controller */
     if (clockwise) {
       tap_code(KC_VOLD); /*volume down*/
     } else {
       tap_code(KC_VOLU); /*volume up*/
+    }
+  } else if (index == 1) { /* Second encoder */  
+    if (clockwise) {
+      tap_code(KC_WH_U); /*mouse wheel up*/
+    } else {
+      tap_code(KC_WH_D); /*mouse wheel down*/
     }
   }
 }
