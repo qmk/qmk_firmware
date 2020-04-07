864e7508d36aeb6e852add7e6e8a0d6160ff4b53
Sergey Vlasov <sigprof@gmail.com>
2020-04-07 20:35:08 +0300

    id80: Remove placeholder functions
    The provided skeletons for `matrix_scan_kb` and `process_record_kb` did
    not do anything useful, so remove them.

--- keyboards/id80/id80.c
+++ keyboards/id80/id80.c
@@ -24,20 +24,6 @@ void led_init_ports(void) {
     setPinOutput(CAPS_LOCK_LED_PIN);
 }
 
-void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
-
-    matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    // put your per-action keyboard code here
-    // runs for every action, just before processing by the firmware
-
-    return process_record_user(keycode, record);
-}
-
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res) {
