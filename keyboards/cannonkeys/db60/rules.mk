6d9928582f1a3acce5c76cf59bc46ecd9a592bbc
Andrew Kannan <andrew.kannan@klaviyo.com>
2020-04-06 22:26:21 -0400

    Apply suggestions from code review
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/cannonkeys/db60/rules.mk
+++ keyboards/cannonkeys/db60/rules.mk
@@ -1,17 +1,16 @@
 # MCU Name
-MCU = STM32F072xB
+MCU = STM32F072
 
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
+BOOTMAGIC_ENABLE = lite # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes   # Mouse keys
+EXTRAKEY_ENABLE = yes   # Audio control and System control
+CONSOLE_ENABLE = yes    # Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	    # USB Nkey Rollover
-CUSTOM_MATRIX = no # Custom matrix file
+NKRO_ENABLE = yes       # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 WS2812_DRIVER = spi
