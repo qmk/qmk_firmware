1c78be99fb75690d2c491795bbdbbbb693779b10
Andrew Kannan <andrew.kannan@gmail.com>
2020-04-06 22:32:46 -0400

    Remove ugly hack comments

--- keyboards/cannonkeys/db60/config.h
+++ keyboards/cannonkeys/db60/config.h
@@ -21,8 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xCA04
 #define PRODUCT_ID      0xDB60
 #define DEVICE_VER      0x0001
-/* in python2: list(u"whatever".encode('utf-16-le')) */
-/*   at most 32 characters or the ugly hack in usb_main.c borks */
+
 #define MANUFACTURER CannonKeys
 #define PRODUCT DB60
 #define DESCRIPTION DB60 Keyboard
