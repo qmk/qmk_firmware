09e02e644e4a48d1b4ed1bf343da922192f8d7ec
fauxpark <fauxpark@gmail.com>
2020-04-07 20:50:10 +1000

    Update BÉPO keymap and sendstring LUT

--- quantum/keymap_extras/sendstring_bepo.h
+++ quantum/keymap_extras/sendstring_bepo.h
@@ -73,26 +73,26 @@ const uint8_t ascii_to_keycode_lut[128] PROGMEM = {
     XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 
     //       !        "        #        $        %        &        '
-    KC_SPC, BP_DCRC, BP_DQOT, BP_DLR, BP_DLR, BP_PERC, BP_P, BP_APOS,
+    KC_SPC,  BP_DCIR, BP_DQUO, BP_DLR,  BP_DLR,  BP_PERC, BP_P,    BP_QUOT,
     // (     )        *        +        ,        -        .        /
     BP_LPRN, BP_RPRN, BP_ASTR, BP_PLUS, BP_COMM, BP_MINS, BP_DOT, BP_SLSH,
     // 0     1        2        3        4        5        6        7
-    BP_ASTR, BP_DQOT, BP_LGIL, BP_RGIL, BP_LPRN, BP_RPRN, BP_AT, BP_PLUS,
+    BP_ASTR, BP_DQUO, BP_LGIL, BP_RGIL, BP_LPRN, BP_RPRN, BP_AT,   BP_PLUS,
     // 8     9        :        ;        <        =        >        ?
-    BP_MINS, BP_SLSH, BP_DOT, BP_COMM, BP_LGIL, BP_EQL, BP_RGIL, BP_APOS,
+    BP_MINS, BP_SLSH, BP_DOT,  BP_COMM, BP_LGIL, BP_EQL,  BP_RGIL, BP_QUOT,
     // @     A        B        C        D        E        F        G
-    BP_AT, BP_A, BP_B, BP_C, BP_D, BP_E, BP_F, BP_G,
+    BP_AT,   BP_A,    BP_B,    BP_C,    BP_D,    BP_E,    BP_F,    BP_G,
     // H     I        J        K        L        M        N        O
-    BP_H, BP_I, BP_J, BP_K, BP_L, BP_M, BP_N, BP_O,
+    BP_H,    BP_I,    BP_J,    BP_K,    BP_L,    BP_M,    BP_N,    BP_O,
     // P     Q        R        S        T        U        V        W
-    BP_P, BP_Q, BP_R, BP_S, BP_T, BP_U, BP_V, BP_W,
+    BP_P,    BP_Q,    BP_R,    BP_S,    BP_T,    BP_U,    BP_V,    BP_W,
     // X     Y        Z        [        \        ]        ^        _
-    BP_X, BP_Y, BP_Z, BP_LPRN, BP_AGRV, BP_RPRN, BP_AT, KC_SPC,
+    BP_X,    BP_Y,    BP_Z,    BP_LPRN, BP_AGRV, BP_RPRN, BP_AT,   KC_SPC,
     // `     a        b        c        d        e        f        g
-    BP_PERC, BP_A, BP_B, BP_C, BP_D, BP_E, BP_F, BP_G,
+    BP_PERC, BP_A,    BP_B,    BP_C,    BP_D,    BP_E,    BP_F,    BP_G,
     // h     i        j        k        l        m        n        o
-    BP_H, BP_I, BP_J, BP_K, BP_L, BP_M, BP_N, BP_O,
+    BP_H,    BP_I,    BP_J,    BP_K,    BP_L,    BP_M,    BP_N,    BP_O,
     // p     q        r        s        t        u        v        w
-    BP_P, BP_Q, BP_R, BP_S, BP_T, BP_U, BP_V, BP_W,
+    BP_P,    BP_Q,    BP_R,    BP_S,    BP_T,    BP_U,    BP_V,    BP_W,
     // x     y        z        {        |        }        ~        DEL
-    BP_X, BP_Y, BP_Z, BP_Y, BP_B, BP_X, BP_K, KC_DEL};
+    BP_X,    BP_Y,    BP_Z,    BP_Y,    BP_B,    BP_X,    BP_K,    KC_DEL};
