e23110297eec860a1278bfdf171b6a4011bea3f1
Sergey Vlasov <sigprof@gmail.com>
2020-04-07 21:21:39 +0300

    id80: Use unique USB vendor/product ID
    Having an unique USB vendor/product ID is required for VIA support.
    The vendor ID value is the same as for the `idobo` (ID75) keyboard.

--- keyboards/id80/config.h
+++ keyboards/id80/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x6964 /* "id" */
+#define PRODUCT_ID      0x0080
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    IDOBAO
 #define PRODUCT         ID80
