8c4e5cdc03a0e876023cc31fba5af21f9c539dd4
skullY <skullydazed@gmail.com>
2020-04-07 16:55:03 -0700

    fix more tests

--- lib/python/qmk/tests/test_cli_commands.py
+++ lib/python/qmk/tests/test_cli_commands.py
@@ -8,35 +8,52 @@ def check_subcommand(command, *args):
     return result
 
 
+def print_stdout_if_error(result):
+    if result.returncode != 0:
+        print('`qmk info -kb handwired/onekey/pytest` stdout:')
+        print(result.stdout)
+        print('returncode:', result.returncode)
+
+
 def test_cformat():
     result = check_subcommand('cformat', 'quantum/matrix.c')
+    print_stdout_if_error(result)
     assert result.returncode == 0
 
 
 def test_compile():
-    assert check_subcommand('compile', '-kb', 'handwired/onekey/pytest', '-km', 'default', '-n').returncode == 0
+    result = check_subcommand('compile', '-kb', 'handwired/onekey/pytest', '-km', 'default', '-n')
+    print_stdout_if_error(result)
+    assert result.returncode == 0
 
 
 def test_flash():
-    assert check_subcommand('flash', '-kb', 'handwired/onekey/pytest', '-km', 'default', '-n').returncode == 0
+    result = check_subcommand('flash', '-kb', 'handwired/onekey/pytest', '-km', 'default', '-n')
+    print_stdout_if_error(result)
+    assert result.returncode == 0
 
 
 def test_flash_bootloaders():
-    assert check_subcommand('flash', '-b').returncode == 1
+    result = check_subcommand('flash', '-b')
+    assert result.returncode == 1
 
 
 def test_config():
     result = check_subcommand('config')
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'general.color' in result.stdout
 
 
 def test_kle2json():
-    assert check_subcommand('kle2json', 'kle.txt', '-f').returncode == 0
+    result = check_subcommand('kle2json', 'kle.txt', '-f')
+    print_stdout_if_error(result)
+    assert result.returncode == 0
 
 
 def test_doctor():
     result = check_subcommand('doctor', '-n')
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'QMK Doctor is checking your environment.' in result.stdout
     assert 'QMK is ready to go' in result.stdout
@@ -44,18 +61,21 @@ def test_doctor():
 
 def test_hello():
     result = check_subcommand('hello')
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'Hello,' in result.stdout
 
 
 def test_pyformat():
     result = check_subcommand('pyformat')
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'Successfully formatted the python code' in result.stdout
 
 
 def test_list_keymaps():
     result = check_subcommand('list-keymaps', '-kb', 'handwired/onekey/pytest')
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'default' and 'test' in result.stdout
 
@@ -86,16 +106,14 @@ def test_list_keymaps_vendor_kb_rev():
 
 def test_list_keymaps_no_keyboard_found():
     result = check_subcommand('list-keymaps', '-kb', 'asdfghjkl')
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'does not exist' in result.stdout
 
 
 def test_info():
     result = check_subcommand('info', '-kb', 'handwired/onekey/pytest')
-    if result.returncode != 0:
-        print('`qmk info -kb handwired/onekey/pytest` stdout:')
-        print(result.stdout)
-        print('returncode:', result.returncode)
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'Keyboard Name: handwired/onekey/pytest' in result.stdout
     assert 'Processor: STM32F303' in result.stdout
@@ -105,8 +123,29 @@ def test_info():
 
 def test_info_keyboard_render():
     result = check_subcommand('info', '-kb', 'handwired/onekey/pytest', '-l')
+    print_stdout_if_error(result)
     assert result.returncode == 0
     assert 'Keyboard Name: handwired/onekey/pytest' in result.stdout
     assert 'Processor: STM32F303' in result.stdout
     assert 'LAYOUT:' in result.stdout
     assert 'k0' in result.stdout
+
+
+def test_info_keymap_render():
+    result = check_subcommand('info', '-kb', 'handwired/onekey/pytest', '-km', 'default')
+    print_stdout_if_error(result)
+    assert result.returncode == 0
+    assert 'Keyboard Name: handwired/onekey/pytest' in result.stdout
+    assert 'Processor: STM32F303' in result.stdout
+    assert '│NO│' in result.stdout
+
+
+def test_info_matrix_render():
+    result = check_subcommand('info', '-kb', 'handwired/onekey/pytest', '-m')
+    print_stdout_if_error(result)
+    assert result.returncode == 0
+    assert 'Keyboard Name: handwired/onekey/pytest' in result.stdout
+    assert 'Processor: STM32F303' in result.stdout
+    assert 'LAYOUT' in result.stdout
+    assert '│0A│' in result.stdout
+    assert 'Matrix for "LAYOUT"' in result.stdout
