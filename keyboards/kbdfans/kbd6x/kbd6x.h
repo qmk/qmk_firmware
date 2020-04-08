c0c22436691a8a212aa8086477fbc4d0b79a1452
George Wietor <george@wietor.com>
2020-04-08 19:35:04 -0400

    Update keyboards/kbdfans/kbd6x/kbd6x.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/kbdfans/kbd6x/kbd6x.h
+++ keyboards/kbdfans/kbd6x/kbd6x.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 mixedfeelings
+/* Copyright 2018 MechMerlin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
