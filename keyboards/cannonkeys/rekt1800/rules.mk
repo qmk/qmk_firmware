a3a68bb7c9a02627f63abbd1d7f6471c86f90377
Andrew Kannan <andrew.kannan@klaviyo.com>
2020-04-08 15:07:18 -0400

    Apply suggestions from code review
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/cannonkeys/rekt1800/rules.mk
+++ keyboards/cannonkeys/rekt1800/rules.mk
@@ -1,20 +1,17 @@
-#MCU Name
+# MCU Name
 MCU = STM32F072
 
 # Build Options
 #   comment out to disable the options.
 #
-
-# project specific files
-
-BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
-SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	    # USB Nkey Rollover
-CUSTOM_MATRIX = no # Custom matrix file
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes           # USB Nkey Rollover
+CUSTOM_MATRIX = no          # Custom matrix file
 BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = no
 
