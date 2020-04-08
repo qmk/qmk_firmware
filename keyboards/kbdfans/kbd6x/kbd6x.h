6905a194d45c1295231cc30931510275c45183db
George Wietor <george@light-gray.attlocal.net>
2020-04-08 16:14:27 -0400

    update author / copyright date

--- keyboards/kbdfans/kbd6x/kbd6x.h
+++ keyboards/kbdfans/kbd6x/kbd6x.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 MechMerlin
+/* Copyright 2020 mixedfeelings
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
