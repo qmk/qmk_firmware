be6b4b622a05c583547b43a4681cf740dbf591d8
pabile <1162412+pabile@users.noreply.github.com>
2020-04-09 19:08:54 +0800

    testing layout config

--- keyboards/pabile/p18/keymaps/default/keymap.c
+++ keyboards/pabile/p18/keymaps/default/keymap.c
@@ -15,6 +15,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 					KC_NO,KC_PPLS,KC_P6, KC_P5, KC_P4, 
 					KC_A, KC_TAB, KC_P3, KC_P2, KC_P1, 
 					KC_B, KC_PENT, KC_PDOT, KC_P0, KC_DEL)
+        
+/* alternate?
+	[0] = LAYOUT(
+					      KC_PMNS, KC_P9,   KC_P8, KC_P7, 
+					      KC_PPLS, KC_P6,   KC_P5, KC_P4, 
+					KC_A, KC_TAB,  KC_P3,   KC_P2, KC_P1, 
+					KC_B, KC_PENT, KC_PDOT, KC_P0, KC_DEL)
+*/
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
