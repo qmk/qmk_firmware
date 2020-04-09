41a515120b689dd4493e255ddbafa2cb9b1c4f4d
Marko Bakan <47256307+obuwunkunubi@users.noreply.github.com>
2020-04-09 09:11:44 +0200

    Update keyboards/handwired/obuwunkunubi/spaget/keymaps/default/keymap.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/handwired/obuwunkunubi/spaget/keymaps/default/keymap.c
+++ keyboards/handwired/obuwunkunubi/spaget/keymaps/default/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_P7,   KC_P8,   KC_P9,   KC_NLCK, \
     KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
     KC_P1,   KC_P2,   KC_P3,   KC_MPLY, \
-    KC_NO,   KC_P0,   KC_PDOT, KC_PENT  \
+             KC_P0,   KC_PDOT, KC_PENT  \
   ),
 
   /* Keymap ONE: Util Layer
