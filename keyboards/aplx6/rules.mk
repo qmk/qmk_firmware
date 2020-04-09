f61543e1c798ef15da5ef22a775cead7b55ac075
Aplyard <40563350+Aplyard@users.noreply.github.com>
2020-04-09 17:56:39 +0300

    Update keyboards/aplx6/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/aplx6/rules.mk
+++ keyboards/aplx6/rules.mk
@@ -12,8 +12,7 @@ MCU = atmega32u4
 BOOTLOADER = caterina
 
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in 
-#   the appropriate keymap folder that will get included automatically
+#   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
