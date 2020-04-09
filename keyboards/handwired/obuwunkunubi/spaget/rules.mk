5255b13c111209477aff021a43f82dd9037229f6
Marko Bakan <47256307+obuwunkunubi@users.noreply.github.com>
2020-04-09 09:09:50 +0200

    Update rules.mk

--- keyboards/handwired/obuwunkunubi/spaget/rules.mk
+++ keyboards/handwired/obuwunkunubi/spaget/rules.mk
@@ -17,7 +17,7 @@ BOOTLOADER = caterina
 BOOTMAGIC_ENABLE = lite  # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes    # Mouse keys
 EXTRAKEY_ENABLE = yes    # Audio control and System control
-CONSOLE_ENABLE = yes     # Console for debug
+CONSOLE_ENABLE = no      # Console for debug
 COMMAND_ENABLE = yes     # Commands for debug and configuration
 NKRO_ENABLE = yes        # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 RGBLIGHT_ENABLE = no     # Enable keyboard underlight functionality
@@ -26,6 +26,9 @@ MIDI_ENABLE = no         # MIDI controls
 AUDIO_ENABLE = no
 UNICODE_ENABLE = yes     # Unicode
 BLUETOOTH_ENABLE = no    # Enable Bluetooth with the Adafruit EZ-Key HID
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+AUDIO_ENABLE = no        # Audio output on port C6
 
 OLED_DRIVER_ENABLE = yes # Enable OLED display support
-ENCODER_ENABLE = yes	   # Enable encoder support
+ENCODER_ENABLE = yes     # Enable encoder support
+
