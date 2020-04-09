44c65c94da1175b9cd38b3e130791206f0e04e1a
Danny Nguyen <danny@keeb.io>
2020-04-09 11:53:45 -0400

    Add backlight/RGB ifdefs

--- keyboards/keebio/quefrency/rev2/rev2.c
+++ keyboards/keebio/quefrency/rev2/rev2.c
@@ -15,11 +15,17 @@ bool led_update_kb(led_t led_state) {
 }
 
 void eeconfig_init_kb(void) {
+#ifdef BACKLIGHT_ENABLE
     backlight_enable();
     backlight_level(3);
+#endif
+#ifdef RGBLIGHT_ENABLE
     rgblight_enable(); // Enable RGB by default
-    rgblight_sethsv(0, 255, 128);  // Set default HSV - red hue, full saturation, medium brightness
+    rgblight_sethsv(0, 255, 255);  // Set default HSV - red hue, full saturation, full brightness
+#ifdef RGBLIGHT_ANIMATIONS
     rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL + 2); // set to RGB_RAINBOW_SWIRL by default
+#endif
+#endif
 
     eeconfig_update_kb(0);
     eeconfig_init_user();
