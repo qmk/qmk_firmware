5e8d285cd34b68bcde5e5f27da3fd2323c6b0847
Marko Bakan <47256307+obuwunkunubi@users.noreply.github.com>
2020-04-09 09:11:10 +0200

    Update keyboards/handwired/obuwunkunubi/spaget/spaget.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/handwired/obuwunkunubi/spaget/spaget.c
+++ keyboards/handwired/obuwunkunubi/spaget/spaget.c
@@ -14,15 +14,3 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
  #include "spaget.h"
-
-void matrix_init_kb(void) {
-  // put your keyboard start-up code here
-  // runs once when the firmware starts up
-  matrix_init_user();
-};
-
-void matrix_scan_kb(void) {
-  // put your looping keyboard code here
-  // runs every cycle (a lot)
-  matrix_scan_user();
-};
