6e09269520d23a8175e895f6891265ed482233a4
HASU <latincompass@163.com>
2020-04-11 06:21:28 +0800

    Update rules.mk

--- keyboards/hasupad/rules.mk
+++ keyboards/hasupad/rules.mk
@@ -22,8 +22,7 @@ BOOTLOADER = caterina
 
 
 # Build Options
-#   change yes to no to disable
-#
+# change yes to no to disable
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
