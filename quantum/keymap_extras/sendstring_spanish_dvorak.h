182f6fab3db5d2da31efb3c87b35a0d0dde8e462
joseandres42 <joseandres42@gmail.com>
2020-04-11 19:47:04 +0200

    Update quantum/keymap_extras/sendstring_spanish_dvorak.h
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- quantum/keymap_extras/sendstring_spanish_dvorak.h
+++ quantum/keymap_extras/sendstring_spanish_dvorak.h
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Sendstring lookup tables for Spanish's dvorak layout
+// Sendstring lookup tables for Spanish Dvorak layout
 
 #pragma once
 
