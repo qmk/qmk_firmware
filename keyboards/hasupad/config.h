f8b07896f590d7416f40159d2e88b5aa6c9aa121
HASU <latincompass@163.com>
2020-04-12 05:48:50 +0800

    Update config.h

--- keyboards/hasupad/config.h
+++ keyboards/hasupad/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Latincompass
+Copyright 2020 Latincompass@latincompass
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
