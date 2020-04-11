9d215a8697260e1dde6280d7608f405f16b8dc36
HASU <latincompass@163.com>
2020-04-12 05:01:14 +0800

    Update hasupad.h

--- keyboards/hasupad/hasupad.h
+++ keyboards/hasupad/hasupad.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 hasuobs
+/* Copyright <Latincompass> (@latincompass)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
