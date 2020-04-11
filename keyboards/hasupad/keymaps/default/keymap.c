904d61e9b8df3b840e5e38731aeabd90f6290217
HASU <latincompass@163.com>
2020-04-12 05:39:25 +0800

    Update keymap.c

--- keyboards/hasupad/keymaps/default/keymap.c
+++ keyboards/hasupad/keymaps/default/keymap.c
@@ -83,10 +83,4 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     }
   }
 }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
-  return true;
-}
-
 void matrix_init_user(void) { render_logo(); }
