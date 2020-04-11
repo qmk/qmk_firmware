51d7f752a22b184d09dcaf50037f9e4582394b02
HASU <latincompass@163.com>
2020-04-12 05:25:56 +0800

    Update hasupad.c

--- keyboards/hasupad/hasupad.c
+++ keyboards/hasupad/hasupad.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 HASU
+/* Copyright 2020 latincompass
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "hasupad.h"
+#include "latinpad.h"
 
 // Optional override functions below.
 // You can leave any or all of these undefined.
