b8c3f4c60b1d58cb8261b96c76e1f4cd03c1b0f8
Purdea Andrei <andrei@purdea.ro>
2020-04-11 14:29:48 +0300

    quantum/debounce: Added sym_pk debounce algorithm (#8587)
    * quantum/debounce: Added sym_pk debounce algorithm
    
    * Apply suggestions from code review
    
    Co-Authored-By: Ryan <fauxpark@gmail.com>
    
    * quantum/debounce/sym_pk: delete comments and rename functions following code review
    
    * quantum/debounce/sym_pk: Modifications for code readability according to code review
    
    * quantum/debounce/sym_pk: Modifications for code readability according to code review (2)
    
    * quantum/debounce/sym_pk: code review: cleaner code
    
    Co-Authored-By: Nick Brassel <nick@tzarc.org>
    
    Co-authored-by: Ryan <fauxpark@gmail.com>
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- quantum/debounce/sym_pk.c
+++ quantum/debounce/sym_pk.c
@@ -1,5 +1,6 @@
 /*
 Copyright 2017 Alex Ong<the.onga@gmail.com>
+Copyright 2020 Andrei Purdea<andrei@purdea.ro>
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
@@ -13,9 +14,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
-Basic per-key algorithm. Uses an 8-bit counter per key.
-After pressing a key, it immediately changes state, and sets a counter.
-No further inputs are accepted until DEBOUNCE milliseconds have occurred.
+Basic symmetric per-key algorithm. Uses an 8-bit counter per key.
+When no state changes have occured for DEBOUNCE milliseconds, we push the state.
 */
 
 #include "matrix.h"
@@ -33,23 +33,22 @@ No further inputs are accepted until DEBOUNCE milliseconds have occurred.
 
 static debounce_counter_t *debounce_counters;
 static bool                counters_need_update;
-static bool                matrix_need_update;
 
 #define DEBOUNCE_ELAPSED 251
 #define MAX_DEBOUNCE (DEBOUNCE_ELAPSED - 1)
 
 static uint8_t wrapping_timer_read(void) {
-    static uint16_t time        = 0;
+    static uint16_t time = 0;
     static uint8_t  last_result = 0;
-    uint16_t        new_time    = timer_read();
-    uint16_t        diff        = new_time - time;
-    time                        = new_time;
-    last_result                 = (last_result + diff) % (MAX_DEBOUNCE + 1);
+    uint16_t new_time = timer_read();
+    uint16_t diff = new_time - time;
+    time = new_time;
+    last_result = (last_result + diff) % (MAX_DEBOUNCE + 1);
     return last_result;
 }
 
-void update_debounce_counters(uint8_t num_rows, uint8_t current_time);
-void transfer_matrix_values(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, uint8_t current_time);
+void update_debounce_counters_and_transfer_if_expired(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, uint8_t current_time);
+void start_debounce_counters(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, uint8_t current_time);
 
 // we use num_rows rather than MATRIX_ROWS to support split keyboards
 void debounce_init(uint8_t num_rows) {
@@ -65,16 +64,15 @@ void debounce_init(uint8_t num_rows) {
 void debounce(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, bool changed) {
     uint8_t current_time = wrapping_timer_read();
     if (counters_need_update) {
-        update_debounce_counters(num_rows, current_time);
+        update_debounce_counters_and_transfer_if_expired(raw, cooked, num_rows, current_time);
     }
 
-    if (changed || matrix_need_update) {
-        transfer_matrix_values(raw, cooked, num_rows, current_time);
+    if (changed) {
+        start_debounce_counters(raw, cooked, num_rows, current_time);
     }
 }
 
-// If the current time is > debounce counter, set the counter to enable input.
-void update_debounce_counters(uint8_t num_rows, uint8_t current_time) {
+void update_debounce_counters_and_transfer_if_expired(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, uint8_t current_time) {
     counters_need_update                 = false;
     debounce_counter_t *debounce_pointer = debounce_counters;
     for (uint8_t row = 0; row < num_rows; row++) {
@@ -82,6 +80,7 @@ void update_debounce_counters(uint8_t num_rows, uint8_t current_time) {
             if (*debounce_pointer != DEBOUNCE_ELAPSED) {
                 if (TIMER_DIFF(current_time, *debounce_pointer, MAX_DEBOUNCE) >= DEBOUNCE) {
                     *debounce_pointer = DEBOUNCE_ELAPSED;
+                    cooked[row]       = (cooked[row] & ~(ROW_SHIFTER << col)) | (raw[row] & (ROW_SHIFTER << col));
                 } else {
                     counters_need_update = true;
                 }
@@ -91,27 +90,21 @@ void update_debounce_counters(uint8_t num_rows, uint8_t current_time) {
     }
 }
 
-// upload from raw_matrix to final matrix;
-void transfer_matrix_values(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, uint8_t current_time) {
-    matrix_need_update                   = false;
+void start_debounce_counters(matrix_row_t raw[], matrix_row_t cooked[], uint8_t num_rows, uint8_t current_time) {
     debounce_counter_t *debounce_pointer = debounce_counters;
     for (uint8_t row = 0; row < num_rows; row++) {
-        matrix_row_t delta        = raw[row] ^ cooked[row];
-        matrix_row_t existing_row = cooked[row];
+        matrix_row_t delta = raw[row] ^ cooked[row];
         for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-            matrix_row_t col_mask = (ROW_SHIFTER << col);
-            if (delta & col_mask) {
+            if (delta & (ROW_SHIFTER << col)) {
                 if (*debounce_pointer == DEBOUNCE_ELAPSED) {
                     *debounce_pointer    = current_time;
                     counters_need_update = true;
-                    existing_row ^= col_mask;  // flip the bit.
-                } else {
-                    matrix_need_update = true;
                 }
+            } else {
+                *debounce_pointer = DEBOUNCE_ELAPSED;
             }
             debounce_pointer++;
         }
-        cooked[row] = existing_row;
     }
 }
 
