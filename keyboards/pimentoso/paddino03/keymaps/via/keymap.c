a1ae079ab6606312f13027118cb3ebc049a7d2ac
Michele Ferri <ferrimiche@gmail.com>
2020-04-11 15:00:03 +0200

    pimentoso/paddino03 via support

--- keyboards/pimentoso/paddino03/keymaps/via/keymap.c
+++ keyboards/pimentoso/paddino03/keymaps/via/keymap.c
@@ -17,8 +17,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_ENT),
-  [1] = LAYOUT(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_ENT),
-  [2] = LAYOUT(KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_ENT),
-  [3] = LAYOUT(KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_ENT),
+  [0] = LAYOUT(KC_Q, KC_W, KC_E, KC_A, KC_S, KC_D, KC_ENT),
+  [1] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT),
+  [2] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT),
+  [3] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT),
 };
