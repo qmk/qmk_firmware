87b5cd93e14f6e13c75b378a172c6f1f3034b16c
MakotoKurauchi <pluis@me.com>
2020-04-12 09:59:49 +0900

    Update keyboards/yushakobo/quick7/rules.mk
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/yushakobo/quick7/rules.mk
+++ keyboards/yushakobo/quick7/rules.mk
@@ -24,7 +24,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
