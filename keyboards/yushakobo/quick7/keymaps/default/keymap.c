2997a7a3cfd5e268d851b78d2d5255d0fae58fdd
MakotoKurauchi <pluis@me.com>
2020-04-12 10:00:35 +0900

    Update keyboards/yushakobo/quick7/keymaps/default/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/yushakobo/quick7/keymaps/default/keymap.c
+++ keyboards/yushakobo/quick7/keymaps/default/keymap.c
@@ -28,16 +28,15 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = LAYOUT( \
-        KC_MUTE,    MO(_FUNC1), RGB_MOD, \
-        S(KC_TAB),  KC_UP,      KC_TAB, \
-        KC_LEFT,    KC_DOWN,    KC_RGHT \
+    [_BASE] = LAYOUT(
+        KC_MUTE,   MO(_FUNC1), RGB_MOD,
+        S(KC_TAB), KC_UP,      KC_TAB,
+        KC_LEFT,   KC_DOWN,    KC_RGHT
     ),
-    [_FUNC1] = LAYOUT( \
-        RESET,   KC_TRNS, RGB_TOG, \
-        KC_HOME, KC_VOLU, KC_END, \
-        KC_MPRV, KC_VOLD, KC_MNXT \
-
+    [_FUNC1] = LAYOUT(
+        RESET,   KC_TRNS, RGB_TOG,
+        KC_HOME, KC_VOLU, KC_END,
+        KC_MPRV, KC_VOLD, KC_MNXT
     )
 };
 
