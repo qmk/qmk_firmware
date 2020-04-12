8d5cd34e5564b9226eb2f37905590b70d3acfbb8
Michele Ferri <ferrimiche@gmail.com>
2020-04-12 15:54:25 +0200

    wip

--- keyboards/pimentoso/paddino03/paddino03.c
+++ keyboards/pimentoso/paddino03/paddino03.c
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "paddino03.h"
+#include "tmk_core/common/eeprom.h"
+#include "print.h"
 
 void keyboard_pre_init_kb(void) {
   // Set the layer LED IO as outputs
@@ -41,10 +43,6 @@ layer_state_t layer_state_set_kb(layer_state_t state) {
   writePin(LAYER_INDICATOR_LED_3, layer == 3);
   return layer_state_set_user(state);
 }
-
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
 
 void matrix_init_kb(void) {
   // put your keyboard start-up code here
@@ -81,23 +79,114 @@ void matrix_init_kb(void) {
 }
 
 uint8_t selected_layer = 0;
-void encoder_update_kb(uint8_t index, bool clockwise) {
-    if (clockwise) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case ENCODER_PRESS:
+      if (record->event.pressed) {
+        // Do something when pressed
+      } else {
+        // Do something else when release
         if (selected_layer < 3) {
             selected_layer ++;
         }
-        // enable this for looping to first layer
-        // else {
-        //     selected_layer = 0;
-        // }
-    } else {
-        if (selected_layer > 0) {
-            selected_layer --;
+        else {
+            selected_layer = 0;
         }
-        // enable this for looping to last layer
-        // else {
-        //     selected_layer = 3;
-        // }
+        layer_move(selected_layer);
+      }
+      return false; // Skip all further processing of this key
+    default:
+      return true; // Process all other keycodes normally
+  }
+}
+
+void encoder_update_kb(uint8_t index, bool clockwise) {
+    uint16_t keycode = get_encoder_key(clockwise ? ENCODER_CW : ENCODER_CCW);
+    tap_code(keycode);
+}
+
+uint16_t get_encoder_key(uint8_t rotation) {
+    // TODO look for the mapped keycode on this layer
+    uint16_t keycode = 0;
+    switch (rotation) {
+        case ENCODER_CW:
+            keycode = KC_AUDIO_VOL_UP;
+            break;
+            // TODO get keycode at row0, col8
+        default:
+            keycode = KC_AUDIO_VOL_DOWN;
+            break;
+            // TODO get keycode at row0, col7
     }
-    layer_move(selected_layer);
+    return keycode;
+}
+
+void raw_hid_receive_kb( uint8_t *data, uint8_t length )
+{
+  uint8_t *command_id = &(data[0]);
+  uint8_t *command_data = &(data[1]);
+
+  print("inside raw_hid_receive_kb\n");
+  xprintf("%d command_id\n", command_id);
+  xprintf("%d command_data\n", command_data);
+
+  switch ( *command_id )
+  {
+    case id_get_keyboard_value:
+    {
+        switch(command_data[0]) {
+            case 0: {
+                uint16_t keycode = get_custom_encoder_config(ENCODER_CW);
+                command_data[2] =  keycode >> 8;
+                command_data[3] = keycode & 0xFF;
+                keycode = get_custom_encoder_config(ENCODER_CCW);
+                command_data[4] =  keycode >> 8;
+                command_data[5] = keycode & 0xFF;
+                break;
+            }
+            default: {
+                *command_id = id_unhandled;
+                break;
+            }
+        }
+        break;
+    }
+    case id_set_keyboard_value:
+    {
+      switch(command_data[0]) {
+        case 0: {
+          uint8_t rotation = command_data[2];
+          uint16_t keycode = (command_data[3] << 8) | command_data[4];
+          set_custom_encoder_config(rotation, keycode);
+          break;
+        }
+        default: {
+          *command_id = id_unhandled;
+          break;
+        }
+      }
+      break;
+    }
+    default:
+    {
+      // Unhandled message.
+      *command_id = id_unhandled;
+      break;
+    }
+  }
+  // DO NOT call raw_hid_send(data,length) here, let caller do this
+}
+
+uint16_t get_custom_encoder_config(uint8_t rotation){
+    void* addr = (void*)(EEPROM_ENCODER_VALUES + (selected_layer * 4) + (rotation * 2));
+    //big endian
+    uint16_t keycode = eeprom_read_byte(addr) << 8;
+    keycode |= eeprom_read_byte(addr + 1);
+    return keycode;
+}
+
+void set_custom_encoder_config(uint8_t rotation, uint16_t new_code){
+    void* addr = (void*)(EEPROM_ENCODER_VALUES + (selected_layer * 4) + (rotation * 2));
+    eeprom_update_byte(addr, (uint8_t)(new_code >> 8));
+    eeprom_update_byte(addr + 1, (uint8_t)(new_code & 0xFF));
 }
