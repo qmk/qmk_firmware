6e4f488f789db4d231728243c6a52eb9b2156f4f
Giulio Mecocci <giuliomecocci@hotmail.it>
2020-04-11 21:32:51 -0400

    Added raise2 layer so we can delete things easily when in layer4 by pressing raise

--- keyboards/lets_split/keymaps/gm2211/keymap.c
+++ keyboards/lets_split/keymaps/gm2211/keymap.c
@@ -10,8 +10,9 @@ enum planck_layers {
   _RAISE,
   _ADJUST,
   _LAYER4,
-  _LAYER5,
+  _RAISE2, // copy of RAISE to make it accessible from LAYER4
   _LAYER6,
+  _LAYER7,
 };
 
 enum custom_keycodes {
@@ -20,6 +21,7 @@ enum custom_keycodes {
 
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
+#define RAISE2 MO(_RAISE2)
 #define ADJUST MO(_ADJUST)
 
 #define KC_MAC_UNDO LGUI(KC_Z)
@@ -33,16 +35,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,      KC_Q,        KC_W,         KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,    KC_I,      KC_O,     KC_P,        KC_BSPACE, \
     MO(4),       KC_A,        KC_S,         KC_D,    KC_F,    KC_G,     KC_H,   KC_J,    KC_K,      KC_L,     KC_SCOLON,   KC_QUOTE, \
     KC_LSHIFT,   KC_Z,        KC_X,         KC_C,    KC_V,    KC_B,     KC_N,   KC_M,    KC_COMMA,  KC_DOT,   KC_SLASH,    KC_RSHIFT, \
-    KC_LCTRL,    KC_LCTRL,    KC_LALT,      KC_LGUI, LOWER,   KC_SPACE, RAISE,  RAISE,   KC_MINUS,  KC_MINUS, KC_GRAVE,    MO(5)
+    KC_LCTRL,    KC_LCTRL,    KC_LALT,      KC_LGUI, LOWER,   KC_SPACE, RAISE,  RAISE,   KC_MINUS,  KC_MINUS, KC_GRAVE,    MO(6)
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12( \
-    LGUI(KC_SCOLON), LGUI(KC_Q),               LGUI(KC_W),        LALT(LGUI(KC_L)),  LALT(KC_ENTER),   LGUI(KC_T),      KC_CIRC,          KC_AMPR,                              KC_ASTR,           LALT(LGUI(KC_O)),    LGUI(KC_P),       KC_TRANSPARENT, \
+    LGUI(KC_SCOLON), LGUI(KC_Q),               LGUI(KC_W),        LALT(LGUI(KC_L)),  LALT(KC_ENTER),   LGUI(KC_T),      KC_CIRC,          KC_AMPR,                            KC_ASTR,           LALT(LGUI(KC_O)),    LGUI(KC_P),       KC_TRANSPARENT, \
     KC_TRANSPARENT,  LGUI(KC_A),               LGUI(KC_S),        KC_ENTER,          LGUI(KC_F),       LGUI(KC_G),      KC_TRANSPARENT,   LGUI(LALT(LCTL(LSFT(KC_SPACE)))),   KC_PLUS,           KC_EQUAL,            KC_PIPE,          KC_PIPE, \
-    KC_TRANSPARENT,  KC_MAC_UNDO,              KC_MAC_CUT,        KC_MAC_COPY,       KC_MAC_PASTE,     LGUI(KC_B),      LGUI(KC_N),       KC_ENTER,                             KC_UNDS,           KC_GRAVE,            KC_BSLASH,        KC_TRANSPARENT, \
-    KC_LCTRL,        LCTL(LGUI(LSFT(KC_4))),   KC_TRANSPARENT,    KC_TRANSPARENT,    KC_TRANSPARENT,   KC_TRANSPARENT,  KC_NO,            KC_TRANSPARENT,                       KC_UNDS,           KC_AUDIO_VOL_DOWN,   KC_AUDIO_VOL_UP,  KC_TRANSPARENT
+    KC_TRANSPARENT,  KC_MAC_UNDO,              KC_MAC_CUT,        KC_MAC_COPY,       KC_MAC_PASTE,     LGUI(KC_B),      LGUI(KC_N),       KC_ENTER,                           KC_UNDS,           KC_GRAVE,            KC_BSLASH,        KC_TRANSPARENT, \
+    KC_LCTRL,        LCTL(LGUI(LSFT(KC_4))),   KC_TRANSPARENT,    KC_TRANSPARENT,    KC_TRANSPARENT,   KC_TRANSPARENT,  KC_NO,            KC_TRANSPARENT,                     KC_UNDS,           KC_AUDIO_VOL_DOWN,   KC_AUDIO_VOL_UP,  KC_TRANSPARENT
 ),
 
+// If you modify something here, also modify it in _RAISE2 - tried to avoid copy-pasting but constants / macros / #define are super-annoying in c and not worth the time
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRAVE,       KC_1,            KC_2,     KC_3,     KC_4,     KC_5,            KC_6,     KC_ENTER,        KC_DELETE,            KC_DELETE,          KC_DELETE,        KC_TRANSPARENT, \
   KC_TRANSPARENT, KC_F1,           KC_F2,    KC_F3,    KC_F4,    KC_F5,           KC_LEFT,  KC_DOWN,         KC_UP,                KC_RIGHT,           KC_BSPACE,        KC_ENTER, \
@@ -61,17 +64,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD,         KC_EXLM,         KC_AT,    KC_LCBR,      KC_RCBR,      KC_PIPE,         KC_KP_SLASH,     KC_7,   KC_8,     KC_9,    KC_AMPR,    KC_BSLASH, \
   KC_TRANSPARENT,  KC_HASH,         KC_DLR,   KC_LPRN,      KC_RPRN,      KC_GRAVE,        KC_KP_ASTERISK,  KC_4,   KC_5,     KC_6,    KC_0,       KC_DQUO, \
   KC_TRANSPARENT,  KC_PERC,         KC_CIRC,  KC_LBRACKET,  KC_RBRACKET,  KC_TILD,         KC_PLUS,         KC_1,   KC_2,     KC_3,    KC_KP_DOT,  KC_ENTER, \
-  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_LALT,  KC_LGUI,      KC_LGUI,      KC_TRANSPARENT,  KC_NO,           RAISE,  KC_UNDS,  KC_DOT,  KC_TILD,    KC_TRANSPARENT
+  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_LALT,  KC_LGUI,      KC_LGUI,      KC_TRANSPARENT,  KC_NO,           RAISE2,  KC_UNDS,  KC_DOT,  KC_TILD,    KC_TRANSPARENT
 ),
 
-[_LAYER5] =  LAYOUT_ortho_4x12( \
-  KC_AUDIO_MUTE,   KC_AUDIO_VOL_DOWN,   KC_AUDIO_VOL_UP,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, \
-  KC_TRANSPARENT,  KC_BRIGHTNESS_DOWN,  KC_BRIGHTNESS_UP, KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, \
-  KC_TRANSPARENT,  KC_F1,               KC_F2,            KC_F3,           KC_F4,           KC_F5,           KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_F11, \
-  KC_TRANSPARENT,  KC_TRANSPARENT,      KC_TRANSPARENT,   KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_NO,           KC_F12,          KC_TRANSPARENT,  KC_TRANSPARENT,  MO(6),           KC_TRANSPARENT
+[_RAISE2] = LAYOUT_ortho_4x12( \
+  KC_GRAVE,       KC_1,            KC_2,     KC_3,     KC_4,     KC_5,            KC_6,     KC_ENTER,        KC_DELETE,            KC_DELETE,          KC_DELETE,        KC_TRANSPARENT, \
+  KC_TRANSPARENT, KC_F1,           KC_F2,    KC_F3,    KC_F4,    KC_F5,           KC_LEFT,  KC_DOWN,         KC_UP,                KC_RIGHT,           KC_BSPACE,        KC_ENTER, \
+  KC_LSHIFT,      KC_F7,           KC_F8,    KC_F9,    KC_F10,   KC_F11,          KC_EQUAL, KC_ESCAPE,       LCTL(KC_SPACE),       KC_TRANSPARENT,     KC_BSLASH,        KC_ENTER, \
+  KC_LCTRL,       KC_TRANSPARENT,  KC_LALT,  KC_LGUI,  KC_LALT,  LALT(KC_SPACE),  KC_NO,    KC_TRANSPARENT,  KC_MEDIA_NEXT_TRACK,  KC_AUDIO_VOL_DOWN,  KC_AUDIO_VOL_UP,  KC_AUDIO_MUTE
 ),
 
 [_LAYER6] =  LAYOUT_ortho_4x12( \
+  KC_AUDIO_MUTE,   KC_AUDIO_VOL_DOWN,   KC_AUDIO_VOL_UP,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, \
+  KC_TRANSPARENT,  KC_BRIGHTNESS_DOWN,  KC_BRIGHTNESS_UP, KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, \
+  KC_TRANSPARENT,  KC_F1,               KC_F2,            KC_F3,           KC_F4,           KC_F5,           KC_F6,           KC_F7,           KC_F8,           KC_F9,           KC_F10,          KC_F11, \
+  KC_TRANSPARENT,  KC_TRANSPARENT,      KC_TRANSPARENT,   KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_NO,           KC_F12,          KC_TRANSPARENT,  KC_TRANSPARENT,  MO(7),           KC_TRANSPARENT
+),
+
+[_LAYER7] =  LAYOUT_ortho_4x12( \
   KC_TRANSPARENT,  KC_TRANSPARENT,      KC_TRANSPARENT,  KC_MS_UP,         KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, \
   KC_TRANSPARENT,  KC_MS_LEFT,          KC_MS_LEFT,      KC_MS_DOWN,       KC_MS_RIGHT,     KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, \
   KC_TRANSPARENT,  KC_TRANSPARENT,      KC_TRANSPARENT,  KC_TRANSPARENT,   KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT, \
