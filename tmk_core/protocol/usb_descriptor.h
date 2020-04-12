7f86d8b50290937b0ce9d1526c10b0392bacae2a
Purdea Andrei <andrei@purdea.ro>
2020-04-12 18:51:12 +0300

    tmk_core/protocol/usb_descriptor.h: make RAW usb endpoint size configurable

--- tmk_core/protocol/usb_descriptor.h
+++ tmk_core/protocol/usb_descriptor.h
@@ -243,7 +243,9 @@ enum usb_endpoints {
 #define KEYBOARD_EPSIZE 8
 #define SHARED_EPSIZE 32
 #define MOUSE_EPSIZE 8
-#define RAW_EPSIZE 32
+#ifndef RAW_EPSIZE
+#    define RAW_EPSIZE 32
+#endif
 #define CONSOLE_EPSIZE 32
 #define MIDI_STREAM_EPSIZE 64
 #define CDC_NOTIFICATION_EPSIZE 8
