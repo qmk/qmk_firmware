57459febf1110c74198a181190b6517e9bea5942
MakotoKurauchi <pluis@me.com>
2020-04-12 09:59:35 +0900

    Update keyboards/yushakobo/quick7/quick7.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/yushakobo/quick7/quick7.c
+++ keyboards/yushakobo/quick7/quick7.c
@@ -15,36 +15,3 @@
  */
 
 #include "quick7.h"
-
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
-
-/*
-void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
-
-    matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
-
-    matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    // put your per-action keyboard code here
-    // runs for every action, just before processing by the firmware
-
-    return process_record_user(keycode, record);
-}
-
-bool led_update_kb(led_t led_state) {
-    // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-    return led_update_user(led_state);
-}
-*/
