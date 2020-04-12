90eef93f6af45bd2a8e467d4ac800fded7bb33b1
pabile <1162412+pabile@users.noreply.github.com>
2020-04-12 21:43:13 +0800

    Update keymap.c
    removed unnecessary comments

--- keyboards/pabile/p18/keymaps/default/keymap.c
+++ keyboards/pabile/p18/keymaps/default/keymap.c
@@ -1,13 +1,4 @@
 #include QMK_KEYBOARD_H
-/* 
-
-
-enable rgb here 
-
-
-*/
-
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
@@ -16,13 +7,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 					KC_A, KC_TAB, KC_P3, KC_P2, KC_P1, 
 					KC_B, KC_PENT, KC_PDOT, KC_P0, KC_DEL)
         
-/* alternate?
-	[0] = LAYOUT(
-					      KC_PMNS, KC_P9,   KC_P8, KC_P7, 
-					      KC_PPLS, KC_P6,   KC_P5, KC_P4, 
-					KC_A, KC_TAB,  KC_P3,   KC_P2, KC_P1, 
-					KC_B, KC_PENT, KC_PDOT, KC_P0, KC_DEL)
-*/
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
