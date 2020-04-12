e73e5eb963f567626fe55370e7712208a97c4344
HASU <latincompass@163.com>
2020-04-12 10:36:19 +0800

    Update keymap.c

--- keyboards/latinpad/keymaps/default/keymap.c
+++ keyboards/latinpad/keymaps/default/keymap.c
@@ -1,6 +1,4 @@
-
-  /* Keymap _1: (Base Layer) Default Layer
-  
+ /* Keymap _1: (Base Layer) Default Layer
    * .-----------.
    * |PGUP | PGDN| 
    * |-----|-----|-----|-----|
@@ -14,7 +12,6 @@
    * .-----------------------.
    */
      /* Keymap _1: (Base Layer) Default Layer
-  
    * .------------------.
    * |NUMLOCK|Caculator | 
    * |-------|-------|-----|-----|
@@ -41,7 +38,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 #include QMK_KEYBOARD_H
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_hasu_5x4(
         KC_KP_7, KC_KP_8, KC_KP_9, MO(1),
@@ -57,12 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NUMLOCK, KC_CALCULATOR, _______, _______),
 };
 
-
 static void render_logo(void) {
     static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,  0};
     oled_write_P(qmk_logo, false);
 }
-
 
 #ifdef OLED_DRIVER_ENABLE
 void oled_task_user(void) { render_logo(); }
