66b5ac7f0d5c45b5cd397f9ea58d5cd47f2fc8ae
Sergey Vlasov <sigprof@gmail.com>
2020-04-12 20:26:35 +0300

    id80: Set RGBLED_NUM to 20 to match the actual PCB
    The vendor-supplied ID80.json file specified that the PCB should have
    28 RGB LEDs in the chain.  However, the actual PCB that was shipped
    from AliExpress had 20 LEDs in the chain (16 underglow LEDs, and then 4
    more LEDs on top of the PCB, to the right of the Enter key location).
    Update RGBLED_NUM to match the actual PCB.

--- keyboards/id80/config.h
+++ keyboards/id80/config.h
@@ -60,7 +60,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN E2
 #ifdef RGB_DI_PIN
-    #define RGBLED_NUM 28
+    #define RGBLED_NUM 20       /* 16 underglow LEDs, 4 top LEDs */
     #define RGBLIGHT_HUE_STEP 8
     #define RGBLIGHT_SAT_STEP 8
     #define RGBLIGHT_VAL_STEP 8
