0d98b19b9a49782ec36158d4f52d378d7fd8d40f
fauxpark <fauxpark@gmail.com>
2020-04-12 14:46:55 +1000

    Enable Bootmagic Lite at keyboard level

--- keyboards/maartenwut/wasdat/config.h
+++ keyboards/maartenwut/wasdat/config.h
@@ -252,7 +252,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* Bootmagic Lite key configuration */
-// #define BOOTMAGIC_LITE_ROW 0
-// #define BOOTMAGIC_LITE_COLUMN 0
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 5
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
