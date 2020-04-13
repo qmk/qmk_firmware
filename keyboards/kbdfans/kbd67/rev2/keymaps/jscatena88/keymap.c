5fbf3b9fb28f5e896db9c99a7875a806a6350915
Jason Scatena <jscatena88@users.noreply.github.com>
2020-04-13 18:21:10 -0400

    Update keyboards/kbdfans/kbd67/rev2/keymaps/jscatena88/keymap.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/kbdfans/kbd67/rev2/keymaps/jscatena88/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/jscatena88/keymap.c
@@ -14,13 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  QMKBEST = SAFE_RANGE,
-  QMKURL
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
