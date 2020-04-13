fea5fa9211b8a0451799fba7801dd4a49d0f045e
Johannes <you@example.com>
2020-04-13 15:29:08 +0200

    audio_arm_dac: basic: add missing bracket

--- quantum/audio/driver_chibios_dac_basic.c
+++ quantum/audio/driver_chibios_dac_basic.c
@@ -138,7 +138,7 @@ float channel_2_get_frequency(void) { return channel_2_frequency; }
 
 static void gpt_cb8(GPTDriver *gptp) {
     if (audio_update_state()) {
-#if (defined (AUDIO_PIN_A4) && defined(AUDIO_PIN_A5)) || (defined(AUDIO_PIN_A4) && defined(AUDIO_PIN_ALT_AS_NEGATIVE) && defined(AUDIO_PIN_ALT_A5) || (defined(AUDIO_PIN_A5) && defined(AUDIO_PIN_ALT_AS_NEGATIVE) && defined(AUDIO_PIN_ALT_A4))
+#if (defined (AUDIO_PIN_A4) && defined(AUDIO_PIN_A5)) || (defined(AUDIO_PIN_A4) && defined(AUDIO_PIN_ALT_AS_NEGATIVE) && defined(AUDIO_PIN_ALT_A5)) || (defined(AUDIO_PIN_A5) && defined(AUDIO_PIN_ALT_AS_NEGATIVE) && defined(AUDIO_PIN_ALT_A4))
         // one piezo/speaker connected to both audio pins, the generated squarewaves are inverted
         channel_1_set_frequency(audio_get_processed_frequency(0));
         channel_2_set_frequency(audio_get_processed_frequency(0));
