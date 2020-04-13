41f9409c5f62dfb58019d810270becf4630d6443
HASU <latincompass@163.com>
2020-04-14 07:14:36 +0800

    Update keyboards/latinpad/config.h
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/latinpad/config.h
+++ keyboards/latinpad/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Latincompass@latincompass
+Copyright 2020 latincompass
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
