34109fbcf7cdaa135812c28e3fa8a5b8b2d2ba4b
Mysti Kruger <khitsule@gmail.com>
2020-04-13 17:25:59 +1000

    iris keymap refined

--- keyboards/keebio/iris/keymaps/khitsule/config.h
+++ keyboards/keebio/iris/keymaps/khitsule/config.h
@@ -18,13 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // #define USE_I2C
-// #define EE_HANDS
-#define MASTER_RIGHT
+#define EE_HANDS
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #undef RGBLED_NUM
-// #define RGBLIGHT_ANIMATIONS
-// #define RGBLED_NUM 12
-// #define RGBLIGHT_HUE_STEP 8
-// #define RGBLIGHT_SAT_STEP 8
-// #define RGBLIGHT_VAL_STEP 8
