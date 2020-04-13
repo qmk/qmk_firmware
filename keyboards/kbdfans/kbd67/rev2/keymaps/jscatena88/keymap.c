bd63ea6068431c8fbf453fe5a62c9ab7a6d0aeb3
Jason Scatena <jscatena88@users.noreply.github.com>
2020-04-13 18:21:48 -0400

    Update keyboards/kbdfans/kbd67/rev2/keymaps/jscatena88/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/kbdfans/kbd67/rev2/keymaps/jscatena88/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/jscatena88/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 'jscatena88'
+/* Copyright 2018 jscatena88
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
