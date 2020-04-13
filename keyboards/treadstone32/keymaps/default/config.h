34965beb1bbe57f2a4ce8ff4b50578085775bd0a
marksard <marksard@rocketmail.com>
2020-04-13 11:34:10 +0900

    I used to set this to a per-keymap setting, so I'll undo it.

--- keyboards/treadstone32/keymaps/default/config.h
+++ keyboards/treadstone32/keymaps/default/config.h
@@ -22,3 +22,19 @@
 
 #define TAPPING_LAYER_TERM 150 // Custom LT Tapping term
 #define TAPPING_TERM_PER_KEY
+
+#ifdef MOUSEKEY_ENABLE
+  #undef MOUSEKEY_INTERVAL
+  #define MOUSEKEY_INTERVAL 1
+  #undef MOUSEKEY_TIME_TO_MAX
+  #define MOUSEKEY_TIME_TO_MAX 150
+
+  #undef MOUSEKEY_MAX_SPEED
+  #define MOUSEKEY_MAX_SPEED 3
+
+  #undef MOUSEKEY_MOVE_DELTA
+  #define MOUSEKEY_MOVE_DELTA 4
+
+  #undef MOUSEKEY_DELAY
+  #define MOUSEKEY_DELAY 0
+#endif
--- keyboards/treadstone32/rev1/config.h
+++ keyboards/treadstone32/rev1/config.h
@@ -95,19 +95,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-#ifdef MOUSEKEY_ENABLE
-  #undef MOUSEKEY_INTERVAL
-  #define MOUSEKEY_INTERVAL 1
-  #undef MOUSEKEY_TIME_TO_MAX
-  #define MOUSEKEY_TIME_TO_MAX 150
-
-  #undef MOUSEKEY_MAX_SPEED
-  #define MOUSEKEY_MAX_SPEED 3
-
-  #undef MOUSEKEY_MOVE_DELTA
-  #define MOUSEKEY_MOVE_DELTA 4
-
-  #undef MOUSEKEY_DELAY
-  #define MOUSEKEY_DELAY 0
-#endif
