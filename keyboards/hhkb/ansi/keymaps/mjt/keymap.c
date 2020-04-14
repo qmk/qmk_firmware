e1323f94c90647af388dd2c03430bef6699fae76
Diego Song <diego.m.song@gmail.com>
2020-04-13 22:06:58 -0400

    Define rules.mk for dynamic macro to rid of warning for ansi/keymaps/mjt/

--- keyboards/hhkb/ansi/keymaps/mjt/keymap.c
+++ keyboards/hhkb/ansi/keymaps/mjt/keymap.c
@@ -17,7 +17,6 @@ enum hhkb_keycodes
     DYNAMIC_MACRO_RANGE,
 };
 
-#include "dynamic_macro.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -104,7 +103,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
     uint16_t macro_kc = (keycode == MO(DYN) ? DYN_REC_STOP : keycode);
-    if (!process_record_dynamic_macro(macro_kc, record))
+    if (!process_dynamic_macro(macro_kc, record))
     {
         return false;
     }
