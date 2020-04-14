1b0cef96ec16b05cf05ff6a22645b7748718da6b
Giulio Mecocci <giuliomecocci@hotmail.it>
2020-04-14 14:27:11 -0400

    Made function keys layout better for debugging in intellij

--- keyboards/lets_split/keymaps/gm2211/keymap.c
+++ keyboards/lets_split/keymaps/gm2211/keymap.c
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If you modify something here, also modify it in _RAISE2 - tried to avoid copy-pasting but constants / macros / #define are super-annoying in c and not worth the time
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRAVE,   KC_1,    KC_2,     KC_3,     KC_4,     KC_5,            KC_6,      KC_ENTER,       KC_DELETE,            KC_DELETE,          KC_DELETE,        ___,      \
-  ___,        KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,           KC_LEFT,   KC_DOWN,        KC_UP,                KC_RIGHT,           KC_BSPACE,        MO(8),    \
-  KC_LSHIFT,  KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_F11,          KC_EQUAL,  KC_ESCAPE,      LCTL(KC_SPACE),       ___,                KC_BSLASH,        KC_ENTER, \
-  ___,        ___,     KC_LALT,  KC_LGUI,  KC_LALT,  LALT(KC_SPACE),  ___,       ___,            KC_MEDIA_NEXT_TRACK,  KC_AUDIO_VOL_DOWN,  KC_AUDIO_VOL_UP,  KC_AUDIO_MUTE
+  ___,        KC_F1,   KC_F3,    KC_F5,    KC_F5,    KC_F7,           KC_LEFT,   KC_DOWN,        KC_UP,                KC_RIGHT,           KC_BSPACE,        MO(8),    \
+  KC_LSHIFT,  KC_F2,   KC_F4,    KC_F6,    KC_F6,    KC_F8,           KC_EQUAL,  KC_ESCAPE,      LCTL(KC_SPACE),       ___,                KC_BSLASH,        KC_ENTER, \
+  ___,        KC_F9,   KC_LALT,  KC_LGUI,  KC_LALT,  LALT(KC_SPACE),  ___,       ___,            KC_MEDIA_NEXT_TRACK,  KC_AUDIO_VOL_DOWN,  KC_AUDIO_VOL_UP,  KC_AUDIO_MUTE
 ),
 
 [_ADJUST] =  LAYOUT_ortho_4x12( \
