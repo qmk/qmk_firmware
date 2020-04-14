65580dbb88777e5c9093f65f23322477115967d0
Khader Syed <khader.syed@aicure.com>
2020-04-14 00:16:00 -0400

    some fixes

--- keyboards/crkbd/keymaps/madhatter/config.h
+++ keyboards/crkbd/keymaps/madhatter/config.h
@@ -5,13 +5,13 @@
 #define SSD1306OLED
 
 #ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+#    undef RGBLED_NUM
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 27
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP 10
+#    define RGBLIGHT_SAT_STEP 17
+#    define RGBLIGHT_VAL_STEP 17
 #endif
 
 #undef PRODUCT
