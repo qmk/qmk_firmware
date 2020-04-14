f86a8076275bfe26fb51b792593b093518708ec9
klackygears <bronzegears@gmail.com>
2020-04-14 12:21:32 -0400

    update, make corrections

--- keyboards/handwired/brain/config.h
+++ keyboards/handwired/brain/config.h
@@ -35,6 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MATRIX_COL_PINS_RIGHT { B5, B4, E6, D7, C6, D4, D1 }
 
+#define DIODE_DIRECTION COL2ROW
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
