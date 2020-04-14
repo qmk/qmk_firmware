4c47c7b5eb08b46961d1a415d30b936d3710281d
Nick Blyumberg <nblyumberg@gmail.com>
2020-04-14 00:29:14 -0400

    Set max LED brightness to 50%

--- keyboards/1upkeyboards/super16/config.h
+++ keyboards/1upkeyboards/super16/config.h
@@ -63,7 +63,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+  #define RGBLIGHT_LIMIT_VAL 128 /* The maximum brightness level - Default is 255 */
   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
   /*== all animations enable ==*/
   #define RGBLIGHT_ANIMATIONS
