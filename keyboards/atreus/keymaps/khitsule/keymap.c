b1841e626f7ca5d007ceb301bbe664e00e93d98c
Mysti Kruger <khitsule@gmail.com>
2020-04-16 09:57:36 +1000

    removed extra closing bracket

--- keyboards/atreus/keymaps/khitsule/keymap.c
+++ keyboards/atreus/keymaps/khitsule/keymap.c
@@ -39,7 +39,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_A,    KC_S,     KC_D,     KC_F,      KC_G,                           KC_H,    KC_J,     KC_K,     KC_L,     KC_SCLN,
     KC_Z,    KC_X,     KC_C,     KC_V,      KC_B,                                  KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
     KC_ESC,  KC_TRNS,  KC_ENT,   KC_LALT,   KC_TAB,  KC_LCTL,  KC_LSFT,  KC_DEL,  KC_LGUI,  TO(ALPH),  KC_QUOT,  KC_MINUS
-  )
   ),
   [LOWR] = LAYOUT(
     KC_PSCR,   KC_HOME,    KC_UP,         KC_END,   KC_TRNS,                      KC_LPRN,        KC_F7,    KC_F8,    KC_F9,    KC_RPRN,
