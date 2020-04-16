94c13006b2a70cc3673b09be2cea2caaad8eabcf
Nicholas Junker <nicholas.junker125@gmail.com>
2020-04-16 12:47:31 -0400

    Update keyboards/dmqdesign/spin/keymaps/default/keymap.c
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/dmqdesign/spin/keymaps/default/keymap.c
+++ keyboards/dmqdesign/spin/keymaps/default/keymap.c
@@ -17,10 +17,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(/* Base */
-                KC_7, KC_8, KC_9, KC_TRNS, \
-                KC_4, KC_5, KC_6, KC_TRNS, \
-                KC_1, KC_2, KC_3, KC_TRNS, \
-                KC_0, RGB_TOG, KC_ENTER  \
+                KC_7, KC_8, KC_9, KC_TRNS,
+                KC_4, KC_5, KC_6, KC_TRNS,
+                KC_1, KC_2, KC_3, KC_TRNS,
+                KC_0, RGB_TOG, KC_ENTER
                 ),
 };
 
