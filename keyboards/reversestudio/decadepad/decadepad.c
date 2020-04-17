2a7370f20d1cdae7ba4171853a7f4248674a6ac9
‭huajijam <strhuaji@gmail.com>
2020-04-18 04:32:19 +0800

    Fixing Numlock LED Bugs

--- keyboards/reversestudio/decadepad/decadepad.c
+++ keyboards/reversestudio/decadepad/decadepad.c
@@ -1,7 +1,20 @@
 #include "decadepad.h"
-bool led_update_kb(led_t led_state) {
-    if (led_update_user(led_state)){
-        writePin(D4, led_state.num_lock);
-    }
-    return true;
+void matrix_init_kb(void) {
+  led_init_ports();
+  matrix_init_user(); 
+};
+
+void led_init_ports(void) {
+  setPinOutput(D4);
+}
+
+void led_set_kb(uint8_t usb_led) {
+  if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+    // Turn NUM Lock LED on
+    writePinLow(D4);
+  } else {
+    // Turn NUM Lock LED off
+    writePinHigh(D4);
+  }
+    led_set_user(usb_led);
 }
