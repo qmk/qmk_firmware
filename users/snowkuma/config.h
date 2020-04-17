9fff42bef58cfe54a28ff255762925c883b5426d
Dan Smith <dan.smith@theappbusiness.com>
2020-04-17 10:27:51 +0100

    Setup basic working keymap userspace based on drashna and ninjonas

---
+++ users/snowkuma/config.h
@@ -0,0 +1,14 @@
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+    #define TAPPING_TERM 200
+#endif
+
+// Mouse Settings: Smoothing out mouse movement on keypress
+#ifndef MOUSEKEY_INTERVAL
+    #undef MOUSEKEY_INTERVAL
+    #define MOUSEKEY_INTERVAL 16
+#endif
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
---
+++ users/snowkuma/snowkuma.c
@@ -0,0 +1,39 @@
+/* Copyright 2019 @ninjonas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "snowkuma.h"
+
+layer_state_t layer_state_set_user (layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+#ifdef RGBLIGHT_ENABLE
+extern rgblight_config_t rgblight_config;
+#endif
+void keyboard_post_init_user() {
+  #ifdef RGBLIGHT_ENABLE
+     // Cycles through the entire hue wheel and resetting to default color
+     uint16_t default_hue = rgblight_config.hue;
+     rgblight_enable_noeeprom(); 
+     layer_state_set_user(layer_state);
+     rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+     for (uint16_t i = 255; i > 0; i--) {
+          rgblight_sethsv_noeeprom((i + default_hue) % 255, rgblight_config.sat, rgblight_config.val);
+          matrix_scan();
+          wait_ms(10);
+     }
+  #endif
+  layer_state_set_user(layer_state);
+}
