5967f7cddee843ba65bd37039fb5c4124c407951
skullydazed <skullydazed@users.noreply.github.com>
2020-04-18 16:18:42 -0700

    Update keyboards/clueboard/2x1800/2019/rules.mk
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/clueboard/2x1800/2019/rules.mk
+++ keyboards/clueboard/2x1800/2019/rules.mk
@@ -1,5 +1,13 @@
 # MCU name
 MCU = at90usb1286
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
 BOOTLOADER = halfkay
 
 # Build Options
