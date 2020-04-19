c3c329ccdebaa7cfac74124a8c3dc13c6098ff9d
Eric Gebhart <e.a.gebhart@gmail.com>
2020-04-19 18:27:09 +0200

    Refactor of rules.mk

--- keyboards/xd75/keymaps/ericgebhart/rules.mk
+++ keyboards/xd75/keymaps/ericgebhart/rules.mk
@@ -1,19 +1,3 @@
-SRC += ericgebhart.c
-
 RGBLIGHT_ENABLE = yes
 RGBLIGHT_ANIMATION = yes
 # BACKLIGHT_ENABLE = yes
-
-MOUSEKEY_ENABLE  = yes # Mouse keys
-EXTRAKEY_ENABLE  = yes # Audio control and System control
-COMMAND_ENABLE   = no # Commands for debug and configuration
-NKRO_ENABLE      = yes # USB Nkey Rollover - for issues, see github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-SWAP_HANDS_ENABLE= no # Allow swapping hands of keyboard
-KEY_LOCK_ENABLE  = yes # Enable the KC_LOCK key
-TAP_DANCE_ENABLE = yes # Enable the tap dance feature.
-CONSOLE_ENABLE   = no  # Console for debug
-
-BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration
-UNICODE_ENABLE   = no
-SLEEP_LED_ENABLE = no
-API_SYSEX_ENABLE = no
