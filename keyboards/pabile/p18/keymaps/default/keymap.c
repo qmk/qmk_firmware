25cc7e45acaec841a3dc5ad2b56926d24499f18a
pabile <1162412+pabile@users.noreply.github.com>
2020-04-19 14:54:04 +0800

    Update keymap.c

--- keyboards/pabile/p18/keymaps/default/keymap.c
+++ keyboards/pabile/p18/keymaps/default/keymap.c
@@ -2,10 +2,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-					KC_NO,KC_PMNS, KC_P9, KC_P8, KC_P7, 
-					KC_NO,KC_PPLS,KC_P6, KC_P5, KC_P4, 
-					KC_A, KC_TAB, KC_P3, KC_P2, KC_P1, 
-					KC_B, KC_PENT, KC_PDOT, KC_P0, KC_DEL)
+					         KC_P7,   KC_P8,   KC_P9, KC_PMNS, 
+					         KC_P4,   KC_P5,   KC_P6, KC_PPLS,
+					KC_MUTE, KC_P1,   KC_P2,   KC_P3, KC_TAB,
+					KC_ESC,  KC_DEL, KC_P0, KC_PDOT, KC_PENT)
         
 };
 
