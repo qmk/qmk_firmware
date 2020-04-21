ca3b8f7ef53bc10409f8a05aa6c3d3008282506f
Joshua Diamond <josh@windowoffire.com>
2020-04-21 13:51:11 -0400

    Print version on startup

--- users/spidey3/init.c
+++ users/spidey3/init.c
@@ -1,8 +1,9 @@
+#include "version.h"
 #include "spidey3.h"
 
 void keyboard_post_init_user(void) {
-  print("keyboard_post_init_user\n");
-  uprintf("\tdebug_enable=%u\n", debug_enable);
+  uprint("keyboard_post_init_user: " QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION ", Built on: " QMK_BUILDDATE);
+  uprintf("debug_enable=%u\n", debug_enable);
 #ifdef RGBLIGHT_ENABLE
   keyboard_post_init_user_rgb();
 #endif
