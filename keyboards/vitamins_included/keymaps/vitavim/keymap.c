c9cc4fea8ec0937887b462c914786202f7e26379
Daniel Dylewsky <54324976+ddylewsky@users.noreply.github.com>
2020-04-20 20:55:55 -0400

    Update keyboards/vitamins_included/keymaps/vitavim/keymap.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/vitamins_included/keymaps/vitavim/keymap.c
+++ keyboards/vitamins_included/keymaps/vitavim/keymap.c
@@ -1,7 +1,4 @@
 #include QMK_KEYBOARD_H
-
-extern keymap_config_t keymap_config;
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
