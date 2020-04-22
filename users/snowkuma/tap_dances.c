7e51721b5954a28d6ae84cead109941be295cf35
Dan Smith <dan.smith@theappbusiness.com>
2020-04-22 12:11:58 +0100

    Adds kraken reset and fills out comment layout

--- users/snowkuma/tap_dances.c
+++ users/snowkuma/tap_dances.c
@@ -1,22 +1,15 @@
 #include "snowkuma.h"
 
-// qk_tap_dance_action_t tap_dance_actions[] = {
-//   [TD_ESC_CAPS]     = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
-//   [TD_TAB_CTRLTAB]  = ACTION_TAP_DANCE_DOUBLE(KC_TAB, LCTL(KC_TAB)),
-//   [TD_GRV_CTRLGRV]  = ACTION_TAP_DANCE_DOUBLE(KC_GRV, LGUI(KC_GRV)),
-//   [TD_GUI_GUISPC]   = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LGUI(KC_SPC)),
-//   [TD_W_CTRLW]      = ACTION_TAP_DANCE_DOUBLE(KC_W, LGUI(KC_W)),
-//   [TD_Q_GUIQ]       = ACTION_TAP_DANCE_DOUBLE(KC_Q, LGUI(KC_Q)),
-// };
-
-
 // Tap Dance Definitions
-void safe_reset(qk_tap_dance_state_t *state, void *user_data) {
+void kraken_safe_reset(qk_tap_dance_state_t *state, void *user_data) {
 	if (state->count >=3) {
 		// Reset the keyboard if you tap the key more than three times
 		reset_keyboard();
 		reset_tap_dance(state);
-	}
+	} else {
+    SEND_STRING(SS_LGUI(" "));
+    SEND_STRING("GitKraken.app\n");
+  }
 };
 
 void zoom_toggle(qk_tap_dance_state_t *state, void *user_data) {
@@ -83,7 +76,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 	[TD_COPYCUT]    = ACTION_TAP_DANCE_FN (copy_cut),
   [TD_LBRC_BACK]  = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, LGUI(KC_LBRC)),
   [TD_RBRC_FWD]   = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, LGUI(KC_RBRC)),
-	[TD_RESET]      = ACTION_TAP_DANCE_FN (safe_reset),
+	[TD_KRAKEN]     = ACTION_TAP_DANCE_FN (kraken_safe_reset),
 	[TD_TILD]       = ACTION_TAP_DANCE_FN_ADVANCED (NULL, tilde_home, tilde_reset),
   [TD_ZOOM]       = ACTION_TAP_DANCE_FN (zoom_toggle) 
 };
--- users/snowkuma/tap_dances.h
+++ users/snowkuma/tap_dances.h
@@ -1,21 +1,12 @@
 #pragma once
 #include "snowkuma.h"
 
-// enum custom_tapdances{
-//   TD_ESC_CAPS = 0,
-//   TD_TAB_CTRLTAB,
-//   TD_GRV_CTRLGRV,
-//   TD_GUI_GUISPC,
-//   TD_W_CTRLW,
-//   TD_Q_GUIQ,
-// };
-
- // Tap Dance Declarations
+// Tap Dance Declarations
 enum {
 	TD_COPYCUT = 0,
+	TD_KRAKEN,
   TD_LBRC_BACK,
   TD_RBRC_FWD,
-	TD_RESET,
 	TD_TILD,
   TD_ZOOM
 };
@@ -23,6 +14,6 @@ enum {
 #define T_CPYCUT TD(TD_COPYCUT) // Tap to copy, double tap for cut
 #define T_LBRC TD(TD_LBRC_BACK) // Tap for [, double tap for back browser
 #define T_RBRC TD(TD_RBRC_FWD)  // Tap for ], double tap for forward browser
-#define T_RESET TD(TD_RESET)    // Tap 3x to reset (works on planck but not Corne)
+#define T_KRAKEN TD(TD_KRAKEN)   // Tap to launch gitKraken, 3x to reset (works on planck but not on corne)
 #define T_TILD TD(TD_TILD)      // Tap for ~, double tap for ~/
 #define T_ZOOM TD(TD_ZOOM)      // Tap for audio mute, double tap for video
