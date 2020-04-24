327f7e2c562131ab42b52e6cccb0c7b9600feb66
elijahblake81 <elijahblake81@gmail.com>
2020-04-24 13:42:11 -0500

    broken

--- keyboards/massdrop/alt/keymaps/elijahblake/keymap.c
+++ keyboards/massdrop/alt/keymaps/elijahblake/keymap.c
@@ -1,44 +1,141 @@
 #include QMK_KEYBOARD_H
 
+enum my_keycodes {
+    U_T_AUTO = SAFE_RANGE, // USB Extra Port Toggle Auto Detect / Always Active
+    U_T_AGCR,              // USB Toggle Automatic GCR control
+    DBG_TOG,               // DEBUG Toggle On / Off
+    DBG_MTRX,              // DEBUG Toggle Matrix Prints
+    DBG_KBD,               // DEBUG Toggle Keyboard Prints
+    DBG_MOU,               // DEBUG Toggle Mouse Prints
+    MD_BOOT,               // Restart into bootloader after hold timeout
+    HK_COSL,               // Clear held-down keys
+    QWERTY,                // Switch to QWERTY layout
+    COLEMAK,               // Switch to Colemak layout
+    DVORAK,                // Switch to Dvorak layout
+    WORKMAN,               // Switch to Workman layout
+};
+
+enum my_layers {
+    _QWERTY = 0,
+    _COLEMAK,
+    _DVORAK,
+    _WORKMAN,
+    _FUNCTION,
+    _NUMPAD,
+    _LAYOUTS,
+};
+
+#define MO_FUNC  MO(_FUNCTION)     // Hold for function layer
+#define TG_NUMP  TG(_NUMPAD)       // Toggle numpad layer
+#define OSL_LAY  OSL(_LAYOUTS)     // One-shot layer to change layout
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_65_ansi_blocker(
-		KC_ESC,  	KC_1, 		KC_2, 		KC_3, 	 KC_4, 	  KC_5,     KC_6, 	         KC_7, 				KC_8,    KC_9,    KC_0, 	  KC_MINS, 	KC_EQL, 	KC_BSPC, 	KC_DEL,	  \
-		KC_TAB,  	KC_Q, 		KC_W, 		KC_E, 	 KC_R, 	  KC_T,     KC_Y, 	         KC_U, 				KC_I,    KC_O,    KC_P, 	  KC_LBRC, 	KC_RBRC, 	KC_BSLS, 	KC_HOME,  \
-		KC_CAPS, 	KC_A, 		KC_S, 		KC_D, 	 KC_F, 	  KC_G,     KC_H, 	         KC_J, 				KC_K,    KC_L,    KC_SCLN,    KC_QUOT, 				KC_ENT, 	KC_PGUP,  \
-		KC_LSFT, 	KC_Z,   	KC_X,     	KC_C,    KC_V,    KC_B,     KC_N,            KC_M,  			KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,   			KC_UP, 		KC_PGDN,  \
-		KC_LCTL, 	KC_LGUI,	KC_LALT, 				                KC_SPC, 	     					          		  MO(2),      MO(1), 	KC_LEFT,  	KC_DOWN,   	KC_RGHT   \
-		),
-	[1] = LAYOUT_65_ansi_blocker(
-		KC_GRV,  	KC_F1,  	KC_F2,    	KC_F3, 	 KC_F4,   KC_F5,    KC_F6,           KC_F7,      		KC_F8,   KC_F9,   KC_F10,     KC_F11,   KC_F12,     _______,    KC_MUTE,  \
-		_______, 	_______,	_______,  _______, 	 _______, _______,  _______,         _______,    		_______, _______, KC_PSCR,    KC_SLCK,  KC_PAUS,    _______,    KC_MPLY,  \
-		_______, 	_______,	_______,  _______, 	 _______, _______,  _______,         _______,    		_______, _______, _______,    _______,  			_______,    KC_MFFD,  \
-		_______, 	_______,	_______,  _______, 	 _______, _______,  _______,         _______,    		_______, _______, _______,    _______, 				KC_VOLU,    KC_MRWD,  \
-		_______, 	_______,	_______, 				 				_______,  	     									  _______,    ______,   KC_HOME,    KC_VOLD,    KC_END    \
-		),
-	[2] = LAYOUT_65_ansi_blocker(
-		_______, 	_______,	_______,  _______, 	 _______, _______,  _______,         _______,    		_______, _______, _______,	  RGB_RMOD, RGB_MOD,    _______,    KC_CALC,  \
-		RGB_TOG, 	_______,	RGB_HUI,  _______, 	 _______, _______,  _______,         _______,    		_______, _______, _______,    _______,    			_______,  	KC_MYCM,  \
-		RGB_M_P, 	RGB_SAD,	RGB_HUD,  RGB_SAI, 	 _______, _______,  _______,         _______,    		_______, _______, _______,    _______,    			_______,  	KC_WSCH,  \
-		_______, 	_______,	_______,  _______, 	 _______, RESET,    MAGIC_HOST_NKRO, MAGIC_UNHOST_NKRO, _______, _______, _______, 	  _______,  			RGB_VAI,    _______,  \
-		_______, _______, _______, 										_______, 											  _______, 	  _______,  RGB_SPD, 	RGB_VAD, 	RGB_SPI   \
-		)
-};
-// Runs just one time when the keyboard initializes.
-void matrix_init_user(void) {
+    /* QWERTY
+     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬────────────┬───────┐
+     * │       │       │       │       │       │       │       │       │       │       │       │       │       │            │       │
+     * │  ESC  │   1   │   2   │   3   │   4   │   5   │   6   │   7   │   8   │   9   │   0   │   -   │   =   │ BackSpace  │  DEL  │
+     * │       │       │       │       │       │       │       │       │       │       │       │       │       │            │       │
+     * ├───────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬─────────┼───────┤
+     * │          │       │       │       │       │       │       │       │       │       │       │       │       │         │       │
+     * │    Tab   │   Q   │   W   │   E   │   R   │   T   │   Y   │   U   │   I   │   O   │   P   │   [   │   ]   │    \    │ HOME  │
+     * │          │       │       │       │       │       │       │       │       │       │       │       │       │         │       │
+     * ├──────────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─────────┼───────┤
+     * │            │       │       │       │       │       │       │       │       │       │       │       │               │       │
+     * │  CapsLock  │   A   │   S   │   D   │   F   │   G   │   H   │   J   │   K   │   L   │   ;   │   '   │    Return     │ PgUp  │
+     * │            │       │       │       │       │       │       │       │       │       │       │       │               │       │
+     * ├────────────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴───────┬───────┼───────┤
+     * │               │       │       │       │       │       │       │       │       │       │       │            │       │       │
+     * │     Shift     │   Z   │   X   │   C   │   V   │   B   │   N   │   M   │   ,   │   .   │   /   │   Shift    │  Up   │ PgDn  │
+     * │               │       │       │       │       │       │       │       │       │       │       │            │       │       │
+     * ├─────────┬─────┴───┬───┴─────┬─┴───────┴───────┴───────┴───────┴───────┴─────┬─┴───────┼───────┴─┬──┬───────┼───────┼───────┤
+     * │         │         │         │                                               │         │         │▒▒│       │       │       │
+     * │  Ctrl   │   GUI   │   Alt   │                     Space                     │   Alt   │  Func   │▒▒│ Left  │ Down  │ Right │
+     * │         │         │         │                                               │         │         │▒▒│       │       │       │
+     * └─────────┴─────────┴─────────┴───────────────────────────────────────────────┴─────────┴─────────┴──┴───────┴───────┴───────┘
+     */
+    [_QWERTY] = LAYOUT_65_ansi_blocker(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
+    ),
+
+    /* Function layer
+     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬────────────┬───────┐
+     * │       │       │       │       │       │       │       │       │       │       │       │       │       │            │       │
+     * │  `    │  F1   │  F2   │  F3   │  F4   │  F5   │  F6   │  F7   │  F8   │  F9   │  F10  │  F11  │  F12  │    F13     │  CALC │
+     * │       │       │       │       │       │       │       │       │       │       │       │       │       │            │       │
+     * ├───────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬─────────┼───────┤
+     * │          │  RGB  │  RGB  │  RGB  │  RGB  │  RGB  │       │  USB  │  USB  │       │       │       │       │         │       │
+     * │          │ Speed │  Val  │ Speed │  Hue  │  Sat  │       │  Port │  GCR  │       │ PrtSc │ ScrLk │ Pause │         │ MYPC  │
+     * │          │   -   │   +   │   +   │   +   │   +   │       │       │       │       │       │       │       │         │       │
+     * ├──────────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─────────┼───────┤
+     * │            │  RGB  │  RGB  │  RGB  │  RGB  │  RGB  │       │       │       │       │       │       │               │       │
+     * │            │  Mode │  Val  │  Mode │  Hue  │  Sat  │       │       │       │       │       │       │               │ Next  │
+     * │            │   -   │   -   │   +   │   -   │   -   │       │       │       │       │       │       │               │       │
+     * ├────────────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴───────┬───────┼───────┤
+     * │               │  RGB  │       │       │       │       │ 6KRO/ │       │       │       │       │            │       │       │
+     * │               │ On/Off│       │       │       │Restart│ NKRO  │ Debug │       │       │       |            │ Vol+  │ PREV  │
+     * │               │       │       │       │       │       │       │       │       │       │       │            │       │       │
+     * ├─────────┬─────┴───┬───┴─────┬─┴───────┴───────┴───────┴───────┴───────┴─────┬─┴───────┼───────┴─┬──┬───────┼───────┼───────┤
+     * │         │         │         │                                               │         │         │▒▒│       │       │       │
+     * │         │         │         │                     Play                      │         │         │▒▒│ Home  │ Vol-  │  End  │
+     * │         │         │         │                                               │         │         │▒▒│       │       │       │
+     * └─────────┴─────────┴─────────┴───────────────────────────────────────────────┴─────────┴─────────┴──┴───────┴───────┴───────┘
+     */
+    [_FUNCTION] = LAYOUT_65_ansi_blocker(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_,
+        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, TG_NUMP, KC_MUTE,
+        KC_CAPS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, MD_LOCK, _______, _______,          _______, KC_VOLU,
+        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, OSL_LAY, _______,          KC_VOLU, KC_VOLD,
+        _______, _______, _______,                            HK_COSL,                            _______, _______, KC_HOME, KC_VOLD, KC_END
+    ),
+
+    /* Template
+     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬────────────┬───────┐
+     * │       │       │       │       │       │       │       │       │       │       │       │       │       │            │       │
+     * │       │       │       │       │       │       │       │       │       │       │       │       │       │            │       │
+     * │       │       │       │       │       │       │       │       │       │       │       │       │       │            │       │
+     * ├───────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬─────────┼───────┤
+     * │          │       │       │       │       │       │       │       │       │       │       │       │       │         │       │
+     * │          │       │       │       │       │       │       │       │       │       │       │       │       │         │       │
+     * │          │       │       │       │       │       │       │       │       │       │       │       │       │         │       │
+     * ├──────────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─┬─────┴─────────┼───────┤
+     * │            │       │       │       │       │       │       │       │       │       │       │       │               │       │
+     * │            │       │       │       │       │       │       │       │       │       │       │       │               │       │
+     * │            │       │       │       │       │       │       │       │       │       │       │       │               │       │
+     * ├────────────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴──┬────┴───────┬───────┼───────┤
+     * │               │       │       │       │       │       │       │       │       │       │       │            │       │       │
+     * │               │       │       │       │       │       │       │       │       │       │       │            │       │       │
+     * │               │       │       │       │       │       │       │       │       │       │       │            │       │       │
+     * ├─────────┬─────┴───┬───┴─────┬─┴───────┴───────┴───────┴───────┴───────┴─────┬─┴───────┼───────┴─┬──┬───────┼───────┼───────┤
+     * │         │         │         │                                               │         │         │▒▒│       │       │       │
+     * │         │         │         │                                               │         │         │▒▒│       │       │       │
+     * │         │         │         │                                               │         │         │▒▒│       │       │       │
+     * └─────────┴─────────┴─────────┴───────────────────────────────────────────────┴─────────┴─────────┴──┴───────┴───────┴───────┘
+    [X] = LAYOUT_65_ansi_blocker(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______
+    ),
+    */
 };
 
-// Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
-};
-
-#define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
-#define MODS_CTRL   (get_mods() & MOD_BIT(KC_LCTL)   || get_mods() & MOD_BIT(KC_RCTRL))
-#define MODS_ALT    (get_mods() & MOD_BIT(KC_LALT)   || get_mods() & MOD_BIT(KC_RALT))
+#define MODS_SHIFT  (get_mods() & MOD_MASK_SHIFT)
+#define MODS_CTRL  (get_mods() & MOD_MASK_CTRL)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
 
     switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return true;
         case U_T_AUTO:
             if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
                 TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
@@ -47,6 +144,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case U_T_AGCR:
             if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
                 TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+            }
+            return false;
+        case DBG_TOG:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+            }
+            return false;
+        case DBG_MTRX:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+            }
+            return false;
+        case DBG_KBD:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+            }
+            return false;
+        case DBG_MOU:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+            }
+            return false;
+        case MD_BOOT:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+            } else {
+                if (timer_elapsed32(key_timer) >= 500) {
+                    reset_keyboard();
+                }
             }
             return false;
         case RGB_TOG:
@@ -62,8 +188,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     rgb_matrix_set_color_all(0, 0, 0);
                   }
                   break;
+                case LED_FLAG_UNDERGLOW: {
+                    rgb_matrix_set_flags(LED_FLAG_NONE);
+                    rgb_matrix_disable_noeeprom();
+                  }
+                  break;
                 default: {
                     rgb_matrix_set_flags(LED_FLAG_ALL);
+                    rgb_matrix_enable_noeeprom();
                   }
                   break;
               }
@@ -73,3 +205,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true; //Process all other keycodes normally
     }
 }
+
