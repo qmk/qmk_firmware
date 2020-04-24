1e2b448e6bdad5a7f5a0a202a075d7566ac28d61
Aplyard <40563350+Aplyard@users.noreply.github.com>
2020-04-24 13:08:05 +0300

    latest suggestion

--- keyboards/handwired/aplx2/rules.mk
+++ keyboards/handwired/aplx2/rules.mk
@@ -30,4 +30,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
-UNICODE_ENABLE = yes        # Unicode
+UNICODE_ENABLE = no        # Unicode
