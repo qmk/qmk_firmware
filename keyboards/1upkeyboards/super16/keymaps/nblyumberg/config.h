34fed7605554ca901b0892194f3ccc1942ea79d0
Nick Blyumberg <nblyumberg@gmail.com>
2020-04-24 10:01:02 -0400

    Update keymap config.h and rules.mk files

--- keyboards/1upkeyboards/super16/keymaps/nblyumberg/config.h
+++ keyboards/1upkeyboards/super16/keymaps/nblyumberg/config.h
@@ -15,8 +15,11 @@
  */
 
 #pragma once
-#define ONESHOT_TIMEOUT 3000
+#define ONESHOT_TIMEOUT 3000 //Time out one shot layers after 3 seconds
 #define RGBLED_NUM 16
 #define RGBLIGHT_LAYERS
+#undef RGBLIGHT_LIMIT_VAL // undefine the current default brightness 
+#define RGBLIGHT_LIMIT_VAL 128 //redefine default brightness to half of 255
 #define TAPPING_TERM 200
+
 // place overrides here
