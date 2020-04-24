f82710bf456813f50af8ef8e940f69c920e0f811
marhalloweenvt <marhalloweenvt@gmail.com>
2020-04-24 09:17:09 +0700

    Adjust p1800fl

--- keyboards/handwired/p1800fl/config.h
+++ keyboards/handwired/p1800fl/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x0000
+#define PRODUCT_ID 0x3EAE
 #define DEVICE_VER 0x0001
 #define MANUFACTURER marhalloweenvt
 #define PRODUCT p1800fl
