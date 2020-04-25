5a81f67454f3020797ac281e314f5bd7d72ce2ac
Stefano <warste@gmail.com>
2020-04-25 20:24:54 +0200

    Update keyboards/40percentclub/gherkin/keymaps/stevexyz/rules.mk
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/40percentclub/gherkin/keymaps/stevexyz/rules.mk
+++ keyboards/40percentclub/gherkin/keymaps/stevexyz/rules.mk
@@ -21,5 +21,3 @@ SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 RGBLIGHT_ENABLE = no
 
-LAYOUTS = ortho_3x10
-
