0967993932a4430acb64a7529441d9b81fbc48b2
ShadowProgr <shadowprogr@gmail.com>
2020-04-25 13:14:00 +0700

    Add default DRIVER_LED_TOTAL value

--- keyboards/lily58/rev1/config.h
+++ keyboards/lily58/rev1/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFC51
-#define PRODUCT_ID      0x0058 
+#define PRODUCT_ID      0x0058
 #define DEVICE_VER      0x0100
 #define MANUFACTURER    F_YUUCHI
 #define PRODUCT         Lily58
@@ -52,7 +52,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
+
+#ifdef RGBLIGHT_ENABLE
 #define RGBLED_NUM 12    // Number of LEDs
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#define RGBLED_NUM 70    // Number of LEDs for Lily58-Glow
+#define DRIVER_LED_TOTAL RGBLED_NUM
+#endif
 
 /*
  * Feature disable options
