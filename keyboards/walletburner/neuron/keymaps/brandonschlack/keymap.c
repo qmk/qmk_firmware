400ca8b11d93a80702cca8701a8a31e4a9cd8605
Brandon Schlack <brandonschlack@gmail.com>
2020-04-25 23:39:27 -0700

    [Keymap] Fix neuron keymap

--- keyboards/walletburner/neuron/keymaps/brandonschlack/keymap.c
+++ keyboards/walletburner/neuron/keymaps/brandonschlack/keymap.c
@@ -10,14 +10,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
  * │Shift │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│?/Sf│
  * ├┬┬┬┬──┴┬──┴──┬┴───┴───┼───┴───┴──┬┴───┴┬──┴┬┬┬┬┤
- * │││││Opt│Cmd  │ SpcLwr │ SpcRse   │  Cmd│Ctl│││││
+ * │││││Opt│Cmd  │ SpcLwr │ SpcRse   │  Ctl│Cmd│││││
  * └┴┴┴┴───┴─────┴────────┴──────────┴─────┴───┴┴┴┴┘
  */
 [_BASE] = LAYOUT( \
     HY_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     CTL_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT,  \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SF_SLSH, \
-             KC_LOPT, KC_LCMD,          SPC_LWR,                   SPC_RAI,          KC_RCMD, KC_RCTL,          \
+             KC_LOPT, KC_LCMD,          SPC_LWR, XXXXXXX, SPC_RAI,                   KC_RCTL, KC_RCMD           \
 ),
 /* Lower
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F13,  KC_F14,  KC_F15,  KC_UP,   KC_PGUP, KC_DELT, _______, \
     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_MPRV, KC_MNXT, KC_LEFT, KC_DOWN, KC_RGHT,          KC_MPLY, \
     _______,          KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_VOLD, KC_VOLU, KC_HOME, KC_END,  KC_PGDN, MUT_SFT, \
-             _______, _______,          _______,                   _______,          _______, _______,          \
+             _______, _______,          _______, XXXXXXX, _______,                   _______, _______           \
 ),
 /* Raise
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_SCLN,          KC_QUOT, \
-    SF_CAPS,          _______, _______, _______, _______, _______, _______, _______, KC_GRV,  KC_SLSH, _______, \
-             _______, _______,          _______,                   _______,          _______, _______,          \
+    SF_CAPS,          _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_GRV,  _______, \
+             _______, _______,          _______, XXXXXXX, _______,                   _______, _______           \
 ),
 /* Adjust/Macro
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QM_MAKE, KC_BRMD, KC_BRMU, MC_MSSN, MC_LHPD, _______, _______, _______, _______, _______, _______, MC_SLPD, \
     NXT_WIN, _______, _______, _______, PRV_TAB, MC_BACK, MC_FWRD, NXT_TAB, _______, _______,          RESET,   \
     _______,          _______, _______, _______, PX_AFLL, OP_AFLL, _______, _______, _______, _______, _______, \
-             _______, _______,          _______,                   _______,          _______, _______,          \
+             _______, _______,          _______, XXXXXXX, _______,                   _______, _______           \
 )
 /* Blank Layer
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-             _______, _______,          _______,                   _______,          _______, _______,          \
+             _______, _______,          _______, XXXXXXX, _______,                   _______, _______           \
 )
 */
 
