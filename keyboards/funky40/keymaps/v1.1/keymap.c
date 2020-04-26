0444efc934276ec6dce62a3e90ee1096d88dcaca
TheFourthCow <63170386+TheFourthCow@users.noreply.github.com>
2020-04-26 18:18:37 -0400

    Update keyboards/funky40/keymaps/v1.1/keymap.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/funky40/keymaps/v1.1/keymap.c
+++ keyboards/funky40/keymaps/v1.1/keymap.c
@@ -18,7 +18,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	KEYMAP(
+	LAYOUT(
 		KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
 		KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_DOT, KC_DOT, KC_SLSH, KC_SFTENT, //kc_sftent is a Space Cadet mod tap code that should allow right shift to be enter when tapped right shift when held
@@ -36,4 +36,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 };
-
