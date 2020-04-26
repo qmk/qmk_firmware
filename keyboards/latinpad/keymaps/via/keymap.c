6af683278bac053891e69716833f5ed006e824b4
HASU <latincompass@163.com>
2020-04-26 16:33:26 +0800

    Update keyboards/latinpad/keymaps/via/keymap.c
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/latinpad/keymaps/via/keymap.c
+++ keyboards/latinpad/keymaps/via/keymap.c
@@ -27,18 +27,18 @@
 
 #include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_5x4(
+    [0] = LAYOUT(
+        KC_PGUP, KC_PGDOWN,
         KC_KP_7, KC_KP_8, KC_KP_9, MO(1),
         KC_P4, KC_P5, KC_P6, KC_KP_PLUS,
         KC_P1, KC_P2, KC_P3, KC_KP_MINUS,
-        KC_P0, KC_PDOT,KC_DELETE, KC_KP_ENTER,
-        KC_PGUP, KC_PGDOWN, _______, _______),
-    [1] = LAYOUT_ortho_5x4(
+        KC_P0, KC_PDOT,KC_DELETE, KC_KP_ENTER),
+    [1] = LAYOUT(
+        KC_NUMLOCK, KC_CALCULATOR,
         RGB_TOG, RGB_MOD, RGB_M_K, RGB_M_X,
         RGB_SAI, RGB_SAD, RGB_HUI, RGB_HUD,
         RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD,
-        RGB_SPI, RGB_SPD, RGB_TOG, KC_TRNS,
-        KC_NUMLOCK, KC_CALCULATOR, _______, _______),
+        RGB_SPI, RGB_SPD, RGB_TOG, KC_TRNS),
 };
 
 static void render_logo(void) {
