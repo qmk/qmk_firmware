7840e0a3914cb951847d33e48734896b62fa35e3
stevexyz <warste@removethisandleavegoogle.com>
2020-04-26 21:12:01 +0200

    removed firmware sizes

--- keyboards/40percentclub/gherkin/keymaps/stevexyz/rules.mk
+++ keyboards/40percentclub/gherkin/keymaps/stevexyz/rules.mk
@@ -1,17 +1,17 @@
 # Build Options (yes/no)
 #
-BOOTMAGIC_ENABLE = lite      # Just bootloader enabled with keys(+?)
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
-CONSOLE_ENABLE = no          # Console for debug(+400)
+BOOTMAGIC_ENABLE = lite      # Just bootloader enabled with keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+CONSOLE_ENABLE = no          # Console for debug
 COMMAND_ENABLE = no          # Commands for debug and configuration
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality on B7 by default
-MIDI_ENABLE = no             # MIDI support (+2400 to 4200, depending on config)
+MIDI_ENABLE = no             # MIDI support 
 UNICODE_ENABLE = no          # Unicode
 BLUETOOTH_ENABLE = no        # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no            # Audio output on port C6
 FAUXCLICKY_ENABLE = no       # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no          # Enable support for HD44780 based LCDs (+400)
+HD44780_ENABLE = no          # Enable support for HD44780 based LCDs
 TAP_DANCE_ENABLE = no
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend (it uses the same timer as BACKLIGHT_ENABLE)
 
