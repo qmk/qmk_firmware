0e41bcdb0715cd2eb579de440ac388d590914c98
HASU <latincompass@163.com>
2020-04-26 16:32:41 +0800

    Update keyboards/latinpad/latinpad.h
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/latinpad/latinpad.h
+++ keyboards/latinpad/latinpad.h
@@ -26,17 +26,17 @@
  * represents the switch matrix.
  */
 
-#define LAYOUT_ortho_5x4( \
+#define LAYOUT( \
+    K40, K41,           \
     K00, K01, K02, K03, \
     K10, K11, K12, K13, \
     K20, K21, K22, K23, \
-    K30, K31, K32, K33, \
-    K40, K41, K42, K43  \
+    K30, K31, K32, K33  \
 ) \
 { \
-    { K00,   K01,   K02,   K03 }, \
-    { K10,   K11,   K12,   K13 }, \
-    { K20,   K21,   K22,   K23 }, \
-    { K30,   K31,   K32,   K33 }, \
-    { K40,   K41,   K42,   K43 }  \
+    { K00,   K01,   K02,   K03   }, \
+    { K10,   K11,   K12,   K13   }, \
+    { K20,   K21,   K22,   K23   }, \
+    { K30,   K31,   K32,   K33   }, \
+    { K40,   K41,   KC_NO, KC_NO }  \
 }
