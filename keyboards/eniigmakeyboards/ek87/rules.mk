617a4ef858ff8da072f96f3d7d093d48a18e88ba
eniigmakeyboards <eniigmastudios@gmail.com>
2020-04-26 19:55:55 -0400

    Removed VIA line from rules

--- keyboards/eniigmakeyboards/ek87/rules.mk
+++ keyboards/eniigmakeyboards/ek87/rules.mk
@@ -11,6 +11,16 @@ MCU = atmega32u4
 #   ATmega328P   USBasp
 BOOTLOADER = atmel-dfu
 
+# If you don't know the bootloader type, then you can specify the
+# Boot Section Size in *bytes* by uncommenting out the OPT_DEFS line
+# Otherwise, delete this section
+#   Teensy halfKay      512
+#   Teensy++ halfKay    1024
+#   Atmel DFU loader    4096
+#   LUFA bootloader     4096
+#   USBaspLoader        2048
+# OPT_DEFS += -DBOOTLOADER_SIZE=4096
+
 # Build Options
 #   change yes to no to disable
 #
@@ -30,4 +40,3 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
-VIA_ENABLE = yes
