cbd8921ff0ec5c3e3551cde6daadce41cbe40f1e
TheFourthCow <63170386+TheFourthCow@users.noreply.github.com>
2020-04-26 18:18:44 -0400

    Update keyboards/funky40/keymaps/v1.1/keymap.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/funky40/keymaps/v1.1/keymap.c
+++ keyboards/funky40/keymaps/v1.1/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_DOT, KC_DOT, KC_SLSH, KC_SFTENT, //kc_sftent is a Space Cadet mod tap code that should allow right shift to be enter when tapped right shift when held
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, LT(1, KC_DEL), KC_SPC, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT),
 										   //^This lt keycode is supposed to make this key delete when tapped and enable a momentary layer to access the next layer down when held
-	KEYMAP(
+	LAYOUT(
 		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, 
 		KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, 
