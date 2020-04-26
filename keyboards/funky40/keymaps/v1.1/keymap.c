8da3159d4ecf65dede57e78536772e5756ba8482
TheFourthCow <63170386+TheFourthCow@users.noreply.github.com>
2020-04-26 18:18:52 -0400

    Update keyboards/funky40/keymaps/v1.1/keymap.c
    Co-Authored-By: Joel Challis <git@zvecr.com>

--- keyboards/funky40/keymaps/v1.1/keymap.c
+++ keyboards/funky40/keymaps/v1.1/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "funky40.h"
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
