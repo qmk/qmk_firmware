63589ee06def9d6fdfd2cde95e944df740bbce4f
TheFourthCow <63170386+TheFourthCow@users.noreply.github.com>
2020-04-27 10:35:34 -0400

    Update keyboards/funky40/config.h
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/funky40/config.h
+++ keyboards/funky40/config.h
@@ -19,7 +19,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define PRODUCT_ID      0xC4B5
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    TheFourthCow
 #define PRODUCT         Funky40
