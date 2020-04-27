ab1d9c8416c656ecdd85ee64667c4a2a24ad83a1
TheFourthCow <63170386+TheFourthCow@users.noreply.github.com>
2020-04-27 10:31:36 -0400

    Update keyboards/funky40/keymaps/default/keymap.c
    Co-Authored-By: Ryan <fauxpark@gmail.com>

--- keyboards/funky40/keymaps/default/keymap.c
+++ keyboards/funky40/keymaps/default/keymap.c
@@ -23,7 +23,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_DOT, KC_DOT, KC_SLSH, KC_SFTENT,
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, LT(1, KC_DEL), KC_SPC, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT),
-										   //^This lt keycode is supposed to make this key delete when tapped and enable a momentary layer to access the next layer down when held
 	LAYOUT(
 		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, 
 		KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_UNDS, KC_PLUS, 
