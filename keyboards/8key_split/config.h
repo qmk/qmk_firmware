3b218c82525c25a358d31fb64afec1910cbb2bd1
mizuhof <moc.liam0000@gmail.com>
2020-04-27 14:48:22 +0900

    [update]
    update keyboards/8key_split;

--- keyboards/8key_split/config.h
+++ keyboards/8key_split/config.h
@@ -48,11 +48,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-#define USE_SERIAL
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
+#define SOFT_SERIAL_PIN D2
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
