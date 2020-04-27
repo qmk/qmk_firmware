bf665cce232a9710de7cdb14de8d054138ad4456
4pplet <4pplet@protonmail.com>
2020-04-27 21:20:50 +0200

    Update keyboards/4pplet/aekiso60_rev_a/config.h
    Co-Authored-By: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/4pplet/aekiso60_rev_a/config.h
+++ keyboards/4pplet/aekiso60_rev_a/config.h
@@ -14,7 +14,7 @@
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 14
 // ROWS: Top to bottom, COLS: Left to right
-//						  0  1  2  3  4  5  6  7  8  9  a  b  c  d
+//                        0  1  2  3  4  5  6  7  8  9  a  b  c  d
 #define MATRIX_ROW_PINS {C2,D0,B0,C7,C5}
 #define MATRIX_COL_PINS {C4,C6,B7,B6,B5,B4,B3,B2,B1,D6,D5,D4,D2,D1}
 
