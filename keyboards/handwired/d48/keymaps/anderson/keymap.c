d2977e6b15604cb16bbd17b0eb595a8ad841ad2c
Andrew Dunai <a@dun.ai>
2020-04-28 15:26:19 +0300

    Changes as per PR.

--- keyboards/handwired/d48/keymaps/anderson/keymap.c
+++ keyboards/handwired/d48/keymaps/anderson/keymap.c
@@ -1,7 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "taphold.h"
 #include "seq.h"
-#include "layout_defs.h"
 #include "ds1307.h"
 #include "lightmode.h"
 #include <stdio.h>
@@ -65,12 +64,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        ┗━━━━━┻━━━━━┷━━━━━┷━━━━━┷━━━━━┷━━━━━┻━━━━━┷━━━━━┷━━━━━┷━━━━━┷━━━━━┻━━━━━┛
        */
     [_MAIN] = LAYOUT( \
-                                                                                KC_MUTE,
-#ifdef LIGHT_MODE
-                                                                                                           KC_LIGHT_MODE,
-#else
-                                                                                                           _______,
-#endif
+                                                                                KC_MUTE,                   LCTL(KC_D),
+// #ifdef LIGHT_MODE
+//                                                                                                            KC_LIGHT_MODE,
+// #else
+//                                                                                                            _______,
+// #endif
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
         KC_ALPHA,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTRL,\
--- users/anderson/layout_defs.h
+++
@@ -1,4 +0,0 @@
-#define KC______ KC_TRNS
-#define KC_DEBUG DEBUG
-#define KC_RESET RESET
-#define KC_RGB_TOG RGB_TOG
