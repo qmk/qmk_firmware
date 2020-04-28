86c71fd01fed00c2747f5d2fb82e0f324c894002
pabile <1162412+pabile@users.noreply.github.com>
2020-04-28 18:17:24 +0800

    Update keyboards/pabile/p18/p18.h
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/pabile/p18/p18.h
+++ keyboards/pabile/p18/p18.h
@@ -3,8 +3,8 @@
 #include "quantum.h"
 
 #define LAYOUT( \
-         k01, k02, k03, k04\
-         k11, k12, k13, k14\
+         k01, k02, k03, k04, \
+         k11, k12, k13, k14, \
     k20, k21, k22, k23, k24, \
     k30, k31, k32, k33, k34 \
 ) \
