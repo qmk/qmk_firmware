020c97370af4487b611bfda43a5be4c06740bde8
Germ <jeremythegeek@gmail.com>
2020-04-29 01:15:50 -0600

    vendor + cformat

--- keyboards/gboards/k/gergo/matrix.c
+++ keyboards/gboards/k/gergo/matrix.c
@@ -29,76 +29,75 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #ifdef BALLER
-#include <avr/interrupt.h>
-#include "pointing_device.h"
+#    include <avr/interrupt.h>
+#    include "pointing_device.h"
 #endif
 
 #ifndef DEBOUNCE
-#   define DEBOUNCE	5
+#    define DEBOUNCE 5
 #endif
 
 // MCP Pin Defs
-#define RROW1 (1<<3)
-#define RROW2 (1<<2)
-#define RROW3 (1<<1)
-#define RROW4 (1<<0)
-#define COL0 (1<<0)
-#define COL1 (1<<1)
-#define COL2 (1<<2)
-#define COL3 (1<<3)
-#define COL4 (1<<4)
-#define COL5 (1<<5)
-#define COL6 (1<<6)
+#define RROW1 (1 << 3)
+#define RROW2 (1 << 2)
+#define RROW3 (1 << 1)
+#define RROW4 (1 << 0)
+#define COL0 (1 << 0)
+#define COL1 (1 << 1)
+#define COL2 (1 << 2)
+#define COL3 (1 << 3)
+#define COL4 (1 << 4)
+#define COL5 (1 << 5)
+#define COL6 (1 << 6)
 
 // ATmega pin defs
-#define ROW1  (1<<6)
-#define ROW2  (1<<5)
-#define ROW3  (1<<4)
-#define ROW4  (1<<1)
-#define COL7 (1<<0)
-#define COL8 (1<<1)
-#define COL9 (1<<2)
-#define COL10 (1<<3)
-#define COL11 (1<<2)
-#define COL12 (1<<3)
-#define COL13 (1<<6)
+#define ROW1 (1 << 6)
+#define ROW2 (1 << 5)
+#define ROW3 (1 << 4)
+#define ROW4 (1 << 1)
+#define COL7 (1 << 0)
+#define COL8 (1 << 1)
+#define COL9 (1 << 2)
+#define COL10 (1 << 3)
+#define COL11 (1 << 2)
+#define COL12 (1 << 3)
+#define COL13 (1 << 6)
 
-//Trackball pin defs
-#define TRKUP (1<<4)
-#define TRKDN (1<<5)
-#define TRKLT (1<<6)
-#define TRKRT (1<<7)
-#define TRKBTN (1<<6)
-
+// Trackball pin defs
+#define TRKUP (1 << 4)
+#define TRKDN (1 << 5)
+#define TRKLT (1 << 6)
+#define TRKRT (1 << 7)
+#define TRKBTN (1 << 6)
 
 // Multiple for mouse moves
 #ifndef TRKSTEP
-#define TRKSTEP 20
+#    define TRKSTEP 20
 #endif
 
 // multiple for mouse scroll
 #ifndef SCROLLSTEP
-#define SCROLLSTEP 5
+#    define SCROLLSTEP 5
 #endif
 
 // bit masks
-#define BMASK     (COL7 | COL8 | COL9 | COL10)
-#define CMASK     (COL13)
-#define DMASK     (COL11 | COL12)
-#define FMASK     (ROW1 | ROW2 | ROW3 | ROW4)
-#define RROWMASK  (RROW1 | RROW2 | RROW3 | RROW4)
-#define MCPMASK   (COL0 | COL1 | COL2 | COL3 | COL4 | COL5 | COL6)
-#define TRKMASK   (TRKUP | TRKDN | TRKRT | TRKLT)
+#define BMASK (COL7 | COL8 | COL9 | COL10)
+#define CMASK (COL13)
+#define DMASK (COL11 | COL12)
+#define FMASK (ROW1 | ROW2 | ROW3 | ROW4)
+#define RROWMASK (RROW1 | RROW2 | RROW3 | RROW4)
+#define MCPMASK (COL0 | COL1 | COL2 | COL3 | COL4 | COL5 | COL6)
+#define TRKMASK (TRKUP | TRKDN | TRKRT | TRKLT)
 
 // Trackball interrupts accumulate over here. Processed on scan
 // Stores prev state of mouse, high bits store direction
 uint8_t trkState    = 0;
 uint8_t trkBtnState = 0;
 
-volatile uint8_t tbUpCnt  = 0;
-volatile uint8_t tbDnCnt  = 0;
-volatile uint8_t tbLtCnt  = 0;
-volatile uint8_t tbRtCnt  = 0;
+volatile uint8_t tbUpCnt = 0;
+volatile uint8_t tbDnCnt = 0;
+volatile uint8_t tbLtCnt = 0;
+volatile uint8_t tbRtCnt = 0;
 
 /* matrix state(1:on, 0:off) */
 static matrix_row_t matrix[MATRIX_ROWS];
@@ -116,29 +115,22 @@ static matrix_row_t read_cols(uint8_t row);
 static void         init_cols(void);
 static void         unselect_rows(void);
 static void         select_row(uint8_t row);
-static void enableInterrupts(void);
+static void         enableInterrupts(void);
 
 static uint8_t mcp23018_reset_loop;
 // static uint16_t mcp23018_reset_loop;
 
-__attribute__ ((weak)) void matrix_init_user(void) {}
+__attribute__((weak)) void matrix_init_user(void) {}
 
-__attribute__ ((weak)) void matrix_scan_user(void) {}
+__attribute__((weak)) void matrix_scan_user(void) {}
 
-__attribute__ ((weak))
-void matrix_init_kb(void) {
-  matrix_init_user();
-}
+__attribute__((weak)) void matrix_init_kb(void) { matrix_init_user(); }
 
-__attribute__ ((weak))
-void matrix_scan_kb(void) {
-  matrix_scan_user();
-}
+__attribute__((weak)) void matrix_scan_kb(void) { matrix_scan_user(); }
 
 inline uint8_t matrix_rows(void) { return MATRIX_ROWS; }
 
 inline uint8_t matrix_cols(void) { return MATRIX_COLS; }
-
 
 void matrix_init(void) {
     // initialize row and col
@@ -146,11 +138,11 @@ void matrix_init(void) {
     unselect_rows();
     init_cols();
 
-  // initialize matrix state: all keys off
-  for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-    matrix[i]     = 0;
-    raw_matrix[i] = 0;
-  }
+    // initialize matrix state: all keys off
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        matrix[i]     = 0;
+        raw_matrix[i] = 0;
+    }
 
     debounce_init(MATRIX_ROWS);
     matrix_init_quantum();
@@ -163,7 +155,7 @@ void matrix_power_up(void) {
     init_cols();
 
     // initialize matrix state: all keys off
-    for (uint8_t i=0; i < MATRIX_ROWS; i++) {
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
         matrix[i] = 0;
     }
 }
@@ -171,15 +163,13 @@ void matrix_power_up(void) {
 // Reads and stores a row, returning
 // whether a change occurred.
 static inline bool store_raw_matrix_row(uint8_t index) {
-  matrix_row_t temp = read_cols(index);
-  if (raw_matrix[index] != temp) {
-    raw_matrix[index] = temp;
-    return true;
-  }
-  return false;
+    matrix_row_t temp = read_cols(index);
+    if (raw_matrix[index] != temp) {
+        raw_matrix[index] = temp;
+        return true;
+    }
+    return false;
 }
-
-
 
 uint8_t matrix_scan(void) {
     // TODO: Find what is trashing interrupts
@@ -187,41 +177,46 @@ uint8_t matrix_scan(void) {
 
     // First we handle the mouse inputs
 #ifdef BALLER
-    uint8_t pBtn   = PINE & TRKBTN;
+    uint8_t pBtn = PINE & TRKBTN;
 
-    #ifdef DEBUG_BALLER
+#    ifdef DEBUG_BALLER
     // Compare to previous, mod report
-    if (tbUpCnt + tbDnCnt + tbLtCnt + tbRtCnt != 0)
-        xprintf("U: %d D: %d L: %d R: %d B: %d\n", tbUpCnt, tbDnCnt, tbLtCnt, tbRtCnt, (trkBtnState >> 6));
-    #endif
+    if (tbUpCnt + tbDnCnt + tbLtCnt + tbRtCnt != 0) xprintf("U: %d D: %d L: %d R: %d B: %d\n", tbUpCnt, tbDnCnt, tbLtCnt, tbRtCnt, (trkBtnState >> 6));
+#    endif
 
     // Modify the report
     report_mouse_t pRprt = pointing_device_get_report();
 
     // Scroll by default, move on layer
     if (layer_state == 0) {
-		  pRprt.h += tbLtCnt * SCROLLSTEP; tbLtCnt = 0;
-		  pRprt.h -= tbRtCnt * SCROLLSTEP; tbRtCnt = 0;
-		  pRprt.v -= tbUpCnt * SCROLLSTEP; tbUpCnt = 0;
-		  pRprt.v += tbDnCnt * SCROLLSTEP; tbDnCnt = 0;
+        pRprt.h += tbLtCnt * SCROLLSTEP;
+        tbLtCnt = 0;
+        pRprt.h -= tbRtCnt * SCROLLSTEP;
+        tbRtCnt = 0;
+        pRprt.v -= tbUpCnt * SCROLLSTEP;
+        tbUpCnt = 0;
+        pRprt.v += tbDnCnt * SCROLLSTEP;
+        tbDnCnt = 0;
     } else {
-		  pRprt.x -= tbLtCnt * TRKSTEP * (layer_state - 1); tbLtCnt = 0;
-		  pRprt.x += tbRtCnt * TRKSTEP * (layer_state - 1); tbRtCnt = 0;
-		  pRprt.y -= tbUpCnt * TRKSTEP * (layer_state - 1); tbUpCnt = 0;
-		  pRprt.y += tbDnCnt * TRKSTEP * (layer_state - 1); tbDnCnt = 0;
+        pRprt.x -= tbLtCnt * TRKSTEP * (layer_state - 1);
+        tbLtCnt = 0;
+        pRprt.x += tbRtCnt * TRKSTEP * (layer_state - 1);
+        tbRtCnt = 0;
+        pRprt.y -= tbUpCnt * TRKSTEP * (layer_state - 1);
+        tbUpCnt = 0;
+        pRprt.y += tbDnCnt * TRKSTEP * (layer_state - 1);
+        tbDnCnt = 0;
     }
 
-#ifdef DEBUG_BALLER
-    if (pRprt.x != 0 || pRprt.y != 0)
-        xprintf("X: %d Y: %d\n", pRprt.x, pRprt.y);
-#endif
+#    ifdef DEBUG_BALLER
+    if (pRprt.x != 0 || pRprt.y != 0) xprintf("X: %d Y: %d\n", pRprt.x, pRprt.y);
+#    endif
 
-    if ((pBtn != trkBtnState) && ((pBtn >> 6) == 0))  pRprt.buttons |= MOUSE_BTN1;
-    if ((pBtn != trkBtnState) && ((pBtn >> 6) == 1))  pRprt.buttons &= ~MOUSE_BTN1;
+    if ((pBtn != trkBtnState) && ((pBtn >> 6) == 0)) pRprt.buttons |= MOUSE_BTN1;
+    if ((pBtn != trkBtnState) && ((pBtn >> 6) == 1)) pRprt.buttons &= ~MOUSE_BTN1;
 
     // Save state, push update
-    if (pRprt.x != 0 || pRprt.y != 0 || pRprt.h != 0 || pRprt.v != 0 || (trkBtnState != pBtn))
-        pointing_device_set_report(pRprt);
+    if (pRprt.x != 0 || pRprt.y != 0 || pRprt.h != 0 || pRprt.v != 0 || (trkBtnState != pBtn)) pointing_device_set_report(pRprt);
 
     trkBtnState = pBtn;
 #endif
@@ -245,7 +240,7 @@ uint8_t matrix_scan(void) {
     bool changed = false;
     for (uint8_t i = 0; i < MATRIX_ROWS_PER_SIDE; i++) {
         // select rows from left and right hands
-        uint8_t left_index = i;
+        uint8_t left_index  = i;
         uint8_t right_index = i + MATRIX_ROWS_PER_SIDE;
         select_row(left_index);
         select_row(right_index);
@@ -266,14 +261,14 @@ uint8_t matrix_scan(void) {
 
 #ifdef DEBUG_MATRIX
     for (uint8_t c = 0; c < MATRIX_COLS; c++)
-		for (uint8_t r = 0; r < MATRIX_ROWS; r++)
-		  if (matrix_is_on(r, c)) xprintf("r:%d c:%d \n", r, c);
+        for (uint8_t r = 0; r < MATRIX_ROWS; r++)
+            if (matrix_is_on(r, c)) xprintf("r:%d c:%d \n", r, c);
 #endif
 
     return 1;
 }
 
-bool matrix_is_modified(void) // deprecated and evidently not called.
+bool matrix_is_modified(void)  // deprecated and evidently not called.
 {
     return true;
 }
@@ -285,7 +280,8 @@ inline matrix_row_t matrix_get_row(uint8_t row) { return matrix[row]; }
 void matrix_print(void) {
     print("\nr/c 0123456789ABCDEF\n");
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        phex(row); print(": ");
+        phex(row);
+        print(": ");
         pbin_reverse16(matrix_get_row(row));
         print("\n");
     }
@@ -300,26 +296,30 @@ uint8_t matrix_key_count(void) {
 }
 
 // Remember this means ROWS
-static void  init_cols(void) {
+static void init_cols(void) {
     // init on mcp23018
     // not needed, already done as part of init_mcp23018()
 
     // Input with pull-up(DDR:0, PORT:1)
-    DDRF  &= ~FMASK;
-    PORTF |=  FMASK;
+    DDRF &= ~FMASK;
+    PORTF |= FMASK;
 }
 
 static matrix_row_t read_cols(uint8_t row) {
     if (row < 7) {
-        if (mcp23018_status) { // if there was an error
+        if (mcp23018_status) {  // if there was an error
             return 0;
         } else {
-            uint8_t data = 0;
-            mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);    if (mcp23018_status) goto out;
-            mcp23018_status = i2c_write(GPIOB, I2C_TIMEOUT);             if (mcp23018_status) goto out;
-            mcp23018_status = i2c_start(I2C_ADDR_READ, I2C_TIMEOUT);     if (mcp23018_status) goto out;
-            mcp23018_status = i2c_read_nack(I2C_TIMEOUT);                if (mcp23018_status < 0) goto out;
-            data = ~((uint8_t)mcp23018_status);
+            uint8_t data    = 0;
+            mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);
+            if (mcp23018_status) goto out;
+            mcp23018_status = i2c_write(GPIOB, I2C_TIMEOUT);
+            if (mcp23018_status) goto out;
+            mcp23018_status = i2c_start(I2C_ADDR_READ, I2C_TIMEOUT);
+            if (mcp23018_status) goto out;
+            mcp23018_status = i2c_read_nack(I2C_TIMEOUT);
+            if (mcp23018_status < 0) goto out;
+            data            = ~((uint8_t)mcp23018_status);
             mcp23018_status = I2C_STATUS_SUCCESS;
         out:
             i2c_stop();
@@ -330,47 +330,45 @@ static matrix_row_t read_cols(uint8_t row) {
             return data;
         }
     } else {
-         /* read from teensy
-	        * bitmask is 0b0111001, but we want the lower four
-	        * we'll return 1s for the top two, but that's harmless.
-	        */
+        /* read from teensy
+         * bitmask is 0b0111001, but we want the lower four
+         * we'll return 1s for the top two, but that's harmless.
+         */
         // So I need to confuckulate all this
-        //return ~(((PIND & DMASK) >> 1  | ((PINC & CMASK) >> 6) | (PIN)));
-        //return ~((PINF & 0x03) | ((PINF & 0xF0) >> 2));
-        return ~(
-           (((PINF & ROW4) >> 1)
-          | ((PINF & (ROW1 | ROW2 | ROW3)) >> 3))
-        & 0xF);
+        // return ~(((PIND & DMASK) >> 1  | ((PINC & CMASK) >> 6) | (PIN)));
+        // return ~((PINF & 0x03) | ((PINF & 0xF0) >> 2));
+        return ~((((PINF & ROW4) >> 1) | ((PINF & (ROW1 | ROW2 | ROW3)) >> 3)) & 0xF);
     }
 }
 
 // Row pin configuration
-static void unselect_rows(void)
-{
+static void unselect_rows(void) {
     // no need to unselect on mcp23018, because the select step sets all
     // the other row bits high, and it's not changing to a different
     // direction
     // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRB  &= ~(BMASK | TRKMASK);
+    DDRB &= ~(BMASK | TRKMASK);
     PORTB &= ~(BMASK);
-    DDRC  &= ~CMASK;
+    DDRC &= ~CMASK;
     PORTC &= ~CMASK;
-    DDRD  &= ~DMASK;
+    DDRD &= ~DMASK;
     PORTD &= ~DMASK;
 
-	// Fix trashing of DDRB for TB
+    // Fix trashing of DDRB for TB
     PORTB |= TRKMASK;
 }
 
-static void select_row(uint8_t row)
-{
+static void select_row(uint8_t row) {
     if (row < 7) {
         // select on mcp23018
-        if (mcp23018_status) { // do nothing on error
-        } else { // set active row low  : 0 // set other rows hi-Z : 1
-            mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);        if (mcp23018_status) goto out;
-            mcp23018_status = i2c_write(GPIOA, I2C_TIMEOUT);                 if (mcp23018_status) goto out;
-            mcp23018_status = i2c_write(0xFF & ~(1<<row), I2C_TIMEOUT);      if (mcp23018_status) goto out;
+        if (mcp23018_status) {  // do nothing on error
+        } else {                // set active row low  : 0 // set other rows hi-Z : 1
+            mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);
+            if (mcp23018_status) goto out;
+            mcp23018_status = i2c_write(GPIOA, I2C_TIMEOUT);
+            if (mcp23018_status) goto out;
+            mcp23018_status = i2c_write(0xFF & ~(1 << row), I2C_TIMEOUT);
+            if (mcp23018_status) goto out;
         out:
             i2c_stop();
         }
@@ -378,68 +376,66 @@ static void select_row(uint8_t row)
         // Output low(DDR:1, PORT:0) to select
         switch (row) {
             case 7:
-                DDRB  |= COL7;
+                DDRB |= COL7;
                 PORTB &= ~COL7;
                 break;
             case 8:
-                DDRB  |= COL8;
+                DDRB |= COL8;
                 PORTB &= ~COL8;
                 break;
             case 9:
-                DDRB  |= COL9;
+                DDRB |= COL9;
                 PORTB &= ~COL9;
                 break;
             case 10:
-                DDRB  |= COL10;
+                DDRB |= COL10;
                 PORTB &= ~COL10;
                 break;
             case 11:
-                DDRD  |= COL11;
+                DDRD |= COL11;
                 PORTD &= ~COL11;
                 break;
             case 12:
-                DDRD  |= COL12;
+                DDRD |= COL12;
                 PORTD &= ~COL12;
                 break;
             case 13:
-                DDRC  |= COL13;
+                DDRC |= COL13;
                 PORTC &= ~COL13;
                 break;
         }
     }
 }
 
-
 // Trackball Interrupts
 static void enableInterrupts(void) {
-    #ifdef BALLER
-      // Set interrupt mask
-      // Set port defs
-      DDRB &= ~TRKMASK;
-      PORTB |= TRKMASK;
-      DDRE &= ~TRKBTN;
-      PORTE |= TRKBTN;
+#ifdef BALLER
+    // Set interrupt mask
+    // Set port defs
+    DDRB &= ~TRKMASK;
+    PORTB |= TRKMASK;
+    DDRE &= ~TRKBTN;
+    PORTE |= TRKBTN;
 
-      // Interrupt shenanigans
-      //EIMSK |= (1 << PCIE0);
-      PCMSK0 |= TRKMASK;
-      PCICR |= (1 << PCIE0);
-      sei();
-    #endif
+    // Interrupt shenanigans
+    // EIMSK |= (1 << PCIE0);
+    PCMSK0 |= TRKMASK;
+    PCICR |= (1 << PCIE0);
+    sei();
+#endif
 
     return;
 }
 #ifdef BALLER
-ISR (PCINT0_vect) {
-  // Don't get fancy, we're in a interrupt here
-  // PCINT reports a interrupt for a change on the bus
-  // We hand the button at scantime for debounce
-  volatile uint8_t pState = PINB & TRKMASK;
-  if ((pState & TRKUP) != (trkState & TRKUP)) tbUpCnt++;
-  if ((pState & TRKDN) != (trkState & TRKDN)) tbDnCnt++;
-  if ((pState & TRKLT) != (trkState & TRKLT)) tbLtCnt++;
-  if ((pState & TRKRT) != (trkState & TRKRT)) tbRtCnt++;
-  trkState = pState;
-
+ISR(PCINT0_vect) {
+    // Don't get fancy, we're in a interrupt here
+    // PCINT reports a interrupt for a change on the bus
+    // We hand the button at scantime for debounce
+    volatile uint8_t pState = PINB & TRKMASK;
+    if ((pState & TRKUP) != (trkState & TRKUP)) tbUpCnt++;
+    if ((pState & TRKDN) != (trkState & TRKDN)) tbDnCnt++;
+    if ((pState & TRKLT) != (trkState & TRKLT)) tbLtCnt++;
+    if ((pState & TRKRT) != (trkState & TRKRT)) tbRtCnt++;
+    trkState = pState;
 }
 #endif
