bc04ded4e42e6f7e7f96541c9b1630ff5cf78b08
Nick Blyumberg <nblyumberg@gmail.com>
2020-04-29 14:20:53 -0400

    Update keyboards/1upkeyboards/super16/keymaps/nblyumberg/config.h
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/1upkeyboards/super16/keymaps/nblyumberg/config.h
+++ keyboards/1upkeyboards/super16/keymaps/nblyumberg/config.h
@@ -1,8 +1,13 @@
 #pragma once
-#define ONESHOT_TIMEOUT 3000 //Time out one shot layers after 3 seconds
+
+// Time out one shot layers after 3 seconds
+#define ONESHOT_TIMEOUT 3000
+
 #define RGBLED_NUM 16
 #define RGBLIGHT_LAYERS
-#undef RGBLIGHT_LIMIT_VAL // undefine the current default brightness 
-#define RGBLIGHT_LIMIT_VAL 128 //redefine default brightness to half of 255
-#define TAPPING_TERM 200
 
+// Undef and redefine default brightness to half of 255
+#undef RGBLIGHT_LIMIT_VAL
+#define RGBLIGHT_LIMIT_VAL 128
+
+#define TAPPING_TERM 200
