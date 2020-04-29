4362f3a729f67de9f673b9b1b0dc349852a2a190
Garret G <45295190+TheRoyalSweatshirt@users.noreply.github.com>
2020-04-29 11:57:55 -0500

    Update keyboards/the_royal/liminal/keymaps/via/keymap.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/the_royal/liminal/keymaps/via/keymap.c
+++ keyboards/the_royal/liminal/keymaps/via/keymap.c
@@ -34,6 +34,10 @@ LAYOUT_base_kit_all(
 
 
 
+void keyboard_post_init_user(void) {
+    layer_state_set_user(layer_state);
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
     case 1:
