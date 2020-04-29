3cd63f41953c30030f3c4c6697a22748d8fe17ab
Germ <jeremythegeek@gmail.com>
2020-04-29 01:16:33 -0600

    vendor + cformat

--- keyboards/gboards/k/gergoplex/gergoplex.c
+++ keyboards/gboards/k/gergoplex/gergoplex.c
@@ -1,29 +1,29 @@
 #include QMK_KEYBOARD_H
 
-bool i2c_initialized = 0;
+bool         i2c_initialized = 0;
 i2c_status_t mcp23018_status = 0x20;
 
 void matrix_init_kb(void) {
     // (tied to Vcc for hardware convenience)
-    //DDRB  &= ~(1<<4);  // set B(4) as input
-    //PORTB &= ~(1<<4);  // set B(4) internal pull-up disabled
+    // DDRB  &= ~(1<<4);  // set B(4) as input
+    // PORTB &= ~(1<<4);  // set B(4) internal pull-up disabled
 
     // unused pins - C7, D4, D5, D7, E6
     // set as input with internal pull-up enabled
-    DDRB  &= ~(1<<0 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
-    PORTB |=  (1<<0 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
+    DDRB &= ~(1 << 0 | 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7);
+    PORTB |= (1 << 0 | 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7);
 
-    DDRC  &= ~(1<<6);
-    PORTC |=  (1<<6);
+    DDRC &= ~(1 << 6);
+    PORTC |= (1 << 6);
 
-    PORTD |=  (1<<5 | 1<<4 | 1<<6 | 1<<7);
-    DDRD  &= ~(1<<5 | 1<<4 | 1<<6 | 1<<7);
+    PORTD |= (1 << 5 | 1 << 4 | 1 << 6 | 1 << 7);
+    DDRD &= ~(1 << 5 | 1 << 4 | 1 << 6 | 1 << 7);
 
-    DDRE  &= ~(1<<6);
-    PORTE |=  (1<<6);
+    DDRE &= ~(1 << 6);
+    PORTE |= (1 << 6);
 
-    DDRF  &= ~(1<<0 | 1<<7);
-    PORTF |=  (1<<0 | 1<<7);
+    DDRF &= ~(1 << 0 | 1 << 7);
+    PORTF |= (1 << 0 | 1 << 7);
 
     matrix_init_user();
 }
@@ -49,24 +49,32 @@ uint8_t init_mcp23018(void) {
     // - unused  : input  : 1
     // - input   : input  : 1
     // - driving : output : 0
-    mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);    if (mcp23018_status) goto out;
-    mcp23018_status = i2c_write(IODIRA, I2C_TIMEOUT);            if (mcp23018_status) goto out;
-    mcp23018_status = i2c_write(0b11000001, I2C_TIMEOUT);        if (mcp23018_status) goto out;
-    mcp23018_status = i2c_write(0b11111111, I2C_TIMEOUT);        if (mcp23018_status) goto out;
+    mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
+    mcp23018_status = i2c_write(IODIRA, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
+    mcp23018_status = i2c_write(0b11000001, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
+    mcp23018_status = i2c_write(0b11111111, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
     i2c_stop();
 
     // set pull-up
     // - unused  : on  : 1
     // - input   : on  : 1
     // - driving : off : 0
-    mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);    if (mcp23018_status) goto out;
-    mcp23018_status = i2c_write(GPPUA, I2C_TIMEOUT);             if (mcp23018_status) goto out;
-    mcp23018_status = i2c_write(0b11000001, I2C_TIMEOUT);        if (mcp23018_status) goto out;
-    mcp23018_status = i2c_write(0b11111111, I2C_TIMEOUT);        if (mcp23018_status) goto out;
+    mcp23018_status = i2c_start(I2C_ADDR_WRITE, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
+    mcp23018_status = i2c_write(GPPUA, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
+    mcp23018_status = i2c_write(0b11000001, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
+    mcp23018_status = i2c_write(0b11111111, I2C_TIMEOUT);
+    if (mcp23018_status) goto out;
 
 out:
     i2c_stop();
     // SREG=sreg_prev;
-    //uprintf("Init %x\n", mcp23018_status);
+    // uprintf("Init %x\n", mcp23018_status);
     return mcp23018_status;
 }
