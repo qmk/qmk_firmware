19612150566aa4689f7f0a84899cd7fcded61ed5
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2020-04-29 10:47:08 -0400

    Update keyboards/themadnoodle/noodlepad/keymaps/default/keymap.c
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/themadnoodle/noodlepad/keymaps/default/keymap.c
+++ keyboards/themadnoodle/noodlepad/keymaps/default/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------'
      */
     
-    [2] = LAYOUT(
+    [2] = LAYOUT_ortho_3x3(
       KC_TRNS, KC_TRNS, KC_TRNS, 
       KC_TRNS, KC_TRNS, KC_TRNS, 
       KC_TRNS, KC_TRNS, KC_TRNS
