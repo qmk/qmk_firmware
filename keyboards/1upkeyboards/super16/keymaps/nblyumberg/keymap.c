b4d88f3ee90ade73f49cd4a3755f2fee6f772512
Nick Blyumberg <nblyumberg@gmail.com>
2020-04-29 14:20:17 -0400

    Update keyboards/1upkeyboards/super16/keymaps/nblyumberg/keymap.c
    Co-Authored-By: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/1upkeyboards/super16/keymaps/nblyumberg/keymap.c
+++ keyboards/1upkeyboards/super16/keymaps/nblyumberg/keymap.c
@@ -13,45 +13,37 @@ enum my_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//Layer 0 - Base Layer (F13 to F24, and One Shot Layer 1,2,3,4)
+        //Layer 0 - Base Layer (F13 to F24, and One Shot Layer 1,2,3,4)
+        [0] = LAYOUT_ortho_4x4(
+                LALT(KC_F13), LALT(KC_F14), LALT(KC_F15), LALT(KC_F16),
+                LALT(KC_F17), LALT(KC_F18), LALT(KC_F19), LALT(KC_F20),
+                LALT(KC_F21), LALT(KC_F22), LALT(KC_F23), LALT(KC_F24),
+                KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS         //Transparent to let you go between layers
+        ),
 
-	[0] = LAYOUT_ortho_4x4(
-		KC_F13, KC_F14, KC_F15, KC_F16, 
-		KC_F17, KC_CCCV, KC_F19, KC_F20, //Changing KC_F18 to KC_CCCV 
-		KC_F21, KC_F22, KC_F23, KC_2ENTER, //Changing KC_F24 to KC_2ENTER
-		OSL(1), OSL(2), OSL(3), TG(4)  //One Shot Layer Activation
-		),
-//Layer 1 - Alt + Function Key Layer
-	[1] = LAYOUT_ortho_4x4(
-		LALT(KC_F13), LALT(KC_F14), LALT(KC_F15), LALT(KC_F16), 
-		LALT(KC_F17), LALT(KC_F18), LALT(KC_F19), LALT(KC_F20), 
-		LALT(KC_F21), LALT(KC_F22), LALT(KC_F23), LALT(KC_F24), 
-	        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS         //Transparent to let you go between layers
-		),
+        //Layer 2 - Shift + Function Key Layer
+        [2] = LAYOUT_ortho_4x4(
+                LSFT(KC_F13), LSFT(KC_F14), LSFT(KC_F15), LSFT(KC_F16),
+                LSFT(KC_F17), LSFT(KC_F18), LSFT(KC_F19), LSFT(KC_F20),
+                LSFT(KC_F21), LSFT(KC_F22), LSFT(KC_F23), LSFT(KC_F24),
+                KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS         //Transparent to let you go between layers
+        ),
 
-//Layer 2 - Shift + Function Key Layer
-	[2] = LAYOUT_ortho_4x4(
-		LSFT(KC_F13), LSFT(KC_F14), LSFT(KC_F15), LSFT(KC_F16), 
-		LSFT(KC_F17), LSFT(KC_F18), LSFT(KC_F19), LSFT(KC_F20), 
-		LSFT(KC_F21), LSFT(KC_F22), LSFT(KC_F23), LSFT(KC_F24), 
-	        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS         //Transparent to let you go between layers
-		),
+        //Layer 3 - Control + Function Key
+        [3] = LAYOUT_ortho_4x4(
+                LCTL(KC_F13), LCTL(KC_F14), LCTL(KC_F15), LCTL(KC_F16),
+                LCTL(KC_F17), LCTL(KC_F18), LCTL(KC_F19), LCTL(KC_F20),
+                LCTL(KC_F21), LCTL(KC_F22), LCTL(KC_F23), LCTL(KC_F24),
+                KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS         //Transparent to let you go between layers
+        ),
 
-//Layer 3 - Control + Function Key 
-	[3] = LAYOUT_ortho_4x4(
-		LCTL(KC_F13), LCTL(KC_F14), LCTL(KC_F15), LCTL(KC_F16), 
-		LCTL(KC_F17), LCTL(KC_F18), LCTL(KC_F19), LCTL(KC_F20), 
-		LCTL(KC_F21), LCTL(KC_F22), LCTL(KC_F23), LCTL(KC_F24), 
-	        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS         //Transparent to let you go between layers
-		),
-
-//Layer 4 - Keyboard Lights. Programming and Special Functions
-	[4] = LAYOUT_ortho_4x4(
-		KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, 
-		RGB_TOG, RGB_MOD, RGB_RMOD, KC_MUTE, 
-		TO(0), RESET, KC_2ENTER, KC_VOLD, 
-	        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS         //Transparent to let you go between layers
-		),
+        //Layer 4 - Keyboard Lights. Programming and Special Functions
+        [4] = LAYOUT_ortho_4x4(
+                KC_MPRV, KC_MPLY, KC_MNXT,   KC_VOLU,
+                RGB_TOG, RGB_MOD, RGB_RMOD,  KC_MUTE,
+                TO(0),   RESET,   KC_2ENTER, KC_VOLD,
+                KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS         //Transparent to let you go between layers
+        ),
 };
 
 
@@ -184,5 +176,4 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 return state;
 }
-
 
