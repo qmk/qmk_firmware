80a0b74d84f1af65dfb7a106e6d34e5eb5384674
Ryan <fauxpark@gmail.com>
2020-05-01 09:45:43 +1000

    Convert V-USB usbdrv to a submodule (#8321)
    * Convert V-USB usbdrv to a submodule
    
    * Fix for Command feature and iWRAP
    
    * Command does not even need this header, in fact
    
    * Add ChangeLog entry

--- tmk_core/protocol/vusb.mk
+++ tmk_core/protocol/vusb.mk
@@ -1,22 +1,23 @@
 VUSB_DIR = protocol/vusb
 
-OPT_DEFS += -DPROTOCOL_VUSB
+# Path to the V-USB library
+VUSB_PATH = $(LIB_PATH)/vusb
 
-SRC +=	$(VUSB_DIR)/main.c \
+SRC += $(VUSB_DIR)/main.c \
 	$(VUSB_DIR)/vusb.c \
-	$(VUSB_DIR)/usbdrv/usbdrv.c \
-	$(VUSB_DIR)/usbdrv/usbdrvasm.S \
-	$(VUSB_DIR)/usbdrv/oddebug.c
-
+	$(VUSB_PATH)/usbdrv/usbdrv.c \
+	$(VUSB_PATH)/usbdrv/usbdrvasm.S \
+	$(VUSB_PATH)/usbdrv/oddebug.c
 
 ifdef NO_UART
-SRC +=	$(COMMON_DIR)/sendchar_null.c
+SRC += $(COMMON_DIR)/sendchar_null.c
 else
-SRC +=	$(COMMON_DIR)/sendchar_uart.c \
+SRC += $(COMMON_DIR)/sendchar_uart.c \
 	$(COMMON_DIR)/uart.c
 endif
 
-
 # Search Path
 VPATH += $(TMK_PATH)/$(VUSB_DIR)
-VPATH += $(TMK_PATH)/$(VUSB_DIR)/usbdrv
+VPATH += $(VUSB_PATH)
+
+OPT_DEFS += -DPROTOCOL_VUSB
