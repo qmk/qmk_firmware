ecf29dbf70eec506e9fcd00924eb95f0f8d3a8a1
mechlovin <57231893+mechlovin@users.noreply.github.com>
2020-05-02 17:47:50 +0700

    update

--- keyboards/mechlovin/adelais/standard_led/rules.mk
+++ keyboards/mechlovin/adelais/standard_led/rules.mk
@@ -1,6 +1,3 @@
-# MCU name
-MCU = STM32F303
-
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 BACKLIGHT_DRIVER = pwm
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
