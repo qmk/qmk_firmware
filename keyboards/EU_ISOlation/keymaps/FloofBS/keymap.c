23dcb508fc110be08e579eeebd9c567cc1793a7c
floof <floof@pop-os.localdomain>
2020-05-04 19:50:22 -0700

    Added EU ISO files? I think?

--- keyboards/EU_ISOlation/keymaps/FloofBS/keymap.c
+++ keyboards/EU_ISOlation/keymaps/FloofBS/keymap.c
@@ -10,8 +10,8 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_EUISO_BIGSPACE(
                         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_F1,  KC_F2,  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, LT(_FN2, KC_QUOT),
-        KC_F3,  KC_F4,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, LT(_FN1, KC_ENT),
+        MO(_FN1), MO(_FN3), KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, LT(_FN2, KC_QUOT),
+        MO(_FN2), KC_CAPS,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, LT(_FN1, KC_ENT),
                         KC_LCTL, KC_LALT,                          KC_SPC,                                  KC_LGUI, MO(_FN3)
     ),
 
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FN3] = LAYOUT_EUISO_BIGSPACE(
-                          _______, _______,   KC_UP, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
-        _______, _______,   RESET, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
-                          _______, _______,                         _______,                                        _______,  _______
+                          _______, _______,   KC_UP, _______, _______, _______, _______,    KC_F1,   KC_F2,   KC_F3,  KC_F4,  _______,
+        _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,    KC_F5,   KC_F6,   KC_F7,  KC_F8,  _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,    KC_F9,   KC_F10,  KC_F11, KC_F12, _______,
+                            RESET, _______,                         _______,                                        _______,  _______
     )
 };
