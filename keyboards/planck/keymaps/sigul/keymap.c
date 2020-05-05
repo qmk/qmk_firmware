bbd7b57b66bc62ac1b0f6a1209917d15ab559422
sigul <desk@silviogulizia.com>
2020-05-05 18:44:37 +0200

    fix secrets order

--- keyboards/planck/keymaps/sigul/keymap.c
+++ keyboards/planck/keymaps/sigul/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_ortho_4x12(
     IT_GRAVE, IT_1,    IT_2,    IT_3,	 IT_4,    IT_5,    IT_6,    IT_7,    IT_8,    IT_9,    IT_0,    _______,
-    _______,  _______, SECRET2, SECRET1, _______, _______, _______, IT_MINS, IT_PLUS, IT_EACC, IT_IACC, IT_UACC,
-    KC_CAPS,  _______, SECRET0, SECRET3, SECRET4, SECRET5, NUMPAD,  _______, _______, IT_LBRC, IT_RBRC, IT_BSLS,
+    _______,  _______, SECRET0, SECRET1, _______, _______, _______, IT_MINS, IT_PLUS, IT_EACC, IT_IACC, IT_UACC,
+    KC_CAPS,  _______, SECRET2, SECRET3, SECRET4, _______, NUMPAD,  SECRET5, _______, IT_LBRC, IT_RBRC, IT_BSLS,
     _______,  _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
