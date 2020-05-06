213eac4689f570a1b7cadd4d545669e51f8a8d8b
Art <artjoms.rizihs@gmail.com>
2020-05-06 13:50:41 +0100

    ergodone GIT LAYER switch

--- keyboards/ergodone/keymaps/art/keymap.c
+++ keyboards/ergodone/keymaps/art/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
  * |--------+------+------+------+------+------| FKEYS|           | FKEYS|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | / git| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCtrl |Media\| Win  |Alt   | NAV  |                                       | Home | End  |Workmn|      | RCtrl|
  *   `----------------------------------'                                       `----------------------------------'
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MINS,          KC_6,         KC_7,       KC_8,       KC_9,              KC_0,      KC_RBRC,
   KC_EQL,           KC_Y,         KC_U,       KC_I,       KC_O,              KC_P,      KC_LBRC,
                     KC_H,         KC_J,       KC_K,       KC_L,              KC_SCLN,   KC_QUOT,
-  TT(FKEYS),        KC_N,         KC_M,       KC_COMM,    KC_DOT,            KC_SLSH,   KC_RSFT,
+  TT(FKEYS),        KC_N,         KC_M,       KC_COMM,    KC_DOT,        LT(GIT,KC_SLSH), KC_RSFT,
                                   KC_HOME,    KC_END,     DF(WORKMAN),       XXXXXXX,   KC_RCTRL,
 
     KC_LEFT,                KC_RIGHT,
