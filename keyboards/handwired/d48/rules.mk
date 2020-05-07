eb1924f408b4fe112328152f6512c36fa14dabbc
Andrew Dunai <a@dun.ai>
2020-05-07 10:19:32 +0300

    Apply suggestions from code review
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/handwired/d48/rules.mk
+++ keyboards/handwired/d48/rules.mk
@@ -2,7 +2,7 @@
 MCU = STM32F303
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no    # Mouse keys
