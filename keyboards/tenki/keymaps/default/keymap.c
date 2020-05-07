7a45f94b46268f61a6fca7d47ad1b9ad990dbd83
eriqadams <eriq.adams@gmail.com>
2020-05-08 04:06:20 +0700

    Fix formatting in keymap.c
    Fix formatting in keymap.c

--- keyboards/tenki/keymaps/default/keymap.c
+++ keyboards/tenki/keymaps/default/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_P7,  	KC_P8,    	KC_P9,    		KC_PPLS,\
     KC_P4,  	KC_P5,    	KC_P6,    		KC_BSPC,\
     KC_P1,  	KC_P2,    	KC_P3,    		KC_PEQL,\
-    KC_P0 , 	KC_PSCR , 	LT(1,KC_PDOT) , KC_PENT
+    KC_P0, 	KC_PSCR , 	LT(1,KC_PDOT), KC_PENT
   ),
 
   [1] = LAYOUT_ortho_5x4(
