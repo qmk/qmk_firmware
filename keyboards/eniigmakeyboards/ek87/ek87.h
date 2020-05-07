55fe0b1463b522c1cd2018662c546a591db0cc46
eniigmakeyboards <support@eniigmakeyboards.com>
2020-05-07 08:10:04 -0400

    Update keyboards/eniigmakeyboards/ek87/ek87.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/eniigmakeyboards/ek87/ek87.h
+++ keyboards/eniigmakeyboards/ek87/ek87.h
@@ -130,7 +130,7 @@
   { k50, k51, XXX, k53, XXX, XXX, k56, XXX, XXX, XXX, k5a, k5b, XXX, k5d, k5e, k5f, k5g, k5h }  \
 }
 
-#define LAYOUT_iso_splitrshift( \
+#define LAYOUT_tkl_iso_split_rshift( \
     k00,      k02, k03, k04, k05,      k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f, k0g, k0h, \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,      k1f, k1g, k1h, \
     k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f, k2g, k2h, \
@@ -146,6 +146,5 @@
   { k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b, XXX, k4d, k4e, XXX, k4g, XXX }, \
   { k50, k51, XXX, k53, XXX, XXX, k56, XXX, XXX, XXX, k5a, k5b, XXX, k5d, k5e, k5f, k5g, k5h }  \
 }
-
 
 
