f1bfd9163c685349f1af2fc4f03a4298e35eb7f1
mechmerlin <mechmerlin@gmail.com>
2020-05-09 08:20:53 -0700

    clean up rules.mk, add community layout suport, and add in bootloader

--- keyboards/projectkb/signature87/rules.mk
+++ keyboards/projectkb/signature87/rules.mk
@@ -1,12 +1,19 @@
-  
 # MCU name
 MCU = STM32F072
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = atmel-dfu
+
 # Build Options
 #   comment out to disable the options.
 #
-
-# project specific files
-
 #BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
@@ -14,8 +21,6 @@ CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
-CUSTOM_MATRIX = no # Custom matrix file
 # BACKLIGHT_ENABLE = yes # This is broken on 072 for some reason
 
-# RAW_ENABLE = yes
-# DYNAMIC_KEYMAP_ENABLE = yes
+LAYOUTS = tkl_ansi
