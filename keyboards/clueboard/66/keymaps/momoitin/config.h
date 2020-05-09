2bc73aa9b0063bda0f91d93d0ea9b9a92e700f37
Evan Dunlap <ringod0708@gmail.com>
2020-05-08 22:43:38 -0400

    Momoitin Keymap Upload Test

--- keyboards/clueboard/66/keymaps/momoitin/config.h
+++ keyboards/clueboard/66/keymaps/momoitin/config.h
@@ -11,4 +11,3 @@
 #define AUDIO_CLICKY			/*Enables Audio*/
 #define TAPPING_TOGGLE 2		/*Sets Tap Toggle "TT()" Layers to only require 2 taps before switching on*/
 #define DAC_SAMPLE_MAX 65535U	/*Sets Volume for ARM device, must be a value of [(2^x) - 1] to function*/
-
