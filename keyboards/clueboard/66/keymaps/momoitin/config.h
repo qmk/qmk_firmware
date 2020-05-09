8b9467f553d7967f1c81f23bf99c19b7645686d0
Evan Dunlap <38865755+momoitin@users.noreply.github.com>
2020-05-08 22:45:32 -0400

    Update config.h

--- keyboards/clueboard/66/keymaps/momoitin/config.h
+++ keyboards/clueboard/66/keymaps/momoitin/config.h
@@ -10,4 +10,4 @@
 #define MUSIC_MAP				/*Enables the Music Map in keymap.c*/
 #define AUDIO_CLICKY			/*Enables Audio*/
 #define TAPPING_TOGGLE 2		/*Sets Tap Toggle "TT()" Layers to only require 2 taps before switching on*/
-#define DAC_SAMPLE_MAX 65535U	/*Sets Volume for ARM device, must be a value of [(2^x) - 1] to function*/git pu
+#define DAC_SAMPLE_MAX 65535U	/*Sets Volume for ARM device, must be a value of [(2^x) - 1] to function*/
