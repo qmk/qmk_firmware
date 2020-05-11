5f2bfc23d0e299e139882c1c719317456743e5c9
Vincent Lee <vlee@vlee.me.uk>
2020-05-11 21:52:48 +0100

    Update keyboards/melody96/rules.mk
    Co-authored-by: MechMerlin <30334081+mechmerlin@users.noreply.github.com>

--- keyboards/melody96/rules.mk
+++ keyboards/melody96/rules.mk
@@ -24,4 +24,4 @@ NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https:/
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = yes
-LINK_TIME_OPTIMIZATION_ENABLE = yes # reduce firmware size
+LTO_ENABLE = yes # reduce firmware size
