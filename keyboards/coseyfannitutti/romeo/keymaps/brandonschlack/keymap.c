e02154746bbef6c64cda772d3ac5aca79370a485
Brandon Schlack <brandonschlack@gmail.com>
2020-05-11 09:47:19 -0700

    [Keymap] Swap R-Option and Lower on Romeo

--- keyboards/coseyfannitutti/romeo/keymaps/brandonschlack/keymap.c
+++ keyboards/coseyfannitutti/romeo/keymaps/brandonschlack/keymap.c
@@ -25,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬──────┤
 * │Shift   │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│ ?/Sft│
 * ├────┬───┼───┼───┴───┴───┴───┴───┴───┴┬──┴─┬─┴─┬────┤
-* │Ctrl│Opt│Cmd│          Space         │ Cmd│Lwr│ Opt│
+* │Ctrl│Opt│Cmd│          Space         │ Cmd│Opt│ Lwr│
 * └────┴───┴───┴────────────────────────┴────┴───┴────┘
 */
 [_BASE] = LAYOUT_ansi_40( \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     HY_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT,  \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SF_SLSH, \
-    KC_LCTL, KC_LOPT, KC_LCMD,                            SPC_RAI,                   KC_RCMD, LOWER,   KC_ROPT  \
+    KC_LCTL, KC_LOPT, KC_LCMD,                            SPC_RAI,                   KC_RCMD, KC_ROPT, LOWER    \
 ),
 /* Lower
 * ┌─────┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
