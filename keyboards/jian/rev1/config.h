10148bb4bfafcfaafa55b7de9d4d4c85cd684bb4
KGOH <kgofhedgehogs@gmail.com>
2020-05-12 00:36:11 +0200

    Add NUM_INVERT

--- keyboards/jian/rev1/config.h
+++ keyboards/jian/rev1/config.h
@@ -57,32 +57,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define CAPS_NMOSFET  //uncomment this if you using n-mosfet
 // #define SCROLL_NMOSFET  //uncomment this if you using n-mosfet
 
+// #define NUM_INVERT // uncomment this if you want to reverse logic of numlock
+// This will make it light up only when lock is off
+// (Doesn't work on mac. There is no num lock, so it will be always off and lit)
+
 #ifdef NUM_NMOSFET
-#define SET_NUM_LOCK_LED() writePinHigh(NUM_LOCK_LED_PIN)
 #define RESET_NUM_LOCK_LED() writePinLow(NUM_LOCK_LED_PIN)
+#ifdef NUM_INVERT
+#define UPDATE_NUM_LOCK_LED() writePin(NUM_LOCK_LED_PIN, !led_state.num_lock)
+#else
+#define UPDATE_NUM_LOCK_LED() writePin(NUM_LOCK_LED_PIN, led_state.num_lock)
+#endif // NUM_INVERT
+#else
+#define RESET_NUM_LOCK_LED() writePinHigh(NUM_LOCK_LED_PIN)
+#ifdef NUM_INVERT
 #define UPDATE_NUM_LOCK_LED() writePin(NUM_LOCK_LED_PIN, led_state.num_lock)
 #else
-#define SET_NUM_LOCK_LED() writePinLow(NUM_LOCK_LED_PIN)
-#define RESET_NUM_LOCK_LED() writePinHigh(NUM_LOCK_LED_PIN)
 #define UPDATE_NUM_LOCK_LED() writePin(NUM_LOCK_LED_PIN, !led_state.num_lock)
+#endif // NUM_INVERT
 #endif // NUM_NMOSFET
 
 #ifdef CAPS_NMOSFET
-#define SET_CAPS_LOCK_LED() writePinHigh(CAPS_LOCK_LED_PIN)
 #define RESET_CAPS_LOCK_LED() writePinLow(CAPS_LOCK_LED_PIN)
 #define UPDATE_CAPS_LOCK_LED() writePin(CAPS_LOCK_LED_PIN, led_state.caps_lock)
 #else
-#define SET_CAPS_LOCK_LED() writePinLow(CAPS_LOCK_LED_PIN)
 #define RESET_CAPS_LOCK_LED() writePinHigh(CAPS_LOCK_LED_PIN)
 #define UPDATE_CAPS_LOCK_LED() writePin(CAPS_LOCK_LED_PIN, !led_state.caps_lock)
 #endif // CAPS_NMOSFET
 
 #ifdef SCROLL_NMOSFET
-#define SET_SCROLL_LOCK_LED() writePinHigh(SCROLL_LOCK_LED_PIN)
 #define RESET_SCROLL_LOCK_LED() writePinLow(SCROLL_LOCK_LED_PIN)
 #define UPDATE_SCROLL_LOCK_LED() writePin(SCROLL_LOCK_LED_PIN, led_state.scroll_lock)
 #else
-#define SET_SCROLL_LOCK_LED() writePinLow(SCROLL_LOCK_LED_PIN)
 #define RESET_SCROLL_LOCK_LED() writePinHigh(SCROLL_LOCK_LED_PIN)
 #define UPDATE_SCROLL_LOCK_LED() writePin(SCROLL_LOCK_LED_PIN, !led_state.scroll_lock)
 #endif // SCROLL_NMOSFET
