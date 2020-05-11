7426166db5648a98f4524799294369e79ad9fd99
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2020-05-11 09:09:00 -0700

    Update keyboards/projectkb/signature87/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/projectkb/signature87/rules.mk
+++ keyboards/projectkb/signature87/rules.mk
@@ -1,16 +1,5 @@
 # MCU name
 MCU = STM32F072
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
-BOOTLOADER = atmel-dfu
-
 # Build Options
 #   comment out to disable the options.
 #
