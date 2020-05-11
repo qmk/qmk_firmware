38cf1b0e829b8540912b3791e88a22914e03dfb5
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2020-05-11 09:09:18 -0700

    Update keyboards/projectkb/signature87/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/projectkb/signature87/rules.mk
+++ keyboards/projectkb/signature87/rules.mk
@@ -10,6 +10,6 @@ CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
-# BACKLIGHT_ENABLE = yes # This is broken on 072 for some reason
+BACKLIGHT_ENABLE = no
 
 LAYOUTS = tkl_ansi
