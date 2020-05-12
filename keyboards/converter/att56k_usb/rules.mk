126f8b96dde49cf88ce675c5d580b4f28b1b0dc3
engicoder <2364890+engicoder@users.noreply.github.com>
2020-05-12 11:23:22 -0400

    Update keyboards/converter/att56k_usb/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/converter/att56k_usb/rules.mk
+++ keyboards/converter/att56k_usb/rules.mk
@@ -13,7 +13,6 @@ BOOTLOADER = caterina
 
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
