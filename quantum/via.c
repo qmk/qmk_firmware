b1738b4ce1b317ba6bde1e5f6a1942d06778e95e
skullY <skullydazed@gmail.com>
2020-05-11 21:26:03 -0700

    Calculate via checksum using __DATE__ instead of QMK_BUILDDATE

--- quantum/via.c
+++ quantum/via.c
@@ -45,7 +45,6 @@
 #include "raw_hid.h"
 #include "dynamic_keymap.h"
 #include "tmk_core/common/eeprom.h"
-#include "version.h"  // for QMK_BUILDDATE used in EEPROM magic
 
 // Forward declare some helpers.
 #if defined(VIA_QMK_BACKLIGHT_ENABLE)
@@ -61,10 +60,10 @@ void via_qmk_rgblight_get_value(uint8_t *data);
 // Can be called in an overriding via_init_kb() to test if keyboard level code usage of
 // EEPROM is invalid and use/save defaults.
 bool via_eeprom_is_valid(void) {
-    char *  p      = QMK_BUILDDATE;  // e.g. "2019-11-05-11:29:54"
-    uint8_t magic0 = ((p[2] & 0x0F) << 4) | (p[3] & 0x0F);
-    uint8_t magic1 = ((p[5] & 0x0F) << 4) | (p[6] & 0x0F);
-    uint8_t magic2 = ((p[8] & 0x0F) << 4) | (p[9] & 0x0F);
+    char *  p      = __DATE__; // e.g. "Jun 12 2021"
+    uint8_t magic0 = (p[0] + p[1] + p[2]);
+    uint8_t magic1 = ((p[4] & 0x0F) << 4) | (p[5] & 0x0F);
+    uint8_t magic2 = ((p[9] & 0x0F) << 4) | (p[10] & 0x0F);
 
     return (eeprom_read_byte((void *)VIA_EEPROM_MAGIC_ADDR + 0) == magic0 && eeprom_read_byte((void *)VIA_EEPROM_MAGIC_ADDR + 1) == magic1 && eeprom_read_byte((void *)VIA_EEPROM_MAGIC_ADDR + 2) == magic2);
 }
@@ -72,10 +71,10 @@ bool via_eeprom_is_valid(void) {
 // Sets VIA/keyboard level usage of EEPROM to valid/invalid
 // Keyboard level code (eg. via_init_kb()) should not call this
 void via_eeprom_set_valid(bool valid) {
-    char *  p      = QMK_BUILDDATE;  // e.g. "2019-11-05-11:29:54"
-    uint8_t magic0 = ((p[2] & 0x0F) << 4) | (p[3] & 0x0F);
-    uint8_t magic1 = ((p[5] & 0x0F) << 4) | (p[6] & 0x0F);
-    uint8_t magic2 = ((p[8] & 0x0F) << 4) | (p[9] & 0x0F);
+    char *  p      = __DATE__; // e.g. "Jun 12 2021"
+    uint8_t magic0 = (p[0] + p[1] + p[2]);
+    uint8_t magic1 = ((p[4] & 0x0F) << 4) | (p[5] & 0x0F);
+    uint8_t magic2 = ((p[9] & 0x0F) << 4) | (p[10] & 0x0F);
 
     eeprom_update_byte((void *)VIA_EEPROM_MAGIC_ADDR + 0, valid ? magic0 : 0xFF);
     eeprom_update_byte((void *)VIA_EEPROM_MAGIC_ADDR + 1, valid ? magic1 : 0xFF);
