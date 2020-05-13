18a12ac17cd5d31a23ba05bf4da105fa72bf15a2
sh-xguitar <ibanez_shine@yahoo.com>
2020-05-14 00:13:35 +0700

    Update keymap.c

--- keyboards/ASH-1800/keymaps/ANSI-WK/keymap.c
+++ keyboards/ASH-1800/keymaps/ANSI-WK/keymap.c
@@ -1,4 +1,5 @@
 /* Copyright 2019 angelbirth
+ *           2020 sh_xguitar
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
