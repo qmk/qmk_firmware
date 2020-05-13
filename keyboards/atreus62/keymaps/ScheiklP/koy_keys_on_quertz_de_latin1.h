9578c1590d45cd4b94ea2eab8ef94c91e5796468
Paul Scheikl <paul.scheikl@kit.edu>
2020-05-13 22:15:11 +0200

    Update keyboards/atreus62/keymaps/ScheiklP/koy_keys_on_quertz_de_latin1.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/atreus62/keymaps/ScheiklP/koy_keys_on_quertz_de_latin1.h
+++ keyboards/atreus62/keymaps/ScheiklP/koy_keys_on_quertz_de_latin1.h
@@ -62,5 +62,3 @@ Additionally, there are some common chars that we dit not yet define:
 #define N_COPY LCTL(KC_C) // CTRL+C
 #define N_CUT LCTL(KC_X) // CTRL+X
 #define N_UNDO LCTL(N_Z) // CTRL+Z
-
-#endif
