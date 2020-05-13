6ed7b798488d27e97c793c69dd96b095de145dd0
powerlemming <47431393+powerlemming@users.noreply.github.com>
2020-05-13 18:17:59 -0400

    Update keyboards/kbdclack/kaishi65/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/kbdclack/kaishi65/keymaps/default/keymap.c
+++ keyboards/kbdclack/kaishi65/keymaps/default/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [_FN] = LAYOUT(
+    [_FN] = LAYOUT_65_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
         _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,
         _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_HOME,
