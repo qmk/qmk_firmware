6fbcbadf44f372412de940a2888917901c2c4f86
dlgoodr <dlg@dsrw.org>
2020-05-14 08:58:58 -0500

    alt-4 as alt-f4

--- keyboards/tada68/keymaps/dlg/keymap.c
+++ keyboards/tada68/keymaps/dlg/keymap.c
@@ -3,11 +3,8 @@
 #define _BL 0
 #define _FL 1
 #define _MAC 2
+#define _LA 3
 
-// investigate later - use lctl / lsft as up/down.
-// https://github.com/qmk/qmk_firmware/tree/master/keyboards/tada68/keymaps/fakb
-#define LCTL_MA LCTL_T(KC_UP)
-#define LSFT_MA LSFT_T(KC_DOWN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
@@ -27,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *       Win   + Esc = `
    */
 [_BL] = LAYOUT_ansi(
-  KC_ESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_GRV,  \
+  KC_GRV, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_ESC,  \
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL , \
   KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,      KC_PGUP, \
-  KC_LSPO,          KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC,  KC_UP,  KC_PGDN, \
-  KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                          KC_RALT, MO(_FL), KC_RCTRL, KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LSFT,          KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,  KC_UP,  KC_PGDN, \
+  KC_LCTL, KC_LGUI, LM(_LA, MOD_LALT),      KC_SPC,                          KC_RALT, MO(_FL), KC_RCTRL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -39,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |     |BL |BL-|BL+|Brt|   |   |   |mac|   |PSc|   |   |     |Ins |
    * |----------------------------------------------------------------|
-   * |      |   |   |   |   |   |<- |Dn |Up | ->|   |   |        |Hme |
+   * |      |   |   |   |   |   |Lef|Dn |Up |Rig|   |   |        |Hme |
    * |----------------------------------------------------------------|
    * |        |   |   |   |   |   |   |MUT|V- |V+ |   |      |PUp|End |
    * |----------------------------------------------------------------|
@@ -73,4 +70,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_LCTL, KC_LALT, KC_LGUI,                        _______,                       KC_RGUI, _______, KC_RALT, _______, _______, _______),
 
+  /* Keymap _LA: Left Alt Layer - LALT-4 maps to LALT-F4 so I can quit apps
+   * ,----------------------------------------------------------------.
+   * |    |   |   |   |F4 |   |   |   |   |   |   |   |   |      |    |
+   * |----------------------------------------------------------------|
+   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |    |
+   * |----------------------------------------------------------------|
+   * |      |   |   |   |   |   |   |   |   |   |   |   |        |    |
+   * |----------------------------------------------------------------|
+   * |        |   |   |   |   |   |   |   |   |   |   |      |   |    |
+   * |----------------------------------------------------------------|
+   * |    |    |    |                       |   |   |    |   |   |    |
+   * `----------------------------------------------------------------'
+   */
+[_LA] = LAYOUT_ansi(
+  _______, _______, _______, _______, KC_F4,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______, \
+  _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______,                        _______,                       _______, _______, _______, _______, _______, _______),
 };
