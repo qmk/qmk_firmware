164428ef8ceb6fc101aa2a3f191084e29d089f17
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-05-15 15:31:47 -0400

    Update keyboards/clawsome/bookerboard/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/clawsome/bookerboard/keymaps/default/keymap.c
+++ keyboards/clawsome/bookerboard/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * | - | 0 | = |
     * --------------
     */
-    [0] = LAYOUT_4x3(
+    [0] = LAYOUT(
         KC_7,    KC_8,    KC_9,
         KC_4,    KC_5,    KC_6,
         KC_1,    KC_2,    KC_3,
@@ -35,4 +35,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 };
-
