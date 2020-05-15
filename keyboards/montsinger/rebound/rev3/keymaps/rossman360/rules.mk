c79d2f44d444d8d1528173646489750ddeaf2fe0
Rossman360 <rmontsinger@gmail.com>
2020-05-15 13:10:09 -0400

    reduntant line

--- keyboards/montsinger/rebound/rev3/keymaps/rossman360/rules.mk
+++ keyboards/montsinger/rebound/rev3/keymaps/rossman360/rules.mk
@@ -1,5 +1,4 @@
 MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no       # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
-OLED_DRIVER_ENABLE = yes
+OLED_DRIVER_ENABLE = no
