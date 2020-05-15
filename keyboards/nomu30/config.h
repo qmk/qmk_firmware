fe78cbb5207040769d502daa7c7a5ae67125023e
Naoto Takai <takai@recompile.net>
2020-05-15 22:11:55 +0900

    Move existing settings to rev1 directory

--- keyboards/nomu30/config.h
+++ keyboards/nomu30/config.h
@@ -18,32 +18,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xC0C0
-#define PRODUCT_ID      0x3000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Naoto Takai
-#define PRODUCT         nomu30
-#define DESCRIPTION     A 30% keyboard with ISO enter.
-
-/* key matrix size */
-#define MATRIX_ROWS 3
-#define MATRIX_COLS 12
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
-*/
-#define MATRIX_ROW_PINS { D1, D0, D4 }
-#define MATRIX_COL_PINS { C6, D7, E6, B4, F4, F5, F6, F7, B1, B3, B2, B6 }
-#define UNUSED_PINS
-
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
