528852e016ea6604b1047877390a60878c056770
trainman419 <legotown@aol.com>
2020-05-16 00:45:33 -0700

    Fix build for RGB; RGB toggle working.

--- build_json.mk
+++ build_json.mk
@@ -1,21 +1,26 @@
 # Look for a json keymap file
 ifneq ("$(wildcard $(MAIN_KEYMAP_PATH_5)/keymap.json)","")
+    -include $(MAIN_KEYMAP_PATH_5)/rules.mk
     KEYMAP_C := $(KEYBOARD_OUTPUT)/src/keymap.c
     KEYMAP_JSON := $(MAIN_KEYMAP_PATH_5)/keymap.json
     KEYMAP_PATH := $(MAIN_KEYMAP_PATH_5)
 else ifneq ("$(wildcard $(MAIN_KEYMAP_PATH_4)/keymap.json)","")
+    -include $(MAIN_KEYMAP_PATH_4)/rules.mk
     KEYMAP_C := $(KEYBOARD_OUTPUT)/src/keymap.c
     KEYMAP_JSON := $(MAIN_KEYMAP_PATH_4)/keymap.json
     KEYMAP_PATH := $(MAIN_KEYMAP_PATH_4)
 else ifneq ("$(wildcard $(MAIN_KEYMAP_PATH_3)/keymap.json)","")
+    -include $(MAIN_KEYMAP_PATH_3)/rules.mk
     KEYMAP_C := $(KEYBOARD_OUTPUT)/src/keymap.c
     KEYMAP_JSON := $(MAIN_KEYMAP_PATH_3)/keymap.json
     KEYMAP_PATH := $(MAIN_KEYMAP_PATH_3)
 else ifneq ("$(wildcard $(MAIN_KEYMAP_PATH_2)/keymap.json)","")
+    -include $(MAIN_KEYMAP_PATH_2)/rules.mk
     KEYMAP_C := $(KEYBOARD_OUTPUT)/src/keymap.c
     KEYMAP_JSON := $(MAIN_KEYMAP_PATH_2)/keymap.json
     KEYMAP_PATH := $(MAIN_KEYMAP_PATH_2)
 else ifneq ("$(wildcard $(MAIN_KEYMAP_PATH_1)/keymap.json)","")
+    -include $(MAIN_KEYMAP_PATH_1)/rules.mk
     KEYMAP_C := $(KEYBOARD_OUTPUT)/src/keymap.c
     KEYMAP_JSON := $(MAIN_KEYMAP_PATH_1)/keymap.json
     KEYMAP_PATH := $(MAIN_KEYMAP_PATH_1)
