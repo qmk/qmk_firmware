73afe69a7142b2dc9a0c3b9d1914c4d3992cb177
Takeshi Nishio <kkeennnn@gmail.com>
2020-05-16 15:25:58 +0900

    Update colice_rr キーマップ調整。

--- keyboards/colice_rr/keymaps/default/keymap.c
+++ keyboards/colice_rr/keymaps/default/keymap.c
@@ -47,15 +47,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,                   KC_PSLS,KC_PAST,TG_NUM,                  KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,                     KC_PSLS,KC_PAST,TG_NUM,  \
             KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,           KC_P7,  KC_P8,  KC_P9,  KC_PMNS,                 KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,     KC_P7,  KC_P8,  KC_P9,  KC_PMNS, \
             KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,           KC_MPRV,KC_MPLY,KC_MNXT,KC_PPLS,                 KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,              KC_MPRV,KC_MPLY,KC_MNXT,KC_PPLS, \
-            KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   RAISE,          KC_MUTE,KC_VOLD,KC_VOLU,KC_PENT,                 KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_UP,  KC_UP,       KC_MUTE,KC_VOLD,KC_VOLU,KC_PENT, \
-    RAISE,  RAISE,                  KC_LALT,KC_LGUI,SP_SFT, SP_SFT,         KC_P0,          KC_PDOT,                         LOWER,  SP_GUI, KC_LALT,        KC_LCTL,KC_LEFT,KC_DOWN,KC_RGHT,     KC_P0,          KC_PDOT         \
+            KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LOWER,          KC_MUTE,KC_VOLD,KC_VOLU,                         KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_UP,  KC_UP,       KC_MUTE,KC_VOLD,KC_VOLU,         \
+    RAISE,  RAISE,                  KC_LALT,KC_LGUI,SP_SFT, SP_SFT,         KC_P0,          KC_PDOT,KC_PENT,                 LOWER,  SP_GUI, KC_LALT,        KC_LCTL,KC_LEFT,KC_DOWN,KC_RGHT,     KC_P0,          KC_PDOT,KC_PENT  \
     ),
     [_NUM] = LAYOUT(
     _______,KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,                   KC_PSLS,KC_PAST,TG_NUM,                  KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,                     KC_PSLS,KC_PAST,TG_NUM, \
             KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,           KC_P7,  KC_P8,  KC_P9,  KC_PMNS,                 KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,     KC_P7,  KC_P8,  KC_P9,  KC_PMNS, \
             KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,           KC_P4,  KC_P5,  KC_P6,  KC_PPLS,                 KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,              KC_P4,  KC_P5,  KC_P6,  KC_PPLS, \
-            KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LOWER,          KC_P1,  KC_P2,  KC_P3,  KC_PENT,                 KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,_______,_______,_______,     KC_P1,  KC_P2,  KC_P3,  KC_PENT, \
-    _______,RAISE,                  KC_LALT,KC_LGUI,_______,_______,        KC_P0,          KC_PDOT,                         _______,_______,_______,        _______,_______,_______,_______,     KC_P0,          KC_PDOT        \
+            KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   RAISE,          KC_P1,  KC_P2,  KC_P3,                           KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,_______,_______,_______,     KC_P1,  KC_P2,  KC_P3,   \
+    _______,RAISE,                  KC_LALT,KC_LGUI,_______,_______,        KC_P0,          KC_PDOT,_______,                 _______,_______,_______,        _______,_______,_______,_______,     KC_P0,          KC_PDOT,_______  \
     ),
     [_LOWER] = LAYOUT(
     _______,_______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,                 _______,_______,_______,                 KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,                         _______,_______,_______, \
@@ -188,6 +188,17 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     }
   #endif
   return state;
+}
+
+// ロータリーエンコーダ
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    }
 }
 
 /*
