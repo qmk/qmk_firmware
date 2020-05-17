4413ea42d96fb14efd3023b37cd6058c53ba25b8
Frothy <frothykillskittens@gmail.com>
2020-05-17 20:27:00 -0300

    PR changes
     * Removed empty via/config.h
     * Changed product ID from 0x6060 to 0x0001

--- keyboards/contra/config.h
+++ keyboards/contra/config.h
@@ -5,7 +5,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4354 /* "CT" */
-#define PRODUCT_ID      0x6060
+#define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Cartel
 #define PRODUCT         Contra
