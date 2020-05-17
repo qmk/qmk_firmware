c43e29303a880f9e2862f3c3f464aad0caece264
MxBlue <mundekkat@hotmail.com>
2020-05-17 19:14:48 +1000

    Add ISO layout to universal

--- keyboards/foxlab/fox65/universal/universal.h
+++ keyboards/foxlab/fox65/universal/universal.h
@@ -46,6 +46,20 @@
     { K40,   K41,   K42,   KC_NO, KC_NO, KC_NO, K46,   KC_NO, KC_NO, KC_NO, KC_NO, K4B,   K4C,   K4D,   KC_NO, K4F   }, \
 }
 
+#define LAYOUT_iso_all( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F,   \
+    K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,         K1F,   \
+    K20,    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K1D,  K2D,  K2F,   \
+    K30,  K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,   K3C,   K3D, K3F,   \
+    K40,  K41,  K42,          K44,  K46,  K48,        K4A,  K4B,     K4C, K4D, K4F    \
+) { \
+    { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D,   K0E,   K0F   }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D,   KC_NO, K1F   }, \
+    { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   KC_NO, K2D,   KC_NO, K2F   }, \
+    { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,   KC_NO, K3F   }, \
+    { K40,   K41,   K42,   KC_NO, K44,   KC_NO, K46,   KC_NO, K48,   KC_NO, K4A,   K4B,   K4C,   K4D,   KC_NO, K4F   }, \
+}
+
 #define LAYOUT_all( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F,   \
     K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,    K1F,   \
