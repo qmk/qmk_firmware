f0057154c6fb9d6bc40a8415f495407812eb8a38
Kamel Makhloufi <fabmanager@esaaa.fr>
2020-05-17 08:15:29 +0200

    Added custom keymap with french mac layout.

---
+++ keyboards/xd60/keymaps/melka/config.h
@@ -0,0 +1,14 @@
+#include "passwd.h"
+
+#ifndef PASSWORD_A
+    #define PASSWORD_A "1234567890"
+#endif
+
+#ifndef PASSWORD_B
+    #define PASSWORD_B "pass123"
+#endif
+
+#define RGBLIGHT_LAYERS
+
+#define LSPO_KEYS KC_LSFT, KC_TRNS, KC_5
+#define RSPC_KEYS KC_RSFT, KC_TRNS, KC_MINS
