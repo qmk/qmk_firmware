f645f4af4507d32c0bbdcd087d031e8d7c85bc74
mechmerlin <mechmerlin@gmail.com>
2020-05-17 15:13:29 -0700

    fix up rgb underglow

--- keyboards/wolfmarkclub/wm1/config.h
+++ keyboards/wolfmarkclub/wm1/config.h
@@ -35,14 +35,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DIODE_DIRECTION COL2ROW
 
-#define RGBLED_NUM 18
 #define RGB_DI_PIN A3
+#ifdef RGB_DI_PIN
+#define RGBLED_NUM 18
+#define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
 #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-
+#endif
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
