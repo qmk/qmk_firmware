7b46910bb59797707cd32b1b68bd63044ff2d38a
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-05-17 20:29:51 -0300

    remove permissive hold from crkbd

--- keyboards/crkbd/keymaps/rhruiz/config.h
+++ keyboards/crkbd/keymaps/rhruiz/config.h
@@ -42,7 +42,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef TAPPING_TERM
 #define TAPPING_TERM 150
-#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #define NO_ACTION_ONESHOT
