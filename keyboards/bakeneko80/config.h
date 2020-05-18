3b06fdcc33ee5c3bb619915025d767c5f2afa24d
Koichi Katano <36572567+kkatano@users.noreply.github.com>
2020-05-18 10:03:52 +0900

    Update keyboards/bakeneko80/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/bakeneko80/config.h
+++ keyboards/bakeneko80/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0x2FCC
+#define VENDOR_ID 0xFEED
 #define PRODUCT_ID 0x8DEF
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Koichi Katano
