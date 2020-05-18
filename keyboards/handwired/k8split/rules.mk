0fa74345a5ad35a959aeaa15daa9828e2b9395e7
ckath <ckat@teknik.io>
2020-05-18 22:51:10 +0200

    New Keyboard: k8split

---
+++ keyboards/handwired/k8split/rules.mk
@@ -0,0 +1,10 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes           # USB Nkey Rollover
