a6278a4bf52f54cac7c090613817b380105e0175
halfenergized <half_energized@yahoo.co.uk>
2020-05-20 03:31:29 +0700

    Update keyboards/tgr/tris/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/tgr/tris/keymaps/default/keymap.c
+++ keyboards/tgr/tris/keymaps/default/keymap.c
@@ -45,17 +45,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,          KC_TRNS
     )
 };
-
-/*
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool led_update_user(led_t led_state) {
-    return true;
-}
-*/
