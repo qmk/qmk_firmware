69c4712b7acf71cf3924fb7476a0bbfa3e4dfa3a
MxBlue <mundekkat@hotmail.com>
2020-05-20 08:08:35 +1000

    Invert capslock LED state

--- keyboards/foxlab/key65/universal/universal.c
+++ keyboards/foxlab/key65/universal/universal.c
@@ -27,7 +27,7 @@ void led_init_ports(void) {
 
 bool led_update_kb(led_t led_state) {
     if (led_update_user(led_state)) {
-        writePin(E6, led_state.caps_lock);
+        writePin(E6, !led_state.caps_lock);
     }
     return true;
 }
