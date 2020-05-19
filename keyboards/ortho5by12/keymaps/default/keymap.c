1deb9da4fe73b20d693e9eea27eed358c272a178
itsnoteasy <pyrosoma1@yahoo.ie>
2020-05-19 18:34:17 +0100

    Update keyboards/ortho5by12/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ortho5by12/keymaps/default/keymap.c
+++ keyboards/ortho5by12/keymaps/default/keymap.c
@@ -206,9 +206,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             #ifdef BACKLIGHT_ENABLE
               backlight_step();
             #endif
-            #ifdef __AVR__
-            //PORTE &= ~(1<<6);
-            #endif
           } else {
             unregister_code(KC_RSFT);
             #ifdef __AVR__
