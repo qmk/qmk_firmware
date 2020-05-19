f5b01c098e8e486a80c6728c3ac68a2acededcfb
Rys Sommefeldt <rys@sommefeldt.com>
2020-05-19 14:43:47 +0100

    Rename keyboard implementation filenames

--- keyboards/aos/tkl/tkl.c
+++ keyboards/aos/tkl/tkl.c
@@ -15,4 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ace_of_spades.h"
+#include "tkl.h"
