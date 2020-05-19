2461dfd0879bf9101b9475ac249a054685a9a374
itsnoteasy <pyrosoma1@yahoo.ie>
2020-05-19 16:40:00 +0100

    Update keyboards/ortho5by12/ortho5by12.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ortho5by12/ortho5by12.c
+++ keyboards/ortho5by12/ortho5by12.c
@@ -16,28 +16,15 @@
 #include "ortho5by12.h"
 
 void matrix_init_kb(void) {
-	// put your keyboard start-up code here
-	// runs once when the firmware starts up
-
-	matrix_init_user();
+    setPinOutput(C4);
+    setPinOutput(C5);
 }
 
-void matrix_scan_kb(void) {
-	// put your looping keyboard code here
-	// runs every cycle (a lot)
-
-	matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-	// put your per-action keyboard code here
-	// runs for every action, just before processing by the firmware
-
-	return process_record_user(keycode, record);
-}
-
-void led_set_kb(uint8_t usb_led) {
-	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-	led_set_user(usb_led);
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        writePin(C4, led_state.num_lock);
+        writePin(C5, led_state.caps_lock);
+    }
+    return res;
 }
