a8d7779ca6f0682c65578c93ccaff7033540cee0
George Wietor <george@LIGHT-GRAY.local>
2020-05-20 13:27:52 -0400

    Add simple VIA support to Planck Rev6

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x6F6C //ol
+#define PRODUCT_ID      0x706C //pl
 #define MANUFACTURER    OLKB
 #define PRODUCT         Planck
 #define DESCRIPTION     A compact ortholinear keyboard
