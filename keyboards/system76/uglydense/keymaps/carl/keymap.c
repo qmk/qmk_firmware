a5532591b1d512192706d5f261dae3e2fd62aa0e
Carl Richell <carl@system76.com>
2020-05-20 09:30:18 -0600

    Update carl keymap

--- keyboards/system76/uglydense/keymaps/carl/keymap.c
+++ keyboards/system76/uglydense/keymaps/carl/keymap.c
@@ -20,7 +20,7 @@ ________________________________________________________________________________
       |   SHIFT    |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    | SHIFT  |   UP   |
   ____|____________|________|________|________|________|________|________|________|________|________|________|________|________|_________
   |            |        |       |        |                 |                 |        |        |             |        |        |        |
-  |    CTRL    |  FN    | LALT  | LGUI   |   BACKSPACE     |      SPACE      | RCTRL  |  RALT  |     FN      |  LEFT  |  DOWN  | RIGHT  |
+  |    CTRL    |  Alt   | FN    | LGUI   |   BACKSPACE     |      SPACE      | RCTRL  |  RALT  |     FN      |  LEFT  |  DOWN  | RIGHT  |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|________|________|________|
 */
 
@@ -30,7 +30,7 @@ ________________________________________________________________________________
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
           KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
               KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-          KC_LCTL, MO(1),   KC_LALT, KC_LGUI,     KC_BSPC,          KC_SPC,     KC_RCTL,   KC_RALT,   MO(1),     KC_LEFT, KC_DOWN, KC_RIGHT
+          KC_LCTL, KC_LALT, MO(1), KC_LGUI,     KC_BSPC,          KC_SPC,     KC_RCTL,   KC_RALT,   MO(1),     KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
     /* Layer 1, function layer
@@ -42,10 +42,10 @@ ________________________________________________________________________________
 |        |        |        |        |        |        |        |        |        |        |        |        |        |            |
 |________|________|________|________|________|________|________|________|________|________|________|________|________|____________|
 |            |        |        |        |        |        |        |        |        |        |        |        |        |        |
-|            |        |        |        |        |        |        |        |        |        |        |        |        |        |
+|            |        |        |        |        |        |  Home  |  PgUp  |  PgDn  |  End   |        |        |        |        |
 |____________|________|________|________|________|________|________|________|________|________|________|________|________|________|
   |            |        |        |        |        |        |        |        |        |        |        |        |            |
-  |            |        |        |        |        |        |        |        |        |        |        |        |            |
+  |            |        |        |        |        |        |  LEFT  |  DOWN  |  UP    |  RIGHT |        |        |            |
   |____________|________|________|________|________|________|________|________|________|________|________|________|____________|
       |            |        |        |        |        |        |        |        |        |        |        |        |        |
       |            |        |        |        |        |        |        |        |        |        |        |        |  PgUp  |
@@ -60,8 +60,8 @@ ________________________________________________________________________________
   [1] = LAYOUT(
     RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PGUP,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,     KC_HOME,  KC_PGDN, KC_END
   )
