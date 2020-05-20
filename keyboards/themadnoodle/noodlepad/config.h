5503a2eac79dcd02cac10d213e54834faa9a4546
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2020-05-20 17:10:16 -0400

    Update keyboards/themadnoodle/noodlepad/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/themadnoodle/noodlepad/config.h
+++ keyboards/themadnoodle/noodlepad/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x1701
-#define DEVICE_VER 		0x0001
+#define DEVICE_VER      0x0001
 #define MANUFACTURER    The Mad Noodle
 #define PRODUCT         Noodle Pad v1
 #define DESCRIPTION     Noodle Pad macro keypad
