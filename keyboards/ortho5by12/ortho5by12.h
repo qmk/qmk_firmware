b7338e1134b4ee8b2cbd66307527ab56193d52f2
itsnoteasy <pyrosoma1@yahoo.ie>
2020-05-21 23:29:47 +0100

    Update keyboards/ortho5by12/ortho5by12.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/ortho5by12/ortho5by12.h
+++ keyboards/ortho5by12/ortho5by12.h
@@ -9,7 +9,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *LAYOUT_ortho_1x2uC
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -55,4 +55,3 @@
     { K81, K83, K85, K91, K93, K95 }, \
     { K80, K82, K84, XXX, K92, K94 }  \
 }
-
