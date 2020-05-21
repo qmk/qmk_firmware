4438bdcde124a6f62a241a6c002e22fa4fa66a00
Nathan Vercaemert <nathan.vercaemert@gmail.com>
2020-05-21 13:37:04 -0400

    starting to customize documentation to keymap

--- keyboards/ergodox_ez/keymaps/nathanvercaemert/keymap.c
+++ keyboards/ergodox_ez/keymaps/nathanvercaemert/keymap.c
@@ -335,17 +335,17 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
-  debug_enable=true;
+//   debug_enable=true;
 //   debug_matrix=true;
-  debug_keyboard=true;
+//   debug_keyboard=true;
   //debug_mouse=true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // If console is enabled, it will print the matrix position and status of each key pressed
-#ifdef CONSOLE_ENABLE
-    uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
-#endif
+// #ifdef CONSOLE_ENABLE
+//     uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
+// #endif
   return true;
 }
 
