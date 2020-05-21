2385dc84363efcd79320d32e359dbb54efa7b8bd
HASU <18438880@qq.com>
2020-05-22 07:13:45 +0800

    Update keymap.c

--- keyboards/18bluetoothpad/keymaps/default/keymap.c
+++ keyboards/18bluetoothpad/keymaps/default/keymap.c
@@ -29,13 +29,13 @@
 
 #include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_all(
         KC_PGUP, KC_PGDOWN,
         KC_KP_7, KC_KP_8, KC_KP_9, MO(1),
         KC_P4, KC_P5, KC_P6, KC_KP_PLUS,
         KC_P1, KC_P2, KC_P3, KC_KP_MINUS,
         KC_P0, KC_PDOT,KC_DELETE, KC_KP_ENTER),
-    [1] = LAYOUT(
+    [1] = LAYOUT_all(
         KC_NUMLOCK, KC_CALCULATOR,
         RGB_TOG, RGB_MOD, RGB_M_K, RGB_M_X,
         RGB_SAI, RGB_SAD, RGB_HUI, RGB_HUD,
