7cbb715501f315e1c8cb415bbdbba23e55ac25b4
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2020-05-20 18:26:48 -0700

    Update keyboards/projectkb/signature87/config.h

--- keyboards/projectkb/signature87/config.h
+++ keyboards/projectkb/signature87/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 Jun Wako <wakojun@gmail.com>
+Copyright 2020 MechMerlin 
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
