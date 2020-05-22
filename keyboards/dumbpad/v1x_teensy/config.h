7d2c1f8c42d52adc1013d377dcb3fb06aa698812
Chip <imchipwood@gmail.com>
2020-05-22 16:34:09 -0700

    adding dumbpad build using teensy 2.0 instead of Pro Micro

---
+++ keyboards/dumbpad/v1x_teensy/config.h
@@ -0,0 +1,43 @@
+/*
+Copyright 2020 imchipwood
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define DEVICE_VER      0x0011
+#define DESCRIPTION     4x4 macro/numpad with rotary encoder
+
+/* Column/Row IO definitions */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 5
+#define MATRIX_ROW_PINS { F5, F6, F7, B6 }
+#define MATRIX_COL_PINS { D1, D2, D3, C6, C7 }
+#define UNUSED_PINS
+
+/* Single rotary encoder on left side */
+#define ENCODERS_PAD_A { D7 }
+#define ENCODERS_PAD_B { D0 }
+
+/* Onboard LEDs  */
+#define LED_00 B3
+#define LED_01 B1
+#define LED_02 B6
+
+/* Bootmagic - hold down rotary encoder pushbutton while plugging in to enter bootloader */
+#define BOOTMAGIC_LITE_ROW 3
+#define BOOTMAGIC_LITE_COLUMN 0
