fe7e2b01d797ce05e197cabeb4d687b8a9f28c15
Khader Syed <khader.syed@aicure.com>
2020-05-22 09:37:48 -0400

    switch to newer mechanisms

--- keyboards/gray_studio/space65/keymaps/madhatter/keymap.c
+++ keyboards/gray_studio/space65/keymaps/madhatter/keymap.c
@@ -15,6 +15,13 @@
  */
 #include QMK_KEYBOARD_H
 
+enum keyboard_layers {
+  _QWERTY,
+  _FNM
+};
+
+#define FNM MO(_FNM)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
@@ -31,12 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------------------------------------'
    */
 
-[0] = LAYOUT( \
+[_QWERTY] = LAYOUT( \
         KC_GESC, KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL, KC_HOME, \
         KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,\
         KC_CAPS, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,      KC_ENT,      KC_PGDN,\
         KC_LSFT, KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,\
-        KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC, KC_SPC, KC_SPC,         MO(1), KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT         \
+        KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC, KC_SPC, KC_SPC,         FNM, KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT         \
     ),
 
   /* 1st Layer
@@ -53,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------------------------------------------------------'
    */
 
-[1] = LAYOUT( \
+[_FNM] = LAYOUT( \
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_VOLD, KC_VOLU, KC_MFFD,\
         KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, RGB_SPD,  RGB_SPI, KC_TRNS, KC_TRNS, RESET, KC_MRWD,\
         AG_TOGG, RGB_M_P, RGB_M_G, RGB_M_K, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS,\
