838c2f80e5a5b1ebf85189a94cba7dc03647a974
HASU <18438880@qq.com>
2020-05-22 15:13:00 +0800

    Update keymap.c

--- keyboards/18bluetoothpad/keymaps/default/keymap.c
+++ keyboards/18bluetoothpad/keymaps/default/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_KP_7, KC_KP_8, KC_KP_9, MO(1),
         KC_P4, KC_P5, KC_P6, KC_KP_PLUS,
         KC_P1, KC_P2, KC_P3, KC_KP_MINUS,
-        KC_P0, KC_PDOT,KC_DELETE, KC_KP_ENTER),
-        KC_PGUP, KC_PGDOWN
+        KC_P0, KC_PDOT,KC_DELETE, KC_KP_ENTER,
+        KC_PGUP, KC_PGDOWN),
     [1] = LAYOUT_all(
         RGB_TOG, RGB_MOD, RGB_M_K, RGB_M_X,
         RGB_SAI, RGB_SAD, RGB_HUI, RGB_HUD,
         RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD,
-        RGB_SPI, RGB_SPD, RGB_TOG, KC_TRNS),
-        KC_NUMLOCK, KC_CALCULATOR
+        RGB_SPI, RGB_SPD, RGB_TOG, KC_TRNS,
+        KC_NUMLOCK, KC_CALCULATOR),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
