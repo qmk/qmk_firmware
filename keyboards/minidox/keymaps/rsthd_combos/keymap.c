86a4b6612c6a199d8a542f74a2e079fd0e66ae7e
squizzler <56064906+squizzler@users.noreply.github.com>
2020-05-23 16:43:37 +0100

    Update keyboards/minidox/keymaps/rsthd_combos/keymap.c
    Updated how the layers are defined to reduce firmware bloat
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/minidox/keymaps/rsthd_combos/keymap.c
+++ keyboards/minidox/keymaps/rsthd_combos/keymap.c
@@ -1,14 +1,12 @@
 #include QMK_KEYBOARD_H
 
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _RSTHD 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _RSTHD,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
 
 enum custom_keycodes {
   RSTHD = SAFE_RANGE,
