2fdec9ea4db5a858154f9504bd1aa707be3acb96
Dan Smith <dan.smith.me@gmail.com>
2020-05-23 15:07:21 +0100

    Revert "Userspace"

--- keyboards/crkbd/keymaps/snowkuma/rules.mk
+++
@@ -1,7 +0,0 @@
-RGB_MATRIX_ENABLE = WS2812
-MOUSEKEY_ENABLE = no
-OLED_DRIVER_ENABLE = yes
-LINK_TIME_OPTIMIZATION_ENABLE = yes
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
