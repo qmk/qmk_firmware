4ea132333cb9167abecde4a885ff81248e89e661
Yu-Xi Lim <limyuxi@gmail.com>
2020-05-24 15:45:46 +0800

    Clean up config

--- keyboards/hhkb_lite_2/config.h
+++ keyboards/hhkb_lite_2/config.h
@@ -4,12 +4,12 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
+#define VENDOR_ID       0x1337
+#define PRODUCT_ID      0x88b2
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    PFU
 #define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define DESCRIPTION     HHKB Lite 2
 
 /* key matrix size */
 #define MATRIX_ROWS 8
@@ -23,20 +23,8 @@
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 3
-#endif
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
 /* key combination for command */
 #define IS_COMMAND() ( \
@@ -45,14 +33,5 @@
 
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
-
-
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
 
 #endif
