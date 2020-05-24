a892994e90777201f5a9338a81cea6b11005934c
zvecr <git@zvecr.com>
2020-05-24 22:47:36 +0100

    Apply suggestions from code review

--- keyboards/zvecr/split_blackpill/split_blackpill.c
+++ keyboards/zvecr/split_blackpill/split_blackpill.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 zvecr<git@zvecr.com>
+/* Copyright 2020 zvecr <git@zvecr.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
