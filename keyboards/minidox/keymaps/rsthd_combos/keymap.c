a2f51a54b6427b87d8cce9f1cebe85da54e0b9dc
squizzler <56064906+squizzler@users.noreply.github.com>
2020-05-24 06:43:36 +0100

    Update keyboards/minidox/keymaps/rsthd_combos/keymap.c
    Removed backslash from each line of the layers in accordance with current convention.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/minidox/keymaps/rsthd_combos/keymap.c
+++ keyboards/minidox/keymaps/rsthd_combos/keymap.c
@@ -81,34 +81,29 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
-[_RSTHD] = LAYOUT( \
-
-    KC_J,    KC_C,    KC_Y,     KC_F, KC_K,              KC_Z,  KC_L,   KC_COMM,  KC_U,   KC_Q, \
-    KC_R,    KC_S,    KC_T,     KC_H, KC_D,              KC_M,  KC_N,   KC_A,     KC_I ,  KC_O,   \
-    LCTL_T(KC_SLSH), LALT_T(KC_V), LGUI_T(KC_G), KC_P, KC_B,      KC_X,  KC_W, RGUI_T(KC_DOT), RALT_T(KC_SCLN),RCTL_T(KC_MINS), \
-          OSM(MOD_LSFT), LT(_LOWER, KC_BSPC), KC_E,      KC_SPC, LT(_RAISE, KC_ENT),  OSM(MOD_LSFT)  \
+[_RSTHD] = LAYOUT(
+    KC_J,    KC_C,    KC_Y,     KC_F, KC_K,              KC_Z,  KC_L,   KC_COMM,  KC_U,   KC_Q,
+    KC_R,    KC_S,    KC_T,     KC_H, KC_D,              KC_M,  KC_N,   KC_A,     KC_I ,  KC_O,
+    LCTL_T(KC_SLSH), LALT_T(KC_V), LGUI_T(KC_G), KC_P, KC_B,      KC_X,  KC_W, RGUI_T(KC_DOT), RALT_T(KC_SCLN),RCTL_T(KC_MINS),
+          OSM(MOD_LSFT), LT(_LOWER, KC_BSPC), KC_E,      KC_SPC, LT(_RAISE, KC_ENT),  OSM(MOD_LSFT)
 ),
-
-[_RAISE] = LAYOUT( \
-  KC_PAST, KC_7,    KC_8,   KC_9,  KC_PLUS,       KC_NUBS, LALT(KC_2), _______, LALT(KC_4), KC_DLR,      \
-  KC_CIRC, KC_4,    KC_5,   KC_6,    KC_EQL,      LSFT(KC_NUBS), KC_GRV,   KC_TILD,  KC_PIPE, KC_BSLS,       \
-  _______, RALT_T(KC_1), KC_2, KC_3,KC_PERC,      _______, _______,    _______, _______, _______, \
-                  _______, MO(_ADJUST), KC_0,     _______, _______, _______                    \
+[_RAISE] = LAYOUT(
+  KC_PAST, KC_7,    KC_8,   KC_9,  KC_PLUS,       KC_NUBS, LALT(KC_2), _______, LALT(KC_4), KC_DLR,
+  KC_CIRC, KC_4,    KC_5,   KC_6,    KC_EQL,      LSFT(KC_NUBS), KC_GRV,   KC_TILD,  KC_PIPE, KC_BSLS,
+  _______, RALT_T(KC_1), KC_2, KC_3,KC_PERC,      _______, _______,    _______, _______, _______,
+                  _______, MO(_ADJUST), KC_0,     _______, _______, _______
 ),
-
-
-[_LOWER] = LAYOUT( \
-   _______, KC_LBRC, KC_QUES, KC_RBRC, KC_BRMU,         KC_PGUP,  KC_HOME, KC_UP,   KC_END,  KC_VOLU,   \
-   KC_ESC, KC_LPRN,  KC_EXLM, KC_RPRN, KC_BRMD,         KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_VOLD,   \
-   _______, _______, _______, _______, _______,         _______, KC_MPRV, KC_MPLY, KC_MNXT, KC__MUTE,      \
-                      _______, _______, _______,        KC_DEL,  MO(_ADJUST),  _______                    \
+[_LOWER] = LAYOUT(
+   _______, KC_LBRC, KC_QUES, KC_RBRC, KC_BRMU,         KC_PGUP,  KC_HOME, KC_UP,   KC_END,  KC_VOLU,
+   KC_ESC, KC_LPRN,  KC_EXLM, KC_RPRN, KC_BRMD,         KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_VOLD,
+   _______, _______, _______, _______, _______,         _______, KC_MPRV, KC_MPLY, KC_MNXT, KC__MUTE,
+                      _______, _______, _______,        KC_DEL,  MO(_ADJUST),  _______
 ),
-
-[_ADJUST] =  LAYOUT( \
-  _______,     KC_F7, KC_F8, KC_F9,  SCRCAP,        _______, KC_F10,  KC_F11, KC_F12, _______, \
-  _______,     KC_F1, KC_F2, KC_F3, _______,        _______, KC_F4, KC_F5,  KC_F6, _______,     \
-  OSM(MOD_LCTL),_______, _______, _______, RESET,    KILL, _______, _______,  _______, OSM(MOD_RCTL), \
-                    _______, _______, _______,       _______, _______,  _______                    \
+[_ADJUST] =  LAYOUT(
+  _______,     KC_F7, KC_F8, KC_F9,  SCRCAP,        _______, KC_F10,  KC_F11, KC_F12, _______,
+  _______,     KC_F1, KC_F2, KC_F3, _______,        _______, KC_F4, KC_F5,  KC_F6, _______,
+  OSM(MOD_LCTL),_______, _______, _______, RESET,    KILL, _______, _______,  _______, OSM(MOD_RCTL),
+                    _______, _______, _______,       _______, _______,  _______
 )
 
 };
