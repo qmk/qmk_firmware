3ecba31dd9787c00edc581ea10afa4d79546484a
Danny Nguyen <danny@keeb.io>
2020-05-26 16:07:25 -0400

    Add two underglow LEDs

--- keyboards/keebio/bdn9/rev2/config.h
+++ keyboards/keebio/bdn9/rev2/config.h
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN B15
 #ifdef RGB_DI_PIN
-#    define RGBLED_NUM 9
+#    define RGBLED_NUM 11
 #    define RGBLIGHT_HUE_STEP 8
 #    define RGBLIGHT_SAT_STEP 8
 #    define RGBLIGHT_VAL_STEP 8
