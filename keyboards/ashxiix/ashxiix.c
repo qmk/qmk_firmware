4f68e658ce6c80b0c4ff084cfb93757d7f572cf8
sh-xguitar <ibanez_shine@yahoo.com>
2020-05-28 04:56:14 +0700

    Update ashxiix.c

--- keyboards/ashxiix/ashxiix.c
+++ keyboards/ashxiix/ashxiix.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 angelbirth
+/* Copyright 2020 sh_xguitar
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
