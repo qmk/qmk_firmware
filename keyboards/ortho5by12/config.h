25191b44b81a003ff9a4109daa0d2e785b282fd5
itsnoteasy <pyrosoma1@yahoo.ie>
2020-05-29 00:06:10 +0100

    Update config.h

--- keyboards/ortho5by12/config.h
+++ keyboards/ortho5by12/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Takuya Urakawa (dm9records.com)
+Copyright 2020 itsnoteasy
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
