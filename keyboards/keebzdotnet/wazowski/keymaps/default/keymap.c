0e7b91373a7e499942cc94102154be580d9ef277
Keebz Dot Net <64744703+keebzdotnet@users.noreply.github.com>
2020-05-27 19:09:13 -0700

    Update keyboards/keebzdotnet/wazowski/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/keebzdotnet/wazowski/keymaps/default/keymap.c
+++ keyboards/keebzdotnet/wazowski/keymaps/default/keymap.c
@@ -25,6 +25,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_A, KC_B, KC_C,KC_D, KC_E,
         KC_F, KC_G, KC_H, KC_I, KC_J,
-        KC_K, KC_L, KC_NO, KC_M, KC_N
+        KC_K, KC_L, KC_M, KC_N
     ),
 };
