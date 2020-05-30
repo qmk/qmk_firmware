460bd9e0c609aa969ce202fd3062c56107067d01
pabile <1162412+pabile@users.noreply.github.com>
2020-05-30 21:19:44 +0800

    Add files via upload

---
+++ keyboards/pabile/p40/keymaps/ortho/config.h
@@ -0,0 +1,18 @@
+
+#include "../../config.h"
+
+/* pin-out for PCB v2 */
+#undefine MATRIX_ROW_PINS
+#define MATRIX_ROW_PINS { D1, D0, F4, F5 }
+
+#unfine MATRIX_COL_PINS
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B2, B3, B1, F7, F6 }
+
+/* Backlight and RGB
+#define RGB_DI_PIN D3
+#undef RGBLED_NUM
+#define RGBLED_NUM 40
+
+#define BACKLIGHT_PIN B5
+#define BACKLIGHT_LEVELS 3
+*/
