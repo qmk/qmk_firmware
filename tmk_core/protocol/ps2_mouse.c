0351a32f5072ff52fc081ed3eba79219757c6901
juank <juank@fktech.net>
2020-05-30 09:33:19 -0300

    Variable (mouse_report->buttons): avoid setting twice https://github.com/qmk/qmk_firmware/pull/9205#discussion_r430783884

--- tmk_core/protocol/ps2_mouse.c
+++ tmk_core/protocol/ps2_mouse.c
@@ -147,13 +147,13 @@ static inline void ps2_mouse_convert_report_to_hid(report_mouse_t *mouse_report)
     mouse_report->x = X_IS_NEG ? ((!X_IS_OVF && -127 <= mouse_report->x && mouse_report->x <= -1) ? mouse_report->x : -127) : ((!X_IS_OVF && 0 <= mouse_report->x && mouse_report->x <= 127) ? mouse_report->x : 127);
     mouse_report->y = Y_IS_NEG ? ((!Y_IS_OVF && -127 <= mouse_report->y && mouse_report->y <= -1) ? mouse_report->y : -127) : ((!Y_IS_OVF && 0 <= mouse_report->y && mouse_report->y <= 127) ? mouse_report->y : 127);
 
-    // remove sign and overflow flags
-    mouse_report->buttons &= PS2_MOUSE_BTN_MASK;
-
 #ifdef PS2_MOUSE_INVERT_BUTTONS
     // swap left & rigth buttons & remove sign and overflow flags
-    uint8_t btns_change = ((mouse_report->buttons >> PS2_MOUSE_BTN_LEFT) ^ (mouse_report->buttons >> PS2_MOUSE_BTN_RIGHT)) & 1;
-    mouse_report->buttons ^= ((btns_change << PS2_MOUSE_BTN_LEFT) | (btns_change << PS2_MOUSE_BTN_RIGHT));
+    uint8_t btns_change   = ((mouse_report->buttons >> PS2_MOUSE_BTN_LEFT) ^ (mouse_report->buttons >> PS2_MOUSE_BTN_RIGHT)) & 1;
+    mouse_report->buttons = (mouse_report->buttons ^ ((btns_change << PS2_MOUSE_BTN_LEFT) | (btns_change << PS2_MOUSE_BTN_RIGHT))) & PS2_MOUSE_BTN_MASK;
+#else
+    // remove sign and overflow flags
+    mouse_report->buttons &= PS2_MOUSE_BTN_MASK;
 #endif
 
 #ifdef PS2_MOUSE_INVERT_X
