f133393aee47c31e7d9b971a8750408a0e022250
Jay Greco <jayv.greco@gmail.com>
2020-05-30 20:48:18 -0700

    Update VID for VIA compatibility

--- keyboards/nullbitsco/nibble/config.h
+++ keyboards/nullbitsco/nibble/config.h
@@ -22,7 +22,7 @@
 #define KEYBOARD_MASTER
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x6E61
 #define PRODUCT_ID      0x6060
 
 #define DEVICE_VER      0x0001
