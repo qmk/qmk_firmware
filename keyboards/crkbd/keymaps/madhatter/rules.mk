408e62cb7763b51c7cc3c391da140b4359c7291a
Sid Carter <sidcarter@users.noreply.github.com>
2020-06-01 18:28:38 -0400

    Update keyboards/crkbd/keymaps/madhatter/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/crkbd/keymaps/madhatter/rules.mk
+++ keyboards/crkbd/keymaps/madhatter/rules.mk
@@ -8,7 +8,4 @@ SRC +=  ./lib/glcdfont.c \
         ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+        ./lib/keylogger.c
