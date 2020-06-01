75cac4b0a45f24fa768fa617362aae591744f885
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-06-01 20:53:28 -0300

    reactivate mouse keys to test if mac can sleep now

--- keyboards/lily58/keymaps/rhruiz/rules.mk
+++ keyboards/lily58/keymaps/rhruiz/rules.mk
@@ -3,7 +3,7 @@
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
