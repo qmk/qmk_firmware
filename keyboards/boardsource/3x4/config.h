f456e503c61195a5d8aae0fd9aa4542f42e1fb53
cole smith <38364556+daysgobye@users.noreply.github.com>
2020-05-31 21:28:14 -0700

    Update keyboards/boardsource/3x4/config.h
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- keyboards/boardsource/3x4/config.h
+++ keyboards/boardsource/3x4/config.h
@@ -16,16 +16,6 @@
 #define MATRIX_ROWS 3   
 #define MATRIX_COLS 4
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
-*/
 #define MATRIX_ROW_PINS { F7, F6, F5,}
 #define MATRIX_COL_PINS {B6, B2, B3, B1}
 
