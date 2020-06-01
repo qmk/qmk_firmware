9e44bf56e958afc0b7fd214f90d48b9704c6502b
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-06-01 19:37:39 -0300

    make alt on the right RALT to allow win/linux accents

--- users/rhruiz/layouts/kc_keys.h
+++ users/rhruiz/layouts/kc_keys.h
@@ -16,7 +16,7 @@
 #define KC_SFN1 SPCFN1
 #define KC_BFN2 BKSFN2
 #define KC_EFN2 LT(_FN2, KC_ENTER)
-#define KC_ALBS LALT_T(KC_BSPC)
+#define KC_ALBS RALT_T(KC_BSPC)
 
 #define KC__RST RESET
 #define KC_MISS MISCTRL
