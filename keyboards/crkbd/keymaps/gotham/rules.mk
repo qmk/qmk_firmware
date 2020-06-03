769f9b68c9691d647a63c279e14b6af488983c88
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-06-03 00:18:51 -0700

    Add custom corne keymap

---
+++ keyboards/crkbd/keymaps/gotham/rules.mk
@@ -0,0 +1,22 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+BOOTMAGIC_ENABLE           = no
+MOUSEKEY_ENABLE            = no
+EXTRAKEY_ENABLE            = yes
+CONSOLE_ENABLE             = no
+COMMAND_ENABLE             = no
+NKRO_ENABLE                = no
+BACKLIGHT_ENABLE           = no
+MIDI_ENABLE                = no
+AUDIO_ENABLE               = yes
+UNICODE_ENABLE             = no
+BLUETOOTH_ENABLE           = no
+RGBLIGHT_ENABLE            = yes
+SWAP_HANDS_ENABLE          = no
+RGB_MATRIX_ENABLE          = no
+HAPTIC_ENABLE              = no
+SLEEP_LED_ENABLE           = no
+OLED_DRIVER_ENABLE         = yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
