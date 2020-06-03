1e026a43d272809cc672f7a1a94e8f64b2653d99
Michael Stapelberg <michael@stapelberg.de>
2020-06-03 22:26:01 +0200

    add kinesis/kint36
    This moves the config_common.h into the files that include ../config.h,
    so that the kint36/config.h does not include it (which would cause
    compilation errors).

--- keyboards/kinesis/config.h
+++ keyboards/kinesis/config.h
@@ -18,8 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define MANUFACTURER    You
