7721c7a8dca69661bab5b0722e5f872132e94224
Frothy <frothykillskittens@gmail.com>
2020-06-02 22:16:01 -0300

    Removed unnecessary variables

--- keyboards/percent/canoe/keymaps/via/keymap.c
+++ keyboards/percent/canoe/keymaps/via/keymap.c
@@ -17,10 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-#define _BL 0
-#define _FL 1
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_65_ansi_blocker(
