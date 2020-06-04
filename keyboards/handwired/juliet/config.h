f1fab10bf1f906195dcdc28ae0e5cded88a9bfaa
Derek <dschmell91@gmail.com>
2020-06-03 20:35:38 -0400

    Update config.h

--- keyboards/handwired/juliet/config.h
+++ keyboards/handwired/juliet/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 coseyfannitutti
+Copyright 2020 na-cly
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
