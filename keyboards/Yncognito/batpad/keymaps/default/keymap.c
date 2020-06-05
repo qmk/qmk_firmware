0cde42e57d605a35cb69a725a84d00a8ca756c70
qunguyentk <40719920+qunguyentk@users.noreply.github.com>
2020-06-05 18:45:46 +0700

    Update keyboards/Yncognito/batpad/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/Yncognito/batpad/keymaps/default/keymap.c
+++ keyboards/Yncognito/batpad/keymaps/default/keymap.c
@@ -16,7 +16,8 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_ortho_2x4(
+
         KC_Q,    KC_W,    KC_E,    KC_R,
         KC_LGUI, KC_SPC,  RGB_TOG, RGB_MOD
     ),
