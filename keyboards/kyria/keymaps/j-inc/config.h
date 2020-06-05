0f821ef95be20a26fa34179362aea82e840b5dda
James Incandenza <64759429+j-inc@users.noreply.github.com>
2020-06-05 12:20:24 -0400

    Update keyboards/kyria/keymaps/j-inc/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kyria/keymaps/j-inc/config.h
+++ keyboards/kyria/keymaps/j-inc/config.h
@@ -29,13 +29,6 @@
 
 #define MASTER_RIGHT
 
-// Space saving
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 #define NO_ACTION_ONESHOT
