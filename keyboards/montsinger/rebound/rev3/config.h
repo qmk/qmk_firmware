9b315c5f6fda9cfd9b7cc5a984042a13ad83d656
Rossman360 <rmontsinger@gmail.com>
2020-06-05 12:39:20 -0400

    now we done broke master

--- keyboards/montsinger/rebound/rev3/config.h
+++ keyboards/montsinger/rebound/rev3/config.h
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 12
+#define MATRIX_ROWS 7
+#define MATRIX_COLS 8
 
 /*
  * Keyboard Matrix Assignments
@@ -42,9 +42,9 @@ diode)
  *
  */
 
-#define MATRIX_ROW_PINS { F4, F5, D1, D0, B0 }
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, B6, B2, B3, B1, F7, F6 }
-#define UNUSED_PINS { }
+#define MATRIX_ROW_PINS { D1, D0, D4, C6, F7, F6, F5, F4 }
+#define MATRIX_COL_PINS { D7, E6, B4, B5, B2, B3, B1 }
+#define UNUSED_PINS { B6 }
 
 #define ENCODERS_PAD_A { D3 }
 #define ENCODERS_PAD_B { D2 }
