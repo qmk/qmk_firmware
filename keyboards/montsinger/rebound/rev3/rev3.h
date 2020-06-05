b9dff534431c41b47f052816c08697d3a6141f72
Rossman360 <rmontsinger@gmail.com>
2020-06-05 17:14:03 -0400

    had encoder pins reversed

--- keyboards/montsinger/rebound/rev3/rev3.h
+++ keyboards/montsinger/rebound/rev3/rev3.h
@@ -27,19 +27,17 @@
  */
 
 #define LAYOUT_ortho_4x12( \
-    K00, K70, K01, K71, K02, K72, K04, K74, K05, K75, K06, K76, \
-    K10, K60, K11, K61, K12, K62, K14, K64, K15, K65, K16, K66, \
-    K20, K50, K21, K51, K22, K52, K24, K54, K25, K55, K26, K56,\
-    K30, K40, K31, K41, K32, K42, K34, K44, K35, K45, K36, K46\
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,\
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B\
     ) { \
-  { K00, K01, K02, K04, KA6, KA7}, \
-  { K10, K11, K12, K14, KB5, KB6, KB7 }, \
-  { K20, K21, K22, K24, KC5, KC6, KC7 }, \
-  { K30, K31, K32, K34, KD5, KD6, KD7 }, \
-  { K40, K41, K42, K44, KE5, KE6, KE7 }, \
-  { K50, K51, K52, K54, KF5, KF6, KF7 }, \
-  { K60, K61, K62, K67, KG5, KG6, KG7 }, \
-  { K70, K71, K72, K74, KH5, KH6, KH7 } \}
+  { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B   }, \
+  { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B   }, \
+  { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B   }, \
+  { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B   }, \
+  { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }  \
+}
 
 #define LAYOUT_all( \
     K00, K01, K02, K03, K04, K05,      K06, K07, K08, K09, K0A, K0B, \
