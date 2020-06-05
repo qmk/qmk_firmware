e597a4f73605686640043ee7780ed5a15a6a6dd5
floof <floof@pop-os.localdomain>
2020-06-05 13:11:01 -0700

    IDK what I did here but it was probably to modify some EU ISO Files so shit works

--- keyboards/EU_ISOlation/keymaps/FloofBS/keymap.c
+++ keyboards/EU_ISOlation/keymaps/FloofBS/keymap.c
@@ -7,12 +7,38 @@ enum layers {
     _FN3
 };
 
+enum combos {
+    AB_ESC = 0,
+    JA_DEL,
+    JR_VOLU,
+    RB_VOLD,
+    WOMBO
+};
+
+enum custom_keycodes {
+    womboComboString = SAFE_RANGE
+};
+
+const uint16_t PROGMEM ab_combo[] = { KC_F1, KC_F3, COMBO_END };
+const uint16_t PROGMEM ja_combo[] = { KC_F2, KC_F3, COMBO_END };
+const uint16_t PROGMEM jr_combo[] = { KC_F1, KC_F2, COMBO_END };
+const uint16_t PROGMEM rb_combo[] = { KC_F3, KC_F4, COMBO_END };
+const uint16_t PROGMEM wombo_combo[] = { KC_F1, KC_F4, KC_F3, KC_F2, COMBO_END };
+
+combo_t key_combos[COMBO_COUNT] = {
+    [AB_ESC] = COMBO(ab_combo, KC_ESC),
+    [JA_DEL] = COMBO(ja_combo, KC_DEL),
+    [JR_VOLU] = COMBO(jr_combo, KC_VOLU),
+    [RB_VOLD] = COMBO(rb_combo, KC_VOLD),
+    [WOMBO] = COMBO(wombo_combo, womboComboString)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_EUISO_BIGSPACE(
-                        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-        MO(_FN1), MO(_FN3), KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, LT(_FN2, KC_QUOT),
-        MO(_FN2), KC_CAPS,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, LT(_FN1, KC_ENT),
-                        KC_LCTL, KC_LALT,                          KC_SPC,                                  KC_LGUI, MO(_FN3)
+                         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_F1,   KC_F2,  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, LT(_FN2, KC_QUOT),
+        KC_F3,   KC_F4,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, LT(_FN1, KC_ENT),
+                         MO(_FN3),KC_LALT,                        LT(_FN2, KC_SPC),                                   KC_LGUI, KC_RCTL
     ),
 
     [_FN1] = LAYOUT_EUISO_BIGSPACE(
@@ -30,9 +56,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FN3] = LAYOUT_EUISO_BIGSPACE(
-                          _______, _______,   KC_UP, _______, _______, _______, _______,    KC_F1,   KC_F2,   KC_F3,  KC_F4,  _______,
-        _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______,    KC_F5,   KC_F6,   KC_F7,  KC_F8,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,    KC_F9,   KC_F10,  KC_F11, KC_F12, _______,
-                            RESET, _______,                         _______,                                        _______,  _______
+                          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_F9,  KC_F10,  KC_F11, KC_F12,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT,  KC_DOWN,  KC_UP, KC_RGHT, _______,  _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
+                          _______,   RESET,                         _______,                                        _______,  _______
     )
+};
+
+// Fix this shitpost at some point. It is supposed to do the shitpost but for some reason comes out to e
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case womboComboString:
+      if (record->event.pressed) {
+        // when this shitpost is pressed
+        SEND_STRING("HAPPY FEET!! WOMBO COMBO!!");
+      } else {
+        SEND_STRING("OH OH OH OH OH!!!!");
+      }
+      break;
+  }
+  return true;
 };
