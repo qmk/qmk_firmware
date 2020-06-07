9590f00f9d49ac16dfa9c57db3a86d207d3b26b1
pabile <1162412+pabile@users.noreply.github.com>
2020-06-07 19:54:06 +0800

    typo

--- keyboards/pabile/p40/keymaps/ortho/config.h
+++ keyboards/pabile/p40/keymaps/ortho/config.h
@@ -2,10 +2,10 @@
 #include "../../config.h"
 
 /* pin-out for PCB v2 */
-#undefine MATRIX_ROW_PINS
+#undef MATRIX_ROW_PINS
 #define MATRIX_ROW_PINS { D1, D0, F4, F5 }
 
-#unfine MATRIX_COL_PINS
+#undef MATRIX_COL_PINS
 #define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B2, B3, B1, F7, F6 }
 
 /* Backlight and RGB
