b21f1721548bdcf51fe7871f2200d4338cc6b817
Maarten Dekkers <maartenwut@gmail.com>
2020-06-08 09:33:46 +0200

    Update keyboards/maartenwut/eon75/eon75.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/maartenwut/eon75/eon75.c
+++ keyboards/maartenwut/eon75/eon75.c
@@ -16,16 +16,10 @@
 #include "eon75.h"
 
 void matrix_init_kb(void) {
-	// put your keyboard start-up code here
-	// runs once when the firmware starts up
-	matrix_init_user();
-	led_init_ports();
-};
-
-void matrix_scan_kb(void) {
-	// put your looping keyboard code here
-	// runs every cycle (a lot)
-	matrix_scan_user();
+    // put your keyboard start-up code here
+    // runs once when the firmware starts up
+    matrix_init_user();
+    led_init_ports();
 };
 
 void led_init_ports(void) {
