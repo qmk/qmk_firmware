e642a7df937a9e0811e24f25069b1faf464b1afc
Jack Kester <Jackrabbit.kester@gmail.com>
2020-06-10 10:39:07 -0500

    add copyright information

--- keyboards/pikatea/keymaps/default/keymap.c
+++ keyboards/pikatea/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Pikatea Company
+/* Copyright 2020 Pikatea
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,23 +13,20 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include QMK_KEYBOARD_H
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* LAYER 0
  * ,---------------------------------------,
  * |   1   |   2   |   3   |   4   |   5   |  Knob
  * '---------------------------------------'
  */
-[0] = LAYOUT(
-  KC_NO,       KC_NO,      KC_MEDIA_PREV_TRACK,       KC_MEDIA_PLAY_PAUSE,      KC_MEDIA_NEXT_TRACK
-),
-[1] = LAYOUT(
-  KC_NO,       KC_NO,      KC_NO,       KC_NO,      KC_NO
-),
-
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT( \
+    KC_NO,       KC_NO,      KC_MEDIA_PREV_TRACK,       KC_MEDIA_PLAY_PAUSE,      KC_MEDIA_NEXT_TRACK
+  ),
+  [1] = LAYOUT( \
+    KC_NO,       KC_NO,      KC_NO,       KC_NO,      KC_NO
+  ),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
