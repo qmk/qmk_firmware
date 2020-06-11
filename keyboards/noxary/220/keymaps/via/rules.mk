52aef9090a390428e098ce95950eeac9def83ff7
Rozakiin <6567067+Rozakiin@users.noreply.github.com>
2020-06-11 14:08:48 +0100

    Update keyboards/noxary/220/keymaps/via/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/noxary/220/keymaps/via/rules.mk
+++ keyboards/noxary/220/keymaps/via/rules.mk
@@ -1,6 +1,5 @@
-MCU = atmega32u2
 VIA_ENABLE = yes
 MOUSEKEY_ENABLE = no
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
-LINK_TIME_OPTIMIZATION_ENABLE = yes
+LTO_ENABLE = yes
