dc2db5b16b849642c3ede6a83c6402c24c4f2a35
Danny Nguyen <danny@keeb.io>
2020-06-11 13:22:09 -0400

    Setup keymaps, layouts, and encoders

--- keyboards/keebio/kbo5000/rev1/config.h
+++ keyboards/keebio/kbo5000/rev1/config.h
@@ -34,12 +34,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 // wiring of each half
 #define MATRIX_ROW_PINS { B1, B2, C7, B4, D7, B0 }
-#define MATRIX_COL_PINS { F4, F1, F0, B7, B3, D2, D3, D5, D4, F1 }
+#define MATRIX_COL_PINS { F4, NO_PIN, F0, B7, B3, D2, D3, D5, D4, NO_PIN }
 #define MATRIX_ROW_PINS_RIGHT { B3, B2, B6, B4, D7, B0 }
 #define MATRIX_COL_PINS_RIGHT { F1, F0, F4, F5, F6, D5, D2, D3, B7, B1 }
 #define SPLIT_HAND_PIN F7
-#define ENCODERS_PAD_A { F5 }
-#define ENCODERS_PAD_B { F6 }
+#define ENCODERS_PAD_A { F5, NO_PIN }
+#define ENCODERS_PAD_B { F6, NO_PIN }
 #define ENCODERS_PAD_A_RIGHT { D6, C7 }
 #define ENCODERS_PAD_B_RIGHT { D4, C6 }
 
@@ -63,7 +63,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLED_NUM 16    // Number of LEDs
 #define RGBLED_SPLIT { 8, 8 }
 #define RGBLIGHT_LED_MAP { 1, 2, 3, 12, 13, 14, 15, 0, 7, 6, 5, 4, 11, 10, 9, 8 }
-
-// Set right column, left macro, 1.75u shift, and 5x1u on by default
-#define VIA_EEPROM_LAYOUT_OPTIONS_DEFAULT 0x00D4
-#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
