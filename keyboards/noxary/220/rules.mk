56e82de114af3add41dd5831fb74aa62f382d3dc
Rozakiin <6567067+Rozakiin@users.noreply.github.com>
2020-06-11 14:09:19 +0100

    Update keyboards/noxary/220/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/noxary/220/rules.mk
+++ keyboards/noxary/220/rules.mk
@@ -15,7 +15,7 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
