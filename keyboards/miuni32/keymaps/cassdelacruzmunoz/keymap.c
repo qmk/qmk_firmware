6086330fb439910d94b0fdc766efb4c8b5150580
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2020-06-14 09:42:35 -0400

    Update keyboards/miuni32/keymaps/cassdelacruzmunoz/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/miuni32/keymaps/cassdelacruzmunoz/keymap.c
+++ keyboards/miuni32/keymaps/cassdelacruzmunoz/keymap.c
@@ -90,49 +90,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-  if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_KANA)) {
-
-  } else {
-
-  }
-
-}
 // Light LEDs 0 through 16 in white when keyboard layer 0 is active
 const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 17, HSV_WHITE}
