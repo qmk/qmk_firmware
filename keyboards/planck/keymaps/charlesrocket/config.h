9e9d4a59e0b2a2eb458a7964edcce17346aacd23
-k <slowdive@me.com>
2020-06-14 12:32:10 -0700

    Update config.h

--- keyboards/planck/keymaps/charlesrocket/config.h
+++ keyboards/planck/keymaps/charlesrocket/config.h
@@ -4,6 +4,17 @@
 #define STARTUP_SONG SONG(PLANCK_SOUND)
 #endif
 
+/*
+ * MIDI options
+ */
+
+/* Prevent use of disabled MIDI features in the keymap */
+//#define MIDI_ENABLE_STRICT 1
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
 #define MIDI_BASIC
 #define AUDIO_CLICKY
 #define AUDIO_CLICKY_FREQ_DEFAULT 2354.0f
@@ -11,10 +22,17 @@
 #define AUDIO_CLICKY_FREQ_RANDOMNESS 0.05f
 #define DAC_SAMPLE_MAX 9350U
 
-#define ENCODER_RESOLUTION 4
-
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
 */
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+// Most tactile encoders have detents every 4 stages
+#define ENCODER_RESOLUTION 4
 #define ORYX_CONFIGURATOR
