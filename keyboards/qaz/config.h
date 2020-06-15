8b26df801c57fd5a4957c47c63cbc7a2fdd1142b
TJ <tom.campie@gmail.com>
2020-06-14 22:21:17 -0500

    Via initial

--- keyboards/qaz/config.h
+++ keyboards/qaz/config.h
@@ -3,11 +3,11 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x3CAE
+#define VENDOR_ID 0x7431
+#define PRODUCT_ID 0x0001
 #define DEVICE_VER 0x0001
 #define MANUFACTURER whydobearsexplode
-#define PRODUCT qaz
+#define PRODUCT qaz keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 6
