d17f2d7a7207da19bcf606966c8b1a0cfbc72064
Alabahuy <rezzaadio@gmail.com>
2020-06-15 08:42:21 +0700

    Update config.h

--- keyboards/rart/rartpad/config.h
+++ keyboards/rart/rartpad/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID		0xFEED  // "AL" 
-#define PRODUCT_ID 		0x4520	// "PAD"
+#define VENDOR_ID		0x414C  // "AL" 
+#define PRODUCT_ID 		0x0020	// "PAD"
 #define DEVICE_VER 		0x0001
 #define MANUFACTURER    Alabahuy
 #define PRODUCT         RARTPAD
