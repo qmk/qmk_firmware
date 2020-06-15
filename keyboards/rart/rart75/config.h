71abf9517fad4b3e041f76bc67c6bd7fed606b01
Alabahuy <rezzaadio@gmail.com>
2020-06-15 08:41:53 +0700

    Update config.h

--- keyboards/rart/rart75/config.h
+++ keyboards/rart/rart75/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID		0xFEED  // "AL"
-#define PRODUCT_ID 		0x6675  // "75"
+#define VENDOR_ID		0x414C  // "AL"
+#define PRODUCT_ID 		0x0075  // "75"
 #define DEVICE_VER 		0x0001
 #define MANUFACTURER    Alabahuy
 #define PRODUCT         RART75
