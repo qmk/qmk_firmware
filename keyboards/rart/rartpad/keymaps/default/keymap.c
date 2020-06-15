d7f3609d6b78031439092e10e9ab2ecbc96021ab
Alabahuy <rezzaadio@gmail.com>
2020-06-15 08:52:50 +0700

    Update keymap.c

--- keyboards/rart/rartpad/keymaps/default/keymap.c
+++ keyboards/rart/rartpad/keymaps/default/keymap.c
@@ -20,19 +20,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT_all(
-		KC_NLCK, KC_PSLS, KC_PAST, KC_TRNS, \
-		KC_P7,   KC_P8,   KC_P9,   KC_PEQL, \
-		KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
-		KC_P1,   KC_P2,   KC_P3,   KC_PMNS, \
-		MO(1),   KC_0,    KC_PDOT, KC_PENT\
+		KC_NLCK, KC_PSLS, KC_PAST, KC_TRNS,
+		KC_P7,   KC_P8,   KC_P9,   KC_PEQL,
+		KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+		MO(1),   KC_0,    KC_PDOT, KC_PENT
 	),
   
 	[1] = LAYOUT_all(
-		KC_TRNS, RGB_HUI, RGB_HUD,  KC_TRNS, \
-		RGB_SAI, RGB_SAD, KC_MNXT,  KC_MPRV, \
-		RGB_VAI, RGB_VAD, KC_MSTP,  KC_MPLY, \
-		KC_COPY, KC_PSTE, KC_MYCM,  KC_CALC, \
-		KC_TRNS, RGB_TOG, RGB_RMOD, RGB_MOD  \
+		KC_TRNS, RGB_HUI, RGB_HUD,  KC_TRNS, 
+		RGB_SAI, RGB_SAD, KC_MNXT,  KC_MPRV, 
+		RGB_VAI, RGB_VAD, KC_MSTP,  KC_MPLY, 
+		KC_COPY, KC_PSTE, KC_MYCM,  KC_CALC, 
+		KC_TRNS, RGB_TOG, RGB_RMOD, RGB_MOD 
 	)
 };
 
