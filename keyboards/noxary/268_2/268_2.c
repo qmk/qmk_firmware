2215635915a952a8abd3c158aa72ae426ee1f11f
Rozakiin <ruari.armstrong@gmail.com>
2020-06-15 13:32:54 +0100

    Remove commented code

--- keyboards/noxary/268_2/268_2.c
+++ keyboards/noxary/268_2/268_2.c
@@ -28,15 +28,3 @@ bool led_update_kb(led_t led_state) {
     }
     return true;
 }
-
-
-// void led_set_kb(uint8_t usb_led) {
-//     if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-//         setPinOutput(B0);
-//         writePinHigh(B0);
-//     } else {
-//         setPinInput(B0);
-//     }
-
-//     led_set_user(usb_led);
-// }
