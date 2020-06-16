587c99292fe278ed78159fbd79e1926453e81676
Dan <loksonarius@users.noreply.github.com>
2020-06-16 13:59:09 -0400

    Update keyboards/keebio/iris/keymaps/dcompact/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keebio/iris/keymaps/dcompact/keymap.c
+++ keyboards/keebio/iris/keymaps/dcompact/keymap.c
@@ -196,7 +196,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
          layer_off(_GAME);
        }
        return false;
-       break;
      case LNUM:
        if (record->event.pressed) {
          layer_on(_LNUM);
