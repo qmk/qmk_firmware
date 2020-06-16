6a995afda89eeec3fe441e5297b6f6f8f5a5a183
Yasha Mostofi <yasha.mostofi@gmail.com>
2020-06-15 20:52:09 -0500

    Revert default

--- keyboards/lets_split/keymaps/yasha/config.h
+++ keyboards/lets_split/keymaps/yasha/config.h
@@ -34,3 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10
