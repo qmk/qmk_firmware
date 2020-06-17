5798bff454933fe212696b0c763d451e80a95a7f
Alabahuy <rezzaadio@gmail.com>
2020-06-17 07:26:22 +0700

    Update keymap.c

--- keyboards/rart/rartpad/keymaps/numpad/keymap.c
+++ keyboards/rart/rartpad/keymaps/numpad/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
 		KC_P4,   KC_P5,   KC_P6,            
 		KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
-		KC_P0,            KC_PDOT,           
+		KC_P0,            KC_PDOT           
 	)
 };
 
