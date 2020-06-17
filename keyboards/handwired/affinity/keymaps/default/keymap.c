534817c250d9e6362d454dfcf17f06bec0ed7328
zk-phi <zk-phi@users.noreply.github.com>
2020-06-17 20:12:42 +0900

    Affinity: Add RAISE layer

--- keyboards/handwired/affinity/keymaps/default/keymap.c
+++ keyboards/handwired/affinity/keymaps/default/keymap.c
@@ -21,7 +21,7 @@
 
 #include "msgeq7.h"
 
-enum layer { DEFAULT };
+enum layer { DEFAULT, RAISE };
 enum custom_keycode { LED_MODE = SAFE_RANGE };
 enum lightshow_pattern { LEVELMETER, LEVELMETER_PEAK, LEVELMETER2, LEVELMETER3, PARTY, PARTY2 };
 
@@ -29,10 +29,16 @@ enum lightshow_pattern pattern = LEVELMETER;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DEFAULT] = LAYOUT(
-    KC_7, KC_8, KC_9, LED_MODE,
+    KC_7, KC_8, KC_9, KC_MINS,
     KC_4, KC_5, KC_6, KC_PLUS,
     KC_1, KC_2, KC_3, KC_ENT,
-    KC_0, KC_0, KC_DOT, RESET
+    KC_0, KC_DOT, KC_TAB, MO(RAISE)
+  ),
+  [RAISE] = LAYOUT(
+    RESET, KC_NO, KC_NO, LED_MODE,
+    KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO
   )
 };
 
