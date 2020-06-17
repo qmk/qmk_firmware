6650b9944322788a5fc96a54a1b9180699eb391c
zk-phi <zk-phi@users.noreply.github.com>
2020-06-17 20:08:46 +0900

    Affinity: Refactor and add patterns

--- keyboards/handwired/affinity/keymaps/default/keymap.c
+++ keyboards/handwired/affinity/keymaps/default/keymap.c
@@ -17,12 +17,13 @@
 
 #define NOISE_FLOOR 50          /* 0 - 2048 */
 #define THRESHOLD 800           /* 0 - 2048 */
+#define LIGHTSHOW_DELAY 150
 
 #include "msgeq7.h"
 
 enum layer { DEFAULT };
 enum custom_keycode { LED_MODE = SAFE_RANGE };
-enum lightshow_pattern { LEVELMETER, LEVELMETER_PEAK, PARTY };
+enum lightshow_pattern { LEVELMETER, LEVELMETER_PEAK, LEVELMETER2, LEVELMETER3, PARTY, PARTY2 };
 
 enum lightshow_pattern pattern = LEVELMETER;
 
@@ -42,7 +43,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       pattern = pattern == LEVELMETER ? (
         LEVELMETER_PEAK
       ) : pattern == LEVELMETER_PEAK ? (
+        LEVELMETER2
+      ) : pattern == LEVELMETER2 ? (
+        LEVELMETER3
+      ) : pattern == LEVELMETER3 ? (
         PARTY
+      ) : pattern == PARTY ? (
+        PARTY2
       ) : (
         LEVELMETER
       );
@@ -56,44 +63,186 @@ void keyboard_post_init_user (void) {
   msgeq7_init();
 }
 
-int _map (int val, int sfrom, int sto, int dfrom, int dto, bool bound) {
+/* ---- utilities */
+
+static inline int _map (int val, int sfrom, int sto, int dfrom, int dto, bool bound) {
   if (val < sfrom) return (bound ? 0 : dfrom);
   else if (val > sto) return (bound ? 0 : dto);
   else return (val - sfrom) * (dto - dfrom) / (sto - sfrom) + dfrom;
 }
 
+static inline float _exp_ave (int val, float ave, int window_size) {
+  float alpha = (float)2 / (1 + (window_size));
+  return val * alpha + ave * (1 - alpha);
+}
+
+bool _kick_detect (int *values) {
+  static int lastkickvalue;
+
+  if (values[0] + values[1] - lastkickvalue > THRESHOLD / 2) {
+    lastkickvalue = values[0] + values[1];
+    return true;
+  } else {
+    lastkickvalue = values[0] + values[1];
+    return false;
+  }
+}
+
 void process_lightshow_levelmeter (int *values, bool peak) {
   /* r1 */
-  sethsv(80, 255, _map(values[0] + values[0], THRESHOLD * 2, 1024 * 2, 0, 96, peak), &led[3]);
-  sethsv(80, 255, _map(values[1] + values[2], THRESHOLD * 2, 1024 * 2, 0, 96, peak), &led[2]);
-  sethsv(80, 255, _map(values[3] + values[4], THRESHOLD * 2, 1024 * 2, 0, 96, peak), &led[1]);
-  sethsv(80, 255, _map(values[5] + values[6], THRESHOLD * 2, 1024 * 2, 0, 96, peak), &led[0]);
+  sethsv(80, 255, _map(values[0] + values[1], THRESHOLD * 2, 1023 * 2, 0, 96, peak), &led[3]);
+  sethsv(80, 255, _map(values[2] + values[2], THRESHOLD * 2, 1023 * 2, 0, 96, peak), &led[2]);
+  sethsv(80, 255, _map(values[3] + values[4], THRESHOLD * 2, 1023 * 2, 0, 96, peak), &led[1]);
+  sethsv(80, 255, _map(values[5] + values[6], THRESHOLD * 2, 1023 * 2, 0, 96, peak), &led[0]);
   /* r2 */
-  sethsv(100, 255, _map(values[0] + values[0], THRESHOLD, THRESHOLD * 2, 0, 96, peak), &led[7]);
-  sethsv(100, 255, _map(values[1] + values[2], THRESHOLD, THRESHOLD * 2, 0, 96, peak), &led[6]);
+  sethsv(100, 255, _map(values[0] + values[1], THRESHOLD, THRESHOLD * 2, 0, 96, peak), &led[7]);
+  sethsv(100, 255, _map(values[2] + values[2], THRESHOLD, THRESHOLD * 2, 0, 96, peak), &led[6]);
   sethsv(100, 255, _map(values[3] + values[4], THRESHOLD, THRESHOLD * 2, 0, 96, peak), &led[5]);
   sethsv(100, 255, _map(values[5] + values[6], THRESHOLD, THRESHOLD * 2, 0, 96, peak), &led[4]);
   /* r3 */
-  sethsv(120, 255, _map(values[0] + values[0], THRESHOLD / 2, THRESHOLD, 0, 96, peak), &led[11]);
-  sethsv(120, 255, _map(values[1] + values[2], THRESHOLD / 2, THRESHOLD, 0, 96, peak), &led[10]);
+  sethsv(120, 255, _map(values[0] + values[1], THRESHOLD / 2, THRESHOLD, 0, 96, peak), &led[11]);
+  sethsv(120, 255, _map(values[2] + values[2], THRESHOLD / 2, THRESHOLD, 0, 96, peak), &led[10]);
   sethsv(120, 255, _map(values[3] + values[4], THRESHOLD / 2, THRESHOLD, 0, 96, peak), &led[9]);
   sethsv(120, 255, _map(values[5] + values[6], THRESHOLD / 2, THRESHOLD, 0, 96, peak), &led[8]);
   /* r4 */
-  sethsv(140, 255, _map(values[0] + values[0], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak), &led[15]);
-  sethsv(140, 255, _map(values[1] + values[2], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak), &led[14]);
+  sethsv(140, 255, _map(values[0] + values[1], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak), &led[15]);
+  sethsv(140, 255, _map(values[2] + values[2], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak), &led[14]);
   sethsv(140, 255, _map(values[3] + values[4], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak), &led[13]);
   sethsv(140, 255, _map(values[5] + values[6], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak), &led[12]);
-  /* flush */
-  ws2812_setleds(led, 16);
+}
+
+void process_lightshow_levelmeter2 (int *values, bool peak) {
+  /* r1 */
+  setrgb(
+    _map(values[0], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    0,
+    _map(values[3], THRESHOLD, 1023, 0, 96, peak),
+    &led[3]
+  );
+  setrgb(
+    _map(values[0], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    0,
+    _map(values[3], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    &led[2]
+  );
+  setrgb(
+    _map(values[0], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    0,
+    _map(values[3], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    &led[1]
+  );
+  setrgb(
+    _map(values[0], THRESHOLD, 1023, 0, 96, peak),
+    0,
+    _map(values[3], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    &led[0]
+  );
+  /* r2 */
+  setrgb(
+    _map(values[1], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    0,
+    _map(values[4], THRESHOLD, 1023, 0, 96, peak),
+    &led[7]
+  );
+  setrgb(
+    _map(values[1], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    0,
+    _map(values[4], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    &led[6]
+  );
+  setrgb(
+    _map(values[1], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    0,
+    _map(values[4], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    &led[5]
+  );
+  setrgb(
+    _map(values[1], THRESHOLD, 1023, 0, 96, peak),
+    0,
+    _map(values[4], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    &led[4]
+  );
+  /* r3 */
+  setrgb(
+    _map(values[2], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    0,
+    _map(values[5], THRESHOLD, 1023, 0, 96, peak),
+    &led[11]
+  );
+  setrgb(
+    _map(values[2], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    0,
+    _map(values[5], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    &led[10]
+  );
+  setrgb(
+    _map(values[2], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    0,
+    _map(values[5], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    &led[9]
+  );
+  setrgb(
+    _map(values[2], THRESHOLD, 1023, 0, 96, peak),
+    0,
+    _map(values[5], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    &led[8]
+  );
+  /* r4 */
+  setrgb(
+    _map(values[3], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    0,
+    _map(values[6], THRESHOLD, 1023, 0, 96, peak),
+    &led[15]
+  );
+  setrgb(
+    _map(values[3], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    0,
+    _map(values[6], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    &led[14]
+  );
+  setrgb(
+    _map(values[3], THRESHOLD / 2, THRESHOLD, 0, 96, peak),
+    0,
+    _map(values[6], THRESHOLD / 4, THRESHOLD / 2, 0, 96, peak),
+    &led[13]
+  );
+  setrgb(
+    _map(values[3], THRESHOLD, 1023, 0, 96, peak),
+    0,
+    _map(values[6], THRESHOLD / 8, THRESHOLD / 4, 0, 96, peak),
+    &led[12]
+  );
+}
+
+void process_lightshow_levelmeter3 (int *values, bool peak) {
+  /* r1 */
+  sethsv(0, 255, _map(values[0], THRESHOLD / 2, 1023, 0, 96, peak), &led[3]);
+  sethsv(40, 255, _map(values[1], THRESHOLD / 2, 1023, 0, 96, peak), &led[2]);
+  sethsv(80, 255, _map(values[2], THRESHOLD / 2, 1023, 0, 96, peak), &led[1]);
+  sethsv(120, 255, _map(values[3], THRESHOLD / 2, 1023, 0, 96, peak), &led[0]);
+  /* r2 */
+  sethsv(0, 255, _map(values[0], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[7]);
+  sethsv(40, 255, _map(values[1], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[6]);
+  sethsv(80, 255, _map(values[2], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[5]);
+  sethsv(120, 255, _map(values[3], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[4]);
+  /* r3 */
+  sethsv(120, 255, _map(values[3], THRESHOLD / 2, 1023, 0, 96, peak), &led[11]);
+  sethsv(160, 255, _map(values[4], THRESHOLD / 2, 1023, 0, 96, peak), &led[10]);
+  sethsv(200, 255, _map(values[5], THRESHOLD / 2, 1023, 0, 96, peak), &led[9]);
+  sethsv(240, 255, _map(values[6], THRESHOLD / 2, 1023, 0, 96, peak), &led[8]);
+  /* r4 */
+  sethsv(120, 255, _map(values[3], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[15]);
+  sethsv(160, 255, _map(values[4], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[14]);
+  sethsv(200, 255, _map(values[5], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[13]);
+  sethsv(240, 255, _map(values[6], THRESHOLD / 8, THRESHOLD / 2, 0, 96, peak), &led[12]);
 }
 
 void process_lightshow_party (int *values) {
-  static int lastkickvalue, hue;
+  static int hue;
 
-  if (values[0] + values[0] - lastkickvalue > THRESHOLD / 2) {
+  if (_kick_detect(values)) {
     hue = (hue + 199) % 256;
   }
-  lastkickvalue = values[0] + values[0];
 
   int val = 0;
   for (int i = 0; i < 7; i++) {
@@ -116,8 +265,29 @@ void process_lightshow_party (int *values) {
     sethsv(hue, 255, val, &led[0 * 4 + col]);
     sethsv(hue, 255, val, &led[3 * 4 + (col + 2)]);
   }
+}
 
-  ws2812_setleds(led, 16);
+void process_lightshow_party2 (int *values) {
+  int blue = 0, red = 0, green = 0, kick = 0;
+
+  for (int i = 0; i < 2; i++) blue += values[i] / (4 * 2 * 2);
+  for (int i = 2; i < 4; i++) red += values[i] / (4 * 2 * 2);
+  for (int i = 4; i < 7; i++) green += values[i] / (4 * 3 * 2);
+  kick = _kick_detect(values) ? 127 : 0;
+
+  for(int row = 0; row < 4; row++) {
+    if (row % 2 == 0) {
+      setrgb(0, 0, blue, &led[row * 4 + 3]);
+      setrgb(red, 0, 0, &led[row * 4 + 2]);
+      setrgb(0, green, 0, &led[row * 4 + 1]);
+      setrgb(kick, kick, kick, &led[row * 4 + 0]);
+    } else {
+      setrgb(0, 0, blue, &led[row * 4 + 0]);
+      setrgb(red, 0, 0, &led[row * 4 + 1]);
+      setrgb(0, green, 0, &led[row * 4 + 2]);
+      setrgb(kick, kick, kick, &led[row * 4 + 3]);
+    }
+  }
 }
 
 int values[7];
@@ -129,10 +299,17 @@ void matrix_scan_user (void) {
       process_lightshow_levelmeter(values, false);
     } else if (pattern == LEVELMETER_PEAK) {
       process_lightshow_levelmeter(values, true);
+    } else if (pattern == LEVELMETER2) {
+      process_lightshow_levelmeter2(values, false);
+    } else if (pattern == LEVELMETER3) {
+      process_lightshow_levelmeter3(values, false);
     } else if (pattern == PARTY) {
       process_lightshow_party(values);
+    } else if (pattern == PARTY2) {
+      process_lightshow_party2(values);
     }
-    delay = 150;
+    ws2812_setleds(led, 16);
+    delay = LIGHTSHOW_DELAY;
   } else {
     delay--;
   }
