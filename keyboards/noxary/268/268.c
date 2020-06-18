96b49e6960012e67e5a4feaec406a2f7d9f7d676
Rozakiin <6567067+Rozakiin@users.noreply.github.com>
2020-06-18 10:46:43 +0100

    Update keyboards/noxary/268/268.c
    Remove commented code
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/noxary/268/268.c
+++ keyboards/noxary/268/268.c
@@ -14,31 +14,3 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "268.h"
-
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
-
-
-// void matrix_init_kb(void) {
-// 	// put your keyboard start-up code here
-// 	// runs once when the firmware starts up
-//     setPinOutput(B6);
-// 	matrix_init_user();
-// }
-
-// bool led_update_kb(led_t led_state) {
-//     if(led_update_user(led_state)) {
-//         writePin(B6, led_state.scroll_lock);
-//     }
-//     return true;
-// }
-
-// void led_set_user(uint8_t usb_led) {
-//     if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-//         DDRB |= (1 << 6); PORTB |= (1 << 6);
-//     }
-//     else {
-//         DDRB &= ~(1 << 6); PORTB &= ~(1 << 6);
-//     }
-// }
