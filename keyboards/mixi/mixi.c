85b659c1f41a60edbbca6c65794db5d1eb36ae74
Gregorio Chiko <chiko150@outlook.com>
2020-06-19 03:09:25 +0700

    fixed RGB not turned on after clearing EEPROM
    caused by the default HSV is not set on init

--- keyboards/mixi/mixi.c
+++ keyboards/mixi/mixi.c
@@ -3,6 +3,7 @@
 void eeconfig_init_kb(void) {
 #ifdef RGBLIGHT_ENABLE
     rgblight_enable(); // Enable RGB underglow by default
+    rgblight_sethsv_noeeprom(0, 255, 255); // Set RGB default color
 #ifdef RGBLIGHT_ANIMATIONS
     rgblight_mode(RGBLIGHT_MODE_TWINKLE + 5); // Set to RGB_TWINKLE animation by default
 #endif
