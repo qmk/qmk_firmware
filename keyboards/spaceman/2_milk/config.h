f38d3c90f4b5285c21733b942daf70bb5d3872ae
Yu Hao Wong <wongyuhao@gmail.com>
2020-06-20 15:34:19 +0800

    added via support to spaceman 2% milk

--- keyboards/spaceman/2_milk/config.h
+++ keyboards/spaceman/2_milk/config.h
@@ -17,8 +17,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0xB195
+#define VENDOR_ID       0x5350
+#define PRODUCT_ID      0x3225
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Spaceman
 #define PRODUCT         2% Milk
---
+++ keyboards/spaceman/2_milk/keymaps/via/keymap.c
@@ -0,0 +1,23 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+      [0] = LAYOUT(
+          KC_Z,
+          KC_X
+          ),
+
+      [1] = LAYOUT(
+      KC_TRNS,
+      KC_TRNS
+      ),
+
+      [2] = LAYOUT(
+      KC_TRNS,
+      KC_TRNS
+      ),
+
+      [3] = LAYOUT(
+      KC_TRNS,
+      KC_TRNS
+      )
+};
