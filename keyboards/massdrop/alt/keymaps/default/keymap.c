1470aaa19666ae22d3b81749757992b7260960c4
Jesper Jensen <jesper@slashwin.dk>
2020-06-20 12:20:35 +0200

    Remove the keymap_config definition from keymaps
    The keymap_config def was conflicting with the one found in
    tmk_core/common/magic.c. Declaring it extern in magic.c breaks a bunch
    of keyboard that rely on that declaration (like the ergodox). Instead
    I've removed the one found in the keymap.c of the massdrop alt.
    
    The same change will have to be made to other keyboards.

--- keyboards/massdrop/alt/keymaps/default/keymap.c
+++ keyboards/massdrop/alt/keymaps/default/keymap.c
@@ -10,8 +10,6 @@ enum alt_keycodes {
     MD_BOOT,               //Restart into bootloader after hold timeout
 };
 
-keymap_config_t keymap_config;
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
