48a10c622b4f5e12986b4e20490535c0df393370
Gregorio Chiko <chiko150@outlook.com>
2020-06-21 11:52:37 +0700

    removed unused and commented callback at mixi.c

--- keyboards/mixi/mixi.c
+++ keyboards/mixi/mixi.c
@@ -3,7 +3,6 @@
 void eeconfig_init_kb(void) {
 #ifdef RGBLIGHT_ENABLE
     rgblight_enable(); // Enable RGB underglow by default
-    //rgblight_sethsv_noeeprom(0, 255, 255); // Set RGB default color
     rgblight_sethsv(0, 255, 255);
 #ifdef RGBLIGHT_ANIMATIONS
     rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL + 5); // Set to RGB_RAINBOW_SWIRL animation by default
