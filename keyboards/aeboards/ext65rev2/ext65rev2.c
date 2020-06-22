9bcd116e16d0c76bf66bc3fcdca7c78c0f5f7890
Xelus22 <preyas22@gmail.com>
2020-06-22 15:51:52 +1000

    update and remove board specific files and add to ext65rev2.c

--- keyboards/aeboards/ext65rev2/ext65rev2.c
+++ keyboards/aeboards/ext65rev2/ext65rev2.c
@@ -1,5 +1,10 @@
 #include "ext65rev2.h"
 
+void board_init(void) {
+  SYSCFG->CFGR1 |= SYSCFG_CFGR1_I2C1_DMA_RMP;
+  SYSCFG->CFGR1 &= ~(SYSCFG_CFGR1_SPI2_DMA_RMP);
+}
+
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_90;  // rotates the display 90 degrees
