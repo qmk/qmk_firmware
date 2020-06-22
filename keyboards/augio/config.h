49cc52bb719d5d772caf8f4e943b3a0be1764ea7
danforthewin <harvey.daniel11@gmail.com>
2020-06-22 16:44:34 -0500

    updated manufacturer

--- keyboards/augio/config.h
+++ keyboards/augio/config.h
@@ -6,7 +6,7 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    ???
+#define MANUFACTURER    github.com/danforthewin
 #define PRODUCT         Augio
 
 /* key matrix size */
