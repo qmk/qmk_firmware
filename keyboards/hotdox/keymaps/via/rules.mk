7d8f0c95b8cfcfb6a5a50e8d2ba5846a1c594b74
unicorn855 <truth_of_spirit@yahoo.ca>
2020-06-23 00:26:56 +0200

    just testing to see if mouse and media keys work in via

--- keyboards/hotdox/keymaps/via/rules.mk
+++ keyboards/hotdox/keymaps/via/rules.mk
@@ -1,8 +1,8 @@
 VIA_ENABLE = yes
 LTO_ENABLE = yes
 
-MOUSEKEY_ENABLE         = no  # Mouse keys
-EXTRAKEY_ENABLE         = no # Audio control and System control
+MOUSEKEY_ENABLE         = yes  # Mouse keys
+EXTRAKEY_ENABLE         = yes # Audio control and System control
 
 LEADER_ENABLE			= no
 FAUXCLICKY_ENABLE		= no
