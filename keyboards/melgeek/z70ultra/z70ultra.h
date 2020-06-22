e1ebe5c9ede5c30c52e5049818ce5ac9a1cda571
MelGeek <65591833+melgeek001365@users.noreply.github.com>
2020-06-22 22:36:18 +0800

    Update keyboards/melgeek/z70ultra/z70ultra.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/melgeek/z70ultra/z70ultra.h
+++ keyboards/melgeek/z70ultra/z70ultra.h
@@ -18,7 +18,7 @@
     { XXX, XXX, XXX, XXX, XXX, XXX, K56, K57, K58, K59, K5A, K5B, K5C, K5D }  \
 }
 
-#define LAYOUT_ANSI( \
+#define LAYOUT_ansi( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D, \
