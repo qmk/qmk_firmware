47dfcc6671e18666c2a3638ee80f498096142b29
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-06-22 22:14:25 -0400

    rm config.h from default brain keymap

--- keyboards/handwired/brain/keymaps/klackygears/config.h
+++ keyboards/handwired/brain/keymaps/klackygears/config.h
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define USE_SERIAL
+//#define USE_SERIAL
 
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM 150
