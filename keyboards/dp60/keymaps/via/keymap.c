1fbfd7b5262301f814d97110861409a632dae53d
paulgali <paulgali@live.co.uk>
2020-06-24 12:57:42 +0400

    Update keyboards/dp60/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dp60/keymaps/via/keymap.c
+++ keyboards/dp60/keymaps/via/keymap.c
@@ -9,11 +9,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL,  KC_LGUI,  KC_LALT,                        KC_SPC,                  KC_RGUI,KC_RALT,  MO(1),   KC_RCTL),
 
     [1] = LAYOUT_all(
-      KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,KC_TRNS,KC_DEL,
-      RESET,   KC_TRNS,KC_UP,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PAUS,KC_SLCK,KC_PSCR,KC_PGUP,KC_TRNS,KC_INS,
-      KC_TRNS,        KC_LEFT,KC_DOWN,KC_RIGHT,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,KC_HOME,KC_END,KC_TRNS, KC_TRNS,
-      KC_TRNS,KC_TRNS,        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PGDN,KC_TRNS,KC_TRNS,
-      KC_TRNS,KC_TRNS,KC_TRNS,                        KC_TRNS,                        KC_TRNS,MO(2),TG(0),KC_TRNS),
+      KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,KC_DEL,
+      RESET,   _______,KC_UP,_______,_______,_______,_______,_______,KC_PAUS,KC_SLCK,KC_PSCR,KC_PGUP,_______,KC_INS,
+      _______,        KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,_______, _______,_______,KC_HOME,KC_END,_______, _______,
+      _______,_______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PGDN,_______,_______,
+      _______,_______,_______,                        _______,                        _______,MO(2),TG(0),_______),
 
     [2] = LAYOUT_all(
       _______, _______,_______,  _______,_______,  _______,_______,  _______,_______,  _______,_______, _______,_______,_______,_______,
