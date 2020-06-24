38417df0c1b0efef99171f3dcf7191d9f68019ac
paulgali <paulgali@live.co.uk>
2020-06-24 12:51:00 +0400

    Update keyboards/dp60/keymaps/via/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dp60/keymaps/via/rules.mk
+++ keyboards/dp60/keymaps/via/rules.mk
@@ -1,5 +1,4 @@
 VIA_ENABLE = yes
 LTO_ENABLE = yes
 RGBLIGHT_ENABLE = yes #enables underglow, but will not disable per key leds
-# BOOTMAGIC_ENABLE = lite #already enabled in base directory
 MOUSEKEY_ENABLE = no
