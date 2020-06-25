351783c35984e0c71c39eba179f0818713cea2c7
Khader Syed <khader.syed@aicure.com>
2020-06-25 18:35:46 -0400

    move quote the first layer

--- keyboards/primekb/prime_e/keymaps/madhatter/keymap.c
+++ keyboards/primekb/prime_e/keymaps/madhatter/keymap.c
@@ -44,14 +44,14 @@ enum ilpse_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_GESC, KC_Q,  KC_W,  KC_E,  KC_R,   KC_T,    KC_Y,  KC_U,  KC_I,  KC_O,    KC_P,    KC_MINS, KC_BSPC,
-    KC_TAB,  KC_A,  KC_S,  KC_D,  KC_F,   KC_G,    KC_H,  KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,
+    KC_TAB,  KC_A,  KC_S,  KC_D,  KC_F,   KC_G,    KC_H,  KC_J,  KC_K,  KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,   KC_B,    KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_LCTL, KC_LALT,          LOWER,  KC_LCMD,    KC_SPC,  RAISE,          KC_RGUI, KC_RCTL
   ),
 
   [_LOWER] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  KC_DEL,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_ENT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_QUOT,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
     KC_TRNS, KC_TRNS,                     LOWER, KC_TRNS,   KC_TRNS,   RAISE,          KC_TRNS, KC_TRNS
   ),
