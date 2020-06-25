b9fa5a13c71d7c00c1f6c44dcac7572d14af222d
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-06-25 17:53:32 -0400

    Update keyboards/clawsome/sedan/rules.mk
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/clawsome/sedan/rules.mk
+++ keyboards/clawsome/sedan/rules.mk
@@ -24,3 +24,4 @@ BLUETOOTH_ENABLE = no        # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no            # Audio output on port C6
 FAUXCLICKY_ENABLE = no       # Use buzzer to emulate clicky switches
 
+LAYOUTS = 65_ansi
