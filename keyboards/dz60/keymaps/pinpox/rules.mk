602e75e3821cd0694e01374ce77c2eb2918ec7ac
Pablo Ovelleiro Corral <github@pablo.tools>
2020-06-25 08:18:32 +0200

    Update keyboards/dz60/keymaps/pinpox/rules.mk
    Co-authored-by: Erovia <Erovia@users.noreply.github.com>

--- keyboards/dz60/keymaps/pinpox/rules.mk
+++ keyboards/dz60/keymaps/pinpox/rules.mk
@@ -1,7 +1,7 @@
 
 # Build Options
 # override the options specified in dz60/rules.mk
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration
 # MOUSEKEY_ENABLE = yes       # Mouse keys
 # EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
