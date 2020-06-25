b220bdbeb929370e49b9cf3433ab04cc71e654a6
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-06-24 22:38:11 -0300

    poor crkbds are those that do not have OLEDs

--- keyboards/crkbd/keymaps/rhruiz/keymap.c
+++ keyboards/crkbd/keymaps/rhruiz/keymap.c
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
 };
 // clang-format on
-#ifdef OLED_DRIVER_ENABLE
+#ifndef OLED_DRIVER_ENABLE
 
 void rhruiz_update_layer_colors(layer_state_t state) {
     writePinHigh(D5);
