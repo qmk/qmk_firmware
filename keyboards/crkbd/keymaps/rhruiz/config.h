a31621612f54adb91138d07690323e8ee2028a56
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-06-26 23:07:43 -0300

    awesomer mouse speeds

--- keyboards/crkbd/keymaps/rhruiz/config.h
+++ keyboards/crkbd/keymaps/rhruiz/config.h
@@ -56,6 +56,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_VAL_STEP 17
 #endif
 
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
 // bootmagic
 
 #define BOOTMAGIC_LITE_ROW 0
