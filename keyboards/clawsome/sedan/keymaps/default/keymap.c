3e720d010a9c36212388d33d05fd957b51a5dbe7
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-06-26 20:19:31 -0400

    updated keymaps

--- keyboards/clawsome/sedan/keymaps/default/keymap.c
+++ keyboards/clawsome/sedan/keymaps/default/keymap.c
@@ -16,12 +16,26 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+   /*
+    *---------------------------------------------------------------------------------
+    *|ESC | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 0  | -  | =  |BSPC     |HOME|
+    *---------------------------------------------------------------------------------
+    *| TAB  | Q  | W  | E  | R  | T  | Y  | U  | I  | O  | P  | [  | ]  | \     |END |
+    *---------------------------------------------------------------------------------
+    *|  CAPS | A  | S  | D  | F  | G  | H  | J  | K  | L  | ;  | "  |   ENTER   |PGUP|
+    *---------------------------------------------------------------------------------
+    *|  SHIFT  | Z  | X  | C  | V  | B  | N  | M  | ,  | .  | ?  |  SHIFT  | ^  |PGDN|
+    *---------------------------------------------------------------------------------
+    *|CTRL|GUI |ALT |             SPACE          |ALT |GUI |MENU |CTRL| <  | v  | >  |
+    *---------------------------------------------------------------------------------
+    */
     [0] = LAYOUT_65_ansi(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
-        KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_PGUP,
-        KC_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,              KC_PGDN,
-        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,        XXXXXXX, KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                              KC_LALT, XXXXXXX, KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX
+        KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_END,
+        KC_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,              KC_PGUP,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,        KC_UP,   KC_PGDN,
+        KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                              KC_GALT, KC_RGUI, KC_GCTL, KC_LEFT, KC_DOWN, KC_RGHT
     )
 
 };
---
+++ keyboards/montsinger/rebound/rev4/rev4 2.h
@@ -0,0 +1,59 @@
+/* Copyright 2020 Ross Montsinger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ * The first section contains all of the arguments representing the
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT_ortho_4x12( \
+    K00, K70, K01, K71, K02, K72, K04, K84, K05, K75, K06, K76, \
+    K10, K60, K11, K61, K12, K62, K14, K74, K15, K65, K16, K66, \
+    K20, K50, K21, K51, K22, K52, K24, K64, K25, K55, K26, K56,\
+    K30, K40, K31, K41, K32, K42, K34, K54, K35, K45, K36, K46\
+    ) { \
+  { K00, K01, K02, K04, K05, K06, K07}, \
+  { K10, K11, K12, K14, K15, K16, K17 }, \
+  { K20, K21, K22, K24, K25, K26, K27 }, \
+  { K30, K31, K32, K34, K35, K36, K37 }, \
+  { K40, K41, K42, K44, K45, K46, K47 }, \
+  { K50, K51, K52, K54, K55, K56, K57 }, \
+  { K60, K61, K62, K64, K65, K66, K67 }, \
+  { K70, K71, K72, K74, K75, K76, K77 } \
+}
+
+#define LAYOUT_all( \
+    K00, K70, K01, K71, K02, K72,      K04, K74, K05, K75, K06, K76, \
+    K10, K60, K11, K61, K12, K62, K13, K14, K64, K15, K65, K16, K66, \
+    K20, K50, K21, K51, K22, K52, K23, K24, K54, K25, K55, K26, K56,\
+    K30, K40, K31, K41, K32, K42, K33, K34, K44, K35, K45, K36, K46\
+    ) { \
+  { K00, K01, K02, KC_NO, K04, K05, K06}, \
+  { K10, K11, K12, K13, K14, K15, K16}, \
+  { K20, K21, K22, K23, K24, K25, K26}, \
+  { K30, K31, K32, K33, K34, K35, K36}, \
+  { K40, K41, K42, KC_NO, K44, K45, K46}, \
+  { K50, K51, K52, KC_NO, K54, K55, K56}, \
+  { K60, K61, K62, KC_NO, K64, K65, K66}, \
+  { K70, K71, K72, KC_NO, K74, K75, K76} \
+}
+
