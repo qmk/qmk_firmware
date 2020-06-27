eda9fd634bafc9f949bd799c087271b685fe445f
Andrew Koh <andrew@springlabs.com>
2020-06-26 18:49:37 -0700

    arisu via changes

--- keyboards/arisu/config.h
+++ keyboards/arisu/config.h
@@ -241,5 +241,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* Bootmagic Lite key configuration */
-// #define BOOTMAGIC_LITE_ROW 0
-// #define BOOTMAGIC_LITE_COLUMN 0
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
