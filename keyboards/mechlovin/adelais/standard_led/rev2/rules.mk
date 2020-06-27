6c98684d198535184b0c4fedd8f28d27674de56c
mechlovin <57231893+mechlovin@users.noreply.github.com>
2020-06-27 08:52:11 +0700

    update

--- keyboards/mechlovin/adelais/standard_led/rev2/rules.mk
+++
@@ -1,3 +0,0 @@
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-BACKLIGHT_DRIVER = pwm
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
--- keyboards/mechlovin/adelais/standard_led/rev3/rules.mk
+++ keyboards/mechlovin/adelais/standard_led/rev3/rules.mk
@@ -1,4 +1,1 @@
 ENCODER_ENABLE = yes
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-BACKLIGHT_DRIVER = pwm
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
