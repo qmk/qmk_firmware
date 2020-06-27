0b96d0788aed783339d2fc8d91946fdd72d2e546
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2020-06-27 06:10:33 +0000

    Update rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/xelus/trinityxttkl/rules.mk
+++ keyboards/xelus/trinityxttkl/rules.mk
@@ -12,7 +12,6 @@ CONSOLE_ENABLE = yes 	# Console for debug
 COMMAND_ENABLE = no     # Commands for debug and configuration
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
-CUSTOM_MATRIX = no 		# Custom matrix file
 BACKLIGHT_ENABLE = no
 RGBLIGHT_ENABLE = no
 
