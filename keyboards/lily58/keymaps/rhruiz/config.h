79b7565b658ffa02a1fbded188a3b7df592e886f
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2020-06-27 00:06:14 -0300

    awesomer mouse speeds

--- keyboards/lily58/keymaps/rhruiz/config.h
+++ keyboards/lily58/keymaps/rhruiz/config.h
@@ -66,6 +66,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 #endif
 
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
 
 // bootloader
 
