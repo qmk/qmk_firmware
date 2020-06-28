483c203ea309ba495e90d0591d3adaf567446e4b
Mojitas <47667093+Mojitas@users.noreply.github.com>
2020-06-28 19:16:00 +0200

    Update keyboards/keebio/iris/keymaps/mojitas/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/keebio/iris/keymaps/mojitas/keymap.c
+++ keyboards/keebio/iris/keymaps/mojitas/keymap.c
@@ -1,9 +1,4 @@
-#include "iris.h"
-#include "action_layer.h"
-#include "eeconfig.h"
-
-extern keymap_config_t keymap_config;
-
+#include QMK_KEYBOARD_H
 
 //Heavily modified keymap. Some features:
 //Multiple layouts, I use dvorak as main.
