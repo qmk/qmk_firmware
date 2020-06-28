0d1fad65bc07e394c668c52006a195c940525ec3
Ramon Imbao <ramonimbao@gmail.com>
2020-06-28 22:35:50 +0800

    Update the PID

--- keyboards/ramonimbao/herringbone/config.h
+++ keyboards/ramonimbao/herringbone/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID 0xB16B
-#define PRODUCT_ID 0x0001
+#define PRODUCT_ID 0x04E5
 #define DEVICE_VER 0x0012
 #define MANUFACTURER Ramon Imbao
 #define PRODUCT Herringbone
