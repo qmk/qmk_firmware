69d2cfe03c03f78eff9bec8dab25f18d9219a26f
Mojitas <47667093+Mojitas@users.noreply.github.com>
2020-06-28 19:16:12 +0200

    Update keyboards/keebio/iris/keymaps/mojitas/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/keebio/iris/keymaps/mojitas/keymap.c
+++ keyboards/keebio/iris/keymaps/mojitas/keymap.c
@@ -195,12 +195,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef AUDIO_ENABLE
 float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
 #endif
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
