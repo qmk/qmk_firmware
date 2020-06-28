6007f8ab0fae76eecc8c4be8734e31ed7d271450
Ramon Imbao <ramonimbao@gmail.com>
2020-06-28 20:40:42 +0800

    Revert to 0xB16B VID

--- keyboards/ramonimbao/herringbone/config.h
+++ keyboards/ramonimbao/herringbone/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xB04E
-#define PRODUCT_ID 0x0001
+#define VENDOR_ID 0xB16B
+#define PRODUCT_ID 0x04E5
 #define DEVICE_VER 0x0012
 #define MANUFACTURER Ramon Imbao
 #define PRODUCT Herringbone
