892567641894f81b002cf9e0aa0e9bbada0c181f
Flatmasterflex <33593585+Flatmasterflex@users.noreply.github.com>
2020-06-29 02:51:33 +0100

    Create config.h

---
+++ keyboards/hacro_8/config.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x2341
+#define PRODUCT_ID      0x8036
+#define MANUFACTURER    Short Circuits
+#define PRODUCT         Hacro 8
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key matrix size */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 4
+
+/* key matrix pins */
+
+#define MATRIX_ROW_PINS { B7, D7 }
+#define MATRIX_COL_PINS { B5, B4, D6, D4 } 
+
+// ws2812 options
+#define RGB_DI_PIN D2 // pin the DI on the ws2812 is hooked-up to
+#define RGBLED_NUM 10 // number of LEDs
+#define RGBLIGHT_ANIMATIONS
+#endif
