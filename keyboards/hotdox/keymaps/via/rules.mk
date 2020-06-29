59e5e303635daf42fb7fd5294dce5dbdbc702452
unicorn855 <truth_of_spirit@yahoo.ca>
2020-06-29 09:34:47 +0200

    Update keyboards/hotdox/keymaps/via/rules.mk
    as per @faupark's suggestion (thank you), those seven lines were removed :)
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/hotdox/keymaps/via/rules.mk
+++ keyboards/hotdox/keymaps/via/rules.mk
@@ -3,11 +3,3 @@ LTO_ENABLE = yes
 
 MOUSEKEY_ENABLE         = yes # Mouse keys
 EXTRAKEY_ENABLE         = yes # Audio control and System control
-
-
-LEADER_ENABLE			      = no
-FAUXCLICKY_ENABLE		    = no
-MIDI_ENABLE			      	= no
-BLUETOOTH_ENABLE    		= no
-KEY_LOCK_ENABLE		    	= no
-TERMINAL_ENABLE		    	= no
