cce098b4740e279c326e6b71b3e9664fcb1c75b2
kwlodarczy <64139515+kwlodarczy@users.noreply.github.com>
2020-06-29 12:05:24 +0100

    Update keymap.c

--- keyboards/rotr/keymaps/default/keymap.c
+++ keyboards/rotr/keymaps/default/keymap.c
@@ -5,11 +5,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MPRV, KC_MPLY, KC_MNXT
     )
 };
-
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
-}
