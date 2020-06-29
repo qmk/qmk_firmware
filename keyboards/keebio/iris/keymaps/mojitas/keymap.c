0587d9574636bc0516428dd80f2d21c311bf662f
Mojitas <47667093+Mojitas@users.noreply.github.com>
2020-06-29 21:29:20 +0200

    Update keyboards/keebio/iris/keymaps/mojitas/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/keebio/iris/keymaps/mojitas/keymap.c
+++ keyboards/keebio/iris/keymaps/mojitas/keymap.c
@@ -251,34 +251,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
   }
   return true;
 }
