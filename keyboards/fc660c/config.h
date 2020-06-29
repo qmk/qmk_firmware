961f15187c7f24587bf96d30d8c60b8a2f6bb5be
Frothy <frothykillskittens@gmail.com>
2020-06-29 07:28:03 -0300

    Updated Manu and Product

--- keyboards/fc660c/config.h
+++ keyboards/fc660c/config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0x4853 /* HS */
 #define PRODUCT_ID      0x660C
 #define DEVICE_VER      0x0100
-#define MANUFACTURER    QMK
-#define PRODUCT         Leopold FC660C with QMK
+#define MANUFACTURER    Hasu
+#define PRODUCT         FC660C
 #define DESCRIPTION     Leopold FC660C with Hasu alternative controller using QMK
 
 /* key matrix size */
