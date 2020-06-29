483a527ef1d804df4de6e1e5ae4ea349bef7763f
kwlodarczy <64139515+kwlodarczy@users.noreply.github.com>
2020-06-29 09:59:56 +0100

    Update keyboards/rotr/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/rotr/keymaps/default/keymap.c
+++ keyboards/rotr/keymaps/default/keymap.c
@@ -1,20 +1,15 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-	LAYOUT(
-		KC_MPRV, KC_MPLY, KC_MNXT)
+    LAYOUT(
+        KC_MPRV, KC_MPLY, KC_MNXT
+    )
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-
-	if (clockwise) {
-
-		tap_code(KC_VOLU);
-
-	} else {
-
-		tap_code(KC_VOLD);
-
-	}
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
 }
