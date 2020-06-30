2c14cf2f8fd28f8f05ca964e0c492924b6e5dc28
Alabahuy <rezzaadio@gmail.com>
2020-06-30 07:41:52 +0700

    Update config.h

--- keyboards/rart/rart67/config.h
+++ keyboards/rart/rart67/config.h
@@ -20,9 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID		  0x414C  // "AL"
-#define PRODUCT_ID 		0x0067  // "67"
-#define DEVICE_VER 		0x0001
+#define VENDOR_ID       0x414C  // "AL"
+#define PRODUCT_ID      0x0067  // "67"
+#define DEVICE_VER      0x0001
 #define MANUFACTURER    Alabahuy
 #define PRODUCT         RART67
 #define DESCRIPTION     65% Mechanical Keyboard
