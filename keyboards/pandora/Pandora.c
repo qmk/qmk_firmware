875c02cde329628b7ca2a74803166e6d09b1e763
Koobaczech <67293963+Koobaczech@users.noreply.github.com>
2020-06-29 23:26:26 -0400

    Pandora

---
+++ keyboards/pandora/Pandora.c
@@ -0,0 +1,37 @@
+/* Copyright 2020 Koobaczech
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Pandora.h"
+
+void matrix_init_kb(void) {
+    matrix_init_user();
+    led_init_ports();
+};
+
+void led_init_ports(void) {
+    setPinOutput(E6);
+    writePinHigh(E6);
+}
+
+void led_set_kb(uint8_t usb_led) {
+    if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+        writePinLow(E6);
+    } else {
+        writePinHigh(E6);
+    }
+
+    led_set_user(usb_led);
+}
---
+++ keyboards/pandora/readme.md
@@ -0,0 +1,15 @@
+# Pandora
+
+![Pandora](imgur.com image replace me!)
+
+A short description of the keyboard/project
+
+* Keyboard Maintainer: QMK Community
+* Hardware Supported: Pandora
+* Availability: Link to come
+
+Make example for this keyboard (after setting up your build environment):
+
+    make Pandora:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
