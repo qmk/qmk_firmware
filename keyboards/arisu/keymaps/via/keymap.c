f33301ad5ea46f17bf59c194053f2ee92e471c1c
akscanb <akscanb@gmail.com>
2020-07-01 15:00:34 -0700

    Update keyboards/arisu/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/arisu/keymaps/via/keymap.c
+++ keyboards/arisu/keymaps/via/keymap.c
@@ -15,8 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-#define FN MO(1)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV, KC_PGUP,
