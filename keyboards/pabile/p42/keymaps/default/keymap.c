aab2e5562ff05c1f3898b0776673779282750961
pabile <1162412+pabile@users.noreply.github.com>
2020-07-02 15:03:26 +0800

    Update keymap.c

--- keyboards/pabile/p42/keymaps/default/keymap.c
+++ keyboards/pabile/p42/keymaps/default/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,   KC_K,    KC_L,   KC_MINS,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_BTN1,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_BSPC, KC_SPC, KC_ENT, KC_APP,  KC_DEL, KC_RTCL, KC_ESC)
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_BSPC, KC_SPC, KC_ENT, KC_APP,  KC_DEL, KC_RCTL, KC_ESC)
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
