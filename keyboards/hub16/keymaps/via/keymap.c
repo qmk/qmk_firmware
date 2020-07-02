4e1fb7c36be389ca0f7db2b76540cec66d5e8668
Josh Johnson <josh@joshajohnson.com>
2020-07-02 19:30:39 +1000

    change default keymap

--- keyboards/hub16/keymaps/via/keymap.c
+++ keyboards/hub16/keymaps/via/keymap.c
@@ -18,14 +18,14 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
       KC_MUTE,   KC_MPLY,
-    KC_P7, KC_P8, KC_P9, KC_PAST,
-    KC_P4, KC_P5, KC_P6, KC_PMNS,
-    KC_P1, KC_P2, KC_P3, KC_PPLS,
-    MO(1), KC_P0, KC_PDOT, KC_PENT
+    KC_7, KC_8, KC_9, KC_PAST,
+    KC_4, KC_5, KC_6, KC_PMNS,
+    KC_1, KC_2, KC_3, KC_PPLS,
+    MO(1), KC_0, KC_PDOT, KC_PENT
   ),
 
   [1] = LAYOUT( /* LED Control */
-        _______,            _______,
+        _______,           _______,
     _______, RGB_MOD, RGB_RMOD, RGB_TOG,
     RGB_VAD, RGB_VAI, RGB_HUD,  RGB_HUI,
     RGB_SAD, RGB_SAI, _______,  _______,
