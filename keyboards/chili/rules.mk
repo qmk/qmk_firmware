e729360e9549660f23204e0d9c4ea157f8fcf0ec
TerryMathews <terry@terrymathews.net>
2020-07-03 15:40:05 -0400

    Update keyboards/chili/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/chili/rules.mk
+++ keyboards/chili/rules.mk
@@ -14,7 +14,6 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-
 BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
