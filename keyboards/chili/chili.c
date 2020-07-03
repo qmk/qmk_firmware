10db80e1a782c4a5248f9778d6717c54e0076a6d
TerryMathews <terry@terrymathews.net>
2020-07-03 15:38:43 -0400

    Update keyboards/chili/chili.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/chili/chili.c
+++ keyboards/chili/chili.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "chili.h"
-#include "led.h"
 
 void matrix_init_kb(void) {
     // put your keyboard start-up code here
