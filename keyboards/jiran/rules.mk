4fd30099faf267a1d88bfb8d19c9c9eed41d7a84
Ladniy <32298096+Ladniy@users.noreply.github.com>
2020-07-04 06:29:26 +0800

    Update keyboards/jiran/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/jiran/rules.mk
+++ keyboards/jiran/rules.mk
@@ -24,7 +24,7 @@ BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
