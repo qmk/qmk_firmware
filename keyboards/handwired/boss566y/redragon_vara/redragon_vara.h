b10b6ef49edd326396ff92cddd0dbf06e376c0b3
boss566y <67247583+boss566y@users.noreply.github.com>
2020-07-03 19:20:34 -0400

    Update keyboards/handwired/boss566y/redragon_vara/redragon_vara.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/boss566y/redragon_vara/redragon_vara.h
+++ keyboards/handwired/boss566y/redragon_vara/redragon_vara.h
@@ -17,7 +17,7 @@
 
 #include "quantum.h"
 
-#define LAYOUT( \
+#define LAYOUT_fullsize_ansi( \
     K00,      K02, K03, K04, K05,   K07, K08, K09, K0A,    K6A, K69, K68, K67,  K66, K65, K64,                      \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K7A, K79, K78,       K76, K75, K74,  K73, K72, K71, K70, \
     K20,   K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K8A, K89, K87,     K86, K85, K84,  K83, K82, K81, K90, \
