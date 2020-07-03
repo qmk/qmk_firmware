4420c4692d6a0b38a1b8a054fb51faeb69719af1
Sam Gowland <gowland.sam@gmail.com>
2020-07-03 20:56:20 +0100

    Gowla Macro Board

---
+++ keyboards/gowla/keymaps/default/keymap.c
@@ -0,0 +1,29 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[0] = LAYOUT( \
+  LT(1, KC_MPRV),       KC_MNXT,      	KC_MPLY, \
+  KC_VOLD,       		KC_UP,			KC_VOLU, \
+  KC_LEFT,       		KC_DOWN,      	KC_RIGHT \
+),
+
+[1] = LAYOUT( \
+  KC_ESC,   KC_MPLY, 	RESET, \
+  KC_P7, 	KC_P1, 		M(0), \
+  KC_F1,	KC_F2,  	M(1)  \
+)
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    if (record->event.pressed) {
+        switch(id) {
+            case 0:
+                return MACRO(D(LCTL), T(C), U(LCTL), END);
+            case 1:
+                return MACRO(D(LCTL), T(V), U(LCTL), END);
+        }
+    }
+    return MACRO_NONE;
+};
---
+++ keyboards/gowla/readme.md
@@ -0,0 +1,14 @@
+Gowla Macro Board
+===
+
+Simple Compact 3x3 Macro Board for use with QMK, designed and sold by Gowla.
+
+Keyboard Maintainer: [Gowla](https://github.com/SamGowland/)
+Hardware Supported: Pro Micro ATmega32U4, Cherry Mount Switches  
+Hardware Availability: [Repo]https://github.com/SamGowland/Gowla-Macro-Board)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make gowla:default
+
+See [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) then the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information.
