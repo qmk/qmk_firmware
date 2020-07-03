4f4ee906ffe51eb2bc6a197e9d2197d17943ff08
TerryMathews <terry@terrymathews.net>
2020-07-03 15:39:46 -0400

    Update keyboards/chili/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/chili/rules.mk
+++ keyboards/chili/rules.mk
@@ -24,10 +24,9 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. Do not enable this with audio at the same time.
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight
 MIDI_ENABLE = no            # MIDI controls
 UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-OLED_DRIVER_ENABLE = no
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
