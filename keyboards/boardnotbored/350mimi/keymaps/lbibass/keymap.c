90bcbed0003849d92adc9ba965242a7e21377e8f
lbibass <ewdries02@gmail.com>
2020-07-03 14:45:30 -0400

    fixed my keymap.

--- keyboards/boardnotbored/350mimi/keymaps/lbibass/keymap.c
+++ keyboards/boardnotbored/350mimi/keymaps/lbibass/keymap.c
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 LAYOUT_60_tsangan_hhkb(
     KC_GRAVE,  KC_BRID,    KC_BRIU,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_MEDIA_PREV_TRACK,    KC_MPLY,    KC_MEDIA_NEXT_TRACK,    KC_MUTE,    KC_VOLD, KC_VOLU,  KC_INS, KC_DEL,
-    KC_CAPS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_UP, KC_RBRC, KC_BSPACE,
+    KC_CAPS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_UP, KC_RBRC, KC_DEL,
     KC_TRNS, KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_LEFT, KC_RIGHT, LT(7, KC_ENTER),
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_DOWN, KC_RSFT,  MO(3),
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                     KC_RALT, KC_RALT, MO(7)
   ),
 LAYOUT_60_tsangan_hhkb(
     KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_INS,KC_DEL,
-    KC_CAPS, _______, _______,   _______, RESET, _______, _______, _______, _______, _______, _______, KC_UP,   _______, KC_BSPACE,
+    KC_CAPS, _______, _______,   _______, RESET, _______, _______, _______, _______, _______, _______, KC_UP,   _______, KC_DEL,
     _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______,
     _______, _______, _______, _______, _______, HPT_TOG,HPT_DWLI,HPT_DWLD, _______, _______, KC_DOWN, _______, MO(2),
     _______, _______, _______,                    _______,                    _______, _______,  _______
