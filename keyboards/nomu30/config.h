bda7efc1b59f5e4f3761e7b015603537425fac73
Naoto Takai <takai@recompile.net>
2020-07-04 08:33:13 +0900

    Generate unique vendor VENDOR_ID

--- keyboards/nomu30/config.h
+++ keyboards/nomu30/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x3100
+#define VENDOR_ID       0x524B // recompile keys
+#define PRODUCT_ID      0x4E31 // Nomu30
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    recompile keys
 #define PRODUCT         recompile keys Nomu30
