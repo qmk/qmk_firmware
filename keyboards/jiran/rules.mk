579ee78975903ce74fe0e2d049d6aa799041fe92
Ladniy <32298096+Ladniy@users.noreply.github.com>
2020-07-04 06:26:55 +0800

    Update keyboards/jiran/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/jiran/rules.mk
+++ keyboards/jiran/rules.mk
@@ -17,10 +17,8 @@ endif
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
+#   change yes to no to disable
 #
-
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes     # Audio control and System control
