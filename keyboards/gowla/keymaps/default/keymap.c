0cd5e1f631c7c5d9be61ace617098af3b2867a89
Sam Gowland <10119462+SamGowland@users.noreply.github.com>
2020-07-03 21:26:39 +0100

    Update keyboards/gowla/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/gowla/keymaps/default/keymap.c
+++ keyboards/gowla/keymaps/default/keymap.c
@@ -1,19 +1,16 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-[0] = LAYOUT( \
-  LT(1, KC_MPRV),       KC_MNXT,      	KC_MPLY, \
-  KC_VOLD,       		KC_UP,			KC_VOLU, \
-  KC_LEFT,       		KC_DOWN,      	KC_RIGHT \
-),
-
-[1] = LAYOUT( \
-  KC_ESC,   KC_MPLY, 	RESET, \
-  KC_P7, 	KC_P1, 		M(0), \
-  KC_F1,	KC_F2,  	M(1)  \
-)
-
+    [0] = LAYOUT(
+        LT(1, KC_MPRV), KC_MNXT, KC_MPLY,
+        KC_VOLD,        KC_UP,   KC_VOLU,
+        KC_LEFT,        KC_DOWN, KC_RGHT
+    ),
+    [1] = LAYOUT(
+        KC_ESC,  KC_MPLY, RESET,
+        KC_P7,   KC_P1,   M(0),
+        KC_F1,   KC_F2,   M(1)
+    )
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
