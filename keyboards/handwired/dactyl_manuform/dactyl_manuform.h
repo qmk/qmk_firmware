7a8a43387592e41748fddf5708bdfbe6af79cef9
Dedalo <dedalo@ferengi.com.ar>
2020-07-04 15:43:45 -0300

    add 78key version of 5x7 layout, compiling. no key personalizaion yet.

--- keyboards/handwired/dactyl_manuform/dactyl_manuform.h
+++ keyboards/handwired/dactyl_manuform/dactyl_manuform.h
@@ -12,6 +12,8 @@
     #include "6x6.h"
 #elif KEYBOARD_handwired_dactyl_manuform_dmote_62key
     #include "62key.h"
+#elif KEYBOARD_handwired_dactyl_manuform_5x7_78key
+    #include "5x7.h"
 #endif
 
 #include "quantum.h"
