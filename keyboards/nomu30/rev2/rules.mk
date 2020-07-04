fb4c1942e1bf50ed258c47cfe11732a3d1bb0b5f
Naoto Takai <takai@recompile.net>
2020-07-04 14:46:29 +0900

    Update keyboards/nomu30/rev2/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nomu30/rev2/rules.mk
+++ keyboards/nomu30/rev2/rules.mk
@@ -26,6 +26,6 @@ NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
