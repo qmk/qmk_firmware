df87eec342dd4201cd963df5a764ee44b4332caa
Sam Gowland <10119462+SamGowland@users.noreply.github.com>
2020-07-04 10:42:33 +0100

    Update keyboards/gowla/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/gowla/config.h
+++ keyboards/gowla/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0007
+#define PRODUCT_ID      0xE9B6
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Gowla
 #define PRODUCT         Gowla Macro Board
