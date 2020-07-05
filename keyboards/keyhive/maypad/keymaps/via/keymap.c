5a1aac9d6ee5344c8d859f6b0eb5285900676683
ilikepi2 <adquan@csu.fullerton.edu>
2020-07-04 23:09:28 -0700

    Update keyboards/keyhive/maypad/keymaps/via/keymap.c
    Update VIA Layout to match the default layout.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keyhive/maypad/keymaps/via/keymap.c
+++ keyboards/keyhive/maypad/keymaps/via/keymap.c
@@ -16,26 +16,11 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-LAYOUT_ortho_5x4(
-    KC_A, KC_B, KC_C, KC_D,
-    KC_E, KC_F, KC_G, KC_H,
-    KC_I, KC_J, KC_K, KC_L,
-    KC_M, KC_N, KC_O, KC_P,
-    KC_Q, KC_R, KC_S, KC_T
-)
-
+    LAYOUT_ortho_5x4(
+        KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,   KC_P0,   KC_PDOT, KC_PENT
+    )
 };
-
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
