57231a1e4bd8b073d02577774a75409913b4a91a
ilikepi2 <adquan@csu.fullerton.edu>
2020-07-04 22:58:09 -0700

    Update keymap.c
    Removed comma causing error when trying to compile.

--- keyboards/keyhive/maypad/keymaps/via/keymap.c
+++ keyboards/keyhive/maypad/keymaps/via/keymap.c
@@ -18,11 +18,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 LAYOUT_ortho_5x4(
-    KC_1, KC_2, KC_3, KC_4,
-    KC_1, KC_2, KC_3, KC_4,
-    KC_1, KC_2, KC_3, KC_4,
-    KC_1, KC_2, KC_3, KC_4,
-    KC_1, KC_2, KC_3, KC_4,
+    KC_A, KC_B, KC_C, KC_D,
+    KC_E, KC_F, KC_G, KC_H,
+    KC_I, KC_J, KC_K, KC_L,
+    KC_M, KC_N, KC_O, KC_P,
+    KC_Q, KC_R, KC_S, KC_T
 )
 
 };
