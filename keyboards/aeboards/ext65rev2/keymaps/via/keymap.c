2744cec9ac0606d7e3e79cf354e9617add1e7c17
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2020-07-05 08:03:55 +0000

    Update OLED usb status
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/aeboards/ext65rev2/keymaps/via/keymap.c
+++ keyboards/aeboards/ext65rev2/keymaps/via/keymap.c
@@ -72,11 +72,11 @@ void render_layer_state(void) {
     oled_write_ln(PSTR(" "), false);
 }
 
-void render_keylock_status(uint8_t led_usb_state) {
+void render_keylock_status(led_t led_state) {
     oled_write_ln(PSTR("Lock:"), false);
-    oled_write(PSTR("N"), led_usb_state & (1 << USB_LED_NUM_LOCK));
-    oled_write(PSTR("C"), led_usb_state & (1 << USB_LED_CAPS_LOCK));
-    oled_write_ln(PSTR("S"), led_usb_state & (1 << USB_LED_SCROLL_LOCK));
+    oled_write(PSTR("N"), led_state.num_lock);
+    oled_write(PSTR("C"), led_state.caps_lock);
+    oled_write_ln(PSTR("S"), led_state.scroll_lock);
     oled_write_ln(PSTR(" "), false);
 }
 
