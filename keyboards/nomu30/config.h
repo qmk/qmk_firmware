3756994513832f7b09a949180cad6792cf7f5c78
Naoto Takai <takai@recompile.net>
2020-07-06 07:45:12 +0900

    Update keyboards/nomu30/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nomu30/config.h
+++ keyboards/nomu30/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x4E31 // Nomu30
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    recompile keys
-#define PRODUCT         recompile keys Nomu30
+#define PRODUCT         Nomu30
 #define DESCRIPTION     recompile keys Nomu30
 
 /* key matrix size */
