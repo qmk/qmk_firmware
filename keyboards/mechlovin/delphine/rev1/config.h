af4abecdaa8a350200f88e606208c786269f417f
mechlovin <57231893+mechlovin@users.noreply.github.com>
2020-07-06 13:33:47 +0700

    ADD

--- keyboards/mechlovin/delphine/rev1/config.h
+++
@@ -1,5 +0,0 @@
-
-#pragma once
-
-#define PRODUCT_ID      0xDEF1
-#define DEVICE_VER      0x0001
---
+++ keyboards/mechlovin/delphine/rgb_led/rules.mk
@@ -0,0 +1,7 @@
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration
+RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = IS31FL3731     # Use RGB matrix
