ff787cca3006b0132293110cc442e88f76b56538
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-07-06 17:37:31 +0800

    reverts breaking changes

--- keyboards/twill/twill.c
+++ keyboards/twill/twill.c
@@ -28,15 +28,14 @@ void matrix_init_kb(void) {
     matrix_init_user();
 }
 
-bool led_update_kb(led_t led_state) {
+void led_set_kb(uint8_t usb_led) {
     // Bit 3 is Green LED, bit 4 is Red LED.
-    if (led_update_user(led_state)) {
-        if (led_state.caps_lock) {
-            send_data = 0x18;
-        } else {
-            send_data = 0x10;
-        }
-        i2c_writeReg((PORT_EXPANDER_ADDRESS << 1), 0x09, &send_data, 1, 20);
+    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+        send_data = 0x18;
+    } else {
+        send_data = 0x10;
     }
-    return true;
+    i2c_writeReg((PORT_EXPANDER_ADDRESS << 1), 0x09, &send_data, 1, 20);
+
+    led_set_user(usb_led);
 }
