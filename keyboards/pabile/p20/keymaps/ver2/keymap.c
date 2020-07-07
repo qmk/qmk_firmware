7699232b748d0961a5e6ec6caebbb974adcf4b56
pabile <1162412+pabile@users.noreply.github.com>
2020-07-07 20:33:55 +0800

    Update keymap.c

--- keyboards/pabile/p20/keymaps/ver2/keymap.c
+++ keyboards/pabile/p20/keymaps/ver2/keymap.c
@@ -2,11 +2,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_5x4(
-        KC_NLCK, KC_PSLS,      KC_PAST, KC_PMNS, 
-        KC_P7,   KC_P8,        KC_P9,   KC_PPLS, 
-        KC_P4,   KC_P5,        KC_P6,   KC_COMM, 
-        KC_P1,   KC_P2,        KC_P3,   KC_EQL, 
-        KC_P0,   KC_P0(KC_P0), KC_PDOT, KC_PENT)
+        KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
+        KC_P4,   KC_P5,   KC_P6,   KC_COMM, 
+        KC_P1,   KC_P2,   KC_P3,   KC_EQL, 
+        KC_P0,   KC_P0,   KC_PDOT, KC_PENT)
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
@@ -20,7 +20,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
       tap_code(KC_MS_U); /*mouse up*/
     } else {
-      tap_code(KC_MS_D); /*volume down*/
+      tap_code(KC_MS_D); /*mouse down*/
     }
   } else if (index == 2) { /* Second encoder */  
     if (clockwise) {
