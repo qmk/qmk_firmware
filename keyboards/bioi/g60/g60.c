3f7cbd679318a0be96d87a07dd900820314789bf
scottywei <scot.wei@gmail.com>
2020-07-07 15:58:14 +0800

    Update keyboards/bioi/g60/g60.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/bioi/g60/g60.c
+++ keyboards/bioi/g60/g60.c
@@ -3,27 +3,15 @@
 #include "../ble.h"
 #include "quantum.h"
 
-void led_set_kb(uint8_t usb_led)
-{
-	if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK))
-	{
-		writePinLow(F0);
-	}
-	else
-	{
-		writePinHigh(F0);
-	}
-	led_set_user(usb_led);
+void keyboard_pre_init_kb(void) {
+    setPinOutput(F0);
+    writePinHigh(F0);
+    keyboard_pre_init_user();
 }
 
-void keyboard_pre_init_kb(void)
-{
-	setPinOutput(F0);
-	writePinHigh(F0);
-	keyboard_pre_init_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record)
-{
-	return process_record_user(keycode, record);
+bool led_update_kb(led_t led_state) {
+    if (led_update_user(led_state)) {
+        writePin(F0, !led_state.caps_lock);
+    }
+    return true;
 }
