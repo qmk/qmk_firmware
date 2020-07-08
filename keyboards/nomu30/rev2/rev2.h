1840c15b094adcaba751cc8355e0fda7a08948e1
Naoto Takai <takai@recompile.net>
2020-07-09 06:47:17 +0900

    Move LAYOUT macro to top level to avoid code duplication

--- keyboards/nomu30/rev2/rev2.h
+++ keyboards/nomu30/rev2/rev2.h
@@ -17,22 +17,3 @@
 #pragma once
 
 #include "quantum.h"
-
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-#define LAYOUT( \
-        K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
-  K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110,       \
-  K200, K201, K202, K203, K204, K205, K206, K207, K208                    \
-) \
-{ \
-  { KC_NO, K001, K002, K003, K004, K005, K006, K007, K008, K009,  K010,  K011  }, \
-  { K100,  K101, K102, K103, K104, K105, K106, K107, K108, K109,  K110,  KC_NO }, \
-  { K200,  K201, K202, K203, K204, K205, K206, K207, K208, KC_NO, KC_NO, KC_NO }, \
-}
