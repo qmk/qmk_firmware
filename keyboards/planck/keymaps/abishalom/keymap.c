7ecb4ec21a58ca2edc081b71ec9bced3d1e425f1
Abi Shalom <abishalom1997@gmail.com>
2020-07-07 21:08:28 -0500

    Update keyboards/planck/keymaps/abishalom/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/planck/keymaps/abishalom/keymap.c
+++ keyboards/planck/keymaps/abishalom/keymap.c
@@ -186,7 +186,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         #endif
       }
       return false;
-      break;
     case PLOVER:
       if (record->event.pressed) {
         #ifdef AUDIO_ENABLE
