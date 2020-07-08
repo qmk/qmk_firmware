5dfa3036229ef9b98c88d98120c70d78dfcaf09a
pcoves <33952527+pcoves@users.noreply.github.com>
2020-07-08 08:12:14 +0200

    Update users/pcoves/unicode.c
    Thanks !
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- users/pcoves/unicode.c
+++ users/pcoves/unicode.c
@@ -10,7 +10,7 @@ bool process_record_unicode(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) send_unicode_string("(ヘ･_･)ヘ┳━┳");
             return false;
         case EMOTE2:
-            if (record->event.pressed) send_unicode_string("¯\_(ツ)_/¯");
+            if (record->event.pressed) send_unicode_string("¯\\_(ツ)_/¯");
             return false;
         case EMOTE3:
             if (record->event.pressed) send_unicode_string("ಠ_ಠ");
