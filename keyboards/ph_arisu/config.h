e4edf8a1c69d65bfdf1c32efc472c7266fa8d11b
Leivince John <devinceble@gmail.com>
2020-07-09 18:44:25 +0800

    Update Remove RGB_DI_PIN

--- keyboards/ph_arisu/config.h
+++ keyboards/ph_arisu/config.h
@@ -34,11 +34,3 @@
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
-
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
