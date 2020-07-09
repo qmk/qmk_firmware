acdc369f0b4fd2bcd947e6a8d7d6d75385a3b3be
Klesh Wong <klesh@qq.com>
2020-07-09 23:51:28 +0800

    [misc] fine tune keymap

--- keyboards/handwired/ks63/keymaps/default/keymap.c
+++ keyboards/handwired/ks63/keymaps/default/keymap.c
@@ -6,8 +6,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,             KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
 		LT(2, KC_TAB) , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T,                  KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
 		LT(1, KC_CAPS), KC_A   , KC_S   , KC_D   , KC_F   , KC_G,                  KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,                         KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , LT(4, KC_ESC),                         KC_SPC , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
+		LSFT_T(KC_GRV), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,                  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, RSFT_T(KC_BSPC),
+		LCTL_T(KC_MINS), KC_LGUI, LALT_T(KC_EQL), LT(3, KC_SPC) , LT(4, KC_ESC),          RGUI_T(KC_SPC) , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
 
 	[1] = KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS,
@@ -25,16 +25,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[3] = KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, RSFT(KC_7), RSFT(KC_8), RSFT(KC_9), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PMNS, RSFT(KC_4), RSFT(KC_5), RSFT(KC_6), KC_PLUS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PAST, RSFT(KC_1), RSFT(KC_2), RSFT(KC_3), KC_PSLS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               RSFT(KC_0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
 	[4] = KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
-		C(KC_TAB), A(KC_F4)  , C(KC_W), C(KC_R) , KC_TRNS, KC_TRNS,                      KC_WBAK, C(KC_PGUP), C(KC_PGDN), KC_WFWD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		C(KC_TAB), A(KC_F4)  , C(KC_W), C(KC_R) , KC_TRNS, KC_TRNS,                KC_WBAK, C(KC_PGUP), C(KC_PGDN), KC_WFWD, C(KC_P), KC_LPRN, KC_RPRN, KC_TRNS,
 		KC_TRNS, KC_HOME, C(KC_S), KC_DEL , KC_RGHT, KC_TRNS,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, C(KC_Z), KC_CUT, KC_COPY, KC_PSTE, KC_LEFT,                      KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_TRNS, KC_TRNS,
+		KC_TRNS, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_LEFT,                      C(KC_N), KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
 	[5] = KEYMAP(
