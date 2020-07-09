028b17c6be326a2863f4a9042b9ccd3cb3650767
ladbahuy <67696967+ladbahuy@users.noreply.github.com>
2020-07-09 14:18:12 +0700

    Update config.h

--- keyboards/rart/rartpad/config.h
+++ keyboards/rart/rartpad/config.h
@@ -39,10 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/* number of backlight levels */
-
 #define NUM_LOCK_LED_PIN D7
-#define BACKLIGHT_LEVELS 0
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
