1cb26a66f5d96b628ea1b3312a80800aa2c8527e
obosob <obosob@riseup.net>
2020-07-09 13:06:20 +0100

    Add Arch-36 keyboard

---
+++ keyboards/arch_36/rules.mk
@@ -0,0 +1,22 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = atmel-dfu
+
+LAYOUTS = split_3x5_3
+
+SPLIT_KEYBOARD = yes       # Split common
+
+# Build Options
+#   change yes to no to disable
+#
+OLED_DRIVER_ENABLE = yes    # Enables the use of OLED displays
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
