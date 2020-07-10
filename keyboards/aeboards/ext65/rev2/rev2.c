770dcfb5e4a3da8ce26291cae5988b343e5be08e
Xelus22 <preyas22@gmail.com>
2020-07-11 00:07:46 +1000

    move board_init to only if OLED is enabled

--- keyboards/aeboards/ext65/rev2/rev2.c
+++ keyboards/aeboards/ext65/rev2/rev2.c
@@ -1,11 +1,11 @@
 #include "rev2.h"
 
+#ifdef OLED_DRIVER_ENABLE
 void board_init(void) {
   SYSCFG->CFGR1 |= SYSCFG_CFGR1_I2C1_DMA_RMP;
   SYSCFG->CFGR1 &= ~(SYSCFG_CFGR1_SPI2_DMA_RMP);
 }
 
-#ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_90;  // rotates the display 90 degrees
 }
