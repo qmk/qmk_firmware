86357f110c5442200962a98b16cec538c6b9b0a5
Klesh Wong <klesh@qq.com>
2020-07-10 17:06:52 +0800

    ...

--- keyboards/handwired/ks63/keymaps/default/keymap.c
+++ keyboards/handwired/ks63/keymaps/default/keymap.c
@@ -5,9 +5,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = KEYMAP(
 		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,             KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
 		LT(2, KC_TAB) , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T,                  KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
-		LT(1, KC_CAPS), KC_A   , KC_S   , KC_D   , KC_F   , KC_G,                  KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT,
+        LT(3, KC_CAPS), KC_A   , KC_S   , KC_D   , KC_F   , KC_G,                  KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT,
 		LSFT_T(KC_GRV), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,                  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, RSFT_T(KC_BSPC),
-		LCTL_T(KC_MINS), KC_LGUI, LALT_T(KC_EQL) , KC_SPC , LT(4, KC_ESC),         LT(3, KC_SPC) , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
+		LCTL_T(KC_MINS), KC_LGUI, LALT_T(KC_EQL) , KC_SPC , LT(4, KC_ESC),         LT(1, KC_SPC) , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
 
 	[1] = KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
