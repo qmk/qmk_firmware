0626c82365e5039c375833b62beeefd844b59f27
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-07-12 01:14:36 +0900

    claw44: change enter and space keys

--- keyboards/claw44/keymaps/ymotongpoo/keymap.c
+++ keyboards/claw44/keymaps/ymotongpoo/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
      KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSFT,
   //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
-                       KC_A_DEL, KC_LGUI, RAISE,    LOWER,       KC_ENT,  KC_SPC,   KC_RALT, KC_DEL
+                       KC_A_DEL, KC_LGUI, RAISE,    LOWER,       KC_RALT, KC_ENT,   KC_SPC,  KC_DEL
   //                 `----------+--------+---------+--------'   `--------+---------+--------+---------'
   ),
 
