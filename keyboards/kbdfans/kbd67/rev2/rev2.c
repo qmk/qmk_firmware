63cdf5a1ad772d4844f7f6a8463a3ed69c67455f
moyi4681 <moyi4681@users.noreply.github.com>
2020-07-12 16:14:18 +0800

    Update rev2.c

--- keyboards/kbdfans/kbd67/rev2/rev2.c
+++ keyboards/kbdfans/kbd67/rev2/rev2.c
@@ -1,7 +1,5 @@
 #include "rev2.h"
 void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
     setPinOutput(D4);
     matrix_init_user();
 }
@@ -11,4 +9,5 @@ bool led_update_kb(led_t led_state) {
         writePin(D4, !led_state.caps_lock);
     }
     return res;
+    return led_update_user(led_state);
 }
