7ba5d5006a0abe92a2630b2a36987d274c14677c
TJ Campie <tom.campie@gmail.com>
2020-07-14 07:51:37 -0500

    Change names of layout macros

--- keyboards/qaz/qaz.h
+++ keyboards/qaz/qaz.h
@@ -4,7 +4,7 @@
 
 #define XXX KC_NO
 
-#define LAYOUT_qaz( \
+#define LAYOUT_split_space( \
   K00, K01, K02, K03, K04, K05, K06, K40, K41,  K42, \
   K10,  K11, K12, K13, K14, K15, K16, K50, K51, K52, \
   K20,    K21, K22, K23, K24, K25, K26, K43,    K53, \
@@ -18,7 +18,7 @@
 { K50, K51, K52, K53, XXX, XXX, XXX } \
 }
 
-#define LAYOUT_qaz_big_space( \
+#define LAYOUT( \
   K00, K01, K02, K03, K04, K05, K06, K40, K41,  K42, \
   K10,  K11, K12, K13, K14, K15, K16, K50, K51, K52, \
   K20,    K21, K22, K23, K24, K25, K26, K43,    K53, \
