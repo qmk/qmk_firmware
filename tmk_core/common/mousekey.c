4d3efba4e4f69621b5a479ec15a3ca2dbc35fb98
nathanvercaemert <50712356+nathanvercaemert@users.noreply.github.com>
2020-07-15 08:02:14 -0400

    Update tmk_core/common/mousekey.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- tmk_core/common/mousekey.c
+++ tmk_core/common/mousekey.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2011 Jun Wako <wakojun@gmail.com>
+ * Copyright 2011 Jun Wako <wakojun@gmail.com>
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
