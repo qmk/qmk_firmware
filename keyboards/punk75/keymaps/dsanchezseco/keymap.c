41afc675b0bfcd4716913bfd0af70b86f1751a9d
dsanchezseco <d.seco@protonmail.com>
2020-07-15 10:38:15 +0200

    personal keymap for punk75

--- keyboards/punk75/keymaps/dsanchezseco/keymap.c
+++ keyboards/punk75/keymaps/dsanchezseco/keymap.c
@@ -17,56 +17,39 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
+    _DVORAK,
+    _QWERTY,
     _FN
 };
 
 // Defines the keycodes used by our macros in process_record_user
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* DVORAK
- * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      |        |        |        | 6      | 7      | 8      | 9      | 0      | BACKSP |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | '      | ,      | .      | P      | Y      |        |        |        | F      | G      | C      | R      | L      | /      |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | CTRL   | A      | O      | E      | U      | I      |        |        |        | D      | H      | T      | N      | S      | -      |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | ;      | Q      | J      | K      | X      |        |        |        | B      | M      | W      | V      | Z      | SFT_ENT|
- * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   | LALT   | FN     |        | SPACE  | SPACE  |        | BACKSPC| BACKSPC|        | FN     | RALT   | RGUI   | RCTRL  |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
 
-    [_BASE] = LAYOUT_grid( /* QWERTY */
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,  _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    _______,  _______, _______, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
-        KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    _______,  _______, _______, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
-        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    _______,  _______, _______, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SFTENT,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), _______, KC_SPC,  _______,  _______, _______, KC_BSPC, _______, MO(_FN), KC_RALT, KC_RGUI, KC_RCTL
+    [_DVORAK] = LAYOUT_grid( /* DVORAK */
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,  KC_NLCK, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MUTE,
+        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_P7,    KC_P8,   KC_P9,   KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+        _______, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_P4,    KC_P5,   KC_P6,   KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_P1,    KC_P2,   KC_P3,   KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SFTENT,
+        KC_LCTL, KC_LGUI, KC_LALT, _______,  MO(_FN), KC_SPC,  KC_SPC,   KC_P0,   KC_BSPC, KC_BSPC, MO(_FN), _______, KC_RALT, KC_RGUI, KC_RCTL
     ),
 
-/* FUNCTION
- * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      |        | RESET  |        |        |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   |        |        |        |        |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        | RGB TG | FN     | RGB RMD| RGB MD | P0     |        | P.     | PENT   | PENT   | FN     |        |        |        |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
+    [_QWERTY] = LAYOUT_grid( /* QWERTY */
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,  _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,  _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH,
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,  _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,  _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, _______,
+        _______, _______, _______, _______, MO(_FN), KC_SPC,  KC_SPC,   _______, KC_BSPC, KC_BSPC, MO(_FN), _______, _______, _______, _______
+    ),
 
     [_FN] = LAYOUT_grid( /* FUNCTION */
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
-        KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______,
-        KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
-        _______, _______, RGB_TOG, MO(_FN), RGB_RMOD,RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, MO(_FN), _______, _______, _______
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, MO(_FN), _______, _______, _______, KC_DEL,  KC_DEL,  MO(_FN), _______, _______, _______, _______
     )
+
 };
 
 void led_keypress_update(uint8_t led, uint16_t keycode, keyrecord_t *record) {
