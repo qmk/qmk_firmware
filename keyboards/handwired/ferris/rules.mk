036be05efbe88a3b093d358b73b8aa37044c3f7b
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-07-16 19:58:44 +0100

    Remove FAUXCLICKY_ENABLE from rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/ferris/rules.mk
+++ keyboards/handwired/ferris/rules.mk
@@ -19,7 +19,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 UNICODE_ENABLE = yes
 CUSTOM_MATRIX = lite
 NO_USB_STARTUP_CHECK = yes
