6e2acf851c3ca218689f59701b10cf1fa515003c
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-07-16 12:05:41 -0700

    extend MCU support, part 2
    - update processing for atmega328 and atmega328p

--- lib/python/qmk/cli/new/keyboard.py
+++ lib/python/qmk/cli/new/keyboard.py
@@ -134,6 +134,10 @@ def new_keyboard(cli):
     if mcu in ['atmega16u2', 'atmega32u2', 'atmega328', 'atmega328p']:
         file_contents = Path(keyboard_path / 'config.h').read_text().replace("F1, F0, B0", "B0, B1, B2")
         Path(keyboard_path / 'config.h').write_text(file_contents)
+    # Replace BOOTLOADER value for atmega328p
+    if mcu == 'atmega328p':
+        file_contents = Path(keyboard_path / 'rules.mk').read_text().replace("BOOTLOADER = atmel-dfu", "BOOTLOADER = USBasp")
+        Path(keyboard_path / 'rules.mk').write_text(file_contents)
 
     # end message to user
     cli.log.info("%s keyboard directory created in: %s", common_name, keyboard_path)
