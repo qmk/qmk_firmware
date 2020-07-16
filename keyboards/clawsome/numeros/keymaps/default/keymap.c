4ea95f41a711300fc59d477265d807cc7adf5609
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-07-16 09:34:41 -0400

    Update keyboards/clawsome/numeros/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/clawsome/numeros/keymaps/default/keymap.c
+++ keyboards/clawsome/numeros/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * | 0       | .  |    |
     * ---------------------
     */
-    [0] = LAYOUT(
+    [0] = LAYOUT_numpad_5x4(
         KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,
@@ -38,4 +38,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 };
-
