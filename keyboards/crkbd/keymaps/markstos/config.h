1868ba09f20b4c78f9e5d4c1799f84eb69eef1c0
Mark Stosberg <mark@rideamigos.com>
2020-07-16 21:33:35 -0400

    [Keymap] add crkbd/keymaps/markstos

---
+++ keyboards/crkbd/keymaps/markstos/config.h
@@ -0,0 +1,96 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+// #define MASTER_LEFT
+#define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define SSD1306OLED
+
+#define USE_SERIAL_PD2
+
+// By default, when holding a dual-function key shortly after tapping it, the
+// tapped key will begin repeating. This is handy for fast typists when typing
+// words with double letters, such as "happy". If you turn this setting ON, it
+// will be counted as a held modifier instead.
+//#define TAPPING_FORCE_HOLD
+
+// Customized by markstos
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+// used for Tapping Term on thumb keys
+#define TAPPING_TERM_THUMB 125
+
+// If you press a dual-role key, press another key, and then release the
+// dual-role key, all within the tapping term, by default the dual-role key
+// will perform its tap action. If the HOLD_ON_OTHER_KEY_PRESS option is
+// enabled, the dual-role key will perform its hold action instead.
+#define HOLD_ON_OTHER_KEY_PRESS
+
+// markstos: prevent keydown and keyup from firing on different layers
+#define PREVENT_STUCK_MODIFERS
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+    #define RGBLIGHT_ANIMATIONS
+    #define RGBLED_NUM 27
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+// markstos: not sure if these are correct
+// They are intended to beep and flash during flashing
+#define QMK_LED     D5
+#define QMK_SPEAKER C6
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// When enabled, typing a mod-tap plus second within term will register as the mod-combo
+// Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold 
+#define PERMISSIVE_HOLD
+
+//#define RGBLIGHT_SLEEP
+//#define RGBLIGHT_EFFECT_BREATHING
+//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#define RGBLIGHT_EFFECT_KNIGHT
+//#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define COMBO_COUNT 3
+
+// Set the COMBO_TERM so low that I won't type the keys one after each other during normal typing.
+// They would have be held together intentionally to trigger this.
+#define COMBO_TERM 40
+
+// markstos
+//#define BACKLIGHT_PIN B7
+
+// These mostly affect my one-shot Shift key, providing a CapsLock alternative.
+// I want a relatively low timeout, so if I accidentally type "Shift", I can pause just briefly and move on.
+#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
+
---
+++ keyboards/crkbd/keymaps/markstos/rules.mk
@@ -0,0 +1,21 @@
+# markstos: Attempt to enable Bootmagic
+#BOOTMAGIC_ENABLE = lite
+
+# markstos: enable media keys
+EXTRAKEY_ENABLE = yes
+
+# markstos: smaller file size, little down-side
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+
+# markstos: for combos!
+COMBO_ENABLE = yes
+
+# markstos
+# Backlighting is not RGB underglow or RGB matrix
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+
+# This is for RGB *underglow*
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.md
+RGBLIGHT_ENABLE = no
+
+TAP_DANCE_ENABLE = no
