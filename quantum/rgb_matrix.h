49a0ec1eb76d0e2f1082474f77df2b3e6ca41309
Drashna Jael're <drashna@live.com>
2020-07-18 02:02:37 -0700

    Run clang-format on rgb_matrix.h

--- quantum/rgb_matrix.h
+++ quantum/rgb_matrix.h
@@ -58,7 +58,7 @@
 #endif
 
 #define RGB_MATRIX_INDICATOR_SET_COLOR(i, r, g, b) \
-    if (i >= led_min && i <= led_max) {    \
+    if (i >= led_min && i <= led_max) {            \
         rgb_matrix_set_color(i, r, g, b);          \
     }
 
@@ -108,7 +108,7 @@ void rgb_matrix_indicators(void);
 void rgb_matrix_indicators_kb(void);
 void rgb_matrix_indicators_user(void);
 
-void rgb_matrix_indicators_advanced(effect_params_t* params);
+void rgb_matrix_indicators_advanced(effect_params_t *params);
 void rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max);
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max);
 
