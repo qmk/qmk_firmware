0c9edc4bdf3db8adec7d9730e5b386d427aad909
imchipwood <imchipwood@gmail.com>
2020-07-18 01:12:54 -0700

    keyboards/dumbpad/v0x/v0x.c - remove matrix_scan_kb, process_record_kb
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/dumbpad/v0x/v0x.c
+++ keyboards/dumbpad/v0x/v0x.c
@@ -57,15 +57,3 @@ void matrix_init_kb(void) {
 
     matrix_init_user();
 }
-
-void matrix_scan_kb(void) {
-    // put your looping keyboard code here
-    // runs every cycle (a lot)
-    matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    // put your per-action keyboard code here
-    // runs for every action, just before processing by the firmware
-    return process_record_user(keycode, record);
-}
