bb139e598a92eacccd617e6d0b72c85114eeed49
pabile <1162412+pabile@users.noreply.github.com>
2020-07-19 19:50:56 +0800

    Update keymap.c

--- keyboards/pabile/p20/keymaps/ver2/keymap.c
+++ keyboards/pabile/p20/keymaps/ver2/keymap.c
@@ -1,10 +1,26 @@
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    DBLZERO = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case DBLZERO:
+        if (record->event.pressed) {
+            SEND_STRING("00");
+        } else {
+        }
+        break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_5x4(
         KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
-        KC_P4,   KC_P5,   KC_P6,   KC_COMM, 
-        KC_P1,   KC_P2,   KC_P3,   KC_EQL, 
-        KC_P0,   KC_P0,   KC_PDOT, KC_PENT)
+        KC_P7,   KC_P8,   KC_P9,   KC_TAB, 
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
+        KC_P1,   KC_P2,   KC_P3,   KC_DEL, 
+        KC_P0,   DBLZERO, KC_PDOT, KC_PENT)
 };
