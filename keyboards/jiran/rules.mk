57e0b3235883ff95d75899b7d1e3aed2c96c0779
Ladniy <32298096+Ladniy@users.noreply.github.com>
2020-07-21 01:47:26 +0800

    Update keyboards/jiran/rules.mk
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/jiran/rules.mk
+++ keyboards/jiran/rules.mk
@@ -20,7 +20,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 SPLIT_KEYBOARD = yes
 
 DEFAULT_FOLDER = jiran/rev1
