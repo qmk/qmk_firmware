fff33ee9dfba55ba6e82ab7bbe733b4b3dd915a6
Ladniy <32298096+Ladniy@users.noreply.github.com>
2020-07-21 01:46:12 +0800

    Update keyboards/jiran/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/jiran/config.h
+++ keyboards/jiran/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0300
 #define MANUFACTURER    Ladniy
 #define PRODUCT         Jiran
-#define DESCRIPTION     Ergo-Split Keyboard
 
 /* key matrix size */
 // Rows are doubled-up
