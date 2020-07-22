1f9ded84a03bbf465fa9fe98dd0068dbbe403aa2
satt99 <satt99kbd@gmail.com>
2020-07-22 22:00:48 +0900

    Change Vendor ID

--- keyboards/satt/vision/config.h
+++ keyboards/satt/vision/config.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x5354
+#define VENDOR_ID       0x5454
 #define PRODUCT_ID      0x5649
 #define DEVICE_VER      0x0001
 #define MANUFACTURER SatT
