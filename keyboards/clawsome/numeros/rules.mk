38d236897a315018c09ac858b47e22b825d783d6
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-07-22 17:52:07 -0400

    Update keyboards/clawsome/numeros/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/clawsome/numeros/rules.mk
+++ keyboards/clawsome/numeros/rules.mk
@@ -18,10 +18,7 @@ SLEEP_LED_ENABLE = no        # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no             # USB Nkey Rollover
 BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
-MIDI_ENABLE = no             # MIDI support
-UNICODE_ENABLE = no          # Unicode
 BLUETOOTH_ENABLE = no        # Enable Bluetooth
 AUDIO_ENABLE = no            # Audio output
-FAUXCLICKY_ENABLE = no       # Use buzzer to emulate clicky switches
 
 LAYOUTS = numpad_5x4
