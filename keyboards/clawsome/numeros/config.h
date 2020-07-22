bce211ef524ef7b4415d7e623804b8164e52b7ef
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-07-22 17:51:49 -0400

    Update keyboards/clawsome/numeros/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/clawsome/numeros/config.h
+++ keyboards/clawsome/numeros/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AlisGraveNil
 #define PRODUCT         Los Numeros
-#define DESCRIPTION     A QMK-powered number pad
 
 /* key matrix size */
 #define MATRIX_ROWS 5
