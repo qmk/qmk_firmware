5fb5a1618ae059f24e17e460e07033316ebc242e
zeriyoshi <zeriyoshi@gmail.com>
2020-07-23 14:49:04 +0900

    Fix IDOBAO 80 disable console and LEDs.

--- keyboards/id80/config.h
+++ keyboards/id80/config.h
@@ -62,21 +62,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 #define BACKLIGHT_PIN B6
-// #define BACKLIGHT_BREATHING
+#define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 3
 
 #define CAPS_LOCK_LED_PIN C7
 
-#define RGB_DI_PIN E2
-#ifdef RGB_DI_PIN
-    #define RGBLED_NUM 20       /* 16 underglow LEDs, 4 top LEDs */
-    #define RGBLIGHT_HUE_STEP 8
-    #define RGBLIGHT_SAT_STEP 8
-    #define RGBLIGHT_VAL_STEP 8
-    #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-    #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-/*== all animations enable ==*/
-    #define RGBLIGHT_ANIMATIONS
+// #define RGB_DI_PIN E2
+// #ifdef RGB_DI_PIN
+//     #define RGBLED_NUM 20       /* 16 underglow LEDs, 4 top LEDs */
+//     #define RGBLIGHT_HUE_STEP 8
+//     #define RGBLIGHT_SAT_STEP 8
+//     #define RGBLIGHT_VAL_STEP 8
+//     #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+//     #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+// /*== all animations enable ==*/
+//     #define RGBLIGHT_ANIMATIONS
 // /*== or choose animations ==*/
 //     #define RGBLIGHT_EFFECT_BREATHING
 //     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
@@ -87,7 +87,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //     #define RGBLIGHT_EFFECT_RGB_TEST
 //     #define RGBLIGHT_EFFECT_ALTERNATING
-#endif
+// #endif
 
 /* Bootmagic Lite key configuration: use the Esc key */
 #define BOOTMAGIC_LITE_ROW      0
