febcb0f664b631057e3050c21d1886c56060c460
moyi4681 <moyi4681@users.noreply.github.com>
2020-07-23 12:24:55 -0700

    add bocc keyboard

---
+++ keyboards/dztech/bocc/bocc.c
@@ -0,0 +1,13 @@
+#include "bocc.h"
+void matrix_init_kb(void) {
+    setPinOutput(E6);
+    matrix_init_user();
+}
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        writePin(E6, !led_state.caps_lock);
+    }
+    return res;
+    return led_update_user(led_state);
+}
