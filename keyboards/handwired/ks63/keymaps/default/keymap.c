4d045fbeb6ab9b36f10d53298f4fdb0a45358d58
Klesh Wong <klesh@qq.com>
2020-07-23 11:46:26 +0800

    Update keyboards/handwired/ks63/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/ks63/keymaps/default/keymap.c
+++ keyboards/handwired/ks63/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT_T(KC_GRV), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,                  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, RSFT_T(KC_BSPC),
         LCTL_T(KC_MINS), KC_LGUI, LALT_T(KC_EQL) , KC_SPC , LT(4, KC_ESC),         LT(1, KC_SPC) , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
 
-    [1] = KEYMAP(
+    [1] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PSCR, KC_F7  , KC_F8  , KC_F9  , KC_F12 , KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PAUS, KC_F4  , KC_F5  , KC_F6  , KC_F11 , KC_TRNS, KC_TRNS,
