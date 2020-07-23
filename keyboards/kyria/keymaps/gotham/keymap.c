dbfa81a24b6ac58d547e05f5dc38dacdbf9f6805
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-07-22 21:43:26 -0700

    Use starfield as an interactive screensaver

--- keyboards/kyria/keymaps/gotham/keymap.c
+++ keyboards/kyria/keymaps/gotham/keymap.c
@@ -117,7 +117,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-layer_state_t layer_state_set_keymap(layer_state_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); }
+#if defined(OLED_DRIVER_ENABLE) && defined(OLED_ANIMATIONS_ENABLE)
+static bool oled_showing_info;
+static uint16_t oled_info_timer;
+#endif
+
+layer_state_t layer_state_set_keymap(layer_state_t state) {
+    layer_state_t new_state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+#ifdef OLED_ANIMATIONS_ENABLE
+    // If entering or leaving game mode, show info
+    if (IS_LAYER_ON_STATE(state, _GAME) || (IS_LAYER_ON(_GAME) && !IS_LAYER_ON_STATE(state, _GAME))) { oled_info_timer = timer_read(); }
+#endif
+    return new_state;
+}
+
+layer_state_t default_layer_state_set_keymap(layer_state_t state) {
+#ifdef OLED_ANIMATIONS_ENABLE
+    oled_info_timer = timer_read();
+#endif
+    return state;
+}
 
 #ifdef THUMBSTICK_ENABLE
 bool process_record_keymap_thumbstick(uint16_t keycode, keyrecord_t *record) {
@@ -132,8 +151,65 @@ bool process_record_keymap_thumbstick(uint16_t keycode, keyrecord_t *record) {
 #endif
 
 #ifdef OLED_DRIVER_ENABLE
-oled_rotation_t oled_init_keymap(oled_rotation_t rotation) { return OLED_ROTATION_180; }
+oled_rotation_t oled_init_keymap(oled_rotation_t rotation) {
+#    ifdef OLED_ANIMATIONS_ENABLE
+    oled_showing_info = false;
+    oled_info_timer = timer_read() + OLED_INFO_TIMEOUT;
+#    endif
+    return OLED_ROTATION_180;
+}
+
+#    ifdef OLED_ANIMATIONS_ENABLE
+void render_status_main(void) {
+    if (IS_LAYER_ON(_ADJUST) || timer_elapsed(oled_info_timer) <= OLED_INFO_TIMEOUT) {
+        if (!oled_showing_info) {
+            oled_clear();
+            set_starfield_center();
+            oled_showing_info = true;
+        }
+        render_layout();
+        render_layer();
+#        ifdef ENCODER_ENABLE
+        render_encoders();
 #endif
+#ifdef THUMBSTICK_ENABLE
+        render_thumbstick();
+#endif
+    } else {
+        if (oled_showing_info) {
+            oled_clear();
+            oled_showing_info = false;
+        }
+#if defined(OLED_ANIM_STARFIELD)
+        render_starfield();
+#elif defined(OLED_ANIM_IMAGE_BOUNCE)
+        render_image_bounce();
+#endif
+    }
+}
+
+bool process_record_keymap_oled(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+#    ifdef THUMBSTICK_ENABLE
+            case TMB_MODE:
+#    endif
+#    ifdef ENCODER_ENABLE
+            case ENC_MODE_L ... ENC_MODE_R:
+#    endif
+                oled_info_timer = timer_read();
+                break;
+            default:
+#    ifdef OLED_ANIM_STARFIELD
+                starfield_speed_set(starfield_speed_get() + 8);
+#    endif
+                break;
+        }
+    }
+    return true;
+}
+#    endif // OLED_ANIMATIONS_ENABLE
+#endif // OLED_DRIVER_ENABLE
 
 #ifdef ENCODER_ENABLE
 void encoder_init_keymap(void) {
