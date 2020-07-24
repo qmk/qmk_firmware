1e0340c7d13a4b849ad22c9ad3ea2c06b734270f
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2020-07-23 20:21:58 -0500

    Update keyboards/sneakbox/aliceclone/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/sneakbox/aliceclone/keymaps/default/keymap.c
+++ keyboards/sneakbox/aliceclone/keymaps/default/keymap.c
@@ -1,4 +1,10 @@
-#include "kb.h"
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
