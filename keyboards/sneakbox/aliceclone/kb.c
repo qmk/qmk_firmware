3d17978fc24e873d2f346316cd234e727bf72cee
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2020-07-23 20:19:32 -0500

    Update keyboards/sneakbox/aliceclone/kb.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/sneakbox/aliceclone/kb.c
+++ keyboards/sneakbox/aliceclone/kb.c
@@ -1,1 +1,19 @@
-#include "kb.h"
+#include "aliceclone.h"
+
+void keyboard_pre_init_kb() {
+    setPinOutput(D7);
+    setPinOutput(D6);
+    setPinOutput(D4);
+    
+    keyboard_pre_init_user();
+}
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        writePin(D7, led_state.num_lock);
+        writePin(D6, led_state.caps_lock);
+        writePin(D4, led_state.scroll_lock);
+    }
+    return res;
+}
