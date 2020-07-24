fea91543b21d42e7bcc30ca038ad3038f1f63fca
Dmitry Nosachev <quartz64@gmail.com>
2020-07-24 21:09:20 +0300

    Update keyboards/handwired/z150/z150.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/z150/z150.c
+++ keyboards/handwired/z150/z150.c
@@ -17,19 +17,19 @@
 #include "z150.h"
 
 void matrix_init_kb(void) {
-	setPinOutput(NUM_LOCK_LED_PIN);
-  setPinOutput(CAPS_LOCK_LED_PIN);
-  setPinOutput(SCROLL_LOCK_LED_PIN);
-  
-  writePinLow(NUM_LOCK_LED_PIN);
-  writePinLow(CAPS_LOCK_LED_PIN);
-  writePinLow(SCROLL_LOCK_LED_PIN);
-  
-	matrix_init_user();
+    setPinOutput(NUM_LOCK_LED_PIN);
+    setPinOutput(CAPS_LOCK_LED_PIN);
+    setPinOutput(SCROLL_LOCK_LED_PIN);
+
+    writePinLow(NUM_LOCK_LED_PIN);
+    writePinLow(CAPS_LOCK_LED_PIN);
+    writePinLow(SCROLL_LOCK_LED_PIN);
+
+    matrix_init_user();
 }
 
 bool led_update_kb(led_t led_state) {
-bool res = led_update_user(led_state);
+    bool res = led_update_user(led_state);
     if(res) {
         writePin(NUM_LOCK_LED_PIN, !led_state.num_lock);
         writePin(CAPS_LOCK_LED_PIN, !led_state.caps_lock);
