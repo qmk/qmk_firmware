5a78d59205f40abbad8373ce0292631d668d7142
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2020-07-23 20:22:46 -0500

    Update keyboards/sneakbox/aliceclone/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/sneakbox/aliceclone/keymaps/default/keymap.c
+++ keyboards/sneakbox/aliceclone/keymaps/default/keymap.c
@@ -8,7 +8,7 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	KEYMAP(
+	[_BASE] = LAYOUT(
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, 
 		KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC, 
 		KC_ESC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, 
