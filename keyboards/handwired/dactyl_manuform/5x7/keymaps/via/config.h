db540e2832b0d3bed6ae2ca3a2c893d9c79a7f5d
RJ Bernau <Ashtoruin@users.noreply.github.com>
2020-07-25 22:36:39 +0100

    Adding VIA support for Dactyl Manuform 5x7 (#9462)
    * adding via support for Dactyl Manuform 5x7
    
    * Changing Vendor ID from FEED to 444D (DM)
    
    * Update keyboards/handwired/dactyl_manuform/4x6/config.h
    
    Fixing typo in Dactyl Manuform 4x6 Product Id
    
    Co-authored-by: Joel Challis <git@zvecr.com>
    
    * Update keyboards/handwired/dactyl_manuform/4x5/config.h
    
    Fixing typo in Dactyl Manuform 4x5 Product Id
    
    Co-authored-by: Joel Challis <git@zvecr.com>
    
    Co-authored-by: Roland Bernau <roland@bernau.dev>
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/via/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/via/config.h
@@ -1,6 +1,5 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,27 +15,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
 #pragma once
 
-#include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define PRODUCT_ID      0x3436
-#define DEVICE_VER      0x0001
-#define DESCRIPTION     A split keyboard for the cheap makers
+#define USE_SERIAL
 
-/* key matrix size */
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+//#define EE_HANDS
 // Rows are doubled-up
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 6
-
-// row-driven
-#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-// WS2812 RGB LED strip input and number of LEDs
-#define RGB_DI_PIN D3
-#define RGBLED_NUM 12
