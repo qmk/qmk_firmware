a36fde3286e0510df8791c8489f7740b8ad19f6a
RJ Bernau <Ashtoruin@users.noreply.github.com>
2020-07-25 20:00:07 +0100

    Update keyboards/handwired/dactyl_manuform/4x6/config.h
    Fixing typo in Dactyl Manuform 4x6 Product Id
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/dactyl_manuform/4x6/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define PRODUCT_ID      0x3536
+#define PRODUCT_ID      0x3436
 #define DEVICE_VER      0x0001
 #define DESCRIPTION     A split keyboard for the cheap makers
 
