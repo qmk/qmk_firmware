26cec4224b6bee25fb7eaa1a6c41979a8ede0e17
RJ Bernau <Ashtoruin@users.noreply.github.com>
2020-07-25 21:27:05 +0100

    Update keyboards/handwired/dactyl_manuform/4x5/config.h
    Fixing typo in Dactyl Manuform 4x5 Product Id
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/dactyl_manuform/4x5/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define PRODUCT_ID      0x3545
+#define PRODUCT_ID      0x3435
 #define DEVICE_VER      0x0001
 
 #define MANUFACTURER    tshort
