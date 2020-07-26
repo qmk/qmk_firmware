92d842e77312d0fa376c4a915d01ae192fad442d
zeriyoshi <zeriyoshi@gmail.com>
2020-07-26 23:48:59 +0900

    Re-enabling to IDOBAO80s underglow rgb light

--- keyboards/id80/config.h
+++ keyboards/id80/config.h
@@ -67,27 +67,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define CAPS_LOCK_LED_PIN C7
 
-// #define RGB_DI_PIN E2
-// #ifdef RGB_DI_PIN
-//     #define RGBLED_NUM 20       /* 16 underglow LEDs, 4 top LEDs */
-//     #define RGBLIGHT_HUE_STEP 8
-//     #define RGBLIGHT_SAT_STEP 8
-//     #define RGBLIGHT_VAL_STEP 8
-//     #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-//     #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-// /*== all animations enable ==*/
-//     #define RGBLIGHT_ANIMATIONS
-// /*== or choose animations ==*/
-//     #define RGBLIGHT_EFFECT_BREATHING
-//     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//     #define RGBLIGHT_EFFECT_SNAKE
-//     #define RGBLIGHT_EFFECT_KNIGHT
-//     #define RGBLIGHT_EFFECT_CHRISTMAS
-//     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//     #define RGBLIGHT_EFFECT_RGB_TEST
-//     #define RGBLIGHT_EFFECT_ALTERNATING
-// #endif
+#define RGB_DI_PIN E2
+#ifdef RGB_DI_PIN
+    #define RGBLED_NUM 20       /* 16 underglow LEDs, 4 top LEDs */
+    #define RGBLIGHT_HUE_STEP 8
+    #define RGBLIGHT_SAT_STEP 8
+    #define RGBLIGHT_VAL_STEP 8
+    #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+    #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+/*== all animations enable ==*/
+    #define RGBLIGHT_ANIMATIONS
+/*== or choose animations ==*/
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_SNAKE
+    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_RGB_TEST
+    #define RGBLIGHT_EFFECT_ALTERNATING
+#endif
 
 /* Bootmagic Lite key configuration: use the Esc key */
 #define BOOTMAGIC_LITE_ROW      0
