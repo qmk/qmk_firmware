90c0d994a18980498b62cadec48d11a24c22b0ef
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-07-27 14:39:15 +0800

    changes pid/vid

--- keyboards/twill/config.h
+++ keyboards/twill/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define VENDOR_ID       0x8968
-#define PRODUCT_ID      0x4748
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    UWA-Makers
 #define PRODUCT         Twill
