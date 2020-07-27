9a13e87ff3e5bfd087a58b369b4977981e3d8f37
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-07-27 14:34:27 +0800

    moves src to the bottom of the file

--- keyboards/twill/rules.mk
+++ keyboards/twill/rules.mk
@@ -1,5 +1,3 @@
-SRC = matrix.c \
-      i2c_master.c
 # MCU name
 MCU = atmega328p
 
@@ -40,5 +38,9 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
+
+# Adds matrix SRC
+SRC = matrix.c \
+      i2c_master.c
 
 CUSTOM_MATRIX           = yes
