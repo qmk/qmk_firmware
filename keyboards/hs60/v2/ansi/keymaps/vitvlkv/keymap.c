7e6ec34cc8c4ed79a24f9be0fdb0c4055e333378
vitvlkv <vitvlkv@yandex-team.ru>
2020-07-27 09:38:30 +0300

    Activate mouse with just one left hand

--- keyboards/hs60/v2/ansi/keymaps/vitvlkv/keymap.c
+++ keyboards/hs60/v2/ansi/keymaps/vitvlkv/keymap.c
@@ -15,12 +15,12 @@
  */
 
 // To compile firmware:
-// qmk_firmware$ qmk compile -kb hs60/v2/ansi -km vitvlkv
+// `qmk compile -kb hs60/v2/ansi -km vitvlkv`
 
 // To flash firmware:
 // plug in keyboard
 // press hardware 'reset' button (on the PCB)
-// execute command: 'qmk flash -kb hs60/v2/ansi -km vitvlkv'
+// execute command: `qmk flash -kb hs60/v2/ansi -km vitvlkv`
 #include QMK_KEYBOARD_H
 
 //This is the ANSI version of the PCB
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_60_ansi( /* Base */
     KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,\
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,\
-    MO(1),    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,\
+    MO(2),    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,\
     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_GRV,\
     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    MO(2),    MO(3)),
 
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,                                _______,                                _______,  _______,  KC_APP,   KC_RCTL),
 
 [2] = LAYOUT_60_ansi( /* Mouse and volume */
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,\
+    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,\
     _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_U,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,\
     _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,\
     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,            _______,\
