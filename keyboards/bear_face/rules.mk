195730130e9edfcfaf8f4e7eb472bbe3a4004a47
chemicalwill <36576135+chemicalwill@users.noreply.github.com>
2020-07-27 00:13:31 -0500

    Update keyboards/bear_face/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/bear_face/rules.mk
+++ keyboards/bear_face/rules.mk
@@ -18,8 +18,7 @@ NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI controls
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output6
 
 DEFAULT_FOLDER = bear_face/v1
