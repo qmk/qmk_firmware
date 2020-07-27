af30f0ce6c5926f68bbc85c573c452f2e660d563
vattern <thys@vektorfoto.co.za>
2020-07-27 14:06:47 +0100

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/stoutgat/stoutgat.h
+++ keyboards/stoutgat/stoutgat.h
@@ -27,7 +27,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT_65_iso( \
+#define LAYOUT( \
        K44,                                                        K48, \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, \
