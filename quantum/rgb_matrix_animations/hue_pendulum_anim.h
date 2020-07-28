79e88abb5681855dc806c6356ea0050c360c3c32
TurtleHunter <48608128+TurtleHunter@users.noreply.github.com>
2020-07-28 14:11:58 -0400

    Update quantum/rgb_matrix_animations/hue_pendulum_anim.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- quantum/rgb_matrix_animations/hue_pendulum_anim.h
+++ quantum/rgb_matrix_animations/hue_pendulum_anim.h
@@ -2,12 +2,12 @@
 RGB_MATRIX_EFFECT(HUE_PENDULUM)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-//Change huedelta to adjust range of hue change. 0-255.
-//Looks better with a low value and slow speed for subtle change.
-//Hue Pendulum - color changes in a wave to the right before reversing direction
+// Change huedelta to adjust range of hue change. 0-255.
+// Looks better with a low value and slow speed for subtle change.
+// Hue Pendulum - color changes in a wave to the right before reversing direction
 static HSV HUE_PENDULUM_math(HSV hsv, uint8_t i, uint8_t time) {
-	uint8_t huedelta = 12;
-    hsv.h = hsv.h + scale8(abs8(sin8(time) + (g_led_config.point[i].x) - 128) * 2, huedelta);
+    uint8_t huedelta = 12;
+    hsv.h            = hsv.h + scale8(abs8(sin8(time) + (g_led_config.point[i].x) - 128) * 2, huedelta);
     return hsv;
 }
 
