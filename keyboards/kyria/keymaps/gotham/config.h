34a1ba14c73c2756617fe5383be825325af1d3f6
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-07-27 17:42:42 -0700

    Move mousekey config to userspace

--- keyboards/kyria/keymaps/gotham/config.h
+++ keyboards/kyria/keymaps/gotham/config.h
@@ -23,17 +23,6 @@
 // Speed up slave half startup
 #define SPLIT_USB_TIMEOUT 1000
 
-#ifdef MOUSEKEY_ENABLE
-#    define MOUSEKEY_DELAY 	           200 // Delay between pressing a movement key and cursor movement
-#    define MOUSEKEY_INTERVAL          16  // Time between cursor movements
-#    define MOUSEKEY_MAX_SPEED         8   // Maximum cursor speed at which acceleration stops
-#    define MOUSEKEY_TIME_TO_MAX       10  // Time until maximum cursor speed is reached
-#    define MOUSEKEY_WHEEL_DELAY       200 // Delay between pressing a wheel key and wheel movement
-#    define MOUSEKEY_WHEEL_INTERVAL    150 // Time between wheel movements
-#    define MOUSEKEY_WHEEL_MAX_SPEED   8   // Maximum number of scroll steps per scroll action
-#    define MOUSEKEY_WHEEL_TIME_TO_MAX 40  // Time until maximum scroll speed is reached
-#endif
-
 #ifdef OLED_DRIVER_ENABLE
 #    define OLED_DISPLAY_128X64
 
--- users/gotham/config.h
+++ users/gotham/config.h
@@ -7,7 +7,18 @@
 
 #ifdef COMBO_ENABLE
 #    define COMBO_COUNT 11
-#    define COMBO_TERM 140
+#    define COMBO_TERM 100
+#endif
+
+#ifdef MOUSEKEY_ENABLE
+#    define MOUSEKEY_DELAY 	           200 // Delay between pressing a movement key and cursor movement
+#    define MOUSEKEY_INTERVAL          16  // Time between cursor movements
+#    define MOUSEKEY_MAX_SPEED         8   // Maximum cursor speed at which acceleration stops
+#    define MOUSEKEY_TIME_TO_MAX       10  // Time until maximum cursor speed is reached
+#    define MOUSEKEY_WHEEL_DELAY       200 // Delay between pressing a wheel key and wheel movement
+#    define MOUSEKEY_WHEEL_INTERVAL    150 // Time between wheel movements
+#    define MOUSEKEY_WHEEL_MAX_SPEED   8   // Maximum number of scroll steps per scroll action
+#    define MOUSEKEY_WHEEL_TIME_TO_MAX 40  // Time until maximum scroll speed is reached
 #endif
 
 #ifdef OLED_DRIVER_ENABLE
