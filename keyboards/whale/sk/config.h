91d82988ea0016f6d047909875393d0f8d60c638
Whale Mo <ncwhale@gmail.com>
2020-07-29 04:29:45 +0800

    Modify keymap layout.

--- keyboards/whale/sk/config.h
+++ keyboards/whale/sk/config.h
@@ -29,30 +29,3 @@ SOFTWARE.
 /* USB Device descriptor parameter */
 #define VENDOR_ID 0xFEED
 #define PRODUCT_ID 0x0495
-#define MANUFACTURER Whale Mo
-#define PRODUCT Splite Keyboard v3
-#define DESCRIPTION Hackable split keyboard/QMK
-/* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 14
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
-#define MATRIX_ROW_PINS \
-    { B1, B2, B3, B4, B5, B6 }
-#define MATRIX_COL_PINS \
-    { D4, C6, D7, E6, F4, F5, F6, F7 }
-
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 3
