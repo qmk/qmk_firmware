233bf4b5ac4e2ee9511c45670ddcaf20e19607db
DreamyState <36541604+DreamyState@users.noreply.github.com>
2020-07-30 23:45:25 +0800

    Update keymap.c

--- keyboards/dreamystate/dy16/keymaps/via/keymap.c
+++ keyboards/dreamystate/dy16/keymaps/via/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q, KC_R,  KC_S,  KC_T
   ),
   [1] = LAYOUT_ortho_5x4(
-    KC_TRNS, KC_TRNS  ,  RESET,  KC_TRNS,
+    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
