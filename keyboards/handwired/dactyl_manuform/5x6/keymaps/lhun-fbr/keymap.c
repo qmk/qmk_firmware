57030184e56c9dffb33d1b820e21c233ae016b4f
Lhun <dylan@foreverconnected.org>
2020-07-29 20:43:22 -0400

    7/30/2020 minor commit, removing invalid tightyl-4x6-altwiring removed other invalid builds, created new branch for brownelvis builds in progress.
    ## Description
    -adding personal and /r/mk community keymap examples,
            - Lhun keymap is for the fbr layout varient 5x6 by /u/brownelvis
    	- c# and にほんごfriendly, reintroduces KC_LBRC KC_RBRC, LALT, LGUI, DEL and PLUS
    　　 - inclusion of KC_MHEN,KC_HENK for microsoft ime
    	- _RAISE layer includes RALT for f-combo purposes
    	- swapped sides for raise and lower to be windows combo aware
    <!--- Describe your changes in detail here. -->
    
    ## Types of Changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all the boxes that apply. -->
    - [ ] Core
    - [ ] Bugfix
    - [ ] New feature
    - [ ] Enhancement/optimization
    - [x] Keyboard (addition or update)
    - [x] Keymap/layout/userspace (addition or update)
    - [ ] Documentation
    
    ## Issues Fixed or Closed by This PR
    
    *
    
    ## Checklist
    
    <!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have read the [**CONTRIBUTING** document](https://docs.qmk.fm/#/contributing).
    - [ ] I have added tests to cover my changes.
    - [x] I have tested the changes and verified that they work and don't break anything (as well as I can manage).

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/lhun-fbr/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/lhun-fbr/keymap.c
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_UP,KC_SLSH,
-     KC_LGUI, KC_LALT , KC_LBRC,KC_RBRC, RAISE, KC_SPC,                       KC_ENT, LOWER, KC_PLUS, KC_LEFT, KC_DOWN, KC_RIGHT,                                       
-                                      KC_GRAVE,KC_DEL,                        KC_BSPC,  KC_RSFT
+     KC_LGUI, KC_LALT,KC_LBRC,KC_RBRC, RAISE, KC_SPC,                        KC_BSPC, LOWER ,KC_PLUS, KC_LEFT, KC_DOWN, KC_RIGHT,                                       
+                                      KC_GRAVE,KC_DEL,                        KC_ENT,  KC_RSFT
   ),
 
   [_LOWER] = LAYOUT_5x6_fbr(
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P1 , KC_P2 , KC_P3 ,KC_MINS,_______,
      _______,_______,_______,_______,_______,_______,                        _______, KC_P0 , KC_DOT, KC_PLUS,_______ ,_______,
      _______,_______,_______, _______,_______,_______,            SYS,_______, _______,_______,_______,_______,
-                                             KC_MHEN,KC_HENK,            _______,_______
+                                             KC_MHEN,KC_INT2,            _______,_______
 
   ),
 
