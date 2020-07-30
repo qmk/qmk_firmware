0bd7f4d7a4b4f7f2f2f500a907ecef2bf41154d7
Brandon Schlack <brandonschlack@gmail.com>
2020-07-30 14:55:59 -0700

    [Keymap] Update launchpad RGB config

--- keyboards/launchpad/keymaps/brandonschlack/config.h
+++ keyboards/launchpad/keymaps/brandonschlack/config.h
@@ -1,9 +1,13 @@
 #pragma once
 
 /* Underlight Configuration */
-#define RGB_DI_PIN F4
+#ifdef RGB_DI_PIN
+    #undef RGB_DI_PIN
+    #define RGB_DI_PIN F4
+    #undef RGBLED_NUM
+    #define RGBLED_NUM 2  // Number of LEDs
+#endif
 #define RGBLIGHT_EFFECT_BREATHING // Only have Breathing Animation
-#define RGBLED_NUM 2  // Number of LEDs
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
