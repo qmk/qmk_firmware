c20e5820fc459f9540730971e8985559c74a2f06
fauxpark <fauxpark@gmail.com>
2020-07-30 10:27:14 +1000

    Partial (literally) RGB Matrix support

--- keyboards/k_type/rules.mk
+++ keyboards/k_type/rules.mk
@@ -26,6 +26,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = IS31FL3733
 CUSTOM_MATRIX = lite
 
 LAYOUTS = tkl_ansi
