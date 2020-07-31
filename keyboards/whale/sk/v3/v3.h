b75a503f805cf814a1eb00eb218ed18d53fea797
Whale Mo <ncwhale@gmail.com>
2020-08-01 07:24:32 +0800

    Update keyboards/whale/sk/v3/v3.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/whale/sk/v3/v3.h
+++ keyboards/whale/sk/v3/v3.h
@@ -28,7 +28,7 @@
     {R50, R51, R52, R53, R54, R55, R56 } \
 }
 
-#define LEFT_HALF_ONLY(\
+#define LAYOUT_left_half_only(\
     L00, L01, L02, L03, L04, L05, L06, L46, \
        L10, L11, L12, L13, L14, L15, L16,   \
       L20, L21, L22, L23, L24, L25, L26,    \
