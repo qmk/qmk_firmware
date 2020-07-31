c68390af9b7ef26452894db0be96c3fa920a6d59
Danilo de Klerk <danilodeklerk@gmail.com>
2020-07-31 16:42:53 +0200

    Rename main layouts for split_3x6_3 keyboards

--- keyboards/centromere/centromere.h
+++ keyboards/centromere/centromere.h
@@ -34,7 +34,7 @@
 // This a shortcut to help you visually see your layout.
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
-#define LAYOUT( \
+#define LAYOUT_split_3x6_3( \
   k0a, k00, k01, k02, k03, k04,      k05, k06, k07, k08, k09, k0b, \
   k1a, k10, k11, k12, k13, k14,      k15, k16, k17, k18, k19, k1b,\
   k2a, k20, k21, k22, k23, k24,      k25, k26, k27, k28, k29, k2b,\
@@ -62,4 +62,4 @@
     { ___, ___, ___, ___, ___, ___, ___, ___, ___, ___ }  \
   }
 
-#define LAYOUT_split_3x6_3 LAYOUT
+#define LAYOUT LAYOUT_split_3x6_3
--- keyboards/crkbd/rev1/rev1.h
+++ keyboards/crkbd/rev1/rev1.h
@@ -22,7 +22,7 @@
 #endif
 
 // clang-format off
-#define LAYOUT( \
+#define LAYOUT_split_3x6_3( \
   L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05, \
   L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15, \
   L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25, \
@@ -70,4 +70,4 @@
   )
 // clang-format on
 
-#define LAYOUT_split_3x6_3 LAYOUT
+#define LAYOUT LAYOUT_split_3x6_3
