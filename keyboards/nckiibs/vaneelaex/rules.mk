d675450a75cb9019e4fa7b61d145348fb089b115
Woongki Sung <noclew@users.noreply.github.com>
2020-07-31 16:46:34 -0400

    Update keyboards/nckiibs/vaneelaex/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/nckiibs/vaneelaex/rules.mk
+++ keyboards/nckiibs/vaneelaex/rules.mk
@@ -19,5 +19,3 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-# FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-# HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
