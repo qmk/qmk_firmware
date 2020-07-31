4da955e7bba99cf28a1855d27bfc8f0f247b78c9
Whale Mo <ncwhale@gmail.com>
2020-08-01 07:23:39 +0800

    Update keyboards/whale/sk/v3/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/whale/sk/v3/config.h
+++ keyboards/whale/sk/v3/config.h
@@ -39,10 +39,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS \
-    { B1, B2, B3, B4, B5, B6 }
-#define MATRIX_COL_PINS \
-    { D4, C6, D7, E6, F4, F5, F6 }
+#define MATRIX_ROW_PINS { B1, B2, B3, B4, B5, B6 }
+#define MATRIX_COL_PINS { D4, C6, D7, E6, F4, F5, F6 }
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
