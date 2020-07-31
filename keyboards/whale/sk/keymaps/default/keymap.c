36d6749ebaf680aebbca00782149be318ec05e71
Whale Mo <ncwhale@gmail.com>
2020-08-01 07:24:01 +0800

    Update keyboards/whale/sk/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/whale/sk/keymaps/default/keymap.c
+++ keyboards/whale/sk/keymaps/default/keymap.c
@@ -18,9 +18,6 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN,
-    _MULTIMEDIA,
-    _EMOJI
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
