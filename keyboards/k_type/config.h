0e7f91adeb0cd75f21513dccf199c8ce44749fae
fauxpark <fauxpark@gmail.com>
2020-08-01 04:38:11 +1000

    Put all RGB config in keymap for now

--- keyboards/k_type/config.h
+++ keyboards/k_type/config.h
@@ -85,23 +85,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
-// i2c_master defines
-#define I2C1_SCL 0 // A2 on pinout = B0
-#define I2C1_SDA 1 // A2 on pinout = B1
-#define I2C1_SCL_PAL_MODE PAL_MODE_ALTERNATIVE_2
-#define I2C1_SDA_PAL_MODE PAL_MODE_ALTERNATIVE_2
-//#define I2C2_SCL 10 // A2 on pinout = C10
-//#define I2C2_SDA 11 // A2 on pinout = C11
-//#define I2C2_SCL_PAL_MODE PAL_MODE_ALTERNATIVE_2
-//#define I2C2_SDA_PAL_MODE PAL_MODE_ALTERNATIVE_2
-
-#define DRIVER_ADDR_1 0b1010000
-#define DRIVER_ADDR_2 0b1010001
-#define DRIVER_COUNT 2
-#define DRIVER_1_LED_TOTAL 64
-//#define DRIVER_2_LED_TOTAL 55
-#define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
-
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
