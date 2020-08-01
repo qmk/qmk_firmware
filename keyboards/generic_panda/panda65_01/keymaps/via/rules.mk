43a43b8b406ecb7fef1676ba6841f74b6e759a3c
Jesse <jessetleung@aol.com>
2020-08-01 01:25:01 -0400

    working via compatibility

--- keyboards/generic_panda/panda65_01/keymaps/via/rules.mk
+++ keyboards/generic_panda/panda65_01/keymaps/via/rules.mk
@@ -1,3 +1,4 @@
-#enable via
-VIA_ENABLE = yes
-LTO_ENABLE = yes #reduce firmware size
+VIA_ENABLE = yes # enable via
+LTO_ENABLE = yes # reduce firmware size
+EXTRAKEY_ENABLE = no # diable extra key for via
+MIDI_ENABLE = no #diable midi for via
