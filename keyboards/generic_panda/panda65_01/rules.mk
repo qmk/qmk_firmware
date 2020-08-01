438e018d5ff0ee49b3587c77933d24717a07527d
Jesse <jessetleung@aol.com>
2020-08-01 01:25:01 -0400

    working via compatibility

--- keyboards/generic_panda/panda65_01/rules.mk
+++ keyboards/generic_panda/panda65_01/rules.mk
@@ -18,3 +18,5 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+
+LAYOUTS = split_bs
