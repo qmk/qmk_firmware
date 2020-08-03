0ff36f050764bcc3419d92c5192fd9015d0892f0
David Camp <Bpendragon@users.noreply.github.com>
2020-08-02 17:20:52 -0700

    now at least parially working

--- keyboards/handwired/selene/rules.mk
+++ keyboards/handwired/selene/rules.mk
@@ -1,19 +1,10 @@
 # MCU name
 MCU = STM32F303
 
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
-
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes        # Console for debug(+400)
@@ -23,7 +14,6 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
@@ -32,3 +22,6 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
 WS2812_DRIVER = bitbang
 RGBLIGHT_ENABLE = yes
+LEADER_ENABLE = no
+KEY_LOCK_ENABLE = no
+TERMINAL_ENABLE = no
--- keyboards/handwired/selene/selene.c
+++ keyboards/handwired/selene/selene.c
@@ -1,1 +1,11 @@
 #include "selene.h"
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        writePin(A2, led_state.num_lock);
+        writePin(A1, led_state.caps_lock);
+        writePin(A0, led_state.scroll_lock);
+    }
+    return res;
+}
