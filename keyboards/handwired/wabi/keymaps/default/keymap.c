4978f66a427a1bba0f266dd7fbc33ef1d419c586
Rossman360 <rmontsinger@gmail.com>
2020-08-04 18:07:14 -0400

    remove blank line

--- keyboards/handwired/wabi/keymaps/default/keymap.c
+++ keyboards/handwired/wabi/keymaps/default/keymap.c
@@ -2,7 +2,6 @@
 
 #define _QWERTY 0
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
