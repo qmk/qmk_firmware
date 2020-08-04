5546b2c6ef11a1aa9e515b316c237184cb8cc306
Rossman360 <rmontsinger@gmail.com>
2020-08-04 18:05:14 -0400

    no descrip

--- keyboards/handwired/wabi/config.h
+++ keyboards/handwired/wabi/config.h
@@ -23,7 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0002
 #define MANUFACTURER LifeIsOnTheWire
 #define PRODUCT PostageBoard
-#define DESCRIPTION "A Handwire Controller Board Round 2"
 
 /* key matrix size */
 
