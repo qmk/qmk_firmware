d19d740900edf9ea1ce72b68be088027f92e9f03
Woongki Sung <noclew@users.noreply.github.com>
2020-08-04 01:56:46 -0400

    Update keyboards/nckiibs/flatbread60/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nckiibs/flatbread60/keymaps/default/keymap.c
+++ keyboards/nckiibs/flatbread60/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower | Space|Space |Raise | Left | Down |  Up  |Right |
+ * |      | Ctrl | Alt  | GUI  |Lower | Space|Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  * */
 [_QWERTY] = LAYOUT_ortho_5x12(
