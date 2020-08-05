197a8b12a05a06913a326ec55b36ed60f3204e54
David Camp <Bpendragon@users.noreply.github.com>
2020-08-05 13:06:24 -0700

    Fix status Lights being wired incorrectly

--- keyboards/handwired/selene/rules.mk
+++ keyboards/handwired/selene/rules.mk
@@ -13,7 +13,6 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
@@ -22,4 +21,3 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
 WS2812_DRIVER = bitbang
 RGBLIGHT_ENABLE = yes
-
--- keyboards/handwired/selene/selene.c
+++ keyboards/handwired/selene/selene.c
@@ -1,9 +1,9 @@
 #include "selene.h"
 
 void matrix_init_kb(void){
-    setPinOutput(A0);
-    setPinOutput(A1);
     setPinOutput(A2);
+    setPinOutput(A1);
+    setPinOutput(A0);
 }
 
 void keyboard_post_init_user(void) {
@@ -13,9 +13,9 @@ void keyboard_post_init_user(void) {
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if(res) {
-        writePin(A2, led_state.num_lock);
-        writePin(A1, led_state.caps_lock);
-        writePin(A0, led_state.scroll_lock);
+        writePin(A0, led_state.num_lock);
+        writePin(A2, led_state.caps_lock);
+        writePin(A1, led_state.scroll_lock);
     }
     return res;
 }
