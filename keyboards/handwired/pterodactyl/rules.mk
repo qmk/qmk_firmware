52561b21910de4ff5c2c0766b294f9703f0e52be
myoung34 <myoung34@my.apsu.edu>
2020-08-05 06:49:08 -0500

    Update keyboards/handwired/pterodactyl/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/pterodactyl/rules.mk
+++ keyboards/handwired/pterodactyl/rules.mk
@@ -1,15 +1,10 @@
 # MCU name
 MCU = atmega32u4
+
+# Processor frequency
 F_CPU = 8000000
 
 # Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = caterina
 
 # Build Options
