15be85b37838073614e9e4d2e2e2ced1398bc09e
Philip (Han) Suh <36554396+phlopsuh@users.noreply.github.com>
2020-08-04 18:45:15 -0700

    Update keyboards/ztboards/noon/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ztboards/noon/config.h
+++ keyboards/ztboards/noon/config.h
@@ -23,7 +23,7 @@
 #define PRODUCT_ID   0x0002
 #define DEVICE_VER   0x0001
 #define MANUFACTURER ZTBoards
-#define PRODUCT      ZTBoards Noon
+#define PRODUCT      Noon
 
 /* key matrix size */
 #define MATRIX_ROWS 5
