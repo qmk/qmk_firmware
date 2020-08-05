deac358140ec1b64756a3829de691a697f6fc5b6
Philip (Han) Suh <36554396+phlopsuh@users.noreply.github.com>
2020-08-04 18:39:06 -0700

    Update keyboards/ztboards/after/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/ztboards/after/config.h
+++ keyboards/ztboards/after/config.h
@@ -24,7 +24,6 @@
 #define DEVICE_VER   0x0001
 #define MANUFACTURER ZTBoards
 #define PRODUCT      ZTBoards After
-#define DESCRIPTION  ZTBoards After
 
 /* key matrix size */
 #define MATRIX_ROWS 5
