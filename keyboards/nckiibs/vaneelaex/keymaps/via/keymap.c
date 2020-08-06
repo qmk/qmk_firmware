293e0cb99573cb47b82cf69422bd04a7fbe910c3
Woongki Sung <noclew@users.noreply.github.com>
2020-08-06 01:37:42 -0400

    Apply suggestions from code review
    Done!
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nckiibs/vaneelaex/keymaps/via/keymap.c
+++ keyboards/nckiibs/vaneelaex/keymaps/via/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  /   |  *   |  .   |   0  | Brite| Ctrl | Alt  | GUI  |Lower | Space|Space |Raise | Left | Down |  Up  |Right |
  * '---------------------------------------------------------------------------------------------------------------'
  * */
-[_BASE] = LAYOUT_ss_6x12( \
+[_BASE] = LAYOUT_ss_6x12(
 KC_KP_EQUAL,    KC_7,           KC_8,           KC_9,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 KC_KP_MINUS,    KC_4,           KC_5,           KC_6,    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
 KC_KP_PLUS,     KC_1,           KC_2,           KC_3,    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
@@ -58,7 +58,7 @@ KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_DOT,      KC_0,    KC_LCTL, KC_LCTL, KC_LA
  * |  /   |  *   |  .   |   0  |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `---------------------------------------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_ss_6x12( \
+[_LOWER] = LAYOUT_ss_6x12(
 KC_KP_EQUAL,    KC_7,           KC_8,           KC_TILDE, KC_9,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
 KC_KP_MINUS,    KC_4,           KC_5,           KC_DEL,   KC_6,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
 KC_KP_PLUS,     KC_1,           KC_2,           _______,  KC_3,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
@@ -76,7 +76,7 @@ KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_DOT,      _______,  KC_0,    _______, ____
  * |  /   |  *   |  .   |   0  |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `---------------------------------------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_ss_6x12( \
+[_RAISE] = LAYOUT_ss_6x12(
   KC_KP_EQUAL,    KC_7,           KC_8,           KC_9,    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_KP_MINUS,    KC_4,           KC_5,           KC_6,    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   KC_KP_PLUS,     KC_1,           KC_2,           KC_3,    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
@@ -94,7 +94,7 @@ KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_DOT,      _______,  KC_0,    _______, ____
  * |  /   |  *   |  .   |   0  |      |      |      |      |      |             |      |      |      |      |      |
  * `---------------------------------------------------------------------------------------------------------------'
  */
-[_FN] = LAYOUT_ss_6x12( \
+[_FN] = LAYOUT_ss_6x12(
 KC_KP_EQUAL,    KC_7,           KC_8,           KC_9,   RESET  , RGB_TOG, RGB_MOD, RGB_HUI, RGB_VAI, RGB_VAD, RGB_SAI, RGB_SAD, _______,TERM_ON, TERM_OFF, KC_DEL,
 KC_KP_MINUS,    KC_4,           KC_5,           KC_6,   _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
 KC_KP_PLUS,     KC_1,           KC_2,           KC_3,   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
