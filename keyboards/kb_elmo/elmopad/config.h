8293a8bad433fc6464f8a09a230576b8ca7b016e
kb-elmo <lorwel@mailbox.org>
2020-08-06 23:30:35 +0200

    Apply suggestions from code review
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/kb_elmo/elmopad/config.h
+++ keyboards/kb_elmo/elmopad/config.h
@@ -42,7 +42,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* Bootmagic Lite key configuration */
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
