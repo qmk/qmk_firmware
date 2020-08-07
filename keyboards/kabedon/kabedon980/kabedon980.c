c8ffbcd53b797695fd13e65ba181172740f9b1dd
KDon <370490639@qq.com>
2020-08-07 16:44:46 +0800

    Update kabedon980.c

--- keyboards/kabedon/kabedon980/kabedon980.c
+++ keyboards/kabedon/kabedon980/kabedon980.c
@@ -4,18 +4,12 @@ void keyboard_pre_init_user(void) {
   /* Set CAPSLOCK indicator pin as output */
   setPinOutput(E6);
 }
-void led_set_keymap(uint8_t usb_led) {}
-
-void led_set_kb(uint8_t usb_led) {
-    // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-  if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-    writePinLow(E6);
-  }
-  else {
-    writePinHigh(E6);
-  }
-   led_set_keymap(usb_led);
-    //led_set_user(usb_led);
+  // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+void led_update_kb(led_t led_state) {
+    if (led_update_user(led_state)) {
+        writePin(E6, !led_state.caps_lock);
+    }
+    return true;
 }
 
 
