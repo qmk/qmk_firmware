9ddcdb297e6551fe552d9ea705112c802192fc5a
myoung34 <myoung34@my.apsu.edu>
2020-08-07 05:16:27 -0500

    Update keyboards/handwired/pterodactyl/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/pterodactyl/keymaps/default/keymap.c
+++ keyboards/handwired/pterodactyl/keymaps/default/keymap.c
@@ -33,24 +33,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_BL] = LAYOUT_pterodactyl(  // layer 0 : default
         // left hand
-          KC_EQL,          KC_1,           KC_2,     KC_3,     KC_4,  KC_5,
-          KC_DEL,          KC_Q,           KC_W,     KC_E,     KC_R,  KC_T,
-          KC_BSPC,         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,
-          KC_LSFT,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,
-  LT(_SYMB,KC_GRV),      KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT,  KC_SPC,
-                                                                      ALT_T(KC_APP),  KC_LGUI,
-                                                                                      KC_HOME,
-                                                                           KC_BSPC,   KC_END,
-        // right hand
-                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
-                             KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
-                             KC_H,   KC_J,     KC_K,     KC_L,  LT(_MDIA, KC_SCLN),  GUI_T(KC_QUOT),
-                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSFT,
-                             KC_ENT, KC_UP, KC_DOWN,  KC_LBRC,            KC_RBRC,       TT(_SYMB),
-          KC_RALT,  CTL_T(KC_ESC),
-          KC_PGUP,
-          KC_PGDN, KC_TAB
-    ),
+          KC_EQL,          KC_1,           KC_2,     KC_3,     KC_4,  KC_5,                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
+          KC_DEL,          KC_Q,           KC_W,     KC_E,     KC_R,  KC_T,                             KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
+          KC_BSPC,         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,                             KC_H,   KC_J,     KC_K,     KC_L,  LT(_MDIA, KC_SCLN),  GUI_T(KC_QUOT),
+          KC_LSFT,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSFT,
+  LT(_SYMB,KC_GRV),      KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT,  KC_SPC,                          KC_ENT, KC_UP, KC_DOWN,  KC_LBRC,            KC_RBRC,       TT(_SYMB),
+
+                                                                 ALT_T(KC_APP),  KC_LGUI,      KC_RALT,  CTL_T(KC_ESC),
+                                                                                 KC_HOME,      KC_PGUP,
+                                                                       KC_BSPC,   KC_END,      KC_PGDN, KC_TAB
+),
 /* Keymap 1: Symbol Layer
  *
  * ,-----------------------------------------.                    ,-----------------------------------------.
