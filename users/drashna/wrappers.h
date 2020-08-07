d3b9f1e5d09732efe34db07994a625b63c5bad12
Drashna Jael're <drashna@live.com>
2020-08-06 18:35:34 -0700

    Update config for corne to support community layouts

--- users/drashna/wrappers.h
+++ users/drashna/wrappers.h
@@ -17,6 +17,7 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
 #define LAYOUT_ortho_4x12_wrapper(...)       LAYOUT_ortho_4x12(__VA_ARGS__)
 #define LAYOUT_ortho_5x12_wrapper(...)       LAYOUT_ortho_5x12(__VA_ARGS__)
 #define LAYOUT_gergo_wrapper(...)            LAYOUT_gergo(__VA_ARGS__)
+#define LAYOUT_split_3x6_3_wrapper(...)      LAYOUT_split_3x6_3(__VA_ARGS__)
 
 /*
 Blocks for each of the four major keyboard layouts
