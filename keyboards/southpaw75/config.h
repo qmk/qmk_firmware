cbde4b5b3f39a51b2aae184826d22361a54836e7
kelvinhall05 <56369403+kelvinhall05@users.noreply.github.com>
2020-08-07 10:43:14 -0400

    Update keyboards/southpaw75/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/southpaw75/config.h
+++ keyboards/southpaw75/config.h
@@ -4,7 +4,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define PRODUCT_ID      0x350C
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    kelvinhall05
 #define PRODUCT         Southpaw75
