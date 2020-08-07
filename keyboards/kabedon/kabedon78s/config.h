fbbe28a2488c3655f5f7cdda5fe7e1ff142ad896
KDon <370490639@qq.com>
2020-08-07 16:49:22 +0800

    Update config.h

--- keyboards/kabedon/kabedon78s/config.h
+++ keyboards/kabedon/kabedon78s/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Kabe_Don
 #define PRODUCT         78S
-#define DESCRIPTION     KabeDon78S
 
 /* key matrix size */
 #define MATRIX_ROWS 6
