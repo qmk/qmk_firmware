c5f12051d5d4313478928b4171b6487425c9c9da
myoung34 <myoung34@my.apsu.edu>
2020-08-07 05:15:47 -0500

    Update keyboards/handwired/pterodactyl/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/pterodactyl/keymaps/default/keymap.c
+++ keyboards/handwired/pterodactyl/keymaps/default/keymap.c
@@ -75,23 +75,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [_SYMB] = LAYOUT_pterodactyl(
        // left hand
-       KC_TRNS,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
-       KC_TRNS,  KC_EXLM,    KC_AT,  KC_LCBR,  KC_RCBR,  KC_PIPE,
-       KC_TRNS,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,   KC_GRV,
-       KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,
-         RESET,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-                                                         KC_TRNS,  KC_TRNS,
-                                                                   KC_TRNS,
-                                               KC_TRNS,  KC_TRNS,  KC_TRNS,
-       // right hand
-                KC_F6,      KC_F7,   KC_F8, KC_F9,  KC_F10,   KC_F11,
-                KC_UP,       KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
-                KC_DOWN,     KC_4,    KC_5,  KC_6, KC_PLUS,  KC_TRNS,
-                KC_AMPR,     KC_1,    KC_2,  KC_3, KC_BSLS,  KC_TRNS,
-                          KC_TRNS,  KC_DOT,  KC_0,  KC_EQL,  KC_TRNS,
-      KC_TRNS,  KC_TRNS,
-      KC_TRNS,
-      KC_TRNS,  KC_TRNS,  KC_TRNS
+       KC_TRNS,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                KC_F6,      KC_F7,   KC_F8, KC_F9,  KC_F10,   KC_F11,
+       KC_TRNS,  KC_EXLM,    KC_AT,  KC_LCBR,  KC_RCBR,  KC_PIPE,                KC_UP,       KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
+       KC_TRNS,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,   KC_GRV,                KC_DOWN,     KC_4,    KC_5,  KC_6, KC_PLUS,  KC_TRNS,
+       KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,                KC_AMPR,     KC_1,    KC_2,  KC_3, KC_BSLS,  KC_TRNS,
+         RESET,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                                    KC_TRNS,  KC_DOT,  KC_0,  KC_EQL,  KC_TRNS,
+
+                                                     KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,
+                                                               KC_TRNS,      KC_TRNS,
+                                           KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
  * ,-----------------------------------------.                    ,-----------------------------------------.
