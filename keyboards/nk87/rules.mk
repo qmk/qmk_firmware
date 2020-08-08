3513e69a3850f25bab802107b03deeda8453265b
yiancar <yiangosyiangou@cytanet.com.cy>
2020-08-08 09:19:34 +0100

    Update keyboards/nk87/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nk87/rules.mk
+++ keyboards/nk87/rules.mk
@@ -7,19 +7,21 @@ MCU = STM32F303
 OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-BACKLIGHT_ENABLE = no              # Enable keyboard backlight functionality
-BOOTMAGIC_ENABLE = lite            # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes              # Mouse keys
-EXTRAKEY_ENABLE = yes              # Audio control and System control
-CONSOLE_ENABLE = no                # Console for debug
-COMMAND_ENABLE = no                # Commands for debug and configuration
-SLEEP_LED_ENABLE = no              # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes                  # USB Nkey Rollover
-AUDIO_ENABLE = no                  # Audio output on port C6
-NO_USB_STARTUP_CHECK = no          # Disable initialization only when usb is plugged in
-#SERIAL_LINK_ENABLE = yes
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 
 CIE1931_CURVE = yes
 
