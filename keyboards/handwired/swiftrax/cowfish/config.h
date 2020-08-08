62f3179f08eece43f61729774b505a15c562743d
Swiftrax <swiftrax@gmail.com>
2020-08-08 00:30:55 -0700

    revert VID to microchip's

--- keyboards/handwired/swiftrax/cowfish/config.h
+++ keyboards/handwired/swiftrax/cowfish/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x7161
+#define VENDOR_ID       0x04D8
 #define PRODUCT_ID      0xEB53
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Swiftrax
