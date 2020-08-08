2b0a9c340d2bc0196630268ec69f3a1aadc86935
James D <boboysdadda@gmail.com>
2020-08-07 21:02:00 -0600

    nonworking initial add for preonic

---
+++ keyboards/preonic/keymaps/boboysdadda/rules.mk
@@ -0,0 +1,25 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+MIDI_ENABLE = no            # MIDI controls
+AUDIO_ENABLE = yes           # Audio output on port C6
+UNICODE_ENABLE = yes        # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight. 
+OLED_DRIVER_ENABLE= no     # OLED display
+ENCODER_ENABLE = no        # Encoder enable
+TAP_DANCE_ENABLE = yes      # Tap Dance
+LEADER_ENABLE = yes         # Leader support
+LTO_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes  # Dynamic Macros
+WPM_ENABLE = no           # Enable WPM calc and transport
+
+SRC += muse.c
