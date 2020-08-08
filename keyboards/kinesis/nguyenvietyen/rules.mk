510e6c917c9e578255972ac1cadd7a261c5e01ff
Viet Yen Nguyen <nguyenvietyen@gmail.com>
2020-08-08 11:50:15 +0200

    chore: update doc
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kinesis/nguyenvietyen/rules.mk
+++ keyboards/kinesis/nguyenvietyen/rules.mk
@@ -6,6 +6,14 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = caterina
 
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no
+COMMAND_ENABLE = yes
+NKRO_ENABLE = yes
+CUSTOM_MATRIX = lite
+
 # Project specific files
 SRC += matrix.c
 
