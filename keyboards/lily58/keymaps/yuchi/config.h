85b50717dcfb90fbf0ea9befcef3b6e12ddd010d
kata0510 <kata0510.com@gmail.com>
2020-08-09 14:53:53 +0900

    Update keymaps and venderID

--- keyboards/lily58/keymaps/yuchi/config.h
+++ keyboards/lily58/keymaps/yuchi/config.h
@@ -34,19 +34,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
-
-// Underglow
-/*
-#undef RGBLED_NUM
-#define RGBLED_NUM 14    // Number of LEDs
-#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_SLEEP
-*/
