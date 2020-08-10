668d8d7676d3478e76080ae3abe1a19f008fcaa0
Logan Marchione <logan@loganmarchione.com>
2020-08-10 11:57:36 -0400

    Make requested changes

--- keyboards/leafcutterlabs/bigknobv2/config.h
+++ keyboards/leafcutterlabs/bigknobv2/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    leafcutterlabs
 #define PRODUCT         bigknobv2
-#define DESCRIPTION     4 key with rotary  board
 
 /* key matrix size */
 #define MATRIX_ROWS 1
--- keyboards/leafcutterlabs/bigknobv2/rules.mk
+++ keyboards/leafcutterlabs/bigknobv2/rules.mk
@@ -2,13 +2,6 @@
 MCU = atmega32u4
 
 # Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = caterina
 
 # Build Options
@@ -25,11 +18,7 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 ENCODER_ENABLE = yes        # Enable rotary encoders
-TAP_DANCE_ENABLE = yes      # Double press does something different
 UNICODE_ENABLE = yes        # Unicode
