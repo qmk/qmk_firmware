88659ced9fc7ce36028d699b5e9d81b91ca6bef5
Max Drasbeck <max@eir.local>
2020-08-12 10:44:40 +0200

    Added license

--- keyboards/lily58/keymaps/danskish/keymap.c
+++ keyboards/lily58/keymaps/danskish/keymap.c
@@ -1,3 +1,14 @@
+/* Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) 2020 Max Drasbeck 
+ *
+ * You are free to:
+ *
+ *  Share — copy and redistribute the material in any medium or format
+ *  Adapt — remix, transform, and build upon the material
+ *   for any purpose, even commercially.
+ *
+ * The licensor cannot revoke these freedoms as long as you follow the license terms.
+ */
+
 #include QMK_KEYBOARD_H
 
 #ifdef PROTOCOL_LUFA
