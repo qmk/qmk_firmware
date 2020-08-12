269df0535efb4548828463eecc3a022c99d98949
Sendy YK <69058604+mrsendyyk@users.noreply.github.com>
2020-08-13 04:20:50 +0700

    Update config.h

--- keyboards/dz60/keymaps/LEdiodes/config.h
+++ keyboards/dz60/keymaps/LEdiodes/config.h
@@ -4,12 +4,11 @@
 #include "config.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x445A  // "DZ"
 #define PRODUCT_ID      0x2260
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    KBDFans
 #define PRODUCT         DZ60
-#define DESCRIPTION     DZ60 Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
