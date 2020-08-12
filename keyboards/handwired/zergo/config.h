c8dc80df88a5b0d05f4c0bce0e2cfdc41e39bf8a
Evgenii <59191442+greenjack-z@users.noreply.github.com>
2020-08-12 17:38:00 +0300

    Update config.h

--- keyboards/handwired/zergo/config.h
+++ keyboards/handwired/zergo/config.h
@@ -15,8 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #include "config_common.h"
 
@@ -53,5 +52,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
-
-#endif
