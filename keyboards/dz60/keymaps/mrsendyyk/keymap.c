ff22c8866c98763815feae164b0a66951ddf960e
Sendy YK <69058604+mrsendyyk@users.noreply.github.com>
2020-08-13 05:01:37 +0700

    Update keyboards/dz60/keymaps/mrsendyyk/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dz60/keymaps/mrsendyyk/keymap.c
+++ keyboards/dz60/keymaps/mrsendyyk/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `--------------------------------------------------------------------------'
    */
 
-	[1] = LAYOUT_60_ANSI_Arrow_mrsendyyk(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+	[1] = LAYOUT_60_ansi_arrows(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
                                        KC_BRIU, _______, _______, KC_END, RESET, _______, _______, _______, KC_INS, _______, KC_PSCR, _______, _______, KC_EJCT,
                                        KC_BRID, _______, KC_SLCK, _______, _______, _______, KC_HOME, _______, _______, _______, _______, _______, KC_MUTE,
                                        KC_VOLU, _______, _______, KC_CALC, _______, _______, KC_NLCK, KC_MAIL, _______, _______, KC_PAUS, KC_PGUP, _______,
