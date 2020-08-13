fdab5cf73b37098478f1c1868b8977ce3a388c1f
Evgenii <59191442+greenjack-z@users.noreply.github.com>
2020-08-13 12:05:48 +0300

    Update config.h

--- keyboards/handwired/zergo/config.h
+++ keyboards/handwired/zergo/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    GreenJack
 #define PRODUCT         zErgo
-#define DESCRIPTION     QMK keyboard firmware for zErgo
 
 /* key matrix size */
 #define MATRIX_ROWS 6
