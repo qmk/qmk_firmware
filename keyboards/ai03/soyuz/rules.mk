cce5955242c59848725380fe989efbc31be83030
Sendy YK <69058604+mrsendyyk@users.noreply.github.com>
2020-08-14 05:02:02 +0700

    Update rules.mk

--- keyboards/ai03/soyuz/rules.mk
+++ keyboards/ai03/soyuz/rules.mk
@@ -31,4 +31,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
-LAYOUTS = ortho_5x4
+LAYOUTS = ortho_5x4 numpad_5x4
