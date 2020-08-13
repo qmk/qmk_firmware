9f2f0625dce748d9dea511a8baa4f91a6e9df4ba
Sendy YK <69058604+mrsendyyk@users.noreply.github.com>
2020-08-14 01:10:39 +0700

    Add Layout & Keymap
    1. Update `soyuz.h` (Add  `LAYOUT_5X4`)
    2. Add `keymap.c`
    3. Add `readme.md`

---
+++ keyboards/ai03/soyuz/keymaps/mrsendyyk/keymap.c
@@ -0,0 +1,60 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /* Layer [0]
+   * ,-------------------.
+   * |Nm L|/   |*   |-   |
+   * |-------------------+
+   * |7   |8   |9   |    |
+   * |--------------|+   |
+   * |4   |5   |6   |    |
+   * |-------------------+
+   * |1   | 2  |3   |    |
+   * |--------------|Entr|
+   * |0        |.   |    |
+   * `-------------------'
+   */
+
+  [0] = LAYOUT_5x4(LT(1, KC_NLCK), KC_PSLS, KC_PAST, KC_PMNS,
+	                 KC_P7, KC_P8, KC_P9,
+									 KC_P4, KC_P5, KC_P6, KC_PPLS,
+									 KC_P1, KC_P2, KC_P3,
+									 KC_P0, KC_PDOT, KC_PENT),
+
+/* Layer [1] (Press & Hold `Num Lock` Key)
+   * ,-------------------.
+   * |    |    |    |    |
+   * |-------------------+
+   * |    |    |    |    |
+   * |--------------|    |
+   * |Rst |    |    |    |
+   * |-------------------+
+   * |    |    |    |    |
+   * |--------------|    |
+   * |         |    |    |
+   * `-------------------'
+   */
+
+  [1] = LAYOUT_5x4(KC_TRNS, _______, _______, _______,
+	                 _______, _______, _______,
+									 RESET, _______, _______, _______,
+									 _______, _______, _______,
+									 _______, _______, _______)
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+};
+
+void matrix_init_user(void) {
+
+};
+
+void matrix_scan_user(void) {
+
+};
+
+void led_set_user(uint8_t usb_led) {
+
+};
---
+++ keyboards/ai03/soyuz/keymaps/mrsendyyk/readme.md
@@ -0,0 +1,41 @@
+# [Sendy YK](https://mr.sendyyk.com)'s Keymap
+
+### Layer [0]
+
+```
+,-------------------.
+|Nm L|/   |*   |-   |
+|-------------------+
+|7   |8   |9   |    |
+|--------------|+   |
+|4   |5   |6   |    |
+|-------------------+
+|1   | 2  |3   |    |
+|--------------|Entr|
+|0        |.   |    |
+`-------------------'
+```
+
+### Layer [1]
+
+Press & Hold `Num Lock` Key
+
+```
+,-------------------.
+|    |    |    |    |
+|-------------------+
+|    |    |    |    |
+|--------------|    |
+|Rst |    |    |    |
+|-------------------+
+|    |    |    |    |
+|--------------|    |
+|         |    |    |
+`-------------------'
+```
+
+## Build The Firmware
+
+You will need to build the firmware. To do so go to your terminal window and run the compile command:
+
+    qmk compile -kb ai03/soyuz -km mrsendyyk
