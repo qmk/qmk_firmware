d2681f0da5d9b7558eb96430cb388fe4e423b907
solid44 <kvasina@outlook.com>
2020-08-13 12:33:36 +0200

    Update keyboards/bm60rgb_iso/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/bm60rgb_iso/config.h
+++ keyboards/bm60rgb_iso/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    KP Republic
 #define PRODUCT         bm60hsrgb-iso
-#define DESCRIPTION     A 60% ISO hotswap inswitch rgb board
 
 /* key matrix size */
 #define MATRIX_ROWS 5
