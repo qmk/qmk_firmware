7a71c11bef216c6e4afbeba65e128aa2a5bc2033
Max Drasbeck <max@maryam.local>
2020-08-14 10:43:25 +0200

    adjusting tapping term

--- keyboards/lily58/keymaps/drasbeck/config.h
+++ keyboards/lily58/keymaps/drasbeck/config.h
@@ -1,21 +1,13 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) 2020 Max Drasbeck 
+ *
+ * You are free to:
+ *
+ *  Share — copy and redistribute the material in any medium or format
+ *  Adapt — remix, transform, and build upon the material
+ *   for any purpose, even commercially.
+ *
+ * The licensor cannot revoke these freedoms as long as you follow the license terms.
 */
 
 #pragma once
@@ -31,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 180
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
