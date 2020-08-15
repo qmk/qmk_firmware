b18e5fcdf0c3ddfd6ede6ff335787bd54d4a7b1b
Sendy YK <mr@sendyyk.com>
2020-08-16 05:14:49 +0700

    Update

--- keyboards/dz60/keymaps/mrsendyyk/keymap.c
+++ keyboards/dz60/keymaps/mrsendyyk/keymap.c
@@ -1,19 +1,19 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+  
   /* Layer [0]
-   * ,--------------------------------------------------------------------------.
-   * |Esc |1   |2   |3   |4   |5   |6   |7   |8   |9   |0   |-   |=   |Backspace|
-   * |--------------------------------------------------------------------------+
-   * |Tab   |Q   |W   |E   |R   |T   |Y   |U   |I   |O   |P   |[   |]   |\      |
-   * |--------------------------------------------------------------------------+
-   * |Caps Lck|A   |S   |D   |F   |G   |H   |J   |K   |L   |;   |'   |Enter     |
-   * |--------------------------------------------------------------------------+
-   * |Shift     |Z   |X   |C   |V   |B   |N   |M   |,   |.   |Shift   |↑   |/   |
-   * |--------------------------------------------------------------------------+
-   * |Ctrl |OS   |Alt  |              Space            |Alt |Ctrl|←   |↓   |→   |
-   * `--------------------------------------------------------------------------'
+   *┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+   *│Esc│1  │2  │3  │4  │5  │6  │7  │8  │9  │0  │-  │=  │Bspc   │
+   *├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+   *│Tab  │Q  │W  │E  │R  │T  │Y  │U  │I  │O  │P  │[  │]  │\    │
+   *├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
+   *│Caps L│A  │S  │D  │F  │G  │H  │J  │K  │L  │;  │'  │Enter   │
+   *├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
+   *│Shift   │Z  │X  │C  │V  │B  │N  │M  │,  │.  │Shift │↑  │/  │
+   *├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬───┼───┼───┤
+   *│Ctl │OS  │Alt │          Space         │Alt│Ctl│←  │↓  │→  │
+   *└────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
    */
 
   [0] = LAYOUT_60_ansi_arrows(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
@@ -23,37 +23,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, LT(2, KC_RALT), LT(1, KC_RCTL), KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Layer [1] (Press & Hold Right `Ctrl` Key)
-   * ,--------------------------------------------------------------------------.
-   * |`   |F1  |F2  |F3  |F4  |F5  |F6  |F7  |F8  |F9  |F10 |F11 |F12 |Delete   |
-   * |--------------------------------------------------------------------------+
-   * |Bri Up|    |    |End |Rst |    |    |    |Ins |    |Pr S|    |    |Eject  |
-   * |--------------------------------------------------------------------------+
-   * |Bri Down|    |Sc L|    |    |    |Home|    |    |    |    |    |Mute      |
-   * |--------------------------------------------------------------------------+
-   * |Vol +     |    |    |Calc|    |    |Nm L|Mail|    |    |Pause   |Pg U|    |
-   * |--------------------------------------------------------------------------+
-   * |Vol -|Rwd  |F Fd |              Play             |M Sp|    |Prev|Pg D|Next|
-   * `--------------------------------------------------------------------------'
+   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+   * │`  │F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│Delete │
+   * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+   * │Bri+ │   │   │End│Rst│   │   │   │Ins│   │P S│   │   │Eject│
+   * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
+   * │Bri-  │   │S L│   │   │   │Hm │   │   │   │   │   │Mute    │
+   * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
+   * │Vol+    │   │   │Cal│   │   │N L│Ml │   │   │Pause │P U│   │
+   * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬───┼───┼───┤
+   * │Vol-│Rwd │F FD│           Play         │Stp│   │Prv│P D│Nxt│
+   * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
    */
 
 	[1] = LAYOUT_60_ansi_arrows(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-                                KC_BRIU, _______, _______, KC_END, RESET, _______, _______, _______, KC_INS, _______, KC_PSCR, _______, _______, KC_EJCT,
-                                KC_BRID, _______, KC_SLCK, _______, _______, _______, KC_HOME, _______, _______, _______, _______, _______, KC_MUTE,
-                                KC_VOLU, _______, _______, KC_CALC, _______, _______, KC_NLCK, KC_MAIL, _______, _______, KC_PAUS, KC_PGUP, _______,
-                                KC_VOLD, KC_MRWD, KC_MFFD, KC_MPLY, KC_MSTP, KC_TRNS, KC_MPRV, KC_PGDN, KC_MNXT),
+                              KC_BRIU, _______, _______, KC_END, RESET, _______, _______, _______, KC_INS, _______, KC_PSCR, _______, _______, KC_EJCT,
+                              KC_BRID, _______, KC_SLCK, _______, _______, _______, KC_HOME, _______, _______, _______, _______, _______, KC_MUTE,
+                              KC_VOLU, _______, _______, KC_CALC, _______, _______, KC_NLCK, KC_MAIL, _______, _______, KC_PAUS, KC_PGUP, _______,
+                              KC_VOLD, KC_MRWD, KC_MFFD, KC_MPLY, KC_MSTP, KC_TRNS, KC_MPRV, KC_PGDN, KC_MNXT),
 
   /* Layer [2] (Press & Hold Right `Alt` Key)
-   * ,--------------------------------------------------------------------------.
-   * |    |    |    |    |    |    |    |    |    |    |    |    |    |         |
-   * |--------------------------------------------------------------------------+
-   * |      |R Tg|R M+|Hue+|Hue-|Sat+|Sat-|Bri+|Bri-|    |    |    |    |       |
-   * |--------------------------------------------------------------------------+
-   * |        |    |    |    |    |    |    |    |    |    |    |    |          |
-   * |--------------------------------------------------------------------------+
-   * |          |    |    |    |BL -|B Tg|BL+|B Cy|     |    |        |    |    |
-   * |--------------------------------------------------------------------------+
-   * |     |     |     |                               |    |    |    |    |    |
-   * `--------------------------------------------------------------------------'
+   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+   * │   │   │   │   │   │   │   │   │   │   │   │   │   │       │
+   * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+   * │     │R T│R M│Hu+│Hu-│Sa+│Sa-│Br+│Br-│   │   │   │   │     │
+   * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
+   * │      │   │   │   │   │   │   │   │   │   │   │   │        │
+   * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
+   * │        │   │   │   │BL-│B T│BL+│B C│   │   │      │   │   │
+   * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬───┼───┼───┤
+   * │    │    │    │                        │   │   │   │   │   │
+   * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
    */
 
 	[2] = LAYOUT_60_ansi_arrows(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -68,30 +68,28 @@ void keyboard_post_init_user(void) {
   rgblight_sethsv_noeeprom(0, 0, 0);
 };
 
-// RGB Underglow as Caps Lock, Num Lock, Scroll Lock, and Layer Indicator
-
 void update_led(void) {
-  if (host_keyboard_led_state().num_lock) { // Num Lock Indicator (Press & Hold Right `Ctrl` Key + Press `N` Key)
+  if (host_keyboard_led_state().num_lock) { // Num Lock indicator
     rgblight_setrgb(225, 8, 0);
   }
-  if (host_keyboard_led_state().scroll_lock) { // Scroll Lock Indicator (Press & Hold Right `Ctrl` Key + Press `S` Key)
+  if (host_keyboard_led_state().scroll_lock) { // Scroll Lock indicator
     rgblight_setrgb(0, 100, 255);
   }
 };
 
 void led_set_user(uint8_t usb_led) {
-  if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) { // Caps Lock Indicator
+  if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) { // Caps Lock indicator
     writePinLow(B2);
     rgblight_setrgb(100, 255, 100);
-  } else { // Layer Indicator
+  } else {
       switch (get_highest_layer(layer_state)) {
-        case 1: // Layer [1] Indicator (Press & Hold Right `Ctrl` Key)
+        case 1: // Layer [1] indicator
           rgblight_setrgb(255, 110, 0);
           break;
-        case 2: // Layer [2] Indicator (Press & Hold Right `Alt` Key)
+        case 2: // Layer [2] indicator
           rgblight_setrgb(255, 110, 0);
           break;
-        default: // Layer [0] Indicator
+        default: // Layer [0] indicator
           rgblight_setrgb(0, 0, 0);
           break;
       }
