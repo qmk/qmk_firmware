414f48e5d87ffc80808261763a365ce053569d2c
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-08-16 10:09:22 -0700

    Tweaked mousekey config

--- users/gotham/config.h
+++ users/gotham/config.h
@@ -12,9 +12,9 @@
 
 #ifdef MOUSEKEY_ENABLE
 #    define MOUSEKEY_DELAY 	           200 // Delay between pressing a movement key and cursor movement
-#    define MOUSEKEY_INTERVAL          16  // Time between cursor movements
+#    define MOUSEKEY_INTERVAL          8   // Time between cursor movements
 #    define MOUSEKEY_MAX_SPEED         8   // Maximum cursor speed at which acceleration stops
-#    define MOUSEKEY_TIME_TO_MAX       10  // Time until maximum cursor speed is reached
+#    define MOUSEKEY_TIME_TO_MAX       20  // Time until maximum cursor speed is reached
 #    define MOUSEKEY_WHEEL_DELAY       200 // Delay between pressing a wheel key and wheel movement
 #    define MOUSEKEY_WHEEL_INTERVAL    150 // Time between wheel movements
 #    define MOUSEKEY_WHEEL_MAX_SPEED   8   // Maximum number of scroll steps per scroll action
