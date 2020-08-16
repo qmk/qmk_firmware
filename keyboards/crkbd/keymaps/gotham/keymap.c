30ee4f83ad50b738a072025a66d22ac38e389e08
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-08-16 02:28:19 -0700

    git commit -m "Use different macro for layout wrapper"

--- keyboards/crkbd/keymaps/gotham/keymap.c
+++ keyboards/crkbd/keymaps/gotham/keymap.c
@@ -28,7 +28,7 @@ enum crkbd_keybodes {
     K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
     K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A  \
   ) \
-  LAYOUT_split_3x6_3( \
+  LAYOUT_wrapper( \
     KC_ESC,   K01,  K02,  K03,  K04,  K05,    K06,  K07,  K08,  K09,  K0A,  KC_BSLS, \
     KC_LSFT,  K11,  K12,  K13,  K14,  K15,    K16,  K17,  K18,  K19,  K1A,  RSFT_T(KC_QUOT), \
     CTL_EQL,  K21,  K22,  K23,  K24,  K25,    K26,  K27,  K28,  K29,  K2A,  LT(_ADJUST, KC_MINS), \
