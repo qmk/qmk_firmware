24f09820a56e0e0fbea51a8a6c751abe6996c21e
BenRoe <mail@benjaminroesner.com>
2020-08-16 19:03:12 +0200

    Add via keymap for Plaid-Pad
    - Add VIA support for the Plaid-Pad
    - Changes Vendor ID and Product ID (to follow VIA's guidelines)

--- keyboards/keycapsss/plaid_pad/config.h
+++ keyboards/keycapsss/plaid_pad/config.h
@@ -3,8 +3,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0xAF12
+#define VENDOR_ID 0x7983
+#define PRODUCT_ID 0x7070   //  8-bit ASCII for PP
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Keycapsss
 #define PRODUCT Plaid-Pad
