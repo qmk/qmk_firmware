6c14bb6e88eca7812c5f2f63a5d596758c4bbeab
Hardy Miranda <hardy@hardymiranda.com>
2020-08-15 22:29:38 -0300

    Adjust to nyquist personal template

--- keyboards/keebio/nyquist/keymaps/hdcore/config.h
+++ keyboards/keebio/nyquist/keymaps/hdcore/config.h
@@ -15,18 +15,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
-
 #define USE_I2C
+// #define USE_SERIAL
 
 /* Select hand configuration */
-// #define MASTER_RIGHT
-// #define MASTER_RIGHT
+#define MASTER_LEFT
 // #define EE_HANDS
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
