e35f31cb1e5750a060c69c27ad1a1555322ca3cf
Yoonbae Cho <y@xcv.kr>
2020-08-17 09:52:10 +0900

    Update keymap.c

--- keyboards/jj50/keymaps/yoonbae81/keymap.c
+++ keyboards/jj50/keymaps/yoonbae81/keymap.c
@@ -13,15 +13,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
-     MO(1),   MO(2),   KC_LGUI, KC_LALT, LT(1,KC_ENT), KC_NO, KC_SPC, KC_NO, LT(1,KC_RALT),LT(2,KC_RCTL), MO(1), MO(2)
+     MO(2),   MO(3), KC_LGUI, KC_LALT, LT(1,KC_ENT),KC_NO, KC_SPC,KC_NO, LT(1,KC_RALT), LT(2,KC_RCTL), KC_APP, TO(2)
   ),
 
   [_FN1] = LAYOUT(
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-     _______, KC_LCBR, KC_PGUP, KC_UP,   KC_HOME, KC_RCBR, KC_NO,   KC_HOME, KC_PGUP, KC_NO,   KC_NO,   KC_NO, 
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     _______, KC_LCBR, KC_PGUP, KC_UP,   KC_HOME, KC_RCBR, KC_NO,   KC_HOME, KC_PGUP, KC_NO,   KC_NO,   KC_F12, 
      _______, KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MINS, KC_EQL,
      _______, KC_LBRC, KC_END,  KC_NO,   KC_PGDN, KC_RBRC, KC_NO,   KC_PGDN, KC_END,  KC_ENT,  KC_INS,  KC_DEL,
-     _______, TO(2),   _______, _______, _______, _______, KC_BSPC, _______, _______, _______, _______, TO(2)
+     _______, _______, _______, _______, _______, _______, KC_BSPC, _______, _______, _______, _______, _______
   ),
 
   [_FN2] = LAYOUT(
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______, KC_NO,   KC_WH_U, KC_MS_U, KC_NO,   KC_NO,   KC_F2,   KC_7,    KC_8,    KC_9,    KC_PAST, KC_NO, 
      _______, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_ENT,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_EQL,
      _______, KC_NO,   KC_NO,   KC_NO,   KC_WH_D, KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_PPLS, KC_BSPC,
-     _______, _______, KC_BTN1, KC_BTN2, _______, _______, KC_0,    _______, KC_PDOT, KC_BTN1, KC_BTN2, KC_NO
+     _______, _______, _______, KC_BTN2, KC_BTN1, _______, KC_0,    _______, KC_PDOT, _______, _______, TO(0)
   ),
 
   [_FN3] = LAYOUT(
-     RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   _______,
      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
      _______, KC_MPRV, KC_MRWD, KC_MFFD, KC_MNXT, _______, _______, _______, _______, _______, _______, _______,
      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
