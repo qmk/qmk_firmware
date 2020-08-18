e6bef2f4759eeb099c82e17fc93be0e719340345
Blake Drayson <blake@ghost7.com>
2020-08-18 08:50:37 +0100

    Licence updates

--- keyboards/handwired/bento/keymaps/mac/keymap.c
+++ keyboards/handwired/bento/keymaps/mac/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+/* Copyright 2020 GhostSeven <work@ghost7.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
