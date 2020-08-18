d2cc920d24d7f4c728baf5611037da13c7b1b489
yiancar <yiangosyiangou@cytanet.com.cy>
2020-08-19 00:19:14 +0100

    Update tmk_core/protocol/vusb/usbconfig.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- tmk_core/protocol/vusb/usbconfig.h
+++ tmk_core/protocol/vusb/usbconfig.h
@@ -47,7 +47,7 @@ section at the end of this file).
  * interrupt, the USB interrupt will also be triggered at Start-Of-Frame
  * markers every millisecond.]
  */
-#define USB_CFG_CHECK_CRC               0
+#define USB_CFG_CHECK_CRC       0
 /* Define this to 1 if you want that the driver checks integrity of incoming
  * data packets (CRC checks). CRC checks cost quite a bit of code size and are
  * currently only available for 18 MHz crystal clock. You must choose
