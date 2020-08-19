91e7040ad40996da1525750fc88ddd73f131edbd
JagdPietr <59313824+JagdPietr@users.noreply.github.com>
2020-08-19 17:06:16 +0800

    Update keyboards/drakon/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/drakon/config.h
+++ keyboards/drakon/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0001
 #define MANUFACTURER jagdpietr
 #define PRODUCT drakon
-#define DESCRIPTION Custom 60% with OLED and ALPS Encoder
 
 /* key matrix size */
 #define MATRIX_ROWS 6
