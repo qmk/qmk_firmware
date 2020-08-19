387e2b337938418ba34530aaf63c83fd3e2659c1
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2020-08-19 09:08:08 +0900

    Update keyboards/ajisai74/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/ajisai74/keymaps/default/keymap.c
+++ keyboards/ajisai74/keymaps/default/keymap.c
@@ -38,8 +38,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------------------------------------------------|   |--------------------------------------------------------------------------------'
   )
 };
-
-//A description for expressing the layer position in LED mode.
-layer_state_t layer_state_set_user(layer_state_t state) {
-return state;
-}
+``
