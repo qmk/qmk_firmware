5f5ed890e8cee5e3bd7cff4998eef92c85e09ac4
Garret G <the-royal@github.com>
2020-08-19 20:54:47 -0500

    fix duplicate matrix key

--- keyboards/45_ats/45_ats.h
+++ keyboards/45_ats/45_ats.h
@@ -53,5 +53,5 @@
     { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D }, \
     { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, xxx, k1D }, \
     { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, xxx, k2D }, \
-    { k30, k31, k32, xxx, xxx, xxx, k36, xxx, k38, k39, k2A, k3B, xxx, k3D }  \
+    { k30, k31, k32, xxx, xxx, xxx, k36, xxx, k38, k39, k3A, k3B, xxx, k3D }  \
 }
