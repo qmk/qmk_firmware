df0bed04be4a80bc2db45181a97809303effb0ab
yulei <yuleiz@gmail.com>
2020-08-20 11:44:10 +0800

    Update keyboards/inett_studio/sqx/universal/universal.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/inett_studio/sqx/universal/universal.h
+++ keyboards/inett_studio/sqx/universal/universal.h
@@ -39,7 +39,7 @@
     {k40,   k42, k43, KC_NO, KC_NO, KC_NO, k47, k07, KC_NO, KC_NO, k48, k49,   k4a, k4b}  \
 }
 
-#define LAYOUT_60_wkl( \
+#define LAYOUT_60_ansi_tsangan_split_rshift( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0e, \
 	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1e, \
 	k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2e, \
