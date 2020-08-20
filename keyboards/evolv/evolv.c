d4295aa9d8c299c35cd75f9b3648c0516501b518
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2020-08-20 11:15:31 -0300

    Fix encoder directional and layer support

--- keyboards/evolv/evolv.c
+++ keyboards/evolv/evolv.c
@@ -16,3 +16,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "evolv.h"
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } 
+}
