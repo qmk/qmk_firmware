0be8599b0daab91741bbaff4b7ec587dc9686afc
Jim Sangwine <jim@sangwine.net>
2020-08-21 20:39:56 +0800

    Update dz60 split spacebar layout

--- keyboards/dz60/keymaps/EvilJimJafar/keymap.c
+++ keyboards/dz60/keymaps/EvilJimJafar/keymap.c
@@ -3,7 +3,7 @@
 #define _DEFAULT 0
 #define _CODING 1
 #define _NAV 2
-#define _NUMPAD 3
+#define _NUM 3
 #define _RGB 4
 
 /* DZ60 layout using following options (from layouts diagram on KBDfans):
@@ -43,34 +43,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // |--------------||--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|------------------||
        //
        // |------2.25--------|--------|--------|--------|--------|--------|--------|--------|--------|--------||----1.75------|--------|--------||
-               KC_LSHIFT,      KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  SFT_T(KC_SLSH),  KC_UP, MO(_RGB),
+               KC_LSHIFT,      KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),  KC_UP, MO(_RGB),
        // |------------------|--------|--------|--------|--------|--------|--------|--------|--------|--------||--------------|--------|--------||
        //
        // |---1.25---|---1.25---||---1.25---||--------2.25--------||---1.25---|------2.75----------||--------|--------|--------|--------|--------|
-             KC_CAPS,  KC_LALT,    KC_LGUI,       MO(_CODING),       MO(_NAV),      KC_SPC,          KC_HYPR, TG(_NUM), KC_LEFT, KC_DOWN, KC_RGHT
+             KC_CAPS,  KC_LALT,    KC_LGUI,       MO(_CODING),       MO(_NAV),      KC_SPC,          TG(_NUM), KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT
        // |----------|----------||----------||--------------------||----------|--------------------||--------|--------|--------|--------|--------|
           ),
 
     [_CODING] = LAYOUT_b_4_9(
          KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, _______, _______, _______, _______, _______, _______, KC_LBRC, _______, _______, KC_RBRC, _______, _______,   RESET,
+        _______, _______, _______, _______, _______, _______, _______, KC_LBRC, _______, _______, KC_RBRC, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR,  KC_GRV,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,          _______, _______,          _______,          _______, _______, _______, _______, _______
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, KC_MUTE,
+        _______, _______, _______,          _______, _______,          _______,          _______, KC_MPLY, KC_MPRV, KC_VOLD, KC_MNXT
     ),
     [_NAV] = LAYOUT_b_4_9(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_DEL,
-        _______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, _______, KC_TILD, _______,   RESET,
-        _______, _______, _______, _______, _______, _______, _______, KC_LEFT, _______, KC_RGHT, _______,  KC_GRV,          _______,
-        _______,          _______, _______, _______, _______, _______, KC_PGDN, KC_DOWN, KC_PGUP, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_TILD,          _______,
+        _______,          _______, _______, _______, _______, _______, KC_PGDN, _______, KC_PGUP, _______, _______, _______, _______,
         _______, _______, _______,          _______, _______,          _______,          _______, _______, _______, _______, _______
     ),
-    [_NUMPAD] = LAYOUT_b_4_9(
-          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_NO,   KC_NO, KC_BSPC,
+    [_NUM] = LAYOUT_b_4_9(
+          TG(_NUM),KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_PSLS, KC_PAST,  KC_PMNS,  KC_NO,   KC_NO, KC_BSPC,
           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_NO,   KC_NO,   KC_NO,
           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PCMM, KC_NO,          KC_PENT,
-          KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,     KC_P3, KC_PEQL,          KC_NO,
-          KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,            KC_NO,            KC_P0,   KC_PDOT, KC_NO,   KC_NO,   KC_NO
+          KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,     KC_P3, KC_PEQL, KC_NO,   KC_NO,
+          KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,            KC_P0,          KC_PDOT,     KC_NO, KC_NO,   KC_NO,   KC_NO
     ),
     [_RGB] = LAYOUT_b_4_9(
         RGB_TOG, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, RGB_SPI, RGB_SPD, _______, _______, _______, _______,
