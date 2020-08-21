895b7144abfcc8cebdc919b20469aca010fc1afa
Drashna Jael're <drashna@live.com>
2020-08-21 01:35:20 -0700

    Makes sure that ol(e)d and new OLED implementation can't coexist

--- keyboards/crkbd/rev1/legacy/post_config.h
+++ keyboards/crkbd/rev1/legacy/post_config.h
@@ -3,3 +3,7 @@
 #ifdef SSD1306OLED
 #    define USE_I2C
 #endif
+
+#if defined(OLED_DRIVER_ENABLE) && (defined(USE_I2C) || defined(USE_MATRIX_I2C))
+#    error Cannot use both legacy i2c driver and new i2c_master driver at the same time. Undefine USE_I2C and/or USE_MATRIX_I2C
+#endif
