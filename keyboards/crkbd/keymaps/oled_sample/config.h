24fd427d74997d15ba67a41a952f83a0ab6f7dce
Drashna Jael're <drashna@live.com>
2020-08-21 17:41:59 -0700

    Add licensing header to files

--- keyboards/crkbd/keymaps/oled_sample/config.h
+++ keyboards/crkbd/keymaps/oled_sample/config.h
@@ -1,8 +1,6 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,6 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+
 
 #pragma once
 
