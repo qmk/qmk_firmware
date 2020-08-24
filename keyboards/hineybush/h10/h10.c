96923446a30cf66e43c77bd4e4668885286c8015
Josh Hinnebusch <joshhinnebusch@gmail.com>
2020-08-24 11:13:45 -0400

    Update keyboards/hineybush/h10/h10.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/hineybush/h10/h10.c
+++ keyboards/hineybush/h10/h10.c
@@ -21,8 +21,8 @@
 // These are only required if you want to perform custom actions.
 
 void matrix_init_kb(void) {
-	// put your keyboard start-up code here
-	// runs once when the firmware starts up
+    // put your keyboard start-up code here
+    // runs once when the firmware starts up
    	setPinOutput(F7);
 }
 
