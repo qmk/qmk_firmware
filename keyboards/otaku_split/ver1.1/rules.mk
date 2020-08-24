0edf51aec1e551d295837901c044c4dac8f32782
takashiski <takashiskibb@gmail.com>
2020-08-25 00:33:33 +0900

    Update keyboards/otaku_split/ver1.1/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/otaku_split/ver1.1/rules.mk
+++ keyboards/otaku_split/ver1.1/rules.mk
@@ -29,6 +29,4 @@ MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
 SPLIT_KEYBOARD=yes
