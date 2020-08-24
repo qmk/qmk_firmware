a41d59a88612ee3f4548f3f24d3b352aa94e1bbd
Sean <3breach@gmail.com>
2020-08-23 23:06:22 -0700

    Near complete

--- keyboards/shawkesPad/keymaps/default/keymap.c
+++ keyboards/shawkesPad/keymaps/default/keymap.c
@@ -15,62 +15,17 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-    _FN
-};
-
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
-    QMKURL
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
-    [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
+    
+    LAYOUT(
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, 
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, 
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, 
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, 
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, 
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, 
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, 
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8
     )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
-    return true;
-}
-
-/*
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool led_update_user(led_t led_state) {
-    return true;
-}
-*/
--- keyboards/shawkesPad/shawkesPad.h
+++ keyboards/shawkesPad/shawkesPad.h
@@ -18,19 +18,26 @@
 
 #include "quantum.h"
 
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
+#define ___ KC_NO
+
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
-) \
-{ \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
+    K00, K01, K02, K03, K04, K05, K06, K07, \
+    K10, K11, K12, K13, K14, K15, K16, K17, \
+    K20, K21, K22, K23, K24, K25, K26, K27, \
+    K30, K31, K32, K33, K34, K35, K36, K37, \
+    K40, K41, K42, K43, K44, K45, K46, K47, \
+    K50, K51, K52, K53, K54, K55, K56, K57, \
+    K60, K61, K62, K63, K64, K65, K66, K67, \
+    K70, K71, K72, K73, K74, K75, K76, K77  \
+) { \
+    { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07    }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17    }, \
+    { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27    }, \
+    { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37    }, \
+    { K40,   K41,   K42,   K43,   K44,   K45,   K46,   K47    }, \
+    { K50,   K51,   K52,   K53,   K54,   K55,   K56,   K57    }, \
+    { K60,   K61,   K62,   K63,   K64,   K65,   K66,   K67    }, \
+    { K70,   K71,   K72,   K73,   K74,   K75,   K76,   K77    }  \
 }
+
+
