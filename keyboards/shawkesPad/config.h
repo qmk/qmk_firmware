b4af535a6af502b45bfce78c906a78fbcc87f038
Sean <3breach@gmail.com>
2020-08-23 23:32:31 -0700

    Some cleanup

--- keyboards/shawkesPad/config.h
+++ keyboards/shawkesPad/config.h
@@ -31,24 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 8
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
-#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4, B5 }
-#define UNUSED_PINS
-
-#define ENCODERS_PAD_A { D5, F1 }
-#define ENCODERS_PAD_B { C7, F0 }
-
-/* COL2ROW, ROW2COL*/
+/* Keyboard Matrix Assignments */
 #define DIODE_DIRECTION COL2ROW
 
 #define BACKLIGHT_PIN B7
@@ -57,7 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
 
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
--- keyboards/shawkesPad/rules.mk
+++ keyboards/shawkesPad/rules.mk
@@ -30,4 +30,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
-ENCODER_ENABLE = yes
+ENCODER_ENABLE = yes		# Enable encorder support
