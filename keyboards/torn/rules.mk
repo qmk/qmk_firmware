189d00a7e73fbc6fad9298ebc1f9217622711c7c
Richard Titmuss <richardt@spotify.com>
2020-08-26 21:30:18 +0200

    Add Torn keyboard

---
+++ keyboards/torn/rules.mk
@@ -0,0 +1,35 @@
+# MCU name
+MCU = atmega328p
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = USBasp
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+UNICODE_ENABLE = no         # Unicode
+ENCODER_ENABLE = yes        # Enable rotary encoder
+OLED_DRIVER_ENABLE = yes
+CUSTOM_MATRIX = lite
+
+SRC += \
+    i2c_master.c \
+    matrix.c \
+    mcp23018.c \
+    torn_encoder.c \
+    torn_oled.c
