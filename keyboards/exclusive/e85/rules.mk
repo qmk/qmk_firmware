c1d6a2cfafa8195d10008f861daa25390b264f63
MechMerlin <30334081+mechmerlin@users.noreply.github.com>
2020-08-27 06:40:42 -0700

    Update keyboards/exclusive/e85/rules.mk
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/exclusive/e85/rules.mk
+++ keyboards/exclusive/e85/rules.mk
@@ -21,3 +21,5 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 LTO_ENABLE = yes
+
+DEFAULT_FOLDER = exclusive/e85/hotswap
