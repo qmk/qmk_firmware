6b91da96019b3311c2c38738ca4adb8cee8f46eb
pabile <1162412+pabile@users.noreply.github.com>
2020-08-27 18:47:01 +0800

    Update keyboards/pabile/p40_ortho/keymaps/default/keymap.c
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/pabile/p40_ortho/keymaps/default/keymap.c
+++ keyboards/pabile/p40_ortho/keymaps/default/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,         KC_J,   KC_K,    KC_L,   LT(3,KC_MINS),
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,         KC_M,   KC_COMM, KC_DOT, LT(4,KC_SLSH),
     KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, LT(2,KC_BSPC), LT(1,KC_SPC), KC_ENT, KC_APP,  KC_DEL, KC_ESC),
-  [1] = LAYOUT_default(
+  [1] = LAYOUT_ortho_4x10(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_TAB,  KC_NO,   KC_NO,   KC_GRV,  KC_LBRC, KC_RBRC, KC_BSLS, KC_SCLN, KC_QUOT, KC_MINS,
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_EQL,  KC_MINS, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,
