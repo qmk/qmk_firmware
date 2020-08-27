2af99744a93a5ea31a77360790e506df567ec1ac
pabile <1162412+pabile@users.noreply.github.com>
2020-08-27 18:48:29 +0800

    Update keyboards/pabile/p40_ortho/p40_ortho.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/pabile/p40_ortho/p40_ortho.h
+++ keyboards/pabile/p40_ortho/p40_ortho.h
@@ -2,7 +2,7 @@
 
 #include "quantum.h"
 
-#define LAYOUT_default( \
+#define LAYOUT_ortho_4x10( \
       k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
       k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
       k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
