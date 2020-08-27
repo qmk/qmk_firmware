294dab2f09650f80ce1452541d5111025f01d82e
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2020-08-27 17:08:40 +0900

    continuous work on ee2c39f1f9bffd97a2deb8f430746933a8d8684c

--- keyboards/helix/rev3/5rows/5rows.c
+++ keyboards/helix/rev3/5rows/5rows.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "helix_5r.h"
+#include "5rows.h"
 
 #ifdef RGB_MATRIX_ENABLE
 led_config_t g_led_config = { {
