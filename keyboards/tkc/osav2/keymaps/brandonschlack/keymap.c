e20349bad2212537661074fd2868feb95da5d6f1
Brandon Schlack <brandonschlack@gmail.com>
2020-08-27 12:04:34 -0700

    [Keymap] Update mods for OSAv2

--- keyboards/tkc/osav2/keymaps/brandonschlack/keymap.c
+++ keyboards/tkc/osav2/keymaps/brandonschlack/keymap.c
@@ -10,17 +10,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├───┤ ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┐        └┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
  * │PgD│ │HyCaps│ A │ S │ D │ F │ G │         │ H │ J │ K │ L │: ;│" '│   Enter│
  * └───┘ ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┐     ┌─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
- *       │LShift  │ Z │ X │ C │ V │ B │     │ B │ N │ M │< ,│> .│? /│↑ Shft│FnP│
+ *       │LShift  │ Z │ X │ C │ V │ B │     │ B │ N │ M │< ,│> .│? /│↑ Shft│Fn │
  *       ├─────┬──┴──┬┴───┴┬──┴───┴┬──┴─┐   ├───┴───┴──┬┴───┴┬──┴───┴────┬─┴───┤
- *       │Ctrl │     │Opt  │Spc/Cmd│Fn2 │   │   Spc/Cmd│  Opt│           │ Ctrl│
+ *       │Opt  │     │Cmd  │Spc/Fn1│Ctrl│   │   Spc/Rai│  Cmd│           │  Opt│
  *       └─────┘     └─────┴───────┴────┘   └──────────┴─────┘           └─────┘
  */
 [_BASE] = LAYOUT_all( \
     KC_HOME, KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  \
     KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
     KC_PGDN, HY_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
-             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, PLY_FN1, \
-             KC_LCTL,          KC_LOPT,     CMD_SPC,      FN2_LYR,          CMD_SPC,          KC_ROPT,                   KC_RCTL \
+             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TT(_FN1),\
+             KC_LOPT,          KC_LCMD,     SPC_LWR,      KC_LCTL,          SPC_RAI,          KC_RCMD,                   KC_ROPT \
 ),
 [_FN1] = LAYOUT_all( \
     KC_MUTE, QM_MAKE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  MC_LHPD, MC_SLPD, \
