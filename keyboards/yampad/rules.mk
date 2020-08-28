f8f826ff9560553b9b5bd1d3824e478d70e90ae2
Daveyr <26368709+Daveyr@users.noreply.github.com>
2020-08-28 21:45:31 +0100

    Update keyboards/yampad/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/yampad/rules.mk
+++ keyboards/yampad/rules.mk
@@ -17,17 +17,15 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   comment out to disable the options.
 #
-#BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no	    # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no	    # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+BOOTMAGIC_ENABLE = no     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no	    # Mouse keys
+EXTRAKEY_ENABLE = no	    # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes		    # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-RGBLIGHT_ENABLE = yes       # Enable keyboard underlight functionality (+4870)
-#MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
+RGBLIGHT_ENABLE = yes       # Enable keyboard underlight functionality
+MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
+AUDIO_ENABLE = no           # Audio output
 OLED_DRIVER_ENABLE = yes    # Enable OLED feature
