7c2c91e54f84b64871e1543823a93011924b2fc7
Shern Shiou Tan <shernshiou@retinai.com>
2020-08-28 16:45:41 +0200

    feat: Add volume control to encoder at default keymap

--- keyboards/pikatea/keymaps/default/keymap.c
+++ keyboards/pikatea/keymaps/default/keymap.c
@@ -31,3 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,       KC_NO,      KC_NO,       KC_NO,      KC_NO
     )
 };
+
+#ifdef ENCODER_ENABLE
+void encoder_update_user(uint8_t index, bool clockwise) {
+    // Volume control
+    if (!clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+}
+#endif
