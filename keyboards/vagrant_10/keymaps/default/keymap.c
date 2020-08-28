fad00436d6f92e8f7cce7dc5505681401707f6b7
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-08-29 00:53:59 +0200

    Update keyboards/vagrant_10/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/vagrant_10/keymaps/default/keymap.c
+++ keyboards/vagrant_10/keymaps/default/keymap.c
@@ -10,26 +10,26 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[HOME] = LAYOUT(
-					  KC_P0,
-		KC_P7, KC_P8, KC_P9,
-		KC_P4, KC_P5, KC_P6,
-		LT(1, KC_P1), LT(2, KC_P2), LT(3, KC_P3)),
+                          KC_P0,
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,
+        LT(1, KC_P1), LT(2, KC_P2), LT(3, KC_P3)),
 
 	[MODS] = LAYOUT(
-						  KC_MUTE,
-		KC_MPLY, KC_NO,   KC_VOLU,
-		KC_MPRV, KC_MNXT, KC_VOLD,
-		KC_TRNS, KC_NO,   KC_NO),
+                          KC_MUTE,
+        KC_MPLY, KC_NO,   KC_VOLU,
+        KC_MPRV, KC_MNXT, KC_VOLD,
+        KC_TRNS, KC_NO,   KC_NO),
 
 	[MODS2] = LAYOUT(
-						KC_F13,
-		KC_F14, KC_F15, KC_F16,
-		KC_F19, KC_F18, KC_F17,
-		KC_NO,  KC_TRNS, KC_NO),
+                          KC_F13,
+        KC_F14,  KC_F15,  KC_F16,
+        KC_F19,  KC_F18,  KC_F17,
+        KC_NO,   KC_TRNS, KC_NO),
 
 	[OTHER] = LAYOUT(
-						RESET,
-		KC_NO, KC_NO, KC_NO,
-		KC_NO, KC_NO, KC_NO,
-		KC_NO, KC_NO, KC_TRNS),
+                          RESET,
+        KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_TRNS),
 };
