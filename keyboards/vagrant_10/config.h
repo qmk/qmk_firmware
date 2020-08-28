0f4860648b9d2e55ef0ecd9a7f1d774cdacffb4c
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-08-29 00:54:58 +0200

    Update keyboards/vagrant_10/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/vagrant_10/config.h
+++ keyboards/vagrant_10/config.h
@@ -3,7 +3,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define PRODUCT_ID      0x5E99
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Shanduur
 #define PRODUCT         vagrant_10
