7f7865d672b0c9020e3cc43fff7216a7456cca1a
santy81855 <62843966+santy81855@users.noreply.github.com>
2020-08-29 12:24:39 -0400

    Update keyboards/handwired/NoStab63/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/NoStab63/config.h
+++ keyboards/handwired/NoStab63/config.h
@@ -3,11 +3,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-<<<<<<< HEAD
-#define VENDOR_ID       0x0000
-=======
 #define VENDOR_ID       0xFEED
->>>>>>> 4291fa68390067549e51bb52195ec9cabf6153cb
 #define PRODUCT_ID      0x1707
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Santy
