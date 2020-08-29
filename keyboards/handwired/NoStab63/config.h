64e80fa29a0401726c3a9b5bb45ac1e2c2d9a660
Santy <mynameissantiagogarcia@gmail.com>
2020-08-29 02:06:14 -0400

    added suggested changes

--- keyboards/handwired/NoStab63/config.h
+++ keyboards/handwired/NoStab63/config.h
@@ -3,12 +3,11 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x0000
+#define PRODUCT_ID      0x1707
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Santy
 #define PRODUCT         NoStab63
-#define DESCRIPTION     An Ergonomic Stabless Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
--- keyboards/handwired/NoStab63/keymaps/default/keymap.c
+++ keyboards/handwired/NoStab63/keymaps/default/keymap.c
@@ -1,7 +1,23 @@
+/* Copyright 2020 Santiago Garcia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _DVORAK,
+	_DVORAK,
 	_CAMILA,
     _NUMBER,
     _FN1,
@@ -12,12 +28,12 @@ enum layers {
 };
 
 enum unicode_names {
-  SE_AA_HIGH,
-  SE_AE_HIGH,
-  SE_OE_HIGH,
-  SE_AA_LOW,
-  SE_AE_LOW,
-  SE_OE_LOW,
+	SE_AA_HIGH,
+	SE_AE_HIGH,
+	SE_OE_HIGH,
+	SE_AA_LOW,
+	SE_AE_LOW,
+	SE_OE_LOW,
 };
 
 const uint32_t PROGMEM unicode_map[] = {
