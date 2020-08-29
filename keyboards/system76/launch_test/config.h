2deeef597497b194b52ee24344f59a36bab68239
Jeremy Soller <jeremy@system76.com>
2020-08-28 20:54:15 -0600

    Add launch testboard

---
+++ keyboards/system76/launch_test/config.h
@@ -0,0 +1,39 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x1776
+#define PRODUCT_ID      0x1776
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    System76
+#define PRODUCT         Launch Test
+#define DESCRIPTION     Launch Keyboard (TEST)
+
+/* key matrix size */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 11
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { F0, F1 }
+#define MATRIX_COL_PINS { C6, B6, B5, B4, D7, D6, D4, D5, D3, D2, B7 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+#define RGB_DI_PIN E6
+#define RGBLED_NUM 22
+#define RGBLIGHT_ANIMATIONS
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#endif // CONFIG_H
---
+++ keyboards/system76/launch_test/launch_test.c
@@ -0,0 +1,10 @@
+
+#include "launch_test.h"
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
