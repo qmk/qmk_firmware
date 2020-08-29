c3a136ca6cd2e577fd4bb159c5c500020637e028
marksard <38324387+marksard@users.noreply.github.com>
2020-08-29 22:25:27 +0900

    Update keyboards/marksard/leftover30/keymaps/default/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/marksard/leftover30/keymaps/default/rules.mk
+++ keyboards/marksard/leftover30/keymaps/default/rules.mk
@@ -1,9 +1,3 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
-
-# If your custom treadstone32 pcb, you can rewrite to yes.
-RGBLIGHT_ENABLE = yes  # LED underglow (Enable WS2812 RGB underlight.)
-MOUSEKEY_ENABLE = yes       # Mouse keys
+RGBLIGHT_ENABLE = yes
+MOUSEKEY_ENABLE = yes
 ENCODER_ENABLE = yes
