1c4e84c784f9dc038eb6ac9107c06df9f0328301
jrdsgl <dayofdoom@users.noreply.github.com>
2020-08-29 13:53:10 -0700

    Update keyboards/sixkeyboard/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/sixkeyboard/keymaps/via/keymap.c
+++ keyboards/sixkeyboard/keymaps/via/keymap.c
@@ -3,22 +3,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_A, KC_B, KC_C, \
-    KC_D, KC_E, KC_F  \
+    KC_A, KC_B, KC_C,
+    KC_D, KC_E, KC_F
   ),
 
   [1] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS  \
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [2] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS  \
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [3] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS  \
+    KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
