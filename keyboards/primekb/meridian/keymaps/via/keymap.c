47b4d76ab1b9899fc481a647ba634db7bca68d46
holtenc <holtenc@primekb.com>
2020-08-28 23:00:52 -0500

    requested changes

--- keyboards/primekb/meridian/keymaps/via/keymap.c
+++ keyboards/primekb/meridian/keymaps/via/keymap.c
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -46,51 +47,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     	_______, _______, _______,          _______,          _______,          _______,                   _______, _______, _______
   )
 };
-
-
-void keyboard_pre_init_user(void){
-    setPinOutput(B12);
-}
-
-//Initialize all RGB indicators to 'off'
-void keyboard_post_init_user(void) {
-    rgblight_setrgb_at(0, 0, 0, 0); // [..., 0] = top LED
-    rgblight_setrgb_at(0, 0, 0, 1); // [..., 1] = middle LED
-    rgblight_setrgb_at(0, 0, 0, 2); // [..., 2] = bottom LED
-}
-
-//Indicator light function
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if (res) {
- //       writePin(B12, !led_state.caps_lock);  //Un-comment this line to enable in-switch capslock indicator
-    if (led_state.caps_lock) {
-        rgblight_setrgb_at(0, 255, 0, 0); //green
-    } else {
-        rgblight_setrgb_at(0, 0, 0, 0);
-    }
-    if (led_state.num_lock) {
-        rgblight_setrgb_at(0, 0, 255, 1); //blue
-    } else {
-        rgblight_setrgb_at(0, 0, 0, 1);
-    }
-   if (led_state.scroll_lock) {          
-        rgblight_setrgb_at(255, 0, 0, 2); //red
-    } else {
-        rgblight_setrgb_at(0, 0, 0, 2);
-    }
-}
-    return res;
-}
-
-//Below is an exmaple of layer indication using one of the RGB indicatiors. As configured, uses the bottom indicator (2) to light up red when layer 1 is in use. 
-/*
-layer_state_t layer_state_set_kb(layer_state_t state) {
-    if (get_highest_layer(state) == 1) {
-        rgblight_setrgb_at(255, 0, 0, 2);
-    } else {
-        rgblight_setrgb_at(0, 0, 0, 2);
-    }
-    return state;
-}
-*/
