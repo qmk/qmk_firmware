9f6100b73bc5762f963c9c2afd99edd028fd9d2f
marksard <38324387+marksard@users.noreply.github.com>
2020-08-29 13:09:58 +0900

    Update keyboards/marksard/leftover30/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/marksard/leftover30/keymaps/default/keymap.c
+++ keyboards/marksard/leftover30/keymaps/default/keymap.c
@@ -15,10 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
 
 enum layer_number {
     _BASE,
