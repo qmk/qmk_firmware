d5615bf8253a0279352d397716bc0db9c0f242cd
Naoto Takai <takai@recompile.net>
2020-08-30 14:32:36 +0900

    Update vendor and product info

--- keyboards/choco60/config.h
+++ keyboards/choco60/config.h
@@ -20,12 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xC0C0
-#define PRODUCT_ID      0x6000
+#define VENDOR_ID       0x524B // recompile keys
+#define PRODUCT_ID      0x4362 // Choco60
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Naoto Takai
-#define PRODUCT         choco60
-#define DESCRIPTION     A 60% split keyboard for programmers.
+#define MANUFACTURER    recompile keys
+#define PRODUCT         Choco60
+#define DESCRIPTION     recompile keys Choco60
 
 /* key matrix size */
 #define MATRIX_ROWS 10
