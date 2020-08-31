34ca851d63953e952be75050ec28e004fb91e367
Jim Sangwine <jim@sangwine.net>
2020-08-31 23:23:41 +0800

    Update dz60 split spacebar layout

--- keyboards/dz60/keymaps/EvilJimJafar/keymap.c
+++ keyboards/dz60/keymaps/EvilJimJafar/keymap.c
@@ -60,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
     LAYOUT_b_4_9(
         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,   KC_BSPC,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,   KC_BSLS,
+        LT(_NAV, KC_TAB),   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,   KC_BSLS,
         KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                KC_ENT,
-        KC_LSHIFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,RSFT_T(KC_SLSH),KC_UP,  MO(_RGB),
-        KC_CAPS,   KC_LALT, KC_LGUI,    LT(_CODING, KC_SPC),LT(_NAV, KC_SPC),KC_SPC,      TG(_NUM), KC_MPLY, KC_LEFT,    KC_DOWN,   KC_RGHT
+        KC_LSHIFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT,RSFT_T(KC_SLSH),KC_UP,  MO(_RGB),
+        KC_CAPS, TG(_NUM), KC_LALT,    MT(MOD_LGUI, KC_SPC),MO(_CODING),KC_SPC, KC_MPLY, KC_MFFD,   KC_LEFT,    KC_DOWN,   KC_RGHT
     ),
 
     /*
