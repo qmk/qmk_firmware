81392b46eb20a10b0f31544421f294c9ccb64a97
Jim Sangwine <jim@sangwine.net>
2020-08-31 23:28:52 +0800

    Update dz60 split spacebar layout

--- keyboards/dz60/keymaps/EvilJimJafar/keymap.c
+++ keyboards/dz60/keymaps/EvilJimJafar/keymap.c
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_NO,   KC_NO,   KC_NO,
           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PCMM, KC_NO,          KC_PENT,
           KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,     KC_P3, KC_PEQL, KC_NO,   KC_NO,
-          KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,            KC_P0,          KC_PDOT,     KC_NO, KC_NO,   KC_NO,   KC_NO
+          KC_NO,TG(_NUM),   KC_NO,            KC_NO,   KC_NO,            KC_P0,          KC_PDOT,     KC_NO, KC_NO,   KC_NO,   KC_NO
     ),
     
     /*
