8f566901df10fb017fe8cafe9588e3374c6275af
Sergey Vlasov <sigprof@gmail.com>
2020-09-01 01:19:13 +0300

    Remove matrix_col_t to allow MATRIX_ROWS > 32 (#10183)
    The matrix_col_t type was added in commit 0284431ad9 (part of #3449),
    but then the code which used that type was removed in #6140, and no
    other users were added since that time.  The presence of that type,
    however, limits MATRIX_ROWS to 32, which probably does not matter for a
    real keyboard, but prevents doing things like making a firmware to test
    all existing pins on a board like Teensy++ 2.0 (which has 46 GPIOs).

--- tmk_core/common/matrix.h
+++ tmk_core/common/matrix.h
@@ -30,16 +30,6 @@ typedef uint32_t matrix_row_t;
 #    error "MATRIX_COLS: invalid value"
 #endif
 
-#if (MATRIX_ROWS <= 8)
-typedef uint8_t matrix_col_t;
-#elif (MATRIX_ROWS <= 16)
-typedef uint16_t matrix_col_t;
-#elif (MATRIX_ROWS <= 32)
-typedef uint32_t matrix_col_t;
-#else
-#    error "MATRIX_ROWS: invalid value"
-#endif
-
 #define MATRIX_ROW_SHIFTER ((matrix_row_t)1)
 
 #define MATRIX_IS_ON(row, col) (matrix_get_row(row) && (1 << col))
