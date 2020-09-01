b6af13f207131cd3cce39b7763664fae83f455c3
Jan Kolkmeier <jankolkmeier@gmail.com>
2020-09-01 08:33:45 +0200

    Update keyboards/bemeier/bmek/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/bemeier/bmek/rules.mk
+++ keyboards/bemeier/bmek/rules.mk
@@ -1,23 +1,24 @@
+# MCU name
 MCU = atmega32u4
+
+# Bootloader selection
 BOOTLOADER = atmel-dfu
-BOOTMAGIC_ENABLE = lite
-MOUSEKEY_ENABLE = no
-EXTRAKEY_ENABLE = yes
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-SLEEP_LED_ENABLE = yes
-SPLIT_KEYBOARD = no
-BACKLIGHT_ENABLE = no
-NKRO_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-UNICODE_ENABLE = no
-UNICODEMAP_ENABLE = no
-BLUETOOTH_ENABLE = no
-AUDIO_ENABLE = no
-FAUXCLICKY_ENABLE = no
-DYNAMIC_MACRO_ENABLE = yes 
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE      = lite
+MOUSEKEY_ENABLE       = no
+EXTRAKEY_ENABLE       = yes
+CONSOLE_ENABLE        = no
+COMMAND_ENABLE        = no
+SLEEP_LED_ENABLE      = yes
+SPLIT_KEYBOARD        = no
+BACKLIGHT_ENABLE      = no
+NKRO_ENABLE           = yes
+RGBLIGHT_ENABLE       = yes
+AUDIO_ENABLE          = no
+DYNAMIC_MACRO_ENABLE  = no
+LTO_ENABLE            = yes
+
 DEFAULT_FOLDER = bemeier/bmek/rev3
-LEADER_ENABLE = no
-KEY_LOCK_ENABLE = no
-TERMINAL_ENABLE = no
-LINK_TIME_OPTIMIZATION_ENABLE = yes
