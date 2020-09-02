4086729f5e2823c484c2b31bb208c424e2be4c70
t-miyajima <55669223+dvorak55@users.noreply.github.com>
2020-09-02 15:29:17 +0900

    [Keyboard] Add Chavdai40% rev2 (#10210)
    * add chavdai40 rev2
    
    * add chavdai40 rev2
    
    * change include quantum.h to top of h files

--- keyboards/chavdai40/rev1/rev1.c
+++ keyboards/chavdai40/rev1/rev1.c
@@ -13,4 +13,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "chavdai40.h"
+#include "rev1.h"
