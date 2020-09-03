4fd02991943e07f0c46b5c2847f0630e4cb387d5
Max Drasbeck <max@drasbeck.dk>
2020-09-03 11:25:36 +0200

    added keymap for preonic rev3

---
+++ keyboards/preonic/keymaps/drasbeck/rules.mk
@@ -0,0 +1,10 @@
+# Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) 2020 Max Drasbeck 
+#
+# You are free to:
+#
+#   Share — copy and redistribute the material in any medium or format
+#   Adapt — remix, transform, and build upon the material for any purpose, even commercially.
+#
+# The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+SRC += muse.c
