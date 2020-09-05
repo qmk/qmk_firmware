bccc10e407300503e88fbb67a9924a562f5a74e8
Hardy Miranda <hardy@hardymiranda.com>
2020-09-05 01:05:06 -0300

    Update quefrency layout naming

--- keyboards/keebio/quefrency/keymaps/hdcore/keymap.c
+++ keyboards/keebio/quefrency/keymaps/hdcore/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MPRV, KC_MNXT, KC_TAB,         KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_END, \
     KC_MSTP, KC_MPLY, LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
     KC_BRID, KC_BRIU, KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_MUTE, KC_MUTE, KC_LCTL,        KC_LGUI, KC_LALT, KC_SPC, MO(_FN1),         MO(_FN1),KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_MUTE, KC_MUTE, KC_LCTL,        KC_LGUI, KC_LALT, KC_SPC, MO(_FN1),         KC_ENT,  KC_RALT, MO(_FN1),KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
@@ -30,3 +30,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F9, KC_F10, KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
+
