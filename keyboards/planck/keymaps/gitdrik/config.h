3454037deacf1c22a0ef8ec4d78535bc4beb4b06
gitdrik <oskarson@gmail.com>
2020-09-06 20:06:38 +0200

    Modified comments and added copyright header.

--- keyboards/planck/keymaps/gitdrik/config.h
+++ keyboards/planck/keymaps/gitdrik/config.h
@@ -1,3 +1,21 @@
+/* Original code probably copyright 2015-2017 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Passed along from planck default to Finnish SFS 5966 by gitdrik, 2020. */
+
 #pragma once
 
 #ifdef AUDIO_ENABLE
@@ -36,4 +54,3 @@
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
-
