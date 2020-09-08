713c1a92eb3e005d347a897b853adb8002ee50ef
BenRoe <mail@benjaminroesner.com>
2020-09-08 19:10:36 +0200

    add rev for Lily58L pcb

---
+++ keyboards/lily58/revlight/rules.mk
@@ -0,0 +1,4 @@
+OLED_DRIVER_ENABLE = yes   			# Enables the use of OLED displays
+ENCODER_ENABLE = yes       			# ENables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      			# Enable keyboard RGB light
+LINK_TIME_OPTIMIZATION_ENABLE = yes # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
