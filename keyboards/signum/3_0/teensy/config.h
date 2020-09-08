32356b1c25577182fb6c37d346c9ff8b31cdc52c
Stan Gurenkov <stan.gurenkov@docusign.com>
2020-09-07 22:45:43 -0700

    new keyboard layout for signum with teensy

--- keyboards/signum/3_0/teensy/config.h
+++ keyboards/signum/3_0/teensy/config.h
@@ -56,3 +56,7 @@
 #endif
 
 #endif
+
+#define LEADER_TIMEOUT 300
+#define LEADER_PER_KEY_TIMING 300
+
--- keyboards/signum/3_0/teensy/rules.mk
+++ keyboards/signum/3_0/teensy/rules.mk
@@ -54,3 +54,4 @@ NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https:
 BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
 AUDIO_ENABLE ?= no
 RGBLIGHT_ENABLE ?= no
+LEADER_ENABLE = yes
