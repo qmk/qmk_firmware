3e6528cb570e3a28ccbf9ba7b1346415a49333e7
Ramon Imbao <ramonimbao@gmail.com>
2020-09-08 17:34:05 +0800

    Fix incorrect ISO layout

--- keyboards/ramonimbao/squishy65/config.h
+++ keyboards/ramonimbao/squishy65/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xB16B
-#define PRODUCT_ID      0x10B4
+#define PRODUCT_ID      0x10B5
 #define DEVICE_VER      0x0065
 #define MANUFACTURER Ramon Imbao
 #define PRODUCT Squishy65
