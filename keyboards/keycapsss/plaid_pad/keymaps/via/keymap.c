aab5aa688e2252ed7b06a8c03b3428ad86a3031d
BenRoe <mail@benjaminroesner.com>
2020-09-08 17:22:03 +0200

    add license to header of *.h and *.c files

--- keyboards/keycapsss/plaid_pad/keymaps/via/keymap.c
+++ keyboards/keycapsss/plaid_pad/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 KEYCAPSSS
+/* Copyright 2020 Ben Roesner (keycapsss.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
