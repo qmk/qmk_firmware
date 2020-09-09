3f003844bd2bc5966240316d065eca26a282485d
Keith Wade <keawade@protonmail.com>
2020-09-08 20:56:47 -0500

    Implement layers

--- keyboards/keaboard/keymaps/default/keymap.c
+++ keyboards/keaboard/keymaps/default/keymap.c
@@ -4,12 +4,6 @@
 #define _LOWER 1
 #define _RAISE 2
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
@@ -20,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F2,            KC_F12,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_DEL,  KC_LALT, KC_LCTL, KC_LGUI, LOWER,            KC_ENT,           KC_SPC,           RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+     KC_DEL,  KC_LALT, KC_LCTL, KC_LGUI, MO(_LOWER),            KC_ENT,           KC_SPC,           MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
   ),
 
