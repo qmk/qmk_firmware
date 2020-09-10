8500c1d4971869cfb9f538dcb0937b240e9fae09
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2020-09-10 16:42:54 +0900

    add tester keymaps for yushakobo/quick7

---
+++ keyboards/yushakobo/quick7/keymaps/tester/config.h
@@ -0,0 +1,10 @@
+#undef RGBLIGHT_LIMIT_VAL
+#define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_MAX_LAYERS 4
+#define RGBLIGHT_LED_MAP {\
+    0, 1, 2,\
+    5, 4, 3,\
+    6, 7, 8,\
+    9, 10, 11, 12\
+}
