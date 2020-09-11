1f2d3e9ef7f65c8475c031ea6ea325f403bbcbd7
DigiD3ar <jmworking92@gmail.com>
2020-09-10 19:35:17 -0500

    Update keyboards/keeblounge/concord45/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/keeblounge/concord45/config.h
+++ keyboards/keeblounge/concord45/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4B4C //KL
-#define PRODUCT_ID      0x4334 //K3
+#define PRODUCT_ID      0x4334 // C4
 #define DEVICE_VER      0x0001 //0001
 #define MANUFACTURER    Keeblounge
 #define PRODUCT         Concord45
