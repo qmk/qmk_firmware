51240b0f477c0c7f5ea9df31d78d9cec20a4ad2b
4pplet <4pplet@protonmail.com>
2020-09-11 15:25:29 +0200

    Update keyboards/masterworks/classy_tkl/rev_a/rules.mk
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/masterworks/classy_tkl/rev_a/rules.mk
+++ keyboards/masterworks/classy_tkl/rev_a/rules.mk
@@ -20,8 +20,8 @@ NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 
 # Supported layouts
 LAYOUTS = tkl_ansi_wkl tkl_iso_wkl
