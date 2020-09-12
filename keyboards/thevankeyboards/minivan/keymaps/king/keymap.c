835f044e8f6206b0f2c54319d18cae97bd09d320
kingwangwong <kingngngco@gmail.com>
2020-09-12 00:06:45 -0500

    KBD67 keymap
    Description
    Add a new keymap for kbd67mkii
    
    Types of Changes
     Core
     Bugfix
     New feature
     Enhancement/optimization
     Keyboard (addition or update)
     [X]Keymap/layout/userspace (addition or update)
     Documentation
    Issues Fixed or Closed by This PR
    Checklist
     [x]My code follows the code style of this project: C, Python
     [x] I have read the PR Checklist document and have made the appropriate changes.
     [x] My change requires a change to the documentation.
     [x] I have updated the documentation accordingly.
      [x]I have read the CONTRIBUTING document.
      [x]I have added tests to cover my changes.
      [x]I have tested the changes and verified that they work and don't break anything (as well as I can manage).

--- keyboards/thevankeyboards/minivan/keymaps/king/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/king/keymap.c
@@ -17,17 +17,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────────┐
    * │ ESC │  Q  │  W  │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │BKSPC    │  
    * ├─────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────────┤
-   * │ TAB  │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │ ;   │'       │
+   * │ TAB  │  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │ ;   │ENTER   │
    * ├──────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬─────┤
-   * │ LSHFT   │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │ENTER│
+   * │ LSHFT   │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │'    │
    * ├────────┬┴─────┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
-   * │LCTRL   │   LGUI │ LALT│SPACE(_NL)  │MO(_NL)         │MO SL│     │MO(RL    │
+   * │LCTRL   │   LGUI │ LALT│SPACE(_NL)  │MO(_NL)         │MO_SL│     │MO(RL    │
    * └────────┴────────┴─────┴────────────┴────────────────┴─────┴─────┴─────────┘
    */
     [_ML] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,     KC_BSPC,  
-        KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,  KC_QUOT,         
-        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,  KC_ENT,     
+        KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,  KC_ENT,         
+        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,  KC_QUOT,     
         KC_LCTL, KC_LGUI, KC_LALT,         SPC_FUN,        MO(_NL),        MO(_SL), XXXXXXX,  MO(_RL)
     ),
 
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────────┐
    * │     │     │HOME │ UP  │END  │PGUP │     │ F1  │ F2  │ F3  │ F4  │         │
    * ├─────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────────┤
-   * │ CAPS │     │LEFT │DOWN │RIGHT│PGDN │     │ F5  │ F6  │ F7  │ F8  │        │
+   * │ CAPS │     │LEFT │DOWN │RIGHT│PGDN │     │ F5  │ F6  │ F7  │ F8  │ QUOT   │
    * ├──────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬─────┤
    * │         │     │     │ DEL │ INS │     │     │ F9  │ F10 │ F11 │ F12 │     │
    * ├────────┬┴─────┴─┬───┴─┬───┴─────┴──┬──┴─────┴─────┴─┬───┴─┬───┴─┬───┴─────┤
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [_FL] = LAYOUT(
         _______,   XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX, KC_F1, KC_F2,   KC_F3,    KC_F4,   _______,  
-        KC_CAPS,   XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_F5, KC_F6,   KC_F7,    KC_F8,   _______,        
+        KC_CAPS,   XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_F5, KC_F6,   KC_F7,    KC_F8,   KC_QUOT,        
         _______,   XXXXXXX, XXXXXXX, KC_DEL,  KC_INS,  XXXXXXX, XXXXXXX, KC_F9, KC_F10,  KC_F11,   KC_F12,  _______,        
         _______,   _______,  _______,         _______,          _______,        _______, _______,  _______
     ),
