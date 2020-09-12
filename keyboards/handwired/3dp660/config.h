7afd44ef0c6f39040a81d88aa7700f17893d7e77
gooberpsycho <64438496+gooberpsycho@users.noreply.github.com>
2020-09-11 21:17:58 -0400

    change product ID to fix ID conflicts.

--- keyboards/handwired/3dp660/config.h
+++ keyboards/handwired/3dp660/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6068
+#define PRODUCT_ID      0x6075
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Handwired
 #define PRODUCT         3DP660
