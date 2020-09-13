e615d4f33329fe383185b21bc3c9e8f0e0dbf81d
Phil Pirozhkov <hello@fili.pp.ru>
2020-09-14 00:40:21 +0300

    Prettify keymap

--- keyboards/gboards/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/default/keymap.c
@@ -1,75 +1,113 @@
 /* Good on you for modifying your layout! if you don't have
  * time to read the QMK docs, a list of keycodes can be found at
- *
  * https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes.md
- *
- * There's also a template for adding new layers at the bottom of this file!
  */
 
 #include QMK_KEYBOARD_H
 #include "g/keymap_combo.h"
 
-#define BASE 0  // default layer
-#define SYMB 1  // symbols
-#define NUMB 2  // numbers/motion
+#define _ALPHA   0  // default
+#define _SPECIAL 1  // special characters
+#define _NUMBERS 2  // numbers/function/motion
 
-/* Combomap
- *
- * ,-----------------------------.       ,--------------------------------.
- * |      |    ESC    |     |     |      |     |    ESC    |    BSLH      |
- * |-----+-----+-----+-----+------|      |--------------------------------|
- * |      |   BSPC   ENT    |     |      |    LES   COLN  GRT    |        |
- * |-----+-----+-----+--RMB+-LMB--+		   |--------------------------------|
- * |      |   MINS    |     |     |      |    QUO   UNDR   |     |        |
- * `------+-----+-----+------+----'		   `--------------------------------'
- *  .-------------------------.           .-----------------.
- *  |        |       |        |           |        |    |   |
- *  '-------------------------'           '-----------------'
- */
+#define KC_CTL_A  MT(MOD_LCTL, KC_A)     // Tap for A, hold for Control
+#define KC_CTL_CL MT(MOD_LCTL, KC_SCLN)  // Tap for colon, hold for Control
+#define KC_SFT_Z  MT(MOD_RSFT, KC_Z)     // Tap for Z, hold for Shift
+#define KC_SFT_SL MT(MOD_RSFT, KC_SLSH)  // Tap for slash, hold for Shift
 
-// Blank template at the bottom
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Keymap 0: Basic layer
+#define KC_GUI_ESC MT(MOD_LGUI, KC_ESC)  // Tap for Esc, hold for GUI (Meta, Command, Win)
+#define KC_ALT_ENT MT(MOD_LALT, KC_ENT)  // Tap for Enter, hold for Alt (Option)
+#define KC_SYM_SPC LT(SYMB, KC_SPC)      // Tap for Space, hold for Symbol layer
+#define KC_NUM_SPC LT(NUMB, KC_SPC)      // Tap for Space, hold for Numbers layer
+#define KC_SFT_TAB MT(MOD_RSFT, KC_TAB)  // Tap for Tab, hold for Right Shift
+
+    /* Combomap
      *
-     * ,-----------------------------.       ,--------------------------------.
-     * |    Q |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |  O  |    P   |
-     * |-----+-----+-----+-----+------|      |--------------------------------|
-     * |CTRL/A|  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |  L  | CTRL/; |
-     * |-----+-----+-----+-----+------+		   |--------------------------------|
-     * |SHFT/Z|  X  |  C  |  V  |  B  |      |  N  |  M  |  <  |  >  | SHFT/? |
-     * `------+-----+-----+------+----'		   `--------------------------------'
-     *  .-------------------------.           .-----------------.
-     *  |ESC/META|ENT/ALT|SPC(SYM)|           |SPC(NUM)|BSPC|TAB|
-     *  '-------------------------'           '-----------------'
+     * ,-------------------------------.      ,-------------------------------.
+     * |       |    ESC    |     |     |      |     |    ESC    |    BSLH     |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |       |   BSPC   ENT    |     |      |    LES   COLN  GRT    |       |
+     * |-------+-----+-----+-RMB-+-LMB-|      |-----+-----+-----+-----+-------|
+     * |       |   MINS    |     |     |      |    QUO   UNDR   |     |       |
+     * `-------------------------------'      `-------------------------------'
+     *            .-----------------.            .-----------------.
+     *            |     |     |     |            |     |     |     |
+     *            '-----------------'            '-----------------'
      */
-    [BASE] = LAYOUT_gergoplex(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, MT(MOD_LCTL, KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, MT(MOD_LCTL, KC_SCLN), MT(MOD_RSFT, KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT, KC_SLSH),
 
-                              MT(MOD_LGUI, KC_ESC), MT(MOD_LALT, KC_ENT), LT(SYMB, KC_SPC),  // Left
-                              LT(NUMB, KC_SPC), KC_LSFT, MT(MOD_RSFT, KC_TAB)                // Right
-                              ),
-    /* Keymap 1: Symbols layer
-     * ,-----------------------------.       ,--------------------------------.
-     * |  !   |  @  |  {  |  }  |       |  `  |  ~  |     |     |    \   |
-     * |-----+-----+-----+-----+------|      |--------------------------------|
-     * |  #   |  $  |  (  |  )  | LMB |      |  +  |  -  |  /  |  *  |    '   |
-     * |-----+-----+-----+-----+------+		   |--------------------------------|
-     * |  %   |  ^  |  [  |  ]  | RMB |      |  &  |  =  |  ,  |  .  |   -    |
-     * `------+-----+-----+------+----'		   `--------------------------------'
-     *  				.-----------------.           .------------------.
-     *  				|MMB |  ;  	|	 = 	|						|  =  |  ;  |  DEL |
-     *  				'-----------------'           '------------------'
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Keymap 0: Alpha layer
+     *
+     * ,-------------------------------.      ,-------------------------------.
+     * |     Q |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |  O  |   P   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * | CTRL A|  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |  L  |CTRL ; |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * | SHFT Z|  X  |  C  |  V  |  B  |      |  N  |  M  |  <  |  >  |SHFT / |
+     * `-------------------------------'      `-------------------------------'
+     *   .------------------------------.    .----------------------.
+     *   | ESC META | ENT ALT | SPC SYM |    | SPC NUM | SHFT | TAB |
+     *   '------------------------------'    '----------------------'
      */
-    [SYMB] = LAYOUT_gergoplex(KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV, KC_TILD, KC_TRNS, KC_TRNS, KC_BSLS, KC_HASH, KC_DLR, KC_LPRN, KC_RPRN, KC_BTN2, KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1, KC_AMPR, KC_EQL, KC_COMM, KC_DOT, KC_MINS, CMB_TOG, KC_SCLN, KC_EQL, KC_EQL, KC_SCLN, KC_DEL),
-    /* Keymap 2: Pad/Function layer
-     * ,-----------------------------.       ,-------------------------------.
-     * |  1   |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |   0   |
-     * |-----+-----+-----+-----+------|      |-------------------------------|
-     * |  F1  | F2  | F3  | F4  |  F5 |      | LFT | DWN | UP  | RGT | VOLUP |
-     * |-----+-----+-----+-----+------+		   |-------------------------------|
-     * |  F6  | F7  | F8  | F9  | F10 |      |MLFT | MDWN| MUP | MRGT| VOLDN |
-     * `------+-----+-----+------+----'		   `-------------------------------'
-     *  				.-----------------.           .-----------------.
-     *  				| F11 | F12|	  	|						|     | PLY | SKP |
-     *  				'-----------------'           '-----------------'
+    [_ALPHA] = LAYOUT_kc(
+    // ,-------------------------------.      ,-------------------------------.
+           Q   ,  W  ,  E  ,  R  ,  T  ,         Y  ,  U  ,  I  ,  O  ,   P   ,
+    // |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+         CTL_A ,  S  ,  D  ,  F  ,  G  ,         H  ,  J  ,  K  ,  L  , CTL_CL,
+    // |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+         SFT_Z ,  X  ,  C  ,  V  ,  B  ,         N  ,  M  ,COMMA, DOT , SFT_SL,
+    // '-------------------------------'      '-------------------------------'
+    //    .---------------------------.        .------------------------.
+            GUI_ESC, ALT_ENT, SYM_SPC ,          NUM_SPC, LSFT, SFT_TAB ),
+    //    '---------------------------'        '------------------------'
+
+    /* Keymap 1: Special characters layer
+     *
+     * ,-------------------------------.      ,-------------------------------.
+     * |    !  |  @  |  {  |  }  |  |  |      |  `  |  ~  |     |     |   \   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |    #  |  $  |  (  |  )  | RMB |      |  +  |  -  |  /  |  *  |   '   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |    %  |  ^  |  [  |  ]  | LMB |      |  &  |  =  |  ,  |  .  |   -   |
+     * `-------------------------------'      `-------------------------------'
+     *     .-------------------------.          .-----------------.
+     *     | ComboToggle |  ;  |  =  |          |  =  |  ;  | DEL |
+     *     '-------------------------'          '-----------------'
      */
-    [NUMB] = LAYOUT_gergoplex(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_VOLU, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_MPLY, KC_MNXT)};
+    [_SPECIAL] = LAYOUT_kc(
+    // ,-------------------------------.      ,-------------------------------.
+         EXLM  , AT  , LCBR, RCBR, PIPE,        GRV , TILD, TRNS, TRNS, BSLS  ,
+    // |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+         HASH  , DLR , LPRN, RPRN, BTN2,        PLUS, MINS, SLSH, ASTR, QUOT  ,
+    // |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+         PERC  , CIRC, LBRC, RBRC, BTN1,        AMPR, EQL , COMM, DOT , MINS  ,
+    // '-------------------------------'      '-------------------------------'
+    //    .---------------------------.        .------------------------.
+            CMB_TOG, SCLN    , EQL    ,          EQL    , SCLN   , DEL  ),
+    //    '---------------------------'        '------------------------'
+
+    /* Keymap 2: Numbers/Function/Motion layer
+     *
+     * ,-------------------------------.      ,-------------------------------.
+     * |   1   |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |   0   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |  F1   | F2  | F3  | F4  | F5  |      | LFT | DWN | UP  | RGT | VOLUP |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |  F6   | F7  | F8  | F9  | F10 |      | MLFT| MDWN| MUP | MRGT| VOLDN |
+     * `-------------------------------'      `-------------------------------'
+     *             .-----------------.          .-----------------.
+     *             | F11 | F12 |     |          |     | PLY | SKP |
+     *             '-----------------'          '-----------------'
+     */
+    [_NUMBERS] = LAYOUT_kc(
+    // ,-------------------------------.      ,-------------------------------.
+           1   ,  2  ,  3  ,  4  ,  5  ,         6  ,  7  ,  8  ,  9  ,   0   ,
+    // |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+           F1  ,  F2 ,  F3 ,  F4 , F5  ,        LEFT, DOWN,  UP , RGHT,  VOLU ,
+    // |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+           F6  ,  F7 ,  F8 ,  F9 , F10 ,        MS_L, MS_D, MS_U, MS_R,  VOLD ,
+    // '-------------------------------'      '-------------------------------'
+    //    .---------------------------.        .------------------------.
+              F11  ,   F12   , TRNS   ,           TRNS  ,  MPLY ,  MNXT )
+    //    '---------------------------'        '------------------------'
+};
