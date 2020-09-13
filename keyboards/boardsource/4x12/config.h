90e2b89abafb83ff2a24a12a43cda920e70471dc
Adam Gwilliam <agwilliam@yaharasoftware.com>
2020-09-12 21:48:46 -0500

    update vendor id of 4x12 to match 3x4 and 5x12 for boardsource

--- keyboards/boardsource/4x12/config.h
+++ keyboards/boardsource/4x12/config.h
@@ -3,7 +3,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xF7E0
+#define VENDOR_ID       0x4273 // "Bs" - Boardsource
 #define PRODUCT_ID      0x0412
 #define DEVICE_VER      0x0000
 #define MANUFACTURER    Boardsource
