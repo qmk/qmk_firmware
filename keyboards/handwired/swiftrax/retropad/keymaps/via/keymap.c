b010800b55a4b6db29d8d4f9e797e3b81b073417
Swiftrax <swiftrax@gmail.com>
2020-09-13 13:05:13 -0700

    fix indicator leds

--- keyboards/handwired/swiftrax/retropad/keymaps/via/keymap.c
+++ keyboards/handwired/swiftrax/retropad/keymaps/via/keymap.c
@@ -11,6 +11,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MUTE,
   KC_LEFT, KC_RGHT,
   KC_UP,   KC_UP),
+
+[2] = LAYOUT(
+  KC_MUTE,
+  KC_LEFT, KC_RGHT,
+  KC_UP,   KC_UP),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
@@ -36,8 +41,16 @@ void matrix_init_user(void) {
 // write to above indicators in a binary fashion based on current layer
 layer_state_t layer_state_set_user(layer_state_t state)
 {
-    writePin(D5, (state & 0x1));
-    writePin(D4, (state & 0x2));
-    writePin(D3, (state & 0x4));
+  switch (get_highest_layer(state)){
+    case 0:
+      writePin(D5, true);
+      break;
+    case 1:
+      writePin(D4, true);
+      break;
+    case 2:
+      writePin(D3, true);
+      break;
+  }
     return state;
 }
