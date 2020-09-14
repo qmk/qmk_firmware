fd28416cf788a625c786b38209dee6f4048854fa
Baris Tosun <me@baristosun.com>
2020-09-14 10:08:21 +0200

    Update keyboards/rominronin/katana60_rev2/rules.mk
    removed redundant code
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/rominronin/katana60_rev2/rules.mk
+++ keyboards/rominronin/katana60_rev2/rules.mk
@@ -27,4 +27,3 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
