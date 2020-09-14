9f9f9859485fca39a6f9bad6c9574bfdf1a5508f
Baris Tosun <me@baristosun.com>
2020-09-14 10:01:32 +0200

    Update keyboards/rominronin/katana60_rev2/rules.mk
    removed 'FAUXCLICKY_ENABLE' line
    
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/rominronin/katana60_rev2/rules.mk
+++ keyboards/rominronin/katana60_rev2/rules.mk
@@ -36,5 +36,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
