a8312a04b899d4d0a507cd63a794fc3cd522d6c8
Croktopus <39040552+Croktopus@users.noreply.github.com>
2020-09-14 21:21:39 -0700

    moved encoder code to timberwolf.c

--- keyboards/metamechs/timberwolf/timberwolf.c
+++ keyboards/metamechs/timberwolf/timberwolf.c
@@ -27,3 +27,11 @@ bool led_update_kb(led_t led_state) {
     }
     return runDefault;
 }
+
+void encoder_update_kb(uint8_t index, bool clockwise) {
+	if (clockwise) {
+		tap_code(KC_VOLU);
+	} else {
+		tap_code(KC_VOLD);
+	}
+}
