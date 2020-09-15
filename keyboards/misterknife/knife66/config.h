a636405a3985604e1d71c880b4166a5c5893030b
Thierry Michel Philippe Kleist <thierry@kle.ist>
2020-09-15 06:15:10 +0200

    feat: added support for ws2812

--- keyboards/misterknife/knife66/config.h
+++ keyboards/misterknife/knife66/config.h
@@ -47,3 +47,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE    5
+
+#define WS2812_PWM_DRIVER PWMD1  // default: PWMD1
+#define WS2812_PWM_CHANNEL 2  // default: 2
+#define WS2812_PWM_PAL_MODE 2  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 2
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM5  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#define WS2812_DMA_CHANNEL 5  // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
