57b3eab369e2e1fa1a0b5b0e165fac7d0de36b7f
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-09-15 03:28:41 -0700

    assign unique Vendor/Product ID pairings

--- keyboards/rominronin/katana60/rev2/config.h
+++ keyboards/rominronin/katana60/rev2/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
-#define PRODUCT_ID 0x0000
+#define VENDOR_ID 0x7272  // "rr" - rominronin
+#define PRODUCT_ID 0xF03B
 #define DEVICE_VER 0x0001
 #define MANUFACTURER RominRonin CandyKeys
 #define PRODUCT Katana60 rev2
