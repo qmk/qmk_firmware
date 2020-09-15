340992f249699e08a9641d0e611208879bd15c0b
DisguisedOtter <github@patatje.eu>
2020-09-15 09:14:03 +0200

    spacing

--- keyboards/yd60/config.h
+++ keyboards/yd60/config.h
@@ -3,8 +3,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x258A
-#define PRODUCT_ID      0x0033
+#define VENDOR_ID	0x258A
+#define PRODUCT_ID	0x0033
 #define DEVICE_VER	0x0001
 #define MANUFACTURER    YMDK
 #define PRODUCT         YD60
