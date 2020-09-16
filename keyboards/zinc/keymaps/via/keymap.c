101bde5d5f1aec9848c7dfc79331f449eaa6fb23
Monksoffunk <monksoffunk@users.noreply.github.com>
2020-09-16 20:43:58 +0900

    Update keyboards/zinc/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/zinc/keymaps/via/keymap.c
+++ keyboards/zinc/keymaps/via/keymap.c
@@ -159,7 +159,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_LANG1);
             }
             return false;
-            break;
         case RGBRST:
 #ifdef RGBLIGHT_ENABLE
             if (record->event.pressed) {
