cff9a1320c1b7e7ee90753d19ca2d58442e3beaf
winterNebs <winterNebs@gmail.com>
2020-09-16 13:00:50 -0400

    Add kyria keymap

---
+++ keyboards/kyria/keymaps/winterNebs/config.h
@@ -0,0 +1,34 @@
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#define OLED_FONT_H "keyboards/kyria/keymaps/winterNebs/glcdfont.c"
+#define OLED_FONT_END 127
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+#endif
+
+#define FORCE_NKRO
+#define ENCODER_RESOLUTION 4
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT
+#define TAPPING_FORCE_HOLD
+//#define NO_ACTION_TAPPING
+// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
+// #define SPLIT_USB_DETECT
+// #define NO_USB_STARTUP_CHECK
---
+++ keyboards/kyria/keymaps/winterNebs/rules.mk
@@ -0,0 +1,13 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more 
+NKRO_ENABLE = yes
+WPM_ENABLE = yes
+EXTRAFLAGS += -flto
+CONSOLE_ENABLE = no     # Console for debug(+400)
+AUDIO_ENABLE   = no     # Audio output on port C6
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+SPACE_CADET_ENABLE = no
+EXTRAKEY_ENABLE = no
+MOUSEKEY_ENABLE = no
+BOOTMAGIC_ENABLE = no
+SWAP_HANDS_ENABLE = yes
