cdd5a728c425a292f9fbcc321c34c3d434dca74a
Nick Brown <nickbrown.dev@gmail.com>
2020-09-16 09:41:26 +0100

    Initial version of Kobrakai firmware

---
+++ keyboards/kyria/keymaps/kobrakai/rules.mk
@@ -0,0 +1,5 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+MOUSEKEY_ENABLE = yes      # Enable mouse keys
