3da94111b31b791d59c3a3b6365080a74e3b4bfe
Khader Syed <khader.syed@aicure.com>
2020-09-17 15:16:04 -0400

    add new map

---
+++ keyboards/handwired/ferris/keymaps/madhatter/keymap.c
@@ -0,0 +1,53 @@
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+enum my_layers {
+  _QWERTY,
+  _RAISE,
+  _LOWER
+};
+
+#define KC_CTSL RCTL_T(KC_SCLN)
+#define KC_CTES CTL_T(KC_ESC)
+#define KC_CTEN RCTL_T(KC_ENT)
+#define KC_CTSC RCTL_T(KC_SCLN)
+#define KC_LSBS SFT_T(KC_BSLS)
+#define KC_LSHZ SFT_T(KC_Z)
+#define KC_SHSL RSFT_T(KC_SLSH)
+#define KC_SPM1 LT(1, KC_SPC)
+#define KC_BSM1 LT(1, KC_BSPC)
+#define KC_GUEN GUI_T(KC_ENT)
+#define KC_ALTA ALGR_T(KC_TAB)
+#define KC_BSM2 LT(2, KC_BSPC)
+#define KC_CLGV CTL_T(KC_GRV)
+#define KC_CLBS CTL_T(KC_BSLS)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT( /* Qwerty */
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
+    KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,
+                                    KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT
+  ),
+
+  [_RAISE] = LAYOUT( /* [> RAISE <] */
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS  
+    ),
+
+  [_LOWER] = LAYOUT( /* [> LOWER <] */
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,             KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   RESET,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS
+    )
+};
