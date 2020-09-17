cb3b781c97193840c41935eca5589fac9c0e29c1
rado <radoosredkar@hotmail.com>
2020-09-17 17:44:58 +0200

    Working loghtning and typing

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = caterina
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
