510f27a67c05dd7eb9ef9330f7e63672852fc6fc
rado <radoosredkar@hotmail.com>
2020-09-17 17:44:58 +0200

    Working loghtning and typing

---
+++ keyboards/crkbd/keymaps/rado-corne/rules.mk
@@ -0,0 +1,10 @@
+RGBLIGHT_ENABLE = yes
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/glcdfont.c \
+        ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
