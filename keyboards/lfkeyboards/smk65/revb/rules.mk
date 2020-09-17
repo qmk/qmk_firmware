b8074ae0ddcdfd64577bf4d811a306314cc0a679
zvecr <git@zvecr.com>
2020-09-17 23:12:45 +0100

    Move existing files out the way

---
+++ keyboards/lfkeyboards/smk65/revb/rules.mk
@@ -0,0 +1,15 @@
+# MCU name
+MCU = at90usb646
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = atmel-dfu
+
+# Extra source files for IS3731 lighting
+SRC = TWIlib.c issi.c lighting.c
