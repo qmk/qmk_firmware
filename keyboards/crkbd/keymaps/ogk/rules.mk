5dcac2c85323f2db4b15017ab806950bed16f7fe
theflyingtako <heyitzgary@gmail.com>
2020-09-16 23:05:40 -0700

    test crkbd/ogk

---
+++ keyboards/crkbd/keymaps/ogk/rules.mk
@@ -0,0 +1,9 @@
+# required for soft reset to work
+BOOTLOADER = atmel-dfu
+
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = WS2812
+MOUSEKEY_ENABLE    = no
+NKRO_ENABLE        = yes
+OLED_DRIVER_ENABLE = yes
+
