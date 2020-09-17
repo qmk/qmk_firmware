e4f4e4cfefee142c318e62e1ddb2c82e628212d5
Swiftrax <swiftrax@gmail.com>
2020-09-16 18:46:39 -0700

    fix keymap

--- keyboards/handwired/swiftrax/retropad/keymaps/default/keymap.c
+++ keyboards/handwired/swiftrax/retropad/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [2] = LAYOUT(
   TO(0),
   KC_HOME, KC_PGUP,
-  KC_END,   KC_PGDN),
+  KC_END,  KC_PGDN),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
--- keyboards/handwired/swiftrax/retropad/keymaps/via/keymap.c
+++ keyboards/handwired/swiftrax/retropad/keymaps/via/keymap.c
@@ -9,13 +9,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [1] = LAYOUT( //macOS
   TO(2),
-  KC_MSTP, MC_MPLY,
+  KC_MSTP, KC_MPLY,
   KC_MRWD, KC_MFFD),
 
 [2] = LAYOUT(
   TO(0),
   KC_HOME, KC_PGUP,
-  KC_END,   KC_PGDN),
+  KC_END,  KC_PGDN),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
