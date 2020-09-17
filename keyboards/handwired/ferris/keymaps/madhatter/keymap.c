29a0b76116162ccdefa6a52b75eaec9458652e63
Khader Syed <khader.syed@aicure.com>
2020-09-17 19:41:58 -0400

    update map

--- keyboards/handwired/ferris/keymaps/madhatter/keymap.c
+++ keyboards/handwired/ferris/keymaps/madhatter/keymap.c
@@ -22,33 +22,34 @@ enum ilpse_keycodes {
   ADJUST
 };
 
-#define KC_CTBS CTL_T(KC_BSPC)
+#define KC_CTLA CTL_T(KC_A)
+#define KC_ALBS ALGR_T(KC_BSPC)
 #define KC_LSHZ SFT_T(KC_Z)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT( /* Qwerty */
+  [_QWERTY] = LAYOUT( /* QWERTY */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
     KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,
-                                      LOWER, KC_CTBS, KC_SPC,  RAISE
+                                      LOWER, KC_ALBS, KC_SPC,  RAISE
   ),
 
-  [_LOWER] = LAYOUT( /* [> RAISE <] */
+  [_LOWER] = LAYOUT( /* [> LOWER <] */
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  
     ),
 
-  [_RAISE] = LAYOUT( /* [> LOWER <] */
+  [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
     KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, KC_TRNS,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
-  [_ADJUST] = LAYOUT( /* [> LOWER <] */
+  [_ADJUST] = LAYOUT( /* [> ADJUST <] */
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
