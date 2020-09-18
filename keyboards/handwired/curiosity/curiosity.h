95b0db531de36a00f0942bd90af9d6bcd9ab060e
Spaceman <rionlion100@gmail.com>
2020-09-18 18:43:39 -0400

    Add default keymap

--- keyboards/handwired/curiosity/curiosity.h
+++ keyboards/handwired/curiosity/curiosity.h
@@ -15,8 +15,8 @@
  */
 #pragma once
 
-
 #include "quantum.h"
+
 #define SMALL_MODS( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b,   k3b, \
      k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b,  k3a, \
