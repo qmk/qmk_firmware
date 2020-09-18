65d2531442edd1abda7be977f8546b0a15bae33f
muuko <muukoa@gmail.com>
2020-09-18 12:19:40 -0400

    lily58 layout with working bongo cat timeout

--- keyboards/lily58/keymaps/muuko/keymap.c
+++ keyboards/lily58/keymaps/muuko/keymap.c
@@ -26,8 +26,18 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, LCA_T(KC_MINS), KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, RGUI_T(KC_EQL), KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, RCTL_T(KC_QUOT), KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_BSLS), KC_LGUI, MO(1), KC_RALT, KC_SPC, KC_ENT, KC_BSPC, MO(1), TG(1)),
-	[1] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TAB, LCTL(KC_DEL), KC_HOME, KC_UP, KC_PGUP, 0x80, KC_PMNS, KC_P7, KC_P8, KC_P9, KC_PSLS, LCTL(KC_RGUI), KC_LCTL, LCTL(KC_BSPC), KC_LEFT, KC_DOWN, KC_RGHT, 0x81, KC_PPLS, KC_P4, KC_P5, KC_P6, KC_PAST, RCTL_T(KC_PEQL), KC_LSFT, KC_DEL, KC_END, LCTL(KC_SLSH), KC_PGDN, 0x7f, KC_LPRN, KC_RPRN, KC_P0, KC_P1, KC_P2, KC_P3, C_S_T(KC_PDOT), KC_RSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+	[0] = LAYOUT(
+        KC_GRV,  KC_1,  KC_2,    KC_3,   KC_4,   KC_5,                      KC_6,    KC_7,    KC_8, KC_9, KC_0,    LCA_T(KC_MINS),
+        KC_TAB,  KC_Q,  KC_W,    KC_E,   KC_R,   KC_T,                      KC_Y,    KC_U,    KC_I, KC_O, KC_P,    RGUI_T(KC_EQL),
+        KC_LCTL, KC_A,  KC_S,    KC_D,   KC_F,   KC_G,                      KC_H,    KC_J,    KC_K, KC_L, KC_SCLN, RCTL_T(KC_QUOT),
+        KC_LSFT, KC_Z,  KC_X,    KC_C,   KC_V,   KC_B,    KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT,          KC_SLSH, RSFT_T(KC_BSLS),
+        KC_LGUI, MO(1), KC_RALT, KC_SPC, KC_ENT, KC_BSPC, MO(1),   TG(1)),
+	[1] = LAYOUT(
+        KC_ESC,        KC_F1,         KC_F2,   KC_F3,         KC_F4,   KC_F5,                  KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,         KC_F11,
+        KC_TAB,        LCTL(KC_DEL),  KC_HOME, KC_UP,         KC_PGUP, 0x80,                   KC_PMNS, KC_P7,   KC_P8,   KC_P9,  KC_PSLS,        LCTL(KC_RGUI),
+        KC_LCTL,       LCTL(KC_BSPC), KC_LEFT, KC_DOWN,       KC_RGHT, 0x81,                   KC_PPLS, KC_P4,   KC_P5,   KC_P6,  KC_PAST,        RCTL_T(KC_PEQL),
+        KC_LSFT,       KC_DEL,        KC_END,  LCTL(KC_SLSH), KC_PGDN, 0x7f, KC_LPRN, KC_RPRN, KC_P0,   KC_P1,   KC_P2,   KC_P3,  C_S_T(KC_PDOT), KC_RSFT,
+        KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS,                                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 void keyboard_post_init_user(void) {
