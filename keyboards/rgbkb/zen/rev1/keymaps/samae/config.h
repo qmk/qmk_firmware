3d7043198e452d628af3d60023fb355154010372
EEva (JPotier) <jpo.contributes.to.nixos@marvid.fr>
2020-09-18 11:15:22 +0300

    Less animations

--- keyboards/rgbkb/zen/rev1/keymaps/samae/config.h
+++ keyboards/rgbkb/zen/rev1/keymaps/samae/config.h
@@ -31,7 +31,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 #undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
 #define RGBLED_NUM 16
 #define RGBLIGHT_HUE_STEP 4
 #define RGBLIGHT_SAT_STEP 4
