63c44bfcedd8779b3b904024eb5b50b132573dec
MakotoKurauchi <pluis@me.com>
2020-09-19 18:23:10 +0900

    change keyboard directory

--- keyboards/helix/helix.h
+++ keyboards/helix/helix.h
@@ -8,8 +8,11 @@
 #ifdef KEYBOARD_helix_rev2
     #include "rev2.h"
 #endif
-#ifdef KEYBOARD_helix_rev3
-    #include "rev3.h"
+#ifdef KEYBOARD_helix_rev3_5rows
+    #include "rev3_5rows.h"
+#endif
+#ifdef KEYBOARD_helix_rev3_4rows
+    #include "rev3_4rows.h"
 #endif
 #ifdef KEYBOARD_helix_pico
     #include "pico.h"
