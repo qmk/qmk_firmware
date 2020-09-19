2ce715210c624cca7eb2cd2c17c40ddf2c92c720
muuko <39163197+muuko@users.noreply.github.com>
2020-09-18 23:18:48 -0400

    Update keyboards/lily58/keymaps/muuko/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lily58/keymaps/muuko/config.h
+++ keyboards/lily58/keymaps/muuko/config.h
@@ -1,16 +1,5 @@
 #pragma once
 
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 6
-
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-
-#define DEBOUNCE 5
-
-#define DISABLE_LEADER
-#define ENCODERS_PAD_A { F4 }
-#define ENCODERS_PAD_B { F5 }
 
 #define EE_HANDS
 #define USE_SERIAL_PD2
@@ -34,4 +23,3 @@
 #endif
 #define COMBO_COUNT 2
 #define COMBO_TERM 20
-
