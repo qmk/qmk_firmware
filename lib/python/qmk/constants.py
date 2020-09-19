63d73fdb020400218817302026cbec2313c7c92a
Ryan <fauxpark@gmail.com>
2020-09-19 13:48:02 +1000

    Add STM32F401/F411 to ARM_PROCESSORS (#10362)

--- lib/python/qmk/constants.py
+++ lib/python/qmk/constants.py
@@ -9,7 +9,7 @@ QMK_FIRMWARE = Path.cwd()
 MAX_KEYBOARD_SUBFOLDERS = 5
 
 # Supported processor types
-ARM_PROCESSORS = 'cortex-m0', 'cortex-m0plus', 'cortex-m3', 'cortex-m4', 'MKL26Z64', 'MK20DX128', 'MK20DX256', 'STM32F042', 'STM32F072', 'STM32F103', 'STM32F303'
+ARM_PROCESSORS = 'cortex-m0', 'cortex-m0plus', 'cortex-m3', 'cortex-m4', 'MKL26Z64', 'MK20DX128', 'MK20DX256', 'STM32F042', 'STM32F072', 'STM32F103', 'STM32F303', 'STM32F401', 'STM32F411'
 AVR_PROCESSORS = 'at90usb1286', 'at90usb646', 'atmega16u2', 'atmega328p', 'atmega32a', 'atmega32u2', 'atmega32u4', None
 ALL_PROCESSORS = ARM_PROCESSORS + AVR_PROCESSORS
 VUSB_PROCESSORS = 'atmega328p', 'atmega32a', 'atmega328', 'attiny85'
