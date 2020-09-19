729fbc746b5fd4cd48fb5d0a523fc1058e4a84a5
Alex Ronke <w.alex.ronke@gmail.com>
2020-09-19 00:19:17 -0400

    Tweaks to my personal layout

--- keyboards/nopunin10did/railroad/keymaps/nopunin10did/keymap.c
+++ keyboards/nopunin10did/railroad/keymaps/nopunin10did/keymap.c
@@ -9,8 +9,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 KEYMAP(
 /* Left Block */
-   KC_ESC ,KC_PSRC,KC_ESC, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,  
-   KC_SLCK,KC_BSPC,    KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_6,
+   KC_ESC ,KC_PAUS,KC_ESC, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,  
+   KC_SLCK,KC_BSPC,    KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
    KC_CAPS,      KC_LCTL,          KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_PGUP,
    KC_CUT,   KC_LSFT,              KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_PGDN,
    KC_COPY,KC_PSTE,  KC_LCTL,    KC_LGUI,  KC_LALT,    KC_GRV,   KC_SPC,     LSHFDEL,
@@ -21,9 +21,9 @@ KEYMAP(
    KC_F4,  KC_F8,  KC_F12,  
 /* Right Block */
    KC_EQL, KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_BSPC,         KC_INS, RESET,  
-   KC_EQL, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,     KC_DEL, /*   ISO */   
-   KC_LBRC,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  KC_ENT,      KC_ENT,    
-   KC_RBRC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,          KC_UP,  
+   KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,     KC_DEL, /*   ISO */   
+   KC_HOME,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  KC_ENT,      KC_ENT,    
+   KC_END ,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,          KC_UP,  
  LSACALC,  KC_NUBS,    KC_NUHS,  RALTHOM,    RCTLEND,       KC_LEFT,KC_DOWN,KC_RGHT 
   )
 
