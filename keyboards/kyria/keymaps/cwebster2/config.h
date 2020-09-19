832de97c19ef9d9f2a59ddbf4842ab9348a42d37
Casey Webster <casey.webster@gmail.com>
2020-09-18 23:15:29 -0500

    Updated keymap and board features

--- keyboards/kyria/keymaps/cwebster2/config.h
+++ keyboards/kyria/keymaps/cwebster2/config.h
@@ -22,12 +22,23 @@
 
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_ANIMATIONS
+//#define RGBLIGHT_EFFECT_ALTERNATING
+//#define RGBLIGHT_EFFECT_BREATHING
+//#define RGBLIGHT_EFFECT_CHRISTMAS
+//#define RGBLIGHT_EFFECT_KNIGHT
+//#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#define RGBLIGHT_EFFECT_RGB_TEST
+//#define RGBLIGHT_EFFECT_SNAKE
+//#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#define RGBLIGHT_EFFECT_TWINKLE
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_SLEEP
 #endif
 
+#define TAPPING_TOGGLE 1
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
@@ -37,6 +48,26 @@
 #define NO_AUTO_SHIFT_ALPHA
 
 #define EE_HANDS
-// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
-// #define NO_USB_STARTUP_CHECK
+
+
+/* Disable unused and unneeded features to reduce on firmware size */
+#ifdef LOCKING_SUPPORT_ENABLE
+#    undef LOCKING_SUPPORT_ENABLE
+#endif
+#ifdef LOCKING_RESYNC_ENABLE
+#    undef LOCKING_RESYNC_ENABLE
+#endif
+
+#define LAYOUT_kc( \
+    L00, L01, L02, L03, L04, L05,                     R05, R04, R03, R02, R01, R00, \
+    L10, L11, L12, L13, L14, L15,                     R15, R14, R13, R12, R11, R10, \
+    L20, L21, L22, L23, L24, L25, L26, L27, R27, R26, R25, R24, R23, R22, R21, R20, \
+                   L33, L34, L35, L36, L37, R37, R36, R35, R34, R33 \
+    ) \
+    LAYOUT ( \
+    KC_##L00, KC_##L01, KC_##L02, KC_##L03, KC_##L04, KC_##L05,                     KC_##R05, KC_##R04, KC_##R03, KC_##R02, KC_##R01, KC_##R00, \
+    KC_##L10, KC_##L11, KC_##L12, KC_##L13, KC_##L14, KC_##L15,                     KC_##R15, KC_##R14, KC_##R13, KC_##R12, KC_##R11, KC_##R10, \
+    KC_##L20, KC_##L21, KC_##L22, KC_##L23, KC_##L24, KC_##L25, KC_##L26, KC_##L27, KC_##R27, KC_##R26, KC_##R25, KC_##R24, KC_##R23, KC_##R22, KC_##R21, KC_##R20, \
+                   KC_##L33, KC_##L34, KC_##L35, KC_##L36, KC_##L37, KC_##R37, KC_##R36, KC_##R35, KC_##R34, KC_##R33 \
+    )
+
--- keyboards/kyria/keymaps/cwebster2/keymap.c
+++ keyboards/kyria/keymaps/cwebster2/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include <raw_hid.h>
+#include "raw_hid.h"
 
 enum layers {
     _QWERTY = 0,
@@ -23,47 +23,63 @@ enum layers {
     _ADJUST
 };
 
+#define KC_RAISE TT(_RAISE)
+#define KC_LOWER TT(_LOWER)
+#define KC_KITTY S(KC_LCTL)
+#define KC_I3 S(KC_LGUI)
+#define KC_CTLESC MT(MOD_LCTL, KC_ESC)
+#define KC_GUIBS MT(MOD_LGUI, KC_BSPC)
+#define KC_ALTCLN MT(MOD_LALT, KC_COLON)
+#define KC_RTOG RGB_TOG
+#define KC_RSAI RGB_SAI
+#define KC_RHUI RGB_HUI
+#define KC_RVAI RGB_VAI
+#define KC_RMOD RGB_MOD
+#define KC_RSAD RGB_SAD
+#define KC_RHUD RGB_HUD
+#define KC_RVAD RGB_VAD
+#define KC_RRMD RGB_RMOD
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: QWERTY
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |RAIS/ESC|   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/BS |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | GUI  | Del  | Enter| Space| Esc  |  | Enter| Space| Tab  | Bksp | AltGr|
- *                        |      |      | Alt  | Lower| Raise|  | Lower| Raise|      |      |      |
- *                        `----------------------------------'  `----------------------------------'
  */
-    // TODO: i3, kitty keys, = somewhere on layer, what on the raise/lower taps, mod on sysreq?
-    [_QWERTY] = LAYOUT(
-      KC_GRV,    KC_Q,   KC_W,   KC_E,       KC_R,    KC_T,                                                      KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_PIPE,
-      KC_TAB,   KC_A,   KC_S,   KC_D,       KC_F,    KC_G,                                                       KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSPO,   KC_Z,   KC_X,   KC_C,       KC_V,    KC_B,      KC_LSPO, KC_LALT,       KC_RALT,       KC_RSPC, KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                                 S(KC_LGUI), S(KC_LCTL), LT(_RAISE, KC_TAB), KC_SPC,  MT(MOD_LCTL, KC_ESC), MT(MOD_LGUI, KC_BSPC), KC_ENT,  LT(_LOWER, KC_TAB), KC_SPC, KC_SYSREQ
+    [_QWERTY] = LAYOUT_kc(
+ // ,-------------------------------------------.                              ,-------------------------------------------.
+      GRV,      Q,     W,     E,     R,    T,                                     Y,     U,     I,     O,     P,    BSLS,
+ // |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+      TAB,      A,     S,     D,     F,    G,                                     H,     J,     K,     L,    SCLN,  QUOT,
+ // |--------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+--------|
+      LSPO,     Z,     X,     C,     V,    B,     LSPO, ALTCLN,    RALT,  RSPC,   N,     M,    COMM,  DOT,   SLSH,  MINS,
+ // `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+                              I3,  KITTY, RAISE,  SPC,  CTLESC,   GUIBS,  ENT,  LOWER,  SPC,   PSCR
+ //                        `----------------------------------'  `----------------------------------'
     ),
 /*
  * Lower Layer: Symbols
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |  !   |  @   |  {   |  }   |  |   |                              |      |      |      |      |      |  | \   |
+ * |        |  !   |  @   |  |   |  [   |  ]   |                              |      |      |      |      |      |  | \   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |  #   |  $   |  (   |  )   |  `   |                              |   +  |  -   |  /   |  *   |  %   |  ' "   |
+ * |        |  #   |  $   |  `   |  {   |  }   |                              |   +  |  -   |  /   |  *   |  %   |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |  %   |  ^   |  [   |  ]   |  ~   |      |      |  |      |      |   &  |  =   |  ,   |  .   |  / ? | - _    |
+ * |        |  %   |  ^   |  ~   |  (   |  )   |      |      |  |      |      |   &  |  =   |  ,   |  .   |  / ? | - _    |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |  ;   |  =   |  |  =   |  ;   |      |      |      |
+ *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_LOWER] = LAYOUT(
-      _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                                     _______, _______, _______, _______, _______, KC_BSLS,
-      _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                      KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOT,
-      _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______, _______, _______, _______, KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    [_LOWER] = LAYOUT_kc(
+ // ,-------------------------------------------.                              ,-------------------------------------------.
+       TRNS,   EXLM,   AT,   PIPE,  LBRC,  RBRC,                                 TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  BSLS,
+ // |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+       TRNS,   HASH,  DLR,   GRV,   LCBR,  RCBR,                                 PLUS,  MINS,  SLSH,  ASTR,  PERC,  QUOT,
+ // |--------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+--------|
+       TRNS,   PERC,  CIRC,  TILD,  LPRN,  RPRN,  TRNS,  TRNS,     TRNS,  TRNS,  AMPR,  EQL,   COMM,   DOT,  SLSH,  MINS,
+ // `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+                             TRNS,  TRNS,  TRNS,  TRNS,  TRNS,     TRNS,  TRNS,  TRNS,  TRNS,  TRNS
+ //                        `----------------------------------'  `----------------------------------'
     ),
 /*
  * Raise Layer: Number keys, media, navigation
@@ -79,12 +95,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_RAISE] = LAYOUT(
-      _______, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-      _______, _______, _______, _______, KC_MUTE, KC_VOLD, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
+    [_RAISE] = LAYOUT_kc(
+ // ,-------------------------------------------.                              ,-------------------------------------------.
+       TRNS,    1,     2,     3,     4,     5,                                    6,     7,     8,     9,     0,    TRNS,
+ // |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+       TRNS,   TRNS,  MPRV,  MPLY,  MNXT,  VOLU,                                 LEFT,  DOWN,   UP,   RGHT,  TRNS,  TRNS,
+ // |--------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+--------|
+       TRNS,   TRNS,  TRNS,  TRNS,  MUTE,  VOLD,  TRNS,  TRNS,     TRNS,  TRNS,  MS_L,  MS_D,  MS_U,  MS_R,  TRNS,  TRNS,
+ // `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+                             TRNS,  TRNS,  TRNS,  TRNS,  TRNS,     TRNS,  TRNS,  TRNS,  TRNS,  TRNS
+ //                        `----------------------------------'  `----------------------------------'
+      ),
 /*
  * Adjust Layer: Function keys, RGB
  *
@@ -99,54 +120,77 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_ADJUST] = LAYOUT(
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-      _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,                                     _______, _______, _______, KC_F11,  KC_F12,  _______,
-      _______, _______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-// /*
-//  * Layer template
-//  *
-//  * ,-------------------------------------------.                              ,-------------------------------------------.
-//  * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
-//  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
-//  * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
-//  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-//  * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
-//  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
-//  *                        |      |      |      |      |      |  |      |      |      |      |      |
-//  *                        |      |      |      |      |      |  |      |      |      |      |      |
-//  *                        `----------------------------------'  `----------------------------------'
-//  */
-//     [_LAYERINDEX] = LAYOUT(
-//       _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
-//       _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
-//       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-//                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-//     ),
+    [_ADJUST] = LAYOUT_kc(
+ // ,-------------------------------------------.                              ,-------------------------------------------.
+       TRNS,    F1,    F2,    F3,    F4,    F5,                                   F6,    F7,    F8,    F9,   F10,   TRNS,
+ // |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+       TRNS,   RTOG,  RSAI,  RHUI,  RVAI,  RMOD,                                 TRNS,  TRNS,  TRNS,   F11,  F12,   TRNS,
+ // |--------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+--------|
+       TRNS,   TRNS,  RSAD,  RHUD,  RVAD,  RRMD,  TRNS,  TRNS,     TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,
+ // `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+                             TRNS,  TRNS,  TRNS,  TRNS,  TRNS,     TRNS,  TRNS,  TRNS,  TRNS,  TRNS
+ //                        `----------------------------------'  `----------------------------------'
+      ),
+ //
+ //  * Layer template
+ //
+ //    [_LAYERINDEX] = LAYOUT_kc(
+ // ,-------------------------------------------.                              ,-------------------------------------------.
+ //    TRNS,   TRNS,  TRNS,  TRNS,  TRNS,  TRNS,                                 TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,
+ // |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+ //    TRNS,   TRNS,  TRNS,  TRNS,  TRNS,  TRNS,                                 TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,
+ // |--------+------+------+------+------+------+------+------.  ,------+------+------+------+------+------+------+--------|
+ //    TRNS,   TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,     TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,
+ // `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+ //                          TRNS,  TRNS,  TRNS,  TRNS,  TRNS,     TRNS,  TRNS,  TRNS,  TRNS,  TRNS
+ //                        `----------------------------------'  `----------------------------------'
+ //    ),
 };
 
 static void send_layer_via_hid(char *layer) {
-    raw_hid_send((uint8_t*)layer, sizeof(layer));
+    //raw_hid_send((uint8_t*)layer, sizeof(layer));
+    return;
 }
+
+/* RGB LIGHT LAYERS
+const rgblight_segment_t PROGMEM my_qwerty_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 10, HSV_AZURE}
+);
+
+const rgblight_segment_t PROGMEM my_raise_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 10, HSV_RED}
+);
+
+const rgblight_segment_t PROGMEM my_lower_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 10, HSV_TURQUOISE}
+);
+
+const rgblight_segment_t PROGMEM my_adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 10, HSV_PURPLE}
+);
+*/
+
+
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     layer_state_t new_state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+
     switch (get_highest_layer(new_state)) {
         case _QWERTY:
             send_layer_via_hid("Default");
-            //rgblight_sethsv_noeeprom(0, 0, 255);
+            rgblight_sethsv_noeeprom(HSV_BLUE);
             break;
         case _LOWER:
             send_layer_via_hid("Lower");
-            //rgblight_sethsv_noeeprom(255, 0, 0);
+            rgblight_sethsv_noeeprom(HSV_RED);
             break;
         case _RAISE:
             send_layer_via_hid("Raise");
+            rgblight_sethsv_noeeprom(HSV_TURQUOISE);
             break;
         case _ADJUST:
             send_layer_via_hid("Adjust");
+            rgblight_sethsv_noeeprom(HSV_PURPLE);
             break;
         default:
             send_layer_via_hid("Undefined");
@@ -200,10 +244,10 @@ static void render_status(void) {
             oled_write_P(PSTR("Default\n"), false);
             break;
         case _LOWER:
-            oled_write_P(PSTR("Lower\n"), false);
+            oled_write_P(PSTR("Lower / Symbols\n"), false);
             break;
         case _RAISE:
-            oled_write_P(PSTR("Raise\n"), false);
+            oled_write_P(PSTR("Raise / Numbers\n"), false);
             break;
         case _ADJUST:
             oled_write_P(PSTR("Adjust\n"), false);
@@ -233,6 +277,7 @@ void oled_task_user(void) {
 void raw_hid_receive(uint8_t *data, uint8_t length) {
     // messages from host
     // add ability to set color
+    raw_hid_send(data, length);
 }
 #endif
 
