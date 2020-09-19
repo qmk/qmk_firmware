a0efe932394492ddd79212c2e652306aae9cf72d
Baris Tosun <me@baristosun.com>
2020-09-19 05:54:08 +0200

    Refactored the Katana60 folders under a rominronin parent folder (#10255)
    * refactored the Katana60 folders under a rominronin parent folder
    
    * Update keyboards/rominronin/katana60/info.json
    
    removed 'keyboard_folder' line
    
    * Update keyboards/rominronin/katana60/info.json
    
    removed 'key_count' line
    
    * Update keyboards/rominronin/katana60_rev2/config.h
    
    removed '#define DESCRIPTION' line
    
    * Update keyboards/rominronin/katana60_rev2/rules.mk
    
    Removed more attributes
    
    * Update keyboards/rominronin/katana60_rev2/rules.mk
    
    removed 'FAUXCLICKY_ENABLE' line
    
    * Update keyboards/rominronin/katana60_rev2/rules.mk
    
    Updated some comments
    
    * Update keyboards/rominronin/katana60_rev2/config.h
    
    Removed redundant comments
    
    * Update keyboards/rominronin/katana60_rev2/config.h
    
    removed redundant comments
    
    * Update keyboards/rominronin/katana60_rev2/config.h
    
    removed redundant comments
    
    * Update keyboards/rominronin/katana60/keymaps/default/keymap.c
    
    Fixed a non-standard keycode
    
    * Update keyboards/rominronin/katana60/keymaps/rominronin/keymap.c
    
    Removed deprecated code
    
    * Update keyboards/rominronin/katana60/rules.mk
    
    removed redundant comments
    
    * Update keyboards/rominronin/katana60_rev2/katana60_rev2.c
    
    removed redundant code
    
    * Update keyboards/rominronin/katana60_rev2/keymaps/rominronin_7u/keymap.c
    
    removed redundant code
    
    * Update keyboards/rominronin/katana60_rev2/rules.mk
    
    removed redundant comments
    
    * Update keyboards/rominronin/katana60_rev2/rules.mk
    
    updated some comments
    
    * Update keyboards/rominronin/katana60_rev2/rules.mk
    
    removed redundant code
    
    * Update keyboards/rominronin/katana60_rev2/keymaps/rominronin_7u/keymap.c
    
    optimised the layers definition
    
    * Update keyboards/rominronin/katana60/rules.mk
    
    removed redundant code
    
    * added the info.json file for every Tsuka60 layout
    
    * updated the info.json file details
    
    * removed broken keymap file, updated other files
    
    * updated the Katana60 rev2 info.json file
    
    * split katana60 into revisions
    
    * assign unique Vendor/Product ID pairings
    
    * modernize codebase
    
    #pragma once on included files; rules.mk templating
    
    * fix rev2 layout macros
    
    * updated the keymaps for correct automated building.
    
    * updated the default layout file
    
    * updated default rominronin 7u layout file
    
    * Update keyboards/rominronin/katana60/rev1/readme.md
    
    resized an image
    
    * Update keyboards/rominronin/katana60/rev2/readme.md
    
    resized an image
    
    * remove dead space from rev2 info.json file

--- keyboards/rominronin/katana60/rev1/keymaps/colemak/keymap.c
+++ keyboards/rominronin/katana60/rev1/keymaps/colemak/keymap.c
@@ -34,7 +34,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT( /* Base */
-  KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    DF(1),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_PLUS,
+  KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    DF(1),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC,          KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
   MO(3),   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_HOME,          KC_PGUP, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_DEL,  KC_PGDN, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
