aeb6e8dec8e9b94d2a87c7e462dfb2b1b4fa0561
Isaac <winternebs@gmail.com>
2020-09-20 19:09:07 -0400

    Update keyboards/planck/keymaps/winterNebs/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/planck/keymaps/winterNebs/keymap.c
+++ keyboards/planck/keymaps/winterNebs/keymap.c
@@ -126,7 +126,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case WORKMAN:
       if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_WORKMAN);
+       set_single_persistent_default_layer(_WORKMAN);
       }
       return false;
       break;
