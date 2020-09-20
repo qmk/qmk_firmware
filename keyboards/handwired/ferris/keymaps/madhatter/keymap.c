fb6a3dbe68482f603c7ad4bf7d23ac872af13c9f
Khader Syed <khader.syed@aicure.com>
2020-09-19 21:47:25 -0400

    add extra control and shift, for when we need to use the other keys

--- keyboards/handwired/ferris/keymaps/madhatter/keymap.c
+++ keyboards/handwired/ferris/keymaps/madhatter/keymap.c
@@ -17,8 +17,9 @@ enum ilpse_layers {
 #define KC_CTES CTL_T(KC_ESC)
 #define KC_CTEN RCTL_T(KC_ENT)
 #define KC_CTSC RCTL_T(KC_SCLN)
-#define KC_LSHZ SFT_T(KC_Z)
-#define KC_SPM1 LT(1, KC_SPC)
+#define KC_CTLA LCTL_T(KC_A)
+#define KC_LSHZ LSFT_T(KC_Z)
+#define KC_RLSH RSFT_T(KC_SLSH)
 #define KC_SPM2 LT(2, KC_SPC)
 #define KC_BSM1 LT(1, KC_BSPC)
 #define KC_GUTA GUI_T(KC_TAB)
@@ -30,8 +31,8 @@ enum ilpse_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* QWERTY */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
-    KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,
+    KC_CTLA, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
+    KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RLSH,
                                     KC_CLGV, KC_BSM1, KC_SPM2, KC_GUTA
   ),
 
