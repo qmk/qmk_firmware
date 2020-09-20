dbbc178ccf35006504df1c8d0782b99d90fa5d09
Isaac <winternebs@gmail.com>
2020-09-20 19:08:48 -0400

    Update keyboards/kyria/keymaps/winterNebs/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kyria/keymaps/winterNebs/rules.mk
+++ keyboards/kyria/keymaps/winterNebs/rules.mk
@@ -5,11 +5,9 @@ WPM_ENABLE = yes
 EXTRAFLAGS += -flto
 CONSOLE_ENABLE = no     # Console for debug(+400)
 AUDIO_ENABLE   = no     # Audio output on port C6
-LINK_TIME_OPTIMIZATION_ENABLE = yes
 SPACE_CADET_ENABLE = no
 EXTRAKEY_ENABLE = no
 MOUSEKEY_ENABLE = no
 BOOTMAGIC_ENABLE = no
 SWAP_HANDS_ENABLE = yes
 LTO_ENABLE = yes
-
