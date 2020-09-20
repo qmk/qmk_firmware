3b94b72e825a0a1795eab49d245d5afd8116985f
Isaac <winternebs@gmail.com>
2020-09-20 19:07:42 -0400

    Update keyboards/keebio/nyquist/keymaps/winterNebs/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keebio/nyquist/keymaps/winterNebs/config.h
+++ keyboards/keebio/nyquist/keymaps/winterNebs/config.h
@@ -37,5 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_MAX_SPEED 4
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 10
 
-#undef FORCE_NKRO
-#define FORCE_NKRO
+#ifndef FORCE_NKRO
+#    define FORCE_NKRO
+#endif
