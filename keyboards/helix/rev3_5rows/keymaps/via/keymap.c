d72de39c0acf2e97bc8c391e9489d557ebd0f9d5
MakotoKurauchi <pluis@me.com>
2020-09-20 12:54:35 +0900

    Update keyboards/helix/rev3_5rows/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/helix/rev3_5rows/keymaps/via/keymap.c
+++ keyboards/helix/rev3_5rows/keymaps/via/keymap.c
@@ -164,9 +164,6 @@ void dip_switch_update_user(uint8_t index, bool active) {
     }
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
