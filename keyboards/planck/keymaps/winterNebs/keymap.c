d69e46e0268032691c9df14d857b51bedb854bbf
Isaac <winternebs@gmail.com>
2020-09-20 19:08:55 -0400

    Update keyboards/planck/keymaps/winterNebs/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/planck/keymaps/winterNebs/keymap.c
+++ keyboards/planck/keymaps/winterNebs/keymap.c
@@ -110,13 +110,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-//uint32_t layer_state_set_user(uint32_t state) {
-//  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-//}
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
 
 
 bool held[7][6] = {{false}};
