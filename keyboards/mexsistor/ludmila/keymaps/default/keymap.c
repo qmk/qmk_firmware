439b9bf26587ebe9bd730bcb9ac2f45ae355a771
mexsistor <68937128+mexsistor@users.noreply.github.com>
2020-09-21 02:08:56 -0500

    Update keymap.c

--- keyboards/mexsistor/ludmila/keymaps/default/keymap.c
+++ keyboards/mexsistor/ludmila/keymaps/default/keymap.c
@@ -15,6 +15,8 @@
  */
 #include QMK_KEYBOARD_H
 
+#include QMK_KEYBOARD_H
+
 // Keyboard Layers
 enum keyboard_layers{
     _BASE = 0,
@@ -24,7 +26,9 @@ enum keyboard_layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT( /* Base */
     KC_A, KC_B, KC_C,
-    KC_D, KC_E,);
+    KC_D, KC_E
+
+  ),
 
 };
 
