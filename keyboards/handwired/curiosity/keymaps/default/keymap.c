e0a3585d9bfeb1a6c8696252db05820ce6a73604
Spaceman <rionlion100@gmail.com>
2020-09-21 14:23:59 -0400

    Update keymap.c

--- keyboards/handwired/curiosity/keymaps/default/keymap.c
+++ keyboards/handwired/curiosity/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 marhalloweenvt
+/* Copyright 2020 Spaceman
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
