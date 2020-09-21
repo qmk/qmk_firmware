d72e2cc6788fa28faba73b3b72a4298c6e833d9c
Max Drasbeck <max@drasbeck.dk>
2020-09-21 22:24:46 +0200

    Update keyboards/lily58/keymaps/drasbeck/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/drasbeck/rules.mk
+++ keyboards/lily58/keymaps/drasbeck/rules.mk
@@ -24,8 +24,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_DRIVER_ENABLE = yes    # OLED display
-
-ENCODER_ENABLE = yes 	    # Enable encoder
+ENCODER_ENABLE = yes        # Enable encoder
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
