78f47b4e42a4bedfb34155044f2e7eeceb4c5428
Brandon Claveria <48102030+swiftrax@users.noreply.github.com>
2020-09-20 18:32:56 -0700

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/swiftrax/retropad/config.h
+++ keyboards/handwired/swiftrax/retropad/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Swiftrax
 #define PRODUCT         Retropad
-#define DESCRIPTION     2x2 macro pad with a rotary encoder
 
 /* key matrix size */
 #define MATRIX_ROWS 3
--- keyboards/handwired/swiftrax/retropad/rules.mk
+++ keyboards/handwired/swiftrax/retropad/rules.mk
@@ -28,5 +28,4 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 ENCODER_ENABLE = yes
