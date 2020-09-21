c3e74eac5760c4e403a73f298e79824f5f5da2d9
mexsistor <68937128+mexsistor@users.noreply.github.com>
2020-09-21 09:45:49 -0500

    Update keyboards/mexsistor/ludmila/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mexsistor/ludmila/rules.mk
+++ keyboards/mexsistor/ludmila/rules.mk
@@ -8,9 +8,8 @@ BOOTLOADER = caterina
 #   change yes to no to disable
 #
 
-CUSTOM_MATRIX = yes			# Custom scanning of matrix
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
@@ -20,13 +19,9 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes        # Rotary Encoder support
-TAP_DANCE_ENABLE = no      # Support for tap dancing
+CUSTOM_MATRIX = yes
 
 SRC = matrix.c
