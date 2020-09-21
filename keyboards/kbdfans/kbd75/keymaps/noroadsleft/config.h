0302f2dfb1aa03548b16890d9c34c89ad1d9fa8b
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-09-21 04:45:34 -0700

    add kbdfans/kbd75/rev1:noroadsleft keymap

---
+++ keyboards/kbdfans/kbd75/keymaps/noroadsleft/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#undef MANUFACTURER
+#undef PRODUCT
+#undef DESCRIPTION
+#define MANUFACTURER    KBDfans
+#define PRODUCT         KBD75 rev1
+#define DESCRIPTION     QMK Firmware: KBD75 rev1
+
+#define BACKLIGHT_BREATHING
