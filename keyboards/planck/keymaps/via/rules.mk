0b663141ae2cbe665132e2319bb7be3065e0ff12
Zach Richard <zach.richard94@gmail.com>
2020-09-21 14:35:31 -0400

    Added support for via for Planck

---
+++ keyboards/planck/keymaps/via/rules.mk
@@ -0,0 +1,7 @@
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+EXTRAKEY_ENABLE  = yes 	# Audio control and System control(+450)
+BACKLIGHT_ENABLE = no 	# Enable keyboard backlight functionality
+RGBLIGHT_ENABLE  = no  	# Enable WS2812 RGB underlight.
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
