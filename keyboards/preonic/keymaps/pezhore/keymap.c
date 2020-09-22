7e43a56422279be2d65d085589dfe2582c86f992
pezhore <pezhore@gmail.com>
2020-09-22 07:59:18 -0500

    Stripping out FN keycodes in favor of a momentary layer

--- keyboards/preonic/keymaps/pezhore/keymap.c
+++ keyboards/preonic/keymaps/pezhore/keymap.c
@@ -27,8 +27,7 @@ enum preonic_layers {
 };
 
 enum preonic_keycodes {
-  FN = SAFE_RANGE,
-  LOWER,
+  LOWER = SAFE_RANGE,
   RAISE,
   SHRUG,  // ¯\_(ツ)_/¯
   TFLIP,  // (╯°□°)╯︵ ┻━┻
@@ -59,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  FN,      KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  MO(_FN), KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* FN
@@ -173,14 +172,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-        case FN:
-          if (record->event.pressed) {
-            layer_on(_FN);
-          } else {
-            layer_off(_FN);
-          }
-          return false;
-          break;
         case LOWER:
           if (record->event.pressed) {
             layer_on(_LOWER);
