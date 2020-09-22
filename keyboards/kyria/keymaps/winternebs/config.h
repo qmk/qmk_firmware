0b14ee572963c485d333b418ed7a33ca4a85fc7b
winterNebs <winterNebs@gmail.com>
2020-09-22 02:14:55 -0400

    fix oled path

--- keyboards/kyria/keymaps/winternebs/config.h
+++ keyboards/kyria/keymaps/winternebs/config.h
@@ -16,10 +16,14 @@
 #pragma once
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define OLED_FONT_H "keyboards/kyria/keymaps/winternebs/glcdfont.c"
 =======
 #define OLED_FONT_H "keyboards/kyria/keymaps/winterNebs/glcdfont.c"
 >>>>>>> Rename to lowercase
+=======
+#define OLED_FONT_H "keyboards/kyria/keymaps/winternebs/glcdfont.c"
+>>>>>>> fix oled path
 #define OLED_FONT_END 127
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X64
