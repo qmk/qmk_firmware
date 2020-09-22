9b4bb8908295761275bda332e596d804f7accdba
Felix Jen <fjen@felixjen.com>
2020-09-22 00:08:56 -0500

    Update keyboards/fjlabs/bolsa65/bolsa65.c
    Fixed indentation as suggested.
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/fjlabs/bolsa65/bolsa65.c
+++ keyboards/fjlabs/bolsa65/bolsa65.c
@@ -1,9 +1,9 @@
 #include "bolsa65.h"
 
 void matrix_init_kb(void) {
-	// Initialize indicator LEDs to output
+    // Initialize indicator LEDs to output
     setPinOutput(F7); // Caps
-	matrix_init_user();
+    matrix_init_user();
 }
 
 bool led_update_kb(led_t led_state) {
