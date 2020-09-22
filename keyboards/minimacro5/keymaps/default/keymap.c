6c3689ec3642f026e6ff60a1edf83ca51c5385c7
dezli <29718605+dezlidezlidezli@users.noreply.github.com>
2020-09-22 10:03:38 +0800

    Update keyboards/minimacro5/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/minimacro5/keymaps/default/keymap.c
+++ keyboards/minimacro5/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
 //
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { //buttion closest to usb is first
-  [_MAIN] = LAYOUT(
+  [_MAIN] = LAYOUT_ortho_1x5(
      KC_A, KC_B, KC_C, KC_D, KC_E
   )
 };
