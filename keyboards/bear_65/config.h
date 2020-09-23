8e2dc837d725abb7b5bc905d8dc1563402f515ae
MudkipMao <splatt9198@gmail.com>
2020-09-22 20:56:58 -0700

    Integrated suggestions and fixed the info.json layout

--- keyboards/bear_65/config.h
+++ keyboards/bear_65/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Jacky
 #define PRODUCT         Jacky Bear 65 Ergo
-#define DESCRIPTION     A gasket-mounted 65% Alice style board
 
 /* key matrix size */
 #define MATRIX_ROWS 5
