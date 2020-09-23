fb03330b61ac81a99c8a8a9f30eb6f548a11d4ba
foostan <ks@fstn.jp>
2020-09-23 10:15:40 +0900

    Revert "Refactor the crkbd/via keymap and fix OLED bugs"
    This reverts commit dc9db029fc6b077fc7600b49e920b755c522d100.

--- keyboards/crkbd/rev1/common/keymaps/via/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/via/rules.mk
@@ -1,13 +1,5 @@
-SRC += ./lib/rgb_state_reader.c \
-       ./lib/layer_state_reader.c \
-       ./lib/logo_reader.c \
-       ./lib/keylogger.c \
-       # ./lib/mode_icon_reader.c \
-       # ./lib/host_led_state_reader.c \
-       # ./lib/timelogger.c \
-
 MOUSEKEY_ENABLE     = no     # Mouse keys
 RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
-VIA_ENABLE          = yes    # Enable VIA
+VIA_ENABLE          = yes         # Enable VIA
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
