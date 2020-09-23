52592f585b52a72bfad1c8db773a4477e1ca3c75
mechmerlin <mechmerlin@gmail.com>
2020-09-23 00:00:51 -0700

    one more small change

--- keyboards/kbdfans/kbd19x/kbd19x.h
+++ keyboards/kbdfans/kbd19x/kbd19x.h
@@ -42,7 +42,7 @@ inline void kbd19x_nmlk_led_off(void)   { DDRB &= ~(1<<2); PORTB &= ~(1<<2); }
  */
 
 // 2u backspace: XXX, k71
-// split backspace: k1d, k71
+// split backspace: k70, k71
 
 // 2u numpad +: k2c, XXX
 // split numpad +: k2c, k3c
@@ -65,7 +65,7 @@ inline void kbd19x_nmlk_led_off(void)   { DDRB &= ~(1<<2); PORTB &= ~(1<<2); }
 
 #define LAYOUT_all( \
   k00,  k01, k02, k03, k04,  k60, k61, k62, k63,  k05, k06, k07, k08,   k72,  k09, k0a, k0b, k0c, \
-  k10, k11, k12, k13, k14, k64, k65, k66, k67, k15, k16, k17, k18, k1d, k71,  k19, k1a, k1b, k1c, \
+  k10, k11, k12, k13, k14, k64, k65, k66, k67, k15, k16, k17, k18, k70, k71,  k19, k1a, k1b, k1c, \
   k20,  k21, k22, k23, k24, k68, k69, k6a, k6b, k25, k26, k27, k28,     k73,  k29, k2a, k2b, k2c, \
   k30,    k31, k32, k33, k34, k6c, k75, k76, k77, k35, k36, k37,        k38,  k39, k3a, k3b, k3c, \
   k40,  k41, k42, k43, k44, k78, k79, k7a, k7b, k45, k46, k47, k48,    k74,   k49, k4a, k4b, k4c, \
@@ -79,7 +79,7 @@ inline void kbd19x_nmlk_led_off(void)   { DDRB &= ~(1<<2); PORTB &= ~(1<<2); }
   {k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b, k4c},\
   {k50, k51, k52, k53, k54, k55, k56, k57, k58, k59, k5a, k5b, k5c},\
   {k60, k61, k62, k63, k64, k65, k66, k67, k68, k69, k6a, k6b, k6c},\
-  {k1d, k71, k72, k73, k74, k75, k76, k77, k78, k79, k7a, k7b, XXX},\
+  {k70, k71, k72, k73, k74, k75, k76, k77, k78, k79, k7a, k7b, XXX},\
 }
 
 #define LAYOUT_ansi( \
