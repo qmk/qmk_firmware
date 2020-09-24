f399763501d595bbf72cc96b8f565cb952e91d15
George Koenig <george.g.koenig@gmail.com>
2020-09-24 13:50:00 -0400

    Slight tweak to default keymap.c

--- keyboards/wsk/alpha9/keymaps/default/keymap.c
+++ keyboards/wsk/alpha9/keymaps/default/keymap.c
@@ -2,19 +2,19 @@
 
 enum alpha9_layers {
     _HOME,
-    _FN1,
+    _FN,
     _FNCHAR,
     _FKEYS,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_HOME] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_Q,         KC_W,         KC_E,           KC_R,    KC_T,     KC_Y, KC_U,     KC_I,         KC_O,         KC_P,
-        KC_P4,   KC_P5,   KC_P6,   KC_A,         KC_S,         KC_D,           KC_F,    KC_G,     KC_H, KC_J,     KC_K,         KC_L,         KC_ENT,
-        KC_P1,   KC_P2,   KC_P3,   LCTL_T(KC_Z), LALT_T(KC_X), LT(_FN1, KC_C), LT(_FNCHAR, KC_V), LSFT_T(KC_SPC), RGUI_T(KC_B), RALT_T(KC_N), RCTL_T(KC_M)
+        KC_P7,   KC_P8,   KC_P9,   KC_Q,         KC_W,         KC_E,          KC_R,    KC_T,     KC_Y, KC_U,     KC_I,         KC_O,         KC_P,
+        KC_P4,   KC_P5,   KC_P6,   KC_A,         KC_S,         KC_D,          KC_F,    KC_G,     KC_H, KC_J,     KC_K,         KC_L,         KC_ENT,
+        KC_P1,   KC_P2,   KC_P3,   LCTL_T(KC_Z), LALT_T(KC_X), LT(_FN, KC_C), LT(_FNCHAR, KC_V), LSFT_T(KC_SPC), RGUI_T(KC_B), RALT_T(KC_N), RCTL_T(KC_M)
     ),
 
-    [_FN1] = LAYOUT(
+    [_FN] = LAYOUT(
         KC_INS,  KC_HOME, KC_PGUP, KC_ESC,  KC_MUTE, KC_VOLD, KC_VOLU,    KC_MPLY,        KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,
         KC_DEL,  KC_UP,   KC_PGDN, KC_TAB,  KC_NO,   KC_NO,   KC_NO,      KC_NO,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
         KC_LEFT, KC_DOWN, KC_RGHT, KC_LCTL, KC_LALT, KC_TRNS, MO(_FKEYS), LSFT_T(KC_SPC),          KC_RGUI,          KC_RALT, KC_RCTL
