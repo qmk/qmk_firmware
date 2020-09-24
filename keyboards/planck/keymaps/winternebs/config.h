9c95254a91c7dc328780e1ed1617b1dbf6700723
winterNebs <winterNebs@gmail.com>
2020-09-23 21:57:16 -0400

    License in the wrong file

--- keyboards/planck/keymaps/winternebs/config.h
+++ keyboards/planck/keymaps/winternebs/config.h
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-/* Copyright 2020 winterNebs <winternebs@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-=======
->>>>>>> Rename to lowercase
 #pragma once
 /*
 #ifdef AUDIO_ENABLE
@@ -61,8 +42,4 @@
 #define MOUSEKEY_INTERVAL 8
 #define MOUSEKEY_MAX_SPEED 3
 #define MOUSEKEY_TIME_TO_MAX 0
-<<<<<<< HEAD
 #define FORCE_NKRO
-=======
-#define FORCE_NKRO
->>>>>>> Rename to lowercase
