b85c6536615050528f6b6f2d62f20a88c7f442b6
Sturek <Sturek@users.noreply.github.com>
2020-09-25 23:44:28 +0200

    Added via support

---
+++ keyboards/sofle/keymaps/via/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+//Add RGB underglow
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 14
+#define RGBLED_SPLIT {7,7}
+#define RGBLIGHT_ANIMATIONS
---
+++ keyboards/sofle/keymaps/via/encoder.c
@@ -0,0 +1,21 @@
+//Setting up what encoder rotation does. If your encoder can be pressed as a button, that function can be set in Via.
+
+#ifdef ENCODER_ENABLE
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_PGDOWN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+}
+
+#endif
