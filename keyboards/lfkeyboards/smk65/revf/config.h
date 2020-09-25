b06e69af55c13df95d8694ab480a9af781ffe472
zvecr <git@zvecr.com>
2020-09-25 01:28:40 +0100

    Update IDs for via

--- keyboards/lfkeyboards/smk65/revf/config.h
+++ keyboards/lfkeyboards/smk65/revf/config.h
@@ -17,9 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6062
-#define DEVICE_VER      0x0001
+#define VENDOR_ID       0x4C46   // "LF"
+#define PRODUCT_ID      0x565F   // "s65f"
+#define DEVICE_VER      0x0006
 #define MANUFACTURER    LFKeyboards
 #define PRODUCT         SMK65v2
 
