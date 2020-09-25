e33321dad684f41a1858a2bcfebf52071b77dc18
matthewdias <matthewdias@me.com>
2020-09-24 19:24:48 -0500

    Remove extra layer

--- keyboards/jnao/config.h
+++ keyboards/jnao/config.h
@@ -51,6 +51,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-/* Enable more layers in VIA */
-#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
-
