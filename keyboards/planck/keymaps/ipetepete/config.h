cb5776b3fd062a3593cd72bfa723386d5bb49d56
Peter P <peter.peterson.iv@gmail.com>
2020-09-25 08:24:06 -0700

    updated ortho keymaps

--- keyboards/planck/keymaps/ipetepete/config.h
+++ keyboards/planck/keymaps/ipetepete/config.h
@@ -1,10 +1,3 @@
 #pragma once
-#define BACKLIGHT_PIN B7
+//#define BACKLIGHT_PIN B7
 #define BACKLIGHT_BREATHING
-
-/* number of backlight levels */
-#ifndef BACKLIGHT_LEVELS
-#define BACKLIGHT_LEVELS 5
-#endif
-//#define PREVENT_STUCK_MODIFIERS
-
--- keyboards/preonic/keymaps/ipetepete/rules.mk
+++ keyboards/preonic/keymaps/ipetepete/rules.mk
@@ -1,9 +1,9 @@
 SRC += muse.c
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = yes         # Console for debug(+400)
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug(+400)
+COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 MIDI_ENABLE = yes            # MIDI controls
