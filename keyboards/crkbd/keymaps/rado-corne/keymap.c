ea76399300b61f43f6bede479daf459af83722a6
rado <radoosredkar@hotmail.com>
2020-09-26 12:03:52 +0200

    Tap dance for layers added

--- keyboards/crkbd/keymaps/rado-corne/keymap.c
+++ keyboards/crkbd/keymaps/rado-corne/keymap.c
@@ -81,20 +81,23 @@ enum macro_keycodes {
 
 // Tap Dance declarations
 enum {
-		TD_ESC_CAPS,
+		TD_PROGRAMMING,
+		TD_NUMPAD,
+		TD_GAMMING,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
 		// Tap once for Escape, twice for Caps Lock
-		[TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_B),
+		[TD_NUMPAD] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_N, 5),
+		[TD_PROGRAMMING] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_P, 6),
+		[TD_GAMMING] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_G, 7) 
 };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_QWERTY] = LAYOUT_split_3x6_3(
-						KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, 						KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
-						LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, 				KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
-						KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, 						KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
+						KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, 						KC_Y, KC_U, KC_I, KC_O, TD(TD_PROGRAMMING), KC_BSPC, 
+						LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, TD(TD_GAMMING), 	KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
+						KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, 						TD(TD_NUMPAD), KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
 						KC_LGUI, MO(1), KC_SPC, 									KC_ENT, MO(2), KC_RALT
 						),
 		[_NUMBER] = LAYOUT_split_3x6_3(
