d56a2749081819bbbe865841bb3461fb40c2d9d3
Oktay <oktayelipek@gmail.com>
2020-09-26 23:26:47 +0300

    macros changed to sendstrings and definitons renew

--- keyboards/OktiBoard/keymaps/default/keymap.c
+++ keyboards/OktiBoard/keymaps/default/keymap.c
@@ -1,33 +1,78 @@
 #include "kb.h"
 #include QMK_KEYBOARD_H
 
+//General Definitions
 #define LOCKSCREEN LCTL(LSFT(KC_POWER)) // Screen Lock shortcut for OSX
+
+// Figma Componenets
+#define CREATECOMPONENT LALT(LGUI(KC_K))
+#define SHOWCOMPONENT LALT(LKC_KP_2)
+#define TEAMLIB LALT(LGUI(KC_O))
+#define DETACH LALT(LGUI(KC_B))
+
+// Figma Shortcuts
+#define FIGLAYERS LALT(LKC_KP_1)
+#define SENDBACK LGUI(KC_LBRACKET)
+#define BRINGFORW LGUI(KC_RBRACKET)
+#define GRID LCTRL(KC_G)
+#define TEXTLEFT LALT(LGUI(KC_L))
+#define TEXTRIGHT LALT(LGUI(KC_R))
+#define TEXTCENTER LALT(LGUI(KC_T))
+#define RUNLASTPLUG LALT(LALT(KC_P))
+#define COPYSTYLE KC_LGUI(KC_LALT(KC_C))
+#define PASTESTYLE KC_LGUI(KC_LALT(KC_V))
+#define AUTOLAYOUT KC_LSHIFT(KC_A)
+#define RMLAYOUT KC_LGUI(KC_LSHIFT(KC_A))
 
 enum oktiboard_layers {
   _FIGMA = 0,
   _LAB,
 };
 
+enum custom_keycodes {
+    M_AUTOLAY = SAFE_RANGE,
+    M_COPYSTYLES,
+};
+
+bool is_copied = false;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case M_AUTOLAY:
+        if(!is_copied) {
+            SEND_STRING(AUTOLAYOUT);
+            is_copied = true;
+        } else {
+            SEND_STRING(RMLAYOUT);
+            is_copied = false;
+        }
+        break;
+
+    case M_COPYSTYLES:
+        if(!is_copied) {
+            SEND_STRING(COPYSTYLE);
+            is_copied = true;
+        } else {
+            SEND_STRING(PASTESTYLE);
+            is_copied = false;
+        }
+        break;
+    }
+    return true;
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _FIGMA:
+        rgblight_setrgb (148,0,211);
+        break;
+    case _LAB:
+        rgblight_setrgb (255,218,185);
+        break;
+    }
+  return state;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-// Macro Declarations
-    enum {
-        Create_Components = 0,
-        Show_Components = 1,
-        Team_Library = 2,
-        Detach_Instance = 3,
-
-        All_Layers = 4,
-        Bring_Forward = 5,
-        Send_Backward = 6,
-        Show_HideGrid = 7,
-
-        Run_Last_Plugin = 8,
-        Text_Align_Left = 9,
-        Text_Align_Center = 10,
-        Text_Align_Right = 11,
-    };
-
 
 /* Figma
  * ,---------------------------.
@@ -42,12 +87,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 
-[_FIGMA] = KEYMAP(
-		M(Create_Components), M(Show_Components), M(Team_Library), M(Detach_Instance),
-		M(All_Layers), M(Bring_Forward), M(Send_Backward), M(Show_HideGrid),
-		M(Run_Last_Plugin), M(Text_Align_Left), M(Text_Align_Center), M(Text_Align_Right),
-		M(AUTOLAY), _______ , M(COPYPASTESTYLE), TO(_LAB)
-	),
+[_FIGMA] = LAYOUT(
+		CREATECOMPONENT, SHOWCOMPONENT, TEAMLIB, DETACH,
+		FIGLAYERS, BRINGFORW, SENDBACK, GRID,
+		RUNLASTPLUG, TEXTLEFT, TEXTCENTER, TEXTRIGHT,
+		{M_AUTOLAY}, _______ , {M_COPYSTYLES}, TO(_LAB)
+),
 
 /* Lab
  * ,---------------------------.
@@ -61,109 +106,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------'
  */
 
-[_LAB] = KEYMAP(
-		TO(_FIGMA), LOCKSCREEN, LOCKSCREEN, LOCKSCREEN,
-		LOCKSCREEN, AU_ON, AU_OFF, LOCKSCREEN,
-		LOCKSCREEN, LOCKSCREEN, LOCKSCREEN, LOCKSCREEN,
-		LOCKSCREEN , RESET, LOCKSCREEN, TO(_FIGMA)
+[_LAB] = LAYOUT(
+		_______, _______, _______, _______,
+		_______, AU_ON, AU_OFF, _______,
+		_______, _______, _______, _______,
+		LOCKSCREEN, _______, _______, TO(_FIGMA)
 	),
 };
 
-bool is_copied = false; // inner variable kopyalandı mı diye
 
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case COPYPASTESTYLE:
-      if(!is_copied) {
-        register_code(KC_LGUI(KC_LALT(KC_C)));
-        is_copied = true;
-      } else {
-        register_code(KC_LGUI(KC_LALT(KC_V)));
-        is_copied = false; // state'i sıfırla
-      }
-      break;
-
-	  case AUTOLAY:
-      if(!is_copied) {
-        register_code(KC_LSHIFT(KC_A));
-        is_copied = true;
-      } else {
-        register_code(KC_LGUI(KC_LSHIFT(KC_A)));
-        is_copied = false; // state'i sıfırla
-      }
-      break;
-  }
-  return true;
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-	keyevent_t event = record->event;
-
-	switch (id) {
-		case 0: // Create Component
-			if (record->event.pressed) {
-				return MACRO( T(LALT), T(LGUI), T(KC_K), END );
-			}
-			break;
-        case 1: // Show Components
-			if (record->event.pressed) {
-				return MACRO( T(LALT), T(KC_KP_2), END );
-			}
-            break;
-		case 2: // Team Library
-			if (record->event.pressed) {
-				return MACRO( T(LALT), T(LGUI), T(KC_O), END );
-			}
-			break;
-		case 3: // Detach Instance
-			if (record->event.pressed) {
-				return MACRO( T(LALT), T(LGUI), T(KC_B), END );
-			}
-			break;
-        case 4: // Send Backward
-			if (record->event.pressed) {
-				return MACRO( T(LGUI), T(KC_LBRACKET), END );
-			}
-			break;
-        case 5: // Bring Forward
-			if (record->event.pressed) {
-				return MACRO( T(LGUI), T(KC_RBRACKET), END );
-			}
-			break;
-        case 6: // Show/Hide Grid
-			if (record->event.pressed) {
-				return MACRO( T(LCTRL), T(KC_G), END );
-			}
-			break;
-        case 7: // Text Align Left
-			if (record->event.pressed) {
-				return MACRO( T(LALT), T(LGUI), T(KC_L), END );
-			}
-			break;
-        case 8: // Text Align Right
-			if (record->event.pressed) {
-				return MACRO( T(LALT), T(LGUI), T(KC_R), END );
-			}
-			break;
-        case 9: // Text Align Center
-			if (record->event.pressed) {
-				return MACRO( T(LALT), T(LGUI), T(KC_T), END );
-			}
-			break;
-        case 10: // Run Last Plugin
-			if (record->event.pressed) {
-				return MACRO( T(LGUI), T(LALT), T(KC_P), END );
-			}
-			break;
-        case 11: // Auto Layout
-			if (record->event.pressed) {
-				//RETURN
-			}
-			break;
-	}
-	return MACRO_NONE;
-}
 
 void matrix_init_user(void) {
 }
