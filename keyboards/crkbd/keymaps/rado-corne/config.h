b3b2807f5221d69fd1b06d01eb9640e5e8b61c6d
rado <radoosredkar@hotmail.com>
2020-09-26 12:03:52 +0200

    Tap dance for layers added

--- keyboards/crkbd/keymaps/rado-corne/config.h
+++ keyboards/crkbd/keymaps/rado-corne/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 500
+#define TAPPING_TERM 150
  
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_SLEEP
