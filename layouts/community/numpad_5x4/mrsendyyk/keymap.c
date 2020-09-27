4a2d1bb1ab3cc2458ab1db970e706600fc3baaec
Sendy YK <mr@sendyyk.com>
2020-09-28 02:57:53 +0700

    Update keymap.c

--- layouts/community/numpad_5x4/mrsendyyk/keymap.c
+++ layouts/community/numpad_5x4/mrsendyyk/keymap.c
@@ -15,7 +15,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-// Number Pad 5x4 layout and keymap by Sendy YK <https://mr.sendyyk.com>.
+// Number Pad 5x4 Layout and Keymap by Sendy YK <https://mr.sendyyk.com>.
 
 #include QMK_KEYBOARD_H
 
