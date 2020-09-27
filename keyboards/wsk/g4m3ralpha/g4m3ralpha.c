2bf7b7332ee9aa48391eafae44eb8bc44480a4e8
ridingqwerty <george.g.koenig@gmail.com>
2020-09-27 00:54:00 -0400

    Update keyboards/wsk/g4m3ralpha/g4m3ralpha.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/wsk/g4m3ralpha/g4m3ralpha.c
+++ keyboards/wsk/g4m3ralpha/g4m3ralpha.c
@@ -15,3 +15,25 @@
  */
 
 #include "g4m3ralpha.h"
+
+
+void matrix_init_kb(void) {
+    setPinOutput(D3);
+    writePinLow(D3);
+    setPinOutput(D2);
+    writePinLow(D2);
+    setPinOutput(D0);
+    writePinLow(D0);
+
+    matrix_init_user();
+};
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        writePin(D3, led_state.num_lock);
+        writePin(D0, led_state.caps_lock);
+        writePin(D2, led_state.scroll_lock);
+    }
+    return res;
+}
