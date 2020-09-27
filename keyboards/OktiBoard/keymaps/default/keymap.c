b112cc8626cbba7e4ef0b3d6f5b98b7bf634f804
Oktay <oktayelipek@gmail.com>
2020-09-27 12:53:53 +0300

    macros added.

--- keyboards/OktiBoard/keymaps/default/keymap.c
+++ keyboards/OktiBoard/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case M_AUTOLAY:
         if(!is_copied) {
-            SEND_STRING(AUTOLAYOUT);
+            SEND_STRING(SS_TAP(AUTOLAYOUT));
             is_copied = true;
         } else {
             SEND_STRING(RMLAYOUT);
@@ -59,15 +59,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
+// My layer names
+#define _FIGMA 0
+#define _LAB 1
+
+uint32_t layer_state_set_user(uint32_t state) {
+  switch (biton32(state)) {
     case _FIGMA:
-        rgblight_setrgb (148,0,211);
-        break;
+      rgblight_sethsv_noeeprom(100, 255, 255);
+      break;
     case _LAB:
-        rgblight_setrgb (255,218,185);
-        break;
-    }
+      rgblight_sethsv_noeeprom(100, 255, 255);
+      break;
+  }
   return state;
 }
 
@@ -90,12 +94,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		CREATECOMPONENT, SHOWCOMPONENT, TEAMLIB, DETACH,
 		FIGLAYERS, BRINGFORW, SENDBACK, GRID,
 		RUNLASTPLUG, TEXTLEFT, TEXTCENTER, TEXTRIGHT,
-		{M_AUTOLAY}, _______ , {M_COPYSTYLES}, TO(_LAB)
+		M_AUTOLAY, _______ , M_COPYSTYLES, TO(_LAB)
 ),
 
 /* Lab
  * ,---------------------------.
- * | FGM  |      |      | RESET|
+ * | FGM  |      |EEP_RS| RESET|
  * |------+------+------+------|
  * |      |AU_ON |AU_OFF|      |
  * |------+------+------+------|
@@ -106,56 +110,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_LAB] = LAYOUT(
-		_______, _______, _______, _______,
+		_______, _______, EEP_RST, RESET,
 		_______, AU_ON, AU_OFF, _______,
 		_______, _______, _______, _______,
 		LOCKSCREEN, _______, _______, TO(_FIGMA)
 	),
 };
-
-
-
-
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-
-	} else {
-
-	}
-
-}
