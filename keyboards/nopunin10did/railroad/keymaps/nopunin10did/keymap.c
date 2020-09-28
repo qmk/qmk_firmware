3a4d3c79383f858895bbacfab0d086e18f202cb9
nopunin10did <w.alex.ronke@gmail.com>
2020-09-28 14:17:26 -0400

    Update keyboards/nopunin10did/railroad/keymaps/nopunin10did/keymap.c
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/nopunin10did/railroad/keymaps/nopunin10did/keymap.c
+++ keyboards/nopunin10did/railroad/keymaps/nopunin10did/keymap.c
@@ -27,7 +27,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-KEYMAP(
+[0] = LAYOUT(
 /* Left Block */
    KC_ESC ,KC_PAUS,KC_ESC, KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,  
    KC_SLCK,KC_BSPC,    KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_6,
