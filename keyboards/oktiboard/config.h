1aa3de7ded1c9e31f28006453d415e89df6a96ce
Oktay <oktayelipek@gmail.com>
2020-09-29 20:38:06 +0300

    licence headers added and numpad deleted

--- keyboards/oktiboard/config.h
+++ keyboards/oktiboard/config.h
@@ -21,7 +21,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x2014
+#define VENDOR_ID       0x5454  // "TT" - Teletubbies
 #define PRODUCT_ID      0x0010
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Teletubbies
