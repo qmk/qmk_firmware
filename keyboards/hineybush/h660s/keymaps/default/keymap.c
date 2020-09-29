f49b42fd3e131e7dbeb1748347574c3384c23fc3
Josh Hinnebusch <joshhinnebusch@gmail.com>
2020-09-29 13:45:05 -0400

    Update keyboards/hineybush/h660s/keymaps/default/keymap.c
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/hineybush/h660s/keymaps/default/keymap.c
+++ keyboards/hineybush/h660s/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,                KC_ENT,
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,    KC_RSFT,                  KC_UP,
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                                      MO(1),    KC_RCTL,                     KC_LEFT, KC_DOWN, KC_RGHT),
-    [_FN] = LAYOUT_ansi_700_2u_bksp(
+    [_FN] = LAYOUT_66_ansi_rwkl(
     KC_TRNS,  RGB_TOG, RGB_MOD, BL_TOGG, BL_DEC,  BL_INC,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,        KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,        KC_TRNS,
     KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,                KC_TRNS,
