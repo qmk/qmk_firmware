5ecb1b2d39f5424979a1325e2642ac73db828c03
Keebz Dot Net <64744703+keebzdotnet@users.noreply.github.com>
2020-09-30 01:50:55 -0700

    Wazowski 23-19 VIA Support (#9730)
    * fixed keymap placement
    
    - online configurator keys were in the wrong location and needed to be fixed
    - fixed a key being to high on the y position
    
    * Create rules.mk
    
    * Create keymap.c
    
    * Update rules.mk
    
    added VIA support
    
    * Changed wazowski via keymap to include skipped switch
    
    * changed the vendor ID
    
    * Update keyboards/keebzdotnet/wazowski/config.h
    
    * Removed VIA enable from keyboard rules.mk
    
    * Update keyboards/keebzdotnet/wazowski/rules.mk
    
    * Fixed wazowski via keymap build errors
    
    * Added multi-layer support for Wazowski via keymap

--- keyboards/keebzdotnet/wazowski/config.h
+++ keyboards/keebzdotnet/wazowski/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xFEED
+#define VENDOR_ID 0x4B5A // "KZ"
 #define PRODUCT_ID 0x53FC
 #define DEVICE_VER 0x0001
 #define MANUFACTURER keebzdotnet
