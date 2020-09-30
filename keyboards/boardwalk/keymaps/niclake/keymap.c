0f223b4769f3e195225e617a5c76e3e78a485cf2
Nic Lake <nic@entretechno.com>
2020-09-29 19:51:41 -0500

    errant K, and _LOWER momentary toggles

--- keyboards/boardwalk/keymaps/niclake/keymap.c
+++ keyboards/boardwalk/keymaps/niclake/keymap.c
@@ -41,11 +41,11 @@ k    /* Base Layer (Colemak)
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT_ortho_2x2u(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC, KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-        MO(_RAISE), KC_A, KC_R,    KC_S,    KC_T,    KC_D,    KC_HOME, KC_PGUP, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LGUI, KC_??,   TT(_NUM),    KC_ENT,           KC_SPC,       KC_LEFT, KC_????, KC_RGUI, KC_RALT, KC_RCTL
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC, KC_RBRC, KC_J,    KC_L,       KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+        MO(_RAISE), KC_A, KC_R,    KC_S,    KC_T,    KC_D,    KC_HOME, KC_PGUP, KC_H,    KC_N,       KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_K,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LALT, KC_LGUI, KC_??,   TT(_NUM),    KC_ENT,           KC_SPC,       MO(_LOWER), KC_????, KC_RGUI, KC_RALT, KC_RCTL
     ),
 
     /* QWERTY
@@ -62,11 +62,11 @@ k    /* Base Layer (Colemak)
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_ortho_2x2u(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        MO(_RAISE), KC_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LGUI, KC_??,   TT(_NUM),    KC_ENT,           KC_SPC,       KC_LEFT, KC_????, KC_RGUI, KC_RALT, KC_RCTL
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,    KC_BSLS,
+        MO(_RAISE), KC_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_PGUP, KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LALT, KC_LGUI, KC_??,   TT(_NUM),    KC_ENT,           KC_SPC,       MO(_LOWER), KC_????, KC_RGUI, KC_RALT, KC_RCTL
     ),
 
     /* RAISE
