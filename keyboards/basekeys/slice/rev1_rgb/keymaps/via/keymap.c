6cc31083957858684e512eb91a06782c75803ac0
Nick Abe <57225836+2Moons-JP@users.noreply.github.com>
2020-09-30 17:56:46 +0900

    Update keymap.c
    Keymap Update2

--- keyboards/basekeys/slice/rev1_rgb/keymaps/via/keymap.c
+++ keyboards/basekeys/slice/rev1_rgb/keymaps/via/keymap.c
@@ -20,7 +20,7 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 enum layer_number {
-  _QWERTY = 0,
+  _QWERTY,
   _FN,
   _LOWER,
   _RAISE,
