0df38681f65719bd036ef31a9b2f7f7e4c9be895
Thomas Holt-Ronczy <twholt19@gmail.com>
2020-10-01 14:52:42 -0400

    Update keyboards/soy20/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/soy20/keymaps/default/keymap.c
+++ keyboards/soy20/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[0] = LAYOUT(
+	[0] = LAYOUT_ortho_5x4(
 		KC_PMNS, KC_PAST, KC_PSLS, MO(1),
 		KC_PPLS, KC_P9, KC_P8, KC_P7,
 		KC_TAB, KC_P5, KC_P6, KC_P4,
