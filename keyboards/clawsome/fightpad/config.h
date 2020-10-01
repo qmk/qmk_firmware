d86f188bd73b66c563bdaddcd25793882da4ddb3
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-10-01 17:58:52 -0600

    Update keyboards/clawsome/fightpad/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/clawsome/fightpad/config.h
+++ keyboards/clawsome/fightpad/config.h
@@ -25,8 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AlisGraveNil
 #define PRODUCT         FightPad
-#define DESCRIPTION     QMK Firmware: FightPad
-
 /* key matrix size */
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 7
