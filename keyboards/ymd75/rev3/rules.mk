641157b806ebea3a3f38c3d6f429475a7a6bc2ec
Jakob <s0muchfrsubtlety@gmail.com>
2020-10-01 16:36:45 +0000

    Update keyboards/ymd75/rev3/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ymd75/rev3/rules.mk
+++ keyboards/ymd75/rev3/rules.mk
@@ -18,10 +18,6 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-
-KEY_LOCK_ENABLE = yes	    # Enable KC_LOCK support
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
+KEY_LOCK_ENABLE = yes       # Enable KC_LOCK support
