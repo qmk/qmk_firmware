f9053cad6b67d86e2e7aa36af065686668a3c519
Peter Peterson <peter.peterson.iv@gmail.com>
2020-10-01 13:44:53 -0700

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/bm60poker/keymaps/ipetepete/keymap.c
+++ keyboards/bm60poker/keymaps/ipetepete/keymap.c
@@ -11,8 +11,7 @@ _spcfn
 
 
 enum keycodes {
-glight = SAFE_RANGE,
-gamer,
+gamer = SAFE_RANGE,
 qwerty,
 colemak
 };
@@ -22,7 +21,7 @@ colemak
 bool show_wasd = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_qwerty] = LAYOUT(
+    [_qwerty] = LAYOUT_60_ansi(
         KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
@@ -30,35 +29,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPFN,                             KC_RALT, KC_RCTL, MO(_fn),   MO(_rgb)
     ),
     // Gamer standard qwerty layout but with normal space for jumping etc
-    [_gamer] = LAYOUT(
+    [_gamer] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,                            KC_SPACE,                            _______, _______, _______, _______
     ),
-    [_colemak] = LAYOUT(
+    [_colemak] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
         _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  _______, _______, _______,
         _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT,          _______,
         _______,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    _______, _______, _______,           _______,
         _______, _______, _______,                            KC_SPFN,                            _______, _______, _______,  _______
     ),
-    [_fn] = LAYOUT(
+    [_fn] = LAYOUT_60_ansi(
         KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, KC_UP,   _______, _______, _______, KC_CALC, _______, KC_INS,  _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP,          _______,
         KC_MPLY,          KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, NK_TOGG, _______, _______, KC_END,  KC_PGDN,          KC_MNXT,
         qwerty, colemak, gamer,                            _______,                            _______, _______, _______, _______
     ),
-    [_rgb] = LAYOUT(
+    [_rgb] = LAYOUT_60_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______, _______, _______, RESET,
         _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
-    [_spcfn] = LAYOUT(
+    [_spcfn] = LAYOUT_60_ansi(
         _______, SGUI(1), SGUI(2), SGUI(3), SGUI(4), SGUI(5), SGUI(6), SGUI(7),  SGUI(8), SGUI(9), SGUI(0), _______,  _______, KC_DEL,
         _______, _______, _______, KC_PGUP, _______, KC_LBRC, KC_RBRC,  _______, KC_UP,   _______, _______, _______,  _______, _______,
         KC_CAPS, _______, _______, KC_PGDN, _______, KC_LCBR, KC_RCBR,  KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,          _______,
@@ -108,7 +107,7 @@ void rgb_matrix_layer_helper (uint8_t red, uint8_t green, uint8_t blue) {
 void rgb_matrix_indicators_kb(void)
 {
 	if (!g_suspend_state && rgb_matrix_config.enable) {
-        switch (biton32(layer_state)) {
+        switch (get_highest_layer(layer_state)) {
             //case _qwerty:
             //    rgb_matrix_set_color(26, 0xFF, 0x00, 0x00);
             //    break;
