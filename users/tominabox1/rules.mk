ba324ca63e4c6e6a73bd197694a51ad80de9ea38
fauxpark <fauxpark@gmail.com>
2020-10-02 03:35:28 +1000

    Remove commented out RGB_MATRIX_ENABLE

--- users/tominabox1/rules.mk
+++ users/tominabox1/rules.mk
@@ -19,7 +19,6 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes        # Audio control and System control
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality on B7 by default
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-# RGB_MATRIX_ENABLE = yes
 endif
 
 
