a706b9f221ab4b038c367d87398b1fb591036a1c
yiancar <yiangosyiangou@cytanet.com.cy>
2020-10-01 08:21:58 +0100

    Update keyboards/nebula68/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nebula68/config.h
+++ keyboards/nebula68/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Yiancar-Designs
 #define PRODUCT         NEBULA68
-#define DESCRIPTION     "A 68-percent, tool-free RGB keyboard"
 
 /* key matrix size */
 #define MATRIX_ROWS 5
