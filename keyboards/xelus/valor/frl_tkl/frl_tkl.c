a87f9403c3801e8c34bb5ccd8a5004a219191c45
Xelus22 <preyas22@gmail.com>
2020-10-03 15:02:11 +1000

    zvecr recommended changes

--- keyboards/xelus/valor/frl_tkl/frl_tkl.c
+++ keyboards/xelus/valor/frl_tkl/frl_tkl.c
@@ -22,5 +22,3 @@ void board_init(void) {
   SYSCFG->CFGR1 |= SYSCFG_CFGR1_I2C1_DMA_RMP;
   SYSCFG->CFGR1 &= ~(SYSCFG_CFGR1_SPI2_DMA_RMP);
 }
-
-void matrix_io_delay(void) { __asm__ volatile("nop\nnop\nnop\n"); }
