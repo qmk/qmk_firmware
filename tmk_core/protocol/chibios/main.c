e2e5a0fcdc2332e4489dac0df62008e9991b2f0b
LSChyi <alan81920@gmail.com>
2020-10-04 03:19:04 +0800

    Add hotfix for chibios keyboards not wake (#10088)

--- tmk_core/protocol/chibios/main.c
+++ tmk_core/protocol/chibios/main.c
@@ -236,6 +236,7 @@ int main(void) {
                 /* Remote wakeup */
                 if (suspend_wakeup_condition()) {
                     usbWakeupHost(&USB_DRIVER);
+                    restart_usb_driver(&USB_DRIVER);
                 }
             }
             /* Woken up */
