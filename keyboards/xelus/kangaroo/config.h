df7312fc1f4458c12726221165712862fae85610
Xelus22 <preyas22@gmail.com>
2020-10-03 15:07:30 +1000

    fix up config

--- keyboards/xelus/kangaroo/config.h
+++ keyboards/xelus/kangaroo/config.h
@@ -17,8 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include_next "usb_descriptor.h"
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x5845  // XE
 #define PRODUCT_ID      0x524F  // RO
