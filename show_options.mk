d591445d6db488380445809503f144725dd52de2
mtei <2170248+mtei@users.noreply.github.com>
2020-10-04 19:50:01 +0900

    add yaml_build_options target

--- show_options.mk
+++ show_options.mk
@@ -85,7 +85,12 @@ OTHER_OPTION_NAMES = \
   LTO_ENABLE
 
 define NAME_ECHO
-	@echo "  $1 = $($1)		# $(origin $1)"
+	@echo "  $1 = $(strip $($1))		# $(origin $1)"
+
+endef
+
+define YAML_NAME_ECHO
+	@echo '  $1 : "$(strip $($1))"'
 
 endef
 
@@ -128,3 +133,18 @@ show_full_features: show_build_options0
 	@echo "Other Options:"
 	$(foreach A_OPTION_NAME,$(sort $(OTHER_OPTION_NAMES)),\
 		$(call NAME_ECHO,$(A_OPTION_NAME)))
+
+.PHONY: yaml_build_options
+yaml_build_options:
+	@echo '- KEYBOARD : "$(KEYBOARD)"'
+	@echo '  KEYMAP : "$(KEYMAP)"'
+	@echo '  MCU : "$(MCU)"'
+	@echo '  MCU_SERIES : "$(MCU_SERIES)"'
+	@echo '  PLATFORM : "$(PLATFORM)"'
+	@echo '  FIRMWARE_FORMAT : "$(FIRMWARE_FORMAT)"'
+	$(foreach A_OPTION_NAME,$(sort $(BUILD_OPTION_NAMES)),\
+		$(call YAML_NAME_ECHO,$(A_OPTION_NAME)))
+	$(foreach A_OPTION_NAME,$(sort $(HARDWARE_OPTION_NAMES)),\
+		$(if $($(A_OPTION_NAME)),$(call YAML_NAME_ECHO,$(A_OPTION_NAME))))
+	$(foreach A_OPTION_NAME,$(sort $(OTHER_OPTION_NAMES)),\
+		$(if $($(A_OPTION_NAME)),$(call YAML_NAME_ECHO,$(A_OPTION_NAME))))
