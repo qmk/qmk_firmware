1aacbc44d18a1d23c58561ee17498618a1e6319d
Fred Silberberg <fred@silberberg.xyz>
2020-10-04 12:24:35 -0700

    [Keymap] Added 333fred 5x6_5 keymap (#10272)
    * First pass at manuform layout.
    
    * Updates from actual usage on my board. Fixed a couple of bugs, added a stormlight quote randomizer.
    
    * Use a 32-bit timer that can actually hit 5 minutes.
    
    * Some updates after a day of usage:
    
    * Turn off WPM. It's too inaccurate and takes up a decent amount of space that can be devoted to quotes.
    * Turn off the OLEDs after 5 minutes
    * Move the keys around a bit.
    * Add many more quotes.
    
    * Adjust the quotes to make room for missing and misswritten WoT quote. Adjusted the keymap slightly. Adjust the mods display to include one_shots.
    
    * Revert osm on lgui as it makes start menu search impossible to use.
    
    * Use LTO_ENABLE flag.
    
    * License all of my (333fred's) keymaps as MIT.
    
    * Update users/333fred/rgb.c
    
    Co-authored-by: Ryan <fauxpark@gmail.com>
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/333fred/config.h
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/333fred/config.h
@@ -22,7 +22,19 @@
 
 #pragma once
 
-#include QMK_KEYBOARD_CONFIG_H
+#define USE_SERIAL
+#define EE_HANDS
 
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
+
+#define OLED_DISPLAY_128X64
+
+#define ENCODERS_PAD_A { F4 }
+#define ENCODERS_PAD_B { F5 }
+
+#undef SOFT_SERIAL_PIN
+#define SOFT_SERIAL_PIN D2
+
+#undef OLED_TIMEOUT
+#define OLED_TIMEOUT 240000
