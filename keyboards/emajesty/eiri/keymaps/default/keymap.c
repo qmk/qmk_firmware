d4668a0f9099602f57565f6de6a322fd03c6e44f
EMajesty <deceit66@gmail.com>
2020-10-04 03:33:48 +0300

    Changes to default keymap

--- keyboards/emajesty/eiri/keymaps/default/keymap.c
+++ keyboards/emajesty/eiri/keymaps/default/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_LGUI, KC_LALT, KC_DEL,  KC_SPC,  MO(1),        MO(2),   KC_BSPC, KC_ENT, KC_RALT, KC_RGUI),
+                          KC_LGUI, KC_LALT, KC_SPC,  KC_DEL,  MO(1),        MO(2),   KC_ENT, KC_BSPC, KC_RALT, KC_RGUI),
     [1] = LAYOUT(
         KC_ESC,  _______, KC_UP,   _______, KC_HOME, KC_PGUP,                        _______, _______, _______, KC_LPRN, KC_RPRN, KC_GRV,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_PGDN,                        _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
