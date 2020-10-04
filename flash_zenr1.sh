7de07c58d4f54d4fa184fd6bf0a385251e4aeaa4
Casey Webster <casey.webster@gmail.com>
2020-10-04 15:55:11 -0500

    Update copyright notices and revert transport.c to master

--- flash_zenr1.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-bin/qmk flash -kb rgbkb/zen/rev1 -km cwebster2
--- keyboards/kyria/keymaps/cwebster2/config.h
+++ keyboards/kyria/keymaps/cwebster2/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Casey Webster <casey.webster@gmail.com>
+/* Copyright 2020 Casey Webster <casey@e1337.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
