73ad357d3987fd13b06823b7f1850629e2d7b3b5
monksoffunk <osamu@sho-k.com>
2020-10-05 18:01:07 +0900

    Change number of RGBLED

--- keyboards/aleth42/rev1/config.h
+++ keyboards/aleth42/rev1/config.h
@@ -62,7 +62,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN B3
  #ifdef RGB_DI_PIN
-   #define RGBLED_NUM 6
+   #define RGBLED_NUM 8
    #define RGBLIGHT_HUE_STEP 8
    #define RGBLIGHT_SAT_STEP 8
    #define RGBLIGHT_VAL_STEP 8
