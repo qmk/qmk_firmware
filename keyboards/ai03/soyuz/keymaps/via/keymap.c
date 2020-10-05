e4f412e8dad88c5f89af9c5058f3ec6bdd254bac
Sendy YK <mr@sendyyk.com>
2020-10-05 14:24:30 +0700

    Update license notice

--- keyboards/ai03/soyuz/keymaps/via/keymap.c
+++ keyboards/ai03/soyuz/keymaps/via/keymap.c
@@ -1,5 +1,7 @@
-/* Copyright (C) 2020 Sendy YK
- * <mr@sendyyk.com - https://github.com/mrsendyyk - https://mr.sendyyk.com>.
+/*
+ * ai03 Soyuz VIA Keymap
+ *
+ * Copyright (C) 2020 Sendy YK <https://mr.sendyyk.com>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +17,6 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-// ai03 Soyuz VIA Keymap
 #include QMK_KEYBOARD_H
 
 enum layer_names {
