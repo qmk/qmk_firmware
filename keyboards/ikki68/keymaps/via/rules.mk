83330009170d483801baf22f9f67053577daf23a
root <root@LAPTOP-GZH.localdomain>
2020-10-06 01:21:32 +0300

    add new keyboard ikki68

---
+++ keyboards/ikki68/keymaps/via/rules.mk
@@ -0,0 +1,8 @@
+# the following features be disabled for VIA support
+LEADER_ENABLE = no
+MOUSEKEY_ENABLE = no
+KEY_LOCK_ENABLE = no
+TERMINAL_ENABLE = no
+VIA_ENABLE = yes
+
+# MIDI_ENABLE and BLUETOOTH_ENABLE have already been disabled
