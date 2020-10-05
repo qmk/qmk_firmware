261fb54144b411141dc951eefd8e050bf89e6304
Casey Webster <casey.webster@gmail.com>
2020-10-04 19:26:16 -0500

    Use LTO_ENABLE to turn on Link_Time_Optimization

--- users/cwebster2/rules.mk
+++ users/cwebster2/rules.mk
@@ -7,8 +7,7 @@ GRAVE_ESC_ENABLE = no
 CONSOLE_ENABLE = no
 SPACE_CADET_ENABLE    = no
 GRAVE_ESC_ENABLE      = no
-Link_Time_Optimization = yes
-EXTRAFLAGS += -flto
+LTO_ENABLE = yes
 
 SRC += cwebster2.c
 SRC += keycodes.c
