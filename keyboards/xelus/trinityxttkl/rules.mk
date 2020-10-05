1f02d47c6e1aa800ae9fbcd81c91f31aa0b32442
Xelus22 <preyas22@gmail.com>
2020-10-05 15:16:03 +1100

    remove accidental

--- keyboards/xelus/trinityxttkl/rules.mk
+++ keyboards/xelus/trinityxttkl/rules.mk
@@ -15,5 +15,6 @@ NKRO_ENABLE = yes	    # USB Nkey Rollover
 BACKLIGHT_ENABLE = no
 RGBLIGHT_ENABLE = no
 
+
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
