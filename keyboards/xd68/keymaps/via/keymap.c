902ded7e400bb3aa23edacd97e86e089ee5ef8de
Dave Rowe <dave@roweware.com>
2020-10-06 15:28:18 -0500

    Adjust the VIA keymap to more closely match vendor
    The vendor for this board displays many typical layouts
    and supported keymaps.  The typical right side layout
    for this size of board would have ALT, FN, CTRL, rather
    than an 'App' key of unknown functionality.
    
    This adjusts the default for VIA enabled boards, such that
    someone getting started with this board and VIA would have
    a known base layout.

--- keyboards/xd68/keymaps/via/keymap.c
+++ keyboards/xd68/keymaps/via/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|End |
      * |----------------------------------------------------------------|
-     * |Ctrl|Win |Alt |        Space          |App|Ctrl|FN |Lef|Dow|Rig |
+     * |Ctrl|Win |Alt |        Space          |Alt|FN |Ctrl|Lef|Dow|Rig |
      * `----------------------------------------------------------------'
      */
     [0] = LAYOUT_65_ansi(
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_APP,  KC_RCTL, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Keymap _FL: Function Layer
