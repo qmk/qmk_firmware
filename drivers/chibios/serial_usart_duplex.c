7f699739b3af7c31032200e2e52e2dcf990a181e
Stefan Kerkmann <karlk90@pm.me>
2020-10-06 12:17:30 +0200

    Update Serial Usart Duplex Driver
    * Use Master / Slave Terminology until qmk_firmware/issues/9376 is decided
    * Enable usart rx tx pin swap using SERIAL_USART_PIN_SWAP define
    * Use SERIAL_USART_TX_PIN and SERIAL_USART_RX_PIN instead of SOFT_SERIAL_PIN

--- drivers/chibios/serial_usart_duplex.c
+++ drivers/chibios/serial_usart_duplex.c
@@ -25,19 +25,19 @@
 #endif
 
 #ifndef SERIAL_USART_CR2
-#    define SERIAL_USART_CR2 0x0  // 1 stop bits
+#    define SERIAL_USART_CR2 (USART_CR2_STOP_1)  // 2 stop bits
 #endif
 
 #ifndef SERIAL_USART_CR3
 #    define SERIAL_USART_CR3 0x0
 #endif
 
-#ifdef SERIAL_PIN_TX
-#    define SERIAL_USART_TX_PIN SERIAL_PIN_TX
+#ifndef SERIAL_USART_TX_PIN
+#    define SERIAL_USART_TX_PIN A9
 #endif
 
-#ifdef SERIAL_PIN_RX
-#    define SERIAL_USART_RX_PIN SERIAL_PIN_RX
+#ifndef SERIAL_USART_RX_PIN
+#    define SERIAL_USART_RX_PIN A10
 #endif
 
 #ifndef SELECT_SERIAL_SPEED
@@ -81,50 +81,54 @@ static UARTConfig uart_config = {NULL, NULL, NULL, NULL, NULL, (SERIAL_USART_SPE
 static SSTD_t* Transaction_table      = NULL;
 static uint8_t Transaction_table_size = 0;
 
-void handle_transactions_target(void);
+void handle_transactions_slave(void);
 
 __attribute__((weak)) void usart_init(void) {
 #if defined(USE_GPIOV1)
-    palSetLineMode(SERIAL_USART_TX_PIN, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
-    palSetLineMode(SERIAL_USART_RX_PIN, PAL_MODE_INPUT);
+    palSetPadMode(PAL_PORT(SERIAL_USART_TX_PIN), PAL_PAD(SERIAL_USART_TX_PIN), PAL_MODE_STM32_ALTERNATE_PUSHPULL);
+    palSetPadMode(PAL_PORT(SERIAL_USART_TX_PIN), PAL_PAD(SERIAL_USART_TX_PIN), PAL_MODE_INPUT);
 #else
-    palSetLineMode(SERIAL_USART_TX_PIN, PAL_MODE_ALTERNATE(SERIAL_USART_TX_PAL_MODE) | PAL_STM32_OTYPE_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-    palSetLineMode(SERIAL_USART_RX_PIN, PAL_STM32_MODE_INPUT);
+    palSetPadMode(PAL_PORT(SERIAL_USART_TX_PIN), PAL_PAD(SERIAL_USART_TX_PIN), PAL_MODE_ALTERNATE(SERIAL_USART_TX_PAL_MODE) | PAL_STM32_OTYPE_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(PAL_PORT(SERIAL_USART_TX_PIN), PAL_PAD(SERIAL_USART_TX_PIN), PAL_STM32_MODE_INPUT);
 #endif
 }
 
 /*
- * This thread runs on the target half and reacts to transactions init from the initiator.
+ * This thread runs on the slave half and reacts to transactions init from the master.
  */
-static THD_WORKING_AREA(waTargetThread, 512);
-static THD_FUNCTION(TargetThread, arg) {
+static THD_WORKING_AREA(waSlaveThread, 512);
+static THD_FUNCTION(SlaveThread, arg) {
     (void)arg;
-    chRegSetThreadName("target_usart_tx_rx");
+    chRegSetThreadName("slave_usart_tx_rx");
 
     while (true) {
-        handle_transactions_target();
+        handle_transactions_slave();
     }
 }
 
-void serial_target_init(SSTD_t* const sstd_table, int sstd_table_size) {
+void soft_serial_target_init(SSTD_t* const sstd_table, int sstd_table_size) {
     Transaction_table      = sstd_table;
     Transaction_table_size = (uint8_t)sstd_table_size;
     usart_init();
 
+#ifdef SERIAL_USART_PIN_SWAP
+    uart_config.cr2 |= ~USART_CR2_SWAP;  // slave has non-swapped TX/RX pins
+#endif
+
     uartStart(&SERIAL_USART_DRIVER, &uart_config);
-    chThdCreateStatic(waTargetThread, sizeof(waTargetThread), HIGHPRIO, TargetThread, NULL);
+    chThdCreateStatic(waSlaveThread, sizeof(waSlaveThread), HIGHPRIO, SlaveThread, NULL);
 }
 
 /**
- * @brief React to transactions started by the initiator.
+ * @brief React to transactions started by the master.
  * This version uses duplex send and receive usart pheriphals and DMA backed transfers.
  */
-void inline handle_transactions_target(void) {
+void inline handle_transactions_slave(void) {
     uint8_t sstd_index  = 0xFF;
     size_t  buffer_size = (size_t)sizeof(sstd_index);
     msg_t   msg         = 0;
 
-    /* Receive transaction table index from initiator, which doubles as basic handshake token. */
+    /* Receive transaction table index from master, which doubles as basic handshake token. */
     uartReceiveTimeout(&SERIAL_USART_DRIVER, &buffer_size, &sstd_index, TIME_MS2I(SERIAL_TIMEOUT_HANDSHAKE));
 
     if (sstd_index > Transaction_table_size) {
@@ -134,7 +138,7 @@ void inline handle_transactions_target(void) {
     SSTD_t* trans = &Transaction_table[sstd_index];
 
     /* Send back the handshake which is XORed as a simple checksum,
-     to signal that the target is ready to receive possible transaction buffers  */
+     to signal that the slave is ready to receive possible transaction buffers  */
     sstd_index ^= HANDSHAKE_MAGIC;
     buffer_size = (size_t)sizeof(sstd_index);
     msg         = uartSendTimeout(&SERIAL_USART_DRIVER, &buffer_size, &sstd_index, TIME_MS2I(SERIAL_TIMEOUT_HANDSHAKE));
@@ -146,7 +150,7 @@ void inline handle_transactions_target(void) {
         return;
     }
 
-    /* Receive transaction buffer from the initiator. If this transaction requires it.*/
+    /* Receive transaction buffer from the master. If this transaction requires it.*/
     buffer_size = (size_t)trans->initiator2target_buffer_size;
     if (buffer_size) {
         msg = uartReceiveTimeout(&SERIAL_USART_DRIVER, &buffer_size, trans->initiator2target_buffer, TIME_MS2I(SERIAL_TIMEOUT_BUFFER));
@@ -158,7 +162,7 @@ void inline handle_transactions_target(void) {
         }
     }
 
-    /* Send transaction buffer to the initiator. If this transaction requires it. */
+    /* Send transaction buffer to the master. If this transaction requires it. */
     buffer_size = (size_t)trans->target2initiator_buffer_size;
     if (buffer_size) {
         msg = uartSendFullTimeout(&SERIAL_USART_DRIVER, &buffer_size, trans->target2initiator_buffer, TIME_MS2I(SERIAL_TIMEOUT_BUFFER));
@@ -175,15 +179,20 @@ void inline handle_transactions_target(void) {
     }
 }
 
-void serial_initiator_init(SSTD_t* const sstd_table, int sstd_table_size) {
+void soft_serial_initiator_init(SSTD_t* const sstd_table, int sstd_table_size) {
     Transaction_table      = sstd_table;
     Transaction_table_size = (uint8_t)sstd_table_size;
     usart_init();
+
+#ifdef SERIAL_USART_PIN_SWAP
+    uart_config.cr2 |= ~USART_CR2_SWAP;  // master has non-swapped TX/RX pins
+#endif
+
     uartStart(&SERIAL_USART_DRIVER, &uart_config);
 }
 
 /**
- * @brief Start transaction from the initiator to the target.
+ * @brief Start transaction from the master to the slave.
  * This version uses duplex send and receive usart pheriphals and DMA backed transfers.
  *
  * @param index Transaction Table index of the transaction to start.
@@ -192,10 +201,10 @@ void serial_initiator_init(SSTD_t* const sstd_table, int sstd_table_size) {
  *             TRANSACTION_END in case of success.
  */
 #ifndef SERIAL_USE_MULTI_TRANSACTION
-int serial_transaction(void) {
+int soft_serial_transaction(void) {
     uint8_t sstd_index = 0;
 #else
-int serial_transaction(int index) {
+int soft_serial_transaction(int index) {
     uint8_t sstd_index = index;
 #endif
 
@@ -207,36 +216,36 @@ int serial_transaction(int index) {
     msg_t         msg         = 0;
     size_t        buffer_size = (size_t)sizeof(sstd_index);
 
-    /* Send transaction table index to the target, which doubles as basic handshake token. */
+    /* Send transaction table index to the slave, which doubles as basic handshake token. */
     uartSendFullTimeout(&SERIAL_USART_DRIVER, &buffer_size, &sstd_index, TIME_MS2I(SERIAL_TIMEOUT_HANDSHAKE));
 
     uint8_t sstd_index_shake = 0xFF;
     buffer_size              = (size_t)sizeof(sstd_index_shake);
 
-    /* Receive the handshake token from the target. The token was XORed by the target as a simple checksum.
-     If the tokens match, the initiator will start to send and receive possible transaction buffers. */
+    /* Receive the handshake token from the slave. The token was XORed by the slave as a simple checksum.
+     If the tokens match, the master will start to send and receive possible transaction buffers. */
     msg = uartReceiveTimeout(&SERIAL_USART_DRIVER, &buffer_size, &sstd_index_shake, TIME_MS2I(SERIAL_TIMEOUT_HANDSHAKE));
     if (msg != MSG_OK || (sstd_index_shake != (sstd_index ^ HANDSHAKE_MAGIC))) {
-        dmsg("Handshake Failed");
+        dprintln("USART: Handshake Failed");
         return TRANSACTION_NO_RESPONSE;
     }
 
-    /* Send transaction buffer to the target. If this transaction requires it. */
+    /* Send transaction buffer to the slave. If this transaction requires it. */
     buffer_size = (size_t)trans->initiator2target_buffer_size;
     if (buffer_size) {
         msg = uartSendFullTimeout(&SERIAL_USART_DRIVER, &buffer_size, trans->initiator2target_buffer, TIME_MS2I(SERIAL_TIMEOUT_BUFFER));
         if (msg != MSG_OK) {
-            dmsg("Send Failed");
+            dprintln("USART: Send Failed");
             return TRANSACTION_NO_RESPONSE;
         }
     }
 
-    /* Receive transaction buffer from the target. If this transaction requires it. */
+    /* Receive transaction buffer from the slave. If this transaction requires it. */
     buffer_size = (size_t)trans->target2initiator_buffer_size;
     if (buffer_size) {
         msg = uartReceiveTimeout(&SERIAL_USART_DRIVER, &buffer_size, trans->target2initiator_buffer, TIME_MS2I(SERIAL_TIMEOUT_BUFFER));
         if (msg != MSG_OK) {
-            dmsg("Receive Failed");
+            dprintln("USART: Receive Failed");
             return TRANSACTION_NO_RESPONSE;
         }
     }
