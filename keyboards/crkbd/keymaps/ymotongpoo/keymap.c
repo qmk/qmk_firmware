af8c02fb594b148d4e3318ccfa496ec7518127a3
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-10-07 00:13:50 +0900

    crkbd: Change LGUI

--- keyboards/crkbd/keymaps/ymotongpoo/keymap.c
+++ keyboards/crkbd/keymaps/ymotongpoo/keymap.c
@@ -19,8 +19,6 @@ extern uint8_t is_master;
 #define _RAISE 4
 #define _ADJUST 5
 
-#define QOT_GUI LGUI_T(KC_QUOT)
-
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
@@ -43,32 +41,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LGUI,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER,  RAISE,      KC_ENT,  KC_SPC, KC_RALT \
                                       //`--------------------------'  `--------------------------'
 
   ),
 
-  [_COLEMAK] = LAYOUT(
+  [_COLEMAK] = LAYOUT_split_3x6_3(
   //+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
-       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,  \
+       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                         KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, \
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LGUI, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER,  RAISE,      KC_ENT,  KC_SPC, KC_RALT \
   //                                    +--------+--------+--------+  +--------+--------+--------+
   ),
 
-  [_EUCALYN] = LAYOUT(
+  [_EUCALYN] = LAYOUT_split_3x6_3(
   //+--------+--------+--------+--------+--------+--------+                    +--------+--------+--------+--------+--------+--------+
        KC_TAB,    KC_Q,    KC_W, KC_COMM,  KC_DOT, KC_SCLN,                         KC_M,    KC_R,    KC_D,    KC_Y,    KC_P, KC_BSPC, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_A,    KC_O,    KC_E,    KC_I,    KC_U,                            KC_G,    KC_T,    KC_K,    KC_S,    KC_N, KC_QUOT, \
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_F,                            KC_B,    KC_H,    KC_J,    KC_L, KC_SLSH, KC_RSFT, \
+      KC_LCTL,    KC_A,    KC_O,    KC_E,    KC_I,    KC_U,                         KC_G,    KC_T,    KC_K,    KC_S,    KC_N, KC_QUOT, \
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_F,                         KC_B,    KC_H,    KC_J,    KC_L, KC_SLSH, KC_LGUI, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER,   RAISE,    KC_ENT,   KC_SPC, KC_RALT \
   //                                    +--------+--------+--------+  +--------+--------+--------+
