2f3ef20faffa39f47f2315757ae8ed1bd7fd3d08
fauxpark <fauxpark@gmail.com>
2020-10-06 18:06:11 +1100

    Add STM32Duino to bootloader selection

--- keyboards/wolfmarkclub/wm1/rules.mk
+++ keyboards/wolfmarkclub/wm1/rules.mk
@@ -1,11 +1,13 @@
 # MCU name
 MCU = STM32F103
 
+# Bootloader selection
+BOOTLOADER = stm32duino
+
 # GENERIC STM32F103C8T6 board - mass storage bootloader
 MCU_LDSCRIPT = wm1_f103
-BOARD = STM32_F103_STM32DUINO
 
-PROGRAM_CMD=echo 'CLI flashing not supported' >&2
+PROGRAM_CMD = echo 'CLI flashing not supported' >&2
 
 # Build Options
 #   change yes to no to disable
