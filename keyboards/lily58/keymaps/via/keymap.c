975652237587f89319c4281fa84c1259789cf59d
Adam Gwilliam <agwilliam@yaharasoftware.com>
2020-10-06 21:12:05 -0500

    pass one on oled updates

--- keyboards/lily58/keymaps/via/keymap.c
+++ keyboards/lily58/keymaps/via/keymap.c
@@ -97,51 +97,125 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
   )
 };
-
-
-
-//SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
-#ifdef OLED_DRIVER_ENABLE
-
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master())
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
   return rotation;
 }
 
-// When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
+// int keylogs_row;
+// int keylogs_col;
+// int keylogs_keycode;
+// int keylogs_name;
+// char keylog_str[24] = {};
+// char keylogs_str[21] = {};
+// int keylogs_str_idx = 0;
 
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
+// const char location_to_string[10][6][3] PROGMEM = {
+//     {"0x0", "0x1", "0x2", "0x3", "0x4", "0x5"},
+//     {"1x0", "1x1", "1x2", "1x3", "1x4", "1x5"},
+//     {"2x0", "2x1", "2x2", "2x3", "2x4", "2x5"},
+//     {"3x0", "3x1", "3x2", "3x3", "3x4", "3x5"},
+//     {"4x0", "4x1", "4x2", "4x3", "4x4", "4x5"},
+//     {"5x0", "5x1", "5x2", "5x3", "5x4", "5x5"},
+//     {"6x0", "6x1", "6x2", "6x3", "6x4", "6x5"},
+//     {"7x0", "7x1", "7x2", "7x3", "7x4", "7x5"},
+//     {"8x0", "8x1", "8x2", "8x3", "8x4", "8x5"},
+//     {"9x0", "9x1", "9x2", "9x3", "9x4", "9x5"}};
+
+// const char code_to_number[60][2] PROGMEM = {
+//     "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+//     "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+//     "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+//     "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+//     "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+//     "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"};
+
+// const char code_to_name[60] PROGMEM = {
+//     ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
+//     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+//     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+//     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+//     'R', 'E', 'B', 'T', ' ', ' ', ' ', ' ', ' ', ' ',
+//     ' ', ';', '\'', ' ', ',', '.', '/', ' ', ' ', ' '};
+
+// void set_keylog(uint16_t keycode, keyrecord_t *record) {
+// //   //char name = ' ';
+// //   if (keycode < 60) {
+// //     keylogs_name = code_to_name[keycode];
+// //   }
+//   keylogs_row = record->event.key.row;
+//   keylogs_col = record->event.key.col;
+//   keylogs_keycode = keycode;
+
+// //   // update keylog
+// //   printf("%dx%d, k%2d : %c\n",
+// //            keylogs_row, keylogs_col,
+// //            keylogs_keycode, keylogs_name);
+
+//   // update keylogs
+// //   if (keylogs_str_idx == sizeof(keylogs_str) - 1) {
+// //     keylogs_str_idx = 0;
+// //     for (int i = 0; i < sizeof(keylogs_str) - 1; i++) {
+// //       keylogs_str[i] = ' ';
+// //     }
+// //   }
+
+// //   keylogs_str[keylogs_str_idx] = keylogs_name;
+// //   keylogs_str_idx++;
+// }
+
+// const char *read_keylog(void) {
+//   return keylog_str;
+// }
+
+// const char *read_keylogs(void) {
+//   return keylogs_str;
+// }
 
 void oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-    oled_write(read_logo(), false);
-  }
-}
-#endif // OLED_DRIVER_ENABLE
+    switch (layer_state)
+    {
+    case 0:
+        oled_write_ln_P(PSTR("Layer: Default"), false);
+        break;
+    case (1 << 2):
+        oled_write_ln_P(PSTR("Layer: Raise"), false);
+        break;
+    case (1 << 1):
+        oled_write_ln_P(PSTR("Layer: Lower"), false);
+        break;
+    case (1 << 3):
+    case ((1 << 3) | (1 << 2) | (1 << 1)):
+        oled_write_ln_P(PSTR("Layer: Adjust"), false);
+        break;
+    default:
+        oled_write_ln_P(PSTR("Layer: Undefined"), false);
+    }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef OLED_DRIVER_ENABLE
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
+    // oled_write_P((char *)pgm_read_word(&(location_to_string[keylogs_row][keylogs_col])), false);
+    // oled_write_P(PSTR(", k"), false);
+    // oled_write_P((char *)pgm_read_word(&(code_to_number[keylogs_keycode])), false);
+    // oled_write_P(PSTR(" : "), false);
+    // if (keylogs_keycode < 60) {
+    //     oled_write_ln_P((char *)pgm_read_word(&(code_to_name[keylogs_keycode])), false);
+    // }
+
+    // oled_write_ln_P(keylogs_row + "x" + keylogs_col + ", k" + keylogs_keycode + " : " + keylogs_name, false);
+    // oled_write_ln_P(read_keylogs(), false);
+  } else {
+    oled_write_P(PSTR("\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\x0"), false);
   }
-  return true;
 }
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   if (record->event.pressed) {
+// #ifdef OLED_DRIVER_ENABLE
+//     set_keylog(keycode, record);
+// #endif
+//     // set_timelog();
+//   }
+//   return true;
+// }
