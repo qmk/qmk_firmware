465f68d50fcba47a8473585081e1d8852dd84f66
Flatmasterflex <33593585+Flatmasterflex@users.noreply.github.com>
2020-10-07 18:20:43 +0100

    Update keyboards/hacro_8/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/hacro_8/keymaps/default/keymap.c
+++ keyboards/hacro_8/keymaps/default/keymap.c
@@ -1,7 +1,8 @@
 #include QMK_KEYBOARD_H
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(
-	    KC_Q, KC_W, KC_E, KC_R,
-		KC_A, KC_S, KC_D, KC_F  
-	)
+    [0] = LAYOUT(
+        KC_Q, KC_W, KC_E, KC_R,
+        KC_A, KC_S, KC_D, KC_F
+    )
 };
