d587b8b6ed3bca6175345a749434f55c42e646b3
Yoshi Yamaguchi <145104+ymotongpoo@users.noreply.github.com>
2020-10-07 10:24:57 +0900

    helix: Add ymotongpoo's 5row config

---
+++ keyboards/helix/rev2/keymaps/ymotongpoo_5rows/rules.mk
@@ -0,0 +1,24 @@
+# QMK Standard Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+#   See TOP/keyboards/helix/rules.mk for a list of options that can be set.
+#   See TOP/docs/config_options.md for more information.
+#
+LINK_TIME_OPTIMIZATION_ENABLE = no  # if firmware size over limit, try this option
+
+# Helix Spacific Build Options
+# you can uncomment and edit follows 7 Variables
+#  jp: 以下の7つの変数を必要に応じて編集し、コメントアウトをはずします。
+HELIX_ROWS = 4              # Helix Rows is 4 or 5
+OLED_ENABLE = yes            # OLED_ENABLE
+# LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" insted of "common/glcdfont.c"
+LED_BACK_ENABLE = no        # LED backlight (Enable WS2812 RGB underlight.)
+LED_UNDERGLOW_ENABLE = yes   # LED underglow (Enable WS2812 RGB underlight.)
+LED_ANIMATIONS = yes        # LED animations
+# IOS_DEVICE_ENABLE = no      # connect to IOS device (iPad,iPhone)
+EXTRAKEY_ENABLE = yes       # Add control for media keys
+
+# convert Helix-specific options (that represent combinations of standard options)
+#   into QMK standard options.
+include $(strip $(KEYBOARD_LOCAL_FEATURES_MK))
