ed3d2dce384f182e2ec634fdc27a06b75cd330c2
Ruari <ruari.armstrong@gmail.com>
2020-10-07 12:13:19 +0100

    Change 260 Product ID from 60 to 260

--- keyboards/noxary/260/config.h
+++ keyboards/noxary/260/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4E58
-#define PRODUCT_ID      0x003C
+#define PRODUCT_ID      0x0104
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Noxary
 #define PRODUCT         260
