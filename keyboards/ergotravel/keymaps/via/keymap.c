e917f91517b0c09c49d9df8fc656be6fa394fd8f
Harshit From Home <harshitgoel96@yahoo.com>
2020-10-08 19:18:34 +0530

    Reset hard to last clean commit, redo changes for Hex Translation and Enum

--- keyboards/ergotravel/keymaps/via/keymap.c
+++ keyboards/ergotravel/keymaps/via/keymap.c
@@ -1,10 +1,28 @@
+ /* Copyright HarshitGoel96 2020
+  * With permission form jpconstantineau, the original maintainer of the Ergo Travel hardware.
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
 #include QMK_KEYBOARD_H
 
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+enum layer_names {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
