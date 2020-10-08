ed59594640279f203b2e266cf07b7ee70e72b064
Spaceman <rionlion100@gmail.com>
2020-10-08 16:30:48 -0400

    Update keyboards/spaceman/pancake/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/spaceman/pancake/config.h
+++ keyboards/spaceman/pancake/config.h
@@ -18,7 +18,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x5342 // SB 
-#define PRODUCT_ID      0x504b // PK
+#define PRODUCT_ID      0x504B // PK
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Spaceman
 #define PRODUCT         Pancake
