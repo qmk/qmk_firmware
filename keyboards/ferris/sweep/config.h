6359876295eff5538d6ec5c02c8035ab46673eaa
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-10-08 08:30:10 +0100

    Fix indentation in sweep's config
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ferris/sweep/config.h
+++ keyboards/ferris/sweep/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    DPB
 #define PRODUCT         Ferris sweep
-#define DESCRIPTION     A minimalistic 34 - keys split keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 8
