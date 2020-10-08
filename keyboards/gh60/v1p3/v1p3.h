3deb26a599603d05023437b00d8a51f180b9602f
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-10-08 07:57:46 -0700

    add LAYOUT_60_ansi
    I'm guessing at this based on a photo of the PCB. Chances are high that this isn't fully functional.

--- keyboards/gh60/v1p3/v1p3.h
+++ keyboards/gh60/v1p3/v1p3.h
@@ -30,3 +30,17 @@
     { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,   K3E,   K3F   }, \
     { K40,   K41,   K42,   K43,   K44,   K45,   KC_NO, KC_NO, K48,   K49,   K4A,   K4B,   K4C,   K4D,   K4E,   K4F   }, \
 }
+
+#define LAYOUT_60_ansi( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C,    K0E  \
+    K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,  K1D, \
+    K20,    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D, \
+    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,         K3D, \
+    K40,  K41,  K43,               K48,               K4A,  K4B,  K4D,  K4F  \
+) { \
+    { K00, K01,   K02,   K03, K04,   K05,   K06,   K07,   K08, K09,   K0A, K0B, K0C,   KC_NO, K0E,   KC_NO }, \
+    { K10, K11,   K12,   K13, K14,   K15,   K16,   K17,   K18, K19,   K1A, K1B, K1C,   K1D,   KC_NO, KC_NO }, \
+    { K20, K21,   K22,   K23, K24,   K25,   K26,   K27,   K28, K29,   K2A, K2B, KC_NO, K2D,   KC_NO, KC_NO }, \
+    { K30, KC_NO, K32,   K33, K34,   K35,   K36,   K37,   K38, K39,   K3A, K3B, KC_NO, K3D,   KC_NO, KC_NO }, \
+    { K40, K41,   KC_NO, K43, KC_NO, KC_NO, KC_NO, KC_NO, K48, KC_NO, K4A, K4B, KC_NO, K4D,   KC_NO, K4F   }, \
+}
