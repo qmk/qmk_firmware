4018c5daa67b87c6da719e64a90cc5b74723f840
Florian Didron <0x6664@hey.com>
2020-10-09 17:26:47 +0900

    feat: adds user locale contributions

---
+++ quantum/keymap_extras/keymap_contributions.h
@@ -0,0 +1,15 @@
+/* If you’d like to contribute a locale, we've created a google sheet:
+ * https://docs.google.com/spreadsheets/d/1gSB063BTHLTNLSGalzkhqz82RdBRVQytXKlabhX5Zcg/edit?usp=sharing
+ * Get in touch with us over email <contact@zsa.io> and we can help you get started!
+ */
+
+//Polish programmer, contributed by Krzysztof Bogacz.
+#define PL_01 ALGR(KC_Z) // ż
+#define PL_02 ALGR(KC_X) // ź
+#define PL_03 ALGR(KC_S) // ś
+#define PL_04 ALGR(KC_O) // ó
+#define PL_05 ALGR(KC_N) // ń
+#define PL_06 ALGR(KC_L) // ł
+#define PL_07 ALGR(KC_E) // ę
+#define PL_08 ALGR(KC_C) // ć
+#define PL_09 ALGR(KC_A) // ą
