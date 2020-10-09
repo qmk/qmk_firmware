abe73f324409d22cb981691e3aec6eef009db2ee
cmdremily <68452184+cmdremily@users.noreply.github.com>
2020-10-09 20:08:54 +0200

    Fix the suggested edits to compile again.

--- keyboards/opendeck/32/rev1/rev1.c
+++ keyboards/opendeck/32/rev1/rev1.c
@@ -66,10 +66,10 @@ const is31_led g_is31_leds[DRIVER_LED_TOTAL] = {
 // clang-format off
 led_config_t g_led_config = {
     // Key Matrix to LED Index
-    LAYOUT(0, 1, 2, 3, 4, 5, 6, 7,
-	      8, 9, 10, 11, 12, 13, 14, 15,
-	      16, 17, 18, 19, 20, 21, 22, 23,
-	      24, 25, 26, 27, 28, 29, 30, 31),
+    LAYOUT_ortho_4x8(0, 1, 2, 3, 4, 5, 6, 7,
+		     8, 9, 10, 11, 12, 13, 14, 15,
+		     16, 17, 18, 19, 20, 21, 22, 23,
+		     24, 25, 26, 27, 28, 29, 30, 31),
     {
         // LED Index to Physical Position
         {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0},	{7, 0},
