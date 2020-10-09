3e0fcf4b9c1356b45c05ec7d2a6f8ce27ced8789
Cmdr. Emily <68452184+cmdremily@users.noreply.github.com>
2020-10-09 20:03:56 +0200

    Apply suggestions from code review
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/opendeck/32/rev1/rev1.h
+++ keyboards/opendeck/32/rev1/rev1.h
@@ -31,14 +31,15 @@
 //#define KC_WRAPPER_KEY KC_F24
 
 // clang-format off
-#define LAYOUT(k00, k01, k02, k03, k04, k05, k06, k07, \
-               k08, k09, k0A, k0B, k0C, k0D, k0E, k0F, \
-               k10, k11, k12, k13, k14, k15, k16, k17, \
-               k18, k19, k1A, k1B, k1C, k1D, k1E, k1F) \
-{ \
-    {k00, k01, k02, k03, k04, k05, k06, k07}, \
-    {k08, k09, k0A, k0B, k0C, k0D, k0E, k0F}, \
-    {k10, k11, k12, k13, k14, k15, k16, k17}, \
-    {k18, k19, k1A, k1B, k1C, k1D, k1E, k1F}  \
+#define LAYOUT_ortho_4x8( \
+    Q00, Q01, Q02, Q03, Q04, Q05, Q06, Q07, \
+    Q10, Q11, Q12, Q13, Q14, Q15, Q16, Q17, \
+    Q20, Q21, Q22, Q23, Q24, Q25, Q26, Q27, \
+    Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37  \
+) { \
+    {Q00, Q01, Q02, Q03, Q04, Q05, Q06, Q07}, \
+    {Q10, Q11, Q12, Q13, Q14, Q15, Q16, Q17}, \
+    {Q20, Q21, Q22, Q23, Q24, Q25, Q26, Q27}, \
+    {Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37}  \
 }
 // clang-format on
--- keyboards/opendeck/32/rev1/rules.mk
+++ keyboards/opendeck/32/rev1/rules.mk
@@ -1,32 +1,28 @@
 # MCU name
 MCU = atmega32u4
+
+# Processor frequency
 F_CPU = 8000000
 
 # Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = atmel-dfu
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = no 	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no    # Mouse keys
-EXTRAKEY_ENABLE = no 	# Audio control and System control
-CONSOLE_ENABLE = no  	# Console for debug
-COMMAND_ENABLE = no     # Commands for debug and configuration
-NKRO_ENABLE = yes       # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = no    # Enable keyboard underlight functionality
-MIDI_ENABLE = no 	# MIDI controls
-UNICODE_ENABLE = no 	# Unicode
-BLUETOOTH_ENABLE = no   # Enable Bluetooth with the Adafruit EZ-Key HID
-UNICODE_ENABLE = no
-LTO_ENABLE = yes
+BOOTMAGIC_ENABLE = no 	    # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no 	    # Audio control and System control
+CONSOLE_ENABLE = no  	    # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = IS31FL3731
+
+LTO_ENABLE = yes
