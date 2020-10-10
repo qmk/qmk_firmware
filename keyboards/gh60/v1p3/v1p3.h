305f719bdcf147a9f539de28000398bb9401b38f
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-10-10 00:05:11 -0700

    add Tsangan, HHKB, and 64_ansi layout macros

--- keyboards/gh60/v1p3/v1p3.h
+++ keyboards/gh60/v1p3/v1p3.h
@@ -123,3 +123,45 @@
     { K30, KC_NO, K32,   K33, K34,   K35,   K36,   K37,   K38, K39,   K3A, KC_NO, K3C,   K3D,   KC_NO, K3F   }, \
     { K40, K41,   KC_NO, K43, KC_NO, KC_NO, KC_NO, KC_NO, K48, KC_NO, K4A, K4B,   K4C,   KC_NO, K4E,   K4F   }, \
 }
+
+#define LAYOUT_60_ansi_tsangan( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C,    K0E, \
+    K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,  K1D, \
+    K20,    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D, \
+    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,         K3D, \
+    K40,   K41,   K43,                K48,                K4B,   K4D,   K4F  \
+) { \
+    { K00, K01,   K02,   K03, K04,   K05,   K06,   K07,   K08, K09,   K0A,   K0B, K0C,   KC_NO, K0E,   KC_NO }, \
+    { K10, K11,   K12,   K13, K14,   K15,   K16,   K17,   K18, K19,   K1A,   K1B, K1C,   K1D,   KC_NO, KC_NO }, \
+    { K20, K21,   K22,   K23, K24,   K25,   K26,   K27,   K28, K29,   K2A,   K2B, KC_NO, K2D,   KC_NO, KC_NO }, \
+    { K30, KC_NO, K32,   K33, K34,   K35,   K36,   K37,   K38, K39,   K3A,   K3B, KC_NO, K3D,   KC_NO, KC_NO }, \
+    { K40, K41,   KC_NO, K43, KC_NO, KC_NO, KC_NO, KC_NO, K48, KC_NO, KC_NO, K4B, KC_NO, K4D,   KC_NO, K4F   }, \
+}
+
+#define LAYOUT_60_hhkb( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0F, \
+    K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,    K1D, \
+    K20,    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,        K2D, \
+    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3E, K3F, \
+           K41,  K43,                   K48,                   K4B,  K4D       \
+) { \
+    { K00,   K01,   K02,   K03, K04,   K05,   K06,   K07,   K08, K09,   K0A,   K0B, K0C,   K0D,   KC_NO, K0F   }, \
+    { K10,   K11,   K12,   K13, K14,   K15,   K16,   K17,   K18, K19,   K1A,   K1B, K1C,   K1D,   KC_NO, KC_NO }, \
+    { K20,   K21,   K22,   K23, K24,   K25,   K26,   K27,   K28, K29,   K2A,   K2B, KC_NO, K2D,   KC_NO, KC_NO }, \
+    { K30,   KC_NO, K32,   K33, K34,   K35,   K36,   K37,   K38, K39,   K3A,   K3B, KC_NO, KC_NO, K3E,   K3F   }, \
+    { KC_NO, K41,   KC_NO, K43, KC_NO, KC_NO, KC_NO, KC_NO, K48, KC_NO, KC_NO, K4B, KC_NO, K4D,   KC_NO, KC_NO }, \
+}
+
+#define LAYOUT_64_ansi( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C,      K0E, \
+    K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,    K1D, \
+    K20,    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,        K2D, \
+    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3F, \
+    K40,  K41,  K43,               K48,               K4A, K4B, K4C, K4E, K4F  \
+) { \
+    { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   KC_NO, K0E,   KC_NO }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D,   KC_NO, KC_NO }, \
+    { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   KC_NO, K2D,   KC_NO, KC_NO }, \
+    { K30,   KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,   KC_NO, K3F   }, \
+    { K40,   K41,   KC_NO, K43,   KC_NO, KC_NO, KC_NO, KC_NO, K48,   KC_NO, K4A,   K4B,   K4C,   KC_NO, K4E,   K4F   }, \
+}
