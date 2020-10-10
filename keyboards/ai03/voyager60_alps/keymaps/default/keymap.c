da68c63b63fc24fe92b70b7a3a03434724da1812
Taylor Walla <taylor.j.walla@gmail.com>
2020-10-09 23:20:49 -0500

    Update keyboards/ai03/voyager60_alps/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ai03/voyager60_alps/keymaps/default/keymap.c
+++ keyboards/ai03/voyager60_alps/keymaps/default/keymap.c
@@ -30,15 +30,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______,          _______,          _______,          _______,                   _______, _______, _______, _______
 	)
 };
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
