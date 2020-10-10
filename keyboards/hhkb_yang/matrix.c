707847acf4e3826c969b41d2d070ce6ab293cf63
Kan-Ru Chen <kanru@kanru.info>
2020-10-10 17:19:36 +0900

    [Keyboard] Implement new matrix scan logic for HHKB
    Based on the explanation https://github.com/tomsmalley/custom-topre-guide

--- keyboards/hhkb_yang/matrix.c
+++ keyboards/hhkb_yang/matrix.c
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // matrix power saving
 #define MATRIX_POWER_SAVE 10000
+#define RELAX_TIME_US 5
+#define ADC_READ_TIME_US 5
 static uint32_t matrix_last_modified = 0;
 
 // matrix state buffer(1:on, 0:off)
@@ -29,8 +31,8 @@ static matrix_row_t *matrix_prev;
 static matrix_row_t  _matrix0[MATRIX_ROWS];
 static matrix_row_t  _matrix1[MATRIX_ROWS];
 
-static inline void key_enable(void) { writePinLow(B6); }
-static inline void key_unable(void) { writePinHigh(B6); }
+static inline void key_strobe_high(void) { writePinLow(B6); }
+static inline void key_strobe_low(void) { writePinHigh(B6); }
 static inline bool key_state(void) { return readPin(D7); }
 static inline void key_prev_on(void) { writePinHigh(B7); }
 static inline void key_prev_off(void) { writePinLow(B7); }
@@ -38,7 +40,8 @@ static inline bool key_power_state(void) { return true; }
 static inline void key_power_on(void) {}
 static inline void key_power_off(void) {}
 
-static inline void key_select(uint8_t row, uint8_t col) { PORTB = (PORTB & 0xC0) | (((col)&0x07) << 3) | ((row)&0x07); }
+static inline void key_select_row(uint8_t row) { PORTB = (PORTB & 0b11111000) | ((row)&0b111); }
+static inline void key_select_col(uint8_t col) { PORTB = (PORTB & 0b11000111) | (((col)&0b111) << 3); }
 static inline bool key_prev_was_on(uint8_t row, uint8_t col) { return matrix_prev[row] & (1 << col); }
 
 void matrix_init(void) {
@@ -47,7 +50,6 @@ void matrix_init(void) {
     for (uint8_t i = 0; i < MATRIX_ROWS; i++) _matrix1[i] = 0x00;
     matrix      = _matrix0;
     matrix_prev = _matrix1;
-    matrix_last_modified = timer_read32();
 
     matrix_init_quantum();
 }
@@ -66,24 +68,25 @@ uint8_t matrix_scan(void) {
         key_power_on();
     }
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-            key_select(row, col);
-            wait_us(5);
+        key_select_row(row);
+        wait_us(RELAX_TIME_US);
 
+        for (uint8_t col = 0; col < MATRIX_COLS; col++) {
             // Hysteresis control: assert(1) when previous key state is on
             if (key_prev_was_on(row, col)) {
                 key_prev_on();
+            } else {
+                key_prev_off();
             }
-            wait_us(10);
 
-            // NOTE: KEY_STATE is valid only in 20us after KEY_ENABLE.
-            // If V-USB interrupts in this section we could lose 40us or so
-            // and would read invalid value from KEY_STATE.
-            uint8_t last = TIMER_RAW;
+            // Disable interrupts to encure the ADC timing is correct
+            cli();
 
-            key_enable();
+            // strobe
+            key_select_col(col);
+            key_strobe_high();
 
-            // Wait for KEY_STATE outputs its value.
+            // Wait for ADC to outputs its value.
             // 1us was ok on one HHKB, but not worked on another.
             // no   wait doesn't work on Teensy++ with pro(1us works)
             // no   wait does    work on tmk PCB(8MHz) with pro2
@@ -95,7 +98,7 @@ uint8_t matrix_scan(void) {
             // 10us wait does    work on Teensy++ with pro
             // 10us wait does    work on 328p+iwrap with pro
             // 10us wait doesn't work on tmk PCB(8MHz) with pro2(very lagged scan)
-            wait_us(5);
+            wait_us(ADC_READ_TIME_US);
 
             if (key_state()) {
                 matrix[row] &= ~(1 << col);
@@ -103,21 +106,12 @@ uint8_t matrix_scan(void) {
                 matrix[row] |= (1 << col);
             }
 
-            // Ignore if this code region execution time elapses more than 20us.
-            // MEMO: 20[us] * (TIMER_RAW_FREQ / 1000000)[count per us]
-            // MEMO: then change above using this rule: a/(b/c) = a*1/(b/c) = a*(c/b)
-            if (TIMER_DIFF_RAW(TIMER_RAW, last) > 20 / (1000000 / TIMER_RAW_FREQ)) {
-                matrix[row] = matrix_prev[row];
-            }
+            key_strobe_low();
+            sei();
 
-            wait_us(5);
-            key_prev_off();
-            key_unable();
-
-            // NOTE: KEY_STATE keep its state in 20us after KEY_ENABLE.
-            // This takes 25us or more to make sure KEY_STATE returns to idle state.
-
-            wait_us(75);
+            // Make sure enough time has elapsed since the last call
+            // This is to ensure the matrix voltages have relaxed
+            wait_us(RELAX_TIME_US);
         }
         if (matrix[row] ^ matrix_prev[row]) {
             matrix_has_changed   = true;
