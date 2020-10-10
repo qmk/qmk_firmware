cf7204fbfe876cadecb02b9fa3361dd762e2c17a
Sönke Hahn <soenkehahn@gmail.com>
2020-10-10 19:14:32 -0400

    more stuff

--- keyboards/moonlander/keymaps/shahn/keymap.c
+++ keyboards/moonlander/keymaps/shahn/keymap.c
@@ -21,6 +21,7 @@
 
 enum layers {
     BASE,  // default layer
+    LAYER_4,
     SYMB,  // symbols
     MDIA,  // media keys
 };
@@ -33,11 +34,20 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C,    KC_W,    TG(SYMB),         TG(SYMB), KC_K,    KC_H,    KC_F,    KC_Q,    KC_P,    KC_BSLS,
-        KC_BSPC, KC_U,    KC_I,    KC_A,    KC_E,    KC_O,    KC_HYPR,           KC_MEH,  KC_S,    KC_R,    KC_T,    KC_D,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
+        KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C,    KC_W,    TG(SYMB),         TG(SYMB), KC_K,    KC_H,    KC_G,    KC_F,    KC_Q,    KC_BSLS,
+        KC_BSPC, KC_U,    KC_I,    KC_A,    KC_E,    KC_O,    KC_HYPR,           KC_MEH,  KC_S,    KC_N,    KC_R,    KC_T,    KC_D,    KC_Y,
         KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_P,    KC_Z,                                KC_B,    KC_M,    KC_COMM, KC_DOT,  LCTL_T(KC_SLSH), KC_LSFT,
-   LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,       LALT_T(KC_APP),    RCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
+   LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,       LALT_T(KC_APP),    RCTL_T(KC_ESC),   MO(LAYER_4),   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
                                             KC_SPC,  KC_BSPC, KC_LGUI,           KC_RALT,  KC_TAB,  KC_ENT
+    ),
+
+    [LAYER_4] = LAYOUT_moonlander(
+        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,           KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_TRNS, KC_EXLM, KC_BSPC, KC_UP,   KC_RCBR, KC_PIPE, KC_TRNS,           KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
+        KC_TRNS, KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_GRV,  KC_TRNS,           KC_TRNS, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+        KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_ENT,  KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+        EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_VAI,           RGB_TOG,          KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                                            RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,KC_TRNS, KC_TRNS
     ),
 
     [SYMB] = LAYOUT_moonlander(
