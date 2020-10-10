b7fd134de43fa7115476b3c7b8d8a006c474bca8
melgeek001365 <melgeek001365@gmail.com>
2020-10-10 17:01:15 +0800

    Update the VID and PID in mj64/config.h add the rev1/rev1.c add the rev2/rev2.c

--- keyboards/melgeek/mj64/config.h
+++ keyboards/melgeek/mj64/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0xED64
+#define VENDOR_ID       0xEDED
+#define PRODUCT_ID      0x6064
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    MelGeek
 #define PRODUCT         MJ64
