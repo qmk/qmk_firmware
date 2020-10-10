2fc200cadb16dc29d3798a7ea7e9da0f2baa643c
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-10-10 05:03:38 -0700

    update copyright headers
    I mean... if someone's gotta "own" this code...

--- keyboards/gh60/v1p3/keymaps/factory_layout5/keymap.c
+++ keyboards/gh60/v1p3/keymaps/factory_layout5/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020
+/* Copyright 2020 noroadsleft
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
