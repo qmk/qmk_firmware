1f158d3dea1d7564dbe0784650da00ba330ee496
Taylor Walla <taylor.j.walla@gmail.com>
2020-10-09 23:21:43 -0500

    Update keyboards/ai03/voyager60_alps/voyager60_alps.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ai03/voyager60_alps/voyager60_alps.c
+++ keyboards/ai03/voyager60_alps/voyager60_alps.c
@@ -13,9 +13,3 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "voyager60_alps.h"
-
-void led_set_kb(uint8_t usb_led) {
-	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-	led_set_user(usb_led);
-}
