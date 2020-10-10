8c3140174cd14a4d99e6849d214c7ae5f810de09
floof <floof@pop-os.localdomain>
2020-10-10 16:55:31 -0700

    Adjusted EU ISO Files, should be ready to go now

--- keyboards/EU_ISOlation/keymaps/FloofBS/keymap.c
+++ keyboards/EU_ISOlation/keymaps/FloofBS/keymap.c
@@ -16,21 +16,21 @@ enum combos {
 };
 
 enum custom_keycodes {
-    womboComboString = SAFE_RANGE
+    WCSTR = SAFE_RANGE
 };
 
 const uint16_t PROGMEM ab_combo[] = { KC_F1, KC_F3, COMBO_END };
 const uint16_t PROGMEM ja_combo[] = { KC_F2, KC_F4, COMBO_END };
 const uint16_t PROGMEM jr_combo[] = { KC_F1, KC_F2, COMBO_END };
 const uint16_t PROGMEM rb_combo[] = { KC_F3, KC_F4, COMBO_END };
-const uint16_t PROGMEM wombo_combo[] = { KC_F1, KC_F4, KC_F3, KC_F2, COMBO_END };
+const uint16_t PROGMEM wombo_combo[] = { KC_F1, KC_F4, COMBO_END };
 
 combo_t key_combos[COMBO_COUNT] = {
     [AB_ESC] = COMBO(ab_combo, KC_ESC),
     [JA_DEL] = COMBO(ja_combo, KC_DEL),
     [JR_VOLU] = COMBO(jr_combo, KC_VOLU),
     [RB_VOLD] = COMBO(rb_combo, KC_VOLD),
-    [WOMBO] = COMBO(wombo_combo, womboComboString)
+    [WOMBO] = COMBO(wombo_combo, WCSTR)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_F1,   KC_F2,  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, LT(_FN2, KC_QUOT),
         KC_F3,   KC_F4,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, LT(_FN1, KC_ENT),
-                         MO(_FN3),KC_LALT,                        LT(_FN2, KC_SPC),                                   KC_LGUI, KC_RCTL
+                         MO(_FN3),KC_LALT,                        KC_SPACE,                                   KC_LGUI, KC_RCTL
     ),
 
     [_FN1] = LAYOUT_EUISO_BIGSPACE(
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-// Fix this shitpost at some point. It is supposed to do the shitpost but for some reason comes out to e
+// Update: The problem does not sit in this area. The function works quite well actually. So it has to do with QMK's comboing software
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case womboComboString:
+    case WCSTR:
       if (record->event.pressed) {
         // when this shitpost is pressed
         SEND_STRING("HAPPY FEET!! WOMBO COMBO!!");
