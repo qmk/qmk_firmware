81090d31181b1e502ea98c20eb24d61c762f287b
Takeshi Nishio <kkeennnn@gmail.com>
2020-10-12 06:10:46 +0900

    Update LED設定変更

--- keyboards/jones/v.0.3/config.h
+++ keyboards/jones/v.0.3/config.h
@@ -72,11 +72,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN B7
 #ifdef RGB_DI_PIN
-  #define RGBLED_NUM 2
+  #define RGBLED_NUM 14
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 40 /* The maximum brightness level */
+  #define RGBLIGHT_LIMIT_VAL 220 /* The maximum brightness level */
   // #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
   /*== Lighting Layers ==*/
   #define RGBLIGHT_LAYERS
@@ -87,11 +87,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   /*== or choose animations ==*/
   // #define RGBLIGHT_EFFECT_BREATHING
   // #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-  // #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
   // #define RGBLIGHT_EFFECT_SNAKE
   // #define RGBLIGHT_EFFECT_KNIGHT
   // #define RGBLIGHT_EFFECT_CHRISTMAS
-  // #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
   // #define RGBLIGHT_EFFECT_RGB_TEST
   // #define RGBLIGHT_EFFECT_ALTERNATING
   // /*== customize breathing effect ==*/
