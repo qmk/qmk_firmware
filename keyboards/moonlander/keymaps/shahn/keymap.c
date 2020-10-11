3283dd04f63fa23bc39cd2403a45d3f12b41f22e
Sönke Hahn <soenkehahn@gmail.com>
2020-10-10 20:06:53 -0400

    more stuff

--- keyboards/moonlander/keymaps/shahn/keymap.c
+++ keyboards/moonlander/keymaps/shahn/keymap.c
@@ -21,6 +21,7 @@
 
 enum layers {
     BASE,  // default layer
+    LAYER_3,
     LAYER_4,
     SYMB,  // symbols
     MDIA,  // media keys
@@ -33,19 +34,33 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_X,    KC_V,    KC_L,    KC_C,    KC_W,    TG(SYMB),         TG(SYMB), KC_K,    KC_H,    KC_G,    KC_F,    KC_Q,    KC_BSLS,
-        KC_BSPC, KC_U,    KC_I,    KC_A,    KC_E,    KC_O,    KC_HYPR,           KC_MEH,  KC_S,    KC_N,    KC_R,    KC_T,    KC_D,    KC_Y,
-        KC_LSFT, LCTL_T(KC_Z),KC_X,KC_C,    KC_P,    KC_Z,                                KC_B,    KC_M,    KC_COMM, KC_DOT,  LCTL_T(KC_SLSH), KC_LSFT,
-   LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,       LALT_T(KC_APP),    RCTL_T(KC_ESC),   MO(LAYER_4),   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
-                                            KC_SPC,  KC_BSPC, KC_LGUI,           KC_RALT,  KC_TAB,  KC_ENT
+        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        XXXXXXX,    XXXXXXX, KC_6,        KC_7,        KC_8,    KC_9,    KC_0,    XXXXXXX,
+        XXXXXXX, KC_X,    KC_V,    KC_L,    KC_C,    KC_W,        XXXXXXX,    XXXXXXX, KC_K,        KC_H,        KC_G,    KC_F,    KC_Q,    XXXXXXX,
+        XXXXXXX, KC_U,    KC_I,    KC_A,    KC_E,    KC_O,        XXXXXXX,    XXXXXXX, KC_S,        KC_N,        KC_R,    KC_T,    KC_D,    KC_Y,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P,    KC_Z,                             KC_B,        KC_M,        KC_COMM, KC_DOT,  XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX,    XXXXXXX,              MO(LAYER_4), KC_RGUI, XXXXXXX, XXXXXXX, XXXXXXX,
+                                            KC_LSFT, MO(LAYER_3), KC_SPC,     KC_SPC,  MO(LAYER_3), KC_RSFT
+    ),
+
+    // copied from my planck layout
+    // XXXXXXX, XXXXXXX, KC_UNDS, KC_LBRC, KC_RBRC, KC_CIRC, KC_EXLM, KC_LABK, KC_RABK, KC_EQL , KC_AMPR, XXXXXXX,
+    // XXXXXXX, KC_BSLS, KC_SLSH, KC_LCBR, KC_RCBR, KC_ASTR, KC_QUES, KC_LPRN, KC_RPRN, KC_MINS, KC_COLN, KC_AT  ,
+    // XXXXXXX, KC_HASH, KC_DLR , KC_PIPE, KC_TILD, KC_GRV , KC_PLUS, KC_PERC, KC_DQUO, KC_QUOT, KC_SCLN, XXXXXXX,
+    // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC , KC_SPC , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    [LAYER_3] = LAYOUT_moonlander(
+        VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,           KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_TRNS, KC_EXLM, KC_BSPC, KC_UP,   KC_DEL,  KC_PIPE, KC_TRNS,           KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
+        KC_TRNS, KC_BSLS, KC_SLSH, KC_LCBR, KC_RCBR, KC_ASTR, KC_TRNS,           KC_TRNS, KC_DOWN, KC_LPRN, KC_RPRN, KC_MINS, KC_COLN, KC_TRNS,
+        KC_TRNS, KC_ESC,  KC_CIRC, KC_LBRC, KC_ENT,  KC_TILD,                             KC_AMPR, KC_PERC, KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+        EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_VAI,           RGB_TOG,          KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+                                            RGB_HUD, KC_TRNS, RGB_HUI, TOGGLE_LAYER_COLOR,KC_TRNS, KC_TRNS
     ),
 
     [LAYER_4] = LAYOUT_moonlander(
         VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,           KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS, KC_EXLM, KC_BSPC, KC_UP,   KC_RCBR, KC_PIPE, KC_TRNS,           KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-        KC_TRNS, KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_GRV,  KC_TRNS,           KC_TRNS, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-        KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_ENT,  KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+        KC_TRNS, KC_EXLM, KC_BSPC, KC_UP,   KC_DEL,  KC_PIPE, KC_TRNS,           KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
+        KC_TRNS, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_TRNS,           KC_TRNS, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+        KC_TRNS, KC_ESC,  KC_CIRC, KC_LBRC, KC_ENT,  KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
         EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_VAI,           RGB_TOG,          KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
                                             RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,KC_TRNS, KC_TRNS
     ),
