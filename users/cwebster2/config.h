2692f9466194da3783e90619a23a1bb0a57b0976
Casey Webster <casey.webster@gmail.com>
2020-10-12 17:41:53 -0500

    Update tapping term for modtap keys

--- users/cwebster2/config.h
+++ users/cwebster2/config.h
@@ -16,7 +16,7 @@
 #pragma once
 
 #define TAPPING_TOGGLE 1
-#define TAPPING_TERM 200
+#define TAPPING_TERM 300
 //#define TAPPING_FORCE_HOLD
 #undef PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
