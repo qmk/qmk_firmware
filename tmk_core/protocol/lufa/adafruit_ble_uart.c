91a3e7f3e12cdef5663f48e275a45160b268125f
Kan-Ru Chen <kanru@kanru.info>
2020-10-12 21:19:36 +0900

    [Keyboard] Use a queue for BLE AT commands to avoid lockup in interrupt handler
    The UART reader uses reciving interrupt so cannot be used inside another
    interrupt handler, e.g. USB wakeup.

--- tmk_core/protocol/lufa/adafruit_ble_uart.c
+++ tmk_core/protocol/lufa/adafruit_ble_uart.c
@@ -1,10 +1,8 @@
 #include "adafruit_ble.h"
 
-#include <avr/io.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <alloca.h>
-#include "host.h"
 #include "protocol/serial.h"
 #include "debug.h"
 #include "timer.h"
@@ -24,10 +22,78 @@ static struct {
     uint16_t last_connection_update;
 } state;
 
-static bool ble51_init(void) {
+// Using a queue for the AT commands because reading the RX requires
+// interrupt so can not be done in USB interrupt handler
+enum queue_type {
+    QTKeyReport,  // 1-byte modifier + 6-byte key report
+    QTConsumer,   // 16-bit key code
+#ifdef MOUSE_ENABLE
+    QTMouseMove,  // 4-byte mouse report
+#endif
+};
+
+struct queue_item {
+    enum queue_type queue_type;
+    union __attribute__((packed)) {
+        struct __attribute__((packed)) {
+            uint8_t modifier;
+            uint8_t keys[6];
+        } key;
+        uint16_t consumer;
+#ifdef MOUSE_ENABLE
+        struct __attribute__((packed)) {
+            int8_t  x, y, scroll, pan;
+            uint8_t buttons;
+        } mousemove;
+#endif
+    };
+};
+
+#define QUEUE_SIZE 32
+
+struct circ_buf {
+    struct queue_item buf[QUEUE_SIZE];
+    uint8_t           head;
+    uint8_t           tail;
+} send_queue;
+
+/* Return count in buffer.  */
+#define CIRC_CNT(head, tail, size) (((head) - (tail)) & ((size)-1))
+/* Return space available, 0..size-1.  We always leave one free char
+   as a completely full buffer has head == tail, which is the same as
+   empty.  */
+#define CIRC_SPACE(head, tail, size) CIRC_CNT((tail), ((head) + 1), (size))
+
+static bool enqueue(struct circ_buf *queue, struct queue_item *item) {
+    uint8_t head = queue->head;
+    uint8_t tail = queue->tail;
+    if (CIRC_SPACE(head, tail, QUEUE_SIZE) > 0) {
+        queue->buf[head] = *item;
+        queue->head      = (head + 1) & (QUEUE_SIZE - 1);
+        return true;
+    }
+    return false;
+}
+
+static bool dequeue(struct circ_buf *queue, struct queue_item *item) {
+    uint8_t head = queue->head;
+    uint8_t tail = queue->tail;
+    if (CIRC_CNT(head, tail, QUEUE_SIZE) > 0) {
+        *item       = queue->buf[tail];
+        queue->tail = (tail + 1) & (QUEUE_SIZE - 1);
+        return true;
+    }
+    return false;
+}
+
+static bool process_queue_item(struct queue_item *item);
+
+static bool ble_init(void) {
     state.initialized  = false;
     state.configured   = false;
     state.is_connected = false;
+    send_queue.head    = 0;
+    send_queue.tail    = 0;
 
     serial_init();
 
@@ -35,7 +101,7 @@ static bool ble51_init(void) {
     return state.initialized;
 }
 
-static void ble51_gets(char *resp, uint16_t resplen, uint16_t timeout) {
+static void uart_gets(char *resp, uint16_t resplen, uint16_t timeout) {
     uint16_t t = timer_read();
     uint8_t  i = 0;
     int16_t  c;
@@ -47,50 +113,47 @@ static void ble51_gets(char *resp, uint16_t resplen, uint16_t timeout) {
             resp[i++] = c;
         }
     }
-    resp[i] = '\0';
 }
 
-static bool ble51_get_ok(uint16_t timeout) {
+static bool uart_get_ok(uint16_t timeout) {
     char ok[32];
-    ble51_gets(ok, sizeof(ok), timeout);
-    if (memcmp(ok, "OK", 2) == 0) {
+    uart_gets(ok, sizeof(ok), timeout);
+    if (memcmp_P(ok, PSTR("OK"), 2) == 0) {
         return true;
     }
     return false;
 }
 
-static void ble51_puts(const char *s) {
+static inline void uart_puts(const char *s) {
     while (*s) serial_send(*s++);
+    serial_send('\n');
 }
 
 static bool at_command(const char *cmd, char *resp, uint16_t resplen) {
-    dprint("Sending AT command: ");
-    dprintf("%s\n", cmd);
-    ble51_puts(cmd);
-    ble51_puts("\n");
+    uart_puts(cmd);
     if (resplen) {
-        ble51_gets(resp, resplen, TIMEOUT);
-        dprintf("Response: %s\n", resp);
-        if (memcmp(resp, "ERROR", 5) == 0) {
+        uart_gets(resp, resplen, TIMEOUT);
+        if (memcmp_P(resp, PSTR("ERROR"), 5) == 0) {
             return false;
         }
-        if (memcmp(resp, "OK", 2) == 0) {
+        if (memcmp_P(resp, PSTR("OK"), 2) == 0) {
             return true;
         }
     }
-    return ble51_get_ok(TIMEOUT);
+    return uart_get_ok(TIMEOUT);
 }
 
 static bool at_command_P(const char *cmd, char *resp, uint16_t resplen) {
-    char *cmdbuf = (char *)alloca(strlen_P(cmd) + 1);
-    strcpy_P(cmdbuf, cmd);
+    size_t len    = strlen_P(cmd) + 1;
+    char * cmdbuf = (char *)alloca(len);
+    memcpy_P(cmdbuf, cmd, len);
     return at_command(cmdbuf, resp, resplen);
 }
 
 bool adafruit_ble_enable_keyboard(void) {
     char resbuf[128];
 
-    if (!state.initialized && !ble51_init()) {
+    if (!state.initialized && !ble_init()) {
         return false;
     }
 
@@ -143,21 +206,22 @@ bool adafruit_ble_is_connected() { return state.is_connected; }
 
 static void set_connected(bool connected) {
     if (connected != state.is_connected) {
-        if (connected) {
-            dprint("****** BLE CONNECT!!!!\n");
-        } else {
-            dprint("****** BLE DISCONNECT!!!!\n");
-        }
         state.is_connected = connected;
     }
 }
 
 void adafruit_ble_task(void) {
-    char resbuf[48];
+    char              resbuf[48];
+    struct queue_item item;
 
     if (!state.configured && !adafruit_ble_enable_keyboard()) {
         return;
     }
+
+    if (dequeue(&send_queue, &item)) {
+        process_queue_item(&item);
+    }
+
     if (timer_elapsed(state.last_connection_update) > ConnectionUpdateInterval) {
         static const char kGetConn[] PROGMEM = "AT+GAPGETCONN";
         state.last_connection_update         = timer_read();
@@ -168,42 +232,84 @@ void adafruit_ble_task(void) {
     }
 }
 
-bool adafruit_ble_send_keys(uint8_t hid_modifier_mask, uint8_t *keys, uint8_t nkeys) {
-    if (!state.configured || !state.is_connected) {
-        return false;
-    }
+static bool process_queue_item(struct queue_item *item) {
     char cmdbuf[48];
-    char fmtbuf[64];
-    strcpy_P(fmtbuf, PSTR("AT+BLEKEYBOARDCODE=%02x-00-%02x-%02x-%02x-%02x-%02x-%02x"));
-    snprintf(cmdbuf, sizeof(cmdbuf), fmtbuf, hid_modifier_mask, keys[0], nkeys >= 1 ? keys[1] : 0, nkeys >= 2 ? keys[2] : 0, nkeys >= 3 ? keys[3] : 0, nkeys >= 4 ? keys[4] : 0, nkeys >= 5 ? keys[5] : 0);
-    return at_command(cmdbuf, NULL, 0);
+
+    // Arrange to re-check connection after keys have settled
+    state.last_connection_update = timer_read();
+
+    switch (item->queue_type) {
+        case QTKeyReport:
+            snprintf_P(cmdbuf, sizeof(cmdbuf), PSTR("AT+BLEKEYBOARDCODE=%02x-00-%02x-%02x-%02x-%02x-%02x-%02x"), item->key.modifier, item->key.keys[0], item->key.keys[1], item->key.keys[2], item->key.keys[3], item->key.keys[4], item->key.keys[5]);
+            return at_command(cmdbuf, NULL, 0);
+
+        case QTConsumer:
+            snprintf_P(cmdbuf, sizeof(cmdbuf), PSTR("AT+BLEHIDCONTROLKEY=0x%04x"), item->consumer);
+            return at_command(cmdbuf, NULL, 0);
+
+#ifdef MOUSE_ENABLE
+        case QTMouseMove:
+            snprintf_P(cmdbuf, sizeof(cmdbuf), PSTR("AT+BLEHIDMOUSEMOVE=%d,%d,%d,%d"), item->mousemove.x, item->mousemove.y, item->mousemove.scroll, item->mousemove.pan);
+            if (!at_command(cmdbuf, NULL, 0)) {
+                return false;
+            }
+            snprintf_P(cmdbuf, sizeof(cmdbuf), PSTR("AT+BLEHIDMOUSEBUTTON=%d"), item->mousemove.buttons);
+            return at_command(cmdbuf, NULL, 0);
+#endif
+        default:
+            return true;
+    }
+}
+
+bool adafruit_ble_send_keys(uint8_t hid_modifier_mask, uint8_t *keys, uint8_t nkeys) {
+    struct queue_item item;
+
+    item.queue_type   = QTKeyReport;
+    item.key.modifier = hid_modifier_mask;
+
+    while (nkeys >= 0) {
+        item.key.keys[0] = keys[0];
+        item.key.keys[1] = nkeys >= 1 ? keys[1] : 0;
+        item.key.keys[2] = nkeys >= 2 ? keys[2] : 0;
+        item.key.keys[3] = nkeys >= 3 ? keys[3] : 0;
+        item.key.keys[4] = nkeys >= 4 ? keys[4] : 0;
+        item.key.keys[5] = nkeys >= 5 ? keys[5] : 0;
+
+        if (!enqueue(&send_queue, &item)) {
+            return false;
+        }
+
+        if (nkeys <= 6) {
+            return true;
+        }
+
+        nkeys -= 6;
+        keys += 6;
+    }
+
+    return true;
 }
 
 bool adafruit_ble_send_consumer_key(uint16_t keycode, int hold_duration) {
-    if (!state.configured || !state.is_connected) {
-        return false;
-    }
-    char cmdbuf[48];
-    char fmtbuf[64];
-    strcpy_P(fmtbuf, PSTR("AT+BLEHIDCONTROLKEY=%d"));
-    snprintf(cmdbuf, sizeof(cmdbuf), fmtbuf, keycode);
-    return at_command(cmdbuf, NULL, 0);
+    struct queue_item item;
+
+    item.queue_type = QTConsumer;
+    item.consumer   = keycode;
+
+    return enqueue(&send_queue, &item);
 }
 
 #ifdef MOUSE_ENABLE
 bool adafruit_ble_send_mouse_move(int8_t x, int8_t y, int8_t scroll, int8_t pan, uint8_t buttons) {
-    if (!state.configured || !state.is_connected) {
-        return false;
-    }
-    char cmdbuf[48];
-    char fmtbuf[64];
-    strcpy_P(fmtbuf, PSTR("AT+BLEHIDMOUSEMOVE=%d,%d,%d,%d"));
-    snprintf(cmdbuf, sizeof(cmdbuf), fmtbuf, x, y, scroll, pan);
-    if (!at_command(cmdbuf, NULL, 0)) {
-        return false;
-    }
-    strcpy_P(fmtbuf, PSTR("AT+BLEHIDMOUSEBUTTON=%d"));
-    snprintf(cmdbuf, sizeof(cmdbuf), fmtbuf, buttons);
-    return at_command(cmdbuf, NULL, 0);
+    struct queue_item item;
+
+    item.queue_type        = QTMouseMove;
+    item.mousemove.x       = x;
+    item.mousemove.y       = y;
+    item.mousemove.scroll  = scroll;
+    item.mousemove.pan     = pan;
+    item.mousemove.buttons = buttons;
+
+    return enqueue(&send_queue, &item);
 }
 #endif
