35bb33cab5e8c4ffb7a86f9aeefa7668026d1304
Daveyr <26368709+Daveyr@users.noreply.github.com>
2020-10-12 20:50:07 +0100

    Update keyboards/yampad/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/yampad/rules.mk
+++ keyboards/yampad/rules.mk
@@ -1,24 +1,23 @@
-
 # MCU name
 MCU = atmega32u4
 
+# Bootloader selection
 BOOTLOADER = atmel-dfu
 
-
-
-
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no	    # Mouse keys
-EXTRAKEY_ENABLE = no	    # Audio control and System control
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes		    # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-RGBLIGHT_ENABLE = yes       # Enable keyboard underlight functionality
-MIDI_ENABLE = no            # MIDI support
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-OLED_DRIVER_ENABLE = yes    # Enable OLED feature
+OLED_DRIVER_ENABLE = yes
