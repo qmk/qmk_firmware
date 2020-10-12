42984c2de0a283890032a7bf9cd92407b268e28e
Richard <davey.richard@gmail.com>
2020-10-12 21:07:57 +0100

    some missed conflicts resolved in readme and rule

--- keyboards/yampad/readme.md
+++ keyboards/yampad/readme.md
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-YAMPAD
+#YAMPAD
 ===
 See [the github page](https://github.com/mattdibi/yampad) for details.
-=======
-# YAMPAD
->>>>>>> 35bb33cab5e8c4ffb7a86f9aeefa7668026d1304
--- keyboards/yampad/rules.mk
+++ keyboards/yampad/rules.mk
@@ -12,19 +12,6 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-<<<<<<< HEAD
-NKRO_ENABLE = yes		    # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-RGBLIGHT_ENABLE = yes       # Enable keyboard underlight functionality (+4870)
-#MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
-OLED_DRIVER_ENABLE = yes    # Enable OLED feature
-DEBOUNCE_TYPE = eager_pk
-DEBOUNCE = 2
-=======
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
@@ -34,4 +21,4 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 OLED_DRIVER_ENABLE = yes
->>>>>>> 35bb33cab5e8c4ffb7a86f9aeefa7668026d1304
+DEBOUNCE_TYPE = eager_pk
