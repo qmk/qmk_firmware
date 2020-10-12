847bff9922bf3971b9c5f4808937581ddc7c3392
Richard <davey.richard@gmail.com>
2020-10-12 20:56:03 +0100

    added debouncing algorithm, delay and a traditional layout

--- keyboards/yampad/config.h
+++ keyboards/yampad/config.h
@@ -53,7 +53,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_ANIMATIONS
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCING_DELAY 0
+//#define DEBOUNCE_TYPE  sym_defer_g
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
--- keyboards/yampad/rules.mk
+++ keyboards/yampad/rules.mk
@@ -75,3 +75,5 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
 OLED_DRIVER_ENABLE = yes    # Enable OLED feature
+DEBOUNCE_TYPE = eager_pk
+DEBOUNCE = 2
