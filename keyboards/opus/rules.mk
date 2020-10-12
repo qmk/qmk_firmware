5ed507e4072133e1ebbb523ca5dffee44be1a14f
Raleigh Wayland <raleigh.wayland@stormapp.com>
2020-10-12 17:09:10 -0600

    Fixing tap dance issue

--- keyboards/opus/rules.mk
+++ keyboards/opus/rules.mk
@@ -65,7 +65,6 @@ MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
-TAP_DANCE_ENABLE=yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
