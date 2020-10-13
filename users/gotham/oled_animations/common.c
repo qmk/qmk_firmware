a4d8b793fb513f6cee57c41e337112fd0dfcd410
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-10-12 23:38:23 -0700

    Add license headers to c and h files

--- users/gotham/oled_animations/common.c
+++ users/gotham/oled_animations/common.c
@@ -1,3 +1,19 @@
+/* Copyright 2020 Gautham Yerroju <gautham.yerroju@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "common.h"
 
 uint8_t rect_out_of_bounds(int16_t x, int16_t y, uint8_t width, uint8_t height, int8_t padding) {
