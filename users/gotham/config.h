f0e71932b5928fa87f9ac7fb7489d54f17683b0f
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-10-12 22:05:47 -0700

    Improve mouse movement

--- users/gotham/config.h
+++ users/gotham/config.h
@@ -38,10 +38,10 @@
 #        undef MOUSEKEY_WHEEL_TIME_TO_MAX
 #    endif
 
-#    define MOUSEKEY_DELAY 100             // Delay between pressing a movement key and cursor movement
-#    define MOUSEKEY_INTERVAL 16            // Time between cursor movements
+#    define MOUSEKEY_DELAY 0               // Delay between pressing a movement key and cursor movement
+#    define MOUSEKEY_INTERVAL 8            // Time between cursor movements
 #    define MOUSEKEY_MAX_SPEED 8           // Maximum cursor speed at which acceleration stops
-#    define MOUSEKEY_TIME_TO_MAX 40        // Time until maximum cursor speed is reached
+#    define MOUSEKEY_TIME_TO_MAX 80        // Time until maximum cursor speed is reached
 #    define MOUSEKEY_WHEEL_DELAY 100       // Delay between pressing a wheel key and wheel movement
 #    define MOUSEKEY_WHEEL_INTERVAL 150    // Time between wheel movements
 #    define MOUSEKEY_WHEEL_MAX_SPEED 8     // Maximum number of scroll steps per scroll action
