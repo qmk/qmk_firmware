11659a7045340131757f8f9af5dde461db9e9fad
Khader Syed <khader.syed@aicure.com>
2020-10-13 16:37:43 -0400

    switch to use lcmd for main alt

--- keyboards/wilba_tech/wt60_d/keymaps/madhatter/keymap.c
+++ keyboards/wilba_tech/wt60_d/keymaps/madhatter/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,  KC_Q,    KC_W,   KC_E,  KC_R, KC_T, KC_Y,  KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 	KC_GRV,  KC_A,    KC_S,   KC_D,  KC_F, KC_G, KC_H,  KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSFT, KC_NO,   KC_Z,   KC_X,  KC_C, KC_V, KC_B,  KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, FNMS,
-	KC_LCTL, KC_LCMD, KC_LALT,                   KC_SPC,                       KC_SPC,  FNMS,    KC_RALT, KC_RCTL),
+	KC_LCTL, KC_LALT, KC_LCMD,                   KC_SPC,                     KC_SPC,  FNMS,    KC_RALT, KC_RCTL),
 
 // Fn1 Layer
 [_FNMS] = LAYOUT_all(
