2429f99faf9b468fe4493294a40c3a23847522b5
Jarrett Drouillard <jarrett@thestyl.us>
2020-10-14 08:24:33 -0400

    chore(planck kuatsure) re-add volume since no rotary encoder

--- keyboards/planck/keymaps/kuatsure/keymap.c
+++ keyboards/planck/keymaps/kuatsure/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | Del  | Left | Down | Right|   ~  |   =  |   (  |   )  |  +   |  :   |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      | Prev | Play | Next |      |   -  |   [  |   ]  |      |  ?   |Mouse |
+ * |      | VolU | Prev | Play | Next | VolU |   -  |   [  |   ]  |      |  ?   |Mouse |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid_wrapper(
     _______, KC_EXLM, KC_AMPR, KC_UP,   _______,  KC_GRV,  _______, ____CRBRACES____, KC_UNDS, _______, KC_PIPE,
     _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_TILD, KC_EQL,  _____PARENS_____, KC_PLUS, KC_COLN, KC_DQT ,
-    _______, _______, KC_MPRV, KC_MPLY, KC_MNXT,  _______, KC_MINS, ___SQBRACKETS___, _______, KC_QUES, MOUSE  ,
+    _______, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT,  KC_VOLU, KC_MINS, ___SQBRACKETS___, _______, KC_QUES, MOUSE  ,
     _______, _______, _______, _______, _______,  _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
