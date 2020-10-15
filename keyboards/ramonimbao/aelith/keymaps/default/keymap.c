2dd0f1a21c3314e8df5435eb495c45157f93f5bd
Ramon Imbao <ramonimbao@gmail.com>
2020-10-15 16:23:18 +0800

    Add AELITH keyboard

--- keyboards/ramonimbao/aelith/keymaps/default/keymap.c
+++ keyboards/ramonimbao/aelith/keymaps/default/keymap.c
@@ -23,4 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  MO(1),
                  KC_LCTL,          KC_LALT,          KC_SPC,  MO(1),            KC_SPC,  KC_RALT,                            KC_RCTL
     ),
+    [1] = LAYOUT_all(
+        RESET,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,
+        KC_HOME, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_END,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                 _______,          _______,          _______, _______,          _______, _______,                            _______
+    ),
 };
--- keyboards/ramonimbao/aelith/readme.md
+++ keyboards/ramonimbao/aelith/readme.md
@@ -1,14 +1,14 @@
-# Herringbone
+# ÆLITH
 
-![Herringbone](https://i.imgur.com/SQyS0j8.png)
+![ÆLITH](https://i.imgur.com/wG1OWLo.png)
 
-A through-hole 75% keyboard.
+A through-hole Alice-layout keyboard.
 
 * Keyboard Maintainer: [Ramon Imbao](https://github.com/ramonimbao)
 * Hardware Supported: ATmega32A
 
 Make example for this keyboard (after setting up your build environment):
 
-    make herringbone:default
+    make ramonimbao/aelith:default:flash
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
