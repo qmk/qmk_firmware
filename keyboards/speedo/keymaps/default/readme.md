59e0484f2adf5da00ecd1e2657c86ad69b440aee
Paul Ewing <pcewing00@gmail.com>
2020-10-15 11:47:39 -0700

    Update the Speedo firmware for v3.0

--- keyboards/speedo/keymaps/default/readme.md
+++ keyboards/speedo/keymaps/default/readme.md
@@ -1,1 +1,10 @@
-# The default keymap for speedo
+# The default Speedo
+
+## Default Layer
+
+![Default Layer](https://raw.githubusercontent.com/cozykeys/speedo/master/layout/speedo_layer_default.svg)
+
+## Function Layer
+
+![Function Layer](https://raw.githubusercontent.com/cozykeys/speedo/master/layout/speedo_layer_fn.svg)
+
--- keyboards/speedo/speedo.h
+++ keyboards/speedo/speedo.h
@@ -13,29 +13,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef SPEEDO_H
-#define SPEEDO_H
+#pragma once
 
 #include "quantum.h"
 
-// This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
-// The first section contains all of the arguments
-// The second converts the arguments into a two-dimensional array
 #define LAYOUT( \
-    k00, k01, k02, k03, k04, k05,           k06, k07, k08, k09, k10, k11, \
-    k12, k13, k14, k15, k16, k17, k18,      k19, k20, k21, k22, k23, k24, \
-    k25, k26, k27, k28, k29, k30,      k31, k32, k33, k34, k35, k36, k37, \
-    k38, k39, k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k50, k51, \
-    k52, k53, k54, k55, k56, k57, k58, k59, k60, k61, k62, k63, k64, k65 \
+    k01, k02, k03, k04, k05, k06,                k07, k08, k09, k10, k11, k12, \
+    k13, k14, k15, k16, k17, k18,      k19,      k20, k21, k22, k23, k24, k25, \
+    k26, k27, k28, k29, k30, k31, k32,      k33, k34, k35, k36, k37, k38, k39, \
+    k40, k41, k42, k43, k44, k45,      k46,      k47, k48, k49, k50, k51, k52, \
+    k53, k54, k55, k56, k57, k58, k59,      k60, k61, k62, k63, k64, k65, k66 \
 ) \
 { \
-    {   k00,   k01,   k02,   k03,   k04,   k05, KC_NO, KC_NO,   k06,   k07,   k08,   k09,   k10,   k11 }, \
-    {   k12,   k13,   k14,   k15,   k16,   k17,   k18, KC_NO,   k19,   k20,   k21,   k22,   k23,   k24 }, \
-    {   k25,   k26,   k27,   k28,   k29,   k30, KC_NO,   k31,   k32,   k33,   k34,   k35,   k36,   k37 }, \
-    {   k38,   k39,   k40,   k41,   k42,   k43,   k44,   k45,   k46,   k47,   k48,   k49,   k50,   k51 }, \
-    {   k52,   k53,   k54,   k55,   k56,   k57,   k58,   k59,   k60,   k61,   k62,   k63,   k64,   k65 } \
+    {   k01,   k02,   k03,   k04,   k05,   k06, KC_NO, KC_NO,   k07,   k08,   k09,   k10,   k11,   k12 }, \
+    {   k13,   k14,   k15,   k16,   k17,   k18, KC_NO, KC_NO,   k20,   k21,   k22,   k23,   k24,   k25 }, \
+    {   k26,   k27,   k28,   k29,   k30,   k31,   k32,   k19,   k34,   k35,   k36,   k37,   k38,   k39 }, \
+    {   k40,   k41,   k42,   k43,   k44,   k45,   k46,   k33,   k47,   k48,   k49,   k50,   k51,   k52 }, \
+    {   k53,   k54,   k55,   k56,   k57,   k58,   k59,   k60,   k61,   k62,   k63,   k64,   k65,   k66 } \
 }
-
-#endif
-
