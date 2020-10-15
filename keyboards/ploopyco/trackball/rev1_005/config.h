40b91cff7dc1db0f987b37c7ea8e5afe0075cdf3
Drashna Jael're <drashna@live.com>
2020-10-15 15:12:33 -0700

    more pins

--- keyboards/ploopyco/trackball/rev1_005/config.h
+++ keyboards/ploopyco/trackball/rev1_005/config.h
@@ -37,3 +37,6 @@
 // They are set as output and pulled high, by default
 #define UNUSED_PINS \
     { D1, D3, B4, B5, B7, D6, C7, F6, F5, F3, F7 }
+
+// If board has a debug LED, you can enable it by defining this
+#define DEBUG_LED_PIN B5
