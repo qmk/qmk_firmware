39bba12b147db7680ccbdd87c4d99611b849adc8
Lei Yu <yuleiz@gmail.com>
2020-10-15 18:21:28 +0800

    added license header and move the ws2812 codes to a seperate c file

--- keyboards/dp60/keymaps/indicator/led_driver.c
+++ keyboards/dp60/keymaps/indicator/led_driver.c
@@ -1,7 +1,7 @@
 /**
- * config.h
+ * led_driver.c
  *
-  Copyright 2020 astro <yuleiz@gmail.com>
+ Copyright 2020 astro <yuleiz@gmail.com>
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 2 of the License, or
@@ -12,14 +12,15 @@
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-#pragma once
+*/
 
-#include "config_common.h"
+#undef RGB_DI_PIN
+#define RGB_DI_PIN RGB_INDICATOR_PIN
+#define ws2812_setleds indicator_setleds
+#define ws2812_setleds_pin indicator_setleds_pin
+#include "ws2812.c"
 
-#define RGB_INDICATOR_NUM 8
-#undef RGBLED_NUM
-#define RGBLED_NUM  (18+RGB_INDICATOR_NUM)
-
-#define RGB_INDICATOR_PIN B1
-#define RGBLIGHT_LAYERS
+void indicator_write(LED_TYPE *start_led, uint8_t num_leds)
+{
+    indicator_setleds(start_led, num_leds);
+}
