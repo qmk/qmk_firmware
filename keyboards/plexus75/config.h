e3b6956f45e75e489102f1b7eab3ecab0261b5d6
npspears <40127181+npspears@users.noreply.github.com>
2020-10-16 17:30:09 -0500

    Update config.h

--- keyboards/plexus75/config.h
+++ keyboards/plexus75/config.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x7O70
+#define VENDOR_ID       0x7070
 #define PRODUCT_ID      0x5338
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    nasp
