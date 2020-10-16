de16c6419d2576f6568b7d03592ce7b0a5138363
Sönke Hahn <soenkehahn@gmail.com>
2020-10-16 16:41:25 -0400

    change copyright notice

--- keyboards/moonlander/keymaps/shahn-neo/config.h
+++ keyboards/moonlander/keymaps/shahn-neo/config.h
@@ -1,6 +1,4 @@
-/* Copyright 2020 ZSA Technology Labs, Inc <@zsa>
- * Copyright 2020 Jack Humbert <jack.humb@gmail.com>
- * Copyright 2020 Drashna Jael're  <drashna@live.com>
+/* Copyright 2020 Sönke Hahn <SoenkeHahn@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
