9d4706af92b8f269476a3c94741739eb739aab3c
npspears <40127181+npspears@users.noreply.github.com>
2020-10-16 17:02:25 -0500

    Update keyboards/plexus75/rules.mk
    Co-authored-by: Erovia <Erovia@users.noreply.github.com>

--- keyboards/plexus75/rules.mk
+++ keyboards/plexus75/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = atmel-dfu
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -20,4 +20,3 @@ UNICODE_ENABLE = yes        # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-
