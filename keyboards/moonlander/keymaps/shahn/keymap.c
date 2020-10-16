0df7819580159ef653d5e7bf287dbb4b497f05bb
Sönke Hahn <soenkehahn@gmail.com>
2020-10-16 16:27:22 -0400

    add comment

--- keyboards/moonlander/keymaps/shahn/keymap.c
+++ keyboards/moonlander/keymaps/shahn/keymap.c
@@ -16,6 +16,21 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* This is my personal keyboard layout based on the Neo keyboard layout:
+ * https://en.wikipedia.org/wiki/Neo_(keyboard_layout)
+ * This layout is not meant as a general layout for other people using the
+ * Neo keyboard layout. It's very specific to my personal needs.
+ * It still might be useful as inspiration to other people that want to
+ * create a Neo layout.
+ *
+ * Some notes:
+ * - I've put the ctrl key, the shift keys and the modifiers to switch to
+ *   layer 4 in the middle to be operated by the thumbs. I wanted to avoid
+ *   having to use my pinkies on the outer rows of the keyboard.
+ * - The umlauts (äöü) and the ß only work on linux when configuring the
+ *   compose key properly. See 'compose_key' below.
+ */
+
 #include QMK_KEYBOARD_H
 #include "version.h"
 
