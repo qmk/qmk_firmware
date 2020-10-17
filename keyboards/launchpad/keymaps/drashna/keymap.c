7f9db03dc3d3bf0615da58965185c8ecf7b191dd
Drashna Jael're <drashna@live.com>
2020-10-17 07:16:58 -0700

    fix review issues

--- keyboards/launchpad/keymaps/drashna/keymap.c
+++ keyboards/launchpad/keymaps/drashna/keymap.c
@@ -1,13 +1,26 @@
-// Below layout is based upon /u/That-Canadian's planck layout
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _RGB 1
-#define _FUNC 2
+enum local_layers {
+    _QWERTY,
+    _RGB,
+    _FUNC,
+};
 
 // Defines for task manager and such
 #define CALTDEL LCTL(LALT(KC_DEL))
