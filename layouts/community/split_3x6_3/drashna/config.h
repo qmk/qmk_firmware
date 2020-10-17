855dd2d218ba875f1fb7bddfbce8bd260e96184e
Drashna Jaelre <drashna@live.com>
2020-10-17 11:46:57 -0700

    [Keymap] Drashna Code Cleanup (#10656)
    * Add Launchpad keymap
    
    Note: RGB and Audio won't work when using B pins for audio
    
    * Add support for rgb matrix on launchpad
    
    * Update config for launchpag
    
    * Disable wait on layer change
    
    * Update config for ErgoDox EZ
    
    * Fixup rgb light userspace code
    
    * Move Corne layout to community layouts folder
    
    * Update config for corne to support community layouts
    
    * Add license header to files
    
    * Minor cleanup of userspace config
    
    * Update Pimironi Trackball code
    
    * Increase debounce time on ergodox
    
    * Fix keymap handling
    
    * Enable wait for USB for moonlander
    
    * Update/add license headers
    
    * fix review issues

--- layouts/community/split_3x6_3/drashna/config.h
+++ layouts/community/split_3x6_3/drashna/config.h
@@ -1,22 +1,18 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -70,7 +66,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef PRODUCT
 #define PRODUCT Drashna Hacked Corne Keyboard
 
-#define OLED_FONT_H "keyboards/crkbd/keymaps/drashna/glcdfont.c"
+#define OLED_FONT_H "layouts/community/split_3x6_3/drashna/glcdfont.c"
 // #define OLED_FONT_WIDTH 5
 // #define OLED_FONT_HEIGHT 7
 
