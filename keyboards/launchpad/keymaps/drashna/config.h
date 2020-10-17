f64c4822c372a9496521e1fb49b47b508755390e
Drashna Jaelre <drashna@live.com>
2020-10-17 11:46:57 -0700

    [Keymap] Drashna Code Cleanup (#10656)
    * Add Launchpad keymap
    
    Note: RGB and Audio won't work when using B pins for audio
    
    * Add support for rgb matrix on launchpad
    
    * Update config for launchpag
    
    * Disable wait on layer change
    
    * Update config for ErgoDox EZ
    
    * Fixup rgb light userspace code
    
    * Move Corne layout to community layouts folder
    
    * Update config for corne to support community layouts
    
    * Add license header to files
    
    * Minor cleanup of userspace config
    
    * Update Pimironi Trackball code
    
    * Increase debounce time on ergodox
    
    * Fix keymap handling
    
    * Enable wait for USB for moonlander
    
    * Update/add license headers
    
    * fix review issues

--- keyboards/launchpad/keymaps/drashna/config.h
+++ keyboards/launchpad/keymaps/drashna/config.h
@@ -15,24 +15,17 @@
  */
 
 #pragma once
-#include "quantum.h"
 
-#if defined(RGBLIGHT_TWINKLE)
-typedef struct {
-    bool     enabled;
-    uint8_t  hue;
-    uint16_t timer;
-    uint8_t  life;
-} rgblight_fadeout;
-#endif
+/* Underlight Configuration */
+#undef RGB_DI_PIN
+#define RGB_DI_PIN F5
+#define RGBLIGHT_ANIMATIONS
+#undef RGBLED_NUM
+#define RGBLED_NUM 8  // Number of LEDs
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
 
-bool          process_record_user_rgb_light(uint16_t keycode, keyrecord_t *record);
-void          keyboard_post_init_rgb_light(void);
-void          matrix_scan_rgb_light(void);
-layer_state_t layer_state_set_rgb_light(layer_state_t state);
-layer_state_t default_layer_state_set_rgb_light(layer_state_t state);
-void          rgblight_sethsv_default_helper(uint8_t index);
-
-#if defined(RGBLIGHT_TWINKLE)
-void scan_rgblight_fadeout(void);
-#endif
+#define DRIVER_LED_TOTAL RGBLED_NUM
+#define B7_AUDIO
+#define AUDIO_CLICKY
