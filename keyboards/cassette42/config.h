bf58b51624c5f1659a7eef9a753b9c9bc1a01d4b
Monksoffunk <monksoffunk@users.noreply.github.com>
2020-10-19 07:53:59 +0900

    Update keyboards/cassette42/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/cassette42/config.h
+++ keyboards/cassette42/config.h
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define USE_I2C
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
