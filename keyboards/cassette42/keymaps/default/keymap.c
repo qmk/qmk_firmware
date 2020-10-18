571ea0236b6e40806a606df68068f767078ea132
Monksoffunk <monksoffunk@users.noreply.github.com>
2020-10-19 07:54:18 +0900

    Update keyboards/cassette42/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/cassette42/keymaps/default/keymap.c
+++ keyboards/cassette42/keymaps/default/keymap.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-extern keymap_config_t keymap_config;
 #ifdef RGBLIGHT_ENABLE
 // Following line allows macro to read current RGB settings
 extern rgblight_config_t rgblight_config;
