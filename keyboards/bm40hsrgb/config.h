bf97fe1a2b7d652937c8bdae3ec2ea8a6a935942
strawy <straw.dylan@gmail.com>
2020-10-18 17:22:09 +1100

    Update keyboards/bm40hsrgb/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/bm40hsrgb/config.h
+++ keyboards/bm40hsrgb/config.h
@@ -19,7 +19,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4B50 // "KP"
-#define PRODUCT_ID      0x3430
+#define PRODUCT_ID      0x3430 // "40"
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    KPRepublic
 #define PRODUCT         BM40 Hotswap RGB
