242d914b2ff6ae0d087128d602133ebbb33817a3
chent7 <34766434+chent7@users.noreply.github.com>
2020-10-19 22:49:42 +0800

    Update config.h

--- keyboards/crazy_keyboard_68/config.h
+++ keyboards/crazy_keyboard_68/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER   0x0001
 #define MANUFACTURER chent7
 #define PRODUCT      Crazy Keyboard 68
-#define DESCRIPTION  Crazy Keyboard 68 65pc keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
