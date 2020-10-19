f1186882408cae90b199e28409004320f5bdf03a
fauxpark <fauxpark@gmail.com>
2020-10-20 01:31:28 +1100

    Tweak Arch script

--- util/install/arch.sh
+++ util/install/arch.sh
@@ -3,14 +3,14 @@
 _qmk_install() {
     echo "Installing dependencies"
 
-    sudo pacman --needed -S \
+    sudo pacman --needed  --noconfirm -S \
         base-devel clang diffutils gcc git unzip wget zip \
         python-pip \
         avr-binutils \
         arm-none-eabi-binutils arm-none-eabi-gcc arm-none-eabi-newlib \
         avrdude dfu-programmer dfu-util
-    sudo pacman --needed -U https://archive.archlinux.org/packages/a/avr-gcc/avr-gcc-8.3.0-1-x86_64.pkg.tar.xz
-    sudo pacman --needed -S avr-libc # Must be installed after the above, or it will bring in the latest avr-gcc instead
+    sudo pacman --needed --noconfirm -U https://archive.archlinux.org/packages/a/avr-gcc/avr-gcc-8.3.0-1-x86_64.pkg.tar.xz
+    sudo pacman --needed --noconfirm -S avr-libc # Must be installed after the above, or it will bring in the latest avr-gcc instead
 
     python3 -m pip install --user -r $QMK_FIRMWARE_DIR/requirements.txt
 }
