54c11b4aff6cdadbfb9057dd4f5af71ce33ba941
Neil Brian Ramirez <nightlykeyboards@gmail.com>
2020-10-19 22:15:41 +0800

    Corrected the rows and columns of encoders on config.h

--- keyboards/nightly_boards/octopad/config.h
+++ keyboards/nightly_boards/octopad/config.h
@@ -59,8 +59,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENCODERS_PAD_A { C7, F4 }
 #define ENCODERS_PAD_B { C6, F5 }
 
-#define ENCODERS_CW_KEY  { { 1, 2 },{ 3, 2 } }
-#define ENCODERS_CCW_KEY { { 0, 2 },{ 2, 2 } }
+#define ENCODERS_CW_KEY  { { 3, 2 },{ 1, 2 } }
+#define ENCODERS_CCW_KEY { { 2, 2 },{ 0, 2 } }
 
 #define ENCODER_RESOLUTION 4
 
