992aec441af304aa33ee3df0c77183f17f3a7ccb
Seth <7864633+fl3tching101@users.noreply.github.com>
2020-10-19 23:07:56 -0500

    Updated dimming step, OLED functionality, and rules for formatting
    - Changed the dimming step size for smoother dimming with the reduced range
    - Added lots of OLED functionality - QMK logo and title at top, num/caps/scroll lock status, and backlight brightness level
    - Updated the rules.mk file to comply more closely with the formatting guidelines

--- keyboards/rocketboard_16/config.h
+++ keyboards/rocketboard_16/config.h
@@ -42,6 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN A4
 #define RGBLED_NUM 16
 #define RGBLIGHT_LIMIT_VAL 128
+#define RGBLIGHT_VAL_STEP 8
 #define RGBLIGHT_SLEEP
 
 // OLED stuff
--- keyboards/rocketboard_16/rules.mk
+++ keyboards/rocketboard_16/rules.mk
@@ -7,15 +7,19 @@ BOOTLOADER = stm32duino
 # Build Options
 #   change yes to no to disable
 
-BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	      # USB Nkey Rollover
-BACKLIGHT_ENABLE = no
-RGBLIGHT_ENABLE = yes
+BOOTMAGIC_ENABLE = yes		# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes			# Mouse keys
+EXTRAKEY_ENABLE = yes			# Audio control and System control
+CONSOLE_ENABLE = yes			# Console for debug
+COMMAND_ENABLE = yes    	# Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no  		# Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes	      	# USB Nkey Rollover
+BACKLIGHT_ENABLE = no			# Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes			# Enable keyboard RGB backlit keys
+BLUETOOTH_ENABLE = no			# Enables Bluetooth
+AUDIO_ENABLE = no					# Audio output
 OLED_DRIVER_ENABLE = yes
 ENCODER_ENABLE = yes
 
