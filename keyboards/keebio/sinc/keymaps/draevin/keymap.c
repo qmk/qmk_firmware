4441a54ea0fd62621d335024ac6a1ae55654f891
Draevin Luke <contact@drae.vin>
2020-10-21 08:00:35 -0700

    License blitz

--- keyboards/keebio/sinc/keymaps/draevin/keymap.c
+++ keyboards/keebio/sinc/keymaps/draevin/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Draevin Luke contact@drae.vin @draevin
+/* Copyright 2020 Draevin Luke <contact@drae.vin> @draevin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
