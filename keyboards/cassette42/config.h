3018e47c9c8e6e3620f8760676dbc20b3a33871e
Monksoffunk <monksoffunk@users.noreply.github.com>
2020-10-23 12:56:14 +0900

    Update keyboards/cassette42/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/cassette42/config.h
+++ keyboards/cassette42/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0xca42
+#define PRODUCT_ID      0xCA42
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    monksoffunk
 #define PRODUCT         cassette42
