4917d9484b98a028e1635588b58d9c5184534300
Fidel Coria <1coriaF@gmail.com>
2020-10-23 07:23:40 -0500

    Apply suggestions from code review part 2
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/pteron44/keymaps/default/keymap.c
+++ keyboards/handwired/pteron44/keymaps/default/keymap.c
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               |Lower | SPC  | Alt  | GUI  |      | Ctrl | GUI  | SPC  |Raise |
  *               +---------------------------+      +---------------------------+
  */
-[_QWERTY] = LAYOUT( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-                    LOWER,   KC_SPC,  KC_LALT, KC_LGUI,         KC_RCTL, KC_RGUI, KC_SPC,  RAISE \
+[_QWERTY] = LAYOUT(
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+                    LOWER,   KC_SPC,  KC_LALT, KC_LGUI,         KC_RCTL, KC_RGUI, KC_SPC,  RAISE
 ),
 
 /* Lower
