e8028a3df6ee3825c575ebf41cf70875b2aee0ed
mechlovin <57231893+mechlovin@users.noreply.github.com>
2020-10-24 23:25:12 +0700

    Update config.h

--- keyboards/mechlovin/hannah65/rgb_led/config.h
+++ keyboards/mechlovin/hannah65/rgb_led/config.h
@@ -1,3 +1,10 @@
 #pragma once
 
-#define RGB_DI_PIN A15
+#ifdef RGBLIGHT_ENABLE
+     #define RGB_DI_PIN A15
+     #define RGBLED_NUM      23
+     #define RGBLIGHT_ANIMATIONS
+     #define RGBLIGHT_HUE_STEP 8
+     #define RGBLIGHT_SAT_STEP 8
+     #define RGBLIGHT_VAL_STEP 8
+#endif
