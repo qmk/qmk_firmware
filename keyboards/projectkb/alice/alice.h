2f2781e92679c2ceb3f4b4dce6e30f8301aa1943
stanrc85 <stanrc@gmail.com>
2020-10-23 20:19:06 -0400

    add define to support LAYOUT_default

--- keyboards/projectkb/alice/alice.h
+++ keyboards/projectkb/alice/alice.h
@@ -3,6 +3,7 @@
 #include "quantum.h"
 
 #define KNO KC_NO
+#define LAYOUT_default LAYOUT_alice
 
 #define LAYOUT_alice( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
