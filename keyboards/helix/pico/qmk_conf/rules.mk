b6bb6c01f8e0790e09acdbe4f2559a74d68a81fe
mtei <2170248+mtei@users.noreply.github.com>
2020-10-24 18:33:07 +0900

    fix helix/pico/qmk_conf compile error

--- keyboards/helix/pico/qmk_conf/rules.mk
+++ keyboards/helix/pico/qmk_conf/rules.mk
@@ -1,5 +1,4 @@
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 AUDIO_ENABLE = yes          # Audio output on port B5
-LED_BACK_ENABLE = yes
 
 include $(strip $(KEYBOARD_LOCAL_FEATURES_MK))
