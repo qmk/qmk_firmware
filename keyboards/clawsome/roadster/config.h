1eeb36e570bdfa49c1866c47a5803519b1ed7d99
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2020-10-24 10:31:41 -0600

    Update keyboards/clawsome/roadster/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/clawsome/roadster/config.h
+++ keyboards/clawsome/roadster/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AlisGraveNil
 #define PRODUCT         Roadster
-#define DESCRIPTION     QMK Firmware: Roadster1.0
 
 /* key matrix size */
 #define MATRIX_ROWS 4
