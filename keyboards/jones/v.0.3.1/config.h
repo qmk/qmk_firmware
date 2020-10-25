c9982121f88f4bd70c42421416bbc134dd105180
Takeshi Nishio <kkeennnn@gmail.com>
2020-10-26 05:36:01 +0900

    Update RGBLIGHT明るさ調整、エフェクト追加。

--- keyboards/jones/v.0.3.1/config.h
+++ keyboards/jones/v.0.3.1/config.h
@@ -76,7 +76,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 220 /* The maximum brightness level */
+  #define RGBLIGHT_LIMIT_VAL 250 /* The maximum brightness level */
   // #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
   /*== Lighting Layers ==*/
   #define RGBLIGHT_LAYERS
@@ -86,10 +86,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   // #define RGBLIGHT_ANIMATIONS
   /*== or choose animations ==*/
   // #define RGBLIGHT_EFFECT_BREATHING
-  // #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
   // #define RGBLIGHT_EFFECT_SNAKE
-  // #define RGBLIGHT_EFFECT_KNIGHT
+  #define RGBLIGHT_EFFECT_KNIGHT
   // #define RGBLIGHT_EFFECT_CHRISTMAS
   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
   // #define RGBLIGHT_EFFECT_RGB_TEST
