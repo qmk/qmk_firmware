a2f1f7733d5777a7c46abed8df1487426d76f3b3
Brandon Claveria <48102030+swiftrax@users.noreply.github.com>
2020-10-25 13:33:05 -0700

    Change PID

--- keyboards/handwired/swiftrax/bebol/config.h
+++ keyboards/handwired/swiftrax/bebol/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x04D8
-#define PRODUCT_ID      0xEAEF
+#define PRODUCT_ID      0xEAC4
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Swiftrax
 #define PRODUCT         Bebol
