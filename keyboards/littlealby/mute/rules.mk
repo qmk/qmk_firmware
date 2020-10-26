86388be8fee719c1fc870643a2c04843decaaca8
Alby Barber <alby@albybarber.com>
2020-10-26 10:47:25 -0400

    Update keyboards/littlealby/mute/rules.mk
    Update tabbing
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/littlealby/mute/rules.mk
+++ keyboards/littlealby/mute/rules.mk
@@ -14,13 +14,16 @@ BOOTLOADER = caterina
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE  = no	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE  = no	# Mouse keys
-EXTRAKEY_ENABLE  = yes	# Audio control and System control
-CONSOLE_ENABLE  = yes	# Console for debug
-COMMAND_ENABLE  = yes   # Commands for debug and configuration
-SLEEP_LED_ENABLE  = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE  = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE  = no  # Custom backlighting code is used, so this should not be enabled
-AUDIO_ENABLE  = no 		# This can be enabled if a speaker is connected to the expansion port. Not compatible with RGBLIGHT below
-RGBLIGHT_ENABLE  = yes 	# This can be enabled if a ws2812 strip is connected to the expansion port.
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
