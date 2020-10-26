b3fc142825cc2f5be5e617535e9e07433ae65db7
Vlad Kvitnevskiy <vladkvit@outlook.com>
2020-10-26 08:23:27 -0700

    filling out boilerplate

---
+++ keyboards/orthograph/config.h
@@ -0,0 +1,45 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x564B
+#define PRODUCT_ID      0x0602
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    vladkvit
+#define PRODUCT         Orthograph
+#define DESCRIPTION     An ortho split keyboard with 6 rows
+
+/* key matrix size */
+/* rows are doubled-up */
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 8
+
+/* wiring of each half */
+#define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2 }
+#define MATRIX_COL_PINS { B5, B4, D7, C6, D4, D2, D3, F4 }
+#define EE_HANDS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 3
+
+/* serial.c configuration for split keyboard */
+#define SOFT_SERIAL_PIN E6
