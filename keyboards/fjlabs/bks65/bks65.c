142cc94b9de6ef05a248e4f09c01df19a1b77f8b
Felix Jen <fjen@felixjen.com>
2020-10-26 11:21:26 -0500

    Updated Caps LED Pin

--- keyboards/fjlabs/bks65/bks65.c
+++ keyboards/fjlabs/bks65/bks65.c
@@ -15,14 +15,14 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 void matrix_init_kb(void) {
     // Initialize indicator LEDs to output
-    setPinOutput(F7); // Caps
+    setPinOutput(C7); // Caps
     matrix_init_user();
 }
 
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if(res) {
-        writePin(F7, led_state.caps_lock);
+        writePin(C7, led_state.caps_lock);
     }
     return res;
 }
