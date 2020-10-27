a602ef75fba2a7c6df932d049d9f89856bafe6c1
stanrc85 <47038504+stanrc85@users.noreply.github.com>
2020-10-27 13:35:38 -0400

    [Keyboard] Add Projectkb/alice/rev2 LAYOUT_alice support (#10697)
    * my alice community layout
    
    * add LAYOUT_alice support to projectkb/alice/rev2
    
    * add addition BSPC key to default layout
    
    * add define to support LAYOUT_default

--- keyboards/projectkb/alice/alice.h
+++ keyboards/projectkb/alice/alice.h
@@ -3,8 +3,9 @@
 #include "quantum.h"
 
 #define KNO KC_NO
+#define LAYOUT_default LAYOUT_alice
 
-#define LAYOUT_default( \
+#define LAYOUT_alice( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
   K10, K11, K12, K13, K14, K15, K16,      K18, K19, K1A, K1B, K1C, K1D, K1E, K1F, \
   K20, K21, K22, K23, K24, K25, K26,      K28, K29, K2A, K2B, K2C, K2D,      K2F, \
