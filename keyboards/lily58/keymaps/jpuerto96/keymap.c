8e5a3643e7f5d1dced3c598b0094fd4386da846e
Juan Puerto <jpuerto96@hotmail.com>
2020-10-28 12:48:33 -0400

    Added back slash

--- keyboards/lily58/keymaps/jpuerto96/keymap.c
+++ keyboards/lily58/keymaps/jpuerto96/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUM_FUN] = LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   _______, _______, _______, _______, _______, KC_LEFT_PAREN,             KC_RIGHT_PAREN, _______, _______, _______, _______, _______, \
-  _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
+  _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLASH, \
   _______, _______, KC_PLUS, KC_MINUS, KC_ASTERISK, KC_SLASH, KC_EQUAL, KC_EQUAL, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_PIPE, _______, _______, \
                          _______, _______, TG(_NUM_FUN), _______, _______, TO(_NAV_SYM), _______, _______\
 ),
