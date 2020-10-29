23c35c6328f0cdcb3df5ffadcad02eafcf0c62cd
zvecr <git@zvecr.com>
2020-10-29 00:26:03 +0000

    Convert cli to generate-docs

--- lib/python/qmk/cli/docs.py
+++ lib/python/qmk/cli/docs.py
@@ -1,7 +1,5 @@
-"""Interact with QMK documentation locally
+"""Build QMK documentation locally
 """
-import http.server
-import os
 import shutil
 import subprocess
 from pathlib import Path
@@ -12,24 +10,8 @@ DOCS_PATH = Path('docs/')
 BUILD_PATH = Path('.build/docs/')
 
 
-def serve(dir):
-    """Spin up a local HTTPServer instance for the QMK docs.
-    """
-    os.chdir(dir)
-
-    with http.server.HTTPServer(('', cli.config.docs.port), http.server.SimpleHTTPRequestHandler) as httpd:
-        cli.log.info("Serving %s QMK docs at http://localhost:%d/", dir, cli.config.docs.port)
-        cli.log.info("Press Control+C to exit.")
-
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            cli.log.info("Stopping HTTP server...")
-        finally:
-            httpd.shutdown()
-
-
-def generate():
+@cli.subcommand('Local interactions wth QMK documentation.', hidden=False if cli.config.user.developer else True)
+def generate_docs(cli):
     """Invoke the docs generation process
 
     TODO(unclaimed):
@@ -53,18 +35,3 @@ def generate():
     subprocess.run(['moxygen', '-q', '-a', '-g', '-o', BUILD_PATH / 'internals_%s.md', 'doxygen/xml'], **args)
 
     cli.log.info('Successfully generated internal docs to %s.', BUILD_PATH)
-
-
-@cli.argument('-g', '--generate', arg_only=True, action='store_true', help='Generate docs.')
-@cli.argument('-s', '--serve', arg_only=True, action='store_true', help='Serve docs locally.')
-@cli.argument('-p', '--port', default=8936, type=int, help='Port number to use.')
-@cli.subcommand('Local interactions wth QMK documentation.', hidden=False if cli.config.user.developer else True)
-def docs(cli):
-    if cli.args.generate:
-        generate()
-        # have we been asked to just serve the just generated docs?
-        if cli.args.serve:
-            serve(BUILD_PATH)
-    else:
-        # if --serve or invoked with no args
-        serve(DOCS_PATH)
