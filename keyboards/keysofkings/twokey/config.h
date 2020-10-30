fd74984330df2ba277a66cdface00061d9e90639
Keys of Kings <keysofkings@gmail.com>
2020-10-30 08:57:43 -0500

    Update keyboards/keysofkings/twokey/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keysofkings/twokey/config.h
+++ keyboards/keysofkings/twokey/config.h
@@ -20,7 +20,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define PRODUCT_ID      0xAE12
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Keys of Kings
 #define PRODUCT         Twokey
