b1d5952a235b6af9a8351a093ca9843abec7917e
Alex Girard <me@alexgirard.ca>
2020-11-01 00:26:15 -0400

    seting up pin mapping for left side

--- keyboards/handwired/dactyl_manuform/5x6_alexgirarddev/5x6_alexgirarddev.h
+++ keyboards/handwired/dactyl_manuform/5x6_alexgirarddev/5x6_alexgirarddev.h
@@ -11,12 +11,13 @@
 #endif
 #endif
 
+
 #define LAYOUT_5x6_alexgirarddev(\
   L00, L01, L02, L03, L04, L05,                          R00, R01, R02, R03, R04, R05, \
   L10, L11, L12, L13, L14, L15,                          R10, R11, R12, R13, R14, R15, \
   L20, L21, L22, L23, L24, L25,                          R20, R21, R22, R23, R24, R25, \
   L30, L31, L32, L33, L34, L35,                          R30, R31, R32, R33, R34, R35, \
-            L42, L43,                                              R42, R43,           \
+  L40, L41, L42,                                              		R42, R43, R45  \
                       L44, L45,                          R40, R41,                     \
                                 L54, L55,      R50, R51,                               \
                                 L52, L53,      R52, R53                                \
