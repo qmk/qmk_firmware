da8be0e90119798bfc1fccc36678d32892fce798
zvecr <git@zvecr.com>
2020-11-01 00:56:41 +0000

    Slight tidy up of backlight caps logic

--- quantum/led.c
+++ quantum/led.c
@@ -1,12 +1,52 @@
+/* Copyright 2020 zvecr<git@zvecr.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "quantum.h"
 
 #ifdef BACKLIGHT_ENABLE
 #    include "backlight.h"
 extern backlight_config_t backlight_config;
+#else
+// Cannot use BACKLIGHT_CAPS_LOCK without backlight being enabled
+#    undef BACKLIGHT_CAPS_LOCK
 #endif
 
 #ifndef LED_PIN_ON_STATE
 #    define LED_PIN_ON_STATE 0
+#endif
+
+#if defined(BACKLIGHT_CAPS_LOCK)
+/** \brief Caps Lock indicator using backlight (for keyboards without dedicated LED)
+ */
+static void handle_backlight_caps_lock(led_t led_state) {
+    // Use backlight as Caps Lock indicator
+    uint8_t bl_toggle_lvl = 0;
+
+    if (led_state.caps_lock && !backlight_config.enable) {
+        // Turning Caps Lock ON and backlight is disabled in config
+        // Toggling backlight to the brightest level
+        bl_toggle_lvl = BACKLIGHT_LEVELS;
+    } else if (!led_state.caps_lock && backlight_config.enable) {
+        // Turning Caps Lock OFF and backlight is enabled in config
+        // Toggling backlight and restoring config level
+        bl_toggle_lvl = backlight_config.level;
+    }
+
+    // Set level without modify backlight_config to keep ability to restore state
+    backlight_set(bl_toggle_lvl);
+}
 #endif
 
 /** \brief Lock LED set callback - keymap/user level
@@ -67,6 +107,8 @@ __attribute__((weak)) bool led_update_kb(led_t led_state) {
     return res;
 }
 
+/** \brief Initialise any LED related hardware and/or state
+ */
 __attribute__((weak)) void led_init_ports(void) {
 #ifdef LED_NUM_LOCK_PIN
     setPinOutput(LED_NUM_LOCK_PIN);
@@ -85,23 +127,11 @@ __attribute__((weak)) void led_init_ports(void) {
 #endif
 }
 
+/** \brief Entrypoint for protocol to LED binding
+ */
 __attribute__((weak)) void led_set(uint8_t usb_led) {
-#if defined(BACKLIGHT_CAPS_LOCK) && defined(BACKLIGHT_ENABLE)
-    // Use backlight as Caps Lock indicator
-    uint8_t bl_toggle_lvl = 0;
-
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK) && !backlight_config.enable) {
-        // Turning Caps Lock ON and backlight is disabled in config
-        // Toggling backlight to the brightest level
-        bl_toggle_lvl = BACKLIGHT_LEVELS;
-    } else if (IS_LED_OFF(usb_led, USB_LED_CAPS_LOCK) && backlight_config.enable) {
-        // Turning Caps Lock OFF and backlight is enabled in config
-        // Toggling backlight and restoring config level
-        bl_toggle_lvl = backlight_config.level;
-    }
-
-    // Set level without modify backlight_config to keep ability to restore state
-    backlight_set(bl_toggle_lvl);
+#ifdef BACKLIGHT_CAPS_LOCK
+    handle_backlight_caps_lock((led_t)usb_led);
 #endif
 
     led_set_kb(usb_led);
