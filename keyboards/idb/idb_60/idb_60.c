20a191ac528e7476759062477d3bdb251903d6d3
itspngu <34752364+itspngu@users.noreply.github.com>
2020-11-01 23:36:16 +0100

    Fixed VIA keymap build warning on gcc 10.2.0, Fixed misleading LED function names, Fixed formatting

--- keyboards/idb/idb_60/idb_60.c
+++ keyboards/idb/idb_60/idb_60.c
@@ -5,15 +5,26 @@ void keyboard_pre_init_kb(void) {
     setPinOutput(C5);
 }
 
+// Capslock LED indicator
+
 void led_set_kb(uint8_t usb_led) {
-
     if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-		_idb_60_caps_led_on();
-		} else {
-		_idb_60_caps_led_off();
+        _idb_60_caps_led_on();
+    } else {
+        _idb_60_caps_led_off();
     }
+    led_set_user(usb_led);
+}
 
-	led_set_user(usb_led);
+// Layer LED indicator - drive LED when not on base layer
+
+layer_state_t layer_state_set_kb(layer_state_t state) {
+    if (get_highest_layer(state) == 0) {
+        _idb_60_fn_led_off();
+    } else {
+        _idb_60_fn_led_on();
+    }
+    return state;
 }
 
 inline void _idb_60_caps_led_on(void) {
