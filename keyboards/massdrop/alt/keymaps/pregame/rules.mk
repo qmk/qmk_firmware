4f90ae3ada130775c8590db2c7db2184af2f84ed
elijahblake81 <elijahblake81@gmail.com>
2020-11-01 22:48:53 -0600

    Changed some settings on rules for clean up.

--- keyboards/massdrop/alt/keymaps/pregame/rules.mk
+++ keyboards/massdrop/alt/keymaps/pregame/rules.mk
@@ -1,7 +1,7 @@
 # RGBLIGHT_ENABLE = no			# Not for MD boards. This is here in case you forget.
 COMMAND_ENABLE = no			# Commands for debug and configuration
-# BOOTMAGIC_ENABLE = full		# Virtual DIP switch configuration
-# BOOTMAGIC_ENABLE = lite
+BOOTMAGIC_ENABLE = no		# Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = no
 # AUTO_SHIFT_ENABLE = yes      	# Auto Shift
 NKRO_ENABLE = yes           	# USB Nkey Rollover
 DYNAMIC_MACRO_ENABLE = no		# Dynamic macro recording and play
