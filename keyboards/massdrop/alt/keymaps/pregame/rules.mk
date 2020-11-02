f37947e2cfdb8270eb80f45794530b4331228e50
elijahblake81 <elijahblake81@gmail.com>
2020-11-01 22:36:59 -0600

    Changed some settings on rules for clean up.

--- keyboards/massdrop/alt/keymaps/pregame/rules.mk
+++ keyboards/massdrop/alt/keymaps/pregame/rules.mk
@@ -1,15 +1,15 @@
 # RGBLIGHT_ENABLE = no			# Not for MD boards. This is here in case you forget.
-COMMAND_ENABLE = yes			# Commands for debug and configuration
+COMMAND_ENABLE = no			# Commands for debug and configuration
 # BOOTMAGIC_ENABLE = full		# Virtual DIP switch configuration
 # BOOTMAGIC_ENABLE = lite
 # AUTO_SHIFT_ENABLE = yes      	# Auto Shift
 NKRO_ENABLE = yes           	# USB Nkey Rollover
-DYNAMIC_MACRO_ENABLE = yes		# Dynamic macro recording and play
-#MOUSEKEY_ENABLE = yes			# Enable mouse control keycodes. Increases firmware size.
-#TAP_DANCE_ENABLE = yes			# Enable tap dance keys
-#CONSOLE_ENABLE = yes			# Enable debugging console. Increases firmware size.
-#TERMINAL_ENABLE = yes
-#EXTRAKEY_ENABLE = yes			# Audio control and System control
+DYNAMIC_MACRO_ENABLE = no		# Dynamic macro recording and play
+MOUSEKEY_ENABLE = no			# Enable mouse control keycodes. Increases firmware size.
+TAP_DANCE_ENABLE = no			# Enable tap dance keys
+CONSOLE_ENABLE = no			# Enable debugging console. Increases firmware size.
+TERMINAL_ENABLE = no
+EXTRAKEY_ENABLE = yes			# Audio control and System control
 # RAW_ENABLE = yes				# Raw HID has not yet been implemented for this keyboard
 # COMBO_ENABLE					# Key combo feature
 # LEADER_ENABLE					# Enable leader key chording
