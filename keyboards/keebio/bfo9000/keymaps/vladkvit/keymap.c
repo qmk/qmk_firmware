e94a66ab5536e158c394f0bc2da1f716ba95fd4f
Vlad Kvitnevskiy <vladkvit@outlook.com>
2020-11-01 17:38:39 -0800

    Removed zip, updated license

--- keyboards/keebio/bfo9000/keymaps/vladkvit/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/vladkvit/keymap.c
@@ -1,5 +1,24 @@
 #include QMK_KEYBOARD_H
 
+/*
+This is the c configuration file for the keymap
+
+Copyright 2020 vladkvit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(KC_NO, KC_NO, KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO,        KC_NO, KC_F6, KC_F7, KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_NO, \
                  KC_NO, KC_NO, KC_GRV, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_NO,        KC_NO, KC_6,  KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
