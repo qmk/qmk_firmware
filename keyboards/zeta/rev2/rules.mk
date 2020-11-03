c389e32a1da762514154c82386e381bc2504316e
Ian Sterling <xyverz@gmail.com>
2020-11-02 19:35:33 -0800

    Further requested changes per @drashna

--- keyboards/zeta/rev2/rules.mk
+++ keyboards/zeta/rev2/rules.mk
@@ -1,37 +1,26 @@
 
 OPT_DEFS += -DZETA_PROMICRO
-OPT_DEFS += -DCATERINA_BOOTLOADER
-INTERPHASE_UPLOAD_COMMAND = while [ ! -r $(USB) ]; do sleep 1; done; \
-                         avrdude -p $(MCU) -c avr109 -U flash:w:$(TARGET).hex -P $(USB)
+
 
 # # project specific files
 SRC = matrix.c
 MCU = atmega32u4
-F_CPU = 8000000
-ARCH = AVR8
-F_USB = $(F_CPU)
+
 BOOTLOADER = caterina
-
-# Interrupt driven control endpoint task(+60)
-OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
-
 
 # Build Options
 #   comment out to disable the options.
-#
-#BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
-CONSOLE_ENABLE = no	# Console for debug(+400)
-COMMAND_ENABLE = yes   # Commands for debug and configuration
-CUSTOM_MATRIX = yes    # Remote matrix from the wireless bridge
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-# SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes		# USB Nkey Rollover - not yet supported in LUFA
-# BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-# MIDI_ENABLE = YES 		# MIDI controls
-UNICODE_ENABLE = YES 		# Unicode
-# BLUETOOTH_ENABLE = yes # Enable Bluetooth with the Adafruit EZ-Key HID
+
+BOOTMAGIC_ENABLE = lite   # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes     # Mouse keys
+EXTRAKEY_ENABLE = yes     # Audio control and System control
+CONSOLE_ENABLE = no       # Console for debug
+COMMAND_ENABLE = yes      # Commands for debug and configuration
+CUSTOM_MATRIX = yes       # Remote matrix from the wireless bridge
+NKRO_ENABLE = yes         # USB Nkey Rollover 
+BACKLIGHT_ENABLE = no     # Enable keyboard backlight functionality
+UNICODE_ENABLE = yes      # Unicode
+BLUETOOTH_ENABLE = no     # Enable Bluetooth 
 
 USB = /dev/ttyACM0
 
