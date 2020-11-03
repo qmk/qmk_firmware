85aa3a26ab8669d827bc0c5c33c74d3dc0ed0519
pabile <1162412+pabile@users.noreply.github.com>
2020-11-03 19:07:37 +0800

    Update keymap.c

--- keyboards/pabile/p20/ver2/keymaps/4encoders/keymap.c
+++ keyboards/pabile/p20/ver2/keymaps/4encoders/keymap.c
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) { /* First encoder */
     if (clockwise) {
-      tap_code(KC_RBRC); /* brush size big */
-    } else {
       tap_code(KC_LBRC); /* brush size small */
+    } else {
+      tap_code(KC_RBRC); /* brush size big */
     }
   } else if (index == 1) { /* Second encoder */  
     if (clockwise) {
