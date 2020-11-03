24a62dc46aae5722d614d70a82606595ef3125af
pabile <1162412+pabile@users.noreply.github.com>
2020-11-03 21:37:54 +0800

    updated p42 with ver2 pcb

--- keyboards/pabile/p42/promicro/readme.md
+++ keyboards/pabile/p42/promicro/readme.md
@@ -9,12 +9,12 @@
 * Hardware Availability: [pabileonline.blogspot.com](https://pabileonline.blogspot.com/)
 
 Make example for this keyboard (after setting up your build environment):
-Pro Micro
+For PCB ver1
 
-    make pabile/p42/promicro:default
+    make pabile/p42/promicro/ver1:default
 
-Feather 32U4 BLE
-  
-    make pabile/p42/feather:default
+For PCB ver2 and above 
+
+    make pabile/p42/promicro/ver2:default
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
---
+++ keyboards/pabile/p42/promicro/ver2/config.h
@@ -0,0 +1,45 @@
+/*
+Copyright 2020 Pabile
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x6666
+#define PRODUCT_ID      0x6670
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Pabile
+#define PRODUCT         P42v2 Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 7
+
+/* pin-out for PROMICRO */
+#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7, E6 }
+#define MATRIX_COL_PINS { B4, F7, B1, B6, B2, B3, F5 }
+
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Encoders
+#define ENCODERS_PAD_A { D3, F5 }
+#define ENCODERS_PAD_B { D2, F4 }
+*/
