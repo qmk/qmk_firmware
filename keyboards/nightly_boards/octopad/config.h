068dc16534d5c5a7edfb87dcdc42bea8609d0bdb
Neil Brian Ramirez <nightlykeyboards@gmail.com>
2020-11-04 22:24:42 +0800

    Changed the Vendor ID to D812 since the first one was already taken

--- keyboards/nightly_boards/octopad/config.h
+++ keyboards/nightly_boards/octopad/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0x0717
+#define VENDOR_ID 0xD812
 #define PRODUCT_ID 0x0004
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Neil Brian Ramirez
