ac59886d3f4da097be97fee3462d3ee4b379f9c5
Jeremy Soller <jeremy@system76.com>
2020-11-05 11:34:51 -0700

    Set default RGB mode, hue, and saturation

--- keyboards/system76/launch_beta_1/config.h
+++ keyboards/system76/launch_beta_1/config.h
@@ -48,7 +48,10 @@
     #define RGB_DISABLE_AFTER_TIMEOUT 0 // OBSOLETE: number of ticks to wait until disabling effects
     #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
     #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 176 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
-    #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_MULTISPLASH // Sets the default mode, if none has been set
+    #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR // Sets the default mode, if none has been set
+    #define RGB_MATRIX_STARTUP_HUE 0
+    #define RGB_MATRIX_STARTUP_SAT 0
+    #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
     #define RGB_MATRIX_DISABLE_KEYCODES // disables control of rgb matrix by keycodes (must use code functions to control the feature)
 #endif
 
