4b7523f493639c791190ad14b518ec86b1c393e8
Oliver Granlund <OlliGranlund@users.noreply.github.com>
2020-11-05 19:56:16 +0200

    Added more GPL text, made requested edits to keymaps

--- keyboards/keebio/iris/keymaps/olligranlund_nordic_v2/keymap.c
+++ keyboards/keebio/iris/keymaps/olligranlund_nordic_v2/keymap.c
@@ -1,11 +1,27 @@
+/* Copyright 2020 Oliver Granlund
+ * 
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 2 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */ 
+
 #include QMK_KEYBOARD_H
 
-extern keymap_config_t keymap_config;
-
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
+enum layers (
+    _QWERTY,
+    _LOWER,
+    _RAISE
+    _ADJUST,
+};
 
 enum custom_keycodes {
 	QWERTY = SAFE_RANGE,
@@ -78,11 +94,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 								  // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	)
 };
-
-void persistent_default_layer_set(uint16_t default_layer) {
-	eeconfig_update_default_layer(default_layer);
-	default_layer_set(default_layer);
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
