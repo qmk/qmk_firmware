859882bd589d3e1f5e4e32b49a1a29b3aa41fbdb
Luke LaCasse <Luke.lacasse13@gmail.com>
2020-11-06 03:32:33 -0500

    Update keyboards/sandortho/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/sandortho/keymaps/default/keymap.c
+++ keyboards/sandortho/keymaps/default/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    	RESET,     KC_LCTRL, KC_LALT, KC_LGUI, KC_NO,      KC_NO,     KC_SPACE,     KC_NO,      KC_NO,    KC_NO,     KC_NO,      KC_NO
     ),
     [_LOWER] = LAYOUT(
-   	KC_NO,     EXCL,     AT,      POUND,   DOLLAR,     PERCENT,   CARAT,        AND,        STAR,     LPAR,      RPAR,        KC_DELETE,               
+   	KC_NO,     KC_EXCL,     KC_AT,      KC_HASH,   KC_DLR,     KC_PERCENT,   KC_CIRC,        KC_AMP,        KC_ASTR,     KC_LPRN,      KC_RPRN,        KC_DELETE,               
    	KC_NO,     KC_F1,    KC_F2,   KC_F3,   KC_F4,      KC_F5,     KC_F6,        KC_F7,      KC_F8,    KC_BSLASH, KC_LBRACKET, KC_RBRACKET,  
    	KC_LSHIFT, KC_F9,    KC_F10,  KC_F11,  KC_F12,     KC_NO,     KC_NO,        KC_NO,      KC_NO,    KC_NO,     KC_NO,       KC_NO,  
    	KC_NO,     KC_LCTRL, KC_LALT, KC_LGUI, KC_NO,      KC_NO,     KC_SPACE,     KC_NO,      KC_HOME,  KC_PGDOWN, KC_PGUP,     KC_END
