bc35697ef5dcb84fa6dd4d40d876de20a3306435
mtei <2170248+mtei@users.noreply.github.com>
2020-11-09 20:18:17 +0900

    update five_rows/oled_display.c

--- keyboards/helix/rev2/keymaps/five_rows/oled_display.c
+++ keyboards/helix/rev2/keymaps/five_rows/oled_display.c
@@ -54,12 +54,12 @@ static void render_logo(struct CharacterMatrix *matrix) {
 static void render_logo(void) {
 #    endif
 
-    static char logo[]={
+    static const char helix_logo[] PROGMEM = {
         0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
         0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
         0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,
         0};
-    oled_write(logo, false);
+    oled_write_P(helix_logo, false);
 #    ifdef RGBLIGHT_ENABLE
     char buf[30];
     if (RGBLIGHT_MODES > 1 && rgblight_is_enabled()) {
@@ -111,15 +111,15 @@ void render_status(void) {
 #    endif
 
     // Render to mode icon
-    static char logo[][2][3]={{{0x95,0x96,0},{0xb5,0xb6,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
+    static const char os_logo[][2][3] PROGMEM ={{{0x95,0x96,0},{0xb5,0xb6,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
     if(keymap_config.swap_lalt_lgui==false){
-        oled_write(logo[0][0], false);
+        oled_write_P(os_logo[0][0], false);
         oled_write_P(PSTR("\n"), false);
-        oled_write(logo[0][1], false);
+        oled_write_P(os_logo[0][1], false);
     } else {
-        oled_write(logo[1][0], false);
+        oled_write_P(os_logo[1][0], false);
         oled_write_P(PSTR("\n"), false);
-        oled_write(logo[1][1], false);
+        oled_write_P(os_logo[1][1], false);
     }
 
     // Define layers here, Have not worked out how to have text displayed for each layer. Copy down the number you see and add a case for it below
@@ -130,8 +130,8 @@ void render_status(void) {
     for (lstate = layer_state, name_num = 0;
          lstate && name_num < sizeof(layer_names)/sizeof(char *);
          lstate >>=1, name_num++) {
-        if( (lstate & 1) != 0 ) {
-            if( layer_names[name_num] ) {
+        if ((lstate & 1) != 0) {
+            if (layer_names[name_num]) {
                 oled_write_P(layer_names[name_num], false);
             }
         }
