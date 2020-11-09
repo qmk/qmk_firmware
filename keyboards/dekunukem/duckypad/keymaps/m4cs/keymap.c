8b01276601d62037498946e9939038f7912b8a89
M4cs <mabridgland@protonmail.com>
2020-11-09 00:55:16 -0500

    Add Keymap ASCII and Clean Up

--- keyboards/dekunukem/duckypad/keymaps/m4cs/keymap.c
+++ keyboards/dekunukem/duckypad/keymaps/m4cs/keymap.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 
-#define ANIM_SIZE 384
+#define LOGO_SIZE 384
 
 bool rgbToggled = false;
 bool altToggled = false;
@@ -49,6 +49,19 @@ enum my_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* 
+  ,=========================================.
+  | Media Prev. | Media Play  | Media Next  |
+  |=========================================|
+  | Mute Sound  | Volume Do.  | Volume Up   |
+  |=========================================|
+  | Macro 1     | Macro 2     | Macro 3     |
+  |=========================================|
+  | Macro 4     | Macro 5     | Macro 6     |
+  |=========================================| ,---------------------.
+  | Macro 7     | Macro 8     | Macro 9     | | RGB Menu | Alt Menu |
+  `=========================================' `--------------------'
+  */
   [_DEFAULT] = LAYOUT(
       KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,
       KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,
@@ -57,14 +70,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       MAC_7, MAC_8, MAC_9,
       RGB_LAYER, ALT_LAYER
   ),
+  /* 
+  ,=========================================.
+  | Plain RGB   | Breathe RGB | Rainbow RGB |
+  |=========================================|
+  | Swirl RGB   | Speed Down  | Speed Up    |
+  |=========================================|
+  | Effect Up   | Sat. Down   | Sat. Up     |
+  |=========================================|
+  | Effect Down | Hue Down    | Hue Up      |
+  |=========================================| ,----------------------.
+  | Toggle RGB  | Brt. Down   | Brt. Up     | | Norm Menu | Alt Menu |
+  `=========================================' `---------------------'
+  */
   [_RGB] = LAYOUT(
       RGB_MODE_PLAIN, RGB_MODE_BREATHE, RGB_MODE_RAINBOW,
       RGB_MODE_SWIRL, RGB_SPD, RGB_SPI,
       RGB_MOD, RGB_SAD, RGB_SAI,
-      RGB_RMOD, RGB_HUI, RGB_HUD,
+      RGB_RMOD, RGB_HUD, RGB_HUI,
       RGB_TOG, RGB_VAD, RGB_VAI,
       RGB_LAYER, ALT_LAYER
   ),
+  /* 
+  ,=========================================.
+  | Macro 10    | Macro 11    | Macro 12    |
+  |=========================================|
+  | Undo        | Cut         | Copy        |
+  |=========================================|
+  | Paste       | Find        | Prnt Scrn.  |
+  |=========================================|
+  | Macro 13    | Macro 14    | Macro 15    |
+  |=========================================| ,----------------------.
+  | Macro 16    | Macro 17    | Macro 18    | | Norm Menu | Alt Menu |
+  `=========================================' `---------------------'
+  */
   [_ALT] = LAYOUT(
       MAC_10, MAC_11, MAC_12,
       KC_UNDO, KC_CUT, KC_COPY,
@@ -76,15 +115,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 static void render_logo(void) {
-   static const char PROGMEM ducky_logo[ANIM_SIZE] = {
+   static const char PROGMEM ducky_logo[LOGO_SIZE] = {
       0, 0, 0, 0,128,128,128,128,128, 0, 0,252,252,252, 0, 0, 0,128,128,128, 0, 0, 0, 0, 0,128,128,128, 0, 0, 0, 0, 0,128,128,128,128,128, 0, 0, 0, 0, 0,252,252,252, 0, 0, 0, 0, 0,128,128,128,128,128,128, 0, 0, 0, 0, 0, 0,128,128,128,128, 0,248,248,248, 56, 56, 56, 56, 56, 56,120,240,224,192, 0, 0,128,128,128,128,128,128, 0, 0, 0, 0, 0, 0, 0, 0,128,128,128,128,128, 0, 0,252,252,252, 0, 0, 0, 0, 0,232, 24, 24,232, 0, 24, 48, 48, 24, 0,232, 24, 24,232, 0, 0,
       0,248,254,255,143, 7, 3, 3, 3, 7,142,255,255,255, 0, 0, 0,255,255,255, 0, 0, 0, 0, 0,255,255,255, 0,248,254,255,143, 7, 3, 3, 3, 3,143, 7, 2, 0, 0,255,255,255,240,120,252,254,231,131, 1, 0, 1, 7, 63,254,240,192, 0,224,252,127, 31, 3, 0, 0,255,255,255,112,112,112,112,112,112, 56, 63, 31, 15,193,231,243,115, 51, 51, 51, 55,255,255,252, 0, 0,248,254,255,143, 7, 3, 3, 3, 7,142,255,255,255, 0, 0, 0, 0, 0, 21,127,127,252,252,252,253,253,252,252,252,255,255,255, 0, 0, 
       0, 0, 3, 7, 15, 15, 14, 14, 14, 7, 3, 15, 15, 15, 0, 0, 0, 3, 7, 15, 15, 14, 14, 6, 3, 15, 15, 15, 0, 0, 3, 7, 7, 15, 14, 14, 14, 14, 7, 7, 2, 0, 0, 15, 15, 15, 0, 0, 0, 1, 3, 15, 15, 12,136,224,224,224,247,127,127, 31, 3, 0, 0, 0, 0, 0, 15, 15, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 7, 15, 14, 14, 14, 6, 3, 15, 15, 15, 0, 0, 0, 3, 7, 15, 15, 14, 14, 14, 7, 3, 15, 15, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 11, 3, 11, 7, 11, 7, 7, 31, 0, 0
    };
-   oled_write_raw_P(ducky_logo, ANIM_SIZE);
+   oled_write_raw_P(ducky_logo, LOGO_SIZE);
 }
 
-/* void raw_hid_receive(uint8_t *data, uint8_t length) {
+
+/* Save this baby for later <3 
+void raw_hid_receive(uint8_t *data, uint8_t length) {
   uint8_t cmd[33] = {0, 0, 3, 0, 45, 66, 6, 6, 4, 32, 24, 77, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
   raw_hid_send(cmd, RAW_EPSIZE);
 } */
