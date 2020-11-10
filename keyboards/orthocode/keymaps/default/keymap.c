a748f5bfefaeac5f18572352c803c76b755841b8
Jordan <blackbournj@gmail.com>
2020-11-11 12:20:00 +1300

    Layout change and implementation of custom keycode

--- keyboards/orthocode/keymaps/default/keymap.c
+++ keyboards/orthocode/keymaps/default/keymap.c
@@ -15,6 +15,11 @@
  */
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  SHIFTSPACE
+};
+
 #define KC_SHSP SHIFTSPACE
 
 
@@ -22,20 +27,20 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
       TG(2),    KC_GESC, KC_1,    KC_2,    KC_3,  KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_HOME,
-      KC_NO,    KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS, KC_DEL,  KC_END,
-      KC_NO,    KC_LCTL, KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+      RGB_TOG,    KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS, KC_DEL,  KC_END,
+      RGB_MOD,    KC_LCTL, KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       KC__MUTE, MO(1),   KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,          KC_UP,
-                KC_LCTL, KC_LGUI, KC_LALT, MO(1),  KC_RSFT, KC_ENT,  KC_BSPC, KC_SPC, MO(1),   KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT),
+                KC_LCTL, KC_LGUI, KC_LALT, MO(1),  KC_RSFT, KC_ENT,  KC_BSPC, KC_SHSP, MO(1),   KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT),
 
   [1] = LAYOUT(
      /*          esc      1        2        3        4       5       6        7                 8          9           0        -         =     home  */
       KC_TRNS, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,           KC_F8,      KC_F9,      KC_F10,  KC_F11,   KC_F12,  KC_TRNS,
      /*          tab      Q       W        E        R        T       Y        U                 I           O          P         \      delete     end */
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,  KC_TRNS,KC_TRNS, LSFT(KC_LBRC), LSFT(KC_RBRC),KC_TRNS, KC_TRNS, KC_TRNS,  KC_PAUS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,  KC_TRNS,KC_TRNS, KC_TRNS,   LSFT(KC_LBRC), LSFT(KC_RBRC), KC_TRNS, KC_TRNS,  KC_PAUS, KC_TRNS,
      /*          caps      A       S        D        F        G      H         J                  K          L         ;        '        enter    */
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,         KC_LBRC,    KC_RBRC, KC_TRNS, KC_TRNS,  KC_TRNS,
      /*         shift      Z         X        C       V       B       N       M                 ,           .        /                       up      */
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS, LSFT(KC_9),    LSFT(KC_0),   KC_TRNS, KC_TRNS,                KC_VOLU,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,       LSFT(KC_9),   LSFT(KC_0), KC_TRNS,                KC_VOLU,
      /*         ctrl     win      alt        fn     th1      th2     th3      th4               fn         alt       ctrl      left     down     right*/
               KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS, KC_VOLD,  KC_TRNS),
   [2] = LAYOUT(
@@ -52,10 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  SHIFTSPACE
-};
+
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -90,9 +92,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void encoder_update_user(uint8_t index, bool clockwise) {
     // Volume control
     if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
         tap_code(KC_VOLD);
+    } else {
+        tap_code(KC_VOLU);
     }
 }
 #endif
