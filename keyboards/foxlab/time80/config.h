f11086723ffd0b97d527d5b4b1ca9068a7f4ba4a
Lukas Alexandre <the.lukelex@gmail.com>
2020-11-10 11:39:12 +0100

    Update keyboards/foxlab/time80/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/foxlab/time80/config.h
+++ keyboards/foxlab/time80/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x6C50
+#define VENDOR_ID    0x464C // "FL"
+#define PRODUCT_ID   0x0005
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Fox Lab
 #define PRODUCT      Time80
