57f060de2e7c129bc507adfca270919a4dfb6b1d
domnantas <domantas@zyro.com>
2020-11-10 21:01:08 +0200

    Swap backspace and enter

--- keyboards/lily58/keymaps/domnantas/keymap.c
+++ keyboards/lily58/keymaps/domnantas/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | RAlt |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LGUI | LAlt |LOWER | / Space /       \ Bksp \  |Enter |RAISE | Del  |
+     *                   | LGUI | LAlt |LOWER | / Space /       \Enter \  | Bksp |RAISE | Del  |
      *                   |      |      |      |/       /         \      \ |      |      |      |
      *                   `----------------------------'           '------''--------------------'
      */
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
         KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         KC_LCTRL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT, \
-                                KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,  KC_BSPC, KC_ENT,  MO(_RAISE), KC_DEL \
+                                KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,  KC_ENT,  KC_BSPC, KC_ENT,  MO(_RAISE), KC_DEL \
     ),
 
     /* LOWER
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LCTRL |   =  |   +  |   -  |   |  |      |-------|    |-------|      |      |      |      |      |      |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LGUI | LAlt |LOWER | / Space /       \ Bksp \  |Enter |RAISE | Del  |
+     *                   | LGUI | LAlt |LOWER | / Space /       \ Enter\  | Bksp |RAISE | Del  |
      *                   |      |      ||||||||/       /         \      \ |      |      |      |
      *                   `----------------------------'           '------''--------------------'
      */
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LCTRL | Undo | Cut  | Copy |Paste |      |-------|    |-------|      |      |MPrev |MNext |MPlPa |      |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LGUI | LAlt |LOWER | / Space /       \ Bksp \  |Enter |RAISE | Del  |
+     *                   | LGUI | LAlt |LOWER | / Space /       \ Enter\  | Bksp |RAISE | Del  |
      *                   |      |      |      |/       /         \      \ |      ||||||||      |
      *                   `----------------------------'           '------''--------------------'
      */
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
      * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  |Enter |RAISE | Del  |
+     *                   | LGUI | LAlt |LOWER | /Space  /       \Enter \  | Bksp |RAISE | Del  |
      *                   |      |      ||||||||/       /         \      \ |      ||||||||      |
      *                   `----------------------------'           '------''--------------------'
      */
