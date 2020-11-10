5cbcb980071aa17ecf89bc1cc4e615284b91cdb9
Pete Curt <pcurt854@gmail.com>
2020-11-10 00:31:19 -0500

    Changed Numpad layer
    The main components are
    
    * Numpad with +-*/^% around it
    * Brackets <>()[]{}
    * Some mouse keys
    * Home/End/PaUp/PgDn in place of the arrow keys

--- keyboards/preonic/keymaps/pcurt854/keymap.c
+++ keyboards/preonic/keymaps/pcurt854/keymap.c
@@ -119,23 +119,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Numpad 3
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * | ____ | XXXX | XXXX | XXXX |   <  |   >  |  =+  |   7  |   8  |   9  |   ^  | ____ |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * | ____ | Ms1  | MsUp | Ms2  |   (  |   )  |  -_  |   4  |   5  |   6  |   %  | ____ |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | ____ | MsLf | MsDn | MsRt |   [  |   ]  |  *   |   1  |   2  |   3  | ____ | ____ |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |      |
+ * | ____ | XXXX | XXXX | XXXX |   {  |   }  |  /   |   0  | ____ | ____ | ____ | PgUp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * | ____ | ____ | ____ | ____ | ____ | ____ | ____ | Home | End  | ____ | ____ | PgDn |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] = LAYOUT_preonic_grid(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, KC_EQL,  KC_7,    KC_8,    KC_9,    KC_CIRC, _______,
+  _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_LPRN, KC_RPRN, KC_MINS, KC_4,    KC_5,    KC_6,    KC_PERC, _______,
+  _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_LBRC, KC_RBRC, KC_ASTR, KC_1,    KC_2,    KC_3,    _______, _______,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_SLSH, KC_0,    _______, _______, _______, KC_PGUP,
+  _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______, _______, KC_PGDN
 ),
 
 /* Raise 4
