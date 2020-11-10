584754a04a71dc2c5f1c89191432ef8637faae86
domnantas <domantas@zyro.com>
2020-11-10 03:31:06 +0200

    Initialize domnantas layout

---
+++ keyboards/lily58/keymaps/domnantas/rules.mk
@@ -0,0 +1,13 @@
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+EXTRAKEY_ENABLE = yes
+WPM_ENABLE = yes
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        # ./lib/logo_reader.c \
+        # ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
