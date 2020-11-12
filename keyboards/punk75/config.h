7b6d07bd56f01c688c1dd66e85600881f98f9feb
dsanchezseco <d.seco@protonmail.com>
2020-11-12 11:25:42 +0100

    Added VIA keymap to punk75 keyboard
    Added/modded files to make punk75 VIA compatible

--- keyboards/punk75/config.h
+++ keyboards/punk75/config.h
@@ -24,7 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0001
 #define DEVICE_VER 0x0001
 #define MANUFACTURER dsanchezseco
-#define PRODUCT punk75
+#define PRODUCT dsanchezseco punk75
+#define DESCRIPTION dsanchezseco punk75
 
 /* key matrix size */
 #define MATRIX_ROWS 5
