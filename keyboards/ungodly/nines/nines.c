27b59c85c974f426944831b091e8bde9ec83e97d
U-LUNA\Luis Godinez <beantek@gmail.com>
2020-11-12 14:44:34 -0800

    Initial commit
    Functional firmware with keymapping and rotary encoder. ToDo(goluis): clean up unused code and comments.

---
+++ keyboards/ungodly/nines/nines.c
@@ -0,0 +1,32 @@
+/* Copyright 2020 Ungodly Design
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ #include "nines.h"
+
+void encoder_update_kb(uint8_t index, bool clockwise) {
+    if (index == 0) { /* Left encoder */
+        if (clockwise) {
+            tap_code(KC_AUDIO_VOL_UP);
+        } else {
+            tap_code(KC_AUDIO_VOL_DOWN);
+        }
+    } else if (index == 1) { /* Right encoder */
+        if (clockwise) {
+            tap_code(KC_AUDIO_VOL_UP);
+        } else {
+            tap_code(KC_AUDIO_VOL_DOWN);
+        }
+    }
+}
