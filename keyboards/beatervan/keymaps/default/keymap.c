949097eeebd24a4f7589197c55aadb9c9c342ab8
ojthetiny <65928618+ojthetiny@users.noreply.github.com>
2020-11-13 23:43:59 +0200

    Update keyboards/beatervan/keymaps/default/keymap.c
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/beatervan/keymaps/default/keymap.c
+++ keyboards/beatervan/keymaps/default/keymap.c
@@ -1,11 +1,13 @@
 #include QMK_KEYBOARD_H
 
-#define _QW 0
-#define _DV 1
-#define _CM 2
-#define _L1 3
-#define _L2 4
-#define _L3 5
+enum layers {
+  _QW,
+  _DV,
+  _CM,
+  _L1,
+  _L2,
+  _L3,
+};
 
 enum custom_keycodes {
   DVORAK = SAFE_RANGE,
