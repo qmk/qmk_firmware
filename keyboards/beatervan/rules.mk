1ab860cef4151f2d76e69219ca72a82c6aa158f4
ojthetiny <65928618+ojthetiny@users.noreply.github.com>
2020-11-13 23:44:48 +0200

    Update keyboards/beatervan/rules.mk
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/beatervan/rules.mk
+++ keyboards/beatervan/rules.mk
@@ -2,13 +2,6 @@
 MCU = atmega32u4
 
 # Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = caterina
 
 # Build Options
@@ -28,4 +21,3 @@ MIDI_ENABLE = no            # MIDI controls
 UNICODE_ENABLE = yes         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-
