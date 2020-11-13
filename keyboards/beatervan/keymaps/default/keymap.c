fd711074cbf8601a27147722a09358dff093d7fb
ojthetiny <65928618+ojthetiny@users.noreply.github.com>
2020-11-13 23:40:16 +0200

    Update keyboards/beatervan/keymaps/default/keymap.c
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/beatervan/keymaps/default/keymap.c
+++ keyboards/beatervan/keymaps/default/keymap.c
@@ -1,11 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
 #define _QW 0
 #define _DV 1
 #define _CM 2
