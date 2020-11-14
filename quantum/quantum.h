b16caee3c7ee1e14f3352107e813b1f3c60def38
mtei <2170248+mtei@users.noreply.github.com>
2020-11-14 20:01:50 +0900

    waitOutputPinValid() rename to waitInputPinDelay() in quantum/quantum.h.

--- quantum/quantum.h
+++ quantum/quantum.h
@@ -208,7 +208,11 @@ typedef uint8_t pin_t;
 
 #    define togglePin(pin) (PORTx_ADDRESS(pin) ^= _BV((pin)&0xF))
 
-#    define waitOutputPinValid() wait_cpuclock(1)
+/*   The AVR series GPIOs have a one clock read delay for changes in the digital input signal. */
+#    if !defined(GPIO_INPUT_PIN_DELAY)
+#        define GPIO_INPUT_PIN_DELAY 1
+#    endif
+#    define waitInputPinDelay() wait_cpuclock(GPIO_INPUT_PIN_DELAY)
 
 #elif defined(PROTOCOL_CHIBIOS)
 typedef ioline_t pin_t;
@@ -229,12 +233,24 @@ typedef ioline_t pin_t;
 #endif
 
 #if defined(__ARMEL__) || defined(__ARMEB__)
-#    if  defined(STM32_SYSCLK)
-#        define WAITOUTPUTPINVALID_DELAY (STM32_SYSCLK/1000000L / 4)
-#    elif defined(KINETIS_SYSCLK_FREQUENCY)
-#        define WAITOUTPUTPINVALID_DELAY (KINETIS_SYSCLK_FREQUENCY/1000000L / 4)
+/* For GPIOs on ARM-based MCUs, the input pins are sampled by the clock of the bus
+ * to which the GPIO is connected.
+ * The connected buses differ depending on the various series of MCUs.
+ * And since the instruction execution clock of the CPU and the bus clock of GPIO are different,
+ * there is a delay of several clocks to read the change of the input signal.
+ *
+ * Define this delay with the GPIO_INPUT_PIN_DELAY macro.
+ * If the GPIO_INPUT_PIN_DELAY macro is not defined, the following default values will be used.
+ * (A fairly large value of 0.25 microseconds is set.)
+ */
+#    if !defined(GPIO_INPUT_PIN_DELAY)
+#        if defined(STM32_SYSCLK)
+#            define GPIO_INPUT_PIN_DELAY (STM32_SYSCLK/1000000L / 4)
+#        elif defined(KINETIS_SYSCLK_FREQUENCY)
+#            define GPIO_INPUT_PIN_DELAY (KINETIS_SYSCLK_FREQUENCY/1000000L / 4)
+#        endif
 #    endif
-#    define waitOutputPinValid() wait_cpuclock(WAITOUTPUTPINVALID_DELAY)
+#    define waitInputPinDelay() wait_cpuclock(GPIO_INPUT_PIN_DELAY)
 #endif
 
 // Atomic macro to help make GPIO and other controls atomic.
