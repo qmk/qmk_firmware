be20c9c5807f65f22c7e9bb6f25dded66b8f4705
lfgberg <lfgaberg@gmail.com>
2020-11-14 22:47:09 -0500

    added keeb
    consists of 2 keymaps and one layout. one default and one which is via compatible

---
+++ keyboards/lfgberg/cumpad/keymaps/via/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /*
+   * ,-------------------------------------------------,
+   * | c  | c  |    | u  |    | u  |    | m  |    | m  |
+   * |----|----|----|----|----|----|----|----|----|----|
+   * | c  |    |    | u  |    | u  |    | m  | m  | m  |
+   * |----|----|----|----|----|----|----|----|----|----|
+   * | c  | c  |    | u  | u  | u  |    | m  |    | m  |
+   * ,-------------------------------------------------,
+   */
+
+   [0] = LAYOUT(
+     KC_C, KC_C,  KC_U,       KC_U,  KC_M,       KC_M,
+     KC_C,        KC_U,       KC_U,  KC_M, KC_M, KC_M,
+     KC_C, KC_C,  KC_U, KC_U, KC_U,  KC_M,       KC_M
+   ),
+   [1] = LAYOUT(
+     KC_TRNS, KC_TRNS,  KC_TRNS,           KC_TRNS,  KC_TRNS,           KC_TRNS,
+     KC_TRNS,           KC_TRNS,           KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,
+     KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,           KC_TRNS
+   ),
+   [2] = LAYOUT(
+     KC_TRNS, KC_TRNS,   KC_TRNS,           KC_TRNS,  KC_TRNS,           KC_TRNS,
+     KC_TRNS,            KC_TRNS,           KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,
+     KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,           KC_TRNS
+   ),
+   [3] = LAYOUT(
+     KC_TRNS, KC_TRNS,   KC_TRNS,           KC_TRNS,  KC_TRNS,           KC_TRNS,
+     KC_TRNS,            KC_TRNS,           KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,
+     KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,           KC_TRNS
+   ),
+};
