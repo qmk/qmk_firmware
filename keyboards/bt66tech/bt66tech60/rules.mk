15b86deb1a72a543f42f190b60d84beaa6b51c7d
bt66tech <39931446+bt66@users.noreply.github.com>
2020-11-16 10:28:15 +0700

    Update rules.mk
    apply suggestion

--- keyboards/bt66tech/bt66tech60/rules.mk
+++ keyboards/bt66tech/bt66tech60/rules.mk
@@ -1,31 +1,35 @@
 # MCU name
 MCU = STM32F103
 
-# GENERIC STM32F103C8T6 board - stm32duino bootloader
-OPT_DEFS = -DCORTEX_VTOR_INIT=0x2000
-MCU_LDSCRIPT = STM32F103x8_stm32duino_bootloader
-BOARD = STM32_F103_STM32DUINO
+# Bootloader selection
+BOOTLOADER = stm32duino
 
-DFU_ARGS = -d 1eaf:0003 -a2 -R
-DFU_SUFFIX_ARGS = -v 1eaf -p 0003
 
-# project specific files
-#VPATH += keyboards/handwired/bt66tech/bt66tech60/bluepill
-#SRC =	bluepill.c
 
-#BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
-SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	      # USB Nkey Rollover
-BACKLIGHT_ENABLE = yes
-RGBLIGHT_ENABLE = yes
+
+
+
+
+
+
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 WS2812_DRIVER = spi
-
-# LAYOUTS = 60_ansi
-
 DEFAULT_FOLDER = handwired/bt66tech/bt66tech60
 
 
