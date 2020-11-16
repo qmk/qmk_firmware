971425e766d8b5111df28d5349b6d18319c6c663
Jay Greco <jayv.greco@gmail.com>
2020-11-16 00:24:21 -0800

    Add SCRAMBLE

---
+++ keyboards/nullbitsco/scramble/readme.md
@@ -0,0 +1,17 @@
+# SCRAMBLE
+
+![SCRAMBLE](https://nullbits.co/static/img/scramble1.jpg)
+
+A 6-switch key tester macropad built by nullbits. [More info at nullbits.co](https://nullbits.co/scramble/)
+
+* Keyboard Maintainer: [Jay Greco](https://github.com/jaygreco)
+* Hardware Supported: SCRAMBLE Rev1
+* Hardware Availability: [nullbits.co](https://nullbits.co/)
+
+Note: If you are seeing issues with MacOS and keyboard hangs after sleep, make sure `NO_USB_STARTUP_CHECK = yes` is set in your rules.mk.
+
+Make example for this keyboard (after setting up your build environment):
+
+    qmk compile -kb nullbitsco/scramble -km default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
---
+++ keyboards/nullbitsco/scramble/rules.mk
@@ -0,0 +1,29 @@
+# MCU name
+MCU = atmega328p
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = USBasp
+
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+ENCODER_ENABLE = yes        # Use rotary encoder
+LTO_ENABLE = yes            # Link-time optimization
+CUSTOM_MATRIX = no          # Custom matrix
