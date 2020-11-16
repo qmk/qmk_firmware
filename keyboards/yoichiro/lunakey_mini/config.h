619abb170e9436087318d9c36aabb17aaf82d8b5
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-11-16 10:08:39 +0900

    Modify the VENDOR and PRODUCT IDs.

--- keyboards/yoichiro/lunakey_mini/config.h
+++ keyboards/yoichiro/lunakey_mini/config.h
@@ -20,11 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x3816
+#define VENDOR_ID    0x5954  // "YT" - Yoichiro Tanaka
+#define PRODUCT_ID   0x0001
 #define DEVICE_VER   0x0001
 #define MANUFACTURER yoichiro
-#define PRODUCT      lunakey_mini
+#define PRODUCT      yoichiro Lunakey Mini
 
 /* key matrix size */
 #define MATRIX_ROWS 8
