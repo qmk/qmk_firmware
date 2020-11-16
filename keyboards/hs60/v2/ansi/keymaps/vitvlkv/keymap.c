a636d65df229dd5edea70d3a99d6ef9fae8af80e
vitvlkv <vitvlkv@yandex-team.ru>
2020-11-16 14:47:25 +0300

    Right Shift reverted back...

--- keyboards/hs60/v2/ansi/keymaps/vitvlkv/keymap.c
+++ keyboards/hs60/v2/ansi/keymaps/vitvlkv/keymap.c
@@ -39,21 +39,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,\
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,\
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,\
-    KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_GRV,\
+    KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,\
     MO(2),    KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    LT(3, KC_APP), KC_RCTL),
 
 [1] = LAYOUT_60_ansi( /* Fn */
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,\
     KC_CAPS,  KC_PGUP,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,\
     _______,  KC_PGDN,  _______,  KC_HOME,  KC_END,   KC_DEL,   KC_BSPC,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_INS,   _______,            _______,\
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_PGDN,  _______,  _______,            KC_RSFT,\
+    _______,            _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_PGDN,  _______,  _______,            _______,\
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
 [2] = LAYOUT_60_ansi( /* Mouse and volume */
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,\
     _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_U,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,\
     _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,  _______,  _______,  KC_ACL0,  KC_ACL1,  KC_ACL2,  _______,  _______,            _______,\
-    _______,            _______,  KC_WH_L,  KC_WH_R,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_RSFT,\
+    _______,            _______,  KC_WH_L,  KC_WH_R,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,\
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 [3] = LAYOUT_60_ansi( /* RGB */
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,\
