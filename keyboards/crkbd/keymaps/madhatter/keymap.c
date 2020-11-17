17b7ed28ff37c5d71e3f724859a8f06f8e3ca91c
Khader Syed <khader.syed@aicure.com>
2020-11-16 22:36:33 -0500

    stick to enum

--- keyboards/crkbd/keymaps/madhatter/keymap.c
+++ keyboards/crkbd/keymaps/madhatter/keymap.c
@@ -26,10 +26,13 @@ extern uint8_t is_master;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+
+enum my_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+}
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
