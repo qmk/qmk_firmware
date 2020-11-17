e38b26d8927a47b6e6b13caaabed2e9f040a7c41
Ben <BenRoe@users.noreply.github.com>
2020-11-17 21:03:13 +0100

    Update keyboards/lily58/keymaps/lily58l/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lily58/keymaps/lily58l/config.h
+++ keyboards/lily58/keymaps/lily58l/config.h
@@ -27,11 +27,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_ANIMATIONS
-  #define RGBLIGHT_HUE_STEP 6 // number of steps to cycle through the hue by
-  #define RGBLIGHT_SAT_STEP 6 // number of steps to increment the saturation by
-  #define RGBLIGHT_VAL_STEP 6 // number of steps to increment the brightness by
-  #define RGBLIGHT_SLEEP //  the RGB lighting will be switched off when the host goes to sleep
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_HUE_STEP 6 // number of steps to cycle through the hue by
+#    define RGBLIGHT_SAT_STEP 6 // number of steps to increment the saturation by
+#    define RGBLIGHT_VAL_STEP 6 // number of steps to increment the brightness by
+#    define RGBLIGHT_SLEEP //  the RGB lighting will be switched off when the host goes to sleep
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
