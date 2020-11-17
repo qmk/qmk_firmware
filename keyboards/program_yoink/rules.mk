947de14258704757012213aade0d65fe429acbac
melonbred <61572569+melonbred@users.noreply.github.com>
2020-11-16 21:06:43 -0800

    Update keyboards/program_yoink/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/program_yoink/rules.mk
+++ keyboards/program_yoink/rules.mk
@@ -2,13 +2,6 @@
 MCU = atmega32u2
 
 # Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = atmel-dfu
 
 # Build Options
@@ -26,10 +19,8 @@ NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 ENCODER_ENABLE = yes		# Enable rotary encoder
 COMBO_ENABLE = yes 
 TAP_DANCE_ENABLE = no
