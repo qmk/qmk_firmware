72be077136a52e9d5e22b2a9cf975f9d1de78489
nopunin10did <w.alex.ronke@gmail.com>
2020-11-18 15:46:32 -0500

    Update keyboards/boardrun/classic/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/boardrun/classic/keymaps/default/keymap.c
+++ keyboards/boardrun/classic/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *          | LGUI   | DEL    | ~`     | LALT   | SPACE  |        | FN     | SPACE  |        | LEFT   | DOWN   | UP     | RIGHT  |
      *          '--------------------------------------------------------------------------------------------------------------------'
      */
-    [_CLASSIC] = LAYOUT_CLASSIC(
+    [_CLASSIC] = LAYOUT_classic(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,           KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,           KC_RBRC, KC_Y,	   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,           KC_PGUP, KC_H,	   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
