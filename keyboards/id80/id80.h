10c7e1495145fb605a37435c714f67b8d00b580f
Pascal Himmelberger <57861914+phi-0@users.noreply.github.com>
2020-11-18 17:34:28 +0100

    Update keyboards/id80/id80.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/id80/id80.h
+++ keyboards/id80/id80.h
@@ -38,7 +38,7 @@
 }
 
 
-#define LAYOUT_ISO( \
+#define LAYOUT_iso( \
     K50,  K51, K52, K53, K54,  K55, K56, K57, K58,  KA7, KA5, KA4, KA3,   KA6,  KA2,  \
     K40, K41, K42, K43, K44, K45, K46, K47, K48, K98, K97, K95, K94, K96,       K92,  \
     K30,   K31, K32, K33, K34, K35, K36, K37, K38, K88, K87, K85, K84, K83,     K82,  \
