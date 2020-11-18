b0f63b533247ba92370e171058adaf4fd8801695
Ben <BenRoe@users.noreply.github.com>
2020-11-18 21:19:31 +0100

    Update keyboards/lily58/keymaps/lily58l/rules.mk
    Co-authored-by: Erovia <Erovia@users.noreply.github.com>

--- keyboards/lily58/keymaps/lily58l/rules.mk
+++ keyboards/lily58/keymaps/lily58l/rules.mk
@@ -6,7 +6,6 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
         ./lib/keylogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
