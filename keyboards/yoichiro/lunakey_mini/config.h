08df382cb20b3c13a64b52adc6412c26c5d4dca4
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-11-19 08:50:00 +0900

    Change the product name.

--- keyboards/yoichiro/lunakey_mini/config.h
+++ keyboards/yoichiro/lunakey_mini/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0001
 #define DEVICE_VER   0x0001
 #define MANUFACTURER yoichiro
-#define PRODUCT      yoichiro Lunakey Mini
+#define PRODUCT      Lunakey Mini
 
 /* key matrix size */
 #define MATRIX_ROWS 8
