73783d01783d59ccdc0464bf3b865cd51cc33b0b
X-Bows Tech <41098278+XBowsTech@users.noreply.github.com>
2020-11-18 15:55:06 +0800

    Update keyboards/xbows/nature/keymaps/default/keymap.c
    Co-authored-by: Erovia <Erovia@users.noreply.github.com>

--- keyboards/xbows/nature/keymaps/default/keymap.c
+++ keyboards/xbows/nature/keymaps/default/keymap.c
@@ -31,7 +31,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl | GUI |     Alter   |     Space   |   Ctrl   |   Shift  |     Space     |     Alter    |  FN  | Ctrl | Lft  |  Dn  |  Rig |
    * |---------------------------------------------------------------------------------------------------------------------------------|
    */
-  // Please note:The actual physical location of the PGUP key is at the end of the third line of the keyboard.(Just like the comment above)
   [0] = LAYOUT(
 		KC_ESC,  KC_F1,   KC_F2, KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_DEL,  KC_PSCR,\
 		KC_GRV,  KC_1,    KC_2,  KC_3,   KC_4,   KC_5,             KC_6,   KC_7,  KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,\
