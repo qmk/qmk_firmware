bcdc99c7776aef9bc75e5e74144a7028c0203c52
M4cs <mabridgland@protonmail.com>
2020-11-18 11:43:13 -0500

    Add comments, fix readmes, clean up

--- keyboards/dekunukem/duckypad/rules.mk
+++ keyboards/dekunukem/duckypad/rules.mk
@@ -1,16 +1,23 @@
+# MCU Name
 MCU = STM32F072
+
 BOARD = ST_STM32F072B_DISCOVERY
 
-BOOTMAGIC_ENABLE = no
-MOUSEKEY_ENABLE = no
-EXTRAKEY_ENABLE = yes
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-SLEEP_LED_ENABLE = no
-NKRO_ENABLE = no
-RGBLIGHT_ENABLE = no
+# Build Options
+# 	change yes to no to disable
+#	change no to yes to enable
+#
+BOOTMAGIC_ENABLE = no		# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no		# Mouse Functionality
+EXTRAKEY_ENABLE = yes		# Audio Control and System Controls
+CONSOLE_ENABLE = no			# Console for Debugging
+COMMAND_ENABLE = no			# Commands for Debugging and Configuration
+SLEEP_LED_ENABLE = no		# Breating Sleep LED during USB Suspend
+NKRO_ENABLE = no 			# USB NKey Rollover | If this doesn't work, see here:  https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+RGBLIGHT_ENABLE = no		# Enable keyboard backlight functionality
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 
-# TODO: Can we get this working with SPI?
 WS2812_DRIVER = bitbang
 RGB_MATRIX_ENABLE = WS2812
 
