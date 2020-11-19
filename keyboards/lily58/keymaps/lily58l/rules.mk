40ba52311c8dca4f267cdf29d39ff5eed2989a4d
BenRoe <mail@benjaminroesner.com>
2020-11-19 21:16:30 +0100

    Not include standard lily58 lib file

--- keyboards/lily58/keymaps/lily58l/rules.mk
+++ keyboards/lily58/keymaps/lily58l/rules.mk
@@ -4,10 +4,3 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
