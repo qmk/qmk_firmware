e8f9895a4482434cf30ca726a8f4079aaaf9a670
ridingqwerty <george.g.koenig@gmail.com>
2020-11-18 21:40:17 -0500

    Update keyboards/wsk/alpha9/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/wsk/alpha9/config.h
+++ keyboards/wsk/alpha9/config.h
@@ -48,9 +48,9 @@
 
 #define RGB_DI_PIN F4
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 5
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 5
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
 #endif
