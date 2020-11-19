339d87e856b2ebe9cc6b7c59f3eaf926dfb3d4d6
vitvlkv <vitvlkv@yandex-team.ru>
2020-11-19 10:05:00 +0300

    - Right Ctrl -> ` - Right Alt -> Fn modifier for "numpad" on "yuihjknm, " keys (as "1234567890") Plus "g" - is a backspace in numpad mode - Removed mouse keys - Volume at "APP" + ",./" (as vol_up, vol_down, vol_toggle)

--- keyboards/hs60/v2/ansi/keymaps/vitvlkv/keymap.c
+++ keyboards/hs60/v2/ansi/keymaps/vitvlkv/keymap.c
@@ -14,22 +14,24 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// To compile firmware:
-// `qmk compile -kb hs60/v2/ansi -km vitvlkv`
+/* To compile firmware:
+qmk compile -kb hs60/v2/ansi -km vitvlkv
 
-// To flash firmware:
-// plug in keyboard
-// press hardware 'reset' button (on the PCB)
-// execute command: `qmk flash -kb hs60/v2/ansi -km vitvlkv`
+To flash firmware:
+- plug in keyboard
+- press hardware 'reset' button (on the PCB)
+- execute command:
+qmk flash -kb hs60/v2/ansi -km vitvlkv
 
-// Backlight specific keys:
-//  EF_INC, EF_DEC,   // next/previous backlight effect
-//  H1_INC, H1_DEC,   // Color 1 hue increase/decrease
-//  S1_INC, S1_DEC,   // Color 1 saturation increase/decrease
-//  H2_INC, H2_DEC,   // Color 2 hue increase/decrease
-//  S2_INC, S2_DEC,   // Color 2 saturation increase/decrease
-//  BR_INC, BR_DEC,   // backlight brightness increase/decrease
-//  ES_INC, ES_DEC,   // ?..
+Backlight specific keys:
+EF_INC, EF_DEC,   // next/previous backlight effect
+H1_INC, H1_DEC,   // Color 1 hue increase/decrease
+S1_INC, S1_DEC,   // Color 1 saturation increase/decrease
+H2_INC, H2_DEC,   // Color 2 hue increase/decrease
+S2_INC, S2_DEC,   // Color 2 saturation increase/decrease
+BR_INC, BR_DEC,   // backlight brightness increase/decrease
+ES_INC, ES_DEC,   // ?..
+*/
 #include QMK_KEYBOARD_H
 
 //This is the ANSI version of the PCB
@@ -40,26 +42,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,\
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,\
     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,\
-    MO(2),    KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    LT(3, KC_APP), KC_RCTL),
+    MO(1),    KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(2),    MO(1),    LT(3, KC_APP), KC_GRV),
 
 [1] = LAYOUT_60_ansi( /* Fn */
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,\
     KC_CAPS,  KC_PGUP,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  _______,\
     _______,  KC_PGDN,  _______,  KC_HOME,  KC_END,   KC_DEL,   KC_BSPC,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_INS,   _______,            _______,\
     _______,            _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_PGDN,  _______,  _______,            _______,\
-    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  KC_RCTL),
 
-[2] = LAYOUT_60_ansi( /* Mouse and volume */
+[2] = LAYOUT_60_ansi( /* Numpad */
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,\
-    _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_U,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,\
-    _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,  _______,  _______,  KC_ACL0,  KC_ACL1,  KC_ACL2,  _______,  _______,            _______,\
-    _______,            _______,  KC_WH_L,  KC_WH_R,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,\
-    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
+    _______,  _______,  _______,  _______,  _______,  _______,  KC_1,     KC_2,     KC_3,     _______,  _______,  _______,  _______,  _______,\
+    _______,  _______,  _______,  _______,  _______,  KC_BSPC,  KC_4,     KC_5,     KC_6,     _______,  _______,  _______,            _______,\
+    _______,            _______,  _______,  _______,  _______,  _______,  KC_7,     KC_8,     KC_9,     _______,  _______,            _______,\
+    _______,  _______,  _______,                                KC_0,                                   _______,  _______,  _______,  _______),
 [3] = LAYOUT_60_ansi( /* RGB */
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,\
-    _______,  EF_INC,   BR_INC,   ES_INC,   H1_INC,   S1_INC,   H2_INC,   S2_INC,   _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,\
+    _______,  EF_INC,   BR_INC,   ES_INC,   H1_INC,   S1_INC,   H2_INC,   S2_INC,   _______,  _______,  _______,  _______,  _______,  _______,\
     _______,  EF_DEC,   BR_DEC,   ES_DEC,   H1_DEC,   S1_DEC,   H2_DEC,   S2_DEC,   _______,  _______,  _______,  _______,            _______,\
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,\
+    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,            _______,\
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
 };
