973b9b84228f28f42401d3dea6bb9279c2cb4414
uuupah <uuupah@gmail.com>
2020-11-19 11:32:17 +1030

    remove unnecessary equals macro

--- keyboards/xd96/keymaps/uuupah/keymap.c
+++ keyboards/xd96/keymaps/uuupah/keymap.c
@@ -26,7 +26,6 @@ void matrix_init_user() {
 
 enum custom_keycodes {
   ZEROZER,
-  EQUALS,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -36,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,  KC_NLCK, _______, KC_PMNS, KC_PSLS,
 	  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_P7,   KC_P8,   KC_P9,   KC_PAST,
 	  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, LT_ENT2,                   KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-	  KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),            KC_P1,   KC_P2,   KC_P3,   EQUALS,
+	  KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),            KC_P1,   KC_P2,   KC_P3,   KC_EQL,
 	  KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,          KC_P0,   ZEROZER, KC_PDOT, KC_PENT
   ),
 
@@ -64,12 +63,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case ZEROZER:
       if (record->event.pressed) {
         SEND_STRING("00");
-      } else {
-      }
-      break;
-    case EQUALS:
-      if (record->event.pressed) {
-        SEND_STRING("=");
       } else {
       }
       break;
