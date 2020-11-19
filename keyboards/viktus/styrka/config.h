b586124bd2d2f2580157f8cc3723b14cc1a7f978
J.Flanagan <jrfhoutx@comcast.net>
2020-11-19 09:55:54 -0600

    Viktus Styrka Update 1
    First edit update for Viktus Styrka
    
    - Corrected LAYOUT macros
    - Updated keymaps
    - Updated info.json to match LAYOUT macros
    - Added and updated VIA keymap
    - Added and updated ALL keymap
    - Removed unused features from config
    - Added more detailed board info to main readme

--- keyboards/viktus/styrka/config.h
+++ keyboards/viktus/styrka/config.h
@@ -54,47 +54,6 @@ B0, which is unconnected on the PCB
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-// #define BACKLIGHT_PIN B0
-// #define BACKLIGHT_PWM_DRIVER PWMD3
-// #define BACKLIGHT_PWM_CHANNEL 3
-
-/*
- * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
- */
-//#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
-
-// #define BACKLIGHT_PIN B7
-// #define BACKLIGHT_BREATHING
-// #define BACKLIGHT_LEVELS 3
-
-// #define RGB_DI_PIN E2
-// #ifdef RGB_DI_PIN
-//   #define RGBLED_NUM 16
-//   #define RGBLIGHT_HUE_STEP 8
-//   #define RGBLIGHT_SAT_STEP 8
-//   #define RGBLIGHT_VAL_STEP 8
-//   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-// /*== all animations enable ==*/
-//   #define RGBLIGHT_ANIMATIONS
-// /*== or choose animations ==*/
-//   #define RGBLIGHT_EFFECT_BREATHING
-//   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//   #define RGBLIGHT_EFFECT_SNAKE
-//   #define RGBLIGHT_EFFECT_KNIGHT
-//   #define RGBLIGHT_EFFECT_CHRISTMAS
-//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//   #define RGBLIGHT_EFFECT_RGB_TEST
-//   #define RGBLIGHT_EFFECT_ALTERNATING
-// /*== customize breathing effect ==*/
-//   /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
-//   #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
-//   /*==== use exp() and sin() ====*/
-//   #define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-//   #define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-// #endif
-
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
@@ -204,30 +163,3 @@ B0, which is unconnected on the PCB
 //#define NO_ACTION_ONESHOT
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
-
-/*
- * MIDI options
- */
-
-/* Prevent use of disabled MIDI features in the keymap */
-//#define MIDI_ENABLE_STRICT 1
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-//#define MIDI_BASIC
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 1
-
-/* Bootmagic Lite key configuration */
-// #define BOOTMAGIC_LITE_ROW 0
-// #define BOOTMAGIC_LITE_COLUMN 0
--- keyboards/viktus/styrka/styrka.h
+++ keyboards/viktus/styrka/styrka.h
@@ -14,7 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #pragma once
+
 #include "quantum.h"
+#define ___ KC_NO
 
 /* This a shortcut to help you visually see your layout.
  *
@@ -23,6 +25,10 @@
  *
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
+ *
+ *
+ * Use LAYOUT_all for ISO and Tsangan builds
+ *
  */
 
 #define LAYOUT_default( \
@@ -33,16 +39,16 @@
     K80, K90, K81, K91, K82,                K84,                K86, K96, K87,      K88, K98  \
 ) \
 { \
-    { K00,    K01,    K02,    K03,    K04,    K05,    K06,  K07,    K08 }, \
-    { K10,    K11,    K12,    K13,    K14,    K15,    K16,  KC_NO,  K18 }, \
-    { K20,    K21,    K22,    K23,    K24,    K25,    K26,  K27,    K28 }, \
-    { K30,    K31,    K32,    K33,    K34,    K35,    K36,  KC_NO,  K38 }, \
-    { K40,    K41,    K42,    K43,    K44,    K45,    K46,  KC_NO,  K48 }, \
-    { K50,    K51,    K52,    K53,    K54,    K55,    K56,  KC_NO,  K58 }, \
-    { K60,    K61,    K62,    K63,    K64,    K65,    K66,  K67,    K68 }, \
-    { K70,    KC_NO,  K72,    K73,    K74,    K75,    K76,  KC_NO,  K78 }, \
-    { K80,    K81,    K82,    KC_NO,  K84,    KC_NO,  K86,  K87,    K88 }, \
-    { K90,    K91,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  K96,  KC_NO,  K98 }  \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08 }, \
+    { K10, K11, K12, K13, K14, K15, K16, ___, K18 }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28 }, \
+    { K30, K31, K32, K33, K34, K35, K36, ___, K38 }, \
+    { K40, K41, K42, K43, K44, K45, K46, ___, K48 }, \
+    { K50, K51, K52, K53, K54, K55, K56, ___, K58 }, \
+    { K60, K61, K62, K63, K64, K65, K66, K67, K68 }, \
+    { K70, ___, K72, K73, K74, K75, K76, ___, K78 }, \
+    { K80, K81, K82, ___, K84, ___, K86, K87, K88 }, \
+    { K90, K91, ___, ___, ___, ___, K96, ___, K98 }  \
 }
 #define LAYOUT_split_bs( \
     K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, K07, K17, K08, K18, \
@@ -52,14 +58,33 @@
     K80, K90, K81, K91, K82,                K84,                K86, K96,      K87, K88, K98  \
 ) \
 { \
-    { K00,    K01,    K02,    K03,    K04,    K05,    K06,  K07,    K08 }, \
-    { K10,    K11,    K12,    K13,    K14,    K15,    K16,  K17,    K18 }, \
-    { K20,    K21,    K22,    K23,    K24,    K25,    K26,  K27,    K28 }, \
-    { K30,    K31,    K32,    K33,    K34,    K35,    K36,  KC_NO,  K38 }, \
-    { K40,    K41,    K42,    K43,    K44,    K45,    K46,  KC_NO,  K48 }, \
-    { K50,    K51,    K52,    K53,    K54,    K55,    K56,  KC_NO,  K58 }, \
-    { K60,    K61,    K62,    K63,    K64,    K65,    K66,  K67,    K68 }, \
-    { K70,    KC_NO,  K72,    K73,    K74,    K75,    K76,  KC_NO,  K78 }, \
-    { K80,    K81,    K82,    KC_NO,  K84,    KC_NO,  K86,  K87,    K88 }, \
-    { K90,    K91,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  K96,  KC_NO,  K98 }  \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08 }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18 }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28 }, \
+    { K30, K31, K32, K33, K34, K35, K36, ___, K38 }, \
+    { K40, K41, K42, K43, K44, K45, K46, ___, K48 }, \
+    { K50, K51, K52, K53, K54, K55, K56, ___, K58 }, \
+    { K60, K61, K62, K63, K64, K65, K66, K67, K68 }, \
+    { K70, ___, K72, K73, K74, K75, K76, ___, K78 }, \
+    { K80, K81, K82, ___, K84, ___, K86, K87, K88 }, \
+    { K90, K91, ___, ___, ___, ___, K96, ___, K98 }  \
+}
+#define LAYOUT_all( \
+    K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, K07, K17, K08, K18, \
+    K20, K30, K21, K31, K22, K32, K23, K33, K24, K34, K25, K35, K26, K36, K27,      K28, K38, \
+    K40, K50, K41, K51, K42, K52, K43, K53, K44, K54, K45, K55, K46, K56, K47,      K48, K58, \
+    K60, K70, K61, K71, K62, K72, K63, K73, K64, K74, K65, K75, K66, K76, K67,      K68, K78, \
+    K80, K90, K81, K91, K82,                K84,                K86, K96,      K87, K88, K98  \
+) \
+{ \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08 }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18 }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28 }, \
+    { K30, K31, K32, K33, K34, K35, K36, ___, K38 }, \
+    { K40, K41, K42, K43, K44, K45, K46, K47, K48 }, \
+    { K50, K51, K52, K53, K54, K55, K56, ___, K58 }, \
+    { K60, K61, K62, K63, K64, K65, K66, K67, K68 }, \
+    { K70, K71, K72, K73, K74, K75, K76, ___, K78 }, \
+    { K80, K81, K82, ___, K84, ___, K86, K87, K88 }, \
+    { K90, K91, ___, ___, ___, ___, K96, ___, K98 }  \
 }
