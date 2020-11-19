fa95446820945cd2e1ffbbefa719f99af9982f29
ridingqwerty <george.g.koenig@gmail.com>
2020-11-18 21:39:52 -0500

    Update keyboards/wsk/alpha9/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/wsk/alpha9/config.h
+++ keyboards/wsk/alpha9/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x5753  // "WS" - Worldspawn00
+#define PRODUCT_ID      0x692A
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Worldspawn00
 #define PRODUCT         Alpha9
