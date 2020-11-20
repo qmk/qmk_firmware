425b21fbca619c8da1bc9d0d3b9edfae52b528ad
Cole Smith <smithcole@protonmail.com>
2020-11-19 19:21:23 -0800

    made default ansi map

--- keyboards/boardsource/the_mark/config.h
+++ keyboards/boardsource/the_mark/config.h
@@ -20,11 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x4273 // "Bs" - Boardsource
+#define PRODUCT_ID   0x0001
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Boardsource
-#define PRODUCT      the_mark
+#define PRODUCT      Boardsource The Mark65
 
 /* key matrix size */
 #define MATRIX_ROWS 5
@@ -40,6 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
+
 #define MATRIX_ROW_PINS {B0, B1, B2, B3, B4,}
 #define MATRIX_COL_PINS { B5, B6, B7, F5, C7, D0, D1, D2, D3, D4, D5, D6, D7,F0, F1, F4}
 #define UNUSED_PINS
