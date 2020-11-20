57b23cdb6be19fb6c5ea0b34a2ebc907ffdf1cc5
Wilba <wilba@wilba.tech>
2020-11-20 13:12:41 +1100

    Update keyboards/wilba_tech/wt65_h1/keymaps/default/keymap.c
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/wilba_tech/wt65_h1/keymaps/default/keymap.c
+++ keyboards/wilba_tech/wt65_h1/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                                     KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
 
 	// Fn2 Layer
-	[2] = LAYOUT_all(
+	[2] = LAYOUT_65_ansi_blocker_tsangan(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,
