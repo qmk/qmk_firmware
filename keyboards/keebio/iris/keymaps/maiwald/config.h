69df0ad7813497696238a10f4b730b70cbe4ee04
Luciano Maiwald <luciano.maiwald@gmail.com>
2020-11-20 13:17:07 +0100

    add permissive hold

--- keyboards/keebio/iris/keymaps/maiwald/config.h
+++ keyboards/keebio/iris/keymaps/maiwald/config.h
@@ -20,4 +20,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 #define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD_PER_KEY
