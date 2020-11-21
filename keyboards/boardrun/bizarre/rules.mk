ca484211fdb13093cc14d06fbdcd5c4530bce04c
nopunin10did <w.alex.ronke@gmail.com>
2020-11-21 16:04:12 -0500

    Update keyboards/boardrun/bizarre/rules.mk
    Co-authored-by: Erovia <Erovia@users.noreply.github.com>

--- keyboards/boardrun/bizarre/rules.mk
+++ keyboards/boardrun/bizarre/rules.mk
@@ -7,7 +7,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -20,4 +20,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-UNICODE_ENABLE = yes        # Unicode
+UNICODE_ENABLE = no         # Unicode
