165bbd07df186999b164d669a4c1fbb7e74bdcf8
Ryan Caltabiano <rcalt2vt@gmail.com>
2020-11-21 12:16:25 -0600

    Fix int wrapping for timer_expired macros and use MAX defines for consistency and clarity

--- tmk_core/common/timer.h
+++ tmk_core/common/timer.h
@@ -45,8 +45,8 @@ uint16_t timer_elapsed(uint16_t last);
 uint32_t timer_elapsed32(uint32_t last);
 
 // Utility functions to check if a future time has expired & autmatically handle time wrapping if checked / reset frequently (half of max value)
-#define timer_expired(current, future) (((uint16_t)current - (uint16_t)future) < 0x8000)
-#define timer_expired32(current, future) (((uint32_t)current - (uint32_t)future) < 0x80000000)
+#define timer_expired(current, future) ((uint16_t)(current - future) < UINT16_MAX / 2)
+#define timer_expired32(current, future) ((uint32_t)(current - future) < UINT32_MAX / 2)
 
 #ifdef __cplusplus
 }
