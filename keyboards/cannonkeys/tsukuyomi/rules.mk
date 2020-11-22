e033d8b06c801af1458f4f249ea7869ee00ff381
Andrew Kannan <andrew.kannan@gmail.com>
2020-11-21 23:37:21 -0500

    add license header and add dfu suffix args

--- keyboards/cannonkeys/tsukuyomi/rules.mk
+++ keyboards/cannonkeys/tsukuyomi/rules.mk
@@ -18,7 +18,7 @@ BACKLIGHT_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 WS2812_DRIVER = spi
 
-DFU_SUFFIX_ARGS =
+DFU_SUFFIX_ARGS = -p FFFF -v FFFF
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
