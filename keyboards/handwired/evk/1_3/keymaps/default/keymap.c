5d452dd7b114f8457237f757bc012d046d1a1f13
YangPiCui <yang.pi.cui@gmail.com>
2020-11-23 21:11:26 +0800

    h

--- keyboards/handwired/evk/1_3/keymaps/default/keymap.c
+++ keyboards/handwired/evk/1_3/keymaps/default/keymap.c
@@ -15,43 +15,14 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes { QMKBEST = SAFE_RANGE, QMKURL };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(/* Base */
-                 KC_SPC, KC_1, KC_2, KC_3, KC_4, KC_5, 
-                 KC_SPC, KC_Q, KC_W, KC_E, KC_R, KC_T, 
-                 KC_SPC, KC_A, KC_S, KC_D, KC_F, KC_G, 
-                 KC_SPC, KC_Z, KC_X, KC_C, KC_V, KC_B, 
-                 KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
-                 KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC),
+                 KC_1, KC_2, KC_3, KC_4, KC_5, KC_ESC,    KC_PSCR, KC_INS, KC_APP, KC_SPC,    KC_SPC, KC_6, KC_7, KC_8, KC_9, KC_0, 
+                 KC_Q, KC_H, KC_R, KC_F, KC_X, KC_CAPS,    KC_F1, KC_F2, KC_F3, KC_F4,    KC_DEL, KC_BSPC, KC_Y, KC_O, KC_U, KC_J, 
+                 KC_L, LC_S, KC_N, KC_T, KC_D, KC_LBRC,    KC_F5, KC_F6, KC_F7, KC_F8,    KC_RBRC, KC_P, KC_I, KC_A, KC_E, KC_K, 
+                 KC_Z, KC_M, KC_C, KC_V, KC_B,  KC_LCTL,   KC_F9, KC_F10, KC_F11, KC_F12,   KC_RCTL,  KC_W, KC_G, KC_COMM, KC_DOT, KC_ENT, 
+                 KC_LALT, KC_MINS, KC_EQL, KC_TAB,  KC_SPC, KC_LGUI,   KC_PGUP, KC_UP, KC_PGDN, KC_HOME,   KC_RGUI, KC_SPC,  KC_QUOT, KC_SCLN, KC_SLSH, KC_RALT, 
+	                  KC_LSFT, KC_GRV,                   KC_LEFT, KC_DOWN, KC_RGHT, KC_END,                       KC_BSLS, KC_RSHT,  
+				 ),
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
-    return true;
-}
-
-void matrix_init_user(void) {}
-
-void matrix_scan_user(void) {}
-
-void led_set_user(uint8_t usb_led) {}
