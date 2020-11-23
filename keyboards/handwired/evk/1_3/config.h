73476779e16ff242c317b83409b3e5731983e9c5
YangPiCui <yang.pi.cui@gmail.com>
2020-11-23 09:46:17 +0800

    Update config.h

--- keyboards/handwired/evk/1_3/config.h
+++ keyboards/handwired/evk/1_3/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 6
-#define MATRIX_COLS 6
+#define MATRIX_COLS 16
 
 /*
  * Keyboard Matrix Assignments
