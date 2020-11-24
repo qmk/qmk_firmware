31d7173f5788ec4c7ef916f01c212c7a020d9600
Rys Sommefeldt <rys@pixeltards.com>
2020-11-24 22:49:42 +0000

    Reviewed fixes for CU65
    Config, layout, copyright, build rules and documentation tweaks
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/capsunlocked/cu65/cu65.h
+++ keyboards/capsunlocked/cu65/cu65.h
@@ -1,3 +1,16 @@
+/*
+Copyright 2020 CapsUnlocked
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #pragma once
 
 #include "quantum.h"
@@ -11,7 +24,7 @@
  * represents the switch matrix.
  */
 
-#define LAYOUT( \
+#define LAYOUT_all( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, K014, K015, \
 	K100,       K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, K114, K115, \
 	K200,       K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213, K214, K215, \
