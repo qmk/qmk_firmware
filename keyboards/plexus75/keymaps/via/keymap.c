238fd49605250ae9cb5b5894e720382b41618d07
npspears <40127181+npspears@users.noreply.github.com>
2020-11-24 16:52:14 -0600

    Update keymap.c
    changed KC_CTRL to KC_LCTL

--- keyboards/plexus75/keymaps/via/keymap.c
+++ keyboards/plexus75/keymaps/via/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_2x2u(
       KC_ESC,        KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_LBRC,   KC_RBRC,   KC_GRV,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
       KC_TAB,        KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_P7,     KC_P8,     KC_P9,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_CTRL,       KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_P4,     KC_P5,     KC_P6,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LCTL,       KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_P4,     KC_P5,     KC_P6,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT,       KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_P1,     KC_P2,     KC_P3,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
       KC_PSCR,    KC_LGUI, KC_LALT, TT(_FN),   KC_SPC,   KC_P0,   KC_SPC,  TT(_FN1),   KC_RALT,  KC_RGUI, KC_CAPS
     ),
