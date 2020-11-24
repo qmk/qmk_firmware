48de68f3d88bbe72239e45dda16b8e976aaa0ad4
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-11-23 23:14:04 -0500

    create eek

---
+++ keyboards/eek/keymaps/default/config.h
@@ -0,0 +1,47 @@
+/* Copyright 2019 gtips
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+
+
+// #ifdef RGB_DI_PIN
+//   #define RGBLED_NUM 11
+//   #define RGBLIGHT_HUE_STEP 16
+//   #define RGBLIGHT_SAT_STEP 16
+//   #define RGBLIGHT_VAL_STEP 16
+//   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+// // /*== all animations enable ==*/
+//   #define RGBLIGHT_ANIMATIONS
+// /*== or choose animations ==*/
+//   #define RGBLIGHT_EFFECT_BREATHING
+//   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//   #define RGBLIGHT_EFFECT_SNAKE
+//   #define RGBLIGHT_EFFECT_KNIGHT
+//   #define RGBLIGHT_EFFECT_CHRISTMAS
+//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//   #define RGBLIGHT_EFFECT_RGB_TEST
+//   #define RGBLIGHT_EFFECT_ALTERNATING
+// /*== customize breathing effect ==*/
+//   /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
+//   #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
+//   /*==== use exp() and sin() ====*/
+//   #define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
+//   #define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
+//#endif
---
+++ keyboards/eek/readme.md
@@ -0,0 +1,15 @@
+# eek!
+
+![eek](picture_url)
+
+The eek! is a 36-key column staggered keyboard, meant to be portable with a steep split angle for holding close to the body.
+
+Keyboard Maintainer: [klackygears](https://github.com/klackygears)  
+Hardware Supported: The eek! PCBs, Pro Micro supported, Low Profile Elite-C supported 
+Hardware Availability: [PCB & Case Data](https://github.com/klackygears/eek)  
+
+Make example for this keyboard (after setting up your build environment):
+
+    make eek:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
