0ace0ae6a9cccfc0ee8d322b1ae95fc2a576a1f0
maikong <704340378@qq.com>
2020-11-25 23:25:37 +0800

    Update keymap.c

--- 键盘/mt64rgb/按键图/默认/keymap.c
+++ 键盘/mt64rgb/按键图/默认/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_UP,  KC_DEL,
         KC_LCTL,  KC_LALT, KC_LWIN,                     KC_SPC,                          MO(1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
                         ),
-    [1]   = LAYOUT(
+    [1]   =  LAYOUT_64_ansi(
         _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9  , KC_F10, KC_F11,    KC_F12,   _______,
         _______, _______,_______,_______, _______,_______,  _______, _______,_______,_______, _______,_______,_______, _______,
         RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, _______, _______,_______, _______,
