be903aaf432d4dfd6b269c904aad02dcf11c706b
npspears <40127181+npspears@users.noreply.github.com>
2020-11-25 16:03:26 -0600

    Update config.h
    took out the manu repeat

--- keyboards/plexus75/config.h
+++ keyboards/plexus75/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x5338
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    nasp
-#define PRODUCT         nasp Plexus75
+#define PRODUCT         Plexus75
 
 /* key matrix size */
 #define MATRIX_ROWS 5
