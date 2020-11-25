ed724ad890d07f02b923727d4ad30001785370f2
James Smith <bronzegears@gmail.com>
2020-11-25 17:41:04 -0500

    Update keyboards/eek/keymaps/ledtest/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/eek/keymaps/ledtest/keymap.c
+++ keyboards/eek/keymaps/ledtest/keymap.c
@@ -80,8 +80,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-  }
 
   switch (keycode) {
     case RGBRST:
