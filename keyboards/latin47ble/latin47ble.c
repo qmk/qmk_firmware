19fa49e8dce7e367d0f17d1a76f510aee02642fd
haierwangwei2005 <69899561+haierwangwei2005@users.noreply.github.com>
2020-11-26 05:13:47 +0800

    Update latin47ble.c

--- keyboards/latin47ble/latin47ble.c
+++ keyboards/latin47ble/latin47ble.c
@@ -1,4 +1,4 @@
- /* Copyright haierwangweei2005
+ /* Copyright haierwangwei2005
   * 
   * This program is free software: you can redistribute it and/or modify 
   * it under the terms of the GNU General Public License as published by 
