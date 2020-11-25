3ba1b12ceb68080d7069161fbf05812890fd5cd0
maikong <704340378@qq.com>
2020-11-25 22:05:44 +0800

    Update keymap.c

--- keyboards/mt64rgb/keymaps/default/keymap.c
+++ keyboards/mt64rgb/keymaps/default/keymap.c
@@ -1,17 +1,20 @@
+* Copyright 2020 MT 
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
 #include QMK_KEYBOARD_H
 
-#ifdef RGB_MATRIX_ENABLE
-#include "rgb_matrix.h"
-#include "i2c_master.h"
-
-extern rgb_config_t rgb_matrix_config;
-#endif
-
-// extern keymap_config_t keymap_config;
-
-#ifdef RGBLIGHT_ENABLE
-extern rgblight_config_t rgblight_config;
-#endif
 
 
 
