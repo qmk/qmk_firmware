f71250342720352ef8f714cc553e84769abe263b
4pplet <4pplet@protonmail.com>
2020-11-26 12:56:27 +0100

    Update keyboards/4pplet/waffling60/rev_b/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/4pplet/waffling60/rev_b/rules.mk
+++ keyboards/4pplet/waffling60/rev_b/rules.mk
@@ -17,8 +17,6 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-UNICODE_ENABLE = no         # Unicode
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE            # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
