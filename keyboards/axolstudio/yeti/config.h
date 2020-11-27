a94911ca6730c2e8668cccc5a256ce57ad3f995c
Lorenz Wellmer <lorwel@mailbox.org>
2020-11-27 20:57:39 +0100

    Update config.h

--- keyboards/axolstudio/yeti/config.h
+++ keyboards/axolstudio/yeti/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x9F9F
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Axolstudio
-#define PRODUCT      Project-No1
+#define PRODUCT      Yeti
 
 /* key matrix size */
 #define MATRIX_ROWS 5
