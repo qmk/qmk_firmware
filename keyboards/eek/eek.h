8087dcc893d6bae16369213b73c23677202d02fa
Klackygears <37446827+Klackygears@users.noreply.github.com>
2020-11-27 00:34:07 -0500

    cleaning things up

--- keyboards/eek/eek.h
+++ keyboards/eek/eek.h
@@ -17,27 +17,6 @@
 
 #include "quantum.h"
 
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
-
-#define LAYOUT_split_3x5_3( \
-    K00, K01, K02, K03, K04,     K05, K06, K07, K08, K09, \
-    K10, K11, K12, K13, K14,     K15, K16, K17, K18, K19, \
-    K20, K21, K22, K23, K24,     K25, K26, K27, K28, K29, \
-              K32, K33, K34,     K35, K36, K37 \
-) \
-{ \
-    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09 }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19 }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29 }, \
-    { KC_NO, KC_NO, K32, K33, K34, K35, K36, K37, KC_NO, KC_NO } \
-}
-*/
 #ifdef KEYBOARD_eek_silk_down
     #include "silk_down.h"
 #endif
