a854a543bf1f4bf78e3d6d4fdbde2c8c54b59c7a
bbrfkr <bbrfkr@gmail.com>
2020-11-27 13:50:27 +0900

    Update keyboards/scatter42/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/scatter42/config.h
+++ keyboards/scatter42/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define PRODUCT_ID   0x3B47
 #define DEVICE_VER   0x0001
 #define MANUFACTURER bbrfkr
 #define PRODUCT      scatter42
