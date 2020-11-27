e11485ff46cd21e738e92e2cb05d13b92e5fe7f6
haierwangwei2005 <69899561+haierwangwei2005@users.noreply.github.com>
2020-11-27 16:04:19 +0800

    Update keyboards/latin47ble/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/latin47ble/rules.mk
+++ keyboards/latin47ble/rules.mk
@@ -1,14 +1,16 @@
 # MCU name
 MCU = atmega32u4
 
-# Bootloader selection
-BOOTLOADER = caterina
+# Processor frequency
 F_CPU = 8000000
 
+# Bootloader selection
+BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
-BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration
+#
+BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -17,11 +19,8 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI controls
-UNICODE_ENABLE = no         # Unicode
-
-AUDIO_ENABLE = no           # Audio output on port C6
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 BLUETOOTH = AdafruitBLE
-RGBLIGHT_ENABLE = yes
