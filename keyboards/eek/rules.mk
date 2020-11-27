c468f7eaba802f30ee1a599c3c511d73e536c1c3
James Smith <bronzegears@gmail.com>
2020-11-27 00:33:40 -0500

    Update keyboards/eek/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/eek/rules.mk
+++ keyboards/eek/rules.mk
@@ -7,8 +7,8 @@ BOOTLOADER = qmk-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no       # Mouse keys
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
@@ -16,14 +16,12 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output 
+AUDIO_ENABLE = no           # Audio output
 
 #OPT_DEFS += -DFLIPPED      # Enable if board is flipped to have eek! silk on top
 #RGB_MATRIX_ENABLE = WS2812 # Enable for pretty RGB matrix effects
-
 
 LAYOUTS = split_3x5_3
