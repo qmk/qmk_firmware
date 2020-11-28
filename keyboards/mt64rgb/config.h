416f2f7b6e6d80efee77824b7c7b2135555484d1
maikong <704340378@qq.com>
2020-11-28 23:00:35 +0800

    Update config.h

--- keyboards/mt64rgb/config.h
+++ keyboards/mt64rgb/config.h
@@ -20,6 +20,10 @@
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4D54  // "MT"
 #define PRODUCT_ID      0x0001
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    MT
+#define PRODUCT         MT64RGB
+
 
 /* key matrix size */
 #define MATRIX_ROWS 5
