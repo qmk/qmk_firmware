b666a906b97f609d488426fd3d87b1ebcf7e925b
maikong <67684248+maikongk@users.noreply.github.com>
2020-11-28 23:15:40 +0800

    Update rules.mk

--- keyboards/mt84/rules.mk
+++ keyboards/mt84/rules.mk
@@ -7,7 +7,7 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -20,3 +20,6 @@ RGB_MATRIX_ENABLE = IS31FL3737      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+
+LAYOUTS = 75_ansi
+
