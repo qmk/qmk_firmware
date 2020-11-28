ab6373c370a0d4d1067006ac2d8f6ed0a86c20bf
Atsushi Morimoto (74th) <site@74th.tech>
2020-11-28 12:14:53 +0900

    fix default keymap back space

--- keyboards/sparrow62/keymaps/default/keymap.c
+++ keyboards/sparrow62/keymaps/default/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  // |-------+-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------+-------|
      KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  KC_LBRC,  KC_RBRC, KC_N,   KC_M,  KC_COMM, KC_DOT,KC_SLSH,KC_BSLS,
  // \-------+-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------+-------/
-                           KC_ESC, KC_LALT,KC_LGUI, KC_SPC,      KC_ENT, MO(_FN),KC_RALT,KC_BSLS
+                           KC_ESC, KC_LALT,KC_LGUI, KC_SPC,      KC_ENT, MO(_FN),KC_RALT,KC_BSPC
  //                       \-------+-------+-------+-------/     \-------+-------+-------+-------/
   ),
 
