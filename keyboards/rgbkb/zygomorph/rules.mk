495c4b00f3c0eb9ec1db7adc52f6f0a475b38709
fauxpark <fauxpark@gmail.com>
2020-11-28 22:12:40 +1100

    Remove enable flag for zygomorph

--- keyboards/rgbkb/zygomorph/rules.mk
+++ keyboards/rgbkb/zygomorph/rules.mk
@@ -30,7 +30,6 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 
 SPLIT_KEYBOARD = yes
