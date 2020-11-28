e1d639632bd498efe7f5001b4f086e6642938259
pabile <pabile@gmail.com>
2020-11-28 17:01:26 +0800

    updated keymaps

--- keyboards/pabile/p42/promicro/ver2/keymaps/2encoders/config.h
+++ keyboards/pabile/p42/promicro/ver2/keymaps/2encoders/config.h
@@ -17,5 +17,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define ENCODERS_PAD_B { D3, F4 }
-#define ENCODERS_PAD_A { D2, F5 }
+#define ENCODERS_PAD_A { D3, F4 }
+#define ENCODERS_PAD_B { D2, F5 }
