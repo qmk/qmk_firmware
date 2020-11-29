5a8d77ef464c6489439d564cc055e17da13452cd
Takeshi Nishio <kkeennnn@gmail.com>
2020-11-30 06:16:28 +0900

    New keyboard "stmeishi"

---
+++ keyboards/stmeishi/stmeishi.c
@@ -0,0 +1,29 @@
+/* Copyright 2017 skully <skullydazed@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "stmeishi.h"
+
+void matrix_init_kb(void) {
+
+}
+
+void matrix_scan_kb(void) {
+
+}
+
+// const uint8_t music_map[MATRIX_ROWS][MATRIX_COLS] = LAYOUT_rr_matrix(
+//      3,  4,  5,
+// 	 0,  1,  2
+// );
