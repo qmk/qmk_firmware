b527b5fb229f61deb2e3fc8c5ead9161b1655990
ojthetiny <65928618+ojthetiny@users.noreply.github.com>
2020-11-29 22:21:17 +0200

    Update keyboards/edc40/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/edc40/config.h
+++ keyboards/edc40/config.h
@@ -18,7 +18,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0x5454 // TT
+#define VENDOR_ID 0x4F4A // "OJ"
 #define PRODUCT_ID 0x0002
 #define DEVICE_VER 0x0001
 #define MANUFACTURER OJ
