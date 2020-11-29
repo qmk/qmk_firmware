20b1522fa42fa6fcee4853721db83f1873f31abb
Giulio Mecocci <giuliomecocci@hotmail.it>
2020-11-29 02:41:56 -0500

    Do not use keypad keycodes

--- keyboards/lets_split/keymaps/gm2211/keymap.c
+++ keyboards/lets_split/keymaps/gm2211/keymap.c
@@ -65,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LAYER4] =  LAYOUT_ortho_4x12( \
-  KC_TILD,         KC_EXLM,         KC_AT,    KC_LCBR,      KC_RCBR,      KC_PIPE,         KC_KP_SLASH,     KC_7,    KC_8,     KC_9,    KC_AMPR,    KC_BSLASH, \
-  ___,             KC_HASH,         KC_DLR,   KC_LPRN,      KC_RPRN,      KC_GRAVE,        KC_KP_ASTERISK,  KC_4,    KC_5,     KC_6,    KC_0,       KC_DQUO,   \
-  ___,             KC_PERC,         KC_CIRC,  KC_LBRACKET,  KC_RBRACKET,  KC_TILD,         KC_PLUS,         KC_1,    KC_2,     KC_3,    KC_KP_DOT,  KC_ENTER,  \
-  ___,             ___,             KC_LALT,  KC_LGUI,      KC_LGUI,      ___,             ___,             RAISE2,  KC_UNDS,  KC_DOT,  KC_TILD,   ___
+  KC_TILD,         KC_EXLM,         KC_AT,    KC_LCBR,      KC_RCBR,      KC_PIPE,         KC_SLASH,     KC_7,    KC_8,     KC_9,    KC_AMPR,   KC_BSLASH, \
+  ___,             KC_HASH,         KC_DLR,   KC_LPRN,      KC_RPRN,      KC_GRAVE,        LSFT(KC_8),   KC_4,    KC_5,     KC_6,    KC_0,      KC_DQUO,   \
+  ___,             KC_PERC,         KC_CIRC,  KC_LBRACKET,  KC_RBRACKET,  KC_TILD,         KC_PLUS,      KC_1,    KC_2,     KC_3,    KC_DOT,    KC_ENTER,  \
+  ___,             ___,             KC_LALT,  KC_LGUI,      KC_LGUI,      ___,             ___,          RAISE2,  KC_UNDS,  KC_DOT,  KC_TILD,   ___
 ),
 
 [_RAISE2] = LAYOUT_ortho_4x12( \
