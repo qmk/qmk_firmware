7f0bfbd807a5da0cb48546d4fb1bc5c82d48f099
Takeshi Nishio <kkeennnn@gmail.com>
2020-11-30 20:49:22 +0900

    Update copyrght.

--- keyboards/jones/v.0.3/matrix.c
+++ keyboards/jones/v.0.3/matrix.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2012-2018 Jun Wako, Jack Humbert, Yiancar
+Copyright 2012-2020 Jun Wako, Jack Humbert, Yiancar, Takeshi Nishio
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
