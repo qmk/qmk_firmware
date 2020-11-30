882df66df608779970c84b7ea740afbe08f00693
Takeshi Nishio <kkeennnn@gmail.com>
2020-11-30 20:34:05 +0900

    Update Rotary Encoder pins to actual used count.

--- keyboards/jones/v.0.3/config.h
+++ keyboards/jones/v.0.3/config.h
@@ -52,9 +52,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define DIODE_DIRECTION COL2ROW
 
 /* Rotary Encoder */
-#define ENCODERS_PAD_A { F6, B3, B5, B4 }
-#define ENCODERS_PAD_B { F7, B2, D3, D2 }
-#define ENCODER_RESOLUTION 4    //the default & suggested is 4
+#define ENCODERS_PAD_A { F6, B3 }
+#define ENCODERS_PAD_B { F7, B2 }
+#define ENCODER_RESOLUTION 4  //the default & suggested is 4
 
 /* Audio */
 #ifdef AUDIO_ENABLE
