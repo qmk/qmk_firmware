0dada741e6f68c2a17c3f2c7f6a86b0a3cb4c910
Daveyr <26368709+Daveyr@users.noreply.github.com>
2020-11-30 12:40:21 +0000

    Update keyboards/yampad/yampad.c
    As suggested
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/yampad/yampad.c
+++ keyboards/yampad/yampad.c
@@ -15,3 +15,48 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "yampad.h"
+
+__attribute__((weak))
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_270;  // flips the display 270 degrees
+}
+
+__attribute__((weak)) 
+void oled_task_user(void) {
+  // Host Keyboard Layer Status
+  oled_write_P(PSTR("Layer"), false);
+  switch (get_highest_layer(layer_state)) {
+    case _BL:
+      oled_write_ln_P(PSTR(" BAS"), false);
+      break;
+    case _NV:
+      oled_write_ln_P(PSTR(" NAV"), false);
+      break;
+    case _FN:
+      oled_write_ln_P(PSTR(" RGB"), false);
+      break;
+    default:
+      // Or use the write_ln shortcut over adding '\n' to the end of your string
+      oled_write_ln_P(PSTR(" UND"), false);
+  }
+
+  // Host Keyboard LED Status
+  led_t led_state = host_keyboard_led_state();
+  oled_write_P(PSTR("-----"), false);
+  oled_write_P(PSTR("Stats"), false);
+  oled_write_P(led_state.num_lock ? PSTR("num:*") : PSTR("num:."), false);
+  oled_write_P(led_state.caps_lock ? PSTR("cap:*") : PSTR("cap:."), false);
+  oled_write_P(led_state.scroll_lock ? PSTR("scr:*") : PSTR("scr:."), false);
+
+  // Host Keyboard RGB backlight status
+  oled_write_P(PSTR("-----"), false);
+  oled_write_P(PSTR("Light"), false);
+
+  static char led_buf[30];
+  snprintf(led_buf, sizeof(led_buf) - 1, "RGB:%cM: %2d\nh: %2ds: %2dv: %2d\n",
+      rgblight_is_enabled() ? '*' : '.', (uint8_t)rgblight_get_mode(),
+      (uint8_t)(rgblight_get_hue() / RGBLIGHT_HUE_STEP),
+      (uint8_t)(rgblight_get_sat() / RGBLIGHT_SAT_STEP),
+      (uint8_t)(rgblight_get_val() / RGBLIGHT_VAL_STEP));
+  oled_write(led_buf, false);
+}
