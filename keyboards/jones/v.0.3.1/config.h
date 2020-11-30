b047ccd1dc620478cd5ce4250359c9d903ada99d
Takeshi Nishio <kkeennnn@gmail.com>
2020-11-30 20:39:05 +0900

    Change default Effects.

--- keyboards/jones/v.0.3.1/config.h
+++ keyboards/jones/v.0.3.1/config.h
@@ -88,13 +88,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   /*== all animations enable ==*/
   // #define RGBLIGHT_ANIMATIONS
   /*== or choose animations ==*/
-  #define RGBLIGHT_EFFECT_BREATHING
-  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  // #define RGBLIGHT_EFFECT_BREATHING
+  // #define RGBLIGHT_EFFECT_RAINBOW_MOOD
   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
   // #define RGBLIGHT_EFFECT_SNAKE
   #define RGBLIGHT_EFFECT_KNIGHT
   // #define RGBLIGHT_EFFECT_CHRISTMAS
-  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+  // #define RGBLIGHT_EFFECT_STATIC_GRADIENT
   // #define RGBLIGHT_EFFECT_RGB_TEST
   // #define RGBLIGHT_EFFECT_ALTERNATING
   // /*== customize breathing effect ==*/
