c008108e2ebdf68638e3c5626c2552615a9f4666
haierwangwei2005 <69899561+haierwangwei2005@users.noreply.github.com>
2020-11-30 08:00:50 +0800

    Update keymap.c

--- keyboards/latin47ble/keymaps/via/keymap.c
+++ keyboards/latin47ble/keymaps/via/keymap.c
@@ -27,6 +27,7 @@ enum layers {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+   const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_47( 
 	KC_ESC,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,    KC_P,    KC_BSPC,
 	KC_X0,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,    KC_SCLN, KC_ENT,
