bba17fbb36a4500f4cea1832ea2726b383b03ffc
haierwangwei2005 <69899561+haierwangwei2005@users.noreply.github.com>
2020-11-30 09:24:04 +0800

    Update 10bleoledhub.h

--- keyboards/10bleoledhub/10bleoledhub.h
+++ keyboards/10bleoledhub/10bleoledhub.h
@@ -16,7 +16,6 @@
 #pragma once
 
 #include "quantum.h"
-
 #define LAYOUT( \
     K00, \
     K10, K11, K12,  \
@@ -24,7 +23,7 @@
     K30, K31, K32   \
 ) { \
     { K00,   KC_NO,   KC_NO }, \
-    { K10,   K11,   K12 }, \
+    { K30,   K31,   K32 }, \
     { K20,   K21,   K22 }, \
-    { K30,   K31,   K32 }  \
+    { K10,   K11,   K12 }  \
 }
