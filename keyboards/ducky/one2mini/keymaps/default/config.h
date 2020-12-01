20d063ef74f948d1cde2277e15c76837da7850b9
Reza Jelveh <git@jelveh.me>
2020-12-01 20:45:54 +0800

    ducky: make default mouse key behavior more linear

--- keyboards/ducky/one2mini/keymaps/default/config.h
+++ keyboards/ducky/one2mini/keymaps/default/config.h
@@ -17,3 +17,8 @@
 #pragma once
 
 // place overrides here
+#define MK_3_SPEED
+#define MK_C_OFFSET_UNMOD   400    /* Cursor offset per movement (unmodified) */
+#define MK_C_INTERVAL_UNMOD  5     /* Time between cursor movements (unmodified) */
+#define MK_W_OFFSET_UNMOD   100    /* Scroll steps per scroll action (unmodified) */
+#define MK_W_INTERVAL_UNMOD 10     /* Time between scroll steps (unmodified) */
