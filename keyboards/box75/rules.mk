e7c6cfb29e616cd5679244c39625fc944681489d
Álvaro A. Volpato <alvaro.volpato@usp.br>
2020-12-01 16:55:49 -0300

    Update keyboards/box75/rules.mk
    Remove comment lines from BLUETOOTH_ENABLE and AUDIO_ENABLE
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/box75/rules.mk
+++ keyboards/box75/rules.mk
@@ -15,8 +15,8 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = no
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
