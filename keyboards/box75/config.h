b40d87179fe40957e3cf184b00a50291c1f6686b
Álvaro A. Volpato <alvaro.volpato@usp.br>
2020-12-01 16:55:05 -0300

    Update keyboards/box75/config.h
    Remove USB description
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/box75/config.h
+++ keyboards/box75/config.h
@@ -23,7 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Lin Design
 #define PRODUCT         BOX75
-#define DESCRIPTION	A 75 percent keyboard that is modern
 
 /* key matrix size */
 #define MATRIX_ROWS 6
