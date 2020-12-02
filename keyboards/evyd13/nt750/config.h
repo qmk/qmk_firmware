6c7344f161651768cb469ef0a6060df5a3f3b1f5
Evy Dekkers <17366381+evyd13@users.noreply.github.com>
2020-12-02 07:51:41 +0100

    update LED configuration

--- keyboards/evyd13/nt750/config.h
+++ keyboards/evyd13/nt750/config.h
@@ -47,6 +47,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
+#define LED_CAPS_LOCK_PIN D5
+#define LED_PIN_ON_STATE 0
+
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
--- keyboards/evyd13/nt750/nt750.c
+++ keyboards/evyd13/nt750/nt750.c
@@ -14,27 +14,3 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "nt750.h"
-
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
-
-void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
-
-    matrix_init_user();
-    led_init_ports();
-}
-
-void led_init_ports(void) {
-    setPinOutput(D5);
-}
-
-bool led_update_kb(led_t led_state) {
-    if(led_update_user(led_state)) {
-        writePin(D5, !led_state.caps_lock);
-    }
-
-    return true;
-}
