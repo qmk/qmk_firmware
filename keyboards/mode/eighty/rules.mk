0330fef02a3b499796bb793064a1ef8e6a0593e6
Álvaro A. Volpato <alvaro.volpato@usp.br>
2020-12-02 15:25:05 -0300

    Update keyboards/mode/eighty/rules.mk
    Remove feature disabling in rules.mk
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mode/eighty/rules.mk
+++ keyboards/mode/eighty/rules.mk
@@ -15,11 +15,8 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
