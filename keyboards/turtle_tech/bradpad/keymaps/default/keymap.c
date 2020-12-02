e6c25b9c79d4469cac93479db3a87877baee1a4b
wxyangf <willsfann@gmail.com>
2020-12-02 17:55:37 -0500

    updated default keymap

--- keyboards/turtle_tech/bradpad/keymaps/default/keymap.c
+++ keyboards/turtle_tech/bradpad/keymaps/default/keymap.c
@@ -2,9 +2,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT( 
-		KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_PLUS, \
-		KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_MINUS, \
-		KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH, \
-		KC_NUMLOCK, KC_KP_0, KC_INSERT, KC_KP_ASTERISK, \
-		     KC_ENT, KC_DELETE)
+		KC_KP_7,    KC_KP_8,     KC_KP_9,   KC_KP_MINUS, \
+		KC_KP_4,    KC_KP_5,     KC_KP_6,   KC_KP_PLUS, \
+		KC_KP_1,    KC_KP_2,     KC_KP_3,   KC_KP_SLASH, \
+		KC_NUMLOCK, KC_KP_0,     KC_KP_DOT, KC_KP_ASTERISK, \
+		            KC_KP_ENTER, KC_KP_EQUAL)
 };
