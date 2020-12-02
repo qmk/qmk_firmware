cb6892890a183f493638d604c675448b2fd052f7
Zach White <skullydazed@gmail.com>
2020-12-01 16:04:38 -0800

    clean up generate-api errors

--- keyboards/ergotravel/rules.mk
+++ keyboards/ergotravel/rules.mk
@@ -1,16 +1,3 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
-BOOTLOADER = caterina
-
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
