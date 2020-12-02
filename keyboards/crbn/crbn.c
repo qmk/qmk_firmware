9c342a0a14597cb12426ef71750e88a396882e96
ReFil <31960031+ReFil@users.noreply.github.com>
2020-12-02 20:30:05 +0000

    formatting tweaks to meet requirements

--- keyboards/crbn/crbn.c
+++ keyboards/crbn/crbn.c
@@ -1,9 +1,10 @@
 #include "crbn.h"
-
+/* Encoder setting. only one encoder despite 4 possible spots */
 void encoder_update_kb(uint8_t index, bool clockwise) {
     if (clockwise) {
         tap_code(KC_VOLU);
-    } else {
+    }
+    else {
         tap_code(KC_VOLD);
     }
 }
