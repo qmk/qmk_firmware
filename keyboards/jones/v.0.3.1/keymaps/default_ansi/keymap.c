84eb026ad9f33befa2d402b12a12e53ca1bb51d1
Takeshi Nishio <kkeennnn@gmail.com>
2020-12-03 21:28:23 +0900

    Update: Change layer handling from process_record_user to layer_state_set_user.

--- keyboards/jones/v.0.3.1/keymaps/default_ansi/keymap.c
+++ keyboards/jones/v.0.3.1/keymaps/default_ansi/keymap.c
@@ -26,20 +26,6 @@ enum layer_number {
     _ADJUST
 };
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  MAC = SAFE_RANGE,
-  WIN,
-  NUM,
-  LOWER,
-  RAISE,
-  NUM_RAISE,
-  ADJUST,
-  ALT_US,
-  ALT_JP,
-  ALT_GRV,
-};
-
 // Tap Dance
 enum tap_dances{
     TD_LSFT_CAPS = 0,
@@ -67,6 +53,12 @@ void ql_each(qk_tap_dance_state_t *state, void *user_data);
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
     [TD_ESC_NUM] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ql_finished, ql_reset, 275),
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  MAC = SAFE_RANGE,
+  WIN,
 };
 
 // Custom keycodes
@@ -110,20 +102,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PSCR,    _______,_______,_______,_______,_______,_______,_______,_______,_______,KC_HOME,KC_UP,      KC_END, KC_VOLU,
         _______,    _______,_______,_______,_______,_______,_______,_______,_______,KC_PGUP,KC_LEFT,KC_RGHT,            KC_VOLD,
         _______,        _______,_______,_______,_______,_______,_______,_______,_______,KC_PGDN,KC_DOWN,_______,KC_PGUP,_______,
-        _______,_______,_______,_______,        _______,        ADJUST,         _______,_______,_______,KC_HOME,KC_PGDN,KC_END
+        _______,_______,_______,_______,        _______,        _______,         _______,_______,_______,KC_HOME,KC_PGDN,KC_END
     ),
     [_RAISE] = LAYOUT_ansi(
         KC_PAUS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_INS, KC_DEL,
         KC_PSCR,    _______,_______,CT_E,   _______,_______,_______,_______,_______,_______,KC_PGUP,KC_DEL,     _______,_______,
         _______,    CT_A,   _______,KC_DEL, KC_RGHT,KC_ESC, KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_MINS,KC_INS,             _______,
-        _______,        _______,_______,_______,_______,KC_LEFT,KC_PGDN,KC_ENT, _______,KC_MRWD,KC_MFFD,_______,KC_PGUP,ADJUST,
+        _______,        _______,_______,_______,_______,KC_LEFT,KC_PGDN,KC_ENT, _______,KC_MRWD,KC_MFFD,_______,KC_PGUP,_______,
         _______,_______,_______,_______,        _______,        _______,        _______,_______,_______,KC_HOME,KC_PGDN,KC_END
     ),
     [_NUM_RAISE] = LAYOUT_ansi(
         KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV,
         KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,    KC_LBRC,KC_RBRC,
         KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENT,            KC_QUOT,
-        S_CAP,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, C_SLSH, KC_RSFT,KC_UP,  ADJUST,
+        S_CAP,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, C_SLSH, KC_RSFT,KC_UP,  _______,
         _______,_______,_______,_______,        _______,        _______,        _______,_______,_______,_______,_______,_______
     ),
     [_ADJUST] = LAYOUT_ansi(
@@ -145,52 +137,22 @@ const uint8_t music_map[MATRIX_ROWS][MATRIX_COLS] = LAYOUT_ansi(
 );
 #endif
 
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
-switch (keycode) {
-    case MAC: // Change default ayer --> Write to EEPROM
-        if (record->event.pressed) {
-            set_single_persistent_default_layer(_MAC);
-        }
-        return false;
-        break;
-    case WIN: // Change default ayer --> Write to EEPROM
-        if (record->event.pressed) {
-            set_single_persistent_default_layer(_WIN);
-        }
-        return false;
-        break;
-    case LOWER:
-        if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        }
-        return false;
-        break;
-    case RAISE:
-        if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-        return false;
-        break;
-    case ADJUST:
-        if (record->event.pressed) {
-            layer_on(_ADJUST);
-        } else {
-            layer_off(_ADJUST);
-        }
-        return false;
-        break;
-    default:
-        break;
+    switch (keycode) {
+        case MAC:  // Change default ayer --> Write to EEPROM
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_MAC);
+            }
+            return false;
+            break;
+        case WIN:  // Change default ayer --> Write to EEPROM
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_WIN);
+            }
+            return false;
+            break;
+        default:
+            break;
     }
     return true;
 }
@@ -256,6 +218,8 @@ void keyboard_post_init_user(void) {
 
 // Enabling and disabling lighting layers
 layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+
     rgblight_set_layer_state(2, layer_state_cmp(state, _NUM));
     rgblight_set_layer_state(4, layer_state_cmp(state, _LOWER));
     rgblight_set_layer_state(5, layer_state_cmp(state, _RAISE));
