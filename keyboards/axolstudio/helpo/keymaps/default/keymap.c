5f80b2ade895707594d641ab378d2b402f29b4e0
kb-elmo <lorwel@mailbox.org>
2020-12-04 08:49:42 +0100

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/axolstudio/helpo/keymaps/default/keymap.c
+++ keyboards/axolstudio/helpo/keymaps/default/keymap.c
@@ -18,15 +18,15 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [0] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5, \
-        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, \
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E, \
-        KC_F,    KC_G,    KC_H,    KC_I,    MO(1) \
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
+        KC_F,    KC_G,    KC_H,    KC_I,    MO(1)
     ),
     [1] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
