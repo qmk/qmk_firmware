93c9d69e501abbe649e7ed3fa564dac02769d2b4
Ramon Imbao <ramonimbao@gmail.com>
2020-12-04 11:17:05 +0800

    Flip encoder direction

--- keyboards/ramonimbao/herringbone/pro/config.h
+++ keyboards/ramonimbao/herringbone/pro/config.h
@@ -51,6 +51,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENCODERS_PAD_A { C1 }
 #define ENCODERS_PAD_B { C0 }
 
+#define ENCODER_DIRECTION_FLIP
+
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
