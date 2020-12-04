8a331144886f1b4b77b965b596ecf5cdf7e5af8d
Reza Jelveh <git@jelveh.me>
2020-12-04 19:29:32 +0800

    ducky: move winkey grave esc to default keymap

--- keyboards/ducky/one2mini/keymaps/default/config.h
+++ keyboards/ducky/one2mini/keymaps/default/config.h
@@ -17,6 +17,7 @@
 #pragma once
 
 // place overrides here
+#define GRAVE_ESC_GUI_OVERRIDE
 #define MK_3_SPEED
 #define MK_C_OFFSET_UNMOD   400    /* Cursor offset per movement (unmodified) */
 #define MK_C_INTERVAL_UNMOD  5     /* Time between cursor movements (unmodified) */
