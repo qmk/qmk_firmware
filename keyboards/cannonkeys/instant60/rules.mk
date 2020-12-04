0f086df89fba8dcc073a15d7b9c000aed21a5f5d
Andrew Kannan <andrew.kannan@klaviyo.com>
2020-12-03 21:22:05 -0500

    Allow APM32 MCUs on certain CannonKeys PCBs (#10899)
    * Allow APM32 MCUs on certain CannonKeys PCBs
    
    * another one
    
    * Update DFU suffix args
    
    * missed one
    
    * Apply suggestions from code review
    
    Co-authored-by: Joel Challis <git@zvecr.com>
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/cannonkeys/instant60/rules.mk
+++ keyboards/cannonkeys/instant60/rules.mk
@@ -19,6 +19,8 @@ WS2812_DRIVER = spi
 
 LAYOUTS = 60_ansi 60_tsangan_hhkb
 
+# Wildcard to allow APM32 MCU 
+DFU_SUFFIX_ARGS = -p FFFF -v FFFF
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
--- keyboards/cannonkeys/satisfaction75/rules.mk
+++ keyboards/cannonkeys/satisfaction75/rules.mk
@@ -24,6 +24,8 @@ QWIIC_ENABLE += MICRO_OLED
 
 DEFAULT_FOLDER = cannonkeys/satisfaction75/rev1
 
+# Wildcard to allow APM32 MCU 
+DFU_SUFFIX_ARGS = -p FFFF -v FFFF
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
