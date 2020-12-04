0472842b741a974c239c9de9f8fdca56381b5dfd
kb-elmo <lorwel@mailbox.org>
2020-12-04 08:50:25 +0100

    Update keyboards/axolstudio/yeti/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/axolstudio/yeti/config.h
+++ keyboards/axolstudio/yeti/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x05E4
+#define VENDOR_ID    0x525C
 #define PRODUCT_ID   0x9F9F
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Axolstudio
