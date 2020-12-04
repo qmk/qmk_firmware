ef653ba44c30b2e2ee7921ef0c20f156f1234e5b
YangPiCui <yang.pi.cui@gmail.com>
2020-12-04 12:44:27 +0800

    Update keyboards/handwired/evk/v1_3/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/evk/v1_3/config.h
+++ keyboards/handwired/evk/v1_3/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Yang Cui
 #define PRODUCT evk v1_3
-#define DESCRIPTION Ergonomic Vertical Keyboard (EVK) version 1.3 with Teensy 2.0
 
 /* key matrix size */
 #define MATRIX_ROWS 6
