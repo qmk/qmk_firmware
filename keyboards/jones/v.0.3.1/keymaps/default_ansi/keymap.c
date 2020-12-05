055115b683f0578c04326b6128d91855a05fecd5
Takeshi Nishio <kkeennnn@gmail.com>
2020-12-06 08:01:10 +0900

    Update: Remove unused keycode.

--- keyboards/jones/v.0.3.1/keymaps/default_ansi/keymap.c
+++ keyboards/jones/v.0.3.1/keymaps/default_ansi/keymap.c
@@ -70,7 +70,6 @@ enum custom_keycodes {
 #define CT_A    LCTL(KC_A)
 #define ALT_GRV LALT(KC_GRV)
 #define LOWER   MO(_LOWER)
-#define ADJUST  MO(_ADJUST)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
