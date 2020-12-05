e62abd406edfd18d8a68104229bfc1207b74fcd4
Drashna Jaelre <drashna@live.com>
2020-12-05 11:43:11 -0800

    Apply suggestions from code review
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/handwired/dactyl_manuform/5x6_right_trackball/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6_right_trackball/rules.mk
@@ -6,21 +6,21 @@ MCU = at90usb1286
 BOOTLOADER = halfkay
 
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
+#   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
-NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output
-UNICODE_ENABLE = no         # Unicode
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+AUDIO_ENABLE = no           # Audio output
 POINTING_DEVICE_ENABLE = yes
 
 SPLIT_KEYBOARD = yes
