8758331c87c276d40ab748b42f661f50c796ebe1
Carter Codell <cjcodell1@gmail.com>
2020-12-06 15:18:54 -0500

    added nimrod firmware

---
+++ keyboards/nimrod/keymaps/default_right_space/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#ifdef COMBO_ENABLE
+#    define COMBO_COUNT 3
+#    define COMBO_TERM 150
+#endif
---
+++ keyboards/nimrod/keymaps/default_split_space/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#ifdef COMBO_ENABLE
+#    define COMBO_COUNT 5
+#    define COMBO_TERM 150
+#endif
