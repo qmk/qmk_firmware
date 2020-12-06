953a91434fd06e5070d7616cda0dec806f29cb29
Joshua Diamond <josh@windowoffire.com>
2020-12-06 17:55:55 -0500

    dmqdesigns spin - a little more refined keymap now

--- keyboards/dmqdesign/spin/keymaps/spidey3spin/keymap.c
+++ keyboards/dmqdesign/spin/keymaps/spidey3spin/keymap.c
@@ -15,10 +15,21 @@
  */
 #include QMK_KEYBOARD_H
 
+#include <version.h>
+#include <stdlib.h>
+
+#define RGB_LAYER_ACK_DURATION 500
+
 enum layers {
+    _MACRO,
     _NUMPAD,
     _RGB,
-    _MACRO
+    _FN
+};
+
+enum layer_base {
+    LAYER_BASE     = _MACRO,
+    LAYER_BASE_END = _FN + 1,
 };
 
 enum custom_keycodes {
@@ -29,10 +40,17 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_MACRO] = LAYOUT(
+                MO(_FN),       HELLO,         CH_SUSP,       TO(_MACRO),
+                KC_NO,         KC_NO,         KC_NO,         TO(_NUMPAD),
+                C(A(KC_COMM)), KC_NO,         C(A(KC_DOT)),  TO(_RGB),
+                KC_NO,         CH_ASST,       CH_CPNL
+                ),
+
     [_NUMPAD] = LAYOUT(
-                KC_KP_7, KC_KP_8, KC_KP_9, TO(_NUMPAD),
-                KC_KP_4, KC_KP_5, KC_KP_6, TO(_RGB),
-                KC_KP_1, KC_KP_2, KC_KP_3, TO(_MACRO),
+                KC_KP_7, KC_KP_8, KC_KP_9, KC_TRNS,
+                KC_KP_4, KC_KP_5, KC_KP_6, KC_TRNS,
+                KC_KP_1, KC_KP_2, KC_KP_3, KC_TRNS,
                 KC_KP_0, KC_PDOT, KC_PENT
                 ),
 
@@ -43,27 +61,144 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 RGB_RMOD, RGB_TOG, RGB_MOD
                 ),
 
-
-    [_MACRO] = LAYOUT(
-                HELLO,   DEBUG,   RESET,   KC_TRNS,
+    [_FN] = LAYOUT(
+                KC_TRNS, DEBUG,   RESET,   KC_TRNS,
+                KC_NO,   KC_NO,   EEP_RST, KC_TRNS,
                 KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-                KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-                CH_CPNL, CH_ASST, CH_SUSP
+                KC_NO,   KC_NO,   KC_NO
                 ),
 };
+
+typedef enum layer_ack {
+    ACK_NO = 0,
+    ACK_YES,
+    ACK_MEH,
+} layer_ack_t;
+
+#define LAYER_OFFSET 0
+const rgblight_segment_t PROGMEM _macro_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_GREEN});
+const rgblight_segment_t PROGMEM _numpad_layer[] = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_GREEN});
+const rgblight_segment_t PROGMEM _rgb_layer[] = RGBLIGHT_LAYER_SEGMENTS({2, 1, HSV_GREEN});
+const rgblight_segment_t PROGMEM _fn_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_PURPLE});
+
+#define ACK_OFFSET 4
+const rgblight_segment_t PROGMEM _no_layer[]     = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_RED});
+const rgblight_segment_t PROGMEM _yes_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_GREEN});
+const rgblight_segment_t PROGMEM _meh_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_YELLOW});
+
+const rgblight_segment_t *const PROGMEM _rgb_layers[] = {
+    [LAYER_OFFSET + 0] = _macro_layer,
+    [LAYER_OFFSET + 1] = _numpad_layer,
+    [LAYER_OFFSET + 2] = _rgb_layer,
+    [LAYER_OFFSET + 3] = _fn_layer, 
+
+    [ACK_OFFSET + ACK_NO]     = _no_layer,
+    [ACK_OFFSET + ACK_YES]    = _yes_layer,
+    [ACK_OFFSET + ACK_MEH]    = _meh_layer,
+
+    [ACK_OFFSET + ACK_MEH + 1] = NULL
+};
+
+const uint8_t PROGMEM _n_rgb_layers = sizeof(_rgb_layers) / sizeof(_rgb_layers[0]) - 1;
+
+void clear_rgb_layers(void) {
+    dprint("clear_rgb_layers()\n");
+    for (uint8_t i = 0; i < _n_rgb_layers; i++) {
+        rgblight_set_layer_state(i, false);
+    }
+}
+
+void do_rgb_layers(layer_state_t state, uint8_t start, uint8_t end) {
+    dprintf("start=%u, end=%u, LAYER_OFFSET=%u\n", start, end, LAYER_OFFSET);
+    for (uint8_t i = start; i < end; i++) {
+        bool is_on = layer_state_cmp(state, i);
+        uint8_t rl = LAYER_OFFSET + i;
+        dprintf("layer[%u]=%u, rl=%u\n", i, is_on, rl);
+        rgblight_set_layer_state(rl, is_on);
+    }   
+}   
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    do_rgb_layers(state, LAYER_BASE, LAYER_BASE_END);
+    return state;
+}
+
+void rgb_layer_ack(layer_ack_t n) {
+    uint8_t layer = ACK_OFFSET + n;
+    dprintf("rgb_layer_ack(%u) ==> %u\n", n, layer);
+    rgblight_blink_layer(layer, RGB_LAYER_ACK_DURATION);
+}
+
+void rgb_layer_ack_yn(bool yn) { rgb_layer_ack(yn ? ACK_YES : ACK_NO); }
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = _rgb_layers;
+    do_rgb_layers(layer_state, LAYER_BASE, LAYER_BASE_END);
+}
+
+void shutdown_user() {
+    clear_rgb_layers();
+    rgblight_enable();
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_sethsv_noeeprom(HSV_RED);
+}
+
+void spidey_glow(void) {
+    rgblight_enable();
+    rgblight_mode(RGBLIGHT_MODE_RAINBOW_MOOD);
+    rgblight_sethsv(255, 230, 128);
+}
+
+void eeconfig_init_user(void) { spidey_glow(); }
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
+            // Re-implement this here, but fix the persistence!
+            case DEBUG:
+                if (!debug_enable) {
+                    debug_enable = 1;
+                } else if (!debug_keyboard) {
+                    debug_keyboard = 1;
+                } else if (!debug_matrix) {
+                    debug_matrix = 1;
+                } else {
+                    debug_enable   = 0;
+                    debug_keyboard = 0;
+                    debug_matrix   = 0;
+                }
+                uprintf("DEBUG: enable=%u, keyboard=%u, matrix=%u\n", debug_enable, debug_keyboard, debug_matrix);
+                uprintln(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION ", Built on: " QMK_BUILDDATE);
+                eeconfig_update_debug(debug_config.raw);
+                return false;
+
+                 // clang-format off
             case CH_CPNL: host_consumer_send(AL_CONTROL_PANEL); return false;
             case CH_ASST: host_consumer_send(AL_ASSISTANT); return false;
             case CH_SUSP: tap_code16(LGUI(LSFT(KC_L))); return true;
             case HELLO:   SEND_STRING("Hello, world!"); return true;
+                 // clang-format on
         }
     }
-    
+
     return true;
 };  
+
+extern rgblight_config_t rgblight_config;
+
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // Acks follow...
+        case DEBUG:
+            rgb_layer_ack_yn(debug_enable);
+            break;
+        case RGB_TOG:
+            rgb_layer_ack_yn(rgblight_config.enable);
+            break;
+    }
+}
 
 void encoder_update_user(uint8_t index, bool clockwise) {
 
@@ -93,15 +228,15 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     default:
       if (index == 0) {
         if (clockwise) {
-          tap_code(KC_BRIU);
+          tap_code16(C(S(KC_MINS)));
         } else {
-          tap_code(KC_BRID);
+          tap_code16(C(S(KC_EQL)));
         }
       } else if (index == 1) {
         if (clockwise) {
-          tap_code(KC_DOWN);
+          tap_code16(C(KC_MINS));
         } else {
-          tap_code(KC_UP);
+          tap_code16(C(KC_EQL));
         }
       } else if (index == 2) {
         if (clockwise) {
@@ -114,3 +249,4 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
   }
 }
+
