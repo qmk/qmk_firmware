b86f03c778e5389b1ae236aaa048b3d3f6c141a5
Takeshi Nishio <kkeennnn@gmail.com>
2020-12-06 13:11:08 +0900

    Update: Change default layout to "ALL".

--- keyboards/jones/v.0.3/keymaps/default/keymap.c
+++ keyboards/jones/v.0.3/keymaps/default/keymap.c
@@ -17,9 +17,9 @@
 #include "keymap_jp.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_jp(
+    [0] = LAYOUT_all(
         KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   JP_MINS,KC_BSPC,JP_CIRC,JP_YEN,
-        KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,               JP_AT,  JP_LBRC,
+        KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    XXXXXXX,   JP_AT,  JP_LBRC,
         KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   JP_SCLN, KC_ENT,    JP_COLN,JP_RBRC,
         KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   JP_COMM,JP_DOT, JP_SLSH,JP_BSLS,KC_UP,  XXXXXXX,
         KC_MUTE,KC_LALT,KC_LGUI,KC_LANG2,       KC_SPC,         KC_SPC,        KC_LANG1,KC_RGUI,KC_RALT,KC_LEFT,KC_DOWN,KC_RGHT
