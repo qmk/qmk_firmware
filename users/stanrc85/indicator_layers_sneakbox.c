b3c217b143c973caab15d5f441e4bf0f9d777810
stanrc85 <stanrc@gmail.com>
2020-12-06 15:10:43 -0500

    cleaned up indicator code

--- users/stanrc85/indicator_layers_sneakbox.c
+++ users/stanrc85/indicator_layers_sneakbox.c
@@ -16,31 +16,13 @@
 
 #include "stanrc85.h"
 
-static uint8_t middle = 1;
-static uint8_t bottom = 1;
-
 layer_state_t layer_state_set_user(layer_state_t state) {
-    middle = bottom = 1;
-    switch (get_highest_layer(state)) {
-    case _FN1_60:
-        middle = 0;
-        break;
-    case _FN2_60:
-        bottom = 0;
-        break;
-    case _DEFAULT:
-        middle = 0;
-        bottom = 0;
-        break;
-    default: //  for any other layers, or the default layer
-        break;
-    }
+    writePin(INDICATOR_PIN_2, layer_state_cmp(state, _FN2_60) || layer_state_cmp(state, _DEFAULT));
+    writePin(INDICATOR_PIN_1, layer_state_cmp(state, _FN1_60) || layer_state_cmp(state, _DEFAULT));    
     return state;
 }
 
 bool led_update_user(led_t led_state) {
     writePin(INDICATOR_PIN_0, led_state.caps_lock);
-    writePin(INDICATOR_PIN_1, !middle);
-    writePin(INDICATOR_PIN_2, !bottom);
     return false;
 }
