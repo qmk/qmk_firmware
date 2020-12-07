9f93f76ada04a141da12ce918b90b06a7f49b628
Joshua Diamond <josh@windowoffire.com>
2020-12-06 20:18:32 -0500

    a few more refinements for spin

--- keyboards/dmqdesign/spin/keymaps/spidey3spin/keymap.c
+++ keyboards/dmqdesign/spin/keymaps/spidey3spin/keymap.c
@@ -20,12 +20,7 @@
 
 #define RGB_LAYER_ACK_DURATION 500
 
-enum layers {
-    _MACRO,
-    _NUMPAD,
-    _RGB,
-    _FN
-};
+enum layers { _MACRO, _NUMPAD, _RGB, _FN };
 
 enum layer_base {
     LAYER_BASE     = _MACRO,
@@ -39,35 +34,33 @@ enum custom_keycodes {
     CH_SUSP,  // Suspend
 };
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MACRO] = LAYOUT(
-                MO(_FN),       HELLO,         CH_SUSP,       TO(_MACRO),
-                KC_NO,         KC_NO,         KC_NO,         TO(_NUMPAD),
-                C(A(KC_COMM)), KC_NO,         C(A(KC_DOT)),  TO(_RGB),
-                KC_NO,         CH_ASST,       CH_CPNL
-                ),
+        A(S(KC_N)),    HELLO,         CH_SUSP,       TO(_MACRO), 
+        KC_MPRV,       KC_MPLY,       KC_MNXT,       TO(_NUMPAD), 
+        C(A(KC_COMM)), KC_F5,         C(A(KC_DOT)),  TO(_RGB),
+        MO(_FN),       CH_ASST,       CH_CPNL),
 
     [_NUMPAD] = LAYOUT(
-                KC_KP_7, KC_KP_8, KC_KP_9, KC_TRNS,
-                KC_KP_4, KC_KP_5, KC_KP_6, KC_TRNS,
-                KC_KP_1, KC_KP_2, KC_KP_3, KC_TRNS,
-                KC_KP_0, KC_PDOT, KC_PENT
-                ),
+        KC_KP_7,   KC_KP_8,   KC_KP_9,   KC_TRNS,
+        KC_KP_4,   KC_KP_5,   KC_KP_6,   KC_TRNS,
+        KC_KP_1,   KC_KP_2,   KC_KP_3,   KC_TRNS,
+        KC_KP_0,   KC_PDOT,   KC_PENT),
 
     [_RGB] = LAYOUT(
-                RGB_HUI,  RGB_SAI, RGB_VAI, KC_TRNS,
-                RGB_HUD,  RGB_SAD, RGB_VAD, KC_TRNS,
-                KC_NO,    KC_NO,   KC_NO,   KC_TRNS,
-                RGB_RMOD, RGB_TOG, RGB_MOD
-                ),
+        RGB_HUI,   RGB_SAI,   RGB_VAI,   KC_TRNS,
+        RGB_HUD,   RGB_SAD,   RGB_VAD,   KC_TRNS,
+        RGB_SPD,   RGB_SPI,   KC_NO,     KC_TRNS,
+        RGB_RMOD,  RGB_TOG,   RGB_MOD),
 
     [_FN] = LAYOUT(
-                KC_TRNS, DEBUG,   RESET,   KC_TRNS,
-                KC_NO,   KC_NO,   EEP_RST, KC_TRNS,
-                KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-                KC_NO,   KC_NO,   KC_NO
-                ),
+        KC_TRNS,   DEBUG,     RESET,     KC_TRNS,
+        KC_NO,     KC_NO,     EEP_RST,   KC_TRNS,
+        KC_NO,     KC_NO,     KC_NO,     KC_TRNS,
+        KC_NO,     KC_NO,     KC_NO),
 };
+// clang-format on 
 
 typedef enum layer_ack {
     ACK_NO = 0,
@@ -76,28 +69,30 @@ typedef enum layer_ack {
 } layer_ack_t;
 
 #define LAYER_OFFSET 0
-const rgblight_segment_t PROGMEM _macro_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_GREEN});
-const rgblight_segment_t PROGMEM _numpad_layer[] = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_GREEN});
-const rgblight_segment_t PROGMEM _rgb_layer[] = RGBLIGHT_LAYER_SEGMENTS({2, 1, HSV_GREEN});
-const rgblight_segment_t PROGMEM _fn_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_PURPLE});
+const rgblight_segment_t PROGMEM _macro_layer[]  = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_TEAL});
+const rgblight_segment_t PROGMEM _numpad_layer[] = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_TEAL});
+const rgblight_segment_t PROGMEM _rgb_layer[]    = RGBLIGHT_LAYER_SEGMENTS({2, 1, HSV_TEAL});
+const rgblight_segment_t PROGMEM _fn_layer[]     = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_PURPLE});
 
 #define ACK_OFFSET 4
-const rgblight_segment_t PROGMEM _no_layer[]     = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_RED});
-const rgblight_segment_t PROGMEM _yes_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_GREEN});
-const rgblight_segment_t PROGMEM _meh_layer[]    = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_YELLOW});
+const rgblight_segment_t PROGMEM _no_layer[]  = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_RED});
+const rgblight_segment_t PROGMEM _yes_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_GREEN});
+const rgblight_segment_t PROGMEM _meh_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_YELLOW});
 
+// clang-format on 
 const rgblight_segment_t *const PROGMEM _rgb_layers[] = {
     [LAYER_OFFSET + 0] = _macro_layer,
     [LAYER_OFFSET + 1] = _numpad_layer,
     [LAYER_OFFSET + 2] = _rgb_layer,
-    [LAYER_OFFSET + 3] = _fn_layer, 
-
-    [ACK_OFFSET + ACK_NO]     = _no_layer,
-    [ACK_OFFSET + ACK_YES]    = _yes_layer,
-    [ACK_OFFSET + ACK_MEH]    = _meh_layer,
+    [LAYER_OFFSET + 3] = _fn_layer,
+   
+    [ACK_OFFSET + ACK_NO] = _no_layer,
+    [ACK_OFFSET + ACK_YES] = _yes_layer,
+    [ACK_OFFSET + ACK_MEH] = _meh_layer,
 
     [ACK_OFFSET + ACK_MEH + 1] = NULL
 };
+// clang-format off 
 
 const uint8_t PROGMEM _n_rgb_layers = sizeof(_rgb_layers) / sizeof(_rgb_layers[0]) - 1;
 
@@ -111,12 +106,12 @@ void clear_rgb_layers(void) {
 void do_rgb_layers(layer_state_t state, uint8_t start, uint8_t end) {
     dprintf("start=%u, end=%u, LAYER_OFFSET=%u\n", start, end, LAYER_OFFSET);
     for (uint8_t i = start; i < end; i++) {
-        bool is_on = layer_state_cmp(state, i);
-        uint8_t rl = LAYER_OFFSET + i;
+        bool    is_on = layer_state_cmp(state, i);
+        uint8_t rl    = LAYER_OFFSET + i;
         dprintf("layer[%u]=%u, rl=%u\n", i, is_on, rl);
         rgblight_set_layer_state(rl, is_on);
-    }   
-}   
+    }
+}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     do_rgb_layers(state, LAYER_BASE, LAYER_BASE_END);
@@ -152,7 +147,6 @@ void spidey_glow(void) {
 
 void eeconfig_init_user(void) { spidey_glow(); }
 
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
@@ -174,17 +168,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 eeconfig_update_debug(debug_config.raw);
                 return false;
 
-                 // clang-format off
+                // clang-format off
             case CH_CPNL: host_consumer_send(AL_CONTROL_PANEL); return false;
             case CH_ASST: host_consumer_send(AL_ASSISTANT); return false;
             case CH_SUSP: tap_code16(LGUI(LSFT(KC_L))); return true;
             case HELLO:   SEND_STRING("Hello, world!"); return true;
-                 // clang-format on
+                // clang-format on
         }
     }
 
     return true;
-};  
+};
 
 extern rgblight_config_t rgblight_config;
 
@@ -201,52 +195,37 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
+    switch (get_highest_layer(layer_state)) {
+        case _RGB:
+            if (index == 0) {
+                if (clockwise) {
+                    rgblight_increase_hue();
+                } else {
+                    rgblight_decrease_hue();
+                }
+            } else if (index == 1) {
+                if (clockwise) {
+                    rgblight_increase_sat();
+                } else {
+                    rgblight_decrease_sat();
+                }
+            } else if (index == 2) {
+                if (clockwise) {
+                    rgblight_increase_val();
+                } else {
+                    rgblight_decrease_val();
+                }
+            }
+            break;
 
-  switch (get_highest_layer(layer_state)) {
-    case _RGB:
-      if (index == 0) {
-        if (clockwise) {
-          rgblight_increase_hue();
-        } else {
-          rgblight_decrease_hue();
-        }
-      } else if (index == 1) {
-        if (clockwise) {
-          rgblight_increase_sat();
-        } else {
-          rgblight_decrease_sat();
-        }
-      } else if (index == 2) {
-        if (clockwise) {
-          rgblight_increase_val();
-        } else {
-          rgblight_decrease_val();
-        }
-      }
-      break;
-
-    default:
-      if (index == 0) {
-        if (clockwise) {
-          tap_code16(C(S(KC_MINS)));
-        } else {
-          tap_code16(C(S(KC_EQL)));
-        }
-      } else if (index == 1) {
-        if (clockwise) {
-          tap_code16(C(KC_MINS));
-        } else {
-          tap_code16(C(KC_EQL));
-        }
-      } else if (index == 2) {
-        if (clockwise) {
-          tap_code(KC_VOLU); 
-        } else {
-          tap_code(KC_VOLD);
-        }
-      }
-      break;
-
-  }
+        default:
+            if (index == 0) {
+                tap_code16(C(S(clockwise ? KC_EQL : KC_MINS)));
+            } else if (index == 1) {
+                tap_code16(C(clockwise ? KC_EQL : KC_MINS));
+            } else if (index == 2) {
+                tap_code(clockwise ? KC_VOLU : KC_VOLD);
+            }
+            break;
+    }
 }
-
