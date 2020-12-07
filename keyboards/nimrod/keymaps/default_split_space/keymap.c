de0ebd03126302d6d81b1331a28e725672a2b1a5
Carter Codell <cjcodell1@gmail.com>
2020-12-06 22:47:44 -0500

    swap space and backspace

--- keyboards/nimrod/keymaps/default_split_space/keymap.c
+++ keyboards/nimrod/keymaps/default_split_space/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T, KC_Y, KC_U,  KC_I,    KC_O,   KC_P,
   KC_CA, KC_AS, KC_GD, KC_SF, KC_G, KC_H, KC_SJ, KC_GK,   KC_AL,  KC_CENT,
   KC_Z,  KC_X,  KC_C,  KC_V,  KC_B, KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH,
-             LT(_NAV, KC_SPC),               LT(_NUM_SYM, KC_BSPC)
+             LT(_NUM_SYM, KC_BSPC),               LT(_NAV, KC_SPC)
   ),
 
   [_NUM_SYM] = LAYOUT_split_space(
