6ff6c45e0b0503956e2466cf150c32c64cc18fe1
wangyifu <18438880@qq.com>
2020-12-07 09:07:01 +0800

    Update rules.mk

--- keyboards/latin64ble/rules.mk
+++ keyboards/latin64ble/rules.mk
@@ -1,9 +1,15 @@
 # MCU name
 MCU = atmega32u4
 
-BOOTLOADER = caterina
 F_CPU = 8000000
-
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
