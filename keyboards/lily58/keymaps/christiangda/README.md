f37329eeb76091ae15dbc01f5a0ae94eb8fb3b20
Christian González Di Antonio <christiangda@gmail.com>
2020-12-08 14:48:40 +0100

    added my custom keymap configuration

---
+++ keyboards/lily58/keymaps/christiangda/README.md
@@ -0,0 +1,9 @@
+# Lily58 PRO christiangda
+
+![Layout](keymap-layout.png)
+
+## MysticMechs keymap
+
+### Build and Flash
+
+    make lily58:christiangda:avrdude
---
+++ keyboards/lily58/keymaps/christiangda/rules.mk
@@ -0,0 +1,4 @@
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+OLED_DRIVER_ENABLE = yes     # OLED display
+ENCODER_ENABLE = yes 		# Rotary Encoder
