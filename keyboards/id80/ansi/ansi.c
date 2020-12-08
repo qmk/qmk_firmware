a3625acf8d20623162bb876151bd15e3c9063ebf
Carsten <modebm@gmail.com>
2020-12-08 13:43:48 +0100

    add support for ID80v2 with ISO layout

--- keyboards/id80/ansi/ansi.c
+++ keyboards/id80/ansi/ansi.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "id80.h"
+#include "ansi.h"
 
 void matrix_init_kb(void) {
     led_init_ports();
