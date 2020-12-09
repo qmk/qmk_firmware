b2a4db8429ba6bb6ce7aab0071a430d889f13f8f
rogthefrog <rogthefrog@gmail.com>
2020-12-08 22:48:25 -0800

    replaced individual constants with an enum for extensibility

--- keyboards/keebio/quefrency/keymaps/rogthefrog/keymap.c
+++ keyboards/keebio/quefrency/keymaps/rogthefrog/keymap.c
@@ -4,8 +4,12 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _FN1 1
+
+enum layers {
+    _BASE,
+    _FN1,
+};
+
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
