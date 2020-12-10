70bd32c1c9fa7a08855f150da389d4846a037bbd
pabile <pabile@gmail.com>
2020-12-10 22:19:43 +0800

    added p9 default keymap

---
+++ keyboards/pabile/p9/keymaps/default/keymap.c
@@ -0,0 +1,48 @@
+/*
+Copyright 2020 Pabile
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3)
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+  if (index == 0) { /* First encoder */
+    if (clockwise) {
+      tap_code(KC_VOLD); 
+    } else {
+      tap_code(KC_VOLU);
+    }
+  } else if (index == 1) { /* Second encoder */  
+    if (clockwise) {
+     tap_code(KC_WH_U); 
+    } else { 
+     tap_code(KC_WH_D); 
+    }
+  } else if (index == 2) { /* Third encoder */  
+    if (clockwise) {
+      tap_code(KC_VOLU);
+    } else {
+      tap_code(KC_VOLD);
+    }
+  }
+}
+
