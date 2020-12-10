7e5de26699eb1de0f34dc8b7046c27346cf7058c
James Badger <james@jamesbadger.ca>
2020-12-09 20:07:28 -0700

    Use namespaced define for revision header files
    The longer name will make it clearer where the define is kept in
    the keyboards directory.
    
    `key_count` is removed as it is optional (qmk#10513) and can be
    auto-counted by reading the layout array.

--- keyboards/ymdk/ymd09/ymd09.h
+++ keyboards/ymdk/ymd09/ymd09.h
@@ -2,8 +2,8 @@
 
 #include "quantum.h"
 
-#if defined(KEYBOARD_ymd09_rev1)
+#if defined(KEYBOARD_ymdk_ymd09_rev1)
     #include "rev1.h"
-#elif defined(KEYBOARD_ymd09_rev2)
+#elif defined(KEYBOARD_ymdk_ymd09_rev2)
     #include "rev2.h"
 #endif
