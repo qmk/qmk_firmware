9d2d6e6324d85163c13327b6b81a0d73fcec5c40
TJ Campie <tom.campie@gmail.com>
2020-12-11 04:54:39 -0600

    rules.mk updates

--- keyboards/le_chiffre/rules.mk
+++ keyboards/le_chiffre/rules.mk
@@ -11,13 +11,14 @@ MCU = atmega32u4
 #   ATmega328P   USBasp
 BOOTLOADER = atmel-dfu
 
-BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes            # USB Nkey Rollover
+NKRO_ENABLE = yes           # USB Nkey Rollover
+RGBLIGHT_ENABLE = no 		# Enable keyboard RGB underglow
 TAP_DANCE_ENABLE = no
 ENCODER_ENABLE = yes
 OLED_DRIVER_ENABLE = no
-
-RGB_MATRIX_ENABLE = WS2812
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
