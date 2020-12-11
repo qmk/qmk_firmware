8a26f9e33645f3976177b89055f1c687bbd330d1
David Doan <ddoan@stanford.edu>
2020-12-10 21:28:38 -0800

    changed VID for future via compatibility

--- keyboards/arrayperipherals/1x4p1/config.h
+++ keyboards/arrayperipherals/1x4p1/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
+#define VENDOR_ID       0x4152 // "AR"
 #define PRODUCT_ID      0x4F46 // "OF"
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Array Peripherals
