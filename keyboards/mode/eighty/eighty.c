7b82aa014b714f4d94705d1fe5fab7196f03f3d7
Álvaro A. Volpato <alvaro.volpato@usp.br>
2020-12-12 18:41:36 -0300

    Apply suggestions from code review
    Correct comment characters
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mode/eighty/eighty.c
+++ keyboards/mode/eighty/eighty.c
@@ -9,6 +9,6 @@
   * but WITHOUT ANY WARRANTY; without even the implied warranty of 
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
   * GNU General Public License for more details. 
-  * 
+  */
 
 #include "eighty.h"
--- keyboards/mode/eighty/eighty.h
+++ keyboards/mode/eighty/eighty.h
@@ -9,7 +9,7 @@
   * but WITHOUT ANY WARRANTY; without even the implied warranty of 
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
   * GNU General Public License for more details. 
-  *
+  */
 
 
 #pragma once
