a0c0ee05e555f7db9a10a57ba5cd617f407bd1cf
Jonathon Carstens <jon.carstens@gmail.com>
2020-12-12 12:46:53 -0800

    Adding Tenkey++ to QMK fork

---
+++ keyboards/lizard_trick/tenkey_plusplus/config.h
@@ -0,0 +1,64 @@
+/*
+Copyright 2020 Jonathon Carstens jonathon@lizardtrick.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0x0000
+#define PRODUCT_ID 0x0000
+#define DEVICE_VER 0x0000
+#define MANUFACTURER Lizard Trick Electronics
+#define PRODUCT Tenkey++
+#define DESCRIPTION 10-key Shaped Macropad
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 4
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+
+#define MATRIX_ROW_PINS { B7, D4, B5, B6, C6, C7, }
+#define MATRIX_COL_PINS { D5, D3, D2, F7 }
+#define UNUSED_PINS
+
+#define ENCODERS_PAD_A \
+    { B4, F0, F4 }
+#define ENCODERS_PAD_B \
+    { D7, E6, F1 }
+
+#define ENCODER_RESOLUTION 4
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
---
+++ keyboards/lizard_trick/tenkey_plusplus/rules.mk
@@ -0,0 +1,6 @@
+MCU = atmega32u4
+
+BOOTLOADER = atmel-dfu
+
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+ENCODER_ENABLE = yes        # Rotary Encoder support
