368981691dcd20b591d2009c73c2f72996199ac2
mtei <2170248+mtei@users.noreply.github.com>
2020-12-12 19:03:24 +0900

    [Keymap] fix and update helix/rev3_5rows:five_rows
    * fix rgblight animation selection
    * use `DEBUG_MATRIX_SCAN_RATE_ENABLE` insted of keyboard_post_init_user_scan.c

--- keyboards/helix/rev3_5rows/keymaps/five_rows/rules.mk
+++ keyboards/helix/rev3_5rows/keymaps/five_rows/rules.mk
@@ -11,11 +11,12 @@
  #      yes, yes +3200
  #      no,  yes +400
 LTO_ENABLE = no  # if firmware size over limit, try this option
+LED_ANIMATIONS = yes
 
 ifneq ($(strip $(HELIX)),)
   define KEYMAP_OPTION_PARSE
-    # $xinfo .$1.x #debug
-    # parse  'dispoff', 'consle', 'back', 'oled'
+    # parse 'dispoff', 'consle', 'back', 'oled', 'no-ani', 'mini-ani', 'lto', 'no-lto', 'scan'
+    $(if $(SHOW_PARCE),$(info parse .$1.))  #debug
     ifeq ($(strip $1),dispoff)
         OLED_DRIVER_ENABLE = no
         RGBLIGHT_ENABLE = no
@@ -29,16 +30,37 @@ ifneq ($(strip $(HELIX)),)
     ifeq ($(strip $1),back)
         RGBLIGHT_ENABLE = yes
     endif
+    ifneq ($(filter na no_ani no-ani,$(strip $1)),)
+        LED_ANIMATIONS = no
+    endif
+    ifneq ($(filter mini-ani mini_ani,$(strip $1)),)
+        LED_ANIMATIONS = mini
+    endif
+    ifneq ($(filter ani animation,$(strip $1)),)
+        LED_ANIMATIONS = yes
+    endif
+    ifeq ($(strip $1),lto)
+        LTO_ENABLE = yes
+    endif
+    ifneq ($(filter nolto no-lto no_lto,$(strip $1)),)
+        LTO_ENABLE = no
+    endif
     ifeq ($(strip $1),scan)
         # use DEBUG_MATRIX_SCAN_RATE
         # see docs/newbs_testing_debugging.md
-        OPT_DEFS +=  -DDEBUG_MATRIX_SCAN_RATE
-        CONSOLE_ENABLE = yes
-        SRC += keyboard_post_init_user_scan.c
+        DEBUG_MATRIX_SCAN_RATE_ENABLE = yes
     endif
   endef # end of KEYMAP_OPTION_PARSE
 
   COMMA=,
   $(eval $(foreach A_OPTION_NAME,$(subst $(COMMA), ,$(HELIX)),  \
       $(call KEYMAP_OPTION_PARSE,$(A_OPTION_NAME))))
+endif
+
+ifeq ($(strip $(LED_ANIMATIONS)), yes)
+    OPT_DEFS += -DLED_ANIMATIONS=2
+endif
+
+ifeq ($(strip $(LED_ANIMATIONS)), mini)
+    OPT_DEFS += -DLED_ANIMATIONS=1
 endif
