c1d73a7eba3dc6c47619d9d7f5fd8168758babf8
Christian González Di Antonio <christiangda@gmail.com>
2020-12-13 12:12:32 +0100

    Revert "added my custom keymap configuration"
    This reverts commit f37329eeb76091ae15dbc01f5a0ae94eb8fb3b20.

--- keyboards/lily58/keymaps/christiangda/README.md
+++
@@ -1,9 +0,0 @@
-# Lily58 PRO christiangda
-
-![Layout](keymap-layout.png)
-
-## MysticMechs keymap
-
-### Build and Flash
-
-    make lily58:christiangda:avrdude
--- keyboards/lily58/keymaps/christiangda/rules.mk
+++
@@ -1,4 +0,0 @@
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
-OLED_DRIVER_ENABLE = yes     # OLED display
-ENCODER_ENABLE = yes 		# Rotary Encoder
