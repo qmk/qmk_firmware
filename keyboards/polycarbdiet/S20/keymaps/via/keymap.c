4236ce9aeed99955d90007204f5dd563e4d22855
Galib <62521946+polycarbdiet@users.noreply.github.com>
2020-12-13 18:25:27 -0500

    via support

---
+++ keyboards/polycarbdiet/S20/keymaps/via/keymap.c
@@ -0,0 +1,47 @@
+/* Copyright 2020 Muhammad Galib <muhammadmgalib@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_NLCK, KC_PAST, KC_PSLS, KC_BSPC,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PMNS,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,   KC_PDOT, MO(1)
+    ),
+    [1] = LAYOUT(
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        KC_MPRV, KC_MPLY, KC_MNXT, _______,
+        KC_VOLD, KC_MUTE, KC_VOLU, _______,
+        _______, MO(2),   _______
+    ),
+    [2] = LAYOUT(
+        RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
+        RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______
+    ),
+    [3] = LAYOUT(
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______
+    )
+};
