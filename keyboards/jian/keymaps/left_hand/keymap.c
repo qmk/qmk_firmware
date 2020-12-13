4a3cd77605e57a57a6b440e658699fe7f5fa2c1a
KGOH <kgofhedgehogs@gmail.com>
2020-12-14 00:18:04 +0200

    Remove mod tap on mirrored layer

--- keyboards/jian/keymaps/left_hand/keymap.c
+++ keyboards/jian/keymaps/left_hand/keymap.c
@@ -18,9 +18,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        LT(_RAIS_L, KC_BSPC), LT(_DFLT_R, KC_SPC), LT(_LOWR_L, KC_ENT)
 ),
 [_DFLT_R] = LAYOUT_symmetric_right(
-  KC_Y,                KC_U,    KC_I,               KC_O,    KC_P,    RGUI_T(KC_LBRC), RSFT_T(KC_RBRC),
-  KC_H,                KC_J,    KC_K,               KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
-  KC_N,                KC_M,    KC_COMM,            KC_DOT,  KC_SLSH, RALT_T(KC_BSLS),
+  KC_Y,                KC_U,    KC_I,               KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+  KC_H,                KC_J,    KC_K,               KC_L,    KC_SCLN, KC_QUOT,
+  KC_N,                KC_M,    KC_COMM,            KC_DOT,  KC_SLSH, KC_BSLS,
   LT(_LOWR_R, KC_ESC), _______, LT(_RAIS_R, KC_DEL)
 ),
 [_RAIS_L] = LAYOUT_symmetric_left(
