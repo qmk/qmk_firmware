818c2333ddbe972929c1d7d3a0188cb103b3ce5d
BenRoe <mail@benjaminroesner.com>
2020-12-14 19:37:17 +0100

    add kimiko rgb matrix keymap (only master working)

---
+++ keyboards/keycapsss/kimiko/keymaps/matrix/rules.mk
@@ -0,0 +1,11 @@
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = WS2812
+# RGB_MATRIX_DRIVER = WS2812
+
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+
+# LTO: Link Time Optimizations.
+# Reduce compiled size, but will automatically disable the legacy TMK Macros and Functions features.
+# This does not affect QMK Macros and Layers
+LTO_ENABLE = yes
