a39f53ba204677cebf4aae5314a5cc30aa9e2274
Franco Rosi <fraanrosi@gmail.com>
2020-12-14 18:19:24 -0300

    -Se agregan Mouse Keys (para manejo del mouse con el teclado). -Se hace fix en el modo "Bloq. Mayús" (Se persiste el cambio de RGB mode mientras el Bloq. Mayús está activado).

--- keyboards/helix/rev2_latam/keymaps/latam/config.h
+++ keyboards/helix/rev2_latam/keymaps/latam/config.h
@@ -36,3 +36,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
    #define RGBLIGHT_EFFECT_RGB_TEST
    #define RGBLIGHT_EFFECT_ALTERNATING
 #endif
+
+
+#ifdef MOUSEKEY_ENABLE
+   #undef MOUSEKEY_INTERVAL
+   #define MOUSEKEY_INTERVAL 20
+
+   #undef MOUSEKEY_TIME_TO_MAX
+   #define MOUSEKEY_TIME_TO_MAX 40
+
+   #undef MOUSEKEY_MAX_SPEED
+   #define MOUSEKEY_MAX_SPEED 6
+  
+   #undef MOUSEKEY_MOVE_DELTA
+   #define MOUSEKEY_MOVE_DELTA 5
+
+   #undef MOUSEKEY_DELAY
+   #define MOUSEKEY_DELAY 0
+#endif
