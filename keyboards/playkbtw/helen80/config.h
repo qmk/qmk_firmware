6a7c3bcdf98c53b370523a00b13208b347a9bea5
Barry Huang <yj7272098@gmail.com>
2020-12-16 01:06:52 +0800

    Reduce size and set correct RGB LED number

--- keyboards/playkbtw/helen80/config.h
+++ keyboards/playkbtw/helen80/config.h
@@ -50,7 +50,7 @@
 
 #define RGB_DI_PIN E2
 #ifdef RGB_DI_PIN
-    #define RGBLED_NUM 95
+    #define RGBLED_NUM 87
     #define RGBLIGHT_HUE_STEP 8
     #define RGBLIGHT_SAT_STEP 8
     #define RGBLIGHT_VAL_STEP 8
