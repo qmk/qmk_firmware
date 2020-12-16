4ac4259088be4656a93c39341611a09bc959d8d4
joedinkle <brandon.bonton@gmail.com>
2020-12-16 07:00:14 -0500

    Formatting and cleanup.

--- keyboards/eyeohdesigns/sprh/keymaps/default/keymap.c
+++ keyboards/eyeohdesigns/sprh/keymaps/default/keymap.c
@@ -28,24 +28,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_CAPS,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,  \
            KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,              KC_RSFT, KC_UP,\
            KC_LCTL,  KC_LGUI, KC_LALT,           KC_SPC,                    MO(FUNCTN), KC_RALT,       KC_RGUI,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT\
-				 ),
-				 
+         ),
+
     [FUNCTN] = LAYOUT(/* Base */
         RESET,     KC_GRAVE,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11, KC_F12,   KC_DEL, \
                KC_TAB,   KC_TRNS,    ______,   ______,   ______,    ______,    ______,    ______, ______,    ______, KC_PSCR,    KC_LBRC, KC_RBRC, KC_BSLS, \
              KC_CAPS,     ______,    ______,  KC_TRNS, ______,    ______,    ______,    ______,    ______, ______,    ______,     ______,       KC_ENT,  \
            KC_LSFT,       ______,    ______,   ______,    ______,    ______,    ______,    ______, ______,    ______,    ______,    ______, KC_PGUP,\
            KC_LCTL,  KC_LGUI, KC_LALT,     KC_SPC,                    KC_RALT,       KC_RGUI,       ______,       KC_RCTL, KC_HOME, KC_PGDN, KC_END\
-				 )
+         )
 };
-
-
-
-void matrix_init_user(void) {}
-
-void matrix_scan_user(void) {}
-
-void led_set_user(uint8_t usb_led) {}
 
 void encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) {
