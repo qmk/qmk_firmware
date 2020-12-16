147ea839f9701c66bfb1fa71418e3e08ca6e921d
Robert Verst <robert.verst@tobit.com>
2020-12-16 15:45:25 +0100

    Add some keycodes

--- users/rverst/rverst.c
+++ users/rverst/rverst.c
@@ -70,6 +70,11 @@ void switch_mode(int mode) {
 #endif
 }
 
+void is_unicode(int mode) {
+    return mode == MAC_UNI || mode == WINDOWS_UNI || mode == LINUX_UNI;
+}
+
+
 //**********************
 // keyboard_pre_init
 //**********************
@@ -139,11 +144,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case RV_SM1:
         case RV_SM1S:
-            set_mode(MAC, keycode == RV_SM1S);
-            return false;
-        case RV_SM2:
-        case RV_SM2S:
-            set_mode(MAC, keycode == RV_SM2S);
+            set_mode(WINDOWS, keycode == RV_SM1S);
             return false;
         case RV_SM3:
         case RV_SM3S:
@@ -158,11 +159,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             set_mode(LINUX_UNI, keycode == RV_SM5S);
             return false;
 
+
+        // Screenshot
+        case RV_SNAP:
+            if (mode == MAC || mode == MAC_UNI) {
+                if (ls) unregister_code(KC_LSFT);
+                if (rs) unregister_code(KC_RSFT);
+
+                register_code(KC_LGUI);
+                register_code(KC_LSFT);
+                if (as) tap_code(KC_5);
+                else tap_code(KC_4);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_LGUI);
+
+                if (ls) register_code(KC_LSFT);
+                if (rs) register_code(KC_RSFT);
+            } else if (mode == WINDOWS || mode == WINDOWS_UNI) {
+                register_code(KC_LGUI);
+                register_code(KC_LSFT);
+                tap_code(KC_S);
+                register_code(KC_LSFT);
+                register_code(KC_LGUI);
+            }
+        return false;
+
         // Umlauts - äÄöÖüÜ
+        // with legacy-mode for MAC and WINDOWS without unicode support
         case RV_AUML:
         case RV_OUML:
         case RV_UUML:
-            if (mode == MAC) {
+            if (is_unicode(mode)) {
+                if (keycode == RV_AUML) {
+                    if (as) send_unicode_string("Ä");
+                    else send_unicode_string("ä");
+                } else if (keycode == RV_OUML) {
+                    if (as) send_unicode_string("Ö");
+                    else send_unicode_string("ö");
+                } else if (keycode == RV_UUML) {
+                    if (as) send_unicode_string("Ü");
+                    else send_unicode_string("ü");
+                }
+            } else if (mode == MAC) {
                 if (ls) unregister_code(KC_LSFT);
                 if (rs) unregister_code(KC_RSFT);
 
@@ -182,43 +220,107 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     unregister_code(KC_LSFT);
                     register_code(KC_RSFT);
                 }
-            } else if (mode > LINUX) {
+            } else if (mode == WINDOWS) {
+                if (ls) unregister_code(KC_LSFT);
+                if (rs) unregister_code(KC_RSFT);
+
+                register_code(KC_RALT);
+                tap_code(KC_1);
                 if (keycode == RV_AUML) {
-                    if (as) send_unicode_string("Ä");
-                    else send_unicode_string("ä");
+                    if (as) tap_code(KC_4);
+                    else tap_code(KC_3);
+                    tap_code(KC_2);
                 } else if (keycode == RV_OUML) {
-                    if (as) send_unicode_string("Ö");
-                    else send_unicode_string("ö");
+                    if (as) {
+                        tap_code(KC_5);
+                        tap_code(KC_3);
+                    } else {
+                        tap_code(KC_4);
+                        tap_code(KC_8);
+                    }
                 } else if (keycode == RV_UUML) {
-                    if (as) send_unicode_string("Ü");
-                    else send_unicode_string("ü");
+                    if (as) {
+                        tap_code(KC_5);
+                        tap_code(KC_4);
+                    } else {
+                        tap_code(KC_2);
+                        tap_code(KC_9);
+                    }
                 }
+                unregister_code(KC_RALT);
+
+                if (ls) register_code(KC_LSFT);
+                if (rs) register_code(KC_RSFT);
+
             }
             return false;
 
         // Euro sign - €
+        // with legacy-mode for MAC and WINDOWS without unicode support
         case RV_EUR:
-            if (mode == MAC) {
+            if (is_unicode(mode)) {
+                send_unicode_string("€");
+            } else if (mode == MAC) {
                 register_code(KC_LALT);
                 register_code(KC_LSFT);
                 tap_code(KC_2);
                 unregister_code(KC_LSFT);
                 unregister_code(KC_LALT);
-            } else if (mode > LINUX) {
-                send_unicode_string("€");
+            } else if (mode == WINDOWS) {
+                register_code(KC_RALT);
+                tap_code(KC_0);
+                tap_code(KC_1);
+                tap_code(KC_2);
+                tap_code(KC_8);
+                unregister_code(KC_RALT);
             }
             return false;
 
         // Sharp-S - ß
+        // with legacy-mode for MAC and WINDOWS without unicode support
         case RV_SZ:
-            if (mode == MAC) {
+            if (is_unicode(mode)) {
+                send_unicode_string("ß");
+            } else if (mode == MAC) {
                 register_code(KC_LALT);
                 tap_code(KC_S);
                 unregister_code(KC_LALT);
-            } else if (mode > LINUX) {
-                send_unicode_string("ß");
+            } else if (mode == WINDOWS) {
+                register_code(KC_RALT);
+                tap_code(KC_2);
+                tap_code(KC_2);
+                tap_code(KC_5);
+                unregister_code(KC_RALT);
             }
             return false;
+
+        // Trademark - ™
+        case RV_TM:
+            if (is_unicode(mode)) {
+                send_unicode_string("™");
+            }
+        return false;
+
+        // Registered trademark - ®
+        case RV_RT:
+            if (is_unicode(mode)) {
+                send_unicode_string("®");
+            }
+        return false;
+
+        // Copyright - ©
+        case RV_CC:
+            if (is_unicode(mode)) {
+                send_unicode_string("©");
+            }
+        return false;
+
+        // Degree - °
+        case RV_DEG:
+            if (is_unicode(mode)) {
+                send_unicode_string("°");
+            }
+        return false;
     }
 
     return process_record_keymap(keycode, record);
--- users/rverst/rverst.h
+++ users/rverst/rverst.h
@@ -6,7 +6,6 @@
 enum userspace_mode {
     MAC = 0,
     WINDOWS,
-    LINUX,
     MAC_UNI,
     WINDOWS_UNI,
     LINUX_UNI,
@@ -16,22 +15,24 @@ enum userspace_keycodes {
     RV_NON = SAFE_RANGE,
     RV_SM0,     // set MAC
     RV_SM1,     // set WINDOWS
-    RV_SM2,     // set LINUX
-    RV_SM3,     // set MAC_UNI
-    RV_SM4,     // set WINDOWS_UNI
-    RV_SM5,     // set LINUX_UNI
+    RV_SM2,     // set MAC_UNI
+    RV_SM3,     // set WINDOWS_UNI
+    RV_SM4,     // set LINUX_UNI
     RV_SM0S,    // set MAC and save
     RV_SM1S,    // set WINDOWS and save
-    RV_SM2S,    // set LINUX and save
-    RV_SM3S,    // set MAC_UNI and save
-    RV_SM4S,    // set WINDOWS_UNI and save
-    RV_SM5S,    // set LINUX_UNI and save
+    RV_SM2S,    // set MAC_UNI and save
+    RV_SM3S,    // set WINDOWS_UNI and save
+    RV_SM4S,    // set LINUX_UNI and save
     RV_AUML,    // äÄ
     RV_OUML,    // öÖ
     RV_UUML,    // üÜ
     RV_EUR,     // €
     RV_SZ,      // ß
-
+    RV_TM,      // ™
+    RV_RT,      // ®
+    RV_CC,      // ©
+    RV_DEG,     // °
+    RV_SNAP,    // Screenshot
 };
 
 typedef union {
@@ -48,6 +49,7 @@ extern userspace_config_t userspace_config;
 int get_mode(void);
 void set_mode(int mode, bool save);
 void switch_mode(int mode);
+bool is_unicode(int mode);
 
 void keyboard_pre_init_keymap(void);
 void keyboard_post_init_keymap(void);
