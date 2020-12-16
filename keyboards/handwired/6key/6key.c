93c5461b147aef0ad35e54df71f64acc0fbd0af2
jyliang2 <jyliang2@illinois.edu>
2020-12-16 03:29:21 -0600

    Add 6key to keyboards/handwired
    * Add keyboards/handwired/6key folder
    * Add 6key.c
    * Add 6key.h
    * Add config.h
    * Add keymaps/default/keymap.c
    * Add readme.md
    * Add rules.mk

---
+++ keyboards/handwired/6key/6key.c
@@ -0,0 +1,5 @@
+#include "6key.h"
+
+void matrix_init_kb(void) {
+    matrix_init_user();
+}
---
+++ keyboards/handwired/6key/6key.h
@@ -0,0 +1,15 @@
+#ifndef SIXKEY_H
+#define SIXKEY_H
+
+#include "quantum.h"
+
+#define LAYOUT( \
+      k00, k01, k02,    \
+      k03, k04, k05     \
+) \
+{ \
+    { k00, k01, k02 },  \
+    { k03, k04, k05 }   \
+} 
+
+#endif
