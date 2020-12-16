dff5f2db1da7edaab41995fe06834d2e118f3265
fauxpark <fauxpark@gmail.com>
2020-12-16 14:49:01 +1100

    Remove comments about custom ldscript for Teensy LC

--- keyboards/clueboard/66_hotswap/gen1/rules.mk
+++ keyboards/clueboard/66_hotswap/gen1/rules.mk
@@ -9,7 +9,6 @@ LED_MATRIX_DRIVER = IS31FL3731
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
-## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug
