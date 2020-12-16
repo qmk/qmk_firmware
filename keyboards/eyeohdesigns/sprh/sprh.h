8a818504c6ecd75df660d7d15ff5555e2c69dad4
joedinkle <brandon.bonton@gmail.com>
2020-12-16 10:08:47 -0500

    Edits to default Layout

--- keyboards/eyeohdesigns/sprh/sprh.h
+++ keyboards/eyeohdesigns/sprh/sprh.h
@@ -25,19 +25,19 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT( \
+#define LAYOUT_default( \
         k43, k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, \
         k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, \
         k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c,      \
         k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,      \
-        k40, k41, k42, k44, k45, k46, k47, k48, k49, k4a, k4b                \
+        k40, k41, k42, k47, k48, k49, k4b, k4c, k4d                \
 ) \
 { \
     {k00, k01, k02, k03, k04,   k05, k06,   k07, k08, k09, k0a, k0b, k0c,   k0d},   \
     {k10, k11, k12, k13, k14,   k15, k16,   k17, k18, k19, k1a, k1b, k1c,   k1d},   \
     {k20, k21, k22, k23, k24,   k25, k26,   k27, k28, k29, k2a, k2b, k2c,   KC_NO}, \
-    {k30, k31, k32, k33, k34,   k35, k36,   k37, k38, k39, k3a, k3b, KC_NO, k3c},   \
-    {k40, k41, k42, k43, KC_NO, k44, KC_NO, k45, k46, k47, k48, k49, k4a,   k4b},   \
+    {k30, k31, k32, k33, k34,   k35, k36,   k37, k38, k39, k3a, k3b, k3c},   \
+    {k40, k41, k42, k43, KC_NO,   KC_NO, KC_NO,   k47, k48, k49, KC_NO, k4b, k4c, k4d},   \
 }
 #define ProtoTest( \
         k43, k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k2d,\
