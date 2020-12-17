5f5857aa28ac62eda06524157e5d76e3862f96c8
joedinkle <brandon.bonton@gmail.com>
2020-12-17 10:49:17 -0500

    Formatting changes and file removal for QMK PR

--- keyboards/eyeohdesigns/s50/keymaps/ansi_right_num/keymap.c
+++ keyboards/eyeohdesigns/s50/keymaps/ansi_right_num/keymap.c
@@ -35,13 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *       CTL-OS| Alt |Raise|            Space /NUMBRS    | Raise |Ctrl|Del |    0    |
      *      `----------------------------------------------------------------------------'
      */ 
-                        KC_ESC,       KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,   KC_O,   KC_P,    KC_7, KC_8, KC_9, \
-KC_MUTE,                                                                                                                   \
-                    KC_TAB,           KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L,   KC_SCLN, KC_4, KC_5, KC_6, \
-                                                                                                                           \
-                MT(MOD_LSFT, KC_CAPS),KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT, KC_SLSH, KC_1, KC_2, KC_3, \
-                                                                                                                           \
-               KC_LGUI,  KC_LALT,KC_LCTL,      MO(RAISE),           LT(NUMBRS,KC_SPC),           KC_RCTL,KC_BSPC,  KC_0    \
+                        KC_ESC,       KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,   KC_O,   KC_P,    KC_7, KC_8, KC_9,
+KC_MUTE,
+                    KC_TAB,           KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L,   KC_SCLN, KC_4, KC_5, KC_6,
+                MT(MOD_LSFT, KC_CAPS),KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT, KC_SLSH, KC_1, KC_2, KC_3,
+               KC_LGUI,  KC_LALT,KC_LCTL,      MO(RAISE),           LT(NUMBRS,KC_SPC),           KC_RCTL,KC_BSPC,  KC_0
 ),
 
 [RAISE] = LAYOUT_ansi_right_num(
@@ -56,13 +54,11 @@ KC_MUTE,                                                                        
      *      | OS  | Ctrl| Alt |                             |       | Ctrl| Del| BL STEP |
      *      `----------------------------------------------------------------------------'
      */
-                      KC_GRAVE,KC_TRNS,KC_TRNS,KC_PGUP, KC_TRNS,  KC_LBRC,KC_RBRC,KC_TRNS,KC_UP,KC_TRNS,KC_PSCR,KC_TRNS,KC_TRNS,KC_TRNS,\
-RESET,                                                                                                                          \
-                   KC_TRNS,KC_TRNS,KC_HOME,KC_PGDN,KC_END,KC_MINS,KC_EQL, KC_LEFT,KC_DOWN,KC_RIGHT,KC_QUOT,KC_ENT,KC_ASON,KC_ASUP,\
-                                                                                                                                \
-                KC_TRNS,      KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,KC_BSLS,  KC_TRNS,KC_ASOFF, KC_ASDN,\
-                                                                                                                                \
-       KC_LGUI,  KC_LALT,   KC_LCTL,                      KC_TRNS,                     KC_TRNS,  KC_RCTL, KC_DEL,      BL_STEP    \
+                      KC_GRAVE,KC_TRNS,KC_TRNS,KC_PGUP, KC_TRNS,  KC_LBRC,KC_RBRC,KC_TRNS,KC_UP,KC_TRNS,KC_PSCR,KC_TRNS,KC_TRNS,KC_TRNS,
+RESET,
+                   KC_TRNS,KC_TRNS,KC_HOME,KC_PGDN,KC_END,KC_MINS,KC_EQL, KC_LEFT,KC_DOWN,KC_RIGHT,KC_QUOT,KC_ENT,KC_ASON,KC_ASUP,
+                KC_TRNS,      KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,KC_BSLS,  KC_TRNS,KC_ASOFF, KC_ASDN,
+       KC_LGUI,  KC_LALT,   KC_LCTL,                      KC_TRNS,                     KC_TRNS,  KC_RCTL, KC_DEL,      BL_STEP
 ),
 [NUMBRS] = LAYOUT_ansi_right_num(
     /* Layer 2
@@ -76,13 +72,11 @@ RESET,                                                                          
      *      | OS  | Ctrl| Alt |                             |       | Ctrl|    |    |    |
      *      `----------------------------------------------------------------------------'
      */  
-                      KC_F1,    KC_F2,   KC_F3, KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,KC_TRNS,KC_TRNS,\
-RESET,                                                                                                                                     \
-                     KC_TAB,    KC_1,    KC_2 , KC_3 ,   KC_4 ,  KC_5 ,   KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 ,  KC_TRNS,KC_TRNS,KC_TRNS,\
-                                                                                                                                           \
-                   KC_TRNS,      KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,KC_TRNS,KC_TRNS,KC_TRNS,\
-                                                                                                                                           \
-                KC_LGUI,  KC_LALT,   KC_LCTL,                      KC_TRNS,                     KC_TRNS,  KC_RCTL,      KC_TRNS,    KC_TRNS    \
+                      KC_F1,    KC_F2,   KC_F3, KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,KC_TRNS,KC_TRNS,
+RESET,
+                     KC_TAB,    KC_1,    KC_2 , KC_3 ,   KC_4 ,  KC_5 ,   KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 ,  KC_TRNS,KC_TRNS,KC_TRNS,
+                   KC_TRNS,      KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,KC_TRNS,KC_TRNS,KC_TRNS,
+                KC_LGUI,  KC_LALT,   KC_LCTL,                      KC_TRNS,                     KC_TRNS,  KC_RCTL,      KC_TRNS,    KC_TRNS
 )
 };
 
