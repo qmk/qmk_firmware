6da4cedfa9f5d04ad9a11a4e24601e81e082bda6
Peter Peterson <peter.peterson.iv@gmail.com>
2020-12-17 07:57:30 -0700

    Update keyboards/preonic/keymaps/ipetepete/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/preonic/keymaps/ipetepete/keymap.c
+++ keyboards/preonic/keymaps/ipetepete/keymap.c
@@ -176,11 +176,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
   switch(biton32(layer_state)){
     case _LOWER:
       if(clockwise){
-        register_code(KC_MNXT);
-        while(timer_elapsed(held_keycode_timer) < MEDIA_KEY_DELAY){
-          // noop
-        }
-        unregister_code(KC_MNXT);
+        tap_code(KC_MNXT);
       }else{
         register_code(KC_MPRV);
         while(timer_elapsed(held_keycode_timer) < MEDIA_KEY_DELAY){
