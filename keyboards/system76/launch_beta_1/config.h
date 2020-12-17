72fecd659dd180039d4cbb254f9bee75dece7220
Jeremy Soller <jeremy@system76.com>
2020-12-17 15:17:35 -0700

    Set default hue

--- keyboards/system76/launch_beta_1/config.h
+++ keyboards/system76/launch_beta_1/config.h
@@ -49,7 +49,7 @@
     #define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
     #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 176 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
     #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_RAINBOW_MOVING_CHEVRON // Sets the default mode, if none has been set
-    #define RGB_MATRIX_STARTUP_HUE 0
+    #define RGB_MATRIX_STARTUP_HUE 142 // 200 degrees
     #define RGB_MATRIX_STARTUP_SAT 255
     #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
     #define RGB_MATRIX_DISABLE_KEYCODES // disables control of rgb matrix by keycodes (must use code functions to control the feature)
