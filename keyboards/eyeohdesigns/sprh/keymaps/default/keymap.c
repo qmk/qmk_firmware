7a5d97b896fbfc0808765fe8989ea33af0b37eb9
joedinkle <brandon.bonton@gmail.com>
2020-12-17 09:15:36 -0500

    Deleted obsolete code.

--- keyboards/eyeohdesigns/sprh/keymaps/default/keymap.c
+++ keyboards/eyeohdesigns/sprh/keymaps/default/keymap.c
@@ -18,9 +18,6 @@
 #define QWERTY 0
 #define FUNCTN 1
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes { QMKBEST = SAFE_RANGE, QMKURL };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT_default(/* Base */
         KC_MUTE, KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,
