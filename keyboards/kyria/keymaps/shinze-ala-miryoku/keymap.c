6dffc8003f3f6c1c2d7b8ed390e88c092091f2b1
Gatan <gaetan.ark@kosmos.fr>
2020-12-20 22:09:06 +0100

    Up

--- keyboards/kyria/keymaps/shinze-ala-miryoku/keymap.c
+++ keyboards/kyria/keymaps/shinze-ala-miryoku/keymap.c
@@ -26,7 +26,7 @@
 // Row 1
 // Left
 #define S00 KC_NO
-#define B BP_B
+#define S01 BP_B
 #define S02 BP_E_ACUTE
 #define S03 BP_P
 #define S04 BP_O
@@ -89,54 +89,54 @@
 // Row1
 // Left
 #define T00 KC_NO
-#define T01 RSF
+#define T01 RSFT
 #define T02 KC_NO
+#define T03 KC_NO
 #define T04 KC_NO
-#define T06 KC_NO
-#define T07 KC_NO
+#define T05 KC_NO
 
 // Right
-#define T08 U_RDO
-#define T09 U_PST
-#define T10 U_CPY
-#define T11 U_CUT
-#define T12 U_UND
-#define T13 KC_NO
+#define T06 U_RDO
+#define T07 U_PST
+#define T08 U_CPY
+#define T09 U_CUT
+#define T10 U_UND
+#define T11 KC_NO
 
 // Row 2
 // Left
-#define T14 KC_NO
-#define T15 KC_LCTL
-#define T16 KC_LALT
-#define T17 KC_LGUI
-#define T18 LSFT
-#define T19 KC_NO
+#define T12 KC_NO
+#define T13 KC_LCTL
+#define T14 KC_LALT
+#define T15 KC_LGUI
+#define T16 LSFT
+#define T17 KC_NO
 
 // Right
-#define T20 KC_LEFT
-#define T21 KC_DOWN
-#define T22 KC_UP
-#define T23 KC_RGHT
-#define T24 KC_CAPS
-#define T25 KC_NO
+#define T18 KC_LEFT
+#define T19 KC_DOWN
+#define T20 KC_UP
+#define T21 KC_RGHT
+#define T22 KC_CAPS
+#define T23 KC_NO
 
 // Row 3
 // Left
+#define T24 KC_NO
+#define T25 KC_LALT
 #define T26 KC_NO
-#define T27 KC_LALT
 #define T28 KC_NO
+#define T29 KC_NO
 #define T30 KC_NO
-#define T31 KC_NO      // Top thumb
-#define T32 KC_NO      // Top thumb
 // Right
-#define T33 KC_NO      // Top thumb
-#define T34 KC_NO      // Top thumb
-#define T35 KC_HOME
-#define T36 KC_PGDN
-#define T37 KC_PGUP
-#define T38 KC_END
-#define T39 KC_INS
-#define T40 KC_NO
+#define T31 KC_NO
+#define T32 KC_NO
+#define T33 KC_HOME
+#define T34 KC_PGDN
+#define T35 KC_PGUP
+#define T36 KC_END
+#define T37 KC_INS
+#define T38 KC_NO
 
 // Row 4
 #define C00 KC_NO
@@ -155,13 +155,20 @@ enum layers { BASE, MBO, MEDR, NAVR, MOUR, NSSL, NSL, FUNL };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-      S00, B, S02, S03, S04, S05,                         S06, S07, S08, S09, S10, S11,
+      S00, S01, S02, S03, S04, S05,                         S06, S07, S08, S09, S10, S11,
       S12, S13, S14, S15, S16, S17,                         S18, S19, S20, S21, S22, S23,
       S24, S25, S26, S27, S28, S29, S30, S31,     S32, S33, S34, S35, S36, S37, S38, S39,
                      B00, B01, B02, B03, B04,     B05, B06, B07, B08, B09
     ),
 
     [NAVR] = LAYOUT(
+      T00, T01, T02, T03, T04, T05,                         T06, T07, T08, T09, T10, T11,
+      T12, T13, T14, T15, T16, T17,                         T18, T19, T20, T21, T22, T23,
+      T24, T25, T26, T27, T28, T29, T30, T31,     T32, T33, T34, T35, T36, T37, T38, T39,
+                     C00, C01, C02, C03, C04,     C05, C06, C07, C08, C09
+    ),
+
+    [MOUR] = LAYOUT(
       S00, S01, S02, S03, S04, S05,                         S06, S07, S08, S09, S10, S11,
       S12, S13, S14, S15, S16, S17,                         S18, S19, S20, S21, S22, S23,
       S24, S25, S26, S27, S28, S29, S30, S31,     S32, S33, S34, S35, S36, S37, S38, S39,
@@ -176,13 +183,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [MEDR] = LAYOUT(
-      S00, S01, S02, S03, S04, S05,                         S06, S07, S08, S09, S10, S11,
-      S12, S13, S14, S15, S16, S17,                         S18, S19, S20, S21, S22, S23,
-      S24, S25, S26, S27, S28, S29, S30, S31,     S32, S33, S34, S35, S36, S37, S38, S39,
-                     B00, B01, B02, B03, B04,     B05, B06, B07, B08, B09
-    ),
-
-    [MOUR] = LAYOUT(
       S00, S01, S02, S03, S04, S05,                         S06, S07, S08, S09, S10, S11,
       S12, S13, S14, S15, S16, S17,                         S18, S19, S20, S21, S22, S23,
       S24, S25, S26, S27, S28, S29, S30, S31,     S32, S33, S34, S35, S36, S37, S38, S39,
