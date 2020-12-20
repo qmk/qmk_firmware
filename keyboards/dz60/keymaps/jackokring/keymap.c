b319b4adc05cbef4069cb97d57400135a5c0d46d
jackokring <jackokring@gmail.com>
2020-12-20 19:14:51 +0000

    basic navigation mode

--- keyboards/dz60/keymaps/jackokring/keymap.c
+++ keyboards/dz60/keymaps/jackokring/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	//Base layer 0
+	//ANSI lock mode 0 =================================================================================== ANSI lock mode 0
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -10,15 +10,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
 		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          KC_RCTL, MO(5)),
 
-	//Nav lock mode 1
+	//Navigation lock mode 1 ============================================================================= Navigation lock mode 1
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          KC_RCTL, MO(6)),
+		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,            KC_RSFT,
+		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_LEFT, KC_DOWN,          KC_RIGHT, MO(6)),
 
-	//Numpad lock mode 2
+	//Numpad lock mode 2 ================================================================================= Numpad lock mode 2
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
 		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          KC_RCTL, MO(7)),
 
-	//Macro lock mode 3
+	//Macro lock mode 3 ================================================================================== Macro lock mode 3
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
 		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          KC_RCTL, MO(8)),
 
-	//Function shift mode 4
+	//ANSI shift mode 4 ================================================================================== ANSI shift mode 4
 	LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
 		KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_INS,
@@ -42,7 +42,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS,          KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_PGUP, KC_PGDN, KC_UP,            KC_TRNS,
 		DF(1),   DF(3),            DF(2),                     DF(0),                              KC_LEFT, KC_DOWN,          KC_RIGHT, KC_TRNS),
 	
-	//Navigation shift mode 5
+	//Navigation shift mode 5 ============================================================================ Navigation shift mode 5
+	LAYOUT_60_ansi(
+		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
+		KC_TRNS,          KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_INS,
+		KC_SLCK,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PAUS, KC_PSCR, KC_ESC,
+		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_QUES,          KC_TRNS,
+		DF(1),   DF(3),            DF(2),                     DF(0),                              KC_HOME, KC_SLSH,          KC_END,  KC_TRNS),
+	
+	//Numpad shift mode 6 ================================================================================ Numpad shift mode 6
 	LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
 		KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_INS,
@@ -50,15 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS,          KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_PGUP, KC_PGDN, KC_UP,            KC_TRNS,
 		DF(1),   DF(3),            DF(2),                     DF(0),                              KC_LEFT, KC_DOWN,          KC_RIGHT, KC_TRNS),
 	
-	//Numpad shift mode 6
-	LAYOUT_60_ansi(
-		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-		KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_INS,
-		KC_SLCK,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PAUS, KC_PSCR, KC_TRNS,
-		KC_TRNS,          KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_PGUP, KC_PGDN, KC_UP,            KC_TRNS,
-		DF(1),   DF(3),            DF(2),                     DF(0),                              KC_LEFT, KC_DOWN,          KC_RIGHT, KC_TRNS),
-	
-	//Macro shift mode 7
+	//Macro shift mode 7 ================================================================================= Macro shift mode 7
 	LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
 		KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_INS,
--- keyboards/dz60/keymaps/jackokring/readme.md
+++ keyboards/dz60/keymaps/jackokring/readme.md
@@ -13,9 +13,9 @@ block of keys. The function key was exchanged with the right control key to get 
 ## ANSI Layer
 * Right control exchanged with function shift key.
 
-## Keyboard Control Layer (Fn ANSI)
+## ANSI Shift Layer (Fn ANSI)
 * Scroll lock placed on caps lock.
-* Right alt/win/control/slash form cusor block
+* Right alt/win/control/slash form cusor block.
 * Backslash is insert.
 * Backspace is delete.
 * F1 to F12 form top row numbers and minus/equals.
@@ -25,4 +25,56 @@ block of keys. The function key was exchanged with the right control key to get 
 * Apostrophy is print screen.
 * RGB controls are Q-I.
 * Backlight controls are C-N.
+* Escape is backtick.
  
+## Navigation Layer
+* Right control exchanged with function shift key.
+* Right alt/win/control/slash form cusor block.
+
+## Navigation Shift Layer (Fn Navigation)
+* Scroll lock placed on caps lock.
+* Right alt/win/control/slash form cusor block.
+* Backslash is insert.
+* Backspace is delete.
+* F1 to F12 form top row numbers and minus/equals.
+* F13 to F24 form second row Q-].
+* Right alt/control are home and end.
+* Comma and period are page up/down.
+* Semicolon is pause.
+* Apostrophy is print screen.
+* Escape is backtick.
+* Enter is pure escape for easy shift escape typing.
+
+## Macroboard Layer
+* Right control exchanged with function shift key.
+
+## Macroboard Shift Layer (Fn Macroboard)
+* Scroll lock placed on caps lock.
+* Right alt/win/control/slash form cusor block.
+* Backslash is insert.
+* Backspace is delete.
+* F1 to F12 form top row numbers and minus/equals.
+* Square bracket are home and end.
+* Comma and period are page up/down.
+* Semicolon is pause.
+* Apostrophy is print screen.
+* RGB controls are Q-I.
+* Backlight controls are C-N.
+* Escape is backtick.
+
+## Numpad Layer
+* Right control exchanged with function shift key.
+
+## Numpad Shift Layer (Fn Numpad)
+* Scroll lock placed on caps lock.
+* Right alt/win/control/slash form cusor block.
+* Backslash is insert.
+* Backspace is delete.
+* F1 to F12 form top row numbers and minus/equals.
+* Square bracket are home and end.
+* Comma and period are page up/down.
+* Semicolon is pause.
+* Apostrophy is print screen.
+* RGB controls are Q-I.
+* Backlight controls are C-N.
+* Escape is backtick.
