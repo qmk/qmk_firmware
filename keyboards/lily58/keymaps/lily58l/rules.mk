7c75fa503b88785e80d9081e4f12fc6c73bca76f
Ben <BenRoe@users.noreply.github.com>
2020-12-20 13:49:54 +0100

    Update keyboards/lily58/keymaps/lily58l/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/lily58l/rules.mk
+++ keyboards/lily58/keymaps/lily58l/rules.mk
@@ -1,6 +1,1 @@
-OLED_DRIVER_ENABLE = yes   	# Enables the use of OLED displays
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-
+EXTRAKEY_ENABLE = yes
