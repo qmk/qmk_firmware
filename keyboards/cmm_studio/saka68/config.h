9f56ad7ff23a498f87f16e39eeb27ffef38f051c
CMMS-Freather <72902384+CMMS-Freather@users.noreply.github.com>
2020-12-21 03:26:17 -0800

    Update keyboards/cmm_studio/saka68/config.h
    tested and does work now. deleting these lines
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/cmm_studio/saka68/config.h
+++ keyboards/cmm_studio/saka68/config.h
@@ -46,16 +46,6 @@
 
 #define DEBOUNCE 5
 
-// EEPROM usage
-
-// TODO: refactor with new user EEPROM code (coming soon)
-#define EEPROM_MAGIC 0x451F
-#define EEPROM_MAGIC_ADDR 32
-// Bump this every time we change what we store
-// This will automatically reset the EEPROM with defaults
-// and avoid loading invalid data from the EEPROM
-#define EEPROM_VERSION 0x08
-#define EEPROM_VERSION_ADDR 34
 
 // Dynamic keymap starts after EEPROM version
 #define DYNAMIC_KEYMAP_EEPROM_ADDR 35
