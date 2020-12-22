db4e14d47f45ea9dba08e80630d0a4c3637aba04
Moritz <moritz.plattner@gmx.net>
2020-12-23 00:19:30 +0100

    Update keyboards/ebastler/isometria_75/rev1/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ebastler/isometria_75/rev1/config.h
+++ keyboards/ebastler/isometria_75/rev1/config.h
@@ -24,8 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x1337
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    ebastler
-#define PRODUCT         ebastler isometria 75
-#define DESCRIPTION     ebastler isometria 75
+#define PRODUCT        isometria 75
 
 /* key matrix size */
 #define MATRIX_ROWS 6
