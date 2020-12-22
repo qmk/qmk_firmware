217b85de189e34e07e94223b5258e1d81ea88f50
jackokring <jackokring@gmail.com>
2020-12-22 15:20:25 +0000

    more docs

--- keyboards/dz60/keymaps/jackokring/keymap.c
+++ keyboards/dz60/keymaps/jackokring/keymap.c
@@ -11,6 +11,7 @@ make bootloader install via: (or use via if it's firmwared) => CHOICE
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	//ANSI lock mode 0 =================================================================================== ANSI lock mode 0
+	// Standard ANSI 60 layout for worldwide ASCII compatibility,
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -19,6 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_APP,           KC_RCTL, MO(4)),//zero index start
 
 	//Navigation lock mode 1 ============================================================================= Navigation lock mode 1
+	// Removed ASCII 47 (/) and right ctrl/win/alt for cursor.
+	// VirtualBox host key needs an Fn.
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -27,22 +30,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_LEFT, KC_DOWN,          KC_RIGHT, MO(5)),
 
 	//Numpad lock mode 2 ================================================================================= Numpad lock mode 2
+	// A single finger (locking modifiers) number pad, cursor and right hand F1-F24.
 	LAYOUT_60_ansi(
 		KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P7,   KC_P8,   KC_P9,   KC_DEL,  KC_PMNS, KC_PPLS,          KC_BSPC,
 		KC_TAB,           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_LPRN, KC_RPRN, KC_PSLS,
 		KC_NLCK,          KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_P1,   KC_P2,   KC_P3,   KC_INS,  KC_CALC, KC_PENT,
 		OSM(MOD_LSFT),    KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_P0,   KC_COMM, KC_PDOT, KC_UP,            MOD_RSFT,
-		OSM(MOD_LCTL),OSM(MOD_LGUI),        OSM(MOD_LALT),                  KC_SPC,                             KC_LEFT, KC_DOWN,          KC_RIGHT, TG(6)),//lock Fn
+		OSM(MOD_LCTL),OSM(MOD_LGUI),OSM(MOD_LALT),            KC_SPC,                             KC_LEFT, KC_DOWN,          KC_RIGHT, TG(6)),//lock Fn
 
 	//Macro lock mode 3 ================================================================================== Macro lock mode 3
+	// A single finger macro launching board producing many modifier and function keys.
+	// Can record one macro of 128 steps (backspace start/stop) and play it (space).
 	LAYOUT_60_ansi(
 		OSM(MOD_LALT|MOD_LSFT),    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           DM_REC1,
 		OSM(MOD_LCTL|MOD_LALT),    KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  ALGR(KC_F24),
 		OSM(MOD_LCTL|MOD_LSFT),    ALGR(KC_F1),   ALGR(KC_F2),   ALGR(KC_F3),   ALGR(KC_F4),   ALGR(KC_F5),   ALGR(KC_F6),   ALGR(KC_F7),   ALGR(KC_F8),   ALGR(KC_F9),   ALGR(KC_F10),  ALGR(KC_F11),  ALGR(KC_F12),
 		OSM(MOD_LSFT),         	   ALGR(KC_F13),  ALGR(KC_F14),  ALGR(KC_F15),  ALGR(KC_F16),  ALGR(KC_F17),  ALGR(KC_F18),  ALGR(KC_F19),  ALGR(KC_F20),  ALGR(KC_F21),  ALGR(KC_F22),  ALGR(KC_F23),
-		OSM(MOD_LCTL),OSM(MOD_LGUI),        OSM(MOD_LALT),                  DM_PLY1,                            OSM(MOD_RALT),OSM(MOD_RGUI),         OSM(MOD_RCTL), TG(7)),
+		OSM(MOD_LCTL),OSM(MOD_LGUI),OSM(MOD_LALT),            DM_PLY1,                            OSM(MOD_RALT),OSM(MOD_RGUI),OSM(MOD_RCTL), TG(7)),
 
 	//ANSI shift mode 4 ================================================================================== ANSI shift mode 4
+	// ISO characters such as £ and control of RGB/backlight and cursor.
+	// Fn + P is power switch for shutdown.
 	LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
 		KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_WHOM, KC_PWR,  KC_HOME, KC_END,  KC_INS,
@@ -51,6 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, DF(3),            DF(1),                     DF(2),                              KC_LEFT, KC_DOWN,          KC_RIGHT, KC_TRNS),
 	
 	//Navigation shift mode 5 ============================================================================ Navigation shift mode 5
+	// All function keys, so Home and End were sacrificed.
+	// Also media player launch and control.
+	// No APP menu as / is on the key, as / is ? key.
 	LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
 		KC_TRNS,          KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_INS,
@@ -59,6 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		DF(0),   DF(3),            KC_LALT,                   DF(2),                              KC_RALT, KC_SLSH,          KC_RCTL, KC_TRNS),
 	
 	//Numpad shift mode 6 ================================================================================ Numpad shift mode 6
+	// ANSI text entry with no left ctrl/win/alt.
 	LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -67,6 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		DF(0),   DF(3),            DF(1),                     KC_SPC,                             KC_RALT, KC_APP,           KC_RCTL, KC_TRNS),
 	
 	//Macro shift mode 7 ================================================================================= Macro shift mode 7
+	// Same macro layer but different modifier grouping for more hot key combinations.
 	LAYOUT_60_ansi(
 		OSM(MOD_LGUI|MOD_LALT|MOD_LSFT),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          OSM(MOD_LCTL|MOD_LALT|MOD_LSFT),
 		OSM(MOD_LGUI|MOD_LCTL|MOD_LALT),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
