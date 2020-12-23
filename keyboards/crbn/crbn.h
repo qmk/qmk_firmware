531004605e09678685dafffa43b2ac3e56fcb57a
ReFil <31960031+ReFil@users.noreply.github.com>
2020-12-23 23:12:26 +0000

    Configurator support

--- keyboards/crbn/crbn.h
+++ keyboards/crbn/crbn.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Harry Herring 
+/* Copyright 2020 Harry Herring
 	*
 	* This program is free software: you can redistribute it and/or modify
 	* it under the terms of the GNU General Public License as published by
@@ -17,14 +17,26 @@
 
 #include "quantum.h"
 
-#define LAYOUT( \
+#define LAYOUT_crbn_1x2u( \
 		K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
 		K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
 		K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, \
-		K300, K301, K302, K303, K304, K305,       K307, K308, K309, K310, K311  \
+		K300, K301, K302, K303, K304,    K305,    K307, K308, K309, K310, K311  \
 ) { \
 		{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011 }, \
 		{ K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111 }, \
 		{ K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211 }, \
-		{ K300,  K301,  K302,  K303,  K304,  K305,  KC_NO, K307,  K308,  K309,  K310,  K311 }  \
+		{ K300,  K301,  K302,  K303,  K304,  K305, KC_NO,  K307,  K308,  K309,  K310,  K311 }  \
+}
+
+#define LAYOUT_crbn_2x2u( \
+		K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
+		K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
+		K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, \
+		K300, K301, K302, K303,    K304,       K307,    K308, K309, K310, K311  \
+) { \
+		{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011 }, \
+		{ K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111 }, \
+		{ K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211 }, \
+		{ K300,  K301,  K302,  K303,  K304, KC_NO, KC_NO,  K307,  K308,  K309,  K310,  K311 }  \
 }
