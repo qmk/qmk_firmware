8c4f92c86e5e863ae4c3be33fefaeefaabb37f50
swampmonster <sb@kriegsmarine.se>
2020-12-23 12:28:09 +0100

    Update keyboards/kyria/kyria.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kyria/kyria.h
+++ keyboards/kyria/kyria.h
@@ -64,7 +64,7 @@
     { R45,   R46,   R47,   R48,   R49,   KC_NO, KC_NO, KC_NO }, \
 }
 
-#define LAYOUT_2x2u( \
+#define LAYOUT_split_3x6_5( \
     L00, L01, L02, L03, L04, L05,                     R06, R07, R08, R09, R10, R11, \
     L12, L13, L14, L15, L16, L17,                     R18, R19, R20, R21, R22, R23, \
     L24, L25, L26, L27, L28, L29,                     R34, R35, R36, R37, R38, R39, \
