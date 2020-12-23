08752ba5f1738371bb7ae949af76fe5bb36d7427
kiwikey <75843996+kiwikey@users.noreply.github.com>
2020-12-23 08:31:51 +0800

    Update keyboards/kiwikey/kawii9/kawii9.h
    Change name of layout: LAYOUT_ortho_3x3
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kiwikey/kawii9/kawii9.h
+++ keyboards/kiwikey/kawii9/kawii9.h
@@ -26,7 +26,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT_3x3( \
+#define LAYOUT_ortho_3x3( \
     k00, k01, k02, \
     k10, k11, k12, \
     k20, k21, k22  \
