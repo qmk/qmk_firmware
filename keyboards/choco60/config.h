b8fde08d2a315fe56212aa04aea130ce3e96c37f
Naoto Takai <takai@recompile.net>
2020-12-23 15:16:18 +0900

    Remove DESCRIPTION
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/choco60/config.h
+++ keyboards/choco60/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    recompile keys
 #define PRODUCT         Choco60
-#define DESCRIPTION     recompile keys Choco60
 
 /* key matrix size */
 #define MATRIX_ROWS 10
