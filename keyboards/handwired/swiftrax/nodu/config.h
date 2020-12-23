45f06231a5b5391fad5797f2b5cdf892ca3ab4ef
Swiftrax <swiftrax@gmail.com>
2020-12-22 18:58:37 -0800

    limit via to 3 layers

--- keyboards/handwired/swiftrax/nodu/config.h
+++ keyboards/handwired/swiftrax/nodu/config.h
@@ -42,3 +42,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
+
+/*EEPROM for via*/
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
