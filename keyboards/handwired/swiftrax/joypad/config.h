b4956f6819ce5cdf5bddb417ec6c6a212d2c13f1
Swiftrax <swiftrax@gmail.com>
2020-12-22 19:18:20 -0800

    add joypad keymaps

--- keyboards/handwired/swiftrax/joypad/config.h
+++ keyboards/handwired/swiftrax/joypad/config.h
@@ -24,31 +24,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0xEB0C
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Swiftrax
-#define PRODUCT         Retropad
+#define PRODUCT         Joypad
 
 /* key matrix size */
-#define MATRIX_ROWS 3
-#define MATRIX_COLS 2
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 4
 
 // ROWS: Top to bottom, COLS: Left to right
 
-#define MATRIX_ROW_PINS { C7, C6, B5 }
-#define MATRIX_COL_PINS { B4, D2 }
+#define MATRIX_ROW_PINS { C6, B3, B0, B1, D6, D5 }
+#define MATRIX_COL_PINS { C7, B4, D0, C2 }
 
-#define ENCODERS_PAD_A { D0 }
-#define ENCODERS_PAD_B { D1 }
+#define ENCODERS_PAD_A { C5 }
+#define ENCODERS_PAD_B { C4 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-#define RGB_DI_PIN B7
-#ifdef  RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 6
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
@@ -58,7 +50,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /*EEPROM for via*/
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
-
-/* bootmagic row col assignment */
-#define BOOTMAGIC_LITE_ROW 1
-#define BOOTMAGIC_LITE_COLUMN 0
--- keyboards/handwired/swiftrax/joypad/keymaps/via/keymap.c
+++ keyboards/handwired/swiftrax/joypad/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-  /* Copyright 2020 swiftrax
+/* Copyright 2020 swiftrax
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,52 +18,33 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT(
-  TO(1), //windows
-  KC_MSTP, KC_MPLY,
-  KC_MPRV, KC_MNXT),
+  KC_MUTE, KC_F13 , KC_F14 , KC_F15 ,
+  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+  KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
+  KC_P4  , KC_P5  , KC_P6  , KC_PPLS,
+  KC_P1  , KC_P2  , KC_P3  , KC_PENT,
+  KC_P0  , KC_P0  , KC_PDOT, KC_PENT),
 
-[1] = LAYOUT( //macOS
-  TO(2),
-  KC_MSTP, KC_MPLY,
-  KC_MRWD, KC_MFFD),
+[1] = LAYOUT(
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
 [2] = LAYOUT(
-  TO(0),
-  KC_HOME, KC_PGUP,
-  KC_END,  KC_PGDN),
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if(IS_LAYER_ON(2)){
-    if (clockwise)
-      tap_code(KC_LEFT);
-    else 
-      tap_code(KC_RGHT);
-  }
-  else{
-    if(clockwise)
-      tap_code(KC_VOLU);
-    else
-      tap_code(KC_VOLD);
-  }
-}
-
-void matrix_init_user(void) {
-  // set top LED to output and off (active low)
-  setPinOutput(D5);
-  writePinHigh(D5);
-  // set middle LED to output and off (active low)
-  setPinOutput(D4);
-  writePinHigh(D4);
-  // set bottom LED to output and off (active low)
-  setPinOutput(D3);
-  writePinHigh(D3);
-}
-
-// write to above indicators in a binary fashion based on current layer
-layer_state_t layer_state_set_user(layer_state_t state) {
-    writePin(D5, get_highest_layer(state));
-    writePin(D4, !layer_state_cmp(state, 1));
-    writePin(D3, !layer_state_cmp(state, 2));
-    return state;
+  if(clockwise)
+    tap_code(KC_VOLU);
+  else
+    tap_code(KC_VOLD);
 }
