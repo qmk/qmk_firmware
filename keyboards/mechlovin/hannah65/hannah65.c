343b72a24063f533ab4857d39fe3b3b63c88be7f
vuhopkep <boy9x0@gmail.com>
2020-12-24 14:19:06 +0700

    Update hannah65.c

--- keyboards/mechlovin/hannah65/hannah65.c
+++ keyboards/mechlovin/hannah65/hannah65.c
@@ -28,7 +28,7 @@ void led_init_ports(void) {
 
 bool led_update_kb(led_t led_state) {
     if(led_update_user(led_state)) {
-        writePin(B9, led_state.caps_lock);
+        writePin(B9, !led_state.caps_lock);
     }
 
     return true;
