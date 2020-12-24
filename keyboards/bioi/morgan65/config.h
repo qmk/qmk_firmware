a5df7766001357a85f768cbd821f4b7a09be16bc
scottywei <scot.wei@gmail.com>
2020-12-24 17:54:23 +0800

    Update config.h

--- keyboards/bioi/morgan65/config.h
+++ keyboards/bioi/morgan65/config.h
@@ -21,7 +21,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 
-#define VENDOR_ID       0x4249    // "BI" = Basic I/O Instruments
+#define VENDOR_ID       0x8101  // 8101 = "BIOI"
 #define PRODUCT_ID      0x6581
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Basic IO Instruments
