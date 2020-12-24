d867bc62568bd7e3ad42287879c970c7b443d88f
vuhopkep <boy9x0@gmail.com>
2020-12-24 12:46:59 +0700

    Update config.h

--- keyboards/mechlovin/hannah65/standard_led/config.h
+++ keyboards/mechlovin/hannah65/standard_led/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BACKLIGHT_PIN B8
 #define BACKLIGHT_BREATHING
-#define BACKLIGHT_PWM_DRIVER PWMD3
+#define BACKLIGHT_PWM_DRIVER PWMD4
 #define BACKLIGHT_PWM_CHANNEL 3
 
 #define BACKLIGHT_LEVELS 3
