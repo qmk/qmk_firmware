a0665b1f5900ba60ebb58b19fc33f832cf434d5c
Gondolindrim <alvaro.volpato@usp.br>
2020-12-25 20:38:27 -0300

    Edit copyright info, add VIA initial support

--- keyboards/evolv/evolv.c
+++ keyboards/evolv/evolv.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Álvaro "Gondolindrim" Volpato  <gondolindrim@acheronproject.com>
+ Copyright 2020 Álvaro "Gondolindrim" Volpato <alvaro.volpato@usp.br>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
