ac6eff015f72387bc2a34e42a107cc83ca8eecea
Barabas Raffai <barabas.raffai@gmail.com>
2020-12-25 12:28:05 +0000

    Remove unneeded code, remove F-keys from raise layer

--- keyboards/lily58/keymaps/barabas/keymap.c
+++ keyboards/lily58/keymaps/barabas/keymap.c
@@ -16,14 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-#ifdef PROTOCOL_LUFA
-#    include "lufa.h"
-#    include "split_util.h"
-#endif
-#ifdef SSD1306OLED
-#    include "ssd1306.h"
-#endif
-
 extern uint8_t is_master;
 
 enum layer_number {
@@ -108,8 +100,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, TG(_GAME),
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                                _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
 
