e277235924e337f4247b4b1d7fdd81b96d667a5a
ec965 <enoch965@gmail.com>
2020-12-25 00:29:12 -0800

    add license

--- keyboards/yd60mq/keymaps/64key/keymap.c
+++ keyboards/yd60mq/keymaps/64key/keymap.c
@@ -1,3 +1,19 @@
+/* Copyright 2020
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -13,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT_all(
         RESET,   KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, KC_DEL,
         _______, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, KC_PSCR, KC_SLCK, KC_PAUS,  KC_GRV,
-        _______, BL_TOGG, BL_DEC,   BL_INC, BL_BRTG, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
+        _______, BL_TOGG, BL_DEC,   BL_INC, BL_BRTG, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, MO(2),   _______, KC_PGUP, _______,
         _______, _______, _______,                   _______, _______, _______,                   _______, _______,  KC_END, KC_PGDN, KC_HOME
     ),
@@ -22,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_WAKE, _______, _______, _______, _______, _______, _______, _______,  KC_PWR, _______, _______, _______,
         _______, _______, KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, _______, _______ 
+        _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, _______, _______
     ),
 
 };
