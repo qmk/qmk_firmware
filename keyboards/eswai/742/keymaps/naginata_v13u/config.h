c1a5a754131bff60650da76f4362f0133ecf8569
eswai <sakamoto.yuki@gmail.com>
2020-12-26 15:15:38 +0900

    薙刀式v13 OS動的切替版

---
+++ keyboards/eswai/742/keymaps/naginata_v13u/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// 薙刀式
+// #define NAGINATA_TATEGAKI
+#define NAGINATA_YOKOGAKI
+#define UNICODE_SELECTED_MODES UC_MAC, UC_LNX, UC_WINC
+#define NAGINATA_KOUCHI_SHIFT // シフトを後置でも有効にする
+// 薙刀式
+
+#define OLED_TIMEOUT 60000
