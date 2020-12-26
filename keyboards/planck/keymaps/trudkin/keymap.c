4f6dd8e32c985f6d3e119ee89e3702424dfd9368
trudkin <trudkin@gmail.com>
2020-12-25 18:10:22 -0700

    Update keyboards/planck/keymaps/trudkin/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/planck/keymaps/trudkin/keymap.c
+++ keyboards/planck/keymaps/trudkin/keymap.c
@@ -295,7 +295,7 @@ bool music_mask_user(uint16_t keycode) {
 
 
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
     state  = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
    return state;
 }
