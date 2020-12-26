7c6136777c716abfc4fc755d0bd735afe4d2b25f
Zach White <skullydazed@gmail.com>
2020-12-26 10:25:12 -0800

    matrix/noah: remove the broken matrix print code to fix compilation

--- keyboards/matrix/noah/matrix.c
+++ keyboards/matrix/noah/matrix.c
@@ -151,16 +151,4 @@ matrix_row_t matrix_get_row(uint8_t row) { return matrix[row]; }
 
 void matrix_print(void)
 {
-    printf("\nr/c 01234567\n");
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        printf("%X0: ", row);
-        matrix_row_t data = matrix_get_row(row);
-        for (int col = 0; col < MATRIX_COLS; col++) {
-            if (data & (1<<col))
-                printf("1");
-            else
-                printf("0");
-        }
-        printf("\n");
-    }
 }
