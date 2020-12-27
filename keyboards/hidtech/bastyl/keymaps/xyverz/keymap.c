48f4130c9645b4b544e71f8e4efaba1737119c79
Ian Sterling <xyverz@gmail.com>
2020-12-26 21:27:31 -0800

    Fixed typos

--- keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [1] = LAYOUT(
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-  _______, _______, _______, KC_UP,   _______, KC_HOME, KC_PGUP, _______, _______, _______, _______, _______, \
-  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_PGDN, _______, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
-  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, \
+  _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, \
+  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_PGUP, _______, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,  KC_PGDN, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, \
                              _______, _______, _______, _______, _______, _______, \
                                       RESET,   _______, _______, _______
   ),
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [2] = LAYOUT(
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
   _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, \
-  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
-  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, \
+  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_PGUP, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
+  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,  KC_PGDN, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, \
                              _______, _______, _______, _______, _______, _______, \
                                       _______, _______, _______, RESET
   ),
