6eb56dcd71256c9cffba720e918acea383593882
Ian Sterling <xyverz@gmail.com>
2020-12-27 12:41:46 -0800

    Thumb Cluster re-do
    Another configuration of the Bastyl thumb cluster

--- keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-                             MO(1),   KC_BSPC, KC_DEL,  KC_ENT,  KC_SPC,  MO(2), \
+                             KC_BSPC, KC_DEL,  MO(1),   MO(2),   KC_ENT,  KC_SPC, \
                                       KC_LCTL, KC_LALT, KC_RGUI, KC_RCTL
   ),
 
