da399398b4bcfdc6e698f76abb00b2a78de180ad
Ian Sterling <503326@MC02YT9K9LVCF.tld>
2020-12-28 09:56:57 -0800

    Reverting arrows back to previous layout.

--- keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
@@ -25,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-                             KC_BSPC, KC_DEL,  MO(1),   MO(2),   KC_ENT,  KC_SPC, \
+                             MO(1),   KC_BSPC, KC_DEL,  KC_ENT,  KC_SPC,  MO(2), \
                                       KC_LCTL, KC_LALT, KC_RGUI, KC_RCTL
   ),
 
 [1] = LAYOUT(
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  KC_CAPS, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_HOME, KC_PGUP, _______, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+  _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, \
+  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_PGUP, _______, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
   _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,  KC_PGDN, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, \
                              _______, _______, _______, _______, _______, _______, \
                                       RESET,   _______, _______, _______
@@ -40,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [2] = LAYOUT(
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  KC_CAPS, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_HOME, KC_PGUP, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
+  _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, \
+  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_PGUP, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
   _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,  KC_PGDN, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, \
                              _______, _______, _______, _______, _______, _______, \
                                       _______, _______, _______, RESET
