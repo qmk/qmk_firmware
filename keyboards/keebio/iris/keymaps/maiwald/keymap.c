b4687727a6a7adc2456f3a934173ea26c632f54a
Luciano Maiwald <luciano.maiwald@gmail.com>
2020-12-28 22:53:21 +0100

    add quot symbol

--- keyboards/keebio/iris/keymaps/maiwald/keymap.c
+++ keyboards/keebio/iris/keymaps/maiwald/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX, KC_EXLM, KC_LT,   KC_DLR,  KC_GT,   KC_AT,                              XXXXXXX, KC_LBRC, KC_UNDS, KC_RBRC, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_EXLM, KC_LT,   KC_DLR,  KC_GT,   KC_AT,                              KC_QUOT, KC_LBRC, KC_UNDS, KC_RBRC, XXXXXXX, XXXXXXX,
    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
       XXXXXXX, KC_BSLS, KC_LPRN, KC_DQUO, KC_RPRN, KC_HASH,                            KC_PERC, KC_LCBR, KC_PEQL, KC_RCBR, KC_PIPE, XXXXXXX,
    //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
