dca3d21531d6aaabda7482f9e623e67e8667f393
Ananya Kirti <AnanyaKirti@users.noreply.github.com>
2020-12-30 03:19:36 +0530

    Update keyboards/hp69/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/hp69/config.h
+++ keyboards/hp69/config.h
@@ -29,11 +29,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
-
-
-#define A4_AUDIO
-#define A5_AUDIO
-
 /*
  * Keyboard Matrix Assignments
  *
