f888d029385f00ff77abcdc79491b77bb81a77ea
Joel Challis <git@zvecr.com>
2020-12-29 23:38:37 +0000

    Update keyboards/handwired/6key/rules.mk

--- keyboards/handwired/6key/rules.mk
+++ keyboards/handwired/6key/rules.mk
@@ -20,5 +20,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-UNICODE_ENABLE = yes
 DIP_SWITCH_ENABLE = yes
