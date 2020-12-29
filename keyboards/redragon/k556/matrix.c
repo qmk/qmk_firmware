345dc28786aa7706e8c79d967910259fe379943e
Adam Honse <calcprogrammer1@gmail.com>
2020-12-29 00:56:46 +0000

    Speed up the matrix scan to prevent double typed keys

--- keyboards/redragon/k556/matrix.c
+++ keyboards/redragon/k556/matrix.c
@@ -155,7 +155,7 @@ void matrix_init(void) {
     SN_CT16B1->MR22 = 0xFF;
 
     // Set prescale value
-    SN_CT16B1->PRE = 0x20;
+    SN_CT16B1->PRE = 0x16;
 
     //Set CT16B1 as the up-counting mode.
 	SN_CT16B1->TMRCTRL = (mskCT16_CRST);
