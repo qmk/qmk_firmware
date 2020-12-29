e5f97500b5a45a531ab297da387bcf75a8d65535
Gondolindrim <alvaro.volpato@usp.br>
2020-12-29 00:23:44 -0300

    Change definitions to lowercase parameters

--- keyboards/mode/eighty/eighty.h
+++ keyboards/mode/eighty/eighty.h
@@ -2,8 +2,8 @@
 
 #include "quantum.h"
 
-#if defined(KEYBOARD_mode_eighty_M80H)
-  #include "M80H.h"
-#elif defined(KEYBOARD_mode_eighty_M80S)
-  #include "M80S.h"
+#if defined(KEYBOARD_mode_eighty_m80h)
+  #include "m80h.h"
+#elif defined(KEYBOARD_mode_eighty_m80s)
+  #include "m80s.h"
 #endif // Mode80 solderable "S" and hotswap "H" revisions
