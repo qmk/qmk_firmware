161fe27d9905144ee7a19cf88dafa10390250031
Oliver Granlund <olli.granlund@gmail.com>
2020-12-30 17:47:58 +0200

    Adding back via-keymap with via-support in rules.mk

--- keyboards/neito/rules.mk
+++ keyboards/neito/rules.mk
@@ -29,6 +29,3 @@ MIDI_ENABLE = no            # MIDI support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-
-VIA_ENABLE = yes
-LTO_ENABLE = yes
