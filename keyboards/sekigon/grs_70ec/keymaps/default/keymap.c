ad3b7c37156f0e9bb10645ac42294a0c7811778b
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2020-12-30 13:16:45 +0900

    Delete unused comment and function

--- keyboards/sekigon/grs_70ec/keymaps/default/keymap.c
+++ keyboards/sekigon/grs_70ec/keymaps/default/keymap.c
@@ -15,12 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-
-// Defines the keycodes used by our macros in process_record_user
-// enum custom_keycodes {
-//     QMKBEST = SAFE_RANGE,
-// };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
              KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
@@ -45,6 +39,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return true;
-}
