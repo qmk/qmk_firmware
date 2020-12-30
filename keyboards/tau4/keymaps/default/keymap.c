0f5bc1fc7eb90731b5f11a6215a5dec4e45b289c
Oliver Gaskell <olivergaskell700@gmail.com>
2020-12-30 19:45:58 +0000

    Fix missing # preventing keymap from building

--- keyboards/tau4/keymaps/default/keymap.c
+++ keyboards/tau4/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-include "kb.h"
+#include "kb.h"
 #include "stdio.h"
 
 enum layers {
