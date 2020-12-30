873636ca13dab679437d3585ff1d2ebab1fa540d
Joshua Diamond <josh@windowoffire.com>
2020-12-30 15:00:28 -0500

    trim some fat

--- users/spidey3/spidey3.c
+++ users/spidey3/spidey3.c
@@ -122,18 +122,11 @@ bool process_gflock(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    dprintf("key event:\n"
-            "kc: %02X, col: %u, row: %u, pressed: %u\n"
-            "mods: %08b "
+    dprintf("key event: kc: %02X, col: %02u, row: %02u, pressed: %u mods: %08b "
 #if !defined(NO_ACTION_ONESHOT)
             "os: %08b "
 #endif
-            "weak: %08b\n"
-            "      CSAGCSAG "
-#if !defined(NO_ACTION_ONESHOT)
-            "    CSAGCSAG "
-#endif
-            "      CSAGCSAG\n",
+            "weak: %08b\n",
             keycode, record->event.key.col, record->event.key.row, record->event.pressed, bitrev(get_mods()),
 #if !defined(NO_ACTION_ONESHOT)
             bitrev(get_oneshot_mods()),
