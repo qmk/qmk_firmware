bad7e44750d82fd1d41a6a4bad29aa33f052a0d6
Brandon Claveria <48102030+swiftrax@users.noreply.github.com>
2020-12-29 23:23:37 -0800

    Update keyboards/handwired/swiftrax/joypad/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/swiftrax/joypad/keymaps/via/keymap.c
+++ keyboards/handwired/swiftrax/joypad/keymaps/via/keymap.c
@@ -43,8 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if(clockwise)
-    tap_code(KC_VOLU);
-  else
-    tap_code(KC_VOLD);
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
 }
