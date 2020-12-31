f4360886d58e112aa67319a5f7a4fec78de1c358
k.bigwheel <k.bigwheel+eng@gmail.com>
2020-12-31 11:55:42 +0900

    Add default keymap

--- keyboards/earth_rover/keymaps/default/keymap.c
+++ keyboards/earth_rover/keymaps/default/keymap.c
@@ -17,46 +17,19 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
-};
-
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
-    QMKURL
+    _BASE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
-    [_FN] = LAYOUT(
-        QMKBEST, QMKURL,  _______,
-            RESET,    XXXXXXX
+              KC_A, KC_B,
+        KC_C, KC_D, KC_E, KC_F,
+        KC_G, KC_H, KC_I, KC_J,
+        KC_K, KC_L, KC_M, KC_N
     )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
     return true;
 }
