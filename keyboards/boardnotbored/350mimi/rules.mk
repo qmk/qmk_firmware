644f745be42eafe6cb8ff7dc97848824c52863d6
lbibass <ewdries02@gmail.com>
2020-12-31 15:52:50 -0500

    added 350 mimi

---
+++ keyboards/boardnotbored/350mimi/rules.mk
@@ -0,0 +1,26 @@
+# MCU name
+MCU = STM32F072
+
+# Build Options
+#   comment out to disable the options.
+#
+
+
+BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = yes	# Console for debug
+COMMAND_ENABLE = yes    # Commands for debug and configuration
+SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+CUSTOM_MATRIX = no # Custom matrix file
+BACKLIGHT_ENABLE = no # This is broken on 072 for some reason
+RGBLIGHT_ENABLE = yes
+WS2812_DRIVER = bitbang
+VIA_ENABLE = yes
+LAYOUTS = 60_tsangan_hhkb 60_ansi_tsangan LAYOUT_ALL
+
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+#LTO_ENABLE = yes
