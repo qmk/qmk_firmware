29a5743d2ce1008392fc649af4e5d73e33e6ef39
Jonas Gessner <JonasGessner@users.noreply.github.com>
2021-01-01 20:40:52 +0100

    Move various options to separate bitfields, add GPL license strings, update docstrings

--- quantum/key_override.c
+++ quantum/key_override.c
@@ -1,9 +1,29 @@
+/* 
+ * Copyright 2021 Jonas Gessner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 #include "report.h"
 
 #include <debug.h>
 
+// For benchmarking the time it takes to call process_key_override on every key press (needs keyboard debugging enabled as well)
 // #define BENCH_KEY_OVERRIDE
+
+// For debug output (needs keyboard debugging enabled as well)
 // #define DEBUG_KEY_OVERRIDE
 
 #ifdef BENCH_KEY_OVERRIDE
@@ -41,7 +61,7 @@ static uint8_t mod_down_count = 0;
 static bool enabled = true;
 
 // Public variables
-__attribute__((weak)) key_override_t **key_overrides = NULL;
+__attribute__((weak)) const key_override_t **key_overrides = NULL;
 
 // Forward decls
 static const key_override_t *clear_active_override(void);
@@ -69,6 +89,7 @@ void key_override_toggle(void) {
 
 bool key_override_is_enabled(void) { return enabled; }
 
+// Returns whether the modifiers that are pressed are such that the override should activate
 static bool key_override_matches_active_modifiers(const key_override_t *override, const uint8_t mods) {
     // Check that negative keys pass
     if ((override->negative_modifier_mask & mods) != 0) {
@@ -126,7 +147,7 @@ const key_override_t *clear_active_override(void) {
 
     // Then unregister the mod-free replacement key if desired
     if (unregister_replacement) {
-        uint8_t replacement = clear_mods_from(active_override->replacement);
+        const uint8_t replacement = clear_mods_from(active_override->replacement);
         if (IS_KEY(replacement)) {
             del_key(replacement);
         } else {
@@ -137,8 +158,8 @@ const key_override_t *clear_active_override(void) {
     }
 
     // Optionally re-register the trigger if it is still down
-    if (active_override->reregister_trigger_after_completion && active_override_trigger_is_down && active_override->trigger != KC_NO) {
-        uint16_t trigger = active_override->trigger;
+    if ((active_override->options & ko_option_reregister_trigger_on_deactivation) != 0 && active_override_trigger_is_down && active_override->trigger != KC_NO) {
+        const uint16_t trigger = active_override->trigger;
 
         key_override_printf("Re-registering trigger: %u\n", trigger);
 
@@ -161,53 +182,87 @@ const key_override_t *clear_active_override(void) {
     return old;
 }
 
-/// Returns true if the key action should be sent
-static bool try_activating_override(const uint16_t keycode, const uint8_t layer, const bool key_down, const uint8_t active_mods, const key_override_t *const blocked) {
+/** Checks if the key event is an allowed activation event for the provided override. Does not check things like whether the correct mods or correct trigger key is down. */
+static bool check_activation_event(const key_override_t *override, const bool key_down, const bool is_mod) {
+    ko_activation_event_t allowed_events = override->allowed_activation_events;
+
+    if (is_mod) {
+        if (key_down) {
+            return (allowed_events & ko_activation_mod_down) != 0;
+        } else {
+            return (allowed_events & ko_activation_mod_up) != 0;
+        }
+    }
+    else {
+        if (key_down) {
+            return (allowed_events & ko_activation_key_down) != 0;
+        } else {
+            return (allowed_events & ko_activation_key_up) != 0;
+        }
+    }
+}
+
+/** Checks if the options of the override allow activating the override given the current keyboard state. */
+static bool check_options(const key_override_t *const override) {
+    const bool no_trigger = override->trigger == KC_NO;
+
+    bool ok = true;
+
+    if ((override->options & ko_option_exclusive_key_on_activate) != 0) {
+        ok &= (key_down_count - mod_down_count) > (1 - (uint8_t)no_trigger);
+    }
+
+    return ok;
+}
+
+/** Iterates through the list of key overrides and tries activating each, until it finds one that activates or reaches the end of overrides. Returns true if the key action for `keycode` should be sent */
+static bool try_activating_override(const uint16_t keycode, const uint8_t layer, const bool key_down, const bool is_mod, const uint8_t active_mods, const key_override_t *const blocked) {
     if (key_overrides == NULL) {
         return true;
     }
 
     for (uint8_t i = 0;; i++) {
-        const key_override_t *override = key_overrides[i];
+        const key_override_t *const override = key_overrides[i];
 
+        // End of array
         if (override == NULL) {
             break;
         }
 
-        // Fast, but not full mods check. Most key presses will not have any mods down, and most overrides will require mods.
+        // Fast, but not full mods check. Most key presses will not have any mods down, and most overrides will require mods. Hence here we filter overrides that require mods to be down while no mods are down
         if (active_mods == 0 && override->trigger_modifiers != 0) {
             key_override_printf("Not activating override: Modifiers don't match\n");
             continue;
         }
 
-        // Check layers
+        // Check layer
         if ((override->layers & (1 << layer)) == 0) {
             key_override_printf("Not activating override: Not set to activate on pressed layer\n");
             continue;
         }
 
-        const bool is_trigger = override->trigger == keycode;
-
-        // Many overrides will require trigger down
-        if (override->trigger_only_on_trigger_down && (!key_down || !is_trigger)) {
-            key_override_printf("Not activating override: Only trigger on trigger down\n");
+        // Check allowed activation events
+        if (!check_activation_event(override, key_down, is_mod)) {
+            key_override_printf("Not activating override: Activation event not allowed\n");
             continue;
         }
 
-        // Check if trigger lifted
+        // Check options
+        if (!check_options(override)) {
+            key_override_printf("Not activating override: Options not allowing activation\n");
+            continue;
+        }
+
+        const bool is_trigger = override->trigger == keycode;
+
+        // Check if trigger lifted. This is a small optimization in order to skip the remaining checks
         if (is_trigger && !key_down) {
             key_override_printf("Not activating override: Trigger lifted\n");
             continue;
         }
 
         // If the trigger is KC_NO it means 'no key', so only the required modifiers need to be down.
-        bool no_trigger = override->trigger == KC_NO;
-
-        // Check if too many keys down. If there is no trigger key and allows_other_keys_down_at_trigger if false we require only the modifiers and no other keys to be down.
-        if (!override->allows_other_keys_down_at_trigger && (key_down_count - mod_down_count) > (1 - (uint8_t)no_trigger)) {
-            key_override_printf("Not activating override: Too many keys down\n");
-            continue;
-        }
+        const bool no_trigger = override->trigger == KC_NO;
 
         // Check if blocked
         if (override == blocked) {
@@ -228,21 +283,11 @@ static bool try_activating_override(const uint16_t keycode, const uint8_t layer,
         }
 
         // Check if trigger key is down.
-        bool should_activate = no_trigger;
-
         const bool trigger_down = is_trigger && key_down;
 
-        if (!should_activate) {
-            if (trigger_down) {
-                should_activate = true;
-                key_override_printf("Trigger just pressed down\n");
-            } else {
-                if (hidden_key == override->trigger) {
-                    should_activate = true;
-                    key_override_printf("Trigger already down: hidden trigger\n");
-                }
-            }
-        }
+        // At this point, all requirements for activation are checked, except whether the trigger key is pressed. Now we check if the required trigger is down
+        // If no trigger key is required, yes. If the trigger was just pressed, yes. If the trigger is the hidden key it is also down, so yes.
+        bool should_activate = no_trigger || trigger_down || hidden_key == override->trigger;
 
         // Look through pressed key codes to see if trigger is down
         if (!should_activate && override->trigger <= UINT8_MAX) {
@@ -261,10 +306,13 @@ static bool try_activating_override(const uint16_t keycode, const uint8_t layer,
 
         key_override_printf("Activating override\n");
 
+#ifdef DEBUG_KEY_OVERRIDE
+        // Sanity check
         if (extract_mod_bits(override->trigger) != 0) {
             key_override_printf("ERROR! Not activating override: Trigger key includes mod bits. It cannot include any mod bits. Use trigger_modifiers for this.\n");
             continue;
         }
+#endif
 
         clear_active_override();
 
@@ -280,7 +328,8 @@ static bool try_activating_override(const uint16_t keycode, const uint8_t layer,
             del_key(override->trigger);
         }
 
-        bool register_replacement = override->replacement != KC_NO;
+        // If the replacement key is KC_NO, there is nothing to register.
+        const bool register_replacement = override->replacement != KC_NO;
 
         // Try firing the custom handler
         if (override->custom_action != NULL) {
@@ -288,8 +337,8 @@ static bool try_activating_override(const uint16_t keycode, const uint8_t layer,
         }
 
         if (register_replacement) {
-            uint8_t  override_mods = extract_mod_bits(override->replacement);
-            uint16_t mod_free      = clear_mods_from(override->replacement);
+            const uint8_t  override_mods = extract_mod_bits(override->replacement);
+            const uint16_t mod_free      = clear_mods_from(override->replacement);
 
             set_weak_override_mods(override_mods);
 
@@ -297,12 +346,12 @@ static bool try_activating_override(const uint16_t keycode, const uint8_t layer,
                 add_key(mod_free);
             } else {
                 key_override_printf("NOT KEY 3\n");
-                // Have to send keyboard report before reigstering non key
+                // Have to send keyboard report before reigstering non key, as those are not sent in the keyboard report.
                 send_keyboard_report();
                 register_code(mod_free);
             }
         } else {
-            // If not registering the replacement key send keyboard report to update the unregistered keys
+            // If not registering the replacement key send keyboard report to update the unregistered keys.
             send_keyboard_report();
         }
 
@@ -349,9 +398,11 @@ bool process_key_override(const uint16_t keycode, const keyrecord_t *const recor
 
     const key_override_t *blocked = NULL;
 
+    // This only takes the physical mods on purpose. Key overrides are currently only supposed to trigger on physical key press events (process_record_quantum).
     uint8_t effective_mods = get_mods();
+    const bool is_mod = IS_MOD(keycode);
 
-    if (IS_MOD(keycode)) {
+    if (is_mod) {
         if (key_down) {
             mod_down_count++;
             effective_mods |= MOD_BIT(keycode);
@@ -374,24 +425,34 @@ bool process_key_override(const uint16_t keycode, const keyrecord_t *const recor
 
         // Check if trigger of current override goes up or if override does not allow additional keys to be down and another key goes down
         if (active_override != NULL) {
-            bool trigger_up = (keycode == active_override->trigger && !key_down);
+            const bool is_trigger = keycode == active_override->trigger;
+            const bool trigger_up = is_trigger && !key_down;
 
-            if (trigger_up) {
+            bool should_deactivate = false;
+            
+            // Check if trigger key lifted
+            if (is_trigger && !key_down) {
                 active_override_trigger_is_down = false;
+                should_deactivate = true;
+                key_override_printf("Deactivating override because trigger key up\n");
             }
 
-            if (trigger_up || (keycode != active_override->trigger && key_down && !active_override->allows_other_keys_down_while_active)) {
-                key_override_printf("Deactivating override because trigger key up or another key was pressed and no other keys are allowed down\n");
+            if (!is_trigger && key_down && (active_override->options & ko_option_exclusive_key_during_active) != 0) {
+                key_override_printf("Deactivating override because another key was pressed and no other keys are allowed down\n");
+                should_deactivate = true;
+            }
+
+            if (should_deactivate) {
                 blocked = clear_active_override();
             }
         }
     }
 
     // Get the exact layer that was hit. It will be cached at this point
-    uint8_t layer = read_source_layers_cache(record->event.key);
+    const uint8_t layer = read_source_layers_cache(record->event.key);
 
     // Use blocked to ensure the same override is not activated again immediately after it is deactivated
-    bool send_key_action = try_activating_override(keycode, layer, key_down, effective_mods, blocked);
+    const bool send_key_action = try_activating_override(keycode, layer, key_down, effective_mods, blocked);
 
     if (!send_key_action) {
         send_keyboard_report();
--- quantum/key_override.h
+++ quantum/key_override.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2021 Jonas Gessner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <stdbool.h>
@@ -7,23 +24,44 @@
 #include "action_layer.h"
 
 /**
- * Key overrides allow you to perform custom actions when a certain modifier-key combination is pressed. 
- * 
- * You can define custom actions for arbitrary modifier+key combinations: if modifiers x + key y are pressed, send modifiers w + key z instead of modifiers x + key y in the keyboard report.
- * This feature basically gives you the power of layers without having to use dedicated layer keys, allowing you to use arbitary modifier key combinations similarly to layers.
- * 
- * You may for example use a single key for media controls and use key overrides to trigger different actions:
- * - Simple key press: play/pause
- * - Ctrl + key: volume up
- * - Ctrl + shift + key: volume down
- * - Alt + key: next track
- * - Alt + shift + key: previous track
- * 
- * This list could continue, for example:
- * - Ctrl + alt + key: screen brightness up
- * - Ctrl + alt + shift + key: screen brightness down
+ * Key overrides allow you to send a different key-modifier combination or perform a custom action when a certain modifier-key combination is pressed.
+ *
+ * For example, you may configure a key override to send the delete key when shift + backspace are pressed together, or that your volume keys become screen brightness keys when holding ctrl. The possibilities are quite vast and the documentation contains a few examples for inspiration.
+ *
+ * See the documentation here: https://docs.qmk.fm/#/feature_key_overrides
  */
 
+/** Bitfield defining which events are allowed to activate a key override. */
+typedef enum {
+    /** When the trigger key is pressed down. */
+    ko_activation_key_down = (1 << 0),
+    /** When another key is released. Only has an effect in conjunction with ko_option_exclusive_key_on_activate. */
+    ko_activation_key_up = (1 << 1),
+    /** When a necessary modifier is pressed down. */
+    ko_activation_mod_down = (1 << 2),
+    /** When a negative modifier is released. */
+    ko_activation_mod_up = (1 << 3),
+
+    /** The default activation events used by the key_override_make_xxx functions. */
+    ko_activation_events_default = ko_activation_key_down,
+} ko_activation_event_t;
+
+/** Bitfield with various options controlling the behavior of a key override. */
+typedef enum {
+    /** If set, any of the modifiers in trigger_modifiers will be enough to activate the override (logical OR of modifiers). If not set, all the modifiers in trigger_modifiers have to be pressed (logical AND of modifiers). */
+    ko_option_one_mod = (1 << 0),
+    /** If set, the override can only activate if no non-modifier key except the trigger key is down. */
+    ko_option_exclusive_key_on_activate = (1 << 1),
+    /** If set, the trigger key has to be the only non-modifier key that is pressed while the override is active. If another key is pressed, the opverride will be deactivated. */
+    ko_option_exclusive_key_during_active = (1 << 2),
+    /** Whether the trigger key should be registered again after the override is deactivated (only in case it is still physically pressed). */
+    ko_option_reregister_trigger_on_deactivation = (1 << 3),
+
+    /** The default options used by the key_override_make_xxx functions. */
+    ko_options_default = ko_option_exclusive_key_during_active,
+} ko_option_t;
+
+/** Defines a single key override */
 typedef struct {
     // The basic keycode that triggers the override. This keycode MUST exclude modifiers. It can also be a custom keycode.
     uint16_t trigger; 
@@ -31,23 +69,14 @@ typedef struct {
     // Which mods need to be down for activation. If both sides of mod are set (e.g. left ctrl and right ctrl) then only one is required to trigger (e.g. left ctrl suffices).
     uint8_t trigger_modifiers;
 
+    // All activation events that may activate this override.
+    ko_activation_event_t allowed_activation_events;
+
     // This is a BITMASK (!), defining which layers this override applies to. To use this override on layer i set the ith bit (1 << i).
     layer_state_t layers;
 
-    // If set to true, any of the modifiers in trigger_modifiers will be enough to activate the override (logical OR of modifiers). If set to false all the modifiers in trigger_modifiers have to be pressed (logical AND of modifiers).
-    bool one_mod;
-
     // Which keys cannot be down: get_mods() & negative_modifier_mask must be 0. Can be used to disallow specific modifiers for fine-grained control.
     uint8_t negative_modifier_mask;
-
-    // Whether allowing to activate this override when other keys are already down. This only concerns non-modifier keys.
-    bool allows_other_keys_down_at_trigger;
-
-    // Whether to deactivate the override an when another key is pressed down (while the override is active). This only concerns non-modifier keys.
-    bool allows_other_keys_down_while_active;
-
-    // Whether to only activate when the trigger key is pressed down while the necessary modifiers are already down. This can be useful for some overrides that would otherwise falsely trigger during quick typing. Setting this to false only works as expected for basic trigger keycodes (keys that appear in the keyboard report), and behaves like setting to true for non-basic trigger keycodes.
-    bool trigger_only_on_trigger_down;
 
     // Modifiers to suppress when active. Can be used to suppress the trigger modifiers, as a trivial example.
     uint8_t suppressed_mods;
@@ -55,8 +84,8 @@ typedef struct {
     // The complex keycode to send as replacement when this override is triggered. This may be a custom keycode or a key-modifier combination. Use in combination with suppressed_mods to get the correct modifiers to be sent if needed.
     uint16_t replacement;
 
-    // Whether the trigger key should be registered again after the override is deactivated (only in case it is still physically pressed).
-    bool reregister_trigger_after_completion;
+    // Additional options controlling the behavior of the override.
+    ko_option_t options;
 
     // If not NULL, this function will be called right before the replacement key is registered, along with the provided context and a flag indicating whether the override was activated or deactivated. This function allows you to run some custom actions for specific key overrides. If you return `false`, the replacement key is not registered/unregistered as it would normally. Return `true` to register and unregister the override normally.
     bool (*custom_action)(bool activated, void *context);
@@ -68,16 +97,19 @@ typedef struct {
     bool *enabled;
 } key_override_t;
 
-/**
- *  Define this as an array of pointers to key overrides.
- */
-extern key_override_t **key_overrides;
+/** Define this as a null-terminated array of pointers to key overrides. These key overrides will be used by qmk. */
+extern const key_override_t **key_overrides;
 
-// On/Off
+/** Turns key overrides on */
 extern void key_override_on(void);
+
+/** Turns key overrides off */
 extern void key_override_off(void);
+
+/** Toggles key overrides on */
 extern void key_override_toggle(void);
 
+/** Returns whether key overrides are enabled */
 extern bool key_override_is_enabled(void);
 
 /**
@@ -99,19 +131,16 @@ extern bool key_override_is_enabled(void);
  * Convenience initializer to create a basic key override. Provide a bitmap with the bits set for each layer on which the override should activate. Also provide a negative modifier mask, that is used to define which modifiers may not be pressed.
  */
 #define key_override_make_for_layers_and_negative_mods(trigger_mods, trigger_key, replacement_key, layer_mask, negative_mask) \
-    ((key_override_t){                                                      \
-        .trigger_modifiers                      = (trigger_mods),           \
-        .one_mod                                = false,                    \
-        .layers                                 = (layer_mask),                 \
-        .suppressed_mods                        = (trigger_mods),           \
-        .allows_other_keys_down_at_trigger      = true,                     \
-        .allows_other_keys_down_while_active    = false,                    \
-        .reregister_trigger_after_completion    = false,                    \
-        .trigger_only_on_trigger_down           = true,                     \
-        .negative_modifier_mask                 = (negative_mask),                        \
-        .custom_action                          = NULL,                     \
-        .context                                = NULL,                     \
-        .trigger                                = (trigger_key),            \
-        .replacement                            = (replacement_key),        \
-        .enabled                                = NULL                      \
+    ((const key_override_t){                                                                \
+        .trigger_modifiers                      = (trigger_mods),                           \
+        .layers                                 = (layer_mask),                             \
+        .suppressed_mods                        = (trigger_mods),                           \
+        .allowed_activation_events              = ko_activation_events_default,             \
+        .options                                = ko_options_default,                       \
+        .negative_modifier_mask                 = (negative_mask),                          \
+        .custom_action                          = NULL,                                     \
+        .context                                = NULL,                                     \
+        .trigger                                = (trigger_key),                            \
+        .replacement                            = (replacement_key),                        \
+        .enabled                                = NULL                                      \
     })
