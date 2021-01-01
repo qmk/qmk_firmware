ab9389f886d912fb64891e1c1e8953bbf4e09602
Drashna Jael're <drashna@live.com>
2020-12-31 20:50:59 -0800

    Only write if it's not the first pass

--- tmk_core/common/action_layer.c
+++ tmk_core/common/action_layer.c
@@ -32,7 +32,12 @@ __attribute__((weak)) layer_state_t default_layer_state_set_kb(layer_state_t sta
  * Static function to set the default layer state, prints debug info and clears keys
  */
 static void default_layer_state_set(layer_state_t state) {
-    eeconfig_update_default_layer(state);
+    static bool has_run = false;
+    if (has_run) {
+        eeconfig_update_default_layer(state);
+    } else {
+        has_run = true;
+    }
     state = default_layer_state_set_kb(state);
     debug("default_layer_state: ");
     default_layer_debug();
