b3e956b41ac558187dba5d552cb332046b0846ae
Jonas Gessner <JonasGessner@users.noreply.github.com>
2021-01-03 00:25:37 +0100

    Handle missing key override activation options

--- quantum/key_override.c
+++ quantum/key_override.c
@@ -221,6 +221,11 @@ const key_override_t *clear_active_override(const bool allow_reregister) {
 static bool check_activation_event(const key_override_t *override, const bool key_down, const bool is_mod) {
     ko_option_t options = override->options;
 
+    if (options & ko_options_all_activations == 0) {
+        // No activation option provided at all. This is wrong, but let's assume the default activations (ko_options_all_activations) were meant...
+        options = ko_options_all_activations;
+    }
+
     if (is_mod) {
         if (key_down) {
             return (options & ko_option_activation_required_mod_down) != 0;
