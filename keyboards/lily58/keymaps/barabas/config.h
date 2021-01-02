874c2a002a7013b6670c50006f6920955f941f33
Barabas Raffai <barabas.raffai@gmail.com>
2021-01-02 18:49:24 +0000

    tidy up config.h and rules.mk
    Remove all unused defines from config.h, and remove options which are the same as the keyboard level make file. Enabled command to be able to toggle NKRO.

--- keyboards/lily58/keymaps/barabas/config.h
+++ keyboards/lily58/keymaps/barabas/config.h
@@ -22,16 +22,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define MASTER_LEFT
-
 #define USE_SERIAL_PD2
-
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
