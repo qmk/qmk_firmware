ad6d0648b455f28a8178b289c4b956da6c86d006
Oliver Gaskell <olivergaskell700@gmail.com>
2021-01-03 00:12:52 +0000

    Remove IS_COMMAND

--- keyboards/tau4/config.h
+++ keyboards/tau4/config.h
@@ -43,11 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
