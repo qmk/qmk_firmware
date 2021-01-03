71246eeb8ba0a528a76c266747f27fc907d41f1c
Oliver Gaskell <olivergaskell700@gmail.com>
2021-01-03 00:09:27 +0000

    Rename kb.c and kb.h to tau4.c and tau4.h

--- keyboards/tau4/keymaps/default/keymap.c
+++ keyboards/tau4/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "kb.h"
+#include "tau4.h"
 #include "stdio.h"
 
 enum layers {
