d19ede6d6fa68c5c1c36ac5712f5481919ee9968
Oliver Gaskell <olivergaskell700@gmail.com>
2021-01-03 00:14:14 +0000

    Remove USB DESCRIPTION definition

--- keyboards/tau4/config.h
+++ keyboards/tau4/config.h
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    taukeyboards
 #define PRODUCT         tau4
-#define DESCRIPTION     Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 4
