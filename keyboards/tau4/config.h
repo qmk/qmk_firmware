b3ff6215798f1d8be6909b86e8d4420823c677ff
Oliver Gaskell <olivergaskell700@gmail.com>
2021-01-03 00:12:12 +0000

    Update DEBOUNCING_DELAY to DEBOUNCE

--- keyboards/tau4/config.h
+++ keyboards/tau4/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* key combination for command */
 #define IS_COMMAND() ( \
