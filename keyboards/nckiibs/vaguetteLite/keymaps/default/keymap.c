194edf7c99f2c9f5a2bdde15b50fea11800fe736
noclew <noclews@gmail.com>
2021-01-04 17:27:30 -0500

    keymaps updated

--- keyboards/nckiibs/vaguetteLite/keymaps/default/keymap.c
+++ keyboards/nckiibs/vaguetteLite/keymaps/default/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT(
     _______,                KC_F1  ,    KC_F2  ,    KC_F3  ,    KC_F4  ,    KC_F5  ,    KC_F6  ,    KC_F7  ,    KC_F8  ,    KC_F9  ,    KC_F10  ,   KC_F11 ,    KC_F12 ,    KC_F12 ,   _______,
-    _______,                KC_EQL ,    KC_7   ,    KC_8   ,    KC_9   ,    KC_BSPC,    KC_CIRC,    KC_LBRC,    KC_RBRC,    KC_AT  ,    KC_EXLM,    KC_BSLS,    _______,    _______,
-    _______,                KC_0   ,    KC_4   ,    KC_5   ,    KC_6   ,    KC_MINS,    KC_UNDS,    KC_LPRN,    KC_RPRN,    KC_AMPR,    KC_QUOT,    _______,    _______,    _______,
-    _______,    _______,    KC_1   ,    KC_2   ,    KC_3   ,    KC_MINS,    KC_PLUS,    _______,    KC_LCBR,    KC_RCBR,    _______,    _______,    _______,    _______,
+    _______,                KC_EQL ,    KC_7   ,    KC_8   ,    KC_9   ,    KC_SLSH,    KC_CIRC,    KC_LBRC,    KC_RBRC,    KC_AT  ,    KC_EXLM,    KC_BSLS,    _______,    _______,
+    KC_BSPC,                KC_0   ,    KC_4   ,    KC_5   ,    KC_6   ,    KC_MINS,    KC_UNDS,    KC_LPRN,    KC_RPRN,    KC_AMPR,    KC_QUOT,    _______,    _______,    _______,
+    _______,    _______,    KC_PAST,    KC_1   ,    KC_2   ,    KC_3   ,    KC_PPLS,    KC_NUHS,    KC_LCBR,    KC_RCBR,    _______,    _______,    _______,    _______,
     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
     ),
 
--- keyboards/nckiibs/waaffle/keymaps/default/keymap.c
+++ keyboards/nckiibs/waaffle/keymaps/default/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    _______,    _______,    _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6  ,    KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12 ,
     _______,    _______,    _______,    _______,    _______,    KC_EQL,     KC_7,       KC_8,       KC_9,       KC_BSPC,    KC_CIRC,    KC_LBRC,    KC_RBRC,    KC_AT  ,    KC_EXLM,    KC_BSLS,
     _______,    _______,    _______,    _______,    _______,    KC_0,       KC_4,       KC_5,       KC_6,       KC_MINS,    KC_UNDS,    KC_LPRN,    KC_RPRN,    KC_AMPR,    KC_QUOT,    _______,
-    _______,    _______,    _______,    _______,    _______,    KC_NUHS,    KC_1,       KC_2,       KC_3,       KC_PPLS,    KC_PAST,    KC_LCBR,    KC_RCBR,    KC_PERC,    KC_DLR ,    _______,
+    _______,    _______,    _______,    _______,    _______,    KC_PAST,    KC_1,       KC_2,       KC_3,       KC_PPLS,    KC_NUHS,    KC_LCBR,    KC_RCBR,    KC_PERC,    KC_DLR ,    _______,
     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
     ),
 
