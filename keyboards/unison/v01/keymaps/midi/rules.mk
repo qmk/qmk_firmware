70b17d336015c4962efd0289658ee429524c99be
Takeshi Nishio <kkeennnn@gmail.com>
2021-01-05 06:32:39 +0900

    Update: Audioはデフォとせず、OLEDをデフォとした。

--- keyboards/unison/v01/keymaps/midi/rules.mk
+++ keyboards/unison/v01/keymaps/midi/rules.mk
@@ -2,9 +2,9 @@ TAP_DANCE_ENABLE = yes		# Enable Tap Dance
 
 # Audio feature can cause HUGE firmware size impact.
 # You should turn off MIDI and OLED.
-AUDIO_ENABLE = yes
+AUDIO_ENABLE = no
 
 MIDI_ENABLE = yes
-OLED_DRIVER_ENABLE = no
+OLED_DRIVER_ENABLE = yes
 
 ENCODER_ENABLE = yes
