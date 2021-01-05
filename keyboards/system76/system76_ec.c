5b9ff3dc7c294530081036a9a8861633a533d2b7
Jeremy Soller <jeremy@system76.com>
2021-01-05 12:00:50 -0700

    Add led value and color commands

--- keyboards/system76/system76_ec.c
+++ keyboards/system76/system76_ec.c
@@ -2,6 +2,7 @@
 
 #include "dynamic_keymap.h"
 #include "raw_hid.h"
+#include "rgb_matrix.h"
 #include "version.h"
 
 enum Command {
@@ -15,7 +16,17 @@ enum Command {
     CMD_KEYMAP_GET = 9,
     // Set keyboard map index
     CMD_KEYMAP_SET = 10,
+    // Get LED value by index
+    CMD_LED_GET_VALUE = 11,
+    // Set LED value by index
+    CMD_LED_SET_VALUE = 12,
+    // Get LED color by index
+    CMD_LED_GET_COLOR = 13,
+    // Set LED color by index
+    CMD_LED_SET_COLOR = 14,
 };
+
+#define CMD_LED_INDEX_ALL 0xFF
 
 static bool keymap_get(uint8_t layer, uint8_t output, uint8_t input, uint16_t *value) {
 	if (layer < dynamic_keymap_get_layer_count()) {
@@ -41,6 +52,33 @@ static bool keymap_set(uint8_t layer, uint8_t output, uint8_t input, uint16_t va
 	return false;
 }
 
+#if defined(RGB_MATRIX_CUSTOM_KB)
+extern uint8_t raw_rgb_r;
+extern uint8_t raw_rgb_g;
+extern uint8_t raw_rgb_b;
+
+static uint8_t led_v = 255;
+static uint8_t led_r = 0;
+static uint8_t led_g = 0;
+static uint8_t led_b = 0;
+
+static uint8_t led_normalize(uint8_t component, uint8_t brightness) {
+    uint32_t x = (uint32_t)component;
+    x *= brightness; // Multiply by software brightness
+    x *= RGB_MATRIX_MAXIMUM_BRIGHTNESS; // Multiply by hardware brightness
+    x /= 255; // Divide by maximum software brightness
+    x /= 255; // Divide by maximum hardware brightness
+    return (uint8_t)x;
+}
+
+static void led_update(void) {
+    raw_rgb_r = led_normalize(led_r, led_v);
+    raw_rgb_g = led_normalize(led_g, led_v);
+    raw_rgb_b = led_normalize(led_b, led_v);
+    rgb_matrix_mode(RGB_MATRIX_CUSTOM_raw_rgb);
+}
+#endif // defined(RGB_MATRIX_CUSTOM_KB)
+
 void raw_hid_receive(uint8_t *data, uint8_t length) {
 	// Error response by default, set to success by commands
 	data[1] = 1;
@@ -62,25 +100,70 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
             strncpy((char *)&data[2], QMK_VERSION, length - 2);
             data[1] = 0;
             break;
-		case CMD_KEYMAP_GET:
-			{
-				uint16_t value = 0;
-				if (keymap_get(data[2], data[3], data[4], &value)) {
-					data[5] = (uint8_t)value;
-					data[6] = (uint8_t)(value >> 8);
-					data[1] = 0;
-				}
-			}
-			break;
+        case CMD_KEYMAP_GET:
+            {
+                uint16_t value = 0;
+                if (keymap_get(data[2], data[3], data[4], &value)) {
+                    data[5] = (uint8_t)value;
+                    data[6] = (uint8_t)(value >> 8);
+                    data[1] = 0;
+                }
+            }
+            break;
         case CMD_KEYMAP_SET:
             {
-				uint16_t value =
+                uint16_t value =
                     ((uint16_t)data[5]) |
                     (((uint16_t)data[6]) << 8);
-				if (keymap_set(data[2], data[3], data[4], value)) {
-					data[1] = 0;
-				}
+                if (keymap_set(data[2], data[3], data[4], value)) {
+                    data[1] = 0;
+                }
             }
+            break;
+#if defined(RGB_MATRIX_CUSTOM_KB)
+        case CMD_LED_GET_VALUE:
+            {
+                //TODO: Getting single LEDs?
+                if (data[2] == CMD_LED_INDEX_ALL) {
+                    data[3] = led_v;
+                    data[1] = 0;
+                }
+            }
+            break;
+        case CMD_LED_SET_VALUE:
+            {
+                //TODO: Setting single LEDs?
+                if (data[2] == CMD_LED_INDEX_ALL) {
+                    led_v = data[3];
+                    led_update();
+                    data[1] = 0;
+                }
+            }
+            break;
+        case CMD_LED_GET_COLOR:
+            {
+                //TODO: Getting single LEDs?
+                if (data[2] == CMD_LED_INDEX_ALL) {
+                    data[3] = led_r;
+                    data[4] = led_g;
+                    data[5] = led_b;
+                    data[1] = 0;
+                }
+            }
+            break;
+        case CMD_LED_SET_COLOR:
+            {
+                //TODO: Setting single LEDs?
+                if (data[2] == CMD_LED_INDEX_ALL) {
+                    led_r = data[3];
+                    led_g = data[4];
+                    led_b = data[5];
+                    led_update();
+                    data[1] = 0;
+                }
+            }
+            break;
+#endif // defined(RGB_MATRIX_CUSTOM_KB)
 	}
 
 	raw_hid_send(data, length);
