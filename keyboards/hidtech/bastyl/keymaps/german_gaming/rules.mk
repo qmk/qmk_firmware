ecbef0f66440674cca7dac8ed6e3e940be12b6bb
CreamyCookie <CreamyCookie@users.noreply.github.com>
2021-01-05 20:23:45 +0100

    Added german_gaming keymap for hidtech/bastyl

---
+++ keyboards/hidtech/bastyl/keymaps/german_gaming/rules.mk
@@ -0,0 +1,6 @@
+
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
