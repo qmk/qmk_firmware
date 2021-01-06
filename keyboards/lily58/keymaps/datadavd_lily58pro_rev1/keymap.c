a38f5354bad5826d08cc85adced39f8f50d16add
David Dansby <ddansby89@gmail.com>
2021-01-06 02:04:24 -0800

    Updt Lily58 keymap (rev2); remove OLED keylogger

--- keyboards/lily58/keymaps/datadavd_lily58pro_rev1/keymap.c
+++ keyboards/lily58/keymaps/datadavd_lily58pro_rev1/keymap.c
@@ -19,15 +19,15 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
- * ,-----------------------------------------.                    ,-------------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BackSP |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------- |
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \|    |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------- |
- * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |  ;:  |  '"    |
- * |------+------+------+------+------+------|   `~  |    | Enter |------+------+------+------+------+------- |
- * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |  ,<  |  .>  |  ?/  | RShift |
- * `-----------------------------------------/       /     \      \-------------------------------------------'
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |BackSP|
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+----- |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | \|   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+----- |
+ * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |  ;:  |  '"  |
+ * |------+------+------+------+------+------|   `~  |    | Enter |------+------+------+------+------+------|
+ * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |  ,<  |  .>  |  ?/  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |  [   | LAlt | LGUI | /SUPER  /       \Space \  |RAISE | RGUI |   ]  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
@@ -38,16 +38,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_GRV, KC_ENT,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LCBR, KC_LALT, KC_LGUI, MO(_SUPER), KC_SPC, MO(_RAISE), KC_RGUI, KC_RCBR \
+                        KC_LBRC, KC_LALT, KC_LGUI, MO(_SUPER), KC_SPC, MO(_RAISE), KC_RGUI, KC_RBRC \
 ),
-/* LOWER
+/* SUPER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |Trans | xxxx | xxxx | xxxx | xxxx | xxxx |                    |  \|  | VOLD | VOLU |  -_  |  =+  |BackSP|
+ * |Trans | XXXX | XXXX | XXXX | XXXX | XXXX |                    |  \|  |  `~  |BackSP|  -_  |  =+  |BackSP|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  Tab | Tab  | ESC  |Enter |LCTRL | CAPS |                    |  `~  |  Tab |  Up  |  {[  |  ]}  |  \|  |
+ * |  Tab | Tab  | ESC  |Enter |LCTRL |  {   |                    |   }  |  Tab |  Up  |  {[  |  ]}  |  \|  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|LShift| LGUI | LAlt |   _  |   (  |-------.    ,-------|   )  | Left | Down |Right |   =  |Enter |
- * |------+------+------+------+------+------|  {    |    |    }  |------+------+------+------+------+------|
+ * |LShift|LShift| LGUI | LAlt |   _  |  (   |-------.    ,-------|   )  | Left | Down |Right |   =  |Enter |
+ * |------+------+------+------+------+------| CAPS  |    |BackSP |------+------+------+------+------+------|
  * |LCTRL |LCTRL | Home |Pg Up |Pg Dwn| End  |-------|    |-------|   B  |   V  |   C  |   X  |   Z  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |Trans | LAlt |LGUI  | /Trans  /       \Space \  |Del   | TGUI |Trans |
@@ -55,33 +55,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_SUPER] = LAYOUT( \
-  KC_TRNS,   KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,                         KC_BSLS,  KC_VOLD,  KC_VOLU,  KC_MINS,  KC_EQL,  KC_BSPC,\
-  KC_TAB,    KC_TAB,   KC_ESC,   KC_ENT,   KC_LCTRL,  KC_CAPS,                       KC_GRV,   KC_TAB,   KC_UP,    KC_LBRC,  KC_RBRC, KC_TRNS, \
+  KC_TRNS,   KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,                         KC_BSLS,  KC_GRV,   KC_BSPC,  KC_MINS,  KC_EQL,  KC_BSPC,\
+  KC_TAB,    KC_TAB,   KC_ESC,   KC_ENT,   KC_LCTRL,  KC_LCBR,                       KC_RCBR,  KC_TAB,   KC_UP,    KC_LBRC,  KC_RBRC, KC_TRNS, \
   KC_LSFT,   KC_LSFT,  KC_LGUI,  KC_LALT,  KC_UNDS,   KC_LPRN,                       KC_RPRN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_EQL,  KC_ENT, \
-  KC_LCTL,   KC_LCTL,  KC_HOME,  KC_PGUP,  KC_PGDN,   KC_END,   KC_LBRC, KC_RBRC,    KC_B,     KC_V,     KC_C,     KC_X,     KC_Z,    KC_RSFT, \
+  KC_LCTL,   KC_LCTL,  KC_HOME,  KC_PGUP,  KC_PGDN,   KC_END,   KC_CAPS, KC_BSLS,    KC_B,     KC_V,     KC_C,     KC_X,     KC_Z,    KC_RSFT, \
                                   KC_TRNS, KC_LALT, KC_LGUI, KC_TRNS, KC_SPC, KC_DEL, KC_RGUI, KC_TRNS\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  | Tab  |      | BRID | BRIU |      |                    | MUTE | VOLU |  Up  |  {[  |  ]}  |  \|  |
+ * | Tab  | Tab  |      |  [   |   ]  | CAPS |                    | MUTE | VOLU |  Up  |  {[  |  ]}  | CAPS |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|LShift|BackSP| DEL  |  (   |  )   |-------.    ,-------| VOLD | Left | Down |Right |RShift|Enter |
- * |------+------+------+------+------+------|  ESC  |    |       |------+------+------+------+------+------|
+ * |LShift|LShift| DEL  |  (   |   )  |  `~  |-------.    ,-------| VOLD | Left | Down |Right |RShift|Enter |
+ * |------+------+------+------+------+------|  BRID |    | BRIU  |------+------+------+------+------+------|
  * |LCTRL |LCTRL | MRWD | MPLY | MFFD | MSTP |-------|    |-------| End  |Pg Dwn|Pg Up | Home |RShift|RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      | LAlt | LGUI | /BackSP /       \Trans \  |Trans |Trans |  |
+ *                   | XXXX | LAlt | LGUI | /BackSP /       \Trans \  |Trans |Trans | XXXX |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_RAISE] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,                      KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12, \
-  KC_TAB,  KC_TAB,  KC_NO,   KC_BRID,  KC_BRIU,  KC_NO,                      KC_MUTE,  KC_VOLU,  KC_UP,   KC_LBRC,  KC_RBRC,  KC_BSLS, \
-  KC_LSFT, KC_LSFT, KC_BSPC, KC_DEL,   KC_LPRN,  KC_RPRN,                    KC_VOLD,  KC_LEFT,  KC_DOWN, KC_RGHT,  KC_RSFT,  KC_ENT, \
-  KC_LCTL, KC_LCTL, KC_MRWD, KC_MPLY,  KC_MFFD,  KC_MSTP,   KC_ESC, KC_NO,   KC_END,  KC_PGDN,  KC_PGUP,  KC_HOME,  KC_RSFT,  KC_RSFT, \
-                             KC_NO, KC_LALT, KC_LGUI,  KC_BSPC, KC_TRNS,  KC_TRNS, KC_TRNS, KC_NO \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,                       KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12, \
+  KC_TAB,  KC_TAB,  KC_NO,   KC_LBRC,  KC_RBRC,  KC_CAPS,                     KC_MUTE,  KC_VOLU,  KC_UP,   KC_LBRC,  KC_RBRC,  KC_CAPS, \
+  KC_LSFT, KC_LSFT, KC_DEL,  KC_LPRN,  KC_RPRN,  KC_GRV,                      KC_VOLD,  KC_LEFT,  KC_DOWN, KC_RGHT,  KC_RSFT,  KC_ENT, \
+  KC_LCTL, KC_LCTL, KC_MRWD, KC_MPLY,  KC_MFFD,  KC_MSTP,   KC_BRID, KC_BRIU, KC_END,   KC_PGDN,  KC_PGUP, KC_HOME,  KC_RSFT,  KC_RSFT, \
+                             KC_NO, KC_LALT, KC_LGUI, KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO \
 )
 };
 
@@ -107,8 +107,8 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 const char *read_layer_state(void);
 const char *read_logo(void);
 void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
+//const char *read_keylog(void);
+//const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
@@ -119,8 +119,8 @@ void oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
+//    oled_write_ln(read_keylog(), false);
+//    oled_write_ln(read_keylogs(), false);
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
@@ -133,7 +133,7 @@ void oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
-    set_keylog(keycode, record);
+//    set_keylog(keycode, record);
 #endif
     // set_timelog();
   }
--- keyboards/lily58/keymaps/datadavd_lily58pro_rev1/rules.mk
+++ keyboards/lily58/keymaps/datadavd_lily58pro_rev1/rules.mk
@@ -24,7 +24,7 @@ SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+        # ./lib/keylogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
