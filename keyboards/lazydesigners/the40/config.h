707a8736f0e70f46a7ea31eb802f66e8095cd23e
jackytrabbit <jacky_lijun@126.com>
2021-01-07 10:19:29 +0800

    Update keyboards/lazydesigners/the40/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lazydesigners/the40/config.h
+++ keyboards/lazydesigners/the40/config.h
@@ -20,7 +20,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x4c44 // "LD"
+#define VENDOR_ID       0x4C44 // "LD"
 #define PRODUCT_ID      0x0042
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    LAZYDESIGNERS
