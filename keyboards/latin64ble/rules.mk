a1df7c84111271067641dc380a5154fe8f456461
wangyifu <18438880@qq.com>
2021-01-08 12:09:22 +0800

    Update keyboards/latin64ble/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/latin64ble/rules.mk
+++ keyboards/latin64ble/rules.mk
@@ -10,9 +10,6 @@ BOOTLOADER = caterina
 # Build Options
 #   change yes to no to disable
 #
-
-# Build Options
-#   change yes to no to disable
 BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
