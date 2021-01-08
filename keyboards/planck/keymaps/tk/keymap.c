60222f129ff248c02d8f4a12de433e12b7a9b83c
Tushar Khan <tushar.54k@gmail.com>
2021-01-08 14:09:58 -0500

    added GPL2+ compatible license headers

--- keyboards/planck/keymaps/tk/keymap.c
+++ keyboards/planck/keymaps/tk/keymap.c
@@ -1,3 +1,19 @@
+/* Copyright 2021 Tushar Khan
+ * 
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 2 of the License, or 
+ * (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */ 
+
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
@@ -367,7 +383,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-// Keypresses
+// Keycode events
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static bool panic_del = false;      // PANIC in delete-mode
