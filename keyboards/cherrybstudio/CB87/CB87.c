f1bfeba4b3c923c841b2869ab2bea8269dd4258b
Duong Anh <56441863+duongaanh@users.noreply.github.com>
2021-01-09 04:15:28 +0700

    Update keyboards/cherrybstudio/CB87/CB87.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/cherrybstudio/CB87/CB87.c
+++ keyboards/cherrybstudio/CB87/CB87.c
@@ -11,21 +11,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "CB87.h"
-
-void matrix_init_kb(void) {
-    led_init_ports();
-    matrix_init_user();
-}
-
-void led_init_ports(void) {
-    setPinOutput(CAPS_LOCK_LED_PIN);
-}
-
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if (res) {
-        writePin(CAPS_LOCK_LED_PIN, led_state.caps_lock);
-    }
-    return res;
-}
+#include "cb87.h"
