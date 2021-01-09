b140da06546854a63e15a739e1abfba2463f34ed
David Dejaeghere <david.dejaeghere@tarpit.be>
2021-01-09 01:54:51 +0100

    Update keyboards/mechstudio/ud_40_ortho/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/mechstudio/ud_40_ortho/keymaps/default/keymap.c
+++ keyboards/mechstudio/ud_40_ortho/keymaps/default/keymap.c
@@ -15,14 +15,12 @@
  */
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
-#define _MA 0 //Main layer
-#define _LO 1 //Low
-#define _HI 2 //High
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _MA,
+    _LO,
+    _HI
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MA] = LAYOUT_ortho_4x12(
