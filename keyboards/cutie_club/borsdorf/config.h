14ba916470c48e50f38922f2d2d530e93c9f500b
Callum Hart <mini-ninja-64@users.noreply.github.com>
2021-01-09 00:37:07 +0000

    Remove redundant description from config
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/cutie_club/borsdorf/config.h
+++ keyboards/cutie_club/borsdorf/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Cutie Club
 #define PRODUCT Borsdorf
-#define DESCRIPTION Borsdorf keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
