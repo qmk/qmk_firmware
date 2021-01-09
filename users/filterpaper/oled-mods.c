ff4aebc6f83ca8867fbf54bd13fe812b6e3812e7
filterpaper <filterpaper@localhost>
2021-01-09 13:06:57 +0800

    Include GNU license text

--- users/filterpaper/oled-mods.c
+++ users/filterpaper/oled-mods.c
@@ -1,3 +1,19 @@
+/* Copyright (C) 2021 @filterpaper
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /* This contains graphical mod display on the primary status side, copied from
    https://github.com/qmk/qmk_firmware/tree/master/keyboards/crkbd/keymaps/soundmonster
    Graphical mod icons are currently not displaying correctly on the OLED
