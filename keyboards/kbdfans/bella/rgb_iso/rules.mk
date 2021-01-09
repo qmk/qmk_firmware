31d20a3b2792c073817025282ea3aea33d1f204f
moyi4681 <moyi4681@users.noreply.github.com>
2021-01-09 10:06:55 +0800

    Update keyboards/kbdfans/bella/rgb_iso/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kbdfans/bella/rgb_iso/rules.mk
+++ keyboards/kbdfans/bella/rgb_iso/rules.mk
@@ -4,6 +4,9 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = atmel-dfu
 
+# Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = lite        # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes          # Mouse keys
 EXTRAKEY_ENABLE = yes          # Audio control and System control
