01fe17e084c779b2203bb32a221a7722e181b2d9
Gondolindrim <alvaro.volpato@usp.br>
2021-01-08 21:12:30 -0300

    Remove KC_NO definition from default keymap

--- keyboards/ck60i/keymaps/default/keymap.c
+++ keyboards/ck60i/keymaps/default/keymap.c
@@ -14,7 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#define _______KC_NO
 
 #include QMK_KEYBOARD_H
 
