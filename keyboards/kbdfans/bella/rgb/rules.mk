b752744332db569cb8d07b629ded10e28cba64bf
moyi4681 <moyi4681@users.noreply.github.com>
2021-01-09 10:07:25 +0800

    Update keyboards/kbdfans/bella/rgb/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kbdfans/bella/rgb/rules.mk
+++ keyboards/kbdfans/bella/rgb/rules.mk
@@ -17,5 +17,6 @@ BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no          # Enable Bluetooth
 AUDIO_ENABLE = no              # Audio output
-RGB_MATRIX_ENABLE = IS31FL3741 # Use RGB matrix
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = IS31FL3741
 NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
