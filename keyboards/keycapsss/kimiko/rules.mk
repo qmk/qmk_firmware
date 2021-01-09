3d86b63fff38a8f0aa3f8cedb108da73c305f319
Ben <BenRoe@users.noreply.github.com>
2021-01-09 21:53:51 +0100

    Update keyboards/keycapsss/kimiko/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keycapsss/kimiko/rules.mk
+++ keyboards/keycapsss/kimiko/rules.mk
@@ -22,8 +22,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
 
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no	# Breathing sleep LED during USB suspend
-
 DEFAULT_FOLDER = keycapsss/kimiko/rev1
