0783c91e67d00df9c59b6fd4c6d765ebd1380a51
Moritz <moritz.plattner@gmx.net>
2021-01-09 22:29:43 +0100

    remove outdated indicator custom code
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/linworks/whale75/whale75.c
+++ keyboards/linworks/whale75/whale75.c
@@ -14,24 +14,3 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "whale75.h"
-
-void matrix_init_kb(void) {
-  led_init_ports();
-}
-
-/* Set lock LEDs as outputs */
-void led_init_ports(void) {
-    setPinOutput(A8);
-    setPinOutput(A9);
-    setPinOutput(A13);
-}
-
-/* Set lock LEDs to their states*/
-bool led_update_kb(led_t led_state) {
-    if (led_update_user(led_state)) {
-        writePin(A8, !led_state.num_lock);
-        writePin(A9, !led_state.caps_lock);
-        writePin(A13, !led_state.scroll_lock);
-    }
-    return true;
-}
