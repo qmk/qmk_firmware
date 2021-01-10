89e8bb49ea389c68bfba40f7e20003d0a3a69258
Daniel Hertz <daniel@dhertz.com>
2021-01-09 18:24:52 -0800

    Add weak user layer change

--- keyboards/mechlovin/kanu/kanu.c
+++ keyboards/mechlovin/kanu/kanu.c
@@ -35,3 +35,13 @@ bool led_update_kb(led_t led_state) {
     return true;
 }
 
+__attribute__((weak))
+layer_state_t layer_state_set_user(layer_state_t state) {
+  // if on layer 1, turn on D2 LED, otherwise off.
+  writePin(D2, get_highest_layer(state) == 1);
+  // if on layer 2, turn on D1 LED, otherwise off.
+  writePin(D1, get_highest_layer(state) == 2);
+  // if on layer 3, turn on D0 LED, otherwise off.
+  writePin(D0, get_highest_layer(state) == 3);
+  return state;
+}
