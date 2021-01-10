f66dedb99abd6238f4fff4352800356a95c4c4e6
Sam Hudson <samsamm777@gmail.com>
2021-01-10 21:59:05 +0000

    updated config

--- keyboards/shk9/config.h
+++ keyboards/shk9/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID    0x5348
 #define PRODUCT_ID   0x4B39
 #define DEVICE_VER   0x0001
-#define MANUFACTURER Sam Hudson
+#define MANUFACTURER Super Human
 #define PRODUCT      SHK9
 #define DESCRIPTION  3x3 Keyboard
 
