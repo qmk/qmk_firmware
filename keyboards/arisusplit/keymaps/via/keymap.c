d398bd15a92e05cd81a7a41633560c1709dfdff0
andi <77161563+nouvogaming@users.noreply.github.com>
2021-01-10 08:03:39 +0700

    Update keymap.c

--- keyboards/arisusplit/keymaps/via/keymap.c
+++ keyboards/arisusplit/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 NaCly
+/* Copyright 2021 Nouvo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
