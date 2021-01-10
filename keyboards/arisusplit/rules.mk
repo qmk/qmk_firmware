9179b66b47397044419e3710d6fc1ebcf7f100ec
andi <77161563+nouvogaming@users.noreply.github.com>
2021-01-10 10:47:57 +0700

    Update rules.mk

--- keyboards/arisusplit/rules.mk
+++ keyboards/arisusplit/rules.mk
@@ -25,11 +25,7 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI controls
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-UNICODE_ENABLE = no         # Unicode
+
 BLUETOOTH_ENABLE = no 		# Enable bluetooth
 AUDIO_ENABLE = no			# Audio Output
 SPLIT_KEYBOARD = yes		# Split keyboard support
