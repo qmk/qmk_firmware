742c1c235fcf8e419e4440120e49837e60dcd4ef
noclew <noclews@gmail.com>
2021-01-11 04:10:53 -0500

    RGB config updated / keymap updated

--- keyboards/nckiibs/vanana/rev1/config.h
+++ keyboards/nckiibs/vanana/rev1/config.h
@@ -60,7 +60,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*== or choose animations ==*/
 //#    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
