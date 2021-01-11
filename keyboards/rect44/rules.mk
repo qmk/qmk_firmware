f9c1cea056c0b5860aa7ef34ed0a41a3eebdd54e
koshinoya <moto_yda@icloud.com>
2021-01-11 13:20:42 +0900

    Update keyboards/rect44/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/rect44/rules.mk
+++ keyboards/rect44/rules.mk
@@ -5,6 +5,8 @@ MCU = atmega32u4
 BOOTLOADER = caterina
 
 # Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
@@ -17,4 +19,4 @@ NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output on port C6
+AUDIO_ENABLE = no           # Audio output
