b32b8c960e711d54023c964926c0a06ad84d46a8
Sendy YK <mr@sendyyk.com>
2021-01-12 01:55:02 +0700

    Update keymap.c

--- layouts/community/60_ansi_arrow_split_bs_7u_spc/mrsendyyk/keymap.c
+++ layouts/community/60_ansi_arrow_split_bs_7u_spc/mrsendyyk/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
      * │Shift   │Z  │X  │C  │V  │B  │N  │M  │,  │.  │Shift │↑  │/  │
      * ├─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴───┴──┬┴──┬───┼───┼───┤
-     * │Ctrl │Opt│Cmd  │Space                      │Fn │←  │↓  │→  │
+     * │Ctrl │OS │Alt  │Space                      │Fn │←  │↓  │→  │
      * └─────┴───┴─────┴───────────────────────────┴───┴───┴───┴───┘
      */
     [_BASE] = LAYOUT_60_ansi_arrow_split_bs_7u_spc(
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q, KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_CAPS,          KC_A, KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,          KC_Z, KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_RSFT, KC_UP,   KC_SLSH,
-        KC_LCTL, KC_LALT,       KC_LGUI,             KC_SPC,                               MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI,       KC_LALT,             KC_SPC,                               MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT
     ),
     /*
      * [_FN] Fn Layer
