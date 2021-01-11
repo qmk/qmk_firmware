2423353cb546b481278276a8c0739bbbd0f60ac4
nibiria <72472777+nibiria@users.noreply.github.com>
2021-01-11 18:04:17 -0500

    committted changes as suggested by drashna

--- keyboards/nibiria/stream15/keymaps/via/keymap.c
+++ keyboards/nibiria/stream15/keymaps/via/keymap.c
@@ -17,19 +17,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   [0] = LAYOUT_all(
+   [0] = LAYOUT_ortho_3x5(
         KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, 
         KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, 
         KC_F23, KC_F24, KC_NO, KC_NO, RESET
     ),
 
-   [1] = LAYOUT_all(
+   [1] = LAYOUT_ortho_3x5(
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, 
         _______, _______, _______, _______, _______ 
     ),
 
-   [2] = LAYOUT_all(
+   [2] = LAYOUT_ortho_3x5(
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, 
         _______, _______, _______, _______, _______
