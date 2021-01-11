b129fa575be8c370675b5d13d69d77fa05018f9f
Florian Didron <0x6664@hey.com>
2021-01-11 09:08:29 +0900

    feat: adds portuguese OSX contributions

--- quantum/keymap_extras/keymap_contributions.h
+++ quantum/keymap_extras/keymap_contributions.h
@@ -59,3 +59,108 @@
 #define KK_42 KC_3        // і
 #define KK_43 KC_2        // ә
 #define KK_44 KC_1        // "
+
+//Portuguese OSX, contributed by André Cruz (@edevil)
+#define PT_OSX_SECT KC_GRV  // §
+#define PT_OSX_QUOT KC_MINS // '
+#define PT_OSX_PLUS KC_EQL  // +
+#define PT_OSX_MORD KC_LBRC // º
+#define PT_OSX_ACUT KC_RBRC // ´ (dead)
+#define PT_OSX_CCED KC_SCLN // Ç
+#define PT_OSX_TILD KC_QUOT // ~ (dead)
+#define PT_OSX_BSLS KC_NUHS // (backslash)
+#define PT_OSX_LABK KC_NUBS // <
+#define PT_OSX_MINS KC_SLSH // -
+#define PT_OSX_PLMN S(PT_OSX_SECT) // ±
+#define PT_OSX_EXLM S(KC_1)    // !
+#define PT_OSX_DQUO S(KC_2)    // "
+#define PT_OSX_HASH S(KC_3)    // #
+#define PT_OSX_DLR  S(KC_4)    // $
+#define PT_OSX_PERC S(KC_5)    // %
+#define PT_OSX_AMPR S(KC_6)    // &
+#define PT_OSX_SLSH S(KC_7)    // /
+#define PT_OSX_LPRN S(KC_8)    // (
+#define PT_OSX_RPRN S(KC_9)    // )
+#define PT_OSX_EQL  S(KC_0)    // =
+#define PT_OSX_QUES S(PT_OSX_QUOT) // ?
+#define PT_OSX_ASTR S(PT_OSX_PLUS) // *
+#define PT_OSX_FORD S(PT_OSX_MORD) // ª
+#define PT_OSX_GRV  S(PT_OSX_ACUT) // ` (dead)
+#define PT_OSX_CIRC S(PT_OSX_TILD) // ^ (dead)
+#define PT_OSX_PIPE S(PT_OSX_BSLS) // |
+#define PT_OSX_RABK S(PT_OSX_LABK) // >
+#define PT_OSX_SCLN S(KC_COMM) // ;
+#define PT_OSX_COLN S(KC_DOT)  // :
+#define PT_OSX_UNDS S(PT_OSX_MINS) // _
+#define PT_OSX_APPL A(KC_1)    //  (Apple logo)
+#define PT_OSX_AT   A(KC_2)    // @
+#define PT_OSX_EURO A(KC_3)    // €
+#define PT_OSX_PND  A(KC_4)    // £
+#define PT_OSX_PERM A(KC_5)    // ‰
+#define PT_OSX_PILC A(KC_6)    // ¶
+#define PT_OSX_DIV  A(KC_7)    // ÷
+#define PT_OSX_LBRC A(KC_8)    // [
+#define PT_OSX_RBRC A(KC_9)    // ]
+#define PT_OSX_NEQL A(KC_0)    // ≠
+#define PT_OSX_OE   A(KC_Q)    // Œ
+#define PT_OSX_NARS A(KC_W)    // ∑
+#define PT_OSX_AE   A(KC_E)    // Æ
+#define PT_OSX_REGD A(KC_R)    // ®
+#define PT_OSX_TM   A(KC_T)    // ™
+#define PT_OSX_YEN  A(KC_Y)    // ¥
+#define PT_OSX_DAGG A(KC_U)    // †
+#define PT_OSX_DLSI A(KC_I)    // ı
+#define PT_OSX_OSTR A(KC_O)    // Ø
+#define PT_OSX_PI   A(KC_P)    // π
+#define PT_OSX_DEG  A(PT_OSX_MORD) // °
+#define PT_OSX_DIAE A(PT_OSX_ACUT) // ¨ (dead)
+#define PT_OSX_ARNG A(KC_A)    // å
+#define PT_OSX_SS   A(KC_S)    // ß
+#define PT_OSX_PDIF A(KC_D)    // ∂
+#define PT_OSX_FHK  A(KC_F)    // ƒ
+#define PT_OSX_DOTA A(KC_G)    // ˙
+#define PT_OSX_CARN A(KC_H)    // ˇ
+#define PT_OSX_MACR A(KC_J)    // ¯
+#define PT_OSX_DLQU A(KC_K)    // „
+#define PT_OSX_LSQU A(KC_L)    // ‘
+#define PT_OSX_CEDL A(PT_OSX_CCED) // ¸
+#define PT_OSX_STIL A(PT_OSX_TILD) // ˜ (dead)
+#define PT_OSX_LSAQ A(PT_OSX_BSLS) // ‹
+#define PT_OSX_LTEQ A(PT_OSX_LABK) // ≤
+#define PT_OSX_OMEG A(KC_Z)    // Ω
+#define PT_OSX_LDAQ A(KC_X)    // «
+#define PT_OSX_COPY A(KC_C)    // ©
+#define PT_OSX_SQRT A(KC_V)    // √
+#define PT_OSX_INTG A(KC_B)    // ∫
+#define PT_OSX_NOT  A(KC_N)    // ¬
+#define PT_OSX_MICR A(KC_M)    // µ
+#define PT_OSX_LDQU A(KC_COMM) // “
+#define PT_OSX_ELLP A(KC_DOT)  // …
+#define PT_OSX_MDSH A(PT_OSX_MINS) // —
+#define PT_OSX_IEXL S(A(KC_1))    // ¡
+#define PT_OSX_FI   S(A(KC_2))    // ﬁ
+#define PT_OSX_FL   S(A(KC_3))    // ﬂ
+#define PT_OSX_CENT S(A(KC_4))    // ¢
+#define PT_OSX_INFN S(A(KC_5))    // ∞
+#define PT_OSX_BULT S(A(KC_6))    // •
+#define PT_OSX_FRSL S(A(KC_7))    // ⁄
+#define PT_OSX_LCBR S(A(KC_8))    // {
+#define PT_OSX_RCBR S(A(KC_9))    // }
+#define PT_OSX_AEQL S(A(KC_0))    // ≈
+#define PT_OSX_IQUE S(A(PT_OSX_QUOT)) // ¿
+#define PT_OSX_LOZN S(A(PT_OSX_PLUS)) // ◊
+#define PT_OSX_DDAG S(A(KC_U))    // ‡
+#define PT_OSX_RNGA S(A(KC_I))    // ˚
+#define PT_OSX_NARP S(A(KC_P))    // ∏
+#define PT_OSX_DACU S(A(PT_OSX_ACUT)) // ˝
+#define PT_OSX_INCR S(A(KC_D))    // ∆
+#define PT_OSX_SLQU S(A(KC_K))    // ‚
+#define PT_OSX_RSQU S(A(KC_L))    // ’
+#define PT_OSX_OGON S(A(PT_OSX_CCED)) // ˛
+#define PT_OSX_DCIR S(A(PT_OSX_TILD)) // ˆ (dead)
+#define PT_OSX_RSAQ S(A(PT_OSX_BSLS)) // ›
+#define PT_OSX_GTEQ S(A(PT_OSX_LABK)) // ≥
+#define PT_OSX_RDAQ S(A(KC_X))    // »
+#define PT_OSX_RDQU S(A(KC_COMM)) // ”
+#define PT_OSX_MDDT S(A(KC_DOT))  // ·
+#define PT_OSX_NDSH S(A(PT_OSX_MINS)) // –
