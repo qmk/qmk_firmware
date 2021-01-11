415d683ea71d516dd2a7d4f2f8e43eb4e3e993cb
Ryan <fauxpark@gmail.com>
2021-01-11 20:25:45 +1100

    Remove unused `action_get_macro()` usages in user files (#11165)

--- keyboards/gh60/revc/keymaps/sethbc/keymap.c
+++ keyboards/gh60/revc/keymaps/sethbc/keymap.c
@@ -28,25 +28,6 @@ const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_FUNCTION(SHIFT_ESC),
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
-    return MACRO_NONE;
-};
-
-void matrix_scan_user(void) {
-
-}
-
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
   static uint8_t shift_esc_shift_mask;
   switch (id) {
