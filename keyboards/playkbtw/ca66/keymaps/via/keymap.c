bd619f9acae7097d6892cf281e8cc431b7d0a65e
J.Flanagan <jrfhoutx@comcast.net>
2021-01-11 21:43:42 -0600

    Minor changes to CA66 for VIA support
    edited keymap info
    enabled backlight

--- keyboards/playkbtw/ca66/keymaps/via/keymap.c
+++ keyboards/playkbtw/ca66/keymaps/via/keymap.c
@@ -1,12 +1,11 @@
 /*
-*KeLorean CA66 keymap.
+*Based on the KeLorean CA66 keymap.
 *Made for HHKB layout with CA66 badge in lower left corner and split spacebars.
 *Left Spacebar - tap for backspace, hold for Layer1
 *Right Spacebar - tap for space, hold for Layer2
 *Layer 2 is same as Layer 1 except Left Spacebar is now delete
 *Cap Locks has been replaced with Momentary Toggle to Layer3
-*Layer 4 can be reached only by Layer3 in the '\' key
-*Layer 4 tab is the soft reset.
+*Layer 3 tab is the soft reset.
 */
 
 #include QMK_KEYBOARD_H
