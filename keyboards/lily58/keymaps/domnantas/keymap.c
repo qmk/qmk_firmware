8a1788d65aab4075751a4f9442349d42238d76b9
Domantas <domantas@zyro.com>
2021-01-12 21:45:44 +0200

    Update keyboards/lily58/keymaps/domnantas/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/domnantas/keymap.c
+++ keyboards/lily58/keymaps/domnantas/keymap.c
@@ -126,12 +126,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   |      |      |      |/       /         \      \ |      ||||||||      |
      *                   `----------------------------'           '------''--------------------'
      */
-    [_RAISE] = LAYOUT( \
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PSCR, \
-        _______, KC_F11,  KC_F12,  _______, _______, _______,                   _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, \
-        _______, _______, _______, _______, _______, _______,                   _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, \
-        _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, \
-                                   _______, _______, _______, _______, _______, _______, _______, _______ \
+    [_RAISE] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PSCR,
+        _______, KC_F11,  KC_F12,  _______, _______, _______,                   _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU,
+        _______, _______, _______, _______, _______, _______,                   _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD,
+        _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     /* ADJUST
