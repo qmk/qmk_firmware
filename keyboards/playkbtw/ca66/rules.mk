b9c66077eb875314e59ac6edc3ef4d71acf7589e
J.Flanagan <jrfhoutx@comcast.net>
2021-01-12 10:39:00 -0600

    Update keyboards/playkbtw/ca66/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/playkbtw/ca66/rules.mk
+++ keyboards/playkbtw/ca66/rules.mk
@@ -15,7 +15,7 @@ BOOTLOADER = atmel-dfu
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = lite
-MOUSEKEY_ENABLE = no	# Mouse keys
+MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = no	# Console for debug
 COMMAND_ENABLE = no    # Commands for debug and configuration
