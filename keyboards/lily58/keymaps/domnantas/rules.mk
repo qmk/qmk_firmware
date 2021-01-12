ca8ea5f2779484ad70dbb50e8ce16d882f3a2feb
Domantas <domantas@zyro.com>
2021-01-12 21:46:03 +0200

    Update keyboards/lily58/keymaps/domnantas/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/domnantas/rules.mk
+++ keyboards/lily58/keymaps/domnantas/rules.mk
@@ -1,4 +1,2 @@
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 EXTRAKEY_ENABLE = yes
 WPM_ENABLE = yes
