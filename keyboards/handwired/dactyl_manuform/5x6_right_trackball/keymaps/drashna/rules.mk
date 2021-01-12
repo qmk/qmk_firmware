dad9f699ba728f708586bad5c4fe50c143760995
Drashna Jael're <drashna@live.com>
2021-01-11 21:29:32 -0800

    more dactyl tweaks

--- keyboards/handwired/dactyl_manuform/5x6_right_trackball/keymaps/drashna/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6_right_trackball/keymaps/drashna/rules.mk
@@ -4,6 +4,6 @@ COMMAND_ENABLE               = no
 AUDIO_ENABLE                 = yes
 TAP_DANCE_ENABLE             = yes
 UNICODE_ENABLE               = yes
+OLED_DRIVER_ENABLE           = yes
 
-
-DEBOUNCE_TYPE = sym_eager_pk
+# DEBOUNCE_TYPE = sym_eager_pk
