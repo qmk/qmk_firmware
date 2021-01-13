6e931cc90b2524d56e118b37cd0e5b52a1931bb9
Joel Challis <git@zvecr.com>
2021-01-13 17:04:18 +0000

    Fix leafcutterlabs/bigknob compiles in configurator (#11531)
    * Fix leafcutterlabs/bigknob compiles in configurator
    
    * Revert change

--- keyboards/leafcutterlabs/bigknob/keymaps/default/keymap.c
+++ keyboards/leafcutterlabs/bigknob/keymaps/default/keymap.c
@@ -1,22 +1,24 @@
-/* Copyright 2021 Craig Gardner 
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
-  
+/* Copyright 2021 Craig Gardner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
 
-#define _MAIN 0
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE
+};
 
 void encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) { 
@@ -47,9 +49,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
  [TD_RGB] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_rgb_finished, NULL)
 };
 
-//
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { //buttion closest to usb is first
-  [_MAIN] = LAYOUT(
+  [_BASE] = LAYOUT(
      KC_MUTE, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_STOP, TD(TD_RGB)
   )
 };
