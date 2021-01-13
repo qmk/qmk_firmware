039b6d385fd47148c15ef82e7046d50a1e931ff6
filterpaper <filterpaper@fruit>
2021-01-13 17:05:17 +0800

    Additional mouse key settings

--- users/filterpaper/config.h
+++ users/filterpaper/config.h
@@ -23,11 +23,27 @@
 #define LEADER_PER_KEY_TIMING
 
 #ifdef MOUSEKEY_ENABLE
+#	define MOUSEKEY_INTERVAL 16
 #	define MOUSEKEY_DELAY 0
 #	define MOUSEKEY_MAX_SPEED 7
-#	define MOUSEKEY_INTERVAL 16
 #	define MOUSEKEY_TIME_TO_MAX 50
 #	define MOUSEKEY_WHEEL_DELAY 0
+#	define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+// Settings from keyboards/kyria/keymaps/pierrechevalier83/config.h
+// Set the mouse settings to a comfortable speed/accuracy trade-off
+// Assume the screen refresh rate is 60 Htz or higher
+// The default is 50. This makes the mouse ~3 times faster and more accurate
+// #define MOUSEKEY_INTERVAL 16
+// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
+// give it more time to accelerate to max speed to retain precise control over short distances.
+// #define MOUSEKEY_TIME_TO_MAX 40
+// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+// #define MOUSEKEY_DELAY 100
+// It makes sense to use the same delay for the mouseweel
+// #define MOUSEKEY_WHEEL_DELAY 100
+// The default is 100
+// #define MOUSEKEY_WHEEL_INTERVAL 50
+// The default is 40
 #endif
 
 #if defined(KEYBOARD_crkbd)
