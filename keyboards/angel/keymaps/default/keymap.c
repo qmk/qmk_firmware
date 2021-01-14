09cbded76320e3025b5e45c353bdcd1de7b87709
james sa <188772+jamessa@users.noreply.github.com>
2021-01-15 00:03:05 +0800

    Update keyboards/angel/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/angel/keymaps/default/keymap.c
+++ keyboards/angel/keymaps/default/keymap.c
@@ -72,8 +72,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
-
-void keyboard_post_init_user(void) {
-  debug_enable=true;
-  debug_matrix=true;
-}
