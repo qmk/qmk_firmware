0ea4546013335276c22ca9c87e06b19d9a517a87
Richard Nunez <richard.nunez@macrossplusinc.com>
2021-01-13 23:52:02 -0600

    Adding split layer with ctl tap dance, alt tap dance, and mouse support.

--- keyboards/handwired/ortho5x14/rules.mk
+++ keyboards/handwired/ortho5x14/rules.mk
@@ -30,4 +30,4 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 TAP_DANCE_ENABLE = yes
-DYNAMIC_MACRO_ENABLE = yes
+# DYNAMIC_MACRO_ENABLE = yes
