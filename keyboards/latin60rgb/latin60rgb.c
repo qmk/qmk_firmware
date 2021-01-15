8a76b821c76e563ba1ebea58aac423d32062dcfa
wangyifu <18438880@qq.com>
2021-01-15 13:11:08 +0800

    Update latin60rgb.c

--- keyboards/latin60rgb/latin60rgb.c
+++ keyboards/latin60rgb/latin60rgb.c
@@ -12,6 +12,7 @@
   * 
   * You should have received a copy of the GNU General Public License 
   * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+   */ 
 #include "latin60rgb.h"
 
 #ifdef RGB_MATRIX_ENABLE
