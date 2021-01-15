39fad848a7eace4e5ace185c9961c9bc3807849c
datafx <2432997+datafx@users.noreply.github.com>
2021-01-15 00:55:11 -0500

    Removed alternate bootloaders

--- keyboards/xo87/rgb/rules.mk
+++ keyboards/xo87/rgb/rules.mk
@@ -1,14 +1,6 @@
 # MCU name
 MCU = at90usb646
 
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = lufa-dfu
 
 # Build Options
