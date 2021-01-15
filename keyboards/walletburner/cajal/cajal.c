437d314b0173c97494f61f42aa011fbc88bfe2f6
ridingqwerty <george.g.koenig@gmail.com>
2021-01-15 15:30:09 -0500

    Update keyboards/walletburner/cajal/cajal.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/walletburner/cajal/cajal.c
+++ keyboards/walletburner/cajal/cajal.c
@@ -15,24 +15,3 @@
  */
 
 #include "cajal.h"
-
-void matrix_init_kb(void) {
-    setPinOutput(B5);
-    writePinLow(B5);
-    setPinOutput(B6);
-    writePinLow(B6);
-    setPinOutput(B7);
-    writePinLow(B7);
-    matrix_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if(res) {
-        writePin(B5, led_state.caps_lock);
-        writePin(B6, led_state.num_lock);
-        writePin(B7, led_state.scroll_lock);
-    }
-    return res;
-}
-
