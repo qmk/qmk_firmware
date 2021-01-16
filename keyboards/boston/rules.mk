c97da6ff481eedd383f3cbc6b4f0218dc742232e
bluepylons <jwang757@bu.edu>
2021-01-15 23:21:48 -0500

    Removed "on port C6" from Line 20 (committed suggestion)
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/boston/rules.mk
+++ keyboards/boston/rules.mk
@@ -17,7 +17,8 @@ NKRO_ENABLE = yes          # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes     # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no      # Enable Bluetooth
-AUDIO_ENABLE = no          # Audio output on port C6
+AUDIO_ENABLE = no          # Audio output
+ENCODER_ENABLE = yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
