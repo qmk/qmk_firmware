e7385cfeaee340c999ccd29e99a911ad0812b0de
Steven Nguyen <steven7c8@gmail.com>
2021-01-17 08:31:34 -0800

    Code cleanup and styling to conform to QMK style guide

--- keyboards/7c8/framework/keymaps/default/keymap.c
+++ keyboards/7c8/framework/keymaps/default/keymap.c
@@ -17,183 +17,179 @@
 #include QMK_KEYBOARD_H
 
 enum framework_layers {
-  _BASE,
-  _LOWER,
-  _RAISE,
-  _BOTH,
-  _FN
+    _BASE,
+    _LOWER,
+    _RAISE,
+    _BOTH,
+    _FN
 };
 
 enum framework_keycodes {
-  LOWER = SAFE_RANGE,
-  RAISE,
-  BOTH,
-  FN
+    LOWER = SAFE_RANGE,
+    RAISE,
+    BOTH,
+    FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = framework_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MEDIA_PLAY_PAUSE,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_LCTL, KC_LGUI, KC_LALT, FN,      LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MEDIA_PLAY_PAUSE,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    KC_LCTL, KC_LGUI, KC_LALT, FN,      LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_LOWER] = framework_grid(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  _______,
-  KC_LEAD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______, _______, _______, _______, _______,
-  KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, _______, _______, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  _______,
+    KC_LEAD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______, _______, _______, _______, _______,
+    KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, _______, _______, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 [_RAISE] = framework_grid(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, KC_TRNS, _______, _______, KC_TRNS, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, KC_TRNS, _______, _______, KC_TRNS, _______, _______, _______, _______
 ),
 
 [_BOTH] = framework_grid(
-  _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-  _______, LALT(KC_F4), _______, KC_MYCM, LGUI(KC_R), _______, _______, _______, _______, _______, _______, _______,
-  _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-  _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-  _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, DEBUG
+    _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+    _______, LALT(KC_F4), _______, KC_MYCM, LGUI(KC_R), _______, _______, _______, _______, _______, _______, _______,
+    _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______,     _______, _______, _______,    _______, _______, _______, _______, _______, _______, DEBUG
 ),
 
 [_FN] = framework_grid(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
-
-void keyboard_post_init_user(void) {
-  eeconfig_init();
-}
 
 LEADER_EXTERNS();
 
 void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
 
-    // qq, alt+f4 close window
-    SEQ_TWO_KEYS(KC_Q, KC_Q) {
-      tap_code16(A(KC_F4));
-    }
+        // qq, alt+f4 close window
+        SEQ_TWO_KEYS(KC_Q, KC_Q) {
+            tap_code16(A(KC_F4));
+        }
 
-    // ee, open explorer
-    SEQ_TWO_KEYS(KC_E, KC_E) {
-      tap_code16(G(KC_E));
-    }
+        // ee, open explorer
+        SEQ_TWO_KEYS(KC_E, KC_E) {
+            tap_code16(G(KC_E));
+        }
 
-    // rr, windows run prompt
-    SEQ_TWO_KEYS(KC_R, KC_R) {
-      tap_code16(G(KC_R));
-    }
+        // rr, windows run prompt
+        SEQ_TWO_KEYS(KC_R, KC_R) {
+            tap_code16(G(KC_R));
+        }
 
-    // ww, maximize window
-    SEQ_TWO_KEYS(KC_W, KC_W) {
-      tap_code16(G(KC_UP));
-    }
+        // ww, maximize window
+        SEQ_TWO_KEYS(KC_W, KC_W) {
+            tap_code16(G(KC_UP));
+        }
 
-    // ss, minimize window
-    SEQ_TWO_KEYS(KC_S, KC_S) {
-      tap_code16(G(KC_DOWN));
-    }
+        // ss, minimize window
+        SEQ_TWO_KEYS(KC_S, KC_S) {
+            tap_code16(G(KC_DOWN));
+        }
 
-    // <space><space>, toggle desktop
-    SEQ_TWO_KEYS(KC_SPC, KC_SPC) {
-      tap_code16(G(KC_D));
+        // <space><space>, toggle desktop
+        SEQ_TWO_KEYS(KC_SPC, KC_SPC) {
+            tap_code16(G(KC_D));
+        }
     }
-  }
-}	
+}   
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if(index == 0) {
-    switch(biton32(layer_state)) {
-      case _BASE:
-        if(clockwise) {
-          tap_code(KC_AUDIO_VOL_UP);
-        } else {
-          tap_code(KC_AUDIO_VOL_DOWN);
-        }
-        break;
+    if (index == 0) {
+        switch (biton32(layer_state)) {
+            case _BASE:
+                if (clockwise) {
+                    tap_code(KC_AUDIO_VOL_UP);
+                } else {
+                    tap_code(KC_AUDIO_VOL_DOWN);
+                }
+                break;
 
-      case _LOWER:
-        if(clockwise) {
-          tap_code16(LCTL(KC_TAB));
-        } else {
-          tap_code16(LCTL(LSFT(KC_TAB)));
-        }
-        break;
+            case _LOWER:
+                if (clockwise) {
+                    tap_code16(LCTL(KC_TAB));
+                } else {
+                    tap_code16(LCTL(LSFT(KC_TAB)));
+                }
+                break;
 
-      case _RAISE:
-        if(clockwise) { 
-          tap_code16(LCTL(KC_RGHT));
-        } else {
-          tap_code16(LCTL(KC_LEFT));
-        }
-        break;
+            case _RAISE:
+                if (clockwise) { 
+                    tap_code16(LCTL(KC_RGHT));
+                } else {
+                    tap_code16(LCTL(KC_LEFT));
+                }
+                break;
 
-      case _BOTH:
-        if(clockwise) {
-          tap_code16(LCTL(KC_Y));
-        } else {
-          tap_code16(LCTL(KC_Z));
-        }
-        break;
+            case _BOTH:
+                if (clockwise) {
+                    tap_code16(LCTL(KC_Y));
+                } else {
+                    tap_code16(LCTL(KC_Z));
+                }
+                break;
 
-      case _FN:
-        if(clockwise) {
-          tap_code(_______);
-        } else {
-          tap_code(_______);
-        }
-        break;
+            case _FN:
+                if (clockwise) {
+                    tap_code(_______);
+                } else {
+                    tap_code(_______);
+                }
+                break;
 
-      default:
-        break;
+            default:
+                break;
+        }
     }
-  }
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _BOTH);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _BOTH);
-      }
-      return false;
+    switch (keycode) {
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _BOTH);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _BOTH);
+            }
+            return false;
 
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _BOTH);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _BOTH);
-      }
-      return false;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _BOTH);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _BOTH);
+            }
+            return false;
 
-    case FN:
-      if (record->event.pressed) {
-        layer_on(_FN);
-      } else {
-        layer_off(_FN);
-      }
-      return false;
-  }
-  return true;
+        case FN:
+            if (record->event.pressed) {
+                layer_on(_FN);
+            } else {
+                layer_off(_FN);
+            }
+            return false;
+    }
+    return true;
 }
--- keyboards/7c8/framework/keymaps/via/keymap.c
+++ keyboards/7c8/framework/keymaps/via/keymap.c
@@ -17,164 +17,113 @@
 #include QMK_KEYBOARD_H
 
 enum framework_layers {
-  _BASE,
-  _LOWER,
-  _RAISE,
-  _BOTH,
-  _FN
+    _BASE,
+    _LOWER,
+    _RAISE,
+    _BOTH,
+    _FN
 };
 
 enum framework_keycodes {
-  LOWER = SAFE_RANGE,
-  RAISE,
-  BOTH,
-  FN
+    LOWER = SAFE_RANGE,
+    RAISE,
+    BOTH,
+    FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = framework_via(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,    KC_6,   KC_7,       KC_8,    KC_9,    KC_0,    KC_MEDIA_PLAY_PAUSE,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), MO(_LOWER), KC_SPC,  KC_SPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-  KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,    KC_6,   KC_7,       KC_8,    KC_9,    KC_0,    KC_MEDIA_PLAY_PAUSE,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), MO(_LOWER), KC_SPC,  KC_SPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP
 ),
 
 [_LOWER] = framework_via(
-  _______, _______, _______, _______, _______, _______, _______, _______,   _______, KC_MINS, KC_EQL,  _______,
-  KC_LEAD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______,   _______, KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______,   _______, _______, _______, _______,
-  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,   _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, MO(_BOTH), KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-  C(S(KC_TAB)), C(KC_TAB)
+    _______, _______, _______, _______, _______, _______, _______, _______,   _______, KC_MINS, KC_EQL,  _______,
+    KC_LEAD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______,   _______, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______,   _______, _______, _______, _______,
+    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,   _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, MO(_BOTH), KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+    C(S(KC_TAB)), C(KC_TAB)
 ),
 
 [_RAISE] = framework_via(
-  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, MO(_BOTH), _______, _______, _______, _______, _______, _______, _______,
-  C(KC_LEFT), C(KC_RIGHT)
+    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, MO(_BOTH), _______, _______, _______, _______, _______, _______, _______,
+    C(KC_LEFT), C(KC_RIGHT)
 ),
 
 [_BOTH] = framework_via(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EEP_RST, DEBUG,
-  C(KC_Z), C(KC_Y)
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EEP_RST, DEBUG,
+    C(KC_Z), C(KC_Y)
 ),
 
 [_FN] = framework_via(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  KC_MS_WH_LEFT, KC_MS_WH_RIGHT
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_MS_WH_LEFT, KC_MS_WH_RIGHT
 )
 };
 
 LEADER_EXTERNS();
 
 void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
 
-    // qq, alt+f4 close window
-    SEQ_TWO_KEYS(KC_Q, KC_Q) {
-      tap_code16(A(KC_F4));
-    }
+        // qq, alt+f4 close window
+        SEQ_TWO_KEYS(KC_Q, KC_Q) {
+            tap_code16(A(KC_F4));
+        }
 
-    // ee, open explorer
-    SEQ_TWO_KEYS(KC_E, KC_E) {
-      tap_code16(G(KC_E));
-    }
+        // ee, open explorer
+        SEQ_TWO_KEYS(KC_E, KC_E) {
+            tap_code16(G(KC_E));
+        }
 
-    // rr, windows run prompt
-    SEQ_TWO_KEYS(KC_R, KC_R) {
-      tap_code16(G(KC_R));
-    }
+        // rr, windows run prompt
+        SEQ_TWO_KEYS(KC_R, KC_R) {
+            tap_code16(G(KC_R));
+        }
 
-    // ww, maximize window
-    SEQ_TWO_KEYS(KC_W, KC_W) {
-      tap_code16(G(KC_UP));
-    }
+        // ww, maximize window
+        SEQ_TWO_KEYS(KC_W, KC_W) {
+            tap_code16(G(KC_UP));
+        }
 
-    // ss, minimize window
-    SEQ_TWO_KEYS(KC_S, KC_S) {
-      tap_code16(G(KC_DOWN));
-    }
+        // ss, minimize window
+        SEQ_TWO_KEYS(KC_S, KC_S) {
+            tap_code16(G(KC_DOWN));
+        }
 
-    // <space><space>, toggle desktop
-    SEQ_TWO_KEYS(KC_SPC, KC_SPC) {
-      tap_code16(G(KC_D));
+        // <space><space>, toggle desktop
+        SEQ_TWO_KEYS(KC_SPC, KC_SPC) {
+            tap_code16(G(KC_D));
+        }
     }
-  }
 } 
-/*
-void encoder_update_user(uint8_t index, bool clockwise) {
-  if(index == 0) {
-    switch(biton32(layer_state)) {
-      case _BASE:
-        if(clockwise) {
-          tap_code(KC_AUDIO_VOL_UP);
-        } else {
-          tap_code(KC_AUDIO_VOL_DOWN);
-        }
-        break;
-
-      case _LOWER:
-        if(clockwise) {
-          tap_code16(LCTL(KC_TAB));
-        } else {
-          tap_code16(LCTL(LSFT(KC_TAB)));
-        }
-        break;
-
-      case _RAISE:
-        if(clockwise) { 
-          tap_code16(LCTL(KC_RGHT));
-        } else {
-          tap_code16(LCTL(KC_LEFT));
-        }
-        break;
-
-      case _BOTH:
-        if(clockwise) {
-          tap_code16(LCTL(KC_Y));
-        } else {
-          tap_code16(LCTL(KC_Z));
-        }
-        break;
-
-      case _FN:
-        if(clockwise) {
-          tap_code(KC_MS_WH_RIGHT);
-        } else {
-          tap_code(KC_MS_WH_LEFT);
-        }
-        break;
-
-      default:
-        break;
-    }
-  }
-}
-*/
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-  uint8_t layer = biton32(layer_state);
-
-  if(index == 0) {
-    if(clockwise) {
-      tap_code16(dynamic_keymap_get_keycode(layer, 10, 1));
-    } else {
-      tap_code16(dynamic_keymap_get_keycode(layer, 10, 0));
+    uint8_t layer = biton32(layer_state);
+    if (index == 0) {
+        if (clockwise) {
+            tap_code16(dynamic_keymap_get_keycode(layer, 10, 1));
+        } else {
+            tap_code16(dynamic_keymap_get_keycode(layer, 10, 0));
+        }
     }
-  }
 }
