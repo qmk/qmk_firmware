8a1eefcfe824300a1d70c739a5873e67260441a8
Drashna Jael're <drashna@live.com>
2021-01-17 09:20:58 -0800

    Update based on user feedback

--- keyboards/splitkb/zima/zima.h
+++ keyboards/splitkb/zima/zima.h
@@ -17,14 +17,6 @@
 
 #include "quantum.h"
 
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
 #define LAYOUT_ortho_4x3( \
     k00, k01, k02, \
     k03, k04, k05, \
