0fc4c94c3ecce20fed5b19165aea24ba86dae6a2
datafx <2432997+datafx@users.noreply.github.com>
2021-01-17 02:21:31 -0500

    Update config.h

--- keyboards/xo87/rgb/config.h
+++ keyboards/xo87/rgb/config.h
@@ -25,7 +25,6 @@
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Monstargear
 #define PRODUCT         XO87
-#define DESCRIPTION     Monstargear XO87
 
 /* key matrix size */
 #define MATRIX_ROWS 6
