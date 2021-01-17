2878f8cb7cc8283d91b5ab5ef99d3642f68a0aa3
Signynt <67801159+Signynt@users.noreply.github.com>
2021-01-17 15:31:52 +0100

    keymap changes

--- keyboards/idobo/keymaps/bonnies_schreibwerk/keymap.c
+++ keyboards/idobo/keymaps/bonnies_schreibwerk/keymap.c
@@ -1,4 +1,4 @@
-// qmk compile -kb idobo -km bonnies_schreibwerk
+// qmk flash -kb idobo -km bonnies_schreibwerk
 
 
 //  .----------------.  .----------------.  .-----------------. .-----------------. .----------------.
@@ -51,9 +51,9 @@ LT(LAYER2, KC_CAPS), KC_NO,     KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO, 
 // Numpad
   [LAYER3] = LAYOUT_ortho_5x15(
      KC_NO,   KC_NO,   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,  KC_F12,  KC_NO,  \
-     KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,      KC_NO,     KC_NO,   KC_NO,   KC_NO,  \
-     KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_4,    KC_5,    KC_6,      KC_NO,     KC_NO,   KC_NO,   KC_NO, \
-     KC_LSFT, KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,      KC_RSFT,   KC_NO,   KC_NO,   KC_NO, \
-     KC_NO,   KC_NO,   KC_NO,     KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_NO,     KC_NO,     KC_NO,   KC_NO,   KC_RST  \
+     KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,     KC_NO,     KC_NO,   KC_NO,   KC_NO,  \
+     KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_4,    KC_5,    KC_6,    KC_NO,     KC_NO,     KC_NO,   KC_NO,   KC_NO, \
+     KC_LSFT, KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,     KC_RSFT,   KC_NO,   KC_NO,   KC_NO, \
+     KC_NO,   KC_NO,   KC_NO,     KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_NO,     KC_NO,     KC_NO,   RGB_TOG, KC_RST  \
   ),
 };
