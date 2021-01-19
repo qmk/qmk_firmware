1e1b469fa2f9aa7480b4c1f1d1f8eb5b0c0c8ce5
Ryan <fauxpark@gmail.com>
2021-01-20 09:56:41 +1100

    Remove `DESCRIPTION`, K-M (#11619)

--- keyboards/keebio/quefrency/rev2/config.h
+++ keyboards/keebio/quefrency/rev2/config.h
@@ -24,7 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0200
 #define MANUFACTURER    Keebio
 #define PRODUCT         Quefrency Rev. 2
-#define DESCRIPTION     Split 60/65 percent staggered keyboard
 
 /* key matrix size */
 // Rows are doubled-up
