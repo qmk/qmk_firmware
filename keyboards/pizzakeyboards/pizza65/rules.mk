efdc8ec11dc27070640ebfc593c9c52cadaa4068
mm0nte <71832926+mm0nte@users.noreply.github.com>
2021-01-19 11:35:58 +0100

    Update rules.mk

--- keyboards/pizzakeyboards/pizza65/rules.mk
+++ keyboards/pizzakeyboards/pizza65/rules.mk
@@ -15,4 +15,4 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 
-LAYOUTS = 65_ansi_blocker 65_iso_blocker 65_ansi_blocker_doublebs 65_iso_blocker_doublebs
+LAYOUTS = LAYOUT_65_ansi_blocker LAYOUT_65_iso_blocker LAYOUT_65_ansi_blocker_doublebs LAYOUT_65_iso_blocker_doublebs
