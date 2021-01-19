caf74972d40b2632624a776cc62bf27bc7aa047a
Sam Hudson <samsamm777@gmail.com>
2021-01-19 20:08:07 +0000

    fixed default keymap

--- keyboards/shk9/keymaps/default/keymap.c
+++ keyboards/shk9/keymaps/default/keymap.c
@@ -15,14 +15,8 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
+    [0] = LAYOUT_3x3( /* Base */
         KC_1, KC_2, KC_3,
         KC_4, KC_5, KC_6,
         KC_7, KC_8, KC_9
