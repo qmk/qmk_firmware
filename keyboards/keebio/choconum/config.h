34d36f0e804f1eb5f64f544eab60de09aab703d9
fauxpark <fauxpark@gmail.com>
2021-01-20 00:56:49 +1100

    Remove `DESCRIPTION`, K-M

--- keyboards/keebio/choconum/config.h
+++ keyboards/keebio/choconum/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0100
 #define MANUFACTURER Keebio
 #define PRODUCT Choconum
-#define DESCRIPTION Low-Profile Numpad
 
 /* key matrix size */
 #define MATRIX_ROWS 5
