8d24f28528901bf2c32bdd684455cfdd6fe8e628
Eithan Shavit <eithan@fb.com>
2021-01-18 23:44:35 -0800

    Breeze Rev1

--- keyboards/afternoonlabs/breeze/rev1/config.h
+++ keyboards/afternoonlabs/breeze/rev1/config.h
@@ -16,9 +16,32 @@
 
 #pragma once
 
-#ifdef KEYBOARD_afternoonlabs_breeze_rev0
-#    include "rev0.h"
-#endif
-#ifdef KEYBOARD_afternoonlabs_breeze_rev1
-#    include "rev1.h"
-#endif
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0x616C
+#define PRODUCT_ID 0x0001
+#define DEVICE_VER 0x0001
+#define MANUFACTURER AfternoonLabs
+#define PRODUCT Breeze
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 9
+
+// wiring of each half
+#define MATRIX_ROW_PINS \
+    { F4, F5, F6, F7, B1 }
+#define MATRIX_COL_PINS \
+    { B2, D1, D0, D4, C6, D7, E6, B4, B5 }
+
+#define SPLIT_HAND_PIN B3
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+#define SOFT_SERIAL_PIN D2
+
+#define DIODE_DIRECTION COL2ROW
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COL 5
