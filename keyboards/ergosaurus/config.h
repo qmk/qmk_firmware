9e214f8276e72c4abf1f948e5a5efa08144505da
Carlos Filoteo <c.filoteo12@gmail.com>
2021-01-19 23:30:50 -0700

    Update RGB_DI_PIN to match breakout on pcb

--- keyboards/ergosaurus/config.h
+++ keyboards/ergosaurus/config.h
@@ -58,7 +58,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define BACKLIGHT_BREATHING
 // #define BACKLIGHT_LEVELS 3
 
-// #define RGB_DI_PIN E2
+// #define RGB_DI_PIN B6
 // #ifdef RGB_DI_PIN
 //   #define RGBLED_NUM 16
 //   #define RGBLIGHT_HUE_STEP 8
