5cabbe685426884c8ae0e3ff790cb0cecf8a0f39
Sam Hudson <samsamm777@gmail.com>
2021-01-20 10:26:27 +0000

    changed layout to ortho

--- keyboards/shk9/shk9.h
+++ keyboards/shk9/shk9.h
@@ -26,7 +26,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT_3x3( \
+#define LAYOUT_ortho_3x3( \
     K00, K01, K02,  \
     K10, K11, K12,  \
     K20, K21, K22   \
