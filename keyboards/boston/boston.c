868603ec4a83719762557af3f13cfec2341fbe98
bluepylons <jwang@fluentbio.com>
2021-01-21 22:46:18 -0500

    Implemented lock LED changes as suggested by drashna

--- keyboards/boston/boston.c
+++ keyboards/boston/boston.c
@@ -16,28 +16,9 @@
 
 #include "boston.h"
 
-void keyboard_pre_init_kb(void) {
-    setPinOutput(A0);
-    setPinOutput(A1);
-    setPinOutput(A2);
-
-    keyboard_pre_init_user();
-}
-
 void keyboard_post_init_kb(void) {
     /* this is to set an LED color at startup so it's not some random color*/
     rgblight_enable_noeeprom(); 
     rgblight_sethsv_noeeprom(0, 0, 128); 
     keyboard_post_init_user();
-}
-
-
-bool led_update_kb(led_t led_state) {
-    if (led_update_user(led_state)) {
-        writePin(A0, led_state.num_lock);
-        writePin(A1, led_state.caps_lock);
-        writePin(A2, led_state.scroll_lock);
-    }
-
-    return true;
 }
