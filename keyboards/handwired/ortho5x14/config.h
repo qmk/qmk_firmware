6b0898401415a11223f4fc2fbd787a5b3717c6e9
Richard Nunez <richard.nunez@macrossplusinc.com>
2021-01-22 12:44:30 -0600

    Removed DESCRIPTION attribute for PR #11597.

--- keyboards/handwired/ortho5x14/config.h
+++ keyboards/handwired/ortho5x14/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    MPInc
 #define PRODUCT         Ortho 5x14
-#define DESCRIPTION     Handwired 5x14 ortholinear keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
