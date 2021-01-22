ea5553808015a75c22d6beee5e7c3561352964ce
andresteare <andresteare@gmail.com>
2021-01-22 00:25:15 -0300

    few changes

--- keyboards/handwired/ibm_wheelwriter/config.h
+++ keyboards/handwired/ibm_wheelwriter/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 REPLACE_WITH_YOUR_NAME
+/* Copyright 2021 andresteare
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    %MANUFACTURER%
+#define MANUFACTURER    ibm
 #define PRODUCT         ibmwheelwriter2
 
 /* key matrix size */
