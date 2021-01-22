f145cec83c1e15419e472f279a83668a24474e5c
haierwangwei2005 <69899561+haierwangwei2005@users.noreply.github.com>
2021-01-22 16:01:21 +0800

    Update config.h

--- keyboards/latinpadble/config.h
+++ keyboards/latinpadble/config.h
@@ -50,15 +50,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #define RGBLIGHT_EFFECT_KNIGHT_OFFSET 20
 
 /*
-#ifdef RGB_MATRIX_ENABLE
+//#ifdef RGB_MATRIX_ENABLE
 //#define DRIVER_LED_TOTAL RGBLED_NUM
-#define RGB_MATRIX_KEYPRESSES
-#endif
+//#define RGB_MATRIX_KEYPRESSES
+//#endif
 */
-//#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-//#define RGB_MATRIX_KEYPRESSES         // reacts to keypresses
+// #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #define RGB_MATRIX_KEYPRESSES         // reacts to keypresses
 // #define RGB_MATRIX_KEYRELEASES        // reacts to keyreleases (instead of keypresses)
-//#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_ALPHAS_MODS // Sets the default mode, if none has been set
+// #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_ALPHAS_MODS // Sets the default mode, if none has been set
 // #define RGBLIGHT_HUE_STEP 12        // Units to step when in/decreasing hue
 // #define RGBLIGHT_SAT_STEP 25        // Units to step when in/decreasing saturation
 // #define RGBLIGHT_VAL_STEP 12        // Units to step when in/decreasing value (brightness)
@@ -66,7 +66,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 // #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 // #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 // #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-//#define RGBLIGHT_ANIMATIONS             // Run RGB animations
+// #define RGBLIGHT_ANIMATIONS             // Run RGB animations
 // #define RGBLIGHT_ANIMATIONS             // Enable all additional animation modes.
 // #define RGBLIGHT_EFFECT_ALTERNATING     // Enable alternating animation mode.
 #define RGBLIGHT_EFFECT_BREATHING       // Enable breathing animation mode.
@@ -83,9 +83,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 //#define MATRIX_HAS_GHOST
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+//#define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+//#define LOCKING_RESYNC_ENABLE
 
 
 #define OLED_FONT_H "./lib/glcdfont.c"
