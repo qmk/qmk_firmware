f83d90ac3513a3e50634b37e18d26802aa473685
haierwangwei2005 <69899561+haierwangwei2005@users.noreply.github.com>
2021-01-23 15:52:06 +0800

    Update rules.mk

--- keyboards/latinpadble/rules.mk
+++ keyboards/latinpadble/rules.mk
@@ -23,7 +23,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096 -DCATERINA_BOOTLOADER
 
 # Build Options
 #   change yes to no to disable
-BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -33,15 +33,12 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality on B7 by default
-
 MIDI_ENABLE = no            # MIDI controls
 UNICODE_ENABLE = no         # Unicode
-
 AUDIO_ENABLE = no           # Audio output on port C6
 BLUETOOTH = AdafruitBLE
 LAYOUTS = pad
 RGBLIGHT_ENABLE = yes
-
 OLED_DRIVER_ENABLE = yes
 ENCODER_ENABLE = yes
 
