261407207e71146ad8443917248a7638dcf8d144
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-01-23 14:02:21 -0800

    move USB indicator code to keyboard level

--- keyboards/primekb/prime_e/prime_e.c
+++ keyboards/primekb/prime_e/prime_e.c
@@ -15,8 +15,38 @@
  */
 #include "prime_e.h"
 
-void led_set_kb(uint8_t usb_led) {
-	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+void matrix_init_kb(void) {
+    // set CapsLock LED to output and low
+    setPinOutput(B1);
+    writePinLow(B1);
+    // set NumLock LED to output and low
+    setPinOutput(B2);
+    writePinLow(B2);
+    // set ScrollLock LED to output and low
+    setPinOutput(B3);
+    writePinLow(B3);
 
-	led_set_user(usb_led);
+    matrix_init_user();
+}
+
+void led_set_kb(uint8_t usb_led) {
+    // put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+        writePinHigh(B2);
+    } else {
+        writePinLow(B2);
+    }
+    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+        writePinHigh(B1);
+    } else {
+        writePinLow(B1);
+    }
+    /*
+      if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
+        writePinHigh(B3);
+      } else {
+        writePinLow(B3);
+      }*/
+
+    led_set_user(usb_led);
 }
