353c1f07040367357567f4bf08fcb63f2cb004cc
juan <jpuerto96@hotmail.com>
2021-01-23 09:31:27 -0500

    Fixed LEDs

--- keyboards/V4N4G0RTH0N/config.h
+++ keyboards/V4N4G0RTH0N/config.h
@@ -145,6 +145,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
 
-#define LED_NUM_LOCK_PIN B4
-#define LED_CAPS_LOCK_PIN D7
-#define LED_SCROLL_LOCK_PIN D6
+//#define LED_NUM_LOCK_PIN B4
+#define LED_CAPS_LOCK_PIN D6
+//#define LED_SCROLL_LOCK_PIN D6
