cb4fc275e02fc48a610b063e828d92365de4c0cd
Weirdo <1248314361@qq.com>
2021-01-25 00:12:50 +0800

    Update keyboards/geminate60/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/geminate60/rules.mk
+++ keyboards/geminate60/rules.mk
@@ -4,17 +4,16 @@ MCU = STM32F303
 # Build Options
 #   change yes to no to disable
 #
-ENCODER_ENABLE = no
-BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
-EXTRAKEY_ENABLE = yes        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug 调试开关
-COMMAND_ENABLE = yes         # Commands for debug and configuration
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no        # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes            # USB Nkey Rollover
-RGBLIGHT_ENABLE = yes
-VIA_ENABLE = NO
-
-
-
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
