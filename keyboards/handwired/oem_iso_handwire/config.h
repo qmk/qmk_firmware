8267abfdaa39a27629ec8ebbab239ecd767f27b3
andresteare <andresteare@gmail.com>
2021-01-24 16:04:48 -0300

    updates

--- keyboards/handwired/oem_iso_handwire/config.h
+++ keyboards/handwired/oem_iso_handwire/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x8F99
-#define PRODUCT_ID      0x9875
+#define VENDOR_ID       0x9856  // "Gamdias"
+#define PRODUCT_ID      0x7070
 #define DEVICE_VER      0x0001
 #define MANUFACTURER   Gamdias
 #define PRODUCT         teclado_oscar
