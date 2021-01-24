a24e79d54ad4af5b171efafcf4addd0915cf79e4
Weirdo <1248314361@qq.com>
2021-01-24 23:03:34 +0800

    Update geminate60.c

--- keyboards/geminate60/geminate60.c
+++ keyboards/geminate60/geminate60.c
@@ -1,4 +1,4 @@
-  /* Copyright %YEAR% %YOUR_NAME% 
+  /* Copyright 2020 Weirdo
   * 
   * This program is free software: you can redistribute it and/or modify 
   * it under the terms of the GNU General Public License as published by 
