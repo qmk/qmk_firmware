d0fdaea924be39475027d0f6f76e45df3845583c
Weirdo <1248314361@qq.com>
2021-01-25 00:09:58 +0800

    Update keyboards/geminate60/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/geminate60/keymaps/default/keymap.c
+++ keyboards/geminate60/keymaps/default/keymap.c
@@ -17,7 +17,10 @@
 
 
 // [Keymaps] -----------------------------------------------------------------//
-enum layer_names { _BASE, _FN ,_FN1 ,_FN2 };
+enum layer_names {
+    _BASE,
+    _FN
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_BASE] = LAYOUT_ortho_5x15(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
