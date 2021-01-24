e5bffb1be6d5391a3966aeffacae3c4897be3f4c
Weirdo <1248314361@qq.com>
2021-01-24 14:31:41 +0800

    Update config.h

--- keyboards/geminate60/config.h
+++ keyboards/geminate60/config.h
@@ -1,6 +1,19 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
+ /* Copyright %YEAR% %YOUR_NAME% 
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
+#pragma once
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
@@ -36,7 +49,4 @@
 #define LOCKING_RESYNC_ENABLE
 
 
-#ifdef CONSOLE_ENABLE
-//#define DEBUG_MATRIX_SCAN_RATE
-#endif
-#endif
+
