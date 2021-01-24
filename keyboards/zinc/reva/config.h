f3a473a6cb29d316f4e19aab0911922dafaeacf7
monksoffunk <osamu@sho-k.com>
2021-01-25 01:57:46 +0900

    Change Manufacturer

--- keyboards/zinc/reva/config.h
+++ keyboards/zinc/reva/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0x04D8
 #define PRODUCT_ID      0xEA3B
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    monksoffunk
+#define MANUFACTURER    25KEYS
 #define PRODUCT         zinc rev.A
 
 #define TAPPING_FORCE_HOLD
