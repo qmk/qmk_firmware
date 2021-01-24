01e7a8c66e73119ff474e378c733aab6a9082dba
melonbred <61572569+melonbred@users.noreply.github.com>
2021-01-23 17:14:56 -0800

    Update keymap.c

--- keyboards/program_yoink/ortho/keymaps/ortho_split/keymap.c
+++ keyboards/program_yoink/ortho/keymaps/ortho_split/keymap.c
@@ -29,10 +29,10 @@ enum combo_events {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_split(
-        KC_ESC,        KC_Q,    KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC, KC_MPLY,
-        CTL_T(KC_TAB), KC_A,    KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_QUOT, KC_ENT,  KC_PGUP,
-        KC_LSFT,       KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_SLSH,   KC_PGDN,
-        KC_LCTL,       KC_LALT, MO(_LAYER2),  KC_BSPC,  LT(_LAYER1, KC_SPC), KC_RGUI, KC_RALT,  KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,        KC_Q,    KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC, KC_MPLY,
+        CTL_T(KC_TAB), KC_A,    KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,    KC_QUOT, KC_ENT,  KC_PGUP,
+        KC_LSFT,       KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_PGDN,
+        KC_LCTL,       KC_LALT, MO(_LAYER2),  KC_BSPC,  LT(_LAYER1, KC_SPC), KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_LAYER1] = LAYOUT_ortho_split(
