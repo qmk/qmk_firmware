d39655e2abf30f4e69c6fea7480e20d6430a7e40
M4cs <mabridgland@protonmail.com>
2021-01-26 23:16:21 -0500

    Fix rules.mk

--- keyboards/dekunukem/duckypad/keymaps/via/rules.mk
+++ keyboards/dekunukem/duckypad/keymaps/via/rules.mk
@@ -1,4 +1,4 @@
 VIA_ENABLE = yes
-BOOTMAGIC_ENABLE = lite
+BOOTMAGIC_ENABLE = lite # Virtual DIP switch configuration
+RGBLIGHT_ENABLE = yes # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no
-RGBLIGHT_ENABLE = yes
