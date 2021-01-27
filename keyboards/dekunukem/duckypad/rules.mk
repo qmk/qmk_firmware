be40c39addadcd38798ab080e5cdf1ff1941c0c8
M4cs <mabridgland@protonmail.com>
2021-01-26 23:14:09 -0500

    Fix rules.mk

--- keyboards/dekunukem/duckypad/rules.mk
+++ keyboards/dekunukem/duckypad/rules.mk
@@ -18,8 +18,8 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 
-# WS2812_DRIVER = bitbang
-# RGB_MATRIX_ENABLE = yes
-# RGB_MATRIX_DRIVER = WS2812
+WS2812_DRIVER = bitbang
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
 
 OLED_DRIVER_ENABLE = yes
