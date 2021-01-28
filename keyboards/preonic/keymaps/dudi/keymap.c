17c841c2fad93f8d7dbdd00fed7c6cc3de8ab64e
Dudi <Dudi.b@claroty.com>
2021-01-28 14:47:59 +0200

    better implam

--- keyboards/preonic/keymaps/dudi/keymap.c
+++ keyboards/preonic/keymaps/dudi/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_MUTE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_BSPC, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,  \
@@ -235,35 +235,36 @@ uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
 
-/*
-void encoder_update_user(uint8_t index, bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      register_code(KC_VOLD);
-      unregister_code(KC_VOLD);
-    } else {
-      register_code(KC_VOLU);
-      unregister_code(KC_VOLU);
-    }
-  }
-}
-*/ 
+
+// void encoder_update_user(uint8_t index, bool clockwise) {
+//   if (muse_mode) {
+//     if (IS_LAYER_ON(_RAISE)) {
+//       if (clockwise) {
+//         muse_offset++;
+//       } else {
+//         muse_offset--;
+//       }
+//     } else {
+//       if (clockwise) {
+//         muse_tempo+=1;
+//       } else {
+//         muse_tempo-=1;
+//       }
+//     }
+//   } else {
+//     if (clockwise) {
+//       register_code(KC_VOLD);
+//       unregister_code(KC_VOLD);
+//     } else {
+//       register_code(KC_VOLU);
+//       unregister_code(KC_VOLU);
+//     }
+//   }
+// }
+ 
 
 void encoder_update_user(uint8_t index, bool clockwise) {
+        tap_code(KC_VOLU);
         if (clockwise) {
            // tap_code(KC_VOLD);
       tap_code(KC_VOLU);
@@ -326,7 +327,11 @@ bool music_mask_user(uint16_t keycode) {
 }
 
 bool led_update_user(led_t led_state) {
-    rgblight_toggle();
+  static uint8_t caps_state = 0;
+    if (caps_state != led_state.caps_lock) {
+        led_state.caps_lock ? rgblight_enable() : rgblight_disable();
+        caps_state = led_state.caps_lock;
+    }
     return true;
 }
 
