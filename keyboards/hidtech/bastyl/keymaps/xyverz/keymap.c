51349e8246d531f316e7cd238e4cdd678ae5b23e
Ian Sterling <xyverz@gmail.com>
2021-01-28 11:43:10 -0800

    Minor tweaks to keymap.

--- keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
-  KC_LSFT, SCLNCTL, QUEALT,  KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    VEEALT,  ZEDCTL,  KC_RSFT, \
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    VEEALT,  ZEDCTL,  KC_RSFT, \
                              LOWER,   KC_BSPC, KC_DEL,      KC_ENT,  KC_SPC,  RAISE, \
                                       KC_LCTL, KC_LALT,     KC_RGUI, KC_RCTL
   ),
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
-  KC_LSFT, SCLNCTL, QUEALT,  KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    VEEALT,  ZEDCTL,  KC_RSFT, \
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    VEEALT,  ZEDCTL,  KC_RSFT, \
                              LOWER,   KC_BSPC, DELGUI,      KC_ENT,  KC_SPC,  RAISE, \
                                       KC_LCTL, KC_LALT,     KC_RGUI, KC_RCTL
   ),
