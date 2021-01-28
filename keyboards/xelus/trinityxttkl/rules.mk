e0baf747c3bf8f1cf315d516f9a400900b14150d
Xelus22 <preyas22@gmail.com>
2021-01-28 11:51:23 +1100

    amp32  initial change

--- keyboards/xelus/trinityxttkl/rules.mk
+++ keyboards/xelus/trinityxttkl/rules.mk
@@ -18,3 +18,6 @@ RGBLIGHT_ENABLE = no
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+# Wildcard to allow APM32 MCU 
+DFU_SUFFIX_ARGS = -p FFFF -v FFFF
