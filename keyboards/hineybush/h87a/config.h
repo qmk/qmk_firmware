811974c00efc32094ce1974c577d3c93a5c72988
itspngu <34752364+itspngu@users.noreply.github.com>
2021-01-30 19:10:06 +0000

    added h87a

--- keyboards/hineybush/h87a/config.h
+++ keyboards/hineybush/h87a/config.h
@@ -23,6 +23,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT         h87a
 #define DESCRIPTION     QMK-programmable TKL PCB for custom keyboards
 
+#define VIAL_KEYBOARD_UID { 0x02, 0x4B, 0x38, 0xA3, 0x30, 0x79, 0x76, 0x3E }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
+
 #define CONFIG_H
 
 #include "config_common.h"
