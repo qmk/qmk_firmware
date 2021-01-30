1e55a186ce0af717e32c8e7efa149c57a3a7d943
datafx <2432997+datafx@users.noreply.github.com>
2021-01-30 01:00:06 -0500

    Remove abandoned code
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/monstargear/xo87/solderable/keymaps/default/keymap.c
+++ keyboards/monstargear/xo87/solderable/keymaps/default/keymap.c
@@ -51,37 +51,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
 };
-
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-	} else {
-
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-
-	} else {
-
-	}
-
-}
