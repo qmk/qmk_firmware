923002bcede4ecb0897837b657328c94d1a0ca45
Galib <62521946+polycarbdiet@users.noreply.github.com>
2021-01-30 01:51:28 -0500

    Update keyboards/polycarbdiet/s20/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/polycarbdiet/s20/keymaps/default/keymap.c
+++ keyboards/polycarbdiet/s20/keymaps/default/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, BL_OFF,  BL_DEC,  BL_BRTG,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-    [3] = LAYOUT_MacroPad(
+    [3] = LAYOUT_ortho_5x4(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
