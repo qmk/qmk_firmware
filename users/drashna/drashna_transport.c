8ca36d0121e19ec4541f3cabb1c612360b0e275a
Drashna Jael're <drashna@live.com>
2021-01-31 13:03:23 -0800

    Fix typo in drashna_transport

--- users/drashna/drashna_transport.c
+++ users/drashna/drashna_transport.c
@@ -1,4 +1,4 @@
-e/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
