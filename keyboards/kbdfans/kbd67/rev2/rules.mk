5a9c35a549ab6af4cfd876a32320e1bd0867e40e
Andy Freeland <andy@andyfreeland.net>
2021-01-30 18:51:48 -0800

    [Keyboard] Add 'LAYOUT_65_ansi_split_bs' support to KBDfans KBD67 rev2
    This is already supported by VIA.

--- keyboards/kbdfans/kbd67/rev2/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/rules.mk
@@ -31,4 +31,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
-LAYOUTS = 65_ansi 65_iso 65_ansi_blocker 65_ansi_blocker_split_bs
+LAYOUTS = 65_ansi 65_iso 65_ansi_blocker 65_ansi_blocker_split_bs 65_ansi_split_bs
