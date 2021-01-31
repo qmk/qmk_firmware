8558f4f367d6cb10ba6dd51d67c149e0be553f56
andresteare <andresteare@gmail.com>
2021-01-31 20:40:12 -0300

    corrections

--- keyboards/handwired/ibm_wheelwriter/keymaps/via/keymap.c
+++ keyboards/handwired/ibm_wheelwriter/keymaps/via/keymap.c
@@ -13,10 +13,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
     [0] = LAYOUT(
         KC_ESC,        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,             MO(1),
         KC_MPLY,       KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_ENT,       KC_VOLU,
@@ -45,4 +45,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,       KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,
         KC_TRNS,                                KC_TRNS,                            KC_TRNS,                                                       KC_TRNS,      KC_TRNS
     ),
+
 };
