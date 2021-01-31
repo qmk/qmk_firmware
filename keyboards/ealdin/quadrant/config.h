d0bb615cf0d332f2931fe573fc52c8ad13fdf2b3
Nuno <me@nuno99costa.xyz>
2021-01-31 22:34:34 +0000

    Fixed encoder support on Quadrant

--- keyboards/ealdin/quadrant/config.h
+++ keyboards/ealdin/quadrant/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-#define NUMBER_OF_ENCODERS 1
+#define ENCODER_DIRECTION_FLIP
 #define ENCODERS_PAD_A { F1 }
 #define ENCODERS_PAD_B { D5 }
 #define ENCODER_RESOLUTION 4
