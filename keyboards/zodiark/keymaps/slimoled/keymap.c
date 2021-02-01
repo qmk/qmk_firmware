7850073a5e3af8dc7ea126192edfd5a8a29fca62
Aleblazer <60912320+Aleblazer@users.noreply.github.com>
2021-01-31 20:57:01 -0600

    Clean up

--- keyboards/zodiark/keymaps/slimoled/keymap.c
+++ keyboards/zodiark/keymaps/slimoled/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,  KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
       KC_ADJUST, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,   KC_DEL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      KC_LCTL, KC_LALT, KC_LGUI, LCA(KC_DEL), KC_LOWER, KC_SPC, KC_ENT,  KC_ENT,  KC_SPC,   KC_RAISE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_LCTL, KC_LALT, KC_LGUI, LALT(KC_TAB), KC_LOWER, KC_SPC, KC_ENT,  KC_ENT,  KC_SPC,   KC_RAISE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
     ),
 		
 	[_LOWER] = LAYOUT( \
