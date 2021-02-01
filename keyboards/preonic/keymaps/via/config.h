6fbe939af7627182c5939cf01f1f275c611fcd39
George Wietor <george@wietor.com>
2021-02-01 03:53:03 -0500

    remove colemak and dvorak sounds

--- keyboards/preonic/keymaps/via/config.h
+++ keyboards/preonic/keymaps/via/config.h
@@ -6,10 +6,7 @@
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
-                                }
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND) }
 #endif
 
 #define MUSIC_MASK (keycode != KC_NO)
