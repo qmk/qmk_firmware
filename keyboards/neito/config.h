f9e2c180b8447e7d36a17ffd8206596a62387419
Oliver Granlund <olli.granlund@gmail.com>
2021-02-01 20:43:10 +0200

    rgb brightness limit

--- keyboards/neito/config.h
+++ keyboards/neito/config.h
@@ -36,7 +36,7 @@
 #define UNUSED_PINS
 
 #define LED_CAPS_LOCK_PIN B6
-#define LED_PIN_ON_STATE 0
+/* #define LED_PIN_ON_STATE 0 */
 
 #define BACKLIGHT_PIN B5
 #define BACKLIGHT_LEVELS 5
@@ -49,3 +49,4 @@
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_LIMIT_VAL 200
