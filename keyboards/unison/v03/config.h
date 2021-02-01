eb53b160d5930d6152e4786c95dea97db8d463f7
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-01 11:29:43 +0900

    Update: MIDIなしのとき、AUDIOのフル機能がファームウェアサイズ内に収まることをチェック。

--- keyboards/unison/v03/config.h
+++ keyboards/unison/v03/config.h
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define B6_AUDIO
   #define C6_AUDIO
   #define AUDIO_CLICKY
-  #define NO_MUSIC_MODE
+//   #define NO_MUSIC_MODE  // MIDI使わないなら、AUDIO_CLICKYとMUSIC_MODEを同時使用可能
 #endif
 
 /* RGB Lighting */
@@ -69,13 +69,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   /*== all animations enable ==*/
   // #define RGBLIGHT_ANIMATIONS
   /*== or choose animations ==*/
-  #define RGBLIGHT_EFFECT_BREATHING
+//   #define RGBLIGHT_EFFECT_BREATHING
   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
   // #define RGBLIGHT_EFFECT_SNAKE
-  #define RGBLIGHT_EFFECT_KNIGHT
+//   #define RGBLIGHT_EFFECT_KNIGHT
   // #define RGBLIGHT_EFFECT_CHRISTMAS
-  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
   // #define RGBLIGHT_EFFECT_RGB_TEST
   // #define RGBLIGHT_EFFECT_ALTERNATING
   // /*== customize breathing effect ==*/
