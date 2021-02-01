1a7e53247e561e95c911f158f7725befc3703ba9
Adam Honse <calcprogrammer1@gmail.com>
2021-01-31 22:29:37 -0600

    Update timer resolution to 32-bit

--- keyboards/redragon/k556/chconf.h
+++ keyboards/redragon/k556/chconf.h
@@ -43,7 +43,7 @@
  * @note    Allowed values are 16 or 32 bits.
  */
 #    if !defined(CH_CFG_ST_RESOLUTION)
-#        define CH_CFG_ST_RESOLUTION 16
+#        define CH_CFG_ST_RESOLUTION 32
 #    endif
 
 /**
