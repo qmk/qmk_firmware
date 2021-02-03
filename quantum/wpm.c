389f2bd0280922b61b63a4cdfa863b57041ffe7a
Drashna Jael're <drashna@live.com>
2021-02-03 10:57:57 -0800

    Fix fix for maths

--- quantum/wpm.c
+++ quantum/wpm.c
@@ -69,7 +69,7 @@ __attribute__((weak)) uint8_t wpm_regress_count(uint16_t keycode) {
 void update_wpm(uint16_t keycode) {
     if (wpm_keycode(keycode)) {
         if (wpm_timer > 0) {
-            current_wpm = ((60000 / timer_elapsed(wpm_timer) / WPM_ESTIMATED_WORD_SIZE) - current_wpm) * wpm_smoothing + current_wpm;
+            current_wpm += ((60000 / timer_elapsed(wpm_timer) / WPM_ESTIMATED_WORD_SIZE) - current_wpm) * wpm_smoothing;
         }
         wpm_timer = timer_read();
     }
@@ -80,7 +80,7 @@ void update_wpm(uint16_t keycode) {
 
 void decay_wpm(void) {
     if (timer_elapsed(wpm_timer) > 1000) {
-        current_wpm = (0 - current_wpm) * wpm_smoothing * current_wpm;
+        current_wpm += (-current_wpm) * wpm_smoothing;
         wpm_timer   = timer_read();
     }
 }
