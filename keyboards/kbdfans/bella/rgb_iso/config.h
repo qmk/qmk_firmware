701f808a2eb92a5ee37cd4871fc3f9471253728a
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-02-04 13:42:39 -0800

    Apply suggestions from code review
    kbdfans/bella/rgb_iso: update keyboard name
    
    Changes remaining instances of "BELLA_RGB_ISO" to "BELLA RGB ISO".

--- keyboards/kbdfans/bella/rgb_iso/config.h
+++ keyboards/kbdfans/bella/rgb_iso/config.h
@@ -21,7 +21,7 @@
 #define PRODUCT_ID      0x0009
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    KBDFANS
-#define PRODUCT         BELLA_RGB_ISO
+#define PRODUCT         BELLA RGB ISO
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 16
