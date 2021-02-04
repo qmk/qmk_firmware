cc52bbd5d46116c3e046b014eb6d041d474db4cf
Ian Sterling <xyverz@gmail.com>
2021-02-03 21:37:21 -0800

    Fixes as requested by @fauxpark
    Removed backslash separators at the end of each line in the
    layout section of the keymap.

--- keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/xyverz/keymap.c
@@ -37,47 +37,47 @@ enum custom_keycodes { WINDOWS = SAFE_RANGE, MAC, ADJUST };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_WINDOWS] = LAYOUT(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
-  KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-                             LOWER,   KC_BSPC, KC_DEL,      KC_ENT,  KC_SPC,  RAISE, \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+  KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+                             LOWER,   KC_BSPC, KC_DEL,      KC_ENT,  KC_SPC,  RAISE,
                                       KC_LCTL, KC_LALT,     KC_RGUI, KC_RCTL
   ),
 
 [_MAC] = LAYOUT(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
-  KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-                             LOWER,   KC_BSPC, DELGUI,      KC_ENT,  KC_SPC,  RAISE, \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+  KC_GRV,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+                             LOWER,   KC_BSPC, DELGUI,      KC_ENT,  KC_SPC,  RAISE,
                                       KC_LCTL, KC_LALT,     KC_RGUI, KC_RCTL
   ),
 
 [_LOWER] = LAYOUT(
-  KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-  RESET,   _______, _______, KC_UP,   _______, _______,     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, \
-  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME,     KC_PGUP, _______, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
-  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,      KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
-                             _______, KC_DEL,  _______,     _______, _______, _______, \
+  KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, 
+  RESET,   _______, _______, KC_UP,   _______, _______,     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
+  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME,     KC_PGUP, _______, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,      KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+                             _______, KC_DEL,  _______,     _______, _______, _______,
                                       MACLOCK, _______,     _______, WINLOCK
   ),
 
 [_RAISE] = LAYOUT(
-  KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-  _______, _______, _______, KC_UP,   _______, _______,     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, RESET, \
-  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME,     KC_PGUP, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
-  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,      KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
-                             _______, KC_DEL,  _______,     _______, _______, _______, \
+  KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, 
+  _______, _______, _______, KC_UP,   _______, _______,     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, RESET,
+  KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME,     KC_PGUP, _______, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_END,      KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+                             _______, KC_DEL,  _______,     _______, _______, _______,
                                       MACLOCK, _______,     _______, WINLOCK
   ),
 
 [_ADJUST] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, \
-  _______, RESET,   _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,     _______, WINDOWS, MAC,     _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, \
-                             _______, _______, _______,     _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+  _______, RESET,   _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,     _______, WINDOWS, MAC,     _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+                             _______, _______, _______,     _______, _______, _______,
                                       _______, _______,     _______, _______
   ),
 
