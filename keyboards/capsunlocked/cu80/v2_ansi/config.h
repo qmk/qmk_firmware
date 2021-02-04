1e08e0629e4ac3a622095d93346adf8dd9b1a434
Rys Sommefeldt <rys@sommefeldt.com>
2021-02-04 21:33:07 +0000

    Give each CU80 v2 a different name for VIA

--- keyboards/capsunlocked/cu80/v2_ansi/config.h
+++ keyboards/capsunlocked/cu80/v2_ansi/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0081
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    CapsUnlocked
-#define PRODUCT         CU80
+#define PRODUCT         CU80 v2 ANSI
 
 /* key matrix size */
 #define MATRIX_ROWS 6
--- keyboards/capsunlocked/cu80/v2_iso/config.h
+++ keyboards/capsunlocked/cu80/v2_iso/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0080
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    CapsUnlocked
-#define PRODUCT         CU80
+#define PRODUCT         CU80 v2 ISO
 
 /* key matrix size */
 #define MATRIX_ROWS 6
