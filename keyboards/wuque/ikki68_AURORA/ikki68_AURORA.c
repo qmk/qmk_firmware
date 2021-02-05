d9b2e92f89f5599e8cfb2eb7d93c6f7d0ab72258
GG <72414103+spbgzh@users.noreply.github.com>
2021-02-05 15:28:31 +0300

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/wuque/ikki68_AURORA/ikki68_AURORA.c
+++ keyboards/wuque/ikki68_AURORA/ikki68_AURORA.c
@@ -15,21 +15,3 @@
  */
 
 #include "ikki68_AURORA.h"
-void matrix_init_kb(void) {
-    setPinOutput(F7);
-    matrix_init_user();
-}
-void matrix_scan_kb(void) {
-    matrix_scan_user();
-}
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    return process_record_user(keycode, record);
-}
-void led_set_kb(uint8_t usb_led) {
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-        writePinLow(F7);
-    } else {
-        writePinHigh(F7);
-    }
-    led_set_user(usb_led);
-}
