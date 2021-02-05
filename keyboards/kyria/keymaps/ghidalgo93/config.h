2c94f989953511b61e009f80998aed1eab2eaf70
Gerardo L Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-02-04 17:14:11 -0700

    [Keymap] add ghidalgo93 for kyria (#11663)
    * adding kyria/rev1 keymap
    
    * adding both hand config
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review

--- keyboards/kyria/keymaps/ghidalgo93/config.h
+++ keyboards/kyria/keymaps/ghidalgo93/config.h
@@ -25,23 +25,17 @@
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_SLEEP
 #endif
 
-// EC11K encoders have a different resolution than other EC11 encoders.
-// When using the default resolution of 4, if you notice your encoder skipping
-// every other tick, lower the resolution to 2.
+// encoder resolution from default 4 to 2
 #define ENCODER_RESOLUTION 2
 
-// The Leader key allows to flexibly assign macros to key sequences.
-#define LEADER_PER_KEY_TIMING
-#define LEADER_TIMEOUT 350
+// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
+// #define SPLIT_USB_DETECT
+// #define NO_USB_STARTUP_CHECK
 
-#define TAPPING_TERM 200
-
-// Allows to use either side as the master. Look at the documentation for info:
-// https://docs.qmk.fm/#/config_options?id=setting-handedness
+//allows for either side as the master.
 #define EE_HANDS
 
-// Allows media codes to properly register in macros and rotary encoder code
-#define TAP_CODE_DELAY 10
+
+
