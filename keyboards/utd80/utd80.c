4ad33e61d68477f103ac14752e3386779a7a4378
Dominic Gan <dominic.gan@accenture.com>
2021-02-05 16:03:07 +0800

    Add GPL license headers

--- keyboards/utd80/utd80.c
+++ keyboards/utd80/utd80.c
@@ -1,4 +1,3 @@
-#include "utd80.h"
 
 
 void matrix_init_kb(void) {
@@ -20,3 +19,19 @@ bool led_update_kb(led_t led_state) {
     }
     return true;
 }
+/* Copyright 2021 UTDKeyboard 
+ * 
+ * this program is free software: you can redistribute it and/or modify 
+ * it under the terms of the gnu general public license as published by 
+ * the free software foundation, either version 2 of the license, or 
+ * (at your option) any later version. 
+ * 
+ * this program is distributed in the hope that it will be useful, 
+ * but without any warranty; without even the implied warranty of 
+ * merchantability or fitness for a particular purpose.  see the 
+ * gnu general public license for more details. 
+ * 
+ * you should have received a copy of the gnu general public license 
+ * along with this program.  if not, see <http://www.gnu.org/licenses/>. 
+ */ 
+#include "utd80.h"
