64c2e8a93b1681fea1ae19f408e40fcbff25d76f
Juan Puerto <jpuerto96@hotmail.com>
2021-02-05 09:28:03 -0500

    Updating my keymaps

--- keyboards/handwired/dactyl_manuform/3x5/keymaps/jpuerto96/keymap.c
+++ keyboards/handwired/dactyl_manuform/3x5/keymaps/jpuerto96/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|                             |------|------+------+------+------|
  * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |
  * |------+------+------+-------------,                             ,-------------+------+------+------,
- *             |TAB+CTL|LOWER |SPC+GUI|                             |ENT+SFT|RAISE |BSP+CTL|
+ *               |SPC   |LOWER | TAB  |                             |ENT+SFT|RAISE |BSP+CTL|
  *               +------'-------------'                             '-------------'------+
  */
 
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    \
   ALT_A, GUI_S, SFT_L, CTL_F,    KC_G,                                      KC_H, CTL_J,   SFT_K,   GUI_L,  ALT_SC, \
   KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, \
-              KC_TAB, LOWER, KC_SPC,                                   KC_ENT, RAISE,   KC_BSPC \
+              LOWER, KC_SPC, KC_TAB,                                   KC_BSPC, KC_ENT,   RAISE \
 ),
 
 /* Raise
