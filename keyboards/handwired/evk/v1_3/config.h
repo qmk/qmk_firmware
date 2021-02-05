61618370591236983e305c6287449b95495217d7
YangPiCui <yang.pi.cui@gmail.com>
2021-02-05 10:16:56 +0800

    Update config.h
    comment row and column assignments

--- keyboards/handwired/evk/v1_3/config.h
+++ keyboards/handwired/evk/v1_3/config.h
@@ -39,13 +39,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS \
-    { B0, B1, B2, B3, B7, D0 }
+    { B0, B1, B2, B3, B7, D0 }  // row 0, 1, 2, ...
 #define MATRIX_COL_PINS \
-    { D1, D2, D3, C6, C7, F0, F1, F4, F5, F6, F7, B6, B5, B4, D7, D6 }
+    { D1, D2, D3, C6, C7, F0, F1, F4, F5, F6, F7, B6, B5, B4, D7, D6 }  // column 0, 1, 2, 3, ...
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION ROW2COL // the current flows into the rows and then out of the columns
+#define DIODE_DIRECTION ROW2COL // the positive current flows into the rows and then out of the columns negative.
 
 /* define whiche Pins to use for the status LEDs*/
 #define LED_CAPS_LOCK_PIN D4
