b6bf0de0d12ea997c89055fb2343e06c27cabe6b
Nick Blyumberg <nblyumberg@gmail.com>
2021-02-05 11:22:53 -0500

    Fixed the comment for consistency

--- keyboards/cannonkeys/ortho75/config.h
+++ keyboards/cannonkeys/ortho75/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xCA04
-#define PRODUCT_ID      0x4F75 //O 75
+#define PRODUCT_ID      0x4F75 //"O" 75
 #define DEVICE_VER      0x0001
 #define MANUFACTURER CannonKeys
 #define PRODUCT Ortho75
