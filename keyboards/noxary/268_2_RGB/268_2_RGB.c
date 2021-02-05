76a601dca7bc417f3c2f04b9d29c4b830215603a
Rozakiin <6567067+Rozakiin@users.noreply.github.com>
2021-02-05 09:29:06 +0000

    Update keyboards/noxary/268_2_RGB/268_2_RGB.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/noxary/268_2_RGB/268_2_RGB.c
+++ keyboards/noxary/268_2_RGB/268_2_RGB.c
@@ -14,17 +14,3 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "268_2_RGB.h"
-
-void matrix_init_kb(void) {
-  // put your keyboard start-up code here
-  // runs once when the firmware starts up
-  setPinOutput(B0);
-  matrix_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-    if(led_update_user(led_state)) {
-        writePin(B0, led_state.caps_lock);
-    }
-    return true;
-}
