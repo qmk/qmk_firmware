afea75234debfff0db617007f3be2bc1a5d26054
dsanchezseco <d.seco@protonmail.com>
2021-02-06 12:44:22 +0100

    Update keyboards/converter/ibm_terminal/keymaps/dsanchezseco/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/converter/ibm_terminal/keymaps/dsanchezseco/rules.mk
+++ keyboards/converter/ibm_terminal/keymaps/dsanchezseco/rules.mk
@@ -1,5 +1,1 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no
