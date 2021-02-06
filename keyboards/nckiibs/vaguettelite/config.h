3a12569413921744eca20f1566c2a8547d932237
noclew <noclew@users.noreply.github.com>
2021-02-05 20:32:20 -0500

    changed Bootmagic key of VaguetteLite as suggested

--- keyboards/nckiibs/vaguettelite/config.h
+++ keyboards/nckiibs/vaguettelite/config.h
@@ -116,4 +116,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Bootmagic Lite key configuration */
 #define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 6
+#define BOOTMAGIC_LITE_COLUMN 0
