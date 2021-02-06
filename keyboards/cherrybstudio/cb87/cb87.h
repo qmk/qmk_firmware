92b763ddfc4c83919dfffa443655612ea5fbc3b6
Duong Anh <56441863+duongaanh@users.noreply.github.com>
2021-02-06 14:20:13 +0700

    Update keyboards/cherrybstudio/cb87/cb87.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/cherrybstudio/cb87/cb87.h
+++ keyboards/cherrybstudio/cb87/cb87.h
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-#define LAYOUT_tkl_ansi( \
+#define LAYOUT_all( \
     K000,      K001, K002, K003, K004,   K005, K006, K007, K008,    K009, K100, K101, K102,    K103, K104, K105,  \
     K106, K107, K108, K109, K200, K201, K202, K203, K204, K205, K206, K207, K208,     K209,    K300, K301, K302,  \
     K303,   K304, K305, K306, K307, K308, K309, K400, K401, K402, K403, K404, K405,   K406,    K407, K408, K409,  \
@@ -33,5 +33,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	{ K700,  K701,  K702,  K703,  K704,  K705,  K706,  K707,  K708,  K709 }, \
 	{ K800,  K801,  K802,  K803,  K804,  K805,  K806,  K807,  K808,  KC_NO }  \
 }
-
 
