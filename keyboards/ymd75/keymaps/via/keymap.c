657c00502596fb79430dd000c75a6390f9a4492e
Konstantin Shpits <hexbug@gmail.com>
2021-02-07 23:49:56 +0500

    Update keyboards/ymd75/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ymd75/keymaps/via/keymap.c
+++ keyboards/ymd75/keymaps/via/keymap.c
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-    P_MACRO = SAFE_RANGE
+    P_MACRO = USER00,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
