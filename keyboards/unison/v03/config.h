d2309b39aa068ca9e1b3dbf2d9739acff1d78ec5
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-09 07:53:51 +0900

    change LED adjusting value.

--- keyboards/unison/v03/config.h
+++ keyboards/unison/v03/config.h
@@ -58,8 +58,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLED_NUM 8 // Layer Indicator(3) + Rotary Encoder(5), optional Underglow(8)
   #define RGBLIGHT_LED_MAP {1, 2, 3, 0, 4, 5, 6, 7} // Layer Indicator --> R.E. --> Under 〓実物確認
   #define RGBLIGHT_HUE_STEP 4
-  #define RGBLIGHT_SAT_STEP 16
-  #define RGBLIGHT_VAL_STEP 16
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
   // #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
   /*== Lighting Layers ==*/
