df5fb1f95a7bb91ab5a9cca269d118e62a394113
Oliver Gaskell <olivergaskell700@gmail.com>
2021-02-08 09:24:41 +0000

    Change LAYOUT to LAYOUT_ortho_4x12, from code review
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/tau4/rules.mk
+++ keyboards/tau4/rules.mk
@@ -24,3 +24,5 @@ ENCODER_ENABLE = yes        # Rotary Encoder support
 OLED_DRIVER_ENABLE = yes    # OLED display support
 # EEPROM_DRIVER ?= i2c        # Driver for external EEPROM chip
 # This is currently not working due to QMK not officially supporting the chip used on the Tau4, I am working on a fix.
+
+LAYOUTS = ortho_4x12
