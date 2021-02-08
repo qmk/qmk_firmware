6af99f04afa879ea1bb763ee70c6a0f6c1a5ac03
PRIVATECC <mariocc@comunidad.unam.mx>
2021-02-08 12:55:20 -0600

    Changes in thefrey

--- users/ninjonas/tap_dances.h
+++ users/ninjonas/tap_dances.h
@@ -20,6 +20,8 @@ enum custom_tapdances{
   TD_ESC_CAPS = 0,
   TD_LBRC_BACK,
   TD_RBRC_FWD,
+  TD_LPRN_LCBR,
+  TD_RPRN_RCBR,
   TD_TAB_CTRLTAB,
   TD_GRV_CTRLGRV,
 //  TD_GUI_GUISPC,
@@ -31,6 +33,8 @@ enum custom_tapdances{
 #define T_ESC  TD(TD_ESC_CAPS)    // Tap for ESC, double tap for CAPSLOCK
 #define T_LBRC TD(TD_LBRC_BACK)   // Tap for [, double tap for back browser
 #define T_RBRC TD(TD_RBRC_FWD)    // Tap for ], double tap for forward browser
+#define T_LPRN TD(TD_LPRN_LCBR)    // Tap for {, double tap for forward browser
+#define T_LPRN TD(TD_RPRN_RCBR)    // Tap for }, double tap for forward browser
 #define T_TAB  TD(TD_TAB_CTRLTAB) // Tap for TAB, double tap for CTRL + TAB
 #define T_GRV  TD(TD_GRV_CTRLGRV) // Tap for GRV, double tap for Cmd + GRV
 //#define T_GUI  TD(TD_GUI_GUISPC)  // Tap for Cmd, double tap for Cmd + Space
