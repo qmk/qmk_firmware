e63beef5c9aa6e7c92ce59f3d7266c99fc7a6c05
TR Rudkin <trudkin@gmail.com>
2021-02-08 06:17:05 -0700

    Change for PR

--- keyboards/ergodox_ez/keymaps/trudkin/rules.mk
+++ keyboards/ergodox_ez/keymaps/trudkin/rules.mk
@@ -1,8 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 
-SRC += tap.c
-SRC += muse.c
 SRC += matrix.c
 
 
