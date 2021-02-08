c847775f40f7f32cb4428bc56fa6a4c294770d44
M4cs <mabridgland@protonmail.com>
2021-02-08 00:34:01 -0500

    Fix Keymap

--- keyboards/dekunukem/duckypad/keymaps/m4cs/keymap.c
+++ keyboards/dekunukem/duckypad/keymaps/m4cs/keymap.c
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "stdio.h"
 #include "raw_hid.h"
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
