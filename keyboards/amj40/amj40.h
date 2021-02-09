a8d71bb765a367c96c5cd4b96207f39b543cd4cd
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-02-08 20:32:06 -0800

    update keyboard source files
    - add license headers (attributed to original keyboard submitter)
    - #pragma once include guards
    - file template matching
    - readme update and template matching

--- keyboards/amj40/amj40.h
+++ keyboards/amj40/amj40.h
@@ -1,5 +1,20 @@
-#ifndef AMJ40_H
-#define AMJ40_H
+/* Copyright 2017 Fabian Topfstedt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include "quantum.h"
 
@@ -28,11 +43,3 @@
     {k20, XXX, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b}, \
     {k30, k31, k32, XXX, k34, k35, XXX, XXX, XXX, k39, k3a, k3b} \
 }
-
-
-
-
-void matrix_init_user(void);
-void matrix_scan_user(void);
-
-#endif
--- keyboards/amj40/readme.md
+++ keyboards/amj40/readme.md
@@ -1,5 +1,4 @@
-AMJ40
-===
+# AMJ40
 
 DIY/Assembled compact 40% keyboard.
 
@@ -11,4 +10,8 @@ Make example for this keyboard (after setting up your build environment):
 
     make amj40:default
 
-See [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) then the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information.
+Flashing example for this keyboard:
+
+    make amj40:default:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
