805cf89f3642db31500951b87142f7c83f4f6d6a
xSteins <42110361+xSteins@users.noreply.github.com>
2021-02-11 08:30:49 +0700

    reverting layout name

--- keyboards/handwired/ergocheap/keymaps/default/keymap.c
+++ keyboards/handwired/ergocheap/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_65_ANSI(
+    [_BASE] = LAYOUT(
               KC_ESC,  KC_1,    KC_2,                                                                                   KC_MINS, KC_EQL,  KC_BSPC,          KC_GRV,
             KC_TAB,       KC_Q,       KC_3,    KC_4,    KC_5,    KC_6,                 KC_7,    KC_8,    KC_9,    KC_0,          KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL,
           KC_CAPS,        KC_A,  KC_W,    KC_E,    KC_R,    KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SCLN, KC_QUOT, KC_ENT,             KC_HOME,
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,                        KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_LGUI,         KC_LEFT, KC_DOWN, KC_RGHT,
                                         KC_LALT,   KC_SPC,           MO(_FN1),        KC_SPC,                  MO(_FN1)
     ),
-    [_FN1] = LAYOUT_65_ANSI(
+    [_FN1] = LAYOUT(
               KC_GRV,  KC_F1,   KC_F2,                                                                                  KC_F11,  KC_F12,  _______,          _______,
             _______,      _______,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                KC_F7,   KC_F8,   KC_F9,   KC_F10,        _______, _______, _______,     _______,
           KC_LCTL,        _______, _______,   _______, _______, _______,              _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______,            _______,
--- keyboards/handwired/ergocheap/rules.mk
+++ keyboards/handwired/ergocheap/rules.mk
@@ -22,4 +22,3 @@ NKRO_ENABLE = yes            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 DEFAULT_FOLDER = ergocheap
-LAYOUTS = 65_ANSI
