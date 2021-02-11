23dadc68ed2728f6876d3a6e57fb19cbcf1e79bd
Adrian Walker <walkeraid@gmail.com>
2021-02-11 22:09:44 +1100

    Removing files and implementing changes for pull request

--- keyboards/phrygian/ph100/config.h
+++ keyboards/phrygian/ph100/config.h
@@ -1,3 +1,19 @@
+/* Copyright 2021 Phrygian Design
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include "config_common.h"
@@ -5,10 +21,9 @@
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0C61
-#define DEVICE_VER      0x00C6
+#define DEVICE_VER      0x0100
 #define MANUFACTURER    PhrygianDesign
-#define PRODUCT         ph100 v1.0.0
-#define DESCRIPTION     100 key keyboard
+#define PRODUCT         ph100
 
 /* key matrix size */
 #define MATRIX_ROWS 10
@@ -19,6 +34,10 @@
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
+
+/* Define capslock and numlock */
+#define LED_NUM_LOCK_PIN  B14
+#define LED_CAPS_LOCK_PIN B15
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 10
