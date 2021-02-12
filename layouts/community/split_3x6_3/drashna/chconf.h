b0280477cee668cc3138dd0f505f3c9203a0c924
Drashna Jael're <drashna@live.com>
2021-02-11 22:23:06 -0800

    Add horrible hack for dev

--- layouts/community/split_3x6_3/drashna/chconf.h
+++ layouts/community/split_3x6_3/drashna/chconf.h
@@ -2,4 +2,8 @@
 #define CH_CFG_ST_RESOLUTION                16
 #define CH_CFG_ST_FREQUENCY                 10000
 
-#include_next "platforms/chibios/common/configs/chconf.h"
+#if __has_include("platforms/chibios/common/configs/chconf.h")
+#    include_next "platforms/chibios/common/configs/chconf.h"
+#else
+#    include_next "chconf.h"
+#endif
