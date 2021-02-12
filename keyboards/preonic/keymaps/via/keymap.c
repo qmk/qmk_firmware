1f12aff76e05d82e7360e002a89d246887e1f238
George Wietor <george@wietor.com>
2021-02-12 01:18:05 -0500

    update keymap credit

--- keyboards/preonic/keymaps/via/keymap.c
+++ keyboards/preonic/keymaps/via/keymap.c
@@ -1,4 +1,5 @@
 /* Copyright 2015-2017 Jack Humbert
+ * Updated for VIA 2021 George Wietor
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
