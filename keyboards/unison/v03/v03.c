073e935b5299227dd4372d8de2571be8b80dc425
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-12 10:16:11 +0900

    Add TODO flag.

--- keyboards/unison/v03/v03.c
+++ keyboards/unison/v03/v03.c
@@ -16,8 +16,8 @@
 
 #include "v03.h"
 
-// MusicーModeは使わない予定
-// #if defined(AUDIO_ENABLE) && defined(MUSIC_MAP)
+#if defined(AUDIO_ENABLE) && defined(MUSIC_MAP)
+//TODO
 // const uint8_t music_map[MATRIX_ROWS][MATRIX_COLS] = LAYOUT_all(
 //     20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,
 //     13, 14,15,16,17,18,19,20,21,22,23,24,  25,26,
@@ -25,4 +25,4 @@
 //     0,     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,11,
 //     0, 0,  0,  0,   0,    0,  0,  0,  0, 0, 0, 0
 // );
-// #endif
+#endif
