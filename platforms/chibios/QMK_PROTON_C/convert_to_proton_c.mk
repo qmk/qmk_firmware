0dc1c43d5f0e6f78e1bda8679434bb63513ecede
Drashna Jael're <drashna@live.com>
2021-02-12 14:37:40 -0800

    Soft set backlight driver based on platform

--- platforms/chibios/QMK_PROTON_C/convert_to_proton_c.mk
+++ platforms/chibios/QMK_PROTON_C/convert_to_proton_c.mk
@@ -7,6 +7,3 @@ OPT_DEFS += -DCONVERT_TO_PROTON_C
 # These are defaults based on what has been implemented for ARM boards
 AUDIO_ENABLE ?= yes
 WS2812_DRIVER ?= bitbang
-
-# Force task driven PWM until ARM can provide automatic configuration
-BACKLIGHT_DRIVER ?= software
