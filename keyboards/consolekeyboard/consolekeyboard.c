3dee04c16ba521fec1f9f05bd4a1de35fd0bc2c1
gazeddy <33955949+gazeddy@users.noreply.github.com>
2021-02-12 21:02:42 +0000

    Update consolekeyboard.c

--- keyboards/consolekeyboard/consolekeyboard.c
+++ keyboards/consolekeyboard/consolekeyboard.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:keyboards/consolekeyboard/ConsoleKeyboard.c
 /* Copyright 2021 Gareth Edwards
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,26 +15,3 @@
  */
 
 #include "consolekeyboard.h"
-
-
-=======
-/* Copyright 2021 Gareth Edwards
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "consolekeyboard.h"
-
-
->>>>>>> cbe2e7c966bf09e666d63bf35526a15c272b9b2c:keyboards/consolekeyboard/consolekeyboard.c
