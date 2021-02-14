22b71d602f1d60944df815f9241767fa5f396c2a
Jonathan Haddock <joncojonathan@gmail.com>
2021-02-14 15:26:22 +0000

    Add copyright block

--- keyboards/leafcutterlabs/minimacro5/keymaps/media/keymap.c
+++ keyboards/leafcutterlabs/minimacro5/keymaps/media/keymap.c
@@ -1,3 +1,11 @@
+/* Copyright 2020 Craig Gardner 
+  * 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * 
+  */ 
 #include QMK_KEYBOARD_H
 
 #define _MAIN 0
--- keyboards/leafcutterlabs/minimacro5/minimacro5.c
+++ keyboards/leafcutterlabs/minimacro5/minimacro5.c
@@ -1,3 +1,11 @@
+/* Copyright 2020 Craig Gardner 
+  * 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * 
+  */ 
 #include "minimacro5.h"
 
 void matrix_init_kb(void) {
