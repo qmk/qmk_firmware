8d7cea1638e1d0183f4983fc4d4fcc7643e2ffd6
William Ehman <will@ehman.com>
2021-02-14 11:23:36 -0700

    Update keyboards/manyboard/macro/config.h to remove trailing newline
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/manyboard/macro/config.h
+++ keyboards/manyboard/macro/config.h
@@ -48,10 +48,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* Define tapdance timing*/
-#define TAPPING_TERM 175
-
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
