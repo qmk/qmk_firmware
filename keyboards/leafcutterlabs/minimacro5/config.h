8cd971a017d867e3d15e0a6e278e415dc3612b77
Jonathan Haddock <joncojonathan@gmail.com>
2021-02-14 15:28:55 +0000

    Remove extraneous space

--- keyboards/leafcutterlabs/minimacro5/config.h
+++ keyboards/leafcutterlabs/minimacro5/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    leafcutterlabs
 #define PRODUCT         miniMACRO5
-#define DESCRIPTION     5 key/rotary  board
+#define DESCRIPTION     5 key/rotary board
 
 /* key matrix size */
 #define MATRIX_ROWS 1
