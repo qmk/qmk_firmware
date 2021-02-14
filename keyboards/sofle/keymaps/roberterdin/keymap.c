86eb2d7ae77b36a76ad799ce02ae9a391d7aeb74
Robert Erdin <robert.erdin@gmail.com>
2021-02-14 13:12:38 +0000

    add media controls to sofle

--- keyboards/sofle/keymaps/roberterdin/keymap.c
+++ keyboards/sofle/keymaps/roberterdin/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | LCTR |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------| MUTE  |    |       |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| MUTE  |    |  Play |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |   \  | LGUI | LAlt |LOWER | /Enter  /       \Space \  |RAISE |   [  |   ]  |  =   |
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_LCTRL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_MINS, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_MINS, \
                 KC_BSLS, KC_LGUI,KC_LALT, KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_LBRC, KC_RBRC, KC_EQL \
 ),
 /*
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  KC_EQL, KC_PLUS, KC_MINS, KC_LEND,   XXXXXXX, _______, \
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______ \
 ),
-/* ADJUST
+/* ADJUST (lower + raise)
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |   /  |   *  |   -  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -661,8 +661,8 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
             switch (get_highest_layer(layer_state)) {
-            case _ADJUST:
-                //left
+            case _LOWER:
+                tap_code(KC_MNXT);
                 break;
             default:
                 tap_code(KC_VOLU);
@@ -670,8 +670,8 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             }
         } else {
             switch (get_highest_layer(layer_state)) {
-            case _ADJUST:
-                //right
+            case _LOWER:
+                tap_code(KC_MPRV);
                 break;
             default:
                 tap_code(KC_VOLD);
