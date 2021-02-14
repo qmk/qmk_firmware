a798b459171df0d0ce4325085235524ce76792cc
lixuebin <li.xuebin@hotmail.com>
2021-02-14 16:19:30 +0800

    fix zx60 layout default keymaps

--- keyboards/sets3n/zx60/keymaps/via/keymap.c
+++ keyboards/sets3n/zx60/keymaps/via/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, XXXXXXX,   KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
-        KC_LCTL, KC_LGUI, KC_LALT,          XXXXXXX,          KC_SPC,           XXXXXXX,          MO(1),   KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(1),   KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
 // 1:Fn Layer
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, KC_NO,   _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, _______, KC_PGUP, _______, 
-        _______, _______, _______,          KC_NO,            _______,          KC_NO,            _______, _______, KC_HOME, KC_PGDOWN, KC_END
+        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDOWN, KC_END
     )
 };
 
