ba019a04bd99da6e1945b301f0de12e262518b89
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2021-02-13 20:16:22 -0600

    Update disarray.c
    moved LED indicator to #define in config.h

--- keyboards/sneakbox/disarray/staggered/disarray.c
+++ keyboards/sneakbox/disarray/staggered/disarray.c
@@ -17,13 +17,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "disarray.h"
 
-void keyboard_pre_init_kb() {
-    setPinOutput(B1);
-    keyboard_pre_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    writePin(B1, 1);
-    return res;
-}
