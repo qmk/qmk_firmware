36f5e36e71138a95e5ff7e465f75f476138ab060
IFo Hancroft <contact@ifohancroft.com>
2021-02-14 05:18:47 +0200

    Shortened the key names

--- keyboards/idobo/keymaps/ifohancroft/keymap.c
+++ keyboards/idobo/keymaps/ifohancroft/keymap.c
@@ -1,47 +1,46 @@
-/* Copyright 2018 MechMerlin
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    /* Qwerty
+    * ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    * | `     || 1     || 2     || 3     ||   4   || 5     || 6     ||                         || =     || 6     || 7     || 8     || 9     || 0     || Del   |
+    * |-------------------------------------------------------------------------------------------------------------------------------------------------------|
+    * | Tab   || Q     || W     || E     ||   R   || T     || 6     ||                         || ]     || Y     || U     || I     || O     || P     || \     |
+    * |-------------------------------------------------------------------------------------------------------------------------------------------------------|
+    * | ESC   || A     || S     || D     ||   F   || G     || "     ||                         || "     || H     || J     || K     || L     || ;     || Enter |
+    * |-------------------------------------------------------------------------------------------------------------------------------------------------------|
+    * | Shift || Z     || X     || C     ||   V   || B     ||       ||                         ||       || N     || M     || ,     || .     || /     || Shift |
+    * |-------------------------------------------------------------------------------------------------------------------------------------------------------|
+    * | Ctr   || Super || Alt   || MO(2) ||       || MO(1) || Bkspc || Space ||       || Space || Bkspc || MO(1) ||       || Left  || Down  || Up    || Right |
+    * ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    */
     LAYOUT_ortho_5x15(
-        KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,     KC_6,     KC_7,      KC_8,     KC_9,      KC_0,      KC_MINUS,    KC_EQUAL,    KC_BSPACE, KC_DELETE,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,     KC_Y,     KC_U,      KC_I,     KC_O,      KC_P,      KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_HOME,
-        KC_ESCAPE, KC_A,    KC_S,    KC_D,    KC_F,      KC_G,     KC_H,     KC_J,      KC_K,     KC_L,      KC_SCOLON, KC_QUOTE,    KC_ENTER,    KC_ENTER,  KC_PGUP,
-        KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,     KC_N,     KC_M,      KC_COMMA, KC_DOT,    KC_SLASH,  KC_RSHIFT,   KC_RSHIFT,   KC_UP,     KC_PGDOWN,
-        KC_LCTRL,  KC_LGUI, KC_LALT, MO(1),   KC_BSPACE, KC_SPACE, KC_SPACE, KC_BSPACE, MO(1),    KC_RALT,   KC_RGUI,   KC_RCTL,     KC_LEFT,     KC_DOWN,   KC_RGHT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
+        KC_ESCP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_BSPC, KC_SPC,  KC_SPC,  KC_BSPC, MO(1),   KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     LAYOUT_ortho_5x15(
-        KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,    KC_F6,    KC_F7,     KC_F8,    KC_F9,     KC_F10,    KC_F11,      KC_F12,      KC_TRNS,   KC_INSERT,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_END,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_INS,
+        KC_TRNS, KC_TRNS, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,
+        KC_TRNS, RGB_SAI, RGB_TOG, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     LAYOUT_ortho_5x15(
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     LAYOUT_ortho_5x15(
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
