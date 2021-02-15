2d86541189a414b83850cc83b00a212f9bcc8a77
nautxx <jrbarnachea@gmail.com>
2021-02-14 17:42:44 -0800

    keymap update
    Removed #DEFINEs

--- keyboards/wilba_tech/rama_works_m6_a/keymaps/naut/keymap.c
+++ keyboards/wilba_tech/rama_works_m6_a/keymaps/naut/keymap.c
@@ -1,17 +1,16 @@
 #include QMK_KEYBOARD_H
 
-// Define Layers
-#define _BASE 0
-#define _FNX 1
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Base Layer
-    [_BASE] = LAYOUT(
+    [0] = LAYOUT(
         TO(1), KC_UP, KC_PGDN,
-        KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LEFT, KC_DOWN, KC_RGHT
+        ),
 
     // Fn Layer
-    [_FNX] = LAYOUT(
+    [1] = LAYOUT(
         TO(0), KC_VOLU, KC_MPLY,
-        KC_MRWD, KC_VOLD, KC_MFFD)};
+        KC_MRWD, KC_VOLD, KC_MFFD
+        ),
+};
