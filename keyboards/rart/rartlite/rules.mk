bce4f587975504b192d457e34f8b70f8487176ea
labahuy <68632528+labahuy@users.noreply.github.com>
2021-02-15 12:40:07 +0700

    Update rules.mk

--- keyboards/rart/rartlite/rules.mk
+++ keyboards/rart/rartlite/rules.mk
@@ -20,6 +20,3 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = no
-
-LAYOUTS = right_numpad left_numpad
