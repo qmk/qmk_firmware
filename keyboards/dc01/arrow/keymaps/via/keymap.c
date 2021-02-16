3fed8bced718e24e414c0d5736554d7988235b20
yiancar <yiangosyiangou@cytanet.com.cy>
2021-02-16 03:36:58 +0200

    Dc01 fixes (#11843)
    * dc01-fixes
    
    * dc01-fixes
    
    * Cleanup and add VIA
    
    * Updated JSONs
    
    * fix

--- keyboards/dc01/arrow/keymaps/via/keymap.c
+++ keyboards/dc01/arrow/keymaps/via/keymap.c
@@ -16,23 +16,31 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_iso( /* Base */
-            KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
-    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,         \
-    KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_NUHS,KC_ENT,  \
-    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,                 KC_RSFT, \
-    KC_SPC,    KC_SPC,     KC_RALT, KC_RGUI,     KC_RGUI,    KC_RCTL  \
-),
+[0] = LAYOUT_all( /* Base */
+    KC_INS,  KC_HOME,  KC_PGUP,
+    KC_DEL,  KC_END,   KC_PGDN,
+
+             KC_UP,
+    KC_LEFT, KC_DOWN,  KC_RIGHT),
+
+[1] = LAYOUT_all( /* Empty for Dynamic keymap */
+    KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,
+
+              KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[2] = LAYOUT_all( /* Empty for Dynamic keymap */
+    KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,
+
+              KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[3] = LAYOUT_all( /* Empty for Dynamic keymap */
+    KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,
+
+              KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS)
 };
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
