84c37edc039481f3d347f75300a07353662261a8
Drashna Jael're <drashna@live.com>
2021-02-17 05:37:38 -0800

    add comments, remove ifdefs

--- quantum/pointing_device_drivers.c
+++ quantum/pointing_device_drivers.c
@@ -1,6 +1,8 @@
 
 #include "pointing_device.h"
 
+// get_report functions should probably be moved to their respective drivers.
+
 #if defined(POINTING_DEVICE_DRIVER_pmw3360)
 #include "pmw3360.h"
 
@@ -63,27 +65,11 @@ short_mouse_t get_report() {
     uint8_t         state[5] = {};
     if (i2c_readReg(TRACKBALL_WRITE, 0x04, state, 5, I2C_TIMEOUT) == I2C_STATUS_SUCCESS) {
         if (!state[4] && !debounce) {
-#ifdef PIMORONI_TRACKBALL_INVERT_X
-                h_offset += mouse_offset(state[2], state[3], 1);
-#else
                 h_offset -= mouse_offset(state[2], state[3], 1);
-#endif
-#ifdef PIMORONI_TRACKBALL_INVERT_Y
-                v_offset += mouse_offset(state[1], state[0], 1);
-#else
                 v_offset -= mouse_offset(state[1], state[0], 1);
-#endif
             } else {
-#ifdef PIMORONI_TRACKBALL_INVERT_X
-                x_offset -= mouse_offset(state[2], state[3], 5);
-#else
                 x_offset += mouse_offset(state[2], state[3], 5);
-#endif
-#ifdef PIMORONI_TRACKBALL_INVERT_Y
-                y_offset -= mouse_offset(state[1], state[0], 5);
-#else
                 y_offset += mouse_offset(state[1], state[0], 5);
-#endif
             }
         } else {
             if (state[4]) {
@@ -101,13 +87,8 @@ short_mouse_t get_report() {
         report.buttons &= ~MOUSE_BTN1;
     }
 
-#ifndef PIMORONI_TRACKBALL_ROTATE
-    update_member(&report.x, &x_offset);
-    update_member(&report.y, &y_offset);
-#else
     update_member(&report.x, &y_offset);
     update_member(&report.y, &x_offset);
-#endif
 
     return report;
 }
