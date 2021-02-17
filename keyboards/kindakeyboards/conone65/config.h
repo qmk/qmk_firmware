6bf96c1c560f4c367ecc862aeae684a15dbf4282
Evy Dekkers <17366381+evyd13@users.noreply.github.com>
2021-02-17 08:22:21 +0100

    Update USB VID/PID

--- keyboards/kindakeyboards/conone65/config.h
+++ keyboards/kindakeyboards/conone65/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x1776
-#define PRODUCT_ID   0x6aab
+#define VENDOR_ID    0x9133
+#define PRODUCT_ID   0x6AAB
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Kindakeyboards
 #define PRODUCT      Conone 65
