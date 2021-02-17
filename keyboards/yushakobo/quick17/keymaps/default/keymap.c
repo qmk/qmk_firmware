06ca08d660398fe24868540fb997c94d9af9e63b
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2021-02-17 14:50:31 +0900

    clean up default keymaps on yushakobo/quick17

--- keyboards/yushakobo/quick17/keymaps/default/keymap.c
+++ keyboards/yushakobo/quick17/keymaps/default/keymap.c
@@ -14,179 +14,140 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-    _PHOTO,
-    _PHOTO_SHIFT,
-    _FN
-};
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-    MAC = SAFE_RANGE,
-    WIN,
-    PS_LANG
+    BASE = SAFE_RANGE,
+    KC_LSPC,
+
+
 };
-
-/*
-    PHOTO,
-    FN,
-*/
-#define FN MO(_FN)
-#define BASE DF(_BASE)
-#define PHOTO DF(_PHOTO)
-#define PHOTO_S MO(_PHOTO_SHIFT)
-#define PS_CMRL KC_LGUI
-#define PS_ERSE KC_E
-#define PS_MGWD KC_W
-#define PS_EYDR KC_I
-#define PS_SLCT KC_A
-#define PS_HAND KC_H
-#define PS_LASS KC_L
-#define PS_MRQE KC_M
-#define PS_PEN KC_P
-#define PS_MOVE KC_V
-#define PS_ZOOM KC_Z
-#define PS_CROP KC_C
-#define PS_BRSD KC_LBRC
-#define PS_BRSI KC_RBRC
-#define PS_BRSP KC_COMM
-#define PS_BRSN KC_DOT
-#define PS_CSHR KC_CAPS
-
-#define PS_ZIN LGUI(KC_PLUS)
-#define PS_ZOUT LGUI(KC_MINS)
-#define PS_CLYR SGUI(KC_N)//ctrl shift n
-#define PS_UNDO LGUI(KC_Z)
-#define PS_REDO SGUI(KC_Z)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = LAYOUT(
-    KC_TAB,  KC_PGUP, KC_UP,   KC_PGDN, KC_HOME, KC_DEL,
-    KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_INS,
-    KC_LSFT, KC_ESC,  KC_LALT, KC_SPC,  FN,      KC_ENT
+    LAYOUT(
+    KC_TAB, KC_PGUP,KC_UP,  KC_PGDN,KC_HOME,KC_INS,
+    KC_LCTL,KC_LEFT,KC_DOWN,KC_RGHT,KC_END, KC_DEL,
+    KC_LSFT,KC_LGUI,KC_ESC, KC_LALT,KC_SPC, DF(1)
 ),
-    [_PHOTO] = LAYOUT(
-    KC_ESC,  PS_ERSE, PS_MGWD, PS_EYDR, PS_ZIN,  KC_BSPC,
-    PS_CMRL, PS_SLCT, PS_HAND, PS_LASS, PS_MRQE, PS_PEN,
-    KC_LSFT, KC_Z,    KC_V,    KC_LALT, PHOTO_S, KC_ENT
+    LAYOUT(
+    KC_ESC, KC_W,   KC_E,   KC_R,   KC_Y,   KC_BSPC,
+    KC_LCTL,KC_A,   KC_D,   KC_F,   KC_H,   LCTL(KC_Z),
+    KC_LSFT,KC_X,   KC_V,   KC_B,   KC_LSPC,LCTL(KC_S)
 ),
-    [_PHOTO_SHIFT] = LAYOUT(
-    KC_ESC,  PS_BRSD, PS_BRSI, PS_UNDO, PS_REDO, KC_DEL,
-    PS_CMRL, PS_BRSP, PS_BRSN, PS_CSHR, PS_CROP, PS_CLYR,
-    KC_LSFT, PS_LANG, BASE,    _______, _______, _______
+    LAYOUT(
+    KC_ESC, KC_Q,   KC_BTN3,KC_INS, KC_NO,  KC_NO,
+    KC_LCTL,KC_LBRC,KC_RBRC,KC_PGDN,KC_PGUP,LCTL(KC_Y),
+    KC_LSFT,DF(3),  KC_NO,  DF(0),  _______,KC_NO
 ),
-    [_FN] = LAYOUT(
-    KC_LANG2,KC_LANG1,XXXXXXX, RGB_TOG, KC_MNXT, KC_VOLU,
-    PHOTO,   BASE,    KC_NO,   RGB_MOD, KC_MPRV, KC_VOLD,
-    CG_NORM, CG_SWAP, KC_NO,   KC_NO,   _______, KC_MUTE
+    LAYOUT(
+    KC_ESC, KC_P0,  KC_P7,  KC_P8,  KC_P9,  KC_NLCK,
+    KC_TAB, KC_PDOT,KC_P4,  KC_P5,  KC_P6,  KC_BSPC,
+    KC_LSFT,KC_ENT, KC_P1,  KC_P2,  KC_P3,  DF(2)
 )
 };
 
-static bool _mode_mac = true;// keymap_config.swap_ctrl_gui
-static bool _mode_ja = false;
+static bool layer_shift = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        /*
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
+        case KC_LSPC:
+            if (record->event.pressed){
+                layer_shift = 1;
+                layer_on(2);
+            }else{
+                layer_off(2);
+                if(layer_shift){
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                }
+            }
+            return false;
+            break;
+        default:
+            if (record->event.pressed){
+                layer_shift = 0;
             }
             break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-        */
     }
     return true;
 }
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        switch (layer_state) {
-            case _BASE:
-                if (clockwise) {
-                    tap_code(KC_WH_U);
-                } else {
-                    tap_code(KC_WH_D);
-                }
-                break;
-            case _PHOTO:
-            case _PHOTO_SHIFT:
-                if (clockwise) {
-                    tap_code(KC_RBRC);
-                } else {
-                    tap_code(KC_LBRC);
-                }
-                break;
-            case _FN:
-                if (clockwise) {
-                    tap_code(KC_VOLU);
-                } else {
-                    tap_code(KC_VOLD);
-                }
-            default:
-                break;
-        }
-    }
-}
+//void encoder_update_user(uint8_t index, bool clockwise) {
+//    if (index == 0) {
+//        switch (layer_state) {
+//            case _BASE:
+//                if (clockwise) {
+//                    tap_code(KC_WH_U);
+//                } else {
+//                    tap_code(KC_WH_D);
+//                }
+//                break;
+//            case _PHOTO:
+//            case _PHOTO_SHIFT:
+//                if (clockwise) {
+//                    tap_code(KC_RBRC);
+//                } else {
+//                    tap_code(KC_LBRC);
+//                }
+//                break;
+//            case _FN:
+//                if (clockwise) {
+//                    tap_code(KC_VOLU);
+//                } else {
+//                    tap_code(KC_VOLD);
+//                }
+//            default:
+//                break;
+//        }
+//    }
+//}
 
-#ifdef RGBLIGHT_LAYERS
-    const rgblight_segment_t PROGMEM mode_mac[] = RGBLIGHT_LAYER_SEGMENTS(
-        {5, 1, HSV_WHITE}
-    );
-    const rgblight_segment_t PROGMEM mode_win[] = RGBLIGHT_LAYER_SEGMENTS(
-        {5, 1, HSV_TEAL}
-    );
-    const rgblight_segment_t PROGMEM mode_photo[] = RGBLIGHT_LAYER_SEGMENTS(
-        {11, 1, HSV_TEAL}
-    );
-    const rgblight_segment_t PROGMEM mode_photo1[] = RGBLIGHT_LAYER_SEGMENTS(
-        {11, 1, HSV_BLUE}
-    );
-    const rgblight_segment_t PROGMEM mode_fn[] = RGBLIGHT_LAYER_SEGMENTS(
-        {11, 1, HSV_SPRINGGREEN}
-    );
-    const rgblight_segment_t PROGMEM mode_ja[] = RGBLIGHT_LAYER_SEGMENTS(
-        {17, 1, HSV_ORANGE}
-    );
-    const rgblight_segment_t PROGMEM mode_en[] = RGBLIGHT_LAYER_SEGMENTS(
-        {17, 1, HSV_YELLOW}
-    );
-    const rgblight_segment_t* const PROGMEM quick17_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-        mode_photo,
-        mode_photo1,
-        mode_fn,
-        mode_mac,
-        mode_win,
-        mode_ja,
-        mode_en
-    );
-    void keyboard_post_init_user(void){
-        rgblight_layers = quick17_rgb_layers;
-    }
-    layer_state_t layer_state_set_user(layer_state_t state){
-        rgblight_set_layer_state(1, layer_state_cmp(state, _PHOTO_SHIFT));
-        rgblight_set_layer_state(2, layer_state_cmp(state, _FN));
-        return state;
-    }
-    bool led_update_user(led_t led_state){
-        rgblight_set_layer_state(0, layer_state_cmp(layer_state, _PHOTO));
-        rgblight_set_layer_state(3, _mode_mac);
-        rgblight_set_layer_state(4, !_mode_mac);
-        rgblight_set_layer_state(5, _mode_ja);
-        rgblight_set_layer_state(6, !_mode_ja);
-        return true;
-    }
-#endif
+//  #ifdef RGBLIGHT_LAYERS
+//      const rgblight_segment_t PROGMEM mode_mac[] = RGBLIGHT_LAYER_SEGMENTS(
+//          {5, 1, HSV_WHITE}
+//      );
+//      const rgblight_segment_t PROGMEM mode_win[] = RGBLIGHT_LAYER_SEGMENTS(
+//          {5, 1, HSV_TEAL}
+//      );
+//      const rgblight_segment_t PROGMEM mode_photo[] = RGBLIGHT_LAYER_SEGMENTS(
+//          {11, 1, HSV_TEAL}
+//      );
+//      const rgblight_segment_t PROGMEM mode_photo1[] = RGBLIGHT_LAYER_SEGMENTS(
+//          {11, 1, HSV_BLUE}
+//      );
+//      const rgblight_segment_t PROGMEM mode_fn[] = RGBLIGHT_LAYER_SEGMENTS(
+//          {11, 1, HSV_SPRINGGREEN}
+//      );
+//      const rgblight_segment_t PROGMEM mode_ja[] = RGBLIGHT_LAYER_SEGMENTS(
+//          {17, 1, HSV_ORANGE}
+//      );
+//      const rgblight_segment_t PROGMEM mode_en[] = RGBLIGHT_LAYER_SEGMENTS(
+//          {17, 1, HSV_YELLOW}
+//      );
+//      const rgblight_segment_t* const PROGMEM quick17_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+//          mode_photo,
+//          mode_photo1,
+//          mode_fn,
+//          mode_mac,
+//          mode_win,
+//          mode_ja,
+//          mode_en
+//      );
+//      void keyboard_post_init_user(void){
+//          rgblight_layers = quick17_rgb_layers;
+//      }
+//      layer_state_t layer_state_set_user(layer_state_t state){
+//          rgblight_set_layer_state(1, layer_state_cmp(state, _PHOTO_SHIFT));
+//          rgblight_set_layer_state(2, layer_state_cmp(state, _FN));
+//          return state;
+//      }
+//      bool led_update_user(led_t led_state){
+//          rgblight_set_layer_state(0, layer_state_cmp(layer_state, _PHOTO));
+//          rgblight_set_layer_state(3, _mode_mac);
+//          rgblight_set_layer_state(4, !_mode_mac);
+//          rgblight_set_layer_state(5, _mode_ja);
+//          rgblight_set_layer_state(6, !_mode_ja);
+//          return true;
+//      }
+//  #endif
