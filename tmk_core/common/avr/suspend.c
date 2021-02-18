f83446a0decba7a337f3b99c39a22b23bc5c63e3
fauxpark <fauxpark@gmail.com>
2021-02-18 18:30:43 +1100

    Move wdt_intr_enable into avr/suspend.c

--- tmk_core/common/avr/suspend.c
+++ tmk_core/common/avr/suspend.c
@@ -4,7 +4,6 @@
 #include <avr/interrupt.h>
 #include "matrix.h"
 #include "action.h"
-#include "suspend_avr.h"
 #include "suspend.h"
 #include "timer.h"
 #include "led.h"
@@ -56,6 +55,24 @@ __attribute__((weak)) void suspend_power_down_user(void) {}
 __attribute__((weak)) void suspend_power_down_kb(void) { suspend_power_down_user(); }
 
 #if !defined(NO_SUSPEND_POWER_DOWN) && defined(WDT_vect)
+
+// clang-format off
+#define wdt_intr_enable(value) \
+__asm__ __volatile__ ( \
+    "in __tmp_reg__,__SREG__" "\n\t" \
+    "cli" "\n\t" \
+    "wdr" "\n\t" \
+    "sts %0,%1" "\n\t" \
+    "out __SREG__,__tmp_reg__" "\n\t" \
+    "sts %0,%2" "\n\t" \
+    : /* no outputs */ \
+    : "M" (_SFR_MEM_ADDR(_WD_CONTROL_REG)), \
+    "r" (_BV(_WD_CHANGE_BIT) | _BV(WDE)), \
+    "r" ((uint8_t) ((value & 0x08 ? _WD_PS3_MASK : 0x00) | _BV(WDIE) | (value & 0x07))) \
+    : "r0" \
+)
+// clang-format on
+
 /** \brief Power down MCU with watchdog timer
  *
  * wdto: watchdog timer timeout defined in <avr/wdt.h>
