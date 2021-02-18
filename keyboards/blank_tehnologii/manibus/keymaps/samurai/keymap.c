b2d68f86ae80b395457ba9edb30c4de79ba16719
Alex Paulescu <alex.paulescu@gmail.com>
2021-02-18 21:32:43 +0200

    Update keyboards/blank_tehnologii/manibus/keymaps/samurai/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/blank_tehnologii/manibus/keymaps/samurai/keymap.c
+++ keyboards/blank_tehnologii/manibus/keymaps/samurai/keymap.c
@@ -200,7 +200,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     case KC_CTLT:
         if(record->event.pressed) {
-            SEND_STRING(SS_DOWN(X_LCTL)SS_TAP(X_T)SS_UP(X_LCTL));
+            tap_code16(C(KC_T));
         }
         return false;
     case KC_CST:
