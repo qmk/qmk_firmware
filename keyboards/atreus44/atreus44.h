3ca40f623bc4da0de0b67b8760ac3b5b29d194be
John Allen <jallen@fb.com>
2021-02-18 17:39:35 -0500

    some items after reading checklist

--- keyboards/atreus44/atreus44.h
+++ keyboards/atreus44/atreus44.h
@@ -15,8 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef ATREUS44_H
-#define ATREUS44_H
+#pragma once
 
 #include "quantum.h"
 #define ___ KC_NO
@@ -54,5 +53,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   {R2C0, R2C1, R2C2, R2C3, R2C4, R2C5, R2C6, R2C7, R2C8, R2C9, R2C10, R2C11 }, \
   {R3C0, R3C1, R3C2, R3C3, R3C4, R3C5, R3C6, R3C7, R3C8, R3C9, R3C10, R3C11 } \
 }
-
-#endif
--- keyboards/atreus44/readme.md
+++ keyboards/atreus44/readme.md
@@ -4,3 +4,6 @@ Atreus44
 ![Atreus](https://shop.keyboard.io/products/keyboardio-atreus)
 
 A small mechanical keyboard that is based around the shape of the human hand.
+
+Place the keyboard in bootloader mode by pressing the reset button the
+back with a paperclip or other poking device.
