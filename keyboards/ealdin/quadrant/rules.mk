22643ac19e85c39bd9c34cd8fe15adcdd904e927
Nuno Costa <nunopf99@gmail.com>
2021-02-18 01:17:39 +0000

    Update keyboards/ealdin/quadrant/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ealdin/quadrant/rules.mk
+++ keyboards/ealdin/quadrant/rules.mk
@@ -21,3 +21,5 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes        # Enable rotary encoders
+
+LAYOUTS = ortho_5x14
