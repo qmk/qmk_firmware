5e346933363b6825b1adfe5d9745aa090f5eedf0
IFo Hancroft <contact@ifohancroft.com>
2021-02-19 13:54:48 +0200

    Removed undef USB_POLLING_INTERVAL_MS from config.h

--- keyboards/primekb/prime_e/keymaps/ifohancroft/config.h
+++ keyboards/primekb/prime_e/keymaps/ifohancroft/config.h
@@ -1,23 +1,5 @@
-/*
-Copyright 2019 Holten Campbell
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 // place overrides here
-#undef USB_POLLING_INTERVAL_MS
 #define USB_POLLING_INTERVAL_MS 1
 #define FORCE_NKRO
