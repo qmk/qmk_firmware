4fb08c7826f8592b129ad4f13b9cbb6dd221d86c
Adam Abrams <adam.abrams@optum.com>
2021-02-20 17:04:02 -0600

    improve mouse and tap-hold configs

--- keyboards/planck/keymaps/adamtabrams/config.h
+++ keyboards/planck/keymaps/adamtabrams/config.h
@@ -8,9 +8,13 @@
 // Mouse Configs
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL
+#define MK_C_OFFSET_1 8
+#define MK_C_OFFSET_UNMOD 14
+#define MK_C_OFFSET_2 24
+#define MK_W_INTERVAL_UNMOD 100
 
 // Tap-Hold Configs
-#define TAPPING_TERM 200
+#define TAPPING_TERM 170
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 #define TAPPING_FORCE_HOLD_PER_KEY
