c5aa52befea3e8524848659a9492394ef8c99ce4
YangPiCui <yang.pi.cui@gmail.com>
2021-02-20 18:43:24 +0800

    Update keyboards/handwired/evk/v1_3/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/evk/v1_3/config.h
+++ keyboards/handwired/evk/v1_3/config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS \
-    { B0, B1, B2, B3, B7, D0 }  // row 0, 1, 2, ...
+    { B0, B1, B2, B3, B7, D0 }
 #define MATRIX_COL_PINS \
     { D1, D2, D3, C6, C7, F0, F1, F4, F5, F6, F7, B6, B5, B4, D7, D6 }  // column 0, 1, 2, 3, ...
 #define UNUSED_PINS
