43031cb1c7f762e38a44286d7c3705482cd84e97
Miguel Filipe <miguel.filipe@gmail.com>
2021-02-20 00:47:00 +0000

    add id80/ansi/keymaps/msf
    this commit adds github.com/msf's keymaps for id80

---
+++ keyboards/id80/ansi/keymaps/msf/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#pragma once
+
+#define COMBO_COUNT 3
+#define COMBO_TERM 200
