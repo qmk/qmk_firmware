101cd772b16bfcc849b820853cd0234c6ae1b8ee
trudkin <trudkin@gmail.com>
2021-02-21 12:02:05 -0700

    Update keyboards/ergodox_ez/keymaps/trudkin/keymap.c
    setting proper type
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ergodox_ez/keymaps/trudkin/keymap.c
+++ keyboards/ergodox_ez/keymaps/trudkin/keymap.c
@@ -457,7 +457,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
 
  
     state = update_tri_layer_state(state,  _LOWER, _RAISE, _ADJUST);
