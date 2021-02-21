4397bbf345428bc8ea8b5641de0f0e6ab539a30c
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-22 07:13:46 +0900

    restore AUDIO enabled.

--- keyboards/unison/keymaps/via/rules.mk
+++ keyboards/unison/keymaps/via/rules.mk
@@ -2,7 +2,7 @@ CONSOLE_ENABLE = no
 
 # Audio feature can cause HUGE firmware size impact.
 # You need to turn off MIDI and Sequencer feature.
-AUDIO_ENABLE = no
+AUDIO_ENABLE = yes
 
 MIDI_ENABLE = no
 SEQUENCER_ENABLE = no
