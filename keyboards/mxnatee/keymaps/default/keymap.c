6e93fd44dc3a324d4903ea57fc9858c28d112c14
juan <jpuerto96@hotmail.com>
2021-02-22 09:07:31 -0500

    Fix firmware

--- keyboards/mxnatee/keymaps/default/keymap.c
+++ keyboards/mxnatee/keymaps/default/keymap.c
@@ -24,11 +24,11 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-              KC_F1, KC_F2, 			          KC_F3, KC_F4,            \
+              RESET, KC_R, 			          KC_F3, KC_F4,            \
               KC_Q,  KC_D, KC_W, KC_B,		KC_J, KC_F,  KC_U, KC_P,       \
               KC_S,  KC_H, KC_T, KC_G,		KC_Y, KC_N,  KC_E, KC_O,       \
         KC_A,        KC_Z, KC_X, KC_M,		KC_V, KC_K,  KC_L,       KC_I, \
-                     KC_1, KC_2, KC_3,      KC_6, KC_7, KC_8,              \
-                           KC_4, KC_5,      KC_9, RESET
+                     KC_1, KC_2, KC_C,      KC_6, KC_7, KC_8,              \
+                           KC_4, KC_5,      KC_9, KC_0
     )
 };
