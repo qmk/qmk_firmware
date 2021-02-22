e0116365ad3737b849f096a84c5ec3586b14fdb5
Takeshi Nishio <kkeennnn@gmail.com>
2021-02-23 08:16:26 +0900

    Add keymap image and change keymap as image shows.

--- keyboards/unison/keymaps/default_music/keymap.c
+++ keyboards/unison/keymaps/default_music/keymap.c
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_music(
                         KC_MUTE,        KC_F1,  KC_F2,  KC_F3,  KC_F4,          KC_MUTE,        KC_MUTE,        KC_MUTE,        KC_MUTE,
         KC_P7,  KC_P8,  KC_P9,  KC_PPLS,KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,        KC_BSPC,
-        KC_P4,  KC_P5,  KC_P6,  KC_PPLS,KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,     KC_ENT,
-        KC_P1,  KC_P2,  KC_P3,  KC_PENT,KC_LSFT,KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,  KC_SLSH,
-        KC_P0,  KC_P0,  KC_PDOT,KC_PENT,KC_BSLS,KC_GRV, KC_LGUI,KC_LALT,KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_RALT,KC_LEFT,KC_DOWN,KC_RGHT
+        KC_P4,  KC_P5,  KC_P6,  KC_PAST,KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,     KC_ENT,
+        KC_P1,  KC_P2,  KC_P3,  KC_PMNS,KC_LSFT,KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,  KC_SLSH,
+        KC_P0,  KC_PDOT,KC_PENT,KC_PPLS,KC_GRV, KC_BSLS,KC_LALT,KC_LGUI,KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_RGUI,KC_LEFT,KC_DOWN,KC_RGHT
     )
 };
--- keyboards/unison/keymaps/default_music/readme.md
+++ keyboards/unison/keymaps/default_music/readme.md
@@ -1,1 +1,6 @@
-# Defalut keymap for Unison Music layout.
+![Unison, Music style Layout Image](https://raw.githubusercontent.com/jpskenn/Unison/main/assets/readme/layout_music_style.png)
+
+# Defalut keymap for Unison Music Layout.
+
+This is the default layout for Unison Music Layout.  
+Inspired by "Teenage Engineering: OP-1".
