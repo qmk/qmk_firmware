e305f85734a4a79c5381f4148e478eff50d6210d
juan <jpuerto96@hotmail.com>
2021-02-22 08:52:25 -0500

    Added mxnatee

---
+++ keyboards/mxnatee/keymaps/default/keymap.c
@@ -0,0 +1,34 @@
+/* Copyright 2021 juan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+              KC_F1, KC_F2, 			          KC_F3, KC_F4,            \
+              KC_Q,  KC_D, KC_W, KC_B,		KC_J, KC_F,  KC_U, KC_P,       \
+              KC_S,  KC_H, KC_T, KC_G,		KC_Y, KC_N,  KC_E, KC_O,       \
+        KC_A,        KC_Z, KC_X, KC_M,		KC_V, KC_K,  KC_L,       KC_I, \
+                     KC_1, KC_2, KC_3,      KC_6, KC_7, KC_8,              \
+                           KC_4, KC_5,      KC_9, RESET
+    )
+};
---
+++ keyboards/mxnatee/mxnatee.h
@@ -0,0 +1,47 @@
+/* Copyright 2021 juan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT( \
+                    l02,    l03,                            r03,    r02,            \
+            l11,    l12,    l13,    l14,            r14,    r13,    r12,    r11,    \
+            l21,    l22,    l23,    l24,            r24,    r23,    r22,    r21,    \
+    l30,            l32,    l33,    l34,            r34,    r33,    r32,            r30,    \
+                        l40,    l41,    l42,     r40,    r41,    r42,                  \
+                                l43,    l44,     r43,    r44     \
+) { \
+{KC_NO,  KC_NO,  l02,    l03,    KC_NO},    \
+{KC_NO,  l11,    l12,    l13,    l14},      \
+{KC_NO,  l21,    l22,    l23,    l24},      \
+{l30,    KC_NO,  l32,    l33,    l34},      \
+{l40,    l41,    l42,    l43,    l44},      \
+{KC_NO,  KC_NO,  r02,    r03,    KC_NO },   \
+{KC_NO,  r11,    r12,    r13,    r14 },     \
+{KC_NO,  r21,    r22,    r23,    r24 },     \
+{r30,    KC_NO,  r32,    r33,    r34 },     \
+{r40,    r41,    r42,    r43,    r44 }      \
+}
