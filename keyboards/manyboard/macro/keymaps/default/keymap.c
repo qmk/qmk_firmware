13140f75926ef03364b094be2fffe0c5769665f7
William Ehman <will@ehman.com>
2021-02-22 22:19:26 -0700

    Update keyboards/manyboard/macro/keymaps/default/keymap.c to add more detailed layout info
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/manyboard/macro/keymaps/default/keymap.c
+++ keyboards/manyboard/macro/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, 
     KC_TRNS,  KC_TRNS,  KC_TRNS,  TO(2)
   ),
-  [2] = LAYOUT( 
+  [2] = LAYOUT_ortho_4x4( 
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, 
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, 
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, 
