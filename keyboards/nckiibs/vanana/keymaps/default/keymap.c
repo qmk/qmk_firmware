4d95d02c075bf2ef9b9c5f016490b423f4128353
noclew <noclews@gmail.com>
2021-02-24 01:24:35 -0500

    reabased from the official repo

--- keyboards/nckiibs/vanana/keymaps/default/keymap.c
+++ keyboards/nckiibs/vanana/keymaps/default/keymap.c
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT_vanana(
     KC_F1  ,    KC_F2  ,    KC_F3  ,    KC_F4  ,    KC_F5  ,    KC_F6  ,                KC_F7  ,    KC_F8  ,    KC_F9  ,    KC_F10 ,    KC_F11 ,    KC_F12 ,
-    KC_BSPC,    KC_EQL ,    KC_7   ,    KC_8   ,    KC_9   ,    KC_SLSH,                KC_CIRC,    KC_LBRC,    KC_RBRC,    KC_AT  ,    KC_EXLM,    KC_BSLS,
+    KC_BSPC,    KC_EQL ,    KC_7   ,    KC_8   ,    KC_9   ,    KC_PPLS,                KC_CIRC,    KC_LBRC,    KC_RBRC,    KC_AT  ,    KC_EXLM,    KC_BSLS,
     KC_ENT ,    KC_0   ,    KC_4   ,    KC_5   ,    KC_6   ,    KC_MINS,    KC_LEFT,    KC_UNDS,    KC_LPRN,    KC_RPRN,    KC_HASH,    KC_DQUO,    _______,
-    KC_DLR ,    KC_PAST,    KC_1   ,    KC_2   ,    KC_3   ,    KC_PPLS,    KC_RGHT,    KC_PIPE,    KC_LCBR,    KC_RCBR,    KC_PERC,    KC_AMPR,    _______,
+    KC_DLR ,    KC_PAST,    KC_1   ,    KC_2   ,    KC_3   ,    KC_SLSH,    KC_RGHT,    KC_PIPE,    KC_LCBR,    KC_RCBR,    KC_PERC,    KC_AMPR,    _______,
                 _______,    _______,    _______,    _______,    _______,                _______,    _______,    _______,    _______,    _______
     ),
 
