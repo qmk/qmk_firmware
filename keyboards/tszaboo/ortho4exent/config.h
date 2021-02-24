fddc0a4748ad478ed43dc00f6a13cdcc4e47d715
tszaboo <tszaboo@gmail.com>
2021-02-24 23:50:38 +0100

    Update config.h

--- keyboards/tszaboo/ortho4exent/config.h
+++ keyboards/tszaboo/ortho4exent/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    tszaboo
 #define PRODUCT         ortho4exent
-#define DESCRIPTION     Ortholinear keyboard for the exent case
 
 /* key matrix size */
 #define MATRIX_ROWS 5
