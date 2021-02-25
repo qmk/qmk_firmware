011c2a4ef07793f6d8c7b403b5cd700f00ef6230
ArifNu <79616820+anutech01@users.noreply.github.com>
2021-02-25 23:59:22 +0700

    Update config.h
    fix mapping pin

--- keyboards/anukey/config.h
+++ keyboards/anukey/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS \
-    { D1, D0, D4, C6, E6 }
+    { D1, D0, D4, C6 }
 #define MATRIX_COL_PINS \
     { D7, E6, B4, B5 }
 
