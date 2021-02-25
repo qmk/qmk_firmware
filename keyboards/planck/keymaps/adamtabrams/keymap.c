60e3e6b76298f8c2d9ec431a8aadf9e6a510bbed
Adam Abrams <adam.abrams@optum.com>
2021-02-24 22:13:36 -0600

    swap equal and hyphen for normal layer

--- keyboards/planck/keymaps/adamtabrams/keymap.c
+++ keyboards/planck/keymaps/adamtabrams/keymap.c
@@ -44,17 +44,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* ## Normal
 
-| C(Tab) |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |C(=+) |
+| C(Tab) |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |C(-_) |
 |Backsp/~|  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  ;:  |A('") |
-| G(-_)  |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /?  |G(Ent)|
+| G(=+)  |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /?  |G(Ent)|
 |  CTRL  | GUI  | ALT  |System|Number|S(Esc)|Space |Arrows|Cursor| GUI  |      |Normal|
  */
 
 [_NORMAL] = LAYOUT_planck_grid(
-    CTL_T(KC_TAB),  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    CTL_T(KC_EQL),
-    BACKTLD,        KC_A,    KC_S,    KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, ALT_T(KC_QUOT),
-    GUI_T(KC_MINS), KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,          KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, GUI_T(KC_ENT),
-    KC_LCTRL,       KC_LGUI, KC_LALT, SYSTEM, NUMBER, SFT_T(KC_ESC), KC_SPC, ARROWS, CURSOR,  KC_RGUI, XXXXXXX, NORMAL
+    CTL_T(KC_TAB), KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    CTL_T(KC_MINS),
+    BACKTLD,       KC_A,    KC_S,    KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, ALT_T(KC_QUOT),
+    GUI_T(KC_EQL), KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,          KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, GUI_T(KC_ENT),
+    KC_LCTRL,      KC_LGUI, KC_LALT, SYSTEM, NUMBER, SFT_T(KC_ESC), KC_SPC, ARROWS, CURSOR,  KC_RGUI, XXXXXXX, NORMAL
 ),
 
 /* ## Number
@@ -181,11 +181,11 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CTL_T(KC_TAB):
             return true;
-        case CTL_T(KC_EQL):
+        case CTL_T(KC_MINS):
             return true;
         case ALT_T(KC_QUOT):
             return true;
-        case GUI_T(KC_MINS):
+        case GUI_T(KC_EQL):
             return true;
         case GUI_T(KC_ENT):
             return true;
