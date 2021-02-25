98b932245b49d57e7c66ed3dbb339f57f7e9fe3d
Sendy Aditya Suryana <sendy.suryana@go-jek.com>
2021-02-25 21:02:18 +0700

    Change DOT to PDOT

--- keyboards/sendyyeah/75pixels/keymaps/via/keymap.c
+++ keyboards/sendyyeah/75pixels/keymaps/via/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E  , KC_R   , KC_T  , KC_Y  , KC_U  , KC_I   , KC_O   , KC_P   , KC_BSLS, KC_P7  , KC_P8  , KC_P9  , \
         KC_CAPS, KC_A   , KC_S   , KC_D  , KC_F   , KC_G  , KC_H  , KC_J  , KC_K   , KC_L   , KC_SCLN, KC_ENT , KC_P4  , KC_P5  , KC_P6  , \
         KC_LSFT, KC_Z   , KC_X   , KC_C  , KC_V   , KC_B  , KC_N  , KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_P1  , KC_P2  , KC_P3  , \
-        KC_LCTL, KC_LGUI, KC_LALT, MO(FN), _______, KC_SPC, KC_SPC, TO(FN), KC_RALT, KC_RGUI, KC_MENU, KC_RCTL, KC_P0  , KC_DOT , KC_ENT
+        KC_LCTL, KC_LGUI, KC_LALT, MO(FN), _______, KC_SPC, KC_SPC, TO(FN), KC_RALT, KC_RGUI, KC_MENU, KC_RCTL, KC_P0  , KC_PDOT, KC_ENT
     ),
     [FN] = LAYOUT(
         _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 , KC_F10 , KC_DEL , _______, _______, _______, \
