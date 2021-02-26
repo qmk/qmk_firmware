f135dd594b29f4ee551cfff01f0155a96e6f867c
Adam Abrams <adam.abrams@optum.com>
2021-02-26 10:51:20 -0600

    change placement of symbols

--- keyboards/planck/keymaps/adamtabrams/keymap.c
+++ keyboards/planck/keymaps/adamtabrams/keymap.c
@@ -44,48 +44,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* ## Normal
 
-| C(Tab) |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |C(-_) |
-|Backsp/~|  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  ;:  |A('") |
-| G(=+)  |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /?  |G(Ent)|
-|  CTRL  | GUI  | ALT  |System|Number|S(Esc)|Space |Arrows|Cursor| GUI  |      |Normal|
+|C(Tab)|  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |C(-_) |
+|Bsp/~ |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  ;:  |A('") |
+| G(!) |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /?  |G(Ent)|
+| CTRL | GUI  | ALT  |System|Number|S(Esc)|Space |Arrows|Cursor| GUI  |      |Normal|
  */
 
 [_NORMAL] = LAYOUT_planck_grid(
-    CTL_T(KC_TAB), KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    CTL_T(KC_MINS),
-    BACKTLD,       KC_A,    KC_S,    KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, ALT_T(KC_QUOT),
-    GUI_T(KC_EQL), KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,          KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, GUI_T(KC_ENT),
-    KC_LCTRL,      KC_LGUI, KC_LALT, SYSTEM, NUMBER, SFT_T(KC_ESC), KC_SPC, ARROWS, CURSOR,  KC_RGUI, XXXXXXX, NORMAL
+    CTL_T(KC_TAB),  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    CTL_T(KC_MINS),
+    BACKTLD,        KC_A,    KC_S,    KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, ALT_T(KC_QUOT),
+    GUI_T(KC_NO),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,          KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, GUI_T(KC_ENT),
+    KC_LCTRL,       KC_LGUI, KC_LALT, SYSTEM, NUMBER, SFT_T(KC_ESC), KC_SPC, ARROWS, CURSOR,  KC_RGUI, XXXXXXX, NORMAL
 ),
 
 /* ## Number
 
-|      |  !   |  @   |  #   |  $   |  %   |  ^   |  &   |  *   |  (   |  )   |      |
+|      |  @   |  +   |  =   |  &   |  |   |  #   |  *   |  ^   |  $   |  %   |      |
 |      |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |  \`  |
-|      |  ~   |  [   |  ]   |  =   |  +   |  -   |  |   |  {   |  }   |  \   |      |
+|      |  ~   |  [   |  {   |  (   |      |      |  )   |  }   |  ]   |  \   |      |
 |      |      |      |      | \\/  |      |      |      |      |      |      |      |
  */
 
 [_NUMBER] = LAYOUT_planck_grid(
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, KC_AT,   KC_PLUS, KC_EQL,  KC_AMPR, KC_PIPE, KC_HASH, KC_ASTR, KC_CIRC, KC_DLR,  KC_PERC, _______,
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-    _______, KC_TILD, KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_MINS, KC_PIPE, KC_LCBR, KC_RCBR, KC_BSLS, _______,
+    _______, KC_TILD, KC_LBRC, KC_LCBR, KC_LPRN, XXXXXXX, XXXXXXX, KC_RPRN, KC_RCBR, KC_RBRC, KC_BSLS, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 
 /* ## Arrows
 
-|      |      |A(->) |A(->) |      |      |      | P_Up |      |      |      |      |
-|      |      |      |P_Down|      |      | Left | Down |  Up  |Right |      |      |
+|      |      |A(->) |A(->) |      |      |      | P_Up |C(<-) |C(->) |      |      |
+|      |C(->) |      |P_Down|      |      | Left | Down |  Up  |Right |C(<-) |      |
 |      |      |      |      |      |A(<-) |      |      |      |      |      |      |
 |      |      |      |      |      |      |      | \\/  |      |      |      |      |
  */
 
 [_ARROWS] = LAYOUT_planck_grid(
-    _______, XXXXXXX, A(KC_RGHT), A(KC_RGHT), XXXXXXX, XXXXXXX,    XXXXXXX, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, XXXXXXX, XXXXXXX,    KC_PGDN,    XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, _______,
-    _______, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX, A(KC_LEFT), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, _______, _______,    _______,    _______, _______,    _______, _______, _______, _______, _______, _______
+    _______, XXXXXXX,    A(KC_RGHT), A(KC_RGHT), XXXXXXX, XXXXXXX,    XXXXXXX, KC_PGUP, C(KC_LEFT), C(KC_RGHT), XXXXXXX,    _______,
+    _______, C(KC_RGHT), XXXXXXX,    KC_PGDN,    XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN,   KC_UP,    KC_RGHT,    C(KC_LEFT), _______,
+    _______, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, A(KC_LEFT), XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,
+    _______, _______,    _______,    _______,    _______, _______,    _______, _______, _______,    _______,    _______,    _______
 ),
 
 /* ## Cursor
@@ -152,18 +152,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static bool shift_is_pressed = false;
+    static bool number_is_active = false;
+    static bool number_was_used = false;
+    static bool lgui_is_pressed = false;
+    static bool lgui_was_used = false;
+
+    if (number_is_active && record->event.pressed) {
+        number_was_used = true;
+    }
+    if (lgui_is_pressed && record->event.pressed) {
+        lgui_was_used = true;
+    }
 
     switch (keycode) {
+        case GUI_T(KC_NO):
+            if (record->event.pressed) {
+                lgui_is_pressed = true;
+            } else {
+                if (!lgui_was_used) {
+                    register_code(KC_LSFT);
+                    tap_code(KC_1);
+                    unregister_code(KC_LSFT);
+                }
+                lgui_is_pressed = false;
+                lgui_was_used = false;
+            }
+            return true;
+
         case SFT_T(KC_ESC):
-            if(record->event.pressed){
+            if (record->event.pressed) {
                 shift_is_pressed = true;
             } else {
                 shift_is_pressed = false;
             }
             return true;
+
         case BACKTLD:
-            if(record->event.pressed){
-                if(shift_is_pressed) {
+            if (record->event.pressed) {
+                if (shift_is_pressed && !number_is_active) {
                     register_code(KC_GRV);
                 } else {
                     register_code(KC_BSPC);
@@ -171,6 +197,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
                 unregister_code(KC_GRV);
                 unregister_code(KC_BSPC);
+            }
+            return false;
+
+        case KC_SPC:
+            if (record->event.pressed) {
+                if (shift_is_pressed && !number_is_active) {
+                    unregister_code(KC_LSFT);
+                    layer_on(_NUMBER);
+                    number_is_active = true;
+                } else {
+                    register_code(KC_SPC);
+                }
+            } else {
+                if (number_is_active) {
+                    layer_off(_NUMBER);
+                    if (shift_is_pressed) {
+                        register_code(KC_LSFT);
+                    }
+                    if (!number_was_used) {
+                        register_code(KC_SPC);
+                    }
+                    number_is_active = false;
+                    number_was_used = false;
+                }
+                unregister_code(KC_SPC);
             }
             return false;
     }
@@ -183,9 +234,9 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
             return true;
         case CTL_T(KC_MINS):
             return true;
-        case ALT_T(KC_QUOT):
-            return true;
-        case GUI_T(KC_EQL):
+        /* case ALT_T(KC_QUOT): */
+        /*     return true; */
+        case GUI_T(KC_NO):
             return true;
         case GUI_T(KC_ENT):
             return true;
