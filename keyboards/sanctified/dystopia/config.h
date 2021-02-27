12207658cfa21a68f51dd31ba86ae10c89ddca20
Sanctified-git <32301784+Sanctified-git@users.noreply.github.com>
2021-02-27 11:58:08 +0100

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/sanctified/dystopia/config.h
+++ keyboards/sanctified/dystopia/config.h
@@ -19,7 +19,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x514d // "QM"
+#define VENDOR_ID       0x514D // "QM"
 #define PRODUCT_ID      0x0001 // First public keyboard
 #define DEVICE_VER      0x0001 // Version 1
 #define MANUFACTURER    Sanctified.Works
