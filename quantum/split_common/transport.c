c73751c2cceec7245108e4b694917ded73be1f8d
Ryan Caltabiano <rcalt2vt@gmail.com>
2021-02-27 08:25:05 -0600

    Suspend State sync for rgb matrix

--- quantum/split_common/transport.c
+++ quantum/split_common/transport.c
@@ -60,6 +60,7 @@ typedef struct _I2C_slave_buffer_t {
 #    endif
 #    if defined(RGB_MATRIX_ENABLE) && defined(RGB_MATRIX_SPLIT)
     rgb_config_t rgb_matrix;
+    bool         rgb_suspend_state;
 #    endif
 } I2C_slave_buffer_t;
 
@@ -76,6 +77,7 @@ static I2C_slave_buffer_t *const i2c_buffer = (I2C_slave_buffer_t *)i2c_slave_re
 #    define I2C_ENCODER_START offsetof(I2C_slave_buffer_t, encoder_state)
 #    define I2C_WPM_START offsetof(I2C_slave_buffer_t, current_wpm)
 #    define I2C_RGB_MATRIX_START offsetof(I2C_slave_buffer_t, rgb_matrix)
+#    define I2C_RGB_SUSPEND_START offsetof(I2C_slave_buffer_t, rgb_suspend_state)
 
 #    define TIMEOUT 100
 
@@ -151,6 +153,7 @@ bool transport_master(matrix_row_t master_matrix[], matrix_row_t slave_matrix[])
 
 #    if defined(RGB_MATRIX_ENABLE) && defined(RGB_MATRIX_SPLIT)
     i2c_writeReg(SLAVE_I2C_ADDRESS, I2C_RGB_MATRIX_START, (void *)rgb_matrix_config, sizeof(i2c_buffer->rgb_matrix), TIMEOUT);
+    i2c_writeReg(SLAVE_I2C_ADDRESS, I2C_RGB_SUSPEND_START, (void *)g_suspend_state, sizeof(i2c_buffer->rgb_suspend_state), TIMEOUT);
 #    endif
 
 #    ifndef DISABLE_SYNC_TIMER
@@ -201,6 +204,7 @@ void transport_slave(matrix_row_t master_matrix[], matrix_row_t slave_matrix[]) 
 
 #    if defined(RGB_MATRIX_ENABLE) && defined(RGB_MATRIX_SPLIT)
     memcpy((void*)i2c_buffer->rgb_matrix, (void *)rgb_matrix_config, sizeof(i2c_buffer->rgb_matrix));
+    memcpy((void*)i2c_buffer->rgb_suspend_state, (void *)g_suspend_state, sizeof(i2c_buffer->rgb_suspend_state));
 #    endif
 }
 
@@ -244,6 +248,7 @@ typedef struct _Serial_m2s_buffer_t {
 #    endif
 #    if defined(RGB_MATRIX_ENABLE) && defined(RGB_MATRIX_SPLIT)
     rgb_config_t rgb_matrix;
+    bool         rgb_suspend_state;
 #    endif
 } Serial_m2s_buffer_t;
 
@@ -365,6 +370,7 @@ bool transport_master(matrix_row_t master_matrix[], matrix_row_t slave_matrix[])
 
 #    if defined(RGB_MATRIX_ENABLE) && defined(RGB_MATRIX_SPLIT)
     serial_m2s_buffer.rgb_matrix = rgb_matrix_config;
+    serial_m2s_buffer.rgb_suspend_state = g_suspend_state;
 #    endif
 
 #    ifndef DISABLE_SYNC_TIMER
@@ -408,6 +414,7 @@ void transport_slave(matrix_row_t master_matrix[], matrix_row_t slave_matrix[]) 
 
 #    if defined(RGB_MATRIX_ENABLE) && defined(RGB_MATRIX_SPLIT)
     rgb_matrix_config = serial_m2s_buffer.rgb_matrix;
+    g_suspend_state = serial_m2s_buffer.rgb_suspend_state;
 #    endif
 }
 
