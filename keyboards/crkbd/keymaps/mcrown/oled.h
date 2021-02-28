84d4fc1fcc82ba980165dabbd3b65ad717382c5e
PRIVATECC <mariocc@comunidad.unam.mx>
2021-02-28 13:45:03 -0600

    Add some coments

--- keyboards/crkbd/keymaps/mcrown/oled.h
+++ keyboards/crkbd/keymaps/mcrown/oled.h
@@ -3,10 +3,12 @@
 #include "mcrown.h"
 
 #ifdef OLED_VERTICAL
-#define KEYLOG_LEN (49)
+#define KEYLOG_LEN (48)
 #else
-#define KEYLOG_LEN (20)
+#define KEYLOG_LEN (19)
 #endif
+
+#define KEYLOG_EOL_LEN (KEYLOG_LEN+1)
 
 extern void add_keylog(uint16_t keycode);
 extern void update_log(void);
