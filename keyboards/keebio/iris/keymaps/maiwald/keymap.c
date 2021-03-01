a7c4ce48d502437d6281cc57c70e75b27bafd8c9
Luciano Maiwald <luciano.maiwald@gmail.com>
2021-03-01 22:27:00 +0100

    move stuff around

--- keyboards/keebio/iris/keymaps/maiwald/keymap.c
+++ keyboards/keebio/iris/keymaps/maiwald/keymap.c
@@ -19,8 +19,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_GUI_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_LALT),
 };
 
-#define MY_A RCTL_T(KC_A)
-#define MY_O LCTL_T(KC_O)
+#define MY_A LCTL_T(KC_A)
+#define MY_O RCTL_T(KC_O)
+#define MY_R LSFT_T(KC_R)
+#define MY_I RSFT_T(KC_I)
 
 #define VIM_ALT S(C(KC_CIRC))
 #define KC_EUR  S(A(KC_2))
@@ -40,26 +42,26 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-      KC_GRV,  KC_1, KC_2, KC_3, KC_4,   KC_5,                                      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_EQL,
-      KC_TAB,  KC_Q, KC_W, KC_F, KC_P,   KC_B,                                      KC_J,    KC_L,   KC_U,    KC_Y,   KC_QUOT, KC_MINS,
-      KC_ESC,  MY_A, KC_R, KC_S, KC_T,   KC_G,                                      KC_M,    KC_N,   KC_E,    KC_I,   MY_O,    KC_ENT,
-      KC_LSFT, KC_Z, KC_X, KC_C, KC_D,   KC_V,          KC_BSPC,      KC_ENT,       KC_K,    KC_H,   MY_COMM, MY_DOT, KC_SLSH, KC_RSFT,
-                                 MY_GUI, OSM(MOD_LSFT), MO(_SYMBOLS), MO(_SYMBOLS), EXT_SPC, KC_RALT
+      KC_GRV,  KC_1, KC_2, KC_3, KC_4,   KC_5,                                        KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS,
+      KC_TAB,  KC_Q, KC_W, KC_F, KC_P,   KC_B,                                        KC_J,    KC_L,   KC_U,    KC_Y,   KC_QUOT, KC_BSPC,
+      KC_ESC,  MY_A, MY_R, KC_S, KC_T,   KC_G,                                        KC_M,    KC_N,   KC_E,    MY_I,   MY_O,    KC_ENT,
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_D,   KC_V,          KC_BSPC,       KC_ENT,        KC_K,    KC_H,   MY_COMM, MY_DOT, KC_SLSH, KC_RSFT,
+                                 MY_GUI, OSM(MOD_LSFT), OSL(_SYMBOLS), OSL(_SYMBOLS), EXT_SPC, KC_RALT
       ),
 
   [_EXTEND] = LAYOUT(
-      XXXXXXX, KC_BRID, KC_BRIU, RGB_TOG, RGB_VAD, RGB_VAI,                    KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, XXXXXXX,
-      _______, XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX,                    KC_HOME, KC_PGUP, VIM_ALT, XXXXXXX, KC_DEL,  XXXXXXX,
-      _______, XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, XXXXXXX,
-      _______, XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX,  _______, RESET,   KC_END,  KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                          _______, SFT_ZERO, KC_LALT, _______, _______, _______
+      XXXXXXX, KC_BRID, KC_BRIU, RGB_TOG, RGB_VAD, RGB_VAI,                       KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, XXXXXXX,
+      _______, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_PLUS,                       KC_HOME, KC_PGUP, VIM_ALT, XXXXXXX, KC_DEL,  XXXXXXX,
+      _______, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_MINS,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, XXXXXXX,
+      _______, XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX,  A(KC_BSPC), RESET,   KC_END,  KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                          _______, SFT_ZERO, KC_LALT,    _______, _______, _______
       ),
 
   [_SYMBOLS] = LAYOUT(
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, KC_EXLM, KC_LT,   KC_AT,   KC_GT,   KC_DLR,                    KC_CIRC, KC_LBRC, KC_UNDS, KC_RBRC, KC_MINS, XXXXXXX,
-      XXXXXXX, KC_BSLS, KC_LPRN, KC_DQUO, KC_RPRN, KC_HASH,                   KC_PERC, KC_LCBR, KC_PEQL, KC_RCBR, KC_PIPE, XXXXXXX,
-      XXXXXXX, XXXXXXX, KC_COLN, KC_QUOT, KC_PLUS, KC_EUR,  _______, _______, KC_GRV,  KC_AMPR, KC_ASTR, KC_TILD, KC_QUES, XXXXXXX,
+      XXXXXXX, KC_EXLM, KC_LT,   KC_AT,   KC_GT,   KC_PLUS,                   KC_CIRC, KC_LBRC, KC_UNDS, KC_RBRC, KC_EUR,  XXXXXXX,
+      XXXXXXX, KC_BSLS, KC_LPRN, KC_DQUO, KC_RPRN, KC_MINS,                   KC_PERC, KC_LCBR, KC_PEQL, KC_RCBR, KC_PIPE, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_TILD, KC_QUOT, KC_HASH, KC_DLR,  _______, _______, KC_GRV,  KC_AMPR, KC_ASTR, KC_COLN, KC_QUES, XXXXXXX,
                                           _______, KC_SPC,  _______, _______, _______, _______
       ),
 };
