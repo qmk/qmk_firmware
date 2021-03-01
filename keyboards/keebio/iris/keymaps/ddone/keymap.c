5073758f3b95b9abaf95db45f9480ffcadf606f8
Mykyta Poturai <ddone@aruko.org>
2021-03-01 17:30:44 +0200

    [Keymap] Add license and convert defines to enum

--- keyboards/keebio/iris/keymaps/ddone/keymap.c
+++ keyboards/keebio/iris/keymaps/ddone/keymap.c
@@ -1,9 +1,27 @@
+/* Copyright 2021 Mykyta Poturai <ddone@aruko.org>
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
 #include QMK_KEYBOARD_H
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+enum custom_layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
 
 #define SPCFN1 LT(_LOWER, KC_SPC)
 #define SFTESC LSFT_T(KC_ESC)
