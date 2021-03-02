13ae5a3c3ca988be90d46269f4716b457d12d671
hanachi-ap <75919318+hanachi-ap@users.noreply.github.com>
2021-03-02 15:41:56 +0900

    Update keyboards/willow/willow64/keymaps/buildtest/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/willow/willow64/keymaps/buildtest/config.h
+++ keyboards/willow/willow64/keymaps/buildtest/config.h
@@ -17,11 +17,11 @@
 #pragma once
 
 #ifdef  RGBLED_SPLIT
-  #undef RGBLED_SPLIT
-  #define RGBLED_SPLIT {35, 35}
+#    undef RGBLED_SPLIT
+#    define RGBLED_SPLIT {35, 35}
 #endif
 
 #ifdef RGBLED_NUM
-  #undef RGBLED_NUM
-  #define RGBLED_NUM 70
+#    undef RGBLED_NUM
+#    define RGBLED_NUM 70
 #endif
