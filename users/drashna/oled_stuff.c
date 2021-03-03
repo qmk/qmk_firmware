212c19bdbe282f4ad86dbb61ade69c12a78dcccb
Drashna Jael're <drashna@live.com>
2021-03-02 20:47:24 -0800

    [CHANGE] Tweak oleds for compatibility

--- users/drashna/oled_stuff.c
+++ users/drashna/oled_stuff.c
@@ -236,8 +236,8 @@ void render_bootmagic_status(void) {
 #endif
     oled_write_P(PSTR(" "), false);
     oled_write_P(PSTR(OLED_RENDER_BOOTMAGIC_ONESHOT), !is_oneshot_enabled());
-    oled_write_P(PSTR(" "), false);
 #ifdef SWAP_HANDS_ENABLE
+    oled_write_P(PSTR(" "), false);
     oled_write_P(PSTR(OLED_RENDER_BOOTMAGIC_SWAP), swap_hands);
 #endif
 #ifdef OLED_DISPLAY_128X64
