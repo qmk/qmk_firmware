c39c0fa914abdd81f349563a3a284a34960c5090
Matteo Montemarani <71832926+mm0nte@users.noreply.github.com>
2021-03-03 16:34:29 +0100

    Update keyboards/pizzakeyboards/pizza65/pizza65.h
    Co-authored-by: Jonas Gessner <JonasGessner@users.noreply.github.com>

--- keyboards/pizzakeyboards/pizza65/pizza65.h
+++ keyboards/pizzakeyboards/pizza65/pizza65.h
@@ -51,8 +51,8 @@
  ) \
  { \
      { K00, K01,   K02,   K03,   K04,   K05, K06,   K07,   K08, K09,   K0A, K0B,   K0C, KC_NO,   K0E, K0F }, \
-     { K10, KC_NO, K12,   K13,   K14,   K15, K16,   K17,   K18, K19,   K1A, K1B,   K1C,   K1D, KC_NO, K1F }, \
-     { K20, KC_NO, K22,   K23,   K24,   K25, K26,   K27,   K28, K29,   K2A, K2B,   K2C,   K2D,   K2E, K2F }, \
+     { K10, KC_NO, K12,   K13,   K14,   K15, K16,   K17,   K18, K19,   K1A, K1B,   K1C,   K1D, K2D, K1F }, \
+     { K20, KC_NO, K22,   K23,   K24,   K25, K26,   K27,   K28, K29,   K2A, K2B,   K2C,   KC_NO,   K2E, K2F }, \
      { K30,   K31, K32,   K33,   K34,   K35, K36,   K37,   K38, K39,   K3A, K3B, KC_NO,   K3D,   K3E, K3F }, \
      { K40, K41,   K42, KC_NO, KC_NO, KC_NO, K46, KC_NO, KC_NO, KC_NO, K4A, K4B, KC_NO,   K4D,   K4E, K4F }, \
  }
