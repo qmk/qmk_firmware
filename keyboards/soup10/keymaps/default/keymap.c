4924e5e9cd920af6a7c13400729d01d5439bb596
Sadeeed <sadeeedw@gmail.com>
2021-03-04 11:55:30 +0500

    made _FN accessible

--- keyboards/soup10/keymaps/default/keymap.c
+++ keyboards/soup10/keymaps/default/keymap.c
@@ -23,10 +23,10 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A, 
-        KC_B, KC_C, KC_D, 
-        KC_E, KC_F, KC_G, 
-        KC_H, KC_I, KC_J),
+        KC_F13, 
+        KC_F14, KC_F15, KC_F16, 
+        KC_F17, KC_F18, KC_F19, 
+        KC_F20, KC_F21, MO(_FN)),
 
     [_FN] = LAYOUT(
         RESET, 
