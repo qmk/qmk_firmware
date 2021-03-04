b2f10d42a1038b507732c8a0cb8e95d7e5e3168f
rainsff <44819800+rainsff@users.noreply.github.com>
2021-03-04 14:04:46 -0800

    fix

--- keyboards/yasui/config.h
+++ keyboards/yasui/config.h
@@ -1,5 +1,19 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+/* Copyright 2021 Regan Palmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
 
 #include "config_common.h"
 
@@ -20,17 +34,12 @@
 #define MATRIX_COL_PINS { D7, B4, B6, B2, B3, B1, F7, F6, F5, F4 }
 #define UNUSED_PINS
 
+/* encoder pins */
 #define ENCODERS_PAD_A { D3 }
 #define ENCODERS_PAD_B { D2 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 3
-#endif
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
@@ -49,6 +58,7 @@
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
+#define RGB_DI_PIN D1
 
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_ANIMATIONS
--- keyboards/yasui/keymaps/oled/keymap.c
+++ keyboards/yasui/keymaps/oled/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
 		KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT,
 		KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
-		KC_LCTL, KC_LALT, MO(1), KC_LSFT, KC_MPLY, KC_SPC, KC_SPC, MO(2), KC_RALT, KC_RCTL),
+		KC_LCTL, KC_LALT, MO(1), KC_LSFT, KC_MPLY, KC_SPC, KC_SPC, MO(2), KC_NO, KC_NO),
 
 	KEYMAP(
 		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
@@ -74,4 +74,36 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
             break;
     }
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {return OLED_ROTATION_180;}
+
+char wpm[10];
+
+void oled_task_user(void) {
+    sprintf(wpm, "WPM: %03d", get_current_wpm());
+
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("       rainkeeb \n"), false);
+    oled_write_P(PSTR(" Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("Raise\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("Adjust\n"), false);
+            break;
+        case 3:
+            oled_write_P(PSTR("Game\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+    oled_write_P(PSTR(" "), false);
+    oled_write(wpm, false);
 }
