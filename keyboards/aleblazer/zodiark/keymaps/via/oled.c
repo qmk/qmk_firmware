ebc6be6ed79f7dd63fa18a0d3f3cbc659ed6162b
Aleblazer <60912320+Aleblazer@users.noreply.github.com>
2021-03-03 20:48:38 -0600

    Corrected VIA Keymap oled.c

--- keyboards/aleblazer/zodiark/keymaps/via/oled.c
+++ keyboards/aleblazer/zodiark/keymaps/via/oled.c
@@ -32,9 +32,6 @@ static void print_status_narrow(void) {
         case 0:
             oled_write_ln_P(PSTR("Qwrt"), false);
             break;
-        case 1:
-            oled_write_ln_P(PSTR("Clmk"), false);
-            break;
         default:
             oled_write_P(PSTR("Mod\n"), false);
             break;
@@ -44,14 +41,16 @@ static void print_status_narrow(void) {
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
         case 0:
-        case 1:
-            oled_write_P(PSTR("Base\n"), false);
+			oled_write_P(PSTR("Base\n"), false);
             break;
-        case 2:
+        case 1:
             oled_write_P(PSTR("Raise"), false);
             break;
-        case 3:
+        case 2:
             oled_write_P(PSTR("Lower"), false);
+            break;
+        case 3:
+            oled_write_P(PSTR("Adjust"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
