9fa9457f61c9876020cfb78bc3492b109543b47e
zvecr <git@zvecr.com>
2021-03-05 01:19:23 +0000

    Convert to config

--- keyboards/exclusive/e65/e65.c
+++ keyboards/exclusive/e65/e65.c
@@ -15,24 +15,3 @@
  */
 
 #include "e65.h"
-
-void matrix_init_kb(void) {
-    // put your keyboard start-up code here
-    // runs once when the firmware starts up
-
-    matrix_init_user();
-    led_init_ports();
-}
-
-void led_init_ports(void) {
-    setPinOutput(B6);
-    writePinHigh(B6);
-}
-
-bool led_update_kb(led_t led_state) {
-    if(led_update_user(led_state)) {
-        writePin(B6, !led_state.caps_lock);
-    }
-
-    return true;
-}
--- keyboards/ymdk_np21/ymdk_np21.c
+++ keyboards/ymdk_np21/ymdk_np21.c
@@ -17,19 +17,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "ymdk_np21.h"
-
-void keyboard_pre_init_kb(void) {
-    led_init_ports();
-    keyboard_pre_init_user();
-}
-
-void led_init_ports(void) {
-    setPinOutput(D0);
-}
-
-bool led_update_kb(led_t led_state) {
-    if (led_update_user(led_state)) {
-        writePin(D0, led_state.num_lock);
-    }
-    return true;
-}
