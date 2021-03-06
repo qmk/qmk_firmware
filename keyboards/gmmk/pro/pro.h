8672d6630ba115090e004bd4e7f48b0d9740bfb7
Drashna Jael're <drashna@live.com>
2021-03-06 14:46:02 -0800

    Updates

--- keyboards/gmmk/pro/pro.h
+++ keyboards/gmmk/pro/pro.h
@@ -16,24 +16,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "gmmk.h"
+#include "quantum.h"
 
-#define LAYOUT_gmmk_pro( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f, k0g, k0h, k0i, k0j, k0k, k0l, k0m, k0n,\
-    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f, k1g, k1h, k1i, k1j, k1k, k1l, k1m, k1n,\
-    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f, k2g, k2h, k2i, k2j, k2k, k2l, k2m, k2n,\
-    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f \
+#define ___ KC_NO
+
+#ifdef ENCODER_ENABLE
+#    define encoder_update(clockwise) encoder_update_user(uint8_t index, clockwise)
+#else
+#    define encoder_update(clockwise)
+#endif
+
+//  ESC  F1   F2   F3   F4   F5   F6   F7   F8   F9   F10  F11  F12  Prt    Rotary(Mute)
+//  ~    1    2    3    4    5    6    7    8    9    0    -    (=)  BSpc     Del
+//  Tab  Q    W    E    R    T    Y    U    I    P    [    ]    \             PgUp
+//  Caps A    S    D    F    G    H    J    K    L    ;    "        Enter     PgDn
+//  Sh_L Z    X    C    V    B    N    M    ,    .    ?       Sh_R       Up   End
+//  Ct_L    Win_L   Alt_L      SPACE          Alt_R   FN   Ct_R     Left Down Right
+
+#define LAYOUT_75_ansi( \
+    k13, k26, k36, k31, k33, k07, k2b, k39, k3e, k2m, k2n, k2j, k2l, k1n,     k01, \
+    k16, k17, k27, k37, k0f, k0e, k1e, k1f, k2f, k3f, k0n, k0m, k2e, k2h,     k2d, \
+    k11, k10, k20, k30, k08, k09, k19, k18, k28, k38, k0g, k0h, k29, k0l,     k15, \
+    k21, k12, k22, k32, k0a, k0b, k1b, k1a, k2a, k3a, k0i, k0j,     k2k,      k25, \
+    k00, k14, k24, k34, k0c, k0d, k1d, k1c, k2c, k3c, k0k,    k1h,       k35, k3d, \
+    k06,    k1g,    k4a,       k1k,          k1l,     k4b, k04,     k03, k3b, k05  \
 ) \
 { \
-    { k00, k01, k02, k03, k04, k05, k06, k07}, \
+    { k00, k01, ___, k03, k04, k05, k06, k07}, \
     { k10, k11, k12, k13, k14, k15, k16, k17}, \
-    { k20, k21, k22, k23, k24, k25, k26, k27}, \
+    { k20, k21, k22, ___, k24, k25, k26, k27}, \
     { k30, k31, k32, k33, k34, k35, k36, k37}, \
     { k08, k09, k0a, k0b, k0c, k0d, k0e, k0f}, \
     { k18, k19, k1a, k1b, k1c, k1d, k1e, k1f}, \
     { k28, k29, k2a, k2b, k2c, k2d, k2e, k2f}, \
     { k38, k39, k3a, k3b, k3c, k3d, k3e, k3f}, \
     { k0g, k0h, k0i, k0j, k0k, k0l, k0m, k0n}, \
-    { k1g, k1h, k1i, k1j, k1k, k1l, k1m, k1n}, \
-    { k2g, k2h, k2i, k2j, k2k, k2l, k2m, k2n}  \
+    { k1g, k1h, ___, ___, k1k, k1l, ___, k1n}, \
+    { ___, k2h, ___, k2j, k2k, k2l, k2m, k2n}  \
 }
