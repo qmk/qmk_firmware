9eb61e9046a3e002cc18f7e94aadf1ee9c85cdd0
Sendy Aditya Suryana <sendzation@gmail.com>
2021-03-08 06:52:35 +0700

    Update keyboards/nix_studio/oxalys80/oxalys80.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/nix_studio/oxalys80/oxalys80.c
+++ keyboards/nix_studio/oxalys80/oxalys80.c
@@ -15,41 +15,6 @@
  */
 #include "oxalys80.h"
 
-void matrix_init_kb(void) {
-	// put your keyboard start-up code here
-	// runs once when the firmware starts up
-    setPinOutput(E6);
-   	setPinOutput(B2);
-	matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-	// put your looping keyboard code here
-	// runs every cycle (a lot)
-
-	matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-	// put your per-action keyboard code here
-	// runs for every action, just before processing by the firmware
-
-	return process_record_user(keycode, record);
-}
-
-void led_set_kb(uint8_t usb_led) {
-	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-
-	led_set_user(usb_led);
-}
-
-bool led_update_kb(led_t led_state) {
-    if(led_update_user(led_state)) {
-        writePin(E6, !led_state.caps_lock);
-        writePin(B2, !led_state.scroll_lock);
-    }
-    return true;
-}
 
 void eeconfig_init_kb(void) {  // EEPROM is getting reset!
   rgblight_enable(); // Enable RGB by default
