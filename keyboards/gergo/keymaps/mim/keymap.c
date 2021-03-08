3d5ea40fa9e2099fa3de9dc8b0eeda34e7f92979
Marthin- <martin.guilloux@gmail.com>
2021-03-08 10:00:41 +0100

    improve readability+indentation
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/gergo/keymaps/mim/keymap.c
+++ keyboards/gergo/keymaps/mim/keymap.c
@@ -28,7 +28,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
         }
         break;
-    } return true; }; 
+    } 
+    return true;
+}; 
 // Tap Dance declarations 
 enum {
     TD_QUOTE_GRAVE,
@@ -45,4 +47,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [1] = LAYOUT_gergo(KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_TRNS, KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL, KC_TRNS, KC_TRNS, KC_TILD, KC_LCBR, KC_LCBR, KC_LBRC, KC_LPRN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RPRN, KC_RBRC, KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
         [2] = LAYOUT_gergo(KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_VOLU, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_VOLD, KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_MRWD, KC_MSTP, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
-
