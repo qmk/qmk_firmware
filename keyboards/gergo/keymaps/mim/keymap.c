da68304c7ee4b56a9e500fec5778a805ce2c4e8f
Marthin- <martin.guilloux@gmail.com>
2021-03-08 10:01:18 +0100

    remove useless empty branch
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/gergo/keymaps/mim/keymap.c
+++ keyboards/gergo/keymaps/mim/keymap.c
@@ -25,7 +25,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case TMUX:
         if (record->event.pressed) {
             SEND_STRING(SS_LCTL("x"));
-        } else {
         }
         break;
     } 
