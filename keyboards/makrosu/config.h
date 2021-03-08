684ae77b3693b42ca0f3febf0d30c7a41fd18379
valdiieee <50755960+valdiieee@users.noreply.github.com>
2021-03-08 17:12:02 +0700

    Update keyboards/makrosu/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/makrosu/config.h
+++ keyboards/makrosu/config.h
@@ -47,21 +47,6 @@
 
 // EEPROM usage
 
-// TODO: refactor with new user EEPROM code (coming soon)
-#define EEPROM_MAGIC 0x451F
-#define EEPROM_MAGIC_ADDR 32
-// Bump this every time we change what we store
-// This will automatically reset the EEPROM with defaults
-// and avoid loading invalid data from the EEPROM
-#define EEPROM_VERSION 0x08
-#define EEPROM_VERSION_ADDR 34
-
-// Dynamic keymap starts after EEPROM version
-#define DYNAMIC_KEYMAP_EEPROM_ADDR 35
-// Dynamic macro starts after dynamic keymaps (35+(4*10*6*2)) = (35+480)
-#define DYNAMIC_KEYMAP_MACRO_EEPROM_ADDR 67    // **** CHANGE THIS BASED ON MATRIX_ROWS & MATRIX_COLS ****
-#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 957    // **** CHANGE THIS BASED ON 1024-DYNAMIC_KEYMAP_MACRO_EEPROM_ADDR ****
-#define DYNAMIC_KEYMAP_MACRO_COUNT 16
 
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_ANIMATIONS
