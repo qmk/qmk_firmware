811a9cf201435b550b3c6f3c8f05e36f8d512deb
vitvlkv <vitvlkv@gmail.com>
2021-03-08 19:05:40 +0300

    Adopted layout to the changes in the schematics. Added CAPS_LOCK indication (with B0 LED on ProMicro).

--- keyboards/avalanche/rev1/config.h
+++ keyboards/avalanche/rev1/config.h
@@ -7,12 +7,14 @@
 #define PRODUCT         Avalanche
 #define DESCRIPTION     QMK keyboard firmware for Avalanche
 
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 6
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 8
 
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
 
 #define DIODE_DIRECTION COL2ROW
 
 #define USE_I2C
+
+#define BACKLIGHT_PIN B0
