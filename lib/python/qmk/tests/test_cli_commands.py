1f3905d68b151ffda8fd3a4b3fc38bd694859db5
Zach White <skullydazed@gmail.com>
2021-03-07 19:10:03 -0800

    Begin the process of deprecating bin/qmk in favor of the global cli (#12109)
    * Begin the process of deprecating bin/qmk in favor of the global cli
    
    * Correctly set the qmk bin

--- lib/python/qmk/tests/test_cli_commands.py
+++ lib/python/qmk/tests/test_cli_commands.py
@@ -8,11 +8,7 @@ is_windows = 'windows' in platform.platform().lower()
 
 def check_subcommand(command, *args):
     cmd = ['qmk', command, *args]
-    result = cli.run(cmd, stdin=DEVNULL, combined_output=True)
     return result
-
-
-def check_subcommand_stdin(file_to_read, command, *args):
     """Pipe content of a file to a command and return output.
     """
     with open(file_to_read, encoding='utf-8') as my_file:
