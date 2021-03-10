6ebfabb21f39f94680294a8bc8555dbc7582628a
Xelus22 <preyas22@gmail.com>
2021-03-10 21:38:18 +1100

    use its own board  files

--- platforms/chibios/GENERIC_STM32_L433XC/board/board.mk
+++ platforms/chibios/GENERIC_STM32_L433XC/board/board.mk
@@ -1,8 +1,8 @@
 # List of all the board related files.
-BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO32_L432KC/board.c
+BOARDSRC = $(BOARD_PATH)/board/board.c
 
-# Required include directories	
-BOARDINC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO32_L432KC
+# Required include directories
+BOARDINC = $(BOARD_PATH)/board
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)
