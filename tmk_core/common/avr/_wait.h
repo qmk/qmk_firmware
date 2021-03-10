40c7ecfdeaf50ab76e10854a84aebfcb82ddb092
Joel Challis <git@zvecr.com>
2021-03-10 22:47:36 +0000

    Move gpio wait logic to wait.h (#12067)

--- tmk_core/common/avr/_wait.h
+++ tmk_core/common/avr/_wait.h
@@ -1,8 +1,8 @@
-/* Copyright 2019
+/* Copyright 2021 QMK
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,20 +15,15 @@
  */
 #pragma once
 
-#define SPLIT_USB_DETECT  // Force this on for now
+#include <util/delay.h>
 
-#if defined(STM32F1XX)
-#    define USE_GPIOV1
+#define wait_ms(ms) _delay_ms(ms)
+#define wait_us(us) _delay_us(us)
+
+/* The AVR series GPIOs have a one clock read delay for changes in the digital input signal.
+ * But here's more margin to make it two clocks. */
+#ifndef GPIO_INPUT_PIN_DELAY
+#    define GPIO_INPUT_PIN_DELAY 2
 #endif
 
-#if defined(STM32F1XX) || defined(STM32F2XX) || defined(STM32F4XX) || defined(STM32L1XX)
-#    define USE_I2CV1
-#endif
-
-// teensy
-#if defined(K20x) || defined(KL2x)
-#    define USE_I2CV1
-#    define USE_I2CV1_CONTRIB  // for some reason a bunch of ChibiOS-Contrib boards only have clock_speed
-#    define USE_GPIOV1
-#    define STM32_SYSCLK KINETIS_SYSCLK_FREQUENCY
-#endif
+#define waitInputPinDelay() __builtin_avr_delay_cycles(GPIO_INPUT_PIN_DELAY)
