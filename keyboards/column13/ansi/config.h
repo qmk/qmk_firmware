d3badcdcf9a1e19fa3a37071c2931952b41ae6d5
kazhida <kazhida@abplus.com>
2021-03-10 14:58:33 +0900

    MANUFACTURERとPRODUCTを修正

--- keyboards/column13/ansi/config.h
+++ keyboards/column13/ansi/config.h
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID    0xAB01
 #define PRODUCT_ID   0xC130
 #define DEVICE_VER   0x0001
-#define MANUFACTURER ABplus Inc. kazhida
-#define PRODUCT      ansi
+#define MANUFACTURER ABplus
+#define PRODUCT      ABplus Column13-ansi
 
 /* key matrix size */
 #define MATRIX_ROWS 6
