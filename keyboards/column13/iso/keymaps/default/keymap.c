18ff23052e4be8c1027833a637723edff3a19039
kazhida <kazhida@abplus.com>
2021-03-11 13:24:26 +0900

    isoとorthoを実装

--- keyboards/column13/iso/keymaps/default/keymap.c
+++ keyboards/column13/iso/keymaps/default/keymap.c
@@ -33,7 +33,7 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, KC_BSPC,
-KL_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KL_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,
         KC_LCTL, KC_LGUI, KC_DEL,  KA_SPC,  KL_EISU,         KS_SPC,  KL_KANA, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT,
 
