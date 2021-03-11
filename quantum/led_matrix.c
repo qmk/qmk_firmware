3212c7229b5d7fa80aa50019d3899311bc66b9ed
fauxpark <fauxpark@gmail.com>
2021-03-12 08:06:50 +1100

    LED Matrix: Clean up includes

--- quantum/led_matrix.c
+++ quantum/led_matrix.c
@@ -17,9 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "quantum.h"
 #include "led_matrix.h"
 #include "progmem.h"
 #include "config.h"
