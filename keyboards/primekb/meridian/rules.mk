0b50950c3326687580e30d25f34b9c283d2caef4
holtenc <holtenc@gmail.com>
2021-03-12 01:17:04 -0600

    Update rules.mk

--- keyboards/primekb/meridian/rules.mk
+++ keyboards/primekb/meridian/rules.mk
@@ -21,4 +21,4 @@ AUDIO_ENABLE = no           # Audio output
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
-DEFAULT_FOLDER = primekb/meridian/v1.1
+DEFAULT_FOLDER = primekb/meridian/v1_1
