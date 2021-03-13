20066091b152e79fdc4ea2724bd30f5d6ad7e088
matthewdias <matthewdias@me.com>
2021-03-13 04:30:18 -0600

    Update copyright comments

--- keyboards/barleycorn_smd/matrix.c
+++ keyboards/barleycorn_smd/matrix.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2012-2020 Jun Wako, Jack Humbert, Yiancar
+Copyright 2012-2021 Jun Wako, Jack Humbert, Yiancar, Matthew Dias
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
