c6eac704158f089dc71300543c5d33a5973990cd
Simon Arlott <sa.me.uk>
2021-03-13 10:51:22 +0000

    Update YMDK NP24 readme, reformat build options

--- keyboards/ymdk/np24/u4rgb6/readme.md
+++ keyboards/ymdk/np24/u4rgb6/readme.md
@@ -9,12 +9,10 @@ Not to be confused with the 8 RGB LED version (white PCB) that has an ATmega32A 
 
 Make example for this keyboard (after setting up your build environment):
 
-    make ymdk/np24rgb:default
+    make ymdk/np24/u4rgb6:default
 
-Flashing example for this keyboard ([after setting up the bootloadHID flashing environment](https://docs.qmk.fm/#/flashing_bootloadhid))
+Flashing example for this keyboard:
 
-    make ymdk/np24rgb:default:flash
-
-**Reset Key**: Hold down the 'Top Left Key' (USB on top) while plugging in the keyboard, or press the button on the bottom of the PCB.
+    make ymdk/np24/u4rgb6:default:flash
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
--- keyboards/ymdk/np24/u4rgb6/rules.mk
+++ keyboards/ymdk/np24/u4rgb6/rules.mk
@@ -5,15 +5,20 @@ MCU = atmega32u4
 BOOTLOADER = atmel-dfu
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = lite # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no    # Mouse keys
-EXTRAKEY_ENABLE = yes   # Audio control and System control
-CONSOLE_ENABLE = no     # Console for debug
-COMMAND_ENABLE = no     # Commands for debug and configuration
-SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
-BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 
 LAYOUTS = ortho_6x4 numpad_6x4 ortho_4x6
