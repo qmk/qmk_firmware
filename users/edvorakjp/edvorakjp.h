900744267cf18af6ec56da5d3bd12d35741bb57f
epaew <epaew.333@gmail.com>
2021-03-13 09:24:49 +0900

    Run `clang-format -style=file -i users/edvorakjp/*.{c,h}`

--- users/edvorakjp/edvorakjp.h
+++ users/edvorakjp/edvorakjp.h
@@ -9,22 +9,22 @@
 extern keymap_config_t keymap_config;
 
 enum edvorakjp_layers {
-  _EDVORAK = 0,
-  _LOWER,
-  _RAISE,
-  _ADJUST,
-  _EXTRA,
+    _EDVORAK = 0,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+    _EXTRA,
 };
 
 enum edvorakjp_keycodes {
-  EDVORAK = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  KC_MAC,
-  KC_WIN,
-  KC_JPN,
-  KC_ENG,
-  NEW_SAFE_RANGE
+    EDVORAK = SAFE_RANGE,
+    LOWER,
+    RAISE,
+    KC_MAC,
+    KC_WIN,
+    KC_JPN,
+    KC_ENG,
+    NEW_SAFE_RANGE,
 };
 
 #define KC_LC(k) LCTL_T(KC_##k)
@@ -37,18 +37,18 @@ enum edvorakjp_keycodes {
 #define KC_RA(k) RALT_T(KC_##k)
 
 enum tap_dance_code {
-  TD_LOWER = 0,
-  TD_RAISE
+    TD_LOWER = 0,
+    TD_RAISE,
 };
 
 // base
-void dvorakj_layer_off(void);
-void matrix_init_user(void);
-void matrix_init_keymap(void);
+void     dvorakj_layer_off(void);
+void     matrix_init_user(void);
+void     matrix_init_keymap(void);
 uint32_t layer_state_set_user(uint32_t state);
 uint32_t layer_state_set_keymap(uint32_t state);
-bool process_record_user(uint16_t keycode, keyrecord_t *record);
-bool process_record_keymap(uint16_t keycode, keyrecord_t *record);
+bool     process_record_user(uint16_t keycode, keyrecord_t *record);
+bool     process_record_keymap(uint16_t keycode, keyrecord_t *record);
 
 // status
 void edvorakjp_status_init(void);
@@ -67,4 +67,4 @@ bool process_record_edvorakjp_config(uint16_t keycode, keyrecord_t *record);
 bool process_record_layer(uint16_t keycode, keyrecord_t *record);
 bool process_record_ime(uint16_t keycode, keyrecord_t *record);
 
-#endif // EDVORAKJP
+#endif  // EDVORAKJP
