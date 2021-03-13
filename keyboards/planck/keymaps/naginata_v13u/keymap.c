97a8565de0ad15620e1c02f772599bbcfa508b40
eswai <>
2021-03-13 21:21:06 +0100

    NM

--- keyboards/planck/keymaps/naginata_v13u/keymap.c
+++ keyboards/planck/keymaps/naginata_v13u/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SRLBY] = LAYOUT_planck_grid(
     KC_TAB        ,KC_Q          ,KC_W          ,KC_E          ,KC_R          ,KC_T          ,KC_Y          ,KC_U          ,KC_I          ,KC_O          ,KC_P          ,KC_BSPC       , \
     CTL_T(KC_ESC) ,KC_A          ,KC_S          ,KC_D          ,KC_F          ,KC_G          ,KC_H          ,KC_J          ,KC_K          ,KC_L          ,KC_SCLN       ,KC_QUOT       , \
-    KC_LSFT       ,KC_Z          ,KC_X          ,KC_C          ,KC_V          ,KC_B          ,KC_M          ,KC_N          ,KC_COMM       ,KC_DOT        ,KC_SLSH       ,KC_UP       , \
+    KC_LSFT       ,KC_Z          ,KC_X          ,KC_C          ,KC_V          ,KC_B          ,KC_N          ,KC_M          ,KC_COMM       ,KC_DOT        ,KC_SLSH       ,KC_UP       , \
     KC_LCTL       ,KC_LALT       ,KC_LCMD       ,MO(_LOWER)    ,XXXXXXX       ,LSFT_T(KC_SPC),XXXXXXX       ,LSFT_T(KC_ENT),MO(_RAISE)    ,KC_LEFT       ,KC_DOWN       ,KC_RGHT
   ),
 
