3f1162dcb6b9f90ac49cc938a772cc88be2d77ea
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-03-14 14:23:04 -0700

    rework layout macro
    Removes the two positions that don't exist on the PCB.

--- keyboards/m10a/m10a.h
+++ keyboards/m10a/m10a.h
@@ -2,15 +2,17 @@
 
 #include "quantum.h"
 
+#define ___ KC_NO
+
 #define LAYOUT( \
       k00, k01, k02, \
       k10, k11, k12, \
       k20, k21, k22, \
-      k30, k31, k32  \
+                k32  \
 ) \
 { \
     { k00, k01, k02 }, \
     { k10, k11, k12 }, \
     { k20, k21, k22 }, \
-    { k30, k31, k32 } \
+    { ___, ___, k32 } \
 }
