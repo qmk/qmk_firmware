1960df1e08038324602f80441263006297fdbd2f
Armando Magalhães <amagalhaes@netflix.com>
2021-03-17 19:39:40 +0100

    fix: use XXXXXX and _______ instead

--- keyboards/crkbd/keymaps/armand1m/keymap.c
+++ keyboards/crkbd/keymaps/armand1m/keymap.c
@@ -46,17 +46,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_split_3x6_3(
-      KC_TRANSPARENT,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,      KC_9,    KC_0, KC_TRANSPARENT,
-      KC_TRANSPARENT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                    KC_LEFT, KC_DOWN,   KC_UP,  KC_RIGHT,   KC_NO, KC_TRANSPARENT,
-      KC_TRANSPARENT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO, KC_TRANSPARENT,
-                            KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+      _______,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,                    KC_6,      KC_7,      KC_8,        KC_9,      KC_0, _______,
+      _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                 KC_LEFT,   KC_DOWN,     KC_UP,    KC_RIGHT,   XXXXXXX, _______,
+      _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                 XXXXXXX,   XXXXXXX,   XXXXXXX,     XXXXXXX,   XXXXXXX, _______,
+                                                  _______, _______, _______,     _______, _______, _______
   ),
 
   [2] = LAYOUT_split_3x6_3(
-      KC_TRANSPARENT, KC_EXLM,   KC_AT, KC_HASH,         KC_DLR, KC_PERC,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRANSPARENT,
-      KC_TRANSPARENT,   KC_NO,   KC_NO,   KC_NO, ARROW_FUNCTION,   KC_LT,             KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
-      KC_TRANSPARENT,   KC_NO,   KC_NO,   KC_NO,          KC_NO,   KC_GT,             KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
-                            KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+      _______,   KC_EXLM,     KC_AT,   KC_HASH,         KC_DLR, KC_PERC,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+      _______,   XXXXXXX,   XXXXXXX,   XXXXXXX, ARROW_FUNCTION,   KC_LT,             KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,        XXXXXXX,   KC_GT,             KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+                                                  _______, _______, _______,     _______, _______, _______
   )
 };
 
