287836bdf645123ddc81d4c0ee75eb923a2c84ba
Robert Verst <robert@verst.eu>
2021-03-17 08:20:25 +0100

    Add license

--- users/rverst/unicode.h
+++ users/rverst/unicode.h
@@ -1,5 +1,20 @@
-#ifndef RV_UNICODE_H
-#define RV_UNICODE_H
+/* Copyright 2021 Robert Verst <robert@verst.eu> @rverst
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #ifdef UNICODE_SELECTED_MODES
 #    undef UNICODE_SELECTED_MODES
@@ -14,4 +29,3 @@ const uint32_t PROGMEM unicode_map[] = {
     [SNEK]  = 0x1F40D,
 };
 
-#endif  // RV_UNICODE_H
