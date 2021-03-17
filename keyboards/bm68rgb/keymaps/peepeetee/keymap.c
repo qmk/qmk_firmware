cb08ac06e571e11f4bd91ee25358fdc6e28829db
peepeetee <yuyangzhao17@gmail.com>
2021-03-17 04:03:27 -0500

    add GPL headers

--- keyboards/bm68rgb/keymaps/peepeetee/keymap.c
+++ keyboards/bm68rgb/keymaps/peepeetee/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 peepeetee
+/* Copyright 2019 ash0x0 2021 peepeetee
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
