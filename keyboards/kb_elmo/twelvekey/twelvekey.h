4805ed399bdef2276d3fcb6bbe623a32190e92ba
kb-elmo <lorwel@mailbox.org>
2021-03-18 15:38:36 +0100

    rename layout

--- keyboards/kb_elmo/twelvekey/twelvekey.h
+++ keyboards/kb_elmo/twelvekey/twelvekey.h
@@ -26,7 +26,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT( \
+#define LAYOUT_ortho_3x4( \
     k00, k01, k02, k03, \
     k10, k11, k12, k13, \
     k20, k21, k22, k23  \
