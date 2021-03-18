babbb98106ff785586cd3e6d06278e1027f7a624
kopibeng <52724926+kopibeng@users.noreply.github.com>
2021-03-18 19:37:11 +0800

    Update keyboards/kopibeng/xt65/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kopibeng/xt65/rules.mk
+++ keyboards/kopibeng/xt65/rules.mk
@@ -20,5 +20,3 @@ BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-
-LAYOUTS = 65_7u_split_bs 65_625u
