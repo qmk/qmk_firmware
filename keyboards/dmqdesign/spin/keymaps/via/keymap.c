c1cd8186bc838b1aa26a0c282d3dc58a8db8e525
adamkraus6 <adamkraus6@gmail.com>
2021-03-19 16:07:08 -0600

    updated copyrights

--- keyboards/dmqdesign/spin/keymaps/via/keymap.c
+++ keyboards/dmqdesign/spin/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019-2020 adamkraus6
+/* Copyright 2020-2021 adamkraus6
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
