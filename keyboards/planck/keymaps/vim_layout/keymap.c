67dc8f0692c9883dc78b32433452c2f49402089b
amoulin <amoulin@apple.com>
2021-03-19 19:25:41 +0000

    Fri 19 Mar 2021 19:18:58 GMT

--- keyboards/planck/keymaps/vim_layout/keymap.c
+++ keyboards/planck/keymaps/vim_layout/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2014-2017 Jack Humbert
+/* Copyright 2021 Arthur Moulin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
