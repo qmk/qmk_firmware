3e015e9f9b126a88bbbefbfa814cce393d5357e9
jath03 <jackthughes@yahoo.com>
2021-03-18 18:08:35 -0700

    Adding keychron keyboards to the automatic build script.

--- bin/build_all.py
+++ bin/build_all.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-BOARDS = ['redragon/k552', 'redragon/k556']
+BOARDS = ['redragon/k552', 'redragon/k556', 'keychron/k4', 'keychron/k6']
 
 for kb in BOARDS:
     subprocess.run(f"bin/qmk compile -kb {kb} -km default -j{os.cpu_count()}", shell=True)
