997c63de779cfc2d310c20f58d02eebca6e4d1e5
Andrew L <andrewl@a-personal.lan>
2021-03-18 21:50:15 -0400

    add missing comma

--- keyboards/preonic/keymaps/ALysaght/keymap.c
+++ keyboards/preonic/keymaps/ALysaght/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid(
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
   KC_TRNS, KC_F11,  KC_F12,  KC_NO,   KC_PSLS, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_MINS, KC_EQL,  KC_RBRC,
   KC_TRNS, KC_INS,  KC_HOME, KC_PGUP, KC_PAST, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_NO,   KC_NO,   KC_BSLS,
   KC_TRNS, KC_DEL,  KC_END,  KC_PGDN, KC_NO,   KC_1,    KC_2,    KC_3,    KC_PDOT, KC_NO,   KC_NO,   KC_ENT,
