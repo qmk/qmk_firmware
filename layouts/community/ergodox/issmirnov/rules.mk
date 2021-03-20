1f9a81e9340180d12a9b1562942b9bf624af5de9
Ivan Smirnov <isgsmirnov@gmail.com>
2021-03-20 00:44:40 -0600

    Update layouts/community/ergodox/issmirnov/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- layouts/community/ergodox/issmirnov/rules.mk
+++ layouts/community/ergodox/issmirnov/rules.mk
@@ -1,9 +1,3 @@
-# https://github.com/qmk/qmk_firmware/blob/master/docs/getting_started_make_guide.md
-ifndef QUANTUM_DIR
-    include ../../../../Makefile
-endif
-
 # Enable ORYX options
 ORYX_ENABLE = yes
 WEBUSB_ENABLE = yes
-
