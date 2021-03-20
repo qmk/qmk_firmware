45a481952e15065e102c0ac4f0ca6d414bddfa88
Sean Johnson <sean@skj.dev>
2021-03-20 09:20:26 -0500

    removed unused MIDI comment
    Signed-off-by: Sean Johnson <sean@skj.dev>

--- keyboards/planck/keymaps/ttys0/config.h
+++ keyboards/planck/keymaps/ttys0/config.h
@@ -21,13 +21,7 @@
 #endif
 
 /*
- * MIDI options
- */
-
-/* Prevent use of disabled MIDI features in the keymap */
-//#define MIDI_ENABLE_STRICT 1
-
-/* enable basic MIDI features:
+    enable basic MIDI features:
    - MIDI notes can be sent when in Music mode is on
 */
 
