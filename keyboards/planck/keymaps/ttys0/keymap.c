8e3cb73ed5b355d210ceb2931fa3d96bb99af83e
Sean Johnson <sean@skj.dev>
2021-03-20 08:14:00 -0500

    added GPL2+ compatible license header
    Signed-off-by: Sean Johnson <sean@skj.dev>

--- keyboards/planck/keymaps/ttys0/keymap.c
+++ keyboards/planck/keymaps/ttys0/keymap.c
@@ -1,3 +1,19 @@
+/* Copyright 2021 Sean Johnson
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
@@ -178,9 +194,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [FUNC] = LAYOUT_planck_grid(
-        rset,   f7,   f5,   f3,   f1,   f9,   f8,  f10,   f2,   f4,   f6, mute,
-        powr,  f17,  f15,  f13,  f11,  f19,  f18,  f20,  f12,  f14,  f16, volu,
-        brup, ____, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, vold,
+        rset,   f7,   f5,   f3,   f1,   f9,   f8,  f10,   f2,   f4,   f6, volu,
+        powr,  f17,  f15,  f13,  f11,  f19,  f18,  f20,  f12,  f14,  f16, vold,
+        brup, ____, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, mute,
         brdn, caps, ____, ____, ____, ____, ____, ____, prev, next, play, ____
     ),
 };
