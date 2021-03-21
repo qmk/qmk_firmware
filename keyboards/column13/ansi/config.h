c5a5155ea0939a5481dd40686f257a50845a1a1d
kazhida <kazhida@abplus.com>
2021-03-21 14:29:31 +0900

    pull requestのための調整

--- keyboards/column13/ansi/config.h
+++ keyboards/column13/ansi/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0xC130
 #define DEVICE_VER   0x0001
 #define MANUFACTURER ABplus
-#define PRODUCT      ABplus Column13-ansi
+#define PRODUCT      Column13-ansi
 
 /* key matrix size */
 #define MATRIX_ROWS 6
