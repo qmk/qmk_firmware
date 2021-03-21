b69c259b052d533bfeecf50e69c7c8edf7ff4c11
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-03-21 12:01:44 -0700

    fixed date

--- keyboards/converter/periboard_512/periboard_512.c
+++ keyboards/converter/periboard_512/periboard_512.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 Sife
+/* Copyright 2021 Sife
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
