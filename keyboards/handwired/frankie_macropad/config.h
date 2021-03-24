438d2c864d32e36432159be9d97dfd9479989e71
Eric Wood <eric@ericwood.org>
2021-03-23 20:37:53 -0600

    Disable action oneshot to reduce binary size

--- keyboards/handwired/frankie_macropad/config.h
+++ keyboards/handwired/frankie_macropad/config.h
@@ -141,7 +141,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define NO_ACTION_ONESHOT
 
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
