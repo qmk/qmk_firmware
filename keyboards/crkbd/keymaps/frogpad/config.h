11f3f69928a2174f8be17277723296d58cd21cdb
konomu <45617438+konomu@users.noreply.github.com>
2021-03-24 01:47:02 -0700

    Update config.h

--- keyboards/crkbd/keymaps/frogpad/config.h
+++ keyboards/crkbd/keymaps/frogpad/config.h
@@ -1,9 +1,6 @@
 /*
 This is the c configuration file for the keymap
 
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
