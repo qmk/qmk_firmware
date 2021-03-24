15c6a1740f3145ab84216c292ec174fb9a951fd7
SonOfAres <nicolas@iriskeyboards.com>
2021-03-24 19:48:54 -0400

    adding iriskeyboards PCB rev0

---
+++ quantum/version.h
@@ -0,0 +1,4 @@
+#define QMK_VERSION "0.12.20-3-ga5576f-dirty"
+#define QMK_BUILDDATE "2021-03-24-11:40:56"
+#define CHIBIOS_VERSION "breaking_2021_q1-dirty"
+#define CHIBIOS_CONTRIB_VERSION "breaking_2021_q1-dirty"
