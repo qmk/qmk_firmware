6ea498026ef0dd1b2a8c289a0e03de65d9ef44e9
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-03-23 23:44:44 -0700

    update VENDOR_ID and PRODUCT_ID

--- keyboards/ymdk/ym68/config.h
+++ keyboards/ymdk/ym68/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x594D // "YM"
+#define PRODUCT_ID      0xD896
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    YMDK
 #define PRODUCT         YM68
