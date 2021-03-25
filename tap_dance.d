9efc5fe4ff0ba63ae7a76f99a095aecef26c937c
andresteare <andresteare@gmail.com>
2021-03-25 19:05:04 -0300

    added handwired Compaq MX-11800 keyboard

---
+++ tap_dance.d
@@ -0,0 +1,139 @@
+.build/obj_handwired_compaq_mx11800_via/../../tap_dance.o: \
+ keyboards/handwired/compaq_mx11800/keymaps/via/../../tap_dance.c \
+ keyboards/handwired/compaq_mx11800/config.h quantum/config_common.h \
+ tmk_core/common/pin_defs.h tmk_core/common/avr/pin_defs.h \
+ quantum/audio/song_list.h quantum/audio/musical_notes.h \
+ .build/obj_handwired_compaq_mx11800/src/info_config.h \
+ .build/obj_handwired_compaq_mx11800/src/layouts.h \
+ keyboards/handwired/compaq_mx11800/keymaps/via/../../tap_dance.h \
+ quantum/quantum.h tmk_core/common/wait.h quantum/matrix.h \
+ quantum/keymap.h tmk_core/common/action.h tmk_core/common/keyboard.h \
+ tmk_core/common/keycode.h tmk_core/common/action_code.h \
+ tmk_core/common/action_macro.h tmk_core/common/progmem.h \
+ tmk_core/common/report.h tmk_core/common/host.h \
+ tmk_core/common/host_driver.h quantum/led.h tmk_core/common/debug.h \
+ tmk_core/common/print.h quantum/util.h quantum/bitwise.h \
+ tmk_core/common/sendchar.h tmk_core/common/avr/_print.h \
+ tmk_core/common/avr/xprintf.h quantum/keycode_config.h \
+ tmk_core/common/eeconfig.h quantum/quantum_keycodes.h \
+ tmk_core/common/action_layer.h tmk_core/common/bootloader.h \
+ tmk_core/common/timer.h tmk_core/common/avr/timer_avr.h \
+ tmk_core/common/sync_timer.h tmk_core/common/gpio.h \
+ tmk_core/common/avr/gpio.h tmk_core/common/atomic_util.h \
+ tmk_core/common/avr/atomic_util.h tmk_core/common/action_util.h \
+ tmk_core/common/action_tapping.h quantum/send_string.h \
+ quantum/send_string_keycodes.h tmk_core/common/suspend.h \
+ quantum/process_keycode/process_tap_dance.h \
+ quantum/process_keycode/process_terminal_nop.h \
+ quantum/process_keycode/process_space_cadet.h \
+ quantum/process_keycode/process_magic.h \
+ quantum/process_keycode/process_grave_esc.h quantum/dynamic_keymap.h \
+ quantum/via.h
+
+keyboards/handwired/compaq_mx11800/config.h:
+
+quantum/config_common.h:
+
+tmk_core/common/pin_defs.h:
+
+tmk_core/common/avr/pin_defs.h:
+
+quantum/audio/song_list.h:
+
+quantum/audio/musical_notes.h:
+
+.build/obj_handwired_compaq_mx11800/src/info_config.h:
+
+.build/obj_handwired_compaq_mx11800/src/layouts.h:
+
+keyboards/handwired/compaq_mx11800/keymaps/via/../../tap_dance.h:
+
+quantum/quantum.h:
+
+tmk_core/common/wait.h:
+
+quantum/matrix.h:
+
+quantum/keymap.h:
+
+tmk_core/common/action.h:
+
+tmk_core/common/keyboard.h:
+
+tmk_core/common/keycode.h:
+
+tmk_core/common/action_code.h:
+
+tmk_core/common/action_macro.h:
+
+tmk_core/common/progmem.h:
+
+tmk_core/common/report.h:
+
+tmk_core/common/host.h:
+
+tmk_core/common/host_driver.h:
+
+quantum/led.h:
+
+tmk_core/common/debug.h:
+
+tmk_core/common/print.h:
+
+quantum/util.h:
+
+quantum/bitwise.h:
+
+tmk_core/common/sendchar.h:
+
+tmk_core/common/avr/_print.h:
+
+tmk_core/common/avr/xprintf.h:
+
+quantum/keycode_config.h:
+
+tmk_core/common/eeconfig.h:
+
+quantum/quantum_keycodes.h:
+
+tmk_core/common/action_layer.h:
+
+tmk_core/common/bootloader.h:
+
+tmk_core/common/timer.h:
+
+tmk_core/common/avr/timer_avr.h:
+
+tmk_core/common/sync_timer.h:
+
+tmk_core/common/gpio.h:
+
+tmk_core/common/avr/gpio.h:
+
+tmk_core/common/atomic_util.h:
+
+tmk_core/common/avr/atomic_util.h:
+
+tmk_core/common/action_util.h:
+
+tmk_core/common/action_tapping.h:
+
+quantum/send_string.h:
+
+quantum/send_string_keycodes.h:
+
+tmk_core/common/suspend.h:
+
+quantum/process_keycode/process_tap_dance.h:
+
+quantum/process_keycode/process_terminal_nop.h:
+
+quantum/process_keycode/process_space_cadet.h:
+
+quantum/process_keycode/process_magic.h:
+
+quantum/process_keycode/process_grave_esc.h:
+
+quantum/dynamic_keymap.h:
+
+quantum/via.h:
