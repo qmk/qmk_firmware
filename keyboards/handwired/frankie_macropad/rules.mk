fd58d5e74da4029834ed9d9c198599857a13b89e
Eric Wood <eric@ericwood.org>
2021-03-25 09:14:34 -0600

    Use LTO_ENABLE instead of EXTRAFLAGS
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/frankie_macropad/rules.mk
+++ keyboards/handwired/frankie_macropad/rules.mk
@@ -21,5 +21,4 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes
-
-EXTRAFLAGS += -flto
+LTO_ENABLE = yes
