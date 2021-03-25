61327f010869cd09b8fed80d628140b67416a011
Adam Honse <calcprogrammer1@gmail.com>
2021-03-25 18:35:14 +0000

    [Ajazz AK33 (SN32F248B)] Add Ajazz AK33, SN34F248B version

---
+++ keyboards/ajazz/ak33/config.h
@@ -0,0 +1,47 @@
+/* Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x0C45
+#define PRODUCT_ID      0x5004
+#define DEVICE_VER      0x0001
+
+#define MANUFACTURER    Ajazz
+#define PRODUCT         AK33
+#define DESCRIPTION     AK33
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 16
+
+#define DIODE_DIRECTION COL2ROW
+
+#define MATRIX_COL_PINS { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, B0 }
+#define MATRIX_ROW_PINS { C15, D11, D10, D9, D8, D7 }
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 1
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+//#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+//#define LOCKING_RESYNC_ENABLE
+
+#include "config_led.h"
