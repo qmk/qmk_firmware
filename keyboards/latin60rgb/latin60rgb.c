b52a145c7a40a6b9496836ee1763d72d4b398690
wangyifu <18438880@qq.com>
2021-03-25 21:53:46 +0800

    Update keyboards/latin60rgb/latin60rgb.c
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- keyboards/latin60rgb/latin60rgb.c
+++ keyboards/latin60rgb/latin60rgb.c
@@ -12,8 +12,6 @@
   * 
   * You should have received a copy of the GNU General Public License 
   * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-     // { 0, B_14, A_14, C_14 },
-     // { 0, H_15, G_15, I_15 },
   */ 
 #include "latin60rgb.h"
 
