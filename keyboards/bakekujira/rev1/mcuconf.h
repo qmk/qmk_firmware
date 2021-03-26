0507ffdd0a6dbb575f4823e02a0c35562e451e6f
alfred <alfredy@gmail.com>
2021-03-25 22:17:00 -0700

    fixing wip fixing leds

--- keyboards/bakekujira/rev1/mcuconf.h
+++ keyboards/bakekujira/rev1/mcuconf.h
@@ -18,11 +18,11 @@
 
 #include_next <mcuconf.h>
 
-// #undef STM32_PWM_USE_TIM3
-// #define STM32_PWM_USE_TIM3 TRUE
+#undef STM32_PWM_USE_TIM3
+#define STM32_PWM_USE_TIM3 TRUE
 
 #undef STM32_SERIAL_USE_USART1
 #define STM32_SERIAL_USE_USART1 TRUE
 
-// #undef STM32_I2C_USE_I2C1
-// #define STM32_I2C_USE_I2C1 TRUE
+#undef STM32_I2C_USE_I2C1
+#define STM32_I2C_USE_I2C1 TRUE
--- keyboards/bakekujira/rev1/rules.mk
+++ keyboards/bakekujira/rev1/rules.mk
@@ -23,8 +23,8 @@ WPM_ENABLE = yes
 # OLED_DRIVER_ENABLE = yes
 
 # RGB related
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+# RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 WS2812_DRIVER = pwm
 #PWM needs patch to <https://github.com/qmk/qmk_firmware/blob/master/platforms/chibios/GENERIC_STM32_F303XC/configs/proton_c.mk>, otherwise defaults to bitbang.
@@ -40,10 +40,10 @@ SERIAL_DRIVER = usart
 # if firmware size over limit, try this option
 # CFLAGS += -flto
 
-# SRC += ./lib/glcdfont.c \
+SRC += ./lib/glcdfont.c \
 
 DEFAULT_FOLDER = bakekujira/rev1
 
-ifeq ($strip $(BAKEKUJIRA)), yes)
-	OPT_DEFS += -DBAKEKUJIRA
-endif
+# ifeq ($strip $(BAKEKUJIRA)), yes)
+# 	OPT_DEFS += -DBAKEKUJIRA
+# endif
