eb7f3aff6fb58433c1ec022e84df0c3b7e8c3f7f
Robert Erdin <robert.erdin@gmail.com>
2021-03-27 15:31:41 +0000

    Add Nav layer to contra

--- keyboards/contra/keymaps/roberterdin/config.h
+++ keyboards/contra/keymaps/roberterdin/config.h
@@ -36,3 +36,5 @@
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+#define TAPPING_TERM 175
--- keyboards/contra/keymaps/roberterdin/keymap.c
+++ keyboards/contra/keymaps/roberterdin/keymap.c
@@ -24,7 +24,8 @@ enum planck_layers {
   _LOWER,
   _RAISE,
   _PLOVER,
-  _ADJUST
+  _ADJUST,
+  _NAV
 };
 
 enum planck_keycodes {
@@ -34,29 +35,35 @@ enum planck_keycodes {
   PLOVER,
   LOWER,
   RAISE,
+  NAV,
   BACKLIT,
-  EXT_PLV
+  EXT_PLV,
+  KC_LSTRT,
+  KC_LEND,
+  KC_PRVWD,
+  KC_NXTWD
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------------------------------------------------.
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+   * |  Tab |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
    * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * |Ctr/Esc|  A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * | Ctrl |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+   * | HYPR | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_planck_mit(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    HYPR_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    LSFT_T(KC_CAPSLOCK), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_ALGR, KC_LCTL, KC_LALT, KC_LGUI, LOWER,       KC_SPC,       RAISE,  LT(_NAV, KC_LEFT), ALGR_T(KC_DOWN), KC_UP,   KC_RGHT
   ),
+
 
   /* Colemak
    * ,-----------------------------------------------------------------------------------.
@@ -102,14 +109,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      | Prev |      |             |      | Next | Vol- | Vol+ | Play |
    * `-----------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT_planck_mit(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______,     _______,      _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, KC_MPRV, _______,     _______,      _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
   ),
 
   /* Raise
@@ -165,98 +172,198 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______,     _______,      _______, _______,  _______, _______, _______
+  ),
+
+    /* Nav
+   * ,-----------------------------------------------------------------------------------.
+   * |      |      |      |      |      |      |      | PWord|  Up  | NWord|      |  Del |
+   * |------+------+------+------+------+-------------+------+------+------+------+------|
+   * |      |      |      |      |      |      |LStart| Left | Down | Right| LEnd |      |
+   * |------+------+------+------+------+------|------+------+------+------+------+------|
+   * |      |      |      |      |      |      |      |      |      |      |      |      |
+   * |------+------+------+------+------+------+------+------+------+------+------+------|
+   * |      |      |      |      |      |             |      |      |      |      |      |
+   * `-----------------------------------------------------------------------------------'
+   */
+  [_NAV] = LAYOUT_planck_mit(
+    _______, _______, _______, _______, _______, _______, _______, KC_PRVWD, KC_UP, KC_NXTWD, _______, _______ ,
+    _______, _______, _______, _______, _______, _______, KC_LSTRT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_LEND,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,      _______,      _______, _______, _______, _______, _______
   )
 
 };
-
+  
 
 #ifdef AUDIO_ENABLE
   float plover_song[][2]     = SONG(PLOVER_SOUND);
   float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
 #endif
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        print("mode just switched to qwerty and this is a huge string\n");
-        set_single_persistent_default_layer(_QWERTY);
+  bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+      switch (keycode) {
+          case QWERTY:
+              if (record->event.pressed) {
+                  print("mode just switched to qwerty and this is a huge string\n");
+                  set_single_persistent_default_layer(_QWERTY);
+              }
+              return false;
+              break;
+          case COLEMAK:
+              if (record->event.pressed) {
+                  set_single_persistent_default_layer(_COLEMAK);
+              }
+              return false;
+              break;
+          case DVORAK:
+              if (record->event.pressed) {
+                  set_single_persistent_default_layer(_DVORAK);
+              }
+              return false;
+              break;
+          case LOWER:
+              if (record->event.pressed) {
+                  layer_on(_LOWER);
+                  update_tri_layer(_LOWER, _RAISE, _ADJUST);
+              } else {
+                  layer_off(_LOWER);
+                  update_tri_layer(_LOWER, _RAISE, _ADJUST);
+              }
+              return false;
+              break;
+          case RAISE:
+              if (record->event.pressed) {
+                  layer_on(_RAISE);
+                  update_tri_layer(_LOWER, _RAISE, _ADJUST);
+              } else {
+                  layer_off(_RAISE);
+                  update_tri_layer(_LOWER, _RAISE, _ADJUST);
+              }
+              return false;
+              break;
+          case NAV:
+            if (record->event.pressed) {
+                layer_on(_NAV);
+            } else {
+                layer_off(_NAV);
+            }
+            return false;
+            break;
+          case BACKLIT:
+              if (record->event.pressed) {
+                  register_code(KC_RSFT);
+#ifdef BACKLIGHT_ENABLE
+                  backlight_step();
+#endif
+                  PORTE &= ~(1 << 6);
+              } else {
+                  unregister_code(KC_RSFT);
+                  PORTE |= (1 << 6);
+              }
+              return false;
+              break;
+          case PLOVER:
+              if (record->event.pressed) {
+#ifdef AUDIO_ENABLE
+                  stop_all_notes();
+                  PLAY_SONG(plover_song);
+#endif
+                  layer_off(_RAISE);
+                  layer_off(_LOWER);
+                  layer_off(_ADJUST);
+                  layer_on(_PLOVER);
+                  if (!eeconfig_is_enabled()) {
+                      eeconfig_init();
+                  }
+                  keymap_config.raw  = eeconfig_read_keymap();
+                  keymap_config.nkro = 1;
+                  eeconfig_update_keymap(keymap_config.raw);
+              }
+              return false;
+              break;
+          case EXT_PLV:
+              if (record->event.pressed) {
+#ifdef AUDIO_ENABLE
+                  PLAY_SONG(plover_gb_song);
+#endif
+                  layer_off(_PLOVER);
+              }
+              return false;
+              break;
+          case KC_PRVWD:
+              if (record->event.pressed) {
+                  if (keymap_config.swap_lctl_lgui) {
+                      register_mods(mod_config(MOD_LALT));
+                      register_code(KC_LEFT);
+                  } else {
+                      register_mods(mod_config(MOD_LCTL));
+                      register_code(KC_LEFT);
+                  }
+              } else {
+                  if (keymap_config.swap_lctl_lgui) {
+                      unregister_mods(mod_config(MOD_LALT));
+                      unregister_code(KC_LEFT);
+                  } else {
+                      unregister_mods(mod_config(MOD_LCTL));
+                      unregister_code(KC_LEFT);
+                  }
+              }
+              break;
+          case KC_NXTWD:
+              if (record->event.pressed) {
+                  if (keymap_config.swap_lctl_lgui) {
+                      register_mods(mod_config(MOD_LALT));
+                      register_code(KC_RIGHT);
+                  } else {
+                      register_mods(mod_config(MOD_LCTL));
+                      register_code(KC_RIGHT);
+                  }
+              } else {
+                  if (keymap_config.swap_lctl_lgui) {
+                      unregister_mods(mod_config(MOD_LALT));
+                      unregister_code(KC_RIGHT);
+                  } else {
+                      unregister_mods(mod_config(MOD_LCTL));
+                      unregister_code(KC_RIGHT);
+                  }
+              }
+              break;
+          case KC_LSTRT:
+              if (record->event.pressed) {
+                  if (keymap_config.swap_lctl_lgui) {
+                      // CMD-arrow on Mac, but we have CTL and GUI swapped
+                      register_mods(mod_config(MOD_LCTL));
+                      register_code(KC_LEFT);
+                  } else {
+                      register_code(KC_HOME);
+                  }
+              } else {
+                  if (keymap_config.swap_lctl_lgui) {
+                      unregister_mods(mod_config(MOD_LCTL));
+                      unregister_code(KC_LEFT);
+                  } else {
+                      unregister_code(KC_HOME);
+                  }
+              }
+              break;
+          case KC_LEND:
+              if (record->event.pressed) {
+                  if (keymap_config.swap_lctl_lgui) {
+                      // CMD-arrow on Mac, but we have CTL and GUI swapped
+                      register_mods(mod_config(MOD_LCTL));
+                      register_code(KC_RIGHT);
+                  } else {
+                      register_code(KC_END);
+                  }
+              } else {
+                  if (keymap_config.swap_lctl_lgui) {
+                      unregister_mods(mod_config(MOD_LCTL));
+                      unregister_code(KC_RIGHT);
+                  } else {
+                      unregister_code(KC_END);
+                  }
+              }
+              break;
       }
-      return false;
-      break;
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-      break;
-    case DVORAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case BACKLIT:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-        #ifdef BACKLIGHT_ENABLE
-          backlight_step();
-        #endif
-        PORTE &= ~(1<<6);
-      } else {
-        unregister_code(KC_RSFT);
-        PORTE |= (1<<6);
-      }
-      return false;
-      break;
-    case PLOVER:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          stop_all_notes();
-          PLAY_SONG(plover_song);
-        #endif
-        layer_off(_RAISE);
-        layer_off(_LOWER);
-        layer_off(_ADJUST);
-        layer_on(_PLOVER);
-        if (!eeconfig_is_enabled()) {
-            eeconfig_init();
-        }
-        keymap_config.raw = eeconfig_read_keymap();
-        keymap_config.nkro = 1;
-        eeconfig_update_keymap(keymap_config.raw);
-      }
-      return false;
-      break;
-    case EXT_PLV:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(plover_gb_song);
-        #endif
-        layer_off(_PLOVER);
-      }
-      return false;
-      break;
+      return true;
   }
-  return true;
-}
