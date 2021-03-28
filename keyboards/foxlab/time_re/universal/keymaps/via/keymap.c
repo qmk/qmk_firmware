f55f9833e25d77e7499e1c39d804f578614fa8d1
matrixzj <matrix.zj@qq.com>
2021-03-28 16:11:40 +0000

    [Keyboard] added Time 80 Reforged by Fox Lab
    * added Time 80 Reforged by Fox Lab
    * split to two sub directories for universal and hotswap pcb
    * enable built-in switch LED support
    * Apply suggestions from code review

--- keyboards/foxlab/time_re/universal/keymaps/via/keymap.c
+++ keyboards/foxlab/time_re/universal/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 REPLACE_WITH_YOUR_NAME
+/* Copyright 2021 matrixzj <matrix.zj@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
