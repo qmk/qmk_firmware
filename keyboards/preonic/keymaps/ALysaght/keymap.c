d4334bd3c81061935ac282d4900581c8eff1b55d
Andrew L <andrewl@a-personal.lan>
2021-03-28 17:23:45 -0400

    add commands

--- keyboards/preonic/keymaps/ALysaght/keymap.c
+++ keyboards/preonic/keymaps/ALysaght/keymap.c
@@ -12,6 +12,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * qmk compile -kb preonic/rev3
+ * qmk flash -kb preonic/rev3
  */
 
 #include QMK_KEYBOARD_H
