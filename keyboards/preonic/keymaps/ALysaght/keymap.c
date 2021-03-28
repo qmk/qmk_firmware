b53c74a29cb82f34479f6a3dccff07d514c43721
Andrew L <andrewl@a-personal.lan>
2021-03-28 17:15:08 -0400

    spilt num layer and clean up

--- keyboards/preonic/keymaps/ALysaght/keymap.c
+++ keyboards/preonic/keymaps/ALysaght/keymap.c
@@ -21,6 +21,7 @@ enum preonic_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
+  _NUM,
   _ADJUST
 };
 
@@ -43,72 +44,94 @@ enum preonic_keycodes {
   GITWTPR,
   GITSTAT,
   GITBDEL,
-  GITMAIN
+  GITMAIN,
+  GITORIG
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [   |
+ * |  `   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |SftEnt|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | NUM  | Ctrl | Alt  | GUI  |Lower | Bksp |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+  KC_GRV,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+  TG(_NUM), KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
+ * | F11  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | F12  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   !  |   @  |   #  |   $  |   7  |   8  |   9  |   -  |   -  |   =  |  ]   |
+ * |      |      |      |      |      |      |      |      |      |      |      |  ]   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  | Ins  | Home | PgUp |   /  |   4  |   5  |   6  |   +  |      |      |  \   |
+ * |      |      |      |      |      |      |      |  <-  |  %>% |   -  |   =  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift| Del  | End  | PgDn |   *  |   1  |   2  |   3  |   .  |      |      | Ent  |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |       0     |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |ScrSht|      | Del  |  Ins |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid(
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
-  KC_TRNS, KC_F11,  KC_F12,  KC_NO,   KC_PSLS, KC_7,    KC_8,    KC_9,    KC_PMNS, KC_MINS, KC_EQL,  KC_RBRC,
-  KC_TRNS, KC_INS,  KC_HOME, KC_PGUP, KC_PAST, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_NO,   KC_NO,   KC_BSLS,
-  KC_TRNS, KC_DEL,  KC_END,  KC_PGDN, KC_NO,   KC_1,    KC_2,    KC_3,    KC_PDOT, KC_NO,   KC_NO,   KC_ENT,
-  _______, _______, _______, _______, _______, KC_0,    KC_0,    _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_F11,  KC_F1,   KC_F2,   KC_F3,         KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+  _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, KC_RBRC,
+  _______, _______, _______, _______,       _______, _______, _______, R_ASIGN, R_PIPE,  KC_MINS, KC_EQL,  _______,
+  _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, SGUI(C(KC_4)), _______, KC_DEL,   KC_INS, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |      |      |      |      |      |      |      |      |  <-  | %>%  |      |
+ * |      |GITCOU|GITFET|GITPUL|GITREB|      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |GITADD|GITSTA|GITCOM|GITPUS|      |      |Origin|      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|      |      |      |      |      |      | main |      |      |      | Ent  |
+ * |      |GITBRC|GITBDE|GITWTA|GITWTP|      |      | Main |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Home | PgDn | PgUp | End  |
+ * |      |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  KC_TRNS, GITCOUT, GITFETC, GITPULL, GITREBA, _______, _______, _______, _______, R_ASIGN,  R_PIPE, _______,
-  KC_TRNS,  GITADD, GITSTAT, GITCOMM, GITPUSH, _______, _______, _______, _______, _______, _______, _______,
-  KC_TRNS, GITBRCH, GITBDEL, GITWTAD, GITWTPR, _______, _______, GITMAIN, _______, _______, _______,  KC_ENT,
+  _______, GITCOUT, GITFETC, GITPULL, GITREBA, _______, _______, _______, _______, _______, _______, _______,
+  _______,  GITADD, GITSTAT, GITCOMM, GITPUSH, _______, _______, GITORIG, _______, _______, _______, _______,
+  _______, GITBRCH, GITBDEL, GITWTAD, GITWTPR, _______, _______, GITMAIN, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
+),
+
+/* "Numpad" layer
+* ,-----------------------------------------------------------------------------------.
+* |      |      |      |      |      |      |  NO  |Clear |  NO  |  NO  |   *  |  NO  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |      |      |      |      |      |      |  NO  |   7  |   8  |   9  |   +  |  NO  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |      |      |      |      |      |      |  NO  |   4  |   5  |   6  |   -  |  NO  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |      |      |      |      |      |      |  NO  |   1  |   2  |   3  |   /  |Enter |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |      |      |      |      |      | Bksp |   0  |   .  | Left | Down |  Up  |Right |
+* `-----------------------------------------------------------------------------------'
+*/
+[_NUM] = LAYOUT(
+  _______, _______, _______, _______, _______, _______, XXXXXXX,    KC_C, XXXXXXX, XXXXXXX, KC_PAST, XXXXXXX,
+  _______, _______, _______, _______, _______, _______, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, XXXXXXX,
+  _______, _______, _______, _______, _______, _______, XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, KC_PMNS, XXXXXXX,
+  _______, _______, _______, _______, _______, _______, XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3, KC_PSLS, KC_PENT,
+  _______, _______, _______, _______, _______, KC_BSPC, KC_KP_0, KC_PDOT, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT
 ),
 
 /* Adjust (Lower + Raise)
@@ -121,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * | Brite|      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
@@ -129,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, _______,
   _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______,
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  BACKLIT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 
@@ -166,7 +189,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case R_ASIGN:
           if (record->event.pressed) {
             // when keycode R_ASIGN is pressed
-            SEND_STRING(" <- ");
+            SEND_STRING("<- ");
           }
           // return false;
           break;
@@ -238,6 +261,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case GITMAIN:
           if (record->event.pressed) {
             SEND_STRING("main ");
+          }
+          break;
+        case GITORIG:
+          if (record->event.pressed) {
+            SEND_STRING("origin ");
           }
           break;
         case BACKLIT:
