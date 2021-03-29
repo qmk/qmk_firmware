cb8a2f576f1dd961f7452ce91e85f31df3890f8b
Robert Verst <robert@verst.eu>
2021-03-29 18:41:50 +0200

    Process the keys...

--- users/rverst/rverst.c
+++ users/rverst/rverst.c
@@ -138,7 +138,7 @@ void eeconfig_init_user(void) {
 __attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t *record) { return true; }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (process_record_keymap(keycode, record)) {
+    if (!process_record_keymap(keycode, record)) {
         return false;
     }
 
