947ad2105717d34e00b61d37a188f773bbb12f4b
Swiftrax <swiftrax@gmail.com>
2021-03-28 18:53:09 -0700

    change pid

--- keyboards/ffkeebs/siris/config.h
+++ keyboards/ffkeebs/siris/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x04D8
-#define PRODUCT_ID      0xEAB1
+#define PRODUCT_ID      0xE96C
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Swiftrax
 #define PRODUCT         Siris
