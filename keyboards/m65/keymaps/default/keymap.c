756f6744072f858393e48500c08af3215fc36951
Alin Marin Elena <alin@elena.space>
2021-03-29 22:20:19 +0100

    Update keyboards/m65/keymaps/default/keymap.c
    Co-authored-by: Sergey Vlasov <sigprof@gmail.com>

--- keyboards/m65/keymaps/default/keymap.c
+++ keyboards/m65/keymaps/default/keymap.c
@@ -55,6 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
       KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
+// clang-format on
 
 // let us assume we start with both layers off
 bool toggle_lwr = false;
