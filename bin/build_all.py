c4edbdada530c156fb0ced096e7661e434175a27
stdvar <stdvar@xyz.com>
2021-03-29 00:23:46 -0400

    Scope all layouts for automatic build

--- bin/build_all.py
+++ bin/build_all.py
@@ -4,4 +4,4 @@ import os
 BOARDS = ['redragon/k552/rev1', 'redragon/k552/rev2', 'redragon/k556', 'keychron/k4', 'keychron/k6', 'ajazz/ak33/rev1', 'ajazz/ak33/rev2']
 
 for kb in BOARDS:
-    subprocess.run(f"bin/qmk compile -kb {kb} -km default -j{os.cpu_count()}", shell=True)
+    subprocess.run(f"bin/qmk compile -kb {kb} -km all -j{os.cpu_count()}", shell=True)
