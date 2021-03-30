c0ebeb4a65a7db175b7c011249f56e416d3c6a1b
Ramon Imbao <ramonimbao@gmail.com>
2021-03-30 21:43:33 +0800

    Add SquishyTKL

---
+++ keyboards/ramonimbao/squishytkl/keymaps/via/config.h
@@ -0,0 +1,6 @@
+#define DYNAMIC_KEYMAP_LAYER_COUNT 1
+// Only way to get this to compile is to reduce layer count to 1.
+// Do TKL users even need more than 1 layer?
+// I'd like that option though. Below also works.
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 2
+// #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 2047
