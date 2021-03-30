084dde1479c2addd40c1d24b2aeee0cf9dc18d18
Swiftrax <swiftrax@gmail.com>
2021-03-29 22:50:35 -0700

    fix define

--- keyboards/feels/feels65/feels65.h
+++ keyboards/feels/feels65/feels65.h
@@ -17,7 +17,7 @@
 
 #include "quantum.h"
 
-#define KC_NO XXX
+#define XXX K_NO
 
 #define LAYOUT_65_iso_blocker( \
   k00,  k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, k0F, \
