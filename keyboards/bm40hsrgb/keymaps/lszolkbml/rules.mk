8dabfc6093359147463b26890f4d250dd48d3301
Dordouille <laurent.sanz@protonmail.com>
2021-03-31 22:32:22 +0200

    fixing game layer pb

--- keyboards/bm40hsrgb/keymaps/lszolkbml/rules.mk
+++ keyboards/bm40hsrgb/keymaps/lszolkbml/rules.mk
@@ -4,5 +4,4 @@ MOUSEKEY_ENABLE = yes # Mouse keys
 BOOTMAGIC_ENABLE = full
 EXTRAKEY_ENABLE = yes # Audio control and System control
 TAP_DANCE_ENABLE = yes
-VIA_ENABLE = yes
 #RGB_MATRIX_ENABLE = no
