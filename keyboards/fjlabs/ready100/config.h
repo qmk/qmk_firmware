adc6e141d61ef3467c671153764fd451dcf42913
Felix Jen <fjen@felixjen.com>
2021-03-30 23:34:14 -0500

    Fixed old comment references

--- keyboards/fjlabs/ready100/config.h
+++ keyboards/fjlabs/ready100/config.h
@@ -21,7 +21,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0x7074 // FJLabs
-#define PRODUCT_ID   0x1003 // BKS65
+#define PRODUCT_ID   0x1003 // Ready100
 #define DEVICE_VER   0x0001 // Version 1
 #define MANUFACTURER    FJLabs
 #define PRODUCT         Ready100
