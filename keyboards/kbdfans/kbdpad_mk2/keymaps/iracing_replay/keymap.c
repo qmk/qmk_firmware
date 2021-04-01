56d72fb265f7370bfe9080abebcf7ac84ba845ca
Aaron VerDow <aaron@verdow.com>
2021-04-01 17:59:03 -0500

    add iracing replay keymap

---
+++ keyboards/kbdfans/kbdpad_mk2/keymaps/iracing_replay/keymap.c
@@ -0,0 +1,177 @@
+#include QMK_KEYBOARD_H
+
+void tap_key(uint16_t keycode) {
+  register_code  (keycode);
+  unregister_code(keycode);
+}
+
+void shift_key(uint16_t keycode) {
+  register_code  (KC_LSFT);
+  tap_key        (keycode);
+  unregister_code(KC_LSFT);
+}
+
+
+// Layer declarations
+enum {
+    DEF_LAYER,
+    MOD_LAYER,
+};
+
+// Tap Dance declarations
+enum {
+    TD_CAR,
+    TD_PRINT,
+    TD_CAM_UP,
+    TD_CAM_DN
+};
+
+enum custom_keycodes {
+    CHASE = 0,
+    REVERSE
+};
+
+void cam_up(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            tap_key(KC_C);           // next cam
+            break;
+        case 2:
+            shift_key(KC_8);
+            shift_key(KC_8);
+            tap_key(KC_2);
+            tap_key(KC_2);
+            tap_key(KC_ENTER);
+            break;
+    }
+}
+
+void cam_down(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            shift_key(KC_C);           // next cam
+            break;
+        case 2:
+            shift_key(KC_8);
+            shift_key(KC_8);
+            tap_key(KC_2);
+            tap_key(KC_0);
+            tap_key(KC_ENTER);
+            break;
+    }
+}
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_CAR] = ACTION_TAP_DANCE_DOUBLE(
+        LSFT(KC_V),
+        LCTL(KC_V)
+    ),
+    [TD_PRINT] = ACTION_TAP_DANCE_DOUBLE(
+        LCTL(LALT(LSFT(KC_S))),
+        LGUI(KC_PSCR)  // print screen
+    ),
+
+    [TD_CAM_UP] = ACTION_TAP_DANCE_FN(cam_up),
+
+    [TD_CAM_DN] = ACTION_TAP_DANCE_FN(cam_down)
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [DEF_LAYER] = LAYOUT_ortho_6x4( /* Base */
+
+    TD(TD_CAM_UP),  // next cam, double tap for reverse chase cam
+    KC_V,           // next car
+    LSFT(KC_P3),    // next lap
+    LCTL(KC_P3),    // next inc
+
+    TD(TD_CAM_DN),     // prev cam, double tap for chase cam
+    TD(TD_CAR),     // prev car, double tap for my car
+	LSFT(KC_P1),    // prev lap
+    LCTL(KC_P1),    // prev inc
+
+	LSFT(KC_P4),    // rewind
+    KC_P5,          // play/pause
+    LSFT(KC_P6),    // fast forward
+    KC_P8,          // slow mo
+
+	KC_P4,          // prev frame
+    KC_W,           // up
+    KC_P6,          // next frame
+    TD(TD_PRINT),   // print screen
+
+	KC_A,           // left
+    KC_S,           // down
+    KC_D,           // right
+    KC_NO,          // ran out of things to assign
+
+	KC_LCTL,        // ctrl 
+    KC_LALT,        // alt
+    LCTL(KC_F12),   // camera tool
+    LT(MOD_LAYER, KC_SPACE)        // UI
+  ),
+
+  [MOD_LAYER] = LAYOUT_ortho_6x4( /* Base */
+
+    KC_B,           // next sub cam
+    KC_PGUP,        // next driver cam
+    LCTL(KC_P6),    // next session
+    KC_P7,          // start
+
+    LSFT(KC_B),     // prev sub cam
+    KC_PGDOWN,      // prev driver cam
+	LCTL(KC_P4),    // prev session
+    KC_P1,          // end
+
+	KC_RBRC,
+    KC_EQL,
+    _______,
+    _______,
+
+	KC_LBRC,
+    KC_MINS,
+    _______,
+    _______,
+
+	_______,
+    _______,
+    _______,
+    _______,
+
+	_______,
+    LALT(KC_M),  // cycle aim
+    RALT(KC_ENTER), // fullscreen.  Doesn't work reliably
+    _______
+  ),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch(keycode) {
+        // these are not used currently, leaving them in case someone wants to bind them to a key instead of tap dance
+        case CHASE:
+            if (record->event.pressed) {
+               SEND_STRING("**20" SS_TAP(X_ENTER));
+            }
+            break;
+        case REVERSE:
+            if (record->event.pressed) {
+               SEND_STRING("**22" SS_TAP(X_ENTER));
+            }
+            break;
+  }
+
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
---
+++ keyboards/kbdfans/kbdpad_mk2/keymaps/iracing_replay/readme.md
@@ -0,0 +1,9 @@
+# iRacing Replay Tool
+
+![](https://imgur.com/0948eBG.jpg)
+
+Replay/camera tool for iRacing.  This uses the default keybindings for the game so nothing should need to be mapped.  This makes replays much easier to view in VR and makes waiting through heats a lot more enjoyable.
+
+To make full use of the arrow keys open the camera tool (you can close the UI after) and optionally switch the AIM to static.
+
+The files for the 3D printed keycap set are ![available here.](https://github.com/AaronVerDow/KeyV2/blob/numpad/src/layouts/numpad/iracing.scad)
