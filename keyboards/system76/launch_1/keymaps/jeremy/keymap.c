9140a6ba96ba3d0f1a06f69ca624732cd3f57d2d
Jeremy Soller <jeremy@system76.com>
2021-04-01 10:59:16 -0600

    Apply updates to jeremy layout

--- keyboards/system76/launch_1/keymaps/jeremy/keymap.c
+++ keyboards/system76/launch_1/keymaps/jeremy/keymap.c
@@ -30,28 +30,28 @@ ________________________________________________________________________________
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGDN,
           MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,       KC_END,
           KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
-          KC_LCTL, MO(1),   KC_LALT, KC_LGUI,     KC_SPC,           KC_BSPC,    KC_RCTL,   KC_RALT,   MO(1),         KC_LEFT, KC_DOWN, KC_RIGHT
+          KC_LCTL, MO(1),   KC_LALT, KC_LGUI,     KC_SPC,           KC_BSPC,    KC_RCTL,   KC_RALT,   MO(1),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
     /* Layer 1, function layer
 __________________________________________________________________________________________________________________________________  ________
-|        |        |        |        |        |        |        |        |        |        |        |        |        |            ||        |
-| RESET  |        |        |        |        |        |        |        |        |        |        |        |        |            ||        |
+|        |        |        |        |        |        |        |        |        |        |        |        |        |            || PLAY/  |
+| RESET  |        |        |        |        |        |        |        |        |        |        |        |        |            || PAUSE  |
 |________|________|________|________|________|________|________|________|________|________|________|________|________|____________||________|
-|        |        |        |        |        |        |        |        |        |        |        |        |        |            ||        |
-|  ESC   |   F1   |   F2   |   F3   |   F4   |   F5   |   F6   |   F7   |   F8   |   F9   |  F10   |  F11   |  F12   |  DELETE    ||        |
+|        |        |        |        |        |        |        |        |        |        |        |        |        |            || VOLUME |
+|  ESC   |   F1   |   F2   |   F3   |   F4   |   F5   |   F6   |   F7   |   F8   |   F9   |  F10   |  F11   |  F12   |  DELETE    ||   UP   |
 |________|________|________|________|________|________|________|________|________|________|________|________|________|____________||________|
-|            |        |        |        |        |        |        |        |        |        |        |        |        |        ||        |
-|            |        |        |        |        |        |        |  PGUP  |  HOME  |  PGDN  |        |        |        |        ||        |
+|            |        |        |        |        |        |        |        |        |        |        |  LED   |  LED   |  LED   || VOLUME |
+|PRINT SCREEN|        |        |        |        |        |        |  PGUP  |  HOME  |  PGDN  |        |  DOWN  |  UP    | TOGGLE ||  DOWN  |
 |____________|________|________|________|________|________|________|________|________|________|________|________|________|________||________|
   |            |        |  VOL   |  VOL   |        |        |        |        |        |        |        |        |            |   |        |
-  |            |        |  DOWN  |  UP    |  MUTE  |        |  LEFT  |  DOWN  |   UP   | RIGHT  |  ESC   |        |            |   |        |
+  |            |        |  DOWN  |  UP    |  MUTE  |        |  LEFT  |  DOWN  |   UP   | RIGHT  |  ESC   |        |            |   |  MUTE  |
   |____________|________|________|________|________|________|________|________|________|________|________|________|____________|___|________|
   |                |        |        |        |        |        |        |        |        |        |        |            |        |
   |                |        |        |        |        |        |  END   |        |        |        |        |            |  PGUP  |
   |________________|________|________|________|________|________|________|________|________|________|________|____________|________|_________
   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
-  |            |        |       |        |                 |   DELETE        |        |        |             |   |  HOME  |  PGDN  |  END   |
+  |            |        |       |        |      ESC        |   DELETE        |        |        |             |   |  HOME  |  PGDN  |  END   |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|   |________|________|________|
 
 * 'RESET' resets the controller and puts the board into firmware flashing mode. If this key is hit accidentally, just unplug the board
@@ -59,12 +59,12 @@ ________________________________________________________________________________
 */
 
   [1] = LAYOUT(
-    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_HOME, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-          KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC,  KC_TRNS,    KC_TRNS,     KC_TRNS,
+    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_MPLY,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      KC_VOLU,
+        KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_HOME, KC_PGDN, KC_TRNS, RGB_VAD, RGB_VAI, RGB_TOG, KC_VOLD,
+          KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC,  KC_TRNS,    KC_TRNS,     KC_MUTE,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_PGUP,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_DEL,       KC_TRNS, KC_TRNS, KC_TRNS,       KC_HOME,  KC_PGDN, KC_END
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_ESC,           KC_DEL,       KC_TRNS, KC_TRNS, KC_TRNS,       KC_HOME,  KC_PGDN, KC_END
   ),
 
   [2] = LAYOUT(
