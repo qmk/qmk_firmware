c311beb8cd6a457bb8f4834f6de54cd438aac9e9
MCKeebs <72995608+chrisquocmai@users.noreply.github.com>
2021-04-02 21:41:09 -0400

    Update keyboards/pkb65/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/pkb65/config.h
+++ keyboards/pkb65/config.h
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -66,4 +66,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #endif
-
