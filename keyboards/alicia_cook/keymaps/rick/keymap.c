2a37803cced0bc99a59287400c282141b2cf2d01
Ibnu Daru Aji <ibnuda@users.noreply.github.com>
2021-04-03 16:33:54 +0700

    Update keyboards/alicia_cook/keymaps/rick/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/alicia_cook/keymaps/rick/keymap.c
+++ keyboards/alicia_cook/keymaps/rick/keymap.c
@@ -35,21 +35,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC, KC_SCLN,KC_COMM,KC_DOT, KC_P,   KC_Y,            KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_BSPC, KC_BSPC, \
-        KC_LCTL,SH_A,   CT_O,   AL_T,   KC_U,   GU_I,            GU_D,   KC_H,   AL_T,   CT_N,   SH_S,   KC_ENT,    \
-        KC_LSFT,KC_QUOT,KC_Q,   KC_J,   KC_K,   KC_X,            KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,   KC_RSFT,MO(1),   \
-        KC_LGUI,KC_LALT,        KC_LGUI,LW_BSPC,SFT_ESC,         ALT_ENT,RS_SPC, KC_LALT,                KC_RGUI,KC_RCTL  \
+        KC_ESC, KC_SCLN,KC_COMM,KC_DOT, KC_P,   KC_Y,            KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_BSPC, KC_BSPC,
+        KC_LCTL,SH_A,   CT_O,   AL_T,   KC_U,   GU_I,            GU_D,   KC_H,   AL_T,   CT_N,   SH_S,   KC_ENT,
+        KC_LSFT,KC_QUOT,KC_Q,   KC_J,   KC_K,   KC_X,            KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,   KC_RSFT,MO(1),
+        KC_LGUI,KC_LALT,        KC_LGUI,LW_BSPC,SFT_ESC,         ALT_ENT,RS_SPC, KC_LALT,                KC_RGUI,KC_RCTL
     ),
     [1] = LAYOUT(
-        KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,            KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_DEL, KC_BSPC, \
-        KC_LCTL,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,         KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,   KC_SCLN,      \
-        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,            KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,MO(2),   \
-        KC_LGUI,KC_LALT,        KC_LGUI,LW_BSPC,SFT_ESC,         ALT_ENT,RS_SPC, KC_LALT,                KC_RGUI,KC_RCTL  \
+        KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,            KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_DEL, KC_BSPC,
+        KC_LCTL,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,         KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,   KC_SCLN,
+        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,            KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,MO(2),
+        KC_LGUI,KC_LALT,        KC_LGUI,LW_BSPC,SFT_ESC,         ALT_ENT,RS_SPC, KC_LALT,                KC_RGUI,KC_RCTL
     ),
     [2] = LAYOUT(
-        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,           KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_F13, \
-        KC_LCTL,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,         KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,   KC_SCLN,     \
-        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   RESET,           KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,MO(1),  \
-        KC_LGUI,KC_LALT,        KC_LGUI,LW_BSPC,SFT_ESC,         ALT_ENT,RS_SPC, KC_LALT,                KC_RGUI,KC_RCTL \
+        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,           KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_F13,
+        KC_LCTL,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,         KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,   KC_SCLN,
+        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   RESET,           KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,MO(1),
+        KC_LGUI,KC_LALT,        KC_LGUI,LW_BSPC,SFT_ESC,         ALT_ENT,RS_SPC, KC_LALT,                KC_RGUI,KC_RCTL
     ),
 };
