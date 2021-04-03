47d1de19ba57d9afac5071a93c0125e784ab8f7d
nopjmp <kthompson@hey.com>
2021-04-03 18:13:02 -0500

    Disable sleep led

--- keyboards/lily58/keymaps/nop/rules.mk
+++ keyboards/lily58/keymaps/nop/rules.mk
@@ -25,7 +25,7 @@ LTO_ENABLE = yes
 OPT = s
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
