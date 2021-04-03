68bed63eb3d2091451710e277f04166cb1ca383f
Fidel Coria <fidel.coria@aa.com>
2021-04-03 14:33:05 -0500

    fix bootloader

--- keyboards/handwired/pteron/keymaps/alzafacon/rules.mk
+++ keyboards/handwired/pteron/keymaps/alzafacon/rules.mk
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = caterina
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   comment out to disable the options.
