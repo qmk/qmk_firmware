cc0d1fa5b0e953c4e790b7ca9834ace46843fcf7
zhuwenq <lonelywolf12138@Outlook.com>
2021-04-04 10:36:17 +0800

    Update keyboards/anton/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/anton/config.h
+++ keyboards/anton/config.h
@@ -23,7 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER ZHUWENQ THE BEST
 #define PRODUCT ANTON
-#define DESCRIPTION Anton ver-1.0
 
 /* key matrix size */
 #define MATRIX_ROWS 6
