7b5ed8766bcf5585158f80ea912f010ed153578c
Laura Brustenga i Moncusí <brust@math.ku.dk>
2021-04-05 14:37:00 +0200

    open-close paren under lower key
    I do not use Homo, Pg Up,.. It seems handier to have all
    paren keys under the same key

--- keyboards/preonic/keymaps/LauraBMo/keymap.c
+++ keyboards/preonic/keymaps/LauraBMo/keymap.c
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
  _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_PSCR,
  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_DQT,
- _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LT,   KC_GT,   KC_QUES, KC_PIPE,
- KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+ OCCBR,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LT,   KC_GT,   KC_QUES, KC_PIPE,
+ _______, _______, OCBRC,   OCPRN,   _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 
