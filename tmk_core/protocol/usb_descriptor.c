65a36d45b12ea469c64e0f7578dfdd5835dbc20c
Michael Stapelberg <michael@stapelberg.de>
2021-04-05 09:16:38 +0200

    core: allow setting USB version in descriptor to USB 2 from config.h

--- tmk_core/protocol/usb_descriptor.c
+++ tmk_core/protocol/usb_descriptor.c
@@ -351,7 +351,11 @@ const USB_Descriptor_Device_t PROGMEM DeviceDescriptor = {
         .Size                   = sizeof(USB_Descriptor_Device_t),
         .Type                   = DTYPE_Device
     },
+#if defined(USB_VERSION_2)
+    .USBSpecification           = VERSION_BCD(2, 0, 0),
+#else
     .USBSpecification           = VERSION_BCD(1, 1, 0),
+#endif
 
 #if VIRTSER_ENABLE
     .Class                      = USB_CSCP_IADDeviceClass,
