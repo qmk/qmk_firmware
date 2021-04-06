402440b1e628e2462968fae8c269b34864da1e97
Nick Brassel <nick@tzarc.org>
2021-04-06 16:38:28 +1000

    Add description on producing a UF2 file.

--- tmk_core/rules.mk
+++ tmk_core/rules.mk
@@ -224,6 +224,8 @@ ifneq ($(filter Darwin FreeBSD,$(shell uname -s)),)
 endif
 
 # UF2 format settings
+# To produce a UF2 file in your build, add to your keyboard's rules.mk:
+#      FIRMWARE_FORMAT = uf2
 UF2CONV = $(TOP_DIR)/util/uf2conv.py
 UF2_FAMILY ?= 0x0
 
