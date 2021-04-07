fe371ce1a5464c20ba8bf77ae9a44c7b0812996e
peepeetee <43021794+peepeetee@users.noreply.github.com>
2021-04-06 21:48:41 -0500

    Update hub20.c

--- keyboards/hub20/hub20.c
+++ keyboards/hub20/hub20.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 joshajohnson
+/* Copyright 2020 joshajohnson 2021 peepeetee
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
