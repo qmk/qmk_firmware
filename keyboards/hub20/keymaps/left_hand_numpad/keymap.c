a74385834a37bbe95df64c010fa0dd71fe3e1171
peepeetee <peepeetee@protonmail.com>
2021-04-07 22:49:49 -0500

    resolved bugs; build successful

--- keyboards/hub20/keymaps/left_hand_numpad/keymap.c
+++ keyboards/hub20/keymaps/left_hand_numpad/keymap.c
@@ -18,21 +18,21 @@
 #define MO_NLCK LT(1, KC_NLCK) // Numlock on tap, layer change on hold
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_all(
+    [0] = LAYOUT_left_handed(
                  KC_MUTE,  KC_MPLY,
        KC_PMNS,  KC_PAST,  KC_PSLS,    MO_NLCK,
-       KC_PPLS,    KC_P7,    KC_P8,      KC_P9,
+                   KC_P7,    KC_P8,      KC_P9,
        KC_PPLS,    KC_P4,    KC_P5,      KC_P6,
-       KC_PENT,    KC_P1,    KC_P2,      KC_P3,
-       KC_PENT,  KC_PDOT,    KC_P0,      KC_P0
+                   KC_P1,    KC_P2,      KC_P3,
+       KC_PENT,  KC_PDOT,    KC_P0
 ),
-    [1] = LAYOUT_all(
+    [1] = LAYOUT_left_handed(
                    RESET,  _______,
        RGB_TOG, RGB_RMOD,  RGB_MOD,    _______,
        _______,  RGB_VAD,  RGB_VAI,    _______,
-       _______,  RGB_HUD,  RGB_HUI,    _______,
+                 RGB_HUD,  RGB_HUI,    _______,
        _______,  RGB_SAD,  RGB_SAI,    _______,
-       _______,  _______,  _______,    _______
+                 _______,        _______
 )
 };
 
--- keyboards/hub20/keymaps/right_hand_numpad/keymap.c
+++ keyboards/hub20/keymaps/right_hand_numpad/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 joshajohnson
+/* Copyright 2021 peepeetee
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,23 +18,28 @@
 #define MO_NLCK LT(1, KC_NLCK) // Numlock on tap, layer change on hold
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_all(
+
+//NOT TESTED, WAITING ENDORSEMENT FROM MANUFACTURER
+    [0] = LAYOUT_right_handed(
                  KC_MUTE,  KC_MPLY,
-       KC_PMNS,  KC_PAST,  KC_PSLS,    MO_NLCK,
-       KC_PPLS,    KC_P7,    KC_P8,      KC_P9,
-       KC_PPLS,    KC_P4,    KC_P5,      KC_P6,
-       KC_PENT,    KC_P1,    KC_P2,      KC_P3,
-       KC_PENT,  KC_PDOT,    KC_P0,      KC_P0
+        MO_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,
+        KC_P0,   KC_PDOT,          KC_PENT
 ),
-    [1] = LAYOUT_all(
+    [1] = LAYOUT_right_handed(
                    RESET,  _______,
        RGB_TOG, RGB_RMOD,  RGB_MOD,    _______,
-       _______,  RGB_VAD,  RGB_VAI,    _______,
+       _______,  RGB_VAD,  RGB_VAI,
        _______,  RGB_HUD,  RGB_HUI,    _______,
-       _______,  RGB_SAD,  RGB_SAI,    _______,
-       _______,  _______,  _______,    _______
+       _______,  RGB_SAD,  RGB_SAI,
+           _______,        _______,    _______
 )
 };
+
+
+
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* Left Encoder */
