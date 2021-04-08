4429c8c8011e23c68e9777b0d79c57a6c3ea3152
David Fryer <dfryer1193@gmail.com>
2021-04-08 18:49:43 -0400

    Move existing keymaps to LAYOUT_BREEZE

--- keyboards/afternoonlabs/breeze/rev1/rev1.h
+++ keyboards/afternoonlabs/breeze/rev1/rev1.h
@@ -20,7 +20,7 @@
 
 #include "quantum.h"
 
-#define LAYOUT( \
+#define LAYOUT_BREEZE( \
     L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05, MC0, MC1, MC2, \
     L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15, MC3, MC4, MC5, \
     L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25,      AUP,      \
