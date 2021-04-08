789e234bf061631ce03c52bfab3dbd21eec0edec
George <terranodar@gmail.com>
2021-04-08 18:40:15 +0300

    updates to the keyboard config to make initial setup

--- keyboards/handwired/g_split_board/keymaps/default/keymap.c
+++ keyboards/handwired/g_split_board/keymaps/default/keymap.c
@@ -18,12 +18,36 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-                          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-                 KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-        KC_SPC,  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RBRC,
-                 KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                                      KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                                              KC_SPC,      KC_DEL,                             KC_BSPC,      KC_SPC,
-                                                                                    KC_SPC,           KC_ENT
+                                         KC_1,    KC_2,    KC_3,    KC_4,      KC_5,                                                                                  KC_6,                   KC_7,    KC_8,    KC_9,    KC_0,
+                  KC_GRV,                KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,                                                                                  KC_Y,                   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+        KC_LGUI,  MT(MOD_LCTL, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,                                                                                  KC_H,                   KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_RCTL, KC_QUOT), MT(MOD_RGUI, KC_RBRC),
+                  MT(MOD_LALT, KC_ESC),  KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,                    LT(1, KC_ENT),                               LT(1, KC_PENT),  KC_N,                   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RALT, KC_BSLS),
+                                                                               MT(MOD_LSFT, KC_SPC),        LT(2, KC_DEL),                      LT(2, KC_BSPC),       MT(MOD_RSFT, KC_SPC),
+                                                                                                                    KC_ENT,           KC_ENT
     ),
 
-};
+    [1] = LAYOUT(
+                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,                                                                               KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                  KC_TRNS,               KC_TRNS, KC_PGUP, KC_UP,   KC_HOME,   KC_EQL,                                                                                KC_PSLS,                KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_TRNS,
+        KC_TRNS,  KC_TRNS,               KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_PPLS,                                                                               KC_PAST,                KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_TRNS, KC_TRNS,
+                  KC_TRNS,               KC_TRNS, KC_PGDN, KC_TRNS, KC_END,    KC_MINS,                 KC_TRNS,                                     KC_TRNS,         KC_P0,                  KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_TRNS,
+                                                                               KC_TRNS,                     KC_TRNS,                            KC_TRNS,              KC_TRNS,
+                                                                                                                    KC_TRNS,          KC_TRNS
+    ),
+    [2] = LAYOUT(
+                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,                                                                               KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                  KC_UNDS,               KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,                                                                                 KC_F6,                  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_TRNS,  KC_LCTL,               KC_EXLM, KC_AT,   KC_HASH, KC_DLR,    KC_PERC,                                                                               KC_CIRC,                KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_RCTL, MT(MOD_RGUI, KC_F12),
+                  KC_LALT,               KC_1,    KC_2,    KC_3,    KC_4,      KC_5,                    KC_TRNS,                                     KC_TRNS,         KC_6,                   KC_7,    KC_8,    KC_9,    KC_0,    KC_RALT,
+                                                                               KC_TRNS,                     KC_TRNS,                            KC_TRNS,              KC_TRNS,
+                                                                                                                    KC_TRNS,          KC_TRNS
+    ),
+
+};  
+// LT(2, KC_BSPC)$1-_7f7a777kjkjjjkkjkjkjjkkjjkjkjkjklkjlkkjJKjk JKJK  jk jk
+// jk
+// jk
+// jk7777ljjjjjjjjjjjjjjjdkls;asdfjasfjuioiuweroiwueroiwueroiwueroiwueroiuwer=++--
+// 
+// 
+// 
