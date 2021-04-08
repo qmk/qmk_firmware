aaddf5ed116610a0e37e8b3f69856dedb440a267
peepeetee <peepeetee@protonmail.com>
2021-04-07 21:54:30 -0500

    add layouts, add led config

--- keyboards/hub20/keymaps/left_hand_numpad/keymap.c
+++ keyboards/hub20/keymaps/left_hand_numpad/keymap.c
@@ -13,13 +13,50 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include QMK_KEYBOARD_H
 
-#include "hub20.h"
+#define MO_NLCK LT(1, KC_NLCK) // Numlock on tap, layer change on hold
 
-<<<<<<< Updated upstream
-=======
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_all(
+                 KC_MUTE,  KC_MPLY,
+       KC_PMNS,  KC_PAST,  KC_PSLS,    MO_NLCK,
+       KC_PPLS,    KC_P7,    KC_P8,      KC_P9,
+       KC_PPLS,    KC_P4,    KC_P5,      KC_P6,
+       KC_PENT,    KC_P1,    KC_P2,      KC_P3,
+       KC_PENT,  KC_PDOT,    KC_P0,      KC_P0
+),
+    [1] = LAYOUT_all(
+                   RESET,  _______,
+       RGB_TOG, RGB_RMOD,  RGB_MOD,    _______,
+       _______,  RGB_VAD,  RGB_VAI,    _______,
+       _______,  RGB_HUD,  RGB_HUI,    _______,
+       _______,  RGB_SAD,  RGB_SAI,    _______,
+       _______,  _______,  _______,    _______
+)
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* Left Encoder */
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) { /* Right Encoder */
+        if (clockwise) {
+            tap_code(KC_MNXT);
+        } else {
+            tap_code(KC_MPRV);
+        }
+    }
+}
+
+void matrix_init_user(void){
+
 #ifdef RGB_MATRIX_ENABLE
-led_config_t g_led_config = { {
+
+g_led_config = (led_config_t){ {
     // Key Matrix to LED Index
     {NO_LED,NO_LED,NO_LED,NO_LED},
     { 7,      8,      9,     10},
@@ -48,5 +85,6 @@ led_config_t g_led_config = { {
     LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT            // row 0
 
 } };
+
 #endif
->>>>>>> Stashed changes
+}
