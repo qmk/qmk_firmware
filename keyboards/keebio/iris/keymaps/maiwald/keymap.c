e16d29bb5b4d696d1250da4268b2448864fff385
Luciano Maiwald <luciano.maiwald@gmail.com>
2021-04-09 12:58:38 +0200

    switch to german keycodes and add umlaut layer

--- keyboards/keebio/iris/keymaps/maiwald/keymap.c
+++ keyboards/keebio/iris/keymaps/maiwald/keymap.c
@@ -1,35 +1,81 @@
 #include QMK_KEYBOARD_H
+#include "keymap_german_osx.h"
 
 enum layers {
   _COLEMAK = 0,
   _EXTEND,
-  _SYMBOLS
+  _SYMBOLS,
+  _GERMAN,
 };
 
 enum custom_keycodes {
-  MY_COMM = SAFE_RANGE,
-  MY_DOT,
+  MY_SLSH = SAFE_RANGE,
 };
 
 enum {
   TD_GUI_ALT,
+  TD_GER_ALT,
 };
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD,
+};
+
+int cur_dance(qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->pressed) return SINGLE_HOLD;
+    else return SINGLE_TAP;
+  }
+
+  return 8;
+}
+
+// Create an instance of 'tap' for the 'x' tap dance.
+static uint8_t gertap_state = 0;
+
+void gertap_finished(qk_tap_dance_state_t *state, void *user_data) {
+  gertap_state = cur_dance(state);
+  switch (gertap_state) {
+    case SINGLE_TAP:
+      set_oneshot_layer(_GERMAN, ONESHOT_START);
+      clear_oneshot_layer_state(ONESHOT_PRESSED);
+      break;
+    case SINGLE_HOLD:
+      register_code(KC_RALT);
+      break;
+  }
+}
+
+void gertap_reset(qk_tap_dance_state_t *state, void *user_data) {
+  switch (gertap_state) {
+    case SINGLE_TAP:
+      break;
+    case SINGLE_HOLD:
+      unregister_code(KC_RALT);
+      break;
+  }
+  gertap_state = 0;
+}
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_GUI_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_LALT),
+  [TD_GER_ALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, gertap_finished, gertap_reset),
 };
 
-#define MY_A LCTL_T(KC_A)
-#define MY_O RCTL_T(KC_O)
-#define MY_R LSFT_T(KC_R)
-#define MY_I RSFT_T(KC_I)
+#define MY_A LCTL_T(DE_A)
+#define MY_O RCTL_T(DE_O)
+#define MY_R LSFT_T(DE_R)
+#define MY_I RSFT_T(DE_I)
 
-#define VIM_ALT S(C(KC_CIRC))
+#define VIM_ALT C(DE_6)
 #define KC_EUR  S(A(KC_2))
 #define MY_GUI  TD(TD_GUI_ALT)
+#define MY_ALT  TD(TD_GER_ALT)
+#define MY_SCSH G(S(KC_4))
 
 #define EXT_SPC  LT(_EXTEND, KC_SPC)
-#define SFT_ZERO MT(MOD_LSFT, KC_0)
+#define SFT_ZERO MT(MOD_LSFT, DE_0)
 
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -42,46 +88,57 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
-      KC_GRV,  KC_1, KC_2, KC_3, KC_4,   KC_5,                                        KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS,
-      KC_TAB,  KC_Q, KC_W, KC_F, KC_P,   KC_B,                                        KC_J,    KC_L,   KC_U,    KC_Y,   KC_QUOT, KC_BSPC,
-      KC_ESC,  MY_A, MY_R, KC_S, KC_T,   KC_G,                                        KC_M,    KC_N,   KC_E,    MY_I,   MY_O,    KC_ENT,
-      KC_LSFT, KC_Z, KC_X, KC_C, KC_D,   KC_V,          KC_BSPC,       KC_ENT,        KC_K,    KC_H,   MY_COMM, MY_DOT, KC_SLSH, KC_RSFT,
-                                 MY_GUI, OSM(MOD_LSFT), OSL(_SYMBOLS), OSL(_SYMBOLS), EXT_SPC, KC_RALT
+      DE_GRV,  DE_1, DE_2, DE_3, DE_4,   DE_5,                                        DE_6,    DE_7,   DE_8,    DE_9,   DE_0,    MY_SCSH,
+      KC_TAB,  DE_Q, DE_W, DE_F, DE_P,   DE_B,                                        DE_J,    DE_L,   DE_U,    DE_Y,   DE_QUOT, KC_BSPC,
+      KC_ESC,  MY_A, MY_R, DE_S, DE_T,   DE_G,                                        DE_M,    DE_N,   DE_E,    MY_I,   MY_O,    KC_ENT,
+      KC_LSFT, DE_Z, DE_X, DE_C, DE_D,   DE_V,          KC_BSPC,       KC_ENT,        DE_K,    DE_H,   DE_COMM, DE_DOT, MY_SLSH, KC_RSFT,
+                                 MY_GUI, OSM(MOD_LSFT), OSL(_SYMBOLS), OSL(_SYMBOLS), EXT_SPC, MY_ALT
       ),
 
   [_EXTEND] = LAYOUT(
       XXXXXXX, KC_BRID, KC_BRIU, RGB_TOG, RGB_VAD, RGB_VAI,                       KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, XXXXXXX,
-      _______, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_PLUS,                       KC_HOME, KC_PGUP, VIM_ALT, XXXXXXX, KC_DEL,  XXXXXXX,
-      _______, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_MINS,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, XXXXXXX,
-      _______, XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX,  A(KC_BSPC), RESET,   KC_END,  KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, DE_7,    DE_8,    DE_9,    DE_PLUS,                       KC_HOME, KC_PGUP, VIM_ALT, XXXXXXX, KC_DEL,  XXXXXXX,
+      _______, XXXXXXX, DE_4,    DE_5,    DE_6,    DE_MINS,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, XXXXXXX,
+      _______, XXXXXXX, DE_1,    DE_2,    DE_3,    XXXXXXX,  A(KC_BSPC), RESET,   KC_END,  KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                           _______, SFT_ZERO, KC_LALT,    _______, _______, _______
       ),
 
   [_SYMBOLS] = LAYOUT(
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, KC_EXLM, KC_LT,   KC_AT,   KC_GT,   KC_PLUS,                   KC_CIRC, KC_LBRC, KC_UNDS, KC_RBRC, KC_EUR,  XXXXXXX,
-      XXXXXXX, KC_BSLS, KC_LPRN, KC_DQUO, KC_RPRN, KC_MINS,                   KC_PERC, KC_LCBR, KC_PEQL, KC_RCBR, KC_PIPE, XXXXXXX,
-      XXXXXXX, XXXXXXX, KC_TILD, KC_QUOT, KC_HASH, KC_DLR,  _______, _______, KC_GRV,  KC_AMPR, KC_ASTR, KC_COLN, KC_QUES, XXXXXXX,
+      XXXXXXX, DE_EXLM, DE_LABK, DE_AT,   DE_RABK, DE_PLUS,                   DE_CIRC, DE_LBRC, DE_UNDS, DE_RBRC, DE_EURO, XXXXXXX,
+      XXXXXXX, DE_BSLS, DE_LPRN, DE_DQUO, DE_RPRN, DE_MINS,                   DE_PERC, DE_LCBR, DE_EQL,  DE_RCBR, DE_PIPE, XXXXXXX,
+      XXXXXXX, XXXXXXX, DE_TILD, DE_QUOT, DE_HASH, DE_DLR,  _______, _______, DE_GRV,  DE_AMPR, DE_ASTR, DE_COLN, DE_QUES, XXXXXXX,
                                           _______, KC_SPC,  _______, _______, _______, _______
+      ),
+
+  [_GERMAN] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,                   _______, _______, DE_UDIA, _______, _______, _______,
+      _______, DE_ADIA, _______, DE_SS,   _______, _______,                   _______, _______, _______, _______, DE_ODIA, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                                          _______, _______, _______, _______, _______, _______
       ),
 };
 
-void handle_umlaut(uint16_t base_key, keyrecord_t *record, uint16_t mod_state, uint16_t one_shot_mod_state) {
-  if (record->event.pressed) {
-    clear_mods();
-    clear_oneshot_mods();
-    tap_code16(LALT(KC_U));
-    set_mods(mod_state & MOD_MASK_SHIFT);
-    set_oneshot_mods(one_shot_mod_state & MOD_MASK_SHIFT);
-    tap_code(base_key);
-  }
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  uint16_t mod_state = get_mods();
   uint16_t one_shot_mod_state = get_oneshot_mods();
 
   switch (keycode) {
+    case KC_TRNS:
+    case KC_NO:
+      /* Always cancel one-shot layer when another key gets pressed */
+      if (record->event.pressed && is_oneshot_layer_active())
+      clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
+      return true;
+    case MY_SLSH:
+      if (record->event.pressed) {
+        if ((get_mods() | one_shot_mod_state) & MOD_MASK_SHIFT) {
+          tap_code16(DE_QUES);
+        } else {
+          tap_code16(DE_SLSH);
+        }
+      }
+      return false;
     case KC_ESC:
       if (one_shot_mod_state) {
         clear_oneshot_mods();
@@ -89,28 +146,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         return true;
       }
-    case MY_COMM:
-      if (record->event.pressed) {
-        if ((mod_state | one_shot_mod_state) & MOD_MASK_SHIFT) {
-          set_mods(mod_state & ~MOD_MASK_SHIFT);
-          set_oneshot_mods(one_shot_mod_state & ~MOD_MASK_SHIFT);
-          tap_code(KC_SCLN);
-          set_mods(mod_state);
-        } else {
-          tap_code(KC_COMM);
-        }
-      }
-      return false;
-    case MY_DOT:
-      if (record->event.pressed) {
-        if ((mod_state | one_shot_mod_state) & MOD_MASK_SHIFT) {
-          // this is with a shift mod active, so KC_SCLN becomes S(KC_SCLN)
-          tap_code(KC_SCLN);
-        } else {
-          tap_code(KC_DOT);
-        }
-      }
-      return false;
     default:
       return true;
   }
