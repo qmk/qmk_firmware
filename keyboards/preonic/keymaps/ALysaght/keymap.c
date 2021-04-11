cfb2948ce26946507c99ffeaef95f8c4fe8d5db8
Andrew L <andrewl@a-personal.lan>
2021-04-11 14:48:45 -0400

    rebuild FFXIV layer

--- keyboards/preonic/keymaps/ALysaght/keymap.c
+++ keyboards/preonic/keymaps/ALysaght/keymap.c
@@ -28,7 +28,7 @@ enum preonic_layers {
   _MOUSE,
   _NSSL,
   _NSL,
-  //_FUNL,
+  _FUNL,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -44,19 +44,16 @@ enum preonic_keycodes {
   R_PIPE,
   GITFETC,
   GITPULL,
-  GITPUSH,
+  GITPUSI,
+  GITPUSO,
   GITREBA,
   GITADD,
   GITCOMM,
   GITBRCH,
   GITCOUT,
-  GITWTAD,
-  GITWTPR,
+  GITCOBR,
   GITSTAT,
-  GITBDEL,
-  GITMAIN,
-  GITORIG,
-  GITIMM
+  GITBDEL
 };
 
 // modifier keys
@@ -75,7 +72,7 @@ enum preonic_keycodes {
 #define LT_TAB  LT(_MOUSE, KC_TAB)
 #define LT_ENT  LT(_NSSL, KC_ENT)
 #define LT_SPC  LT(_NSL, KC_SPC)
-#define LT_DEL  KC_DEL //LT(_FUNL, KC_DEL)
+#define LT_DEL  LT(_FUNL, KC_DEL)
 
 #define UNDO  LGUI(KC_Z)
 #define CUT   LGUI(KC_X)
@@ -86,6 +83,8 @@ enum preonic_keycodes {
 #define NX_END  LCTL(KC_E)
 #define NX_WB   LALT(KC_B)
 #define NX_WF   LALT(KC_F)
+
+#define SCR_GRB SGUI(C(KC_4))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -99,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   Z  |   X  |   C  |   V  |   B  |  NO  |  NO  |   N  |   M  |   ,  |   .  |   /  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Lower |  NO  |LTEsc |LTBksp|LTTab |  NO  |  NO  |LTEnt |LTSpc |LTDel |  NO  |Raise |
+ * |  NO  |  NO  |LTEsc |LTBksp|LTTab |  NO  |  NO  |LTEnt |LTSpc |LTDel |  NO  |  NO  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
@@ -107,28 +106,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   XXXXXXX, XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,   XXXXXXX, XXXXXXX, KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   XXXXXXX, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-  LOWER,   XXXXXXX, LT_ESC,  LT_BSPC, LT_TAB, XXXXXXX, XXXXXXX, LT_ENT,  LT_SPC,  LT_DEL,  XXXXXXX, RAISE
+  XXXXXXX, XXXXXXX, LT_ESC,  LT_BSPC, LT_TAB, XXXXXXX, XXXXXXX, LT_ENT,  LT_SPC,  LT_DEL,  XXXXXXX, XXXXXXX
 ),
 
 /* Final Fantasy XIV
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  \   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  `   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | F11  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |LOWER |RAISE | Alt  | GUI  |Shift | Bksp |Space | F12  | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
+* ,-----------------------------------------------------------------------------------.
+* |   1  |   2  |   3  |   4  |   5  |  NO  |  NO  | LT6  |   7  |   8  |   9  |   0  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |   Q  |   W  |   E  |   R  |   T  |  NO  |  NO  |   Y  |   U  |   I  |   O  |   P  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |   A  |   S  |   D  |   F  |   G  |  NO  |  NO  |   H  |   J  |   K  |   L  |   ;  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |   Z  |   X  |   C  |   V  |   B  |  NO  |  NO  |   N  |   M  |   ,  |   .  |   /  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |  NO  |  NO  | Alt  |Shift | GUI  |  NO  |  NO  |Enter |Space | Bksp |  NO  |  NO  |
+* `-----------------------------------------------------------------------------------'
  */
 [_FFXIV] = LAYOUT_preonic_grid(
-  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-  KC_GRV,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_F11,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  LOWER,    XXXXXXX, KC_LALT, KC_LGUI, KC_LSFT, KC_BSPC, KC_SPC,  KC_F12,  KC_LEFT, KC_DOWN, KC_UP,   RAISE
+  KC_ESC,  KC_GRV,  KC_TAB,  KC_4,    LT(_ADJUST, KC_5), XXXXXXX, XXXXXXX,   KC_6,    KC_7,   KC_8,    KC_9,    KC_0,
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              XXXXXXX, XXXXXXX,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              XXXXXXX, XXXXXXX,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              XXXXXXX, XXXXXXX,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,
+  XXXXXXX, XXXXXXX, KC_LALT, KC_LSFT, KC_LGUI,           XXXXXXX, XXXXXXX,   LT_ENT,  LT_SPC, KC_BSPC, XXXXXXX, XXXXXXX
 ),
 
 
@@ -153,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NX_HOME, NX_WB,   NX_WF,   NX_END,
   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
   UNDO,    CUT,     COPY,    PASTE,   XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-  XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT,  KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX
+  XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT,  KC_SPC,  KC_DEL,  XXXXXXX, XXXXXXX
 ),
 
 /* SHIFTED Number and Symbol layer
@@ -220,7 +219,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, XXXXXXX
+  XXXXXXX, XXXXXXX, MO(_ADJUST), XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, XXXXXXX
 ),
 
 
@@ -243,75 +242,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,
-  XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MSTP, KC_MPLY, KC_MUTE, XXXXXXX, XXXXXXX
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BACKLIT, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,
+  XXXXXXX, XXXXXXX, _______, XXXXXXX, MO(_ADJUST), XXXXXXX, XXXXXXX, KC_MSTP, KC_MPLY, KC_MUTE, XXXXXXX, XXXXXXX
+),
+
+
+/* FUNCTION layer
+* ,-----------------------------------------------------------------------------------.
+* |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |
+* |------+------+------+------+------+-------------+------+------+------+------+------|
+* |GITFET|GITPUL|      |GITREB|      |  NO  |  NO  |      |  F9  | F10  | F11  | F12  |
+* |------+------+------+------+------+-------------+------+------+------+------+------|
+* |GITADD|GITSTA|GITCOM|GITPUI|GITPUO|  NO  |  NO  |      |  F5  |  F6  |  F7  |  F8  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |GITBDE|GITBRC|GITCOU|GITCOB|GITBRC|  NO  |  NO  |      |  F1  |  F2  |  F3  |  F4  |
+* |------+------+------+------+------+------+------+------+------+------+------+------|
+* |  NO  |  NO  |ScrGrb| <-\s |%>%\n |  NO  |  NO  |  NO  |  NO  |      |  NO  |  NO  |
+* `-----------------------------------------------------------------------------------'
+*/
+[_FUNL] = LAYOUT_preonic_grid(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  GITFETC, GITPULL, XXXXXXX, GITREBA, XXXXXXX,   XXXXXXX, XXXXXXX,   XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  GITADD,  GITSTAT, GITCOMM, GITPUSO, GITPUSI,   XXXXXXX, XXXXXXX,   XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,
+  GITBDEL, XXXXXXX, GITCOUT, GITCOBR, GITBRCH,   XXXXXXX, XXXXXXX,   XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,
+  XXXXXXX, XXXXXXX, SCR_GRB, R_ASIGN, R_PIPE,    XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX
 ),
 
 
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |ScrSht|      |      |      |      |  <-  |  %>% |      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_LOWER] = LAYOUT_preonic_grid(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,         KC_F5,   XXXXXXX, XXXXXXX,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_F11,  KC_F12,  _______, _______,       _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______,
-  _______, _______, _______, SGUI(C(KC_4)), _______, XXXXXXX, XXXXXXX, _______, R_ASIGN, R_PIPE,  _______, _______,
-  _______, _______, _______, _______,       _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______,
-  _______, XXXXXXX, _______, _______,       _______, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, _______
-),
 
-/* Raise
+/* Adjust (_MOUSE + _MEDIA)
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |GITCOU|GITFET|GITPUL|GITREB|      |      |      |      |      |      |      |      |
+ * |Reset |Debug |      |      |      |      |      |TermOf|TermOn|      |AGnorm|AGswap|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |GITADD|GITSTA|GITCOM|GITPUS|      |      |      |      |Origin|      |      |      |
+ * |      |      |      |      |      |      |      |FFXIV |Qwerty|Voice-|Voice+|Aud cy|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |GITBRC|GITBDE|GITWTA|GITWTP|      |      |      | Imm  | Main |      |      |      |
+ * |      |      |      |      |      |      |      |MidiOn|MusOn |Aud on|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-[_RAISE] = LAYOUT_preonic_grid(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  GITCOUT, GITFETC, GITPULL, GITREBA, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  GITADD,  GITSTAT, GITCOMM, GITPUSH, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GITORIG, XXXXXXX, XXXXXXX, XXXXXXX,
-  GITBRCH, GITBDEL, GITWTAD, GITWTPR, XXXXXXX, XXXXXXX, XXXXXXX, GITIMM,  GITMAIN, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
-),
-
-
-
-/* Adjust (Lower + Raise)
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Reset |Debug |      |      |      |      |      |TermOf|TermOn|      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |Aud cy|Aud on|AudOff|AGnorm|AGswap|      |      |      |Qwerty|FFXIV |      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Voice-|Voice+|Mus on|MusOff|      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |Brite |MidiOn|MidOff|      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |MidOff|MusOff|AudOff|      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  RESET,   DEBUG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TERM_ON, TERM_OFF,XXXXXXX, XXXXXXX, XXXXXXX,
-  MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, QWERTY,  FFXIV,   XXXXXXX, XXXXXXX,
-  MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, BACKLIT, MI_ON,   MI_OFF,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
+  RESET,   DEBUG,   XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,   TERM_ON, TERM_OFF, XXXXXXX, AG_NORM, AG_SWAP,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,   FFXIV,   QWERTY,   MUV_DE,  MUV_IN,  MU_MOD,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,   MI_ON,   MU_ON,    AU_ON,   XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, _______, XXXXXXX, _______,   XXXXXXX, XXXXXXX,   MI_OFF,  MU_OFF,   AU_OFF,  XXXXXXX, XXXXXXX
 )
 
 
@@ -373,9 +351,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING("git pull ");
           }
           break;
-        case GITPUSH:
+        case GITPUSI:
           if (record->event.pressed) {
-            SEND_STRING("git push ");
+            SEND_STRING("git push imm ");
+          }
+          break;
+        case GITPUSO:
+          if (record->event.pressed) {
+            SEND_STRING("git push origin ");
           }
           break;
         case GITREBA:
@@ -403,14 +386,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING("git checkout ");
           }
           break;
-        case GITWTAD:
+        case GITCOBR:
           if (record->event.pressed) {
-            SEND_STRING("git worktree add ");
-          }
-          break;
-        case GITWTPR:
-          if (record->event.pressed) {
-            SEND_STRING("git git worktree prune ");
+            SEND_STRING("git checkout -b ");
           }
           break;
         case GITSTAT:
@@ -421,21 +399,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case GITBDEL:
           if (record->event.pressed) {
             SEND_STRING("git branch -d ");
-          }
-          break;;
-        case GITMAIN:
-          if (record->event.pressed) {
-            SEND_STRING("main ");
-          }
-          break;
-        case GITORIG:
-          if (record->event.pressed) {
-            SEND_STRING("origin ");
-          }
-          break;
-        case GITIMM:
-          if (record->event.pressed) {
-            SEND_STRING("imm ");
           }
           break;
         case BACKLIT:
