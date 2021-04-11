a6893ae26cc6cf852b20173676477bcd0c68bc9b
alfred <alfredy@gmail.com>
2021-04-10 23:13:58 -0700

    wip on fixing pinouts on right side

--- keyboards/bakekujira/keymaps/default/keymap.c
+++ keyboards/bakekujira/keymaps/default/keymap.c
@@ -29,10 +29,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split(
   //,--------------------------------------------------------------.                 ,--------------------------------------------------------------------.    ,---------------------------.
-      TD(TD_GRAVE_ESC),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,           KC_7,    KC_8,    KC_9,    KC_0, KC_MINUS,  KC_EQUAL, KC_BSPACE,        KC_INS, KC_HOME,  KC_PGUP,
-  //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,   KC_LBRC,   KC_RBRC,        KC_DEL, KC_END, KC_PGDOWN,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
+      TD(TD_GRAVE_ESC),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,            KC_7,    KC_8,    KC_9,    KC_0, KC_MINUS,  KC_EQUAL, KC_BSPACE,         KC_INS, KC_HOME,  KC_PGUP,
+  //|--------+--------+--------+--------+--------+--------+--------|         |-------|--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,     KC_U,   KC_I,   KC_O,     KC_P,   KC_LBRC,   KC_RBRC,  KC_BSLASH,      KC_DEL, KC_END, KC_PGDOWN,
+  //|--------+--------+--------+--------+--------+--------|                  |-------|--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
         MO(_FUNC),    KC_A,    KC_S,    KC_D,   KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,    KC_ENTER,
   //|-----------+--------+--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------+--------+--------------|             ,--------.
          KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                            KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,             KC_RSFT,                  KC_UP,
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FUNC] = LAYOUT_split(
   //,--------------------------------------------------------------.                 ,--------------------------------------------------------------------.    ,---------------------------.
-       TD(TD_GRAVE_ESC),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,    KC_F12,    XXXXXXX,      XXXXXXX, XXXXXXX,  XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
+       TD(TD_GRAVE_ESC),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,    KC_F12,    XXXXXXX,         XXXXXXX, XXXXXXX,  XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|         |-------|--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                  |-------|--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
         XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
   //|-----------+--------+--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------+--------+--------------|             ,--------.
          KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX,               XXXXXXX,
@@ -57,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MEDIA] = LAYOUT_split(
   //,--------------------------------------------------------------.                 ,--------------------------------------------------------------------.    ,---------------------------.
-       TD(TD_GRAVE_ESC),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,    KC_F12,    XXXXXXX,       XXXXXXX, KC_MPLY, KC_VOLU,
-  //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,        XXXXXXX, KC_MSTP, KC_VOLD,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
+      TD(TD_GRAVE_ESC),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,    KC_F12,    XXXXXXX,       XXXXXXX, KC_MPLY, KC_VOLU,
+  //|--------+--------+--------+--------+--------+--------+--------|         |-------|--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,       XXXXXXX, KC_MSTP, KC_VOLD,
+  //|--------+--------+--------+--------+--------+--------|                  |-------|--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
         KC_MPRV, KC_MNXT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
   //|-----------+--------+--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------+--------+--------------|             ,--------.
          KC_LSFT, XXXXXXX, XXXXXXX, KC_CALC, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,               XXXXXXX,               XXXXXXX,
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SYSTEM] = LAYOUT_split(
   //,--------------------------------------------------------------.                 ,--------------------------------------------------------------------.    ,---------------------------.
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,      XXXXXXX, XXXXXXX,  XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX,                            BL_TOGG,  BL_INC, BL_BRTG, XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,      XXXXXXX, XXXXXXX,  XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------|--------+--------+--------+--------+---------+----------+-----------|    +--------+--------+---------|
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX,                   BL_TOGG,  BL_INC, BL_BRTG, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,      XXXXXXX, XXXXXXX,  XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                 |--------|--------+--------+--------+--------+---------+----------+-----------|    `---------------------------.
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX,                            BL_STEP,  BL_DEC, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,
   //|-----------+--------+--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------+--------+--------------|             ,--------.
        KC_PWR,   XXXXXXX,  KC_SLEP,  XXXXXXX, XXXXXXX,XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,              XXXXXXX,               XXXXXXX,
