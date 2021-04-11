4311909a8f62cbf784d3bba77ad3067ff3640b1b
WholesomeDucky <chris.niner@gmail.com>
2021-04-11 04:40:48 -0400

    Corrected for RAlt and Fn being swapped

--- keyboards/gmmk/pro/keymaps/wholesomeducky/keymap.c
+++ keyboards/gmmk/pro/keymaps/wholesomeducky/keymap.c
@@ -32,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_RGUI,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_APP, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(1),   KC_RALT, KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
-
+// Alt and Fn are swapped for some reason on the keymap, thank you Glorious, very cool
     [1] = LAYOUT(
-        RESET,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,
+        KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,
         KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,          KC_INS,
         KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_PSCR,   KC_SLCK,   KC_PAUS,  KC_TRNS, KC_TRNS,  KC_TRNS,          KC_HOME,
         KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_MPLY,  KC_TRNS,           KC_TRNS,          KC_END,
-        KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_MPRV,   KC_MNXT,  KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,             KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_MPRV,   KC_MNXT,  RESET,             KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,  KC_TRNS,   KC_TRNS,                                    KC_TRNS,                                    KC_TRNS,  KC_TRNS, KC_TRNS,  KC_HOME, KC_TRNS, KC_END
     ),
 
