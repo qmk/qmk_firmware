d0a882ecd4dd84ff04b94720f5a542b5739a07b0
Andrew Lysaght <alysaght@AL-MB-4.local>
2021-04-12 10:06:19 -0400

    refine tap hold keys

--- keyboards/atreus62/keymaps/ALysaght/keymap.c
+++ keyboards/atreus62/keymaps/ALysaght/keymap.c
@@ -45,22 +45,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                ,-----------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |                |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+------|                |------+------+------+------+------+------|
- * |  ` sd  |   Q  |   W  |   E  |   R  |   T  |                |   Y  |   U  |   I  |   O  |   P  |  [   |
+ * |  `   |   Q  |   W  |   E  |   R  |   T  |                |   Y  |   U  |   I  |   O  |   P  |  [   |
  * |------+------+------+------+------+------|                |------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |,------.,------.|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------||      ||      ||------+------+------+------+------+------|
- * |Shift |   Z  |   X  |   C  |   V  |   B  ||GuiBsp||GuiSpc||   N  |   M  |   ,  |   .  |   /  |SftEnt|
+ * |Shift |   Z  |   X  |   C  |   V  |   B  ||GuiBsp||Space ||   N  |   M  |   ,  |   .  |   /  |SftEnt|
  * |------+------+------+------+------+------||      ||      ||------+------+------+------+------+------|
- * | NUM  |Ctrl  | Alt  |   _  |Lower |GuiBsp|`------'`------'|GuiSpc|Raise | Left | Down |  Up  |Right |
+ * | NUM  |Ctrl  | Alt  |   _  |Lower |GuiBsp|`------'`------'|Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------'                `-----------------------------------------'
  */
 
   [_QWERTY] = LAYOUT(
-      KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,              KC_5,                                            KC_6,    KC_7,       KC_8,    KC_9,      KC_0,    KC_BSLS,
-      KC_GRV,     KC_Q,    KC_W,    KC_E,    KC_R,              KC_T,                                            KC_Y,    KC_U,       KC_I,    KC_O,      KC_P,    KC_LBRC,
-      KC_TAB,     KC_A,    KC_S,    KC_D,    KC_F,              KC_G,                                            KC_H,    KC_J,       KC_K,    KC_L,   KC_SCLN,    KC_QUOT,
-     KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,              KC_B,                                            KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_SFTENT,
-    TG(_NUM),  KC_LCTL, KC_LALT, KC_UNDS,   LOWER,   LGUI_T(KC_BSPC), LGUI_T(KC_BSPC), LGUI_T(KC_SPC), LGUI_T(KC_SPC),   RAISE,    KC_LEFT, KC_DOWN,     KC_UP,   KC_RIGHT
+      KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,              KC_5,                            KC_6,    KC_7,       KC_8,    KC_9,      KC_0,    KC_BSLS,
+      KC_GRV,     KC_Q,    KC_W,    KC_E,    KC_R,              KC_T,                            KC_Y,    KC_U,       KC_I,    KC_O,      KC_P,    KC_LBRC,
+      KC_TAB,     KC_A,    KC_S,    KC_D,    KC_F,              KC_G,                            KC_H,    KC_J,       KC_K,    KC_L,   KC_SCLN,    KC_QUOT,
+     KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,              KC_B,                            KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_SFTENT,
+    TG(_NUM),  KC_LCTL, KC_LALT, KC_UNDS,   LOWER,   LGUI_T(KC_BSPC), LGUI_T(KC_BSPC), KC_SPC, KC_SPC,   RAISE,    KC_LEFT, KC_DOWN,     KC_UP,   KC_RIGHT
   ),
 
 
