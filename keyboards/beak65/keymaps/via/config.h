d95311f2da01b2bd53f351531d9e69cd5cc75aa6
Adam Naldal <adamnaldal@gmail.com>
2021-04-12 14:57:10 +0200

    removed encoder code

--- keyboards/beak65/keymaps/via/config.h
+++ keyboards/beak65/keymaps/via/config.h
@@ -18,5 +18,7 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 2
 
 /* defining an extra row for encoder assignment in VIA */
+/*
 #undef MATRIX_ROWS
 #define MATRIX_ROWS 11
+*/
--- keyboards/beak65/keymaps/via/keymap.c
+++ keyboards/beak65/keymaps/via/keymap.c
@@ -15,40 +15,19 @@
  */
 #include QMK_KEYBOARD_H
 
-enum layers {
-    _0,
-    _1,
-    _2,
-    _3,
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_0] = beak65_via(
+    [0] = beak65_via(
 		 KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,
 		 KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,           KC_MPLY,
 		KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,           KC_ENT,
 		KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,                 KC_RSFT,             KC_UP,
-		KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT, KC_RGUI,   MO(1), KC_RCTL,  KC_LEFT, KC_DOWN, KC_RIGHT,
-    KC_VOLD, KC_VOLU),
-    [_1] = beak65_via(
+		KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT, KC_RGUI,   MO(1), KC_RCTL,  KC_LEFT, KC_DOWN, KC_RIGHT),
+    [1] = beak65_via(
      KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,
   	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
   	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
   	_______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                 _______,           _______,
-  	_______, _______, _______,                            _______,                            _______, _______, _______, _______,  _______, _______, _______,
-    KC_WH_D, KC_WH_U)
+  	_______, _______, _______,                            _______,                            _______, _______, _______, _______,  _______, _______, _______)
 };
-
-#ifdef ENCODER_ENABLE
-void encoder_update_user(uint8_t index, bool clockwise) {
-    uint8_t layer = get_highest_layer(layer_state);
-    if (index == 0) {
-        if (clockwise) {
-            tap_code16(dynamic_keymap_get_keycode(layer, 5, 1));
-        } else {
-            tap_code16(dynamic_keymap_get_keycode(layer, 5, 0));
-        }
-    }
-}
-#endif
