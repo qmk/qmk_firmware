34a453e05cd4a6d602a9bdfe5cfa9765ae3e8d93
Marius Kavoliunas <marius.kavoliunas@telesoftas.com>
2021-04-13 07:22:07 +0300

    remove unnecessary config.h parameters

--- keyboards/cybergear/macro25/config.h
+++ keyboards/cybergear/macro25/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 CyberGear Marius Kavoliunas
+	/* Copyright 2021 CyberGear Marius Kavoliunas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,17 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x1209
-#define PRODUCT_ID      0x69A1
-#define DEVICE_VER      0x0001
 #define MANUFACTURER    CyberGear
 #define PRODUCT         Macro25 (mk-macro-01)
 
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 5
-
-#define MATRIX_ROW_PINS { E6, B4 }
-#define MATRIX_COL_PINS { B1, F7, F6, F5, F4 }
-
-#define DIODE_DIRECTION COL2ROW
-
