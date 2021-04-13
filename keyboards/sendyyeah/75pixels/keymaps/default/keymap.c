e0a03c4db74d34de8194268de9242199d4d678d7
Sendy Aditya Suryana <sendzation@gmail.com>
2021-04-13 10:53:25 +0700

    Update keyboards/sendyyeah/75pixels/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/sendyyeah/75pixels/keymaps/default/keymap.c
+++ keyboards/sendyyeah/75pixels/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z   , KC_X   , KC_C  , KC_V   , KC_B  , KC_N  , KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_P1  , KC_P2  , KC_P3  , \
         KC_LCTL, KC_LGUI, KC_LALT, MO(FN), _______, KC_SPC, KC_SPC, TO(FN), KC_RALT, KC_RGUI, KC_MENU, KC_RCTL, KC_P0  , KC_PDOT, KC_ENT
     ),
-    [FN] = LAYOUT(
+    [FN] = LAYOUT_ortho_5x15(
         _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 , KC_F10 , KC_DEL , _______, _______, _______, \
         _______, KC_F11 , KC_F12 , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
