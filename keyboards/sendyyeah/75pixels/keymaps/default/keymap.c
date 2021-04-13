9be931294e8dc4912413a7577bf7a62655424ee7
Sendy Aditya Suryana <sendzation@gmail.com>
2021-04-13 10:53:30 +0700

    Update keyboards/sendyyeah/75pixels/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/sendyyeah/75pixels/keymaps/default/keymap.c
+++ keyboards/sendyyeah/75pixels/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [QW] = LAYOUT(
+    [QW] = LAYOUT_ortho_5x15(
         KC_ESC , KC_1   , KC_2   , KC_3  , KC_4   , KC_5  , KC_6  , KC_7  , KC_8   , KC_9   , KC_0   , KC_BSPC, KC_NLCK, KC_PSLS, KC_PAST, \
         KC_TAB , KC_Q   , KC_W   , KC_E  , KC_R   , KC_T  , KC_Y  , KC_U  , KC_I   , KC_O   , KC_P   , KC_BSLS, KC_P7  , KC_P8  , KC_P9  , \
         KC_CAPS, KC_A   , KC_S   , KC_D  , KC_F   , KC_G  , KC_H  , KC_J  , KC_K   , KC_L   , KC_SCLN, KC_ENT , KC_P4  , KC_P5  , KC_P6  , \
