1d359ab1c9ff5ee776e7aa04ccf0c4867af781a2
wiley <bwsharp@hey.com>
2021-04-12 21:58:31 -0500

    updated config.h

--- keyboards/keebio/quefrency/keymaps/shabby-goddess/config.h
+++ keyboards/keebio/quefrency/keymaps/shabby-goddess/config.h
@@ -22,3 +22,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // #define USE_I2C
+
+#define ENCODER_RESOLUTION 2
+
+
+// set rbg to a slow breathe effect and a soft purple color. see https://docs.qmk.fm/#/feature_rgblight
+#define RGBLIGHT_SLEEP 1
+#define RGBLIGHT_SPLIT 1
+
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING
+#define RGBLIGHT_EFFECT_BREATHE_MAX 170
+
+#define RGBLIGHT_DEFAULT_SPD 85
+
+#define RGBLIGHT_DEFAULT_HUE 188
+#define RGBLIGHT_DEFAULT_SAT 89
+#define RGBLIGHT_DEFAULT_VAL 78
+
