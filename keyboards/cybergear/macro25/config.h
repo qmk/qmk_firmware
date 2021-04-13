0b85644646b70551333807f9fff214e5fbf3f795
Marius Kavoliunas <kavoliunas.m@gmail.com>
2021-04-13 08:43:14 +0300

    Update keyboards/cybergear/macro25/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/cybergear/macro25/config.h
+++ keyboards/cybergear/macro25/config.h
@@ -1,4 +1,4 @@
-	/* Copyright 2021 CyberGear Marius Kavoliunas
+/* Copyright 2021 CyberGear Marius Kavoliunas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
