409477e4669b74f55d1ed4eb6ca2e2449e862467
Laura Brustenga i Moncusí <brust@math.ku.dk>
2021-04-14 13:30:36 +0200

    empty pssw

--- keyboards/preonic/keymaps/LauraBMo/keymap.c
+++ keyboards/preonic/keymaps/LauraBMo/keymap.c
@@ -224,6 +224,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     break;
   case PSSW:
     if (record->event.pressed) {
+      SEND_STRING("");
     }
     return false;
     break;
