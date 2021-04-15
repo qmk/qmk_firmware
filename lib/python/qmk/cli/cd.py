7303e6937b231cd902aac971c4924a4e37fb0da8
Erovia <erovia@users.noreply.github.com>
2021-04-15 13:23:39 +0100

    Fix for Windows and do not run if already under QMK Home

--- lib/python/qmk/cli/cd.py
+++ lib/python/qmk/cli/cd.py
@@ -1,14 +1,31 @@
 """Open a shell in the QMK Home directory
 """
-import pty
-import getpass
-import pwd
+import os
 
 from milc import cli
 
+from qmk.path import under_qmk_firmware
 
 @cli.subcommand('Go to QMK Home')
 def cd(cli):
     """Go to QMK Home
     """
-    pty.spawn(pwd.getpwnam(getpass.getuser()).pw_shell)
+    if not under_qmk_firmware():
+        # Only do anything if the user is not under qmk_firmware already
+        # in order to reduce the possibility of starting multiple shells
+        if not cli.platform.lower().startswith('windows'):
+            # For Linux/Mac/etc
+            # Check the user's login shell from 'passwd'
+            # alternatively fall back to $SHELL env var
+            # and finally to '/bin/bash'.
+            import pty, getpass, pwd
+            shell = pwd.getpwnam(getpass.getuser()).pw_shell
+            if not shell:
+                shell = os.environ.get('SHELL', '/bin/bash')
+            pty.spawn(shell)
+        else:
+            # For Windows
+            # We just start a new subshell using $SHELL and
+            # fall back to '/usr/bin/bash'.
+            from qmk.commands import run
+            run([os.environ.get('SHELL', '/usr/bin/bash')])
