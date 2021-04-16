e3018ff1ed86cb54b488773e74b0889b86336c2e
Ben Fiedler <git@bfiedler.ch>
2021-04-16 19:22:20 +0200

    Remove unused left momentary toggle, add right reset button

--- keyboards/keebio/quefrency/keymaps/bfiedler/keymap.c
+++ keyboards/keebio/quefrency/keymaps/bfiedler/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,          KC_Q,         KC_W,    KC_E,     KC_R,    KC_T,   KC_Y,             KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC, KC_RBRC,        KC_BSLS, KC_END, \
     LGUI_T(KC_BSPC), KC_A,         KC_S,    KC_D,     KC_F,    KC_G,   KC_H,             KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT, LGUI_T(KC_ENT), KC_PGUP, \
     KC_LSFT,         LCTL_T(KC_Z), KC_X,    KC_C,     KC_V,    KC_B,   KC_N,             KC_M,    KC_COMM, KC_DOT,  LCTL_T(KC_SLSH), KC_RSFT, KC_UP,          KC_PGDN, \
-    KC_LCTL,         KC_LALT,      KC_LGUI, MO(_FN1), KC_SPC,          LT(_FN1, KC_ENT), KC_RALT, _______, KC_RCTL, KC_RGUI,         KC_LEFT, KC_DOWN,        KC_RGHT
+    KC_LCTL,         KC_LALT,      KC_LGUI, KC_LALT,  KC_SPC,          LT(_FN1, KC_ENT), KC_RALT, _______, KC_RCTL, KC_RGUI,         KC_LEFT, KC_DOWN,        KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65(
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_TOG,         RGB_MOD,      _______, _______, _______, _______, _______,          _______, _______, _______, _______,         _______, _______,        _______, _______, \
     _______,         _______,      _______, _______, _______, _______, KC_LEFT,          KC_DOWN, KC_UP,   KC_RGHT, _______,         _______, _______,        _______, \
     _______,         _______,      _______, _______, _______, _______, _______,          _______, _______, _______, _______,         _______, _______,        _______, \
-    KC_RST,          _______,      _______, _______, _______,          _______,          _______, _______, _______, _______,         _______, _______,        _______
+    KC_RST,          _______,      _______, _______, _______,          _______,          _______, _______, _______, _______,         _______, _______,        KC_RST
   )
 };
 // clang-format on
