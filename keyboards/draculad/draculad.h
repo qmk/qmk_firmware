f6e92dca63006c49bc2001ebc84f4141ee4dfbd8
mangoiv <mangoiv@outlook.com>
2021-04-17 12:21:57 +0200

    added changes as recommended by QMK reviewer

--- keyboards/draculad/draculad.h
+++ keyboards/draculad/draculad.h
@@ -1,8 +1,25 @@
+/*
+Copyright 2021 @mangoiv
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include "quantum.h"
 
-#define ___ KC_NO
+#define XXX KC_NO
 
 #define LAYOUT( \
   L00, L01, L02, L03, L04,           R00, R01, R02, R03, R04, \
@@ -14,9 +31,9 @@
     { L00, L01, L02, L03, L04 }, \
     { L10, L11, L12, L13, L14 }, \
     { L20, L21, L22, L23, L24 }, \
-    { ___, L30, L31, L32, L33 }, \
+    { XXX, L30, L31, L32, L33 }, \
     { R04, R03, R02, R01, R00 }, \
     { R14, R13, R12, R11, R10 }, \
     { R24, R23, R22, R21, R20 }, \
-    { ___, R33, R32, R31, R30 } \
+    { XXX, R33, R32, R31, R30 } \
   }
