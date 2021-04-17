c1b0b1e1bba63ff47aa8b29330d2174bcf010938
Christian Eiden <christian@eiden.ch>
2021-04-17 13:23:28 +0200

    finetuning personal keymap
    Signed-off-by: Christian Eiden <christian@eiden.ch>

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/config.h
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/config.h
@@ -44,11 +44,16 @@
 #define BOOTMAGIC_LITE_COLUMN_RIGHT 5
 
 #define TAPPING_TERM 200
+
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
+
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 // #define TAPPING_FORCE_HOLD
+
 // Apply the modifier on keys that are tapped during a short hold of a modtap
 // #define PERMISSIVE_HOLD
+
+#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
 
 #define FORCE_NKRO
