19faa81fe8be07c83b60e037eb34c74693974b8b
Weirdo <1248314361@qq.com>
2021-04-17 18:39:11 +0800

    Create rules.mk

---
+++ keyboards/tiger910/rules.mk
@@ -0,0 +1,19 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = yes	# Console for debug
+COMMAND_ENABLE = yes    # Commands for debug and configuration
+SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no	      # USB Nkey Rollover
+RGBLIGHT_ENABLE = no
+
+LAYOUTS = 60_ansi_split_bs_rshift 60_ansi 60_tsangan_hhkb
