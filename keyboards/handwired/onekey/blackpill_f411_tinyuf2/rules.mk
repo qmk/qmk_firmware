819eb5bf470d45988040a5f026d6209958e0b60d
Nick Brassel <nick@tzarc.org>
2021-04-17 11:16:43 +1000

    Add support for jumping to tinyuf2 bootloader. Adds blackpill UF2 example.

---
+++ keyboards/handwired/onekey/blackpill_f411_tinyuf2/rules.mk
@@ -0,0 +1,10 @@
+# MCU name
+MCU = STM32F411
+
+# Build Options
+#   change yes to no to disable
+#
+KEYBOARD_SHARED_EP       = yes
+
+# We want to use the tinyuf2 bootloader...
+BOOTLOADER = tinyuf2
