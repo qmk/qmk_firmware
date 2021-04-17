45a7616d150dc13d6c554814ace5da7463103e85
vitvlkv <vitvlkv@gmail.com>
2021-04-17 21:38:56 +0300

    Searching for a better position for KC_MINS

--- keyboards/avalanche/keymaps/default/keymap.c
+++ keyboards/avalanche/keymaps/default/keymap.c
@@ -12,9 +12,9 @@ enum layer {
     LAYER_3
 };
 
-#define FN_1 MO(LAYER_1)
+#define FN_1 LT(LAYER_1, KC_MINS)
 #define FN_2 LT(LAYER_2, KC_SPC)
-#define LFN_3 LT(LAYER_3, KC_MINS)
+#define LFN_3 LT(LAYER_3, KC_GRV)
 #define RFN_3 LT(LAYER_3, KC_EQL)
 #define KC_QTSH RSFT_T(KC_QUOT)
 #define KC_ETAL RALT_T(KC_ENT)
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
                  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_LGUI, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL,  KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QTSH, KC_RBRC,
-                 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_INS,  KC_PSCR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-                                            KC_GRV,  LFN_3,   FN_2,    KC_LALT, KC_ETAL, FN_1,    RFN_3,   KC_APP
+                 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_CAPS, KC_PSCR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
+                                            KC_INS,  LFN_3,   FN_2,    KC_LALT, KC_ETAL, FN_1,    RFN_3,   KC_APP
     ),
     [LAYER_1] = LAYOUT(
                  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
