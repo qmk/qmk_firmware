ee15cb59be15e1fa741a6d08f6db7547d8125385
Julian Jelfs <julian.jelfs@ef.com>
2021-04-18 16:52:13 +0100

    adding my configs

---
+++ keyboards/kyria/keymaps/jelfs/rules.mk
@@ -0,0 +1,5 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+WPM_ENABLE = yes
---
+++ keyboards/pinky/4/rules.mk
@@ -0,0 +1,2 @@
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+TAP_DANCE_ENABLE = yes       # enable tap dance
