85f72b63ecf4e9c128bc69083482db55fa3e2509
Christiano Haesbaert <haesbaert@haesbaert.org>
2021-04-19 10:29:33 +0200

    Update keyboards/handwired/amigopunk/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/amigopunk/rules.mk
+++ keyboards/handwired/amigopunk/rules.mk
@@ -7,15 +7,15 @@ BOOTLOADER = halfkay
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE ?= yes # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE ?= yes  # Mouse keys(+4700)
-EXTRAKEY_ENABLE ?= yes  # Audio control and System control(+450)
-CONSOLE_ENABLE ?= no    # Console for debug(+400)
-COMMAND_ENABLE ?= no    # Commands for debug and configuration
-SLEEP_LED_ENABLE ?= no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE ?= yes      # USB Nkey Rollover
-BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
-AUDIO_ENABLE ?= no      # Enable audio
-RGBLIGHT_ENABLE ?= no   # Enable RGB
-ENCODER_ENABLE ?= yes   # Enable rotary encoders
-OLED_DRIVER_ENABLE ?= yes # OLED screen
+BOOTMAGIC_ENABLE = yes # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes  # Mouse keys
+EXTRAKEY_ENABLE = yes  # Audio control and System control
+CONSOLE_ENABLE = no    # Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes      # USB Nkey Rollover
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
+AUDIO_ENABLE = no      # Enable audio
+RGBLIGHT_ENABLE = no   # Enable RGB
+ENCODER_ENABLE = yes   # Enable rotary encoders
+OLED_DRIVER_ENABLE = yes # OLED screen
