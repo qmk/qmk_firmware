3b0f9da29518c7bcf06d31ce7dae73ba050578dc
David Fryer <dfryer1193@gmail.com>
2021-04-20 18:22:09 -0400

    Remove via from default keymaps, add via keymaps

--- keyboards/afternoonlabs/summer_breeze/keymaps/default/rules.mk
+++ keyboards/afternoonlabs/summer_breeze/keymaps/default/rules.mk
@@ -1,4 +1,3 @@
-VIA_ENABLE = yes
 # LTO: link time optimization makes the build take slightly longer
 # but makes the resulting .hex file smaller, which allows you to
 # fit more features into smaller MCUs:
--- keyboards/afternoonlabs/summer_breeze/keymaps/via/rules.mk
+++ keyboards/afternoonlabs/summer_breeze/keymaps/via/rules.mk
@@ -1,3 +1,4 @@
+VIA_ENABLE = yes
 # LTO: link time optimization makes the build take slightly longer
 # but makes the resulting .hex file smaller, which allows you to
 # fit more features into smaller MCUs:
