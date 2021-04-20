7055d9a3a753851474af95b9b1f6b159e30b41c0
Mango The Fourth <40720523+MangoIV@users.noreply.github.com>
2021-04-20 13:25:01 +0200

    Update keyboards/draculad/keymaps/pimoroni/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/draculad/keymaps/pimoroni/rules.mk
+++ keyboards/draculad/keymaps/pimoroni/rules.mk
@@ -1,10 +1,6 @@
 # only uncomment on the side you have your trackball on
-PIMORONI_TRACKBALL_ENABLE = yes
-
-ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
-    POINTING_DEVICE_ENABLE = yes
-    SRC += pimoroni_trackball.c
-    QUANTUM_LIB_SRC += i2c_master.c
-	OLED_DRIVER_ENABLE = yes
-	MOUSEKEY_ENABLE = no
-endif
+POINTING_DEVICE_ENABLE = yes
+SRC += pimoroni_trackball.c
+QUANTUM_LIB_SRC += i2c_master.c
+OLED_DRIVER_ENABLE = yes
+MOUSEKEY_ENABLE = no
