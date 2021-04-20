ed077a73524f352ba3a6f147b4872f5f3638a648
Brandon <brandon.abr@hotmail.com>
2021-04-19 21:41:27 -0500

    Requested changes

--- keyboards/smallice/config.h
+++ keyboards/smallice/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x514b // QK Quark
+#define VENDOR_ID    0x514B // QK Quark
 #define PRODUCT_ID   0x5341 // SA smAllice
 #define DEVICE_VER   0x0001
 #define MANUFACTURER quark
