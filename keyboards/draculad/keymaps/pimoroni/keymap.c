fa3981e4bddc9806c009ccfe21902a9dc8465562
Mango The Fourth <40720523+MangoIV@users.noreply.github.com>
2021-04-20 13:22:58 +0200

    Update keyboards/draculad/keymaps/pimoroni/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/draculad/keymaps/pimoroni/keymap.c
+++ keyboards/draculad/keymaps/pimoroni/keymap.c
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "pimoroni_trackball.h"
 #include "pointing_device.h"
 
-extern uint8_t is_master;
 
 enum layer_number {
   _BASE = 0,
