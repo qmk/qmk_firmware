f9b5f3d79f0d8335f41f4fe11491eecaea8bde1f
Mango The Fourth <40720523+MangoIV@users.noreply.github.com>
2021-04-20 13:26:29 +0200

    Update keyboards/draculad/keymaps/pimoroni/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/draculad/keymaps/pimoroni/keymap.c
+++ keyboards/draculad/keymaps/pimoroni/keymap.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "draculad.h"
+#include QMK_KEYBOARD_H
 #include "pimoroni_trackball.h"
 #include "pointing_device.h"
 
