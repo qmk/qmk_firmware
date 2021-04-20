c91974b16de0dca5505783ecf24d3cc513716f51
Victor Pivert <76471338+piv3rt@users.noreply.github.com>
2021-04-20 09:21:56 +0200

    Move keypad toggle and disable MAC led indicator

--- keyboards/dztech/dz60rgb/keymaps/piv3rt/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/piv3rt/keymap.c
@@ -17,7 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define LT_DEL  LT(_RGB, KC_DEL)
 #define LT_CAPS LT(_FNC, KC_CAPS)
-#define LT_NUM  LT(_NUM, KC_LGUI)
 #define LM_LALT LM(_FNC, MOD_LALT)
 #define LM_NALT LM(_NUM, MOD_LALT)
 #define RSFT_SL RSFT_T(KC_SLSH)
@@ -56,39 +55,39 @@ enum piv3rt_rgbprofiles current_profile = OFF;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEF] = LAYOUT(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
-        LT_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,           KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   RSFT_SL,  KC_UP,   LT_DEL,
-        KC_LCTL, LT_NUM,  LM_LALT,                            KC_SPC,                    RALT_F6, MO(_FNC), KC_LEFT,  KC_DOWN, KC_RGHT
+        KC_GESC, KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
+        KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
+        LT_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,           KC_ENT,
+        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   RSFT_SL,  KC_UP,   LT_DEL,
+        KC_LCTL, KC_LGUI,  LM_LALT,                            KC_SPC,                    RALT_F6, MO(_FNC), KC_LEFT,  KC_DOWN, KC_RGHT
     ),
     [_MAC] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,           _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______,
-        KC_LGUI, KC_LCTL, LT_NUM,                             _______,                   _______, _______,  _______,  _______, _______
+        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______,
+        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______,
+        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,           _______,
+        _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______,
+        KC_LGUI, KC_LCTL,  KC_LGUI,                            _______,                   _______, _______,  _______,  _______, _______
     ),
     [_FNC] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,
-        _______, AGRAVE,  KC_UP,   EACUTE,  _______, KC_INS,  _______, _______, KC_UP,   KC_UP,   KC_PGUP,  KC_HOME,  KC_PSCR, KC_NUBS,
-        _______, KC_HOME, KC_DOWN, KC_END,  CTLINS,  SFTINS,  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  KC_END,            KC_F8,
-        _______,          _______, KC_CALC, CCED,    _______, _______, _______, _______, FRQLT,   FRQGT,    TG(_MAC), KC_VOLU, KC_MUTE,
-        _______, _______, LM_NALT,                            _______,                   KC_MPLY, _______,  KC_MPRV,  KC_VOLD, KC_MNXT
+        _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,
+        _______, AGRAVE,   KC_UP,   EACUTE,  _______, KC_INS,  _______, _______, KC_UP,   KC_UP,   KC_PGUP,  KC_HOME,  KC_PSCR, KC_NUBS,
+        _______, KC_HOME,  KC_DOWN, KC_END,  CTLINS,  SFTINS,  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  KC_END,            KC_F8,
+        _______,           _______, KC_CALC, CCED,    _______, _______, _______, _______, FRQLT,   FRQGT,    TG(_NUM), KC_VOLU, KC_MUTE,
+        _______, TG(_MAC), LM_NALT,                            _______,                   KC_MPLY, _______,  KC_MPRV,  KC_VOLD, KC_MNXT
     ),
     [_RGB] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,
-        _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______,  _______,  _______, RESET,
-        _______, _______, _______, _______, _______, RGB_PCY, _______, _______, RGB_SPI, RGB_SPD, _______,  _______,           EEP_RST,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  RGB_000, _______,
-        _______, _______, _______,                            RGB_RST,                   _______, _______,  KC_PWR,   AMDREC,  AMDREP
+        _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,
+        _______, RGB_TOG,  _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______,  _______,  _______, RESET,
+        _______, _______,  _______, _______, _______, RGB_PCY, _______, _______, RGB_SPI, RGB_SPD, _______,  _______,           EEP_RST,
+        _______,           _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  RGB_000, _______,
+        _______, _______,  _______,                            RGB_RST,                   _______, _______,  KC_PWR,   AMDREC,  AMDREP
     ),
     [_NUM] = LAYOUT(
-        KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,    KC_PMNS,  KC_PPLS, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,    _______,  _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,    _______,           KC_PENT,
-        _______,          _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT,  _______,  _______, _______,
-        _______, _______, _______,                            KC_PENT,                   _______, _______,  _______,  _______, _______
+        KC_NLCK, _______,  _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,    _______,  _______, _______,
+        _______, _______,  _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,    _______,  _______, _______,
+        _______, _______,  _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,    KC_PENT,           KC_PENT,
+        _______,           _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT,  TG(_NUM), _______, _______,
+        _______, _______,  _______,                            KC_PENT,                   _______, _______,  _______,  _______, _______
     ),
 };
 
@@ -172,11 +171,6 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             RGB_MATRIX_INDICATOR_SET_COLOR(13, 0xff, 0x55, 0x55);
         }
 
-        if (layer_state_is(_MAC)) {
-            // OS key = 61
-            RGB_MATRIX_INDICATOR_SET_COLOR(61, 0xff, 0x55, 0x55);
-        }
-
         if (layer_state_is(_NUM)) {
             // Num pad
             for (int i=0; i<3; i++) {
@@ -184,8 +178,7 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                 RGB_MATRIX_INDICATOR_SET_COLOR(17 + i, 0xff, 0x55, 0x55);
                 RGB_MATRIX_INDICATOR_SET_COLOR(30 + i, 0xff, 0x55, 0x55);
             }
-            RGB_MATRIX_INDICATOR_SET_COLOR(44, 0xff, 0x55, 0x55);
-            RGB_MATRIX_INDICATOR_SET_COLOR(45, 0xff, 0x55, 0x55);
+            RGB_MATRIX_INDICATOR_SET_COLOR(43, 0xff, 0x00, 0x00);
             return;
         }
 
