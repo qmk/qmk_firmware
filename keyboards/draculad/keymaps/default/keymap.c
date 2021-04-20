1e2d0f9cb2c249e5abad80c9710136ab6d3e6b94
Mango The Fourth <40720523+MangoIV@users.noreply.github.com>
2021-04-20 13:19:32 +0200

    Update keyboards/draculad/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/draculad/keymaps/default/keymap.c
+++ keyboards/draculad/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "draculad.h"
+#include QMK_KEYBOARD_H
 
 extern uint8_t is_master;
 
