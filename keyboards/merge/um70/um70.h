48c8e1bac5d6b728e81a98a64de906ed4e90238f
duoshock <33636898+duoshock@users.noreply.github.com>
2021-04-21 14:09:30 +0100

    Update keyboards/merge/um70/um70.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/merge/um70/um70.h
+++ keyboards/merge/um70/um70.h
@@ -77,7 +77,7 @@
     { R40, R41, R42, R43, R44, R45, XXX, R47 } \
 }
 
-#define LAYOUT_LSpace_SplitBksp( \
+#define LAYOUT_lspace_splitbksp( \
             L00, L01, L02, L03, L04, L05, L06,              R00, R01, R02, R03, R04, R05, R06, R07,     R27, \
             L10, L11, L12, L13, L14, L15,                   R10, R11, R12, R13, R14, R15, R16, R17,     R37, \
     L45,    L20, L21, L22, L23, L24, L25,                   R20, R21, R22, R23, R24, R25, R26,          R47, \
