235cb36805c4314bdfdec50a581245d7c12d3fcd
rainkeebs <44819800+rainkeebs@users.noreply.github.com>
2021-04-20 19:41:37 -0700

    removed unnecessary lines as per review

--- keyboards/delilah/keymaps/default/keymap.c
+++ keyboards/delilah/keymaps/default/keymap.c
@@ -41,7 +41,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, 1, 2, 3);
-}
