96aa64c83b31b21d7a26cf555d966b0621169a3d
coarse <mfmayol@up.edu.ph>
2021-04-21 22:49:54 +0800

    Use correct led_state

--- keyboards/charue/sunsetter/sunsetter.c
+++ keyboards/charue/sunsetter/sunsetter.c
@@ -26,7 +26,7 @@ void keyboard_pre_init_kb(void) {
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (led_update_user(led_state)) {
-        writePin(F1, !led_state.num_lock);
+        writePin(F1, !led_state.caps_lock);
     }
     return res;
 }
