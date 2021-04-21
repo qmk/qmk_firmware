d0eddb2f668f1641fb376611df47469b01e1a8c5
bbrfkr <bbrfkr@gmail.com>
2021-04-21 11:27:29 +0900

    Update keyboards/molecule/adns.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/molecule/adns.h
+++ keyboards/molecule/adns.h
@@ -23,7 +23,6 @@ void adns_write(uint8_t reg_addr, uint8_t data);
 
 uint8_t adns_read(uint8_t reg_addr);
 
-void pointing_device_init(void);
 
 int16_t convertDeltaToInt(uint8_t high, uint8_t low);
 
