0515c771aef98f2d93df38e67c55b5e36fe73a65
Mango The Fourth <40720523+MangoIV@users.noreply.github.com>
2021-04-21 09:40:17 +0200

    Update keyboards/draculad/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/draculad/rules.mk
+++ keyboards/draculad/rules.mk
@@ -1,7 +1,6 @@
 # MCU name
 MCU = atmega32u4
 
-
 # Bootloader selection
 BOOTLOADER = caterina
 
@@ -9,23 +8,20 @@ BOOTLOADER = caterina
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes         # USB Nkey Rollover
+NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-UNICODE_ENABLE = no        # Unicode
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = yes        # Use shared split_common code
-
+SPLIT_KEYBOARD = yes
 OLED_DRIVER_ENABLE = yes
 WPM_ENABLE = yes
 ENCODER_ENABLE = yes
-RGBLIGHT_ENABLE = yes 
-LTO_ENABLE=yes
-OLED_DRIVER_ENABLE = yes
+LTO_ENABLE = yes
