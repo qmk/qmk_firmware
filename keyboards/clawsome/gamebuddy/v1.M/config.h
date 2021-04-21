80c506b978682caa382dd34b005dddb76cdae36d
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2021-04-20 22:54:55 -0600

    Update config.h

--- keyboards/clawsome/gamebuddy/v1.M/config.h
+++ keyboards/clawsome/gamebuddy/v1.M/config.h
@@ -23,7 +23,7 @@
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AlisGraveNil
-#define PRODUCT         GameBuddy2.M
+#define PRODUCT         GameBuddy1.M
 
 /* key matrix size */
 #define MATRIX_ROWS 5
