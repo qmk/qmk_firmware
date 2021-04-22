9e5dbfd8fe2ba43da4635f65fb3ee5201a07ce92
FateEverywhere <41215461+FateEverywhere@users.noreply.github.com>
2021-04-22 16:46:52 -0400

    Update config.h

--- keyboards/edi/hardlight/config.h
+++ keyboards/edi/hardlight/config.h
@@ -1,4 +1,6 @@
 /*
+©2021 Everywhere Defense Industries / Fate Everywhere <fate@7storm.org>
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
