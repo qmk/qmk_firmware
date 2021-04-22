946b77f404d150bf31b639060d8d578c96a4b219
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2021-04-22 14:27:01 +0900

    enable rotary encoder

--- keyboards/yushakobo/quick17/rules.mk
+++ keyboards/yushakobo/quick17/rules.mk
@@ -20,5 +20,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+ENCODER_ENABLE = yes
 
 #RGB_MATRIX_ENABLE = WS2812
