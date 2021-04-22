ee8ccc2b71e49426bffba32c5bf18a5bd49ae8ab
FateEverywhere <41215461+FateEverywhere@users.noreply.github.com>
2021-04-22 16:44:05 -0400

    Update keyboards/edi/hardlight/hardlight.h
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/edi/hardlight/hardlight.h
+++ keyboards/edi/hardlight/hardlight.h
@@ -13,8 +13,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef HARDLIGHT_H
-#define HARDLIGHT_H
+#pragma once
 
 #include "quantum.h"
 #define ___ KC_NO
