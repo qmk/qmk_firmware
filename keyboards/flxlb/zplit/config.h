a5acdd19132a7201b8400ad91c0c7115c9210b5e
cccywj <33842880+cccywj@users.noreply.github.com>
2021-04-22 23:24:20 -0700

    Update keyboards/flxlb/zplit/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/flxlb/zplit/config.h
+++ keyboards/flxlb/zplit/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    fluxlab
-#define PRODUCT         fluxlab zplit
+#define PRODUCT         zplit
 
 /* key matrix size */
 // Rows are doubled-up
