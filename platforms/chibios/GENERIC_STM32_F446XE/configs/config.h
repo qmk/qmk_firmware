e8d0067f61a804f61553b3605845a655b3f4458e
Purdea Andrei <andrei@purdea.ro>
2021-04-25 00:05:42 +0300

    Update platforms/chibios/GENERIC_STM32_F446XE/configs/config.h
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- platforms/chibios/GENERIC_STM32_F446XE/configs/config.h
+++ platforms/chibios/GENERIC_STM32_F446XE/configs/config.h
@@ -15,10 +15,7 @@
  */
 
 /* Address for jumping to bootloader on STM32 chips. */
-/* It is chip dependent, the correct number can be looked up here (page 175):
- * http://www.st.com/web/en/resource/technical/document/application_note/CD00167594.pdf
- * This also requires a patch to chibios:
- *  <tmk_dir>/tmk_core/tool/chibios/ch-bootloader-jump.patch
+/* It is chip dependent, the correct number can be looked up by checking against ST's application note AN2606.
  */
 #define STM32_BOOTLOADER_ADDRESS 0x1FFF0000
 #ifndef EARLY_INIT_PERFORM_BOOTLOADER_JUMP
