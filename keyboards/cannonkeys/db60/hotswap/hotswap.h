94314f0c7f50db6f6be91199109f562ca2e0328c
Andrew Kannan <andrew.kannan@gmail.com>
2021-04-24 11:59:29 -0400

    Fix matrix on hotswap

--- keyboards/cannonkeys/db60/hotswap/hotswap.h
+++ keyboards/cannonkeys/db60/hotswap/hotswap.h
@@ -27,7 +27,7 @@
   K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, \
   K40, K41,  K42,              K45,                 K4A, K4B, K4C,      K4E \
 ) { \
-  {  K00,  K01,  K02,  K03,  K04,    K05,    K06,    K07,    K08,    K09,  K0A,  K0B,  K0C,  KNO,  KOE}, \
+  {  K00,  K01,  K02,  K03,  K04,    K05,    K06,    K07,    K08,    K09,  K0A,  K0B,  K0C,  KNO,  K0E}, \
   {  K10,  K11,  K12,  K13,  K14,    K15,    K16,    K17,    K18,    K19,  K1A,  K1B,  K1C,  KNO,  K1E   }, \
   {  K20,  K21,  K22,  K23,  K24,    K25,    K26,    K27,    K28,    K29,  K2A,  K2B,  KNO,  KNO,  K2E  }, \
   {  K30,  KNO,  K32,  K33,  K34,    K35,    K36,    K37,    K38,    K39,  K3A,  K3B,  K3C,  KNO,  KNO  }, \
