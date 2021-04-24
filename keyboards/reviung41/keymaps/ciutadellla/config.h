ac713792245c7f90204ec6df480277f74a19ab59
Ciutadellla <pedro.aspiazu@gmail.com>
2021-04-24 03:54:34 +0000

    [Keymap] Reviung41_Ciutadellla

---
+++ keyboards/reviung41/keymaps/ciutadellla/config.h
@@ -0,0 +1,14 @@
+
+#pragma once
+#define RGBLIGHT_LAYERS
+#ifdef RGB_DI_PIN
+    // /*== all animations enable ==*/
+    //#define RGBLIGHT_ANIMATIONS
+    // /*== or choose animations ==*/
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#endif
+
+#define TAPPING_TERM 200
+#define UNICODE_SELECTED_MODES UC_MAC, UC_LNX, UC_WINC
