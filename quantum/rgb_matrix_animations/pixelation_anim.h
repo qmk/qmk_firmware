aac5d8e821b9daa24c0511636ff40fe9311b5827
Albert Y <76888457+filterpaper@users.noreply.github.com>
2021-04-25 15:40:21 +0800

    Remove led_max limit condition, always return false.
    Toggling effect do not need to cover all LEDs on each tick. The `prng` function will handle entire range.

--- quantum/rgb_matrix_animations/pixelation_anim.h
+++ quantum/rgb_matrix_animations/pixelation_anim.h
@@ -19,13 +19,10 @@ RGB_MATRIX_EFFECT(PIXELATION)
 #   ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 static bool PIXELATION(effect_params_t* params) {
-    RGB_MATRIX_USE_LIMITS(led_min, led_max);
-
     // Bob Jenkin's fast PRNG, smaller than rand()
     #define rot8(x,k) (((x) << (k))|((x) >> (8 - (k))))
     uint8_t prng(void) {
         static uint8_t a = 0x71, b = 0x1f, c = 0x2f, d = 0x3f;
-
         uint8_t e = a - rot8(b, 1);
         a = b ^ rot8(c, 4);
         b = c + d;
@@ -33,21 +30,18 @@ static bool PIXELATION(effect_params_t* params) {
         return d = e + a;
     }
 
-    void set_pixel_rgb(int i, effect_params_t* params, bool on) {
+    void set_rgb(int i, effect_params_t* params, bool on) {
         if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) { return; }
         on ? rgb_matrix_set_color(i, prng(), prng(), prng()) : rgb_matrix_set_color(i, RGB_OFF);
     }
 
-    if (!params->init) {
-        if (scale16by8(g_rgb_timer, qadd8((rgb_matrix_config.speed >> 5), 1)) % 10 == 0) {
-            set_pixel_rgb(prng() % DRIVER_LED_TOTAL, params, 1);
-        }
-        if (scale16by8(g_rgb_timer, qadd8((rgb_matrix_config.speed >> 5), 1)) % 5 == 0) {
-            set_pixel_rgb(prng() % DRIVER_LED_TOTAL, params, 0);
-        }
-        return false;
+    if (scale16by8(g_rgb_timer, qadd8((rgb_matrix_config.speed >> 5), 1)) % 10 == 0) {
+        set_rgb(prng() % DRIVER_LED_TOTAL, params, 1);
     }
-    return led_max < DRIVER_LED_TOTAL;
+    if (scale16by8(g_rgb_timer, qadd8((rgb_matrix_config.speed >> 5), 1)) % 5 == 0) {
+        set_rgb(prng() % DRIVER_LED_TOTAL, params, 0);
+    }
+    return false;
 }
 
 #   endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
