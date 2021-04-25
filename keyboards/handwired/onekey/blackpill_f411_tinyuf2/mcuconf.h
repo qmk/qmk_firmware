34a732c9dfbf99855d0e0153290268a18dab8609
Nick Brassel <nick@tzarc.org>
2021-04-25 13:07:23 +1000

    Update mcuconf.h

--- keyboards/handwired/onekey/blackpill_f411_tinyuf2/mcuconf.h
+++ keyboards/handwired/onekey/blackpill_f411_tinyuf2/mcuconf.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Nick Brassel (tzarc)
+/* Copyright 2021 QMK
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
