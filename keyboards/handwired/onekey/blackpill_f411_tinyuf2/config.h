0d676e344586049c91c9593161bdb9ca1b6f0142
Nick Brassel <nick@tzarc.org>
2021-04-25 13:06:30 +1000

    Update config.h

--- keyboards/handwired/onekey/blackpill_f411_tinyuf2/config.h
+++ keyboards/handwired/onekey/blackpill_f411_tinyuf2/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2019
+/* Copyright 2021 QMK
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
