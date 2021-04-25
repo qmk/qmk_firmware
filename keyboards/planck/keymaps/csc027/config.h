1afc873437a7a717acffe7735bf850e2f8596923
Constantine Chen <csc_dev@protonmail.com>
2021-04-25 13:08:06 -0700

    Added a tap code delay of 10 ms to handle unresponsiveness of mod-tap in RDP sessions.

--- keyboards/planck/keymaps/csc027/config.h
+++ keyboards/planck/keymaps/csc027/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Danny Nguyen <danny@keeb.io>
+Copyright 2021 Constantine Chen @csc027
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,17 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define EE_HANDS
-
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
 
 #define USB_POLLING_INTERVAL_MS 1
 
