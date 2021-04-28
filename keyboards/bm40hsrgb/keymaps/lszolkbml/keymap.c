977aacc89e314e1fc868ded11a0f1313e1b44aec
Dordouille <laurent.sanz@protonmail.com>
2021-04-28 19:02:08 +0200

    add del key with nice color and duplicate some keys on number layers

--- keyboards/bm40hsrgb/keymaps/lszolkbml/keymap.c
+++ keyboards/bm40hsrgb/keymaps/lszolkbml/keymap.c
@@ -315,13 +315,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NAVR] = LAYOUT_planck_mit(
     RGB_TOG,    RGB_VAI, RGB_SAI, RGB_HUI, RGB_MOD,  RGB_SPI, KC_NO,  KC_BTN1, KC_MS_U, KC_BTN2, KC_NO,   KC_RST,
     TG(CODE),   RGB_VAD, RGB_SAD, RGB_HUD, RGB_RMOD, RGB_SPD, KC_NO,  KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_NO,
-    TG(COLORS), KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_INS, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_ENT,
+    TG(COLORS), KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_ENT,
     TG(GAME),   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_BSPC,     KC_NO,   KC_MPRV, KC_VOLU, KC_VOLD, KC_TD(TD_MEDIA)
   ),
   [NUMBER] = LAYOUT_planck_mit(
     KC_GRV,  KC_1,   KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_F1,  KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_MINS,  KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_LSFT, KC_F7,  KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,   KC_NO,   KC_NO,   KC_NO,   KC_ENT,
+    KC_LSFT, KC_F7,  KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,   KC_COMM,  KC_DOT, KC_SLSH, KC_ENT,
     KC_LCTL, KC_NO,  KC_LALT, KC_UNDS,  KC_LSFT,      KC_SPC,      KC_NO,    KC_HOME, KC_PGUP, KC_PGDN, KC_END
   ),
   [GAME] = LAYOUT_planck_mit(
