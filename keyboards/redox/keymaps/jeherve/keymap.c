8cce9d40f72eacdda49c90c12f9fd0acdf6d3567
Jeremy Herve <jeremy@jeremy.hu>
2021-04-28 12:51:06 +0200

    Replace use of deprecated send_unicode_hex_string()

--- keyboards/redox/keymaps/jeherve/keymap.c
+++ keyboards/redox/keymaps/jeherve/keymap.c
@@ -41,7 +41,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SHRUG:
             if (record->event.pressed) {
-                send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF");
+                send_unicode_string("¯\\_(ツ)_/¯");
             }
             return false;
             break;
