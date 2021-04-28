554a96c5964e3fddeea7fadd72faa015466fe54c
rookiebwoy <81021475+rookiebwoy@users.noreply.github.com>
2021-04-28 05:54:26 +0200

    removed coomented code for debugging

--- keyboards/neopad/rev1/keymaps/default/keymap.c
+++ keyboards/neopad/rev1/keymaps/default/keymap.c
@@ -70,23 +70,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // If console is enabled, it will print the matrix position and status of each key pressed
-/*
-#ifdef CONSOLE_ENABLE
-    uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
-#endif
-*/
-    return true;
-}
-
-void keyboard_post_init_user(void) {
-    // Customise these values to desired behaviour
-    // debug_enable = true;
-    // debug_matrix = true;
-    // debug_keyboard = true;
-    // debug_mouse = true;
-}
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {											/* LEFT ENCODER */
