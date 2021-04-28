3fcefa99e4ba7b5ecaf560e812b17622dac9e821
Chester "The Bed-Wetter" Lee Ming Chung <55699197+Sife-ops@users.noreply.github.com>
2021-04-28 10:52:24 +0000

    Update keyboards/converter/periboard_512/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/converter/periboard_512/rules.mk
+++ keyboards/converter/periboard_512/rules.mk
@@ -7,16 +7,16 @@ BOOTLOADER = halfkay
 # Build Options
 #   change yes to no to disable
 #
-AUDIO_ENABLE = no           # Audio output on port C6
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-COMMAND_ENABLE = no         # Commands for debug and configuration
-CONSOLE_ENABLE = no         # Console for debug
-EXTRAKEY_ENABLE = no        # Audio control and System control
-MIDI_ENABLE = no            # MIDI support
 MOUSEKEY_ENABLE = no        # Mouse keys
-NKRO_ENABLE = no            # USB Nkey Rollover
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-UNICODEMAP_ENABLE = yes        # Unicode
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
