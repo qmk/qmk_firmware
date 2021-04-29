affda44feec40b7a3baa06d2712de10f1c9ee221
Jeremy Herve <jeremy@jeremy.hu>
2021-04-29 12:09:04 +0200

    Add GPL header comment

--- keyboards/redox/keymaps/jeherve/config.h
+++ keyboards/redox/keymaps/jeherve/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Jeremy Herve <jeremy@jeremy.hu>
+Copyright 2021 Jeremy Herve <jeremy@jeremy.hu>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
