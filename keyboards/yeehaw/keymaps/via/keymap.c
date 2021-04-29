5b1dc9ee755e126b77e79d3d9fb78eb6379ec69e
Caleb Lightfoot <61706196+Tsquash@users.noreply.github.com>
2021-04-28 22:47:48 -0500

    Update keyboards/yeehaw/keymaps/via/keymap.c
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/yeehaw/keymaps/via/keymap.c
+++ keyboards/yeehaw/keymaps/via/keymap.c
@@ -68,7 +68,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case YEEHAW:
         if (record->event.pressed) {
             SEND_STRING("yeehaw!");
-        } else {
         }
         break;
         
