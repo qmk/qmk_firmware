80145262775e35ab7c4537d06c1e1f96f9929009
Caleb Lightfoot <61706196+Tsquash@users.noreply.github.com>
2021-04-28 22:48:01 -0500

    Update keyboards/yeehaw/keymaps/via/keymap.c
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/yeehaw/keymaps/via/keymap.c
+++ keyboards/yeehaw/keymaps/via/keymap.c
@@ -54,12 +54,6 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         } else {
             tap_code(KC_VOLU);
         }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_DOWN);
-        } else {
-            tap_code(KC_UP);
-        }
     }
 }
 
