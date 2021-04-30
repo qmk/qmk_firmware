2744f389fa852a949371869e460add99f8804e8d
yfuku <fukumoto@gmail.com>
2021-04-30 23:30:01 +0900

    Sorry. Fixed the keymap.

--- keyboards/wings42/keymaps/via/keymap.c
+++ keyboards/wings42/keymaps/via/keymap.c
@@ -21,11 +21,12 @@ enum layer_number {
     _LOWER,
 };
 
+#define KC_G_TAB LGUI_T(KC_TAB)
+#define KC_G_BS LGUI_T(KC_BSPC)
 #define KC_L_SPC LT(_LOWER, KC_SPC)
 #define KC_R_ENT LT(_RAISE, KC_ENT)
-#define KC_G_JA LGUI_T(KC_LANG1)
-#define KC_G_EN LGUI_T(KC_LANG2)
-#define KC_C_BS LCTL_T(KC_BSPC)
+#define KC_S_JA LSFT_T(KC_LANG1)
+#define KC_S_EN LSFT_T(KC_LANG2)
 #define KC_A_DEL ALT_T(KC_DEL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -33,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+---------+--------+---------+--------.   ,--------+---------+--------+---------+--------+--------.
        KC_ESC , KC_Q   , KC_W    , KC_E   , KC_R    , KC_T   ,     KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , KC_MINS,
     //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
-       KC_TAB , KC_A   , KC_S    , KC_D   , KC_F    , KC_G   ,     KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, KC_QUOT,
+      KC_G_TAB, KC_A   , KC_S    , KC_D   , KC_F    , KC_G   ,     KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, KC_G_BS ,
     //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
-       KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSFT,
+       KC_LCTL, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RCTL,
     //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
-                                   KC_G_EN, KC_L_SPC, KC_C_BS,     KC_C_BS, KC_R_ENT, KC_G_JA
+                                  KC_A_DEL, KC_S_EN ,KC_L_SPC,    KC_R_ENT, KC_S_JA , KC_A_DEL
     //                           `+--------+---------+--------'   `--------+---------+--------+'
     ),
 
