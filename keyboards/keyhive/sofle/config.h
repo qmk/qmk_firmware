1fd8ce15821ad0cdd49c2c429e6379bdb6cf464a
Carlos Filoteo <c.filoteo12@gmail.com>
2021-04-29 22:55:28 -0600

    Move RGB configs upwards

--- keyboards/keyhive/sofle/config.h
+++ keyboards/keyhive/sofle/config.h
@@ -2,3 +2,15 @@
 
 #include "config_common.h"
 
+#ifdef RGBLIGHT_ENABLE
+
+//Add RGB underglow
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 74
+#define RGBLED_SPLIT {37,37}
+#define RGBLIGHT_ANIMATIONS
+
+//50% brightness to avoid crashing 
+#define RGBLIGHT_LIMIT_VAL 128 
+
+#endif
