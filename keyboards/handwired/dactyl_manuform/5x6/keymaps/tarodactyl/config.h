1385a1a1fc8873bc2ebff68caf0a6cc3d0952f3d
Will Tao <willtao@live.com>
2021-05-01 00:36:04 -0700

    add tarodactyl files

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/tarodactyl/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/tarodactyl/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 
 //#define MASTER_LEFT
-#define MASTER_RIGHT
-//#define EE_HANDS
+//#define MASTER_RIGHT
+#define EE_HANDS
 // Rows are doubled-up
 
 #ifdef RGBLED_NUM
--- keyboards/handwired/dactyl_manuform/5x6/keymaps/tarodactyl/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/tarodactyl/keymap.c
@@ -4,46 +4,79 @@
 
 
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+#define _COLEMAKDH 1
+#define _ROBOT 2
+#define _SUPER 3
+#define _LOWER 4
 
-#define RAISE MO(_RAISE)
+//#define QWERTY TG(_QWERTY)
+#define COLMKDH TG(_COLEMAKDH)
+#define ROBOT MO(_ROBOT)
+#define SUPER MO(_SUPER)
 #define LOWER MO(_LOWER)
+
+#define CTRLESC MT(MOD_LCTL, KC_ESC)
+#define RUNPRMT RGUI(KC_R)
+#define UNDO RCTL(KC_Z)
+#define REDO RCTL(KC_Y)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
-     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                      KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
+     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , UNDO  ,
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLS,
+     CTRLESC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
+                     KC_MINS,_______,                                                        KC_EQL , KC_PLUS,
+                                       ROBOT,KC_BSPC,                         KC_SPC, SUPER ,
+                                      KC_LALT,KC_DEL,                         KC_ENT , LOWER,
+                                     KC_LGUI,_______,                        _______,RUNPRMT 
+  ),
+
+  [_COLEMAKDH] = LAYOUT_5x6(
+     KC_GESC, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , UNDO  ,
+     KC_TAB , KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,                         KC_J  , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_BSLS,
+     CTRLESC, KC_A  , KC_R  , KC_S  , KC_T  , KC_G  ,                         KC_M  , KC_N  , KC_E  , KC_I  , KC_O  ,KC_QUOT,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_D  , KC_V  ,                         KC_K  , KC_H  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
+                     KC_MINS,_______,                                                        KC_EQL , KC_PLUS,
+                                       ROBOT,KC_BSPC,                         KC_SPC, SUPER ,
+                                     KC_LALT ,KC_DEL,                         KC_ENT , LOWER,
+                                     KC_LGUI,_______,                        _______,RUNPRMT
+  ),
+//------------------------------------------------------------------------------------------------------------------------------ 
+
+  [_ROBOT] = LAYOUT_5x6(
+     KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+     _______,KC_TILD,KC_DLR ,KC_AMPR,KC_PIPE,KC_LBRC,                        KC_RBRC,KC_HOME, KC_UP ,KC_END ,KC_PGUP,_______,
+     _______,KC_AT  ,KC_HASH,KC_QUES,KC_EXLM,KC_LPRN,                        KC_RPRN,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,_______,
+     KC_SLCK,KC_CIRC,KC_PERC,KC_ASTR,KC_UNDS,KC_LCBR,                        KC_RCBR,KC_QUES,_______,_______,_______,_______,
+                     _______,_______,                                                        _______,_______,
+                                      ROBOT ,_______,                        _______,_______,
+                                     _______,_______,                        _______,_______,
+                                     _______,RESET  ,                        _______,_______
+  ),
+
+  [_SUPER] = LAYOUT_5x6(
+
+     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______, REDO  ,
+     _______,_______,_______,_______,KC_PIPE,KC_LBRC,                        KC_RBRC,KC_AMPR,_______,_______,_______,_______,
+     _______,_______,_______,_______,KC_EXLM,KC_LPRN,                        KC_RPRN,_______,_______,_______,_______,_______,
+     _______,_______,_______,_______,KC_UNDS,KC_LCBR,                        KC_RCBR,KC_QUES,_______,_______,_______,KC_PAUS,
+                     _______,_______,                                                        _______,_______,
+                                     _______,_______,                        _______, SUPER ,
+                                     _______,_______,                        _______,_______,
+                                      KC_INS,COLMKDH,                         RESET ,KC_PSCR
+
   ),
 
   [_LOWER] = LAYOUT_5x6(
-
-     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-     _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                             _______,KC_PSCR,            _______, KC_P0,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
-
-  ),
-
-  [_RAISE] = LAYOUT_5x6(
-       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-       _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-       _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-       RGB_TOG,RGB_MOD,RGB_HUI,RGB_SAI,RGB_VAI,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-                                               _______,_______,            KC_EQL ,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______,
-                                               _______,_______,            _______,_______
+     _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+     _______,_______, KC_P7 , KC_P8 , KC_P9 ,_______,                        RGB_VAI,RGB_M_SW,RGB_M_SN,RGB_M_K,_______,KC_MUTE,
+     _______,_______, KC_P4 , KC_P5 , KC_P6 ,_______,                        RGB_HUI,RGB_M_P,RGB_M_B,RGB_M_R,_______,KC_VOLU,
+     _______,_______, KC_P1 , KC_P2 , KC_P3 ,KC_PDOT,                        RGB_SAI,RGB_M_G,RGB_M_T,RGB_M_X,_______,KC_VOLD,
+                     _______, KC_P0 ,                                                        RGB_MOD,RGB_TOG,
+                                     KC_PMNS,KC_PPLS,                        _______,_______,
+                                     KC_PSLS,KC_PAST,                        _______, LOWER ,
+                                     KC_PEQL,KC_NLCK,                        _______,_______
   ),
 };
