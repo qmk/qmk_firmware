78952d514aefb669a1d56dd886a6fcefb61b2ed6
Joe Scotto <contact@joescotto.com>
2021-05-01 15:25:33 -0400

    Add songs and mapping

--- quantum/audio/song_list.h
+++ quantum/audio/song_list.h
@@ -161,15 +161,9 @@
 #ifndef IN_LIKE_FLINT
 #    define IN_LIKE_FLINT
 #endif
-#ifndef IMPERIAL_MARCH
-#    define IMPERIAL_MARCH
-#endif
-#ifndef BASKET_CASE
-#    define BASKET_CASE
-#endif
-#ifndef COIN_SOUND
-#    define COIN_SOUND
-#endif
+#define IMPERIAL_MARCH HD_NOTE(_A4), HD_NOTE(_A4), HD_NOTE(_A4), QD_NOTE(_F4), QD_NOTE(_C5), HD_NOTE(_A4), QD_NOTE(_F4),  QD_NOTE(_C5), WD_NOTE(_A4), HD_NOTE(_E5), HD_NOTE(_E5), HD_NOTE(_E5), QD_NOTE(_F5), QD_NOTE(_C5), HD_NOTE(_A4), QD_NOTE(_F4),  QD_NOTE(_C5), WD_NOTE(_A4)
+#define BASKET_CASE QD_NOTE(_G3), E__NOTE(_F3), E__NOTE(_E3), Q__NOTE(_F3), M__NOTE(_G3, 8+32), Q__NOTE(_REST), Q__NOTE(_B4), Q__NOTE(_C4), Q__NOTE(_B4), E__NOTE(_A4), Q__NOTE(_G3), M__NOTE(_G3, 8+32), Q__NOTE(_REST), Q__NOTE(_B4), Q__NOTE(_C4), Q__NOTE(_B4), E__NOTE(_A4), Q__NOTE(_G3), Q__NOTE(_G3), Q__NOTE(_G3), Q__NOTE(_G3), E__NOTE(_A4), E__NOTE(_C4), QD_NOTE(_B4), HD_NOTE(_B4)
+#define COIN_SOUND E__NOTE(_A5  ), HD_NOTE(_E6  )
 #ifndef ONE_UP_SOUND
 #    define ONE_UP_SOUND
 #endif
@@ -185,15 +179,9 @@
 #ifndef OVERWATCH_THEME
 #    define OVERWATCH_THEME
 #endif
-#ifndef MARIO_THEME
-#    define MARIO_THEME
-#endif
-#ifndef MARIO_GAMEOVER
-#    define MARIO_GAMEOVER
-#endif
-#ifndef MARIO_MUSHROOM
-#    define MARIO_MUSHROOM
-#endif
+#define MARIO_THEME Q__NOTE(_E5), H__NOTE(_E5), H__NOTE(_E5), Q__NOTE(_C5), H__NOTE(_E5), W__NOTE(_G5), Q__NOTE(_G4)
+#define MARIO_GAMEOVER HD_NOTE(_C5 ), HD_NOTE(_G4 ), H__NOTE(_E4 ), H__NOTE(_A4 ), H__NOTE(_B4 ), H__NOTE(_A4 ), H__NOTE(_AF4), H__NOTE(_BF4), H__NOTE(_AF4), WD_NOTE(_G4 )
+#define MARIO_MUSHROOM S__NOTE(_C5 ), S__NOTE(_G4 ), S__NOTE(_C5 ), S__NOTE(_E5 ), S__NOTE(_G5 ), S__NOTE(_C6 ), S__NOTE(_G5 ), S__NOTE(_GS4), S__NOTE(_C5 ), S__NOTE(_DS5), S__NOTE(_GS5), S__NOTE(_DS5), S__NOTE(_GS5), S__NOTE(_C6 ), S__NOTE(_DS6), S__NOTE(_GS6), S__NOTE(_DS6), S__NOTE(_AS4), S__NOTE(_D5 ), S__NOTE(_F5 ), S__NOTE(_AS5), S__NOTE(_D6 ), S__NOTE(_F6 ), S__NOTE(_AS6), S__NOTE(_F6 )
 #ifndef E1M1_DOOM
 #    define E1M1_DOOM
 #endif
@@ -212,12 +200,8 @@
 #ifndef VICTORY_FANFARE_SHORT
 #    define VICTORY_FANFARE_SHORT
 #endif
-#ifndef ALL_STAR
-#    define ALL_STAR
-#endif
-#ifndef RICK_ROLL
-#    define RICK_ROLL
-#endif
+#define ALL_STAR H__NOTE(_AS4), W__NOTE(_FS4), Q__NOTE(_FS4), Q__NOTE(_DS4), H__NOTE(_FS4), W__NOTE(_FS4), Q__NOTE(_FS4), Q__NOTE(_DS4), H__NOTE(_FS4), W__NOTE(_FS4), W__NOTE(_FS4), QD_NOTE(_AS4), H__NOTE(_AS4), W__NOTE(_FS4), Q__NOTE(_FS4), Q__NOTE(_DS4), H__NOTE(_FS4), W__NOTE(_FS4), Q__NOTE(_FS4), Q__NOTE(_DS4), H__NOTE(_FS4), W__NOTE(_FS4), W__NOTE(_FS4), W__NOTE(_AS4), H__NOTE(_REST),W__NOTE(_AS4), W__NOTE(_CS5), H__NOTE(_B4),  H__NOTE(_CS5), H__NOTE(_DS5), W__NOTE(_FS5), H__NOTE(_GS5), W__NOTE(_GS5), H__NOTE(_FS4), H__NOTE(_FS4), H__NOTE(_GS4), H__NOTE(_FS4), H__NOTE(_AS4), W__NOTE(_GS4), W__NOTE(_GS4), W__NOTE(_FS4), W__NOTE(_GS4), H__NOTE(_AS4), WD_NOTE(_DS4)
+#define RICK_ROLL Q__NOTE(_F4), Q__NOTE(_G4), Q__NOTE(_BF4), Q__NOTE(_G4), HD_NOTE(_D5), HD_NOTE(_D5), W__NOTE(_C5), S__NOTE(_REST), Q__NOTE(_F4), Q__NOTE(_G4), Q__NOTE(_BF4), Q__NOTE(_G4), HD_NOTE(_C5), HD_NOTE(_C5), W__NOTE(_BF4), S__NOTE(_REST), Q__NOTE(_F4), Q__NOTE(_G4), Q__NOTE(_BF4), Q__NOTE(_G4), W__NOTE(_BF4), H__NOTE(_C5), H__NOTE(_A4), H__NOTE(_A4), H__NOTE(_G4), H__NOTE(_F4), H__NOTE(_F4), W__NOTE(_C5), W__NOTE(_BF4)
 #ifndef FF_PRELUDE
 #    define FF_PRELUDE
 #endif
