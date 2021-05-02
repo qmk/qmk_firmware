b30254341f1e5dbe29378bcc68deb4b3ec0c75ce
Andrew L <andrewl@a-personal.lan>
2021-05-02 15:31:53 -0400

    add rgb support

--- keyboards/handwired/ortho36/config.h
+++ keyboards/handwired/ortho36/config.h
@@ -31,10 +31,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Planck PCB default pin-out */
 #define MATRIX_ROW_PINS { D1, B4, B5, B2 }
 #define MATRIX_COL_PINS { B3, B1, F7, F6, F5, D0, D4, C6, D7, E6 }
-#define UNUSED_PINS { D3, D2, B0, F4, B6, B7, D5, C7, F1, F0 }
+#define UNUSED_PINS { D2, B0, F4, B6, B7, D5, C7, F1, F0 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
+
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 11
+#define RGBLIGHT_ANIMATIONS
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
