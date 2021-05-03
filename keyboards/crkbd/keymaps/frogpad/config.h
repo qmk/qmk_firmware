2f59562682d22d4af776983a29818b08c1d2143c
konomu <45617438+konomu@users.noreply.github.com>
2021-05-02 17:53:23 -0700

    Add files via upload

--- keyboards/crkbd/keymaps/frogpad/config.h
+++ keyboards/crkbd/keymaps/frogpad/config.h
@@ -1,6 +1,9 @@
 /*
 This is the c configuration file for the keymap
 
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
@@ -25,10 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define SSD1306OLED
-
 #define USE_SERIAL_PD2
-
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
