9627240c73da4f071ca15928c86411d93abe6c11
Henry Hy <henry@studiokestra.ca>
2021-05-04 00:53:00 -0600

    Removed layouts from rules.mk

--- keyboards/studiokestra/cascade/rules.mk
+++ keyboards/studiokestra/cascade/rules.mk
@@ -20,5 +20,3 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-
-LAYOUTS = 60_ansi 60_tsangan_hhkb
