1a8ac2954055a3652068a82d8188b07af7189acb
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-05-05 23:45:44 +0700

    update

--- keyboards/mechlovin/mechlovin9/mechlovin9.h
+++ keyboards/mechlovin/mechlovin9/mechlovin9.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Team Mechlovin
+/* Copyright 2021 Mechlovin' Studio
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
