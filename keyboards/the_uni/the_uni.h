d07cc6529dd462734382a7f052ce52964e6f29e7
Peter Park <peterchpark@gmail.com>
2021-05-06 23:01:24 -0700

    Apply suggestions from code review by ridingqwerty
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/the_uni/the_uni.h
+++ keyboards/the_uni/the_uni.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-#define KEYMAP( \
+#define LAYOUT( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, \
 	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, \
 	            K202, K203, K204, K205, K206, K207  \
