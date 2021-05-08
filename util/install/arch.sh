273f7ff5785e6f658489508960e5f3e12ff323ca
Zach White <skullydazed@gmail.com>
2021-05-08 08:42:16 -0700

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- util/install/arch.sh
+++ util/install/arch.sh
@@ -5,8 +5,8 @@ _qmk_install() {
 
     sudo pacman --needed  --noconfirm -S \
         base-devel clang diffutils gcc git unzip wget zip python-pip \
-	avr-binutils arm-none-eabi-binutils arm-none-eabi-gcc \
-	arm-none-eabi-newlib avrdude dfu-programmer dfu-util
+        avr-binutils arm-none-eabi-binutils arm-none-eabi-gcc \
+        arm-none-eabi-newlib avrdude dfu-programmer dfu-util
     sudo pacman --needed --noconfirm -U https://archive.archlinux.org/packages/a/avr-gcc/avr-gcc-8.3.0-1-x86_64.pkg.tar.xz
     sudo pacman --needed --noconfirm -S avr-libc # Must be installed after the above, or it will bring in the latest avr-gcc instead
 
