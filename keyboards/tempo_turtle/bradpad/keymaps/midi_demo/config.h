28addf86253e049e3ce229883db0e107895db9f6
wxyangf <willsfann@gmail.com>
2021-05-08 20:54:18 -0400

    fixed license again...

--- keyboards/tempo_turtle/bradpad/keymaps/midi_demo/config.h
+++ keyboards/tempo_turtle/bradpad/keymaps/midi_demo/config.h
@@ -14,19 +14,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //taken from: https://github.com/qmk/qmk_firmware/blob/master/keyboards/contra/keymaps/dana/config.h
 
-/*
-Copyright 2021 v3ritas <v3ritas@tempoturtle.com>
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
