2d9bb42611d13531991441bb5c12714a90fe62e5
Yang Hu <hueyhy@gmail.com>
2021-05-08 22:56:05 -0700

    Add keyboards/yanghu/unicorne initial files.

---
+++ keyboards/yanghu/unicorne/config.h
@@ -0,0 +1,80 @@
+/* Copyright 2020 Yang Hu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0204
+#define DEVICE_VER 0x0001
+#define MANUFACTURER yanghu
+#define PRODUCT unicorne
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+#define MATRIX_COL_PINS \
+    { A14, A15, B13, B14, B15, A13, A0, A1, A2, A3, A6, A7 }
+#define MATRIX_ROW_PINS \
+    { B9, B8, A10, A9 }
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+#define ENCODERS_PAD_A \
+    { B12, B0 }
+#define ENCODERS_PAD_B \
+    { B10, B2 }
+
+/* Audio */
+#define MUSIC_MAP
+
+#define AUDIO_PIN A8
+#define AUDIO_PWM_PAL_MODE 1
+#define AUDIO_PWM_DRIVER PWMD1
+#define AUDIO_PWM_CHANNEL 1
+#define AUDIO_STATE_TIMER GPTD4
+
+/* RGB LED */
+#define RGB_DI_PIN B1
+#define RGBLED_NUM 8
+#define RGBLIGHT_ANIMATIONS
+
+#define WS2812_PWM_DRIVER PWMD3
+#define WS2812_PWM_CHANNEL 4
+#define WS2812_PWM_PAL_MODE 2
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM2
+#define WS2812_DMA_CHANNEL 5
+#define WS2812_EXTERNAL_PULLUP
+
+/* OLED display */
+#define I2C_DRIVER I2CD1
+#define I2C1_SCL_BANK GPIOB
+#define I2C1_SCL 6
+#define I2C1_SCL_PAL_MODE 4
+#define I2C1_SDA_BANK GPIOB
+#define I2C1_SDA 7
+#define I2C1_SDA_PAL_MODE 4
+/* Use fast mode. For more details, see:
+https://www.playembedded.org/blog/stm32-i2c-chibios/#7_I2Cv1_configuration_structure */
+#define I2C1_CLOCK_SPEED 400000
+#define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
---
+++ keyboards/yanghu/unicorne/rules.mk
@@ -0,0 +1,18 @@
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = yes       # Mouse keys
+NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+COMMAND_ENABLE = no         # Commands for debug and configuration
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+ENCODER_ENABLE = yes        # Enable rotary encoder support
+AUDIO_ENABLE = yes          # Audio output
+
+OLED_DRIVER_ENABLE = yes
+WS2812_DRIVER = pwm
+AUDIO_DRIVER = pwm_hardware
+
+DEFAULT_FOLDER = yanghu/unicorne/f411
+
+LAYOUTS = ortho_4x12
+
