669e3aee74c2147c0e1d350e6d5b346f0540a893
fauxpark <fauxpark@gmail.com>
2021-05-10 04:41:52 +1000

    KBDMini refactor

--- keyboards/kbdfans/kbdmini/config.h
+++ keyboards/kbdfans/kbdmini/config.h
@@ -7,23 +7,38 @@
 #define PRODUCT_ID      0x2001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    DZTECH
-
 #define PRODUCT         KBDMINI
+
+/* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 13
 
-#define MATRIX_ROW_PINS {B7, E6, F5, F4}
-#define MATRIX_COL_PINS {B3, B2, B1, B0, F1, F0, C6, B6, B5, B4, D7, D6, D4}
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { B7, E6, F5, F4 }
+#define MATRIX_COL_PINS { B3, B2, B1, B0, F1, F0, C6, B6, B5, B4, D7, D6, D4 }
 #define UNUSED_PINS
+
+/* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 3
+
 #ifdef RGB_MATRIX_ENABLE
 #define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 #define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
 #define RGB_MATRIX_KEYPRESSES
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN	
+#define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #define DISABLE_RGB_MATRIX_BAND_SAT
 #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
 #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
@@ -44,3 +59,7 @@
 #define DRIVER_1_LED_TOTAL 52
 #define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
 #endif
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
