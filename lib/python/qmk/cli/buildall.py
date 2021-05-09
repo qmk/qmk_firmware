613ea802480c63b1cf43852868e07201ab6465c5
Nick Brassel <nick@tzarc.org>
2021-05-09 23:12:17 +1000

    Add ability to filter and build only split boards.

--- lib/python/qmk/cli/buildall.py
+++ lib/python/qmk/cli/buildall.py
@@ -11,17 +11,14 @@ from qmk.commands import _find_make
 import qmk.keyboard
 
 
-def _determine_default_folder(keyboard_name):
-    conf = qmk.keyboard.config_h(keyboard_name)
-    print(f"""\
-{keyboard_name}:
-{conf}
-""")
-    return keyboard_name
+def _is_split(keyboard_name):
+    rules_mk = qmk.keyboard.rules_mk(keyboard_name)
+    return True if 'SPLIT_KEYBOARD' in rules_mk and rules_mk['SPLIT_KEYBOARD'] == 'yes' else False
 
 
 @cli.argument('-j', '--parallel', type=int, default=1, help="Set the number of parallel make jobs to run.")
 @cli.argument('-c', '--clean', arg_only=True, action='store_true', help="Remove object files before compiling.")
+@cli.argument('-s', '--split-only', arg_only=True, action='store_true', help="Only builds boards with 'SPLIT_KEYBOARD = yes' specified in their rules.mk.")
 @cli.subcommand('Compile QMK Firmware for all keyboards.', hidden=False if cli.config.user.developer else True)
 def buildall(cli):
     """Compile QMK Firmware against all keyboards.
@@ -34,9 +31,13 @@ def buildall(cli):
     builddir = Path(QMK_FIRMWARE) / '.build'
     makefile = builddir / 'parallel_kb_builds.mk'
 
+    keyboard_list = qmk.keyboard.list_keyboards()
+    if bool(cli.args.split_only):
+        keyboard_list = filter(_is_split, keyboard_list)
+
     builddir.mkdir(parents=True, exist_ok=True)
     with open(makefile, "w") as f:
-        for keyboard_name in qmk.keyboard.list_keyboards():
+        for keyboard_name in keyboard_list:
             keyboard_safe = keyboard_name.replace('/', '_')
             f.write(
                 f"""\
@@ -46,9 +47,9 @@ all: {keyboard_safe}_binary
 	+@$(MAKE) -C "{QMK_FIRMWARE}" -f "{QMK_FIRMWARE}/build_keyboard.mk" KEYBOARD="{keyboard_name}" KEYMAP="default" REQUIRE_PLATFORM_KEY= COLOR=true SILENT=false \\
 		>>"{QMK_FIRMWARE}/.build/build.log.{keyboard_safe}" 2>&1 \\
 		|| cp "{QMK_FIRMWARE}/.build/build.log.{keyboard_safe}" "{QMK_FIRMWARE}/.build/failed.log.{keyboard_safe}"
-	@{{ grep '\[ERRORS\]' "{QMK_FIRMWARE}/.build/build.log.{keyboard_safe}" >/dev/null 2>&1 && printf "Build %-64s \e[1;31m[ERRORS]\e[0m\\n" "{keyboard_safe}:default" ; }} \\
-		|| {{ grep '\[WARNINGS\]' "{QMK_FIRMWARE}/.build/build.log.{keyboard_safe}" >/dev/null 2>&1 && printf "Build %-64s \e[1;33m[WARNINGS]\e[0m\\n" "{keyboard_safe}:default" ; }} \\
-		|| printf "Build %-64s \e[1;32m[OK]\e[0m\\n" "{keyboard_safe}:default"
+	@{{ grep '\[ERRORS\]' "{QMK_FIRMWARE}/.build/build.log.{keyboard_safe}" >/dev/null 2>&1 && printf "Build %-64s \e[1;31m[ERRORS]\e[0m\\n" "{keyboard_name}:default" ; }} \\
+		|| {{ grep '\[WARNINGS\]' "{QMK_FIRMWARE}/.build/build.log.{keyboard_safe}" >/dev/null 2>&1 && printf "Build %-64s \e[1;33m[WARNINGS]\e[0m\\n" "{keyboard_name}:default" ; }} \\
+		|| printf "Build %-64s \e[1;32m[OK]\e[0m\\n" "{keyboard_name}:default"
 	@rm -f "{QMK_FIRMWARE}/.build/build.log.{keyboard_safe}" || true
 
 """
