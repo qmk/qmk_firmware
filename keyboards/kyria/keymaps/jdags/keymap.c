8a88eba9d3fe0216804be466dcc05e0c445f10fd
j-dags <jon.dagdagan@gmail.com>
2021-05-10 22:46:32 -0500

    added hotkey to toggle audio devices

--- keyboards/kyria/keymaps/jdags/keymap.c
+++ keyboards/kyria/keymaps/jdags/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
       KC_TAB, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, XXXXXXX, KC_LGUI, KC_LGUI, XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-              XXXXXXX, KC_LSFT, MO(NAV), KC_SPC, KC_ENT, KC_BSPC, KC_LSFT, MO(SYMBOLS), KC_LALT, XXXXXXX
+              XXXXXXX, KC_LSFT, MO(NAV), KC_SPC, KC_ENT, KC_BSPC, KC_LSFT, MO(SYMBOLS), KC_LALT, LCTL(LSFT(LALT(KC_F1)))
     ),
 /*
  * NAV Layer: Nav, Numpa
