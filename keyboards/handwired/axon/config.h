bd17ce7b2d8fae025417233fedb444543b2538b8
RobinL <Robin.Liu.831@gmail.com>
2021-05-11 19:16:18 -0400

    Via support

--- keyboards/handwired/axon/config.h
+++ keyboards/handwired/axon/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x524C // "RL" - Robin Liu
+#define PRODUCT_ID      0x0011 // version format: v###.#. e.g. 0011 is v1.1
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    RobinL
 #define PRODUCT         Axon
