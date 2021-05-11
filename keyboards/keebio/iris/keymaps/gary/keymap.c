38d8d5445ebd60932d602184b1f27a112ac79f71
Zach White <skullydazed@gmail.com>
2021-05-11 10:10:31 -0700

    Remove KEYMAP and LAYOUT_kc (#12160)
    * alias KEYMAP to LAYOUT
    
    * remove KEYMAP and LAYOUT_kc

--- keyboards/keebio/iris/keymaps/gary/keymap.c
+++ keyboards/keebio/iris/keymaps/gary/keymap.c
@@ -2,46 +2,46 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT_kc(
+  [_QWERTY] = LAYOUT(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
+     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,DEL ,
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_DEL ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     RASE, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
+     KC_RASE, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
   //|----+----+----+----   +----+----+----.    ,----|----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,NEXT,     FULL , N  , M  ,COMM,DOT ,SLSH,SFTENT,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_NEXT,     KC_FULL , KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_SFTENT,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LGUI,LOWR,SPC ,        GARY, ENT,LALT
+                       KC_LGUI,KC_LOWR,KC_SPC ,        KC_GARY, KC_ENT,KC_LALT
   //                  `----+----+----'        `----+----+----'
   ),
 
 
-  [_LOWER] = LAYOUT_kc(
+  [_LOWER] = LAYOUT(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-    GRV ,EXLM, AT ,HASH,DLR ,PERC,                CIRC,AMPR,ASTR,LPRN,RPRN,BSPC,
+    KC_GRV ,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     CLTB,    ,CNTR,UPLF,UPRG,    ,                   ,    ,PLUS,LBRC,RBRC,OPASS,
+     KC_CLTB,_______,KC_CNTR,KC_UPLF,KC_UPRG,_______,               _______,_______,KC_PLUS,KC_LBRC,KC_RBRC,KC_OPASS,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,LHLF,RHLF,DNLF,DNRG,    ,                   ,    ,MINS,    ,    ,PIPE,
+     _______,KC_LHLF,KC_RHLF,KC_DNLF,KC_DNRG,_______,               _______,_______,KC_MINS,_______,_______,KC_PIPE,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|`
-         ,    ,    ,CTLC,    ,    ,    ,         ,    ,    ,    ,EQL ,    ,UNDS ,
+     _______,_______,_______,KC_CTLC,_______,_______,_______,     _______,_______,_______,_______,KC_EQL ,_______,KC_UNDS ,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,    ,DEL ,        BSPC ,    ,
+                       _______,_______,KC_DEL ,        KC_BSPC ,_______, _______
   //                  `----+----+----'        `----+----+----'
   ),
 
-  [_RAISE] = LAYOUT_kc(
+  [_RAISE] = LAYOUT(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     TILD, F1 , F2 , F3 ,SHOT, F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
+     KC_TILD, KC_F1 , KC_F2 , KC_F3 ,KC_SHOT, KC_F5 ,                KC_F6 , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,MPRV,MPLY,MNXT,    ,    ,                   ,PGUP, UP ,PGDN,    ,    ,
+     _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,               _______,KC_PGUP, KC_UP ,KC_PGDN,_______,_______,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,    ,VOLD,VOLU,MUTE,    ,                   ,LEFT,DOWN,RGHT,    ,    ,
+     _______,_______,KC_VOLD,KC_VOLU,KC_MUTE,_______,               _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,    ,      RST,    ,    ,    ,    ,    ,    ,
+     _______,_______,_______,_______,_______,_______,_______,      KC_RST,_______,_______,_______,_______,_______,_______,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                            ,LALT,    ,             ,    ,
+                        _______,KC_LALT,_______,         _______,_______, _______
   //                  `----+----+----'        `----+----+----'
   ),
 };
