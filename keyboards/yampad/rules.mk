baacc03d0c6b4444e77df577d19c3a8d151528b5
Richard <davey.richard@gmail.com>
2021-05-11 20:45:56 +0100

    corrected the bootloader in rules.mk

--- keyboards/yampad/rules.mk
+++ keyboards/yampad/rules.mk
@@ -2,7 +2,14 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = atmel-dfu
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
