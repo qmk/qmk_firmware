8f6ce3398949192a4f6b701eeebf0cfbfef510c8
Steve Purcell <steve@sanityinc.com>
2021-05-11 15:10:45 +1200

    Use upstreamed poetry2nix overrides for hid and pyusb

--- shell.nix
+++ shell.nix
@@ -1,39 +1,17 @@
 { avr ? true, arm ? true, teensy ? true }:
 let
   # We specify sources via Niv: use "niv update nixpkgs" to update nixpkgs, for example.
-  sources = import ./nix/sources.nix {};
-  pkgs = import sources.nixpkgs {};
+  sources = import ./nix/sources.nix { };
+  pkgs = import sources.nixpkgs { };
+
+  poetry2nix = pkgs.callPackage (import sources.poetry2nix) { };
 
   # Builds the python env based on nix/pyproject.toml and
   # nix/poetry.lock Use the "poetry update --lock", "poetry add
   # --lock" etc. in the nix folder to adjust the contents of those
   # files if the requirements*.txt files change
-  pythonEnv = pkgs.poetry2nix.mkPoetryEnv {
+  pythonEnv = poetry2nix.mkPoetryEnv {
     projectDir = ./nix;
-    overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-      hid = super.hid.overridePythonAttrs (old: {
-        postPatch =
-          ''
-            found=
-            for name in libhidapi-hidraw libhidapi-libusb libhidapi-iohidmanager libhidapi; do
-              full_path=${pkgs.hidapi.out}/lib/$name${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}
-              if test -f $full_path; then
-                found=t
-                sed -i -e "s|'$name\..*'|'$full_path'|" hid/__init__.py
-              fi
-            done
-            test -n "$found" || { echo "ERROR: No known libraries found in ${pkgs.hidapi.out}/lib, please update/fix this build expression."; exit 1; }
-          '';
-      });
-      pyusb = super.pyusb.overridePythonAttrs (old: {
-        postPatch =
-          ''
-            libusb=${pkgs.libusb1.out}/lib/libusb-1.0${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}
-            test -f $libusb || { echo "ERROR: $libusb doesn't exist, please update/fix this build expression."; exit 1; }
-            sed -i -e "s|find_library=None|find_library=lambda _:\"$libusb\"|" usb/backend/libusb1.py
-          '';
-      });
-    });
   };
 in
 
