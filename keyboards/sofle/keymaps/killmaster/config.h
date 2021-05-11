eff22d4b8e4bc068683f0c66aeef070c95b67864
Carlos Martins <carlosmartins8@gmail.com>
2021-05-11 04:07:14 +0100

    rgb matrix working properly on the sofle rgb

---
+++ keyboards/sofle/keymaps/killmaster/config.h
@@ -0,0 +1,43 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define RGB_DI_PIN D3
+
+#ifdef RGB_MATRIX_ENABLE
+
+#define RGBLED_NUM 72
+#define DRIVER_LED_TOTAL RGBLED_NUM
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#define RGB_MATRIX_HUE_STEP 8
+#define RGB_MATRIX_SAT_STEP 8
+#define RGB_MATRIX_VAL_STEP 8
+#define RGB_MATRIX_SPD_STEP 10
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_SPLIT {36,36}
+
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+#define RGBLIGHT_SPLIT
+#define RGBLED_NUM 70
+#define RGB_SPLIT {36,36}
+#define RGBLIGHT_LIMIT_VAL 120
+//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+// /*== all animations enable ==*/
+//   #define RGBLIGHT_ANIMATIONS
+// /*== or choose animations ==*/
+//   #define RGBLIGHT_EFFECT_BREATHING
+   //#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+   //#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//   #define RGBLIGHT_EFFECT_SNAKE
+//   #define RGBLIGHT_EFFECT_KNIGHT
+//   #define RGBLIGHT_EFFECT_CHRISTMAS
+   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+   #define RGBLIGHT_EFFECT_RGB_TEST
+//   #define RGBLIGHT_EFFECT_ALTERNATING
+#endif
--- keyboards/sofle/rev1/rev1.c
+++ keyboards/sofle/rev1/rev1.c
@@ -1,1 +1,85 @@
 #include "sofle.h"
+
+#ifdef RGB_MATRIX_ENABLE
+#define RLO 35
+#define LLI(x) (x)
+#define LLP(x,y) {(x),(y)}
+#define RLI(x) (RLO+(x))
+#define RLP(x,y) {(224-(x)),((y))}
+/* #define LED_FLAG_NONE 0
+#define LED_FLAG_UNDERGLOW 2
+#define LED_FLAG_KEYLIGHT 4
+#define LED_FLAG_INDICATOR 8
+ */
+  // Physical Layout
+  // Columns
+  // 0  1  2  3  4  5  6  7  8  9  10 11 12 13
+  //                                           ROWS
+  // 12 13 22 23 32 33       33 32 23 22 13 12  0
+  //    02    03    04       04    03    02
+  // 11 14 21 24 31 34       34 31 24 21 14 11  1
+  //                01       01
+  // 10 15 20 25 30 35       35 30 25 20 15 10  2
+  //
+  // 09 16 19 26 29 36       36 29 26 19 16 09  3
+  //
+  //     08 17 18 27 28     28 27 18 17 08      4
+  //    07    06    05       05    06    07
+
+led_config_t g_led_config = {
+    {
+        {  LLI(11),  LLI(12),  LLI(21),  LLI(22),  LLI(31),  LLI(32) },
+        {  LLI(10),  LLI(13),  LLI(20),  LLI(23),  LLI(30),  LLI(33) },
+        {  LLI(9),  LLI(14),  LLI(19),  LLI(24),  LLI(29),  LLI(34)},
+        {  LLI(8),  LLI(15),  LLI(18),  LLI(25),  LLI(28),  LLI(35)},
+        {  NO_LED,   LLI(7),  LLI(16),  LLI(17),  LLI(26),  LLI(27) },
+        {  RLI(11),  RLI(12),  RLI(21),  RLI(22),  RLI(31),  RLI(32)},
+        {  RLI(10),  RLI(13),  RLI(20),  RLI(23),  RLI(30),  RLI(33)},
+        {  RLI(9),  RLI(14),  RLI(19),  RLI(24),  RLI(29),  RLI(34)},
+        {  RLI(8),  RLI(15),  RLI(18),  RLI(25),  RLI(28),  RLI(35)},
+        {  NO_LED,   RLI(7),  RLI(16),  RLI(17),  RLI(26),  RLI(27) }
+    },
+    {
+        // Left side underglow
+        LLP(96, 40), LLP(16, 20), LLP(48, 10), LLP(80, 18), LLP(88, 60), LLP(56, 57), LLP(24,60),
+        // Left side Matrix
+        LLP(32, 57), LLP( 0, 48), LLP( 0, 36), LLP( 0, 24), LLP( 0, 12),
+        LLP(16, 12), LLP(16, 24), LLP(16, 36), LLP(16, 48), LLP(48, 55),
+        LLP(64, 57), LLP(32, 45), LLP(32, 33), LLP(32, 21), LLP(32,  9),
+        LLP(48,  7), LLP(48, 19), LLP(48, 31), LLP(48, 43), LLP(80, 59),
+        LLP(96, 64), LLP(64, 45), LLP(64, 33), LLP(64, 21), LLP(64,  9),
+        LLP(80, 10), LLP(80, 22), LLP(80, 34), LLP(80, 47),
+
+        // Right side underglow
+        RLP(24, 60), RLP(56, 57), RLP(88, 60), RLP(80, 18), RLP(48, 10), RLP(16, 20), RLP(96,40),
+        // Right side Matrix
+        RLP( 0, 12), RLP( 0, 24), RLP( 0, 36), RLP( 0, 48), RLP(32, 57),
+        RLP(48, 55), RLP(16, 48), RLP(16, 36), RLP(16, 24), RLP(16, 12),
+        RLP(32,  9), RLP(32, 21), RLP(32, 33), RLP(32, 45), RLP(64, 57),
+        RLP(80, 59), RLP(48, 43), RLP(48, 31), RLP(48, 19), RLP(48,  7),
+        RLP(64,  9), RLP(64, 21), RLP(64, 33), RLP(64, 45), RLP(96, 64),
+        RLP(80, 47), RLP(80, 34), RLP(80, 22), RLP(80, 10)
+    },
+    {
+        LED_FLAG_NONE, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_NONE, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW, LED_FLAG_UNDERGLOW,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT
+    }
+};
+#endif
+
+void matrix_init_kb(void) {
+    matrix_init_user();
+    //rgb_matrix_mode(RGB_MATRIX_CYCLE_LEFT_RIGHT);
+}
