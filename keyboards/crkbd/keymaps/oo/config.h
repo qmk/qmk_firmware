ec658c568baea5783f3dbc218e03efa65c201545
Oliver Ocean <github@oliverocean.co>
2021-05-13 17:22:47 -0500

    Remove extraneous defines

--- keyboards/crkbd/keymaps/oo/config.h
+++ keyboards/crkbd/keymaps/oo/config.h
@@ -20,29 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-
-#define USE_SERIAL_PD2
-
 #define SPLIT_USB_DETECT
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
-
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
