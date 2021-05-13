f1a4502a5f107c000ef78825a34f49a004e47b92
Felix Jen <fjen@felixjen.com>
2021-05-13 14:44:11 -0500

    Update keyboards/fjlabs/midway60/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/fjlabs/midway60/rules.mk
+++ keyboards/fjlabs/midway60/rules.mk
@@ -23,6 +23,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-KEYBOARD_LOCK_ENABLE = yes
 
 LAYOUTS = all
