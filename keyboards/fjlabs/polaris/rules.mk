6d4c4437ac6bfe9e2dcb7ad362d72cd7c06f30b6
Felix Jen <fjen@felixjen.com>
2021-05-13 14:43:02 -0500

    Update keyboards/fjlabs/polaris/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/fjlabs/polaris/rules.mk
+++ keyboards/fjlabs/polaris/rules.mk
@@ -23,4 +23,3 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-KEYBOARD_LOCK_ENABLE = yes
