qmk/qmk_firmware

commit 9633f9a60f9b987c09edf28517a093a640a25d03

[Keyboard] x-bows nature: fix led order and colors (#12871)

keyboards/xbows/nature/nature.c
@@ -17,99 +17,99 @@
  #ifdef RGB_MATRIX_ENABLE
  const is31_led g_is31_leds[DRIVER_LED_TOTAL] = {
 
- 	{0, C3_3,  C2_3, C1_3},   // L01
- 	{0, C3_4,  C2_4, C1_4},   // L02
- 	{0, C3_5,  C2_5, C1_5},   // L03
- 	{0, C3_6,  C2_6, C1_6},   // L04
- 	{0, C3_7,  C2_7, C1_7},   // L05
- 	{0, C3_8,  C2_8, C1_8},   // L06
- 	{1, C3_3,  C2_3, C1_3},   // L07
- 	{1, C3_4,  C2_4, C1_4},   // L08
- 	{1, C3_5,  C2_5, C1_5},   // L09
- 	{1, C3_6,  C2_6, C1_6},   // L10
- 	{1, C3_7,  C2_7, C1_7},   // L11
- 	{1, C3_8,  C2_8, C1_8},   // L12
- 	{2, C3_3,  C2_3, C1_3},   // L13
- 	{2, C3_4,  C2_4, C1_4},   // L14
- 	{2, C3_5,  C2_5, C1_5},   // L15
+	{0, C1_3, C2_3, C3_3},   // L01
+	{0, C1_4, C2_4, C3_4},   // L02
+	{0, C1_5, C2_5, C3_5},   // L03
+	{0, C1_6, C2_6, C3_6},   // L04
+	{0, C1_7, C2_7, C3_7},   // L05
+	{0, C1_8, C2_8, C3_8},   // L06
+	{1, C1_3, C2_3, C3_3},   // L07
+	{1, C1_4, C2_4, C3_4},   // L08
+	{1, C1_5, C2_5, C3_5},   // L09
+	{1, C1_6, C2_6, C3_6},   // L10
+	{1, C1_7, C2_7, C3_7},   // L11
+	{1, C1_8, C2_8, C3_8},   // L12
+	{2, C1_3, C2_3, C3_3},   // L13
+	{2, C1_4, C2_4, C3_4},   // L14
+	{2, C1_5, C2_5, C3_5},   // L15
 
- 	{0, C4_1,  C5_1, C6_1},   // L16
- 	{0, C4_2,  C5_2, C6_2},   // L17
- 	{0, C4_3,  C5_3, C6_3},   // L18
- 	{0, C4_6,  C5_6, C6_6},   // L19
- 	{0, C4_7,  C5_7, C6_7},   // L20
- 	{0, C4_8,  C5_8, C6_8},   // L21
- 	{1, C4_1,  C5_1, C6_1},   // L87
- 	{1, C4_2,  C5_2, C6_2},   // L22
- 	{1, C4_3,  C5_3, C6_3},   // L23
- 	{1, C4_6,  C5_6, C6_6},   // L24
- 	{1, C4_7,  C5_7, C6_7},   // L25
- 	{1, C4_8,  C5_8, C6_8},   // L26
- 	{2, C4_1,  C5_1, C6_1},   // L27
- 	{2, C4_2,  C5_2, C6_2},   // L28
- 	{2, C4_3,  C5_3, C6_3},   // L29
+	{0, C6_1, C5_1, C4_1},   // L16
+	{0, C6_2, C5_2, C4_2},   // L17
+	{0, C6_3, C5_3, C4_3},   // L18
+	{0, C6_6, C5_6, C4_6},   // L19
+	{0, C6_7, C5_7, C4_7},   // L20
+	{0, C6_8, C5_8, C4_8},   // L21
+	{1, C6_1, C5_1, C4_1},   // L87
+	{1, C6_2, C5_2, C4_2},   // L22
+	{1, C6_3, C5_3, C4_3},   // L23
+	{1, C6_6, C5_6, C4_6},   // L24
+	{1, C6_7, C5_7, C4_7},   // L25
+	{1, C6_8, C5_8, C4_8},   // L26
+	{2, C6_1, C5_1, C4_1},   // L27
+	{2, C6_2, C5_2, C4_2},   // L28
+	{2, C6_3, C5_3, C4_3},   // L29
 
- 	{0, C7_1,  C8_1, C9_1},   // L30
- 	{0, C7_2,  C8_2, C9_2},   // L31
- 	{0, C7_3,  C8_3, C9_3},   // L32
- 	{0, C7_4,  C8_4, C9_4},   // L33
- 	{0, C7_5,  C8_5, C9_5},   // L34
- 	{0, C7_6,  C8_6, C9_6},   // L35
- 	{2, C4_8,  C5_8, C6_8},   // L44
- 	{1, C7_1,  C8_1, C9_1},   // L36
- 	{1, C7_2,  C8_2, C9_2},   // L37
- 	{1, C7_3,  C8_3, C9_3},   // L38
- 	{1, C7_4,  C8_4, C9_4},   // L39
- 	{1, C7_5,  C8_5, C9_5},   // L40
- 	{1, C7_6,  C8_6, C9_6},   // L41
- 	{2, C4_6,  C5_6, C6_6},   // L42
- 	{2, C4_7,  C5_7, C6_7},   // L43
+	{0, C9_1, C8_1, C7_1},   // L30
+	{0, C9_2, C8_2, C7_2},   // L31
+	{0, C9_3, C8_3, C7_3},   // L32
+	{0, C9_4, C8_4, C7_4},   // L33
+	{0, C9_5, C8_5, C7_5},   // L34
+	{0, C9_6, C8_6, C7_6},   // L35
+	{1, C9_1, C8_1, C7_1},   // L36
+	{1, C9_2, C8_2, C7_2},   // L37
+	{1, C9_3, C8_3, C7_3},   // L38
+	{1, C9_4, C8_4, C7_4},   // L39
+	{1, C9_5, C8_5, C7_5},   // L40
+	{1, C9_6, C8_6, C7_6},   // L41
+	{2, C6_6, C5_6, C4_6},   // L42
+	{2, C6_7, C5_7, C4_7},   // L43
+	{2, C6_8, C5_8, C4_8},   // L44
 
 
- 	{0, C3_11,  C2_11, C1_11},   // L45
- 	{0, C3_12,  C2_12, C1_12},   // L46
- 	{0, C3_13,  C2_13, C1_13},   // L47
- 	{0, C3_14,  C2_14, C1_14},   // L48
- 	{0, C3_15,  C2_15, C1_15},   // L49
- 	{0, C3_16,  C2_16, C1_16},   // L50
- 	{1, C7_9,   C8_9,  C9_9},    // L51
- 	{1, C3_11,  C2_11, C1_11},   // L52
- 	{1, C3_12,  C2_12, C1_12},   // L53
- 	{1, C3_13,  C2_13, C1_13},   // L54
- 	{1, C3_14,  C2_14, C1_14},   // L55
- 	{1, C3_15,  C2_15, C1_15},   // L56
- 	{1, C3_16,  C2_16, C1_16},   // L57
- 	{2, C7_4,   C8_4,  C9_4},    // L58
- 	{2, C7_5,   C8_5,  C9_5},    // L59
+	{0, C1_11, C2_11, C3_11},   // L45
+	{0, C1_12, C2_12, C3_12},   // L46
+	{0, C1_13, C2_13, C3_13},   // L47
+	{0, C1_14, C2_14, C3_14},   // L48
+	{0, C1_15, C2_15, C3_15},   // L49
+	{0, C1_16, C2_16, C3_16},   // L50
+	{1, C9_9, C8_9, C7_9},      // L51
+	{1, C1_11, C2_11, C3_11},   // L52
+	{1, C1_12, C2_12, C3_12},   // L53
+	{1, C1_13, C2_13, C3_13},   // L54
+	{1, C1_14, C2_14, C3_14},   // L55
+	{1, C1_15, C2_15, C3_15},   // L56
+	{1, C1_16, C2_16, C3_16},   // L57
+	{2, C9_4, C8_4, C7_4},      // L58
+	{2, C9_5, C8_5, C7_5},      // L59
 
- 	{0, C4_9,  C5_9, C6_9},      // L60
- 	{0, C4_10,  C5_10, C6_10},   // L61
- 	{0, C4_11,  C5_11, C6_11},   // L62
- 	{0, C4_14,  C5_14, C6_14},   // L63
- 	{0, C4_15,  C5_15, C6_15},   // L64
- 	{0, C4_16,  C5_16, C6_16},   // L65
- 	{1, C7_10,  C8_10, C9_10},   // L66
- 	{1, C4_9,  C5_9, C6_9},      // L67
- 	{1, C4_10,  C5_10, C6_10},   // L68
- 	{1, C4_11,  C5_11, C6_11},   // L69
- 	{1, C4_14,  C5_14, C6_14},   // L70
- 	{1, C4_15,  C5_15, C6_15},   // L71
- 	{1, C4_16,  C5_16, C6_16},   // L72
- 	{2, C7_6,   C8_6,  C9_6},    // L73
+	{0, C6_9, C5_9, C4_9},      // L60
+	{0, C6_10, C5_10, C4_10},   // L61
+	{0, C6_11, C5_11, C4_11},   // L62
+	{0, C6_14, C5_14, C4_14},   // L63
+	{0, C6_15, C5_15, C4_15},   // L64
+	{0, C6_16, C5_16, C4_16},   // L65
+	{1, C9_10, C8_10, C7_10},   // L66
+	{1, C6_9, C5_9, C4_9},      // L67
+	{1, C6_10, C5_10, C4_10},   // L68
+	{1, C6_11, C5_11, C4_11},   // L69
+	{1, C6_14, C5_14, C4_14},   // L70
+	{1, C6_15, C5_15, C4_15},   // L71
+	{1, C6_16, C5_16, C4_16},   // L72
+	{2, C9_6, C8_6, C7_6},      // L73
 
- 	{0, C7_9,   C8_9, C9_9},     // L74
- 	{0, C7_10,  C8_10, C9_10},   // L75
- 	{0, C7_11,  C8_11, C9_11},   // L76
- 	{0, C7_12,  C8_12, C9_12},   // L77
- 	{0, C7_13,  C8_13, C9_13},   // L78
- 	{0, C7_14,  C8_14, C9_14},   // L79
- 	{1, C7_11,  C8_11, C9_11},   // L80
- 	{1, C7_12,  C8_12, C9_12},   // L81
- 	{1, C7_13,  C8_13, C9_13},   // L82
- 	{1, C7_14,  C8_14, C9_14},   // L83
- 	{2, C3_6,  C2_6, C1_6},      // L84
- 	{2, C3_7,  C2_7, C1_7},      // L85
- 	{2, C3_8,  C2_8, C1_8},      // L86
+	{0, C9_9, C8_9, C7_9},      // L74
+	{0, C9_10, C8_10, C7_10},   // L75
+	{0, C9_11, C8_11, C7_11},   // L76
+	{0, C9_12, C8_12, C7_12},   // L77
+	{0, C9_13, C8_13, C7_13},   // L78
+	{0, C9_14, C8_14, C7_14},   // L79
+	{1, C9_11, C8_11, C7_11},   // L80
+	{1, C9_12, C8_12, C7_12},   // L81
+	{1, C9_13, C8_13, C7_13},   // L82
+	{1, C9_14, C8_14, C7_14},   // L83
+	{2, C1_6, C2_6, C3_6},      // L84
+	{2, C1_7, C2_7, C3_7},      // L85
+	{2, C1_8, C2_8, C3_8},      // L86
  };
 
  led_config_t g_led_config = { {

commit 8ee1f1a50f552a378edf362647b880e77fdb1ade

revert error introduced in PR #12871

keyboards/xbows/nature/nature.c
@@ -55,6 +55,7 @@
 	{0, C9_4, C8_4, C7_4},   // L33
 	{0, C9_5, C8_5, C7_5},   // L34
 	{0, C9_6, C8_6, C7_6},   // L35
+	{2, C6_8, C5_8, C4_8},   // L44
 	{1, C9_1, C8_1, C7_1},   // L36
 	{1, C9_2, C8_2, C7_2},   // L37
 	{1, C9_3, C8_3, C7_3},   // L38
@@ -63,7 +64,6 @@
 	{1, C9_6, C8_6, C7_6},   // L41
 	{2, C6_6, C5_6, C4_6},   // L42
 	{2, C6_7, C5_7, C4_7},   // L43
-	{2, C6_8, C5_8, C4_8},   // L44
 
 
 	{0, C1_11, C2_11, C3_11},   // L45