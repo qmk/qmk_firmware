a3317bf8b6b3c4ed997461cc635e7025a7008aa1
Carlos Martins <carlosmartins8@gmail.com>
2021-05-13 18:36:20 +0100

    Some of the proposed changes. Added license; Using tap_code_delay; Deleted encoder.c since it wasn't doing anything;

--- keyboards/sofle/keymaps/killmaster/keymap.c
+++ keyboards/sofle/keymaps/killmaster/keymap.c
@@ -1,5 +1,20 @@
+/* Copyright 2021 Carlos Martins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
-#define MEDIA_KEY_DELAY 2
 
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
@@ -416,17 +431,13 @@ void encoder_update_user(int8_t index, bool clockwise) {
                 tap_code(KC_MS_WH_DOWN);
             }
         } else if (index == 0) { /* Second encoder */
-            uint16_t held_keycode_timer = timer_read();
             uint16_t mapped_code        = 0;
             if (clockwise) {
                 mapped_code = KC_VOLD;
             } else {
                 mapped_code = KC_VOLU;
             }
-            register_code(mapped_code);
-            while (timer_elapsed(held_keycode_timer) < MEDIA_KEY_DELAY)
-                ; /* no-op */
-            unregister_code(mapped_code);
+            tap_code_delay(mapped_code, MEDIA_KEY_DELAY);
         }
     }
 }
