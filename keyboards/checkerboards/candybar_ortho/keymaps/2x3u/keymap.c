478dc547220d4782432dbffec1883989a0af4c42
npspears <40127181+npspears@users.noreply.github.com>
2021-05-13 00:03:38 -0500

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/checkerboards/candybar_ortho/keymaps/2x3u/keymap.c
+++ keyboards/checkerboards/candybar_ortho/keymaps/2x3u/keymap.c
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Grv  | L-OS | LAlt |  RGB |       Lay1-TT      |  NO |  Dot  |   /  |     Spac(Lay2)     | RALT |RCTRL |C.A.D.| Caps |
  * '----------------------------------------------------------------------------------------------------------------------'
  */
-[0] = LAYOUT_2x3u( \
-  KC_HOME, KC_TAB,          KC_Q,      KC_W,     KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_NLCK,  KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,      KC_BSPC,  \
-  KC_PGUP, CTL_T(KC_ESC),   KC_A,      KC_S,     KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,  KC_H,     KC_J,   KC_K,     KC_L,     KC_SCLN,   KC_QUOT,  \
-  KC_PGDN, KC_LSFT,         KC_Z,      KC_X,     KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_PMNS,  KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,   KC_ENT,   \
-  KC_GRV,  KC_LGUI,         KC_LALT,   RGB_TOG,           TT(1),            KC_P0,   KC_PDOT, KC_BSLS,        LT(2, KC_SPC),       KC_RALT,  KC_RCTRL, CALTDEL,   KC_CAPS   \
+[0] = LAYOUT_2x3u(
+  KC_HOME, KC_TAB,          KC_Q,      KC_W,     KC_E,    KC_R,    KC_T,    KC_P7,   KC_P8,   KC_P9,   KC_NLCK,  KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,      KC_BSPC,
+  KC_PGUP, CTL_T(KC_ESC),   KC_A,      KC_S,     KC_D,    KC_F,    KC_G,    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,  KC_H,     KC_J,   KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
+  KC_PGDN, KC_LSFT,         KC_Z,      KC_X,     KC_C,    KC_V,    KC_B,    KC_P1,   KC_P2,   KC_P3,   KC_PMNS,  KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,   KC_ENT,
+  KC_GRV,  KC_LGUI,         KC_LALT,   RGB_TOG,           TT(1),            KC_P0,   KC_PDOT, KC_BSLS,        LT(2, KC_SPC),       KC_RALT,  KC_RCTRL, CALTDEL,   KC_CAPS
 ),
 
 /* 1
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |C.A.D.| TMGR |      |      |                    |      |      |      |                    |      |  SPC |   |  |   `  |
  * '----------------------------------------------------------------------------------------------------------------------'
  */
-[1] = LAYOUT_2x3u( \
-  KC_TILD, _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_BSPC, \
-  KC_SLSH, _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, _______, _______, _______, _______,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0, KC_BSLS, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_PPLS,  KC_EQL, _______, _______, _______, \
-  CALTDEL,  TSKMGR, _______, _______, _______, _______, _______, _______, _______, _______, KC_SPC, KC_NUBS,   KC_GRV  \
+[1] = LAYOUT_2x3u(
+  KC_TILD, _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_BSPC,
+  KC_SLSH, _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, _______, _______, _______, _______,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0, KC_BSLS,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_PPLS,  KC_EQL, _______, _______, _______,
+  CALTDEL,  TSKMGR, _______, _______, _______, _______, _______, _______, _______, _______, KC_SPC, KC_NUBS,   KC_GRV
 ),
 
 /* 2
@@ -74,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |RESET | RTOG |      |      |                    |      |      |      |                    |      |      | RMOD |RRMOD |
  * '----------------------------------------------------------------------------------------------------------------------'
  */
-[2] = LAYOUT_2x3u( \
-  RGB_HUI,  RGB_SAI, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_UNDS, _______, KC_LBRC, KC_RBRC, RGB_VAI, \
-  RGB_HUD,  RGB_SAD, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_MINS, _______, KC_LCBR, KC_RCBR, RGB_VAD, \
-  _______,  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, _______, _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, \
-  RESET, RGB_TOG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_RMOD \
+[2] = LAYOUT_2x3u(
+  RGB_HUI,  RGB_SAI, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_UNDS, _______, KC_LBRC, KC_RBRC, RGB_VAI,
+  RGB_HUD,  RGB_SAD, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_MINS, _______, KC_LCBR, KC_RCBR, RGB_VAD,
+  _______,  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, _______, _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,
+  RESET, RGB_TOG,  _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_RMOD
 ),
 };
--- keyboards/checkerboards/nop60/readme.md
+++ keyboards/checkerboards/nop60/readme.md
@@ -8,6 +8,6 @@
 
 Make example for this keyboard (after setting up your build environment):
 
-    make nop60:default
+    make checkerboards/nop60:default
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
