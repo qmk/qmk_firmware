cb6e67c96b39b612e3547abe927f1f3b555577ef
thompson-ele <ele.thompson12@gmail.com>
2021-05-14 10:26:37 -0700

    Update keyboards/keebzdotnet/fme/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keebzdotnet/fme/config.h
+++ keyboards/keebzdotnet/fme/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x8008
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    keebzdotnet
-#define PRODUCT         keyboard
+#define PRODUCT         FMe
 
 /* key matrix size */
 #define MATRIX_ROWS 4
