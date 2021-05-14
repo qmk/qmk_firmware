426bda926aafeac05dac1119e784e9f7fbab2330
Steven D. Lander <stevendlander@gmail.com>
2021-05-14 10:29:50 -0400

    Added SysRq on alt, updated macro and num layer toggle change

--- keyboards/unicomp/mini_m/keymaps/default/keymap.c
+++ keyboards/unicomp/mini_m/keymaps/default/keymap.c
@@ -18,19 +18,22 @@
 // Layer shortcuts
 enum custom_layers {
     _QWERTY,
-    _NUMPAD
+    _NUMPAD,
 };
 
 enum custom_keycodes {
-    NUMSLCK = SAFE_RANGE // Num Lock on shift, Scroll Lock regularly
+    NUMSLCK = SAFE_RANGE, // Num Lock on shift, Scroll Lock regularly
+    PAUSRST,  // Resets keyboard on shift, Pause|break regularly
+    PSCSYSR,  // Sends SYSREQ on alt, Print Screen regularly
 };
 
-// Shift detection
+// Mod key detection
 #define MODS_SHIFT_MASK (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+#define MODS_ALT_MASK (MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
-    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, NUMSLCK, KC_PAUS,
+    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             PSCSYSR, NUMSLCK, PAUSRST,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,            KC_DEL,  KC_END,  KC_PGDN,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,                              
@@ -47,14 +50,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-bool process_user_record(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case NUMSLCK: {
             if (record->event.pressed) {
                 if (keyboard_report->mods & MODS_SHIFT_MASK) {
-                    layer_invert(_NUMPAD);
-                    register_code(KC_NLCK);
-                    unregister_code(KC_NLCK);
+                    tap_code(KC_NLCK);
                 } else {
                     register_code(KC_SLCK);
                 }
@@ -63,6 +64,43 @@ bool process_user_record(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         }
+        case PAUSRST: {
+            if (record->event.pressed) {
+                if (keyboard_report->mods & MODS_SHIFT_MASK) {
+                    reset_keyboard();
+                } else {
+                    register_code(KC_PAUS);
+                }
+            } else {
+                unregister_code(KC_PAUS);
+            }
+            break;
+        }
+        case PSCSYSR: {
+            if (record->event.pressed) {
+                if (keyboard_report->mods & MODS_ALT_MASK) {
+                    tap_code(KC_SYSREQ);
+                } else {
+                    register_code(KC_PAUS);
+                }
+            } else {
+                unregister_code(KC_PAUS);
+            }
+            break;
+        }
+    }
+    return true;
+}
+
+bool led_update_user(led_t led_state) {
+    static bool num_state = false;
+    if (num_state != led_state.num_lock) {
+        if (led_state.num_lock) {
+            layer_on(_NUMPAD);
+        } else { 
+            layer_off(_NUMPAD);
+        }
+        num_state = led_state.num_lock;
     }
     return true;
 }
