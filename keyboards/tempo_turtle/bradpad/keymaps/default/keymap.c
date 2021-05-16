aed96f3f389aa9410a21dbc859ed3fc8e1f2d9bc
wsremand <willsfann@gmail.com>
2021-05-16 00:03:38 -0400

    added newlines at ends of files for compatability reasons

--- keyboards/tempo_turtle/bradpad/keymaps/default/keymap.c
+++ keyboards/tempo_turtle/bradpad/keymaps/default/keymap.c
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( 
+    [0] = LAYOUT(
         KC_KP_7,    KC_KP_8,     KC_KP_9,   KC_KP_MINUS, \
         KC_KP_4,    KC_KP_5,     KC_KP_6,   KC_KP_PLUS, \
         KC_KP_1,    KC_KP_2,     KC_KP_3,   KC_KP_SLASH, \
--- keyboards/tempo_turtle/bradpad/keymaps/midi_demo/keymap.c
+++ keyboards/tempo_turtle/bradpad/keymaps/midi_demo/keymap.c
@@ -20,19 +20,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_NATURAL] = LAYOUT( 
+    [_NATURAL] = LAYOUT(
         MI_C, MI_BENDD, MI_BENDU, MI_G, \
         MI_D, MI_VELD,  MI_VELU,  MI_A, \
         MI_E, MI_TRNSD, MI_TRNSU, MI_B, \
         MI_F, MI_OCTD,  MI_OCTU,  MI_C,\
              MO(_SHARP), MO(_FLAT)),
-    [_SHARP] = LAYOUT( 
+    [_SHARP] = LAYOUT(
         MI_Cs, KC_TRNS, KC_TRNS, MI_Gs, \
         MI_Ds, KC_TRNS, KC_TRNS, MI_As, \
         MI_F , KC_TRNS, KC_TRNS, MI_C , \
         MI_Fs, KC_TRNS, KC_TRNS, MI_Cs,\
              KC_TRNS, KC_TRNS),
-    [_FLAT] = LAYOUT( 
+    [_FLAT] = LAYOUT(
         MI_B , KC_TRNS, KC_TRNS, MI_Gb, \
         MI_Db, KC_TRNS, KC_TRNS, MI_Ab, \
         MI_Eb, KC_TRNS, KC_TRNS, MI_Bb, \
