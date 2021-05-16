17d31ad03742642d232d78815b64bd3964c06386
kb-elmo <lorwel@mailbox.org>
2021-05-16 11:01:48 +0200

    change USB name casing
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/friedrich/config.h
+++ keyboards/friedrich/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0xB4A2
 #define DEVICE_VER   0x0001
 #define MANUFACTURER kb-elmo
-#define PRODUCT      friedrich
+#define PRODUCT      Friedrich
 
 /* key matrix size */
 #define MATRIX_ROWS 5
