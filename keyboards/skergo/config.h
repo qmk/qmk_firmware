630562c45fe0d3a95e5f8e63c444a92d56a27fd0
C1intMason <zhao1939@gmail.com>
2021-05-16 15:50:29 -0700

    Add SKErgo support

---
+++ keyboards/skergo/config.h
@@ -0,0 +1,74 @@
+/* Copyright (C) 2021  Keyz.io Ltd.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x4B49
+#define PRODUCT_ID      0x534B
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Keyzio
+#define PRODUCT         SKErgo
+#define DESCRIPTION     Through-hole Ergonomic Keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { B0, B4, B3, B2, B1 }
+#define MATRIX_COL_PINS { A1, A2, A3, A4, A5, A6, A7, C7, C6, C5, C4, C2, C1, C0, D7 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* number of backlight levels */
+
+// #ifdef BACKLIGHT_PIN
+// #define BACKLIGHT_LEVELS 0
+// #endif
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+
+// #ifdef RGB_DI_PIN
+// #define RGBLIGHT_ANIMATIONS
+// #define RGBLED_NUM 0
+// #define RGBLIGHT_HUE_STEP 8
+// #define RGBLIGHT_SAT_STEP 8
+// #define RGBLIGHT_VAL_STEP 8
+// #endif
+
+#endif
---
+++ keyboards/skergo/readme.md
@@ -0,0 +1,15 @@
+# SKErgo
+
+![skergo](https://cdn.shopify.com/s/files/1/0396/3001/2582/products/Keyzio-5_1024x1024@2x.jpg?v=1612909648)
+
+An ergonomic layout keyboard with a through-hole component design
+
+* Keyboard Maintainer: [Keyz.io](https://github.com/C1intMason)
+* Hardware Supported: SKErgo, Atmega32a
+* Hardware Availability: [Group Buy](https://keyz.io)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make skergo:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
