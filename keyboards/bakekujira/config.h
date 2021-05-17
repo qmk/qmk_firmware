676cb0b2781b06d4693cdc340b2e2b92bfec9f38
alfred <alfredy@gmail.com>
2021-05-16 19:59:14 -0700

    added via support, moved system/media keys around

--- keyboards/bakekujira/config.h
+++ keyboards/bakekujira/config.h
@@ -20,8 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0x1213
 #define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    ogkeylab
-#define PRODUCT         bakekujira
+#define MANUFACTURER    ogkey.lab
+#define PRODUCT         ogkey.lab bakekujira
+#define DESCRIPTION     ogkey.lab bakekujira
 
 // #define TAPPING_TERM 200
 
