de1bd9e065373b13c977fadd2685a69f5b63a9ea
Kyle McCreery <mccreery.kyle@gmail.com>
2021-05-18 14:02:37 -1000

    Update keyboards/mechwild/murphpad/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechwild/murphpad/rules.mk
+++ keyboards/mechwild/murphpad/rules.mk
@@ -20,5 +20,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = yes		# Enable encoder
+ENCODER_ENABLE = yes        # Enable encoder
 OLED_DRIVER_ENABLE = yes	# Enable OLED Screen
