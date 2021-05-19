9267ac849d29b21f73ecc535529fd09820365f43
Kyle McCreery <mccreery.kyle@gmail.com>
2021-05-18 14:49:17 -1000

    Apply suggestions from code review
    Re applying the changes that had been committed the first time but were undone while fixing submodules.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechwild/murphpad/rules.mk
+++ keyboards/mechwild/murphpad/rules.mk
@@ -21,4 +21,4 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes        # Enable encoder
-OLED_DRIVER_ENABLE = yes	# Enable OLED Screen
+OLED_DRIVER_ENABLE = yes    # Enable OLED Screen
