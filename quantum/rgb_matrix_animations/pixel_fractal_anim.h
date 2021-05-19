fc9cbe7a31d159d1c435b160aadf55b916ea21ac
filterpaper <filterpaper@localhost>
2021-05-19 08:50:08 +0800

    Add own x y pointer typedef because it will be changed on develop

--- quantum/rgb_matrix_animations/pixel_fractal_anim.h
+++ quantum/rgb_matrix_animations/pixel_fractal_anim.h
@@ -27,6 +27,7 @@ static bool PIXEL_FRACTAL(effect_params_t* params) {
     #define FRACTAL_RATIO 16  // Ratio assumes 224 x 64 matrix
 
     typedef struct PACKED { RGB rgb; bool tag; } fractal_led_t;
+    typedef struct PACKED { uint8_t x; uint8_t y; } fractal_point_t;
     static fractal_led_t led[FRACTAL_WIDTH][FRACTAL_HEIGHT];
     static uint32_t wait_timer = 0;
     if (wait_timer > g_rgb_timer) { return false; }
@@ -34,14 +35,14 @@ static bool PIXEL_FRACTAL(effect_params_t* params) {
     inline uint32_t interval(void) { return 3000 / scale16by8(qadd8(rgb_matrix_config.speed, 16), 16); }
 
     #define FRACTAL_PIXEL() (random8() & 3) ? (RGB){0,0,0} : rgb_matrix_hsv_to_rgb((HSV){random8(), qadd8(random8() >> 1, 127), rgb_matrix_config.hsv.v})
-    #define GET_FRACTAL_POINT(k) (point_t){scale8(g_led_config.point[k].x, FRACTAL_RATIO), scale8(g_led_config.point[k].y, FRACTAL_RATIO)}
+    #define GET_FRACTAL_POINT(k) (fractal_point_t){scale8(g_led_config.point[k].x, FRACTAL_RATIO), scale8(g_led_config.point[k].y, FRACTAL_RATIO)}
 
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
     if (params->init) { random16_set_seed((uint16_t)g_rgb_timer); }
     // Main LED lighting loop
     for (uint8_t i = led_min; i < led_max; ++i) {
         RGB_MATRIX_TEST_LED_FLAGS();
-        point_t map = GET_FRACTAL_POINT(i);
+        fractal_point_t map = GET_FRACTAL_POINT(i);
         led[map.x][map.y].tag = true; // Tag used arrays after scaling by FRACTAL_RATIO
         rgb_matrix_set_color(i, led[map.x][map.y].rgb.r, led[map.x][map.y].rgb.g, led[map.x][map.y].rgb.b);
     }
