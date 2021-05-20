879481c7e1059f17424d9dbf893c5245bd30ccc1
lfgberg <lfgaberg@gmail.com>
2021-05-20 19:50:48 -0400

    Fixed ISO layout macro, and removed redundant declarations

--- keyboards/anomalykb/a65i/a65i.h
+++ keyboards/anomalykb/a65i/a65i.h
@@ -72,14 +72,14 @@
 
 #define LAYOUT_iso( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K0F, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,           K1F, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      K2E, K2F, \
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      K3E, K3F, \
     K40, K41, K42,                     K47,           K4A, K4B,      K4D, K4E, K4F  \
 ) \
 { \
     { K00, K01, K02, K03,   K04,   K05,   K06,   K07, K08,   K09,   K0A, K0B, K0C,   K0D,   KC_NO, K0F }, \
-    { K10, K11, K12, K13,   K14,   K15,   K16,   K17, K18,   K19,   K1A, K1B, K1C,   KC_NO, K1E,   K1F }, \
+    { K10, K11, K12, K13,   K14,   K15,   K16,   K17, K18,   K19,   K1A, K1B, K1C,   KC_NO, KC_NO, K1F }, \
     { K20, K21, K22, K23,   K24,   K25,   K26,   K27, K28,   K29,   K2A, K2B, K2C,   KC_NO, K2E,   K2F }, \
     { K30, K31, K32, K33,   K34,   K35,   K36,   K37, K38,   K39,   K3A, K3B, K3C,   KC_NO, K3E,   K3F }, \
     { K40, K41, K42, KC_NO, KC_NO, KC_NO, KC_NO, K47, KC_NO, KC_NO, K4A, K4B, KC_NO, K4D,   K4E,   K4F }, \
@@ -87,14 +87,14 @@
 
 #define LAYOUT_iso_splitbs( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,           K1F, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      K2E, K2F, \
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      K3E, K3F, \
     K40, K41, K42,                     K47,           K4A, K4B,      K4D, K4E, K4F  \
 ) \
 { \
     { K00, K01, K02, K03,   K04,   K05,   K06,   K07, K08,   K09,   K0A, K0B, K0C,   K0D,   K0E,   K0F }, \
-    { K10, K11, K12, K13,   K14,   K15,   K16,   K17, K18,   K19,   K1A, K1B, K1C,   KC_NO, K1E,   K1F }, \
+    { K10, K11, K12, K13,   K14,   K15,   K16,   K17, K18,   K19,   K1A, K1B, K1C,   KC_NO, KC_NO, K1F }, \
     { K20, K21, K22, K23,   K24,   K25,   K26,   K27, K28,   K29,   K2A, K2B, K2C,   KC_NO, K2E,   K2F }, \
     { K30, K31, K32, K33,   K34,   K35,   K36,   K37, K38,   K39,   K3A, K3B, K3C,   KC_NO, K3E,   K3F }, \
     { K40, K41, K42, KC_NO, KC_NO, KC_NO, KC_NO, K47, KC_NO, KC_NO, K4A, K4B, KC_NO, K4D,   K4E,   K4F }, \
