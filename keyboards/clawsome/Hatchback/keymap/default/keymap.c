1559deda46d42f8bdbb04aa0c7a85ec2ad9e09c6
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2021-05-19 21:40:02 -0600

    Hatchback

--- keyboards/clawsome/Hatchback/keymap/default/keymap.c
+++ keyboards/clawsome/Hatchback/keymap/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 AAClawson (AlisGraveNil)
+/* Copyright 2021 REPLACE_WITH_YOUR_NAME
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
