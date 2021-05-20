a45ec6dc09f4934d9c2231d910aa1202976457ed
shinjikun1011 <57225836+2Moons-JP@users.noreply.github.com>
2021-05-20 09:35:08 +0900

    remove via

--- keyboards/plx/config.h
+++ keyboards/plx/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Peac Design and Basekeys.com
+/* Copyright 2020 Peac Design and Basekeys
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
