2495cb1de1cf98dcf0b28a174c7eb4806f9e92e4
Flexerm <51996324+Flexerm@users.noreply.github.com>
2021-05-21 08:21:34 +0100

    Update keyboards/prototypist/j01/j01.h
    Accepted Fauxpark's suggestion.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/prototypist/j01/j01.h
+++ keyboards/prototypist/j01/j01.h
@@ -20,8 +20,6 @@
 
 #define ___ KC_NO
 
-// 
-
 #define LAYOUT_all( \
     K00, K01,   K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, K0G,    K0H,   \
     K10, K11,   K12,    K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F,      K1H,   \
