1f86a3ecd50ad79a0c2a5434b69e326b0e491d57
j-dags <jon.dagdagan@gmail.com>
2021-05-21 14:38:23 -0500

    reverted commit

--- keyboards/kyria/keymaps/jdags/keymap.c
+++ keyboards/kyria/keymaps/jdags/keymap.c
@@ -35,17 +35,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * | ESC    |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  PIPE  |
+ * | ESC    |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |   \ |  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * | Lsft   |   A  |   S  |   D  |   F  |  G   |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | Lctl  |   Z  |   X  |   C   |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |  - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |RotEnc| Alt  | NAV  | Space| Ent  |  | Del  | Bspc |SYMBOLS| Alt |RotEnc|
+ *                        |RotEnc| Alt  | NAV  | Space| Ent  |  | Del  | Bspc | SMBL | Alt |RotEnc|
  *                        `----------------------------------'  `----------------------------------'
  */
     [QWERTY] = LAYOUT(
-      KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
+      KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
       KC_LSFT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
               XXXXXXX, KC_LALT, MO(SYMBOLS), KC_SPC, KC_ENT, KC_DEL, KC_BSPC, MO(NAV), KC_LALT, LCTL(LSFT(LALT(KC_F1)))
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |      |   1  |   2  |   3  |  0   |      |      |  |      |      |  -   |      |      |      |      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        |      |      | NAV  |      |      |  | Del  |   0  |SYMBOLS|     |      |
+ *                        |      |      | NAV  |      |      |  | Del  |   0  | SMBL |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [NAV] = LAYOUT(
