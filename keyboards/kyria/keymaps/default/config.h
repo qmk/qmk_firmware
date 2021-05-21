f908d1c9f529e29dd515b7d9f423e66fd10ef6b2
bartoszluka <bartoszluka1@gmail.com>
2021-05-21 13:44:27 +0200

    make right hand master

--- keyboards/kyria/keymaps/default/config.h
+++ keyboards/kyria/keymaps/default/config.h
@@ -16,16 +16,18 @@
 
 #pragma once
 
+#define MASTER_RIGHT
+
 #ifdef OLED_DRIVER_ENABLE
-  #define OLED_DISPLAY_128X64
+#    define OLED_DISPLAY_128X64
 #endif
 
 #ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_ANIMATIONS
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 150
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_LIMIT_VAL 150
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
--- keyboards/kyria/keymaps/default/rules.mk
+++ keyboards/kyria/keymaps/default/rules.mk
@@ -1,3 +1,3 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
