73479a5d6779dd8745c41b4a8f7ca12f7ecb7331
peepeetee <peepeetee@protonmail.com>
2021-05-22 01:44:42 -0500

    restore erroneously included bm68rgb config

--- keyboards/kprepublic/bm68rgb/config.h
+++ keyboards/kprepublic/bm68rgb/config.h
@@ -51,8 +51,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-
-
 // The pin connected to the data pin of the LEDs
 #define RGB_DI_PIN E2
 // The number of LEDs connected
