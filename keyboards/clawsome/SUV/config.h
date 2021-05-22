0efbb89af318d95f2a2a895d53e5900498b2dc9a
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2021-05-22 09:49:55 -0600

    22 may

--- keyboards/clawsome/SUV/config.h
+++ keyboards/clawsome/SUV/config.h
@@ -23,7 +23,7 @@
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AlisGraveNil
-#define PRODUCT         SUV1.1
+#define PRODUCT         suv1.1
 
 /* key matrix size */
 #define MATRIX_ROWS 12
