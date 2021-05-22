86bed2530831dcc2754e05bec3cf87e58edf0caa
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2021-05-22 09:18:15 -0600

    Update config.h

--- keyboards/clawsome/Hatchback/config.h
+++ keyboards/clawsome/Hatchback/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x7767
+#define VENDOR_ID       0x7767
+#define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AlisGraveNil
 #define PRODUCT         hatchbackTKL
