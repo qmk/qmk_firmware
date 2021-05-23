7ca2566a73a03555b95d8a7890864148490ab68a
Bartek <bartoszluka1@gmail.com>
2021-05-23 18:46:39 +0200

    add my layout

---
+++ keyboards/kyria/keymaps/bartoszluka1/rules.mk
@@ -0,0 +1,3 @@
+OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
