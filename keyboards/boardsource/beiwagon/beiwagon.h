0ba7e7abeaf5e57050b2349fabc765fad68d1584
Cole Smith <smithcole@protonmail.com>
2021-05-23 19:41:49 -0700

    removed old code

--- keyboards/boardsource/beiwagon/beiwagon.h
+++ keyboards/boardsource/beiwagon/beiwagon.h
@@ -18,15 +18,6 @@
 
 #include "quantum.h"
 
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-
 #define LAYOUT( \
     K00, K01, K02, \
     K10, K11, K12, \
