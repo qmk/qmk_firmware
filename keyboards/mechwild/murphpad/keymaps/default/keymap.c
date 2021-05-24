df61c4b485d7e21d99dbe603ac9a17e0129194a2
Kyle McCreery <mccreery.kyle@gmail.com>
2021-05-23 21:52:34 -1000

    Adding in second encoder, updating readme device versioning, and changing default&via keymaps to support other layout options more readily by default.

--- keyboards/mechwild/murphpad/keymaps/default/keymap.c
+++ keyboards/mechwild/murphpad/keymaps/default/keymap.c
@@ -28,12 +28,12 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-                 KC_F1,    KC_F2,   KC_F3,   KC_F4,
-				 KC_NLCK,  KC_PSLS, KC_PAST, KC_PMNS,
-                 KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
-        KC_MUTE, KC_P4,    KC_P5,   KC_P6,   KC_NO,
+                  KC_F1,    KC_F2,   KC_F3,   KC_F4,
+				  KC_NLCK,  KC_PSLS, KC_PAST, KC_PMNS,
+                  KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
+        KC_MUTE,  KC_P4,    KC_P5,   KC_P6,   _______,
         MO(_FN1), KC_P1,    KC_P2,   KC_P3,   KC_PENT,
-        KC_BSPC, KC_P0,    KC_NO,   KC_PDOT, KC_NO,
+        KC_BSPC,  KC_P0,    _______, KC_PDOT, _______,
 		
 				     KC_F5,   KC_F6,    KC_F7
 
@@ -42,31 +42,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  _______,  _______, _______, _______,				
                  _______,  _______, _______, _______,
                  RGB_HUD,  RGB_SPI, RGB_HUI, _______,
-        _______, RGB_RMOD, RGB_TOG, RGB_MOD, KC_NO,
+        _______, RGB_RMOD, RGB_TOG, RGB_MOD, _______,
         _______, RGB_VAD,  RGB_SPD, RGB_VAI, _______,
-        _______, RGB_SAD,  KC_NO,   RGB_SAI, KC_NO,
+        _______, RGB_SAD,  _______, RGB_SAI, _______,
 		
                      _______, _______, _______
 
     ),
 	[_FN2] = LAYOUT(
-                 _______,  _______, _______, _______,
-                 _______,  _______, _______, _______,
-                 _______,  _______, _______, _______,
-        _______,  _______, _______, _______, KC_NO,
+                  _______, _______, _______, _______,
+                  _______, _______, _______, _______,
+                  _______, _______, _______, _______,
         _______,  _______, _______, _______, _______,
-        _______,  _______, _______, _______, KC_NO,
-				
+        _______,  _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______,
+						   
                   _______, _______, _______
 
     ),
 	[_FN3] = LAYOUT(
-                 _______,  _______, _______, _______,
-                 _______,  _______, _______, _______,
-                 _______,  _______, _______, _______,
-        _______,  _______, _______, _______, KC_NO,
+                  _______, _______, _______, _______,
+                  _______, _______, _______, _______,
+                  _______, _______, _______, _______,
         _______,  _______, _______, _______, _______,
-        _______,  _______, _______, _______, KC_NO,
+        _______,  _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______,
 		
                   _______, _______, _______
 
@@ -83,6 +83,13 @@ void encoder_update_user(uint8_t index, bool clockwise) {
                 tap_code(KC_VOLD);
             }
         break;
+		case 1:
+            if (clockwise) {
+                tap_code(KC_BRIU);
+            } else {
+                tap_code(KC_BRID);
+            }
+        break;
     }
     
 }
@@ -95,6 +102,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
 	static void render_logo(void) {     // Render MechWild "MW" Logo
 		static const char PROGMEM logo_1[] = {0x8A, 0x8B, 0x8C, 0x8D, 0x00};
+		static const char PROGMEM logo_2[] = {0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0x00};
 		static const char PROGMEM logo_2[] = {0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0x00};
 		static const char PROGMEM logo_3[] = {0xCA, 0xCB, 0xCC, 0xCD, 0x00};
 		static const char PROGMEM logo_4[] = {0x20, 0x8E, 0x8F, 0x90, 0x00};
