4d4bfceb1545fe1a9eae27ae250e39a2e4c30444
Gun Pinyo <gunpinyo@gmail.com>
2021-05-26 16:12:09 +0700

    Update keyboards/planck/keymaps/gunp/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/planck/keymaps/gunp/rules.mk
+++ keyboards/planck/keymaps/gunp/rules.mk
@@ -2,17 +2,19 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = no        # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE  = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE  = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE   = yes       # Console for debug(+400)
-COMMAND_ENABLE   = no        # Commands for debug and configuration
-BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
-BLUETOOTH_ENABLE = no        # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE  = no        # Enable WS2812 RGB underlight.
-                             #   note: this option is not for Planck `light`
-NKRO_ENABLE      = yes       # Nkey Rollover - if this doesn't work, see here:
-# https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = yes          # Audio output
 
 MIDI_ENABLE          = no
 UNICODE_ENABLE       = no
