7b0ac541b585e688cc9626e312adcd21dcf7ee43
FateEverywhere <41215461+FateEverywhere@users.noreply.github.com>
2021-05-27 19:45:07 -0400

    Update keyboards/edi/hardlight/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/edi/hardlight/rules.mk
+++ keyboards/edi/hardlight/rules.mk
@@ -16,8 +16,7 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-UNICODE_ENABLE = no         # Unicode
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output on port C6
-RGBLIGHT_ENABLE = yes       # Enable RGB underlighting support
+AUDIO_ENABLE = no           # Audio output
