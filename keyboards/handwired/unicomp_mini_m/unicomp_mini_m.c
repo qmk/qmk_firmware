910b69c1b28826deff487532159b875cabceff1a
Steven D. Lander <stevendlander@gmail.com>
2021-05-28 10:26:58 -0400

    Moving directory to handwired, which more accurately describes the project

--- keyboards/handwired/unicomp_mini_m/unicomp_mini_m.c
+++ keyboards/handwired/unicomp_mini_m/unicomp_mini_m.c
@@ -13,4 +13,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "mini_m.h"
+#include "unicomp_mini_m.h"
