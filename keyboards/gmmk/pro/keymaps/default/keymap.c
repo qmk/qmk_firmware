4edf9a76a007e0a8e92346e9fbe5ca5e6d0aa180
Jasper Chan <jasperchan515@gmail.com>
2021-05-28 00:19:17 -0700

    Return keymap to default

--- keyboards/gmmk/pro/keymaps/default/keymap.c
+++ keyboards/gmmk/pro/keymaps/default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // To put the keyboard in bootloader mode, use FN+backslash. If you accidentally put it into bootloader, you can just unplug the USB cable and
     // it'll be back to normal when you plug it back in.
     [0] = LAYOUT(
-        KC_ESC,  RGB_TOG,   RGB_MODE_RAINBOW,   RGB_MODE_PLAIN,   RGB_VAI,   RGB_SAI,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
@@ -62,13 +62,4 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
       tap_code(KC_VOLD);
     }
     return true;
-}
-
-void keyboard_post_init_user(void) {
-    debug_enable = true;
-#ifdef RGB_MATRIX_ENABLE
-    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
-    rgb_matrix_set_color_all(200, 200, 200);
-    rgb_matrix_enable();
-#endif
 }
