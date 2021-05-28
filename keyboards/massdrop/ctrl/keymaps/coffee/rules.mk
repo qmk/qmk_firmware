eff2561fe3e7581d955def7c67f687d7b1dc2c75
Kasper <ofek4430@gmail.com>
2021-05-28 21:54:45 +0300

    wasn't meant to be comitted

--- keyboards/massdrop/ctrl/keymaps/coffee/rules.mk
+++
@@ -1,6 +0,0 @@
-EXTRAKEY_ENABLE = yes        # Audio control and System control
-COMMAND_ENABLE = yes         # Commands for debug and configuration
-NKRO_ENABLE = yes            # USB Nkey Rollover
-OPENRGB_ENABLE = yes	 	# Enable or Disable OpenRGB support
-OPENRGB_CUSTOM_MATRIX = yes
-DYNAMIC_MACRO_ENABLE = yes	# Dynamic macro recording and play
