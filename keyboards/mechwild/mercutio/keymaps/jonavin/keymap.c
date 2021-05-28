b96304930528c891f30686911884c8551db601fb
Drashna Jaelre <drashna@live.com>
2021-05-27 21:30:47 -0700

    Fix keyboards/keymaps for boolean encoder callback changes (#12985)

--- keyboards/mechwild/mercutio/keymaps/jonavin/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/jonavin/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_ENABLE       // Encoder Functionality
     uint8_t selected_layer = 0;
-    void encoder_update_user(uint8_t index, bool clockwise) {
+    bool encoder_update_user(uint8_t index, bool clockwise) {
         #ifdef OLED_DRIVER_ENABLE
             oled_clear();
             oled_render();
@@ -81,6 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     }
                 }
         }
+        return true;
     }
 #endif
 
