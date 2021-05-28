883ee8f306a60d3bcd2c2a3ebe7c13b242e2c294
Kasper <ofek4430@gmail.com>
2021-05-28 21:35:10 +0300

    Update Drop CTRL OPENRGB_SWITCH_MATRIX_TO_PHYSICAL_POS_MAP for the new automatic matrix

--- keyboards/massdrop/ctrl/config_openrgb.h
+++ keyboards/massdrop/ctrl/config_openrgb.h
@@ -1,10 +1,13 @@
+#define OPENRGB_MATRIX_COLUMNS 19
+#define OPENRGB_MATRIX_ROWS 6
+
+#define NA NA
+
 #define OPENRGB_SWITCH_MATRIX_TO_PHYSICAL_POS_MAP { \
-    { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,  NO_LED, NO_LED, NO_LED,  NO_LED,  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,  NO_LED,  NO_LED,  NO_LED,  NO_LED }, \
-    { NO_LED,   0   , NO_LED,    1  ,    2  ,     3  ,    4  ,   5   ,     6  ,     7  ,   48  ,  49   ,   50  ,   51  ,   52  , NO_LED,    53  ,    54  ,    55  ,  NO_LED }, \
-    { NO_LED,   8   ,    9  ,   10  ,   11  ,    12  ,   13  ,  14   ,    15  ,    56  ,   57  ,  58   ,   59  ,   60  ,   61  , NO_LED,    62  ,    63  ,    79  ,  NO_LED }, \
-    { NO_LED,   16  ,   17  ,   18  ,   19  ,    20  ,   21  ,  22   ,    23  ,    64  ,   65  ,  66   ,   67  ,   68  ,   69  , NO_LED,    70  ,    71  ,    78  ,  NO_LED }, \
-    { NO_LED,   24  , NO_LED,   25  ,   26  ,    27  ,   28  ,  29   ,    30  ,    31  ,   72  ,  73   ,   74  ,   75  ,   76  , NO_LED,  NO_LED,  NO_LED,  NO_LED,  NO_LED }, \
-    { NO_LED,   32  , NO_LED,   33  ,   34  ,    35  ,   36  ,  37   ,    38  ,    39  ,   80  ,  81   ,   82  ,   83  , NO_LED, NO_LED,  NO_LED,   77   ,  NO_LED,  NO_LED }, \
-    { NO_LED,   40  ,   41  ,   42  , NO_LED,  NO_LED, NO_LED,  43   ,  NO_LED,  NO_LED, NO_LED,  44   ,   45  ,   46  ,   47  , NO_LED,    84  ,   85   ,    86  ,  NO_LED }, \
-    { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,  NO_LED, NO_LED, NO_LED,  NO_LED,  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,  NO_LED,  NO_LED,  NO_LED,  NO_LED }  \
+    {  0,  NA,   1,   2,   3,   4,  NA,   5,   6,   7,  48,  49,  50,  51,  52,  NA,  53,  54,  55 }, \
+    {  8,   9,  10,  11,  12,  13,  14,  15,  56,  57,  58,  59,  60,  NA,  61,  NA,  62,  63,  79 }, \
+    { 16,  NA,  17,  18,  19,  20,  21,  22,  23,  64,  65,  66,  67,  68,  69,  NA,  70,  71,  78 }, \
+    { 24,  NA,  25,  26,  27,  28,  29,  30,  31,  72,  73,  74,  75,  NA,  76,  NA,  NA,  NA,  NA }, \
+    { NA,  32,  33,  34,  35,  36,  37,  38,  39,  80,  81,  82,  NA,  83,  NA,  NA,  NA,  77,  NA }, \
+    { 40,  41,  NA,  42,  NA,  NA,  43,  NA,  NA,  NA,  44,  NA,  45,  46,  47,  NA,  84,  85,  86 }, \
 }
