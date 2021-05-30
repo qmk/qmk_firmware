ea572d314b09dd1385b7155013d91af8a8f232e0
Florian Scheibeck <info@floookay.de>
2021-05-30 13:21:18 +0200

    updated keymap

--- keyboards/pluckey/keymaps/default/keymap.c
+++ keyboards/pluckey/keymaps/default/keymap.c
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR, KC_HOME,      KC_END,  M_DTHIS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN,                        KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR, KC_HOME,      KC_END,  KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
                           KC_LGUI, KC_LALT, MO_LOW,      KC_SPC,    KC_BSPC,            KC_ENT,    KC_SPC,      MO_RAIS, KC_RALT, KC_MEH                     \
     ),
     [_LOWER] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                         KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PSCR, \
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LPRN,                        KC_RPRN, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
         _______, _______, _______, _______, KC_PIPE, KC_UNDS, KC_LPRN,                        KC_RPRN, KC_PLUS, KC_PIPE, _______, _______, _______, _______, \
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LPRN,                        KC_RPRN, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
         _______, _______, _______, _______, _______, _______, M_ARROW, KC_PGUP,      KC_PGDN, M_DTHIS, _______, _______, _______, _______, _______, _______, \
                           _______, _______, MO_CURR,     _______,   KC_DEL,             _______,   _______,     MO_ADJU, _______, _______                    \
     ),
