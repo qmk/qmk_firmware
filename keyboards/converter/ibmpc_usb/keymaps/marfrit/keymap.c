4c5dc8ee6e725f53f0ab59a6ab44675bc41c1816
Markus Fritsche <fritsche.markus@gmail.com>
2021-05-31 13:10:20 +0200

    my keymap on xt

--- keyboards/converter/ibmpc_usb/keymaps/marfrit/keymap.c
+++ keyboards/converter/ibmpc_usb/keymaps/marfrit/keymap.c
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
     LAYOUT_xt(
         KC_F1,   KC_F2,     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_NLCK,          KC_SLCK,
-        KC_F3,   KC_F4,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
-        KC_F5,   KC_F6,     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_GRV,  KC_ENT,  KC_P4,   KC_P5,   KC_P6,
-        KC_F7,   KC_F8,     KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_PAST, KC_P1,   KC_P2,   KC_P3,   KC_PPLS,
+        KC_F3,   KC_F4,     KC_TAB,  ALL_T(KC_K), KC_U,         KC_Q,         KC_DOT,       KC_J,    KC_V,    KC_G,         KC_C,         KC_L,         KC_F,    KC_LBRC, KC_RBRC,          KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
+        KC_F5,   KC_F6,     KC_LCTL, KC_H,        LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A), KC_O,    KC_D,    RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S, KC_QUOT, KC_GRV,  KC_ENT,  KC_P4,   KC_P5,   KC_P6,
+        KC_F7,   KC_F8,     KC_LSFT, KC_NUBS,     KC_X,         RALT_T(KC_Y), KC_SCLN,      KC_COMM, KC_SLSH, KC_B,         KC_P,         KC_W,         RALT_T(KC_M), KC_Z, KC_RSFT, KC_PAST, KC_P1,   KC_P2,   KC_P3,   KC_PPLS,
         KC_F9,   KC_F10,    KC_LALT,                                              KC_SPC,                                      KC_CAPS,          KC_P0,            KC_PDOT
     ),
 
