812180ff3edb69f74139e6edc43104766b4c369b
Markus Fritsche <fritsche.markus@gmail.com>
2021-05-31 10:30:44 +0200

    added xt support to keymap

--- keyboards/converter/ibmpc_usb/ibmpc_usb.h
+++ keyboards/converter/ibmpc_usb/ibmpc_usb.h
@@ -82,6 +82,31 @@ void matrix_init_user(void);
     { K78,   K79,   K7A,   K7B,   K7C,   K7D,   K7E,   K7F   }  \
 }
 
+#define LAYOUT_xt( \
+    K3B, K3C,   K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K45,      K46, \
+    K3D, K3E,   K0F, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B,      K47, K48, K49, K4A, \
+    K3F, K40,   K1D, K1E, K1F, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K1C, K4B, K4C, K4D, \
+    K41, K42,   K2A, K2B, K2C, K2D, K2E, K2F, K30, K31, K32, K33, K34, K35, K36, K37, K4F, K50, K51, K4E, \
+    K43, K44,   K38,                          K39,                     K3A,      K52,      K53 \
+) { \
+    { KC_NO, K01, K02, K03, K04, K05, K06, K07 }, \
+    { K08, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17 }, \
+    { K18, K19, K1A, K1B, K1C, K1D, K1E, K1F }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27 }, \
+    { K28, K29, K2A, K2B, K2C, K2D, K2E, K2F }, \
+    { K30, K31, K32, K33, K34, K35, K36, K37 }, \
+    { K38, K39, K3A, K3B, K3C, K3D, K3E, K3F }, \
+    { K40, K41, K42, K43, K44, K45, K46, K47 }, \
+    { K48, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
+    { K50, K51, K52, K53, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO } \
+}
+
 typedef enum { NONE, PC_XT, PC_AT, PC_TERMINAL, PC_AT_Z150 } keyboard_kind_t;
 #define KEYBOARD_KIND_STR(kind) \
     (kind == PC_XT ? "XT" :   \
