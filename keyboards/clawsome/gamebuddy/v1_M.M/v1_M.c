2c9b58a04e09760080e2e7b207c8b99c91241f1f
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2021-05-31 14:24:11 -0600

    Update keyboards/clawsome/gamebuddy/v1_M.M/v1_M.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/clawsome/gamebuddy/v1_M.M/v1_M.c
+++ keyboards/clawsome/gamebuddy/v1_M.M/v1_M.c
@@ -13,4 +13,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "v1.M.h"
+#include "v1_m.h"
