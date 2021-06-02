8d7986423d83695d8a4800a57f17cd6a64670f4b
shiftux <shiftux@gmail.com>
2021-06-02 14:13:56 +0200

    adding a new revision 'media' to the redox keyboard

---
+++ keyboards/redox/keymaps/media-CH/keymap.c
@@ -0,0 +1,78 @@
+#include QMK_KEYBOARD_H
+#include "../../media/media.h"
+
+extern keymap_config_t keymap_config;
+
+#define _QWERTZ 0 // standard layer
+#define _SYMB 1   // symbols layer
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+};
+
+enum custom_keycodes {
+  QWERTZ = SAFE_RANGE,
+  SYMB,
+};
+
+// Shortcut to make keymap more readable
+#define KC_BKSL KC_BSLASH
+#define SYM_L   MO(_SYMB)
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTZ] = LAYOUT(
+  //                               ┌────────┐                                                            ┌────────┬────────┐
+                                    KC_MUTE,                                                              KC_MPLY, KC_MNXT,
+  //┌────────┬────────┬────────┬───┼────┬───┼────┬────────┐                                           ┌──┼─────┬──┼─────┬──┼─────┬────────┬────────┬────────┐
+     KC_ESC,   KC_1,    KC_2,    KC_3,   KC_4,    KC_5,                                                 KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_NUBS,                            KC_MINS,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_CAPS,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_EQL,                            KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_PGDN,  KC_PGUP,        KC_HOME, KC_END,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     KC_LCTL,  SYM_L,   KC_LALT, KC_GRV,      KC_DEL,     KC_LGUI,   KC_SPC,         KC_ENT,   KC_SPC,     KC_RALT,       KC_LEFT,  KC_UP,  KC_DOWN,  KC_RGHT
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+
+  [_SYMB] = LAYOUT(
+   //                              ┌────────┐                                                            ┌────────┬────────┐
+                                    KC_MPRV,                                                              XXXXXXX, XXXXXXX,
+  //┌────────┬────────┬────────┬───┼────┬───┼────┬────────┐                                           ┌──┼─────┬──┼─────┬──┼─────┬────────┬────────┬────────┐
+     RESET    ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5,                                               KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_F12 ,                           XXXXXXX ,XXXXXXX ,XXXXXXX, KC_UP ,XXXXXXX ,XXXXXXX ,XXXXXXX  ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX  ,XXXXXXX ,XXXXXXX,        XXXXXXX ,XXXXXXX ,KC_TILD ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  )
+
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_WH_D);
+        } else {
+            tap_code(KC_WH_U);
+        }
+    }
+}
---
+++ keyboards/redox/media/README.md
@@ -0,0 +1,65 @@
+# The Redox Media Keyboard
+
+I've taken the Redox keyboard and modified the SCAD files as well as the QMK layout to include media and scroll features:
+- 3 Media buttons
+    - Play / Pause button
+    - Next track
+    - Mute (previous track on secondary layout)
+- Volume knob
+- Scroll wheel on the keyboard
+
+For an in-depth report and video see [my page](https://shiftux.org/making_projects/keyboard.html).
+
+## IMAGE
+
+### The build
+I've used 2 [Sparkfun Pro Micros (5V)](https://www.sparkfun.com/products/12640) and a single hand master setup providing power and serial connection via a TRS jack.
+
+### The pro micro and how to flash it
+To try out your pro micro and setup your environment to flash such a device I recommend following [this guide](https://learn.sparkfun.com/tutorials/pro-micro--fio-v3-hookup-guide/all). It will take you through the install and setup and provides a first easy to flash program so you can test your setup quickly.
+
+Whenever you can't program one of the pro micros or the USB-COM port is not found on your computer, it most probably means that is is not in "bootloader mode". What you want to do is connect the GND and RST pin twice to reset the device and put it into bootloader mode (details [here](https://learn.sparkfun.com/tutorials/pro-micro--fio-v3-hookup-guide/troubleshooting-and-faq#ts-serial)).
+
+To find out how your serial port is called on your computer, you can find it with the following command:
+```
+ll /dev | grep "tty\."
+```
+Mine is called `/dev/tty.usbmodem14522301` for example.
+
+### Compiling and flashing
+
+Obviously start by [installing QMK](https://docs.qmk.fm/#/getting_started_build_tools?id=set-up-your-environment).
+
+Once you are comfortable flashing your Pro Micros and your setup works continue with the following steps:
+
+- I've had greatest success with using the `EE_HANDS` setting and flashing the EEPROM to indentify left and right halves of the keyboard. To do this start with flashing the EEPROM of the 2 pro micros (make sure the COM port is the correct one and change left to right hand for other half) Reset the device: connect GND to RST twice, then you have 8 seconds to upload the new eep file with this command (from the qmk root). From the root of the QMK repo:
+```
+avrdude -c avr109 -p m32u4 -P /dev/tty.usbmodem14522301 -U eeprom:w:"./quantum/split_common/eeprom-lefthand.eep":a
+```
+
+- Next compile the firmware from the qmk repo root:
+```
+qmk compile -kb redox/media -km media-CH
+```
+The generated files are output to the `.build` directory. You'll want the `.hex` file.
+- Finally flash the generated hex (make sure the COM port is the correct one) from the QMK root:
+```
+avrdude -p atmega32u4 -c avr109 -U flash:w:./.build/redox_media_media.hex:i -P /dev/tty.usbmodem14522301
+```
+
+### Adapting the keyboard layout
+
+Copy the `keymaps/media-CH` folder to `keymaps/media-<your-layout>` and adapt the `keymap.c` file in there.
+
+I recommend testing the key codes with the [QMK tester](https://config.qmk.fm/#/test) and then adapting the `keymap.c` file accordingly.
+
+Finally compile your new layout from the QMK root with:
+```
+qmk compile -kb redox/media -km media-<your-layout>
+```
+and proceed to flashing it equivalent to the above instructions.
+
+### CAD files and additional resources
+See [here](https://shiftux.org/making_projects/keyboard.html) for a detailed build description and video.
+
+You can find the CAD and SCAD files, I created here: https://github.com/shiftux/redox-media-keyboard
