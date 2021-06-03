b1b12d9270456e2019c33e5b87e16309e5ddd9d6
csandven <chs_91@hotmail.com>
2021-06-03 22:41:55 +0200

    Add music mode and raise layer

--- keyboards/keyprez/rhino/rhino.h
+++ keyboards/keyprez/rhino/rhino.h
@@ -18,6 +18,10 @@
 
 #include "quantum.h"
 
+#ifndef encoder_update_user
+bool encoder_update_user(int index, bool clockwise);
+#endif
+
 /* This is a shortcut to help you visually see your layout.
  *
  * The first section contains all of the arguments representing the physical
