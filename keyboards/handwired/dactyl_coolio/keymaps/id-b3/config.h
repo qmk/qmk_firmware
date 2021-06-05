1a98a135854d835dbf99499a9e25386988f6514c
id-b3 <58172188+id-b3@users.noreply.github.com>
2021-06-05 22:35:52 +0200

    Update keyboards/handwired/dactyl_coolio/keymaps/id-b3/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/dactyl_coolio/keymaps/id-b3/config.h
+++ keyboards/handwired/dactyl_coolio/keymaps/id-b3/config.h
@@ -29,7 +29,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_VAL_STEP 8
 #endif
 
-#define DISABLE_LEADER
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
