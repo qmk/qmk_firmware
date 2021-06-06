ee00a00fd87209f97319550840ac2ab693f1113a
Joshua T <joshua@sonofone.net>
2021-06-06 14:19:27 -0500

    Fix NUMWORD and Spacebar issue
    Correct an issue where KC_SPC wasn't getting sent when NUMWORD was disabled.

--- keyboards/xd75/keymaps/replicaJunction/keymap.c
+++ keyboards/xd75/keymaps/replicaJunction/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HASH, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, SFT_TAB,
         _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, _______, _______, KC_PPLS, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_TAB,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_BSLS, KC_EQL,
-        _______, _______, _______, _______, ooooooo, _______, _______, _______, _______, KC_SPC,  KC_P0,   KC_P0,   KC_PDOT, KC_PENT, _______
+        _______, _______, _______, _______, ooooooo, _______, _______, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, KC_PENT, _______
     ),
 
 
