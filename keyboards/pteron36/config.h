4161ab45b7a38e434d78a0da39031bf5d8eac453
Harshit Goel--HOME <harshitgoel96@yahoo.com>
2021-06-07 00:00:49 +0530

    use available usb id

--- keyboards/pteron36/config.h
+++ keyboards/pteron36/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x4847 //HG
+#define PRODUCT_ID   0x5054 //PT
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Harshit Goel
 #define PRODUCT      Pteron36
