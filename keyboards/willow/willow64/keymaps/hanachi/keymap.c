8d99030e6bb669f7308b3d2389c5fd6a0c69fe9a
hanachi-ap <75919318+hanachi-ap@users.noreply.github.com>
2021-06-06 18:04:18 +0900

    Update keyboards/willow/willow64/keymaps/hanachi/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/willow/willow64/keymaps/hanachi/keymap.c
+++ keyboards/willow/willow64/keymaps/hanachi/keymap.c
@@ -66,9 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 int layer = _QWERTY;
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-#ifdef CONSOLE_ENABLE
     uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
-#endif
     return true;
 }
 
