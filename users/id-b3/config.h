7c9fc105728ffda1e966fb9a0d1fef9897a84c62
id-b3 <58172188+id-b3@users.noreply.github.com>
2021-06-07 13:07:19 +0200

    Update users/id-b3/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- users/id-b3/config.h
+++ users/id-b3/config.h
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef OLED_DRIVER_ENABLE
-  #define OLED_DISPLAY_128X64
-  #define OLED_FONT_H "coolio_font.h"
-  #define OLED_TIMEOUT 60000
+#    define OLED_DISPLAY_128X64
+#    define OLED_FONT_H "coolio_font.h"
+#    define OLED_TIMEOUT 60000
 #endif
