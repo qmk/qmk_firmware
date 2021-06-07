8f40b48274a61cad781306c4a62d9f4c57a90fca
Christian Eiden <christian@eiden.ch>
2021-06-07 09:09:19 +0200

    switch deafault to colemak
    Signed-off-by: Christian Eiden <christian@eiden.ch>

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/keymap.c
@@ -28,41 +28,14 @@
 #define COLEM   DF(_COLEMAK_DH)
 
 enum layer_names {
-  _QWERTY,
   _COLEMAK_DH,
+  _QWERTY,
   _SYMBOL,
   _ADJUST
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  [_QWERTY] = LAYOUT_5x6_5(
-  /* QWERTY
-   * .-----------------------------------------.                                  .-----------------------------------------.
-   * | Esc  |   1  |   2  |   3  |   4  |   5  |                                  |   6  |   7  |   8  |   9  |   0  |  Bsp |
-   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |                                  |   Y  |   U  |   I  |   O  |   P  |  \   |
-   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |                                  |   H  |   J  |   K  |   L  |   ;  |  '   |
-   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Shft |   Z  |   X  |   C  |   V  |   B  |                                  |   N  |   M  |   ,  |   .  |   /  | Shft |
-   * '-----------------------------------------/                                  \-----------------------------------------'
-   *               |  ADJ |   =  |    /  SYM  /---------------.    .---------------\  SYM  \    |   -  | ADJ  |
-   *               |      |      |   /       /  Cmd  / Shift /      \ Shift \  Cmd  \       \   |      |      |
-   *               '-------------'  '-------/ Space / Enter /        \ Enter \ Space \-------'  '-------------'
-   *                                       /---------------/          \---------------\
-   *                                      /  Alt  / Ctrl  /            \  Ctrl \  Alt  \
-   *                                     /       /       /              \       \       \
-   *                                    '---------------'                '---------------'
-   */
-   KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-   KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
-   KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                     ADJ    , KC_EQL ,          SYM    , CM_SPAL, SH_ENL , SH_ENR , CM_SPAR, SYM    ,          KC_MINS, ADJ    ,
-                                                         KC_LALT, KC_LCTL, KC_RCTL, KC_RALT
-   ),
   [_COLEMAK_DH] = LAYOUT_5x6_5(
   /* COLEMAK_DH
    * .-----------------------------------------.                                  .-----------------------------------------.
@@ -86,6 +59,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSLS,
    KC_LCTL, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                                     KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,                                     KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+                     ADJ    , KC_EQL ,          SYM    , CM_SPAL, SH_ENL , SH_ENR , CM_SPAR, SYM    ,          KC_MINS, ADJ    ,
+                                                         KC_LALT, KC_LCTL, KC_RCTL, KC_RALT
+   ),
+  [_QWERTY] = LAYOUT_5x6_5(
+  /* QWERTY
+   * .-----------------------------------------.                                  .-----------------------------------------.
+   * | Esc  |   1  |   2  |   3  |   4  |   5  |                                  |   6  |   7  |   8  |   9  |   0  |  Bsp |
+   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |                                  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |                                  |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+   * | Shft |   Z  |   X  |   C  |   V  |   B  |                                  |   N  |   M  |   ,  |   .  |   /  | Shft |
+   * '-----------------------------------------/                                  \-----------------------------------------'
+   *               |  ADJ |   =  |    /  SYM  /---------------.    .---------------\  SYM  \    |   -  | ADJ  |
+   *               |      |      |   /       /  Cmd  / Shift /      \ Shift \  Cmd  \       \   |      |      |
+   *               '-------------'  '-------/ Space / Enter /        \ Enter \ Space \-------'  '-------------'
+   *                                       /---------------/          \---------------\
+   *                                      /  Alt  / Ctrl  /            \  Ctrl \  Alt  \
+   *                                     /       /       /              \       \       \
+   *                                    '---------------'                '---------------'
+   */
+   KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
+   KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
+   KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
                      ADJ    , KC_EQL ,          SYM    , CM_SPAL, SH_ENL , SH_ENR , CM_SPAR, SYM    ,          KC_MINS, ADJ    ,
                                                          KC_LALT, KC_LCTL, KC_RCTL, KC_RALT
    ),
