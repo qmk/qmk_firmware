1d0dbd9842c88616dc1df9bbf3958761bc3e72b0
hyoungjkim <hyoungjkim@ebay.com>
2021-06-07 20:06:42 +0900

    Changed `matrix.c` implementation to `CUSTOM_MATRIX = lite` style.

--- keyboards/kmac_pad/rules.mk
+++ keyboards/kmac_pad/rules.mk
@@ -12,8 +12,8 @@ BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-CUSTOM_MATRIX = yes         # Custom matrix file
+COMMAND_ENABLE = no         # Commands for debug and configuration
+CUSTOM_MATRIX = lite        # Custom matrix file
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
