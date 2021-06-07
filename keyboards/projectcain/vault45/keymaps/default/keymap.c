0d4c666e6fe16a6087031d95ee4e5ed4b331abb0
projectcain <71684775+projectcain@users.noreply.github.com>
2021-06-07 03:07:59 -0500

    Update keyboards/projectcain/vault45/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/projectcain/vault45/keymaps/default/keymap.c
+++ keyboards/projectcain/vault45/keymaps/default/keymap.c
@@ -32,22 +32,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP); 
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_DOWN); 
-        } else {
-            tap_code(KC_UP);
-        }
-    }
-    return true;
-}
 
 enum combos {
   KL_SLSH,
