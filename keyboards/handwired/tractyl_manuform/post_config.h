147d226b8f37fbd28a44de92d397ca9653ecca01
Drashna Jael're <drashna@live.com>
2021-06-06 17:06:30 -0700

    Add mouse defaults to tractyl

--- keyboards/handwired/tractyl_manuform/post_config.h
+++ keyboards/handwired/tractyl_manuform/post_config.h
@@ -18,34 +18,78 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* mouse config */
-#ifndef MOUSEKEY_INTERVAL
-#    define MOUSEKEY_INTERVAL 20
-#endif
-#ifndef MOUSEKEY_DELAY
-#    define MOUSEKEY_DELAY 0
-#endif
-#ifndef MOUSEKEY_TIME_TO_MAX
-#    define MOUSEKEY_TIME_TO_MAX 60
-#endif
-#ifndef MOUSEKEY_MAX_SPEED
-#    define MOUSEKEY_MAX_SPEED 7
-#endif
-#ifndef MOUSEKEY_WHEEL_DELTA
-#    define MOUSEKEY_WHEEL_DELTA 1
-#endif
-#ifndef MOUSEKEY_WHEEL_DELAY
-#    define MOUSEKEY_WHEEL_DELAY 200
-#endif
-#ifndef MOUSEKEY_WHEEL_INTERVAL
-#    define MOUSEKEY_WHEEL_INTERVAL 50
-#endif
-#ifndef MOUSEKEY_WHEEL_MAX_SPEED
-#    define MOUSEKEY_WHEEL_MAX_SPEED 8
-#endif
-#ifndef MOUSEKEY_WHEEL_TIME_TO_MAX
-#    define MOUSEKEY_WHEEL_TIME_TO_MAX 80
-#endif
+// mouse config
+#    ifndef MOUSEKEY_MOVE_DELTA
+#        ifndef MK_KINETIC_SPEED
+#            define MOUSEKEY_MOVE_DELTA 5
+#        else
+#            define MOUSEKEY_MOVE_DELTA 25
+#        endif
+#    endif
+#    ifndef MOUSEKEY_DELAY
+#        ifndef MK_KINETIC_SPEED
+#            define MOUSEKEY_DELAY 300
+#        else
+#            define MOUSEKEY_DELAY 8
+#        endif
+#    endif
+#    ifndef MOUSEKEY_INTERVAL
+#        ifndef MK_KINETIC_SPEED
+#            define MOUSEKEY_INTERVAL 50
+#        else
+#            define MOUSEKEY_INTERVAL 20
+#        endif
+#    endif
+#    ifndef MOUSEKEY_MAX_SPEED
+#        define MOUSEKEY_MAX_SPEED 7
+#    endif
+#    ifndef MOUSEKEY_TIME_TO_MAX
+#        define MOUSEKEY_TIME_TO_MAX 60
+#    endif
+#    ifndef MOUSEKEY_INITIAL_SPEED
+#        define MOUSEKEY_INITIAL_SPEED 100
+#    endif
+#    ifndef MOUSEKEY_BASE_SPEED
+#        define MOUSEKEY_BASE_SPEED 1000
+#    endif
+#    ifndef MOUSEKEY_DECELERATED_SPEED
+#        define MOUSEKEY_DECELERATED_SPEED 400
+#    endif
+#    ifndef MOUSEKEY_ACCELERATED_SPEED
+#        define MOUSEKEY_ACCELERATED_SPEED 3000
+#    endif
+
+
+// mouse scroll config
+#    ifndef MOUSEKEY_WHEEL_DELAY
+#        define MOUSEKEY_WHEEL_DELAY 20
+#    endif
+#    ifndef MOUSEKEY_WHEEL_DELTA
+#        define MOUSEKEY_WHEEL_DELTA 1
+#    endif
+#    ifndef MOUSEKEY_WHEEL_INTERVAL
+#        define MOUSEKEY_WHEEL_INTERVAL 50
+#    endif
+#    ifndef MOUSEKEY_WHEEL_MAX_SPEED
+#        define MOUSEKEY_WHEEL_MAX_SPEED 8
+#    endif
+#    ifndef MOUSEKEY_WHEEL_TIME_TO_MAX
+#        define MOUSEKEY_WHEEL_TIME_TO_MAX 80
+#    endif
+
+#    ifndef MOUSEKEY_WHEEL_INITIAL_MOVEMENTS
+#        define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 16
+#    endif
+#    ifndef MOUSEKEY_WHEEL_BASE_MOVEMENTS
+#        define MOUSEKEY_WHEEL_BASE_MOVEMENTS 32
+#    endif
+#    ifndef MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS
+#        define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48
+#    endif
+#    ifndef MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS
+#        define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8
+#    endif
+
 
 #ifndef DEBOUNCE
 #    define DEBOUNCE 5
