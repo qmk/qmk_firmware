02c3605b2f41a03814fb6779b679bb0f5a7c5976
Thys de Wet <thys@vectorphoto.co.uk>
2021-06-07 13:41:30 +0100

    Stuff

--- keyboards/spaceman/2_milk/keymaps/emoji/keymap.c
+++ keyboards/spaceman/2_milk/keymaps/emoji/keymap.c
@@ -12,7 +12,7 @@ void dance_key_one (qk_tap_dance_state_t *state, void *user_data) {
         reset_tap_dance (state);
     } else if (state->count == 2) {
         cycle_unicode_input_mode(+1);
-        reset_tap_dance (state);
+        reset_tap_dance (state);//
     }
 }
 
@@ -34,8 +34,7 @@ void dance_key_two (qk_tap_dance_state_t *state, void *user_data) {
         SEND_STRING(SS_TAP(X_ENTER));
         reset_tap_dance (state);
     } else if (state->count == 5) {
-        send_unicode_hex_string("0028 FE3A FE39 FE3A 0029"); // (︺︹︺)
-        SEND_STRING(SS_TAP(X_ENTER));
+        send_string("Mord@nt"); // (︺︹︺)
         reset_tap_dance (state);
     }
 }
--- users/drashna/pimoroni_trackball.h
+++ users/drashna/pimoroni_trackball.h
@@ -22,8 +22,8 @@
 #ifndef TRACKBALL_ADDRESS
 #    define TRACKBALL_ADDRESS 0x0A
 #endif
-#define TRACKBALL_WRITE ((TRACKBALL_ADDRESS << 1) | I2C_WRITE)
-#define TRACKBALL_READ ((TRACKBALL_ADDRESS << 1) | I2C_READ)
+#define TRACKBALL_WRITE ((TRACKBALL_ADDRESS << 1) | 0x00)
+#define TRACKBALL_READ ((TRACKBALL_ADDRESS << 1) | 0x01)
 
 void trackball_set_rgbw(uint8_t red, uint8_t green, uint8_t blue, uint8_t white);
 void trackball_check_click(bool pressed, report_mouse_t *mouse);
