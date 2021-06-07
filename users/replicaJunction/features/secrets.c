0533c34356f0096403c048df27fc1432c530c590
Joshua T <joshua@sonofone.net>
2021-06-07 11:25:11 -0500

    Add GPL2 headers

--- users/replicaJunction/features/secrets.c
+++ users/replicaJunction/features/secrets.c
@@ -1,3 +1,19 @@
+/* Copyright 2021 Joshua T.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Before you can compile with this feature, you'll need to manually
 // create a file in this directory called "secret_definitions.h"
 // containing the data to be added.
