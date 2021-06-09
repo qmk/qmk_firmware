c64c9cf02407b8debd6393996a3e65c49110e037
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2021-06-09 14:37:44 -0300

    Implement suggestions from code review by fauxpark

--- keyboards/noxary/378/rules.mk
+++ keyboards/noxary/378/rules.mk
@@ -1,7 +1,10 @@
 # MCU name
 MCU = STM32F072
 
-# Build Options
+# Bootloader selection
+BOOTLOADER = stm32-dfu
+
+# Build options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = full      # Virtual DIP switch configuration
@@ -15,8 +18,8 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
