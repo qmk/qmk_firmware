dea151cffd5ed4deb98499060c6ebf9ce9fb3bbc
jackytrabbit <jacky_lijun@126.com>
2021-06-09 12:23:32 +0800

    Delete the60.h

--- keyboards/lazydesigners/the60/the60.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "quantum.h"
-
-#if defined(KEYBOARD_lazydesigners_the60_rev1)
-    #include "rev1.h"
-#elif defined(KEYBOARD_lazydesigners_the60_rev2)
-    #include "rev2.h"
-#endif
