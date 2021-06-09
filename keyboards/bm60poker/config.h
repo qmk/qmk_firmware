79af853402d3f3d345b507634e93509908ef5d4c
Cebby <cebby2420@outlook.com>
2021-06-09 18:26:50 +0200

    Add Vial keymap for BM60Poker

--- keyboards/bm60poker/config.h
+++ keyboards/bm60poker/config.h
@@ -45,6 +45,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DIODE_DIRECTION COL2ROW
 
+#define VIAL_KEYBOARD_UID {0x1B, 0x11, 0x17, 0xDD, 0x7C, 0xD2, 0x16, 0xB2}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13}
+
 #define RGB_DI_PIN E2
 #define DRIVER_LED_TOTAL 67
 #ifdef RGB_DI_PIN
