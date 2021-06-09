1d07a1b22017d24730a39bf8fbe3e237e1e3e17e
Cebby <cebby2420@outlook.com>
2021-06-09 18:25:14 +0200

    Add Vial keymap for Primus75

--- keyboards/ilumkb/primus75/config.h
+++ keyboards/ilumkb/primus75/config.h
@@ -60,3 +60,8 @@
 #define NO_ACTION_FUNCTION
 
 #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
+
+/* Vial settings */
+#define VIAL_KEYBOARD_UID { 0x03, 0xB7, 0xC6, 0x97, 0x0B, 0xB1, 0x6B, 0x19 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15 }
