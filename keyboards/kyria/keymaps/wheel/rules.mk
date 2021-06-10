d3ef4a238a6b4153556fb9aaba57b4d97bd35acf
Will Tao <willtao@live.com>
2021-06-10 09:04:09 -0700

    update keymap; add bowser logo;

--- keyboards/kyria/keymaps/wheel/rules.mk
+++ keyboards/kyria/keymaps/wheel/rules.mk
@@ -2,4 +2,4 @@ OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = yes        # Enable the Leader Key feature
-MOUSEKEY_ENABLE = no
+MOUSEKEY_ENABLE = yes      # Enable mouse key features
