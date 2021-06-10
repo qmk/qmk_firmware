869102cf6f252b707ec8cb3120df95bcbaa8e233
Gigahawk <jasperchan515@gmail.com>
2021-06-09 18:40:25 -0700

    GMMK Pro RGB Support (#13147)
    * Enable SPI1 for GMMK pro
    
    * Setup initial boilerplate for new LED driver
    
    * RGB matrix minimally functional
    
    * Map full LED matrix
    
    * Return keymap to default
    
    * Fix printscreen LED mapping
    
    * Reduce max brightness
    
    * Default values for AW20216
    
    * Add documentation for AW20216
    
    * Disable console and warnings
    
    * Run cformat
    
    * Update drivers/awinic/aw20216.h
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * make aw struct match issi struct
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * add led location defines
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Use led pin definitions in keyboard.c
    
    * Add driver indices to led map
    
    * Fix elif typo
    
    * Run cformat
    
    * Update docs
    
    * Fix typo in docs
    
    * Document global brightness limits
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- quantum/rgb_matrix.h
+++ quantum/rgb_matrix.h
@@ -33,6 +33,8 @@
 #    include "is31fl3737.h"
 #elif defined(IS31FL3741)
 #    include "is31fl3741.h"
+#elif defined(AW20216)
+#    include "aw20216.h"
 #elif defined(WS2812)
 #    include "ws2812.h"
 #endif
