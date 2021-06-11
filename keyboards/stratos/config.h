7a27c13443d897e63911c1d63fb890c493ecb0a5
kb-elmo <lorwel@mailbox.org>
2021-06-11 11:41:55 +0200

    add RGB support

--- keyboards/stratos/config.h
+++ keyboards/stratos/config.h
@@ -40,3 +40,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
+
+/* RGB LEDs */
+#ifdef RGBLIGHT_ENABLE
+#define RGB_DI_PIN D2
+#define RGBLED_NUM 14
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_LIMIT_VAL 200
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_ANIMATIONS
+#endif
