cde28387dc893ef6c213b0c69f837abe567ec6f2
marby3 <64513524+marby3@users.noreply.github.com>
2021-06-11 21:26:40 +0900

    product ID

--- keyboards/alico48/config.h
+++ keyboards/alico48/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 yushakobo
+Copyright 2021 Marby
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,14 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x3265 //yushakobo
-#define PRODUCT_ID   0x0007
+#define VENDOR_ID    0xFEED
+#define PRODUCT_ID   0x0000
 #define DEVICE_VER   0x0001
-#define MANUFACTURER yushakobo
-#define PRODUCT      Provo50 single
+#define MANUFACTURER marby3
+#define PRODUCT      alico48
 
 /* key matrix size */
-#define MATRIX_ROWS 5
+#define MATRIX_ROWS 12
 #define MATRIX_COLS 10
 
 /*
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { E6, D7, C6, D4, D3 }
-#define MATRIX_COL_PINS { B4, B5, F4, F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { B6, B2, B3, B1, F7, F6, D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, D4, C6, D7, E6, B4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
@@ -62,16 +62,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define BACKLIGHT_LEVELS 3
 //#define BACKLIGHT_BREATHING
 
-//#define RGB_DI_PIN E2
-//#ifdef RGB_DI_PIN
-//#    define RGBLED_NUM 16
-//#    define RGBLIGHT_HUE_STEP 8
-//#    define RGBLIGHT_SAT_STEP 8
-//#    define RGBLIGHT_VAL_STEP 8
-//#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-//#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+#define RGB_DI_PIN D3
+#ifdef RGB_DI_PIN
+#    define RGBLED_NUM 24
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 /*== all animations enable ==*/
-//#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
 //#    define RGBLIGHT_EFFECT_BREATHING
 //#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
@@ -86,9 +86,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
 //#    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
 /*==== use exp() and sin() ====*/
-//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-//#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-//#endif
+//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 2  // 1 to 2.7
+//#    define RGBLIGHT_EFFECT_BREATHE_MAX    128   // 0 to 255
+#endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
