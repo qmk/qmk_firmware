186690806939d3019159a1b5b730ef8ca6a77618
jiaxin96 <572673807@qq.com>
2021-06-12 16:23:18 +0800

    ADD bs28 TEST

---
+++ keyboards/yandrstudio/whiteMouse28T/config.h
@@ -0,0 +1,74 @@
+/* Copyright 2021 JasonRen(biu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xAA96
+#define PRODUCT_ID      0xAA05
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    JasonRen biu
+#define PRODUCT         whiteMouse28T
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 7
+
+//                         0   1   2       3   4    5    6    7    8   9   10  11  12  13  14
+#define MATRIX_COL_PINS { B14, B15, A8, A9, A10, B4, B3}
+#define MATRIX_ROW_PINS { B13, B12, B7, B6}
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+
+// enable the nkro when using the VIA.
+#define FORCE_NKRO
+
+// fix VIA RGB_light
+#define VIA_HAS_BROKEN_KEYCODES
+
+/* define if matrix has ghost */
+//#define MATRIX_HAS_GHOST
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+
+
+/* RGN Matrix */
+#ifdef RGB_MATRIX_ENABLE
+
+#    define RGB_DI_PIN B5
+#    define RGBLED_NUM 2
+#    define DRIVER_LED_TOTAL RGBLED_NUM
+
+
+#    define WS2812_SPI SPID1 // default: SPID1
+#    define WS2812_SPI_MOSI_PAL_MODE 5
+
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
+#    define RGBLIGHT_VAL_STEP 18
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true
+#    define RGB_MATRIX_CENTER { 32, 96 }
+#    define RGB_MATRIX_KEYPRESSES
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+#endif
+
+
+
