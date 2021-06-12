bedd819bea6a58445a69cf5df44d9830aa0efca7
jiaxin96 <572673807@qq.com>
2021-06-13 00:16:29 +0800

    fix via col and row number for wm28

--- keyboards/yandrstudio/whiteMouse28T/keymaps/via/keymap.c
+++ keyboards/yandrstudio/whiteMouse28T/keymaps/via/keymap.c
@@ -18,9 +18,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT(
-    KC_GESC, KC_1,    KC_2,       KC_3,    KC_4,    KC_5,    KC_6,
+    KC_ESC,  KC_1,    KC_2,       KC_3,    KC_4,    KC_5,    KC_6,
     KC_TAB,  KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,
     KC_CAPS, KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,
-    KC_LCTL, KC_LALT, KC_LGUI,    KC_SPC,  KC_RALT, KC_TRNS, KC_RCTL)
-
+    KC_LSFT, KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N)
 };
