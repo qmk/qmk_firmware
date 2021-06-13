1378cd2268ed54a71a5eb9aadfa2dae7a05b8853
SergioPoverony <sergiopoverony@gmail.com>
2021-06-13 11:14:11 +0500

    Update rules.mk

--- keyboards/sergiopoverony/creator_pro/rules.mk
+++ keyboards/sergiopoverony/creator_pro/rules.mk
@@ -11,26 +11,8 @@ MCU = atmega32u4
 #   ATmega328P   USBasp
 BOOTLOADER = caterina
 
-EXTRAFLAGS += -flto
-
-#BOOTMAGIC_ENABLE = no     # Virtual DIP switch configuration
-#BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-TERMINAL_ENABLE = no
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no           # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output
-UNICODE_ENABLE = no
-ENCODER_ENABLE = yes
-HD44780_ENABLE = no
-FAUXCLICKY_ENABLE = no
-MIDI_ENABLE = no
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+CONSOLE_ENABLE  = yes
+COMMAND_ENABLE  = yes
+ENCODER_ENABLE  = yes
