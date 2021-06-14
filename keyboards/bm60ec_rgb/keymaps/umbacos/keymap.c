06bb570f3e65189d9977426b5fc3d6322c03d314
umbacos <umbacos@gmail.com>
2021-06-14 13:53:04 +0200

    fixed encoders

--- keyboards/bm60ec_rgb/keymaps/umbacos/keymap.c
+++ keyboards/bm60ec_rgb/keymaps/umbacos/keymap.c
@@ -65,14 +65,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-void encoder_update_kb(uint8_t index, bool clockwise) {
-    encoder_update_user(index, clockwise);
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    return encoder_update_user(index, clockwise);
 }
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLD);
+    } else {
+        tap_code(KC_VOLU);
+    }
+    return true;
 }
