880592bdfb4d03e7641589b6ea9077050e94a840
StefanGrindelwald <42373600+StefanGrindelwald@users.noreply.github.com>
2021-06-16 00:10:04 +0800

    Update keyboards/momoka_ergo/keymaps/default/keymap.c
    Co-authored-by: precondition <57645186+precondition@users.noreply.github.com>

--- keyboards/momoka_ergo/keymaps/default/keymap.c
+++ keyboards/momoka_ergo/keymaps/default/keymap.c
@@ -30,22 +30,22 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_ESC,    KC_1,    KC_2,    KC_3,  KC_4,    KC_5, KC_6,    KC_7,    KC_8,    KC_9,  KC_0,    KC_MINS,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F, KC_G, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT,    KC_Z,    KC_X, KC_C,  KC_V, KC_B, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        MO(_FN), KC_MINS,   KC_EQL, KC_LEFT, KC_RGHT, KC_LALT, KC_RGUI, KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(_FN),
-        KC_BSPC, KC_LCTL, KC_HOME,  KC_PGUP, KC_RCTL, KC_SPC,
-        KC_DEL, KC_END,  KC_PGDN, KC_ENT
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        MO(_FN), KC_MINS, KC_EQL,  KC_LEFT, KC_RGHT, KC_LALT, KC_RGUI, KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, MO(_FN),
+        KC_BSPC, KC_LCTL, KC_HOME, KC_PGUP, KC_RCTL, KC_SPC,
+        KC_DEL,  KC_END,  KC_PGDN, KC_ENT
     ),
     [_FN] = LAYOUT(
-        RGB_TOG,    RGB_MODE_FORWARD,    RGB_MODE_RAINBOW,    KC_9,  KC_0,    KC_MINS, KC_6,    KC_7,    KC_8,    KC_9,  KC_0,    KC_MINS,
-        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_RGUI, KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(_FN), KC_RGUI, KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(_FN),
-        KC_BSPC, KC_LCTL, KC_HOME,  KC_PGUP, KC_RCTL, KC_SPC,
-        KC_DEL, KC_END,  KC_PGDN, KC_ENT
+        RGB_TOG, RGB_MOD, RGB_M_R, KC_9,    KC_0,    KC_MINS, KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_RGUI, KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(_FN), KC_RGUI, KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, MO(_FN),
+        KC_BSPC, KC_LCTL, KC_HOME, KC_PGUP, KC_RCTL, KC_SPC,
+        KC_DEL,  KC_END,  KC_PGDN, KC_ENT
     )
 };
 /*
