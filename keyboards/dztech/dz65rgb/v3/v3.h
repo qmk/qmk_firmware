61cb11548dfaa79c14b70d3bb2d6996796f794dc
moyi4681 <moyi4681@users.noreply.github.com>
2021-06-15 08:56:33 +0800

    Update keyboards/dztech/dz65rgb/v3/v3.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dztech/dz65rgb/v3/v3.h
+++ keyboards/dztech/dz65rgb/v3/v3.h
@@ -15,8 +15,10 @@
  */
  
 #pragma once
-#define XXX KC_NO
+
 #include "quantum.h"
+
+#define XXX KC_NO
 
 #define LAYOUT_65_ansi( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
