9369df0e255a159ebfc616ccc8c11d6449b94092
umbacos <umbacos@gmail.com>
2021-06-15 23:23:28 +0200

    ok

--- keyboards/bm60ec_rgb/keymaps/umbacos/keymap.c
+++ keyboards/bm60ec_rgb/keymaps/umbacos/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    IT_MINS, IT_EQL,  KC_BSPC, KC_MUTE,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    IT_LBRC, IT_RBRC, IT_BSLS,
         SFT_T(KC_CAPS), KC_A,    KC_S,    LT(_NAVIGATOR, KC_D),    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    IT_SCLN, IT_QUOT,          KC_ENT,
-        MO(_SHIFT),     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MO(_SHIFT), KC_UP,   IT_SLSH,
+        LT(_SHIFT, KC_DEL),     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MO(_SHIFT), KC_UP,   IT_SLSH,
         KC_LCTL,        KC_LALT, KC_LGUI,                        LT(_ACCENT, KC_SPC),                    KC_RALT, MO(_SERVICE),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_SHIFT] = LAYOUT(
@@ -72,8 +72,10 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
         tap_code(KC_VOLD);
+        rgb_matrix_disable();
     } else {
         tap_code(KC_VOLU);
+        rgb_matrix_enable();
     }
     return true;
 }
