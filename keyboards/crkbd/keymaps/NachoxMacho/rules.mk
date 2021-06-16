2d249bde68afc657be040062bfcc1d9a4c0ebbdc
Rob Owens <rtowens17@gmail.com>
2021-06-15 21:48:30 -0500

    Delete keyboards/crkbd/keymaps/NachoxMacho directory

--- keyboards/crkbd/keymaps/NachoxMacho/rules.mk
+++
@@ -1,6 +0,0 @@
-# This enable the RGB lights
-RGBLIGHT_ENABLE = yes
-# This is the bootloader to use which enables the reset keycode
-BOOTLOADER = atmel-dfu
-# This enables the media keys
-EXTRAKEY_ENABLE = yes
