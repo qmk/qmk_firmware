9f6835c9284803e7253d72e6e0a4407edde2fca9
projectcain <71684775+projectcain@users.noreply.github.com>
2021-06-16 16:25:57 -0500

    Update keymap.c

--- keyboards/projectcain/vault45/keymaps/default/keymap.c
+++ keyboards/projectcain/vault45/keymaps/default/keymap.c
@@ -1,4 +1,16 @@
-/* Vault 45 Default */
+ /* Copyright 2021 projectcain 
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * */
+
 #include QMK_KEYBOARD_H
 enum layers{
   BASE,
