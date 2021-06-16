d67a94b555127a0518ce85d15cb35617abecb22a
talsu <hyoungjkim@ebay.com>
2021-06-17 08:29:37 +0900

    Delete unnecessary `#include`
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kmac_pad/matrix.c
+++ keyboards/kmac_pad/matrix.c
@@ -18,11 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdint.h>
 #include <stdbool.h>
 #include "wait.h"
-#include "print.h"
-#include "debug.h"
-#include "util.h"
 #include "matrix.h"
-#include "debounce.h"
 #include "quantum.h"
 
 static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
