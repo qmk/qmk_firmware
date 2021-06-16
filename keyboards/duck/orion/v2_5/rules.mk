5ac5be0a7e96e76aecbe1a30b1f4bf05f3b07f7a
Natac24 <85228624+Natac24@users.noreply.github.com>
2021-06-16 09:36:28 +0200

    Update rules.mk

--- keyboards/duck/orion/v2_5/rules.mk
+++ keyboards/duck/orion/v2_5/rules.mk
@@ -31,7 +31,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 RGBLIGHT_ENABLE = yes
 
-CUSTOM_MATRIX = lite
+CUSTOM_MATRIX = yes
 SRC += indicator_leds.c \
 			 matrix.c duck_led/duck_led.c
 
