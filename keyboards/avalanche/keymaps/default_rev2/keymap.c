2ce44c8e3415da2d23f43e373bb5bbe094094060
vitvlkv <vitvlkv@gmail.com>
2021-06-17 16:50:35 +0300

    Revert "Moved controls one row up (where used to be shifts)."
    This reverts commit 885c6ad444fdb1c774a9d93f0908c89a5f1c0ef7.

--- keyboards/avalanche/keymaps/default_rev2/keymap.c
+++ keyboards/avalanche/keymaps/default_rev2/keymap.c
@@ -17,7 +17,7 @@ enum layer {
 #define FN_3 LT(LAYER_3, KC_INS)
 #define LFN_3 LSFT_T(KC_EQL)
 #define RFN_3 RSFT_T(KC_MINS)
-#define KC_QTCT RCTL_T(KC_QUOT)
+#define KC_QTSH RSFT_T(KC_QUOT)
 #define EN_LALT LALT_T(KC_ENT)
 #define EN_RALT RALT_T(KC_ENT)
 
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_0] = LAYOUT(
                  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
                  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-        KC_LGUI, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QTCT, KC_RBRC,
-                 XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,  KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+        KC_LGUI, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QTSH, KC_RBRC,
+                 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,  KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
                                    KC__MUTE,KC_GRV,  LFN_3,   FN_2,    EN_LALT, EN_RALT, FN_1,    RFN_3,   FN_3,    KC_LSCR
     ),
     [LAYER_1] = LAYOUT(
