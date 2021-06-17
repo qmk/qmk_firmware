9475659fa72f5241115e0933fe7db4d1d14a5514
leonardusdipa <leonardus.dipa95@gmail.com>
2021-06-17 23:54:09 +0700

    Update config.h

--- keyboards/atreus/promicro/config.h
+++ keyboards/atreus/promicro/config.h
@@ -18,14 +18,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-
-#define VENDOR_ID       0x1209
-#define PRODUCT_ID      0xA1E5
-#define DEVICE_VER      0x0008
-#define MANUFACTURER    Technomancy
-#define PRODUCT         Atreus
-
 /*
  * Keyboard Matrix Assignments
  *
@@ -52,6 +44,3 @@
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
