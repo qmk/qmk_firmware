10aafd3bb4dc60fde209329004c9c2d86ffd0c92
Frooastside <47527944+Frooastside@users.noreply.github.com>
2021-06-17 19:01:40 +0200

    Update rules.mk

--- keyboards/frooastboard/rules.mk
+++ keyboards/frooastboard/rules.mk
@@ -1,6 +1,8 @@
 # MCU name
 MCU = atmega32u2
 
+BOOTLOADER = atmel-dfu
+
 # Processor frequency.
 #     This will define a symbol, F_CPU, in all source code files equal to the
 #     processor frequency in Hz. You can then use this symbol in your source code to
