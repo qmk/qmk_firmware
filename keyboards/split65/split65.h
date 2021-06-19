f4ab49762f785edf6d46da1680be358586b051c6
percyjw-2 <joris.wachsmuth@gmx.de>
2021-06-19 10:19:21 +0200

    Add Split65 Keyboard

---
+++ keyboards/split65/split65.h
@@ -0,0 +1,13 @@
+#ifndef SPLIT65_H
+#define SPLIT65_H
+
+#include "quantum.h"
+
+#if defined(KEYBOARD_split65_stm32)
+    #include "stm32.h"
+#elif defined(KEYBOARD_split65_promicro)
+    #include "promicro.h"      
+#endif
+
+#endif
+
---
+++ keyboards/split65/stm32/config.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "config_common.h"
+
+// USB Device descriptor parameter
+#define VENDOR_ID     0x1209
+#define PRODUCT_ID    0x0001
+#define DEVICE_VER    0x0001
+#define MANUFACTURER  percyjw
+#define PRODUCT       split65
+
+// Matrix Assignments
+#define MATRIX_ROWS 10 //Rows are doubled-up
+#define MATRIX_COLS 8
+
+#define MATRIX_COL_PINS {A2, A1, A0, B8, B13, B14, B15, B9}
+#define MATRIX_ROW_PINS {B5, B4, B3, B2, B1}
+
+// COL2ROW, ROW2COL
+#define DIODE_DIRECTION COL2ROW
+
+#define MATRIX_IO_DELAY 5
+
+// Split Keyboard specifics
+#define MASTER_LEFT
+#define SOFT_SERIAL_PIN A9
+#define SELECT_SOFT_SERIAL_SPEED 1
+#define SERIAL_USART_DRIVER SD1
+#define SERIAL_USART_TX_PAL_MODE 7
+
+// Debounce to reduce unintended double-presses use 0 if not needed
+#define DEBOUNCE 5
+
+// Feature diable options
+//#define NO_DEBUG
+//#define NO_PRINT
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
