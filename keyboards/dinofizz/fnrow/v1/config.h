f51c68cb719dc72c1bab2fff161ed67005474201
Dino Fizzotti <dino@dinofizzotti.com>
2021-06-19 10:25:54 +0100

    FnRow v1 config changes as suggested in PR

--- keyboards/dinofizz/fnrow/v1/config.h
+++ keyboards/dinofizz/fnrow/v1/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0100
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Dino Fizzotti
-#define PRODUCT         Dino Fizzotti FnRow v1
+#define PRODUCT         FnRow v1
 
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 7
