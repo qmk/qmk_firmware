744d89133737458c500c29fc1a1464c07690ca50
Rob Owens <rtowens17@gmail.com>
2021-06-20 09:29:47 -0500

    Apply suggestions from PR

--- keyboards/montex/keymaps/default/config.h
+++ keyboards/montex/keymaps/default/config.h
@@ -1,17 +1,17 @@
-/* Copyright 2021 NachoxMacho   
+/* Copyright 2021 NachoxMacho
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
 *   
-* This program is free software: you can redistribute it and/or modify   
-* it under the terms of the GNU General Public License as published by   
-* the Free Software Foundation, either version 2 of the License, or   
-* (at your option) any later version.   
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
 *   
-* This program is distributed in the hope that it will be useful,   
-* but WITHOUT ANY WARRANTY; without even the implied warranty of   
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   
-* GNU General Public License for more details.   
-*   
-* You should have received a copy of the GNU General Public License   
-* along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.   
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
 */
 
 #pragma once
@@ -22,6 +22,8 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
+#define MANUFACTURER    Idobao
+#define PRODUCT         Montex
 
 /* key matrix size */
 #define MATRIX_ROWS 6
--- keyboards/montex/keymaps/default/keymap.c
+++ keyboards/montex/keymaps/default/keymap.c
@@ -1,17 +1,17 @@
-/* Copyright 2021 NachoxMacho   
-*   
-* This program is free software: you can redistribute it and/or modify   
-* it under the terms of the GNU General Public License as published by   
-* the Free Software Foundation, either version 2 of the License, or   
-* (at your option) any later version.   
-*   
-* This program is distributed in the hope that it will be useful,   
-* but WITHOUT ANY WARRANTY; without even the implied warranty of   
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   
-* GNU General Public License for more details.   
-*   
-* You should have received a copy of the GNU General Public License   
-* along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.   
+/* Copyright 2021 NachoxMacho
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
 */
 
 #include QMK_KEYBOARD_H
@@ -21,15 +21,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───┌───┬───┬───┬───┐
      * │ ` │Esc│Tab│MO1│Bsp│
      * ├───┼───┼───┼───┼───┤
-     * │ , │Num│ / │ * │ - │
+     * │ , │Num│ / │ * │ - │
      * ├───┼───┼───┼───┼───┤
-     * │Gui│ 7 │ 8 │ 9 │   │
-     * ├───┼───┼───┼───┤ + │
-     * │Alt│ 4 │ 5 │ 6 │   │
+     * │Gui│ 7 │ 8 │ 9 │   │
+     * ├───┼───┼───┼───┤ + │
+     * │Alt│ 4 │ 5 │ 6 │   │
      * ├───┼───┼───┼───┼───┤
-     * │Shi│ 1 │ 2 │ 3 │   │
+     * │Shi│ 1 │ 2 │ 3 │   │
      * ├───┼───┴───┼───┤Ent│
-     * │Ctr│ 0     │ . │   │
+     * │Ctr│ 0     │ . │   │
      * └───┴───────┴───┴───┘
      */
     [0] = LAYOUT_numpad_6x5(
@@ -45,15 +45,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───┌───┐───┬───┬───┐
      * │Rst│ ` │Tab│MO1│Bsp│
      * ├───┼───┘───┼───┼───┤
-     * │ , │Num│ / │ * │ - │
+     * │ , │Num│ / │ * │ - │
      * ├───┼───┼───┼───┼───┤
-     * │Gui│Hom│ ↑ │PgU│   │
-     * ├───┼───┼───┼───┤ + │
-     * │Alt│ ← │   │ → │   │
+     * │Gui│Hom│ ↑ │PgU│   │
+     * ├───┼───┼───┼───┤ + │
+     * │Alt│ ← │   │ → │   │
      * ├───┼───┼───┼───┤───┤
-     * │Shi│End│ ↓ │PgD│   │
+     * │Shi│End│ ↓ │PgD│   │
      * ├───┼───┴───┼───┤Ent│
-     * │Ctr│Insert │Del│   │
+     * │Ctr│Insert │Del│   │
      * └───┴───────┴───┘───┘
      */
     [1] = LAYOUT_numpad_6x5(
