a3dbdc965028c890c58bd48caf2c595aa44ecc54
Drashna Jael're <drashna@live.com>
2021-06-20 08:31:44 -0700

    add additional layouts

--- keyboards/phase_studio/titan65/soldered/soldered.h
+++ keyboards/phase_studio/titan65/soldered/soldered.h
@@ -21,15 +21,71 @@
 #define ___ KC_NO
 
 #define LAYOUT_all( \
-	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f, \
-	k10,   k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,   k1e,   k1f, \
-	k20,     k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d,  k2e,  k2f, \
-	k30,  k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,    k3d,   k3e, k3f, \
-	k40, k42, k43,                k47,                   k4b, k4c,    k4d, k4e, k4f  \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e,  k0f, \
+    k10,   k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,   k1e,   k1f, \
+    k20,     k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d,  k2e,  k2f, \
+    k30,  k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,    k3d,   k3e, k3f, \
+    k40, k42, k43,                k47,                   k4b, k4c,    k4d, k4e, k4f  \
 ) { \
-	{ k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f }, \
-	{ k10, ___, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f }, \
-	{ k20, ___, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f }, \
-	{ k30, ___, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f }, \
-	{ k40, ___, k42, k43, ___, ___, ___, k47, ___, ___, ___, k4b, k4c, k4d, k4e, k4f }  \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f }, \
+    { k10, ___, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f }, \
+    { k20, ___, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f }, \
+    { k30, ___, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f }, \
+    { k40, ___, k42, k43, ___, ___, ___, k47, ___, ___, ___, k4b, k4c, k4d, k4e, k4f }  \
+}
+
+#define LAYOUT_ansi( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,    k0d,    k0f, \
+    k10,   k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,   k1e,   k1f, \
+    k20,     k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c,       k2e,  k2f, \
+    k30,       k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,    k3d,   k3e, k3f, \
+    k40, k42, k43,                k47,                   k4b, k4c,    k4d, k4e, k4f  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, ___, k0f }, \
+    { k10, ___, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f }, \
+    { k20, ___, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, ___, k2e, k2f }, \
+    { k30, ___, ___, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f }, \
+    { k40, ___, k42, k43, ___, ___, ___, k47, ___, ___, ___, k4b, k4c, k4d, k4e, k4f }  \
+}
+
+#define LAYOUT_ansi_split_bspc( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e,  k0f, \
+    k10,   k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,   k1e,   k1f, \
+    k20,     k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c,       k2e,  k2f, \
+    k30,       k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,    k3d,   k3e, k3f, \
+    k40, k42, k43,                k47,                   k4b, k4c,    k4d, k4e, k4f  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f }, \
+    { k10, ___, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f }, \
+    { k20, ___, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, ___, k2e, k2f }, \
+    { k30, ___, ___, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f }, \
+    { k40, ___, k42, k43, ___, ___, ___, k47, ___, ___, ___, k4b, k4c, k4d, k4e, k4f }  \
+}
+
+#define LAYOUT_iso( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,    k0d,    k0f, \
+    k10,   k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,          k1f, \
+    k20,     k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d,  k2e,  k2f, \
+    k30,  k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,    k3d,   k3e, k3f, \
+    k40, k42, k43,                k47,                   k4b, k4c,    k4d, k4e, k4f  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, ___, k0f }, \
+    { k10, ___, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, ___, k1f }, \
+    { k20, ___, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f }, \
+    { k30, ___, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f }, \
+    { k40, ___, k42, k43, ___, ___, ___, k47, ___, ___, ___, k4b, k4c, k4d, k4e, k4f }  \
+}
+
+#define LAYOUT_iso_split_bspc( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e,  k0f, \
+    k10,   k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d,          k1f, \
+    k20,     k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d,  k2e,  k2f, \
+    k30,  k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c,    k3d,   k3e, k3f, \
+    k40, k42, k43,                k47,                   k4b, k4c,    k4d, k4e, k4f  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f }, \
+    { k10, ___, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, ___, k1f }, \
+    { k20, ___, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f }, \
+    { k30, ___, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f }, \
+    { k40, ___, k42, k43, ___, ___, ___, k47, ___, ___, ___, k4b, k4c, k4d, k4e, k4f }  \
 }
