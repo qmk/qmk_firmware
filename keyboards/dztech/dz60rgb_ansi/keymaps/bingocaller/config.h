f3b8d8f3bd4f4d4082622125c6607b938cddacb2
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2021-06-20 02:27:28 +0200

    Create bingocaller keymap for DZ60RGB-ANSI
    This is more or less a copy of the existing DZ60 bingocaller keymap but
    with a few adjustments to the keymap itself, plus Mouse Key tweaks, and
    an updated README.

---
+++ keyboards/dztech/dz60rgb_ansi/keymaps/bingocaller/config.h
@@ -0,0 +1,9 @@
+#define MOUSEKEY_DELAY 0 // default is 300ms
+#define MOUSEKEY_INTERVAL 16 // ~ 60Hz (1/60)
+#define MOUSEKEY_MAX_SPEED 8 // default is 10; the lower interval, the lower this ought to be
+#define MOUSEKEY_TIME_TO_MAX 15 // default is 20
+
+#define MOUSEKEY_WHEEL_DELAY 0 // default is 300ms
+#define MOUSEKEY_WHEEL_INTERVAL 50 // default is 100ms
+#define MOUSEKEY_WHEEL_MAX_SPEED 10 // default is 8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 30 // default is 40
