dbe8098178abaf7bd95fc369904aecaac58a32d1
Luigi311 <luigi311.lg@gmail.com>
2021-06-21 11:31:34 -0600

    Streamcheap personal
    Signed-off-by: Luigi311 <luigi311.lg@gmail.com>

--- keyboards/handwired/stream_cheap/2x4/config.h
+++ keyboards/handwired/stream_cheap/2x4/config.h
@@ -3,19 +3,23 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x1214
+#define VENDOR_ID       0x4C47 // LG
+#define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Kyle Hart
-#define PRODUCT         Stream Cheap 2x4
+#define MANUFACTURER    Luigi311
+#define PRODUCT         Stream Cheap
 
 /* key matrix size */
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 4
 
 /* define direct pins used */
-#define DIRECT_PINS { { B5,D7,C6,D1}, { B4,E6,D4,D0} }
-
+//Luigi311
+//#define DIRECT_PINS { { B4, E6, D4, D0}, { B5, D7, C6, D1} }
+//Darklegacy
+//#define DIRECT_PINS { { B4, D4, D7, D1}, { B5, C6, D0, E6} }
+//Vancurls Craigerbomb Scuhzy
+//#define DIRECT_PINS { { D7, E6, B3, B1}, { B5, B4, B2, B6} }
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
