cc9eaeb8a517d2ebf69afd2a58250d8ce9387e43
Stefan Kerkmann <karlk90@pm.me>
2021-06-21 20:05:16 +0200

    Increase default serial TX/RX buffer size to 128 bytes

--- platforms/chibios/QMK_PROTON_C/configs/halconf.h
+++ platforms/chibios/QMK_PROTON_C/configs/halconf.h
@@ -412,7 +412,7 @@
  *          buffers.
  */
 #if !defined(SERIAL_BUFFERS_SIZE) || defined(__DOXYGEN__)
-#define SERIAL_BUFFERS_SIZE                 16
+#define SERIAL_BUFFERS_SIZE                 128
 #endif
 
 /*===========================================================================*/
