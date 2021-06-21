931c0ce61dbf7e68f0420e90c1e6f0b7352d41f2
4pplet <4pplet@protonmail.com>
2021-06-21 22:55:50 +0200

    Update keyboards/4pplet/eagle_viper_rep/rev_a/config.h
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- keyboards/4pplet/eagle_viper_rep/rev_a/config.h
+++ keyboards/4pplet/eagle_viper_rep/rev_a/config.h
@@ -43,6 +43,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN  A7
 #define WS2812_SPI  SPID1
 #define WS2812_SPI_MOSI_PAL_MODE 0
+#define WS2812_SPI_SCK_PIN A5
+#define WS2812_SPI_SCK_PAL_MODE 0
 #define RGBLED_NUM  16
 #define RGBLIGHT_ANIMATIONS
 
@@ -68,4 +70,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Added extra layer for use of layer leds */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-
