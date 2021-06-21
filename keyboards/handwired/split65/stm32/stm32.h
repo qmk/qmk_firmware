5ecb0311751e78913349aec44064d31e503f07a4
Joris Wachsmuth <52176899+PercyJW-2@users.noreply.github.com>
2021-06-21 18:56:09 +0200

    Update keyboards/handwired/split65/stm32/stm32.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/split65/stm32/stm32.h
+++ keyboards/handwired/split65/stm32/stm32.h
@@ -13,8 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef STM_32
-#define STM32
+#pragma once
 
 #include "split65.h"
 
