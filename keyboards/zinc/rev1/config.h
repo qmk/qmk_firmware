aab37e7b6eec30a0bf8e6eb775a48d6918e37872
monksoffunk <osamu@sho-k.com>
2021-06-21 22:22:32 +0900

    Fix / Add copyright credit

--- keyboards/zinc/rev1/config.h
+++ keyboards/zinc/rev1/config.h
@@ -1,6 +1,5 @@
 /*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+Copyright 2018 monksoffunk
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
