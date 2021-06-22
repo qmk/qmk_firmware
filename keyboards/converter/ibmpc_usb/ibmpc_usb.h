908bf3a0a59c9a61a219438f708697aaa1772e6f
Markus Fritsche <mfritsche@reauktion.de>
2021-06-23 00:00:47 +0200

    created translation matrix

--- keyboards/converter/ibmpc_usb/ibmpc_usb.h
+++ keyboards/converter/ibmpc_usb/ibmpc_usb.h
@@ -41,16 +41,16 @@ PF9, PF10.    LCL,      LAT,                     SPC,           RAT,            
 
 */
 #define LAYOUT_127(\
-                   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
-                   K0C, K0D, K0E, K0F, K10, K11, K12, K13, K14, K15, K16, K17, \
+                   K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
+                   K0D, K0E, K0F, K10, K11, K12, K13, K14, K15, K16, K17, K18, \
 \
-K18, K19,   K1A, K1B, K1C, K1D, K1E, K1F, K20, K21, K22, K23, K24, K25, K26, K27, K28,   K29, K2A, K2B,   K2C, K2D, K2E, K2F, \
-K30, K31,   K32,  K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E,     K3F,   K40, K41, K42,   K43, K44, K45, K46, \
-K47, K48,   K49,   K4A, K4B, K4C, K4D, K4E, K4F, K50, K51, K52, K53, K54, K55,    K56,        K57,        K58, K59, K5A, K5B, \
-K5C, K5D,   K5E,  K5F, K60, K61, K62, K63, K64, K65, K66, K67, K68, K69, K6A,     K6B,   K6C, K6D, K6E,   K6F, K70, K71, K72, \
-K73, K74,   K75,       K76,               K77,                        K78,        K79,        K7A,        K7B, K7C, K7D, K7E  \
+K19, K1A,   K1B, K1C, K1D, K1E, K1F, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29,   K2A, K2B, K2C,   K2D, K2E, K2F, K30, \
+K31, K32,   K33,  K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, K3F,     K40,   K41, K42, K43,   K44, K45, K46, K47, \
+K48, K49,   K4A,   K4B, K4C, K4D, K4E, K4F, K50, K51, K52, K53, K54, K55, K56,    K57,        K58,        K59, K5A, K5B, K5C, \
+K5D, K5E,   K5F, K60, K61, K62, K63, K64, K65, K66, K67, K68, K69, K6A,  K6B,     K6C,   K6D, K6E, K6F,   K70, K71, K72, K73, \
+K74, K75,   K76,       K77,               K78,                        K79,        K7A,        K7B,        K7C, K7D, K7E, K7F  \
 ) { \
- { K00, K01, K02, K03, K04, K05, K06, K07 }, \
+ { XXX, K01, K02, K03, K04, K05, K06, K07 }, \
  { K08, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
  { K10, K11, K12, K13, K14, K15, K16, K17 }, \
  { K18, K19, K1A, K1B, K1C, K1D, K1E, K1F }, \
@@ -65,86 +65,7 @@ K73, K74,   K75,       K76,               K77,                        K78,      
  { K60, K61, K62, K63, K64, K65, K66, K67 }, \
  { K68, K69, K6A, K6B, K6C, K6D, K6E, K6F }, \
  { K70, K71, K72, K73, K74, K75, K76, K77 }, \
- { K78, K79, K7A, K7B, K7C, K7D, K7E, XXX } \
-}
-
-#define LAYOUT_105( \
-    K76,    K05,K06,K04,K0C,K03,K0B,K02,K0A,K01,K09,K78,K07, K7F,K7E,K00,                   \
-    K0E,K16,K1E,K26,K25,K2E,K36,K3D,K3E,K46,K45,K4E,K55,K66, K39,K2F,K5E,  K77,K60,K7C,K7B, \
-    K0D,K15,K1D,K24,K2D,K2C,K35,K3C,K43,K44,K4D,K54,K5B,     K37,K27,K56,  K6C,K75,K7D,     \
-    K58,K1C,K1B,K23,K2B,K34,K33,K3B,K42,K4B,K4C,K52,K5D,K5A,               K6B,K73,K74,K79, \
-    K12,K61,K1A,K22,K21,K2A,K32,K31,K3A,K41,K49,K4A,    K59,     K4F,      K69,K72,K7A,     \
-    K14,K19,K11,        K29,                K0F,K1F,K5C,K17, K53,K3F,K47,  K70,    K71,K62  \
-) { \
-    { K00, K01, K02, K03, K04, K05, K06, K07 }, \
-    { XXX, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
-    { XXX, K11, K12, XXX, K14, K15, K16, K17 }, \
-    { XXX, K19, K1A, K1B, K1C, K1D, K1E, K1F }, \
-    { XXX, K21, K22, K23, K24, K25, K26, K27 }, \
-    { XXX, K29, K2A, K2B, K2C, K2D, K2E, K2F }, \
-    { XXX, K31, K32, K33, K34, K35, K36, K37 }, \
-    { XXX, K39, K3A, K3B, K3C, K3D, K3E, K3F }, \
-    { XXX, K41, K42, K43, K44, K45, K46, K47 }, \
-    { XXX, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
-    { XXX, XXX, K52, K53, K54, K55, K56, XXX }, \
-    { K58, K59, K5A, K5B, K5C, K5D, K5E, XXX }, \
-    { K60, K61, K62, XXX, XXX, XXX, K66, XXX }, \
-    { XXX, K69, XXX, K6B, K6C, XXX, XXX, XXX }, \
-    { K70, K71, K72, K73, K74, K75, K76, K77 }, \
-    { K78, K79, K7A, K7B, K7C, K7D, K7E, K7F }  \
-}
-
-#define LAYOUT_CS3( \
-                  K08,K10,K18,K20,K28,K30,K38,K40,K48,K50,K57,K5F,                                         \
-                  K07,K0F,K17,K1F,K27,K2F,K37,K3F,K47,K4F,K56,K5E,                                         \
-                                                                                                           \
-    K05,K06,  K0E,K16,K1E,K26,K25,K2E,K36,K3D,K3E,K46,K45,K4E,K55,K5D,K66,  K67,K6E,K6F,  K76,K77,K7E,K7F, \
-    K04,K0C,  K0D,K15,K1D,K24,K2D,K2C,K35,K3C,K43,K44,K4D,K54,K5B,    K5C,  K64,K65,K6D,  K6C,K75,K7D,K7C, \
-    K03,K0B,  K14,K1C,K1B,K23,K2B,K34,K33,K3B,K42,K4B,K4C,K52,    K53,K5A,      K63,      K6B,K73,K74,K7B, \
-    K02,K0A,  K12,K13,K1A,K22,K21,K2A,K32,K31,K3A,K41,K49,K4A,    K51,K59,  K61,K62,K6A,  K69,K72,K7A,K79, \
-    K01,K09,  K11,    K19,            K29,                    K39,    K58,      K60,      K68,K70,K71,K78  \
-) { \
-    { XXX, K01,   K02,   K03,   K04,   K05,   K06,   K07   }, \
-    { K08, K09,   K0A,   K0B,   K0C,   K0D,   K0E,   K0F   }, \
-    { K10, K11,   K12,   K13,   K14,   K15,   K16,   K17   }, \
-    { K18, K19,   K1A,   K1B,   K1C,   K1D,   K1E,   K1F   }, \
-    { K20, K21,   K22,   K23,   K24,   K25,   K26,   K27   }, \
-    { K28, K29,   K2A,   K2B,   K2C,   K2D,   K2E,   K2F   }, \
-    { K30, K31,   K32,   K33,   K34,   K35,   K36,   K37   }, \
-    { K38, K39,   K3A,   K3B,   K3C,   K3D,   K3E,   K3F   }, \
-    { K40, K41,   K42,   K43,   K44,   K45,   K46,   K47   }, \
-    { K48, K49,   K4A,   K4B,   K4C,   K4D,   K4E,   K4F   }, \
-    { K50, K51,   K52,   K53,   K54,   K55,   K56,   K57   }, \
-    { K58, K59,   K5A,   K5B,   K5C,   K5D,   K5E,   K5F   }, \
-    { K60, K61,   K62,   K63,   K64,   K65,   K66,   K67   }, \
-    { K68, K69,   K6A,   K6B,   K6C,   K6D,   K6E,   K6F   }, \
-    { K70, K71,   K72,   K73,   K74,   K75,   K76,   K77   }, \
-    { K78, K79,   K7A,   K7B,   K7C,   K7D,   K7E,   K7F   }  \
-}
-
-#define LAYOUT_xt( \
-    K3B, K3C,   K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K45,      K46, \
-    K3D, K3E,   K0F, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B,      K47, K48, K49, K4A, \
-    K3F, K40,   K1D, K1E, K1F, K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K1C, K4B, K4C, K4D, \
-    K41, K42,   K2A, K2B, K2C, K2D, K2E, K2F, K30, K31, K32, K33, K34, K35, K36, K37, K4F, K50, K51, K4E, \
-    K43, K44,   K38,                          K39,                     K3A,      K52,      K53 \
-) { \
-    { XXX, K01, K02, K03, K04, K05, K06, K07 }, \
-    { K08, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17 }, \
-    { K18, K19, K1A, K1B, K1C, K1D, K1E, K1F }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27 }, \
-    { K28, K29, K2A, K2B, K2C, K2D, K2E, K2F }, \
-    { K30, K31, K32, K33, K34, K35, K36, K37 }, \
-    { K38, K39, K3A, K3B, K3C, K3D, K3E, K3F }, \
-    { K40, K41, K42, K43, K44, K45, K46, K47 }, \
-    { K48, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
-    { K50, K51, K52, K53, XXX, XXX, XXX, XXX }, \
-    { XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX }, \
-    { XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX }, \
-    { XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX }, \
-    { XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX }, \
-    { XXX, XXX, XXX, XXX, XXX, XXX, XXX, XXX } \
+ { K78, K79, K7A, K7B, K7C, K7D, K7E, K7F } \
 }
 
 typedef enum { NONE, PC_XT, PC_AT, PC_TERMINAL, PC_AT_Z150 } keyboard_kind_t;
@@ -156,5 +77,44 @@ typedef enum { NONE, PC_XT, PC_AT, PC_TERMINAL, PC_AT_Z150 } keyboard_kind_t;
 
 extern uint16_t keyboard_id;
 extern keyboard_kind_t keyboard_kind;
+/*
+const uint8_t PROGMEM map_cs1[MATRIX_ROWS][MATRIX_COLS] = {
+    { XXX, 0x1A, KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6     },  00-07 */
+/*    { KC_7,     KC_8,     KC_9,     KC_0,     KC_MINUS, KC_EQUAL, KC_BSPACE,KC_TAB   },  08-0F */
+/*    { KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I     },  10-17 */
+/*    { KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_ENTER, KC_LCTL,  KC_A,     KC_S,    },  18-1F */
+/*    { KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN  },  20-27 */
+/*    { KC_QUOTE, KC_GRAVE, KC_LSHIFT,KC_BSLASH,KC_Z,     KC_X,     KC_C,     KC_V,    },  28-2F */
+/*    { KC_B,     KC_N,     KC_M,     KC_COMMA, KC_DOT,   KC_SLASH, KC_RSHIFT,KC_PAST  },  30-37 */
+/*    { KC_LALT,  KC_SPACE, KC_CAPS,  0x0D,    KC_F2,    KC_F3,    KC_F4,    KC_F5    },  38-3F */
+/*    { KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_NLCK,  KC_SLCK,  KC_P7    },  40-47 */
+/*    { KC_P8,    KC_P9,    KC_PMNS,  KC_P4,    KC_P5,    KC_P6,    KC_PPLS,  KC_P1    },  48-4F */
+/*    { KC_P2,    KC_P3,    KC_P0,    KC_PDOT,  KC_PSCR,  KC_PAUSE, KC_NUHS,  KC_F11   },  50-57 */
+/*    { KC_F12,   KC_PEQL,  KC_LGUI,  KC_RGUI,  KC_APP,   KC_MUTE,  KC_VOLD,  KC_VOLU  },  58-5F */
+/*    { KC_UP,    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_F13,   KC_F14,   KC_F15,   KC_F16   },  60-67 */
+/*    { KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,   KC_PENT  },  68-6F */
+/*    { KC_KANA,  KC_INSERT,KC_DELETE,KC_RO,    KC_HOME,  KC_END,   KC_F24,   KC_PGUP  },  70-77 */
+/*    { KC_PGDN,  KC_HENK,  KC_RCTL,  KC_MHEN,  KC_RALT,  KC_JYEN,  KC_PCMM,  KC_PSLS  },  78-7F */
+/*};*/
 
+/*
+const uint8_t PROGMEM map_cs3[MATRIX_ROWS][MATRIX_COLS] = {
+    { XXX,  0x73, 0x5C, 0x47, 0x30, 0x18, 0x19, 0x00 },  00-07
+    { 0x0C, 0x74, 0x5D, 0x48, 0x31, 0x32, 0x1A, 0x01 },  08-0F
+    { 0x0D, 0x75, 0x5E, 0x5F, 0x49, 0x33, 0x1B, 0x02 },  10-17
+    { 0x0E, 0x76, 0x60, 0x4B, 0x4A, 0x34, 0x1C, 0x03 },  18-1F
+    { 0x1F, 0x62, 0x61, 0x4C, 0x35, 0x1E, 0x1D, 0x04 },  20-27
+    { 0x10, 0x77, 0x63, 0x4D, 0x37, 0x36, 0x1F, 0x05 },  28-2F
+    { 0x11, 0x65, 0x64, 0x4F, 0x4E, 0x38, 0x20, 0x06 },  30-37
+    { 0x12, 0x78, 0x66, 0x50, 0x39, 0x21, 0x22, 0x07 },  38-3F
+    { 0x13, 0x67, 0x51, 0x3A, 0x3B, 0x24, 0x23, 0x08 },  40-47
+    { 0x14, 0x68, 0x69, 0x52, 0x53, 0x3C, 0x25, 0x09 },  48-4F
+    { 0x15, 0x6A, 0x54, 0x55, 0x3D, 0x26, 0x0A, 0x16 },  50-57
+    { 0x79, 0x6B, 0x56, 0x3E, 0x3F, 0x27, 0x0B, 0x17 },  58-5F
+    { 0x7A, 0x6C, 0x6D, 0x57, 0x40, 0x41, 0x28, 0x29 },  60-67
+    { 0x7B, 0x6F, 0x6E, 0x58, 0x43, 0x42, 0x2A, 0x2B },  68-6F
+    { 0x7C, 0x7D, 0x70, 0x59, 0x5A, 0x44, 0x2C, 0x2D },  70-77
+    { 0x7E, 0x72, 0x71, 0x5B, 0x46, 0x45, 0x2E, 0x2F },  78-7F
+};
+*/
 #endif
--- keyboards/converter/ibmpc_usb/keymaps/marfrit/keymap.c
+++ keyboards/converter/ibmpc_usb/keymaps/marfrit/keymap.c
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24,
                         KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
 
-    RESET  ,KC_ESC,     KC_ESC,  KC_1,        KC_2,         KC_3,         KC_4,         KC_5,    KC_6,    KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS, KC_EQL,  KC_NO,   KC_BSPC,    KC_INS,  KC_HOME,KC_PGUP,    KC_NLCK,KC_PSLS,KC_PAST,KC_PMNS,
+    RESET  ,KC_ESC,     KC_GRV,  KC_1,        KC_2,         KC_3,         KC_4,         KC_5,    KC_6,    KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS, KC_EQL,  KC_BSLS,   KC_BSPC,    KC_INS,  KC_HOME,KC_PGUP,    KC_NLCK,KC_PSLS,KC_PAST,KC_PMNS,
     KC_SLCK,KC_MPLY,    KC_TAB,  ALL_T(KC_K), KC_U,         KC_Q,         KC_DOT,       KC_J,    KC_V,    KC_G,         KC_C,         KC_L,         KC_F,         KC_LBRC, KC_RBRC,          KC_NO,      KC_DEL,  KC_END, KC_PGDN,    KC_P7,  KC_P8,  KC_P9,  KC_PPLS,
     KC_PAUS,KC_INT5,    KC_LCTL, KC_H,        LALT_T(KC_I), LCTL_T(KC_E), LSFT_T(KC_A), KC_O,    KC_D,    RSFT_T(KC_T), RCTL_T(KC_R), LALT_T(KC_N), KC_S,         KC_QUOT, KC_BSLS, KC_ENT,                       KC_UP,              KC_P4,  KC_P5,  KC_P6,  KC_PCMM,
-    KC_APP, KC_INT6,    KC_LSFT, KC_NUBS,     KC_X,         RALT_T(KC_Y), KC_SCLN,      KC_COMM, KC_SLSH, KC_B,         KC_P,         KC_W,         RALT_T(KC_M), KC_Z,    KC_NO,   KC_RSFT,             KC_LEFT, KC_INT2,KC_RGHT,    KC_P1,  KC_P2,  KC_P3,  KC_PENT,
+    KC_APP, KC_PSCR,    KC_LSFT, KC_NUBS,     KC_X,         RALT_T(KC_Y), KC_SCLN,      KC_COMM, KC_SLSH, KC_B,         KC_P,         KC_W,         RALT_T(KC_M), KC_Z,    KC_NO,   KC_RSFT,             KC_LEFT, KC_INT2,KC_RGHT,    KC_P1,  KC_P2,  KC_P3,  KC_PENT,
     KC_RGUI,KC_LGUI,    KC_CAPS,              KC_LALT,                                           KC_SPC,                                            KC_RALT,                        KC_RCTL,                      KC_DOWN,            KC_NO,  KC_P0,  KC_PDOT,KC_NO
 
     ),
@@ -58,27 +58,4 @@ void keyboard_post_init_user(void) {
   //debug_matrix=true;
   //debug_keyboard=true;
   //debug_mouse=true;
-}
-
-//action_t action_for_key(uint8_t layer, keypos_t key)
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    uint8_t unimap_pos;
-    switch (keyboard_kind) {
-        case PC_XT:
-            unimap_pos = pgm_read_byte(&map_cs1[key.row][key.col]);
-            break;
-        case PC_AT:
-            unimap_pos = pgm_read_byte(&map_cs2[key.row][key.col]);
-            break;
-        case PC_TERMINAL:
-            unimap_pos = pgm_read_byte(&map_cs3[key.row][key.col]);
-            break;
-        default:
-            return (action_t)ACTION_NO;
-    }
-
-    if (unimap_pos == UNIMAP_NO) return (action_t)ACTION_NO;
-
-    return (action_t)pgm_read_word(&actionmaps[(layer)][(unimap_pos & 0x70) >> 4][(unimap_pos & 0x0f)]);
 }
