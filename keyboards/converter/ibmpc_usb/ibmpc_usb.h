3f51336ca131657e4b93c108baec2073a971b633
Markus Fritsche <fritsche.markus@gmail.com>
2021-06-23 13:04:22 +0200

    change layout name

--- keyboards/converter/ibmpc_usb/ibmpc_usb.h
+++ keyboards/converter/ibmpc_usb/ibmpc_usb.h
@@ -40,7 +40,7 @@ PF7, PF8,     LSF, <,   Z,   X,   C,   V,   B,   N,   M,   ,,   .,   /,   STB   
 PF9, PF10.    LCL,      LAT,                     SPC,           RAT,               RCL,         DN,         STB, 0,   DOT
 
 */
-#define LAYOUT_127(\
+#define LAYOUT(\
                    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
                    K0D, K0E, K0F, K10, K11, K12, K13, K14, K15, K16, K17, K18, \
 \
