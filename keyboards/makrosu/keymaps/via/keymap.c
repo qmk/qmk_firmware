ced7e9a0ced2fc394eca1cc9e935caa0317b7172
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2021-06-23 13:30:42 +1000

    [Keyboard][Bug] Makrosu VIA compile fix encoder and formatting (#13306)
    * fix encoder and formatting
    
    * remove redundant breaks
    
    * Update keyboards/makrosu/keymaps/default/keymap.c
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/makrosu/keymaps/via/keymap.c
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/makrosu/keymaps/via/keymap.c
+++ keyboards/makrosu/keymaps/via/keymap.c
@@ -1,17 +1,17 @@
-/* Copyright 2021 Valdydesu_ 
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+/* Copyright 2021 Valdydesu_
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
 
 #include QMK_KEYBOARD_H
@@ -29,7 +29,7 @@ enum planck_keycodes {
   L1 = SAFE_RANGE,
   L2,
   L3
-  
+
 };
 
 #define LOWER MO(_4)
@@ -39,20 +39,20 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[_1] = LAYOUT(
-		KC_ESC, KC_F1, KC_F2,
+    [_1] = LAYOUT(
+        KC_ESC, KC_F1, KC_F2,
         LOWER, KC_Z, KC_X),
 
-	[_2] = LAYOUT(
-		LALT(KC_TAB), LGUI(KC_TAB), LCTL(KC_S),
+    [_2] = LAYOUT(
+        LALT(KC_TAB), LGUI(KC_TAB), LCTL(KC_S),
         LOWER, LCTL(KC_C), LCTL(KC_V)),
 
-	[_3] = LAYOUT(
-		KC_TRNS, KC_TRNS, KC_TRNS,
+    [_3] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS),
 
-	[_4] = LAYOUT(
-		L1, L2, L3,
+    [_4] = LAYOUT(
+        L1, L2, L3,
         _______, _______, _______),
 
 };
@@ -61,38 +61,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, _4, X_PAUSE, X_PAUSE);
     writePin(IND_1, layer_state_cmp(state, 1));
-	writePin(IND_2, layer_state_cmp(state, 2));
- 	writePin(IND_3, layer_state_cmp(state, 3)); 
- 	return state;
+    writePin(IND_2, layer_state_cmp(state, 2));
+    writePin(IND_3, layer_state_cmp(state, 3));
+    return state;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case L1:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_1);
-	
-
-      }
-      return false;
-      break;
-    case L2:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_2);
-	
-      }
-      return false;
-      break;
-    case L3:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_3);
-	
-      }
-      return false;
-      break;
-    
-  }
-  return true;
+    switch (keycode) {
+        case L1:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_1);
+            }
+            return false;
+        case L2:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_2);
+            }
+            return false;
+        case L3:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_3);
+            }
+            return false;
+    }
+    return true;
 }
 
 void matrix_init_user(void) {
@@ -106,28 +98,25 @@ void matrix_init_user(void) {
     writePinHigh(IND_3);
 }
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (layer_state_is(_1)) {
-	if (clockwise) {
-      tap_code(KC_UP);
-    } else {
-      tap_code(KC_DOWN);
+        if (clockwise) {
+            tap_code(KC_UP);
+        } else {
+        tap_code(KC_DOWN);
+        }
+    } else if (layer_state_is(_2)) {
+        if (clockwise) {
+            tap_code(KC_RGHT);
+        } else {
+            tap_code(KC_LEFT);
+        }
+    } else if (layer_state_is(_3)) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
     }
-   }
-
-    else if (layer_state_is(_2)) {
-	if (clockwise) {
-      tap_code(KC_RGHT);
-    } else {
-      tap_code(KC_LEFT);
-    }
-   }
-
-    else if (layer_state_is(_3)) {
-	if (clockwise) {
-      tap_code(KC_VOLU);
-    } else {
-      tap_code(KC_VOLD);
-    }
-   }
+    return true;
 }
