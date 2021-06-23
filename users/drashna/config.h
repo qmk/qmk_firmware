818665f3d45d1a61864585e92f0a0cc1ee50defd
Drashna Jael're <drashna@live.com>
2021-06-22 17:14:47 -0700

    Fix compiler issues

--- users/drashna/config.h
+++ users/drashna/config.h
@@ -221,31 +221,76 @@
 
 #ifdef MOUSEKEY_ENABLE
 // mouse movement config
+#    ifdef MK_3_SPEED
+#        undef MK_3_SPEED
+#    endif
 #    define MK_KINETIC_SPEED
 #    ifdef MK_KINETIC_SPEED
-#        define MOUSEKEY_DELAY      8
-#        define MOUSEKEY_INTERVAL   20
-#        define MOUSEKEY_MOVE_DELTA 25
+#        ifndef MOUSEKEY_DELAY
+#            define MOUSEKEY_DELAY 8
+#        endif
+#        ifndef MOUSEKEY_INTERVAL
+#            define MOUSEKEY_INTERVAL 20
+#        endif
+#        ifdef MOUSEKEY_MOVE_DELTA
+#            define MOUSEKEY_MOVE_DELTA 25
+#        endif
 #    else
-#        define MOUSEKEY_DELAY      300
-#        define MOUSEKEY_INTERVAL   50
-#        define MOUSEKEY_MOVE_DELTA 5
+#        ifndef MOUSEKEY_DELAY
+#            define MOUSEKEY_DELAY 300
+#        endif
+#        ifndef MOUSEKEY_INTERVAL
+#            define MOUSEKEY_INTERVAL 50
+#        endif
+#        ifndef MOUSEKEY_MOVE_DELTA
+#            define MOUSEKEY_MOVE_DELTA 5
+#        endif
 #    endif
-#    define MOUSEKEY_MAX_SPEED                   7
-#    define MOUSEKEY_TIME_TO_MAX                 60
-#    define MOUSEKEY_INITIAL_SPEED               100
-#    define MOUSEKEY_BASE_SPEED                  1000
-#    define MOUSEKEY_DECELERATED_SPEED           400
-#    define MOUSEKEY_ACCELERATED_SPEED           3000
+#    ifndef MOUSEKEY_MAX_SPEED
+#        define MOUSEKEY_MAX_SPEED 7
+#    endif
+#    ifndef MOUSEKEY_TIME_TO_MAX
+#        define MOUSEKEY_TIME_TO_MAX 60
+#    endif
+#    ifndef MOUSEKEY_INITIAL_SPEED
+#        define MOUSEKEY_INITIAL_SPEED 100
+#    endif
+#    ifndef MOUSEKEY_BASE_SPEED
+#        define MOUSEKEY_BASE_SPEED 1000
+#    endif
+#    ifndef MOUSEKEY_DECELERATED_SPEED
+#        define MOUSEKEY_DECELERATED_SPEED 400
+#    endif
+#    ifndef MOUSEKEY_ACCELERATED_SPEED
+#        define MOUSEKEY_ACCELERATED_SPEED 3000
+#    endif
 // mouse scroll config
-#    define MOUSEKEY_WHEEL_DELAY                 15
-#    define MOUSEKEY_WHEEL_DELTA                 1
-#    define MOUSEKEY_WHEEL_INTERVAL              50
-#    define MOUSEKEY_WHEEL_MAX_SPEED             8
-#    define MOUSEKEY_WHEEL_TIME_TO_MAX           80
+#    ifndef MOUSEKEY_WHEEL_DELAY
+#        define MOUSEKEY_WHEEL_DELAY 15
+#    endif
+#    ifndef MOUSEKEY_WHEEL_DELTA
+#        define MOUSEKEY_WHEEL_DELTA 1
+#    endif
+#    ifndef MOUSEKEY_WHEEL_INTERVAL
+#        define MOUSEKEY_WHEEL_INTERVAL 50
+#    endif
+#    ifndef MOUSEKEY_WHEEL_MAX_SPEED
+#        define MOUSEKEY_WHEEL_MAX_SPEED 8
+#    endif
+#    ifndef MOUSEKEY_WHEEL_TIME_TO_MAX
+#        define MOUSEKEY_WHEEL_TIME_TO_MAX 80
+#    endif
 // mouse scroll kinetic config
-#    define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS     8
-#    define MOUSEKEY_WHEEL_BASE_MOVEMENTS        48
-#    define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48
-#    define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8
+#    ifndef MOUSEKEY_WHEEL_INITIAL_MOVEMENTS
+#        define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 8
+#    endif
+#    ifndef MOUSEKEY_WHEEL_BASE_MOVEMENTS
+#        define MOUSEKEY_WHEEL_BASE_MOVEMENTS 48
+#    endif
+#    ifndef MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS
+#        define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48
+#    endif
+#    ifndef MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS
+#        define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8
+#    endif
 #endif // MOUSEKEY_ENABLE
