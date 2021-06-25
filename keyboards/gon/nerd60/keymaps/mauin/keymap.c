f7d3e0b5ad254993dcb84d9167bd3ce60c721759
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-06-25 14:22:16 -0700

    gon/nerd60:mauin - refactor Grave/Escape key
    Refactors the Grave/Escape key to use `process_record_user()` instead of TMK's `action_function()`.

--- keyboards/gon/nerd60/keymaps/mauin/keymap.c
+++ keyboards/gon/nerd60/keymaps/mauin/keymap.c
@@ -22,6 +22,9 @@ enum layer_names {
     _SY  // System Layer
 };
 
+enum custom_keycodes {
+    ESC_GRV = SAFE_RANGE
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Layer 0: Default Layer
@@ -38,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
   [_BS] = LAYOUT_60(
-      F(0),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
+      ESC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
       MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
       KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
@@ -58,13 +61,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |     |   |     |                       |     |   |   |     |
    * `-----------------------------------------------------------'
    */
-   [_FN] = LAYOUT_60(
+  [_FN] = LAYOUT_60(
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_DEL,
       XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_SLCK, KC_PAUS, XXXXXXX,
       KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,          XXXXXXX,
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-   ),
+  ),
 
   /* Layer 2: System Layer
    * ,-----------------------------------------------------------.
@@ -88,36 +91,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-enum function_id {
-    ESC_GRV, // Makes Esc behave like `~ when pressed with the left GUI modifier. This is the "switch between windows of the same application" key combination in macOS
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-  [0]  = ACTION_FUNCTION(ESC_GRV),
-};
-
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  static uint8_t esc_grv_mask;
-  switch (id) {
-    case ESC_GRV:
-      esc_grv_mask = get_mods() & MOD_BIT(KC_LGUI);
-      if (record->event.pressed) {
-        if (esc_grv_mask) {
-          add_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          add_key(KC_ESC);
-          send_keyboard_report();
-        }
-      } else {
-        if (esc_grv_mask) {
-          del_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          del_key(KC_ESC);
-          send_keyboard_report();
-        }
-      }
-      break;
-  }
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint8_t esc_grv_mask = get_mods() & MOD_BIT(KC_LGUI);
+    switch (keycode) {
+        case ESC_GRV:
+            if (esc_grv_mask) {
+                if (record->event.pressed) {
+                    register_code16(G(KC_GRV));
+                } else {
+                    unregister_code16(G(KC_GRV));
+                }
+            } else {
+                if (record->event.pressed) {
+                    register_code(KC_ESC);
+                } else {
+                    unregister_code(KC_ESC);
+                }
+            }
+            break;
+    }
+    return true;
 }
