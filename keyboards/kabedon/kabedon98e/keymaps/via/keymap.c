cb025160d75cdc58515e2db238d2114fea5348eb
KDon <370490639@qq.com>
2021-06-27 01:43:08 +0800

    Update keymap.c

--- keyboards/kabedon/kabedon98e/keymaps/via/keymap.c
+++ keyboards/kabedon/kabedon98e/keymaps/via/keymap.c
@@ -1,5 +1,20 @@
-#include QMK_KEYBOARD_H
+/* Copyright 2021 KDon<370490639@qq.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+#include QMK_KEYBOARD_H
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
@@ -28,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 │AB ││BB││BD││BC││BF                     ││AH ││BH │ ┌──┐┌──┐┌──┐ │AK││BK││  │  │8A│
 └───┘└──┘└──┘└──┘└───────────────────────┘└───┘└───┘ │AI││BI││BJ│ └──┘└──┘└──┘  └──┘
                                                      └──┘└──┘└──┘
-*/
+ */
 [_BASE] = LAYOUT(
 	KC_ESC,      KC_F1,  KC_F2,  KC_F3,  KC_F4,       KC_F5,  KC_F6,  KC_F7,  KC_F8,       KC_F9,  KC_F10,  KC_F11,  KC_F12,      KC_DEL,      KC_INS, 		 KC_MUTE, 
 	KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,     KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,   KC_HOME,
@@ -55,20 +70,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  uint8_t layer = biton32(layer_state);
-  if (index == 0) {
-    if (clockwise) {
-      tap_code(keymap_key_to_keycode(layer, (keypos_t) {.row = 0, .col = 0}));
-    } else {
-      tap_code(keymap_key_to_keycode(layer, (keypos_t) {.row = 2, .col = 0}));
+static uint8_t encoder_state[ENCODERS] = {0};
+static keypos_t encoder_cw[ENCODERS] = ENCODERS_CW_KEY;
+static keypos_t encoder_ccw[ENCODERS] = ENCODERS_CCW_KEY;
+
+void encoder_action_unregister(void) {
+    for (int index = 0; index < ENCODERS; ++index) {
+        if (encoder_state[index]) {
+            keyevent_t encoder_event = (keyevent_t) {
+                .key = encoder_state[index] >> 1 ? encoder_cw[index] : encoder_ccw[index],
+                .pressed = false,
+                .time = (timer_read() | 1)
+            };
+            encoder_state[index] = 0;
+            action_exec(encoder_event);
+        }
     }
-  } else if (index == 1) {
-    if (clockwise) {
-      tap_code(keymap_key_to_keycode(layer, (keypos_t) {.row = 6, .col = 0}));
-    } else {
-      tap_code(keymap_key_to_keycode(layer, (keypos_t) {.row = 8, .col = 0}));
-    }
-  }
-    return true;
 }
+
+void encoder_action_register(uint8_t index, bool clockwise) {
+    keyevent_t encoder_event = (keyevent_t) {
+        .key = clockwise ? encoder_cw[index] : encoder_ccw[index],
+        .pressed = true,
+        .time = (timer_read() | 1)
+    };
+    encoder_state[index] = (clockwise ^ 1) | (clockwise << 1);
+    action_exec(encoder_event);
+}
+
+void matrix_scan_user(void) {
+    encoder_action_unregister();
+}
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    encoder_action_register(index, clockwise);
+    return true;
+};
