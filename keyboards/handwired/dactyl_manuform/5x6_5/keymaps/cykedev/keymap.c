3d04109281d81fd8b8949fcd4f28528729c6b6e1
Christian Eiden <christian@eiden.ch>
2021-06-26 10:42:15 +0200

    further adjustments
    Signed-off-by: Christian Eiden <christian@eiden.ch>

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/keymap.c
@@ -43,11 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   F  |   P  |   B  |                                  |   J  |   L  |   U  |   Y  |   ;  |  \   |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Ctrl |   A  |   R  |   S  |   T  |   G  |                                  |   M  |   N  |   E  |   I  |   O  |  '   |
+   * | Del  |   A  |   R  |   S  |   T  |   G  |                                  |   M  |   N  |   E  |   I  |   O  |  '   |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Shft |   Z  |   X  |   C  |   D  |   V  |                                  |   K  |   H  |   ,  |   .  |   /  | Shft |
+   * | ADJ  |   Z  |   X  |   C  |   D  |   V  |                                  |   K  |   H  |   ,  |   .  |   /  |  ADJ |
    * '-----------------------------------------/                                  \-----------------------------------------'
-   *               |  ADJ |   =  |    /  SYM  /---------------.    .---------------\  SYM  \    |   -  | ADJ  |
+   *               |      |      |    /  SYM  /---------------.    .---------------\  SYM  \    |      |      |
    *               |      |      |   /       /  Cmd  / Shift /      \ Shift \  Cmd  \       \   |      |      |
    *               '-------------'  '-------/ Space / Enter /        \ Enter \ Space \-------'  '-------------'
    *                                       /---------------/          \---------------\
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
    KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
    KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSLS,
-   KC_LCTL, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                                     KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
-   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,                                     KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                     ADJ    , KC_EQL ,          SYM    , CM_SPAL, SH_ENL , SH_ENR , CM_SPAR, SYM    ,          KC_MINS, ADJ    ,
+   KC_DEL , KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,                                     KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
+   ADJ    , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,                                     KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, ADJ    ,
+                     XXXXXXX, XXXXXXX,          SYM    , CM_SPAL, SH_ENL , SH_ENR , CM_SPAR, SYM    ,          XXXXXXX, XXXXXXX,
                                                          KC_LALT, KC_LCTL, KC_RCTL, KC_RALT
    ),
   [_QWERTY] = LAYOUT_5x6_5(
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |                                  |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |                                  |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * | Del  |   A  |   S  |   D  |   F  |   G  |                                  |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * | Shft |   Z  |   X  |   C  |   V  |   B  |                                  |   N  |   M  |   ,  |   .  |   /  | Shft |
+   * | ADJ  |   Z  |   X  |   C  |   V  |   B  |                                  |   N  |   M  |   ,  |   .  |   /  |  ADJ |
    * '-----------------------------------------/                                  \-----------------------------------------'
-   *               |  ADJ |   =  |    /  SYM  /---------------.    .---------------\  SYM  \    |   -  | ADJ  |
+   *               |      |      |    /  SYM  /---------------.    .---------------\  SYM  \    |      |      |
    *               |      |      |   /       /  Cmd  / Shift /      \ Shift \  Cmd  \       \   |      |      |
    *               '-------------'  '-------/ Space / Enter /        \ Enter \ Space \-------'  '-------------'
    *                                       /---------------/          \---------------\
@@ -83,35 +83,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
    KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
-   KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                     ADJ    , KC_EQL ,          SYM    , CM_SPAL, SH_ENL , SH_ENR , CM_SPAR, SYM    ,          KC_MINS, ADJ    ,
+   KC_DEL , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+   ADJ    , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, ADJ    ,
+                     XXXXXXX, XXXXXXX,          SYM    , CM_SPAL, SH_ENL , SH_ENR , CM_SPAR, SYM    ,          XXXXXXX, XXXXXXX,
                                                          KC_LALT, KC_LCTL, KC_RCTL, KC_RALT
    ),
   [_SYMBOL] = LAYOUT_5x6_5(
   /* SYMBOL
    * .-----------------------------------------.                                  .-----------------------------------------.
-   * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |                                  |  F6  |  F7  |  F8  |  F9  |  F10 |  Del |
+   * |  `   |  F1  |  F2  |  F3  |  F4  |  F5  |                                  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * |      |   !  |   @  |   [  |   ]  |   +  |                                  |      |      |  up  |      |      |      |
+   * |      |   !  |   @  |   [  |   ]  |   +  |                                  |      |      |  up  |      |      |  F12 |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
-   * |      |   &  |   |  |   (  |   )  |   -  |                                  |      | left | down | right|  ?   |      |
+   * |  Ins |   &  |   |  |   (  |   )  |   -  |                                  |      | left | down | right|  ?   |      |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
    * |      |   *  |   /  |   {  |   }  |   =  |                                  |      |      |      |      |      |      |
    * '-----------------------------------------/                                  \-----------------------------------------'
-   *               |      |      |    /       /---------------.    .---------------\       \    |      |      |
-   *               |      |      |   /       /       /       /      \       \       \       \   |      |      |
+   *               | page | page |    /       /---------------.    .---------------\       \    | home | end  |
+   *               |  up  | down |   /       /       /       /      \       \       \       \   |      |      |
    *               '-------------'  '-------/       /       /        \       \       \-------'  '-------------'
    *                                       /---------------/          \---------------\
    *                                      /       /       /            \       \       \
    *                                     /       /       /              \       \       \
    *                                    '---------------'                '---------------'
    */
-   KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_DEL ,
-   XXXXXXX, KC_EXLM, KC_AT  , KC_LBRC, KC_RBRC, KC_PPLS,                                     XXXXXXX, XXXXXXX, KC_UP  , XXXXXXX, XXXXXXX, XXXXXXX,
-   _______, KC_AMPR, KC_PIPE, KC_LPRN, KC_RPRN, KC_MINS,                                     XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUES, XXXXXXX,
-   _______, KC_ASTR, KC_SLSH, KC_LCBR, KC_RCBR, KC_PEQL,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                     XXXXXXX, XXXXXXX,          _______, _______, _______, _______, _______, _______,          XXXXXXX, XXXXXXX,
+   KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
+   XXXXXXX, KC_EXLM, KC_AT  , KC_LBRC, KC_RBRC, KC_PPLS,                                     XXXXXXX, XXXXXXX, KC_UP  , XXXXXXX, XXXXXXX, KC_F12 ,
+   KC_INS , KC_AMPR, KC_PIPE, KC_LPRN, KC_RPRN, KC_MINS,                                     XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUES, XXXXXXX,
+   XXXXXXX, KC_ASTR, KC_SLSH, KC_LCBR, KC_RCBR, KC_PEQL,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                     KC_PGUP, KC_PGDN,          _______, _______, _______, _______, _______, _______,          KC_HOME, KC_END ,
                                                          _______, _______, _______, _______
    ),
   [_ADJUST] = LAYOUT_5x6_5(
@@ -125,8 +125,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
    * |      |      |      |      |      | Mute |                                  |      |      |      |      |      |      |
    * '-----------------------------------------/                                  \-----------------------------------------'
-   *               |      |      |    /       /---------------.    .---------------\       \    |      |      |
-   *               |      |      |   /       /       /       /      \       \       \       \   |      |      |
+   *               | page | page |    /       /---------------.    .---------------\       \    | home | end  |
+   *               |  up  | down |   /       /       /       /      \       \       \       \   |      |      |
    *               '-------------'  '-------/       /       /        \       \       \-------'  '-------------'
    *                                       /---------------/          \---------------\
    *                                      /       /       /            \       \       \
@@ -136,8 +136,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QWERT  ,                                     COLEM  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET  ,
    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,                                     KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,                                     KC_BRID, KC_MRWD, KC_MPLY, KC_MFFD, XXXXXXX, XXXXXXX,
-   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                     _______, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, _______,
+   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                     KC_PGUP, KC_PGDN,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_HOME, KC_END ,
                                                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
    )
 };
