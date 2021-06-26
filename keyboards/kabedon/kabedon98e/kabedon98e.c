d3c4408e1d80cb6f2c6fd91a1edc812ad8a1fd3a
KDon <370490639@qq.com>
2021-06-26 23:32:08 +0800

    Update kabedon98e.c

--- keyboards/kabedon/kabedon98e/kabedon98e.c
+++ keyboards/kabedon/kabedon98e/kabedon98e.c
@@ -1,4 +1,18 @@
-
+/* Copyright 2021 KDon<370490639@qq.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "kabedon98e.h"
 
 void keyboard_pre_init_user(void) {
@@ -7,7 +21,7 @@ void keyboard_pre_init_user(void) {
 
 bool led_update_kb(led_t led_state) {
     if (led_update_user(led_state)) {
-		writePin(B5, !led_state.caps_lock);
+		writePin(A13, !led_state.caps_lock);
     }
     return true;
 }
