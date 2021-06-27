46b070575bf10acc4231ffda04c80b076e969684
Jurgen <jurgen.kluft@gmail.com>
2021-06-27 08:12:16 +0800

    * wpm; some changes to be more in line with the code style

--- quantum/wpm.c
+++ quantum/wpm.c
@@ -34,30 +34,30 @@ __attribute__((weak)) bool wpm_keycode_user(uint16_t keycode) {
     return false;
 }
 
-// The shorter you make the capture duration the quicker the fall off once you stop typing.
-// Comment this out to get a longer fall-off but also a longer ramp up time to display
-// the actual WPM
+/* The shorter you make the capture duration the quicker the fall off once you stop typing. */
+/* Comment this out to get a longer fall-off but also a longer ramp up time to display */
+/* the actual WPM */
 #define WPM_SHORT_WINDOW
 
 #ifndef WPM_SHORT_WINDOW
-//    Full range we capture here is 100 * 100 ms = 10 s, this also means that it will take
-//      ~10 seconds to display the actual WPM once you start typing.
-//    The computed WPM needs to be multiplied by 6 (6*10s=60s=1min) 
-//      to get the actual WPM, see 'WPM_WINDOW_TO_WPM' (x*6 = x*4 + x*2)
+/*    Full range we capture here is 100 * 100 ms = 10 s, this also means that it will take */
+/*      ~10 seconds to display the actual WPM once you start typing. */
+/*    The computed WPM needs to be multiplied by 6 (6*10s=60s=1min)  */
+/*      to get the actual WPM, see 'WPM_WINDOW_TO_WPM' (x*6 = x*4 + x*2) */
 #    define WPM_WINDOW_NUM      100
 #    define WPM_WINDOW_MS       100
 #    define WPM_WINDOW_TO_WPM(x)  (((x) << 2) + ((x) << 1))
 
 #else
-//    Full range we capture here is 100 * 60 ms = 6 s, this also means that it will take
-//      ~6 seconds to display the actual WPM once you start typing.
-//    x*10 = x*8 + x*2
+/*    Full range we capture here is 100 * 60 ms = 6 s, this also means that it will take */
+/*      ~6 seconds to display the actual WPM once you start typing. */
+/*    x*10 = x*8 + x*2 */
 #    define WPM_WINDOW_NUM      100
 #    define WPM_WINDOW_MS       60
 #    define WPM_WINDOW_TO_WPM(x)  (((x) << 3) + ((x) << 1))
 #endif
 
-// State names
+/* State names */
 #    define WPM_PROCESS_DECAY 0
 #    define WPM_PROCESS_KEYPRESS 1
 
@@ -72,7 +72,7 @@ typedef struct {
 
 static wpm_state_t s_wpm_state = { .period = -1, .word = 0, .wpm = 0, .wpm_displayed = 0 };
 
-// This should only be called on the slave side (when receiving I2C data)
+/* This should only be called on the slave side (when receiving I2C data) */
 void set_current_wpm(uint8_t new_wpm) {    
     s_wpm_state.wpm_displayed = new_wpm;
 }
@@ -87,7 +87,7 @@ void process_wpm(uint8_t mode) {
     s_wpm_state.word += mode;
     if (elapsed >= WPM_WINDOW_MS) {
 
-        // TODO: Can we move this to some initialization step?
+        /* TODO: Can we move this to some initialization step? */
         if (s_wpm_state.period == -1) {
             for (int8_t i=0; i<WPM_WINDOW_NUM; i++) {
                 s_wpm_state.windows[i] = 0;
@@ -101,8 +101,8 @@ void process_wpm(uint8_t mode) {
         }
 
         if (mode == WPM_PROCESS_DECAY) {
-            // Have the wpm display value move towards the actual (lower) wpm value in a timely manner
-            if ((s_wpm_state.period & 0x7) == 0) {  // Delaying by 8 * WPM_WINDOW_MS = 800 ms
+            /* Have the wpm display value move towards the actual (lower) wpm value in a timely manner */
+            if ((s_wpm_state.period & 0x7) == 0) {  /* Delaying by 8 * WPM_WINDOW_MS = 800 ms */
                 uint8_t wpm = WPM_WINDOW_TO_WPM(s_wpm_state.wpm);
                 if (wpm < s_wpm_state.wpm_displayed) {
                     s_wpm_state.wpm_displayed = s_wpm_state.wpm_displayed - (((s_wpm_state.wpm_displayed - wpm) >> 2) | 1);
@@ -124,7 +124,7 @@ void update_wpm(uint16_t keycode) {
     if (wpm_keycode(keycode)) {
         process_wpm(WPM_PROCESS_KEYPRESS);
         {   
-            // Have the wpm display value move towards the actual (higher) wpm value in a rapid manner
+            /* Have the wpm display value move towards the actual (higher) wpm value in a rapid manner */
             uint8_t wpm = (s_wpm_state.wpm << 2) + (s_wpm_state.wpm << 1);
             if (wpm > s_wpm_state.wpm_displayed) {
                 s_wpm_state.wpm_displayed = wpm;
