be07ced0c52c4c3e770c4bafa50669e87b7be98a
drhigsby <71532157+drhigsby@users.noreply.github.com>
2021-06-27 20:14:00 -0700

    Update keyboards/ogurec/keymaps/dack/keymap.c
    based on drashna's input
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ogurec/keymaps/dack/keymap.c
+++ keyboards/ogurec/keymaps/dack/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_TAB,  KC_PIPE, vvv,    LOCK,     _______,  _______,  KC_PGDN,   KC_LEFT,    KC_DOWN,  KC_RIGHT,  KC_DEL,
         UNDO,    CUT,     COPY,    PASTE,  KC_LBRC,  KC_RBRC,  KC_LCBR,  KC_RCBR,   _______,    _______,  _______,   _______
   ),
-    [FUNKY] = LAYOUT_LEFT(
+    [_FUNKY] = LAYOUT_LEFT(
         KC_F1,    KC_F2,    KC_F3,   KC_F4,    KC_F5,    KC_F6,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
         _______,  _______,  vvv,     _______,  _______,  _______, _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______, _______,  _______,  KC_VOLD, KC_VOLU,  _______,  _______,  _______,  _______,  _______
