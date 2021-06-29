b8c2b3de59c0faa4c1924b34dfec930ff2d418dd
gtips <gtips@slackhack.net>
2021-06-29 11:24:18 +0900

    update keyboards/reviung5/keymaps/default_lre/keymap.c

--- keyboards/reviung5/keymaps/default_lre/keymap.c
+++ keyboards/reviung5/keymaps/default_lre/keymap.c
@@ -24,7 +24,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT_reviung5(
-        TG(_LOWER),    KC_MUTE,       KC_MPRV,       KC_MPLY,       KC_MNXT
+        TG(_LOWER),       KC_MPRV,       KC_MPLY,       KC_MNXT,    KC_MUTE
     ),
     [_LOWER] = LAYOUT_reviung5(
         _______,       LCTL(KC_X),    LCTL(KC_C),    LCTL(KC_V),    RGB_TOG
