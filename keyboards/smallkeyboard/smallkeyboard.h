3162589819f2472c3369d5b154c876ee16949e93
zhouqiong19840119 <82140728+zhouqiong19840119@users.noreply.github.com>
2021-06-30 14:16:55 +0800

    Update smallkeyboard.h

--- keyboards/smallkeyboard/smallkeyboard.h
+++ keyboards/smallkeyboard/smallkeyboard.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 zhouqiong19840119
+/* Copyright 2021 zhouqiong198401189
   * 
   * This program is free software: you can redistribute it and/or modify 
   * it under the terms of the GNU General Public License as published by 
