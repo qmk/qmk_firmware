c8cfe76cdd333e5a81e2582d4f20ed26250be857
Kwabena Aduse-Poku <kaduse432@gmail.com>
2021-06-30 12:23:23 -0400

    changes made to accomodate via: changed manufacturer and product id in config.h, added more layers and changed layer definitions in keymaps/via/keymap.c

--- keyboards/kwub/bloop/config.h
+++ keyboards/kwub/bloop/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x0432 // Kwub
+#define PRODUCT_ID      0x0001 
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Kwub
 #define PRODUCT         Bloop65
