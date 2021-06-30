35ba4076273b08f2f0fa6c1250645a5de6df74f0
scda <dsop@mailbox.org>
2021-06-30 16:48:45 +0200

    replace duplicate undo with redo

--- keyboards/kyria/keymaps/scda/keymap.c
+++ keyboards/kyria/keymaps/scda/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MEDIA] = LAYOUT(
         KC_TRNS, KC_NO, KC_MUTE, KC_NO, KC_NO, KC_NO,                                           KC_NO, G(KC_X), G(KC_R), KC_NO, C(KC_LEFT), KC_TRNS,
         KC_TRNS, KC_BRIU, KC_VOLU, KC_MPLY, KC_MNXT, KC_NO,                                     G(C(KC_SPACE)), G(KC_C), G(KC_Z), G(KC_F), C(KC_UP), KC_NO,
-        KC_TRNS, KC_BRID, KC_VOLD, KC_NO, KC_MPRV, KC_NO, KC_NO, KC_NO,                         KC_NO, KC_NO, KC_NO, G(KC_V), G(KC_Z), KC_NO, C(KC_RGHT), KC_TRNS,
+        KC_TRNS, KC_BRID, KC_VOLD, KC_NO, KC_MPRV, KC_NO, KC_NO, KC_NO,                         KC_NO, KC_NO, KC_NO, G(KC_V), G(S(KC_Z)), KC_NO, C(KC_RGHT), KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [_KBD_CTRL] = LAYOUT(
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
