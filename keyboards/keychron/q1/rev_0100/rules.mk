50700476077127a99cf2bfe8e0940ecc2c2b364e
lokher <lokher@gmail.com>
2021-07-01 11:25:21 +0800

    Add keyboard Q1

--- keyboards/keychron/q1/rev_0100/rules.mk
+++ keyboards/keychron/q1/rev_0100/rules.mk
@@ -1,6 +1,7 @@
 # MCU name
 MCU = atmega32u4
 
+<<<<<<< HEAD
 # Bootloader selection
 BOOTLOADER = atmel-dfu
 
@@ -20,6 +21,23 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+=======
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   comment out to disable the options.
+#
+BOOTMAGIC_ENABLE = yes		# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no		# Mouse keys
+EXTRAKEY_ENABLE = yes		# Audio control and System control
+CONSOLE_ENABLE = no		    # Console for debug
+COMMAND_ENABLE = no		    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no		# Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes		    # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no		# Enable keyboard backlight functionality
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
+>>>>>>> 8baa59b914 (Add keyboard Q1)
 DIP_SWITCH_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = IS31FL3733
