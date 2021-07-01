0c5f5693880e2f5c5f700da8a06c24d4ee8eaebd
Kwabena Aduse-Poku <kaduse432@gmail.com>
2021-07-01 15:13:34 -0400

    changed product ID

--- keyboards/kwub/bloop/config.h
+++ keyboards/kwub/bloop/config.h
@@ -19,7 +19,7 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x0432 // Kwub
+#define VENDOR_ID       0x6b77 // Kwub
 #define PRODUCT_ID      0x0001 
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Kwub
