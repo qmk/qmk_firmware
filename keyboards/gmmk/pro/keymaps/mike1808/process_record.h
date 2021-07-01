4119cfcd5404bbbb4b7a03515f28b9b9292792ec
Mikael Manukyan <arm.localhost@gmail.com>
2021-06-30 20:53:38 -0700

    Add licenses

--- keyboards/gmmk/pro/keymaps/mike1808/process_record.h
+++ keyboards/gmmk/pro/keymaps/mike1808/process_record.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+/* Copyright 2021 Mikael Manukyan <arm.localhost@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 #include "mike1808.h"
 
