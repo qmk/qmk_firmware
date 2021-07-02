37336bff1d8bba6e14f3bc44bcdad6fcb18190c7
esca47 <RedYoshi90@gmail.com>
2021-07-01 22:54:02 -0700

    Getawayvan(F072 and F042)
    Adds my Getawayvan and the two branches as f042 didnt play nice with the f072 version. Both boards will have default keymap and a basic 7u keymap.

---
+++ keyboards/esca/getawayvan_f042/rules.mk
@@ -0,0 +1,17 @@
+# MCU name
+MCU = STM32F042
+
+# Build Options
+#   comment out to disable the options.
+#
+BACKLIGHT_ENABLE = no
+BOOTMAGIC_ENABLE = no				# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no				# Mouse keys
+EXTRAKEY_ENABLE = yes				# Audio control and System control
+CONSOLE_ENABLE = no					# Console for debug
+COMMAND_ENABLE = no    				# Commands for debug and configuration
+NKRO_ENABLE = yes					# USB Nkey Rollover
+NO_USB_STARTUP_CHECK = no         	# Disable initialization only when usb is plugged in
+LTO_ENABLE = yes
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
