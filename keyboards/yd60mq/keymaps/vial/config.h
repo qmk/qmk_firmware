77c3d45bcee7619b2ec914068a99e88d0506470f
hornslyn <86503162+hornslyn@users.noreply.github.com>
2021-07-04 02:26:23 +0800

    Vial support for YD60MQ (#24)
    * CX60-Vial - Initial Commit
    
    * Edited readme.md
    
    * Edited #ifdef in config.h
    
    * Edited rules.mk
    
    * Edited capslock codes
    
    * Added Vial Support to YD60MQ
    
    * Cleanup for proper submission
    
    * Removed VIAL_INSECURE = no from rules.mk

---
+++ keyboards/yd60mq/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+/* Vial configuration */
+#define VIAL_KEYBOARD_UID {0x99, 0x73, 0xEF, 0xAB, 0x11, 0x33, 0xA0, 0xC4}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
---
+++ keyboards/yd60mq/keymaps/vial/readme.md
@@ -0,0 +1,4 @@
+Vial Support for YD60MQ
+
+make yd60mq/12led:vial
+make yd60mq/16led:vial
