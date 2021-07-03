8b1ec0bb138e880e256f130dd757295363463249
scda <dsop@mailbox.org>
2021-07-03 13:55:16 +0200

    modify/fix switched keys (compared to desired layout)

--- keyboards/kyria/keymaps/scda/keymap.c
+++ keyboards/kyria/keymaps/scda/keymap.c
@@ -6,9 +6,9 @@ enum layers { _LETTERS = 0, _SYMBOLS, _NUMBERS, _MEDIA, _KBD_CTRL };
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LETTERS] = LAYOUT(
-        C(KC_LEFT), KC_X, DE_DOT, KC_O, DE_COMM, KC_Y,                                          KC_V, KC_G, KC_C, KC_L, KC_J, C(KC_RGHT),
+        C(KC_LEFT), KC_X, DE_DOT, KC_O, DE_COMM, DE_Y,                                          KC_V, KC_G, KC_C, KC_L, KC_J, C(KC_RGHT),
         LT(4,KC_ESC), CTL_T(KC_H), ALT_T(KC_A), GUI_T(KC_E), SFT_T(KC_I), KC_U,                 KC_D, SFT_T(KC_T), GUI_T(KC_R), ALT_T(KC_N), CTL_T(KC_S), KC_F,
-        KC_NO, KC_K, KC_Q, DE_ADIA, DE_UDIA, DE_ODIA, KC_NO, KC_NO,                             KC_NO, KC_NO, KC_B, KC_P, KC_W, KC_M, KC_Z, DE_SS,
+        KC_NO, KC_K, KC_Q, DE_ADIA, DE_UDIA, DE_ODIA, KC_NO, KC_NO,                             KC_NO, KC_NO, KC_B, KC_P, KC_W, KC_M, DE_Z, DE_SS,
         KC_HOME, KC_BSPC, LT(1,KC_TAB), LT(2,KC_SPC), LT(3,KC_ENT),                             LT(3,KC_ENT), LT(2,KC_SPC), LT(1,KC_TAB), KC_DEL, KC_END),
         // LT(_LAYER_NAME, KC_XY) will break the json import in the configurator, so use numbers
     [_SYMBOLS] = LAYOUT(
@@ -20,11 +20,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                             KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
         KC_TRNS, CTL_T(DE_1), ALT_T(DE_2), GUI_T(DE_3), SFT_T(DE_4), DE_5,                      DE_6, SFT_T(DE_7), GUI_T(DE_8), ALT_T(DE_9), CTL_T(DE_0), KC_F12,
         KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO,                         KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_BTN1, KC_BTN2, KC_WH_U, KC_WH_D, KC_TRNS),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_BTN1, KC_BTN2, KC_WH_D, KC_WH_U, KC_TRNS), // scrolling reversed (due to natural scrolling?)
     [_MEDIA] = LAYOUT(
         KC_TRNS, KC_NO, KC_VOLU, KC_NO, KC_NO, KC_NO,                                           KC_NO, G(KC_X), G(KC_R), KC_NO, C(KC_LEFT), KC_TRNS,
-        KC_TRNS, KC_BRIU, KC_MUTE, KC_MPLY, KC_MNXT, KC_NO,                                     G(C(KC_SPACE)), G(KC_C), G(KC_Z), G(KC_F), C(KC_UP), KC_NO,
-        KC_TRNS, KC_BRID, KC_VOLD, KC_NO, KC_MPRV, KC_NO, KC_NO, KC_NO,                         KC_NO, KC_NO, KC_NO, G(KC_V), G(S(KC_Z)), KC_NO, C(KC_RGHT), KC_TRNS,
+        KC_TRNS, KC_BRIU, KC_MUTE, KC_MPLY, KC_MNXT, KC_NO,                                     G(C(KC_SPACE)), G(KC_C), G(DE_Z), G(KC_F), C(KC_UP), KC_NO,
+        KC_TRNS, KC_BRID, KC_VOLD, KC_NO, KC_MPRV, KC_NO, KC_NO, KC_NO,                         KC_NO, KC_NO, KC_NO, G(KC_V), G(S(DE_Z)), KC_NO, C(KC_RGHT), KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [_KBD_CTRL] = LAYOUT(
         KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
