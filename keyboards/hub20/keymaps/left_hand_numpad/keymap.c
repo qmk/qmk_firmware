671755c509e67e661a211b25019cc2ea95cec9dd
Drashna Jael're <drashna@live.com>
2021-07-03 17:11:17 -0700

    Additional callback cleanup

--- keyboards/hub20/keymaps/left_hand_numpad/keymap.c
+++ keyboards/hub20/keymaps/left_hand_numpad/keymap.c
@@ -15,35 +15,15 @@
  */
 #include QMK_KEYBOARD_H
 
-
-
 // #define LED_MERGE_NUMPAD_LEFT_HANDED_PLUS TRUE
 // #define LED_MERGE_NUMPAD_LEFT_HANDED_ENTER TRUE
 // #define LED_MERGE_NUMPAD_LEFT_HANDED_ZERO TRUE
 
-
 #define MO_NLCK LT(1, KC_NLCK) // Numlock on tap, layer change on hold
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_left_handed(
-                 KC_MUTE,  KC_MPLY,
-       KC_PMNS,  KC_PAST,  KC_PSLS,    MO_NLCK,
-                   KC_P7,    KC_P8,      KC_P9,
-       KC_PPLS,    KC_P4,    KC_P5,      KC_P6,
-                   KC_P1,    KC_P2,      KC_P3,
-       KC_PENT,  KC_PDOT,    KC_P0
-),
-    [1] = LAYOUT_left_handed(
-                   RESET,  _______,
-       RGB_TOG, RGB_RMOD,  RGB_MOD,    _______,
-       _______,  RGB_VAD,  RGB_VAI,    _______,
-                 RGB_HUD,  RGB_HUI,    _______,
-       _______,  RGB_SAD,  RGB_SAI,    _______,
-                 _______,        _______
-)
-};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_left_handed(KC_MUTE, KC_MPLY, KC_PMNS, KC_PAST, KC_PSLS, MO_NLCK, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_P4, KC_P5, KC_P6, KC_P1, KC_P2, KC_P3, KC_PENT, KC_PDOT, KC_P0), [1] = LAYOUT_left_handed(RESET, _______, RGB_TOG, RGB_RMOD, RGB_MOD, _______, _______, RGB_VAD, RGB_VAI, _______, RGB_HUD, RGB_HUI, _______, _______, RGB_SAD, RGB_SAI, _______, _______, _______)};
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* Left Encoder */
         if (clockwise) {
             tap_code(KC_VOLU);
@@ -57,5 +37,5 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_MPRV);
         }
     }
+    return true;
 }
-
