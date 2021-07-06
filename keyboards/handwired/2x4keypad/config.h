b747d574abce5aaaae8a66611719d5da50e23758
Kuba Tyszko <kuba@lbl.pl>
2021-07-06 03:00:31 -0700

    adding GPL2 license pragmas and cleaning up source

--- keyboards/handwired/2x4keypad/config.h
+++ keyboards/handwired/2x4keypad/config.h
@@ -1,3 +1,19 @@
+ /* Copyright 2021 Kuba Tyszko
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
+
 #pragma once
 
 #include "config_common.h"
@@ -15,7 +31,7 @@
 
 /* key matrix pins */
 #define MATRIX_ROW_PINS { B3, B2 }
-#define MATRIX_COL_PINS { D7, B4, B5, B6 }
+#define MATRIX_COL_PINS { D7, B4, D6, D4 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
@@ -29,12 +45,6 @@
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
 /* Tap dancing params */
 #define TAPPING_TERM 250
