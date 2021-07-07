2163b9c9ed3fac3a5a6ff0d2b52900835e9b7e69
Harshti Goel <harshitgoel@licious.com>
2021-07-07 10:29:51 +0530

    update changes requested by darshana

--- keyboards/pteron36/config.h
+++ keyboards/pteron36/config.h
@@ -159,5 +159,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENCODERS_PAD_B { F4 }
 
 //right encoder
-#define ENCODERS_PAD_B_RIGHT { F5 }
 #define ENCODERS_PAD_A_RIGHT { F4 }
+#define ENCODERS_PAD_B_RIGHT { F5 }
