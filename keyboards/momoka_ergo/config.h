3c5b303d979e9b22cde173eaa1865d3df3a5f082
StefanGrindelwald <42373600+StefanGrindelwald@users.noreply.github.com>
2021-07-07 13:00:22 +0800

    Update keyboards/momoka_ergo/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/momoka_ergo/config.h
+++ keyboards/momoka_ergo/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0000
 #define DEVICE_VER   0x0001
 #define MANUFACTURER StefanGrindelwald
-#define PRODUCT      momoka_ergo
+#define PRODUCT      Momoka Ergo
 
 /* key matrix size */
 #define MATRIX_ROWS 14
