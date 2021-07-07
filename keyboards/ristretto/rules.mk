a7c3a669ab774d929b85ea80b3dce3c8e5318301
Brandon Lewis <blewis308@users.noreply.github.com>
2021-07-07 01:18:05 -0500

    Add Ristretto Keyboard

---
+++ keyboards/ristretto/rules.mk
@@ -0,0 +1,14 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+# 	change yes to no to disable
+#
+ENCODER_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+WAIT_FOR_USB = yes
+NKRO_ENABLE = yes
