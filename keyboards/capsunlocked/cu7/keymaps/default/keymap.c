92d917446b637c29d422bafccfa6c19eae789788
Rys Sommefeldt <rys@sommefeldt.com>
2021-07-08 23:09:31 +0100

    Simplify encoder_update_user impl and rules.mk

--- keyboards/capsunlocked/cu7/keymaps/default/keymap.c
+++ keyboards/capsunlocked/cu7/keymaps/default/keymap.c
@@ -16,34 +16,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-#define MEDIA_KEY_DELAY 10
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
              MO(1),
     KC_1,    KC_2,    KC_3,
-	KC_4,    KC_5,    KC_6
+    KC_4,    KC_5,    KC_6
   ),
   [1] = LAYOUT(
              _______,
     RGB_MOD, KC_UP,   RESET,
-	KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LEFT, KC_DOWN, KC_RGHT
   ),
 };
 
 // Volume up/down on the encoder
 bool encoder_update_user(uint8_t index, bool clockwise) {
-  uint16_t held_keycode_timer = timer_read();
-
   if (clockwise) {
-    register_code(KC_VOLU);
-    while (timer_elapsed(held_keycode_timer) < MEDIA_KEY_DELAY) {}
-    unregister_code(KC_VOLU);
+    tap_code_delay(KC_VOLU, 10);
   }
   else {
-    register_code(KC_VOLD);
-    while (timer_elapsed(held_keycode_timer) < MEDIA_KEY_DELAY) {}
-    unregister_code(KC_VOLD);
+    tap_code_delay(KC_VOLD, 10);
   }
 
   return true;
