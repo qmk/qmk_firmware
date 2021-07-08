f92ee0c25f8127ab4350b6d691daf8df9c7baa36
Nick Brassel <nick@tzarc.org>
2021-07-09 08:30:52 +1000

    New keyboards.

--- keyboards/tender/macrowo_pad/rules.mk
+++ keyboards/tender/macrowo_pad/rules.mk
@@ -16,7 +16,6 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
