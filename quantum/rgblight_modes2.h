7f94824e932816b872c3bfa2ec2e74d0a0752096
mtei <2170248+mtei@users.noreply.github.com>
2021-07-10 17:34:39 +0900

    refine quantum/rgblight_modes2.h

--- quantum/rgblight_modes2.h
+++ quantum/rgblight_modes2.h
@@ -1,4 +1,5 @@
 /* Copyright 2016-2017 Yang Liu
+ * Copyright 2021 mtei
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,70 +32,70 @@
 #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
-#define _RGBM_DEF_BREATHING ,(BREATHING, 4, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_BREATHING
-#    undef _RGBM_DEF_BREATHING
+#ifdef RGBLIGHT_EFFECT_BREATHING
+#    define _RGBM_DEF_BREATHING ,(BREATHING, 4, DYNAMIC)
+#else
 #    define _RGBM_DEF_BREATHING
-#endif    
+#endif
 
-#define _RGBM_DEF_RAINBOW_MOOD ,(RAINBOW_MOOD, 3, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    undef _RGBM_DEF_RAINBOW_MOOD
+#ifdef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define _RGBM_DEF_RAINBOW_MOOD ,(RAINBOW_MOOD, 3, DYNAMIC)
+#else
 #    define _RGBM_DEF_RAINBOW_MOOD
 #endif
 
-#define _RGBM_DEF_RAINBOW_SWIRL ,(RAINBOW_SWIRL, 6, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    undef _RGBM_DEF_RAINBOW_SWIRL
+#ifdef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define _RGBM_DEF_RAINBOW_SWIRL ,(RAINBOW_SWIRL, 6, DYNAMIC)
+#else
 #    define _RGBM_DEF_RAINBOW_SWIRL
 #endif
 
-#define _RGBM_DEF_SNAKE ,(SNAKE, 6, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_SNAKE
-#    undef _RGBM_DEF_SNAKE
+#ifdef RGBLIGHT_EFFECT_SNAKE
+#    define _RGBM_DEF_SNAKE ,(SNAKE, 6, DYNAMIC)
+#else
 #    define _RGBM_DEF_SNAKE
 #endif
 
-#define _RGBM_DEF_KNIGHT ,(KNIGHT, 3, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_KNIGHT
-#    undef _RGBM_DEF_KNIGHT
+#ifdef RGBLIGHT_EFFECT_KNIGHT
+#    define _RGBM_DEF_KNIGHT ,(KNIGHT, 3, DYNAMIC)
+#else
 #    define _RGBM_DEF_KNIGHT
 #endif
 
-#define _RGBM_DEF_CHRISTMAS ,(CHRISTMAS, 1, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_CHRISTMAS
-#    undef _RGBM_DEF_CHRISTMAS
+#ifdef RGBLIGHT_EFFECT_CHRISTMAS
+#    define _RGBM_DEF_CHRISTMAS ,(CHRISTMAS, 1, DYNAMIC)
+#else
 #    define _RGBM_DEF_CHRISTMAS
 #endif
 
-#define _RGBM_DEF_STATIC_GRADIENT ,(STATIC_GRADIENT, 10, STATIC)
-#ifndef RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    undef _RGBM_DEF_STATIC_GRADIENT
+#ifdef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define _RGBM_DEF_STATIC_GRADIENT ,(STATIC_GRADIENT, 10, STATIC)
+#else
 #    define _RGBM_DEF_STATIC_GRADIENT
 #endif
 
-#define _RGBM_DEF_RGB_TEST ,(RGB_TEST, 1, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_RGB_TEST
-#    undef _RGBM_DEF_RGB_TEST
+#ifdef RGBLIGHT_EFFECT_RGB_TEST
+#    define _RGBM_DEF_RGB_TEST ,(RGB_TEST, 1, DYNAMIC)
+#else
 #    define _RGBM_DEF_RGB_TEST
 #endif
 
-#define _RGBM_DEF_ALTERNATING ,(ALTERNATING, 1, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_ALTERNATING
-#    undef _RGBM_DEF_ALTERNATING
+#ifdef RGBLIGHT_EFFECT_ALTERNATING
+#    define _RGBM_DEF_ALTERNATING ,(ALTERNATING, 1, DYNAMIC)
+#else
 #    define _RGBM_DEF_ALTERNATING
 #endif
 
-#define _RGBM_DEF_TWINKLE ,(TWINKLE, 6, DYNAMIC)
-#ifndef RGBLIGHT_EFFECT_TWINKLE
-#    undef _RGBM_DEF_TWINKLE
+#ifdef RGBLIGHT_EFFECT_TWINKLE
+#    define _RGBM_DEF_TWINKLE ,(TWINKLE, 6, DYNAMIC)
+#else
 #    define _RGBM_DEF_TWINKLE
 #endif
 
 ////  Add a new mode here.
-// #define _RGBM_DEF_<name> ,(<name>, <num_of_submodes>, <STATIC|DYNAMIC>)
-// #ifndef RGBLIGHT_EFFECT_<name>
-// #    undef _RGBM_DEF_<name>
+// #ifdef RGBLIGHT_EFFECT_<name>
+// #    define _RGBM_DEF_<name> ,(<name>, <num_of_submodes>, <STATIC|DYNAMIC>)
+// #else
 // #    define _RGBM_DEF_<name>
 // #endif
 // then append new mode into RGBLIGHT_EFECTS__LIST
