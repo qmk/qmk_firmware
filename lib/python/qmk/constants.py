e05f9c4a08cef384989cd20ebe27b66622912055
Erovia <Erovia@users.noreply.github.com>
2021-07-10 16:04:50 +0100

    CLI: Add git and venv info to doctor's output (#13405)
    Most of the checks are saved from zvecr's retired 'up/status'
    subcommand PR.

--- lib/python/qmk/constants.py
+++ lib/python/qmk/constants.py
@@ -6,6 +6,9 @@ from pathlib import Path
 # The root of the qmk_firmware tree.
 QMK_FIRMWARE = Path.cwd()
 
+# Upstream repo url
+QMK_FIRMWARE_UPSTREAM = 'qmk/qmk_firmware'
+
 # This is the number of directories under `qmk_firmware/keyboards` that will be traversed. This is currently a limitation of our make system.
 MAX_KEYBOARD_SUBFOLDERS = 5
 
