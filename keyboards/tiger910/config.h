0b6fd7664ec7b17266f84c223269b2d7011b5f9f
Weirdo <1248314361@qq.com>
2021-07-10 16:31:50 +0800

    Update config.h

--- keyboards/tiger910/config.h
+++ keyboards/tiger910/config.h
@@ -28,11 +28,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 5
-#define MATRIX_COLS 15
+#define MATRIX_COLS 16
 
-
-#define MATRIX_COL_PINS { D0, D1, D2, D3, D4, D5, D6, D7, B0, B1, B2, B3, B4, B5, B6 }
-#define MATRIX_ROW_PINS { B7, C1, C2, C3, C4 }
+#define MATRIX_ROW_PINS { B0, B1, B2, B3, B4 }
+#define MATRIX_COL_PINS { B5, B6, B7, C0, C1, C2, C3, C4, C5, C6, C7, D0, D1, D2, D3, D4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
