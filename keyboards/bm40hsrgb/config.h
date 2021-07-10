718440e9ed8a0a539c195b4451a4e9428f5325a7
usrfriendly <2502060+usrfriendly@users.noreply.github.com>
2021-07-10 14:51:17 -0700

    Ported kprepublic bm40hsrgb (#28)
    * ported bm40hsrgb
    
    * cleaned up keyboard-layout.json

--- keyboards/bm40hsrgb/config.h
+++ keyboards/bm40hsrgb/config.h
@@ -53,3 +53,10 @@
 #ifndef RGB_DISABLE_WHEN_USB_SUSPENDED
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
 #endif
+
+/* Configuration for Vial */
+#define VIAL_KEYBOARD_UID {0x6A, 0x02, 0xE7, 0x64, 0x1D, 0x59, 0x0D, 0xC8}
+
+// to unlock the keyboard
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
