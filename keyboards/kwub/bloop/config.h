5a1317410102742a31f58feb84e71c045fd10c92
Drashna Jael're <drashna@live.com>
2021-07-10 22:22:51 -0700

    Cleanup PR

--- keyboards/kwub/bloop/config.h
+++ keyboards/kwub/bloop/config.h
@@ -19,19 +19,21 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x6B77 // Kwub
-#define PRODUCT_ID      0x0001
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Kwub
-#define PRODUCT         Bloop65
+#define VENDOR_ID    0x6B77  // Kwub
+#define PRODUCT_ID   0x0001
+#define DEVICE_VER   0x0001
+#define MANUFACTURER Kwub
+#define PRODUCT      Bloop65
 
 /* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
+#define MATRIX_ROWS  5
+#define MATRIX_COLS  15
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { F5, F4, C6, C7, D7 }
-#define MATRIX_COL_PINS { B4, B5, B6, F6, F1, F7, F0, B0, B7, D3, D2, D1, D5, D4, D6 }
+#define MATRIX_ROW_PINS \
+    { F5, F4, C6, C7, D7 }
+#define MATRIX_COL_PINS \
+    { B4, B5, B6, F6, F1, F7, F0, B0, B7, D3, D2, D1, D5, D4, D6 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
@@ -40,7 +42,7 @@
 /* number of backlight levels */
 
 #ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 3
+#    define BACKLIGHT_LEVELS 3
 #endif
 
 /* Set 0 if debouncing isn't needed */
@@ -53,9 +55,9 @@
 #define LOCKING_RESYNC_ENABLE
 
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM        0
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
 #endif
