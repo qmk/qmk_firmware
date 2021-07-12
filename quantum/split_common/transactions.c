0d7b986da6fb2d88ba150418b88b7c39a928f103
Joakim Tufvegren <jocke@barbanet.com>
2021-07-12 14:23:02 +0200

    Make solo half of split keyboards (more) usable.
    Using only one half of a split keyboard (that's using the split_common
    framework to communicate) is not a great experience, since several read
    timeouts per scan cycle cause an unusably slow scan rate.
    
    This change blocks all split communication attempts for 500 ms
    (configurable) after an error occurs, causing the scan rate to become at
    least _more_ usable, but might need some tweaking to work fully on most
    keyboards. One read timeout still needs to occur after the 500 ms has
    passed, and if that timeout isn't low enough, some scan cycles may still
    be too slow.

--- quantum/split_common/transactions.c
+++ quantum/split_common/transactions.c
@@ -31,6 +31,10 @@
 #    define FORCED_SYNC_THROTTLE_MS 100
 #endif  // FORCED_SYNC_THROTTLE_MS
 
+#ifndef SYNC_CONNECTION_CHECK_TIMEOUT
+#    define SYNC_CONNECTION_CHECK_TIMEOUT 500
+#endif
+
 #define sizeof_member(type, member) sizeof(((type *)NULL)->member)
 
 #define trans_initiator2target_initializer_cb(member, cb) \
@@ -41,8 +45,8 @@
     { &dummy, 0, 0, sizeof_member(split_shared_memory_t, member), offsetof(split_shared_memory_t, member), cb }
 #define trans_target2initiator_initializer(member) trans_target2initiator_initializer_cb(member, NULL)
 
-#define transport_write(id, data, length) transport_execute_transaction(id, data, length, NULL, 0)
-#define transport_read(id, data, length) transport_execute_transaction(id, NULL, 0, data, length)
+#define transport_write(id, data, length) transport_transaction(id, data, length, NULL, 0)
+#define transport_read(id, data, length) transport_transaction(id, NULL, 0, data, length)
 
 #if defined(SPLIT_TRANSACTION_IDS_KB) || defined(SPLIT_TRANSACTION_IDS_USER)
 // Forward-declare the RPC callback handlers
@@ -52,6 +56,28 @@ void slave_rpc_exec_callback(uint8_t initiator2target_buffer_size, const void *i
 
 ////////////////////////////////////////////////////
 // Helpers
+
+bool transport_transaction(int8_t id, const void *initiator2target_buf, uint16_t initiator2target_length, void *target2initiator_buf, uint16_t target2initiator_length) {
+    // Throttle transaction attempts if target isn't connected
+    // Without this, a solo half becomes unusable due to constant read timeouts
+    static bool target_connected = true;
+    static uint16_t connection_check_timer = 0;
+    if (!target_connected && timer_elapsed(connection_check_timer) < SYNC_CONNECTION_CHECK_TIMEOUT) {
+        return false;
+    }
+    connection_check_timer = timer_read();
+
+    if (!transport_execute_transaction(id, initiator2target_buf, initiator2target_length, target2initiator_buf, target2initiator_length)) {
+        target_connected = false;
+        dprintln("Target disconnected, throttling connection attempts");
+        return false;
+    } else if (!target_connected) {
+        dprintln("Target connected");
+    }
+
+    target_connected = true;
+    return true;
+}
 
 bool transaction_handler_master(bool okay, matrix_row_t master_matrix[], matrix_row_t slave_matrix[], const char *prefix, bool (*handler)(matrix_row_t master_matrix[], matrix_row_t slave_matrix[])) {
     if (okay) {
