e11f740062375272c4889a7bf6101827b129ed15
drhigsby <doctorhigsby@gmail.com>
2021-07-12 10:45:11 -0700

    updating layout names
    I am updating the layout names to make it even clearer which one should be chosen for building custom keymaps. "Flipped" was not clear enough to imply that the PM is soldered onto the left side of the PCB.

--- keyboards/ogurec/keymaps/default_left/keymap.c
+++ keyboards/ogurec/keymaps/default_left/keymap.c
@@ -55,19 +55,19 @@ combo_t key_combos[COMBO_COUNT] = {
 #define xxx KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_ortho_3x12_flipped(
+    [_BASE] = LAYOUT_ortho_3x12_left(
         KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A, KC_S, KC_D, KC_F, KC_G, FN, KC_QUOT, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
         LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, NUM, KC_SPC, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH)
   ),
 
-    [_NUM] = LAYOUT_ortho_3x12_flipped(
+    [_NUM] = LAYOUT_ortho_3x12_left(
         KC_1, KC_2, KC_3, KC_4, KC_5, KC_HOME, KC_PGUP, KC_6, KC_7, KC_8, KC_9, KC_0,
         KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_END, KC_PGDN, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,
         KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_LCBR, KC_LT, KC_GT, KC_RCBR, KC_PIPE, KC_BSLS, KC_DQUO, KC_QUOT
   ),
 
-    [_FN] = LAYOUT_ortho_3x12_flipped(
+    [_FN] = LAYOUT_ortho_3x12_left(
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
         xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx,
         KC_VOLD, KC_VOLU, KC_MSTP, KC_MPLY, KC_MPRV, KC_MNXT, KC_MUTE, KC_MRWD, KC_MFFD, xxx, xxx, xxx       
