2a75af52475b7e4febc36b9c5c960c15968bbf31
Koobaczech <67293963+Koobaczech@users.noreply.github.com>
2021-07-14 13:28:06 -0400

    Update keyboards/pearlboards/pandora/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/pearlboards/pandora/config.h
+++ keyboards/pearlboards/pandora/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0002
 #define MANUFACTURER Koobaczech
 #define PRODUCT Pandora
-#define DESCRIPTION Premium 60% with an encoder, rgb, usb passthrough 10u support and lock indicators
 
 /* key matrix size */
 #define MATRIX_ROWS 5
