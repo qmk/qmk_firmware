c10fa44946ee4aa7a6492935f432645548915744
U-LUNA\Luis Godinez <beantek@gmail.com>
2021-07-13 23:19:37 -0700

    Fix compiling issues
    Add return value to bool

--- keyboards/ungodly/launch_pad/keymaps/warzone/keymap.c
+++ keyboards/ungodly/launch_pad/keymaps/warzone/keymap.c
@@ -73,12 +73,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // (Optional) Rotary Encoder, Volume Control
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (clockwise) {
       tap_code(KC_VOLU);
   } else {
       tap_code(KC_VOLD);
   }
+  return true;
 }
 
 // Potentiometer Slider, MIDI Control
