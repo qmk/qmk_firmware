c914f99856b2660e1cc377fb53925788a37d89ac
Tanuj Palaspagar <tanuj.palaspagar@gmail.com>
2021-07-14 08:58:11 +0530

    my keymap (#40)
    * Added personal keymap
    
    updated thrice
    
    * deleted old map
    
    * new map file
    
    * map touch-up
    
    * concept update
    
    * ironed out bugs
    
    * Layer_2 edits
    
    optimised layout on layer 2
    base layer changed to esc, for using tap-arrows on right bottom cluster
    added tab for ease of access

---
+++ keyboards/annepro2/keymaps/CarmineCrown/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define PERMISSIVE_HOLD
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 140
+
+// #define RETRO_TAPPING
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 3
+
+#define CAPS_LOCATION (MATRIX_COLS * 0 + 0)
