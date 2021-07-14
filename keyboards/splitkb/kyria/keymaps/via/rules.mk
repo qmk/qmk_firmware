2910f9e29df75190a55916ca74e13fd7f2ab38e5
Drashna Jael're <drashna@live.com>
2021-07-14 11:52:09 -0700

    More via fixes

--- keyboards/splitkb/kyria/keymaps/via/rules.mk
+++ keyboards/splitkb/kyria/keymaps/via/rules.mk
@@ -2,3 +2,5 @@ OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 VIA_ENABLE = yes
+NKRO_ENABLE = yes
+KEY_LOCK_ENABLE = no
