a0076b43effb083cd73a6b829b47dc1db700b93f
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-07-13 19:45:57 -0700

    rename LAYOUT_2_shifts to LAYOUT_all

--- keyboards/acr60/acr60.h
+++ keyboards/acr60/acr60.h
@@ -62,7 +62,7 @@
     { ___, K41, ___, K43, ___, ___, K46, ___, ___, ___, K4A, K4B, ___, ___, ___ }  \
 }
 
-#define LAYOUT_2_shifts( \
+#define LAYOUT_all( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
     K10, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E,      \
     K20, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      K2D,      \
