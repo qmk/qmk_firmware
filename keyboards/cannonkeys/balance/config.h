241dd8be146a27203299b53b4b2d030a7f2cff14
Andrew Kannan <andrew.kannan@klaviyo.com>
2021-07-13 23:05:27 -0400

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/cannonkeys/balance/config.h
+++ keyboards/cannonkeys/balance/config.h
@@ -23,7 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER CannonKeys
 #define PRODUCT Balance
-#define DESCRIPTION Balance Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
