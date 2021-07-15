c6a6d29213e4c6f0965da302a4fafa0811c3ef4f
FateEverywhere <41215461+FateEverywhere@users.noreply.github.com>
2021-07-15 11:17:05 -0400

    Last few keymap fixes, I hope.

--- keyboards/edi/hardlight/mk1/keymaps/default/config.h
+++ keyboards/edi/hardlight/mk1/keymaps/default/config.h
@@ -17,8 +17,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#undef  LEADER_TIMEOUT
-#define LEADER_TIMEOUT 250
-#define LEADER_PER_KEY_TIMING
-
 // place overrides here
--- keyboards/edi/hardlight/mk1/mk1.c
+++ keyboards/edi/hardlight/mk1/mk1.c
@@ -15,4 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "hardlight.h"
+#include "mk1.h"
