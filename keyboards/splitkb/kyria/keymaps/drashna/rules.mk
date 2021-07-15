17d598d8fe0752e3f68c72ef9a5fbed55cad3c18
Drashna Jael're <drashna@live.com>
2021-07-14 20:55:13 -0700

    Reduce kyria firmware size

--- keyboards/splitkb/kyria/keymaps/drashna/rules.mk
+++ keyboards/splitkb/kyria/keymaps/drashna/rules.mk
@@ -3,13 +3,15 @@ ENCODER_ENABLE = yes       # ENables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 RGBLIGHT_STARTUP_ANIMATION = no
 
-BOOTMAGIC_ENABLE = lite    # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = no    # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes          # USB Nkey Rollover
-UNICODE_ENABLE = yes       # Unicode
-BOOTLOADER = atmel-dfu
+UNICODE_ENABLE = no       # Unicode
+KEY_LOCK_ENABLE = no
 
+
+BOOTLOADER = atmel-dfu
 # TAP_DANCE_ENABLE = yes
