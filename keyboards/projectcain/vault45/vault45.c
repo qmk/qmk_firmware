69d9bd3bcdf1a22e5376627b0b3ad861ea7baccf
projectcain <71684775+projectcain@users.noreply.github.com>
2021-07-15 17:20:45 -0500

    Update keyboards/projectcain/vault45/vault45.c
    Co-authored-by: ridingqwerty <george.g.koenig@gmail.com>

--- keyboards/projectcain/vault45/vault45.c
+++ keyboards/projectcain/vault45/vault45.c
@@ -17,7 +17,7 @@
 #include "vault45.h"
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise) { return false; }
+    if (!encoder_update_user(index, clockwise)) { return false; }
     if (index == 0) { /* First encoder */
         if (clockwise) {
             tap_code(KC_PGDN);
