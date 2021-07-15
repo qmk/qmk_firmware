f04ae2ffca8f69cb3d3aad78cbe3ab10300ad368
N2-Sumikko <54104281+N2-Sumikko@users.noreply.github.com>
2021-07-15 12:28:12 +0900

    Update keyboards/halfcliff/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/halfcliff/keymaps/via/keymap.c
+++ keyboards/halfcliff/keymaps/via/keymap.c
@@ -14,9 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-//#include "keymap_jp.h"
-#include <stdio.h>
-#include "keycode.h"
 #include "keymap_jp.h"
 
 #define RAISE FN_MO13
