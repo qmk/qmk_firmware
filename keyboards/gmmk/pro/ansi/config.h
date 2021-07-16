0bed575c6523e2a1e11b2b38e319b0a22d37842e
Jasper Chan <jasperchan515@gmail.com>
2021-07-16 11:54:44 -0700

    gmmk/pro: Split ANSI and ISO PCBs into different revisions, properly support RGB on ISO

--- keyboards/gmmk/pro/ansi/config.h
+++ keyboards/gmmk/pro/ansi/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+/* Copyright 2021 Gigahawk
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,4 +13,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "pro.h"
+
+#pragma once
+
+#include "config_common.h"
+
+#define DRIVER_1_LED_TOTAL 66
+#define DRIVER_2_LED_TOTAL 32
+#define DRIVER_LED_TOTAL (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
