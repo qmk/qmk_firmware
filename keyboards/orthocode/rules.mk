8c52f4b19ec813e3f69127e9446e2f3e11b828f8
Jordan Blackbourn <blackbournj@gmail.com>
2021-07-16 19:46:50 +1200

    Update keyboards/orthocode/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/orthocode/rules.mk
+++ keyboards/orthocode/rules.mk
@@ -24,5 +24,3 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes        # Enables the use of one or more encoders
-
-LAYOUTS = orthocode
