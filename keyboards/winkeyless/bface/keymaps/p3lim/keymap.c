d53fed566388e84369ef6353c81ded24fe1fc34d
Adrian L Lange <mail@p3lim.net>
2021-07-17 16:16:39 +0200

    Update licence

--- keyboards/winkeyless/bface/keymaps/p3lim/keymap.c
+++ keyboards/winkeyless/bface/keymaps/p3lim/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Adrian L Lange <legal@p3lim.net>
+Copyright 2019-2021 Adrian L Lange <legal@p3lim.net>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
