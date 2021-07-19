1967b29e9a6dce23e8ff31bdb309cc6e98fdfad3
Don Kjer <don.kjer@gmail.com>
2021-07-18 20:24:19 -0700

    Fix for unit tests failing to build on linux

--- tmk_core/common/test/rules.mk
+++ tmk_core/common/test/rules.mk
@@ -18,6 +18,7 @@ eeprom_stm32_large_INC := $(eeprom_stm32_INC)
 eeprom_stm32_SRC := \
 	$(TMK_PATH)/common/test/eeprom_stm32_tests.cpp \
 	$(TMK_PATH)/common/test/flash_stm32_mock.c \
+	$(TMK_PATH)/common/debug.c \
 	$(TMK_PATH)/common/chibios/eeprom_stm32.c
 eeprom_stm32_tiny_SRC := $(eeprom_stm32_SRC)
 eeprom_stm32_large_SRC := $(eeprom_stm32_SRC)
