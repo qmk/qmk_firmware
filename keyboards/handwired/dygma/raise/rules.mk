693117b2188e19d4d434eda0cdfc4b4a52fc11cd
Islam Sharabash <islam.sharabash@gmail.com>
2021-07-19 02:42:51 -0700

    Enable mouse keys

--- keyboards/handwired/dygma/raise/rules.mk
+++ keyboards/handwired/dygma/raise/rules.mk
@@ -14,6 +14,13 @@ CUSTOM_MATRIX = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = custom
 
+# TODO(ibash) we don't actually need to enable raw, but there's some side effect
+# in the usb driver this triggers that allows mousekeys to work. The same side
+# effect happens if console or midi is enabled -- so something to do with
+# alternate usb endpoints.
+RAW_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
 QUANTUM_LIB_SRC += i2c_master.c
 SRC += leds.c
 SRC += matrix.c
