0e0b19f866292ab65687b8e206607e25cfd3fb6c
Drashna Jael're <drashna@live.com>
2021-07-20 13:29:00 -0700

    Fix missing closing bracket

--- quantum/eeconfig.c
+++ quantum/eeconfig.c
@@ -146,94 +146,95 @@ bool eeconfig_is_disabled(void) {
     }
 #endif
     return is_eeprom_disabled;
+}
 
-    /** \brief eeconfig read debug
-     *
-     * FIXME: needs doc
-     */
-    uint8_t eeconfig_read_debug(void) { return eeprom_read_byte(EECONFIG_DEBUG); }
-    /** \brief eeconfig update debug
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_debug(uint8_t val) { eeprom_update_byte(EECONFIG_DEBUG, val); }
+/** \brief eeconfig read debug
+ *
+ * FIXME: needs doc
+ */
+uint8_t eeconfig_read_debug(void) { return eeprom_read_byte(EECONFIG_DEBUG); }
+/** \brief eeconfig update debug
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_debug(uint8_t val) { eeprom_update_byte(EECONFIG_DEBUG, val); }
 
-    /** \brief eeconfig read default layer
-     *
-     * FIXME: needs doc
-     */
-    uint8_t eeconfig_read_default_layer(void) { return eeprom_read_byte(EECONFIG_DEFAULT_LAYER); }
-    /** \brief eeconfig update default layer
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_default_layer(uint8_t val) { eeprom_update_byte(EECONFIG_DEFAULT_LAYER, val); }
+/** \brief eeconfig read default layer
+ *
+ * FIXME: needs doc
+ */
+uint8_t eeconfig_read_default_layer(void) { return eeprom_read_byte(EECONFIG_DEFAULT_LAYER); }
+/** \brief eeconfig update default layer
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_default_layer(uint8_t val) { eeprom_update_byte(EECONFIG_DEFAULT_LAYER, val); }
 
-    /** \brief eeconfig read keymap
-     *
-     * FIXME: needs doc
-     */
-    uint16_t eeconfig_read_keymap(void) { return (eeprom_read_byte(EECONFIG_KEYMAP_LOWER_BYTE) | (eeprom_read_byte(EECONFIG_KEYMAP_UPPER_BYTE) << 8)); }
-    /** \brief eeconfig update keymap
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_keymap(uint16_t val) {
-        eeprom_update_byte(EECONFIG_KEYMAP_LOWER_BYTE, val & 0xFF);
-        eeprom_update_byte(EECONFIG_KEYMAP_UPPER_BYTE, (val >> 8) & 0xFF);
-    }
+/** \brief eeconfig read keymap
+ *
+ * FIXME: needs doc
+ */
+uint16_t eeconfig_read_keymap(void) { return (eeprom_read_byte(EECONFIG_KEYMAP_LOWER_BYTE) | (eeprom_read_byte(EECONFIG_KEYMAP_UPPER_BYTE) << 8)); }
+/** \brief eeconfig update keymap
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_keymap(uint16_t val) {
+    eeprom_update_byte(EECONFIG_KEYMAP_LOWER_BYTE, val & 0xFF);
+    eeprom_update_byte(EECONFIG_KEYMAP_UPPER_BYTE, (val >> 8) & 0xFF);
+}
 
-    /** \brief eeconfig read audio
-     *
-     * FIXME: needs doc
-     */
-    uint8_t eeconfig_read_audio(void) { return eeprom_read_byte(EECONFIG_AUDIO); }
-    /** \brief eeconfig update audio
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_audio(uint8_t val) { eeprom_update_byte(EECONFIG_AUDIO, val); }
+/** \brief eeconfig read audio
+ *
+ * FIXME: needs doc
+ */
+uint8_t eeconfig_read_audio(void) { return eeprom_read_byte(EECONFIG_AUDIO); }
+/** \brief eeconfig update audio
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_audio(uint8_t val) { eeprom_update_byte(EECONFIG_AUDIO, val); }
 
-    /** \brief eeconfig read kb
-     *
-     * FIXME: needs doc
-     */
-    uint32_t eeconfig_read_kb(void) { return eeprom_read_dword(EECONFIG_KEYBOARD); }
-    /** \brief eeconfig update kb
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_kb(uint32_t val) { eeprom_update_dword(EECONFIG_KEYBOARD, val); }
+/** \brief eeconfig read kb
+ *
+ * FIXME: needs doc
+ */
+uint32_t eeconfig_read_kb(void) { return eeprom_read_dword(EECONFIG_KEYBOARD); }
+/** \brief eeconfig update kb
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_kb(uint32_t val) { eeprom_update_dword(EECONFIG_KEYBOARD, val); }
 
-    /** \brief eeconfig read user
-     *
-     * FIXME: needs doc
-     */
-    uint32_t eeconfig_read_user(void) { return eeprom_read_dword(EECONFIG_USER); }
-    /** \brief eeconfig update user
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_user(uint32_t val) { eeprom_update_dword(EECONFIG_USER, val); }
+/** \brief eeconfig read user
+ *
+ * FIXME: needs doc
+ */
+uint32_t eeconfig_read_user(void) { return eeprom_read_dword(EECONFIG_USER); }
+/** \brief eeconfig update user
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_user(uint32_t val) { eeprom_update_dword(EECONFIG_USER, val); }
 
-    /** \brief eeconfig read haptic
-     *
-     * FIXME: needs doc
-     */
-    uint32_t eeconfig_read_haptic(void) { return eeprom_read_dword(EECONFIG_HAPTIC); }
-    /** \brief eeconfig update haptic
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_haptic(uint32_t val) { eeprom_update_dword(EECONFIG_HAPTIC, val); }
+/** \brief eeconfig read haptic
+ *
+ * FIXME: needs doc
+ */
+uint32_t eeconfig_read_haptic(void) { return eeprom_read_dword(EECONFIG_HAPTIC); }
+/** \brief eeconfig update haptic
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_haptic(uint32_t val) { eeprom_update_dword(EECONFIG_HAPTIC, val); }
 
-    /** \brief eeconfig read split handedness
-     *
-     * FIXME: needs doc
-     */
-    bool eeconfig_read_handedness(void) { return !!eeprom_read_byte(EECONFIG_HANDEDNESS); }
-    /** \brief eeconfig update split handedness
-     *
-     * FIXME: needs doc
-     */
-    void eeconfig_update_handedness(bool val) { eeprom_update_byte(EECONFIG_HANDEDNESS, !!val); }
+/** \brief eeconfig read split handedness
+ *
+ * FIXME: needs doc
+ */
+bool eeconfig_read_handedness(void) { return !!eeprom_read_byte(EECONFIG_HANDEDNESS); }
+/** \brief eeconfig update split handedness
+ *
+ * FIXME: needs doc
+ */
+void eeconfig_update_handedness(bool val) { eeprom_update_byte(EECONFIG_HANDEDNESS, !!val); }
