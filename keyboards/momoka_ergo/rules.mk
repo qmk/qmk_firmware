1af7063463fbb574710cefd8e3e7fe7cd22bbf00
StefanGrindelwald <42373600+StefanGrindelwald@users.noreply.github.com>
2021-07-20 15:27:44 +0800

    Update keyboards/momoka_ergo/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/momoka_ergo/rules.mk
+++ keyboards/momoka_ergo/rules.mk
@@ -20,5 +20,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_DRIVER = WS2812
 SPLIT_KEYBOARD = yes
