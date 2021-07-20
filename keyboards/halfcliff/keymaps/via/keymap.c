b5e73dccae693fc7fcbcdc9db816be18b5f81a88
N2-sumikko <na0523na731@yahoo.co.jp>
2021-07-20 23:38:07 +0900

    Modify keymap.c

--- keyboards/halfcliff/keymaps/via/keymap.c
+++ keyboards/halfcliff/keymaps/via/keymap.c
@@ -28,56 +28,6 @@ enum layer_names {
 /*    _FN */
 };
 
-#ifdef OLED_DRIVER_ENABLE
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_master()) {
-        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-    }
-    return rotation;
-}
-
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-    oled_write_P(qmk_logo, false);
-}
-
-void oled_task_user(void) {
-  if (!is_keyboard_master()) {
-      render_logo();
-  } else {
-      // Host Keyboard Layer Status
-      oled_write_P(PSTR("Layer: "), false);
-      switch (get_highest_layer(layer_state)) {
-          case _DEFAULT:
-              oled_write_P(PSTR("DEFAULT\n"), false);
-              break;
-          case _RAISE:
-              oled_write_P(PSTR("RAISE\n"), false);
-              break;
-          case _LOWER:
-              oled_write_P(PSTR("LOWER\n"), false);
-          break;
-          case _ADJUST:
-              oled_write_P(PSTR("ADJUST\n"), false);
-          break;
-          default:
-              // Or use the write_ln shortcut over adding '\n' to the end of your string
-          oled_write_ln_P(PSTR("Undefined"), false);
-      };
-      // Host Keyboard LED Status
-      led_t led_state = host_keyboard_led_state();
-      oled_write_P(led_state.num_lock ? PSTR("NUMLCK ") : PSTR("       "), false);
-      oled_write_P(led_state.caps_lock ? PSTR("CAPLCK ") : PSTR("       "), false);
-      oled_write_P(led_state.scroll_lock ? PSTR("SCRLCK\n") : PSTR("      \n"), false);
-    }
-}
-#endif
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /* DEFAULT
     * ,--------------------------------------------------------------------------------------------------------------------------------------.
@@ -160,20 +110,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* Left side encoder */
-        if (clockwise) {
-            tap_code(KC_WH_D);
-        } else {
-            tap_code(KC_WH_U);
-        }
-    } else if (index == 1) { /* Right side encoder */
-        if (clockwise) {
-            tap_code(KC_WH_U);
-        } else {
-            tap_code(KC_WH_D);
-        }
-    }
-    return true;
-}
