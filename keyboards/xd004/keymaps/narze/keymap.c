4617192b689dc69596d45abb354587443d4d4edd
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-22 01:41:03 +0700

    Add narze/xd004 keymap

---
+++ keyboards/xd004/keymaps/narze/keymap.c
@@ -0,0 +1,13 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    // 0: Base Layer
+    LAYOUT_all(HYPR(KC_F2), HYPR(KC_F1), LCTL(KC_B), LCTL(KC_X)),
+
+};
+
+// Loop
+void matrix_scan_user(void){
+    // Empty
+};
