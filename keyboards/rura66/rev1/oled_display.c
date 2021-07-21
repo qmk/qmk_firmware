030c16e6b1311fc416f78703d4f87f36804cfa4f
Nick Abe <57225836+2Moons-JP@users.noreply.github.com>
2021-07-21 14:17:41 +0900

    Update keyboards/rura66/rev1/oled_display.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/rura66/rev1/oled_display.c
+++ keyboards/rura66/rev1/oled_display.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include QMK_KEYBOARD_H
+#include "quantum.h"
 #include <stdio.h>
 
 // Defines names for use in layer keycodes and the keymap
