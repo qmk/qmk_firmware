0e0ce12d925475e7415d40dab951a2af914d80af
AquaCylinder <56627792+AquaCylinder@users.noreply.github.com>
2021-07-22 20:43:34 +0200

    Addition of copyright and lining up the layout

--- keyboards/Emi20/Emi20.h
+++ keyboards/Emi20/Emi20.h
@@ -1,17 +1,33 @@
 #pragma once
 
+/* Copyright 2021 Aquacylinder 
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
+
 #include "quantum.h"
 
-#define LAYOUT( \
-    K11, K12, K13, K14 \
-    K21, K22, K23, K24 \
-    K31, K32, K33, K34 \
-    K41, K42, K43, K44 \
-    K51, K52, K53, K54 \
+#define LAYOUT_ortho_5x4( \
+	K00, K01, K02, K03, \
+	K10, K11, K12, K13, \
+	K20, K21, K22, K23, \
+	K30, K31, K32, K33, \
+	K40, K41, K42, K43  \
 ) { \
-    { KC_NO, K01,   K02,   K03}, \
-    { KC_NO, K11,   K12,   K13}, \
-    { KC_NO, K21,   K22,   K23}, \
-    { KC_NO, K31,   K32,   K33}, \
-    { KC_NO, K41,   K42,   K43}, \
+	{ K00,   K01,   K02,   K03 }, \
+	{ K10,   K11,   K12,   K13 }, \
+	{ K20,   K21,   K22,   K23 }, \
+	{ K30,   K31,   K32,   K33 }, \
+	{ K40,   K41,   K42,   K43 }  \
 }
