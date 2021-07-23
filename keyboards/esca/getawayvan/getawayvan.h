b871daa170326aee7a01a0046c8ce3a3e1c804bf
ESCA47 <RedYoshi90@gmail.com>
2021-07-23 01:52:35 -0700

    Update keyboards/esca/getawayvan/getawayvan.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/esca/getawayvan/getawayvan.h
+++ keyboards/esca/getawayvan/getawayvan.h
@@ -1,3 +1,19 @@
+/* Copyright 2021 Enrique Cabrera
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
 #pragma once
 
 #include "quantum.h"
@@ -15,19 +31,3 @@
   {K24, K25, K26, K27, K28, K29, K30, K31, K32, K33, K34, K35}, \
   {K36, K37, K38, KNO, K39, KNO, K40, KNO, K41, K42, K43, K45}  \
 }
-
-/* Copyright 2021 Enrique Cabrera 
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
