78b1d82c526ec526267c09a86908044f034aacf2
ESCA47 <RedYoshi90@gmail.com>
2021-07-23 01:53:00 -0700

    Update keyboards/esca/getawayvan/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/esca/getawayvan/rules.mk
+++ keyboards/esca/getawayvan/rules.mk
@@ -1,23 +1,20 @@
-# Bootloader Selection
-Bootloader = stm32-dfu
-
+# Bootloader selection
+BOOTLOADER = stm32-dfu
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-
-
-BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = no	    # Console for debug
-COMMAND_ENABLE = no     # Commands for debug and configuration
-SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	    # USB Nkey Rollover
-CUSTOM_MATRIX = no # Custom matrix file
-BACKLIGHT_ENABLE = no
-RGBLIGHT_ENABLE = yes
-NO_USB_STARTUP_CHECK = no         	# Disable initialization only when usb is plugged in
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 LTO_ENABLE = yes
-
-
