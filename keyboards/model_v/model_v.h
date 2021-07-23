f96cb47afc8a0ae906ebea38525c8a05de190bb3
matthewdias <matthewdias@me.com>
2021-07-22 19:37:26 -0500

    Configurator support

--- keyboards/model_v/model_v.h
+++ keyboards/model_v/model_v.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define XXX KC_NO
 
-#define LAYOUT( \
+#define LAYOUT_split( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
@@ -32,4 +32,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B }, \
     { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B }, \
     { K30, K31, K32, K33, XXX, K35, XXX, K37, K38, K39, K3A, K3B }  \
+}
+
+#define LAYOUT_2x3u( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
+    K30, K31, K32,           K35,      K37,      K39, K3A, K3B  \
+) \
+{ \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B }, \
+    { K30, K31, K32, XXX, XXX, K35, XXX, K37, XXX, K39, K3A, K3B }  \
+}
+
+#define LAYOUT_bigbar( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
+    K30, K31, K32,           K35,                K39, K3A, K3B  \
+) \
+{ \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B }, \
+    { K30, K31, K32, XXX, XXX, K35, XXX, XXX, XXX, K39, K3A, K3B }  \
 }
