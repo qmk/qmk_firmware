4d9082f36ddbb746f771213d60c828b15a7c2ece
Jonavin <code@jonavin.net>
2021-07-23 14:47:40 -0400

    add WinLock fearure with OLED indicator

--- keyboards/mechwild/mercutio/keymaps/jonavin/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/jonavin/keymap.c
@@ -28,6 +28,7 @@ enum custom_layers {
 
 enum custom_keycodes {
   ENCFUNC = SAFE_RANGE, // encoder function keys
+  KC_WINLCK,    //Toggles Win key on and off
 };
 
 // Tap Dance Definitions
@@ -40,6 +41,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_LSFT_CAPSLOCK] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
 };
 
+bool _isWinKeyDisabled = false;
+
 #define KC_LSFTCAPS TD(TD_LSFT_CAPSLOCK)
 #define KC_CAD	LALT(LCTL(KC_DEL))
 #define KC_AF4	LALT(KC_F4)
@@ -49,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_all(
                                                                                                                 KC_MUTE,
     KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    TT(_RAISE),            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSFTCAPS, KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_SFTENT,
+    TT(_RAISE),       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFTCAPS, KC_SLSH, KC_Z,  KC_X,  KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_SFTENT,
     KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  LT(_LOWER,KC_SPC),         KC_SPC,           KC_RALT, MO(_FN1),  KC_RCTL ),
 
   [_FN1] = LAYOUT_all(
@@ -58,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
     KC_CAPS,          KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,   KC_NO,
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NLCK, KC_NO,   KC_NO,   KC_NO,            KC_SFTENT,
-    KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS ),
+    KC_TRNS, KC_WINLCK, KC_TRNS,        KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS ),
 
   [_LOWER] = LAYOUT_all(
                                                                                                                 KC_TRNS,
@@ -69,13 +72,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_all(
                                                                                                                 KC_TRNS,
-  	KC_TAB,          KC_HOME,  KC_UP,   KC_END,  KC_PGUP, KC_PMNS, KC_PPLS, KC_P7,   KC_P8,   KC_P9,    KC_P0,   KC_TRNS,
+  	KC_TAB,          KC_HOME,  KC_UP,   KC_END,  KC_PGUP, KC_PMNS, KC_PPLS, KC_P7,   KC_P8,   KC_P9,    KC_P0,  KC_TRNS,
   	TT(_RAISE),      KC_LEFT,  KC_DOWN, KC_RIGHT,KC_PGDN, KC_PSLS, KC_TAB,  KC_P4,   KC_P5,   KC_P6,   KC_PDOT, KC_PEQL,
-  	KC_TRNS, KC_TRNS, KC_NO,   KC_DEL,  KC_INS,  KC_NO,   KC_PAST, KC_P0, KC_P1,   KC_P2,   KC_P3,            KC_PENT,
+  	KC_TRNS, KC_TRNS, KC_NO,   KC_DEL,  KC_INS,  KC_NO,   KC_PAST, KC_P0,   KC_P1,   KC_P2,   KC_P3,            KC_PENT,
   	KC_TRNS, KC_TRNS, KC_TRNS,          KC_BSPC, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS )
 };
 
-/*  These are needed whether encoder function is enabled or not when ENCFUNC keycode is pressed.
+/*  These are needed whether encoder function is enabled or not when ENCFUNC keycode is pressed.?
     Defaults never changes if no encoder present to change it
 */
 typedef struct {
@@ -123,6 +126,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
             // when keycode is released
         }
+        break;
+    case KC_WINLCK:
+        if (record->event.pressed) {
+            _isWinKeyDisabled = !_isWinKeyDisabled; //toggle status
+            if(_isWinKeyDisabled) {
+                process_magic(GUI_OFF, record);
+            } else {
+                process_magic(GUI_ON, record);
+            }
+        } else  unregister_code16(keycode);
         break;
     }
     return true;
@@ -238,6 +251,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             render_logo();
             oled_set_cursor(8,2);
             char fn_str[12];
+            oled_write_P(_isWinKeyDisabled ? PSTR("WL ") : PSTR(" "), false);
             switch(selected_layer){
                 case 0:
                     oled_write_P(PSTR("BASE"), false);
