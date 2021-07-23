5745ffa77a345668877f34870e6e2feef991fc81
James Won <github@jwon.me>
2021-07-23 07:33:24 -0700

    Update keyboards/kinesis/keymaps/jwon/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kinesis/keymaps/jwon/rules.mk
+++ keyboards/kinesis/keymaps/jwon/rules.mk
@@ -1,7 +1,3 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in 
-#   the appropriate keymap folder that will get included automatically
-#
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = no
+NKRO_ENABLE = yes
