4ab2ca0b183c318ca89b9b428b9a93d15cabf900
fauxpark <fauxpark@gmail.com>
2021-07-25 03:46:43 +1000

    Fix keyboard define checks

--- keyboards/handwired/dactyl_manuform/dactyl_manuform.h
+++ keyboards/handwired/dactyl_manuform/dactyl_manuform.h
@@ -1,20 +1,20 @@
 #pragma once
 
-#ifdef KEYBOARD_handwired_dactyl_manuform_4x5
+#ifdef defined(KEYBOARD_handwired_dactyl_manuform_4x5)
 #    include "4x5.h"
-#elif KEYBOARD_handwired_dactyl_manuform_4x6
+#elif defined(KEYBOARD_handwired_dactyl_manuform_4x6)
 #    include "4x6.h"
-#elif KEYBOARD_handwired_dactyl_manuform_5x6
+#elif defined(KEYBOARD_handwired_dactyl_manuform_5x6)
 #    include "5x6.h"
-#elif KEYBOARD_handwired_dactyl_manuform_5x6_5
+#elif defined(KEYBOARD_handwired_dactyl_manuform_5x6_5)
 #    include "5x6_5.h"
-#elif KEYBOARD_handwired_dactyl_manuform_5x6_2_5
+#elif defined(KEYBOARD_handwired_dactyl_manuform_5x6_2_5)
 #    include "5x6_2_5.h"
-#elif KEYBOARD_handwired_dactyl_manuform_5x7
+#elif defined(KEYBOARD_handwired_dactyl_manuform_5x7)
 #    include "5x7.h"
-#elif KEYBOARD_handwired_dactyl_manuform_6x6
+#elif defined(KEYBOARD_handwired_dactyl_manuform_6x6)
 #    include "6x6.h"
-#elif KEYBOARD_handwired_dactyl_manuform_dmote_62key
+#elif defined(KEYBOARD_handwired_dactyl_manuform_dmote_62key)
 #    include "62key.h"
 #endif
 
