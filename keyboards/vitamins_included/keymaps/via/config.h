bc6ff7223356ec17a78709e1b83d5c8b94bf5ee0
Drashna Jael're <drashna@live.com>
2021-07-24 13:47:26 -0700

    [Keyboard] Fix compile size for the Vitamins Included via keymap

--- keyboards/vitamins_included/keymaps/via/config.h
+++ keyboards/vitamins_included/keymaps/via/config.h
@@ -1,6 +1,7 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
+Copyright 2019 Mikkel Jeppesen
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,12 +18,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-//#define USE_I2C
 
-/* Common USB Device descriptor parameters */
-#define VENDOR_ID       0x1209
-#define PRODUCT_ID      0xBEE5
-#define MANUFACTURER    Duckle29
-#define PRODUCT         Vitamins included
-
-#include "config_common.h"
+#    define NO_MUSIC_MODE
