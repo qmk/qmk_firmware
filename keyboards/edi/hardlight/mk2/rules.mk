ad09a755a268f9ec2ce8710f36e4aa4dd83cf223
FateEverywhere <41215461+FateEverywhere@users.noreply.github.com>
2021-07-25 17:08:30 -0400

    Add Hard Light Mk2 and keymaps
    Hard Light Mk2 is ready. This commit provides firmware that will work on Rev 2F and Mark 2 Hard Light units.

---
+++ keyboards/edi/hardlight/mk2/rules.mk
@@ -0,0 +1,45 @@
+# ©2021 Everywhere Defense Industries / Fate Everywhere <fate@7storm.org>
+ 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# MCU name
+MCU = STM32F072
+
+# Build Options
+#   comment out to disable the options.
+
+BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = yes	# Console for debug
+COMMAND_ENABLE = yes    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+CUSTOM_MATRIX = no      # Custom matrix file
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
+AUDIO_ENABLE = no       # Audio output
+WS2812_DRIVER = pwm
+KEY_LOCK_ENABLE = yes
+VELOCIKEY_ENABLE = yes
+
+# Layout definitions
+LAYOUTS = ortho_4x16
+
+# Wildcard to allow APM32 MCU 
+DFU_SUFFIX_ARGS = -p FFFF -v FFFF
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
