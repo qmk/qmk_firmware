a2ed10f396eda523fdab8c88cacbda5c6d9f3977
e3w2q <50942793+e3w2q@users.noreply.github.com>
2021-07-26 01:13:10 +0900

    Add Kasumigasane

---
+++ keyboards/e3w2q/kasumigasane/keymaps/default/keymap.c
@@ -0,0 +1,112 @@
+/* Copyright 2021 e3w2q
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  SEND_00 = SAFE_RANGE,
+  SEND_000
+};
+
+#define _DEFAULT 0
+#define _LAYER1 1
+
+enum encoder_number {
+  _1ST_ENC = 0,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [0] = LAYOUT( /* Base */ 
+    KC_P7,    KC_P8,    KC_P9,    KC_PMNS,
+    KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+    KC_P1,    KC_P2,    KC_P3,    KC_ENT, 
+    KC_P0,    SEND_00,  KC_PDOT,  KC_COMM
+  ),
+
+  [1] = LAYOUT(
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+  ),
+
+  [2] = LAYOUT(
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+  ),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case SEND_00:
+      if (record->event.pressed) {
+        // when keycode SEND_00 is pressed
+        SEND_STRING("00");
+      } else {
+        // when keycode SEND_00 is released
+      }
+      break;
+    case SEND_000:
+      if (record->event.pressed) {
+        // when keycode SEND_000 is pressed
+        //SEND_STRING("000" SS_TAP(X_ENTER));
+        SEND_STRING("000");
+      } else {
+        // when keycode SEND_000 is released
+      }
+      break;
+  }
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+  if (IS_LAYER_ON(_LAYER1)) {
+    switch (index) {
+      case _1ST_ENC:
+        if (clockwise) {
+          SEND_STRING("1R" SS_TAP(X_ENTER));
+        } else {
+          SEND_STRING("1L" SS_TAP(X_ENTER));
+        }
+        break;
+    }
+  } else {
+    switch (index) {
+      case _1ST_ENC:
+        if (clockwise) {
+          tap_code(KC_RIGHT);
+        } else {
+          tap_code(KC_LEFT);
+        }
+        break;
+    }
+  }
+}
---
+++ keyboards/e3w2q/kasumigasane/readme.md
@@ -0,0 +1,23 @@
+# Kasumigasane
+
+![Kasumigasane](https://github.com/e3w2q/Kasumigasane-keypad/blob/master/mainimage.jpg?raw=true)
+
+A simple 16-key keypad.
+
+Keyboard Maintainer: [e3w2q](https://github.com/e3w2q)  
+Hardware Supported: [Kasumigasane PCB](https://github.com/e3w2q/Kasumigasane-keypad/tree/master/pcb), Pro Micro ATmega32u4  
+Hardware Availability: [Kasumigasane Repository](https://github.com/e3w2q/Kasumigasane-keypad/)
+
+Make example for this keyboard (after setting up your build environment):
+
+    qmk compile -kb e3w2q/kasumigasane -km default
+
+Example of flashing this keyboard:
+
+    qmk flash -kb e3w2q/kasumigasane -km default
+
+Note:  
+  Current firmware is **rev1**.
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
