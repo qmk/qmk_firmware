85be4bfb9439d2c9ea406f91a94db958a80a69c3
FateEverywhere <41215461+FateEverywhere@users.noreply.github.com>
2021-07-25 00:51:41 -0400

    Added edi/hardlight/mk2
    Known-good firmware. Compatible with Rev F and final version Mark 2 units. No audio.

---
+++ keyboards/edi/hardlight/mk2/mk2.c
@@ -0,0 +1,1 @@
+#include "mk2.h"
---
+++ keyboards/edi/hardlight/mk2/rules.mk
@@ -0,0 +1,29 @@
+# MCU name
+MCU = STM32F072
+
+# Build Options
+#   comment out to disable the options.
+
+BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = yes	# Console for debug
+COMMAND_ENABLE = yes    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+CUSTOM_MATRIX = no      # Custom matrix file
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
+AUDIO_ENABLE = no       # Audio output
+WS2812_DRIVER = pwm
+KEY_LOCK_ENABLE = yes
+VELOCIKEY_ENABLE = yes
+
+# Layout definitions
+LAYOUTS = ortho_4x16
+
+# Wildcard to allow APM32 MCU 
+DFU_SUFFIX_ARGS = -p FFFF -v FFFF
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
