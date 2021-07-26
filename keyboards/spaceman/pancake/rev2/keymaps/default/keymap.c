657e7516ea9df6c9e3ff57147d199603b0b3c66a
spaceman <Rionlion100@gmail.com>
2021-07-26 11:47:07 -0400

    Add rev2

---
+++ keyboards/spaceman/pancake/rev2/keymaps/default/keymap.c
@@ -0,0 +1,111 @@
+#include QMK_KEYBOARD_H
+enum layers {
+  _DEFAULT,
+  _LOWER,
+  _RAISE,
+  _FN
+};
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define FN MO(_FN)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Default
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |  Tab |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   "  |  ;   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | GUI  |  FN  | Lower|    Space    | Del  |   /  | Left | Down |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_DEFAULT] = LAYOUT_ortho_4x12(
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,    KC_ENT ,
+    KC_LCTL, KC_LALT, KC_LGUI, FN,      RAISE,   KC_SPC,  KC_SPC,  KC_DEL,   KC_SLSH, KC_LEFT, KC_DOWN,  KC_RGHT
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |  -   |   =   |     |  \   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |   [  |   ]  | Vol- |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      | Vol+ |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = LAYOUT_ortho_4x12(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  _______, KC_BSLS,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_VOLU, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, _______ 
+),
+ 
+ /* FN
+ * ,-----------------------------------------------------------------------------------.
+ * | Reset| F1   |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |  F11 |  F12 |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | TRNS |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FN] = LAYOUT_ortho_4x12(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F11,  KC_F12,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_INC, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_TOGG, BL_DEC, BL_BRTG
+),
+
+
+};
+
+
+#ifdef OLED_DRIVER_ENABLE
+	oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+			return OLED_ROTATION_270;
+	}
+	void render_logo(void) {
+		static const char PROGMEM pancake_logo[] = {
+			0x00, 0x00, 0x3e, 0x0a, 0x04, 0x00, 0x3c, 0x0a, 0x3c, 0x00, 0x3e, 0x0c, 0x18, 0x3e, 0x00, 0x3e, 
+			0x22, 0x22, 0x00, 0x3c, 0x0a, 0x3c, 0x00, 0x3e, 0x08, 0x36, 0x00, 0x3e, 0x2a, 0x22, 0x00, 0x00, 
+			0x00, 0x30, 0xc8, 0x84, 0x84, 0x02, 0x02, 0x02, 0x02, 0x01, 0x01, 0x01, 0x01, 0x01, 0x19, 0x1d, 
+			0x1d, 0x0d, 0x01, 0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x02, 0x84, 0x84, 0xc8, 0x30, 0x00, 
+			0x00, 0x63, 0x94, 0x08, 0x08, 0x11, 0x71, 0x17, 0x13, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x62, 
+			0xe2, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x11, 0x11, 0x11, 0x31, 0x08, 0x08, 0x94, 0x63, 0x00, 
+			0x00, 0x00, 0x03, 0x01, 0x01, 0x02, 0x02, 0x02, 0x02, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 
+			0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x06, 0x02, 0x02, 0x02, 0x01, 0x01, 0x00, 0x00, 0x00
+		};
+		oled_write_raw_P(pancake_logo, sizeof(pancake_logo));
+	}
+	void oled_task_user(void) {
+		// Host Keyboard Layer Status
+		render_logo();
+		oled_set_cursor(0,4);
+		oled_write_P(PSTR("\nLAYER\n-----\n"), false);
+
+		switch (get_highest_layer(layer_state)) {
+				case _DEFAULT:
+						oled_write_P(PSTR("DEFLT\n"), false);
+						break;
+				case _FN:
+						oled_write_P(PSTR("FUNCT\n"), false);
+						break;
+				case _RAISE:
+						oled_write_P(PSTR("RAISE\n"), false);
+						break;
+				default:
+						// Or use the write_ln shortcut over adding '\n' to the end of your string
+						oled_write_ln_P(PSTR("Undefined"), false);
+		}
+	}
+#endif
