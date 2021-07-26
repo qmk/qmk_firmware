d6c17f1728b35b65a411d5e01770141d68963bd8
flare576 <flare576@gmail.com>
2021-07-26 08:45:48 -0500

    fix(TG TO): Better layer swapping

--- keyboards/sofle/keymaps/flare576/keymap.c
+++ keyboards/sofle/keymaps/flare576/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| TRNS  |    | TRNS  |------+------+------+------+------+------|
  * | TRNS |PAUSE |      |      | END  | PGDN |-------|    |-------|MOUS1 |MOUS2 |  [   |   ]  |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | TRNS | TRNS | TRNS |TRNS  | / TRNS  /       \ TRNS \  |MOUS3 |MOUS4 |MOUS5 |      |
+ *            |      | TRNS | TRNS |TRNS  | / TRNS  /       \ TRNS \  |MOUS3 |MOUS4 |MOUS5 |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  KC_PSCR,  XXXXXXX,  KC_MS_U,  KC_HOME,  KC_PGUP,                      KC_WH_U,  KC_WH_D,  KC_WH_L,  KC_WH_R,   XXXXXXX,  _COMP1,
   _______,  KC_SLCK,  KC_MS_L,  KC_MS_D,  KC_MS_R,  XXXXXXX,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  XXXXXXX,  _COMP2,
   _______,  KC_PAUS,  XXXXXXX,  XXXXXXX,  KC_END,   KC_PGDN,  _______,  _______,  KC_BTN1,  KC_BTN2,  KC_LBRC,  KC_RBRC,   XXXXXXX,  XXXXXXX,
-            _______,  _______,  _______,  _______,  _______,                      _______,  KC_BTN3,  KC_BTN4,  KC_BTN5,   XXXXXXX
+            XXXXXXX,  _______,  _______,  _______,  _______,                      _______,  KC_BTN3,  KC_BTN4,  KC_BTN5,   XXXXXXX
 ),
 /* SYMS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,    XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                          XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
   _______,    KC_EXLM,    KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                          KC_CIRC,  KC_AMPR,  KC_ASTR,  XXXXXXX,  XXXXXXX,  XXXXXXX,
   _______,    V_FS,       T_FS,     XXXXXXX,  XXXXXXX,  XXXXXXX,   _______,    _______,   XXXXXXX,  XXXXXXX,  KC_LPRN,  KC_RPRN,  XXXXXXX,  XXXXXXX,
-              _______,    _______,  _______,  _______,  _______,                          _______, _______,   _______,  _______,  _______
+              XXXXXXX,    _______,  _______,  _______,  _______,                          _______, _______,   _______,  _______,  XXXXXXX
 ),
 
 /* MDIA
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_END,   KC_P7,    KC_P8,    KC_P9,    KC_PPLS,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
   KC_LSFT,  KC_PGUP,  KC_P4,    KC_P5,    KC_P6,    KC_PCMM,                      KC_DOWN,  KC_UP,    KC_LEFT,  KC_RIGHT,  XXXXXXX,  XXXXXXX,
   KC_LCTL,  KC_PGDN,  KC_P1,    KC_P2,    KC_P3,    KC_PEQL,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,
-            _______,  KC_P0,    KC_PDOT,  KC_PENT,  _______,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   _______
+            TO(_GAME),KC_P0,    KC_PDOT,  KC_PENT,  _______,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   _______
 ),
 
 /* GAME
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_T,    KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,                        RCTL(KC_Y), RCTL(KC_U), RCTL(KC_I),    RCTL(KC_O),    RCTL(KC_P),    KC_PGUP,
   KC_G,    KC_LSFT,         KC_A,    KC_S,    KC_D,    KC_F,                        RCTL(KC_H), RCTL(KC_J), RCTL(KC_K),    RCTL(KC_L),    RCTL(KC_SCLN), KC_PGDN,
   KC_B,    KC_LCTRL,        KC_Z,    KC_X,    KC_C,    KC_V,  _______,    _______,  RCTL(KC_N), RCTL(KC_M), RCTL(KC_COMM), RCTL(KC_DOT),  RCTL(KC_SLSH), KC_RIGHT,
-           _______,         XXXXXXX, XXXXXXX, KC_LALT, _______,                     _______,    _______,    KC_PSCR,       _______,       _______
+           _______,         XXXXXXX, XXXXXXX, KC_LALT, _______,                     _______,    _______,    KC_PSCR,       _______,       TO(_NPAD)
 )
 };
 
