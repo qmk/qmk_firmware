b2c701a20c043682d633cc36ff9e83820883716c
TreasureTypes <73436366+TreasureTypes@users.noreply.github.com>
2021-07-26 21:32:51 +0200

    Update keyboards/treasure/type9s2/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/treasure/type9s2/keymaps/via/keymap.c
+++ keyboards/treasure/type9s2/keymaps/via/keymap.c
@@ -1,21 +1,27 @@
 #include QMK_KEYBOARD_H
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	
-  [0] = LAYOUT(KC_KP_7, KC_KP_8, KC_KP_9,
-               KC_KP_4, LT(1, KC_KP_5), KC_KP_6,
-               KC_KP_1, KC_KP_2, KC_KP_3),
+    [0] = LAYOUT_ortho_3x3(
+        KC_KP_7, KC_KP_8,        KC_KP_9,
+        KC_KP_4, LT(1, KC_KP_5), KC_KP_6,
+        KC_KP_1, KC_KP_2,        KC_KP_3
+    ),
 
-  [1] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS),
+    [1] = LAYOUT_ortho_3x3(
+        _______, _______, _______,
+        _______, _______, _______,
+        _______, _______, _______
+    ),
 
-  [2] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS),
+    [2] = LAYOUT_ortho_3x3(
+        _______, _______, _______,
+        _______, _______, _______,
+        _______, _______, _______
+    ),
 
-  [3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS,
-               KC_TRNS, KC_TRNS, KC_TRNS),
+    [3] = LAYOUT_ortho_3x3(
+        _______, _______, _______,
+        _______, _______, _______,
+        _______, _______, _______
+    )
 };
