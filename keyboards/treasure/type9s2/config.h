d19cc49095700b36b5402d1a044d1be942d0a19c
TreasureTypes <73436366+TreasureTypes@users.noreply.github.com>
2021-07-27 09:47:36 +0200

    Update keyboards/treasure/type9s2/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/treasure/type9s2/config.h
+++ keyboards/treasure/type9s2/config.h
@@ -48,8 +48,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* number of backlight levels */
 #define BACKLIGHT_LEVELS 3
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
