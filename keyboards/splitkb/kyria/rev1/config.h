24289ffa7421e52d43688d4fc150f46a0f284dec
Albert Y <76888457+filterpaper@users.noreply.github.com>
2021-07-27 09:37:55 +0800

    Update keyboards/splitkb/kyria/rev1/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/splitkb/kyria/rev1/config.h
+++ keyboards/splitkb/kyria/rev1/config.h
@@ -79,7 +79,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define DRIVER_LED_TOTAL RGBLED_NUM // Number of LEDs
 #    define RGB_MATRIX_SPLIT { 10, 10 }
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 170
-#    ifndef RGB_DISABLE_WHEN_USB_SUSPENDED
-#        define RGB_DISABLE_WHEN_USB_SUSPENDED true
-#    endif
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED
 #endif
