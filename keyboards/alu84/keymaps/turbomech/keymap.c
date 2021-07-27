4c959dc9eabbfd15b79bc1c0ccc940b606ddddf1
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-07-26 20:44:00 -0700

    rename LAYOUT to LAYOUT_75_ansi
    Also enables Community Layout Support

--- keyboards/alu84/keymaps/turbomech/keymap.c
+++ keyboards/alu84/keymaps/turbomech/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
  */
 
-    [_QWERTY] = LAYOUT(
+    [_QWERTY] = LAYOUT_75_ansi(
         KC_ESC,  KC_F14,  KC_F15,  C(KC_UP), C(KC_L), KC_F11,  A(G(KC_D)), C(KC_S), C(S(KC_O)), C(G(KC_1)), C(G(KC_2)), C(G(KC_3)), KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU,
         KC_GRV,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,       KC_7,    KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,           KC_BSPC, KC_POWER,
         KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,       KC_U,    KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC, KC_BSLS,          C(G(KC_N)),
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
  */
 
-    [_FUNCTION] = LAYOUT(
+    [_FUNCTION] = LAYOUT_75_ansi(
         A(G(KC_Q)), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_RESET,
         _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_DEL,  C(A(G(KC_S))),
         _______,    RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______,          _______,
