0e584d091cdf522fbbe732a42cb4d4567de4ea5a
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-07-26 20:49:41 -0700

    refactor atomic.h
    - add license header
    - use #pragma once include guard
    - remove redundant file includes

--- keyboards/atomic/atomic.h
+++ keyboards/atomic/atomic.h
@@ -1,10 +1,22 @@
-#ifndef ATOMIC_H
-#define ATOMIC_H
+/* Copyright 2015 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #include "quantum.h"
-#include <stddef.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
 
 // This a shortcut to help you visually see your layout.
 // The following is an example using the Planck MIT layout
@@ -37,5 +49,3 @@
     { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E }, \
     { K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4D, K4E }  \
 }
-
-#endif
