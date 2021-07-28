c14f5a1b720e5b8eb20caf09cf0e421d6cf2464c
Jonavin <code@jonavin.net>
2021-07-27 20:58:14 -0400

    add rgb layer and caps/win lock indicators

--- keyboards/keebio/quefrency/keymaps/jonavin/config.h
+++ keyboards/keebio/quefrency/keymaps/jonavin/config.h
@@ -37,3 +37,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     //Always send Escape if Control is pressed
 
 #define TAPPING_TERM 180
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_LAYERS
+    #define RGBLIGHT_SLEEP
+#endif // RGBLIGHT_ENABLE
+
