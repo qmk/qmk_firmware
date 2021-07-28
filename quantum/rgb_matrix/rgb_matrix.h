d74cd5f0ba244fb67b6daf7cce65adeff62d9e48
Drashna Jael're <drashna@live.com>
2021-07-28 08:21:23 -0700

    Add support for legacy config

--- quantum/rgb_matrix/rgb_matrix.h
+++ quantum/rgb_matrix/rgb_matrix.h
@@ -65,6 +65,10 @@
 #define RGB_MATRIX_TEST_LED_FLAGS() \
     if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) continue
 
+#ifdef RGB_MATRIX_LEGACY_ENABLES
+#include "rgb_matrix_legacy_enables.h"
+#endif
+
 enum rgb_matrix_effects {
     RGB_MATRIX_NONE = 0,
 
