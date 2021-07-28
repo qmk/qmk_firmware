3355c631bbd4d447a328aff6dcf503b85a96e26c
Vanilla <77503059+vanillakeyboards@users.noreply.github.com>
2021-07-28 15:56:27 -0600

    Update keymap.c
    Changed off of the good looking XXX because pull request

--- keyboards/spacey/keymaps/default/keymap.c
+++ keyboards/spacey/keymaps/default/keymap.c
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#define XXX KC_NO
 #define FN MO(_FN)
 
 // Defines names for use in layer keycodes and the keymap
@@ -36,14 +35,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CLCK,KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_UP,
         KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT,KC_SLSH,KC_RSFT, KC_LEFT, KC_DOWN,
-        KC_LCTL, KC_LGUI, FN, KC_MUTE, KC_LALT, KC_SPC,  XXX, KC_BSPC, KC_DEL, XXX,  KC_RALT, KC_RGUI, KC_RCTL, KC_RGHT
+        KC_LCTL, KC_LGUI, FN, KC_MUTE, KC_LALT, KC_SPC,  KC_NO, KC_BSPC, KC_DEL, KC_NO,  KC_RALT, KC_RGUI, KC_RCTL, KC_RGHT
     ),
     [_FN] = LAYOUT(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC,
         KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_DEL,
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_ENT, KC_MS_U,
-        KC_LSFT,XXX, XXX, XXX, XXX, XXX, XXX, XXX, KC_COMM,KC_DOT,KC_SLSH,KC_RSFT, KC_MS_L, KC_MS_D,
-        KC_LCTL, KC_LGUI, XXX, KC_ESC, KC_LALT, KC_SPC,  XXX,  KC_BSPC,   KC_RALT, XXX,     KC_APP, KC_RGUI, KC_RCTL, KC_MS_R
+        KC_LSFT,KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_COMM,KC_DOT,KC_SLSH,KC_RSFT, KC_MS_L, KC_MS_D,
+        KC_LCTL, KC_LGUI, KC_NO, KC_ESC, KC_LALT, KC_SPC,  KC_NO,  KC_BSPC,   KC_RALT, KC_NO,     KC_APP, KC_RGUI, KC_RCTL, KC_MS_R
     )
 };
 
