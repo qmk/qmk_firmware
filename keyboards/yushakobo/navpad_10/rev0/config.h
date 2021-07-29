22a81c87f2a558d8f81c8e17b59fbaf998bd5872
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2021-07-29 18:17:10 +0900

    uodate device versions

--- keyboards/yushakobo/navpad_10/rev0/config.h
+++ keyboards/yushakobo/navpad_10/rev0/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0x3265
 #define PRODUCT_ID   0x0008 //not confirmed
-#define DEVICE_VER   0x0001
+#define DEVICE_VER   0x0000
 #define MANUFACTURER yushakobo
 #define PRODUCT      navpad_1.0
 
