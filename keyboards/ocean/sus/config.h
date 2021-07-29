c18ace0c3378f37f45dfca3fd407d09e63e96891
alabahuy <rezolutionrez@gmail.com>
2021-07-29 18:29:55 +0700

    update diode direction

--- keyboards/ocean/sus/config.h
+++ keyboards/ocean/sus/config.h
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
