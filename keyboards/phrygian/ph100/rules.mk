62700c25f340cfd4be22332cacbe80f7db224ea7
phrygiandesign <77276236+phrygiandesign@users.noreply.github.com>
2021-07-30 20:48:40 +1000

    Update keyboards/phrygian/ph100/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/phrygian/ph100/rules.mk
+++ keyboards/phrygian/ph100/rules.mk
@@ -4,7 +4,7 @@ MCU = STM32F042
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
