f126e1efcc0848d72c46783a6878afb54e73be8c
Stephen J. Bush <muppetjones@gmail.com>
2021-07-31 12:31:35 -0500

    Little more cleanup.

--- keyboards/planck/keymaps/muppetjones/config.h
+++ keyboards/planck/keymaps/muppetjones/config.h
@@ -12,9 +12,6 @@
  * MIDI options
  */
 
-/* Prevent use of disabled MIDI features in the keymap */
-//#define MIDI_ENABLE_STRICT 1
-
 /* enable basic MIDI features:
    - MIDI notes can be sent when in Music mode is on
 */
@@ -29,11 +26,9 @@
 */
 //#define MIDI_ADVANCED
 
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
-
-// Most tactile encoders have detents every 4 stages
-#define ENCODER_RESOLUTION 4
+/*
+ * Other options
+ */
 
 // Enable lighting layers
 #define RGBLIGHT_LAYERS
