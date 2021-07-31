01fa5d941641fd1199b586d4996d06a2ecba14d8
adiabatic <adiabatic@users.noreply.github.com>
2021-07-30 20:58:50 -0700

    Update layouts/community/ergodox/zweihander-macos/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- layouts/community/ergodox/zweihander-macos/rules.mk
+++ layouts/community/ergodox/zweihander-macos/rules.mk
@@ -4,8 +4,4 @@ SLEEP_LED_ENABLE = no
 
 # other debounce types: sym_g (whole-keyboard, default), eager_pr (eager per-row), eager_pk (per-key)
 
-DEBOUNCE_DIR:= $(QUANTUM_DIR)/debounce
-DEBOUNCE_TYPE?= eager_pr
-ifneq ($(strip $(DEBOUNCE_TYPE)), custom)
-    QUANTUM_SRC += $(DEBOUNCE_DIR)/$(strip $(DEBOUNCE_TYPE)).c
-endif
+DEBOUNCE_TYPE = eager_pr
