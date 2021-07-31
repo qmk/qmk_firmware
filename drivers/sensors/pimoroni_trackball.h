308584ea8382ea3fc99c7e29aaad0e67c83d0c72
daskygit <32983009+daskygit@users.noreply.github.com>
2021-07-31 03:49:20 +0100

    fix typo

--- drivers/sensors/pimoroni_trackball.h
+++ drivers/sensors/pimoroni_trackball.h
@@ -23,7 +23,7 @@
 #    define TRACKBALL_ADDRESS 0x0A
 #endif
 #define TRACKBALL_WRITE ((TRACKBALL_ADDRESS << 1) | I2C_WRITE)
-#define TRACKBALL_READ ((TRACKBALL_ADDRESS << 1) | I2C_READ)
+#define TRACKBALL_READ (TRACKBALL_ADDRESS << 1)
 
 void trackball_set_rgbw(uint8_t red, uint8_t green, uint8_t blue, uint8_t white);
 void trackball_check_click(bool pressed, report_mouse_t *mouse);
