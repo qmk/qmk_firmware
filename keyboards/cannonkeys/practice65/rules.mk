c2a68c216a93e56ebcef1101d16953464af3f11f
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-08-01 13:07:04 -0700

    unify commented BOOTMAGIC_ENABLE rules in keyboards
    Explicitly sets `BOOTMAGIC_ENABLE = no` in keyboards where the rule was commented out.
    
    Command:
    
    ```
    find keyboards/ -type f -name 'rules.mk' -and -not -path '*/keymaps/*' -exec sed -i -e 's;#[ \t]*\(BOOTMAGIC_ENABLE\)[ \t=]\+\([a-zA-Z]\+\).*;\1 = no       # Virtual DIP switch configuration;g' {} +
    ```

--- keyboards/cannonkeys/practice65/rules.mk
+++ keyboards/cannonkeys/practice65/rules.mk
@@ -4,7 +4,7 @@ MCU = STM32F103
 # Bootloader selection
 BOOTLOADER = stm32duino
 
-#BOOTMAGIC_ENABLE = full	# Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug
