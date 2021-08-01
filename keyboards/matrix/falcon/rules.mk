741864b3d849ccef3e736d8a8d40db2319a5612f
yulei <yuleiz@gmail.com>
2021-08-01 14:40:15 +0800

    Update keyboards/matrix/falcon/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/matrix/falcon/rules.mk
+++ keyboards/matrix/falcon/rules.mk
@@ -16,12 +16,9 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-MIDI_ENABLE = no            # MIDI support
-UNICODE_ENABLE = no         # Unicode
-AUDIO_ENABLE = no           # Audio output on port C6
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = yes       # Use RGB underglow light
 
 LAYOUTS = 60_hhkb
