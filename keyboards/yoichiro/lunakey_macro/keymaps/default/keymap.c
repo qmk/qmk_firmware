1dcbaae6d3ac1021626b74cb7424bab007b5d780
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-08-02 20:56:11 +0900

    Change the number key codes.

--- keyboards/yoichiro/lunakey_macro/keymaps/default/keymap.c
+++ keyboards/yoichiro/lunakey_macro/keymaps/default/keymap.c
@@ -24,9 +24,9 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBERS] = LAYOUT(
-        KC_KP_1,      KC_KP_2,    KC_KP_3,     KC_KP_4, \
-        KC_KP_5,      KC_KP_6,    KC_KP_7,     KC_KP_8, \
-        KC_KP_9,      KC_KP_0,    DF(_CURSOR) \
+        KC_1,         KC_2,       KC_3,        KC_4, \
+        KC_5,         KC_6,       KC_7,        KC_8, \
+        KC_9,         KC_0,       DF(_CURSOR) \
     ),
     [_CURSOR] = LAYOUT(
         KC_PGUP,      KC_HOME,    KC_UP,       KC_END, \
