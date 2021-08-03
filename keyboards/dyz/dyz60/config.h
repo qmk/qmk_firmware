680c6453e3b262bb910996ba83f64579d83fea39
dayatz <dayat.py@gmail.com>
2021-08-03 16:45:11 +0800

    update product id

--- keyboards/dyz/dyz60/config.h
+++ keyboards/dyz/dyz60/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xD772
-#define PRODUCT_ID      0x0017
+#define PRODUCT_ID      0x000A
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    dyz
 #define PRODUCT         dyz60
