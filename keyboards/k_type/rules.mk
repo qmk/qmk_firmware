1409b368517a8ce9af1320acc75f7a8fbcdfca18
Joakim Tufvegren <jocke@barbanet.com>
2021-08-03 23:38:37 +0200

    Remove the #10088 hotfix for Teensy 3.1-like Input:Club keyboards (#12870)
    * Remove the #10088 hotfix for K20x MCU:s.
    
    It seems to _cause_ the issue it intended to solve there.
    
    * Cleaner way of removing #10088 hotfix.
    
    Now only affects Ergodox Infinity, Whitefox and K-type, though.
    
    Switches over Ergodox Infinity to the `IC_TEENSY_3_1` board, since that
    was a nice place to implement the `restart_usb_driver` override.
    However, I would guess this issue is present for other K20x/Teensy 3.1
    boards as well...
    
    * Fix comment regarding `IC_TEENSY_3_1` for all keyboards using it.

--- keyboards/k_type/rules.mk
+++ keyboards/k_type/rules.mk
@@ -7,7 +7,8 @@ BOOTLOADER = kiibohd
 # Board: it should exist either in <chibios>/os/hal/boards/
 #  or <this_dir>/boards
 # This board was copied from PJRC_TEENSY_3_1. The only difference should be a
-# hack to ensure the watchdog has started before trying to disable it.
+# hack to ensure the watchdog has started before trying to disable it, and an
+# override to disable restart of USB driver after returning from suspend.
 BOARD = IC_TEENSY_3_1
 
 # Build Options
