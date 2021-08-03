335956665cb8faa48af678a4f5ff0b8fd0580026
Cian Johnston <public@cianjohnston.ie>
2021-08-03 11:13:59 +0100

    s/ansi_fullsize/fullsize_ansi/g

--- keyboards/handwired/oem_ansi_fullsize/rules.mk
+++ keyboards/handwired/oem_ansi_fullsize/rules.mk
@@ -21,4 +21,4 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 
-LAYOUTS = ansi_fullsize
+LAYOUTS = fullsize_ansi
