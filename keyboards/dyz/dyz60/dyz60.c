8b9f6c5914a0ded229d64510256b8899880d9b19
dayatz <dayat.py@gmail.com>
2021-08-03 11:44:31 +0800

    update copyright name

--- keyboards/dyz/dyz60/dyz60.c
+++ keyboards/dyz/dyz60/dyz60.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 Ryota Goto
+/* Copyright 2021 dayatz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
