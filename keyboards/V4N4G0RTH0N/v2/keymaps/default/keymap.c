202d85ae87ba0b69b0ddbd1194a54a6a1f55e57e
Juan Puerto <jpuerto96@hotmail.com>
2021-08-04 16:16:39 -0400

    Add v4n slider code

--- keyboards/V4N4G0RTH0N/v2/keymaps/default/keymap.c
+++ keyboards/V4N4G0RTH0N/v2/keymaps/default/keymap.c
@@ -15,10 +15,15 @@
  */
 #include QMK_KEYBOARD_H
 #include "analog.h"
+int16_t max_pot_val = 1023;
+int16_t max_ticks = 20;
 int16_t pot_oldVal = 0;
 int16_t pot_val    = 0;
-#define POT_TOLERANCE 25
+bool moving     = false;
+#define POT_TOLERANCE 50
 #define POT_PIN F0
+#include "print.h"
+
 
 void matrix_init_user(void) {
     analogReference(ADC_REF_POWER);
@@ -29,8 +34,22 @@ void matrix_scan_user(void){
 
     // If there is a big enough change, then we need to do something
     if (abs(pot_val - pot_oldVal) > POT_TOLERANCE) {
-	tap_code(pot_val > pot_oldVal ? KC__VOLUP : KC__VOLDOWN);
-	pot_oldVal = pot_val;
+        moving = true;
+        pot_oldVal = pot_val;
+    }
+    else{
+        if (moving){
+            // Do some fancy conversion to get 'absolute' position to num tap_codes to send
+            // Reset moving to 0 so that we don't get multiple attempts to do this
+            int num_ticks = ((float)pot_val/max_pot_val)*max_ticks;
+            for (int i = 0; i<max_ticks;++i){
+                tap_code(KC__VOLDOWN);
+            }
+            for (int i = 0; i<num_ticks;++i){
+                tap_code(KC__VOLUP);
+            }
+            moving = false;
+        }
     }
 }
 
