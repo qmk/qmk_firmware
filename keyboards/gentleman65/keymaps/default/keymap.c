72643804d124521023d4421bd240600b30b2c9b5
Ramya Challa <ramyac6@cs.washington.edu>
2021-08-03 23:57:20 -0700

    make encoder behavior default

--- keyboards/gentleman65/keymaps/default/keymap.c
+++ keyboards/gentleman65/keymaps/default/keymap.c
@@ -34,13 +34,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 };
-
-void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
-    if (clockwise) {
-      tap_code(KC_AUDIO_VOL_UP);
-    } else {
-      tap_code(KC_AUDIO_VOL_DOWN);
-    }
-  }
-}
