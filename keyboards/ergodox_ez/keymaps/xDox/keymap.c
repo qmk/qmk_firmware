96c448f36c32f856392453ed7a6400c30da431e4
Xiao Yu <xiao@xster.net>
2021-08-05 00:07:19 -0700

    more ergodox improvements

--- keyboards/ergodox_ez/keymaps/xDox/keymap.c
+++ keyboards/ergodox_ez/keymaps/xDox/keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_RBRACKET,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_DELETE,                                      KC_BSPACE,      KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_LBRACKET,
     KC_BSPACE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
-    KC_LSHIFT,      LCTL_T(KC_Z),   LALT_T(KC_X),   LGUI_T(KC_C),   KC_V,           KC_B,           KC_ENTER,                                       LCTL(KC_GRAVE), KC_N,           KC_M,           RGUI_T(KC_COMMA),RALT_T(KC_DOT), RCTL_T(KC_SLASH),KC_RSHIFT,
-    LT(1,KC_GRAVE), KC_UP,          KC_DOWN,        KC_LEFT,        SH_T(KC_RIGHT),                                                                                                       LT(1,KC_UP),    KC_DOWN,        KC_LEFT,        KC_RIGHT,       KC_CAPSLOCK,
+    KC_LSPO,      LCTL_T(KC_Z),   LALT_T(KC_X),     KC_C,           KC_V,           KC_B,           KC_ENTER,                                       LCTL(KC_GRAVE), KC_N,           KC_M,           KC_COMMA,       RALT_T(KC_DOT), RCTL_T(KC_SLASH),KC_RSPC,
+    LT(1,KC_GRAVE), KC_UP,          KC_DOWN,        LT(2,KC_LEFT),        SH_T(KC_RIGHT),                                                                                                       LT(1,KC_UP),    KC_DOWN,        KC_LEFT,        KC_RIGHT,       KC_CAPSLOCK,
                                                                                                     LALT(LCTL(LGUI(LSFT(KC_TAB)))),KC_GRAVE,       KC_MINUS,       KC_EQUAL,
                                                                                                                     KC_BSLASH,      KC_PGUP,
                                                                                     KC_SPACE,       KC_LGUI,        LCTL(KC_GRAVE), KC_PGDOWN,      KC_SPACE,       KC_ENTER
@@ -38,20 +38,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(2,KC_NO),    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_UP,          KC_7,           KC_8,           KC_9,           KC_KP_PLUS,     KC_F12,
     KC_TRANSPARENT, KC_HASH,        LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   LGUI(KC_W),     KC_GRAVE,                                                                       KC_DOWN,        KC_4,           KC_5,           KC_6,           KC_KP_MINUS,    KC_TRANSPARENT,
     KC_TRANSPARENT, KC_PERC,        LGUI(KC_LEFT),  LGUI(KC_RIGHT), LGUI(KC_RIGHT), KC_TILD,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_BSPACE,      KC_1,           KC_2,           KC_3,           KC_KP_ASTERISK, KC_MEDIA_PLAY_PAUSE,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_WH_UP,        KC_MS_WH_DOWN,      KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_KP_0,        RALT_T(KC_KP_DOT),KC_KP_SLASH,    KC_RSHIFT,
+    KC_TRANSPARENT, KC_MS_WH_LEFT,  KC_MS_WH_UP,        KC_MS_WH_DOWN,      KC_MS_WH_RIGHT,                                                                                                 KC_TRANSPARENT, KC_KP_0,        RALT_T(KC_KP_DOT),KC_KP_SLASH,    KC_RSHIFT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_F13,         KC_F15,         KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP
   ),
   [2] = LAYOUT_ergodox_pretty(
     RESET,          LCTL(LSFT(KC_POWER)),KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_UP,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_UP,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,        KC_DOWN,     KC_RIGHT,       KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_BTN2,                                                                                                     KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,  KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_WWW_BACK
+                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LSFT(LALT(KC_AUDIO_VOL_DOWN)), LSFT(LALT(KC_AUDIO_VOL_UP))
   ),
 };
 
