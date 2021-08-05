c4e7a819c31de5b5c687029c39a74781cac7a28a
Juan Pablo Kutianski <jkutianski@gmail.com>
2021-08-05 15:09:11 -0700

    Update tmk_core/protocol/ps2_mouse.c
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- tmk_core/protocol/ps2_mouse.c
+++ tmk_core/protocol/ps2_mouse.c
@@ -148,9 +148,10 @@ static inline void ps2_mouse_convert_report_to_hid(report_mouse_t *mouse_report)
     mouse_report->y = Y_IS_NEG ? ((!Y_IS_OVF && -127 <= mouse_report->y && mouse_report->y <= -1) ? mouse_report->y : -127) : ((!Y_IS_OVF && 0 <= mouse_report->y && mouse_report->y <= 127) ? mouse_report->y : 127);
 
 #ifdef PS2_MOUSE_INVERT_BUTTONS
-    // swap left & rigth buttons & remove sign and overflow flags
-    uint8_t btns_change   = ((mouse_report->buttons >> PS2_MOUSE_BTN_LEFT) ^ (mouse_report->buttons >> PS2_MOUSE_BTN_RIGHT)) & 1;
-    mouse_report->buttons = (mouse_report->buttons ^ ((btns_change << PS2_MOUSE_BTN_LEFT) | (btns_change << PS2_MOUSE_BTN_RIGHT))) & PS2_MOUSE_BTN_MASK;
+    // swap left & right buttons
+    uint8_t needs_left = mouse_report->buttons & PS2_MOUSE_BTN_RIGHT;
+    uint8_t needs_right = mouse_report->buttons & PS2_MOUSE_BTN_LEFT;
+    mouse_report->buttons = (mouse_report->buttons & ~(PS2_MOUSE_BTN_MASK)) | (needs_left ? PS2_MOUSE_BTN_LEFT : 0) | (needs_right ? PS2_MOUSE_BTN_RIGHT : 0);
 #else
     // remove sign and overflow flags
     mouse_report->buttons &= PS2_MOUSE_BTN_MASK;
