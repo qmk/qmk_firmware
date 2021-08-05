843b3cf48de46e95e94b2f38dfc5d2c0f8b8ef5d
morganpa <66316054+morganpa@users.noreply.github.com>
2021-08-04 21:03:18 -0700

    upgrades

--- keyboards/keebio/bfo9000/keymaps/morgan/config.h
+++ keyboards/keebio/bfo9000/keymaps/morgan/config.h
@@ -25,5 +25,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 // #define MASTER_RIGHT
 // #define EE_HANDS
-#define TAPPING_TERM 200
+#define TAPPING_TERM 275
 #define IGNORE_MOD_TAP_INTERRUPT
