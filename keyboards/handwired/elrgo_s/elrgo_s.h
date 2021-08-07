dbe3d8d10af6754683767a07641019cad33fe607
Yaroslav Smirnov <elorenn@bk.ru>
2021-08-07 11:47:57 +0300

    Update keyboards/handwired/elrgo_s/elrgo_s.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/elrgo_s/elrgo_s.h
+++ keyboards/handwired/elrgo_s/elrgo_s.h
@@ -2,14 +2,6 @@
 
 #include "quantum.h"
 
-#ifdef USE_I2C
-#include <stddef.h>
-#ifdef __AVR__
-    #include <avr/io.h>
-    #include <avr/interrupt.h>
-#endif
-#endif
-
 
 #define LAYOUT_6x4(\
     L00, L01, L02, L03, L04, L05,                       R00, R01, R02, R03, R04, R05, \
