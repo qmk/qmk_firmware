446e161b503ce75833beaa876ecaf2e687e813b6
Yaroslav Smirnov <elorenn@bk.ru>
2021-08-07 11:47:48 +0300

    Update keyboards/handwired/elrgo_s/elrgo_s.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/elrgo_s/elrgo_s.c
+++ keyboards/handwired/elrgo_s/elrgo_s.c
@@ -1,12 +1,1 @@
 #include "elrgo_s.h"
-
-#ifdef SSD1306OLED
-void led_set_kb(uint8_t usb_led) {
-    // Put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
-    led_set_user(usb_led);
-}
-#endif
-
-void matrix_init_kb(void) {
-	matrix_init_user();
-};
