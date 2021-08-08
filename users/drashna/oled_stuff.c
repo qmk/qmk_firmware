32fc13023a57bc22c8a49ee7f248b9a0e09d91b7
Drashna Jael're <drashna@live.com>
2021-08-08 12:37:35 -0700

    Fill the oleds with right mods

--- users/drashna/oled_stuff.c
+++ users/drashna/oled_stuff.c
@@ -160,17 +160,23 @@ void render_matrix_scan_rate(void) {
 void render_mod_status(uint8_t modifiers) {
     static const char PROGMEM mod_status[5][3] = {{0xE8, 0xE9, 0}, {0xE4, 0xE5, 0}, {0xE6, 0xE7, 0}, {0xEA, 0xEB, 0}, {0xEC, 0xED, 0}};
     oled_write_P(PSTR(OLED_RENDER_MODS_NAME), false);
-    oled_write_P(mod_status[0], (modifiers & MOD_MASK_SHIFT));
-    oled_write_P(mod_status[!keymap_config.swap_lctl_lgui ? 3 : 4], (modifiers & MOD_MASK_GUI));
+    oled_write_P(mod_status[0], (modifiers & MOD_BIT(KC_LSHIFT)));
+    oled_write_P(mod_status[!keymap_config.swap_lctl_lgui ? 3 : 4], (modifiers & MOD_BIT(KC_LGUI)));
 #if !defined(OLED_DISPLAY_128X64)
     oled_write_P(PSTR(" "), false);
 #endif
-    oled_write_P(mod_status[2], (modifiers & MOD_MASK_ALT));
-    oled_write_P(mod_status[1], (modifiers & MOD_MASK_CTRL));
+    oled_write_P(mod_status[2], (modifiers & MOD_BIT(KC_LALT)));
+    oled_write_P(mod_status[1], (modifiers & MOD_BIT(KC_LCTL)));
 
-    render_matrix_scan_rate();
+//    render_matrix_scan_rate();
 #if defined(OLED_DISPLAY_128X64)
-    oled_advance_page(true);
+    oled_write_P(mod_status[1], (modifiers & MOD_BIT(KC_RCTL)));
+    oled_write_P(mod_status[2], (modifiers & MOD_BIT(KC_RALT)));
+#    if !defined(OLED_DISPLAY_128X64)
+    oled_write_P(PSTR(" "), false);
+#    endif
+    oled_write_P(mod_status[!keymap_config.swap_lctl_lgui ? 3 : 4], (modifiers & MOD_BIT(KC_RGUI)));
+    oled_write_P(mod_status[0], (modifiers & MOD_BIT(KC_RSHIFT)));
 #endif
 }
 
