6991315103290a1123bc619d2c2d07aea4af7bc8
zvecr <git@zvecr.com>
2021-08-09 23:12:49 +0100

    Fix up after rebase

--- tmk_core/common/avr/platform.c
+++ tmk_core/common/avr/platform.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "platform.h"
+#include "platform_deps.h"
 
 void platform_setup(void) {
     // do nothing
