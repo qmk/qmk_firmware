8d1255ccab40eb3bff00fd24079bf92d73687e87
Luigi311 <luigi311.lg@gmail.com>
2021-08-10 15:40:30 -0600

    Handwired/Stream_Cheap/2x4: Add via support (#13297)
    * Handwired/Stream_Cheap/2x4: Add via support
    
    Signed-off-by: Luigi311 <luigi311.lg@gmail.com>
    
    * Handwired/Stream_Cheap: Update readme for via
    
    Signed-off-by: Luigi311 <luigi311.lg@gmail.com>
    
    * Handwired/Stream_Cheap/2x4: Add 2x4 to via json
    
    Signed-off-by: Luigi311 <luigi311.lg@gmail.com>
    
    * Handwired/Stream_Cheap/2x4: Add GPL2+ License to keymap
    
    Signed-off-by: Luigi311 <luigi311.lg@gmail.com>

--- keyboards/handwired/stream_cheap/2x4/rules.mk
+++ keyboards/handwired/stream_cheap/2x4/rules.mk
@@ -20,3 +20,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+LTO_ENABLE = yes            # Enable optimizations
