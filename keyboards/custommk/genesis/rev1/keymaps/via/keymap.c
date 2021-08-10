1496bc4de18b8f040196641425c062b31301bae2
David Hoelscher <infinityis@users.noreply.github.com>
2021-08-10 02:28:50 -0500

    swap + key and PgUp key for VIA keymaps

--- keyboards/custommk/genesis/rev1/keymaps/via/keymap.c
+++ keyboards/custommk/genesis/rev1/keymaps/via/keymap.c
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT_ortho_5x4(
 			MO(1),   KC_PSLS, KC_PAST, KC_PMNS, 
-			KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
-			KC_P4,   KC_P5,   KC_P6,   KC_PGUP, 
+			KC_P7,   KC_P8,   KC_P9,   KC_PGUP, 
+			KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
 			KC_P1,   KC_P2,   KC_P3,   KC_PGDN, 
 			KC_P0,   KC_SPC,  KC_PDOT, KC_PENT),
 
