1ab56c0ac437f05f7396022e25adcb9222b08c3a
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2021-08-11 01:06:46 +0200

    Edited to match advices
    Thanks to sigprof removed locking support and commented D6 and E6 pin issues

--- keyboards/cest73/tkm/config.h
+++ keyboards/cest73/tkm/config.h
@@ -21,27 +21,24 @@
 #define MATRIX_COL_PINS { D1, D2, D3, D4, D5, D6, D7, F0, F1, F4 }
 #define UNUSED_PINS { AF }
 
+//NOTE: if D6 pin shows any issues in exploatation the LED on the Teensy is to be removed
+
 /* the LEDs:
  *  PF5 - CAPS
  *  PF6 - NUM
  *  PF7 - SCLK
  *  PE6 - Backlight */
-
+//TODO change the E6 pin to either B5, B6, B7 or C6 to utilize hardware PWM on a future PCB revision
 #define BACKLIGHT_PIN E6
 
-#define SLEEP_LED E6
+//Not clear how to be used (yet)
+//#define SLEEP_LED E6
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
 /* force n-key rollover*/
 #define FORCE_NKRO
