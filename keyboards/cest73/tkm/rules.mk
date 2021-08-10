46a0e4c49844c9d3acffc2ea97e8750f7c4774f2
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2021-08-11 01:10:34 +0200

    Switched off Unicode support
    following the advice of sigprof and fauxpark

--- keyboards/cest73/tkm/rules.mk
+++ keyboards/cest73/tkm/rules.mk
@@ -22,7 +22,7 @@ COMMAND_ENABLE = no     # Commands for debug and configuration
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes       # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
-UNICODE_ENABLE = yes    #
+UNICODE_ENABLE = no     #
 UNICODEMAP_ENABLE = no  # send Unicode characters using X(<map index>) in your keymap. All code points (up to 0x10FFFF) are supported
 UCIS_ENABLE = no        # send Unicode characters by inputting a mnemonic. All possible code points (up to 0x10FFFF) are supported
 
