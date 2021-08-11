e252a8847211dd703758a43ad9c15d25880bfd6d
fire-h0und <55053506+fire-h0und@users.noreply.github.com>
2021-08-11 09:56:45 +0200

    Update keyboards/cest73/tkm/tkm.c
    double code (defined in config.h)
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/cest73/tkm/tkm.c
+++ keyboards/cest73/tkm/tkm.c
@@ -1,12 +1,1 @@
 #include "tkm.h"
-
-/* the LEDs:  
-*  PF5 - CAPS
-*  PF6 - NUM
-*  PF7 - SCLK
-*  PE6 - Backlight */
-    
-#define LED_NUM_LOCK_PIN F6
-#define LED_CAPS_LOCK_PIN F5
-#define LED_SCROLL_LOCK_PIN F7
-#define LED_PIN_ON_STATE 0
