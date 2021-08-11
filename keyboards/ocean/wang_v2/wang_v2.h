e886e56e73816bea25e97efc5a7fcb64ba7cd5eb
Rifaa Subekti <68178825+oceeean@users.noreply.github.com>
2021-08-11 14:27:18 +0800

    Update keyboards/ocean/wang_v2/wang_v2.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ocean/wang_v2/wang_v2.h
+++ keyboards/ocean/wang_v2/wang_v2.h
@@ -25,7 +25,7 @@
 ) \
 { \
   { K00, K01, K02, K03, K04,   K05,   K06, K07,   K08, K09, K0A, K0B, K0C }, \
-  { K10, K11, K12, K13, K14,   K15,   K16, K17,   K18, K19, K1A, K1B }, \
-  { K20, K21, K22, K23, K24,   K25,   K26, K27,   K28, K29, K2A, K2B }, \
-  { K30, K31, K32, K33, KC_NO, K35,   K36, KC_NO, K38, K39, K3A, K3B }, \
+  { K10, K11, K12, K13, K14,   K15,   K16, K17,   K18, K19, K1A, K1B, KC_NO }, \
+  { K20, K21, K22, K23, K24,   K25,   K26, K27,   K28, K29, K2A, K2B, KC_NO }, \
+  { K30, K31, K32, K33, KC_NO, K35,   K36, KC_NO, K38, K39, K3A, K3B, KC_NO }, \
 }
