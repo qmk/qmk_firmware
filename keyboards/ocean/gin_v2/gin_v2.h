39498be61745c5c91e66bf2ab77e36046fb8d801
Rifaa Subekti <68178825+oceeean@users.noreply.github.com>
2021-08-11 15:12:24 +0800

    Update keyboards/ocean/gin_v2/gin_v2.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ocean/gin_v2/gin_v2.h
+++ keyboards/ocean/gin_v2/gin_v2.h
@@ -17,6 +17,8 @@
 
 #include "quantum.h"
 
+#define XXX KC_NO
+
 #define LAYOUT(\
   K00, K10, K01,  K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, K07, K27, \
   K20, K30, K21,  K31, K22, K32, K23, K33, K24, K34, K25, K35, K26, K36, K47,   \
