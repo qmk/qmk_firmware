c9bdd3784ec23225798707df504ac734ae9796ab
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2021-08-11 08:45:03 -0500

    made requested pr changes

--- layouts/community/ergodox/jjerrell/rules.mk
+++ layouts/community/ergodox/jjerrell/rules.mk
@@ -1,31 +1,14 @@
-# Copyright (C) 2021 Jerrell, Jacob <@jjerrell>
-# 
-# This file is part of qmk_firmware.
-# 
-# qmk_firmware is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# qmk_firmware is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with qmk_firmware.  If not, see <http://www.gnu.org/licenses/>.
-
-BOOTMAGIC_ENABLE   = lite
-TAP_DANCE_ENABLE   = no
-COMMAND_ENABLE     = no  # Commands for debug and configuration
-CONSOLE_ENABLE 	   = yes
+BOOTMAGIC_ENABLE = lite
+TAP_DANCE_ENABLE = no
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = yes
 SPACE_CADET_ENABLE = no
-KEY_LOCK_ENABLE    = no
-MOUSEKEY_ENABLE    = yes
-EXTRAKEY_ENABLE	   = yes # Audio control and System control(+450)
-LEADER_ENABLE	   = yes
+KEY_LOCK_ENABLE = no
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+LEADER_ENABLE = yes
 
-UNICODE_ENABLE     = no
-UNICODEMAP_ENABLE  = no
-RGB_MATRIX_ENABLE  = no
-RGBLIGHT_ENABLE    = no
+UNICODE_ENABLE = no
+UNICODEMAP_ENABLE = no
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
--- layouts/community/ortho_4x12/jjerrell/rules.mk
+++ layouts/community/ortho_4x12/jjerrell/rules.mk
@@ -1,8 +1,8 @@
 SRC += muse.c
 
-BOOTMAGIC_ENABLE   = lite       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE    = no       # Mouse keys(+4700)
-EXTRAKEY_ENABLE    = yes       # Audio control and System control(+450)
-TAP_DANCE_ENABLE   = no
+BOOTMAGIC_ENABLE = lite
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
+TAP_DANCE_ENABLE = no
 SPACE_CADET_ENABLE = no
-NKRO_ENABLE        = no
+NKRO_ENABLE = no
