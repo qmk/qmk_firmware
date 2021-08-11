84aa3412e47e76abd1d03ad9b6ded78600299f37
Rifaa Subekti <68178825+oceeean@users.noreply.github.com>
2021-08-11 15:13:49 +0800

    Update keyboards/ocean/wang_v2/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ocean/wang_v2/keymaps/default/keymap.c
+++ keyboards/ocean/wang_v2/keymaps/default/keymap.c
@@ -15,7 +15,6 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  {
   [0] = LAYOUT(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
