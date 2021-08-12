b6be8132767d616904ba930e0ded6fc835ffe2aa
Malevolti <82970232+Malevolti@users.noreply.github.com>
2021-08-12 18:10:28 +0100

    Update keymap.c

--- keyboards/lyra/keymaps/via/keymap.c
+++ keyboards/lyra/keymaps/via/keymap.c
@@ -10,6 +10,9 @@
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
   * GNU General Public License for more details. 
   * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 char wpm_str[10];
