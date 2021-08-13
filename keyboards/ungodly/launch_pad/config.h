9cd9bd6281f6fd52a203055e4d43b5654158b10d
Drashna Jael're <drashna@live.com>
2021-08-13 10:42:25 -0700

    [Keyboard] Fixup Ungodly Launch Pad config

--- keyboards/ungodly/launch_pad/config.h
+++ keyboards/ungodly/launch_pad/config.h
@@ -49,22 +49,6 @@
 #define MIDI_ADVANCED
 
 /* RGB Matrix configuration */
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_KEYPRESSES
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#    define DISABLE_RGB_MATRIX_PIXEL_FLOW
-#    define DISABLE_RGB_MATRIX_PIXEL_FRACTAL
-#    define DISABLE_RGB_MATRIX_PIXEL_RAIN
-#    define DRIVER_LED_TOTAL 22
-#endif
-
 #define RGB_DI_PIN F7
 #ifdef RGB_MATRIX_ENABLE
 #    define RGBLED_NUM 22
@@ -72,7 +56,7 @@
 #    define RGB_MATRIX_KEYPRESSES  // reacts to keypresses
 // #    define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 // #    define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
-#    define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED  // turn off effects when suspended
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 // #    define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 // #    define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
@@ -124,5 +108,3 @@
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-#define LOCKING_RESYNC_ENABLE
