123786e62efe2349596af0b38b00d6a785f51f9b
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-08-13 14:57:03 -0700

    physically arrange layout macro, phase 1
    Break the keymap up into rows.

--- keyboards/pimentoso/touhoupad/touhoupad.h
+++ keyboards/pimentoso/touhoupad/touhoupad.h
@@ -17,7 +17,10 @@
 
 #include "quantum.h"
 
-#define LAYOUT(k00, k01, k02, k03, k04, k05, k06, k07, k08, k09) \
-{ \
+#define LAYOUT( \
+    k00, k01, k02, k03, \
+    k04, k05, k06, k07, \
+    k08, k09 \
+) { \
     { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09 } \
 }
