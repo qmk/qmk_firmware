ce1741299ec992f8bb13bb53df2af9a4961d1294
Malevolti <82970232+Malevolti@users.noreply.github.com>
2021-08-13 19:11:38 +0100

    Update keyboards/lyra/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lyra/keymaps/default/keymap.c
+++ keyboards/lyra/keymaps/default/keymap.c
@@ -113,7 +113,7 @@ static void render_status(void) {
     led_t led_usb_state = host_keyboard_led_state();
     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
 	oled_write_P(PSTR("\n\n"), false);
-	oled_write_P(PSTR("Lyra\n"), false);
+	oled_write_ln_P(PSTR("Lyra"), false);
 	oled_write_P(PSTR("v1.1\n"), false);
 	oled_write_P(PSTR("by\n"), false);
 	oled_write_P(PSTR("Dom C"), false);
