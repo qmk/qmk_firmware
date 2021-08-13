17faa822b60c50dd392a4ac168beeba619fd2fba
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2021-08-13 12:15:59 -0500

    Update keyboards/sneakbox/ava/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/sneakbox/ava/keymaps/via/keymap.c
+++ keyboards/sneakbox/ava/keymaps/via/keymap.c
@@ -53,18 +53,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_TRNS,   KC_TRNS,                   KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS,                                 KC_TRNS,    KC_TRNS,    KC_TRNS),
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    }
-}
