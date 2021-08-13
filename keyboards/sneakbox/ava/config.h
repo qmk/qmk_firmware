a77e49053bc8e95a624cf34060ba719319db1dbd
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2021-08-13 12:17:22 -0500

    Update keyboards/sneakbox/ava/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/sneakbox/ava/config.h
+++ keyboards/sneakbox/ava/config.h
@@ -51,11 +51,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
 #define LED_NUM_LOCK_PIN  D4
 #define LED_CAPS_LOCK_PIN D6
