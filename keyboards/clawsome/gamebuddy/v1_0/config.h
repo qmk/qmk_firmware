403084ff796cadf4417ec4e116cdcd403a3d1ecb
AlisGraveNil <63020540+AlisGraveNil@users.noreply.github.com>
2021-08-12 20:42:27 -0600

    12 Aug - 1

--- keyboards/clawsome/gamebuddy/v1_0/config.h
+++ keyboards/clawsome/gamebuddy/v1_0/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x17B9
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    AlisGraveNil
-#define PRODUCT         GameBuddy
+#define PRODUCT         GameBuddy1_0
 
 /* key matrix size */
 #define MATRIX_ROWS 5
--- keyboards/clawsome/gamebuddy/v1_m/v1_m.h
+++ keyboards/clawsome/gamebuddy/v1_m/v1_m.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 AAClawson (AlisGraveNil)
+/* Copyright 2021 AAClawson (AlisGraveNil)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,4 +13,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "gamebuddy.h"
+
+#pragma once
+
+#include "quantum.h"
+
+#define LAYOUT( \
+    K00, K01, K02, K03, K04, K05,        \
+    K10, K11, K12, K13, K14, K15,        \
+    K20, K21, K22, K23, K24, K25,        \
+    K30, K31, K32, K33, K34, K35, K46,   \
+                        K44, K45         \
+) { \
+    { K00,   K01,   K02,   K03,   K04,   K05,   KC_NO }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   KC_NO }, \
+    { K20,   K21,   K22,   K23,   K24,   K25,   KC_NO }, \
+    { K30,   K31,   K32,   K33,   K34,   K35,   KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, K44,   K45,   K46   }, \
+}
