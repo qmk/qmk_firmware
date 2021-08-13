dc36adbe10aaa8d7bb6db5dafcf8e38b843a9f1f
Ananya Kirti <AnanyaKirti@users.noreply.github.com>
2021-08-13 16:42:53 +0530

    init
    added support for Bobpad

---
+++ keyboards/bobpad/keymaps/default/keymap.c
@@ -0,0 +1,61 @@
+/* Copyright 2021 Ananya Kirti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_CAPSLOCK,  MO(1), KC_PGUP,
+        KC_DEL,  KC_END,  KC_PGDN
+    ),
+    [1] = LAYOUT(
+        KC_CAPSLOCK,  MO(1), KC_PGUP,
+        KC_DEL,  KC_END,  KC_PGDN
+    ),
+
+};
+
+
+
+void matrix_scan_user(void) {
+    writePin(D0, layer_state_is(1));
+}
+
+bool led_update_user(led_t led_state) {
+    writePin(LED_CAPS_LOCK_PIN, led_state.caps_lock);
+    return false;
+};
+
+
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if(IS_LAYER_ON(1)) { // on Raise layer
+      if (clockwise) {
+          tap_code(KC_A);
+      } else {
+          tap_code(KC_B);
+      }
+    }
+    // default layer
+    else {
+      if (clockwise) {
+          tap_code(KC_C);
+      } else {
+          tap_code(KC_D);
+      }
+    }
+    return true;
+}
---
+++ keyboards/bobpad/readme.md
@@ -0,0 +1,19 @@
+# bobpad
+
+![bobpad](https://i.imgur.com/93zguh1.jpg)
+
+A bentopad based macropad with incline
+
+* Keyboard Maintainer: [Ananya Kirti](https://github.com/AnanyaKirti)
+* Hardware Supported: Custom 3D printed Case
+* Hardware Availability: Contact bob at IMKC Discord
+
+Make example for this keyboard (after setting up your build environment):
+
+    make bobpad:default
+
+Flashing example for this keyboard:
+
+    make bobpad:default:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
