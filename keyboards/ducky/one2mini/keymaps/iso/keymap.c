dc02fea92c27b6f02d5d2fda60cd78923f92f68e
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-08-13 04:13:14 -0700

    rename LAYOUT_iso to LAYOUT_60_iso

--- keyboards/ducky/one2mini/keymaps/iso/keymap.c
+++ keyboards/ducky/one2mini/keymaps/iso/keymap.c
@@ -27,7 +27,7 @@ enum Layer {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT_iso(
+  [_QWERTY] = LAYOUT_60_iso(
     //       2        3        4        5        6        7        8        9        10       11       12       13       14
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI, MO(_FN), KC_RCTL
   ),
 
-  [_FUNCTION] = LAYOUT_iso(
+  [_FUNCTION] = LAYOUT_60_iso(
     //       2        3        4        5        6        7        8        9        10       11       12       13       14
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, KC_INS,  _______, KC_UP,   KC_PAUS, KC_PGUP, KC_HOME, KC_PSCR,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_APP,  MO(_CLR),                  _______,                            _______, KC_APP,  _______, _______
   ),
 
-  [_COLOUR] = LAYOUT_iso(
+  [_COLOUR] = LAYOUT_60_iso(
     //       2        3        4        5        6        7        8        9        10       11       12       13       14
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, _______,
