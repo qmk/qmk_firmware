b7f5b642de802c85a0da152cf79feb4659e5bbad
Justin Sawyer <jtsaw36@gmail.com>
2021-08-15 16:52:06 -0400

    Fix spacing

--- keyboards/crkbd/keymaps/jussaw/keymap.c
+++ keyboards/crkbd/keymaps/jussaw/keymap.c
@@ -37,7 +37,6 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
   //|  Tab   |    Q   |    W   |    E   |    R   |    T   |                    |    Y   |    U   |    I   |    O   |    P   |  Bksp  |
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -52,7 +51,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
       KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                                           KC_LCTL,   MO(1),  KC_SPC,    KC_RSFT,   MO(2), KC_RGUI
-
   ),
 
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
