ad5c758e1da56f17125a42f74af4bf23c43dc4ad
csandven <chs_91@hotmail.com>
2021-08-15 23:15:40 +0200

    First layer done

--- keyboards/keyprez/bison/keymaps/default/keymap.c
+++ keyboards/keyprez/bison/keymaps/default/keymap.c
@@ -29,28 +29,30 @@ enum custom_keycodes {
     KC_ADJUST
 };
 
+#define ESC_CTL MT(MOD_LCTL, KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
     * QWERTY
-    * ,-------------------------------------------------------.                    ,-----------------------------------------.
-    * |  F1  |  F2  |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
-    * |------+------+------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    * |  F3  |  F4  | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
-    * |------+------+------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    * |  F5  |  F6  | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
-    * |------+------+------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-    * |  F7  |  F8  |LShift|   Z  |   X  |   C  |   V  |   B  |       |    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
-    * `-------------------------------------------------------/-------/     \      \-----------------------------------------'
-    *                          | LGUI | LAlt | LCTR |LOWER |  Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+    * ,-------------------------------------------------------.                    ,-------------------------------------------------------.
+    * |  F1  |  F2  |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |   =  |  DEL |
+    * |------+------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------+------|
+    * |  F3  |  F4  | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | [    |   ]  |  |   |
+    * |------+------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------+------|
+    * |  F5  |  F6  | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |  BS  |  PU  |
+    * |------+------+------+------+------+------+------+------|       |    |       |------+------+------+------+------+------+------+------|
+    * |  F7  |  F8  |LShift|   Z  |   X  |   C  |   V  |   B  |       |    |       |   N  |   M  |   ,  |   .  |   /  | Enter| Home |  PD  |
+    * `-------------------------------------------------------/-------/     \------\-------------------------------------------------------'
+    *                          | LGUI | LAlt | LCTR |LOWER |  BS     /       \Space \  |RAISE | RCTR | RAlt | RGUI |
     *                          |      |      |      |      |        /         \      \ |      |      |      |      |
     *                           `----------------------------------'           '------''---------------------------'
     */
 
     [_QWERTY] = LAYOUT( \
-        KC_F1,   KC_F2,   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, KC_7,    KC_8, \
-        KC_F3,   KC_F4,   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, KC_U,    KC_I, \
-        KC_F5,   KC_F6,   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, KC_J,    KC_K, \
-        KC_F7,   KC_F8,   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, KC_M, KC_COMM, \
-                                              KC_LGUI,KC_LALT, KC_LOWER, KC_ENT,              KC_SPC,  KC_RAISE, KC_RALT, KC_RGUI \
+        KC_F1,   KC_F2,   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, KC_EQL,  KC_DEL , \
+        KC_F3,   KC_F4,   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC, KC_RBRC, KC_PIPE, \
+        KC_F5,   KC_F6,   ESC_CTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, KC_BSPC, KC_PGUP, \
+        KC_F7,   KC_F8,   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,  KC_HOME, KC_PGDN, \
+                                              KC_LGUI,KC_LALT, KC_LOWER, KC_BSPC,              KC_SPC,  KC_RAISE, KC_RALT, KC_RGUI \
     ),
 };
--- keyboards/keyprez/bison/rules.mk
+++ keyboards/keyprez/bison/rules.mk
@@ -2,7 +2,7 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = caterina
+BOOTLOADER = dfu
 
 # Build Options
 #   change yes to no to disable
