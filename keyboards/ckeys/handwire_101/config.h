06f843a1ce9738554be1fa8632711c49f99c3876
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-08-14 20:59:18 -0700

    remove MIDI configuration boilerplate from keyboard config.h files

--- keyboards/ckeys/handwire_101/config.h
+++ keyboards/ckeys/handwire_101/config.h
@@ -65,26 +65,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_RESYNC_ENABLE
 
 /*
- * MIDI options
- */
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-//#define MIDI_BASIC
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 1
-
-/*
  * Force NKRO
  *
  * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
