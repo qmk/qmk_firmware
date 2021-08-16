d82e6d62e78acc89cf353de85d230df86a379b02
AquaCylinder <jjspelen@gmail.com>
2021-08-16 11:31:27 +0200

    Update keymap.c
    Switched + and - around, same with / and *.

--- keyboards/emi20/keymaps/default/keymap.c
+++ keyboards/emi20/keymaps/default/keymap.c
@@ -1,18 +1,18 @@
-/* Copyright 2021 Aquacylinder 
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+/* Copyright 2021 Aquacylinder
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 
 #include QMK_KEYBOARD_H
 
@@ -27,13 +27,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			//Macro's are mentioned here allong with the keycodes
 	LAYOUT_ortho_5x4( //Base layer (0)
 		TG(1), 		KC_VOLD, 	KC_VOLU, 	KC_BSPC,
-		KC_P7, 		KC_P8, 		KC_P9, 		KC_PSLS,
-		KC_P4, 		KC_P5, 		KC_P6, 		KC_PAST,
-		KC_P1, 		KC_P2, 		KC_P3, 		KC_PMNS,
-		KC_P0,  	KC_PDOT,    KC_PENT, 	KC_PPLS),
+		KC_P7, 		KC_P8, 		KC_P9, 		KC_PAST,
+		KC_P4, 		KC_P5, 		KC_P6, 		KC_PSLS,
+		KC_P1, 		KC_P2, 		KC_P3, 		KC_PPLS,
+		KC_P0,  	KC_PDOT,    KC_PENT, 	KC_PMNS),
 
 	LAYOUT_ortho_5x4( //Layer 1
-		KC_TRNS, 	KC_VOLD, 	KC_VOLU, 	KC_CALC,
+		KC_TRNS, 	KC_TRNS, 	KC_TRNS, 	KC_CALC,
 		KC_MPRV, 	KC_MSTP, 	KC_MPLY, 	KC_MNXT,
 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO,
 		KC_NO, 		KC_NO, 		KC_UP, 		KC_NO,
