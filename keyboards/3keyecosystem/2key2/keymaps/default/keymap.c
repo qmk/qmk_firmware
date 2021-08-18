a5bb786e763c4fca15c6656d7f2a3f9a8e6a1dc4
John Mueller <softplus@users.noreply.github.com>
2021-08-18 16:01:10 +0200

    Update keyboards/3keyecosystem/2key2/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/3keyecosystem/2key2/keymaps/default/keymap.c
+++ keyboards/3keyecosystem/2key2/keymaps/default/keymap.c
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "quantum.h" 
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
