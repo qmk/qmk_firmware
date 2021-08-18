68cc1e9e2e0a057581625a0531dadb6d67156fa8
ll3macorn <69098787+ll3macorn@users.noreply.github.com>
2021-08-19 02:22:13 +0800

    My new macropad (#43)
    * adding support for the PASSWORD board
    
    its a project im working on and a really nice macropad
    
    * just getting rid of the old keymap
    
    * fixed
    
    * fixed again

---
+++ keyboards/password/password.c
@@ -0,0 +1,1 @@
+#include "password.h"
---
+++ keyboards/password/password.h
@@ -0,0 +1,9 @@
+#ifndef KB_H
+#define KB_H
+
+#include "quantum.h"
+
+#define LAYOUT( K00, K01, K02, K03, K04, K05, K06, K07  ) { { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07 } }
+
+
+#endif
