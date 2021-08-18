75a42f4567f12e329a592d112c15d5530172c246
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-08-17 18:31:49 -0700

    Apply suggestions from code review
    Apply most suggestions. The rest will be addressed individually.
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/converter/modelm_ssk/keymaps/default/keymap.c
+++ keyboards/converter/modelm_ssk/keymaps/default/keymap.c
@@ -15,7 +15,10 @@
  */
 #include QMK_KEYBOARD_H
 
-#define NUMPAD_LAYER 7
+enum custom_layers {
+    _BASE,
+    _NUMPAD,
+};
 
 enum custom_keycodes {
   NUM_SCRL = SAFE_RANGE
@@ -24,7 +27,7 @@ enum custom_keycodes {
 void toggle_numpad_layer(int set_state);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base layer */
+  [_BASE] = LAYOUT( /* Base layer */
     KC_ESC,           KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_PSCR, NUM_SCRL, KC_PAUS,
     KC_GRV,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME,  KC_PGUP,
     KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,   KC_PGDN,
@@ -32,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT, KC_NUBS, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,            KC_UP,
     KC_LCTL,          KC_LALT,                           KC_SPC,                                      KC_RALT,          KC_RCTL,   KC_LEFT, KC_DOWN,  KC_RGHT
   ),
-  [NUMPAD_LAYER] = LAYOUT( /* Numpad Layer */
+  [_NUMPAD] = LAYOUT( /* Numpad Layer */
      KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
 
      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_TRNS,        KC_KP_MINUS, KC_KP_PLUS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
@@ -74,7 +77,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (get_mods() & MOD_MASK_SHIFT) {
         /* Remove the shift modifiers */
         uint8_t shift_mods = get_mods() & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
-        set_mods(get_mods() & ~(MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)) );
+        del_mods(MOD_MASK_SHIFT);
 
         last_num_scroll = KC_NUMLOCK;
         register_code(last_num_scroll);
@@ -106,14 +109,14 @@ void toggle_numpad_layer(int set_state) {
   }
 
   if (numlock_enabled) {
-    layer_on(NUMPAD_LAYER);
+    layer_on(_NUMPAD);
   }
   else {
-    layer_off(NUMPAD_LAYER);
+    layer_off(_NUMPAD);
   }
 }
 
-bool led_update_kb(led_t led_state) {
+bool led_update_user(led_t led_state) {
   /* In rare and unlikely conditions, it's possible for numlock
      state to change externally from the keyboard. If this happens,
      we want to match the new state.
@@ -131,16 +134,3 @@ bool led_update_kb(led_t led_state) {
   return true;
 }
 
-/* Override if necessary */
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
--- keyboards/converter/modelm_ssk/modelm_ssk.c
+++ keyboards/converter/modelm_ssk/modelm_ssk.c
@@ -15,53 +15,5 @@
  */
 #include "modelm_ssk.h"
 
-void keyboard_pre_init_kb(void) {
-  /* Setting status LEDs pins to output and +5V (off) */
-  setPinOutput(B4);
-  setPinOutput(B5);
-  setPinOutput(B6);
-  writePinHigh(B4);
-  writePinHigh(B5);
-  writePinHigh(B6);
-}
 
-void matrix_init_kb(void) {
-  /* put your keyboard start-up code here
-   * runs once when the firmware starts up */
 
-  matrix_init_user();
-}
-
-void matrix_scan_kb(void) {
-  /* put your looping keyboard code here
-   * runs every cycle (a lot) */
-
-  matrix_scan_user();
-}
-
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-  /* put your per-action keyboard code here
-   * runs for every action, just before processing by the firmware */
-
-  return process_record_user(keycode, record);
-}
-
-void led_set_kb(uint8_t usb_led) {
-  if (usb_led & (1<<USB_LED_NUM_LOCK)) {
-    writePinLow(B4);
-  } else {
-    writePinHigh(B4);
-  }
-  if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-    writePinLow(B6);
-  } else {
-    writePinHigh(B6);
-  }
-  if (usb_led & (1<<USB_LED_SCROLL_LOCK)) {
-    writePinLow(B5);
-  } else {
-    writePinHigh(B5);
-  }
-
-  led_set_user(usb_led);
-}
