1b06023e68d25f904ef7a650bc0a665d21eb4cb4
Karl Berggren <berggren.karl+git@gmail.com>
2021-08-18 23:22:50 +0200

    cleaned up, finished info.json and started on config.h

--- keyboards/b-karl/kbic65/config.h
+++ keyboards/b-karl/kbic65/config.h
@@ -24,25 +24,24 @@
 #define PRODUCT_ID      0x0060
 #define DEVICE_VER      0x0001
 
-#define MANUFACTURER    ven0mtr0n
-#define PRODUCT         0-Sixty
+#define MANUFACTURER    b-karl
+#define PRODUCT         kbic65
 
 /* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 12
-#define MATRIX_ROW_PINS { B1, F7, F6, F5, F4 }
-#define MATRIX_COL_PINS { D3, D2, D1, D0, D4, C6, D7, E6, B4, B5, B3, B2 }
+#define MATRIX_ROWS 9
+#define MATRIX_COLS 8
+#define MATRIX_ROW_PINS { B5, D1,  B4, D0, E6, D4, D7, C6, D2 }
+#define MATRIX_COL_PINS { B2, B6, B3, B1, F7, F6, F5, F4 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
 /* indicator leds */
-#define LED_CAPS_LOCK_PIN B6
+#define LED_CAPS_LOCK_PIN
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
