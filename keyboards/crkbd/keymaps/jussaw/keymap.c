3f6e128aebe062afb5290ebba8f1d6505b7ec85a
Justin Sawyer <jtsaw36@gmail.com>
2021-08-19 01:36:09 -0400

    Add Colemak Mod-DH layer

--- keyboards/crkbd/keymaps/jussaw/keymap.c
+++ keyboards/crkbd/keymaps/jussaw/keymap.c
@@ -24,7 +24,8 @@ enum layers {
     _LOWER,
     _UPPER,
     _ADJUST,
-    _GAME
+    _GAME,
+    _COLEMAKDH
 };
 
 enum custom_keycodes {
@@ -32,7 +33,8 @@ enum custom_keycodes {
     LOWER,
     UPPER,
     ADJUST,
-    GAME
+    GAME,
+    COLEMAKDH
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,6 +52,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
       KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
+                                          KC_LCTL,   MO(1),  KC_SPC,    KC_RSFT,   MO(2), KC_RGUI
+  ),
+
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+  //|  Tab   |    Q   |    W   |    F   |    P   |    B   |                    |    J   |    L   |    U   |    Y   |    ;   |  Bksp  |
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+  //|  Esc   |    A   |    R   |    S   |    T   |    G   |                    |    M   |    N   |    E   |    I   |    O   |   ""   |
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+  //|  Alt   |    Z   |    X   |    C   |    D   |    V   |                    |    K   |    H   |    ,   |    .   |    /   | Enter  |
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+  //                                    |  Ctrl  | Lower  | Space       Shift  | Upper  |  GUI   |
+                                      //`--------------------------'  `--------------------------'
+  [_COLEMAKDH] = LAYOUT_split_3x6_3(
+       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
+       KC_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
+      KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                                           KC_LCTL,   MO(1),  KC_SPC,    KC_RSFT,   MO(2), KC_RGUI
   ),
 
@@ -75,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|  Del   |        |        |        |        |        |                    |        |  Left  |  Down  |   Up   | Right  |   ""   |
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
   //|  Alt   |        |        |        |        |        |                    |        |  Home  |Pg Down | Pg Up  |  End   | Enter  |
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+  //|--------+--------+--------+--------+--------+--------+---q-----|  |--------+--------+--------+--------+--------+--------+--------|
   //                                    |  Ctrl  | Adjust | Space       Shift  | Trans  |  GUI   |
                                       //`--------------------------'  `--------------------------'
   [_UPPER] = LAYOUT_split_3x6_3(
@@ -86,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-  //|  Reset |        |        |        |        | TG(4)  |                    |  Play  | Mouse1 | Mouse2 | Mouse4 | Mouse5 |Ms Acc1 |
+  //|  Reset |  DF(0) |  DF(5) |        |        | TG(4)  |                    |  Play  | Mouse1 | Mouse2 | Mouse4 | Mouse5 |Ms Acc1 |
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
   //|  RGB   |  Hue+  |  Sat+  |Bright+ |Effect- |AG_SWAP |                    |  Vol+  |Ms Left |Ms Down | Ms Up  |Ms Right|Ms Acc2 |
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -95,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                                    |  Ctrl  | Lower  | Space       Shift  | Upper  |  GUI   |
                                       //`--------------------------'  `--------------------------'
   [_ADJUST] = LAYOUT_split_3x6_3(
-        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   TG(4),                      KC_MPLY, KC_BTN1, KC_BTN2, KC_BTN4, KC_BTN5, KC_ACL0,
+        RESET,   DF(0),   DF(5), XXXXXXX, XXXXXXX,   TG(4),                      KC_MPLY, KC_BTN1, KC_BTN2, KC_BTN4, KC_BTN5, KC_ACL0,
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, AG_SWAP,                      KC_VOLU, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ACL1,
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, AG_NORM,                      KC_VOLD, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_ACL2,
                                           XXXXXXX, KC_TRNS, XXXXXXX,    XXXXXXX, KC_TRNS, XXXXXXX
@@ -115,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_QUOT, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                         KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
        KC_ENT, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
                                            KC_ESC,  KC_LALT, KC_SPC,    XXXXXXX, XXXXXXX,   TG(4)
-  )
+  ),
 };
 
 #ifdef OLED_DRIVER_ENABLE
