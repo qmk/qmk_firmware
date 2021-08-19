90b41426b0bf6a15f1cf4c2b1905a7bd694c9b01
Tran Kim Tung <contact@trankimtung.com>
2021-08-19 08:29:46 +0800

    dz60rgb_wkl/v2_1: remove NO_USB_STARTUP_CHECK
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dztech/dz60rgb_wkl/v2_1/rules.mk
+++ keyboards/dztech/dz60rgb_wkl/v2_1/rules.mk
@@ -23,7 +23,6 @@ BLUETOOTH_ENABLE = no          # Enable Bluetooth
 AUDIO_ENABLE = no              # Audio output
 RGB_MATRIX_ENABLE = yes        # Use RGB matrix
 RGB_MATRIX_DRIVER = IS31FL3733
-NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
 LTO_ENABLE = yes
 
 LAYOUTS = 60_tsangan_hhkb
