0f5e187ffc2c3421e7749cd87c3154d768a8cb47
J.Flanagan <jrfhoutx@comcast.net>
2021-08-20 09:44:15 -0500

    Update keyboards/viktus/sp_mini/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/viktus/sp_mini/config.h
+++ keyboards/viktus/sp_mini/config.h
@@ -47,10 +47,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 #define USE_I2C
 
-/* serial.c configuration for split keyboard */
-//#define SOFT_SERIAL_PIN D1
-
-
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
