ecb23cd2c56436e029833f398858a062bc12c984
Yang Hu <hueyhy@gmail.com>
2021-08-19 17:49:37 -0700

    Update keyboards/yanghu/unicorne/f411/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/yanghu/unicorne/f411/rules.mk
+++ keyboards/yanghu/unicorne/f411/rules.mk
@@ -1,7 +1,5 @@
 # MCU name
 MCU = STM32F411
 
-# Address of the bootloader in system memory
-# * It is chip dependent, the correct number can be looked up here: (page 359)
-# * http://www.st.com/web/en/resource/technical/document/application_note/CD00167594.pdf
-STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
+# Bootloader selection
+BOOTLOADER = stm32-dfu
