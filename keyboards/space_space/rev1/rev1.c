f2d3e0da056d849de7ae6e603d667e5649fcc893
Drashna Jael're <drashna@live.com>
2021-08-19 21:17:57 -0700

    [Keyboard] Fix space_space rev1 keyboard files

--- keyboards/space_space/rev1/rev1.c
+++ keyboards/space_space/rev1/rev1.c
@@ -14,4 +14,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "space_space.h"
+#include "rev1.h"
