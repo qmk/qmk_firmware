5fb841dc34d0d5e91de58f86a96ff4d03d7d5faa
Christian Sandven <chs_91@hotmail.com>
2021-08-21 20:44:08 +0200

    Update keyboards/keyprez/bison/keymaps/default_6_6/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keyprez/bison/keymaps/default_6_6/keymap.c
+++ keyboards/keyprez/bison/keymaps/default_6_6/keymap.c
@@ -85,12 +85,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *            |      |      |      |      |        /         \      \ |      |      |      |      |
     *             `----------------------------------'           '------''---------------------------'
     */
-    [_COLEMAK] = LAYOUT_6_6( \
-        KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,  \
-        KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC, \
-        ESC_CTL,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT, \
-        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,  \
-                                          KC_LGUI,KC_LALT, LOWER,       KC_BSPC,              KC_SPC,      RAISE, KC_RALT, FN \
+    [_COLEMAK] = LAYOUT_6_6(
+        KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
+        KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
+        ESC_CTL,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
+        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
+                                          KC_LGUI,KC_LALT, LOWER,       KC_BSPC,              KC_SPC,      RAISE, KC_RALT, FN
     ),
 
     /*
