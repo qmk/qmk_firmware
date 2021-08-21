3a46e503e054b0ba39f6a35d0c8e65474af1ee17
csandven <chs_91@hotmail.com>
2021-08-21 11:24:30 +0200

    Move default rotary encoder behavior to bison.c

--- keyboards/keyprez/bison/bison.c
+++ keyboards/keyprez/bison/bison.c
@@ -18,6 +18,18 @@
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
-    tap_code(clockwise ? KC_VOLU : KC_VOLD);
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_PGDOWN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
     return true;
 }
--- keyboards/keyprez/bison/keymaps/default_6_8/keymap.c
+++ keyboards/keyprez/bison/keymaps/default_6_8/keymap.c
@@ -224,20 +224,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDOWN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
-    return false;
-}
