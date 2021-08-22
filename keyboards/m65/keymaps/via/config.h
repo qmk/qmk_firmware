33a93c0281dd0dd2e408152b14321ed84c2d04ab
alin elena <alin@elena.space>
2021-08-22 22:46:00 +0100

    via changes

--- keyboards/m65/keymaps/via/config.h
+++ keyboards/m65/keymaps/via/config.h
@@ -16,13 +16,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#define EEPROM_PAGE_SIZE
-#define FEE_PAGE_SIZE (uint16_t)0x800
-#define FEE_DENSITY_PAGES 4
-
-#define EEPROM_START_ADDRESS
-#define FEE_MCU_FLASH_SIZE                              \
-({                                                      \
-    uint16_t flash_size = *(uint16_t*)FLASHSIZE_BASE;   \
-    (flash_size <= 512) ? flash_size : 512;             \
-})
