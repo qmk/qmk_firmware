4773c8132d6dd0aa337837c261b0a1ec50f69207
drhigsby <doctorhigsby@gmail.com>
2021-08-22 19:23:09 -0700

    Fixedallthethings

--- keyboards/packrat/keymaps/default/keymap.c
+++ keyboards/packrat/keymaps/default/keymap.c
@@ -60,25 +60,25 @@ combo_t key_combos[COMBO_COUNT] = {
 #define xxx KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(
+    [_BASE] = LAYOUT_2x3uC(
         KC_Q, KC_W, KC_E, KC_R, KC_T, KC_DEL, KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A, KC_S, KC_D, KC_F, KC_G, KC_HOME, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
         LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_END, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),
-                KC_LCTL, KC_LALT, NUM, KC_SPC, KC_SPC, KC_RGUI, FN
+                KC_LCTL, KC_LALT, NUM, KC_SPC, KC_RGUI, FN
   ),
 
-    [_NUM] = LAYOUT(
+    [_NUM] = LAYOUT_2x3uC(
         KC_1, KC_2, KC_3, KC_4, KC_5, KC_GRV, KC_6, KC_7, KC_8, KC_9, KC_0,
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, xxx, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,
-        KC_LEFT,KC_DOWN, KC_UP, KC_RGHT, xxx, xxx, xxx, KC_PIPE,KC_BSLS, KC_DQUO, KC_QUOT,
-        xxx, xxx, xxx, xxx, xxx, KC_PGUP, KC_PGDN
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CAPS, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, xxx, xxx, xxx, KC_PIPE, KC_BSLS, KC_DQUO, KC_QUOT,
+        xxx, xxx, xxx, xxx, KC_PGUP, KC_PGDN
   ),
 
-    [_FN] = LAYOUT(
+    [_FN] = LAYOUT_2x3uC(
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, xxx, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
         xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, KC_F11, KC_F12,
         xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx,
-        xxx, xxx, xxx, xxx, xxx, xxx, xxx        
+        xxx, xxx, xxx, xxx, xxx, xxx       
   )
 
 };
@@ -86,9 +86,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_PGUP);
+            tap_code(KC_VOLU);
         } else {
-            tap_code(KC_PGDN);
+            tap_code(KC_VOLD);
         }
     }
 }
