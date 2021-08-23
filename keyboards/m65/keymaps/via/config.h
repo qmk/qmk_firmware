7389950f895e1c534f6f225fabeb0fc3bdff1819
alin elena <alin@elena.space>
2021-08-23 08:49:29 +0100

    via changes toggle default

--- keyboards/m65/keymaps/via/config.h
+++ keyboards/m65/keymaps/via/config.h
@@ -15,4 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define TAPPING_TOGGLE 2
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
