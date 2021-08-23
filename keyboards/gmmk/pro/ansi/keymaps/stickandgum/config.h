786c92688d6dcf7439e9c13def7088bc7bfd7e51
stickandgum <stickandgum@users.noreply.github.com>
2021-08-22 20:05:44 -0400

    Update keyboards/gmmk/pro/ansi/keymaps/stickandgum/config.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/gmmk/pro/ansi/keymaps/stickandgum/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/stickandgum/config.h
@@ -1,6 +1,4 @@
-/*  StickAndGum github. */
-
-/* Copyright 2021
+/* Copyright 2021 stickandgum
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
