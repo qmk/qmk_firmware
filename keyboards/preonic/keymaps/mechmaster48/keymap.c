0ac06d219651b479b7a612d7d71bdf7c98588d1f
Zachary Foster <zfoster777@gmail.com>
2021-08-23 18:43:31 -0700

    Updated copyright per documentation.

--- keyboards/preonic/keymaps/mechmaster48/keymap.c
+++ keyboards/preonic/keymaps/mechmaster48/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2015-2017 Jack Humbert
+/* Copyright 2021 MechMaster48 admin@mechbox.net
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
