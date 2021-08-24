24435f7617a9ecedb55c26b9c1517bd29e03a724
Weirdo <1248314361@qq.com>
2021-08-24 16:29:03 +0800

    updata

--- keyboards/kelowna/rgb64/rules.mk
+++ keyboards/kelowna/rgb64/rules.mk
@@ -21,3 +21,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+LAYOUTS = 64_ansi
