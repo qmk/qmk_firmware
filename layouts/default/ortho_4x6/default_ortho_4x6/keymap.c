b405db48c77d13bce1995279b9e99c7bc632b24c
Trent Hoh <trenthoh@gmail.com>
2021-08-24 16:51:34 +0800

    Update layouts/default/ortho_4x6/default_ortho_4x6/keymap.c
    Keymap
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- layouts/default/ortho_4x6/default_ortho_4x6/keymap.c
+++ layouts/default/ortho_4x6/default_ortho_4x6/keymap.c
@@ -1,7 +1,17 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+    /*
+     * ┌───┬───┬───┬───┬───┬───┐
+     * │Tab│ Q │ W │ E │ R │ T │
+     * ├───┼───┼───┼───┼───┼───┤
+     * │Bsp│ A │ S │ D │ F │ G │
+     * ├───┼───┼───┼───┼───┼───┤
+     * │Sft│ Z │ X │ C │ V │ B │
+     * ├───┼───┼───┼───┼───┼───┤
+     * │Ctl│App│GUI│Alt│Spc│Spc│
+     * └───┴───┴───┴───┴───┴───┘
+     */
     [0] = LAYOUT_ortho_4x6(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   
         KC_BKSP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   
