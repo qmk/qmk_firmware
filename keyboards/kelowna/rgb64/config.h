2584b91cb2edc2754704af40ecd611357aab3b10
Weirdo <1248314361@qq.com>
2021-08-24 16:22:53 +0800

    updata

--- keyboards/kelowna/rgb64/config.h
+++ keyboards/kelowna/rgb64/config.h
@@ -23,20 +23,19 @@
 #define MANUFACTURER Weirdo
 #define PRODUCT kelownaRGB64
 
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
 
-#define MATRIX_ROW_PINS { E6, B7, F7, F4, F5 }
-#define MATRIX_COL_PINS { F6, B0, F1, C7, C6, B6, B5, B4, D7, D6, D4, D5, D3, D2, D1 }
+#define MATRIX_COL_PINS { A3, A4, A5, A7, B0, B1, B10, B15, A8, A9, A10, B7, B6, B5, B4}
+#define MATRIX_ROW_PINS { B12, B13, B14, C11, A1}
 #define DIODE_DIRECTION ROW2COL
 #define DEBOUNCE 5
 
 
 
-#define RGB_DI_PIN B1
-#define RGBLED_NUM 80
+#define RGB_DI_PIN A6
+#define RGBLED_NUM 64
 //#define RGBLIGHT_DISABLE_KEYCODES
 
 
