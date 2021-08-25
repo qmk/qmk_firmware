df0c21b66aa99f1597da6c82164ef018f1853d42
Jose Gisbert <jisbert@users.noreply.github.com>
2021-08-25 11:05:12 +0200

    Cambia la posición de lower y raise
    Además, lower no se mantiene, solo mientras se mantiene la tecla
    pulsada.

--- keyboards/kyria/keymaps/jisbert/keymap.c
+++ keyboards/kyria/keymaps/jisbert/keymap.c
@@ -38,14 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |  ¿  ¡  |   Z  |   X  |   C  |   V  |   B  |SH/Del| Alt  |  | Tab  |LShift|   N  |   M  | ,  ; | . :  | -  _ |  ?  !  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | GUI  | @  € | Raise| Bksp | Ctrl |  | Enter| Space| Lower| AltGr|  ≡   |
+ *                        | GUI  | @  € | Lower| Bksp | Ctrl |  | Enter| Space| Raise| AltGr|  ≡   |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
         KC_ESC, KC_Q, KC_W, KC_E,    KC_R,       KC_T,                                                         KC_Y,        KC_U,    KC_I,    KC_O,   KC_P,    KC_NUHS,
         KC_GRV, KC_A, KC_S, KC_D,    KC_F,       KC_G,                                                         KC_H,        KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_EQL, KC_Z, KC_X, KC_C,    KC_V,       KC_B,        SFT_T(KC_DEL),  KC_LALT, KC_TAB,  OSM(MOD_LSFT), KC_N,        KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_EMQM,
-                            KC_LGUI, KC_ATEU,    MO(_RAISE),  KC_BSPC,        KC_LCTL, KC_ENT,  KC_SPC,        TG(_LOWER),  KC_RALT, KC_APP
+                            KC_LGUI, KC_ATEU,    MO(_LOWER),  KC_BSPC,        KC_LCTL, KC_ENT,  KC_SPC,        MO(_RAISE),  KC_RALT, KC_APP
     ),
 /*
  * Lower Layer: navigation
