37983002045ee210c54360457352db0e0e2365a7
Chad Stovern <chad@stovern.me>
2021-08-25 15:12:34 -0500

    enable interchangable controller masters; enable underglow

--- keyboards/boardsource/microdox/keymaps/via/config.h
+++ keyboards/boardsource/microdox/keymaps/via/config.h
@@ -3,3 +3,15 @@
 
 /* https://docs.qmk.fm/#/tap_hold?id=retro-tapping */
 #define RETRO_TAPPING
+
+/*
+  allow the microdox board to work with either controller plugged in
+  docs: https://docs.qmk.fm/#/feature_split_keyboard?id=handedness-by-eeprom
+
+  flashing the controllers:
+  make boardsource/microdox:via:dfu-split-left
+  make boardsource/microdox:via:dfu-split-right
+
+  loading keymap with via: load your via keymap with each side plugged in
+*/
+#define EE_HANDS
