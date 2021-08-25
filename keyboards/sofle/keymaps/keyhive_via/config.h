dbdd43445d0ee0e28694f7daebd9ee2129fa5af2
gitgat <bryan@smartpcrepair.com>
2021-08-24 21:31:58 -0400

    setup gitgat keymap, updated encoder configured autoshift

--- keyboards/sofle/keymaps/keyhive_via/config.h
+++ keyboards/sofle/keymaps/keyhive_via/config.h
@@ -1,19 +1,19 @@
  /* Copyright 2020 Josef Adamcik
   * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 
 #pragma once
 
@@ -29,4 +29,5 @@ for more options.
 #define RGBLED_NUM 74
 #define RGBLED_SPLIT {37,37}
 #define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_LIMIT_VAL 192 
+#define RGBLIGHT_LIMIT_VAL 192
+#define AUTO_SHIFT_TIMEOUT 140
