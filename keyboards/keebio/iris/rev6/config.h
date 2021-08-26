c37666dce131006e39d48dea106375062e280b43
Danny Nguyen <danny@keeb.io>
2021-08-26 17:28:09 -0400

    Set pinouts

--- keyboards/keebio/iris/rev6/config.h
+++ keyboards/keebio/iris/rev6/config.h
@@ -30,20 +30,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 6
 
 // wiring of each half
-#define MATRIX_ROW_PINS { B1, F0, F5, F6, F7 }
-#define MATRIX_COL_PINS { F1, F4, D3, D2, B7, D4 }
-#define MATRIX_ROW_PINS_RIGHT { B1, B2, D2, F1, F4 }
-#define MATRIX_COL_PINS_RIGHT { D4, D7, D3, B7, F0, B3 }
+#define MATRIX_ROW_PINS { B1, F0, F5, B4, D7 }
+#define MATRIX_COL_PINS { F1, F4, B5, C7, D4, D6 }
+#define MATRIX_ROW_PINS_RIGHT { B1, F0, F5, B4, B5 }
+#define MATRIX_COL_PINS_RIGHT { D4, D6, D7, C7, F1, F4 }
 #define SPLIT_HAND_PIN D5
-#define QMK_ESC_OUTPUT F1
-#define QMK_ESC_INPUT B1
-#define QMK_LED B0
-#define QMK_SPEAKER C6
 
 #define ENCODERS_PAD_A { B2 }
 #define ENCODERS_PAD_B { B3 }
-#define ENCODERS_PAD_A_RIGHT { F7 }
-#define ENCODERS_PAD_B_RIGHT { F6 }
+#define ENCODERS_PAD_A_RIGHT { B2 }
+#define ENCODERS_PAD_B_RIGHT { B3 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -60,24 +56,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* serial.c configuration for split keyboard */
 #define SOFT_SERIAL_PIN D0
 
-
-#define BACKLIGHT_PIN B5
-#define BACKLIGHT_LEVELS 5
-#define BACKLIGHT_DEFAULT_LEVEL 3
-
 /* ws2812 RGB LED */
-#define RGB_DI_PIN D6
-#define RGBLED_NUM 12    // Number of LEDs
-#define RGBLED_SPLIT { 6, 6 }
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#define RGBLIGHT_DEFAULT_VAL 128
-#define RGBLIGHT_DEFAULT_MODE (RGBLIGHT_MODE_RAINBOW_SWIRL + 2)
+#define RGB_DI_PIN E6
+#ifdef RGB_DI_PIN
+#    define RGBLED_NUM 68
+#    define RGBLED_SPLIT { 34, 34 }
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+#    define RGBLIGHT_SLEEP         /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+/*== enabled animations ==*/
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_DEFAULT_VAL 128
+#    define RGBLIGHT_DEFAULT_MODE (RGBLIGHT_MODE_RAINBOW_SWIRL + 2)
+// RGB Matrix
+#    ifdef RGB_MATRIX_ENABLE
+#        define DRIVER_LED_TOTAL RGBLED_NUM
+#    endif
+#endif
--- keyboards/keebio/iris/rev6/rules.mk
+++ keyboards/keebio/iris/rev6/rules.mk
@@ -15,11 +15,13 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = WS2812
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
