60bb7734b7d05cbe9c49faaa37fae41ca38c3ea6
jpuerto96 <jpuerto96@hotmail.com>
2021-08-26 07:47:14 -0400

    Update keyboards/v4n4g0rth0n/v4n4g0rth0n.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/v4n4g0rth0n/v4n4g0rth0n.h
+++ keyboards/v4n4g0rth0n/v4n4g0rth0n.h
@@ -28,16 +28,16 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT(                                                      \
-    k00, k01, k02, k03, k04, k05,                                    \
-    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k110, k111,    \
-    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k210, k211,    \
-    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k310, k311,    \
-    k40, k41, k42,      k44,           k47,      k49, k410, k411    \
+#define LAYOUT( \
+    k00, k01, k02, k03, k04, k05, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, \
+    k40, k41, k42,      k44,           k47,      k49, k4A, k4B \
 ) { \
-    { k00, k01, k02, k03, k04, k05 }, \
-    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k110, k111 },  \
-    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k210, k211 }, \
-    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k310, k311 }, \
-    { k40, k41, k42, KC_NO, k44, KC_NO, KC_NO, k47, KC_NO, k49, k410, k411 } \
+    { k00, k01, k02, k03, k04, k05, XXX, XXX, XXX, XXX, XXX, XXX }, \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B }, \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B }, \
+    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B }, \
+    { k40, k41, k42, XXX, k44, XXX, XXX, k47, XXX, k49, k4A, k4B } \
 }
