5ca33ce69c703cd76e538c6ba4e7318e4f89ba81
Chad Stovern <chad@stovern.me>
2021-08-26 07:47:07 -0500

    keymap and config updates:
    - added gaming layer (3) to keymap in via
    - added hardcoded colemak-dh variant with gaming layer so gaming layer can be qwerty while keeping the base layer colemak-dh
    - adjusted configs to be consistent between my custom keymap and the via enabled keymap

--- keyboards/boardsource/microdox/keymaps/chadhs/config.h
+++ keyboards/boardsource/microdox/keymaps/chadhs/config.h
@@ -1,1 +1,17 @@
+/* https://docs.qmk.fm/#/tap_hold?id=ignore-mod-tap-interrupt */
 #define IGNORE_MOD_TAP_INTERRUPT
+
+/* https://docs.qmk.fm/#/tap_hold?id=retro-tapping */
+#define RETRO_TAPPING
+
+/*
+  allow the microdox board to work with either controller plugged in
+  docs: https://docs.qmk.fm/#/feature_split_keyboard?id=handedness-by-eeprom
+
+  flashing the controllers:
+  make boardsource/microdox:via:dfu-split-left
+  make boardsource/microdox:via:dfu-split-right
+
+  loading keymap with via: load your via keymap with each side plugged in
+*/
+#define EE_HANDS
