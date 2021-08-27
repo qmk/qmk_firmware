a2d0502f49e89aaa1fb99b92bd33af5f9cc623ce
aka whitehatmiddleman <whitehatmiddleman@gmail.com>
2021-08-27 14:24:28 +0800

    Update keyboards/crkbd/keymaps/greyhatmiddleman_trackball_right/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/crkbd/keymaps/greyhatmiddleman_trackball_right/config.h
+++ keyboards/crkbd/keymaps/greyhatmiddleman_trackball_right/config.h
@@ -27,7 +27,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_RIGHT
 //#define EE_HANDS
 
-#define USE_SERIAL_PD2
 
 #define AUTO_SHIFT_TIMEOUT 175
 #define NO_AUTO_SHIFT_NUMERIC
