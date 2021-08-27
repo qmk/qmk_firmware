b556417d112469c00756a0f6adbfc26c78d81507
Chad Stovern <chad@stovern.me>
2021-08-27 08:45:34 -0500

    updated custom keymap to match the setup i built up in via

--- keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
+++ keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
@@ -18,33 +18,66 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY,
-    _RAISE,
-    _LOWER,
+    _COLEMAKDH,
+    _NUMSYMNAV,
+    _FUNCTION,
+    _GAMING,
 };
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+/* thumb  mods */
+#define LTHUMB1 LT(_NUMSYMNAV,KC_TAB)
+#define LTHUMB2 CMD_T(KC_BSPC)
+#define LTHUMB3 OPT_T(KC_ESC)
+#define RTHUMB1 LT(_FUNCTION,KC_ENT)
+#define LTHUMBG TG(_GAMING)
 
+/* home row mods */
+#define LHRMA SFT_T(KC_A)
+#define LHRMR CTL_T(KC_R)
+#define LHRMS OPT_T(KC_S)
+#define LHRMT CMD_T(KC_T)
+#define LHRMZ SFT_T(KC_Z)
+#define LHRMGR SFT_T(KC_GRV)
+#define RHRMN CMD_T(KC_N)
+#define RHRME OPT_T(KC_E)
+#define RHRMI CTL_T(KC_I)
+#define RHRMO SFT_T(KC_O)
+#define RHRMSL SFT_T(KC_SLSH)
+#define RHRMDWN CMD_T(KC_DOWN)
+#define RHRMUP OPT_T(KC_UP)
+#define RHRMRGT CTL_T(KC_RGHT)
+#define RHRMQT SFT_T(KC_QUOT)
+#define RHRMBSL SFT_T(KC_BSLS)
+
+/* aliases */
+#define RGB_RMD RGB_RMOD
+
+/* layer definitions */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_QWERTY] = LAYOUT_split_3x5_3(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                    KC_LCTL, LOWER,   KC_BSPC,      KC_SPC,  RAISE,   KC_ENT
+[_COLEMAKDH] = LAYOUT_split_3x5_3(
+  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+  LHRMA,   LHRMR,   LHRMS,   LHRMT,   KC_G,         KC_M,    RHRMN,   RHRME,   RHRMI,   RHRMO,
+  LHRMZ,   KC_X,    KC_C,    KC_D,    KC_V,         KC_K,    KC_H,    KC_COMM, KC_DOT,  RHRMSL,
+                    LTHUMB3, LTHUMB2, LTHUMB1,      RTHUMB1, KC_SPC,   KC_BSPC
 ),
 
-[_RAISE] = LAYOUT_split_3x5_3(
+[_NUMSYMNAV] = LAYOUT_split_3x5_3(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,  KC_QUOT,
-                    XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
+  LHRMGR,  KC_LCTL, KC_LOPT, KC_LCMD, XXXXXXX,      KC_LEFT, RHRMDWN, RHRMUP,  RHRMRGT, RHRMQT,
+  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS, KC_EQL,  KC_LBRC, RHRMBSL, KC_RBRC,
+                    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
-[_LOWER] = LAYOUT_split_3x5_3(
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-  KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,
-  KC_CAPS, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,  KC_DQT,
-                    XXXXXXX, XXXXXXX, XXXXXXX,      KC_ENT,  XXXXXXX, KC_DEL
-)
+[_FUNCTION] = LAYOUT_split_3x5_3(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,
+  RGB_TOG, RGB_RMD, RGB_MOD, RGB_VAD, RGB_VAI,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                    LTHUMBG, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
+),
+[_GAMING] = LAYOUT_split_3x5_3(
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  KC_ESC,  KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BSLS,
+                    LTHUMBG, KC_LCMD, KC_SPC,       KC_TRNS, KC_TRNS, KC_TRNS
+),
 };
