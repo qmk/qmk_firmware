ed54cd9a9067f8ec8ce7dd81350b2d33d6489690
drhigsby <doctorhigsby@gmail.com>
2021-08-27 14:03:22 -0700

    update keymap preference

--- keyboards/packrat/keymaps/3u/keymap.c
+++ keyboards/packrat/keymaps/3u/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_E, KC_R, KC_T,               MATH,          KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A, KC_S, KC_D, KC_F, KC_G,               KC_HOME,       KC_H, KC_J, KC_K, KC_L, KC_SCLN,
         LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,       KC_END,        KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),
-              KC_LCTL, KC_LALT, CODE,               KC_SPC,        CODE, KC_RGUI, FUNC
+              KC_LALT, KC_LCTL, CODE,               KC_SPC,        CODE, KC_RGUI, FUNC
   ),
 
     [_CODE] = LAYOUT_3uC(
