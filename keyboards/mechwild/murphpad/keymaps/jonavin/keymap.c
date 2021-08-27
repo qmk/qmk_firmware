7f6fa1e5e42e4876d504c7119b23a63b5a6e9f21
jonavin <71780717+Jonavin@users.noreply.github.com>
2021-08-27 06:15:39 -0400

    Update keyboards/mechwild/murphpad/keymaps/jonavin/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechwild/murphpad/keymaps/jonavin/keymap.c
+++ keyboards/mechwild/murphpad/keymaps/jonavin/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                  TT(_FN3), TT(_FN4), TT(_RGB)
     ),
-    [_FN1] = LAYOUT_LANDSCAPE(
+    [_FN1] = LAYOUT_landscape(
         _______,  _______, ENCFUNC,
 
         _______,  KC_F10,   KC_F11,  KC_F12,  KC_PSCR,      _______,
