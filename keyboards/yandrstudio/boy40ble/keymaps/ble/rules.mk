ac74851f2962497e34764fbb3e115e3c25c182d1
JX <572673807@qq.com>
2021-08-28 01:44:43 +0800

    add zhou65

--- keyboards/yandrstudio/boy40ble/keymaps/ble/rules.mk
+++
@@ -1,4 +0,0 @@
-VIA_ENABLE = yes
-CONSOLE_ENABLE = yes         # Console for debug
-
-BIU_BLE5_ENABLE = yes
--- keyboards/yandrstudio/nlhhkb/keymaps/via/rules.mk
+++
@@ -1,1 +0,0 @@
-VIA_ENABLE = yes            # VIA support should be enabled here due to the main() loop will be compiled first.
