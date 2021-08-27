35f84ac68938e4d8e11f38b8df2d30a73dfc67dd
JX <572673807@qq.com>
2021-08-28 02:02:22 +0800

    fix arm ws2812 spi divisor

--- keyboards/yandrstudio/final80v2/mcuconf.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include_next <mcuconf.h>
-
-#undef STM32_I2C_USE_I2C1
-#define STM32_I2C_USE_I2C1                  TRUE
-
-#undef STM32_PWM_USE_TIM3
-#define STM32_PWM_USE_TIM3                  TRUE
