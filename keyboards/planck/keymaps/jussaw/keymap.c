c92b70c9d783edc4465058655e894a0208bd61f1
Justin Sawyer <jtsaw36@gmail.com>
2021-08-28 14:25:51 -0400

    Fix transparancies and activation of adjust layer

--- keyboards/planck/keymaps/jussaw/keymap.c
+++ keyboards/planck/keymaps/jussaw/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    _______, _______, _______, KC_LCTL, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, _______, _______, _______
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_RGUI, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Lower
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_GRV,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12,  KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, _______, _______,KC_LCTL,  KC_TRNS, KC_SPC,  KC_RSFT, ADJUST,  KC_RALT, _______, _______, _______
+    XXXXXXX, XXXXXXX, XXXXXXX,KC_LCTL,  KC_TRNS, KC_SPC,  KC_RSFT, KC_TRNS, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Raise
@@ -82,16 +82,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |      |      |      |      |      |      | Left | Down |  Up  |Right |  ""  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Alt  |      |      |      |      |      |      | Left |Pg Dn |Pg Up | End  |Enter |
+ * | Alt  |      |      |      |      |      |      | Home |Pg Dn |Pg Up | End  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | Ctrl |Adjust|Space |Shift |Trans | GUI  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_QUOT,
-    KC_LALT, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT,
-    _______, _______, _______, KC_LCTL,  ADJUST,  KC_SPC, KC_RSFT, KC_TRNS, KC_RGUI, _______, _______, _______
+    KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_QUOT,
+    KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT,
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_TRNS,  KC_SPC, KC_RSFT, KC_TRNS, KC_RGUI, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Adjust (Lower + Raise)
@@ -107,10 +107,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    RESET,   DF(_QWERTY),_______, XXXXXXX, XXXXXXX, TG(_GAME),KC_MPLY, KC_BTN1, KC_BTN2,  KC_BTN4, KC_BTN5, KC_ACL0,
+    RESET,   DF(_QWERTY),XXXXXXX, XXXXXXX, XXXXXXX, TG(_GAME),KC_MPLY, KC_BTN1, KC_BTN2,  KC_BTN4, KC_BTN5, KC_ACL0,
     RGB_TOG, RGB_HUI,    RGB_SAI, RGB_VAI, RGB_SPI, AG_SWAP,  KC_VOLU, KC_MS_L, KC_MS_D,  KC_MS_U, KC_MS_R, KC_ACL1,
     RGB_MOD, RGB_HUD,    RGB_SAD, RGB_VAD, RGB_SPD, AG_NORM,  KC_VOLD, KC_WH_L, KC_WH_D,  KC_WH_U, KC_WH_R, KC_ACL2,
-    _______, _______,    _______, _______, KC_TRNS, _______,  _______, KC_TRNS, _______,  _______, _______, _______
+    XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, KC_TRNS, XXXXXXX,  XXXXXXX, KC_TRNS, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 //TODO: Edit layer
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL, _______, _______, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  _______, _______, _______, _______, TG(_GAME)
+    KC_LCTL, XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_GAME)
 )
 
 };
