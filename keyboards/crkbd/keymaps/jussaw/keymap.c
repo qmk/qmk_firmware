dc19d44ffa45ad998040547460075dfb00c39c8b
Justin Sawyer <jtsaw36@gmail.com>
2021-08-28 12:26:54 -0400

    Improve layer references

--- keyboards/crkbd/keymaps/jussaw/keymap.c
+++ keyboards/crkbd/keymaps/jussaw/keymap.c
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                                    |  Ctrl  | Lower  | Space       Shift  | Upper  |  GUI   |
                                       //`--------------------------'  `--------------------------'
   [_ADJUST] = LAYOUT_split_3x6_3(
-        RESET,   DF(0),   DF(1), XXXXXXX, XXXXXXX,   TG(5),                      KC_MPLY, KC_BTN1, KC_BTN2, KC_BTN4, KC_BTN5, KC_ACL0,
+        RESET,DF(_QWERTY),DF(_COLEMAKDH),XXXXXXX, XXXXXXX,TG(_GAME),             KC_MPLY, KC_BTN1, KC_BTN2, KC_BTN4, KC_BTN5, KC_ACL0,
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, AG_SWAP,                      KC_VOLU, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ACL1,
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, AG_NORM,                      KC_VOLD, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_ACL2,
                                           XXXXXXX, KC_TRNS, XXXXXXX,    XXXXXXX, KC_TRNS, XXXXXXX
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_BSPC,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
       KC_QUOT, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                         KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
        KC_ENT, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-                                           KC_ESC,  KC_LALT, KC_SPC,    XXXXXXX, XXXXXXX,   TG(4)
+                                           KC_ESC,  KC_LALT, KC_SPC,    XXXXXXX, XXXXXXX,   TG(_GAME)
   ),
 };
 
