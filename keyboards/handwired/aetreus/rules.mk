94432528a628b8ce77176c2e6511135d17d22b0e
Johannes Jansson <johannes.e.jansson@gmail.com>
2021-08-29 22:19:29 +0200

    Apply suggestions from code review

--- keyboards/handwired/aetreus/rules.mk
+++ keyboards/handwired/aetreus/rules.mk
@@ -1,19 +1,7 @@
-# MCU name
 MCU = atmega32u4
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328p   USBasp
 BOOTLOADER = caterina
 
 # Build Options
-#   change yes to no to disable
-#
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
