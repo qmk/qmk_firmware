94693dcfe18e054e79a6cc15fd71a30573907358
Ananya Kirti <AnanyaKirti@users.noreply.github.com>
2021-08-29 23:15:59 +0530

    Update keyboards/bobpad/bobpad.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/bobpad/bobpad.c
+++ keyboards/bobpad/bobpad.c
@@ -15,3 +15,31 @@
  */
 
 #include "bobpad.h"
+
+bool led_update_kb(led_t led_state) {
+    if (!led_update_user(led_state)) { return false; }
+    
+    writePin(LED_CAPS_LOCK_PIN, led_state.caps_lock);
+    return true;
+};
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    
+    if(IS_LAYER_ON(1)) { // on Raise layer
+      if (clockwise) {
+          tap_code(KC_A);
+      } else {
+          tap_code(KC_B);
+      }
+    }
+    // default layer
+    else {
+      if (clockwise) {
+          tap_code(KC_C);
+      } else {
+          tap_code(KC_D);
+      }
+    }
+    return true;
+}
