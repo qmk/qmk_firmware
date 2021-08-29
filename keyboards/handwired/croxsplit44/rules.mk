ae8cfc82230067e260771a099ef8f982c635c832
Samuel Fischer <it.fischer.samuel@gmail.com>
2021-08-30 00:55:53 +0200

    Add files via upload

---
+++ keyboards/handwired/croxsplit44/rules.mk
@@ -0,0 +1,15 @@
+MCU = at90usb1286
+
+BOOTLOADER = atmel-dfu
+
+BOOTMAGIC_ENABLE = yes    
+MOUSEKEY_ENABLE = yes      
+EXTRAKEY_ENABLE = yes       
+CONSOLE_ENABLE = no        
+COMMAND_ENABLE = no         
+SLEEP_LED_ENABLE = no       
+NKRO_ENABLE = no            
+BACKLIGHT_ENABLE = no       
+RGBLIGHT_ENABLE = yes        
+BLUETOOTH_ENABLE = no       
+AUDIO_ENABLE = no           
