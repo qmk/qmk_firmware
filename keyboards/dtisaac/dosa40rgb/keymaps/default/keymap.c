c068f792378784a162db7815aea3b3dfa3fc15bf
Dao Tak Isaac <daotran0940@gmail.com>
2021-08-30 12:21:42 +0700

    Update keyboards/dtisaac/dosa40rgb/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dtisaac/dosa40rgb/keymaps/default/keymap.c
+++ keyboards/dtisaac/dosa40rgb/keymaps/default/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void rgb_matrix_indicators_user(void)
 {
- 	if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK))
+ 	if (host_keyboard_led_state().caps_lock)
 	{
 		rgb_matrix_set_color(22, 200, 200, 200);
 	}
