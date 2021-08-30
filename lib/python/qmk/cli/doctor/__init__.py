d93dd3e0fc9ddd61a39d05f98119156ad1fef578
JX <572673807@qq.com>
2021-08-30 12:58:39 +0800

    del libs

--- lib/python/qmk/cli/doctor/__init__.py
+++
@@ -1,5 +0,0 @@
-"""QMK Doctor
-
-Check out the user's QMK environment and make sure it's ready to compile.
-"""
-from .main import doctor
--- lib/python/qmk/tests/test_qmk_errors.py
+++
@@ -1,8 +0,0 @@
-from qmk.errors import NoSuchKeyboardError
-
-
-def test_nosuchkeyboarderror():
-    try:
-        raise NoSuchKeyboardError("test message")
-    except NoSuchKeyboardError as e:
-        assert e.message == 'test message'
