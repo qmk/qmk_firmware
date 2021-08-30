dfa3cbc2991fd11f49f2d76029d0eedca3d4d490
Dao Tak Isaac <daotran0940@gmail.com>
2021-08-30 12:21:51 +0700

    Update keyboards/dtisaac/dosa40rgb/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dtisaac/dosa40rgb/rules.mk
+++ keyboards/dtisaac/dosa40rgb/rules.mk
@@ -7,9 +7,9 @@ BOOTLOADER = lufa-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
@@ -18,9 +18,8 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-UNICODE_ENABLE = no 		# Unicode
-BLUETOOTH = AdafruitBLE     # Enable Bluetooth
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+BLUETOOTH = AdafruitBLE     # Enable Bluetooth
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
