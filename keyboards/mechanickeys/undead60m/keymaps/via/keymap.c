8bba945c0e924c1bb4981096d8ca750ffe9456c5
Francisco Escobar <jfescobar18@hotmail.com>
2021-08-30 02:55:43 -0500

    Copyright leyend updated and folder organization

--- keyboards/mechanickeys/undead60m/keymaps/via/keymap.c
+++ keyboards/mechanickeys/undead60m/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Francisco Escobar
+/* Copyright 2021 jfescobar18
   * 
   * This program is free software: you can redistribute it and/or modify 
   * it under the terms of the GNU General Public License as published by 
