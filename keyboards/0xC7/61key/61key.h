3ce233f8a2f1e473b25bdfeed0da8214339c3b39
RealEmanGaming <eman607@icloud.com>
2021-08-31 14:00:49 -0600

    Updated 61key

--- keyboards/0xC7/61key/61key.h
+++ keyboards/0xC7/61key/61key.h
@@ -18,6 +18,8 @@
 
 #include "quantum.h"
 
+#define ___ KC_NO
+
 /* This is a shortcut to help you visually see your layout.
  *
  * The first section contains all of the arguments representing the physical
@@ -35,7 +37,7 @@
 ) { \
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D }, \
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, KC_NO,    K2D }, \
-    { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, KC_NO,    KC_NO,    K3D }, \
-    { K40, K41, K42, KC_NO,    KC_NO,    K45, KC_NO,    KC_NO,    K48, K49, KC_NO,    K4B, KC_NO,    K4D }  \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, ___, K2D }, \
+    { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, ___, ___, K3D }, \
+    { K40, K41, K42, ___, ___, K45, ___, ___, K48, K49, ___, K4B, ___, K4D }  \
 }
