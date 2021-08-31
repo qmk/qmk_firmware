c7f346bef000ef2ffe7612be0c6fc606455a3e71
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2021-08-31 17:07:13 -0400

    Added code for the bigseries pcb.

---
+++ keyboards/bigseries/1key/keymaps/bbaserdem/keymap.c
@@ -0,0 +1,28 @@
+/*
+Copyright 2019 Batuhan Baserdem
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include QMK_KEYBOARD_H
+#include "bbaserdem.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(BB_BIGS)
+};
+
+void matrix_init_keymap(void) {
+    rgblight_enable();
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_MOOD);
+    rgblight_sethsv_noeeprom(0,255,255);
+}
