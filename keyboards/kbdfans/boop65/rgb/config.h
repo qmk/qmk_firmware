8633132c6c725c29ae9286098c2482b40d7a80b4
moyi4681 <moyi4681@users.noreply.github.com>
2021-08-31 09:28:03 +0800

    Update keyboards/kbdfans/boop65/rgb/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kbdfans/boop65/rgb/config.h
+++ keyboards/kbdfans/boop65/rgb/config.h
@@ -40,21 +40,24 @@
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
+
 #define USB_SUSPEND_WAKEUP_DELAY 5000
+
 #ifdef RGB_MATRIX_ENABLE
-#define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
-#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#define RGB_MATRIX_KEYPRESSES
-#define RGB_MATRIX_LED_PROCESS_LIMIT 4
-#define RGB_MATRIX_LED_FLUSH_LIMIT 26
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
-#define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_ALL
-#define DRIVER_ADDR_1 0b0110000
-#define DRIVER_ADDR_2 0b0110000 // this is here for compliancy reasons.
-#define DRIVER_COUNT 1
-#define DRIVER_1_LED_TOTAL 83
-#define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
-#define DRIVER_INDICATOR_LED_TOTAL 0
+#    define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#    define RGB_MATRIX_KEYPRESSES
+#    define RGB_MATRIX_LED_PROCESS_LIMIT 4
+#    define RGB_MATRIX_LED_FLUSH_LIMIT 26
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#    define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_ALL
+#    define DRIVER_ADDR_1 0b0110000
+#    define DRIVER_ADDR_2 0b0110000 // this is here for compliancy reasons.
+#    define DRIVER_COUNT 1
+#    define DRIVER_1_LED_TOTAL 83
+#    define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
+#    define DRIVER_INDICATOR_LED_TOTAL 0
 #endif
+
 #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
