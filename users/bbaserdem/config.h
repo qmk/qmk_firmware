6160b6198af4da40f3eadcc4a8f41130041b140b
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2021-08-31 17:01:04 -0400

    Removed obsolete code

--- users/bbaserdem/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef USERSPACE_CONFIG_H
-#define USERSPACE_CONFIG_H
-
-#ifdef TAP_DANCE_ENABLE
-    #define TAPPING_TERM 300
-    #define TAPPING_TOGGLE 1
-#endif
-
-#endif // !USERSPACE_CONFIG_H
