b8c68afb2f46fba3bae2a0cb6fdef867f95353a8
Takeshi ISHII <2170248+mtei@users.noreply.github.com>
2021-08-31 16:58:07 +0900

    [Keyboard] Helix use `post_rules.mk` (#14216)
    * Helix/pico use post_rules.mk insted of local_features.mk
    
    Tested with the following procedure.
    ```
    git checkout 77a93fec79 (qmk/develop)
    make clean
    make helix/pico:all:check-md5
    make helix/pico/sc:all:check-md5
    git checkout helix_use_post_rules.mk
    make clean
    make helix/pico:all:check-md5
    make helix/pico/sc:all:check-md5
    ```
    
    * Helix/rev2 use post_rules.mk
    
    Tested with the following procedure.
    ```
    git checkout 77a93fec79 (qmk/develop)
    make clean
    make helix/pico:all:check-md5
    make helix/pico/sc:all:check-md5
    make helix/rev2:all:check-md5
    make helix/rev2/sc:all:check-md5
    git checkout helix_use_post_rules.mk
    make clean
    make helix/pico:all:check-md5
    make helix/pico/sc:all:check-md5
    make helix/rev2:all:check-md5
    make helix/rev2/sc:all:check-md5
    ```

--- keyboards/helix/rev2/keymaps/five_rows/rules.mk
+++ keyboards/helix/rev2/keymaps/five_rows/rules.mk
@@ -95,7 +95,3 @@ endif
 ifeq ($(strip $(OLED_ENABLE)), yes)
     SRC += oled_display.c
 endif
-
-# convert Helix-specific options (that represent combinations of standard options)
-#   into QMK standard options.
-include $(strip $(KEYBOARD_LOCAL_FEATURES_MK))
