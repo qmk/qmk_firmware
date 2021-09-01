1ceb585fb4d982913bf9640b718fad5376b372c5
Shay Merrill <shaymdev@gmail.com>
2021-08-31 18:06:32 -0600

    orthodox rgb fix

--- keyboards/orthodox/keymaps/shaymdev/config.h
+++ keyboards/orthodox/keymaps/shaymdev/config.h
@@ -19,10 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "config_common.h"
+#pragma once
 
 /* Use I2C or Serial, not both */
 
@@ -30,9 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 
 /* Select hand configuration */
-
 // #define MASTER_LEFT
-// #define _MASTER_RIGHT
+// #define MASTER_RIGHT
 #define EE_HANDS
 
 #undef RGBLED_NUM
@@ -40,5 +36,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLED_NUM 10
 #undef RGB_DI_PIN
 #define RGB_DI_PIN D3
-
-#endif
