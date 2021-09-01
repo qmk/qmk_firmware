9395685fad1c3b755c5241b8ee9ac1bf540b9876
Jonavin <code@jonavin.net>
2021-09-01 13:35:59 -0400

    fix typo

--- keyboards/mechwild/murphpad/keymaps/jonavin/keymap.c
+++ keyboards/mechwild/murphpad/keymaps/jonavin/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_NLCK,  KC_PSLS,  KC_PAST, KC_PMNS, KC_PPLS,      KC_BSPC,
         KC_TAB,   KC_P7,    KC_P8,   KC_P9,   KC_PDOT,      KC_RGUI,
-        KC_LRSFT, KC_P4,    KC_P5,   KC_P6,   KC_COMMA,     KC_RCTL,
+        KC_RSFT,  KC_P4,    KC_P5,   KC_P6,   KC_COMMA,     KC_RCTL,
         KC_P0,    KC_P1,    KC_P2,   KC_P3,   KC_PENT,      KC_RALT,
 
                  TT(_FN3), TT(_FN4), TT(_RGB)
