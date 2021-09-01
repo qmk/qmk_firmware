a21758521c564678579ada5038e299dc3d08e46a
Hampus <hampus.andersson@gmail.com>
2021-09-01 07:50:58 +0200

    Removed empty file and ref

--- users/hvp/per_key.c
+++ users/hvp/per_key.c
@@ -14,8 +14,8 @@
   * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
   */ 
 
-#pragma once
-#include per_key.h
+
+// #include per_key.h
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
