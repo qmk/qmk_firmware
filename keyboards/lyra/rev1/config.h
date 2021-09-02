37e3792839a03d750174ba0343988e963b6d6573
Malevolti <82970232+Malevolti@users.noreply.github.com>
2021-09-02 18:21:43 +0100

    Update config.h

--- keyboards/lyra/rev1/config.h
+++ keyboards/lyra/rev1/config.h
@@ -28,8 +28,8 @@
 #define MATRIX_COLS 7
 
 // wiring of each half
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, }
-#define MATRIX_COL_PINS { B2, B3, B1, F7, F6, F5, F4, }
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { B2, B3, B1, F7, F6, F5, F4 }
 
 #define DIODE_DIRECTION COL2ROW
 
