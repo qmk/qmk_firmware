cb53c4fd41885ba60192cbd0b5fd23d6a687ab17
hvp <32483282+hvpkod@users.noreply.github.com>
2021-09-02 05:44:36 +0200

    [Keymap] hvp keymap for Cornelius, updated ut47.2 and userspace (#14187)

--- users/hvp/config.h
+++ users/hvp/config.h
@@ -13,31 +13,6 @@
   * You should have received a copy of the GNU General Public License 
   * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
   */ 
-
-
 #pragma once
-#include "quantum.h"
-#undef TAPPING_TERM
-#define TAPPING_TERM 150
 
-#ifdef TAP_DANCE_ENABLE
-#    include "tap_dances.h"
-#endif
-
-#ifdef TAPPING_TERM_PER_KEY
-#    include "per_key.h"
-#endif
-
-
-#define D_NAVI MT(MOD_LCTL | MOD_LSFT, KC_D)
-
-/* Home-row mods */
-#define LG_A MT(MOD_LGUI, KC_A)
-#define LA_S MT(MOD_LALT, KC_S)
-#define LS_D MT(MOD_LSFT, KC_D)
-#define LC_F MT(MOD_LCTL, KC_F)
-
-#define RC_H MT(MOD_RCTL, KC_H)
-#define RS_J MT(MOD_RSFT, KC_J)
-#define RA_K MT(MOD_RALT, KC_K)
-#define RG_L MT(MOD_RGUI, KC_L)
+#define LONG_TAPPING_TERM 1000
