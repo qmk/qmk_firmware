4f40eca358a10718e2d8a02fcca7b1d52537ac66
donicrosby <donicrosby1995@gmail.com>
2021-09-02 18:45:13 -0400

    Added Copywrite stuff on source files

--- drivers/led/issi/is31fl3733.h
+++ drivers/led/issi/is31fl3733.h
@@ -1,6 +1,7 @@
 /* Copyright 2017 Jason Williams
  * Copyright 2018 Jack Humbert
  * Copyright 2018 Yiancar
+ * Copyright 2021 Doni Crosby
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
