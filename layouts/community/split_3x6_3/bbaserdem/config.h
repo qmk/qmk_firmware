5249512387b8bf9a9b9dfba5e9afed7ed5fbfa9d
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2021-09-01 23:06:05 -0400

    Moved layout a bit to ease non-confusion in build; and brought up to date.

--- layouts/community/split_3x6_3/bbaserdem/config.h
+++ layouts/community/split_3x6_3/bbaserdem/config.h
@@ -15,47 +15,62 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Split definition; use the following to flash;
 //  pro-micro:      avrdude-split-left/right
 //  elite-c:        dfu-split-left/right
-#define EE_HANDS
 
 // Code pertaining to Corne ARM; from Waffle's keymap
 #ifdef KEYBOARD_crkbd_rev1
-    // Undo Eeprom handedness
-    #undef EE_HANDS
     #include "config_common.h"
+
+    // Undo Eeprom handedness, try EE_HANDS later
+    #undef EE_HANDS
+    #define MASTER_RIGHT
+
+    // Rebrand the PCB
     #undef PRODUCT
     #undef MANUFACTURER
     #define MANUFACTURER Waffles
     #define PRODUCT Protorne Keyboard
+
+    // Remove OLED definitions? 
     #undef USE_I2C
     #undef SSD1306OLED
-    #define MASTER_RIGHT
+
+    // Audio output on pin A5 (actualy will be output on B1)
     #ifdef AUDIO_ENABLE
         #define AUDIO_PIN A5
     #endif
+
+    // Communication has to be serial on ARM
     #define USE_SERIAL_PD2
-    #ifdef RGB_MATRIX_ENABLE
-        #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
-        #define RGB_MATRIX_SPLIT { 27, 27 }
-        #define SPLIT_TRANSPORT_MIRROR
-    #endif
+    // Set serial pin as D3 for sure
+    #undef SOFT_SERIAL_PIN
+    #define SOFT_SERIAL_PIN D3
+    #define SERIAL_USART_SPEED 921600
+    #define SERIAL_USART_DRIVER SD1
+    #define SERIAL_USART_TX_PAL_MODE 7
+
+    // Things to make RGB matrix work
     #undef RGBLED_NUM
     #define RGBLED_NUM 54
     #undef RGB_DI_PIN
     #define RGB_DI_PIN B5
-    #ifdef OLED_DRIVER_ENABLE
-        #define OLED_TIMEOUT 50000
-        #define OLED_UPDATE_INTERVAL 100
+    #ifdef RGB_MATRIX_ENABLE
+        #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
+        #define RGB_MATRIX_SPLIT { 27, 27 }
+        #define SPLIT_TRANSPORT_MIRROR
     #endif
     #define WS2812_PWM_DRIVER PWMD3
     #define WS2812_PWM_CHANNEL 3
     #define WS2812_PWM_PAL_MODE 2
     #define WS2812_DMA_STREAM STM32_DMA1_STREAM3
     #define WS2812_DMA_CHANNEL 3
-    #undef SOFT_SERIAL_PIN
-    #define SOFT_SERIAL_PIN D3
-    #define SERIAL_USART_SPEED 921600
-    #define SERIAL_USART_DRIVER SD1
-    #define SERIAL_USART_TX_PAL_MODE 7
+
+    // OLED related code
+    #ifdef OLED_DRIVER_ENABLE
+        #define OLED_TIMEOUT 50000
+        #define OLED_UPDATE_INTERVAL 100
+    #endif
+    
+    // Encoder definitons
     #define ENCODERS_PAD_A { B2 }
     #define ENCODERS_PAD_B { B6 }
     #define ENCODERS_PAD_A_RIGHT { B2 }
