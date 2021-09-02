2745bcb7e80d7805851ac6e748dd9968124fa402
GG <72414103+spbgzh@users.noreply.github.com>
2021-09-02 03:54:35 +0300

    Update zoom65.h

--- keyboards/meletrix/zoom65/zoom65.h
+++ keyboards/meletrix/zoom65/zoom65.h
@@ -183,5 +183,5 @@
     { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D,   K1E   }, \
     { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   KC_NO, K2E   }, \
     { K30,   KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D,   K3E   }, \
-    { K40,   K41,   K42,   KC_NO, K44,   KC_NO, K46, KC_NO, K48,   KC_NO, K4A,   K4B,   K4C,   K4D,   K4E   }, \
+    { K40,   K41,   K42,   KC_NO, K44,   KC_NO, K46,   KC_NO, K48,   KC_NO, K4A,   K4B,   K4C,   K4D,   K4E   }, \
 }
