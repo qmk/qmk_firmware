e6272a97bc0575e8f7bc0d31cf17d50335141c92
projectcain <71684775+projectcain@users.noreply.github.com>
2021-09-02 07:29:11 -0500

    Update keyboards/projectcain/vault35/vault35.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/projectcain/vault35/vault35.h
+++ keyboards/projectcain/vault35/vault35.h
@@ -27,7 +27,7 @@
  * represents the switch matrix.
  */
 
-#define LAYOUT_split1( \
+#define LAYOUT_split_4space( \
     K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, \
     K100, K101, K102, K103, K104, K105, K106, K107, K108, K109,       \
     K200, K201, K202, K203, K204, K205, K206, K207, K208, K209,       \
