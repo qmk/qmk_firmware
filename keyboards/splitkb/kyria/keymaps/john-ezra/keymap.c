da61cd458c4a1e6bc8cb7d19b1b9b586f19fe72e
aro6ant <johnezra@dempseytech.com>
2021-09-03 01:44:00 -0700

    added comments for clarity

--- keyboards/splitkb/kyria/keymaps/john-ezra/keymap.c
+++ keyboards/splitkb/kyria/keymaps/john-ezra/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-        case HNTS:
+        case HNTS:  //Layer Control
           if (record->event.pressed) {
             set_single_persistent_default_layer(_HNTS);
           }
