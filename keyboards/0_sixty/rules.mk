87732ee89f95dceb72cd7a965e32d085a841917b
Vinam Arora <vinam@posteo.de>
2021-09-05 12:21:44 +0530

    0-Sixty: Adds sub-keyboard with underglow

--- keyboards/0_sixty/rules.mk
+++ keyboards/0_sixty/rules.mk
@@ -19,3 +19,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+
+LTO_ENABLE = yes            # Link-time opt
