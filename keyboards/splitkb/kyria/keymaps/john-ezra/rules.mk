9cd968736c58c6387ae58eb051237e8d1f688a3d
aro6ant <johnezra@dempseytech.com>
2021-09-04 21:31:58 -0700

    Changed OLED driver setup in rules.mk

--- keyboards/splitkb/kyria/keymaps/john-ezra/rules.mk
+++ keyboards/splitkb/kyria/keymaps/john-ezra/rules.mk
@@ -1,4 +1,5 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+OLED_ENABLE = yes          # Enables the use of OLED displays
+OLED_DRIVER = SSD1306
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 NKRO_ENABLE = yes          # Enable N-Key Rollover
 ENCODER_ENABLE = no        # Disable Rotary Encoder
