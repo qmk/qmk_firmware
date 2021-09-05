866b28407cb8f0d8dfdf41bcb7519ec2e16a4fbe
Alin Marin Elena <alin@elena.space>
2021-09-05 23:21:46 +0100

    Update keyboards/m65/keymaps/via/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/m65/keymaps/via/config.h
+++ keyboards/m65/keymaps/via/config.h
@@ -15,6 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#pragma once
+
 #define TAPPING_TOGGLE 2
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
