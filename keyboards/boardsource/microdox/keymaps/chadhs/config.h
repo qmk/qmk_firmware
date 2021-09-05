f3215d2422370f44eb943623d255fac0c0e6de5c
Chad Stovern <chad@stovern.me>
2021-09-05 07:51:14 -0500

    trying a new home row mods setup

--- keyboards/boardsource/microdox/keymaps/chadhs/config.h
+++ keyboards/boardsource/microdox/keymaps/chadhs/config.h
@@ -1,13 +1,16 @@
-/* https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#tapping-term */
+// Configure the global tapping term (default: 200ms)
+// https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#tapping-term
 #define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
 
-/* https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt */
+// Prevent normal rollover on alphas from accidentally triggering mods.
+// https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
 #define IGNORE_MOD_TAP_INTERRUPT
 #define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
 
-/* https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#retro-tapping */
-/* #define RETRO_TAPPING */
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#tapping-force-hold
+#define TAPPING_FORCE_HOLD
 
 /*
   allow the microdox board to work with either controller plugged in
@@ -16,15 +19,13 @@
   flashing the controllers:
   make boardsource/microdox:chadhs:dfu-split-left
   make boardsource/microdox:chadhs:dfu-split-right
-
-  loading keymap with via: load your via keymap with each side plugged in
 */
 #define EE_HANDS
 
-/* https://beta.docs.qmk.fm/developing-qmk/qmk-reference/config_options#features-that-can-be-enabled */
+// https://beta.docs.qmk.fm/developing-qmk/qmk-reference/config_options#features-that-can-be-enabled
 #define FORCE_NKRO
 
-/* combos: https://beta.docs.qmk.fm/using-qmk/software-features/feature_combo */
+// https://beta.docs.qmk.fm/using-qmk/software-features/feature_combo
 #define COMBO_ALLOW_ACTION_KEYS
 #define COMBO_VARIABLE_LEN
 #define COMBO_PERMISSIVE_HOLD
--- keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
+++ keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
@@ -32,21 +32,21 @@ enum layers {
 #define GAMING TG(_GAMING)
 
 /* home row mods */
-#define HOME_A SFT_T(KC_A)
-#define HOME_R CTL_T(KC_R)
-#define HOME_S OPT_T(KC_S)
-#define HOME_T CMD_T(KC_T)
+#define HOME_A CTL_T(KC_A)
+#define HOME_R OPT_T(KC_R)
+#define HOME_S CMD_T(KC_S)
+#define HOME_T SFT_T(KC_T)
 #define HOME_Z SFT_T(KC_Z)
-#define HOME_BT SFT_T(KC_GRV)
-#define HOME_N CMD_T(KC_N)
-#define HOME_E OPT_T(KC_E)
-#define HOME_I CTL_T(KC_I)
-#define HOME_O SFT_T(KC_O)
+#define HOME_BT CTL_T(KC_GRV)
+#define HOME_N SFT_T(KC_N)
+#define HOME_E CMD_T(KC_E)
+#define HOME_I OPT_T(KC_I)
+#define HOME_O CTL_T(KC_O)
 #define HOME_SL SFT_T(KC_SLSH)
-#define HOME_DWN CMD_T(KC_DOWN)
-#define HOME_UP OPT_T(KC_UP)
-#define HOME_RGT CTL_T(KC_RGHT)
-#define HOME_QT SFT_T(KC_QUOT)
+#define HOME_DWN SFT_T(KC_DOWN)
+#define HOME_UP CMD_T(KC_UP)
+#define HOME_RGT OPT_T(KC_RGHT)
+#define HOME_QT CTL_T(KC_QUOT)
 #define HOME_BSL SFT_T(KC_BSLS)
 
 /* layer definitions */
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  |      |      |      |      |      |            |      |      |      |      |      |
  |------+------+------+------+------|            |------+------+------+------+------|
  | A    | R    | S    | T    | G    |            | M    | N    | E    | I    | O    |
- | SFT  | CTL  | OPT  | CMD  |      |            |      | CMD  | OPT  | CTL  | SFT  |
+ | CTL  | OPT  | CMD  | SFT  |      |            |      | SFT  | CMD  | OPT  | CTL  |
  |------+------+------+------+------|            |------+------+------+------+------|
  | Z    | X    | C    | D    | V    |            | K    | H    | , <  | . >  | / ?  |
  | SFT  |      |      |      |      |            |      |      |      |      | SFT  |
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  |      |      |      |      |      |            |      |      |      |      |      |
  |------+------+------+------+------|            |------+------+------+------+------|
  | ` ~  |      |      |      |      |            | ←    | ↓    | ↑    | →    | ' "  |
- | SFT  | CTL  | OPT  | CMD  |      |            |      | CMD  | OPT  | CTL  | SFT  |
+ | CTL  | OPT  | CMD  | SFT  |      |            |      | SFT  | CMD  | OPT  | CTL  |
  |------+------+------+------+------|            |------+------+------+------+------|
- | Z    |      |      |      |      |            | - _  | = +  | [ {  | ] }  | \ |  |
+ |      |      |      |      |      |            | - _  | = +  | [ {  | ] }  | \ |  |
  | SFT  |      |      |      |      |            |      |      |      |      | SFT  |
  `------'------'------'------'------'            `------'------'------'------'------'
                   .------.------.------.      .------.------.------.
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   '------'------'------'      '------'------'------'
 */
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,     KC_8,    KC_9,     KC_0,
-  HOME_BT, KC_LCTL, KC_LOPT, KC_LCMD, XXXXXXX,      KC_LEFT, HOME_DWN, HOME_UP, HOME_RGT, HOME_QT,
+  HOME_BT, KC_LOPT, KC_LCMD, KC_LSFT, XXXXXXX,      KC_LEFT, HOME_DWN, HOME_UP, HOME_RGT, HOME_QT,
   KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS, KC_EQL,   KC_LBRC, KC_RBRC,  HOME_BSL,
                     XXXXXXX, KC_ESC,  KC_TRNS,      KC_TRNS, KC_TRNS,  XXXXXXX
 ),
@@ -106,18 +106,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  |      |      |      |      |      |            |      |      |      | F11  | F12  |
  |      |      |      |      |      |            |      |      |      |      |      |
  |------+------+------+------+------|            |------+------+------+------+------|
- | RGB  | RGB  | RGB  | RGB- | RGB+ |            |      |      |      |      |      |
+ | RGB  | RGB  | RGB  | RGB- | RGB+ |            |      |      |      |      | GAME |
  | TGGL | RMOD | MOD  |      |      |            |      |      |      |      |      |
  `------'------'------'------'------'            `------'------'------'------'------'
                   .------.------.------.      .------.------.------.
-                  | XXX  | ESC  | DEL  |      | ↓↓↓  | GAME | XXX  |
+                  | XXX  | ESC  | DEL  |      | ↓↓↓  | ↓↓↓  | XXX  |
                   |      |      |      |      |      |      |      |
                   '------'------'------'      '------'------'------'
 */
   KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,
   RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAD, RGB_VAI,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GAMING,
-                     XXXXXXX, KC_ESC,  KC_DEL,       KC_TRNS, GAMING,  XXXXXXX
+                     XXXXXXX, KC_ESC,  KC_DEL,       KC_TRNS, KC_TRNS,  XXXXXXX
 ),
 
 [_GAMING] = LAYOUT_split_3x5_3(
@@ -150,7 +150,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // https://precondition.github.io/home-row-mods#finding-the-sweet-spot
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case CMD_TAB: /* make CMD SPC faster */
+    case CMD_TAB: // make CMD SPC faster
+    case HOME_Z:  // make alternate shift faster
+    case HOME_SL: // make alternate shift faster
         return false;
     default:
         return true;
@@ -161,19 +163,15 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     /* adjust home row key timing to prevent letter rolls triggering modifiers */
+    case HOME_A:
     case HOME_R:
     case HOME_S:
     case HOME_T:
     case HOME_N:
     case HOME_E:
     case HOME_I:
-        return TAPPING_TERM + 150;
-    /* adjust mod-tap shift keys separately for quick typing */
-    case HOME_A:
     case HOME_O:
-    case HOME_Z:
-    case HOME_SL:
-        return TAPPING_TERM - 20;
+        return TAPPING_TERM + 150;
     default:
         return TAPPING_TERM;
     }
