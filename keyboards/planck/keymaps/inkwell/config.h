80d879c8a42fbf257b7dcdbb88e28f56852e52aa
Kevin Hartley <kevin.s.hartley@gmail.com>
2021-09-05 10:38:11 -0600

    feat(inkwell): GNU license addition

--- keyboards/planck/keymaps/inkwell/config.h
+++ keyboards/planck/keymaps/inkwell/config.h
@@ -1,3 +1,19 @@
+/* Copyright 2021 Kevin Hartley
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
 #pragma once
 
 #ifdef AUDIO_ENABLE
@@ -33,6 +49,5 @@
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
-#define MOUSEKEY_WHEEL_MAX_SPEED 14
-
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 10
+#define MOUSEKEY_MAX_SPEED 12
