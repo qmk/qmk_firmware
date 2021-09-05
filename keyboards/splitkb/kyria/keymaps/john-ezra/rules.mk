8de4c5575a246a7b6d0ab201daee815137c91160
aro6ant <johnezra@dempseytech.com>
2021-09-04 21:35:16 -0700

    Disabled unused features to save space

--- keyboards/splitkb/kyria/keymaps/john-ezra/rules.mk
+++ keyboards/splitkb/kyria/keymaps/john-ezra/rules.mk
@@ -3,3 +3,6 @@ OLED_DRIVER = SSD1306
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 NKRO_ENABLE = yes          # Enable N-Key Rollover
 ENCODER_ENABLE = no        # Disable Rotary Encoder
+SPACE_CADET_ENABLE = no    # Disable Space Cadet Shift
+GRAVE_ESC_ENABLE = no      # Disable Grave Escape
+MOUSEKEY_ENABLE =  no      # Disable Mousekeys
