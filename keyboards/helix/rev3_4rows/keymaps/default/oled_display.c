4ddf9b8762659025bc166a9b97238bbb1cefdcd4
mtei <2170248+mtei@users.noreply.github.com>
2021-09-07 04:37:51 +0900

    remove unnecessary comment in rev3_Xrows/keymaps/default/oled_display.c

--- keyboards/helix/rev3_4rows/keymaps/default/oled_display.c
+++ keyboards/helix/rev3_4rows/keymaps/default/oled_display.c
@@ -14,17 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
-   How to Customize
-
-   $ make helix/rev3_4rows:YOUR_KEYMAP:clean
-   $ cp keyboards/helix/rev3_4rows/oled_display.c keyboards/helix/rev3_4rows/keymaps/YOUR_KEYMAP
-
-   $ edit keyboards/helix/rev3_4rows/keymaps/YOUR_KEYMAP/oled_display.c
-   $ make helix/rev3_4rows:YOUR_KEYMAP
-   $ make helix/rev3_4rows:YOUR_KEYMAP:flash
- */
-
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
