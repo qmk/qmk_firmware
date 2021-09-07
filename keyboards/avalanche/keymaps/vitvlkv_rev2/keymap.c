bd3d02e606a8ade0abca7f6e25ede39336cb7f85
vitvlkv <vitvlkv@gmail.com>
2021-09-07 22:02:30 +0300

    Returned back tap-hold thumb shifts

--- keyboards/avalanche/keymaps/vitvlkv_rev2/keymap.c
+++ keyboards/avalanche/keymaps/vitvlkv_rev2/keymap.c
@@ -14,8 +14,8 @@ enum layer {
 #define FN_1 MO(LAYER_1)
 #define LFN_2 LT(LAYER_2, KC_GRV)
 #define RFN_2 LT(LAYER_2, KC_RGUI)
-// #define LFN_3 LSFT_T(KC_EQL)
-// #define RFN_3 RSFT_T(KC_MINS)
+#define LFN_3 LSFT_T(KC_EQL)
+#define RFN_3 RSFT_T(KC_MINS)
 #define EN_LALT LALT_T(KC_ENT)
 #define EN_RALT RALT_T(KC_ENT)
 
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_CAPS, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL,  KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RBRC,
                  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS, KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-                                   KC__MUTE,LFN_2,   KC_LSFT, KC_SPC,  EN_LALT, EN_RALT, FN_1,    KC_RSFT, RFN_2,   KC_PSCR
+                                   KC__MUTE,LFN_2,   LFN_3,   KC_SPC,  EN_LALT, EN_RALT, FN_1,    RFN_3,   RFN_2,   KC_PSCR
     ),
     [LAYER_1] = LAYOUT(
                  _______, KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,
