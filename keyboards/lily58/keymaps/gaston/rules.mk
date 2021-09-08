e6800618d802fafddd05d8ffd29f1f5ab4ac6ad8
Gastón Jorquera <email@gaston.life>
2021-09-08 16:02:12 -0700

    Remove unnecessary rules.mk flags
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/gaston/rules.mk
+++ keyboards/lily58/keymaps/gaston/rules.mk
@@ -1,19 +1,7 @@
-MOUSEKEY_ENABLE   = yes
-EXTRAKEY_ENABLE   = yes
-
-BOOTMAGIC_ENABLE  = no
-CONSOLE_ENABLE    = no
-COMMAND_ENABLE    = no
-NKRO_ENABLE       = no
-BACKLIGHT_ENABLE  = no
-MIDI_ENABLE       = no
-AUDIO_ENABLE      = no
-UNICODE_ENABLE    = no
-BLUETOOTH_ENABLE  = no
-RGBLIGHT_ENABLE   = no
-SWAP_HANDS_ENABLE = no
-OLED_ENABLE       = no
-SLEEP_LED_ENABLE  = no
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+RGBLIGHT_ENABLE = no
+OLED_ENABLE = no
 
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
