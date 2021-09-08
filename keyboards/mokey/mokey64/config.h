7c2a97dc98d2cd3c9367a8b99d82fcae5a20da68
RH <hej163@163.com>
2021-09-08 14:26:32 +0800

    ReorganizationReorganization

--- keyboards/mokey/mokey64/config.h
+++ keyboards/mokey/mokey64/config.h
@@ -24,10 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT MOKEY64
 
 #define MATRIX_ROWS 5
-#define MATRIX_COLS 15
+#define MATRIX_COLS 14
 
 #define MATRIX_ROW_PINS { A1, A2, A3, A4, A5 }
-#define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12, B13, B14, }
+#define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12, B13 }
 #define UNUSED_PINS
 
 #define DIODE_DIRECTION COL2ROW
