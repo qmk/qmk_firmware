18a9fee460a93662c1dd9014721220c7980cd3ff
Chad Stovern <chad@stovern.me>
2021-09-08 14:04:06 -0500

    add a ctrl key to gaming layer

--- keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
+++ keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
@@ -30,6 +30,7 @@ enum layers {
 #define FUN_SPC LT(_FUNCTION,KC_SPC)
 #define CMD_TAB CMD_T(KC_TAB)
 #define GAMING TG(_GAMING)
+#define CTL_SPC CTL_T(KC_SPC)
 
 /* home row mods */
 #define HOME_A CTL_T(KC_A)
@@ -133,14 +134,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  | SFT  |      |      |      |      |            |      |      |      |      |      |
  `------'------'------'------'------'            `------'------'------'------'------'
                   .------.------.------.      .------.------.------.
-                  | XXX  |      | SPC  |      | ↓↓↓  | GAME | XXX  |
-                  |      | CMD  |      |      |      |      |      |
+                  | XXX  |      | SPC  |      | SPC  | GAME | XXX  |
+                  |      | CMD  |      |      | CTL  |      |      |
                   '------'------'------'      '------'------'------'
 */
   KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   KC_ESC,  KC_A, KC_S,    KC_D,    KC_F,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
   KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BSLS,
-                 XXXXXXX, KC_LCMD, KC_SPC,      KC_TRNS, GAMING,  XXXXXXX
+                 XXXXXXX, KC_LCMD, KC_SPC,      CTL_SPC, GAMING,  XXXXXXX
 ),
 };
 
