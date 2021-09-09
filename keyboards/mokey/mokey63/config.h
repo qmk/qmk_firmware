4e222d3e896c3b0b7985ea84735c498297ce377a
RH <hej163@163.com>
2021-09-09 10:50:51 +0800

    Revise

--- keyboards/mokey/mokey63/config.h
+++ keyboards/mokey/mokey63/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 15
 
 #define MATRIX_ROW_PINS { B5, B6, B2, B3, B1 }
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4，F4, F5, F6, F7, D5, D4, D6, D7 }
+#define MATRIX_COL_PINS { C7, F6, F5, F4, F1, E6, D0, D1, D2, D3, D5, D4, D6, D7, B4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
