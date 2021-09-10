7ce196705c6926579f31b0a813c6d2ea8da015cc
Wolf Van Herreweghe <wolfvanh@gmail.com>
2021-09-10 20:48:25 +0200

    Fix space bar LED and Switch assignment

--- keyboards/keybee/keybee65/keybee65.h
+++ keyboards/keybee/keybee65/keybee65.h
@@ -24,9 +24,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
     K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,      K3E, K3F, \
     K40, K41, K42,                K46,                K4A, K4B, K4C, K4D, K4E, K4F \
 ) { \
-    { K00, K01,   K02, K03,   K04,   K05,   K06, K07,   K08,   K09,   K0A, K0B, K0C, KC_NO, K0E, K0F   }, \
-    { K10, KC_NO, K12, K13,   K14,   K15,   K16, K17,   K18,   K19,   K1A, K1B, K1C, K1D,   K1E, K1F   }, \
-    { K20, KC_NO, K22, K23,   K24,   K25,   K26, K27,   K28,   K29,   K2A, K2B, K2C, KC_NO, K2E, K2F   }, \
-    { K30, KC_NO, K32, K33,   K34,   K35,   K36, K37,   K38,   K39,   K3A, K3B, K3C, KC_NO, K3E, K3F   }, \
-    { K40, K41,   K42, KC_NO, KC_NO, KC_NO, K46, KC_NO, KC_NO, KC_NO, K4A, K4B, K4C, K4D,   K4E, K4F   }  \
+    { K00, K01,   K02, K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A, K0B, K0C, KC_NO, K0E, K0F   }, \
+    { K10, KC_NO, K12, K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A, K1B, K1C, K1D,   K1E, K1F   }, \
+    { K20, KC_NO, K22, K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A, K2B, K2C, KC_NO, K2E, K2F   }, \
+    { K30, KC_NO, K32, K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A, K3B, K3C, KC_NO, K3E, K3F   }, \
+    { K40, K41,   K42, KC_NO, KC_NO, KC_NO, KC_NO, K46,   KC_NO, KC_NO, K4A, K4B, K4C, K4D,   K4E, K4F   }  \
 }
