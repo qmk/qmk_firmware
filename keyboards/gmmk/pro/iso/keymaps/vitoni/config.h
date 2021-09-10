a1db1307a1bea255c79dbf0ae5b8948f2415944d
Victor Toni <victor.toni@gmail.com>
2021-09-10 19:01:27 +0200

    Configure idle / USB suspend settings

--- keyboards/gmmk/pro/iso/keymaps/vitoni/config.h
+++ keyboards/gmmk/pro/iso/keymaps/vitoni/config.h
@@ -5,4 +5,7 @@
 
 #if defined(RGB_MATRIX_ENABLE)
     #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+     // number of milliseconds to wait until turning off RGB automatically
+    #define RGB_DISABLE_TIMEOUT 300000 // 300 seconds / 5 min
+    #define RGB_DISABLE_WHEN_USB_SUSPENDED
 #endif
