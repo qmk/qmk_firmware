62eea671806d4f6f5a7cbce780342e76f2807b4b
fauxpark <fauxpark@gmail.com>
2021-09-11 15:05:17 +1000

    Fix last test

--- keyboards/handwired/pytest/config.h
+++ keyboards/handwired/pytest/config.h
@@ -2,13 +2,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6465
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    none
-#define PRODUCT         pytest
-
 /* key matrix size */
 #define MATRIX_ROWS 1
 #define MATRIX_COLS 1
