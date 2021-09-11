265a4d278c9b0a683fa16bba33f0c0833361752a
Greg Knapp <virtual.greg@gmail.com>
2021-09-11 21:41:54 +0100

    Add via support

--- keyboards/yampad/rules.mk
+++ keyboards/yampad/rules.mk
@@ -2,24 +2,24 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = caterina
+#BOOTLOADER = caterina  # ProMicro
+BOOTLOADER = atmel-dfu  # Elite-C, SeaMicro etc.
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+AUDIO_ENABLE = no           # Audio output
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
 COMMAND_ENABLE = no         # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+CONSOLE_ENABLE = no         # Console for debug
+DEBOUNCE_TYPE = sym_eager_pk
+EXTRAKEY_ENABLE = no        # Audio control and System control
+LTO_ENABLE = yes            # Link time optimise, reduce firmware size
+MOUSEKEY_ENABLE = no        # Mouse keys
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output
-OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
-DEBOUNCE_TYPE = sym_eager_pk
+OLED_ENABLE = yes
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
