853a7e55b286a7e0a3210165e003be799d42dd91
fauxpark <fauxpark@gmail.com>
2021-09-11 12:33:42 +1000

    Remove BLUETOOTH_ENABLE from keyboard-level rules.mk, handwired

--- keyboards/handwired/frenchdev/rules.mk
+++ keyboards/handwired/frenchdev/rules.mk
@@ -18,7 +18,6 @@ NKRO_ENABLE      = yes # USB Nkey Rollover - if this doesn't work, see here: htt
 UNICODE_ENABLE   = yes # Unicode
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
 UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 RGBLIGHT_ENABLE = no
 
