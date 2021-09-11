73ef5677c4e878aac4f40806bcc2c4913371c8da
Alfie Lockrey <47219621+AlfieLockrey@users.noreply.github.com>
2021-09-11 14:37:54 +0100

    Moved Encoder code from keymaps to keyboard.c file
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/paulpcb/paulpcb.c
+++ keyboards/paulpcb/paulpcb.c
@@ -15,3 +15,13 @@
  */
 
 #include "paulpcb.h"
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    if (clockwise) {
+      tap_code(KC_VOLU);
+    } else {
+      tap_code(KC_VOLD);
+    }
+    return true;
+}
