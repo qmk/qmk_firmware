fa5bc611b3cb9057379032e0ea9c1d79229bc62f
Elliot Powell <johnp8727@gmail.com>
2021-09-12 23:02:25 +0100

    liscenses

--- keyboards/keebsforall/coarse60/config.h
+++ keyboards/keebsforall/coarse60/config.h
@@ -1,7 +1,5 @@
-#pragma once
-
 /*
-Copyright 2015 Jun Wako <wakojun@gmail.com>
+Copyright 2021 Elliot Powell
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,7 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #pragma once
 
 /* USB Device descriptor parameter */
