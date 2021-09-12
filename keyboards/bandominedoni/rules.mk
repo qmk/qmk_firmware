d1c343ad78c4a322448cab4f9f27100f8a07b36d
3araht <3araht@gmail.com>
2021-09-12 21:05:14 +0900

    firmware shrink by LTO_ENABLE.

--- keyboards/bandominedoni/rules.mk
+++ keyboards/bandominedoni/rules.mk
@@ -24,3 +24,5 @@ ENCODER_ENABLE = yes        # encoder on mute button
 SPLIT_KEYBOARD = yes        # Enables split keyboard support
 RGB_MATRIX_ENABLE = no      # Use RGB matrix (Don't enable this when RGBLIGHT_ENABLE is used.)
 RGB_MATRIX_DRIVER = WS2812  #
+
+LTO_ENABLE = yes
