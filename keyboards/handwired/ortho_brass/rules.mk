469854d7210a0775236d1b23de191f222e24b8a1
BifbofII <christoph@jabs-family.de>
2021-09-13 08:40:19 +0300

    Clean up rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/ortho_brass/rules.mk
+++ keyboards/handwired/ortho_brass/rules.mk
@@ -4,24 +4,19 @@ MCU = atmega32u4
 # Bootloader
 BOOTLOADER = qmk-dfu
 
-# Interrupt driven control endpoint task(+60)
-OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
-
 # Build Options
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # N-key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
-API_SYSEX_ENABLE = no
+RGBLIGHT_ENABLE = no        # Enable RGB underlight.
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 LAYOUTS = ortho_4x12
-LAYOUTS_HAS_RGB = no
+
+RGBLIGHT_SUPPORTED = no
