09a3f447c4ffd8314278e54cc1ed949da8ea2c9b
Elliot Powell <32494740+e11i0t23@users.noreply.github.com>
2021-09-13 23:26:24 +0100

    Update keyboards/keebsforall/coarse60/coarse60.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/keebsforall/coarse60/coarse60.c
+++ keyboards/keebsforall/coarse60/coarse60.c
@@ -15,22 +15,3 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "coarse60.h"
-
-void keyboard_pre_init_kb(void) {
-    setPinOutput(INDICATOR_PIN_0);
-    setPinOutput(INDICATOR_PIN_1);
-    setPinOutput(INDICATOR_PIN_2);
-
-    keyboard_pre_init_user();
-}
-
-
-bool led_update_kb(led_t led_state) {
-    bool runDefault = led_update_user(led_state);
-    if (runDefault) {
-      writePin(INDICATOR_PIN_0, !led_state.num_lock);
-      writePin(INDICATOR_PIN_1, !led_state.caps_lock);
-      writePin(INDICATOR_PIN_2, !led_state.scroll_lock);
-    }
-    return runDefault;
-}
