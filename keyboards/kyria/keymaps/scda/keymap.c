9a07ddec00f4eeaf4a063f6838a01753004a4193
scda <dsop@mailbox.org>
2021-09-13 20:53:58 +0200

    reorder media and nav keys add one hand media mod key

--- keyboards/kyria/keymaps/scda/keymap.c
+++ keyboards/kyria/keymaps/scda/keymap.c
@@ -6,10 +6,10 @@ enum layers { _LETTERS = 0, _SYMBOLS, _NUMBERS, _CODE, _NAV, _FN, _MEDIA, _KBD_C
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LETTERS] = LAYOUT(
-        C(KC_LEFT), KC_X,        DE_DOT,      KC_O,          DE_COMM,     DE_Y,                                                                            KC_V,          KC_G,        KC_C,        KC_L,        KC_J,        C(KC_RGHT),
-        KC_ESC,     CTL_T(KC_H), ALT_T(KC_A), GUI_T(KC_E),   SFT_T(KC_I), KC_U,                                                                            KC_D,          SFT_T(KC_T), GUI_T(KC_R), ALT_T(KC_N), CTL_T(KC_S), KC_F,
-        C(KC_UP),   KC_K,        KC_Q,        DE_ADIA,       DE_UDIA,     DE_ODIA,      KC_NO,        KC_NO,                  KC_NO,         KC_NO,        KC_B,          KC_P,        KC_W,        KC_M,        DE_Z,        DE_SS,
-                                              LT(7,KC_NO),   G(DE_Z),     LT(3,KC_TAB), LT(2,KC_SPC), LT(4,KC_ENT),           LT(6,KC_BSPC), LT(1,KC_DEL), LT(5,KC_HOME), KC_END,      KC_NO),
+        C(KC_LEFT),   KC_X,        DE_DOT,      KC_O,          DE_COMM,     DE_Y,                                                                            KC_V,          KC_G,        KC_C,        KC_L,        KC_J,        C(KC_RGHT),
+        LT(6,KC_ESC), CTL_T(KC_H), ALT_T(KC_A), GUI_T(KC_E),   SFT_T(KC_I), KC_U,                                                                            KC_D,          SFT_T(KC_T), GUI_T(KC_R), ALT_T(KC_N), CTL_T(KC_S), KC_F,
+        C(KC_UP),     KC_K,        KC_Q,        DE_ADIA,       DE_UDIA,     DE_ODIA,      KC_NO,        KC_NO,                  KC_NO,         KC_NO,        KC_B,          KC_P,        KC_W,        KC_M,        DE_Z,        DE_SS,
+                                                LT(7,KC_WAKE), G(DE_Z),     LT(3,KC_TAB), LT(2,KC_SPC), LT(4,KC_ENT),           LT(6,KC_BSPC), LT(1,KC_DEL), LT(5,KC_HOME), KC_END,      KC_NO),
     [_SYMBOLS] = LAYOUT(
         KC_NO, DE_AT,   DE_PERC, DE_LCBR, DE_RCBR, DE_PIPE,                                                  DE_EXLM, KC_GRV,  S(KC_GRV), DE_EQL,  DE_AMPR, KC_NO,
         KC_NO, KC_NUBS, DE_TILD, DE_LPRN, DE_RPRN, DE_ASTR,                                                  DE_QUES, DE_SLSH, DE_COLN,   DE_MINS, DE_UNDS, KC_NO,
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT(
         KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                                    G(A(KC_RGHT)), KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,
         KC_NO, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_NO,                                                    G(A(KC_UP)),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGUP,
-        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO,   KC_NO,   G(A(KC_LEFT)), KC_BTN1, KC_BTN2, KC_WH_D, KC_WH_U, KC_PGDN,
+        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                KC_NO,   KC_NO,   G(A(KC_LEFT)), KC_BTN1, KC_WH_D, KC_WH_U, KC_BTN2, KC_PGDN,
                                  KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS,       KC_NO,   KC_NO),
     [_FN] = LAYOUT(
         KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,                                                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
@@ -36,10 +36,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                               KC_NO,  KC_NO,  KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO),
     [_MEDIA] = LAYOUT(
-        KC_NO, KC_NO,   KC_VOLU, KC_NO,   KC_NO,   KC_NO,                                                    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO, KC_BRIU, KC_MUTE, KC_MPLY, KC_MNXT, G(C(KC_SPACE)),                                           KC_NO,   KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, KC_NO,
-        KC_NO, KC_BRID, KC_VOLD, KC_NO,   KC_MPRV, KC_NO,          KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                 KC_NO,   KC_NO,   KC_TRNS,        KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO),
+        KC_NO, KC_NO, KC_BRIU, KC_VOLU, KC_MNXT, KC_NO,                                                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_NO, KC_NO,   KC_MUTE, KC_MPLY, G(C(KC_SPACE)),                                             KC_NO,   KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, KC_NO,
+        KC_NO, KC_NO, KC_BRID, KC_VOLD, KC_MPRV, KC_NO,          KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                               KC_NO,   KC_NO,   KC_TRNS,        KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO),
     [_KBD_CTRL] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                            KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,                                                          KC_NO,   RGB_VAI, KC_NO, KC_NO, KC_NO, KC_NO,
@@ -108,7 +108,7 @@ void oled_task_user(void) {
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();  // enables Rgb, without saving settings
     rgblight_mode_noeeprom(RGBLIGHT_DEFAULT_MODE);
-    rgblight_sethsv_noeeprom(HSV_CHARTREUSE);
+    rgblight_sethsv_noeeprom(HSV_OFF);
 }
 
 void sethsv_master(uint8_t hue, uint8_t sat, uint8_t val) { rgblight_sethsv_range(hue, sat, val, 0, (uint8_t)RGBLED_NUM / 2); }
@@ -119,7 +119,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     // setrgb, sethsv_noeeprom, setrgb_range do not write to eeprom
     switch (get_highest_layer(state)) {
         case _LETTERS:
-            rgblight_sethsv_noeeprom(HSV_CHARTREUSE);
+            rgblight_sethsv_noeeprom(HSV_OFF);
             break;
         case _SYMBOLS:
             rgblight_sethsv_noeeprom(HSV_ORANGE);
@@ -129,7 +129,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             sethsv_master(HSV_OFF);
             break;
         case _CODE:
-            rgblight_sethsv_noeeprom(HSV_PINK);
+            rgblight_sethsv_noeeprom(HSV_CHARTREUSE);
             sethsv_master(HSV_OFF);
             break;
         case _NAV:
@@ -138,14 +138,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             break;
         case _FN:
             rgblight_sethsv_noeeprom(HSV_OFF);
-            sethsv_master(HSV_PURPLE);
+            sethsv_master(HSV_MAGENTA);
             break;
         case _MEDIA:
             rgblight_sethsv_noeeprom(HSV_OFF);
-            sethsv_master(HSV_TEAL);
+            sethsv_master(HSV_WHITE);
             break;
         case _KBD_CTRL:
-            rgblight_sethsv_noeeprom_white();
+            rgblight_sethsv_noeeprom_red();
             break;
         default:  //  for any other layers (that should not exist/be accessible)
             rgblight_setrgb(HSV_RED);
