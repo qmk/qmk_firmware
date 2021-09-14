a64df492c84026cfb9e641d8326d38d23739baa4
lmlask <luimlask@gmail.com>
2021-09-13 21:16:51 -0300

    added midi layer, polished adjust layer

--- keyboards/kprepublic/bm40hsrgb/keymaps/wolff_abnt2/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/wolff_abnt2/keymap.c
@@ -26,7 +26,8 @@ enum layers {
   _FUNCTION,
   _NAV,
   _NUM,
-  _ADJUST
+  _ADJUST,
+  _MIDI
 };
 
 enum planck_keycodes {
@@ -47,29 +48,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //qwerty base layer ok
 [_QWERTY] = LAYOUT_planck_mit(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    BR_TILD,
-    MYTAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_C,    BR_ACUT,
+    MYTAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    BR_CCED, BR_ACUT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  BR_SLSH, KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI, SYM,     MYNAV,       KC_SPC,       MYNUM,   FUN,     KC_RGUI, KC_LALT, RCTL_T(KC_ENT)
 ),
 
 //workman base layer ok
 [_WORKMAN] = LAYOUT_planck_mit(
-    KC_ESC,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    BR_SCLN, BR_TILD,
+    KC_ESC,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    BR_CCED, BR_TILD,
     MYTAB,   KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    BR_ACUT,
     KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM, KC_DOT,  BR_SLSH, KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI, SYM,     MYNAV,       KC_SPC,       MYNUM,   FUN,     KC_RGUI, KC_LALT, RCTL_T(KC_ENT)
+),
 
 //dvorak base layer ok
 [_DVORAK] = LAYOUT_planck_mit(
-    KC_ESC,  BR_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_C,    KC_R,    KC_L,    BR_SLSH, BR_TILD,
+    KC_ESC,  KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    BR_SLSH, BR_TILD,
     MYTAB,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    BR_ACUT,
-    KC_LSFT, BR_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
+    KC_LSFT, BR_CCED, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI, SYM,     MYNAV,       KC_SPC,       MYNUM,   FUN,     KC_RGUI, KC_LALT, RCTL_T(KC_ENT)
 ),
 
 //colemak base layer ok
 [_COLEMAK] = LAYOUT_planck_mit(
-    KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    BR_SCLN, BR_TILD,
+    KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    BR_CCED, BR_TILD,
     MYTAB,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    BR_ACUT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  BR_SLSH, KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI, SYM,     MYNAV,       KC_SPC,       MYNUM,   FUN,     KC_RGUI, KC_LALT, RCTL_T(KC_ENT)
@@ -80,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_VOLU,
     _______, _______, _______, _______, BR_QUOT, _______, KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_VOLD,
     KC_TRNS, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, KC_BSPC, KC_DEL,  _______, _______, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, _______, _______,     _______,      LT(_NUM, KC_BSPC), _______, KC_MPRV, KC_MPLY, KC_MNXT
+    KC_TRNS, KC_TRNS, KC_TRNS, _______, _______,     _______,      MYNUM, _______, KC_MPRV, KC_MPLY, KC_MNXT
 ),
 
 //symbols layer ok
@@ -96,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    BR_MINS,
     _______, BR_ASTR, BR_SLSH, BR_MINS, BR_PLUS, _______, _______, KC_4,    KC_5,    KC_6,    BR_COMM, BR_PLUS,
     KC_TRNS, BR_PERC, BR_EQL,  BR_DOT,  BR_COMM, _______, _______, KC_1,    KC_2,    KC_3,    BR_DOT,  KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, _______, LT(_NAV, KC_DEL),     _______,      _______, KC_0,    KC_0,    KC_COMM, KC_ENT
+    KC_TRNS, KC_TRNS, KC_TRNS, _______, MYNAV,       _______,      _______, KC_0,    KC_0,    KC_COMM, KC_ENT
 ),
 
 //FN layer 
@@ -109,9 +111,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // adjust layer ok
 [_ADJUST] = LAYOUT_planck_mit(
-    RESET,   _______, _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______,
-    KC_CAPS, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  DVORAK,  COLEMAK, WORKMAN, _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  RGB_M_R, _______, _______, _______, _______,
+    RESET,   RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______,
+    KC_CAPS, RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, QWERTY,  DVORAK,  COLEMAK, WORKMAN, _______,
+    _______, MI_ON,   MI_OFF,  MI_TOG,  MU_ON,   MU_OFF,  MU_TOG,  MU_MOD,  AU_ON,   AU_OFF,  _______, _______,
+    RGB_TOG, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+),
+
+// midi layer
+[_MIDI] = LAYOUT_planck_mit(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
 )
 };
