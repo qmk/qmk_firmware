85d0e9fcac7cf29bd4b804796f0f98240cbc0cbb
qpockets <herrstrange42@gmail.com>
2021-09-13 20:02:03 -0400

    add synapse pcb to QMK

---
+++ keyboards/synapse/config.h
@@ -0,0 +1,59 @@
+/* Copyright 2021 qpockets
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0x7170
+#define PRODUCT_ID 0x5359
+#define DEVICE_VER 0x0001
+#define MANUFACTURER drmmr
+#define PRODUCT synapse
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+#define MATRIX_ROW_PINS \
+    { E6, B0, F4, F1 }
+#define MATRIX_COL_PINS \
+    { F0, D4, F5, B1, B2, B3, B7, D0, D1, D2, D3, B6 }
+
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION COL2ROW
+
+/* ROTARY ENCODERS */
+#define ENCODERS_PAD_A \
+    { D6, C6 }
+#define ENCODERS_PAD_B \
+    { D7, C7 }
+
+#define ENCODER_RESOLUTION 4
+#define ENCODER_DIRECTION_FLIP
+
+#define DEBOUNCE 5
+
+#define LOCKING_SUPPORT_ENABLE
+
+//#define RGB_DI_PIN D5
+//#define RGBLED_NUM 4
+
+//#define DRIVER_LED_TOTAL RGBLED_NUM
+//#define RGBLIGHT_DEFAULT_HUE 130
---
+++ keyboards/synapse/readme.md
@@ -0,0 +1,18 @@
+# synapse
+
+![synapse](https://i.imgur.com/OQNNPBh.jpg)
+
+Stagger PCB for Cervello!
+
+* Keyboard Maintainer: https://github.com/qpockets
+* Hardware Availability: [P3D Store](https://p3dstore.com/products/cervello-acrylic-keyboard-case-and-pcb-group-buy?_pos=4&_sid=fd078da46&_ss=r&variant=40398412677304) Group Buy
+
+Make example for this keyboard (after setting up your build environment):
+
+    make synapse:default
+
+Flashing example for this keyboard:
+
+    make synapse:default:flash
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
