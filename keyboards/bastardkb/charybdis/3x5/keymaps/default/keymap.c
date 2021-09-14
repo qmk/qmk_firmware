7cf29a80e093280a980f02808065956634ebff1e
Quentin <qlebastard@gmail.com>
2021-09-14 20:38:25 +0200

    tab to space

--- keyboards/bastardkb/charybdis/3x5/keymaps/default/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/default/keymap.c
@@ -20,7 +20,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0]    = LAYOUT_split_3x5_3(
       KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,                   KC_Y,             KC_U,               KC_I,         KC_O,         KC_P,  
-	    GUI_T(KC_A),      ALT_T(KC_S),      SFT_T(KC_D),      CTL_T(KC_F),      KC_G,                   KC_H,             CTL_T(KC_J),        SFT_T(KC_K),  ALT_T(KC_L),  GUI_T(KC_SCLN), 
+      GUI_T(KC_A),      ALT_T(KC_S),      SFT_T(KC_D),      CTL_T(KC_F),      KC_G,                   KC_H,             CTL_T(KC_J),        SFT_T(KC_K),  ALT_T(KC_L),  GUI_T(KC_SCLN), 
       LT(MOUSE, KC_Z),  KC_X,             KC_C,             KC_V,             KC_B,                   KC_N,             KC_M,               KC_COMM,      KC_DOT,       LT(MOUSE, KC_SLSH),  
                                           LT(NAV, KC_BSPC), LT(MBO, KC_ENT),  LT(MEDIA, KC_TAB),      LT(NUM, KC_ESC),  LT(SYM, KC_SPC),    U_NP
 
