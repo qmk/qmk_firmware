bee266a9f8803c737e8c746beca11fe506b0943b
Quentin <qlebastard@gmail.com>
2021-09-14 20:12:12 +0200

    create stock and default keymaps

--- keyboards/bastardkb/charybdis/3x5/keymaps/default/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/default/keymap.c
@@ -16,24 +16,13 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "wrappers.h"
-
-#define LAYOUT_split_3x5_3_wrapper(...)      LAYOUT_split_3x5_3(__VA_ARGS__)
-
-#define _BASE_QWERTY \
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  \
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, \
-    LT(MOUSE, KC_Z),  KC_X,    KC_C,    KC_V,  KC_B,                  KC_N,    KC_M,    KC_COMM, KC_DOT, LT(MOUSE, KC_SLSH),  \
-        LT(NAV, KC_BSPC), LT(MBO, KC_ENT), LT(MEDIA, KC_TAB),         LT(NUM, KC_ESC),   LT(SYM, KC_SPC),   U_NP
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE]    = LAYOUT_split_3x5_3_wrapper(HRM_GASC_wrapper(_BASE_QWERTY)),
-  [MBO]     = LAYOUT_split_3x5_3_wrapper(_MBO),
-  [MEDIA]   = LAYOUT_split_3x5_3_wrapper(_MEDIA),
-  [NAV]     = LAYOUT_split_3x5_3_wrapper(_NAV),
-  [MOUSE]   = LAYOUT_split_3x5_3_wrapper(_MOUSE),
-  [SYM]     = LAYOUT_split_3x5_3_wrapper(_SYM),
-  [NUM]     = LAYOUT_split_3x5_3_wrapper(_NUM),
-  [FUN]     = LAYOUT_split_3x5_3_wrapper(_FUN)
+  [0]    = LAYOUT_split_3x5_3(
+      KC_Q,             KC_W,             KC_E,             KC_R,             KC_T,                   KC_Y,             KC_U,               KC_I,         KC_O,         KC_P,  
+	    GUI_T(KC_A),      ALT_T(KC_S),      SFT_T(KC_D),      CTL_T(KC_F),      KC_G,                   KC_H,             CTL_T(KC_J),        SFT_T(KC_K),  ALT_T(KC_L),  GUI_T(KC_SCLN), 
+      LT(MOUSE, KC_Z),  KC_X,             KC_C,             KC_V,             KC_B,                   KC_N,             KC_M,               KC_COMM,      KC_DOT,       LT(MOUSE, KC_SLSH),  
+                                          LT(NAV, KC_BSPC), LT(MBO, KC_ENT),  LT(MEDIA, KC_TAB),      LT(NUM, KC_ESC),  LT(SYM, KC_SPC),    U_NP
+
+  )
 };
