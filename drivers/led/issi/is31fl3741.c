d3c0942d21119ae9ff6e7bdb7feca869d334c6bf
Drashna Jael're <drashna@live.com>
2021-09-15 09:07:07 -0700

    Fix formatting

--- drivers/led/issi/is31fl3741.c
+++ drivers/led/issi/is31fl3741.c
@@ -169,9 +169,9 @@ void IS31FL3741_set_color(int index, uint8_t red, uint8_t green, uint8_t blue) {
     if (index >= 0 && index < DRIVER_LED_TOTAL) {
         is31_led led = g_is31_leds[index];
 
-        g_pwm_buffer[led.driver][led.r] = red;
-        g_pwm_buffer[led.driver][led.g] = green;
-        g_pwm_buffer[led.driver][led.b] = blue;
+        g_pwm_buffer[led.driver][led.r]          = red;
+        g_pwm_buffer[led.driver][led.g]          = green;
+        g_pwm_buffer[led.driver][led.b]          = blue;
         g_pwm_buffer_update_required[led.driver] = true;
     }
 }
