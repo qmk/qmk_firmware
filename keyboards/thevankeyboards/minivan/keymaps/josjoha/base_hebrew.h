7c8fbf31a028ee24e9b45068c5353ee2fe6d13cd
Jos Boersema <joshb@xs4all.nl>
2021-09-15 13:35:17 +0200

    © 《author》 statement comment was wrong year
    Now set to 2021.

--- keyboards/thevankeyboards/minivan/keymaps/josjoha/base_hebrew.h
+++ keyboards/thevankeyboards/minivan/keymaps/josjoha/base_hebrew.h
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- * Author: © 2020 by Jos Boersema
+ * Author: © 2021 by Jos Boersema
  *
  */
 
