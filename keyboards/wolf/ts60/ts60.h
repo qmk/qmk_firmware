2a161b23dce4e0c5fd328c357ee130df0ae3cdcb
Wolf Van Herreweghe <wolfvh@getupgamesofficial.com>
2021-09-16 10:34:01 +0200

    Update Keymap

--- keyboards/wolf/ts60/ts60.h
+++ keyboards/wolf/ts60/ts60.h
@@ -68,12 +68,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
      K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3a, K3b,      K3d,      \
      K40, K41, K42,                K46,                K4a, K4b,      K4d, K4e  \
  ) { \
-    { K00,   K02,   K04,   K06,   K08,   K0a, K0c,   KC_NO }, \
-    { K01,   K03,   K05,   K07,   K09,   K0b, KC_NO, K0e   }, \
+    { K00,   K02,   K04,   K06,   K08,   K0a, K0c,   K0e   }, \
+    { K01,   K03,   K05,   K07,   K09,   K0b, KC_NO, KC_NO }, \
     { K10,   K12,   K14,   K16,   K18,   K1a, K1c,   K1d   }, \
     { KC_NO, K13,   K15,   K17,   K19,   K1b, KC_NO, K1e   }, \
-    { K20,   K22,   K24,   K26,   K28,   K2a, K2c,   K2d   }, \
-    { KC_NO, K23,   K25,   K27,   K29,   K2b, KC_NO, KC_NO }, \
+    { K20,   K22,   K24,   K26,   K28,   K2a, K2c,   KC_NO }, \
+    { KC_NO, K23,   K25,   K27,   K29,   K2b, KC_NO, K2d   }, \
     { K30,   K32,   K34,   K36,   K38,   K3a, KC_NO, K3d   }, \
     { KC_NO, K33,   K35,   K37,   K39,   K3b, KC_NO, KC_NO }, \
     { K40,   K42,   KC_NO, K46,   KC_NO, K4a, KC_NO, K4d   }, \
