2e60da2eb55903fe0229ae80c25d8826a463931a
James Smith <bronzegears@gmail.com>
2021-09-16 01:25:00 -0400

    Update rules.mk

--- keyboards/ein_60/keymaps/ledtest/rules.mk
+++ keyboards/ein_60/keymaps/ledtest/rules.mk
@@ -2,4 +2,3 @@
 #   change yes to no to disable
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no     # Enable for pretty RGB matrix effects
-#RGB_MATRIX_DRIVER = WS2812  # Define RGB Matrix
