7d77033a668d586fbf5d63b08bdbfc21ddc8836a
Klackygears <37446827+Klackygears@users.noreply.github.com>
2021-09-16 14:23:08 -0400

    adding in klackygears keymap for example purposes

---
+++ keyboards/ein_60/keymaps/klackygears/rules.mk
@@ -0,0 +1,9 @@
+
+# Build Options
+#   change yes to no to disable
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+WPM_ENABLE = yes
+RGB_MATRIX_ENABLE = yes       # Enable for pretty RGB matrix effects
+AUDIO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
