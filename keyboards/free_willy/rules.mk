68cfb3b9dd3ae587c5b45605e3bd513af626f80c
Caleb Lightfoot <caleblightfoot05@gmail.com>
2021-09-16 20:50:48 -0500

    added mcu, bootloader, and settings config to rules.mk

--- keyboards/free_willy/rules.mk
+++ keyboards/free_willy/rules.mk
@@ -1,1 +1,15 @@
-
+MCU = atmega32u4
+# Bootloader selection
+BOOTLOADER = caterina
+# Build Options
+#   change yes to no to disable
+MOUSEKEY_ENABLE = no      # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no       # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no            # USB Nkey Rollover
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+UNICODE_ENABLE = no         # Unicode
+ENCODER_ENABLE = no
