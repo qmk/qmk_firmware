ada9deb9d90538c3963be36c4a48a165cc8e6d87
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-17 14:10:28 -0700

    rework LAYOUT macro
    * use QMK 3-character notation in thedora.h
    * position arguments to resemble layout of assembled keyboard
    * correct layout macro reference in info.json

--- keyboards/ckeys/thedora/thedora.h
+++ keyboards/ckeys/thedora/thedora.h
@@ -2,20 +2,20 @@
 
 #include "quantum.h"
 
+#define XXX KC_NO
+
 // This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
-// The first section contains all of the arguements
+// The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
 #define LAYOUT( \
-    k00, k01, k02, k03, k04, k05, \
-    k06, k07, k08, k09, k10, \
-    k11, k12, k13, k14, k15, \
-    k16, k17, k18, k19, k20 \
+         k00, k01, k02, k03, k04, \
+         k10, k11, k12, k13, k14, \
+         k20, k21, k22, k23, k24, \
+    k05, k30, k31, k32, k33, k34  \
 ) \
 { \
-    { k00, k01, k02, k03, k04, k05, }, \
-    { k06, k07, k08, k09, k10 },       \
-    { k11, k12, k13, k14, k15 },       \
-    { k16, k17, k18, k19, k20 }        \
+    { k00, k01, k02, k03, k04, k05 }, \
+    { k10, k11, k12, k13, k14, XXX }, \
+    { k20, k21, k22, k23, k24, XXX }, \
+    { k30, k31, k32, k33, k34, XXX }  \
 }
-
