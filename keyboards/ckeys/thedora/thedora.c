d081532818ba69b522d8eb039c90de8b20e0b70e
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-18 04:39:25 -0700

    add encoder_update_kb() function to thedora.c
    Enabled firmware compiled through QMK Configurator to have rotary encoder functionality.

--- keyboards/ckeys/thedora/thedora.c
+++ keyboards/ckeys/thedora/thedora.c
@@ -1,1 +1,15 @@
 #include "thedora.h"
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+    return true;
+}
