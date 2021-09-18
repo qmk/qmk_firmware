f26e77b67299150b2dae304aaf7da155ff0f9b12
im a can what do you think lmao? <33337640+lyso1@users.noreply.github.com>
2021-09-17 23:16:31 -0400

    Update config.h
    removed backlight and rgb code. board does not support it.

--- keyboards/lefishe/config.h
+++ keyboards/lefishe/config.h
@@ -37,12 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 3
-#endif
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
@@ -51,14 +45,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
-
-
-
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
 
