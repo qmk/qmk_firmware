eeae5dcfd722a4780d39b8004e18d40734936f21
PiKeeb <pikeeb@gmail.com>
2021-09-18 14:00:56 +0300

    debug enable

--- keyboards/supernova/keymaps/test/keymap.c
+++ keyboards/supernova/keymaps/test/keymap.c
@@ -29,17 +29,17 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = LAYOUT_TEST(
-      KC_ESC,         KC_F1, KC_F2, KC_F3, KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,      KC_F9,   KC_F10, KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, KC_PAUS,
+    [_BASE] = LAYOUT_test(
+      RGB_TOG,         RGB_MOD, KC_F2, KC_F3, KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,      KC_F9,   KC_F10, KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, KC_PAUS,
       KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS, KC_EQL, KC_BSPC, MO(_FN),    KC_INS,  KC_HOME, KC_PGUP,
        KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,   KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,       KC_DEL,  KC_END,  KC_PGDN,
          KC_CAPS, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,
   KC_LSFT, QMKURL,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,  QMKBEST,                        KC_UP,
       KC_LCTRL, KC_LGUI, KC_LALT, KC_LGUI,   KC_SPC,      KC_SPC,     KC_RGUI,    KC_RALT,   KC_RGUI,  KC_RCTRL,               KC_LEFT, KC_DOWN, KC_RIGHT
     ),
-    [_FN] = LAYOUT_TEST(
+    [_FN] = LAYOUT_test(
       RESET,          KC_F1, KC_F2, KC_F3, KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,      KC_F9,   KC_F10, KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, KC_PAUS,
-      KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS, KC_EQL, KC_BSPC, XXXXXXX,     KC_INS,  KC_HOME, KC_PGUP,
+      KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS, KC_EQL, KC_BSPC, ______,     KC_INS,  KC_HOME, KC_PGUP,
        KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,   KC_P,    KC_LBRC, KC_RBRC,    KC_BSLS,       KC_DEL,  KC_END,  KC_PGDN,
          KC_CAPS, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,
   KC_LSFT, QMKURL,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,  QMKBEST,                        KC_UP,
@@ -66,8 +66,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
     };
-        #ifdef CONSOLE_ENABLE
-            uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
-        #endif
     return true;
 };
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
