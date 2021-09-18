42728645ceda89cd7e01ad4a3b48a480c787d1d5
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2021-09-18 17:41:29 -0400

    Fixed and refreshed all licence headers

--- users/bbaserdem/bb-oled-extra.h
+++ users/bbaserdem/bb-oled-extra.h
@@ -1,18 +1,22 @@
-/*
-Copyright 2019 Batuhan Başerdem <baserdem.batuhan@gmail.com> @bbaserdem
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright 2021 Batuhan Başerdem
+ * <baserdem.batuhan@gmail.com> @bbaserdem
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #pragma once
 #include "bbaserdem.h"
+
 // OLED layout images writer function
 void render_modifiers(uint8_t row, uint8_t col, uint8_t mods);
 void render_layout(uint8_t row, uint8_t col, uint8_t mods, bool isLeft);
