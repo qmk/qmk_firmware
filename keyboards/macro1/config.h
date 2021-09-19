18960a2ed5cad3b2c615c952e7cd21c4f010ccae
Laneware <support@laneware.xyz>
2021-09-19 11:04:50 +1000

    changes made per request

--- keyboards/macro1/config.h
+++ keyboards/macro1/config.h
@@ -1,3 +1,19 @@
+/* Copyright 2021 Laneware Peripherals 
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
+
 #pragma once
 
 #include "config_common.h"
@@ -8,7 +24,6 @@
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Laneware_Peripherals
 #define PRODUCT         Laneware_MACRO_1
-#define DESCRIPTION     Laneware_MACRO_1
 
 /* key matrix size */
 #define MATRIX_ROWS 6
@@ -36,11 +51,5 @@
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
 
-/* prevent stuck modifiers */
-#define PREVENT_STUCK_MODIFIERS
 
