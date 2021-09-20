574b6734afb6ec7073f6fb816dd454f03b7eb26f
Ryan <fauxpark@gmail.com>
2021-09-20 16:51:00 +1000

    Remove backlight pin references in rules.mk (#14513)

--- keyboards/handwired/trackpoint/rules.mk
+++ keyboards/handwired/trackpoint/rules.mk
@@ -12,7 +12,7 @@ CONSOLE_ENABLE = yes        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 UNICODE_ENABLE = no         # Unicode
 AUDIO_ENABLE = no           # Audio output on port C6
 
