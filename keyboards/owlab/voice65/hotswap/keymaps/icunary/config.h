2862c9b08d0c8d4e55d6102bb9cf551176c8ebd2
icunary <91033437+icunary@users.noreply.github.com>
2021-09-20 02:42:16 -0400

    Added statement for tap code delay Voice65
    Statement to disable 1ms delay (instead of 10ms) delay for tap codes in config file of custom firmware

--- keyboards/owlab/voice65/hotswap/keymaps/icunary/config.h
+++ keyboards/owlab/voice65/hotswap/keymaps/icunary/config.h
@@ -5,3 +5,5 @@
 #define DEBOUNCE 3
 #define QMK_KEYS_PER_SCAN 12
 #define RGBLIGHT_SLEEP
+//#undef TAP_CODE_DELAY
+//#define TAP_CODE_DELAY 1 // to make tap codes (vol and LT) 1ms. disabled for now//
