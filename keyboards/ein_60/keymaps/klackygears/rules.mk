6748d240cc9b86f57df578fd08f7863d211550ca
Klackygears <37446827+Klackygears@users.noreply.github.com>
2021-09-20 01:35:30 -0400

    rolling back oled changes bc issues

--- keyboards/ein_60/keymaps/klackygears/rules.mk
+++ keyboards/ein_60/keymaps/klackygears/rules.mk
@@ -1,9 +1,10 @@
 
 # Build Options
 #   change yes to no to disable
-ENCODER_ENABLE = yes       # ENables the use of one or more encoders
-WPM_ENABLE = yes
-RGB_MATRIX_ENABLE = yes       # Enable for pretty RGB matrix effects
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+WPM_ENABLE = yes           # Enables the WPM counter on the OLED
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes    # Enable for pretty RGB matrix effects
 AUDIO_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
