4ca90e55abe462a8780b4de7d38ef3aac347847b
ChrisGVE <christian.berclaz@mac.com>
2021-09-21 19:54:06 +0800

    Added the caps/ctrl trick to the dz60 tsangan

--- keyboards/dz60/keymaps/chrisgve_tsangan/keymap.c
+++ keyboards/dz60/keymaps/chrisgve_tsangan/keymap.c
@@ -1,22 +1,23 @@
 #include QMK_KEYBOARD_H
 
 enum keyboard_layers {
-  _BL = 0, // Base Layer
-  _FN,     // Function Layer
-  _DIR,    // Direction layer
-  _FULL_DIR,
+  _BL = 0,    // Base Layer
+  _FN,        // Function Layer
+  _DIR,       // Direction layer
+  _FULL_DIR,  // Full direction layer
 };
 
 #define FN      MO(_FN)
 #define NAV     LT(_DIR, KC_D)
 #define F_NAV   MO(_FULL_DIR)
+#define CPS_CTL CTL_T(KC_CAPS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_BL] = LAYOUT_60_tsangan(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_CAPS, KC_A,    KC_S,    NAV,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+		CPS_CTL, KC_A,    KC_S,    NAV,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, MO(_FN),
 		KC_LCTL, KC_LALT,          KC_LGUI,                   KC_SPC,                                      KC_RGUI,          FN,      KC_RCTL
 	),
