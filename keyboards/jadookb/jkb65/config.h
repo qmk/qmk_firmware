efecb347da28d18ea612074d94da3c25e983c439
Drashna Jaelre <drashna@live.com>
2021-09-21 22:51:54 -0700

    [Keyboard] Fix jkb65 compile issues (#14554)

--- keyboards/jadookb/jkb65/config.h
+++ keyboards/jadookb/jkb65/config.h
@@ -1,24 +1,24 @@
- /* Copyright 2021 Wizad-GG 
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+ /* Copyright 2021 Wizad-GG
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 
 #pragma once
 
 #include "config_common.h"
 
-#define VENDOR_ID    0x4A4B 
+#define VENDOR_ID    0x4A4B
 #define PRODUCT_ID   0xEF6A
 #define DEVICE_VER   0x0001
 #define MANUFACTURER JadooKB
@@ -37,18 +37,11 @@
 #define RGB_DI_PIN F0
 
 #define DRIVER_LED_TOTAL 67
-#ifdef RGB_DI_PIN
-    #define RGBLED_NUM 67
 
-    #define RGB_DISABLE_WHEN_USB_SUSPENDED
-    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-    #define RGB_MATRIX_KEYPRESSES
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_SLEEP
-    #define RGB_DISABLE_TIMEOUT 0 
-    #define RGB_DISABLE_AFTER_TIMEOUT 0
-    #define RGBLIGHT_TIMEOUT 60000
-#endif
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_DISABLE_TIMEOUT 60000
 
 
 #define DEBOUNCE 5
@@ -57,4 +50,3 @@
 
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
-
