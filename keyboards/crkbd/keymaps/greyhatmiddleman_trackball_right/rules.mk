a9459731adc48cfcca30dfb178ad93659f611119
aka whitehatmiddleman <whitehatmiddleman@gmail.com>
2021-09-22 20:36:53 +0800

    moved options from rules.mk to config.h

--- keyboards/crkbd/keymaps/greyhatmiddleman_trackball_right/rules.mk
+++ keyboards/crkbd/keymaps/greyhatmiddleman_trackball_right/rules.mk
@@ -2,11 +2,6 @@ PIMORONI_TRACKBALL_ENABLE = yes
 ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
     POINTING_DEVICE_ENABLE = yes
     SRC += drivers/sensors/pimoroni_trackball.c
-    OPT_DEFS += -DPIMORONI_TRACKBALL_ENABLE
-    OPT_DEFS += -DPIMORONI_TRACKBALL_CLICK
-#orientation pimoroni text on top and trackball text at the bottom
-    OPT_DEFS += -DPIMORONI_TRACKBALL_ROTATE
-    OPT_DEFS += -DPIMORONI_TRACKBALL_INVERT_X
     QUANTUM_LIB_SRC += i2c_master.c
 endif
 
