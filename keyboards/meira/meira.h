e1cea4e6f1436426f180fc7cbcc8ad8681226612
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-22 11:43:40 -0700

    update layout macro aliases
    * move layout macro aliases to info.json
      * alias KEYMAP to LAYOUT_ortho_4x12 so the user keymaps don't have to be edited

--- keyboards/meira/meira.h
+++ keyboards/meira/meira.h
@@ -36,5 +36,3 @@ void reset_keyboard_kb(void);
     { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b }, \
     { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b }  \
 }
-
-#define LAYOUT LAYOUT_ortho_4x12
