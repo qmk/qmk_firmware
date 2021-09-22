ad8b6ed283d80f226da494c57c4588e1d12414d1
aka whitehatmiddleman <whitehatmiddleman@gmail.com>
2021-09-22 20:34:30 +0800

    moved options from rules.mk to config.h

--- keyboards/crkbd/keymaps/greyhatmiddleman_trackball_left/rules.mk
+++ keyboards/crkbd/keymaps/greyhatmiddleman_trackball_left/rules.mk
@@ -2,17 +2,10 @@ PIMORONI_TRACKBALL_ENABLE = no
 ifeq ($(strip $(PIMORONI_TRACKBALL_ENABLE)), yes)
     POINTING_DEVICE_ENABLE = yes
     SRC += drivers/sensors/pimoroni_trackball.c
-    OPT_DEFS += -DPIMORONI_TRACKBALL_ENABLE
-    OPT_DEFS += -DPIMORONI_TRACKBALL_CLICK
-#orientation pimoroni text on top and trackball text at the bottom
-    OPT_DEFS += -DPIMORONI_TRACKBALL_ROTATE
-    OPT_DEFS += -DPIMORONI_TRACKBALL_INVERT_X
     QUANTUM_LIB_SRC += i2c_master.c
 endif
 
 OLED_ENABLE = yes
 ifeq ($(strip $(OLED_ENABLE)), yes)
     OLED_DRIVER = SSD1306
-    OPT_DEFS += -DOLED_ENABLE
-    OPT_DEFS += -DSPLIT_OLED_ENABLE
 endif
