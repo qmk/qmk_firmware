357e473bbd5ada741dbe00747765d70053157525
cest <cest@localhost>
2021-09-22 11:23:02 +0200

    Changed the layout to match experience gathered over time
    Some more options were added in the other layer,
    build options got tweaked a little too

--- keyboards/cest73/tkm/config.h
+++ keyboards/cest73/tkm/config.h
@@ -1,18 +1,18 @@
- /* Copyright 2021 cest73 
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+ /* Copyright 2021 cest73
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 #pragma once
 
 #include "config_common.h"
@@ -41,9 +41,12 @@
 #define LED_NUM_LOCK_PIN F6
 #define LED_CAPS_LOCK_PIN F5
 #define LED_SCROLL_LOCK_PIN F7
-#define LED_PIN_ON_STATE 0
+#define LED_PIN_ON_STATE 1
 //TODO change the E6 pin to either B5, B6, B7 or C6 to utilize hardware PWM on a future PCB revision
 #define BACKLIGHT_PIN E6
+
+// mouse keys acceleration mode
+#define MK_COMBINED
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
