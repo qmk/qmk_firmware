059d9a8d5420665d183c4f85f6e0eaa57ecea7d9
Andrew Braini <abraini@nascentdigital.com>
2021-09-22 07:49:01 -0400

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/battleship_gamepad/config.h
+++ keyboards/handwired/battleship_gamepad/config.h
@@ -43,7 +43,7 @@
 #define DIODE_DIRECTION COL2ROW
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -51,10 +51,3 @@
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
-/* prevent stuck modifiers */
-#define PREVENT_STUCK_MODIFIERS
