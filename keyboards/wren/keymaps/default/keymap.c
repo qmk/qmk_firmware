fd96dbf27258fcfbaab2e17a4b9b66d228646d23
reasonsandreasons <walterphanley@gmail.com>
2021-09-22 14:05:48 -0500

    Update /default/keymap.c
    Make keyboard.c "pristine" to the best of my ability and add information about configuring a "non-standard" layout.

--- keyboards/wren/keymaps/default/keymap.c
+++ keyboards/wren/keymaps/default/keymap.c
@@ -15,50 +15,23 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines keycode alias
-#define ECTL LCTL_T(KC_ESC)
-#define DELSYM LT(_SYMBOL, KC_DEL)
-#define SCLNAV LT(_NAV, KC_SCLN)
-
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE
-    _SYMBOL
-    _NAV
 };
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
-    QMKURL
-};
-
+/* This layout preserves the entire keyboard matrix, but only includes keycodes  
+ * for a numpad/nav cluster configuration. Depending on your configuration, you 
+ * may need to replace some of the existing KC_NO values with actual keycodes.
+ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
      KC_PSLS, KC_7,    KC_8,    KC_9,    KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  KC_INS,  KC_HOME, KC_PGUP, KC_NO,
      KC_PAST, KC_4,    KC_5,    KC_6,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, KC_NO,
-     KC_PMNS, KC_1,    KC_2,    KC_3,    ECTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    SCLNAV,  KC_QUOT, KC_NO,   KC_UP,   KC_NO,   KC_NO,
-     KC_PPLS, KC_0,    KC_DOT,  KC_ENT,  KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,
-                                                           KC_NO,   KC_LALT, KC_LGUI, KC_BSPC, KC_ENT,  KC_SPC,  DELSYM,  KC_RGUI, KC_RALT, KC_MUTE
-    ),
-
-    /* Symbols and F-Keys */
-    [_SYMBOL] = LAYOUT(
-     _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, KC_UNDS, KC_LCBR, KC_LBRC, _______,                   _______, KC_RBRC, KC_RCBR, KC_MINS, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                                           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-
-    /* Nav Cluster and Text Macros*/
-    [_NAV] = LAYOUT(
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______,                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                                           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+     KC_PMNS, KC_1,    KC_2,    KC_3,    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_UP,   KC_NO,   KC_NO,
+     KC_PPLS, KC_0,    KC_DOT,  KC_ENT,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,
+                                                           KC_NLCK, KC_LALT, KC_LGUI, KC_BSPC, KC_ENT,  KC_SPC,  KC_DEL,  KC_RGUI, KC_RALT, KC_MUTE
     )
 };
 
@@ -77,26 +50,4 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     }
     return false;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
-    return true;
 }
