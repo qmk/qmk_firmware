e4ef9ac4cebadf52d220902bfb4bad41573ebd44
fire-h0und <cest73@gmail.com>
2021-09-22 14:15:06 +0200

    File somehow got corrupt, here i fixed it
    I hope this helps?
    Just compiled the firmware and am typin off of it.

--- keyboards/cest73/tkm/rules.mk
+++ keyboards/cest73/tkm/rules.mk
@@ -7,21 +7,12 @@ BOOTLOADER = halfkay
 # Build Options
 #   change yes to no to disable
 #
-<<<<<<< HEAD
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
-=======
-BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
->>>>>>> 01b0eeac1ebbf45ef974152f240b3132470d0da6
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-<<<<<<< HEAD
-=======
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
->>>>>>> 01b0eeac1ebbf45ef974152f240b3132470d0da6
+
