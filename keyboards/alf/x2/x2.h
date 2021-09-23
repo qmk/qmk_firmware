d55d9d27afd6dbb2e4569ad95472e169897845d0
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-23 02:46:02 -0700

    remove LAYOUT macro
    Turns out LAYOUT is the same as LAYOUT_all.

--- keyboards/alf/x2/x2.h
+++ keyboards/alf/x2/x2.h
@@ -22,21 +22,6 @@
 
 // K44 and K48 are the microswitches at the top of the PCB
 
-#define LAYOUT( \
-            K44, K48,                                                          \
-    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
-    K10, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, \
-    K20, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      K2D, \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, \
-    K40, K41,      K43,           K46,                K4A, K4B, K4C, K4D, K4E  \
-) { \
-    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E }, \
-    { K10, ___, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E }, \
-    { K20, ___, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, ___ }, \
-    { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E }, \
-    { K40, K41, ___, K43, K44, ___, K46, ___, K48, ___, K4A, K4B, K4C, K4D, K4E }  \
-}
-
 #define LAYOUT_all( \
             K44, K48,                                                          \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
