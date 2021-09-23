9cbf05d275eda6d8ac9819ae30c419b2e58cd2a3
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-23 02:58:55 -0700

    Rename layout macros
    - rename LAYOUT_60_wkl to LAYOUT_60_ansi_tsangan_split_rshift
    - rename LAYOUT_60_wkl_split_bs to LAYOUT_60_tsangan_hhkb

--- keyboards/exclusive/e6_rgb/e6_rgb.h
+++ keyboards/exclusive/e6_rgb/e6_rgb.h
@@ -26,7 +26,7 @@
     { K40, K41, K42, ___, ___, ___, K46, ___, ___, ___, K4A, K4B, K4C, K4D }  \
 }
 
-#define LAYOUT_60_wkl( \
+#define LAYOUT_60_ansi_tsangan_split_rshift( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K2D, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2C, \
@@ -56,7 +56,7 @@
     { ___, K41, K42, ___, ___, ___, K46, ___, ___, ___, ___, K4B, K4C, ___ }  \
 }
 
-#define LAYOUT_60_wkl_split_bs( \
+#define LAYOUT_60_tsangan_hhkb( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K2D, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2C, \
