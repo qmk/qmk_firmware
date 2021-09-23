fa451508695db3e7366922007c39b9f882014cbd
Chad Stovern <chad@stovern.me>
2021-09-23 17:46:07 -0500

    updating nav layer setup and some cleanup
    - switch nav layer to use hjkl
    - add ←↓↑→ nave to function layer
    - relocate f11 and f12 to same location as - + but on function layer (for volume control)
    - remove unused alternate shift keys and related config

--- keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
+++ keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
@@ -41,18 +41,18 @@ enum layers {
 #define HOME_R OPT_T(KC_R)
 #define HOME_S CMD_T(KC_S)
 #define HOME_T SFT_T(KC_T)
-#define HOME_Z SFT_T(KC_Z)
 #define HOME_BT CTL_T(KC_GRV)
 #define HOME_N SFT_T(KC_N)
 #define HOME_E CMD_T(KC_E)
 #define HOME_I OPT_T(KC_I)
 #define HOME_O CTL_T(KC_O)
-#define HOME_SL SFT_T(KC_SLSH)
 #define HOME_DWN SFT_T(KC_DOWN)
 #define HOME_UP CMD_T(KC_UP)
 #define HOME_RGT OPT_T(KC_RGHT)
+#define HOME_J SFT_T(KC_J)
+#define HOME_K CMD_T(KC_K)
+#define HOME_L OPT_T(KC_L)
 #define HOME_QT CTL_T(KC_QUOT)
-#define HOME_BSL SFT_T(KC_BSLS)
 
 /* layer definitions */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  | CTL  | OPT  | CMD  | SFT  |      |            |      | SFT  | CMD  | OPT  | CTL  |
  |------+------+------+------+------|            |------+------+------+------+------|
  | Z    | X    | C    | D    | V    |            | K    | H    | , <  | . >  | / ?  |
- | SFT  |      |      |      |      |            |      |      |      |      | SFT  |
+ |      |      |      |      |      |            |      |      |      |      |      |
  `------'------'------'------'------'            `------'------'------'------'------'
                          .------.------.      .------.------.
                          | TAB  | BSPC |      | SPC  | ENT  |
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
   KC_Q,   KC_W,   KC_F,    KC_P,     KC_B,         KC_J,    KC_L,   KC_U,    KC_Y,   KC_SCLN,
   HOME_A, HOME_R, HOME_S,  HOME_T,   KC_G,         KC_M,    HOME_N, HOME_E,  HOME_I, HOME_O,
-  HOME_Z, KC_X,   KC_C,    KC_D,     KC_V,         KC_K,    KC_H,   KC_COMM, KC_DOT, HOME_SL,
+  KC_Z,   KC_X,   KC_C,    KC_D,     KC_V,         KC_K,    KC_H,   KC_COMM, KC_DOT, KC_SLSH,
                   XXXXXXX, CMD_TAB,  NUM_BSPC,     FUN_SPC, KC_ENT, XXXXXXX
 ),
 
@@ -85,11 +85,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  | 1 !  | 2 @  | 3 #  | 4 $  | 5 %  |            | 6 ^  | 7 &  | 8 *  | 9 (  | 0 )  |
  |      |      |      |      |      |            |      |      |      |      |      |
  |------+------+------+------+------|            |------+------+------+------+------|
- | ` ~  |      |      |      |      |            | ←    | ↓    | ↑    | →    | ' "  |
+ | ` ~  |      |      |      |      |            | H    | J    | K    | L    | ' "  |
  | CTL  | OPT  | CMD  | SFT  |      |            |      | SFT  | CMD  | OPT  | CTL  |
  |------+------+------+------+------|            |------+------+------+------+------|
  |      |      |      |      |      |            | - _  | = +  | [ {  | ] }  | \ |  |
- | SFT  |      |      |      |      |            |      |      |      |      | SFT  |
+ |      |      |      |      |      |            |      |      |      |      |      |
  `------'------'------'------'------'            `------'------'------'------'------'
                          .------.------.      .------.------.
                          | ESC  | ↓↓↓  |      | ↓↓↓  | ↓↓↓  |
@@ -97,8 +97,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          '------'------'      '------'------'
 */
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,     KC_8,    KC_9,     KC_0,
-  HOME_BT, KC_LOPT, KC_LCMD, KC_LSFT, XXXXXXX,      KC_LEFT, HOME_DWN, HOME_UP, HOME_RGT, HOME_QT,
-  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS, KC_EQL,   KC_LBRC, KC_RBRC,  HOME_BSL,
+  HOME_BT, KC_LOPT, KC_LCMD, KC_LSFT, XXXXXXX,      KC_H,    HOME_J,   HOME_K,  HOME_L,   HOME_QT,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MINS, KC_EQL,   KC_LBRC, KC_RBRC,  KC_BSLS,
                     XXXXXXX, KC_ESC,  KC_TRNS,      KC_TRNS, KC_TRNS,  XXXXXXX
 ),
 
@@ -108,10 +108,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  | F1   | F2   | F3   | F4   | F5   |            | F6   | F7   | F8   | F9   | F10  |
  |      |      |      |      |      |            |      |      |      |      |      |
  |------+------+------+------+------|            |------+------+------+------+------|
- |      |      |      |      |      |            |      |      |      | F11  | F12  |
- |      |      |      |      |      |            |      |      |      |      |      |
+ |      |      |      |      |      |            | ←    | ↓    | ↑    | →    |      |
+ | CTL  | OPT  | CMD  | SFT  |      |            |      | SFT  | CMD  | OPT  | CTL  |
  |------+------+------+------+------|            |------+------+------+------+------|
- | RGB  | RGB  | RGB  | RGB- | RGB+ |            |      |      |      |      | GAME |
+ | RGB  | RGB  | RGB  | RGB- | RGB+ |            | F11  | F12  |      |      | GAME |
  | TGGL | RMOD | MOD  |      |      |            |      |      |      |      |      |
  `------'------'------'------'------'            `------'------'------'------'------'
                          .------.------.      .------.------.
@@ -119,9 +119,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          |      |      |      |      |      |
                          '------'------'      '------'------'
 */
-  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,  KC_F12,
-  RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAD, RGB_VAI,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GAMING,
+  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,
+  KC_LCTL, KC_LOPT,  KC_LGUI, KC_LSFT, XXXXXXX,      KC_LEFT, HOME_DWN, HOME_UP, HOME_RGT, KC_LCTL,
+  RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAD, RGB_VAI,      KC_F11,  KC_F12,   XXXXXXX, XXXXXXX,  GAMING,
                      XXXXXXX, KC_ESC,  KC_DEL,       KC_TRNS, KC_TRNS,  XXXXXXX
 ),
 
@@ -179,8 +179,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case CMD_TAB: // make CMD SPC faster
-    case HOME_Z:  // make alternate shift faster
-    case HOME_SL: // make alternate shift faster
         return false;
     default:
         return true;
