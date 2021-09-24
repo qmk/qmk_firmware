237650e7da27802d6d44e27d2ace6fe90e39e986
drhigsby <71532157+drhigsby@users.noreply.github.com>
2021-09-24 08:26:54 -0700

    Update keyboards/packrat/keymaps/3Uc/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/packrat/keymaps/3Uc/keymap.c
+++ keyboards/packrat/keymaps/3Uc/keymap.c
@@ -90,13 +90,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    return true;
-}
