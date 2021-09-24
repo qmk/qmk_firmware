1a7511bec88fbfa86cc6b8e903e7593a0c80bf3f
Bahrul Hidayat <dayat.py@gmail.com>
2021-09-25 05:49:36 +0800

    Update keyboards/dyz/selka40/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/dyz/selka40/config.h
+++ keyboards/dyz/selka40/config.h
@@ -20,9 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xD772
-#define PRODUCT_ID      0x0012
-#define DEVICE_VER      0x0001
 #define MANUFACTURER    dyz
 #define PRODUCT         selka40
 
