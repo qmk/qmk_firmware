f2f4d3c84eb527ce86e0ff01233db6869a9755dc
reasonsandreasons <walterphanley@gmail.com>
2021-09-24 12:42:56 -0500

    Update keyboards/wren/keymaps/walterhanley/keymap.c
    As per feedback, move encoder definition to wren.c from keymap.c
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/wren/keymaps/walterhanley/keymap.c
+++ keyboards/wren/keymaps/walterhanley/keymap.c
@@ -61,22 +61,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_TAB);
-        } else {
-            tap_code(KC_TAB);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    return false;
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
