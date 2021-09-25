68e6cba764c164f925c9c89f3dc3afef588202cd
zigotica <zigotica@gmail.com>
2021-09-25 19:24:29 +0200

    encoder_update_kb to use bool and return

--- keyboards/z12/z12.c
+++ keyboards/z12/z12.c
@@ -17,7 +17,7 @@
 #include "z12.h"
 
 #ifdef ENCODER_ENABLE
-void encoder_update_kb(uint8_t index, bool clockwise) {
+bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
     if (index == 0) { // LEFT
         // Scroll
@@ -34,5 +34,6 @@ void encoder_update_kb(uint8_t index, bool clockwise) {
             tap_code_delay(KC_VOLD, 10);
         }
     }
+    return false;
 }
 #endif
