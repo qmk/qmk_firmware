6281a85cfb57168d14957eae844f7cd6cf45baa1
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-25 02:49:24 -0700

    Genesis Hotswap: rename LAYOUT to LAYOUT_tkl_ansi

--- keyboards/ggkeyboards/genesis/hotswap/hotswap.h
+++ keyboards/ggkeyboards/genesis/hotswap/hotswap.h
@@ -19,7 +19,7 @@
 #include "quantum.h"
 
 // clang-format off
-#define LAYOUT( \
+#define LAYOUT_tkl_ansi( \
     K00,      K01, K02, K03, K04,   K05, K06, K07, K08,    K09, K0A, K0B, K0C,  K0F, K0G, K0H,  \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,       K2F, K2G, K2H,  \
     K30,   K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,     K3F, K3G, K3H,  \
