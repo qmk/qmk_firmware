3f45821fd755a0748aab891af80137e4fe5c9817
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-24 19:05:02 -0700

    correct keyboard metadata
    * specify keyboard variant in info.json and config.h
    * correct maintainer field in info.json

--- keyboards/cmm_studio/saka68/hotswap/config.h
+++ keyboards/cmm_studio/saka68/hotswap/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x5348
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    CMM.Studio
-#define PRODUCT         Saka68
+#define PRODUCT         Saka68 Hotswap
 
 /* key matrix size */
 #define MATRIX_ROWS 5
