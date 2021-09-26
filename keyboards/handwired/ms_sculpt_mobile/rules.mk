a935fa922191edd29f5116b044ea9a0166a68edb
milestogo <milestogo@users.noreply.github.com>
2021-09-26 13:05:02 -0700

    fix merge conflict with master in rules.mk

--- keyboards/handwired/ms_sculpt_mobile/rules.mk
+++ keyboards/handwired/ms_sculpt_mobile/rules.mk
@@ -1,13 +1,14 @@
-BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes        # Console for debug(+400)
 COMMAND_ENABLE = yes        # Commands for debug and configuration
-#Do not enable SLEEP_LED_ENABLE.it uses the same timer as BACKLIGHT_ENABLE
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
-MIDI_ENABLE = no            # MIDI controls
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output on port C6
+AUDIO_ENABLE = no           # Audio output
+
 DEFAULT_FOLDER = handwired/ms_sculpt_mobile/8x18_arm
