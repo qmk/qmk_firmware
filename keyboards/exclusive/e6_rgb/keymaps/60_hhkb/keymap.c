dd2af71df800554b009ad4f674dd113650f1c27f
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-25 22:08:25 -0700

    fix layer index on 60_hhkb keymap

--- keyboards/exclusive/e6_rgb/keymaps/60_hhkb/keymap.c
+++ keyboards/exclusive/e6_rgb/keymaps/60_hhkb/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_BSLS,
                  KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, MO(1)
     ),
-    [0] = LAYOUT_60_hhkb(
+    [1] = LAYOUT_60_hhkb(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         RESET,   RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
