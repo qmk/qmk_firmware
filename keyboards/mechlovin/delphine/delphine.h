93d6c8f2fcaffbb1aade6ffe6433a83f668c2d4c
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-27 10:14:17 -0700

    add LAYOUT_numpad_6x4

--- keyboards/mechlovin/delphine/delphine.h
+++ keyboards/mechlovin/delphine/delphine.h
@@ -18,6 +18,8 @@
 
 #include "quantum.h"
 
+#define XXX KC_NO
+
 #define LAYOUT_ortho_6x4( \
     K00, K01, K02, K03,   \
     K10, K11, K12, K13,   \
@@ -32,4 +34,20 @@
     { K30,   K31,   K32,   K33   }, \
     { K40,   K41,   K42,   K43   }, \
     { K50,   K51,   K52,   K53   }, \
+}
+
+#define LAYOUT_numpad_6x4( \
+    K00, K01, K02, K03, \
+    K10, K11, K12, K13, \
+    K20, K21, K22,      \
+    K30, K31, K32, K23, \
+    K40, K41, K42,      \
+    K51,      K52, K43  \
+) { \
+    { K00, K01, K02, K03 }, \
+    { K10, K11, K12, K13 }, \
+    { K20, K21, K22, K23 }, \
+    { K30, K31, K32, XXX }, \
+    { K40, K41, K42, K43 }, \
+    { XXX, K51, K52, XXX }, \
 }
