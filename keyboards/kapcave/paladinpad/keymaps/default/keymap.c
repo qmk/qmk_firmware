7f995b7ba3417a8a476cad2853591fbf2b0b065f
nachie <me@nachie.com>
2021-09-28 12:24:36 -0700

    Update keyboards/kapcave/paladinpad/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kapcave/paladinpad/keymaps/default/keymap.c
+++ keyboards/kapcave/paladinpad/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "paladinpad.h"
+#include QMK_KEYBOARD_H
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
