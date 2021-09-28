f65a812429f9732cd392e5e4b4b30d8bfa6ab9c7
John Ezra <johnezra@dempseytech.com>
2021-09-28 01:41:19 -0700

    changed layer icon handling for OLED

--- keyboards/splitkb/kyria/keymaps/john-ezra/config.h
+++ keyboards/splitkb/kyria/keymaps/john-ezra/config.h
@@ -18,10 +18,8 @@
 
 #define OLED_FONT_H "keyboards/splitkb/kyria/keymaps/john-ezra/glcdfont.c"
 #define OLED_FONT_END 255
-
-#ifdef OLED_DRIVER_ENABLE
-  #define OLED_DISPLAY_128X64
-#endif
+#define OLED_TIMEOUT 30000
+#define OLED_DISPLAY_128X64
 
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_HUE_STEP 5
