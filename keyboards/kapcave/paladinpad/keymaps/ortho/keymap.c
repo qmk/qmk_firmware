feb2d8592335a13344e9b67a5794b850b3136f83
nachie <me@nachie.com>
2021-09-28 12:24:57 -0700

    Update keyboards/kapcave/paladinpad/keymaps/ortho/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kapcave/paladinpad/keymaps/ortho/keymap.c
+++ keyboards/kapcave/paladinpad/keymaps/ortho/keymap.c
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "paladinpad.h"
+#include QMK_KEYBOARD_H
 
 enum custom_keycodes {
   DBLZERO = SAFE_RANGE
