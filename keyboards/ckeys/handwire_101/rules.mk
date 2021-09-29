6d3fb64b8b41ea2279d02e67bc2915ee1485ecc7
fauxpark <fauxpark@gmail.com>
2021-09-30 08:22:20 +1000

    Remove redundant `MIDI_ENABLE = no` in keyboard-level rules.mk

--- keyboards/ckeys/handwire_101/rules.mk
+++ keyboards/ckeys/handwire_101/rules.mk
@@ -18,5 +18,4 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no             # MIDI support
 AUDIO_ENABLE = no           # Audio output
