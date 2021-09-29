012a99779b90f5f104616625575b07149483d793
Derek Hsu <derekhybrid@gmail.com>
2021-09-29 12:04:55 +0800

    Add license to .c and .h files

--- keyboards/runner3680/5x6_5x8/config.h
+++ keyboards/runner3680/5x6_5x8/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 omkbd
+/* Copyright 2021 omkbd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
