ee2cc231d5b75d0070488f31acc062669e6a72f5
fauxpark <fauxpark@gmail.com>
2021-09-29 17:59:05 +1000

    Remove keyboard-level `UNICODE_ENABLE = no`, A-D

--- keyboards/converter/ibm_terminal/keymaps/default/rules.mk
+++ keyboards/converter/ibm_terminal/keymaps/default/rules.mk
@@ -11,7 +11,6 @@ NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: ht
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output
-UNICODE_ENABLE = no         # Unicode
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 PS2_USE_USART = yes
 
