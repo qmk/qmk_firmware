4b00cd99d77f69441411c7f4ea839f2b4e514683
Grayson Carr <gcarr@barracuda.com>
2021-09-28 20:50:30 -0400

    Formatted code and added aliases for custom keycodes

--- keyboards/keychron/q1/rev_0100/keymaps/gtg465x/keymap.c
+++ keyboards/keychron/q1/rev_0100/keymaps/gtg465x/keymap.c
@@ -24,18 +24,20 @@ enum layers{
   WIN_FN
 };
 
-enum my_keycodes {
+enum custom_keycodes {
   KC_MISSION_CONTROL = SAFE_RANGE,
   KC_LAUNCHPAD
 };
 
+#define KC_MCTL KC_MISSION_CONTROL
+#define KC_LPAD KC_LAUNCHPAD
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [MAC_BASE] = LAYOUT_ansi_82(
-     KC_ESC,             KC_BRID,  KC_BRIU,  KC_MISSION_CONTROL,    KC_LAUNCHPAD,    RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,   KC_INS,
+     KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,   KC_INS,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
@@ -69,22 +71,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case KC_MISSION_CONTROL:
-      if (record->event.pressed) {
-        host_consumer_send(0x29F);
-      } else {
-        host_consumer_send(0);
-      }
-      return false; // Skip all further processing of this key
-    case KC_LAUNCHPAD:
-      if (record->event.pressed) {
-        host_consumer_send(0x2A0);
-      } else {
-        host_consumer_send(0);
-      }
-      return false; // Skip all further processing of this key
-    default:
-      return true; // Process all other keycodes normally
-  }
+    switch (keycode) {
+        case KC_MISSION_CONTROL:
+            if (record->event.pressed) {
+                host_consumer_send(0x29F);
+            } else {
+                host_consumer_send(0);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_LAUNCHPAD:
+            if (record->event.pressed) {
+                host_consumer_send(0x2A0);
+            } else {
+                host_consumer_send(0);
+            }
+            return false;  // Skip all further processing of this key
+        default:
+            return true;  // Process all other keycodes normally
+    }
 }
