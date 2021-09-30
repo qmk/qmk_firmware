79b93567749de0e1152605c8aac44292817876b3
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-30 14:48:36 +0000

    Remove firmware size impact notes from <keyboard>/rules.mk (#14652)

--- keyboards/handwired/aek64/rules.mk
+++ keyboards/handwired/aek64/rules.mk
@@ -8,9 +8,9 @@ BOOTLOADER = halfkay
 #   change to no to disable the options.
 #
 BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
-MOUSEKEY_ENABLE  = yes # Mouse keys(+4700)
-EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
-CONSOLE_ENABLE   = yes # Console for debug(+400)
+MOUSEKEY_ENABLE  = yes # Mouse keys
+EXTRAKEY_ENABLE  = yes # Audio control and System control
+CONSOLE_ENABLE   = yes # Console for debug
 COMMAND_ENABLE   = yes # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE      = no  # USB Nkey Rollover - not yet supported in LUFA
