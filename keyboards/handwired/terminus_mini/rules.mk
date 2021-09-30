81406939712b8159daf48667d6289d68d5ee86bb
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-09-30 00:40:00 -0700

    Remove firmware size impact notes from <keyboard>/rules.mk

--- keyboards/handwired/terminus_mini/rules.mk
+++ keyboards/handwired/terminus_mini/rules.mk
@@ -8,9 +8,9 @@ BOOTLOADER = halfkay
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
