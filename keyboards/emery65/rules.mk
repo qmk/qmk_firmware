23496a7ba042514100117b59abb2c007621c05c9
yiancar <yiancar@gmail.com>
2021-10-01 18:11:48 +0100

    rules.mk

--- keyboards/emery65/rules.mk
+++ keyboards/emery65/rules.mk
@@ -4,6 +4,7 @@ MCU = STM32F072
 # Bootloader selection
 BOOTLOADER = stm32-dfu
 
+# Wildcard to allow APM32 MCU
 DFU_SUFFIX_ARGS = -v FFFF -p FFFF
 
 # Do not put the microcontroller into power saving mode
