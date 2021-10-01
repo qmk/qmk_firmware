416f4535f2995ea3c23e9d63b739310187d6d3df
U-MAINMAN\smerrill <shaymdev@gmail.com>
2021-10-01 17:26:30 -0600

    Enable middle button trackpoint scroll

--- keyboards/santoku/keymaps/shaymdev/config.h
+++ keyboards/santoku/keymaps/shaymdev/config.h
@@ -7,3 +7,9 @@
 #define TAPPING_TERM 220
 //#define PERMISSIVE_HOLD
 //#define TAPPING_FORCE_HOLD
+
+#undef ENCODER_RESOLUTION 
+#define ENCODER_RESOLUTION 1
+
+#undef PS2_MOUSE_SCROLL_BTN_MASK  //shaymdev: maybe set his to middle (default?) possible good info here: https://beta.docs.qmk.fm/using-qmk/hardware-features/feature_ps2_mouse
+#define PS2_MOUSE_SCROLL_MASK 0x0F
