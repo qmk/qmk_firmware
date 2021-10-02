2aefb372df4be37726ac1eb7c6f813a7324d0605
morganpa <66316054+morganpa@users.noreply.github.com>
2021-10-02 01:26:11 -0700

    not really sure tbh

--- keyboards/keebio/bfo9000/keymaps/morgan_moveRightLeft/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/morgan_moveRightLeft/keymap.c
@@ -14,12 +14,12 @@ uint16_t alt_tab_modifier  = KC_LGUI;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_OSX] = LAYOUT(
-        KC_ESC,       A(KC_P0),       KC_ESC,  KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,          KC_WH_U,              KC_VOLU,      KC_F6,        KC_F7,        KC_F8,        KC_F9,        KC_F10,                 KC_F11,   KC_F12,  KC_DEL,
-        A(KC_PPLS),   A(KC_PMNS),     KC_GRV,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,           KC_WH_D,              KC_VOLD,      KC_6,         KC_7,         KC_8,         KC_9,         KC_0,                   _______,  _______, KC_BSPC,
-        _______,      _______,        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,           _______,              KC_EQL,       KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,                   _______,  _______, _______,
-        DM_PLY1,      DM_PLY2,        KC_ESC,  LCTL_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LOPT_T(KC_F), KC_G,           KC_BSPC,              KC_MINS,      KC_H,         LOPT_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LCTL_T(KC_SCLN),        KC_QUOT,  KC_ENT,  KC_NO,
-        TO(_WINDOWS), TO(_GAMING),    KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,           ALT_TAB,              KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,                KC_RSFT,  KC_UP,   A(KC_P0),
-        TO(_OSX),     MO(_OSXMACROS), KC_LCTL, LCTL(KC_B),   KC_LCTL,      KC_LALT,      KC_LGUI,      KC_SPC,  MO(_OSXMACROS),              MO(_SYMBOLS), KC_MS_BTN1,   KC_MS_BTN3,   KC_MS_BTN2,   JIGGLER,      LCTL(LSFT(LGUI(KC_4))), KC_LEFT,  KC_DOWN, KC_RGHT
+        KC_ESC,       A(KC_P0),       KC_ESC,  KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,          KC_WH_U,              KC_F6,        KC_F7,        KC_F8,        KC_F9,        KC_F10,                 KC_F11,   KC_F12,  KC_DEL,  _______
+        A(KC_PPLS),   A(KC_PMNS),     KC_GRV,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,           KC_WH_D,              KC_6,         KC_7,         KC_8,         KC_9,         KC_0,                   KC_MINS,  KC_EQL,  KC_BSPC, _______
+        _______,      _______,        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,           _______,              KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,                   _______,  _______, _______, _______
+        DM_PLY1,      DM_PLY2,        KC_ESC,  LCTL_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LOPT_T(KC_F), KC_G,           KC_BSPC,              KC_H,         LOPT_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LCTL_T(KC_SCLN),        KC_QUOT,  KC_ENT,  KC_NO,   _______
+        TO(_WINDOWS), TO(_GAMING),    KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,           ALT_TAB,              KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,                KC_RSFT,  KC_UP,   _______, A(KC_P0)
+        TO(_OSX),     MO(_OSXMACROS), KC_LCTL, LCTL(KC_B),   KC_LCTL,      KC_LALT,      KC_LGUI,      KC_SPC,  MO(_OSXMACROS),              KC_MS_BTN1,   KC_MS_BTN3,   KC_MS_BTN2,   JIGGLER,      LCTL(LSFT(LGUI(KC_4))), KC_LEFT,  KC_DOWN, KC_RGHT, _______
     ),
     [_OSXMACROS] = LAYOUT(
         G(C(KC_Q)), _______, _______, _______,  _______,  _______,  _______,  _______,        _______,                                _______, _______,    _______,  _______,     _______,  _______, _______, _______, _______,
@@ -30,12 +30,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______, _______, _______,  _______,  _______,  _______,  _______,        _______,                                _______, G(KC_LEFT), _______,  G(KC_RIGHT), _______,  _______, _______, _______, _______
     ),
     [_SYMBOLS] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 _______, _______,    _______,    _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______,                 _______, S(KC_6),    S(KC_7),    S(KC_8), S(KC_9), S(KC_0), _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 KC_EQL,  S(KC_LBRC), S(KC_RBRC), _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 KC_MINS, KC_LBRC,    KC_RBRC,    _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 KC_BSLS, KC_QUOT,    S(KC_QUOT), _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 _______, KC_MRWD,    KC_MPLY,    KC_MFFD, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______,                 _______, S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 KC_EQL,  _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 KC_MINS, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 KC_BSLS, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______,                 _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, _______, _______
     ),
     [_WINDOWS] = LAYOUT(
         KC_ESC,  KC_9,               _______, _______,      _______,      _______,      _______,      _______, _______,               _______,      _______, _______, _______, _______, _______, _______, _______, _______,
