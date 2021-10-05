11ea996adff792e4d43be01e0a67f88bab0c3fdf
jonavin <71780717+Jonavin@users.noreply.github.com>
2021-10-04 21:39:31 -0400

    Update keyboards/mechwild/obe/keymaps/jonavin/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/mechwild/obe/keymaps/jonavin/keymap.c
+++ keyboards/mechwild/obe/keymaps/jonavin/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void keyboard_post_init_keymap(void) {
     // keyboard_post_init_user() moved to userspace
     #ifdef RGBLIGHT_ENABLE
-        rgblight_mode(1); // single colour mode
+        rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
         rgblight_setrgb(RGB_OBE_BOW); // Default startup colour
         rgblight_layers = _rgb_layers;
     #endif
