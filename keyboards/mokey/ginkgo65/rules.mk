7e6629a26c8986769645f5d9d64f39c2e88594d2
rhmokey <57172003+rhmokey@users.noreply.github.com>
2021-10-05 23:18:39 +0800

    Update keyboards/mokey/ginkgo65/rules.mk
    # MCU name
    MCU = atmega32u4
    
    # Bootloader selection
    BOOTLOADER = atmel-dfu
    
    # Build Options
    #   change yes to no to disable
    #
    BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
    MOUSEKEY_ENABLE = no        # Mouse keys
    EXTRAKEY_ENABLE = yes        # Audio control and System control
    CONSOLE_ENABLE = no         # Console for debug
    COMMAND_ENABLE = no         # Commands for debug and configuration
    # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
    SLEEP_LED_ENABLE = no        # Breathing sleep LED during USB suspend
    # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
    NKRO_ENABLE = no             # USB Nkey Rollover
    BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
    RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
    AUDIO_ENABLE = no            # Audio output on port C6
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/mokey/ginkgo65/rules.mk
+++ keyboards/mokey/ginkgo65/rules.mk
@@ -7,19 +7,16 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes        # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no        # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no             # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
-MIDI_ENABLE = no             # MIDI support
-UNICODE_ENABLE = no          # Unicode
-AUDIO_ENABLE = no            # Audio output on port C6
-FAUXCLICKY_ENABLE = no       # Use buzzer to emulate clicky switches
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no            # Audio output
 
