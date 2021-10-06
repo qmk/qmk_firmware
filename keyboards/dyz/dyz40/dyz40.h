4aad829864e01ba70ef2e9ce9f817ddf0f4b87bf
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-10-06 03:52:47 -0700

    convert tabs to spaces in dyz40.h

--- keyboards/dyz/dyz40/dyz40.h
+++ keyboards/dyz/dyz40/dyz40.h
@@ -36,15 +36,15 @@
  */
 
 #define LAYOUT_all( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
     K30, K31, K32, K33,           K36, K37,           K3A, K3B, K3C \
 ) { \
-	{ K00,  K01,  K02,  K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C }, \
-	{ K10,  K11,  K12,  K13, K14, K15, K16, K17, K18, K19, K1A, K1B, _X_ }, \
-	{ K20,  K21,  K22,  K23, K24, K25, K26, K27, K28, K29, K2A, K2B, _X_ }, \
-	{ K30,  K31,  K32,  K33, _X_, _X_, K36, K37, _X_, _X_, K3A, K3B, K3C } \
+    { K00,  K01,  K02,  K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C }, \
+    { K10,  K11,  K12,  K13, K14, K15, K16, K17, K18, K19, K1A, K1B, _X_ }, \
+    { K20,  K21,  K22,  K23, K24, K25, K26, K27, K28, K29, K2A, K2B, _X_ }, \
+    { K30,  K31,  K32,  K33, _X_, _X_, K36, K37, _X_, _X_, K3A, K3B, K3C } \
 }
 
 #define LAYOUT_7u_space( \
