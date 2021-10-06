d5fae3a6f96d44a432350d5f487204a962c5f9bc
Grayson Carr <734415+gtg465x@users.noreply.github.com>
2021-10-06 11:36:04 -0400

    Updated Keychron Q1 user keymap (#14666)
    * Updated Keychron Q1 user keymap
    - Added Caps Lock indicator
    - Mission Control and Launchpad custom keycodes are now defined using the VIA user keycodes range so they can be labeled properly in VIA
    
    * Only use VIA keycode range if VIA is enabled
    
    * Reformatted led_indexes enum to match layout
    
    * Feature: When the Fn layer is active, RGB lighting turns off for keys that are not assigned
    
    * Fixes
    
    * Cleanup: Updated method signature of rgb_matrix_set_color_by_keycode to more closely match rgb_matrix_set_color
    
    * Fixed int sizing for keycodes

--- keyboards/keychron/q1/rev_0100/keymaps/gtg465x/rgb_matrix_user.h
+++ keyboards/keychron/q1/rev_0100/keymaps/gtg465x/rgb_matrix_user.h
@@ -16,10 +16,9 @@
 
 #pragma once
 
-/* RGB Matrix Configuration */
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_DISABLE_WHEN_USB_SUSPENDED
-#    define CAPS_LOCK_INDICATOR_COLOR RGB_RED
-#    define CAPS_LOCK_INDICATOR_LIGHT_ALPHAS
-#    define FN_LAYER_TRANSPARENT_KEYS_OFF
-#endif
+void rgb_matrix_init_user(void);
+
+void rgb_matrix_set_color_by_keycode(uint8_t led_min, uint8_t led_max, uint8_t layer, bool (*is_keycode)(uint16_t), uint8_t red, uint8_t green, uint8_t blue);
+
+bool is_caps_lock_indicator(uint16_t keycode);
+bool is_transparent(uint16_t keycode);
