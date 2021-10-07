9e7c515e77ac6566825897806a4831128986bcf4
jackytrabbit <jacky_lijun@126.com>
2021-10-07 22:43:52 +0800

    Create rules.mk

---
+++ keyboards/lazydesigners/dimple/rules.mk
@@ -0,0 +1,24 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+NKRO_ENABLE = yes            # USB Nkey Rollover
+
+# Disable unsupported hardware
+AUDIO_SUPPORTED = no
+DEFAULT_FOLDER = lazydesigners/dimple/staggered/rev1
