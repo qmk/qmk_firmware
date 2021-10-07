951331cd8d24480bb95cdf2fe4527ea99b32f1c1
Joel Challis <git@zvecr.com>
2021-10-07 01:45:59 +0100

    Fix i2c_readReg16 (#14730)

--- platforms/chibios/drivers/i2c_master.c
+++ platforms/chibios/drivers/i2c_master.c
@@ -137,7 +137,7 @@ i2c_status_t i2c_readReg16(uint8_t devaddr, uint16_t regaddr, uint8_t* data, uin
     i2c_address = devaddr;
     i2cStart(&I2C_DRIVER, &i2cconfig);
     uint8_t register_packet[2] = {regaddr >> 8, regaddr & 0xFF};
-    msg_t   status             = i2cMasterTransmitTimeout(&I2C_DRIVER, (i2c_address >> 1), &register_packet, 2, data, length, TIME_MS2I(timeout));
+    msg_t   status             = i2cMasterTransmitTimeout(&I2C_DRIVER, (i2c_address >> 1), register_packet, 2, data, length, TIME_MS2I(timeout));
     return chibios_to_qmk(&status);
 }
 
