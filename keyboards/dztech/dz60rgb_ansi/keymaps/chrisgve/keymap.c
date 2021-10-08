02acf756af75d402bae51ba7b0c0756421310d65
ChrisGVE <christian.berclaz@mac.com>
2021-10-08 22:00:33 +0800

    Added a Shift key in nav and exchanged Fn and \ keys for dz60_tsangan

--- keyboards/dztech/dz60rgb_ansi/keymaps/chrisgve/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/chrisgve/keymap.c
@@ -29,6 +29,7 @@ enum ansi_layers {
 #define F_DIR       MO(_FULL_DIR)
 #define F_WORD      A(KC_RIGHT)
 #define B_WORD      A(KC_LEFT)
+#define S_SHIFT     SFT_T(KC_S)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_60_ansi(
@@ -36,13 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         CPS_CTL, KC_A,    KC_S,    D_DIR,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, FN,      KC_RALT, KC_RCTL
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             FN,      KC_RGUI, KC_RALT, KC_RCTL
     ),
 
     [_DIR] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, F_WORD,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, F_DIR,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
+        _______, _______, S_SHIFT, _______, F_DIR,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
         _______,          _______, _______, _______, B_WORD,  _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
--- users/chrisgve/chrisgve.h
+++ users/chrisgve/chrisgve.h
@@ -15,36 +15,162 @@
  */
 #pragma once
 
-// Standard layers
+#include QMK_KEYBOARD_H
 
+// Custom layers
 enum CHRISGVE_layers {
     _QWERTY_MAC,
-    _DIR,
-    _FULL_DIR,
+    _MAC_NAV_1,
+    _MAC_NAV_2,
     _QWERTY_LINUX,
+    _LINUX_NAV_1,
+    _LINUX_NAV_2,
     _QWERTY_WIN,
+    _WIN_NAV_1,
+    _WIN_NAV_2,
     _ADJUST,
     _CONFIG,
-    _FN,
     _TMUX,
     _VIM,
 };
 
+// Custom keys
+enum custom_keycodes {
+    TM_CREATE = SAFE_RANGE,
+    TM_SEL1,
+    TM_SEL2,
+    TM_SEL3,
+    TM_SEL4,
+    TM_SEL5,
+    TM_SEL6,
+    TM_SEL7,
+    TM_SEL8,
+};
+
+
 #define CHAR_MOVE   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
 #define LINE_MOVE   KC_HOME, KC_PGDN, KC_PGUP, KC_END
 
+#define MAC_NAV     _______, S_SHIFT, _______, M_NAV_2,   _______
+#define LINUX_NAV   _______, S_SHIFT, _______, L_NAV_2,   _______
+#define WIN_NAV     _______, S_SHIFT, _______, W_NAV_2,   _______
+
+#define TWO_TRS     _______, _______
+#define THREE_TRS   TWO_TRS, _______
+#define FOUR_TRS    THREE_TRS, _______
+#define FIVE_TRS    FOUR_TRS, _______
+#define SIX_TRS     FIVE_TRS, _______
+#define SEVEN_TRS   SIX_TRS, _______
+
+#define TWO_NOP     XXXXXXX, XXXXXXX
+#define THREE_NOP   TWO_NOP, XXXXXXX
+#define FOUR_NOP    THREE_NOP, XXXXXXX
+#define FIVE_NOP    FOUR_NOP, XXXXXXX
+#define SIX_NOP     FIVE_NOP, XXXXXXX
+#define SEVEN_NOP   SIX_NOP, XXXXXXX
+#define KC_FUNC     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12
 
 #define CPS_CTL     CTL_T(KC_CAPS)
 #define ADJUST      MO(_ADJUST)
 #define CONFIG      MO(_CONFIG)
-#define FN          MO(_FN)
-#define D_DIR       LT(_DIR, KC_D)
-#define F_DIR       MO(_FULL_DIR)
-#define F_WORD      A(KC_RIGHT)
-#define B_WORD      A(KC_LEFT)
+#define TMUX        LT(_TMUX, KC_SCLN)
+#define VIM         LT(_VIM, KC_V;)
+#define S_SHIFT     SFT_T(KC_S)
+
+#define DF_MAC      DF(_QWERTY_MAC)
+#define DF_WIN      DF(_QWERTY_WIN)
+#define DF_LINUX    DF(_QWERTY_LINUX)
+
+// Mac only definitions
+#define M_NAV_1     LT(_MAC_NAV_1, KC_D)
+#define M_NAV_2     MO(_MAC_NAV_2)
+#define M_F_WORD    A(KC_RIGHT)
+#define M_B_WORD    A(KC_LEFT)
+
+// Linux only definitions
+#define L_NAV_1     LT(_LINUX_NAV_1, KC_D)
+#define L_NAV_2     MO(_LINUS_NAV_2)
+#define L_F_WORD    A(KC_RIGHT)
+#define L_B_WORD    A(KC_LEFT)
+
+// Windows only definitions
+#define W_NAV_1     LT(_WIN_NAV_1, KC_D)
+#define W_NAV_2     MO(_WIN_NAV_2)
+#define W_F_WORD    C(KC_RIGHT)
+#define W_B_WORD    C(KC_LEFT)
+
+
+// VIM only definitions
+
+
+// TMUX only definitions
+
+
+#define TMUX_SL  TM_SEL1, TM_SEL2, TM_SEL3, TM_SEL4, TM_SEL5, TM_SEL6, TM_SEL7, TM_SEL8
 
 /*
  * Move via D + any of CHAR_MOVE or D + F + any of LINE_MOVE
  * Hidden keys, e.g. function keys, via Fn + Adjust layer
- * Configuation keys via Fn +
+ * Configuation keys via Fn  + RCTL + Config layer
+ * TMUX keys via ; + Tmux layer
+ * VIM keys via V + VIM layer
+ */
+
+/*
+ * VIM layer:
+ * ----------
+ *
+ */
+
+/*
+ * TMUX layer:
+ * -----------
+ * C -> TM_CREA
+ * 1 -> TM_SEL1
+ * 2 -> TM_SEL2
+ * 3 -> TM_SEL3
+ * 4 -> TM_SEL4
+ */
+
+/*
+ * Adjust layer:
+ * -------------
+ *
+ * ESC -> GRV
+ * 1 to = -> F1 to F12
+ * BS -> DEL
+ * SPACE -> PLAY
+ * H -> PREVIOUS
+ * J -> VOLUME DOWN
+ * K -> VOLUME UP
+ * L -> NEXT
+ * M -> MUTE
+ * R -> KC_WREF (Web refresh)
+ * B -> KC_WBAK (Web back)
+ * F -> KC_WFWD (Web forward)
+ * S -> KC_WSCH (Web search)
+ * R CTRL -> CONFIG
+ */
+
+/*
+ * Config layer:
+ * -------------
+ * Tab -> RESET
+ * Q -> NK_ON
+ * W -> NK_OFF
+ * E -> EEP_RST
+ * A -> GE_SWAP
+ * S -> GE_NORM
+ * D -> DEBUG
+ * F -> RGB_MOD
+ * G -> RGB_HUI
+ * H -> RGB_SAI
+ * J -> RGB_SPI
+ * K -> RGB_VAI
+ * L -> RGB_M_B
+ * Z -> LAG_SWP
+ * X -> LAG_NRM
+ * 1 -> MACOS (DF_MAC)
+ * 2 -> LINUX (DF_LINUX)
+ * 3 -> WIN   (DF_WIN)
  */
