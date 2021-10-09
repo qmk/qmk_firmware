f1c07cbef85a59d51b0cbb15b4d2e73bea836528
George Petri <t8_8t@icloud.com>
2021-10-09 18:24:54 +0300

    add readme

--- keyboards/gboards/gergoplex/keymaps/georgepetri/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/georgepetri/keymap.c
@@ -75,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-------------------------------.      ,-------------------------------.
      * |    !  |  @  |  {  |  }  |  |  |      |  `  |  -  |  =  |  /  |   \   |
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
-     * |    #  |  $  |  (  |  )  | RMB |      | LFT | DWN | UP  | RGT |   '   |
+     * |    #  |  $  |  (  |  )  |     |      | LFT | DWN | UP  | RGT |   '   |
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
-     * |    %  |  ^  |  [  |  ]  | LMB |      |  &  |  ~  |  +  |  *  |       |
+     * |    %  |  ^  |  [  |  ]  |     |      |  &  |  ~  |  +  |  *  |       |
      * `-------------------------------'      `-------------------------------'
      *           .-------------------.          .-----------------.
      *           |       |     |     |          |     |     |     |
@@ -85,8 +85,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_SPECIAL] = LAYOUT_split_3x5_3(
          KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,          KC_GRV,  KC_MINS, KC_EQL , KC_SLSH, KC_BSLS,
-         KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_QUOT,
-         KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,          KC_AMPR, KC_TILD, KC_PLUS, KC_ASTR, KC_TRNS,
+         KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_TRNS,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_QUOT,
+         KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TRNS,          KC_AMPR, KC_TILD, KC_PLUS, KC_ASTR, KC_TRNS,
                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS),
 
     /* Keymap 2: Numbers/Function/Motion layer
