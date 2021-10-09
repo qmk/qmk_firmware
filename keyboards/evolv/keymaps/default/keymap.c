b228d2945422a4a5ce596f63fc8841f1b9fc39cf
Gondolindrim <alvaro.volpato@usp.br>
2021-10-08 21:07:28 -0300

    Update encoder callback

--- keyboards/evolv/keymaps/default/keymap.c
+++ keyboards/evolv/keymaps/default/keymap.c
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#define MEDIA_KEY_DELAY 100
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [0] = LAYOUT_evolv_ansi(
         KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_INS ,           
@@ -47,3 +49,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,  
         _______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______)
 };
+
+uint32_t held_keycode_timer = 0;
+uint16_t mapped_code = 0;
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) mapped_code = KC_VOLU;
+    else mapped_code = KC_VOLD;
+    register_code(mapped_code);
+    held_keycode_timer = timer_read32();
+    while (timer_elapsed32(held_keycode_timer) < MEDIA_KEY_DELAY) ;
+    unregister_code(mapped_code);
+    return true;
+}
