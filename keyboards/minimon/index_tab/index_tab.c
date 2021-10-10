99d2cacc341907fc72181e49ca1306b013a76ea5
Kyrre Havik <Kyrremann@gmail.com>
2021-10-10 10:06:04 +0200

    Fix: added license to all files
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/minimon/index_tab/index_tab.c
+++ keyboards/minimon/index_tab/index_tab.c
@@ -1,13 +1,14 @@
+/*
+Copyright 2020 Kyrre Havik Eriksen
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "index_tab.h"
-
-void keyboard_pre_init_kb(void) {
-  // Keyboard start-up code goes here
-  // Runs once when the firmware starts up
-  keyboard_pre_init_user();
-};
-
-void matrix_scan_kb(void) {
-  // Looping keyboard code goes here
-  // This runs every cycle (a lot)
-  matrix_scan_user();
-};
