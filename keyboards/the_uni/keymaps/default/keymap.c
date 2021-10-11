cda1fada5c75acd56921ce99a13bf1f5cb5f13b0
Drashna Jael're <drashna@live.com>
2021-10-10 22:06:09 -0700

    Additional fixes

--- keyboards/the_uni/keymaps/default/keymap.c
+++ keyboards/the_uni/keymaps/default/keymap.c
@@ -19,18 +19,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap_steno.h"
 
 enum uni_layers {
-  _PLOVER,
+    _PLOVER,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[_PLOVER] = LAYOUT(
-		STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,  STN_ST3,  STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR ,
-		STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2,  STN_ST4,  STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR ,
-		                  STN_N1,  STN_A,   STN_O,    STN_E,    STN_U,   STN_N2),
+    [_PLOVER] = LAYOUT(
+        STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,  STN_ST3,  STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR ,
+        STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2,  STN_ST4,  STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR ,
+                          STN_N1,  STN_A,   STN_O,    STN_E,    STN_U,   STN_N2),
 
 };
 
  void matrix_init_user() {
-   steno_set_mode(STENO_MODE_GEMINI); // or STENO_MODE_BOLT
+    steno_set_mode(STENO_MODE_GEMINI); // or STENO_MODE_BOLT
  }
--- keyboards/the_uni/keymaps/qwerty/keymap.c
+++ keyboards/the_uni/keymaps/qwerty/keymap.c
@@ -16,20 +16,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-//#include "keymap_steno.h"
 
 enum uni_layers {
-  _QWERTY,
+    _QWERTY,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	 [_QWERTY] = LAYOUT(
-	 	KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
-	 	KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-	 	            KC_1, KC_C, KC_V, KC_N, KC_M, KC_2),
+    [_QWERTY] = LAYOUT(
+        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
+        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+                    KC_1, KC_C, KC_V, KC_N, KC_M, KC_2),
 
 };
-
- void matrix_init_user() {
-   //steno_set_mode(STENO_MODE_GEMINI); // or STENO_MODE_BOLT
- }
