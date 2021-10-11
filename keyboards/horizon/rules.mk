0b38fc4f0967d5de05f968c7452affc8080ddf6c
Steven Karrmann <skarrmann@gmail.com>
2021-10-11 06:21:05 +0000

    Update keyboards/horizon/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/horizon/rules.mk
+++ keyboards/horizon/rules.mk
@@ -11,6 +11,7 @@ BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = yes        # Console for debug
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
