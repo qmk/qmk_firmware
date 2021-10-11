c7f8dfdfaf704305bc7550ec530cff286ee375b3
Derek Hsu <derekhybrid@gmail.com>
2021-10-11 16:03:20 +0800

    [Keymap] Clean up default keymaps of Runner3680

--- keyboards/runner3680/5x6_5x8/keymaps/jis/keymap.c
+++ keyboards/runner3680/5x6_5x8/keymaps/jis/keymap.c
@@ -22,14 +22,9 @@ enum layer_number {
     _ADJUST
 };
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  ADJUST,
-  RGBRST
-};
-
 // Fillers to make layering more clear
 #define EISU LALT(KC_GRV)
+#define ADJUST MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -58,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------.      ,--------------------------------------------------------.
      * |      |   F1 |   F2 |   F3 |   F4 |   F5 |      |   F6 |   F7 |   F8 |   F9 |  F10 |  F11 |  F12 |      |
      * |------+------+------+------+------+------|      |------+------+------+------+------+------+------+------|
-     * |      |RGBRST| RESET|      |      |      |      |      |      |      |      |      |      |      |      |
+     * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
      * |------+------+------+------+------+------|      |------+------+------+------+------+------+------+------|
      * |      |  TOG |  HUI |  SAI |  VAI |      |      |      |      |      |      |      |      |      |      |
      * |------+------+------+------+------+------|      |------+------+------+------+------+------+------+------|
@@ -69,38 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
   [_ADJUST] =  LAYOUT(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
-      _______, RGBRST,  RESET,   _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
       _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
       _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______
       )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-      case QWERTY:
-        if (record->event.pressed) {
-           print("mode just switched to qwerty and this is a huge string\n");
-          set_single_persistent_default_layer(_QWERTY);
-        }
-        break;
-
-      case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        break;
-
-      case RGBRST:
-        #ifdef RGBLIGHT_ENABLE
-          if (record->event.pressed) {
-            eeconfig_update_rgblight_default();
-            rgblight_enable();
-          }
-        #endif
-        break;
-  }
-  return true;
-}
