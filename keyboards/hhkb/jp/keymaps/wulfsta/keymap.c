2e5b47acbf2862b4972853e55738b45d6116573d
Wulfsta <wulfstawulfsta@gmail.com>
2021-10-13 16:06:07 -0400

    hhkb/wulfsta: update layers

--- keyboards/hhkb/jp/keymaps/wulfsta/keymap.c
+++ keyboards/hhkb/jp/keymaps/wulfsta/keymap.c
@@ -4,9 +4,8 @@
 #define MOD_MASK_NONE 0
 
 #define BASE_LAYER (1 << 0)
-#define NUMROW_LAYER (1 << 1)
-#define CMDKEY_LAYER (1 << 2)
-#define NUMPAD_LAYER (1 << 3)
+#define CMDKEY_LAYER (1 << 1)
+#define NUMPAD_LAYER (1 << 2)
 #define ALL_LAYERS ~0
 
 
@@ -24,20 +23,13 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_JP(
-        O_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_DEL, KC_BSPC,
+        O_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_DEL, KC_BSPC,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_RSFT,
-        MO(1), MO(2), KC_LGUI, KC_LALT, MO(3), KC_SPC, KC_RGUI, KC_ENT, KC_RALT, MO(3), KC_LEFT, KC_DOWN, KC_RGHT),
+        MO(2), MO(1), KC_LGUI, KC_LALT, MO(2), KC_SPC, KC_RGUI, KC_ENT, KC_RALT, MO(2), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [1] = LAYOUT_JP(
-        _______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______, _______, _______, _______,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
-
-    [2] = LAYOUT_JP(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, _______,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, _______, _______, _______, _______,
@@ -45,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
 
-    [3] = LAYOUT_JP(
+    [2] = LAYOUT_JP(
         KC_GRV, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, _______,
         KC_TAB, _______, _______, O_MINS, O_PLUS, O_EQL, _______, KC_1, KC_2, KC_3, KC_P, _______, _______,
         _______, O_LCBR, O_RCBR, O_LPRN, O_RPRN, _______, _______, KC_4, KC_5, KC_6, _______, _______, _______, _______,
