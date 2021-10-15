c912c7a913673429f6f62b8158c58d74ca57c8d1
Shay Merrill <shaymdev@gmail.com>
2021-10-15 12:20:10 -0600

    Just some mouse settings thoughts commented out

--- keyboards/santoku/keymaps/shaymdev/config.h
+++ keyboards/santoku/keymaps/shaymdev/config.h
@@ -13,3 +13,8 @@
 
 #undef PS2_MOUSE_SCROLL_BTN_MASK  //shaymdev: maybe set his to middle (default?) possible good info here: https://beta.docs.qmk.fm/using-qmk/hardware-features/feature_ps2_mouse
 #define PS2_MOUSE_SCROLL_MASK 0x0F
+
+//#define PS2_MOUSE_SCROLL_BTN_SEND 500 /* Default is 300 */ //shaymdev: might want to turn this back on with scroll_btn_mask
+
+// Shaymdev: I think I might want this one enabled... play with that.... maybe I don't: https://github.com/qmk/qmk_firmware/issues/10957
+// #define PS2_MOUSE_ENABLE_SCROLLING
