8564b7d45106dc137255e06943ed79e95d8be4e3
npspears <40127181+npspears@users.noreply.github.com>
2021-10-14 19:11:07 -0500

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/checkerboards/phoenix45_ortho/phoenix45_ortho.c
+++ keyboards/checkerboards/phoenix45_ortho/phoenix45_ortho.c
@@ -15,3 +15,15 @@
  */
 
 #include "phoenix45_ortho.h"
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    if (index == 0) {
+        if (clockwise) {
+            tap_code_delay(KC_VOLD, 10);
+        } else {
+            tap_code_delay(KC_VOLU, 10);
+        }
+    }
+    return true;
+}
