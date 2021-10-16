9a0addc77e3e48b908b846fc78326d4a20c30bc9
Drashna Jaelre <drashna@live.com>
2021-10-15 22:30:23 -0700

    [Keymap] Drashna Keymap Updates (#14842)
    * Fix issues with user split transport code
    * Improve OLED (More font stuff, improved keylogger, etc)
    * Add `KEYLOCK` macro to disable USB, borrowed from command feature.
    * Convert Kyria fully to proton C (no more AVR kyria)
    * Add Work Louder Work Board keymap

--- keyboards/work_louder/work_board/keymaps/drashna/config.h
+++ keyboards/work_louder/work_board/keymaps/drashna/config.h
@@ -1,4 +1,3 @@
-
 /* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,21 +15,3 @@
  */
 
 #pragma once
-
-#include "drashna.h"
-
-typedef union {
-    uint32_t raw;
-    struct {
-        bool audio_enable         :1;
-        bool audio_clicky_enable  :1;
-        bool tap_toggling         :1;
-        bool unicode_mode         :1;
-        bool swap_hands           :1;
-        bool host_driver_disabled :1;
-    };
-} user_runtime_config_t;
-
-extern user_runtime_config_t user_state;
-
-void keyboard_post_init_transport_sync(void);
