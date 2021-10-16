c242b3fb38fd6251deacfbdd7056d60c32a25a3f
Klesh Wong <klesh@klesh-pc.localdomain>
2021-10-16 18:34:54 +0800

    [Keyboard] Add handwired plack 40% keyboard

---
+++ keyboards/handwired/plack/config.h
@@ -0,0 +1,32 @@
+ /* Copyright 2020 Imam Rafii
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x3062
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    "kleshwong"
+#define PRODUCT         "Plack"
+
+
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 1
---
+++ keyboards/handwired/plack/rules.mk
@@ -0,0 +1,9 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+
+DEFAULT_FOLDER = handwired/plack/blackpill_f401
