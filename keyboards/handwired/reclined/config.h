ec5c7059c44d15391e01a8aa372f46e64f07947a
Daniel Perrett <perrettdl@googlemail.com>
2021-10-17 21:31:39 +0100

    Remove or comment out lines which info.json makes redundant in handwired/reclined

--- keyboards/handwired/reclined/config.h
+++ keyboards/handwired/reclined/config.h
@@ -19,13 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    pdl
-#define PRODUCT         reclined
-
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
@@ -33,25 +26,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Keyboard Matrix Assignments
  *
- * Change this to how you wired your keyboard
+ * Change this to how you wired your keyboard (to overwrite info.json)
  * COLS: AVR pins used for columns, left to right
  * ROWS: AVR pins used for rows, top to bottom
  * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D3, C6, B6, B5 }
-#define MATRIX_COL_PINS { B2, F4, B3, F5, B1, F6, D4, D7, D0, E6, D1, B4 }
-#define UNUSED_PINS
+// #define MATRIX_ROW_PINS { D3, C6, B6, B5 }
+// #define MATRIX_COL_PINS { B2, F4, B3, F5, B1, F6, D4, D7, D0, E6, D1, B4 }
+// #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
-#define DIODE_DIRECTION ROW2COL
+// #define DIODE_DIRECTION ROW2COL
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
+// #define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
+// #define MATRIX_HAS_GHOST
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
