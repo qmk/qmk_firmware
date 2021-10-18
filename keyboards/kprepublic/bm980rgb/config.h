21b1e5a40b56a380bc8ed869e912b489acd4764c
peepeetee <peepeetee@protonmail.com>
2021-10-18 20:42:43 +0800

    rename to bm980rgb

--- keyboards/kprepublic/bm980rgb/config.h
+++ keyboards/kprepublic/bm980rgb/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define PRODUCT_ID   0xEF61
 // #define DEVICE_VER   0x0001
 // #define MANUFACTURER KPrepublic
-#define PRODUCT      bm980
+#define PRODUCT      bm980rgb
 
 /* key matrix size */
 // #define MATRIX_ROWS 6
