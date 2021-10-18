c6931a786bd0d6783330dfea528b40fb5c5bf311
Ryan <fauxpark@gmail.com>
2021-10-18 13:11:57 +1100

    Update platforms/avr/drivers/uart.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- platforms/avr/drivers/uart.c
+++ platforms/avr/drivers/uart.c
@@ -28,7 +28,6 @@
 #include <avr/interrupt.h>
 
 #include "uart.h"
-#include "progmem.h"
 
 #if defined(__AVR_AT90USB162__) || defined(__AVR_ATmega16U2__) || defined(__AVR_ATmega32U2__) || defined(__AVR_ATmega16U4__) || defined(__AVR_ATmega32U4__) || defined(__AVR_AT90USB646__) || defined(__AVR_AT90USB647__) || defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB1287__)
 #    define UDRn UDR1
