b9464bc81ff736ecc404e90f2aca3cb63c741038
Klesh Wong <klesh@qq.com>
2021-10-18 12:03:49 +0800

    [Keyboard] Fix BROWSER BACK not working

--- keyboards/handwired/plack/rules.mk
+++ keyboards/handwired/plack/rules.mk
@@ -3,6 +3,7 @@
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
