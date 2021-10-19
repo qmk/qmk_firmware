6d224e046ddd233951765e6f5dca0eb06f7bf29d
rmkb <75864254+twingofan@users.noreply.github.com>
2021-10-19 03:42:00 -0500

    Split l/r ctrl on left half

--- keyboards/kyria/keymaps/twingofan/keymap.c
+++ keyboards/kyria/keymaps/twingofan/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_MPLY, KC_Q, KC_W, KC_E, KC_R, KC_T,                                                                KC_Y, KC_U, KC_I, KC_O, KC_P, TD(TD_BRACKETS),
        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,                                                                KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL, _______,                              _______, KC_ESC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
-                           KC_LALT, KC_LGUI, KC_LCTL, KC_SPC, LT(_NAV,KC_TAB), LT(_FUNC,KC_ENT), KC_BSPC, KC_RSFT, KC_RALT, KC_APP
+                           KC_LALT, KC_LGUI, KC_RCTL, KC_SPC, LT(_NAV,KC_TAB), LT(_FUNC,KC_ENT), KC_BSPC, KC_RSFT, KC_RALT, KC_APP
      ),
 
 /*
