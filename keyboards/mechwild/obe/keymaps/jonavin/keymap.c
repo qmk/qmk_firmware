a9fd4ae1bcd0b55d9bf449fbaa44b7d6586a9a84
Jonavin <=>
2021-10-19 19:56:11 -0400

    adjust TAPPING_TERM_PER_KEY for LT(,SPC) and SFTUP

--- keyboards/mechwild/obe/keymaps/jonavin/keymap.c
+++ keyboards/mechwild/obe/keymaps/jonavin/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
     KC_PGDN, TT(_RAISE), KC_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFTCAPSWIN,KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LT(_LOWER,KC_B), KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SFTUP,KC_RSFT,
-             KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  MO(_FN1),  LT(_RAISE,KC_SPC),       KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+             KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,  MO(_FN1),         KC_RAISESPC,      KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN1] = LAYOUT(
     KC_NO,   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_INS,
