30c279004458e8af2ca9f31ede54197d92772880
swiftrax <swiftrax@github.com>
2021-10-21 21:40:47 -0700

    change PID

--- keyboards/handwired/swiftrax/bumblebee/config.h
+++ keyboards/handwired/swiftrax/bumblebee/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x04D8
-#define PRODUCT_ID      0xE964
+#define PRODUCT_ID      0xE881
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Swiftrax
 #define PRODUCT         BumbleBee
--- keyboards/handwired/swiftrax/bumblebee/keymaps/default/keymap.c
+++ keyboards/handwired/swiftrax/bumblebee/keymaps/default/keymap.c
@@ -16,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_default(
         KC_ESC , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL , 
