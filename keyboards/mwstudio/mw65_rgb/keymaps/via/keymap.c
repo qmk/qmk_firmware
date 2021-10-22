2aea5fc6b54333380f787498c25a985e6cd9764e
TW59420 <40019210+TW59420@users.noreply.github.com>
2021-10-22 20:34:12 +0800

    Update keymap.c

--- keyboards/mwstudio/mw65_rgb/keymaps/via/keymap.c
+++ keyboards/mwstudio/mw65_rgb/keymaps/via/keymap.c
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, RGB_HUI,
         RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_F23,  RGB_SAI,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F24,  RGB_VAI,  RGB_SAD,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          USER00,  RGB_SAI,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, USER01,  RGB_VAI,  RGB_SAD,
         _______, _______, _______, _______, _______,          _______,                            RGB_MOD, _______, RGB_SPD, RGB_VAD, RGB_SPI
     ),
 
@@ -68,7 +68,7 @@ void keyboard_post_init_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case KC_F23:
+    case USER00:
       if (record->event.pressed) {
         // Do something when pressed
         user_config.top_rgb_change ^= 1; // Toggles the status
@@ -77,7 +77,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // Do something else when release
       }
       return false; // Skip all further processing of this key
-    case KC_F24:
+    case USER01:
       if (record->event.pressed) {
         // Do something when pressed
         user_config.bottom_rgb_change ^= 1; // Toggles the status
