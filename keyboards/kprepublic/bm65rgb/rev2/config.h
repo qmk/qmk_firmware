de45aaf706cd1c108d28ca59baf9ce89d0a38b38
peepeetee <peepeetee@protonmail.com>
2021-10-23 07:42:41 +0800

    moved rgb config to rev1, change version number for rev2

--- keyboards/kprepublic/bm65rgb/rev2/config.h
+++ keyboards/kprepublic/bm65rgb/rev2/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0x4B50 //KP
 #define PRODUCT_ID   0xEF6E
-#define DEVICE_VER   0x0001
+#define DEVICE_VER   0x0002
 #define MANUFACTURER KPRepublic
 #define PRODUCT      BM65 RGB
 
