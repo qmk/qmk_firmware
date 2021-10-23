7216fce2cda8ff4f78653564844b4487c608849a
Gianluca D'Abrosca <63099818+dbrglc@users.noreply.github.com>
2021-10-23 19:19:08 +0200

    Update keyboards/lily58/keymaps/bongocat_wpm_responsive/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lily58/keymaps/bongocat_wpm_responsive/config.h
+++ keyboards/lily58/keymaps/bongocat_wpm_responsive/config.h
@@ -38,11 +38,3 @@
  #define RGBLIGHT_HUE_STEP 10
  #define RGBLIGHT_SAT_STEP 17
  #define RGBLIGHT_VAL_STEP 17
-
- // Underglow
- /*
- #undef RGBLED_NUM
- #define RGBLED_NUM 14    // Number of LEDs
- #define RGBLIGHT_ANIMATIONS
- #define RGBLIGHT_SLEEP
- */
