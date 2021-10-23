6844b2f6592f72a288d8b4c73d288d0c2ecb83f9
peepeetee <peepeetee@protonmail.com>
2021-10-23 08:07:54 +0800

    apply code review from #13361

--- keyboards/kprepublic/bm65rgb/rev2/rules.mk
+++ keyboards/kprepublic/bm65rgb/rev2/rules.mk
@@ -18,10 +18,9 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
 
-LAYOUTS = 65_ansi
+LAYOUTS = 65_ansi_blocker
