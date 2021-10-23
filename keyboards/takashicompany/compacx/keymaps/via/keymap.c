b06b1b5fec6190f18f61ddb9a7f75de6753c6bd9
takashicompany <t@kashi.company>
2021-10-24 01:50:59 +0900

    Update keyboards/takashicompany/compacx/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/takashicompany/compacx/keymaps/via/keymap.c
+++ keyboards/takashicompany/compacx/keymaps/via/keymap.c
@@ -66,24 +66,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    switch (index) {
-        case 1:
-            if (!clockwise) {
-                tap_code(KC_MS_WH_UP);
-            } else {
-                tap_code(KC_MS_WH_DOWN);
-            }
-            break;
-
-        default:
-            if (clockwise) {
-                tap_code(KC_MS_WH_UP);
-            } else {
-                tap_code(KC_MS_WH_DOWN);
-            }
-            break;
-    }
-
-    return true;
-}
