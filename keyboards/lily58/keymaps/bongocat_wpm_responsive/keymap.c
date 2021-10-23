82810882c80ed4797edc4b215dfd4a9907dc192a
Gianluca D'Abrosca <63099818+dbrglc@users.noreply.github.com>
2021-10-23 19:25:28 +0200

    Update keyboards/lily58/keymaps/bongocat_wpm_responsive/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lily58/keymaps/bongocat_wpm_responsive/keymap.c
+++ keyboards/lily58/keymaps/bongocat_wpm_responsive/keymap.c
@@ -1,14 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#ifdef PROTOCOL_LUFA
-  #include "lufa.h"
-  #include "split_util.h"
-#endif
-#ifdef SSD1306OLED
-  #include "ssd1306.h"
-#endif
-
-extern uint8_t is_master;
 
 #define _QWERTY 0
 #define _LOWER 1
