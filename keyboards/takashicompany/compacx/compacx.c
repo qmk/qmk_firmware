42ae7828dd6837de6ee57ac832c27e77f1da9866
takashicompany <t@kashi.company>
2021-10-24 01:51:22 +0900

    Update keyboards/takashicompany/compacx/compacx.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/takashicompany/compacx/compacx.c
+++ keyboards/takashicompany/compacx/compacx.c
@@ -15,3 +15,14 @@
  */
 
 #include "compacx.h"
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    if (!clockwise) {
+        tap_code_delay(KC_MS_WH_UP);
+    } else {
+        tap_code_delay(KC_MS_WH_DOWN);
+    }
+
+    return true;
+}
