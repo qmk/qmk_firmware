8491ac650033483751ecaee3df38a238a7cad842
binepad <69730862+binepad@users.noreply.github.com>
2021-10-23 12:30:07 +0530

    Update keyboards/binepad/bn009/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/binepad/bn009/keymaps/default/keymap.c
+++ keyboards/binepad/bn009/keymaps/default/keymap.c
@@ -20,23 +20,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [0] = LAYOUT( \
-  KC_7,       KC_8,      KC_9,       \
-  KC_4,       KC_5,      KC_6,       \
-  KC_1,       KC_2,      KC_3        \
+  KC_7,    KC_8,    KC_9,
+  KC_4,    KC_5,    KC_6,
+  KC_1,    KC_2,    KC_3
 ),
 
 	
 [1] = LAYOUT( \
-  KC_NO,   KC_NO, KC_NO, \
-  KC_NO,   KC_NO, KC_NO, \
-  KC_NO,   KC_NO, KC_NO  \
+  KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO
 ),
 
 	
 [2] = LAYOUT( \
-  KC_NO,   KC_NO, KC_NO, \
-  KC_NO,   KC_NO, KC_NO, \
-  KC_NO,   KC_NO, KC_NO  \
+  KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO,
+  KC_NO,   KC_NO,   KC_NO
 )
 
 };
