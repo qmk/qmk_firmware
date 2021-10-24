7b612f91cfeddb5f8f23bdb612946d76126d3df7
takashicompany <t@kashi.company>
2021-10-24 12:16:32 +0900

    Update keyboards/takashicompany/compacx/compacx.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/takashicompany/compacx/compacx.c
+++ keyboards/takashicompany/compacx/compacx.c
@@ -19,9 +19,9 @@
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
     if (!clockwise) {
-        tap_code_delay(KC_MS_WH_UP);
+        tap_code(KC_MS_WH_UP);
     } else {
-        tap_code_delay(KC_MS_WH_DOWN);
+        tap_code(KC_MS_WH_DOWN);
     }
 
     return true;
