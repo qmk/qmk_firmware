22913eb0b9b56346b61c54491726db50efab677c
Mo <mhijazi1@github.com>
2021-10-25 02:24:08 +0000

    fix typo

--- keyboards/ergodox_ez/keymaps/mhijazi1/keymap.c
+++ keyboards/ergodox_ez/keymaps/mhijazi1/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   KC_ESC,          KC_1,        KC_2,          KC_3,    KC_4,             KC_5,    KC_LBRC,                     KC_RBRC,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_MINS,
   KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,             KC_T,    KC_LPRN,                     KC_RPRN,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_BSLS,
-  KC_HOME,         ALT_T(KC_A), KC_S,          KC_D,    LT(SYMB,K_F),     KC_G,                                               KC_H,    KC_J,    KC_K,    KC_L,    ALT_T(KC_SCLN),    KC_QUOT,
+  KC_HOME,         ALT_T(KC_A), KC_S,          KC_D,    LT(SYMB, KC_F),   KC_G,                                               KC_H,    KC_J,    KC_K,    KC_L,    ALT_T(KC_SCLN),    KC_QUOT,
   KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,             KC_B,    KC_LCBR,                     KC_RCBR,      KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    KC_RSFT,
   LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                                                       KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,           KC_EQL,
                                                                           ALT_T(KC_APP), KC_LEAD,             KC_PGUP, KC_PGDN,
