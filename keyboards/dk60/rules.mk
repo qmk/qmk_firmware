028edfc2df5bc291773e8c7cc19ed38c9b9470dc
Drashna Jaelre <drashna@live.com>
2021-10-25 21:14:46 -0700

    Remove unused KEYBOARD_LOCK_ENABLE from rules (#14920)

--- keyboards/dk60/rules.mk
+++ keyboards/dk60/rules.mk
@@ -19,5 +19,4 @@ NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-KEYBOARD_LOCK_ENABLE = yes
 UNICODE_ENABLE = yes
