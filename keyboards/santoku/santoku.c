07631a5477c44de90bfe238538fcef6a0d8da569
Shay Merrill <shaymdev@gmail.com>
2021-10-27 10:57:46 -0600

    Update keyboards/santoku/santoku.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/santoku/santoku.c
+++ keyboards/santoku/santoku.c
@@ -17,6 +17,7 @@
 #include "santoku.h"
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
 	if (clockwise) {
 		tap_code(KC_WH_U);
 	} else {
