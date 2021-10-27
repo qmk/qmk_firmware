a09ac5ee945b635bf087e2441c6ffab6a438998b
Shay Merrill <shaymdev@gmail.com>
2021-10-26 18:53:06 -0600

    Keyboard level oled task

--- keyboards/santoku/santoku.c
+++ keyboards/santoku/santoku.c
@@ -24,3 +24,65 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
 	}
 	return true;
 }
+
+#ifdef OLED_ENABLE
+__attribute__((weak)) void oled_task_user(void) {
+	// Vanity Text
+	static bool show_vanity_text = true;
+	if (show_vanity_text) {
+		oled_write_ln_P(PSTR("  Santoku Keyboard"), false);
+		oled_write_ln_P(PSTR("       by Tye"), false);
+		oled_write_ln_P(PSTR(""), false);
+		oled_write_ln_P(PSTR("    Hello, world."), false);
+		if (timer_read() > 7500) {
+			show_vanity_text = false;
+		}
+	} else {
+	#ifdef WPM_ENABLE
+			uint8_t n = get_current_wpm();
+			char    wpm_counter[4];
+			wpm_counter[3] = '\0';
+			wpm_counter[2] = '0' + n % 10;
+			wpm_counter[1] = (n /= 10) % 10 ? '0' + (n) % 10 : (n / 10) % 10 ? '0' : ' ';
+			wpm_counter[0] = n / 10 ? '0' + n / 10 : ' ';
+			oled_write_P(PSTR("WPM:"), false);
+			oled_write(wpm_counter, false);
+	#endif
+
+	}
+
+	// Host Keyboard Layer Status
+	switch (get_highest_layer(layer_state)) {
+		case 0:
+			oled_write_P(PSTR("QWERTY\n"), false);
+			oled_write_ln_P(PSTR(""), false);
+			oled_write_ln_P(PSTR("TB  qwert | yuiop\\"), false);
+			oled_write_ln_P(PSTR("ES  asdfg | hjkl;'"), false);
+			oled_write_ln_P(PSTR("SH  zxcvb | nm,./"), false);
+			break;
+		case 1:
+			oled_write_P(PSTR("   Symbol   \n"), true);
+			oled_write_ln_P(PSTR(""), false);
+			oled_write_ln_P(PSTR(" `  !@#$% | ^&*()-"), false);
+			oled_write_ln_P(PSTR("ES  12345 | 67890="), false);
+			oled_write_ln_P(PSTR("SH  \\_+{} | [],./"), false);
+			break;
+		case 2:
+			oled_write_P(PSTR(" Navigation \n"), true);
+			oled_write_ln_P(PSTR(""), false);
+			oled_write_ln_P(PSTR("   | HM PD PU EN NT"), false);
+			oled_write_ln_P(PSTR("   | << vv ^^ >>"), false);
+			oled_write_ln_P(PSTR("ov | D[ D] D+ D-"), false);
+			break;
+		case 3:
+			oled_write_P(PSTR("  Function  \n"), true);
+			oled_write_ln_P(PSTR(""), false);
+			oled_write_ln_P(PSTR("          |"), false);
+			oled_write_ln_P(PSTR("ES F12345 | 67890"), false);
+			oled_write_ln_P(PSTR("CP F      | ab"), false);
+			break;
+		default:
+			oled_write_ln_P(PSTR("Undefined"), false);
+	}
+}
+#endif
