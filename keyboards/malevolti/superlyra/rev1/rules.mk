962bcc97c85b288c50943f8b7b295f96fd599706
Domanic Calleja <82970232+Malevolti@users.noreply.github.com>
2021-10-27 09:38:02 +0100

    Update rules.mk

--- keyboards/malevolti/superlyra/rev1/rules.mk
+++ keyboards/malevolti/superlyra/rev1/rules.mk
@@ -20,7 +20,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-SPLIT_KEYBOARD = no
-OLED_ENABLE = no
-WPM_ENABLE = no
 LTO_ENABLE = yes 
