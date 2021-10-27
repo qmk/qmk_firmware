72f007549c807eb6747df720d85948c87e2095af
Domanic Calleja <82970232+Malevolti@users.noreply.github.com>
2021-10-27 09:34:18 +0100

    Update keyboards/malevolti/lyra/rev1/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/malevolti/lyra/rev1/rules.mk
+++ keyboards/malevolti/lyra/rev1/rules.mk
@@ -21,6 +21,4 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
 SPLIT_KEYBOARD = yes
-OLED_ENABLE = no
-WPM_ENABLE = no
 LTO_ENABLE = yes 
