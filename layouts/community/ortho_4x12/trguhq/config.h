65df85e2946a1d910d8d93c2b037326254017c48
trguhq <93248917+trguhq@users.noreply.github.com>
2021-10-27 03:41:41 -0700

    Date and typo

--- layouts/community/ortho_4x12/trguhq/config.h
+++ layouts/community/ortho_4x12/trguhq/config.h
@@ -1,6 +1,6 @@
 /* Ortho 4x12 Unicode Rune Layout
  *
- * Copyright 2020 trguhq
+ * Copyright 2021 trguhq
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
