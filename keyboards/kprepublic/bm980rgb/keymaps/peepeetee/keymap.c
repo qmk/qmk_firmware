27ca389174c4eb80be9e4e8a66d429b0bb3e7889
peepeetee <peepeetee@protonmail.com>
2021-10-27 20:37:20 +0800

    change peepeetee keymap

--- keyboards/kprepublic/bm980rgb/keymaps/peepeetee/keymap.c
+++ keyboards/kprepublic/bm980rgb/keymaps/peepeetee/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	  KC_CAPS,          KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,               KC_P4,    KC_P5,    KC_P6,
 	  KC_LSFT,              KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,                    KC_P1,    KC_P2,    KC_P3,  KC_PENT,
                                                                                                                                                   KC_UP,
-	  KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,                                                   KC_RALT,  MO(1),     KC_RCTL,                                     KC_P0,    KC_PDOT,
+	  KC_LCTL,    KC_LALT,    KC_LGUI,    KC_SPC,                                                   KC_RALT,  MO(1),     KC_RCTL,                                     KC_P0,    KC_PDOT,
                                                                                                                                         KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
