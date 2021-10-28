c64f8fb5a3c3987d60e9a89f6406d8d5747a03a4
zvecr <git@zvecr.com>
2021-10-28 02:38:26 +0100

    Remove SERIAL_MOUSE

--- tmk_core/protocol.mk
+++ tmk_core/protocol.mk
@@ -1,25 +1,5 @@
 PROTOCOL_DIR = protocol
 
-ifeq ($(strip $(SERIAL_MOUSE_MICROSOFT_ENABLE)), yes)
-    SRC += $(PROTOCOL_DIR)/serial_mouse_microsoft.c
-    OPT_DEFS += -DSERIAL_MOUSE_ENABLE -DSERIAL_MOUSE_MICROSOFT \
-                -DMOUSE_ENABLE
-endif
-
-ifeq ($(strip $(SERIAL_MOUSE_MOUSESYSTEMS_ENABLE)), yes)
-    SRC += $(PROTOCOL_DIR)/serial_mouse_mousesystems.c
-    OPT_DEFS += -DSERIAL_MOUSE_ENABLE -DSERIAL_MOUSE_MOUSESYSTEMS \
-                -DMOUSE_ENABLE
-endif
-
-ifeq ($(strip $(SERIAL_MOUSE_USE_SOFT)), yes)
-    SRC += $(PROTOCOL_DIR)/serial_soft.c
-endif
-
-ifeq ($(strip $(SERIAL_MOUSE_USE_UART)), yes)
-    SRC += $(PROTOCOL_DIR)/serial_uart.c
-endif
-
 ifeq ($(strip $(USB_HID_ENABLE)), yes)
     include $(TMK_DIR)/protocol/usb_hid.mk
 endif
