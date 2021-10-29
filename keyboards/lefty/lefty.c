680af555099dd9b0527a0849ec88b9119a1f77ab
Drashna Jael're <drashna@live.com>
2021-10-29 15:40:28 -0700

    update lefty

--- keyboards/lefty/lefty.c
+++ keyboards/lefty/lefty.c
@@ -17,7 +17,10 @@
 #include "lefty.h"
 
 #ifdef OLED_ENABLE
-__attribute__((weak)) void oled_task_user(void) {
+bool oled_task_kb(void) {
+    if (!oled_task_user()) {
+        return false;
+    }
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
