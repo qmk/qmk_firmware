ca25a2ad99667320af81601796f10c04b676fe44
TW59420 <40019210+TW59420@users.noreply.github.com>
2021-10-29 12:47:38 +0800

    Update rules.mk

--- keyboards/mwstudio/mw65_r2/rules.mk
+++ keyboards/mwstudio/mw65_r2/rules.mk
@@ -17,8 +17,6 @@ SLEEP_LED_ENABLE = no      # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+AUDIO_ENABLE = yes           # Audio output
 ENCODER_ENABLE = yes
