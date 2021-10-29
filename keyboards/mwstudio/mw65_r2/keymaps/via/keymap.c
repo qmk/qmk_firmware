ad98b5e2600e0bbcf64537cc9aafb9ccd9dc13ce
TW59420 <40019210+TW59420@users.noreply.github.com>
2021-10-29 13:08:39 +0800

    Update keymap.c

--- keyboards/mwstudio/mw65_r2/keymaps/via/keymap.c
+++ keyboards/mwstudio/mw65_r2/keymaps/via/keymap.c
@@ -16,22 +16,6 @@
 
 #include QMK_KEYBOARD_H
 
-#undef LAYOUT
-#define LAYOUT( \
-	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, K014, \
-	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, K114, \
-	K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212,       K214, \
-	K300,       K302, K303, K304, K305, K306, K307, K308, K309, K310, K311, K312, K313, K314, \
-	K400, K401, K402, K403, K404,       K406,                   K410, K411, K412, K413, K414  \
-) { \
-	{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011,  K012,  K013,  K014 }, \
-	{ K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111,  K112,  K113,  K114 }, \
-	{ K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211,  K212,  KC_NO,  K214 }, \
-	{ K300,  KC_NO,  K302,  K303,  K304,  K305,  K306,  K307,  K308,  K309,  K310,  K311,  K312,  K313,  K314 }, \
-	{ K400,  K401,  K402,  K403,  K404,  KC_NO,  K406,  KC_NO,  KC_NO,  KC_NO,  K410,  K411,  K412,  K413,  K414 }  \
-	}
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT(
@@ -70,12 +54,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef ENCODER_ENABLE
 #define ENCODERS 1
 static uint8_t  encoder_state[ENCODERS] = {0};
-static keypos_t encoder_cw[ENCODERS]    = {3, 4};
-static keypos_t encoder_ccw[ENCODERS]   = {4, 4};
+static keypos_t encoder_cw[ENCODERS] = {{3, 4}};
+static keypos_t encoder_ccw[ENCODERS] = {{4, 4}};
 
 
 void encoder_action_unregister(void) {
-#    ifdef ENCODERS
+#ifdef ENCODERS
     for (uint8_t index = 0; index < ENCODERS; ++index) {
         if (encoder_state[index]) {
             keyevent_t encoder_event = (keyevent_t) {
@@ -87,7 +71,7 @@ void encoder_action_unregister(void) {
             action_exec(encoder_event);
         }
     }
-#    endif
+#endif
 }
 
 void encoder_action_register(uint8_t index, bool clockwise) {
