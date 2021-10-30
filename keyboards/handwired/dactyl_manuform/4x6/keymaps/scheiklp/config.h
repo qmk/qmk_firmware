aa8371d94427279dff13d67daffaf1aa4f6b93cc
Paul Maria Scheikl <paul.scheikl@kit.edu>
2021-10-30 23:06:38 +0200

    Fixed error where handedness was not correctly determined because of combining vbus pins of both controllers.

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/scheiklp/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/scheiklp/config.h
@@ -20,19 +20,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define USE_I2C
-
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
-#define EE_HANDS
-// Rows are doubled-up
+//
+// VBUS is used to power the LEDs without going "through" the MCU -> detecting handedness cannot rely on VBUS
+// From the QMK documentation: Enabling this option changes the startup behavior to listen for an active USB communication to delegate which part is master and which is slave. With this option enabled and theres's USB communication, then that half assumes it is the master, otherwise it assumes it is the slave.
+#define SPLIT_USB_DETECT
 
 // WS2812 RGB LED strip input and number of LEDs
 #undef RGB_DI_PIN
 #undef RGBLED_NUM
 #define RGB_DI_PIN F4
 #define RGBLED_NUM 52
+#define RGBLED_SPLIT {26, 26}
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_SNAKE
 
-#define AUTO_SHIFT_TIMEOUT 125
+// Amoeba royale PCBs (https://github.com/mtl/keyboard-pcbs/tree/master/amoeba-royale) use row to column layout (current can flow from row pin to column pin).
+#undef DIODE_DIRECTION
+#define DIODE_DIRECTION ROW2COL
+
+#define DEBOUNCE 5
+
+#define AUTO_SHIFT_TIMEOUT 100
 #define NO_AUTO_SHIFT_SPECIAL
 #define NO_AUTO_SHIFT_NUMERIC
 
