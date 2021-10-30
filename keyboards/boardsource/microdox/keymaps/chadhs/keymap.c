5cd22a46baca88541015df4afac0c953884f6241
Chad Stovern <chad@stovern.me>
2021-10-30 14:47:05 -0500

    update gaming layer mods

--- keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
+++ keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   KC_ESC,  KC_A, KC_S,    KC_D,    KC_F,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
   KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,        KC_N,    KC_M,    KC_COMM, KC_DOT,  GAMING,
-                 XXXXXXX, KC_LCMD, KC_SPC,      G2_SPC,  KC_LCTL, XXXXXXX
+                 XXXXXXX, KC_LCTL, KC_SPC,      G2_SPC,  KC_LOPT, XXXXXXX
 ),
 
 [_GAMING_2] = LAYOUT_split_3x5_3(
