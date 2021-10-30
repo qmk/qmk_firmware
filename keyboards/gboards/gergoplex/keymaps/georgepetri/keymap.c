2b0f42a180fc08e8cec1f7925def374648052e6e
George Petri <t8_8t@icloud.com>
2021-10-30 13:08:04 +0300

    start work on gaming layer

--- keyboards/gboards/gergoplex/keymaps/georgepetri/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/georgepetri/keymap.c
@@ -25,13 +25,14 @@
 enum {
     _ALPHA,   // default
     _SPECIAL, // special characters
-    _NUMBERS  // numbers/function/motion
+    _NUMBERS, // numbers/function/motion
+    _GAME     // for gaming
 };
 
 #define KC_CTL_A  MT(MOD_LCTL, KC_A)     // Tap for A, hold for Control
 #define KC_SFT_Z  MT(MOD_RSFT, KC_Z)     // Tap for Z, hold for Shift
 #define KC_SFT_SL MT(MOD_RSFT, KC_SLSH)  // Tap for slash, hold for Shift
-
+//todo remove this
 #define KC_ALT_ENT MT(MOD_LALT, KC_ENT)  // Tap for Enter, hold for Alt (Option)
 #define KC_SPE_SPC LT(_SPECIAL, KC_SPC)  // Tap for Space, hold for Special layer
 #define KC_NUM_SPC LT(_NUMBERS, KC_SPC)  // Tap for Space, hold for Numbers layer
@@ -106,5 +107,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_1,  KC_2,  KC_3,  KC_4,  KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
          KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
          KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
-                    KC_F11, KC_F12, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS)
+                    KC_F11, KC_F12, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS),
+
+    /* Keymap 3: Gaming layer
+     *
+     * ,-------------------------------.      ,-------------------------------.
+     * |     Q |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |  O  |   P   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * | CTRL A|  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |  L  |   ;   |
+     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
+     * |  SHFT |  Z  |  X  |  C  |  V  |      |  N  |  M  |  ,  |  .  |SHFT / |
+     * `-------------------------------'      `-------------------------------'
+     *       .----------------------.           .----------------------.
+     *       | META | ENT ALT | SPC |           | SPC NUM | TAB | SHFT |
+     *       '----------------------'           '----------------------'
+     */
+    [_GAME] = LAYOUT_split_3x5_3(
+         KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,     KC_O,   KC_P,
+         KC_CTL_A, KC_S,   KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,     KC_L,   KC_SCLN,
+         KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,          KC_N,   KC_M,   KC_COMMA, KC_DOT, KC_SFT_SL,
+                    KC_LGUI, KC_ALT_ENT, KC_SPC,          KC_NUM_SPC, KC_TAB, KC_RSFT)
 };
