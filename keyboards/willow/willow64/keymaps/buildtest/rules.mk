48b069079992cb9dab0d55eba4243f0b0c57a40a
hanachi-ap <75919318+hanachi-ap@users.noreply.github.com>
2021-10-31 10:25:13 +0900

    Update keyboards/willow/willow64/keymaps/buildtest/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/willow/willow64/keymaps/buildtest/rules.mk
+++ keyboards/willow/willow64/keymaps/buildtest/rules.mk
@@ -1,4 +1,1 @@
-
-CONSOLE_ENABLE = yes       # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-RGBLIGHT_ENABLE = yes      # Enable WS2812 RGB underlight.
+CONSOLE_ENABLE = yes
