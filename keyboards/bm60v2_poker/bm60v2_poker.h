cccf229248d6b1d7adf20dead49b36768ae89547
bdtc123 <810997916@qq.com>
2021-11-01 20:57:19 +0800

    1

---
+++ keyboards/bm60v2_poker/bm60v2_poker.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "quantum.h"
+
+#define LAYOUT( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,        \
+    K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      \
+    K20,    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2D,          \
+    K30,       K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A,        K3D,     \
+    K40,  K41,  K42,              K45,                 K49,  K4A,   K4B,  K4D    \
+) { \
+    { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D   }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D   }, \
+    { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   KC_NO, K2D   }, \
+    { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   KC_NO, KC_NO, K3D   }, \
+    { K40,   K41,   K42,   KC_NO, KC_NO, K45,   KC_NO, KC_NO, KC_NO, K49,   K4A,   K4B,   KC_NO, K4D   }, \
+}
+
+#if defined(RGB_MATRIX_DISABLE_KEYCODES)
+#    ifndef RGB_MATRIX_TOGGLE
+#        define RGB_MATRIX_TOGGLE   KC_F16
+#    endif
+#    ifndef RGB_MATRIX_STEP
+#        define RGB_MATRIX_STEP     KC_F17
+#    endif
+#    ifndef RGB_MATRIX_INC_HUE
+#        define RGB_MATRIX_INC_HUE  KC_F18
+#    endif
+#    ifndef RGB_MATRIX_DEC_HUE
+#        define RGB_MATRIX_DEC_HUE  KC_F19
+#    endif
+#    ifndef RGB_MATRIX_INC_SAT
+#        define RGB_MATRIX_INC_SAT  KC_F20
+#    endif
+#    ifndef RGB_MATRIX_DEC_SAT
+#        define RGB_MATRIX_DEC_SAT  KC_F21
+#    endif
+#    ifndef RGB_MATRIX_INC_VAL
+#        define RGB_MATRIX_INC_VAL  KC_F22
+#    endif
+#    ifndef RGB_MATRIX_DEC_VAL
+#        define RGB_MATRIX_DEC_VAL  KC_F23
+#    endif
+#endif
+
+
+
---
+++ keyboards/bm60v2_poker/config.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x4B50
+#define PRODUCT_ID      0x1122
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    KP REPUBLIC
+#define PRODUCT         BM60V2_poker
+#define DESCRIPTION     BM60V2_poker RGB keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { E6, D2, D3, D5, F6 }
+#define MATRIX_COL_PINS { B2, B3, B7, B0, B1, F7, D4, D6, D7, B4, B5, B6, C6, C7 } 
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL*/
+#define DIODE_DIRECTION ROW2COL
+#define USB_POLLING_INTERVAL_MS 1
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 3
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+//rgb light setting
+#define RGBLIGHT_LIMIT_VAL 150
+#define RGBLED_NUM          6
+#define RGB_DI_PIN          F0
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+#define RGBLIGHT_HUE_STEP   8
+#define RGBLIGHT_SAT_STEP   8
+#define RGBLIGHT_VAL_STEP   8
+
+#    define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#    define RGB_MATRIX_KEYPRESSES
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_ALL
+#    define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    define DISABLE_RGB_MATRIX_BAND_SAT
+#    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#    define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#    define DISABLE_RGB_MATRIX_RAINDROPS
+#    define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#    define DRIVER_ADDR_1 0b1010000
+#    define DRIVER_ADDR_2 0b1010000 // this is here for compliancy reasons.
+#    define DRIVER_COUNT 2
+#    define DRIVER_1_LED_TOTAL 61
+#    define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
+
+#if defined(RGB_MATRIX_ENABLE) && defined(RGBLIGHT_ENABLE)
+    #define RGB_MATRIX_DISABLE_KEYCODES
+#endif
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
+
