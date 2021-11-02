5b170c687fcfa1ab10343f7825ba297bfe271ce0
bdtc123 <93507097+bdtc123@users.noreply.github.com>
2021-11-02 10:33:28 +0800

    Update keyboards/bm60v2_poker/rules.mk
    Co-authored-by: Sergey Vlasov <sigprof@gmail.com>

--- keyboards/bm60v2_poker/rules.mk
+++ keyboards/bm60v2_poker/rules.mk
@@ -30,4 +30,5 @@ BLUETOOTH_ENABLE = no          # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no              # Audio output on port C6
 RGB_MATRIX_ENABLE = yes        # Use RGB matrix
 RGB_MATRIX_DRIVER = IS31FL3733
-NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
+
+LAYOUTS = 60_ansi
