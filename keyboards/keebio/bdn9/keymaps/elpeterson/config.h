782e82768f447e99483997bdc0a850726f62bd03
Eric Peterson <elpeterson16@gmail.com>
2021-11-02 15:31:15 -0400

    fix: resolved issue with older coding and number of LEDs
    fix: resolved issue with older coding and number of LEDs

---
+++ keyboards/keebio/bdn9/keymaps/elpeterson/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "config_common.h"
+
+/* For USB nonsense in various OSs */
+#define TAP_CODE_DELAY 10
+
+#undef RGBLED_NUM
+#define RGBLED_NUM 10
--- keyboards/keebio/bdn9/keymaps/elpeterson/keymap.c
+++ keyboards/keebio/bdn9/keymaps/elpeterson/keymap.c
@@ -1,3 +1,18 @@
+/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
 
 enum layer {
@@ -5,15 +20,32 @@ enum layer {
     LYR1,
 };
 
+enum encoder_names {
+  _LEFT,
+  _RIGHT,
+};
+
 /* Toggle between layers */
 #define LYRTOG TG(LYR1)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+        | Knob 1: Vol Dn/Up |              | Knob 2: Page Dn/Up |
+        | Press: Mute       | Toggle Layer | Press: App View    |
+        | Home              | Up           | End                |
+        | Left              | Down         | Right              |
+     */
     [LYR0] = LAYOUT(
         KC_AUDIO_MUTE, LYRTOG,  G(KC_TAB),
         KC_HOME,       KC_UP,   KC_END,
         KC_LEFT,       KC_DOWN, KC_RGHT
     ),
+    /*
+        | Knob 1: Vol Dn/Up |              | Knob 2: Page Dn/Up |
+        | Press: Mute       | Toggle Layer | Press: App View    |
+        | Hue +             | Sat +        | Brightness +       |
+        | Hue -             | Sat -        | Brightness -       |
+     */
     [LYR1] = LAYOUT(
         RGB_TOG, KC_TRNS, RGB_MOD,
         RGB_HUI, RGB_SAI, RGB_VAI,
@@ -21,18 +53,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  if (index == _LEFT) { /* Left encoder */
     if (clockwise) {
       tap_code(KC_AUDIO_VOL_UP);
     } else {
       tap_code(KC_AUDIO_VOL_DOWN);
     }
-  } else if (index == 1) { /* Second encoder */
+  } else if (index == _RIGHT) { /* Right encoder */
     if (clockwise) {
       tap_code16(G(C(KC_RGHT)));
     } else {
       tap_code16(G(C(KC_LEFT)));
     }
   }
+  return true;
 }
