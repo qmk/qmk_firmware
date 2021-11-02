rjmigliori/qmk_clone

haptic: Feature to disable it when usb port is not configured or suspended.
This also add support for specifying a LED pin to indicate haptic status,
and also adds support for a haptic-enable pin, which is useful to turn off
the boost converter on the solenoid driver.

drivers/haptic/solenoid.c
@@ -18,6 +18,7 @@
 #include <timer.h>
 #include "solenoid.h"
 #include "haptic.h"
+#include "power.h"
 
 bool     solenoid_on      = false;
 bool     solenoid_buzzing = false;
@@ -35,7 +36,7 @@ void solenoid_set_buzz(int buzz) { haptic_set_buzz(buzz); }
 void solenoid_set_dwell(uint8_t dwell) { solenoid_dwell = dwell; }
 
 void solenoid_stop(void) {
-    writePinLow(SOLENOID_PIN);
+    SOLENOID_PIN_WRITE_INACTIVE();
     solenoid_on      = false;
     solenoid_buzzing = false;
 }
@@ -47,7 +48,7 @@ void solenoid_fire(void) {
     solenoid_on      = true;
     solenoid_buzzing = true;
     solenoid_start   = timer_read();
-    writePinHigh(SOLENOID_PIN);
+    SOLENOID_PIN_WRITE_ACTIVE();
 }
 
 void solenoid_check(void) {
@@ -68,20 +69,23 @@ void solenoid_check(void) {
         if ((elapsed % (SOLENOID_BUZZ_ACTUATED + SOLENOID_BUZZ_NONACTUATED)) < SOLENOID_BUZZ_ACTUATED) {
             if (!solenoid_buzzing) {
                 solenoid_buzzing = true;
-                writePinHigh(SOLENOID_PIN);
+                SOLENOID_PIN_WRITE_ACTIVE();
             }
         } else {
             if (solenoid_buzzing) {
                 solenoid_buzzing = false;
-                writePinLow(SOLENOID_PIN);
+                SOLENOID_PIN_WRITE_INACTIVE();
             }
         }
     }
 }
 
 void solenoid_setup(void) {
+    SOLENOID_PIN_WRITE_INACTIVE();
     setPinOutput(SOLENOID_PIN);
-    solenoid_fire();
+    if ((!HAPTIC_OFF_IN_LOW_POWER) || (power_state == POWER_STATE_CONFIGURED)) {
+        solenoid_fire();
+    }
 }
 
-void solenoid_shutdown(void) { writePinLow(SOLENOID_PIN); }
+void solenoid_shutdown(void) { SOLENOID_PIN_WRITE_INACTIVE(); }

drivers/haptic/haptic.c
@@ -18,6 +18,7 @@
 #include "eeconfig.h"
 #include "progmem.h"
 #include "debug.h"
+#include "power.h"
 #ifdef DRV2605L
 #    include "DRV2605L.h"
 #endif
@@ -26,6 +27,30 @@
 #endif
 
 haptic_config_t haptic_config;
+
+static void update_haptic_enable_gpios(void) {
+    if (haptic_config.enable && ((!HAPTIC_OFF_IN_LOW_POWER) || (power_state == POWER_STATE_CONFIGURED))) {
+#if defined(HAPTIC_ENABLE_PIN)
+        HAPTIC_ENABLE_PIN_WRITE_ACTIVE();
+#endif
+#if defined(HAPTIC_ENABLE_STATUS_LED)
+        HAPTIC_ENABLE_STATUS_LED_WRITE_ACTIVE();
+#endif
+    } else {
+#if defined(HAPTIC_ENABLE_PIN)
+        HAPTIC_ENABLE_PIN_WRITE_INACTIVE();
+#endif
+#if defined(HAPTIC_ENABLE_STATUS_LED)
+        HAPTIC_ENABLE_STATUS_LED_WRITE_INACTIVE();
+#endif
+    }
+}
+
+static void set_haptic_config_enable(bool enabled)
+{
+    haptic_config.enable = enabled;
+    update_haptic_enable_gpios();
+}
 
 void haptic_init(void) {
     debug_enable = 1;  // Debug is ON!
@@ -46,6 +71,10 @@ void haptic_init(void) {
         // or the previous firmware didn't have solenoid enabled,
         // and the current one has solenoid enabled.
         haptic_reset();
+    } else {
+        // Haptic configuration has been loaded through the "raw" union item.
+        // This is to execute any side effects of the configuration.
+        set_haptic_config_enable(haptic_config.enable);
     }
 #ifdef SOLENOID_ENABLE
     solenoid_setup();
@@ -56,6 +85,12 @@ void haptic_init(void) {
     dprintf("DRV2605 driver initialized\n");
 #endif
     eeconfig_debug_haptic();
+#ifdef HAPTIC_ENABLE_PIN
+    setPinOutput(HAPTIC_ENABLE_PIN);
+#endif
+#ifdef HAPTIC_ENABLE_STATUS_LED
+    setPinOutput(HAPTIC_ENABLE_STATUS_LED);
+#endif
 }
 
 void haptic_task(void) {
@@ -71,13 +106,13 @@ void eeconfig_debug_haptic(void) {
 }
 
 void haptic_enable(void) {
-    haptic_config.enable = 1;
+    set_haptic_config_enable(true);
     xprintf("haptic_config.enable = %u\n", haptic_config.enable);
     eeconfig_update_haptic(haptic_config.raw);
 }
 
 void haptic_disable(void) {
-    haptic_config.enable = 0;
+    set_haptic_config_enable(false);
     xprintf("haptic_config.enable = %u\n", haptic_config.enable);
     eeconfig_update_haptic(haptic_config.raw);
 }
@@ -159,7 +194,7 @@ void haptic_dwell_decrease(void) {
 }
 
 void haptic_reset(void) {
-    haptic_config.enable   = true;
+    set_haptic_config_enable(true);
     uint8_t feedback       = HAPTIC_FEEDBACK_DEFAULT;
     haptic_config.feedback = feedback;
 #ifdef DRV2605L
@@ -332,7 +367,7 @@ bool process_haptic(uint16_t keycode, keyrecord_t *record) {
         haptic_cont_decrease();
     }
 
-    if (haptic_config.enable) {
+    if (haptic_config.enable && ((!HAPTIC_OFF_IN_LOW_POWER) || (power_state == POWER_STATE_CONFIGURED))) {
         if (record->event.pressed) {
             // keypress
             if (haptic_config.feedback < 2) {
@@ -351,5 +386,15 @@ bool process_haptic(uint16_t keycode, keyrecord_t *record) {
 void haptic_shutdown(void) {
 #ifdef SOLENOID_ENABLE
     solenoid_shutdown();
+#endif
+}
+
+void haptic_notify_power_state_change(void) {
+    update_haptic_enable_gpios();
+#if defined(HAPTIC_ENABLE_PIN)
+    setPinOutput(HAPTIC_ENABLE_PIN);
+#endif
+#if defined(HAPTIC_ENABLE_STATUS_LED)
+    setPinOutput(HAPTIC_ENABLE_STATUS_LED);
 #endif
 }

qmk/qmk_firmware

drivers/haptic/solenoid.c
@@ -19,6 +19,7 @@
 #include "solenoid.h"
 #include "haptic.h"
 #include "gpio.h"
+#include "usb_device_state.h"
 
 bool     solenoid_on      = false;
 bool     solenoid_buzzing = false;
@@ -36,7 +37,7 @@ void solenoid_set_buzz(int buzz) { haptic_set_buzz(buzz); }
 void solenoid_set_dwell(uint8_t dwell) { solenoid_dwell = dwell; }
 
 void solenoid_stop(void) {
-    writePinLow(SOLENOID_PIN);
+    SOLENOID_PIN_WRITE_INACTIVE();
     solenoid_on      = false;
     solenoid_buzzing = false;
 }
@@ -48,7 +49,7 @@ void solenoid_fire(void) {
     solenoid_on      = true;
     solenoid_buzzing = true;
     solenoid_start   = timer_read();
-    writePinHigh(SOLENOID_PIN);
+    SOLENOID_PIN_WRITE_ACTIVE();
 }
 
 void solenoid_check(void) {
@@ -69,20 +70,23 @@ void solenoid_check(void) {
         if ((elapsed % (SOLENOID_BUZZ_ACTUATED + SOLENOID_BUZZ_NONACTUATED)) < SOLENOID_BUZZ_ACTUATED) {
             if (!solenoid_buzzing) {
                 solenoid_buzzing = true;
-                writePinHigh(SOLENOID_PIN);
+                SOLENOID_PIN_WRITE_ACTIVE();
             }
         } else {
             if (solenoid_buzzing) {
                 solenoid_buzzing = false;
-                writePinLow(SOLENOID_PIN);
+                SOLENOID_PIN_WRITE_INACTIVE();
             }
         }
     }
 }
 
 void solenoid_setup(void) {
+    SOLENOID_PIN_WRITE_INACTIVE();
     setPinOutput(SOLENOID_PIN);
-    solenoid_fire();
+    if ((!HAPTIC_OFF_IN_LOW_POWER) || (usb_device_state == USB_DEVICE_STATE_CONFIGURED)) {
+        solenoid_fire();
+    }
 }
 
-void solenoid_shutdown(void) { writePinLow(SOLENOID_PIN); }
+void solenoid_shutdown(void) { SOLENOID_PIN_WRITE_INACTIVE(); }

quantum/haptic.c
@@ -17,6 +17,8 @@
 #include "haptic.h"
 #include "eeconfig.h"
 #include "debug.h"
+#include "usb_device_state.h"
+#include "gpio.h"
 #ifdef DRV2605L
 #    include "DRV2605L.h"
 #endif
@@ -25,6 +27,29 @@
 #endif
 
 haptic_config_t haptic_config;
+
+static void update_haptic_enable_gpios(void) {
+    if (haptic_config.enable && ((!HAPTIC_OFF_IN_LOW_POWER) || (usb_device_state == USB_DEVICE_STATE_CONFIGURED))) {
+#if defined(HAPTIC_ENABLE_PIN)
+        HAPTIC_ENABLE_PIN_WRITE_ACTIVE();
+#endif
+#if defined(HAPTIC_ENABLE_STATUS_LED)
+        HAPTIC_ENABLE_STATUS_LED_WRITE_ACTIVE();
+#endif
+    } else {
+#if defined(HAPTIC_ENABLE_PIN)
+        HAPTIC_ENABLE_PIN_WRITE_INACTIVE();
+#endif
+#if defined(HAPTIC_ENABLE_STATUS_LED)
+        HAPTIC_ENABLE_STATUS_LED_WRITE_INACTIVE();
+#endif
+    }
+}
+
+static void set_haptic_config_enable(bool enabled) {
+    haptic_config.enable = enabled;
+    update_haptic_enable_gpios();
+}
 
 void haptic_init(void) {
     if (!eeconfig_is_enabled()) {
@@ -44,6 +69,10 @@ void haptic_init(void) {
         // or the previous firmware didn't have solenoid enabled,
         // and the current one has solenoid enabled.
         haptic_reset();
+    } else {
+        // Haptic configuration has been loaded through the "raw" union item.
+        // This is to execute any side effects of the configuration.
+        set_haptic_config_enable(haptic_config.enable);
     }
 #ifdef SOLENOID_ENABLE
     solenoid_setup();
@@ -54,6 +83,12 @@ void haptic_init(void) {
     dprintf("DRV2605 driver initialized\n");
 #endif
     eeconfig_debug_haptic();
+#ifdef HAPTIC_ENABLE_PIN
+    setPinOutput(HAPTIC_ENABLE_PIN);
+#endif
+#ifdef HAPTIC_ENABLE_STATUS_LED
+    setPinOutput(HAPTIC_ENABLE_STATUS_LED);
+#endif
 }
 
 void haptic_task(void) {
@@ -69,13 +104,13 @@ void eeconfig_debug_haptic(void) {
 }
 
 void haptic_enable(void) {
-    haptic_config.enable = 1;
+    set_haptic_config_enable(true);
     xprintf("haptic_config.enable = %u\n", haptic_config.enable);
     eeconfig_update_haptic(haptic_config.raw);
 }
 
 void haptic_disable(void) {
-    haptic_config.enable = 0;
+    set_haptic_config_enable(false);
     xprintf("haptic_config.enable = %u\n", haptic_config.enable);
     eeconfig_update_haptic(haptic_config.raw);
 }
@@ -157,7 +192,7 @@ void haptic_dwell_decrease(void) {
 }
 
 void haptic_reset(void) {
-    haptic_config.enable   = true;
+    set_haptic_config_enable(true);
     uint8_t feedback       = HAPTIC_FEEDBACK_DEFAULT;
     haptic_config.feedback = feedback;
 #ifdef DRV2605L
@@ -291,5 +326,15 @@ void haptic_play(void) {
 void haptic_shutdown(void) {
 #ifdef SOLENOID_ENABLE
     solenoid_shutdown();
+#endif
+}
+
+void haptic_notify_usb_device_state_change(void) {
+    update_haptic_enable_gpios();
+#if defined(HAPTIC_ENABLE_PIN)
+    setPinOutput(HAPTIC_ENABLE_PIN);
+#endif
+#if defined(HAPTIC_ENABLE_STATUS_LED)
+    setPinOutput(HAPTIC_ENABLE_STATUS_LED);
 #endif
 }