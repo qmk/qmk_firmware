85fc4200816fe05edb01a65859a59c1aebfa51d7
marby3 <64513524+marby3@users.noreply.github.com>
2021-11-04 20:28:55 +0900

    joystick change

--- keyboards/formy46/keymaps/default/keymap.c
+++ keyboards/formy46/keymaps/default/keymap.c
@@ -98,41 +98,41 @@ joystick_config_t joystick_axes[JOYSTICK_AXES_COUNT] = {
 void joystick_task(){
 	switch (joystickMode) {
 	case 0: // gamepad
-		joystick_status.axes[0] = analogReadPin(F5)/4 - 128;
-		joystick_status.axes[1] = analogReadPin(F4)/4 - 128;
+		joystick_status.axes[0] = analogReadPin(F4)/4 - 128;
+		joystick_status.axes[1] = analogReadPin(F5)/4 - 128;
 		joystick_status.status |= JS_UPDATED;
         send_joystick_packet(&joystick_status);
 		break;
     case 1: // arrows
-        if (!arrows[0] && analogReadPin(F4) - 512 > actuation){
+        if (!arrows[0] && analogReadPin(F5) - 512 > actuation){
             arrows[0] = true;
             register_code16(arrow_keys[2]);
         }
-        else if (arrows[0] &&  analogReadPin(F4) - 512 < actuation){
+        else if (arrows[0] &&  analogReadPin(F5) - 512 < actuation){
             arrows[0] = false;
             unregister_code16(arrow_keys[2]);
         }
-        if (!arrows[1] && analogReadPin(F4) - 512 < -actuation){
+        if (!arrows[1] && analogReadPin(F5) - 512 < -actuation){
             arrows[1] = true;
             register_code16(arrow_keys[0]);
         }
-        else if (arrows[1] && analogReadPin(F4) - 512 > -actuation){
+        else if (arrows[1] && analogReadPin(F5) - 512 > -actuation){
             arrows[1] = false;
             unregister_code16(arrow_keys[0]);
         }
-        if (!arrows[2] && analogReadPin(F5) - 512 > actuation){
+        if (!arrows[2] && analogReadPin(F4) - 512 > actuation){
             arrows[2] = true;
             register_code16(arrow_keys[3]);
         }
-        else if (arrows[2] &&  analogReadPin(F5) - 512 < actuation){
+        else if (arrows[2] &&  analogReadPin(F4) - 512 < actuation){
             arrows[2] = false;
             unregister_code16(arrow_keys[3]);
         }
-        if (!arrows[3] && analogReadPin(F5) - 512 < -actuation){
+        if (!arrows[3] && analogReadPin(F4) - 512 < -actuation){
             arrows[3] = true;
             register_code16(arrow_keys[1]);
         }
-        else if (arrows[3] && analogReadPin(F5) - 512 > -actuation){
+        else if (arrows[3] && analogReadPin(F4) - 512 > -actuation){
             arrows[3] = false;
             unregister_code16(arrow_keys[1]);
         }
@@ -140,8 +140,8 @@ void joystick_task(){
     case 2: // mouse
         ;
         report_mouse_t currentReport = pointing_device_get_report();
-        currentReport.x = (analogReadPin(F5) - 512) / joystickResolution;
-        currentReport.y = (analogReadPin(F4) - 512) / joystickResolution;
+        currentReport.x = (analogReadPin(F4) - 512) / joystickResolution;
+        currentReport.y = (analogReadPin(F5) - 512) / joystickResolution;
         if (currentReport.x < joystickThreshold && currentReport.x > -joystickThreshold){
             currentReport.x = 0;
         }
