dc45c868035854cf8095090c83a5ebdb64c3d04c
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2021-11-04 18:30:07 +0800

    Update keyboards/keychron/q1/rev_0101/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keychron/q1/rev_0101/keymaps/default/keymap.c
+++ keyboards/keychron/q1/rev_0101/keymaps/default/keymap.c
@@ -67,16 +67,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-                tap_code_delay(KC_VOLU, 10);
-        } else {
-                tap_code_delay(KC_VOLD, 10);
-        }
-    }
-    return false;
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
