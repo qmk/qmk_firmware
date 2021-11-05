e4e61f1b2a684c090e029660e3d99c8483ab561b
Nick Brassel <nick@tzarc.org>
2021-11-05 19:56:03 +1100

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/planck/keymaps/dvz/config.h
+++ keyboards/planck/keymaps/dvz/config.h
@@ -33,9 +33,6 @@
  * MIDI options
  */
 
-/* Prevent use of disabled MIDI features in the keymap */
-//#define MIDI_ENABLE_STRICT 1
-
 /* enable basic MIDI features:
    - MIDI notes can be sent when in Music mode is on
 */
@@ -49,9 +46,6 @@
    - etc.
 */
 //#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
