07236ccb86e3065a06386c7a6a3ebd4e905058c2
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-11-04 19:42:51 -0700

    alias LAYOUT to LAYOUT_all

--- keyboards/idb/idb_60/idb_60.h
+++ keyboards/idb/idb_60/idb_60.h
@@ -10,7 +10,7 @@ void _idb_60_fn_led_off(void);
 #define _idb_60_esc_led_on() _idb_60_fn_led_on()
 #define _idb_60_esc_led_off() _idb_60_fn_led_off()
 
-#define LAYOUT( \
+#define LAYOUT_all( \
         K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, K07, \
         K20, K30, K21, K31, K22, K32, K23, K33, K24, K34, K25, K35, K26, K36, \
         K40, K50, K41, K51, K42, K52, K43, K53, K44, K54, K45, K55,      K56, \
@@ -29,7 +29,7 @@ void _idb_60_fn_led_off(void);
         { K90,   KC_NO, KC_NO, K93,   KC_NO, K95,   K96        } \
 }
 
-#define LAYOUT_all LAYOUT
+#define LAYOUT LAYOUT_all
 
 #define LAYOUT_default( \
         K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, \
