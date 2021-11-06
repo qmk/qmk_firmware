9ef7545dbe0f5f1db3e1b9d20a8fbddc601d68f9
Terry Mathews <terry@terrymathews.net>
2021-11-06 10:28:45 -0400

    clean up source files

--- keyboards/tkc/portico75/config.h
+++ keyboards/tkc/portico75/config.h
@@ -1,3 +1,20 @@
+/*
+Copyright 2021 Terry Mathews
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include "config_common.h"
@@ -61,13 +78,13 @@
 #define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
 #define DRIVER_INDICATOR_LED_TOTAL 0
 #else
-// IS31FL3741 driver
+// WT_RGB IS31FL3741 driver code
 #define DRIVER_COUNT 1
 #define DRIVER_LED_TOTAL 98
 
 #define RGB_BACKLIGHT_ENABLED 1
 
-// This conditionally compiles the backlight code for Dawn60 specifics
+// This conditionally compiles the backlight code for Portico75 specifics
 #define RGB_BACKLIGHT_PORTICO75
 
 // disable backlight when USB suspended (PC sleep/hibernate/shutdown)
