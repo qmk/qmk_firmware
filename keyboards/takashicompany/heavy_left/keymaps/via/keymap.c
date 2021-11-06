7d49232e306d15e09b6723397bf71223d1132178
takashicompany <t@kashi.company>
2021-11-06 14:16:55 +0900

    Update keyboards/takashicompany/heavy_left/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/takashicompany/heavy_left/keymaps/via/keymap.c
+++ keyboards/takashicompany/heavy_left/keymaps/via/keymap.c
@@ -63,7 +63,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 SEND_STRING("00");
             }
-            break;
+            return false;
     }
     return true;
 }
