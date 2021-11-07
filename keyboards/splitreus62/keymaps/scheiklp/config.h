8b4aa3b1ed0d87e0856e37464810445174856c01
Paul Scheikl <paul.scheikl@kit.edu>
2021-11-07 18:13:33 +0100

    Update config.h

--- keyboards/splitreus62/keymaps/scheiklp/config.h
+++ keyboards/splitreus62/keymaps/scheiklp/config.h
@@ -18,8 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xBEEF
 #define PRODUCT_ID      0xFED0
