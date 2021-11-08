c419f5dc226a4a06ee479bd4f7dfe39474fb42bc
Xiao Yu <xiao@xster.net>
2021-11-07 22:51:53 -0800

    getting ready for light switches

--- keyboards/xiudi/xd75/keymaps/xo/rules.mk
+++ keyboards/xiudi/xd75/keymaps/xo/rules.mk
@@ -12,6 +12,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 BACKLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = yes
 CONSOLE_ENABLED = yes
 MOUSEKEY_ENABLED = no
 SWAP_HANDS_ENABLE = yes
