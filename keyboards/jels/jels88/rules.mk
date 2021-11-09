d28f00ab90c89c6ad6407e0c77f3e7f1e0868a74
jels <66403163+Jels-kb@users.noreply.github.com>
2021-11-09 23:18:01 +1000

    Update rules.mk

--- keyboards/jels/jels88/rules.mk
+++ keyboards/jels/jels88/rules.mk
@@ -15,7 +15,7 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -25,7 +25,5 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no 	    # Enable keyboard backlight functionality on B7 by default
 UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output on port C6
-RGBLIGHT_ENABLE = yes      # Enable WS2812 RGB underlight. - We have custom RGB underglow
-ENCODER_ENABLE = no		# For Knob support
+RGBLIGHT_ENABLE = yes      # Enable WS2812 RGB underlight.
