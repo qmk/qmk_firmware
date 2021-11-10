347915515485a39a104e0f1df7000b6fb8589026
Leon Omelan <rozpierog@gmail.com>
2021-11-10 18:11:38 +0100

    Update keyboards/handwired/nozbe_macro/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/nozbe_macro/keymaps/default/keymap.c
+++ keyboards/handwired/nozbe_macro/keymaps/default/keymap.c
@@ -16,9 +16,16 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer_names { _BASE, _FN };
+enum layer_names { 
+    _BASE, 
+    _FN,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(LT(_FN, KC_N), LALT(KC_UP), LALT(KC_DOWN), LALT(KC_D)),
-    [_FN]   = LAYOUT(KC_TRNS, KC_1, KC_2, BL_STEP),
+    [_BASE] = LAYOUT(
+        LT(_FN, KC_N), LALT(KC_UP), LALT(KC_DOWN), LALT(KC_D)
+    ),
+    [_FN]   = LAYOUT(
+        KC_TRNS, KC_1, KC_2, BL_STEP
+    ),
 };
