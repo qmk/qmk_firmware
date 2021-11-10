3f5561872a3805f4a177214767856d2d48193cba
Uy Bui <uybv.cntt@gmail.com>
2021-11-10 13:44:41 +0900

    [Keyboard] Stelo encoder action

--- keyboards/wekey/stelo/stelo.c
+++ keyboards/wekey/stelo/stelo.c
@@ -2,3 +2,15 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "stelo.h"
+#include "encoder_actions.h"
+
+void matrix_scan_kb(void) {
+    encoder_action_unregister();
+    matrix_scan_user();
+}
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    encoder_action_register(index, clockwise);
+    return true;
+};
