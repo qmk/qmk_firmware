b946b17fc88172d134a0ca7e07ec4048899043c9
X-Bows Tech <41098278+XBowsTech@users.noreply.github.com>
2021-11-11 09:27:02 +0800

    Update keyboards/xbows/knight/knight.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/xbows/knight/knight.c
+++ keyboards/xbows/knight/knight.c
@@ -134,15 +134,6 @@
      4, 4, 4,    4, 4, 4, 4,    4, 4, 4, 1, 1, 1
  } };
 
- void suspend_power_down_kb(void) {
-     rgb_matrix_set_suspend_state(true);
-     suspend_power_down_user();
- }
-
- void suspend_wakeup_init_kb(void) {
-     rgb_matrix_set_suspend_state(false);
-     suspend_wakeup_init_user();
- }
 
  __attribute__ ((weak)) void rgb_matrix_indicators_user(void) {
     if (host_keyboard_led_state().caps_lock) {
