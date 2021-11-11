ceb5207c0967aeeee22c7b979041b5ef3524ff8d
mini-ninja-64 <callumhart@btinternet.com>
2021-11-11 23:38:17 +0000

    Remove key_matrix_definitions

--- keyboards/cutie_club/keebcats/denis/denis.h
+++ keyboards/cutie_club/keebcats/denis/denis.h
@@ -16,140 +16,140 @@
 
 #pragma once
 
-#include "key_matrix_definitions.h"
+#include "quantum.h"
 
 #define LAYOUT_all( \
-    ESC, ALPHAS_R0, SPLIT_BACKSPACE,                   \
-    TAB, ALPHAS_R1_ANSI,                               \
-    CAPS, ALPHAS_R2, ISO_ENTER,                        \
-    SPLIT_LSHIFT, ALPHAS_R3, SPLIT_RSHIFT,             \
-    LEFT_CTRL_CLUSTER, SPLIT_SPACE, RIGHT_CTRL_CLUSTER \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14,                   \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13,                               \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13,                        \
+    k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13,             \
+    k04_00, k04_01, k04_02, k04_04, k04_06, k04_08, k04_10, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, SPLIT_BACKSPACE_MATRIX },                   \
-    { TAB_MATRIX, ALPHAS_R1_ANSI_MATRIX },                                      \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ISO_ENTER_MATRIX },                        \
-    { SPLIT_LSHIFT_MATRIX, ALPHAS_R3_MATRIX, SPLIT_RSHIFT_MATRIX },             \
-    { LEFT_CTRL_CLUSTER_MATRIX, SPLIT_SPACE_MATRIX, RIGHT_CTRL_CLUSTER_MATRIX } \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14 },                   \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13, KC_NO },                                      \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13, KC_NO },                        \
+    { k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13, KC_NO },             \
+    { k04_00, k04_01, k04_02, KC_NO, k04_04, KC_NO, k04_06, KC_NO, k04_08, KC_NO, k04_10, k04_11, k04_12, k04_13, KC_NO } \
 }
 
 #define LAYOUT_60_ansi( \
-    ESC, ALPHAS_R0, BACKSPACE,                   \
-    TAB, ALPHAS_R1_ANSI,                         \
-    CAPS, ALPHAS_R2, ANSI_ENTER,                 \
-    LSHIFT, ALPHAS_R3, RSHIFT,                   \
-    LEFT_CTRL_CLUSTER, SPACE, RIGHT_CTRL_CLUSTER \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_14,                   \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13,                         \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, ANSI_ENTER,                 \
+    k03_00, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12,                   \
+    k04_00, k04_01, k04_02, k04_06, k04_10, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, BACKSPACE_MATRIX },                   \
-    { TAB_MATRIX, ALPHAS_R1_ANSI_MATRIX },                                \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ANSI_ENTER_MATRIX },                 \
-    { LSHIFT_MATRIX, ALPHAS_R3_MATRIX, RSHIFT_MATRIX },                   \
-    { LEFT_CTRL_CLUSTER_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_MATRIX } \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, KC_NO, k00_14 },                   \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13, KC_NO },                                \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, KC_NO, k02_13, KC_NO },                 \
+    { k03_00, KC_NO, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, KC_NO, KC_NO },                   \
+    { k04_00, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, k04_10, k04_11, k04_12, k04_13, KC_NO } \
 }
 
-#define LAYOUT_60_ansi_split_bs_rshift( \
-    ESC, ALPHAS_R0, SPLIT_BACKSPACE,             \
-    TAB, ALPHAS_R1_ANSI,                         \
-    CAPS, ALPHAS_R2, ANSI_ENTER,                 \
-    LSHIFT, ALPHAS_R3, SPLIT_RSHIFT,             \
-    LEFT_CTRL_CLUSTER, SPACE, RIGHT_CTRL_CLUSTER \
+#define LAYOUT_60_ansi_split_bs_k03_12( \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14,             \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13,                         \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, ANSI_ENTER,                 \
+    k03_00, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13,             \
+    k04_00, k04_01, k04_02, k04_06, k04_10, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, SPLIT_BACKSPACE_MATRIX },             \
-    { TAB_MATRIX, ALPHAS_R1_ANSI_MATRIX },                                \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ANSI_ENTER_MATRIX },                 \
-    { LSHIFT_MATRIX, ALPHAS_R3_MATRIX, SPLIT_RSHIFT_MATRIX },             \
-    { LEFT_CTRL_CLUSTER_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_MATRIX } \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14 },             \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13, KC_NO },                                \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, KC_NO, k02_13, KC_NO },                 \
+    { k03_00, KC_NO, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13, KC_NO }, \
+    { k04_00, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, k04_10, k04_11, k04_12, k04_13, KC_NO }       \
 }
 
 #define LAYOUT_60_ansi_tsangan( \
-    ESC, ALPHAS_R0, BACKSPACE,                                   \
-    TAB, ALPHAS_R1_ANSI,                                         \
-    CAPS, ALPHAS_R2, ANSI_ENTER,                                 \
-    LSHIFT, ALPHAS_R3, RSHIFT,                                   \
-    LEFT_CTRL_CLUSTER_TSANGAN, SPACE, RIGHT_CTRL_CLUSTER_TSANGAN \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_14,                                   \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13,                                         \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, ANSI_ENTER,                                 \
+    k03_00, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12,                                   \
+    k04_00, k04_01, k04_02, k04_06, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, BACKSPACE_MATRIX },                                    \
-    { TAB_MATRIX, ALPHAS_R1_ANSI_MATRIX },                                                 \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ANSI_ENTER_MATRIX },                                  \
-    { LSHIFT_MATRIX, ALPHAS_R3_MATRIX, RSHIFT_MATRIX },                                    \
-    { LEFT_CTRL_CLUSTER_TSANGAN_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_TSANGAN_MATRIX }  \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, KC_NO, k00_14 },                                    \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13, KC_NO },                                                 \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, KC_NO, k02_13, KC_NO },                                  \
+    { k03_00, KC_NO, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, KC_NO, KC_NO },                                    \
+    { k04_00, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, KC_NO, k04_11, k04_12, k04_13, KC_NO }  \
 }
 
 #define LAYOUT_60_hhkb( \
-    ESC, ALPHAS_R0, SPLIT_BACKSPACE,                       \
-    TAB, ALPHAS_R1_ANSI,                                   \
-    CAPS, ALPHAS_R2, ANSI_ENTER,                           \
-    LSHIFT, ALPHAS_R3, SPLIT_RSHIFT,                       \
-    LEFT_CTRL_CLUSTER_HHKB, SPACE, RIGHT_CTRL_CLUSTER_HHKB \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14,                       \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13,                                   \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, ANSI_ENTER,                           \
+    k03_00, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13,                       \
+    k04_01, k04_02, k04_06, k04_11, k04_12 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, SPLIT_BACKSPACE_MATRIX },                        \
-    { TAB_MATRIX, ALPHAS_R1_ANSI_MATRIX },                                           \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ANSI_ENTER_MATRIX },                            \
-    { LSHIFT_MATRIX, ALPHAS_R3_MATRIX, SPLIT_RSHIFT_MATRIX },                        \
-    { LEFT_CTRL_CLUSTER_HHKB_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_HHKB_MATRIX }  \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14 },                        \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13, KC_NO },                                           \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, KC_NO, k02_13, KC_NO },                            \
+    { k03_00, KC_NO, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13, KC_NO },                        \
+    { KC_NO, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, KC_NO, k04_11, k04_12, KC_NO, KC_NO }  \
 }
 
 #define LAYOUT_60_tsangan_hhkb( \
-    ESC, ALPHAS_R0, SPLIT_BACKSPACE,                             \
-    TAB, ALPHAS_R1_ANSI,                                         \
-    CAPS, ALPHAS_R2, ANSI_ENTER,                                 \
-    LSHIFT, ALPHAS_R3, SPLIT_RSHIFT,                             \
-    LEFT_CTRL_CLUSTER_TSANGAN, SPACE, RIGHT_CTRL_CLUSTER_TSANGAN \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14,                             \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13,                                         \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, ANSI_ENTER,                                 \
+    k03_00, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13,                             \
+    k04_00, k04_01, k04_02, k04_06, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, SPLIT_BACKSPACE_MATRIX },                              \
-    { TAB_MATRIX, ALPHAS_R1_ANSI_MATRIX },                                                 \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ANSI_ENTER_MATRIX },                                  \
-    { LSHIFT_MATRIX, ALPHAS_R3_MATRIX, SPLIT_RSHIFT_MATRIX },                              \
-    { LEFT_CTRL_CLUSTER_TSANGAN_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_TSANGAN_MATRIX }  \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14 },                              \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, k01_13, KC_NO },                                                 \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, KC_NO, k02_13, KC_NO },                                  \
+    { k03_00, KC_NO, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13, KC_NO },                              \
+    { k04_00, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, KC_NO, k04_11, k04_12, k04_13, KC_NO }  \
 }
 
 
 #define LAYOUT_60_iso( \
-    ESC, ALPHAS_R0, BACKSPACE,                   \
-    TAB, ALPHAS_R1_ISO,                          \
-    CAPS, ALPHAS_R2, ISO_ENTER,                  \
-    SPLIT_LSHIFT, ALPHAS_R3, RSHIFT,             \
-    LEFT_CTRL_CLUSTER, SPACE, RIGHT_CTRL_CLUSTER \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_14,                   \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12,                          \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13,                  \
+    k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12,             \
+    k04_00, k04_01, k04_02, k04_06, k04_10, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, BACKSPACE_MATRIX },                   \
-    { TAB_MATRIX, ALPHAS_R1_ISO_MATRIX },                                 \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ISO_ENTER_MATRIX },                  \
-    { SPLIT_LSHIFT_MATRIX, ALPHAS_R3_MATRIX, RSHIFT_MATRIX },             \
-    { LEFT_CTRL_CLUSTER_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_MATRIX } \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, KC_NO, k00_14 },                   \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, KC_NO, KC_NO },                                 \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13, KC_NO },                  \
+    { k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, KC_NO, KC_NO },             \
+    { k04_00, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, k04_10, k04_11, k04_12, k04_13, KC_NO } \
 }
 
-#define LAYOUT_60_iso_split_bs_rshift( \
-    ESC, ALPHAS_R0, SPLIT_BACKSPACE,             \
-    TAB, ALPHAS_R1_ISO,                          \
-    CAPS, ALPHAS_R2, ISO_ENTER,                  \
-    SPLIT_LSHIFT, ALPHAS_R3, SPLIT_RSHIFT,       \
-    LEFT_CTRL_CLUSTER, SPACE, RIGHT_CTRL_CLUSTER \
+#define LAYOUT_60_iso_split_bs_k03_12( \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14,             \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12,                          \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13,                  \
+    k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13,       \
+    k04_00, k04_01, k04_02, k04_06, k04_10, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, SPLIT_BACKSPACE_MATRIX },             \
-    { TAB_MATRIX, ALPHAS_R1_ISO_MATRIX },                                 \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ISO_ENTER_MATRIX },                  \
-    { SPLIT_LSHIFT_MATRIX, ALPHAS_R3_MATRIX, SPLIT_RSHIFT_MATRIX },       \
-    { LEFT_CTRL_CLUSTER_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_MATRIX } \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_13, k00_14 },             \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, KC_NO, KC_NO },                                 \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13, KC_NO },                  \
+    { k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, k03_13, KC_NO },       \
+    { k04_00, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, k04_10, k04_11, k04_12, k04_13, KC_NO } \
 }
 
 #define LAYOUT_60_iso_tsangan( \
-    ESC, ALPHAS_R0, BACKSPACE,                                   \
-    TAB, ALPHAS_R1_ISO,                                          \
-    CAPS, ALPHAS_R2, ISO_ENTER,                                  \
-    SPLIT_LSHIFT, ALPHAS_R3, RSHIFT,                             \
-    LEFT_CTRL_CLUSTER_TSANGAN, SPACE, RIGHT_CTRL_CLUSTER_TSANGAN \
+    k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, k00_14,                                   \
+    k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12,                                          \
+    k02_00, k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13,                                  \
+    k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12,                             \
+    k04_00, k04_01, k04_02, k04_06, k04_11, k04_12, k04_13 \
 ) \
 { \
-    { ESC_MATRIX, ALPHAS_R0_MATRIX, BACKSPACE_MATRIX },                                   \
-    { TAB_MATRIX, ALPHAS_R1_ISO_MATRIX },                                                 \
-    { CAPS_MATRIX, ALPHAS_R2_MATRIX, ISO_ENTER_MATRIX },                                  \
-    { SPLIT_LSHIFT_MATRIX, ALPHAS_R3_MATRIX, RSHIFT_MATRIX },                             \
-    { LEFT_CTRL_CLUSTER_TSANGAN_MATRIX, SPACE_MATRIX, RIGHT_CTRL_CLUSTER_TSANGAN_MATRIX } \
+    { k00_00, k00_01, k00_02, k00_03, k00_04, k00_05, k00_06, k00_07, k00_08, k00_09, k00_10, k00_11, k00_12, KC_NO, k00_14 },                                   \
+    { k01_00, k01_01, k01_02, k01_03, k01_04, k01_05, k01_06, k01_07, k01_08, k01_09, k01_10, k01_11, k01_12, KC_NO, KC_NO },                                                 \
+    { k02_00,  k02_01, k02_02, k02_03, k02_04, k02_05, k02_06, k02_07, k02_08, k02_09, k02_10, k02_11, k02_12, k02_13, KC_NO },                                  \
+    { k03_00, k03_01, k03_02, k03_03, k03_04, k03_05, k03_06, k03_07, k03_08, k03_09, k03_10, k03_11, k03_12, KC_NO, KC_NO },                             \
+    { k04_00, k04_01, k04_02, KC_NO, KC_NO, KC_NO, k04_06, KC_NO, KC_NO, KC_NO, KC_NO, k04_11, k04_12, k04_13, KC_NO } \
 }
