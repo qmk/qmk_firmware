2837bd8da9ed6a350201927af7ecbb08db461179
fauxpark <fauxpark@gmail.com>
2021-11-12 19:36:29 +1100

    Got to "device descriptor failed" by comparing with Teensy 3.6 code

--- keyboards/handwired/onekey/teensy_35/mcuconf.h
+++ keyboards/handwired/onekey/teensy_35/mcuconf.h
@@ -25,64 +25,26 @@
 
 /* Select the MCU clocking mode below by enabling the appropriate block. */
 
-/* PEE mode - 48MHz system clock driven by external crystal. */
-#if 1
+/* PEE mode - 120MHz system clock driven by external crystal. */
 #define KINETIS_MCG_MODE            KINETIS_MCG_MODE_PEE
-#define KINETIS_PLLCLK_FREQUENCY    96000000UL
-#define KINETIS_SYSCLK_FREQUENCY    48000000UL
-#endif
+#define KINETIS_PLLCLK_FREQUENCY    120000000UL  // 120 MHz (RUN)
+#define KINETIS_SYSCLK_FREQUENCY    KINETIS_PLLCLK_FREQUENCY
+#define KINETIS_BUSCLK_FREQUENCY    60000000UL
+#define KINETIS_FLASHCLK_FREQUENCY  24000000UL  // 24 MHz (RUN)
 
-/* FEI mode (~48MHz) */
-#if 0
-#define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEI
-#define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
-#define KINETIS_MCG_FLL_DRS         1           /* 1464x FLL factor */
-#define KINETIS_SYSCLK_FREQUENCY    47972352UL  /* 32.768 kHz * 1464 (~48 MHz) */
-#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide MCGCLKOUT (~48MHz) by 1 to SYSCLK */
-#define KINETIS_CLKDIV1_OUTDIV2     1           /* Divide by 1 for (~48MHz) peripheral clock */
-#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide by 2 for (~24MHz) flash clock */
-#define KINETIS_BUSCLK_FREQUENCY    KINETIS_SYSCLK_FREQUENCY
-#define KINETIS_FLASHCLK_FREQUENCY  KINETIS_SYSCLK_FREQUENCY/2
-#endif /* 0 */
-
-/* FEE mode - 24 MHz with external 32.768 kHz crystal */
-/* not implemented */
-#if 0
-#define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEE
-#define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
-#define KINETIS_MCG_FLL_DRS         0           /* 732x FLL factor */
-#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 24 MHz */
-#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 12 MHz */
-#define KINETIS_SYSCLK_FREQUENCY    23986176UL  /* 32.768 kHz*732 (~24 MHz) */
-#define KINETIS_UART0_CLOCK_FREQ    (32768 * 732) /* FLL output */
-#define KINETIS_UART0_CLOCK_SRC     1           /* Select FLL clock */
-#define KINETIS_BUSCLK_FREQUENCY    (KINETIS_SYSCLK_FREQUENCY / KINETIS_CLKDIV1_OUTDIV4)
-#endif /* 0 */
-
-/* FEE mode - 48 MHz */
-/* not implemented */
-#if 0
-#define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEE
-#define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
-#define KINETIS_MCG_FLL_DRS         1           /* 1464x FLL factor */
-#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 48 MHz */
-#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 24 MHz */
-#define KINETIS_SYSCLK_FREQUENCY    47972352UL  /* 32.768 kHz * 1464 (~48 MHz) */
-#endif /* 0 */
+#define KINETIS_CLKDIV1_OUTDIV1 1  // -> 0
+#define KINETIS_CLKDIV1_OUTDIV2 2  // -> 1
+#define KINETIS_CLKDIV1_OUTDIV4 5  // -> 4
 
 /*
  * SERIAL driver system settings.
  */
-#define KINETIS_SERIAL_USE_UART0              TRUE
+#define KINETIS_SERIAL_USE_UART4              TRUE
 
 /*
  * USB driver settings
  */
 #define KINETIS_USB_USE_USB0                  TRUE
-
-/* Need to redefine this, since the default (configured for K20x) might not apply
- *   2 for Teensy LC
- *   5 for Teensy 3.x */
 #define KINETIS_USB_USB0_IRQ_PRIORITY         5
 
 #endif /* _MCUCONF_H_ */
