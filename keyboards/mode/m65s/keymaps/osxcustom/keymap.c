a9e437a7b51225bdb03674c5434811f66874ecfc
WeedMD-Support <support@weedmd.com>
2021-11-12 15:13:00 -0500

    m65

--- keyboards/mode/m65s/keymaps/osxcustom/keymap.c
+++ keyboards/mode/m65s/keymaps/osxcustom/keymap.c
@@ -23,12 +23,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,          KC_BSLS, KC_HOME ,
         KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT ,          KC_PGUP ,
 		KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          KC_UP  , KC_PGDN  ,
-        KC_LCTL, LM(1, KC_LALT), KC_LGUI,                            KC_SPC ,                            KC_RALT, MO(1)  ,          KC_LEFT, KC_DOWN, KC_RGHT 
+        KC_LCTL, LM(1, KC_LALT), KC_LGUI,                            KC_SPC ,                            KC_RGUI, MO(1)  ,          KC_LEFT, KC_DOWN, KC_RGHT 
 	),
    [1] = LAYOUT_all(
     KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_MUTE,
-	_______, _______, _______, _______, _______, LSFT(LCTL(KC_4)), _______, _______, _______, _______, _______, LSFT(LCTL(KC_3)), _______,          _______, KC_END,
-	_______, _______, _______, LCTL(LGUI(KC_D)), _______, _______, _______, _______, _______, _______, LCTL(LGUI(KC_Q)), _______, _______, _______,          _______,
+	_______, _______, _______, _______, _______, LSFT(LCTL(KC_4)), _______, _______, _______, _______, LSFT(LCTL(KC_3)), _______ , _______,          _______, KC_END,
+	_______, _______, _______, LCTL(LGUI(KC_D)), _______, _______, _______, _______, _______, LCTL(LGUI(KC_Q)), _______ , _______, _______, KC_WSCH,          _______,
 	_______, _______, _______, _______, _______, RESET, _______, _______, _______, _______, _______, _______, _______,          KC_VOLU, _______,
 	_______, _______, _______,                            KC_MPLY,                            _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT
 	)
