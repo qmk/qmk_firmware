879fdd68cdd85af4a5b435c07d6526b10c01e98f
Albert Y <76888457+filterpaper@users.noreply.github.com>
2021-11-13 07:24:09 +0800

    Replace page movement with mouse wheel.

--- keyboards/macro3/macro3.c
+++ keyboards/macro3/macro3.c
@@ -28,9 +28,9 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
         }
     } else if (index == 0) {
         if (clockwise) {
-            tap_code(KC_PGDN);
+            tap_code(KC_WH_D);
         } else {
-            tap_code(KC_PGUP);
+            tap_code(KC_WH_U);
         }
     }
     return true;
