a2691b8b8751f7ab90072caa641fd39bd32a703a
ryjelsum <ryjelsum@gmail.com>
2021-11-14 15:35:30 -0800

    For real this time: Cleaning up extraneous crud left over from kbfirmware's automatically generated & dated codebase (I believe)

--- keyboards/40percentclub/polyandry/keymaps/default/keymap.c
+++ keyboards/40percentclub/polyandry/keymaps/default/keymap.c
@@ -1,4 +1,5 @@
-#include "poly.h"
+//#include "poly.h" //not sure why this is here?
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -13,46 +14,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-		
-	} else {
-		
-	}
-
-}
