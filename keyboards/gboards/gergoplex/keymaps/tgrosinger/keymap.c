e5e5e8aabe1809e015d24c9b4902f296aa91f100
Tony Grosinger <tony@grosinger.net>
2021-11-13 17:02:40 -0800

    Add improved shortcuts for PopOS

--- keyboards/gboards/gergoplex/keymaps/tgrosinger/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/tgrosinger/keymap.c
@@ -7,6 +7,7 @@ enum {
   _NUMB,   // Numbers
   _ARROWS, // Arrows and OS
   _TMUX,   // TMUX Nav layer
+  _POPOS,  // Gnome Shell, aka PopOS
 };
 
 
@@ -145,14 +146,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
  * |-----+-----+-----+--RMB+-LMB-|      |-----+-----+-----+-----------|
  * |     |     |    ENT    |     |      |    QUO   UNDR   |     |     |
  * `-----+-----+-----+-----+-----'      `-----------------------------'
- *     .-------------------------.           .-----------------.
- *     |        |       |        |           |        |    |   |
- *     '-------------------------'           '-----------------'
+ *     .-------------------------.           .-------------------.
+ *     |        |       |        |           |        |    |     |
+ *     '-------------------------'           '-------------------'
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap 0: Default layer
-  * ,-----------------------------.       ,----------------------------------.
+  * ,------------------------------.      ,----------------------------------.
   * |  Q   |  W  |  E  |  R  |  T  |      |  Y  |  U  |  I  |   O   |    P   |
   * |------+-----+-----+-----+-----|      |-----+-----+-----+-------+--------|
   * |CTRL/A|  S  |  D  |  F  |  G  |      |  H  |  J  |  K  |   L   | CTRL/; |
@@ -160,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |SHFT/Z|ALT/X|  C  |  V  |  B  |      |  N  |  M  |  <  | ALT/> | SHFT/? |
   * `------+-----+-----+------+----'      `----------------------------------'
   *           .-----------------.             .----------------------.
-  *           |   |GUI |SPC(NUM)|             |SPC(SYM)|TMUX |       |
+  *           |   |GUI |SPC(NUM)|             |SPC(SYM)|TMUX | PopOS |
   *           '-----------------'             '----------------------'
   */
   [_ALPHA] = LAYOUT_split_3x5_3(
@@ -169,11 +170,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       MT(MOD_LSFT, KC_Z),MT(MOD_LALT, KC_X), KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, MT(MOD_LALT, KC_DOT), MT(MOD_RSFT, KC_SLSH),
               
       KC_NO,   KC_LGUI, LT(_NUMB, KC_SPC),       // Left
-      LT(_SYMB, KC_SPC), MO(_TMUX), KC_NO        // Right
+      LT(_SYMB, KC_SPC), MO(_TMUX), MO(_POPOS)   // Right
       ),
 
   /* Keymap 1: Symbols layer
-  * ,-----------------------------.       ,-------------------------------.
+  * ,------------------------------.      ,-------------------------------.
   * |  !   |  @  |  #  |  $  |  %  |      |  ^  |  &  |  *  |  +  |   =   |
   * |------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
   * |  F1  | F2  | F3  | F4  | F5  |      | F6  | F7  | F8  | F9  |  F10  |
@@ -188,11 +189,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_F11,  KC_F12,
-                    KC_NO, KC_DEL, MO(_ARROWS),        KC_TRNS,  KC_SPC, KC_NO
+                KC_NO, KC_DEL, MO(_ARROWS),                 KC_TRNS,  KC_SPC, KC_NO
       ),
 
   /* Keymap 2: Number layer
-  * ,-----------------------------.       ,-------------------------------.
+  * ,------------------------------.      ,-------------------------------.
   * |  1   |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |   0   |
   * |------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
   * | CTRL |     |  (  |  )  |     |      |     |  4  |  7  |  6  |       |
@@ -205,14 +206,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_NUMB] = LAYOUT_split_3x5_3(
       KC_1,    KC_2,  KC_3,    KC_4,    KC_5,       KC_6,  KC_7, KC_8, KC_9,  KC_0,
-      KC_LCTL,   KC_NO, KC_LPRN, KC_RPRN, KC_NO,      KC_NO, KC_4, KC_5, KC_6,  KC_NO,
-      KC_LSFT, KC_NO, KC_LBRC, KC_RBRC, KC_NO,      KC_NO, KC_1, KC_2, KC_3, KC_DOT,
-                  KC_NO,KC_NO,KC_TRNS,             MO(_ARROWS),KC_0,KC_NO
+      KC_LCTL, KC_NO, KC_LPRN, KC_RPRN, KC_NO,      KC_NO, KC_4, KC_5, KC_6,  KC_NO,
+      KC_LSFT, KC_NO, KC_LBRC, KC_RBRC, KC_NO,      KC_NO, KC_1, KC_2, KC_3,  KC_DOT,
+                  KC_NO,KC_NO,KC_TRNS,                   MO(_ARROWS),KC_0,KC_NO
       ),
 
   /* Keymap 3: Arrows and OS
-  * ,-----------------------------.       ,------------------------------------.
-  * |      |     |     |     |     |      | MV-DWN| D-DWN | D-UP | MV-UP |     |
+  * ,------------------------------.      ,------------------------------------.
+  * |      |     |     |     |     |      |       |       |      |       |     |
   * |------+-----+-----+-----+-----|      |-------+-------+------+-------+-----|
   * | CTRL |HOME | PUP |     |     |      | LEFT  | DOWN  |  UP  | RIGHT |     |
   * |------+-----+-----+-----+-----|      |-------+-------+------+-------+-----|
@@ -223,14 +224,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *        '-----------------'                   '-----------------'
   */
   [_ARROWS] = LAYOUT_split_3x5_3(
-      KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,     SGUI(KC_PGDN),LGUI(KC_PGDN), LGUI(KC_PGUP), SGUI(KC_PGUP), KC_NO,
+      KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,     KC_NO,        KC_NO,         KC_NO,         KC_NO,         KC_NO,
       KC_LCTL, KC_HOME, KC_PGUP, KC_NO, KC_NO,     KC_LEFT,      KC_DOWN,       KC_UP,         KC_RGHT,       KC_NO,
-      KC_LSFT, KC_END,  KC_PGDN, KC_NO, KC_NO,     KC_NO,        KC_NO,         KC_NO,         KC_NO,       KC_NO,
+      KC_LSFT, KC_END,  KC_PGDN, KC_NO, KC_NO,     KC_NO,        KC_NO,         KC_NO,         KC_NO,         KC_NO,
                   KC_NO,KC_NO,KC_TRNS,                   KC_TRNS,KC_NO,KC_NO
       ),
       
   /* Keymap 4: Tmux navigation layer
-  * ,--------------------------------.      ,---------------------------------------.
+  * ,----------------------------------.      ,---------------------------------------.
   * | Vi Mcr |     |     |     | SP VT |      | Vi Ynk|       |       |       | Vi PT |
   * |--------+-----+-----+-----+-------|      |-------+-------+-------+-------+-------|
   * |        | SCRL|     |     |       |      | PN LT | PN DN | PN UP | PN RT | SP HZ |
@@ -246,5 +247,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,     TMUX_SCROLL, KC_NO,    KC_NO, KC_NO,           TMUX_PN_LT,  TMUX_PN_DN, TMUX_PN_UP, TMUX_PN_RT, TMUX_SP_HZ,
       TMUX_ZOOM, KC_NO,       TMUX_NEW, KC_NO, KC_NO,           TMUX_WN_LT,  KC_NO,      KC_NO,      TMUX_WN_RT, KC_NO,
                   KC_NO,KC_NO,KC_NO,                              KC_NO,KC_TRNS,KC_NO
-      )
+      ),
+
+  /* Keymap 5: Gnome Shell (PopOS)
+  * ,------------------------------.      ,--------------------------------------.
+  * |      |     |     |     |     |      | MV-DWN| D-DWN | D-UP  | MV-UP | Flip |
+  * |------+-----+-----+-----+-----|      |-------+-------+-------+-------+------|
+  * |      |     |     |     |     |      |  W-L  | W-DWN | W-UP  | W-R   |      |
+  * |------+-----+-----+-----+-----|      |-------+-------+-------+-------+------|
+  * |      |     |     |     |     |      | Scrns | Apps  | Close |       |      |
+  * `------+-----+-----+-----+-----'      `--------------------------------------'
+  *        .-----------------.                   .-----------------.
+  *        |     |     |     |                   |     |     | HLD |
+  *        '-----------------'                   '-----------------'
+  */
+  [_POPOS] = LAYOUT_split_3x5_3(
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     SGUI(KC_J), LCTL(LGUI(KC_J)), LCTL(LGUI(KC_K)), SGUI(KC_K), LGUI(KC_O),
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     LGUI(KC_H), LGUI(KC_J),       LGUI(KC_K),       LGUI(KC_L), KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     LGUI(KC_D), LGUI(KC_A),       LGUI(KC_Q),       KC_NO,      KC_NO,
+                  KC_NO,KC_NO,KC_NO,                   KC_NO,KC_NO,KC_TRNS
+      ),
 };
