242085782d37695187d31c02d5fa8b58ee9efab1
SandwichRising <46250499+SandwichRising@users.noreply.github.com>
2021-11-14 15:44:50 -0800

    Update keyboards/sandwich/keeb68/keymaps/grv_esc/keymap.c
    Formatting change
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/sandwich/keeb68/keymaps/grv_esc/keymap.c
+++ keyboards/sandwich/keeb68/keymaps/grv_esc/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, 	KC_LGUI, KC_LALT,               	KC_SPC, KC_EQL,	KC_RBRC,KC_RALT,MO(_FN),KC_RCTL, KC_LEFT, 	KC_DOWN, 	KC_RGHT
 		
     ),
-    [_FN] = LAYOUT_KEEB68(
+    [_FN] = LAYOUT(
         _______, 	KC_F1,	 KC_F2, 	KC_F3, 	KC_F4, 	KC_F5, 	KC_F6, 	KC_F7, 	KC_F8,  KC_F9, 	KC_F10,  KC_F11, 	KC_DEL,		KC_PSCREEN,
 		_______,	_______, BL_INC,	_______,_______,_______,_______,_______,_______,_______,_______,_______,	_______,	KC_PAUSE,
 		_______,	BL_TOGG, BL_DEC,	BL_BRTG,_______,_______,_______,_______,_______,_______,_______,_______,	_______,	KC_HOME,
