a2df6ed476e5db03ec6ced9db9956eedff946288
Josh Hinnebusch <joshhinnebusch@gmail.com>
2021-11-15 14:53:26 -0500

    Update keyboards/hineybush/h65_hotswap/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/hineybush/h65_hotswap/keymaps/default/keymap.c
+++ keyboards/hineybush/h65_hotswap/keymaps/default/keymap.c
@@ -44,25 +44,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_MPLY,  KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS
     )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
-    return true;
-}
