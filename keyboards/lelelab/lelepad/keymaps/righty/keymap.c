379e49058f8ed52f40db40a4fe4ae49d227b90d6
Lelelab <andywith@163.com>
2021-11-15 11:05:41 +0800

    Update keyboards/lelelab/lelepad/keymaps/righty/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lelelab/lelepad/keymaps/righty/keymap.c
+++ keyboards/lelelab/lelepad/keymaps/righty/keymap.c
@@ -14,8 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "quantum.h"
-#include "matrix.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
