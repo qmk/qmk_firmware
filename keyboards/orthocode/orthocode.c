c211dc21eb65b586dff01b705174d986347074a3
Jordan Blackbourn <blackbournj@gmail.com>
2021-11-16 10:26:12 +1300

    Changes as per review

--- keyboards/orthocode/orthocode.c
+++ keyboards/orthocode/orthocode.c
@@ -17,6 +17,7 @@
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
     // Volume control
     if (clockwise) {
         tap_code(KC_VOLU);
