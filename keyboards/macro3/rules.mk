2cc2ca1a7de00d2bdd827a20dc649e27f5837c78
Albert Y <76888457+filterpaper@users.noreply.github.com>
2021-11-16 07:59:48 +0800

    Remove directive from the top.
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/macro3/rules.mk
+++ keyboards/macro3/rules.mk
@@ -7,7 +7,6 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-ENCODER_ENABLE = yes
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
