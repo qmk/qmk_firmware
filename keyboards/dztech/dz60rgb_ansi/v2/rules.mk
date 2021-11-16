398e3b38526767d80c50f01b5e66835e6d0dfd59
Drashna Jael're <drashna@live.com>
2021-11-16 15:36:00 -0800

    [Keyboard] Fix handful of boards compiling too large due to RGB matrix changes

--- keyboards/dztech/dz60rgb_ansi/v2/rules.mk
+++ keyboards/dztech/dz60rgb_ansi/v2/rules.mk
@@ -22,3 +22,5 @@ AUDIO_ENABLE = no              # Audio output
 RGB_MATRIX_ENABLE = yes        # Use RGB matrix
 RGB_MATRIX_DRIVER = IS31FL3733
 NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
+
+LTO_ENABLE = yes
