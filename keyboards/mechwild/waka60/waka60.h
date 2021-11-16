cda1768d4a495007d0c6a7b13eee9ef6128ea355
Kyle McCreery <mccreery.kyle@gmail.com>
2021-11-15 21:17:16 -0500

    Update keyboards/mechwild/waka60/waka60.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechwild/waka60/waka60.h
+++ keyboards/mechwild/waka60/waka60.h
@@ -29,13 +29,6 @@
  * represents the switch matrix.
  */
  
- /*
-  * KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  * KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  * MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
-  * KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-  * KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC, KC_SPC,   KC_SPC,  KC_SPC,  MO(1),   KC_RALT, KC_RGUI, KC_RCTL
-  */
 #define LAYOUT( \
 	k00, k01, k02, k03, k04, k05,      k50, k51, k52, k53, k54, k55, \
 	k10, k11, k12, k13, k14, k15,      k60, k61, k62, k63, k64, k65, \
