73f5d7219b7142fd5018381d4b02862f52bdb04c
rooski15 <34818505+rooski15@users.noreply.github.com>
2021-11-15 16:40:33 -0800

    Update rules.mk
    Added VIA_ENABLE

--- keyboards/walletburner/cajal/rules.mk
+++ keyboards/walletburner/cajal/rules.mk
@@ -18,3 +18,4 @@ BACKLIGHT_ENABLE = no   # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = yes
 ENCODER_ENABLE = yes    # Enable support for EC11 Rotary Encoder
+VIA_ENABLE = yes
