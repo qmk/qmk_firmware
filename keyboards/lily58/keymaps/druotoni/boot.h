4e4ce56312956111c89edf6634dd919da60794f9
druotoni <89318351+druotoni@users.noreply.github.com>
2021-11-17 00:37:03 +0100

    Add files via upload

---
+++ keyboards/lily58/keymaps/druotoni/boot.h
@@ -0,0 +1,2 @@
+void render_boot(void);
+void reset_boot(void);
---
+++ keyboards/lily58/keymaps/druotoni/layer_frame.h
@@ -0,0 +1,5 @@
+enum layer_number { _QWERTY = 0, _LOWER, _RAISE };
+
+void render_gears(void) ;
+void render_layer_frame(gui_state_t t);
+void update_layer_frame(layer_state_t state);
