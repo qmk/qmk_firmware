9a34d09b99da961633441d3d2f3e59c0ee4e4a4d
Jels-kb <joahnelson@gmail.com>
2021-11-16 17:26:35 +1000

    Update PR
    remove bootloader options, explicit rgb options
    
    Co-Authored-By: Drashna Jaelre <drashna@live.com>

--- keyboards/winkeyless/b87/rules.mk
+++ keyboards/winkeyless/b87/rules.mk
@@ -1,14 +1,5 @@
 # MCU name
 MCU = atmega32a
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = bootloadHID
 
 # Build Options
@@ -22,9 +13,8 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover
+NKRO_ENABLE = no            # USB Nkey Rollover Doesn't work on V-USB
 BACKLIGHT_ENABLE = yes 	    # Enable keyboard backlight functionality on B7 by default
-UNICODE_ENABLE = no         # Unicode
 AUDIO_ENABLE = no           # Audio output on port C6
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 WS2812_DRIVER = i2c
