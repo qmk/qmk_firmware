8ff957e0f29892a7df77e3ad1a0d7e4d72ff08f6
Ramon Imbao <ramonimbao@gmail.com>
2021-11-19 00:05:18 +0800

    Remove QoL improvements

--- keyboards/ramonimbao/tkl_ff/config.h
+++ keyboards/ramonimbao/tkl_ff/config.h
@@ -150,7 +150,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
-
-// QoL improvements
-#define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
