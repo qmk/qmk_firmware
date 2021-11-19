1bf8dc6b93a35ae581bb56e731df56109351d2e8
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-11-19 13:27:16 -0800

    rename LAYOUT to LAYOUT_ortho_6x13

--- keyboards/minimon/index_tab/index_tab.h
+++ keyboards/minimon/index_tab/index_tab.h
@@ -18,7 +18,7 @@
 
 #include "quantum.h"
 
-#define LAYOUT( \
+#define LAYOUT_ortho_6x13( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0a, K0b, K0c, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1a, K1b, K1c, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2a, K2b, K2c, \
