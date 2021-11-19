1a24578a18d26a272900a90dd9caee1b1eaa1671
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-11-18 17:05:03 -0800

    split89.h: use XXX for KC_NO

--- keyboards/handwired/split89/split89.h
+++ keyboards/handwired/split89/split89.h
@@ -18,6 +18,8 @@
 
 #include "quantum.h"
 
+#define XXX KC_NO
+
 /* This is a shortcut to help you visually see your layout.
  *
  * The first section contains all of the arguments representing the physical
@@ -35,16 +37,16 @@
     k75, k76, k77, k78,    k79,             k81,          k82, k83, k84, k85,   k86, k87, k88 \
     ) \
     { \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k00, k01, k02, k03, k04, k05 }, \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k16, k17, k18, k19, k20, k21 },  \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k32, k33, k34, k35, k36, k37 },  \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k48, k49, k50, k51, k52, k53 },  \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k61, k62, k63, k64, k65, k66, },  \
-      { KC_NO, KC_NO, KC_NO, KC_NO, k74, k75, k76, k77, k78, k79, },  \
+      { XXX, XXX, XXX, XXX, k00, k01, k02, k03, k04, k05 }, \
+      { XXX, XXX, XXX, XXX, k16, k17, k18, k19, k20, k21 }, \
+      { XXX, XXX, XXX, XXX, k32, k33, k34, k35, k36, k37 }, \
+      { XXX, XXX, XXX, XXX, k48, k49, k50, k51, k52, k53 }, \
+      { XXX, XXX, XXX, XXX, k61, k62, k63, k64, k65, k66 }, \
+      { XXX, XXX, XXX, XXX, k74, k75, k76, k77, k78, k79 }, \
       { k06, k07, k08, k09, k10, k11, k12, k13, k14, k15 }, \
-      { k22, k23, k24, k25, k26, k27, k28, k29, k30, k31 },  \
-      { k38, k39, k40, k41, k42, k43, k44, k45, k46, k47 },  \
-      { k54, k55, k56, k57, k58, k59, k60, KC_NO, KC_NO, KC_NO },  \
-      { k67, k68, k69, k70, k71, KC_NO, k72, KC_NO, k73, KC_NO },  \
-      { k80, k81, KC_NO, k82, k83, k84, k85, k86, k87, k88 }  \
+      { k22, k23, k24, k25, k26, k27, k28, k29, k30, k31 }, \
+      { k38, k39, k40, k41, k42, k43, k44, k45, k46, k47 }, \
+      { k54, k55, k56, k57, k58, k59, k60, XXX, XXX, XXX }, \
+      { k67, k68, k69, k70, k71, XXX, k72, XXX, k73, XXX }, \
+      { k80, k81, XXX, k82, k83, k84, k85, k86, k87, k88 }  \
 }
