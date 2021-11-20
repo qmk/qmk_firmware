aaffc314b1c2f6532ce721acda52d7730fec7216
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-11-20 03:47:36 -0800

    rules.mk: update Community Layout support

--- keyboards/mokey/ginkgo65/rules.mk
+++ keyboards/mokey/ginkgo65/rules.mk
@@ -20,4 +20,4 @@ BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-LAYOUTS = 65_ansi_blocker 65_iso_blocker_split_bs
+LAYOUTS = 65_ansi_blocker 65_ansi_blocker_split_bs 65_iso_blocker 65_iso_blocker_split_bs
