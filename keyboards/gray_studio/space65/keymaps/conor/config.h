061107fe82bd2ea22dfc39196af070d4847660e6
Conor Burns <mail@conor-burns.com>
2021-11-20 13:32:54 +0100

    added keymap conor with rgb layers

---
+++ keyboards/gray_studio/space65/keymaps/conor/config.h
@@ -0,0 +1,25 @@
+/* Copyright 2021 Conor Burns
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define RGBLIGHT_LAYERS
+#define GRAVE_ESC_GUI_OVERRIDE
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_EFFECT_BREATHING + 2
+#define RGBLIGHT_DEFAULT_HUE 152
+#define RGBLIGHT_DEFAULT_SAT 232
+#define RGBLIGHT_DEFAULT_VAR 255
+#define RGBLIGHT_DEFAULT_SPD 2
