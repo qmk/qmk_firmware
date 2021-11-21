362cf1b5c78d49de49ed9a58b67d92c16afb23b8
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-11-20 22:27:01 -0800

    portal_66/soldered: rename LAYOUT_65_ansi to LAYOUT_65_ansi_blocker

--- keyboards/portal_66/soldered/soldered.h
+++ keyboards/portal_66/soldered/soldered.h
@@ -51,7 +51,7 @@
     { k40, k41, k42, XXX, XXX, XXX, k46, XXX, XXX, k49, XXX, k4B, k4C, k4D, XXX, k4F }  \
 }
 
-#define LAYOUT_65_ansi( \
+#define LAYOUT_65_ansi_blocker( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C,      k0E, k0F, \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C,      k1D, k1F, \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B,           k2D, k2F, \
