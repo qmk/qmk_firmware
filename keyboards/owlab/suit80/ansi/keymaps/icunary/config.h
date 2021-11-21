e0a60af4338a5821abe19147b0271dcade936ff9
icunary <91033437+icunary@users.noreply.github.com>
2021-11-20 19:56:29 -0500

    Added Custom Suit Firmware
    Created custom keymap
    with 1000hz polling rate
    12 keys per scan
    changed default layout

---
+++ keyboards/owlab/suit80/ansi/keymaps/icunary/config.h
@@ -0,0 +1,5 @@
+#define USB_POLLING_INTERVAL_MS 1
+//#define DYNAMIC_KEYMAP_LAYER_COUNT 5 //for 1 more via layer
+//#undef DEBOUNCE
+//#define DEBOUNCE 3 //default is 5
+#define QMK_KEYS_PER_SCAN 12 
