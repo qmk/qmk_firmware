fbc50370247a0249e1d1d277949d8239643f85af
bululau <80664187+bululau@users.noreply.github.com>
2021-11-21 14:10:38 +0800

    update

---
+++ keyboards/lpad/plus5c/config.h
@@ -0,0 +1,91 @@
+/* Copyright 2021 bululau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    lpad
+#define PRODUCT         plus
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 6
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { D2, D3, D4, D5, D6 }
+#define MATRIX_COL_PINS { B4, B5, B6, B7, D7, E6 }
+#define UNUSED_PINS
+
+/* via plus layer */
+// #ifndef DYNAMIC_KEYMAP_LAYER_COUNT
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+
+#define ENCODERS_PAD_B { C6, F5, F0 }
+#define ENCODERS_PAD_A { C7, F4, F1 }
+
+#define ENCODER_RESOLUTION 4
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+// #define OLED_FONT_H "./lib/glcdfont.c"
+
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 3
+#endif
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+#define RGB_DI_PIN B0
+#ifdef RGB_DI_PIN
+// #define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
+// #define RGB_DI_PIN B0
+// #define DRIVER_LED_TOTAL 23 //一共有多少个灯，64+12个底灯﻿
+// #define RGB_DISABLE_WHEN_USB_SUSPENDED true
+#define RGBLED_NUM 30
+// #define RGBLIGHT_HUE_STEP 8
+// #define RGBLIGHT_SAT_STEP 8
+// #define RGBLIGHT_VAL_STEP 8
+// #define RGBLIGHT_SLEEP
+// #define RGBLIGHT_LIMIT_VAL 255
+#endif
+
+/* EEPROM for via */
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 7
