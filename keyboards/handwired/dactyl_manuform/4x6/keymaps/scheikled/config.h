81028d336b15ac8c9db64b13a043bf44b7904ea1
Paul Maria Scheikl <paul.scheikl@kit.edu>
2021-11-21 19:45:05 +0100

    Removed duplicate code. Updated copyright info.

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/scheikled/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/scheikled/config.h
@@ -4,16 +4,9 @@
 
 #pragma once
 
-
 #define USE_I2C
-//
-// VBUS is used to power the LEDs without going "through" the MCU -> detecting handedness cannot rely on VBUS
-// From the QMK documentation: Enabling this option changes the startup behavior to listen for an active USB communication to delegate which part is master and which is slave. With this option enabled and theres's USB communication, then that half assumes it is the master, otherwise it assumes it is the slave.
-/* #define SPLIT_USB_DETECT */
-
 
 #define SPLIT_HAND_PIN F6
-/* #define SPLIT_HAND_PIN_LOW_IS_LEFT */
 
 // WS2812 RGB LED strip input and number of LEDs
 #undef RGB_DI_PIN
