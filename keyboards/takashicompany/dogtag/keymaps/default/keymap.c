bdb50070dd815c7ec1750e690c454482f182d566
takashicompany <t@kashi.company>
2021-11-21 23:08:47 +0900

    fix rotary

--- keyboards/takashicompany/dogtag/keymaps/default/keymap.c
+++ keyboards/takashicompany/dogtag/keymaps/default/keymap.c
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool encoder_update_user(uint8_t index, bool clockwise) {
 
     if (clockwise) {
-        tap_code(KC_MS_WH_UP);
-    } else {
         tap_code(KC_MS_WH_DOWN);
+    } else {
+        tap_code(KC_MS_WH_UP);
     }
 
     return true;
