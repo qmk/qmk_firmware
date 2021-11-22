196d85b9d397971434993384aba94de64e161be4
ChrisGVE <christian.berclaz@mac.com>
2021-11-23 00:44:56 +0800

    dz60 and dz65rgb_wkl: Implemented a space shift to get numbers and shift numbers in the middle row and above, similar to the Adelie keyboard.

--- keyboards/dz60/keymaps/chrisgve_tsangan/keymap.c
+++ keyboards/dz60/keymaps/chrisgve_tsangan/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 		CPS_CTL, KC_A,    KC_S,    M_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   R_SHIFT, ADJ_M,
-		XXXXXXX, KC_LALT, KC_LGUI,                                     KC_SPC,                                      KC_RGUI, KC_RALT, XXXXXXX
+		XXXXXXX, KC_LALT, KC_LGUI,                                     SPC_RSE,                                     KC_RGUI, KC_RALT, XXXXXXX
 	),
 
 	[_MAC_NAV_1] = LAYOUT_60_tsangan_hhkb(
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 		CPS_CTL, KC_A,    KC_S,    L_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   R_SHIFT, ADJ_L,
-		XXXXXXX, KC_LGUI, KC_LALT,                                     KC_SPC,                                      KC_RALT, KC_RGUI, XXXXXXX
+		XXXXXXX, KC_LGUI, KC_LALT,                                     SPC_RSE,                                     KC_RALT, KC_RGUI, XXXXXXX
 	),
 
 	[_LINUX_NAV_1] = LAYOUT_60_tsangan_hhkb(
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 		CPS_CTL, KC_A,    KC_S,    W_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   R_SHIFT, ADJ_W,
-		XXXXXXX, KC_LGUI, KC_LALT,                                     KC_SPC,                                      KC_RALT, KC_RGUI, XXXXXXX
+		XXXXXXX, KC_LGUI, KC_LALT,                                     SPC_RSE,                                     KC_RALT, KC_RGUI, XXXXXXX
 	),
 
 	[_WIN_NAV_1] = LAYOUT_60_tsangan_hhkb(
@@ -72,6 +72,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,          _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______,
+		_______, _______, _______,                                     _______,                                     _______, _______, _______
+	),
+
+	[_RAISE] = LAYOUT_60_tsangan_hhkb(
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______,
+		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,          KC_EQL, 
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______,
 		_______, _______, _______,                                     _______,                                     _______, _______, _______
 	),
