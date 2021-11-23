2d7b8f1f9050308af5ff0776d38adef0fd39a26c
JX <572673807@qq.com>
2021-11-23 10:36:50 +0800

    Update rules.mk

--- keyboards/yandrstudio/zhou65/f401/rules.mk
+++ keyboards/yandrstudio/zhou65/f401/rules.mk
@@ -1,11 +1,6 @@
 # MCU name
 MCU = STM32F401
 
-# Address of the bootloader in system memory
-STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
-
 # Bootloader selection
 BOOTLOADER = stm32-dfu
-# ENCODER_ENABLE = yes
-# # project specific file
-CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
+
