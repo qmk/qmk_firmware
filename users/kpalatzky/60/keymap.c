5a6cfff5120ec6dbd32d2b0e3552a684d9b677d7
kpalatzky <kevin.palatzky@gmail.com>
2021-11-24 19:54:31 +0100

    Fixed copyright template

--- users/kpalatzky/60/keymap.c
+++ users/kpalatzky/60/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Hadi Iskandarani
+/* Copyright 2021 Kevin Palatzky
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,11 @@
  */
 #include QMK_KEYBOARD_H
 
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
 #define _BASE 0
 #define _NAV 1
 #define _SYMB 2
@@ -30,9 +35,13 @@ enum layers {
 };
 
 
+// Shortcut to make keymap more readable
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    // DEFAULT 60% ISO (Template):
+    // DEFAULT 60% ISO Template:
     //
     //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────────────┐
     //  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
