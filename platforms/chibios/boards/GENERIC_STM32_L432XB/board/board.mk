960c82b6e7657b0c37760f843d667daf488b7b5a
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2021-11-25 17:04:06 +0800

    It already exists a PR for STML432KC, and keychron q2 based on STM32L432KC.

--- platforms/chibios/boards/GENERIC_STM32_L432XB/board/board.mk
+++
@@ -1,9 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO32_L432KC/board.c
-
-# Required include directories
-BOARDINC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO32_L432KC
-
-# Shared variables
-ALLCSRC += $(BOARDSRC)
-ALLINC  += $(BOARDINC)
