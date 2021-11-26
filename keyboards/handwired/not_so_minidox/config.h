b49d11921b64ce863b5ed5d1c47a1aa4ff5b826d
zvecr <git@zvecr.com>
2021-11-26 00:44:07 +0000

    Convert not_so_minidox to SPLIT_KEYBOARD

--- keyboards/handwired/not_so_minidox/config.h
+++ keyboards/handwired/not_so_minidox/config.h
@@ -40,6 +40,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 
+/*
+ * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
+ */
+#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+
 //#define EE_HANDS
 
 #define MASTER_LEFT
