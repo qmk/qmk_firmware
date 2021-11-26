34cecf4b0cb2dae0906e0aeb3b7547300530681f
zvecr <git@zvecr.com>
2021-11-26 00:34:03 +0000

    Convert ergoinu to SPLIT_KEYBOARD

--- keyboards/dm9records/ergoinu/post_rules.mk
+++
@@ -1,9 +0,0 @@
-ifneq ($(strip $(ERGOINU)),)
-  ifeq ($(findstring promicroled, $(ERGOINU)), promicroled)
-    DISABLE_PROMICRO_LEDs = no
-  endif
-endif
-
-ifeq ($(strip $(DISABLE_PROMICRO_LEDs)), yes)
-  OPT_DEFS += -DDISABLE_PROMICRO_LEDs
-endif
