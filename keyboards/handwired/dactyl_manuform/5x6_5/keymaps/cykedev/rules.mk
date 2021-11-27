654b27e1fbfb23bfd5afe360f42645bb17368a18
Christian Eiden <christian.eiden@neteleven.de>
2021-11-27 14:49:57 +0100

    Update keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/cykedev/rules.mk
@@ -1,17 +1,2 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = caterina
-
-BOOTMAGIC_ENABLE    = yes     # Enable Bootmagic Lite
-MOUSEKEY_ENABLE     = no      # Mouse keys
-EXTRAKEY_ENABLE     = yes     # Audio control and System control
-CONSOLE_ENABLE      = no      # Console for debug
-COMMAND_ENABLE      = no      # Commands for debug and configuration
-NKRO_ENABLE         = yes     # USB Nkey Rollover
-BLUETOOTH_ENABLE    = no      # Enable Bluetooth
-AUDIO_ENABLE        = no      # Audio output
-KEY_OVERRIDE_ENABLE = yes     # Enable Key Overrides, see https://docs.qmk.fm/#/feature_key_overrides
-SPLIT_KEYBOARD      = yes     # Split Keyboard
-LTO_ENABLE          = no
+MOUSEKEY_ENABLE = no
+KEY_OVERRIDE_ENABLE = yes
