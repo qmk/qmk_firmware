edeb7fcf2847de402783822f14a27713df5d05f4
xia0 <cpt.acky@gmail.com>
2021-11-29 01:02:47 +1100

    added licence in header

--- keyboards/handwired/wakizashi40/keymaps/default/keymap.c
+++ keyboards/handwired/wakizashi40/keymaps/default/keymap.c
@@ -1,3 +1,18 @@
+/* Copyright 2021 xiao (@xia0)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /*
 [8:10 PM] я лов днкс: hey i was wondering if anyone could help me understand how to get a board onto VIA
 [8:11 PM] я лов днкс: from what I gather, i have to get it into the qmk repo then wait for the via team to update?
