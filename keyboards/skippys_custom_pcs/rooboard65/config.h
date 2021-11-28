f1dd889151f707105e77121d57133af124042706
Felix Jen <fjen@felixjen.com>
2021-11-27 22:04:04 -0600

    Update keyboards/skippys_custom_pcs/rooboard65/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/skippys_custom_pcs/rooboard65/config.h
+++ keyboards/skippys_custom_pcs/rooboard65/config.h
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x36b6 // Skippys Custom PCs
+#define VENDOR_ID    0x36B6 // Skippys Custom PCs
 #define PRODUCT_ID   0x0002 // Rooboard65
 #define DEVICE_VER   0x0001 // Version 1
 #define MANUFACTURER    FJLabs
