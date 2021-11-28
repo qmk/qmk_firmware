7ae287dd2909e045a161dc60384d487f324ec396
Wolf Van Herreweghe <wolfvanh@gmail.com>
2021-11-28 05:06:23 +0100

    Capitalize vendor ID

--- keyboards/skippys_custom_pcs/roopad/config.h
+++ keyboards/skippys_custom_pcs/roopad/config.h
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x36b6 // skippys_custom_pcs
+#define VENDOR_ID    0x36B6 // skippys_custom_pcs
 #define PRODUCT_ID   0x0001 // RooPad
 #define DEVICE_VER   0x0001 // Version 1
 #define MANUFACTURER    ToastyStoemp
