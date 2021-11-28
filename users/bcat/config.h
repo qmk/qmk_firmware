cfcffdd2a59b331834ed1c2dfb86b203aa111b75
Jonathan Rascher <jon@bcat.name>
2021-11-28 15:20:19 -0600

    Enable specific RGBLIGHT modes instead of default
    The general trend these days seems to be enabling only the modes you
    want, so I'm manually expanding the ones currently enabled by
    RGBLIGHT_ANIMATIONS.
    
    I'd like to try out the TWINKLE mode too, but it seems not to work at
    all on ARM right now, and all my usable RGBLIGHT keebs are ARM boards.

--- users/bcat/config.h
+++ users/bcat/config.h
@@ -87,8 +87,18 @@
 #    define RGBLIGHT_SAT_STEP 17
 #    define RGBLIGHT_VAL_STEP 17
 
-/* Turn on additional RGB animations. */
-#    define RGBLIGHT_ANIMATIONS
+/* Enable specific underglow animation modes. (Skip TWINKLE because it seems to
+ * be broken on ARM: https://github.com/qmk/qmk_firmware/issues/15345.)
+ */
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 #endif
 
 #if defined(BACKLIGHT_ENABLE)
