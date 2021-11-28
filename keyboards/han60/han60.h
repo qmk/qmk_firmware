91f6dc48e29d59fac27cc69604729f94b4220711
Farhan Rob <73175556+farhandsome@users.noreply.github.com>
2021-11-28 14:24:24 -0600

    Update keyboards/han60/han60.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/han60/han60.h
+++ keyboards/han60/han60.h
@@ -86,7 +86,7 @@
 	{ K40, K41, K42, K43, K44, XXX, XXX, K47, XXX, K49, XXX, K4B, XXX, XXX }  \
 }
 
-#define LAYOUT_tsangan_ISO_arrowkeys( \
+#define LAYOUT_tsangan_iso_arrowkeys( \
 	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
 	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K2C, \
 	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K1D, \
