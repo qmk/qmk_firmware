ce7f8120a98f685f5b1d6b7f648e73cc90f6261c
Franz Miltz <franz@miltz.me>
2021-11-29 23:56:32 +0000

    Properly delete lower layer keys

--- keyboards/keebio/iris/keymaps/miltfra/keymap.c
+++ keyboards/keebio/iris/keymaps/miltfra/keymap.c
@@ -13,6 +13,11 @@ enum custom_keycodes {
   ADJUST,
 };
 
+#define KC_LBE KC_LEFT_CURLY_BRACE
+#define KC_RBE KC_RIGHT_CURLY_BRACE
+#define KC_LBK KC_LBRACKET
+#define KC_RBK KC_RBRACKET
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
@@ -21,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GRAVE, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_GRAVE,KC_A,    KC_S,   KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LBRACKET,KC_Z, KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRACKET,
+     XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     MT(MOD_LCTL,KC_ESC),KC_LALT, KC_SPC,   MT(MOD_LSFT,KC_ENT),LOWER,KC_BSPC
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -31,15 +36,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_EQUAL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,
+     KC_EQUAL,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______,  _______, _______, _______, _______, _______,                           KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,_______, _______,
+     XXXXXXX, XXXXXXX, KC_LBE,  KC_LBK,  KC_RBK,  KC_RBE,                             KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼───────┼────────┤
-     _______,  _______, _______, _______, _______,LCA(KC_DEL),_______,       _______, _______, _______, _______, _______, _______, _______,
+     XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,LCA(KC_DEL),XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, KC_LWIN, KC_LSFT,                   _______, _______, KC_DEL
+                                    XXXXXXX, KC_LWIN, KC_LSFT,                   XXXXXXX, XXXXXXX, KC_DEL
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -71,6 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
+
+#undef KC_LBE
+#undef KC_RBE
+#undef KC_LBK
+#undef KC_RBK
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
