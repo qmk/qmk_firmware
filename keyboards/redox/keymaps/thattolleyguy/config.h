be208a17f83ed0e6ed86f94c6923811dcba9c08f
Tyler Tolley <thattolleyguy@gmail.com>
2021-11-29 08:53:39 -0700

    Update licenses

--- keyboards/redox/keymaps/thattolleyguy/config.h
+++ keyboards/redox/keymaps/thattolleyguy/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Mattia Dal Ben <matthewdibi@gmail.com>
+Copyright 2021 Tyler Tolley <thattolleyguy@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
