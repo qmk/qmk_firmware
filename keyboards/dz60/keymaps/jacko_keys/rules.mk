a3f870c5852552007aaafa4ece1217afb0deeb08
jackokring <jackokring@gmail.com>
2021-11-29 14:09:50 +0000

    As per pull request ... ting.

--- keyboards/dz60/keymaps/jacko_keys/rules.mk
+++ keyboards/dz60/keymaps/jacko_keys/rules.mk
@@ -1,11 +1,11 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration (just Escape!!)
-#KEY_LOCK_ENABLE = yes		# Keylock mode
-#UNICODE_ENABLE = yes		# Linux unicode default
-#DYNAMIC_MACRO_ENABLE = yes	# Use a dynamic macro (might have to remove)
-UNICODEMAP_ENABLE = yes		# Use extended Unicode
-RGBLIGHT_ENABLE = yes			# Use lights for background RGB
-BACKLIGHT_ENABLE = yes		# Use monchromatic backlights for keys
-LTO_ENABLE = yes					# Link time optimizer for size
+BOOTMAGIC_ENABLE = lite
+#KEY_LOCK_ENABLE = yes
+#UNICODE_ENABLE = yes
+#DYNAMIC_MACRO_ENABLE = yes
+UNICODEMAP_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+BACKLIGHT_ENABLE = yes
+LTO_ENABLE = yes
