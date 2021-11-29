19a023bf1c19870786d0b7f28e23d4d87969e367
jolofsor <jolofsor@gmail.com>
2021-11-29 16:53:11 +0800

    Update keyboards/jolofsor/denial75/config.h
    Remove quotation marks for define product in config.h
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/jolofsor/denial75/config.h
+++ keyboards/jolofsor/denial75/config.h
@@ -22,7 +22,7 @@
 #define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    jsor-hpoli
-#define PRODUCT         "denial75"
+#define PRODUCT         denial75
 
 /* key matrix size */
 #define MATRIX_ROWS 6
