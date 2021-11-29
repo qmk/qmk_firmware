08c307238e7e9b176400b90c4a2dfe760e8508ef
ll3macorn <gamertuber08@gmail.com>
2021-11-29 12:24:39 +0800

    remove old thing

--- keyboards/ll3macorn/password/password.c
+++
@@ -1,3 +0,0 @@
-// Copyright 2021 Ll3macorn (@ll3macorn)
-// SPDX-License-Identifier: GPL-2.0-or-later
-#include "password.h"
--- keyboards/ll3macorn/password/password.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 Ll3macorn (@ll3macorn)
-// SPDX-License-Identifier: GPL-2.0-or-later
-#pragma once
-
-#include "quantum.h"
-
-#define LAYOUT( K00, K01, K02, K03, K04, K05, K06, K07  ) { { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07 } }
-
-
