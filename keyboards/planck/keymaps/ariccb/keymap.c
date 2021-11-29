b583ea1a04f7f73332da572aa5215311986d3602
Aric <aricbouwers@outlook.com>
2021-11-29 09:39:44 -0700

    Implement requested changes

--- keyboards/planck/keymaps/ariccb/keymap.c
+++ keyboards/planck/keymaps/ariccb/keymap.c
@@ -1,3 +1,19 @@
+ /* Copyright 2021 Aric Crosson Bouwers 
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
+
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
@@ -56,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid( /* QWERTY */
-    LT(_FN, KC_ESC),    KC_Q,     KC_W,    KC_E,   KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-    LT(KC_HYPR, KC_TAB),KC_A,     KC_S,    KC_D,   KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
-    KC_LSFT,            KC_Z,     KC_X,    KC_C,   KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, 
-    KC_LCTL,            KC_LGUI,  KC_LALT, KC_ENT, LOWER, KC_SPC, KC_SPC, RAISE, KC_APP,  KC_MPLY, KC_LEFT, KC_RGHT
+    LT(_FN, KC_ESC),      KC_Q,     KC_W,    KC_E,   KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
+    MT(MOD_HYPR, KC_TAB), KC_A,     KC_S,    KC_D,   KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
+    KC_LSFT,              KC_Z,     KC_X,    KC_C,   KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, 
+    KC_LCTL,              KC_LGUI,  KC_LALT, KC_ENT, LOWER, KC_SPC, KC_SPC, RAISE, KC_APP,  KC_MPLY, KC_LEFT, KC_RGHT
  ),
 
  /* MIT Layout (COLEMAK layer)
@@ -75,33 +91,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid( /* COLEMAK */
-    LT(_FN, KC_ESC),    KC_Q,    KC_W,    KC_F,   KC_P,  KC_B,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    LT(KC_HYPR, KC_TAB),KC_A,    KC_R,    KC_S,   KC_T,  KC_G,   KC_M,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT, 
-    KC_LSFT,            KC_Z,    KC_X,    KC_C,   KC_D,  KC_V,   KC_K,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, 
-    KC_LCTL,            KC_LGUI, KC_LALT, KC_ENT, LOWER, KC_SPC, KC_SPC, RAISE, KC_APP,  KC_MPLY, KC_LEFT, KC_RGHT
+    LT(_FN, KC_ESC),      KC_Q,    KC_W,    KC_F,   KC_P,  KC_B,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    MT(MOD_HYPR, KC_TAB), KC_A,    KC_R,    KC_S,   KC_T,  KC_G,   KC_M,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT, 
+    KC_LSFT,              KC_Z,    KC_X,    KC_C,   KC_D,  KC_V,   KC_K,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, 
+    KC_LCTL,              KC_LGUI, KC_LALT, KC_ENT, LOWER, KC_SPC, KC_SPC, RAISE, KC_APP,  KC_MPLY, KC_LEFT, KC_RGHT
  ),
 
 /* MIT Layout (RAISED_LAYER)
  *
  * ,-----------------------------------------------------------------------.
- * |  ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  =  |  :  |  +  |
+ * |  ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  =  |  :  | Bksp|
  * |-----------------------------------------------------------------------|
  * |  `  |  _  |  <  |  [  |  {  |  (  |  )  |  }  |  ]  |  >  |  -  |  "  |
  * |-----------------------------------------------------------------------|
- * |     |     |     |  %  |  +  |  '  |  "  |  #  |  $  |  ^  |  ?  |  |  |
+ * |     |     |     |     |     |  %  |  ^  |  #  |  $  |  +  |  ?  |  |  |
  * |-----------------------------------------------------------------------|
  * |     |     |     |     |     |           |     |     |     |     |     |
  * `-----------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid( /* RAISE */
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_COLN, KC_PPLS,
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_COLN, KC_BSPC,
   KC_GRV,  KC_UNDS, KC_LT,   KC_LBRC, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_RBRC, KC_GT,   KC_MINS, KC_DQUO, 
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_PERC, KC_PPLS, KC_QUOT, KC_DQUO, KC_HASH, KC_DLR,  KC_CIRC, KC_QUES, KC_PIPE, 
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PERC, KC_CIRC, KC_HASH, KC_DLR,  KC_PPLS, KC_QUES, KC_PIPE, 
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* MIT Layout (LOWER)
- *
+ *.
  * ,-----------------------------------------------------------------------.
  * |  `  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Bksp|
  * |-----------------------------------------------------------------------|
--- keyboards/planck/keymaps/ariccb/rules.mk
+++ keyboards/planck/keymaps/ariccb/rules.mk
@@ -3,16 +3,14 @@
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 MIDI_ENABLE = yes           # MIDI controls
-AUDIO_ENABLE = yes          # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = yes          # Audio output
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight. 
 TAP_DANCE_ENABLE = no 		# Enable Tap Dance Features
 LTO_ENABLE = no 			# Link Time Optimization
