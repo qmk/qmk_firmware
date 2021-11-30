fd03c83779b883a2edd930e6aa7f7f9002a115dc
fauxpark <fauxpark@gmail.com>
2021-12-01 04:35:15 +1100

    Remove SLEEP_LED_ENABLE from keyboard-level rules.mk, S

--- keyboards/sparrow62/rules.mk
+++ keyboards/sparrow62/rules.mk
@@ -12,7 +12,6 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
