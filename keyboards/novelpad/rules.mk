23b0ee53c2b21e415c3282b04ff551547cf48a63
fauxpark <fauxpark@gmail.com>
2021-12-01 04:02:02 +1100

    Remove SLEEP_LED_ENABLE from keyboard-level rules.mk, N

--- keyboards/novelpad/rules.mk
+++ keyboards/novelpad/rules.mk
@@ -12,7 +12,6 @@ MOUSEKEY_ENABLE = no	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes  # In-switch LEDs
 AUDIO_ENABLE = no      # There is no available timer or pin for audio on the NovelPad
--- keyboards/noxary/260/rules.mk
+++ keyboards/noxary/260/rules.mk
@@ -12,8 +12,6 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
