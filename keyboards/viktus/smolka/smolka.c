77771da331cf9ed36caf1a724500a8cd9ecfe2be
J.Flanagan <jrfhoutx@comcast.net>
2021-11-29 23:38:17 -0600

    Updates
    updates to info.json to correct lint error
    updates to smolka.c to correct encoder ifdef

--- keyboards/viktus/smolka/smolka.c
+++ keyboards/viktus/smolka/smolka.c
@@ -16,6 +16,22 @@
 
 #include "smolka.h"
 
+#ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-    return encoder_update_user(index, clockwise);
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+        }
+    }
+    return true;
 }
+#endif
