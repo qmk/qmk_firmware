f4ec8e3e3be342cb98a3cf14c7b8da13b11b50d9
fauxpark <fauxpark@gmail.com>
2021-12-02 17:29:11 +1100

    Tidy up keyboard-level NKRO_ENABLE rules, C

--- keyboards/coarse/vinta/rules.mk
+++ keyboards/coarse/vinta/rules.mk
@@ -13,7 +13,7 @@ MOUSEKEY_ENABLE = yes				# Mouse keys
 EXTRAKEY_ENABLE = yes				# Audio control and System control
 CONSOLE_ENABLE = no					# Console for debug
 COMMAND_ENABLE = no    				# Commands for debug and configuration
-NKRO_ENABLE = yes					# USB Nkey Rollover
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 NO_USB_STARTUP_CHECK = no         	# Disable initialization only when usb is plugged in
 
 LAYOUTS = 65_ansi_blocker
