c629f273720474f0b1b6547a367db6c924645f6b
fauxpark <fauxpark@gmail.com>
2021-12-02 18:54:21 +1100

    Tidy up keyboard-level NKRO_ENABLE rules, handwired

--- keyboards/handwired/hexon38/rules.mk
+++ keyboards/handwired/hexon38/rules.mk
@@ -10,7 +10,7 @@ MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
-NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 
 # Disabled build options:
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
