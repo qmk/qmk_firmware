22252ad91fa5c51026c115901c5c2cdc47d1b197
Francisco Escobar <jfescobar18@hotmail.com>
2021-12-02 06:37:46 -0600

    keymap fixed

--- keyboards/mechanickeys/hfkb/hfkb.h
+++ keyboards/mechanickeys/hfkb/hfkb.h
@@ -20,10 +20,10 @@
 
 #define LAYOUT_all( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
-  K10, K11, K12, K13, K14, K15, K16, K01, K18, K19, K1A, K1B, K1C,    K1D,   \
-  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,    K2E,   \
-  K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,           \
-  K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B,   K4C,    K4E, \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, \
+  K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2E,      \
+  K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3E,      \
+  K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, K4E,      \
        K51, K52,                K56,           K59, K5A                      \
 ) \
 { \
@@ -37,7 +37,7 @@
 
 #define LAYOUT_ANSI( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
-  K10, K11, K12, K13, K14, K15, K16, K01, K18, K19, K1A, K1B, K1C,    K1D,   \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,    K1D,   \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,    K2E,   \
   K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3C,      \
   K40,      K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C,      K4E, \
@@ -54,7 +54,7 @@
 
 #define LAYOUT_ANSI_Splitted_Backspace( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
-  K10, K11, K12, K13, K14, K15, K16, K01, K18, K19, K1A, K1B, K1C, K1D, K1E, \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,    K2E,   \
   K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3C,      \
   K40,      K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C,      K4E, \
@@ -71,7 +71,7 @@
 
 #define LAYOUT_Alps_Big_Ass_Enter( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
-  K10, K11, K12, K13, K14, K15, K16, K01, K18, K19, K1A, K1B, K1C, K1D, K1E, \
+  K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,           \
   K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3C,      \
   K40,      K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C,      K4E, \
