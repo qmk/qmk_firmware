f7f2df64c910b395f9f91a19bd699409b8af9a66
Injung Lee <edndn@kaist.ac.kr>
2021-12-02 12:39:38 +0900

    Update keyboards/keebio/foldkb/keymaps/edndn-ctrlmidrow/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keebio/foldkb/keymaps/edndn-ctrlmidrow/keymap.c
+++ keyboards/keebio/foldkb/keymaps/edndn-ctrlmidrow/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        switch(biton32(layer_state)){
+        switch(get_highest_layer(layer_state)){
             case 0:
                 if (clockwise){
                     tap_code(KC_VOLU);
