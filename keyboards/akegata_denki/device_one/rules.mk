60c4d994c90013f2b0f9d300134d54703380cf72
fauxpark <fauxpark@gmail.com>
2021-12-02 16:47:56 +1100

    Tidy up keyboard-level NKRO_ENABLE rules, A

--- keyboards/akegata_denki/device_one/rules.mk
+++ keyboards/akegata_denki/device_one/rules.mk
@@ -14,7 +14,7 @@ MOUSEKEY_ENABLE = no				# Mouse keys
 EXTRAKEY_ENABLE = no				# Audio control and System control
 CONSOLE_ENABLE = no					# Console for debug
 COMMAND_ENABLE = no    			# Commands for debug and configuration
-NKRO_ENABLE = yes					  # USB Nkey Rollover
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 NO_USB_STARTUP_CHECK = no   # Disable initialization only when usb is plugged in
 
 LAYOUTS = 60_ansi 60_iso
