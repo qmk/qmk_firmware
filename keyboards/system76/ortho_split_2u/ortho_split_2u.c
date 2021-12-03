975bf30978980c07c97719f4dda248e5bf54c36a
Jimmy Cassis <KernelOops@outlook.com>
2021-12-03 00:28:34 -0800

    Remove System76 pre-release or test keyboards and keymaps

--- keyboards/system76/ortho_split_2u/ortho_split_2u.c
+++
@@ -1,9 +0,0 @@
-#include "ortho_split_2u.h"
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
-}
--- keyboards/system76/virgo/virgo.c
+++
@@ -1,9 +0,0 @@
-#include "virgo.h"
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
-}
