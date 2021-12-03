6782edca3c43de6121225925748ede6d926e2a9d
Vicktor Hutama <vicktor.hutama@gmail.com>
2021-12-03 15:36:51 +0700

    Update keyboards/runes/vaengr/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/runes/vaengr/config.h
+++ keyboards/runes/vaengr/config.h
@@ -42,8 +42,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define MATRIX_ROW_PINS { B3, B7, B0, F7, C6 }
 #define MATRIX_COL_PINS { E6, F0, F1, F4, F5, D0, D1, D6, D4, D2, D3, D5 }
-// #define MATRIX_ROW_PINS { C7, E6, D5, B3, B7 }
-// #define MATRIX_COL_PINS { D4, D6, D7, B4, B5, B6, F7, F6, F5, F4, F1, F0 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
