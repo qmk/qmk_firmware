397333fcece7c4920e5e1485286a1d8762746f26
peepeetee <43021794+peepeetee@users.noreply.github.com>
2021-12-03 13:22:35 +0800

    Update RGB flags

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/rev2.c
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/rev2.c
@@ -116,10 +116,10 @@ led_config_t g_led_config = {
 }, {
     // Esc, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -, =, Backspace
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
-    // Tab, Q, W, E, R, T, Y, U, I, O, P, [, ],  ,
-    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+    // Tab, Q, W, E, R, T, Y, U, I, O, P, [, ], \
+    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
     // Capslock, A, S, D, F, G, H, J, K, L, ;, ', Enter
-    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+    9, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
     // LShift, Z, X, C, V, B, N, M, ,, ., /, Shift, Up,
     1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
     // Ctrl, GUI, Alt, Space, RAlt, FN, Left, Down, Right
