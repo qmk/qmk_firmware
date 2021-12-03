d5ae2d283f9172b4215a8a791f3fb6806e832dfb
peepeetee <43021794+peepeetee@users.noreply.github.com>
2021-12-03 13:19:19 +0800

    Update RGB flags

--- keyboards/kprepublic/bm60hsrgb_iso/rev2/rev2.c
+++ keyboards/kprepublic/bm60hsrgb_iso/rev2/rev2.c
@@ -120,12 +120,12 @@ led_config_t g_led_config = { {
 }, {
     // Esc, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -, =, Backspace
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
-    // Tab, Q, W, E, R, T, Y, U, I, O, P, [, ],  ,
+    // Tab, Q, W, E, R, T, Y, U, I, O, P, [, ], Enter
     1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
-    // Capslock, A, S, D, F, G, H, J, K, L, ;, ', Enter
-    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
-    // LShift,<, Z, X, C, V, B, N, M, ,, ., Shift, Up, /
-    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4,
+    // Capslock, A, S, D, F, G, H, J, K, L, ;, ',#
+    9, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    // LShift,\, Z, X, C, V, B, N, M, ,, ., Shift, Up, /
+    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 4,
     // Ctrl, GUI, Alt, Space, RAlt, FN, Left, Down, Right
     1, 1, 1, 4, 1, 1, 1, 1, 1
 #    if WS2812_LED_TOTAL > 0
