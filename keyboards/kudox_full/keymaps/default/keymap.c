e6356e28431a0a9a1be99faf555766dbdd51aa02
x1- <viva008@gmail.com>
2021-12-04 17:37:30 +0900

    Move to <keyboard>.c oled_task_user and eeconfig_init_user

--- keyboards/kudox_full/keymaps/default/keymap.c
+++ keyboards/kudox_full/keymaps/default/keymap.c
@@ -84,39 +84,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
-
-
-#ifdef RGBLIGHT_ENABLE
-void eeconfig_init_user(void) {
-    rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL);
-};
-#endif
-
-#ifdef OLED_ENABLE
-void oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_ln_P(PSTR("Kudox Keyboard"), false);
-    switch (get_highest_layer(layer_state)) {
-        case _BASE:
-            oled_write_ln_P(PSTR("\n"), false);
-            break;
-        case _FN1:
-            oled_write_ln_P(PSTR("\nLayer: Symbol"), false);
-            break;
-        case _FN2:
-            oled_write_ln_P(PSTR("\nLayer: Function"), false);
-            break;
-        case _FN3:
-            oled_write_ln_P(PSTR("\nLayer: Light"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-            oled_write_ln_P(PSTR("\nLayer: Undefined"), false);
-    }
-    // Host Keyboard LED Status
-    led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
-}
-#endif
