bc63b442cd5f49dd694c2f9e2d8cbade62cdcbbe
fOmey <pauly.galea@gmail.com>
2021-12-04 17:14:22 +1100

    Cleanup default keymap

--- keyboards/atlantis/ak81_ve/keymaps/default/keymap.c
+++ keyboards/atlantis/ak81_ve/keymaps/default/keymap.c
@@ -36,16 +36,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
-
 void led_set_user(uint8_t usb_led) {
-
 	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
 		DDRD |= (1 << 4); PORTD &= ~(1 << 4);
 	} else {
 		DDRD &= ~(1 << 4); PORTD &= ~(1 << 4);
 	}
-
 }
