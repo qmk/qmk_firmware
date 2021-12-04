8b73f54b6cdaa8930f4df024bdec411a61aa525b
Travis <Travis.Hibble@gmail.com>
2021-12-04 15:10:09 +1000

    Init commit

---
+++ keyboards/splitkb/kyria/keymaps/travishi/rules.mk
@@ -0,0 +1,9 @@
+# Enabled features
+LEADER_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+
+# Disabled features
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+MOUSEKEY_ENABLE = no
