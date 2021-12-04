feb8b8369ba40eec213d5e90849884020aa50582
Charly Delay <0xcharly@users.noreply.github.com>
2021-12-05 01:38:09 +0900

    bastardkb/charybdis: future proofs it for #15304 (split support for pointing devices)

--- keyboards/bastardkb/charybdis/config.h
+++ keyboards/bastardkb/charybdis/config.h
@@ -20,5 +20,16 @@
 
 #include "config_common.h"
 
-/** Invert `x` value on mouse reports. */
+/* Pointing device configuration. */
+
+// Enable use of pointing device on slave split.
+#define SPLIT_POINTING_ENABLE
+
+// Pointing device is on the right split.
+#define POINTING_DEVICE_RIGHT
+
+// Limits the frequency that the sensor is polled for motion.
+#define POINTING_DEVICE_TASK_THROTTLE_MS 1
+
+/** \brief Invert X axis on mouse reports. */
 #define POINTING_DEVICE_INVERT_X
