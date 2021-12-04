d7fa5100125df2b86ca8e82508b0577f39a21623
Will Winder <wwinder.unh@gmail.com>
2021-12-04 18:51:09 -0500

    More PR feedback.
    * Better credit.
    * Disable NKRO.

--- keyboards/sofle/keyhive/rules.mk
+++ keyboards/sofle/keyhive/rules.mk
@@ -15,7 +15,7 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
+NKRO_ENABLE = no            # USB Nkey Rollover. 6KRO by default. NKRO works if enabled. (304)
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
