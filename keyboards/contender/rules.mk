e184297dc42bd5fdd33306b9a59c2ae80574dd56
sotoba <sotoba.s+git@gmail.com>
2021-12-05 22:40:46 +0900

    Update keyboards/contender/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/contender/rules.mk
+++ keyboards/contender/rules.mk
@@ -16,5 +16,4 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
