abbabbcfec5e127d83741fb6165e5e3fdf2a1342
ChrisGVE <christian.berclaz@mac.com>
2021-12-05 21:36:44 +0800

    dz60 tsangan: added 'Excel' navigation keys in first navigation layer

--- keyboards/cannonkeys/adelie/keymaps/chrisgve/keymap.c
+++ keyboards/cannonkeys/adelie/keymaps/chrisgve/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSPC, KC_PGUP,
     TAB_CTL, KC_A,    KC_S,    M_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGDN,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,  KC_UP,             ADJ_M,
-    KC_LCTL, KC_LALT, KC_LGUI, SPC_RSE, SPC_RSE, KC_RGUI, KC_RGUI, KC_RALT,                            KC_LEFT,  KC_DOWN, KC_RIGHT
+    KC_LCTL, KC_LALT, KC_LGUI,          SPC_RSE, SPC_RSE,          KC_RGUI, KC_RGUI, KC_RALT,          KC_LEFT,  KC_DOWN, KC_RIGHT
   ),
 
   [_MAC_NAV_1] = LAYOUT_all(
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSPC, KC_PGUP,
     TAB_CTL, KC_A,    KC_S,    L_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGDN,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,  KC_UP,             ADJ_L,
-    KC_LCTL, KC_LGUI, KC_LALT, SPC_RSE, SPC_RSE, KC_RALT, KC_RALT, KC_RGUI,                            KC_LEFT,  KC_DOWN, KC_RIGHT
+    KC_LCTL, KC_LGUI, KC_LALT,          SPC_RSE, SPC_RSE,          KC_RALT, KC_RALT, KC_RGUI,          KC_LEFT,  KC_DOWN, KC_RIGHT
   ),
 
   [_LINUX_NAV_1] = LAYOUT_all(
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSPC, KC_PGUP,
     TAB_CTL, KC_A,    KC_S,    W_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGDN,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,  KC_UP,             ADJ_W,
-    KC_LCTL, KC_LGUI, KC_LALT, SPC_RSE, SPC_RSE, KC_RALT, KC_RALT, KC_RGUI,                            KC_LEFT,  KC_DOWN, KC_RIGHT
+    KC_LCTL, KC_LGUI, KC_LALT,          SPC_RSE, SPC_RSE,          KC_RALT, KC_RALT, KC_RGUI,          KC_LEFT,  KC_DOWN, KC_RIGHT
   ),
 
   [_WIN_NAV_1] = LAYOUT_all(
