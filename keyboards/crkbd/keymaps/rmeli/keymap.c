69f34503a690add83f915bd8b75e8e1de5ed1ef1
Rocco Meli <r.meli@bluemail.ch>
2021-12-06 11:22:21 +0100

    add tap dance for ESC/DEL

--- keyboards/crkbd/keymaps/rmeli/keymap.c
+++ keyboards/crkbd/keymaps/rmeli/keymap.c
@@ -26,10 +26,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum {
     TD_RCTL_CAPS, // Tap once for KC_RCTL, twice for CAPS LOCK
+    TD_ESC_DEL, // Tap once for KC_ESC, twice for KC_DEL
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_RCTL_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RCTL, KC_CAPS),
+    [TD_ESC_DEL] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_DEL)
 };
 
 // + -------------------- +
@@ -57,13 +59,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------z------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+       TD(TD_ESC_DEL),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), TD(TD_RCTL_CAPS)
                                       //`--------------------------'  `--------------------------'
-
   ),
 
   [1] = LAYOUT_split_3x6_3(
