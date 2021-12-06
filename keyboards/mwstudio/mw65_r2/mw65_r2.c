2726e62b79f07ef350d3c5a98ac745ef310d4ae7
TW59420 <40019210+TW59420@users.noreply.github.com>
2021-12-06 21:49:39 +0800

    Update keyboards/mwstudio/mw65_r2/mw65_r2.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/mwstudio/mw65_r2/mw65_r2.c
+++ keyboards/mwstudio/mw65_r2/mw65_r2.c
@@ -15,3 +15,17 @@
  */
 
 #include "mw65_r2.h"
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    if (index == 0) {
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    }
+    return true;
+}
+#endif
