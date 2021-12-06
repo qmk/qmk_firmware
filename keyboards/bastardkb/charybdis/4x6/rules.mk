1277e46e396d32decfb844101878e19cc48749eb
Charly Delay <0xcharly@users.noreply.github.com>
2021-12-06 13:38:27 +0900

    bastardkb/charybdis: remove SLEEP_LED_ENABLE from rules.mk

--- keyboards/bastardkb/charybdis/4x6/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/rules.mk
@@ -21,7 +21,6 @@ AUDIO_ENABLE = no           # Audio output
 AUDIO_SUPPORTED = no        # Audio is not supported.
 RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default.
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default.
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix functionality
 RGB_MATRIX_DRIVER = WS2812
 
