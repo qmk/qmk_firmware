1e2e338f3641933c98d303322ca4194c8018020f
x1- <viva008@gmail.com>
2021-12-06 19:01:49 +0900

    Set none as default, but values

--- keyboards/kudox_full/post_config.h
+++ keyboards/kudox_full/post_config.h
@@ -1,7 +1,20 @@
-#ifndef MOUSEKEY_ENABLE
-    #define MOUSEKEY_INTERVAL       20
-    #define MOUSEKEY_DELAY          0
-    #define MOUSEKEY_TIME_TO_MAX    60
-    #define MOUSEKEY_MAX_SPEED      7
-    #define MOUSEKEY_WHEEL_DELAY    0
+// Copyright 2021 Kumao Kobo (@kumaokobo)
+// SPDX-License-Identifier: GPL-2.0+
+
+#pragma once
+
+#ifndef MOUSEKEY_INTERVAL
+#    define MOUSEKEY_INTERVAL       20
+#endif
+#ifndef MOUSEKEY_DELAY
+#    define MOUSEKEY_DELAY          0
+#endif
+#ifndef MOUSEKEY_TIME_TO_MAX
+#    define MOUSEKEY_TIME_TO_MAX    60
+#endif
+#ifndef MOUSEKEY_MAX_SPEED
+#    define MOUSEKEY_MAX_SPEED      7
+#endif
+#ifndef MOUSEKEY_WHEEL_DELAY
+#    define MOUSEKEY_WHEEL_DELAY    0
 #endif
