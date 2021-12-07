f1c5386ad02ca230f73fbfd6c4507e03c65f3cff
fauxpark <fauxpark@gmail.com>
2021-12-08 05:59:18 +1100

    Remove references to Makefile in keyboard-level rules.mk

--- keyboards/helix/rules.mk
+++ keyboards/helix/rules.mk
@@ -4,9 +4,8 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = caterina
 
-# QMK Standard Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
+# Build Options
+#   change yes to no to disable
 #
 #   See TOP/docs/config_options.md for more information.
 #
