3494d4861bd1a28126785d069219369f01ac76e3
Andrew-Fahmy <andrewj.fahmy@gmail.com>
2021-12-06 19:46:06 -0600

    Update keyboards/k_type/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/k_type/rules.mk
+++ keyboards/k_type/rules.mk
@@ -35,5 +35,7 @@ LAYOUTS = tkl_ansi
 SRC += k_type-rgbdriver.c
 QUANTUM_LIB_SRC += i2c_master.c is31fl3733-dual.c
 
+LAYOUTS = tkl_ansi
+
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
