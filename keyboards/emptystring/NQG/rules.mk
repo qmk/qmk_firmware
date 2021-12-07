7f90cb178159d22744ce44b5f6e2223980da5ac6
Ryan <fauxpark@gmail.com>
2021-12-08 10:40:26 +1100

    Remove references to Makefile in keyboard-level rules.mk (#15427)

--- keyboards/emptystring/NQG/rules.mk
+++ keyboards/emptystring/NQG/rules.mk
@@ -5,8 +5,7 @@ MCU = atmega32u4
 BOOTLOADER = caterina
 
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
+#   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no      # Mouse keys
--- keyboards/xw60/rules.mk
+++ keyboards/xw60/rules.mk
@@ -5,8 +5,7 @@ MCU = atmega32u4
 BOOTLOADER = atmel-dfu
 
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in 
-#   the appropriate keymap folder that will get included automatically
+#   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
