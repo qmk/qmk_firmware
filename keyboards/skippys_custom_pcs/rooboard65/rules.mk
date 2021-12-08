cf62e22b13bfca3afb86e8a3cbffa83cfb060145
Felix Jen <fjen@felixjen.com>
2021-12-07 22:35:31 -0600

    Update keyboards/skippys_custom_pcs/rooboard65/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/skippys_custom_pcs/rooboard65/rules.mk
+++ keyboards/skippys_custom_pcs/rooboard65/rules.mk
@@ -21,5 +21,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-
 LAYOUTS = 65_ansi
