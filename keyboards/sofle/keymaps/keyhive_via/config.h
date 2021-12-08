770a157b19ac309eea18caf312d381df90431226
Will Winder <wwinder.unh@gmail.com>
2021-12-08 08:47:21 -0500

    Disable RGBLIGHT_ENABLE in keymap for better compatibility with default keymap.

--- keyboards/sofle/keymaps/keyhive_via/config.h
+++ keyboards/sofle/keymaps/keyhive_via/config.h
@@ -24,18 +24,20 @@ for more options.
 
 // More lighting options in quantum/rgblight/rgblight.h
 // Disabling some of these is a good way to save flash space.
-#define RGBLIGHT_EFFECT_ALTERNATING     // 108
-#define RGBLIGHT_EFFECT_RGB_TEST        // 158
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD    // 160
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT // 168
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL   // 192
-#define RGBLIGHT_EFFECT_BREATHING       // 348
-#define RGBLIGHT_EFFECT_KNIGHT          // 336
-#define RGBLIGHT_EFFECT_SNAKE           // 406
-#define RGBLIGHT_EFFECT_CHRISTMAS       // 508
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_EFFECT_ALTERNATING     // 108
+#    define RGBLIGHT_EFFECT_RGB_TEST        // 158
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD    // 160
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT // 168
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL   // 192
+#    define RGBLIGHT_EFFECT_BREATHING       // 348
+#    define RGBLIGHT_EFFECT_KNIGHT          // 336
+#    define RGBLIGHT_EFFECT_SNAKE           // 406
+#    define RGBLIGHT_EFFECT_CHRISTMAS       // 508
 /*
 #define RGBLIGHT_EFFECT_TWINKLE         // 1156
 */
+#endif
 
 // Disabled to save space
 #define NO_ACTION_ONESHOT  // 332
