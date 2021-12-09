a8d440e4e043dc8427f17c07631556d0ff3efc20
Ryan <fauxpark@gmail.com>
2021-12-09 16:05:44 +1100

    Tidy up NKRO_ENABLE rules (#15382)

--- keyboards/jd45/rules.mk
+++ keyboards/jd45/rules.mk
@@ -12,6 +12,6 @@ MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
-# NKRO_ENABLE = yes		# USB Nkey Rollover - not yet supported in LUFA
+NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
 MIDI_ENABLE = yes           # MIDI support
