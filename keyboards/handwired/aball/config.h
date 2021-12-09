82098eceb3f03350b603224b7bcc16a1ae8bc21b
brickbots <rich@brickbots.com>
2021-12-08 16:28:51 -0800

    Update rules with new feature descriptors, add info.json

--- keyboards/handwired/aball/config.h
+++ keyboards/handwired/aball/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0001
 #define MANUFACTURER Richard Sutherland
 #define PRODUCT ABall
-#define DESCRIPTION Buttonless trackball designed for use with a keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 1
