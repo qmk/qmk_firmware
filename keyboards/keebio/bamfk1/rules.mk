bd1656b1cfd1dfadfee11b6264aecb671a0e249c
Danny Nguyen <danny@keeb.io>
2021-12-09 12:32:59 -0500

    Update rules

--- keyboards/keebio/bamfk1/rules.mk
+++ keyboards/keebio/bamfk1/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -18,5 +18,5 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-AUDIO_ENABLE = no          # Audio output
+AUDIO_ENABLE = yes          # Audio output
 ENCODER_ENABLE = yes
