4d1ed67f6fb0b6256def4500e6e714dc31717aed
peepeetee <43021794+peepeetee@users.noreply.github.com>
2021-12-10 12:39:41 +0800

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/rules.mk
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/rules.mk
@@ -21,11 +21,6 @@ LTO_ENABLE = yes
 # RGB Matrix is required to support per-key LEDs connected to IS31FL3733.
 RGB_MATRIX_ENABLE = yes
 
-# The simpler RGB Lighting code can be enabled to control just the underglow
-# LEDs from it; leave it disabled to make the RGB Matrix code control all LEDs,
-# including underglow.
-RGBLIGHT_ENABLE = no
-
 # The custom RGB Matrix driver combines IS31FL3733 and WS2812; things that are
 # normally done by common_features.mk for both of these drivers need to be done
 # here manually.
