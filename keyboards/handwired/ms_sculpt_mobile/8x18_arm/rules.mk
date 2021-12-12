06f25cc1a9ba1c344b2e5259ad8762032f35a04c
milestogo <milestogo@users.noreply.github.com>
2021-12-12 00:56:08 -0800

    Update keyboards/handwired/ms_sculpt_mobile/8x18_arm/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/ms_sculpt_mobile/8x18_arm/rules.mk
+++ keyboards/handwired/ms_sculpt_mobile/8x18_arm/rules.mk
@@ -1,18 +1,10 @@
-# GENERIC STM32F103C8T6 board - stm32duino bootloader
-# MCU NAME
+# MCU name
 MCU = STM32F103
+
+# Bootloader selection
 BOOTLOADER = stm32duino
+
 WS2812_DRIVER = bitbang
 
-# GENERIC STM32F103C8T6 board - stm32duino bootloader
-OPT_DEFS = -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-## Chibi options that should no longer be needed. 
-##-DCORTEX_VTOR_INIT=0x2000
-# Options to pass to dfu-util when flashing
-##DFU_ARGS = -d 1EAF:0003 -a 2 -R
-##DFU_SUFFIX_ARGS  ?= -v 1EAF -p 0003
-
-# avoid warning about LTO & ChibiOS, even though it mostly works
-#LTO_ENABLE = no
-
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
