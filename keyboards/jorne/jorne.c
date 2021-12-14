46bf73e5a9af9a7aef8945b0cdc7242d97f6ece5
joric <joric@users.noreply.github.com>
2021-12-14 12:39:08 +0500

    Update keyboards/jorne/jorne.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/jorne/jorne.c
+++ keyboards/jorne/jorne.c
@@ -53,9 +53,9 @@ static void render_status(void) {
 
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK) ? PSTR("NUMLCK ") : PSTR("       "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK) ? PSTR("CAPLCK ") : PSTR("       "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
+    oled_write_P(led_state.num_lock ? PSTR("NUMLCK ") : PSTR("       "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPLCK ") : PSTR("       "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
 }
 
 bool oled_task_kb(void) {
