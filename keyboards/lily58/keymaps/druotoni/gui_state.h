a181814261b2065ff34624a6020c77b62bcb0922
druotoni <89318351+druotoni@users.noreply.github.com>
2021-12-14 14:02:16 +0100

    Update keyboards/lily58/keymaps/druotoni/gui_state.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/lily58/keymaps/druotoni/gui_state.h
+++ keyboards/lily58/keymaps/druotoni/gui_state.h
@@ -1,6 +1,8 @@
 // Copyright 2021 Nicolas Druoton (druotoni)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#pragma once
+
 // states timing
 #define BOOTING_TIME_TRESHOLD 7000
 #define WAKING_UP_TIME_TRESHOLD 300
