5f93d72e365d89f76abca41468fecba110fc5fb7
ploopyco <54917504+ploopyco@users.noreply.github.com>
2021-12-14 15:36:04 -0500

    Update keyboards/ploopyco/trackball_thumb/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ploopyco/trackball_thumb/rules.mk
+++ keyboards/ploopyco/trackball_thumb/rules.mk
@@ -11,6 +11,7 @@ F_CPU = 8000000
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
