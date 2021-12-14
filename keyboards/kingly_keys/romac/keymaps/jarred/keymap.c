14e86b796960c6f722a53cc4588dbf8a0a0db46e
Sylivan Kenobi <sypsilon@hotmail.com>
2021-12-14 21:52:21 +0100

    add my keymaps

--- keyboards/kingly_keys/romac/keymaps/jarred/keymap.c
+++ keyboards/kingly_keys/romac/keymaps/jarred/keymap.c
@@ -22,10 +22,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_BASE] = LAYOUT(
-		KC_7, KC_8, KC_9, \
-		KC_4, KC_5, KC_6, \
-		KC_1, KC_2, KC_3, \
-		LT(_FN1, KC_0), KC_ENT, KC_DOT \
+      KC_F19, KC_F20, KC_F21, \
+      KC_F16, KC_F17, KC_F18, \
+      KC_F13, KC_F14, KC_F15, \
+      KC_MUTE, KC_VOLD, KC_VOLU \
 	),
   
 	[_FN1] = LAYOUT(
