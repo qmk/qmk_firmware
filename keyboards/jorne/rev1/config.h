3679b638f59410dea0d080e358c4ffbd023bc126
joric <joric@users.noreply.github.com>
2021-12-14 14:59:35 +0500

    vendor_id set to 0x4753

--- keyboards/jorne/rev1/config.h
+++ keyboards/jorne/rev1/config.h
@@ -4,8 +4,8 @@
 
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x4753
+#define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    joric
 #define PRODUCT         Jorne Keyboard
