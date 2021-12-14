3d1e79240d145c16ed46fed7b4dedfe362ef2219
Andrew Kannan <andrew.kannan@gmail.com>
2021-12-14 14:00:45 -0500

    Rename folder

--- keyboards/cannonkeys/hoodrowg/config.h
+++ keyboards/cannonkeys/hoodrowg/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0006
 #define DEVICE_VER      0x0001
 #define MANUFACTURER CannonKeys
-#define PRODUCT 1.2 HoodrowG
+#define PRODUCT HoodrowG
 
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 9
