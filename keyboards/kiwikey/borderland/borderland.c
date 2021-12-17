10dba8a83dd5faffd0785bbd2f14dd6991bdda9a
KiwiKey <kiwikey.vn@gmail.com>
2021-12-17 12:28:56 +0700

    Commit new keyboard Borderland to qmk_firmware

---
+++ keyboards/kiwikey/borderland/borderland.c
@@ -0,0 +1,4 @@
+// Copyright 2021 KiwiKey (@KiwiKey)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "borderland.h"
---
+++ keyboards/kiwikey/borderland/keymaps/default/encoder.c
@@ -0,0 +1,10 @@
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+	if (clockwise) {
+		tap_code(KC_VOLD);
+	} else {
+		tap_code(KC_VOLU);
+	}
+    return true;	
+}
+#endif
