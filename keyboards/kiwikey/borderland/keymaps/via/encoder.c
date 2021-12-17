136dd5b8b1ca01c1d1d7fa716c96532ffbb538bd
KiwiKey <kiwikey.vn@gmail.com>
2021-12-17 13:07:47 +0700

    Add license headers on all "encoder.c"

--- keyboards/kiwikey/borderland/keymaps/via/encoder.c
+++ keyboards/kiwikey/borderland/keymaps/via/encoder.c
@@ -1,3 +1,6 @@
+// Copyright 2021 KiwiKey (@KiwiKey)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
 	if (clockwise) {
