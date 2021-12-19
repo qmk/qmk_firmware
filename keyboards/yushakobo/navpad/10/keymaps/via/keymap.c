e64a2caaa9a54e443d534347d9b37e0950fc38ec
MakotoKurauchi <pluis@me.com>
2021-12-19 14:04:09 +0900

    add encoder code to via

--- keyboards/yushakobo/navpad/10/keymaps/via/keymap.c
+++ keyboards/yushakobo/navpad/10/keymaps/via/keymap.c
@@ -53,3 +53,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO \
   )
 };
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  if (index == 0) { /* First encoder */
+    if (clockwise) {
+      tap_code16(KC_WH_U);
+    } else {
+      tap_code16(KC_WH_D);
+    }
+  }
+  return false;
+}
+#endif
