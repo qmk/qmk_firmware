2966e9725f23cf1fd54dad473c551138eb73764b
Andrew Kannan <andrew.kannan@klaviyo.com>
2021-12-20 09:42:53 -0500

    Update keyboards/cannonkeys/hoodrowg/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/cannonkeys/hoodrowg/rules.mk
+++ keyboards/cannonkeys/hoodrowg/rules.mk
@@ -5,14 +5,14 @@ MCU = atmega32u4
 BOOTLOADER = atmel-dfu
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes	    # USB Nkey Rollover
-BACKLIGHT_ENABLE = no
-RGBLIGHT_ENABLE = yes
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
