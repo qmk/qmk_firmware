adf967200825c662870be7dc329a2943f4d068a1
Chad Stovern <chad@stovern.me>
2021-12-21 13:43:04 -0600

    restore unadorned spc for game mode

--- keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
+++ keyboards/boardsource/microdox/keymaps/chadhs/keymap.c
@@ -131,13 +131,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  '------'------'------'------'------'            '------'------'------'------'------'
                   .------.------.------.      .------.------.------.
                   | ↓↓↓  |      | SPC  |      | SPC  |      | ↓↓↓  |
-                  |      | CMD  | GNUM |      | GFUN | CTL  |      |
+                  |      | CTL  |      |      | GFUN | OPT  |      |
                   '------'------'------'      '------'------'------'
 */
   KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   KC_ESC,  KC_A, KC_S,    KC_D,    KC_F,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
   KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,        KC_N,    KC_M,    KC_COMM, KC_DOT,  GAME,
-                 KC_TRNS, KC_LCTL, GNUM_SPC,     GFUN_SPC,  KC_LOPT, KC_TRNS
+                 KC_TRNS, KC_LCTL, KC_SPC,     GFUN_SPC,  KC_LOPT, KC_TRNS
 ),
 
 [_GAME_FUN] = LAYOUT_split_3x5_3(
