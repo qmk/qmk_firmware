5ecbc417d69290ae0606843c0ebd13459ca8c449
ChrisGVE <christian.berclaz@mac.com>
2021-12-21 22:12:32 +0800

    Wind.X: fixed the layer problem (by overriding the VIA_ENABLE from the rules.mk) and added mouse support

--- keyboards/winx/keymaps/chrisgve/rules.mk
+++ keyboards/winx/keymaps/chrisgve/rules.mk
@@ -1,1 +1,17 @@
+# VIA_ENABLE = yes
+LTO_ENABLE = yes
+
+# APPLE_FN_ENABLE = yes
+NKRO_ENABLE = yes
+
 USER_NAME := chrisgve
+
+SRC += chrisgve.c
+
+MOUSEKEY_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+RGBMATRIX_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+
+VIA_ENABLE = no
