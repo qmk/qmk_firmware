59507f4c37fe29033b2f671a18f332636fd2d97b
Andrzej Kotulski <gh@akotulski.com>
2021-12-21 22:44:59 +0100

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/dactyl_manuform/4x6_5/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6_5/keymaps/default/keymap.c
@@ -68,7 +68,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-void persistent_default_layer_set(uint16_t default_layer) {
-    eeconfig_update_default_layer(default_layer);
-    default_layer_set(default_layer);
-}
