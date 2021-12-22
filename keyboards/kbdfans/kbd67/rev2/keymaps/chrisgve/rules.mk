5911b01effc87c0a7009f677316d1af7327741e0
ChrisGVE <christian.berclaz@mac.com>
2021-12-22 20:26:00 +0800

    kbd67 rev2: Added mouse support

--- keyboards/kbdfans/kbd67/rev2/keymaps/chrisgve/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/keymaps/chrisgve/rules.mk
@@ -1,15 +1,18 @@
+USER_NAME := chrisgve
+
+SRC += chrisgve.c
+
 LTO_ENABLE = yes
 NKRO_ENABLE = yes
 
 RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = yes
 
-src += chrisgve.c
 
 MOUSEKEY_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 
 APPLE_FN_ENABLE = yes
 
-REDUCE_RGB = yes
+# REDUCE_RGB = yes
 
