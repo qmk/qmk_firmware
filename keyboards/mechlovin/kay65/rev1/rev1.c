9933b002036fa67e3d5975d0afdd28b2133e1b8c
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-12-23 16:21:57 +0700

    update

--- keyboards/mechlovin/kay65/rev1/rev1.c
+++ keyboards/mechlovin/kay65/rev1/rev1.c
@@ -2,3 +2,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "rev1.h"
+
+void keyboard_pre_init_user(void) {
+  // Call the keyboard pre init code.
+
+  // Set our LED pins as output
+  setPinOutput(D7);
+}
