ae9331cdb67d92ed3c808540d155cc9219c25d8f
MakotoKurauchi <pluis@me.com>
2021-12-24 17:58:13 +0900

    define RGBLIGHT_LAYERS move to keymap

--- keyboards/yushakobo/navpad/10_helix_r/config.h
+++ keyboards/yushakobo/navpad/10_helix_r/config.h
@@ -99,11 +99,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
 //#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
 
-#   define RGBLIGHT_LAYERS
-#   ifdef RGBLIGHT_LAYERS
-#       define RGBLIGHT_MAX_LAYERS 6
-#   endif
-
 #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
