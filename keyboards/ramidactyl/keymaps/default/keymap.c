50076b3586a0aa54c603a1ddc5a62cb2d2672adb
idloco <idloco@users.noreply.github.com>
2021-12-25 23:09:45 -0600

    Update keymap.c
    Keymap set

--- keyboards/ramidactyl/keymaps/default/keymap.c
+++ keyboards/ramidactyl/keymaps/default/keymap.c
@@ -43,7 +43,7 @@ const char *read_logo(void);
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
-    }
+}
 // #ifdef OLED_DRIVER_ENABLE
 //     set_keylog(keycode, record);
 // #endif
@@ -79,38 +79,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
      KC_GESC  , KC_1  , KC_2    ,   KC_3  , KC_4  ,   KC_5  ,                KC_6   ,   KC_7  ,   KC_8  ,   KC_9    , KC_0    , KC_BSPC ,
-     KC_TAB   , KC_Q  , KC_W    ,   KC_E  , KC_R  ,   KC_T  ,                KC_Y   ,   KC_U  ,   KC_I  ,   KC_O    , KC_P    , KC_BSLS ,
-     KC_LCTL  , KC_A  , KC_S    ,   KC_D  , KC_F  ,   KC_G  ,                KC_H   ,   KC_J  ,   KC_K  ,   KC_L    , KC_SCLN , KC_QUOT ,
-     KC_LSFT  , KC_Z  , KC_X    ,   KC_C  , KC_V  ,   KC_B  ,                KC_N   ,   KC_M  ,   KC_COMM,  KC_DOT  , KC_SLSH , OSL(1) ,
-                        KC_LBRC  ,  KC_RBRC,                                                      KC_MINS,  KC_EQL,
-                                    KC_LGUI,  MO(1),                                    KC_ENT,   KC_SPC,
-                                            KC_LALT , KC_DEL,                KC_PGUP, KC_LGUI,
-                                            KC_INS  , KC_INS,                KC_N , KC_PGDN
+     KC_TAB   , KC_Q  , KC_W    ,   KC_E  , KC_R  ,   KC_T  ,                KC_Y   ,   KC_U  ,   KC_I  ,   KC_O    , KC_P    , KC_LBRC ,
+     KC_LSFT  , KC_A  , KC_S    ,   KC_D  , KC_F  ,   KC_G  ,                KC_H   ,   KC_J  ,   KC_K  ,   KC_L    , KC_SCLN , KC_QUOT ,
+     KC_LCTL  , KC_Z  , KC_X    ,   KC_C  , KC_V  ,   KC_B  ,                KC_N   ,   KC_M  ,   KC_COMM,  KC_DOT  , KC_SLSH , KC_BSLS ,
+                             LCTL(KC_C),  LCTL(KC_V),                                                      KC_MINS, KC_EQL,
+                                    KC_SPC,  KC_ENT,                                        KC_ENT,   KC_SPC,
+                                             KC_LGUI, TT(1),                          KC_DEL, KC_HOME,
+                                             KC_LALT, TT(2),                LCTL(LSFT(KC_M)) , KC_END
   ),
 
   [_LOWER] = LAYOUT(
 
-     KC_TILD    , KC_F1   , KC_F2   , KC_F3   , KC_F4     , KC_F5  ,      KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_DEL ,
-     _______    , _______ , _______  , _______ , RESET     , _______,      _______ , _______ , _______ , _______ , _______ , _______,
-     _______    , _______ , _______ , _______ , _______ , _______,        KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT, _______ , _______,
+     DF(0)      , KC_F1   , KC_F2   , KC_F3   , KC_F4     , KC_F5  ,      KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_DEL ,
+     _______    , _______ , KC_UP   , _______ , _______   , KC_F11 ,      KC_F12  , _______ , _______ , _______ , _______ , KC_RBRC,
+     _______    , KC_LEFT , KC_DOWN , KC_RIGHT, _______   , _______,      _______ , _______ , _______ , _______ , _______ , _______,
      _______    , _______ , _______   , _______ , _______ , _______,      _______ , _______ , _______ , _______ , _______ , _______,
                             _______ , _______ ,                                               _______ , _______ ,
                                       _______ , _______ ,                           _______ , _______ ,
-                                                KC_TRNS   , _______,      _______ , _______,
-                                                KC_TRNS   , _______,      _______ , _______
-
+                                                  _______ , _______,      _______ , _______,
+                                                  _______ , _______,      _______ , _______
   ),
 
   [_RAISE] = LAYOUT(
 
-     _______  , _______ , _______ , _______ , _______ , _______,      _______ , _______ , _______ , _______ , _______ , _______,
-     _______  , _______ , _______ , _______ , _______ , _______,      _______ , _______ , _______ , _______ , _______ , _______,
-     _______  , _______ , _______ , _______ , _______ , _______,      _______ , _______ , _______ , _______ , _______ , _______,
-     _______  , _______ , _______ , _______ , _______ , _______,      _______ , _______ , _______ , _______ , _______ , _______,
-                              _______ , _______ ,                                         _______ , _______ ,
-                                    _______ , _______ ,                         _______ , _______ ,
-                                              _______ , _______,      _______ , _______,
-                                              _______ , _______,      _______ , _______
+        _______, _______ , _______ , _______ , _______ , _______,      _______ , _______ , _______ , _______ , _______ , _______,
+        _______, _______ , _______ , _______ , _______ , KC_LBRC,      KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+        _______, KC_HOME , KC_PGUP , KC_PGDN , KC_END  , KC_LPRN,      KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
+        _______, _______ , _______ , _______ , _______ , _______,      _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                                         _______,KC_PSCR,                   _______, KC_P0,
+                                                _______,_______,           _______,_______,
+                                                _______,_______,        _______,_______,
+                                                _______,_______,        _______,_______
   ),
 
   [_ADJUST] = LAYOUT(
