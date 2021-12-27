e91070401d0eda5e25210e9f4155fa70d0d61912
Paul Scheikl <paul.scheikl@kit.edu>
2021-12-27 04:40:47 +0100

    K.O.Y and German layout for several boards (#14991)
    * Added accent.
    
    * Added keymap for 5x6 dactyl manuform on KOY layout
    
    * Added xd75 folder that is not in the main repo anymore?
    
    * Added keymap for naked48 on KOY layout
    
    * Added keymap for splitreus62 on KOY layout
    
    * Added keymap for Dactyl Manuform 4x6 with RGB LEDs and K.O,Y layout
    
    * Fixed error where handedness was not correctly determined because of combining vbus pins of both controllers.
    
    * Replaced dynamic macros bith backspace and delete.
    
    * Changed detecting handedness from detecting usb communication to checking a wired pin. This avoids problems when booting the PC, where VBUS is already high, but no communication is happening -> both halves think they are not master.
    
    * Update keyboards/handwired/dactyl_manuform/4x6/keymaps/scheiklp/rules.mk
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/naked48/keymaps/scheiklp/keymap.c
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/naked48/keymaps/scheiklp/keymap.c
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/naked48/keymaps/scheiklp/keymap.c
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keymap.c
    
    Added copyright
    
    * Update config.h
    
    * Update rules.mk
    
    * Updated files according to PR requests.
    
    * Update keyboards/xd75/config.h
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/xd75/config.h
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/xd75/keymaps/default/keymap.c
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Update keyboards/xd75/config.h
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    
    * Removed duplicate code. Updated copyright info.
    
    * Restored readme
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/scheiklp/rules.mk
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/scheiklp/rules.mk
@@ -1,9 +1,9 @@
 # Copyright 2021 Paul Maria Scheikl (@ScheiklP)
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-BOOTMAGIC_ENABLE = no
-COMMAND_ENABLE = no
-RGBLIGHT_ENABLE = no
-DYNAMIC_MACRO_ENABLE = yes
+BOOTLOADER = atmel-dfu
 AUTO_SHIFT_ENABLE = yes
-KEY_LOCK_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+NKRO_ENABLE = yes
+DEBOUNCE_TYPE = asym_eager_defer_pk
