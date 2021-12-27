6f81880f17f94cb9a2e7a09f2f028c83e5d8b1db
Ryan <fauxpark@gmail.com>
2021-12-27 21:15:56 +1100

    Convert some more boards to Matrix Lite (#15489)

--- keyboards/chimera_ortho_plus/rules.mk
+++ keyboards/chimera_ortho_plus/rules.mk
@@ -17,7 +17,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = yes
-CUSTOM_MATRIX = yes
+CUSTOM_MATRIX = lite
 
 # project specific files
 SRC += matrix.c
