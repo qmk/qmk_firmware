4e077250d56f7e786af0cdef00f4b41d77e2b85c
3araht <69518343+3araht@users.noreply.github.com>
2021-12-27 19:27:15 +0900

    bandominedoni, Drashna's method is applied to encoder_update_kb. (#15287)
    * Drashna's method is applied to rotary encoder rotation detection. rgb_matrix_user.inc -> rgb_matrix_kb.inc.
    
    * Disabled some RGB MATRIX effects to shrink the firmware size.
    
    * via # of layers changed from 5 to 4.

--- keyboards/bandominedoni/keymaps/via/rules.mk
+++ keyboards/bandominedoni/keymaps/via/rules.mk
@@ -1,3 +1,4 @@
-RGB_MATRIX_ENABLE = yes       # Use RGB matrix (Don't enable this when RGBLIGHT_ENABLE is used.)
+RGB_MATRIX_ENABLE = yes      # Use RGB matrix (Don't enable this when RGBLIGHT_ENABLE is used.)
+RGB_MATRIX_CUSTOM_KB = yes   #
 VIA_ENABLE = yes
 MOUSEKEY_ENABLE = yes        # Mouse keys
