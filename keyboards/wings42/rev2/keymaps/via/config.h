55c75da524df1ee00ccbd5b578fb61733906fedf
yfuku <30647434+yfuku@users.noreply.github.com>
2021-12-27 12:05:31 +0900

    Add wings42 rev2 (#13702)
    * add rev2
    
    * add encoder settings
    
    * add license header

--- keyboards/wings42/rev2/keymaps/via/config.h
+++ keyboards/wings42/rev2/keymaps/via/config.h
@@ -13,18 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
-#include "quantum.h"
-
-#ifdef KEYBOARD_wings42_rev1
-#    include "rev1.h"
-#endif
-#ifdef KEYBOARD_wings42_rev1_extkeys
-#    include "rev1_extkeys.h"
-#endif
-#ifdef KEYBOARD_wings42_rev2
-#    include "rev2.h"
-#endif
-
+#define ENCODERS_PAD_A { B5, B6 }
+#define ENCODERS_PAD_B { B4, B2 }
+#define ENCODER_RESOLUTION 4
