63825638f73d7b7f87d5198fea2bfc71912d79bc
Drashna Jael're <drashna@live.com>
2021-12-28 11:33:28 -0800

    Re-fix copyright headers

--- users/drashna/template.c
+++ users/drashna/template.c
@@ -1,4 +1,4 @@
-// Copyright 2021 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "template.h"
