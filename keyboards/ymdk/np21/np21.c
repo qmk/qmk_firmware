a29df7e66f03d1f93306bf240c57558d90f3c0dd
peepeetee <peepeetee@protonmail.com>
2021-12-29 19:56:59 +0800

    rename ymdk_np21 to np21; move to ymdk vendor folder

--- keyboards/ymdk/np21/np21.c
+++ keyboards/ymdk/np21/np21.c
@@ -16,4 +16,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ymdk_np21.h"
+#include "np21.h"
