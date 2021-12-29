5ef48a8a6c8fd7a97db695efbed7a7f33179c424
peepeetee <peepeetee@protonmail.com>
2021-12-29 21:41:22 +0800

    fix compilation problems, amend readme

--- keyboards/ymdk/ymd75/ymd75.h
+++ keyboards/ymdk/ymd75/ymd75.h
@@ -2,10 +2,10 @@
 
 #include "quantum.h"
 
-#if defined(KEYBOARD_ymd75_rev1)
+#if defined(KEYBOARD_ymdk_ymd75_rev1)
     #include "rev1.h"
-#elif defined(KEYBOARD_ymd75_rev2)
+#elif defined(KEYBOARD_ymdk_ymd75_rev2)
     #include "rev2.h"
-#elif defined(KEYBOARD_ymd75_rev3)
+#elif defined(KEYBOARD_ymdk_ymd75_rev3)
     #include "rev3.h"
 #endif
