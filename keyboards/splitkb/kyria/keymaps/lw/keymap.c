ab36c7c749048f258fc9f913d9e20f028c02a905
LucasWaelti <lucas.waelti@gmail.com>
2021-12-29 15:59:12 +0100

    Tested firmware

--- keyboards/splitkb/kyria/keymaps/lw/keymap.c
+++ keyboards/splitkb/kyria/keymaps/lw/keymap.c
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_SYM] = LAYOUT(
    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                       KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
- KC_PDOT,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                                       KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0, KC_PEQL,
+ KC_PDOT,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_PEQL,
  KC_LALT, KC_QUES, KC_EXLM,   KC_AT, KC_HASH,   KC_LT, KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC,   KC_GT, KC_CIRC, KC_PSLS, KC_ASTR, KC_PLUS, KC_PMNS,
-                             KC_DLR, KC_PERC, KC_AMPR, KC_DQUO, KC_TRNS, KC_TRNS, KC_RALT, KC_TILD, KC_UNDS, KC_PIPE
+                             KC_DLR, KC_PERC, KC_AMPR, KC_DQUO,   MO(1),   MO(2), KC_RALT, KC_TILD, KC_UNDS, KC_PIPE
     ),
 
 /*
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  _______, KC_BRIU, KC_MNXT, KC_MFFD, KC_MPLY, KC_VOLU,                                     KC_HOME, KC_PGUP,   KC_UP, KC_PGDN, _______,  KC_DEL,
  _______, KC_BRID, KC_MPRV, KC_MRWD, KC_MSTP, KC_VOLD,                                      KC_END, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_BSPC,
  KC_LSFT, _______, _______, _______, _______, KC_MUTE, _______, _______, KC_RSFT,  KC_APP, KC_PSCR, _______, _______, _______, KC_SLCK,  KC_INS,
-                            KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS, KC_TRNS, KC_TRNS, KC_UNDO,  KC_CUT, KC_COPY, KC_PSTE
+                            KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS,   MO(1),   MO(2), KC_UNDO,  KC_CUT, KC_COPY, KC_PSTE
     ),
 
 // /*
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * DO NOT edit the rev1.c file; instead override the weakly defined default functions by your own.
  */
 
-// DELETE THIS LINE TO UNCOMMENT (1/2)
+/* DELETE THIS LINE TO UNCOMMENT (1/2)
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 
@@ -201,4 +201,4 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return false;
 }
 #endif
-// DELETE THIS LINE TO UNCOMMENT (2/2) 
+// DELETE THIS LINE TO UNCOMMENT (2/2) */
