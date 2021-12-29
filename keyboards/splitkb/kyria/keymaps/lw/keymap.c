33a1c52a96ac3f53c10fc31d2d01bcf01eed3d0d
LucasWaelti <lucas.waelti@gmail.com>
2021-12-29 19:21:55 +0100

    Swapped caps lock and enter + moved semi-colon

--- keyboards/splitkb/kyria/keymaps/lw/keymap.c
+++ keyboards/splitkb/kyria/keymaps/lw/keymap.c
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTZ] = LAYOUT(
   KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_BSPC,
- KC_LSFT,    KC_Y,    KC_X,    KC_C,    KC_V,    KC_B, KC_BSLS, KC_LPRN, KC_RPRN,  KC_GRV,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_MINS,  KC_ENT,
-                            KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS,   MO(1),   MO(2),  KC_SPC, KC_SCLN, KC_EXLM, KC_SLSH
+ KC_LSFT,    KC_Y,    KC_X,    KC_C,    KC_V,    KC_B, KC_BSLS, KC_LPRN, KC_RPRN,  KC_GRV,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SCLN, KC_CAPS,
+                            KC_LCTL, KC_LGUI, KC_LALT,  KC_ENT,   MO(1),   MO(2),  KC_SPC, KC_MINS, KC_EXLM, KC_SLSH
     ),
 
 /*
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_SYM] = LAYOUT(
    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                       KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
- KC_PDOT,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_PEQL,
+  KC_DOT,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_PEQL,
  KC_LALT, KC_QUES, KC_EXLM,   KC_AT, KC_HASH,   KC_LT, KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC,   KC_GT, KC_CIRC, KC_PSLS, KC_ASTR, KC_PLUS, KC_PMNS,
                              KC_DLR, KC_PERC, KC_AMPR, KC_DQUO,   MO(1),   MO(2), KC_RALT, KC_TILD, KC_UNDS, KC_PIPE
     ),
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  _______, KC_BRIU, KC_MNXT, KC_MFFD, KC_MPLY, KC_VOLU,                                     KC_HOME, KC_PGUP,   KC_UP, KC_PGDN, _______,  KC_DEL,
  _______, KC_BRID, KC_MPRV, KC_MRWD, KC_MSTP, KC_VOLD,                                      KC_END, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_BSPC,
  KC_LSFT, _______, _______, _______, _______, KC_MUTE, _______, _______, KC_RSFT,  KC_APP, KC_PSCR, _______, _______, _______, KC_SLCK,  KC_INS,
-                            KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS,   MO(1),   MO(2), KC_UNDO,  KC_CUT, KC_COPY, KC_PSTE
+                            KC_LCTL, KC_LGUI, KC_LALT, _______,   MO(1),   MO(2), KC_UNDO,  KC_CUT, KC_COPY, KC_PSTE
     ),
 
 // /*
