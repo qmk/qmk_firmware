ae5ec395cfd7971fae85115c3e2cb71ccd1f2ef8
Ga68 <github.ga68.e@grxme.com>
2021-12-28 22:05:59 -0800

    Adjust tap_code16 to account for TAP_HOLD_CAPS_DELAY

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -98,9 +98,12 @@ void unregister_code16(uint16_t code) {
 
 void tap_code16(uint16_t code) {
     register_code16(code);
-#if TAP_CODE_DELAY > 0
-    wait_ms(TAP_CODE_DELAY);
-#endif
+    if (code == KC_CAPS_LOCK) {
+        wait_ms(TAP_HOLD_CAPS_DELAY);
+    }
+    else if (TAP_CODE_DELAY > 0) {
+        wait_ms(TAP_CODE_DELAY); 
+    }
     unregister_code16(code);
 }
 
