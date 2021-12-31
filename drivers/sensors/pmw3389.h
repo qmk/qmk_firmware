4f07db74c9f2dfce8a9d1b5ef8094aab0155815f
Alabastard-64 <96358682+Alabastard-64@users.noreply.github.com>
2021-12-31 09:59:51 -0700

    matching latest pmw3360 code

--- drivers/sensors/pmw3389.h
+++ drivers/sensors/pmw3389.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 Alabastard (@Alapastard-64)
+/* Copyright 2021 Alabastard (@Alabastard-64)
  * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
  * Copyright 2019 Sunjun Kim
  * Copyright 2020 Ploopy Corporation
@@ -20,8 +20,6 @@
 #pragma once
 
 #include <stdint.h>
-#include "report.h"
-#include "spi_master.h"
 
 #ifndef PMW3389_CPI
 #    define PMW3389_CPI 2000
@@ -63,30 +61,20 @@
 #    define PMW3389_FIRMWARE_H "pmw3389_firmware.h"
 #endif
 
-#ifdef CONSOLE_ENABLE
-void print_byte(uint8_t byte);
-#endif
-
 typedef struct {
     int8_t  motion;
     bool    isMotion;     // True if a motion is detected.
     bool    isOnSurface;  // True when a chip is on a surface
     int16_t dx;           // displacement on x directions. Unit: Count. (CPI * Count = Inch value)
     int8_t  mdx;
-    int16_t dy;  // displacement on y directions.
+    int16_t dy;           // displacement on y directions.
     int8_t  mdy;
 } report_pmw3389_t;
 
-bool             spi_start_adv(void);
-void             spi_stop_adv(void);
-spi_status_t     spi_write_adv(uint8_t reg_addr, uint8_t data);
-uint8_t          spi_read_adv(uint8_t reg_addr);
 bool             pmw3389_init(void);
-void             pmw3389_set_cpi(uint16_t cpi);
-uint16_t         pmw3389_get_cpi(void);
 void             pmw3389_upload_firmware(void);
 bool             pmw3389_check_signature(void);
+uint16_t         pmw3389_get_cpi(void);
+void             pmw3389_set_cpi(uint16_t cpi);
+/* Reads and clears the current delta values on the sensor */
 report_pmw3389_t pmw3389_read_burst(void);
-
-#define degToRad(angleInDegrees) ((angleInDegrees)*M_PI / 180.0)
-#define radToDeg(angleInRadians) ((angleInRadians)*180.0 / M_PI)
