5f33834713e5f5d662593e1ebb0d9b10bccecddd
Dan White <github@bluetufa.com>
2021-12-31 13:41:27 -0700

    latest and greatest changes after resetting master branch

--- users/badger/badger.h
+++ users/badger/badger.h
@@ -17,37 +17,53 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 enum layers {
-  _QWERTY_MAC,
-  _MOVE_MAC,
+  _QWERTY,
+  _MOVE,
   _QWERTY_LINUX,
   _MOVE_LINUX,
-  _ADJUST,
+  _FUNCTION,
+  _MACRO,
   _CONFIG
 };
 
 enum CustomKeys {
   CS_RIGHT = SAFE_RANGE,
-  CS_DOWN
+  CS_DOWN,
+  SHEBANG,
+  SHRUG,
+  FN,
+  CONST,
+  SELECT,
+  FOR_LOOP,
+  SED,
+  BASH_IF,
+  P_PAREN,
+  P_BRACE,
+  P_BRACK,
+  TS_IMPT,
 };
 
 #define OS_POP     LCTL(KC_F10)
-#define MAC_POP    LCTL(KC_UP)
+#define MAC_POP    LT(_CONFIG, LCTL(KC_UP))
 #define MAC_FRC    LGUI(LALT(KC_ESC))
 #define OS_COPY    LSFT(LCTL(KC_C))
 #define OS_PAST    LSFT(LCTL(KC_V))
 #define MAC_PST    LGUI(KC_V)
 #define MAC_CPY    LGUI(KC_C)
-#define KC_BACK    LCTL(LSFT(KC_LBRC))
-#define KC_NEXT    LCTL(LSFT(KC_RBRC))
+#define KC_BACK    LGUI(KC_LBRC)
+#define KC_NEXT    LGUI(KC_RBRC)
 
 #define MOVE       LT(_MOVE_LINUX, KC_ESC)
-#define MOVE_MAC   LT(_MOVE_MAC, KC_ESC)
-#define ADJUST     MO(_ADJUST)
-#define CFG_MAC    LT(_CONFIG, MAC_POP)
-#define CFG_LNX    LT(_CONFIG, OS_POP)
+#define MOVE_MAC   LT(_MOVE, KC_ESC)
+#define LT_END     LT(_MACRO, KC_END)
+#define FUNCTION   MO(_FUNCTION)
+#define CONFIG     MO(_CONFIG)
+#define J_MACRO    MO(_MACRO)
+#define CFG_LNX    MO(_CONFIG)
 
 #define WD_BACK    LALT(KC_LEFT)
 #define WD_FRWD    LALT(KC_RIGHT)
+#define WD_DELE    LALT(KC_BSPC)
 
 #define VD_1       LCTL(KC_F1)
 #define VD_2       LCTL(KC_F2)
@@ -79,13 +95,31 @@ enum CustomKeys {
 #define IJ_RUN     KC_F9
 #define IJ_STEP    KC_F8
 #define IJ_INTO    LSFT(KC_F7)
-#define IJ_OUT     LSFT(KC_F8) 
-#define IJ_STOP    LGUI(KC_F2) 
+#define IJ_OUT     LSFT(KC_F8)
+#define IJ_STOP    LGUI(KC_F2)
 #define IJ_IMPS    LCTL(LALT(LSFT(KC_EQUAL)))
 #define IJ_IMPH    LCTL(LALT(LSFT(KC_MINUS)))
 #define IJ_TOP     LGUI(KC_HOME)
 #define IJ_BOTT    LGUI(KC_END)
 #define IJ_FIND    LGUI(LSFT(KC_F))
+#define IJ_RELD    LGUI(LSFT(KC_F9))
+
+/* VS Code shortcuts */
+#define VS_CMD     LGUI(LSFT(KC_A))
+#define VS_EXP     LGUI(LSFT(KC_E))
+#define VS_DBG     LGUI(LSFT(KC_F8))
+#define VS_SCC     LCTL(LSFT(KC_G))
+#define VS_RUN     KC_F5
+#define VS_STEP    KC_F8
+#define VS_INTO    KC_F7
+#define VS_BPT     LGUI(KC_F8)
+#define VS_OUT     LSFT(KC_F8)
+#define VS_STOP    LSFT(KC_F5)
+#define VS_TERM    LSFT(KC_ESC)
+#define VS_FILE    LSFT(LGUI(KC_O))
+#define VS_LINE    LGUI(KC_L)
+#define VS_MVUP    LSFT(LALT(KC_UP))
+#define VS_MVDN    LSFT(LALT(KC_DOWN))
 
 /* MacOS virtual desktop shortcuts */
 #define M_VD1      LCTL(KC_1)
@@ -97,7 +131,7 @@ enum CustomKeys {
 #define MM_URCN    LCTL(LGUI(KC_RIGHT))
 #define MM_LLCN    LCTL(LSFT(LGUI(KC_LEFT)))
 #define MM_LRCN    LCTL(LSFT(LGUI(KC_RIGHT)))
-#define MM_MAX     LALT(LGUI(KC_F))
+#define MM_MAX     LCTL(LALT(KC_ENTER))
 #define MM_LH      HYPR(KC_LBRC)
 #define MM_RH      HYPR(KC_RBRC)
 #define MM_UH      LALT(LGUI(KC_UP))
@@ -105,5 +139,7 @@ enum CustomKeys {
 #define MM_LEFT    LCTL(LALT(LGUI(KC_LEFT)))
 #define MM_RGHT    LCTL(LALT(LGUI(KC_RIGHT)))
 
-#define DF_1       DF(_QWERTY_MAC)
-#define DF_2       DF(_QWERTY_LINUX)
+#define DF_1       DF(_QWERTY)
+#define DF_2       DF(_MOVE)
+#define DF_3       DF(_FUNCTION)
+#define DF_4       DF(_MACRO)
--- users/badger/ortho.c
+++ users/badger/ortho.c
@@ -28,7 +28,7 @@ float raiseLayerSong[][2]              = SONG(RAISE_LAYER_SONG);
 float lowerLayerSong[][2]              = SONG(LOWER_LAYER_SONG);
 float agSwapSong[][2]                  = SONG(LONG_AG_SWAP);
 float agNormSong[][2]                  = SONG(LONG_AG_NORM);
-#endif 
+#endif
 
 __attribute__ ((weak))
 void keyboard_post_init_user(void) {
@@ -109,7 +109,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // don't lock the ADJUST layer
         // since this key is accessible via the ADJUST
         // layer, as it will require tricky state management
-        if (++_currentLayer == _ADJUST_ORTHO) {
+        if (++_currentLayer == _FUNCTION_ORTHO) {
           _currentLayer = _QWERTY_MAC_ORTHO;
         } else {
           layer_on(_currentLayer);
@@ -149,3 +149,4 @@ void playSongForLayer(int currentLayer) {
   }
   #endif
 }
+
