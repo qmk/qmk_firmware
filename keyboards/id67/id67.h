334094941b1ec9412154058d07fd5533eebae2a9
thewerther <johannes.puntigam@student.tugraz.at>
2021-12-31 11:46:47 +0100

    Revert commits that replaced the RGB Lightning feature with the RGB Matrix feature.
    Reverted commits:
    - 217fcfa6a08978e1e6f3c45378f532703d79c738
    - a6d9930363984ba3f3aaf4e070304870a7597c6e
    - e4d54f779a84c9c2e4f645d5b711d9145d760c15
    - 75a864d9cf8e92fa6e1fe969b70cf1769c55f00c
    - dad105def947930230d7f5da7431742fb9dd361a
    - d6ba4d6943de513f2d855633f6b23c79f00e2032
    - f3dd25f8b76f3b03f73f36767e1277ad23dfdc00
    - 5c8bb49f0fd59807ee2126ef970b9b56db9cbbdb
    - 264ad540879dca85026d6e99c44a339f5ca9cfd6
    - 1a33103ba2fe582131b9a4c9c46a1e4c535187bd

--- keyboards/id67/id67.h
+++ keyboards/id67/id67.h
@@ -1,5 +1,4 @@
 /* Copyright 2021 Tybera
- * Copyright 2021 thewerther
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- keyboards/id67/rules.mk
+++ keyboards/id67/rules.mk
@@ -14,11 +14,8 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-
-RGB_MATRIX_ENABLE = yes     # Enable RGB Matrix feature
-RGB_MATRIX_DRIVER = WS2812 	# ID67 uses WS2812 driver
 
 KEY_LOCK_ENABLE = yes       # Enable KC_LOCK support
 
