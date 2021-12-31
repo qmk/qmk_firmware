4979485aea9f91056f854e626c78b38130bedce3
onefiftynine <jlee@projectkeyboard.com>
2021-12-31 16:01:35 -0600

    Signature65 Layout
    Nomenclature

--- keyboards/projectkb/signature65/signature65.h
+++ keyboards/projectkb/signature65/signature65.h
@@ -3,7 +3,6 @@
 #include "quantum.h"
 
 #define KNO KC_NO
-#define LAYOUT_default LAYOUT_fc65_625_split_bs
 
 #define LAYOUT_all( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
@@ -19,7 +18,7 @@
   {  K40, K41, K42, KNO, KNO, K45, KNO, KNO, KNO, K49, K4A, KNO, K4C, K4D, KNO, K4F  } \
 }
 
-#define LAYOUT_fc65_625_full_bs( \
+#define LAYOUT_65_625_full_bs( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K0F, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K2F, \
@@ -33,7 +32,7 @@
   {  K40, K41, K42, KNO, KNO, K45, KNO, KNO, KNO, K49, K4A, KNO, K4C, K4D, KNO, K4F  } \
 }
 
-#define LAYOUT_fc65_625_split_bs( \
+#define LAYOUT_65_625_split_bs( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K2F, \
@@ -47,7 +46,7 @@
   {  K40, K41, K42, KNO, KNO, K45, KNO, KNO, KNO, K49, K4A, KNO, K4C, K4D, KNO, K4F  } \
 }
 
-#define LAYOUT_fc65_700_full_bs( \
+#define LAYOUT_65_700_full_bs( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      K0F, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K2F, \
@@ -61,7 +60,7 @@
   {  K40, K41, K42, KNO, KNO, K45, KNO, KNO, KNO, K49, K4A, KNO, K4C, K4D, KNO, K4F  } \
 }
 
-#define LAYOUT_fc65_700_split_bs( \
+#define LAYOUT_65_700_split_bs( \
   K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
   K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,      K2F, \
