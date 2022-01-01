01cfdc0e528602fdba8cbd7efaa8b99a7205ade0
EricGebhart <e.a.gebhart@gmail.com>
2022-01-01 12:06:27 -0500

    updated ergodox ez, to the new way.

--- keyboards/ergodox_ez/keymaps/ericgebhart/layouts.h
+++ keyboards/ergodox_ez/keymaps/ericgebhart/layouts.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
   Copyright 2018 Eric Gebhart <e.a.gebhart@gmail.com>
 
@@ -15,6 +16,8 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+=======
+>>>>>>> 9f39ec8648 (updated ergodox ez, to the new way.)
 /********************************************************************/
 /* Ergodox EZ                                                       */
 /********************************************************************/
@@ -80,6 +83,7 @@
                                                                         )
 
 
+<<<<<<< HEAD
 #define Transient_dox(                                                 \
                       K01, K02, K03, K04, K05,                      \
                       K07, K08, K09, K0A, K0B,                      \
@@ -101,3 +105,62 @@
              ___5___, ___5___,                                          \
              ___12___                                                   \
                                                                         )
+=======
+#define Base_dox_bepo6(                                                 \
+                       K01, K02, K03, K04, K05, K06,                    \
+                       K07, K08, K09, K0A, K0B, K0C,                    \
+                       K11, K12, K13, K14, K15, K16,                    \
+                       K17, K18, K19, K1A, K1B, K1C,                    \
+                       K21, K22, K23, K24, K25, K26,                    \
+                       K27, K28, K29, K2A, K2B, K2C                     \
+                                                                        ) \
+  LVARG_edox(                                                           \
+             ___6SYMBOL_BEPO_L___,                                      \
+             ___2_MIDDLE_T_BP___,                                       \
+             ___6SYMBOL_BEPO_R___,                                      \
+             ROW1_LEFT_BP6(K01, K02, K03, K04, K05, K06),               \
+             ___2_MIDDLE_2_BP___,                                       \
+             ROW1_RIGHT_BP6(K07, K08, K09, K0A, K0B, K0C),              \
+                                                                        \
+             ROW2_LEFT_BP6(K11, K12, K13, K14, K15, K16),               \
+             ROW2_RIGHT_BP6(K17, K18, K19, K1A, K1B, K1C),              \
+                                                                        \
+             ROW3_LEFT_BP6(K21, K22, K23, K24, K25, K26),               \
+             ___2_MIDDLE_3_BP___,                                       \
+             ROW3_RIGHT_BP6(K27, K28, K29, K2A, K2B, K2C),              \
+                                                                        \
+             ___5_BOTTOM_LEFT_BP___, ___5_BOTTOM_RIGHT_BP___,           \
+             ___12_DOX_ALL_THUMBS_BP___                                 \
+                                                                        )
+
+#define Transient_dox6(                                                 \
+                       K01, K02, K03, K04, K05, K06,                    \
+                       K07, K08, K09, K0A, K0B, K0C,                    \
+                       K11, K12, K13, K14, K15, K16,                    \
+                       K17, K18, K19, K1A, K1B, K1C,                    \
+                       K21, K22, K23, K24, K25, K26,                    \
+                       K27, K28, K29, K2A, K2B, K2C,                    \
+                       K31, K32, K33, K34, K35, K36,                    \
+                       K37, K38, K39, K3A, K3B, K3C                     \
+                                                                        ) \
+  LVARG_edox(                                                           \
+             K01, K02, K03, K04, K05, K06,                              \
+             ___2___,                                                   \
+             K07, K08, K09, K0A, K0B, K0C,                              \
+             K11, K12, K13, K14, K15, K16,                              \
+             ___2___,                                                   \
+             K17, K18, K19, K1A, K1B, K1C,                              \
+             K21, K22, K23, K24, K25, K26,                              \
+             K27, K28, K29, K2A, K2B, K2C,                              \
+             K31, K32, K33, K34, K35, K36,                              \
+             ___2___,                                                   \
+             K37, K38, K39, K3A, K3B, K3C,                              \
+             ___5___, ___5___,                                          \
+             ___12___                                                   \
+                                                                        )
+
+#define Dox_base(...)        Base_dox(__VA_ARGS__)
+#define Dox_bepo_base(...)   Base_dox_bepo(__VA_ARGS__)
+#define Dox_bepo_base6(...)  Base_dox_bepo6(__VA_ARGS__)
+#define Dox_transient(...)   Transient_dox6(__VA_ARGS__)
+>>>>>>> 9f39ec8648 (updated ergodox ez, to the new way.)
