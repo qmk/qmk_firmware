741ccc2a62988b7989a636ad2195f89c4c370484
ChrisGVE <christian.berclaz@mac.com>
2022-01-02 22:22:12 +0800

    Odin: added mouse support (including momentary)

--- keyboards/kbdfans/odin/soldered/keymaps/chrisgve/keymap.c
+++ keyboards/kbdfans/odin/soldered/keymaps/chrisgve/keymap.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SLCK, KC_PAUS, KC_PGUP, KC_PGDN, 		
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,  KC_HOME, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
 		KC_LCTL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_END,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
-		KC_CAPS, KC_A,    KC_S,    M_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-		KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_M,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
+		KC_CAPS, A_MOUSE, KC_S,    M_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		SFT_MSE, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_M,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
 		KC_LCTL, KC_LALT, KC_LGUI,                   SPC_RSE,                   KC_RGUI, KC_RALT, _______, _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
     ),
 
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SLCK, KC_PAUS, KC_PGUP, KC_PGDN, 		
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,  KC_HOME, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_END,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
-		KC_LCTL, KC_A,    KC_S,    L_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-		KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_L,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
+		KC_LCTL, A_MOUSE, KC_S,    L_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		SFT_MSE, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_L,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
 		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   KC_RALT, KC_RGUI, _______, _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
     ),
 
@@ -78,8 +78,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SLCK, KC_PAUS, KC_PGUP, KC_PGDN, 		
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,  KC_HOME, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_END,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
-		KC_LCTL, KC_A,    KC_S,    W_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-		KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_W,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
+		KC_LCTL, A_MOUSE, KC_S,    W_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		SFT_MSE, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_W,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
 		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   KC_RALT, KC_RGUI, _______, _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
     ),
 
@@ -113,28 +113,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ADJUST_MAC] =LAYOUT_all(
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, 		
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-		RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, 
-		_______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,                   _______, _______, _______, _______,
-		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, _______, KC_VOLU,          _______, _______, _______, _______, 
+		RESET,   MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, 
+		_______, DF_M_P,  DF_L_P,  DF_W_P,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,                   _______, _______, _______, _______,
+		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, _______, KC_MUTE, _______, _______, _______, KC_APFN, _______, KC_VOLU,          _______, _______, _______, _______, 
 		_______, _______, _______,                   KC_MPLY,                   _______, _______, _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT, _______, _______, _______
     ),
 
 	[_ADJUST_LINUX] =LAYOUT_all(
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, 		
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-		RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, 
-		_______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,                   _______, _______, _______, _______,
-		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, _______, KC_VOLU,          _______, _______, _______, _______, 
+		RESET,   MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, 
+		_______, DF_M_P,  DF_L_P,  DF_W_P,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,                   _______, _______, _______, _______,
+		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, _______, KC_MUTE, _______, _______, _______, INV_AFN, _______, KC_VOLU,          _______, _______, _______, _______, 
 		_______, _______, _______,                   KC_MPLY,                   _______, _______, _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT, _______, _______, _______
     ),
 
 	[_ADJUST_WIN] =LAYOUT_all(
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, 		
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-		RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, 
-		_______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,                   _______, _______, _______, _______,
-		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, _______, KC_VOLU,          _______, _______, _______, _______, 
+		RESET,   MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, 
+		_______, DF_M_P,  DF_L_P,  DF_W_P,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,                   _______, _______, _______, _______,
+		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, _______, KC_MUTE, _______, _______, _______, INV_AFN, _______, KC_VOLU,          _______, _______, _______, _______, 
 		_______, _______, _______,                   KC_MPLY,                   _______, _______, _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT, _______, _______, _______
+    ),
+
+	[_MOUSE] =LAYOUT_all(
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, 		
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, 
+		_______, _______, KC_ACL2, KC_ACL1, KC_ACL0, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,          _______,                   _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, KC_WH_U, KC_WH_D, _______, _______, _______, _______, _______, _______, KC_MS_U,          _______, _______, _______, _______, 
+		_______, _______, _______,                   KC_BTN1,                   _______, _______, _______, _______,          KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______
+    ),
+
+	[_EX_MOUSE] =LAYOUT_all(
+		TG_MSE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 		
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+		XXXXXXX, XXXXXXX, KC_ACL2, KC_ACL1, KC_ACL0, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,          XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		TG_MSE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, KC_WH_D, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_U,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+		XXXXXXX, XXXXXXX, XXXXXXX,                   KC_BTN1,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
 };
