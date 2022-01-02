2a9109fcc3b47673f01822119c02dd75883e1ad3
Yuannan <yuannan@users.noreply.github.com>
2022-01-02 23:01:31 +0000

    Added second delay array for autoclicker to compensate for RGB animation CPU loads

--- keyboards/dztech/dz65rgb/keymaps/yuannan/README.md
+++ keyboards/dztech/dz65rgb/keymaps/yuannan/README.md
@@ -11,14 +11,17 @@
 
 1. VIA/VIAL support
 2. Improved navigation via VIM like keys.
-3. Much better mouse support. Now able to: move mouse around with 3 levels of accleration, click and nav using mouse buttons 1-5, mouse scroll in all 4 directions, autoclick with 4 different levels of speed (toggle or burst!)
-4. Improved granularity of the RGB Matrix now with steps of 8/256 (32 steps!)
-5. Dynamic Macros now enabled!
-6. Disabled some effects to free up more space.
+3. Dynamic Macros now enabled!
+4. Custom Editing Macros such as:
+	- Deleting entire line
+	- Deleting from cursor to start of line
+	- Deleting form cursor to end of line
+5. Much better mouse support. Now able to: move mouse around with 3 levels of accleration, click and nav using mouse buttons 1-5, mouse scroll in all 4 directions, autoclick with 5 different levels of speed! Toggle in speeds of {0, 20, 50, 100, 1000} -> {1000, 50, 20, 10, 1} clicks per second. In reality it will be lower due to board and OS lag.
+6. Improved granularity of the RGB Matrix now with steps of 8/256 (32 steps!)
+7. Disabled some effects to free up more space.
 
-### VIAL Support
+### Notes
 
-Extra macros in FN1:
-- Deleting entire line
-- Deleting from cursor to start of line
-- Deleting form cursor to end of line
+The RGB animation on the board actually eats a load of CPU cycles. Enough to actually put a massive dent in the clicks per second output. So if you wish to optimal CPS then disable the RGB. Even you set the brightness all the way down to 0 the animation code will still run. This is true as of (2022/01/02), I hope someone may fix this. In the meantime I have created a second array for delays for the autoclicker while RGB mode is active. Users to note there is yet another caveat to this. Because of  the FN layer indicator setting it's own colour the animation does not compute it for that key. When your FN layers are mostly populated like it is on this keymap it actually speeds up the animation compute time by a lot. I could compensate for this once again but it'll be a chase into oblivion.
+
+TL;DR: If you wish for the fastest and most time accurate autotlicker you should turn RGB OFF COMPLETELY. Setting brighness to 0 is not the same.
--- keyboards/dztech/dz65rgb/keymaps/yuannan/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/yuannan/keymap.c
@@ -1,34 +1,52 @@
-#include QMK_KEYBOARD_H
+/* Copyright 2021 Yuannan (https://github.com/yuannan)
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
 
+#include QMK_KEYBOARD_H
 #include "mousekey.h"
 
 enum custom_keycodes {
     AUTOCLICK_TOGGLE = SAFE_RANGE,
     RAPID_CLICK,
-    CYCLE_CLICK_SPEEDS
+    CYCLE_CLICK_SPEEDS,
+    DEL_LINE,
+    DEL_HOME,
+    DEL_END
 };
 
 enum my_layers {
-	_ALPHA_LAYER,
-	_VIM_EDITING_LAYER,
-	_MOUSE_LAYER,
-	_HYPER_LAYER
+    _ALPHA_LAYER,
+    _VIM_EDITING_LAYER,
+    _MOUSE_LAYER,
+    _HYPER_LAYER
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_ALPHA_LAYER] = LAYOUT_65_ansi(
-		KC_GESC,		KC_1,		KC_2,	KC_3,	KC_4,	KC_5,	KC_6,	KC_7,	KC_8,		KC_9,	KC_0,		KC_MINUS,		KC_EQUAL,		KC_BSPACE,	KC_HOME,
-		KC_TAB,			KC_Q,		KC_W,	KC_E,	KC_R,	KC_T,	KC_Y,	KC_U,	KC_I,		KC_O,	KC_P,		KC_LBRACKET,	KC_RBRACKET,	KC_BSLASH,	KC_PGUP,
-		KC_LCTRL,		KC_A,		KC_S,	KC_D,	KC_F,	KC_G,	KC_H,	KC_J,	KC_K,		KC_L,	KC_SCOLON,	KC_QUOTE,						KC_ENTER,	KC_PGDOWN,
-		KC_LSHIFT,		KC_Z,		KC_X,	KC_C,	KC_V,	KC_B,	KC_N,	KC_M,	KC_COMMA,	KC_DOT,	KC_SLASH,	KC_RSHIFT,						KC_UP,		KC_END,
-		KC_CAPSLOCK,	KC_LGUI,	FN_MO13,				KC_SPACE,				FN_MO23,	KC_RALT,	KC_RCTRL,	KC_LEFT,					KC_DOWN,	KC_RIGHT
+	[_ALPHA_LAYER] = LAYOUT_65_ansi(
+		KC_GESC,	KC_1,	KC_2,	KC_3,   KC_4,	KC_5,	KC_6,	KC_7,	KC_8,		KC_9,	KC_0,		KC_MINUS,	KC_EQUAL,		KC_BSPACE,	KC_HOME,
+		KC_TAB,		KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	KC_Y,	KC_U,	KC_I,		KC_O,	KC_P,		KC_LBRACKET,KC_RBRACKET,	KC_BSLASH,	KC_PGUP,
+		KC_LCTRL,	KC_A,   KC_S,	KC_D,	KC_F,	KC_G,	KC_H,	KC_J,	KC_K,		KC_L,	KC_SCOLON,	KC_QUOTE,	        KC_ENTER,	        KC_PGDOWN,
+		KC_LSHIFT,	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,	KC_N,	KC_M,	KC_COMMA,	KC_DOT,	KC_SLASH,           KC_RSHIFT,          KC_UP,		KC_END,
+		KC_CAPSLOCK,KC_LGUI,FN_MO13,				KC_SPACE,                       FN_MO23,	KC_RALT,    KC_RCTRL,       KC_LEFT,	KC_DOWN,	KC_RIGHT
     ),
     [_VIM_EDITING_LAYER] = LAYOUT_65_ansi(
-		KC_GRAVE,	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,		KC_DELETE,	KC_INSERT,
-		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_HOME,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_SCROLLLOCK,
-		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_LEFT,	KC_DOWN,	KC_UP,		KC_RIGHT,	KC_TRNS,	KC_TRNS,				KC_TRNS,	KC_PAUSE,
-		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_END,	KC_TRNS,	KC_TRNS,	KC_TRNS,		KC_TRNS,				KC_VOLU,	KC_PSCREEN,
-		KC_TRNS,	KC_TRNS,	KC_TRNS,							KC_MPLY,						KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_MPRV,					KC_VOLD,	KC_MNXT
+		KC_GRAVE,	    KC_F1,		        KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,		KC_DELETE,	KC_INSERT,
+		KC_TRNS,	    KC_TRNS,	        KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	DEL_HOME,	KC_HOME,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_SCROLLLOCK,
+		KC_TRNS,	    KC_TRNS,	        KC_TRNS,	KC_TRNS,	KC_TRNS,	DEL_LINE,	KC_LEFT,	KC_DOWN,	KC_UP,		KC_RIGHT,	KC_TRNS,	KC_TRNS,		    KC_MPLY,        KC_PAUSE,
+		KC_TRNS,        AUTOCLICK_TOGGLE,   KC_TRNS,	RAPID_CLICK,KC_TRNS,	KC_TRNS,	DEL_END,	KC_END,	    KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,				KC_VOLU,	KC_PSCREEN,
+		CYCLE_CLICK_SPEEDS,	KC_TRNS,	    KC_TRNS,                                KC_TRNS,						            KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MPRV,    KC_VOLD,	KC_MNXT
     ),
     [_MOUSE_LAYER] = LAYOUT_65_ansi(
 		KC_GRAVE,	KC_F1,				KC_F2,		KC_F3,			KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,		KC_TRNS,	KC_TRNS,
@@ -43,32 +61,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO,			KC_NO,				KC_NO,				KC_NO,		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,				KC_TRNS,	KC_TRNS,
 		KC_LSHIFT,		KC_NO,				KC_NO,				RGB_HUI,	RGB_SAI,	RGB_VAI,	RGB_SPI,	RGB_MOD,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,				KC_TRNS,	KC_TRNS,
 		KC_TRNS,		KC_TRNS,			KC_TRNS,									RGB_TOG,							KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,				KC_TRNS,	KC_TRNS
-    )
+	)
 };
 
-static bool autoclick = false;
-static bool autoclick_state = false;
-static uint16_t autoclick_timer = 0;
-
+#define CLICKSPEEDS 5
+const int clickspeed_sets[2][CLICKSPEEDS] = {{0, 19, 49, 99, 1000},{0, 14, 42, 97, 1000}};
 int clickspeed_set = 0;
-#define CLICKSPEEDS 4
-const int clickspeed_sets[CLICKSPEEDS] = {0, 20, 100, 1000};
-int press_interval = clickspeed_sets[0];
+bool autoclick = false;
+bool rapid_click = false;
+uint16_t autoclick_timer = 0;
 
 void matrix_scan_user(void) {
-  if (autoclick && timer_elapsed(autoclick_timer) > (autoclick_state ? 1 : press_interval)) {
-    if (autoclick_state) {
-      mousekey_off(KC_MS_BTN1);
-    } else {
-      mousekey_on(KC_MS_BTN1);
+    if (rapid_click){
+        tap_code(KC_BTN1);
+    } else if (autoclick && timer_elapsed(autoclick_timer) > (clickspeed_sets[rgb_matrix_is_enabled()][clickspeed_set])) {
+		tap_code(KC_BTN1);
+		autoclick_timer = timer_read();
     }
-    mousekey_send();
-    autoclick_state = !autoclick_state;
-    autoclick_timer = timer_read();
-  }
 }
 
-bool toggle_status = false;
+// Custom process key to allow for custom keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case AUTOCLICK_TOGGLE:
@@ -79,27 +91,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case RAPID_CLICK:
             if (record->event.pressed){
-                toggle_status = autoclick;
-                autoclick = true;
-                press_interval = 0;
+                rapid_click = true;
             } else{
-                autoclick = toggle_status;
-                press_interval = clickspeed_sets[clickspeed_set];
+                rapid_click = false;
             }
             break;
         case CYCLE_CLICK_SPEEDS:
             if(record->event.pressed){
                 clickspeed_set++;
                 clickspeed_set %= CLICKSPEEDS;
-                press_interval = clickspeed_sets[clickspeed_set];
             }
             break;
-    }    
-    
+        case DEL_LINE:
+            if(record->event.pressed){
+                SEND_STRING(SS_TAP(X_HOME) SS_LSFT(SS_TAP(X_END)) SS_TAP(X_DELETE));
+            }
+            break;
+        case DEL_HOME:
+            if(record->event.pressed){
+                SEND_STRING(SS_LSFT(SS_TAP(X_HOME)) SS_TAP(X_DELETE));
+            }
+            break;
+        case DEL_END:
+            if(record->event.pressed){
+                SEND_STRING(SS_LSFT(SS_TAP(X_END)) SS_TAP(X_DELETE));
+            }
+            break;
+    } 
+
     return true;
 };
 
-// show green indicator for layers with a keybind
+// show coloured indicator for layers with a keybind
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (get_highest_layer(layer_state) > 0) {
         uint8_t layer = get_highest_layer(layer_state);
@@ -110,10 +133,9 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
                 if (index >= led_min && index <= led_max && index != NO_LED &&
                 keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, 0x00, 0xFF, 0x00);
+                    rgb_matrix_set_color(index, 0xFF, 0x12, 0x34);
                 }
             }
         }
     }
-
 }
