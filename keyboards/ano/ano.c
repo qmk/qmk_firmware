a97db374272d7ddcd0a979048ff583d39dd92c30
sauvehoo <76417304+sauvehoo@users.noreply.github.com>
2022-01-03 11:21:40 -0800

    Update keyboards/ano/ano.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ano/ano.c
+++ keyboards/ano/ano.c
@@ -27,7 +27,8 @@
  */
  
 #ifdef ENCODER_ENABLE
-bool encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
     if (index == 0) { 
         if (clockwise) {
             tap_code(KC_AUDIO_VOL_UP);
