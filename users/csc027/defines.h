b2578a9ffeb20a013b9f431c739b87f0120e3c3f
Constantine Chen <csc_dev@protonmail.com>
2022-01-02 23:58:22 -0800

    Added some HSV color definitions without the value portion to allow using the existing value.

--- users/csc027/defines.h
+++ users/csc027/defines.h
@@ -321,3 +321,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     CUSTOM_NAME(vtdc)       CUSTOM_STRING(SS_LCTL(SS_LGUI(SS_TAP(X_F4))))     CUSTOM_DELIM() \
     CUSTOM_NAME(vtdn)       CUSTOM_STRING(SS_LCTL(SS_LGUI("d")))              CUSTOM_DELIM() \
     CUSTOM_NAME(vtdr)       CUSTOM_STRING(SS_LCTL(SS_LGUI(SS_TAP(X_RIGHT))))
+
+// Additional color definitions for simplicity
+#define HS_AZURE       132, 102
+#define HS_BLACK         0,   0
+#define HS_BLUE        170, 255
+#define HS_CHARTREUSE   64, 255
+#define HS_CORAL        11, 176
+#define HS_CYAN        128, 255
+#define HS_GOLD         36, 255
+#define HS_GOLDENROD    30, 218
+#define HS_GRAY          0,   0
+#define HS_GREEN        85, 255
+#define HS_MAGENTA     213, 255
+#define HS_ORANGE       28, 255
+#define HS_PINK        234, 128
+#define HS_PURPLE      191, 255
+#define HS_RED           0, 255
+#define HS_SPRINGGREEN 106, 255
+#define HS_TEAL        128, 255
+#define HS_TURQUOISE   123,  90
+#define HS_WHITE         0,   0
+#define HS_YELLOW       43, 255
