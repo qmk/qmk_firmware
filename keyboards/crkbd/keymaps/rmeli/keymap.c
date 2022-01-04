af9a276f731ea36c28759b9706f81bcef748a712
Rocco Meli <r.meli@bluemail.ch>
2022-01-05 00:07:13 +0100

    report auto shift timeout

--- keyboards/crkbd/keymaps/rmeli/keymap.c
+++ keyboards/crkbd/keymaps/rmeli/keymap.c
@@ -264,7 +264,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CFG] = LAYOUT_split_3x6_3(
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
-        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_ASTG, XXXXXXX, XXXXXXX, XXXXXXX,DF(_QWY),
+        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_ASRP, XXXXXXX, XXXXXXX, XXXXXXX,DF(_QWY),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                       UC_MOD, KC_ASTG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
