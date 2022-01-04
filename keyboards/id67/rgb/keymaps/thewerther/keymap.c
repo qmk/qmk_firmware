9e7aeb487fcf1b34e33759c294718d89a18c539c
thewerther <johannes.puntigam@student.tugraz.at>
2022-01-04 20:10:56 +0100

    Fixed underglow for all reactive effects.

--- keyboards/id67/rgb/keymaps/thewerther/keymap.c
+++ keyboards/id67/rgb/keymaps/thewerther/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_65_ansi_blocker(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          _______,
-        _______,     _______, _______,  _______, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______,     _______,
+        _______,     _______, _______,  _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______,     _______,
         _______,        _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,  _______, _______, _______,            _______,
         _______,           _______,  _______,  _______, _______, _______, _______, _______, _______, _______,  _______,  _______,  _______,    _______,
         _______,   _______,   _______,                      _______,                              _______,   _______,   _______,   _______,    _______
@@ -44,7 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void matrix_scan_user(void) {
 #   if defined(RGB_MATRIX_ENABLE)
-    if (rgb_matrix_get_mode() == RGB_MATRIX_SOLID_REACTIVE_SIMPLE) {
+    int current_effect = rgb_matrix_get_mode();
+    if (current_effect >= RGB_MATRIX_SOLID_REACTIVE_SIMPLE && current_effect <= RGB_MATRIX_SOLID_MULTISPLASH) {
         // set all underglow leds to current color
         RGB current_color = hsv_to_rgb(rgb_matrix_get_hsv());
         for (int i = DRIVER_LED_TOTAL - DRIVER_LED_UNDERGLOW; i < DRIVER_LED_TOTAL; i++) {
