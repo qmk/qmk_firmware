add46ec7fbd200ab0ff6c7f98075fe160fcc5ab5
Alabastard-64 <96358682+Alabastard-64@users.noreply.github.com>
2022-01-04 15:36:43 -0700

    Update pmw3389.h

--- drivers/sensors/pmw3389.h
+++ drivers/sensors/pmw3389.h
@@ -1,5 +1,4 @@
-/* Copyright 2021 Alabastard (@Alabastard-64)
- * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
  * Copyright 2019 Sunjun Kim
  * Copyright 2020 Ploopy Corporation
  *
