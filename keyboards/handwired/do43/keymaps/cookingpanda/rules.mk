b1be8b17f8b9f70710400cda01980f41a9c7d403
Rocco Jiang <rocco.jiang5@gmail.com>
2022-01-04 16:59:40 +0000

    Use native post_rules.mk implementation

--- keyboards/handwired/do43/keymaps/cookingpanda/rules.mk
+++
@@ -1,4 +0,0 @@
-# Keymap-specific build options and overrides
-
-# Don't modify this!
-include keyboards/$(KEYBOARD)/post_rules.mk
--- keyboards/handwired/do43/post_rules.mk
+++ keyboards/handwired/do43/post_rules.mk
@@ -1,5 +1,3 @@
-# As long as the users rules.mk has include $(KEYBOARD)/post_rules.mk this will be run after to properly setup any keyboard features and defines
-
 ifeq ($(strip $(OLED_ENABLE)), yes)
     SRC += do43_oled.c
 endif
