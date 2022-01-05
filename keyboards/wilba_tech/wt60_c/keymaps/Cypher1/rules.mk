1ebb2b57625da38cfba12f58d45e1cd562a1b710
J Pratt <jp10010101010000@gmail.com>
2022-01-05 15:46:16 +1100

    Update configs to match VIA settings

--- keyboards/wilba_tech/wt60_c/keymaps/Cypher1/rules.mk
+++ keyboards/wilba_tech/wt60_c/keymaps/Cypher1/rules.mk
@@ -5,8 +5,8 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 
-# RGBLIGHT_ENABLE = no        # Enable support for RGB LEDs
-# BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable support for RGB LEDs
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
