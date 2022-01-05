b064e1d1b54e61bad8b26d9064dfba2305cd5612
J Pratt <jp10010101010000@gmail.com>
2022-01-05 13:58:43 +1100

    Add custom keymapping

---
+++ keyboards/wilba_tech/wt60_c/keymaps/Cypher1/rules.mk
@@ -0,0 +1,13 @@
+VIA_ENABLE = yes
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+
+# RGBLIGHT_ENABLE = no        # Enable support for RGB LEDs
+# BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+AUDIO_ENABLE = no           # Audio output
