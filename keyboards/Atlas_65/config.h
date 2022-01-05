e210cfc09b82f5dab8dfa4e7e91a04a709f03e57
Ryan Smith <ryan@ryanstevensmith.co.uk>
2022-01-05 09:56:27 +0000

    first release
    first attempt at releasing the board

--- keyboards/Atlas_65/config.h
+++ keyboards/Atlas_65/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Joshua Nguyen
-#define PRODUCT         atlas-65
+#define PRODUCT         atlas_65
 
 /* key matrix size */
 #define MATRIX_ROWS 5
