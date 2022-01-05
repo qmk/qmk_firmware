0d9f61fd9e9b9db312cc7dab9b560150f174f6fd
Nigel Choi <9gel@users.noreply.github.com>
2022-01-06 01:18:55 +0800

    Example to hook up CK protocol with lights

--- keyboards/chonkerkeys/protocol.c
+++ keyboards/chonkerkeys/protocol.c
@@ -4,27 +4,45 @@
 #include "event_type.h"
 #include "event.h"
 #include "command.h"
+#include "rgb_strands.h"
+#include "color.h"
 #include <stdio.h>
+#include <string.h>
 
 uint8_t get_key_light_type(uint8_t x, uint8_t y) {
     return mute;
 }
 
 void update_light(uint8_t type, uint8_t state) {
-    // TODO
+    // TODO: need to figure out the RGB strand for the light type
+    //uint8_t strand = key_strand[0][0];
+    uint8_t strand = 0;
+
     if (type == mute) {
         if (state == 0) {
-            SEND_STRING("mute light is off");
+            // off
+            rgb_strand_set_color(strand, HSV_OFF);
         } else if (state == 1) {
-            SEND_STRING("mute light is on");
+            // mute
+            rgb_strand_set_color(strand, HSV_RED);
+        } else if (state == 2) {
+            // unmute
+            rgb_strand_set_color(strand, HSV_GREEN);
         }
     } else if (type == share_screen) {
         if (state == 0) {
-            SEND_STRING("share screen light is off");
+            // off
+            rgb_strand_set_color(strand, HSV_OFF);
         } else if (state == 1) {
-            SEND_STRING("share screen light is intermediate");
+            // blinking
+            const rgb_strand_anim_config_t *dcfg = get_default_rgb_strand_anim_config(RGB_STRAND_EFFECT_BLINKY);
+            rgb_strand_anim_config_t cfg;
+            memcpy(&cfg, dcfg, sizeof(rgb_strand_anim_config_t));
+            cfg.num_times = 0; // blink forever until shared
+            rgb_strand_animation_start(strand, RGB_STRAND_EFFECT_BLINKY, &cfg, RGB_STRAND_ANIM_STATE_START);
         } else if (state == 2) {
-            SEND_STRING("share screen light is on");
+            // screen sharing
+            rgb_strand_set_color(strand, HSV_GREEN);
         }
     }
 }
