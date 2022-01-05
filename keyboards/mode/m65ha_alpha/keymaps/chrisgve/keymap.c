10a1ba287499b3bf6c7be3c3abf5021fc8aa5134
ChrisGVE <christian.berclaz@mac.com>
2022-01-06 00:03:11 +0800

    M65 Hotswap: Switched the right alt/gui key

--- keyboards/mode/m65ha_alpha/keymaps/chrisgve/keymap.c
+++ keyboards/mode/m65ha_alpha/keymaps/chrisgve/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGUP,
         KC_LCTL, A_MOUSE, KC_S,    M_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT ,          KC_PGDN,
 	    SFT_MSE,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,          KC_UP,   END_F_M,
-        KC_LCTL, KC_LALT, KC_LGUI,                            SPC_RSE,                            KC_RGUI, _______,          KC_LEFT, KC_DOWN, KC_RGHT 
+        KC_LCTL, KC_LALT, KC_LGUI,                            SPC_RSE,                            KC_RGUI, KC_RGUI,          KC_LEFT, KC_DOWN, KC_RGHT 
 	),
 
    [_MAC_NAV_1] = LAYOUT_all(
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGDN,
         KC_LCTL, A_MOUSE, KC_S,    L_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT ,          KC_PGDN,
 	    SFT_MSE,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,          KC_UP,   ADJ_L,
-        KC_LCTL, KC_LGUI, KC_LALT,                            SPC_RSE,                            KC_RALT, _______,          KC_LEFT, KC_DOWN, KC_RGHT 
+        KC_LCTL, KC_LGUI, KC_LALT,                            SPC_RSE,                            KC_RALT, KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT 
 	),
 
    [_LINUX_NAV_1] = LAYOUT_all(
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGDN,
         KC_LCTL, A_MOUSE, KC_S,    W_NAV_1, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT ,          KC_PGDN,
 	    SFT_MSE,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,          KC_UP,   ADJ_W,
-        KC_LCTL, KC_LGUI, KC_LALT,                            SPC_RSE,                            KC_RALT, _______,          KC_LEFT, KC_DOWN, KC_RGHT 
+        KC_LCTL, KC_LGUI, KC_LALT,                            SPC_RSE,                            KC_RALT, KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT 
 	),
 
    [_WIN_NAV_1] = LAYOUT_all(
