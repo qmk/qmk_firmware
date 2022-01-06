74631dc5c43db9cadd0beae8c02cb5ab83ef5977
LucasWaelti <lucas.waelti@gmail.com>
2022-01-06 10:57:13 +0100

    Corrected copy/paste macros

--- keyboards/splitkb/kyria/keymaps/lw/keymap.c
+++ keyboards/splitkb/kyria/keymaps/lw/keymap.c
@@ -104,21 +104,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * 2. Nav Layer: Media, navigation, accents
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        | Brt+ | Next |      | Play | Vol+ |                              | Home | PgUp |  Up  | PgDn |      | Del    |
+ * |        | Brt+ | Next | Fwd  | Play | Vol+ |                              | Home | PgUp |  Up  | PgDn |      | Del    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        | Brt- | Prev |      | Stop | Vol- |                              |  End | Left | Down | Right|ScrLck| BkSpace|
+ * |        | Brt- | Prev | Bckw | Stop | Vol- |                              |  End | Left | Down | Right|ScrLck| BkSpace|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |  ´   |  `   |  ^   |  ¨   | Mute |      |UC_MOD|  |RShift| Menu | PrtSc| ç/Ç  | § °  |      |      | Insert |
+ * | LShift |  ´   |  `   |  ^   |  ¨   | Mute |      |UC_MOD|  |RShift| Menu | PrtSc| ç/Ç  | § °  |      | SAVE | Insert |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |LCtrl | LGUI | LAlt |      | MO(1)|  | MO(2)|      |      |      |      |
+ *                        |LCtrl | LGUI | LAlt |      | MO(1)|  | MO(2)| UNDO |  CUT | COPY | PASTE|
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAV] = LAYOUT(
  _______, KC_BRIU, KC_MNXT, KC_MFFD, KC_MPLY, KC_VOLU,                                     KC_HOME, KC_PGUP,   KC_UP, KC_PGDN, _______,  KC_DEL,
  _______, KC_BRID, KC_MPRV, KC_MRWD, KC_MSTP, KC_VOLD,                                      KC_END, KC_LEFT, KC_DOWN, KC_RGHT, KC_SLCK, KC_BSPC,
- KC_LSFT, US_ACUT, US_DGRV, US_DCIR, US_DIAE, KC_MUTE, _______,  UC_MOD, KC_RSFT,  KC_APP, KC_PSCR, XP(CEDIL,CEDIL_MAJ), XP(SECTION,DEGREE), _______, _______,  KC_INS,
-                            KC_LCTL, KC_LGUI, KC_LALT, _______,   MO(1),   MO(2), KC_UNDO,  KC_CUT, KC_COPY, KC_PSTE
+ KC_LSFT, US_ACUT, US_DGRV, US_DCIR, US_DIAE, KC_MUTE, _______,  UC_MOD, KC_RSFT,  KC_APP, KC_PSCR, XP(CEDIL,CEDIL_MAJ), XP(SECTION,DEGREE), _______, LCTL(KC_S),  KC_INS,
+                            KC_LCTL, KC_LGUI, KC_LALT, _______,   MO(1),   MO(2), LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V)
     ),
 
 // /*
