990e895286829c2b90c75806362f4c7c199f0421
Tim Liu <74063834+Timliuzhaolu@users.noreply.github.com>
2022-01-07 14:08:05 +0800

    Update thera.h

--- keyboards/creatkeebs/thera/thera.h
+++ keyboards/creatkeebs/thera/thera.h
@@ -10,6 +10,9 @@
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
   * GNU General Public License for more details. 
   * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
 
 #pragma once
 
