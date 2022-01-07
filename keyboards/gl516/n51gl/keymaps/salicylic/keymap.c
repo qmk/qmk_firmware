3dc0084e54611b0c10ddfc0d7b9dcd5f1397bfd9
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2022-01-07 22:45:14 +0900

    Update keyboards/gl516/n51gl/keymaps/salicylic/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/gl516/n51gl/keymaps/salicylic/keymap.c
+++ keyboards/gl516/n51gl/keymaps/salicylic/keymap.c
@@ -18,7 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include "keymap_jp.h"
 
-extern uint8_t is_master;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
