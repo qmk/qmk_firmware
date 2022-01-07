d51aa8298179de6ce9418e2dc49da5bac83fbc99
Tim Liu <74063834+Timliuzhaolu@users.noreply.github.com>
2022-01-07 14:07:38 +0800

    Add license

--- keyboards/creatkeebs/thera/thera.h
+++ keyboards/creatkeebs/thera/thera.h
@@ -1,3 +1,16 @@
+/* Copyright %2022% %Tim% 
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+
 #pragma once
 
 #include "quantum.h"
