85dbde9740d9534e8f7cb087ede3f8f7927413d4
Tim Liu <74063834+Timliuzhaolu@users.noreply.github.com>
2022-01-07 14:11:06 +0800

    Update config.h

--- keyboards/creatkeebs/thera/config.h
+++ keyboards/creatkeebs/thera/config.h
@@ -1,4 +1,4 @@
-/* Copyright %2022% %Tim% 
+/* Copyright 2022 Tim (https://github.com/Timliuzhaolu)
   * 
   * This program is free software: you can redistribute it and/or modify 
   * it under the terms of the GNU General Public License as published by 
