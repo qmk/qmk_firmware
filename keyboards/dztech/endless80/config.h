09b5aae3fb63d539a0356d76ef567e73eaf4625d
moyi4681 <moyi4681@users.noreply.github.com>
2022-01-07 15:58:36 +0800

    Update keyboards/dztech/endless80/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/dztech/endless80/config.h
+++ keyboards/dztech/endless80/config.h
@@ -48,7 +48,6 @@
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 10
 #define RGBLIGHT_VAL_STEP 10
-#define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_LIMIT_VAL 130 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 #endif
 #define DEBOUNCE 5
