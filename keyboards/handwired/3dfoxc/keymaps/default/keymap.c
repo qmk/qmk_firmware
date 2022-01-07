d0575a4172053c544dab1717669300e0987190ab
david l goodrich <dlg@dsrw.org>
2022-01-06 20:26:29 -0600

    copyright, readme fixes

--- keyboards/handwired/3dfoxc/keymaps/default/keymap.c
+++ keyboards/handwired/3dfoxc/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 david l goodrich
+/* Copyright 2022 david l goodrich <dlg@dsrw.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
