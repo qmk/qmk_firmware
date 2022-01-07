6cb760d562d2d02a5e32a8dd0f5fa144f4aa492d
moyi4681 <moyi4681@users.noreply.github.com>
2022-01-07 08:48:54 +0800

    Update keyboards/kbdfans/kbd75rgb/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kbdfans/kbd75rgb/rules.mk
+++ keyboards/kbdfans/kbd75rgb/rules.mk
@@ -11,7 +11,6 @@ MOUSEKEY_ENABLE = no          # Mouse keys
 EXTRAKEY_ENABLE = yes          # Audio control and System control
 CONSOLE_ENABLE = no            # Console for debug
 COMMAND_ENABLE = no            # Commands for debug and configuration
-FORCE_NKRO = yes
 NKRO_ENABLE = yes              # USB Nkey Rollover
 BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
