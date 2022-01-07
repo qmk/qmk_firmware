c7a130d44309b71c925ef4162436e43c19aa4e5f
Tim Liu <74063834+Timliuzhaolu@users.noreply.github.com>
2022-01-07 12:04:27 +0800

    Update config.h

--- keyboards/creatkeebs/thera/config.h
+++ keyboards/creatkeebs/thera/config.h
@@ -7,9 +7,8 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define MANUFACTURER    creatkeebs
+#define PRODUCT         thera
 
 /* key matrix size */
 #define MATRIX_ROWS 6
