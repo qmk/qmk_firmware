8879eca839105ed2642ccf3facc13ab2bbc7f7ae
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-01-08 13:15:16 +0800

    config.h consistency for CRKBD

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -28,15 +28,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define USE_SERIAL_PD2
-
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+//#define USE_SERIAL_PD2
+//#define TAPPING_FORCE_HOLD
+//#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
     #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
