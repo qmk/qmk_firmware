59c4ed5343bd3f1d9b9fa1b7f672b9b6a8817e97
Brandon Claveria <48102030+swiftrax@users.noreply.github.com>
2022-01-08 15:31:23 -0800

    Update keyboards/handwired/swiftrax/bumblebee/keymaps/via/keymap.c
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/handwired/swiftrax/bumblebee/keymaps/via/keymap.c
+++ keyboards/handwired/swiftrax/bumblebee/keymaps/via/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F14 , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , KC_END ,      
         KC_F15 , KC_LCTL,                   KC_LALT, KC_SPC , KC_MUTE,                   MO(1)  , MO(2)  ,          KC_LEFT, KC_DOWN, KC_RGHT       
     ),
-    [1] = LAYOUT_default(
+    [1] = LAYOUT_all(
         _______, _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , _______, _______, 
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      
