8a916d696abf33f562d74081f731fd148e67c6d6
PollyV1 <70621744+PollyV1@users.noreply.github.com>
2022-01-08 13:52:06 +0800

    Update keyboards/carpolly/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/carpolly/keymaps/default/keymap.c
+++ keyboards/carpolly/keymaps/default/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  
         KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,  
         KC_LSFT, KC_MPLY, KC_VOLD, KC_VOLU, _______, _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, 
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_HOME, KC_PGDN, KC_PGUP  
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_HOME, KC_PGDN, KC_PGUP  
     ),
 
     [2] = LAYOUT(
