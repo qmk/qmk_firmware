43ee120390f75e8e26ddb51a97b90c1809835a12
jonavin <71780717+Jonavin@users.noreply.github.com>
2022-01-08 04:43:08 -0500

    Update keyboards/gmmk/pro/iso/keymaps/jonavin/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/gmmk/pro/iso/keymaps/jonavin/config.h
+++ keyboards/gmmk/pro/iso/keymaps/jonavin/config.h
@@ -28,12 +28,12 @@
 #define TAPPING_TERM 180
 
 #ifdef RGB_MATRIX_ENABLE
-    #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
-    #define RGB_DISABLE_WHEN_USB_SUSPENDED
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED
 #endif
 
 // add fifth layer for colemak  -- set "COLEMAK_LAYER_ENABLE = yes" in rules.mk to enable
 #if defined COLEMAK_LAYER_ENABLE
-    #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-    #define _COLEMAK 4
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#    define _COLEMAK 4
 #endif // COLEMAK_LAYER_ENABLE
