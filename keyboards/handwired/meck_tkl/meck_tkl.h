4b8812358cd9dcdca16e45c93d7188fb980e1b87
Gabriel Kim <gabrielkim13@gmail.com>
2022-01-09 00:24:52 -0300

    Improve layout macro naming

--- keyboards/handwired/meck_tkl/meck_tkl.h
+++ keyboards/handwired/meck_tkl/meck_tkl.h
@@ -1,4 +1,4 @@
-// Copyright 2021 Gabriel Kim (@gabrielkimD)
+// Copyright 2021 Gabriel Kim (@gabrielkim13)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
@@ -21,7 +21,7 @@
  * |   Shift   |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |    Shift     |        |  ^  |
  * | Ctrl | Win  | alt  |               Space                 | alt  |  fn  | Menu | Ctrl |  |  <  |  v  |  >  |
  */
-#define LAYOUT( \
+#define LAYOUT_tkl_ansi( \
      k00 ,      k01 , k02 , k03 , k04 ,   k05 , k06 , k07 , k08 ,   k09 , k0A , k0B , k0C ,    k0D , k0E , k0F , \
      k10 , k11 , k12 , k13 , k14 , k15 , k16 , k17 , k18 , k19 , k1A , k1B , k1C ,  k1D   ,    k1E , k1F , k1G , \
       k20  , k21 , k22 , k23 , k24 , k25 , k26 , k27 , k28 , k29 , k2A , k2B , k2C , k2D  ,    k2E , k2F , k2G , \
