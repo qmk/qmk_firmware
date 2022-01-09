4b0aa1a2af5934aad40c21ceb9f83cf6b5034d5b
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-01-08 16:12:37 -0800

    kbdfans/kbd67/mkiirgb_iso: LAYOUT_65_iso_blocker conformance (#15794)

--- keyboards/kbdfans/kbd67/mkiirgb_iso/rules.mk
+++ keyboards/kbdfans/kbd67/mkiirgb_iso/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = lufa-ms
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -18,3 +18,5 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
+
+LAYOUTS = 65_iso_blocker
