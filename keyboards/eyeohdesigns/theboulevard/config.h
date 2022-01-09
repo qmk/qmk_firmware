068d6bf43808aa04a298e19298219af80111510d
joedinkle <brandon.bonton@gmail.com>
2022-01-08 20:22:15 -0800

    Update keyboards/eyeohdesigns/theboulevard/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/eyeohdesigns/theboulevard/config.h
+++ keyboards/eyeohdesigns/theboulevard/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER 0x0001
 #define MANUFACTURER eye oh designs
 #define PRODUCT theboulevard
-#define DESCRIPTION battle station style keeb
 
 /* key matrix size */
 #define MATRIX_ROWS 5
