e6c768b13e332f9620bb1cdf7150dd022331667a
takishim <tadashi@kishimo.to>
2022-01-09 15:16:28 +0900

    Fixed Vendor ID and added an explanation of how to enter the boot loader.

--- keyboards/mikeneko65/rev4/config.h
+++ keyboards/mikeneko65/rev4/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x1028
+#define VENDOR_ID    0x7934
 #define PRODUCT_ID   0x6D54
 #define DEVICE_VER   0x0001
 #define MANUFACTURER takishim
