fb7356c93bde3e87c8e459d6457598a6c129c1dd
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-01-09 09:19:52 +0800

    Keep both rules.mk file consistent

--- keyboards/crkbd/keymaps/default/rules.mk
+++ keyboards/crkbd/keymaps/default/rules.mk
@@ -1,3 +1,5 @@
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306
-LTO_ENABLE  = yes
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
