5f588f52cbd49ad4f040f55342c0b6478917b7a7
moyi4681 <moyi4681@users.noreply.github.com>
2022-01-10 10:04:45 +0800

    Update keyboards/kbdfans/kbd75rgb/rules.mk
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/kbdfans/kbd75rgb/rules.mk
+++ keyboards/kbdfans/kbd75rgb/rules.mk
@@ -7,15 +7,15 @@ BOOTLOADER = lufa-ms
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no          # Mouse keys
-EXTRAKEY_ENABLE = yes          # Audio control and System control
-CONSOLE_ENABLE = no            # Console for debug
-COMMAND_ENABLE = no            # Commands for debug and configuration
-NKRO_ENABLE = yes              # USB Nkey Rollover
-BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
-AUDIO_ENABLE = no              # Audio output on port C6
-RGB_MATRIX_ENABLE = yes        # Use RGB matrix
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = yes     # Use RGB matrix
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
 
