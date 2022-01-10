5e305edfc900331c05f90d8f2bf00f281cea7fa7
TerryMathews <terry@terrymathews.net>
2022-01-10 12:25:28 -0500

    Apply suggestions from code review
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/tkc/candybar/lefty_r3/rules.mk
+++ keyboards/tkc/candybar/lefty_r3/rules.mk
@@ -5,17 +5,15 @@ MCU = atmega32u4
 BOOTLOADER = atmel-dfu
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-# EXTRAFLAGS+=-flto
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
 LTO_ENABLE = yes
-BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = yes  # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no # Mouse keys
-EXTRAKEY_ENABLE = yes # Audio control and System control
-CONSOLE_ENABLE = no  # Console for debug
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes     # USB Nkey Rollover
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = no
--- keyboards/tkc/candybar/righty_r3/keymaps/via/keymap.c
+++ keyboards/tkc/candybar/righty_r3/keymaps/via/keymap.c
@@ -16,10 +16,12 @@
 
 #include QMK_KEYBOARD_H
 
-#define _BL 0
-#define _FL 1
-#define _AL 2
-#define _LL 3
+enum layer_names {
+    _BL, 
+    _FL, 
+    _AL, 
+    _LL, 
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
