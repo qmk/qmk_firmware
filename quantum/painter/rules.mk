250eed4de260e546129f1921cef8344d9f922457
Nick Brassel <nick@tzarc.org>
2022-01-11 04:17:33 +1100

    Add support for GC9A01.

--- quantum/painter/rules.mk
+++ quantum/painter/rules.mk
@@ -2,7 +2,7 @@
 QUANTUM_PAINTER_DRIVERS ?=
 
 # The list of permissible drivers that can be listed in QUANTUM_PAINTER_DRIVERS
-VALID_QUANTUM_PAINTER_DRIVERS := st7789_spi
+VALID_QUANTUM_PAINTER_DRIVERS := st7789_spi gc9a01_spi
 
 #-------------------------------------------------------------------------------
 
@@ -35,6 +35,17 @@ define handle_quantum_painter_driver
         SRC += \
             $(DRIVER_PATH)/painter/tft_panel/qp_tft_panel.c \
             $(DRIVER_PATH)/painter/st77xx/qp_st7789.c
+
+    else ifeq ($$(strip $$(CURRENT_PAINTER_DRIVER)),gc9a01_spi)
+        QUANTUM_PAINTER_NEEDS_COMMS_SPI := yes
+        QUANTUM_PAINTER_NEEDS_COMMS_SPI_DC_RESET := yes
+        OPT_DEFS += -DQUANTUM_PAINTER_GC9A01_ENABLE -DQUANTUM_PAINTER_GC9A01_SPI_ENABLE
+        COMMON_VPATH += \
+            $(DRIVER_PATH)/painter/tft_panel \
+            $(DRIVER_PATH)/painter/gc9a01
+        SRC += \
+            $(DRIVER_PATH)/painter/tft_panel/qp_tft_panel.c \
+            $(DRIVER_PATH)/painter/gc9a01/qp_gc9a01.c
 
 
     endif
