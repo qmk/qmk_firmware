a6018e35c2f62f7f85c1f0b01179dc2cb822c262
yiancar <yiangosyiangou@cytanet.com.cy>
2022-01-10 13:20:45 +0000

    Update keyboards/nebula68b/nebula68b.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/nebula68b/nebula68b.h
+++ keyboards/nebula68b/nebula68b.h
@@ -21,7 +21,7 @@
 
 // This a shortcut to help you visually see your layout.
 
-#define LAYOUT_68_ansi( \
+#define LAYOUT_68_ansi_split_bs( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K1D, K0D, K0E, K2E, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K2C,      K1E, K3E, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,                \
