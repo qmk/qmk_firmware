64cc6dc9a747aff311ac00fb1ac70231ecde04cb
r4zr1 <ska4ok@gmail.com>
2022-01-10 05:58:27 +0300

    change a little

--- keyboards/yd60mq/rules.mk
+++ keyboards/yd60mq/rules.mk
@@ -13,10 +13,10 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
 LAYOUTS = 60_ansi 60_ansi_split_bs_rshift 60_ansi_tsangan 60_hhkb 60_iso 60_iso_split_bs_rshift 60_iso_tsangan
 
-DEFAULT_FOLDER = yd60mq/12led
+DEFAULT_FOLDER = yd60mq/16led
