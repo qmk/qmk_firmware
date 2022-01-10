525a595e5d26ec2ae0db9131ec3479064dd58c58
Alin Marin Elena <alin@elena.space>
2022-01-10 21:15:01 +0000

    Update keyboards/mlego/m65/m65.h
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/mlego/m65/m65.h
+++ keyboards/mlego/m65/m65.h
@@ -33,8 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { K400, K401, K402, K403, K404, K405, K406, K407, K408, K409, K410, K411, K412 }  \
 }
 
-enum layer_names { _QW = 0, _LWR, _RSE, _ADJ };
-
 enum unicode_names {
     la = 0,
     lA,
