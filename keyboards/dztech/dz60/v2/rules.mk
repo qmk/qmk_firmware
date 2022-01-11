7a753386344749546072b5555c036a6be290c09a
moyi4681 <moyi4681@users.noreply.github.com>
2022-01-11 10:29:54 +0800

    Update keyboards/dztech/dz60/v2/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dztech/dz60/v2/rules.mk
+++ keyboards/dztech/dz60/v2/rules.mk
@@ -12,10 +12,7 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 LAYOUTS = 60_ansi 60_ansi_arrow_split_bs_7u_spc 60_ansi_arrow 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_abnt2 60_tsangan_hhkb
