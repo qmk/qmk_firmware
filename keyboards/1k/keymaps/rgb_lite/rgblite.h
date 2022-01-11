72e61f69c44f76d77e8d8b9e2c711d95eec244cf
MakotoKurauchi <pluis@me.com>
2022-01-11 12:18:30 +0900

    add a GPL2+ compatible license

--- keyboards/1k/keymaps/rgb_lite/rgblite.h
+++ keyboards/1k/keymaps/rgb_lite/rgblite.h
@@ -1,3 +1,6 @@
+// Copyright 2022 Makoto Kurauchi (@MakotoKurauchi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 #include "ws2812.h"
--- keyboards/1k/keymaps/tap_dance/keymap.c
+++ keyboards/1k/keymaps/tap_dance/keymap.c
@@ -1,3 +1,6 @@
+// Copyright 2022 Makoto Kurauchi (@MakotoKurauchi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 enum layers {
