fa67c22403f30526abc99fbf2fe60a5ede30f3a6
moyi4681 <moyi4681@users.noreply.github.com>
2022-01-11 10:29:59 +0800

    Update keyboards/dztech/dz60/v2/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/dztech/dz60/v2/rules.mk
+++ keyboards/dztech/dz60/v2/rules.mk
@@ -15,4 +15,5 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+
 LAYOUTS = 60_ansi 60_ansi_arrow_split_bs_7u_spc 60_ansi_arrow 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_abnt2 60_tsangan_hhkb
