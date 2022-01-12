5afb925198daad65a67640096b06566e912691dc
niolang <nlangroudi@gmail.com>
2022-01-13 00:01:56 +0100

    personnal layout for bépo lily58

---
+++ keyboards/lily58/keymaps/niolang/config.h
@@ -0,0 +1,38 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+/* Necessary because of use fo tap dance for ç (leads to un wanted modifications after typing c if not) */
+#define IGNORE_MOD_TAP_INTERRUPT
+
+/* RGB light */
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_HUE_STEP 5 // number of steps to cycle through the hue by
+#    define RGBLIGHT_SAT_STEP 30 // number of steps to increment the saturation by
+#    define RGBLIGHT_VAL_STEP 30 // number of steps to increment the brightness by
+#    define RGBLIGHT_SLEEP //  the RGB lighting will be switched off when the host goes to sleep
+#endif
