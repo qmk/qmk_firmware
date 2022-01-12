d901e7d6795886f393d5ab8f610dbe23c25b1b6a
peepeetee <peepeetee@protonmail.com>
2022-01-12 09:27:51 +0800

    update DEFAULT_FOLDER to match

--- keyboards/kakunpc/business_card/rules.mk
+++ keyboards/kakunpc/business_card/rules.mk
@@ -17,4 +17,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-DEFAULT_FOLDER = business_card/beta
+DEFAULT_FOLDER = kakunpc/business_card/beta
