945b215b4663cd2b58ae8f9dfd65b415e66fbc00
Rock <vzox.rock@gmail.com>
2022-01-12 19:27:52 +0800

    Move key_strand to keymap.c #chonkerkeys/262
    They are moved as they are not supposed to be configurable (AFAIK key_strand
    is _just_ key.row * MATRIX_ROW + key.col, assuming top-right is the origin)

--- keyboards/chonkerkeys/max/keymaps/default/keymap.c
+++ keyboards/chonkerkeys/max/keymaps/default/keymap.c
@@ -17,4 +17,11 @@
 #include "virtser.h"
 #include "../protocol.c"
 #include "config.c"
+
+const uint8_t key_strand[MATRIX_ROWS][MATRIX_COLS] = {
+    { 3,  2,  1,  0},
+    { 7,  6,  5,  4},
+    {11, 10,  9,  8}
+};
+
 #include "../core.c"
