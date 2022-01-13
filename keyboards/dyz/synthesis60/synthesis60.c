3573eb557d6a480eaf2de8ea9e4c890332bba2ef
Bahrul Hidayat <dayat.py@gmail.com>
2022-01-13 11:36:58 +0800

    Update keyboards/dyz/synthesis60/synthesis60.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/dyz/synthesis60/synthesis60.c
+++ keyboards/dyz/synthesis60/synthesis60.c
@@ -14,3 +14,18 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "synthesis60.h"
+
+#ifdef OLED_ENABLE
+bool oled_task_kb(void) {
+    if (!oled_task_kb()) { return false; }
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Synthesis60\nLayer: "), false);
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return true;
+}
+#endif
