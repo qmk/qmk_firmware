face718c837d8d0b2b72d8f9862ada99af912e21
Eric Gebhart <e.a.gebhart@gmail.com>
2022-01-13 10:51:26 -0500

    Update keyboards/montsinger/rebound/rev4/keymaps/ericgebhart/rules.mk
    Co-authored-by: Dasky <32983009+daskygit@users.noreply.github.com>

--- keyboards/montsinger/rebound/rev4/keymaps/ericgebhart/rules.mk
+++ keyboards/montsinger/rebound/rev4/keymaps/ericgebhart/rules.mk
@@ -1,32 +1,18 @@
 # Build Options
-<<<<<<< HEAD
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-=======
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
->>>>>>> c84fcae7e7 (refactor)
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
-<<<<<<< HEAD
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = no         # Unicode
-=======
-NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
->>>>>>> c84fcae7e7 (refactor)
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 TAP_DANCE_ENABLE = yes      # Enable the tap dance feature.
