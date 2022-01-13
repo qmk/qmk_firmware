2fe6f7dec1f54c4086dd52c16c9ad932c8cc3371
Charly Delay <0xcharly@users.noreply.github.com>
2022-01-13 01:11:21 +0000

    bastardkb/charybdis/3x5/via: replace `KC_SLCN` by `KC_QUOT` on the base layer (to better match the default Miryoku layout)

--- keyboards/bastardkb/charybdis/3x5/keymaps/via/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/via/keymap.c
@@ -56,7 +56,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, \
+       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
                       ESC_MED, SPC_NAV, TAB_FUN, ENT_SYM, BSP_NUM
 
