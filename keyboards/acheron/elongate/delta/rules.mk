89e9ab95d370a68506649cd160bd5ad00e37044f
Álvaro A. Volpato <alvaro.volpato@usp.br>
2022-01-12 22:29:00 -0300

    Remove MIDI and BL from rules.mk
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/acheron/elongate/delta/rules.mk
+++ keyboards/acheron/elongate/delta/rules.mk
@@ -18,8 +18,6 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output on port C6
+AUDIO_ENABLE = no           # Audio output
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
