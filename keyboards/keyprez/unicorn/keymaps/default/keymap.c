6e137e153689d73e07f21ce8ba9a7af3a4291726
csandven <chs_91@hotmail.com>
2022-01-14 21:01:41 +0100

    Setup rotary encoder function

--- keyboards/keyprez/unicorn/keymaps/default/keymap.c
+++ keyboards/keyprez/unicorn/keymaps/default/keymap.c
@@ -6,8 +6,11 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    _FN,
+    _CMD
 };
+
+#define CMD   MO(_CMD)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ISO(
@@ -15,6 +18,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_END, KC_DEL, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_1,    KC_ENT, KC_PGUP, KC_INSERT, \
         KC_LSFT, KC_9,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, KC_2, \
-        KC_LCTL, KC_LALT, KC_LGUI, KC_8,    KC_SPC,                    KC_SPC,  KC_RALT, KC_3,    KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_4
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_8,                               KC_SPC,     KC_RALT, KC_3,    KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_4
+    ),
+
+    /* CMD
+     * ,-----------------------------------------------------------------------------------.
+     * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   1  |   2  |   3  |  -   |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   4  |   5  |   6  |  +   |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |   7  |   8  |   9  |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |             |      |      |   0  |   .  |      |
+     * `-----------------------------------------------------------------------------------'
+    */
+    [_CMD] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, _______,
+        _______
     ),
 };
