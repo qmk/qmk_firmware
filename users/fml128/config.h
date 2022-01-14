597571ea8c6c7932f51a49d68ec99724b949eb1c
Merlin Jehli <merlin@jeh.li>
2022-01-14 12:15:56 +0100

    removed usb ids

--- users/fml128/config.h
+++ users/fml128/config.h
@@ -1,14 +1,7 @@
-#undef VENDOR_ID
-#undef PRODUCT_ID
-#undef DEVICE_VER
-
 #undef MANUFACTURER
 #undef PRODUCT
 #undef DESCRIPTION
 
-#define VENDOR_ID           0x0128
-#define PRODUCT_ID          0x0128
-#define DEVICE_VER          0x0128
 
 #define MANUFACTURER        "FML128.ch"
 #define PRODUCT             "FMK128"
