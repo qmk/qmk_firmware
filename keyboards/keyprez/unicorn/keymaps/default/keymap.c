e50d7f0eb16d7d88b60e398af844c80206f17042
csandven <chs_91@hotmail.com>
2022-01-15 21:32:14 +0100

    Add support for rotary encoder

--- keyboards/keyprez/unicorn/keymaps/default/keymap.c
+++ keyboards/keyprez/unicorn/keymaps/default/keymap.c
@@ -10,33 +10,32 @@ enum layer_names {
     _CMD
 };
 
+#define FN   MO(_FN)
 #define CMD   MO(_CMD)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ISO(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, KC_MUTE, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_END, KC_DEL, \
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_1,    KC_ENT, KC_PGUP, KC_INSERT, \
-        KC_LSFT, KC_9,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, KC_2, \
-        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_8,                               KC_SPC,     KC_RALT, KC_3,    KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_4
+        KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, KC_MUTE,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_END, KC_DEL,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, KC_PGUP, KC_INSERT,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, KC_COPY,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,                             KC_SPC,  KC_RALT, FN,      KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PASTE
     ),
 
-    /* CMD
-     * ,-----------------------------------------------------------------------------------.
-     * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   1  |   2  |   3  |  -   |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   4  |   5  |   6  |  +   |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |   7  |   8  |   9  |      |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |             |      |      |   0  |   .  |      |
-     * `-----------------------------------------------------------------------------------'
-    */
-    [_CMD] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, _______,
-        _______
+    [_FN] = LAYOUT_ISO(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    return false;
+}
--- keyboards/keyprez/unicorn/unicorn.c
+++ keyboards/keyprez/unicorn/unicorn.c
@@ -2,9 +2,3 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "unicorn.h"
-
-bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) { return false; }
-    tap_code(clockwise ? KC_VOLU : KC_VOLD);
-    return true;
-}
