1eb769b576b669467a0c38eff43f8e869ff8d6e4
holtenc <holtenc@primekb.com>
2022-01-14 22:23:26 -0600

    Initial commit for obro

---
+++ keyboards/handwired/obro/obro.c
@@ -0,0 +1,43 @@
+/*
+Copyright 2020 Holten Campbell
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "obro.h"
+
+/*
+led_config_t g_led_config = { {
+  // Key Matrix to LED Index
+  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 },
+  { 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14 },
+  { 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 },
+  { 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41},
+  { 55, 56, 57, 58, 59, 60, 61, 62, 63 }
+}, {
+  // LED Index to Physical Position
+  { 0, 0 }, { 17, 0 }, { 34, 0 }, { 51, 0 }, { 68, 0 }, { 85, 0 }, { 102, 0 }, { 119, 0 }, { 136, 0 }, { 153, 0 }, { 170, 0 }, { 187, 0 }, { 204, 0 }, { 221, 0 },
+  { 0, 16 }, { 17, 16 }, { 34, 16 }, { 51, 16 }, { 68, 16 }, { 85, 16 }, { 102, 16 }, { 119, 16 }, { 136, 16 }, { 153, 16 }, { 170, 16 }, { 187, 16 }, { 204, 16 }, { 221, 16 },
+  { 0, 32 }, { 17, 32 }, { 34, 32 }, { 51, 32 }, { 68, 32 }, { 85, 32 }, { 102, 32 }, { 119, 32 }, { 136, 32 }, { 153, 32 }, { 170, 32 }, { 187, 32 }, { 221, 32 },
+  { 0, 48 }, { 17, 48 }, { 34, 48 }, { 51, 48 }, { 68, 48 }, { 85, 48 }, { 102, 48 }, { 119, 48 }, { 136, 48 }, { 153, 48 }, { 170, 48 }, { 187, 48 }, { 204, 48 }, { 221, 48 },
+  { 0, 64 }, { 17, 64}, { 51, 64 }, { 85, 64 }, { 119, 64 }, { 153, 64 }, { 187, 64 }, { 204, 64 }, { 221, 64}
+}, {
+  // LED Index to Flag
+  1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+  1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+  1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1
+} };
+*/
