c72ed7c02473dec4da6cb263c1e0fb2ca4856b94
Ryan <fauxpark@gmail.com>
2022-01-17 08:44:34 +1100

    CLI: Parse USB device version BCD (#14580)
    * CLI: Parse USB device version BCD
    
    * Apply suggestions

--- keyboards/lets_split_eh/config.h
+++ keyboards/lets_split_eh/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0xE401
-#define DEVICE_VER      0x0E41
+#define DEVICE_VER      0x0100
 #define MANUFACTURER    That-Canadian
 #define PRODUCT         Lets Split Eh?
 
