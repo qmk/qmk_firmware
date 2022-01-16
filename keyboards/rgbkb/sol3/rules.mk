268d62ef78d55d018b6c9ebbcd86a4257cbde786
Franklyn Tackitt <franklyn@tackitt.net>
2022-01-16 09:23:46 -0700

    Enable bootmagic lite, disable audio clicky on eeprom init

--- keyboards/rgbkb/sol3/rules.mk
+++ keyboards/rgbkb/sol3/rules.mk
@@ -12,7 +12,7 @@ QUANTUM_LIB_SRC += i2c_master.c
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
--- keyboards/rgbkb/sol3/sol3.c
+++ keyboards/rgbkb/sol3/sol3.c
@@ -8,13 +8,28 @@
  */
 
 #include "sol3.h"
+#include "eeconfig.h"
+#include "audio.h"
 #include <transactions.h>
+
+extern audio_config_t audio_config;
 
 void keyboard_post_init_kb(void) {
     touch_encoder_init();
     transaction_register_rpc(TOUCH_ENCODER_SYNC, touch_encoder_slave_sync);
     transaction_register_rpc(RGB_MENU_SYNC, rgb_menu_slave_sync);
     keyboard_post_init_user();
+}
+
+void eeconfig_init_kb(void) {
+    // Reset Keyboard EEPROM value to blank, rather than to a set value
+    eeconfig_update_kb(0);
+
+    audio_config.raw = eeconfig_read_audio();
+    audio_config.clicky_enable = false;
+    eeconfig_update_audio(audio_config.raw);
+
+    eeconfig_init_user();
 }
 
 void housekeeping_task_kb(void) {
