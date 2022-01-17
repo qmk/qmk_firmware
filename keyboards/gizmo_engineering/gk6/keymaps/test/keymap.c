f660c1da32ecf877863b66e282995006512876cb
federicoweber <federicoweber@gmail.com>
2022-01-16 17:58:38 -0800

    Update keyboards/gizmo_engineering/gk6/keymaps/test/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/gizmo_engineering/gk6/keymaps/test/keymap.c
+++ keyboards/gizmo_engineering/gk6/keymaps/test/keymap.c
@@ -13,7 +13,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include QMK_KEYBOARD_H
+
 enum layers {
   _QWERTY,
   _LOWER,
