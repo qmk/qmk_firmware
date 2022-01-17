7ed0cf8091fb85637dae0be3383c7759a2ef76bf
Jan Lindblom <jan@namnlos.io>
2022-01-17 15:40:43 +0100

    Updated according to PR checklist.

--- keyboards/b_sides/rev41lp/keymaps/namnlos/keymap.c
+++ keyboards/b_sides/rev41lp/keymaps/namnlos/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 cyril279
+/* Copyright 2022 Jan Lindblom (@janlindblom)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include QMK_KEYBOARD_H
 #include "keymap_swedish.h"
 #include "sendstring_swedish.h"
--- keyboards/b_sides/rev41lp/rev41lp.h
+++ keyboards/b_sides/rev41lp/rev41lp.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 cyril279
+/* Copyright 2021 @cyril279
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,14 +17,6 @@
 
 #include "quantum.h"
 
-/* This a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
 #define LAYOUT_rev41lp( \
     K00, K01, K02, K03, K04, K05,      K10, K11, K12, K13, K14, K15, \
     K20, K21, K22, K23, K24, K25,      K30, K31, K32, K33, K34, K35, \
