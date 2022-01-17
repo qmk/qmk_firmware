7ab1cc98f92f55e94f621c3161ffca1e7932514b
ChrisGVE <christian.berclaz@mac.com>
2022-01-17 23:09:41 +0800

    Wind.X: added arrow keys to control the mouse.

--- keyboards/winx/keymaps/chrisgve/keymap.c
+++ keyboards/winx/keymaps/chrisgve/keymap.c
@@ -112,16 +112,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,  _______, _______, _______, _______,
 		_______, _______, KC_ACL2, KC_ACL1, KC_ACL0, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,          _______,           _______,  _______, _______, _______, _______,
-		_______, _______, _______, _______, _______, _______, KC_WH_U, KC_WH_D, _______, _______, _______, _______,          _______,                     _______, _______, _______, _______,
-		_______, _______, _______,                   KC_BTN1,                            _______,                   _______, _______, _______,            _______,          _______ 
+		_______, _______, _______, _______, _______, _______, KC_WH_U, KC_WH_D, _______, _______, _______, _______,          KC_MS_U,                     _______, _______, _______, _______,
+		_______, _______, _______,                   KC_BTN1,                            _______,                   KC_MS_L, KC_MS_D, KC_MS_R,            _______,          _______ 
     ),
     
 	[_EX_MOUSE] = LAYOUT_tsangan(
 		TG_MSE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		XXXXXXX, XXXXXXX, KC_ACL2, KC_ACL1, KC_ACL0, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,          XXXXXXX,           XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		TG_MSE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, KC_WH_D, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-		XXXXXXX, XXXXXXX, XXXXXXX,                   KC_BTN1,                            XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX,          XXXXXXX 
+		TG_MSE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, KC_WH_D, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_MS_U,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+		XXXXXXX, XXXXXXX, XXXXXXX,                   KC_BTN1,                            XXXXXXX,                   KC_MS_L, KC_MS_D, KC_MS_R,            XXXXXXX,          XXXXXXX 
     ),
     
 };
