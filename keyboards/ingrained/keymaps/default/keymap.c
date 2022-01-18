18ab2c971fbb4b6fc279952ba9a6db0a37575470
Juan Puerto <jpuerto@psc.edu>
2022-01-18 13:36:09 -0500

    clean up keymaps again

--- keyboards/ingrained/keymaps/default/keymap.c
+++ keyboards/ingrained/keymaps/default/keymap.c
@@ -20,5 +20,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                                KC_LGUI, KC_SPC,  KC_TAB,        KC_BSPC, KC_ENT,  KC_RALT
   ),
-  )
 };
