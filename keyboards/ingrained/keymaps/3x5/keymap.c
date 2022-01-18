5c4619ac7849cf7208bad0e65e29541953cca191
Juan Puerto <jpuerto@psc.edu>
2022-01-18 12:05:53 -0500

    clean up keymaps

--- keyboards/ingrained/keymaps/3x5/keymap.c
+++ keyboards/ingrained/keymaps/3x5/keymap.c
@@ -15,9 +15,10 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_3x5(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_A, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
-    KC_Z, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,
-                                    KC_LGUI, KC_SPC, KC_TAB, KC_BSPC, KC_ENT, KC_RALT
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,
+                      KC_LGUI, KC_SPC,  KC_TAB,        KC_BSPC, KC_ENT,  KC_RALT
+  ),
   )
 };
