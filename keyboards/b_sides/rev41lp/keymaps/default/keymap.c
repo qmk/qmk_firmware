e9479dd8f6d655c7f16339e171a433e47b8fa8a3
Jan Lindblom <janlindblom@fastmail.fm>
2022-01-19 12:00:56 +0100

    Update keyboards/b_sides/rev41lp/keymaps/default/keymap.c
    Co-authored-by: Albert Y <76888457+filterpaper@users.noreply.github.com>

--- keyboards/b_sides/rev41lp/keymaps/default/keymap.c
+++ keyboards/b_sides/rev41lp/keymaps/default/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______,   _______,  KC_ENT,   _______,  _______
   ),
   
-  [_RAISE] = LAYOUT_rev41lp(
+  [2] = LAYOUT_rev41lp(
     _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
     _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
     _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
