1739ba67e02aa439351ab85cfab7ef03a83fc319
fOmey <pauly.galea@gmail.com>
2022-01-21 07:52:34 +1100

    Whitespace fix

--- keyboards/atlantis/ak81_ve/rules.mk
+++ keyboards/atlantis/ak81_ve/rules.mk
@@ -7,17 +7,17 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # N-Key Rollover
-BACKLIGHT_ENABLE = no       # Keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Keyboard RGB underglow
+BOOTMAGIC_ENABLE = yes		# Bootmagic Lite
+MOUSEKEY_ENABLE = yes		# Mouse keys
+EXTRAKEY_ENABLE = yes		# Audio control and System control
+CONSOLE_ENABLE = no			# Console for debug
+COMMAND_ENABLE = no			# Commands for debug and configuration
+NKRO_ENABLE = yes			# N-Key Rollover
+BACKLIGHT_ENABLE = no		# Keyboard backlight functionality
+RGBLIGHT_ENABLE = no		# Keyboard RGB underglow
 RGB_MATRIX_ENABLE = yes		# RGB matrix effects
 RGB_MATRIX_DRIVER = WS2812	# RGB matrix driver
 LTO_ENABLE = yes			# Link time optimization
-AUDIO_ENABLE = no           # Audio output
+AUDIO_ENABLE = no			# Audio output
 DYNAMIC_MACRO_ENABLE = yes	# Macro recording in realtime
 ENCODER_ENABLE = yes		# Encoder
