fa57d68bd6500f95d69b49c3bc25cdd2f72e7343
zvecr <git@zvecr.com>
2022-01-20 20:44:25 +0000

    Move callback defs

--- quantum/led.h
+++ quantum/led.h
@@ -55,6 +55,12 @@ void led_wakeup(void);
 
 void led_task(void);
 
+/* Callbacks */
+void led_set_user(uint8_t usb_led);
+void led_set_kb(uint8_t usb_led);
+bool led_update_user(led_t led_state);
+bool led_update_kb(led_t led_state);
+
 #ifdef __cplusplus
 }
 #endif
