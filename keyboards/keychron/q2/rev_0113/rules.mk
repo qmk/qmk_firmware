4f3811ffa4cbe9e06ea147beca83f1c265c82fbe
lalalademaxiya1 <2831039915@qq.com>
2022-01-20 15:30:13 +0800

    Add rgb matrix support for q2 of keychron.

--- keyboards/keychron/q2/rev_0113/rules.mk
+++ keyboards/keychron/q2/rev_0113/rules.mk
@@ -23,12 +23,9 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes        # Enable Encoder
 DIP_SWITCH_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = CKLED2001
 EEPROM_DRIVER = i2c
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-# custom matrix setup
-CUSTOM_MATRIX = lite
-
-SRC += matrix.c
