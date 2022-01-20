30f4dc4a29b77cfc333e98df43aaa839a5ef4523
fOmey <pauly.galea@gmail.com>
2022-01-20 17:57:14 +1100

    Move LTO to board rules & add some comments

--- keyboards/atlantis/ak81_ve/rules.mk
+++ keyboards/atlantis/ak81_ve/rules.mk
@@ -7,16 +7,17 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes      # Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+NKRO_ENABLE = yes           # N-Key Rollover
 BACKLIGHT_ENABLE = no       # Keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Keyboard RGB underglow
-RGB_MATRIX_ENABLE = yes		# Enable RGB matrix effects
-RGB_MATRIX_DRIVER = WS2812
+RGB_MATRIX_ENABLE = yes		# RGB matrix effects
+RGB_MATRIX_DRIVER = WS2812	# RGB matrix driver
+LTO_ENABLE = yes			# Link time optimization
 AUDIO_ENABLE = no           # Audio output
-DYNAMIC_MACRO_ENABLE = yes
-ENCODER_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes	# Macro recording in realtime
+ENCODER_ENABLE = yes		# Encoder
