2f8938102d6275c8077c4230cdc99cc4e6b5ebcd
fOmey <pauly.galea@gmail.com>
2022-01-21 09:50:59 +1100

    Whitespace fix 2
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/atlantis/ak81_ve/rules.mk
+++ keyboards/atlantis/ak81_ve/rules.mk
@@ -7,17 +7,17 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes		# Bootmagic Lite
-MOUSEKEY_ENABLE = yes		# Mouse keys
-EXTRAKEY_ENABLE = yes		# Audio control and System control
-CONSOLE_ENABLE = no			# Console for debug
-COMMAND_ENABLE = no			# Commands for debug and configuration
-NKRO_ENABLE = yes			# N-Key Rollover
-BACKLIGHT_ENABLE = no		# Keyboard backlight functionality
-RGBLIGHT_ENABLE = no		# Keyboard RGB underglow
-RGB_MATRIX_ENABLE = yes		# RGB matrix effects
-RGB_MATRIX_DRIVER = WS2812	# RGB matrix driver
-LTO_ENABLE = yes			# Link time optimization
+BOOTMAGIC_ENABLE = yes      # Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Keyboard RGB underglow
 AUDIO_ENABLE = no			# Audio output
-DYNAMIC_MACRO_ENABLE = yes	# Macro recording in realtime
-ENCODER_ENABLE = yes		# Encoder
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+LTO_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+ENCODER_ENABLE = yes
