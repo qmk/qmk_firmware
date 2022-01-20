527fd6bcf72d2625f7e5bcf6c98f3ea18d4962c9
Cole Smith <smithcole@protonmail.com>
2022-01-19 20:34:35 -0800

    added via map

--- keyboards/boardsource/lulu/keymaps/via/keymap.c
+++ keyboards/boardsource/lulu/keymaps/via/keymap.c
@@ -117,33 +117,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-
-
-#ifdef ENCODER_ENABLE
-static uint8_t  encoder_state[2] = {0};
-static keypos_t encoder_ccw[2]   = {{0, 0}, {0, 1}};
-static keypos_t encoder_cw[2]    = {{1, 0}, {1, 1}};
-
-void encoder_action_unregister(void) {
-    for (int index = 0; index < 2; ++index) {
-        if (encoder_state[index]) {
-            keyevent_t encoder_event = (keyevent_t){.key = encoder_state[index] >> 1 ? encoder_cw[index] : encoder_ccw[index], .pressed = false, .time = (timer_read() | 1)};
-            encoder_state[index]     = 0;
-            action_exec(encoder_event);
-        }
-    }
-}
-
-void encoder_action_register(uint8_t index, bool clockwise) {
-    keyevent_t encoder_event = (keyevent_t){.key = clockwise ? encoder_cw[index] : encoder_ccw[index], .pressed = true, .time = (timer_read() | 1)};
-    encoder_state[index]     = (clockwise ^ 1) | (clockwise << 1);
-    action_exec(encoder_event);
-}
-
-void matrix_scan_user(void) { encoder_action_unregister(); }
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    encoder_action_register(index, clockwise);
-    return false;
-};
-#endif
