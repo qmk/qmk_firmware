4ed2fd727d4bf4d1cd78466961b30da4a880ad75
T. Imanishi <task@mxxiv.net>
2022-01-21 22:26:22 +0900

    [Keymap] Fix Corne Cherry default RGBLIGHT setting.

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -35,8 +35,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
+    #undef RGBLED_SPLIT
     #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
+    #define RGBLED_NUM 54
+    #define RGBLED_SPLIT { 27, 27 }
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
