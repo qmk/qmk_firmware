dd78f16b80f8c69025325d7e6da4c441259d1e5f
Gondolindrim <alvaro.volpato@usp.br>
2022-01-21 20:50:03 -0300

    Add M75S initial support

--- keyboards/mode/m75s/config.h
+++ keyboards/mode/m75s/config.h
@@ -19,18 +19,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x00DE
-#define PRODUCT_ID      0x7572 // For 75H
+#define PRODUCT_ID      0x7583 // For 75S
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Mode
-#define PRODUCT         Mode 75H
+#define PRODUCT         Mode 75S
 
 /* key matrix size */
 #define MATRIX_ROWS 7
 #define MATRIX_COLS 16
                         //C0, C1, C2,  C3,  C4,  C5,  C6,  C7, C8,  C9, C10, C11, C12, C13, C14, C15
-#define MATRIX_COL_PINS { C8, C7, A10, B13, B12, B10, B1, C10, C11, D2, C12, B3,  B4,  B5,  B8,  B9 }
+#define MATRIX_COL_PINS { C8, A8, A10, B13, B12, B10, B1, C10, C11, D2, C12, B3,  B4,  B5,  B8,  B9 }
                         //R0, R1, R2,  R3,  R4,  R5 , R6
-#define MATRIX_ROW_PINS { C5, B0, B14, B15, A8 , C9 , A15} 
+#define MATRIX_ROW_PINS { C5, B0, B14, B15, C7 , C9 , A15} 
 #define DIODE_DIRECTION COL2ROW
 
 /* define if matrix has ghost */
