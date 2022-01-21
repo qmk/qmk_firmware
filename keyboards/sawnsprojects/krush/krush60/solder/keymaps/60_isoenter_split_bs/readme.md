75fdb12034c7a2ee9fb1b1c8b0d9a8dfa2a15311
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-01-20 22:35:49 -0800

    rework LAYOUT_60_iso to LAYOUT_60_isoenter_split_bs
    True ISO layouts are not supported on this keyboard because the PCB lacks support for split Left Shift. Renames the `iso` to `isoenter` to specify this.
    
    - denotes use of Split Backspace
    - move Backslash keycode to home row

---
+++ keyboards/sawnsprojects/krush/krush60/solder/keymaps/60_isoenter_split_bs/readme.md
@@ -0,0 +1,1 @@
+# The 60_isoenter_split_bs keymap for krush60
--- keyboards/sawnsprojects/krush/krush60/solder/solder.h
+++ keyboards/sawnsprojects/krush/krush60/solder/solder.h
@@ -87,10 +87,10 @@
     { K90, XXX, K92, XXX, K94, K95, XXX, K97 }, \
 }
 
-#define LAYOUT_60_iso(\
+#define LAYOUT_60_isoenter_split_bs(\
     K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K26, K17, \
-    K20, K30, K21, K31, K22, K32, K23, K33, K24, K34, K25, K35, K36, K46, \
-    K40, K50, K41, K51, K42, K52, K43, K53, K44, K54, K45, K55, K47, \
+    K20, K30, K21, K31, K22, K32, K23, K33, K24, K34, K25, K35, K36, \
+    K40, K50, K41, K51, K42, K52, K43, K53, K44, K54, K45, K55, K46, K47, \
     K60, K70, K61, K71, K62, K72, K63, K73, K64, K74, K65, K66, \
     K80, K90, K81, K92, K94, K85, K95, K97  \
 ) { \
