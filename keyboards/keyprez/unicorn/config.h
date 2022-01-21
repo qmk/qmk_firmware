f98be21305a210d5593d914ee0261584c0a54ac4
csandven <chs_91@hotmail.com>
2022-01-21 21:46:02 +0100

    Setup vendor and product id

--- keyboards/keyprez/unicorn/config.h
+++ keyboards/keyprez/unicorn/config.h
@@ -6,8 +6,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x6b7a
+#define PRODUCT_ID   0x7563
 #define DEVICE_VER   0x0001
 #define MANUFACTURER keyprez
 #define PRODUCT      unicorn
