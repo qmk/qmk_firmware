4f2718c2c6e6f411a816a80c4838373516d5d273
jason <jrucker2004@gmail.com>
2022-01-22 02:07:36 +0000

    adding rm_numpad

---
+++ keyboards/rmkeebs/rm_numpad/keymaps/split_plus/keymap.c
@@ -0,0 +1,39 @@
+/* Copyright 2021 RuckerMachine
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    LAYOUT(
+        KC_MPLY, KC_MPRV, KC_MNXT, KC_MUTE,
+        KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
+        KC_P7,   KC_P8,   KC_P9,   KC_PEQL,   KC_NO,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
+        KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_PENT,
+        KC_NO,   KC_P0,   KC_NO,   KC_PDOT, KC_NO
+    )
+
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+};
