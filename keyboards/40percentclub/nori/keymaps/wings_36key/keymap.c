2b370074fe0556d3dfd2fa48c7aaca7ac441c3e4
wyethGR <48162735+wyethGR@users.noreply.github.com>
2022-01-21 21:36:32 -0800

    updated keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/40percentclub/nori/keymaps/wings_36key/keymap.c
+++ keyboards/40percentclub/nori/keymaps/wings_36key/keymap.c
@@ -4,10 +4,12 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+enum custom_layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
 
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
