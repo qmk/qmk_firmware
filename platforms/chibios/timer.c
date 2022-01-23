a4e5a241afa96aeb60bb923f261da9d46862fe05
Sergey Vlasov <sigprof@gmail.com>
2022-01-23 21:34:27 +0300

    chibios/timer: Move the 16-bit timer handling into a separate function
    Extract the code which effectively makes a 32-bit tick counter from a
    possibly 16-bit ChibiOS system timer into a separate function.  Does
    not really change the behavior of the timer API, but makes the actions
    done in `timer_clear()` and `timer_read32()` more obvious.

--- platforms/chibios/timer.c
+++ platforms/chibios/timer.c
@@ -8,38 +8,44 @@ static uint32_t last_systime = 0;
 static uint32_t overflow     = 0;
 #endif
 
+// Get the current system time in ticks as a 32-bit number.
+// This function must be called from within a system lock zone (so that it can safely use and update the static data).
+static inline uint32_t get_system_time_ticks(void) {
+    uint32_t systime = (uint32_t)chVTGetSystemTimeX();
+
+#if CH_CFG_ST_RESOLUTION < 32
+    // If the real system timer resolution is less than 32 bits, provide the missing bits by checking for the counter
+    // overflow.  For this to work, this function must be called at least once for every overflow of the system timer.
+    // In the 16-bit case, the corresponding times are:
+    //    - CH_CFG_ST_FREQUENCY = 100000, overflow will occur every ~0.65 seconds
+    //    - CH_CFG_ST_FREQUENCY = 10000, overflow will occur every ~6.5 seconds
+    //    - CH_CFG_ST_FREQUENCY = 1000, overflow will occur every ~65 seconds
+    if (systime < last_systime) {
+        overflow += ((uint32_t)1) << CH_CFG_ST_RESOLUTION;
+    }
+    last_systime = systime;
+    systime += overflow;
+#endif
+
+    return systime;
+}
+
 void timer_init(void) { timer_clear(); }
 
 void timer_clear(void) {
-    reset_point = (uint32_t)chVTGetSystemTime();
-#if CH_CFG_ST_RESOLUTION < 32
-    last_systime = reset_point;
-    overflow     = 0;
-#endif
+    chSysLock();
+    reset_point = get_system_time_ticks();
+    chSysUnlock();
 }
 
 uint16_t timer_read(void) { return (uint16_t)timer_read32(); }
 
 uint32_t timer_read32(void) {
-    uint32_t systime = (uint32_t)chVTGetSystemTime();
+    chSysLock();
+    uint32_t systime = get_system_time_ticks() - reset_point;
+    chSysUnlock();
 
-#if CH_CFG_ST_RESOLUTION < 32
-    // If/when we need to support 64-bit chips, this may need to be modified to match the native bit-ness of the MCU.
-    // At this point, the only SysTick resolution allowed other than 32 is 16 bit.
-    // In the 16-bit case, at:
-    //    - CH_CFG_ST_FREQUENCY = 100000, overflow will occur every ~0.65 seconds
-    //    - CH_CFG_ST_FREQUENCY = 10000, overflow will occur every ~6.5 seconds
-    //    - CH_CFG_ST_FREQUENCY = 1000, overflow will occur every ~65 seconds
-    // With this implementation, as long as we ensure a timer read happens at least once during the overflow period, timing should be accurate.
-    if (systime < last_systime) {
-        overflow += ((uint32_t)1) << CH_CFG_ST_RESOLUTION;
-    }
-
-    last_systime = systime;
-    return (uint32_t)TIME_I2MS(systime - reset_point + overflow);
-#else
-    return (uint32_t)TIME_I2MS(systime - reset_point);
-#endif
+    return (uint32_t)TIME_I2MS(systime);
 }
 
 uint16_t timer_elapsed(uint16_t last) { return TIMER_DIFF_16(timer_read(), last); }
