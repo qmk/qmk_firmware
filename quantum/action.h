b290cd6ce686bc6ff75f4776db9abe8357224af0
zvecr <git@zvecr.com>
2022-01-24 22:18:21 +0000

    Fix up issue with PROGMEM and hand_swap_config

--- quantum/action.h
+++ quantum/action.h
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "progmem.h"
 #include "keyboard.h"
 #include "keycode.h"
 #include "action_code.h"
