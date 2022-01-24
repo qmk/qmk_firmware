ccbeab77b48caeb3ee6fe7a2183e7c0922c9abd5
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-01-24 01:44:52 -0800

    rename LAYOUT_60_ansi_split_bs_7u_spc to LAYOUT_60_ansi_tsangan_split_bs
    - renames `60_ansi_split_bs_7u_spc` keymap to `60_ansi_tsangan_split_bs`
    - removes `layout_aliases` entry from `info.json` (creates incompatible data conflict)

--- keyboards/sawnsprojects/krush/krush60/solder/keymaps/60_ansi_tsangan_split_bs/readme.md
+++ keyboards/sawnsprojects/krush/krush60/solder/keymaps/60_ansi_tsangan_split_bs/readme.md
@@ -1,4 +1,4 @@
-# The 60_ansi_split_bs_7u_spc keymap for krush60 solder
+# The 60_ansi_tsangan_split_bs keymap for krush60 solder
 
 * 60% ANSI layout
   * Tsangan bottom row
--- keyboards/sawnsprojects/krush/krush60/solder/solder.h
+++ keyboards/sawnsprojects/krush/krush60/solder/solder.h
@@ -182,7 +182,7 @@
     { K90, XXX, K92, XXX, XXX, K95, XXX, K97 }, \
 }
 
-#define LAYOUT_60_ansi_split_bs_7u_spc( \
+#define LAYOUT_60_ansi_tsangan_split_bs( \
     K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K26, K17, \
     K20, K30, K21, K31, K22, K32, K23, K33, K24, K34, K25, K35, K36, K46, \
     K40, K50, K41, K51, K42, K52, K43, K53, K44, K54, K45, K55,      K47, \
