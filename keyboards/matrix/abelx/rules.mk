4f6c0d0ea5eb922d98413f30c1710b7800aa2bd3
Ryan <fauxpark@gmail.com>
2022-01-26 07:21:08 +1100

    More keyboard rules.mk cleanups (#16044)
    * More keyboard rules.mk cleanups
    
    * Found a couple more

--- keyboards/matrix/abelx/rules.mk
+++ keyboards/matrix/abelx/rules.mk
@@ -28,7 +28,7 @@ USE_FPU = yes
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -36,7 +36,6 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 NO_USB_STARTUP_CHECK = yes  # Disable initialization only when usb is plugged in
 
