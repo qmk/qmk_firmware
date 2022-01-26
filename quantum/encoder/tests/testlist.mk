a06424198799b156c451f2e2e5d423675e6cb555
Nick Brassel <nick@tzarc.org>
2022-01-27 04:20:33 +1100

    Enable encoder test suite, fix encoder test suite, enable asymmetric encoder support.

--- quantum/encoder/tests/testlist.mk
+++ quantum/encoder/tests/testlist.mk
@@ -1,3 +1,5 @@
 TEST_LIST += \
 	encoder \
-	encoder_split
+	encoder_split_left_eq_right \
+	encoder_split_left_gt_right \
+	encoder_split_left_lt_right
--- quantum/keymap.h
+++ quantum/keymap.h
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "host.h"
 // #include "print.h"
 #include "debug.h"
+#include "util.h"
 #include "keycode_config.h"
 
 // ChibiOS uses RESET in its FlagStatus enumeration
@@ -49,3 +50,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 uint16_t keymap_key_to_keycode(uint8_t layer, keypos_t key);
 
 extern const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS];
+
+#if defined(ENCODER_ENABLE)
+#    ifdef SPLIT_KEYBOARD
+#        if defined(ENCODERS_PAD_A_RIGHT)
+#            define NUM_ENCODERS_LEFT (sizeof(((pin_t[])ENCODERS_PAD_A)) / sizeof(pin_t))
+#            define NUM_ENCODERS_RIGHT (sizeof(((pin_t[])ENCODERS_PAD_A_RIGHT)) / sizeof(pin_t))
+#        else
+#            define NUM_ENCODERS_LEFT (sizeof(((pin_t[])ENCODERS_PAD_A)) / sizeof(pin_t))
+#            define NUM_ENCODERS_RIGHT NUM_ENCODERS_LEFT
+#        endif
+#        define NUM_ENCODERS (NUM_ENCODERS_LEFT + NUM_ENCODERS_RIGHT)
+#    else  // SPLIT_KEYBOARD
+#        define NUM_ENCODERS (sizeof(((pin_t[])ENCODERS_PAD_A)) / sizeof(pin_t))
+#        define NUM_ENCODERS_LEFT NUM_ENCODERS
+#        define NUM_ENCODERS_RIGHT 0
+#    endif  // SPLIT_KEYBOARD
+#    define NUM_ENCODERS_MAX_PER_SIDE MAX(NUM_ENCODERS_LEFT, NUM_ENCODERS_RIGHT)
+#else  // defined(ENCODER_ENABLE)
+#    define NUM_ENCODERS 0
+#    define NUM_ENCODERS_MAX_PER_SIDE NUM_ENCODERS
+#endif  // defined(ENCODER_ENABLE)
