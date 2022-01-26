3d4ea0735473f42c56aea2c6778754d46c8745f5
Álvaro A. Volpato <alvaro.volpato@usp.br>
2022-01-26 16:05:06 -0300

    Add BOOTLOADER and remove BOOTLOADER address from rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mode/m75h/rules.mk
+++ keyboards/mode/m75h/rules.mk
@@ -1,6 +1,9 @@
 # MCU name
 MCU = STM32F401
 
+# Bootloader selection
+BOOTLOADER = stm32-dfu
+
 # Build Options
 #   change yes to no to disable
 #
@@ -15,6 +18,6 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 EEPROM_DRIVER = i2c
 LTO_ENABLE = yes
+
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE -DDEBUG_EEPROM_OUTPUT=TRUE
-STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
