5600cf68623cedf25df57c836f3be20c7dfc7b49
Taeyoon Kim <partrita@users.noreply.github.com>
2022-01-26 13:29:18 +0900

    fix typo

---
+++ keyboards/kprepublic/bm40hsrgb/keymaps/34keys/rules.mk
@@ -0,0 +1,4 @@
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes # Mouse keys
+COMBO_ENABLE = yes  #  The firmware is too large!
+EXTRAKEY_ENABLE = yes # Audio control and System control
