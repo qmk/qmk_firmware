c67e39a482e57b217ee1339e8efdab698e6b31c9
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-01-26 10:56:31 -0800

    fix syntax errors in keymaps, take 2

--- keyboards/sawnsprojects/krush/krush60/solder/keymaps/60_ansi_arrow_split_spc/keymap.c
+++ keyboards/sawnsprojects/krush/krush60/solder/keymaps/60_ansi_arrow_split_spc/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, KC_UP,   KC_SLSH,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  KC_SPC,  KC_SPC,                    MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  KC_SPC,  KC_SPC,           KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT_60_ansi_arrow_split_spc(
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  KC_HOME, KC_PGUP,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, _______
+        _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, _______, _______
     ),
 
 };
