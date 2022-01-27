2fa64b36370aa9783f1ff8defeed3468843f8c65
Christian Sandven <chs_91@hotmail.com>
2022-01-27 20:53:00 +0100

    Update keyboards/keyprez/unicorn/keymaps/jorge/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/keyprez/unicorn/keymaps/jorge/keymap.c
+++ keyboards/keyprez/unicorn/keymaps/jorge/keymap.c
@@ -14,7 +14,7 @@ enum layer_names {
 #define CMD   MO(_CMD)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_ISO(
+    [_BASE] = LAYOUT(
         KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, KC_MUTE,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_END, KC_DEL,
         KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, KC_PGUP, KC_INSERT,
