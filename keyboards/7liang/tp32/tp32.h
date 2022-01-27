67adc8604be62cff1e07ccfc4f46ac443624d9c1
七两半 <sky_jun@qq.com>
2022-01-27 19:42:58 +0800

    Update tp32.h

--- keyboards/7liang/tp32/tp32.h
+++ keyboards/7liang/tp32/tp32.h
@@ -1,13 +1,4 @@
-/*
- * @Description    : 
- * @version        : 
- * @Author         : JunLee
- * @Date           : 2022-01-06 14:34:42
- * @LastEditTime   : 2022-01-27 11:33:45
- * @FilePath       : \qmk_firmware\keyboards\7liang\tp32\tp32.h
- */
-// Copyright 2022 Pkerr (@sky_jun@qq.com)
-// SPDX-License-Identifier: GPL-2.0-or-later
+
 
 #pragma once
 
