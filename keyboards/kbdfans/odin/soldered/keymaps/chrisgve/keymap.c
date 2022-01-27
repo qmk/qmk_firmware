1e2081ae3b4e342583a5d24285b6343a0496f28c
ChrisGVE <christian.berclaz@mac.com>
2022-01-27 10:22:59 +0800

    Odin soldered: corrected the position of the right modifiers ALT and GUI

--- keyboards/kbdfans/odin/soldered/keymaps/chrisgve/keymap.c
+++ keyboards/kbdfans/odin/soldered/keymaps/chrisgve/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,          KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
 		CPS_CTL, A_MOUSE, KC_S,    M_NAV_1, M_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		SFT_MSE, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_M,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
-		KC_LCTL, KC_LALT, KC_LGUI,                   SPC_RSE,                   KC_RGUI, _______, KC_RALT, _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
+		KC_LCTL, KC_LALT, KC_LGUI,                   SPC_RSE,                   _______, KC_RGUI, _______, KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
     ),
 
 	[_MAC_NAV_1] =LAYOUT_all(
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,          KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
 		CPS_CTL, A_MOUSE, KC_S,    L_NAV_1, L_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		SFT_MSE, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_L,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
-		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   KC_RALT, _______, KC_RGUI, _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
+		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   _______, KC_RALT, _______, KC_RGUI,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
     ),
 
 	[_LINUX_NAV_1] =LAYOUT_all(
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,          KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
 		CPS_CTL, A_MOUSE, KC_S,    W_NAV_1, W_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		SFT_MSE, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, ADJ_W,   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT, 
-		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   KC_RALT, _______, KC_RGUI, _______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
+		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   _______, KC_RALT, _______, KC_RGUI,          KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,   KC_PDOT, KC_PENT
     ),
 
 	[_WIN_NAV_1] =LAYOUT_all(
