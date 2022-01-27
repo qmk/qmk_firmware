70e059e9b6b14bc78536d382afa227240f66ff4c
Tim Liu <74063834+Timliuzhaolu@users.noreply.github.com>
2022-01-27 20:54:52 +0800

    Add files via upload

---
+++ keyboards/creatkeebs/glacier/config.h
@@ -0,0 +1,70 @@
+/* Copyright 2022 Tim (https://github.com/Timliuzhaolu)
+  * 
+  * This program is free software: you can redistribute it and/or modify 
+  * it under the terms of the GNU General Public License as published by 
+  * the Free Software Foundation, either version 2 of the License, or 
+  * (at your option) any later version. 
+  * 
+  * This program is distributed in the hope that it will be useful, 
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  * GNU General Public License for more details. 
+  * 
+  * You should have received a copy of the GNU General Public License 
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  */ 
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    creatkeebs
+#define PRODUCT         glacier
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 17
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { F0, F1, F4, E6, F5, D0 }
+#define MATRIX_COL_PINS { D4, D6, D7, B4, B5, F6, B0, B6, C6, C7, B1, B2, B3, B7, D3, D2, D1 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* number of backlight levels */
+
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 0
+#endif
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 0
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#endif
---
+++ keyboards/creatkeebs/glacier/readme.md
@@ -0,0 +1,27 @@
+# Glacier
+A 80% keyboard
+
+![glacier](https://cdn.shopify.com/s/files/1/0481/5840/8853/products/4b8d52820aab652152cd749123d3f8b_6a073c5e-336d-47b4-90fc-8b0fe5a790fe_720x.png?v=1641814324)
+
+### Features
+* PCB gasket
+* Nickel-cobalt weight
+
+### Additional Information
+* Keyboard Maintainer: [Tim](https://github.com/Timliuzhaolu/qmk_firmware)
+* Hardware Supported: glacier PCB
+* Hardware Availability: [ESCHIT.com](https://eschit.com/products/gb-thera75)
+
+### Entering Bootloader
+
+Enter the bootloader in 2 ways:
+
+* Physical reset button: Press the `RESET` button on the back of the PCB.
+* Keycode in layout: Press the key mapped to `RESET` if it is available.
+
+### Building the Firmware
+Make example for this keyboard (after setting up your build environment):
+
+    make creatkeebs/thera:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
