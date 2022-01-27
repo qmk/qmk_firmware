2180e88c3de44b9a77084de22c60b07e69598afa
七两半 <sky_jun@qq.com>
2022-01-27 19:34:31 +0800

    add keyboard tp32

---
+++ keyboards/7liang/tp32/config.h
@@ -0,0 +1,21 @@
+/*
+ * @Description    : 
+ * @version        : 
+ * @Author         : JunLee
+ * @Date           : 2022-01-27 08:04:25
+ * @LastEditTime   : 2022-01-27 11:41:25
+ * @FilePath       : \qmk_firmware\keyboards\7liang\tp32\config.h
+ */
+// Copyright 2022 Pkerr (@sky_jun@qq.com)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID    0x1503
+#define MANUFACTURER Pkerr
+#define PRODUCT      Tp32
+
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 12
