15b28e985342cca860c7319e1863778ff36dbc27
Merlin Jehli <merlin@jeh.li>
2022-01-28 21:47:28 +0100

    Update keyboards/massdrop/alt/keymaps/fml128/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/massdrop/alt/keymaps/fml128/keymap.c
+++ keyboards/massdrop/alt/keymaps/fml128/keymap.c
@@ -19,9 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #ifdef RGB_MATRIX_ENABLE
-    static uint16_t idle_timer;
-    static uint8_t minute_counter;
-    static bool led_on;
+static uint16_t idle_timer;
+static uint8_t minute_counter;
+static bool led_on;
 #endif
 
 
