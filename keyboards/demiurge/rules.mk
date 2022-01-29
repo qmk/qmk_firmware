eedf1869821451fe99d8a963b127404e6812a5a8
ojthetiny <65928618+ojthetiny@users.noreply.github.com>
2022-01-29 01:14:08 +0000

    Update keyboards/demiurge/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/demiurge/rules.mk
+++ keyboards/demiurge/rules.mk
@@ -8,12 +8,11 @@ BOOTLOADER = atmel-dfu
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no	    # Mouse keys
-EXTRAKEY_ENABLE = yes	    # Audio control and System control
-CONSOLE_ENABLE = no	        # Console for debug
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes		    # Enable N-key Rollover
-BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
+NKRO_ENABLE = yes           # Enable N-key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
