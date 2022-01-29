7cfcbacba5baead9042c44af70ca1d0392dee477
ChrisGVE <christian.berclaz@mac.com>
2022-01-29 14:46:22 +0800

    Adelais: small change in layout, changed middle bottom key to Fn from Caps

--- keyboards/mechlovin/adelais/keymaps/chrisgve/keymap.c
+++ keyboards/mechlovin/adelais/keymaps/chrisgve/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_PGDN, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,
      KC_END,  CPS_CTL, A_MOUSE, KC_S,    M_NAV_1, M_F_NAV, KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
               SFT_MSE,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  ADJ_M,   R_SHIFT, KC_SLSH,
-              KC_LALT,          KC_LGUI,          SPC_RSE,          KC_CAPS,                  SPC_RSE,          KC_RGUI,                            KC_RALT
+              KC_LALT,          KC_LGUI,          SPC_RSE,          ADJ_M,                    SPC_RSE,          KC_RGUI,                            KC_RALT
     ),
 
     [_MAC_NAV_1] = LAYOUT_alice_split_bs(
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_PGDN, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,
      KC_END,  CPS_CTL, A_MOUSE, KC_S,    L_NAV_1, L_F_NAV, KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
               SFT_MSE,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, R_SHIFT, ADJ_L,
-              KC_LGUI,          KC_LALT,          SPC_RSE,          KC_CAPS,                  SPC_RSE,          KC_RALT,                            KC_RGUI
+              KC_LGUI,          KC_LALT,          SPC_RSE,          ADJ_L,                    SPC_RSE,          KC_RALT,                            KC_RGUI
     ),
 
     [_LINUX_NAV_1] = LAYOUT_alice_split_bs(
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_PGDN, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,
      KC_END,  CPS_CTL, A_MOUSE, KC_S,    W_NAV_1, W_F_NAV, KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
               SFT_MSE,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT, R_SHIFT, ADJ_W,
-              KC_LGUI,          KC_LALT,          SPC_RSE,          KC_CAPS,                  SPC_RSE,          KC_RALT,                            KC_RGUI
+              KC_LGUI,          KC_LALT,          SPC_RSE,          ADJ_W,                    SPC_RSE,          KC_RALT,                            KC_RGUI
     ),
 
     [_WIN_NAV_1] = LAYOUT_alice_split_bs(
