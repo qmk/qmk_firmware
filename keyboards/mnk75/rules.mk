6cdbfde38b8bf204676945038ffb861fd4990a65
yiancar <yiangosyiangou@cytanet.com.cy>
2022-01-29 20:36:31 +0000

    Update keyboards/mnk75/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mnk75/rules.mk
+++ keyboards/mnk75/rules.mk
@@ -23,6 +23,5 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 NO_USB_STARTUP_CHECK = no   # Disable initialization only when usb is plugged in
