9482a097c83264792cc593cb5f96a5b06e09192b
Ryan <fauxpark@gmail.com>
2022-01-30 08:17:18 +1100

    Fix Cypher rev6 VIA keymap (#16117)

--- keyboards/cablecardesigns/cypher/rev6/keymaps/via/keymap.c
+++ keyboards/cablecardesigns/cypher/rev6/keymaps/via/keymap.c
@@ -34,7 +34,7 @@ bool led_update_user(led_t led_state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(2, layer_state_cmp(state, _F1));
+    rgblight_set_layer_state(2, layer_state_cmp(state, 1));
     return state;
 }
 
