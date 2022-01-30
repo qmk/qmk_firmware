db54ea7c0e34f38af566b0f943f681279e492604
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-01-31 08:18:36 +1100

    add custom matrix.c to q2

--- keyboards/keychron/q2/q2_iso_stm32l432/rules.mk
+++ keyboards/keychron/q2/q2_iso_stm32l432/rules.mk
@@ -20,3 +20,8 @@ RGB_MATRIX_DRIVER = CKLED2001
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+# custom matrix setup
+CUSTOM_MATRIX = lite
+
+SRC += matrix.c
