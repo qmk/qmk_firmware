c8273003ca044ab4835bcbb60083f27330f6a6cd
Andrew Garver <andrewsgarver@gmail.com>
2022-01-30 00:25:29 -0700

    macros

--- keyboards/sofle/keymaps/FreshCourageTake/keymap.c
+++ keyboards/sofle/keymaps/FreshCourageTake/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_0,    DM_PLY1,                                     DM_REC1,    DM_RSTP,    DM_PLY1,    _______,    _______,  TG(_NUMPAD), \
+  KC_ESC,   KC_1,   KC_2,    KC_0,    DM_PLY1,    DM_PLY2,                                  DM_REC1,    DM_REC2,    DM_RSTP,    DM_PLY1,    DM_PLY2,  TG(_NUMPAD), \
   _______,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINUS, \
   OSM(MOD_LSFT),   KC_A,   KC_S,    LT(_FUNPAD, KC_D),    LT(_NUMPAD, KC_F), KC_G,          KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  OSM(MOD_RSFT), \
   KC_LALT,  KC_Z,   KC_X,    LGUI_T(KC_C),    KC_V,    KC_B, KC_MUTE,                           _______, KC_N, KC_M, RGUI_T(KC_COMM),  KC_DOT, KC_SLSH,  KC_BSLS, \
