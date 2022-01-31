9710990b43737a0ca0095aaba8e4049997f58b73
Laneware <68452738+Laneware@users.noreply.github.com>
2022-01-31 13:53:30 +1000

    Update keyboards/lw67/lw67.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/lw67/lw67.c
+++ keyboards/lw67/lw67.c
@@ -16,7 +16,8 @@
 
 #include "lw67.h"
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) return false;
     if (index == 0) { /* First encoder */
         if (clockwise) {
             tap_code(KC_VOLU);
