9410b61ef062bb503ba399effc25fb0747d2a910
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-01-31 15:34:43 -0800

    solder.h: use ___ for KC_NO

--- keyboards/mtbkeys/mtb60/solder/solder.h
+++ keyboards/mtbkeys/mtb60/solder/solder.h
@@ -18,6 +18,8 @@
 
 #include "quantum.h"
 
+#define ___ KC_NO
+
 #define LAYOUT( \
     k00, k10, k20, k30, k40, k50, k60, k70, k80, k90, ka0, kb0, kc0, kd0, ke0, \
          k11, k21, k31, k41, k51, k61, k71, k81, k91, ka1, kb1, kc1, kd1, ke1, \
@@ -25,9 +27,9 @@
     k03, k13, k23, k33, k43, k53, k63, k73, k83, k93, ka3, kb3, kc3, kd3, ke3, \
     k04, k14,      k34, k44,      k64,      k84,      ka4, kb4, kc4, kd4, ke4  \
 ) { \
-    { k00,   k10,   k20,   k30, k40, k50,   k60, k70,   k80, k90,   ka0, kb0, kc0, kd0, ke0   }, \
-    { KC_NO, k11,   k21,   k31, k41, k51,   k61, k71,   k81, k91,   ka1, kb1, kc1, kd1, ke1   }, \
-    { k02,   KC_NO, k22,   k32, k42, k52,   k62, k72,   k82, k92,   ka2, kb2, kc2, kd2, KC_NO }, \
-    { k03,   k13,   k23,   k33, k43, k53,   k63, k73,   k83, k93,   ka3, kb3, kc3, kd3, ke3   }, \
-    { k04,   k14,   KC_NO, k34, k44, KC_NO, k64, KC_NO, k84, KC_NO, ka4, kb4, kc4, kd4, ke4   }  \
+    { k00, k10, k20, k30, k40, k50, k60, k70, k80, k90, ka0, kb0, kc0, kd0, ke0 }, \
+    { ___, k11, k21, k31, k41, k51, k61, k71, k81, k91, ka1, kb1, kc1, kd1, ke1 }, \
+    { k02, ___, k22, k32, k42, k52, k62, k72, k82, k92, ka2, kb2, kc2, kd2, ___ }, \
+    { k03, k13, k23, k33, k43, k53, k63, k73, k83, k93, ka3, kb3, kc3, kd3, ke3 }, \
+    { k04, k14, ___, k34, k44, ___, k64, ___, k84, ___, ka4, kb4, kc4, kd4, ke4 }  \
 }
