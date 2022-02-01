a97fb9536915ca54b0cf96c17d71baee212ddf5f
Felix Sargent <felix@truework.com>
2022-02-01 13:33:22 -0800

    Cleaned up combos for Sofle, Sym layer, and made a combo sym toggle.

--- keyboards/sofle/keymaps/fsargent/keymap.c
+++ keyboards/sofle/keymaps/fsargent/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,							KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_EQL,
 	MT(MOD_MEH,KC_ESC),	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,							KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,
 		KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,	C(G(KC_SPC)),			TO(WIN),	KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	MT(MOD_LSFT,KC_BSLS),
-				MT(MOD_LCTL, KC_LBRC),	MT(MOD_LALT, KC_RBRC),	LT(SYM,KC_DEL), MO(NAV), CMD_T(KC_BSPC),		KC_SPC,		MT(SYM, KC_ENT),	MT(MOD_LGUI, KC_DEL),	MT(MOD_LALT, KC_MINS), 	MT(MOD_LCTL, KC_EQL)
+				MT(MOD_LCTL, KC_LBRC),	MT(MOD_LALT, KC_RBRC),	LT(SYM,KC_DEL), MO(NAV), CMD_T(KC_BSPC),		KC_SPC,		LT(SYM, KC_ENT),	MT(MOD_LGUI, KC_DEL),	MT(MOD_LALT, KC_MINS), 	MT(MOD_LCTL, KC_EQL)
 	),
 	// [CMK] = LAYOUT(
 	// 	KC_GRV,			KC_1,	KC_2,	KC_3,	KC_4,	KC_5,											KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,
@@ -50,23 +50,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,_______,_______,_______,_______,_______,					_______,_______,_______,_______,_______,_______,
 		_______,_______,_______,_______,_______,_______,					_______,_______,_______,_______,_______,_______,
 		_______,_______,_______,_______,_______,_______,					_______,_______,_______,_______,_______,_______,
-		_______,_______,_______,_______,_______,_______,KC_LGUI,			TO(GAME),_______,_______,_______,_______,_______,_______,
-		_______,_______,_______,MO(WINNAV),	CTL_T(KC_BSPC),				_______,_______,_______,_______,_______
+		_______,_______,_______,_______,_______,_______,KC_LGUI,		TO(GAME),	_______,_______,_______,_______,_______,_______,
+		_______,_______,_______,MO(WINNAV),	CTL_T(KC_BSPC),					_______,_______,_______,_______,_______
 	),
 	[GAME] = LAYOUT(
 		KC_GRV,		_______,	_______,	_______,	_______,	_______,				_______,_______,_______,_______,_______,_______,
 		KC_TAB,		_______,	_______,	_______,	_______,	_______,				_______,_______,_______,_______,_______,_______,
 		KC_ESC,		_______,	_______,	_______,	_______,	_______,				_______,_______,_______,_______,_______,_______,
 		KC_LSFT,	_______,	_______,	_______,	_______,	_______,MO(SYM),	TO(BASE),	_______,_______,_______,_______,_______,_______,
-		KC_LCTL,	KC_LALT,	MO(WINNAV),	KC_LCTL,	KC_BSPC,				KC_SPC,		LT(SYM, KC_ENT),	MT(MOD_LGUI,KC_DEL),	MT(MOD_LALT, KC_MINS), 	MT(MOD_LCTL, KC_EQL)
+		KC_LCTL,	KC_LALT,	MO(WINNAV),	KC_LCTL,	KC_BSPC,						_______,_______,_______,_______,_______
 	),
 	[SYM]	=	LAYOUT(
 		// C(G(KC_SPC)) is the OS X Emoji Editor
 		C(G(KC_SPC)),	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,							KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,
-		_______,	KC_GRV,		KC_7,		KC_8,		KC_9,		KC_PLUS,						KC_INS,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_F12,
-		_______,	KC_0, 		KC_4,		KC_5,		KC_6,		KC_0,							KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_0,		KC_PSCR,
-		_______,	KC_DOT,		KC_1,		KC_2,		KC_3,		KC_MINUS,	C(G(KC_SPC)),		TG(SYM),	KC_MINS,	KC_EQL,		KC_LBRC,	KC_RBRC,	KC_BSLS,	KC_NLCK,
-		_______,	_______,	_______,	_______,	_______,			_______,	_______,	_______,	_______,	_______
+		_______,	KC_EQL,		KC_7,		KC_8,		KC_9,		KC_MINUS,						KC_INS,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_F12,
+		_______,	KC_0, 		KC_4,		KC_5,		KC_6,		KC_PLUS,							KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_0,		KC_PSCR,
+		_______,	KC_DOT,		KC_1,		KC_2,		KC_3,		KC_ENT,	C(G(KC_SPC)),		TG(SYM),	KC_MINS,	KC_EQL,		KC_LBRC,	KC_RBRC,	KC_BSLS,	KC_NLCK,
+						_______,	KC_0,		KC_NO,		_______,	_______,		_______,	_______,	_______,	_______,	_______
 ),
 	[NAV]=LAYOUT(
 		C(KC_GRV),	KC_HOME,	KC_PGUP,	KC_NO,		KC_PGDN,	KC_END,						KC_MPRV,	KC_MPLY,	KC_MNXT,	KC_MINS,	KC_EQL,		KC_EQL,
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,	_______,	_______,	_______,	_______,								KC_BTN1,	KC_BTN2,	KC_BTN3,	RGB_RMOD,	RGB_MOD
 	),
 	[WINNAV]=LAYOUT(
-		C(KC_GRV),	MEH(2),		KC_NO,		KC_WH_U,	KC_WH_R,	G(KC_ENT),						KC_NO,		KC_MPRV,	KC_MPLY,	KC_MNXT,	KC_MUTE,	KC_EQL,
+		C(KC_GRV),	KC_HOME,	KC_PGUP,	KC_NO,		KC_PGDN,	KC_END,							KC_NO,		KC_MPRV,	KC_MPLY,	KC_MNXT,	KC_MUTE,	KC_EQL,
 		A(KC_TAB), 	S(KC_TAB),	C(KC_BSPC),	KC_UP,		C(KC_DEL),	KC_TAB,							KC_WH_U,	KC_WH_L,	KC_MS_U,	KC_WH_R,	KC_END,		KC_RBRC,
 		KC_LALT,	C(KC_LEFT),	KC_LEFT,	KC_DOWN,	KC_RGHT,	C(KC_RGHT),						KC_BTN3,	KC_MS_L,	KC_MS_D,	KC_MS_R,	KC_NO,		KC_LGUI,
 		KC_LSFT,	KC_HOME,	A(KC_LEFT),	KC_ENT,		A(KC_RGHT),	KC_END,		G(KC_ENT),		TG(GAME),	KC_WH_D,	KC_NO,		KC_LCBR,	KC_RCBR,	KC_BSLS,	KC_DEL,
--- keyboards/sofle/keymaps/fsargent/rgb.c
+++ keyboards/sofle/keymaps/fsargent/rgb.c
@@ -2,18 +2,17 @@
 
 
 // Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
-const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {1, 8, HSV_RED},     // Light 4 LEDs, starting with LED 6
-    {37, 8, HSV_RED}       // Light 4 LEDs, starting with LED 12
-);
-// Default
+// const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {1, 8, HSV_RED},     // Light 4 LEDs, starting with LED 6
+//     {37, 8, HSV_RED}       // Light 4 LEDs, starting with LED 12
+// );
+// OS X or Default
 const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 1, HSV_CYAN}
+    {0, 1, HSV_PINK}
 );
-// Light LEDs 11 & 12 in purple when keyboard layer 2 is active
+// Windows
 const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {8, 6, HSV_PURPLE},
-    {45, 6, HSV_PURPLE}
+    {0, 74, HSV_PINK}
 );
 // Game
 const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -23,24 +22,31 @@ const rgblight_segment_t PROGMEM my_layer4_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 74, HSV_GOLDENROD}
 );
 const rgblight_segment_t PROGMEM my_layer5_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 74, HSV_PINK}
+    {0, 74, HSV_BLUE}
 );
 const rgblight_segment_t PROGMEM game[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 74, HSV_GREEN}
 );
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(0, led_state.caps_lock);
-    return true;
-};
+// bool led_update_user(led_t led_state) {
+//     rgblight_set_layer_state(0, led_state.caps_lock);
+//     return true;
+// };
 
 layer_state_t default_layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(1, layer_state_cmp(state, 0));
+    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
     return state;
 }
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
+    rgblight_set_layer_state(2, layer_state_cmp(state, 2));
+    rgblight_set_layer_state(3, layer_state_cmp(state, 3));
+    rgblight_set_layer_state(4, layer_state_cmp(state, 4));
+    rgblight_set_layer_state(5, layer_state_cmp(state, 5));
+    return state;
+}
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_capslock_layer,
     my_layer1_layer,    // Overrides caps lock layer
     my_layer2_layer,    // Overrides other layers
     my_layer3_layer,
