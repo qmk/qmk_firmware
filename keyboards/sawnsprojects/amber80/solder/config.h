fa9d6685f866a3e065cb92eea935f3c3adb81d04
Sắn <59417802+MaiTheSan@users.noreply.github.com>
2022-02-01 07:26:29 +0700

    Update config.h

--- keyboards/sawnsprojects/amber80/solder/config.h
+++ keyboards/sawnsprojects/amber80/solder/config.h
@@ -1,19 +1,18 @@
-/*
-Copyright 2022 SawnsProjects
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright 2022 SawnsProjects
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -30,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 9
 
-/* Caps Lock */
+/* Indicator */
 #define LED_CAPS_LOCK_PIN F5
 #define LED_SCROLL_LOCK_PIN B0
 #define LED_PIN_ON_STATE 0
