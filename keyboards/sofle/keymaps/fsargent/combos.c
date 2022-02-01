65ebdd48c14b6bedd3d7a10122fc30118b37e887
Felix Sargent <felix@truework.com>
2022-02-01 13:37:11 -0800

    Sym layer now works on windows layer.

--- keyboards/sofle/keymaps/fsargent/combos.c
+++ keyboards/sofle/keymaps/fsargent/combos.c
@@ -2,7 +2,7 @@
 
 #ifdef COMBO_ENABLE
 
-enum combo_events { BKT, TGSYM, CBRC, PAREN, LTGT, CTRLALTDEL, CMD_ENTER, ENTER, TAB, SHIFT_TAB, CTRLC, CTRLR, CUT, CPY, PST, CAL, DELWD, EML, PHONE, EMW, WINDELWD, CTILD, CTILD_SHIFT, COMBO_LENGTH };
+enum combo_events { BKT, WINTGSYM, TGSYM, CBRC, PAREN, LTGT, CTRLALTDEL, CMD_ENTER, ENTER, TAB, SHIFT_TAB, CTRLC, CTRLR, CUT, CPY, PST, CAL, DELWD, EML, PHONE, EMW, WINDELWD, CTILD, CTILD_SHIFT, COMBO_LENGTH };
 uint16_t COMBO_LEN = COMBO_LENGTH;  // remove the COMBO_COUNT define and use this instead!
 
 const uint16_t PROGMEM asdf_combo[]    = {KC_A, KC_S, KC_D, KC_F, COMBO_END};
@@ -10,6 +10,7 @@ const uint16_t PROGMEM zxcv_combo[]    = {KC_Z, KC_X, KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM uiop_combo[]    = {KC_U, KC_I, KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM jklsemi_combo[] = {KC_J, KC_K, KC_L, KC_SCLN, COMBO_END};
 const uint16_t PROGMEM thumbs_combo[]  = {MO(4), LT(3, KC_ENT), COMBO_END};
+const uint16_t PROGMEM win_thumbs_combo[]  = {MO(5), LT(3, KC_ENT), COMBO_END};
 
 const uint16_t PROGMEM zx_combo[] = {KC_Z, KC_X, COMBO_END};
 const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
@@ -35,6 +36,7 @@ combo_t key_combos[] = {
     [CTILD]       = COMBO_ACTION(qw_combo),
     [CTILD_SHIFT] = COMBO_ACTION(jk_combo),
     [TGSYM]       = COMBO_ACTION(thumbs_combo),
+    [WINTGSYM]       = COMBO_ACTION(win_thumbs_combo),
 
     // 4 key combos
     [CMD_ENTER]  = COMBO_ACTION(asdf_combo),
@@ -98,6 +100,11 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
     };
     switch (combo_index) {
         case TGSYM:
+            if (pressed) {
+                layer_invert(3);
+            }
+            break;
+	case WINTGSYM:
             if (pressed) {
                 layer_invert(3);
             }
