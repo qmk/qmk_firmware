6987988385a0b620180be64f74b1d9fc0f4ed813
ChrisGVE <christian.berclaz@mac.com>
2022-02-01 19:58:46 +0800

    Added an overall make script for all owned keyboard

---
+++ makeall.sh
@@ -0,0 +1,16 @@
+make mechlovin/adelais/standard_led/arm/rev4/apm32f103:chrisgve
+make kbdfans/odin/soldered:chrisgve
+make preonic/rev3:chrisgve_2x2u
+make preonic/rev3:chrisgve
+make zlant_xl:chrisgve
+make mode/m65s:chrisgve
+make cannonkeys/adelie:chrisgve
+make cannonkeys/instant60:chrisgve
+make cannonkeys/savage65:chrisgve_tsangan
+make dz60:chrisgve_tsangan
+make dztech/dz60rgb_wkl/v2_1:chrisgve
+make kbdfans/kbd67/rev2:chrisgve_v3
+make winx:chrisgve
+make hineybush/h60:chrisgve
+make hifinger75:chrisgve
+
