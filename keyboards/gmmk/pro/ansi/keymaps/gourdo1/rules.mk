374924047aef11c6643671728127d41a92b3274b
gourdo1 <gourdo1@users.noreply.github.com>
2022-02-01 17:54:36 -0800

    Initial commits using @Jonavin firmware as baseline
    Commits on Feb 1, 2022
    * Expanded RGB definitions of LED_LIST_NUMPAD to include most active numpad layer keys
    * Reverted color.h back to QMK default
    * Moved RGB custom color defs to gourdo1.h
    * Numpad enter added to bottom right corner keys
    
    Commits on Jan 31, 2022
    * Simplified numpad layer; mapped regular numbers as numpad versions
    
    Commits on Jan 27, 2022
    * Invader = Home; Fn+11,F12,Invader = Print,Scroll,Pause
    * Del key moved to right of Backspace
    
    Commits on Jan 24, 2022
    * CTRL & SHIFT-Space work like Space
    * Make Ctrl-Space act like Space; RGB Hue step more granular
    
    Commits on Jan 23, 2022
    * Created GMMK Pro Cheatsheet.pdf
    * Moved Pause to Fn + P
    * moved DEL to func row & added INS as SHIFT-DEL
    
    Commits on Jan 22, 2022
    * My latest firmware as of 01-22-2022
    
    Commits on Jan 21, 2022
    * VIA disabled; RGB gaming mode + gordotest keymap
    
    Commits on Jan 19, 2022
    * Enabled LTO + some cosmetics
    
    Commits on Jan 18, 2022
    * More rgb tweaks and fixes; default RGB timeout increase
    * Many RGB Tweaks & customizations; FN layer RGB tweaks
    * Added RGB test definitions
    
    Commits on Jan 12, 2022
    * Merge branch 'Jonavin:master' into master
    
    Commits on Jan 11, 2022
    * RGB tweaks
    * Attempt to fix left shift sticking after capslock triggered
    * Moved dance_LSFT_finished to first arg
    * Removed tap dance test
    
    Commits on Jan 10, 2022
    * Fix tap dance declaration location
    * Testing Escape TapDance idea
    
    Commits on Jan 5, 2022
    * Various tweaks
    * Sorted dueling config.h files
    * Update config.h again
    * Test longer tapping_term in config.h
    
    Commits on Jan 4, 2022
    * Added solartempest's keymap for reference
    * fixed more references
    * Added gourdo1 folder and fixed references
    * Fixed include reference
    * Reverted changes to jonavin keymap.c
    * Folder cleanup
    * Create tmp
    * Delete gourdo1
    * Create gourdo1
    * Added Solartemp
    * Update keymap.c

---
+++ keyboards/gmmk/pro/ansi/keymaps/gourdo1/rules.mk
@@ -0,0 +1,16 @@
+LTO_ENABLE = yes    	       # link time optimization -- achieves a smaller compiled size
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+
+VIA_ENABLE = no
+TAP_DANCE_ENABLE = yes
+BOOTMAGIC_ENABLE = yes         # Enable Bootmagic Lite
+
+TD_LSFT_CAPSLOCK_ENABLE = yes
+IDLE_TIMEOUT_ENABLE = yes
+STARTUP_NUMLOCK_ON = yes
+ENCODER_DEFAULTACTIONS_ENABLE = no
+
+COLEMAK_LAYER_ENABLE = yes     # Enable Colemak layer / set to no to disable
+INVERT_NUMLOCK_INDICATOR = yes
