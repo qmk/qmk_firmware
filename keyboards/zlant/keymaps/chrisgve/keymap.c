5c1ca5b5bc242ad9f9d4224ed8213e49ae8f20c5
ChrisGVE <christian.berclaz@mac.com>
2022-02-02 18:34:53 +0800

    Updated the overall make script to make use of multicore

--- keyboards/zlant/keymaps/chrisgve/keymap.c
+++ keyboards/zlant/keymaps/chrisgve/keymap.c
@@ -1,25 +1,24 @@
 #include QMK_KEYBOARD_H
 
-#define BASE 0
-#define FN   1
+#include "chrisgve.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [BASE] = LAYOUT(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    RGB_HUI, RGB_HUD, KC_LGUI, KC_LALT, KC_LSFT, KC_SPC,  KC_SPC,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  [_QWERTY_MAC] = LAYOUT(
+    KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    CU_BSPC,
+    TAB_CTL, A_MOUSE, KC_S,    M_NAV_1, M_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    SFT_MSE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, QUT_SFT,
+    ADJ_M,   TD_ADJM, KC_LALT, KC_LGUI, LOWER,   SPC_RSE, SPC_RSE, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
   /*  BASE LAYER
    * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
-   * │ Tab  │   Q  │   W  │   E  │   R  │   T  │   Y  │   U  │   I  │   O  │   P  │ Bspc │
+   * │ GEsc │   Q  │   W  │   E  │   R  │   T  │   Y  │   U  │   I  │   O  │   P  │ Bspc │
    * └┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┐
-   *  │ LCtrl│   A  │   S  │   D  │   F  │   G  │   H  │   J  │   K  │   L  │   ;  │   '  │
+   *  │TabCtl│   A  │   S  │   D  │   F  │   G  │   H  │   J  │   K  │   L  │   ;  │ Enter│
    *  └┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┐
-   *   │ Esc  │   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │ Enter│
+   *   │ SFT  │   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │ '-SFT│
    *   └┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴──────┴┬─────┴┬─────┴┬─────┴┬─────┴┬─────┴┐
-   *    │LED H+│LED H-│ LGUI │ LAlt │LShift│    Space    │  Fn  │ Left │ Down │  Up  │ Right│
+   *    │ Fn   │      │ LAlt │ LGUI │Lower │    Space    │ Raise│ Left │ Down │  Up  │ Right│
    *    └──────┴──────┴──────┴──────┴──────┴─────────────┴──────┴──────┴──────┴──────┴──────┘
    */
   [FN]   = LAYOUT(
--- makeall.sh
+++ makeall.sh
@@ -1,18 +1,18 @@
-make mechlovin/adelais/standard_led/arm/rev4/apm32f103:chrisgve
-make kbdfans/odin/soldered:chrisgve
-make preonic/rev3:chrisgve_2x2u
-make preonic/rev3:chrisgve
-make zlant_xl:chrisgve
-make mode/m65s:chrisgve
-make cannonkeys/adelie:chrisgve
-make cannonkeys/instant60:chrisgve
-make cannonkeys/savage65:chrisgve_tsangan
-make dz60:chrisgve_tsangan
-make dztech/dz60rgb_wkl/v2_1:chrisgve
-make kbdfans/kbd67/rev2:chrisgve_v3
-make winx:chrisgve
-make hineybush/h60:chrisgve
-make hifinger75:chrisgve
-make bemeier/bmek/rev3:chrisgve
-make kbdfans/D60_B:chrisgve
+make -j mechlovin/adelais/standard_led/arm/rev4/apm32f103:chrisgve
+make -j kbdfans/odin/soldered:chrisgve
+make -j preonic/rev3:chrisgve_2x2u
+make -j preonic/rev3:chrisgve
+make -j zlant_xl:chrisgve
+make -j mode/m65s:chrisgve
+make -j cannonkeys/adelie:chrisgve
+make -j cannonkeys/instant60:chrisgve
+make -j cannonkeys/savage65:chrisgve_tsangan
+make -j dz60:chrisgve_tsangan
+make -j dztech/dz60rgb_wkl/v2_1:chrisgve
+make -j kbdfans/kbd67/rev2:chrisgve_v3
+make -j winx:chrisgve
+make -j hineybush/h60:chrisgve
+make -j hifinger75:chrisgve
+make -j bemeier/bmek/rev3:chrisgve
+make -j kbdfans/D60_B:chrisgve
 
