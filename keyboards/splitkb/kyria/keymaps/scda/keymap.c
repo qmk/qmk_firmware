766fccd2de646102ca44faaf91b974e1bb7e0ca9
scda <dsop@mailbox.org>
2022-02-03 14:30:41 +0100

    add equal and dot to number layer

--- keyboards/splitkb/kyria/keymaps/scda/keymap.c
+++ keyboards/splitkb/kyria/keymaps/scda/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, DE_EURO, KC_AMPR, KC_TILD, KC_SLASH, KC_PIPE, KC_NO,   KC_NO,             KC_NO,   KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_UNDS, KC_GRV,  KC_NO,
                                 KC_TRNS,    NAV,      ALT_TAB, CMD_SPC, SFT_ENT,           KC_LSFT, CMD_BSP, ALT_DEL, KC_TRNS, NAV),
     [_NUMBERS] = LAYOUT(
-        KC_TRNS, KC_NO, KC_F7, KC_F8, KC_F9,   KC_F12,                                                       KC_NO,   KC_7, KC_8, KC_9, KC_NO, KC_TRNS,
+        KC_TRNS, KC_NO, KC_F7, KC_F8, KC_F9,   KC_F12,                                                       KC_DOT,  KC_7, KC_8, KC_9, KC_EQL,  KC_TRNS,
         XOY,     KC_NO, KC_F4, KC_F5, KC_F6,   KC_F11,                                                       KC_0,    KC_4, KC_5, KC_6, KC_PLUS, KC_ASTR,
         KC_TRNS, KC_NO, KC_F1, KC_F2, KC_F3,   KC_F10,  KC_NO,   KC_NO,                    KC_NO,   KC_NO,   KC_COMM, KC_1, KC_2, KC_3, KC_MINS, KC_SLASH,
                                 MECO, KC_TRNS, ALT_TAB, CMD_SPC, SFT_ENT,                  KC_LSFT, CMD_BSP, ALT_DEL, MECO, KC_TRNS),
