b6ea844afdf7a40ed977d7f0fd0482f2137dadb9
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2022-02-04 08:21:07 +0900

    Update keyboards/gl516/a52gl/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/gl516/a52gl/keymaps/via/keymap.c
+++ keyboards/gl516/a52gl/keymaps/via/keymap.c
@@ -26,7 +26,7 @@ LT(1,KC_CAPS),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_B,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_UP, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-     KC_LCTRL, KC_LGUI, KC_LALT,       KC_SPC,      KC_SPC,         KC_SPC,       KC_LALT,KC_RCTRL, KC_LEFT, KC_DOWN, KC_RGHT
+     KC_LCTL, KC_LGUI, KC_LALT,       KC_SPC,      KC_SPC,         KC_SPC,       KC_LALT,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   //|-----------------------------------------------------|   |--------------------------------------------------------------'
   ),
   [1] = LAYOUT(
