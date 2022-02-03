e60ab28d72c81a282ad7d9891d20e60271a6841d
Elliot Powell <32494740+e11i0t23@users.noreply.github.com>
2022-02-03 10:12:04 +0000

    Update keyboards/keebsforall/freebirdnp/lite/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keebsforall/freebirdnp/lite/rules.mk
+++ keyboards/keebsforall/freebirdnp/lite/rules.mk
@@ -12,8 +12,9 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-# Community layouts supported
 LAYOUTS = numpad_5x4 ortho_5x4
