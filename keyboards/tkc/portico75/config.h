1274960c92b08d597b26a128eebe3fee8afef8b1
Drashna Jael're <drashna@live.com>
2022-02-04 13:11:31 -0800

    Updates for PR

--- keyboards/tkc/portico75/config.h
+++ keyboards/tkc/portico75/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x544B //TK
+#define VENDOR_ID       0x544B // TK
 #define PRODUCT_ID      0x0011
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    TKC
@@ -30,12 +30,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 15
 
-#define MATRIX_ROW_PINS { B0, E6, F0, F1, D3, D2 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, C7, C6, B6, B5, B4, D7, D6, D4, D5, B3, B7 }
+#define MATRIX_ROW_PINS \
+    { B0, E6, F0, F1, D3, D2 }
+#define MATRIX_COL_PINS \
+    { F4, F5, F6, F7, C7, C6, B6, B5, B4, D7, D6, D4, D5, B3, B7 }
 
 /*Rotary Encoder Pins*/
-#define ENCODERS_PAD_A { B1 }
-#define ENCODERS_PAD_B { B2 }
+#define ENCODERS_PAD_A \
+    { B1 }
+#define ENCODERS_PAD_B \
+    { B2 }
 
 /*Sets the number of pulses per increment*/
 #define ENCODER_RESOLUTION 2
@@ -48,90 +52,129 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_FUNCTION
 
 #ifdef RGB_MATRIX_ENABLE
-#define RGB_MATRIX_LED_PROCESS_LIMIT 4
-#define RGB_MATRIX_LED_FLUSH_LIMIT 26
-#define DEBOUNCE 3
-#define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
-#define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
-#define RGB_MATRIX_KEYPRESSES
-#define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-#define DISABLE_RGB_MATRIX_BAND_SAT
-#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-#define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#define DISABLE_RGB_MATRIX_SPLASH
-#define DISABLE_RGB_MATRIX_MULTISPLASH
-#define DISABLE_RGB_MATRIX_SOLID_SPLASH
-#define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
-#define DISABLE_RGB_MATRIX_DIGITAL_RAIN
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
-#define DRIVER_ADDR_1 0b0110000
-#define DRIVER_ADDR_2 0b0110000 // this is here for compliancy reasons.
-#define DRIVER_COUNT 1
-#define DRIVER_1_LED_TOTAL 98
-#define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
-#define DRIVER_INDICATOR_LED_TOTAL 0
+#    define RGB_MATRIX_LED_PROCESS_LIMIT 4
+#    define RGB_MATRIX_LED_FLUSH_LIMIT 26
+#    define DEBOUNCE 3
+#    define RGB_DISABLE_AFTER_TIMEOUT 0     // number of ticks to wait until disabling effects
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED  // turn off effects when suspended
+#    define RGB_MATRIX_KEYPRESSES
+
+// RGB Matrix Animation modes. Explicitly enabled
+// For full list of effects, see:
+// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
+// #    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#    define ENABLE_RGB_MATRIX_BREATHING
+// #    define ENABLE_RGB_MATRIX_BAND_SAT
+#    define ENABLE_RGB_MATRIX_BAND_VAL
+// #    define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#    define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+// #    define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#    define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#    define ENABLE_RGB_MATRIX_CYCLE_ALL
+#    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#    define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#    define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#    define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#    define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#    define ENABLE_RGB_MATRIX_DUAL_BEACON
+#    define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#    define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#    define ENABLE_RGB_MATRIX_RAINDROPS
+#    define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#    define ENABLE_RGB_MATRIX_HUE_BREATHING
+#    define ENABLE_RGB_MATRIX_HUE_PENDULUM
+#    define ENABLE_RGB_MATRIX_HUE_WAVE
+#    define ENABLE_RGB_MATRIX_PIXEL_RAIN
+#    define ENABLE_RGB_MATRIX_PIXEL_FLOW
+#    define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+// enabled only if RGB_MATRIX_FRAMEBUFFER_EFFECTS is defined
+#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+// #    define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
+// #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #    define ENABLE_RGB_MATRIX_SPLASH
+// #    define ENABLE_RGB_MATRIX_MULTISPLASH
+// #    define ENABLE_RGB_MATRIX_SOLID_SPLASH
+// #    define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
+#    define DRIVER_ADDR_1 0b0110000
+#    define DRIVER_ADDR_2 0b0110000  // this is here for compliancy reasons.
+#    define DRIVER_COUNT 1
+#    define DRIVER_1_LED_TOTAL 98
+#    define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
+#    define DRIVER_INDICATOR_LED_TOTAL 0
 #else
 // WT_RGB IS31FL3741 driver code
-#define DRIVER_COUNT 1
-#define DRIVER_LED_TOTAL 98
+#    define DRIVER_COUNT 1
+#    define DRIVER_LED_TOTAL 98
 
-#define RGB_BACKLIGHT_ENABLED 1
+#    define RGB_BACKLIGHT_ENABLED 1
 
 // This conditionally compiles the backlight code for Portico75 specifics
-#define RGB_BACKLIGHT_PORTICO75
+#    define RGB_BACKLIGHT_PORTICO75
 
 // disable backlight when USB suspended (PC sleep/hibernate/shutdown)
-#define RGB_BACKLIGHT_DISABLE_WHEN_USB_SUSPENDED 1
+#    define RGB_BACKLIGHT_DISABLE_WHEN_USB_SUSPENDED 1
 
 // disable backlight after timeout in minutes, 0 = no timeout
-#define RGB_BACKLIGHT_DISABLE_AFTER_TIMEOUT 0
+#    define RGB_BACKLIGHT_DISABLE_AFTER_TIMEOUT 0
 
 // the default brightness
-#define RGB_BACKLIGHT_BRIGHTNESS 255
+#    define RGB_BACKLIGHT_BRIGHTNESS 255
 
 // the default effect (RGB test)
-#define RGB_BACKLIGHT_EFFECT 255
+#    define RGB_BACKLIGHT_EFFECT 255
 
 // the default effect speed (0-3)
-#define RGB_BACKLIGHT_EFFECT_SPEED 0
+#    define RGB_BACKLIGHT_EFFECT_SPEED 0
 
 // the default color1 and color2
-#define RGB_BACKLIGHT_COLOR_1 { .h = 0, .s = 255 }
-#define RGB_BACKLIGHT_COLOR_2 { .h = 127, .s = 255 }
+#    define RGB_BACKLIGHT_COLOR_1 \
+        { .h = 0, .s = 255 }
+#    define RGB_BACKLIGHT_COLOR_2 \
+        { .h = 127, .s = 255 }
 
-#define RGB_BACKLIGHT_USE_SPLIT_BACKSPACE 0
-#define RGB_BACKLIGHT_USE_SPLIT_LEFT_SHIFT 0
-#define RGB_BACKLIGHT_USE_SPLIT_RIGHT_SHIFT 0
-#define RGB_BACKLIGHT_USE_7U_SPACEBAR 0
-#define RGB_BACKLIGHT_USE_ISO_ENTER 0
-#define RGB_BACKLIGHT_DISABLE_HHKB_BLOCKER_LEDS 0
-#define RGB_BACKLIGHT_CAPS_LOCK_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
-#define RGB_BACKLIGHT_LAYER_1_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
-#define RGB_BACKLIGHT_LAYER_2_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
-#define RGB_BACKLIGHT_LAYER_3_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
+#    define RGB_BACKLIGHT_USE_SPLIT_BACKSPACE 0
+#    define RGB_BACKLIGHT_USE_SPLIT_LEFT_SHIFT 0
+#    define RGB_BACKLIGHT_USE_SPLIT_RIGHT_SHIFT 0
+#    define RGB_BACKLIGHT_USE_7U_SPACEBAR 0
+#    define RGB_BACKLIGHT_USE_ISO_ENTER 0
+#    define RGB_BACKLIGHT_DISABLE_HHKB_BLOCKER_LEDS 0
+#    define RGB_BACKLIGHT_CAPS_LOCK_INDICATOR \
+        { .color = {.h = 0, .s = 0}, .index = 255 }
+#    define RGB_BACKLIGHT_LAYER_1_INDICATOR \
+        { .color = {.h = 0, .s = 0}, .index = 255 }
+#    define RGB_BACKLIGHT_LAYER_2_INDICATOR \
+        { .color = {.h = 0, .s = 0}, .index = 255 }
+#    define RGB_BACKLIGHT_LAYER_3_INDICATOR \
+        { .color = {.h = 0, .s = 0}, .index = 255 }
 
 // These define which keys in the matrix are alphas/mods
 // Used for backlight effects so colors are different for
 // alphas vs. mods
 // Each value is for a row, bit 0 is column 0
 // Alpha=0 Mod=1
-#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_0 0b0110000000000001
-#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_1 0b0110000000000001
-#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_2 0b0111000000000001
-#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_3 0b0111100000000001
-#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_4 0b1111111111111111
+#    define RGB_BACKLIGHT_ALPHAS_MODS_ROW_0 0b0110000000000001
+#    define RGB_BACKLIGHT_ALPHAS_MODS_ROW_1 0b0110000000000001
+#    define RGB_BACKLIGHT_ALPHAS_MODS_ROW_2 0b0111000000000001
+#    define RGB_BACKLIGHT_ALPHAS_MODS_ROW_3 0b0111100000000001
+#    define RGB_BACKLIGHT_ALPHAS_MODS_ROW_4 0b1111111111111111
 
 // Backlight config starts after VIA's EEPROM usage,
 // dynamic keymaps start after this.
-#define VIA_EEPROM_CUSTOM_CONFIG_SIZE 31
+#    define VIA_EEPROM_CUSTOM_CONFIG_SIZE 31
 
 // VIA lighting is handled by the keyboard-level code
-#define VIA_CUSTOM_LIGHTING_ENABLE
+#    define VIA_CUSTOM_LIGHTING_ENABLE
 #endif
--- keyboards/tkc/portico75/keymaps/default/keymap.c
+++ keyboards/tkc/portico75/keymaps/default/keymap.c
@@ -29,14 +29,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                    KC_SPC,                            KC_RALT,   MO(1), KC_LEFT, KC_DOWN, KC_RGHT
     )
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-	return false;
-}
