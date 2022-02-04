c55673e2efd42497d7c112bf565c15315308df39
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2022-02-04 19:46:39 +0900

    Removed LED control for iOS.
    Removed LED control for iOS.

--- keyboards/gl516/n51gl/config.h
+++ keyboards/gl516/n51gl/config.h
@@ -65,26 +65,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_EFFECT_ALTERNATING
 #define RGBLIGHT_EFFECT_TWINKLE
 
-#ifndef IOS_DEVICE_ENABLE
-  #define RGBLIGHT_LIMIT_VAL 90
-  #define RGBLIGHT_VAL_STEP 17
-#else
-  #define RGBLIGHT_LIMIT_VAL 30
-  #define RGBLIGHT_VAL_STEP 4
-#endif
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-
-#if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
-// USB_MAX_POWER_CONSUMPTION value for naked48 keyboard
-//  120  RGBoff, OLEDoff
-//  120  OLED
-//  330  RGB 6
-//  300  RGB 32
-//  310  OLED & RGB 32
-  #define USB_MAX_POWER_CONSUMPTION 400
-#else
-  // fix iPhone and iPad power adapter issue
-  // iOS device need lessthan 100
-  #define USB_MAX_POWER_CONSUMPTION 100
-#endif
