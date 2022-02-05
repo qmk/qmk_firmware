247703ff1a1fbd0f66353861cf9f9b1c00052f6f
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2022-02-05 05:34:17 +0100

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/macroboard/keymaps/default/keymap.c
+++ keyboards/handwired/macroboard/keymaps/default/keymap.c
@@ -16,7 +16,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_ortho_5x6(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
--- keyboards/handwired/macroboard/macroboard.h
+++ keyboards/handwired/macroboard/macroboard.h
@@ -26,7 +26,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT( \
+#define LAYOUT_ortho_5x6( \
     k00, k01, k02, k03, k04, k05, \
     k10, k11, k12, k13, k14, k15, \
     k20, k21, k22, k23, k24, k25, \
