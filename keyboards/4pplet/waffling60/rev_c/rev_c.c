0b7a79165631144f5dae73d0abf0b1844b90b1e5
4pplet <mail@4pplet.com>
2022-02-05 09:45:36 +0100

    Update keyboards/4pplet/waffling60/rev_c/rev_c.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/4pplet/waffling60/rev_c/rev_c.c
+++ keyboards/4pplet/waffling60/rev_c/rev_c.c
@@ -18,15 +18,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
-    if(CAPS_LOCK_ENABLE){
-        if(res) {
-            if(led_state.caps_lock){
-                rgblight_enable();
-                rgblight_mode(1);
-                rgblight_sethsv_noeeprom(CAPS_LOCK_COLOR);
-            }
-            else
-                rgblight_disable_noeeprom();
+    if (CAPS_LOCK_ENABLE && res) {
+        if (led_state.caps_lock){
+            rgblight_enable();
+            rgblight_mode(1);
+            rgblight_sethsv_noeeprom(CAPS_LOCK_COLOR);
+        } else {
+            rgblight_disable_noeeprom();
         }
     }
     return res;
