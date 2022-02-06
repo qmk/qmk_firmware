b1b26a32d08e1b1fba20e375e40b81f09b9f8db7
peepeetee <peepeetee@protonmail.com>
2022-02-06 20:19:54 +0800

    key to led mapping, rest of g_led_config non-functional

--- keyboards/idobao/id80/v3/rules.mk
+++ keyboards/idobao/id80/v3/rules.mk
@@ -17,3 +17,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes
+
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
