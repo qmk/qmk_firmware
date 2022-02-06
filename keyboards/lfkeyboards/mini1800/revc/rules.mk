1122a0f0bbad349ef2b3bde2c1e00cabc6ecf5b9
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-02-06 01:28:34 -0800

    move common keymap rules.mk settings to revision level

--- keyboards/lfkeyboards/mini1800/revc/rules.mk
+++ keyboards/lfkeyboards/mini1800/revc/rules.mk
@@ -11,4 +11,28 @@ SRC = TWIlib.c issi.c lighting.c
 # Build Options
 #   change yes to no to disable
 #
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = yes          # Audio output
+
+# RGB code is implemented in lfkeyboards, not WS2812
+RGBLIGHT_CUSTOM_DRIVER = yes
 BACKLIGHT_DRIVER = custom
+SLEEP_LED_ENABLE = yes
+
+ISSI_ENABLE = yes           # If the I2C pullup resistors aren't install this must be disabled
+WATCHDOG_ENABLE = yes       # Resets keyboard if matrix_scan isn't run every 250ms
+
+ifeq ($(strip $(ISSI_ENABLE)), yes)
+    TMK_COMMON_DEFS += -DISSI_ENABLE
+endif
+
+ifeq ($(strip $(WATCHDOG_ENABLE)), yes)
+    TMK_COMMON_DEFS += -DWATCHDOG_ENABLE
+endif
