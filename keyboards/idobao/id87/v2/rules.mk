9fed7d4261973cfde1e023cca74d94da68c3d882
peepeetee <peepeetee@protonmail.com>
2022-02-06 13:16:33 +0800

    add rgb matrix

--- keyboards/idobao/id87/v2/rules.mk
+++ keyboards/idobao/id87/v2/rules.mk
@@ -16,5 +16,7 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
 
 LAYOUTS = tkl_ansi
