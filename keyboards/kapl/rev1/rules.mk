e3c240ebc9e54b4496687dbb590131d4b51d8aa5
Alexander <7888825+keyzog@users.noreply.github.com>
2022-02-08 01:20:11 +0300

    Update keyboards/kapl/rev1/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kapl/rev1/rules.mk
+++ keyboards/kapl/rev1/rules.mk
@@ -3,7 +3,5 @@ RGBLIGHT_ENABLE = no       				# Enable keyboard RGB underglow
 # Do not enable RGB_MATRIX_ENABLE together with RGBLIGHT_ENABLE
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
-RGB_MATRIX_KEYPRESSES = yes  			# Enable reactive per-key effects.
-RGB_MATRIX_FRAMEBUFFER_EFFECTS = no		# Enable framebuffer effects
 
 LTO_ENABLE = yes
