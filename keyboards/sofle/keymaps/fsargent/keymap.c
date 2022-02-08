5f5a53ca1d5800ff9a4bf2a0ef289741b603724f
Felix Sargent <fsargent@users.noreply.github.com>
2022-02-07 23:35:34 -0800

    Added scrolling menu of layers

--- keyboards/sofle/keymaps/fsargent/keymap.c
+++ keyboards/sofle/keymaps/fsargent/keymap.c
@@ -18,7 +18,7 @@
 #include QMK_KEYBOARD_H
 
 bool reversed = false; // ADD this near the begining of keymap.c
-enum layers { BASE, WIN, GAME, SYM, NAV, WINNAV};
+enum layers { BASE, WIN, GAME, SYM, NAV, WINNAV, SHFT};
 
 
 #include "oled.c"
@@ -90,15 +90,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,		_______,	_______,	_______,	_______,	_______,				_______,_______,_______,_______,_______,KC_MINS,
 		KC_TAB,		_______,	_______,	_______,	_______,	_______,				_______,_______,_______,_______,_______,KC_EQL,
 		KC_ESC,		_______,	_______,	_______,	_______,	_______,				_______,_______,_______,_______,_______,KC_QUOT,
-		KC_LSFT,	_______,	_______,	_______,	_______,	_______,	ENCODER_SWAP,	TO(BASE),	_______,_______,_______,_______,_______,KC_BSLS,
+		KC_LSFT,	_______,	_______,	_______,	_______,	_______,	ENCODER_SWAP,	TO(SHFT),	_______,_______,_______,_______,_______,KC_BSLS,
 		KC_LCTL,	KC_LALT,	MO(WINNAV),	KC_LCTL,	KC_BSPC,						_______,_______,_______,_______,_______
+	),
+	[SHFT] = LAYOUT(
+		KC_GRV,		KC_5,		KC_1,		KC_2,		KC_3,		KC_4,							KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		MT(MOD_LGUI,KC_MINS),
+		KC_TAB,		KC_T,		KC_Q,		KC_W,		KC_E,		KC_R,							KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		MT(MOD_LCTL,KC_EQL),
+	MT(MOD_MEH,KC_ESC),	KC_G,		KC_A,		KC_S,		KC_D,		KC_F,							KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	MT(MOD_LALT,KC_QUOT),
+		KC_LSFT,	KC_B,		KC_Z,		KC_X,		KC_C,		KC_V,		ENCODER_SWAP,		TO(BASE),	KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	MT(MOD_LSFT,KC_BSLS),
+				MT(MOD_LCTL, KC_LBRC),	MT(MOD_LALT, KC_RBRC),	LT(SYM,KC_DEL), MO(NAV),	CMD_T(KC_BSPC),		KC_SPC,		LT(SYM, KC_ENT),	MT(MOD_LGUI, KC_DEL),	MT(MOD_LALT, KC_MINS), 	MT(MOD_LCTL, KC_EQL)
 	),
 	[SYM] = LAYOUT(
 		// C(G(KC_SPC)) is the OS X Emoji Editor
 		C(G(KC_SPC)),	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,							KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,
-		_______,	KC_EQL,		KC_7,		KC_8,		KC_9,		KC_MINUS,						KC_INS,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_F12,
-		_______,	KC_0, 		KC_4,		KC_5,		KC_6,		KC_PLUS,							KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_0,		KC_PSCR,
-		_______,	KC_DOT,		KC_1,		KC_2,		KC_3,		KC_ENT,		ENCODER_SWAP,		TG(SYM),	KC_MINS,	KC_EQL,		KC_LBRC,	KC_RBRC,	KC_BSLS,	KC_NLCK,
+		_______,	KC_EQL,		KC_7,		KC_8,		KC_9,		KC_MINUS,						KC_NO,		KC_NO,		KC_INS,		KC_NO,		KC_PSCR,	KC_F12,
+		_______,	KC_0, 		KC_4,		KC_5,		KC_6,		KC_PLUS,						KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NLCK,
+		_______,	KC_DOT,		KC_1,		KC_2,		KC_3,		KC_ENT,		ENCODER_SWAP,		TG(SYM),	KC_NLCK,	KC_SLCK,	KC_NO,		KC_NO,		KC_NO,		KC_SLCK,
 						_______,	KC_0,		KC_NO,		_______,	_______,		_______,	_______,	_______,	_______,	_______
 ),
 	[NAV]=LAYOUT(
@@ -114,5 +121,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LALT,	C(KC_LEFT),	KC_LEFT,	KC_DOWN,	KC_RGHT,	C(KC_RGHT),						KC_BTN3,	KC_MS_L,	KC_MS_D,	KC_MS_R,	KC_NO,		KC_LGUI,
 		KC_LSFT,	KC_HOME,	A(KC_LEFT),	KC_ENT,		A(KC_RGHT),	KC_END,		ENCODER_SWAP,		TG(GAME),	KC_WH_D,	KC_NO,		KC_LCBR,	KC_RCBR,	KC_BSLS,	KC_DEL,
 						_______,	_______,	_______,	_______,	_______,		KC_BTN1,	KC_BTN2,	KC_BTN3,	KC_LBRC,	KC_RBRC
-	)
+	),
+
 };
