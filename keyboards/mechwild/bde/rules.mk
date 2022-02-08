33dee210e24c4167b6fcc235e3da730288f3f19b
Kyle McCreery <mccreery.kyle@gmail.com>
2022-02-07 23:44:48 -0500

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechwild/bde/rules.mk
+++ keyboards/mechwild/bde/rules.mk
@@ -12,11 +12,9 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-key Rollover
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = no         # Enable encoder
-OLED_ENABLE = no            # Enable OLED Screen
 
 DEFAULT_FOLDER = mechwild/bde/rev2
