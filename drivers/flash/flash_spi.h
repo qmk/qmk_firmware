10b83e505bf17671b377fab6b6207910b25e2cb2
Joy Lee <chang.li@westberrytech.com>
2022-02-09 09:32:53 +0800

    Update drivers/flash/flash_spi.h
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- drivers/flash/flash_spi.h
+++ drivers/flash/flash_spi.h
@@ -71,10 +71,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /*
-    The sector size of the FLASH in Kbytes, as specified in the datasheet.
+    The sector size of the FLASH in bytes, as specified in the datasheet.
 */
 #ifndef EXTERNAL_FLASH_SECTOR_SIZE
-#    define EXTERNAL_FLASH_SECTOR_SIZE 4
+#    define EXTERNAL_FLASH_SECTOR_SIZE (4 * 1024)
 #endif
 
 /*
