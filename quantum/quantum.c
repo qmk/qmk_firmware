c9d500243a9fb5cce7ccad1d94ba2cd3a68235ae
Drashna Jael're <drashna@live.com>
2022-02-11 12:40:27 -0800

    Move no-compile option around

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -359,15 +359,14 @@ bool process_record_quantum(keyrecord_t *record) {
                 break;
 #endif
 #ifndef NO_COMPILE_KEYCODE
-#    if defined(QMK_KEYBOARD) && defined(QMK_KEYMAP)  // tests don't generate these defines
-            case QK_MAKE:                             // Compiles the firmware, and adds the flash command based on keyboard bootloader
+            case QK_MAKE:  // Compiles the firmware, and adds the flash command based on keyboard bootloader
             {
                 uint8_t temp_mod = mod_config(get_mods());
                 clear_mods();
-#        ifndef NO_ACTION_ONESHOT
+#    ifndef NO_ACTION_ONESHOT
                 temp_mod |= mod_config(get_oneshot_mods());
                 clear_oneshot_mods();
-#        endif
+#    endif
                 send_string_with_delay_P(PSTR("qmk"), TAP_CODE_DELAY);
                 if (temp_mod & MOD_MASK_SHIFT) {  // if shift is held, flash rather than compile
                     SEND_STRING_DELAY(" flash ", TAP_CODE_DELAY);
@@ -375,12 +374,11 @@ bool process_record_quantum(keyrecord_t *record) {
                     SEND_STRING_DELAY(" compile ", TAP_CODE_DELAY);
                 }
                 SEND_STRING_DELAY("-kb " QMK_KEYBOARD " -km " QMK_KEYMAP, TAP_CODE_DELAY);
-#        ifdef CONVERT_TO_PROTON_C  // If CTPC is set, ensure it stays set
+#    ifdef CONVERT_TO_PROTON_C  // If CTPC is set, ensure it stays set
                 SEND_STRING_DELAY(" -e CTPC=yes", TAP_CODE_DELAY);
-#        endif
+#    endif
                 SEND_STRING_DELAY(SS_TAP(X_ENTER), TAP_CODE_DELAY);
             }
-#    endif
 #endif
         }
     }
