bd35fa7de1323d572d530cfa4dbe9cf68cf1480b
peepeetee <43021794+peepeetee@users.noreply.github.com>
2022-02-12 05:51:20 +0800

    move @takashiski 's keebs into /takashiski (#16089)

--- keyboards/takashiski/hecomi/rules.mk
+++ keyboards/takashiski/hecomi/rules.mk
@@ -7,15 +7,15 @@ BOOTLOADER = caterina
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+SPLIT_KEYBOARD = yes
 
-#RGBLIGHT_ENABLE = yes # uncomment if you want addressable led strips
-
-DEFAULT_FOLDER = namecard2x4/rev2
+DEFAULT_FOLDER = takashiski/hecomi/alpha
