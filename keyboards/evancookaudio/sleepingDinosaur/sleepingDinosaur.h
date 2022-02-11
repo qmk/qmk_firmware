9f2bab87faf7513eaf9ac8d5c2c54858d23c0252
evanmcook <42785294+evanmcook@users.noreply.github.com>
2022-02-11 14:33:44 -0500

    layout descriptions

--- keyboards/evancookaudio/sleepingDinosaur/sleepingDinosaur.h
+++ keyboards/evancookaudio/sleepingDinosaur/sleepingDinosaur.h
@@ -25,7 +25,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT(\
+#define LAYOUT_ortho_5x6(\
     k00, k01, k02, k03, k04, k05, \
     k06, k07, k08, k09, k10, k11, \
     k12, k13, k14, k15, k16, k17, \
