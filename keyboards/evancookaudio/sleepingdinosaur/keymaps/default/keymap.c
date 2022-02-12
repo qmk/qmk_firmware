7a216aa01b749f2049b25ab2307e46042a8959f5
evanmcook <42785294+evanmcook@users.noreply.github.com>
2022-02-11 20:14:15 -0500

    Update keyboards/evancookaudio/sleepingdinosaur/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/evancookaudio/sleepingdinosaur/keymaps/default/keymap.c
+++ keyboards/evancookaudio/sleepingdinosaur/keymaps/default/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |       |   Z   |   X   |   C   |       |   N   |
  * ,-----------------------------------------------.
  */
-[0] = LAYOUT_ortho_5x6(
+[0] = LAYOUT(
   KC_1,       KC_2,      KC_3,      KC_4,      KC_5,      KC_MINS,
   KC_6,       KC_7,      KC_8,      KC_9,      KC_0,      KC_R,
   KC_SLSH,    KC_Q,      KC_W,      KC_E,      KC_U,      KC_V,
