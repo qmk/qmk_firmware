45a7cbd74f6fff6563210f1a64b63f7e480715af
holtenc <holtenc@gmail.com>
2022-02-13 18:14:50 -0600

    Update keyboards/handwired/obro/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/obro/rules.mk
+++ keyboards/handwired/obro/rules.mk
@@ -12,7 +12,6 @@ MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = yes       # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
-SLEEP_LED_ENABLE = no      # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes          # Enable N-key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
