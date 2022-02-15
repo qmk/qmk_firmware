d63c0572e0e0c577bb21afb94fe0dd8ad9f55acb
David J. Fiander <david@fiander.info>
2022-02-15 10:31:25 -0500

    Don't need the watchdog code after all.

--- keyboards/handwired/onekey/teensy_32/teensy_32.c
+++ keyboards/handwired/onekey/teensy_32/teensy_32.c
@@ -3,12 +3,3 @@
 void restart_usb_driver(USBDriver *usbp) {
     // Do nothing. Restarting the USB driver on these boards breaks it.
 }
-
-#define WDOG_TMROUTL *(volatile uint16_t *)0x40052012
-
-void early_hardware_init_pre(void) {
-    // This is a dirty hack and should only be used as a temporary fix until this
-    // is upstreamed.
-    while (WDOG_TMROUTL < 2)
-        ;  // Must wait for WDOG timer if already running, before jumping
-}
