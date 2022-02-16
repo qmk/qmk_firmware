d5e858fe3b878b14f8b2840b3d4835992afe3de7
toinux <toinux@gmail.com>
2022-02-17 00:51:12 +0100

    enable RGB_MATRIX effects

--- keyboards/crkbd/keymaps/toinux/rules.mk
+++ keyboards/crkbd/keymaps/toinux/rules.mk
@@ -1,6 +1,7 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
-BOOTLOADER      = atmel-dfu
+MOUSEKEY_ENABLE   = yes    # Mouse keys
+RGBLIGHT_ENABLE   = no
+RGB_MATRIX_ENABLE = yes
+OLED_ENABLE       = yes
+OLED_DRIVER       = SSD1306
+LTO_ENABLE        = yes
+BOOTLOADER        = atmel-dfu
