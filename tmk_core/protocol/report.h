aa3a5804806686bedcafee3279f0ca08c29da61c
Drashna Jael're <drashna@live.com>
2022-02-15 23:28:32 -0800

    liiiiiiint

--- tmk_core/protocol/report.h
+++ tmk_core/protocol/report.h
@@ -207,16 +207,16 @@ typedef struct {
 #endif
     uint8_t buttons;
 #ifdef MOUSE_EXT_REPORT
-    int8_t boot_x;
-    int8_t boot_y;
+    int8_t  boot_x;
+    int8_t  boot_y;
     int16_t x;
     int16_t y;
 #else
-    int8_t  x;
-    int8_t  y;
+    int8_t x;
+    int8_t y;
 #endif
-    int8_t  v;
-    int8_t  h;
+    int8_t v;
+    int8_t h;
 } __attribute__((packed)) report_mouse_t;
 
 typedef struct {
