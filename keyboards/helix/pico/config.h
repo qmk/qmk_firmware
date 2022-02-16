e40e7f31e2f2f92b80f7a7731ddbd8872cb8e282
mtei <2170248+mtei@users.noreply.github.com>
2022-02-17 01:20:34 +0900

    add licenses message to helix/pico files

--- keyboards/helix/pico/config.h
+++ keyboards/helix/pico/config.h
@@ -1,6 +1,7 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
+Copyright 2018 MakotoKurauchi
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
