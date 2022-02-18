fca3711f0f8f5c869a0587edc80c790f97c7e813
Danny Nguyen <danny@keeb.io>
2022-02-18 16:55:43 -0500

    Add Quefrency encoder defaults

--- keyboards/keebio/quefrency/rev4/rev4.c
+++ keyboards/keebio/quefrency/rev4/rev4.c
@@ -15,3 +15,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "quefrency.h"
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) { return false; }
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    return false;
+}
