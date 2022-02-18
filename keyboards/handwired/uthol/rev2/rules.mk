0d30dc4023f7dec0f58793e1011993d4efccd242
Uthol <83434905+Uthol@users.noreply.github.com>
2022-02-17 18:06:44 -0700

    Updates as per PR

---
+++ keyboards/handwired/uthol/rev2/rules.mk
@@ -0,0 +1,11 @@
+# MCU name
+MCU = atmega32u4
+
+BOOTLOADER = caterina
+
+BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes            # Nkey Rollover
+
+
+LTO_ENABLE = yes
---
+++ keyboards/handwired/uthol/rev3/rules.mk
@@ -0,0 +1,14 @@
+# MCU name
+MCU = STM32F401
+
+# Bootloader selection
+BOOTLOADER = stm32-dfu
+
+BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+KEYBOARD_SHARED_EP = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+WPM_ENABLE = yes
+EXTRAKEY_ENABLE = yes 
+# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
