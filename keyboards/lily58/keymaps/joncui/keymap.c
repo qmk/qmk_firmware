bc93df146555280db6eb308807e91ab017ca88ce
Jonathan Cui <joncui1101@gmail.com>
2022-02-18 14:41:53 -0800

    Undo up and down layers swap

--- keyboards/lily58/keymaps/joncui/keymap.c
+++ keyboards/lily58/keymaps/joncui/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL, \
                              KC_LCTL, KC_LGUI, MO(_DN), KC_BSPC, KC_SPC,  MO(_UP), KC_ENT,  KC_LALT \
   ),
-/* UP
+/* DN
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -49,18 +49,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCTRL| LAlt |  DN  | /BackSP /       \Space \  |  UP  |Enter | RGUI |
+ *                   | LCTRL| LGUI |  DN  | /BackSP /       \Space \  |  UP  |Enter | LALT |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-  [_UP] = LAYOUT( \
+  [_DN] = LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS, \
   KC_TRNS, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU,   KC_NO,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO, \
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, \
-                             KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RGUI \
+                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
   ),
-/* DN
+/* UP
  * ,-----------------------------------------.                    +------+------+------+------+------+------.
  * |      |      |      |      |      |      |                    |      |  7   |  8   |  9   |  /   |  *   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -70,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|      |      |      |      |      |-------|    |-------|      |  0   |  .   | Enter|      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCTRL| LGUI |  DN  | /BackSP /       \Space \  |  UP  |Enter | RAlt |
+ *                   | LCTRL| LGUI |  DN  | /BackSP /       \Space \  |  UP  |Enter | LAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-  [_DN] = LAYOUT( \
+  [_UP] = LAYOUT( \
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_PAST, \
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_PPLS, \
   KC_TRNS, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,                     KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM, KC_PEQL, \
@@ -114,12 +114,13 @@ static const char PROGMEM raw_logo[] = {
         0,  0,  0, 12, 12, 12, 12, 15, 15,  0,  0,  0,  0,  0,  1,  3,  7, 15, 29, 57, 49,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 };
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
   if (is_keyboard_master()) {
     render_layer_state();
   } else {
     oled_write_raw_P(raw_logo, sizeof(raw_logo));
   }
+  return false;
 }
 
 #endif // OLED_DRIVER_ENABLE
