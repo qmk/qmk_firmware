022371f54fb92da2929c8ba539ebcb928dfd1fd2
alabbe9545 <57238155+alabbe9545@users.noreply.github.com>
2022-02-19 22:29:59 +0100

    [Keyboard] Add labbeminiv1

---
+++ keyboards/labbe/labbeminiv1/keymaps/rgb/keymap.c
@@ -0,0 +1,28 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_wasd( KC_W, KC_A, KC_S, KC_D),
+};
+
+
+/*void matrix_init_user(void) {
+    // Set RGB to rainbow mood light
+    rgblight_enable();
+    rgblight_sethsv(255,255,255);
+    rgblight_mode(6);
+}*/
