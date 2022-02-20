09f0fb011ef3b173ce833c9b5077ca79730520c5
jack <applej955@gmail.com>
2022-02-20 02:14:34 -0700

    fix compilation issues

--- keyboards/hineybush/h08_ocelot/keymaps/via/keymap.c
+++ keyboards/hineybush/h08_ocelot/keymaps/via/keymap.c
@@ -27,16 +27,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
         KC_1, KC_2, KC_3, KC_4,
-        KC_5, KC_6, KC_7, MO(_FN)
+        KC_5, KC_6, KC_7, MO(_FN1)
     ),
     [_FN1] = LAYOUT(
         RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    )
+    ),
     [_FN2] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    )
+    ),
     [_FN3] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
