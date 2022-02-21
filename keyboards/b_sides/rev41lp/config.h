3f06880f153c567c7220c18e04b17184c6a1ab76
Jan Lindblom <janlindblom@fastmail.fm>
2022-02-21 10:56:01 +0200

    Update keyboards/b_sides/rev41lp/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/b_sides/rev41lp/config.h
+++ keyboards/b_sides/rev41lp/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    cyril279
 #define PRODUCT         rev41lp
-#define DESCRIPTION     A 41-key keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 7
