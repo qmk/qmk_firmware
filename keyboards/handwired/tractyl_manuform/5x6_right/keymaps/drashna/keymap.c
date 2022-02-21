1d77fbf3efc252f96ba866b2bd514e43fc307daf
Drashna Jael're <drashna@live.com>
2022-02-21 00:48:49 -0800

    Fix deferred encoder bug

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/keymap.c
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/keymap.c
@@ -150,8 +150,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 // clang-format on
 #else
 
-deferred_token encoder_token = INVALID_DEFERRED_TOKEN;
-static int8_t last_direction = -1;
+deferred_token encoder_token  = INVALID_DEFERRED_TOKEN;
+static int8_t  last_direction = -1;
 
 static uint32_t encoder_callback(uint32_t trigger_time, void *cb_arg) {
     unregister_code(last_direction ? KC_WH_D : KC_WH_U);
@@ -170,9 +170,11 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else if (index == 1) {
         if (last_direction != clockwise || encoder_token == INVALID_DEFERRED_TOKEN) {
             uint8_t keycode = clockwise ? KC_WH_D : KC_WH_U;
-            last_direction = clockwise;
+            last_direction  = clockwise;
             if (encoder_token != INVALID_DEFERRED_TOKEN) {
-                cancel_deferred_exec(encoder_token);
+                if (cancel_deferred_exec(encoder_token)) {
+                    encoder_token = INVALID_DEFERRED_TOKEN;
+                }
                 unregister_code(clockwise ? KC_WH_U : KC_WH_D);
             }
             register_code(keycode);
@@ -188,7 +190,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 #ifdef OLED_ENABLE
 extern uint16_t typing_mode;
 
-oled_rotation_t oled_init_keymap(oled_rotation_t rotation) { return OLED_ROTATION_180; }
+oled_rotation_t oled_init_keymap(oled_rotation_t rotation) {
+    return OLED_ROTATION_180;
+}
 
 void oled_render_large_display(void) {
     if (is_keyboard_left()) {
@@ -197,14 +201,16 @@ void oled_render_large_display(void) {
         oled_advance_page(true);
         oled_advance_page(true);
 
+        // clang-format off
         static const char PROGMEM logo[] = {
             0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
             0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
             0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
         };
+        // clang-format on
         oled_write_P(logo, false);
 
-#ifdef CUSTOM_UNICODE_ENABLE
+#    ifdef CUSTOM_UNICODE_ENABLE
         oled_set_cursor(1, 14);
         oled_write_ln_P(PSTR("Unicode:"), false);
         switch (typing_mode) {
@@ -233,7 +239,7 @@ void oled_render_large_display(void) {
                 oled_write_P(PSTR("     Unknown"), false);
                 break;
         }
-#endif
+#    endif
     }
 }
 #endif
