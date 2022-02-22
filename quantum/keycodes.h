57124b48ebb8e4c48ee35648cc8ada16165337f4
zvecr <git@zvecr.com>
2022-02-22 01:22:58 +0000

    Add shim

---
+++ quantum/keycodes.h
@@ -0,0 +1,8 @@
+// Copyright 2022 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+// TODO: handle data driven keycodes
+//       short term - shim to "legacy" headers
+#include "keycodes.h"
+#include "quantum_keycodes.h"
