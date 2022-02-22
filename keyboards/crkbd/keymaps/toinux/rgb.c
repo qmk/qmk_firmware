b118e3ed36aeec805e310020d23fdd8d22c59323
toinux <toinux@gmail.com>
2022-02-23 00:28:50 +0100

    Finish rgb config

--- keyboards/crkbd/keymaps/toinux/rgb.c
+++ keyboards/crkbd/keymaps/toinux/rgb.c
@@ -5,6 +5,7 @@
 #include "keycodes.h"
 
 static const char gaming_leds[] = {18, 22, 19, 16};
+static const char gaming2_leds[] = {23, 18, 17, 10, 9, 22, 19, 16, 11, 8};
 static const char nav_leds[] = {38, 43, 44, 46};
 static const char fun_leds[] = {45, 44, 37, 46, 43, 38, 47, 42, 39, 40};
 static const char mouse_leds[] = {11, 16, 17, 19};
@@ -21,25 +22,17 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         rgb_matrix_set_color(26, RGB_RED);
     }
     switch(get_highest_layer(layer_state|default_layer_state)) {
-        case _RAISE:
-            for (uint8_t i = led_min; i <= led_max; i++) {
-                if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-//                    rgb_matrix_set_color(i, RGB_BLUE);
-                }
-            }
-            break;
-        case _LOWER:
-            for (uint8_t i = led_min; i <= led_max; i++) {
-                if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-//                    rgb_matrix_set_color(i, RGB_GOLDENROD);
-                }
-            }
-            break;
         case _GAMING:
-        case _GAMING2:
             if (is_keyboard_master()) {
                 for (uint8_t i = 0; i < 4; i++) {
                     rgb_matrix_set_color(gaming_leds[i], RGB_RED);
+                }
+            }
+            break;
+        case _GAMING2:
+            if (is_keyboard_master()) {
+                for (uint8_t i = 0; i < 10; i++) {
+                    rgb_matrix_set_color(gaming2_leds[i], RGB_GREEN);
                 }
             }
             break;
