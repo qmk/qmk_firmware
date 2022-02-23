a1052146dd736fe78893dc0ae397a2d007608cc4
savaged <savagedavid@gmail.com>
2022-02-23 16:36:35 +0000

    easier

--- keyboards/idobo/keymaps/savaged/keymap.c
+++ keyboards/idobo/keymaps/savaged/keymap.c
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
  * | `      | A      | S      | D      | F      | G      | MENU   | VOL-   | H      | J      | K      | L      | ;      | '      | ENTER  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | [      | ]      | N      | M      | ,      | .      | /      | CAP LK | RSHIFT |
+ * | LSHIFT | Z      | X      | C      | V      | B      | [      | ]      | N      | M      | ,      | .      | /      | RALT   | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------+--------|
- * | LCTRL  | PG UP  | PG DN  | BACKSP | FN     | SPACE  | LGUI   | LGUI   | SPACE  | FN     | ENTER  | HOME   | END    | RALT   | RCTRL  |
+ * | LCTRL  | PG UP  | PG DN  | BACKSP | FN     | SPACE  | LGUI   | LGUI   | SPACE  | FN     | ENTER  | END    | HOME   | CAP LK | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,  KC_VOLU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LALT, KC_MUTE, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_EQL,
     KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_APP,  KC_VOLD, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_CAPS, KC_RSFT,
-    KC_LCTL, KC_PGUP, KC_PGDN, KC_BSPC, MO(_FN), KC_SPC,  KC_LGUI, KC_LGUI, KC_SPC,  MO(_FN), KC_ENT,  KC_HOME, KC_END,  KC_RALT, KC_RCTL
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT, KC_RSFT,
+    KC_LCTL, KC_PGUP, KC_PGDN, KC_BSPC, MO(_FN), KC_SPC,  KC_LGUI, KC_LGUI, KC_SPC,  MO(_FN), KC_ENT,  KC_END,  KC_HOME, KC_CAPS, KC_RCTL
  ),
 
 /* FUNCTION
