280ae44cfcf26c79d278d10673050ef624e3cdf6
takashicompany <t@kashi.company>
2022-02-25 19:50:12 +0900

    ENCODER RESOULUTION

--- keyboards/takashicompany/dogtag/config.h
+++ keyboards/takashicompany/dogtag/config.h
@@ -50,7 +50,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-#define ENCODER_RESOLUTION 1
+#define ENCODER_RESOLUTION 4
 #define ENCODERS_PAD_A { D4 }
 #define ENCODERS_PAD_B { C6 }
 
