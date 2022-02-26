009d7ddad36362ecf7bf7893047d1aa96607bdc1
peepeetee <peepeetee@protonmail.com>
2022-02-26 22:49:20 +0800

    sort out keymaps

---
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/via/keymap.c
@@ -0,0 +1,88 @@
+#include QMK_KEYBOARD_H
+
+enum my_keycodes {
+    RMT = SAFE_RANGE,
+    RMS,
+    RMIH,
+    RMDH,
+    RMIS,
+    RMDS,
+    RMIV,
+    RMDV
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RMT:
+      if (record->event.pressed)
+      {rgb_matrix_toggle();
+      }
+      return false;
+    case RMS:
+      if (record->event.pressed)
+      {rgb_matrix_step();
+      }
+      return false;
+      case RMIH:
+      if (record->event.pressed)
+      {rgb_matrix_increase_hue();
+      }
+      return false;
+      case RMDH:
+      if (record->event.pressed)
+      {rgb_matrix_decrease_hue();
+      }
+      return false;
+      case RMIS:
+      if (record->event.pressed)
+      {rgb_matrix_increase_sat();
+      }
+      return false;
+      case RMDS:
+      if (record->event.pressed)
+      {rgb_matrix_decrease_sat();
+      }
+      return false;
+      case RMIV:
+      if (record->event.pressed)
+      {rgb_matrix_increase_val();
+      }
+      return false;
+      case RMDV:
+      if (record->event.pressed)
+      {rgb_matrix_decrease_val();
+      }
+      return true;
+      default:
+      return true;
+  }
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT_planck_mit(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, 
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, 
+        KC_CAPS, KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,           KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  
+    ),
+
+    [1] = LAYOUT_planck_mit(
+        RESET,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______,  RMT,    RMS, 
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  
+    ),
+    [2] = LAYOUT_planck_mit(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  
+    ),
+    [3] = LAYOUT_planck_mit(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  
+    ),
+
+};
