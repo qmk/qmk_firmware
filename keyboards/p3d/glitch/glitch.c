7dbbab06d53a431c6969cf30361af71c5a1aa440
Matthew Dias <matthewdias@me.com>
2022-02-27 14:24:30 -0600

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/p3d/glitch/glitch.c
+++ keyboards/p3d/glitch/glitch.c
@@ -14,3 +14,45 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "glitch.h"
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
+    if (clockwise) {
+        // tap_code(RGB_MOD);
+        rgblight_step();
+    } else {
+        // tap_code(RGB_RMOD);
+        rgblight_step_reverse();
+    }
+
+    return false;
+}
+#endif
+
+#ifdef OLED_ENABLE
+bool oled_task_kb(void) {
+    if (!oled_task_user()) {
+        return false;
+    }
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        default:
+            oled_write_P(PSTR("Undefined\n"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
+}
