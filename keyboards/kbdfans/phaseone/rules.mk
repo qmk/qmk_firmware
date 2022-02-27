b10d1133b4abae1c021d6c9b04b7a74a9944055d
moyi4681 <moyi4681@users.noreply.github.com>
2022-02-27 20:58:37 +0800

    Update keyboards/kbdfans/phaseone/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kbdfans/phaseone/rules.mk
+++ keyboards/kbdfans/phaseone/rules.mk
@@ -15,5 +15,5 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
