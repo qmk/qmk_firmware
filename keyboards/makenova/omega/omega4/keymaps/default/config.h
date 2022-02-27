c1608d628516b8b17fb4de7a2fb1c1cf74033ae0
makenova <themarchmyth@gmail.com>
2022-02-27 00:51:43 -0700

    license update

--- keyboards/makenova/omega/omega4/keymaps/default/config.h
+++ keyboards/makenova/omega/omega4/keymaps/default/config.h
@@ -1,3 +1,6 @@
+// Copyright 2022 makenova (@makenova)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 #ifdef COMBO_ENABLE
--- keyboards/makenova/omega/omega4/keymaps/default/rules.mk
+++ keyboards/makenova/omega/omega4/keymaps/default/rules.mk
@@ -1,2 +1,1 @@
 COMBO_ENABLE = yes
-BOOTLOADER = caterina
