0c9baaa65d61f16801d475cc89226e9cf562cb49
peepeetee <43021794+peepeetee@users.noreply.github.com>
2022-02-28 10:38:21 +0800

    Update keyboards/momokai/tap_trio/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/momokai/tap_trio/config.h
+++ keyboards/momokai/tap_trio/config.h
@@ -40,8 +40,6 @@
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
-/*== all animations enable ==*/
-//  #define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
   #define RGBLIGHT_EFFECT_BREATHING
   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
