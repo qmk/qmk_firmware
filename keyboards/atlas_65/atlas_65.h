decb1d1945c17f6fdd9a0dff167877f95fe81883
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-02-28 19:56:41 -0800

    atlas_65.h: apply linting
    - convert tabs to spaces
      - four-space indent
    - align backslashes in layout macro

--- keyboards/atlas_65/atlas_65.h
+++ keyboards/atlas_65/atlas_65.h
@@ -35,16 +35,16 @@
  */
 
 #define LAYOUT( \
-	k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,           \
-	k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D,      k1E, \
-	k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C,           k2E, \
-	k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B,      k3D,      k3E,  \
-	k40,      k42,      k44, k45, k46, k47,      k49,           k4C, k4D, k4E       \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,           \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D,      k1E, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C,           k2E, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B,      k3D,      k3E, \
+    k40,      k42,      k44, k45, k46, k47,      k49,           k4C, k4D, k4E       \
 ) \
 { \
-	{ k00,   k01,   k02,   k03,   k04,   k05,   k06,   k07,   k08,   k09,   k0A,   k0B,   k0C,   k0D,   XXX }, \
-	{ k10,   k11,   k12,   k13,   k14,   k15,   k16,   k17,   k18,   k19,   k1A,   k1B,   k1C,   k1D,   k1E }, \
-	{ k20,   k21,   k22,   k23,   k24,   k25,   k26,   k27,   k28,   k29,   k2A,   k2B,   k2C,   XXX,   k2E }, \
-	{ k30,   k31,   k32,   k33,   k34,   k35,   k36,   k37,   k38,   k39,   k3A,   k3B,   XXX,   k3D,   k3E }, \
-	{ k40,   XXX,   k42,   XXX,   k44,   k45,   k46,   k47,   XXX,   k49,   XXX,   XXX,   k4C,   k4D,   k4E } \
+    { k00,   k01,   k02,   k03,   k04,   k05,   k06,   k07,   k08,   k09,   k0A,   k0B,   k0C,   k0D,   XXX }, \
+    { k10,   k11,   k12,   k13,   k14,   k15,   k16,   k17,   k18,   k19,   k1A,   k1B,   k1C,   k1D,   k1E }, \
+    { k20,   k21,   k22,   k23,   k24,   k25,   k26,   k27,   k28,   k29,   k2A,   k2B,   k2C,   XXX,   k2E }, \
+    { k30,   k31,   k32,   k33,   k34,   k35,   k36,   k37,   k38,   k39,   k3A,   k3B,   XXX,   k3D,   k3E }, \
+    { k40,   XXX,   k42,   XXX,   k44,   k45,   k46,   k47,   XXX,   k49,   XXX,   XXX,   k4C,   k4D,   k4E }  \
 }
