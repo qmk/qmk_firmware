0554933f1829ee97f89eb4249afa66a1720bbf4a
Andrew Garver <andrewsgarver@gmail.com>
2022-03-01 11:07:12 -0700

    updated enum def

--- keyboards/keebio/fourier/keymaps/via/keymap.c
+++ keyboards/keebio/fourier/keymaps/via/keymap.c
@@ -21,10 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _FN1 1
-#define _FN2 2
-#define _FN3 3
+enum custom_layers {
+  _BASE,
+  _FN1,
+  _FN2,
+  _FN3,
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
