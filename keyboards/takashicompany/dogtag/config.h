0e0ca7c126ad786227883f72b65549411e2fa059
takashicompany <t@kashi.company>
2022-03-01 17:42:13 +0900

    Update keyboards/takashicompany/dogtag/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/takashicompany/dogtag/config.h
+++ keyboards/takashicompany/dogtag/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      DogTag
 
 /* key matrix size */
-#define MATRIX_ROWS 3 * 2
+#define MATRIX_ROWS 6
 #define MATRIX_COLS 5
 
 /*
