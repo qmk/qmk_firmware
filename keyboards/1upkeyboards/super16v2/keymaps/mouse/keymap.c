b346ded49fd23be22c37b9bdf25b993c1124f80a
ziptyze <mwcordier@gmail.com>
2022-03-02 10:59:12 -0500

    super16 update

--- keyboards/1upkeyboards/super16v2/keymaps/mouse/keymap.c
+++ keyboards/1upkeyboards/super16v2/keymaps/mouse/keymap.c
@@ -18,10 +18,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho_4x4( /* Base */
-    KC_P7,    KC_P8,    KC_P9,    KC_PMNS,
+    KC_TRNS,    KC_DELETE,    KC_BACKSPACE,    KC_MUTE,
     KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
     KC_P1,    KC_P2,    KC_P3,    KC_PSLS,
-    MO(1),    KC_P0,    KC_PDOT,  KC_PENT
+    MO(1),    KC_P0,    KC_PDOT,  KC_BTN3
   ),
 
   [1] = LAYOUT_ortho_4x4( /* Fn Layer */
@@ -35,15 +35,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder */
         if (clockwise) {
-            tap_code_delay(KC_VOLU, 10);
+            tap_code_delay(KC_MS_RIGHT, 10);
         } else {
-            tap_code_delay(KC_VOLD, 10);
+            tap_code_delay(KC_MS_LEFT, 10);
         }
     } else if (index == 1) { /* Second encoder */
         if (clockwise) {
-            rgb_matrix_increase_hue();
+            tap_code_delay(KC_AUDIO_VOL_UP, 10);
         } else {
-            rgb_matrix_decrease_hue();
+            tap_code_delay(KC_AUDIO_VOL_DOWN, 10);
         }
     }
     return false;
