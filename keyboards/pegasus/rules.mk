ff45cee43ea511b2f7065c84cb1ec1a22a738cb0
melonbred <61572569+melonbred@users.noreply.github.com>
2022-03-01 18:40:06 -0800

    initial commit

---
+++ keyboards/pegasus/rules.mk
@@ -0,0 +1,24 @@
+# MCU name
+MCU = atmega32u4
+
+# Build Options
+#   comment out to disable the options.
+#
+
+BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = no	# Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+CUSTOM_MATRIX = no # Custom matrix file
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+WS2812_DRIVER = no
+ENCODER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+
+
+
