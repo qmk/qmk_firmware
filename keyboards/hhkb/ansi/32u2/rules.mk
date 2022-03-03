e6a6d1b03f8324aee1c483d9be1afca38dc52224
Jels-Kb <joahnelson@gmail.com>
2022-03-03 16:13:32 +1000

    hhkb-jels
    Support for an atmega32u2 based hhkb controller

---
+++ keyboards/hhkb/ansi/32u2/rules.mk
@@ -0,0 +1,7 @@
+# MCU name
+MCU = atmega32u2
+
+MOUSEKEY_ENABLE = no  # Mouse keys
+CONSOLE_ENABLE = no   # Console for debug
+COMMAND_ENABLE = no   # Commands for debug and configuration
+RGBLIGHT_ENABLE = yes
