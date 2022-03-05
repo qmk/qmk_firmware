868bc6e980eb013597a929f47680698aea15b083
fauxpark <fauxpark@gmail.com>
2022-03-05 13:04:57 +1100

    Make linter go away

--- lib/python/qmk/keyboard.py
+++ lib/python/qmk/keyboard.py
@@ -216,7 +216,7 @@ def render_key_rect(textpad, x, y, w, h, label, style):
 
     label_blank = ' ' * label_len
     label_border = box_chars['h'] * label_len
-    label_middle = label + ' '*label_leftover # noqa: yapf insists there be no whitespace around *
+    label_middle = label + ' '*label_leftover  # noqa: yapf insists there be no whitespace around *
 
     top_line = array('u', box_chars['tl'] + label_border + box_chars['tr'])
     lab_line = array('u', box_chars['v'] + label_middle + box_chars['v'])
@@ -243,10 +243,10 @@ def render_key_isoenter(textpad, x, y, w, h, label, style):
     if len(label) > label_len:
         label = label[:label_len]
 
-    label_blank = ' ' * (label_len-1) # noqa: yapf insists there be no whitespace around - and *
+    label_blank = ' ' * (label_len-1)  # noqa: yapf insists there be no whitespace around - and *
     label_border_top = box_chars['h'] * label_len
-    label_border_bottom = box_chars['h'] * (label_len-1) # noqa
-    label_middle = label + ' '*label_leftover # noqa
+    label_border_bottom = box_chars['h'] * (label_len-1)  # noqa
+    label_middle = label + ' '*label_leftover  # noqa
 
     top_line = array('u', box_chars['tl'] + label_border_top + box_chars['tr'])
     lab_line = array('u', box_chars['v'] + label_middle + box_chars['v'])
