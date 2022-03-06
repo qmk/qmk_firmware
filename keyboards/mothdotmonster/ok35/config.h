2aafeefe28048660b011e4f53a9aed3d7dc41772
Chesapeake <root@moth.monster>
2022-03-05 22:48:10 -0500

    add ok35

---
+++ keyboards/mothdotmonster/ok35/config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x86FE
+#define PRODUCT_ID      0x0035
+#define DEVICE_VER      0x0000
+#define MANUFACTURER    mothdotmonster
+#define PRODUCT         OK35
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 7
+
+// wiring of each half
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6, F5 }
+#define SPLIT_HAND_PIN F4
+#define SOFT_SERIAL_PIN D0
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
---
+++ keyboards/mothdotmonster/ok35/readme.md
@@ -0,0 +1,15 @@
+# OK35
+
+![OK35](https://i.redd.it/o9gc51mxlol81.jpg)
+
+A 5x14 split ortholinear keyboard.
+
+* Keyboard Maintainer: mothdotmonster
+* Hardware Supported: Pro Micro and compatibles
+* Hardware Availability: TBA
+
+Make example for this keyboard (after setting up your build environment):
+
+    make mothdotmonster/ok35:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
