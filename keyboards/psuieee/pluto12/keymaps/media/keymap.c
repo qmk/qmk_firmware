f1b998edd46f300495fe4aa97d3bf88047998e4b
Will McGloughlin <3410869+wymcg@users.noreply.github.com>
2022-03-05 19:57:06 -0500

    Expanded media keymap

--- keyboards/psuieee/pluto12/keymaps/media/keymap.c
+++ keyboards/psuieee/pluto12/keymaps/media/keymap.c
@@ -5,29 +5,52 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
+    _NAV,
+    _PROD,
+    _BASE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(
-        KC_PGUP,   KC_MUTE,   KC_VOLD,   KC_VOLU,
-        KC_PGDN,   KC_MPLY,   KC_MPRV,   KC_MNXT,
-        KC_NO,     KC_HOME,   KC_END,    OSL(_FN)
-    ),
-    [_FN] = LAYOUT(
-        KC_F1,     KC_F2,     KC_F3,     KC_F4,
-        KC_F5,     KC_F6,     KC_F7,     KC_F8,
-        KC_F9,     KC_F10,    KC_F11,    KC_F12
-    )
+  [_BASE] = LAYOUT(
+    KC_MUTE,    KC_MPRV,    KC_MPLY,    KC_MNXT,
+    TO(_NAV),   G(KC_1),    G(KC_2),    G(KC_3),
+    TO(_PROD),  G(KC_4),    G(KC_5),    G(KC_6)
+  ),
+  [_NAV] = LAYOUT(
+    TO(_BASE),  KC_HOME,    KC_UP,      KC_END,
+    A(KC_TAB),  KC_LEFT,    KC_DOWN,    KC_RIGHT,
+    G(KC_TAB),  KC_PGDN,    KC_PGUP,    MO(_BASE)
+  ),
+  [_PROD] = LAYOUT(
+    TO(_BASE),  KC_NO,      KC_NO,      KC_NO,
+    C(KC_Z),    C(KC_X),    C(KC_C),    C(KC_V),
+    C(KC_A),    C(KC_LEFT), C(KC_RIGHT),MO(_BASE)
+  )
 };
 
-
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-      tap_code(KC_UP);
-    } else {
-      tap_code(KC_DOWN);
+    switch (biton32(layer_state)) {
+        case _BASE:
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
+            break;
+        case _NAV:
+            if (clockwise) {
+                tap_code16(C(G(KC_RIGHT)));
+            } else {
+                tap_code16(C(G(KC_LEFT)));
+            }
+            break;
+        case _PROD:
+            if (clockwise) {
+                tap_code16(C(KC_Y));
+            } else {
+                tap_code16(C(KC_Z));
+            }
+            break;
     }
-  return false;
+    return false;
 }
