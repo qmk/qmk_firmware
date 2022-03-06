9db28d6ece730b0fcd046f673f3204602d3fce9a
Diego Palacios <diepala@gmail.com>
2022-03-06 22:43:18 +0100

    Add cantor keyboard
    Add firmware for the Cantor keyboard

---
+++ keyboards/cantor/cantor.c
@@ -0,0 +1,4 @@
+// Copyright 2022 Diego Palacios (@diepala)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "cantor.h"
---
+++ keyboards/cantor/rules.mk
@@ -0,0 +1,4 @@
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = usart
