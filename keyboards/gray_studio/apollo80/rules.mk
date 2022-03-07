d2965fd36a33b18b7b34709e69468f82c9c3b884
ChrisBeswick <Chris-beswick@hotmail.co.uk>
2022-03-07 11:15:25 +0000

    Update keyboards/gray_studio/apollo80/rules.mk - Removed port declaration from comment on AUDIO_ENABLE variable
    Removed " on port C6" from the comment on the AUDIO_ENABLE variable.
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/gray_studio/apollo80/rules.mk
+++ keyboards/gray_studio/apollo80/rules.mk
@@ -15,4 +15,4 @@ COMMAND_ENABLE = no            # Commands for debug and configuration
 NKRO_ENABLE = yes              # USB Nkey Rollover
 BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes          # Enable keyboard RGB underglow
-AUDIO_ENABLE = no              # Audio output on port C6
+AUDIO_ENABLE = no              # Audio output
