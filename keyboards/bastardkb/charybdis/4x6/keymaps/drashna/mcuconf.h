ff6c70415c536e6435023627197550889ffa1501
Drashna Jaelre <drashna@live.com>
2022-03-06 22:12:17 -0800

    [Keymap] Drashna Mouse keys and oled updates (#16556)

--- keyboards/bastardkb/charybdis/4x6/keymaps/drashna/mcuconf.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/drashna/mcuconf.h
@@ -17,7 +17,6 @@
 #pragma once
 
 #include_next "mcuconf.h"
-
 #undef STM32_I2C_USE_I2C1
 #define STM32_I2C_USE_I2C1 TRUE
 
@@ -33,10 +32,10 @@
 #define STM32_PWM_USE_TIM3 TRUE
 
 #undef STM32_SPI_USE_SPI1
-#define STM32_SPI_USE_SPI1           TRUE
+#define STM32_SPI_USE_SPI1 TRUE
 
-#undef STM32_SERIAL_USE_USART2
-#define STM32_SERIAL_USE_USART2 TRUE
+#undef STM32_SERIAL_USE_USART1
+#define STM32_SERIAL_USE_USART1 TRUE
 
 #undef STM32_GPT_USE_TIM4
 #define STM32_GPT_USE_TIM4 TRUE
