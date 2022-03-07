e5c8079a70306c8f0d715ca6370bf5a095c45cef
Uthol <83434905+Uthol@users.noreply.github.com>
2022-03-06 21:43:40 -0700

    Update keyboards/handwired/uthol/rev2/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/uthol/rev2/rules.mk
+++ keyboards/handwired/uthol/rev2/rules.mk
@@ -1,5 +1,12 @@
+# MCU name
 MCU = atmega32u4
+
+# Bootloader selection
 BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 NKRO_ENABLE = yes
