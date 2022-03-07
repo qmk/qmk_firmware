2b0de4327eb5fdb55c048e183f43acfd8a324a64
Uthol <83434905+Uthol@users.noreply.github.com>
2022-03-06 21:43:13 -0700

    Update keyboards/handwired/uthol/rev3/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/uthol/rev3/rules.mk
+++ keyboards/handwired/uthol/rev3/rules.mk
@@ -1,5 +1,12 @@
+# MCU name
 MCU = STM32F401
+
+# Bootloader selection
 BOOTLOADER = stm32-dfu
+
+# Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = yes
 NKRO_ENABLE = yes
 KEYBOARD_SHARED_EP = yes
