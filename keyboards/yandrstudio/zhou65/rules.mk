d51e35a25df5e44f211acf85f067a45fc3dca614
JX <572673807@qq.com>
2022-03-08 12:44:18 +0800

    Update keyboards/yandrstudio/zhou65/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/yandrstudio/zhou65/rules.mk
+++ keyboards/yandrstudio/zhou65/rules.mk
@@ -1,11 +1,12 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
-KEYBOARD_SHARED_EP = no    # Free up some extra endpoints - needed if console+mouse+extra
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-NO_USB_STARTUP_CHECK = no
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
