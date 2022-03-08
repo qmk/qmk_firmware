cec6fa988eb165a1806c027da5cf0a1286691e10
Mariappan Ramasamy <947300+Mariappan@users.noreply.github.com>
2022-03-08 18:56:22 +0800

    Add STeMCell controller board config

---
+++ platforms/chibios/boards/STEMCELL/configs/chconf.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define CH_CFG_ST_RESOLUTION 16
+#define CH_CFG_ST_FREQUENCY 10000
+
+#include_next <chconf.h>
--- platforms/chibios/pin_defs.h
+++ platforms/chibios/pin_defs.h
@@ -53,6 +53,63 @@
 #        define D5 PAL_LINE(GPIOC, 13)
 #        define B0 PAL_LINE(GPIOC, 14)
 #    endif
+
+#elif defined(CONVERT_TO_STEMCELL)
+
+/* #if SOFT_SERIAL_PIN==D0 || SOFT_SERIAL_PIN==D1
+#    define SERIAL_USART_DRIVER      SD1
+#    define USART1_REMAP
+#elif  SOFT_SERIAL_PIN==D2 || SOFT_SERIAL_PIN==D3
+#    define SERIAL_USART_DRIVER      SD2
+#    define USART2_REMAP
+#endif */
+
+// Left side (front)
+#    ifdef CONVERT_TO_STEMCELL_UART_SWAP
+#        define D3 PAL_LINE(GPIOA, 3)
+#        define D2 PAL_LINE(GPIOA, 2)
+#    else
+#        define D3 PAL_LINE(GPIOA, 2)
+#        define D2 PAL_LINE(GPIOA, 3)
+#    endif
+//      GND
+//      GND
+#    ifdef CONVERT_TO_STEMCELL_I2C_SWAP
+#        define D1 PAL_LINE(GPIOB, 6)
+#        define D0 PAL_LINE(GPIOB, 7)
+#    else
+#        define D1 PAL_LINE(GPIOB, 7)
+#        define D0 PAL_LINE(GPIOB, 6)
+#    endif
+
+#    define D4 PAL_LINE(GPIOA, 15)
+#    define C6 PAL_LINE(GPIOB, 3)
+#    define D7 PAL_LINE(GPIOB, 4)
+#    define E6 PAL_LINE(GPIOB, 5)
+#    define B4 PAL_LINE(GPIOB, 8)
+#    define B5 PAL_LINE(GPIOB, 9)
+
+// Right side (front)
+//      RAW
+//      GND
+//      RESET
+//      VCC
+#    define F4 PAL_LINE(GPIOB, 10)
+#    define F5 PAL_LINE(GPIOB, 2)
+#    define F6 PAL_LINE(GPIOB, 1)
+#    define F7 PAL_LINE(GPIOB, 0)
+#    define B1 PAL_LINE(GPIOA, 7)
+#    define B3 PAL_LINE(GPIOA, 6)
+#    define B2 PAL_LINE(GPIOA, 5)
+#    define B6 PAL_LINE(GPIOA, 4)
+
+#    define D5 PAL_LINE(GPIOA, 8)
+#    define B0 PAL_LINE(GPIOA, 9) // unconnected pin
+
+#    define SERIAL_USART_DRIVER      SD1
+#    define I2C1_SCL_PIN             D0
+#    define I2C1_SDA_PIN             D1
+
 #else
 #    define A0 PAL_LINE(GPIOA, 0)
 #    define A1 PAL_LINE(GPIOA, 1)
