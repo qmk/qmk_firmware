f8497c0bcc65429ea432c21c0d17b6ef4596ce0b
hunter <hcdarrach@gmail.com>
2022-03-10 14:21:10 -0500

    Update keyboards/wavtype/p01_ultra/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/wavtype/p01_ultra/config.h
+++ keyboards/wavtype/p01_ultra/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x03a7
+#define VENDOR_ID    0x03A7
 #define PRODUCT_ID   0x0004
 #define DEVICE_VER   0x0001
 #define MANUFACTURER wavtype
