2089e775d5d92cb1e6c260677125b842f89c491a
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2022-03-10 21:15:34 -0700

    Update keyboards/clickety_split/leeloo/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/clickety_split/leeloo/rules.mk
+++ keyboards/clickety_split/leeloo/rules.mk
@@ -13,23 +13,10 @@ MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = no           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no           # Enable N-Key rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no          # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no     # Enable one-hand typing
-TAP_DANCE_ENABLE = no      # Enable Tap Dance
-COMBO_ENABLE = no          # Enable Combos
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no      # Breathing sleep LED during USB suspend
-
-# To enable Rotary Encoders
-ENCODER_ENABLE = no
-
-# To enable OLED Displays
-OLED_ENABLE = no
-OLED_DRIVER = SSD1306      # OLED display
 
 SPLIT_KEYBOARD = yes
 DEFAULT_FOLDER = clickety_split/leeloo/rev1
