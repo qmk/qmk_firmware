81c7ca1ca5e37879b9a44d321b427dd85c272586
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2022-03-10 21:15:58 -0700

    Update keyboards/clickety_split/leeloo/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/clickety_split/leeloo/rules.mk
+++ keyboards/clickety_split/leeloo/rules.mk
@@ -5,8 +5,7 @@ MCU = atmega32u4
 BOOTLOADER = caterina       # Pro Micro
 
 # Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
+#   change to "no" to disable the options
 #
 BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no       # Mouse keys
