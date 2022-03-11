8a1266c8eaa8976f1f89e334fabdde788fcc06a2
Droxx <101306764+furfuzz@users.noreply.github.com>
2022-03-10 17:13:38 -0700

    fixed rules.mk

--- keyboards/kbdfans/kbd67/rev2/keymaps/droxx/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/keymaps/droxx/rules.mk
@@ -1,6 +1,3 @@
-# Build Options
-#   change yes to no to disable
-#
-MOUSEKEY_ENABLE = no        # Mouse keys (+4700)
-BACKLIGHT_ENABLE = no      	# Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = yes       # Enable keyboard underglow functionality
+MOUSEKEY_ENABLE = no
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
