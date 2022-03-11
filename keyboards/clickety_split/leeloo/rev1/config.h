bf5a9963b6b2767184d81df5d62271b057d6a519
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2022-03-10 21:18:58 -0700

    Update keyboards/clickety_split/leeloo/rev1/config.h
    Reference to this define will be removed in default/keymap.c.
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/clickety_split/leeloo/rev1/config.h
+++ keyboards/clickety_split/leeloo/rev1/config.h
@@ -25,7 +25,6 @@
 #define DEVICE_VER      0x0013
 #define MANUFACTURER    Clickety Split Ltd
 #define PRODUCT         Leeloo
-#define LEELOO_VERSION  "v1.0"
 
 /* key matrix size */
 // Rows are doubled-up
