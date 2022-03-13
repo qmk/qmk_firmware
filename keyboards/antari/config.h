ae3c1359ed2feaca53a9c164acd276252b0257c1
nendezkombet <82454371+nendezkombet@users.noreply.github.com>
2022-03-13 23:31:01 +0700

    Update config.h
    VID and PID added

--- keyboards/antari/config.h
+++ keyboards/antari/config.h
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x458F
+#define PRODUCT_ID   0x6F82
 #define DEVICE_VER   0x0001
 #define MANUFACTURER nendezkombet/sandipratama
 #define PRODUCT      ANTARI
