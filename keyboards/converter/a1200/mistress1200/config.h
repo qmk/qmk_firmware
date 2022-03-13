0c9801cd6dad49bfd49616f01538bf6e0118843e
8bits4ever <87928173+8bits4ever@users.noreply.github.com>
2022-03-13 17:24:56 +0100

    Update config.h

--- keyboards/converter/a1200/mistress1200/config.h
+++ keyboards/converter/a1200/mistress1200/config.h
@@ -58,5 +58,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define DISABLE_LEADER
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
