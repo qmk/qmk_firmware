40be67599fb930f40ec28cf725be731874bd5b17
Ele Thompson <ele.thompson12@gmail.com>
2022-03-12 20:34:54 -0800

    Updated bacca70 gpl license year

--- keyboards/bacca70/config.h
+++ keyboards/bacca70/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 keebnewb
+Copyright 2022 keebnewb
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
