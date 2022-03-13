375813f2ffd5bdb92c99d80208826dc20f88b52f
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2022-03-13 17:34:40 +0900

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/yd3xn15mx/rules.mk
+++ keyboards/yd3xn15mx/rules.mk
@@ -10,23 +10,13 @@ BOOTLOADER = atmel-dfu
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-
-# Debug options
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-
-# RGB Options
-#RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
-
 RGBLIGHT_SUPPORTED = yes
 RGB_MATRIX_SUPPORTED = yes
-
-AUDIO_ENABLE = no           # Audio output
-
-# Encoders
-ENCODER_ENABLE = no 		# Enable support for Rotary Encoder
