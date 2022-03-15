14561ce7aee134774d15ca8cd825c22cac9442e5
jack <0x6A73@pm.me>
2022-03-14 20:44:46 -0600

    initial

--- keyboards/evancookaudio/sleepingdinosaur/keymaps/via/keymap.c
+++ keyboards/evancookaudio/sleepingdinosaur/keymaps/via/keymap.c
@@ -15,8 +15,6 @@
 */
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
 /* LAYER 0
  * ,-----------------------------------------------.
  * |   1   |   2   |   3   |   4   |   5   |   -   |
@@ -30,32 +28,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |       |   Z   |   X   |   C   |       |   N   |
  * ,-----------------------------------------------.
  */
-LAYOUT(
-  KC_1,       KC_2,      KC_3,      KC_4,      KC_5,      KC_MINS,
-  KC_6,       KC_7,      KC_8,      KC_9,      KC_0,      KC_R,
-  LT(1,KC_SLSH), KC_Q,   KC_W,      KC_E,      KC_U,      KC_V,
-  KC_ENT,     KC_A,      KC_S,      KC_D,      KC_SPC,    KC_O,
-              KC_Z,      KC_X,      KC_C,                 KC_N     
-)
-LAYOUT(
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-              KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS   
-)
-LAYOUT(
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-              KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS   
-)
-LAYOUT(
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-              KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS   
-)
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT(
+    KC_1,       KC_2,      KC_3,      KC_4,      KC_5,      KC_MINS,
+    KC_6,       KC_7,      KC_8,      KC_9,      KC_0,      KC_R,
+    LT(1,KC_SLSH), KC_Q,   KC_W,      KC_E,      KC_U,      KC_V,
+    KC_ENT,     KC_A,      KC_S,      KC_D,      KC_SPC,    KC_O,
+                KC_Z,      KC_X,      KC_C,                 KC_N     
+  ),
+  [1] = LAYOUT(
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+                KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS   
+  ),
+  [2] = LAYOUT(
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+                KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS   
+  ),
+  [3] = LAYOUT(
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+                KC_TRNS,   KC_TRNS,   KC_TRNS,              KC_TRNS   
+  )
 };
