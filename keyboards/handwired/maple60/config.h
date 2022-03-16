61e08bb6f776e98a2102fc9dfb503a1f0661ae0e
Toofty5 <jay.cheung@gmail.com>
2022-03-16 11:43:03 -0400

    Update keyboards/handwired/maple60/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/maple60/config.h
+++ keyboards/handwired/maple60/config.h
@@ -23,7 +23,7 @@
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Jay
-#define PRODUCT         handwired/maple60
+#define PRODUCT         Maple60
 
 /* key matrix size */
 #define MATRIX_ROWS 5
