c787a3e8ca12a3891b8b1b25c09916171479e5f3
sharkbyte <sgquin@gmail.com>
2022-03-16 12:49:32 -0700

    removed autoshift and lctl on s

--- keyboards/crkbd/keymaps/sharkby7e/rules.mk
+++ keyboards/crkbd/keymaps/sharkby7e/rules.mk
@@ -1,6 +1,7 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
-BOOTLOADER = caterina
+MOUSEKEY_ENABLE   = yes    # Mouse keys
+RGBLIGHT_ENABLE   = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE       = yes
+OLED_DRIVER       = SSD1306
+LTO_ENABLE        = yes
+BOOTLOADER        = caterina
+# AUTO_SHIFT_ENABLE = yes
