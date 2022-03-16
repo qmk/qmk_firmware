1d552b19110fb5cc90bd5b26a5e04a9512fe0318
Mads Kiilerich <mads@kiilerich.com>
2022-03-16 16:34:04 +0100

    qmk info: Show label centered on the key
    This will in most cases make the layout more readable - especially now
    when the keys are rendered wider.
    
    Using str.center also happens to simplify the code.

--- lib/python/qmk/keyboard.py
+++ lib/python/qmk/keyboard.py
@@ -211,14 +211,9 @@ def render_key_rect(textpad, x, y, w, h, label, style):
     h = ceil(h * 3)
 
     label_len = w - 2
-    label_leftover = label_len - len(label)
-
-    if len(label) > label_len:
-        label = label[:label_len]
-
     label_blank = ' ' * label_len
     label_border = box_chars['h'] * label_len
-    label_middle = label + ' '*label_leftover  # noqa: yapf insists there be no whitespace around *
+    label_middle = label.center(label_len)[:label_len]
 
     top_line = array('u', box_chars['tl'] + label_border + box_chars['tr'])
     lab_line = array('u', box_chars['v'] + label_middle + box_chars['v'])
@@ -240,15 +235,10 @@ def render_key_isoenter(textpad, x, y, w, h, label, style):
     h = ceil(h * 3)
 
     label_len = w - 1
-    label_leftover = label_len - len(label)
-
-    if len(label) > label_len:
-        label = label[:label_len]
-
     label_blank = ' ' * (label_len-1)  # noqa: yapf insists there be no whitespace around - and *
     label_border_top = box_chars['h'] * label_len
     label_border_bottom = box_chars['h'] * (label_len-1)  # noqa
-    label_middle = label + ' '*label_leftover  # noqa
+    label_middle = label.center(label_len)[:label_len]
 
     top_line = array('u', box_chars['tl'] + label_border_top + box_chars['tr'])
     lab_line = array('u', box_chars['v'] + label_middle + box_chars['v'])
