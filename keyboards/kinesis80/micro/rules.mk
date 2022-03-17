db8dce4878b014b6de9f2eff7a0cd8cb6e99b0c6
Dmitry Nosachev <quartz64@gmail.com>
2022-03-17 16:44:20 +0300

    clean up

--- keyboards/kinesis80/micro/rules.mk
+++ keyboards/kinesis80/micro/rules.mk
@@ -1,11 +1,3 @@
 MCU = atmega32u4
-
-# Bootloader selection
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu: You can redefine 'bootloader exit' key and LED pin in config.h
-#   atmega32a    bootloadHID
 BOOTLOADER = lufa-dfu
-
 FIRMWARE_FORMAT = hex
