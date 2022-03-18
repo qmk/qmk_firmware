9a74764754e821d8ad0c305efd5923287e6948f3
Jouke Witteveen <j.witteveen@gmail.com>
2022-03-18 14:02:46 +0100

    Add GET_TAPPING_TERM macro to reduce duplicate code
    The macro gives the right tapping term depending on whether per-key
    tapping terms and/or dynamic tapping terms are enabled. Unnecessary
    function calls and variable resolution are avoided.
    
    Fixes #16472.

--- quantum/action_tapping.h
+++ quantum/action_tapping.h
@@ -44,3 +44,11 @@ bool     get_retro_tapping(uint16_t keycode, keyrecord_t *record);
 #ifdef DYNAMIC_TAPPING_TERM_ENABLE
 extern uint16_t g_tapping_term;
 #endif
+
+#ifdef TAPPING_TERM_PER_KEY
+#    define GET_TAPPING_TERM(keycode, record) get_tapping_term(keycode, record)
+#elif defined(DYNAMIC_TAPPING_TERM_ENABLE)
+#    define GET_TAPPING_TERM(keycode, record) g_tapping_term
+#else
+#    define GET_TAPPING_TERM(keycode, record) (TAPPING_TERM)
+#endif
