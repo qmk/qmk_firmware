4935114fdbcd037b1ce61b58143f2472b3b396f9
ChrisGVE <christian.berclaz@mac.com>
2022-03-20 12:01:58 +0800

    Adelais HHKB: Added a new layout for a quasi-HHKB

---
+++ keyboards/mechlovin/adelais/keymaps/chrisgve_hhkb/config.h
@@ -0,0 +1,19 @@
+/* #ifdef DEBOUNCE
+#    undef DEBOUNCE
+#endif */
+
+#define DEBOUNCE 5
+
+/* #ifdef TAPPING_TERM
+#    undef TAPPING_TERM
+#    define TAPPING_TERM 100
+#else
+#    define TAPPING_TERM 100
+#endif */
+
+/* #ifndef RETRO_TAPPING
+#    define RETRO_TAPPING
+#endif */
+
+#define PERMISSIVE_HOLD
+
---
+++ keyboards/mechlovin/adelais/keymaps/chrisgve_hhkb/rules.mk
@@ -0,0 +1,16 @@
+USER_NAME := chrisgve
+SRC += chrisgve.c
+
+LTO_ENABLE = yes
+# APPLE_FN_ENABLE = yes
+NKRO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+SLEEP_LED_ENABLE = no
+SHIFT_ENABLE = yes
+
+ENCODER_ENABLE = no
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
