fb3659de4178323095255f524bb47c2933fcdde9
peepeetee <43021794+peepeetee@users.noreply.github.com>
2022-03-20 12:24:41 +0800

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kprepublic/bm40hsrgb/rev2/config.h
+++ keyboards/kprepublic/bm40hsrgb/rev2/config.h
@@ -44,14 +44,10 @@
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION ROW2COL
-#define USB_POLLING_INTERVAL_MS 1
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
-/* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
 //rgb light setting
 #define RGBLIGHT_LIMIT_VAL 150
 #define RGBLED_NUM          6
@@ -59,8 +55,6 @@
 #define RGBLIGHT_HUE_STEP   8
 #define RGBLIGHT_SAT_STEP   8
 #define RGBLIGHT_VAL_STEP   8
-/*== all animations enable ==*/
-//  #define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
   #define RGBLIGHT_EFFECT_BREATHING
   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
