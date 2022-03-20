2dc6fe97e78d24bb05de091327373c8a58ca4498
N L <64655861+niolang@users.noreply.github.com>
2022-03-20 21:48:17 +0100

    Update keyboards/lily58/keymaps/niolang/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lily58/keymaps/niolang/config.h
+++ keyboards/lily58/keymaps/niolang/config.h
@@ -30,7 +30,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* RGB light */
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
 #    define RGBLIGHT_HUE_STEP 5 // number of steps to cycle through the hue by
 #    define RGBLIGHT_SAT_STEP 10 // number of steps to increment the saturation by
 #    define RGBLIGHT_VAL_STEP 10 // number of steps to increment the brightness by
