0e7f21f6338b62e2f36a40caffa7ed85fad0b558
Shandon Anderson <shandon@shandon.codes>
2022-03-21 11:55:19 -0400

    Changed field values

--- keyboards/mino/hotswap/rules.mk
+++ keyboards/mino/hotswap/rules.mk
@@ -2,13 +2,18 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = caterina #Make sure you change this if you are not using the kit Pro Micro
+BOOTLOADER = caterina
 
 # Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
 OLED_ENABLE = yes
 WPM_ENABLE = yes
