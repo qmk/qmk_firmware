582ad8276c3200f1f3c9aef96110f9f22bda9db9
mtei <2170248+mtei@users.noreply.github.com>
2022-03-24 16:54:27 +0900

    remove custom matrix.c and custom split_util.c

--- keyboards/helix/rev2/local_features.mk
+++ keyboards/helix/rev2/local_features.mk
@@ -40,24 +40,6 @@ ifneq ($(strip $(HELIX)),)
     SHOW_HELIX_OPTIONS = yes
 endif
 
-ifneq ($(strip $(SPLIT_KEYBOARD)), yes)
-  # In the very near future, all keymaps will be compatible with split_common and this block will be removed.
-  SRC += local_drivers/serial.c
-  KEYBOARD_PATHS += $(HELIX_TOP_DIR)/local_drivers
-
-  # A workaround until #7089 is merged.
-  #   serial.c must not be compiled with the -lto option.
-  #   The current LIB_SRC has a side effect with the -fno-lto option, so use it.
-  LIB_SRC += local_drivers/serial.c
-
-  CUSTOM_MATRIX = yes
-
-  SRC += rev2/custom/matrix.c
-  SRC += rev2/custom/split_util.c
-  SRC += rev2/custom/split_scomm.c
-  KEYBOARD_PATHS += $(HELIX_TOP_DIR)/rev2/custom
-endif
-
 ########
 # convert Helix-specific options (that represent combinations of standard options)
 #   into QMK standard options.
