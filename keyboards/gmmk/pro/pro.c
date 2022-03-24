83ddbfc83b2aecd3a01fd21375c2f0194d0aa04b
jack <0x6A73@pm.me>
2022-03-23 18:49:39 -0600

    formatting

--- keyboards/gmmk/pro/pro.c
+++ keyboards/gmmk/pro/pro.c
@@ -17,12 +17,14 @@
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-  if (!encoder_update_user(index, clockwise)) return false;
-  if (clockwise) {
-    tap_code_delay(KC_VOLU, 10);
-  } else {
-    tap_code_delay(KC_VOLD, 10);
-  }
-  return false;
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
+    if (clockwise) {
+        tap_code_delay(KC_VOLU, 10);
+    } else {
+        tap_code_delay(KC_VOLD, 10);
+    }
+    return false;
 }
 #endif
