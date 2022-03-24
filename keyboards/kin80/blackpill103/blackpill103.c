c43a53353ddeea1477b1548fc78df70b6c7991a4
Dmitry Nosachev <quartz64@gmail.com>
2022-03-24 15:11:31 +0300

    Update keyboards/kin80/blackpill103/blackpill103.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kin80/blackpill103/blackpill103.c
+++ keyboards/kin80/blackpill103/blackpill103.c
@@ -21,25 +21,8 @@ void matrix_init_kb(void) {
   //AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
   
   /* LED pins setup */
-  setPinOutput(NUM_LOCK_LED_PIN);
-  setPinOutput(CAPS_LOCK_LED_PIN);
-  setPinOutput(SCROLL_LOCK_LED_PIN);
   setPinOutput(LED4_PIN);
-  
-  writePinLow(NUM_LOCK_LED_PIN);
-  writePinLow(CAPS_LOCK_LED_PIN);
-  writePinLow(SCROLL_LOCK_LED_PIN);
-  writePinLow(LED4_PIN);
+    writePinLow(LED4_PIN);
 
 	matrix_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-bool res = led_update_user(led_state);
-    if(res) {
-        writePin(NUM_LOCK_LED_PIN, !led_state.num_lock);
-        writePin(CAPS_LOCK_LED_PIN, !led_state.caps_lock);
-        writePin(SCROLL_LOCK_LED_PIN, !led_state.scroll_lock);
-    }
-    return res;
 }
