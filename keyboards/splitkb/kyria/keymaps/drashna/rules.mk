a240a72bbd4344beb5007eaaa57ee97fc35c5f36
Drashna Jael're <drashna@live.com>
2022-03-24 20:38:42 -0700

    Remove debug matrix scanning from kyria

--- keyboards/splitkb/kyria/keymaps/drashna/rules.mk
+++ keyboards/splitkb/kyria/keymaps/drashna/rules.mk
@@ -21,7 +21,6 @@ ifeq ($(strip $(KEYBOARD)), splitkb/kyria/rev1/proton_c)
     AUTOCORRECTION_ENABLE  = yes
     CAPS_WORD_ENABLE       = yes
     OLED_DRIVER            = custom
-    DEBUG_MATRIX_SCAN_RATE_ENABLE = api
 else
     LTO_ENABLE             = yes
     BOOTLOADER             = qmk-hid
