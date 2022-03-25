95f49700ad2d004688035818dd4564f186c700fa
WestMProducts <WestMProducts@outlook.com>
2022-03-25 02:24:37 -0700

    updated macropad keymap

--- keyboards/westm/westm9/keymaps/default/keymap.c
+++ keyboards/westm/westm9/keymaps/default/keymap.c
@@ -17,14 +17,14 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_macropad( /* Base */
-        RGB_TOG, RGB_RMOD, RGB_MOD,
-        RGB_VAI, RGB_VAD, KC_6,
-        RGB_HUI, KC_1, KC_2
+    [0] = LAYOUT_macropad(
+        KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,
+        KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE,
+        KC_NO, KC_NO, MO(1)
     ),
-    [1] = LAYOUT_macropad( /* FN */
-        KC_1, KC_2, KC_3,
-        KC_4, KC_5, KC_6,
-        KC_7, KC_8, KC_9
+    [1] = LAYOUT_macropad(
+        RGB_TOG, RGB_RMOD, RGB_MOD,
+        RGB_VAD, RGB_VAI, KC_NO,
+        RGB_HUD, RGB_HUI, KC_NO
     )
 };
