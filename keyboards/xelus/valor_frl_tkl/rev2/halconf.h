7b2f28b51c2527cd0bda8423c908df7b39dd4863
xelus22 <preyas22@gmail.com>
2022-03-26 18:51:59 +1100

    remove i2c usage

--- keyboards/xelus/valor_frl_tkl/rev2/halconf.h
+++ keyboards/xelus/valor_frl_tkl/rev2/halconf.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#define HAL_USE_I2C TRUE
+// #define HAL_USE_I2C TRUE
 
 #include_next <halconf.h>
 
--- keyboards/xelus/valor_frl_tkl/rev2/rules.mk
+++ keyboards/xelus/valor_frl_tkl/rev2/rules.mk
@@ -17,7 +17,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-EEPROM_DRIVER = i2c
+# EEPROM_DRIVER = i2c
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
