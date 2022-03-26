e86f65e0af4b87a92c3021a1b01720f830283f86
Stephon Parker <stephonparker@Stephons-MacBook-Pro.local>
2022-03-26 10:31:24 -0400

    updated second layer keys

--- keyboards/mechwild/mercutio/keymaps/fearless_spiff_EN/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/fearless_spiff_EN/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           TD(TD_TAB_ESC),   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
           LT(1,KC_CAPSLOCK),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_PLUS,          KC_ENT,
           KC_LSFT, KC_LABK, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
-          KC_LCTL, KC_LGUI, KC_LALT,   KC_SPC,  KC_SPC,   KC_SPC,        KC_RALT, MO(2),          KC_RCTL ),
+          KC_LCTL, KC_LGUI, LT(2,KC_LALT),   KC_SPC,  KC_SPC,   KC_SPC,        KC_RALT, MO(2),          KC_RCTL ),
 
     [FN_1] = LAYOUT_all(
                                                                                                                        KC_MUTE,
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     	  KC_ESC,            KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,  KC_0,  KC_BSPC,
     	  _______,             KC_EXLM,    KC_AT,    KC_HASH,    KC_DLR,    KC_AMPR,    KC_PERC,    KC_ASTR,    KC_LPRN,    KC_RPRN,  _______, _______,
     	  _______, _______, KC_MINS, KC_PLUS, KC_BSLS, KC_SLSH, KC_SCLN, KC_COLN, KC_QUES, KC_LBRC, KC_RBRC, KC_DOT,
-    	  _______, KC_QUOT, KC_DQUO,          _______, _______,          _______,          _______, _______,          KC_PIPE )
+    	  KC_DQUO, KC_QUOT, KC_DQUO,          _______, _______,          _______,          _______, _______,          KC_PIPE )
 };
 
 #ifdef ENCODER_ENABLE
