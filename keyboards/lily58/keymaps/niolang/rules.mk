f79ed9e9f2e59f0c2029eea27e891834ccddcfac
N L <64655861+niolang@users.noreply.github.com>
2022-03-27 17:30:28 +0200

    Update keyboards/lily58/keymaps/niolang/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/lily58/keymaps/niolang/rules.mk
+++ keyboards/lily58/keymaps/niolang/rules.mk
@@ -1,7 +1,2 @@
 EXTRAKEY_ENABLE = yes
 OLED_ENABLE = yes
-
-# Alreadu enabled in lily58/light/rules.mk :
-#ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-#RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
-#TO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
