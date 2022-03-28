02ff631f2433c29161d0a697154f0df4a7c9b552
Simon <simon@benezan.de>
2022-03-28 13:26:00 +0200

    rename folder

--- keyboards/frooastboard/walnut/config.h
+++ keyboards/frooastboard/walnut/config.h
@@ -1,30 +1,7 @@
-/* Copyright 2022 frooastside
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2022 Simon Benezan (@Frooastside)
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #pragma once
-
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define MANUFACTURER frooastside
-#define PRODUCT Frooastboard Walnut
-
-/* key matrix size */
-#define MATRIX_ROWS 9
-#define MATRIX_COLS 8
 
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
--- keyboards/frooastboard/walnut/walnut.c
+++ keyboards/frooastboard/walnut/walnut.c
@@ -1,20 +1,7 @@
-/* Copyright 2022 frooastside
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2022 Simon Benezan (@Frooastside)
+// SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "walnut_69.h"
+#include "walnut.h"
 
 #if defined(RGB_MATRIX_ENABLE)
 
