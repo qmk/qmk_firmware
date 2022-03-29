bb83a8062662c29c966a2aab1c18ae5ba05b9f22
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2022-03-29 02:22:24 -0300

    Add proper per-key RGB support

--- keyboards/acheron/apollo87h/config.h
+++ keyboards/acheron/apollo87h/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x4150 // AP for AcheronProject
-#define PRODUCT_ID      0x8772 // For 87H
+#define PRODUCT_ID      0x8775 // For 87H + 3 (revision Delta)
 #define DEVICE_VER      0x0004 // Revision Delta
 #define MANUFACTURER    AcheronProject
 #define PRODUCT         Apollo87H
@@ -54,21 +54,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define STM32_HSECLK 8000000
 
 #define RGB_DI_PIN B15
-#define RGBLED_NUM 87
-#define RGBLIGHT_ANIMATIONS
-
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#define RGBLIGHT_DEFAULT_VAL 120
-#define RGBLIGHT_DEFAULT_MODE (RGBLIGHT_MODE_RAINBOW_SWIRL + 2)
+#define DRIVER_LED_TOTAL 87
 
 #define WS2812_PWM_COMPLEMENTARY_OUTPUT
 #define WS2812_PWM_DRIVER PWMD1
@@ -76,6 +62,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define WS2812_PWM_PAL_MODE 1
 #define WS2812_DMA_STREAM STM32_DMA2_STREAM5
 #define WS2812_DMA_CHANNEL 6
+
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120
+#define ENABLE_RGB_GRADIENT_UP_DOWN
+#define ENABLE_RGB_GRADIENT_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#define ENABLE_RGB_MATRIX_RAINDROPS	
+#define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_CYCLE_ALL
 
 /*
  * Feature disable options
