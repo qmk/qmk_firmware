9d3125efd57427ce4fec35d3f577d872ab43b1f9
J Pratt <jp10010101010000@gmail.com>
2022-03-29 12:37:50 +1100

    Typo fix

--- keyboards/wilba_tech/wt60_c/keymaps/Cypher1/rules.mk
+++ keyboards/wilba_tech/wt60_c/keymaps/Cypher1/rules.mk
@@ -6,7 +6,7 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 
 RGBLIGHT_ENABLE = no        # Disable support for addressable RGB LEDs
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Disable keyboard backlight functionality
 
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
