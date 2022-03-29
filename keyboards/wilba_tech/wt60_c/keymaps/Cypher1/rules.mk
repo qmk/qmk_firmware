dfba49b6f6f5e741083387c9d693033777dc083c
J Pratt <jp10010101010000@gmail.com>
2022-03-29 11:44:55 +1100

    Add a gaming mode and use an enum to make layer switching more readable

--- keyboards/wilba_tech/wt60_c/keymaps/Cypher1/rules.mk
+++ keyboards/wilba_tech/wt60_c/keymaps/Cypher1/rules.mk
@@ -5,7 +5,7 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 
-RGBLIGHT_ENABLE = no        # Disable support for RGB LEDs
+RGBLIGHT_ENABLE = no        # Disable support for addressable RGB LEDs
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 
 CONSOLE_ENABLE = no         # Console for debug
