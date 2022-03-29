5cd906d1ed5bf2ef9295bcca21417c45700a9039
J Pratt <jp10010101010000@gmail.com>
2022-03-29 12:57:20 +1100

    Use defines to clean up formatting, and avoid overriding esc/grv key in FN layers

--- keyboards/wilba_tech/wt60_c/keymaps/Cypher1/keymap.c
+++ keyboards/wilba_tech/wt60_c/keymaps/Cypher1/keymap.c
@@ -12,6 +12,8 @@
 #define MS_DOWN KC_MS_DOWN
 #define MS_LEFT KC_MS_LEFT
 #define MS_RGHT KC_MS_RIGHT
+#define DF_CODE DF(CODING)
+#define DF_GAME DF(GAMING)
 
 enum LAYERS {
   GAMING, // Simpler control scheme for games (e.g. space is just space)
@@ -26,32 +28,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,   KC_NO,
       KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
       MO(MS),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, DF(CODING),
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, DF_CODE,
       KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT, MO(KBD), KC_RCTL
   ),
   [CODING] = LAYOUT_all(
       KC_GESC, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
       ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
       ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
-      ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, DF(GAMING),
+      ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, DF_GAME,
       ___X___, ___X___, ___X___,                         SPC_FN(FN),                            ___X___, ___X___, ___X___
   ),
   [FN] = LAYOUT_all(
-      KC_GRV,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,   KC_NO,
+      ___X___,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,   KC_NO,
       ___X___, KC_BRID, KC_BRIU, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_PGDN, KC_PGUP, ___X___,
       ___X___, KC_VOLD, KC_VOLU, KC_MUTE, ___X___, KC_HOME, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_END, ___X___, ___X___,
       ___X___, KC_MPRV, KC_MPLY, KC_MNXT, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
       ___X___, ___X___, ___X___,                            ___X___,                            ___X___, ___X___, ___X___
   ),
   [MS] = LAYOUT_all(
-      KC_GRV,  ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,   KC_NO,
+      ___X___,  ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,   KC_NO,
       ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
       ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, ___X___, ___X___, ___X___,
       ___X___, MS_W_DN, MS_W_UP, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
       MS_BTN1, MS_BTN3, MS_BTN2,                            ___X___,                            ___X___, ___X___, ___X___
   ),
   [KBD] = LAYOUT_all(
-      KC_GRV,   EF_DEC,  EF_INC,  H1_DEC,  H1_INC,  H2_DEC,  H2_INC, ___X___, ___X___, ___X___, ___X___,  BR_DEC,  BR_INC, ___X___,   KC_NO,
+      ___X___,  EF_DEC,  EF_INC,  H1_DEC,  H1_INC,  H2_DEC,  H2_INC, ___X___, ___X___, ___X___, ___X___,  BR_DEC,  BR_INC, ___X___,   KC_NO,
       ___X___, ___X___, ___X___,  S1_DEC,  S1_INC,  S2_DEC,  S2_INC, ___X___, ___X___, ___X___, ___X___,  ES_DEC,  ES_INC, ___X___,
       ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
       ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
