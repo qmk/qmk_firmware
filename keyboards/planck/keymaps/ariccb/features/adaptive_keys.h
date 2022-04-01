0626b4db520671666fd52dc2fe47db68bc85ef6a
Aric Crosson Bouwers <aricbouwers@outlook.com>
2022-04-01 13:54:06 -0600

    Speeding up PT adaptive by removing pr option

--- keyboards/planck/keymaps/ariccb/features/adaptive_keys.h
+++ keyboards/planck/keymaps/ariccb/features/adaptive_keys.h
@@ -65,6 +65,7 @@ bool process_adaptive_key(uint16_t keycode, const keyrecord_t *record) {
         tap_code16(first);
         // uncomment below if it doesn't work to fix the missing caps when adaptive keys are pressed while SHIFT is held
         // clear_mods();
+        set_mods(prior_saved_mods);
         tap_code16(second);
       }
     }
