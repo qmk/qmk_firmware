9b6ab60ed459fe6d14273909d37b738b106f50f2
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-04-02 02:07:45 -0700

    h65.h: use QMK 3-character notation

--- keyboards/hineybush/h65/h65.h
+++ keyboards/hineybush/h65/h65.h
@@ -18,24 +18,18 @@
 
 #include "quantum.h"
 
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
+#define ___ KC_NO
+
 #define LAYOUT_all( \
-    k000, k001, k002, k003, k004, k005, k006, k007, k008, k009, k010, k011, k012, k013, k014, k114, \
-    k100, k101, k102, k103, k104, k105, k106, k107, k108, k109, k110, k111, k112, k113, k214,  \
-    k200, k201, k202, k203, k204, k205, k206, k207, k208, k209, k210, k211, k212, k213, k314,  \
-    k300, k301, k302, k303, k304, k305, k306, k307, k308, k309, k310, k311, k312, k313, k414,  \
-    k400, k401, k402,       k403,       k406,       k408, k409, k410, k411, k412, k413   \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, k1E, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k2E, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D, k3E, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k4E, \
+    k40, k41, k42,           k43, k46, k48,      k49, k4A,      k4B, k4C, k4D  \
 ) { \
-    { k000, k001, k002, k003, k004, k005, k006, k007, k008, k009, k010, k011, k012, k013, k014 }, \
-    { k100, k101, k102, k103, k104, k105, k106, k107, k108, k109, k110, k111, k112, k113, k114 }, \
-    { k200, k201, k202, k203, k204, k205, k206, k207, k208, k209, k210, k211, k212, k213, k214 }, \
-    { k300, k301, k302, k303, k304, k305, k306, k307, k308, k309, k310, k311, k312, k313, k314 }, \
-    { k400, k401, k402, k403, KC_NO,KC_NO,k406,KC_NO, k408, k409, k410, k411, k412, k413, k414 }  \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E }, \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E }, \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D, k2E }, \
+    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k3E }, \
+    { k40, k41, k42, k43, ___, ___, k46, ___, k48, k49, k4A, k4B, k4C, k4D, k4E }  \
 }
