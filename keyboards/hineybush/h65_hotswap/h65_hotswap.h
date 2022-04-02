0d2dbb5df6103b20ff6b092d633dad249860514f
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-04-02 13:55:06 -0700

    add LAYOUT_65_ansi_blocker

--- keyboards/hineybush/h65_hotswap/h65_hotswap.h
+++ keyboards/hineybush/h65_hotswap/h65_hotswap.h
@@ -50,3 +50,17 @@
     { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k3E }, \
     { k40, k41, k42, ___, k44, ___, k46, k47, ___, k49, k4A, k4B, k4C, k4D, k4E }  \
 }
+
+#define LAYOUT_65_ansi_blocker( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0E, k1E, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k2E, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B,      k2D, k3E, \
+    k30,      k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k4E, \
+    k40, k41, k42,                k46,           k49, k4A,      k4B, k4C, k4D  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, ___, k0E }, \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E }, \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, ___, k2D, k2E }, \
+    { k30, ___, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, k3E }, \
+    { k40, k41, k42, ___, ___, ___, k46, ___, ___, k49, k4A, k4B, k4C, k4D, k4E }  \
+}
