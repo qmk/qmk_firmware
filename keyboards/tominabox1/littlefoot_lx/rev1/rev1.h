d0a059518e849566cbbc3e9fc408173b034fcf9a
TJ <tom.campie@gmail.com>
2022-04-01 20:51:31 -0500

    update copyright dates

--- keyboards/tominabox1/littlefoot_lx/rev1/rev1.h
+++ keyboards/tominabox1/littlefoot_lx/rev1/rev1.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 tominabox1
+Copyright 2022 tominabox1
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
