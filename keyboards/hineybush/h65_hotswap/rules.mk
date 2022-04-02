15ebf65a39e6afed81eb6486e2b1568ac42dad98
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-04-02 14:42:27 -0700

    add LAYOUT_65_ansi_blocker_split_bs

--- keyboards/hineybush/h65_hotswap/rules.mk
+++ keyboards/hineybush/h65_hotswap/rules.mk
@@ -17,4 +17,4 @@ BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-LAYOUTS = 65_ansi_blocker
+LAYOUTS = 65_ansi_blocker 65_ansi_blocker_split_bs
