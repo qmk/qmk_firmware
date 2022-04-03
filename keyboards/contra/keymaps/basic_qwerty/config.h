fc46005974055da502b78f51a4debd28e77f98f3
Stephon Parker <sgparker62@gmail.com>
2022-04-03 19:05:54 -0400

    Renaming root again
    Didn't stash all the files in my last commit. Rename root directory, updated the readme. Original PR was to update QWERTY layer and remove colemak layer.

--- keyboards/contra/keymaps/basic_qwerty/config.h
+++ keyboards/contra/keymaps/basic_qwerty/config.h
@@ -8,7 +8,6 @@
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
                                   SONG(DVORAK_SOUND) \
                                 }
 #endif
@@ -22,7 +21,7 @@
 /* enable basic MIDI features:
    - MIDI notes can be sent when in Music mode is on
 */
-                                
+
 #define MIDI_BASIC
 
 /* enable advanced MIDI features:
