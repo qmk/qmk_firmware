ec4f6553338919771038114c9d6e15c0be3778ec
Nick Brassel <nick@tzarc.org>
2022-04-04 04:17:38 +1000

    Add warning for CTCP/CONVERT_TO_PROTON_C. (#16782)

--- builddefs/converters.mk
+++ builddefs/converters.mk
@@ -1,8 +1,19 @@
+# Note for new boards -- CTPC and CONVERT_TO_PROTON_C are deprecated terms
+# and should not be replicated for new boards. These will be removed from
+# documentation as well as existing keymaps in due course.
 ifeq ($(strip $(CTPC)), yes)
     CONVERT_TO_PROTON_C=yes
 endif
 ifeq ($(strip $(CONVERT_TO_PROTON_C)), yes)
     CONVERT_TO=proton_c
+
+cpfirmware: ctpc_warning
+.INTERMEDIATE: ctpc_warning
+ctpc_warning: elf
+	$(info @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@)
+	$(info The `CONVERT_TO_PROTON_C` and `CTPC` options are soon to be deprecated.)
+	$(info Boards should be changed to use `CONVERT_TO=proton_c` instead.)
+	$(info @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@)
 endif
 
 # TODO: opt in rather than assume everything uses a pro micro
