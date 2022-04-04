48a2207fd92095f57cb8d47b6b3c5cd89db6f3c4
Nathan <nathan.cooke@compass.com>
2022-04-04 13:43:51 -0400

    google meet mapped

--- keyboards/bastardkb/charybdis/3x5/keymaps/nathancooke7/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/nathancooke7/keymap.c
@@ -121,10 +121,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_MISC] = LAYOUT_charybdis_3x5(
-    _______  , _______ , DESKTOP , _______ , _______ ,                    _______  , _______  , _______ , _______  , RESET    ,
-    AM_LEFT  , DESK_L  , MISSION , DESK_R  , AM_RITE ,                    AM_CYCLE , AM_1     , AM_2    , AM_CCW   , _______  ,
-    KC_LCTRL , _______ , _______ , _______ , _______ ,                    _______  , MS_BACK  , MS_FORE , TAB_BACK , TAB_FORE ,
-                         _______ , KC_NO   , _______ ,                    _______  , _______
+    _______  , _______ , DESKTOP  , _______ , _______  ,                    _______  , _______  , _______ , _______  , RESET    ,
+    AM_LEFT  , DESK_L  , MISSION  , DESK_R  , AM_RITE  ,                    AM_CYCLE , AM_1     , AM_2    , AM_CCW   , _______  ,
+    KC_LCTRL , _______ , _______  , _______ , _______  ,                    _______  , MS_BACK  , MS_FORE , TAB_BACK , TAB_FORE ,
+                         MUTE_MIC , KC_NO   , MUTE_CAM ,                    _______  , RAZ_HAND
   ),
 
   [_MOUSE] = LAYOUT_charybdis_3x5(
