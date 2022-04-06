8384898a2263585d65488d82c75acf5840f8ebaa
Nathan <nathan.cooke@compass.com>
2022-04-06 12:01:52 -0400

    add mod tap mouse later and move mouse l r buttons

--- keyboards/bastardkb/charybdis/3x5/keymaps/nathancooke7/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/nathancooke7/keymap.c
@@ -2,9 +2,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_charybdis_3x5(
-    KC_Q   , KC_W   , KC_E    , KC_R    , KC_T       ,                    KC_Y      , KC_U      , KC_I      , KC_O   , KC_P     ,
-    KC_CA  , KC_AS  , KC_GD   , KC_SF   , KC_G       ,                    KC_H      , KC_SJ     , KC_GK     , KC_AL  , KC_CSCLN ,
-    NAV_Z  , KC_X   , KC_C    , KC_V    , KC_B       ,                    KC_N      , KC_M      , KC_COMM   , KC_DOT , SYM_SLSH ,
+    KC_Q   , KC_W  , KC_E    , KC_R    , KC_T       ,                    KC_Y      , KC_U      , KC_I      , KC_O   , KC_P     ,
+    KC_CA  , KC_AS , KC_GD   , KC_SF   , KC_G       ,                    KC_H      , KC_SJ     , KC_GK     , KC_AL  , KC_CSCLN ,
+    NAV_Z  , MOU_X , KC_C    , KC_V    , KC_B       ,                    KC_N      , KC_M      , KC_COMM   , KC_DOT , SYM_SLSH ,
                       SYM_ENT , KC_NO   , BACK_WORDS ,                                MISC_DEL , NAV_SPACE
   ),
 
@@ -17,9 +17,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MOUSE] = LAYOUT_charybdis_3x5(
     _______ , _______ , _______ , _______ , _______ ,                     _______ , _______ , _______ , _______ , _______ ,
-    _______ , _______ , _______ , _______ , _______ ,                     _______ , _______ , _______ , _______ , _______ ,
-    _______ , _______ , SNIPING , DRGSCRL , _______ ,                     _______ , _______ , _______ , _______ , _______ ,
-                        L_CLICK , KC_NO   , R_CLICK ,                     _______ , _______
+    _______ , _______ , _______ , _______ , L_CLICK ,                     _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , SNIPING , DRGSCRL , R_CLICK ,                     _______ , _______ , _______ , _______ , _______ ,
+                        _______ , KC_NO   , _______ ,                     _______ , _______
   ),
 
   [_NAV] = LAYOUT_charybdis_3x5(
