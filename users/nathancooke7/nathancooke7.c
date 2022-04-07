313e9541b0c805b246e8de4f79b7d19e616d2af4
Nathan <nathan.cooke@compass.com>
2022-04-07 00:02:08 -0400

    fix keymap and add & to capsword continue list'

--- users/nathancooke7/nathancooke7.c
+++ users/nathancooke7/nathancooke7.c
@@ -49,3 +49,22 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
   return true;
 }
 #endif
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case KC_AMPERSAND:
+            return true;
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
