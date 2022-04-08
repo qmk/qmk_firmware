95700a20a601dc92d349699fc8e95c8b7557d005
yeimialberth <alberthomc@gmail.com>
2022-04-08 01:19:04 -0500

    Update keyboards/crkbd/keymaps/crkdves/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/crkbd/keymaps/crkdves/keymap.c
+++ keyboards/crkbd/keymaps/crkdves/keymap.c
@@ -399,7 +399,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed) {
           eeconfig_update_rgblight_default();
           rgblight_enable();
-          RGB_current_mode = rgblight_config.mode;
         }
       #endif
       #ifdef RGB_MATRIX_ENABLE
