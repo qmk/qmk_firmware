c54fc72483b6586d1a8f2357525ab607c11b11aa
Nathan <nathan.cooke@compass.com>
2022-04-08 20:49:12 -0400

    notes on getting encoder to work

--- keyboards/bastardkb/charybdis/3x5/keymaps/nathancooke7/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/nathancooke7/config.h
@@ -6,7 +6,9 @@
 
 #ifdef ENCODER_ENABLE
 /* Free pins { B7 D5 C7 } */
-/* SPI pins { B1 B2 B3 F1 } */
+/* Encoder is NOT fully compatible with the SPI pins { B1 B2 B3 F1 } */
+/* You will need to connect the GND and VCC pins to SPI pins as marked */
+/* You will need to connect the TK and TK pins to { B7 D5 and C7 } */
 #define ENCODERS_PAD_A { B1 }
 #define ENCODERS_PAD_B { B2 }
 #define ENCODER_RESOLUTIONS { 4 }
