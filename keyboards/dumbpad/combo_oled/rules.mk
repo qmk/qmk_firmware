d6b9ea7d6d22b8101c570c3e6b08dcba28a338af
medoix <medoix@shivv.com>
2022-04-11 20:32:50 +1000

    Firmware for the dumbpad combo_oled version modified by KEEBD

---
+++ keyboards/dumbpad/combo_oled/rules.mk
@@ -0,0 +1,22 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+ENCODER_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+OLED_DRIVER = SSD1306
+WPM_ENABLE = yes
