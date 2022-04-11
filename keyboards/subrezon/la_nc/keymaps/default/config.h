d80e3a615479e717681740c8a5b260b7486a4d42
subrezon <88534270+subrezon@users.noreply.github.com>
2022-04-11 15:16:17 +0200

    Apply suggestions from code review
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/subrezon/la_nc/keymaps/default/config.h
+++ keyboards/subrezon/la_nc/keymaps/default/config.h
@@ -1,5 +1,6 @@
-#pragma once
+// Copyright 2022 Daniel Osipishin (@subrezon)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "config_common.h"
+#pragma once
 
 //#define OLED_FONT_H "keyboards/subrezon/lib/font.c"
