d02878bf236c73a4034e8e6a1a234901c8c5e6f9
David Kristoffersen <david.alta2010@gmail.com>
2022-04-11 15:06:23 +0200

    Remove redundant defines

--- keyboards/planck/keymaps/prog_qgmlwb/src/base/keymap_norwegian.h
+++ keyboards/planck/keymaps/prog_qgmlwb/src/base/keymap_norwegian.h
@@ -3,13 +3,6 @@
 #include "keymap_extras/keymap_norwegian.h"
 #include "../base.h"
 
-// WRONG: |, ^, ", ~, `
-// Norwegian layout fix
-// #undef NO_QUOT
-// #define NO_QUOT KC_BSLS  // '
-// #undef NO_TILD
-// #define NO_TILD RALT(KC_RBRC)  // ~
-
 // Better norwegian defines
 #undef NO_LESS
 #define NO_LESS KC_NUBS // <
@@ -21,9 +14,3 @@
 #define NO_OE KC_SCLN // Ø
 #undef NO_AA
 #define NO_AA KC_LBRC // Å
-
-// Shift layout fix
-// #undef NO_SQUOT
-// #define NO_SQUOT KC_AT  // "
-// #undef NO_SBSLS
-// #define NO_SBSLS KC_GRAVE  // |
