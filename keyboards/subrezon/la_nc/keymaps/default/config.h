bf151d74e4b29ccaaa129685b09da56c8fdd58b2
Daniel Osipishin <daniel.osipishin@retarus.de>
2022-04-11 14:26:15 +0200

    created subrezon/la_nc keyboard and default keymap

---
+++ keyboards/subrezon/la_nc/keymaps/default/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "config_common.h"
+
+//#define OLED_FONT_H "keyboards/subrezon/lib/font.c"
---
+++ keyboards/subrezon/la_nc/rules.mk
@@ -0,0 +1,14 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build options
+BOOTMAGIC_ENABLE = yes	# Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = no		# Console for debug
+COMMAND_ENABLE = yes	# Commands for debug and configuration
+NKRO_ENABLE = yes		# Enable N-key Rollover
+
