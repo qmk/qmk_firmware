e249143b74746acf5b33f15db77b0fb8a533c707
Álvaro A. Volpato <alvaro.augusto.volpato@gmail.com>
2022-04-13 16:58:34 -0300

    Formatting of rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/acheron/shark/beta/rules.mk
+++ keyboards/acheron/shark/beta/rules.mk
@@ -6,22 +6,20 @@ BOOTLOADER = stm32-dfu
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes        # Mouse keys
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes            # USB Nkey Rollover
-BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
-BACKLIGHT_DRIVER = pwm
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+NKRO_ENABLE = yes           # Enable N-Key rollover
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+BACKLIGHT_DRIVER = pwm
 LTO_ENABLE = yes
-ENCODER_ENABLE = no
-LAYOUTS=ortho_4x12
-ENCODER_ENABLE=yes
+ENCODER_ENABLE = yes
+
+LAYOUTS = ortho_4x12
+
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
