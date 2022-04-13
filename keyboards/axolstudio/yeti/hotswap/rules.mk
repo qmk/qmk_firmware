51c483648c39b21afd8d82c802d19ecb5201b9a7
kb-elmo <lorwel@mailbox.org>
2022-04-13 12:41:47 +0200

    Update keyboards/axolstudio/yeti/hotswap/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/axolstudio/yeti/hotswap/rules.mk
+++ keyboards/axolstudio/yeti/hotswap/rules.mk
@@ -15,7 +15,6 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-FORCE_NKRO = yes
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
 RGB_MATRIX_DRIVER = IS31FL3733
 
