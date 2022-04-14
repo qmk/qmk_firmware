d2472513bf8dccd6b74cd9c16f6dfeaa37eee07d
X-Bows Tech <41098278+XBowsTech@users.noreply.github.com>
2022-04-14 10:57:32 +0800

    Update keyboards/xbows/nature/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/xbows/nature/config.h
+++ keyboards/xbows/nature/config.h
@@ -18,7 +18,7 @@
 #include "config_common.h"
 
 #define VENDOR_ID    0x5842
-#define PRODUCT_ID   0x4e61
+#define PRODUCT_ID   0x4E61
 #define DEVICE_VER   0x0001
 #define MANUFACTURER X-BOWS
 #define PRODUCT      NATURE
