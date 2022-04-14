cb4064cc7ea549bdabe52fee622ec3595e02c8f4
khoang65 <khoang65@gmail.com>
2022-04-14 10:43:47 -0400

    Added enum to represent layers

--- keyboards/omkbd/ergodash/rev1/keymaps/khoang65/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/khoang65/keymap.c
@@ -1,12 +1,11 @@
 #include QMK_KEYBOARD_H
 
- enum layers {
-    _BASE,
-    _FUNCTION, 
-    _VIM,
-    _SHIFTVIM,
-    _NUMPAD,
-    _SYMBOL,
+ enum layer_names {
+    BASE = 0,
+    FUNCTION, 
+    VIM,
+    NUMPAD,
+    ADJUST
 };
 
 enum custom_keycodes {
@@ -16,7 +15,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
-   /* Keymap: BASE layer
+   /* Keymap 0: BASE layer
    * ,-----------------------------------------------------------.                              ,-----------------------------------------------------------.
    * | ESC     | !1      | @2      | #3      | $4      | %5      |---------.          ,---------| ^6      | &7      | *8      | (9      | )0      | _-      |
    * |---------+---------+---------+---------+---------+---------| {[      |          | }]      |---------+---------+---------+---------+---------+---------|
@@ -34,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                         | Space   |         | CST(F14)|          |         |         | BckSpc  |
    *                                         `-----------------------------'          `-----------------------------'
    */
-	[0] = LAYOUT_4key_2u_inner(
+	[BASE] = LAYOUT_4key_2u_inner(
       KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_LBRC,             KC_RBRC,  KC_6,     KC_7,     KC_8,      KC_9,     KC_0,    KC_MINS, 
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_F4,               KC_EQL,   KC_Y,     KC_U,     KC_I,      KC_O,     KC_P,    KC_LEAD, 
       KC_GRV,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_PSCR,             KC_BSLS,  KC_H,     KC_J,     KC_K,      KC_L,     KC_SCLN, KC_QUOT, 
@@ -43,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
   
 
-   /* Keymap: F layer
+   /* Keymap 1: F layer
    * ,-----------------------------------------------------------.                              ,-----------------------------------------------------------.
    * | TRNS    | F1      | F2      | F3      | F4      | F5      |---------.          ,---------| F6      | F7      | F8      | F9      | F10     | TRNS    |
    * |---------+---------+---------+---------+---------+---------| F11     |          | F12     |---------+---------+---------+---------+---------+---------|
@@ -61,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                         | TRNS    |         |         |          |         |         | TRNS    |
    *                                         `-----------------------------'          `-----------------------------'
    */  
-	[1] = LAYOUT_4key_2u_inner(
+	[FUNCTION] = LAYOUT_4key_2u_inner(
       KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,              KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_TRNS, 
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, 
       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, 
@@ -70,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
       
       
-   /* Keymap: VIM layer
+   /* Keymap 2: VIM layer
    * ,-----------------------------------------------------------.                              ,-----------------------------------------------------------.
    * |         |         |         |         |         |         |---------.          ,---------| Rewind  |         |         | Fastfwd |         |         |
    * |---------+---------+---------+---------+---------+---------|         |          |         |---------+---------+---------+---------+---------+---------|
@@ -88,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                         | Spc     |         |         |          |         |         | BckSpc  |
    *                                         `-----------------------------'          `-----------------------------'
    */     
-	[2] = LAYOUT_4key_2u_inner(
+	[VIM] = LAYOUT_4key_2u_inner(
       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,               KC_NO,    KC_MRWD,    KC_NO,    KC_NO,    KC_MFFD,    KC_NO,    KC_NO, 
       KC_NO,    KC_NO,    LCTL(KC_RGHT), KC_NO, KC_AGIN, KC_NO,   KC_NO,               KC_MPLY,  LCTL(KC_C), LCTL(KC_Z), KC_NO,  KC_NO,      LCTL(KC_V), KC_NO, 
       KC_NO,    KC_NO,    KC_NO,    LCTL(KC_X), KC_FIND, KC_NO,   KC_NO,               KC_MUTE,  KC_LEFT,    KC_DOWN,  KC_UP,    KC_RGHT,    KC_NO,    KC_CALC, 
@@ -97,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
       
       
-   /* Keymap: NUMPAD Layer
+   /* Keymap 3: NUMPAD Layer
    * ,-----------------------------------------------------------.                              ,-----------------------------------------------------------.
    * | Esc     | !       | @       | #       | $       | %       |---------.          ,---------| ^       | &       | Num Lk  | *       | /       | -       |
    * |---------+---------+---------+---------+---------+---------| {[      |          | }]      |---------+---------+---------+---------+---------+---------|
@@ -115,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                         | Spc     |         |         |          |         |         | BckSpc  |
    *                                         `-----------------------------'          `-----------------------------'
    */     
-	[3] = LAYOUT_4key_2u_inner(
+	[NUMPAD] = LAYOUT_4key_2u_inner(
       KC_ESC,  KC_EXLM,   KC_AT,    KC_HASH,   KC_DLR,   KC_PERC, KC_LBRC,             KC_RBRC,  KC_CIRC,  KC_AMPR,  KC_NLCK,  KC_PAST,  KC_PSLS,  KC_PMNS, 
       KC_TAB,  KC_NO,     KC_NO,    KC_NO,     KC_NO,    KC_NO,   TO(4),               KC_EQL,   KC_NO,    KC_7,     KC_8,     KC_9,     KC_PPLS,  KC_NO, 
       KC_GRV,  KC_NO,     KC_NO,    KC_NO,     KC_NO,    KC_NO,   MEH(KC_NO),          KC_BSLS,  KC_NO,    KC_4,     KC_5,     KC_6,     KC_PCMM,  KC_QUOT,
@@ -124,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
 	
   
-   /* Keymap: SYMBOL/ADJUST Layer
+   /* Keymap 4: SYMBOL/ADJUST Layer
    * ,-----------------------------------------------------------.                              ,-----------------------------------------------------------.
    * | Esc     | !       | @       | #       | $       | %       |---------.          ,---------|         |         |         |         |         |         |
    * |---------+---------+---------+---------+---------+---------| {[      |          |         |---------+---------+---------+---------+---------+---------|
@@ -142,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                         |         |         |         |          |         |         |         |
    *                                         `-----------------------------'          `-----------------------------'
    */ 
-  [4] = LAYOUT_4key_2u_inner(
+  [ADJUST] = LAYOUT_4key_2u_inner(
       KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_LBRC,             KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, 
       KC_NO,    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_MINS,  KC_UNDS,  KC_QUOT,             KC_NO,    RGB_HUD,  RGB_VAD,  RGB_VAI,  RGB_HUI,  KC_NO,    KC_NO, 
       KC_TILD,  KC_DQUO,  KC_LT,    KC_LCBR,  KC_LPRN,  KC_DQUO,  KC_COLN,             KC_NO,    RGB_RMOD, RGB_TOG,  RGB_M_P,  RGB_MOD,  KC_NO,    KC_NO, 
