1354d582160e8118e542b575085856aeb9d3d871
moyi4681 <moyi4681@users.noreply.github.com>
2022-04-14 15:06:32 +0800

    Update keyboards/kbdfans/baguette66/soldered/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kbdfans/baguette66/soldered/config.h
+++ keyboards/kbdfans/baguette66/soldered/config.h
@@ -37,8 +37,6 @@
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 /* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
 #define USB_SUSPEND_WAKEUP_DELAY 5000
 #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
 
