603db5898aa601f6f79794d748f121eb7e885484
skeletonkbd <skeletonkbd@gmail.com>
2022-04-15 23:30:32 +0900

    fix

--- keyboards/skeletonkbd/skeletonnumpad/keymaps/via/keymap.c
+++ keyboards/skeletonkbd/skeletonnumpad/keymaps/via/keymap.c
@@ -24,29 +24,32 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
+    [_BASE] = LAYOUT_numpad_5x4(
 		KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-		KC_P7, KC_P8, KC_P9, KC_PPLS,
-		KC_P4, KC_P5, KC_P6,
-		KC_P1, KC_P2, KC_P3, KC_PENT,
-		KC_P0, KC_PDOT),
-    [_L1] = LAYOUT(
-		_______, _______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______),
-    [_L2] = LAYOUT(
-		_______, _______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______),
-    [_L3] = LAYOUT(
-		_______, _______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______),
+		KC_P7, KC_P8, KC_P9,
+		KC_P4, KC_P5, KC_P6,   KC_PPLS,
+		KC_P1, KC_P2, KC_P3,
+		KC_P0,        KC_PDOT, KC_PENT
+	),
+    [_L1] = LAYOUT_numpad_5x4(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,          KC_TRNS, KC_TRNS
+	),
+    [_L2] = LAYOUT_numpad_5x4(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,          KC_TRNS, KC_TRNS
+	),
+    [_L3] = LAYOUT_numpad_5x4(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,          KC_TRNS, KC_TRNS
+	),
 };
