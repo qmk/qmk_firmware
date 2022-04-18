1a12f961bd4bb5a6c4b67d97d628364b23b74a6e
4pplet <mail@4pplet.com>
2022-04-18 09:23:12 +0200

    Update rules.mk

--- keyboards/4pplet/perk60_iso/rev_a/rules.mk
+++ keyboards/4pplet/perk60_iso/rev_a/rules.mk
@@ -5,6 +5,8 @@ MCU = STM32F411
 BOOTLOADER = stm32-dfu
 
 # Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
