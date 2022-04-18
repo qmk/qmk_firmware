f50a623909a29e45fb9bebfd16afd1835277b530
Nick Brassel <nick@tzarc.org>
2022-04-19 00:39:45 +1000

    Fixup AVR builds. (#16875)
    * Fixup AVR builds.
    
    * Update platforms/avr/hardware_id.c
    
    Co-authored-by: Ryan <fauxpark@gmail.com>
    
    Co-authored-by: Joel Challis <git@zvecr.com>
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- platforms/avr/hardware_id.c
+++ platforms/avr/hardware_id.c
@@ -1,6 +1,12 @@
 // Copyright 2022 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+// For some reason this bit is undocumented for some AVR parts and not defined in their avr-libc IO headers
+// See https://stackoverflow.com/questions/12350914/how-to-read-atmega-32-signature-row
+#ifndef SIGRD
+#    define SIGRD 5
+#endif // SIGRD
+
 #include <avr/boot.h>
 #include "hardware_id.h"
 
