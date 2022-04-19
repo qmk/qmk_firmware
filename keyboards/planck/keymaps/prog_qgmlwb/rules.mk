db671a5dd5b509d9339263e76ef90eb9201a5d23
David Kristoffersen <david.alta2010@gmail.com>
2022-04-19 11:37:39 +0200

    Reorder macro first

--- keyboards/planck/keymaps/prog_qgmlwb/rules.mk
+++ keyboards/planck/keymaps/prog_qgmlwb/rules.mk
@@ -1,5 +1,5 @@
 # Compiler source files
-SRC += src/base.c src/base/functions.c src/macros/static.c src/macros/dynamic.c src/macros/macros.c
+SRC += src/base.c src/base/functions.c src/macros/macros.c src/macros/static.c src/macros/dynamic.c
 
 # Firmware size optimizations
 CONSOLE_ENABLE	= no	# 1646: Console for debug
