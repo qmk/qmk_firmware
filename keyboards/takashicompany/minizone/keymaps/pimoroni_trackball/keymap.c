03eed47c5d5f174538bcbbb3af18c53202f12d65
takashicompany <t@kashi.company>
2022-04-19 14:37:27 +0900

    trackball

--- keyboards/takashicompany/minizone/keymaps/pimoroni_trackball/keymap.c
+++ keyboards/takashicompany/minizone/keymaps/pimoroni_trackball/keymap.c
@@ -2,7 +2,43 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "math.h"
+
+#include "pointing_device.h"
+#include "paw3204.h"
+
+enum custom_keycodes {
+    KC_MY_BTN1 = SAFE_RANGE,
+    KC_MY_BTN2,
+    KC_MY_BTN3,
+    KC_MY_SCR,
+};
+
+report_mouse_t mouse_report;
+
+enum click_state {
+    NONE = 0,
+    WAITING,    // マウスレイヤーが有効になるのを待つ
+    CLICKABLE,  // マウスレイヤー有効になりクリック入力が取れる
+    CLICKING,   // クリックをしている
+    SCROLLING   // スクロール中
+};
+
+enum click_state state;     // 現在のクリック入力受付の状態
+uint16_t click_timer;       // タイマー。状態に応じて時間で判定する
+
+uint16_t to_clickable_time = 10;   // この秒数(千分の一秒)、WAITING状態ならクリックレイヤーが有効になる
+uint16_t to_reset_time = 1000; // この秒数(千分の一秒)、CLICKABLE状態ならクリックレイヤーが無効になる
+
+uint16_t click_layer = 9;   // マウス入力が可能になった際に有効になるレイヤー
+
+int16_t scroll_v_counter;   // 垂直スクロールの入力をカウントする
+int16_t scroll_h_counter;   // 水平スクロールの入力をカウントする
+
+int16_t scroll_v_threshold = 30;    // この閾値を超える度に垂直スクロールが実行される
+int16_t scroll_h_threshold = 30;    // この閾値を超える度に水平スクロールが実行される
+
+int16_t after_click_lock_movement = 0;      // クリック入力後の移動量を測定する変数
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -55,12 +91,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     LAYOUT(
-        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, DF(0), DF(2), 
-        RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-        RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MY_BTN1, KC_MY_SCR, KC_MY_BTN2, KC_MY_BTN3, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
+
+// クリック用のレイヤーを有効にする
+void enable_click_layer(void) {
+    layer_on(click_layer);
+    click_timer = timer_read();
+    state = CLICKABLE;
+}
+
+// クリック用のレイヤーを無効にする
+void disable_click_layer(void) {
+    state = NONE;
+    layer_off(click_layer);
+    scroll_v_counter = 0;
+    scroll_h_counter = 0;
+}
 
 // 自前の絶対数を返す関数
 int16_t my_abs(int16_t num) {
@@ -69,6 +120,11 @@ int16_t my_abs(int16_t num) {
     }
 
     return num;
+}
+
+// 現在クリックが可能な状態か
+bool is_clickable_mode(void) {
+    return state == CLICKABLE || state == CLICKING || state == SCROLLING;
 }
 
 int16_t sign(int16_t num) {
@@ -95,6 +151,200 @@ int16_t min(int16_t a, int16_t b) {
     return b;
 }
 
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    
+    switch (keycode) {
+        case KC_MY_BTN1:
+        case KC_MY_BTN2:
+        case KC_MY_BTN3:
+        {
+            report_mouse_t currentReport = pointing_device_get_report();
+
+            // どこのビットを対象にするか
+            uint8_t btn = 1 << (keycode - KC_MY_BTN1);
+            
+            if (record->event.pressed) {
+                // ビットORは演算子の左辺と右辺の同じ位置にあるビットを比較して、両方のビットのどちらかが「1」の場合に「1」にします。
+                currentReport.buttons |= btn;
+                mouse_report.buttons |= btn;
+                state = CLICKING;
+                after_click_lock_movement = 30;
+            } else {
+                // ビットANDは演算子の左辺と右辺の同じ位置にあるビットを比較して、両方のビットが共に「1」の場合だけ「1」にします。
+                currentReport.buttons &= ~btn;
+                mouse_report.buttons &= ~btn;
+                enable_click_layer();
+            }
+
+            pointing_device_set_report(currentReport);
+            return false;
+        }
+
+        case KC_MY_SCR:
+            if (record->event.pressed) {
+                state = SCROLLING;
+            } else {
+                // disable_click_layer(); // スクロールキーを離したらクリックレイヤーを無効にする
+
+                enable_click_layer();   // スクロールキーを離した時に再度クリックレイヤーを有効にする
+            }
+         return false;
+
+         default:
+            if  (record->event.pressed) {
+                disable_click_layer();
+            }
+        
+    }
+   
+    return true;
+}
+
+int history_length = 10;
+
+int16_t history_x[10] = {};
+int16_t history_y[10] = {};
+int16_t history_t[10] = {};
+
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+
+    int16_t current_x = history_x[0];
+    int16_t current_y = history_y[0];
+
+    int start = 1;
+    int read_count = 10;
+
+    if (current_x != 0 || current_y != 0)
+    {
+        for (int i = start; i < start + read_count && i < history_length; i++)
+        {
+            current_x += history_x[i];
+            current_y += history_y[i];
+            start = i;
+        }
+    }
+
+    for (int i = start; i < history_length; i++)
+    {
+        history_x[i - start] = history_x[i];
+        history_y[i - start] = history_y[i];
+    }
+
+    history_x[history_length - 1] = mouse_report.x;
+    history_y[history_length - 1] = mouse_report.y;
+
+   
+    // dprintf("x:%4d y:%4d \n", mouse_report.x,  mouse_report.y);
+    
+    if (mouse_report.x != 0 || mouse_report.y != 0) {
+
+        switch (state) {
+            case CLICKABLE:
+                click_timer = timer_read();
+                break;
+
+            case CLICKING:
+                after_click_lock_movement -= my_abs(mouse_report.x) + my_abs(mouse_report.y);
+
+                if (after_click_lock_movement > 0) {
+                    mouse_report.x = 0;
+                    mouse_report.y = 0;
+                }
+
+                break;
+
+            case SCROLLING:
+            {
+                int8_t rep_v = 0;
+                int8_t rep_h = 0;
+
+                // 垂直スクロールの方の感度を高める
+                if (my_abs(mouse_report.y) * 2 > my_abs(mouse_report.x)) {
+
+                    scroll_v_counter += mouse_report.y;
+                    while (my_abs(scroll_v_counter) > scroll_v_threshold) {
+                        if (scroll_v_counter < 0) {
+                            //tap_code16(KC_WH_U);
+                            scroll_v_counter += scroll_v_threshold;
+                            rep_v += scroll_v_threshold;
+                        } else {
+                            //tap_code16(KC_WH_D);
+                            scroll_v_counter -= scroll_v_threshold;
+                            rep_v -= scroll_v_threshold;
+                        }
+                        
+                    }
+                } else {
+
+                    scroll_h_counter += mouse_report.x;
+
+                    while (my_abs(scroll_h_counter) > scroll_h_threshold) {
+                        if (scroll_h_counter < 0) {
+                            // tap_code16(KC_WH_L);
+                            scroll_h_counter += scroll_h_threshold;
+                            rep_h += scroll_h_threshold;
+                        } else {
+                            // tap_code16(KC_WH_R);
+                            scroll_h_counter -= scroll_h_threshold;
+                            rep_h -= scroll_h_threshold;
+                        }
+                    }
+                }
+
+                mouse_report.h = rep_h / scroll_h_threshold;
+                mouse_report.v = -rep_v / scroll_v_threshold;
+                mouse_report.x = 0;
+                mouse_report.y = 0;
+            }
+                break;
+
+            case WAITING:
+                if (timer_elapsed(click_timer) > to_clickable_time) {
+                    enable_click_layer();
+                }
+                break;
+
+            default:
+                click_timer = timer_read();
+                state = WAITING;
+        }
+
+        pointing_device_set_report(mouse_report);
+    }
+    else
+    {
+        switch (state) {
+            case CLICKING:
+            case SCROLLING:
+
+                break;
+
+            case CLICKABLE:
+                if (timer_elapsed(click_timer) > to_reset_time) {
+                    disable_click_layer();
+                }
+                break;
+
+             case WAITING:
+                if (timer_elapsed(click_timer) > 50) {
+                    state = NONE;
+                }
+                break;
+
+            default:
+                state = NONE;
+        }
+    }
+
+    mouse_report.x = current_x * 2;
+    mouse_report.y = current_y * 2;
+
+    
+    return mouse_report;
+
+}
 
 
 void keyboard_post_init_user(void) {
@@ -198,44 +448,4 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 */
 
 
-int history_length = 10;
 
-int16_t history_x[10] = {};
-int16_t history_y[10] = {};
-int16_t history_t[10] = {};
-
-
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    dprintf("time:%d x:%d y:%d \n", timer_elapsed(0), mouse_report.x, mouse_report.y);
-
-    int16_t current_x = history_x[0];
-    int16_t current_y = history_y[0];
-
-    int start = 1;
-    int read_count = 10;
-
-    if (current_x != 0 || current_y != 0)
-    {
-        for (int i = start; i < start + read_count && i < history_length; i++)
-        {
-            current_x += history_x[i];
-            current_y += history_y[i];
-            start = i;
-        }
-    }
-
-    for (int i = start; i < history_length; i++)
-    {
-        history_x[i - start] = history_x[i];
-        history_y[i - start] = history_y[i];
-    }
-
-    history_x[history_length - 1] = mouse_report.x;
-    history_y[history_length - 1] = mouse_report.y;
-
-    mouse_report.x = current_x * 2;
-    mouse_report.y = current_y * 2;
-
-    return mouse_report;
-
-}
