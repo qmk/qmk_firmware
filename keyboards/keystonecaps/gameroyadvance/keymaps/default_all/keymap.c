7587f719ee4ba2d7b13316d2137b982a695e8e34
ROYMEETSWORLD <63612683+ROYMEETSWORLD@users.noreply.github.com>
2022-04-19 16:20:33 -0400

    Update keyboards/keystonecaps/gameroyadvance/keymaps/default_all/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keystonecaps/gameroyadvance/keymaps/default_all/keymap.c
+++ keyboards/keystonecaps/gameroyadvance/keymaps/default_all/keymap.c
@@ -43,13 +43,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  )
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    }
-    return false;
-}
