296b26b39500be21bdb3ce77779d518f5b4b4150
ALTAiN <Altainworks@gmail.com>
2022-04-19 15:57:44 +0900

    Typo Fixes

--- keyboards/blaster75/keymaps/default/keymap.c
+++ keyboards/blaster75/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 by Altain Studios
+/* Copyright 2021 by Altain
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- keyboards/blaster75/keymaps/via/keymap.c
+++ keyboards/blaster75/keymaps/via/keymap.c
@@ -1,5 +1,4 @@
-/* Copyright 2021 by Altain Studios
- *
+/* Copyright 2021 by Altain
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
