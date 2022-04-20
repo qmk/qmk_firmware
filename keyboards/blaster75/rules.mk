9b2c9da081e880c5a223817b40a1f212a3f71f4b
ALTAiN <Altainworks@gmail.com>
2022-04-21 08:41:32 +0900

    fixes for keymaps, image links and RGB rules

--- keyboards/blaster75/rules.mk
+++ keyboards/blaster75/rules.mk
@@ -16,5 +16,4 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = no
 
