dd923f045e6f57163f9122ad324ddf9c7f0a6aee
rooski15 <34818505+rooski15@users.noreply.github.com>
2022-04-21 20:33:21 -0700

    Update keyboards/blockboy/ac980mini/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/blockboy/ac980mini/rules.mk
+++ keyboards/blockboy/ac980mini/rules.mk
@@ -12,11 +12,10 @@ BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 
 RGB_MATRIX_ENABLE = yes
