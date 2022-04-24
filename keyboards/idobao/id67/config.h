d16efd5816dd1a4c42db256942d937c0ddee3e81
Vino Rodrigues <github@m.vinorodrigues.com>
2022-04-24 19:13:55 +1000

    don't need RGBLIGHT_ENABLE codeblock

--- keyboards/idobao/id67/config.h
+++ keyboards/idobao/id67/config.h
@@ -46,28 +46,6 @@
 #define DIODE_DIRECTION COL2ROW
 #define RGB_DI_PIN F0
 
-// RGB Light config
-#if defined(RGBLIGHT_ENABLE)
-    #define RGBLED_NUM 77
-    #define RGBLIGHT_HUE_STEP 8
-    #define RGBLIGHT_SAT_STEP 8
-    #define RGBLIGHT_VAL_STEP 8
-    #define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
-    #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-/*== all animations enable ==*/
-    #define RGBLIGHT_ANIMATIONS
-// /*== or choose animations ==*/
-//     #define RGBLIGHT_EFFECT_BREATHING
-//     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//     #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//     #define RGBLIGHT_EFFECT_SNAKE
-//     #define RGBLIGHT_EFFECT_KNIGHT
-//     #define RGBLIGHT_EFFECT_CHRISTMAS
-//     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//     #define RGBLIGHT_EFFECT_RGB_TEST
-//     #define RGBLIGHT_EFFECT_ALTERNATING
-#endif
-
 // RGB Matrix config
 #if defined(RGB_MATRIX_ENABLE)
     #define DRIVER_LED_TOTAL 77
