50a15a902a66c2ff8f1fd9810f60650a734d1101
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-04-25 10:58:01 +1000

    Update config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/idobao/id42/config.h
+++ keyboards/idobao/id42/config.h
@@ -18,13 +18,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x6964  /* "id" */
-#define PRODUCT_ID      0x0042
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    IDOBAO
-#define PRODUCT         ID42
-
 /* Key Matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
