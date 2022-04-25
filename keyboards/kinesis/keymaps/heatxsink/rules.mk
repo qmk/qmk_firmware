82e934b3828d3189b61143fc01096887eba2f4d1
Nicholas Granado <ngranado@gmail.com>
2022-04-25 09:26:55 -0700

    Update keyboards/kinesis/keymaps/heatxsink/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kinesis/keymaps/heatxsink/rules.mk
+++ keyboards/kinesis/keymaps/heatxsink/rules.mk
@@ -1,5 +1,5 @@
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
+NKRO_ENABLE = yes
+SLEEP_LED_ENABLE = yes
