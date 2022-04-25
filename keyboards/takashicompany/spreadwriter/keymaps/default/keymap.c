dbef504b1c56d9dd1e795ad9e49ff61905917a6f
takashicompany <t@kashi.company>
2022-04-26 00:26:39 +0900

    keymap

--- keyboards/takashicompany/spreadwriter/keymaps/default/keymap.c
+++ keyboards/takashicompany/spreadwriter/keymaps/default/keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
         KC_ESC,  LT(2, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
-        KC_TAB, KC_A, KC_S, LT(2, KC_D), KC_F, KC_G, KC_1, KC_H, KC_J, LT(2, KC_K), KC_L, KC_ENT, KC_DEL,
+        KC_TAB, KC_A, KC_S, LT(2, KC_D), KC_F, KC_G, RESET, KC_H, KC_J, LT(2, KC_K), KC_L, KC_ENT, KC_DEL,
         KC_LSFT, SFT_T(KC_Z), GUI_T(KC_X), KC_C, KC_V, KC_B, KC_2, KC_3,  KC_N, KC_M, KC_COMM, CTL_T(KC_DOT), KC_BSPC, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_5, KC_6, KC_LALT, ALT_T(KC_LANG2), SFT_T(KC_TAB), KC_4, KC_SPC, LT(1, KC_LANG1), KC_RALT, KC_RGUI, KC_RCTL, KC_7, KC_8
     )
