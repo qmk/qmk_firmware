dfbd4cd6333fa2c0648fae734364f29442ff0937
Enrique <esaenger@me.com>
2022-04-25 22:35:09 -0500

    removed rgb effects and turned off mouse keys

--- keyboards/crkbd/keymaps/enriquesaenger/rules.mk
+++ keyboards/crkbd/keymaps/enriquesaenger/rules.mk
@@ -1,4 +1,4 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
+MOUSEKEY_ENABLE = no     # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
