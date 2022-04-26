65eeafb4006e68c4ee8c133fe2c698122f298b37
fauxpark <fauxpark@gmail.com>
2022-04-26 16:07:06 +1000

    dactyl_manuform/3x5_3: fix layout

--- keyboards/handwired/dactyl_manuform/3x5_3/rules.mk
+++ keyboards/handwired/dactyl_manuform/3x5_3/rules.mk
@@ -1,10 +1,12 @@
-# Build Options
 # MCU name
 MCU = atmega32u4
 
 # Bootloader selection
 BOOTLOADER = caterina
 
+# Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
