0f9bfabf91117ac1359497b53105952fd52a4a18
mtei <2170248+mtei@users.noreply.github.com>
2022-04-27 01:11:05 +0900

    add users/mtei/{config.h,rules.mk,user_featues.mk,user_options.mk}

---
+++ users/mtei/debug_config.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Takeshi Ishii (mtei@github)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// clang-format off
+
+#ifndef DEBUG_CONFIG_USER_MTEI_H
+#define DEBUG_CONFIG_USER_MTEI_H
+#pragma once
+
+#ifdef KEYBOARD_helix_rev3_5rows
+#endif
+
+#endif /* DEBUG_CONFIG_USER_MTEI_H */
