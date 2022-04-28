59870789bb61be5436b38869cd7bd76cb6a7423b
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-04-28 18:24:09 +0800

    Update encoder.h

--- quantum/encoder.h
+++ quantum/encoder.h
@@ -24,7 +24,6 @@ bool encoder_read(void);
 
 bool encoder_update_kb(uint8_t index, bool clockwise);
 bool encoder_update_user(uint8_t index, bool clockwise);
-void encoder_insert_state(uint8_t index);
 
 #ifdef SPLIT_KEYBOARD
 void encoder_state_raw(uint8_t* slave_state);
