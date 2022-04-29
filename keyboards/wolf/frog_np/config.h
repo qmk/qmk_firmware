9f75a71eaa48ff6f4ff74e707eacace91224a658
Wolf Van Herreweghe <wolfvh@getupgamesofficial.com>
2022-04-29 03:32:12 +0200

    remove depricated QMK codes

--- keyboards/wolf/frog_np/config.h
+++ keyboards/wolf/frog_np/config.h
@@ -60,7 +60,3 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* disable action features */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
