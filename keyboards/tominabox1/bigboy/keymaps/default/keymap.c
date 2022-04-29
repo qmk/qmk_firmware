2f5706cc1e9560426f706a3733381fcc641fd305
TJ Campie <tom.campie@gmail.com>
2022-04-29 17:27:00 -0500

    define rgb effects, move rotary control programming

--- keyboards/tominabox1/bigboy/keymaps/default/keymap.c
+++ keyboards/tominabox1/bigboy/keymaps/default/keymap.c
@@ -32,14 +32,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,     KC_NO,   KC_NO
         ),
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
-    if (clockwise) {
-      tap_code(KC_VOLU);
-    } else {
-      tap_code(KC_VOLD);
-    }
-  }
-  return false;
-}
