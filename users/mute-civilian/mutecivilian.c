6188f33c62021ce91ce7e9b99fca856c0b377364
Nathan <nathan.cooke@compass.com>
2022-04-28 20:13:31 -0400

    capsword allows -

--- users/mute-civilian/mutecivilian.c
+++ users/mute-civilian/mutecivilian.c
@@ -54,7 +54,6 @@ bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
         // Keycodes that continue Caps Word, with shift applied.
         case KC_A ... KC_Z:
-        case KC_MINS:
             add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
             return true;
         // Keycodes that continue Caps Word, without shifting.
@@ -62,6 +61,7 @@ bool caps_word_press_user(uint16_t keycode) {
         case KC_BSPC:
         case KC_DEL:
         case KC_UNDS:
+        case KC_MINS:
         case KC_AMPERSAND:
             return true;
         default:
