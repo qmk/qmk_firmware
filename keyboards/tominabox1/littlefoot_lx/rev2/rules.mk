db9535343d9a9d16a70c81826afe65d1275bbf80
TJ <tom.campie@gmail.com>
2022-04-28 19:37:55 -0500

    Update keyboards/tominabox1/littlefoot_lx/rev2/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/tominabox1/littlefoot_lx/rev2/rules.mk
+++ keyboards/tominabox1/littlefoot_lx/rev2/rules.mk
@@ -5,14 +5,14 @@ MCU = atmega32u4
 BOOTLOADER = atmel-dfu
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes	    # Enable bootmagic lite
-MOUSEKEY_ENABLE = no	    # Mouse keys
-EXTRAKEY_ENABLE = no	    # Audio control and System control
-CONSOLE_ENABLE = no	        # Console for debug
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes		    # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
