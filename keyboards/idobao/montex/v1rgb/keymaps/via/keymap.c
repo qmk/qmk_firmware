c0ee4909555a1fcb8bb4ebd863f735eb2c00fb9e
Vino Rodrigues <github@m.vinorodrigues.com>
2022-04-30 21:02:44 +1000

    copt v2 keymap with [Fn] at bottom left

--- keyboards/idobao/montex/v1rgb/keymaps/via/keymap.c
+++ keyboards/idobao/montex/v1rgb/keymaps/via/keymap.c
@@ -1,28 +1,21 @@
-/* Copyright 2021 NachoxMacho
-*  Copyright 2022 peepeetee
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
-*/
+// Copyright 2022 peepeetee (@peepeetee)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+
+enum montex_layers {
+  _BASE,
+  _FN,
+  _EMPTY2, // Just to initialize dynamic layers in VIA
+  _EMPTY3
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┌───┬───┬───┬───┐
-     * │ ` │Esc│Tab│MO1│Bsp│
+     * │Esc│Tab│ F1│Cal│Bsp│
      * ├───┼───┼───┼───┼───┤
-     * │ , │Num│ / │ * │ - │
+     * │Ctr│Num│ / │ * │ - │
      * ├───┼───┼───┼───┼───┤
      * │Gui│ 7 │ 8 │ 9 │   │
      * ├───┼───┼───┼───┤ + │
@@ -30,42 +23,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┤
      * │Shi│ 1 │ 2 │ 3 │   │
      * ├───┼───┴───┼───┤Ent│
-     * │Ctr│ 0     │ . │   │
+     * │MO1│ 0     │ . │   │
      * └───┴───────┴───┴───┘
      */
-    [0] = LAYOUT_numpad_6x5(
-        KC_GRAVE,  KC_ESC,  KC_TAB,  MO(1),   KC_BSPC,
-        KC_COMMA,  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_LGUI,   KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_LALT,   KC_P4,   KC_P5,   KC_P6,
-        KC_LSHIFT, KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_LCTRL,  KC_P0,            KC_PDOT
+    [_BASE] = LAYOUT(
+        KC_GESC,   KC_TAB,   KC_F1,   KC_CALC, KC_BSPC,
+        KC_LCTRL,  KC_NLCK,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_LGUI,   KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
+        KC_LALT,   KC_P4,    KC_P5,   KC_P6,
+        KC_LSHIFT, KC_P1,    KC_P2,   KC_P3,   KC_PENT,
+        MO(1),     KC_P0,             KC_PDOT
     ),
 
     /*
      * ┌───┌───┐───┬───┬───┐
-     * │Rst│ ` │Tab│MO1│Bsp│
+     * │Rst│TOG│MOD│PSs│SLk│
      * ├───┼───┘───┼───┼───┤
-     * │ , │Num│ / │ * │ - │
+     * │Hu+│   │Sp-│Sp+│Br-│
      * ├───┼───┼───┼───┼───┤
-     * │Gui│Hom│ ↑ │PgU│   │
-     * ├───┼───┼───┼───┤ + │
-     * │Alt│ ← │   │ → │   │
+     * │Hu-│Hom│ ↑ │PgU│   │
+     * ├───┼───┼───┼───┤Br+│
+     * │St+│ ← │   │ → │   │
      * ├───┼───┼───┼───┤───┤
-     * │Shi│End│ ↓ │PgD│   │
+     * │St-│End│ ↓ │PgD│   │
      * ├───┼───┴───┼───┤Ent│
-     * │Ctr│Insert │Del│   │
+     * │mo1│Insert │Del│   │
      * └───┴───────┴───┘───┘
      */
-    [1] = LAYOUT_numpad_6x5(
-        RESET,   RGB_TOG, RGB_MOD, _______, _______,
-        _______, _______, _______, _______, _______,
-        RGB_SPI, RGB_HUI, RGB_SAI, RGB_VAI, _______,
-        RGB_SPD, RGB_HUD, RGB_SAD, RGB_VAD,
-        _______, _______, _______, _______, _______,
-        _______, _______,           _______
+    [_FN] = LAYOUT(
+        RESET,   RGB_TOG, RGB_MOD, KC_PSCR, KC_SLCK,
+        RGB_HUI, XXXXXXX, RGB_SPD, RGB_SPI, RGB_VAD,
+        RGB_HUD, KC_HOME, KC_UP,   KC_PGUP, RGB_VAI,
+        RGB_SAI, KC_LEFT, XXXXXXX, KC_RGHT,
+        RGB_SAD, KC_END,  KC_DOWN, KC_PGDN, KC_ENT,
+        _______, KC_INS,           KC_DEL
     ),
-    [2] = LAYOUT_numpad_6x5(
+
+    /*
+     * ┌───┌───┐───┬───┬───┐
+     * │   │   │   │   │   │
+     * ├───┼───┘───┼───┼───┤
+     * │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │
+     * ├───┼───┼───┼───┤   │
+     * │   │   │   │   │   │
+     * ├───┼───┼───┼───┤───┤
+     * │   │   │   │   │   │
+     * ├───┼───┴───┼───┤   │
+     * │   │       │   │   │
+     * └───┴───────┴───┘───┘
+     */
+    [_EMPTY2] = LAYOUT(
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,
@@ -73,7 +82,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______,
         _______, _______,           _______
     ),
-    [3] = LAYOUT_numpad_6x5(
+
+    [_EMPTY3] = LAYOUT(
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,
@@ -81,5 +91,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______,
         _______, _______,           _______
     )
-
 };
