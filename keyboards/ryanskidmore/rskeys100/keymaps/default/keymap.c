c9a89f904dc3ce51d7e8bdeda51c0a4d7723213d
Ryan Skidmore <github@ryanskidmore.co.uk>
2022-04-30 14:12:36 +0100

    keyboards/ryanskidmore/rskeys100: pr feedback

--- keyboards/ryanskidmore/rskeys100/keymaps/default/keymap.c
+++ keyboards/ryanskidmore/rskeys100/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
      */
     /* Default Layer */
-    [0] = LAYOUT(
+    [0] = LAYOUT_fullsize_iso(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  LT(1, KC_F12),       KC_PSCR, KC_SLCK, KC_PAUS,
 
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
     ),
     /* RGB Control Layer */
-    [1] = LAYOUT(
+    [1] = LAYOUT_fullsize_iso(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______,
 
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,    _______, _______, _______, _______,
@@ -52,7 +52,7 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
      * matrix animation. */
 
     if (host_keyboard_led_state().caps_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(76, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(73, 255, 255, 255);
     }
 
     if (host_keyboard_led_state().num_lock) {
