4c4c392d6eef4fff1cc0b3407377220085f8f07e
Takeshi ISHII <2170248+mtei@users.noreply.github.com>
2022-04-30 18:34:19 +0900

    Update keyboards/helix/pico/keymaps/mtei/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/helix/pico/keymaps/mtei/keymap.c
+++ keyboards/helix/pico/keymaps/mtei/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include <key_blocks.h>
+#include "key_blocks.h"
 
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 #define kc5(a,b,c,d,e) KC_##a, KC_##b, KC_##c, KC_##d, KC_##e
