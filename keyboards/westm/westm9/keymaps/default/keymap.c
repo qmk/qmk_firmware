216fa76c52089363ae2e3240729e6398974bf771
WestMProducts <WestMProducts@outlook.com>
2022-04-30 01:34:13 -0700

    updated keymap

--- keyboards/westm/westm9/keymaps/default/keymap.c
+++ keyboards/westm/westm9/keymaps/default/keymap.c
@@ -23,8 +23,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, MO(1)
     ),
     [1] = LAYOUT_macropad(
-        RGB_TOG, RGB_RMOD, RGB_MOD,
-        RGB_VAD, RGB_VAI, KC_NO,
+        RGB_TOG, RGB_RMOD, RGB_SAI,
+        RGB_VAD, RGB_VAI, RGB_SAD,
         RGB_HUD, RGB_HUI, KC_NO
     )
 };
