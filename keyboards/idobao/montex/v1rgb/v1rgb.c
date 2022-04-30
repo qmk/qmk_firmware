1a3c749c58a74ed4e5d43cd954d899d365e5f916
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-01 08:48:33 +1000

    added keymap readme

--- keyboards/idobao/montex/v1rgb/v1rgb.c
+++ keyboards/idobao/montex/v1rgb/v1rgb.c
@@ -5,7 +5,7 @@
 
 #ifdef RGB_MATRIX_ENABLE
 
-/* WS2812 index start at 0
+/* WS2812 indices always start at 0
  * underglow = 0-3
  * note: starting point of 4 and jump from 6 to 7
  * ┌───┌───┐───┬───┬───┐
@@ -33,16 +33,16 @@ led_config_t g_led_config = { {
     { 7,  8,  9,  10, NO_LED}
 }, {
     // LED Index to Physical Position
-    /* underglow: because this is under the kb, you want the colors on the edge of the acrylic plate */
-    { 224,0  }, { 224,64},  {   0,64 }, {  0,0   },             // 0-3 (underglow)
+    /* underglow: because this is under the kb, you want the colors on the edge (corners) of the acrylic plate */
+    { 224,0  }, { 224,64},  {   0,64 }, {  0,0   },              // 0-3 (underglow)
     /* per-key: origins generated from https://xelus.netlify.app/guides/KLE_to_RGB_parser */
-    { 112,0  }, {  56,0  }, {   0,0  }, /* ---- */  /* ---- */  // 4-6 (top row middle to left)
-    {   0,64 }, {  84,64 }, { 168,64 },                         // 7-9 (jump to bottom left)
-    { 224,58 }, { 168,41 }, { 112,41 }, {  56,41 }, {   0,41 }, // 10-14 (flow Z pattern upwards...)
-    {   0,29 }, {  56,29 }, { 112,29 }, { 168,29 },             // 15-18
-    { 224,35 }, { 168,29 }, { 112,29 }, {  56,29 }, {   0,29 }, // 19-23
-    {   0,17 }, {  56,17 }, { 112,17 }, { 168,17 }, { 224,17 }, // 24-28
-    /* ---- */  /* ---- */  /* ---- */  { 224,0  }, { 168,0  }  // 29-30 (end at top)
+    { 112,0  }, {  56,0  }, {   0,0  }, /* ---- */  /* ---- */   // 4-6 (top row middle to left)
+    {   0,64 }, {  84,64 }, { 168,64 },                          // 7-9 (jump to bottom left)
+    { 224,58 }, { 168,41 }, { 112,41 }, {  56,41 }, {   0,41 },  // 10-14 (flow Z pattern upwards...)
+    {   0,29 }, {  56,29 }, { 112,29 }, { 168,29 },              // 15-18
+    { 224,35 }, { 168,29 }, { 112,29 }, {  56,29 }, {   0,29 },  // 19-23
+    {   0,17 }, {  56,17 }, { 112,17 }, { 168,17 }, { 224,17 },  // 24-28
+    /* ---- */  /* ---- */  /* ---- */  { 224,0  }, { 168,0  }   // 29-30 (end at top)
 }, {
     // LED Index to Flag
     2, 2, 2, 2,     // 0 1 2 3 (underglow)
