043a21c5793345f068721a15dfc22fa6e079424b
4pplet <mail@4pplet.com>
2022-05-02 08:37:42 +0200

    Update keyboards/4pplet/yakiimo/proto/proto.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/4pplet/yakiimo/proto/proto.c
+++ keyboards/4pplet/yakiimo/proto/proto.c
@@ -15,20 +15,3 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "proto.h"
-
-void matrix_init_kb(void) {
-
-    setPinOutput(CAPS_PIN);
-    setPinOutput(SCROLL_PIN);
-
-    matrix_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if(res) {
-        writePin(CAPS_PIN, led_state.caps_lock);
-        writePin(SCROLL_PIN, led_state.scroll_lock);
-    }
-    return res;
-}
