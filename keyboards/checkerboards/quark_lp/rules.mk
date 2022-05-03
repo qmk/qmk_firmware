5a0ee18ae31477cc4852d9f3c442f720dd91e2dd
npspears <40127181+npspears@users.noreply.github.com>
2022-05-03 10:04:40 -0500

    add RGB Matrix

--- keyboards/checkerboards/quark_lp/rules.mk
+++ keyboards/checkerboards/quark_lp/rules.mk
@@ -16,3 +16,4 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = yes
