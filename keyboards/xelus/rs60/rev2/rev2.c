7aa6eda414ed5ef1d5e3b17b19cc397125a2e3bf
Xelus22 <preyas22@gmail.com>
2022-05-03 15:06:37 +1000

    rs60 rev2 initial

--- keyboards/xelus/rs60/rev2/rev2.c
+++ keyboards/xelus/rs60/rev2/rev2.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 Harrison Chan (Xelus)
+/* Copyright 2021 Harrison Chan (Xelus)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,5 +13,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#include "config_common.h"
+ #include "rev2.h"
