4c95ad454a16149f50c6d872d47e29172b28af1e
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-05-04 16:58:32 +0700

    Update keyboards/mechlovin/adelais/rgb_led/rev3/mcuconf.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechlovin/adelais/rgb_led/rev3/mcuconf.h
+++ keyboards/mechlovin/adelais/rgb_led/rev3/mcuconf.h
@@ -1,30 +1,9 @@
-/*
-    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
-#ifndef _MCUCONF_H_
-#define _MCUCONF_H_
-
-#define STM32F103_MCUCONF
-
-/*
- * STM32F103 drivers configuration.
- * The following settings override the default settings present in
- * the various device driver implementation headers.
- * Note that the settings for each driver only have effect if the whole
- * driver is enabled in halconf.h.
+/* Copyright 2020 QMK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
  *
  * IRQ priorities:
  * 15...0       Lowest...Highest.
