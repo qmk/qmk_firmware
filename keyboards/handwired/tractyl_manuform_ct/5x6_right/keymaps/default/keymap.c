ab538bca3c66d9599ec7bf7400ca6a13177a7222
Chris Trotter <ctrotter@gmail.com>
2022-05-04 13:09:22 -0400

    updating the keymap, and disabling console output

--- keyboards/handwired/tractyl_manuform_ct/5x6_right/keymaps/default/keymap.c
+++ keyboards/handwired/tractyl_manuform_ct/5x6_right/keymaps/default/keymap.c
@@ -99,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 KC_LSFT, KC_LALT, KC_LGUI, KC_R,        KC_MPLY, KC_LCTL, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT
 ),
 [_NAV] = LAYOUT( \
-    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
-    KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_SCLN,
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_MS_BTN3,    KC_O,    KC_P,    KC_MINUS,
+    KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_MS_BTN1,    KC_MS_BTN2,    KC_L,    KC_QUOT, KC_SCLN,
     KC_ESC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH, KC_P,
-                KC_BSPC, KC_DEL,  KC_ESC, KC_LBRC, KC_RBRC,        KC_MPLY, KC_LCTL, KC_LGUI, KC_R, KC_ENTER, KC_SPACE,
-                                KC_LSFT, KC_LALT, KC_LGUI, KC_R,        KC_COMM, KC_DOT, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT
+                KC_BSPC, KC_DEL,  KC_ESC, KC_LBRC, KC_RBRC,        KC_HOME, KC_END, KC_LGUI, KC_R, KC_ENTER, KC_SPACE,
+                                KC_LSFT, KC_LALT, KC_LGUI, KC_R,        KC_PGUP, KC_PGDN, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT
 )
 };
