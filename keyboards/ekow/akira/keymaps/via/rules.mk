23b70c20760d973f047087ac65512ceb737286aa
eugenepy <tn00372136@gmail.com>
2022-05-04 17:49:06 +0800

    modify ekow/akira:via

--- keyboards/ekow/akira/keymaps/via/rules.mk
+++ keyboards/ekow/akira/keymaps/via/rules.mk
@@ -1,1 +1,8 @@
 VIA_ENABLE = yes
+
+CONSOLE_ENABLE = no 		# Console for debug
+COMMAND_ENABLE = no			# Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
