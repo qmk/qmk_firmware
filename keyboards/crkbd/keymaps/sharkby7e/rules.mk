a52c4b02c04418f1191bb1678b37b5afdd8b10da
sharkbyte <sgquin@gmail.com>
2022-05-04 01:39:51 -0700

    updated for sharkby7e

--- keyboards/crkbd/keymaps/sharkby7e/rules.mk
+++ keyboards/crkbd/keymaps/sharkby7e/rules.mk
@@ -1,7 +1,6 @@
-MOUSEKEY_ENABLE   = yes    # Mouse keys
+# MOUSEKEY_ENABLE   = yes    # Mouse keys
 RGBLIGHT_ENABLE   = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE       = yes
 OLED_DRIVER       = SSD1306
 LTO_ENABLE        = yes
 BOOTLOADER        = caterina
-# AUTO_SHIFT_ENABLE = yes
