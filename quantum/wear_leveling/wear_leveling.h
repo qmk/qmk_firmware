d315df9fbbfc4829958d123cc80ae8e816cac260
Nick Brassel <nick@tzarc.org>
2022-05-05 20:33:07 +1000

    Docs tweaks.

--- quantum/wear_leveling/wear_leveling.h
+++ quantum/wear_leveling/wear_leveling.h
@@ -10,7 +10,7 @@
 typedef enum wear_leveling_status_t {
     WEAR_LEVELING_FAILED,      //< Invocation failed
     WEAR_LEVELING_SUCCESS,     //< Invocation succeeded
-    WEAR_LEVELING_CONSOLIDATED //< Invocation succeded, consolidation occurred
+    WEAR_LEVELING_CONSOLIDATED //< Invocation succeeded, consolidation occurred
 } wear_leveling_status_t;
 
 /**
@@ -23,12 +23,18 @@ wear_leveling_status_t wear_leveling_init(void);
 /**
  * Wear-leveling erasure.
  *
+ * Clears the wear-leveling area, with the definition that the "reset state" of all data is zero.
+ *
  * @return Status of the request
  */
 wear_leveling_status_t wear_leveling_erase(void);
 
 /**
- * Writes logical data into the backing store. Skips writes if there are no changes to values.
+ * Writes logical data into the backing store.
+ *
+ * Skips writes if there are no changes to written values. The entire written block is considered when attempting to
+ * determine if an overwrite should occur -- if there is any data mismatch the entire block will be written to the log,
+ * not just the changed bytes.
  *
  * @param address[in] the logical address to write data
  * @param value[in] pointer to the source buffer
