c610b3767733011a5884ae41b0b20f133e5feea4
Eugene-Yuan Kou <r02323025@ntu.edu.tw>
2022-05-05 08:53:48 +0800

    Update keyboards/ekow/akira/keymaps/via/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ekow/akira/keymaps/via/rules.mk
+++ keyboards/ekow/akira/keymaps/via/rules.mk
@@ -1,8 +1,3 @@
 VIA_ENABLE = yes
-
-CONSOLE_ENABLE = no 		# Console for debug
-COMMAND_ENABLE = no			# Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
