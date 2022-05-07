02d5acb4b127c3f6f645c6ff77364db54071c396
zvecr <git@zvecr.com>
2022-05-07 20:31:19 +0100

    Cherry pick some tidy up from xap branch

--- lib/python/qmk/cli/generate/api.py
+++ lib/python/qmk/cli/generate/api.py
@@ -17,9 +17,22 @@ TEMPLATE_PATH = DATA_PATH / 'templates/api/'
 BUILD_API_PATH = Path('.build/api_data/')
 
 
+def _filtered_keyboard_list():
+    """Perform basic filtering of list_keyboards
+    """
+    keyboard_list = list_keyboards()
+    if cli.args.filter:
+        kb_list = []
+        for keyboard_name in keyboard_list:
+            if any(i in keyboard_name for i in cli.args.filter):
+                kb_list.append(keyboard_name)
+        keyboard_list = kb_list
+    return keyboard_list
+
+
 @cli.argument('-n', '--dry-run', arg_only=True, action='store_true', help="Don't write the data to disk.")
 @cli.argument('-f', '--filter', arg_only=True, action='append', default=[], help="Filter the list of keyboards based on partial name matches the supplied value. May be passed multiple times.")
-@cli.subcommand('Creates a new keymap for the keyboard of your choosing', hidden=False if cli.config.user.developer else True)
+@cli.subcommand('Generate QMK API data', hidden=False if cli.config.user.developer else True)
 def generate_api(cli):
     """Generates the QMK API data.
     """
@@ -37,13 +50,7 @@ def generate_api(cli):
     shutil.copytree(DATA_PATH, v1_dir)
 
     # Filter down when required
-    keyboard_list = list_keyboards()
-    if cli.args.filter:
-        kb_list = []
-        for keyboard_name in keyboard_list:
-            if any(i in keyboard_name for i in cli.args.filter):
-                kb_list.append(keyboard_name)
-        keyboard_list = kb_list
+    keyboard_list = _filtered_keyboard_list()
 
     kb_all = {}
     usb_list = {}
