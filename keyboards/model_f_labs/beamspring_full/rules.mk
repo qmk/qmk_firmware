141a040c53ccd4f10f1064c7bf84e27e1167a547
Matthew J. Wolf <matthew.wolf.hpsdr@speciosus.net>
2022-05-07 16:38:38 -0400

    Purdea Andrei 15-aug-2021 - Added brand new beamspring command by model F labs.

---
+++ keyboards/model_f_labs/beamspring_full/rules.mk
@@ -0,0 +1,2 @@
+DEFAULT_FOLDER = model_f_labs/beamspring_full/wcass
+# Brand new model f keyboards by default come with the wcass controller.
---
+++ keyboards/model_f_labs/beamspring_ssk/rules.mk
@@ -0,0 +1,2 @@
+DEFAULT_FOLDER = model_f_labs/beamspring_ssk/wcass
+# Brand new model f keyboards by default come with the wcass controller.
