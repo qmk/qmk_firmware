a531f76f2e176c62c96b6163ae0a52767960bfee
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2022-05-08 16:33:56 -0300

    First Frog Mini S and H support

---
+++ keyboards/frog_mini/fmh/config.h
@@ -0,0 +1,27 @@
+/*
+Copyright 2020 Gondolindrim
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0x2737 // GW for GeonWorks
+#define PRODUCT_ID 0x2D28 // MH for "mini-H"
+#define DEVICE_VER 0x0001 // Rev. Alpha
+#define MANUFACTURER GeonWorks
+#define PRODUCT FrogMini FM-H
---
+++ keyboards/frog_mini/fms/config.h
@@ -0,0 +1,15 @@
+#define VENDOR_ID 0x2737 // GW for GeonWorks
+#define PRODUCT_ID 0x2D33 // MS for "mini-S"
+#define DEVICE_VER 0x0001 // Rev. Alpha
+#define MANUFACTURER GeonWorks
+#define PRODUCT FrogMini FM-S
+
+#define BACKLIGHT_PIN           A10
+#define BACKLIGHT_PWM_DRIVER    PWMD1
+#define BACKLIGHT_PWM_CHANNEL   3
+#define BACKLIGHT_PAL_MODE      1
+#define BACKLIGHT_LEVELS 20
+#define BACKLIGHT_BREATHING
+#define BREATHING_PERIOD 5
+
+#define BACKLIGHT_CAPS_LOCK
