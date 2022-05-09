8fa39eeed779029e8d7ee424027f37b92973c651
yiancar <yiancar@gmail.com>
2022-05-09 17:21:43 +0100

    Add APM32 option

--- keyboards/lvl_type1_80/rules.mk
+++ keyboards/lvl_type1_80/rules.mk
@@ -4,6 +4,9 @@ MCU = STM32F072
 # Bootloader selection
 BOOTLOADER = stm32-dfu
 
+# Wildcard to allow APM32 MCU
+DFU_SUFFIX_ARGS = -v FFFF -p FFFF
+
 # Do not put the microcontroller into power saving mode
 # when we get USB suspend event. We want it to keep updating
 # backlight effects.
