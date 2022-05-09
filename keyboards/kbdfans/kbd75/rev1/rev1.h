bc4ede7b14cc111b752ee5e57019abc71f46ef2c
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-05-09 13:36:28 -0700

    refactor layout macros
    - switch `LAYOUT_ansi_1u` with `LAYOUT_75_ansi`
      - makes `LAYOUT_75_ansi` the canonical macro name, with `LAYOUT_ansi_1u` as an alias for backwards compatibility
      - move `LAYOUT_ansi_1u` alias to `info.json` files from revision headers
    - remove `LAYOUT_iso_1u` (was unused)
    - rename `LAYOUT_ansi` to `LAYOUT_75_ansi_rwkl`
    - rename `LAYOUT_iso` to `LAYOUT_75_iso_rwkl`

--- keyboards/kbdfans/kbd75/rev1/rev1.h
+++ keyboards/kbdfans/kbd75/rev1/rev1.h
@@ -56,7 +56,7 @@
  * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
  */
 
-#define LAYOUT_ansi_1u( \
+#define LAYOUT_75_ansi( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
     K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, \
@@ -69,24 +69,6 @@
     { K20, ___, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F }, \
     { K30, ___, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, ___, K3F }, \
     { K40, ___, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, ___, K4D, K4E, K4F }, \
-    { K50, K51, ___, K53, ___, ___, K56, ___, ___, ___, K5A, K5B, K5C, K5D, K5E, K5F }  \
-}
-
-#define LAYOUT_75_ansi LAYOUT_ansi_1u
-
-#define LAYOUT_iso_1u( \
-    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
-    K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K3D, K2F, \
-    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K2E,      K3F, \
-    K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B,      K4D, K4E, K4F, \
-    K50, K51,      K53,           K56,                K5A, K5B, K5C, K5D, K5E, K5F  \
-) { \
-    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, ___, K1E, K1F }, \
-    { K20, ___, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F }, \
-    { K30, ___, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, ___, K3F }, \
-    { K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, ___, K4D, K4E, K4F }, \
     { K50, K51, ___, K53, ___, ___, K56, ___, ___, ___, K5A, K5B, K5C, K5D, K5E, K5F }  \
 }
 
@@ -112,7 +94,7 @@
  * └────┴────┴────┴────────────────────────┴─────┴─────┴───┴───┴───┘
  */
 
-#define LAYOUT_ansi( \
+#define LAYOUT_75_ansi_rwkl( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
     K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, \
@@ -128,7 +110,7 @@
     { K50, K51, ___, K53, ___, ___, K56, ___, ___, ___, K5A, ___, K5C, K5D, K5E, K5F }  \
 }
 
-#define LAYOUT_iso( \
+#define LAYOUT_75_iso_rwkl( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
     K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K3D, K2F, \
