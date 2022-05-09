73b06285b28891e0257f85978addb84d1a0aacf6
duoshock <33636898+duoshock@users.noreply.github.com>
2022-05-10 00:23:05 +0100

    Update keyboards/merge/um80/keymaps/via/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/merge/um80/keymaps/via/config.h
+++ keyboards/merge/um80/keymaps/via/config.h
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
