e482c292a75e518c9fcd7244c086a1dd64884ebe
Santiago Intxausti <intxausti@gmail.com>
2022-05-11 00:07:21 +0200

    Se hacen persistentes ls teclas Control Alt GUI Altgr

--- keyboards/ymdk/ymd40/v2/keymaps/custom01/config.h
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/config.h
@@ -1,5 +1,7 @@
 #define TAPPING_TERM 200
 #define LEADER_TIMEOUT 1000
 #define TAPPING_TOGGLE 2
-#define ONESHOT_TAP_TOGGLE 1  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
+// Tapping this number of times holds the key until tapped once again.
+#define ONESHOT_TAP_TOGGLE 2
+// Time (in ms) before the one shot key is released
+#define ONESHOT_TIMEOUT 200
--- keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
@@ -88,10 +88,10 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY]=LAYOUT_ortho_4x12(
-		TD(TD_RHAND_LAYER),		KC_Q,		KC_W,			KC_E,			KC_R,					KC_T,					KC_Y,					KC_U,				KC_I,				KC_O,		KC_P,		KC_BSPC,
-		LT(_MOV,KC_ESC),		KC_A,		KC_S,			KC_D,			KC_F,					KC_G,					KC_H,					KC_J,				KC_K,				KC_L,		KC_SCLN,	KC_QUOT,
-		TD(TD_SIFT_CAPSLOCK),	KC_Z,		KC_X,			KC_C,			KC_V,					KC_B,					TD(TD_ENE),				KC_M,				KC_COMM,			KC_DOT,		KC_SLSH,	KC_SFTENT,
-		KC_LCTL,				KC_LALT,	OSM(MOD_RALT),	KC_LGUI,		LT(_LOWER,KC_BSPC),		LT(_ONE_HAND,KC_SPACE),	LT(_ONE_HAND,KC_SPACE),	LT(_RAISE,KC_ENT),	TD(TD_WIN_MENU),	KC_RALT,	KC_RCTL,	KC_LEAD
+		TD(TD_RHAND_LAYER),		KC_Q,			KC_W,			KC_E,			KC_R,					KC_T,					KC_Y,					KC_U,				KC_I,				KC_O,		KC_P,		KC_BSPC,
+		LT(_MOV,KC_ESC),		KC_A,			KC_S,			KC_D,			KC_F,					KC_G,					KC_H,					KC_J,				KC_K,				KC_L,		KC_SCLN,	KC_QUOT,
+		TD(TD_SIFT_CAPSLOCK),	KC_Z,			KC_X,			KC_C,			KC_V,					KC_B,					TD(TD_ENE),				KC_M,				KC_COMM,			KC_DOT,		KC_SLSH,	KC_SFTENT,
+		OSM(MOD_LCTL),			OSM(MOD_LALT),	OSM(MOD_RALT),	OSM(MOD_LGUI),	LT(_LOWER,KC_BSPC),		LT(_ONE_HAND,KC_SPACE),	LT(_ONE_HAND,KC_SPACE),	LT(_RAISE,KC_ENT),	TD(TD_WIN_MENU),	KC_RALT,	KC_RCTL,	KC_LEAD
 	),
 
     [_RAISE] = LAYOUT_ortho_4x12(
