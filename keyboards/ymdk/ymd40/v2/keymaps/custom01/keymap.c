c6b7fd84cad6ca5895f9ad1e7dd7d46d3f6f75af
Santiago Intxausti <intxausti@gmail.com>
2022-05-10 20:53:18 +0200

    Se cambia de sitio el acceso a la capa left hand.

--- keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
@@ -88,10 +88,10 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY]=LAYOUT_ortho_4x12(
-		TD(TD_RHAND_LAYER),		KC_Q,		KC_W,			KC_E,			KC_R,					KC_T,					KC_Y,					KC_U,				KC_I,				KC_O,		KC_P,				KC_BSPC,
-		LT(_MOV,KC_ESC),		KC_A,		KC_S,			KC_D,			KC_F,					KC_G,					KC_H,					KC_J,				KC_K,				KC_L,		KC_SCLN,			KC_QUOT,
-		TD(TD_SIFT_CAPSLOCK),	KC_Z,		KC_X,			KC_C,			KC_V,					KC_B,					TD(TD_ENE),				KC_M,				KC_COMM,			KC_DOT,		KC_SLSH,			KC_SFTENT,
-		KC_LCTL,				KC_LALT,	OSM(MOD_RALT),	KC_LGUI,		LT(_LOWER,KC_BSPC),		LT(_ONE_HAND,KC_SPACE),	LT(_ONE_HAND,KC_SPACE),	LT(_RAISE,KC_ENT),	TD(TD_WIN_MENU),	KC_RALT,	TG(_LEFT_NUM_PAD),	KC_LEAD
+		TD(TD_RHAND_LAYER),		KC_Q,		KC_W,			KC_E,			KC_R,					KC_T,					KC_Y,					KC_U,				KC_I,				KC_O,		KC_P,		KC_BSPC,
+		LT(_MOV,KC_ESC),		KC_A,		KC_S,			KC_D,			KC_F,					KC_G,					KC_H,					KC_J,				KC_K,				KC_L,		KC_SCLN,	KC_QUOT,
+		TD(TD_SIFT_CAPSLOCK),	KC_Z,		KC_X,			KC_C,			KC_V,					KC_B,					TD(TD_ENE),				KC_M,				KC_COMM,			KC_DOT,		KC_SLSH,	KC_SFTENT,
+		KC_LCTL,				KC_LALT,	OSM(MOD_RALT),	KC_LGUI,		LT(_LOWER,KC_BSPC),		LT(_ONE_HAND,KC_SPACE),	LT(_ONE_HAND,KC_SPACE),	LT(_RAISE,KC_ENT),	TD(TD_WIN_MENU),	KC_RALT,	KC_RCTL,	KC_LEAD
 	),
 
     [_RAISE] = LAYOUT_ortho_4x12(
@@ -109,10 +109,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
     [_ADJUST] = LAYOUT_ortho_4x12(
-		RESET,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		DEBUG,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		LAG_NRM,	LAG_SWP,	KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		DM_REC1,	DM_REC2,	DM_RSTP,	DM_RSTP,	DM_RSTP,
-		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS
+		RESET,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		TO(_LEFT_NUM_PAD),	KC_NO,		DEBUG,
+		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		LAG_NRM,	LAG_SWP,	KC_NO,		KC_NO,		KC_NO,				KC_NO,		KC_NO,
+		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		DM_REC1,	DM_REC2,	DM_RSTP,			DM_RSTP,	DM_RSTP,
+		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,			KC_TRNS,	KC_TRNS
     ),
 
     [_MOV] = LAYOUT_ortho_4x12(
@@ -137,10 +137,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
     [_LEFT_NUM_PAD] = LAYOUT_ortho_4x12(
-        KC_TAB,   				KC_BSPC, 	KC_7, 		KC_8,	KC_9, 	KC_NO,		KC_NO, 		KC_NO,	KC_NO,	KC_NO,	KC_NO,		KC_NO,
-        LM(_QWERTY, MOD_LCTL),	KC_BSPC, 	KC_4, 		KC_5, 	KC_6,	KC_NO,		KC_NO, 		KC_NO, 	KC_NO, 	KC_NO, 	KC_NO,		KC_NO,
-        LM(_QWERTY, MOD_LCTL), 	KC_BSPC, 	KC_1, 		KC_2, 	KC_3, 	KC_NO, 		KC_NO, 		KC_NO,	KC_NO, 	KC_NO,	KC_NO,		KC_NO,
-        LM(_QWERTY, MOD_LCTL),	KC_0, 		KC_0,  		KC_0, 	KC_0, 	A(KC_TAB), 	A(KC_TAB), 	KC_NO,	KC_NO, 	KC_NO, 	KC_TRNS,	KC_TRNS
+        KC_TAB,   				KC_BSPC, 	KC_7, 		KC_8,	KC_9, 	KC_NO,		KC_NO, 		KC_NO,	KC_NO,	KC_TRNS,	KC_NO,		TO(_QWERTY),
+        LM(_QWERTY, MOD_LCTL),	KC_BSPC, 	KC_4, 		KC_5, 	KC_6,	KC_NO,		KC_NO, 		KC_NO, 	KC_NO, 	KC_NO, 		KC_NO,		KC_NO,
+        LM(_QWERTY, MOD_LCTL), 	KC_BSPC, 	KC_1, 		KC_2, 	KC_3, 	KC_NO, 		KC_NO, 		KC_NO,	KC_NO, 	KC_NO,		KC_NO,		KC_NO,
+        LM(_QWERTY, MOD_LCTL),	KC_0, 		KC_0,  		KC_0, 	KC_0, 	A(KC_TAB), 	A(KC_TAB), 	KC_NO,	KC_NO, 	KC_NO, 		KC_TRNS,	KC_TRNS
     )
 
 };
