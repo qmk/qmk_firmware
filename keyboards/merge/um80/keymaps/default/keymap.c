336534792b8e526b7091640e01bca8b48a6efc6d
duoshock <33636898+duoshock@users.noreply.github.com>
2022-05-12 00:28:56 +0100

    Update keyboards/merge/um80/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/merge/um80/keymaps/default/keymap.c
+++ keyboards/merge/um80/keymaps/default/keymap.c
@@ -69,20 +69,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TRNS,                               KC_TRNS, KC_TRNS, KC_TRNS
 ),
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* Master Left */
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    } else if (index == 1) { /* Master Right */
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    return true;
-}
