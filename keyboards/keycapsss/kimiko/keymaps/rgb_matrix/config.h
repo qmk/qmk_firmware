877d46073a0025e4ca5ace660fb128dbdfa8bd71
jairo-vidaip <jairo.ramirez@vidaip.es>
2022-05-12 01:26:45 +0200

    Edited ifdef rgb_matrix_enable with the necessary parameters and effects.

--- keyboards/keycapsss/kimiko/keymaps/rgb_matrix/config.h
+++ keyboards/keycapsss/kimiko/keymaps/rgb_matrix/config.h
@@ -1,5 +1,6 @@
 /* Copyright 2019 MechMerlin
  * Copyright 2020 @ben_roe (keycapsss.com)
+ * Copyright 2022 @oriaj3
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,27 +23,51 @@
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define OLED_FONT_H "keyboards/keycapsss/kimiko/keymaps/default/glcdfont.c"
+#define OLED_FONT_H "keyboards/keycapsss/kimiko/keymaps/rgb_matrix/glcdfont.c"
 // #define OLED_FONT_WIDTH 5
 // #define OLED_FONT_HEIGHT 7
 
-#ifdef RGBLIGHT_ENABLE
-#   define RGBLIGHT_HUE_STEP 8
-#   define RGBLIGHT_SAT_STEP 8
-#   define RGBLIGHT_VAL_STEP 8
-#   define RGBLIGHT_SLEEP       /* the RGB lighting will be switched off when the host goes to sleep */
-/*== all animations enable ==*/
-#   define RGBLIGHT_ANIMATIONS
-/*== or choose animations to save space ==*/
-// #   define RGBLIGHT_EFFECT_BREATHING
-// #   define RGBLIGHT_EFFECT_RAINBOW_MOOD
-// #   define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #   define RGBLIGHT_EFFECT_SNAKE
-// #   define RGBLIGHT_EFFECT_KNIGHT
-// #   define RGBLIGHT_EFFECT_CHRISTMAS
-// #   define RGBLIGHT_EFFECT_STATIC_GRADIENT
-// #   define RGBLIGHT_EFFECT_RGB_TEST
-// #   define RGBLIGHT_EFFECT_ALTERNATING
+
+#ifdef RGB_MATRIX_ENABLE
+
+// The pin connected to the data pin of the LEDs
+#define RGB_DI_PIN D3
+// The number of LEDs connected
+#define DRIVER_LED_TOTAL 60
+#define RGBLED_NUM 60
+#define RGB_MATRIX_SPLIT {30,30}
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
+
+
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+// #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 80 // limits maximum brightness of LEDs to 80 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+
+
+
+//  ENABLES EFFECTS. See alls the effects -> https://github.com/samhocevar-forks/qmk-firmware/blob/master/docs/feature_rgb_matrix.md#rgb-matrix-effects
+#   define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
+#   define ENABLE_RGB_MATRIX_BAND_SPLIT
+#   define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#   define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#   define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+//#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+//#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+//#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+//#   define ENABLE_RGB_MATRIX_MULTISPLASH
+//#   define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#  define ENABLE_RGB_MATRIX_SPLASH
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
