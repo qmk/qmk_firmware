d3748c0302492e42eb65a0530ab53f46010e7cce
Santiago Intxausti <intxausti@gmail.com>
2022-05-11 18:28:50 +0200

    Se crea capa qwerty2 para acceder a ella con modificadores.

--- keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
@@ -84,7 +84,8 @@ enum layer_names {
   _ONE_HAND,
   _RIGHT_NUM_PAD,
   _RIGHT_NUM_PAD2,
-  _LEFT_NUM_PAD
+  _LEFT_NUM_PAD,
+  _QWERTY2,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -145,11 +146,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
     [_LEFT_NUM_PAD] = LAYOUT_ortho_4x12(
-        KC_TAB,   				KC_BSPC, 	KC_7, 		KC_8,	KC_9, 	KC_NO,		KC_NO, 		KC_NO,	KC_NO,	KC_TRNS,	KC_NO,		TO(_QWERTY),
-        LM(_QWERTY, MOD_LCTL),	KC_BSPC, 	KC_4, 		KC_5, 	KC_6,	KC_NO,		KC_NO, 		KC_NO, 	KC_NO, 	KC_NO, 		KC_NO,		KC_NO,
-        LM(_QWERTY, MOD_LCTL), 	KC_BSPC, 	KC_1, 		KC_2, 	KC_3, 	KC_NO, 		KC_NO, 		KC_NO,	KC_NO, 	KC_NO,		KC_NO,		KC_NO,
-        LM(_QWERTY, MOD_LCTL),	KC_0, 		KC_0,  		KC_0, 	KC_0, 	A(KC_TAB), 	A(KC_TAB), 	KC_NO,	KC_NO, 	KC_NO, 		KC_TRNS,	KC_TRNS
-    )
+        LM(_QWERTY2, MOD_LALT),	KC_BSPC, 	KC_7, 		KC_8,	KC_9, 	KC_NO,		KC_NO, 		KC_NO,	KC_NO,	KC_TRNS,	KC_NO,		TO(_QWERTY),
+        LM(_QWERTY2, MOD_LALT),	KC_BSPC, 	KC_4, 		KC_5, 	KC_6,	KC_NO,		KC_NO, 		KC_NO, 	KC_NO, 	KC_NO, 		KC_NO,		KC_NO,
+        LM(_QWERTY2, MOD_LCTL), KC_BSPC, 	KC_1, 		KC_2, 	KC_3, 	KC_NO, 		KC_NO, 		KC_NO,	KC_NO, 	KC_NO,		KC_NO,		KC_NO,
+        LM(_QWERTY2, MOD_LCTL),	KC_0, 		KC_0,  		KC_0, 	KC_0, 	A(KC_TAB), 	A(KC_TAB), 	KC_NO,	KC_NO, 	KC_NO, 		KC_TRNS,	KC_TRNS
+    ),
+    
+	[_QWERTY2]=LAYOUT_ortho_4x12(
+		KC_TRNS,	KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_TRNS,
+		KC_TRNS,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_TRNS,
+		KC_TRNS,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_TRNS,
+		KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS
+	)
+
 
 };
 
