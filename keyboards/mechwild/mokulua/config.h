59fa34a6fab9a3dfa281e8595bdbd08f728dacb8
jack <0x6A73@pm.me>
2022-05-12 12:24:03 -0600

    [Keyboard] Refactor mechwild/mokulua (#17068)

--- keyboards/mechwild/mokulua/config.h
+++ keyboards/mechwild/mokulua/config.h
@@ -32,7 +32,7 @@
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-#define OLED_FONT_H "keyboards/mechwild/mokulua/lib/mokulua.c"
+#define OLED_FONT_H "keyboards/mechwild/mokulua/glcdfont.c"
 
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
@@ -78,9 +78,6 @@
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -131,5 +128,5 @@
 
 
 /* Bootmagic Lite key configuration */
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
+//#define BOOTMAGIC_LITE_ROW 0
+//#define BOOTMAGIC_LITE_COLUMN 0
