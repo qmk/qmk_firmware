72de2d64dd4b51f2e602f3d2e44e41a8e397ebfd
radlinskii <radlinskiignacy@gmail.com>
2022-05-13 00:26:47 +0200

    add debugging template

--- keyboards/keebio/iris/keymaps/radlinskii/keymap.c
+++ keyboards/keebio/iris/keymaps/radlinskii/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+// #include "print.h"
 
 #define _QWERTY 0
 #define _LOWER 1
@@ -69,3 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   // If console is enabled, it will print the matrix position and status of each key pressed
+// #ifdef CONSOLE_ENABLE
+//   uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+// #endif
+//   return true;
+// }
