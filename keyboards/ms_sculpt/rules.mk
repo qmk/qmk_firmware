14b645c33ffbac660d4f4536ef74a2892eb9420c
jn-bernard <105235786+jn-bernard@users.noreply.github.com>
2022-05-12 08:42:01 +0200

    Update rules.mk

--- keyboards/ms_sculpt/rules.mk
+++ keyboards/ms_sculpt/rules.mk
@@ -1,13 +1,3 @@
 # This file intentionally left blank
 DEBOUNCE_TYPE = sym_eager_pk
 DEBOUNCE = 3
-
-RGBLIGHT_ENABLE         = yes       # Enable global lighting effects. Do not enable with RGB Matrix
-RGBLIGHT_ANIMATIONS     = yes
-RGB_MATRIX_ENABLE       = no        # Enable per-key coordinate based RGB effects. Do not enable with RGBlight
-
-RGBLIGHT_FULL_POWER     = yes
-
-RAW_ENABLE = yes
-
-WS2812_DRIVER = pwm
