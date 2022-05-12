f507bc94c1bd933761a1c976d0b48c46b76fba10
Ramon Imbao <ramonimbao@gmail.com>
2022-05-12 08:08:04 +0800

    Update keyboards/plywrks/lune/lune.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/plywrks/lune/lune.c
+++ keyboards/plywrks/lune/lune.c
@@ -15,3 +15,24 @@
  */
 
 #include "lune.h"
+
+#ifdef OLED_ENABLE
+bool oled_task_kb(void) {
+    if (!oled_task_user()) {
+        return false;
+    }
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer :", false);
+    oled_write(get_u8_str(get_highest_layer(layer_state), ' '), false);
+    
+    oled_advance_page(true);
+
+        // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
+}
+#endif
