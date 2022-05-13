71c5bd242f69c5fa89cea15683025d2be4d50cf6
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2022-05-13 02:00:25 -0700

    Add files via upload

--- keyboards/rad/keymaps/via/keymap.c
+++ keyboards/rad/keymaps/via/keymap.c
@@ -2,24 +2,24 @@
 
 #define ____ KC_TRNS
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT(
+[0] = LAYOUT_horizontal(
 		KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC),
-		[1] = LAYOUT(
+		[1] = LAYOUT_horizontal(
 			    ____ , ____, 
                 ____ , ____, ____,
                 ____ , ____, ____,
 				____ , ____, ____
 		),
-		[2] = LAYOUT(
+		[2] = LAYOUT_horizontal(
 			   ____ , ____, 
                 ____ , ____, ____,
                 ____ , ____, ____,
 				____ , ____, ____
 		),
-		[3] = LAYOUT(
+		[3] = LAYOUT_horizontal(
 			    ____ , ____, 
                 ____ , ____, ____,
                 ____ , ____, ____,
