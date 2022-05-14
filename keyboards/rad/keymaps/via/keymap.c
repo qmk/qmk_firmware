725ec287c49feca171e6eac7a4327c79ae113cad
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2022-05-14 12:51:35 -0700

    Update keymap.c

--- keyboards/rad/keymaps/via/keymap.c
+++ keyboards/rad/keymaps/via/keymap.c
@@ -16,31 +16,30 @@
 
 #define ____ KC_TRNS
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_horizontal(
+[0] = LAYOUT(
 		KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC),
-		[1] = LAYOUT_horizontal(
+		[1] = LAYOUT(
 			    ____ , ____, 
                 ____ , ____, ____,
                 ____ , ____, ____,
 				____ , ____, ____
 		),
-		[2] = LAYOUT_horizontal(
+		[2] = LAYOUT(
 			    ____ , ____, 
                 ____ , ____, ____,
                 ____ , ____, ____,
 				____ , ____, ____
 		),
-		[3] = LAYOUT_horizontal(
+		[3] = LAYOUT(
 			    ____ , ____, 
                 ____ , ____, ____,
                 ____ , ____, ____,
 				____ , ____, ____
 		)
 }
-
 
 
         
