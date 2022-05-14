b74bba7b6e465853c6f1d2888d53732267e61998
Daniel Kao <daniel.m.kao@gmail.com>
2022-05-13 18:52:51 -0700

    Basic overlay selection for Cirque circle trackpad
    From Cirque's sample code, ADC attenuation set to:
      2X for thick plastic curved overlay
      4X for thin flat overlay
    
    Just a basic setting for usability at the moment.
    Additional features could be introduced in the future such as a Z-value
    look-up table for hover rejection.

--- drivers/sensors/cirque_pinnacle.c
+++ drivers/sensors/cirque_pinnacle.c
@@ -38,6 +38,13 @@
 #define ADC_ATTENUATE_3X     0x80
 #define ADC_ATTENUATE_4X     0xC0
 
+// Set ADC-attenuation according to overlay
+#ifdef CIRQUE_PINNACLE_CURVED_OVERLAY
+#define ADC_ATTENUATION_VALUE ADC_ATTENUATE_2X
+#else
+#define ADC_ATTENUATION_VALUE ADC_ATTENUATE_4X
+#endif
+
 // Register config values for this demo
 #define SYSCONFIG_1_VALUE    0x00
 #define FEEDCONFIG_1_VALUE   0x03  // 0x03 for absolute mode 0x01 for relative mode
@@ -214,7 +221,7 @@ void cirque_pinnacle_init(void) {
     // Host sets z-idle packet count to 5 (default is 30)
     RAP_Write(Z_IDLE_COUNT, Z_IDLE_COUNT_VALUE);
 
-    cirque_pinnacle_set_adc_attenuation(0xFF);
+    cirque_pinnacle_set_adc_attenuation(ADC_ATTENUATION_VALUE);
     cirque_pinnacle_tune_edge_sensitivity();
     cirque_pinnacle_enable_feed(true);
 }
