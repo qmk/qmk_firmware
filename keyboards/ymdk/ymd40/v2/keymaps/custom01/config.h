93356e0bf431215fa4a21deba1d4c764e86f348a
Santiago Intxausti <intxausti@gmail.com>
2022-05-14 11:20:34 +0200

    Se añade doble pulsación para abrir admiración e interrogación.

--- keyboards/ymdk/ymd40/v2/keymaps/custom01/config.h
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/config.h
@@ -2,6 +2,6 @@
 #define LEADER_TIMEOUT 1000
 #define TAPPING_TOGGLE 2
 // Tapping this number of times holds the key until tapped once again.
-#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TAP_TOGGLE 3
 // Time (in ms) before the one shot key is released
 #define ONESHOT_TIMEOUT 200
--- keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
@@ -54,12 +54,45 @@ void dance_cln_reset(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
+
+void dance_exclam_finished(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        register_code16(KC_EXLM);
+    } else {
+        SEND_STRING(SS_DOWN(X_RALT) SS_TAP(X_1));
+        clear_keyboard();
+    }
+}
+
+void dance_exclam_reset(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        unregister_code16(KC_N);
+    }
+}
+
+void dance_interrog_finished(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        register_code16(KC_EXLM);
+    } else {
+        SEND_STRING(SS_DOWN(X_RALT) SS_TAP(X_1));
+        clear_keyboard();
+    }
+}
+
+void dance_interrog_reset(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        unregister_code16(KC_N);
+    }
+}
+
 //NOMBRES TAP DANCE
 enum {
     TD_ENE,
     TD_SIFT_CAPSLOCK,
     TD_RHAND_LAYER,
-    TD_WIN_MENU
+    TD_WIN_MENU,
+    TD_EXCLAM,
+    TD_INTERROG
 
 };
 
@@ -70,6 +103,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_SIFT_CAPSLOCK]  = ACTION_TAP_DANCE_DOUBLE(KC_LEFT_SHIFT, KC_CAPS_LOCK),
     [TD_RHAND_LAYER] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ql_finished, ql_reset, 275),
     [TD_WIN_MENU]  = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_APP),
+    [TD_EXCLAM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_exclam_finished, dance_exclam_reset),
+    [TD_INTERROG] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_interrog_finished, dance_interrog_reset),
 
 };
 
@@ -90,17 +125,17 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY]=LAYOUT_ortho_4x12(
-		TD(TD_RHAND_LAYER),		KC_Q,			KC_W,			KC_E,			KC_R,					KC_T,					KC_Y,					KC_U,				KC_I,				KC_O,		KC_P,		KC_BSPC,
-		LT(_MOV,KC_ESC),		KC_A,			KC_S,			KC_D,			KC_F,					KC_G,					KC_H,					KC_J,				KC_K,				KC_L,		KC_SCLN,	KC_QUOT,
-		TD(TD_SIFT_CAPSLOCK),	KC_Z,			KC_X,			KC_C,			KC_V,					KC_B,					TD(TD_ENE),				KC_M,				KC_COMM,			KC_DOT,		KC_SLSH,	KC_SFTENT,
-		OSM(MOD_LCTL),			OSM(MOD_LALT),	OSM(MOD_RALT),	OSM(MOD_LGUI),	LT(_LOWER,KC_BSPC),		LT(_ONE_HAND,KC_SPACE),	LT(_ONE_HAND,KC_SPACE),	LT(_RAISE,KC_ENT),	TD(TD_WIN_MENU),	KC_RALT,	KC_RCTL,	KC_LEAD
+		TD(TD_RHAND_LAYER),		KC_Q,			KC_W,			KC_E,			KC_R,					KC_T,					KC_Y,					KC_U,				KC_I,				KC_O,		KC_P,				KC_BSPC,
+		LT(_MOV,KC_ESC),		KC_A,			KC_S,			KC_D,			KC_F,					KC_G,					KC_H,					KC_J,				KC_K,				KC_L,		KC_SCLN,			KC_QUOT,
+		TD(TD_SIFT_CAPSLOCK),	KC_Z,			KC_X,			KC_C,			KC_V,					KC_B,					TD(TD_ENE),				KC_M,				KC_COMM,			KC_DOT,		TD(TD_INTERROG),	KC_SFTENT,
+		OSM(MOD_LCTL),			OSM(MOD_LALT),	OSM(MOD_RALT),	OSM(MOD_LGUI),	LT(_LOWER,KC_BSPC),		LT(_ONE_HAND,KC_SPACE),	LT(_ONE_HAND,KC_SPACE),	LT(_RAISE,KC_ENT),	TD(TD_WIN_MENU),	KC_RALT,	KC_RCTL,			KC_LEAD
 	),
 
     [_RAISE] = LAYOUT_ortho_4x12(
-        KC_TILD,	KC_EXLM,	KC_AT,		KC_HASH,	KC_DLR,			KC_PERC,	KC_CIRC,	KC_AMPR,	KC_ASTR,	KC_LPRN,	KC_RPRN,	KC_BSPC,
-        KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,			KC_F6,		KC_PIPE,	KC_UNDS,	KC_PLUS,	KC_LCBR,	KC_RCBR,	KC_PIPE,
-        KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,			KC_F12,		KC_NO,		KC_MUTE,	KC_NO,		KC_LBRC,	KC_RBRC,	KC_VOLU,
-        KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	MO(_ADJUST),	KC_SPC,		KC_SPC,		KC_TRNS,	KC_MRWD,	KC_MPLY,	KC_MFFD,	KC_VOLD
+        KC_TILD,	TD(TD_EXCLAM),	KC_AT,		KC_HASH,	KC_DLR,			KC_PERC,	KC_CIRC,	KC_AMPR,	KC_ASTR,	KC_LPRN,	KC_RPRN,	KC_BSPC,
+        KC_F1,		KC_F2,			KC_F3,		KC_F4,		KC_F5,			KC_F6,		KC_PIPE,	KC_UNDS,	KC_PLUS,	KC_LCBR,	KC_RCBR,	KC_PIPE,
+        KC_F7,		KC_F8,			KC_F9,		KC_F10,		KC_F11,			KC_F12,		KC_NO,		KC_MUTE,	KC_NO,		KC_LBRC,	KC_RBRC,	KC_VOLU,
+        KC_TRNS,	KC_TRNS,		KC_TRNS,	KC_TRNS,	MO(_ADJUST),	KC_SPC,		KC_SPC,		KC_TRNS,	KC_MRWD,	KC_MPLY,	KC_MFFD,	KC_VOLD
     ),
 
     [_LOWER] = LAYOUT_ortho_4x12(
