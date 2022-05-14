b9656c57be6033f60eca65f3ed9df09e682b50de
Alexander Lee <aclee3@CGHan-Macbook-2015.local>
2022-05-14 00:32:50 -0400

    add str merro variant

---
+++ keyboards/chlx/str_merro60/rules.mk
@@ -0,0 +1,21 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
+
+Layouts = 60_ansi 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_iso_split_bs_rshift 60_tsangan_hhkb
