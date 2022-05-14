1ec3447004be13be07c4c5e87841b2397dc3443a
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2022-05-14 22:44:47 +1000

    [miryoku] Change to X macros for layer list
    - Supports adding and removing layers from custom_config.h
    - Add "U_" prefix to layer names
    - Add layer name strings
    - Populate keymaps array from X macro
    - Populate layers enum from X macro
    - Move layers enum to manna-harbour_miryoku.h
    - Rename miryoku_alternatives.h to miryoku_layer_alternatives.h
    - Rename miryoku_layer.h to miryoku_layer_selection.h
    - Remove miryoku_layer_names.h
    - Add miryoku_layer_list.h

--- users/manna-harbour_miryoku/manna-harbour_miryoku.h
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.h
@@ -1,12 +1,18 @@
-// Copyright 2019 Manna Harbour
+// Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "miryoku_babel/miryoku_layer_names.h"
-#include "miryoku_babel/miryoku_layer.h"
+#include "miryoku_babel/miryoku_layer_selection.h"
+#include "miryoku_babel/miryoku_layer_list.h"
+
+enum miryoku_layers {
+#define MIRYOKU_X(LAYER, STRING) U_##LAYER,
+MIRYOKU_LAYER_LIST
+#undef MIRYOKU_X
+};
 
 #define U_MACRO_VA_ARGS(macro, ...) macro(__VA_ARGS__)
 
