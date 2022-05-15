597941b33ad4db3b9d33abab79e245163e6e5c81
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2022-05-14 23:11:22 -0700

    Update keymap.c

--- keyboards/rad/keymaps/default/keymap.c
+++ keyboards/rad/keymaps/default/keymap.c
@@ -14,7 +14,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_horizontal(
+[0] = LAYOUT(
 		KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC, 
 		KC_SPC, KC_SPC, KC_SPC, 
