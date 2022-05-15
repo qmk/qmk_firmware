d7cf0789fd7b8ab985880c58aff0b7479a337c00
Jan Lindblom <janlindblom@fastmail.fm>
2022-05-16 00:40:08 +0300

    Update keyboards/b_sides/rev41lp/keymaps/namnlos/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/b_sides/rev41lp/keymaps/namnlos/rules.mk
+++ keyboards/b_sides/rev41lp/keymaps/namnlos/rules.mk
@@ -1,20 +1,7 @@
-
-# Build Options
-#   change yes to no to disable
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-UNICODE_ENABLE = yes        # Unicode
-TAP_DANCE_ENABLE = yes      # Let's dance
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-KEY_OVERRIDE_ENABLE = yes   # Enable key overrides
+SLEEP_LED_ENABLE = yes
+NKRO_ENABLE = yes
+UNICODE_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 DEBOUNCE_TYPE = sym_defer_pk
-
-LTO_ENABLE      = yes
-
-#Debug options
-VERBOSE = no
+LTO_ENABLE = yes
