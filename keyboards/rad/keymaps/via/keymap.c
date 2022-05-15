6978c971f51a31d0fb99d1f413fb3e5dd13a02e1
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2022-05-14 23:13:11 -0700

    Update keymap.c

--- keyboards/rad/keymaps/via/keymap.c
+++ keyboards/rad/keymaps/via/keymap.c
@@ -13,8 +13,6 @@
 	  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 	  */ 
 #include QMK_KEYBOARD_H
-
-#define ____ KC_TRNS
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
 		KC_SPC, KC_SPC, 
@@ -40,6 +38,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 				____ , ____, ____
 		)
 }
-
-
-        
