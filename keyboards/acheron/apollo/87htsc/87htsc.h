7b3f64917779cc4f566fd2d1a449853ea52f5ffd
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2022-05-15 22:00:33 -0300

    Fix layout naming

--- keyboards/acheron/apollo/87htsc/87htsc.h
+++ keyboards/acheron/apollo/87htsc/87htsc.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define ___ KC_NO
 
-// 87T for 87-key plus tsangan
-#define LAYOUT_87T( \
+// 87t for 87-key plus tsangan
+#define LAYOUT_87t( \
 	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C,      K0E, K0F, K0G, \
 	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F, K1G, \
 	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, K2G, \
