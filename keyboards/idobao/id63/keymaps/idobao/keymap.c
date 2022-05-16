0a0bc326c28f09af3fb5ce6087fcbc8bc1bee027
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-16 18:24:11 +1000

    change rgb_tog to 4-state persistent

--- keyboards/idobao/id63/keymaps/idobao/keymap.c
+++ keyboards/idobao/id63/keymaps/idobao/keymap.c
@@ -1,4 +1,5 @@
-// Copyright 2022 idobaokb (@idobaokb)
+// Copyright 2022 Vino Rodrigues (@vinorodrigues)
+// Copyright 2022 IDOBAO (@idobaokb)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 /* ------------------------------------------------------------------
@@ -7,6 +8,14 @@
 
 #include QMK_KEYBOARD_H
 #include "version.h"
+
+typedef union {
+    uint32_t raw;
+    struct {
+        bool rgb_disable_perkey:1;
+        bool rgb_disable_underglow:1;
+    };
+} user_config_t;
 
 enum {
     _BASE = 0,
@@ -18,6 +27,8 @@ enum {
 enum {
     KC_MCON = USER00,  // macOS Open Mission Control
     KC_LPAD,           // macOS Open Launchpad
+    RGB_TPK,           // Toggle Per-Key
+    RGB_TUG,           // Toggle Underglow
     KB_VRSN = USER09   // debug, type version
 };
 
@@ -38,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
      * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │
      * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
-     * │*Caps*│ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │  *Caps* => Tap = Caps Lock, Hold = Layer 1
+     * │*Caps*│ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │  *Caps* => Tap = Caps Lock, Hold = Layer 1 (Fn1)
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
      * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │Shift │Up │ / │
      * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬───┼───┼───┤
-     * │Ctrl│Win │Alt │                        │*M*│Fn1│Lf │Dn │Rt │  *M* => Tap = Menu, Hold = Layer 2
+     * │Ctrl│Win │Alt │                        │Fn1│*M*│Lf │Dn │Rt │  *M* => Tap = Menu, Hold = Layer 2 (Fn2)
      * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
      */
     [_BASE] = LAYOUT_63_ansi(
@@ -50,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         SK_LT1C, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, KC_UP,   KC_SLSH,
-        KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                    SK_LT2M, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                    FN_MO13, SK_LT2M, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /*
@@ -61,32 +72,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
      * │ Caps │lf │dn │rt │   │   │   │   │sp+│sp-│   │   │        │
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
-     * │        │Rst│   │mod│   │   │   │   │   │   │      │PUp│ / │
+     * │        │Rst│   │mod│   │   │NKR│   │   │   │      │PUp│ / │
      * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬───┼───┼───┤
      * │    │    │    │                        │   │ * │Hom│PDn│End│
      * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
      */
     [_FN1] = LAYOUT_63_ansi(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, RGB_TOG, KC_UP,   RGB_MOD, RGB_HUI,  RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,  _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          _______,
-        _______,          RESET,   _______, RGB_RMOD, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
-        _______, _______,          _______,                    _______,                   _______, _______, KC_HOME, KC_PGDN, KC_END
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,
+        _______, RGB_TOG, KC_UP,   RGB_MOD, RGB_HUI,  RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_PSCR,  KC_SLCK, KC_PAUS, KC_INS,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,  _______, _______, _______, RGB_SPI, RGB_SPD, _______,  _______,          _______,
+        _______,          RESET,   _______, RGB_RMOD, _______, _______, NK_TOGG, _______, _______, _______,  _______, KC_PGUP, _______,
+        _______, _______,          _______,                    _______,                   _______, MO(_FN3), KC_HOME, KC_PGDN, KC_END
     ),
 
     [_FN2] = LAYOUT_63_ansi(
-        KC_ESC,  KC_BRID, KC_BRIU, KC_MCON, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_POWER,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______,          _______, _______, _______, KB_VRSN, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______,          _______,                   _______,                   _______, _______, _______, _______, _______
+        KC_ESC,  KC_BRID, KC_BRIU, KC_MCON, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT,  KC_MUTE, KC_VOLD, KC_VOLU, KC_POWER,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,          _______,
+        _______,          XXXXXXX, _______, _______, KB_VRSN, _______, _______, _______, _______,  _______, _______, _______, _______,
+        _______, _______,          _______,                   _______,                   MO(_FN3), _______, _______, _______, _______
     ),
 
     [_FN3] = LAYOUT_63_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______,          _______,                   _______,                   _______, _______, _______, _______, _______
     )
 };
@@ -97,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * RGB Stuff
  */
 
-#define ID63_CAPS_LOCK_KEY_INDEX 34  // position of Caps Lock key
+user_config_t user_config;
 
-bool isRGBOff = false;
+#define ID63_CAPS_LOCK_KEY_INDEX 34  // position of Caps Lock key
 
 #define ID63_CAPS_LOCK_MAX_BRIGHTNESS 0xFF
 #ifdef RGB_MATRIX_MAXIMUM_BRIGHTNESS
@@ -113,13 +124,45 @@ bool isRGBOff = false;
     #define ID63_CAPS_LOCK_VAL_STEP RGB_MATRIX_VAL_STEP
 #endif
 
-void keyboard_post_init_user(void) {
-    isRGBOff = false;
+void id63_update_rgb_mode(void) {
+    uint8_t flags = LED_FLAG_ALL;
+
+    if (user_config.rgb_disable_perkey  &&  user_config.rgb_disable_underglow) {
+        flags = 0;  // All OFF Condition
+    } else {
+        if (user_config.rgb_disable_perkey) {
+            flags = LED_FLAG_UNDERGLOW | 0xF0;
+        }
+        if (user_config.rgb_disable_underglow) {
+            flags = LED_FLAG_MODIFIER | LED_FLAG_KEYLIGHT | LED_FLAG_INDICATOR | 0xF0;
+        }
+    }
+
+    if (flags == 0) {
+        rgb_matrix_set_flags(0);
+        rgb_matrix_set_color_all(HSV_OFF);
+    } else {
+        rgb_matrix_set_flags(flags);
+        rgb_matrix_enable_noeeprom();
+    }
+
+    eeconfig_update_kb(user_config.raw);  // write back to EEPROM
 }
 
-// void rgb_matrix_indicators_user(void) {
-//     // do nothing, override base <<weak>> function to disable it
-// }
+void id63_get_rgb_mode(void) {
+    user_config.raw = eeconfig_read_kb();  // read config from EEPROM
+    id63_update_rgb_mode();
+}
+
+void keyboard_post_init_user(void) {
+    id63_get_rgb_mode();
+}
+
+void eeconfig_init_user(void) {
+    // EEPROM is getting reset!
+    user_config.raw = 0;
+    id63_update_rgb_mode();
+}
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // Caps Lock key stuff
@@ -129,14 +172,14 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         if (v < ID63_CAPS_LOCK_VAL_STEP) {
             v = ID63_CAPS_LOCK_VAL_STEP;
         } else if (v < (ID63_CAPS_LOCK_MAX_BRIGHTNESS - ID63_CAPS_LOCK_VAL_STEP)) {
-            if (!isRGBOff) {
+            if (!user_config.rgb_disable_perkey) {
                 v += ID63_CAPS_LOCK_VAL_STEP;  // inc. by one more step than current brightness
             }  // else leave as current brightness
         } else {
             v = ID63_CAPS_LOCK_MAX_BRIGHTNESS;
         }
         rgb_matrix_set_color(ID63_CAPS_LOCK_KEY_INDEX, v, v, v);  // white, brightness adjusted
-    } else if (isRGBOff) {
+    } else if (user_config.rgb_disable_perkey) {
         rgb_matrix_set_color(ID63_CAPS_LOCK_KEY_INDEX, HSV_OFF);  // off
     }
 }
@@ -150,23 +193,55 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     switch (keycode) {
+
         // handle RGB toggle key - this ensures caps lock always works
         #ifdef RGB_MATRIX_ENABLE
+
         case RGB_TOG:
+            /* roll through the LED modes
+             * |    Level   | Per-key | Underglow |
+             * |------------|---------|-----------|
+             * | 0 (defalt) |   on    |    on     |
+             * |     1      |   OFF   |    on     |
+             * |     2      |   on    |    OFF    |
+             * |     3      |   OFF   |    OFF    |
+             */
             if (record->event.pressed) {
-                switch (rgb_matrix_get_flags()) {
-                    case LED_FLAG_ALL:
-                        isRGBOff = true;
-                        rgb_matrix_set_flags(0);
-                        rgb_matrix_set_color_all(HSV_OFF);
-                        break;
-                    default:
-                        isRGBOff = false;
-                        rgb_matrix_set_flags(LED_FLAG_ALL);
-                        rgb_matrix_enable_noeeprom();  // turn the lights back on
+                if ( (!user_config.rgb_disable_perkey) && (!user_config.rgb_disable_underglow) ) {
+                    user_config.rgb_disable_perkey = 1;
+                } else if ( user_config.rgb_disable_perkey && (!user_config.rgb_disable_underglow) ) {
+                    user_config.rgb_disable_perkey = 0;
+                    user_config.rgb_disable_underglow = 1;
+                } else if ( (!user_config.rgb_disable_perkey) && user_config.rgb_disable_underglow ) {
+                    user_config.rgb_disable_perkey = 1;
+                } else {
+                    user_config.rgb_disable_perkey = 0;
+                    user_config.rgb_disable_underglow = 0;
                 }
+                id63_update_rgb_mode();
             }
             return false;
+
+        case RGB_TPK:
+            if (record->event.pressed) {
+                user_config.rgb_disable_perkey ^= 1;
+                id63_update_rgb_mode();
+            }
+            return false;
+
+        case RGB_TUG:
+            if (record->event.pressed) {
+                user_config.rgb_disable_underglow ^= 1;
+                id63_update_rgb_mode();
+            }
+            return false;
+
+        case EE_CLR:
+            if (!record->event.pressed) {  // on release
+                id63_get_rgb_mode();
+            }
+            return true;  // let this one pass on
+
         #endif
 
         // print firmware version
