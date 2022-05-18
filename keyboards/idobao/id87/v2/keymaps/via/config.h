a040adf05b628fc8a95ed6fce605ee7f6481d8b4
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-19 00:18:34 +1000

    re-up to develop

---
+++ keyboards/idobao/id87/v2/keymaps/via/config.h
@@ -0,0 +1,8 @@
+// Copyright 2022 Vino Rodrigues (@vinorodrigues)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#ifndef VIA_QMK_RGBLIGHT_ENABLE
+    #define VIA_QMK_RGBLIGHT_ENABLE
+#endif
