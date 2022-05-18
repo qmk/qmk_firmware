4a0ad168df0626ddaa2e331c5c7f8fc3525e28c3
Jeffrey Abbott <abbottj03@gmail.com>
2022-05-18 11:21:36 -0500

    new keymap for sweep

---
+++ keyboards/crkbd/config_LOCAL_6884.h
@@ -0,0 +1,108 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x4653
+#define PRODUCT_ID      0x0001
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    foostan
+#define PRODUCT         Corne
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
+#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
+
+// wiring of each half
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
+// #define MATRIX_COL_PINS { B2, B3, B1, F7, F6, F5, F4 } //uncomment this line and comment line above if you need to reverse left-to-right key order
+
+/* define if matrix has ghost */
+//#define MATRIX_HAS_GHOST
+
+/* number of backlight levels */
+// #define BACKLIGHT_LEVELS 3
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+//#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+//#define LOCKING_RESYNC_ENABLE
+
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+// #define NO_DEBUG
+
+/* disable print */
+// #define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+#define USE_SERIAL
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+/*
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+// #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+
+ Disable the animations you don't want/need.  You will need to disable a good number of these because they take up a lot of space.  Disable until you can successfully compile your firmware. /
+// #   define DISABLE_RGB_MATRIX_ALPHAS_MODS
+// #   define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+// #   define DISABLE_RGB_MATRIX_BREATHING
+// #   define DISABLE_RGB_MATRIX_CYCLE_ALL
+// #   define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #   define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+// #   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+// #   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #   define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+// #   define DISABLE_RGB_MATRIX_DUAL_BEACON
+// #   define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+// #   define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+// #   define DISABLE_RGB_MATRIX_RAINDROPS
+// #   define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #   define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+// #   define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #   define DISABLE_RGB_MATRIX_SPLASH
+// #   define DISABLE_RGB_MATRIX_MULTISPLASH
+// #   define DISABLE_RGB_MATRIX_SOLID_SPLASH
+// #   define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif 
+*/
+
