214670352c9ec7ddedc1851329f193cd4968f4c7
mike <falter@gmail.com>
2022-05-18 09:30:20 -0500

    dygma/raise: enable audio, system, and debug keys

--- keyboards/handwired/dygma/raise/rules.mk
+++ keyboards/handwired/dygma/raise/rules.mk
@@ -9,9 +9,9 @@ BOOTLOADER = stm32-dfu
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
