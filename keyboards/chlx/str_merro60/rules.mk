6954eb35e81f4fa3ee78f33d8b196e4893269a22
chalex <68408520+gaclee3b@users.noreply.github.com>
2022-05-19 02:00:18 -0400

    Update keyboards/chlx/str_merro60/rules.mk
    remove bluetooth_enable, change nkro_enable comment per drashna suggestion
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/chlx/str_merro60/rules.mk
+++ keyboards/chlx/str_merro60/rules.mk
@@ -12,10 +12,9 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # USB Nkey Rollover
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 
 Layouts = 60_ansi 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_tsangan_hhkb
