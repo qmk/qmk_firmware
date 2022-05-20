e57bcd8716e7f6a448ce2a88aee3ee5a2dc7eb89
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-05-20 22:35:24 +0700

    delete files

--- keyboards/mechlovin/adelais/rgb_led/rev3/boards/adelais_f103/board.mk
+++
@@ -1,9 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/boards/adelais_f103/board.c
-
-# Required include directories
-BOARDINC = $(BOARD_PATH)/boards/adelais_f103
-
-# Shared variables
-ALLCSRC += $(BOARDSRC)
-ALLINC  += $(BOARDINC)
