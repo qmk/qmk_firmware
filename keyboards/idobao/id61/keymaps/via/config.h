e0d2006683972e7513936e5a1767b5295c0f2eea
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-20 15:34:58 +1000

    Delete config.h

--- keyboards/idobao/id61/keymaps/via/config.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 Vino Rodrigues (@vinorodrigues)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-// #define DYNAMIC_KEYMAP_LAYER_COUNT 4
-
-#if defined(RGB_MATRIX_ENABLE) && !defined(VIA_QMK_RGBLIGHT_ENABLE)
-    #define VIA_QMK_RGBLIGHT_ENABLE
-#endif
