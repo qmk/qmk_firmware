2cc7e980c06e87cd543f19e2f5a44021aa64f04f
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-20 15:19:47 +1000

    adjust keymap

--- keyboards/idobao/id61/keymaps/via/config.h
+++ keyboards/idobao/id61/keymaps/via/config.h
@@ -1,7 +1,7 @@
 // Copyright 2022 Vino Rodrigues (@vinorodrigues)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
 #if defined(RGB_MATRIX_ENABLE) && !defined(VIA_QMK_RGBLIGHT_ENABLE)
     #define VIA_QMK_RGBLIGHT_ENABLE
