9210a0886e47615f52aecd03a1382c3cc42154b7
Santiago Intxausti <intxausti@gmail.com>
2022-05-20 10:21:47 +0200

    Eliminating double tappering on slash key

--- keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/keymap.c
@@ -115,6 +115,7 @@ void dance_exclam_reset(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
+/*
 void dance_interrog_finished(qk_tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {
         register_code16(KC_SLSH);
@@ -129,7 +130,7 @@ void dance_interrog_reset(qk_tap_dance_state_t *state, void *user_data) {
         unregister_code16(KC_SLSH);
     }
 }
-
+*/
 
 //NOMBRES TAP DANCE
 enum {
@@ -137,8 +138,7 @@ enum {
     TD_SIFT_CAPSLOCK,
     TD_RHAND_LAYER,
     TD_WIN_MENU,
-    TD_EXCLAM,
-    TD_INTERROG
+    TD_EXCLAM
 
 };
 
@@ -150,7 +150,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_RHAND_LAYER] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, ql_finished, ql_reset, 275),
     [TD_WIN_MENU]  = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_APP),
     [TD_EXCLAM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_exclam_finished, dance_exclam_reset),
-    [TD_INTERROG] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_interrog_finished, dance_interrog_reset),
 
 };
 
@@ -174,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY]=LAYOUT_ortho_4x12(
 		TD(TD_RHAND_LAYER),		KC_Q,			KC_W,			KC_E,			KC_R,					KC_T,					KC_Y,					KC_U,				KC_I,				KC_O,		KC_P,				KC_BSPC,
 		LT(_MOV,KC_ESC),		KC_A,			KC_S,			KC_D,			KC_F,					KC_G,					KC_H,					KC_J,				KC_K,				KC_L,		KC_SCLN,			KC_QUOT,
-		TD(TD_SIFT_CAPSLOCK),	KC_Z,			KC_X,			KC_C,			KC_V,					KC_B,					KC_N,				KC_M,				KC_COMM,			KC_DOT,		TD(TD_INTERROG),	KC_SFTENT,
+		TD(TD_SIFT_CAPSLOCK),	KC_Z,			KC_X,			KC_C,			KC_V,					KC_B,					KC_N,				KC_M,				KC_COMM,			KC_DOT,		KC_SLSH,	KC_SFTENT,
 		OSM(MOD_LCTL),			OSM(MOD_LALT),	OSM(MOD_RALT),	OSM(MOD_LGUI),	LT(_LOWER,KC_BSPC),		LT(_ONE_HAND,KC_SPACE),	LT(_ONE_HAND,KC_SPACE),	LT(_RAISE,KC_ENT),	TD(TD_WIN_MENU),	KC_RALT,	KC_RCTL,			KC_LEAD
 	),
 
