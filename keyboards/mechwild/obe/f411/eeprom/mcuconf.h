c60c19e8d5ebb1066193e8cb73b0c9f235bc5b36
Kyle McCreery <mccreery.kyle@gmail.com>
2022-05-20 12:10:05 -0400

    [Keyboard] Add eeprom IC-less rev for MechWild OBE and Waka60 (#17126)

--- keyboards/mechwild/obe/f411/eeprom/mcuconf.h
+++ keyboards/mechwild/obe/f411/eeprom/mcuconf.h
@@ -20,8 +20,3 @@
 
 #undef STM32_I2C_USE_I2C1
 #define STM32_I2C_USE_I2C1 TRUE
-
-#undef STM32_PWM_USE_TIM1
-#define STM32_PWM_USE_TIM1                  TRUE
-#undef STM32_GPT_USE_TIM4
-#define STM32_GPT_USE_TIM4                  TRUE
