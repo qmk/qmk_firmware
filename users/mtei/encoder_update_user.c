b935e2d19b5b4ec8ad68b7d19b879b7c18b68759
mtei <2170248+mtei@users.noreply.github.com>
2022-05-21 15:03:26 +0900

    Fix users/mtei/encoder_update_user.c
    It seems that calling tap_code() on the slave side should not be done because the keycode cannot be sent to the host.

--- users/mtei/encoder_update_user.c
+++ users/mtei/encoder_update_user.c
@@ -3,6 +3,8 @@
 
 #include QMK_KEYBOARD_H
 
+#define TAP_CODE(x) if (is_keyboard_master()) { tap_code(x); }
+
 /* weak reference */ __attribute__((weak))
 int get_encoder_over_count(void);
 
@@ -10,23 +12,27 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 #ifndef ENCODER_DETECT_OVER_SPEED
     switch (index) {
     /* Left side encoder */
-    case 0: tap_code(clockwise ? KC_LEFT : KC_RGHT); break;
+    case 0: TAP_CODE(clockwise ? KC_LEFT : KC_RGHT); break;
     /* Right side encoder */
-    case 1: tap_code(clockwise ? KC_DOWN : KC_UP); break;
+    case 1: TAP_CODE(clockwise ? KC_DOWN : KC_UP); break;
     }
 #else
     // Is there a get_encoder_over_count() in quantum/encoder.c
     if (get_encoder_over_count != NULL) {
         int enc_over = get_encoder_over_count();
+#if 0
         for (; enc_over > 0; enc_over--) {
-            tap_code(KC_MINUS);
+            TAP_CODE(KC_MINUS);
         }
+#elsd
+        // other debug dump [WIP]
+#endif
     }
     switch (index) {
-    case 0: tap_code(clockwise ? KC_A : KC_B); break;
-    case 1: tap_code(clockwise ? KC_C : KC_D); break;
-    case 2: tap_code(clockwise ? KC_E : KC_F); break;
-    case 3: tap_code(clockwise ? KC_G : KC_H); break;
+    case 0: TAP_CODE(clockwise ? KC_A : KC_B); break;
+    case 1: TAP_CODE(clockwise ? KC_C : KC_D); break;
+    case 2: TAP_CODE(clockwise ? KC_E : KC_F); break;
+    case 3: TAP_CODE(clockwise ? KC_G : KC_H); break;
     }
 #endif
     return true;
