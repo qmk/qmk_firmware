e80157f7b3c6bb52dc615c05f6afcefe8f1194b9
Derek <dschmell91@gmail.com>
2022-05-22 17:27:00 -0400

    Update keyboards/nacly/sodium62/sodium62.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/nacly/sodium62/sodium62.c
+++ keyboards/nacly/sodium62/sodium62.c
@@ -14,3 +14,39 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "sodium62.h"
+
+#ifdef OLED_ENABLE
+bool oled_task_kb(void) {
+    if (!oled_task_user()) {
+        return false;
+    }
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("Base\n"), false);
+            break;
+        case _L1:
+            oled_write_P(PSTR("ONE\n"), false);
+            break;
+        case _L2:
+            oled_write_P(PSTR("TWO\n"), false);
+            break;
+        case _L3:
+            oled_write_P(PSTR("Three\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return true;
+}
+#endif
