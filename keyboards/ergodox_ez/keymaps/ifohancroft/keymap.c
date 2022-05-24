d8daccb6383287d39cafd13c36e3d1e9e1eeb552
IFo Hancroft <contact@ifohancroft.com>
2022-05-25 00:58:04 +0300

    Fixed a thing or two

--- keyboards/ergodox_ez/keymaps/ifohancroft/keymap.c
+++ keyboards/ergodox_ez/keymaps/ifohancroft/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |---------+---------+---------+---------+---------+---------+---------*                             *---------+---------+---------+---------+---------+---------+---------|
     * | Shift   | Z       | X       | C       | V       | B       |                                                 | N       | M       | ,       | .       | /       | Shift   |
     * |---------+---------+---------+---------+---------+---------*                                                 *---------+---------+---------+---------+---------+---------|
-    * | Ctrl    | Super   | Alt     |         | OSL(1)  |                                                                     | MO(1)   | TT(1)   | Alt     | Super   | Ctrl    |
+    * | Ctrl    | Super   | Alt     |         | OSL(1)  |                                                                     | TT(1)   |         | Alt     | Super   | Ctrl    |
     * *-------------------------------------------------*                                                                     *-------------------------------------------------*
     *
     *                                                             *-------------------*         *-------------------*
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,      KC_T,     KC_LBRC,                                KC_RBRC,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
         KC_ESC,   KC_A,     KC_S,     KC_D,    KC_F,      KC_G,                                                       KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,      KC_B,     KC_NO,                                  KC_NO,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-        KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,   OSL(1),                                                                          MO(1),    TT(1),    KC_RALT,  KC_RGUI,  KC_RCTL,
+        KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,   OSL(1),                                                                          TT(1),    KC_NO,    KC_RALT,  KC_RGUI,  KC_RCTL,
 
                                                                     KC_NO,    KC_NO,              KC_NO,    KC_NO,
                                                                               KC_NO,              KC_NO,
@@ -155,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-    /* Left Hand Layer
+    /* Swap Hands (Left becomes Right, Right stays the same)
     * *---------------------------------------------------------------------*                             *---------------------------------------------------------------------*
     * | Del     | 6       | 7       | 8       | 9       | 0       | =       |                             |         |         |         |         |         |         |         |
     * |---------+---------+---------+---------+---------+---------+---------|                             |---------+---------+---------+---------+---------+---------+---------|
