7adde1c33c33e09ead7a4b6d3883fea42dca7d95
hafnix <hafnix@mailbox.org>
2022-05-26 20:41:10 +0200

    Improved default RGB

--- keyboards/durgod/dgk6x/venus/keymaps/hafnix/config.h
+++ keyboards/durgod/dgk6x/venus/keymaps/hafnix/config.h
@@ -3,5 +3,7 @@
 
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
 #define RGB_MATRIX_STARTUP_HUE 162 /* blue (170) minus 1 step */
+#define RGB_MATRIX_STARTUP_VAL 191 /* max bri minus 4 steps */
+#define RGB_MATRIX_STARTUP_SPD 111 /* default 127 minus 1 step */
 
 #define TAPPING_TERM 200
