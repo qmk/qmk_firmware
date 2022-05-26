ff2a0e45fcd36ea5e804530a6eae5f678ec6fdf5
rwijtvliet <rwijtvliet@gmail.com>
2022-05-26 14:39:20 +0200

    Two nav layers, some combos, not all modifiers working yet

---
+++ keyboards/keyboardio/atreus/keymaps/rwijtvliet/config.h
@@ -0,0 +1,15 @@
+
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+
+#define COMBO_TERM 30
+#define COMBO_ONLY_FROM_LAYER 1
+#define COMBO_VARIABLE_LEN
---
+++ keyboards/keyboardio/atreus/keymaps/rwijtvliet/rules.mk
@@ -0,0 +1,2 @@
+COMBO_ENABLE = yes
+VPATH += keyboards/gboards
