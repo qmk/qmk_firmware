bffb5eb976f577ca4afaf85c5053fc6c95b77e9d
Aidan Gauland <aidalgol@fastmail.net>
2022-05-26 12:48:12 +1200

    Fix maddie layout for ploopy/trackball_nano
    This keymap was using a user function that had been removed from QMK, so it was
    never being called.  The function that replaces it provides a slightly different
    interface, so the implementation in this keymap had to be updated as well as the
    function signature.

--- keyboards/ploopyco/trackball_nano/keymaps/maddie/keymap.c
+++ keyboards/ploopyco/trackball_nano/keymaps/maddie/keymap.c
@@ -19,38 +19,20 @@
 #include QMK_KEYBOARD_H
 
 // safe range starts at `PLOOPY_SAFE_RANGE` instead.
-uint8_t scroll_enabled = 0;
-uint8_t lock_state = 0;
-int8_t  delta_x        = 0;
-int8_t  delta_y        = 0;
+bool scroll_enabled = 0;
+bool lock_state     = 0;
 
 // Dummy
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {{{ KC_NO }}};
 
-void process_mouse_user(report_mouse_t *mouse_report, int8_t x, int8_t y) {
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     if (scroll_enabled) {
-        delta_x += x;
-		delta_y += y;
-
-		if (delta_x > 60) {
-			mouse_report->h = 1;
-			delta_x = 0;
-		} else if (delta_x < -60) {
-			mouse_report->h = -1;
-			delta_x = 0;
-		}
-
-		if (delta_y > 15) {
-			mouse_report->v = -1;
-			delta_y = 0;
-		} else if (delta_y < -15) {
-			mouse_report->v = 1;
-			delta_y = 0;
-		}
-    } else {
-        mouse_report->x = x;
-		mouse_report->y = y;
+        mouse_report.h = mouse_report.x;
+        mouse_report.v = mouse_report.y;
+        mouse_report.x = 0;
+        mouse_report.y = 0;
     }
+    return mouse_report;
 }
 
 void keyboard_post_init_user(void) {
@@ -72,8 +54,6 @@ bool led_update_user(led_t led_state) {
 		if (lock_count == 2) {
 			scroll_enabled = !scroll_enabled;
 			lock_count = 0;
-			delta_x = 0;
-			delta_y = 0;
 		}
 	}
 
