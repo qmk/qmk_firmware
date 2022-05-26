6410349f347534132f4151c5da36cdbf34498471
Andrew Garver <andrewsgarver@gmail.com>
2022-05-25 21:30:54 -0600

    updated lighting toggle keys

--- keyboards/keebmaker/perigee/keymaps/default/keymap.c
+++ keyboards/keebmaker/perigee/keymaps/default/keymap.c
@@ -16,18 +16,6 @@
 
 #include QMK_KEYBOARD_H
 
-enum perigee_layers {
-  _QWERTY,
-  _LOWER,
-  _RAISE,
-};
-
-enum perigee_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -43,12 +31,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl | Ctrl | Alt  | GUI  |Lower | Spc  |                      | Spc  |Raise | Left | Down |  Up  |Right |
  * `------------------------------------------                      ------------------------------------------'
  */
-[_QWERTY] = LAYOUT_ortho_5x12(
+[0] = LAYOUT_ortho_5x12(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,            KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,            KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,      KC_G,            KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,            KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(LOWER), KC_SPC,          KC_SPC,  MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(1),     KC_SPC,          KC_SPC,  MO(2),     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -64,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |                      |      |      | Next | Vol- | Vol+ | Play |
  * `------------------------------------------                      ------------------------------------------'
  */
-[_LOWER] = LAYOUT_ortho_5x12(
+[1] = LAYOUT_ortho_5x12(
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
@@ -85,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |                      |      |      | Next | Vol- | Vol+ | Play |
  * `------------------------------------------                      ------------------------------------------'
  */
-[_RAISE] = LAYOUT_ortho_5x12(
+[2] = LAYOUT_ortho_5x12(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,               KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
--- keyboards/keebmaker/perigee/keymaps/default/rules.mk
+++ keyboards/keebmaker/perigee/keymaps/default/rules.mk
@@ -1,1 +1,2 @@
-
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = no
