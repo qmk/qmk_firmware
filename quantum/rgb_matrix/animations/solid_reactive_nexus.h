d4831dd8408887c073f5fb865fc7f2002492a662
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-05-27 15:49:21 +0800

    Add solid reactive gradient mode

--- quantum/rgb_matrix/animations/solid_reactive_nexus.h
+++ quantum/rgb_matrix/animations/solid_reactive_nexus.h
@@ -16,6 +16,9 @@ static HSV SOLID_REACTIVE_NEXUS_math(HSV hsv, int16_t dx, int16_t dy, uint8_t di
     if (effect > 255) effect = 255;
     if (dist > 72) effect = 255;
     if ((dx > 8 || dx < -8) && (dy > 8 || dy < -8)) effect = 255;
+#            ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
+    hsv.h = scale16by8(g_rgb_timer, add8(rgb_matrix_config.speed, 1) >> 6);
+#            endif
     hsv.v = qadd8(hsv.v, 255 - effect);
     hsv.h = rgb_matrix_config.hsv.h + dy / 4;
     return hsv;
