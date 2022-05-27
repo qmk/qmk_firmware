6eea3eb1515ed21c613aeb72a227f844c404fdbd
Minke Zhang <minke.zhang@gmail.com>
2022-05-27 16:33:39 -0700

    Update keyboards/ymdk/sp64/keymaps/minkezhang/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ymdk/sp64/keymaps/minkezhang/keymap.c
+++ keyboards/ymdk/sp64/keymaps/minkezhang/keymap.c
@@ -134,7 +134,7 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
 
 /* Add some indication for current function layer.
  *
