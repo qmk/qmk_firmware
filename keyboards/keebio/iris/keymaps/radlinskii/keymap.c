d4dc440d15b9a512c501aa8d0a14349eeda8af51
radlinskii <radlinskiignacy@gmail.com>
2022-05-27 15:39:13 +0200

    create separate layer for nav and separate for symbols and numbers

--- keyboards/keebio/iris/keymaps/radlinskii/keymap.c
+++ keyboards/keebio/iris/keymaps/radlinskii/keymap.c
@@ -1,9 +1,9 @@
 #include QMK_KEYBOARD_H
 
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _MOUSE 3
+#define _NUM_SYM 1
+#define _NAV 2
+#define _MISC 3
 #define _ADJUST 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -14,53 +14,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,            KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), LALT_T(KC_SCLN), KC_QUOT,
+     KC_LCTL, LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,            KC_H,RSFT_T(KC_J),RGUI_T(KC_K),RCTL_T(KC_L),RALT_T(KC_SCLN),KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,          KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, TT(_LOWER), KC_ENT,               KC_SPC, TO(_RAISE),KC_RALT
+                                    KC_LGUI, TT(_NUM_SYM), KC_ENT,               KC_SPC, TT(_NAV),KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_LOWER] = LAYOUT(
+    [_NUM_SYM] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, TO(_ADJUST),
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______,  KC_GRV, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_PLUS, KC_EQL,
+     _______,LALT_T(KC_1),LCTL_T(KC_2),LGUI_T(KC_3),LSFT_T(KC_4),KC_5,               KC_6,RSFT_T(KC_7),RGUI_T(KC_8),RCTL_T(KC_9),RALT_T(KC_0),KC_PIPE,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, KC_UNDS, KC_BSLS, KC_LCBR, KC_RCBR, _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_MINS, _______,
+     _______, _______, KC_LPRN, KC_LPRN, KC_LCBR, KC_RCBR, _______,          _______, KC_LBRC, KC_RBRC, KC_GRV,  KC_EQL, KC_MINS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, TO(_QWERTY), _______,               _______,  TO(_RAISE), _______
+                                   _______,TO(_QWERTY),_______,               _______, TO(_NAV), _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_RAISE] = LAYOUT(
+    [_NAV] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, KC_WBAK, KC_WFWD, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,          KC_BTN2, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, TO(_QWERTY), _______,              KC_BTN1, TO(_MISC), _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_MISC] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, RGB_MOD, BL_STEP, _______,
+     _______, _______, _______, _______, _______, _______,                           _______, _______, _______, RGB_MOD, BL_STEP, TO(_ADJUST),
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_MUTE, KC_MPRV, KC_MNXT, KC_VOLU, KC_BRIU, _______,                            _______,  _______, RGB_HUI, RGB_SAI, RGB_VAI, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_CAPS, KC_MSTP, KC_MPLY, KC_VOLD,  KC_BRID, _______, _______,          _______, _______, _______,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, TO(_QWERTY), _______,               _______, TO(_MOUSE), _______
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-  ),
-
-  [_MOUSE] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL0, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL1, XXXXXXX, XXXXXXX,                            KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, XXXXXXX, XXXXXXX, KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    XXXXXXX, TO(_QWERTY), KC_BTN2,              KC_BTN1, TO(_LOWER), XXXXXXX
+                                    _______, TO(_QWERTY), _______,               _______, TO(_NUM_SYM), _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -94,7 +94,7 @@ const rgblight_segment_t PROGMEM QWERTY_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {58, 4, 85, 255, 255}
 );
 
-const rgblight_segment_t PROGMEM LOWER_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM NUM_SYM_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 6, 201, 255, 255},
     {6, 6, 191, 255, 255},
     {12, 6, 181, 255, 255},
@@ -108,7 +108,7 @@ const rgblight_segment_t PROGMEM LOWER_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {58, 4, 169, 255, 255}
 );
 
-const rgblight_segment_t PROGMEM RAISE_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM NAV_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 6, 248, 255, 255},
     {6, 6, 240, 255, 255},
     {12, 6, 225, 255, 255},
@@ -122,7 +122,7 @@ const rgblight_segment_t PROGMEM RAISE_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {58, 4, 201, 255, 255}
 );
 
-const rgblight_segment_t PROGMEM MOUSE_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM MISC_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 6, 11, 255, 255},
     {6, 6, 19, 255, 255},
     {12, 6, 27, 255, 255},
@@ -143,9 +143,9 @@ const rgblight_segment_t PROGMEM ADJUST_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     QWERTY_Layer,
-    LOWER_Layer,
-    RAISE_Layer,
-    MOUSE_Layer,
+    NUM_SYM_Layer,
+    NAV_Layer,
+    MISC_Layer,
     ADJUST_Layer
 );
 
@@ -163,9 +163,9 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
-    rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _MOUSE));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _NUM_SYM));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _NAV));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _MISC));
     rgblight_set_layer_state(4, layer_state_cmp(state, _ADJUST));
 
     return state;
