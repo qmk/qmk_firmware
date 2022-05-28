f5d271caa28dfd66a36bf15659ede8c2ff6c3211
hafnix <hafnix@mailbox.org>
2022-05-28 08:12:05 +0200

    some changes...

--- keyboards/durgod/dgk6x/venus/keymaps/hafnix/config.h
+++ keyboards/durgod/dgk6x/venus/keymaps/hafnix/config.h
@@ -6,4 +6,7 @@
 #define RGB_MATRIX_STARTUP_VAL 191 /* max bri minus 4 steps */
 #define RGB_MATRIX_STARTUP_SPD 111 /* default 127 minus 1 step */
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
+
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
