0e65d83db0fb10d6bf442c0d02954257000ced84
David Kristoffersen <david.alta2010@gmail.com>
2022-05-28 06:00:39 +0200

    Add size optimization to userspace

--- keyboards/planck/keymaps/prog_qgmlwb/rules.mk
+++ keyboards/planck/keymaps/prog_qgmlwb/rules.mk
@@ -3,7 +3,4 @@ SRC += src/layouts.c
 
 USER_NAME := davidkristoffersen
 
-# Firmware size optimizations
-CONSOLE_ENABLE	= no	# 1646: Console for debug
-AUDIO_ENABLE	= no	# 8682: Audio output on port C6
-# LTO_ENABLE	= yes	# 2772: Link time optimization, breaks `action_get_macro` and `fn_action` if enabled
+SIZE_OPTIMIZE = yes
