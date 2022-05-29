f960a2c0cb4f84c4fdbba7bd0b49f2b78d56325f
radlinskii <radlinskiignacy@gmail.com>
2022-05-29 22:05:27 +0200

    add changing underglow lights on layers change

--- keyboards/keebio/iris/keymaps/radlinskii/keymap.c
+++ keyboards/keebio/iris/keymaps/radlinskii/keymap.c
@@ -86,6 +86,8 @@ const rgblight_segment_t PROGMEM QWERTY_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {24, 1, 85, 255, 255},
     {25, 1, 169, 255, 255}, // TT(_NUM_SYM)
     {26, 2, 85, 255, 255},
+    {28, 3, 85, 255, 255}, // underglow
+    {31, 3, 117, 255, 255}, // underglow
 
     {34, 6, 117, 255, 255},
     {40, 6, 107, 255, 255},
@@ -93,7 +95,9 @@ const rgblight_segment_t PROGMEM QWERTY_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {52, 6, 90, 255, 255},
     {58, 1, 85, 255, 255},
     {59, 1, 201, 255, 255}, // TT(_NAV)
-    {60, 2, 85, 255, 255}
+    {60, 2, 85, 255, 255},
+    {62, 3, 85, 255, 255}, // underglow
+    {65, 3, 117, 255, 255} // underglow
 );
 
 const rgblight_segment_t PROGMEM NUM_SYM_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -104,6 +108,8 @@ const rgblight_segment_t PROGMEM NUM_SYM_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {24, 1, 169, 255, 255},
     {25, 1, 85, 255, 255}, // TO(_QWERTY)
     {26, 2, 169, 255, 255},
+    {28, 3, 169, 255, 255},
+    {31, 3, 201, 255, 255},
 
     {34, 6, 201, 255, 255},
     {40, 6, 191, 255, 255},
@@ -111,7 +117,9 @@ const rgblight_segment_t PROGMEM NUM_SYM_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {52, 6, 171, 255, 255},
     {58, 1, 169, 255, 255},
     {59, 1, 201, 255, 255}, // TO(_NAV)
-    {60, 2, 169, 255, 255}
+    {60, 2, 169, 255, 255},
+    {62, 3, 169, 255, 255},
+    {65, 3, 201, 255, 255}
 );
 
 const rgblight_segment_t PROGMEM NAV_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -122,6 +130,8 @@ const rgblight_segment_t PROGMEM NAV_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {24, 1, 201, 255, 255},
     {25, 1, 85, 255, 255}, // TO(_QWERTY)
     {26, 2, 201, 255, 255},
+    {28, 3, 201, 255, 255},
+    {31, 3, 248, 255, 255},
 
     {34, 6, 248, 255, 255},
     {40, 6, 240, 255, 255},
@@ -129,7 +139,9 @@ const rgblight_segment_t PROGMEM NAV_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {52, 6, 210, 255, 255},
     {58, 1, 201, 255, 255},
     {59, 1, 43, 255, 255}, // TO(_MISC)
-    {60, 2, 201, 255, 255}
+    {60, 2, 201, 255, 255},
+    {62, 3, 201, 255, 255},
+    {65, 3, 248, 255, 255}
 );
 
 const rgblight_segment_t PROGMEM MISC_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -140,6 +152,8 @@ const rgblight_segment_t PROGMEM MISC_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {24, 1, 43, 255, 255},
     {25, 1, 85, 255, 255}, // TO(_QWERTY)
     {26, 2, 43, 255, 255},
+    {28, 3, 43, 255, 255},
+    {31, 3, 11, 255, 255},
 
     {34, 6, 11, 255, 255},
     {40, 5, 19, 255, 255},
@@ -148,18 +162,18 @@ const rgblight_segment_t PROGMEM MISC_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {52, 6, 35, 255, 255},
     {58, 1, 43, 255, 255},
     {59, 1, 169, 255, 255}, // TO(_NUM_SYM)
-    {60, 2, 43, 255, 255}
+    {60, 2, 43, 255, 255},
+    {62, 3, 43, 255, 255},
+    {65, 3, 11, 255, 255}
 );
 
 const rgblight_segment_t PROGMEM DANGER_Layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 1, 0, 0, 255},  // RESET
     {1, 24, 0, 255, 255},
     {25, 1, 85, 255, 255}, // TO(_QWERTY)
-    {26, 2, 0, 255, 255},
-
-    {34, 25, 0, 255, 255},
+    {26, 33, 0, 255, 255},
     {59, 1, 85, 255, 255}, // TO(_QWERTY)
-    {60, 2, 0, 255, 255}
+    {60, 8, 0, 255, 255}
 );
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
