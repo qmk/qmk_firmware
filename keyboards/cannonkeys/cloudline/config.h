e8e4f29608f73f103803d5e24ab2f6da1e769e7d
Andrew Kannan <andrew.kannan@gmail.com>
2022-05-30 12:39:52 -0400

    Change to 7U, add bootloader info and copyrights

--- keyboards/cannonkeys/cloudline/config.h
+++ keyboards/cannonkeys/cloudline/config.h
@@ -1,19 +1,5 @@
-/*
-Copyright 2015 Jun Wako <wakojun@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2022 Andrew Kannan (@awkannan)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
@@ -30,7 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { B1, B2, B10, B11, B12, B14, A8, A9, A10, A3, B0, A2, A1, A7, A0, B4, B6, B7 }
 #define MATRIX_ROW_PINS { A15, B3, B5, A4, A5, F1 }
 #define DIODE_DIRECTION COL2ROW
-
 
 #define BACKLIGHT_PIN           A6
 #define BACKLIGHT_PWM_DRIVER    PWMD3
@@ -55,13 +40,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-#define RGBLIGHT_ANIMATIONS
 #define RGB_DI_PIN B15
 #define RGBLED_NUM 20
 #define WS2812_SPI SPID2
 #define WS2812_SPI_MOSI_PAL_MODE 0
 #define WS2812_SPI_SCK_PAL_MODE 0
 #define WS2812_SPI_SCK_PIN B13
+
+
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
+
 
 /*
  * Feature disable options
--- keyboards/cannonkeys/cloudline/halconf.h
+++ keyboards/cannonkeys/cloudline/halconf.h
@@ -21,8 +21,6 @@
 
 #pragma once
 
-#define HAL_USE_I2C TRUE
-
 #define HAL_USE_PWM TRUE
 
 #define HAL_USE_SPI TRUE
