63d1cfca7bdd09609cd8d62ca3d77bc17814d0ef
Nick Brassel <nick@tzarc.org>
2022-05-31 09:30:53 +1000

    Remove TERM_ON and TERM_OFF from default-ish keymaps. Add error if anyone tries to use them.

--- keyboards/handwired/riblee_f401/keymaps/default/keymap.c
+++ keyboards/handwired/riblee_f401/keymaps/default/keymap.c
@@ -158,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_ortho_5x12(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
-    _______, QK_BOOT,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______,  _______, KC_DEL,
+    _______, QK_BOOT,   DEBUG,   _______, _______, _______, _______, _______, _______,_______,  _______, KC_DEL,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,   _______, _______,
     _______, _______, _______, _______, _______, _______, NK_TOGG, LCG_SWP, LCG_NRM, _______,  _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______
