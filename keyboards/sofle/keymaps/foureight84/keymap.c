ead17e1e7f2d559e4c09a6d44679005e004c74e5
Khoa Truong <foureight84@gmail.com>
2022-05-29 17:45:04 -0700

    fixed issue with right side not working with via enabled per ticket https://github.com/qmk/qmk_firmware/issues/17206

--- keyboards/sofle/keymaps/foureight84/keymap.c
+++ keyboards/sofle/keymaps/foureight84/keymap.c
@@ -599,7 +599,7 @@ report_mouse_t smooth_mouse_movement(report_mouse_t mouse_report) {
 }
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (has_mouse_report_changed(last_mouse_report, mouse_report)) {
+    if (has_mouse_report_changed(&last_mouse_report, &mouse_report)) {
         last_mouse_activity = timer_read32();
         memcpy(&last_mouse_report, &mouse_report, sizeof(mouse_report));
     }
