854547330704fb1b1f07d547d49728da8b92b2a3
Nick Brassel <nick@tzarc.org>
2022-05-31 15:38:08 +1000

    Removes terminal from QMK. (#17258)

--- keyboards/ckeys/handwire_101/rules.mk
+++ keyboards/ckeys/handwire_101/rules.mk
@@ -12,7 +12,6 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
-TERMINAL_ENABLE = yes
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
