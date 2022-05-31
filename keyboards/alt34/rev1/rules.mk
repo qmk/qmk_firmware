7c718ac4038170a2d4ced715bfd0a2fa73d07809
Tommy Alatalo <tommy.alatalo@gmail.com>
2022-05-31 17:30:21 +0200

    Move rules.mk into rev1 folder entirely

--- keyboards/alt34/rev1/rules.mk
+++ keyboards/alt34/rev1/rules.mk
@@ -1,2 +1,20 @@
-# Disable unsupported hardware
-AUDIO_SUPPORTED = no
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+#   change to "no" to disable the options
+
+BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no           # Enable N-Key Rollover
+UNICODE_ENABLE = no        # Unicode
+
+SPLIT_KEYBOARD = yes
+LAYOUTS = split_3x5_2
+DEFAULT_FOLDER = alt34/rev1
