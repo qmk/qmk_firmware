23639b2a5c96e6b62963f6d886d59a196c4fe56c
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-06-01 10:27:33 +0200

    Update keytabs.h
    added header

--- keyboards/crkbd/keymaps/JulianTurner/keytabs.h
+++ keyboards/crkbd/keymaps/JulianTurner/keytabs.h
@@ -1,3 +1,20 @@
+ /* Copyright 2022 Julian Turner
+	  * 
+	  * This program is free software: you can redistribute it and/or modify 
+	  * it under the terms of the GNU General Public License as published by 
+	  * the Free Software Foundation, either version 2 of the License, or 
+	  * (at your option) any later version. 
+	  * 
+	  * This program is distributed in the hope that it will be useful, 
+	  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+	  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+	  * GNU General Public License for more details. 
+	  * 
+	  * You should have received a copy of the GNU General Public License 
+	  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+
 // Left-hand home row mods
 #define HOME_A LGUI_T(DE_A)
 #define HOME_S LALT_T(DE_S)
