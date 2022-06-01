c83fa34213e44e0100f9a0ded07182821341c02c
Andrew Garver <andrewsgarver@gmail.com>
2022-05-31 21:14:57 -0600

    Update keyboards/keebmaker/perigee/rev1/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keebmaker/perigee/rev1/rules.mk
+++ keyboards/keebmaker/perigee/rev1/rules.mk
@@ -11,11 +11,11 @@ BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 
