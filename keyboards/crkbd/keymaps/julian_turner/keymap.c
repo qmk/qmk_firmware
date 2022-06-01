fbfbf1fe71f6012917f3e4e74297a88f5ad0cc81
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-06-01 11:16:47 +0200

    updated files from pull request

--- keyboards/crkbd/keymaps/julian_turner/keymap.c
+++ keyboards/crkbd/keymaps/julian_turner/keymap.c
@@ -1,11 +1,22 @@
+/* Copyright 2022 Julian Turner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include QMK_KEYBOARD_H
 #include "keycodes.h"
 #include "keytabs.h"
-
-#ifdef OLED_ENABLE
-#    include "oled.c"
-#endif
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -47,4 +58,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
 };
 // clang-format off
-
--- keyboards/crkbd/keymaps/julian_turner/oled.c
+++ keyboards/crkbd/keymaps/julian_turner/oled.c
@@ -1,3 +1,21 @@
+/* Copyright 2022 Julian Turner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "layers.h"
+
 char wpm_str[10];
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -48,8 +66,8 @@ void oled_render_logo(void) {
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         oled_render_layer_state();
-        sprintf(wpm_str, "WPM:%d", get_current_wpm());
-        oled_write(wpm_str, false);
+        oled_write_P(PSTR("WPM: "), false);
+        oled_write(get_u8_str(get_current_wpm(), ' '), false);
 
     } else {
         oled_render_logo();
