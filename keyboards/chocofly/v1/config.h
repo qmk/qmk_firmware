fa4b909c2c42fcefbcc6e522af9ab4f70564d119
Will Winder <wwinder.unh@gmail.com>
2022-06-01 14:35:10 -0400

    [Keyboard] Add Chocofly v1

---
+++ keyboards/chocofly/v1/config.h
@@ -0,0 +1,22 @@
+// Copyright 2022 Vitaly Volkov (@vlkv)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VENDOR_ID       0xCEE2
+#define PRODUCT_ID      0x1001
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    vitvlkv
+#define PRODUCT         Chocofly
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 8
+
+#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
+
+#define DIODE_DIRECTION COL2ROW
+
+#define ENCODERS_PAD_A { D3 }
+#define ENCODERS_PAD_B { D2 }
+#define ENCODER_RESOLUTION 2
---
+++ keyboards/chocofly/v1/rules.mk
@@ -0,0 +1,12 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+RGBLIGHT_ENABLE = no
+SPLIT_KEYBOARD = no
+ENCODER_ENABLE = yes
