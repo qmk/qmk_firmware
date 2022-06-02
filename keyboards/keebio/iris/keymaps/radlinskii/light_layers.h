aceda003e4d08f7060015f1b86ef571ab7219a3c
radlinskii <radlinskiignacy@gmail.com>
2022-06-02 14:05:51 +0200

    update light colors for colemak layer

--- keyboards/keebio/iris/keymaps/radlinskii/light_layers.h
+++ keyboards/keebio/iris/keymaps/radlinskii/light_layers.h
@@ -28,25 +28,25 @@ const rgblight_segment_t PROGMEM QWERTY_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
 
 const rgblight_segment_t PROGMEM COLEMAK_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
     // left side
-    {0, 6, 84, 255, 255},
-    {6, 6, 89, 255, 255},
-    {12, 6, 96, 255, 255},
-    {18, 6, 106, 255, 255},
-    {24, 1, 116, 255, 255},
+    {0, 6, 53, 255, 255},
+    {6, 6, 63, 255, 255},
+    {12, 6, 73, 255, 255},
+    {18, 6, 80, 255, 255},
+    {24, 1, 85, 255, 255},
     {25, 1, 169, 255, 255}, // TT(_NUM_SYM)
-    {26, 2, 116, 255, 255},
-    {28, 3, 116, 255, 255}, // underglow
-    {31, 3, 84, 255, 255}, // underglow
+    {26, 2, 85, 255, 255},
+    {28, 3, 85, 255, 255}, // underglow
+    {31, 3, 53, 255, 255}, // underglow
     // right side
-    {34, 6, 84, 255, 255},
-    {40, 6, 89, 255, 255},
-    {46, 6, 96, 255, 255},
-    {52, 6, 106, 255, 255},
-    {58, 1, 116, 255, 255},
+    {34, 6, 53, 255, 255},
+    {40, 6, 63, 255, 255},
+    {46, 6, 73, 255, 255},
+    {52, 6, 80, 255, 255},
+    {58, 1, 85, 255, 255},
     {59, 1, 201, 255, 255}, // TT(_NAV)
-    {60, 2, 116, 255, 255},
-    {62, 3, 116, 255, 255}, // underglow
-    {65, 3, 84, 255, 255} // underglow
+    {60, 2, 85, 255, 255},
+    {62, 3, 85, 255, 255}, // underglow
+    {65, 3, 53, 255, 255} // underglow
 );
 
 const rgblight_segment_t PROGMEM NUM_SYM_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -113,7 +113,7 @@ const rgblight_segment_t PROGMEM MOUSE_MEDIA_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGM
     {47, 5, 27, 255, 255},
     {52, 6, 35, 255, 255},
     {58, 1, 43, 255, 255},
-    {59, 1, 169, 255, 255}, // TO(_NUM_SYM)
+    {59, 1, 201, 255, 255}, // TO(_NAV)
     {60, 2, 43, 255, 255},
     {62, 3, 43, 255, 255}, // underglow
     {65, 3, 11, 255, 255} // underglow
@@ -123,11 +123,11 @@ const rgblight_segment_t PROGMEM DANGER_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
     // both sides
     {0, 1, 0, 0, 255}, // RESET
     {1, 4, 0, 255, 255},
-    {5, 1, 85, 255, 255}, // QWERTY
+    {5, 1, 117, 255, 255}, // QWERTY
     {6, 19, 0, 255, 255},
     {25, 1, 85, 255, 255}, // TO(_QWERTY)
     {26, 13, 0, 255, 255},
-    {39, 1, 116, 255, 255}, // COLEMAK
+    {39, 1, 53, 255, 255}, // COLEMAK
     {40, 19, 0, 255, 255},
     {59, 1, 85, 255, 255}, // TO(_QWERTY)
     {60, 8, 0, 255, 255} // including right side underglow
