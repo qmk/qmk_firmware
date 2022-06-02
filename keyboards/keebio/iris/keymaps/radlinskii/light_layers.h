c25b75cf2593a147c51f572325a005d9894324b6
radlinskii <radlinskiignacy@gmail.com>
2022-06-02 16:38:17 +0200

    update qwerty layer light

--- keyboards/keebio/iris/keymaps/radlinskii/light_layers.h
+++ keyboards/keebio/iris/keymaps/radlinskii/light_layers.h
@@ -5,25 +5,25 @@
 
 const rgblight_segment_t PROGMEM QWERTY_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
     // left side
-    {0, 6, 117, 255, 255},
-    {6, 6, 107, 255, 255},
+    {0, 6, 127, 255, 255},
+    {6, 6, 115, 255, 255},
     {12, 6, 97, 255, 255},
     {18, 6, 90, 255, 255},
     {24, 1, 85, 255, 255},
     {25, 1, 169, 255, 255}, // TT(_NUM_SYM)
     {26, 2, 85, 255, 255},
     {28, 3, 85, 255, 255}, // underglow
-    {31, 3, 117, 255, 255}, // underglow
+    {31, 3, 127, 255, 255}, // underglow
     // right side
-    {34, 6, 117, 255, 255},
-    {40, 6, 107, 255, 255},
+    {34, 6, 127, 255, 255},
+    {40, 6, 115, 255, 255},
     {46, 6, 97, 255, 255},
     {52, 6, 90, 255, 255},
     {58, 1, 85, 255, 255},
     {59, 1, 201, 255, 255}, // TT(_NAV)
     {60, 2, 85, 255, 255},
     {62, 3, 85, 255, 255}, // underglow
-    {65, 3, 117, 255, 255} // underglow
+    {65, 3, 127, 255, 255} // underglow
 );
 
 const rgblight_segment_t PROGMEM COLEMAK_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -123,7 +123,7 @@ const rgblight_segment_t PROGMEM DANGER_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
     // both sides
     {0, 1, 180, 255, 255}, // RESET
     {1, 4, 0, 255, 255},
-    {5, 1, 117, 255, 255}, // QWERTY
+    {5, 1, 127, 255, 255}, // QWERTY
     {6, 19, 0, 255, 255},
     {25, 1, 85, 255, 255}, // TO(_QWERTY)
     {26, 13, 0, 255, 255},
