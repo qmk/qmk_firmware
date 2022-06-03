8e807cf0b4dffe05305e16778675d24e1f1af706
fauxpark <fauxpark@gmail.com>
2022-06-04 08:25:31 +1000

    Add missing flashers

--- util/install/msys2.sh
+++ util/install/msys2.sh
@@ -9,10 +9,10 @@ _qmk_install() {
 
     pacman --needed --noconfirm --disable-download-timeout -S pactoys
     pacboy sync --needed --noconfirm --disable-download-timeout \
-        base-devel: toolchain:x clang:x python-qmk:x \
+        base-devel: toolchain:x clang:x python-qmk:x hidapi:x \
         avr-binutils:x avr-gcc:x avr-libc:x \
         arm-none-eabi-binutils:x arm-none-eabi-gcc:x arm-none-eabi-newlib:x \
-        avrdude:x bootloadhid:x dfu-programmer:x dfu-util:x teensy-loader-cli:x hidapi:x
+        avrdude:x bootloadhid:x dfu-programmer:x dfu-util:x hid-bootloader-cli:x mdloader:x teensy-loader-cli:x wb32-dfu-updater:x
 
     _qmk_install_drivers
 }
