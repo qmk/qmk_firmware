ca8d50465445805589004b261d9a363a7493061b
Vicktor Hutama <vicktor.hutama@gmail.com>
2022-06-04 21:13:15 +0700

    removed comments

--- keyboards/runes/skjoldr/keymaps/via/keymap.c
+++ keyboards/runes/skjoldr/keymaps/via/keymap.c
@@ -52,8 +52,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
-
-// void keyboard_post_init_user(void) {
-//   debug_enable=true;
-//   debug_matrix=true;
-// }
