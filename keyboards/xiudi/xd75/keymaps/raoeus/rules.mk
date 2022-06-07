869f88a95ac3b49f78e8b9409212aa1ae02f0186
Zach Rice <mgmt@zachrice.media>
2022-06-07 16:09:11 -0400

    Renamed my kemapping to 'raoeus' and made it mimic my Preonic keymapping

---
+++ keyboards/xiudi/xd75/keymaps/raoeus/rules.mk
@@ -0,0 +1,3 @@
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+STENO_ENABLE = yes          # Additional protocols for Stenography(+1700), requires VIRTSER
+AUTO_SHIFT_ENABLE = yes	    # When you hold down a character in QWERTY, shift is automatically applied
