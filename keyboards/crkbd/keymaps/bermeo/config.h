b8d10fecc2952ccaf5422c2c3283509f09dc152f
Guilherme Bermêo <guilherme.bermeo@gmail.com>
2022-06-08 17:37:22 -0300

    Update keyboards/crkbd/keymaps/bermeo/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/crkbd/keymaps/bermeo/config.h
+++ keyboards/crkbd/keymaps/bermeo/config.h
@@ -28,12 +28,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-// #define SSD1306OLED
-#undef USE_I2C
-#undef SSD1306OLED
-
-#define USE_SERIAL_PD2
-
 // #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 150
 // #define RETRO_TAPPING
