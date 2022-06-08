8ca31b3be2e4f048d1c573e425959a474abd6b70
Brian <Bapowell00@gmail.com>
2022-06-08 11:54:55 -0700

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/coseyfannitutti/discipline/keymaps/briianpowell/keymap.c
+++ keyboards/coseyfannitutti/discipline/keymaps/briianpowell/keymap.c
@@ -44,12 +44,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |Ctrl|Gui |Alt |         Space    |Gui |Alt |Ctrl|  |Lef|Dow|Rig|
   * `---------------------------------------------------------------'
   */
-  [_QWERTY] = LAYOUT_65_ansi( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,  KC_4, KC_5, KC_6, KC_7, KC_8,   KC_9,  KC_0,   KC_MINS, KC_EQL, KC_BSPC,  KC_GRV,\
-    KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R, KC_T, KC_Y, KC_U, KC_I,   KC_O,  KC_P,   KC_LBRC,KC_RBRC, KC_BSLS,  KC_DEL,\
-    FN_ESC,  KC_A,    KC_S,    KC_D,  KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L,  KC_SCLN,KC_QUOT,         KC_ENT,  KC_HOME,\
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,  KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT,KC_SLSH,        KC_RSFT, KC_UP,    KC_END,\
-    KC_LCTL,  KC_LGUI, KC_LALT,            KC_SPC,                  KC_RALT, KC_RGUI, KC_RCTL,KC_LEFT, KC_DOWN, KC_RGHT \
+  [_QWERTY] = LAYOUT_65_ansi(
+    KC_ESC,  KC_1,    KC_2,    KC_3,  KC_4, KC_5, KC_6, KC_7, KC_8,   KC_9,  KC_0,   KC_MINS, KC_EQL, KC_BSPC,  KC_GRV,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R, KC_T, KC_Y, KC_U, KC_I,   KC_O,  KC_P,   KC_LBRC,KC_RBRC, KC_BSLS,  KC_DEL,
+    FN_ESC,  KC_A,    KC_S,    KC_D,  KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L,  KC_SCLN,KC_QUOT,         KC_ENT,  KC_HOME,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,  KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT,KC_SLSH,        KC_RSFT, KC_UP,    KC_END,
+    KC_LCTL,  KC_LGUI, KC_LALT,            KC_SPC,                  KC_RALT, KC_RGUI, KC_RCTL,KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* Workman
@@ -65,12 +65,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Alt |Gui |         Space    |Gui |Alt |Ctrl|  |Lef|Dow|Rig|
    * `---------------------------------------------------------------'
    */
-  [_WORKMAN] = LAYOUT_65_ansi( \
-      KC_ESC,  KC_1,   KC_2,   KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,  KC_EQL, KC_BSPC, KC_GRV,\
-      KC_TAB,  KC_Q,   KC_D,   KC_R, KC_W, KC_B, KC_J, KC_F, KC_U,    KC_P,   KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,\
-      FN_ESC,  KC_A,   KC_S,   KC_H, KC_T, KC_G, KC_Y, KC_N, KC_E,    KC_O,   KC_I,    KC_QUOT,          KC_ENT, KC_HOME,\
-      KC_LSFT,  KC_Z,   KC_X,   KC_M, KC_C, KC_V, KC_K, KC_L, KC_COMM, KC_DOT, KC_SLSH,         KC_RSFT,  KC_UP,   KC_END,\
-      KC_LCTL,  KC_LGUI,KC_LALT,            KC_SPC,                  KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT \
+  [_WORKMAN] = LAYOUT_65_ansi(
+      KC_ESC,  KC_1,   KC_2,   KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,  KC_EQL, KC_BSPC, KC_GRV,
+      KC_TAB,  KC_Q,   KC_D,   KC_R, KC_W, KC_B, KC_J, KC_F, KC_U,    KC_P,   KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+      FN_ESC,  KC_A,   KC_S,   KC_H, KC_T, KC_G, KC_Y, KC_N, KC_E,    KC_O,   KC_I,    KC_QUOT,          KC_ENT, KC_HOME,
+      KC_LSFT,  KC_Z,   KC_X,   KC_M, KC_C, KC_V, KC_K, KC_L, KC_COMM, KC_DOT, KC_SLSH,         KC_RSFT,  KC_UP,   KC_END,
+      KC_LCTL,  KC_LGUI,KC_LALT,            KC_SPC,                  KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Function
@@ -86,12 +86,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |      PLY/PS      |    |    |    |  |PRV|VL-|NXT|
      * `---------------------------------------------------------------'
      */
-    [_FUNC] = LAYOUT_65_ansi( \
+    [_FUNC] = LAYOUT_65_ansi(
       _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,   RESET,\
-      _______,KC_WH_U,KC_BTN1,KC_MS_U,KC_BTN2,_______,_______,AG_NORM,AG_SWAP,QWERTY,WORKMAN,_______,_______,_______,   KC_INS,\
-      _______,KC_WH_D,KC_MS_L,KC_MS_D,KC_MS_R,_______,_______,_______,_______,_______,_______,_______,       _______,  KC_PGUP,\
-      _______,KC_WH_L,KC_BTN3,KC_WH_R,_______,_______,_______,_______,_______,_______,_______,        _______,KC_VOLU, KC_PGDN,\
-      _______,_______,_______,                        KC_MPLY,                _______,_______,_______,KC_MPRV,KC_VOLD, KC_MNXT \
+      _______,KC_WH_U,KC_BTN1,KC_MS_U,KC_BTN2,_______,_______,AG_NORM,AG_SWAP,QWERTY,WORKMAN,_______,_______,_______,   KC_INS,
+      _______,KC_WH_D,KC_MS_L,KC_MS_D,KC_MS_R,_______,_______,_______,_______,_______,_______,_______,       _______,  KC_PGUP,
+      _______,KC_WH_L,KC_BTN3,KC_WH_R,_______,_______,_______,_______,_______,_______,_______,        _______,KC_VOLU, KC_PGDN,
+      _______,_______,_______,                        KC_MPLY,                _______,_______,_______,KC_MPRV,KC_VOLD, KC_MNXT
     ),
 };
 
