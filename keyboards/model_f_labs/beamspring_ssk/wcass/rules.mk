938b58fa3bfade638344e08a3845e6004fd569f8
Matthew J. Wolf <matthew.wolf.hpsdr@speciosus.net>
2022-06-08 22:27:33 -0400

    Added what Joe of Model F Labs requested.

--- keyboards/model_f_labs/beamspring_ssk/wcass/rules.mk
+++ keyboards/model_f_labs/beamspring_ssk/wcass/rules.mk
@@ -38,7 +38,7 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover
+NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
@@ -47,7 +47,9 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 RAW_ENABLE = yes
+LTO_ENABLE = yes            # Link Time Optimization - reduces file size
+HAPTIC_ENABLE = yes
+HAPTIC_DRIVER = SOLENOID
 
 CUSTOM_MATRIX=lite
 SRC += ../xwhatsit_capsense_matrix.c ../xwhatsit_util_comm.c
-HAPTIC_ENABLE += SOLENOID
