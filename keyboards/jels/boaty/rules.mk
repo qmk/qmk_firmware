e9a8df8f66e413a0148ef89c7e31ff82e0d2b0a6
Jels02 <joahnelson@gmail.com>
2022-06-13 22:34:17 +1000

    add info.json and remove bootloader options

--- keyboards/jels/boaty/rules.mk
+++ keyboards/jels/boaty/rules.mk
@@ -1,14 +1,6 @@
 # MCU name
 MCU = atmega328p
 
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = usbasploader
 
 # Build Options
