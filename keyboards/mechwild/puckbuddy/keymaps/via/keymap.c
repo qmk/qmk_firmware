9a37b4415fe84918b723eff76d194c0d2cd26c53
Kyle McCreery <mccreery.kyle@gmail.com>
2022-06-14 14:30:31 -0400

    Updated default keymaps to have the tap and dpi keys on by default

--- keyboards/mechwild/puckbuddy/keymaps/via/keymap.c
+++ keyboards/mechwild/puckbuddy/keymaps/via/keymap.c
@@ -3,14 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-//void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  //debug_enable=true;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-//}
-
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
@@ -18,7 +10,8 @@ enum layer_names {
     _FN2,
     _FN3
 };
-/*
+
+/*   Physical Layout:
  *   /-------------------\
  *   |ENC| 1 | 2 | 3 |ENC|
  *   |---+---'---'---+---|
@@ -30,29 +23,36 @@ enum layer_names {
  *   \-----,---,---,-----/
  *     | A | B | C | D |
  *     \---'---'---'---/
+ *
+ *   Keymap Layout:
+ *   ENC, 1, 2, 3,    ENC,
+ *   4,               5,
+ *   6,               7,
+ *   8,   A, B, C, D, 9,
+ *   
  */
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
     [_BASE] = LAYOUT(
     KC_MUTE,  MO(_FN3), MO(_FN2), MO(_FN1),       LGUI(KC_D),
-    KC_HOME,                                      KC_PGUP,
-    KC_END,                                       KC_PGDN,
+    MO(_FN2),                                     KC_HOME,
+    MO(_FN3),                                     KC_END,
     KC_BTN3,  KC_BTN1, KC_BTN2, KC_BTN2, KC_BTN1, DPI_FINE
     ),
     [_FN1] = LAYOUT(
-    RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS,          RESET,
-    RGB_MOD,                                     DPI_UP,
-    RGB_RMOD,                                    DPI_DN,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS,          TAP_TOG,
+    DPI_UP,                                      TAP_UP,
+    DPI_DN,                                      TAP_DN,
+    KC_TRNS, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_TRNS
     ),
     [_FN2] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-    KC_TRNS,                                     KC_TRNS,
-    KC_TRNS,                                     KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_TOG,
+    KC_TRNS,                                     RGB_MOD,
+    KC_TRNS,                                     RGB_RMOD,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [_FN3] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RESET,
     KC_TRNS,                                     KC_TRNS,
     KC_TRNS,                                     KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
