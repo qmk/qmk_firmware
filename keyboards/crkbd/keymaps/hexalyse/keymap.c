32d8c1fc824c8e461993cbc1a9b20a590d0f5f17
Hexalyse <reversebob@gmail.com>
2022-06-15 19:40:02 +0200

    change order of arrays to reflect order of layers

--- keyboards/crkbd/keymaps/hexalyse/keymap.c
+++ keyboards/crkbd/keymaps/hexalyse/keymap.c
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _COLEMAK_HOME_MODS 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 3
-#define _NUM 4
+#define _NUM 3
+#define _ADJUST 4
 #define _COLEMAK 5
 
 enum custom_keycodes {
@@ -31,18 +31,6 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_COLEMAK] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_ESC,     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I, KC_O,    KC_QUOT,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            MT(MOD_LCTL, KC_LGUI),MO(_LOWER),KC_SPC,MT(MOD_LGUI, KC_BSPC),LT(_RAISE, KC_ENT),  KC_RALT
-                                      //`--------------------------'  `--------------------------'
-
-  ),
 
    [_COLEMAK_HOME_MODS] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -52,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+-------/                     |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT   ),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                         MT(MOD_LCTL, KC_LGUI),MO(_LOWER),KC_SPC,    MT(MOD_LGUI, KC_BSPC) ,LT(_RAISE, KC_ENT),  KC_RALT 
+                                         KC_LCTL,MO(_LOWER),KC_SPC,    MT(MOD_LGUI, KC_BSPC) ,LT(_RAISE, KC_ENT),  KC_RALT 
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END, XXXXXXX, KC_BSPC,
+      XXXXXXX, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END, XXXXXXX, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ESC, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,                      KC_PGDN, KC_LEFT, KC_DOWN,KC_RIGHT, XXXXXXX, XXXXXXX,
+       KC_ESC, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_VOLD,                      KC_PGDN, KC_LEFT, KC_DOWN,KC_RIGHT, KC_BSPC,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -94,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+      XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9, KC_COLN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -102,6 +90,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______,    KC_0
                                       //`--------------------------'  `--------------------------'
+  ),
+
+    [_COLEMAK] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_ESC,     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I, KC_O,    KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                            MT(MOD_LCTL, KC_LGUI),MO(_LOWER),KC_SPC,MT(MOD_LGUI, KC_BSPC),LT(_RAISE, KC_ENT),  KC_RALT
+                                      //`--------------------------'  `--------------------------'
+
   )
 };
 
--- keyboards/crkbd/keymaps/hexalyse/rules.mk
+++ keyboards/crkbd/keymaps/hexalyse/rules.mk
@@ -1,5 +1,6 @@
 MOUSEKEY_ENABLE = no    # Mouse keys
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+EXTRAKEY_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
