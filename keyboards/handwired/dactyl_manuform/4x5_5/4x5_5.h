bf726d831a68b020b178249cc670eaa6b96d9166
Shem Sedrick <ssedrick1@gmail.com>
2022-06-16 11:02:09 -0500

    Working on new dactyl

---
+++ keyboards/handwired/dactyl_manuform/4x5_5/4x5_5.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "dactyl_manuform.h"
+
+#define XXX KC_NO
+#define LAYOUT( \
+    L00, L01, L02, L03, L04,                     R00, R01, R02, R03, R04, \
+    L10, L11, L12, L13, L14,                     R10, R11, R12, R13, R14, \
+    L20, L21, L22, L23, L24,                     R20, R21, R22, R23, R24, \
+         L31, L32,                                         R32, R33, \
+                   L33,                               R43, \
+                        L34, L44,           R40, R31, \
+                        L42, L43,           R41, R42  \
+)
---
+++ keyboards/handwired/dactyl_manuform/4x5_5/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "config_common.h"
+
+
+/* USB Device descriptor parameters */
+#define PRODUCT_ID 0x3435
+#define DEVICE_VER 0x0001
+#define MANUFACTURER tshort
+
+#define MATRIX_ROWS 10
+#define MATRIX_COLUMNS 5
+
+// Wiring pins
+#define MATRIX_ROW_PINS {}
+#define MATRIX_COL_PINS {}
+
+#define DIODE_DIRECTION COL2ROW
