c942ffd86d7601e13c1d296468c3a891265002f4
Aric Crosson Bouwers <aricbouwers@outlook.com>
2022-06-17 13:45:58 -0600

    Updating Mouse keys speed

--- keyboards/planck/keymaps/ariccb/config.h
+++ keyboards/planck/keymaps/ariccb/config.h
@@ -50,10 +50,10 @@
 
 #define MOUSEKEY_INTERVAL 8            //Time between cursor movements in milliseconds.
                                        //If the refresh rate of your display is 60Hz, you could set it to 16 (1/60). As this raises the cursor speed significantly, you may want to lower MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED 12          //Maximum cursor speed at which acceleration stops
+#define MOUSEKEY_MAX_SPEED 10          //Maximum cursor speed at which acceleration stops
 #define MOUSEKEY_TIME_TO_MAX 200       //Time until maximum scroll speed is reached
 #define MOUSEKEY_DELAY 0               //Delay between pressing a movement key and cursor movement
-#define MOUSEKEY_MOVE_DELTA 10         //Step size
+#define MOUSEKEY_MOVE_DELTA 6          //Step size
 #define MOUSEKEY_WHEEL_DELAY 0         //Delay between pressing a movement key and wheel scroll movement
 #define MK_KINETIC_SPEED               //Enable kinetic mode, acceleration follows quadratic curve
 #define MOUSEKEY_WHEEL_MAX_SPEED 10    //Maximum number of scroll steps per scroll action
