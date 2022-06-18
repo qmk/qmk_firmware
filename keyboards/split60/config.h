65dfab11dc5fd2137ce3dc294943a56cf8aa1cf7
furioustank986 <51895478+furioustank986@users.noreply.github.com>
2022-06-18 12:27:10 +0900

    Update keyboards/split60/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/split60/config.h
+++ keyboards/split60/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 10
-#define MATRIX_COLS 15
+#define MATRIX_COLS 8
 
 /*
  * Keyboard Matrix Assignments
