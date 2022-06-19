e084ba96697b8564392ee97d37d2053d398ece9e
mtei <2170248+mtei@users.noreply.github.com>
2022-06-20 03:40:44 +0900

    Make fast matrix_read_cols_on_row.c available in `keyboards/handwired/symmetric70_proto/promicro/`.

--- keyboards/handwired/symmetric70_proto/promicro/boost/config.h
+++ keyboards/handwired/symmetric70_proto/promicro/boost/config.h
@@ -6,19 +6,44 @@
 #define PRODUCT      BASE_PRODUCT boost
 
 //#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
-//#define MATRIX_COL_PINS { Fa4,Fb4,Fa5,Fb5, Fa6,Fb6,Fa7,Fb7,  Ba6,Bb6,Ba2,Bb2, Ba3,Bb3,Ba1,Bb1 }
-
-#define MATRIX_MUL_SEL  {  0, 1, 0, 1,  0, 1, 0, 1,   1, 0, 1, 0,  1, 0, 1, 0 }
+//#define MATRIX_COL_PINS { F4,F4,F5,F5, F6,F6,F7,F7,  B6,B6,B2,B2, B3,B3,B1,B1 }
+//#define MATRIX_MUL_SEL  {  0, 1, 0, 1,  0, 1, 0, 1,   1, 0, 1, 0,  1, 0, 1, 0 }
 /* use 74HC157: quadruple 2-line to 1-line data selectors / multiplexers */
-#define MATRIX_MUL_SELECT  B5 /* 74HC157 pin1:~A/B */
+//#define MATRIX_MUL_SELECT  B5 /* 74HC157 pin1:~A/B */
 
- /* #define MATRIX_COL_PINS { Fa4,Fb4,Fa5,Fb5, Fa6,Fb6,Fa7,Fb7, Ba6,Bb6,Ba2,Bb2, Ba3,Bb3,Ba1,Bb1 } */
+#define MATRIX_EXTENSION_74HC157 B5 /* 74HC157 pin1:~A/B */
+
+#define EXTEND_MATRIX_ROW_PINS \
+        (MCU_GPIO, D0, 4), \
+        (MCU_GPIO, C0, 6), \
+        (MCU_GPIO, D0, 7), \
+        (MCU_GPIO, E0, 6), \
+        (MCU_GPIO, B0, 4)
+#define EXTEND_MATRIX_COL_PINS \
+        (MCU_GPIO_74HC157_A, F0, 4), \
+        (MCU_GPIO_74HC157_B, F0, 4), \
+        (MCU_GPIO_74HC157_A, F0, 5), \
+        (MCU_GPIO_74HC157_B, F0, 5), \
+        (MCU_GPIO_74HC157_A, F0, 6), \
+        (MCU_GPIO_74HC157_B, F0, 6), \
+        (MCU_GPIO_74HC157_A, F0, 7), \
+        (MCU_GPIO_74HC157_B, F0, 7), \
+        (MCU_GPIO_74HC157_B, B0, 6), \
+        (MCU_GPIO_74HC157_A, B0, 6), \
+        (MCU_GPIO_74HC157_B, B0, 2), \
+        (MCU_GPIO_74HC157_A, B0, 2), \
+        (MCU_GPIO_74HC157_B, B0, 3), \
+        (MCU_GPIO_74HC157_A, B0, 3), \
+        (MCU_GPIO_74HC157_B, B0, 1), \
+        (MCU_GPIO_74HC157_A, B0, 1)
+
+ /* #define EXTEND_MATRIX_COL_PINS (MCU_GPIO_74HC157_A, F0, 4), (MCU_GPIO_74HC157_B, F0, 4), (MCU_GPIO_74HC157_A, F0, 5), (MCU_GPIO_74HC157_B, F0, 5), (MCU_GPIO_74HC157_A, F0, 6), (MCU_GPIO_74HC157_B, F0, 6), (MCU_GPIO_74HC157_A, F0, 7), (MCU_GPIO_74HC157_B, F0, 7), (MCU_GPIO_74HC157_B, B0, 6), (MCU_GPIO_74HC157_A, B0, 6), (MCU_GPIO_74HC157_B, B0, 2), (MCU_GPIO_74HC157_A, B0, 2), (MCU_GPIO_74HC157_B, B0, 3), (MCU_GPIO_74HC157_A, B0, 3), (MCU_GPIO_74HC157_B, B0, 1), (MCU_GPIO_74HC157_A, B0, 1) */
 #define SWITCH_MATRIX_INPUT_0 \
-    /* ( ( <port>, <mask> [, <dev>] ), ... ) */ \
-    ( (F0, 0xf0, 0),   \
-      (F0, 0xf0, 1),   \
-      (B0, 0x4e, 0),   \
-      (B0, 0x4e, 1) ), \
+    /* ( ( <port>, <mask>, <dev> ), ... ) */ \
+    ( (F0, 0xf0, MCU_GPIO_74HC157_A), \
+      (F0, 0xf0, MCU_GPIO_74HC157_B), \
+      (B0, 0x4e, MCU_GPIO_74HC157_B), \
+      (B0, 0x4e, MCU_GPIO_74HC157_A) ),\
     /* ( ( <port_index>, <port_mask>, <matrix_row_mask> ), ... ) */ \
     ( (0, 0x10, 0x01), \
       (1, 0x10, 0x02), \
@@ -37,13 +62,13 @@
       (2, 0x02, 0x4000), \
       (3, 0x02, 0x8000) )
 
- /* #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 } */
+ /* #define EXTEND_MATRIX_ROW_PINS (MCU_GPIO, D0, 4), (MCU_GPIO, C0, 6), (MCU_GPIO, D0, 7), (MCU_GPIO, E0, 6), (MCU_GPIO, B0, 4) */
 #define SWITCH_MATRIX_OUTPUT_0 \
-    /* ( ( <port>, <mask> [, <dev>] ), ... ) */ \
-    ( (D0, 0x90), \
-      (C0, 0x40), \
-      (E0, 0x40), \
-      (B0, 0x10) ),\
+    /* ( ( <port>, <mask>, <dev> ), ... ) */ \
+    ( (D0, 0x90, MCU_GPIO), \
+      (C0, 0x40, MCU_GPIO), \
+      (E0, 0x40, MCU_GPIO), \
+      (B0, 0x10, MCU_GPIO) ),\
     /* ( ( <port_index>, <port_mask>, <matrix_row_mask> ), ... ) */ \
     ( (0, 0x10, 0x01), \
       (1, 0x40, 0x02), \
