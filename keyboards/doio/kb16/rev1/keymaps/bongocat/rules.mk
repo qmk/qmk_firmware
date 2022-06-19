153f8c3696df93074c3d3123492862b44db75fb2
HorrorTroll <sonicvipduc@gmail.com>
2022-06-19 11:23:50 +0700

    Added new keyboard KB16-01

---
+++ keyboards/doio/kb16/rev1/keymaps/bongocat/rules.mk
@@ -0,0 +1,12 @@
+SRC += oled/bongocat.c
+
+VIA_ENABLE = yes
+
+# Additional thing to reduce compiled size
+GRAVE_ESC_ENABLE = no 
+
+# OLED enabled
+WPM_ENABLE = yes
+
+# Encoder enabled
+ENCODER_MAP_ENABLE = no
