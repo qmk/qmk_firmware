c37d63b269f3fc9c61bc08dac0e7d3e8499d6164
ZyBeR <n/a>
2022-06-21 11:08:54 +0200

    Lunar AEK keymap updated

--- keyboards/ai03/lunar/keymaps/zyber/keymap.c
+++ keyboards/ai03/lunar/keymaps/zyber/keymap.c
@@ -18,35 +18,35 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	
+
 	/*
-	
+
 	Layout
-	
+
 	K000 K001 K002 K003 K004 K005 K006 K007 K008 K009 K010 K011 K012 K013 K212 K014
 	K100  K101  K102 K103 K104 K105 K106 K107 K108 K109 K110 K111   K112  K113 K114
 	K200    K201 K202 K203 K204 K205 K206 K207 K208 K209 K210 K211     K213    K214
 	K300      K301 K302 K303 K304 K305 K306 K307 K308 K309 K310 K311    K313   K314
 	K400 K401 K402        K403       K405       K407       K409 K410 K411 K413 K414
-	
+
 	K212 = Split backspace (bound to Del by default)
 	K403, K407 = Split space (bound to space by default)
 	K405 = Main space
-	
+
 	*/
-	
+
 	[0] = LAYOUT( /* Base */
-		KC_ESC,  KC_1,    KC_2,    KC_3,    TD(SSHT), KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC, KC_BSPC, KC_PGUP, 
-		KC_TAB,           KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     TD(LBRC), KC_RBRC, KC_BSLS, KC_PGDN, 
-		L1_EXPL,          KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(SCLN), TD(QUOT),          KC_ENT,  KC_DEL, 
-		KC_LSFT,          KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_LSFT,           KC_UP,   MO(1), 
-		KC_LCTL, KC_LALT, KC_LGUI,          _______,           KC_SPC,           _______,                   KC_RALT,  KC_GRV,   KC_LEFT, KC_DOWN, KC_RGHT
+		KC_ESC,  KC_1,    KC_2,    KC_3,    TD(SSHT), KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC, KC_BSPC, KC_PGUP,
+		KC_TAB,           KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     TD(LBRC), KC_RBRC, KC_BSLS, KC_PGDN,
+		L1_EXPL,          KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(SCLN), TD(QUOT),          KC_ENT,  KC_DEL,
+		KC_LSFT,          KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_LSFT,           KC_UP,   MO(1),
+		KC_LCTL, KC_LALT, KC_LCMD,          _______,           LCMD_T(KC_SPC),           _______,          ROPT_SRH,  KC_GRV,   KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	[1] = LAYOUT( /* FN */
-		RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_DEL,  KC_DEL,  KC_PAUS, 
-		KC_CAPS,          _______, KC_UP,   _______,  _______, _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_MPRV,  KC_MPLY,  KC_MNXT, _______, KC_SLCK, 
-		_______,          KC_LEFT, KC_DOWN, KC_RGHT,  _______, KC_VOLD, KC_VOLU, KC_P4,   KC_P5,   KC_P6,   _______,  _______,           _______, _______, 
-		_______,          KC_RCTL, KC_RGUI, KC_RALT,  _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   _______,  _______,           KC_PGUP, _______, 
+		RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_DEL,  KC_DEL,  KC_PAUS,
+		KC_CAPS,          _______, KC_UP,   _______,  _______, _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_MPRV,  KC_MPLY,  KC_MNXT, _______, KC_SLCK,
+		_______,          KC_LEFT, KC_DOWN, KC_RGHT,  _______, KC_VOLD, KC_VOLU, KC_P4,   KC_P5,   KC_P6,   _______,  _______,           _______, _______,
+		_______,          KC_RCTL, KC_RGUI, KC_RALT,  _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   _______,  _______,           KC_PGUP, _______,
 		_______, _______, _______,          _______,           _______,          _______,                   _______,  _______,  KC_HOME, KC_PGDN, KC_END
 	)
 };
