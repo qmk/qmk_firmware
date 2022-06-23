dd4f9b721ea18fc4eb44454a606a44c0490ec279
Felix Sargent <felix@truework.com>
2022-06-23 12:26:53 -0700

    Change around location of -=.

--- keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 	[_MAC] = LAYOUT(
-	KC_GRV,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_MINS,			KC_NO,	KC_6,	KC_7,	KC_8,		KC_9,	KC_0,	KC_MINUS,
-	KC_TAB,	KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	KC_EQL,			KC_NO,	KC_Y,	KC_U,	KC_I,		KC_O,	KC_P,	KC_BSLS,
+	KC_GRV,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_EQL,			KC_NO,	KC_6,	KC_7,	KC_8,		KC_9,	KC_0,	KC_MINUS,
+	KC_TAB,	KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	KC_MINS,			KC_NO,	KC_Y,	KC_U,	KC_I,		KC_O,	KC_P,	KC_BSLS,
 	MEHESC,	KC_A,	KC_S,	KC_D,	KC_F,	KC_G,	KC_LPRN,			UP,		KC_H,	KC_J,	KC_K,		KC_L,	KC_SCLN,	KC_QUOT,
 	KC_LSFT,	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,	KC_LBRC,			DOWN,	KC_N,	KC_M,	KC_COMM,		KC_DOT,	KC_SLSH,	KC_RSFT,
 
@@ -118,9 +118,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_NAV] = LAYOUT(
 	C(KC_GRV),	KC_F1,		KC_F2,		KC_F3,	KC_F4,		KC_F5,		KC_F11,					_______,	KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	KC_DEL,
-	C(KC_TAB),	A(KC_BSPC),	A(KC_LEFT),	KC_UP,	A(KC_RGHT),	A(KC_DEL),	KC_F12,					_______,	KC_J,	KC_WH_R,	KC_MS_U,	KC_WH_L,	KC_SCLN,	_______,
+	C(KC_TAB),	A(KC_BSPC),	A(KC_LEFT),	KC_UP,	A(KC_RGHT),	A(KC_DEL),	KC_F12,					_______,	KC_J,	KC_WH_R,	KC_MS_U,	KC_WH_L,	KC_NO,	_______,
 	KC_MEH,		G(KC_LEFT),	KC_LEFT,		KC_DOWN,	KC_RGHT,		G(KC_RGHT),	KC_RPRN,					KC_VOLU,	KC_WH_U,	KC_MS_L,	KC_MS_D,	KC_MS_R,	KC_WH_U,	KC_WH_U,
-	KC_LSFT,		KC_HOME,		G(KC_DOWN),	KC_PGDN,	A(KC_DOWN),	KC_END,		KC_RBRC,					KC_VOLD,	KC_WH_D,	KC_M,	KC_COMM,	KC_DOT,	KC_WH_D,	KC_WH_D,
+	KC_LSFT,		KC_HOME,		G(KC_DOWN),	KC_PGDN,	A(KC_DOWN),	KC_END,		KC_RBRC,					KC_VOLD,	KC_WH_D,	KC_WH_D,	KC_WH_U,	KC_NO,	KC_WH_D,	KC_WH_D,
 	_______,		_______,		_______,		_______,	_______,
 	_______,		_______,		_______,
 	KC_BTN3,		KC_BTN2,		KC_BTN1,
