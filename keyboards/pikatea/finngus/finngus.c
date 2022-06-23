dc333943759585926e37df9492eac266d207db0e
Jack Kester <66742583+JackPikatea@users.noreply.github.com>
2022-06-23 08:24:09 -0500

    Update keyboards/pikatea/finngus/finngus.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/pikatea/finngus/finngus.c
+++ keyboards/pikatea/finngus/finngus.c
@@ -15,7 +15,10 @@
 #include "finngus.h"
 
 #if defined(ENCODER_ENABLE)
-bool encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
     if (index == 0) { /* First encoder */
         if (clockwise) {
             tap_code_delay(KC_VOLU, 10);
