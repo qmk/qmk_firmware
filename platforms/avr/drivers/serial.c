5326588605bc9f6d6847310b56d49d1d5c250785
Drashna Jael're <drashna@live.com>
2022-06-24 00:43:11 -0700

    clarify error mesagge

--- platforms/avr/drivers/serial.c
+++ platforms/avr/drivers/serial.c
@@ -500,7 +500,7 @@ bool soft_serial_transaction(int sstd_index) {
 }
 #else
 #    ifndef USE_I2C
-#        error SOFT_SERIAL_PIN is required but has not been defined.
+#        error SOFT_SERIAL_PIN or USE_I2C is required but has not been defined.
 #    endif
 #endif
 
