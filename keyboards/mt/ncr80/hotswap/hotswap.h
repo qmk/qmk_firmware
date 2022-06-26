298b7c8539a4224a5e839113193bb1a38353cdc7
peepeetee <43021794+peepeetee@users.noreply.github.com>
2022-06-27 02:46:41 +0800

    Update keyboards/mt/ncr80/hotswap/hotswap.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mt/ncr80/hotswap/hotswap.h
+++ keyboards/mt/ncr80/hotswap/hotswap.h
@@ -20,7 +20,7 @@
 #define XXX KC_NO
 
 
-#define LAYOUT( \
+#define LAYOUT_tkl_ansi_wkl( \
     K00,      K02, K03, K04, K05,    K06, K07, K08, K68,    K67, K65, K64, K63, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K78, K70, K77, K75, K73,            K76, K72, K71, \
      K20, K21, K22, K23, K24, K25, K26, K27, K28, K88, K87, K85, K84, K83,           K86, K82, K81, \
