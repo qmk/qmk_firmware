fd492a8407cea247e304dfca587584654c285189
takashicompany <t@kashi.company>
2022-06-26 10:07:20 +0900

    fix temp

--- keyboards/takashicompany/minizone/keymaps/pimoroni_trackball_v2/keymap.c
+++ keyboards/takashicompany/minizone/keymaps/pimoroni_trackball_v2/keymap.c
@@ -31,84 +31,179 @@ uint16_t to_reset_time = 1000; // この秒数(千分の一秒)、CLICKABLE状�
 
 uint16_t click_layer = 9;   // マウス入力が可能になった際に有効になるレイヤー
 
-int16_t scroll_v_counter;   // 垂直スクロールの入力をカウントする
-int16_t scroll_h_counter;   // 水平スクロールの入力をカウントする
+int16_t scroll_v_mouse_interval_counter;   // 垂直スクロールの入力をカウントする
+int16_t scroll_h_mouse_interval_counter;   // 水平スクロールの入力をカウントする
 
 int16_t scroll_v_threshold = 30;    // この閾値を超える度に垂直スクロールが実行される
 int16_t scroll_h_threshold = 30;    // この閾値を超える度に水平スクロールが実行される
 
 int16_t after_click_lock_movement = 0;      // クリック入力後の移動量を測定する変数
 
+int16_t mouse_record_threshold = 30;    // ポインターの動きを一時的に記録するフレーム数
+int16_t mouse_move_count_ratio = 5;     // ポインターの動きを再生する際の移動フレームの係数。
+
+int16_t mouse_record_x;
+int16_t mouse_record_y;
+int16_t mouse_record_count;
+
+int16_t mouse_move_remain_count;
+
+bool is_record_mouse;
+
+bool is_mouse_move_x_min;
+int16_t mouse_move_x_sign;
+int16_t mouse_move_y_sign;
+
+double mouse_interval_delta;
+double mouse_interval_counter;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     LAYOUT(
-        LT(6, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
-        KC_A, KC_S, LT(5, KC_D), KC_F, KC_G, KC_H, KC_J, LT(5, KC_K), KC_L, KC_ENT, 
+        LT(7, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
+        KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT, 
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC, 
-        KC_LCTL, KC_LGUI, LALT_T(KC_LANG2), LSFT_T(KC_TAB), KC_SPC, LT(1, KC_LANG1), KC_PGUP, KC_NO
+        KC_LCTL, KC_LGUI, LALT_T(KC_LANG2), LSFT_T(KC_TAB), LT(2, KC_SPC), LT(1, KC_LANG1), KC_PGUP, KC_PGDN
     ),
-
+    
     LAYOUT(
         KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
         LCTL_T(KC_EQL), KC_LBRC, KC_SLSH, KC_MINS, KC_RO, KC_SCLN, KC_QUOT, KC_RBRC, KC_NUHS, KC_JYEN, 
+        LSFT_T(KC_PLUS), KC_LCBR, KC_QUES, KC_UNDS, LSFT(KC_RO), KC_COLN, KC_DQUO, KC_RCBR, LSFT(KC_NUHS), LSFT(KC_JYEN), 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    
+    LAYOUT(
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, LGUI(KC_JYEN), 
+        KC_PLUS, KC_LCBR, KC_QUES, KC_UNDS, LSFT(KC_RO), KC_COLN, KC_DQUO, KC_RCBR, LSFT(KC_NUHS), LSFT(KC_JYEN), 
         KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_LSFT, KC_SPC, KC_LANG1, KC_TRNS, KC_TRNS, KC_DEL, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
-    LAYOUT(LT(6, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
-        KC_A, KC_S, LT(5, KC_D), KC_F, KC_G, KC_H, KC_J, LT(5, KC_K), KC_L, KC_ENT, 
+    
+    LAYOUT(
+        LT(7, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
+        KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT, 
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC, 
-        KC_TRNS, KC_TRNS, LALT_T(KC_LANG2), LSFT_T(KC_TAB), KC_SPC, LT(3, KC_LANG1), KC_TRNS, KC_TRNS
+        KC_LCTL, KC_LGUI, LALT_T(KC_LANG2), LSFT_T(KC_TAB), LT(5, KC_SPC), LT(4, KC_LANG1), KC_PGUP, KC_PGDN
     ),
-
+        
     LAYOUT(
         KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
         KC_CIRC, KC_AT, KC_SLSH, KC_MINS, KC_UNDS, KC_SCLN, KC_COLN, KC_LBRC, KC_RBRC, KC_JYEN, 
-        MO(4), KC_LGUI, KC_LALT, KC_LANG2, KC_LSFT, KC_SPC, KC_LANG1, KC_TRNS, KC_TRNS, KC_DEL, 
+        LT(5, KC_TILD), KC_GRV, KC_QUES, KC_EQL, KC_UNDS, KC_PLUS, KC_ASTR, KC_LCBR, KC_RCBR, KC_PIPE, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
+    
     LAYOUT(
-        KC_EXLM, KC_DQUO, KC_HASH, KC_DLR, KC_PERC, KC_AMPR, KC_QUOT, KC_LPRN, KC_RPRN, KC_0, 
+        KC_EXLM, KC_DQUO, KC_HASH, KC_DLR, KC_PERC, KC_AMPR, KC_QUOT, KC_LPRN, KC_RPRN, KC_BSLS, 
         KC_TILD, KC_GRV, KC_QUES, KC_EQL, KC_UNDS, KC_PLUS, KC_ASTR, KC_LCBR, KC_RCBR, KC_PIPE, 
         KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_LSFT, KC_SPC, KC_LANG1, KC_TRNS, KC_TRNS, KC_DEL, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
+    
     LAYOUT(
-        KC_ESC, KC_TAB, KC_NO, KC_NO, KC_NO, KC_MS_BTN1, KC_MS_BTN2, KC_UP, KC_NO, KC_NO, 
-        KC_LCTL, KC_TRNS, KC_QUES, KC_EXLM, KC_NO, KC_WH_U, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, 
-        KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_TRNS, KC_WH_D, KC_LANG1, KC_NO, KC_NO, KC_DEL, 
+        KC_ESC, KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, 
+        KC_LCTL, KC_TRNS, KC_QUES, KC_EXLM, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, 
+        KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_TRNS, KC_NO, KC_LANG1, KC_NO, KC_NO, KC_DEL, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-
+    ), 
+    
     LAYOUT(
         KC_NO, KC_TAB, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, 
         KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
-        KC_LSFT, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, MO(7), MO(8), 
+        KC_LSFT, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, MO(8), MO(9), 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
+    
     LAYOUT(
-        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, DF(0), DF(2), 
+        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, DF(0), DF(3), 
         RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-        RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, 
+        RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-
-     LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-
+    ), 
+    
+    // LAYOUT(
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+    
     LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MY_BTN1, KC_MY_SCR, KC_MY_BTN2, KC_MY_BTN3, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_MY_SCR, KC_MY_BTN1, KC_TRNS, KC_MY_SCR, KC_MY_BTN2, KC_MY_BTN3, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
+
+    // LAYOUT(
+    //     LT(6, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
+    //     KC_A, KC_S, LT(5, KC_D), KC_F, KC_G, KC_H, KC_J, LT(5, KC_K), KC_L, KC_ENT, 
+    //     LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC, 
+    //     KC_LCTL, KC_LGUI, LALT_T(KC_LANG2), LSFT_T(KC_TAB), KC_SPC, LT(1, KC_LANG1), KC_PGUP, KC_NO
+    // ),
+
+    // LAYOUT(
+    //     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
+    //     LCTL_T(KC_EQL), KC_LBRC, KC_SLSH, KC_MINS, KC_RO, KC_SCLN, KC_QUOT, KC_RBRC, KC_NUHS, KC_JYEN, 
+    //     KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_LSFT, KC_SPC, KC_LANG1, KC_TRNS, KC_TRNS, KC_DEL, 
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+
+    // LAYOUT(LT(6, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
+    //     KC_A, KC_S, LT(5, KC_D), KC_F, KC_G, KC_H, KC_J, LT(5, KC_K), KC_L, KC_ENT, 
+    //     LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC, 
+    //     KC_TRNS, KC_TRNS, LALT_T(KC_LANG2), LSFT_T(KC_TAB), KC_SPC, LT(3, KC_LANG1), KC_TRNS, KC_TRNS
+    // ),
+
+    // LAYOUT(
+    //     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
+    //     KC_CIRC, KC_AT, KC_SLSH, KC_MINS, KC_UNDS, KC_SCLN, KC_COLN, KC_LBRC, KC_RBRC, KC_JYEN, 
+    //     MO(4), KC_LGUI, KC_LALT, KC_LANG2, KC_LSFT, KC_SPC, KC_LANG1, KC_TRNS, KC_TRNS, KC_DEL, 
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+
+    // LAYOUT(
+    //     KC_EXLM, KC_DQUO, KC_HASH, KC_DLR, KC_PERC, KC_AMPR, KC_QUOT, KC_LPRN, KC_RPRN, KC_0, 
+    //     KC_TILD, KC_GRV, KC_QUES, KC_EQL, KC_UNDS, KC_PLUS, KC_ASTR, KC_LCBR, KC_RCBR, KC_PIPE, 
+    //     KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_LSFT, KC_SPC, KC_LANG1, KC_TRNS, KC_TRNS, KC_DEL, 
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+
+    // LAYOUT(
+    //     KC_ESC, KC_TAB, KC_NO, KC_NO, KC_NO, KC_MS_BTN1, KC_MS_BTN2, KC_UP, KC_NO, KC_NO, 
+    //     KC_LCTL, KC_TRNS, KC_QUES, KC_EXLM, KC_NO, KC_WH_U, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, 
+    //     KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_TRNS, KC_WH_D, KC_LANG1, KC_NO, KC_NO, KC_DEL, 
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+
+    // LAYOUT(
+    //     KC_NO, KC_TAB, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, 
+    //     KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
+    //     KC_LSFT, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, MO(7), MO(8), 
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+
+    // LAYOUT(
+    //     RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_NO, DF(0), DF(2), 
+    //     RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+    //     RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, 
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+
+    //  LAYOUT(
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // ),
+
+    // LAYOUT(
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MY_BTN1, KC_MY_SCR, KC_MY_BTN2, KC_MY_BTN3, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_MY_SCR, KC_MY_BTN1, KC_TRNS, KC_MY_SCR, KC_MY_BTN2, KC_MY_BTN3, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    // )
 };
 
 // クリック用のレイヤーを有効にする
@@ -122,8 +217,8 @@ void enable_click_layer(void) {
 void disable_click_layer(void) {
     state = NONE;
     layer_off(click_layer);
-    scroll_v_counter = 0;
-    scroll_h_counter = 0;
+    scroll_v_mouse_interval_counter = 0;
+    scroll_h_mouse_interval_counter = 0;
 }
 
 // 自前の絶対数を返す関数
@@ -135,7 +230,8 @@ int16_t my_abs(int16_t num) {
     return num;
 }
 
-int16_t my_sign(int16_t num) {
+// 自前の符号を返す関数
+int16_t mmouse_move_y_sign(int16_t num) {
     if (num < 0)
     {
         return -1;
@@ -196,122 +292,99 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-enum pointer_state
-{
-    POINTER_NONE = 0,
-    POINTER_INPUT = 1,
-};
-
-int16_t record_x;
-int16_t record_y;
-int16_t record_count;
-
-int16_t move_count;
-
-enum pointer_state current_state;
-
-bool isXMin;
-int16_t x_sign;
-int16_t y_sign;
-
-double delta;
-double counter;
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 
-    if (current_state == POINTER_NONE)
-    {
-        if (mouse_report.x != 0 || mouse_report.y != 0)
-        {
-            current_state = POINTER_INPUT;
-            record_x = 0;
-            record_y = 0;
-            record_count = 0;
+    if (!is_record_mouse) {
+        if (mouse_report.x != 0 || mouse_report.y != 0) {
+            is_record_mouse = true;
+            mouse_record_x = 0;
+            mouse_record_y = 0;
+            mouse_record_count = 0;
         }
     }
 
-    if (current_state == POINTER_INPUT)
+    if (is_record_mouse)
     {
-        record_x += mouse_report.x;
-        record_y += mouse_report.y;
-        record_count++;
+        mouse_record_x += mouse_report.x;
+        mouse_record_y += mouse_report.y;
+        mouse_record_count++;
 
-        if (record_count > 30)
+        if (mouse_record_count >= mouse_record_threshold)
         {
-            counter = 0;
-            int16_t absX = my_abs(record_x);
-            int16_t absY = my_abs(record_y);
-            isXMin = absX < absY;
+            mouse_interval_counter = 0;
+            int16_t absX = my_abs(mouse_record_x);
+            int16_t absY = my_abs(mouse_record_y);
+            is_mouse_move_x_min = absX < absY;
 
-            // move_count = isXMin ? absY : absX;
-            move_count = absY + absX;
-            move_count *= 5;
+            mouse_move_remain_count = absY + absX;
+            mouse_move_remain_count *= mouse_move_count_ratio;
 
-            x_sign = my_sign(record_x);
-            y_sign = my_sign(record_y);
+            mouse_move_x_sign = mmouse_move_y_sign(mouse_record_x);
+            mouse_move_y_sign = mmouse_move_y_sign(mouse_record_y);
 
-            if (isXMin)
+            if (is_mouse_move_x_min)
             {
-                if (record_x == 0)
+                if (mouse_record_x == 0)
                 {
-                    delta = 0;
+                    mouse_interval_delta = 0;
                 }
                 else
                 {
-                    delta = (double)absX / (double)absY;
+                    mouse_interval_delta = (double)absX / (double)absY;
                 }
             }
             else
             {
-                if (record_y == 0)
+                if (mouse_record_y == 0)
                 {
-                    delta = 0;
+                    mouse_interval_delta = 0;
                 }
                 else
                 {
-                    delta = (double)absY / (double)absX;
+                    mouse_interval_delta = (double)absY / (double)absX;
                 }
             }
 
-            current_state = POINTER_NONE;
-            record_count = 0;
+            is_record_mouse = false;
+            mouse_record_count = 0;
         }
     }
 
-    if (move_count > 0)
+    if (mouse_move_remain_count > 0)
     {
-        counter += delta;
+        mouse_interval_counter += mouse_interval_delta;
 
-        bool minMove = counter >= 1;
+        bool can_move_min = mouse_interval_counter >= 1;
 
-        if (minMove)
+        if (can_move_min)
         {
-            counter -= 1;
+            mouse_interval_counter -= 1;
         }
 
-        if (isXMin)
+        if (is_mouse_move_x_min)
         {
-            mouse_report.y = y_sign;
+            mouse_report.y = mouse_move_y_sign;
 
-            if (minMove)
+            if (can_move_min)
             {
-                mouse_report.x = x_sign;
+                mouse_report.x = mouse_move_x_sign;
             }
         }
         else
         {
-            mouse_report.x = x_sign;
+            mouse_report.x = mouse_move_x_sign;
             
-            if (minMove)
+            if (can_move_min)
             {
-                mouse_report.y = y_sign;
+                mouse_report.y = mouse_move_y_sign;
             } 
         }
 
-        mouse_report.x *= 1 + move_count / 10;
-        mouse_report.y *= 1 + move_count / 10;
+        mouse_report.x *= 1 + mouse_move_remain_count / 10;
+        mouse_report.y *= 1 + mouse_move_remain_count / 10;
 
-        move_count--;
+        mouse_move_remain_count--;
     }
     else
     {
@@ -349,27 +422,27 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
                 // 垂直スクロールの方の感度を高める
                 if (my_abs(current_y) * 2 > my_abs(current_x)) {
 
-                    scroll_v_counter += current_y;
-                    while (my_abs(scroll_v_counter) > scroll_v_threshold) {
-                        if (scroll_v_counter < 0) {
-                            scroll_v_counter += scroll_v_threshold;
+                    scroll_v_mouse_interval_counter += current_y;
+                    while (my_abs(scroll_v_mouse_interval_counter) > scroll_v_threshold) {
+                        if (scroll_v_mouse_interval_counter < 0) {
+                            scroll_v_mouse_interval_counter += scroll_v_threshold;
                             rep_v += scroll_v_threshold;
                         } else {
-                            scroll_v_counter -= scroll_v_threshold;
+                            scroll_v_mouse_interval_counter -= scroll_v_threshold;
                             rep_v -= scroll_v_threshold;
                         }
                         
                     }
                 } else {
 
-                    scroll_h_counter += current_x;
+                    scroll_h_mouse_interval_counter += current_x;
 
-                    while (my_abs(scroll_h_counter) > scroll_h_threshold) {
-                        if (scroll_h_counter < 0) {
-                            scroll_h_counter += scroll_h_threshold;
+                    while (my_abs(scroll_h_mouse_interval_counter) > scroll_h_threshold) {
+                        if (scroll_h_mouse_interval_counter < 0) {
+                            scroll_h_mouse_interval_counter += scroll_h_threshold;
                             rep_h += scroll_h_threshold;
                         } else {
-                            scroll_h_counter -= scroll_h_threshold;
+                            scroll_h_mouse_interval_counter -= scroll_h_threshold;
                             rep_h -= scroll_h_threshold;
                         }
                     }
