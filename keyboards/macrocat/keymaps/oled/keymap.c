2bbe0cba700e23695b6aeb2d8f6b5755a2bb1a95
Uzen Huang <uzen.huang@gmail.com>
2022-06-26 12:18:40 +0800

    modified keymap to non numpad keys

--- keyboards/macrocat/keymaps/oled/keymap.c
+++ keyboards/macrocat/keymaps/oled/keymap.c
@@ -5,16 +5,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = {
-        {KC_PPLS,   KC_P9,      KC_P8,  KC_P7},
-        {KC_PMNS,   KC_P6,      KC_P5,  KC_P4},
-        {KC_PDOT,   KC_P3,      KC_P2,  KC_P1},
+        {KC_PPLS,   KC_9,       KC_8,   KC_7 },
+        {KC_PMNS,   KC_6,       KC_5,   KC_4 },
+        {KC_PDOT,   KC_3,       KC_2,   KC_1 },
         {KC_PENT,   KC_SPACE,   KC_P0,  MO(1)}
     },
     [1] = {
         {KC_PAST,   KC_NO,      KC_NO,  KC_NO},
-        {KC_PSLS,   LSFT(KC_9), KC_UP,  LSFT(KC_0)},
+        {KC_PSLS,   KC_LPRN,    KC_UP,  KC_RPRN},
         {KC_COMM,   KC_LEFT,    KC_DOWN,KC_RIGHT},
-        {KC_TAB,    KC_SPACE,   KC_P0,  KC_TRNS}
+        {KC_TAB,    KC_BSPC,    KC_P0,  KC_TRNS}
     },
     [2] = {
         {KC_NO,     KC_NO,      KC_NO,  KC_NO},
