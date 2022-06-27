e9085d9d8cbfd64427d717d279a4d615ab378be8
Taeyoon Kim <partrita@users.noreply.github.com>
2022-06-27 09:58:55 +0900

    Update rules.mk

--- keyboards/kprepublic/bm40hsrgb/keymaps/34keys/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/keymaps/34keys/rules.mk
@@ -1,4 +1,4 @@
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes # Mouse keys
-COMBO_ENABLE = yes  #  The firmware is too large!
-EXTRAKEY_ENABLE = yes # Audio control and System control
+BOOTMAGIC_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+COMBO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
