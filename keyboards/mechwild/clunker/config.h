be3d93b3d3d55664230423a6a9dc443d9ac9cb80
Kyle McCreery <mccreery.kyle@gmail.com>
2022-07-01 15:18:32 -0400

    updated config for new layout

--- keyboards/mechwild/clunker/config.h
+++ keyboards/mechwild/clunker/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 8
 
 /* Key matrix pins */
-#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3, B2 }
+#define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2, B6 }
 #define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4, B5 }
 #define UNUSED_PINS
 
@@ -59,4 +59,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
 
-#define SOLENOID_PIN B6
+#define SOLENOID_PIN F4
