9ea8c50055111ff18ab1dc6eea85177b5e756b58
bojiguard <107382014+bojiguard@users.noreply.github.com>
2022-07-02 01:42:58 +0900

    change master request

--- keyboards/fungo/rev1/rules.mk
+++ keyboards/fungo/rev1/rules.mk
@@ -3,7 +3,7 @@ MCU = atmega32u4
 
 # Bootloader selection
 BOOTLOADER = atmel-dfu   # controller Elite-C
-#BOOTLOADER = caterina   # controller pro micro
+
 
 # Build Options
 #   change yes to no to disable
@@ -30,11 +30,3 @@ SPLIT_KEYBOARD = yes       # split type
 
 DEFAULT_FOLDER = fungo/rev1
 
-# point device future(mouse)
-
-# POINTING_DEVICE_DRIVER = adns9800
-# ADNS9800_CLOCK_SPEED 	(Optional) Sets the clock speed that the sensor runs at. 	2000000
-# ADNS9800_SPI_LSBFIRST 	(Optional) Sets the Least/Most Significant Byte First setting for SPI. 	false
-# ADNS9800_SPI_MODE 	(Optional) Sets the SPI Mode for the sensor. 	3
-# ADNS9800_SPI_DIVISOR 	(Optional) Sets the SPI Divisor used for SPI communication. 	varies
-# ADNS9800_CS_PIN 	(Required) Sets the Cable Select pin connected to the sensor. 	not defined
