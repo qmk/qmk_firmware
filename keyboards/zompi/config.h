3fac02d691e8f9b76241b0044c8b951215a5e497
Thomas Haukland <thomas.haukland@gmail.com>
2022-07-02 16:05:48 +0200

    Zompi compiles and flashes!

---
+++ keyboards/zompi/config.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Thomas.Haukland@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include "config_common.h"
+#define VENDOR_ID 0xA8F8
+#define PRODUCT_ID 0x1830
+#define DEVICE_VER 0x0001
+#define MANUFACTURER tompi
+#define PRODUCT Zompi
+
+//#define SOFT_SERIAL_PIN GP0
+
+//#define MASTER_RIGHT
+
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 5
+#define RGBLIGHT_LIMIT_VAL 180
+#define MATRIX_ROW_PINS { GP2, GP3, GP4, GP5 }
+#define MATRIX_COL_PINS { GP6, GP7, GP8, GP14, GP15 }
+
+#define DIODE_DIRECTION ROW2COL
+
+#define DEBOUNCE 5
+
---
+++ keyboards/zompi/readme.md
@@ -0,0 +1,11 @@
+# Zompi
+
+My experiment with qmk and the rp2040-zero.
+
+Blingy split keyboard using audio, leds and rotary encoders, because these
+things are essentials, right? 
+
+Make example for this keyboard (after setting up your build environment):
+
+    make zompi:default
+    
