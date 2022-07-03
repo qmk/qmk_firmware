ccc0f5dca7a3c6367e94f5372b5d1c84af1fbcd2
Vino Rodrigues <github@m.vinorodrigues.com>
2022-07-03 12:26:51 +1000

    review changes recommended by tzarc

--- keyboards/idobao/id61/rules.mk
+++ keyboards/idobao/id61/rules.mk
@@ -1,4 +1,5 @@
-# This file intentionally left blank
+# Copyright 2022 Vino Rodrigues (@vinorodrigues)
+# SPDX-License-Identifier: GPL-2.0-or-later
 # ** settings are data driven & stored in `info.json` **
 
 RGB_MATRIX_ENABLE = yes
