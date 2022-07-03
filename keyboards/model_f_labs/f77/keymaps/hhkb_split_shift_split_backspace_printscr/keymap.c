5f92a74ed42b597bc8607b840596ed80d022edf5
Matthew J. Wolf <matthew.wolf.hpsdr@speciosus.net>
2022-07-02 23:42:46 -0400

    model f labs: F62, F72 - Replaced KC_NO in keymaps

--- keyboards/model_f_labs/f77/keymaps/hhkb_split_shift_split_backspace_printscr/keymap.c
+++ keyboards/model_f_labs/f77/keymaps/hhkb_split_shift_split_backspace_printscr/keymap.c
@@ -67,15 +67,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	   KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,     KC_PSCR, KC_SLCK, KC_PAUS, 
 	   KC_TAB,       KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP, 
 	   KC_LCTL,      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,     KC_DEL,  KC_END,  KC_PGDN, 
-	   KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN1),   KC_NO,   KC_UP,   KC_NO, 
-	   KC_CAPS, KC_LGUI, KC_LALT,               KC_SPC,            KC_NO,   KC_RALT,         KC_NLCK, KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
+	   KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN1),   _______,   KC_UP,   _______, 
+	   KC_CAPS, KC_LGUI, KC_LALT,               KC_SPC,            _______,   KC_RALT,         KC_NLCK, KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
 	 ),
 	[_FN1] = LAYOUT_all(
-           KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_DEL,                KC_NO, KC_NO, KC_NO,
-           KC_NO,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,  KC_NO, KC_DEL,                KC_NO, KC_NO, KC_NO,
-           _______,       KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, KC_NO, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
-           _______, _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, _______, _______,               KC_NO, KC_NO, KC_NO,
-           _______, KC_NO, _______,                MO(_FN2),                              _______, _______, KC_NO, _______,                 KC_NO, KC_NO, KC_NO
+           KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, KC_DEL,                _______, _______, _______,
+           _______,         _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,  _______, KC_DEL,                _______, _______, _______,
+           _______,       KC_VOLD, KC_VOLU, KC_MUTE, KC_EJCT, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, _______,    _______, _______, _______,
+           _______, _______, _______, _______, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, _______, _______,               _______, _______, _______,
+           _______, _______, _______,                MO(_FN2),                              _______, _______, _______, _______,                 _______, _______, _______
         ),
         [_FN2] = LAYOUT_all(
            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, HPT_DWLD, HPT_DWLI, _______, _______,   _______, _______, _______, 
