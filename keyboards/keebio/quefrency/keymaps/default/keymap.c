ffba857231f25addfb3f2808f7c24ee385b50e91
Danny Nguyen <danny@keeb.io>
2022-07-02 23:02:45 -0400

    Add GPL headers and enums

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -1,13 +1,16 @@
-#include QMK_KEYBOARD_H
+// Copyright 2022 Danny Nguyen (@nooges)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-extern keymap_config_t keymap_config;
+#include QMK_KEYBOARD_H
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _FN1 1
+enum custom_layer {
+    _BASE,
+    _FN1,
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
