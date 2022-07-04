959f222d56457bae25a38ca3958e1d767cb6574e
TW59420 <1393018289@qq.com>
2022-07-04 11:11:45 +0800

    add mw80 keyboard

---
+++ keyboards/mwstudio/mw80/apm32f103/rules.mk
@@ -0,0 +1,10 @@
+# MCU name
+MCU = STM32F103
+
+# Bootloader selection
+# BOOTLOADER = tinyuf2
+BOOTLOADER = stm32duino
+
+RGB_MATRIX_DRIVER = WS2812
+WS2812_DRIVER = pwm
+
---
+++ keyboards/mwstudio/mw80/mw80.c
@@ -0,0 +1,32 @@
+/* Copyright 2022 TW59420 <https://github.com/TW59420>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mw80.h"
+
+#ifdef RGB_MATRIX_ENABLE
+led_config_t g_led_config = { {
+
+}, {
+    { 224, 32 }, { 179, 32 }, { 134, 32 }, { 90, 32 }, { 45, 32 }, { 0, 32 },
+    { 224, 32 }, { 199, 32 }, { 174, 32 }, { 149, 32 }, { 125, 32 }, { 100, 32 }, { 75, 32 }, { 50, 32 }, { 25, 32 }, { 0, 32 }
+    //这一部分对应灯珠的物理矩阵位置，按顺序写
+}, {
+    1, 1, 1, 1, 1, 1,
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+    //标注灯珠类型，一般把轴灯和底灯分开即可
+} };
+#endif
+
