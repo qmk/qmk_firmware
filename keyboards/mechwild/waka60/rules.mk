11642bc9e82c9372bb804a0ae6bde1b62d150f5c
zykrah <zykrahgaming@gmail.com>
2022-07-05 01:57:11 +1000

    Fixed waka60 audio keymap

--- keyboards/mechwild/waka60/rules.mk
+++ keyboards/mechwild/waka60/rules.mk
@@ -12,4 +12,6 @@ RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes        # Encoder Enabled
 
+EEPROM_DRIVER = i2c 
+
 DEFAULT_FOLDER = mechwild/waka60/f401
