2d452db840b8fc2f4407a764b142b8636e685e37
zvecr <git@zvecr.com>
2022-07-05 14:05:23 +0100

    Fixup after board rename

--- platforms/chibios/converters/promicro_to_kb2040/converter.mk
+++ platforms/chibios/converters/promicro_to_kb2040/converter.mk
@@ -1,6 +1,6 @@
 # Adafruit KB2040 MCU settings for converting AVR projects
 MCU := RP2040
-BOARD := QMK_RP2040
+BOARD := QMK_PM2040
 BOOTLOADER := rp2040
 
 # These are defaults based on what has been implemented for RP2040 boards
