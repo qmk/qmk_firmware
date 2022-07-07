8f2e8cb0e7fe6c5bfb05454995320db8383eb7bb
mincedshon <sean.tan300@gmail.com>
2022-07-07 15:01:42 +0800

    update product id
    updated product id for via

--- keyboards/ecila/rev1/config.h
+++ keyboards/ecila/rev1/config.h
@@ -7,7 +7,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x534B
-#define PRODUCT_ID      0x0001
+#define PRODUCT_ID      0x534B
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    mincedshon
 #define PRODUCT         ecila
