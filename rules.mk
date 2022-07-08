e850f1cf7b19645382785150ae95f7d5a25f940b
LittlePlanlessFool <91974061+LittlePlanlessFool@users.noreply.github.com>
2022-07-08 14:06:29 +0200

    GRUB, Layout
    swapped 2. and 3. thumb button, force disable NKRO

---
+++ rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+UNICODEMAP_ENABLE  = yes
+NKRO_ENABLE = no
