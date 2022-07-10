526a1029c61fa1482c4345d53f421f089f164ff5
Michal <michal@tar.black>
2022-07-10 01:36:10 +0100

    michal keymap for sofle rev1

---
+++ keyboards/sofle/keymaps/michal/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define MIDI_ADVANCED
+#define AUTO_SHIFT_TIMEOUT 165
+
+#define LAYER_STATE_8BIT
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_TAPPING
+#define NO_MUSIC_MODE
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
