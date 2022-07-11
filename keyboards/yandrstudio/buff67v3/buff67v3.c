e9df35e41a6afb7e4cb73c3e19cbe043c17382d1
jiaxin96 <572673807@qq.com>
2022-07-11 21:33:57 +0800

    fix some extra code

--- keyboards/yandrstudio/buff67v3/buff67v3.c
+++ keyboards/yandrstudio/buff67v3/buff67v3.c
@@ -41,12 +41,13 @@ void keyboard_post_init_kb(void) {
 #endif
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_user(keycode, record)) { return false; }
     switch(keycode) {
         case LOCK_GUI:
             process_magic(GUI_TOG, record);
             return false;
         default:
-            return true;
+            break;
     }
     return true;
 }
