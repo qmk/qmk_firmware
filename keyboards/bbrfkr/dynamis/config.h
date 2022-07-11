ee8461e226c6b615011a1fc0ff4b7e4a3871f52b
bbrfkr <bbrfkr@gmail.com>
2022-07-11 16:43:08 +0000

    support via for remapping encoder

--- keyboards/bbrfkr/dynamis/config.h
+++ keyboards/bbrfkr/dynamis/config.h
@@ -26,9 +26,9 @@
 #define PRODUCT dynamis
 
 /* key matrix */
-#define MATRIX_ROWS 10
+#define MATRIX_ROWS 11
 #define MATRIX_COLS 7
-#define MATRIX_ROW_PINS { B6, B4, D6, D5, D1, C6, B5, D7, D4, D0 }
+#define MATRIX_ROW_PINS { B6, B4, D6, D5, D1, C6, B5, D7, D4, D0, D2 }
 #define MATRIX_COL_PINS { F0, F1, F4, F5, F6, F7, C7 }
 
 /* rgb num */
