2714c70bd774489f039b7c60a2f8634a64ff40c3
Joel Challis <git@zvecr.com>
2022-07-11 22:45:56 +0100

    Fix layout macro for nightly_boards/paraluman (#17633)

--- keyboards/nightly_boards/paraluman/paraluman.h
+++ keyboards/nightly_boards/paraluman/paraluman.h
@@ -30,14 +30,14 @@
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K1D,   \
     K10,   K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K2D,      \
     K20,    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,          \
-    K30,       K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,     K3D,  \
-    K40,  K42,  K43,              K47,                K4A,  K4B,  K4C,  K4D   \
+    K30,       K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,     K3D,   \
+    K40,  K42,  K43,              K47,                K4A,  K4B,  K4C,  K4D      \
 ) { \
     { K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A,   K0B,   K0C,   K0D   }, \
     { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D   }, \
     { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   K2D   }, \
     { K30,   KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D   }, \
-    { K40,   KC_NO, K42,   K43,   KC_NO, KC_NO, KC_NO, K47,   KC_NO,  KC_NO, K4A,   K4B,   K4C,   K4D   }, \
+    { K40,   KC_NO, K42,   K43,   KC_NO, KC_NO, KC_NO, K47,   KC_NO, KC_NO, K4A,   K4B,   K4C,   K4D   } \
 }
 
 #define LAYOUT_60_ansi_split_bs_rshift_tsangan( \
@@ -51,7 +51,5 @@
     { K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D   }, \
     { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   K2D   }, \
     { K30,   KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   K3C,   K3D   }, \
-    { K40,   KC_NO, K42,   K43,   KC_NO, KC_NO, KC_NO, K47,   KC_NO,  KC_NO, KC_NO, K4B,   K4C,   K4D   }, \
-	{ KC_NO,  KC_NO } \
+    { K40,   KC_NO, K42,   K43,   KC_NO, KC_NO, KC_NO, K47,   KC_NO, KC_NO, KC_NO, K4B,   K4C,   K4D   } \
 }
-
