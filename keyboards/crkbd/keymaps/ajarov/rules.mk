bd574eaaffd3491b49d980558cd1ecc049669575
Alejandro J <ajarov@users.noreply.github.com>
2022-07-11 22:51:40 -0300

    Update keyboards/crkbd/keymaps/ajarov/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/crkbd/keymaps/ajarov/rules.mk
+++ keyboards/crkbd/keymaps/ajarov/rules.mk
@@ -1,5 +1,5 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+MOUSEKEY_ENABLE = yes
+RGBLIGHT_ENABLE = yes
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
