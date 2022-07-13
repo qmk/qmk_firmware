1f7448790be8cf6bc31306a49abf4db1eb0f7b8a
byanni <89546220+byanni@users.noreply.github.com>
2022-07-12 20:10:47 -0400

    Update byanni.h

--- keyboards/handwired/byanni/byanni.h
+++ keyboards/handwired/byanni/byanni.h
@@ -2,14 +2,14 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   [0] = LAYOUT(
+
+#define LAYOUT( \
 	K000, K001, K002, K003, K004, K005,                                     K012, K013, 
 	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, K112, K113, 
 	K200,       K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, K213, 
 	K300,       K302, K303, K304, K305, K306, K307, K308, K309, K310, K311, K312, K313, 
-	K400, K401, K402,       K404,       K406,       K408,       K410, K411, K412, K413, ),
-	                                                                                    
-[1] = LAYOUT(
+	K400, K401, K402,       K404,       K406,       K408,       K410, K411, K412, K413, ),	                                                                                    
+) { \
 	K000,  K001,  K002,  K003,  K004,  K005,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, K012,  K013 , 
 	K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111,  K112,  K113 , 
 	K200,  KC_NO, K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211,  K212,  K213 , 
