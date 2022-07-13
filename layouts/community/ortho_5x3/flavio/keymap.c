7a2b9f7706dcc37d8280ed2779ebb07852b09467
Flavio Di Natale <dinataleflavio@gmail.com>
2022-07-13 23:35:42 +0200

    fix layout

--- layouts/community/ortho_5x3/flavio/keymap.c
+++ layouts/community/ortho_5x3/flavio/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ 0 │00 │ . │
      * └───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_5x3(
+    LAYOUT_ortho_5x3(
         KC_A, KC_B, KC_C,
         KC_D, KC_E, KC_F,
         KC_G, KC_H, KC_I,
