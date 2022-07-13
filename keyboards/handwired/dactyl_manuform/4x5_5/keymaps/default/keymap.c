75bb5a7c88c640ef066a65eb7b7520018d1e38d4
Shem Sedrick <ssedrick1@gmail.com>
2022-07-13 13:32:50 -0500

    Updating to match c style guide

--- keyboards/handwired/dactyl_manuform/4x5_5/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5_5/keymaps/default/keymap.c
@@ -109,34 +109,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=false;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // If console is enabled, it will print the matrix position and status of each key pressed
 #ifdef CONSOLE_ENABLE
     uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 #endif 
-  switch (keycode) {
-      case QWERTY:
-          if (record->event.pressed) {
-              set_single_persistent_default_layer(_BASE);
-          }
-          return false;
-          break;
-      case COLEMAK:
-          if (record->event.pressed) {
-              set_single_persistent_default_layer(_COLEMAK);
-          }
-          return false;
-          break;
-  }
-  return true;
+    switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_BASE);
+            }
+            return false;
+            break;
+        case COLEMAK:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_COLEMAK);
+            }
+            return false;
+            break;
+    }
+    return true;
 }
 
 void persistent_default_layer_set(uint16_t default_layer) {
