643fbb25ea650f9e0a6f3021c8d6c8a73cf19038
byanni <89546220+byanni@users.noreply.github.com>
2022-07-13 00:24:47 -0400

    Update config.h

--- keyboards/handwired/byanni/config.h
+++ keyboards/handwired/byanni/config.h
@@ -6,8 +6,8 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Yanni
-#define PRODUCT         keyboard
+#define MANUFACTURER    Izzy
+#define PRODUCT         Byanni
 
 /* key matrix size */
 #define MATRIX_ROWS 5
