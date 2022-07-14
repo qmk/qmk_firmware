d8563e1cb046aea1c23a5d9767d4d5b233d610d7
Stefan Kerkmann <karlk90@pm.me>
2022-07-14 16:56:45 +0200

    Convert custom split transport keyboards

--- keyboards/whale/sk/v3/config.h
+++ keyboards/whale/sk/v3/config.h
@@ -47,3 +47,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 3
+
+/* Split communication */
+#define SOFT_SERIAL_PIN D3
