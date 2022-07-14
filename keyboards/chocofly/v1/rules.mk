bc8b530cf46e83eebd90ac8454e43b97a192f393
Will Winder <wwinder.unh@gmail.com>
2022-07-14 15:24:32 -0400

    Apply suggestions from code review
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/chocofly/v1/rules.mk
+++ keyboards/chocofly/v1/rules.mk
@@ -7,6 +7,14 @@ BOOTLOADER = caterina
 # Build Options
 #   change yes to no to disable
 #
-RGBLIGHT_ENABLE = no
-SPLIT_KEYBOARD = no
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
 ENCODER_ENABLE = yes
