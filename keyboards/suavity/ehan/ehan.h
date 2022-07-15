2735b4ec3bfdc5b423981c8d533cc16283e8a31e
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-07-15 16:41:22 -0700

    rename LAYOUT_ansi_default to LAYOUT_ansi_split_bs_rshift_lspace

--- keyboards/suavity/ehan/ehan.h
+++ keyboards/suavity/ehan/ehan.h
@@ -42,7 +42,7 @@
     { K50, ___, ___, K53, K54, K55, ___, ___, K58, ___, K5A, ___, K5C, K5D, K5E, K5F, K5G, }  \
 }
 
-#define LAYOUT_ansi_default( \
+#define LAYOUT_ansi_split_bs_rshift_lspace( \
     K00, K01, K02, K03, K04, K05,        K06, K07, K08, K09, K0A, K0B, K0C, K0D,   K0E, K0F, K0G, \
     K10, K11, K12, K13, K14, K15, K16,   K17, K18, K19, K1A, K1B, K1C, K1D, K3E,   K1E, K1F, K1G, \
     K20, K21, K22, K23, K24, K25,        K26, K27, K28, K29, K2A, K2B, K2C, K2D,   K2E, K2F, K2G, \
