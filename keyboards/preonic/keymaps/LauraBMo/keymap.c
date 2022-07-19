d351fd51d125a9dd989767e97d35739ea9d6259f
Laura Brustenga i Moncusí <brust@math.ku.dk>
2022-07-19 13:24:04 +0200

    add all Fi into lower left hand

--- keyboards/preonic/keymaps/LauraBMo/keymap.c
+++ keyboards/preonic/keymaps/LauraBMo/keymap.c
@@ -86,9 +86,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid(
  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
  _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_CPTW, KC_CPTP,
- _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_DQT,
- OCCBR,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LT,   KC_GT,   KC_QUES, KC_PIPE,
- _______, _______, OCBRC,   OCPRN,   _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+ KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_DQT,
+ KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_LT,   KC_GT,   KC_QUES, KC_PIPE,
+ _______, OCCBR,   OCBRC,   OCPRN,   _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 
