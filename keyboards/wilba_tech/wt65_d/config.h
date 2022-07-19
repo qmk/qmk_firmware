ab3817a701e5dfb59dd902c1a5d651b9da796935
Wilba <wilba@wilba.tech>
2022-07-20 09:29:24 +1000

    Update keyboards/wilba_tech/wt65_d/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/wilba_tech/wt65_d/config.h
+++ keyboards/wilba_tech/wt65_d/config.h
@@ -24,6 +24,8 @@
 // Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed
 #define DEBOUNCE 5
 
+#define LED_CAPS_LOCK_PIN F1
+
 // Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap 
 #define LOCKING_SUPPORT_ENABLE
 
