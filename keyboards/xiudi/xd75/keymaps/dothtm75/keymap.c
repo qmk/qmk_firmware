b9ba2eb1f8d55286cfadc19422f05d8b8226b249
Mike Cramer <mcramer@austin.utexas.edu>
2022-07-19 10:14:52 -0500

    Trying a less aggressive space cluster.

--- keyboards/xiudi/xd75/keymaps/dothtm75/keymap.c
+++ keyboards/xiudi/xd75/keymaps/dothtm75/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,        KC_T,         KC_LCBR,      KC_RCBR,       KC_Y,         KC_U,         KC_I,    KC_O,    KC_P,    KC_GRV,     KC_BSLS,
         KC_QUOT, KC_A,    KC_S,    KC_D,    KC_F,        KC_G,         KC_LBRC,      KC_RBRC,       KC_H,         KC_J,         KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,        KC_B,         KC_QUES,      KC_SLSH,       KC_N,         KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_UP,      KC_RSFT,
-        MO(2),  KC_LCTL, KC_LALT, KC_LGUI, LT(1,KC_SPC), LT(1,KC_SPC), LT(1,KC_SPC), LT(1,KC_SPC),  LT(1,KC_SPC), LT(1,KC_SPC), KC_RGUI, MO(2),   KC_LEFT, KC_DOWN,    KC_RGHT
+        MO(2),   KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,      KC_SPC,       MO(1),        MO(1),         KC_SPC,       KC_SPC,       KC_RGUI, MO(2),   KC_LEFT, KC_DOWN,    KC_RGHT
     ),
 
     [1] = LAYOUT_ortho_5x15(
