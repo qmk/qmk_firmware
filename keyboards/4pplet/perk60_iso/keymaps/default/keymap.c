3fa88472251780b937eee2e4d0bec6b47ba8373e
4pplet <mail@4pplet.com>
2022-07-19 11:29:16 +0200

    Update keyboards/4pplet/perk60_iso/keymaps/default/keymap.c
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/4pplet/perk60_iso/keymaps/default/keymap.c
+++ keyboards/4pplet/perk60_iso/keymaps/default/keymap.c
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,       KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,  KC_SLSH, KC_RSFT,
   KC_LCTL,  KC_LGUI,  KC_LALT,                                  KC_SPC,                                     KC_RALT,   KC_RGUI, MO(1), KC_RCTL),
   // basic function layer
-[1] = LAYOUT_all(
+[1] = LAYOUT_60_iso(
   RESET,    KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,     KC_F6,      KC_F7,          KC_F8,              KC_F9,      KC_F10,  KC_F11,   KC_F12,  KC_DEL,
-  KC_TRNS,  KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,        KC_TRNS,            KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,        KC_TRNS,            KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,        KC_TRNS,            KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,
+  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,        KC_TRNS,            KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
   KC_TRNS,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_SAI,   RGB_VAI,   RGB_SPI,    RGB_MODE_PLAIN, RGB_MODE_BREATHE,   RGB_MODE_RAINBOW,    KC_TRNS, KC_TRNS,  KC_TRNS,
   KC_TRNS,  KC_TRNS,  KC_TRNS,                       KC_TRNS,                                                   KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS)
 };
