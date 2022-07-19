08b64ff1cfc6ba84d8af20d6263737e594019060
ILWrites <pikeeb@gmail.com>
2022-07-19 14:12:15 +0300

    Update keyboards/charue/supernova/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/charue/supernova/config.h
+++ keyboards/charue/supernova/config.h
@@ -35,10 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 /* Encoder */
-#define ENCODERS_PAD_A { D0 }
-#define ENCODERS_PAD_B { D1 }
+#define ENCODERS_PAD_A { D1 }
+#define ENCODERS_PAD_B { D0 }
 #define ENCODER_RESOLUTION 4
-#define ENCODER_DIRECTION_FLIP
 
 /* RGB */
 #define RGB_DI_PIN D2
