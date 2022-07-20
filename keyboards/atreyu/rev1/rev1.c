c9bd88add34b4094d3220eae9b668933f85da4e3
Jesus Climent <climent@gmail.com>
2022-07-20 02:37:53 -0400

    Update keyboards/atreyu/rev1/rev1.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/atreyu/rev1/rev1.c
+++ keyboards/atreyu/rev1/rev1.c
@@ -14,4 +14,34 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "atreyu.h"
+#ifdef ENCODER_ENABLE
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+        return false; 
+    }
+
+    if (index == 1) {
+      if (clockwise) {
+          tap_code(KC_VOLU);
+      } else {
+          tap_code(KC_VOLD);
+      }
+    }
+    if (index == 0) {
+      if (IS_LAYER_ON(_LOWER)) {
+        if (clockwise) {
+            tap_code(KC_WH_U);
+        } else {
+            tap_code(KC_WH_D);
+        }
+      } else {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+      }
+    }
+    return true;
+}
+#endif
