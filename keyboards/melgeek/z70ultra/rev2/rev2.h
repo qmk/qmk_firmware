83948a3e6e4cf2041173eb039f72d323445c1522
bomb <84309788+mj-bomb@users.noreply.github.com>
2022-07-20 08:30:12 +0800

    Update rev2.h

--- keyboards/melgeek/z70ultra/rev2/rev2.h
+++ keyboards/melgeek/z70ultra/rev2/rev2.h
@@ -18,8 +18,6 @@
 
 #include "z70ultra.h"
 
-#include "quantum.h"
-
 #define XXX KC_NO
 #define LAYOUT_65_ansi_blocker( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, \
