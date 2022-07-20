4b73cead7b1737ca594975068bf36900cf8ae72c
Charly Delay <0xcharly@codesink.dev>
2022-07-20 21:47:21 +0900

    bastardkb/dilemma: adjust trackpad configuration

--- keyboards/bastardkb/dilemma/config.h
+++ keyboards/bastardkb/dilemma/config.h
@@ -40,3 +40,10 @@
 
 // Limits the frequency that the sensor is polled for motion.
 #define POINTING_DEVICE_TASK_THROTTLE_MS 10
+
+// Adjust trackpad rotation.
+#define POINTING_DEVICE_ROTATION_90
+
+// Configure for the Cirque model used on the Dilemma.
+#define CIRQUE_PINNACLE_DIAMETER_MM 35
+#define CIRQUE_PINNACLE_CURVED_OVERLAY
