b9ac4f58ada68c09e535e4dae34eea1471466971
Scott Bennett <scottbennett027@gmail.com>
2022-07-19 21:48:26 -0400

    Start of dz65v2 map for sbennett13

---
+++ keyboards/dztech/dz65rgb/keymaps/sbennett13/config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define NO_MUSIC_MODE
+#define FORCE_NKRO
+
+// I don't use a bunch of layers
+#define LAYER_STATE_8BIT
+
+// Disable the RGB Light (underglow) stuff
+#undef RBGLIGHT_ANIMATIONS
+#undef RBGLIGHT_EFFECT_BREATHING
+#undef RBGLIGHT_EFFECT_RAINBOW_MOOD
+#undef RBGLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RBGLIGHT_EFFECT_SNAKE
+#undef RBGLIGHT_EFFECT_KNIGHT
+#undef RBGLIGHT_EFFECT_CHRISTMAS
+#undef RBGLIGHT_EFFECT_STATIC_GRADIENT
+#undef RBGLIGHT_EFFECT_RGB_TEST
+#undef RBGLIGHT_EFFECT_ALTERNATING
+#undef RBGLIGHT_EFFECT_TWINKLE
+
+// No Cherry MX Lock switch
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+// No oneshot (Active until another key hit)
+#define NO_ACTION_ONESHOT
