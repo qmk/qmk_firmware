b159d02ade3bcf8e817f708fe9fc18672b8111b9
Jesus Climent <climent@gmail.com>
2022-07-20 02:36:59 -0400

    Update keyboards/atreyu/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/atreyu/keymaps/default/keymap.c
+++ keyboards/atreyu/keymaps/default/keymap.c
@@ -121,30 +121,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef ENCODER_ENABLE
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 1) {
-      if (clockwise) {
-          tap_code(KC_VOLU);
-      } else {
-          tap_code(KC_VOLD);
-      }
-    }
-    if (index == 0) {
-      if (IS_LAYER_ON(_LOWER)) {
-        if (clockwise) {
-            tap_code(KC_WH_U);
-        } else {
-            tap_code(KC_WH_D);
-        }
-      } else {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-      }
-    }
-    return true;
-}
-
-#endif
