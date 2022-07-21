ee0b8116a4089698c8ea5dcd6a720c1685a786b9
Jesper Severinsen <30658160+jesperseverinsen@users.noreply.github.com>
2022-07-21 11:26:29 +0200

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nyhxis/nfr_70/config.h
+++ keyboards/nyhxis/nfr_70/config.h
@@ -23,7 +23,7 @@
 #define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Nyhxis
-#define PRODUCT         Nyhxis NFR-70
+#define PRODUCT         NFR-70
 
 /* key matrix size */
 #define MATRIX_ROWS 11
