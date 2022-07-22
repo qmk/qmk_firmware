4cb2e4f8a37b498b48eef4d65317905132195ff0
jack <0x6A73@pm.me>
2022-07-21 19:25:55 -0600

    lint

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -401,7 +401,7 @@ bool process_record_quantum(keyrecord_t *record) {
                 }
                 SEND_STRING_DELAY("-kb " QMK_KEYBOARD " -km " QMK_KEYMAP SS_TAP(X_ENTER), TAP_CODE_DELAY);
                 if (temp_mod & MOD_MASK_CS) {
-                  reset_keyboard();
+                    reset_keyboard();
                 }
             }
 #endif
