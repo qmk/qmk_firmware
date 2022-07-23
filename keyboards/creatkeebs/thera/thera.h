d16bdea6df4613366fce3efd6f096c910e455845
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-07-22 18:35:57 -0700

    rename LAYOUT to LAYOUT_ansi

--- keyboards/creatkeebs/thera/thera.h
+++ keyboards/creatkeebs/thera/thera.h
@@ -61,7 +61,7 @@
     { K50,  K51,  K52,  ___,  ___,  ___,  K56,  ___,  ___,  K59,  K5A,  K5B,  K5C,  K5D,  K5E }  \
 }
 
-#define LAYOUT( \
+#define LAYOUT_ansi( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,      \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, \
