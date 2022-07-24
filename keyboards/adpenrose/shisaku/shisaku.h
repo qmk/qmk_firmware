9277ea7324d71fdaaa0590402fc86cc996df6200
Arturo Avila <mysticmetromx@gmail.com>
2022-07-24 11:39:11 -0500

    Added requested changes to the shisaku.h file.

--- keyboards/adpenrose/shisaku/shisaku.h
+++ keyboards/adpenrose/shisaku/shisaku.h
@@ -13,18 +13,20 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
+#define XXX KC_NO
+
 #define LAYOUT_all( \
-    	 K00, K01, K02, K03, K04, K05,	 K06, K10, K11, K12, K13, K14, \
-    	 K15, K16, K20, K21, K22, K23,	 K24, K25, K26, K30, K33, K34, \
-    K35, K40, K41, K42, K43, K44, K45,	 K50, K51, K52, K53, K54, K55, K60, \
-    K61, 	            K62, K63, K64, 	 K65,                K70, K74, K75 \
+    	 k00, k01, k02, k03, k04, k05,	 k06, k10, k11, k12, k13, k14, \
+    	 k15, k16, k20, k21, k22, k23,	 k24, k25, k26, k30, k33, k34, \
+    k35, k40, k41, k42, k43, k44, k45,	 k50, k51, k52, k53, k54, k55, k60, \
+    k61, 	        k62, k63, k64, 	 k65,                k70, k74, k75 \
 ) { \
-    { K00, K01, K02, K03, K04, K05, K06 }, \
-    { K10, K11, K12, K13, K14, K15, K16 }, \
-    { K20, K21, K22, K23, K24, K25, K26 }, \
-    { K30, KC_NO, KC_NO, K33, K34, K35 }, \
-    { K40, K41, K42, K43, K44, K45, KC_NO}, \
-    { K50, K51, K52, K53, K54, K55, KC_NO }, \
-    { K60, K61, K62, K63, K64, K65, KC_NO }, \
-    { K70, KC_NO, KC_NO, KC_NO, K74, K75, KC_NO } \
+    { k00, k01, k02, k03, k04, k05, k06 }, \
+    { k10, k11, k12, k13, k14, k15, k16 }, \
+    { k20, k21, k22, k23, k24, k25, k26 }, \
+    { k30, XXX, XXX, k33, k34, k35, XXX }, \
+    { k40, k41, k42, k43, k44, k45, XXX }, \
+    { k50, k51, k52, k53, k54, k55, XXX }, \
+    { k60, k61, k62, k63, k64, k65, XXX }, \
+    { k70, XXX, XXX, XXX, k74, k75, XXX } \
 }
