c6ab700bc5c31714b0c7201413e5dbc8c8b7ffb9
fauxpark <fauxpark@gmail.com>
2022-07-26 02:34:49 +1000

    Move keyboard USB IDs and strings to data driven: C

--- keyboards/charue/charon/config.h
+++ keyboards/charue/charon/config.h
@@ -19,13 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x4344 // CD for CharueDesign
-#define PRODUCT_ID   0x4348 // CH for Charon
-#define DEVICE_VER   0x0001 // Revision prototype
-#define MANUFACTURER Charue Design
-#define PRODUCT      Charon
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
