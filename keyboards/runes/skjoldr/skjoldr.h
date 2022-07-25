ebcf6c3c534b7c535033775e8b5c03e012243435
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-07-25 03:30:15 -0700

    skjoldr.h: use K<row><column> notation
    Matrix was previously defined using identifiers in the format of K<column><row>.

--- keyboards/runes/skjoldr/skjoldr.h
+++ keyboards/runes/skjoldr/skjoldr.h
@@ -29,15 +29,15 @@
  * represents the switch matrix.
  */
 #define LAYOUT(  \
-	K00, K10, K20, K30, K40, K50, K60, K70, K80, K90, KA0, KB0, KC0, KD0, \
-	K01, K11, K21, K31, K41, K51, K61, K71, K81, K91, KA1, KB1, KC1, KD1, \
-	K02, K12, K22, K32, K42, K52, K62, K72, K82, K92, KA2, KB2, KC2,       \
-	K03, K13, K23, K33, K43, K53, K63, K73, K83, K93, KA3,      KC3, KD3, \
-	K04, K14, K24,           K54,                K94, KA4, KB4, KC4, KD4 \
+	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
+	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
+	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2C, \
+	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A,      K3C, K3D, \
+	K40, K41, K42,           K45,                K49, K4A, K4B, K4C, K4D  \
 ) { \
-	{ K00, K10, K20, K30, K40, K50, K60, K70, K80, K90, KA0, KB0, KC0, KD0 }, \
-	{ K01, K11, K21, K31, K41, K51, K61, K71, K81, K91, KA1, KB1, KC1, KD1 }, \
-	{ K02, K12, K22, K32, K42, K52, K62, K72, K82, K92, KA2, KB2, KC2, ___ }, \
-	{ K03, K13, K23, K33, K43, K53, K63, K73, K83, K93, KA3, ___, KC3, KD3 }, \
-	{ K04, K14, K24, ___, ___, K54, ___, ___, ___, K94, KA4, KB4, KC4, KD4 }  \
+	{ K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D }, \
+	{ K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D }, \
+	{ K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, ___ }, \
+	{ K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, ___, K3C, K3D }, \
+	{ K40, K41, K42, ___, ___, K45, ___, ___, ___, K49, K4A, K4B, K4C, K4D }  \
 }
