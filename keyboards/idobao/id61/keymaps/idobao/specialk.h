83d538077740162ebc3ee048f51e841a69748d6b
Vino Rodrigues <github@m.vinorodrigues.com>
2022-07-26 18:47:22 +1000

    post drashna review

--- keyboards/idobao/id61/keymaps/idobao/specialk.h
+++ keyboards/idobao/id61/keymaps/idobao/specialk.h
@@ -1,8 +1,7 @@
 // Copyright 2022 Vino Rodrigues (@vinorodrigues)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#ifndef _SPECIALK_H_
-#define _SPECIALK_H_
+#pragma once
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -11,5 +10,3 @@
 
 bool ID61_process_special_k(uint16_t keycode, keyrecord_t *record, bool arrow_mode, uint8_t k_norm, uint8_t k_spcl, uint8_t k_altr);
 bool ID61_backspace_special(uint16_t keycode, keyrecord_t *record);
-
-#endif /* _SPECIALK_H_ */
