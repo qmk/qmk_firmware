3a472bf86f2806ce7f95289ad46c890c62c23769
mtei <2170248+mtei@users.noreply.github.com>
2022-07-26 16:46:24 +0900

    add OPENDRAIN_PULLUP into chibios/gpio_extr.h

--- users/mtei/platforms/chibios/gpio_extr.h
+++ users/mtei/platforms/chibios/gpio_extr.h
@@ -4,6 +4,12 @@
 
 #ifndef readPort
 
+#ifdef PAL_MODE_OUTPUT_OPENDRAIN
+#    ifdef PAL_STM32_PUPDR_PULLUP
+#        define PAL_MODE_OUTPUT_OPENDRAIN_PULLUP (PAL_MODE_OUTPUT_OPENDRAIN | PAL_STM32_PUPDR_PULLUP)
+#    endif
+#endif
+
 typedef uint16_t port_data_t;
 
 #define readPort(pin) palReadPort(PAL_PORT(pin))
@@ -13,17 +19,30 @@ typedef uint16_t port_data_t;
 #define setPortBitInputLow(pin, bit) palSetPadMode(PAL_PORT(pin), (bit), PAL_MODE_INPUT_PULLDOWN)
 
 #define setPortBitOutputPushPull(pin, bit) palSetPadMode(PAL_PORT(pin), (bit), PAL_MODE_OUTPUT_PUSHPULL)
-#define setPortBitOutputOpenDrain(pin, bit) palSetPadMode(PAL_PORT(pin), (bit), PAL_MODE_OUTPUT_OPENDRAIN)
 #define setPortBitOutput(pin, bit) setPortBitOutputPushPull(pin, bit)
+
+#ifdef PAL_MODE_OUTPUT_OPENDRAIN
+#    define setPortBitOutputOpenDrain(pin, bit) palSetPadMode(PAL_PORT(pin), (bit), PAL_MODE_OUTPUT_OPENDRAIN)
+#endif
+#ifdef PAL_MODE_OUTPUT_OPENDRAIN_PULLUP
+#    define setPortBitOutputOpenDrainPullup(pin, bit) palSetPadMode(PAL_PORT(pin), (bit), PAL_MODE_OUTPUT_OPENDRAIN_PULLUP)
+#endif
 
 #define writePortBitLow(pin, bit) palClearLine(PAL_LINE(PAL_PORT(pin), (bit)))
 #define writePortBitHigh(pin, bit) palSetLine(PAL_LINE(PAL_PORT(pin), (bit)))
 
-#define setPortBunchInput(port, bunch) palSetGroupMode(PAL_PORT(port),(bunch),0,PAL_MODE_INPUT)
-#define setPortBunchInputHigh(port, bunch) palSetGroupMode(PAL_PORT(port),(bunch),0,PAL_MODE_INPUT_PULLUP)
+#define setPortBunchInput(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_INPUT)
+#define setPortBunchInputHigh(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_INPUT_PULLUP)
 
-#define setPortBunchOutputPusuPull(port, bunch) palSetGroupMode(PAL_PORT(port),(bunch),0,PAL_MODE_OUTPUT_PUSHPULL)
+#define setPortBunchOutputPusuPull(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_PUSHPULL)
 #define setPortBunchOutput(port, bunch) setPortBunchOutputPusuPull(port, bunch)
+
+#ifdef PAL_MODE_OUTPUT_OPENDRAIN
+#    define setPortBunchOutputOpenDrain(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_OPENDRAIN)
+#endif
+#ifdef PAL_MODE_OUTPUT_OPENDRAIN_PULLUP
+#    define setPortBunchOutputOpenDrainPullup(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_OPENDRAIN_PULLUP)
+#endif
 
 #define writePortBunchLow(port, bunch) palWriteGroup(PAL_PORT(port), (bunch), 0, 0)
 #define writePortBunchHigh(port, bunch) palWriteGroup(PAL_PORT(port), (bunch), 0, (bunch))
