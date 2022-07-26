94ea31546f031982c1beab7add06e3a08f477103
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-07-25 21:59:14 -0700

    balloondogcaps/tr90pm: rename LAYOUT to LAYOUT_ortho_3x3

--- keyboards/balloondogcaps/tr90pm/keymaps/default/keymap.c
+++ keyboards/balloondogcaps/tr90pm/keymaps/default/keymap.c
@@ -16,12 +16,12 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( /* Base */
+    [0] = LAYOUT_ortho_3x3( /* Base */
         KC_1,  KC_2, KC_3,
         KC_4,  KC_5, KC_6,
         KC_VOLD,  KC_VOLU, MO(1)
     ),
-    [1] = LAYOUT(
+    [1] = LAYOUT_ortho_3x3(
         KC_7, KC_8, KC_9,
         KC_0, KC_MINUS, KC_PLUS,
         RESET, KC_AT , KC_TRNS
