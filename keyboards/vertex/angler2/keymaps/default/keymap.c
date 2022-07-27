3710df26d5683bef670f387209792a1cae7cfd88
Vertex-kb <102476474+Vertex-kb@users.noreply.github.com>
2022-07-28 02:20:07 +0800

    Update keyboards/vertex/angler2/keymaps/default/keymap.c
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/vertex/angler2/keymaps/default/keymap.c
+++ keyboards/vertex/angler2/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT, KC_SLSH, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH,          KC_LSFT,             KC_UP,
 		KC_LCTL, KC_LGUI, KC_LALT,                    KC_SPC,                     KC_LALT, MO(1),   KC_APP, KC_RCTL,    KC_LEFT, KC_DOWN,KC_RGHT
   ),
-  [1] = LAYOUT(
+  [1] = LAYOUT_all(
 
     RESET,  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   _______,_______,_______,
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   _______,_______,_______,
