912b01c495198e7ff5d71060972d675204fdea96
Mathiaszmrga <services@keyquest.tech>
2022-07-27 01:45:28 -0500

    fixes to rules, info and config

--- keyboards/keyquest/enclave/config.h
+++ keyboards/keyquest/enclave/config.h
@@ -13,12 +13,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x1117
-#define PRODUCT_ID      0x0E0E
-#define DEVICE_VER      0x0001
-#define PRODUCT         Enclave-1
-
 /* key matrix size */
 #define MATRIX_ROWS 3
 #define MATRIX_COLS 3
