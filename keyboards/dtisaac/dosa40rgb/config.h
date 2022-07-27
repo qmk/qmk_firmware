24e58722752e0d354cea847d0c4dbe529cb0fa32
fauxpark <fauxpark@gmail.com>
2022-07-27 15:34:39 +1000

    Move keyboard USB IDs and strings to data driven: D

--- keyboards/dtisaac/dosa40rgb/config.h
+++ keyboards/dtisaac/dosa40rgb/config.h
@@ -19,13 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x4454 // DT
-#define PRODUCT_ID   0x4973 // Is
-#define DEVICE_VER   0x0004
-#define MANUFACTURER DTIsaac
-#define PRODUCT      >_Dosa40
-
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 11
