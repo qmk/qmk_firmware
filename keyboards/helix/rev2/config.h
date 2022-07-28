e05381dd7ec0340ba33101533e2fad876f2f2b56
fauxpark <fauxpark@gmail.com>
2022-07-28 12:38:37 +1000

    Move keyboard USB IDs and strings to data driven: H

--- keyboards/helix/rev2/config.h
+++ keyboards/helix/rev2/config.h
@@ -19,13 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x3265
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Yushakobo
-#define PRODUCT         Helix Beta
-
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
