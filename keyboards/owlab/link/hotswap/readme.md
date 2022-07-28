6fda6c86f613b2cdfa863ca38694a96a62a2e7e7
owlab-git <keyboardpro303@gmail.com>
2022-07-28 22:32:24 +0800

    add Link files

---
+++ keyboards/owlab/link/hotswap/readme.md
@@ -0,0 +1,21 @@
+# LINK - hotswap PCB
+
+![LINK](https://imgur.com/a/Jqcl5Dq.png)
+
+A 65% made by Owlab.
+
+* Keyboard Maintainer: [Owlab](https://github.com/owlab-git)
+* Hardware Supported: LINK Hotswap PCB
+* Hardware Availability: 
+
+Make example for this keyboard (after setting up your build environment):
+
+    make owlab/link/hotswap:default
+
+Flashing example for this keyboard:
+
+    make wlab/link/hotswap:default:flash
+
+**Bootloader:** Press the physical reset button on the bottom side of the PCB (labeled SW1) or hold down ESC while plugging in the board.
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
---
+++ keyboards/owlab/link/solder/config.h
@@ -0,0 +1,51 @@
+/* 
+Copyright 2022 Owlab
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x4F53  //0x4F53 for owlab
+#define PRODUCT_ID      0x4C53  //0x4C53 LS for LINK Solder
+#define DEVICE_VER      0x0001 
+#define MANUFACTURER    Owlab 
+#define PRODUCT         LINK Solder
+
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* Keyboard Matrix Assignments */
+#define MATRIX_ROW_PINS { A1, A2, A3, A4, A5 }
+#define MATRIX_COL_PINS { A7, B0, B1, B2, B10, B11, B12, B13, B14, B15, A8, A9, A10, A6, A15 }
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+#define USB_POLLING_INTERVAL_MS 1
+
+#define LED_CAPS_LOCK_PIN A0
+#define LED_PIN_ON_STATE	0
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* NKRO */
+#ifdef NKRO_ENABLE
+#    define FORCE_NKRO
+#endif
