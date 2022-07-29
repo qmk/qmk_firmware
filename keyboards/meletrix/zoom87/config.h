63c5e2d1beeb7251601d63494f8172d766a1ad6c
GG <zjjhgzh@gmail.com>
2022-07-29 23:22:05 +0300

    change 'wuque' to 'meletrix'

--- keyboards/meletrix/zoom87/config.h
+++ keyboards/meletrix/zoom87/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2022 wuque
+/* Copyright 2022 meletrix
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
