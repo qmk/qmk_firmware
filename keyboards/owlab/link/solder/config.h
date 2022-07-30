6209660f2351188235e0dd618b9fe459f6e5c73e
owlab-git <keyboardpro303@gmail.com>
2022-07-30 19:18:28 +0800

    modified config.h

--- keyboards/owlab/link/solder/config.h
+++ keyboards/owlab/link/solder/config.h
@@ -40,8 +40,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* NKRO */
-#ifdef NKRO_ENABLE
-#    define FORCE_NKRO
-#endif
