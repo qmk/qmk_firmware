2d778d2ff28ed10c43c79783e6c519f00dee1429
Ryan <fauxpark@gmail.com>
2022-07-30 17:31:44 +1000

    Move keyboard USB IDs and strings to data driven: handwired (#17822)

--- keyboards/handwired/mechboards_micropad/config.h
+++ keyboards/handwired/mechboards_micropad/config.h
@@ -19,13 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x8968
-#define PRODUCT_ID      0x0001
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Yiancar
-#define PRODUCT         Mechboards Micropad
-
 /* key matrix size */
 #define MATRIX_ROWS 1
 #define MATRIX_COLS 4
