03740d0b3fa1435c81806d3fc4a93c90aba80db8
fauxpark <fauxpark@gmail.com>
2022-07-31 21:33:15 +1000

    Move keyboard USB IDs and strings to data driven: M

--- keyboards/manyboard/macro/config.h
+++ keyboards/manyboard/macro/config.h
@@ -18,13 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x4D42 // MB
-#define PRODUCT_ID   0x0015 // 15 keys
-#define DEVICE_VER   0x0103
-#define MANUFACTURER Manyboard
-#define PRODUCT      MB_Macro
-
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 4
