056b0cea9a88e3809144c24e7af0aa099f902a8d
Ramon Imbao <ramonimbao@gmail.com>
2022-08-02 00:05:23 +0800

    Separate into soldered and hotswap versions

--- keyboards/ghs/jem/config.h
+++ keyboards/ghs/jem/config.h
@@ -21,8 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0x0645
-#define PRODUCT_ID   0x0003
-#define DEVICE_VER   0x0001
 #define MANUFACTURER Gone Hacking Studio
 #define PRODUCT      GHS.JEM
 
