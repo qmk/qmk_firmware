3da6575e2480d46399be01c21270c57420e0f16d
Ryan <fauxpark@gmail.com>
2022-08-03 17:15:43 +1000

    Move keyboard USB IDs and strings to data driven: M (#17859)

--- keyboards/meletrix/zoom87/config.h
+++ keyboards/meletrix/zoom87/config.h
@@ -18,13 +18,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x906F
-#define PRODUCT_ID      0x0007
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    meletrix
-#define PRODUCT         zoom87
-
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 17
