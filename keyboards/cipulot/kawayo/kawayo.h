46a6a15bbc498401e86937d993f3dbfcac5e9a28
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-08-02 23:48:29 -0700

    add LAYOUT_ansi_blocker macro with reference keymap

--- keyboards/cipulot/kawayo/kawayo.h
+++ keyboards/cipulot/kawayo/kawayo.h
@@ -37,6 +37,20 @@
  * └─────┴───┴─────┴───────────────────────────┴─────┘
  */
 
+#define LAYOUT_ansi_blocker( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0E,      \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B,      k2C, k2D, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B,      k3C, k3D, \
+    k40, k41, k42,           k45,                k49, k4A,      k4B, k4C, k4D  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, ___, k0E }, \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E }, \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D, ___ }, \
+    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, ___ }, \
+    { k40, k41, k42, ___, ___, k45, ___, ___, ___, k49, k4A, k4B, k4C, k4D, ___ }  \
+}
+
 #define LAYOUT_ansi_blocker_split_bs( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E,      \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C,   k1D,    k1E, \
