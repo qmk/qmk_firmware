77ffaa19a3bd9ce1b5e21a2fd701e45527935ac7
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2022-08-04 15:06:42 +0800

    use existing board config files from chibios

--- platforms/chibios/boards/STEMCELL/board/board.mk
+++ platforms/chibios/boards/STEMCELL/board/board.mk
@@ -1,8 +1,11 @@
+# Default pin config of nucleo64_411re has most pins in input pull up mode
+
 # List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/board/board.c
+BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO64_F411RE/board.c
 
 # Required include directories
-BOARDINC = $(BOARD_PATH)/board
+BOARDINC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO64_F411RE
+
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)
