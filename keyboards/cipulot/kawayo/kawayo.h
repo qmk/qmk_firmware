e368299b6cba5f36f909301e74800eb06d67e9e7
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-08-04 12:39:18 -0700

    add LAYOUT_65_ansi_blocker_tsangan_split_bs macro

--- keyboards/cipulot/kawayo/kawayo.h
+++ keyboards/cipulot/kawayo/kawayo.h
@@ -82,6 +82,20 @@
     { k40, k41, k42, ___, ___, k45, ___, ___, ___, ___, k4A, k4B, k4C, k4D, ___ }  \
 }
 
+#define LAYOUT_65_ansi_blocker_tsangan_split_bs( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, k2E, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C,      k1D, k1E, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B,           k2C, k2D, \
+    k30,      k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B,      k3C, k3D, \
+    k40, k41, k42,           k45,                     k4A,           k4B, k4C, k4D  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E }, \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E }, \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D, k2E }, \
+    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, ___ }, \
+    { k40, k41, k42, ___, ___, k45, ___, ___, ___, ___, k4A, k4B, k4C, k4D, ___ }  \
+}
+
 #define LAYOUT_65_ansi_cb_blocker( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C,   k0E,         \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C,      k1D, k1E, \
