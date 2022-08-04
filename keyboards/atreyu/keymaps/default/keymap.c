f28cbbdc94df4ce7f378997e0a9f0f888e17f6e4
Jesus Climent <climent@gmail.com>
2022-08-04 10:57:56 +0100

    Update keyboards/atreyu/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/atreyu/keymaps/default/keymap.c
+++ keyboards/atreyu/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P    ,KC_BSPC ,
      KC_LCTRL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN ,KC_QUOT ,
      KC_LSFT , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH ,KC_RSFT ,
-     KC_LCTRL,          LWR    , KC_LALT, KC_LGUI, KC_ENT , KC_LBRC, KC_RBRC, KC_SPC , RSE    , KC_NO  ,  KC_NO ,          KC_RCTRL 
+     KC_LCTL,           LWR    , KC_LALT, KC_LGUI, KC_ENT , KC_LBRC, KC_RBRC, KC_SPC , RSE    , KC_NO  ,  KC_NO ,          KC_RCTL 
                 
   ),
 
