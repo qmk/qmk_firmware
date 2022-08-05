18c5c2ab8bb6541fab10de6906977177715d9859
Felix Jen <fjen@felixjen.com>
2022-08-05 14:17:53 -0700

    Update keyboards/keygem/kg65rgbv2/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keygem/kg65rgbv2/config.h
+++ keyboards/keygem/kg65rgbv2/config.h
@@ -19,13 +19,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x4B47 // FJLabs
-#define PRODUCT_ID   0x0002 // tf60v2
-#define DEVICE_VER   0x0001 // Version 1
-#define MANUFACTURER    FJLabs
-#define PRODUCT         KG65RGBv2
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
