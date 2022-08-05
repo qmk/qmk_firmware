ec4d86bf97730cea355972f39b31d08bf1f3ca89
yiancar <yiancar@gmail.com>
2022-08-05 23:46:29 +0100

    more pretty

--- keyboards/protozoa/p01/keymaps/via/keymap.c
+++ keyboards/protozoa/p01/keymaps/via/keymap.c
@@ -58,6 +58,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
