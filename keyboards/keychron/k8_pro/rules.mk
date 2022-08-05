5d8caab1fa289c0a086a7d56a946f2101af52f62
lokher <lokher@gmail.com>
2022-08-05 14:58:19 +0800

    Disable console

--- keyboards/keychron/k8_pro/rules.mk
+++ keyboards/keychron/k8_pro/rules.mk
@@ -10,7 +10,7 @@ BOOTLOADER = stm32-dfu
 BOOTMAGIC_ENABLE = yes      # Enable boot magic
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
