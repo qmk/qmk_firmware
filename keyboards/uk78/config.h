82bd930e572cd57aca490dfe5d3db5ed015e324a
Nick Brassel <nick@tzarc.org>
2022-08-06 22:12:28 +1000

    Remove `UNUSED_PINS` from U

--- keyboards/uk78/config.h
+++ keyboards/uk78/config.h
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* key matrix pins */
 #define MATRIX_ROW_PINS { F3, F2, F1, F0, A0 }
 #define MATRIX_COL_PINS { A2, A1, F5, F4, E6, E7, E5, E4, B7, D0, D1, D2, D3, D4, D5, D6, D7, B5, E0 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
--- keyboards/utd80/config.h
+++ keyboards/utd80/config.h
@@ -25,7 +25,6 @@
 
 #define MATRIX_ROW_PINS { B4, D5, D0, B2, B3, B0 }
 #define MATRIX_COL_PINS { B1, F0, F1, F4, F5, F6, F7, C7, C6, D3, E6, D7, D6, D4, D2, D1 }
-#define UNUSED_PINS
 
 #define BACKLIGHT_PIN B7
 #define BACKLIGHT_BREATHING
