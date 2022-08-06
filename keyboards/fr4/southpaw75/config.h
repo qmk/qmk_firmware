ca0de5701579b9d39d9c0e07c477e2410c359f67
Nick Brassel <nick@tzarc.org>
2022-08-06 22:04:21 +1000

    Remove `UNUSED_PINS` from F

--- keyboards/fr4/southpaw75/config.h
+++ keyboards/fr4/southpaw75/config.h
@@ -25,7 +25,6 @@
 /* key matrix pins */
 #define MATRIX_ROW_PINS { D3, D2, D1, D0, D4, C6, D7, E6, B4 }
 #define MATRIX_COL_PINS { B5, F4, F5, F6, F7, B1, B3, B2, B6 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
