df7a1c8e6599a172a617a08ca17ee6d0fd0ac986
Nick Brassel <nick@tzarc.org>
2022-08-06 22:02:06 +1000

    Remove `UNUSED_PINS` from 0-9

--- keyboards/1upkeyboards/sweet16/v2/proton_c/config.h
+++ keyboards/1upkeyboards/sweet16/v2/proton_c/config.h
@@ -9,7 +9,6 @@
 /* key matrix pins */
 #define MATRIX_ROW_PINS { B5, B7, B2, B0 }
 #define MATRIX_COL_PINS { B8, A0, A10, A9 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
