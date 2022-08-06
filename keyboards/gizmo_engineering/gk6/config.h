601114a7592e2766af8a6975bc55283e80cef07f
Nick Brassel <nick@tzarc.org>
2022-08-06 22:04:40 +1000

    Remove `UNUSED_PINS` from G

--- keyboards/gizmo_engineering/gk6/config.h
+++ keyboards/gizmo_engineering/gk6/config.h
@@ -28,7 +28,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #define MATRIX_ROW_PINS { B6, B4, D7, D6, D4}
 #define MATRIX_COL_PINS { B5, C6, C7, F7, F6, D5, D3, D2, F1, F4, B7, F5 }
-#define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION ROW2COL
