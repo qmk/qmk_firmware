7d805e5cd28af36f50a1407af5e4220bb2b9985d
Joshua Diamond <josh@windowoffire.com>
2022-08-06 16:26:33 -0400

    Add GPL2+ compatible license

--- layouts/community/65_ansi_blocker/spidey3/keymap.c
+++ layouts/community/65_ansi_blocker/spidey3/keymap.c
@@ -1,3 +1,6 @@
+// Copyright 2022 Joshua Diamond josh@windowoffire.com (@spidey3)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "spidey3.h"
 
 #define FN_MENU LT(_FN,KC_APP)
--- users/spidey3/config.h
+++ users/spidey3/config.h
@@ -1,3 +1,6 @@
+// Copyright 2022 Joshua Diamond josh@windowoffire.com (@spidey3)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 #define LED_DISABLE_WHEN_USB_SUSPENDED
