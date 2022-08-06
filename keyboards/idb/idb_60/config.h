08ab98d6d0d287530733a46277bb15901399f6df
Nick Brassel <nick@tzarc.org>
2022-08-06 22:05:34 +1000

    Remove `UNUSED_PINS` from I

--- keyboards/idb/idb_60/config.h
+++ keyboards/idb/idb_60/config.h
@@ -35,7 +35,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 #define MATRIX_ROW_PINS { C2, D0, D1, D2, D3, D4, D5, D6, B0, B1 }
 #define MATRIX_COL_PINS { B2, B3, B4, C6, B6, B7, C7, B5 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
