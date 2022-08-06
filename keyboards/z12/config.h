5ac75c0231def8912cb714bbb2f8c87cd4ff5ae3
Nick Brassel <nick@tzarc.org>
2022-08-06 22:13:43 +1000

    Remove `UNUSED_PINS` from Z

--- keyboards/z12/config.h
+++ keyboards/z12/config.h
@@ -34,7 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { F6, F5, F4, NO_PIN } \
 }
 
-#define UNUSED_PINS
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
