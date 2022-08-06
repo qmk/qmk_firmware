1fe6de4f11d500dfcd3677e6fd37c0d7012c0c75
Nick Brassel <nick@tzarc.org>
2022-08-06 22:12:40 +1000

    Remove `UNUSED_PINS` from V

--- keyboards/viktus/smolka/config.h
+++ keyboards/viktus/smolka/config.h
@@ -35,7 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define MATRIX_ROW_PINS { F0, F1, F4, F5, F6, F7, C7, C6 }
 #define MATRIX_COL_PINS { D6, D7, B4, B5, B6, D4, B1, B2 }
-#define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
