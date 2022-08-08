40a84d8f64c036f60a814f0464d4761c7abb0908
jels <66403163+Jels02@users.noreply.github.com>
2022-08-08 18:27:22 +1000

    Update rules.mk

--- keyboards/qwertlekeys/calice/rules.mk
+++ keyboards/qwertlekeys/calice/rules.mk
@@ -1,17 +1,18 @@
 # MCU name
 MCU = atmega32u4
 
+# Bootloader Selection
 BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = no 	    # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. - We have custom RGB underglow
+BACKLIGHT_ENABLE = no 	    # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable RGB underglow
 ENCODER_ENABLE = yes		# For Knob support
