8ec9bf15b1a44f2dd0a147d28f748b7abd8792e4
Álvaro A. Volpato <alvaro.augusto.volpato@gmail.com>
2022-08-08 22:47:44 -0300

    Move FMH USB descriptors to info.json
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/geonworks/frogmini/fms/config.h
+++ keyboards/geonworks/frogmini/fms/config.h
@@ -19,12 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define VENDOR_ID 0x6777 // gw for geonworks
-#define PRODUCT_ID 0x2D33 // MS for "mini-S"
-#define DEVICE_VER 0x0001 // Rev. Alpha
-#define MANUFACTURER GeonWorks
-#define PRODUCT FrogMini FM-S
-
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 14
 
