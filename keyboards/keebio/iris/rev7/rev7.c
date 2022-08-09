4d805f5fcc870c97b66f45cecf43142ceea95a3a
Danny Nguyen <danny@keeb.io>
2022-08-09 17:34:45 -0400

    Add defaults for additional encoders

--- keyboards/keebio/iris/rev7/rev7.c
+++ keyboards/keebio/iris/rev7/rev7.c
@@ -136,6 +136,20 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
             tap_code(KC_PGUP);
         }
     }
+    } else if (index == 2) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    } else if (index == 3) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
     return false;
 }
 #endif
