7fc299710760c56024bd786787ac599ab2f8ceb2
Andy Chiu <0836449@gmail.com>
2022-08-08 20:32:47 -0500

    Update keyboards/handwired/uc2/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/uc2/config.h
+++ keyboards/handwired/uc2/config.h
@@ -28,16 +28,6 @@
 #define MANUFACTURER AndyChiu
 #define PRODUCT      UniversalCentury2
 
-/* key matrix size */
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 8
-
-#define MATRIX_ROW_PINS { D1,D0,D4,C6,D7,E6 }
-#define MATRIX_COL_PINS { F4,F5,F6,F7,B1,B3,B2,B6 }
-
-#define UNUSED_PINS
-
-#define DIODE_DIRECTION ROW2COL
 
 #define SOFT_SERIAL_PIN D2  // or D1, D2, D3, E6
 
