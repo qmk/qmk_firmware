2d6b6395a87f9692f71f5688cc3b282317139697
Álvaro A. Volpato <alvaro.augusto.volpato@gmail.com>
2022-08-09 01:28:44 -0300

    Move USB descriptors to info.json
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/viendi8l/config.h
+++ keyboards/viendi8l/config.h
@@ -18,13 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #include "common_config.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x8673 // VI for Viendi
-#define PRODUCT_ID      0x0877 // 8L + 1, 8L was prototype
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Viendi
-#define PRODUCT         8L
-
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 18
