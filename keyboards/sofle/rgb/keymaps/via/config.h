1d4a66ff1e9e671344af61d8e8f578a694ef602c
duese <duese@users.noreply.github.com>
2022-08-10 15:21:37 +0200

    Fix keymap sofle/rgb:via

--- keyboards/sofle/rgb/keymaps/via/config.h
+++ keyboards/sofle/rgb/keymaps/via/config.h
@@ -22,21 +22,18 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#if defined(KEYBOARD_sofle_rev1)
 // Add RGB underglow and top facing lighting
-#    define RGB_DI_PIN D3
-#    define RGBLED_NUM 70
-#    define RGBLED_SPLIT \
-        { 36, 36 }
-#    ifdef RGB_MATRIX_ENABLE
-#        define DRIVER_LED_TOTAL RGBLED_NUM
-#        define RGB_MATRIX_SPLIT RGBLED_SPLIT
-#        define SPLIT_TRANSPORT_MIRROR
-#    else
-#        define RGBLIGHT_ANIMATIONS
-#        define RGBLIGHT_LIMIT_VAL 120
-#        define RGBLIGHT_HUE_STEP  10
-#        define RGBLIGHT_SAT_STEP  17
-#        define RGBLIGHT_VAL_STEP  17
-#    endif
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 70
+#define RGBLED_SPLIT { 36, 36 }
+#ifdef RGB_MATRIX_ENABLE
+#    define DRIVER_LED_TOTAL RGBLED_NUM
+#    define RGB_MATRIX_SPLIT RGBLED_SPLIT
+#    define SPLIT_TRANSPORT_MIRROR
+#else
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP  10
+#    define RGBLIGHT_SAT_STEP  17
+#    define RGBLIGHT_VAL_STEP  17
 #endif
