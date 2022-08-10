aca5372c41c7f20c11f352e18ce6863903a0bcac
Gary Kong <kongkm88@gmail.com>
2022-08-10 10:25:53 +0900

    Update keyboards/bajjak/rules.mk
    Co-authored-by: James Young <18669334+noroadsleft@users.noreply.github.com>

--- keyboards/bajjak/rules.mk
+++ keyboards/bajjak/rules.mk
@@ -12,15 +12,23 @@ OPT_DEFS += -DLEFT_LEDS
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no   # Enable Bootmagic Lite
-MOUSEKEY_ENABLE  = yes  # Mouse keys
-EXTRAKEY_ENABLE  = yes  # Audio control and System control
-CONSOLE_ENABLE   = no   # Console for debug
-COMMAND_ENABLE   = yes  # Commands for debug and configuration
-CUSTOM_MATRIX    = lite # Custom matrix file for the BAJJAK
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-UNICODE_ENABLE   = yes  # Unicode
-SWAP_HANDS_ENABLE= yes  # Allow swapping hands of keyboard
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+CUSTOM_MATRIX = lite        # Custom matrix file for the BAJJAK
+UNICODE_ENABLE = yes        # Unicode
+SWAP_HANDS_ENABLE = yes     # Allow swapping hands of keyboard
+
+# Disable unsupported hardware
+BACKLIGHT_SUPPORTED = no
+AUDIO_SUPPORTED = no
 
 DEBOUNCE_TYPE = sym_eager_pr
 
