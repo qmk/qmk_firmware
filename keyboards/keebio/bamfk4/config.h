c1991551e219e29e5362cb2a4bffee1bb014b23f
emlah <88459992+emlah@users.noreply.github.com>
2022-08-12 15:01:22 -0400

    removed usb descriptor from config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keebio/bamfk4/config.h
+++ keyboards/keebio/bamfk4/config.h
@@ -5,12 +5,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0xCB10
-#define PRODUCT_ID   0x1114
-#define DEVICE_VER   0x0001
-#define MANUFACTURER Keebio
-#define PRODUCT      BAMFK-4
 
 /* key matrix size */
 #define MATRIX_ROWS 1
