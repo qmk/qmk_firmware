4e871e9b5251395c6ffe6ac0d8607e7d20f6f9f7
Jack Kester <Jack.pikatea@gmail.com>
2022-08-13 11:13:03 -0500

    update/complete checklist for r26. no midi

--- keyboards/pikatea/pikatea_macropad_r26_led_kit/config.h
+++ keyboards/pikatea/pikatea_macropad_r26_led_kit/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Jack Kester
+Copyright 2022 Jack Kester
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
