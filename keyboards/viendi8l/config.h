536e39a5d9a00e53228ddea24361bdb7ee57faf7
Ryan <fauxpark@gmail.com>
2022-08-13 12:04:18 +1000

    Fix viendi8l compilation (#18017)

--- keyboards/viendi8l/config.h
+++ keyboards/viendi8l/config.h
@@ -16,7 +16,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#include "common_config.h"
+
+#include "config_common.h"
 
 /* key matrix size */
 #define MATRIX_ROWS 6
