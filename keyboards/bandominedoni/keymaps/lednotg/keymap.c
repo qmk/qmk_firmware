e145c7ad3fde668a0d89d0e402f49c0fb6a1b5ed
3araht <69518343+3araht@users.noreply.github.com>
2022-08-14 04:44:29 +0900

    Bandominedoni version output feature and a new keymap added (#17947)
    * SPLIT_USB_DETECT added.
    
    * lednotg keymap added.
    
    * lednotg missing modification fixed.
    
    * VERSION is available.
    
    * USER00 is used instead of SAFE_RANGE in via/keymap.c

--- keyboards/bandominedoni/keymaps/lednotg/keymap.c
+++ keyboards/bandominedoni/keymaps/lednotg/keymap.c
@@ -16,6 +16,30 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
+#define DF_COLE  DF(_COLEMAK)
+
+#define _________________QWERTY_L1_________________ KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
+#define _________________QWERTY_L2_________________ KC_A,    KC_S,    KC_D,    KC_F,    KC_G
+#define _________________QWERTY_L3_________________ KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
+
+#define _________________QWERTY_R1_________________ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
+#define _________________QWERTY_R2_________________ KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN
+#define _________________QWERTY_R3_________________ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
+
+#define _________________COLEMAK_L1________________ KC_Q,    KC_W,    KC_F,    KC_P,    KC_G
+#define _________________COLEMAK_L2________________ KC_A,    KC_R,    KC_S,    KC_T,    KC_D
+#define _________________COLEMAK_L3________________ KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
+
+#define _________________COLEMAK_R1________________ KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN
+#define _________________COLEMAK_R2________________ KC_H,    KC_N,    KC_E,    KC_I,    KC_O
+#define _________________COLEMAK_R3________________ KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
+
+#define _________________NUMBER_L__________________ KC_1,    KC_2,    KC_3,    KC_4,    KC_5
+// #define _________________NUMBER_R__________________ KC_6,    KC_7,    KC_8,    KC_9,    KC_0
+
+#define _________________FUNC__L___________________ KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5
+// #define _________________FUNC__R___________________ KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10
+
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
 #ifndef PEDAL_NORMALLY_CLOSED
@@ -25,26 +49,31 @@ enum layer_names {
 #ifdef PEDAL_NORMALLY_CLOSED
     _OPEN,
 #endif
-    _MISC,
-    _FN
+     _QWERTY,
+     _COLEMAK,
+     _MISC,
+     _FN
 };
 
 enum custom_keycodes {
-    VERSION = USER00
+    VERSION = SAFE_RANGE
 };
+
+// Alias layout macros that expand groups of keys.
+#define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_OPEN] = LAYOUT(
                   MI_Gs_1, MI_As_1, MI_Cs_2, MI_F_2, MI_Gs_3,
        MI_E_1, MI_A_1, MI_G_2, MI_Ds_2, MI_F_3, MI_As_2, MI_F_1,
-         MI_D_2, MI_A_2, MI_C_3, MI_E_3, MI_C_2, MI_G_1, TG_SWAP,
+         MI_D_2, MI_A_2, MI_C_3, MI_E_3, MI_C_2, MI_G_1, MI_B,
        MI_E_2, MI_Gs_2, MI_B_2, MI_D_3, MI_Fs_3, MI_Cs_3, MI_Fs_1,
      MI_D_1, MI_B_1, MI_G_3, MI_A_3, MI_Ds_3, MI_Fs_2, MI_Ds_1, MI_C_1,
 
      MO_SWAP,    MI_B_5, MI_Gs_5, MI_G_5, MI_F_5,     FN_MUTE,
          MI_Cs_3, MI_A_5, MI_Fs_5, MI_E_5, MI_Ds_5, KC_VOLD, KC_VOLU,
         MI_C_3, MI_D_3, MI_G_3, MI_As_4, MI_C_5, MI_D_5,
-     TG_SWAP, MI_B_2, MI_E_3, MI_Cs_4, MI_Fs_3, MI_A_3, MI_C_4, MI_E_4,
+     MI_G_2, MI_B_2, MI_E_3, MI_Cs_4, MI_Fs_3, MI_A_3, MI_C_4, MI_E_4,
        MI_A_2, MI_F_3, MI_As_3, MI_Gs_3, MI_B_3, MI_D_4, MI_Gs_4, MI_B_4,
      MI_Gs_2, MI_As_2, MI_Ds_3, MI_F_4, MI_Ds_4, MI_Fs_4, MI_A_4, MI_Cs_5, MI_G_4
     ),
@@ -52,42 +81,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_CLOSE] = LAYOUT(
                   MI_Gs_1, MI_As_1, MI_Ds_2, MI_Ds_3, MI_G_3,
        MI_D_1, MI_D_2, MI_As_2, MI_C_3, MI_Cs_2, MI_C_2, MI_Fs_1,
-         MI_G_1, MI_G_2, MI_B_2, MI_D_3, MI_F_3, MI_Fs_2, TG_SWAP,
+         MI_G_1, MI_G_2, MI_B_2, MI_D_3, MI_F_3, MI_Fs_2, MI_B,
        MI_A_1, MI_E_2, MI_A_2, MI_Cs_3, MI_E_3, MI_Gs_2, MI_B_1,
      MI_E_1, MI_E_2, MI_Fs_3, MI_Gs_3, MI_B_3, MI_F_2, MI_Cs_1, MI_F_1,
 
      MO_SWAP,    MI_A_5, MI_Gs_5, MI_Fs_5, MI_F_5,     FN_MUTE,
          MI_C_3, MI_G_5, MI_As_4, MI_C_5, MI_Ds_5, KC_VOLD, KC_VOLU,
         MI_D_3, MI_Cs_3, MI_Gs_3, MI_As_3, MI_C_4, MI_D_5,
-     TG_SWAP, MI_B_2, MI_Fs_3, MI_Fs_4, MI_G_3, MI_B_3, MI_D_4, MI_G_4,
+     MI_G_2, MI_B_2, MI_Fs_3, MI_Fs_4, MI_G_3, MI_B_3, MI_D_4, MI_G_4,
        MI_A_2, MI_F_3, MI_E_3, MI_A_3, MI_Cs_4, MI_E_4, MI_A_4, MI_Cs_5,
      MI_Gs_2, MI_As_2, MI_Ds_3, MI_F_4, MI_E_4, MI_Gs_4, MI_B_4, MI_E_5, MI_Ds_4
     ),
 
-    [_MISC] = LAYOUT(
-                  _______, _______, _______, _______, _______,
+    [_QWERTY] = LAYOUT_wrapper(
+                   _________________NUMBER_L__________________,
+       KC_GESC, KC_TAB, _________________QWERTY_L1_________________,
+          KC_CAPS, _________________QWERTY_L2_________________, KC_H,
+       KC_LSFT, _________________QWERTY_L3_________________, KC_N,
+     KC_LCTL, KC_GRV, KC_LGUI, KC_LALT, MIS_EIS, KC_SPC, KC_SPC, KC_PSCR,
+
+     MO(_MISC),        KC_LBRC, KC_RBRC, KC_BSLS, KC_0,         _______,
+                  KC_6, KC_7, KC_8, KC_9, KC_BSPC, _______, _______,
+                _________________QWERTY_R1_________________, KC_DEL,
+     KC_PGUP, KC_G, _________________QWERTY_R2_________________, KC_ENT,
+            KC_PGDN, KC_B, _________________QWERTY_R3_________________, KC_RSFT,
+         KC_HOME, KC_END, KC_SPC, MIS_KAN, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    ),
+
+    [_COLEMAK] = LAYOUT_wrapper(
+                   _________________NUMBER_L__________________,
+       KC_GESC, KC_TAB, _________________COLEMAK_L1________________,
+          KC_LCTL, _________________COLEMAK_L2________________, KC_ENT,
+       KC_LSFT, _________________COLEMAK_L3________________, KC_M,
+     KC_CAPS, KC_GRV, KC_LGUI, KC_LALT, MIS_EIS, KC_SPC, KC_SPC, KC_PSCR,
+
+     MO(_MISC),        KC_LBRC, KC_RBRC, KC_BSLS, KC_0,         _______,
+                  KC_6, KC_7, KC_8, KC_9, KC_BSPC, _______, _______,
+                _________________COLEMAK_R1________________, KC_DEL,
+     KC_PGUP, KC_D, _________________COLEMAK_R2________________, KC_ENT,
+            KC_PGDN, KC_B, _________________COLEMAK_R3________________, KC_RSFT,
+         KC_HOME, KC_END, KC_SPC, MIS_KAN, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    ),
+
+    [_MISC] = LAYOUT_wrapper(
+                    _________________FUNC__L___________________,
        _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, _______, _______, _______, _______,
        _______, _______, _______, _______, _______, _______, _______,
      _______, _______, _______, _______, _______, _______, _______, _______,
 
-     _______,        _______, _______, _______, _______,         _______,
-                  _______, _______, _______, _______, _______, _______, _______,
+     _______,        _______, _______, _______, KC_F10,         _______,
+                  KC_F6, KC_F7, KC_F8, KC_F9, _______, _______, _______,
                 _______, _______, _______, _______, _______, _______,
-     _______, _______, _______, _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, _______, KC_QUOT, _______,
             _______, _______, _______, _______, _______, _______, _______, _______,
          _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     [_FN] = LAYOUT(
                    DF_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, DF_QWER, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+          DF_COLE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 
      _______,        MI_OCTD, MI_OCTU, MI_VELD, MI_VELU,         _______,
-                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_RMOD, RGB_MOD,
+                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  RGB_RMOD, RGB_MOD,
                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, VERSION,
      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
             RGB_SAD, RGB_SAI, RGB_HUD, RGB_HUI, RGB_SPD, RGB_SPI, RGB_VAD, RGB_VAI,
@@ -118,6 +177,9 @@ void eeconfig_init_user(void) {  // EEPROM is getting reset!
 
 void keyboard_post_init_user(void) {
     my_init();
+
+    //  party mode (for LED soldering test. Enable rainbow color effect, and disable led_indicator to check all LEDs)
+    rgb_matrix_mode(RGB_MATRIX_RAINBOW_MOVING_CHEVRON);
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -144,9 +206,18 @@ void rgb_matrix_indicators_user(void) {
                 break;
 
             case _FN:
-                // rgb_matrix_set_color(pgm_read_byte(&convert_led_location2number[11]),  RGB_RED);         //  RGB_TOG  <- too heavy.
-                rgb_matrix_set_color(14, RGB_DARKYELLOW);      //  MI_OCTD
-                rgb_matrix_set_color(25, RGB_DARKGREEN);       //  MI_OCTU
+                // left hand side
+                rgb_matrix_set_color( 46, RGB_DARKORANGE);     //  DF_OPEN
+                rgb_matrix_set_color( 48, RGB_DARKWHITE);      //  DF_QWER
+                rgb_matrix_set_color( 60, RGB_DARKWHITE);      //  DF_COLE
+
+                rgb_matrix_set_color( 73, RGB_DARKYELLOW);     //  AG_SWAP
+                rgb_matrix_set_color( 72, RGB_DARKYELLOW);     //  AG_NORM
+
+
+                // right hand side
+                rgb_matrix_set_color( 1, RGB_DARKYELLOW);      //  MI_OCTD
+                rgb_matrix_set_color(13, RGB_DARKGREEN);       //  MI_OCTU
                 rgb_matrix_set_color(14, RGB_DARKYELLOW);      //  MI_VELD
                 rgb_matrix_set_color(25, RGB_DARKGREEN);       //  MI_VELU
 
