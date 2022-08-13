1627f8ded9a9790f41e409f55079fb326ac7e0c9
HorrorTroll <sonicvipduc@gmail.com>
2022-08-13 17:19:51 +0700

    Update LED custom effect and refactor keymap a bit

--- keyboards/horrortroll/handwired_k552/keymaps/via/led/flower_blooming/flower_blooming.c
+++ keyboards/horrortroll/handwired_k552/keymaps/via/led/flower_blooming/flower_blooming.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 HorrorTroll <https://github.com/HorrorTroll>
+/* Copyright 2022 HorrorTroll <https://github.com/HorrorTroll>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,9 +14,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-static HSV DIAGONAL_math(HSV hsv, uint8_t i, uint8_t time) {
-    hsv.h = g_led_config.point[i].x - g_led_config.point[i].y - time;
+#include "led/flower_blooming/flower_blooming.h"
+
+static HSV FLOWER_BLOOMING_math(HSV hsv, uint8_t i, uint8_t time) {
+    if (g_led_config.point[i].y > k_rgb_matrix_center.y)
+        hsv.h = g_led_config.point[i].x * 3 - g_led_config.point[i].y * 3 + time;
+    else
+        hsv.h = g_led_config.point[i].x * 3 - g_led_config.point[i].y * 3 - time;
     return hsv;
 }
 
-bool DIAGONAL(effect_params_t* params) { return effect_runner_i(params, &DIAGONAL_math); }
+bool FLOWER_BLOOMING(effect_params_t* params) { return effect_runner_bloom(params, &FLOWER_BLOOMING_math); }
