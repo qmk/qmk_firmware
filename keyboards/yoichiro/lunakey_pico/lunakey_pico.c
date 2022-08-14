09a9c0a4a7b5751e20db904972cd6aaeed0752ee
Yoichiro Tanaka <yoichiro6642@gmail.com>
2022-08-15 07:29:02 +0900

    Add a new keyboard `Lunakey Pico`.

---
+++ keyboards/yoichiro/lunakey_pico/lunakey_pico.c
@@ -0,0 +1,12 @@
+// Copyright 2022 Yoichiro Tanaka (@yoichiro)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "lunakey_pico.h"
+
+//void keyboard_post_init_kb(void) {
+//    debug_enable   = true;
+//    debug_matrix   = true;
+//    debug_keyboard = true;
+//    debug_mouse    = true;
+//    keyboard_post_init_user();
+//}
---
+++ keyboards/yoichiro/lunakey_pico/mcuconf.h
@@ -0,0 +1,16 @@
+// Copyright 2022 Yoichiro Tanaka (@yoichiro)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef RP_SIO_USE_UART0
+#define RP_SIO_USE_UART0 TRUE
+#undef RP_SIO_USE_UART1
+#define RP_SIO_USE_UART1 FALSE
+
+//#undef RP_SERIAL_USE_UART0
+//#define RP_SERIAL_USE_UART0 TRUE
+//#undef RP_SERIAL_USE_UART1
+//#define RP_SERIAL_USE_UART1 FALSE
