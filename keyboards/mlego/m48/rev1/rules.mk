9fe041e14c74a0f26ecb0fd674834ebffebbdbbf
alin m elena <alinm.elena@gmail.com>
2022-08-15 09:19:13 +0100

    update

--- keyboards/mlego/m48/rev1/rules.mk
+++ keyboards/mlego/m48/rev1/rules.mk
@@ -1,8 +1,2 @@
-# MCU name
-MCU = STM32F401
-BOARD = BLACKPILL_STM32_F401
-
-# Bootloader selection
-BOOTLOADER = tinyuf2
 MCU_LDSCRIPT = STM32F401xE
 EEPROM_DRIVER = vendor
--- keyboards/mlego/m65/rev3/rules.mk
+++ keyboards/mlego/m65/rev3/rules.mk
@@ -1,9 +1,2 @@
-# MCU name
-MCU = STM32F401
-BOARD = BLACKPILL_STM32_F401
-
-# Bootloader selection
-BOOTLOADER = tinyuf2
-#BOOTLOADER = stm32-dfu
 MCU_LDSCRIPT = STM32F401xE
 EEPROM_DRIVER = vendor
