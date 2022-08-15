cb4b26a12cadd7577b53fac0ccb1aa7431efb5ee
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-08-15 10:37:26 +0700

    Update keyboards/mechlovin/zed65/wearhaus66/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechlovin/zed65/wearhaus66/keymaps/via/keymap.c
+++ keyboards/mechlovin/zed65/wearhaus66/keymaps/via/keymap.c
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum my_keycodes {
   LOGO_LED_ON = USER00,
-  LOGO_LED_OFF = USER01
+  LOGO_LED_OFF
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
