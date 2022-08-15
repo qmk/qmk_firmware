37fb72f97f778f6c2ec4ac67a67ba3cdf3226606
Huy Ta <huytbt@gmail.com>
2022-08-15 22:33:44 +0700

    Fix typo

--- keyboards/huytbt/h47/h47.h
+++ keyboards/huytbt/h47/h47.h
@@ -4,7 +4,7 @@
 
 #define XXX KC_NO
 
-/* JD45 keymap definition macro
+/* H47 keymap definition macro
  */
 #define LAYOUT( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, \
