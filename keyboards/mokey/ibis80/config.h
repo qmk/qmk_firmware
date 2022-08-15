18ccb94adc27dd4cae0cf961b7549cc2cacd674a
Runheme <95535123+Runheme@users.noreply.github.com>
2022-08-15 09:27:42 +0800

    Update keyboards/mokey/ibis80/config.h
    /*
    Copyright 2021 Kyle McCreery
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 2 of the License, or
    (at your option) any later version.
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
    */
    
    #pragma once
    
    #include "config_common.h"
    
    /* USB Device descriptor parameter */
    #define VENDOR_ID       0x6653
    #define PRODUCT_ID      0x3380
    #define DEVICE_VER      0x0001
    #define MANUFACTURER    Mokey
    #define PRODUCT         IBIS 80
    /* key matrix size */
    #define MATRIX_ROWS 6
    #define MATRIX_COLS 17
    
    #define MATRIX_ROW_PINS { B0, B1, B2, E6, F0, F1 }
    #define MATRIX_COL_PINS { D0, D1, D2, D3, D5, D4, D6, D7, B4, B5, B6, C6, C7, F7, F6, F5, F4 }
    #define UNUSED_PINS
    
    /* COL2ROW, ROW2COL*/
    #define DIODE_DIRECTION COL2ROW
    
    /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
    #define DEBOUNCE 5
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/mokey/ibis80/config.h
+++ keyboards/mokey/ibis80/config.h
@@ -16,13 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x6653
-#define PRODUCT_ID      0x3380
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Mokey
-#define PRODUCT         IBIS 80
-
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 17
