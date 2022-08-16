fef3f57a3e91a75c3a8af4336e66aae51400a37d
JX <572673807@qq.com>
2022-08-16 12:04:22 +0800

    Update keyboards/yandrstudio/nz67v2/config.h
    Delete the duplicate definition of info.json
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/yandrstudio/nz67v2/config.h
+++ keyboards/yandrstudio/nz67v2/config.h
@@ -18,13 +18,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xAA96
-#define PRODUCT_ID      0xAA83
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Y&R
-#define PRODUCT         nz67v2
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
