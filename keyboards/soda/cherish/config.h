e5463eee3d997399840dc72b796feadb6313ce29
ZhaoYou <gezhaoyou@126.com>
2022-08-16 09:26:44 +0800

    Update keyboard ‘soda/cherish’
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/soda/cherish/config.h
+++ keyboards/soda/cherish/config.h
@@ -17,12 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xEB50 // AP for Cherish-75
-#define PRODUCT_ID      0xEB52 // AR for Cherish-75
-#define DEVICE_VER      0x0001 // Revision pre-Alpha
-#define MANUFACTURER    gezhaoyou
-#define PRODUCT         Cherish-75
 
 /* key matrix size */
 #define MATRIX_ROWS 6
@@ -69,7 +63,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN A13
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
 #define RGBLED_NUM 1
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
