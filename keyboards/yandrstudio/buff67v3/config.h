f558e446f015e2b3f111115cef9b71edc547b29c
JX <572673807@qq.com>
2022-08-16 12:05:18 +0800

    Update keyboards/yandrstudio/buff67v3/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/yandrstudio/buff67v3/config.h
+++ keyboards/yandrstudio/buff67v3/config.h
@@ -17,13 +17,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xAA96
-#define PRODUCT_ID      0xAA88
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    BUFF_Y&R
-#define PRODUCT         buff67v3
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
