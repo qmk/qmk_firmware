375a63caa65094ef58fd31a86b0a9f08debc23cc
CMMS-Freather <72902384+CMMS-Freather@users.noreply.github.com>
2022-08-17 01:15:30 -0700

    Update keyboards/cmm_studio/fuji75_hotswap/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/cmm_studio/fuji75_hotswap/config.h
+++ keyboards/cmm_studio/fuji75_hotswap/config.h
@@ -18,12 +18,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x434D
-#define PRODUCT_ID      0x4638
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    CMM.Studio
-#define PRODUCT         Fuji75_Hotswap
 
 /* key matrix size */
 #define MATRIX_ROWS 6
