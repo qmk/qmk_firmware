f870e2cd072f30f86dec36cfb8cb635532379d6b
AliceH66 <105761645+AliceH66@users.noreply.github.com>
2022-08-17 14:52:46 -0700

    Vial files
    Files removed for QMK.

--- keyboards/aliceh66/pianoforte/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x37, 0xFC, 0xE4, 0xB0, 0x9B, 0xC6, 0x1A, 0xD8}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
--- keyboards/aliceh66/pianoforte_hs/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xF3, 0xC1, 0x73, 0xB5, 0x52, 0xD4, 0x0E, 0x99}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 6 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
