06f6d512d5a1e2dae8a8380e075539f107a4bcb9
CMMS-Freather <72902384+CMMS-Freather@users.noreply.github.com>
2022-08-17 01:14:36 -0700

    Update keyboards/keyspensory/kp60/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keyspensory/kp60/config.h
+++ keyboards/keyspensory/kp60/config.h
@@ -5,12 +5,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x4B50
-#define PRODUCT_ID      0x0060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Keyspensory
-#define PRODUCT         KP60
 
 /* key matrix size */
 #define MATRIX_ROWS 5
