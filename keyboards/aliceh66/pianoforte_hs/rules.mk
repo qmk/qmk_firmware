3d1280d497707885bf0cd6d82d25e444edba8e42
AliceH66 <105761645+AliceH66@users.noreply.github.com>
2022-08-17 17:04:52 -0400

    Update keyboards/aliceh66/pianoforte_hs/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/aliceh66/pianoforte_hs/rules.mk
+++ keyboards/aliceh66/pianoforte_hs/rules.mk
@@ -20,4 +20,3 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-QMK_SETTINGS = no 			# QMK Settings
