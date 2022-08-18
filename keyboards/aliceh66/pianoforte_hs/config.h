fb098de0b10fc6251a338ff8a89ebc36b03e1ac8
AliceH66 <105761645+AliceH66@users.noreply.github.com>
2022-08-18 01:12:32 -0400

    Update keyboards/aliceh66/pianoforte_hs/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/aliceh66/pianoforte_hs/config.h
+++ keyboards/aliceh66/pianoforte_hs/config.h
@@ -30,9 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE    5
-#define VIAL_KEYBOARD_UID {0xF3, 0xC1, 0x73, 0xB5, 0x52, 0xD4, 0x0E, 0x99}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 6 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
 #define DYNAMIC_KEYMAP_LAYER_COUNT 2
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
