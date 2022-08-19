a2f99ce79b6d5a15ba197dd3ba7b67b64f1bc0bc
Joe Scotto <contact@joescotto.com>
2022-08-19 00:14:37 -0400

    Add license to keymap

--- keyboards/crkbd/keymaps/joe_scotto/config.h
+++ keyboards/crkbd/keymaps/joe_scotto/config.h
@@ -1,6 +1,5 @@
 /*
-Copyright 2019 @foostan
-Copyright 2020 Drashna Jaelre <@drashna>
+Copyright 2022 Joe Scotto
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
