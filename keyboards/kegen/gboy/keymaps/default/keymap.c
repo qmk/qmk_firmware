86fd9ea58a919f6bb3168477beade4f2f7799f5a
Matt Chan <5021+mattchan@users.noreply.github.com>
2022-08-18 21:04:20 -0400

    Update keyboards/kegen/gboy/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kegen/gboy/keymaps/default/keymap.c
+++ keyboards/kegen/gboy/keymaps/default/keymap.c
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "gboy.h"
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
