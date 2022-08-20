2a64a2cf0187c4dd27b883467bcc81fe77eaa42b
nendezkombet <82454371+nendezkombet@users.noreply.github.com>
2022-08-20 15:55:56 +0700

    Update keyboards/keebformom/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keebformom/config.h
+++ keyboards/keebformom/config.h
@@ -36,7 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* KFM PCB default pin-out */
 #define MATRIX_ROW_PINS { F5, F6, F7, B1 }
 #define MATRIX_COL_PINS { B3, B2, D1, D0, D4, C6, D7, E6, B4, B5 }
-#define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
