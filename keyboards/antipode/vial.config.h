78edeca83c9789b9db6758d5fe73af588060e81f
arnstadm <arnstad@gmail.com>
2022-08-20 21:50:41 +0200

    add antipode/vero

---
+++ keyboards/antipode/vial.config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x04, 0x5B, 0xCC, 0x19, 0xAC, 0x25, 0x19, 0x9E}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
