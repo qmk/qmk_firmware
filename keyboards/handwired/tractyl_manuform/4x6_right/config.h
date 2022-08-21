b0eda7701a372e820264c6a78afa1f8c410845f7
Ryan <fauxpark@gmail.com>
2022-08-22 08:08:50 +1000

    Move keyboard USB IDs and strings to data driven, pass 3 (#18111)

--- keyboards/handwired/tractyl_manuform/4x6_right/config.h
+++ keyboards/handwired/tractyl_manuform/4x6_right/config.h
@@ -20,10 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define PRODUCT_ID  0x3537
-#define DEVICE_VER  0x0001
-#define PRODUCT     Tractyl Manuform(4x6)
-
 /* key matrix size */
 // Rows are doubled-up
 #define MATRIX_ROWS 10
