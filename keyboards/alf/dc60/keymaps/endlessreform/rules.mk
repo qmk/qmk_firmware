e12b1a8cec7d7dfffd4a61160a4ffc37a5272bca
jkeisling <jlowellkeisling@gmail.com>
2022-08-21 10:50:26 -0500

    Add OS overview combo

--- keyboards/alf/dc60/keymaps/endlessreform/rules.mk
+++ keyboards/alf/dc60/keymaps/endlessreform/rules.mk
@@ -24,3 +24,5 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+VPATH += keyboards/gboards/
