b77b5225d238f261a1b6490ec59a25b0e0bdb226
TDC <64564678+kthorpe88@users.noreply.github.com>
2022-08-21 13:07:10 -0600

    Update keymap.c

--- keyboards/tdcleft104/keymaps/default/keymap.c
+++ keyboards/tdcleft104/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    	    /* LAYOUT_tdcleft104 0: Default LAYOUT_tdcleft104
+    	    /* LAYOUT0: Default LAYOUT
      * |---------------------------------------------------------------------------------------------------------
      * |Esc |Del |Calc|Nlck|----|Pscr|----|F1  |F2  |F3  |F4  |F5  |F6  |F7  |F8  |F9  |F10 |F11 |F12 |Slck|_M_P|
      * |---------------------------------------------------------------------------------------------------------
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------------------------------------------------
      */
     //default
-    [0] = LAYOUT_tdcleft104(
+    [0] = LAYOUT(
 	    KC_ESC,  KC_DEL,   KC_CALC,  KC_NLCK,      KC_PSCR,            KC_F1,   KC_F2,  KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_SLCK, RGB_M_P, 
 	    KC_BSPC, KC_PSLS,  KC_PAST,  KC_PMNS,      KC_GRV,    KC_1,    KC_2,    KC_3,   KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,    KC_0,   KC_MINS, KC_EQL,           KC_BSPC, RGB_TOG, 
 	    KC_P7,   KC_P8,    KC_P9,    KC_PPLS,      KC_TAB,             KC_Q,    KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, RGB_MOD, 
