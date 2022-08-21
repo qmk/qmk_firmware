eb085608e80108916d45e9087cfb5329241c9a40
Claes Rytlig <cry@cvation.com>
2022-08-21 16:29:30 +0200

    Changed Mod keys

--- keyboards/idobao/id75/keymaps/crytlig75/keymap.c
+++ keyboards/idobao/id75/keymaps/crytlig75/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_BSPC, KC_INS,
         TG(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SLSH, KC_H,     KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,  KC_ENT,  KC_RSFT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SCLN, KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_MINS, S_NO,    KC_HOME, KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  KC_LALT,  MO(2),   KC_RGUI, KC_RALT, KC_RCTL, TG(1),   MO(1),   KC_PSCR
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  KC_LALT,  MO(2),   KC_RGUI, KC_RALT, KC_RCTL, MO(1),   TG(1),   KC_PSCR
     ),
 
     [1] = LAYOUT_ortho_5x15(
