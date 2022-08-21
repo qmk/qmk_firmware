6fdfaa3be1bff55c7cb23e3b599c75c26dc7fdb4
honorless <86894501+lesshonor@users.noreply.github.com>
2022-08-21 14:16:29 -0400

    refactor: migrate rev2 to data-driven cfg (mostly)

--- keyboards/mechwild/bde/rev2/config.h
+++ keyboards/mechwild/bde/rev2/config.h
@@ -17,14 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 7
-
-/* Key matrix pins */
-#define MATRIX_ROW_PINS { C6, D7, B4, D4, E6, B2 }
-#define MATRIX_COL_PINS { B3, B1, F7, F6, F5, F4, B5 }
-
 /* Encoder pins */
 #define ENCODERS_PAD_A { D3 }
 #define ENCODERS_PAD_B { D2 }
@@ -32,14 +24,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Encoder resolution */
 #define ENCODER_RESOLUTION 4
 
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
 #define OLED_FONT_H "keyboards/mechwild/bde/lib/rev2.c"
-
-#define RGB_DI_PIN B6
-#ifdef RGB_DI_PIN
-#    define RGBLED_NUM 8
-/*== Chosen enabled animations ==*/
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#endif
