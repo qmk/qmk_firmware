b5cf56948c04683250dd18935ace9c484d013da3
Drashna Jael're <drashna@live.com>
2022-08-22 16:46:01 -0700

    [Keyboard] Add Work Louder revisions and updated keymaps

--- keyboards/work_louder/nano/keymaps/via/keymap.c
+++ keyboards/work_louder/nano/keymaps/via/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_PGDN, KC_PGDN) },
+    [0] = { ENCODER_CCW_CW(C(KC_Z), C(KC_Y)) },
     [1] = { ENCODER_CCW_CW(_______, _______) },
     [2] = { ENCODER_CCW_CW(_______, _______) },
     [3] = { ENCODER_CCW_CW(_______, _______) },
