9e7a924a71e22a5ccc18c92f26c4dbf053709eab
Cameron Larsen <camjlarsen@gmail.com>
2022-08-22 00:11:00 -0700

    Added keymap for corne

---
+++ keyboards/crkbd/keymaps/cameronjlarsen/rules.mk
@@ -0,0 +1,17 @@
+SRC += features/caps_word.c
+SRC += features/oneshot.c
+
+BOOTMAGIC_ENABLE = no
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+RGBLIGHT_ENABLE = no    # Enable keyboard RGB underglow
+COMMAND_ENABLE = no 		# Disables the command feature
+COMBO_ENABLE = yes
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+AUDIO_ENABLE = no
+MIDI_ENABLE = no
+BLUETOOTH_ENABLE = no
+BACKLIGHT_ENABLE = no
+
+LAYOUTS = split_3x5_3 split_3x6_3
