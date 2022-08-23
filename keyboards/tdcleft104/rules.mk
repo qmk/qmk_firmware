847bbce8c013e857e00c953f3b57a47aaa4f1be3
TDC <64564678+kthorpe88@users.noreply.github.com>
2022-08-23 07:43:30 -0600

    Update rules.mk

--- keyboards/tdcleft104/rules.mk
+++ keyboards/tdcleft104/rules.mk
@@ -5,11 +5,14 @@ MCU = STM32F072
 BOOTLOADER = stm32-dfu
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-AUDIO_ENABLE = no           # Audio output
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
