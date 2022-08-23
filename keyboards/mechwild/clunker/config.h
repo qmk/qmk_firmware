47f501325ee65a3d7106e2b7f91f0bc5a45c3391
Kyle McCreery <mccreery.kyle@gmail.com>
2022-08-23 00:53:00 -0400

    Built to use the info.json file for primary info

--- keyboards/mechwild/clunker/config.h
+++ keyboards/mechwild/clunker/config.h
@@ -3,46 +3,24 @@
 
 #pragma once
 
-#include "config_common.h"
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x6D77 // mw = "MechWild"
-#define PRODUCT_ID      0x1711
-#define DEVICE_VER      0x0103
-#define MANUFACTURER    MechWild
-#define PRODUCT         Clunker
+/* disable debug print */
+//#define NO_DEBUG
 
-/* Key matrix size */
-#define MATRIX_ROWS 7
-#define MATRIX_COLS 8
+/* disable print */
+//#define NO_PRINT
 
-/* Key matrix pins */
-#define MATRIX_ROW_PINS { F5, F6, B3, F7, B2, B1, B6 }
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4, B5 }
-#define UNUSED_PINS
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
 
-/* Encoder pins */
-#define ENCODERS_PAD_A { D2 }
-#define ENCODERS_PAD_B { D3 }
-
-/* Encoder resolution */
+#define ENCODERS_PAD_A { B4 }
+#define ENCODERS_PAD_B { B5 }
 #define ENCODER_RESOLUTION 4
-#define TAP_CODE_DELAY 10
-
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* Bootmagic Lite key configuration */
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
 
 #define SOLENOID_PIN F4
