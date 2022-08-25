a1f65d930206b43421edef7933d00759e90c4445
Ben Allen <25768863+BenResTech@users.noreply.github.com>
2022-08-25 12:54:15 +0100

    Adding current iteration of configs
    Per-layer RGB now working :)

---
+++ keyboards/crkbd/keymaps/benrestech/rules.mk
@@ -0,0 +1,12 @@
+
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+EXTRAKEY_ENABLE = yes     # Audio control and System control
+LTO_ENABLE      = yes     # Enable link time optimisation
+MOUSEKEY_ENABLE = yes     # Mouse keys
+NKRO_ENABLE     = yes     # Enable N-Key Rollover
+RGBLIGHT_ENABLE = yes     # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes     # Enable OLED
+OLED_DRIVER     = SSD1306 # Define driver for OLED
