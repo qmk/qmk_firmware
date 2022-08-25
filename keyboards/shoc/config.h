b95fb8e05e25924d32e0101adc54b578e1908472
Jannik Becker <89019698+JannikB5825@users.noreply.github.com>
2022-08-25 11:00:43 +0200

    Update config.h

--- keyboards/shoc/config.h
+++ keyboards/shoc/config.h
@@ -22,13 +22,8 @@
 
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 8
-
-/* key matrix pins */
 #define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3, B6, B2 }
 #define MATRIX_COL_PINS { D3, D2, D4, C6, D7, E6, B4, B5 }
-#define UNUSED_PINS
-
-/* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
 #define DEBOUNCE 5
