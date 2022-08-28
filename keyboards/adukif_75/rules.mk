caf13ef72c5e9942c52034240827f47584964979
Luka <57495732+lukaaduashvili@users.noreply.github.com>
2022-08-28 19:25:02 +0400

    Update keyboards/adukif_75/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/adukif_75/rules.mk
+++ keyboards/adukif_75/rules.mk
@@ -10,11 +10,12 @@ BOOTLOADER = stm32duino
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-NKRO_ENABLE = yes
-
-DEFAULT_FOLDER = adukif_75
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
 
 LAYOUTS = 75_ansi
 
