ea841ae349bd0ee2424cb854c506de5e2bb08208
jack <0x6A73@pm.me>
2022-08-29 01:08:59 -0600

    lincense & pass lint

--- keyboards/boardsource/lulu/avr/config.h
+++ keyboards/boardsource/lulu/avr/config.h
@@ -1,2 +1,4 @@
+// Copyright 2022 jack (@waffle87)
+// SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 #define RGB_DI_PIN D3
--- keyboards/boardsource/lulu/rp2040/config.h
+++ keyboards/boardsource/lulu/rp2040/config.h
@@ -1,3 +1,5 @@
+// Copyright 2022 jack (@waffle87)
+// SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
@@ -5,7 +7,7 @@
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
 #define RGB_DI_PIN GP29
-#define RGBLED_NUM DRIVER_LED_TOTAL
+#define RGBLED_NUM 70
 #define I2C_DRIVER I2CD2
 #define I2C1_SDA_PIN GP22
 #define I2C1_SCL_PIN GP23
