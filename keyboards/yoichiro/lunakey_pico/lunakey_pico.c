0de92c90ec3d89a267249c075048dc876b91e855
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2022-08-30 06:45:12 +0900

    Update keyboards/yoichiro/lunakey_pico/lunakey_pico.c
    Remove unnecessary comment lines.
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/yoichiro/lunakey_pico/lunakey_pico.c
+++ keyboards/yoichiro/lunakey_pico/lunakey_pico.c
@@ -2,11 +2,3 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "lunakey_pico.h"
-
-//void keyboard_post_init_kb(void) {
-//    debug_enable   = true;
-//    debug_matrix   = true;
-//    debug_keyboard = true;
-//    debug_mouse    = true;
-//    keyboard_post_init_user();
-//}
