f20626420a2d1c52ce20da1f27322679f3a514e1
Alabastard-64 <96358682+Alabastard-64@users.noreply.github.com>
2022-08-30 00:52:20 -0600

    move keyrecord processing
    This fixes the issue of overridden keys being missed by mouse_key_processing

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -215,9 +215,6 @@ bool pre_process_record_quantum(keyrecord_t *record) {
 /* Get keycode, and then call keyboard function */
 void post_process_record_quantum(keyrecord_t *record) {
     uint16_t keycode = get_record_keycode(record, false);
-#if defined(POINTING_DEVICE_ENABLE) && defined(POINTING_DEVICE_AUTO_MOUSE_ENABLE)
-    process_auto_mouse(keycode, record);
-#endif
     post_process_record_kb(keycode, record);
 }
 
@@ -274,6 +271,9 @@ bool process_record_quantum(keyrecord_t *record) {
 #endif
 #if defined(VIA_ENABLE)
             process_record_via(keycode, record) &&
+#endif
+#if defined(POINTING_DEVICE_ENABLE) && defined(POINTING_DEVICE_AUTO_MOUSE_ENABLE)
+            process_auto_mouse(keycode, record) &&
 #endif
             process_record_kb(keycode, record) &&
 #if defined(SECURE_ENABLE)
