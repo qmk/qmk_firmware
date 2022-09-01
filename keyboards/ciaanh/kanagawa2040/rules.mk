a738485264815bab4c711b543bfecf38fd76b80e
Ciaanh <nicolas.fabre.21@outlook.fr>
2022-09-01 14:27:12 +0200

    Edit kanagawa 2040

--- keyboards/ciaanh/kanagawa2040/rules.mk
+++ keyboards/ciaanh/kanagawa2040/rules.mk
@@ -2,6 +2,7 @@
 MCU = RP2040
 # Bootloader selection
 BOOTLOADER = rp2040
+
 # Build Options
 #   change yes to no to disable
 #
@@ -14,13 +15,11 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-# AUDIO_DRIVER = pwm_software
+
 ENCODER_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 WS2812_DRIVER = vendor
 OLED_ENABLE = yes
-OLED_DRIVER = custom
-# Project specific files
-SRC += lib/ssd1306_sh1106.c
-QUANTUM_LIB_SRC += spi_master.c
+
+OPT_DEFS += -DHAL_USE_I2C=TRUE
