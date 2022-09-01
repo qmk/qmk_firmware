236ea89956e951b8d2b317b85f6b1fdabc602d94
Ciaanh <nicolas.fabre.21@outlook.fr>
2022-09-01 14:27:12 +0200

    Edit kanagawa 2040

--- keyboards/ciaanh/kanagawa2040/config.h
+++ keyboards/ciaanh/kanagawa2040/config.h
@@ -34,25 +34,12 @@
 // clang-format on
 
 /* OLED SPI Defines */
-#define OLED_DISPLAY_128X64
-#define OLED_IC OLED_IC_SH1106
+#define OLED_DISPLAY_128X32
 
-/* OLED SPI Pins */
-#define OLED_DC_PIN GP24
-#define OLED_CS_PIN GP22
-#define OLED_RST_PIN GP23
-
-/* Shift OLED columns by 2 pixels */
-#define OLED_COLUMN_OFFSET 2
-
-/* Divisor for OLED */
-#define OLED_SPI_DIVISOR 4
-
-/* ChibiOS SPI definitions */
-#define SPI_DRIVER SPID1
-#define SPI_SCK_PIN GP26
-#define SPI_MOSI_PIN GP27
-#define SPI_MISO_PIN GP28
+/* I2C Conf */
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP12
+#define I2C1_SCL_PIN GP13
 
 /* Encoders */
 #define ENCODERS_PAD_A { GP18 }
@@ -69,10 +56,6 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP13
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
-
-/* Audio (Unsupported for now)*/
-// #define AUDIO_PIN GP16
-// #define SPEAKER_SHUTDOWN GP14
 
 #ifdef RGB_MATRIX_ENABLE
 
