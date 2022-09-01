1487a11c33176b0d91e763f1469caa61e720dc4c
arnstadm <arnstad@gmail.com>
2022-09-01 17:31:27 +0200

    add vero

---
+++ keyboards/treasure30/rules.mk
@@ -0,0 +1,8 @@
+# This file intentionally left blank
+# MCU name
+
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
