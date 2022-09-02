ffa07869943818ef9571e48463921c0df0fab721
Andy Chiu <0836449@gmail.com>
2022-09-01 22:47:03 -0500

    Update keyboards/handwired/uc2/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/uc2/config.h
+++ keyboards/handwired/uc2/config.h
@@ -3,22 +3,6 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
 #include "config_common.h"
 
 
