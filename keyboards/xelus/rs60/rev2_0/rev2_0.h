699da6e0eb44417882ebf00871dfe2b227f89ce8
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2022-09-04 01:27:00 +1000

    [Keyboard] RS60 Rev2 change to eeprom emulation (#18201)
    by xelus

--- keyboards/xelus/rs60/rev2_0/rev2_0.h
+++ keyboards/xelus/rs60/rev2_0/rev2_0.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 Harrison Chan (Xelus)
+/* Copyright 2022 Harrison Chan (Xelus)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
