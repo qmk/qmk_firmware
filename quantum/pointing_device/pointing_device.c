41decc5262b8053bc8efb318204c962c7f7ed357
Drashna Jael're <drashna@live.com>
2022-09-03 11:18:16 -0700

    Fix handling

--- quantum/pointing_device/pointing_device.c
+++ quantum/pointing_device/pointing_device.c
@@ -471,7 +471,7 @@ __attribute__((weak)) report_mouse_t pointing_device_task_combined_user(report_m
 
 void pointing_device_keycode_handler(uint16_t keycode, bool pressed) {
     if IS_MOUSEKEY_BUTTON (keycode) {
-        local_mouse_report.buttons = pointing_device_handle_buttons(local_mouse_report.buttons, true, keycode - KC_MS_BTN1);
+        local_mouse_report.buttons = pointing_device_handle_buttons(local_mouse_report.buttons, pressed, keycode - KC_MS_BTN1);
         pointing_device_set_report(local_mouse_report);
         pointing_device_send();
     // } else if IS_MOUSEKEY_WHEEL (code) {
