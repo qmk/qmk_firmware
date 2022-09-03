fdbe833801fa7db14aa7c32f6ee453804366ab72
harshit goel <harshitgoel96@yahoo.com>
2022-09-03 13:15:44 +0530

    update keymap license

--- keyboards/pteron36/keymaps/default/keymap.c
+++ keyboards/pteron36/keymaps/default/keymap.c
@@ -1,6 +1,4 @@
  /* Copyright HarshitGoel96 2020
-  * With permission from mattdibi, the original maintainer of the Redox hardware.
-  * 
   * This program is free software: you can redistribute it and/or modify 
   * it under the terms of the GNU General Public License as published by 
   * the Free Software Foundation, either version 2 of the License, or 
