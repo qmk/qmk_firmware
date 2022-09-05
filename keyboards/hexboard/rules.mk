769f9a8793bb93605497e5b82ed06a3361d9f86f
Zach DeCook <zachdecook@librem.one>
2022-09-05 08:25:54 -0400

    hexboard: update qmk

--- keyboards/hexboard/rules.mk
+++ keyboards/hexboard/rules.mk
@@ -1,5 +1,8 @@
-# This file intentionally left blank
+# MCU name
+MCU = MKL26Z64
 USE_CHIBIOS_CONTRIB = yes
+
+BOOTLOADER = halfkay
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
