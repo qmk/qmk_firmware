5a28ef6264a0011f0f52cae206933a2c082c8f34
Vino Rodrigues <github@m.vinorodrigues.com>
2022-09-05 21:38:33 +1000

    use bitmask instead of bool array in specialk.c

--- keyboards/idobao/id61/keymaps/idobao/specialk.c
+++ keyboards/idobao/id61/keymaps/idobao/specialk.c
@@ -3,15 +3,26 @@
 
 #include "specialk.h"
 
-bool special_registered;
-bool delkey_registered;
-bool keycode_raised[15];
+bool delkey_registered = false;
+uint32_t __keycode_raised = 0;
+
+bool get_keycode_raised(uint8_t n) {
+    return __keycode_raised & (1 << n);
+}
+
+void set_keycode_raised(uint8_t n, bool b) {
+    if (b) {
+        __keycode_raised |= 1 << n;
+    } else {
+        __keycode_raised &= ~(1 << n);
+    }
+}
 
 bool ID61_process_special_k(uint16_t keycode, keyrecord_t *record, bool arrow_mode, uint8_t k_norm, uint8_t k_spcl, uint8_t k_altr) {
     bool is_raised = get_highest_layer(layer_state|default_layer_state) != 0;
 
     if (record->event.pressed) {
-        keycode_raised[keycode - USER00] = is_raised;  // save for key release event
+        set_keycode_raised(keycode - USER00, is_raised);  // save for key release event
         if (is_raised) {
             // *** Fn keyed ***
             if (arrow_mode) {
@@ -32,7 +43,7 @@ bool ID61_process_special_k(uint16_t keycode, keyrecord_t *record, bool arrow_mo
             }
         }
     } else {
-        if (keycode_raised[keycode - USER00]) {
+        if (get_keycode_raised(keycode - USER00)) {
             // *** Fn keyed ***
             if (arrow_mode) {
                 // alternate key
