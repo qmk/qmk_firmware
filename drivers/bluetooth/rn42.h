035fd4e82077038676b9886d3fa95b9d113d1f97
fauxpark <fauxpark@gmail.com>
2022-09-08 18:43:38 +1000

    RN42 driver: small cleanups

--- drivers/bluetooth/rn42.h
+++ drivers/bluetooth/rn42.h
@@ -22,4 +22,4 @@ void rn42_send_keyboard(report_keyboard_t *report);
 
 void rn42_send_mouse(report_mouse_t *report);
 
-void rn42_send_consumer(uint16_t data);
+void rn42_send_consumer(uint16_t usage);
