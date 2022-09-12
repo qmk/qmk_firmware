9af12224c427b84a73bb9c8648e75b6368397999
ysshen <hustshenyang@126.com>
2022-09-12 21:30:20 +0800

    Add miiiw blackio 98 keyboard.

--- keyboards/miiiw/blackio98/keymaps/default/keymap.c
+++ keyboards/miiiw/blackio98/keymaps/default/keymap.c
@@ -16,77 +16,130 @@
 
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    [0] = LAYOUT(
-        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,          KC_DEL,  KC_INS,  KC_PGDN, KC_PGUP,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,         KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         KC_P7,   KC_P8,   KC_P9,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,          KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,     KC_UP,       KC_P1,   KC_P2,   KC_P3,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, MO(1),       KC_LEFT, KC_DOWN, KC_RGHT,     KC_P0,   KC_PDOT, KC_PENT
-    ),
-
-    [1] = LAYOUT(
-        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,           KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, BL_TOGG, BL_DEC,  BL_INC,           BL_STEP,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
-    )
-
+// Defines names for use in layer keycodes and the keymap
+enum keymap_layers {
+    WIN_BL,
+    WIN_FL,
+    MAC_BL,
+    MAC_FL,
 };
 
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap WIN_BL: Win Base Layer (Default Layer)
+   */
+    [WIN_BL] = LAYOUT(
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,  KC_INS,  KC_PGUP, KC_PGDN,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_P7,   KC_P8,   KC_P9,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,             KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,      KC_UP,         KC_P1,   KC_P2,   KC_P3,
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    KC_RALT, MO(WIN_FL), KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT,       KC_P0,   KC_PDOT, KC_PENT
+    ),
 
-//以下是RGB灯作为层切换指示
-const rgblight_segment_t PROGMEM my_scroll_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-	{1, 1, HSV_CYAN}
-);
+  /* Keymap WIN_FL: Win Function Layer
+   */
+    [WIN_FL] = LAYOUT(
+        RGB_RST,          KC_BRID, KC_BRIU, ALT_TAB, G(KC_D), KC_WBAK, KC_WSCH, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,   _______, KC_PSCR, KC_HOME, KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,    _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      RGB_VAI,       _______, _______, _______,
+        _______, _______, _______,                   _______,                   _______, _______, _______,     RGB_RMOD, RGB_VAD, RGB_MOD,       _______, _______, QK_BOOT
+    ),
 
-const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-	{2, 1, HSV_MAGENTA},
-	{56, 1, HSV_CYAN}
-);
+  /* Keymap MAC_BL: Mac Base Layer
+   */
+    [MAC_BL] = LAYOUT(
+        KC_ESC,           KC_BRID, KC_BRIU,C(KC_UP),C(KC_DOWN),KC_F11,G(KC_SPC),KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,   KC_DEL,  KC_INS,  KC_PGUP, KC_PGDN,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_P7,   KC_P8,   KC_P9,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,             KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,      KC_UP,         KC_P1,   KC_P2,   KC_P3,
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    KC_RGUI, MO(MAC_FL), KC_RALT,   KC_LEFT, KC_DOWN, KC_RGHT,       KC_P0,   KC_PDOT, KC_PENT
+    ),
 
+  /* Keymap MAC_FL: Mac Function Layer
+   */
+    [MAC_FL] = LAYOUT(
+        RGB_RST,          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, KC_PSCR, KC_HOME, KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,    _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      RGB_VAI,       _______, _______, _______,
+        _______, _______, _______,                   _______,                   _______, _______, _______,     RGB_RMOD, RGB_VAD, RGB_MOD,       _______, _______, QK_BOOT
+    ),
+};
 
-const rgblight_segment_t PROGMEM my_bluetooth_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-	{3, 1, HSV_WHITE}
-);
+// Implement Super-alt↯tab
+// See https://docs.qmk.fm/#/feature_macros?id=super-alt↯tab
+static bool is_alt_tab_active = false;
+static uint32_t alt_tab_timer = 0;
 
-const rgblight_segment_t PROGMEM my_numlocak_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-	{4, 1, HSV_GREEN},
-	{35, 1, HSV_RED}
-);
-
-// Define the array of layers. Later layers take precedence
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-	my_scroll_layer,
-	my_capslock_layer,
-	my_bluetooth_layer,
-	my_numlocak_layer
-);
-
-void keyboard_post_init_user(void){
-	//Enable the LED layers
-	rgblight_layers = my_rgb_layers;
+#ifdef DIP_SWITCH_ENABLE
+bool dip_switch_update_user(uint8_t index, bool active) {
+    switch (index) {
+    case 2:
+        default_layer_set(1UL << (active ? WIN_BL : MAC_BL));
+        break;
+   }
+   return true;
 }
+#endif
 
-// Enabling and disabling lighting layers
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Both layers will light up if both kb layers are active
-    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
-    rgblight_set_layer_state(3, layer_state_cmp(state, 3));
-
-    return state;
-}
-
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(0, led_state.scroll_lock);
-    rgblight_set_layer_state(1, led_state.caps_lock);
-    rgblight_set_layer_state(3, led_state.num_lock);
+// Processing all the key pressed.
+// Alt+tab.
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint8_t shift_mods = get_mods() & MOD_MASK_SHIFT;
+    switch (keycode) { // This will do most of the grunt work with the keycodes.
+    case ALT_TAB:
+        if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+                is_alt_tab_active = true;
+                register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read32();
+            register_code(KC_TAB);
+        } else {
+            unregister_code(KC_TAB);
+        }
+        return false;
+    case RGB_RST:
+        if (record->event.pressed) {
+            eeconfig_update_rgb_matrix_default();
+        }
+        return false;
+    case RGB_VAI:
+        if(shift_mods) {
+            if (record->event.pressed) rgb_matrix_increase_speed();
+            return false;
+        }
+        break;
+    case RGB_VAD:
+        if(shift_mods) {
+            if (record->event.pressed && rgb_matrix_get_speed() >= RGB_MATRIX_SPD_STEP) rgb_matrix_decrease_speed();
+            return false;
+        }
+        break;
+    case RGB_MOD:
+        if(shift_mods) {
+            if (record->event.pressed) rgb_matrix_increase_hue();
+            return false;
+        }
+        break;
+    case RGB_RMOD:
+        if(shift_mods) {
+            if (record->event.pressed) rgb_matrix_decrease_hue();
+            return false;
+        }
+        break;
+    }
 
     return true;
 }
 
+void matrix_scan_user(void) {     // The very important timer.
+    if (is_alt_tab_active && timer_elapsed32(alt_tab_timer) > 750) {
+        unregister_code(KC_LALT);
+        is_alt_tab_active = false;
+    }
+}
---
+++ keyboards/miiiw/common/gw6606.c
@@ -0,0 +1,201 @@
+#include "quantum.h"
+#include "ws2812.h"
+#include <ch.h>
+#include <hal.h>
+
+// Push Pull or Open Drain Configuration
+// Default Push Pull
+#ifndef WS2812_EXTERNAL_PULLUP
+#    define WS2812_OUTPUT_MODE PAL_MODE_OUTPUT_PUSHPULL
+#else
+#    define WS2812_OUTPUT_MODE PAL_MODE_OUTPUT_OPENDRAIN
+#endif
+
+// The reset gap can be 6000 ns, but depending on the LED strip it may have to be increased
+// to values like 600000 ns. If it is too small, the pixels will show nothing most of the time.
+#ifndef WS2812_RES
+#    define WS2812_RES (WS2812_TRST_US) // Width of the low gap between bits to cause a frame to latch
+#endif
+
+#define wait_ns(x) { \
+    for (int i = 0; i < x; i++) { \
+        __asm__ volatile("nop\n\tnop\n\tnop\n\tnop\n\t" \
+                         "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                         "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                         "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                         "nop\n\tnop\n\tnop\n\tnop\n\t"); \
+    } \
+}
+
+#define wait_bit1H() { \
+    __asm__ volatile("nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t"); \
+}
+#define wait_bit1L() { __asm__ volatile("nop\n\tnop\n\t"); }
+
+#define wait_bit0H() { __asm__ volatile("nop\n\tnop\n\t"); }
+#define wait_bit0L() { \
+    __asm__ volatile("nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t" \
+                     "nop\n\tnop\n\tnop\n\tnop\n\t"); \
+}
+
+static void sendByte(uint8_t byte, uint8_t start) {
+    // WS2812 protocol wants most significant bits first
+    for (unsigned char bit = start; bit < 8; bit++) {
+        // using something like wait_ns(is_one ? T1L : T0L) here throws off timings
+        if (byte & (0x80 >> bit)) {
+            // 1
+            writePinHigh(RGB_DI_PIN);
+            wait_bit1H();
+            writePinLow(RGB_DI_PIN);
+            wait_bit1L();
+        } else {
+            // 0
+            writePinHigh(RGB_DI_PIN);
+            wait_bit0H();
+            writePinLow(RGB_DI_PIN);
+            wait_bit0L();
+        }
+    }
+}
+
+enum { DATA_HEADER = 0x00, CURRENT_HEADER = 0x02 };
+enum { I_0_35mA = 0x00, I_0_7mA = 0x01, I_1_05mA = 0x02, I_1_4mA = 0x03, I_1_75mA = 0x04, I_2_1mA = 0x05, I_2_45mA = 0x06, I_2_8mA = 0x07, 
+       I_3_15mA = 0x08, I_3_5mA = 0x09, I_3_85mA = 0x0A, I_4_2mA = 0x0B, I_4_55mA = 0x0C, I_4_9mA = 0x0D, I_5_25mA = 0x0E, I_5_5mA = 0x0F, };
+static void set_led_current(uint8_t ri, uint8_t gi, uint8_t bi) {
+    // this code is very time dependent, so we need to disable interrupts
+    chSysLock();
+
+    writePinLow(RGB_DI_PIN);
+    wait_ns(WS2812_RES);
+    sendByte(CURRENT_HEADER, 0);
+    for (uint8_t i = 0; i < RGBLED_NUM; i++) {
+        // WS2812 protocol dictates grb order
+#if (WS2812_BYTE_ORDER == WS2812_BYTE_ORDER_GRB)
+        sendByte(gi, 2);
+        sendByte(ri, 2);
+        sendByte(bi, 2);
+#elif (WS2812_BYTE_ORDER == WS2812_BYTE_ORDER_RGB)
+        sendByte(ri, 2);
+        sendByte(gi, 2);
+        sendByte(bi, 2);
+#elif (WS2812_BYTE_ORDER == WS2812_BYTE_ORDER_BGR)
+        sendByte(bi, 2);
+        sendByte(gi, 2);
+        sendByte(ri, 2);
+#endif
+    }
+
+    writePinHigh(RGB_DI_PIN);
+    wait_bit0H();
+    writePinLow(RGB_DI_PIN);
+    wait_ns(WS2812_RES);
+    writePinHigh(RGB_DI_PIN);
+
+    chSysUnlock();
+}
+
+void ws2812_init(void) {
+    palSetLineMode(RGB_DI_PIN, WS2812_OUTPUT_MODE);
+}
+
+#ifndef GW6606_SETUP_COUNTS
+#   define GW6606_SETUP_COUNTS 5
+#endif
+
+#ifndef GW6606_CURRENT
+#   define GW6606_CURRENT I_2_1mA
+#endif
+
+#ifndef GW6606_SETUP_INTERVAL
+#   define GW6606_SETUP_INTERVAL 1000
+#endif
+
+static bool p_setup = false;
+static bool s_init = false;
+static uint8_t c_init = GW6606_SETUP_COUNTS;
+static uint32_t c_timer = 0;
+void ws2812_poweron(void) {
+    if(p_setup) return;
+    p_setup = true;
+    s_init = false;
+    c_init = GW6606_SETUP_COUNTS;
+    setPinOutput(RGB_EN_PIN);
+    writePinHigh(RGB_EN_PIN);
+}
+
+void ws2812_poweroff(void) {
+    if(!p_setup) return;
+    p_setup = false;
+    setPinInputLow(RGB_DI_PIN);
+    writePinLow(RGB_EN_PIN);
+}
+
+// Setleds for standard RGB
+void ws2812_setleds(LED_TYPE *ledarray, uint16_t leds) {
+    if (!s_init) {
+        ws2812_init();
+        s_init = true;
+    }
+
+    if(!c_init && timer_elapsed32(c_timer) > GW6606_SETUP_INTERVAL) {
+        c_init = 2;
+    }
+
+    if(c_init) {
+        c_init--;
+        set_led_current(GW6606_CURRENT, GW6606_CURRENT, GW6606_CURRENT);
+        c_timer = timer_read32();
+    }
+
+    // this code is very time dependent, so we need to disable interrupts
+    chSysLock();
+
+    writePinLow(RGB_DI_PIN);
+    wait_ns(WS2812_RES);
+    sendByte(DATA_HEADER, 0);
+    for (uint8_t i = 0; i < leds; i++) {
+        // WS2812 protocol dictates grb order
+#if (WS2812_BYTE_ORDER == WS2812_BYTE_ORDER_GRB)
+        sendByte(ledarray[i].g, 0);
+        sendByte(0, 0);
+        sendByte(ledarray[i].r, 0);
+        sendByte(0, 0);
+        sendByte(ledarray[i].b, 0);
+        sendByte(0, 0);
+#elif (WS2812_BYTE_ORDER == WS2812_BYTE_ORDER_RGB)
+        sendByte(ledarray[i].r, 0);
+        sendByte(0, 0);
+        sendByte(ledarray[i].g, 0);
+        sendByte(0, 0);
+        sendByte(ledarray[i].b, 0);
+        sendByte(0, 0);
+#elif (WS2812_BYTE_ORDER == WS2812_BYTE_ORDER_BGR)
+        sendByte(ledarray[i].b, 0);
+        sendByte(0, 0);
+        sendByte(ledarray[i].g, 0);
+        sendByte(0, 0);
+        sendByte(ledarray[i].r, 0);
+        sendByte(0, 0);
+#endif
+
+#ifdef RGBW
+        sendByte(ledarray[i].w, 0);
+        sendByte(0, 0);
+#endif
+    }
+
+    writePinHigh(RGB_DI_PIN);
+    wait_bit0H();
+    writePinLow(RGB_DI_PIN);
+    wait_ns(WS2812_RES);
+    writePinHigh(RGB_DI_PIN);
+
+    chSysUnlock();
+}
