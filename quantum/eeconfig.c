5325b8a1c47ce650ef025d171ae4de9c6b1a5873
Dasky <32983009+daskygit@users.noreply.github.com>
2022-09-12 12:49:07 +0100

    Move fake EE_HANDS

--- quantum/eeconfig.c
+++ quantum/eeconfig.c
@@ -57,16 +57,6 @@ void eeconfig_init_quantum(void) {
     eeprom_update_dword(EECONFIG_RGB_MATRIX, 0);
     eeprom_update_word(EECONFIG_RGB_MATRIX_EXTENDED, 0);
 
-    // TODO: Remove once ARM has a way to configure EECONFIG_HANDEDNESS
-    //        within the emulated eeprom via dfu-util or another tool
-#if defined INIT_EE_HANDS_LEFT
-#    pragma message "Faking EE_HANDS for left hand"
-    eeprom_update_byte(EECONFIG_HANDEDNESS, 1);
-#elif defined INIT_EE_HANDS_RIGHT
-#    pragma message "Faking EE_HANDS for right hand"
-    eeprom_update_byte(EECONFIG_HANDEDNESS, 0);
-#endif
-
 #if defined(HAPTIC_ENABLE)
     haptic_reset();
 #else
