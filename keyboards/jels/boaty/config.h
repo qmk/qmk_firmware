8f4cccee44294ac1ba4bca159a23a608ec9e0847
Jels02 <joahnelson@gmail.com>
2022-09-12 14:43:45 +1000

    move config to info.json

--- keyboards/jels/boaty/config.h
+++ keyboards/jels/boaty/config.h
@@ -16,30 +16,6 @@
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor paramater */
-#define VENDOR_ID       0x6a6b     //jels
-#define PRODUCT_ID      0x000b     //Boaty
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    jels
-#define PRODUCT         boaty
-
-/* Define Matrix size */
-#define MATRIX_ROWS 7
-#define MATRIX_COLS 11
-
-/*Define Matrix Pins */
-#define MATRIX_ROW_PINS {D6, B0, D7, B5, B3, B4, B2}
-#define MATRIX_COL_PINS {B1, C0, C1, C2, D4, D1, D0, C5, C4, C3, D5}
-/* ws2812 RGB LED */
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
