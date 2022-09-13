07ff3e28fe8faa653ac793d969647c579262dc86
adophoxia <andyao1528@gmail.com>
2022-09-12 21:46:55 -0700

    Update copyright year

--- keyboards/keychron/q0/rev_0130/keymaps/via/keymap.c
+++ keyboards/keychron/q0/rev_0130/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 @ Keychron (https://www.keychron.com)
+/* Copyright 2022 @ Keychron (https://www.keychron.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
