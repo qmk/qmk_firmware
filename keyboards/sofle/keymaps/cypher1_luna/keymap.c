d34906ca4b22092a7cb5f828e4e1f7c0537e28ac
J Pratt <jp10010101010000@gmail.com>
2022-09-13 15:36:21 +1000

    Formatting

--- keyboards/sofle/keymaps/cypher1_luna/keymap.c
+++ keyboards/sofle/keymaps/cypher1_luna/keymap.c
@@ -31,27 +31,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERT */
 
 [0] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,     KC_7,    KC_8,    KC_9,    KC_0,   KC_GRV,
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX,    KC_N,     KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI, KC_LALT,KC_LCTRL,  L1_SPC,  L2_ENT,     KC_SPC,   MO(3), KC_RCTRL, KC_RALT, KC_RGUI
+   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,   KC_GRV,
+   KC_ESC,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_BSPC,
+   KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
+  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  XXXXXXX,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
+                      KC_LGUI,  KC_LALT, KC_LCTRL,   L1_SPC,   L2_ENT,   KC_SPC,    MO(3), KC_RCTRL,  KC_RALT,  KC_RGUI
 ),
 /* YUIOP */
 [1] = LAYOUT(
-  KC_DEL,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,                         KC_6,     KC_7,    KC_8,    KC_9,    KC_0,   KC_GRV,
-  KC_BSPC,  KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,                         KC_J,     KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
-  KC_QUOT,  KC_H,   KC_J,    KC_K,    KC_L, KC_SCLN,                         KC_H,     KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
-  KC_RSFT,  KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_MUTE,    XXXXXXX,    KC_K,     KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI, KC_LALT,KC_LCTRL, _______,  L3_ENT,     KC_SPC, _______, KC_RCTRL, KC_RALT, KC_RGUI
+   KC_DEL,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,   KC_GRV,
+  KC_BSPC,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,                         KC_J,     KC_L,     KC_U,     KC_Y,  KC_SCLN,  KC_BSPC,
+  KC_QUOT,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,                         KC_H,     KC_N,     KC_E,     KC_I,     KC_O,  KC_QUOT,
+  KC_RSFT,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_MUTE,  XXXXXXX,     KC_K,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
+                      KC_LGUI,  KC_LALT, KC_LCTRL,  _______,   L3_ENT,   KC_SPC,  _______, KC_RCTRL,  KC_RALT,  KC_RGUI
 ),
 /* FUNCTION */
 [2] = LAYOUT(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,    KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,
-  _______, XXXXXXX,   KC_UP, XXXXXXX, KC_LBRC, KC_RBRC,                   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_F12,
-  KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END, KC_BSLS,                   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
-  _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MINS,  KC_EQL, _______, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
-                    _______, _______, _______,   TG(0), _______, _______, _______,  _______, _______, _______
+  _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
+  _______,  XXXXXXX,    KC_UP,  XXXXXXX,  KC_LBRC,  KC_RBRC,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   KC_F12,
+  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,  KC_BSLS,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MINS,   KC_EQL,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                      _______,  _______,  _______,    TG(0),  _______,  _______,  _______,  _______,  _______, _______
 ),
 /* KEYBOARD CONTROL */
 [3] = LAYOUT(
