8f016ca622deaada9d5b8bdb11b6d5741d5326d6
Pangorin <68218885+Pangorin@users.noreply.github.com>
2022-09-13 21:16:57 +0700

    Update rules.mk

--- keyboards/pangorin/pila87_mod/rules.mk
+++ keyboards/pangorin/pila87_mod/rules.mk
@@ -17,7 +17,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 LTO_ENABLE = no
-VIA_ENABLE = yes
 
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
