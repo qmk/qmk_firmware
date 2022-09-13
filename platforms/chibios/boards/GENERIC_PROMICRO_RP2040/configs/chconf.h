8fca06dca19b252c1852355cafd5c4b9924394d2
Stefan Kerkmann <karlk90@pm.me>
2022-09-13 21:49:43 +0200

    RP2040: tentative fix for XIP lockups
    Due to unpredictable spikes in execution when the XIP cache is missed
    and code / data has to be fetched from the external flash the virtual
    timers in ChibiOS could lockup. Until a proposed patch is merged into
    ChibiOS and lands in QMK we switch to the classic periodic tick
    implementation which is immune to the spike in the critical execution
    path.
    
    Co-authored-by: Ilya Zhuravlev <whatever@xyz.is>

--- platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/chconf.h
+++ platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/chconf.h
@@ -5,9 +5,15 @@
 
 #define CH_CFG_SMP_MODE                     TRUE
 #define CH_CFG_ST_RESOLUTION                32
-#define CH_CFG_ST_FREQUENCY                 1000000
+#define CH_CFG_ST_FREQUENCY                 10000
 #define CH_CFG_INTERVALS_SIZE               32
 #define CH_CFG_TIME_TYPES_SIZE              32
-#define CH_CFG_ST_TIMEDELTA                 20
+#define CH_CFG_ST_TIMEDELTA                 0
+
+/* Workaround a bug in chibios where port_timer_enable is not defined for RP2040 in tick mode */
+#if !defined(_FROM_ASM_)
+void stBind(void);
+#define port_timer_enable(oip) stBind()
+#endif
 
 #include_next <chconf.h>
