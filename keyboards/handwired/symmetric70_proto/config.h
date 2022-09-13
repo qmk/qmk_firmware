6971f5bd9bc29266ecbab3904c1755cb53aece54
mtei <2170248+mtei@users.noreply.github.com>
2022-09-13 17:10:51 +0900

    remove VENDOR_ID, PRODUCT_ID and MANUFACTURER from handwired/symmetric70_proto/config.h

--- keyboards/handwired/symmetric70_proto/config.h
+++ keyboards/handwired/symmetric70_proto/config.h
@@ -5,11 +5,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x2BE5
-#define MANUFACTURER mtei
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 16
