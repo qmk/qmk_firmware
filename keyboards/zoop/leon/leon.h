99a81dba2b24adccfffb83eec25517fb9a648a23
HuangJuanLR <huangjuanlr@gmail.com>
2022-09-14 11:30:54 +0800

    fix qmk compilation bugs

--- keyboards/zoop/leon/leon.h
+++ keyboards/zoop/leon/leon.h
@@ -2,8 +2,8 @@
 
 #include "quantum.h"
 
-#if defined(KEYBOARD_juan_leon_leonh)
+#if defined(KEYBOARD_zoop_leon_leonh)
   #include "leonh.h"
-#elif defined(KEYBOARD_juan_leon_leons)
+#elif defined(KEYBOARD_zoop_leon_leons)
   #include "leons.h"
 #endif
