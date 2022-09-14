d68adb208a7dc115493b915f7023a09423bc15da
ahamilton <ahamilton@emissary.co.jp>
2022-09-14 16:48:23 +0900

    Add plum47

---
+++ keyboards/plum47/config.h
@@ -0,0 +1,23 @@
+// Copyright 2022 Andrew Hamilton (@awh_tokyo)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
