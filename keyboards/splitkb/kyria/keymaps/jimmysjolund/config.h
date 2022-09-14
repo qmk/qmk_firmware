47693b3b9db1c4ed7912ce5931a2e0acd493b9cd
jimmysjolund <jimmy@sjolund.se>
2022-09-14 20:36:56 +0200

    Removed RGB light from kyria config.h

--- keyboards/splitkb/kyria/keymaps/jimmysjolund/config.h
+++ keyboards/splitkb/kyria/keymaps/jimmysjolund/config.h
@@ -16,13 +16,5 @@
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_ANIMATIONS
-#    define RGBLIGHT_HUE_STEP  8
-#    define RGBLIGHT_SAT_STEP  8
-#    define RGBLIGHT_VAL_STEP  8
-#    define RGBLIGHT_LIMIT_VAL 150
-#endif
-
 // Lets you roll mod-tap keys
 #define IGNORE_MOD_TAP_INTERRUPT
