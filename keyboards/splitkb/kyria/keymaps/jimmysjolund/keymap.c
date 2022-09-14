c89f557e0e6c2d1fa2e087be647b3a865ebda0ff
jimmysjolund <jimmy@sjolund.se>
2022-09-14 11:33:44 +0200

    Adjusted qwerty layout

--- keyboards/splitkb/kyria/keymaps/jimmysjolund/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jimmysjolund/keymap.c
@@ -63,19 +63,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 /*
- * jimmysjolund 2022-09-10
+ * jimmysjolund 2022-09-13
  *
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |  Esc   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  Tab   |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
+ * |  Tab   |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |  Sym   |   Z  |   X  |   C  |   V  |   B  | [ {  | Alt  |  | Nav  |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * |  Nav   |   Z  |   X  |   C  |   V  |   B  | [ {  | Alt  |  | Nav  |  ] } |   N  |   M  | ,  < | . >  | /  ? | Adjust |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | Shift| Bksp | Ctrl |  | Enter| Space| AltGr|  ←   |  →   |
- *                        |      |      |      |      |      |  |      |      |      |      |      |
+ *                        |Ctrl+ | LGUI | Shift| Bksp | Ctrl |  |Enter/| Space| AltGr|  ←   |  →   |
+ *                        |  Alt |      |      |      |      |  | Sym  |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
 
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
      KC_ESC , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     KC_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     SYM    , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_LALT,     NAV    , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST , KC_LGUI, KC_LSFT, KC_BSPC,KC_LCTL,     KC_ENT , KC_SPC ,KC_ALGR, KC_LEFT, KC_RGHT
+     KC_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
+     NAV    , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_LALT,     NAV    , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, ADJUST,
+                                KC_LCA , KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL,     SYM_T(KC_ENT), KC_SPC ,KC_ALGR, KC_LEFT, KC_RGHT
     ),
 
 /*
