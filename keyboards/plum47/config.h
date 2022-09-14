e5128fa85bad69b957584d5f29987f57674a8920
ahamilton <ahamilton@emissary.co.jp>
2022-09-14 21:58:11 +0900

    Remove unnecessary files

--- keyboards/plum47/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 Andrew Hamilton (@awh-tokyo)
-// Based on code by Masayuki Takagi (@takagi)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-// Set the upper left key as the boot loader key
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
