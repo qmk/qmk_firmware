b2d65fa477ad432b26ee15bafc683351a543b0ca
Ramon Imbao <ramonimbao@gmail.com>
2022-09-15 18:40:37 +0800

    Add 6.25u bottom row support

--- keyboards/saevus/cor_tkl/rules.mk
+++ keyboards/saevus/cor_tkl/rules.mk
@@ -9,5 +9,5 @@ RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 WS2812_DRIVER = vendor
 
-LAYOUTS = tkl_f13_ansi_tsangan tkl_f13_ansi_tsangan_split_bs_rshift tkl_f13_iso_tsangan tkl_f13_iso_tsangan_split_bs_rshift
+LAYOUTS = tkl_f13_ansi tkl_f13_ansi_split_bs_rshift tkl_f13_iso tkl_f13_iso_split_bs_rshift
 
