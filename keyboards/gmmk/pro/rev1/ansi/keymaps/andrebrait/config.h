3a7a23111a174c5b01af2b6fceec2b43c58677e3
Andre Brait <andrebrait@gmail.com>
2022-09-15 21:53:38 +0200

    GMMK Pro: fix unintended volume encoder taps
    Fixes some user layouts that were affected by e335d62e (#16721

--- keyboards/gmmk/pro/rev1/ansi/keymaps/andrebrait/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/andrebrait/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 Andre Brait
+/* Copyright 2022 Andre Brait
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
