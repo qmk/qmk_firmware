c0f7dd32004f73102ab0f2d28996d8830ef6ec17
Alabastard-64 <96358682+Alabastard-64@users.noreply.github.com>
2022-09-15 07:56:22 -0600

    update keycode to KC_NO

--- quantum/pointing_device/pointing_device_auto_mouse.h
+++ quantum/pointing_device/pointing_device_auto_mouse.h
@@ -34,7 +34,7 @@
 #    define AUTO_MOUSE_TIME 650
 #endif
 #ifndef AUTO_MOUSE_DELAY
-    #define AUTO_MOUSE_DELAY GET_TAPPING_TERM(KC_XXX, &(keyrecord_t){})
+    #define AUTO_MOUSE_DELAY GET_TAPPING_TERM(KC_NO, &(keyrecord_t){})
 #endif
 #ifndef AUTO_MOUSE_DEBOUNCE
 #    define AUTO_MOUSE_DEBOUNCE 25
