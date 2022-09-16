5dbeafda3b74acf00c7171c4d8f7d0f2f59fb606
Rocco Meli <r.meli@bluemail.ch>
2022-09-16 23:29:42 +0100

    fix copyright notice

--- keyboards/boardsource/lulu/keymaps/rmeli/config.h
+++ keyboards/boardsource/lulu/keymaps/rmeli/config.h
@@ -1,9 +1,5 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-Copyright 2021 Rocco Meli <@RMeli>
+Copyright 2022 Rocco Meli <@RMeli>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
