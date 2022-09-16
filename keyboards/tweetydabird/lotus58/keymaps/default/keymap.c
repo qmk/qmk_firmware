7a97b17ad8ecfe090c665d910b852886ac7320a6
TweetyDaBird <markus.knutsson@tweety.se>
2022-09-16 13:45:00 +0200

    Update keymap.c
    added thumb shift

--- keyboards/tweetydabird/lotus58/keymaps/default/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/default/keymap.c
@@ -40,10 +40,11 @@ enum custom_keycodes {
  * |--------+-------+-------+-------+-------+-------|                        |-------+-------+-------+-------+-------+--------|
  * | Capslk |   A   |   S   |   D   |   F   |   G   |                        |   H   |   J   |   K   |   L   | ; (Ö) | ' (Ä)  |
  * |--------+-------+-------+-------+-------+-------+----------.  .----------+-------+-------+-------+-------+-------+--------|
- * |  Ctrl  |   Z   |   X   |   C   |   V   |   B   |   RGB    |  |   RGB    |   N   |   M   |   ,   |   .   |   /   |  Ctrl  |
+ * |  Ctrl  |   Z   |   X   |   C   |   V   |   B   |   Down   |  |    Up    |   N   |   M   |   ,   |   .   |   /   |  Ctrl  |
  * '--------'-------'-------+-------+-------+-------+-.--------'  '--------.-+-------+-------+-------+-------'-------'--------'
  *                          |  Win  | Lower |  Alt  |/ Space  /    \  Enter \| AltGr | Raise | Bksp  |
- *                          '-------'-------'-------'--------'      '--------'-------'-------'-------'
+							|		|		|		| Shift  /      \  Shift |       |       |       |
+ *                          '-------'-------'-------'-------'        '-------'-------'-------'-------'
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB, 	KC_Q, 	KC_W,   KC_E,   KC_R,    KC_T,                             	KC_Y, 	KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC,
 	KC_CAPS, 	KC_A, 	KC_S,   KC_D,   KC_F,    KC_G,                             	KC_H, 	KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,
 	KC_LCTRL, 	KC_Z, 	KC_X,   KC_C,   KC_V,    KC_B, 	KC_DOWN,     	KC_UP,  KC_N,	KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RCTRL,
-							KC_LGUI, TG(_FN),  KC_LALT,    KC_SPC,      KC_ENT,	KC_RALT, TG(_NUM),	KC_BSPC
+							KC_LGUI, TG(_FN),  KC_LALT,    LSFT_T(KC_SPC),      RSFT_T(KC_ENT),	KC_RALT, TG(_NUM),	KC_BSPC
 	),
 
 [_NUM] = LAYOUT(
