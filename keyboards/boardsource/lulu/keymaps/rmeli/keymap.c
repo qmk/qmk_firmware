ec7f1d75b53f62a94f46c1cc787567b344949a06
Rocco Meli <r.meli@bluemail.ch>
2022-09-17 15:03:20 +0100

    clang-format lulu keymap

--- keyboards/boardsource/lulu/keymaps/rmeli/keymap.c
+++ keyboards/boardsource/lulu/keymaps/rmeli/keymap.c
@@ -49,44 +49,55 @@ enum layers { _QWERTY, _COLEMAK_DH, _RAISE, _LOWER, _ADJUST };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT_wrapper(
+  // clang-format off
   ______________NUMBER_LEFT_x6_______________,                   ______________NUMBER_RIGHT_x6______________,
   _______________QWERTY_L1_x6________________,                   _______________QWERTY_R1_x6________________,
   _______________QWERTY_L2_x6________________,                   _______________QWERTY_R2_x6________________,
   _______________QWERTY_L3_x6________________, KC_LBRC, KC_RBRC, _______________QWERTY_R3_x6________________,
                     KC_LALT, KC_LGUI,   LOWER,  KC_SPC,  KC_ENT,   RAISE, KC_BSPC, KC_RGUI
+  // clang-format on
+  ),
 ),
 
  [_COLEMAK_DH] = LAYOUT_wrapper(
+  // clang-format off
   XXXXXXX, ______________NUMBER_LEFT_x5_______________,                   ______________NUMBER_RIGHT_x5______________, XXXXXXX,
   XXXXXXX, ______________COLEMAK_MOD_DH_L1____________,                   ______________COLEMAK_MOD_DH_R1____________, XXXXXXX,
   XXXXXXX, ______________COLEMAK_MOD_DH_L2____________,                   ______________COLEMAK_MOD_DH_R2____________, XXXXXXX,
   XXXXXXX, ______________COLEMAK_MOD_DH_L3____________, KC_LBRC, KC_RBRC, ______________COLEMAK_MOD_DH_R3____________, XXXXXXX,
                                       KC_LALT, KC_LGUI,   LOWER,  KC_SPC,  KC_ENT,   RAISE, KC_BSPC, KC_RGUI
+  // clang-format on
 ),
 
 [_LOWER] = LAYOUT_wrapper(
+  // clang-format off
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   ____________________FUNC_LEFT_x6____________________,                   ____________________FUNC_RIGHT_x6___________________,
   ___________________SYMBOL_LEFT_x6___________________,                   ___________________SYMBOL_RIGHT_x6__________________,
   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
+  // clang-format on
 ),
 
 [_RAISE] = LAYOUT(
+  // clang-format off
   RGB_MOD, RGB_RMOD,RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,                     _______, _______, _______, _______, _______, _______,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   RGB_TOG, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
+  // clang-format on
 ),
 
 [_ADJUST] = LAYOUT(
+  // clang-format off
   QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    UC_MOD,  KC_ASUP,   NK_ON, XXXXXXX, XXXXXXX,  QWY_DF,
   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                    XXXXXXX, KC_ASTG, NK_TOGG, CG_TOGG, XXXXXXX, XXXXXXX,
   XXXXXXX,RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, _______,  UC_RMOD, KC_ASDN,  NK_OFF, CG_NORM, XXXXXXX,  CMK_DF,
                              _______, _______, _______, _______, _______,  _______, _______, _______
-  )
+  // clang-format on
+)
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
