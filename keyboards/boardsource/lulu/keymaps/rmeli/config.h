0dbf3cf49d1c858a39fac35796a61a6f5d4cc6a0
Rocco Meli <r.meli@bluemail.ch>
2022-09-17 22:57:20 +0100

    add rgb to lulu

--- keyboards/boardsource/lulu/keymaps/rmeli/config.h
+++ keyboards/boardsource/lulu/keymaps/rmeli/config.h
@@ -20,4 +20,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "global_config.h"
 
+
+#define MASTER_LEFT // Left side is the master
+#define SPLIT_LED_STATE_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+// Configure RGB Matrix
+#    define RGB_MATRIX_KEYPRESSES // enable keypress effects
+#    define RGB_MATRIX_LED_FLUSH_LIMIT 16
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED
+#    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+#    define RGB_MATRIX_STARTUP_HUE 10
+#    define RGB_MATRIX_STARTUP_SAT 255
+#    define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
+// Disable RGB Matrix effects (from lulu/config.h)
+#    undef ENABLE_RGB_MATRIX_ALPHAS_MODS
+#    undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#    undef ENABLE_RGB_MATRIX_BREATHING
+#    undef ENABLE_RGB_MATRIX_BAND_SAT
+#    undef ENABLE_RGB_MATRIX_BAND_VAL
+// Enable RGB Matrix effects
+#    define ENABLE_RGB_MATRIX_BREATHING
+#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#    define ENABLE_RGB_MATRIX_SOLID_COLOR
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#endif
+
 #endif // RMELI_KM_LULU
