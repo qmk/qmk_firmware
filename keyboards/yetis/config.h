4167a13b4c3662dba54b65ab53e0e74b4514df15
jOgmE <qwertzuiopp81@gmail.com>
2022-09-17 11:07:15 +0200

    [Keyboard] Add YetiS keyboard
    The yeti truly exists!
    
    Signed-off-by: Norbert Pocs <norbertpocs0@gmail.com>

---
+++ keyboards/yetis/config.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2023 Norbert Pocs (norbertpocs0@gmail.com, jogme)
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+/* Uncomment if you wish to use USB on the right half */
+#define MASTER_RIGHT
+//#define MASTER_LEFT
+
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
