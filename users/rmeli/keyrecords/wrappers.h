2db738497730ad9bdb03d4e57dc82b398c52c931
Rocco Meli <r.meli@bluemail.ch>
2022-09-18 11:05:12 +0100

    add home row mods to colemak layout

--- users/rmeli/keyrecords/wrappers.h
+++ users/rmeli/keyrecords/wrappers.h
@@ -30,15 +30,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // https://precondition.github.io/home-row-mods
 
+// Left, QWERTY
 #define HM_A LCTL_T(KC_A)
 #define HM_S LALT_T(KC_S)
 #define HM_D LGUI_T(KC_D)
 #define HM_F LSFT_T(KC_F)
 
+// Left, COLEMAK
+#define HM_R LALT_T(KC_R)
+#define HM_S LGUI_T(KC_S)
+#define HM_T LSFT_T(KC_T)
+
+// Right, QWERTY
 #define HM_J RSFT_T(KC_J)
 #define HM_K RGUI_T(KC_K)
-#define HM_L LALT_T(KC_L)
+#define HM_L LALT_T(KC_L) // LALT, not RALT
 #define HM_SCLN RCTL_T(KC_SCLN)
+
+// Right, COLEMAK
+#define HM_N RSFT_T(KC_N)
+#define HM_E RGUI_T(KC_E)
+#define HM_I LALT_T(KC_I) // LALT, not RALT
+#define HM_O RCTL_T(KC_O)
 
 // clang-format off
 
@@ -46,12 +59,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // + QWERTY |
 // + ------ +
 
-#define _____________QWERTY_HRM_LEFT______________ HM_A, HM_S, HM_D, HM_F
-#define _____________QWERTY_HRM_RIGHT_____________ HM_J, HM_K, HM_L, HM_SCLN
+#define _________QWERTY_HRM_LEFT__________    HM_A,    HM_S,    HM_D,    HM_F
+#define _________QWERTY_HRM_RIGHT_________    HM_J,    HM_K,    HM_L, HM_SCLN
 
 #define _______________QWERTY_L1_x5________________    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
 #ifdef HOME_ROW_MODS_ENABLED
-#define _______________QWERTY_L2_x5________________    _____________QWERTY_HRM_LEFT______________,    KC_G
+#define _______________QWERTY_L2_x5________________ _________QWERTY_HRM_LEFT__________,    KC_G
 #else
 #define _______________QWERTY_L2_x5________________    KC_A,    KC_S,    KC_D,    KC_F,    KC_G
 #endif
@@ -59,41 +72,52 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define _______________QWERTY_R1_x5________________    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
 #ifdef HOME_ROW_MODS_ENABLED
-#define _______________QWERTY_R2_x5________________    KC_H,    _____________QWERTY_HRM_RIGHT_____________
+#define _______________QWERTY_R2_x5________________    KC_H, _________QWERTY_HRM_RIGHT_________
 #else
-#define _______________QWERTY_R2_x5________________    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN
+#define _______________QWERTY_R2_x5________________    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN
 #endif
-#define _______________QWERTY_R3_x5________________    KC_N,    KC_M, KC_COMM,  KC_DOT,    KC_SLSH
+#define _______________QWERTY_R3_x5________________    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH
 
 // + ------ +
 
-#define ___________________QWERTY_L1_x6_____________________   KC_TAB, _______________QWERTY_L1_x5________________
-#define ___________________QWERTY_L2_x6_____________________    TD_ED, _______________QWERTY_L2_x5________________
-#define ___________________QWERTY_L3_x6_____________________  TD_LSPC, _______________QWERTY_L3_x5________________
-#define ___________________QWERTY_R1_x6_____________________  _______________QWERTY_R1_x5________________, KC_BSPC
-#define ___________________QWERTY_R2_x6_____________________  _______________QWERTY_R2_x5________________, KC_QUOT
-#define ___________________QWERTY_R3_x6_____________________  _______________QWERTY_R3_x5________________, TD_RSPC
+#define ___________________QWERTY_L1_x6_____________________  KC_TAB, _______________QWERTY_L1_x5________________
+#define ___________________QWERTY_L2_x6_____________________   TD_ED, _______________QWERTY_L2_x5________________
+#define ___________________QWERTY_L3_x6_____________________ TD_LSPC, _______________QWERTY_L3_x5________________
+#define ___________________QWERTY_R1_x6_____________________ _______________QWERTY_R1_x5________________, KC_BSPC
+#define ___________________QWERTY_R2_x6_____________________ _______________QWERTY_R2_x5________________, KC_QUOT
+#define ___________________QWERTY_R3_x6_____________________ _______________QWERTY_R3_x5________________, TD_RSPC
 
 // + -------------- +
 // + COLEMAK_MOD_DH |
 // + -------------- +
 
+#define _____COLEMAK_MOD_DH_HRM_LEFT______    HM_A,    HM_R,    HM_S,    HM_T
+#define _____COLEMAK_MOD_DH_HMR_RIGHT_____    HM_N,    HM_E,    HM_I,    HM_O
+
 #define ____________COLEMAK_MOD_DH_L1_x5___________    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B
+#ifdef HOME_ROW_MODS_ENABLED
+#define ____________COLEMAK_MOD_DH_L2_x5___________ _____COLEMAK_MOD_DH_HRM_LEFT______,    KC_G
+#else
 #define ____________COLEMAK_MOD_DH_L2_x5___________    KC_A,    KC_R,    KC_S,    KC_T,    KC_G
+#endif
 #define ____________COLEMAK_MOD_DH_L3_x5___________    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V
 
-#define ____________COLEMAK_MOD_DH_R1_x5___________    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN
+#define ____________COLEMAK_MOD_DH_R1_x5___________    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN
+#ifdef HOME_ROW_MODS_ENABLED
+#define ____________COLEMAK_MOD_DH_R2_x5___________    KC_M, _____COLEMAK_MOD_DH_HMR_RIGHT_____
+#else
 #define ____________COLEMAK_MOD_DH_R2_x5___________    KC_M,    KC_N,    KC_E,    KC_I,    KC_O
-#define ____________COLEMAK_MOD_DH_R3_x5___________    KC_K,    KC_H, KC_COMM,  KC_DOT,    KC_SLASH
+#endif
+#define ____________COLEMAK_MOD_DH_R3_x5___________    KC_K,    KC_H, KC_COMM,  KC_DOT,KC_SLASH
 
 // + ------- +
 
-#define ________________COLEMAK_MOD_DH_L1_x6________________   KC_TAB, ____________COLEMAK_MOD_DH_L1_x5___________
-#define ________________COLEMAK_MOD_DH_L2_x6________________    TD_ED, ____________COLEMAK_MOD_DH_L2_x5___________
-#define ________________COLEMAK_MOD_DH_L3_x6________________  TD_LSPC, ____________COLEMAK_MOD_DH_L3_x5___________
-#define ________________COLEMAK_MOD_DH_R1_x6________________  ____________COLEMAK_MOD_DH_R1_x5___________, KC_BSPC
-#define ________________COLEMAK_MOD_DH_R2_x6________________  ____________COLEMAK_MOD_DH_R2_x5___________, KC_QUOT
-#define ________________COLEMAK_MOD_DH_R3_x6________________  ____________COLEMAK_MOD_DH_R3_x5___________, TD_RSPC
+#define ________________COLEMAK_MOD_DH_L1_x6________________  KC_TAB, ____________COLEMAK_MOD_DH_L1_x5___________
+#define ________________COLEMAK_MOD_DH_L2_x6________________   TD_ED, ____________COLEMAK_MOD_DH_L2_x5___________
+#define ________________COLEMAK_MOD_DH_L3_x6________________ TD_LSPC, ____________COLEMAK_MOD_DH_L3_x5___________
+#define ________________COLEMAK_MOD_DH_R1_x6________________ ____________COLEMAK_MOD_DH_R1_x5___________, KC_BSPC
+#define ________________COLEMAK_MOD_DH_R2_x6________________ ____________COLEMAK_MOD_DH_R2_x5___________, KC_QUOT
+#define ________________COLEMAK_MOD_DH_R3_x6________________ ____________COLEMAK_MOD_DH_R3_x5___________, TD_RSPC
 
 // + --------------- +
 // + NUMBERS/SYMBOLS |
@@ -101,41 +125,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define ______________NUMBER_LEFT_x5_______________    KC_1,    KC_2,    KC_3,    KC_4,    KC_5
 #define ______________NUMBER_RIGHT_x5______________    KC_6,    KC_7,    KC_8,    KC_9,    KC_0
-#define ___________________NUMBER_LEFT_x6___________________    KC_GRV,  ______________NUMBER_LEFT_x5_______________
-#define ___________________NUMBER_RIGHT_x6__________________    ______________NUMBER_RIGHT_x5______________, KC_MINS
+#define ___________________NUMBER_LEFT_x6___________________  KC_GRV, ______________NUMBER_LEFT_x5_______________
+#define ___________________NUMBER_RIGHT_x6__________________ ______________NUMBER_RIGHT_x5______________, KC_MINS
 
-#define ______________SYMBOL_LEFT_x5_______________    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC
-#define ______________SYMBOL_RIGHT_x5______________    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN
-#define ___________________SYMBOL_LEFT_x6___________________    KC_GRV, ______________SYMBOL_LEFT_x5_______________
-#define ___________________SYMBOL_RIGHT_x6__________________    ______________SYMBOL_RIGHT_x5______________, KC_TILD
+#define ______________SYMBOL_LEFT_x5_______________ KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC
+#define ______________SYMBOL_RIGHT_x5______________ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN
+#define ___________________SYMBOL_LEFT_x6___________________  KC_GRV, ______________SYMBOL_LEFT_x5_______________
+#define ___________________SYMBOL_RIGHT_x6__________________ ______________SYMBOL_RIGHT_x5______________, KC_TILD
 
 // + ------- +
 // + UNICODE |
 // + ------- +
 
 #ifdef UNICODEMAP_ENABLE
-#define ______________UNICODE_L2_x5________________    A_GRV,   E_GRV,   I_GRV,   O_GRV,   U_GRV
-#define ______________UNICODE_L3_x5________________    A_UML,   E_ACT,   I_CIR,   O_UML,   U_UML
+#define ______________UNICODE_L2_x5________________   A_GRV,   E_GRV,   I_GRV,   O_GRV,   U_GRV
+#define ______________UNICODE_L3_x5________________   A_UML,   E_ACT,   I_CIR,   O_UML,   U_UML
 #endif
 
 // + ---- +
 // + FUNC |
 // + ---- +
 
-#define ______________FUNC_LEFT_5x_________________    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5
-#define ______________FUNC_RIGHT_5x________________    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10
-#define ____________________FUNC_LEFT_x6____________________    ______________FUNC_LEFT_5x_________________, KC_F6
-#define ____________________FUNC_RIGHT_x6___________________    KC_F7, ______________FUNC_RIGHT_5x________________
+#define ______________FUNC_LEFT_5x_________________   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5
+#define ______________FUNC_RIGHT_5x________________   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10
+#define ____________________FUNC_LEFT_x6____________________ ______________FUNC_LEFT_5x_________________, KC_F6
+#define ____________________FUNC_RIGHT_x6___________________ KC_F7, ______________FUNC_RIGHT_5x________________
 
 // + ---- +
 // + MISC |
 // + ---- +
 
-#define _________________BLANK_5x__________________    _______, _______, _______, _______, _______
-#define _________________BLANK_6x__________________    _________________BLANK_5x__________________, _______
+#define _________________BLANK_5x__________________ _______, _______, _______, _______, _______
+#define _________________BLANK_6x__________________ _________________BLANK_5x__________________, _______
 
-#define ________________NAV_R2_x5__________________    XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, KC_PGUP
-#define ________________NAV_R3_x5__________________    XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN
+#define ________________NAV_R2_x5__________________ XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, KC_PGUP
+#define ________________NAV_R3_x5__________________ XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN
 
 // clang-format on
 
