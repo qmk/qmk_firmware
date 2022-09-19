f3cac7c177089707f0df6972074b0c3cdf76db8e
kuriatsu <kuriatsubayashi712@gmail.com>
2022-09-19 09:55:48 +0900

    solve lint error

--- keyboards/handwired/tsubasa/config.h
+++ keyboards/handwired/tsubasa/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 kuriatsu <kuriatsubayashi712@gmail.com>
+Copyright 2021 @kuriatsu
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -45,7 +45,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-#define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -69,10 +68,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
-
-/* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
 
 #define ENCODERS_PAD_A { F4 }
 #define ENCODERS_PAD_B { F5 }
