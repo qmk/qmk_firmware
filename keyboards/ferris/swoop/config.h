24837b721453d26ab05869d4643d8e8c5fedc3e7
Duccio <ducciobreschi@gmail.com>
2022-09-20 23:34:50 +0200

    Update copyright holder

--- keyboards/ferris/swoop/config.h
+++ keyboards/ferris/swoop/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2018-2020 ENDO Katsuhiro <ka2hiro@curlybracket.co.jp> David Philip Barr <@davidphilipbarr> Pierre Chevalier <pierrechevalier83@gmail.com>
+/* Copyright 2022 James White <jamesmnw@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
