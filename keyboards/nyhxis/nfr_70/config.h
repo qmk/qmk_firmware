2b2cda6258879a3ebcb71d0a82172cfac499eb6b
Jesper Severinsen <juul.severinsen@gmail.com>
2022-09-20 08:13:03 +0200

    Remove USB from config.h

--- keyboards/nyhxis/nfr_70/config.h
+++ keyboards/nyhxis/nfr_70/config.h
@@ -18,13 +18,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xABCD
-#define PRODUCT_ID      0x0001
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Nyhxis
-#define PRODUCT         NFR-70
-
 /* key matrix size */
 #define MATRIX_ROWS 11
 #define MATRIX_COLS 7
