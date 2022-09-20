37a80ed7aa7c6c3a389cd83d0f48151562b22bf5
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2022-09-20 16:50:43 +1000

    Fix lint error

--- keyboards/converter/xmk/post_rules.mk
+++ keyboards/converter/xmk/post_rules.mk
@@ -3,8 +3,9 @@
 
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-SRC += xmk_matrix.c
-SRC += xmk_shell.c
-CUSTOM_MATRIX = lite
-
-#XMK_DEBUG = yes
+ifeq ($(strip $(XMK_DEBUG)), yes)
+  CONSOLE_ENABLE = yes
+  OPT_DEFS += -DXMK_DEBUG
+else
+  CONSOLE_ENABLE = no
+endif
