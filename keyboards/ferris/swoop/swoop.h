2785f06b63b80b9e4915c03f3a7ebc4cd1f05eab
Duccio <ducciobreschi@gmail.com>
2022-09-21 19:56:20 +0200

    Revert "Change layout ID"
    This reverts commit 2ec883dd5bee617523f8e07ad9ef3ee154f089b4.

--- keyboards/ferris/swoop/swoop.h
+++ keyboards/ferris/swoop/swoop.h
@@ -18,7 +18,7 @@
 
 #define ___ KC_NO
 
-#define LAYOUT_split_3x5_3( \
+#define LAYOUT( \
     L00, L01, L02, L03, L04,          R00, R01, R02, R03, R04, \
     L10, L11, L12, L13, L14,          R10, R11, R12, R13, R14, \
     L20, L21, L22, L23, L24,          R20, R21, R22, R23, R24, \
