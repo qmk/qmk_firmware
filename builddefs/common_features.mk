676842d592e2fdb1517667192bb282e3dafdcc9f
Alabastard-64 <96358682+Alabastard-64@users.noreply.github.com>
2022-09-24 00:43:55 -0600

    [Core] Pointing Device Automatic Mouse Layer (#17962)
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Stefan Kerkmann <karlk90@pm.me>

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -136,6 +136,7 @@ ifeq ($(strip $(POINTING_DEVICE_ENABLE)), yes)
         VPATH += $(QUANTUM_DIR)/pointing_device
         SRC += $(QUANTUM_DIR)/pointing_device/pointing_device.c
         SRC += $(QUANTUM_DIR)/pointing_device/pointing_device_drivers.c
+        SRC += $(QUANTUM_DIR)/pointing_device/pointing_device_auto_mouse.c
         ifneq ($(strip $(POINTING_DEVICE_DRIVER)), custom)
             SRC += drivers/sensors/$(strip $(POINTING_DEVICE_DRIVER)).c
             OPT_DEFS += -DPOINTING_DEVICE_DRIVER_$(strip $(shell echo $(POINTING_DEVICE_DRIVER) | tr '[:lower:]' '[:upper:]'))
