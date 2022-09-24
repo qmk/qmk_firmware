d9d8bd83673fc02d062534c1fab7938b215346d1
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-09-23 18:28:06 -0700

    add LAYOUT_tkl_ansi

--- keyboards/linworks/fave87h/fave87h.h
+++ keyboards/linworks/fave87h/fave87h.h
@@ -36,6 +36,22 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* FAve 87H Keymap Definitions */
+#define LAYOUT_tkl_ansi( \
+    K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,  K0E, K0F, K0G, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,  K1E, K1F, K1G, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D,  K2E, K2F, K2G, \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3D,                 \
+    K40,      K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A,      K4C,       K4F,      \
+    K50, K51, K52,                K55,                K59, K5A, K5C, K5D,  K5E, K5F, K5G  \
+) { \
+    { K00, ___, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E, K0F, K0G }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F, K1G }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, K2G }, \
+    { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, ___, K3D, ___, ___, ___ }, \
+    { K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, ___, K4C, ___, ___, K4F, ___ }, \
+    { K50, K51, K52, ___, ___, K55, ___, ___, ___, K59, K5A, ___, K5C, K5D, K5E, K5F, K5G }  \
+}
+
 #define LAYOUT_tkl_ansi_split_bs( \
     K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,       K0E, K0F, K0G, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K3C,  K1E, K1F, K1G, \
