721f7af7a295c35e91bb4ca002d0545adb026cd0
honorless <86894501+lesshonor@users.noreply.github.com>
2022-09-24 01:15:21 -0400

    refactor: move most info to data-driven config

--- keyboards/tau4/config.h
+++ keyboards/tau4/config.h
@@ -19,16 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-#define ENCODERS_PAD_A { B3 }
-#define ENCODERS_PAD_B { B7 }
-#define ENCODER_RESOLUTION 1
-
 // Buggy, currently disabled
 /* #define EXTERNAL_EEPROM_I2C_BASE_ADDRESS 0b10100000
 #define EXTERNAL_EEPROM_I2C_ADDRESS(loc) (EXTERNAL_EEPROM_I2C_BASE_ADDRESS | ((((loc) >> 8) & 0x07) << 1))
@@ -39,9 +29,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGBLIGHT_LAYERS_RETAIN_VAL
 
-#define OLED_DISPLAY_ADDRESS 0x3C
-#define OLED_BRIGHTNESS 255
-#define OLED_IC OLED_IC_SSD1306
 #define OLED_DISPLAY_WIDTH 128
 #define OLED_DISPLAY_HEIGHT 32
 #define OLED_FONT_H "keyboards/tau4/common/glcdfont.c"
