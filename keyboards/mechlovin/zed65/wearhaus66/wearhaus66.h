515f93257daaf2034ddc2cba903f170144dcb0ff
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-09-24 02:51:49 -0700

    rename LAYOUT_65_ansi_blocker to LAYOUT_ansi_blocker

--- keyboards/mechlovin/zed65/wearhaus66/wearhaus66.h
+++ keyboards/mechlovin/zed65/wearhaus66/wearhaus66.h
@@ -50,7 +50,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { K40,   K41,   K42, KC_NO, KC_NO, KC_NO,   K46, KC_NO, KC_NO,   K49,   K4A, KC_NO,   K4C,   K4D,   K4E   }, \
 }
 
-#define LAYOUT_65_ansi_blocker( \
+#define LAYOUT_ansi_blocker( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D,        \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K2C, K1E,   \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D, K2E,   \
