219b2b1be3dc660ee49ba82d0e11e1ff7f27a715
Dennis Kruyt <dennis@kruyt.org>
2022-09-26 23:05:39 +0200

    linting fixes

--- keyboards/phantagom/rgb_npad/config.h
+++ keyboards/phantagom/rgb_npad/config.h
@@ -24,7 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0003
 #define MANUFACTURER    phantagom
 #define PRODUCT         rgb npad
-#define DESCRIPTION     rgb numpad with extra function row
 
 #define TAPPING_TERM 400
 
