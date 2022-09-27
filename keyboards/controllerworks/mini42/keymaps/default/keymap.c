1c12d4b7cf737fc9e6d4b4f634633b8221674654
Controller Works <103382872+controller-works@users.noreply.github.com>
2022-09-26 23:15:35 -0400

    Update keyboards/controllerworks/mini42/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/controllerworks/mini42/keymaps/default/keymap.c
+++ keyboards/controllerworks/mini42/keymaps/default/keymap.c
@@ -15,7 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include <stdio.h>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
