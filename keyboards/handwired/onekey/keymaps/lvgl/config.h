4463ac7fb01f73b8196239c06557be984d680adf
pablin.123.ra@gmail.com <pablin.123.ra@gmail.com>
2022-09-26 20:40:40 -0500

    Add LVGL onekey keymap

---
+++ keyboards/handwired/onekey/keymaps/lvgl/config.h
@@ -0,0 +1,10 @@
+/* SPI pins */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
+#define SPI_MISO_PIN GP20
+
+/* LCD Configuration */
+#define LCD_RST_PIN GP0
+#define LCD_DC_PIN GP1
+#define LCD_CS_PIN GP2
