5be76e872ca22c353b53f912c66a06dfe0e39db7
atsai <cgmandy@gmail.com>
2022-09-27 06:07:13 -0700

    added license headers, removed macropad

--- keyboards/hardwareabstraction/handwire/custom_fonts.c
+++ keyboards/hardwareabstraction/handwire/custom_fonts.c
@@ -1,3 +1,6 @@
+// Copyright 2022 Andy Tsai (@atsai)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "progmem.h"
 
 static const unsigned char font[] PROGMEM = {
--- keyboards/hardwareabstraction/handwire/keymaps/default/keymap.c
+++ keyboards/hardwareabstraction/handwire/keymaps/default/keymap.c
@@ -1,3 +1,6 @@
+// Copyright 2022 Andy Tsai (@atsai)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 #ifdef BUZZER_ENABLE
