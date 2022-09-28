cb637f0f9ceff79e0cc277f770460740e65e4714
ZyBeR <n/a>
2022-09-28 16:40:33 +0200

    [Keymap] Zeal Zephyr (ZyBeR)

--- keyboards/wilba_tech/zeal65/rules.mk
+++ keyboards/wilba_tech/zeal65/rules.mk
@@ -20,7 +20,7 @@ COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 
 CIE1931_CURVE = yes
 
