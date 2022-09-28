782703b7dba684d5ae088928a9a23b9de07070c4
ZyBeR <n/a>
2022-09-28 16:22:10 +0200

    Updated FC660C keymap with “tap option” to search

--- keyboards/fc660c/keymaps/zyber/keymap.c
+++ keyboards/fc660c/keymaps/zyber/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     TD(LBRC), KC_RBRC, KC_BSLS,     KC_DEL,
         L1_EXPL, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   TD(SCLN), TD(QUOT),   KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,   KC_UP,
-        KC_LCTL, KC_LALT, KC_LGUI,                 KC_SPC,                  KC_RALT, KC_RCTL, MO(1),         KC_LEFT,KC_DOWN,KC_RGHT
+        KC_LCTL, ROPT_SRH, KC_LGUI,                 KC_SPC,                  KC_RALT, KC_RCTL, MO(1),         KC_LEFT,KC_DOWN,KC_RGHT
     ),
   /* FN layer
    * ,--------------------------------------------------------------------------------------------------.
