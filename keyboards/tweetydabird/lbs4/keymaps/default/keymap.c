ef4c96898f5d252ee27ecf608d2c7876c73eae2e
TweetyDaBird <markus.knutsson@tweety.se>
2022-09-28 14:00:07 +0200

    LBS4 default

--- keyboards/tweetydabird/lbs4/keymaps/default/keymap.c
+++ keyboards/tweetydabird/lbs4/keymaps/default/keymap.c
@@ -12,42 +12,24 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        TO(_RGB),	KC_MSEL,	KC_MPRV,	KC_MNXT,	KC_MPLY
+        KC_MPLY,
+		TO(_RGB),	KC_MSEL,	
+		KC_MPRV,	KC_MNXT
     ),
     [_RGB] = LAYOUT(
-        TO(_BASE),	RGB_VAI,	RGB_TOG,	RGB_VAD, 	RGB_TOG
+        KC_MPLY,
+		TO(_BASE),	RGB_VAI,	
+		RGB_TOG,	RGB_VAD
     )
 };
 
-#ifdef ENCODER_ENABLE
-
-// switch(biton32(layer_state)) will get you the number of the current layer
-
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        switch(biton32(layer_state)){
-            case _RGB:
-                // rgb brightness up/down
-                if(clockwise){
-                    // tap_code(RGB_VAI);
-                    //rgblight_increase_val();
-					tap_code(KC_UP);
-                } else {
-                    // tap_code(RGB_VAD);
-                    //rgblight_decrease_val();
-					tap_code(KC_DOWN);
-                }
-                break;
-            default:
-                //volume up/down
-                if (clockwise) {
-                    tap_code(KC_VOLU);
-                } else {
-                    tap_code(KC_VOLD);
-                }
-        }
-    } 
+    if (index == 0) {
+		if (clockwise) {
+			tap_code(KC_VOLU);
+		} else {
+			tap_code(KC_VOLD);
+		}        
+	} 
 	return true;
 };
-
-#endif
--- keyboards/tweetydabird/lbs4/readme.md
+++ keyboards/tweetydabird/lbs4/readme.md
@@ -5,7 +5,7 @@
 *A simple 4-key or 2-key plus OLED macropad with a clickable scrollwheel, for media controls and whatever else you can imagine.*
 
 * Keyboard Maintainer: [Markus Knutsson](https://github.com/TweetyDaBird/)
-* Hardware Supported: LSB4 PCB plus Pro Micro or pin compatible
+* Hardware Supported: LBS4 PCB plus Pro Micro or pin compatible
 * Hardware Availability: https://lectronz.com/stores/tweetys-wild-thinking
 * Hardware Design: https://github.com/TweetyDaBird/Little-Big-Scroll
 
