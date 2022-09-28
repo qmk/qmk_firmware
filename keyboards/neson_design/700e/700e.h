47d0214fcd125604f117c20157389fd210d60c7a
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-09-28 15:26:06 -0700

    add LAYOUT_ansi_tsangan with reference keymap

--- keyboards/neson_design/700e/700e.h
+++ keyboards/neson_design/700e/700e.h
@@ -54,3 +54,18 @@
     { K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, K3F }, \
     { K40, K41, K42, ___, ___, ___, ___, ___, ___, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
 }
+
+#define LAYOUT_ansi_tsangan( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K4D,   K0E, K0F, K1F, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,   K1E, K2E, K2F, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      K2D,                  \
+    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B,      K3C,        K3E,      \
+    K40, K41, K42,                K4A,                     K49, K4B, K4C,   K3F, K4E, K4F  \
+) \
+{ \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, ___, K0E, K0F }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, ___, K2D, K2E, K2F }, \
+    { K30, ___, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, ___, K3E, K3F }, \
+    { K40, K41, K42, ___, ___, ___, ___, ___, ___, K49, K4A, K4B, K4C, K4D, K4E, K4F }, \
+}
