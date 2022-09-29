d2ee0573751ad4a1042afef0aac84e33b8e3603b
Controller Works <103382872+controller-works@users.noreply.github.com>
2022-09-29 08:33:09 -0400

    Update keyboards/controllerworks/mini42/mini42.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/controllerworks/mini42/mini42.c
+++ keyboards/controllerworks/mini42/mini42.c
@@ -14,9 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include QMK_KEYBOARD_H
 #include "mini42.h"
-#include "split_util.h"
 
 #ifdef OLED_ENABLE
 
