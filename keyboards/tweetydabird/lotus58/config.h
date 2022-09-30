6e23b9597f7b2b95961bf6815bf548b23fc01ef8
TweetyDaBird <markus.knutsson@tweety.se>
2022-10-01 00:18:46 +0200

    Update config.h

--- keyboards/tweetydabird/lotus58/config.h
+++ keyboards/tweetydabird/lotus58/config.h
@@ -18,11 +18,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 #include "config_common.h"
 
-/*
+// Not working correct in json implementation
 #define SPLIT_USB_DETECT
 #define SPLIT_USB_TIMEOUT 2000
 #define SPLIT_USB_TIMEOUT_POLL 10
-*/
+
+// Not yet supported in json
 
 // Bootmagic Lite key configuration
 #define BOOTMAGIC_LITE_ROW 0
@@ -30,9 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BOOTMAGIC_LITE_ROW_RIGHT 5
 #define BOOTMAGIC_LITE_COLUMN_RIGHT 0
 
-// Not yet supported in json
 #define SPLIT_HAND_PIN B5
-
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
 #define SPLIT_OLED_ENABLE
