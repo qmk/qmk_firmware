1ddd1feafd5ba8ba292cd19b1b9cae62c4ed23e3
Seth Barberee <seth.barberee@gmail.com>
2022-09-29 19:23:02 -0500

    [Keyboard] Convert most of sinc to data driven info.json (#18481)

--- keyboards/keebio/sinc/rev1/rev1.c
+++ keyboards/keebio/sinc/rev1/rev1.c
@@ -2,14 +2,14 @@
 #include "split_util.h"
 
 void matrix_init_kb(void) {
-    setPinOutput(CAPS_LOCK_LED_PIN);
+    setPinOutput(LED_CAPS_LOCK_PIN);
     matrix_init_user();
 }
 
 bool led_update_kb(led_t led_state) {
     // Only update if left half
     if (isLeftHand && led_update_user(led_state)) {
-        writePin(CAPS_LOCK_LED_PIN, !led_state.caps_lock);
+        writePin(LED_CAPS_LOCK_PIN, !led_state.caps_lock);
     }
     return true;
 }
