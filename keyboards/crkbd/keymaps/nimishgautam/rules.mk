8d444fe8be6e5f5d6908a7762aa80d254b3ad060
nimishgautam <nimishg@gmail.com>
2022-09-30 12:19:02 +0200

    Update keyboards/crkbd/keymaps/nimishgautam/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/crkbd/keymaps/nimishgautam/rules.mk
+++ keyboards/crkbd/keymaps/nimishgautam/rules.mk
@@ -6,7 +6,6 @@ TAP_DANCE_ENABLE = yes  # enable tap dance features
 EXTRAKEY_ENABLE = no
 GRAVE_ESC_ENABLE = no
 SPACE_CADET_ENABLE = no
-SPLIT_KEYBOARD = yes
 #WPM_ENABLE = yes
 #CAPS_WORD_ENABLE = yes # if you hold down both shifts, you get lshift enabled for 1 word, but only works with KC shift, not MOD shift :(
 #AUTO_SHIFT_ENABLE = yes
