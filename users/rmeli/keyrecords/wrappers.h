c4ea6e3c0694067d25ccc2679842520749d0f3de
Rocco Meli <r.meli@bluemail.ch>
2022-10-01 19:43:00 +0100

    Update users/rmeli/keyrecords/wrappers.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- users/rmeli/keyrecords/wrappers.h
+++ users/rmeli/keyrecords/wrappers.h
@@ -17,8 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef RMELI_WRAPPERS
-#define RMELI_WRAPPERS
+#pragma once
 
 #ifdef UNICODEMAP_ENABLE
 #    include "keyrecords/unicode.h"
