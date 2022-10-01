57666173b623228629ee719b68d7a804cb32be0a
4pplet <mail@4pplet.com>
2022-10-01 19:44:52 +0200

    [Keyboard] Add waffling60 Rev D ISO Hotswap (#18372)
    Co-authored-by: 4pplet <4pplet@protonmail.com>
    Co-authored-by: 4pplet <stefan.ess@gmail.com>

--- keyboards/4pplet/waffling60/rev_d_iso/config.h
+++ keyboards/4pplet/waffling60/rev_d_iso/config.h
@@ -19,21 +19,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 11
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
 // ROWS: Top to bottom, COLS: Left to right
 
-#define MATRIX_ROW_PINS {D4,D1,C2,C4,C7,B2}
-#define MATRIX_COL_PINS {D3,D0,D2,B6,B5,B4,B3,D6,D5,B0,B1}
+#define MATRIX_ROW_PINS {B14,A9,B6,B5,B4}
+#define MATRIX_COL_PINS {B2,A4,A3,A2,F1,F0,C15,C14,C13,B9,B8,B7,A15,B3}
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
-#define QMK_ESC_OUTPUT D3 // usually COL
-#define QMK_ESC_INPUT D4 // usually ROW
+#define QMK_ESC_OUTPUT B2 // usually COL
+#define QMK_ESC_INPUT B14 // usually ROW
--- keyboards/4pplet/waffling60/rev_d_iso/rev_d_iso.h
+++ keyboards/4pplet/waffling60/rev_d_iso/rev_d_iso.h
@@ -20,17 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // k<row><col>
 #define LAYOUT( \
-    k00, k01, k11, k02, k03, k04, k05, k15, k06, k07, k08, k18, k09, k0a, k1a, \
-    k10, k21, k12, k22, k13, k14, k25, k35, k16, k17, k28, k19, k29, k2a, \
-    k20, k31, k32, k23, k33, k24, k45, k26, k36, k27, k38, k39, k3a, \
-    k30, k40, k41, k42, k43, k34, k44, k55, k46, k37, k47, k48, k49, k4a, \
-    k50, k51, k52, k53, k54, k56, k57, k58, k59, k5a  \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k1d, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, \
+    k40, k41, k42, k44, k46, k48, k4b, k4c, k4d  \
 ) \
 { \
-    {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a}, \
-    {k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a}, \
-    {k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a}, \
-    {k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a}, \
-    {k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a}, \
-    {k50, k51, k52, k53, k54, k55, k56, k57, k58, k59, k5a}  \
+    {k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d}, \
+    {k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d}, \
+    {k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d}, \
+    {k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d}, \
+    {k40, k41, k42, KC_NO, k44, KC_NO, k46, KC_NO, k48, KC_NO, KC_NO, k4b, k4c, k4d} \
 }
