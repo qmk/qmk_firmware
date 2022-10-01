1056f58e2b229ee5a2fff8efb2356ecc06ef38ab
Rocco Meli <r.meli@bluemail.ch>
2022-10-01 19:41:52 +0100

    Update users/rmeli/rmeli.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- users/rmeli/rmeli.h
+++ users/rmeli/rmeli.h
@@ -15,10 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef RMELI
-#define RMELI
-
-#include "global_config.h"
+#pragma once
 
 #include "keyrecords/wrappers.h"
 
