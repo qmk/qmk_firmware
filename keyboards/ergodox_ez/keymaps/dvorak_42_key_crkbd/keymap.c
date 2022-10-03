8a2731d759e811d651835c313313ae957f0bf1e6
luc wastiaux <luc.wastiaux@airpost.net>
2022-10-03 21:33:40 +0800

    add caps word support

--- keyboards/ergodox_ez/keymaps/dvorak_42_key_crkbd/keymap.c
+++ keyboards/ergodox_ez/keymaps/dvorak_42_key_crkbd/keymap.c
@@ -229,7 +229,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       MEH(KC_1), KC_F,  KC_G,  KC_C,   KC_R,   KC_L,   KC_TAB,
                  KC_D,  KC_H,  KC_T,   KC_N,   KC_S,   RCTL(KC_BSPC),
-      MEH(KC_2), KC_B,  KC_M,  KC_W,   KC_V,   KC_Z,   MEH(KC_8),
+      MEH(KC_2), KC_B,  KC_M,  KC_W,   KC_V,   KC_Z,   OSM(MOD_LSFT),
       RCTL(KC_BSPC), RCTL(KC_BSPC), KC_TRNS, KC_TRNS, KC_TRNS,
 
       // right thumb cluster
