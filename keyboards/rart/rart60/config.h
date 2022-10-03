5d4d2aaaf9398bcf57e997ac19f2b6a1572a72f8
Alabahuy <rezzaadio@gmail.com>
2022-10-03 10:01:38 +0700

    Update config.h

--- keyboards/rart/rart60/config.h
+++ keyboards/rart/rart60/config.h
@@ -17,18 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 16
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
