ec9380cef67e057bcf82e1849f84d5dd32c266f0
luc wastiaux <luc.wastiaux@airpost.net>
2022-10-05 19:00:11 +0800

    fix red led issue

--- keyboards/ergodox_ez/keymaps/dvorak_42_key_crkbd/keymap.c
+++ keyboards/ergodox_ez/keymaps/dvorak_42_key_crkbd/keymap.c
@@ -817,7 +817,7 @@ void matrix_scan_user(void) {
 
     uint8_t layer = get_highest_layer(layer_state);
 
-    ergodox_board_led_off();
+    ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
