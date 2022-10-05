00da10b80f310b255d0333f170d352332612b80e
Richard Dawe <richdawe@gmail.com>
2022-10-05 14:15:16 +0100

    Go back to specific layer key, because of issues with space repeating

--- keyboards/dm9records/lain/keymaps/richdawe/keymap.c
+++ keyboards/dm9records/lain/keymaps/richdawe/keymap.c
@@ -14,8 +14,6 @@ ps2pdf lain-keymap-richdawe.ps lain-keymap-richdawe.pdf
 enum layers { BASE, NUM, SYM, FUNC };
 enum custom_keycodes { LED_EN = SAFE_RANGE };
 
-#define LTS_SPC LT(SYM, KC_SPC)
-#define LTN_SPC LT(NUM, KC_SPC)
 #define LTF_TAB LT(FUNC, KC_TAB)
 
 // clang-format off
@@ -30,13 +28,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------'     \-----------------------------------|
  * |LShft|  Z  /  X  /  C  /  V  /  B  /           \  \  \  N  \  M  \  ,  \  .  |ShEnt|
  * `-----------------------------------'           \-----------------------------------|
- *    |LCtrl|  /LGUI /LAlt /Space/SpLSy/           \SpLNu\Space\RAlt \RCtrl\  /  |  `  |
+ *    |LCtrl|  /LGUI /LAlt /Space/LSym /           \LNum \Space\RAlt \RCtrl\  /  |  `  |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         LTF_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SFTENT,
-        KC_LCTL,          KC_LGUI, KC_LALT, KC_SPC,  LTS_SPC,            LTN_SPC, KC_SPC,  KC_RALT, KC_RCTL, KC_SLSH, KC_GRV
+        KC_LCTL,          KC_LGUI, KC_LALT, KC_SPC,  MO(SYM),            MO(NUM), KC_SPC,  KC_RALT, KC_RCTL, KC_SLSH, KC_GRV
     ),
 
     [NUM] = LAYOUT(
