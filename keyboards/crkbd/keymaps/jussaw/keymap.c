7b4a6cce91a421c671b60d2ff177f4c433211944
Justin Sawyer <jtsaw36@gmail.com>
2022-10-04 20:27:09 -0600

    Update gaming layer

--- keyboards/crkbd/keymaps/jussaw/keymap.c
+++ keyboards/crkbd/keymaps/jussaw/keymap.c
@@ -140,18 +140,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------.                    ,-----------------------------------------------------.
    * |  Bksp  |  Tab   |    Q   |    W   |    E   |    R   |                    |    T   |    Y   |    U   |    I   |    O   |    P   |
    * |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   * |   ""   |  Esc   |    A   |    S   |    D   |    F   |                    |    G   |    H   |    J   |    K   |    L   |    ;   |
+   * |   ""   |  Shift |    A   |    S   |    D   |    F   |                    |    G   |    H   |    J   |    K   |    L   |    ;   |
    * |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   * | Enter  |  Shift |    Z   |    X   |    C   |    V   |                    |    B   |    N   |    M   |    ,   |    .   |    /   |
+   * | Enter  |  Ctrl  |    Z   |    X   |    C   |    V   |                    |    B   |    N   |    M   |    ,   |    .   |    /   |
    * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-   *                                     |  Ctrl  | Space  |  Alt   |  |        |        | TG(4)  |
+   *                                     |   Esc  |  Alt   | Space  |  |        |        | TG(4)  |
    *                                     `--------------------------'  `--------------------------'
    */
   [_GAME] = LAYOUT_split_3x6_3(
       KC_BSPC,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-      KC_QUOT,  KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,                         KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-       KC_ENT,  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-                                          KC_LCTL,  KC_SPC, KC_LALT,    XXXXXXX, XXXXXXX,   TG(_GAME)
+      KC_QUOT,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,                         KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
+       KC_ENT,  KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+                                           KC_ESC, KC_LALT, KC_SPC,     XXXXXXX, XXXXXXX,    TG(_GAME)
   ),
 };
 
