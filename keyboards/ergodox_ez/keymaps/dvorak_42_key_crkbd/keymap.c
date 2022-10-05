c84d10773abce657edff85f50629f2ce9d386d81
luc wastiaux <luc.wastiaux@airpost.net>
2022-10-05 08:37:56 +0800

    notepadpp duplicate line shortcut

--- keyboards/ergodox_ez/keymaps/dvorak_42_key_crkbd/keymap.c
+++ keyboards/ergodox_ez/keymaps/dvorak_42_key_crkbd/keymap.c
@@ -111,6 +111,10 @@ enum custom_keycodes {
 #define SHORTCUTS          13 // shortcuts to be intercepted by autohotkey
 
 
+// Notepad++ shortcuts
+// ===================
+#define NP_DUPE_LINE LCTL(KC_D)
+
 
 // Android Studio shortcuts
 #define AS_TABLEFT LALT(KC_LEFT)
@@ -287,11 +291,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [KEYNAV] = LAYOUT_ergodox(
     // left hand
-    KC_TRNS,         KC_TRNS,     KC_TRNS,        KC_TRNS,         KC_TRNS,          KC_TRNS,     KC_TRNS,
-    KC_TRNS,         KC_ESC,      KC_TRNS,        RCTL(KC_Z),      RCTL(KC_S),       RCTL(KC_N),  KC_TRNS,
-    KC_TRNS,         MO(KEYSEL),  KC_TRNS,        RSFT(KC_TAB),    KC_TAB,           RCTL(KC_F),
-    KC_TRNS,         KC_TRNS,     OSM(MOD_LGUI),  OSM(MOD_LALT),   OSM(MOD_LCTL),    KC_TRNS,     KC_TRNS,
-    KC_TRNS,         KC_TRNS,     KC_TRNS,        KC_TRNS,         KC_TRNS,
+    KC_TRNS,         KC_TRNS,       KC_TRNS,        KC_TRNS,         KC_TRNS,          KC_TRNS,      KC_TRNS,
+    KC_TRNS,         KC_ESC,        KC_TRNS,        RCTL(KC_Z),      RCTL(KC_S),       RCTL(KC_N),   KC_TRNS,
+    KC_TRNS,         MO(KEYSEL),    KC_TRNS,        RSFT(KC_TAB),    KC_TAB,           RCTL(KC_F),
+    KC_TRNS,         OSM(MOD_LSFT), OSM(MOD_LGUI),  OSM(MOD_LALT),   OSM(MOD_LCTL),    NP_DUPE_LINE, KC_TRNS,
+    KC_TRNS,         KC_TRNS,       KC_TRNS,        KC_TRNS,         KC_TRNS,
     // left thumb cluster
     KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
 
