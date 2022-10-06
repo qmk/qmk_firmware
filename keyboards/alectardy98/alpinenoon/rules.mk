ef169010892ae6dd001335aa0a0182e2680f6b32
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2022-10-06 19:52:06 +0200

    Update keyboards/alectardy98/alpinenoon/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/alectardy98/alpinenoon/rules.mk
+++ keyboards/alectardy98/alpinenoon/rules.mk
@@ -5,21 +5,15 @@ MCU = atmega32u4
 BOOTLOADER = caterina
 
 # Build Options
-#   comment out to disable the options.
+#   change yes to no to disable
 #
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 
-BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	    # Console for debug
-COMMAND_ENABLE = no     # Commands for debug and configuration
-NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-RGBLIGHT_ENABLE = no   # Enable keyboard underlight functionality
-BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
-MIDI_ENABLE = no 		# MIDI controls
-AUDIO_ENABLE = no      #used to enable the beeper
-UNICODE_ENABLE = yes 		# Unicode
-BLUETOOTH_ENABLE = no # Enable Bluetooth with the Adafruit EZ-Key HID
 LTO_ENABLE = yes  #make the file smaller
 ENCODER_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
