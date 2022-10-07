e639e534aeb08a090049b8e6cfd58bca44991cbd
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2022-10-07 14:42:57 +0200

    needed to add locking support to the .h
    It did not work when trying to add it to the .json

--- keyboards/alectardy98/alpinenoon/config.h
+++ keyboards/alectardy98/alpinenoon/config.h
@@ -14,4 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#include "config_common.h"
+
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
