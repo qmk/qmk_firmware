e6955f87c8f99dbb1d227275a95f53f6dd7e8c33
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2022-10-07 13:49:28 +0200

    console enable yes

--- keyboards/alectardy98/alpinenoon/rules.mk
+++ keyboards/alectardy98/alpinenoon/rules.mk
@@ -10,7 +10,7 @@ BOOTLOADER = caterina
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 
