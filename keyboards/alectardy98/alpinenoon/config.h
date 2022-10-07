eeadd78b559ca9ff8b822822232b440204a592b7
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2022-10-07 11:53:11 +0200

    adding more to .json

--- keyboards/alectardy98/alpinenoon/config.h
+++ keyboards/alectardy98/alpinenoon/config.h
@@ -15,29 +15,3 @@
  */
 
 #pragma once
-
-#include "config_common.h"
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 17
-
-// ROWS: Top to bottom, COLS: Left to right
-
-#define MATRIX_ROW_PINS {D2,D1,D0,B7,B3}
-#define MATRIX_COL_PINS {D5,D3,C7,C6,B6,B5,B4,D7,D6,D4,F0,F1,F4,F5,F6,F7,E6}
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-#define LOCKING_SUPPORT_ENABLE
-#define LOCKING_RESYNC_ENABLE
-
-#define ENCODERS_PAD_A {B1}
-#define ENCODERS_PAD_B {B2}
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
