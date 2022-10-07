ec0405674c113e57e70ffe588835e235e6662473
Richard Dawe <richdawe@gmail.com>
2022-10-07 16:13:14 +0100

    Swap slashes for more ergonomic positions for Unix paths

--- keyboards/dm9records/lain/keymaps/richdawe/keymap.c
+++ keyboards/dm9records/lain/keymaps/richdawe/keymap.c
@@ -30,13 +30,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------'     \-----------------------------------|
  * |LShft|  Z  /  X  /  C  /  V  /  B  /           \  B  \  N  \  M  \  ,  \  .  |ShEnt|
  * `-----------------------------------'           \-----------------------------------|
- *    |LCtrl|  /LGUI /LAlt /SpLSy/SpLSy/           \SpLNu\SpLNu\RAlt \  \  \  /  |  `  |
+ *    |LCtrl|  /LGUI /LAlt /SpLSy/SpLSy/           \SpLNu\SpLNu\RAlt \  /  \  \  |  `  |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         LTF_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SFTENT,
-        KC_LCTL,          KC_LGUI, KC_LALT, LTS_SPC, LTS_SPC,            LTN_SPC, LTN_SPC, KC_RALT, KC_BSLS, KC_SLSH, KC_GRV
+        KC_LCTL,          KC_LGUI, KC_LALT, LTS_SPC, LTS_SPC,            LTN_SPC, LTN_SPC, KC_RALT, KC_SLSH, KC_BSLS, KC_GRV
     ),
 
     [NUM] = LAYOUT(
