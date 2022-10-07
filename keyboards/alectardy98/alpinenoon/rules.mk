b062ebbb06eab6182ce2103a69891f8c6d11b2fb
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2022-10-07 18:13:41 +0200

    psuh

---
+++ keyboards/alectardy98/alpinenoon/rules.mk
@@ -0,0 +1,20 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+
+LTO_ENABLE = yes  #make the file smaller
+ENCODER_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+UNICODE_ENABLE = yes 		# Unicode
