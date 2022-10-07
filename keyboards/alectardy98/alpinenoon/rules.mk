345e39a5592a0027e9ffee73717e1d8bc29f39bc
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2022-10-07 13:50:14 +0200

    mouse key no

--- keyboards/alectardy98/alpinenoon/rules.mk
+++ keyboards/alectardy98/alpinenoon/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = caterina
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
