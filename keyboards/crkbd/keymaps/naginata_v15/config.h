103f5f5b6c45349e97d3eb379e1a9c18a004b748
eswai <>
2022-10-08 09:38:11 +0900

    薙刀式v15更新

---
+++ keyboards/crkbd/keymaps/naginata_v15/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#undef PRODUCT
+#define PRODUCT   Crkbd Naginata Style V15
+
+#define MASTER_LEFT
+#define USE_SERIAL_PD2
+#define SPLIT_USB_DETECT
+
+#define TAPPING_TERM 300
+#define USB_MAX_POWER_CONSUMPTION 100
+
+#define OLED_UPDATE_INTERVAL 100
+#define OLED_TIMEOUT 60000
