6326bf1a11a52bf6468dfdd378d68e132d88e57f
kpagratis <kostas.pagratis@gmail.com>
2022-10-09 10:33:20 -0600

    Update keymap.c

--- keyboards/handwired/dactyl_manuform/5x6_68/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_68/keymaps/default/keymap.c
@@ -4,12 +4,12 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6_68(
-        KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_EQL ,
+        KC_EQL , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                        KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                        KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-        KC_NO  , KC_NO  , KC_LEFT, KC_RGHT,                                                            KC_DOWN, KC_UP  , KC_LBRC, KC_RBRC,
-                                            KC_NO  , KC_SPC , KC_LGUI,       KC_PGUP,KC_ENT , KC_DEL ,
-                                            KC_LALT, KC_LCTL, KC_NO  ,       KC_PGDN,KC_BSPC, KC_LALT
+        KC_GRV , KC_NO  , KC_LEFT, KC_RGHT,                                                            KC_DOWN, KC_UP  , KC_LBRC, KC_RBRC,
+                                            KC_NO  , KC_LCTL, KC_LGUI,       KC_PGUP,KC_BSPC, KC_DEL ,
+                                            KC_LALT, KC_SPC , KC_NO  ,       KC_PGDN,KC_ENT , KC_LALT
     )
 };
