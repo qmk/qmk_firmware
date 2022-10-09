cd56a37272ccf72f31b72049e7d2f7d9d9db1be0
Richard Dawe <richdawe@gmail.com>
2022-10-09 22:14:11 +0100

    Move cursors from IJKL to IJKM

--- keyboards/dm9records/lain/keymaps/richdawe/keymap.c
+++ keyboards/dm9records/lain/keymaps/richdawe/keymap.c
@@ -44,17 +44,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.     ,-----------------------------------.
  * |     |     /  1  /  2  /  3  /  :  /  (  /     \  -  \  =  \ Up  \  0  \  .  |Bspc |
  * |-----------------------------------------/     \-----------------------------------|
- * |     |     /  4  /  5  /  6  /  .  /  )  /     \     \Left \Down \Right\     |     |
+ * |     |     /  4  /  5  /  6  /  .  /  )  /     \     \Left \Right\     \     |     |
  * |-----------------------------------------'     \-----------------------------------|
- * |     |     /  7  /  8  /  9  /  0  /           \     \     \     \     \     |     |
+ * |     |     /  7  /  8  /  9  /  0  /           \     \     \Down \     \     |     |
  * `-----------------------------------'           \-----------------------------------|
  *    |     |  /     /     /  -  /  -  /           \     \     \     \     \     |     |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
-        _______, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_COLON, KC_LPRN,   KC_MINS, KC_EQL,  KC_UP,   KC_0,    KC_DOT,  KC_BSPC,
-        _______, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_DOT,   KC_RPRN,   XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_0,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        _______,          _______, _______, KC_MINS, KC_MINS,             _______, _______, _______, _______, XXXXXXX, XXXXXXX
+        _______, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_COLON, KC_LPRN,   KC_MINS, KC_EQL,  KC_UP,    KC_0,    KC_DOT,  KC_BSPC,
+        _______, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_DOT,   KC_RPRN,   XXXXXXX, KC_LEFT, KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_0,                XXXXXXX, XXXXXXX, KC_DOWN,  XXXXXXX, XXXXXXX, _______,
+        _______,          _______, _______, KC_MINS, KC_MINS,             _______, _______, _______,  _______, XXXXXXX, XXXXXXX
     ),
 
     [SYM] = LAYOUT(
@@ -62,16 +62,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.     ,-----------------------------------.
  * |     |     /  !  /  @  /  #  /     /  (  /     \  _  \  +  \PgUp \     \     |Bspc |
  * |-----------------------------------------/     \-----------------------------------|
- * |     |     /  $  /  %  /  ^  /     /  )  /     \     \Home \PgDn \ End \     |     |
+ * |     |     /  $  /  %  /  ^  /     /  )  /     \     \Home \ End \     \     |     |
  * |-----------------------------------------'     \-----------------------------------|
- * |     |     /  &  /  *  /  (  /  )  /           \     \     \     \     \     |     |
+ * |     |     /  &  /  *  /  (  /  )  /           \     \     \PgDn \     \     |     |
  * `-----------------------------------'           \-----------------------------------|
  *    |     |  /     /     /     /     /           \  -  \  -  \     \     \     |     |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         _______, XXXXXXX, S(KC_1), S(KC_2), S(KC_3), XXXXXXX, KC_LPRN,   S(KC_MINS), S(KC_EQL), KC_PGUP, XXXXXXX, XXXXXXX, KC_BSPC,
-        _______, XXXXXXX, S(KC_4), S(KC_5), S(KC_6), XXXXXXX, KC_RPRN,   XXXXXXX,    KC_HOME,   KC_PGDN, KC_END,  XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, S(KC_7), S(KC_8), S(KC_9), S(KC_0),            XXXXXXX,    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, XXXXXXX, S(KC_4), S(KC_5), S(KC_6), XXXXXXX, KC_RPRN,   XXXXXXX,    KC_HOME,   KC_END,  XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, S(KC_7), S(KC_8), S(KC_9), S(KC_0),            XXXXXXX,    XXXXXXX,   KC_PGDN, XXXXXXX, XXXXXXX, _______,
         _______,          _______, _______, _______, _______,            KC_MINS,    KC_MINS,   _______, _______, XXXXXXX, XXXXXXX
     ),
 
@@ -80,16 +80,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.     ,-----------------------------------.
  * |     |     / F1  / F2  / F3  /     /     /     \     \     \VolUp\     \     | Del |
  * |-----------------------------------------/     \-----------------------------------|
- * |CapsL|LEDEn/ F4  / F5  / F6  /     /     /     \     \MPrev\VolDn\MNext\     |     |
+ * |CapsL|LEDEn/ F4  / F5  / F6  /     /     /     \     \MPrev\MNext\     \     |     |
  * |-----------------------------------------'     \-----------------------------------|
- * |     |     / F7  / F8  / F9  /     /           \     \     \VMute\     \     |     |
+ * |     |     / F7  / F8  / F9  /     /           \     \VMute\VolDn\     \     |     |
  * `-----------------------------------'           \-----------------------------------|
  *    |Bootl|  / F10 / F11 / F12 /MPlay/           \MPlay\MPlay\     \     \     |     |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, KC_VOLU, XXXXXXX, XXXXXXX, KC_DEL,
-        KC_CAPS, LED_EN,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX,   XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT, XXXXXXX, _______,
-        _______, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,            XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, _______,
+        KC_CAPS, LED_EN,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX,   XXXXXXX, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX, _______,
+        _______, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,            XXXXXXX, KC_MUTE, KC_VOLD, XXXXXXX, XXXXXXX, _______,
         QK_BOOT,          KC_F10,  KC_F11,  KC_F12,  KC_MPLY,            KC_MPLY, KC_MPLY, _______, _______, XXXXXXX, XXXXXXX
     ),
 };
