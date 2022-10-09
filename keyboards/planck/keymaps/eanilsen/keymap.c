94164f5cd95596df2421469cae69f4162bb42efe
Omni801 <omni801@protonmail.com>
2022-10-09 12:16:22 +0200

    RSWPAPP

--- keyboards/planck/keymaps/eanilsen/keymap.c
+++ keyboards/planck/keymaps/eanilsen/keymap.c
@@ -23,6 +23,7 @@ enum planck_layers {
 #define HOME TO(_ISRT)
 #define MTLCTL MT(MOD_LCTL,KC_T)
 #define MTRCTL MT(MOD_RCTL,KC_N)
+#define RSWPAPP LT(0,KC_R)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ISRT] = LAYOUT_ortho_4x12(
     KC_TAB,  KC_Y,    KC_C,    KC_L,    KC_M,   KC_K,   KC_Z,     KC_F,    KC_U,              KC_COMM, KC_QUOT, KC_DEL,
-    KC_ESC,  KC_I,    KC_S,    KC_R,    MTLCTL, KC_G,   KC_P,     MTRCTL,  MT(MOD_RALT,KC_E), KC_A,    KC_O,    KC_ENT,
+    KC_ESC,  KC_I,    KC_S,    RSWPAPP, MTLCTL, KC_G,   KC_P,     MTRCTL,  MT(MOD_RALT,KC_E), KC_A,    KC_O,    KC_ENT,
     KC_LSFT, KC_Q,    KC_V,    KC_W,    KC_D,   KC_J,   KC_B,     KC_H,    KC_SLSH,           KC_DOT,  KC_X,    KC_RSFT,
     CAPSWRD, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, SYMBOL, FUNCTION, KC_BSPC, NAV,               KC_LGUI, KC_LEFT, KC_RGHT
     ),
@@ -73,13 +74,25 @@ bool is_shift_held(void) { return (get_mods() & MOD_BIT(KC_LSFT)) || (get_mods()
 bool is_ctrl_held(void) { return get_mods() & MOD_BIT(KC_LCTL); }
 bool is_gui_held(void) { return get_mods() & MOD_BIT(KC_LGUI); }
 
-void send_mac_or_win(uint16_t mac_code, uint16_t win_code, bool is_pressed) {
+void send_mac_or_win(uint16_t mac_code, uint16_t win_code, bool is_pressed)
+{
   uint16_t code = is_mac_the_default() ? mac_code : win_code;
   if (is_pressed) {
     register_code16(code);
   } else {
     unregister_code16(code);
   }
+}
+
+static bool process_tap_or_long_press_key(keyrecord_t *record, uint16_t long_press_keycode)
+{
+  if (!record->tap.count) { // Key is being held
+    if (record->event.pressed) {
+      tap_code16(long_press_keycode);
+    }
+    return false;
+  }
+  return true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
@@ -92,16 +105,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
   /* } */
 
   switch (keycode) {
-  case LT(0,KC_R): {
-    uint16_t code = is_mac_the_default() ? G(KC_TAB) : A(KC_TAB);
-    if (isHeld) {
-      register_code16(code);
-    } else if (!record->tap.count) {
-      unregister_code16(code);
-      return false;
+  case RSWPAPP:
+    if (!record->tap.count) {
+      if (isPressed) {
+	tap_code16(KC_R);
+      } else {
+	uint16_t code = is_mac_the_default() ? G(KC_TAB) : A(KC_TAB);
+	if (isPressed) {
+	  register_code16(code);
+	} else {
+	  unregister_code16(code);
+	}
+      }
     }
-    return true;
-  }
+    return false;
   case LT(0,KC_SCLN):
     if (isHeld) {
       tap_code16(KC_COLN);
