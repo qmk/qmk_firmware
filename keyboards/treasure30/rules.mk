bad81400788a73af7d0e9ed22616d67f9096fd22
arnstadm <arnstad@gmail.com>
2022-10-09 08:17:44 +0200

    removed folder

--- keyboards/treasure30/rules.mk
+++
@@ -1,8 +0,0 @@
-# This file intentionally left blank
-# MCU name
-
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
