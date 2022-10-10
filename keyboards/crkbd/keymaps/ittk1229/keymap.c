070248611761baea474f54f001f4753013f8c4ee
ittk1229 <ittk1229@gmail.com>
2022-10-10 11:35:54 +0900

    ESC_MHENが2回入力される問題を対処

--- keyboards/crkbd/keymaps/ittk1229/keymap.c
+++ keyboards/crkbd/keymaps/ittk1229/keymap.c
@@ -199,6 +199,7 @@ static uint16_t  ctl_spc_pressed_time = 0;
 static uint16_t sft_ent_pressed_time = 0;
 
 // マウス用に長押しで通常の修飾キーに
+// 逆にスペース、エンターの押しっぱなしができなくなるのか
 void matrix_scan_user(void) {
   if (ctl_spc_pressed && timer_elapsed(ctl_spc_pressed_time) > TAPPING_TERM) {
     register_code(KC_LCTL);
@@ -251,25 +252,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
     case ESC_MHEN:
-      register_code(KC_ESC);
-      unregister_code(KC_ESC);
-      register_code(KC_MHEN);
-      unregister_code(KC_MHEN);
+      if (record->event.pressed){
+        tap_code(KC_ESC);
+        tap_code(KC_MHEN);
+      }
       return false;
       break;
     case LOWER:
       if (record->event.pressed) {
         lower_pressed = true;
-
         layer_on(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-
         if (lower_pressed) {
-          register_code(KC_MHEN);
-          unregister_code(KC_MHEN);
+          tap_code(KC_MHEN);
         }
         lower_pressed = false;
       }
@@ -278,16 +276,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case RAISE:
       if (record->event.pressed) {
         raise_pressed = true;
-
         layer_on(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-
         if (raise_pressed) {
-          register_code(KC_HENK);
-          unregister_code(KC_HENK);
+          tap_code(KC_HENK);
         }
         raise_pressed = false;
       }
