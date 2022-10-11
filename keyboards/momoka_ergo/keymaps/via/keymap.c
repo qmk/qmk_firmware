798a0dd609bf4d79862e79fb5364cd68f47e7a58
Babuki <kevin.boss@outlook.com>
2022-10-11 22:18:50 +0200

    Updated license

--- keyboards/momoka_ergo/keymaps/via/keymap.c
+++ keyboards/momoka_ergo/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 StefanGrindelwald
+/* Copyright 2022 Kevin Boss <kevin.boss@outlook.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
