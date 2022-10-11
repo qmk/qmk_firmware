2700c42bcee046cf165463149c3b09b627857a51
IFo Hancroft <contact@ifohancroft.com>
2022-10-11 21:56:00 +0300

    Testing stuff

--- keyboards/hotdox76v2/rules.mk
+++ keyboards/hotdox76v2/rules.mk
@@ -15,15 +15,15 @@ CONSOLE_ENABLE = no	# Console for debug(+400)
 COMMAND_ENABLE = no    # Commands for debug and configuration
 NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 
-OLED_ENABLE = yes    # Enable Support for SSD1306 or SH1106 OLED Displays; Communicating over I2C
-OLED_DRIVER = SSD1306
+#OLED_ENABLE = yes    # Enable Support for SSD1306 or SH1106 OLED Displays; Communicating over I2C
+#OLED_DRIVER = SSD1306
 
 LTO_ENABLE = yes
 
 # NO_USB_STARTUP_CHECK = yes
 
-RGBLIGHT_ENABLE = yes
-RGBLIGHT_DRIVER = WS2812
+#RGBLIGHT_ENABLE = yes
+#RGBLIGHT_DRIVER = WS2812
 # RGB_MATRIX_ENABLE = yes
 # RGB_MATRIX_DRIVER = WS2812
 SPLIT_KEYBOARD = yes
