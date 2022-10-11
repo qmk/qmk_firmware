24644165e5ca98338ce6b17a11419bf0763818ec
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2022-10-11 10:34:54 +0200

    Update keyboards/alectardy98/alpinenoon/keymaps/alec/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/alectardy98/alpinenoon/keymaps/alec/keymap.c
+++ keyboards/alectardy98/alpinenoon/keymaps/alec/keymap.c
@@ -175,10 +175,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT(
-     QUIT,    TASK,  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, _______, KC_BSPC, KC_MPLY,  \
-  DM_PLY1, DM_PLY2,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,TG(_NUM),  \
-     SNIP,    EXPL, KC_LCAP,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,   KC_NO,  KC_ENT,  KC_DEL,  \
-     COPY,    PAST, KC_LSFT,   KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,   KC_UP,MO(_FRONT),\
+     QUIT,    TASK,  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, _______, KC_BSPC, KC_MPLY,
+  DM_PLY1, DM_PLY2,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,TG(_NUM),
+     SNIP,    EXPL, KC_LCAP,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,   KC_NO,  KC_ENT,  KC_DEL,
+     COPY,    PAST, KC_LSFT,   KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,   KC_UP,MO(_FRONT),
      VDLT,    VDRT, KC_LCTL, MO(_FN), KC_LALT,                             KC_SPC,                   KC_RALT, KC_GRV,             KC_LEFT, KC_DOWN,KC_RGHT),
     
 [_NUM] = LAYOUT(
