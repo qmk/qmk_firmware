e9cf3d0a77b4445278bfb41224ee85d2a9010ef2
jack <0x6A73@pm.me>
2022-10-12 14:10:38 -0600

    check for user function

--- keyboards/keychron/q3/q3.c
+++ keyboards/keychron/q3/q3.c
@@ -73,6 +73,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) { return false; }
     // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
 
     if (host_keyboard_led_state().caps_lock) {
