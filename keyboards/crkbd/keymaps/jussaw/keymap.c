1b68c3d0835cce8b0fe757054b8a98bb435cf9af
Justin Sawyer <jtsaw36@gmail.com>
2022-10-11 18:11:03 -0600

    Adjusts columns in Game layer

--- keyboards/crkbd/keymaps/jussaw/keymap.c
+++ keyboards/crkbd/keymaps/jussaw/keymap.c
@@ -137,18 +137,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Game
    * ,-----------------------------------------------------.                    ,-----------------------------------------------------.
-   * |        |  Tab   |    Q   |    W   |    E   |    R   |                    |    T   |    Y   |    U   |    I   |    O   |    P   |
+   * |    T   |  Tab   |    Q   |    W   |    E   |    R   |                    |    Y   |    U   |    I   |    O   |    P   |  Bksp  |
    * |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   * |        |  Shift |    A   |    S   |    D   |    F   |                    |    G   |    H   |    J   |    K   |    L   |    ;   |
+   * |    G   |  Shift |    A   |    S   |    D   |    F   |                    |    H   |    J   |    K   |    L   |    ;   |   ""   |
    * |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   * |        |  Ctrl  |    Z   |    X   |    C   |    V   |                    |    B   |    N   |    M   |    ,   |    .   |    /   |
+   * |    B   |  Ctrl  |    Z   |    X   |    C   |    V   |                    |    N   |    M   |    ,   |    .   |    /   | Enter  |
    * |--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-   *                                     |   Esc  |  Alt   | Space  |  |        |        | 
+   *                                     |   Esc  |  Alt   | Space  |  |        |        |TG(GAME)|
+   *                                     `--------------------------'  `--------------------------'
    */
   [_GAME] = LAYOUT_split_3x6_3(
-      XXXXXXX,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-      XXXXXXX,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,                         KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-      XXXXXXX,  KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,                         KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+         KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+         KC_G,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+         KC_B,  KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                                            KC_ESC, KC_LALT, KC_SPC,     XXXXXXX, XXXXXXX,    TG(_GAME)
   ),
 };
