2160967e49fcca93a727013155cbfb25721a87fe
jack <0x6A73@pm.me>
2022-10-13 15:37:17 -0600

    undo

--- keyboards/momoka_ergo/config.h
+++ keyboards/momoka_ergo/config.h
@@ -26,6 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
+#define SELECT_SOFT_SERIAL_SPEED 5
+
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
