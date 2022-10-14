84d40fad4f5ca4e401b648c32df653810a98170b
Daniel Reibl <janos.daniel.reibl@protonmail.com>
2022-10-14 17:38:02 +0200

    fix: Remove OLED support

--- keyboards/handwired/riblee_f411/halconf.h
+++ keyboards/handwired/riblee_f411/halconf.h
@@ -21,7 +21,5 @@
 
 #pragma once
 
-#define HAL_USE_I2C TRUE
-
 #include_next <halconf.h>
 
--- keyboards/handwired/riblee_f411/mcuconf.h
+++ keyboards/handwired/riblee_f411/mcuconf.h
@@ -17,6 +17,3 @@
 #pragma once
 
 #include_next "mcuconf.h"
-
-#undef STM32_I2C_USE_I2C1
-#define STM32_I2C_USE_I2C1 TRUE
