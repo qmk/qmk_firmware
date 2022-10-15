91e809378f5e2c139049405928fe9e48e2d2d977
jack <0x6A73@pm.me>
2022-10-15 13:54:25 -0600

    initial

---
+++ keyboards/work_louder/micro/keymaps/via/config.h
@@ -0,0 +1,7 @@
+// Copyright 2022 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#undef ENABLE_RGB_MATRIX_PIXEL_RAIN
+#undef ENABLE_RGB_MATRIX_PIXEL_FLOW
+#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
