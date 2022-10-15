d2d404c2b0b3936daa321348f0d820d6419ccd60
Nicola Lombardi <lombardi.nicola23@gmail.com>
2022-10-15 02:33:54 +0200

    swap arrow up with del

--- keyboards/xiudi/xd60/keymaps/nicola_lombardi/keymap.c
+++ keyboards/xiudi/xd60/keymaps/nicola_lombardi/keymap.c
@@ -32,16 +32,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,     KC_3,  KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_NO,  KC_NO,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,     KC_E,  KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_NO,  KC_NO, KC_BSLS,
         KC_CAPS, KC_A,    KC_S,     KC_D,  KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,  KC_NO,   KC_ENT,
-        KC_LSFT, KC_NO,   KC_Z,     KC_X,  KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT, KC_DEL,
-        KC_LCTL, KC_LGUI, KC_LALT,         MO(1),    KC_SPC,     KC_RCTL, MO(2),   KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT),
+        KC_LSFT, KC_NO,   KC_Z,     KC_X,  KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT, KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,         MO(1),    KC_SPC,     KC_RCTL, MO(2),   KC_DEL,  KC_LEFT, KC_DOWN,   KC_RGHT),
 
     // 1: Function Layer
     LAYOUT_split_spacebar(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC,
         KC_TAB, LSFT(KC_LBRC), LSFT(KC_RBRC), KC_LBRC, KC_RBRC, LCTL(LSFT(KC_F5)), KC_NO, KC_HOME, KC_UP, KC_END, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_CAPS, KC_MINS, LSFT(KC_MINS), KC_EQL, LSFT(KC_EQL), LCTL(LSFT(KC_F10)), KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_MINS, KC_EQL, KC_NO, KC_ENT,
-        KC_LSFT, KC_NO, KC_F12, LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RIGHT)), KC_NO, KC_NO, KC_NO, KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT, KC_DEL,
-        KC_LCTL, KC_LGUI, KC_LALT,       KC_TRNS, KC_SPC,         KC_RCTL, KC_NO,  KC_LEFT,  KC_DOWN, KC_UP, TD(TD_RIGHT_GAMINGPROFILE)),
+        KC_LSFT, KC_NO, KC_F12, LCTL(LGUI(KC_LEFT)), LCTL(LGUI(KC_RIGHT)), KC_NO, KC_NO, KC_NO, KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT, KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,       KC_TRNS, KC_SPC,         KC_RCTL, KC_NO,  KC_DEL,  KC_LEFT, KC_DOWN, TD(TD_RIGHT_GAMINGPROFILE)),
 
     // 2: Extra functions layer
     LAYOUT_split_spacebar(
@@ -56,6 +56,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,     KC_3,  KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,     KC_E,  KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,
         KC_CAPS, KC_A,    KC_S,     KC_D,  KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,  KC_NO,   KC_ENT,
-        KC_LSFT, KC_NO,   KC_Z,     KC_X,  KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT, KC_DEL,
-        KC_LCTL, KC_LGUI, KC_LALT,         KC_SPC,    KC_SPC,     KC_RCTL, KC_RALT,   KC_LEFT,  KC_DOWN, KC_UP,   TD(TD_RIGHT_GAMINGPROFILE)),
+        KC_LSFT, KC_NO,   KC_Z,     KC_X,  KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT, KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,         KC_SPC,    KC_SPC,     KC_RCTL, KC_RALT,   KC_DEL,  KC_LEFT, KC_DOWN,   TD(TD_RIGHT_GAMINGPROFILE)),
 };
