4cfc125a8ae0ac7ce393d1451e1bce10a4999568
jack <0x6a73@protonmail.com>
2022-10-15 14:25:07 -0600

    Update keyboards/work_louder/micro/keymaps/via/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/work_louder/micro/keymaps/via/config.h
+++ keyboards/work_louder/micro/keymaps/via/config.h
@@ -2,6 +2,4 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-#undef ENABLE_RGB_MATRIX_PIXEL_RAIN
-#undef ENABLE_RGB_MATRIX_PIXEL_FLOW
-#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+#undef RGBLIGHT_EFFECT_TWINKLE
