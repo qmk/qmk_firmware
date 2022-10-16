d75aa3cbd59a1ce42e6fe3809e75074bd81879a3
Controller Works <info@controller.works>
2022-10-15 21:30:13 -0400

    Removed mini36.h

--- keyboards/controllerworks/mini36/mini36.c
+++ keyboards/controllerworks/mini36/mini36.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "mini36.h"
+#include "quantum.h"
 
 #ifdef OLED_ENABLE
 
