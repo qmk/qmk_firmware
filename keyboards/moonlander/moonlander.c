2e793318c68f775596414fdb4b2d0a605c43fbe2
Jouke Witteveen <j.witteveen@gmail.com>
2022-10-16 04:31:51 +0200

    Mark the moonlander keyboard default music map as weak (#18715)
    This allows user keymaps to set a different music map.

--- keyboards/moonlander/moonlander.c
+++ keyboards/moonlander/moonlander.c
@@ -358,6 +358,7 @@ void keyboard_post_init_kb(void) {
 
 #if defined(AUDIO_ENABLE) && defined(MUSIC_MAP)
 // clang-format off
+__attribute__ ((weak))
 const uint8_t music_map[MATRIX_ROWS][MATRIX_COLS] = LAYOUT_moonlander(
     58, 59, 60, 61, 62, 63, 64,    65, 66, 67, 68, 69, 70, 71,
     44, 45, 46, 47, 48, 49, 50,    51, 52, 53, 54, 55, 56, 57,
