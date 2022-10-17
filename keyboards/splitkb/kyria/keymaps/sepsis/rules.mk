79a75705495e5bd0043b6ccf4c76238ce265acf1
sepsis1 <sepp66@live.de>
2022-10-17 19:01:47 +0200

    kyria

---
+++ keyboards/splitkb/kyria/keymaps/sepsis/rules.mk
@@ -0,0 +1,6 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pimoroni_trackball
