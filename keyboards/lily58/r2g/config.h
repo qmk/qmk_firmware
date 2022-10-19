01db12ed66a8e0f40c28ec59bfa2028945867f68
Elliot Powell <johnp8727@gmail.com>
2022-10-19 16:35:41 +0800

    add max brightness

--- keyboards/lily58/r2g/config.h
+++ keyboards/lily58/r2g/config.h
@@ -50,5 +50,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
-
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
+#define ENABLE_RGB_MATRIX_ALPHAS_MODS
 #define ENABLE_RGB_MATRIX_BREATHING
