28c66bcec672fe82d6de08aba189d808f1bb8760
PHSC138 <PHSC138@users.noreply.github.com>
2022-10-18 21:53:56 -0400

    Current atom47

---
+++ keyboards/evyd13/atom47/keymaps/PHSC138/config.h
@@ -0,0 +1,6 @@
+#ifndef PHSC138_CONFIG_H
+#define PHSC138_CONFIG_H
+
+#define TAPPING_TERM 150 // Tap dance time limit
+
+#endif
---
+++ keyboards/evyd13/atom47/keymaps/PHSC138/rules.mk
@@ -0,0 +1,16 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+RGBLIGHT_ENABLE = no        # Enable keyboard underlight functionality (+4870)
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+MIDI_ENABLE = no            # Midi
+AUDIO_ENABLE = no           # Audio output
+UNICODE_ENABLE  = no        # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+TAP_DANCE_ENABLE = yes      # Enable single keys to have multiple 
