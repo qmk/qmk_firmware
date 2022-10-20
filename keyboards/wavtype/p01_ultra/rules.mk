718ed356678988e00d3891ab0e9836fe250964e6
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-10-20 01:27:22 -0700

    update Community Layout support

--- keyboards/wavtype/p01_ultra/rules.mk
+++ keyboards/wavtype/p01_ultra/rules.mk
@@ -17,4 +17,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-LAYOUTS = tkl_ansi tkl_ansi_tsangan tkl_iso tkl_iso_tsangan
+LAYOUTS = tkl_ansi tkl_ansi_split_bs_rshift tkl_ansi_tsangan tkl_ansi_tsangan_split_bs_rshift tkl_iso tkl_iso_split_bs_rshift tkl_iso_tsangan tkl_iso_tsangan_split_bs_rshift
