fc9037a45a75c4bfd91797d93513684160dbe149
fauxpark <fauxpark@gmail.com>
2022-10-21 15:08:40 +1100

    Deprecate `KC_LOCK` for `QK_LOCK`

--- keyboards/dz60/keymaps/split_space_arrows/rules.mk
+++ keyboards/dz60/keymaps/split_space_arrows/rules.mk
@@ -4,4 +4,4 @@ BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-KEY_LOCK_ENABLE = yes       # Enables use of KC_LOCK
+KEY_LOCK_ENABLE = yes
