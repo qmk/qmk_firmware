18f935a39403471b447fa9c355bee1b019bd1ded
Drashna Jael're <drashna@live.com>
2022-10-21 11:55:23 -0700

    Make license headers consistent

--- keyboards/hotdox76v2/config.h
+++ keyboards/hotdox76v2/config.h
@@ -1,3 +1,4 @@
+// Copyright 2021 JasonRen(biu)
 // Copyright 2022 Drashna Jael're (@Drashna Jael're)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
--- keyboards/hotdox76v2/keymaps/default/keymap.c
+++ keyboards/hotdox76v2/keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 // Copyright 2021 JasonRen(biu)
+// Copyright 2022 Drashna Jael're (@Drashna Jael're)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
