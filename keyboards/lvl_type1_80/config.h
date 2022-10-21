b7aeb008023f182174d8a3fb86c443db48876c81
yiancar <yiancar@gmail.com>
2022-10-21 15:58:15 +0100

    White

--- keyboards/lvl_type1_80/config.h
+++ keyboards/lvl_type1_80/config.h
@@ -51,6 +51,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_DEFAULT_SAT 0
 #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
 #define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
 #define RGBLIGHT_EFFECT_BREATHING
