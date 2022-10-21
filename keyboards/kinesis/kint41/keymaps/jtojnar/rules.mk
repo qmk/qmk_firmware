291566e9beedfda5b6e682a21e50fb927fe21258
Jan Tojnar <jtojnar@gmail.com>
2022-10-21 15:59:39 +0200

    keymaps: Add jtojnar’s kinesis keymap for KinT
    - Add usage instructions.
    - Move layout schema to README.
    - Align keymaps more nicely compared to `default`.
    - Use enum for layers.
    - Add comments separating keymap sections.
    - Move comments in rules.mk to separate line.

---
+++ keyboards/kinesis/kint41/keymaps/jtojnar/rules.mk
@@ -0,0 +1,18 @@
+# Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = no
+# Mouse keys
+MOUSEKEY_ENABLE = no
+# Audio control and System control
+EXTRAKEY_ENABLE = yes
+# Console for debug
+CONSOLE_ENABLE = no
+# Commands for debug and configuration
+COMMAND_ENABLE = yes
+# N-Key rollover
+NKRO_ENABLE = yes
+# Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no
+# Audio output
+AUDIO_ENABLE = no
+# Enable WS2812 RGB underlight
+RGBLIGHT_ENABLE = no
