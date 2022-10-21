ba7ff7287e0f60d9eef4432f9dac7cc4cf2b0e46
Dor Zvulun <zdori4@gmail.com>
2022-10-21 10:35:17 +0300

    added N-Key rollover in rules.mk, added window control for 1/4, 3/4, 4/4 and center 1/2

--- keyboards/sofle/keymaps/rgb_default/rules.mk
+++ keyboards/sofle/keymaps/rgb_default/rules.mk
@@ -1,8 +1,10 @@
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = yes
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes		# Audio control and System control
 ENCODER_ENABLE = yes
 LTO_ENABLE = yes
 # TAP_DANCE_ENABLE = yes
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 
 # RGB Configurations
 RGBLIGHT_ENABLE = yes
@@ -11,6 +13,12 @@ RGBLIGHT_ENABLE = yes
 
 # VIA Support created problems with default layers nooked the keyboard
 VIA_ENABLE = no
-CONSOLE_ENABLE = no
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
+
+
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+
+AUDIO_ENABLE = no           # Audio output
