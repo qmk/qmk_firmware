461aff244c92955b6bb1ae7f1a0f28d745f72ae1
vii33 <1887246+vii33@users.noreply.github.com>
2022-10-21 23:02:40 +0200

    stable alpha

--- keyboards/lily58/keymaps/lily58l_vii33/config.h
+++ keyboards/lily58/keymaps/lily58l_vii33/config.h
@@ -60,7 +60,7 @@ use define EE_HANDS at flashing time*/
 
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-//#define SPLIT_USB_DETECT
-//#define NO_USB_STARTUP_CHECK
+#define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
 
 
