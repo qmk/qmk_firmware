875aa447be6106d4f9804ae3d50dfd7536cd2712
Franz Miltz <franz@miltz.me>
2022-10-22 10:53:13 +0100

    Fixed iris

--- keyboards/keebio/iris/keymaps/miltfra/keymap.c
+++ keyboards/keebio/iris/keymaps/miltfra/keymap.c
@@ -13,54 +13,50 @@ enum custom_keycodes {
   ADJUST,
 };
 
-#define KC_LBE KC_LEFT_CURLY_BRACE
-#define KC_RBE KC_RIGHT_CURLY_BRACE
 #define KC_LBK KC_LBRACKET
 #define KC_RBK KC_RBRACKET
-
-// clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
       //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+         XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX,    KC_A,    KC_S,   KC_D,    KC_F,     KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
+         XXXXXXX, KC_A,    KC_S,   KC_D,    KC_F,     KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,          XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,          XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
       //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-      MT(MOD_LCTL,KC_ESC),                       LOWER,   KC_SPC,                    MT(MOD_LSFT,KC_ENT), RAISE, MOD_LALT
-      // -                             └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+                                        KC_LCTL, LOWER,   KC_SPC,                    KC_LSFT,RAISE,   KC_LALT
+      //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
       ),
 
   [_LOWER] = LAYOUT(
-      //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      //┌────────┬───────┬────────┬────────┬────────┬─────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+         XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX, KC_GRAVE,KC_EQUAL, XXXXXXX,  KC_LBK,  KC_RBK,                              KC_BSLS, KC_TAB, XXXXXXX, KC_MINUS,KC_QUOT, XXXXXXX,
+         XXXXXXX, KC_GRAVE,KC_EQUAL,KC_ESC,  KC_LBK,  KC_RBK,                             KC_BSLS, KC_ENT,  KC_TAB, KC_MINUS,KC_QUOT, XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX,
       //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-      XXXXXXX,                                   XXXXXXX, XXXXXXX,                   MOD_LSFT, XXXXXXX, LSFT(MOD_LALT)
-      // -                             └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+                                        XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LSFT, XXXXXXX, LSFT(KC_LALT)
+      //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
       ),
 
   [_RAISE] = LAYOUT(
       //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
+         XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
       //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX, KC_F12, XXXXXXX, KC_BSPC, KC_DEL, XXXXXXX,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_F11,  XXXXXXX,
-      //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,LCA(KC_DEL),XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+         XXXXXXX, KC_F12,  XXXXXXX, KC_BSPC, KC_DEL, XXXXXXX,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_F11,  XXXXXXX,
+      //├────────┼────────┼────────┼─────-──┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,LCA(KC_DEL),XXXXXXX,        XXXXXXX, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
       //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-      KC_DEL,                                    KC_LWIN, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
+                                        KC_LCTL, KC_LWIN, KC_SPC,                   XXXXXXX, XXXXXXX, XXXXXXX
       // -                             └────────┴────────┴────────┘                 └────────┴────────┴────────┘
       ),
 
@@ -79,10 +75,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       )
 };
 
-// clang-format on
-
-#undef KC_LBE
-#undef KC_RBE
 #undef KC_LBK
 #undef KC_RBK
 
