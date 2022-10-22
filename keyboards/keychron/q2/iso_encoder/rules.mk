78afda60e4aafee0980a8db8899f5f06258f2674
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-10-22 11:08:15 +0800

    Delete "LTO_ENABLE = yes"

--- keyboards/keychron/q2/iso_encoder/rules.mk
+++ keyboards/keychron/q2/iso_encoder/rules.mk
@@ -20,7 +20,6 @@ ENCODER_ENABLE = yes        # Enable Encoder
 DIP_SWITCH_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = CKLED2001
-LTO_ENABLE = yes
 EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = embedded_flash
 
