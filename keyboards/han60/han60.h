50a3bf28e37f02b846e9ce58766a8a3cb7f227a4
James Young <18669334+noroadsleft@users.noreply.github.com>
2022-10-22 15:13:56 -0700

    refactor LAYOUT_tsangan_iso_arrowkeys into LAYOUT_60_iso_arrow_tsangan
    Moves position `K2C` (Enter) to the home row.

--- keyboards/han60/han60.h
+++ keyboards/han60/han60.h
@@ -102,10 +102,10 @@
     { K40, K41, K42, K43, K44, XXX, XXX, K47, XXX, K49, XXX, K4B, XXX, XXX }  \
 }
 
-#define LAYOUT_tsangan_iso_arrowkeys( \
+#define LAYOUT_60_iso_arrow_tsangan( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K2C, \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K1D, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K1D, K2C, \
     K30, K40, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3B, K3D, K4D, \
     K41, K42, K43,                K44,                K47, K48, K4A, K4C  \
 ) { \
