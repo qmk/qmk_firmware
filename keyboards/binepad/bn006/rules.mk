29927ae12cca4d27e544097633665bc7cdad0f2d
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-10-22 23:11:26 +1100

    Binepad BN006 keyboard code

---
+++ keyboards/binepad/bn006/rules.mk
@@ -0,0 +1,8 @@
+# Copyright 2022 Binepad (@binpad)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+EPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
+
+RGB_MATRIX_ENABLE = yes 
+RGB_MATRIX_DRIVER = WS2812
