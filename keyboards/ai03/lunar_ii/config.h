74b59c998afc0a142015f92529fb9b0d6ef3c36c
ai03 <admin@ai03.com>
2022-10-23 18:33:52 +0900

    Update copyright notices from 2 years ago

--- keyboards/ai03/lunar_ii/config.h
+++ keyboards/ai03/lunar_ii/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 ai03 Design Studio
+Copyright 2022 ai03 Design Studio
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- keyboards/ai03/lunar_ii/keymaps/default/keymap.c
+++ keyboards/ai03/lunar_ii/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Ryota Goto
+/* Copyright 2022 ai03 Design Studio
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
