bf758f471aab3dd62d4212dedcbeb4012e6c7170
Markus Korn <markus.korn@gmail.com>
2022-10-23 09:28:44 +0200

    Update keyboards/gmmk/pro/rev1/ansi/keymaps/andrebrait/config.h
    Commit suggested change
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/gmmk/pro/rev1/ansi/keymaps/andrebrait/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/andrebrait/config.h
@@ -16,11 +16,6 @@
 
 #pragma once
 
-/*
- * #undef DEBOUNCE
- * #define DEBOUNCE 40
- */
-
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_DISABLE_TIMEOUT 1200000     // 20 minutes (20 * 60 * 1000ms)
     #define RGB_DISABLE_WHEN_USB_SUSPENDED
