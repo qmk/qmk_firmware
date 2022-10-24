9b12860b53316c14ab7367debc07678f5b8a1798
fauxpark <fauxpark@gmail.com>
2022-10-25 00:21:37 +1100

    Normalise Joystick and Programmable Button keycodes

--- keyboards/handwired/misterdeck/keymaps/default/keymap.c
+++ keyboards/handwired/misterdeck/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NORMAL_LAYER] = LAYOUT(
         C(S(KC_F1)), C(S(KC_F2)), C(S(KC_F3)), C(S(KC_F4)),
         C(S(KC_F5)), C(S(KC_F6)), C(S(KC_F7)), C(S(KC_F8)),
-        JS_BUTTON0,  JS_BUTTON1,  JS_BUTTON2,  JS_BUTTON3
+        JS_0,        JS_1,        JS_2,        JS_3
     )
 };
 
