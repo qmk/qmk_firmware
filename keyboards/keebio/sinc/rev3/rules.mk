4066f65b57cfaf60c156ce54f5bd9b692104a1ed
Danny Nguyen <danny@keeb.io>
2022-10-24 16:32:25 -0400

    Convert RGB Matrix config to DD format

--- keyboards/keebio/sinc/rev3/rules.mk
+++ keyboards/keebio/sinc/rev3/rules.mk
@@ -1,8 +1,2 @@
-# Build Options
-RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
-RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
-RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix functionality
-RGB_MATRIX_DRIVER = WS2812
-
 SERIAL_DRIVER = vendor
 WS2812_DRIVER = vendor
