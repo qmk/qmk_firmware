2563848668a4aeca32d3588077e80e670af526ae
seityaya <seityaya@ukr.net>
2022-10-24 10:07:34 +0300

    Remap

--- keyboards/kepler_33/proto/keymaps/default/keymap.c
+++ keyboards/kepler_33/proto/keymaps/default/keymap.c
@@ -25,6 +25,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_DEL, 
 		MO(15),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,    KC_ENT, 
 		KC_LSFT,      KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    KC_BSPC, 
-		KC_LCTL, MO(1), KC_LALT,         KC_SPC, KC_SPC, KC_MENU,            KC_RALT, MO(2), KC_RCTL
+		KC_LCTL, MO(1), KC_LALT,          KC_SPC, KC_SPC, KC_APP,            KC_RALT, MO(2), KC_RCTL
     )
 };
