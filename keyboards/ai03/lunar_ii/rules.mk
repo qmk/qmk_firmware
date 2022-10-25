9776b742339c6b7e02991e3ed6db0ce44aebe102
ai03 <admin@ai03.com>
2022-10-25 16:32:05 +0900

    Update keyboards/ai03/lunar_ii/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ai03/lunar_ii/rules.mk
+++ keyboards/ai03/lunar_ii/rules.mk
@@ -14,7 +14,6 @@ CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
