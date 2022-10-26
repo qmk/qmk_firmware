c6140013b2a887179c17dfa3b8e80bc848d0e4ff
TW59420 <1393018289@qq.com>
2022-10-26 12:16:32 +0800

    add mw660 keyboard

---
+++ keyboards/mwstudio/mw660/rules.mk
@@ -0,0 +1,4 @@
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+ENCODER_ENABLE = no
