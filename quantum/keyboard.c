4d7992fc094a150dbd03173217fa2b1dd62a1a8c
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-10-26 13:00:50 +0300

    core: allow locking the matrix state
    introducing flexibility on matrix tasks timing. matrix state can now be locked when we don't need a matrix scan

--- quantum/keyboard.c
+++ quantum/keyboard.c
@@ -252,6 +252,14 @@ __attribute__((weak)) void keyboard_post_init_kb(void) {
     keyboard_post_init_user();
 }
 
+/** \brief is_matrix_locked
+ *
+ * FIXME: needs doc
+ */
+__attribute__((weak)) bool is_matrix_locked(void) {
+    return false;
+}
+
 /** \brief keyboard_setup
  *
  * FIXME: needs doc
@@ -449,54 +457,57 @@ static inline void generate_tick_event(void) {
  * @return false Matrix didn't change
  */
 static bool matrix_task(void) {
-    static matrix_row_t matrix_previous[MATRIX_ROWS];
+    if (!is_matrix_locked()) {
+        static matrix_row_t matrix_previous[MATRIX_ROWS];
 
-    matrix_scan();
+        matrix_scan();
 
-    bool matrix_changed = false;
-    for (uint8_t row = 0; row < MATRIX_ROWS && !matrix_changed; row++) {
-        matrix_changed |= matrix_previous[row] ^ matrix_get_row(row);
-    }
-
-    matrix_scan_perf_task();
-
-    // Short-circuit the complete matrix processing if it is not necessary
-    if (!matrix_changed) {
-        generate_tick_event();
-        return matrix_changed;
-    }
-
-    if (debug_config.matrix) {
-        matrix_print();
-    }
-
-    const bool process_keypress = should_process_keypress();
-
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        const matrix_row_t current_row = matrix_get_row(row);
-        const matrix_row_t row_changes = current_row ^ matrix_previous[row];
-
-        if (!row_changes || has_ghost_in_row(row, current_row)) {
-            continue;
+        bool matrix_changed = false;
+        for (uint8_t row = 0; row < MATRIX_ROWS && !matrix_changed; row++) {
+            matrix_changed |= matrix_previous[row] ^ matrix_get_row(row);
         }
 
-        matrix_row_t col_mask = 1;
-        for (uint8_t col = 0; col < MATRIX_COLS; col++, col_mask <<= 1) {
-            if (row_changes & col_mask) {
-                const bool key_pressed = current_row & col_mask;
+        matrix_scan_perf_task();
 
-                if (process_keypress) {
-                    action_exec(MAKE_KEYEVENT(row, col, key_pressed));
-                }
+        // Short-circuit the complete matrix processing if it is not necessary
+        if (!matrix_changed) {
+            generate_tick_event();
+            return matrix_changed;
+        }
 
-                switch_events(row, col, key_pressed);
+        if (debug_config.matrix) {
+            matrix_print();
+        }
+
+        const bool process_keypress = should_process_keypress();
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+            const matrix_row_t current_row = matrix_get_row(row);
+            const matrix_row_t row_changes = current_row ^ matrix_previous[row];
+
+            if (!row_changes || has_ghost_in_row(row, current_row)) {
+                continue;
             }
+
+            matrix_row_t col_mask = 1;
+            for (uint8_t col = 0; col < MATRIX_COLS; col++, col_mask <<= 1) {
+                if (row_changes & col_mask) {
+                    const bool key_pressed = current_row & col_mask;
+
+                    if (process_keypress) {
+                        action_exec(MAKE_KEYEVENT(row, col, key_pressed));
+                    }
+
+                    switch_events(row, col, key_pressed);
+                }
+            }
+
+            matrix_previous[row] = current_row;
         }
 
-        matrix_previous[row] = current_row;
-    }
-
-    return matrix_changed;
+        return matrix_changed;
+    } else
+        return false;
 }
 
 /** \brief Tasks previously located in matrix_scan_quantum
