4014342e8f34ded0a288788c18a056acdf325ee6
TW59420 <40019210+TW59420@users.noreply.github.com>
2022-10-27 10:33:21 +0800

    Update keyboards/mwstudio/mw660/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/mwstudio/mw660/rules.mk
+++ keyboards/mwstudio/mw660/rules.mk
@@ -1,4 +1,1 @@
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = no
+# This file intentionally left blank
