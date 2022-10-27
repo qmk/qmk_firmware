a86b42d8c5850ca5796485501d82e067077d2195
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2022-10-27 23:50:32 +1100

    [Keymap] Update Miryoku (#18773)
    * [miryoku] Revert "[Keymap] manna-harbour_miryoku RESET to QK_BOOT (#18755)"
    
    This reverts commit 89dbc181617e69a5b4d91d9d772724daa2d387bb.
    
    * [miryoku] Add keyboardio/model01
    
    * [miryoku] Add handwired/dactyl_manuform/4x6
    
    * [miryoku] Update BT keycodes
    
    - Update BT keycodes on Media inner index:
      - Move OUT_AUTO to bottom row
    - Move RGB_TOG in vi to pinkie column to match other TOG keycodes, and rotate row
    
    * [miryoku] Add Extra and Tap layers
    
    * [miryoku] Rename config workflow option to custom_config
    
    * [miryoku] Update custom_config.h and custom_rules.mk behaviour
    
    * [miryoku] Make mapping macro customisable globally and per layer
    
    * [miryoku] Add layer lock
    
    * [miryoku] Update RGB order in media-l-invertedt
    
    * [miryoku] Change mouse button order
    
    * [miryoku] Change to X macros for layer list
    
    - Supports adding and removing layers from custom_config.h
    - Add "U_" prefix to layer names
    - Add layer name strings
    - Populate keymaps array from X macro
    - Populate layers enum from X macro
    - Move layers enum to manna-harbour_miryoku.h
    - Rename miryoku_alternatives.h to miryoku_layer_alternatives.h
    - Rename miryoku_layer.h to miryoku_layer_selection.h
    - Remove miryoku_layer_names.h
    - Add miryoku_layer_list.h
    
    * [miryoku] Reformat split_3x6_3 mapping
    
    * [miryoku] Add merge field to workflows
    
    * [miryoku] Update thumb combos with new layer names
    
    * [miryoku] Update cover image
    
    * [miryoku] Add satt/vision
    
    * [miryoku] Update Branches and Building docs
    
    * [miryoku] Update reset keycode
    
    * [miryoku] Add xmk
    
    * [miryoku] Add boardsource/lulu
    
    * [miryoku] Fix keyboardio/model01

--- keyboards/satt/vision/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/satt/vision/keymaps/manna-harbour_miryoku/config.h
@@ -8,14 +8,14 @@
 #define XXX KC_NO
 
 #define LAYOUT_miryoku( \
-      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
-      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
-      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
-      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+          K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, \
+          K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, \
+          K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, \
+          N30, N31, K32, K33, K34,           K35, K36, K37, N38, N39 \
 ) \
-LAYOUT_split_3x6_3( \
-XXX,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  XXX, \
-XXX,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  XXX, \
-XXX,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  XXX , \
-                  K32,  K33,  K34,         K35,  K36,  K37 \
+LAYOUT( \
+XXX, XXX, K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, XXX, XXX, \
+XXX, XXX, K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, XXX, \
+     XXX, K20, K21, K22, K23, K24,      XXX, K25, K26, K27, K28, K29, XXX,\
+     XXX,           K32, K33, K34,           K35, K36, K37,      XXX \
 )
