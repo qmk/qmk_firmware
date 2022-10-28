a046eb0ac336312db640d2750408f2c112c084d7
Ryan <fauxpark@gmail.com>
2022-10-29 03:26:10 +1100

    Update quantum/unicode/unicode.h

--- quantum/unicode/unicode.h
+++ quantum/unicode/unicode.h
@@ -37,7 +37,7 @@ enum unicode_input_modes {
     UNICODE_MODE_WINCOMPOSE, // Windows using WinCompose (https://github.com/samhocevar/wincompose)
     UNICODE_MODE_EMACS,      // Emacs is an operating system in search of a good text editor
 
-    UNICODE_MODE_COUNT       // Number of available input modes (always leave at the end)
+    UNICODE_MODE_COUNT // Number of available input modes (always leave at the end)
 };
 
 void    unicode_input_mode_init(void);
