33603471b5757c3e5090f9d9b590382057c604c7
mtei <2170248+mtei@users.noreply.github.com>
2022-10-28 18:03:22 +0900

    refine users/mtei/platforms/chibios/gpio_extr.h

--- users/mtei/platforms/chibios/gpio_extr.h
+++ users/mtei/platforms/chibios/gpio_extr.h
@@ -31,40 +31,40 @@ typedef uint16_t port_data_t;
 #define writePortBitLow(pin, bit) palClearLine(PAL_LINE(PAL_PORT(pin), (bit)))
 #define writePortBitHigh(pin, bit) palSetLine(PAL_LINE(PAL_PORT(pin), (bit)))
 
-#define setPortBunchInput(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_INPUT)
-#define setPortBunchInputHigh(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_INPUT_PULLUP)
+#define setPortBunchInput(port, bunch) palSetGroupMode_wrap(PAL_PORT(port), (bunch), 0, PAL_MODE_INPUT)
+#define setPortBunchInputHigh(port, bunch) palSetGroupMode_wrap(PAL_PORT(port), (bunch), 0, PAL_MODE_INPUT_PULLUP)
 
-#define setPortBunchOutputPusuPull(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_PUSHPULL)
+#define setPortBunchOutputPusuPull(port, bunch) palSetGroupMode_wrap(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_PUSHPULL)
 #define setPortBunchOutput(port, bunch) setPortBunchOutputPusuPull(port, bunch)
 
 #ifdef PAL_MODE_OUTPUT_OPENDRAIN
-#    define setPortBunchOutputOpenDrain(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_OPENDRAIN)
+#    define setPortBunchOutputOpenDrain(port, bunch) palSetGroupMode_wrap(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_OPENDRAIN)
 #endif
 #ifdef PAL_MODE_OUTPUT_OPENDRAIN_PULLUP
-#    define setPortBunchOutputOpenDrainPullup(port, bunch) palSetGroupMode(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_OPENDRAIN_PULLUP)
+#    define setPortBunchOutputOpenDrainPullup(port, bunch) palSetGroupMode_wrap(PAL_PORT(port), (bunch), 0, PAL_MODE_OUTPUT_OPENDRAIN_PULLUP)
 #endif
 
 #define writePortBunchLow(port, bunch) palWriteGroup(PAL_PORT(port), (bunch), 0, 0)
 #define writePortBunchHigh(port, bunch) palWriteGroup(PAL_PORT(port), (bunch), 0, (bunch))
 
+#define palSetGroupMode_wrap(port, bunch, offset, mode) palSetGroupMode((port), (bunch), (offset), (mode))
+
 // ChibiOS's GPIO implementation for RP2040 doesn't seem to implement palSetGroupMode(),
 // so as a workaround, use the implementation below.
 #    ifndef pal_lld_setgroupmode
 //#        pragma message "Use alternate palSetGroupMode"
-#        undef palSetGroupMode
+#        undef palSetGroupMode_wrap
 
-#        define palSetGroupMode(port, mask, offset, mode)       \
-              pal_lld_setgroupmode(port, mask, offset, mode)
-#        define pal_lld_setgroupmode(port, mask, offset, mode)  \
-              _pal_lld_setgroupmode(port, mask << offset, mode)
+#        define palSetGroupMode_wrap(port, mask, offset, mode)           \
+             palSetGroupMode_impl((port), (mask), (offset), (mode))
+#        define palSetGroupMode_impl(port, mask, offset, mode)           \
+             _palSetGroupMode_impl((port), ((mask) << (offset)), (mode))
 
-static inline void _pal_lld_setgroupmode(ioportid_t port,
-                             ioportmask_t mask,
-                             iomode_t mode) {
+static inline void _palSetGroupMode_impl(ioportid_t port, ioportmask_t mask, iomode_t mode) {
     iopadid_t pad = 0;
     while (mask != 0) {
         if ((mask & 1) != 0) {
-            pal_lld_setpadmode(port, pad, mode);
+            palSetPadMode(port, pad, mode);
         }
         pad ++;
         mask = mask >> 1;
