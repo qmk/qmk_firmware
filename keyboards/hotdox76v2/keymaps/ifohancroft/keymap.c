470f2bdd83b204f39ce5b28134e739985f77a4c5
IFo Hancroft <contact@ifohancroft.com>
2022-10-29 01:52:43 +0300

    Changed LAYOUT to LAYOUT_ergodox

--- keyboards/hotdox76v2/keymaps/ifohancroft/keymap.c
+++ keyboards/hotdox76v2/keymaps/ifohancroft/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *                                                   *---------+---------+---------*         *---------+---------+---------*
     */
 
-    LAYOUT(
+    LAYOUT_ergodox(
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_MINS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRC,
         KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *                                                   *---------+---------+---------*         *---------+---------+---------*
     */
 
-    LAYOUT(
+    LAYOUT_ergodox(
         KC_NUM,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,
         _______,  KC_PPLS,  KC_P7,    KC_P8,    KC_P9,    KC_PAST,  _______,
         _______,  KC_PMNS,  KC_P4,    KC_P5,    KC_P6,    KC_PSLS,
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /* Empty Layer */
-    LAYOUT(
+    LAYOUT_ergodox(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,
@@ -143,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /* Empty Layer */
-    LAYOUT(
+    LAYOUT_ergodox(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,
