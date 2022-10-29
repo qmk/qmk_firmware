cd8b9a8440bfcdef27ef2aa1c5cdd017a2499592
Ahmet Karalar <akaralar@gmail.com>
2022-10-29 17:52:04 +0200

    Add copyright headers

--- keyboards/ergodox_ez/keymaps/akaralar/config.h
+++ keyboards/ergodox_ez/keymaps/akaralar/config.h
@@ -1,7 +1,18 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
+/* Copyright 2021 Your Name (@yourgithub)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #define TAPPING_FORCE_HOLD
 
