b7106aa8b1b5c9105e510b49fab94530b4711825
Drashna Jael're <drashna@live.com>
2022-10-30 15:11:44 -0700

    fixup copyright headers

--- keyboards/work_louder/numpad/config.h
+++ keyboards/work_louder/numpad/config.h
@@ -1,4 +1,4 @@
-// Copyright 2022 Drashna Jael're (@Drashna Jael're)
+// Copyright 2022 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
--- keyboards/work_louder/numpad/keymaps/default/keymap.c
+++ keyboards/work_louder/numpad/keymaps/default/keymap.c
@@ -1,3 +1,6 @@
+// Copyright 2022 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
