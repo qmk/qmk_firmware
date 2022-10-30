ca2b32ed29b7e67d959088fba39227aa817a6a21
Archaengel <god11341258@gmail.com>
2022-10-30 16:25:36 -0700

    Reintro layer buttons as oneshot cancels

--- users/archaengel/archaengel.c
+++ users/archaengel/archaengel.c
@@ -72,7 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool is_oneshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
-        case KC_ESC:
+        case LA_SYM:
+        case LA_NAV:
             return true;
         default:
             return false;
