aed8e5245108944013d936267923bba434b6008a
Drashna Jael're <drashna@live.com>
2022-10-30 14:54:03 -0700

    [Keyboard] Add Work Louder Numpad

---
+++ keyboards/work_louder/numpad/config.h
@@ -0,0 +1,32 @@
+// Copyright 2022 Drashna Jael're (@Drashna Jael're)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+
+// #define RGB_DI_PIN D1
+#define DRIVER_LED_TOTAL 16
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
+#define RGB_MATRIX_DISABLE_KEYCODES
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+
+#define RGBLIGHT_DI_PIN D2
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_GRADIENT + 9
+#define RGBLIGHT_DEFAULT_HUE 213
