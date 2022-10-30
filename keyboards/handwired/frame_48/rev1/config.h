89467e7ddce319c5c4e8aa37c6c0901036166e77
Greg Mathews <gregdmathews@gmail.com>
2022-10-30 15:39:01 -0700

    Removing the converter

--- keyboards/handwired/frame_48/rev1/config.h
+++ keyboards/handwired/frame_48/rev1/config.h
@@ -30,24 +30,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS \
-    { B6, B2, B3, B1 }
+    { GP10, GP19, GP20, GP18 }
 #define MATRIX_COL_PINS \
-    { B5, B4, E6, D7, C6, D4, D0 }
+    { GP9, GP8, GP7, GP6, GP5, GP4, GP3 }
 
 /* Encoder pins */
 #define ENCODERS_PAD_A \
-    { D3 }
+    { GP0 }
 #define ENCODERS_PAD_B \
-    { D2 }
+    { GP1 }
 
 #define ENCODERS_PAD_A_RIGHT \
-    { D3 }
+    { GP0 }
 #define ENCODERS_PAD_B_RIGHT \
-    { D2 }
+    { GP1 }
 
 #define ENCODER_DIRECTION_FLIP
 
 /* Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN. */
 #define USE_SERIAL
-#define SOFT_SERIAL_PIN F4 // or D1, D2, D3, E6
+#define SOFT_SERIAL_PIN GP29 // or D1, D2, D3, E6
 #define SELECT_SOFT_SERIAL_SPEED 0
