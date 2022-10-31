fcab197d25b0e7b3d642164be25c3f6e14a4c822
Joy <chang.li@westberrytech.com>
2022-10-31 12:05:58 +0800

    Update onekey.

---
+++ keyboards/handwired/onekey/evb_wb32f3g71/halconf.h
@@ -0,0 +1,7 @@
+// Copyright 2022 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define HAL_USE_ADC TRUE
+
+#include_next <halconf.h>
---
+++ keyboards/handwired/onekey/evb_wb32fq95/mcuconf.h
@@ -0,0 +1,11 @@
+// Copyright 2022 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#include_next "mcuconf.h"
+
+#undef WB32_ADC_USE_ADC1
+#define WB32_ADC_USE_ADC1 TRUE
+
+#undef WB32_DMA_REQUIRED
+#define WB32_DMA_REQUIRED TRUE
