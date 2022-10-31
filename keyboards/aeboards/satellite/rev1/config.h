1f97c883dcc4ad5bcaba68b570a5a21b674f534a
Xelus22 <preyas22@gmail.com>
2022-10-31 21:10:32 +1100

    update and move usb to info json

--- keyboards/aeboards/satellite/rev1/config.h
+++ keyboards/aeboards/satellite/rev1/config.h
@@ -16,36 +16,6 @@
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x4145 // AE
-#define PRODUCT_ID      0x6553 // 65 - S
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    AEBoards
-#define PRODUCT         AEBoards Satellite Rev1
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
-
-/* key matrix pins */
-#define MATRIX_ROW_PINS { B0, B1, F0, F4, F1 }
-#define MATRIX_COL_PINS { E6, D5, B2, B3, B7, F6, F7, C7, C6, B6, B5, B4, D7, D6, D4 }
-#define UNUSED_PINS
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
 //RGB Matrix defines
 #define DRIVER_ADDR_1 0x74
 #define DRIVER_ADDR_2 0x76
