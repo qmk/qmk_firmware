f7edee19eb8b84ae31bcdd624d4a2c1ac2808070
Joy <chang.li@westberrytech.com>
2022-10-31 14:19:00 +0800

    Update onekey.

--- keyboards/handwired/onekey/evb_wb32fq95/halconf.h
+++ keyboards/handwired/onekey/evb_wb32fq95/halconf.h
@@ -4,4 +4,8 @@
 
 #define HAL_USE_ADC TRUE
 
+#define HAL_USE_I2C TRUE
+
+#define HAL_USE_PWM TRUE
+
 #include_next <halconf.h>
--- keyboards/handwired/onekey/evb_wb32fq95/rules.mk
+++
@@ -1,3 +0,0 @@
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
-NKRO_ENABLE = no           # Enable N-Key Rollover
