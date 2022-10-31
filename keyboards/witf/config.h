8b9b476a5f944badd0fec7ea91a37fee5db06a03
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2022-10-31 09:58:04 -0400

    Add WITF

---
+++ keyboards/witf/config.h
@@ -0,0 +1,24 @@
+// Copyright 2022 Sleepdealer (@Sleepdealr)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+#define LED_CAPS_LOCK_PIN A3
+#define LED_SCROLL_LOCK_PIN A4
+#define LED_NUM_LOCK_PIN A5
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
