2e49ac5df0f425fe4df4999ddfdafeda0dda78d0
Griffin Yourick <gryphon92@gmail.com>
2022-10-31 15:58:15 -0400

    Tweak layer 1

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/tough-griff/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/tough-griff/keymap.c
@@ -25,10 +25,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LALT, KC_LGUI,             KC_SPC,                      KC_RGUI, MO(_L1),          KC_LEFT, KC_DOWN,   KC_RIGHT),
 
 
+    /* Layer 1
+     * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────────────┬──────┐
+     * │ GEsc │  F1  │  F2  │  F3  │  F4  │  F5  │  F6  │  F7  │  F8  │  F9  │  F10 │  F11 │  F12 │    Delete    │ Home │
+     * ├──────┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──────────┼──────┤
+     * │          │RgbTog│RgbMod│RgbHui│RgbHud│RgbSai│RgbSad│RgbVai│RgbVad│      │PrtScr│      │      │BOOTLOADER│ PgUp │
+     * ├──────────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴──────────┼──────┤
+     * │  Caps Lock │RgbSpi│RgbSpd│      │      │      │      │      │      │      │      │      │               │ PgDn │
+     * ├────────────┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴────────┬──────┼──────┤
+     * │      LSft      │      │      │      │      │      │      │      │      │      │      │           │  V+  │ Mute │
+     * ├────────┬───────┴┬─────┴──┬───┴──────┴──────┴──────┴──────┴──────┴────┬─┴──────┼──────┴─┬──┬──────┼──────┼──────┤
+     * │        │        │        │                                           │        │        │  │ Prev │  V-  │ Next │
+     * └────────┴────────┴────────┴───────────────────────────────────────────┴────────┴────────┘  └──────┴──────┴──────┘
+     */
     [_L1] = LAYOUT_65_ansi_blocker(
         KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
-        KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, QK_BOOT, KC_PGUP,
-        KC_CAPS, RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          EEP_RST, KC_PGDN,
+        KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, QK_BOOT, KC_PGUP,
+        KC_CAPS, RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_PGDN,
         KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_VOLU, KC_MUTE,
         KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,          KC_MPRV, KC_VOLD, KC_MNXT),
  };
