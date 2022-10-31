ad74d41706a01935c7fded398db07406446767f4
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-10-31 12:47:52 +0700

    Update keymap.c

--- keyboards/mechlovin/hex6c/keymaps/default/keymap.c
+++ keyboards/mechlovin/hex6c/keymaps/default/keymap.c
@@ -29,7 +29,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] =   {ENCODER_CCW_CW(RGB_MOD, RGB_RMOD)}
+    [0] =   {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
 };
 #endif
