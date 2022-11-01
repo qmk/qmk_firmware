55afb0719d8277e3787ec4a22eb714dd02e36998
Andrew Kannan <andrew.kannan@klaviyo.com>
2022-11-01 14:32:42 -0400

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/cannonkeys/ortho60v2/keymaps/default/keymap.c
+++ keyboards/cannonkeys/ortho60v2/keymaps/default/keymap.c
@@ -21,9 +21,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _RAISE 1
-#define _LOWER 2
+enum custom_layers {
+    _BASE,
+    _RAISE,
+    _LOWER,
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
