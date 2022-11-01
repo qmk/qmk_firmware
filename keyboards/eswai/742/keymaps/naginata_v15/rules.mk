0af517c697488ec151f3734602c91f89da06e1cf
eswai <>
2022-11-01 22:02:28 +0900

    users/naginata_v15へ移行

--- keyboards/eswai/742/keymaps/naginata_v15/rules.mk
+++ keyboards/eswai/742/keymaps/naginata_v15/rules.mk
@@ -2,7 +2,4 @@ UNICODE_ENABLE = yes
 # CONSOLE_ENABLE = yes
 # COMMAND_ENABLE = yes
 
-# If you want to change the display of OLED, you need to change here
-SRC +=  naginata_v15.c
-SRC +=  twpair_on_jis.c
 EXTRAFLAGS += -flto
