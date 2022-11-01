342f0835c2e64ec10f9c344eee1569630d342af5
tarneo <tarneo@tarneo.fr>
2022-11-01 11:04:53 +0100

    correct config for supersplit

--- keyboards/supersplit/config.h
+++ keyboards/supersplit/config.h
@@ -35,10 +35,3 @@
 
 #define SOFT_SERIAL_PIN D2
 #define MASTER_LEFT
-
-
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 6
-
-#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6 }
--- keyboards/supersplit/supersplit.c
+++
@@ -1,1 +0,0 @@
-#include "supersplit.h"
