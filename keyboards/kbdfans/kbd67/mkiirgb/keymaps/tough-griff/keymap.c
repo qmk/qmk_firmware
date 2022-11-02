ac9a1cb639a21552c575cc458175bfef4ee0b321
Griffin Yourick <gryphon92@gmail.com>
2022-11-02 13:50:09 -0400

    refactor

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/tough-griff/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/tough-griff/keymap.c
@@ -1,7 +1,9 @@
 #include QMK_KEYBOARD_H
 
-#define _L0 0
-#define _L1 1
+enum Layers {
+    _BASE,
+    _L1,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base Layer
@@ -17,19 +19,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │  LCtl  │  LAlt  │  LGUI  │                   Space                   │  RGUI  │ MO(1)  │  │ Left │ Down │ Rght │
      * └────────┴────────┴────────┴───────────────────────────────────────────┴────────┴────────┘  └──────┴──────┴──────┘
      */
-    [_L0] = LAYOUT_65_ansi_blocker(
-        KC_GESC, KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_HOME,
-        KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLASH, KC_PGUP,
-        KC_BSPC, KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,    KC_PGDN,
-        KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,     KC_END,
-        KC_LCTL, KC_LALT, KC_LGUI,             KC_SPC,                      KC_RGUI, MO(_L1),          KC_LEFT, KC_DOWN,   KC_RIGHT),
-
+    // clang-format off
+    [_BASE] = LAYOUT_65_ansi_blocker(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_HOME,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGUP,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,   KC_PGDN,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,    KC_END,
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                            KC_RGUI, MO(_L1),          KC_LEFT, KC_DOWN,  KC_RGHT),
+    // clang-format on
 
     /* Layer 1
      * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────────────┬──────┐
-     * │ GEsc │  F1  │  F2  │  F3  │  F4  │  F5  │  F6  │  F7  │  F8  │  F9  │  F10 │  F11 │  F12 │    Delete    │ Home │
+     * │ GEsc │  F1  │  F2  │  F3  │  F4  │  F5  │  F6  │  F7  │  F8  │  F9  │  F10 │  F11 │  F12 │    Delete    │ BOOT │
      * ├──────┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──────────┼──────┤
-     * │          │RgbTog│RgbMod│RgbHui│RgbHud│RgbSai│RgbSad│RgbVai│RgbVad│      │PrtScr│      │      │BOOTLOADER│ PgUp │
+     * │          │RgbTog│RgbMod│RgbHui│RgbHud│RgbSai│RgbSad│RgbVai│RgbVad│      │PrtScr│ScrLck│ Pause│          │ PgUp │
      * ├──────────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴──────────┼──────┤
      * │  Caps Lock │RgbSpi│RgbSpd│      │      │      │      │      │      │      │      │      │               │ PgDn │
      * ├────────────┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴────────┬──────┼──────┤
@@ -38,10 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │        │        │        │                                           │        │        │  │ Prev │  V-  │ Next │
      * └────────┴────────┴────────┴───────────────────────────────────────────┴────────┴────────┘  └──────┴──────┴──────┘
      */
+    // clang-format off
     [_L1] = LAYOUT_65_ansi_blocker(
-        KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
-        KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, QK_BOOT, KC_PGUP,
-        KC_CAPS, RGB_SPI, RGB_SPD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_PGDN,
-        KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_VOLU, KC_MUTE,
-        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,          KC_MPRV, KC_VOLD, KC_MNXT),
- };
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  QK_BOOT,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SCRL, KC_PAUS, _______, KC_PGUP,
+        KC_CAPS, RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_PGDN,
+        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLU, KC_MUTE,
+        _______, _______, _______,                   _______,                            _______, _______,          KC_MPRV, KC_VOLD, KC_MNXT),
+    // clang-format on
+};
