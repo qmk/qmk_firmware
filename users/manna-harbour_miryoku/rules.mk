8c5086758cba760acb025856a27d02d2109e618d
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2022-11-05 21:17:03 +1100

    [miryoku] Add Caps Word in place of Caps Lock
    Combine with Shift for Caps Lock

--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -5,6 +5,8 @@ MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 
 INTROSPECTION_KEYMAP_C = manna-harbour_miryoku.c # keymaps
 
