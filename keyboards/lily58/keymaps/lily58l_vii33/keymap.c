edb897d71ff00fc3465e7ef3e6e5a68c2d7bb54b
vii33 <1887246+vii33@users.noreply.github.com>
2022-11-06 11:44:06 +0100

    added delete key to improve one handed ops

--- keyboards/lily58/keymaps/lily58l_vii33/keymap.c
+++ keyboards/lily58/keymaps/lily58l_vii33/keymap.c
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11, 
   _______,  XXXXXXX, WINSCNS, QC_UNDO, QC_REDO, XXXXXXX,                      XXXXXXX, KC_HOME, KC_UP,   KC_END,   KC_PGUP, KC_F12, 
-  XXXXXXX,  XXXXXXX, KC_RGUI, KC_LALT, KC_LCTL, KC_LSFT,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,  KC_PGDN, XXXXXXX, 
+  XXXXXXX,  KC_DEL,  KC_RGUI, KC_LALT, KC_LCTL, KC_LSFT,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,  KC_PGDN, XXXXXXX, 
   XXXXXXX,  KC_BSPC, QC_CTLX, QC_CTLC, QC_CTLV, KC_ENTER, _______,  _______,  XXXXXXX, WRDLFT,  KC_BSPC, WRDRGT,   XXXXXXX, KC_ENT, 
                               _______, _______, _______, _______,   KC_LSFT,  _______, KC_BSPC, KC_DEL
 ),
