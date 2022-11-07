0e5cf60f82d6bb2e902d67456ade8bb702798aa7
mtei <2170248+mtei@users.noreply.github.com>
2022-11-07 20:51:48 +0900

    change 0x?? to 0x??U in symmetric70_proto/{promicro|proton_c}/boost/config.h

--- keyboards/handwired/symmetric70_proto/promicro/boost/config.h
+++ keyboards/handwired/symmetric70_proto/promicro/boost/config.h
@@ -45,39 +45,39 @@
  /* #define EXTENDED_MATRIX_COL_PINS (MCU_GPIO_74HC157_A, F0, 4), (MCU_GPIO_74HC157_B, F0, 4), (MCU_GPIO_74HC157_A, F0, 5), (MCU_GPIO_74HC157_B, F0, 5), (MCU_GPIO_74HC157_A, F0, 6), (MCU_GPIO_74HC157_B, F0, 6), (MCU_GPIO_74HC157_A, F0, 7), (MCU_GPIO_74HC157_B, F0, 7), (MCU_GPIO_74HC157_B, B0, 6), (MCU_GPIO_74HC157_A, B0, 6), (MCU_GPIO_74HC157_B, B0, 2), (MCU_GPIO_74HC157_A, B0, 2), (MCU_GPIO_74HC157_B, B0, 3), (MCU_GPIO_74HC157_A, B0, 3), (MCU_GPIO_74HC157_B, B0, 1), (MCU_GPIO_74HC157_A, B0, 1) */
 #define SWITCH_MATRIX_INPUT_0 \
     /* ( ( <port>, <mask>, <dev> ), ... ) */ \
-    ( (F0, 0xf0, MCU_GPIO_74HC157_A), \
-      (F0, 0xf0, MCU_GPIO_74HC157_B), \
-      (B0, 0x4e, MCU_GPIO_74HC157_B), \
-      (B0, 0x4e, MCU_GPIO_74HC157_A) ),\
+    ( (F0, 0xf0U, MCU_GPIO_74HC157_A), \
+      (F0, 0xf0U, MCU_GPIO_74HC157_B), \
+      (B0, 0x4eU, MCU_GPIO_74HC157_B), \
+      (B0, 0x4eU, MCU_GPIO_74HC157_A) ),\
     /* ( ( <port_index>, <port_mask>, <matrix_row_mask> ), ... ) */ \
-    ( (0, 0x10, 0x01), \
-      (1, 0x10, 0x02), \
-      (0, 0x20, 0x04), \
-      (1, 0x20, 0x08), \
-      (0, 0x40, 0x10), \
-      (1, 0x40, 0x20), \
-      (0, 0x80, 0x40), \
-      (1, 0x80, 0x80), \
-      (2, 0x40, 0x100), \
-      (3, 0x40, 0x200), \
-      (2, 0x04, 0x400), \
-      (3, 0x04, 0x800), \
-      (2, 0x08, 0x1000), \
-      (3, 0x08, 0x2000), \
-      (2, 0x02, 0x4000), \
-      (3, 0x02, 0x8000) )
+    ( (0, 0x10U, 0x01U), \
+      (1, 0x10U, 0x02U), \
+      (0, 0x20U, 0x04U), \
+      (1, 0x20U, 0x08U), \
+      (0, 0x40U, 0x10U), \
+      (1, 0x40U, 0x20U), \
+      (0, 0x80U, 0x40U), \
+      (1, 0x80U, 0x80U), \
+      (2, 0x40U, 0x100U), \
+      (3, 0x40U, 0x200U), \
+      (2, 0x04U, 0x400U), \
+      (3, 0x04U, 0x800U), \
+      (2, 0x08U, 0x1000U), \
+      (3, 0x08U, 0x2000U), \
+      (2, 0x02U, 0x4000U), \
+      (3, 0x02U, 0x8000U) )
 
  /* #define EXTENDED_MATRIX_ROW_PINS (MCU_GPIO, D0, 4), (MCU_GPIO, C0, 6), (MCU_GPIO, D0, 7), (MCU_GPIO, E0, 6), (MCU_GPIO, B0, 4) */
 #define SWITCH_MATRIX_OUTPUT_0 \
     /* ( ( <port>, <mask>, <dev> ), ... ) */ \
-    ( (D0, 0x90, MCU_GPIO), \
-      (C0, 0x40, MCU_GPIO), \
-      (E0, 0x40, MCU_GPIO), \
-      (B0, 0x10, MCU_GPIO) ),\
+    ( (D0, 0x90U, MCU_GPIO), \
+      (C0, 0x40U, MCU_GPIO), \
+      (E0, 0x40U, MCU_GPIO), \
+      (B0, 0x10U, MCU_GPIO) ),\
     /* ( ( <port_index>, <port_mask>, <matrix_row_mask> ), ... ) */ \
-    ( (0, 0x10, 0x01), \
-      (1, 0x40, 0x02), \
-      (0, 0x80, 0x04), \
-      (2, 0x40, 0x08), \
-      (3, 0x10, 0x10) )
+    ( (0, 0x10U, 0x01U), \
+      (1, 0x40U, 0x02U), \
+      (0, 0x80U, 0x04U), \
+      (2, 0x40U, 0x08U), \
+      (3, 0x10U, 0x10U) )
 
