e715694ea3cafc3e89ecd98a24137ff9ab98ba3d
Arturo Avila <69410272+ADPenrose@users.noreply.github.com>
2022-11-08 22:53:34 -0600

    Reformated rules.mk file
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/adpenrose/akemipad/rules.mk
+++ keyboards/adpenrose/akemipad/rules.mk
@@ -5,7 +5,7 @@ MCU = atmega32u4
 BOOTLOADER = caterina
 
 # Build Options
-# 	change yes to no to disable
+#   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
