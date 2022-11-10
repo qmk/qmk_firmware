8104ceac908c65dd662cc59d7c65e95fe8b0e1d6
Jels02 <joahnelson@gmail.com>
2022-11-10 18:28:06 +1000

    move matrix to info json

--- keyboards/jels/boaty/rules.mk
+++ keyboards/jels/boaty/rules.mk
@@ -1,16 +1,1 @@
-# MCU name
-MCU = atmega328p
-
-BOOTLOADER = usbasploader
-
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for deb
-NKRO_ENABLE = no            # USB Nkey Rollover - Doesn't work with USBasp
-BACKLIGHT_ENABLE = no 	    # Enable keyboard backlight functionality on B7 by default
-
+# This file is intentionally left blank
