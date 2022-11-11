5a6b636f0d682a7c9c3cb54b2dba678278a86710
gksygithub <106651989+gksygithub@users.noreply.github.com>
2022-11-11 12:59:57 +0800

    update via/keymap.c

--- keyboards/darkproject/kd87a_bfg_edition/keymaps/via/keymap.c
+++ keyboards/darkproject/kd87a_bfg_edition/keymaps/via/keymap.c
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-void rgb_matrix_indicators_user(void) {
+bool rgb_matrix_indicators_user(void) {
 	if (host_keyboard_led_state().caps_lock) {
 		setPinInputHigh (C14);
 	}
@@ -181,4 +181,6 @@ void rgb_matrix_indicators_user(void) {
 	if (IS_LAYER_ON(1)) {   
 		setPinInputHigh (C0); 
 	}
+	
+	return true;
 }
