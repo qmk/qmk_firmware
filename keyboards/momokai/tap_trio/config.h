3bcf048178816b8d7f28dd36873a682cae315d32
peepeetee <peepeetee@protonmail.com>
2022-11-11 19:40:20 -0600

    enable mousekey for momokai tap_trio again, this time it should work

--- keyboards/momokai/tap_trio/config.h
+++ keyboards/momokai/tap_trio/config.h
@@ -27,6 +27,19 @@
 #define DIODE_DIRECTION COL2ROW
 
 
+// Tweak how the mouse cursor moves. https://docs.qmk.fm/#/feature_mouse_keys
+// Accelerated mode (default).
+#define MOUSEKEY_DELAY  10
+#define MOUSEKEY_INTERVAL 20
+#define MOUSEKEY_MOVE_DELTA 8
+#define MOUSEKEY_MAX_SPEED  10
+#define MOUSEKEY_TIME_TO_MAX  30
+#define MOUSEKEY_WHEEL_DELAY  10
+#define MOUSEKEY_WHEEL_INTERVAL 80
+#define MOUSEKEY_WHEEL_MAX_SPEED  8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX  40
+
+
 //TODO: implement RGB Matrix
 #define RGB_DI_PIN F0
 #define RGBLED_NUM 5
