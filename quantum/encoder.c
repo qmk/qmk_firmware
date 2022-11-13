7e23e38bd3e012467ac95f4847c970ccc085b5b2
zvecr <git@zvecr.com>
2022-11-13 01:17:39 +0000

    Add/fix tests

--- quantum/encoder.c
+++ quantum/encoder.c
@@ -80,6 +80,10 @@ __attribute__((weak)) bool encoder_update_kb(uint8_t index, bool clockwise) {
     return encoder_update_user(index, clockwise);
 }
 
+__attribute__((weak)) bool should_process_encoder(void) {
+    return is_keyboard_master();
+}
+
 void encoder_init(void) {
 #ifdef SPLIT_KEYBOARD
     thisHand  = isLeftHand ? 0 : NUM_ENCODERS_LEFT;
@@ -180,7 +184,7 @@ static bool encoder_update(uint8_t index, uint8_t state) {
             encoder_value[index]++;
             changed = true;
 #ifdef SPLIT_KEYBOARD
-            if (is_keyboard_master())
+            if (should_process_encoder())
 #endif // SPLIT_KEYBOARD
 #ifdef ENCODER_MAP_ENABLE
                 encoder_exec_mapping(index, ENCODER_COUNTER_CLOCKWISE);
@@ -197,7 +201,7 @@ static bool encoder_update(uint8_t index, uint8_t state) {
             encoder_value[index]--;
             changed = true;
 #ifdef SPLIT_KEYBOARD
-            if (is_keyboard_master())
+            if (should_process_encoder())
 #endif // SPLIT_KEYBOARD
 #ifdef ENCODER_MAP_ENABLE
                 encoder_exec_mapping(index, ENCODER_CLOCKWISE);
