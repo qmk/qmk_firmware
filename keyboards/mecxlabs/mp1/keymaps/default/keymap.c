517d2fb799cd90493a84a941cee5273fe42a59c4
Patryk <61631089+patrykf03@users.noreply.github.com>
2022-11-13 20:11:59 +0000

    Apply suggestions from code review
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/mecxlabs/mp1/keymaps/default/keymap.c
+++ keyboards/mecxlabs/mp1/keymaps/default/keymap.c
@@ -4,29 +4,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[0] = LAYOUT_all(
+	[0] = LAYOUT_ortho_3x3(
         KC_1, KC_2, KC_3,
         KC_4, KC_5, KC_6,
         KC_7, KC_8, KC_9
-	),
-
-	[1] = LAYOUT_all(
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS
-	),
-
-	[2] = LAYOUT_all(
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS
-	),
-
-	[3] = LAYOUT_all(
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS
-	),
+	)
 
 };
 
--- keyboards/mecxlabs/mp1/rules.mk
+++ keyboards/mecxlabs/mp1/rules.mk
@@ -1,11 +1,4 @@
 # Wildcard to allow APM32 MCU
 DFU_SUFFIX_ARGS = -v FFFF -p FFFF
-
-BOOTMAGIC_ENABLE = yes
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
-
-
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
