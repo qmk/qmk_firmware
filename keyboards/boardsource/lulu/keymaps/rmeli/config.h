317b800bbb28c5ae91f413cfd17471a06de76c2f
Rocco Meli <r.meli@bluemail.ch>
2022-11-13 02:00:13 +0100

    [Keymap] Update/refactor userspace and add keymaps for rmeli (#18398)
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Ryan <fauxpark@gmail.com>
    Co-authored-by: Elliot Powell <johnp8727@gmail.com>

--- keyboards/boardsource/lulu/keymaps/rmeli/config.h
+++ keyboards/boardsource/lulu/keymaps/rmeli/config.h
@@ -1,9 +1,5 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-Copyright 2021 Rocco Meli <@RMeli>
+Copyright 2022 Rocco Meli <@RMeli>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,43 +21,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_LED_STATE_ENABLE
 
 #ifdef RGB_MATRIX_ENABLE
-// RGB matrix options
+// Configure RGB Matrix
 #    define RGB_MATRIX_KEYPRESSES // enable keypress effects
 #    define RGB_MATRIX_LED_FLUSH_LIMIT 16
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED
-// Disable unwanted R2G effects (from r2g/config.h)
-#    undef ENABLE_RGB_MATRIX_ALPHAS_MODS
-#    undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-#    undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-#    define ENABLE_RGB_MATRIX_BREATHING
-#    undef ENABLE_RGB_MATRIX_BAND_SAT
-#    undef ENABLE_RGB_MATRIX_BAND_VAL
-#    undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-#    undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-#    undef ENABLE_RGB_MATRIX_CYCLE_ALL
-#    undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-#    undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-#    undef ENABLE_RGB_MATRIX_RAINDROPS
-#    undef ENABLE_RGB_MATRIX_HUE_BREATHING
-#    undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
-#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#    undef ENABLE_RGB_MATRIX_SPLASH
-#    undef ENABLE_RGB_MATRIX_SOLID_SPLASH
-// Enable effects
-#    define ENABLE_RGB_MATRIX_SOLID_COLOR
-#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-// Default effect
 #    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
 #    define RGB_MATRIX_STARTUP_HUE 10
 #    define RGB_MATRIX_STARTUP_SAT 255
 #    define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
+// Disable RGB Matrix effects (from lulu/config.h)
+#    undef ENABLE_RGB_MATRIX_ALPHAS_MODS
+#    undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#    undef ENABLE_RGB_MATRIX_BREATHING
+#    undef ENABLE_RGB_MATRIX_BAND_SAT
+#    undef ENABLE_RGB_MATRIX_BAND_VAL
+// Enable RGB Matrix effects
+#    define ENABLE_RGB_MATRIX_BREATHING
+#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#    define ENABLE_RGB_MATRIX_SOLID_COLOR
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 #endif
-
-// https://github.com/qmk/qmk_firmware/blob/develop/docs/squeezing_avr.md
-#undef LOCKING_SUPPORT_ENABLE
-#undef LOCKING_RESYNC_ENABLE
-#define NO_MUSIC_MODE
-#define LAYER_STATE_8BIT // Limit to 8 layers
