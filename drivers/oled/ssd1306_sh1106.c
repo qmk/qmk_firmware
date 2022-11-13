0de72941ce63636d4a6df7773b8666f11f988ce3
zvecr <git@zvecr.com>
2022-11-13 21:00:41 +0000

    Add default limit to OLED dirty processing

--- drivers/oled/ssd1306_sh1106.c
+++ drivers/oled/ssd1306_sh1106.c
@@ -300,8 +300,9 @@ void oled_render(void) {
     // Turn on display if it is off
     oled_on();
 
-    uint8_t update_start = 0;
-    while (oled_dirty) { // render all dirty blocks
+    uint8_t update_start     = 0;
+    uint8_t oled_dirty_count = 0;
+    while (oled_dirty && oled_dirty_count++ < OLED_UPDATE_PROCESS_LIMIT) { // render all dirty blocks
         // Find next dirty block
         while (!(oled_dirty & ((OLED_BLOCK_TYPE)1 << update_start))) {
             ++update_start;
