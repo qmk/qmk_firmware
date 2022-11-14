fa1f5ac4923c59108af961a4774b9b9c9b4ca890
jay_adams <blurryllama@gmail.com>
2022-11-14 16:52:18 -0500

    relocate tab, mod keys. Remove Layer State Reader, added mod key indicator.

--- keyboards/lily58/keymaps/blurryllama/rules.mk
+++ keyboards/lily58/keymaps/blurryllama/rules.mk
@@ -3,10 +3,10 @@ EXTRAKEY_ENABLE = yes
 LTO_ENABLE = yes
 
  # If you want to change the display of OLED, you need to change here
- SRC +=  ./lib/rgb_state_reader.c \
-         ./lib/layer_state_reader.c \
-         ./lib/logo_reader.c \
+ SRC +=  ./lib/logo_reader.c \
          oled/bongocat.c \
+ 		# ./lib/rgb_state_reader.c \
+         # ./lib/layer_state_reader.c \
          # ./lib/mode_icon_reader.c \
          # ./lib/host_led_state_reader.c \
          # ./lib/timelogger.c \
