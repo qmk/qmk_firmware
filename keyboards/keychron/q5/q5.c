a9d213bef0472a2c3c5a395256e37cfeb53e44bc
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-11-14 12:20:29 +0800

    Revert to previous commit

--- keyboards/keychron/q5/q5.c
+++ keyboards/keychron/q5/q5.c
@@ -69,10 +69,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
-    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
-        return false;
-    }
+__attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
 #    if defined(CAPS_LOCK_LED_INDEX)
     if (host_keyboard_led_state().caps_lock) {
@@ -92,7 +89,6 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
         }
     }
 #    endif // NUM_LOCK_LED_INDEX
-    return true;
 }
 
 #endif // RGB_MATRIX_ENABLE...
