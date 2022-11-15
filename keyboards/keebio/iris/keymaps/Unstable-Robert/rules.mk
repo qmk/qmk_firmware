2b1cb6d6b68a0f4516c75825622f16f889d21519
Robert Mannuzza <Robert.mannuzza@gmail.com>
2022-11-15 02:07:45 -0500

    added keymap for kb
    still need to work on the encoders for vol and and panning.  than to add zooming

---
+++ keyboards/keebio/iris/keymaps/Unstable-Robert/rules.mk
@@ -0,0 +1,7 @@
+# Copyright 2022 Ryan Neff (@JellyTitan)
+# SPDX-License-Identifier: GPL-2.0-or-later
+ENCODER_MAP_ENABLE = yes
+# Console for debug:
+# This is enabled by the parent iris rev6a config - turning it off saves us some space.
+# You can find the other iris default options here:  keyboards/keebio/iris/rev6a/rules.mk
+CONSOLE_ENABLE = no
