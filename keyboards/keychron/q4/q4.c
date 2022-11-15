a3791a6ed567af0aacfb43115744a37084ad87a2
adophoxia <andyao1528@gmail.com>
2022-11-14 18:10:15 -0800

    Update q4.c
    Remove test/factory code for caps lock and status LED and replace with production variant.

--- keyboards/keychron/q4/q4.c
+++ keyboards/keychron/q4/q4.c
@@ -37,74 +37,39 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
 #endif // DIP_SWITCH_ENABLE
 
 #if defined(RGB_MATRIX_ENABLE) && defined(CAPS_LOCK_LED_INDEX)
-
-#    define CAPS_LOCK_MAX_BRIGHTNESS 0xFF
-#    ifdef RGB_MATRIX_MAXIMUM_BRIGHTNESS
-#        undef CAPS_LOCK_MAX_BRIGHTNESS
-#        define CAPS_LOCK_MAX_BRIGHTNESS RGB_MATRIX_MAXIMUM_BRIGHTNESS
-#    endif
-
-#    define CAPS_LOCK_VAL_STEP 8
-#    ifdef RGB_MATRIX_VAL_STEP
-#        undef CAPS_LOCK_VAL_STEP
-#        define CAPS_LOCK_VAL_STEP RGB_MATRIX_VAL_STEP
-#    endif
-
-extern void rgb_matrix_update_pwm_buffers(void);
-
-static uint8_t light_brightness_get(void) {
-    uint8_t value = rgb_matrix_get_val();
-    if (value < CAPS_LOCK_VAL_STEP) {
-        value = CAPS_LOCK_VAL_STEP;
-    } else if (value < (CAPS_LOCK_MAX_BRIGHTNESS - CAPS_LOCK_VAL_STEP)) {
-        value += CAPS_LOCK_VAL_STEP; // one step more than current brightness
-    } else {
-        value = CAPS_LOCK_MAX_BRIGHTNESS;
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_user(keycode, record)) { return false; }
+    switch (keycode) {
+#ifdef RGB_MATRIX_ENABLE
+        case RGB_TOG:
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        rgb_matrix_set_flags(LED_FLAG_NONE);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                    } break;
+                    default: {
+                        rgb_matrix_set_flags(LED_FLAG_ALL);
+                    } break;
+                }
+            }
+            if (!rgb_matrix_is_enabled()) {
+                rgb_matrix_set_flags(LED_FLAG_ALL);
+                rgb_matrix_enable();
+            }
+            return false;
+#endif
     }
-
-    return value;
+    return true;
 }
-
-void rgb_matrix_indicators_kb(void) {
+__attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
     if (host_keyboard_led_state().caps_lock) {
-        uint8_t v = light_brightness_get();
-        rgb_matrix_set_color(CAPS_LOCK_LED_INDEX, v, v, v); // white, with the adjusted brightness
-    }
-}
-
-void rgb_matrix_indicators_none_kb(void) {
-    rgb_matrix_indicators_kb();
-    rgb_matrix_update_pwm_buffers();
-}
-
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-
-    if (rgb_matrix_is_enabled()
-#    if defined(ENABLE_RGB_MATRIX_RAINDROPS)
-        && (rgb_matrix_get_mode() != RGB_MATRIX_RAINDROPS)
-#    endif
-#    if defined(ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS)
-        && (rgb_matrix_get_mode() != RGB_MATRIX_JELLYBEAN_RAINDROPS)
-#    endif
-#    if defined(ENABLE_RGB_MATRIX_PIXEL_RAIN)
-        && (rgb_matrix_get_mode() != RGB_MATRIX_PIXEL_RAIN)
-#    endif
-    ) {
-        return res;
-    }
-
-    if (res) {
-        if (led_state.caps_lock) {
-            uint8_t v = light_brightness_get();
-            rgb_matrix_set_color(CAPS_LOCK_LED_INDEX, v, v, v);
-        } else {
-            rgb_matrix_set_color(CAPS_LOCK_LED_INDEX, 0, 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 255, 255, 255);
+    } else {
+        if (!rgb_matrix_get_flags()) {
+           RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
         }
-        rgb_matrix_update_pwm_buffers();
     }
-
-    return res;
 }
-
 #endif // CAPS_LOCK_LED_INDEX
