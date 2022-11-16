e090b0aa32d36682269ba5fe69498df2ee6353f5
nopunin10did <w.alex.ronke@gmail.com>
2022-11-16 07:45:49 -0500

    Update keyboards/nopunin10did/jabberwocky/v2/keymaps/via/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/nopunin10did/jabberwocky/v2/keymaps/via/keymap.c
+++ keyboards/nopunin10did/jabberwocky/v2/keymaps/via/keymap.c
@@ -25,7 +25,7 @@ KC_NUM ,KC_PSLS,KC_PAST,KC_PMNS,        KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,
 KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,        KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_LBRC,KC_RBRC,KC_BSLS,                
 KC_P4  ,KC_P5  ,KC_P6  ,KC_PPLS,        KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,KC_ENT ,KC_ENT ,                
 KC_P1  ,KC_P2  ,KC_P3  ,KC_PENT,KC_LSFT,KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,MO(1),  MO(1),  KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,KC_RSFT,KC_UP  ,                
-KC_P0  ,KC_P0  ,KC_PDOT,KC_PENT,        KC_LCTL,KC_LGUI,        KC_LALT,KC_MENU,KC_SPC ,                KC_SPC ,        KC_ALGR,        KC_RGUI,KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT)        ,
+KC_P0  ,KC_P0  ,KC_PDOT,KC_PENT,        KC_LCTL,KC_LGUI,        KC_LALT,KC_APP ,KC_SPC ,                KC_SPC ,        KC_ALGR,        KC_RGUI,KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT)        ,
 
 
 [1] = LAYOUT_lh(
