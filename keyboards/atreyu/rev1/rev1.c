92f24f76af6a0705c73bc1594d4c878c42d3a4fe
Jesus Climent <climent@gmail.com>
2022-11-16 16:57:30 +0000

    Small fixes for the Atreyu rev1 keyboard.

--- keyboards/atreyu/rev1/rev1.c
+++ keyboards/atreyu/rev1/rev1.c
@@ -13,35 +13,3 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#ifdef ENCODER_ENABLE
-bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) {
-        return false; 
-    }
-
-    if (index == 1) {
-      if (clockwise) {
-          tap_code(KC_VOLU);
-      } else {
-          tap_code(KC_VOLD);
-      }
-    }
-    if (index == 0) {
-      if (IS_LAYER_ON(_LOWER)) {
-        if (clockwise) {
-            tap_code(KC_WH_U);
-        } else {
-            tap_code(KC_WH_D);
-        }
-      } else {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-      }
-    }
-    return true;
-}
-#endif
