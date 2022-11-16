f913946e6837e3ff06c0a97bec5b069c4378637e
Commander1024 <47283778+Commander1024@users.noreply.github.com>
2022-11-16 15:36:14 +0100

    Update keyboards/handwired/daskeyboard/daskeyboard4/daskeyboard4.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/daskeyboard/daskeyboard4/daskeyboard4.c
+++ keyboards/handwired/daskeyboard/daskeyboard4/daskeyboard4.c
@@ -3,6 +3,7 @@
 #include "quantum.h"
 
 // rotary encoder
+#ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) {
         return false;
