e38023428c3b0df52a1f4b913f5bc6822ffa5f32
Richard Dawe <richdawe@gmail.com>
2022-11-16 12:33:17 +0000

    Replace enter with backspace on bottom row

--- keyboards/dm9records/lain/keymaps/richdawe/keymap.c
+++ keyboards/dm9records/lain/keymaps/richdawe/keymap.c
@@ -30,13 +30,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------'     \-----------------------------------|
  * |LShft|  Z  /  X  /  C  /  V  /  B  /           \  B  \  N  \  M  \  ,  \  .  |ShEnt|
  * `-----------------------------------'           \-----------------------------------|
- *    |LCtrl|  /LGUI /LAlt /SpLSy/Space/           \Bspc \SpLNu\Enter\RAlt \  `  |  /  |
+ *    |LCtrl|  /LGUI /LAlt /SpLSy/Space/           \SpLNu\SpLNu\Bspc \RAlt \  `  |  /  |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         LTF_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SFTENT,
-        KC_LCTL,          KC_LGUI, KC_LALT, LTS_SPC, KC_SPC,             KC_BSPC, LTN_SPC, KC_ENT,  KC_RALT, KC_GRV,  KC_SLSH
+        KC_LCTL,          KC_LGUI, KC_LALT, LTS_SPC, KC_SPC,             LTN_SPC, LTN_SPC, KC_BSPC, KC_RALT, KC_GRV,  KC_SLSH
     ),
 
     [NUM] = LAYOUT(
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_COLON, KC_LPRN,   KC_MINS, KC_EQL,  KC_UP,    KC_0,    KC_DOT,  XXXXXXX,
         _______, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_DOT,   KC_RPRN,   XXXXXXX, KC_LEFT, KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_0,                XXXXXXX, XXXXXXX, KC_DOWN,  XXXXXXX, XXXXXXX, _______,
-        _______,          _______, _______, KC_MINS, KC_MINS,             XXXXXXX, _______, XXXXXXX,  _______, XXXXXXX, XXXXXXX
+        _______,          _______, _______, KC_MINS, KC_MINS,             _______, _______, XXXXXXX,  _______, XXXXXXX, XXXXXXX
     ),
 
     [SYM] = LAYOUT(
@@ -66,13 +66,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------'     \-----------------------------------|
  * |     |     /  &  /  *  /  (  /  )  /           \     \     \PgDn \     \     |     |
  * `-----------------------------------'           \-----------------------------------|
- *    |     |  /     /     /     /     /           \     \  -  \     \     \     |     |
+ *    |     |  /     /     /     /     /           \  -  \  -  \     \     \     |     |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         _______, XXXXXXX, S(KC_1), S(KC_2), S(KC_3), XXXXXXX, KC_LPRN,   S(KC_MINS), S(KC_EQL), KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, S(KC_4), S(KC_5), S(KC_6), XXXXXXX, KC_RPRN,   XXXXXXX,    KC_HOME,   KC_END,  XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, S(KC_7), S(KC_8), S(KC_9), S(KC_0),            XXXXXXX,    XXXXXXX,   KC_PGDN, XXXXXXX, XXXXXXX, _______,
-        _______,          _______, _______, _______, _______,            XXXXXXX,    KC_MINS,   XXXXXXX, _______, XXXXXXX, XXXXXXX
+        _______,          _______, _______, _______, _______,            KC_MINS,    KC_MINS,   XXXXXXX, _______, XXXXXXX, XXXXXXX
     ),
 
     [FUNC] = LAYOUT(
@@ -84,13 +84,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------'     \-----------------------------------|
  * |     |     / F7  / F8  / F9  /     /           \     \     \VMute\     \     |     |
  * `-----------------------------------'           \-----------------------------------|
- *    |Bootl|  / F10 / F11 / F12 /     /           \ Del \     \     \     \     |     |
+ *    |Bootl|  / F10 / F11 / F12 /     /           \     \     \ Del \     \     |     |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   XXXXXXX, KC_MPRV,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, XXXXXXX,
         KC_CAPS, LED_EN,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX, KC_MNXT,   XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, _______,
         _______, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,            XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, _______,
-        QK_BOOT,          KC_F10,  KC_F11,  KC_F12,  XXXXXXX,            KC_DEL,  XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX
+        QK_BOOT,          KC_F10,  KC_F11,  KC_F12,  XXXXXXX,            XXXXXXX, XXXXXXX, KC_DEL,  _______, XXXXXXX, XXXXXXX
     ),
 };
 
