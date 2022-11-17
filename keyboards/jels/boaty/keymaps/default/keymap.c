4143045ed21f9a0127cc8e2726a22d86b382a263
jels <66403163+Jels02@users.noreply.github.com>
2022-11-17 17:24:23 +1000

    Apply suggestions from code review
    Replace KC_NLCK with KC_NUM
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/jels/boaty/keymaps/default/keymap.c
+++ keyboards/jels/boaty/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //base layer
 [0] = LAYOUT_all(
-                                            KC_NLCK,
+                                            KC_NUM,
                                             KC_PAST, KC_PSLS,
                                             KC_P7,   KC_P8,   KC_P9,
                                             KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
