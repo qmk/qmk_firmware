8563439c51406fcc258f0eb1940b00265a93f27f
Lex Brugman <lex@squla.nl>
2022-11-19 21:28:56 +0100

    Use LAYOUT for odin/v2 since it only has one layout

--- keyboards/kbdfans/odin/v2/v2.h
+++ keyboards/kbdfans/odin/v2/v2.h
@@ -18,7 +18,7 @@
 
 #include "quantum.h"
 
-#define LAYOUT_all( \
+#define LAYOUT( \
     k00,      k02, k03, k04, k05,      k07, k08, k09, k0A, k0B, k0C, k0D, k0E, k0F, k0G, k0H, k0I, k0J, \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D,      k1F, k1G, k1H, k1I, k1J, \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, k2D,      k2F, k2G, k2H, k2I, k2J, \
