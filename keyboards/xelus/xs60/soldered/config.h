0fd2ec98e24af080104864362607423dac81dd4e
Xelus22 <preyas22@gmail.com>
2022-11-20 20:11:08 +1100

    remove animations

--- keyboards/xelus/xs60/soldered/config.h
+++ keyboards/xelus/xs60/soldered/config.h
@@ -43,7 +43,6 @@
 // RGBLIGHT
 #define RGB_DI_PIN A10
 #define RGBLED_NUM 8            // actually only has 1
-#define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_LAYERS
 #define WS2812_EXTERNAL_PULLUP
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
