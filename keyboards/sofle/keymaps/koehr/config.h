6b6a9344adca44aec22fe27c9005ef8a45d7c7c0
Norman Köhring <norman.koehring@spenoki.de>
2022-11-21 14:02:16 +0100

    update sofle:koehr

--- keyboards/sofle/keymaps/koehr/config.h
+++ keyboards/sofle/keymaps/koehr/config.h
@@ -1,24 +1,31 @@
- /* Copyright 2020 Josef Adamcik
-  * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-  */ 
+ /* Copyright 2022 Norman Köhring
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
 
 #pragma once
 
-#define ENCODER_RESOLUTION 2
+// #define MASTER_LEFT
+// #define MASTER_RIGHT
+#define EE_HANDS
 
+#define CUSTOM_LAYER_READ //if you remove this it causes issues - needs better guarding
+
+#define ENCODER_RESOLUTION 2
+// #define ENCODER_DIRECTION_FLIP
+
+// #define TAPPING_FORCE_HOLD
 #ifdef TAPPING_TERM
     #undef TAPPING_TERM
     #define TAPPING_TERM 200
