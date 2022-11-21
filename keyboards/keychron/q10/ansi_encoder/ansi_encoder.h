a6eb056768de295e6d8a675ced79209ed490219d
lalalademaxiya1 <2831039915@qq.com>
2022-11-21 15:42:04 +0800

    Add Keychron Q10

---
+++ keyboards/keychron/q10/ansi_encoder/ansi_encoder.h
@@ -0,0 +1,27 @@
+/*
+ * @Author: lalalademaxiya1 2831039915@qq.com
+ * @Date: 2022-08-23 23:29:21
+ * @LastEditors: lalalademaxiya1 2831039915@qq.com
+ * @LastEditTime: 2022-08-27 11:00:26
+ * @FilePath: /qmk_firmware/keyboards/keychron/q10/q10_ansi_stm32l432_ec11/q10_ansi_stm32l432_ec11.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/* Copyright 2021 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
