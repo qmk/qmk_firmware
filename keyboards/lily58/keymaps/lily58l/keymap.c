eb0bc1617444201b60e75259e6dc0569dac4666c
Jeremy Laskar <jeremy.laskar@fictiv.com>
2022-11-20 18:57:10 -1000

    replace pgm_read_byte with pgm_compressed_byte

--- keyboards/lily58/keymaps/lily58l/keymap.c
+++ keyboards/lily58/keymaps/lily58l/keymap.c
@@ -297,43 +297,55 @@ static const char PROGMEM mask_row_4_list[] = {
     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
 };
 
+static char get_compressed_byte(const char* input_block_list, const char* input_block_map, const int i) {
+    uint8_t bit = i%8;
+    uint8_t map_index = i/8;
+    uint8_t _block_map = (uint8_t)pgm_read_byte_near(input_block_map + map_index);
+    uint8_t nonzero_byte = (_block_map & (1 << bit));
+    if (nonzero_byte) {
+        return (const char)pgm_read_byte_near(input_block_list + i);
+    } else {
+        return (const char)0x00;
+    }
+}
+
 static void render_space(void) {
     char wpm = get_current_wpm();
     char render_row[128];
     int i;
     oled_set_cursor(0,0);
     for(i=0; i<wpm/4; i++) {
-        render_row[i] = pgm_read_byte(space_row_1+i+state);
+        render_row[i] = get_compressed_byte(space_row_1_list, space_row_1_map, i+state);
     };
     for(i=wpm/4; i<128; i++) {
-        render_row[i] = (pgm_read_byte(space_row_1+i+state)&pgm_read_byte(mask_row_1+i-wpm/4)) | pgm_read_byte(ship_row_1+i-wpm/4);
+        render_row[i] = (get_compressed_byte(space_row_1_list, space_row_1_map, i+state)&get_compressed_byte(mask_row_1_list, mask_row_1_map, i-wpm/4)) | get_compressed_byte(ship_row_1_list, ship_row_1_map, i-wpm/4);
     };
 
     oled_write_raw(render_row, 128);
     // oled_write_raw_P(space_row_1, 128);
     oled_set_cursor(0,1);
     for(i=0; i<wpm/4; i++) {
-        render_row[i] = pgm_read_byte(space_row_2+i+state);
+        render_row[i] = get_compressed_byte(space_row_2_list, space_row_2_map, i+state);
     };
     for(i=wpm/4; i<128; i++) {
-        render_row[i] = (pgm_read_byte(space_row_2+i+state)&pgm_read_byte(mask_row_2+i-wpm/4)) | pgm_read_byte(ship_row_2+i-wpm/4);
+        render_row[i] = (get_compressed_byte(space_row_2_list, space_row_2_map, i+state)&get_compressed_byte(mask_row_2_list, mask_row_2_map, i-wpm/4)) | get_compressed_byte(ship_row_2_list, ship_row_2_map, i-wpm/4);
     };
     oled_write_raw(render_row, 128);
     oled_set_cursor(0,2);
     for(i=0; i<wpm/4; i++) {
-        render_row[i] = pgm_read_byte(space_row_3+i+state);
+        render_row[i] = get_compressed_byte(space_row_3_list, space_row_3_map, i+state);
     };
     for(i=wpm/4; i<128; i++) {
-        render_row[i] = (pgm_read_byte(space_row_3+i+state)&pgm_read_byte(mask_row_3+i-wpm/4)) | pgm_read_byte(ship_row_3+i-wpm/4);
+        render_row[i] = (get_compressed_byte(space_row_3_list, space_row_3_map, i+state)&get_compressed_byte(mask_row_3_list, mask_row_3_map, i-wpm/4)) | get_compressed_byte(ship_row_3_list, ship_row_3_map, i-wpm/4);
     };
 
     oled_write_raw(render_row, 128);
     oled_set_cursor(0,3);
     for(i=0; i<wpm/4; i++) {
-        render_row[i] = pgm_read_byte(space_row_4+i+state);
+        render_row[i] = get_compressed_byte(space_row_4_list, space_row_4_map, i+state);
     };
     for(i=wpm/4; i<128; i++) {
-        render_row[i] = (pgm_read_byte(space_row_4+i+state)&pgm_read_byte(mask_row_4+i-wpm/4)) | pgm_read_byte(ship_row_4+i-wpm/4);
+        render_row[i] = (get_compressed_byte(space_row_4_list, space_row_4_map, i+state)&get_compressed_byte(mask_row_4_list, mask_row_4_map, i-wpm/4)) | get_compressed_byte(ship_row_4_list, ship_row_4_map, i-wpm/4);
     };
 
     oled_write_raw(render_row, 128);
