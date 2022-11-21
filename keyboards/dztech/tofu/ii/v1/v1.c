e23b576635ed37a0897ffb08ed39ce8cfbef8f82
moyi4681 <moyi4681@users.noreply.github.com>
2022-11-21 09:05:31 +0800

    update

--- keyboards/dztech/tofu/ii/v1/v1.c
+++ keyboards/dztech/tofu/ii/v1/v1.c
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#include "v1.h"
+ #include "is31fl3737.h"
+ #include "process_unicodemap.c"
 
 #ifdef RGB_MATRIX_ENABLE
 
@@ -29,28 +29,28 @@ const is31_led PROGMEM g_is31_leds[DRIVER_LED_TOTAL] = {
     { 1, K_5,  J_5,  L_5 },
     { 1, K_4,  J_4,  L_4 },
     { 1, K_3,  J_3,  L_3 },
-    { 1, K_2,  J_2,  L_2 },    
-    { 1, K_1,  J_1,  L_1 }, 
-    { 1, H_1,  G_1,  I_1 }, 
-    { 1, H_2,  G_2,  I_2 }, 
+    { 1, K_2,  J_2,  L_2 },
+    { 1, K_1,  J_1,  L_1 },
+    { 1, H_1,  G_1,  I_1 },
+    { 1, H_2,  G_2,  I_2 },
     { 1, H_3,  G_3,  I_3 },
-    
+
     { 0, B_12, A_12, C_12 },
-    { 0, B_11, A_11, C_11 }, 
+    { 0, B_11, A_11, C_11 },
     { 0, B_10, A_10, C_10 },
-    { 0, B_9,  A_9,  C_9 }, 
-    { 0, B_8,  A_8,  C_8 }, 
+    { 0, B_9,  A_9,  C_9 },
+    { 0, B_8,  A_8,  C_8 },
     { 0, B_7,  A_7,  C_7 },
     { 0, B_6,  A_6,  C_6 },
     { 0, B_5,  A_5,  C_5 },
     { 0, B_4,  A_4,  C_4 },
     { 0, B_3,  A_3,  C_3 },
     { 0, B_2,  A_2,  C_2 },
-    { 0, B_1,  A_1,  C_1 }, 
-    { 1, H_6,  G_6,  I_6 }, 
-    { 1, H_5,  G_5,  I_5 }, 
-    { 1, H_4,  G_4,  I_4 }, 
-    
+    { 0, B_1,  A_1,  C_1 },
+    { 1, H_6,  G_6,  I_6 },
+    { 1, H_5,  G_5,  I_5 },
+    { 1, H_4,  G_4,  I_4 },
+
     { 0, E_12, D_12, F_12 },
     { 0, E_11, D_11, F_11 },
     { 0, E_10, D_10, F_10 },
@@ -62,10 +62,10 @@ const is31_led PROGMEM g_is31_leds[DRIVER_LED_TOTAL] = {
     { 0, E_4,  D_4,  F_4 },
     { 0, E_3,  D_3,  F_3 },
     { 0, E_2,  D_2,  F_2 },
-    { 0, E_1,  D_1,  F_1 },          
-    { 1, H_7,  G_7,  I_7 }, 
-    { 1, H_8,  G_8,  I_8 },   
-    
+    { 0, E_1,  D_1,  F_1 },
+    { 1, H_7,  G_7,  I_7 },
+    { 1, H_8,  G_8,  I_8 },
+
     { 0, H_12, G_12, I_12 },
     { 0, H_11, G_11, I_11 },
     { 0, H_10, G_10, I_10 },
@@ -79,14 +79,14 @@ const is31_led PROGMEM g_is31_leds[DRIVER_LED_TOTAL] = {
     { 0, H_2,  G_2,  I_2 },
     { 0, H_1,  G_1,  I_1 },
     { 0, K_9,  J_9,  L_9 },
-    
+
     { 0, K_12, J_12, L_12 },
     { 0, K_11, J_11, L_11 },
     { 0, K_10, J_10, L_10 },
     { 0, K_7,  J_7,  L_7 },
     { 0, K_4,  J_4,  L_4 },
     { 0, K_2,  J_2,  L_2 },
-    { 0, K_1,  J_1,  L_1 },    
+    { 0, K_1,  J_1,  L_1 },
     { 0, K_6,  J_6,  L_6 },
     { 0, K_5,  J_5,  L_5 },
 };
