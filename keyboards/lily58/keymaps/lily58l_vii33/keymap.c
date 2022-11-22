6a286d1d18655478073f671a40e45e952833b8b9
vii33 <1887246+vii33@users.noreply.github.com>
2022-11-22 12:56:29 +0100

    swapped game layer accorind to main layer

--- keyboards/lily58/keymaps/lily58l_vii33/keymap.c
+++ keyboards/lily58/keymaps/lily58l_vii33/keymap.c
@@ -138,11 +138,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
   [_GAME] = LAYOUT(
-    KC_ESC,  KC_1,  KC_2,  KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,     KC_8,    KC_9,   KC_0,     GAME, 
-    KC_TAB,  KC_Q,  KC_W,  KC_E,    KC_R,    KC_T,                             KC_Z,    KC_U,     KC_I,    KC_O,   KC_P,     KC_PGUP, 
-    KC_LSFT, KC_A,  KC_S,  KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,     KC_K,    KC_L,   KC_F6,    KC_PGDN, 
-    KC_LCTL, KC_Y,  KC_X,  KC_C,    KC_V,    KC_B,    GMESCN,         KC_F12,  KC_N,    KC_M,     KC_LEFT, KC_UP,  KC_RIGHT, KC_DOWN, 
-                           KC_CAPS, KC_LALT, KC_SPC,  KC_SPC,         KC_ENT,  KC_F7,   KC_BSPC,  KC_DEL
+    KC_ESC,  KC_1,  KC_2,  KC_3,    KC_4,    KC_5,                             KC_6,     KC_7,   KC_8,    KC_9,   KC_0,     GAME, 
+    KC_TAB,  KC_Q,  KC_W,  KC_E,    KC_R,    KC_T,                             KC_Z,     KC_U,   KC_I,    KC_O,   KC_P,     KC_PGUP, 
+    KC_LSFT, KC_A,  KC_S,  KC_D,    KC_F,    KC_G,                             KC_H,     KC_J,   KC_K,    KC_L,   KC_F6,    KC_PGDN, 
+    KC_LCTL, KC_Y,  KC_X,  KC_C,    KC_V,    KC_B,    GMESCN,         KC_F12,  KC_N,     KC_M,   KC_LEFT, KC_UP,  KC_RIGHT, KC_DOWN, 
+                           KC_CAPS, KC_LALT, KC_SPC,  KC_SPC,         KC_ENT,  KC_BSPC,  KC_F7,  KC_DEL
 )
 
 };
