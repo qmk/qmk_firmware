4ccc43b8c78261253598734a2e8eea2e07cf7c6c
doesntfazer <39163867+doesntfazer@users.noreply.github.com>
2022-11-25 08:13:46 -0500

    Update keyboards/crowboard/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/crowboard/keymaps/default/keymap.c
+++ keyboards/crowboard/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ _ADJUST,
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_QWERTY] = LAYOUT_ortho_4x10(
+    [_QWERTY] = LAYOUT (
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         LCTL_T(KC_A),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
         LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,    KC_DOT, RSFT_T(KC_SLSH),
