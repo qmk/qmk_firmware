26fb59d9e0e374c2824fb66c4330d878964ccd37
doesntfazer <39163867+doesntfazer@users.noreply.github.com>
2022-11-25 08:15:00 -0500

    Update keyboards/crowboard/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/crowboard/keymaps/default/keymap.c
+++ keyboards/crowboard/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_LCTL,    LOWER,    KC_SPACE,    KC_BSPC,    RAISE,    KC_RALT
     ),
 
-    [_LOWER] = LAYOUT_ortho_4x10(
+    [_LOWER] = LAYOUT (
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_TAB,    KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,    KC_TRNS,    KC_MINUS,    KC_EQUAL,    KC_LBRC,    KC_RBRC,
         KC_LCTL,    KC_GRAVE,    KC_LGUI,    KC_LALT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_BSLS,    KC_SCLN,
