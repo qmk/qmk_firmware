846edb7a90390c74f8ffbdd50a45e027565d07d9
Callum Hart <github@callumh.art>
2022-11-26 22:01:27 +0000

    Add pcal6416a gina revision

---
+++ keyboards/cutie_club/keebcats/gina/rev2/matrix.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "quantum.h"
+#include "i2c_master.h"
+#include "pcal6416a/pcal6416a.h"
+
+static const pcal_gpio_pin row_pins[MATRIX_ROWS] = PCAL_ROW_PINS;
+static const pcal_gpio_pin column_pins[MATRIX_COLS] = PCAL_COL_PINS;
+
+static bool update_matrix_for_row(uint8_t row, matrix_row_t current_matrix[]) {
+    matrix_row_t previous_row_state = current_matrix[row];
+    current_matrix[row] = 0;
+
+    pcal_write_pin(row_pins[row], LOW);
+
+    for (uint8_t column = 0; column < MATRIX_COLS; column++) {
+        pcal_gpio_pin current_column_pin = column_pins[column];
+
+        if(pcal_read_pin(current_column_pin) == LOW) {
+            current_matrix[row] |= (1 << column);
+        }
+    }
+
+    pcal_write_pin(row_pins[row], HIGH);
+
+    return previous_row_state != current_matrix[row];
+}
+
+void matrix_init_custom(void) {
+    i2c_init();
+
+    for (uint8_t column = 0; column < MATRIX_COLS; column++) {
+        pcal_set_pin_direction(column_pins[column], INPUT_PULLUP);
+    }
+
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        pcal_set_pin_direction(row_pins[row], OUTPUT);
+        pcal_write_pin(row_pins[row], HIGH);
+    }
+}
+
+bool matrix_scan_custom(matrix_row_t current_matrix[]) {
+    bool matrix_has_changed = false;
+
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        if(update_matrix_for_row(row, current_matrix)) matrix_has_changed = true;
+    }
+    return matrix_has_changed;
+}
---
+++ keyboards/cutie_club/keebcats/gina/rev2/pcal6416a/pcal6416a.c
@@ -0,0 +1,43 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "i2c_master.h"
+#include "lib/i2c_helpers.h"
+#include "pcal6416a/pcal6416a.h"
+
+pcal_gpio_state pcal_read_pin(pcal_gpio_pin pin) {
+    uint8_t input_register = REGISTER_FOR_PIN(pin, INPUT);
+    uint8_t register_data;
+    i2c_readReg(PCAL_ADDRESS, input_register, &register_data, 1, 20);
+
+    pcal_gpio_state pin_state = (register_data >> GET_PIN_NUMBER(pin)) & 1;
+    return pin_state;
+}
+
+void pcal_write_pin(pcal_gpio_pin pin, pcal_gpio_state state) {
+    uint8_t output_register = REGISTER_FOR_PIN(pin, OUTPUT);
+    i2c_updateRegBit(PCAL_ADDRESS, output_register, GET_PIN_NUMBER(pin), state, 20);
+}
+
+static bool is_input(pcal_gpio_dir dir) {
+    if(dir == INPUT_PULLUP || dir == INPUT_PULLDOWN || dir == INPUT) return true;
+    return false;
+}
+
+void pcal_set_pin_direction(pcal_gpio_pin pin, pcal_gpio_dir dir) {
+    uint8_t direction_register = REGISTER_FOR_PIN(pin, CONFIGURATION);
+    bool input = is_input(dir);
+    i2c_updateRegBit(PCAL_ADDRESS, direction_register, GET_PIN_NUMBER(pin), input, 20);
+
+    if(input) {
+        uint8_t enable_biasing_register = REGISTER_FOR_PIN(pin, PULLUP_PULLDOWN_ENABLE);
+        bool biasing = (dir == INPUT_PULLUP || dir == INPUT_PULLDOWN);
+        i2c_updateRegBit(PCAL_ADDRESS, enable_biasing_register, GET_PIN_NUMBER(pin), biasing, 20);
+
+        if (biasing) {
+            uint8_t biasing_direction_register = REGISTER_FOR_PIN(pin, PULLUP_PULLDOWN_SELECT);
+            uint8_t biasing_direction = (dir == INPUT_PULLUP) ? 1 : 0;
+            i2c_updateRegBit(PCAL_ADDRESS, biasing_direction_register, GET_PIN_NUMBER(pin), biasing_direction, 20);
+        }
+    }
+}
