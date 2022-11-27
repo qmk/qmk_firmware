4af84dad4e9d000595c8aa8ea344174859104616
christian <christian.lueke@sonoware.de>
2022-11-27 22:23:19 +0100

    Add license

--- layouts/community/ortho_4x12/neo2marin/config.h
+++ layouts/community/ortho_4x12/neo2marin/config.h
@@ -1,20 +1,30 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
+/* Copyright 2018-2022 TheAmarin (@theamarin)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-//#define MACROS_ENABLE
+
+#pragma once
 
 /* Use I2C or Serial, not both */
 #define USE_SERIAL
 //#define USE_I2C
 
-/* Select hand configuration */
-
+// Tune the tapping term for dual-use `y`/`MOD3` key
 #define TAPPING_TERM 150
 
 #define NO_DEBUG
 #define NO_PRINT
 #define NO_ACTION_ONESHOT
 #define NO_ACTION_MACRO
-
-
-#endif
