fc712d4ee5c5525d6812a1bccd5c3fe310116aef
lalalademaxiya1 <2831039915@qq.com>
2022-11-28 09:45:13 +0800

    Update q8

--- keyboards/keychron/q8/q8.c
+++ keyboards/keychron/q8/q8.c
@@ -43,7 +43,6 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         return false;
     }
     switch (keycode) {
-#    ifdef RGB_MATRIX_ENABLE
         case RGB_TOG:
             if (record->event.pressed) {
                 switch (rgb_matrix_get_flags()) {
@@ -61,13 +60,14 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
                 rgb_matrix_enable();
             }
             return false;
-#    endif
     }
     return true;
 }
 
-__attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+        return false;
+    }
 
     if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 255, 255, 255);
@@ -76,6 +76,7 @@ __attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, 
             RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
         }
     }
+    return true;
 }
 
 #endif // CAPS_LOCK_LED_INDEX
