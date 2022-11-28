e1275070427c42dd0ffcb39f4bdf23b06d3d8d09
adophoxia <andyao1528@gmail.com>
2022-11-27 19:08:56 -0800

    Update callback used for Caps Lock LED Indicator
    Also use it as boolean due to recent `develop` merge with `master`.

--- keyboards/keychron/q4/q4.c
+++ keyboards/keychron/q4/q4.c
@@ -62,15 +62,20 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-__attribute__((weak)) void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+        return false;
+    }
     // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
     if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 255, 255, 255);
     } else {
         if (!rgb_matrix_get_flags()) {
-           RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
         }
     }
-    
+    return true;
 }
+
 #endif // CAPS_LOCK_LED_INDEX
