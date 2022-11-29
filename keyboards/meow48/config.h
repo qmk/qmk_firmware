99b58265d6114c4c274d11b50591844103e5c829
tsubuan_145 <88652606+tsubuan145@users.noreply.github.com>
2022-11-29 17:31:57 +0900

    Fixed config.h

--- keyboards/meow48/config.h
+++ keyboards/meow48/config.h
@@ -45,7 +45,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 8
     #define RGBLIGHT_SAT_STEP 8
     #define RGBLIGHT_VAL_STEP 8
-    #define RGBLIGHT_LIMIT_VAL  /* The maximum brightness level */
+    #define RGBLIGHT_LIMIT_VAL 100 /* The maximum brightness level */
     #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 
 /*== all animations enable ==*/
