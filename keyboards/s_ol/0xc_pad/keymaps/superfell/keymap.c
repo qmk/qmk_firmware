24ab40ded2347a2ba7bb72957fb47022b91c9364
Simon Fell <fellforce@gmail.com>
2022-11-29 20:50:18 -0800

    improvements from PR review

--- keyboards/s_ol/0xc_pad/keymaps/superfell/keymap.c
+++ keyboards/s_ol/0xc_pad/keymaps/superfell/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (biton32(state)) {
+    switch (get_highest_layer(state)) {
         case _NUMPAD:
             rgblight_sethsv_noeeprom(170, 255, 128);
             rgblight_mode_noeeprom(2);
