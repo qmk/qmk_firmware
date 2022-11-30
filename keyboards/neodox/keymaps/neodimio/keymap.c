681345fd62ed23b3466735fe3ff4dfcbc4f50a22
Matteo Dal Molin <matteodalmo@gmail.com>
2022-11-30 18:29:58 +0100

    update neodimio keymap

--- keyboards/neodox/keymaps/neodimio/keymap.c
+++ keyboards/neodox/keymaps/neodimio/keymap.c
@@ -212,9 +212,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,                                            KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                           ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO   ,KC_RCX   ,KC_RCC  ,KC_MS_U ,KC_RCV  ,KC_NO  ,                                            KC_NO   ,KC_NO   ,KC_WH_U ,KC_NO   ,KC_NO   ,KC_NO   ,
+     KC_NO   ,KC_RCX   ,KC_RCC  ,KC_MS_U ,KC_RCV ,KC_WH_U ,                                            KC_NO   ,KC_NO   ,KC_WH_U ,KC_NO   ,KC_NO   ,KC_NO   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                           ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO   ,KC_RSGS ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_NO   ,                                            KC_ENT  ,KC_WH_L ,KC_WH_D ,KC_WH_R ,KC_NO   ,KC_NO   ,
+     KC_NO   ,KC_RSGS ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_WH_D ,                                            KC_ENT  ,KC_WH_L ,KC_WH_D ,KC_WH_R ,KC_NO   ,KC_NO   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┬────────┐       ┌────────┬────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TRNS ,KC_NO   ,KC_BTN4 ,KC_BTN3 ,KC_BTN5 ,KC_NO   ,KC_TRNS ,KC_TRNS ,        KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_TRNS ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
@@ -239,7 +239,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FUNC] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,                                            BL_TOGG ,BL_INC  ,BL_DEC  ,KC_NO   ,KC_NO   ,KC_NO   ,
+     KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,                                            KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                           ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,                                            KC_NO   ,KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,KC_NO   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                           ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -270,7 +270,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_ADJUST] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,                                            KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,
+     KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,                                            BL_TOGG ,BL_INC  ,BL_DEC  ,KC_NO   ,KC_NO   ,KC_NO   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                           ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,                                            RGB_M_X ,RGB_M_SW,RGB_M_K ,RGB_M_G ,RGB_M_SN,RGB_M_B ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                                           ├────────┼────────┼────────┼────────┼────────┼────────┤
