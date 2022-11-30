b39304221d7a88c123df0382ed556e11cce518e6
moritz plattner <moritz.plattner@gmx.net>
2022-11-30 01:16:41 +0100

    prepared for OLED keymaps

--- keyboards/ebastler/e80_1800/config.h
+++ keyboards/ebastler/e80_1800/config.h
@@ -3,3 +3,20 @@
 #define BACKLIGHT_PWM_DRIVER PWMD1
 #define BACKLIGHT_PWM_CHANNEL 2
 #define BACKLIGHT_PAL_MODE 2
+
+
+#ifdef OLED_ENABLE
+
+/* I2C for OLED - only enable defines if driver is enabled */
+#define I2C_DRIVER I2CD2
+#define I2C1_SCL_PIN        B10
+#define I2C1_SDA_PIN        B11
+#define I2C1_SCL_PAL_MODE   1
+#define I2C1_SDA_PAL_MODE   1
+#define I2C1_TIMINGR_PRESC  0U
+#define I2C1_TIMINGR_SCLDEL 3U
+#define I2C1_TIMINGR_SDADEL 1U
+#define I2C1_TIMINGR_SCLH   3U
+#define I2C1_TIMINGR_SCLL   9U
+
+#endif
