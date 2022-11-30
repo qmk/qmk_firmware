dfe1f8fb7c6cc474cc19f77bfc3999e705018ede
Cobbleston <canovasimo@gmail.com>
2022-11-30 10:41:21 +0100

    fixed typo

--- keyboards/cantor/keymaps/Cobbleston/keymap.c
+++ keyboards/cantor/keymaps/Cobbleston/keymap.c
@@ -34,7 +34,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_split_3x6_3(
 		CAPS_WORD,	KC_Q,			KC_W,			KC_E,			KC_R,			KC_T,	/**/	KC_Y,	KC_U,			KC_I, 			KC_O, 			KC_P, 				KC_PSCR, 
-		KC_NO,		LGUI_T(KC_A),	LALT_T(KC_S),	LCTL_T(KC_D),	LSFT_T(KC_F),	KC_G,	/**/	KC_H,	RSFT_T(KC_J),	RCTL_T(KC_K),	RALT_T(KC_L),	RGUI_T(KC_SCNL),	KC_NO,
+		KC_NO,		LGUI_T(KC_A),	LALT_T(KC_S),	LCTL_T(KC_D),	LSFT_T(KC_F),	KC_G,	/**/	KC_H,	RSFT_T(KC_J),	RCTL_T(KC_K),	RALT_T(KC_L),	RGUI_T(KC_SCLN),	KC_NO,
 		KC_NO, 		KC_Z,		 	KC_X,			KC_C,			KC_V,			KC_B,	/**/	KC_N,	KC_M,			KC_COMM,		KC_DOT,			KC_SLSH,			KC_NO,
 								LT(NUMBER, KC_DEL),	LT(ARROW, KC_TAB),	LT(MOUSE, KC_ESC),	/**/	LT(MISC, KC_ENT),	LT(SYM, KC_SPC),	KC_BSPC
 	),
