b100d5c1e01a84d01e49886c436354df3c3d2b4c
Omni <omni801@protonmail.com>
2022-12-04 17:45:11 +0100

    Revert "Saving some space"
    This reverts commit 3f946b48eeec70d377c2b09168b9a56434a3bb08.

--- keyboards/splitkb/aurora/sweep/keymaps/eanilsen/rules.mk
+++ keyboards/splitkb/aurora/sweep/keymaps/eanilsen/rules.mk
@@ -1,9 +1,10 @@
-LTO_ENABLE = yes		# Link time optimization
-MOUSEKEY_ENABLE = yes		# Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes		# Audio control and System control(+450)
-NKRO_ENABLE = yes		# Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 CAPS_WORD_ENABLE = yes
 SEND_STRING_ENABLE = yes
 
 SRC += features/swapper.c
+SRC += features/select_word.c
 SRC += features/custom_shift_keys.c
