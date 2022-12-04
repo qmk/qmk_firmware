c2e3603c9cc3af8fa9b35bc704ccf172fb475de2
christian <christian.lueke@sonoware.de>
2022-12-04 15:32:52 +0100

    Remove reduntant default settings from config.h

--- layouts/community/ortho_4x12/neo2marin/config.h
+++ layouts/community/ortho_4x12/neo2marin/config.h
@@ -16,20 +16,8 @@
 
 #pragma once
 
-/* Use I2C or Serial, not both */
-#define USE_SERIAL
-//#define USE_I2C
-
 // Tune the tapping term for dual-use `y`/`MOD3` key
 #define TAPPING_TERM 150
-
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif
-
-#ifndef NO_PRINT
-#define NO_PRINT
-#endif
 
 #define NO_ACTION_ONESHOT
 #define NO_MUSIC_MODE
