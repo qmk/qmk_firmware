c865e7ff19fa09d44354d2c26aeb11b84034a6bd
RIghtBUser <119793288+RIghtBUser@users.noreply.github.com>
2022-12-04 22:20:47 +0900

    Update config.h
    Changed wrong B7 pins to correct F4 pin
    Add soft serial pin to flash.

--- keyboards/handwired/dactyl_manuform/6x7/config.h
+++ keyboards/handwired/dactyl_manuform/6x7/config.h
@@ -26,9 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 7
 
 // wiring of each half
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, B7 }
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, F4 }
 #define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2, B6 }
-
+#define SOFT_SERIAL_PIN D0
 #define DIODE_DIRECTION COL2ROW
 
 // WS2812 RGB LED strip input and number of LEDs
