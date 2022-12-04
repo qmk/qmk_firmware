24bc1440b795055cbd86cd2c498142bfe26f24bf
Leo Deng <myst.dg@gmail.com>
2022-12-04 13:57:21 +0800

    Update keyboards/deng/thirty/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/deng/thirty/rules.mk
+++ keyboards/deng/thirty/rules.mk
@@ -1,9 +1,6 @@
 MCU = STM32F103
 BOOTLOADER = stm32duino
 
-# MCU = STM32F072
-# BOOTLOADER = stm32-dfu
-
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
