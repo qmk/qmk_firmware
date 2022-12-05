a73ab65be0c5f864915cc2e27034e0652c164475
EnviousData <Enviousdata@gmail.com>
2022-12-05 13:49:37 +0000

    Update keyboards/enviousdesign/tkl/rgb/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/enviousdesign/tkl/rgb/keymaps/default/keymap.c
+++ keyboards/enviousdesign/tkl/rgb/keymaps/default/keymap.c
@@ -65,13 +65,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-void suspend_power_down_user(void) {
-    rgb_matrix_set_suspend_state(true);
-}
-
-void suspend_wakeup_init_user(void) {
-    rgb_matrix_set_suspend_state(false);
-}
 
 /*
 void rgb_matrix_indicators_kb(void) {
