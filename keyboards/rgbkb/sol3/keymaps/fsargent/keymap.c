3afc54ae646cccedcd02c117cb9b1408f5c6b9bc
Felix Sargent <fsargent@users.noreply.github.com>
2022-12-06 11:04:59 -0800

    Game layout now has dedicated esc key

--- keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
@@ -128,10 +128,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 
 	[_GAME] = LAYOUT(
-	_______,	_______,	_______,	_______,	_______,	_______,	KC_ESC,					_______,	_______,	_______,	_______,	_______,	_______,	KC_BSPC,
+	_______,	_______,	_______,	_______,	_______,	_______,	TO(_WIN),				_______,	_______,	_______,	_______,	_______,	_______,	KC_BSPC,
 	_______,	_______,	_______,	_______,	_______,	_______,	KC_GRV,					_______,	_______,	_______,	_______,	_______,	_______,	_______,
-	MEHESC,	_______,	_______,	_______,	_______,	_______,	_______,					KC_GRV,	_______,	_______,	_______,	_______,	_______,	_______,
-	_______,	_______,	_______,	_______,	_______,	_______,	_______,					KC_ESC,	_______,	_______,	_______,	_______,	_______,	_______,
+	KC_ESC,	_______,	_______,	_______,	_______,	_______,	_______,					KC_ESC,	_______,	_______,	_______,	_______,	_______,	_______,
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,					KC_GRV,	_______,	_______,	_______,	_______,	_______,	_______,
 
 	_______,	_______,	_______,	_______,	MO(_WINNAV),
 	KC_SPC,	MT(MOD_LCTL, KC_BSPC), MT(MOD_LALT, KC_SPC),
@@ -173,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	MEH(KC_Q),	MEH(KC_W),	MEH(KC_E),	MEH(KC_R),	MEH(KC_T),	_______,						_______,	MEH(KC_Y),	MEH(KC_U),	MEH(KC_I),	MEH(KC_O),	MEH(KC_P),	_______,
 	_______,	MEH(KC_A),	MEH(KC_S),	MEH(KC_D),	MEH(KC_F),	MEH(KC_G),	_______,						KC_VOLU,	MEH(KC_H),	MEH(KC_J),	MEH(KC_K),	MEH(KC_L),	MEH(KC_SCLN),	_______,
 	_______,	MEH(KC_Z),	MEH(KC_X),	MEH(KC_C),	MEH(KC_V),	MEH(KC_B),	_______,						KC_VOLD,	MEH(KC_N),	MEH(KC_M),	MEH(KC_COMM),	MEH(KC_DOT),	MEH(KC_SLSH),	_______,
-	_______,	_______,		RGB_RMOD,		RGB_TOG,		RGB_MOD, 		TOG(_WIN),	TOG(_GAME),	TO(_MAC),
+	_______,	_______,		RGB_RMOD,		RGB_TOG,		RGB_MOD, 		TG(_WIN),	TG(_GAME),	TO(_MAC),
 																																														KC_MNXT,	KC_MPRV,	KC_MPLY,
 																																																												TO(_MAC),	TO(_WIN),		TO(_GAME),		TO(_SYM),		_______,
 
