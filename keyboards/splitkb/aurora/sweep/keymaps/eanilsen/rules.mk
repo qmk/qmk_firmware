afda335146508d32e43230cf083d393340b51d40
Even Adrian Nilsen <even.adrian.nilsen@dnb.no>
2022-12-06 14:23:14 +0100

    LTO, RGB off and cleanup

--- keyboards/splitkb/aurora/sweep/keymaps/eanilsen/rules.mk
+++ keyboards/splitkb/aurora/sweep/keymaps/eanilsen/rules.mk
@@ -1,10 +1,10 @@
-
-MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-CAPS_WORD_ENABLE = yes
-SEND_STRING_ENABLE = yes
+LTO_ENABLE 		= yes	# Link time optimisation
+MOUSEKEY_ENABLE		= yes	# Mouse keys(+4700)
+EXTRAKEY_ENABLE 	= yes	# Audio control and System control(+450)
+NKRO_ENABLE 		= yes	# Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+CAPS_WORD_ENABLE 	= yes
+SEND_STRING_ENABLE 	= yes
+RGB_MATRIX_ENABLE	= no	# Disable the per-key LEDs
 
 SRC += features/swapper.c
-SRC += features/select_word.c
 SRC += features/custom_shift_keys.c
