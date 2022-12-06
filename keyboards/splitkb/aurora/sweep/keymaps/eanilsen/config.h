f1b19966a249a15e14d978de7152c9eeec24123f
Even Adrian Nilsen <even.adrian.nilsen@dnb.no>
2022-12-06 15:11:31 +0100

    GPL2 header on *.h and *.c

--- keyboards/splitkb/aurora/sweep/keymaps/eanilsen/config.h
+++ keyboards/splitkb/aurora/sweep/keymaps/eanilsen/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2022 splitkb.com <support@splitkb.com>
+/* Copyright 2022 Even A. Nilsen (@eanilsen)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
