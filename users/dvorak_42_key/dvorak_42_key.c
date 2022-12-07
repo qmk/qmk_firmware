0aa350ffe576c09ac6e4d39ee7f66d98fd5863f2
Luc <luc.wastiaux@xsmail.com>
2022-12-07 08:26:26 +0800

    Update users/dvorak_42_key/dvorak_42_key.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- users/dvorak_42_key/dvorak_42_key.c
+++ users/dvorak_42_key/dvorak_42_key.c
@@ -84,7 +84,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING(SS_LCTL("a") "d");
             break;
         case SCREEN_RENAME:
-            SEND_STRING(SS_LCTL("a") SS_LSFT("a"));
+            SEND_STRING(SS_LCTL("a") "A");
             break;
         case SCREEN_NUMBER:
             SEND_STRING(SS_LCTL("a") ":number ");
