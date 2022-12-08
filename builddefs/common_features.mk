85ee55ff3becd29e1590376c3462c6a99c976fa0
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2022-12-08 16:45:30 +0000

    Detect host OS based on USB fingerprint (#18463)
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -907,3 +907,11 @@ ifeq ($(strip $(ENCODER_ENABLE)), yes)
         OPT_DEFS += -DENCODER_MAP_ENABLE
     endif
 endif
+
+ifeq ($(strip $(OS_DETECTION_ENABLE)), yes)
+    SRC += $(QUANTUM_DIR)/os_detection.c
+    OPT_DEFS += -DOS_DETECTION_ENABLE
+    ifeq ($(strip $(OS_DETECTION_DEBUG_ENABLE)), yes)
+        OPT_DEFS += -DOS_DETECTION_DEBUG_ENABLE
+    endif
+endif
