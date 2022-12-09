b0adb6eee1b5da37779ff234512d2be27785ca77
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2022-12-08 23:56:38 -0700

    Deleting file based on review and recommendation.

--- keyboards/clickety_split/leeloo/keymaps/default_led/rules.mk
+++
@@ -1,3 +0,0 @@
-# Intentionally left blank.
-# See qmk_firmware\keyboards\clickety_split\leeloo\rules.mk for default settings.
-# See qmk_firmware\keyboards\clickety_split\leeloo\rev2\rules.mk for overridden settings.
