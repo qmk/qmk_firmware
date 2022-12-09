0c39bec8d36960fab63bb160286e6172ab24bb3e
PeterFalken <luis@bitjester.com>
2022-12-09 10:28:56 -0600

    chore: disable RGB underglow

---
+++ keyboards/work_louder/micro/keymaps/PeterFalken/rules.mk
@@ -0,0 +1,18 @@
+# Enable VIAL support when merged into QMK
+# ENABLE_VIAL = yes
+
+# Setup QMK features
+BOOTMAGIC_ENABLE = no	# Won't use for now
+DIP_SWITCH_ENABLE = no	# Won't use for now
+COMMAND_ENABLE = no		# Won't use for now
+CONSOLE_ENABLE = no		# Won't use for now
+ENCODER_ENABLE = yes	# Enable both encoders
+GRAVE_ESC_ENABLE = no	# Not needed
+MAGIC_ENABLE = no		# Won't use for now
+MOUSEKEY_ENABLE = no	# Won't use for now
+RAW_ENABLE = no			# Disable raw HID commands
+RGBLIGHT_ENABLE = no	# Disable RGB underglow
+SPACE_CADET_ENABLE = no	# Free up space
+
+# Enable Link Time Optimizations
+LTO_ENABLE = yes
