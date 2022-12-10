6a301749bf90927f76aa596aeb7374d8b4ed68d6
4pplet <mail@4pplet.com>
2022-12-10 10:00:13 +0100

    cont

--- keyboards/4pplet/ASK/rev_a/config.h
+++ keyboards/4pplet/ASK/rev_a/config.h
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 14
 // ROWS: Top to bottom, COLS: Left to right
 
-#define MATRIX_ROW_PINS {B14,A9,B6,B5,B4}
-#define MATRIX_COL_PINS {B2,A4,A1,A2,F1,F0,C15,C14,C13,B9,B8,B7,A15,B3}
+#define MATRIX_ROW_PINS {B14,PB15,B6,B5,B4}
+#define MATRIX_COL_PINS {B2,A4,A1,A2,F1,F0,C15,C14,C13,B9,B8,B7,B12,B3}
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -38,9 +38,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLED_NUM 12
 #define WS2812_EXTERNAL_PULLUP
 #define RGB_DI_PIN A8
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
 
 #define BACKLIGHT_LEVELS 5
 
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#define LED_CAPS_LOCK_PIN A10
+#define NEXT_LED_L A9
+#define NEXT_LED_R A15
--- keyboards/4pplet/ask/rev_a/rev_a.c
+++ keyboards/4pplet/ask/rev_a/rev_a.c
@@ -14,4 +14,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "rev_d.h"
+#include "rev_a.h"
