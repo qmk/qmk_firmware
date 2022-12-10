2c86f24f2e71c19e878c0e7f047301f9f5c0c921
vii33 <1887246+vii33@users.noreply.github.com>
2022-12-10 16:19:34 +0100

    Fix for right oled

--- keyboards/lily58/keymaps/lily58l_vii33/oled.c
+++ keyboards/lily58/keymaps/lily58l_vii33/oled.c
@@ -32,7 +32,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
         return OLED_ROTATION_270;  
     } else {
-        return OLED_ROTATION_270;
+        return OLED_ROTATION_0;
     }
 }
 
