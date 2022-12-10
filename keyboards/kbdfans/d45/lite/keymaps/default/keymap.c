7af5d9b0f9cced0576914c3ccb0f2580c51330ba
dztech <moyi4681@users.noreply.github.com>
2022-12-10 19:21:48 +0800

    Update keyboards/kbdfans/d45/lite/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/kbdfans/d45/lite/keymaps/default/keymap.c
+++ keyboards/kbdfans/d45/lite/keymaps/default/keymap.c
@@ -21,19 +21,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    KC_F2,  KC_CAPS,   KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,  KC_HOME,
 		    KC_F3,  KC_LSFT,   KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,   KC_END,
 		    KC_F4,  KC_LCTL,   KC_LALT,            KC_SPC,       KC_SPC,         KC_RALT, KC_RCTL,                   KC_LEFT,  KC_DOWN, KC_RIGHT),
-		[1] = LAYOUT( /* FN */
-		    KC_ESC, KC_TAB,    KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,
-		    KC_F2,  KC_CAPS,   KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,  KC_HOME,
-		    KC_F3,  KC_LSFT,   KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,   KC_END,
-		    KC_F4,  KC_LCTL,   KC_LALT,            KC_SPC,       KC_SPC,         KC_RALT, KC_RCTL,                   KC_LEFT,  KC_DOWN, KC_RIGHT),
- 		[2] = LAYOUT( /* FN */
-		    KC_ESC, KC_TAB,    KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,
-		    KC_F2,  KC_CAPS,   KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,  KC_HOME,
-		    KC_F3,  KC_LSFT,   KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,   KC_END,
-		    KC_F4,  KC_LCTL,   KC_LALT,            KC_SPC,       KC_SPC,         KC_RALT, KC_RCTL,                   KC_LEFT,  KC_DOWN, KC_RIGHT),
-		[3] = LAYOUT( /* FN */
-		    KC_ESC, KC_TAB,    KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,
-		    KC_F2,  KC_CAPS,   KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,  KC_HOME,
-		    KC_F3,  KC_LSFT,   KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,   KC_END,
-		    KC_F4,  KC_LCTL,   KC_LALT,            KC_SPC,       KC_SPC,         KC_RALT, KC_RCTL,                   KC_LEFT,  KC_DOWN, KC_RIGHT),
  };
