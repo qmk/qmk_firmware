9dc3f791965e4c54bc969dcc2f2c7597f59e2262
Stefan Kerkmann <karlk90@pm.me>
2022-12-11 14:04:29 +0100

    [RP2040] update i2c drivers to reflect peripheral number (#19277)

--- keyboards/handwired/onekey/kb2040/config.h
+++ keyboards/handwired/onekey/kb2040/config.h
@@ -16,7 +16,7 @@
 
 // settings for the oled keyboard demo with Adafruit 0.91" OLED display on the Stemma QT port
 #define OLED_DISPLAY_128X32
-#define I2C_DRIVER I2CD1
+#define I2C_DRIVER I2CD0
 #define I2C1_SDA_PIN GP12
 #define I2C1_SCL_PIN GP13
  
