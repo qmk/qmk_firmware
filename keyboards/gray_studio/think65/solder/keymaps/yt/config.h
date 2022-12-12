b9a6b99bd26a20cf107ae2a49ef15e9d064620f1
Mona Lisa <youturn45@gmail.com>
2022-12-12 00:29:28 -0500

    Added win/mac switch

---
+++ keyboards/gray_studio/think65/solder/keymaps/yt/config.h
@@ -0,0 +1,8 @@
+#define RGBLIGHT_LAYERS
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
+#define DISABLE_LEADER
---
+++ keyboards/gray_studio/think65/solder/keymaps/yt/rules.mk
@@ -0,0 +1,4 @@
+LTO_ENABLE = yes
+VIA_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+KEY_LOCK_ENABLE = yes
