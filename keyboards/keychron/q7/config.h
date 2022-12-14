d7fbe8ea1c70fc2696972bcd3beafdef6673b650
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2022-12-14 10:15:03 +0800

    Update keyboards/keychron/q7/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/keychron/q7/config.h
+++ keyboards/keychron/q7/config.h
@@ -16,10 +16,6 @@
 
 #pragma once
 
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 16
-
 /* key matrix pins */
 #define MATRIX_ROW_PINS { B4, B3, A15, A14, A13 }
 #define MATRIX_COL_PINS { C14, C15, A0, A1, A2, A3, A4, A5, A6, A7, B0, B1, A8, A9, A10, B5 }
