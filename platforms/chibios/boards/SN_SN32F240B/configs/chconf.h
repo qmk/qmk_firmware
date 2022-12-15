6df99f2444dd939b1c74583d5b26493dc0fe411e
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-12-15 11:11:20 +0200

    SN32: leverage the hybrid CT+SysTick ST freerunning mode

--- platforms/chibios/boards/SN_SN32F240B/configs/chconf.h
+++ platforms/chibios/boards/SN_SN32F240B/configs/chconf.h
@@ -21,9 +21,9 @@
 
 #pragma once
 
-#define CH_CFG_ST_FREQUENCY 10000
+#define CH_CFG_ST_RESOLUTION 16
 
-#define CH_CFG_ST_TIMEDELTA 0
+#define CH_CFG_ST_FREQUENCY 187500
 
 #include_next <chconf.h>
 
