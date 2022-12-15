fc77086f7385224a6f7daa23ff95bfef2da9a0af
tan-t <lee30x@gmail.com>
2022-12-15 21:02:01 +0900

    update mothwing configurations

--- keyboards/mothwing/rules.mk
+++ keyboards/mothwing/rules.mk
@@ -14,7 +14,7 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow. 
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
 
 DEFAULT_FOLDER = mothwing/rev1
