d1e6703b67ee76a850ee296dd5893b256421fa32
mechlovin <57231893+mechlovin@users.noreply.github.com>
2022-12-15 11:07:07 +0700

    Add a GPL2+ compatible license

--- keyboards/mechlovin/zed1800/config.h
+++ keyboards/mechlovin/zed1800/config.h
@@ -52,3 +52,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
+
+#define WEAR_LEVELING_BACKING_SIZE 4096
+#define WEAR_LEVELING_LOGICAL_SIZE 2048
