ea2cab99e4bb868c26115a5cbbb886f9563d6594
4pplet <mail@4pplet.com>
2022-12-16 17:20:33 +0100

    Update rev_a.c

--- keyboards/4pplet/ask/rev_a/rev_a.c
+++ keyboards/4pplet/ask/rev_a/rev_a.c
@@ -15,3 +15,25 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "rev_a.h"
+
+void board_init(void) {
+    setPinInputHigh(NEXT_LED_L);
+    setPinInputHigh(NEXT_LED_R);
+}
+
+/* Set indicator leds to indicate lock states */
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        if(led_state.caps_lock){
+            setPinOutput(NEXT_LED_L);
+            setPinOutput(NEXT_LED_R);
+            writePin(NEXT_LED_L, 0);
+            writePin(NEXT_LED_R, 0);
+        }
+        else
+            setPinInputHigh(NEXT_LED_L);
+            setPinInputHigh(NEXT_LED_R);
+    }
+    return res;
+}
