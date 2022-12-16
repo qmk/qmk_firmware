fd1ea167ab37d18fbe119e7e8e96ae957e99a05e
Danny Nguyen <danny@keeb.io>
2022-12-15 19:00:05 -0500

    Initial commit for Cepstrum

---
+++ keyboards/keebio/cepstrum/cepstrum.h
@@ -0,0 +1,10 @@
+// Copyright 2022 Danny Nguyen (danny@keeb.io)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "quantum.h"
+
+#ifdef KEYBOARD_keebio_cepstrum_rev1
+    #include "rev1.h"
+#endif
