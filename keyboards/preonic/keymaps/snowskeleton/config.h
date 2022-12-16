1fa3eca44afee2ec8ae6fb87c725dcc4c3390973
Isaac Lyons <isaac@snowskeleton.net>
2022-12-16 13:48:42 -0500

    cleaned up and commented unused code

--- keyboards/preonic/keymaps/snowskeleton/config.h
+++ keyboards/preonic/keymaps/snowskeleton/config.h
@@ -17,11 +17,9 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(PREONIC_SOUND)
-// #define STARTUP_SONG SONG(NO_SOUND)
+#define STARTUP_SONG SONG(PREONIC_SOUND)
+// #define STARTUP_SONG SONG(NO_SOUND) // uncomment to silence startup sounds
 
-#    define DEFAULT_LAYER_SONGS \
-        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
 #endif
 #define MUSIC_MASK (keycode != KC_NO)
 #define MIDI_BASIC
