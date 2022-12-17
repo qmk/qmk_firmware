d1617b0df67fddd0fb051b01505514199a5ce818
Aaron Hong <hong.aaron.c@gmail.com>
2022-12-16 21:19:06 -0800

    Fixed RGB Matrix config for Synth Labs 060

--- keyboards/synthlabs/060/060.h
+++ keyboards/synthlabs/060/060.h
@@ -10,7 +10,7 @@
 // This a shortcut to help you visually see your layout.
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
-#define LAYOUT_all(                                                         \
+#define LAYOUT(                                                         \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, k12, k13,   \
     k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, k24, k25, k26, k27,   \
     k28, k29, k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k40, k41,   \
