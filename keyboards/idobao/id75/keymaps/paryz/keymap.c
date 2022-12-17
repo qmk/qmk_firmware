0f9912d585b78d5097ff1b3cf15c5e442fda1f81
Filip Paryż <paryz.filip@gmail.com>
2022-12-17 11:36:33 +0100

    Keymap fix

--- keyboards/idobao/id75/keymaps/paryz/keymap.c
+++ keyboards/idobao/id75/keymaps/paryz/keymap.c
@@ -78,26 +78,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_FN] = LAYOUT_ortho_5x15(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_NUM,  KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    KC_PSCR, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_NO,   KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS,
+    KC_PSCR, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_MPRV, KC_MPLY, KC_MNXT, RGB_HUI, KC_NO,   KC_NO,    KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_NO,   QK_BOOT, KC_NO,   KC_NO,   KC_NO,
     KC_VOLD, KC_MUTE, KC_VOLU, RGB_SAI, RGB_SPI, KC_NO,    KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_NO,   KC_NO,   RGB_TOG, RGB_VAI, RGB_MOD, KC_NO,    KC_P0,   KC_NO,   KC_PDOT, KC_PENT, KC_PENT, KC_NO,   KC_NO,   KC_NO,   KC_NO
  ),
 
  [_LOWER] = LAYOUT_ortho_5x15(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_NUHS, KC_NUBS, KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P0,   KC_NO,   KC_PDOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_NUHS, KC_NUBS, KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P0,   KC_NO,   KC_PDOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
 ),
 
 [_RAISE] = LAYOUT_ortho_5x15(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_NUHS, KC_NUBS, KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P0,   KC_NO,   KC_PDOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    KC_NO,   KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO,   KC_NUHS, KC_NUBS, KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_P0,   KC_NO,   KC_PDOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
 )
 };
 
