2fbe7b883db661278314db0f58a97a780eaf2628
Filip Paryż <paryz.filip@gmail.com>
2022-12-17 10:09:35 +0100

    Fixes suggested by reviewers.

--- keyboards/idobao/id75/keymaps/paryz/keymap.c
+++ keyboards/idobao/id75/keymaps/paryz/keymap.c
@@ -45,10 +45,12 @@ led_config_t custom_led_config = { {
 #endif
 
 // Keyboard Layers
-#define _QWERTY 0
-#define _FN 1
-#define _LOWER 2
-#define _RAISE 3
+enum layers {
+    _QWERTY,
+    _FN,
+    _LOWER,
+    _RAISE
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -67,19 +69,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT_ortho_5x15( /* QWERTY */
-    KC_GESC,            KC_1,   KC_2,    KC_3,    KC_4,   KC_5,  KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-    KC_TAB,             KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,  KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT, \
-    LT(_LOWER,KC_CAPS), KC_A,   KC_S,    KC_D,    KC_F,   KC_G,  KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(_RAISE, KC_QUOT), \
-    KC_LSFT,            KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,  KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_SLSH), \
+    QK_GESC,            KC_1,   KC_2,    KC_3,    KC_4,   KC_5,  KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_TAB,             KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,  KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+    LT(_LOWER,KC_CAPS), KC_A,   KC_S,    KC_D,    KC_F,   KC_G,  KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(_RAISE, KC_QUOT),
+    KC_LSFT,            KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,  KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_SLSH),
     KC_LCTL,            MO(_FN),KC_LGUI, KC_LALT, KC_SPC, KC_SPC,KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_BSPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
  ),
 
  [_FN] = LAYOUT_ortho_5x15(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-    KC_PSCR, _______, _______, _______, _______, _______,  KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
-    KC_MPRV, KC_MPLY, KC_MNXT, RGB_HUI, _______, _______,  KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, QK_BOOT, _______, _______, _______, \
-    KC_VOLD, KC_MUTE, KC_VOLU, RGB_SAI, RGB_SPI, _______,  KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______, \
-    _______, _______, RGB_TOG, RGB_VAI, RGB_MOD, _______,  KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, _______, _______, RGB_M_R, RGB_M_SW \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    KC_PSCR, _______, _______, _______, _______, _______,  KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
+    KC_MPRV, KC_MPLY, KC_MNXT, RGB_HUI, _______, _______,  KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, QK_BOOT, _______, _______, _______,
+    KC_VOLD, KC_MUTE, KC_VOLU, RGB_SAI, RGB_SPI, _______,  KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
+    _______, _______, RGB_TOG, RGB_VAI, RGB_MOD, _______,  KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, _______, _______, RGB_M_R, RGB_M_SW
  ),
 
  [_LOWER] = LAYOUT_ortho_5x15(
