ac78af58d935e88d710a364235552821d32e5543
Aaron Hong <hong.aaron.c@gmail.com>
2022-12-16 22:27:19 -0800

    Remove redundant files, already covered by info.json

--- keyboards/synthlabs/060/060.c
+++
@@ -1,4 +0,0 @@
-// Copyright 2022 Aaron Hong (@hongaaronc)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#include "060.h"
--- keyboards/synthlabs/060/keymaps/default/readme.md
+++
@@ -1,3 +0,0 @@
-# Default Synth Labs 060 Layout
-
-This is the default keymap for the 060 keyboard.
