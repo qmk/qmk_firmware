67bd893275edb988f8201d0ff66feaf1ea6b49c7
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-12-17 17:34:21 +1100

    add bnr1v2

---
+++ keyboards/binepad/bnr1/v2/config.h
@@ -0,0 +1,9 @@
+// Copyright 2022 BINEPAD (@binepad)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "config_common.h"
+
+#define WEAR_LEVELING_LOGICAL_SIZE 1024
+#define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 2)
---
+++ keyboards/binepad/bnr1/v2/rules.mk
@@ -0,0 +1,4 @@
+# This file only contains EFL/WL settings
+
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
