7c67e6fb94569bdbdb9b4a438689e24daa2b7fc9
yonatanzunger <30514250+yonatanzunger@users.noreply.github.com>
2022-12-18 13:02:33 -0800

    Fix directory naming; add LALT/LGUI swap.

--- keyboards/wilba_tech/wt65_xt/keymaps/zunger/keymap.c
+++ keyboards/wilba_tech/wt65_xt/keymaps/zunger/keymap.c
@@ -15,6 +15,7 @@
  */
 #include QMK_KEYBOARD_H
 #include <assert.h>
+#include "wt65_xt.h"
 
 // This keymap is designed to make it easy to type in a wide variety of languages, as well as
 // generate mathematical symbols (à la Space Cadet).
@@ -196,7 +197,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_PLATFORM, KC_MPLY,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
 		KC_SCRNSHT,  KC_YIDDISH, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGDN,
 		KC_GREEK,    KC_CADET,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   KC_END,
-		KC_VC_HAND,  KC_VC_MUTE, KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                                      MO_FN,            KC_LEFT, KC_DOWN, KC_RGHT),
+		KC_VC_HAND,  KC_VC_MUTE, KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                      MO_FN,            KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* The Greek and Cadet layers. Tab, backspace, the nav and modifier keys, and the control block
    * are always transparent.
@@ -289,7 +290,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // The function layer mostly contains the accent marks, but also has a few meta-control
   // operations. The accent marks are placed by analogy with Mac OS.
 	[_FUNCTION] = LAYOUT_all(
-		RESET,    KC_TRNS,    KC_CGRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,          XXXXXXX,
+		QK_BOOT,  KC_TRNS,    KC_CGRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,          XXXXXXX,
 		KC_TRNS,  KC_TRNS,    KC_TRNS, XXXXXXX, XXXXXXX, KC_CAGU, XXXXXXX, XXXXXXX, XXXXXXX, KC_CDIA, KC_CCIR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
 		KC_TRNS,  KC_TRNS,    KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,
 		KC_TRNS,  KC_TRNS,    KC_TRNS, XXXXXXX, XXXXXXX, KC_CCED, XXXXXXX, XXXXXXX, KC_CTIL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,                   XXXXXXX, XXXXXXX,
@@ -443,6 +444,8 @@ void set_os_mode(int new_mode) {
   // set_unicode_input_mode, because we don't want to persist this and so we shouldn't put
   // extra load on the EEPROMs.
   unicode_config.input_mode = (os_mode == _MAC ? UC_MAC : UC_WINC);
+  // Swap LALT and LGUI depending on Mac/Windows.
+  keymap_config.swap_lalt_lgui = (os_mode == _MAC);
   // This would be a great moment for some auditory or visual feedback, but this keyboard
   // doesn't support it. :(
 }
@@ -498,8 +501,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // state variables.
   switch (keycode) {
     // Monitoring the modifier keys, because we'll need them for our logic!
-    case KC_LSHIFT:
-    case KC_RSHIFT:
+    case KC_LSFT:
+    case KC_RSFT:
       shift_held = record->event.pressed;
       key_type = _MODIFIER_KEY;
       break;
@@ -514,8 +517,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       alt_held = record->event.pressed;
       key_type = _MODIFIER_KEY;
       break;
-    case KC_LCTRL:
-    case KC_RCTRL:
+    case KC_LCTL:
+    case KC_RCTL:
       ctrl_held = record->event.pressed;
       key_type = _MODIFIER_KEY;
       break;
@@ -618,7 +621,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       key_type = _SPECIAL_KEY;
       break;
 
-    case RESET:
+    case QK_BOOT:
       key_type = _SPECIAL_KEY;
       break;
   }
