f6e52c693d47a0f0eacaaff195ade3b9e8afc666
youturn45 <youturn45@gmail.com>
2022-12-19 14:15:45 -0800

    Update keyboards/gray_studio/think65/solder/keymaps/yt/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/gray_studio/think65/solder/keymaps/yt/config.h
+++ keyboards/gray_studio/think65/solder/keymaps/yt/config.h
@@ -17,10 +17,4 @@
 #pragma once
 
 #define RGBLIGHT_LAYERS
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
 #define DISABLE_LEADER
