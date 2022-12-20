27cbe69e82c8e333d9badb741280f277cf5cd32f
fauxpark <fauxpark@gmail.com>
2022-12-21 01:40:15 +1100

    Lone `LAYOUT_all` -> `LAYOUT`, handwired

--- keyboards/handwired/swiftrax/glacier/glacier.h
+++ keyboards/handwired/swiftrax/glacier/glacier.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-#define LAYOUT_all( \
+#define LAYOUT( \
     K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16,      K07,       K17, K08, K18, K09,  \
     K20, K30, K21, K31, K22, K32, K23, K33, K24, K34, K25, K35, K26, K36, K78, K27,       K37, K28, K38, K29,  \
     K40, K50, K41, K51, K42, K52, K43, K53, K44, K54, K45, K55, K46, K56,      K47,       K57, K48, K58, K49,  \
--- keyboards/handwired/swiftrax/the_galleon/the_galleon.h
+++ keyboards/handwired/swiftrax/the_galleon/the_galleon.h
@@ -19,7 +19,7 @@
 
 #define XXX KC_NO
 
-#define LAYOUT_all( \
+#define LAYOUT( \
     k00, k10, k01, k11, k02, k12, k03, k13, k04, k14, k05, k15, k06,              k07, k17, k08, k18, \
     k20, k30, k21, k31, k22, k32, k23, k33, k24, k34, k25, k35, k26,              k27, k37, k28, k38, \
     k40, k50, k41, k51, k42, k52, k43, k53, k44, k54, k45, k55, k46, k56, k96,    k47, k57, k48, k58, \
