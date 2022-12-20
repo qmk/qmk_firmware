bec62e69dd7443f1b9cff8269addd09322b26b26
fauxpark <fauxpark@gmail.com>
2022-12-21 01:38:51 +1100

    Lone `LAYOUT_all` -> `LAYOUT`, E

--- keyboards/evyd13/mx5160/mx5160.h
+++ keyboards/evyd13/mx5160/mx5160.h
@@ -26,7 +26,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT_all( \
+#define LAYOUT( \
     k00, k10,      k01, k11, k02, k12, k03, k13, k04, k14, k05, k15, k06, k16, k07, k87, k17,      k08, k18, k09, k19,    \
     k20, k30,      k21, k31, k22, k32, k23, k33, k24, k34, k25, k35, k26, k36, k27, k37,           k28, k38, k29, k39,    \
     k40, k50,      k41, k51, k42, k52, k43, k53, k44, k54, k45, k55, k46, k56, k47, k57,           k48, k58, k49, k59,    \
