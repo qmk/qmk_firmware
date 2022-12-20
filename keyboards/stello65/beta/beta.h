8f119aebf9451908cbceba05234c6a5a8243b42a
fauxpark <fauxpark@gmail.com>
2022-12-21 01:42:20 +1100

    Lone `LAYOUT_all` -> `LAYOUT`, S

--- keyboards/stello65/beta/beta.h
+++ keyboards/stello65/beta/beta.h
@@ -15,7 +15,7 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT_all( \
+#define LAYOUT( \
 	K00, K10, K01, K11, K02, K12, K03, K13, K04, K14, K05, K15, K06, K16, K07, K17, K46, K47, \
 	K20, K30, K21, K31, K22, K32, K23, K33, K24, K34, K25, K35, K26, K36,      K37, \
 	K40, K50, K41, K51, K42, K52, K43, K53, K44, K54, K45, K55,      K56,      K57, \
