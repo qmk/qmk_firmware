d49cf8c7fb086570706c54810f43c9743af23d4d
tommyamoszhao <60314626+tommyamoszhao@users.noreply.github.com>
2022-12-22 23:32:25 +0800

    updated as required
    1. Meaningless defines deleted in config.h
    2. Dead codes deleted in both defatul and via keymap.c
    3. "LT()" function is deleted in keymap

--- keyboards/quarkeys/z60/keymaps/default/keymap.c
+++ keyboards/quarkeys/z60/keymaps/default/keymap.c
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 	[0] = LAYOUT(
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL, 
-		LT(3, KC_TAB),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
-		LT(4, KC_CAPS),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_HASH, KC_ENT, 
+		KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
+		KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_HASH, KC_ENT, 
 		KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_UP),    MO(1), 
 		KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,  MO(2),   RALT_T(KC_LEFT),  RGUI_T(KC_DOWN),  RCTL_T(KC_RGHT)),
 
@@ -60,8 +60,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS({23, 1, HSV_RED});
 
-//const rgblight_segment_t PROGMEM my_base_layer[] = RGBLIGHT_LAYER_SEGMENTS({80, 4, HSV_CYAN});
-
 const rgblight_segment_t PROGMEM my_ar_numpad_layer[] = RGBLIGHT_LAYER_SEGMENTS({42, 2, HSV_WHITE});
 
 const rgblight_segment_t PROGMEM my_symbol_layer[] = RGBLIGHT_LAYER_SEGMENTS({28, 2, HSV_WHITE});
@@ -70,12 +68,9 @@ const rgblight_segment_t PROGMEM my_other_layer[] = RGBLIGHT_LAYER_SEGMENTS({15,
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer,
-    //my_base_layer,		// Default layer
     my_ar_numpad_layer,		// Arrow keys & Numpad, Operator keys, reset key, RGB control and media control
 	my_symbol_layer,		// F1-F12 keys & Symbols
 	my_other_layer			// Self-Defined
-	//my_game_layer			// Self-Defined hotkey or macros for a game
-	//my_empty_layer		// Preserved layer for customization
 );
 
 void keyboard_post_init_user(void) {
@@ -87,12 +82,8 @@ bool led_update_user(led_t led_state) {
     return true;
 }
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Both layers will light up if both kb layers are active
-	//rgblight_set_layer_state(0, layer_state_cmp(state, 0));
     rgblight_set_layer_state(1, layer_state_cmp(state, 1));
     rgblight_set_layer_state(2, layer_state_cmp(state, 2));
     rgblight_set_layer_state(3, layer_state_cmp(state, 3));
-    //rgblight_set_layer_state(4, layer_state_cmp(state, 4)); 
-    //rgblight_set_layer_state(5, layer_state_cmp(state, 5));
     return state;
 }
