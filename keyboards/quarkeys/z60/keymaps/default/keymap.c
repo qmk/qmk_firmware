771c75769277caebdf7200d4a2ce36360341adf7
tommyamoszhao <60314626+tommyamoszhao@users.noreply.github.com>
2022-12-22 20:57:06 +0800

    License update, and config.h file merged

--- keyboards/quarkeys/z60/keymaps/default/keymap.c
+++ keyboards/quarkeys/z60/keymaps/default/keymap.c
@@ -1,5 +1,16 @@
-// Copyright 2021 @tommyamoszhao
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* 
+/ Copyright 2022 quarkeys
+/ This program is free software: you can redistribute it and/or modify
+/ it under the terms of the GNU General Public License as published by
+/ the Free Software Foundation, either version 2 of the License, or
+/ (at your option) any later version.
+/ This program is distributed in the hope that it will be useful,
+/ but WITHOUT ANY WARRANTY; without even the implied warranty of
+/ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/ GNU General Public License for more details.
+/ You should have received a copy of the GNU General Public License
+/ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include QMK_KEYBOARD_H
 
@@ -85,4 +96,3 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     //rgblight_set_layer_state(5, layer_state_cmp(state, 5));
     return state;
 }
-
--- keyboards/quarkeys/z60/keymaps/via/keymap.c
+++ keyboards/quarkeys/z60/keymaps/via/keymap.c
@@ -1,10 +1,34 @@
-// Copyright 2021 @tommyamoszhao
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* 
+/ Copyright 2022 quarkeys
+/ This program is free software: you can redistribute it and/or modify
+/ it under the terms of the GNU General Public License as published by
+/ the Free Software Foundation, either version 2 of the License, or
+/ (at your option) any later version.
+/ This program is distributed in the hope that it will be useful,
+/ but WITHOUT ANY WARRANTY; without even the implied warranty of
+/ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/ GNU General Public License for more details.
+/ You should have received a copy of the GNU General Public License
+/ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+	    /* [0]
+     * ,--------------------------------------------------------------------------------------------------------.
+     * | Esc  |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |  -   |  +   | Back | Del  | 
+	 * |------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
+     * | Tab     |  Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |  {   |  }   |   \      |
+     * |---------+------+------+------+------+------+------+------+------+------+------+------+------+----------|
+     * | Capslock   |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  ;   |  "   |  ~   | Enter |
+     * |------------+------+------+------+------+------|------+------+------+------+------+------+--------------|
+     * | SHIFT  |  \   |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |   Shift   |  Fn  |
+     * |--------+------+------+------+------+------+------+------+------+------+------+------+-----------+------|
+     * | Ctrl   |  Win   |  ALT   |                  SPACE                     |  ALT  |  Win  |  Manu |  Ctrl  |
+     * `--------+--------+--------+--------------------------------------------+-------+-------+-------+--------'     
+     */
 	[0] = LAYOUT(
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL, 
 		LT(3, KC_TAB),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
