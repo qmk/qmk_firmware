8dba6f26b9d3860bca6c3283a29ca2195ccf7b5e
Kai Cataldo <kai@kaicataldo.com>
2022-12-23 11:19:10 -0500

    fix: add DEBOUNCE interval of 5ms to creatkeebs/thera
    Key chatter has been widely reported in the createkeebs's Discord. After a cursory look, I noticed that all the firmware source code I looked at for other popular boards had this DEBOUNCE interval defined. It looks like this is follows Cherry's own data sheets that state that bounce can occur for <=5ms for MX switches.
    
    First time contributing, so please let me know if I'm missing anything! Thanks.

--- keyboards/creatkeebs/thera/config.h
+++ keyboards/creatkeebs/thera/config.h
@@ -29,6 +29,9 @@
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
