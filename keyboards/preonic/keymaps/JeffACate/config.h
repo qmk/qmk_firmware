2b8e64d10d01cff552568c4e821e9ebe22e1494a
JeffACate <Jeff.A.Cate@gmail.com>
2022-12-23 10:59:56 -0600

    Added: Scroll keys, Num Pad. Updated: Game layer toggling, Mouse Acceleration.

--- keyboards/preonic/keymaps/JeffACate/config.h
+++ keyboards/preonic/keymaps/JeffACate/config.h
@@ -26,7 +26,19 @@
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
 
+// audio stuff (default Preonic)
 #ifdef AUDIO_ENABLE
 #    define STARTUP_SONG SONG(PREONIC_SOUND)
 // #define STARTUP_SONG SONG(NO_SOUND)
