40dde5e09e82719c1f6501533fc5055ae4e6d37e
hypanim <34974724+hypanim@users.noreply.github.com>
2022-12-23 09:38:45 -0600

    Update keyboards/matrimony/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/matrimony/rules.mk
+++ keyboards/matrimony/rules.mk
@@ -9,13 +9,7 @@ CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-OLED_DRIVER_ENABLE = no	  # prevents lag
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 
 SPLIT_KEYBOARD = yes
