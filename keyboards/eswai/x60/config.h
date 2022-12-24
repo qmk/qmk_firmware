0d0cff1cdbbe847185a24492eb032bce1ec7ab35
eswai <>
2022-12-24 13:59:42 +0900

    不要なもの削除

--- keyboards/eswai/x60/config.h
+++ keyboards/eswai/x60/config.h
@@ -27,40 +27,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #define MATRIX_ROW_PINS { B6, B5, B4, D7, E6 }
 #define MATRIX_COL_PINS { D2, D1, D3, D5, D4, D6, C6, F0, F1, F4, F5, F6, F7, C7}
-#define UNUSED_PINS
+// #define UNUSED_PINS
 
 #define DIODE_DIRECTION COL2ROW
-
-/*
- * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
- */
-#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
-
-// define BACKLIGHT_PIN B7
-// define BACKLIGHT_BREATHING
-// #define BACKLIGHT_LEVELS 3
-
-#define RGB_DI_PIN B0
-#ifdef RGB_DI_PIN
-  #define RGBLED_NUM 14
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
-  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-  #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-// /*== all animations enable ==*/
-  #define RGBLIGHT_ANIMATIONS
-// /*== or choose animations ==*/
-//   #define RGBLIGHT_EFFECT_BREATHING
-//   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//   #define RGBLIGHT_EFFECT_SNAKE
-//   #define RGBLIGHT_EFFECT_KNIGHT
-//   #define RGBLIGHT_EFFECT_CHRISTMAS
-//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//   #define RGBLIGHT_EFFECT_RGB_TEST
-//   #define RGBLIGHT_EFFECT_ALTERNATING
-#endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
