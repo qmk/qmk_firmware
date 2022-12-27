7d2ee34488496222f9dacd6b4b4cfc014259018f
Eugenio Pastoral <gino@achrovisual.com>
2022-12-27 22:03:41 +0800

    Added GPL2+ compatible license header

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/achrovisual/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/achrovisual/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+/* Copyright 2022 Eugenio Pastoral
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
