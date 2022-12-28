b5a82392a59ebce732900315eef80c2c5434e08c
Ivan Gromov <38141348+key10iq@users.noreply.github.com>
2022-12-28 17:15:08 +0400

    Update keyboards/keyten/kt3700/rules.mk
    Co-authored-by: Sergey Vlasov <sigprof@gmail.com>

--- keyboards/keyten/kt3700/rules.mk
+++ keyboards/keyten/kt3700/rules.mk
@@ -17,4 +17,3 @@ NKRO_ENABLE = yes           # Enable N-key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-LTO_ENABLE = yes
