a5b6e714c0a63856dae4821638f03ea7663b176e
Pham Ngoc Diep <diep.pham@your.rentals>
2022-12-28 13:37:21 +0700

    fix: update configuration

--- keyboards/ergodash/rev1/keymaps/anhdiepmmk/keymap.c
+++ keyboards/ergodash/rev1/keymaps/anhdiepmmk/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                            KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,                             KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RAISE,                              KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
-    KC_LCTL, KC_LALT, NUMPAD,  KC_LGUI,          KC_LCTL, LOWER,   KC_SPC,           KC_ENT,  KC_RALT, KC_BSPC,          KC_RGUI, KC_APP,  KC_RALT, KC_RCTL  \
+    KC_LCTL, KC_LGUI, NUMPAD,  KC_LALT,          KC_LCTL, LOWER,   KC_SPC,           KC_ENT,  KC_RALT, KC_BSPC,          KC_RGUI, KC_APP,  KC_RALT, KC_RCTL  \
   ),
 
   /* Linux Qwerty
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                            KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,                             KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RAISE,                              KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
-    KC_LCTL, KC_LALT, NUMPAD,  KC_LGUI,          KC_LCTL, LOWER,   KC_SPC,           KC_ENT,  KC_RALT, KC_BSPC,          KC_RGUI, KC_APP,  KC_RALT, KC_RCTL  \
+    KC_LCTL, KC_LGUI, NUMPAD,  KC_LALT,          KC_LCTL, LOWER,   KC_SPC,           KC_ENT,  KC_RALT, KC_BSPC,          KC_RGUI, KC_APP,  KC_RALT, KC_RCTL  \
   ),
 
   /* Numpad
