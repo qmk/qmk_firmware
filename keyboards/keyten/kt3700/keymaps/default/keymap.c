225e2fb4230e755105ca313a49b5cdff8b72c91d
Ivan Gromov <38141348+key10iq@users.noreply.github.com>
2022-12-28 21:50:57 +0400

    Update keyboards/keyten/kt3700/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/keyten/kt3700/keymaps/default/keymap.c
+++ keyboards/keyten/kt3700/keymaps/default/keymap.c
@@ -24,13 +24,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_P7,   KC_P8,   KC_P9,   KC_PSCR,
 		KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		KC_P1,   KC_P2,   KC_P3,   KC_DEL,
-		KC_INS,  KC_P0,   KC_PDOT, KC_PENT),
-
-	[1] = LAYOUT_ortho_6x4(
-		_______, _______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______, _______, _______,
-		_______, _______, _______, _______)
+		KC_INS,  KC_P0,   KC_PDOT, KC_PENT)
 };
