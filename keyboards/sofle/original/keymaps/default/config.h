13bcc1cb28578aa158f898ec9ffdf105cebeca7c
duese <duese@users.noreply.github.com>
2022-12-29 00:41:06 +0100

    Remove empty config.h file.

--- keyboards/sofle/original/keymaps/default/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Josef Adamčík (@josefadamcik)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-/* The way how "handedness" is decided (which half is which),
-see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
