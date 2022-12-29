58baf2e55bd82a020267abf67b425ca403a0923b
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2022-12-28 19:48:31 -0700

    Update keyboards/clickety_split/leeloo/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/clickety_split/leeloo/rules.mk
+++ keyboards/clickety_split/leeloo/rules.mk
@@ -6,9 +6,10 @@ BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+#
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key rollover
