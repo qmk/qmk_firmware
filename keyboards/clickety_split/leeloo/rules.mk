79b837b8d9474bb92ec7f0f00ba71cb49da17e58
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2022-12-28 19:48:52 -0700

    Update keyboards/clickety_split/leeloo/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/clickety_split/leeloo/rules.mk
+++ keyboards/clickety_split/leeloo/rules.mk
@@ -13,9 +13,9 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key rollover
-AUDIO_ENABLE = no           # Audio output
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
 
 # Enables the use of OLED displays
