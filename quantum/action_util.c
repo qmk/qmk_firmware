df7b1382398f97f6507ceeca2645a051a9a5fe8b
taudris <patches11@gmail.com>
2022-12-31 02:25:52 -0800

    Add DOUBLE_REPORT. Remove REPORT_MODS_SEPARATELY.

--- quantum/action_util.c
+++ quantum/action_util.c
@@ -270,28 +270,30 @@ void send_keyboard_report(void) {
     keyboard_report->mods |= weak_override_mods;
 #endif
 
+#ifdef PROTOCOL_VUSB
+#   ifdef DOUBLE_REPORT
+    static report_keyboard_t last_report;
+    memcpy(&last_report, keyboard_report, sizeof(report_keyboard_t));
+#   endif
+
+    host_keyboard_send(keyboard_report);
+
+#   ifdef DOUBLE_REPORT
+    memcpy(keyboard_report, &last_report, sizeof(report_keyboard_t)); // host_keyboard_send() sometimes modifies keyboard_report, so restore it from last_report before sending again
+    host_keyboard_send(keyboard_report);
+#   endif
+#else
     static report_keyboard_t last_report;
 
-#ifdef REPORT_MODS_SEPARATELY
-    if (last_report.mods != keyboard_report->mods) {
-        // Build a keyboard report that only updates the mods
-        report_keyboard_t mod_report;
-        memcpy(&mod_report, &last_report, sizeof(report_keyboard_t));
-        mod_report.mods = keyboard_report->mods;
-
-        host_keyboard_send(&mod_report);
-        memcpy(&last_report, &mod_report, sizeof(report_keyboard_t));
-    }
-#endif
-
-#ifdef PROTOCOL_VUSB
-    host_keyboard_send(keyboard_report);
-    memcpy(&last_report, keyboard_report, sizeof(report_keyboard_t));
-#else
     /* Only send the report if there are changes to propagate to the host. */
-    if (memcmp(keyboard_report, &last_report, sizeof(report_keyboard_t)) != 0) {
-        host_keyboard_send(keyboard_report);
+    if (memcmp(&last_report, keyboard_report, sizeof(report_keyboard_t)) != 0) {
         memcpy(&last_report, keyboard_report, sizeof(report_keyboard_t));
+        host_keyboard_send(keyboard_report);
+
+#   ifdef DOUBLE_REPORT
+        memcpy(keyboard_report, &last_report, sizeof(report_keyboard_t)); // host_keyboard_send() sometimes modifies keyboard_report, so restore it from last_report before sending again
+        host_keyboard_send(keyboard_report);
+#   endif
     }
 #endif
 }
