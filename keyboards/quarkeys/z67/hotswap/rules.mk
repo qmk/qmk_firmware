efa87055b9cd2910d7028d7a12084ca658eb338f
TommyZ <60314626+tommyamoszhao@users.noreply.github.com>
2023-01-02 00:32:17 +0800

    Update keyboards/quarkeys/z67/hotswap/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/quarkeys/z67/hotswap/rules.mk
+++ keyboards/quarkeys/z67/hotswap/rules.mk
@@ -4,12 +4,11 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = atmel-dfu
 
-BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
-CONSOLE_ENABLE = no	# Console for debug(+400)
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+BOOTMAGIC_ENABLE = yes  # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes   # Mouse keys
+EXTRAKEY_ENABLE = yes   # Audio control and System control
+CONSOLE_ENABLE = no	    # Console for debug
+COMMAND_ENABLE = no     # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
