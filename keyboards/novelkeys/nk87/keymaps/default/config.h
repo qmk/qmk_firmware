01c9742c14f5b702cfff915e3e4b725d3ef6a69a
Kirill Malgichev <makirill@gmail.com>
2023-01-01 13:09:44 -0500

    Fixing coding style
    Making preprocessor directives indentation style compliant with the
    qmk coding conventions.

--- keyboards/novelkeys/nk87/keymaps/default/config.h
+++ keyboards/novelkeys/nk87/keymaps/default/config.h
@@ -16,10 +16,9 @@
 #pragma once
 
 #ifdef RGB_MATRIX_ENABLE
-    #define RGB_DISABLE_WHEN_USB_SUSPENDED true
-    #define RGB_MATRIX_TIMEOUT 900000     // 15 minutes (15 * 60 * 1000ms)
-
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED
+#    define RGB_MATRIX_TIMEOUT 900000     // 15 minutes (15 * 60 * 1000ms)
     // Effects
-    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS  // Heatmap, Rain
-    #define RGB_MATRIX_KEYPRESSES           // REACTIVE, SPLASH modes
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS  // Heatmap, Rain
+#    define RGB_MATRIX_KEYPRESSES           // REACTIVE, SPLASH modes
 #endif // RGB_MATRIX_ENABLE
