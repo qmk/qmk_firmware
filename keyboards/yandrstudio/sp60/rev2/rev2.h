d83220f31fbeb0b6c60b6eb7dbc17db9adb8466f
JX <572673807@qq.com>
2023-01-02 01:21:37 +0800

    Update keyboards/yandrstudio/sp60/rev2/rev2.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/yandrstudio/sp60/rev2/rev2.h
+++ keyboards/yandrstudio/sp60/rev2/rev2.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include "quantum.h"
+#include "sp60.h"
 
 #define LAYOUT_60_tsangan_split_rshift( \
     K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012,       K014,\
