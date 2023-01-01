61696fda83be949c0a3ef46eec9bf6a206a4ffac
Joel Challis <git@zvecr.com>
2023-01-01 00:43:30 +0000

    CLI flashers should allow files outside qmk_firmware folder (#19454)

--- lib/python/qmk/flashers.py
+++ lib/python/qmk/flashers.py
@@ -178,25 +178,25 @@ def flasher(mcu, file):
     # Add a small sleep to avoid race conditions
     time.sleep(1)
     if bl == 'atmel-dfu':
-        _flash_atmel_dfu(details, file.name)
+        _flash_atmel_dfu(details, file)
     elif bl == 'caterina':
-        if _flash_caterina(details, file.name):
+        if _flash_caterina(details, file):
             return (True, "The Caterina bootloader was found but is not writable. Check 'qmk doctor' output for advice.")
     elif bl == 'hid-bootloader':
         if mcu:
-            if _flash_hid_bootloader(mcu, details, file.name):
+            if _flash_hid_bootloader(mcu, details, file):
                 return (True, "Please make sure 'teensy_loader_cli' or 'hid_bootloader_cli' is available on your system.")
         else:
             return (True, "Specifying the MCU with '-m' is necessary for HalfKay/HID bootloaders!")
     elif bl == 'stm32-dfu' or bl == 'apm32-dfu' or bl == 'gd32v-dfu' or bl == 'kiibohd':
-        _flash_dfu_util(details, file.name)
+        _flash_dfu_util(details, file)
     elif bl == 'usbasploader' or bl == 'usbtinyisp':
         if mcu:
-            _flash_isp(mcu, bl, file.name)
+            _flash_isp(mcu, bl, file)
         else:
             return (True, "Specifying the MCU with '-m' is necessary for ISP flashing!")
     elif bl == 'md-boot':
-        _flash_mdloader(file.name)
+        _flash_mdloader(file)
     else:
         return (True, "Known bootloader found but flashing not currently supported!")
 
