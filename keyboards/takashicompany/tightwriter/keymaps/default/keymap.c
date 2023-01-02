36bbf23b25a700f5971a21de4979190e9c1077bd
takashicompany <t@kashi.company>
2023-01-02 18:34:03 +0900

    SWITCH LANG

--- keyboards/takashicompany/tightwriter/keymaps/default/keymap.c
+++ keyboards/takashicompany/tightwriter/keymaps/default/keymap.c
@@ -1,11 +1,15 @@
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    SWITCH_LANG = SAFE_RANGE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
         LT(7, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT, 
+        KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT,
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT),
-        KC_LGUI, LALT_T(KC_LANG2), LT(2, KC_SPC), LT(1, KC_LANG1), KC_BSPC
+        KC_LGUI, LALT_T(KC_LANG2), LT(1, KC_SPC), LT(1, KC_LANG1), KC_BSPC
     ),
     
     LAYOUT(
@@ -26,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(7, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_TRNS,
         KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT,
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT),
-        KC_LGUI, LALT_T(KC_LANG2), LT(5, KC_SPC), LT(4, KC_LANG1), KC_BSPC
+        KC_LGUI, LALT_T(KC_LANG2), LT(4, KC_SPC), LT(4, KC_LANG1), KC_BSPC
     ),
         
     LAYOUT(
@@ -47,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_TRNS,
         KC_LCTL, KC_TRNS, KC_QUES, KC_EXLM, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,
         KC_LSFT, KC_LGUI, KC_LALT, KC_LANG2, KC_TRNS, KC_NO, KC_LANG1, KC_NO, KC_NO,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, SWITCH_LANG, KC_TRNS, KC_TRNS
     ), 
     
     LAYOUT(
@@ -64,3 +68,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
+
+bool is_lang1 = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case SWITCH_LANG:
+            if (record->event.pressed) {
+
+                if (is_lang1) {
+                    register_code(KC_LANG1);
+                    is_lang1 = false;
+                }
+                else {
+                    register_code(KC_LANG2);
+                    is_lang1 = true;
+                }
+            }
+            break;
+        case KC_LANG1:
+            if (record->event.pressed) {
+                is_lang1 = false;
+            }
+            break;
+
+        case KC_LANG2:
+            if (record->event.pressed) {
+                is_lang1 = true;
+            }
+            break;
+    }
+    return true;
+}
