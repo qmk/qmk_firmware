f96505115f21187500d7e32bae1c1119890a9046
zvecr <git@zvecr.com>
2023-01-03 02:26:05 +0000

    Add cli flag

--- lib/python/qmk/cli/list/keyboards.py
+++ lib/python/qmk/cli/list/keyboards.py
@@ -5,9 +5,10 @@ from milc import cli
 import qmk.keyboard
 
 
+@cli.argument('--resolve_defaults', arg_only=True, action='store_false', help='Parse any "DEFAULT_FOLDER" within keyboards rules.mk')
 @cli.subcommand("List the keyboards currently defined within QMK")
 def list_keyboards(cli):
     """List the keyboards currently defined within QMK
     """
-    for keyboard_name in qmk.keyboard.list_keyboards(resolve_defaults=False):
+    for keyboard_name in qmk.keyboard.list_keyboards(cli.args.resolve_defaults):
         print(keyboard_name)
