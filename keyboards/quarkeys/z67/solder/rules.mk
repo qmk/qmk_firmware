ed52bcada651a31e8f88d3ee2b436d79641aa19e
TommyZ <60314626+tommyamoszhao@users.noreply.github.com>
2023-01-04 13:30:54 +0800

    Update keyboards/quarkeys/z67/solder/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/quarkeys/z67/solder/rules.mk
+++ keyboards/quarkeys/z67/solder/rules.mk
@@ -4,6 +4,9 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = atmel-dfu
 
+# Build Options
+#   change yes to no to disable
+#
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
