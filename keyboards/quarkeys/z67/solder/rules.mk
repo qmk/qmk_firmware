dbcddeaab9064bc64c109439e783a11a729cf9c1
TommyZ <60314626+tommyamoszhao@users.noreply.github.com>
2023-01-04 13:30:46 +0800

    Update keyboards/quarkeys/z67/solder/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/quarkeys/z67/solder/rules.mk
+++ keyboards/quarkeys/z67/solder/rules.mk
@@ -11,5 +11,5 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes
-AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
