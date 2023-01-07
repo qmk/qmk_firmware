1c1ccee0c581b43e9966012148c8f9bcdee7267e
fauxpark <fauxpark@gmail.com>
2023-01-08 02:04:04 +1100

    Migrate `MCU` and `BOOTLOADER` to data-driven, E

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -1,9 +1,3 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = halfkay
-
 # If you have Left LEDs (see
 # https://geekhack.org/index.php?topic=22780.msg873819#msg873819 for
 # details), include the following define:
