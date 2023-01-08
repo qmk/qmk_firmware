35e58337795b42bbc6e8916b44b03a25e6284eb1
Shandon Anderson <shandon@shandon.codes>
2023-01-08 08:55:57 -0500

    Updated per comments

--- keyboards/mino_plus/soldered/config.h
+++ keyboards/mino_plus/soldered/config.h
@@ -1,6 +1,5 @@
 // Copyright 2022 ShandonCodes (@ShandonCodes)
 // SPDX-License-Identifier: GPL-2.0-or-later
-
 #pragma once
 
 #include "config_common.h"
--- keyboards/mino_plus/soldered/mcuconf.h
+++ keyboards/mino_plus/soldered/mcuconf.h
@@ -1,5 +1,7 @@
 // Copyright 2022 ShandonCodes (@ShandonCodes)
 // SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
 #include_next "mcuconf.h"
 
 #undef STM32_I2C_USE_I2C1
