0c82dedc5d5f2a029cd0356eb4c8619e26c2f835
fauxpark <fauxpark@gmail.com>
2023-01-08 22:03:07 +1100

    Migrate `LAYOUTS` to data driven, F

--- keyboards/fjlabs/ldk65/rules.mk
+++ keyboards/fjlabs/ldk65/rules.mk
@@ -19,5 +19,3 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-
-LAYOUTS = 65_ansi
