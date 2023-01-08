f6ea5a07e98bbc91e089515a7736bdbc4746e481
jason <jrucker2004@gmail.com>
2023-01-08 03:18:27 +0000

    fixing keymap del -> ent

--- keyboards/rmkeebs/rm_numpad/keymaps/big_enter/keymap.c
+++ keyboards/rmkeebs/rm_numpad/keymaps/big_enter/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P7,   KC_P8,   KC_P9,   KC_EQL,
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
         KC_P1,   KC_P2,   KC_P3,
-        KC_P0,   DBL_ZRO, KC_PDOT, KC_DEL
+        KC_P0,   DBL_ZRO, KC_PDOT, KC_ENT
     )
 };
 
