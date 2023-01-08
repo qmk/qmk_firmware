fb4c6d19654857656e824306433e2c746a54a475
fauxpark <fauxpark@gmail.com>
2023-01-08 22:04:49 +1100

    Migrate `LAYOUTS` to data driven, I

--- keyboards/idobao/montex/v1rgb/rules.mk
+++ keyboards/idobao/montex/v1rgb/rules.mk
@@ -17,5 +17,3 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
-
-LAYOUTS = numpad_6x5
