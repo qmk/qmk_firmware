0f5aa83232d63ec93f5db09132e8fd92d3ae47cc
MatteoDM <matteodalmo@gmail.com>
2023-01-09 20:30:49 +0100

    Update keyboards/neodox/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/neodox/rules.mk
+++ keyboards/neodox/rules.mk
@@ -7,7 +7,7 @@ BOOTLOADER = rp2040
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System controliouy
 CONSOLE_ENABLE = no         # Console for debug
