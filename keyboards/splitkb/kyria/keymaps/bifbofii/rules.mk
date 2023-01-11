3f32ae355b10c4744b7fc5221e5b5279a036ac0f
BifbofII <christoph@jabs-family.de>
2023-01-11 23:04:56 +0200

    Apply suggestions from code review
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/splitkb/kyria/keymaps/bifbofii/rules.mk
+++ keyboards/splitkb/kyria/keymaps/bifbofii/rules.mk
@@ -1,4 +1,4 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+OLED_ENABLE = yes   # Enables the use of OLED displays
 OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = no        # ENables the use of one or more encoders
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
