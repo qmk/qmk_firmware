eea3b209ce365c5d2fa73742f5ab29ee815f4d61
Felix Sargent <fsargent@recruitbot.com>
2023-01-12 15:56:30 -0800

    Meh now has an opporunity to be promoted to HYPER.

--- keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
@@ -79,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * |MEHESC│   A  │   S  │   D  │   F  │   G  │   (  │  │ PGUP │   H  │   J  │   K  │   L  │   ;  │   '  │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │Shift │   Z  │   X  │   C  │   V  │   B  │   {  │  │ PGDN │   N  │   M  │   ,  │   .  │   /  │Shift │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
+ * │Shift │   Z  │   X  │  C  │   V  │   B  │   {  │  │ PGDN │   N  │   M  │   ,  │   .  │   /  │Shift │
+ * ├──────┼──────┼──────┼──────┼───────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │ Ctrl │  Alt │  Win │  SYM │ NAV  │      │2Sym  │  │2Ctrl │      │  +   │   -  │   =  │   _  │RCTRL │
- * └──────┴──────┴──────┴──────┴──────┤1Space├──────┤  ├──────┤3Bspc ├──────┴──────┴──────┴──────┴──────┘
+ * └──────┴──────┴──────┴──────┴───────┤1BSpc ├──────┤  ├──────┤3Spc ├──────┴──────┴──────┴──────┴──────┘
  *                                    │      │3Ctrl │  │1Enter│      │
  *                                    └──────┴──────┘  └──────┴──────┘
  */
@@ -190,9 +190,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	MEH(KC_Q),	MEH(KC_W),	MEH(KC_E),	MEH(KC_R),	MEH(KC_T),	_______,						_______,	MEH(KC_Y),	MEH(KC_U),	MEH(KC_I),	MEH(KC_O),	MEH(KC_P),	_______,
 	_______,	MEH(KC_A),	MEH(KC_S),	MEH(KC_D),	MEH(KC_F),	MEH(KC_G),	_______,						KC_VOLU,	MEH(KC_H),	MEH(KC_J),	MEH(KC_K),	MEH(KC_L),	MEH(KC_SCLN),	_______,
 	_______,	MEH(KC_Z),	MEH(KC_X),	MEH(KC_C),	MEH(KC_V),	MEH(KC_B),	_______,						KC_VOLD,	MEH(KC_N),	MEH(KC_M),	MEH(KC_COMM),	MEH(KC_DOT),	MEH(KC_SLSH),	_______,
-	_______,	_______,		RGB_RMOD,		RGB_TOG,		RGB_MOD, 		TG(_WIN),	TG(_GAME),	TO(_MAC),
-																																														KC_MNXT,	KC_MPRV,	KC_MPLY,
-																																																												TO(_MAC),	TO(_WIN),		TO(_GAME),		TO(_SYM),		_______,
+	_______,	_______,		RGB_RMOD,		RGB_TOG,		RGB_MOD, 		KC_LGUI, KC_LALT, KC_LCTL,
+																									KC_MNXT,	KC_MPRV,	KC_MPLY,
+																									TO(_MAC),	TO(_WIN),		TO(_GAME),		TO(_SYM),		_______,
 
 	_______,	_______,	_______,	_______,	_______,	_______,										 _______,	_______,	_______,	_______,	_______,	_______,
 	_______,	_______,	_______,	_______,	_______,														_______,	_______,	_______,	_______,		_______
