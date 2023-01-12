e0abe7135f8314e6aacc1b3b60209fd48ace798c
J Pratt <jp10010101010000@gmail.com>
2023-01-12 17:53:11 +1100

    Swap enter and backspace (and delete)

--- keyboards/sofle/keymaps/cypher1/keymap.c
+++ keyboards/sofle/keymaps/cypher1/keymap.c
@@ -17,7 +17,7 @@
 
 #include QMK_KEYBOARD_H
 #define L1_SPC LT(1, KC_SPC)
-#define L2_ENT LT(2, KC_ENT)
+#define L2_BSPC LT(2, KC_BSPC)
 #define MS_BTN1 KC_MS_BTN1
 #define MS_BTN2 KC_MS_BTN2
 #define MS_BTN3 KC_MS_BTN3
@@ -46,16 +46,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
-  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_BSPC,
-                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   L1_SPC,   L2_ENT,  KC_RGUI, KC_RCTRL,  KC_RALT, TG(GAMES)
+  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,   KC_ENT,
+                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   L1_SPC,  L2_BSPC,  KC_RGUI, KC_RCTRL,  KC_RALT, TG(GAMES)
 ),
 /* FUNCTION */
 [FUNCS] = LAYOUT(
   _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
   _______,  XXXXXXX,    KC_UP,  XXXXXXX,  KC_LBRC,  KC_RBRC,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   KC_F12,
   KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,  KC_BSLS,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MINS,   KC_EQL,  _______,  MS_BTN1,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   KC_DEL,
-                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______
+  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MINS,   KC_EQL,  _______,  MS_BTN1,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
+                      _______,  _______,  _______,  _______,  _______,   KC_DEL,  _______,  _______,  _______, _______
 ),
 /* KEYBOARD CONTROL */
 [CNTRL] = LAYOUT(
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
-  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_BSPC,
-                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   KC_SPC,   KC_ENT,  KC_RGUI, KC_RCTRL,  KC_RALT, TG(GAMES)
+  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,   KC_ENT,
+                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   KC_SPC,  KC_BSPC,  KC_RGUI, KC_RCTRL,  KC_RALT, TG(GAMES)
 )
 };
 
