bfed084acb05361bf30069cf6ecd361afa9e6672
Ciaanh <nicolas.fabre.21@outlook.fr>
2023-01-12 16:01:55 +0100

    adjust rgb coords

--- keyboards/ciaanh/kanagawa2040/keymaps/default/keymap.c
+++ keyboards/ciaanh/kanagawa2040/keymaps/default/keymap.c
@@ -42,12 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *   
 */
     [_BASE] = LAYOUT_84_ansi( 
-          KC_ESC,   KC_F1,      KC_F2,      KC_F3,      KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,              KC_MPLY,  \
+          KC_ESC,   KC_F1,      KC_F2,      KC_F3,      KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,           KC_MPLY,  \
        
-          KC_GRV,    KC_1,       KC_2,       KC_3,       KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,      KC_DEL,  \
-          KC_TAB,    KC_Q,       KC_W,       KC_E,       KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_HOME,  \
-         KC_CAPS,    KC_A, LT(0,KC_S),       KC_D,       KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,               KC_END,  \
-         KC_LSFT,    KC_Z, LT(0,KC_X), LT(0,KC_C), LT(0,KC_V),    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,                KC_UP,           \
+          KC_GRV,    KC_1,       KC_2,       KC_3,       KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,   KC_DEL,  \
+          KC_TAB,    KC_Q,       KC_W,       KC_E,       KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,  KC_HOME,  \
+         KC_CAPS,    KC_A, LT(0,KC_S),       KC_D,       KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,            KC_END,  \
+         KC_LSFT,    KC_Z, LT(0,KC_X), LT(0,KC_C), LT(0,KC_V),    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,             KC_UP,           \
          KC_LCTL, KC_LGUI,    KC_LALT,            L_DWN,  KC_SPC,        KC_SPC,    L_UP,       KC_RALT, TO_CODE, KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT   \
     ),
 
@@ -73,13 +73,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_CODE] = LAYOUT_84_ansi(
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX,  \
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             RGB_TOG,  \
 
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,  \
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,  \
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX,  \
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              XXXXXXX,           \
-         XXXXXXX, XXXXXXX, XXXXXXX,       _______, XXXXXXX,       XXXXXXX, _______,       XXXXXXX, TO_BASE, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX   \
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     RGB_VAI,  \
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              RGB_VAD,  \
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,              RGB_HUI,           \
+         XXXXXXX, XXXXXXX, XXXXXXX,       _______, XXXXXXX,       XXXXXXX, _______,       XXXXXXX, TO_BASE, XXXXXXX,     RGB_SAD, RGB_HUD, RGB_SAI   \
     ),
 
 };
@@ -231,10 +231,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [_BASE] =   { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
-    [_LOWER] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_UPPER] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_CODE] =   { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
+    [_BASE] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_LOWER] =  { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
+    [_UPPER] =  { ENCODER_CCW_CW(KC_WH_L, KC_WH_R) },
+    [_CODE] =   { ENCODER_CCW_CW(RGB_MOD, RGB_RMOD) },
 };
 #endif
 
