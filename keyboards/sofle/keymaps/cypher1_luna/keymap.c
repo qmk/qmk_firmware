ecb60146fd6cfe3e821c6f91a7fcc0dbce32b2cf
J Pratt <jp10010101010000@gmail.com>
2023-01-12 15:12:19 +1100

    Try a split keeb

--- keyboards/sofle/keymaps/cypher1_luna/keymap.c
+++ keyboards/sofle/keymaps/cypher1_luna/keymap.c
@@ -28,25 +28,18 @@
 
 enum LAYERS {
   QWERT, // All the bells and whistles for coding.
-  YUIOP, // Simpler control scheme for games (e.g. space is just space).
   FN,     // Function keys (e.g. screen brightness, media keys).
   CTRL,   // Controls for the keyboard (e.g. brightness, colours) and mouse (buttons and movement).
+  GAMES, // Simpler control scheme for games (e.g. space is just space).
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QWERT] = LAYOUT(
-   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,   KC_GRV,
-   KC_ESC,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_BSPC,
-   KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
-  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  XXXXXXX,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
-                      KC_LGUI,  KC_LALT, KC_LCTRL,   L1_SPC,   L2_ENT,   KC_SPC,    MO(3), KC_RCTRL,  KC_RALT,  KC_RGUI
-),
-[YUIOP] = LAYOUT(
-   KC_DEL,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,   KC_GRV,
-  KC_BSPC,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,                         KC_J,     KC_L,     KC_U,     KC_Y,  KC_SCLN,  KC_BSPC,
-  KC_QUOT,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,                         KC_H,     KC_N,     KC_E,     KC_I,     KC_O,  KC_QUOT,
-  KC_RSFT,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_MPLY,  XXXXXXX,     KC_K,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
-                      KC_LGUI,  KC_LALT, KC_LCTRL,  _______,   L3_ENT,   KC_SPC,  _______, KC_RCTRL,  KC_RALT,  KC_RGUI
+   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
+   KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
+   KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
+  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  XXXXXXX,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_BSPC,
+                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   L1_SPC,   L2_ENT,  KC_RGUI, KC_RCTRL,  KC_RALT, TG(GAMES)
 ),
 /* FUNCTION */
 [FN] = LAYOUT(
@@ -58,11 +51,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* KEYBOARD CONTROL */
 [CTRL] = LAYOUT(
-  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_RBRC,  XXXXXXX,
   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                      XXXXXXX,  XXXXXXX,  XXXXXXX,    TG(3),  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+),
+[GAMES] = LAYOUT(
+   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
+   KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
+   KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
+  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  XXXXXXX,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_BSPC,
+                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   KC_SPC,   KC_ENT,  KC_RGUI, KC_RCTRL,  KC_RALT, TG(GAMES)
 )
 };
 
