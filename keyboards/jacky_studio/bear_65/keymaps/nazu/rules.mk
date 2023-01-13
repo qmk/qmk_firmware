c29a06ce684bea72a7672d39d0a91b1fd2baf894
beelzebubi <tom@sennewald.me>
2023-01-13 10:47:18 +0100

    add bear65 v2 keymap

---
+++ keyboards/jacky_studio/bear_65/keymaps/nazu/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+MOUSEKEY_ENABLE = no       # Mouse keys
+NKRO_ENABLE = yes          # Enable N-Key Rollover
