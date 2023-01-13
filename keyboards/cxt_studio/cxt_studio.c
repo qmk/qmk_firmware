45375e00dded06e3a771a84b2415afe2de96f348
Colin Kinloch <colin@kinlo.ch>
2023-01-13 12:07:31 +0000

    Apply suggestions from code review
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/cxt_studio/cxt_studio.c
+++ keyboards/cxt_studio/cxt_studio.c
@@ -5,6 +5,7 @@
 
 static uint8_t anim = 0;
 
+#ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) {
         return false;
@@ -51,3 +52,4 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     }
     return true;
 }
+#endif
