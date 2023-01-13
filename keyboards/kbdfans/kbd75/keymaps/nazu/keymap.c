79a2f76380d52f8541e938536124d8f90022de25
beelzebubi <tom@sennewald.me>
2023-01-13 10:46:27 +0100

    add kbd75 keymap

---
+++ keyboards/kbdfans/kbd75/keymaps/nazu/keymap.c
@@ -0,0 +1,105 @@
+#include QMK_KEYBOARD_H
+#include "nazu.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /*
+   * QWERTZ (LAYOUT_ansi_1u)
+   * ,-----------------------------------------------------------------------------------------------,
+   * | Esc | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |Print| Emo | Del |
+   * ,-----------------------------------------------------------------------------------------------|
+   * |  ^  | 1 ! | 2 " | 3 § | 4 $ | 5 % | 6 & | 7 / | 8 ( | 9 ) | 0 = | ß ? | ´ ` | Backspace |Home |
+   * |-----------------------------------------------------------------------------------------------|
+   * |  Tab   |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  | [ { | ] } |   \ |  |PgUp |
+   * |----------------------------------------------------------------------------------       |-----|
+   * |   Caps  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  | ; : | ' " |    Enter    |PgDwn|
+   * |-----------------------------------------------------------------------------------------------|
+   * |    Shift    |  Z  |  X  |  C  |  V  |  B  |  N  |  M  | , ; | . : | - _ |  Shift  | Up  | End |
+   * |-----------------------------------------------------------------------------------------------|
+   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt |RCtl | FN1 |Left |Down |Right|
+   * `-----------------------------------------------------------------------------------------------´
+   */
+	[_QWERTY] = LAYOUT_75_ansi(
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_PAUS,  KC_DEL,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_HOME,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
+    KC_BSPC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_PGDN,
+    TD_CAPS,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                                TD_SPC,                                 KC_R2,    KC_R1,    MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
+  ),
+
+  /*
+   * Colemak
+   * ,-----------------------------------------------------------------------------------------------,
+   * | Esc | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |Print|Pause| Del |
+   * ,-----------------------------------------------------------------------------------------------|
+   * |  `  | 1 ! | 2 @ | 3 # | 4 $ | 5 % | 6 ^ | 7 & | 8 * | 9 ( | 0 ) | - _ | = + | Backspace |Home |
+   * |-----------------------------------------------------------------------------------------------|
+   * |  Tab   |  Q  |  W  |  F  |  P  |  G  |  J  |  L  |  U  |  Y  | ; : | [ { | ] } |   \ |  |PgUp |
+   * |-----------------------------------------------------------------------------------------|-----|
+   * |Backspace|  A  |  R  |  S  |  T  |  D  |  H  |  N  |  E  |  I  |  O  | ' " |    Enter    |PgDwn|
+   * |-----------------------------------------------------------------------------------------------|
+   * |    Shift    |  Z  |  X  |  C  |  V  |  B  |  K  |  M  | , < | . > | / ? |  Shift  | Up  | End |
+   * |-----------------------------------------------------------------------------------------------|
+   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt |RCtl | FN1 |Left |Down |Right|
+   * `-----------------------------------------------------------------------------------------------´
+   */
+	[_COLEMAK] = LAYOUT_75_ansi(
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_PAUS,  KC_DEL,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_HOME,
+    KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
+    KC_BSPC,  KC_A,     KC_R,     KC_S,     KC_T,     KC_D,     KC_H,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,             KC_PGDN,
+    TD_CAPS,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                                TD_SPC,                                 KC_R2,    KC_R1,    MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
+),
+
+  /*
+   * Colemak DH
+   * ,-----------------------------------------------------------------------------------------------,
+   * | Esc | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |Print|Pause| Del |
+   * ,-----------------------------------------------------------------------------------------------|
+   * |  `  | 1 ! | 2 @ | 3 # | 4 $ | 5 % | 6 ^ | 7 & | 8 * | 9 ( | 0 ) | - _ | = + | Backspace |Home |
+   * |-----------------------------------------------------------------------------------------------|
+   * |  Tab   |  Q  |  W  |  F  |  P  |  B  |  J  |  L  |  U  |  Y  | ; : | [ { | ] } |   \ |  |PgUp |
+   * |-----------------------------------------------------------------------------------------|-----|
+   * |Backspace|  A  |  R  |  S  |  T  |  G  |  M  |  N  |  E  |  I  |  O  | ' " |    Enter    |PgDwn|
+   * |-----------------------------------------------------------------------------------------------|
+   * |    Shift    |  Z  |  X  |  C  |  D  |  V  |  K  |  H  | , < | . > | / ? |  Shift  | Up  | End |
+   * |-----------------------------------------------------------------------------------------------|
+   * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt |RCtl | FN1 |Left |Down |Right|
+   * `-----------------------------------------------------------------------------------------------´
+   */
+	[_COLEMAK_DH] = LAYOUT_75_ansi(
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_PAUS,  KC_DEL,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_HOME,
+    KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
+    KC_BSPC,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,             KC_PGDN,
+    TD_CAPS,            KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                                TD_SPC,                                 KC_R2,    KC_R1,    MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT
+),
+
+  /*
+   * Colemak DH
+   * ,-----------------------------------------------------------------------------------------------,
+   * |RESET|     |     |     |     |     |     |     |     |     |     |     |     |     |     |Mute |
+   * ,-----------------------------------------------------------------------------------------------|
+   * |     |     |     |     |     |     |     |     |     |     |     |     |     |           |     |
+   * |-----------------------------------------------------------------------------------------------|
+   * |        |QWRTY|CLMAK|CLMKD|     |     |     |     |     |     |     |     |     |        |SWAP |
+   * |-----------------------------------------------------------------------------------------|-----|
+   * |         |     |     |     |     |     |     |     |     |     |     |     |             |NORM |
+   * |-----------------------------------------------------------------------------------------------|
+   * |             |     |     |     |     |     |     |     |     |     |     |         |VolU | End |
+   * |-----------------------------------------------------------------------------------------------|
+   * |       |       |       |           Play/Pause              |     |     |     |Prev |VolD |Next |
+   * `-----------------------------------------------------------------------------------------------´
+   */
+	[_FN1] = LAYOUT_75_ansi(
+    QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MUTE,
+    _______,  QWERTY,   COLEMAK,  CLMKDH,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            _______,  XXXXXXX,
+    _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            KC_SWAP,
+    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,            KC_NORM,
+    _______,            XXXXXXX,  XXXXXXX,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            KC_VOLU,  XXXXXXX,
+    _______,  _______,  _______,                                KC_MPLY,                                _______,  _______,  XXXXXXX,  KC_MPRV,  KC_VOLD,  KC_MNXT
+  )
+};
