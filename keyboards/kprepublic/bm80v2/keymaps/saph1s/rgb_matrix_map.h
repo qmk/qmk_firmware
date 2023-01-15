9d447c81056adff16839ac0cb1cb0d528c98bfec
Saph1s <saph1s@nyasquad.com>
2023-01-16 00:51:29 +0300

    Copyright

--- keyboards/kprepublic/bm80v2/keymaps/saph1s/rgb_matrix_map.h
+++ keyboards/kprepublic/bm80v2/keymaps/saph1s/rgb_matrix_map.h
@@ -1,4 +1,4 @@
-/* Copyright 2023 Saph1s
+/* Copyright 2023 Saph1s @Saph1s
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
