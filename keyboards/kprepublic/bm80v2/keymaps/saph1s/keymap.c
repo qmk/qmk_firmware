746a55a359d302b6ec98c495907376a3b5b337df
Saph1s <saph1s@nyasquad.com>
2023-01-16 00:24:44 +0300

    Add GUI toggle

--- keyboards/kprepublic/bm80v2/keymaps/saph1s/keymap.c
+++ keyboards/kprepublic/bm80v2/keymaps/saph1s/keymap.c
@@ -1,19 +1,22 @@
 /* Copyright 2023 Saph1s
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include QMK_KEYBOARD_H
+
+#include "rgb_matrix_map.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -54,3 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 
 };
+
+#ifdef RGB_MATRIX_ENABLE
+    bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+        if (keymap_config.no_gui) {
+            rgb_matrix_set_color(LED_LGUI, RGB_RED);  //Toggle GUI
+        }
+        return false;
+    }
+#endif
