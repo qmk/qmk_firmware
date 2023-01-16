adcf9c993def8db352dbe3bddeeca75de9f57ef5
beelzebubi <tom@sennewald.me>
2023-01-16 11:40:42 +0100

    SRC'd c files

--- users/nazu/nazu.h
+++ users/nazu/nazu.h
@@ -1,5 +1,22 @@
+/* Copyright 2023 Tom Sennewald (@beelzebubi)
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #pragma once
 #include QMK_KEYBOARD_H
+
+#include "keyrecords/wrappers.h"
+#include "keyrecords/process_records.h"
+
+/* Tap Dance definitions */
+#ifdef TAP_DANCE_ENABLE
+#   include "keyrecords/tap_dances.h"
+#endif
+
+// /* OLED definitions */
+// #ifdef OLED_ENABLE
+// #   include "oled/oled.h"
+// #endif
 
 /* Define layer names */
 enum userspace_layers {
@@ -15,19 +32,3 @@ enum userspace_layers {
 };
 
 
-/* Tap Dance definitions */
-#ifdef TAP_DANCE_ENABLE
-#   include "keyrecords/tap_dances.h"
-#   include "keyrecords/tap_dances.c"
-#endif
-
-#ifdef OLED_ENABLE
-/* status variables */
-#    include "oled/oled_rocket.c"
-#    include "oled/oled_luna.c"
-// #    include "oled/oled_status.c"
-#endif
-
-#include "keyrecords/wrappers.h"
-#include "keyrecords/process_records.h"
-#include "keyrecords/process_records.c"
---
+++ users/nazu/rules.mk
@@ -0,0 +1,14 @@
+SRC += $(USER_PATH)/nazu.c \
+        $(USER_PATH)/keyrecords/process_records.c \
+
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+# ifdef TAP_DANCE_ENABLE
+	SRC += $(USER_PATH)/keyrecords/tap_dances.c
+endif
+
+# ifeq ($(strip $(OLED_ENABLE)), yes)
+# ifdef OLED_ENABLE
+	# SRC += $(USER_PATH)/oled/oled_rocket.c \
+	# 		# $(USER_PATH)/oled/oled_status.c \
+	# 		$(USER_PATH)/oled/oled_luna.c
+# endif
