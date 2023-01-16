2e0abc065a37f368981ddce1ce4fa52437b88f48
mechlovin <57231893+mechlovin@users.noreply.github.com>
2023-01-16 17:27:59 +0700

    Update config.h

--- keyboards/mechlovin/zed1800/saber/config.h
+++ keyboards/mechlovin/zed1800/saber/config.h
@@ -34,10 +34,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS { B8, B7, B6, B5, B4, B3}
-#define MATRIX_COL_PINS { A8, B15, B14, B13, B12, B11, B10, B2, B1, B0, A7, A6, A5, A4, A3, A2, A1, A0, C13}
+#define MATRIX_COL_PINS { A8, A15, B14, B13, B12, B11, B10, B2, B1, B0, A7, A6, A5, A4, A3, A2, A1, A0, C13}
 
 #ifdef RGBLIGHT_ENABLE
 #define RGBLIGHT_LAYERS
-#define RGB_DI_PIN A15
+#define RGB_DI_PIN B15
 #define RGBLED_NUM 6
 #endif
