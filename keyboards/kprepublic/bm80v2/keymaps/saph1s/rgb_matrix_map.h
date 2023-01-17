3fb57e9868d7b52dae219dbab083df6ef864c033
jack <0x6a73@protonmail.com>
2023-01-17 10:44:37 -0700

    Update keyboards/kprepublic/bm80v2/keymaps/saph1s/rgb_matrix_map.h

--- keyboards/kprepublic/bm80v2/keymaps/saph1s/rgb_matrix_map.h
+++ keyboards/kprepublic/bm80v2/keymaps/saph1s/rgb_matrix_map.h
@@ -13,7 +13,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#pragma once
 #ifdef RGB_MATRIX_ENABLE
 
     enum led_location_map {
