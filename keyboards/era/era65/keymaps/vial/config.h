b7c00a1b56d6ee9717a443bbbbd99d01a33899fe
eerraa <phyojinbin@gmail.com>
2023-01-19 06:02:46 +0900

    Add ERA65 vial keymap
    Add ERA65 vial keymap
    Change DEBOUNCE_TYPE

---
+++ keyboards/era/era65/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDF, 0x29, 0x49, 0x62, 0x30, 0xC9, 0x3B, 0xBB}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
