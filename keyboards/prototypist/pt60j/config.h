3f6303cedeae4df4f54f59353fa6e6d418422698
Anjheos <Anjheos.Designs@gmail.com>
2023-01-19 20:24:30 -0800

    Adding PT-60J PCB Firmware.

---
+++ keyboards/prototypist/pt60j/config.h
@@ -0,0 +1,22 @@
+// Copyright 2023 Josh (@Anjheos)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
