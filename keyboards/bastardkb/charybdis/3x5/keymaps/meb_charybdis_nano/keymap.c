74748d5985242b075702754295ecfba82c56e3d3
mbissonn <mbissonn@gmail.com>
2023-01-20 13:56:36 -0500

    more changes to charybdis nano

--- keyboards/bastardkb/charybdis/3x5/keymaps/meb_charybdis_nano/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/meb_charybdis_nano/keymap.c
@@ -17,6 +17,9 @@
 
 #include QMK_KEYBOARD_H
 
+#define DPI_UP POINTER_DEFAULT_DPI_FORWARD
+#define DPI_DOWN POINTER_DEFAULT_DPI_REVERSE
+
 enum layers {
     BASE,
     LOWER,
@@ -24,107 +27,111 @@ enum layers {
     AUTO_MOUSE,
 };
 
-bool nav_columns = false;
-bool alt_tabber = false;
-
-void q_esc(qk_tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        case 1:
-            if (nav_columns == false) {
-                tap_code(KC_Q);
-            } else {
-                tap_code(KC_ESC);
-            }
-            break;
-        case 2:
-            if (nav_columns == false) {
-                tap_code(KC_ESC);
-            } else {
-                tap_code(KC_Q);
-            }
-    }
-}
-
-void a_tab(qk_tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        case 1:
-            if (nav_columns == false && alt_tabber == false) {
-                tap_code(KC_A);
-            } else {
-                tap_code(KC_TAB);
-            }
-            break;
-        case 2:
-            if (nav_columns == false && alt_tabber == false) {
-                tap_code(KC_TAB);
-            } else {
-                tap_code(KC_A);
-            }
-    }
-}
-
-void p_bspc(qk_tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        case 1:
-            if (nav_columns == false) {
-                tap_code(KC_P);
-            } else {
-                tap_code(KC_BSPC);
-            }
-            break;
-        case 2:
-            if (nav_columns == false) {
-                tap_code(KC_BSPC);
-            } else {
-                tap_code(KC_P);
-            }
-    }
-}
-
-void quot_ent(qk_tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        case 1:
-            if (nav_columns == false) {
-                tap_code(KC_QUOT);
-            } else {
-                tap_code(KC_ENT);
-            }
-            break;
-        case 2:
-            if (nav_columns == false) {
-                tap_code(KC_ENT);
-            } else {
-                tap_code(KC_QUOT);
-            }
-    }
-}
-
-enum tap_dances {
-    TD_Q_ESC = 0,
-    TD_A_TAB,
-    TD_P_BSPC,
-    TD_QUOT_ENT,
+enum custom_keycodes {
+    A_TAB = CHARYBDIS_SAFE_RANGE,
 };
 
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_Q_ESC] = ACTION_TAP_DANCE_FN(q_esc),
-    [TD_A_TAB] = ACTION_TAP_DANCE_FN(a_tab),
-    [TD_P_BSPC] = ACTION_TAP_DANCE_FN(p_bspc),
-    [TD_QUOT_ENT] = ACTION_TAP_DANCE_FN(quot_ent),
-};
+// bool nav_columns = false;
+// bool alt_tabber = false;
+
+// void q_esc(qk_tap_dance_state_t *state, void *user_data) {
+//     switch (state->count) {
+//         case 1:
+//             if (nav_columns == false) {
+//                 tap_code(KC_Q);
+//             } else {
+//                 tap_code(KC_ESC);
+//             }
+//             break;
+//         case 2:
+//             if (nav_columns == false) {
+//                 tap_code(KC_ESC);
+//             } else {
+//                 tap_code(KC_Q);
+//             }
+//     }
+// }
+
+// void a_tab(qk_tap_dance_state_t *state, void *user_data) {
+//     switch (state->count) {
+//         case 1:
+//             if (nav_columns == false && alt_tabber == false) {
+//                 tap_code(KC_A);
+//             } else {
+//                 tap_code(KC_TAB);
+//             }
+//             break;
+//         case 2:
+//             if (nav_columns == false && alt_tabber == false) {
+//                 tap_code(KC_TAB);
+//             } else {
+//                 tap_code(KC_A);
+//             }
+//     }
+// }
+
+// void p_bspc(qk_tap_dance_state_t *state, void *user_data) {
+//     switch (state->count) {
+//         case 1:
+//             if (nav_columns == false) {
+//                 tap_code(KC_P);
+//             } else {
+//                 tap_code(KC_BSPC);
+//             }
+//             break;
+//         case 2:
+//             if (nav_columns == false) {
+//                 tap_code(KC_BSPC);
+//             } else {
+//                 tap_code(KC_P);
+//             }
+//     }
+// }
+
+// void quot_ent(qk_tap_dance_state_t *state, void *user_data) {
+//     switch (state->count) {
+//         case 1:
+//             if (nav_columns == false) {
+//                 tap_code(KC_QUOT);
+//             } else {
+//                 tap_code(KC_ENT);
+//             }
+//             break;
+//         case 2:
+//             if (nav_columns == false) {
+//                 tap_code(KC_ENT);
+//             } else {
+//                 tap_code(KC_QUOT);
+//             }
+//     }
+// }
+
+// enum tap_dances {
+//     TD_Q_ESC = 0,
+//     TD_A_TAB,
+//     TD_P_BSPC,
+//     TD_QUOT_ENT,
+// };
+
+// qk_tap_dance_action_t tap_dance_actions[] = {
+//     [TD_Q_ESC] = ACTION_TAP_DANCE_FN(q_esc),
+//     [TD_A_TAB] = ACTION_TAP_DANCE_FN(a_tab),
+//     [TD_P_BSPC] = ACTION_TAP_DANCE_FN(p_bspc),
+//     [TD_QUOT_ENT] = ACTION_TAP_DANCE_FN(quot_ent),
+// };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_charybdis_3x5(
-        TD(TD_Q_ESC)        , KC_W    , KC_E               , KC_R            , KC_T               , KC_Y                , KC_U            , KC_I      , KC_O      , TD(TD_P_BSPC)       ,
-        TD(TD_A_TAB)        , KC_S    , KC_D               , KC_F            , KC_G               , KC_H                , KC_J            , KC_K      , KC_L      , TD(TD_QUOT_ENT)     ,
+        KC_Q                , KC_W    , KC_E               , KC_R            , KC_T               , KC_Y                , KC_U            , KC_I      , KC_O      , KC_P                ,
+        A_TAB               , KC_S    , KC_D               , KC_F            , KC_G               , KC_H                , KC_J            , KC_K      , KC_L      , KC_QUOT             ,
         MT(MOD_LSFT,KC_Z)   , KC_X    , KC_C               , KC_V            , KC_B               , KC_N                , KC_M            , KC_COMM   , KC_DOT    , MT(MOD_RSFT,KC_SLSH),
                                         MT(MOD_LCTL,KC_ESC), LT(LOWER,KC_ENT), MT(MOD_LALT,KC_DEL), MT(MOD_LGUI,KC_BSPC), LT(LOWER,KC_SPC)
     ),
     [LOWER] = LAYOUT_charybdis_3x5(
-        KC_HOME             , KC_UP  , KC_END              , KC_PLUS         , KC_LPRN            , KC_RPRN             , KC_7            , KC_8      , KC_9      , KC_DEL              ,
+        KC_HOME             , KC_UP  , KC_END              , KC_PLUS         , KC_LPRN            , KC_RPRN             , KC_7            , KC_8      , KC_9      , KC_TAB              ,
         KC_LEFT             , KC_DOWN, KC_RIGHT            , KC_EQL          , KC_LCBR            , KC_RCBR             , KC_4            , KC_5      , KC_6      , KC_SCLN             ,
         MT(MOD_LSFT,KC_PIPE), KC_UNDS, KC_COLN             , KC_MINS         , KC_LBRC            , KC_RBRC             , KC_1            , KC_2      , KC_3      , MT(MOD_RSFT,KC_BSLS),
-                                       KC_TRNS             , MO(RAISE)       , KC_TRNS            , KC_0                , MO(RAISE)
+                                       KC_TRNS             , MO(RAISE)       , KC_TRNS            , KC_TRNS             , LT(RAISE, KC_0)
     ),
     [RAISE] = LAYOUT_charybdis_3x5(
         KC_1                , KC_2    , KC_3               , KC_4            , KC_5               , KC_6                , KC_7            , KC_8      , KC_9      , KC_0                ,
@@ -134,47 +141,65 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [AUTO_MOUSE] = LAYOUT_charybdis_3x5(
         KC_TRNS             , KC_TRNS , KC_TRNS            , KC_TRNS         , KC_TRNS            , KC_TRNS             , KC_TRNS         , KC_TRNS   , KC_TRNS   , KC_TRNS             ,
-        KC_TRNS             , KC_TRNS , KC_TRNS            , KC_TRNS         , KC_TRNS            , KC_TRNS             , KC_BTN1         , KC_BTN2   , KC_TRNS   , KC_TRNS             ,
-        KC_BTN2             , KC_TRNS , KC_TRNS            , KC_TRNS         , KC_TRNS            , KC_TRNS             , KC_TRNS         , KC_TRNS   , KC_TRNS   , KC_BTN1             ,
-                                        KC_TRNS            , KC_TRNS         , KC_TRNS            , KC_BTN2             , KC_BTN1
+        KC_BTN2             , KC_TRNS , KC_TRNS            , KC_TRNS         , KC_TRNS            , KC_TRNS             , KC_BTN1         , KC_BTN2   , KC_TRNS   , KC_BTN1             ,
+        KC_TRNS             , KC_TRNS , KC_TRNS            , KC_TRNS         , KC_TRNS            , KC_TRNS             , KC_TRNS         , KC_TRNS   , KC_TRNS   , KC_TRNS             ,
+                                        KC_TRNS            , KC_TRNS         , KC_TRNS            , KC_TRNS             , KC_TRNS
     ),
 };
 
 void pointing_device_init_user(void) {
     set_auto_mouse_enable(true);
-}
+};
 
-bool set_scrolling = false;
+bool alt_tabber = false;
+// bool set_scrolling = false;
 
-report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (set_scrolling) {
-        mouse_report.h = mouse_report.x;
-        mouse_report.v = -mouse_report.y;
-        mouse_report.x = mouse_report.y = 0;
-    } else {
-        mouse_report.x = mouse_report.x;
-        mouse_report.y = mouse_report.y;
-    }
-    return mouse_report;
-}
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     if (set_scrolling) {
+//         mouse_report.h = mouse_report.x;
+//         mouse_report.v = -mouse_report.y;
+//         mouse_report.x = mouse_report.y = 0;
+//     } else {
+//         mouse_report.x = mouse_report.x;
+//         mouse_report.y = mouse_report.y;
+//     }
+//     return mouse_report;
+// };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record){
-  switch (keycode){
-    case LT(LOWER,KC_ENT):
-    case LT(LOWER,KC_SPC):
-        if(record->event.pressed){
-            set_scrolling = true;
-        } else {
-            set_scrolling = false;
-        }
-        break;
-    case MT(MOD_LALT,KC_DEL):
-        if(record->event.pressed){
-            alt_tabber = true;
-        } else {
-            alt_tabber = false;
-        }
-        break;
-  }
+    switch (keycode){
+        case LT(LOWER,KC_ENT):
+        case LT(LOWER,KC_SPC):
+            if(record->event.pressed){
+                // set_scrolling = true;
+                charybdis_set_pointer_dragscroll_enabled(true);
+            } else {
+                // set_scrolling = false;
+                charybdis_set_pointer_dragscroll_enabled(false);
+            }
+            break;
+        case MT(MOD_LALT,KC_DEL):
+            if(record->event.pressed){
+                alt_tabber = true;
+            } else {
+                alt_tabber = false;
+            }
+            break;
+        case A_TAB:
+            if(record->event.pressed){
+                if(alt_tabber == true){
+                    register_code(KC_TAB);
+                } else {
+                    register_code(KC_A);
+                }
+            } else {
+                if(alt_tabber == true){
+                    unregister_code(KC_TAB);
+                } else {
+                    unregister_code(KC_A);
+                }
+            }
+            return false;
+    }
   return true;
-}
+};
