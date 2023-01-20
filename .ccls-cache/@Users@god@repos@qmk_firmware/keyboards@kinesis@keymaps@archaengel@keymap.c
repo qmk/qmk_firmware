e61bd178890619161b9bcd234433a2da1235f478
Archaengel <god11341258@gmail.com>
2023-01-19 23:53:53 -0800

    Kind of working with kint

---
+++ .ccls-cache/@Users@god@repos@qmk_firmware/keyboards@kinesis@keymaps@archaengel@keymap.c
@@ -0,0 +1,1 @@
+// This is intentionally left blank.
--- users/archaengel/rules.mk
+++ users/archaengel/rules.mk
@@ -1,6 +1,7 @@
 SRC += oneshot.c
 SRC += swapper.c
-BOOTLOADER = atmel-dfu
+# Disabling while I figure out how to get this to work across different boot loaders
+# BOOTLOADER = atmel-dfu
 
 INTROSPECTION_KEYMAP_C = archaengel.c
 
