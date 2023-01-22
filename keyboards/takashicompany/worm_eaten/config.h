62df267bdcdf00bed59e60a2a687ce1c492df052
takashicompany <t@kashi.company>
2023-01-22 18:58:57 +0900

    fix

--- keyboards/takashicompany/worm_eaten/config.h
+++ keyboards/takashicompany/worm_eaten/config.h
@@ -3,16 +3,17 @@
 
 #pragma once
 
-#define MATRIX_ROWS 4
-#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-
-#ifdef INTEGRATED
+#ifdef INTEGRATE
+	#define MATRIX_ROWS 4
 	#define MATRIX_COLS 10
 	#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5, B4 }
 #else
+	#define MATRIX_ROWS 4 * 2
 	#define MATRIX_COLS 5
 	#define MATRIX_COL_PINS { F4, F5, F6, F7, B1 }
 #endif
+
+#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
 
 #define SOFT_SERIAL_PIN D2
 
