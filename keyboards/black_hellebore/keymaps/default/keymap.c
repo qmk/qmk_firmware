53f7f7f48ae7282eb2b9592a9e8c67edf4f0acc6
MATTMCCA <mccardlematthew0@gmail.com>
2023-01-21 22:28:36 -0500

    fixed License header

--- keyboards/black_hellebore/keymaps/default/keymap.c
+++ keyboards/black_hellebore/keymaps/default/keymap.c
@@ -1,9 +1,9 @@
-﻿/* Copyright 2022 MATTMCCA (@MATTMCCA)
+﻿/* Copyright 2023 MATTMCCA (@MATTMCCA)
+ * SPDX-License-Identifier: GPL-2.0-only
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,7 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 #include QMK_KEYBOARD_H
 
@@ -39,4 +38,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL
 	)		
 };
-
--- keyboards/black_hellebore/mcuconf.h
+++ keyboards/black_hellebore/mcuconf.h
@@ -1,9 +1,9 @@
 /* Copyright 2023 MATTMCCA (@MATTMCCA)
+ * SPDX-License-Identifier: GPL-2.0-only
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
