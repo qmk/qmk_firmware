295d2e17ca3b6b65fcafb47de2bcf771ace24b9b
MATTMCCA <mccardlematthew0@gmail.com>
2023-01-21 22:42:05 -0500

    removed ascii layout comments

--- keyboards/black_hellebore/keymaps/via/keymap.c
+++ keyboards/black_hellebore/keymaps/via/keymap.c
@@ -17,19 +17,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	/*
-	 * +-----------------------------------------------------------+
-	 * ¦ ` ¦ 1 ¦ 2 ¦ 3 ¦ 4 ¦ 5 ¦ 6 ¦ 7 ¦ 8 ¦ 9 ¦ 0 ¦ - ¦ = ¦ Backsp¦
-	 * +-----------------------------------------------------------¦
-	 * ¦ Tab ¦ Q ¦ W ¦ E ¦ R ¦ T ¦ Y ¦ U ¦ I ¦ O ¦ P ¦ [ ¦ ] ¦  \  ¦
-	 * +-----------------------------------------------------------¦
-	 * ¦ Caps ¦ A ¦ S ¦ D ¦ F ¦ G ¦ H ¦ J ¦ K ¦ L ¦ ; ¦ ' ¦  Enter ¦
-	 * +-----------------------------------------------------------¦
-	 * ¦ Shift  ¦ Z ¦ X ¦ C ¦ V ¦ B ¦ N ¦ M ¦ , ¦ . ¦ / ¦    Shift ¦
-	 * +--------------------------------------------+--------------¦
-	 * ¦Ctrl¦GUI ¦Alt ¦                        ¦ Alt¦ GUI¦Menu¦Ctrl¦
-	 * +-----------------------------------------------------------+
-	 */
+
 	[0] = LAYOUT_60_ansi(
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
@@ -63,4 +51,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	)
 		
 };
-
