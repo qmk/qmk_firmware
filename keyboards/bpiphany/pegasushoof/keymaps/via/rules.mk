1d76a41f0c59e9f74856c7c496d2782202df6651
Fucong Wang <wangfuco@Fucongs-Mac-mini.local>
2023-01-21 20:13:05 -0800

    cleanup and restructure

--- keyboards/bpiphany/pegasushoof/keymaps/via/rules.mk
+++ keyboards/bpiphany/pegasushoof/keymaps/via/rules.mk
@@ -2,16 +2,9 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-CUSTOM_MATRIX = yes         # Custom matrix file for the Pegasus Hoof due to the 2x74HC42
-NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # RGB underlight.
 
 VIA_ENABLE = yes
 LTO_ENABLE = yes
