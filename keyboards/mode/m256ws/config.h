3f9aedf26b1a248ef56620b0f9e45c11eb8ed8b1
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2023-01-23 13:06:50 -0300

    Fix column pin mapping

--- keyboards/mode/m256ws/config.h
+++ keyboards/mode/m256ws/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 15
 
 //                        C0 , C1 , C2 , C3 , C4 , C5 , C6 , C7 , C8 , C9 , C10, C11, C12, C13, C14
-#define MATRIX_COL_PINS { A4 , C6 , C7 , C8 , C9 , A8 , A10, B5 , B4 , B3 , D2 , C12, C11, C10, A15 }
+#define MATRIX_COL_PINS { A4 , C6 , C7 , C8 , C9 , A8 , A10, B5 , B4 , B3 , D2 , C12, C11, A15, C10 }
 //                        R0 , R1 , R2 , R3 , R4 , R5
-	#define MATRIX_ROW_PINS { B10, B12, B8 , C0 , C1 , A3 }
+#define MATRIX_ROW_PINS { B10, B12, B8 , C0 , C1 , A3 }
 
 #define DIODE_DIRECTION COL2ROW
 
