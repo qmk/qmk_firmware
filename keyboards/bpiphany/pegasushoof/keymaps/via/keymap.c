98ce31b6ffaffdc4332303bbd63165f4d660d0dd
Emrik Östling <emrik.ostling@gmail.com>
2023-01-23 13:03:33 +0100

    Update keyboards/bpiphany/pegasushoof/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/bpiphany/pegasushoof/keymaps/via/keymap.c
+++ keyboards/bpiphany/pegasushoof/keymaps/via/keymap.c
@@ -39,17 +39,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-void led_set_user(uint8_t usb_led) {
-  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-    ph_caps_led_on();
-  } else {
-    ph_caps_led_off();
-  }
+bool led_update_user(led_t led_state) {
+    if (led_state.caps_lock) {
+        ph_caps_led_on();
+    } else {
+        ph_caps_led_off();
+    }
 
-  if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-    ph_sclk_led_on();
-  } else {
-    ph_sclk_led_off();
-  }
+    if (led_state.scroll_lock) {
+        ph_sclk_led_on();
+    } else {
+        ph_sclk_led_off();
+    }
+    return false;
 }
 
