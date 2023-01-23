16eaeb5f14d510815b566870694ed01f821ff9d4
zen <desu@boxxy>
2023-01-23 09:46:50 -0600

    modified:   keyboards/gmmk/gmmk2/p65/ansi/keymaps/zen/config.h 	modified:   keyboards/gmmk/gmmk2/p65/ansi/keymaps/zen/halconf.h 	modified:   keyboards/gmmk/gmmk2/p65/ansi/keymaps/zen/mcuconf.h

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/zen/config.h
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/zen/config.h
@@ -1,3 +1,19 @@
+/* Copyright 2023 zen <github@desultory>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
 
 // Undefine rgb modes enabled for the keyboard but not used
