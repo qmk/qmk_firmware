65e72529b476a28c135a8f14fc602c80ebb729f7
Daggette <KatrinaPepperKat@protonmail.com>
2023-01-23 22:04:37 -0700

    Adjustments to kb38.c

--- keyboards/doio/kb38/kb38.c
+++ keyboards/doio/kb38/kb38.c
@@ -17,6 +17,7 @@
 #include "quantum.h"
 
 //Display
+#ifdef OLED_ENABLE
 static void render_logo(void) {
     static const char PROGMEM qmk_logo[] = {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
@@ -32,8 +33,10 @@ bool oled_task_kb(void) {
     render_logo();
     return true;
 }
+#endif
 
 //Knobs
+#ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) {
         return false;
@@ -63,3 +66,4 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     }
     return true;
 }
+#endif
