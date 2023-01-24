0e2a7ae4e5c3645040bb857b14c62e7190c7c55b
wangfuco <46733695+wangfuco@users.noreply.github.com>
2023-01-23 19:16:56 -0800

    Update keyboards/bpiphany/pegasushoof/keymaps/via/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/bpiphany/pegasushoof/keymaps/via/rules.mk
+++ keyboards/bpiphany/pegasushoof/keymaps/via/rules.mk
@@ -1,10 +1,5 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
-MOUSEKEY_ENABLE = no        # Mouse keys
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
 VIA_ENABLE = yes
 LTO_ENABLE = yes
