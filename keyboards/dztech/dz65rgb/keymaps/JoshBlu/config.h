5a734ba134d457e511ff0b150f1df2be90d56781
JoshBlu <>
2023-01-24 16:35:44 -0700

    Added via support and reduced firmware size to fit

--- keyboards/dztech/dz65rgb/keymaps/JoshBlu/config.h
+++ keyboards/dztech/dz65rgb/keymaps/JoshBlu/config.h
@@ -1,1 +1,7 @@
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+//Following rules to reduce firmware size
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_MUSIC_MODE
+#define LAYER_STATE_8BIT
