b5514e7caedcf99c2ae86cc100cf176c2d16ebf2
JoshBlu <>
2023-01-24 16:35:44 -0700

    Added via support and reduced firmware size to fit

--- keyboards/dztech/dz65rgb/keymaps/JoshBlu/rules.mk
+++ keyboards/dztech/dz65rgb/keymaps/JoshBlu/rules.mk
@@ -1,1 +1,6 @@
 CAPS_WORD_ENABLE = yes
+VIA_ENABLE = yes
+LTO_ENABLE = yes	#Recuse the size of the firmware through link time optimization
+#Following rules to reduce size of firmware size
+GRAVE_ESC_ENABLE = no
+MUSIC_ENABLE = no
