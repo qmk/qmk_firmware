14f9c4ae8dce357cbba092621ab425dd3dc2c412
4pplet <mail@4pplet.com>
2023-01-26 19:50:03 +0100

    Update keyboards/4pplet/steezy60/rev_b/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/4pplet/steezy60/rev_b/config.h
+++ keyboards/4pplet/steezy60/rev_b/config.h
@@ -18,9 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 14
 // ROWS: Top to bottom, COLS: Left to right
 #define MATRIX_ROW_PINS {B8,A15,C13,A2,A6}
 #define MATRIX_COL_PINS {A7,A5,A4,A3,A1,F1,F0,C15,C14,B9,B6,B5,B4,B3}
@@ -28,16 +25,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-#define QMK_ESC_OUTPUT A7 // usually COL
-#define QMK_ESC_INPUT B8 // usually ROW
-
 #define RGB_DI_PIN A8
 #define WS2812_EXTERNAL_PULLUP
 #define RGBLED_NUM 12
-#define BACKLIGHT_LEVELS 5
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
