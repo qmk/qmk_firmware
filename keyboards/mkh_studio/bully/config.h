e7d4126718fba6b82ce542025d6339664b5e9161
Zhol Internet <zhol@internet.keyboard>
2023-01-27 15:55:59 -0800

    [Keyboard] Add mkh studio bully

---
+++ keyboards/mkh_studio/bully/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Use 1000hz polling */
+#define USB_POLLING_INTERVAL_MS 1
