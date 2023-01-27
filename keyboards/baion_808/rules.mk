b23dd86ab8f8a0cbcc7429f2cdf1ddb414e8a3a1
yiancar <yiancar@gmail.com>
2023-01-27 15:32:28 +0000

    Add one too many layouts :D

--- keyboards/baion_808/rules.mk
+++ keyboards/baion_808/rules.mk
@@ -18,3 +18,5 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+
+LAYOUTS = tkl_f13_ansi tkl_f13_ansi_tsangan tkl_f13_ansi_split_bs_rshift tkl_f13_ansi_tsangan_split_bs_rshift tkl_f13_iso tkl_f13_iso_split_bs_rshift tkl_f13_iso_tsangan tkl_f13_iso_tsangan_split_bs_rshift
