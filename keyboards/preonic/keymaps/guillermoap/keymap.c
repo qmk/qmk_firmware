3ae73c5af0aff3c909ad56cf5849bf8f92568f8f
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-01-28 18:12:25 -0300

    Copyright and update README

--- keyboards/preonic/keymaps/guillermoap/keymap.c
+++ keyboards/preonic/keymaps/guillermoap/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2015-2021 Jack Humbert
+/* Copyright 2023 Guillermo Aguirre
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
