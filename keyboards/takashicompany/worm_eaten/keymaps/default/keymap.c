dbefc7304b493e468f28f4b3fab2137d2362a6df
takashicompany <t@kashi.company>
2023-01-28 15:20:54 +0900

    Update keyboards/takashicompany/worm_eaten/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/takashicompany/worm_eaten/keymaps/default/keymap.c
+++ keyboards/takashicompany/worm_eaten/keymaps/default/keymap.c
@@ -5,7 +5,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    LAYOUT(
+    [0] = LAYOUT(
         LT(7, KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A, KC_S, LT(6, KC_D), KC_F, KC_G, KC_H, KC_J, LT(6, KC_K), KC_L, KC_ENT,
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC,
