a768620cfecdf020193487f0c39c566abce8f92b
dlewallen-figure <dlewallen@figure.com>
2023-01-29 09:55:40 -0600

    refactor: moves backspace/del to thumb

--- keyboards/keebio/iris/keymaps/via/keymap.c
+++ keyboards/keebio/iris/keymaps/via/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, LT(3, KC_TAB), LT(1,KC_ENT),         LT(2,KC_SPC),  KC_RALT, KC_RGUI
+                                    KC_LGUI, LT(3, KC_TAB), LT(1,KC_ENT),         LT(2,KC_SPC),  KC_BSPC, KC_DEL
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
