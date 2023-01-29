9c12bd3b6b8ff915a9d8786e30f9978fb6160e5c
lingawakad <noah@spaceling.sh>
2023-01-29 10:54:36 -0500

    add ferris/lingawakad

---
+++ keyboards/ferris/keymaps/lingawakad/README.md
@@ -0,0 +1,5 @@
+the spacemonkey_custom sweep layout.
+
+![layer0](./images/layer0.png)
+![layer1](./images/layer1.png)
+![layer2](./images/layer2.png)
---
+++ keyboards/ferris/keymaps/lingawakad/rules.mk
@@ -0,0 +1,25 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+LTO_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+NKRO_ENABLE = no
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+UNICODE_ENABLE = yes
+AUDIO_ENABLE = no
+SPLIT_KEYBOARD = yes
+CAPS_WORD_ENABLE = yes
+LAYOUTS = split_3x5_2
