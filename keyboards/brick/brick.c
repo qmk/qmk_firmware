5670523025327c4b9e9ec4f80357eb5f5ae57fe9
Juno Nguyen <juno.ngx@gmail.com>
2023-02-01 23:13:15 +0800

    Move brick rotary encoder code to brick.c

--- keyboards/brick/brick.c
+++ keyboards/brick/brick.c
@@ -16,3 +16,15 @@
  */     
 
 #include "brick.h"
+
+#ifdef ENCODER_ENABLE
+    bool encoder_update_kb(uint8_t index, bool clockwise) {
+        if (!encoder_update_user(index, clockwise)) { return false; }
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+        return true;
+    }
+#endif
--- keyboards/brick/keymaps/default/keymap.c
+++ keyboards/brick/keymaps/default/keymap.c
@@ -72,12 +72,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
     )
 };
-bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) { return false; }
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
-    return true;
-}
