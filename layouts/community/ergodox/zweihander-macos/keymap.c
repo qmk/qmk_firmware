1c4e5e8b57602d61f81072d4957bcf2284e82832
Nathan Galt <adiabatic@users.noreply.github.com>
2023-01-31 21:11:25 -0800

    Replace `'` with `⇪`

--- layouts/community/ergodox/zweihander-macos/keymap.c
+++ layouts/community/ergodox/zweihander-macos/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |  L⌃  |  L⌥  |  L⌘  |   ←  |   →  |                                       |   ↑  |   ↓  |   [  |   ]  |  ↩︎   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |  `~  |  '"  |       |   ⎋  |    ⌫   |
+ *                                        |  `~  |  ⇪   |       |   ⎋  |    ⌫   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |  L⌥  |       |  R⌥  |        |      |
  *                                 |   ↩︎  |  ⇥  |------|       |------|   ⇥   |      |
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_BSPC,        KC_A,         KC_S,     KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,     KC_C,   KC_V,   KC_B,   KC_LGUI,
         KC_LCTL,        KC_LALT,      KC_LGUI,  KC_LEFT,KC_RGHT,
-                                                KC_GRV, KC_QUOT,
+                                                KC_GRV, KC_CAPS,
                                                                 KC_LALT,
                                                 KC_ENT ,KC_TAB ,KC_LCTL,
         // right hand
