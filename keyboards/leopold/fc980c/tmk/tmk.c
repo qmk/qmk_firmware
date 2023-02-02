eb04ba35da24d26ac3dede6308963d3b0ed0e1c9
Wilhelm Schuster <dev@rot13.io>
2023-02-02 13:02:58 +0100

    Move fc660c and fc980c under leopold/

--- keyboards/leopold/fc980c/tmk/tmk.c
+++ keyboards/leopold/fc980c/tmk/tmk.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "fc980c.h"
+#include QMK_KEYBOARD_H
 
 #ifdef ACTUATION_DEPTH_ADJUSTMENT
 #include "actuation_point.h"
