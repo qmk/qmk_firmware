d94581bdd30f11d3448ea316aa784d070c55f783
Juno Nguyen <juno.ngx@gmail.com>
2023-02-02 11:43:01 +0800

    Update keyboards/brick/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/brick/rules.mk
+++ keyboards/brick/rules.mk
@@ -1,12 +1,14 @@
-LTO_ENABLE = yes
-BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = no 	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no    # Mouse keys
-EXTRAKEY_ENABLE = yes   # Audio control and System control
-CONSOLE_ENABLE = no     # Console for debug
-COMMAND_ENABLE = no     # Commands for debug and configuration
-SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes       # USB Nkey Rollover
-RGBLIGHT_ENABLE = yes    # RGB lighting
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes
-AUDIO_ENABLE = no
+LTO_ENABLE = yes
