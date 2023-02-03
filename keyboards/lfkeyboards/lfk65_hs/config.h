96d0c39273ee44a2d56bc329902e1d85881c2af1
fauxpark <fauxpark@gmail.com>
2023-02-03 20:35:45 +1100

    Clean up `DEBOUNCE` in config.h, L

--- keyboards/lfkeyboards/lfk65_hs/config.h
+++ keyboards/lfkeyboards/lfk65_hs/config.h
@@ -32,9 +32,6 @@
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
 
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
