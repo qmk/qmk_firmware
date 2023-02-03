359df86889f5e8a792d1bdd3593d63b34fd7eec6
fauxpark <fauxpark@gmail.com>
2023-02-03 20:41:16 +1100

    Clean up `DEBOUNCE` in config.h, R

--- keyboards/ryanbaekr/rb69/config.h
+++ keyboards/ryanbaekr/rb69/config.h
@@ -39,11 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 //#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
 
-
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
 #define BACKLIGHT_LEVELS 10
 
 /* Underglow options */
