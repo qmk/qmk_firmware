8adaf1559df4113ce162b658a24d2957ea58445e
fauxpark <fauxpark@gmail.com>
2023-02-03 20:44:09 +1100

    Clean up `DEBOUNCE` in config.h, U

--- keyboards/ungodly/launch_pad/config.h
+++ keyboards/ungodly/launch_pad/config.h
@@ -108,6 +108,3 @@
 // #    define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
 
 #endif
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
--- keyboards/utd80/config.h
+++ keyboards/utd80/config.h
@@ -37,9 +37,6 @@
 #define LED_CAPS_LOCK_PIN B6
 #define LED_SCROLL_LOCK_PIN B5
 
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
