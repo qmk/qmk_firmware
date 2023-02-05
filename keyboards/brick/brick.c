82a9d4b6d13c1600cab46294f0be93a4a3aebbdd
Joel Challis <git@zvecr.com>
2023-02-05 20:43:46 +0000

    Apply suggestions from code review

--- keyboards/brick/brick.c
+++ keyboards/brick/brick.c
@@ -1,5 +1,4 @@
-/*
- * Copyright 2020 Jason Chestnut <jason.chestnut@gmail.com>
+/* Copyright 2023 Juno Nguyen <juno.ngx@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +12,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */     
+ */
 
 #include "brick.h"
 
