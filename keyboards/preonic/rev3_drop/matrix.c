8a102b64addc67ce9cde1e1a0c70011698bd213b
jack <0x6A73@pm.me>
2023-02-05 08:24:08 -0700

    replace macros

--- keyboards/preonic/rev3_drop/matrix.c
+++ keyboards/preonic/rev3_drop/matrix.c
@@ -132,7 +132,7 @@ uint8_t matrix_scan(void) {
         }
     }
 
-    if (debouncing && timer_elapsed(debouncing_time) > 5) {
+    if (debouncing && timer_elapsed(debouncing_time) > DEBOUNCE) {
         for (int row = 0; row < MATRIX_ROWS; row++) {
             matrix[row] = 0;
             for (int col = 0; col < MATRIX_COLS; col++) {
