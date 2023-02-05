186a4d81364d881bb0d8fac9ed2e2394c44beb13
TommyZ <60314626+tommyamoszhao@users.noreply.github.com>
2023-02-05 13:27:08 +0800

    Update keyboards/quarkeys/z40/rgblgt/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/quarkeys/z40/rgblgt/rules.mk
+++ keyboards/quarkeys/z40/rgblgt/rules.mk
@@ -9,5 +9,4 @@ COMMAND_ENABLE = no        	# Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
