374d184177a90e02754c5a48fd48d5ae1cca11fd
eason <98533237+EasonQian1@users.noreply.github.com>
2023-02-06 08:48:03 +0800

    Update keyboards/eason/greatsword80/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/eason/greatsword80/config.h
+++ keyboards/eason/greatsword80/config.h
@@ -16,22 +16,6 @@
 
 #pragma once
 
-#include "config_common.h"
-
-/* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 16
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
 #define MATRIX_ROW_PINS { D0, D1, D2, D3, D5, D4 }
 #define MATRIX_COL_PINS { D6, D7, B4, B5, B6, C6, C7, E2, F7, F6, F5, F4, B0, B1, B2, B3 }
 
