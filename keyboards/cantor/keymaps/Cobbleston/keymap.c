600a16c3cf9cabc8c3c9d09772784df81f82d32d
Cobbleston <canovasimo@gmail.com>
2023-02-07 08:27:26 +0100

    Fix extra tab

--- keyboards/cantor/keymaps/Cobbleston/keymap.c
+++ keyboards/cantor/keymaps/Cobbleston/keymap.c
@@ -53,7 +53,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_split_3x6_3(
 		KC_TAB,		KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	/**/	KC_Y,	KC_U,	KC_I, 		KC_O, 	KC_P, 		KC_DEL, 
-		KC_LALT,		KC_A,	KC_S,	KC_D,	KC_F,	KC_G,	/**/	KC_H,	KC_J,	KC_K,		KC_L,	KC_SCLN,	KC_PSCR,
+		KC_LALT,	KC_A,	KC_S,	KC_D,	KC_F,	KC_G,	/**/	KC_H,	KC_J,	KC_K,		KC_L,	KC_SCLN,	KC_PSCR,
 		KC_LSFT, 	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,	/**/	KC_N,	KC_M,	KC_COMM,	KC_DOT,	KC_SLSH,	OSL(EXTRA),
 						CTRL_ENT,	GUI_ESC,	TAB_NUM,	/**/	BSPC_NAV,	SPC_SYM,	KC_LSFT
 	),
