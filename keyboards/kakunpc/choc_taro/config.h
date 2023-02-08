566d88332d3ba0eb80deebf5d9291389e923842f
fauxpark <fauxpark@gmail.com>
2023-02-08 13:30:41 +1100

    Remove `SOFT_SERIAL_PIN` for non-split boards

--- keyboards/kakunpc/choc_taro/config.h
+++ keyboards/kakunpc/choc_taro/config.h
@@ -34,11 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
 #define MATRIX_COL_PINS { D4, C6, D7, E6, B4 }
 
-/*
- * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
- */
-#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
-
 // #define RGB_DI_PIN E2
 // #ifdef RGB_DI_PIN
 //   #define RGBLED_NUM 16
