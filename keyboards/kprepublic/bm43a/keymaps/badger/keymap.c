fa66dc7138d34beba87ea3c5d309b8859da6edca
Dan White <github@bluetufa.com>
2023-02-08 17:16:19 -0700

    Badger BM43a

--- keyboards/kprepublic/bm43a/keymaps/badger/keymap.c
+++ keyboards/kprepublic/bm43a/keymaps/badger/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Dan White <opensource@bluetufa.com>
+Copyright 2023 Dan White <opensource@bluetufa.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,19 +15,43 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "badger.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY]     = LAYOUT(
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        MOVE_MAC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  LT_SLSH,
+        KC_LCTL,  KC_RALT, KC_LGUI, LT_SPACE,         LT_SPACE,          LT_FUNC, KC_DOWN, KC_UP,   KC_RGHT
+    ),
+    [_MOVE]       = LAYOUT(
+        KC_GRV,   VS_STEP, VS_INTO, VS_OUT,  VS_RUN,  VS_STOP,  VS_MVUP, WD_BACK, KC_HOME, KC_END,  WD_FRWD, WD_DELE,
+        _______,  MM_LH,   MM_MAX,  MM_RH,   IJ_FIND, VS_LINE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______,  MM_UH,   MM_BH,   MAC_CPY, MAC_PST, _______,  VS_MVDN, IJ_REN,  IJ_BACK, IJ_FWD,  _______,
+        _______,  _______, _______, KC_BSPC,          _______,           _______, _______, _______, _______
+    ),
+    [_MACRO]      = LAYOUT(
+        KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+        _______,  _______, _______, _______, _______, KC_SCLN,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
+        _______,  _______, _______, _______, _______, _______,  _______, _______, KC_SCLN, KC_QUOT, _______,
+        _______,  _______, _______, _______,          _______,           _______, _______, _______, _______
+    ),
+    [_FUNCTION]   = LAYOUT(
+        KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+        _______,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_COLN,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DQUO,
+        KC_0,     KC_7,    KC_2,    KC_3,    KC_PLUS, _______,  _______, _______, KC_COLN, KC_DQUO, CONFIG,
+        _______,  _______, _______, _______,          _______,           _______, _______, _______, _______
+    ),
+    [_CONFIG]     = LAYOUT(
+        QK_BOOT,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
+        _______,  BL_TOGG, BL_DOWN, BL_UP,   BL_STEP, _______,  _______, _______, KC_F11,  KC_F12,  _______,
+        _______,  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI,  RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,
+        _______,  _______, _______, _______,           _______,          _______, _______, _______, _______
+    )
+    /*
   [_QWERTY]     = LAYOUT_60_ansi(\
      KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,   KC_BSPC, \
      KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS, \
      MOVE_MAC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,   KC_ENT,   \
      KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   \
-     KC_LCTL,   KC_LALT,  KC_LGUI,  KC_SPC,   LT_FUNC,  KC_RGUI,  KC_RALT,  J_MACRO),
-
-  [_MOVE]       = LAYOUT_60_ansi(\
-     MAC_FRC,   MM_LEFT,  MM_RGHT,  KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  P_BRACE,  P_PAREN,  P_BRACK,  IJ_TOP,    IJ_BOTT,  WD_DELE, \
-     KC_BACK,   VS_STEP,  VS_INTO,  VS_OUT,   VS_RUN,   VS_STOP,  VS_MVUP,  WD_BACK,  KC_HOME,  KC_END,   WD_FRWD,  IJ_BACK,   IJ_FWD,   KC_NEXT, \
-     _______,   MM_LH,    MM_MAX,   MM_RH,    IJ_FIND,  VS_LINE,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  CM_RIGHT, CM_DOWN,   _______,  \
-     _______,   MM_UH,    MM_BH,    MAC_CPY,  MAC_PST,  SHEBANG,  VS_MVDN,  IJ_REN,   IJ_IMPL,  IJ_DECL,  IJ_USAG,  _______,   \
-     _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______),
+     KC_LCTL,   KC_LALT,  KC_LGUI,  KC_SPC,   FUNCTION, KC_RGUI,  KC_RALT,  J_MACRO),
 
   [_FUNCTION]   = LAYOUT_60_ansi(\
      KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_DEL, \
@@ -45,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CONFIG]     = LAYOUT_60_ansi(\
      _______,   DF_1,     DF_2,     DF_3,     DF_4,      KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_DEL, \
-     _______,   _______,  _______,  EE_CLR,   QK_BOOT,   _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_INS, \
-     _______,   _______,  _______,  DB_TOGG,  RGB_MOD,   RGB_HUI,  RGB_SAI,  RGB_SPI,  RGB_M_B,  _______,  _______,  RGB_TOG,   _______,  \
+     _______,   _______,  _______,  EEP_RST,  RESET,     _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  KC_INS, \
+     _______,   _______,  _______,  DEBUG,    RGB_MOD,   RGB_HUI,  RGB_SAI,  RGB_SPI,  RGB_M_B,  _______,  _______,  RGB_TOG,   _______,  \
      _______,   _______,  _______,  _______,  RGB_RMOD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  RGB_M_K,  _______,  _______,  _______,   \
-     _______,   _______,  _______,  _______,  _______,   _______,  _______,  _______)
+     _______,   _______,  _______,  _______,  _______,   _______,  _______,  _______) */
 };
