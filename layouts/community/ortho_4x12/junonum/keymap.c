2335948c9f9732977f65dbd3b4d2902df30e8efd
Juno Nguyen <juno.ngx@gmail.com>
2023-02-09 12:01:30 +0800

    user junonum: remove unnecessary diff

--- layouts/community/ortho_4x12/junonum/keymap.c
+++ layouts/community/ortho_4x12/junonum/keymap.c
@@ -359,12 +359,3 @@ bool music_mask_user(uint16_t keycode) {
             return true;
     }
 }
-
-#ifdef RGBLIGHT_ENABLE
-    void keyboard_post_init_user(void) {
-        rgblight_enable_noeeprom(); // Enables RGB, without saving settings
-        // Vibrant cyan
-        rgblight_sethsv_noeeprom(HSV_YELLOW);
-        rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-    }
-#endif
