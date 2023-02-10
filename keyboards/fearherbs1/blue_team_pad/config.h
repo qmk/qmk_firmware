1f15707342d6dc88845a7ad5e9579fe78ddc2305
Thomas <fearherbs1@gmail.com>
2023-02-10 17:29:43 -0500

    Add blue_team_pad
    add the blue team pad macropad

---
+++ keyboards/fearherbs1/blue_team_pad/config.h
@@ -0,0 +1,16 @@
+// Copyright 2022-2023 Thomas Autiello Jr (@fearherbs1)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "config_common.h"
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+#ifdef OLED_ENABLE
+#    define OLED_DISPLAY_128X32
+#define I2C1_SCL_PIN        GP27
+#define I2C1_SDA_PIN        GP26
+#define I2C_DRIVER I2CD2
+#define OLED_BRIGHTNESS 128
+#endif
---
+++ keyboards/fearherbs1/blue_team_pad/rules.mk
@@ -0,0 +1,8 @@
+# Features
+BACKLIGHT_ENABLE = no       # Enable Keyboard Backlight Functionality
+RGBLIGHT_ENABLE = no        # Enable Keyboard RGB Underglow
+AUDIO_ENABLE = no           # Audio Output
+ENCODER_MAP_ENABLE = yes    # Encoder Mapping Support
+# OLED
+OLED_ENABLE = yes           # Enable OLED
+OLED_DRIVER = SSD1306       # Oled Driver Selection
