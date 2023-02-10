7e6ca200fd00bbaa93cc06e8551f9387cd3ade52
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2023-02-10 18:03:49 +0800

    Update keyboards/hfdkb/keyboard_sw/mk47/mk47.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/hfdkb/keyboard_sw/mk47/mk47.c
+++ keyboards/hfdkb/keyboard_sw/mk47/mk47.c
@@ -168,3 +168,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+#ifdef RGB_MATRIX_ENABLE
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+        return false;
+    }
+    // caps lock cyan
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(12, 255, 255, 255);
+    }
+    return true;
+}
+#endif
