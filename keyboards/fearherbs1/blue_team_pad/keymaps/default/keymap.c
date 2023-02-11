3bfa56c6e07a2c792950f022fcdbd3b37b77f135
Thomas <fearherbs1@gmail.com>
2023-02-10 22:57:13 -0500

    Update keyboards/fearherbs1/blue_team_pad/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/fearherbs1/blue_team_pad/keymaps/default/keymap.c
+++ keyboards/fearherbs1/blue_team_pad/keymaps/default/keymap.c
@@ -18,8 +18,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT)  },
-    [1] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
-    [2] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),          ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
 #endif
