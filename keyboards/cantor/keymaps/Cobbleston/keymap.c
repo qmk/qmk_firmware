f9c311c859088d0f4b79bcbef7fc76e9ea736132
Cobbleston <canovasimo@gmail.com>
2023-02-11 17:46:16 +0100

    Fixed some bug and added _ in symbol layer

--- keyboards/cantor/keymaps/Cobbleston/keymap.c
+++ keyboards/cantor/keymaps/Cobbleston/keymap.c
@@ -5,7 +5,7 @@ enum layers {
 	NUMBER,
 	NAVIG,
 	SYMBOL,
-	EXTRA
+	EXTRA,
 };
 
 enum custom_keycodes {
@@ -52,33 +52,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_split_3x6_3(
-		KC_TAB,		KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	/**/	KC_Y,	KC_U,	KC_I, 		KC_O, 	KC_P, 		KC_DEL, 
-		KC_LALT,	KC_A,	KC_S,	KC_D,	KC_F,	KC_G,	/**/	KC_H,	KC_J,	KC_K,		KC_L,	KC_SCLN,	KC_PSCR,
-		KC_LSFT, 	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,	/**/	KC_N,	KC_M,	KC_COMM,	KC_DOT,	KC_SLSH,	OSL(EXTRA),
+		KC_GRAVE,	KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	/**/	KC_Y,	KC_U,	KC_I, 		KC_O, 	KC_P, 		KC_DEL, 
+		KC_PSCR,	KC_A,	KC_S,	KC_D,	KC_F,	KC_G,	/**/	KC_H,	KC_J,	KC_K,		KC_L,	KC_SCLN,	KC_QUOT,
+		KC_LALT, 	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,	/**/	KC_N,	KC_M,	KC_COMM,	KC_DOT,	KC_SLSH,	KC_RALT,
 						CTRL_ENT,	GUI_ESC,	TAB_NUM,	/**/	BSPC_NAV,	SPC_SYM,	KC_LSFT
 	),
 	[NUMBER] = LAYOUT_split_3x6_3( // NUMBER + GUI/ALT/CTRL/SHIFT + FUNCTION
 		KC_NO,	KC_F1,		 KC_F2,		KC_F3,		KC_F4,		KC_F5,	/**/	KC_PAST,	KC_7,	KC_8,	KC_9,	KC_PMNS,	KC_NO,
 		KC_F11,	KC_LGUI,	KC_LALT,	KC_LCTL,	KC_LSFT,	KC_NO,	/**/	KC_PSLS,	KC_4,	KC_5,	KC_6,	KC_PPLS,	KC_NO,
 		KC_F12,	KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,	/**/	KC_PEQL,	KC_1,	KC_2,	KC_3,	KC_NO,		KC_NO,
-										KC_NO,		KC_NO,		OSL(EXTRA),	/**/	KC_ENT,		KC_0,	KC_DOT
+										KC_NO,		KC_NO,		KC_NO,	/**/	KC_BSPC,	KC_0,	KC_DOT
 	),
 	[NAVIG] = LAYOUT_split_3x6_3( // ARROW + ÀÈÌÒÙ
 		KC_NO, 	ULIT,	ALIT,	ELIT,	ILIT,	OLIT,	/**/	KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,
 	  	KC_NO, 	KC_LGUI,KC_LALT,KC_LCTL,KC_LSFT,KC_NO,	/**/	KC_LEFT, 	KC_DOWN,	KC_UP,		KC_RGHT,	KC_CAPS,	KC_NO,
 	    KC_NO, 	UBIG,	ABIG,	EBIG,	IBIG,	OBIG,	/**/	KC_HOME, 	KC_PGDN,	KC_PGUP,	KC_END,		KC_INS,		KC_NO,
-						KC_NO,	 	KC_NO,		KC_NO,	/**/	OSL(EXTRA),	KC_SPC,		KC_LSFT
+						KC_NO,	 	KC_NO,		KC_NO,	/**/	KC_BSPC,	KC_SPC,		KC_LSFT
 	),
 	[SYMBOL] = LAYOUT_split_3x6_3(
-		KC_NO,		KC_QUOT,	KC_LT,		KC_GT,		KC_DQUO,	KC_GRAVE,	/**/	KC_AMPR,	DP2X,		KC_LBRC,	KC_RBRC,	KC_PERC,	KC_NO,
+		KC_NO,		KC_QUOT,	KC_LT,		KC_GT,		KC_DQUO,	KC_GRAVE,	/**/	KC_AMPR,	KC_UNDS,	KC_LBRC,	KC_RBRC,	KC_PERC,	KC_NO,
 		KC_NO,		KC_EXLM,	KC_MINS,	KC_PLUS,	KC_EQL,		KC_HASH,	/**/	KC_PIPE,	KC_COLN,	KC_LPRN,	KC_RPRN,	KC_QUES,	KC_NO,
 		KC_NO,		KC_CIRC,	KC_SLSH,	KC_ASTR,	KC_BSLS,	UPDIR,		/**/	KC_TILD,	KC_DLR,		KC_LCBR,	KC_RCBR,	KC_AT,		KC_NO,
-											KC_DEL,		KC_TAB,		KC_ESC,		/**/	KC_NO,		KC_NO,		KC_NO
-	),
-	[EXTRA] = LAYOUT_split_3x6_3(
-		QK_BOOT,KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	/**/	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,
-		KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	/**/	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,
-		KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	/**/	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,
-						KC_NO,		KC_NO,		KC_NO,	/**/	KC_NO,		KC_NO,		KC_NO
+											KC_LCTL,	KC_LGUI,	KC_TAB,		/**/	KC_NO,		KC_NO,		KC_NO
 	)
 };
--- keyboards/cantor/keymaps/Cobbleston/rules.mk
+++ keyboards/cantor/keymaps/Cobbleston/rules.mk
@@ -1,7 +1,7 @@
 SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = usart
 
-CAPS_WORD_ENABLE = yes
+#CAPS_WORD_ENABLE = yes
 UNICODE_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+#MOUSEKEY_ENABLE = yes
 LTO_ENABLE = yes # If firmware package is too large, this can shrink it a bit
