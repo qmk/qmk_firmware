20a3a8158e83e7c1d6480d5c4816450b519687fe
Thomas <fearherbs1@gmail.com>
2023-02-10 21:57:01 -0500

    Update keyboards/fearherbs1/blue_team_pad/blue_team_pad.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/fearherbs1/blue_team_pad/blue_team_pad.c
+++ keyboards/fearherbs1/blue_team_pad/blue_team_pad.c
@@ -3,7 +3,10 @@
 
 #include "quantum.h"
 #ifdef OLED_ENABLE
-bool oled_task_user(void) {
+bool oled_task_kb(void) {
+    if (!oled_task_user()) {
+        return false;
+    }
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Blue Team Pad \nCurrent Layer: "), false);
 
