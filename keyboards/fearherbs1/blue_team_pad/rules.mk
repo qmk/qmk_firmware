e4b78d22e89403af09660fdd1f554c575dbe4950
Thomas <fearherbs1@gmail.com>
2023-02-10 21:28:45 -0500

    Final Formatting Fixes

--- keyboards/fearherbs1/blue_team_pad/rules.mk
+++ keyboards/fearherbs1/blue_team_pad/rules.mk
@@ -1,8 +1,5 @@
-# Features
-BACKLIGHT_ENABLE = no       # Enable Keyboard Backlight Functionality
-RGBLIGHT_ENABLE = no        # Enable Keyboard RGB Underglow
-AUDIO_ENABLE = no           # Audio Output
-ENCODER_MAP_ENABLE = yes    # Encoder Mapping Support
-# OLED
-OLED_ENABLE = yes           # Enable OLED
-OLED_DRIVER = SSD1306       # Oled Driver Selection
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+AUDIO_ENABLE = no
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
