9c9ec672864ae1384ed5f0f32dcb653e9adffaea
Keith Wade <keawade@protonmail.com>
2023-02-11 17:31:09 -0500

    Update keaboard config

--- keyboards/keaboard/keymaps/default/keymap.c
+++ keyboards/keaboard/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Keith Wade <keawade@protonmail.com>
+Copyright 2023 Keith Wade <https://github.com/keawade>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- keyboards/keaboard/rev1/config.h
+++ keyboards/keaboard/rev1/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Keith Wade <keawade@protonmail.com>
+Copyright 2023 Keith Wade <https://github.com/keawade>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,36 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x9090
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    You
-#define PRODUCT         Keaboard Rev 1
-#define DESCRIPTION     Split 40 percent ergonomic hotswap keyboard
-
 /* key matrix size */
 // Rows are doubled-up
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 7
-
-// wiring of each half
-#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
 /* serial.c configuration for split keyboard */
 #define SOFT_SERIAL_PIN D0
