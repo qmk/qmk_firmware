3a271aebfcafb1846456b59f7e72da0956fd6285
takashicompany <t@kashi.company>
2023-02-11 15:12:29 +0900

    Update keyboards/takashicompany/worm_eaten/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/takashicompany/worm_eaten/config.h
+++ keyboards/takashicompany/worm_eaten/config.h
@@ -7,7 +7,7 @@
 
 #define RGB_DI_PIN D3
 #ifdef RGB_DI_PIN
-#    define RGBLED_NUM 3 * 2
+#    define RGBLED_NUM 6
 #    define RGBLED_SPLIT { 3, 3 }
 #    define RGBLIGHT_HUE_STEP 8
 #    define RGBLIGHT_SAT_STEP 8
