fed1257bd2b596bda755a621a4ec45d90754c9b1
chickenman112 <76463635+chickenman112@users.noreply.github.com>
2023-02-11 21:25:48 -0800

    Update ciel65.h

--- keyboards/chickenman/ciel65/ciel65.h
+++ keyboards/chickenman/ciel65/ciel65.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Koichi Katano
+/* Copyright 2023 ChickenMan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
