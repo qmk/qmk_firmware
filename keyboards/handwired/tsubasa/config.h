df4a1a4e98d3b1aa449a8e93da72e2070200018d
Kuribayashi Atsushi <kuribayashi.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2023-02-12 15:57:10 +0900

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/tsubasa/config.h
+++ keyboards/handwired/tsubasa/config.h
@@ -17,12 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
-/* key matrix size */
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 6
-
 // #define EE_HANDS
 #define MASTER_RIGHT
 /*
