78b66167fb50750409cd5160f1acfd7cf6de2925
Kuribayashi Atsushi <kuribayashi.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2023-02-12 15:58:29 +0900

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/tsubasa/rules.mk
+++ keyboards/handwired/tsubasa/rules.mk
@@ -6,7 +6,6 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
