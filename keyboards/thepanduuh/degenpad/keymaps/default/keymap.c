b0d179aaef60bbeb20c62faa1d823c8a7831fcce
ThePanduuh <feojoe2@gmail.com>
2023-02-12 09:06:26 -0500

    Added requested changes per drashna's suggestions.

--- keyboards/thepanduuh/degenpad/keymaps/default/keymap.c
+++ keyboards/thepanduuh/degenpad/keymaps/default/keymap.c
@@ -25,24 +25,24 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_BASE] = LAYOUT_numpad_6x4(
+  [_BASE] = LAYOUT_ortho_6x4(
   KC_ESC , KC_EQL , KC_BSPC, MO(1)  ,
   KC_NUM , KC_PSLS, KC_PAST, KC_PMNS,
-  KC_P7  , KC_P8  , KC_P9  ,
+  KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
   KC_P4  , KC_P5  , KC_P6  , KC_PPLS,
-  KC_P1  , KC_P2  , KC_P3  ,
-           KC_P0  , KC_PDOT, KC_PENT
+  KC_P1  , KC_P2  , KC_P3  , KC_PENT,
+  KC_P0  , KC_P0  , KC_PDOT, KC_PENT
   ),
 
-  [_FN] = LAYOUT_numpad_6x4(
+  [_FN] = LAYOUT_ortho_6x4(
   KC_TRNS, KC_TRNS, KC_DEL , KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_HOME, KC_UP  , KC_PGUP,
+  KC_HOME, KC_UP  , KC_PGUP, KC_TRNS,
   KC_LEFT, KC_TRNS, KC_RGHT, KC_TRNS,
-  KC_END , KC_DOWN, KC_PGDN,
-           KC_INS , KC_DEL , QK_BOOT
+  KC_END , KC_DOWN, KC_PGDN, QK_BOOT,
+  KC_INS , KC_INS , KC_DEL , QK_BOOT
   )
-  
+
 };
 
 #if defined(ENCODER_MAP_ENABLE)
--- keyboards/thepanduuh/degenpad/keymaps/via/keymap.c
+++ keyboards/thepanduuh/degenpad/keymaps/via/keymap.c
@@ -18,38 +18,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*Base*/
-  [0] = LAYOUT_numpad_6x4(
+  [0] = LAYOUT_ortho_6x4(
   KC_ESC , KC_EQL , KC_BSPC, MO(1)  ,
   KC_NUM , KC_PSLS, KC_PAST, KC_PMNS,
-  KC_P7  , KC_P8  , KC_P9  ,
+  KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
   KC_P4  , KC_P5  , KC_P6  , KC_PPLS,
-  KC_P1  , KC_P2  , KC_P3  ,
-           KC_P0  , KC_PDOT, KC_PENT
+  KC_P1  , KC_P2  , KC_P3  , KC_PENT,
+  KC_P0  , KC_P0  , KC_PDOT, KC_PENT
   ),
 
-  [1] = LAYOUT_numpad_6x4(
+  [1] = LAYOUT_ortho_6x4(
   KC_TRNS, KC_TRNS, KC_DEL , KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_HOME, KC_UP  , KC_PGUP,
+  KC_HOME, KC_UP  , KC_PGUP, KC_TRNS,
   KC_LEFT, KC_TRNS, KC_RGHT, KC_TRNS,
-  KC_END , KC_DOWN, KC_PGDN,
-           KC_INS , KC_DEL , QK_BOOT
+  KC_END , KC_DOWN, KC_PGDN, QK_BOOT,
+  KC_INS , KC_INS , KC_DEL , QK_BOOT
   ),
-  [2] = LAYOUT_numpad_6x4(
+  [2] = LAYOUT_ortho_6x4(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
-           KC_TRNS, KC_TRNS, KC_TRNS
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
-  [3] = LAYOUT_numpad_6x4(
+  [3] = LAYOUT_ortho_6x4(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,
-           KC_TRNS, KC_TRNS, KC_TRNS
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 
