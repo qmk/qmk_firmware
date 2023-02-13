66686cffb1a4170fffd950cbd8acbd18d0ec8e61
raylax <raylax@inurl.org>
2023-02-13 06:31:24 +0000

    update: license header

--- keyboards/rxkb/hhkb/hhkb.c
+++ keyboards/rxkb/hhkb/hhkb.c
@@ -1,1 +1,4 @@
+// Copyright 2022 raylax (@raylax)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "hhkb.h"
--- keyboards/rxkb/hhkb/hhkb_avr.h
+++ keyboards/rxkb/hhkb/hhkb_avr.h
@@ -1,3 +1,6 @@
+// Copyright 2022 raylax (@raylax)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 #include <stdint.h>
