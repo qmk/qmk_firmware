7ad71e85b515d6cb8162896fb8dbdc9c72f7f485
Xelus22 <preyas22@gmail.com>
2023-02-13 21:31:00 +1100

    refactor rgb led to use a function and add enable and override

--- keyboards/xelus/valor/rev2/rev2.c
+++ keyboards/xelus/valor/rev2/rev2.c
@@ -18,90 +18,61 @@
 
 #ifdef RGB_MATRIX_ENABLE
 
-led_config_t g_led_config = { {
-  // Key Matrix to LED Index
-  { 24,     NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { 25,     NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { 26,     27,     NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED }
-}, {
-  // LED Index to Physical Position
-    { 224,  0 },
-    { 224,  9 },
-    { 224, 21 },
-    { 224, 33 },
-    { 224, 45 },
-    { 223, 51 },
-    { 206, 52 },
-    { 190, 52 },
-    { 172, 52 },
-    { 154, 57 },
-    { 136, 61 },
-    { 118, 63 },
-    { 101, 64 },
-    { 80 , 60 },
-    { 64 , 57 },
-    { 46 , 53 },
-    { 28 , 53 },
-    { 10 , 52 },
-    { 3  , 51 },
-    { 2  , 45 },
-    { 2  , 33 },
-    { 2  , 20 },
-    { 2  ,  9 },
-    { 0  ,  0 },
-    { 9  ,  3 },
-    { 6  , 14 },
-    { 4  , 26 },
-    { 21 , 27 }
+led_config_t g_led_config = {{// Key Matrix to LED Index
+                              {24, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED},
+                              {25, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED},
+                              {26, 27, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED},
+                              {NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED},
+                              {NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED}},
+                             {// LED Index to Physical Position
+                              {224, 0}, {224, 9}, {224, 21}, {224, 33}, {224, 45}, {223, 51}, {206, 52}, {190, 52}, {172, 52}, {154, 57}, {136, 61}, {118, 63}, {101, 64}, {80, 60}, {64, 57},
+                              {46, 53}, {28, 53}, {10, 52},  {3, 51},   {2, 45},   {2, 33},   {2, 20},   {2, 9},    {0, 0},    {9, 3},    {6, 14},   {4, 26},   {21, 27}
 
-}, {
-  // LED Index to Flag
-  2, 2, 2, 2, 2, 2, 2, 2,
-  2, 2, 2, 2, 2, 2, 2, 2,
-  2, 2, 2, 2, 2, 2, 2, 2,
-  8, 8, 8, 8
-} };
-
+                             },
+                             {// LED Index to Flag
+                              2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 8, 8, 8, 8}};
 
 void keyboard_pre_init_kb(void) {
-    rgb_matrix_set_flags(LED_FLAG_MODIFIER|LED_FLAG_UNDERGLOW|LED_FLAG_KEYLIGHT);
+    rgb_matrix_set_flags(LED_FLAG_MODIFIER | LED_FLAG_UNDERGLOW | LED_FLAG_KEYLIGHT);
     keyboard_pre_init_user();
 }
 
-#ifdef VIA_ENABLE
-#include "quantum.h"
-#include "eeprom.h"
+#    ifdef VIA_ENABLE
+#        include <quantum.h>
+#        include <eeprom.h>
 
 indicator_settings_config g_config = {
     .caps_lock_indicator = {0, 0, 128},
-    .num_lock_indicator = {0, 0, 128},
-    .scroll_lock_indicator = {0, 0, 128},
-    .layer_indicator = {0, 0, 128},
+    .num_lock_indicator = {60, 0, 128},
+    .scroll_lock_indicator = {120, 0, 128},
+    .layer_indicator = {180, 0, 128},
     .caps_lock_key = 24,
     .num_lock_key = 25,
     .scroll_lock_key = 26,
-    .layer_indicator_key = 27 + 1
-};
+    .layer_indicator_key = 27,
+    .enable_caps_lock = true,
+    .enable_num_lock = false,
+    .enable_scroll_lock = true,
+    .enable_layer_indicator = false,
+    .caps_override_bl = true,
+    .num_override_bl = true,
+    .scroll_override_bl = true,
+    .layer_override_bl = true};
 
-void values_load(void)
-{
-    eeprom_read_block( &g_config, ((void*)VIA_EEPROM_CUSTOM_CONFIG_ADDR), sizeof(g_config) );
+void values_load(void) {
+    eeprom_read_block(&g_config, ((void *)VIA_EEPROM_CUSTOM_CONFIG_ADDR), sizeof(g_config));
 }
 
-void values_save(void)
-{
-    eeprom_update_block( &g_config, ((void*)VIA_EEPROM_CUSTOM_CONFIG_ADDR), sizeof(g_config) );
+void values_save(void) {
+    eeprom_update_block(&g_config, ((void *)VIA_EEPROM_CUSTOM_CONFIG_ADDR), sizeof(g_config));
 }
 
-void via_init_kb(void)
-{
+void via_init_kb(void) {
     // If the EEPROM has the magic, the data is good.
     // OK to load from EEPROM
     if (via_eeprom_is_valid()) {
         values_load();
-    } else	{
+    } else {
         values_save();
         // DO NOT set EEPROM valid here, let caller do this
     }
@@ -113,26 +84,21 @@ void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
     uint8_t *channel_id        = &(data[1]);
     uint8_t *value_id_and_data = &(data[2]);
 
-    if ( *channel_id == id_custom_channel ) {
-        switch ( *command_id )
-        {
-            case id_custom_set_value:
-            {
+    if (*channel_id == id_custom_channel) {
+        switch (*command_id) {
+            case id_custom_set_value: {
                 indicator_config_set_value(value_id_and_data);
                 break;
             }
-            case id_custom_get_value:
-            {
+            case id_custom_get_value: {
                 indicator_config_get_value(value_id_and_data);
                 break;
             }
-            case id_custom_save:
-            {
+            case id_custom_save: {
                 values_save();
                 break;
             }
-            default:
-            {
+            default: {
                 // Unhandled message.
                 *command_id = id_unhandled;
                 break;
@@ -147,14 +113,24 @@ void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
     // DO NOT call raw_hid_send(data,length) here, let caller do this
 }
 
-void indicator_config_set_value( uint8_t *data )
-{
+void indicator_config_set_value(uint8_t *data) {
     // data = [ value_id, value_data ]
     uint8_t *value_id   = &(data[0]);
     uint8_t *value_data = &(data[1]);
 
-    switch ( *value_id )
-    {
+    switch (*value_id) {
+        case id_caps_lock_enable:
+            g_config.enable_caps_lock = *value_data;
+            break;
+        case id_num_lock_enable:
+            g_config.enable_num_lock = *value_data;
+            break;
+        case id_scroll_lock_enable:
+            g_config.enable_scroll_lock = *value_data;
+            break;
+        case id_layer_indicator_enable:
+            g_config.enable_layer_indicator = *value_data;
+            break;
         case id_caps_lock_brightness:
             g_config.caps_lock_indicator.v = *value_data;
             break;
@@ -168,16 +144,16 @@ void indicator_config_set_value( uint8_t *data )
             g_config.layer_indicator.v = *value_data;
             break;
         case id_caps_lock_color:
-            _set_color( &(g_config.caps_lock_indicator), value_data );
+            _set_color(&(g_config.caps_lock_indicator), value_data);
             break;
         case id_num_lock_color:
-            _set_color( &(g_config.num_lock_indicator), value_data );
+            _set_color(&(g_config.num_lock_indicator), value_data);
             break;
         case id_scroll_lock_color:
-            _set_color( &(g_config.scroll_lock_indicator), value_data );
+            _set_color(&(g_config.scroll_lock_indicator), value_data);
             break;
         case id_layer_indicator_color:
-            _set_color( &(g_config.layer_indicator), value_data );
+            _set_color(&(g_config.layer_indicator), value_data);
             break;
         case id_caps_lock_key:
             g_config.caps_lock_key = *value_data;
@@ -191,16 +167,38 @@ void indicator_config_set_value( uint8_t *data )
         case id_layer_indicator_key:
             g_config.layer_indicator_key = *value_data;
             break;
+        case id_caps_lock_override:
+            g_config.caps_override_bl = *value_data;
+            break;
+        case id_num_lock_override:
+            g_config.num_override_bl = *value_data;
+            break;
+        case id_scroll_lock_override:
+            g_config.scroll_override_bl = *value_data;
+            break;
+        case id_layer_indicator_override:
+            g_config.layer_override_bl = *value_data;
+            break;
     }
 }
 
-void indicator_config_get_value( uint8_t *data )
-{
+void indicator_config_get_value(uint8_t *data) {
     uint8_t *value_id   = &(data[0]);
     uint8_t *value_data = &(data[1]);
 
-    switch ( *value_id )
-    {
+    switch (*value_id) {
+        case id_caps_lock_enable:
+            *value_data = g_config.enable_caps_lock;
+            break;
+        case id_num_lock_enable:
+            *value_data = g_config.enable_num_lock;
+            break;
+        case id_scroll_lock_enable:
+            *value_data = g_config.enable_scroll_lock;
+            break;
+        case id_layer_indicator_enable:
+            *value_data = g_config.enable_layer_indicator;
+            break;
         case id_caps_lock_brightness:
             *value_data = g_config.caps_lock_indicator.v;
             break;
@@ -214,16 +212,16 @@ void indicator_config_get_value( uint8_t *data )
             *value_data = g_config.layer_indicator.v;
             break;
         case id_caps_lock_color:
-            _get_color( &(g_config.caps_lock_indicator), value_data );
+            _get_color(&(g_config.caps_lock_indicator), value_data);
             break;
         case id_num_lock_color:
-            _get_color( &(g_config.num_lock_indicator), value_data );
+            _get_color(&(g_config.num_lock_indicator), value_data);
             break;
         case id_scroll_lock_color:
-            _get_color( &(g_config.scroll_lock_indicator), value_data );
+            _get_color(&(g_config.scroll_lock_indicator), value_data);
             break;
         case id_layer_indicator_color:
-            _get_color( &(g_config.layer_indicator), value_data );
+            _get_color(&(g_config.layer_indicator), value_data);
             break;
         case id_caps_lock_key:
             *value_data = g_config.caps_lock_key;
@@ -237,72 +235,73 @@ void indicator_config_get_value( uint8_t *data )
         case id_layer_indicator_key:
             *value_data = g_config.layer_indicator_key;
             break;
+        case id_caps_lock_override:
+            *value_data = g_config.caps_override_bl;
+            break;
+        case id_num_lock_override:
+            *value_data = g_config.num_override_bl;
+            break;
+        case id_scroll_lock_override:
+            *value_data = g_config.scroll_override_bl;
+            break;
+        case id_layer_indicator_override:
+            *value_data = g_config.layer_override_bl;
+            break;
     }
 }
 
 // Some helpers for setting/getting HSV
-void _set_color( HSV *color, uint8_t *data )
-{
+void _set_color(HSV *color, uint8_t *data) {
     color->h = data[0];
     color->s = data[1];
 }
 
-void _get_color( HSV *color, uint8_t *data )
-{
+void _get_color(HSV *color, uint8_t *data) {
     data[0] = color->h;
     data[1] = color->s;
 }
 
+void rgb_led_indicator_helper(bool enable_indicator, HSV color, bool host_indicator, bool override, uint8_t key, uint8_t led_min, uint8_t led_max) {
+    if (enable_indicator) {
+        RGB rgb_led = hsv_to_rgb((HSV){.h = color.h, .s = color.s, .v = color.v});
+        if (host_indicator) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(key, rgb_led.r, rgb_led.g, rgb_led.b);
+        } else {
+            if (override) {
+                RGB_MATRIX_INDICATOR_SET_COLOR(key, 0, 0, 0);
+            }
+        }
+    }
+}
+
 // Set the indicators with RGB Matrix subsystem
 bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
-    // caps lock cyan
-    RGB rgb_caps = hsv_to_rgb( (HSV){ .h = g_config.caps_lock_indicator.h,
-                                      .s = g_config.caps_lock_indicator.s,
-                                      .v = g_config.caps_lock_indicator.v } );
-    if (host_keyboard_led_state().caps_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(g_config.caps_lock_key, rgb_caps.r, rgb_caps.g, rgb_caps.b);
-    } else {
-        RGB_MATRIX_INDICATOR_SET_COLOR(g_config.caps_lock_key, 0, 0, 0);
-    }
+    rgb_led_indicator_helper(g_config.enable_caps_lock, g_config.caps_lock_indicator, host_keyboard_led_state().caps_lock, g_config.caps_override_bl, g_config.caps_lock_key, led_min, led_max);
 
-    // num lock cyan
-    RGB rgb_num = hsv_to_rgb( (HSV){ .h = g_config.num_lock_indicator.h,
-                                     .s = g_config.num_lock_indicator.s,
-                                     .v = g_config.num_lock_indicator.v } );
-    if (host_keyboard_led_state().num_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(g_config.num_lock_key, rgb_num.r, rgb_num.g, rgb_num.b);
-    } else {
-        RGB_MATRIX_INDICATOR_SET_COLOR(g_config.num_lock_key, 0, 0, 0);
-    }
+    rgb_led_indicator_helper(g_config.enable_num_lock, g_config.num_lock_indicator, host_keyboard_led_state().num_lock, g_config.num_override_bl, g_config.num_lock_key, led_min, led_max);
 
-    // scroll lock cyan
-    RGB rgb_scroll = hsv_to_rgb( (HSV){ .h = g_config.scroll_lock_indicator.h,
-                                        .s = g_config.scroll_lock_indicator.s,
-                                        .v = g_config.scroll_lock_indicator.v } );
-    if (host_keyboard_led_state().scroll_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(g_config.scroll_lock_key, rgb_scroll.r, rgb_scroll.g, rgb_scroll.b);
-    } else {
-        RGB_MATRIX_INDICATOR_SET_COLOR(g_config.scroll_lock_key, 0, 0, 0);
-    }
+    rgb_led_indicator_helper(g_config.enable_scroll_lock, g_config.scroll_lock_indicator, host_keyboard_led_state().scroll_lock, g_config.scroll_override_bl, g_config.scroll_lock_key, led_min, led_max);
 
     // layer state
-    RGB rgb_layer = hsv_to_rgb( (HSV){ .h = g_config.layer_indicator.h,
-                                       .s = g_config.layer_indicator.s,
-                                       .v = g_config.layer_indicator.v } );
-    switch (get_highest_layer(layer_state)) {
-        case 0:
-            RGB_MATRIX_INDICATOR_SET_COLOR(g_config.layer_indicator_key - 1, 0, 0, 0);
-            break;
-        case 1:
-            RGB_MATRIX_INDICATOR_SET_COLOR(g_config.layer_indicator_key - 1, rgb_layer.r, rgb_layer.g, rgb_layer.b);
-            break;
-        default:
-            // white
-            RGB_MATRIX_INDICATOR_SET_COLOR(24, 128, 128, 128);
-            break;
+    if (g_config.enable_layer_indicator) {
+        RGB rgb_layer = hsv_to_rgb((HSV){.h = g_config.layer_indicator.h, .s = g_config.layer_indicator.s, .v = g_config.layer_indicator.v});
+        switch (get_highest_layer(layer_state)) {
+            case 0:
+                if (g_config.layer_override_bl) {
+                    RGB_MATRIX_INDICATOR_SET_COLOR(g_config.layer_indicator_key, 0, 0, 0);
+                }
+                break;
+            case 1:
+                RGB_MATRIX_INDICATOR_SET_COLOR(g_config.layer_indicator_key, rgb_layer.r, rgb_layer.g, rgb_layer.b);
+                break;
+            default:
+                // white
+                RGB_MATRIX_INDICATOR_SET_COLOR(g_config.layer_indicator_key, 128, 128, 128);
+                break;
+        }
     }
     return false;
 }
 
-#endif
+#    endif
 #endif
--- keyboards/xelus/valor/rev2/rev2.h
+++ keyboards/xelus/valor/rev2/rev2.h
@@ -21,22 +21,38 @@
 #ifdef VIA_ENABLE
 // custom ID codes
 enum via_indicator_value {
-    id_caps_lock_brightness   = 1,
+    id_caps_lock_enable = 1,
+    id_caps_lock_brightness,
     id_caps_lock_color,
     id_caps_lock_key,
+    id_caps_lock_override,
+    id_num_lock_enable,
     id_num_lock_brightness,
     id_num_lock_color,
     id_num_lock_key,
+    id_num_lock_override,
+    id_scroll_lock_enable,
     id_scroll_lock_brightness,
     id_scroll_lock_color,
     id_scroll_lock_key,
+    id_scroll_lock_override,
+    id_layer_indicator_enable,
     id_layer_indicator_brightness,
     id_layer_indicator_color,
-    id_layer_indicator_key
+    id_layer_indicator_key,
+    id_layer_indicator_override
 };
 
 // struct to save things
 typedef struct {
+    bool enable_caps_lock:1;            // |
+    bool enable_num_lock:1;             // |
+    bool enable_scroll_lock:1;          // |
+    bool enable_layer_indicator:1;      // |
+    bool caps_override_bl:1;            // |
+    bool num_override_bl:1;             // |
+    bool scroll_override_bl:1;          // |
+    bool layer_override_bl:1;           // 1 byte
     HSV caps_lock_indicator;    // 3 bytes
     HSV num_lock_indicator;     // 3 bytes
     HSV scroll_lock_indicator;  // 3 bytes
@@ -46,7 +62,7 @@ typedef struct {
     uint8_t scroll_lock_key;    // 1 byte
     uint8_t layer_indicator_key;// 1 byte
 } indicator_settings_config;
-// total 16 bytes
+// total 17 bytes
 
 // function declaration
 void indicator_config_set_value( uint8_t *data );
