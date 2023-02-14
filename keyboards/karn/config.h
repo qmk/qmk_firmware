88d9db02bb167c3bf029b157f678ea1f29cfd8c2
Robert Carter Mills <robcmills@gmail.com>
2023-02-14 12:41:18 -0800

    Various changes:
    - Move keymap specific defines out of root config.h
    - Move tap dance definitions out of default keymap and root rules.mk

--- keyboards/karn/config.h
+++ keyboards/karn/config.h
@@ -24,11 +24,3 @@
 #define BOOTMAGIC_LITE_ROW_RIGHT    4
 #define BOOTMAGIC_LITE_COLUMN_RIGHT 4
 
-// Equivalent to zmk behavior-hold-tap tap-preferred flavor
-// Do not force the mod-tap key press to be handled as a modifier
-// if any other key was pressed while the mod-tap key is held down.
-#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
-
-// Equivalent to zmk behavior-hold-tap hold-preferred flavor
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-
--- keyboards/karn/keymaps/default/keymap.c
+++ keyboards/karn/keymaps/default/keymap.c
@@ -3,61 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-// Tap Dance declarations
-enum {
-    TD_F1_F11,
-    TD_F2_F12,
-};
-
-// Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for F1, twice for F11
-    [TD_F1_F11] = ACTION_TAP_DANCE_DOUBLE(KC_F1, KC_F11),
-    [TD_F2_F12] = ACTION_TAP_DANCE_DOUBLE(KC_F2, KC_F12),
-};
-
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case SFT_T(KC_Z): // Special case for Shift
-            return true; // Immediately select the hold action when another key is pressed.
-        case SFT_T(KC_ESC):
-            return true;
-        default:
-            return false; // Do not select the hold action when another key is pressed.
-    }
-}
-
-bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case CTL_T(KC_X):
-            return true;
-        case ALT_T(KC_C):
-            return true;
-        case CMD_T(KC_V):
-            return true;
-        case CMD_T(KC_M):
-            return true;
-        case ALT_T(KC_COMM):
-            return true;
-        case CTL_T(KC_DOT):
-            return true;
-        case KC_X:
-            return true;
-        case KC_C:
-            return true;
-        case KC_V:
-            return true;
-        case KC_M:
-            return true;
-        case KC_COMM:
-            return true;
-        case KC_DOT:
-            return true;
-        default:
-            return false;
-    }
-}
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
@@ -87,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // numbers
     [3] = LAYOUT_split_3x6_3(
-        _______, TD(TD_F1_F11), TD(TD_F2_F12), KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        _______, KC_1,          KC_2,          KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, _______,       _______,       _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
-                                                        _______, _______, _______,  _______, _______, _______
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,  _______, _______, _______
     )
 };
 
