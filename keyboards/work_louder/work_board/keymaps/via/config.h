38808748484ca732a4430bf370ed9bca5e491c02
jack <0x6A73@pm.me>
2023-02-14 14:02:57 -0700

    disable oneshot keys

--- keyboards/work_louder/work_board/keymaps/via/config.h
+++ keyboards/work_louder/work_board/keymaps/via/config.h
@@ -1,5 +1,4 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
-#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
-#undef ENABLE_RGB_MATRIX_PIXEL_RAIN
+#define NO_ACTION_ONESHOT
