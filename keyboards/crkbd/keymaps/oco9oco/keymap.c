658c0d6a584d88a565b922a8897fc1f4ab3e91a6
Sungsoo Ryu <40929552+oco9oco@users.noreply.github.com>
2023-02-15 12:59:09 +0900

    CRKBD KEYMAP CHANGE

--- keyboards/crkbd/keymaps/oco9oco/keymap.c
+++ keyboards/crkbd/keymaps/oco9oco/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  __________________QWERTY_L1________________,                            __________________QWERTY_R1________________, KC_EQL,
     KC_CAPS, __________________QWERTY_SACS_L2___________,                            __________________QWERTY_HOME_R2___________, KC_QUOT,
     KC_LSFT, __________________QWERTY_GUIZ_L3___________,                            __________________QWERTY_R3________________, KC_BSLS,
-                                        THUMB_L1, THUMB_L2, KC_DEL,       THUMB_R3, THUMB_R2, THUMB_R1
+                                        THUMB_L1, THUMB_L2, LT(_NUM,KC_DEL),       THUMB_R3, THUMB_R2, THUMB_R1
     ),
   [_BASE_NOMOD] = LAYOUT_crkbd_wrapper(
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
@@ -42,30 +42,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
   [_NUM] = LAYOUT_crkbd_wrapper(
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-    _______, KC_LGUI, KC_LALT, _NUM_MIN,KC_LSFT, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_COLN, KC_DQUO,
-    _______, KC_SPC,  BULLET_U,KC_COMM, BULLET_D,KC_DOT,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,
-                                        _______, _______, _______,          A(KC_1), LT(_NAV,KC_0),KC_SPC
+    _______, KC_P,    KC_O,    KC_I,    KC_U,    KC_Y,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EQL,
+    _______, XXXXXXX, KC_L,    KC_K,    KC_J,    KC_H,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_COLN, KC_DQUO,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_M,    KC_N,                               KC_B,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,
+                                        _______, _______, _______,          _______, _______, _______
     ),
   [_NAV] = LAYOUT_split_3x6_3(          //Navigations
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-    KC_ESC,  LCA(KC_H),LCA(KC_J),LCA(KC_K),LCA(KC_L),KC_F5,                          XXXXXXX, A(KC_4), A(KC_6), A(KC_7), A(KC_8), XXXXXXX,
-    KC_F2,   KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_F6,                          KC_INS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-    C(KC_F1),NAV(Z),  C(KC_X),  C(KC_C),  C(KC_V),   KC_F7,                          KC_APP,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
+    KC_ESC,  LCA(KC_H),LCA(KC_J),LCA(KC_K),LCA(KC_L),KC_F6,                          XXXXXXX, A(KC_4), A(KC_6), A(KC_7), A(KC_8), XXXXXXX,
+    KC_F2,   KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_F7,                          KC_INS,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
+    C(KC_F1),NAV(Z),  C(KC_X),  C(KC_C),  C(KC_V),   C(KC_B),                        KC_APP,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
                                            _______, KC_BSPC, KC_DEL,        _______, _______, _______
     ),
   [_FUNC] = LAYOUT_crkbd_wrapper(       //Functions
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
     XXXXXXX, C(KC_F1),KC_F2,   KC_F3,   A(KC_F4),KC_F5,                              _____________FUNC_2789____________, XXXXXXX, KC_BSPC,
-    XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_F6,                              _____________FUNC_1456____________, XXXXXXX, KC_DEL,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            _____________FUNC_0123____________, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_LSFT, KC_LALT, KC_LCTL, KC_LSFT, KC_F6,                              _____________FUNC_1456____________, XXXXXXX, KC_DEL,
+    XXXXXXX, KC_LGUI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            _____________FUNC_0123____________, XXXXXXX, XXXXXXX,
                                         _______, _______, _______,          _______, _______, _______
     ),
   [_MOUSE] = LAYOUT_split_3x6_3(       //Mouse keys
 // |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-    KC_U,    C(KC_F1), KC_WH_D, KC_MS_U, KC_WH_U, XXXXXXX,                              XXXXXXX,KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-    KC_P,    KC_H,     KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,                              KC_INS, KC_ACL0, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX,
-    KC_X,    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                KC_APP, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX,
+    KC_ESC,  C(KC_F1), KC_F2,   KC_F3,  A(KC_F4), XXXXXXX,                           XXXXXXX, KC_ACL0, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
+    XXXXXXX, KC_LSFT,  KC_WH_D, KC_MS_U, KC_WH_U, XXXXXXX,                           KC_ACL0, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, QK_MAKE,
+    XXXXXXX, XXXXXXX,  KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,                           XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
                                         _______, _______, _______,          _______, _______, _______
     ),
   [_ONEHAND] = LAYOUT_split_3x6_3( //Trackball Manipulation
@@ -79,37 +79,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //clang-format on
 
 // COMBOS
-enum combos {
-    COMBO_ONEHAND,
-    COMBO_NOMOD,
-    COMBO_HOME,
-    COMBO_OS,
-    COMBO_ONEHAND_HOME,
-    //ZB_STENO,
-    COMBO_LENGTH
-};
-uint16_t COMBO_LEN = COMBO_LENGTH;
-const uint16_t PROGMEM qw_combo[] = {KC_W,    KC_E,    COMBO_END};
-const uint16_t PROGMEM pe_combo[] = {KC_P,    KC_EQL,  COMBO_END};
-const uint16_t PROGMEM sq_combo[] = {KC_SCLN, KC_QUOT, COMBO_END};
-const uint16_t PROGMEM os_combo[] = {KC_BSLS, KC_SLSH, COMBO_END};
-const uint16_t PROGMEM oh_combo[] = {KC_BTN2, KC_UP,   COMBO_END};
+// enum combos {
+//     COMBO_ONEHAND,
+//     COMBO_NOMOD,
+//     COMBO_HOME,
+//     COMBO_OS,
+//     COMBO_ONEHAND_HOME,
+//     //ZB_STENO,
+//     COMBO_LENGTH
+// };
+// uint16_t COMBO_LEN = COMBO_LENGTH;
+// const uint16_t PROGMEM qw_combo[] = {KC_W,    KC_E,    COMBO_END};
+// const uint16_t PROGMEM pe_combo[] = {KC_P,    KC_EQL,  COMBO_END};
+// const uint16_t PROGMEM sq_combo[] = {KC_SCLN, KC_QUOT, COMBO_END};
+// const uint16_t PROGMEM os_combo[] = {KC_BSLS, KC_SLSH, COMBO_END};
+// const uint16_t PROGMEM oh_combo[] = {KC_BTN2, KC_UP,   COMBO_END};
 
-combo_t key_combos[] = {
-    [COMBO_ONEHAND]               = COMBO(qw_combo, TO(_ONEHAND)),
-    [COMBO_NOMOD]	              = COMBO(pe_combo, TO(_BASE_NOMOD)),
-    [COMBO_HOME]                  = COMBO(sq_combo, TO(_BASE)),
-    [COMBO_OS]                    = COMBO(os_combo, CG_TOGG),
-    [COMBO_ONEHAND_HOME]          = COMBO(oh_combo, TO(_BASE)),
-};
+// combo_t key_combos[] = {
+//     [COMBO_ONEHAND]               = COMBO(qw_combo, TO(_ONEHAND)),
+//     [COMBO_NOMOD]	              = COMBO(pe_combo, TO(_BASE_NOMOD)),
+//     [COMBO_HOME]                  = COMBO(sq_combo, TO(_BASE)),
+//     [COMBO_OS]                    = COMBO(os_combo, CG_TOGG),
+//     [COMBO_ONEHAND_HOME]          = COMBO(oh_combo, TO(_BASE)),
+// };
 
-// Key overrides
-
-const key_override_t DEL_override = ko_make_basic(MOD_MASK_SHIFT, KC_DEL, KC_BSPC);   // Shift + del = bspc
-//   // shift + caps lock = toggle control - gui
-
-const key_override_t **key_overrides = (const key_override_t *[]){                              // This globally defines all key overrides to be used
-    &DEL_override, //&OS_override,
-    NULL                                                                                        // Null terminate the array of overrides!
-};
 
