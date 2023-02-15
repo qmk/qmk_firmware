ca2fb5ce10f1aacb906171f210781af8efc1bd2d
pauperboards <pauperboards@gmail.com>
2023-02-15 11:24:52 -0500

    Fix PR comments.

--- keyboards/pauperboards/brick/keymaps/default/keymap.c
+++ keyboards/pauperboards/brick/keymaps/default/keymap.c
@@ -1,3 +1,19 @@
+/* Copyright 2023 Jason Chestnut <pauperboards@gmail.com> @pauperboards
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -26,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |      | Vol- | Vol+ | Mute |      |      |      |  F11 |  F12 |      |      |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |      |      |      |  RST |
+     * |      |      |      |      |      |      |      |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      *        |      |      |      |             |             |      |      |      |
      *        `---------------------------------------------------------------------'
@@ -34,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_ortho_2x2u (
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, KC_F11,  KC_F12,  _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                  _______, _______, _______,          _______, _______,          _______, _______, _______
     ),
 
@@ -44,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |      |r_TOG |r_Mode|r_Hue+|r_Hue-|      |      |   -  |   =  |   [  |   ]  |  \   |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |BL_TOG|BL_STEP|     |      |      |      |      |      |      |      |      |
+     * |      |BL_TOG|BL_STEP|     |      |      |      |      |      |      |      | BOOT |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      *        |      |      |      |             |             |      |      |      |
      *        `---------------------------------------------------------------------'
@@ -52,52 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_ortho_2x2u (
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,
                  _______, _______, _______,          _______, _______,          _______, _______, _______
     )
 };
-
-
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
-
-    } else {
-
-    }
-
-    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
-
-    } else {
-
-    }
-
-    if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
-
-    } else {
-
-    }
-
-    if (IS_LED_ON(usb_led, USB_LED_COMPOSE)) {
-
-    } else {
-
-    }
-
-    if (IS_LED_ON(usb_led, USB_LED_KANA)) {
-
-    } else {
-
-    }
-
-}
--- keyboards/pauperboards/brick/readme.md
+++ keyboards/pauperboards/brick/readme.md
@@ -1,7 +1,7 @@
 # Brick
 
-![Brick, top view](https://i.imgur.com/BLJ6Tum.jpg)
-![Brick, layout options](https://i.imgur.com/JhsWzY7.png)
+![Brick, top view](https://i.imgur.com/BLJ6Tumh.jpg)
+![Brick, layout options](https://i.imgur.com/JhsWzY7h.png)
 
 Brick is a 12u, 4 row ortholinear keyboard designed as a drop in replacement for PCBs like the Niu Mini. Brick features a USB-C port, an ATMega32u4, RGB underglow, three possible rotary encoder locations, and multiple ortho layout options.
 
@@ -13,11 +13,11 @@ Brick is a 12u, 4 row ortholinear keyboard designed as a drop in replacement for
 
 Make example for this keyboard (after setting up your build environment):
 
-    make pauper/brick:default
+    make pauperboards/brick:default
 
 Flashing example for this keyboard:
     
-    make pauper/brick:default:flash
+    make pauperboards/brick:default:flash
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
 
