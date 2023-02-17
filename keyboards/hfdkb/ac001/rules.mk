993f74d5da3ad6d7e23a78fae4452785534bf479
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2023-02-17 18:52:36 +0800

    Update keyboards/hfdkb/ac001/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/hfdkb/ac001/rules.mk
+++ keyboards/hfdkb/ac001/rules.mk
@@ -11,7 +11,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
-AUDIO_ENABLE = no           # Audio output
-ENCODER_ENABLE = no
 EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = spi_flash
