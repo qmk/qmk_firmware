1342a02672c120198707c4dcea5e9d1a4c783c25
Kasper Flye <kasperflye@gmail.com>
2023-02-17 08:36:16 +0100

    Added pgup/down + home/end

--- keyboards/keychron/v1/iso_encoder/keymaps/cflyeus/keymap.c
+++ keyboards/keychron/v1/iso_encoder/keymaps/cflyeus/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_iso_83(
          KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_F11,    KC_F12,   TOG_WS,             KC_MPLY,
          KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     XXXXXXX,  KC_6,     KC_7,     KC_8,      KC_9,     KC_0,      KC_MINS,  KC_BSPC,            KC_DEL,
-         KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     XXXXXXX,  KC_J,     KC_L,     KC_U,      KC_Y,     KC_SCLN,   KC_SLSH,                      KC_PGUP,
+         KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     XXXXXXX,  KC_J,     KC_L,     KC_U,      KC_Y,     KC_SCLN,   KC_SLSH,                      KC_HOME,
          ESC_CTL,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_AA,    KC_M,     KC_N,     KC_E,      KC_I,     KC_O,      KC_QUOT,  ENT_CTL,            KC_END,
          KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_AE,    KC_OE,    KC_K,     KC_H,      KC_COMM,  KC_DOT,    KC_RSFT,            KC_UP,    
          KC_LGUI,  KC_LALT,  MO(_LOWER),                             KC_SPC,                                  MO(_RAISE),KC_LALT,  KC_LGUI,  KC_LEFT,  KC_DOWN,  KC_RGHT
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSLS,  KC_PAST,  KC_PMNS,    _______,  _______,            _______,
          _______,  _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  KC_P7  ,  KC_P8,    KC_P9  ,  KC_PPLS,    _______,                      _______,
-         _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  KC_P4  ,  KC_P5,    KC_P6  ,  KC_DOT,     KC_COLN,  _______,            _______,
-         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_P1,    KC_P2  ,  KC_P3,      _______,            _______,
+         _______,  KC_WH_U,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGUP,  _______,  _______,  KC_P4  ,  KC_P5,    KC_P6  ,  KC_DOT,     KC_COLN,  _______,            _______,
+         _______,  KC_WH_D,  _______,  KC_HOME,  KC_END,   KC_PGDN,  _______,  _______,  _______,  KC_P1,    KC_P2  ,  KC_P3,      _______,            _______,
          _______,  _______,  _______,                            _______,                                    _______,  KC_P0,      _______,  _______,  _______,  _______
      ),
 
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      ),
      
     [_ADJUST] = LAYOUT_iso_83(
-         _______,  KC_MYCM,  KC_WHOM,  KC_CALC,  KC_MSEL,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_MSTP,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,  _______,            _______,
+         _______,  KC_MYCM,  KC_WHOM,  KC_CALC,  KC_MSEL,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_MSTP,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,  EE_CLR,             DB_TOGG,
          _______,  RGB_TOG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  QK_BOOT,            _______,
          _______,  _______,  _______,  KC_MS_U,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  _______,    _______,                      _______,
          _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN1,  VRSN,     _______,  KC_MPRV,  KC_MNXT,  KC_VOLU,  _______,    _______,  _______,            _______,
