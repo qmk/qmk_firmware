2a0aa8aea49cb66497f007a75972bca224722b18
Laneware <68452738+Laneware@users.noreply.github.com>
2023-02-19 06:46:11 +1000

    Update keyboards/lpad/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/lpad/config.h
+++ keyboards/lpad/config.h
@@ -3,23 +3,13 @@
 
 #pragma once
 
-#include "config_common.h"
-
 /* key matrix pins */
 #define MATRIX_ROW_PINS { E6, B7, D0}
 #define MATRIX_COL_PINS { D3, D4, D6 }
 
-/* key matrix size */
-#define MATRIX_ROWS 3
-#define MATRIX_COLS 3
-
 /*ENCODER*/
 #define ENCODERS_PAD_A { F0 }
 #define ENCODERS_PAD_B { F1 }
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
