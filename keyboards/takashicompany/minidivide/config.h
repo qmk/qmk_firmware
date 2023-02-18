19fe7d05b2e0c9bb3e315e4e6e57b9b9b1598fb9
takashicompany <t@kashi.company>
2023-02-19 02:32:17 +0900

    Update keyboards/takashicompany/minidivide/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/takashicompany/minidivide/config.h
+++ keyboards/takashicompany/minidivide/config.h
@@ -3,19 +3,10 @@
 
 #pragma once
 
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 5
-
 #define MATRIX_ROW_PINS { F4, F5, F6, F7 }
 #define MATRIX_COL_PINS { D4, C6, D7, E6, B4 }
 #define MATRIX_ROW_PINS_RIGHT { F4, F5, F6, F7 }
 #define MATRIX_COL_PINS_RIGHT { B4, E6, D7, C6, D4 }
-
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-#define SOFT_SERIAL_PIN D2
-
 #define RGB_DI_PIN D3
 #ifdef RGB_DI_PIN
 #    define RGBLED_NUM 10
