4e40abbcedd2fc9ecbf90760c3f959aac7e56fee
Elliot Powell <johnp8727@gmail.com>
2023-02-18 12:03:12 +0000

    remove debounce settings from config

--- keyboards/lily58/r2g/config.h
+++ keyboards/lily58/r2g/config.h
@@ -21,17 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 //#define SERIAL_USE_MULTI_TRANSACTION
 
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-//#define LOCKING_RESYNC_ENABLE
-
 
 #ifdef RGB_MATRIX_ENABLE
 #   define RGB_MATRIX_LED_COUNT 74
