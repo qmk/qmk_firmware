064b7ef9943f9eee4ff2c0f07dbebf51da7f1049
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2023-02-21 16:15:50 +0800

    Update keyboards/hfdkb/keyboard_sw/mk47/mk47.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/hfdkb/keyboard_sw/mk47/mk47.h
+++ keyboards/hfdkb/keyboard_sw/mk47/mk47.h
@@ -18,6 +18,9 @@
 
 #include "quantum.h"
 
+void led_test(uint8_t color);
+void clear_eeprom(void);
+
 #define LAYOUT( \
 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
 	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
