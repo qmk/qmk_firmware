782aeaa8e0e242c2dd69f0a78914aee99aabe72e
Jacob Carthew-Gabriel <floodedcodeboy@gmail.com>
2023-02-21 00:27:22 +0000

    remove unnecessary encoder resolution definition

--- keyboards/planck/keymaps/floodedcodeboy/config.h
+++ keyboards/planck/keymaps/floodedcodeboy/config.h
@@ -18,7 +18,6 @@
 
 #ifdef AUDIO_ENABLE
 #    define STARTUP_SONG SONG(PLANCK_SOUND)
-// #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
         { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
@@ -44,9 +43,6 @@
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
-
-// Most tactile encoders have detents every 4 stages
-#define ENCODER_RESOLUTION 4
 
 // LIGHTING 
 #ifdef RGBLIGHT_ENABLE
