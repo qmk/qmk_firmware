44fe11a7439e4d431acb2febc0d29b0196795b50
fauxpark <fauxpark@gmail.com>
2023-02-24 00:36:46 +1100

    Move encoder config to info.json, Y

--- keyboards/yushakobo/quick17/config.h
+++ keyboards/yushakobo/quick17/config.h
@@ -85,11 +85,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #endif
 
-#ifdef ENCODER_ENABLE
-#define ENCODERS_PAD_A { B4 }
-#define ENCODERS_PAD_B { B5 }
-#endif
-
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
