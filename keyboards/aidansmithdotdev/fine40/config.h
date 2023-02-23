be588b8338ef835fb399093ed72a5342046ef1fc
Aidan Smith <aidan@aidansmith.dev>
2023-02-22 20:55:46 -0500

    Updated Config.h to swap encoder direction
    Swapping the behavior of the encoder was not working when done in the keymap.

--- keyboards/aidansmithdotdev/fine40/config.h
+++ keyboards/aidansmithdotdev/fine40/config.h
@@ -6,7 +6,7 @@
 #include "config_common.h"
 
 #define ENCODER_RESOLUTION 2
-#define ENCODERS_PAD_A { D5 }
-#define ENCODERS_PAD_B { B7 }
+#define ENCODERS_PAD_A { B7 }
+#define ENCODERS_PAD_B { D5 }
 
 #define OLED_DISPLAY_128X64 //Comment this out to change the screen size
