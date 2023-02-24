358083803ee9a8515fe08dadc920b50bb6adb826
PollyV1 <70621744+PollyV1@users.noreply.github.com>
2023-02-24 23:30:45 +0800

    Update config.h

--- keyboards/handwired/polly40/config.h
+++ keyboards/handwired/polly40/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 REPLACE_WITH_YOUR_NAME
+/* Copyright 2023 PAUL ENRICO N. VIOLA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,11 +19,6 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x6532
-#define PRODUCT_ID      0x0069
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    paul
-#define PRODUCT         polly40 v2
 
 /* key matrix size */
 #define MATRIX_ROWS 4
