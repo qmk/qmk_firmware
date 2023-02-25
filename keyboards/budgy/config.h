4d38d76e61d37a9551e9043936bdb52b79013acf
Alex Miller <39163867+doesntfazer@users.noreply.github.com>
2023-02-25 11:48:36 -0500

    Update keyboards/budgy/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/budgy/config.h
+++ keyboards/budgy/config.h
@@ -14,13 +14,3 @@
 #define SERIAL_USART_RX_PIN GP0
 
 // #define SERIAL_USART_PIN_SWAP
-
-#define DIRECT_PINS {{GP6,  GP5,  GP4,    GP3,    GP2}, \
-                     {GP11, GP10, GP9,    GP8,    GP7}, \
-                     {GP15, GP14, GP13,   GP12,   GP16}, \
-                     {GP17, GP18, NO_PIN, NO_PIN, NO_PIN}}
-
-#define DIRECT_PINS_RIGHT {{GP2, GP3, GP4, GP5, GP6}, \
-                           {GP7, GP8, GP9, GP10, GP11}, \
-                           {GP16, GP12, GP13, GP14, GP15}, \
-                           {GP18, GP17, NO_PIN, NO_PIN, NO_PIN}}
