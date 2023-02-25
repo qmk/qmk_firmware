886f8a678152b07898ee1dcc53490a4e8e6542a8
Alex Miller <39163867+doesntfazer@users.noreply.github.com>
2023-02-25 11:49:00 -0500

    Update keyboards/crowboard/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/crowboard/rules.mk
+++ keyboards/crowboard/rules.mk
@@ -3,8 +3,3 @@ MCU = RP2040
 
 ALLOW_WARNINGS = yes
 PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS!
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
