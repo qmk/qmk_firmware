7f109a5e4313d7705995cf22e28ebea77e60ce7d
PollyV1 <70621744+PollyV1@users.noreply.github.com>
2023-02-25 21:10:38 +0800

    Update keyboards/handwired/polly40/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/polly40/config.h
+++ keyboards/handwired/polly40/config.h
@@ -31,7 +31,6 @@
 #define MATRIX_COL_PINS { F4, D7, B5, B6, C6, C7, D4, D6, D5, D0, D1, D2 }
 
 #define DIODE_DIRECTION COL2ROW
-#define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
