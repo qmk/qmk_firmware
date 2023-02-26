e303081fa67998c208b0cef41a58d99e985ef6f0
PollyV1 <70621744+PollyV1@users.noreply.github.com>
2023-02-26 18:01:42 +0800

    Update keyboards/handwired/polly40/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/handwired/polly40/config.h
+++ keyboards/handwired/polly40/config.h
@@ -16,20 +16,8 @@
 
 #pragma once
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
 #define MATRIX_ROW_PINS { F0, F1, F5, B4 }
 #define MATRIX_COL_PINS { F4, D7, B5, B6, C6, C7, D4, D6, D5, D0, D1, D2 }
-
-#define DIODE_DIRECTION COL2ROW
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
