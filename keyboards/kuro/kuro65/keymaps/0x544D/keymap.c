7e88bd0ad646935eddc91aeb5be0ceea67b27831
Tobias Minn <tobias.minn@gmail.com>
2023-02-27 22:13:58 +0100

    Changed default keymap for Kuro65 and added user keymap.

--- keyboards/kuro/kuro65/keymaps/0x544D/keymap.c
+++ keyboards/kuro/kuro65/keymaps/0x544D/keymap.c
@@ -16,9 +16,10 @@
 
 #include QMK_KEYBOARD_H
 
-#define BASE   0 // base layer Windows
-#define FN1    1 // function layer 1
-#define FN2    2 // function layer 2
+#define BL_WIN 0 // base layer Windows
+#define BL_MAC 1 // base layer Mac OS
+#define FN1    2 // function layer 1
+#define FN2    3 // function layer 2
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,12 +38,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt |MO(1)|RCtl | Lft | Dwn | Rgh |
    * `-----------------------------------------------------------------------------------------------'
    */
-    [BASE] = LAYOUT_65_iso(
+    [BL_WIN] = LAYOUT_65_iso(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,          KC_PGUP,
-        MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS, KC_ENT,  KC_PGDN,
+        MO(3),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS, KC_ENT,  KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,   KC_DEL,  
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(2),   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    [BL_MAC] = LAYOUT_65_iso(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,          KC_PGUP,
+        MO(3),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS, KC_ENT,  KC_PGDN,
+        KC_LSFT, KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,   KC_DEL,  
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(2),   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
    /* Funtion Layer 1: Media Controls with arrow keys / RShift (Play Pause), F keys,
@@ -76,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------------------------       |-----|
    * |  Trans  |MPrv |MPlay| MNxt|     |     |     |     |     |     |     |     |     |       | End |
    * |-----------------------------------------------------------------------------------------------|
-   * |       |     |     |     |     |     |     |     |     |     |     |     |         |PGUP | End |
+   * |       |     |     |     |     |     |     |     |TG(1)|     |     |     |         |PGUP | End |
    * |-----------------------------------------------------------------------------------------------|
    * |       |       |       |                                   |     |     |     | Home|PGDN | End |
    * `-----------------------------------------------------------------------------------------------'
@@ -85,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,  KC_HOME,
         KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_BOOT, KC_NO,    KC_NO,            KC_HOME,
         KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_END,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_PGUP, KC_END,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TG(1),   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_PGUP, KC_END,
         KC_NO,   KC_NO,   KC_NO,                              KC_NO,                     KC_NO,   KC_NO,   KC_NO,    KC_HOME, KC_PGDN, KC_END
     )
 
--- keyboards/kuro/kuro65/keymaps/via/keymap.c
+++ keyboards/kuro/kuro65/keymaps/via/keymap.c
@@ -16,10 +16,9 @@
 
 #include QMK_KEYBOARD_H
 
-#define BL_WIN 0 // base layer Windows
-#define BL_MAC 1 // base layer Mac OS
-#define FN1    2 // function layer 1
-#define FN2    3 // function layer 2
+#define BASE   0 // base layer Windows
+#define FN1    1 // function layer 1
+#define FN2    2 // function layer 2
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,20 +37,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt |MO(1)|RCtl | Lft | Dwn | Rgh |
    * `-----------------------------------------------------------------------------------------------'
    */
-    [BL_WIN] = LAYOUT_65_iso(
+    [BASE] = LAYOUT_65_iso(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,          KC_PGUP,
-        MO(3),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS, KC_ENT,  KC_PGDN,
+        MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS, KC_ENT,  KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,   KC_DEL,  
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(2),   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-
-    [BL_MAC] = LAYOUT_65_iso(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC, KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,          KC_PGUP,
-        MO(3),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS, KC_ENT,  KC_PGDN,
-        KC_LSFT, KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,   KC_DEL,  
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(2),   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
    /* Funtion Layer 1: Media Controls with arrow keys / RShift (Play Pause), F keys,
@@ -81,20 +72,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------------------------------------------.
    * | °^  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |   Delete  |Home |
    * |-----------------------------------------------------------------------------------------------|
-   * |        |     |     |     |     |     |     |     |     |     |RESET|     |     |        |Home |
+   * |        |     |Vol_D|Vol_U|     |     |     |     |     |     |RESET|     |     |        |Home |
    * |----------------------------------------------------------------------------------       |-----|
-   * |  Trans  |     |     |     |     |     |     |     |     |     |     |     |     |       | End |
+   * |  Trans  |MPrv |MPlay| MNxt|     |     |     |     |     |     |     |     |     |       | End |
    * |-----------------------------------------------------------------------------------------------|
-   * |       |     |     |     |     |     |     |     |TG(1)|     |     |     |         |PGUP | End |
+   * |       |     |     |     |     |     |     |     |     |     |     |     |         |PGUP | End |
    * |-----------------------------------------------------------------------------------------------|
    * |       |       |       |                                   |     |     |     | Home|PGDN | End |
    * `-----------------------------------------------------------------------------------------------'
    */
     [FN2] = LAYOUT_65_iso(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,  KC_HOME,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_BOOT, KC_NO,    KC_NO,            KC_HOME,
-        KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_END,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TG(1),   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_PGUP, KC_END,
+        KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_BOOT, KC_NO,    KC_NO,            KC_HOME,
+        KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_END,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_PGUP, KC_END,
         KC_NO,   KC_NO,   KC_NO,                              KC_NO,                     KC_NO,   KC_NO,   KC_NO,    KC_HOME, KC_PGDN, KC_END
     )
 
