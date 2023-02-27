a73d6a42f5162ed226a267b020a84d00334ab97b
PollyV1 <70621744+PollyV1@users.noreply.github.com>
2023-02-27 08:20:29 +0800

    Update keyboards/handwired/polly40/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/handwired/polly40/keymaps/default/keymap.c
+++ keyboards/handwired/polly40/keymaps/default/keymap.c
@@ -28,15 +28,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,  
-        KC_PSCR, KC_MPLY, KC_VOLD, KC_VOLU, KC_INS,  KC_DEL,  KC_COMM, KC_DOT,  KC_SLSH, KC_PGUP, MO(0),   
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_PGDN, KC_HOME  
+        KC_PSCR, KC_MPLY, KC_VOLD, KC_VOLU, KC_INS,  KC_DEL,  KC_COMM, KC_DOT,  KC_SLSH, KC_PGUP, _______,   
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_PGDN, _______  
     ),
 
     [2] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_INS,  
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_UP,   KC_DEL,  
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_LEFT, KC_DOWN, KC_RGHT, MO(0),   
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_HOME, _______  
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_LEFT, KC_DOWN, KC_RGHT, _______,   
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  _______, _______  
     ),
 
     [3] = LAYOUT(
