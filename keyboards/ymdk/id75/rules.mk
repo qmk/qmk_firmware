2720acc13eec37feb2f9222326249205b47e7d98
honorless <86894501+lesshonor@users.noreply.github.com>
2023-02-28 11:32:11 -0500

    [keyboard] ymdk/id75

---
+++ keyboards/ymdk/id75/rules.mk
@@ -0,0 +1,6 @@
+# Wildcard to allow APM32 MCU
+DFU_SUFFIX_ARGS = -p FFFF -v FFFF
+
+WS2812_DRIVER = pwm
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
