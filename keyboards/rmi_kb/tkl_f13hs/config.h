8ab5ea57dc5edc190fa14defdfa8ab7d85b8d707
Ramon Imbao <ramonimbao@gmail.com>
2023-02-28 10:58:11 +0800

    Remove config_common and debounce in config.h

--- keyboards/rmi_kb/tkl_f13hs/config.h
+++ keyboards/rmi_kb/tkl_f13hs/config.h
@@ -17,8 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 /* VIA custom menu*/
 #define VIA_EEPROM_CUSTOM_CONFIG_SIZE 8
 
@@ -47,10 +45,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENABLE_RGB_MATRIX_HUE_PENDULUM
 #define ENABLE_RGB_MATRIX_HUE_WAVE
 #endif
-
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
