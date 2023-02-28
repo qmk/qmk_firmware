4c17b0b02ffe4d53f6982723d2b66028dc86c359
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-02-28 11:43:01 +0800

    Update keyboards/keychron/q12/iso_encoder/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keychron/q12/iso_encoder/config.h
+++ keyboards/keychron/q12/iso_encoder/config.h
@@ -21,9 +21,6 @@
 #define DRIVER_2_LED_TOTAL 47
 #define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
 
-/* Encoder Configuration */
-#define ENCODERS_PAD_A { A2 }
-#define ENCODERS_PAD_B { C14 }
 #define ENCODER_DEFAULT_POS 0x3
 
 /* Enable caps-lock LED */
