37c5fbae28f04e4aaa4769aa20d10815a758123a
Ramon Imbao <ramonimbao@gmail.com>
2023-03-01 17:31:56 +0800

    Update keyboards/rmi_kb/tkl_f13hs/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/rmi_kb/tkl_f13hs/keymaps/via/keymap.c
+++ keyboards/rmi_kb/tkl_f13hs/keymaps/via/keymap.c
@@ -14,8 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "via.h"
-#include "eeprom_driver.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
