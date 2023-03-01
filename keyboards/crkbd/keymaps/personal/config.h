40dc8ca908a4a0c2eb406c683058e2c4a88d163a
HiroshiLG <jorge__regula@hotmail.com>
2023-02-28 22:09:38 -0300

    restart

--- keyboards/crkbd/keymaps/personal/config.h
+++ keyboards/crkbd/keymaps/personal/config.h
@@ -2,7 +2,7 @@
 
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
+for more options..
 */
 
 /* Memory saving*/
