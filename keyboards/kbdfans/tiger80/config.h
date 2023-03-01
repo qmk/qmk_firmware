1c71046b80ef90f6ae4f9b86eee4b1993271b95c
dztech <moyi4681@users.noreply.github.com>
2023-03-01 11:26:29 +0800

    Update keyboards/kbdfans/tiger80/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/kbdfans/tiger80/config.h
+++ keyboards/kbdfans/tiger80/config.h
@@ -13,6 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+ #pragma once
 
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_EFFECT_BREATHING
