190209283580bf8cfeb7d77bab93ec51ff245fa1
Andrew <andrewc@ultra.me>
2023-03-01 07:09:47 -0800

    chore: added new branch

---
+++ keyboards/crkbd/keymaps/jayedeem/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+RGB_MATRIX_ENABLE = yes
+
