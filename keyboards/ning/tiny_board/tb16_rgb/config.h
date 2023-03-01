1072a82d59c575f112401ec8cc85544eaef9771b
Ning <vipning@qq.com>
2023-03-01 23:10:01 +0800

    Create a new keyboard.

---
+++ keyboards/ning/tiny_board/tb16_rgb/config.h
@@ -0,0 +1,25 @@
+// Copyright 2023 Ning (@ningjx)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "config_common.h"
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 4
+
+#define TAPPING_TERM 200
+
+#define RGB_DI_PIN B5
+#define RGB_MATRIX_LED_COUNT 16
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_TYPING_HEATMAP
+#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
+
+
+
+
