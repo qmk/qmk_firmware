66d3f421a333c454dbe3b221f1bdffb351ee7ff2
aluminumfalkon <robert@rlyons.dev>
2023-03-01 19:52:26 -0500

    Basic sofle keymap for testing

--- keyboards/sofle/keymaps/aluminumfalkon/keymap.c
+++ keyboards/sofle/keymaps/aluminumfalkon/keymap.c
@@ -1,52 +1,5 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
-#include "keymap_german.h"
-#include "keymap_nordic.h"
-#include "keymap_french.h"
-#include "keymap_spanish.h"
-#include "keymap_hungarian.h"
-#include "keymap_swedish.h"
-#include "keymap_br_abnt2.h"
-#include "keymap_canadian_multilingual.h"
-#include "keymap_german_ch.h"
-#include "keymap_jp.h"
-#include "keymap_korean.h"
-#include "keymap_bepo.h"
-#include "keymap_italian.h"
-#include "keymap_slovenian.h"
-#include "keymap_lithuanian_azerty.h"
-#include "keymap_danish.h"
-#include "keymap_norwegian.h"
-#include "keymap_portuguese.h"
-#include "keymap_contributions.h"
-#include "keymap_czech.h"
-#include "keymap_romanian.h"
-#include "keymap_russian.h"
-#include "keymap_uk.h"
-#include "keymap_estonian.h"
-#include "keymap_belgian.h"
-#include "keymap_us_international.h"
-#include "keymap_croatian.h"
-#include "keymap_turkish_q.h"
-#include "keymap_slovak.h"
-
-#define KC_MAC_UNDO LGUI(KC_Z)
-#define KC_MAC_CUT LGUI(KC_X)
-#define KC_MAC_COPY LGUI(KC_C)
-#define KC_MAC_PASTE LGUI(KC_V)
-#define KC_PC_UNDO LCTL(KC_Z)
-#define KC_PC_CUT LCTL(KC_X)
-#define KC_PC_COPY LCTL(KC_C)
-#define KC_PC_PASTE LCTL(KC_V)
-#define ES_LESS_MAC KC_GRAVE
-#define ES_GRTR_MAC LSFT(KC_GRAVE)
-#define ES_BSLS_MAC ALGR(KC_6)
-#define NO_PIPE_ALT KC_GRAVE
-#define NO_BSLS_ALT KC_EQUAL
-#define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
-#define BP_NDSH_MAC ALGR(KC_8)
-#define SE_SECT_MAC ALGR(KC_6)
-#define MOON_LED_LEVEL LED_LEVEL
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
@@ -82,233 +35,238 @@ enum tap_dance_codes {
   DANCE_3,
 };
 
+enum layers {
+    LAYER_QWERTY,
+    LAYER_ARROWS
+};
+
 #define __BUILDER_KEYMAP__ "const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS]"
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[QWERTY] = LAYOUT_sofle(         KC_TRANSPARENT, KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
-KC_MINUS, KC_ESC, KC_Q, KC_TRANSPARENT, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BKSP, KC_TAB, MT(MOD_LCTL,
-KC_A), MT(MOD_LALT, KC_S), MT(MOD_LGUI, KC_D), MT(MOD_LSFT, KC_F), MT(MOD_HYPR, KC_G), MT(MOD_HYPR, KC_H), MT(MOD_RSFT,
-KC_J), MT(MOD_RGUI, KC_K), MT(MOD_RALT, KC_L), KC_SCOLON, KC_QUOTE, KC_NONE, MT(RCTL, KC_Z), KC_X, KC_C, KC_V, KC_TILD,
-KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_SPACE,
-KC_TRANSPARENT, KC_TRANSPARENT, KC_ENTER, KC_TRANSPARENT, KC_TRANSPARENT )
+[QWERTY] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_TAB, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), ALL_T(KC_G), ALL_T(KC_H), RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TILD, TT(4), TT(5), TT(2), KC_F3, KC_SPC, KC_ENT, TT(3), TT(2), TT(5), TT(4)),
+    [LAYER_ARROWS] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [2] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_F12, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, KC_TRNS, KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(4), KC_TRNS, KC_TRNS, KC_TRNS),
+    [3] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SGUI(KC_4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_PSCR, KC_APP, LCTL(KC_LEFT), LCTL(KC_RGHT), LGUI(KC_UP), LALT(KC_LEFT), KC_UP, LALT(KC_RGHT), LALT(KC_BSPC), KC_BSPC, KC_TRNS, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_CAPS, LGUI(KC_DOWN), KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL, KC_BSPC, KC_TRNS, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), CW_TOGG, KC_TRNS, KC_TRNS, KC_NO, LGUI(KC_LEFT), KC_NO, LGUI(KC_RGHT), KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [4] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, CG_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, CG_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [5] = LAYOUT(KC_NO, KC_NO, KC_PAST, KC_PSLS, KC_PPLS, KC_PMNS, KC_NO, KC_P7, KC_P8, KC_P9, KC_PAST, KC_NO, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO, KC_P4, KC_P5, KC_P6, KC_PSLS, KC_BSPC, KC_TAB, KC_6, KC_7, KC_8, KC_9, KC_0, KC_COLN, KC_P1, KC_P2, KC_P3, KC_PPLS, KC_NO, KC_LSFT, KC_UNDS, KC_PEQL, KC_PCMM, KC_COLN, KC_PDOT, KC_NO, KC_NO, KC_PEQL, KC_P0, KC_PCMM, KC_PDOT, KC_PMNS, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_SPC, KC_ENT, KC_NO, KC_NO, KC_TRNS, KC_NO)
 };
 
-extern rgb_config_t rgb_matrix_config;
+//extern rgb_config_t rgb_matrix_config;
 
-void keyboard_post_init_user(void) {
-  rgb_matrix_enable();
-}
+//void keyboard_post_init_user(void) {
+//  rgb_matrix_enable();
+//}
 
 #define __BUILDER_LEDMAP__ "const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3]"
-const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-[QWERTY] = { {29,113,204}, {235,219,0}, {55,255,0}, {55,255,0}, {55,255,0}, {55,255,0}, {55,255,0}, {55,255,0},
-{55,255,0}, {55,255,0}, {55,255,0}, {55,255,0}, {235,219,0}, {204,0,51}, {29,113,204}, {29,113,204}, {29,113,204},
-{29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {204,0,51},
-{235,219,0}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204},
-{29,113,204}, {29,113,204}, {235,219,0}, {235,219,0}, {235,219,0}, {29,113,204}, {29,113,204}, {29,113,204},
-{29,113,204}, {235,219,0}, {29,113,204}, {29,113,204}, {235,219,0}, {235,219,0}, {235,219,0}, {29,113,204},
-{29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204},
-{29,113,204}}
-};
-
-void set_layer_color(int layer) {
-  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
-    };
-    if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color( i, 0, 0, 0 );
-    } else {
-        RGB rgb = hsv_to_rgb( hsv );
-        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
-    }
-  }
-}
-
-void rgb_matrix_indicators_user(void) {
-  if (keyboard_config.disable_layer_led) { return; }
-  switch (biton32(layer_state)) {
-    case 0:
-      set_layer_color(0);
-      break;
-    case 1:
-      set_layer_color(1);
-      break;
-    case 2:
-      set_layer_color(2);
-      break;
-    case 3:
-      set_layer_color(3);
-      break;
-    case 4:
-      set_layer_color(4);
-      break;
-    case 5:
-      set_layer_color(5);
-      break;
-    case 6:
-      set_layer_color(6);
-      break;
-    case 7:
-      set_layer_color(7);
-      break;
-    case 9:
-      set_layer_color(9);
-      break;
-    case 10:
-      set_layer_color(10);
-      break;
-   default:
-    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
-      rgb_matrix_set_color_all(0, 0, 0);
-    break;
-  }
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case ST_MACRO_0:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_Q));
-
-    }
-    break;
-    case ST_MACRO_1:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_W));
-
-    }
-    break;
-    case ST_MACRO_2:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_E));
-
-    }
-    break;
-    case ST_MACRO_3:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_A));
-
-    }
-    break;
-    case ST_MACRO_4:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_S));
-
-    }
-    break;
-    case ST_MACRO_5:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_D));
-
-    }
-    break;
-    case ST_MACRO_6:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_Z));
-
-    }
-    break;
-    case ST_MACRO_7:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_X));
-
-    }
-    break;
-    case ST_MACRO_8:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_C));
-
-    }
-    break;
-    case ST_MACRO_9:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_Y));
-
-    }
-    break;
-    case ST_MACRO_10:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_U));
-
-    }
-    break;
-    case ST_MACRO_11:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_H));
-
-    }
-    break;
-    case ST_MACRO_12:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_J));
-
-    }
-    break;
-    case ST_MACRO_13:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_N));
-
-    }
-    break;
-    case ST_MACRO_14:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_M));
-
-    }
-    break;
-    case ST_MACRO_15:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_N) SS_DELAY(100) SS_TAP(X_F)  SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
-    case ST_MACRO_16:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_I) SS_DELAY(100) SS_TAP(X_T)  SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
-    case ST_MACRO_17:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_A) SS_DELAY(100) SS_TAP(X_G)  SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
-    case ST_MACRO_18:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_S) SS_DELAY(100) SS_TAP(X_L) SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
-    case ST_MACRO_19:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_D) SS_DELAY(100) SS_TAP(X_G)  SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
-    case ST_MACRO_20:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_I) SS_DELAY(100) SS_TAP(X_J)  SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
-    case ST_MACRO_21:
-    if (record->event.pressed) {
-      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_O) SS_DELAY(100) SS_TAP(X_U) SS_DELAY(100) SS_TAP(X_T)  SS_DELAY(100) SS_TAP(X_ENTER));
-
-    }
-    break;
-    case RGB_SLD:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
-  }
-  return true;
-}
+//const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
+//[QWERTY] = { {29,113,204}, {235,219,0}, {55,255,0}, {55,255,0}, {55,255,0}, {55,255,0}, {55,255,0}, {55,255,0},
+//{55,255,0}, {55,255,0}, {55,255,0}, {55,255,0}, {235,219,0}, {204,0,51}, {29,113,204}, {29,113,204}, {29,113,204},
+//{29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {204,0,51},
+//{235,219,0}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204},
+//{29,113,204}, {29,113,204}, {235,219,0}, {235,219,0}, {235,219,0}, {29,113,204}, {29,113,204}, {29,113,204},
+//{29,113,204}, {235,219,0}, {29,113,204}, {29,113,204}, {235,219,0}, {235,219,0}, {235,219,0}, {29,113,204},
+//{29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204}, {29,113,204},
+//{29,113,204}}
+//};
+//
+//void set_layer_color(int layer) {
+//  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
+//    HSV hsv = {
+//      .h = pgm_read_byte(&ledmap[layer][i][0]),
+//      .s = pgm_read_byte(&ledmap[layer][i][1]),
+//      .v = pgm_read_byte(&ledmap[layer][i][2]),
+//    };
+//    if (!hsv.h && !hsv.s && !hsv.v) {
+//        rgb_matrix_set_color( i, 0, 0, 0 );
+//    } else {
+//        RGB rgb = hsv_to_rgb( hsv );
+//        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+//        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+//    }
+//  }
+//}
+//
+//void rgb_matrix_indicators_user(void) {
+//  if (keyboard_config.disable_layer_led) { return; }
+//  switch (biton32(layer_state)) {
+//    case 0:
+//      set_layer_color(0);
+//      break;
+//    case 1:
+//      set_layer_color(1);
+//      break;
+//    case 2:
+//      set_layer_color(2);
+//      break;
+//    case 3:
+//      set_layer_color(3);
+//      break;
+//    case 4:
+//      set_layer_color(4);
+//      break;
+//    case 5:
+//      set_layer_color(5);
+//      break;
+//    case 6:
+//      set_layer_color(6);
+//      break;
+//    case 7:
+//      set_layer_color(7);
+//      break;
+//    case 9:
+//      set_layer_color(9);
+//      break;
+//    case 10:
+//      set_layer_color(10);
+//      break;
+//   default:
+//    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+//      rgb_matrix_set_color_all(0, 0, 0);
+//    break;
+//  }
+//}
+//
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//  switch (keycode) {
+//    case ST_MACRO_0:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_Q));
+//
+//    }
+//    break;
+//    case ST_MACRO_1:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_W));
+//
+//    }
+//    break;
+//    case ST_MACRO_2:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_E));
+//
+//    }
+//    break;
+//    case ST_MACRO_3:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_A));
+//
+//    }
+//    break;
+//    case ST_MACRO_4:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_S));
+//
+//    }
+//    break;
+//    case ST_MACRO_5:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_D));
+//
+//    }
+//    break;
+//    case ST_MACRO_6:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_Z));
+//
+//    }
+//    break;
+//    case ST_MACRO_7:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_X));
+//
+//    }
+//    break;
+//    case ST_MACRO_8:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_C));
+//
+//    }
+//    break;
+//    case ST_MACRO_9:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_Y));
+//
+//    }
+//    break;
+//    case ST_MACRO_10:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_U));
+//
+//    }
+//    break;
+//    case ST_MACRO_11:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_H));
+//
+//    }
+//    break;
+//    case ST_MACRO_12:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_J));
+//
+//    }
+//    break;
+//    case ST_MACRO_13:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_N));
+//
+//    }
+//    break;
+//    case ST_MACRO_14:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LALT(SS_LCTL(SS_LGUI(SS_LSFT(SS_TAP(X_R))))) SS_DELAY(100) SS_TAP(X_M));
+//
+//    }
+//    break;
+//    case ST_MACRO_15:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_N) SS_DELAY(100) SS_TAP(X_F)  SS_DELAY(100) SS_TAP(X_ENTER));
+//
+//    }
+//    break;
+//    case ST_MACRO_16:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_I) SS_DELAY(100) SS_TAP(X_T)  SS_DELAY(100) SS_TAP(X_ENTER));
+//
+//    }
+//    break;
+//    case ST_MACRO_17:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_A) SS_DELAY(100) SS_TAP(X_G)  SS_DELAY(100) SS_TAP(X_ENTER));
+//
+//    }
+//    break;
+//    case ST_MACRO_18:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_S) SS_DELAY(100) SS_TAP(X_L) SS_DELAY(100) SS_TAP(X_ENTER));
+//
+//    }
+//    break;
+//    case ST_MACRO_19:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_D) SS_DELAY(100) SS_TAP(X_G)  SS_DELAY(100) SS_TAP(X_ENTER));
+//
+//    }
+//    break;
+//    case ST_MACRO_20:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_I) SS_DELAY(100) SS_TAP(X_J)  SS_DELAY(100) SS_TAP(X_ENTER));
+//
+//    }
+//    break;
+//    case ST_MACRO_21:
+//    if (record->event.pressed) {
+//      SEND_STRING(SS_LGUI(SS_TAP(X_SPACE)) SS_DELAY(100) SS_TAP(X_O) SS_DELAY(100) SS_TAP(X_U) SS_DELAY(100) SS_TAP(X_T)  SS_DELAY(100) SS_TAP(X_ENTER));
+//
+//    }
+//    break;
+//    case RGB_SLD:
+//      if (record->event.pressed) {
+//        rgblight_mode(1);
+//      }
+//      return false;
+//  }
+//  return true;
+//}
 
 typedef struct {
     bool is_press_action;
