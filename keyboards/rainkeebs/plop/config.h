46500344b5ce163e005e6577ac99d767e3c1abe0
lukeski14 <73951271+lukeski14@users.noreply.github.com>
2023-03-02 09:29:56 -0500

    Update keyboards/rainkeebs/plop/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/rainkeebs/plop/config.h
+++ keyboards/rainkeebs/plop/config.h
@@ -7,12 +7,6 @@
 #define MATRIX_ROW_PINS { E6, D5, B3, B2 }
 #define MATRIX_COL_PINS { C7, C6, B6, B5, B4, D7, D6, D4, F6, F7, F5, F4 }
 
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING 5
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
