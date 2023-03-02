5f9cc636ccd6d232ee9bae1dab4e48894e2b4e42
Kraken-Jokes <67011325+Kraken-Jokes@users.noreply.github.com>
2023-03-02 13:55:20 -0700

    Update keymap.c

--- keyboards/pteron56/keymaps/via/keymap.c
+++ keyboards/pteron56/keymaps/via/keymap.c
@@ -30,18 +30,3 @@ void matrix_init_user(void) {
 
 void matrix_scan_user(void) {
 }
-
-//#	ifdef OCEAN_DREAM_ENABLE
-//        render_stars();
-//#    endif
-
-//bool encoder_update_user(uint8_t index, bool clockwise) {
-//    if (index == 0) { /* First encoder */
-//        if (clockwise) {
-//            tap_code(KC_VOLU);
-//        } else {
-//            tap_code(KC_VOLD);
-//        }
-//    }
-//    return false;
-//}
