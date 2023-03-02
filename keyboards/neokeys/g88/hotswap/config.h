8f8f7c3eea697d7637c7a6fbbd450d0c51eb9c91
TommyZ <60314626+tommyamoszhao@users.noreply.github.com>
2023-03-02 11:10:19 +0800

    Update keyboards/neokeys/g88/hotswap/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/neokeys/g88/hotswap/config.h
+++ keyboards/neokeys/g88/hotswap/config.h
@@ -51,22 +51,3 @@
 #define ENABLE_RGB_MATRIX_MULTISPLASH               // Full gradient & value pulse away from multiple key hits then fades value out
 #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH         // Hue & value pulse away from multiple key hits then fades value out
 #endif
-
-#if defined(RGBLIGHT_ENABLE)
-    #define RGBLED_NUM 107
-    #define RGBLIGHT_HUE_STEP 8
-    #define RGBLIGHT_SAT_STEP 8
-    #define RGBLIGHT_VAL_STEP 8
-    #define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
-    #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#endif
