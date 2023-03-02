qmk/qmk_firmware

commit c74148db94f85893eb1b85cf2291d7c792deab42

Implement GitHub PR suggestions for system76/launch_1

keyboards/system76/launch_1/config.h
@@ -49,18 +49,6 @@
 // Set 0 if debouncing isn't needed
 #define DEBOUNCE 5
 
-// NKRO must be used
-#define FORCE_NKRO
-
-#ifdef RGBLIGHT_ENABLE
-#    define RGB_DI_PIN E2
-#    define RGBLED_NUM 84
-#    define RGBLIGHT_ANIMATIONS
-// Limit brightness to support USB-A at 0.5 A
-// TODO: do this dynamically based on power source
-#    define RGBLIGHT_LIMIT_VAL 176
-#endif  // RGBLIGHT_ENABLE
-
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_DI_PIN E2
 #    define DRIVER_LED_TOTAL 84
@@ -70,6 +58,8 @@
 #    define RGB_DISABLE_TIMEOUT 0                                      // Number of milliseconds to wait until RGB automatically turns off
 #    define RGB_DISABLE_AFTER_TIMEOUT 0                                // OBSOLETE: Number of ticks to wait until disabling effects
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED                             // Turns off effects when suspended
+// Limit brightness to support USB-A at 0.5 A
+// TODO: Do this dynamically based on power source
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 176                          // Limits maximum brightness of LEDs to 176 out of 255. If not defined, maximum brightness is set to 255
 #    define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_RAINBOW_MOVING_CHEVRON  // Sets the default mode, if none has been set
 #    define RGB_MATRIX_STARTUP_HUE 142                                 // Sets the default hue value, if none has been set
@@ -100,12 +90,11 @@
 // I2C {
 #define F_SCL 100000UL // Run I2C bus at 100 kHz
 #define I2C_START_RETRY_COUNT 20
-#define I2C_TIMEOUT 10 // milliseconds
+#define I2C_TIMEOUT 100 // milliseconds
 // } I2C
 
 // EEPROM {
 #define EEPROM_SIZE 1024
-
 // TODO: Refactor with new user EEPROM code (coming soon)
 #define EEPROM_MAGIC 0x76EC
 #define EEPROM_MAGIC_ADDR 64
@@ -116,16 +105,15 @@
 #define EEPROM_VERSION_ADDR (EEPROM_MAGIC_ADDR + 2)
 // } EEPROM
 
-// Dynamic keyboard support {
+// Dynamic keymap {
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define DYNAMIC_KEYMAP_MACRO_COUNT 0
 // Dynamic keymap starts after EEPROM version
 #define DYNAMIC_KEYMAP_EEPROM_ADDR (EEPROM_VERSION_ADDR + 1)
-#define DYNAMIC_KEYMAP_EEPROM_SIZE (DYNAMIC_KEYMAP_LAYER_COUNT * MATRIX_ROWS * MATRIX_COLS * 2)
 // Dynamic macro starts after dynamic keymaps, it is disabled
-#define DYNAMIC_KEYMAP_MACRO_EEPROM_ADDR (DYNAMIC_KEYMAP_EEPROM_ADDR + DYNAMIC_KEYMAP_EEPROM_SIZE)
+#define DYNAMIC_KEYMAP_MACRO_EEPROM_ADDR (DYNAMIC_KEYMAP_EEPROM_ADDR + (DYNAMIC_KEYMAP_LAYER_COUNT * MATRIX_ROWS * MATRIX_COLS * 2))
 #define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 0
-#define DYNAMIC_KEYMAP_MACRO_COUNT 0
-// } Dynamic keyboard support
+// } Dynamic keymap
 
 // System76 EC {
 #define SYSTEM76_EC_EEPROM_ADDR (DYNAMIC_KEYMAP_MACRO_EEPROM_ADDR + DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE)

keyboards/system76/launch_1/launch_1.c
@@ -17,9 +17,6 @@
 
 #include "launch_1.h"
 
-#include "dynamic_keymap.h"
-#include "eeprom.h"
-#include "rgb_matrix.h"
 #include "usb_mux.h"
 
 // clang-format off
@@ -83,31 +80,11 @@ void eeprom_set_valid(bool valid) {
     eeprom_update_byte(((void *)EEPROM_VERSION_ADDR), valid ? EEPROM_VERSION : 0xFF);
 }
 
-void eeprom_reset(void) {
+void bootmagic_lite_reset_eeprom(void) {
     // Set the keyboard-specific EEPROM state as invalid
     eeprom_set_valid(false);
     // Set the TMK/QMK EEPROM state as invalid
     eeconfig_disable();
-}
-
-// The lite version of TMK's bootmagic based on Wilba.
-// 100% less potential for accidentally making the keyboard do stupid things.
-void bootmagic_lite(void) {
-    // Do multiple scans because debouncing cannot be turned off
-    matrix_scan();
-#if defined(DEBOUNCE) && DEBOUNCE > 0
-    wait_ms(DEBOUNCE * 2);
-#else
-    wait_ms(30);
-#endif
-    matrix_scan();
-
-    // If the `Esc' key (matrix 0,0) is held down on power up,
-    // reset the EEPROM valid state and jump to bootloader
-    if (matrix_get_row(0) & (1 << 0)) {
-        eeprom_reset();
-        bootloader_jump();
-    }
 }
 
 void system76_ec_rgb_eeprom(bool write);
@@ -217,16 +194,6 @@ layer_state_t layer_state_set_kb(layer_state_t layer_state) {
     system76_ec_rgb_layer(layer_state);
 
     return layer_state_set_user(layer_state);
-}
-
-void suspend_power_down_kb(void) {
-    rgb_matrix_set_suspend_state(true);
-    suspend_power_down_user();
-}
-
-void suspend_wakeup_init_kb(void) {
-    rgb_matrix_set_suspend_state(false);
-    suspend_wakeup_init_user();
 }
 
 #ifdef CONSOLE_ENABLE

keyboards/system76/launch_1/rules.mk
@@ -1,7 +1,7 @@
 # MCU name
 MCU = atmega32u4
 
-# Allow flashing with usbasp
+# Allow flashing with USBasp
 AVRDUDE_MCU = $(MCU)
 
 # CPU frequency divided by two since AVR is at 3.3 V
@@ -14,9 +14,9 @@ F_USB = 16000000
 BOOTLOADER = atmel-dfu
 
 # Build options
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes      # Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and system control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 DYNAMIC_KEYMAP_ENABLE = yes # Reconfigurable keyboard without flashing firmware
@@ -26,14 +26,11 @@ RGBLIGHT_ENABLE = no        # RGB backlight (conflicts with RGB matrix)
 RGB_MATRIX_ENABLE = yes     # RGB matrix
 RGB_MATRIX_DRIVER = WS2812
 RGB_MATRIX_CUSTOM_KB = yes  # Custom keyboard effects
-USB_6KRO_ENABLE = no        # USB 6-key rollover
 LTO_ENABLE = yes            # Link-time optimization for smaller binary
 
 # Add System76 EC command interface
 SRC += ../system76_ec.c
 
-# Add I2C driver
+# Add System76 EC command interface as well as I2C and USB mux drivers
+SRC += system76_ec.c usb_mux.c
 QUANTUM_LIB_SRC += i2c_master.c
-
-# Add USB mux driver
-SRC += usb_mux.c

commit 7593a5f1b4874d5ff5ad770033908e4171685011

Fix rgb matrix animation on launch_1
Upstream broke the animations in
https://github.com/qmk/qmk_firmware/pull/15395/commits/c74148db94f85893eb1b85cf2291d7c792deab42

This reverts part of that commit

keyboards/system76/launch_1/config.h
@@ -72,7 +72,7 @@
 // I2C {
 #define F_SCL 100000UL // Run I2C bus at 100 kHz
 #define I2C_START_RETRY_COUNT 20
-#define I2C_TIMEOUT 100 // milliseconds
+#define I2C_TIMEOUT 10 // milliseconds
 // } I2C
 
 // EEPROM {