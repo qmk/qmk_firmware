acbbcd0cd35dc5121b44e0dede6f1352111963db
Kraken-Jokes <67011325+Kraken-Jokes@users.noreply.github.com>
2023-03-02 13:51:42 -0700

    Update config.h

--- keyboards/pteron56/keymaps/brant/config.h
+++ keyboards/pteron56/keymaps/brant/config.h
@@ -25,11 +25,11 @@
 /* number of backlight levels */
 
 #ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 0
+#define BACKLIGHT_LEVELS 1
 #endif
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
