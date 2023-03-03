d474231533a899846dddfb60c5e64744f67c7a45
fauxpark <fauxpark@gmail.com>
2023-03-03 22:05:37 +1100

    Move matrix config to info.json, A

--- keyboards/acheron/elongate/beta/config.h
+++ keyboards/acheron/elongate/beta/config.h
@@ -17,23 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
-#define MATRIX_ROW_PINS { D3, B7, D5, B5, D6 }
-#define MATRIX_COL_PINS { F5, F6, F4, F1, F0, B2, B1, C6, B0, B3, E6, D4, B4}
-
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
 #define RGB_DI_PIN D7
 #ifdef RGB_DI_PIN
   #define RGBLED_NUM 16
--- keyboards/avalanche/v4/config.h
+++ keyboards/avalanche/v4/config.h
@@ -3,11 +3,6 @@
 
 #pragma once
 
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
-#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
-
-#define DIODE_DIRECTION COL2ROW
-
 #ifdef RGBLIGHT_ENABLE
 #    define RGB_DI_PIN D3
 #    define RGBLED_NUM 64
