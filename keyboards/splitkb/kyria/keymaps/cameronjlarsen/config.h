1d6c399d487fbc7c6beea5f74cdfddad9250ce4f
Cameron Larsen <camjlarsen@gmail.com>
2023-03-03 20:55:02 -0800

    Updated licences to be consistent

--- keyboards/splitkb/kyria/keymaps/cameronjlarsen/config.h
+++ keyboards/splitkb/kyria/keymaps/cameronjlarsen/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+/* Copyright 2022 Cameron Larsen <camjlarsen@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
