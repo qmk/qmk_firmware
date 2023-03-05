575bef819dfce4f1f4e611520a38360335347b1c
paulgali <paulgali@live.co.uk>
2023-03-06 02:33:16 +0400

    Moved configs to info.json as requested

--- keyboards/skme/zeno/keymaps/default/keymap.c
+++ keyboards/skme/zeno/keymaps/default/keymap.c
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_default(
 		QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
