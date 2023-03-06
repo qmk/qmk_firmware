c48d04600385a266e4958a0bdde1c1eef791049a
Ryan <PHSC138@users.noreply.github.com>
2023-03-05 23:28:01 -0500

    Update keyboards/evyd13/atom47/keymaps/phsc138/keymap.c
    KC_GESC -> QK_GESC
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/evyd13/atom47/keymaps/phsc138/keymap.c
+++ keyboards/evyd13/atom47/keymaps/phsc138/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Tap pn for toggle to _PROG, or hold for numpad
 [_BASE] = LAYOUT_split_space(
-    KC_GESC,  KC_Q,     KC_W,     KC_E,        KC_R,        KC_T,       KC_Y,    KC_U,      KC_I,    KC_O,       KC_P,     KC_DEL,   KC_BSPC,
+    QK_GESC,  KC_Q,     KC_W,     KC_E,        KC_R,        KC_T,       KC_Y,    KC_U,      KC_I,    KC_O,       KC_P,     KC_DEL,   KC_BSPC,
     KC_TAB,   KC_A,     KC_S,     KC_D,        KC_F,        KC_G,       KC_H,    KC_J,      KC_K,    KC_L,       KC_SCLN,            KC_ENT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,        KC_V,        KC_B,       KC_N,    KC_M,      KC_COMM, KC_DOT,     KC_RSFT,            MO(_FN1),
     KC_LCTL,  KC_LGUI,  KC_LALT,  TD(PN_SWAP),              KC_SPC,              KC_SPC,             MO(_FN),    KC_RALT,  KC_APP,   KC_RCTRL),
