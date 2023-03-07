e9dfc69e8f3ec5318b1ca120de023a633c8b4577
esva3030 <simon.varvat@soprasteria.com>
2023-03-07 17:19:07 +0100

    #WORK: Evolution keymap part 11

--- keyboards/hhkb/ansi/keymaps/svarvat/keymap.c
+++ keyboards/hhkb/ansi/keymaps/svarvat/keymap.c
@@ -1085,13 +1085,13 @@ bool processKeycodeIfLPinkyZ(uint16_t keycode, keyrecord_t* record) {
     return true;
 }
 
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  //debug_keyboard=true;
-  debug_mouse=true;
-}
+//void keyboard_post_init_user(void) {
+//    Customise these values to desired behaviour
+//    debug_enable=true;
+//    debug_matrix=true;
+//    debug_keyboard=true;
+//    debug_mouse=true;
+//}
 
 void matrix_scan_user(void) {
     if (isMouseX1Started) {
