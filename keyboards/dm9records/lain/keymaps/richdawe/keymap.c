70110df9fe0505279850b7f8978e21e21623ab5b
Richard Dawe <richdawe@gmail.com>
2023-03-07 10:26:10 +0000

    start v2: separate nav layer

--- keyboards/dm9records/lain/keymaps/richdawe/keymap.c
+++ keyboards/dm9records/lain/keymaps/richdawe/keymap.c
@@ -1,5 +1,5 @@
 // Copyright 2022 Takuya Urakawa @hsgw (dm9records.com, 5z6p.com)
-// Copyright 2022 Richard Dawe
+// Copyright 2022, 2023 Richard Dawe
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 // Convert this into a printable keymap on Linux using:
@@ -11,9 +11,10 @@ ps2pdf lain-keymap-richdawe.ps lain-keymap-richdawe.pdf
 
 #include QMK_KEYBOARD_H
 
-enum layers { BASE, NUM, SYM, FUNC };
+enum layers { BASE, NAV, NUM, SYM, FUNC };
 enum custom_keycodes { LED_EN = SAFE_RANGE };
 
+#define LTV_SPC LT(NAV, KC_SPC)
 #define LTS_SPC LT(SYM, KC_SPC)
 #define LTN_SPC LT(NUM, KC_SPC)
 #define LTF_TAB LT(FUNC, KC_TAB)
@@ -34,13 +35,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------'     \-----------------------------------|
  * |LShft|  Z  /  X  /  C  /  V  /  B  /           \  B  \  N  \  M  \  ,  \  .  |ShEnt|
  * `-----------------------------------'           \-----------------------------------|
- *    |LCtrl|  /LGUI /LAlt /SpLSy/SpLSy/           \SpLNu\SpLNu\RAlt \  /? \  \| |  `~ |
+ *    |LCtrl|  /LGUI /LAlt /SpLSy/SpLNu/           \SpLNv\SpLSy\RAlt \RGui \ `~  | /?  |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         LTF_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SFTENT,
-        KC_LCTL,          KC_LGUI, KC_LALT, LTS_SPC, LTS_SPC,            LTN_SPC, LTN_SPC, KC_RALT, SH_SLSH, SH_BSLS, SH_GRV
+        KC_LCTL,          KC_LGUI, KC_LALT, LTS_SPC, LTN_SPC,            LTV_SPC, LTS_SPC, KC_RALT, KC_RGUI, SH_GRV,  SH_SLSH
+    ),
+
+    [NAV] = LAYOUT(
+/*
+ * ,-----------------------------------------.     ,-----------------------------------.
+ * |     |     /     /     /     /     /     /     \Home \PgDn \PgUp \ End \     |Bspc |
+ * |-----------------------------------------/     \-----------------------------------|
+ * |     |     /     /     /     /     /     /     \Left \Down \ Up  \Right\     |     |
+ * |-----------------------------------------'     \-----------------------------------|
+ * |     |     /     /     /     /     /           \     \     \     \     \     |     |
+ * `-----------------------------------'           \-----------------------------------|
+ *    |     |  /     /     /  -  /  -  /           \     \  -  \     \     \     |     |
+ *    `-----'  `-----------------------'           `-----------------------------------'
+ */
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  XXXXXXX, KC_BSPC,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
+        _______,          _______, _______, KC_MINS, KC_MINS,             _______, KC_MINS, _______,  _______, XXXXXXX, XXXXXXX
     ),
 
     [NUM] = LAYOUT(
@@ -48,17 +67,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.     ,-----------------------------------.
  * |     |     /  1  /  2  /  3  /  :  /  (  /     \  -  \  =  \     \  0  \  .  |Bspc |
  * |-----------------------------------------/     \-----------------------------------|
- * |     |     /  4  /  5  /  6  /  .  /  )  /     \Left \Down \ Up  \Right\     |     |
+ * |     |     /  4  /  5  /  6  /  .  /  )  /     \     \     \     \     \     |     |
  * |-----------------------------------------'     \-----------------------------------|
  * |     |     /  7  /  8  /  9  /  0  /           \     \     \     \     \     |     |
  * `-----------------------------------'           \-----------------------------------|
- *    |     |  /     /     /  -  /  -  /           \     \     \     \     \     |     |
+ *    |     |  /     /     /  -  /     /           \  -  \  -  \     \     \     |     |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         _______, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_COLON, KC_LPRN,   KC_MINS, KC_EQL,  XXXXXXX,  KC_0,    KC_DOT,  KC_BSPC,
-        _______, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_DOT,   KC_RPRN,   KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT,XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_DOT,   KC_RPRN,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_0,                XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
-        _______,          _______, _______, KC_MINS, KC_MINS,             _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
+        _______,          _______, _______, KC_MINS, _______,             KC_MINS, KC_MINS, _______,  _______, XXXXXXX, XXXXXXX
     ),
 
     [SYM] = LAYOUT(
@@ -66,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.     ,-----------------------------------.
  * |     |     /  !  /  @  /  #  /     /  (  /     \  _  \  +  \     \     \     |Bspc |
  * |-----------------------------------------/     \-----------------------------------|
- * |     |     /  $  /  %  /  ^  /     /  )  /     \Home \PgDn \PgUp \ End \     |     |
+ * |     |     /  $  /  %  /  ^  /     /  )  /     \     \     \     \     \     | \|  |
  * |-----------------------------------------'     \-----------------------------------|
  * |     |     /  &  /  *  /  (  /  )  /           \     \     \     \     \     |     |
  * `-----------------------------------'           \-----------------------------------|
@@ -74,27 +93,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
         _______, XXXXXXX, S(KC_1), S(KC_2), S(KC_3), XXXXXXX, KC_LPRN,   S(KC_MINS), S(KC_EQL), XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
-        _______, XXXXXXX, S(KC_4), S(KC_5), S(KC_6), XXXXXXX, KC_RPRN,   KC_HOME,    KC_PGDN,   KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, S(KC_4), S(KC_5), S(KC_6), XXXXXXX, KC_RPRN,   XXXXXXX,    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, SH_BSLS,
         _______, XXXXXXX, S(KC_7), S(KC_8), S(KC_9), S(KC_0),            XXXXXXX,    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        _______,          _______, _______, _______, _______,            KC_MINS,    KC_MINS,   _______, XXXXXXX, XXXXXXX, XXXXXXX
+        _______,          _______, _______, _______, KC_MINS,            KC_MINS,    _______,   _______, _______, XXXXXXX, XXXXXXX
     ),
 
     [FUNC] = LAYOUT(
 /*
  * ,-----------------------------------------.     ,-----------------------------------.
- * |     |     / F1  / F2  / F3  /     /MPrev/     \     \     \     \     \MPlay| Del |
+ * |     |     / F1  / F2  / F3  /     /     /     \     \VMute\MPlay\     \MPlay| Del |
  * |-----------------------------------------/     \-----------------------------------|
- * |CapsL|LEDEn/ F4  / F5  / F6  /     /MNext/     \     \VolDn\VolUp\     \     |     |
+ * |CapsL|LEDEn/ F4  / F5  / F6  /     /     /     \MPrev\VolDn\VolUp\MNext\     |     |
  * |-----------------------------------------'     \-----------------------------------|
- * |     |     / F7  / F8  / F9  /     /           \     \     \VMute\     \     |     |
+ * |     |     / F7  / F8  / F9  /     /           \     \     \     \     \     |     |
  * `-----------------------------------'           \-----------------------------------|
- *    |Bootl|  / F10 / F11 / F12 /MPlay/           \MPlay\     \     \     \     |     |
+ *    |Bootl|  / F10 / F11 / F12 /     /           \     \     \     \     \     |     |
  *    `-----'  `-----------------------'           `-----------------------------------'
  */
-        _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   XXXXXXX, KC_MPRV,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, KC_DEL,
-        KC_CAPS, LED_EN,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX, KC_MNXT,   XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, _______,
-        _______, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,            XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, _______,
-        QK_BOOT,          KC_F10,  KC_F11,  KC_F12,  KC_MPLY,            KC_MPLY, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
+        _______, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   XXXXXXX, XXXXXXX,   XXXXXXX, KC_MUTE, KC_MPLY, XXXXXXX, KC_MPLY, KC_DEL,
+        KC_CAPS, LED_EN,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX,   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        QK_BOOT,          KC_F10,  KC_F11,  KC_F12,  XXXXXXX,            XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX
     ),
 };
 
@@ -164,6 +183,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             lain_set_led(1, 0);
             lain_set_led(2, 1);
             break;
+        case NAV:
         case FUNC:
             lain_set_led(1, 1);
             lain_set_led(2, 1);
