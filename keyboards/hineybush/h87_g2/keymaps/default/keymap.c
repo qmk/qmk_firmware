ac086daf493b37d6f0a48f475937cb9bb9096789
Josh Hinnebusch <joshhinnebusch@gmail.com>
2023-03-07 21:56:12 -0500

    Update keyboards/hineybush/h87_g2/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/hineybush/h87_g2/keymaps/default/keymap.c
+++ keyboards/hineybush/h87_g2/keymaps/default/keymap.c
@@ -1,6 +1,6 @@
-#include QMK_KEYBOARD_H
 // Copyright 2023 Josh Hinnebusch
 // SPDX-License-Identifier: GPL-2.0-or-later
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
