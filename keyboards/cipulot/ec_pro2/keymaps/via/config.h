dfd997d8b22c9df706c9eeee923af9f5c1dce550
Cipulot <40441626+Cipulot@users.noreply.github.com>
2023-03-08 02:09:27 +0100

    Update to comply with `_kb` and `_user` separation

--- keyboards/cipulot/ec_pro2/keymaps/via/config.h
+++ keyboards/cipulot/ec_pro2/keymaps/via/config.h
@@ -16,30 +16,5 @@
 
 #pragma once
 
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
-
-/* Custom matrix pins and port select array */
-#define MATRIX_ROW_PINS \
-    { B15, A8, B0, A7, B1 }
-#define MATRIX_COL_CHANNELS \
-    { 0, 3, 1, 2, 5, 7, 6, 4 }
-#define MUX_SEL_PINS \
-    { B6, B5, B4 }
-
-/* Hardware peripherals pins */
-#define APLEX_EN_PIN_0 B7
-#define APLEX_EN_PIN_1 B3
-#define DISCHARGE_PIN A6
-#define ANALOG_PORT A3
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-#define DEFAULT_ACTUATION_LEVEL 550
-#define DEFAULT_RELEASE_LEVEL 500
-
-#define DISCHARGE_TIME 10
+// This is the size of the EEPROM for the custom VIA-specific data
+#define EECONFIG_USER_DATA_SIZE 4
