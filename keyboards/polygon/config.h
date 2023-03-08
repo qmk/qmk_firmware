e19d807bd7a05cdba140d69f03b6f7584788a98e
Rotwitt <lasse.rotwitt@gmail.com>
2023-03-08 16:13:59 +0100

    Update keyboards/polygon/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/polygon/config.h
+++ keyboards/polygon/config.h
@@ -47,17 +47,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { B0, B1, B2, B3, B7 }
 #define MATRIX_COL_PINS { D4, D6, D7, B4, B5, B6, C6, C7, F7, F6, F5, F4, F1, F0, E6, D2 }
 
-//#define UNUSED_PINS {  }
-
-/* COL2ROW, ROW2COL */
-/*#define DIODE_DIRECTION COL2ROW*/
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
