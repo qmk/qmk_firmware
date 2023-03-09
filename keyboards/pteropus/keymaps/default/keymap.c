5ca0e69573068ae72e267b1f45e1f906a7d2f3c3
Chi-Han Yang <chyang@enrichtx.com>
2023-03-09 11:57:51 -0500

    Add miryoku layout

--- keyboards/pteropus/keymaps/default/keymap.c
+++ keyboards/pteropus/keymaps/default/keymap.c
@@ -18,22 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   │   │   ├───┘
       *                   └───┘   └───┘
       */
-    // [0] = LAYOUT_SPLIT_3x5_3(
-    //     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    //     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    //     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    //                                KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
-    // )
-#define LAYOUT(
-  K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09,
-  K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19,
-  K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29,
-                 K32, K33, K34, K35, K36, K37
-  )
-  {
-    {K00,   K01,   K02, K03, K04, K05, K06, K07, K08,   K09},
-    {K10,   K11,   K12, K13, K14, K15, K16, K17, K18,   K19},
-    {K20,   K21,   K22, K23, K24, K25, K26, K27, K28,   K29},
-    {KC_NO, KC_NO, K32, K33, K34, K35, K36, K37, KC_NO, KC_NO},
-  }
+    [0] = LAYOUT_split_3x5_3(
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+    ),
 };
