ca89529c323d7f5dbf6e9f792999719fe88bcf13
Mikael Manukyan <arm.localhost@gmail.com>
2023-03-09 23:16:31 -0800

    Update keyboards/bastardkb/charybdis/3x6/kb2040/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/bastardkb/charybdis/3x6/kb2040/rules.mk
+++ keyboards/bastardkb/charybdis/3x6/kb2040/rules.mk
@@ -16,14 +16,6 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-
-AUDIO_SUPPORTED = no        # Audio is not supported
-RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
-RGBLIGHT_SUPPORTED = no     # RGB underglow is supported, but not enabled by default
-RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
-
-SPLIT_KEYBOARD = yes
-
 POINTING_DEVICE_ENABLE = yes # Enable trackball
 POINTING_DEVICE_DRIVER = pmw3360
 
