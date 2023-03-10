9b59060aa91ba59894a4b3a1664798e670a2f639
Agent Blu, 006 <blu006@ucr.edu>
2023-03-09 22:23:14 -0800

    Update keyboards/blu/vimclutch/vimclutch.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/blu/vimclutch/vimclutch.c
+++ keyboards/blu/vimclutch/vimclutch.c
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "vimclutch.h"
+#include "quantm.h"
 
 void keyboard_pre_init_kb(void)
 {
