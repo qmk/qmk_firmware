caa8c81086a806ea048c91ba142d424dc6a49f50
Ryan <fauxpark@gmail.com>
2023-03-11 00:39:56 +1100

    Move matrix config to info.json, part 7 (#20020)

--- keyboards/mechbrewery/mb65s/config.h
+++ keyboards/mechbrewery/mb65s/config.h
@@ -17,13 +17,6 @@
 
 #pragma once
 
-
-#define MATRIX_ROW_PINS { B7, D0, F0, F1, F4 }
-#define MATRIX_COL_PINS { B0, B1, B2, B3, D1, D2, D3, D6, D7, B4, B6, C6, C7, F7, F6, F5 }
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
 #define RGB_DI_PIN E2
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_EFFECT_BREATHING
--- keyboards/momoka_ergo/config.h
+++ keyboards/momoka_ergo/config.h
@@ -17,12 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5, B6, B7 }
-#define MATRIX_COL_PINS { F7, F6, F5, F4, F1, F0 }
-
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
 #define SELECT_SOFT_SERIAL_SPEED 5
 
 #define RGB_DI_PIN C7
