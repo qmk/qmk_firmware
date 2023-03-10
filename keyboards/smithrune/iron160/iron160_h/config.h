65909ac610b097c0a66465de64bda299a1e280a0
Álvaro A. Volpato <alvaro.augusto.volpato@gmail.com>
2023-03-10 15:27:27 -0300

    Remove processor, bootloader, backlight and matrix configurations from source files
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/smithrune/iron160/iron160_h/config.h
+++ keyboards/smithrune/iron160/iron160_h/config.h
@@ -17,17 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//                        C0 , C1 , C2 , C3 , C4 , C5 , C6 , C7 , C8 , C9 , C10, C11, C12, C13 
-#define MATRIX_COL_PINS { A3 , B9 , B8 , B5 , B4 , B3 , A6 , A5 , A4 , A2 , A1 , A0 , C15, C14}
-//                        R0 , R1 , R2 , R3 , R4
-#define MATRIX_ROW_PINS { A10, A15, C13, A7 , B0 }
-#define DIODE_DIRECTION COL2ROW
-
 #define RGB_DI_PIN B15
 #define RGBLED_NUM 1
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE    5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
--- keyboards/smithrune/iron160/iron160_s/config.h
+++ keyboards/smithrune/iron160/iron160_s/config.h
@@ -17,20 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//                        C0 , C1 , C2 , C3 , C4 , C5 , C6 , C7 , C8 , C9 , C10, C11, C12, C13 
-#define MATRIX_COL_PINS { A3 , B9 , B8 , B5 , B4 , B3 , A6 , A5 , A4 , A2 , A1 , A0 , C15, C14}
-//                        R0 , R1 , R2 , R3 , R4
-#define MATRIX_ROW_PINS { A10, A15, C13, B1 , B10}
-#define DIODE_DIRECTION COL2ROW
-
-#define BACKLIGHT_PIN           A7
 #define BACKLIGHT_PWM_DRIVER    PWMD3
 #define BACKLIGHT_PWM_CHANNEL   2
 #define BACKLIGHT_PAL_MODE      1
-#define BACKLIGHT_LEVELS 20
-#define BACKLIGHT_BREATHING
-#define BREATHING_PERIOD 3
-#define BACKLIGHT_ON_STATE 1
 #define LED_CAPS_LOCK_PIN B0
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE    5
