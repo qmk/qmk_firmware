21a4a32b1f1c10b63d8bd899573314aa1d85853a
Moritz Plattner <moritz.plattner@gmx.net>
2023-03-13 14:56:47 +0100

    Apply suggestions from code review (remove empty layers)
    Co-authored-by: Pablo Martínez <58857054+elpekenin@users.noreply.github.com>

--- keyboards/geistmaschine/macropod/keymaps/via/keymap.c
+++ keyboards/geistmaschine/macropod/keymaps/via/keymap.c
@@ -9,20 +9,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
-    [2] = LAYOUT_fourkey (
-        KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-
-    [3] = LAYOUT_fourkey (
-        KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS
-    )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
     [1] = { ENCODER_CCW_CW(KC_MRWD, KC_MFFD)  },
-    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
 };
 #endif
