e049a9d5ba18f77d898b16dc03646e0d2f7e3fe5
dztech <moyi4681@users.noreply.github.com>
2023-03-13 21:10:20 +0800

    Update keyboards/kbdfans/odin75/halconf.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/kbdfans/odin75/halconf.h
+++ keyboards/kbdfans/odin75/halconf.h
@@ -1,4 +1,4 @@
- /* Copyright 2022 DZTECH <moyi4681@live.cn>
+/* Copyright 2022 DZTECH <moyi4681@live.cn>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
