167c0995ac5ce44baf0c2988d0df4065646ee691
moritz plattner <moritz.plattner@gmx.net>
2023-03-13 14:12:19 +0100

    overhaul keymaps

--- keyboards/geistmaschine/macropod/keymaps/default/keymap.c
+++ keyboards/geistmaschine/macropod/keymaps/default/keymap.c
@@ -2,26 +2,26 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_fourkey (
-        KC_MPLY, KC_A,    KC_B,    KC_C
+        LT(1, KC_MUTE),     KC_MPRV, KC_MPLY, KC_MNXT
     ),
 
     [1] = LAYOUT_fourkey (
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [2] = LAYOUT_fourkey (
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [3] = LAYOUT_fourkey (
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [1] = { ENCODER_CCW_CW(KC_MRWD, KC_MFFD)  },
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
     [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
 };
