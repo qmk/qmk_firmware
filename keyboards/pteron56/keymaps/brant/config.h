3dd8b9a992a77be3866f6d4f86ce0b2a2fa21df2
Kraken-Jokes <67011325+Kraken-Jokes@users.noreply.github.com>
2023-03-13 15:39:28 -0600

    Editing out redundant info

--- keyboards/pteron56/keymaps/brant/config.h
+++ keyboards/pteron56/keymaps/brant/config.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x6D4B
-#define PRODUCT_ID      0x7056
-#define DEVICE_VER      0x0002
-#define MANUFACTURER    MooreKeys
-
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 12
@@ -19,12 +11,6 @@
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
-
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 1
-#endif
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNE 5
@@ -40,11 +26,3 @@
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
-
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
