e62fc866dbac6b558b328be14e8096835c3aadc7
Nick Brassel <nick@tzarc.org>
2023-03-15 08:31:10 +1100

    Let's use workflows to parallelise the builds. (#20120)
    * Let's use workflows to parallelise the builds.
    
    * Nofail if files aren't present.
    
    * Formatting.

--- lib/python/qmk/cli/mass_compile.py
+++ lib/python/qmk/cli/mass_compile.py
@@ -106,7 +106,7 @@ def mass_compile(cli):
 
                     def _make_filter(k, v):
                         expr = fnmatch.translate(v)
-                        rule = re.compile(expr, re.IGNORECASE)
+                        rule = re.compile(f'^{expr}$', re.IGNORECASE)
 
                         def f(e):
                             lhs = e[2].get(k)
