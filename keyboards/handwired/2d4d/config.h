35e5bac282fb48c500b2e2f035950575f5ff871e
mechMehmet <muh.mehmet.degirmencioglu@gmail.com>
2023-03-16 01:11:21 +0300

    merge all files to info.json

--- keyboards/handwired/2d4d/config.h
+++ keyboards/handwired/2d4d/config.h
@@ -4,33 +4,17 @@
 #pragma once
 
 
-#define MATRIX_ROWS 8  // Rows are doubled-up
-#define MATRIX_COLS 6
 
-#define DEBOUNCE 5
 
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
+
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-//sırayla yaz
-#define MATRIX_ROW_PINS \
-    { B3, B4, B5, B6, A2, A3, A4, A5 }
-#define MATRIX_COL_PINS \
-    { B12, B13, B14, B15, A8, A15 }
 
-//A VE B kanalı çok önemli değil, eğer yanlış bağlarsan bunları yer değiştir.
-#ifdef ENCODER_ENABLE
-#define ENCODERS_PAD_A \
-    { A1 }
-#define ENCODERS_PAD_B \
-    { A0 }
-#define ENCODER_RESOLUTION 4
-#endif
+
 
     //proje amacı 18 pin kullanmaktı. 14 pin satır-sütun kullanıyorum. 4 pin arttı.
     //1 encoder için 2 pin, böylece 2 pini led için kullanabilirim
