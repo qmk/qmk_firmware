a25dca48f686f0902c2d3466a7849e9161dc474c
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-03-15 06:07:47 +0300

    Update config.h

--- keyboards/ymdk/ymd09/config.h
+++ keyboards/ymdk/ymd09/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Kirill Andreev
+Copyright 2023 SHVD3x
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
