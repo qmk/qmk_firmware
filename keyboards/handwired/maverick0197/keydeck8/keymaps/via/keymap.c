6c4f5b02d30277dabcaec136789c2e23ae274e03
Maverick0197 <123402474+Maverick0197@users.noreply.github.com>
2023-03-15 15:43:52 -0500

    Update keymap.c

--- keyboards/handwired/maverick0197/keydeck8/keymaps/via/keymap.c
+++ keyboards/handwired/maverick0197/keydeck8/keymaps/via/keymap.c
@@ -15,6 +15,8 @@
  */
 #include QMK_KEYBOARD_H
 
+// clang-format off
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
     KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,
@@ -41,3 +43,5 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
         [3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
 };
 #endif // ENCODER_MAP_ENABLE
+
+// clang-format on
