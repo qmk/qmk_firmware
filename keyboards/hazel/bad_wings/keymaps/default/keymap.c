fbb8b0001175850eb7f9fdfccd06b90437192642
Jason Hazel <jhazel@matrixmediaservices.com>
2023-03-16 19:40:18 -0400

    clean up indentation

--- keyboards/hazel/bad_wings/keymaps/default/keymap.c
+++ keyboards/hazel/bad_wings/keymaps/default/keymap.c
@@ -4,47 +4,40 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-  _ALPHA,
-  _SYMBOL,
-  _NUMBER,
-  _NAVIGATION,
-  LAYER_LENGTH
+    _ALPHA,
+    _SYMBOL,
+    _NUMBER,
+    _NAVIGATION,
+    LAYER_LENGTH
 };
 
 
 enum tapdances {
-  TD_QESC,
-  TD_SBKT,
-  TD_CBKT,
-  TD_PARN,
-  TD_LTGT,
-  TD_ATAB,
-  TAPDANCE_LENGTH
+    TD_QESC,
+    TD_SBKT,
+    TD_CBKT,
+    TD_PARN,
+    TD_LTGT,
+    TD_ATAB,
+    TAPDANCE_LENGTH
 };
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SYMBOL, _NUMBER, _NAVIGATION);
+    return update_tri_layer_state(state, _SYMBOL, _NUMBER, _NAVIGATION);
 }
 
-// begin tapdances
 #define KC_QESC     TD(TD_QESC) // Q, ESC
 #define KC_SBKT     TD(TD_SBKT) // [, ]
 #define KC_CBKT     TD(TD_CBKT) // {, }
 #define KC_PARN     TD(TD_PARN) // (, )
 #define KC_LTGT     TD(TD_LTGT) // <, >
-
-// tap hold
-#define KC_GUIX     LGUI_T(KC_X) // X, GUI
-#define KC_ALTC     LALT_T(KC_C) // C, ALT
-
-// oneshot mods
+#define KC_GUIX     LGUI_T(KC_X)
+#define KC_ALTC     LALT_T(KC_C)
 #define KC_OSFT     OSM(MOD_LSFT)
 #define KC_OALT     OSM(MOD_LALT)
-
-// layer change
 #define KC_OSYM     OSL(_SYMBOL) 
-#define KC_ONUM     LT(_NUMBER, KC_BSPC) // _NUMBER on hold, BACKSPACE on tap
+#define KC_ONUM     LT(_NUMBER, KC_BSPC) 
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
@@ -56,7 +49,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-// tapdances
 tap_dance_action_t tap_dance_actions[] = {
     [TD_QESC]   = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
     [TD_SBKT]   = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
@@ -64,55 +56,30 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_PARN]   = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
     [TD_LTGT]   = ACTION_TAP_DANCE_DOUBLE(KC_LABK, KC_RABK),
 };
-// end tapdances
 
-// start combos
-enum combos {
-  COMBO_LENGTH
-};
-
-uint16_t COMBO_LEN = COMBO_LENGTH;
-// const uint16_t PROGMEM combo_navigation[] = { TRIGGER_ONE, TRIGGER_TWO, COMBO_END };
-combo_t key_combos[] = {
-  // [COMBO_NAME] = COMBO(combo_name, RESULT)
-};
-
-uint16_t get_combo_term(uint16_t index, combo_t *combo) {
-  switch(index) {
-    default:
-      return COMBO_TERM;
-  }
-}
-// end combos
-
-
-// begin layers
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_ALPHA] = LAYOUT(
-    KC_QESC,    KC_W,       KC_E,       KC_R,       KC_T,             KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,         \
-    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,             KC_H,       KC_J,       KC_K,       KC_L,       KC_QUOT,      \
-    KC_Z,       KC_GUIX,    KC_ALTC,    KC_V,       KC_B,             KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,      \
-                            KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
-  ),
-  [_SYMBOL] = LAYOUT(
-    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,          KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_PIPE,    KC_NO,        \
-    KC_GRV,     KC_TILD,    KC_UNDS,    KC_EQL,     KC_NO,            KC_SBKT,    KC_CBKT,    KC_PARN,    KC_LTGT,    KC_BACKSLASH, \
-    KC_NO,      KC_NO,      KC_PLUS,    KC_MINS,    KC_NO,            KC_NO,      KC_NO,      KC_COLN,    KC_DOT,     KC_SCLN,      \
-                            KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
-  ),
-  [_NUMBER] = LAYOUT(
-    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,        \
-    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,             KC_6,       KC_7,       KC_8,       KC_9,       KC_0,         \
-    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_NO,      KC_NO,      KC_DOT,     KC_NO,        \
-                            KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
-  ),
-  [_NAVIGATION] = LAYOUT(
-    KC_NO,      KC_F2,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_HOME,    KC_UP,      KC_END,     KC_BSPC,      \
-    KC_TAB,     KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_ENT,       \
-    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_DEL,       \
-                            KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT 
-  )
+    [_ALPHA] = LAYOUT(
+        KC_QESC,    KC_W,       KC_E,       KC_R,       KC_T,             KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,         \
+        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,             KC_H,       KC_J,       KC_K,       KC_L,       KC_QUOT,      \
+        KC_Z,       KC_GUIX,    KC_ALTC,    KC_V,       KC_B,             KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,      \
+                                KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
+    ),
+    [_SYMBOL] = LAYOUT(
+        KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,          KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_PIPE,    KC_NO,        \
+        KC_GRV,     KC_TILD,    KC_UNDS,    KC_EQL,     KC_NO,            KC_SBKT,    KC_CBKT,    KC_PARN,    KC_LTGT,    KC_BACKSLASH, \
+        KC_NO,      KC_NO,      KC_PLUS,    KC_MINS,    KC_NO,            KC_NO,      KC_NO,      KC_COLN,    KC_DOT,     KC_SCLN,      \
+                                KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
+    ),
+    [_NUMBER] = LAYOUT(
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,        \
+        KC_1,       KC_2,       KC_3,       KC_4,       KC_5,             KC_6,       KC_7,       KC_8,       KC_9,       KC_0,         \
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_NO,      KC_NO,      KC_DOT,     KC_NO,        \
+                                KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
+    ),
+    [_NAVIGATION] = LAYOUT(
+        KC_NO,      KC_F2,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_HOME,    KC_UP,      KC_END,     KC_BSPC,      \
+        KC_TAB,     KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_ENT,       \
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_DEL,       \
+                                KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT 
+    )
 };
-
-
-
