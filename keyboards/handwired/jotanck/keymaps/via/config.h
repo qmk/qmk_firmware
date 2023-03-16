bf749a37d4c6a0a10cb24859ba345647ebc59f83
jotix <jujodeve@gmail.com>
2023-03-16 08:11:16 -0300

    add suport to via firmware

---
+++ keyboards/handwired/jotanck/keymaps/via/config.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Configuration for Vial 
+#define VIAL_KEYBOARD_UID {0xC7, 0xF7, 0x32, 0x54, 0xCB, 0x4B, 0x52, 0x89}
+
+// to unlock the keyboard
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+
+// number of layers
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
