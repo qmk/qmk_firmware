2c607d1605635684cd364bc55c50c78175dfad96
jotix <jujodeve@gmail.com>
2023-03-16 08:36:08 -0300

    add VIA firmware support

--- keyboards/handwired/jotanck/rules.mk
+++ keyboards/handwired/jotanck/rules.mk
@@ -1,7 +1,7 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
@@ -10,3 +10,4 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+VIA_ENABLE = yes            # Enable VIA firmware
