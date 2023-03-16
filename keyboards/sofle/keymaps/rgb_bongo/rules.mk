b3ef95b4762acf586ecb0ff0e590ab8f11f3a7f4
James Peng <james_peng@apple.com>
2023-03-15 21:34:19 -0700

    bring in keymap for bongo cat

---
+++ keyboards/sofle/keymaps/rgb_bongo/rules.mk
@@ -0,0 +1,17 @@
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
+CONSOLE_ENABLE = no
+RGBLIGHT_ENABLE = yes
+ENCODER_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+WPM_ENABLE = yes
+COMMAND_ENABLE = no
+EXTRAFLAGS += -flto
+
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+NKRO_ENABLE = no            # USB Nkey Rollover
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
+# NO_USB_STARTUP_CHECK = no
