46f10c22a488cd5008fabfac9ade06e23023114f
Sam Gowland <10119462+SamGowland@users.noreply.github.com>
2023-03-17 16:38:59 +0000

    update to info.json

--- keyboards/tapedeck/config.h
+++ keyboards/tapedeck/config.h
@@ -15,33 +15,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Pin-out */
-#define MATRIX_ROW_PINS { B2, B3, B1 }
-#define MATRIX_COL_PINS { B5, B4, E6, D7, C6 }
-#define QMK_LED B0
-
-#define QMK_ESC_OUTPUT B2
-#define QMK_ESC_INPUT B5
-
-/* ROW2COL */
-#define DIODE_DIRECTION ROW2COL
-
-/* RGB */
-#define RGB_DI_PIN B6
-#define RGBLED_NUM 3
-#define RGBLIGHT_EFFECT_STATIC_LIGHT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_LIMIT_VAL 225
-
-/* Rotary Encoder*/
-#define ENCODERS_PAD_A { F4, F6 }
-#define ENCODERS_PAD_B { F5, F7 }
-#define ENCODER_RESOLUTION 4
-
 /* SLIDER */
 #define POT_ENABLE
 
