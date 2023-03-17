0ef8e4b029afa4495f4d54569cd41171dac20ad6
Sam Gowland <10119462+SamGowland@users.noreply.github.com>
2023-03-17 16:27:06 +0000

    Update keyboards/tapedeck/tapedeck.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/tapedeck/tapedeck.c
+++ keyboards/tapedeck/tapedeck.c
@@ -14,4 +14,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "tapedeck.h"
+#include "quantum.h"
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_WH_U);
+        } else {
+            tap_code(KC_WH_D);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_BRIU);
+        } else {
+            tap_code(KC_BRID);
+        }
+    }
+    return true;
+}
