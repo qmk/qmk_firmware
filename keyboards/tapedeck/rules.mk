49bee459aad849f01674629b8a2e39ef4bb94b0d
Sam Gowland <10119462+SamGowland@users.noreply.github.com>
2023-03-17 23:05:33 +0000

    Update keyboards/tapedeck/rules.mk
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/tapedeck/rules.mk
+++ keyboards/tapedeck/rules.mk
@@ -1,12 +1,4 @@
 # Build Options
 #   change yes to no to disable
 #
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-ENCODER_ENABLE = yes		# Enable Rotary Encoder
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # USB Nkey Rollover
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 SRC += analog.c
-BOOTMAGIC_ENABLE = yes
