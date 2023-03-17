986d3dfe2c30d1526d8da15c10182563fac695d1
Sam Gowland <10119462+SamGowland@users.noreply.github.com>
2023-03-17 16:22:42 +0000

    Update keyboards/tapedeck/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/tapedeck/rules.mk
+++ keyboards/tapedeck/rules.mk
@@ -17,18 +17,9 @@ BOOTLOADER = qmk-dfu
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 ENCODER_ENABLE = yes		# Enable Rotary Encoder
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-MIDI_ENABLE = no            # MIDI support
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no           # Audio output
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 SRC += analog.c
-CONSOLE_ENABLE = yes
 BOOTMAGIC_ENABLE = yes
