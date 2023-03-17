86262571ee7d952d99df1da78c9649ff85af7f12
王宁 <vipning@qq.com>
2023-03-17 15:50:07 +0800

    add rgb definition

--- keyboards/ning/tiny_board/tb16_rgb/rgb.c
+++ keyboards/ning/tiny_board/tb16_rgb/rgb.c
@@ -14,15 +14,18 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#include "quantum.h"
 
-#define RGB_DI_PIN B5
-#define RGB_MATRIX_LED_COUNT 16
-
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_TYPING_HEATMAP
-#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
-
-
+led_config_t g_led_config = { {
+  { 0, 1, 2, 3 },
+  { 4, 5, 6, 7 },
+  { 8, 9, 10, 11 },
+  { 12, 13, 14, 15 }
+}, {
+  {0,0},{75,0},{150,0},{224,0},
+  {0,21},{75,21},{150,21},{224,21},
+  {0,42},{75,42},{150,42},{224,42},
+  {0,64},{75,64},{150,64},{224,64}
+}, {
+  4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
+} };
