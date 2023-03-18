22d829087d9de4c8be55a7d05d5defd8333e6511
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-03-18 20:48:08 +0300

    Update kd83a_bfg_edition.c

--- keyboards/darkproject/kd83a_bfg_edition/kd83a_bfg_edition.c
+++ keyboards/darkproject/kd83a_bfg_edition/kd83a_bfg_edition.c
@@ -163,34 +163,3 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     return true;
 }
 #endif
-
-
-// Credits to Michael Stapelberg https://github.com/stapelberg
-// delay_inline sleeps for |cycles| (e.g. sleeping for F_CPU will sleep 1s).
-// delay_inline assumes the cycle counter has already been initialized and
-// should not be modified, i.e. it is safe to call during keyboard matrix scan.
-//
-// ChibiOS enables the cycle counter in chcore_v7m.c:
-// https://github.com/ChibiOS/ChibiOS/blob/b63023915c304092acb9f33bbab40f3ec07a7f0e/os/common/ports/ARMCMx/chcore_v7m.c#L263
-static void delay_inline(const uint32_t cycles) {
-    const uint32_t start = DWT->CYCCNT;
-    while ((DWT->CYCCNT - start) < cycles) {
-        // busy-loop until time has passed
-    }
-}
-
-void matrix_output_unselect_delay(uint8_t line, bool key_pressed) {
-    // Use the cycle counter to do precise timing in microseconds. The ChibiOS
-    // thread sleep functions only allow sleep durations starting at 1 tick, which
-    // is 100μs in our configuration.
-
-    // Empirically: e.g. 5μs is not enough, will result in keys that don’t work
-    // and ghost key presses. 10μs seems to work well.
-
-    // On some variants of the hardware, 20μs seems to be required. This was found
-    // on a combination of KB600LF+stapelberg v2020-06-30+teensy41.
-
-    // 600 cycles at 0.6 cycles/ns == 1μs
-    const uint32_t cycles_per_us = 96;
-    delay_inline(3 * cycles_per_us); // min working for KD83A is 3
-}
