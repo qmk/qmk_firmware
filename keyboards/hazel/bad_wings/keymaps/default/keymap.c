899d7b312dd74c91c7caafea325ead8db846f8e8
Jason Hazel <me@jasonhazel.com>
2023-03-18 12:15:29 -0400

    Update keyboards/hazel/bad_wings/keymaps/default/keymap.c
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/hazel/bad_wings/keymaps/default/keymap.c
+++ keyboards/hazel/bad_wings/keymaps/default/keymap.c
@@ -58,7 +58,7 @@ tap_dance_action_t tap_dance_actions[] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_ALPHA] = LAYOUT(
+    [_ALPHA] = LAYOUT_split_3x5_3(
         KC_QESC,    KC_W,       KC_E,       KC_R,       KC_T,             KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,         \
         KC_A,       KC_S,       KC_D,       KC_F,       KC_G,             KC_H,       KC_J,       KC_K,       KC_L,       KC_QUOT,      \
         KC_Z,       KC_GUIX,    KC_ALTC,    KC_V,       KC_B,             KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,      \
