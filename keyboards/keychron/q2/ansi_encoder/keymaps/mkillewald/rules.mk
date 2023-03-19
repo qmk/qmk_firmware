0b487e587f243a5c018a4e25811cb66df721df53
Mike Killewald <mkillewald@yahoo.com>
2023-03-19 11:00:33 -0400

    initial commit

---
+++ keyboards/keychron/q2/ansi_encoder/keymaps/mkillewald/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+SRC += keymap_user_config.c
+
+ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
+    SRC += rgb_matrix_user.c
+endif
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
