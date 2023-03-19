962659d28aa9188a2e68571c41eb9912aedff53b
SimonV <simon.varvat@gmail.com>
2023-03-19 22:04:49 +0100

    #HOME - Création keymap hhkb part 54

--- keyboards/hhkb/ansi/keymaps/svarvat/layers_macros.c
+++ keyboards/hhkb/ansi/keymaps/svarvat/layers_macros.c
@@ -771,7 +771,6 @@ bool processKeycodeIfLThumbMs(uint16_t keycode, keyrecord_t* record) {
         case MA_LTHUMBMS:
             if (!(record->event.pressed)) {
                 layer_off(LA_LTHUMBMS);
-                isScrollX1Started = false;
                 isMouseX2Started = false;
                 isScrollX2Started = false;
                 isMouseX4Started = false;
