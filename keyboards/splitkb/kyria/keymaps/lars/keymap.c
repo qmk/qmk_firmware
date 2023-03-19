37b4e279552641266c461c034e5104910e7125a5
Lars Grahmann <lars.grahmann@virtualminds.com>
2023-03-19 19:37:25 +0100

    remove unecessary information

--- keyboards/splitkb/kyria/keymaps/lars/keymap.c
+++ keyboards/splitkb/kyria/keymaps/lars/keymap.c
@@ -16,7 +16,6 @@
 #include QMK_KEYBOARD_H
 #include "keymap_german.h"
 #include "os_detection.h"
-
 enum layers {
     _COLEMAK_DHk = 0,
     _NAV,
@@ -212,26 +211,6 @@ bool oled_task_user(void) {
                 break;
             default:
                 oled_write_P(PSTR("Undefined\n"), false);
-        }
-
-        oled_write_P(PSTR("\nRunning on Host:\n"), false);
-
-        switch (detected_host_os()) {
-            case OS_UNSURE:
-                oled_write_P(PSTR("Not sure\n"), false);
-                break;
-            case OS_WINDOWS:
-                oled_write_P(PSTR("Windows... ew\n"), false);
-                break;
-            case OS_MACOS:
-                oled_write_P(PSTR("MacOS\n"), false);
-                break;
-            case OS_IOS:
-                oled_write_P(PSTR("IOS\n"), false);
-                break;
-            case OS_LINUX:
-                oled_write_P(PSTR("Linux\n"), false);
-                break;
         }
     } else {
 oled_write_P(PSTR("\nRunning on Host:\n"), false);
