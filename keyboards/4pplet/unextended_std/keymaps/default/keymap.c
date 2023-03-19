7dde5c97e2c20d6572eeceb8752d056c23adba43
4pplet <mail@4pplet.com>
2023-03-19 23:58:45 +0100

    Update keymap.c

--- keyboards/4pplet/unextended_std/keymaps/default/keymap.c
+++ keyboards/4pplet/unextended_std/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Stefan Sundin "4pplet" <4pplet@protonmail.com>
+Copyright 2023 Stefan Sundin "4pplet" <4pplet@protonmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
