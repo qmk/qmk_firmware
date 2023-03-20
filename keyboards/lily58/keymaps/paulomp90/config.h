00c55c7661a2c783ff026b2627ee9490e74eb206
Paulo Pereira <paulo.pereira@issuu.com>
2023-03-20 14:55:41 +0000

    improvements

--- keyboards/lily58/keymaps/paulomp90/config.h
+++ keyboards/lily58/keymaps/paulomp90/config.h
@@ -46,6 +46,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
 
+#define TAP_CODE_DELAY 10
+
 // Underglow
 /*
 #undef RGBLED_NUM
--- keyboards/lily58/keymaps/paulomp90/rules.mk
+++ keyboards/lily58/keymaps/paulomp90/rules.mk
@@ -1,6 +1,6 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no
