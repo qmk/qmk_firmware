07740baec582c65bf359d3ce134e289f3ddc835b
Will Brookes <will.brookes@planetinnovation.com.au>
2023-03-20 15:54:43 +1100

    Add mousekeys
    * Use by holding space, then LCTRL
    * z = left click, x = right, c = middle
    * Adds config.h file

--- keyboards/dztech/dz60rgb_ansi/keymaps/brookesy66/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/brookesy66/keymap.c
@@ -5,6 +5,7 @@
 #define _ML 1 // modify/movement layer
 #define _UL 2 // utility layer (fn keys & RGB)
 #define _GL 3 // gaming layer (act like a pleb keeb)
+#define _PL 4 // mouse (pointer) layer
 #define SPC_ML LT(_ML, KC_SPC) // tap for space, hold for modify/movement layer
 
 // Tap Dance declarations
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 //          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
 //          _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-//          _______, _______, _______, _______,                            _______,                            _______, _______, _______,
+//          _______,          _______, _______,                            _______,                            _______, _______, _______,
 
     [_BL] = LAYOUT_60_ansi(
         QK_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,        _______, _______, _______, _______, _______, _______, KC_PGUP, KC_INS,  _______, _______, _______,  _______, _______,
         _______,        _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,           _______,
         KC_LSFT,                 KC_Z,    KC_DEL,  KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           _______,
-        _______,        _______, _______,                            _______,                            _______, _______,  _______, _______
+        MO(_PL),        _______, _______,                            _______,                            _______, _______,  _______, _______
     ),
     [_UL] = LAYOUT_60_ansi(
         KC_GRV,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,
@@ -69,5 +70,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
         KC_LSPO,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSPC,
         KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, TD(TD_G), MO(_UL), KC_RCTL
-    )
+    ),
+    [_PL] = LAYOUT_60_ansi(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,          _______,
+        _______,          KC_BTN1, KC_BTN2, KC_BTN3, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______,          _______, _______,                            _______,                            _______, _______, _______,
+    ),
 };
