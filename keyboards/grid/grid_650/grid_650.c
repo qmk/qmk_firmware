ca3e7d05bfbaa478c4be76fc4405035c33484df4
gridishere <47846103+gridishere@users.noreply.github.com>
2023-03-22 03:43:51 +0800

    Update keyboards/grid/grid_650/grid_650.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/grid/grid_650/grid_650.c
+++ keyboards/grid/grid_650/grid_650.c
@@ -555,12 +555,6 @@ void housekeeping_task_kb(void) {
     matrix_scan_user();
 }
 
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    // put your per-action keyboard code here
-    // runs for every action, just before processing by the firmware
-
-    return process_record_user(keycode, record);
-}
 
 void led_set_user(uint8_t usb_led) {
   #ifndef BLE_ENABLE
