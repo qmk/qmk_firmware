e9b64b083f2dd0db9fce09c8e6215f0e8c0de597
Alejandro Chacón <alhenx@gmail.com>
2023-03-21 11:45:22 +0100

    Update keymap.c

--- keyboards/alhenkb/macropad5x4/keymaps/via/keymap.c
+++ keyboards/alhenkb/macropad5x4/keymaps/via/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ Z │ X │ C │ V │
      * └───────┴───┴───┘
      */
-    [0] = LAYOUT(
+    [0] = LAYOUT_ortho_5x4(
         KC_P1,   KC_P2,   KC_P3,   KC_P4,
         KC_P5,   KC_P6,   KC_P7,   KC_P8,
         KC_Q,    KC_W,    KC_E,    KC_R,
