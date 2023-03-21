3f3dbaaa27378e011cb5cf0e43d1269baff7e731
Tomohisa Takaoka <ttakaoka@jtechs.com>
2023-03-21 00:12:02 -0700

    とりあえずビルドが通るようにする

--- keyboards/handwired/avocado/config.h
+++ keyboards/handwired/avocado/config.h
@@ -17,14 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
-#define DEVICE_VER   0x0001
-#define MANUFACTURER JTech Creations Inc
-#define PRODUCT      avocado
 
 /* key matrix size */
 #define MATRIX_ROWS 3
@@ -47,7 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define UNUSED_PINS
 #define MATRIX_ROW_PINS { B4, B5, B6 }
 #define MATRIX_COL_PINS { E6, D7, C6, D4, D0 }
-#define UNUSED_PINS { D1, F6, F4, F5, F6 }
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -155,7 +147,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 13
-#define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
