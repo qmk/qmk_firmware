0bb9eada83d75f967d29a3381315ea0dcab22cdf
spbgzh <72414103+spbgzh@users.noreply.github.com>
2023-03-22 03:07:47 +0800

    [Keyboard] update creek70 change rgb num (#20196)
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/wuque/creek70/config.h
+++ keyboards/wuque/creek70/config.h
@@ -18,14 +18,20 @@
 
 #define RGB_DI_PIN D0
 #ifdef RGB_DI_PIN
-    #define RGBLED_NUM 1
-    #define RGBLIGHT_HUE_STEP 8
-    #define RGBLIGHT_SAT_STEP 8
-    #define RGBLIGHT_VAL_STEP 8
-    #define RGBLIGHT_LIMIT_VAL 200 /* The maximum brightness level */
-    #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-/*======= RGB function=======*/
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLED_NUM 7
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_LIMIT_VAL 200 /* The maximum brightness level */
+#    define RGBLIGHT_SLEEP
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
