8a7ad6f263bc872d004d4441b6ca7b9f937aba20
butterkeebs <124812866+butterkeebs@users.noreply.github.com>
2023-03-22 18:21:24 +0000

    Update keyboards/ButterKeebs/PocketPad/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/ButterKeebs/PocketPad/keymaps/default/keymap.c
+++ keyboards/ButterKeebs/PocketPad/keymaps/default/keymap.c
@@ -1,6 +1,6 @@
 // ButterKeebs <butterkeebs@github>
 // SPDX-License-Identifier: GPL-2.0+
-#include "QMK_KEYBOARD_H"
+#include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
