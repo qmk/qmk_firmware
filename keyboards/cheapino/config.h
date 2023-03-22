2fa3131f76ca4828667cde43ea95f04c14cd55a5
Thomas Haukland <thomas.haukland@gmail.com>
2023-03-22 22:47:54 +0100

    The cheapino works, except for the encoder

--- keyboards/cheapino/config.h
+++ keyboards/cheapino/config.h
@@ -18,3 +18,12 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 230
+// #define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+//#define RETRO_TAPPING
--- keyboards/cheapino/rules.mk
+++ keyboards/cheapino/rules.mk
@@ -1,1 +1,2 @@
-# This file intentionally left blank
+CUSTOM_MATRIX = lite
+SRC +=	matrix.c
