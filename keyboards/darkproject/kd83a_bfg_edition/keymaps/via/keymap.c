3dec857224ffa397b3fa30898de725eaa2c4d107
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-03-22 19:12:30 +0300

    Update keymap.c

--- keyboards/darkproject/kd83a_bfg_edition/keymaps/via/keymap.c
+++ keyboards/darkproject/kd83a_bfg_edition/keymaps/via/keymap.c
@@ -1,4 +1,5 @@
 /* Copyright 2022 GSKY <gskyGit@gsky.com.tw>
+ * Copyright 2023 SHVD3x
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
