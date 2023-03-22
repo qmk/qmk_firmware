813357d96c931fbff32bc76152e903da07aa0ddd
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-03-22 19:14:02 +0300

    Update keymap.c

--- keyboards/darkproject/kd83a_bfg_edition/keymaps/gaming_by_shvd3x/keymap.c
+++ keyboards/darkproject/kd83a_bfg_edition/keymaps/gaming_by_shvd3x/keymap.c
@@ -1,19 +1,19 @@
-/*
-Copyright 2023 SHVD3x
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* Copyright 2022 GSKY <gskyGit@gsky.com.tw>
+ * Copyright 2023 SHVD3x
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // !!! If you experience unintended doublepresses and it bothers you then you can go to rules.mk and change DEBOUNCE_TYPE value to asym_eager_defer_pk or sym_defer_pk
 // Debounce algorithms responsiveness simplified: 
