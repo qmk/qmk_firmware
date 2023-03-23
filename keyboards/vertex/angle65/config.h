1c8e79908a9611254bd3dd1e5a874aa8490aeac0
Vertex-kb <102476474+Vertex-kb@users.noreply.github.com>
2023-03-23 09:57:30 +0800

    Add files via upload

--- keyboards/vertex/angle65/config.h
+++ keyboards/vertex/angle65/config.h
@@ -15,8 +15,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#define MATRIX_COL_PINS { B9, B8, B7, B6, B5, B4, B3, B11, A15, A10, A9, B14, B13, B12, A5 }
-#define MATRIX_ROW_PINS { B10, B1, B0, A7, A6 }
 
 #define SOLENOID_PIN A2
 
