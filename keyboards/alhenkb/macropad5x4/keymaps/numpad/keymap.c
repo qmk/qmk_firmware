3cd48e463efe0fa79ff010d71fce473b9ef7be9e
Alejandro Chacón <alhenx@gmail.com>
2023-03-23 11:44:00 +0100

    Update keymap.c

--- keyboards/alhenkb/macropad5x4/keymaps/numpad/keymap.c
+++ keyboards/alhenkb/macropad5x4/keymaps/numpad/keymap.c
@@ -17,12 +17,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │   0   │ . │   │
      * └───────┴───┴───┘
      */
-    [0] = LAYOUT_num4x5(
+    [0] = LAYOUT_numpad_5x4(
         TG(1),   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,   
-        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,     KC_PDOT
+        KC_P7,   KC_P8,   KC_P9,   
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,   
+        KC_P0,     KC_PDOT,        KC_PENT
     ),
 
     /*
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Insert │Del│   │
      * └───────┴───┘───┘
      */
-    [1] = LAYOUT_num4x5(
+    [1] = LAYOUT_numpad_5x4(
         _______, _______, _______, _______,
-        KC_HOME, KC_UP,   KC_PGUP, _______,
-        KC_LEFT, XXXXXXX, KC_RGHT,
-        KC_END,  KC_DOWN, KC_PGDN,  _______,
-        KC_INS,           KC_DEL
+        KC_HOME, KC_UP,   KC_PGUP, 
+        KC_LEFT, XXXXXXX, KC_RGHT, _______,
+        KC_END,  KC_DOWN, KC_PGDN,  
+        KC_INS,           KC_DEL,  _______
     )
 };
