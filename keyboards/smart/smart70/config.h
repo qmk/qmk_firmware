46ed9f1f55709524b112ceb18f6ff215a61d5fda
JackyJia73 <128167503+JackyJia73@users.noreply.github.com>
2023-03-24 17:26:54 +0800

    UPDATE BOOT

--- keyboards/smart/smart70/config.h
+++ keyboards/smart/smart70/config.h
@@ -14,9 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 #pragma once
+#define LED_PIN_ON_STATE 1
 
+#define DEBUG_MATRIX_SCAN_RATE
 #define USB_POLLING_INTERVAL_MS 1
-
-#define FORCE_NKRO
-
--- keyboards/smart/smart70/rules.mk
+++ keyboards/smart/smart70/rules.mk
@@ -1,13 +1,7 @@
 # Build Options
 # change yes to no to disable
 #
-MCU_LDSCRIPT = Smartbootloader
+MCU_LDSCRIPT = smartbootloader
 
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes            # Enable N-Key Rollover
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 
 
