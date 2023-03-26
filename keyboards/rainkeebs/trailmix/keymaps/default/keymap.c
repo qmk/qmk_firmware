f973e2837477a30d6315e1667a07ddb45c807fae
Alberto Pavano <38161192+ATron789@users.noreply.github.com>
2023-03-26 20:48:11 +0100

    Update keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
+++ keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
@@ -57,16 +57,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
-
 bool encoder_update_user(uint8_t index, bool clockwise) {
 
 
