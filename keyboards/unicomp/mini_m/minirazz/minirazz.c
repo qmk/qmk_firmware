48cc2c26f9501141c7153ea981d8eada6816e0f2
Purdea Andrei <andrei@purdea.ro>
2023-03-26 02:20:20 +0200

    Update keyboards/unicomp/mini_m/minirazz/minirazz.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/unicomp/mini_m/minirazz/minirazz.c
+++ keyboards/unicomp/mini_m/minirazz/minirazz.c
@@ -1,13 +1,3 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "minirazz.h"
-void keyboard_post_init_kb(void) {
-/*    setPinOutput(GP16);
-    writePinHigh(GP16);
-    setPinOutput(GP17);
-    writePinHigh(GP17);
-    writePinLow(GP21);
-    setPinOutput(GP21);
-    
-*/
-}
