32c033d2c15df2d9c265e834d537789a177f0953
Alberto Pavano <38161192+ATron789@users.noreply.github.com>
2023-03-26 20:48:03 +0100

    Update keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
+++ keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "trailmix.h"
+#include QMK_KEYBOARD_H
 
 enum layers {
   _BASE,
