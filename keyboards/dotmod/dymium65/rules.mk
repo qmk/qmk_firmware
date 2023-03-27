c002a9291b1b1e401b9335311765d86dbb1a1915
尚明 <110142291+shangmingzhang@users.noreply.github.com>
2023-03-27 14:03:26 +0800

    Update rules.mk

--- keyboards/dotmod/dymium65/rules.mk
+++ keyboards/dotmod/dymium65/rules.mk
@@ -1,13 +1,4 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-MOUSEKEY_ENABLE = yes       # Mouse keys
-ENCODER_ENABLE = yes	    # Enable ENCODER
-RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
-RGB_MATRIX_DRIVER = WS2812  # RGB matrix driver support
-LTO_ENABLE = yes
+ENCODER_MAP_ENABLE = yes    # Enable ENCODER MAP
