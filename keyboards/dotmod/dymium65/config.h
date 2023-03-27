4b0ef9d507a217a2cadd113755949302cbe4c7b5
尚明 <110142291+shangmingzhang@users.noreply.github.com>
2023-03-27 13:44:34 +0800

    Update config.h

--- keyboards/dotmod/dymium65/config.h
+++ keyboards/dotmod/dymium65/config.h
@@ -1,31 +1,49 @@
-// Copyright 2023 <felix@fjlaboratories.com>
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright 2023 Finalkey
+ * Copyright 2023 LiWenLiu <https://github.com/LiuLiuQMK>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
-
-#define MATRIX_ROW_PINS { F7, F6, F5, F4, F1, F0}
-#define MATRIX_COL_PINS { E6, B0, B1, B2, B3, B7, D2, D3, D5, D4, D6, C6, B4, B5, B6, E2}
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
+/* Define less important options */
+
+
 #define FORCE_NKRO
+/*
+ * Feature disable options
+ * These options are also useful to firmware size reduction.
+ */
 
-#define NO_ACTION_ONESHOT
+/* disable debug print */
+#define NO_DEBUG
 
+/* disable print */
+#define NO_PRINT
+
+// The pin connected to the data pin of the LEDs
 #define RGB_DI_PIN D7
+// The number of LEDs connected
 #define RGB_MATRIX_LED_COUNT 66
 #define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_KEYRELEASES
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_DISABLE_AFTER_TIMEOUT 0
+#define RGB_MATRIX_LED_FLUSH_LIMIT 16
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
-#define ENABLE_RGB_MATRIX_BREATHING	            
-#define ENABLE_RGB_MATRIX_CYCLE_ALL             
-#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT      
-#define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN           
-#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL       
-#define ENABLE_RGB_MATRIX_CYCLE_SPIRAL            
-#define ENABLE_RGB_MATRIX_PIXEL_FLOW              
-#define ENABLE_RGB_MATRIX_SOLID_REACTIVE	         
-#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE	 
-#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS	     
