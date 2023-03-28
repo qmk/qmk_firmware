fbed1502636334ca4be673d20c6cf4128d7aa140
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-03-28 17:13:14 +0300

    Update matrix.c

--- keyboards/darkproject/kd83a_ltd/matrix.c
+++ keyboards/darkproject/kd83a_ltd/matrix.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 SHVD3x
+Copyright 2019 SHVD3x
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
@@ -34,23 +34,20 @@ static inline matrix_row_t read_cols(void) {
 
 static void unselect_rows(void) {
     ATOMIC_BLOCK_FORCEON {
-    pal_lld_setgroupmode(PAL_PORT(B0),  PAL_PORT_BIT(PAL_PAD(B0)) | 
-                                        PAL_PORT_BIT(PAL_PAD(B1)) |
-                                        PAL_PORT_BIT(PAL_PAD(B2)) |
-                                        PAL_PORT_BIT(PAL_PAD(B3)) |
-                                        PAL_PORT_BIT(PAL_PAD(B4)) |
-                                        PAL_PORT_BIT(PAL_PAD(B5)) |
-                                        PAL_PORT_BIT(PAL_PAD(B6)) |
-                                        PAL_PORT_BIT(PAL_PAD(B7)) |
-                                        PAL_PORT_BIT(PAL_PAD(B8)) |
-                                        PAL_PORT_BIT(PAL_PAD(B9)) |
-                                        PAL_PORT_BIT(PAL_PAD(B10)) |
-                                        PAL_PORT_BIT(PAL_PAD(B12)), 
-                                        0U, 
-                                        (PAL_WB32_MODE_INPUT | 
-                                        PAL_WB32_PUPDR_PULLUP | 
-                                        PAL_WB32_OSPEED_HIGH));
+    pal_lld_setport(PAL_PORT(B0),   PAL_PORT_BIT(PAL_PAD(B0)) | 
+                                    PAL_PORT_BIT(PAL_PAD(B1)) |
+                                    PAL_PORT_BIT(PAL_PAD(B2)) |
+                                    PAL_PORT_BIT(PAL_PAD(B3)) |
+                                    PAL_PORT_BIT(PAL_PAD(B4)) |
+                                    PAL_PORT_BIT(PAL_PAD(B5)) |
+                                    PAL_PORT_BIT(PAL_PAD(B6)) |
+                                    PAL_PORT_BIT(PAL_PAD(B7)) |
+                                    PAL_PORT_BIT(PAL_PAD(B8)) |
+                                    PAL_PORT_BIT(PAL_PAD(B9)) |
+                                    PAL_PORT_BIT(PAL_PAD(B10)) |
+                                    PAL_PORT_BIT(PAL_PAD(B12)));
     }
+    
 }
 
 
@@ -58,124 +55,57 @@ static void select_row(uint8_t row) {
     switch (row) {
         case 0:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B0),
-                                PAL_PORT_BIT(PAL_PAD(B0)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B0), PAL_PORT_BIT(PAL_PAD(B0)));
             }
             break;
         case 1:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B1),
-                                PAL_PORT_BIT(PAL_PAD(B1)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B1), PAL_PORT_BIT(PAL_PAD(B1)));
             }
             break;
         case 2:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B2),
-                                PAL_PORT_BIT(PAL_PAD(B2)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B2), PAL_PORT_BIT(PAL_PAD(B2)));
             }
             break;
         case 3:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B3),
-                                PAL_PORT_BIT(PAL_PAD(B3)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B3), PAL_PORT_BIT(PAL_PAD(B3)));
             }
             break;
         case 4:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B4),
-                                PAL_PORT_BIT(PAL_PAD(B4)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B4), PAL_PORT_BIT(PAL_PAD(B4)));
             }
             break;
         case 5:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B5),
-                                PAL_PORT_BIT(PAL_PAD(B5)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B5), PAL_PORT_BIT(PAL_PAD(B5)));
             }
             break;
         case 6:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B6),
-                                PAL_PORT_BIT(PAL_PAD(B6)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B6), PAL_PORT_BIT(PAL_PAD(B6)));
             }
             break;
         case 7:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B7),
-                                PAL_PORT_BIT(PAL_PAD(B7)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B7), PAL_PORT_BIT(PAL_PAD(B7)));
             }
             break;
         case 8:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B8),
-                                PAL_PORT_BIT(PAL_PAD(B8)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B8), PAL_PORT_BIT(PAL_PAD(B8)));
             }
             break;
         case 9: // Pin B12 was removed from matrix definition. This row can be scanned during scan of B9 row
             ATOMIC_BLOCK_FORCEON {
-                pal_lld_setgroupmode(PAL_PORT(B9),
-                                    PAL_PORT_BIT(PAL_PAD(B9)) | 
-                                    PAL_PORT_BIT(PAL_PAD(B12)),
-                                    0U,
-                                    (PAL_WB32_MODE_OUTPUT |
-                                    PAL_WB32_OTYPE_PUSHPULL | 
-                                    PAL_WB32_OSPEED_HIGH));
                 pal_lld_clearport(PAL_PORT(B9), PAL_PORT_BIT(PAL_PAD(B9)) | 
                                                 PAL_PORT_BIT(PAL_PAD(B12)));
             }
             break;
         case 10:
             ATOMIC_BLOCK_FORCEON {
-            pal_lld_setgroupmode(PAL_PORT(B10),
-                                PAL_PORT_BIT(PAL_PAD(B10)),
-                                0U,
-                                (PAL_WB32_MODE_OUTPUT |
-                                PAL_WB32_OTYPE_PUSHPULL | 
-                                PAL_WB32_OSPEED_HIGH));
             pal_lld_clearport(PAL_PORT(B10), PAL_PORT_BIT(PAL_PAD(B10)));
             }
             break;
@@ -187,6 +117,23 @@ static void select_row(uint8_t row) {
 void matrix_init_custom(void) {
     // initialize key pins
     ATOMIC_BLOCK_FORCEON {
+        pal_lld_setgroupmode(PAL_PORT(B0),  PAL_PORT_BIT(PAL_PAD(B0)) | 
+                                            PAL_PORT_BIT(PAL_PAD(B1)) |
+                                            PAL_PORT_BIT(PAL_PAD(B2)) |
+                                            PAL_PORT_BIT(PAL_PAD(B3)) |
+                                            PAL_PORT_BIT(PAL_PAD(B4)) |
+                                            PAL_PORT_BIT(PAL_PAD(B5)) |
+                                            PAL_PORT_BIT(PAL_PAD(B6)) |
+                                            PAL_PORT_BIT(PAL_PAD(B7)) |
+                                            PAL_PORT_BIT(PAL_PAD(B8)) |
+                                            PAL_PORT_BIT(PAL_PAD(B9)) |
+                                            PAL_PORT_BIT(PAL_PAD(B10)) |
+                                            PAL_PORT_BIT(PAL_PAD(B12)), 
+                                            0U, 
+                                            (PAL_WB32_MODE_OUTPUT |
+                                            PAL_WB32_OTYPE_PUSHPULL | 
+                                            PAL_WB32_OSPEED_HIGH | 
+                                            PAL_WB32_CURRENT_LEVEL3));
         pal_lld_setgroupmode(PAL_PORT(A0),  PAL_PORT_BIT(PAL_PAD(A0)) | 
                                             PAL_PORT_BIT(PAL_PAD(A1)) |
                                             PAL_PORT_BIT(PAL_PAD(A2)) |
@@ -207,11 +154,10 @@ uint8_t matrix_scan_custom(matrix_row_t current_matrix[]) {
     bool changed = false;
 
     // Set col, read rows
-    //for (uint_fast8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
     for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
         select_row(current_row);
         matrix_output_select_delay();
-
+        
         matrix_row_t cols = read_cols();
 
         unselect_rows();
