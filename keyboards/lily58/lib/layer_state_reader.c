5b5c6e285d34f866b4b79cee3e8224291d1ee3e7
jay_adams <blurryllama@gmail.com>
2023-03-28 16:19:19 -0400

    merge issues

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -4,7 +4,10 @@
 #include "lily58.h"
 
 #define L_BASE 0
-#define L_FN (1 << 1)
+#define L_LOWER (1 << 1)
+#define L_RAISE (1 << 2)
+#define L_ADJUST (1 << 3)
+#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
 
 char layer_state_str[24];
 
@@ -12,13 +15,20 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "DEF");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
     break;
-  case L_FN:
-    snprintf(layer_state_str, sizeof(layer_state_str), " FN");
+  case L_RAISE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+    break;
+  case L_LOWER:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+    break;
+  case L_ADJUST:
+  case L_ADJUST_TRI:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
     break;
   default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "UN-%u", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
   }
 
   return layer_state_str;
