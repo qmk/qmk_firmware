281f02048820599eaa18a0bd392b2f41504d6c7e
Jacky <128167503+JackyJia73@users.noreply.github.com>
2023-03-30 07:25:36 +0800

    Update keyboards/smart/smart80/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/smart/smart80/rules.mk
+++ keyboards/smart/smart80/rules.mk
@@ -1,14 +1,1 @@
 MCU_LDSCRIPT = Smartbootloader
-
-
-#flash
-#WEAR_LEVELING_DRIVER = spi_flash
-#EEPROM_DRIVER = wear_leveling
-#WEAR_LEVELING_DRIVER = spi_flash
-
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-#OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-#UART_DEBUG = yes
-#SERIAL_DRIVER = usart
-#QUANTUM_LIB_SRC += uart.c
