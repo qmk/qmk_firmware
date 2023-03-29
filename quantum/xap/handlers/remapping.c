f3c48f100a32096896c90654685dea4484d725e5
Nick Brassel <nick@tzarc.org>
2023-03-30 08:49:10 +1100

    Fixup compilation failures.

--- quantum/xap/handlers/remapping.c
+++ quantum/xap/handlers/remapping.c
@@ -3,8 +3,10 @@
 
 #include "quantum.h"
 #include "xap.h"
+#include "keymap_introspection.h"
 
 #ifdef DYNAMIC_KEYMAP_ENABLE
+#    include "dynamic_keymap.h"
 #    define keymap_max_layer_count() DYNAMIC_KEYMAP_LAYER_COUNT
 #else
 #    define keymap_max_layer_count() keymap_layer_count()
@@ -14,7 +16,6 @@ bool xap_execute_keymap_get_layer_count(xap_token_t token) {
     uint8_t ret = keymap_max_layer_count();
     return xap_respond_data(token, &ret, sizeof(ret));
 }
-
 
 bool xap_execute_get_keymap_keycode(xap_token_t token, const xap_route_keymap_get_keymap_keycode_arg_t *arg) {
     if (arg->layer >= keymap_max_layer_count()) {
