dda1bf24f9eeb0555dd544d00649e2973df8fee9
Lasse <lasse.rotwitt@gmail.com>
2023-03-30 11:00:46 +0200

    Added fallback for encoder

--- keyboards/polygon/keymaps/ansi/keymap.c
+++ keyboards/polygon/keymaps/ansi/keymap.c
@@ -17,13 +17,13 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [0] = LAYOUT_ansi(
+    [0] = LAYOUT_65_ansi_blocker(
 		 KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,              KC_MPLY,
 		 KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,               KC_DEL,
 		LT(1,KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,        KC_ENT,              KC_HOME,
 		KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,                 KC_RSFT,      KC_UP,   KC_END,
 		KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT,      KC_RGUI,        KC_LEFT, KC_DOWN, KC_RIGHT),
-    [1] = LAYOUT_ansi(
+    [1] = LAYOUT_65_ansi_blocker(
      KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,              _______,
   	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______,
   	LT(1,KC_CAPS), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,              _______,
@@ -31,25 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   	_______, _______, _______,                            _______,                            _______,      _______,        RGB_RMOD, RGB_VAD, RGB_MOD)
 };
 
-#ifdef ENCODER_ENABLE
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-      if (IS_LAYER_ON(1)){
-        // Scroll
-        if (clockwise) {
-            tap_code(KC_WH_D);
-        } else {
-            tap_code(KC_WH_U);
-        }
-      } else {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-      }
-    }
-    return false;
-}
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][1] = {
+    [_BASE] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [_LOWER] =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
+};
 #endif
--- keyboards/polygon/polygon.c
+++ keyboards/polygon/polygon.c
@@ -15,3 +15,7 @@
  */
 
 #include "polygon.h"
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    return encoder_update_user(index, clockwise);
+}
