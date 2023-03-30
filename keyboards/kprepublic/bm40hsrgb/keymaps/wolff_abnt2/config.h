8e261c2ba8ccbb627076c584c1979e77a499b700
Lui Wolff <luimlask@gmail.com>
2023-03-30 09:31:41 -0300

    keymap updated

--- keyboards/kprepublic/bm40hsrgb/keymaps/wolff_abnt2/config.h
+++ keyboards/kprepublic/bm40hsrgb/keymaps/wolff_abnt2/config.h
@@ -33,28 +33,6 @@
 #define QUICK_TAP_TERM 0 //No autorepeat in tap-hold keys
 #define HOLD_ON_OTHER_KEY_PRESS //For fast typing
 
-
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 12
-
-/* key matrix pins */
-#define MATRIX_ROW_PINS { B3, B2, E6, B5 }
-#define MATRIX_COL_PINS { B6, C6, B4, D7, D4, D6, C7, F6, F5, F4, F1, F0 }
-#define UNUSED_PINS
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
 // Min 0, max 32
 #define JOYSTICK_BUTTON_COUNT 32
 // Min 0, max 6: X, Y, Z, Rx, Ry, Rz
