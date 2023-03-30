7cdde9f5f2cc02d0539c7c3e15c7b7245c16e2e5
Lui Wolff <luimlask@gmail.com>
2023-03-30 09:44:08 -0300

    keymap updated

--- keyboards/kprepublic/bm40hsrgb/keymaps/wolff_abnt2/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/keymaps/wolff_abnt2/rules.mk
@@ -2,16 +2,12 @@ LTO_ENABLE = yes
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 TERMINAL_ENABLE = no
+
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = no
 KEY_LOCK_ENABLE = no
 RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
 LAYOUTS = planck_mit
-LAYOUTS_HAS_RGB = yes
 
 MIDI_ENABLE = yes
 JOYSTICK_ENABLE = yes
