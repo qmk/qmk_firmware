507179f751d5be24c6b9ecd42479b5136873a442
fauxpark <fauxpark@gmail.com>
2023-03-31 17:54:23 +1100

    Migrate `RGB_DI_PIN` to `ws2812.pin`, handwired

--- keyboards/handwired/owlet60/config.h
+++ keyboards/handwired/owlet60/config.h
@@ -40,7 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-#define RGB_DI_PIN D0
   #define RGBLED_NUM 8
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
