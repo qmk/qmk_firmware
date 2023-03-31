6bf8331e5d37992046a281728c5cf57f924babc5
Yizhen Liu <62583086+edwardslau@users.noreply.github.com>
2023-03-30 20:57:48 -0600

    changes as required

--- keyboards/gray_studio/think65v3/keymaps/default/keymap.c
+++ keyboards/gray_studio/think65v3/keymaps/default/keymap.c
@@ -32,18 +32,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                   _______, _______,                   _______, _______, _______
     )
 };
-const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0,2, HSV_WHITE}
-);     
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_capslock_layer
-);
 
-void keyboard_post_init_user(void) {
-    // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
-}
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(0, led_state.caps_lock);
-    return true;
-}
--- keyboards/gray_studio/think65v3/think65v3.c
+++ keyboards/gray_studio/think65v3/think65v3.c
@@ -1,3 +1,18 @@
 // Copyright 2023 Yizhen Liu (@edwardslau)
 // SPDX-License-Identifier: GPL-2.0
-#include "think65v3.h"
+#include "quantum.h"
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0,6, HSV_WHITE}
+);     
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer
+);
+void keyboard_post_init_kb(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+bool led_update_kb(led_t led_state) {
+    if (!led_update_kb(led_state)) {return false;}
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
