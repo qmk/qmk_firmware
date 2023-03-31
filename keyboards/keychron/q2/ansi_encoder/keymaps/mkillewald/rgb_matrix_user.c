8842ab1a3678f81f0fd2ad2509471a744ccfb5b3
Mike Killewald <mkillewald@yahoo.com>
2023-03-31 14:35:22 -0400

    removed commented out block

--- keyboards/keychron/q2/ansi_encoder/keymaps/mkillewald/rgb_matrix_user.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/mkillewald/rgb_matrix_user.c
@@ -33,11 +33,7 @@ void rgb_matrix_init_user(void) {
     }
 }
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-/*    if(!rgb_matrix_indicators_advanced_keychron(led_min, led_max)) {
-        return false;
-    }*/
-    
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {    
     uint8_t current_layer = get_highest_layer(layer_state);
     switch (current_layer) {
         case MAC_BASE:
