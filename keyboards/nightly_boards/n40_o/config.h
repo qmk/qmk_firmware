dadc764f0f0c9890ceedf0ea08fa01f32d982f39
fauxpark <fauxpark@gmail.com>
2023-03-31 17:55:20 +1100

    Migrate `RGB_DI_PIN` to `ws2812.pin`, N

--- keyboards/nightly_boards/n40_o/config.h
+++ keyboards/nightly_boards/n40_o/config.h
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-#define RGB_DI_PIN B4
 #define RGBLED_NUM 12
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
