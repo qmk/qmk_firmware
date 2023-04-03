2f4caa0d2ca81383eef178fa37519ca0303e34e3
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-04-03 17:24:16 +0800

    Update keyboards/keychron/q11/q11.c
    Co-authored-by: adophoxia <100170946+adophoxia@users.noreply.github.com>

--- keyboards/keychron/q11/q11.c
+++ keyboards/keychron/q11/q11.c
@@ -47,6 +47,28 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
 }
 #endif
 
+#ifdef ENCODER_ENABLE
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+      return false; /* Don't process further events if user function exists and returns false */
+    }
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    }
+    return true;
+}
+#endif
+
 #if defined(RGB_MATRIX_ENABLE) && (defined(CAPS_LOCK_LED_INDEX) || defined(NUM_LOCK_LED_INDEX))
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_user(keycode, record)) {
