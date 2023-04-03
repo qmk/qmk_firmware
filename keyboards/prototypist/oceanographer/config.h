156b64f67c60e48feaa9cd0a77a961b4061a5bd7
Anjheos <Anjheos.Designs@gmail.com>
2023-04-03 01:06:47 -0700

    Initial commit for Oceanographer.

---
+++ keyboards/prototypist/oceanographer/config.h
@@ -0,0 +1,25 @@
+// Copyright 2023 Anjheos (@Anjheos)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// RGB Light settings
+#define RGBLIGHT_LAYERS
+
+#define RGBLIGHT_MAX_LAYERS 4
+#define RGBLIGHT_LIMIT_VAL 155
+#define RGBLIGHT_DEFAULT_VAL 155
+
+// Audio Settings
+#ifdef AUDIO_ENABLE
+
+#define AUDIO_PIN C6
+
+#define AUDIO_INIT_DELAY
+
+#endif
+
+// Encoder Settings
+#define ENCODERS_PAD_A { B2 }
+#define ENCODERS_PAD_B { B1 }
+#define ENCODER_RESOLUTION 2
