060c15f32bc294c5564aa16d95a6a0b02e503551
Jack Humbert <jack.humb@gmail.com>
2023-04-04 12:46:29 -0400

    fully compiling with qmk target

--- cmake/features/backlight.cmake
+++ cmake/features/backlight.cmake
@@ -4,27 +4,27 @@ set_property(CACHE BACKLIGHT_DRIVER PROPERTY STRINGS pwm timer software custom)
 
 string(JSON BACKLIGHT_PIN ERROR_VARIABLE NO_BACKLIGHT_PIN GET ${QMK_KEYBOARD_INFO_JSON_STRING} backlight pin)
 if(${BACKLIGHT_ENABLE} AND NOT ${NO_BACKLIGHT_PIN} STREQUAL "backlight-NOTFOUND")
-    add_library(backlight
+    target_sources(qmk PUBLIC
       quantum/backlight/backlight.c 
       quantum/process_keycode/process_backlight.c
     )
-    add_compile_definitions(
+    target_compile_definitions(qmk PUBLIC
         BACKLIGHT_ENABLE
         BACKLIGHT_PIN=${BACKLIGHT_PIN}
     )
     if(${BACKLIGHT_DRIVER} STREQUAL "custom")
-        add_compile_definitions(BACKLIGHT_CUSTOM_DRIVER)
+        target_compile_definitions(qmk PUBLIC BACKLIGHT_CUSTOM_DRIVER)
     else()
-        target_sources(backlight PRIVATE quantum/backlight/backlight_driver_common.c)
+        target_sources(qmk PUBLIC quantum/backlight/backlight_driver_common.c)
         if(${BACKLIGHT_DRIVER} STREQUAL "pwm")
-            target_sources(backlight PRIVATE quantum/backlight/backlight_${QMK_PLATFORM}.c)
+            target_sources(qmk PUBLIC quantum/backlight/backlight_${QMK_PLATFORM}.c)
         else()
-            target_sources(backlight PRIVATE quantum/backlight/backlight_${BACKLIGHT_DRIVER}.c)
+            target_sources(qmk PUBLIC quantum/backlight/backlight_${BACKLIGHT_DRIVER}.c)
         endif()
     endif()
     target_include_directories(qmk PUBLIC quantum/backlight)
     target_include_directories(qmk PUBLIC drivers/oled)
-    target_link_libraries(backlight qmk)
+    # target_link_libraries(backlight qmk)
     # target_include_directories(backlight PUBLIC quantum/backlight)
     # target_include_directories(quantum PUBLIC quantum/backlight)
     # target_link_libraries(backlight ${QMK_TARGET})
--- cmake/features/oled.cmake
+++ cmake/features/oled.cmake
@@ -1,13 +1,13 @@
 option(OLED_ENABLE "" TRUE)
 if(${OLED_ENABLE})
-    add_library(oled
+    target_sources(qmk PUBLIC
       drivers/oled/ssd1306_sh1106.c 
       platforms/${QMK_PLATFORM}/drivers/i2c_master.c)
-    add_compile_definitions(OLED_ENABLE)
-    target_include_directories(oled PUBLIC drivers)
-    target_include_directories(oled PUBLIC drivers/oled)
-    target_include_directories(oled PUBLIC platforms/${QMK_PLATFORM}/drivers)
-    target_link_libraries(oled qmk)
+    target_compile_definitions(qmk PUBLIC OLED_ENABLE)
+    target_include_directories(qmk PUBLIC drivers)
+    target_include_directories(qmk PUBLIC drivers/oled)
+    target_include_directories(qmk PUBLIC platforms/${QMK_PLATFORM}/drivers)
+    # target_link_libraries(oled qmk)
     # target_link_libraries(oled ${QMK_TARGET})
     # target_link_libraries(oled platforms)
 endif()
