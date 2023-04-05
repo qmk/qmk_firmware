04422917bf10875b0b68c065a711400faed272a0
Jesse Estes <jestes5111@gmail.com>
2023-04-05 08:41:27 -0400

    Updated README.md and copyrights in config.h

--- keyboards/keebio/iris/keymaps/jestes5111/README.md
+++ keyboards/keebio/iris/keymaps/jestes5111/README.md
@@ -1,7 +1,6 @@
+![Keymap SVG](keymap.svg)
+
 # My Iris
 
-WIP
-
 Designed for creating/editing in VIM and Excel, gaming, and general keyboard-focused media consumption.
-
-![Keymap SVG](keymap.svg)
+WIP.
--- keyboards/keebio/iris/keymaps/jestes5111/config.h
+++ keyboards/keebio/iris/keymaps/jestes5111/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Danny Nguyen <danny@keeb.io>
+Copyright 2023 Jesse Estes
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
