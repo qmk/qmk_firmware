9b916376cd89dddc47144f89bb0b8259d5b8e0f6
Lasse <lasse.rotwitt@gmail.com>
2023-04-05 15:48:20 +0200

    Added encoder fallback

--- keyboards/polygon/polygon.c
+++ keyboards/polygon/polygon.c
@@ -15,3 +15,17 @@
  */
 
 #include "polygon.h"
+
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+      return false; /* Don't process further events if user function exists and returns false */
+    }
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 4);
+        } else {
+            tap_code_delay(KC_VOLD, 4);
+        }
+    return true;
+}
