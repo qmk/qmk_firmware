53b541173b92d6ed75805cefa8adb5e69c2507dc
Charly Delay <charly@delay.gg>
2023-04-06 00:21:59 +0900

    charybdis/4x6: Add USB_VBUS_PIN define

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -25,6 +25,9 @@
 // #define SPLIT_HAND_PIN GP13
 // #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
+/* VBUS detection. */
+#define USB_VBUS_PIN GP25
+
 /* SPI & PMW3360 settings. */
 #define SPI_DRIVER SPID0
 #define SPI_SCK_PIN GP18
