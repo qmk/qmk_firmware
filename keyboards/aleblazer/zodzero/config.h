bba9e5e8e3b4679ecf234e62dfc99f25a926dca8
Aleblazer <60912320+Aleblazer@users.noreply.github.com>
2023-04-04 21:41:39 -0500

    Fix I2C for ZodZero in config.h

--- keyboards/aleblazer/zodzero/config.h
+++ keyboards/aleblazer/zodzero/config.h
@@ -20,6 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SERIAL_USART_RX_PIN GP13
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
-#define I2C_DRIVER I2CD2
+#define I2C_DRIVER I2CD1
 #define I2C1_SCL_PIN GP15
 #define I2C1_SDA_PIN GP14
