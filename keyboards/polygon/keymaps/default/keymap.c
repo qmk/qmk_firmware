faabb11b64735daa0ac87f692b65e471d25554d7
Lasse <lasse.rotwitt@gmail.com>
2023-04-05 17:12:17 +0200

    Enocder mapping mod and add MUTE on layer 1

--- keyboards/polygon/keymaps/default/keymap.c
+++ keyboards/polygon/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,                 KC_RSFT,      KC_UP,   KC_END,
 		KC_LCTL, KC_LGUI, KC_LALT,                             KC_SPC,                            KC_RALT,      KC_RGUI,        KC_LEFT, KC_DOWN, KC_RIGHT),
     [1] = LAYOUT_ansi(
-     KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,              _______,
+     KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,              KC_MUTE,
   	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,              _______,
   	LT(1,KC_CAPS), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,              _______,
   	_______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                 _______,    RGB_VAI,  _______,
@@ -32,9 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][1] = {
     [_BASE] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_RAISE] =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)}
 };
 #endif
-
