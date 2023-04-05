9e9f7f88f42d2df8d0626ffc91d9fcc61dd8045a
Charly Delay <charly@delay.gg>
2023-04-06 00:21:59 +0900

    Add USB_VBUS_PIN define for all Splinky-based keyboards

--- keyboards/bastardkb/tbkmini/v2/splinky_2/config.h
+++ keyboards/bastardkb/tbkmini/v2/splinky_2/config.h
@@ -25,6 +25,9 @@
 // #define SPLIT_HAND_PIN GP13
 // #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
+/* VBUS detection. */
+#define USB_VBUS_PIN GP25
+
 /* Reset. */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
