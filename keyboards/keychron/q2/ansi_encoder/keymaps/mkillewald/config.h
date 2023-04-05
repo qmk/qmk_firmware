77601dde6702c92d6103fb97d3b321049231878b
Mike Killewald <mkillewald@yahoo.com>
2023-04-04 21:18:15 -0400

    change Fn2 layer to yellow

--- keyboards/keychron/q2/ansi_encoder/keymaps/mkillewald/config.h
+++ keyboards/keychron/q2/ansi_encoder/keymaps/mkillewald/config.h
@@ -27,5 +27,5 @@
 #    define HUE_WIN_BASE  80 // HSV green hue component
 #    define CAPS_LOCK_INDICATOR_COLOR RGB_RED
 #    define FN1_LAYER_COLOR 0xFE, 0x23, 0x00
-#    define FN2_LAYER_COLOR RGB_GREEN
+#    define FN2_LAYER_COLOR RGB_YELLOW
 #endif
