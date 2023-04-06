05f7eb743e2257e8b23cc4c4e0f4bfbca496a838
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-07 00:34:55 +0300

    Update rules.mk

--- keyboards/darkproject/kd87b_ltd/keymaps/gaming_by_shvd3x/rules.mk
+++ keyboards/darkproject/kd87b_ltd/keymaps/gaming_by_shvd3x/rules.mk
@@ -1,4 +1,7 @@
 VIA_ENABLE = yes
-DEBOUNCE_TYPE = sym_eager_pk
+DEBOUNCE_TYPE = sym_eager_pk #If you have issues with double clicking open keymap's readme and follow the instructions
 
 OPT = 3
+
+GRAVE_ESC_ENABLE = no #No need for this feature because keyboard has F row and dedicated escape button
+SPACE_CADET_ENABLE = no #If you for some reason need this just remove this string
