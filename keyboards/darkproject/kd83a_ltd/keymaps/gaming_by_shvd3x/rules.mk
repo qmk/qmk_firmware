a2faba49c634b05e7f44017903a148b09fa5a9b4
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-07 00:21:02 +0300

    Update rules.mk

--- keyboards/darkproject/kd83a_ltd/keymaps/gaming_by_shvd3x/rules.mk
+++ keyboards/darkproject/kd83a_ltd/keymaps/gaming_by_shvd3x/rules.mk
@@ -3,3 +3,6 @@ ENCODER_MAP_ENABLE = yes
 DEBOUNCE_TYPE = sym_eager_pk
 
 OPT = 3
+
+GRAVE_ESC_ENABLE = no #No need for this feature because keyboard has F row and dedicated escape button
+SPACE_CADET_ENABLE = no #If you for some reason need this feature just remove this string
