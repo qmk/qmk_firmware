05656cbbdecf6ba984a88d93bde2546582d02225
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-07 00:30:43 +0300

    Update config.h

--- keyboards/darkproject/kd87b_ltd/config.h
+++ keyboards/darkproject/kd87b_ltd/config.h
@@ -17,15 +17,9 @@
 
 #pragma once
 
-//#define TAP_CODE_DELAY 10
+//#define TAP_CODE_DELAY 5
 
-#define MATRIX_IO_DELAY 5
 #define GPIO_INPUT_PIN_DELAY 0
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_KEYPRESSES
