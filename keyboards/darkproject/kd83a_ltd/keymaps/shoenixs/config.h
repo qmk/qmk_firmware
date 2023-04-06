efb6f5e878359aea8880ac5884453868e0ea1edc
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-07 01:26:41 +0300

    Add files via upload

---
+++ keyboards/darkproject/kd83a_ltd/keymaps/shoenixs/config.h
@@ -0,0 +1,8 @@
+#define DEBOUNCE 3
+#define FORCE_NKRO
+#undef RGB_MATRIX_LED_FLUSH_LIMIT 
+#define RGB_MATRIX_LED_FLUSH_LIMIT 5
+#define LAYER_STATE_8BIT // Limits max number of layers to 8. Reduces keymap size.
+#define NO_ACTION_ONESHOT //If you need this feature just remove this string
+//#define DEBUG_MATRIX_SCAN_RATE
+//#define KEYBOARD_SHARED_EP
---
+++ keyboards/darkproject/kd83a_ltd/keymaps/shoenixs/rules.mk
@@ -0,0 +1,10 @@
+VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+DEBOUNCE_TYPE = sym_eager_pk
+
+OPT = 3
+
+GRAVE_ESC_ENABLE = no #No need for this feature because keyboard has F row and dedicated escape button
+SPACE_CADET_ENABLE = no #If you for some reason need this just remove this string
+
+MOUSEKEY_ENABLE = no
