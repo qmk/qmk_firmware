e4f65327b30f3fa9efa1f03effdeb5f2a1bcb2a0
9R <git@9-r.net>
2023-04-06 21:23:42 +0200

    add last_wasd to stop register_keycode spam

--- keyboards/handwired/replicazeron/common/thumbstick.h
+++ keyboards/handwired/replicazeron/common/thumbstick.h
@@ -40,6 +40,7 @@ typedef struct {
 thumbstick_polar_position_t thumbstick_polar_position ;
 
 wasd_state_t wasd_state;
+wasd_state_t last_wasd_state;
 
 void init_wasd_state(void);
 
@@ -47,6 +48,6 @@ thumbstick_polar_position_t get_thumbstick_polar_position(int16_t x, int16_t y);
 
 bool update_keystate(uint16_t angle_from, uint16_t angle_to, uint16_t angle);
 
-void update_keycode(uint16_t keycode, bool keystate);
+void update_keycode(uint16_t keycode, bool keystate, bool last_keystate);
 
 void thumbstick(controller_state_t controller_state);
