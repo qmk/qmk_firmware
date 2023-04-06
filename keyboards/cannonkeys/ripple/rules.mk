f613bd75ac4ae35dccfaee7474f7445e6aaaa596
fauxpark <fauxpark@gmail.com>
2023-04-06 19:15:25 +1000

    Fix up remaining WS2812 pin/driver migrations

--- keyboards/cannonkeys/ripple/rules.mk
+++ keyboards/cannonkeys/ripple/rules.mk
@@ -1,7 +1,5 @@
 # Wildcard to allow APM32 MCU
 DFU_SUFFIX_ARGS = -v FFFF -p FFFF
 
-WS2812_DRIVER = spi
-
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
