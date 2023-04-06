ec781cacbb45a2eec2d198022cae4c3123ff3342
skeletonkbd <skeletonkbd@gmail.com>
2023-04-07 04:50:26 +0900

    fix format

--- keyboards/skeletonkbd/frost68/keymaps/via/keymap.c
+++ keyboards/skeletonkbd/frost68/keymaps/via/keymap.c
@@ -1,29 +1,12 @@
-/* Copyright 2020 MudkipMao
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2023 skeletonkbd (@skeletonkbd)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
-enum layer_names
-{
-	_BASE,
-	_L1,
-	_L2,
-	_L3
-};
+enum layer_names { _BASE, _L1, _L2, _L3 };
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT_alice(
 		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,
