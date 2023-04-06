1e034301a8ba65b1dc528a8ef00319fe3971e3d6
gksygithub <106651989+gksygithub@users.noreply.github.com>
2023-04-06 13:40:13 +0800

    Resolved requested changes.

--- keyboards/darkproject/kd87a_bfg_edition/config.h
+++ keyboards/darkproject/kd87a_bfg_edition/config.h
@@ -17,7 +17,7 @@
 #pragma once
 
 #define MATRIX_ROW_PINS \
-    { B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12, B13 }
+    { B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B12 }
 #define MATRIX_COL_PINS \
     { A0, A1, A2, A3, A4, A8, A9, A10 }
 
