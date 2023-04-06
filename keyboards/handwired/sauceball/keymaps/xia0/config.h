d92dce4edb45c5b788f58a760aa3a0de6718848f
xia0 <cpt.acky@gmail.com>
2023-04-07 00:16:27 +0930

    Revert "Reverted setting for drag scrolling with custom key"
    This reverts commit 89d04075e20161cd21c0b073b85084ca7a0c0395.

--- keyboards/handwired/sauceball/keymaps/xia0/config.h
+++ keyboards/handwired/sauceball/keymaps/xia0/config.h
@@ -1,23 +1,18 @@
 // Copyright 2023 xia0 (@xia0)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-
-
 // modify movement rates as a percentage
 // use this to reduce mouse sensitivity especially for drag-scrolling
-#define SENSITIVITY_X 50
-#define SENSITIVITY_Y 50
+#define SENSITIVITY_X 40
+#define SENSITIVITY_Y 40
 #define SENSITIVITY_H 3
 #define SENSITIVITY_V 3
+
+// define custom key to activate drag scroll when held
+#define DRAG_SCROLL_KEY KC_BTN4
 
 // since we are implementing our own drag scroll, we should disable default behaviour which will allow us to drag with middle mouse without scrolling.
 // by default, this behaviour is set to middle mouse as i think the ps/2 functionality of qmk has trackpoint modules in mind.
 // comment out the next line to use the default behaviour (middle mouse = drag scroll)
-//#define PS2_MOUSE_SCROLL_BTN_MASK 0
+#define PS2_MOUSE_SCROLL_BTN_MASK 0
 #define PS2_MOUSE_SCROLL_BTN_SEND 300
-
-// define custom key to activate drag scroll when held
-#define DRAG_SCROLL_KEY KC_MS_BTN3
-
-// scrolling method. 1 = natural, -1 = reverse
-#define DRAG_SCROLL_NATURAL -1
--- keyboards/handwired/sauceball/keymaps/xia0/keymap.c
+++ keyboards/handwired/sauceball/keymaps/xia0/keymap.c
@@ -5,31 +5,30 @@
 #include "print.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_mouse(KC_BTN1, KC_BTN3, KC_BTN4, KC_BTN2)
+  [0] = LAYOUT_mouse(KC_BTN1, KC_BTN4, KC_BTN3, KC_BTN2)
 };
 
 int is_drag_scrolling = 0;              // should we be scrolling or just moving the cursor
 uint16_t drag_scroll_key_timer;         // keep track of how long the drag key is held for
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  //uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+  uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 
-#ifdef PS2_MOUSE_SCROLL_BTN_MASK
   if (PS2_MOUSE_SCROLL_BTN_MASK == 0 && keycode == DRAG_SCROLL_KEY) {
+
     // if scroll button is not held, send a tap of the key
     // duration of this is defined by PS2_MOUSE_SCROLL_BTN_SEND
     if (record->event.pressed == 1) {
       drag_scroll_key_timer = timer_read();
     }
     else if (timer_elapsed(drag_scroll_key_timer) < PS2_MOUSE_SCROLL_BTN_SEND) {  // key was released within ms defined by PS2_MOUSE_SCROLL_BTN_SEND (default 300)
-      tap_code(DRAG_SCROLL_KEY);  // TODO why does this not work? keys other than KC_BTN# seem to work
+      tap_code(DRAG_SCROLL_KEY);
     }
 
     is_drag_scrolling = record->event.pressed;
     return false;
 
   }
-#endif
 
   return true;
 }
@@ -45,7 +44,7 @@ void ps2_mouse_moved_user(report_mouse_t *mouse_report) {
   // if drag-scrolling, replace the h and v values with x and y
   if (is_drag_scrolling == 1) {
     mouse_report->h = mouse_report->x;
-    mouse_report->v = mouse_report->y * DRAG_SCROLL_NATURAL;
+    mouse_report->v = mouse_report->y;
     mouse_report->x = 0;
     mouse_report->y = 0;
   }
