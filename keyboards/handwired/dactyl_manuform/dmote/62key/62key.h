f3f029c2476dd4368e456662951b40ee6da37df3
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-08 00:27:36 +0100

    Updated dmote subfolder
    - added license headers to child files
    - renamed layout definition header in 62key.h from 62key to dmote_62

--- keyboards/handwired/dactyl_manuform/dmote/62key/62key.h
+++ keyboards/handwired/dactyl_manuform/dmote/62key/62key.h
@@ -1,3 +1,20 @@
+/*
+Copyright 2023 Victor Eikman <viktor.eikman@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include "quantum.h"
@@ -11,7 +28,7 @@
 // of the thumb cluster extending along the sides of the finger
 // cluster so that everything can be contained in a 6x6 pattern.
 
-#define LAYOUT_62key( \
+#define LAYOUT_dmote_62( \
     LA_20, LA_10, LF_35, LF_25, LF_15, LF_05, \
     LF_55, LF_45, LF_34, LF_24, LF_14, LF_04, \
     LF_54, LF_44, LF_33, LF_23, LF_13, LF_03, \
