765e346b870e0bba5823cacb4a6a2720d17f4ec9
Jesse Estes <jestes5111@gmail.com>
2023-04-07 11:01:30 -0400

    Removed duplicates from rules.mk

--- keyboards/keebio/iris/keymaps/jestes5111/rules.mk
+++ keyboards/keebio/iris/keymaps/jestes5111/rules.mk
@@ -1,26 +1,9 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = qmk-dfu
-
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = no
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
-CONSOLE_ENABLE = no  
-COMMAND_ENABLE = no  
-NKRO_ENABLE = yes    
+NKRO_ENABLE = yes
 BACKLIGHT_ENABLE = no
-AUDIO_ENABLE = no    
-RGBLIGHT_ENABLE = yes
 KEY_LOCK_ENABLE = yes
-UNICODE_ENABLE = yes 
+UNICODE_ENABLE = yes
 
-SPLIT_KEYBOARD = yes
 ENCODER_ENABLE = no
 TRI_LAYER_ENABLE = yes
-
-LTO_ENABLE = yes
