3ef9e10a784ae0652e7f0ffcb88235a201d2754b
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-07 23:56:07 +0100

    Update Dactyl Manuform keyboard
    - moved split keyboard definition to info.json for all sub-keyboards
    - Updated readme.md

--- keyboards/handwired/dactyl_manuform/3x5_3/rules.mk
+++ keyboards/handwired/dactyl_manuform/3x5_3/rules.mk
@@ -13,7 +13,6 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 
 RGB_MATRIX_ENABLE = yes     # Enable WS2812 RGB matrix
 RGB_MATRIX_DRIVER = WS2812
-SPLIT_KEYBOARD = yes
 
 RGBLIGHT_SUPPORTED = yes
 RGB_MATRIX_SUPPORTED = yes
