b7a3ce756342a84e11a2be05ef090a2096bec4a7
Ex3c4Def <Joe_No@gmx.net>
2023-04-07 14:30:30 +0200

    Enabled Encoder on keymap level
    Signed-off-by: Ex3c4Def <Joe_No@gmx.net>

--- keyboards/keycapsss/kimiko/keymaps/rev2/rules.mk
+++ keyboards/keycapsss/kimiko/keymaps/rev2/rules.mk
@@ -1,6 +1,6 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-ENCODER_ENABLE = no       # ENables the use of one or more encoders
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 
 # LTO: Link Time Optimizations.
