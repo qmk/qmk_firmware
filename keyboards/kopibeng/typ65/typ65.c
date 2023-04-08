b81e155e0ccd0f8f78c96ad41b357d3472902dc3
fauxpark <fauxpark@gmail.com>
2023-04-08 16:36:44 +1000

    Move single `LAYOUT`s to data driven, K

--- keyboards/kopibeng/typ65/typ65.c
+++ keyboards/kopibeng/typ65/typ65.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "typ65.h"
+#include "quantum.h"
 
 void keyboard_pre_init_kb (void) {
   setPinOutput(INDICATOR_0);
