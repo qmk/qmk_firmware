afbbb9e039eb92f06a6827b938410ce9bbaf4991
fauxpark <fauxpark@gmail.com>
2023-04-08 19:14:26 +1000

    Fix build failures

--- keyboards/jones/v1/v1.c
+++ keyboards/jones/v1/v1.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "quantum.h"
+#include QMK_KEYBOARD_H
 
 #if defined(AUDIO_ENABLE) && defined(MUSIC_MAP)
 const uint8_t music_map[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
