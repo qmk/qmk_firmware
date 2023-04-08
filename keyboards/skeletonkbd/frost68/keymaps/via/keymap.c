f2ac0812aa61651fbec42bf2e36429570722d3f2
skeletonkbd <skeletonkbd@gmail.com>
2023-04-09 02:02:23 +0900

    change layout name to 'LAYOUT'(fix)

--- keyboards/skeletonkbd/frost68/keymaps/via/keymap.c
+++ keyboards/skeletonkbd/frost68/keymaps/via/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,            KC_LNG1, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
 		KC_LCTL, KC_LALT,          KC_SPC,            MO(_L1),         KC_SPC,                    KC_RGUI,          KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-	[_L1] = LAYOUT_alice(
+	[_L1] = LAYOUT(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,          KC_PSCR,
 		KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI,          RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, KC_SCRL, KC_PAUS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          QK_BOOT,
