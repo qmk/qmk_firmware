3207e95a9800e35e00ae7b6c9da9cdfc38672a9f
TweetyDaBird <markus.knutsson@tweety.se>
2023-04-09 16:00:52 +0200

    Cleanup
    Cleaned up the default keymap
    Made versions for bootloaders

--- keyboards/tweetydabird/lotus58/config.h
+++ keyboards/tweetydabird/lotus58/config.h
@@ -2,22 +2,14 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-/*#include "config_common.h"
 
-// Not working correct in json implementation
-#define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 2000
-#define SPLIT_USB_TIMEOUT_POLL 10
-
-// Not yet supported in json
-
-// Bootmagic Lite key configuration
+// Bootmagic Lite key configuration (these do not transfer to Vial in info.json)
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
 #define BOOTMAGIC_LITE_ROW_RIGHT 5
 #define BOOTMAGIC_LITE_COLUMN_RIGHT 0
-*/
-// RGB options not yet fully implemented in info.json
+
+// Options not yet fully implemented in info.json
 #define RGBLIGHT_LIMIT_VAL 175
 #define RGBLIGHT_DEFAULT_VAL ( RGBLIGHT_LIMIT_VAL / 2 )
 #define RGBLIGHT_SLEEP
