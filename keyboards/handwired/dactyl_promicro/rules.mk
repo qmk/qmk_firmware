47bfe61221f6702958ff1a64884e9cee66c0a960
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-04-09 22:05:13 +0100

    Delete rules.mk

--- keyboards/handwired/dactyl_promicro/rules.mk
+++
@@ -1,6 +0,0 @@
-# Build Options
-#   change yes to no to disable
-#
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
