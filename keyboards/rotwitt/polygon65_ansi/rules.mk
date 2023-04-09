dfbb0a4dd843d2a4f49482b50a74ced81fd885f7
Lasse <lasse.rotwitt@gmail.com>
2023-04-09 22:56:47 +0200

    Ansi ready for VIA testing

--- keyboards/rotwitt/polygon65_ansi/rules.mk
+++ keyboards/rotwitt/polygon65_ansi/rules.mk
@@ -2,14 +2,9 @@
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
+ENCODER_MAP_ENABLE = yes	# Enable encoder mapping
 
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = IS31FL3741
