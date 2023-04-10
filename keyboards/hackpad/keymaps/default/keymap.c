d08bff65e8cff5344c0cdabf0a9ad0beba39dab4
ThePurox <28731015+ThePurox@users.noreply.github.com>
2023-04-10 21:30:29 +0200

    Apply most suggestions from code review
    Co-authored-by: Sergey Vlasov <sigprof@gmail.com>

--- keyboards/hackpad/keymaps/default/keymap.c
+++ keyboards/hackpad/keymaps/default/keymap.c
@@ -17,13 +17,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ 0 │ . │Ent│ / │
      * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_numpad_4x4(                      LT(1, KC_MUTE),
+    [0] = LAYOUT(                                 LT(1, KC_MUTE),
                             KC_7, KC_8,   KC_9,   DE_PLUS,
                             KC_4, KC_5,   KC_6,   DE_MINS,
                             KC_1, KC_2,   KC_3,   DE_ASTR,
                             KC_0, KC_DOT, KC_ENT, DE_SLSH),
 
-    [1] = LAYOUT_numpad_4x4(                              XXXXXXX,
+    [1] = LAYOUT(                                         XXXXXXX,
                             RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_HUI,
                             RGB_M_SW, RGB_M_SN, RGB_M_K,  RGB_HUD,
                             RGB_M_X,  RGB_M_G,  RGB_M_TW, RGB_VAI,
