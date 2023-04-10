492a3e60f71f9bd3a62c2117f84bc6b9409c1656
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-10 19:41:06 +0100

    files revised
    variants:
    - 4x6_2_5
    - 4x6_56
    - 5x7_5

--- keyboards/handwired/dactyl_manuform/4x6_56/4x6_56.h
+++ keyboards/handwired/dactyl_manuform/4x6_56/4x6_56.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define XXX KC_NO
 
-#define LAYOUT( \
+#define LAYOUT_4x6_56( \
     L00, L01, L02, L03, L04, L05,                    R00, R01, R02, R03, R04, R05, \
     L10, L11, L12, L13, L14, L15,                    R10, R11, R12, R13, R14, R15, \
     L20, L21, L22, L23, L24, L25,                    R20, R21, R22, R23, R24, R25, \
