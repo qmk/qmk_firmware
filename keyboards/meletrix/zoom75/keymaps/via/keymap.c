69dd29000ba00fb3b175d45f090d30ae7ada7d9e
GG <zjjhgzh@gmail.com>
2023-04-10 15:40:51 +0300

    update

--- keyboards/meletrix/zoom75/keymaps/via/keymap.c
+++ keyboards/meletrix/zoom75/keymaps/via/keymap.c
@@ -18,7 +18,7 @@
 
 
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][1] = {
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = {  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
     [1] = {  ENCODER_CCW_CW(KC_MNXT, KC_MPRV)  },
 };
