d1ea9d79e9c48a2b1b3bab63855feb57fb7f12f6
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-11 15:27:02 +0300

    Update kd87b_ltd.c

--- keyboards/darkproject/kd87b_ltd/kd87b_ltd.c
+++ keyboards/darkproject/kd87b_ltd/kd87b_ltd.c
@@ -125,27 +125,3 @@ const aw_led g_aw_leds[RGB_MATRIX_LED_COUNT] = {
     {1, CS10_SW5,  CS11_SW5,  CS12_SW5},  //  86, Right
 };
 #endif
-
-#ifdef EEPROM_ENABLE
-
-#include "spi_master.h"
-
-void spi_init(void) {
-    static bool is_initialised = false;
-    if (!is_initialised) {
-        is_initialised = true;
-
-        // Try releasing special pins for a short time
-        setPinInput(SPI_SCK_PIN);
-        setPinInput(SPI_MOSI_PIN);
-        setPinInput(SPI_MISO_PIN);
-
-        chThdSleepMilliseconds(10);
-
-        palSetPadMode(PAL_PORT(SPI_SCK_PIN), PAL_PAD(SPI_SCK_PIN), PAL_MODE_ALTERNATE(SPI_SCK_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST | PAL_WB32_CURRENT_LEVEL3);
-        palSetPadMode(PAL_PORT(SPI_MOSI_PIN), PAL_PAD(SPI_MOSI_PIN), PAL_MODE_ALTERNATE(SPI_MOSI_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST);
-        palSetPadMode(PAL_PORT(SPI_MISO_PIN), PAL_PAD(SPI_MISO_PIN), PAL_MODE_ALTERNATE(SPI_MISO_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST);
-    }
-}
-
-#endif
