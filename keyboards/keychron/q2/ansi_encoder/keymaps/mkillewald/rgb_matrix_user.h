332180e1101cc3e5769f0d2a52e604190d925299
Mike Killewald <mkillewald@yahoo.com>
2023-04-11 01:29:01 -0400

    code reorg. moved layer stuff into keymap.c

--- keyboards/keychron/q2/ansi_encoder/keymaps/mkillewald/rgb_matrix_user.h
+++ keyboards/keychron/q2/ansi_encoder/keymaps/mkillewald/rgb_matrix_user.h
@@ -17,12 +17,8 @@
 #pragma once
 
 void rgb_matrix_init_user(void);
-void rgb_matrix_set_cyber_colors(void);
 void rgb_matrix_set_color_by_keycode(uint8_t led_min, uint8_t led_max, uint8_t layer, bool (*is_keycode)(uint16_t), uint8_t red, uint8_t green, uint8_t blue);
-
 bool is_caps_lock_indicator(uint16_t keycode);
 bool is_caps_word_indicator(uint16_t keycode);
 bool is_transparent(uint16_t keycode);
 bool is_not_transparent(uint16_t keycode);
-
-void set_hsv_win_base(HSV hsv);
