cfa18859d228de2886ee8fef745c29de3e88aced
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-11 15:26:38 +0300

    Update kd83a_ltd.c

--- keyboards/darkproject/kd83a_ltd/kd83a_ltd.c
+++ keyboards/darkproject/kd83a_ltd/kd83a_ltd.c
@@ -125,30 +125,6 @@ const aw_led g_aw_leds[RGB_MATRIX_LED_COUNT] = {
 };
 #endif
 
-#ifdef EEPROM_ENABLE
-
-#include "spi_master.h"
-
-void spi_init(void) {
-    static bool is_initialised = false;
-    if (!is_initialised) {
-        is_initialised = true;
-
-        // Try releasing special pins for a short time
-        setPinInput(SPI_SCK_PIN);
-        setPinInput(SPI_MOSI_PIN);
-        setPinInput(SPI_MISO_PIN);
-
-        chThdSleepMilliseconds(10);
-
-        palSetPadMode(PAL_PORT(SPI_SCK_PIN), PAL_PAD(SPI_SCK_PIN), PAL_MODE_ALTERNATE(SPI_SCK_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST | PAL_WB32_CURRENT_LEVEL3);
-        palSetPadMode(PAL_PORT(SPI_MOSI_PIN), PAL_PAD(SPI_MOSI_PIN), PAL_MODE_ALTERNATE(SPI_MOSI_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST);
-        palSetPadMode(PAL_PORT(SPI_MISO_PIN), PAL_PAD(SPI_MISO_PIN), PAL_MODE_ALTERNATE(SPI_MISO_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST);
-    }
-}
-
-#endif
-
 #ifdef ENCODER_ENABLE
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
