fa227d9b7d7ef3d4910ffdb2b8c4aa6ba3539f50
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-04-11 15:14:26 +0100

    update `keymaps/default/keymap.c`
    default keymap sanitised

--- keyboards/narfkb/keymaps/default/keymap.c
+++ keyboards/narfkb/keymaps/default/keymap.c
@@ -23,11 +23,43 @@ SOFTWARE.
 
 #include QMK_KEYBOARD_H
 
+#define CUT LCTL(KC_X)
+#define COPY LCTL(KC_C)
+#define PSTE LCTL(KC_V)
+#define UNDO LCTL(KC_Z)
+#define SLCTA LCTL(KC_A)
+#define APPFW LALT(KC_TAB)
+#define MG_A LGUI_T(KC_A)
+#define MA_S LALT_T(KC_S)
+#define MC_D LCTL_T(KC_D)
+#define MS_F LSFT_T(KC_F)
+#define MS_J LSFT_T(KC_J)
+#define MC_K LCTL_T(KC_K)
+#define MA_L LALT_T(KC_L)
+#define MG_SCLN LGUI_T(KC_SCLN)
+#define TASKM RCS(KC_ESC)
+#define LY1SPC LT(1, KC_SPC)
+#define LY2BS LT(2, KC_BSPC)
+#define DTMGR LGUI(KC_TAB)
+#define REDO LCTL(KC_Y)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x5_6(
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,                                                       S(KC_A),    S(KC_B),    S(KC_C),    S(KC_D),    S(KC_E),
-        KC_F,    KC_G,    KC_H,    KC_I,    KC_J,      KC_K,                                   S(KC_F), S(KC_G),    S(KC_H),    S(KC_I),    S(KC_J),    S(KC_K),
-        KC_L,    KC_M,    KC_N,    KC_O,    KC_P,      KC_Q, KC_R, KC_S,     S(KC_L), S(KC_M), S(KC_N), S(KC_O),    S(KC_P),    S(KC_Q),    S(KC_R),    S(KC_S),
-                                                       KC_T, KC_U, KC_V,     S(KC_T), S(KC_U), S(KC_V)
+        KC_Q, KC_W, KC_E, KC_R, KC_T,                                                         KC_Y, KC_U,   KC_I,    KC_O,    KC_P,
+        MG_A, MA_S, MC_D, MS_F, KC_G, KC_MUTE,                                         TASKM, KC_H, MS_J,   MC_K,    MA_L, MG_SCLN,
+        KC_Z, KC_X, KC_C, KC_V, KC_B,     CUT,   COPY,   PSTE,          SLCTA, APPFW,   UNDO, KC_N, KC_M, KC_DOT, KC_COMM, KC_SLSH,
+                                       KC_ESC, LY1SPC, KC_TAB,         KC_ENT, LY2BS, KC_DEL
+    ),
+    [1] = LAYOUT_split_3x5_6(
+        KC_F1,     KC_F2, KC_F3,   KC_F4,  KC_F5,                                                                KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10,
+        KC_1,       KC_2,  KC_3,    KC_4,   KC_5, KC_TRNS,                                            KC_TRNS,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,   DTMGR,    REDO, KC_BSLS, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,
+                                                    KC_NO, KC_TRNS,   KC_NO,        KC_LALT, KC_RSFT, KC_RCTL
+    ),
+    [2] = LAYOUT_split_3x5_6(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,                                                                KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                                     KC_TRNS, QK_BOOT, KC_TRNS,          KC_NO, KC_TRNS, KC_NO
     )
 };
