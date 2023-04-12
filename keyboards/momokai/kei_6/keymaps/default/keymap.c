e546135453fdd95d17c065f747c138bfacc90866
peepeetee <peepeetee@protonmail.com>
2023-04-11 23:42:33 -0500

    fix default encoder mapping

--- keyboards/momokai/kei_6/keymaps/default/keymap.c
+++ keyboards/momokai/kei_6/keymaps/default/keymap.c
@@ -13,10 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
-    [1]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
-    [2]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
-    [3]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
+    [0]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) }
     
 };
 #endif
