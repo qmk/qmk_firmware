d0190e81b4748264964995cf48bf7a55e745a27c
peepeetee <peepeetee@protonmail.com>
2023-04-11 23:38:26 -0500

    new keyboard - momokai/kei 6

---
+++ keyboards/momokai/kei_6/keymaps/via/keymap.c
@@ -0,0 +1,37 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+                KC_MUTE,
+        KC_Z,   KC_X,   KC_C,
+        KC_GRV,   KC_ESC,   KC_F2
+    ),
+    [1] = LAYOUT(
+                KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS
+    ),
+    [2] = LAYOUT(
+                KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS
+    ),
+    [3] = LAYOUT(
+                KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
+    [1]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
+    [2]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
+    [3]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
+    
+};
+#endif
