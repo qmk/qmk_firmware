2cc4e6bedb1aa2d6df6ac9bf408be793e0a7c0ad
Mike Killewald <mkillewald@yahoo.com>
2023-04-12 02:21:33 -0400

    renamed config.h option for clarity

--- keyboards/keychron/q1/ansi/keymaps/mkillewald/config.h
+++ keyboards/keychron/q1/ansi/keymaps/mkillewald/config.h
@@ -23,8 +23,8 @@
 #define DEFAULT_CAPS_LOCK_LIGHT_ALPHA    1            // 1 = enable, 0 = disable
 #define DEFAULT_FN_LAYER_TRANSPARENT_OFF 0            // 1 = enable, 0 = disable
 #define DEFAULT_FN_LAYER_SHOW_COLOR      1            // 1 = enable, 0 = disable
-#define DEFAULT_ENABLE_MAC_BASE          1            // 1 = enable, 0 = disable
-#define DEFAULT_ENABLE_WIN_BASE          1            // 1 = enable, 0 = disable
+#define DEFAULT_RGB_ENABLE_MAC_BASE          1            // 1 = enable, 0 = disable
+#define DEFAULT_RGB_ENABLE_WIN_BASE          1            // 1 = enable, 0 = disable
 #define DEFAULT_HUE_WIN_BASE             80           // HSV green hue (Win Base layer)
 #define RGB_MATRIX_DEFAULT_HUE           0            // HSV red hue  (Mac Base layer)
 #define RGB_MATRIX_DEFAULT_SAT           255          // Both Win/Mac Base layers
