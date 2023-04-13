1edfa50efb011d3b0f3e710554f1f8dd2fda2441
Drashna Jael're <drashna@live.com>
2023-04-12 19:42:13 -0700

    Change order in makefiles

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -809,9 +809,9 @@ endif
 
 ifeq ($(strip $(PS2_MOUSE_ENABLE)), yes)
     PS2_ENABLE := yes
+    MOUSE_ENABLE := yes
     SRC += ps2_mouse.c
     OPT_DEFS += -DPS2_MOUSE_ENABLE
-    MOUSE_ENABLE := yes
 endif
 
 VALID_PS2_DRIVER_TYPES := busywait interrupt usart vendor
