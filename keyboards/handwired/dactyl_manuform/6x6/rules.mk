adddc5f4f6bd5f75bfa09fdc2bfc71cd73ebc14f
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-14 08:17:22 +0100

    Moved definitions from rules.mk to info.json

--- keyboards/handwired/dactyl_manuform/6x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/6x6/rules.mk
@@ -1,8 +1,1 @@
-# Build Options
-#   change yes to no to disable
-#
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-
 DEFAULT_FOLDER = handwired/dactyl_manuform/6x6/promicro
