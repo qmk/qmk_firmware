b89b8a781bf7fb8d9aa741c65fdbed3feba5c76a
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-14 20:09:27 +0100

    fixes applied to pass lint checks

--- keyboards/handwired/dactyl/dactyl/teensy2/dactyl_teensy2.c
+++ keyboards/handwired/dactyl/dactyl/teensy2/dactyl_teensy2.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "dactyl.h"
+#include "dactyl_teensy2.h"
 
 #ifdef SWAP_HANDS_ENABLE
 __attribute__ ((weak))
