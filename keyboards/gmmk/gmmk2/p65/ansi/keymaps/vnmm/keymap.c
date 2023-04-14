162bbbbf60be866c1f90eab58b7e18a0f5795d81
eatmyvenom <eat.my.venomm@gmail.com>
2023-04-14 00:55:40 -0700

    port mac layer support to gmmk keyboards

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/vnmm/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/vnmm/keymap.c
@@ -30,9 +30,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,         KC_RSFT,     KC_UP,    KC_END,
   KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  WIN_F,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
-  /* Keymap WIN_FN: Function Layer
+  /* Keymap MAC_BASE: Alternate base layer available if I end up on macbook.
+   */
+[MAC_BASE] = LAYOUT(
+  QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
+  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
+  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,        KC_ENT,       KC_PGDN,
+  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,         KC_RSFT,     KC_UP,    KC_END,
+  KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RALT,  MAC_F,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+
+
+  /* Keymap WIN_FN: Function Layer WIN_BASE
    */
 [WIN_FN] = LAYOUT(
+  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,
+  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_SPI,  RGB_HUI,  RGB_SAI,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,  _______,  _______,
+  _______,  RGB_RMOD, RGB_VAD,  RGB_SPD,  RGB_HUD,  RGB_SAD,  _______,  _______,  _______,  _______,  _______,  _______,        _______,      _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  _______,  _______,  _______,         _______,     KC_VOLU,  KC_MPLY,
+  EE_CLR,   _______,  _______,                                QK_BOOT,                                _______,  _______,  KC_MPRV,  KC_VOLD,  KC_MNXT),
+
+  /* Keymap MAC_FN: Function Layer for MAC_BASE
+   */
+[MAC_FN] = LAYOUT(
   KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,
   RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_SPI,  RGB_HUI,  RGB_SAI,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,  _______,  _______,
   _______,  RGB_RMOD, RGB_VAD,  RGB_SPD,  RGB_HUD,  RGB_SAD,  _______,  _______,  _______,  _______,  _______,  _______,        _______,      _______,
--- keyboards/gmmk/pro/rev1/ansi/keymaps/vnmm/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/vnmm/keymap.c
@@ -30,8 +30,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, WIN_F,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
+    [MAC_BASE] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, WIN_F,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
     [WIN_FN] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY,
+        _______, DF_WIN,  DF_MAC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        RGB_TOG, RGB_MOD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, RGB_RMOD,RGB_VAD, RGB_SPD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, _______, _______,
+        EE_CLR,  _______, _______,                            QK_BOOT,                            _______, _______, _______, KC_MPRV, _______, KC_MNXT
+    ),
+
+    [MAC_FN] = LAYOUT(
+        _______, DF_WIN,  DF_MAC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, RGB_RMOD,RGB_VAD, RGB_SPD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______,          _______,
