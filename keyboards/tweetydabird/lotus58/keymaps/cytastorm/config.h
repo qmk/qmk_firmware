dc81122cec2d1c99e75a30fab71aeaf55b29ed68
TweetyDaBird <markus.knutsson@tweety.se>
2023-04-15 12:04:26 +0200

    Added CytaStorm keymap
    Co-Authored-By: CytaStorm <28843423+cytastorm@users.noreply.github.com>

---
+++ keyboards/tweetydabird/lotus58/keymaps/cytastorm/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#undef RGBLIGHT_LIMIT_VAL 175
+#undef RGBLIGHT_DEFAULT_VAL ( RGBLIGHT_LIMIT_VAL / 2 )
+#undef RGBLIGHT_SLEEP
+
+#undef SPLIT_OLED_ENABLE
