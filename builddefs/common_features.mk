qmk/qmk_firmware

commit bebfdad795add9fbc8c6c1393d1b817d542474ed

NVRAM refactor, phase 1. (#18969)
* Rename `eeprom_stm32` to `eeprom_legacy_emulated_flash`.

* Rename `flash_stm32` to `legacy_flash_ops`.

* Rename `eeprom_teensy` to `eeprom_kinetis_flexram`.

builddefs/common_features.mk
@@ -222,10 +222,10 @@ else
     else ifeq ($(PLATFORM),CHIBIOS)
       ifneq ($(filter %_STM32F072xB %_STM32F042x6, $(MCU_SERIES)_$(MCU_LDSCRIPT)),)
         # STM32 Emulated EEPROM, backed by MCU flash (soon to be deprecated)
-        OPT_DEFS += -DEEPROM_DRIVER -DEEPROM_STM32_FLASH_EMULATED
+        OPT_DEFS += -DEEPROM_DRIVER -DEEPROM_LEGACY_EMULATED_FLASH
         COMMON_VPATH += $(PLATFORM_PATH)/$(PLATFORM_KEY)/$(DRIVER_DIR)/flash
         COMMON_VPATH += $(DRIVER_PATH)/flash
-        SRC += eeprom_driver.c eeprom_stm32.c flash_stm32.c
+        SRC += eeprom_driver.c eeprom_legacy_emulated_flash.c legacy_flash_ops.c
       else ifneq ($(filter $(MCU_SERIES),STM32F1xx STM32F3xx STM32F4xx STM32L4xx STM32G4xx WB32F3G71xx WB32FQ95xx GD32VF103),)
         # Wear-leveling EEPROM implementation, backed by MCU flash
         OPT_DEFS += -DEEPROM_DRIVER -DEEPROM_WEAR_LEVELING
@@ -242,8 +242,8 @@ else
         WEAR_LEVELING_DRIVER = rp2040_flash
       else ifneq ($(filter $(MCU_SERIES),KL2x K20x),)
         # Teensy EEPROM implementations
-        OPT_DEFS += -DEEPROM_TEENSY
-        SRC += eeprom_teensy.c
+        OPT_DEFS += -DEEPROM_KINETIS_FLEXRAM
+        SRC += eeprom_kinetis_flexram.c
       else
         # Fall back to transient, i.e. non-persistent
         OPT_DEFS += -DEEPROM_DRIVER -DEEPROM_TRANSIENT
@@ -287,7 +287,7 @@ ifneq ($(strip $(WEAR_LEVELING_DRIVER)),none)
       POST_CONFIG_H += $(PLATFORM_PATH)/$(PLATFORM_KEY)/$(DRIVER_PATH)/wear_leveling/wear_leveling_rp2040_flash_config.h
     else ifeq ($(strip $(WEAR_LEVELING_DRIVER)), legacy)
       COMMON_VPATH += $(PLATFORM_PATH)/$(PLATFORM_KEY)/$(DRIVER_DIR)/flash
-      SRC += flash_stm32.c wear_leveling_legacy.c
+      SRC += legacy_flash_ops.c wear_leveling_legacy.c
       POST_CONFIG_H += $(PLATFORM_PATH)/$(PLATFORM_KEY)/$(DRIVER_DIR)/wear_leveling/wear_leveling_legacy_config.h
     endif
   endif

platforms/chibios/drivers/eeprom/eeprom_kinetis_flexram.c
@@ -1,7 +1,7 @@
 #include <ch.h>
 #include <hal.h>
 
-#include "eeprom_teensy.h"
+#include "eeprom_kinetis_flexram.h"
 #include "eeconfig.h"
 
 /*************************************/

platforms/chibios/drivers/eeprom/eeprom_legacy_emulated_flash.c
@@ -22,8 +22,8 @@
 #include <stdbool.h>
 #include "util.h"
 #include "debug.h"
-#include "eeprom_stm32.h"
-#include "flash_stm32.h"
+#include "eeprom_legacy_emulated_flash.h"
+#include "legacy_flash_ops.h"
 
 /*
  * We emulate eeprom by writing a snapshot compacted view of eeprom contents,
@@ -132,7 +132,7 @@
  *
  */
 
-#include "eeprom_stm32_defs.h"
+#include "eeprom_legacy_emulated_flash_defs.h"
 /* These bits are used for optimizing encoding of bytes, 0 and 1 */
 #define FEE_WORD_ENCODING 0x8000
 #define FEE_VALUE_NEXT 0x6000

platforms/chibios/drivers/flash/legacy_flash_ops.c
@@ -17,7 +17,7 @@
  */
 
 #include <hal.h>
-#include "flash_stm32.h"
+#include "legacy_flash_ops.h"
 
 #if defined(STM32F1XX)
 #    define FLASH_SR_WRPERR FLASH_SR_WRPRTERR

platforms/chibios/drivers/flash/legacy_flash_ops.h
@@ -24,7 +24,7 @@ extern "C" {
 
 #include <stdint.h>
 
-#ifdef FLASH_STM32_MOCKED
+#ifdef LEGACY_FLASH_OPS_MOCKED
 extern uint8_t FlashBuf[MOCK_FLASH_SIZE];
 #endif
 

commit 564c3764d30f2c0c22d19fe967b4257a3d69c632

Fix EEPROM_DRIVER=legacy_stm32_flash
regression was introduced in commit bebfdad795add9fbc8c6c1393d1b817d542474ed

builddefs/common_features.mk
@@ -213,10 +213,10 @@ else
     SRC += eeprom_driver.c eeprom_spi.c
   else ifeq ($(strip $(EEPROM_DRIVER)), legacy_stm32_flash)
     # STM32 Emulated EEPROM, backed by MCU flash (soon to be deprecated)
-    OPT_DEFS += -DEEPROM_DRIVER -DEEPROM_STM32_FLASH_EMULATED
+    OPT_DEFS += -DEEPROM_DRIVER -DEEPROM_LEGACY_EMULATED_FLASH
     COMMON_VPATH += $(PLATFORM_PATH)/$(PLATFORM_KEY)/$(DRIVER_DIR)/flash
     COMMON_VPATH += $(DRIVER_PATH)/flash
-    SRC += eeprom_driver.c eeprom_stm32.c flash_stm32.c
+    SRC += eeprom_driver.c eeprom_legacy_emulated_flash.c legacy_flash_ops.c
   else ifeq ($(strip $(EEPROM_DRIVER)), transient)
     # Transient EEPROM implementation -- no data storage but provides runtime area for it
     OPT_DEFS += -DEEPROM_DRIVER -DEEPROM_TRANSIENT