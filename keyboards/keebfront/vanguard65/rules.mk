200aa62f1104acaef2c00075a78de1fbb8b3fcdd
Felix Jen <fjen@felixjen.com>
2023-04-16 16:13:05 -0500

    new qmk

--- keyboards/keebfront/vanguard65/rules.mk
+++ keyboards/keebfront/vanguard65/rules.mk
@@ -11,5 +11,4 @@ COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes          # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-ENCODER_ENABLE = yes       # Enable encoder
 MIDI_ENABLE = yes          # Enable MIDI
