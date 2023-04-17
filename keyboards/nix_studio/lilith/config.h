dd97f799aeeb291616a8a6228dbbdb9621738567
arnstadm <45672752+arnstadm@users.noreply.github.com>
2023-04-17 11:20:02 +0200

    Update keyboards/nix_studio/lilith/config.h
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/nix_studio/lilith/config.h
+++ keyboards/nix_studio/lilith/config.h
@@ -3,21 +3,5 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
 /* Set HSE clock since it differs from F411 default */
 #define STM32_HSECLK 16000000
