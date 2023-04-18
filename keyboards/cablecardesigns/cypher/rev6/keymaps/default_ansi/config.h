8c478a1bb4da60678379dc1a2be280147fea0894
James Young <18669334+noroadsleft@users.noreply.github.com>
2023-04-18 08:24:50 -0700

    add LAYOUT_ansi with reference keymap

---
+++ keyboards/cablecardesigns/cypher/rev6/keymaps/default_ansi/config.h
@@ -0,0 +1,7 @@
+// Copyright 2022 Cable Car Designs (@westfoxtrot)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#define RGBLIGHT_DEFAULT_VAL 0
