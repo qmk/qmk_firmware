6bf9d86baf1feb4cdd596e4a0aad9bd9a0a2f27c
Richard Goulter <richard.goulter@gmail.com>
2023-04-19 22:17:02 +0700

    keyboard: bm40hsrgb: describe matrix in layout

--- keyboards/kprepublic/bm40hsrgb/rev2/rev2.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/rev2.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "rev2.h"
+#include "quantum.h"
 
 #if defined(RGB_MATRIX_ENABLE)
 
