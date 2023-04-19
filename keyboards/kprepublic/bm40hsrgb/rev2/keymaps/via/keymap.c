6e37e6316477344d7c46aae05992242e30fa7edf
Richard Goulter <richard.goulter@gmail.com>
2023-04-19 22:03:15 +0700

    keyboard: bm40hsrgb: use tri_layer feature in via keymap

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/via/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/via/keymap.c
@@ -22,10 +22,6 @@ enum layers {
   _ADJUST
 };
 
-#define LOWER  FN_MO13
-#define RAISE  FN_MO23
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -43,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, TL_LOWR, KC_SPC,  TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
