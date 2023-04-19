bad0f1abf980526b82a5bba3bf3b17ec5102437b
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-19 12:11:03 +0300

    Update matrix.c

--- keyboards/darkproject/kd83a_ltd/matrix.c
+++ keyboards/darkproject/kd83a_ltd/matrix.c
@@ -20,6 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "debounce.h"
 #include "quantum.h"
 
+#define rows_bits   PAL_PORT_BIT(PAL_PAD(B0)) | PAL_PORT_BIT(PAL_PAD(B1)) | PAL_PORT_BIT(PAL_PAD(B2)) | PAL_PORT_BIT(PAL_PAD(B3)) | PAL_PORT_BIT(PAL_PAD(B4)) | PAL_PORT_BIT(PAL_PAD(B5)) | PAL_PORT_BIT(PAL_PAD(B6)) | PAL_PORT_BIT(PAL_PAD(B7)) | PAL_PORT_BIT(PAL_PAD(B8)) | PAL_PORT_BIT(PAL_PAD(B9)) | PAL_PORT_BIT(PAL_PAD(B10)) | PAL_PORT_BIT(PAL_PAD(B12))
+#define cols_bits   PAL_PORT_BIT(PAL_PAD(A0)) | PAL_PORT_BIT(PAL_PAD(A1)) | PAL_PORT_BIT(PAL_PAD(A2)) | PAL_PORT_BIT(PAL_PAD(A3)) | PAL_PORT_BIT(PAL_PAD(A4)) | PAL_PORT_BIT(PAL_PAD(A8)) | PAL_PORT_BIT(PAL_PAD(A9)) | PAL_PORT_BIT(PAL_PAD(A10))
+
 /* matrix state(1:on, 0:off) */
 extern matrix_row_t raw_matrix[MATRIX_ROWS];  // raw values
 extern matrix_row_t matrix[MATRIX_ROWS];      // debounced values
@@ -32,25 +35,11 @@ static inline matrix_row_t read_cols(void) {
             (((portA_pin_state >> (PAL_PAD(A8) - 5)) & (((1U << 3) - 1U) << 5)) ^ ((((1U << 3) - 1U)) << 5)));
 }
 
-
 static inline void unselect_rows(void) {
     ATOMIC_BLOCK_FORCEON {
-    pal_lld_setport(PAL_PORT(B0),   PAL_PORT_BIT(PAL_PAD(B0)) | 
-                                    PAL_PORT_BIT(PAL_PAD(B1)) |
-                                    PAL_PORT_BIT(PAL_PAD(B2)) |
-                                    PAL_PORT_BIT(PAL_PAD(B3)) |
-                                    PAL_PORT_BIT(PAL_PAD(B4)) |
-                                    PAL_PORT_BIT(PAL_PAD(B5)) |
-                                    PAL_PORT_BIT(PAL_PAD(B6)) |
-                                    PAL_PORT_BIT(PAL_PAD(B7)) |
-                                    PAL_PORT_BIT(PAL_PAD(B8)) |
-                                    PAL_PORT_BIT(PAL_PAD(B9)) |
-                                    PAL_PORT_BIT(PAL_PAD(B10)) |
-                                    PAL_PORT_BIT(PAL_PAD(B12)));
+    pal_lld_setport(PAL_PORT(B0), rows_bits);
     }
-    
 }
-
 
 static inline void select_row(uint8_t row) {
     if (row != 9)
@@ -63,52 +52,13 @@ static inline void select_row(uint8_t row) {
         }
 }
 
-
 // Only need to init the pins. Debounce / raw matrix are initialized already for us.
 void matrix_init_custom(void) {
     // initialize key pins
     ATOMIC_BLOCK_FORCEON {
-        pal_lld_setgroupmode(PAL_PORT(B0),  PAL_PORT_BIT(PAL_PAD(B0)) | 
-                                            PAL_PORT_BIT(PAL_PAD(B1)) |
-                                            PAL_PORT_BIT(PAL_PAD(B2)) |
-                                            PAL_PORT_BIT(PAL_PAD(B3)) |
-                                            PAL_PORT_BIT(PAL_PAD(B4)) |
-                                            PAL_PORT_BIT(PAL_PAD(B5)) |
-                                            PAL_PORT_BIT(PAL_PAD(B6)) |
-                                            PAL_PORT_BIT(PAL_PAD(B7)) |
-                                            PAL_PORT_BIT(PAL_PAD(B8)) |
-                                            PAL_PORT_BIT(PAL_PAD(B9)) |
-                                            PAL_PORT_BIT(PAL_PAD(B10)) |
-                                            PAL_PORT_BIT(PAL_PAD(B12)), 
-                                            0U, 
-                                            (PAL_WB32_MODE_OUTPUT |
-                                            PAL_WB32_OTYPE_PUSHPULL | 
-                                            PAL_WB32_OSPEED_HIGH | 
-                                            PAL_WB32_CURRENT_LEVEL3));
-        pal_lld_setport(PAL_PORT(B0),   PAL_PORT_BIT(PAL_PAD(B0)) | 
-                                        PAL_PORT_BIT(PAL_PAD(B1)) |
-                                        PAL_PORT_BIT(PAL_PAD(B2)) |
-                                        PAL_PORT_BIT(PAL_PAD(B3)) |
-                                        PAL_PORT_BIT(PAL_PAD(B4)) |
-                                        PAL_PORT_BIT(PAL_PAD(B5)) |
-                                        PAL_PORT_BIT(PAL_PAD(B6)) |
-                                        PAL_PORT_BIT(PAL_PAD(B7)) |
-                                        PAL_PORT_BIT(PAL_PAD(B8)) |
-                                        PAL_PORT_BIT(PAL_PAD(B9)) |
-                                        PAL_PORT_BIT(PAL_PAD(B10)) |
-                                        PAL_PORT_BIT(PAL_PAD(B12)));
-        pal_lld_setgroupmode(PAL_PORT(A0),  PAL_PORT_BIT(PAL_PAD(A0)) | 
-                                            PAL_PORT_BIT(PAL_PAD(A1)) |
-                                            PAL_PORT_BIT(PAL_PAD(A2)) |
-                                            PAL_PORT_BIT(PAL_PAD(A3)) |
-                                            PAL_PORT_BIT(PAL_PAD(A4)) |
-                                            PAL_PORT_BIT(PAL_PAD(A8)) |
-                                            PAL_PORT_BIT(PAL_PAD(A9)) |
-                                            PAL_PORT_BIT(PAL_PAD(A10)), 
-                                            0U, 
-                                            (PAL_WB32_MODE_INPUT | 
-                                            PAL_WB32_PUPDR_PULLUP | 
-                                            PAL_WB32_OSPEED_HIGH));
+        pal_lld_setgroupmode(PAL_PORT(B0),  rows_bits, 0U, (PAL_WB32_MODE_OUTPUT | PAL_WB32_OTYPE_PUSHPULL | PAL_WB32_OSPEED_HIGH | PAL_WB32_CURRENT_LEVEL3));
+        pal_lld_setport(PAL_PORT(B0),   rows_bits);
+        pal_lld_setgroupmode(PAL_PORT(A0),  cols_bits, 0U, (PAL_WB32_MODE_INPUT | PAL_WB32_PUPDR_PULLUP | PAL_WB32_OSPEED_HIGH));
     }
 }
 
