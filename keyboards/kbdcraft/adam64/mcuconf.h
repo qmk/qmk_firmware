bfe35e92800de4349b1a79dd6f217a6598e17fdb
Adophoxia <andyao1528@gmail.com>
2023-04-18 21:08:33 -0700

    Re-fix copyright header for `mcuocnf.h` to align with other files

--- keyboards/kbdcraft/adam64/mcuconf.h
+++ keyboards/kbdcraft/adam64/mcuconf.h
@@ -1,5 +1,6 @@
-/* Copyright 2023 KBDCraft
-Copyright 2023 Adophoxia <andyao1528@gmail.com>
+/* 
+ * Copyright 2023 KBDCraft
+ * Copyright 2023 Adophoxia <andyao1528@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
