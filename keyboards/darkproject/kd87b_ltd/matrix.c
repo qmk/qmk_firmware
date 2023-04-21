3ded1c96b9360fe2bc9bf8c7162ce2f6be0c6701
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-04-21 03:55:50 +0300

    Update matrix.c

--- keyboards/darkproject/kd87b_ltd/matrix.c
+++ keyboards/darkproject/kd87b_ltd/matrix.c
@@ -81,7 +81,8 @@ uint8_t matrix_scan_custom(matrix_row_t current_matrix[]) {
         current_matrix[current_row] = cols;
  
         while ((((PAL_PORT(A0))->IDR) & ((((1U << 5) - 1U)) | (((1U << 3) - 1U) << 8))) != ((((1U << 5) - 1U)) | (((1U << 3) - 1U) << 8)))    // Wait for all Col signals to go HIGH
-            wait_us(1);
+            chSysPolledDelayX(RTC2US(REALTIME_COUNTER_CLOCK, 48));
+            //wait_us(1);
         //wait_us(MATRIX_IO_DELAY);
 
     }
