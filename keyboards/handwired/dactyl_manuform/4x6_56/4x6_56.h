fe21d7f54216397494ffe0c779d068e9abdca53e
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-04-21 14:53:43 +0100

    Update 4x6_56.h

--- keyboards/handwired/dactyl_manuform/4x6_56/4x6_56.h
+++ keyboards/handwired/dactyl_manuform/4x6_56/4x6_56.h
@@ -23,13 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define XXX KC_NO
 
 #define LAYOUT_4x6_56( \
-    L00, L01, L02, L03, L04, L05,                    R00, R01, R02, R03, R04, R05, \
-    L10, L11, L12, L13, L14, L15,                    R10, R11, R12, R13, R14, R15, \
-    L20, L21, L22, L23, L24, L25,                    R20, R21, R22, R23, R24, R25, \
-    L30, L31, L32, L33,                                        R32, R33, R34, R35, \
-                        L34, L35,                    R30, R31, \
-                        L44, L45,                    R40, R41, \
-              L42, L43,                                        R42, R43 \
+    L00, L01, L02, L03, L04, L05,                         R00, R01, R02, R03, R04, R05, \
+    L10, L11, L12, L13, L14, L15,                         R10, R11, R12, R13, R14, R15, \
+    L20, L21, L22, L23, L24, L25,                         R20, R21, R22, R23, R24, R25, \
+    L30, L31, L32, L33,                                             R32, R33, R34, R35, \
+                        L34, L35,                         R30, R31, \
+                                  L44, L45,     R40, R41, \
+                                  L42, L43,     R42, R43 \
 ) { \
     { L00, L01, L02, L03, L04, L05 }, \
     { L10, L11, L12, L13, L14, L15 }, \
