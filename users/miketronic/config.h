d90a33bad4910df1c7225821bb94e1bffba6ed25
miketronic <mikebarnoski@gmail.com>
2023-04-22 07:50:26 -0500

    userspace config refactor

--- users/miketronic/config.h
+++ users/miketronic/config.h
@@ -6,17 +6,14 @@
 // specific defines example
 // #if defined(TAP_DANCE_ENABLE) && defined(KEYBOARD_lets_split_rev2) || defined(KEYBOARD_keebio_iris_rev2)
 
-
-
 // Common QMK settings
 #define DEBOUNCE 10
 #if defined(TAP_DANCE_ENABLE)
-    #define TAP_CODE_DELAY 5
-    #define TAP_HOLD_CAPS_DELAY 25
+#    define TAP_CODE_DELAY 5
+#    define TAP_HOLD_CAPS_DELAY 25
 #endif
 #define ENCODER_RESOLUTION 4
 #define MACRO_TIMER 5
-
 
 //////////////
 // For Pi40 //
@@ -28,18 +25,17 @@
 // https://docs.qmk.fm/#/ChangeLog/20230226?id=quick-tap-term
 
 // Tap-hold settings
-    #if defined(TAP_DANCE_ENABLE)
-      #define TAPPING_TERM 170
-      #define TAPPING_TERM_PER_KEY
-      #define QUICK_TAP_TERM TAPPING_TERM / 2
-      #define QUICK_TAP_TERM_PER_KEY
-    #endif
+#    if defined(TAP_DANCE_ENABLE)
+#        define TAPPING_TERM 170
+#        define TAPPING_TERM_PER_KEY
+#        define QUICK_TAP_TERM TAPPING_TERM / 2
+#        define QUICK_TAP_TERM_PER_KEY
+#    endif
 
-#define PERMISSIVE_HOLD_PER_KEY
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#    define PERMISSIVE_HOLD_PER_KEY
+#    define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
 #endif
-
 
 ////////////////
 // For SATPAD //
@@ -47,14 +43,14 @@
 #if defined(KEYBOARD_hazel_satpad)
 
 // Tap-hold settings
-    #if defined(TAP_DANCE_ENABLE)
-      #define TAPPING_TERM 170
-      #define TAPPING_TERM_PER_KEY
-      #define QUICK_TAP_TERM TAPPING_TERM / 2
-      #define QUICK_TAP_TERM_PER_KEY
-    #endif
+#    if defined(TAP_DANCE_ENABLE)
+#        define TAPPING_TERM 170
+#        define TAPPING_TERM_PER_KEY
+#        define QUICK_TAP_TERM TAPPING_TERM / 2
+#        define QUICK_TAP_TERM_PER_KEY
+#    endif
 
-#define PERMISSIVE_HOLD_PER_KEY
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#    define PERMISSIVE_HOLD_PER_KEY
+#    define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
 #endif
