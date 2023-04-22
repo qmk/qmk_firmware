8601a3052b2fd02865360d463431ef4715bec290
eswai <>
2023-04-23 08:46:59 +0900

    Vpico

---
+++ keyboards/eswai/vpico/keymaps/default/keymap.c
@@ -0,0 +1,20 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_ortho_4x10(
+        KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,          KC_Y,          KC_U,      KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,      KC_G,          KC_H,          KC_J,      KC_K,    KC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,          KC_N,          KC_M,      KC_COMM, KC_DOT,  KC_SLSH,
+        KC_LCTL, KC_LSFT, KC_LALT, MO(_LOWER),LSFT_T(KC_SPC),LSFT_T(KC_ENT),MO(_RAISE),KC_BSPC, KC_DEL,  KC_RSFT
+    )
+};
