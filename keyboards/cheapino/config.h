ca8ab1dcf60b2f7ce4fd577104adb47a426d1104
Thomas Haukland <thomas.haukland@gmail.com>
2023-04-23 15:12:47 +0200

    RGB matrix works

--- keyboards/cheapino/config.h
+++ keyboards/cheapino/config.h
@@ -20,6 +20,12 @@
 //#define NO_ACTION_ONESHOT
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
+#define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
+//#define WS2812_TRST_US 80
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
+#define RGB_MATRIX_DEFAULT_VAL 32
+
 
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 230
@@ -27,3 +33,11 @@
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 //#define RETRO_TAPPING
+
+#ifdef RGB_MATRIX_ENABLE
+//     #define NOP_FUDGE 0.4
+     #define RGB_DI_PIN GP16                               // The pin connected to the data pin of the LEDs
+     #define RGB_MATRIX_LED_COUNT 1                     // The number of LEDs connected
+     #define ENABLE_RGB_MATRIX_SINGLE_COLOR                // Single hue brightness cycling animation
+     #define ENABLE_RGB_MATRIX_BREATHING
+#endif
--- keyboards/cheapino/rules.mk
+++ keyboards/cheapino/rules.mk
@@ -1,4 +1,5 @@
 CAPS_WORD_ENABLE = yes
 CUSTOM_MATRIX = lite
+WS2812_DRIVER = vendor
 SRC += encoder.c
 SRC += matrix.c
