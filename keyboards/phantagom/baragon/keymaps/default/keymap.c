e9a2e2fd6c0688b7756850c1eb981ec664c0c9fd
Dennis Kruyt <dennis@kruyt.org>
2023-04-24 16:51:09 +0200

    Update keyboards/phantagom/baragon/keymaps/default/keymap.c
    Co-authored-by: Pablo Martínez <58857054+elpekenin@users.noreply.github.com>

--- keyboards/phantagom/baragon/keymaps/default/keymap.c
+++ keyboards/phantagom/baragon/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ enum Layer {
 #define _NP _NUMPAD
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_numpad_3x4( /* Base */
+  [0] = LAYOUT( /* Base */
     KC_P1,  KC_P2, KC_P3, KC_P4,
     KC_P5,  KC_P6, KC_P7,
     KC_P8,   MO(1),   KC_P9   
