1b9ddc581739e75e95d6738a257e43dac77a4cc8
Drashna Jaelre <drashna@live.com>
2023-04-24 00:25:43 -0700

    Apply suggestions from code review
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- quantum/eeconfig.c
+++ quantum/eeconfig.c
@@ -58,7 +58,8 @@ void eeconfig_init_quantum(void) {
     eeprom_update_byte(EECONFIG_STENOMODE, 0);
     eeprom_update_dword(EECONFIG_HAPTIC, 0);
     eeprom_update_byte(EECONFIG_VELOCIKEY, 0);
-    eeprom_update_block(0, EECONFIG_RGB_MATRIX, sizeof(uint64_t));
+    uint64_t dummy = 0;
+    eeprom_update_block(&dummy, EECONFIG_RGB_MATRIX, sizeof(uint64_t));
 
 #if defined(HAPTIC_ENABLE)
     haptic_reset();
