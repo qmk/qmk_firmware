9ebe4913307664235a0c7662439781e3bb4150fc
HJ <hej163@163.com>
2023-04-25 04:38:27 +0800

    [Keyboard] Add mokey12x2 keyboard
    [Keyboard] Add mokey12x2 keyboard

--- keyboards/mokey/mokey12x2/keymaps/default/keymap.c
+++ keyboards/mokey/mokey12x2/keymaps/default/keymap.c
@@ -16,7 +16,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,
         KC_CAPS, KC_A,    KC_S,    KC_D,
