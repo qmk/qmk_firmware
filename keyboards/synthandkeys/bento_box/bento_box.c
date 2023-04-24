aa25faeb96804d8de8036f25cda88f3c61d58c61
Synth and Keys <117340582+Synth-and-Keys@users.noreply.github.com>
2023-04-23 18:49:24 -0600

    Revising keyboard.c

--- keyboards/synthandkeys/bento_box/bento_box.c
+++ keyboards/synthandkeys/bento_box/bento_box.c
@@ -1,3 +1,8 @@
+// Copyright 2023 Synth-and-Keys (@Synth-and-Keys)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "quantum.h"
+
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
