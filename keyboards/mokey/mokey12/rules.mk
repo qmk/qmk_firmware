6615551883636d5f68ed538a7a1dbef8f79ff984
rhmokey <57172003+rhmokey@users.noreply.github.com>
2023-04-25 02:36:44 +0800

    Update keyboards/mokey/mokey12/rules.mk
    # Build Options
    #   change yes to no to disable
    #
    BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
    MOUSEKEY_ENABLE = yes	# Mouse keys
    EXTRAKEY_ENABLE = yes	# Audio control and System control
    CONSOLE_ENABLE = no	# Console for debug
    COMMAND_ENABLE = no    # Commands for debug and configuration
    NKRO_ENABLE = yes           # Enable N-Key Rollover
    BACKLIGHT_ENABLE = no
    RGBLIGHT_ENABLE = yes
    SLEEP_LED_ENABLE = no
    WS2812_DRIVER = spi
    ENCODER_ENABLE = yes
    
    # Enter lower-power sleep mode when on the ChibiOS idle thread
    OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
    
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/mokey/mokey12/rules.mk
+++ keyboards/mokey/mokey12/rules.mk
@@ -1,15 +1,15 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
+CONSOLE_ENABLE = no	# Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no
 RGBLIGHT_ENABLE = yes
-SLEEP_LED_ENABLE = yes
+SLEEP_LED_ENABLE = no
 WS2812_DRIVER = spi
 ENCODER_ENABLE = yes
 
