72e36f5d1d9bc9a8d0c9a41e093c46cd2138b9df
John Stegeman <john.stegeman@gmail.com>
2023-04-24 10:14:07 -0400

    Brought into compliance with coding standards

--- keyboards/keebio/iris/keymaps/johnstegeman/keymap.c
+++ keyboards/keebio/iris/keymaps/johnstegeman/keymap.c
@@ -1,10 +1,29 @@
+/*
+Copyright 2023 John Stegeman
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include QMK_KEYBOARD_H
 #include "light_layers.h"
 
-#define _COLEMAK 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+enum layers {
+_COLEMAK=0,
+_LOWER,
+_RAISE,
+_ADJUST,
+};
 
 // Enter when tapped, shift when held
 #define SHEN MT(MOD_LSFT, KC_ENT) 
