cacc26b0b820a1b62c47232ae7916cfa8d907c97
Mike Killewald <mkillewald@yahoo.com>
2023-04-25 20:02:11 -0400

    code reorg utilizing userspace folder

--- keyboards/keychron/q1/ansi/keymaps/mkillewald/config.h
+++ keyboards/keychron/q1/ansi/keymaps/mkillewald/config.h
@@ -16,15 +16,13 @@
 
 #pragma once
 
-#define EECONFIG_USER_DATA_SIZE 20  // size of eeprom data, do not change.
-
 // default options applied when eeprom is cleared
 #define DEFAULT_CAPS_LOCK_LIGHT_TAB      0            // 1 = enable, 0 = disable
 #define DEFAULT_CAPS_LOCK_LIGHT_ALPHA    1            // 1 = enable, 0 = disable
 #define DEFAULT_FN_LAYER_TRANSPARENT_OFF 0            // 1 = enable, 0 = disable
 #define DEFAULT_FN_LAYER_SHOW_COLOR      1            // 1 = enable, 0 = disable
-#define DEFAULT_RGB_ENABLE_MAC_BASE          1            // 1 = enable, 0 = disable
-#define DEFAULT_RGB_ENABLE_WIN_BASE          1            // 1 = enable, 0 = disable
+#define DEFAULT_RGB_ENABLE_MAC_BASE      1            // 1 = enable, 0 = disable
+#define DEFAULT_RGB_ENABLE_WIN_BASE      1            // 1 = enable, 0 = disable
 #define DEFAULT_HUE_WIN_BASE             80           // HSV green hue (Win Base layer)
 #define RGB_MATRIX_DEFAULT_HUE           0            // HSV red hue  (Mac Base layer)
 #define RGB_MATRIX_DEFAULT_SAT           255          // Both Win/Mac Base layers
--- keyboards/keychron/q1/ansi/keymaps/mkillewald/rules.mk
+++ keyboards/keychron/q1/ansi/keymaps/mkillewald/rules.mk
@@ -2,11 +2,5 @@ VIA_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 AUTOCORRECT_ENABLE = yes
 
-SRC += keymap_user_config.c
-
-ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
-    SRC += rgb_matrix_user.c
-endif
-
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
