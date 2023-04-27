7f50118995f8835c48cf97ae2d2f857efbedfe57
Ian Sterling <80361942+iansterlingaaa@users.noreply.github.com>
2023-04-27 09:02:00 -0700

    Changes to KeyboardIO Atreus layout.

--- keyboards/keyboardio/atreus/keymaps/xyverz/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/xyverz/keymap.c
@@ -24,8 +24,6 @@
 
 enum layer_names {
   _DVORAK,
-  _QWERTY,
-  _COLEMAK,
   _DVORMAC,
   _LOWER,
   _RAISE,
@@ -34,8 +32,6 @@ enum layer_names {
 
 enum planck_keycodes {
   DVORAK = SAFE_RANGE,
-  QWERTY,
-  COLEMAK,
   DVORMAC,
   LOWER,
   RAISE,
@@ -52,6 +48,7 @@ enum planck_keycodes {
 #define DELGUI GUI_T(KC_DEL)
 #define DELCTL CTL_T(KC_DEL)
 #define MACLOCK LGUI(LCTL(KC_Q))
+#define WINLOCK LGUI(KC_L)
 #define ADJUST MO(_ADJUST)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
@@ -72,40 +69,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S   ,
     SFTSCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_HOME, KC_END,  KC_B,    KC_M,    KC_W,    KC_V,    SFTZED ,
     ESCTRL,  TABALT,  KC_LGUI, LOWER,   KC_BSPC, DELCTL,  ALTENT,  KC_SPC,  RAISE,   KC_MINS, KC_SLSH, KC_BSLS
-  ),
-
-  /* QWERTY Layer
-	,----------------------------------.              ,----------------------------------.
-	|   Q  |   W  |   E  |   R  |   T  |              |   Y  |   U  |   I  |   O  |   P  |
-	|------+------+------+------+------|              |------+------+------+------+------|
-	|   A  |   S  |   D  |   F  |   G  |              |   H  |   J  |   K  |   L  |   ;  |
-	|------+------+------+------+------+------.,------+------+------+------+------+------|
-	|SFT/ Z|   X  |   C  |   V  |   B  | Home || End  |   N  |   M  |   ,  |   .  |SFT/ /|
-	|------+------+------+------+------+------||------+------+------+------+------+------|
-	|  Esc |  Tab |  GUI | LOWER| BkSp | LCTL ||ALTENT|  Spc | RAISE|   -  |   '  |   \  |
-	`-----------------------------------------'`-----------------------------------------' */
-  [_QWERTY] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P   ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    SFTZED,  KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME, KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  SFTSLSH,
-    ESCTRL,  TABALT,  KC_LGUI, LOWER,   KC_BSPC, KC_LCTL, ALTENT,  KC_SPC,  RAISE,   KC_MINS, KC_QUOT, KC_BSLS
-  ),
-
-  /* Colemak Layer
-	,----------------------------------.              ,----------------------------------.
-	|   Q  |   W  |   F  |   P  |   G  |              |   J  |   L  |   U  |   Y  |   L  |
-	|------+------+------+------+------|              |------+------+------+------+------|
-	|   A  |   R  |   S  |   T  |   D  |              |   H  |   N  |   E  |   I  |   S  |
-	|------+------+------+------+------+------.,------+------+------+------+------+------|
-	|SFT/ Z|   X  |   C  |   V  |   B  | Home || End  |   K  |   M  |   ,  |   .  |SFT/ /|
-	|------+------+------+------+------+------||------+------+------+------+------+------|
-	|  Esc |  Tab |  GUI | LOWER| BkSp | LCTL ||ALTENT|  Spc | RAISE|   -  |   '  |   \  |
-	`-----------------------------------------'`-----------------------------------------'*/
-  [_COLEMAK] = LAYOUT(
-    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
-    KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O   ,
-    SFTZED,  KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME, KC_END,  KC_K,    KC_M,    KC_COMM, KC_DOT,  SFTSLSH,
-    ESCTRL,  TABALT,  KC_LGUI, LOWER,   KC_BSPC, KC_LCTL, ALTENT,  KC_SPC,  RAISE,   KC_MINS, KC_QUOT, KC_BSLS
   ),
 
   /* Dvorak Layer with Command key on left thumb instead of Control
@@ -131,14 +94,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	|------+------+------+------+------|              |------+------+------+------+------|
 	| CAPS |      |  UP  |      | Home |              | PgDn |      |   +  |   {  |   }  |
 	|------+------+------+------+------+------.,------+------+------+------+------+------|
-	|      | Left | Down | Right| End  |      ||      | PgUp | Mute | Vol- | Vol+ |      |
+	|      | Left | Down | Right| End  |MacLck||WinLck| PgUp | Mute | Vol- | Vol+ |      |
 	|------+------+------+------+------+------||------+------+------+------+------+------|
 	|   ~  |      |      |      | Del  |      ||      | Ins  |      |      |      |      |
 	`-----------------------------------------'`-----------------------------------------'*/
   [_LOWER] = LAYOUT(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     KC_CAPS, _______, KC_UP,   _______, KC_HOME,                   KC_PGUP, _______, KC_PLUS, KC_LCBR, KC_RCBR,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______, _______, KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  MACLOCK, WINLOCK, KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, _______,
     KC_TILD, KC_LEFT, KC_RGHT, _______, KC_DEL,  _______, _______, KC_INS,  _______, KC_UP,   KC_DOWN, _______
   ),
 
@@ -148,14 +111,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	|------+------+------+------+------|              |------+------+------+------+------|
 	| CAPS |      |  UP  |      | Home |              | PgDn |      |   =  |   [  |   ]  |
 	|------+------+------+------+------+------.,------+------+------+------+------+------|
-	|      | Left | Down | Right| End  |MacLck||      | PgUp | Prev | Play | Next |      |
+	|      | Left | Down | Right| End  |MacLck||WinLck| PgUp | Prev | Play | Next |      |
 	|------+------+------+------+------+------||------+------+------+------+------+------|
 	|   `  |      |      |      | Del  |      ||      | Ins  |      |      |      |      |
 	`-----------------------------------------'`-----------------------------------------'*/
   [_RAISE] = LAYOUT(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0   ,
     KC_CAPS, _______, KC_UP,   _______, KC_HOME,                   KC_PGUP, _______, KC_EQL,  KC_LBRC, KC_RBRC,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  MACLOCK, _______, KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  MACLOCK, WINLOCK, KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, _______,
     KC_GRV,  KC_LEFT, KC_RGHT, _______, KC_DEL,  _______, _______, KC_INS,  _______, KC_UP,   KC_DOWN, _______
   ),
 
@@ -163,16 +126,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	,----------------------------------.              ,----------------------------------.
 	|  F1  |  F2  |  F3  |  F4  |  F5  |              |  F6  |  F7  |  F8  |  F9  | F10  |
 	|------+------+------+------+------|              |------+------+------+------+------|
-	|  F11 |      |      |      |      |              |      | PrSc | ScLk | Paus | F12  |
+	|  F11 |      |      |DVORAK|DVORMC|              |      | PrSc | ScLk | Paus | F12  |
 	|------+------+------+------+------+------.,------+------+------+------+------+------|
-	|      |QWERTY|COLEMK|DVORAK|DVORMC|      ||      |      |      |      |      |      |
+	|      |      |      |      |      |      ||      |      |      |      |      |      |
 	|------+------+------+------+------+------||------+------+------+------+------+------|
 	|      |      |      |      |      |      ||      |      |      |      |      | QK_BOOT|
 	`-----------------------------------------'`-----------------------------------------'*/
   [_ADJUST] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10 ,
-    KC_F11,  _______, _______, _______, _______,                   _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_F12 ,
-    _______, QWERTY,  COLEMAK, DVORAK,  DVORMAC, _______, _______, _______, _______, _______, _______, _______,
+    KC_F11,  _______, _______, DVORAK,  DVORMAC,                   _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_F12 ,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT  
   ),
 };
@@ -185,16 +148,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case DVORAK:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_DVORAK);
-      }
-      return false;
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
       }
       return false;
     case DVORMAC:
