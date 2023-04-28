82f8456061c440b6a4affa3241285a5111770769
biuboom-jun <65760331+biuboom-jun@users.noreply.github.com>
2023-04-28 13:53:37 +0800

    Update config.h

--- keyboards/kibou/fukuro/config.h
+++ keyboards/kibou/fukuro/config.h
@@ -4,7 +4,7 @@
 #pragma once
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE    1
+//#define DEBOUNCE    1
 
 #define TAP_CODE_DELAY 50
 
@@ -12,9 +12,6 @@
 
 /* turn off effects when suspended */
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
-
-/* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
-#define USB_POLLING_INTERVAL_MS 1
 
 /* Bootmagic Lite key configuration */
 #define BOOTMAGIC_LITE_ROW 0
