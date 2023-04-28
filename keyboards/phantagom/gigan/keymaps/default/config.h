e8ee756898e9dd668e1d8e68534a57c20714230e
Dennis Kruyt <dennis@kruyt.org>
2023-04-28 14:12:01 +0200

    license info

--- keyboards/phantagom/gigan/keymaps/default/config.h
+++ keyboards/phantagom/gigan/keymaps/default/config.h
@@ -1,1 +1,4 @@
+// Copyright 2023 <dennis@kruyt.org>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
--- keyboards/phantagom/gigan/keymaps/via/keymap.c
+++ keyboards/phantagom/gigan/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-// Copyright 2022 Dennis Kruyt
+// Copyright 2023 <dennis@kruyt.org>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
