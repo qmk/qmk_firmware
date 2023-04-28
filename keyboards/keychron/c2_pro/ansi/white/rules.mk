d208577c4f609a9c50a3fb836692be77a2780525
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-04-28 17:56:34 +0800

    Add Keychron C2 Pro

---
+++ keyboards/keychron/c2_pro/ansi/white/rules.mk
@@ -0,0 +1,13 @@
+# Build Options
+#   change yes to no to disable.
+#
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+# custom matrix setup
+CUSTOM_MATRIX = lite
+
+SRC += matrix.c
