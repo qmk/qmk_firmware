26d18e816c11bfa8152ab008cda54bc35a7249ad
Ex3c4Def <Joe_No@gmx.net>
2023-04-28 16:46:38 +0200

    added via keymap - plus some extra RGB effects - The firmware size is approaching the maximum - 28562/28672 (99%, 110 bytes free) - the json file for the via configurator is included: kimiko_rev2_via.json  <br />
    Signed-off-by: Ex3c4Def <Joe_No@gmx.net>

---
+++ keyboards/keycapsss/kimiko/keymaps/rev2_via/rules.mk
@@ -0,0 +1,19 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+ENCODER_MAP_ENABLE = yes
+
+RGBLIGHT_ENABLE = no      # Enable keyboard 'old' RGB lightning
+RGB_MATRIX_ENABLE = yes   # Enable keyboard RGB Matrix lightning
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pimoroni_trackball
+# POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+
+# LTO: Link Time Optimizations.
+# Reduce compiled size, but will automatically disable the legacy TMK Macros and Functions features.
+# This does not affect QMK Macros and Layers
+LTO_ENABLE = yes
+
+VIA_ENABLE = yes
