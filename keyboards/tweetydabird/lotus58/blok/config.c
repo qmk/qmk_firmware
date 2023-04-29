256ebd5477263a92ee41ad437ffab19cd49cc31a
Markus Knutsson <markus.knutsson@tweety.se>
2023-04-29 20:42:07 +0200

    Update keyboards/tweetydabird/lotus58/blok/config.c
    Co-authored-by: Sergey Vlasov <sigprof@gmail.com>

--- keyboards/tweetydabird/lotus58/blok/config.c
+++ keyboards/tweetydabird/lotus58/blok/config.c
@@ -5,7 +5,6 @@
 //using raw pin values, blok converter doesn't catch this
 #define I2C0_SDA_PIN 17U
 #define I2C0_SCL_PIN 16U
-#define I2C0_CLOCK_SPEED  400000
 
 #define SPLIT_MODS_ENABLE
 #define SPLIT_TRANSPORT_MIRROR
