ed07e9ede63abe040ce51964c37ed43e0a24f6e4
Jerome Berclaz <jerome.berclaz@a3.epfl.ch>
2023-04-29 07:58:23 -0700

    cleanup

--- keyboards/modelh/rules.mk
+++ keyboards/modelh/rules.mk
@@ -12,10 +12,10 @@ DFU_SUFFIX_ARGS = -v 1eaf -p 0003
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
-#BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
-COMMAND_ENABLE = yes    # Commands for debug and configuration
+CONSOLE_ENABLE = no	# Console for debug
+COMMAND_ENABLE = no     # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no	# USB Nkey Rollover
 CUSTOM_MATRIX = no 	# Custom matrix file
