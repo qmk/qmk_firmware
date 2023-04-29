dabf2a18ba954ac5b6a2d1dec6c71ee457bf7cd2
TweetyDaBird <markus.knutsson@tweety.se>
2023-04-29 18:16:21 +0200

    added headers

--- keyboards/tweetydabird/lotus58/blok/config.c
+++ keyboards/tweetydabird/lotus58/blok/config.c
@@ -1,3 +1,6 @@
+// Copyright 2023 Markus Knutsson (@TweetyDaBird)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #define I2C_DRIVER I2CD0
 //using raw pin values, blok converter doesn't catch this
 #define I2C0_SDA_PIN 17U
