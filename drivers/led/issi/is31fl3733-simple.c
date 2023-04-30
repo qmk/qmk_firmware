6a619e64037d54e04148e1ee7e1c170533d49236
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2023-04-30 12:35:27 +1000

    [Core] Clean up ISSI drivers, Add IS31FL3736 support (#20572)
    Co-authored-by: Pablo Martínez <58857054+elpekenin@users.noreply.github.com>

--- drivers/led/issi/is31fl3733-simple.c
+++ drivers/led/issi/is31fl3733-simple.c
@@ -129,9 +129,7 @@ bool IS31FL3733_write_pwm_buffer(uint8_t addr, uint8_t *pwm_buffer) {
         // Copy the data from i to i+15.
         // Device will auto-increment register for data after the first byte
         // Thus this sets registers 0x00-0x0F, 0x10-0x1F, etc. in one transfer.
-        for (int j = 0; j < 16; j++) {
-            g_twi_transfer_buffer[1 + j] = pwm_buffer[i + j];
-        }
+        memcpy(g_twi_transfer_buffer + 1, pwm_buffer + i, 16);
 
 #if ISSI_PERSISTENCE > 0
         for (uint8_t i = 0; i < ISSI_PERSISTENCE; i++) {
