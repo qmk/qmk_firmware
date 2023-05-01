5fef5d3c853072f5058867399af66749021203ac
Adophoxia <andyao1528@gmail.com>
2023-04-30 19:33:29 -0700

    Update copyright

--- keyboards/fancytech/fancyalice66/fancyalice66.c
+++ keyboards/fancytech/fancyalice66/fancyalice66.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 chent7 <https://github.com/chent7>
+/* Copyright 2023 Adophoxia <https://github.com/Adophoxia>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
