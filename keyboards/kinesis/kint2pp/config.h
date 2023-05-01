2944e938703e2049bb6262a0ab31e701d27e8ab3
Ryan <fauxpark@gmail.com>
2023-05-01 17:37:12 +1000

    Move `USB_MAX_POWER_CONSUMPTION` to data driven (#20648)

--- keyboards/kinesis/kint2pp/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// The Teensy 2++ consumes about 60 mA of current at its full speed of 16 MHz as
-// per https://www.pjrc.com/teensy/low_power.html
-#define USB_MAX_POWER_CONSUMPTION 100
