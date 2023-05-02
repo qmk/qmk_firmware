213dad3c3f70ac2ae106e6e8d2e551a47bf98d05
Casey Tucker <dctucker@hotmail.com>
2023-05-02 16:07:52 -0700

    dctucker ergodox_ez comma on left

--- keyboards/ergodox_ez/keymaps/dctucker/keymap.c
+++ keyboards/ergodox_ez/keymaps/dctucker/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , LT1SIX ,  LT2FIVE, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS,
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_EQL ,  KC_BSLS, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , MONEYQ ,
 		CTLESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                    KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-		KC_LSFT, KC_Z   , MOUSE1 , KC_X   , KC_C   , KC_V   , KC_B   ,  KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, RSHL4  ,
+		KC_LSFT, KC_Z   , KC_COMM, KC_X   , KC_C   , KC_V   , KC_B   ,  MOUSE1 , KC_B   , KC_N   , KC_M   , KC_DOT , KC_SLSH, RSHL4  ,
 		CTLDEL , KC_F1  , KC_PPLS, KC_LALT, KC_LGUI,                                      KC_RGUI, KC_RALT, KC_PMNS, KC_LBRC, KC_RBRC,
 		                                             KC_HOME, KC_END ,  KC_LEFT, KC_RGHT,
 		                                                      KC_PGUP,  KC_UP  ,
@@ -74,12 +74,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT_ergodox_pretty(
 		_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , _______,  KC_PIPE, KC_CIRC, KC_AMPR, KC_PAST, KC_LPRN, KC_RPRN, _______,
 		_______, _______, _______, _______, _______, _______, _______,  KC_PIPE, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_BSLS,
-		_______, _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, _______,
+		KC_ESC , _______, _______, _______, _______, _______,                    KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, _______,
 		_______, _______, _______, _______, _______, _______, _______,  _______, BACKN  , KC_MINS, EARROW , ARROW  , COMMENT, _______,
 		_______, _______, _______, _______, _______,                                      _______, _______, _______, KC_LPRN, KC_RPRN,
 		                                             KC_F10 , KC_F11 ,  KC_F14 , KC_F15 ,
 		                                                      KC_F9  ,  KC_F13 ,
-		                                    H12    , _______, KC_F12 ,  KC_F16 , _______, H16
+		                                    H12    , _______, KC_F12 ,  KC_F16 , KC_PENT, H16
 	),
 	[4] = LAYOUT_ergodox_pretty(
 		_______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
