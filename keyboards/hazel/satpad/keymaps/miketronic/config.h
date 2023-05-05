67427f7479ac28eaae96ba88fef2ea9ecaabf41f
miketronic <mikebarnoski@gmail.com>
2023-05-05 13:41:04 -0500

    Delete keyboards/hazel/satpad directory

--- keyboards/hazel/satpad/keymaps/miketronic/config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#    define TAPPING_TERM 170
-#    define TAPPING_TERM_PER_KEY
-#    define QUICK_TAP_TERM TAPPING_TERM / 2
-#    define QUICK_TAP_TERM_PER_KEY
-#    define PERMISSIVE_HOLD_PER_KEY
-#    define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
