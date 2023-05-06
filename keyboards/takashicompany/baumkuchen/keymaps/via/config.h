269b866dc1f5d3a379f05273c9e1ea308fc6fe4d
takashicompany <t@kashi.company>
2023-05-06 23:19:52 +0900

    via

---
+++ keyboards/takashicompany/baumkuchen/keymaps/via/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 takashicompany (@takashicompany)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 24
+
+#define ENCODERS_CW_KEY  { { 13, 0 }}
+#define ENCODERS_CCW_KEY { { 14, 0 }}
