1731c8b5c884bf977b2841b25fe5e55f7e434d89
takashicompany <t@kashi.company>
2023-05-07 02:14:42 +0900

    もろもろ修正

--- keyboards/takashicompany/baumkuchen/keymaps/default/keymap.c
+++ keyboards/takashicompany/baumkuchen/keymaps/default/keymap.c
@@ -4,17 +4,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R,
         KC_A, KC_S, KC_D, KC_F,
-        KC_Z, KC_X, KC_C, KC_V, KC_B,
-                                KC_N, KC_M
+        KC_Z, KC_X, KC_C, LT(1, KC_V), KC_B,
+                                KC_NO, KC_NO
+    ),
+    [1] = LAYOUT(
+        RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS,
+        KC_TRNS, RGB_HUI, RGB_SAI, RGB_VAI,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                   KC_TRNS, KC_TRNS
     )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
 
     if (clockwise) {
-        tap_code(KC_MS_WH_DOWN);
+        tap_code(KC_WH_D);
     } else {
-        tap_code(KC_MS_WH_UP);
+        tap_code(KC_WH_U);
     }
 
     return true;
--- keyboards/takashicompany/baumkuchen/keymaps/via/keymap.c
+++ keyboards/takashicompany/baumkuchen/keymaps/via/keymap.c
@@ -2,10 +2,24 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R,
-        KC_A, KC_S, KC_D, KC_F,
-        KC_Z, KC_X, KC_C, KC_V, KC_B,
-                                KC_N, KC_M
+        LT(1, KC_ESC), KC_Q, KC_W, KC_E, 
+        KC_TAB, KC_A, KC_S, KC_D,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V,
+        KC_WH_U, KC_WH_D
+    ),
+
+    [1] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, MO(0), KC_TRNS,
+        KC_TRNS,KC_TRNS
+    ),
+
+    [2] = LAYOUT(
+        RGB_TOG, RGB_MOD, KC_TRNS, QK_BOOT,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,KC_TRNS
     )
 };
 
