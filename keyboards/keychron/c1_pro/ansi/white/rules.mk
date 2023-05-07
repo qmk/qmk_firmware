b615bc7ec43ec1229e6c7a08208826688c694418
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-05-07 11:06:16 +0800

    Add c1_pro/rule.mk

--- keyboards/keychron/c1_pro/ansi/white/rules.mk
+++ keyboards/keychron/c1_pro/ansi/white/rules.mk
@@ -1,5 +1,1 @@
-# Build Options
-#   change yes to no to disable.
-#
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+# This file intentionally left blank
