bbed85607efb0438197c710fd90ba0a7a8868f71
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-05-07 10:22:14 +0800

    Update keyboards/keychron/c2_pro/ansi/rgb/rules.mk
    Co-authored-by: adophoxia <100170946+adophoxia@users.noreply.github.com>

--- keyboards/keychron/c2_pro/ansi/rgb/rules.mk
+++ keyboards/keychron/c2_pro/ansi/rgb/rules.mk
@@ -1,11 +1,1 @@
-# Build Options
-#   change yes to no to disable.
-#
-
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-# custom matrix setup
-CUSTOM_MATRIX = lite
-
-SRC += matrix.c
+# This file intentionally left blank
