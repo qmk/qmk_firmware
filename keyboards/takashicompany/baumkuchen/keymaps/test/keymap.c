3482cc00d72be71d6c8868e98422ea1d3a05d4f0
takashicompany <t@kashi.company>
2023-05-07 15:54:48 +0900

    for test

---
+++ keyboards/takashicompany/baumkuchen/keymaps/test/keymap.c
@@ -0,0 +1,28 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_Q, KC_W, KC_E, KC_R,
+        KC_A, KC_S, KC_D, KC_F,
+        KC_Z, KC_X, KC_C, KC_V, KC_B,
+                                KC_NO, KC_NO
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    
+    rgblight_toggle();  // for test
+
+    return true;
+}
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+
+    if (clockwise) {
+        tap_code(KC_1);
+    } else {
+        tap_code(KC_2);
+    }
+
+    return true;
+}
