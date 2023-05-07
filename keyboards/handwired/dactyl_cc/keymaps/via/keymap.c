d4105ed16964b5e5903877258cf65d034b4b5773
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-05-07 18:56:13 +0100

    Update keymap.c

--- keyboards/handwired/dactyl_cc/keymaps/via/keymap.c
+++ keyboards/handwired/dactyl_cc/keymaps/via/keymap.c
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      _______, _______, _______,     _______, _______,   MO(3)
     ),
     [_SYS] = LAYOUT_dactylk(
-        KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                           KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  QK_F12,
+        KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                           KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  KC_F12,
         _______, KC_VOLU, KC_WBAK, KC_MS_U, KC_WFWD, KC_WH_U,                                         _______,   KC_NO,   KC_NO,  KC_NO,   KC_NO, _______,
         _______, KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                                         _______, KC_PSCR, KC_PAUS, KC_INS, KC_SCRL, _______,
         _______, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP,                                         _______,   KC_NO,   KC_NO,  KC_NO,   KC_NO, _______,
-                 _______, _______, _______, _______,                                                           _______, _______,  KC_NO,   KC_NO, 
+                 _______, _______, _______, _______,                                                           _______, _______,  KC_UP, KC_RGHT, 
                                                               _______, _______,     _______, _______,
                                                                        _______,     _______,
                                                      KC_BTN1, KC_BTN2, _______,     _______, _______, _______
