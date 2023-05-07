78457ba18b2903ca44f4f5d39efcbe4c681783da
Coom <kazunori.takahashi.g@gmail.com>
2023-05-07 17:22:05 +0900

    Tweaked QMK Configuration for VIA.

--- keyboards/handwired/ziyoulang_k3_mod/keymaps/via/rules.mk
+++ keyboards/handwired/ziyoulang_k3_mod/keymaps/via/rules.mk
@@ -1,5 +1,5 @@
 VIA_ENABLE = yes
-
-BOOTMAGIC_ENABLE = lite
-
+# Blue Pill STM32F103C6T6 which only has 22KB of available space.
+# In order to reduce the size of Firmware, disabling mousekey and via LTO were necessary.
+MOUSEKEY_ENABLE = no
 LTO_ENABLE = yes
