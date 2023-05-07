b706967db0abd3fe0377ae7eaddf96574602ae68
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-05-07 18:52:10 +0100

    Update keymap.c

--- keyboards/handwired/dactyl_cc/keymaps/via/keymap.c
+++ keyboards/handwired/dactyl_cc/keymaps/via/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT_dactylk(
         _______, _______, _______, _______, _______, _______,                                         _______,  KC_NUM, KC_PEQL, KC_PSLS, KC_PAST, _______,
         _______, _______, _______, _______, _______, _______,                                         _______,   KC_P7,   KC_P8,   KC_P9, KC_PMNS, _______,
-        _______, _______, _______, _______, _______, _______,                                         _______,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
+        _______, QK_BOOT, _______, _______, KC_SLEP, _______,                                         _______,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
         _______, _______, _______, _______, _______, _______,                                         _______,   KC_P1,   KC_P2,   KC_P3, KC_PENT, _______,
                  _______, _______, _______, _______,                                                           _______, _______, KC_PDOT, KC_PENT, 
                                                               _______, _______,     _______,   KC_P0,
@@ -66,13 +66,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
     [_BLANK] = LAYOUT_dactylk(
-        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO,                                   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO,                                   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO,                                   KC_NO, KC_SLEP, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO,                                   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-               KC_NO, KC_NO, KC_NO,   KC_NO,                                                   KC_NO, KC_NO, KC_NO, KC_NO,
-                                                    KC_NO, KC_NO,       KC_NO, KC_NO,
-                                                           KC_NO,       KC_NO,
-                                             KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO,  
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+               KC_NO, KC_NO, KC_NO, KC_NO,                                                 KC_NO, KC_NO, KC_NO, KC_NO,
+                                                  KC_NO, KC_NO,       KC_NO, KC_NO,
+                                                         KC_NO,       KC_NO,
+                                           KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO,  
     )
 };
