71eb864b448a70b8e24a7e1df58c741f3e13a5f8
ziptyze <mwcordier@gmail.com>
2023-05-08 13:10:32 -0400

    update to super16v2

--- keyboards/1upkeyboards/super16v2/config.h
+++ keyboards/1upkeyboards/super16v2/config.h
@@ -17,30 +17,10 @@
 #pragma once
 
 
-#define MOUSEKEY_MOVE_DELTA 25
-
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
-
-//                         0   1   2   3
-#define MATRIX_ROW_PINS { D1, D2, D3, D4 }
-#define MATRIX_COL_PINS { D5, D6, C2, D0 }
-
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
 #define RGB_DI_PIN B5
 #define RGB_MATRIX_LED_COUNT 20
 #ifdef RGB_DI_PIN
-#    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+//#    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 #    define RGBLIGHT_LIMIT_VAL 255
 #endif
 #ifdef RGB_MATRIX_ENABLE
@@ -49,7 +29,7 @@
 // RGB Matrix Animation modes. Explicitly enabled
 // For full list of effects, see:
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
-#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
+//#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
 #    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #    define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
 #    define ENABLE_RGB_MATRIX_BREATHING
@@ -77,20 +57,20 @@
 #    define ENABLE_RGB_MATRIX_HUE_WAVE
 #    define ENABLE_RGB_MATRIX_PIXEL_RAIN
 #    define ENABLE_RGB_MATRIX_PIXEL_FLOW
-#    define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+//#    define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
 // enabled only if RGB_MATRIX_FRAMEBUFFER_EFFECTS is defined
 //#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
 //#    define ENABLE_RGB_MATRIX_DIGITAL_RAIN
 // enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
-#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+//#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+//#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#    define ENABLE_RGB_MATRIX_SPLASH
+//#    define ENABLE_RGB_MATRIX_SPLASH
 //#    define ENABLE_RGB_MATRIX_MULTISPLASH
 //#    define ENABLE_RGB_MATRIX_SOLID_SPLASH
 //#    define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
