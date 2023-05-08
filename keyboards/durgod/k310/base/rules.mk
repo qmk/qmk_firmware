05f4d410cb48da20c49e4af7bc09f4f4c70ce809
Ryan L <57370351+rlew631@users.noreply.github.com>
2023-05-07 17:45:47 -0700

    Update keyboards/durgod/k310/base/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/durgod/k310/base/rules.mk
+++ keyboards/durgod/k310/base/rules.mk
@@ -1,7 +1,9 @@
+# Wildcard to allow APM32 MCU
+DFU_SUFFIX_ARGS = -v FFFF -p FFFF
+
 # Build Options
 #   change yes to no to disable
 #
-DFU_SUFFIX_ARGS = -v FFFF -p FFFF
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
