98ada8d31b2a7103ff90e0a559139e943b90ae68
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-05-09 12:14:57 +0100

    LAYOUT name reverted

--- keyboards/handwired/dactyl_cc/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_cc/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ enum layers {
 #define LT_SCLN LT(NAV, KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BASE] = LAYOUT_dactylk(
+    [BASE] = LAYOUT(
         KC_GRV,     KC_1,   KC_2,    KC_3,    KC_4, KC_5,                                           KC_6,    KC_7,    KC_8,   KC_9,    KC_0, KC_HOME,
         KC_TAB,     KC_Q,   KC_W,    KC_E,    KC_R, KC_T,                                           KC_Y,    KC_U,    KC_I,   KC_O,    KC_P, KC_BSLS,
         CTL_CAP,    KC_A,   KC_S,    KC_D,    KC_F, KC_G,                                           KC_H,    KC_J,    KC_K,   KC_L, LT_SCLN, KC_QUOT,
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                    KC_LGUI,     KC_PGDN,
                                                  KC_BSPC, CTL_ESC, KC_LALT,     KC_RALT,  KC_ENT, KC_SPC
     ),
-    [NAV] = LAYOUT_dactylk(
+    [NAV] = LAYOUT(
         QK_BOOT,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,                                         KC_BRIU,   KC_P7,  KC_P8,   KC_P9,   KC_NO, QK_BOOT,
         _______, KC_VOLU, KC_WBAK, KC_MS_U, KC_WFWD, KC_WH_U,                                         KC_BRID,   KC_P4,  KC_P5,   KC_P6, KC_PMNS, _______,
         _______, KC_VOLD, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                                         KC_PPLS,   KC_P1,  KC_P2,   KC_P3, KC_PSLS, _______,
--- keyboards/handwired/dactyl_cc/keymaps/via/keymap.c
+++ keyboards/handwired/dactyl_cc/keymaps/via/keymap.c
@@ -34,7 +34,7 @@ enum layers {
 #define CG_HOME MT(MOD_LGUI | MOD_LCTL, KC_HOME)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_dactylk(
+    [_BASE] = LAYOUT(
         KC_ESC,     KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,    KC_7,    KC_8,   KC_9,    KC_0, KC_GRV,
         KC_TAB,     KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                            KC_Y,    KC_U,    KC_I,   KC_O,    KC_P, KC_BSLS,
         KC_CAP,     KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                            KC_H,    KC_J,    KC_K,   KC_L, LT_SCLN, KC_QUOT,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                       CG_HOME,     AG_PGUP,
                                                     LT_1BSP,  MH_DEL, CAG_END,     CA_PGDN,  HY_ENT, LT_2SPC
     ),
-    [_NAV] = LAYOUT_dactylk(
+    [_NAV] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                                         _______,  KC_NUM, KC_PEQL, KC_PSLS, KC_PAST, _______,
         _______, _______, _______, _______, _______, _______,                                         _______,   KC_P7,   KC_P8,   KC_P9, KC_PMNS, _______,
         _______, QK_BOOT, _______, _______, KC_SLEP, _______,                                         _______,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                        _______,     _______,
                                                      _______, _______, _______,     _______, _______,   MO(3)
     ),
-    [_SYS] = LAYOUT_dactylk(
+    [_SYS] = LAYOUT(
         KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                           KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,  KC_F12,
         _______, KC_VOLU, KC_WBAK, KC_MS_U, KC_WFWD, KC_WH_U,                                         _______,   KC_NO,   KC_NO,  KC_NO,   KC_NO, _______,
         _______, KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                                         _______, KC_PSCR, KC_PAUS, KC_INS, KC_SCRL, _______,
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      KC_BTN1, KC_BTN2, _______,     _______, _______, _______
 
     ),
-    [_BLANK] = LAYOUT_dactylk(
+    [_BLANK] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
