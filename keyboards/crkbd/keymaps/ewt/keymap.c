8a6458ac1f3df856b23b62e4b79dcb6ad57f5ae8
dverdonschot <36795362+dverdonschot@users.noreply.github.com>
2023-05-10 21:24:53 +0200

    Update keymap.c
    Added esc-ctrl and removed ctrl for f and d.

--- keyboards/crkbd/keymaps/ewt/keymap.c
+++ keyboards/crkbd/keymaps/ewt/keymap.c
@@ -30,11 +30,9 @@ enum custom_layers {
 #define NAV MO(_NAVIGATION)
 #define MED MO(_FKEYS)
 
-
-#define FCTRL LCTL_T(KC_F)
+#define ECTRL LCTL_T(KC_ESC)
 #define JCTRL RCTL_T(KC_J)
-#define KALT LALT_T(KC_K)       
-#define DALT LALT_T(KC_D)  
+#define KALT LALT_T(KC_K)        
 #define DASHMED LT(MED, KC_MINS)    
 #define TMUX LCTL(KC_B)
 #define KILL LALT(KC_F4)
@@ -50,9 +48,9 @@ enum custom_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  DASHMED,
+      ECTRL,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  DASHMED,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB,    KC_A,    KC_S,     DALT,   FCTRL,    KC_G,                         KC_H,   JCTRL,    KALT,    KC_L, KC_SCLN, KC_QUOT,
+      KC_TAB,    KC_A,    KC_S,     KC_D,   KC_F,    KC_G,                         KC_H,   JCTRL,    KALT,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -63,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYMBOL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC,  KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                      KC_PEQL, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_AMPR,
+      ECTRL,  KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                      KC_PEQL, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_AMPR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB,  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                       KC_DOT,  KC_4,    KC_5,    KC_6,    KC_PLUS, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -75,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAVIGATION] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_ESC,  XXXXXXX, KC_LCMD, KC_MS_U,  KC_F5, KC_WH_U,                       KC_PGUP, KC_HOME, KC_MINS, XXXXXXX, KC_END,  XXXXXXX,
+      ECTRL,  XXXXXXX, KC_LCMD, KC_MS_U,  KC_F5, KC_WH_U,                       KC_PGUP, KC_HOME, KC_MINS, XXXXXXX, KC_END,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB,  XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX, KC_MPLY,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
