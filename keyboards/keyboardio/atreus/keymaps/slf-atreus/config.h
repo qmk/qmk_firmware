de0e9679c5deecbabcfe9768bc2ea3e29685bcff
PoweredByPorridge <>
2023-05-11 23:51:26 +0100

    Changes to fit checklist.

--- keyboards/keyboardio/atreus/keymaps/slf-atreus/config.h
+++ keyboards/keyboardio/atreus/keymaps/slf-atreus/config.h
@@ -1,3 +1,19 @@
+/* Copyright 2021 Your Name (@PoweredByPorridge)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #define AUTO_SHIFT_TIMEOUT 210
@@ -17,7 +33,7 @@
 
 // Configure the global tapping term (default: 200ms)
 // Lower than 250 and I can't tap dance brackets without a run up.
-#define TAPPING_TERM 250
+#define TAPPING_TERM 210
 
 #define TAPPING_TERM_PER_KEY
 
--- keyboards/keyboardio/atreus/keymaps/slf-atreus/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/slf-atreus/keymap.c
@@ -1,3 +1,19 @@
+/* Copyright 2021 Your Name (@PoweredByPorridge)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
 // Left-hand home row mods
@@ -51,10 +67,25 @@ void tache_finished(tap_dance_state_t *state, void *user_data);
 void tache_reset(tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NUBS, KC_SLSH, KC_N, KC_M, KC_COMM, KC_DOT, KC_ENT, LCTL_T(KC_ESC), KC_LALT, KC_LGUI, KC_LSFT, MO(1), KC_SPC, KC_SPC, MO(2), TD(QUOTS), KC_RSFT, KC_LALT, LCTL_T(KC_TAB)),
-	[1] = LAYOUT(KC_EXLM, KC_DQUO, LSFT(KC_3), KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_DEL, KC_BSPC, KC_F24, LALT(KC_F3), KC_NO, KC_NO, TD(L_BRAX), TD(R_BRAX), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LCTL(KC_F12), LALT(KC_X), KC_NO, KC_NO, KC_NO, TD(QUOTS), TD(TACHE), KC_MINS, KC_EQL, KC_COMM, KC_DOT, KC_ENT, KC_TRNS, KC_NO, KC_NO, MO(3), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LSFT(KC_TAB)),
-	[2] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, LALT(KC_F3), KC_NO, KC_NO, TD(L_BRAX), TD(R_BRAX), KC_HOME, KC_PGDN, KC_PGUP, KC_END, LCTL(KC_F12), LALT(KC_X), KC_NO, KC_NO, KC_NO, TD(QUOTS), TD(TACHE), KC_MINS, KC_EQL, KC_COMM, KC_DOT, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, MO(3), KC_NO, KC_NO, KC_NO, MO(3), KC_NO, KC_NO, KC_TRNS),
-	[3] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, AS_TOGG, KC_F24, KC_NO, KC_NO, KC_NO, KC_NO, LSA(KC_LEFT), LSA(KC_DOWN), LSA(KC_UP), LSA(KC_RGHT), KC_LCTL, LALT(KC_DEL), KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_NO, KC_F12, KC_F13, KC_F14, KC_NO, KC_TRNS, KC_F23, KC_LCTL, KC_NO, KC_TRNS, KC_NO, KC_VOLD, KC_MPLY, KC_LSFT, KC_NO, KC_NO, KC_TRNS)
+	[0] = LAYOUT(KC_Q,         KC_W,        KC_E,       KC_R,     KC_T,                              KC_Y,        KC_U,      KC_I,     KC_O,    KC_P,
+		     KC_A,         KC_S,        KC_D,       KC_F,     KC_G,                              KC_H,        KC_J,      KC_K,     KC_L,    KC_SCLN,
+		     KC_Z,         KC_X,        KC_C,       KC_V,     KC_B,      KC_NUBS,     KC_SLSH,   KC_N,        KC_M,      KC_COMM,  KC_DOT,  KC_ENT,
+	   LCTL_T(KC_ESC),         KC_LALT,     KC_LGUI,    KC_LSFT,  MO(1),     KC_SPC,      KC_SPC,    MO(2),       TD(QUOTS), KC_RSFT,  KC_LALT, LCTL_T(KC_TAB)),
+	
+	[1] = LAYOUT(KC_EXLM,      KC_DQUO,     LSFT(KC_3), KC_DLR,   KC_PERC,                           KC_CIRC,     KC_AMPR,   KC_ASTR,  KC_DEL,  KC_BSPC,
+		     KC_F24,       LALT(KC_F3), KC_NO,      KC_NO,    TD(L_BRAX),                        TD(R_BRAX),  KC_LEFT,   KC_DOWN,  KC_UP,   KC_RGHT,
+		     LCTL(KC_F12), LALT(KC_X),  KC_NO,      KC_NO,    KC_NO,      TD(QUOTS),  TD(TACHE), KC_MINS,     KC_EQL,    KC_COMM,  KC_DOT,  KC_ENT,
+		     KC_TRNS,      KC_NO,       KC_NO,      MO(3),    KC_NO,      KC_NO,      KC_NO,     KC_NO,       KC_NO,     KC_NO,    KC_NO,   LSFT(KC_TAB)),
+	
+	[2] = LAYOUT(KC_1,         KC_2,        KC_3,       KC_4,     KC_5,                              KC_6,        KC_7,      KC_8,     KC_9,    KC_0,
+		     KC_NO,        LALT(KC_F3), KC_NO,      KC_NO,    TD(L_BRAX),                        TD(R_BRAX),  KC_HOME,   KC_PGDN,  KC_PGUP, KC_END,
+     		     LCTL(KC_F12), LALT(KC_X),  KC_NO,      KC_NO,    KC_NO,      TD(QUOTS),  TD(TACHE), KC_MINS,     KC_EQL,    KC_COMM,  KC_DOT,  KC_NO,
+		     KC_TRNS,      KC_NO,       KC_NO,      KC_NO,    MO(3),      KC_NO,      KC_NO,     KC_NO,       MO(3),     KC_NO,    KC_NO,   KC_TRNS),
+	
+	[3] = LAYOUT(KC_F1,        KC_F2,       KC_F3,      KC_F4,    KC_F5,                             KC_F6,       KC_F7,     KC_F8,    KC_F9,   KC_F10,
+		     AS_TOGG,      KC_F24,      KC_NO,      KC_NO,    KC_NO,                             KC_NO,       LSA(KC_LEFT), LSA(KC_DOWN), LSA(KC_UP), LSA(KC_RGHT),
+		     KC_LCTL,      LALT(KC_DEL),KC_NO,      KC_NO,    KC_NO,      KC_NO,      KC_VOLU,   KC_NO,       KC_F12,    KC_F13,   KC_F14,  KC_NO,
+		     KC_TRNS,      KC_F23,      KC_LCTL,    KC_NO,    KC_TRNS,    KC_NO,     KC_VOLD,    KC_MPLY,     KC_LSFT,   KC_NO,   KC_NO,  KC_TRNS)
 };
 
 
