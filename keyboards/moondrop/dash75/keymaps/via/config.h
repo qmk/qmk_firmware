68befb07d85303bc31d0298a9938b1d5ea09fa03
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-05-12 14:01:08 +1000

    Add Moondrop Dash75

---
+++ keyboards/moondrop/dash75/keymaps/via/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 moondroplab (@moondroplab)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+#define VIA_FIRMWARE_VERSION 1
---
+++ keyboards/moondrop/dash75/r1/config.h
@@ -0,0 +1,24 @@
+// Copyright 2023 moondroplab (@moondroplab)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define USB_SUSPEND_WAKEUP_DELAY 5000  // wait for USB hub to start
+
+#define HOLD_ON_OTHER_KEY_PRESS
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
