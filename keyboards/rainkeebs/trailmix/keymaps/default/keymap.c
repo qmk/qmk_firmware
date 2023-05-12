c0f2d3284a2a4b5f125a4c287fec5227226c489e
ATron789 <albertopavano@gmail.com>
2023-05-12 09:55:57 +0100

    fix: pr formatting comment

--- keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
+++ keyboards/rainkeebs/trailmix/keymaps/default/keymap.c
@@ -15,7 +15,12 @@
  */
 #include QMK_KEYBOARD_H
 
-enum layers { _BASE, _LOWER, _RAISE, _ADJUST };
+enum layers {
+    _BASE,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
@@ -23,47 +28,31 @@ enum layers { _BASE, _LOWER, _RAISE, _ADJUST };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R, KC_T,
-        KC_Y, KC_U, KC_I, KC_O, KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_H, KC_J, KC_K, KC_L, KC_ENTER,
-        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY,
-        KC_PSCR, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
-        KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_LSFT,
-        KC_SPC, RAISE, KC_RGUI, KC_ESC, KC_TAB
+        KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_A, KC_S, KC_D, KC_F, KC_G,               KC_H, KC_J, KC_K, KC_L, KC_ENTER,
+        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY,      KC_PSCR, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
+        KC_LCTL, KC_LALT, KC_LGUI, LOWER,KC_LSFT,   KC_SPC, RAISE, KC_RGUI, KC_ESC, KC_TAB
     ),
 
     [_LOWER] = LAYOUT(
-        KC_1, KC_2, KC_3, KC_4, KC_5,
-        KC_6, KC_7, KC_8, KC_9, KC_0,
-        KC_ESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-        KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, KC_TRNS,
-        KC_TAB, KC_INSERT, KC_DELETE, KC_BACKSLASH, KC_MINUS, KC_TRNS,
-        KC_TRNS, KC_PLUS, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_LGUI, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_RGUI, KC_TRNS, KC_TRNS
+        KC_1, KC_2, KC_3, KC_4, KC_5,                                   KC_6, KC_7, KC_8, KC_9, KC_0,
+        KC_ESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,                      KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, KC_TRNS,
+        KC_TAB, KC_INSERT, KC_DELETE, KC_BACKSLASH,KC_MINUS, KC_TRNS,   KC_TRNS, KC_PLUS, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_LGUI, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, KC_RGUI, KC_TRNS, KC_TRNS
     ),
 
     [_RAISE] = LAYOUT(
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,
-        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-        KC_GRAVE, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
-        KC_LCBR, KC_RCBR, KC_COLN, KC_DQT, KC_TRNS,
-        KC_TRNS, KC_CAPS, KC_TRNS, KC_PIPE, KC_UNDS, KC_TRNS,
-        KC_TRNS, KC_EQUAL, KC_QUESTION, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, ADJUST, KC_TRNS, KC_TRNS
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,               KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+        KC_GRAVE, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,            KC_LCBR, KC_RCBR, KC_COLN, KC_DQT, KC_TRNS,
+        KC_TRNS, KC_CAPS, KC_TRNS, KC_PIPE, KC_UNDS, KC_TRNS,   KC_TRNS, KC_EQUAL, KC_QUESTION, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, ADJUST, KC_TRNS, KC_TRNS
     ),
 
     [_ADJUST] = LAYOUT(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,
-        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-        KC_F11, KC_F12, KC_PRINT_SCREEN, KC_SCROLL_LOCK, KC_PAUSE,
-        KC_H, KC_J, KC_K, KC_L, KC_ENTER,
-        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY,
-        KC_PSCR, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
-        KC_LCTL, KC_LALT, ADJUST, RAISE, KC_SPC,
-        KC_LSFT, LOWER, ADJUST, KC_RALT, KC_RCTL
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                  KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+        KC_F11, KC_F12, KC_PRINT_SCREEN, KC_SCROLL_LOCK,    KC_PAUSE, KC_H, KC_J, KC_K, KC_L, KC_ENTER,
+        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY,              KC_PSCR, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPC,
+        KC_LCTL, KC_LALT, ADJUST, RAISE, KC_SPC,            KC_LSFT, LOWER, ADJUST, KC_RALT, KC_RCTL
     ),
 };
 
