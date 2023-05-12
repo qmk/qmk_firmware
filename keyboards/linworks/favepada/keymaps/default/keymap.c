53ae6796855660d8181801dd104ac8ffe4571868
DUILCHOI <48977039+duilchoi@users.noreply.github.com>
2023-05-12 14:40:37 +0900

    Update keyboards/linworks/favepada/keymaps/default/keymap.c
    thanks bro!
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/linworks/favepada/keymaps/default/keymap.c
+++ keyboards/linworks/favepada/keymaps/default/keymap.c
@@ -16,13 +16,13 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_numpad_6x4(
         MO(1),    KC_A,    KC_B,    KC_C,
         KC_NUM,   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,    KC_P5,   KC_P6,   
-        KC_P1,    KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,             KC_PDOT 
+        KC_P7,    KC_P8,   KC_P9,
+        KC_P4,    KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,    KC_P2,   KC_P3,
+        KC_P0,             KC_PDOT, KC_PENT
     ),
     [1] = LAYOUT(
         KC_TRNS,  RGB_TOG, RGB_MOD, KC_D,
