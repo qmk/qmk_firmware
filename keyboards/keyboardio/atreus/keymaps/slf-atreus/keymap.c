3e9aec48b559d03a85a8926e22fa30f321cd0c9b
PoweredByPorridge <>
2023-05-12 11:47:40 +0100

    Updated copyright year.

--- keyboards/keyboardio/atreus/keymaps/slf-atreus/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/slf-atreus/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Your Name (@PoweredByPorridge)
+/* Copyright 2023 @PoweredByPorridge
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
