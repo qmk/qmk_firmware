f725b9db5b70ac440484ff364207c852d45477e7
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2023-05-13 11:14:12 -0600

    Update keyboards/clickety_split/leeloo/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/clickety_split/leeloo/keymaps/default/keymap.c
+++ keyboards/clickety_split/leeloo/keymaps/default/keymap.c
@@ -112,8 +112,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-
-  return state;
-}
