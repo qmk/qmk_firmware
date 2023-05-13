7072a5f58e900c5ff09961b662be5442556a9bd1
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2023-05-13 11:16:08 -0600

    Update keyboards/clickety_split/leeloo/rev2/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/clickety_split/leeloo/rev2/rules.mk
+++ keyboards/clickety_split/leeloo/rev2/rules.mk
@@ -1,4 +1,1 @@
-# Enable LEDs
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
-RGB_MATRIX_DRIVER = WS2812  # RGB matrix driver support
+# This file intentionally left blank
