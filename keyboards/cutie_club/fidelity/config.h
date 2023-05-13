db1a07d7aa474c56c7fcc9a09840392d637e36fb
mini-ninja-64 <github@callumh.art>
2023-05-13 16:14:32 +0100

    Update fidelity to use info.json

--- keyboards/cutie_club/fidelity/config.h
+++ keyboards/cutie_club/fidelity/config.h
@@ -16,27 +16,5 @@
 
 #pragma once
 
-#include "config_common.h"
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 16
-
-/*
- * Keyboard Matrix Assignments
- */
-#define MATRIX_ROW_PINS { A15, B3, B5, B6, B7 }
-#define MATRIX_COL_PINS { A7, A6, A5, A4, A3, A9, A8, B15, B14, B13, B12, B11, B10, B2, B1, B0 }
-
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-#define LED_CAPS_LOCK_PIN B4
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
