cf1937af3794f4800a24ab308ff2527fbbeb026c
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2023-05-14 20:54:02 +0900

    Create directory keyboards/sekigon/keyboard_quantizer/mini

---
+++ keyboards/sekigon/keyboard_quantizer/mini/keymaps/default/keymap.c
@@ -0,0 +1,24 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_4x4(
+        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
+        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    )
+};
