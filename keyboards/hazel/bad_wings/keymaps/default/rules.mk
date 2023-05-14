fd85f4093ba31f6a0951b70c805b6b608d1768ee
Jason Hazel <jhazel@matrixmediaservices.com>
2023-05-14 14:34:22 -0400

    add more missing license info

--- keyboards/hazel/bad_wings/keymaps/default/rules.mk
+++ keyboards/hazel/bad_wings/keymaps/default/rules.mk
@@ -1,2 +1,5 @@
+# Copyright 2023 Jason Hazel (@jasonhazel)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 MOUSEKEY_ENABLE			= yes
 EXTRAKEY_ENABLE			= yes
--- keyboards/hazel/bad_wings/keymaps/jasonhazel/rules.mk
+++ keyboards/hazel/bad_wings/keymaps/jasonhazel/rules.mk
@@ -1,3 +1,6 @@
+# Copyright 2023 Jason Hazel (@jasonhazel)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 MOUSEKEY_ENABLE			= yes
 EXTRAKEY_ENABLE			= yes
 COMBO_ENABLE				= yes
