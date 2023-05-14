78b5f60050a963dcb8eb6db71f32e99c67b04638
seflm <marek.sefl@seznam.cz>
2023-05-14 19:46:36 +0200

    init 2

--- keyboards/qvex/lynepad/config.h
+++ keyboards/qvex/lynepad/config.h
@@ -3,22 +3,7 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
 //Digital joystick (4-way switch + encoder)
 #define PIN_JU D4
@@ -33,4 +18,18 @@
 //Thumbwheel and joystick encoders
 #define ENCODERS_PAD_A { D6, D1 }
 #define ENCODERS_PAD_B { B5, D0 }
+#define ENCODER_RESOLUTIONS { 2, 2 }
 
+//Backlighting
+#define BACKLIGHT_PIN B7
+#define BACKLIGHT_LEVELS 8
+#define BACKLIGHT_LIMIT_VAL 192
+#define BACKLIGHT_BREATHING
+#define BACKLIGHT_DEFAULT_LEVEL 5
+
+//RGB ring
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 5
+#define RGBLIGHT_DEFAULT_VAL 64
+#define RGBLIGHT_LIMIT_VAL 192
+#define RGBLIGHT_SLEEP
