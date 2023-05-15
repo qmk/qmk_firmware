62b923ea72ff36ca99bba78f2d098e7d44711161
Joe Scotto <contact@joescotto.com>
2023-05-14 21:14:00 -0400

    Update VIA

--- keyboards/handwired/scottokeebs/scottomacrodeck/keymaps/via/keymap.c
+++ keyboards/handwired/scottokeebs/scottomacrodeck/keymaps/via/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Joe Scotto
+Copyright 2023 Joe Scotto
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,26 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-// Keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_3x4(
         KC_1, KC_2, KC_3,
         KC_Q, KC_W, KC_E, KC_R,
         KC_A, KC_S, KC_D, KC_F
-    ),
-    [1] = LAYOUT_ortho_3x4(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    [2] = LAYOUT_ortho_3x4(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    [3] = LAYOUT_ortho_3x4(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
