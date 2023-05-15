8cf0fe94520dae68295819086301c8d5535d0fca
Felix Jen <fjen@felixjen.com>
2023-05-15 17:18:12 -0500

    Removed extraneous parts of hte rules.mk

--- keyboards/keebfront/vanguard65/rules.mk
+++ keyboards/keebfront/vanguard65/rules.mk
@@ -3,12 +3,5 @@ F_CPU = 8000000
 # Build Options
 #   change yes to no to disable
 LTO_ENABLE = yes
-BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes      # Mouse keys 
-EXTRAKEY_ENABLE = yes      # Audio control and System control
-CONSOLE_ENABLE = no        # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = yes          # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 MIDI_ENABLE = yes          # Enable MIDI
