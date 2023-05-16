c5651f0a5aabf9c2b3908652f9c5c674a115c7b9
seflm <marek.sefl@seznam.cz>
2023-05-16 22:22:16 +0200

    Edit license headers, fix missing header

--- keyboards/qvex/lynepad/keymaps/default/keymap.c
+++ keyboards/qvex/lynepad/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 seflm (@seflm)
+Copyright 2023 QVEX Tech
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
