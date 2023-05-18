9f0f1d922a58f554c67ceba8917f32d8dff39c6e
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-05-18 18:32:48 +0800

    Removed c2_pro/rule.mk

--- keyboards/keychron/c2_pro/ansi/white/rules.mk
+++ keyboards/keychron/c2_pro/ansi/white/rules.mk
@@ -1,1 +1,5 @@
-# This file intentionally left blank
+# Build Options
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+SRC += matrix.c
--- keyboards/keychron/c2_pro/rules.mk
+++
@@ -1,7 +0,0 @@
-# Build Options
-#   change yes to no to disable.
-#
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-SRC += matrix.c
