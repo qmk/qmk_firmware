535005e114e8f5d0680c0a49eb664685028e1f0b
lalalademaxiya1 <66767061+lalalademaxiya1@users.noreply.github.com>
2023-05-18 18:21:34 +0800

    Removed q1v2/rule.mk

--- keyboards/keychron/q1v2/iso_encoder/rules.mk
+++ keyboards/keychron/q1v2/iso_encoder/rules.mk
@@ -1,1 +1,7 @@
-# This file intentionally left blank
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+# custom matrix setup
+CUSTOM_MATRIX = lite
+
+SRC += matrix.c
--- keyboards/keychron/q1v2/rules.mk
+++
@@ -1,8 +0,0 @@
-# Build Options
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
-# custom matrix setup
-CUSTOM_MATRIX = lite
-
-SRC += matrix.c
