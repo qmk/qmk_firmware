4639e2927f87e84e7e9113d1260df276844d92bb
seflm <marek.sefl@seznam.cz>
2023-05-19 10:25:24 +0200

    Move to data-driven, add qvex.eu URL

--- keyboards/qvex/lynepad/config.h
+++ keyboards/qvex/lynepad/config.h
@@ -28,21 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //Thumbwheel
 #define PIN_TC D2
 
-//Thumbwheel and joystick encoders
-#define ENCODERS_PAD_A { D6, D1 }
-#define ENCODERS_PAD_B { B5, D0 }
-#define ENCODER_RESOLUTIONS { 2, 2 }
-
 //Backlighting
-#define BACKLIGHT_PIN B7
-#define BACKLIGHT_LEVELS 8
-#define BACKLIGHT_LIMIT_VAL 192
-#define BACKLIGHT_BREATHING
 #define BACKLIGHT_DEFAULT_LEVEL 5
 
 //RGB ring
-#define WS2812_DI_PIN D3
-#define RGBLED_NUM 5
 #define RGBLIGHT_DEFAULT_VAL 64
-#define RGBLIGHT_LIMIT_VAL 192
-#define RGBLIGHT_SLEEP
