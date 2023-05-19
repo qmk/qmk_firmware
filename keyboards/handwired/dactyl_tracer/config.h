260039233f97402950c58abc32263b517ddd4726
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-05-19 22:47:20 +0100

    Add 'dactyl_tracer' keyboard files

---
+++ keyboards/handwired/dactyl_tracer/config.h
@@ -0,0 +1,13 @@
+// Copyright 2022 mjohns
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 15
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Enables This makes it easier for fast typists to use dual-function keys */
+#define PERMISSIVE_HOLD
