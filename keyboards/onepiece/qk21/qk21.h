b847ca8b3038153b92ded5a60f12c036c7aca56c
linlin <92699069+linlin012@users.noreply.github.com>
2023-05-19 10:24:53 +0800

    Update qk21.h

--- keyboards/onepiece/qk21/qk21.h
+++ keyboards/onepiece/qk21/qk21.h
@@ -16,27 +16,3 @@
 
 #pragma once
 #include "quantum.h"
-
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-#define LAYOUT( \
-    k00, k01, k02, k03, \
-    k10, k11, k12, k13, \
-    k20, k21, k22,      \
-    k30, k31, k32, k33, \
-    k40, k41, k42,      \
-    k50,      k52, k53  \
-) { \
-    { k00, k01, k02, k03 }, \
-    { k10, k11, k12, k13 }, \
-    { k20, k21, k22, KC_NO }, \
-    { k30, k31, k32, k33 }, \
-    { k40, k41, k42, KC_NO }, \
-    { k50, KC_NO, k52, k53} \
-}
