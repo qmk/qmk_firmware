c2bf1397ef81c30c714562daabf8d696aca5e1a4
Ciaanh <nicolas.fabre.21@gmail.com>
2023-05-19 22:43:16 +0200

    Review comments

--- keyboards/ciaanh/kanagawa/iso/iso.c
+++ keyboards/ciaanh/kanagawa/iso/iso.c
@@ -13,8 +13,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
- #include "kanagawa.h"
+#include "quantum.h"
+
+#define NA NO_LED
 
 #ifdef RGB_MATRIX_ENABLE
 
