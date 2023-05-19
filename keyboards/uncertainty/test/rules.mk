016ed63c2797a50fc7e80e89ced7aea9c61779ae
Vinh Le <vinhcatba1@gmail.com>
2023-05-19 16:31:35 +0700

    vinh: test fw for uncertainty (deprecated)

---
+++ keyboards/uncertainty/test/rules.mk
@@ -0,0 +1,13 @@
+# using pwm driver on stm32f401
+WS2812_DRIVER = pwm
+
+# using external i2c eeprom
+EEPROM_DRIVER = i2c
+
+# using SSD1306 OLED driver
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+WPM_ENABLE = yes # WPM counter for oled
+
+# using rotary encoder
+# ENCODER_ENABLE = yes
