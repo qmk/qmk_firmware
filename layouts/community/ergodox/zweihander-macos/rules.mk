f35a335c53e356f9ad680b60966aa9b235918937
Nathan Galt <adiabatic@users.noreply.github.com>
2023-05-19 18:02:05 -0700

    Don’t pretend to be a mouse

--- layouts/community/ergodox/zweihander-macos/rules.mk
+++ layouts/community/ergodox/zweihander-macos/rules.mk
@@ -1,3 +1,9 @@
 # Don’t do a tricolor flashing-light wave when the computer is shut down in Windows,
 # or when the machine is sleeping in macOS
 SLEEP_LED_ENABLE = no
+
+# Don’t pretend to be a mouse according to <https://docs.qmk.fm/#/feature_pointing_device?id=troubleshooting>
+POINTING_DEVICE_ENABLE = no
+
+# Don’t pretend to be a mouse according to <https://github.com/qmk/qmk_firmware/issues/510#issuecomment-232067620> (actually worked)
+MOUSEKEY_ENABLE = no
