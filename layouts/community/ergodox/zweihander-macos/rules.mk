092fa0cab941d944d09499d5360acb5a9618b7c4
Nathan Galt <adiabatic@users.noreply.github.com>
2023-05-21 15:21:09 -0700

    Pare down to `MOUSEKEY_ENABLE = no`; remove links

--- layouts/community/ergodox/zweihander-macos/rules.mk
+++ layouts/community/ergodox/zweihander-macos/rules.mk
@@ -2,8 +2,5 @@
 # or when the machine is sleeping in macOS
 SLEEP_LED_ENABLE = no
 
-# Don’t pretend to be a mouse according to <https://docs.qmk.fm/#/feature_pointing_device?id=troubleshooting>
-POINTING_DEVICE_ENABLE = no
-
-# Don’t pretend to be a mouse according to <https://github.com/qmk/qmk_firmware/issues/510#issuecomment-232067620> (actually worked)
+# Don’t pretend to be a mouse (if a a mouse is plugged into a FreeBSD machine, an annoying cursor will pop up)
 MOUSEKEY_ENABLE = no
