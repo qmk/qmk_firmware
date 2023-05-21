bca0ec6f1fc0d2784bce71839f52e3aaf513eee7
Ian Sterling <xyverz@gmail.com>
2023-05-20 23:41:57 -0700

    Reverting my iris keymap

--- keyboards/keebio/iris/keymaps/xyverz/keymap.c
+++ keyboards/keebio/iris/keymaps/xyverz/keymap.c
@@ -5,7 +5,9 @@ extern keymap_config_t keymap_config;
 
 enum layer_names {
   _DVORAK,
-  _DESTINY,
+  _QWERTY,
+  _COLEMAK,
+  _WOW,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -13,7 +15,9 @@ enum layer_names {
 
 enum custom_keycodes {
   DVORAK = SAFE_RANGE,
-  DESTINY,
+  QWERTY,
+  COLEMAK,
+  WOW,
   LOWER,
   RAISE,
   ADJUST
@@ -23,15 +27,14 @@ enum custom_keycodes {
 #define KC_RASE RAISE
 #define KC_RST QK_BOOT
 #define KC_BL_S BL_STEP
+#define KC_QWRT QWERTY
+#define KC_CLMK COLEMAK
 #define KC_DVRK DVORAK
-#define KC_DSTN DESTINY
-#define GUI_ENT GUI_T(KC_ENT)
+#define KC_WOW WOW
 #define KC_BSLT ALT_T(KC_BSPC)
 #define ADJUST MO(_ADJUST)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define MACLOCK LGUI_T(LCTL_T(KC_Q))
-#define WINLOCK LGUI_T(KC_L)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -44,12 +47,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_LCTL,KC_BSLT,KC_LGUI,         KC_ENT ,KC_SPC ,KC_LALT
   ),
 
-  [_DESTINY] = LAYOUT (
+  [_QWERTY] = LAYOUT (
+     KC_EQL , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLS,
+     KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_LOWR,     KC_RASE, KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+                       KC_LCTL,KC_BSPC,KC_LGUI,         KC_ENT ,KC_SPC ,KC_LALT
+  ),
+
+  [_COLEMAK] = LAYOUT (
+     KC_EQL , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+     KC_TAB , KC_Q  , KC_W  , KC_F  , KC_P  , KC_G  ,                KC_J  , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_BSPC,
+     KC_ESC , KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                KC_H  , KC_N  , KC_E  , KC_I  , KC_O  ,KC_QUOT,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_LOWR,     KC_RASE, KC_K  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+                       KC_LCTL,KC_BSPC,KC_LGUI,         KC_ENT ,KC_SPC ,KC_LALT
+  ),
+
+  [_WOW] = LAYOUT (
      KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSLS,
      KC_TAB ,KC_QUOT,KC_COMM,KC_DOT , KC_P  , KC_Y  ,                KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_SLSH,
      KC_ESC , KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,                KC_D  , KC_H  , KC_T  , KC_N  , KC_S  ,KC_MINS,
-     KC_LSFT,KC_SCLN, KC_Q  , KC_J  , KC_K  , KC_X  ,KC_LALT,     RAISE, KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,KC_RSFT,
-                       KC_LCTL,KC_BSPC,KC_DEL,         GUI_ENT,KC_SPC ,ADJUST
+     KC_LSFT,KC_SCLN, KC_Q  , KC_J  , KC_K  , KC_X  ,KC_LALT,     KC_RGUI, KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,KC_RSFT,
+                       KC_LOWR,KC_BSPC,KC_LCTL,         KC_ENT ,KC_SPC ,KC_RASE
   ),
 
   [_LOWER] = LAYOUT (
@@ -71,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT (
      KC_F11 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                KC_F6 , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F12 ,
      _______,KC_RST ,_______,_______,_______,_______,               _______,_______,_______,_______,_______,_______,
-     _______,_______,_______,_______,_______,_______,               _______,KC_DVRK,KC_DSTN,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,               _______,KC_QWRT,KC_CLMK,KC_DVRK,KC_WOW ,_______,
      _______,_______,_______,_______,_______,_______,_______,     _______,_______,_______,_______,_______,_______,_______,
                        _______,_______,_______,         _______,_______,_______
   )
@@ -100,9 +119,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         set_single_persistent_default_layer(_DVORAK);
       }
       return false;
-    case DESTINY:
+    case COLEMAK:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_DESTINY);
+        set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+    case WOW:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_WOW);
       }
       return false;
   }
