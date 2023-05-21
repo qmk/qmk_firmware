f49e673f788a9272b15139c77e7de13a97d762d3
Thong Phan <67009134+thong-phn@users.noreply.github.com>
2023-05-21 09:46:28 +0000

    Update rules.mk

--- keyboards/tn_alice/config.h
+++ keyboards/tn_alice/config.h
@@ -16,11 +16,11 @@
 
 #pragma once
 
-//----------------------------------------------------------------
 // RGB Underglow settings
 #define RGBLED_NUM 22
 #define RGBLIGHT_LED_MAP {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21}
 #define RGBLIGHT_LAYERS
+
 // PWM Configuration 
 #define WS2812_PWM_DRIVER PWMD2 
 #define WS2812_PWM_CHANNEL 1  
