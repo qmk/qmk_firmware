6af77470d75a4a49345c465fa82c955d4d5a913c
skyloong <92424712+JZ-Skyloong@users.noreply.github.com>
2023-05-23 17:02:31 +0800

    Update keyboards/skyloong/dt40/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/skyloong/dt40/rules.mk
+++ keyboards/skyloong/dt40/rules.mk
@@ -1,12 +1,2 @@
-
-# RGB matrix enabled
-//RGB_MATRIX_ENABLE = yes
-//RGBLIGHT_ENABLE = yes
-
-# select RGB matrix driver IC
-//RGB_MATRIX_DRIVER = WS2812
-//RGBLIGHT_DRIVER = WS2812
-
-# select driver type of WS2812
 WS2812_DRIVER = pwm
 
