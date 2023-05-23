40bb0d12379e9a083ea3ecda5307b0d8a4314c52
Christoph Zimmermann <nussgipfel@brain4free.org>
2023-05-23 22:15:44 +0200

    Clean up according to PR checklist
    Check the keymaps/nuess0r how you can make better use of your Model M
    including Windows, multimedia keys etc.

--- keyboards/ibm/model_m/ctrl_m/ctrl_m.c
+++ keyboards/ibm/model_m/ctrl_m/ctrl_m.c
@@ -14,22 +14,3 @@
  * along with this program.  If not, see <http://www./gnu.org/licenses/>.
  */
 #include "ctrl_m.h"
-
-void keyboard_pre_init_kb(void) {
-  /* Setting status LEDs pins to output and +5V (off) */
-  setPinOutput(LED_NUM_LOCK_PIN);
-  setPinOutput(LED_CAPS_LOCK_PIN);
-  setPinOutput(LED_SCROLL_LOCK_PIN);
-  writePinHigh(LED_NUM_LOCK_PIN);
-  writePinHigh(LED_CAPS_LOCK_PIN);
-  writePinHigh(LED_SCROLL_LOCK_PIN);
-}
-
-bool led_update_kb(led_t led_state) {
-    if(led_update_user(led_state)) {
-        writePin(LED_NUM_LOCK_PIN, !led_state.num_lock);
-        writePin(LED_CAPS_LOCK_PIN, !led_state.caps_lock);
-        writePin(LED_SCROLL_LOCK_PIN, !led_state.scroll_lock);
-    }
-    return true;
-}
