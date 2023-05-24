2c4711dc0a163804381f0c10b535c498acd9ae64
ziptyze <mwcordier@gmail.com>
2023-05-24 15:10:45 -0400

    slider8_update

--- keyboards/1upkeyboards/1upslider8/keymaps/via/keymap.c
+++ keyboards/1upkeyboards/1upslider8/keymaps/via/keymap.c
@@ -20,7 +20,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_2x4(
-                                   QK_BOOT,
+                                   RGB_TOG,
         KC_1,     KC_2,     KC_3,     KC_4,
         KC_5,     KC_6,     KC_7,     KC_8
     ),
