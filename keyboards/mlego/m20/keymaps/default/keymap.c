803420465c644f884b7f45890c5193e858d217f7
Alin Marin Elena <alin@elena.space>
2023-05-24 08:04:49 +0100

    now default is more pristine than pristine

--- keyboards/mlego/m20/keymaps/default/keymap.c
+++ keyboards/mlego/m20/keymaps/default/keymap.c
@@ -3,10 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-// let us assume we start with both layers off
-static bool toggle_lwr = false;
-static bool toggle_rse = false;
-
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -51,47 +47,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 #endif
 
-void matrix_scan_user(void) {
-    toggle_leds(toggle_lwr, toggle_rse);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    switch (keycode) {
-        case (TT(_LWR)):
-            if (!record->event.pressed && record->tap.count == TAPPING_TOGGLE) {
-                // This runs before the TT() handler toggles the layer state, so the current layer state is the opposite of the final one after toggle.
-                set_led_toggle(_LWR, !layer_state_is(_LWR));
-            }
-            return true;
-            break;
-        case (TT(_RSE)):
-            if (record->event.pressed && record->tap.count == TAPPING_TOGGLE) {
-                set_led_toggle(_RSE, !layer_state_is(_RSE));
-            }
-            return true;
-            break;
-        default:
-            return true;
-    }
-}
-
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LWR, _RSE, _ADJ);
-}
-
-void set_led_toggle(const uint8_t layer, const bool state) {
-    switch (layer) {
-        case _LWR:
-            toggle_lwr = state;
-            break;
-        case _RSE:
-            toggle_rse = state;
-            break;
-        default:
-            break;
-    }
-}
-
-void keyboard_post_init_user(void) {
-    init_lwr_rse_led();
 }
