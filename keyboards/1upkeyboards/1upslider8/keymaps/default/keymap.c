b06568030b2ceb674e8ea6cdf7e4b099e26d2c76
ziptyze <mwcordier@gmail.com>
2023-05-24 22:07:12 -0400

    Update keyboards/1upkeyboards/1upslider8/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/1upkeyboards/1upslider8/keymaps/default/keymap.c
+++ keyboards/1upkeyboards/1upslider8/keymaps/default/keymap.c
@@ -28,16 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =  { ENCODER_CCW_CW(RGB_MOD,  RGB_RMOD)},
-    [1] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [2] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [3] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [4] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [5] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [6] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [7] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [8] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)},
-    [9] =  { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS)}
+    [0] =  { ENCODER_CCW_CW(RGB_MOD,  RGB_RMOD)}
 };
 #endif
 
