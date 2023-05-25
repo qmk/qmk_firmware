a71cd5cb46e8bce964e08da5e8fd1cf5f2f7475d
ziptyze <mwcordier@gmail.com>
2023-05-24 22:02:53 -0400

    Update keyboards/1upkeyboards/1uposu5k/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/1upkeyboards/1uposu5k/config.h
+++ keyboards/1upkeyboards/1uposu5k/config.h
@@ -19,7 +19,6 @@
 #define RGB_DI_PIN GP24
 #define RGB_MATRIX_LED_COUNT 32
 #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-#define RGBLIGHT_LIMIT_VAL 255
 
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
