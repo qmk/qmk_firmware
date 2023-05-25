3d212d57567e1ddb4ebb020074ebb24039470688
ziptyze <mwcordier@gmail.com>
2023-05-24 22:03:03 -0400

    Update keyboards/1upkeyboards/1uposu5k/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/1upkeyboards/1uposu5k/config.h
+++ keyboards/1upkeyboards/1uposu5k/config.h
@@ -21,7 +21,6 @@
 #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
 // RGB Matrix Animation modes. Explicitly enabled
 // For full list of effects, see:
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
