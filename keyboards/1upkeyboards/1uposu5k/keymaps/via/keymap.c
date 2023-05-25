4a9dc78765cff6571cdf7f9189fae65db825eb89
ziptyze <mwcordier@gmail.com>
2023-05-24 22:06:28 -0400

    Update keyboards/1upkeyboards/1uposu5k/keymaps/via/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/1upkeyboards/1uposu5k/keymaps/via/keymap.c
+++ keyboards/1upkeyboards/1uposu5k/keymaps/via/keymap.c
@@ -30,13 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [1] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [2] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [3] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [4] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [5] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [6] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [7] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [8] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [9] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}
+    [2] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}
 };
 #endif
