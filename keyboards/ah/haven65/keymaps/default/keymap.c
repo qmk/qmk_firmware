5d48cfc9e010a8854d62c0d82550045d404f4eba
CMM.Studio Freather <50167286+frankBTHID@users.noreply.github.com>
2023-05-26 13:16:19 +0800

    update ah/haven65 and ah/haven80 and teahouse/ayleen
    update ah/haven65 and ah/haven80 and teahouse/ayleen
    update made based on suggestions given about the via keycode. removed that part to allow successful merge in qmk repo

--- keyboards/ah/haven65/keymaps/default/keymap.c
+++ keyboards/ah/haven65/keymaps/default/keymap.c
@@ -20,3 +20,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if (res) {
+ //       writePin(B12, !led_state.caps_lock);  //Un-comment this line to enable in-switch capslock indicator
+    if (led_state.caps_lock) {
+        rgblight_sethsv_at(255, 255, 255, 0); //green
+    } else {
+        rgblight_sethsv_at(0, 0, 0, 0);
+    }
+  }
+    return res;
+}
