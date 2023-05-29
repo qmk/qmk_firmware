24097c86006b625d055b58d2d412d2b2f56033e5
blindassassin111 <38090555+blindassassin111@users.noreply.github.com>
2023-05-29 13:01:12 -0500

    Updating keymaps

--- keyboards/viktus/osav2_topre/keymaps/default/keymap.c
+++ keyboards/viktus/osav2_topre/keymaps/default/keymap.c
@@ -23,5 +23,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PGDN, KC_CAPS, KC_A, KC_S,    KC_D, KC_F,   KC_G,       KC_H, KC_J,   KC_K, KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
                  KC_LSFT, KC_Z, KC_X,    KC_C, KC_V,   KC_B,       KC_B, KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
                  KC_LCTL,       KC_LALT,       KC_SPC, MO(1),            KC_SPC,                KC_RALT,                   KC_RCTL
+    ),
+    [1] = LAYOUT_split_back_175u_shift(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS,          KC_TRNS,          KC_TRNS,  KC_TRNS,                   KC_TRNS,                   KC_TRNS,                   KC_TRNS
     )
 };
