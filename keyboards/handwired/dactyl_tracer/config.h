624a77f96aed332ec657c823fe9f98dc9f7e6fac
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-05-29 09:35:21 +0100

    Update keyboards/handwired/dactyl_tracer/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/dactyl_tracer/config.h
+++ keyboards/handwired/dactyl_tracer/config.h
@@ -1,9 +1,6 @@
 // Copyright 2022 mjohns
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 15
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
