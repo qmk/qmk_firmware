6ffb44b15babfacfc9f0d83ac58a6bf608480980
Joe Scotto <contact@joescotto.com>
2023-05-30 15:04:28 -0400

    Remove mod-tap again

--- keyboards/handwired/scottokeebs/scotto34/keymaps/default/config.h
+++ keyboards/handwired/scottokeebs/scotto34/keymaps/default/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Joe Scotto
+Copyright 2023 Joe Scotto
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,4 +20,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Define options
 #define TAPPING_TERM 135
 #define PERMISSIVE_HOLD
-#define TAPPING_TERM_PER_KEY
--- keyboards/handwired/scottokeebs/scotto34/keymaps/scotto/config.h
+++ keyboards/handwired/scottokeebs/scotto34/keymaps/scotto/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Joe Scotto
+Copyright 2023 Joe Scotto
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,3 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 135
 #define PERMISSIVE_HOLD
 #define TAPPING_TERM_PER_KEY
+
+// Onboard RGB LED
+#define WS2812_DI_PIN 23
+#define RGBLED_NUM 1
