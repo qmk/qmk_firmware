203654061716be2a12b05d75ea497d0177d028e7
eswai <sakamoto.yuki@gmail.com>
2023-05-30 22:39:35 +0900

    "config_common.h"

--- keyboards/eswai/742/config.h
+++ keyboards/eswai/742/config.h
@@ -17,8 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
 // #define VENDOR_ID    0xE2A1
 // #define PRODUCT_ID   0x0742
