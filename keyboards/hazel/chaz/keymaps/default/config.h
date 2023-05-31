af4c103b0768dd591b3b3a39b48123785932881e
Jason Hazel <jhazel@matrixmediaservices.com>
2023-05-30 22:11:37 -0400

    adding chaz config

---
+++ keyboards/hazel/chaz/keymaps/default/config.h
@@ -0,0 +1,14 @@
+// Copyright 2023 Jason Hazel (@jasonhazel)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM_PER_KEY
+
+#define ONESHOT_TAP_TOGGLE 10
+#define ONESHOT_TIMEOUT 500
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
