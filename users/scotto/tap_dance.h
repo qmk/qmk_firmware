ce2827525d937395963695537d0daf9a33d66aad
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-06-01 23:11:17 +0100

    more tapdance admendments

--- users/scotto/tap_dance.h
+++ users/scotto/tap_dance.h
@@ -25,28 +25,14 @@ typedef enum {
     TD_TRIPLE_TAP
 } td_state_t;
 
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
 enum {
     TD_LCTL_ESC_SPOTLIGHT_EMOJI = 0,
     TD_LALT_ESC_WINDOWS_EMOJI,
     TD_ESC_LALT_LCTL_SPOTLIGHT_EMOJI,
     TD_ESC_LCTL_LALT_WINDOWS_EMOJI
 };
-
-typedef struct {
-    bool is_press_action;
-    td_state_t state;
-} td_tap_t;
-
-td_state_t cur_dance(tap_dance_state_t *state);
-
-// For the x tap dance. Put it here so it can be used in any keymap
-void x_finished(tap_dance_state_t *state, void *user_data);
-void x_reset(tap_dance_state_t *state, void *user_data);
-void td_esc_lalt_lctl_spotlight_emoji_finished(tap_dance_state_t *state, void *user_data);
-void td_esc_lalt_lctl_spotlight_emoji_reset(tap_dance_state_t *state, void *user_data);
-void td_esc_lctl_lalt_windows_emoji_finished(tap_dance_state_t *state, void *user_data);
-void td_esc_lctl_lalt_windows_emoji_reset(tap_dance_state_t *state, void *user_data);
-void td_lalt_esc_windowr_emoji_finished(tap_dance_state_t *state, void *user_data);
-void td_lalt_esc_windowr_emoji_reset(tap_dance_state_t *state, void *user_data);
-void td_lctl_esc_spotlight_emoji_finished(tap_dance_state_t *state, void *user_data);
-void td_lctl_esc_spotlight_emoji_reset(tap_dance_state_t *state, void *user_data);
