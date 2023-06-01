2262669450c0375bc3d505f93b363be524a85d5c
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-06-02 00:07:54 +0100

    corrections added

--- layouts/community/split_3x5_3/scotto/keymap.c
+++ layouts/community/split_3x5_3/scotto/keymap.c
@@ -17,55 +17,55 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LAYOUT_split_3x5_3_wrapper(...)   LAYOUT_split_3x5_3(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x5_3_wrapper((
+    [0] = LAYOUT_split_3x5_3_wrapper(
         _________________COLEMAK1_________________,
         _________________COLEMAK2_________________,
         _________________COLEMAK3a________________,
         _______THUMB6_1_______
     ),
-    [1] = LAYOUT_split_3x5_3_wrapper((
+    [1] = LAYOUT_split_3x5_3_wrapper(
         ______________SYM_MEDIA_NAV1______________,
         ______________SYM_MEDIA_NAV2______________,
         ______________SYM_MEDIA_NAV3______________,
         _____THUMB6_TRNS______
     ),
-    [2] = LAYOUT_split_3x5_3_wrapper((
+    [2] = LAYOUT_split_3x5_3_wrapper(
         _________________NUM_SYM1_________________,
         _________________NUM_SYM2_________________,
         _________________NUM_SYM3a________________,
         _____THUMB6_TRNS______
     ),
-    [3] = LAYOUT_split_3x5_3_wrapper((
+    [3] = LAYOUT_split_3x5_3_wrapper(
         ________________FUNC_SYS1a________________,
         ________________FUNC_SYS2_________________,
         ________________FUNC_SYS3_________________,
         _____THUMB6_TRNS______
     ),
-    [4] = LAYOUT_split_3x5_3_wrapper((
+    [4] = LAYOUT_split_3x5_3_wrapper(
         _________________COLEMAK1_________________,
         _________________COLEMAK2_________________,
         _________________COLEMAK3a________________,
         _______THUMB6_4_______
     ),
-    [5] = LAYOUT_split_3x5_3_wrapper((
+    [5] = LAYOUT_split_3x5_3_wrapper(
         _________________COLEMAK1_________________,
         _________________COLEMAK2_________________,
         _________________COLEMAK3b________________,
         _______THUMB6_5_______
     ),
-    [6] = LAYOUT_split_3x5_3_wrapper((
+    [6] = LAYOUT_split_3x5_3_wrapper(
         ______________SYM_MEDIA_NAV1______________,
         ______________SYM_MEDIA_NAV2______________,
         ______________SYM_MEDIA_NAV3______________,
         _____THUMB6_TRNS______
     ),
-    [7] = LAYOUT_split_3x5_3_wrapper((
+    [7] = LAYOUT_split_3x5_3_wrapper(
         _________________NUM_SYM1_________________,
         _________________NUM_SYM2_________________,
         _________________NUM_SYM3b________________,
         _____THUMB6_TRNS______
     ),
-    [8] = LAYOUT_split_3x5_3_wrapper((
+    [8] = LAYOUT_split_3x5_3_wrapper(
         ________________FUNC_SYS1b________________,
         ________________FUNC_SYS2_________________,
         ________________FUNC_SYS3_________________,
--- users/scotto/rules.mk
+++ users/scotto/rules.mk
@@ -1,3 +1,7 @@
 SRC += scotto.c tap_dance.c
 
 TAP_DANCE_ENABLE = yes
+
+ifneq ($(strip $(KEYMAP)),)
+  OPT_DEFS += -DKEYMAP_$(KEYMAP)
+endif
