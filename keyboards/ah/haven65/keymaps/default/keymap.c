fab9e7d80a44d63fc1f8c27221fcf02d1c977919
CMM.Studio Freather <50167286+frankBTHID@users.noreply.github.com>
2023-06-01 21:18:43 +0800

    update to fix some unable to build issue

--- keyboards/ah/haven65/keymaps/default/keymap.c
+++ keyboards/ah/haven65/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res) {
     if (led_state.caps_lock) {
-        rgblight_sethsv_at(0, 0, 255, 0); 
+        rgblight_sethsv_at(0, 0, 255, 0);
     } else {
         rgblight_sethsv_at(0, 0, 0, 0);
     }
