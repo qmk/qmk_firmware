52c32378df07f8652ad2155f5be71032dcb698c9
NortonTech <contact@nortontech.tech>
2023-06-02 17:25:16 -0600

    edited default keymap

--- keyboards/handwired/nortontechpad/keymaps/default/keymap.c
+++ keyboards/handwired/nortontechpad/keymaps/default/keymap.c
@@ -17,11 +17,35 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_numpad_5x4(
+    [0] = LAYOUT(
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,
-        KC_P4,   KC_P5,   KC_P6, KC_PPLS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,
         KC_P1,   KC_P2,   KC_P3,
-                 KC_P0,   KC_PDOT, KC_PENT
+        KC_P0,            KC_PDOT, KC_PENT
+    ),
+
+    [1] = LAYOUT(
+        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3,
+        KC_P0,            KC_PDOT, KC_PENT
+    ),
+
+    [2] = LAYOUT(
+        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3,
+        KC_P0,            KC_PDOT, KC_PENT
+    ),
+
+    [3] = LAYOUT(
+        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3,   
+        KC_P0,            KC_PDOT, KC_PENT
     )
 };
