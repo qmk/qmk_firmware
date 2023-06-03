da2f91db96296d4b0acd04612f0cb5c10811aedd
syntax-magic <dan.syntaxmagic@gmail.com>
2023-06-03 06:17:50 -0700

    Update keymap.c
    Copied and pasted original default/keymap.c

--- keyboards/kprepublic/bm40hsrgb/keymaps/default/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/default/keymap.c
@@ -19,6 +19,7 @@ enum layers {
   _QWERTY,
   _LOWER,
   _RAISE,
+  _ADJUST
 };
 
 #define LOWER MO(_LOWER)
@@ -104,4 +105,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
-
