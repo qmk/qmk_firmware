8f7d6d86b67b93890570970f304c30f883ef5f02
Will Hedges <wshedges@outlook.com>
2023-06-03 12:20:28 -0500

    [g60ble/chemicalwill] moved Ctrl+Alt+Del to _FN1, KC_APP back to _WORK

--- keyboards/bioi/g60ble/keymaps/chemicalwill/keymap.c
+++ keyboards/bioi/g60ble/keymaps/chemicalwill/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,            KC_Q,    KC_W,            KC_E,   KC_R,    KC_T,      KC_Y,       KC_U,    KC_I,    KC_O,      KC_P,            KC_LBRC,      KC_RBRC,     TD(DEL_BSLS),
         LT(_FN1, KC_CAPS), KC_A,    KC_S,            KC_D,   KC_F,    TD(G_END), TD(H_HOME), KC_J,    KC_K,    KC_L,      KC_SCLN,         KC_QUOT,                   KC_ENT,
         KC_LSFT,                    KC_Z,            KC_X,   KC_C,    KC_V,      KC_B,       KC_N,    KC_M,    KC_COMM,   KC_DOT,          KC_SLSH,                   KC_RSFT,
-        KC_LCTL,           KC_LGUI, LALT_T(KC_PGUP),                  KC_SPC,                                             RALT_T(KC_PGDN), C(A(KC_DEL)), MO(_FN1),    KC_RCTL
+        KC_LCTL,           KC_LGUI, LALT_T(KC_PGUP),                  KC_SPC,                                             RALT_T(KC_PGDN), KC_APP,       MO(_FN1),    KC_RCTL
     ),
 
     [_QWER] = LAYOUT_60_ansi(
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FN1] = LAYOUT_60_ansi(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,  KC_DEL,
-        _______, _______, KC_PGUP, _______, QK_RBT,  _______, _______, _______, KC_UP,   _______, _______, _______,   _______, _______,
-        _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,            _______,
-        _______,          _______, _______, _______, _______, QK_BOOT, _______, _______, _______, _______, _______,            _______,
-        _______, _______, _______,                   _______,                                     _______, DF(_WORK), _______, DF(_QWER)
+        _______, KC_F1,        KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,  KC_DEL,
+        _______, _______,      KC_PGUP, _______, QK_RBT,  _______, _______, _______, KC_UP,   _______, _______, _______,   _______, _______,
+        _______, KC_HOME,      KC_PGDN, KC_END,  _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,            _______,
+        _______,               _______, _______, _______, _______, QK_BOOT, _______, _______, _______, _______, _______,            _______,
+        _______, C(A(KC_DEL)), _______,                   _______,                                     _______, DF(_WORK), _______, DF(_QWER)
     )
 
 };
