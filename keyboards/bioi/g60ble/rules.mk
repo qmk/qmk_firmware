59d9f5efbdf3aea7854b806bf2635127f9097306
Will Hedges <wshedges@outlook.com>
2023-06-02 23:03:38 -0500

    moved tap dance C file to keymap, disabled NKRO at kb level * BT does not support NKRO

--- keyboards/bioi/g60ble/rules.mk
+++ keyboards/bioi/g60ble/rules.mk
@@ -9,7 +9,6 @@ MOUSEKEY_ENABLE = yes      # Mouse keys
 EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = yes       # Commands for debug and configuration
-NKRO_ENABLE = yes          # Enable N-Key Rollover
 BACKLIGHT_ENABLE = yes     # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes
 LTO_ENABLE = yes
