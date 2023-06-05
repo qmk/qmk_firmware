513548df6c565ee495d2745e60915b6532489d26
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-06-05 19:13:30 +0100

    compliing errors corrected

--- keyboards/handwired/dactyl_manuform/6x6_2_5/keymaps/via/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6_2_5/keymaps/via/keymap.c
@@ -7,7 +7,7 @@
 enum custom_layers {
     _QWERTY,
     _LOWER,
-    _RAISE.
+    _RAISE,
     _BLANK
 };
 
--- keyboards/handwired/dactyl_manuform/6x7_5/keymaps/via/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x7_5/keymaps/via/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_LSFT , KC_A   , KC_S  , KC_D  , KC_F  , KC_G  ,                                         KC_H  , KC_J   , KC_K   , KC_L  ,KC_SCLN, KC_LSFT  , _______,
         _______, KC_LCTL , KC_Z   , KC_X  , KC_C  , KC_V  , KC_B  ,                                         KC_N  , KC_M   , KC_COMM, KC_DOT,KC_SLSH, KC_LCTL  , _______,
                                     KC_LBRC,KC_RBRC,                                                                         KC_PLUS, KC_EQL,
-                                                            RAISE ,KC_SPC , KC_HOME,       KC_END, KC_ENT,  LOWER ,
+                                                            MO(2) ,KC_SPC , KC_HOME,       KC_END, KC_ENT,  MO(1),
                                                                    KC_BSPC, KC_LALT,       KC_LGUI, KC_DEL
     ),
 
