35e464c15946e9ed35be2f9240d537c74dfa18a3
DUILCHOI <48977039+duilchoi@users.noreply.github.com>
2023-06-05 18:38:51 +0900

    Update keyboards/linworks/fave60/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/linworks/fave60/config.h
+++ keyboards/linworks/fave60/config.h
@@ -15,7 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define WS2812_DI_PIN F5
 #define RGBLED_NUM 18
 #define RGBLIGHT_SLEEP // Turns LEDs off if the PC goes to sleep/shutdown
 #define RGBLIGHT_EFFECT_BREATHING
