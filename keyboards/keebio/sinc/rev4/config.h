1ecd16565d60707883815bf36a11ee0797b22afa
Danny Nguyen <danny@keeb.io>
2023-06-04 22:45:03 -0400

    Update copyrights

--- keyboards/keebio/sinc/rev4/config.h
+++ keyboards/keebio/sinc/rev4/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2022 Danny Nguyen <danny@keeb.io>
+/* Copyright 2023 Danny Nguyen <danny@keeb.io>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- keyboards/keebio/sinc/rev4/mcuconf.h
+++ keyboards/keebio/sinc/rev4/mcuconf.h
@@ -1,5 +1,4 @@
-/*
- * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+/* Copyright 2023 Danny Nguyen <danny@keeb.io>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
