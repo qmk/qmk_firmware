f456abb0fc990bd555d1022124b55fd0255c3f7e
Duncan Sutherland <75046609+dunk2k@users.noreply.github.com>
2023-06-06 09:16:06 +0100

    Update layouts/community/ortho_4x10/scotto/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- layouts/community/ortho_4x10/scotto/keymap.c
+++ layouts/community/ortho_4x10/scotto/keymap.c
@@ -12,7 +12,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include QMK_KEYBOARD_H
 #include "scotto.h"
 
 #define LAYOUT_ortho_4x10_wrapper(...)  LAYOUT_ortho_4x10(__VA_ARGS__)
