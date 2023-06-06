3e3fbeeb750d3f7a0ab6f0e93350cfcf8bdf8655
NortonTech <haydentnorton@gmail.com>
2023-06-06 16:19:29 -0600

    Update keyboards/handwired/nortontechpad/keymaps/default/keymap.c
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/handwired/nortontechpad/keymaps/default/keymap.c
+++ keyboards/handwired/nortontechpad/keymaps/default/keymap.c
@@ -23,29 +23,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P4,   KC_P5,   KC_P6,
         KC_P1,   KC_P2,   KC_P3,
         KC_P0,            KC_PDOT, KC_PENT
-    ),
-
-    [1] = LAYOUT(
-        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3,
-        KC_P0,            KC_PDOT, KC_PENT
-    ),
-
-    [2] = LAYOUT(
-        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3,
-        KC_P0,            KC_PDOT, KC_PENT
-    ),
-
-    [3] = LAYOUT(
-        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3,   
-        KC_P0,            KC_PDOT, KC_PENT
     )
 };
