a63c32b9f72a574fe0694efe7aebcb07b61d3894
Vinh Le <vinhcatba1@gmail.com>
2023-06-07 18:21:08 +0700

    update readme.md, rules.mk regarding PR comment

--- keyboards/vinhcatba/uncertainty/rules.mk
+++ keyboards/vinhcatba/uncertainty/rules.mk
@@ -1,11 +1,8 @@
-# using pwm driver on stm32f401 (moved to using info.json)
-#WS2812_DRIVER = pwm
 
 # using external i2c eeprom
 EEPROM_DRIVER = i2c
 
 # using SSD1306 OLED driver
-OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 WPM_ENABLE = yes # WPM counter for oled
 ENCODER_MAP_ENABLE = yes
