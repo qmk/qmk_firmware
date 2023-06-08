c6bb91f5e37931c3731373ba668d8be5a6df042b
skyloong <92424712+JZ-Skyloong@users.noreply.github.com>
2023-06-08 09:38:55 +0800

    Update keyboards/skyloong/dt40/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/skyloong/dt40/config.h
+++ keyboards/skyloong/dt40/config.h
@@ -13,10 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-
 #pragma once
-
 
 /*
  * RGB matrix driver config
