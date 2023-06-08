1fbff8b2f689198bbe5cc1ce5035a82f202ecbab
Moritz <moritz.plattner@gmx.net>
2023-06-09 01:51:12 +0200

    began theseus75 FW

---
+++ keyboards/haverworks/Theseus75/config.h
@@ -0,0 +1,32 @@
+/* Copyright 2023 Moritz Plattner
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// Set up split communications
+#define SERIAL_USART_DRIVER SD3
+#define SERIAL_USART_TX_PIN B10         // The GPIO pin that is used split communication.
+#define USART3_REMAP                    // Remap USART TX and RX pins for half duplex
+#define SERIAL_USART_TX_PAL_MODE 7      // Pin alternate function 7 = USART3_TX for PB10
+
+//#define SELECT_SOFT_SERIAL_SPEED {0}    // Experimental, will make connection faster but may need testing.
+
+// Set up various split configurations
+//#define SPLIT_USB_DETECT                // The side that manages to establish a USB connection will be master. Disabled because default on ARM targets.
+
+#define SPLIT_HAND_PIN B9               // The MCUs will probe a pin to determine if they are left or right side. high = left, low = right
+
+#define SPLIT_TRANSPORT_MIRROR          // Sync data for RGB animations to match
