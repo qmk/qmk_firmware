1def7c7b79ce58113540b84de2d0a654b518b837
skyloong <zhangyuanji@jikedingzhi.com>
2023-06-09 14:14:18 +0800

    create GK61 default keymap

--- keyboards/skyloong/gk61/keymaps/default/keymap.c
+++ keyboards/skyloong/gk61/keymaps/default/keymap.c
@@ -20,16 +20,26 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
+    _FN,
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_ESC,     KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,   KC_Y,     KC_U,      KC_O,      KC_I,    KC_BSPC,
-       KC_CAPS,     KC_A,    KC_S,     KC_D,    KC_F,   KC_G,   KC_H,     KC_J,      KC_L,      KC_P,     KC_ENT,
-       KC_LSFT,     KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_N,     KC_M,   KC_RSFT,     KC_UP,    RGB_TOG,
-       KC_LCTL,  KC_LALT,  KC_SPC,  KC_LWIN,  KC_SPC,   KC_K, KC_SPC, 	KC_APP,   KC_LEFT,   KC_DOWN,    KC_RGHT
+      KC_GRAVE,     KC_1,     KC_2,     KC_3,      KC_4,     KC_5,     KC_6,     KC_7,       KC_8,      KC_9,        KC_0,    KC_MINUS,    KC_EQUAL,    KC_BSPC,
+        KC_TAB,     KC_Q,     KC_W,     KC_E,      KC_R,     KC_T,     KC_Y,     KC_U,       KC_I,      KC_O,        KC_P,     KC_LBRC,     KC_RBRC,   KC_BSLS ,
+       KC_CAPS,     KC_A,     KC_S,     KC_D,      KC_F,     KC_G,     KC_H,     KC_J,       KC_K,      KC_L,     KC_SCLN,     KC_QUOT,        ____,     KC_ENT,
+       KC_LSFT,     KC_Z,     KC_X,     KC_C,      KC_V,     KC_B,     KC_N,     KC_M,   KC_COMM ,    KC_DOT,   KC__SLASH,        ____,     KC_RSFT,       ____,
+       KC_LCTL,  KC_LWIN,  KC_LALT,     ____,    KC_SPC,   KC_SPC,   KC_SPC, 	  ____,     KC_SPC,   KC_RALT,      KC_APP,     KC_RCTL,       ____,       MO(1)
+    ),
+    /* Fn+ */
+        [_FN] = LAYOUT(
+        KC_ESC,    KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,      KC_F8,     KC_F9,      KC_F10,      KC_F11,      KC_F12,    KC_PSCR,
+       RGB_TOG,  RGB_VAD,  RGB_VAI,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,       KC_NO,     KC_VOLD,     KC_VOLU,    KC_MUTE,
+       RGB_MOD,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_MPRV,     KC_MNXT,        ____,    KC_MPLY,
+       KC_LSFT,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_UP,      KC__UP,        ____,     KC_RSFT,       ____,
+       KC_CALC,  KC_MAIL,    KC_NO,     ____,    KC_SPC,   KC_SPC,   KC_SPC, 	 ____,    KC_LEFT,   KC_LEFT,     KC_DOWN,     KC_RGHT,        ____,       KC_NO
     ),
 
 };
