0b04a38c000279a42d02811dc6d91b4b27647012
Erik Trinkle <ectrink@gmail.com>
2023-06-09 08:07:48 -0400

    align keymaps for different physical configurations

--- keyboards/cyboard/dactyl/manuform_number_row/keymaps/default/keymap.c
+++ keyboards/cyboard/dactyl/manuform_number_row/keymaps/default/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_UP,   KC_DOWN, KC_LGUI,   KC_LALT,   KC_LGUI, KC_ENT, KC_DEL,           KC_BSPC,  KC_SPC,  MO(2), KC_LBRC,  KC_RBRC, KC_LEFT, KC_RIGHT,
-                                            KC_HOME, KC_END,    KC_ESC,           MO(3),  KC_CAPS,  KC_APP
+                                            KC_HOME, KC_END,    KC_ESC,           KC_GRAVE,  KC_CAPS,  KC_APP
     ),
 
     [1] = LAYOUT_manuform_num(
--- keyboards/cyboard/dactyl/single_arc_function_row/keymaps/default/keymap.c
+++ keyboards/cyboard/dactyl/single_arc_function_row/keymaps/default/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_UP,   KC_DOWN, KC_LGUI,   KC_LALT,   KC_LGUI, KC_ENT, MO(1),           KC_BSPC,  KC_SPC,  MO(2), KC_LBRC,  KC_RBRC, KC_LEFT, KC_RIGHT
+        KC_UP,   KC_DOWN, KC_LGUI,   KC_LALT,   KC_LGUI, KC_ENT, KC_DEL,           KC_BSPC,  KC_SPC,  MO(2), KC_LBRC,  KC_RBRC, KC_LEFT, KC_RIGHT
     ),
 
     [1] = LAYOUT_single_arc_fun(
