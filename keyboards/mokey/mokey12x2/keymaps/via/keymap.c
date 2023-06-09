7dedbb98a59dbcf56eb6e6fc0f56432d12122503
rhmokey <57172003+rhmokey@users.noreply.github.com>
2023-06-09 08:32:16 +0800

    Update keymap.c

--- keyboards/mokey/mokey12x2/keymaps/via/keymap.c
+++ keyboards/mokey/mokey12x2/keymaps/via/keymap.c
@@ -18,9 +18,9 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-                KC_A,    KC_B,    KC_C,    KC_D,   KC_1,
-                KC_E,    KC_F,    KC_G,    KC_H,   KC_2,
-                KC_I,    KC_J,    KC_K,    KC_L,
-                KC_I,    KC_J,    KC_K,    KC_L)
+    LAYOUT(
+           KC_A,    KC_B,    KC_C,    KC_D,   KC_1,
+           KC_E,    KC_F,    KC_G,    KC_H,   KC_2,
+           KC_I,    KC_J,    KC_K,    KC_L,
+           KC_I,    KC_J,    KC_K,    KC_L)
 };
