5f5c51d292553ab66912f1bdd5b29f5bc90f173c
skyloong <zhangyuanji@jikedingzhi.com>
2023-06-10 10:26:16 +0800

    create via keymap for GK61

--- keyboards/skyloong/gk61/keymaps/via/keymap.c
+++ keyboards/skyloong/gk61/keymaps/via/keymap.c
@@ -19,41 +19,29 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _QWERTY,
-    _LOWER,
-    _RAISE,
-    _ADJUST,
+    _BASE,
+    _FN,
 };
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_QWERTY] = LAYOUT(
-         KC_ESC,     KC_Q,    KC_W,      KC_E,     KC_R,   KC_T,    KC_Y,     KC_U,      KC_O,      KC_I,   KC_BSPC,
-        KC_CAPS,     KC_A,    KC_S,      KC_D,     KC_F,   KC_G,    KC_H,     KC_J,      KC_L,      KC_P,    KC_ENT,
-        KC_LSFT,     KC_Z,    KC_X,      KC_C,     KC_V,   KC_B,    KC_N,     KC_M,   KC_RSFT,     KC_UP,     MO(3),
-        KC_LCTL,  KC_LALT,   MO(1),   KC_LWIN,   KC_SPC,   KC_K,   MO(2),   KC_APP,   KC_LEFT,   KC_DOWN,   KC_RGHT
+    [_BASE] = LAYOUT(
+      KC_GRAVE,     KC_1,     KC_2,     KC_3,      KC_4,     KC_5,     KC_6,     KC_7,       KC_8,      KC_9,        KC_0,    KC_MINUS,    KC_EQUAL,    KC_BSPC,
+        KC_TAB,     KC_Q,     KC_W,     KC_E,      KC_R,     KC_T,     KC_Y,     KC_U,       KC_I,      KC_O,        KC_P,     KC_LBRC,     KC_RBRC,    KC_BSLS,
+       KC_CAPS,     KC_A,     KC_S,     KC_D,      KC_F,     KC_G,     KC_H,     KC_J,       KC_K,      KC_L,     KC_SCLN,     KC_QUOT,                  KC_ENT,
+       KC_LSFT,     KC_Z,     KC_X,     KC_C,      KC_V,     KC_B,     KC_N,     KC_M,   KC_COMM ,    KC_DOT,    KC_SLASH,                  KC_RSFT,
+       KC_LCTL,  KC_LWIN,  KC_LALT,              KC_SPC,   KC_SPC,   KC_SPC, 	           KC_SPC,   KC_RALT,      KC_APP,     KC_RCTL,                   MO(1)
     ),
-
-    [_LOWER] = LAYOUT(
-         RGB_TOG,  RGB_M_P,  RGB_M_R,  RGB_M_B, RGB_M_SW, _______, _______, _______,   KC_EQL, KC_MINS,  _______,
-         _______,  _______,  _______,  _______,  _______, _______, _______, _______,  KC_RBRC, KC_BSLS,  _______,
-         _______,  _______,  _______,  _______,  _______, _______, _______, _______,  _______, RGB_VAI,  _______,
-         _______,  _______,  _______,  _______,  _______, KC_LBRC, _______, _______, RGB_RMOD, RGB_VAD,  RGB_MOD
+    /* Fn+ */
+        [_FN] = LAYOUT(
+        KC_ESC,    KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,      KC_F8,     KC_F9,      KC_F10,      KC_F11,      KC_F12,    KC_PSCR,
+       RGB_TOG,  RGB_VAD,  RGB_VAI,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,       KC_NO,     KC_VOLD,     KC_VOLU,    KC_MUTE,
+       RGB_MOD,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,     KC_MPRV,     KC_MNXT,                 KC_MPLY,
+       KC_LSFT,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_UP,       KC_UP,                   KC_RSFT,
+       KC_CALC,  KC_MAIL,    KC_NO,              KC_SPC,   KC_SPC,    KC_SPC, 	          KC_LEFT,   KC_LEFT,     KC_DOWN,     KC_RGHT,                    KC_NO
     ),
-
-    [_RAISE] = LAYOUT(
-        _______,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_9,    KC_8, _______,
-         KC_TAB,   KC_GRV, _______, _______, _______, _______, _______, KC_SCLN, _______,    KC_0, _______,
-        _______,  _______, _______, _______, _______, KC_COMM,  KC_DOT, KC_SLSH, _______, _______, _______,
-        _______,  _______, _______, _______, _______, KC_QUOT, _______, _______, _______, _______, _______
-    ),
-
-    [_ADJUST] = LAYOUT(
-        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F9,   KC_F8, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,  KC_F12,  KC_F10, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,  KC_F11, _______, _______, _______, _______, _______
-    )
 
 };
+
 
