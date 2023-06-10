63ba9f8156b2f681d0c2b7ed9c62b1b3a8467d2f
Noah Beidelman <nbeidelman9@gmail.com>
2023-06-10 02:32:42 -0700

    update keymaps to have numpad keys

--- keyboards/printedpad/keymaps/via/keymap.c
+++ keyboards/printedpad/keymaps/via/keymap.c
@@ -17,29 +17,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT_ortho_4x3(
         LT(0, KC_NO),
-        KC_0,   KC_2,   KC_3,
+        KC_7,   KC_8,   KC_9,
         KC_4,   KC_5,   KC_6,
-        KC_7,   KC_8,   KC_9
+        KC_1,   KC_2,   KC_3
     ),
 
     [1] = LAYOUT_ortho_4x3(
         LT(0, KC_NO),
-        KC_1,   KC_2,   KC_3,
+        KC_7,   KC_8,   KC_9,
         KC_4,   KC_5,   KC_6,
-        KC_7,   KC_8,   KC_9
+        KC_1,   KC_2,   KC_3
     ),
 
     [2] = LAYOUT_ortho_4x3(
         LT(0, KC_NO),
-        KC_2,   KC_2,   KC_3,
+        KC_7,   KC_8,   KC_9,
         KC_4,   KC_5,   KC_6,
-        KC_7,   KC_8,   KC_9
+        KC_1,   KC_2,   KC_3
     ),
 
     [3] = LAYOUT_ortho_4x3(
         LT(0, KC_NO),
-        KC_3,   KC_2,   KC_3,
+        KC_7,   KC_8,   KC_9,
         KC_4,   KC_5,   KC_6,
-        KC_7,   KC_8,   KC_9
+        KC_1,   KC_2,   KC_3
     )
 };
