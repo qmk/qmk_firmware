c2acff285dd5a766f99692bef44686c0f231124a
noahbei <123449614+noahbei@users.noreply.github.com>
2023-06-10 18:53:20 -0700

    Apply suggestions from code review
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/printedpad/config.h
+++ keyboards/printedpad/config.h
@@ -4,8 +4,6 @@
 #pragma once
 
 #define I2C_DRIVER I2CD1
-#define I2C1_SCL_PIN B6
-#define I2C1_SDA_PIN B7
 #define I2C1_SCL_PAL_MODE 1
 #define I2C1_SDA_PAL_MODE 1
 #define I2C1_TIMINGR_PRESC 0x00U
@@ -13,23 +11,4 @@
 #define I2C1_TIMINGR_SDADEL 0x01U
 #define I2C1_TIMINGR_SCLH 0x03U
 #define I2C1_TIMINGR_SCLL 0x09U
-#define MY_I2C_ADDRESS (0x3C << 1)
-
 #define OLED_TIMEOUT 300000
-
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
