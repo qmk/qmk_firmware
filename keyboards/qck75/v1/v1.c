8a9d4721b27da4b168018aba380ebce20bfbf2f9
Poring <97579443+PoringH@users.noreply.github.com>
2023-06-12 12:19:38 +0700

    Update keyboards/qck75/v1/v1.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/qck75/v1/v1.c
+++ keyboards/qck75/v1/v1.c
@@ -10,11 +10,11 @@ enum {
 	FOURTH = 3
 };
 
+#ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-	return encoder_update_user(index, clockwise);
-}
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
 	switch(get_highest_layer(layer_state)) {
 		default: 
 			if(clockwise) {
