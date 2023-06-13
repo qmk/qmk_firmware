037812f1d497548696cf53ad853b7a56bac1a0b1
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-06-13 23:21:16 +0100

    amend tkl_jis

--- layouts/default/tkl_jis/default_tkl_jis/keymap.c
+++ layouts/default/tkl_jis/default_tkl_jis/keymap.c
@@ -16,9 +16,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ : │ ] │    │
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┤     ┌───┐
      * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ \ │ Shft │     │ ↑ │
-     * ├────┬───┴┬──┴─┬─┴──┬┴───┴───┴───┼───┴┬──┴─┬─┴─┬─┴─┬─┴─┬────┤ ┌───┼───┼───┐
-     * │Ctrl│GUI │Alt │Mhen│   Space    │Henk│Kana│Alt│GUI│App│Ctrl│ │ ← │ ↓ │ → │
-     * └────┴────┴────┴────┴────────────┴────┴────┴───┴───┴───┴────┘ └───┴───┴───┘
+     * ├────┬───┴┬──┴─┬─┴──┬┴───┴───┴───┴─┬─┴──┬┴───┼───┴┬──┴─┬────┤ ┌───┼───┼───┐
+     * │Ctrl│GUI │Alt │Mhen│    Space     │Henk│Kana│Alt │GUI │Ctrl│ │ ← │ ↓ │ → │
+     * └────┴────┴────┴────┴──────────────┴────┴────┴────┴────┴────┘ └───┴───┴───┘
      */
     [0] = LAYOUT_tkl_jis(
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
@@ -27,6 +27,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,             KC_DEL,  KC_END,  KC_PGDN,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_INT1, KC_RSFT,             KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_INT5,               KC_SPC,               KC_INT4, KC_INT2, KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, KC_INT5,               KC_SPC,                        KC_INT4, KC_INT2, KC_RALT, KC_RGUI, KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT
     )
 };
