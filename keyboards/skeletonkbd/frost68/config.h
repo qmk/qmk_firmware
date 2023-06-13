34731f42f2f098c625ef08b1a675bff9a5e055db
skeletonkbd <90203406+skeletonkbd@users.noreply.github.com>
2023-06-13 18:50:35 +0900

    Update keyboards/skeletonkbd/frost68/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/skeletonkbd/frost68/config.h
+++ keyboards/skeletonkbd/frost68/config.h
@@ -3,25 +3,7 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-// #define NO_DEBUG
-
-/* disable print */
-// #define NO_PRINT
-
-/* disable action features */
-// #define NO_ACTION_LAYER
-// #define NO_ACTION_TAPPING
-// #define NO_ACTION_ONESHOT
-
 #define WS2812_PIO_USE_PIO1
-
-#define WS2812_DI_PIN GP0
 #define RGB_MATRIX_LED_COUNT 68
 
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
