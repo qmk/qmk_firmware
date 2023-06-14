f1333c847ae32c713d070c2f9fbbeee51d6b8bc1
mechlovin <57231893+mechlovin@users.noreply.github.com>
2023-06-14 19:10:24 +0700

    add infintiy87 rev3

--- keyboards/mechlovin/infinity87/rev3/standard/halconf.h
+++ keyboards/mechlovin/infinity87/rev3/standard/halconf.h
@@ -1,4 +1,10 @@
+<<<<<<<< HEAD:keyboards/bastardkb/dilemma/4x6_4/keymaps/via/config.h
+/**
+ * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+ * Copyright 2023 casuanoob <casuanoob@hotmail.com> (@casuanoob)
+========
 /* Copyright 2023 Mechlovin' Stuido
+>>>>>>>> 32630cc61b (add infintiy87 rev3):keyboards/mechlovin/infinity87/rev3/standard/halconf.h
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,9 +19,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
+<<<<<<<< HEAD:keyboards/bastardkb/dilemma/4x6_4/keymaps/via/config.h
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+========
 #define HAL_USE_SPI TRUE
 
 #include_next <halconf.h>
+
+>>>>>>>> 32630cc61b (add infintiy87 rev3):keyboards/mechlovin/infinity87/rev3/standard/halconf.h
