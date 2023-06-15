fdf7f387fe90d225d5835ded66e19d08a41be337
Santiago Garcia <62843966+santy81855@users.noreply.github.com>
2023-06-15 13:31:59 -0400

    Update keymap.c

--- keyboards/santy65/keymaps/default/keymap.c
+++ keyboards/santy65/keymaps/default/keymap.c
@@ -29,6 +29,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_MAIN] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(C(KC_PGUP), C(KC_PGDN))  },
+    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(C(KC_PGUP), C(KC_PGDN))  },
 };
 #endif
