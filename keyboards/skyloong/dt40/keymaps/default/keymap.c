12349b59a425d75ddfc9f1ad07edd655e55816af
skyloong <92424712+JZ-Skyloong@users.noreply.github.com>
2023-06-15 13:51:38 +0800

    Update keyboards/skyloong/dt40/keymaps/default/keymap.c
    OK, thank you for your suggestion.
    
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/skyloong/dt40/keymaps/default/keymap.c
+++ keyboards/skyloong/dt40/keymaps/default/keymap.c
@@ -17,21 +17,14 @@
 
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-};
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_BASE] = LAYOUT(
+    [0] = LAYOUT(
         KC_ESC,     KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,   KC_Y,     KC_U,      KC_O,      KC_I,    KC_BSPC,
        KC_CAPS,     KC_A,    KC_S,     KC_D,    KC_F,   KC_G,   KC_H,     KC_J,      KC_L,      KC_P,     KC_ENT,
        KC_LSFT,     KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_N,     KC_M,   KC_RSFT,     KC_UP,    RGB_TOG,
        KC_LCTL,  KC_LALT,  KC_SPC,  KC_LWIN,  KC_SPC,   KC_K, KC_SPC, 	KC_APP,   KC_LEFT,   KC_DOWN,    KC_RGHT
-    ),
-
+    )
 };
 
 
