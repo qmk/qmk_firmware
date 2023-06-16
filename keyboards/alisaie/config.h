dd21354853f0931d8318edaa03afeae566db7230
automata-killa <53969619+Automata02@users.noreply.github.com>
2023-06-16 17:50:33 +0300

    Modified info

--- keyboards/alisaie/config.h
+++ keyboards/alisaie/config.h
@@ -17,23 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define usb.vid                 0xFEED
-#define usb.pid                 0x2012
-#define usb.device_version      0x0001
-#define manufacturer            Automata
-#define keyboard_name           Alisaie
 
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 14
-
-// ROWS: Top to bottom, COLS: Left to right
-
-#define MATRIX_ROW_PINS { E6, F6, D5, D4, B1 }
-#define MATRIX_COL_PINS { B6, F0, F1, F4, F5, B5, B7, D3, D2, D1, D0, B3, B2, B0 }
-
-#define DIODE_DIRECTION COL2ROW
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE    5
