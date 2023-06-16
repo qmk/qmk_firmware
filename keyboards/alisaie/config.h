91b88330eb0f36e58d02f87842e805721f67072f
automata-killa <53969619+Automata02@users.noreply.github.com>
2023-06-16 17:37:49 +0300

    Update config.h

--- keyboards/alisaie/config.h
+++ keyboards/alisaie/config.h
@@ -17,8 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
 #define usb.vid                 0xFEED
 #define usb.pid                 0x2012
