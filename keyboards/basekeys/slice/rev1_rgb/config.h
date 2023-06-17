3a94b9d4bed3e4fcd80bdc23c6d7836dde5ef9fc
fauxpark <fauxpark@gmail.com>
2023-06-18 05:34:24 +1000

    `RGBLED_NUM` -> `rgblight.led_count`, B

--- keyboards/basekeys/slice/rev1_rgb/config.h
+++ keyboards/basekeys/slice/rev1_rgb/config.h
@@ -28,7 +28,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* RGB LED */
 #ifdef RGBLIGHT_ENABLE
-#define RGBLED_NUM 69    // Number of LEDs. backlight x69
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
