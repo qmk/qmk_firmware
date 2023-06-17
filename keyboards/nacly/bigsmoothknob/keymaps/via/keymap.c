67689b9bf8e3642036a6e2cdfb9b308933c74aad
Derek <dschmell91@gmail.com>
2023-06-17 06:48:23 -0400

    Update keyboards/nacly/bigsmoothknob/keymaps/via/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/nacly/bigsmoothknob/keymaps/via/keymap.c
+++ keyboards/nacly/bigsmoothknob/keymaps/via/keymap.c
@@ -25,6 +25,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif
