4539529382c53ce9b46eeeff7d9ff147c36503e1
automata-killa <53969619+Automata02@users.noreply.github.com>
2023-06-17 07:54:50 +0300

    Removed redundant layouts and settings

--- keyboards/automata02/alisaie/keymaps/via/keymap.c
+++ keyboards/automata02/alisaie/keymaps/via/keymap.c
@@ -31,19 +31,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         	_______, KC_HOME, KC_PGDN, KC_END,  _______, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT, KC_LEFT, KC_RGHT,                   _______,
         	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DOWN,          KC_PSCR, _______,
         	_______, _______,                   _______, _______, _______, _______, _______,                            _______, _______
-    ),
-    [2] = LAYOUT_split_bs_rshift(
-        	QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        	_______, _______, KC_PGUP, _______, _______, _______, _______, _______, KC_UP,   _______, KC_MPRV, KC_UP,   KC_MNXT,          _______,
-        	_______, KC_HOME, KC_PGDN, KC_END,  _______, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT, KC_LEFT, KC_RGHT,                   _______,
-        	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DOWN,          KC_PSCR, _______,
-        	_______, _______,                   _______, _______, _______, _______, _______,                            _______, _______
-    ),
-    [3] = LAYOUT_split_bs_rshift(
-        	QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        	_______, _______, KC_PGUP, _______, _______, _______, _______, _______, KC_UP,   _______, KC_MPRV, KC_UP,   KC_MNXT,          _______,
-        	_______, KC_HOME, KC_PGDN, KC_END,  _______, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT, KC_LEFT, KC_RGHT,                   _______,
-        	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DOWN,          KC_PSCR, _______,
-        	_______, _______,                   _______, _______, _______, _______, _______,                            _______, _______
-    ),
+    )
 };
--- keyboards/automata02/alisaie/readme.md
+++ keyboards/automata02/alisaie/readme.md
@@ -1,1 +1,20 @@
-60% Alice style keyboard.
+# Alisaie
+
+![Alisaie layout](https://i.imgur.com/0YsrKvw.png)
+
+A 60% alice like keyboard sharing the same footprint and 8 degree alpha rotation as Meridian but with a tsangan bottom row.
+
+* Keyboard Maintainer: [Automata02](https://github.com/Automata02/)
+* Hardware Supported: Alisaie PCB version 0.0.4b
+
+Make example for this keyboard (after setting up your build environment):
+
+> make automata02/alisaie:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+## Bootloader
+
+Enter the bootloader:
+
+* **Physical reset button**: Briefly press the button on the backside of the PCB located bellow the Y keyswitch.
