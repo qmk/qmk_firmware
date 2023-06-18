a296f170f07a0d5777e6c3583ccef6f80d5724d6
akruvia <akruvia@gmail.com>
2023-06-18 23:37:26 +0800

    Remove the default QMK comments

--- keyboards/akruvia/una/config.h
+++ keyboards/akruvia/una/config.h
@@ -3,21 +3,6 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
 #define WS2812_PIO_USE_PIO1
 #define WS2812_DI_PIN GP4
 #define RGBLED_NUM 4
