6242bf64d797d51c320b327138f7f0ef93441326
akruvia <akruvia@gmail.com>
2023-06-18 23:59:54 +0800

    Put the functions keyboard_pre_init_kb() and matrix_scan_kb() here

---
+++ keyboards/akruvia/una/una.c
@@ -0,0 +1,23 @@
+// Copyright 2023 akruvia (@akruvia)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "una.h"
+
+void keyboard_pre_init_kb(void) {
+    setPinInputHigh(GP3);
+    setPinInputHigh(GP2);
+    keyboard_pre_init_user();
+}
+
+void matrix_scan_kb(void) {
+    if (!readPin(GP3)) {
+        register_code(KC_MS_BTN1);
+    } else {
+        unregister_code(KC_MS_BTN1);
+    }
+    if (!readPin(GP2)) {
+        register_code(KC_MS_BTN2);
+    } else {
+        unregister_code(KC_MS_BTN2);
+    }
+}
