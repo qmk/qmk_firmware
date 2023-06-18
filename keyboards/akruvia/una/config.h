a3174d6b127119df3440bd9658fb7cd806a281a0
akruvia <akruvia@gmail.com>
2023-06-18 23:44:41 +0800

    Remove WS2812_DI_PIN and RGBLED_NUM definitions

--- keyboards/akruvia/una/config.h
+++ keyboards/akruvia/una/config.h
@@ -4,8 +4,6 @@
 #pragma once
 
 #define WS2812_PIO_USE_PIO1
-#define WS2812_DI_PIN GP4
-#define RGBLED_NUM 4
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF 
 //The label on PCB v0.02 for the analog thumbstick should be interchanged.
