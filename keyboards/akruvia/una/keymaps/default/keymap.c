37414b5158ebc78ee1d381d0c129890eab08e69b
akruvia <akruvia@gmail.com>
2023-06-18 23:58:26 +0800

    Remove the functions keyboard_pre_init_kb() and matrix_scan_kb()

--- keyboards/akruvia/una/keymaps/default/keymap.c
+++ keyboards/akruvia/una/keymaps/default/keymap.c
@@ -78,25 +78,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-void keyboard_pre_init_kb(void) {
-    setPinInputHigh(GP3);
-    setPinInputHigh(GP2);
-    keyboard_pre_init_user();
-}
-
-void matrix_scan_kb(void) {
-    if (!readPin(GP3)) {
-        register_code(KC_MS_BTN1);
-    } else {
-        unregister_code(KC_MS_BTN1);
-    }
-    if (!readPin(GP2)) {
-        register_code(KC_MS_BTN2);
-    } else {
-        unregister_code(KC_MS_BTN2);
-    }
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     uint8_t current_layer;
 
