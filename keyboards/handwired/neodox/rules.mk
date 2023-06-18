f81b65783f630aef1dba9422ef58d7a35b3ed763
Matteo Dal Molin <matteodalmo@gmail.com>
2023-06-18 16:21:14 +0200

    move some settings into json due to new qmk feature set, update keymap, enable rgblight

--- keyboards/handwired/neodox/rules.mk
+++ keyboards/handwired/neodox/rules.mk
@@ -1,10 +1,5 @@
 # Build Options
 #   change yes to no to disable
 #
-BACKLIGHT_DRIVER = pwm
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
 SERIAL_DRIVER = vendor
-WS2812_DRIVER = vendor
 QUANTUM_PAINTER_DRIVERS = gc9a01_spi
-DEBOUNCE_TYPE = asym_eager_defer_pk
