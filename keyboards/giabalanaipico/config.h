76291679b51e6ef16e1b09c85ba84db4650996c1
3araht <3araht@gmail.com>
2023-06-18 18:16:26 +0900

    WS2812_DI_PIN definition changed.

--- keyboards/giabalanaipico/config.h
+++ keyboards/giabalanaipico/config.h
@@ -37,7 +37,6 @@
 //#define BACKLIGHT_BREATHING
 
 /* ws2812 RGB LED */
-#define RGB_DI_PIN GP6
 #define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
 
 #ifdef RGB_MATRIX_ENABLE
