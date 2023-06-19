04728a7b254380d3a24b1687797e4689faed81c0
mini-ninja-64 <github@callumh.art>
2023-06-19 20:11:23 +0100

    Migrate configuration to info.json

--- keyboards/cutie_club/keebcats/gina/config.h
+++ keyboards/cutie_club/keebcats/gina/config.h
@@ -18,34 +18,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "lib/pcal6416a/pcal6416a.h"
 
-/* key matrix size */
+/*
+ * Keyboard Matrix PCB Configuration
+ */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 5
 
-/*
- * Keyboard Matrix Pin Assignments
- */
 #define PCAL_ADDRESS (0b0100000 << 1)
 #define PCAL_ROW_PINS \
     { PCAL_1_5, PCAL_1_0, PCAL_1_1, PCAL_1_2, PCAL_1_3 }
 #define PCAL_COL_PINS \
     { PCAL_1_4, PCAL_0_5, PCAL_0_4, PCAL_0_3, PCAL_0_2 }
 
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
 #define LED_PCAL_PINS \
     { PCAL_0_0, PCAL_0_1 }
 
-#define WS2812_DI_PIN B2
-#define RGBLED_NUM 1
-
+/*
+ * Default LED Configuration
+ */
 #define DEFAULT_INDICATOR_LED NUM_LOCK
+
+/*
+ * QMK Features
+ */
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
