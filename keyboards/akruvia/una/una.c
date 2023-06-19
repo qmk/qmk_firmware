e6c1d9a4bd2b24c8e6d40f4969fd6ba3ce754128
akruvia <akruvia@gmail.com>
2023-06-19 20:10:37 +0800

    Changed una.h to quantum.h

--- keyboards/akruvia/una/una.c
+++ keyboards/akruvia/una/una.c
@@ -1,7 +1,7 @@
 // Copyright 2023 akruvia (@akruvia)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "una.h"
+#include "quantum.h"
 
 void keyboard_pre_init_kb(void) {
     setPinInputHigh(GP3);
