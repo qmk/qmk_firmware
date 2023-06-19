9b092c766ee066121a802fe131e700bfb05d225e
akruvia <akruvia@gmail.com>
2023-06-19 20:34:44 +0800

    Removed the unused user macro keys

--- keyboards/akruvia/una/keymaps/default/keymap.c
+++ keyboards/akruvia/una/keymaps/default/keymap.c
@@ -11,14 +11,7 @@ enum layers {
 
 enum keycodes {
     KC_TOGGLE_LOWER_LAYER = SAFE_RANGE,
-    KC_TOGGLE_UPPER_LAYER,
-    KC_USER_MACRO_1,
-    KC_USER_MACRO_2,
-    KC_USER_MACRO_3,
-    KC_USER_MACRO_4,
-    KC_USER_MACRO_5,
-    KC_USER_MACRO_6,
-    KC_USER_MACRO_7
+    KC_TOGGLE_UPPER_LAYER
 };
 
 enum rgb_led {
@@ -36,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LEFT_CTRL,	KC_LEFT_GUI,	KC_LEFT_ALT,	KC_DELETE,	KC_TOGGLE_LOWER_LAYER,	KC_SPACE,	KC_SPACE,	KC_TOGGLE_UPPER_LAYER,	KC_ENTER,	KC_RIGHT_CTRL,	KC_RIGHT_GUI,	KC_RIGHT_ALT
     ),	
     [_LAYER_LOWER] = LAYOUT(
-        KC_USER_MACRO_1,	KC_USER_MACRO_2,	KC_USER_MACRO_3,	KC_USER_MACRO_4,	KC_USER_MACRO_5,	KC_USER_MACRO_6,	KC_USER_MACRO_7,	KC_AUDIO_MUTE,	KC_AUDIO_VOL_DOWN,	KC_AUDIO_VOL_UP,	KC_MEDIA_PLAY_PAUSE,	KC_MEDIA_STOP,
+        KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_AUDIO_MUTE,	KC_AUDIO_VOL_DOWN,	KC_AUDIO_VOL_UP,	KC_MEDIA_PLAY_PAUSE,	KC_MEDIA_STOP,
         KC_GRAVE,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_BACKSLASH,
         KC_TRANSPARENT,	KC_HOME,	KC_PAGE_UP,	KC_PAGE_DOWN,	KC_END,	KC_NO,	KC_NO,	KC_NO,	KC_LEFT_BRACKET,	KC_RIGHT_BRACKET,	KC_UP,	KC_TRANSPARENT,
         KC_TRANSPARENT,	KC_TRANSPARENT,	KC_TRANSPARENT,	KC_CAPS_LOCK,	KC_TRANSPARENT,	KC_MINUS,	KC_EQUAL,	KC_TRANSPARENT,	KC_PRINT_SCREEN,	KC_LEFT,	KC_DOWN,	KC_RIGHT
@@ -103,27 +96,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     layer_move(_DEFAULT);
                 }
             }
-            return false;
-        case KC_USER_MACRO_1:
-            // insert code here
-            return false;
-        case KC_USER_MACRO_2:
-            // insert code here
-            return false;
-        case KC_USER_MACRO_3:
-            // insert code here
-            return false;
-        case KC_USER_MACRO_4:
-            // insert code here
-            return false;
-        case KC_USER_MACRO_5:
-            // insert code here
-            return false;
-        case KC_USER_MACRO_6:
-            // insert code here
-            return false;
-        case KC_USER_MACRO_7:
-            // insert code here
             return false;
         // Process other keycodes normally
         default:
