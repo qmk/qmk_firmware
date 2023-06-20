c27986ade51b2555f68ac1cf5d0065c89cb517e0
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-06-20 11:12:40 +0300

    Revert "Merge remote-tracking branch 'Dexter93/matrix_lock' into sn32_develop"
    This reverts commit 406af934514daaa37b19c106988afd3f99b6b404, reversing
    changes made to cdea8afa9ae29ef9ed86b223509a5f640121b278.

--- quantum/keyboard.c
+++ quantum/keyboard.c
@@ -249,14 +249,6 @@ __attribute__((weak)) void keyboard_post_init_kb(void) {
     keyboard_post_init_user();
 }
 
-/** \brief matrix_available
- *
- * Override this function if you have a condition where matrix tasks should not be available
- */
-__attribute__((weak)) bool matrix_available(void) {
-    return true;
-}
-
 /** \brief keyboard_setup
  *
  * FIXME: needs doc
@@ -454,17 +446,11 @@ static inline void generate_tick_event(void) {
  * @return false Matrix didn't change
  */
 static bool matrix_task(void) {
-    bool matrix_changed = false;
-
-    if (!matrix_available()) {
-        generate_tick_event();
-        return matrix_changed;
-    }
-
     static matrix_row_t matrix_previous[MATRIX_ROWS];
 
     matrix_scan();
 
+    bool matrix_changed = false;
     for (uint8_t row = 0; row < MATRIX_ROWS && !matrix_changed; row++) {
         matrix_changed |= matrix_previous[row] ^ matrix_get_row(row);
     }
