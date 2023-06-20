24c994571fb05757ef128e1807ca5eee59006202
Kyle McCreery <mccreery.kyle@gmail.com>
2023-06-19 23:49:25 -0400

    Initial bbpad commit

---
+++ keyboards/mechwild/bbpad/config.h
@@ -0,0 +1,11 @@
+// Copyright 2022 Kyle McCreery (@kylemccreery)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* RGB settings, uncomment this define to enable RGB */
+#define WS2812_DI_PIN B6
+#define RGB_MATRIX_LED_COUNT  28
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
---
+++ keyboards/mechwild/bbpad/rules.mk
@@ -0,0 +1,5 @@
+# Build Options
+#   change yes to no to disable
+#
+
+DEFAULT_FOLDER = mechwild/bbpad/f401
