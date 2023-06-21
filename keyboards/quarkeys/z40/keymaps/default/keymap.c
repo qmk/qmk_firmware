2ee85a295b34753f1c46038d8bf203d423a8e334
James Young <18669334+noroadsleft@users.noreply.github.com>
2023-06-21 00:22:26 -0700

    [refactor] rename LAYOUT to LAYOUT_planck_mit

--- keyboards/quarkeys/z40/keymaps/default/keymap.c
+++ keyboards/quarkeys/z40/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * | Ctrl  | Caps |  ALT |  OS  | Ly1  |    SPACE    | Ly2  |  M1  |  M2  |  M3  | Enter |
      * `-------------------------------------------------------------------------------------'
      */
-    [0] = LAYOUT(
+    [0] = LAYOUT_planck_mit(
         KC_ESC,	    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,		KC_I,   	KC_O,   	KC_P,    	KC_BSPC, 
 		KC_TAB,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,		KC_K,   	KC_L,   	KC_SCLN, 	KC_QUOT, 
 		KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,		KC_COMM,	KC_DOT, 	KC_SLSH, 	KC_RSFT, 
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-------+------+------+------+------+-------------+------+------+------+-------+------'
      */
      
-    [1] = LAYOUT(
+    [1] = LAYOUT_planck_mit(
         QK_BOOT,	KC_PGUP,	KC_HOME,	KC_UP,		KC_END,		KC_DEL,		KC_BSLS,	KC_7,		KC_8,		KC_9,		RGB_TOG,	_______, 
 		_______, 	KC_PGDN, 	KC_LEFT, 	KC_DOWN, 	KC_RGHT, 	KC_PLUS, 	KC_MINS, 	KC_4, 		KC_5, 		KC_6, 		RGB_MOD, 	_______, 
 		_______, 	KC_VOLD, 	KC_VOLU, 	_______, 	_______, 	KC_ASTR, 	KC_SLSH, 	KC_1, 		KC_2, 		KC_3, 		RGB_HUI, 	_______,
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |             |      |      |      |       |       |
      * `-------------------------------------------------------------------------------------'
      */
-    [2] = LAYOUT(
+    [2] = LAYOUT_planck_mit(
         KC_F1, 		KC_F2, 		KC_F3, 		KC_F4, 		KC_F5, 		KC_F6, 		KC_F7, 		KC_F8, 		KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 
 		_______, 	KC_AMPR, 	KC_PIPE, 	KC_LPRN, 	KC_RPRN, 	KC_PLUS, 	KC_MINS, 	KC_LCBR, 	KC_RCBR, 	KC_LBRC, 	KC_RBRC, 	_______, 
 		_______, 	KC_EXLM, 	KC_AT, 		KC_HASH, 	KC_CIRC, 	KC_ASTR, 	KC_SLSH, 	KC_TILD, 	KC_LABK, 	KC_RABK, 	KC_QUES, 	_______, 
--- keyboards/quarkeys/z40/keymaps/via/keymap.c
+++ keyboards/quarkeys/z40/keymaps/via/keymap.c
@@ -15,21 +15,21 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_planck_mit(
         KC_ESC,	    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,		KC_I,   	KC_O,   	KC_P,    	KC_BSPC, 
 		KC_TAB,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,		KC_K,   	KC_L,   	KC_SCLN, 	KC_QUOT, 
 		KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,		KC_COMM,	KC_DOT, 	KC_SLSH, 	KC_RSFT, 
 		KC_LCTL,    KC_CAPS,    KC_LALT,    KC_LGUI,    MO(1),          KC_SPC,             MO(2),		MO(3),   	_______,   	_______,    KC_ENT
     ),
 
-    [1] = LAYOUT(
+    [1] = LAYOUT_planck_mit(
         QK_BOOT,	KC_PGUP,	KC_HOME,	KC_UP,		KC_END,		KC_DEL,		KC_BSLS,	KC_7,		KC_8,		KC_9,		RGB_TOG,	_______, 
 		_______, 	KC_PGDN, 	KC_LEFT, 	KC_DOWN, 	KC_RGHT, 	KC_PLUS, 	KC_MINS, 	KC_4, 		KC_5, 		KC_6, 		RGB_MOD, 	_______, 
 		_______, 	KC_VOLD, 	KC_VOLU, 	_______, 	_______, 	KC_ASTR, 	KC_SLSH, 	KC_1, 		KC_2, 		KC_3, 		RGB_HUI, 	_______,
 		RGB_SAI, 	RGB_SAD, 	RGB_VAI, 	RGB_VAD, 	_______, 			KC_0, 			_______, 	_______, 	KC_DOT, 	RGB_HUD, 	KC_EQL
 	),
 
-    [2] = LAYOUT(
+    [2] = LAYOUT_planck_mit(
         KC_F1, 		KC_F2, 		KC_F3, 		KC_F4, 		KC_F5, 		KC_F6, 		KC_F7, 		KC_F8, 		KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 
 		_______, 	KC_AMPR, 	KC_PIPE, 	KC_LPRN, 	KC_RPRN, 	KC_PLUS, 	KC_MINS, 	KC_LCBR, 	KC_RCBR, 	KC_LBRC, 	KC_RBRC, 	_______, 
 		_______, 	KC_EXLM, 	KC_AT, 		KC_HASH, 	KC_CIRC, 	KC_ASTR, 	KC_SLSH, 	KC_TILD, 	KC_LABK, 	KC_RABK, 	KC_QUES, 	_______, 
