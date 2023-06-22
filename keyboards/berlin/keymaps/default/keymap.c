7d7b2be8a326ca4e917128c91a285a21a6a19258
9hax <me@9hax.net>
2023-06-22 20:03:14 +0200

    update keymap and add rotary controllers which don't currently work

--- keyboards/berlin/keymaps/default/keymap.c
+++ keyboards/berlin/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│GUI │Alt │                        │ Alt│ GUI│Menu│Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │   │
      * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
      */
-    [0] = KEYMAP(
+    [0] = LAYOUT_fullsize_iso(
 
 		KC_ESC,  KC_F13,  KC_F14,  KC_F15, KC_F16, KC_F17, KC_F18, KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_VOLU, KC_VOLD,
 		KC_ESC,  KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MPRV, KC_MPLY, KC_MNXT, KC_PMNS, KC_PPLS, KC_PENT,
