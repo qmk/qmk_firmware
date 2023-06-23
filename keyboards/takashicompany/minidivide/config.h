947a88a086c4872f62890c3187ceeee270990b79
takashicompany <t@kashi.company>
2023-06-23 10:32:05 +0900

    Update keyboards/takashicompany/minidivide/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/takashicompany/minidivide/config.h
+++ keyboards/takashicompany/minidivide/config.h
@@ -7,37 +7,3 @@
 #define MATRIX_COL_PINS { D4, C6, D7, E6, B4 }
 #define MATRIX_ROW_PINS_RIGHT { F4, F5, F6, F7 }
 #define MATRIX_COL_PINS_RIGHT { B4, E6, D7, C6, D4 }
-#    define RGBLED_NUM 10
-#    define RGBLED_SPLIT { 5, 5 }
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
