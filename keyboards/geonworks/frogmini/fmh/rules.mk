416af0171c6433a7ecb198386dd2c3ac70d4cbd2
Nick Brassel <nick@tzarc.org>
2023-06-24 05:31:25 +1000

    Remove CORTEX_ENABLE_WFI_IDLE from keyboards. (#21353)
    * Remove CORTEX_ENABLE_WFI_IDLE from the codebase.
    
    * Turn on CORTEX_ENABLE_WFI_IDLE by default.

--- keyboards/geonworks/frogmini/fmh/rules.mk
+++ keyboards/geonworks/frogmini/fmh/rules.mk
@@ -16,5 +16,3 @@ BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 
 EEPROM_DRIVER = i2c
 
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
