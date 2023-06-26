26aa4db8e1de649d740253fc1f85c8f4abd3bb51
skyloong <92424712+JZ-Skyloong@users.noreply.github.com>
2023-06-26 10:21:31 +0800

    Update keyboards/skyloong/gk61/v1/mcuconf.h
    OK, Thank you for your suggestion.
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/skyloong/gk61/v1/mcuconf.h
+++ keyboards/skyloong/gk61/v1/mcuconf.h
@@ -20,9 +20,6 @@
 
 #include_next <mcuconf.h>
 
-#undef STM32_PWM_USE_TIM3
-#define STM32_PWM_USE_TIM3 TRUE
-
 #undef STM32_I2C_USE_I2C1
 #define STM32_I2C_USE_I2C1 TRUE
 #define STM32_I2C_BUSY_TIMEOUT 50
