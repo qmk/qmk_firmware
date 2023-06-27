b698d63fb10611f05833576940e341054b1fed12
akruvia <akruvia@gmail.com>
2023-06-27 20:32:48 +0800

    Remove matrix_scan_kb

--- keyboards/akruvia/una/una.c
+++ keyboards/akruvia/una/una.c
@@ -8,17 +8,3 @@ void keyboard_pre_init_kb(void) {
     setPinInputHigh(GP2);
     keyboard_pre_init_user();
 }
-
-void matrix_scan_kb(void) {
-    if (!readPin(GP3)) {
-        register_code(KC_MS_BTN1);
-    } else {
-        unregister_code(KC_MS_BTN1);
-    }
-    if (!readPin(GP2)) {
-        register_code(KC_MS_BTN2);
-    } else {
-        unregister_code(KC_MS_BTN2);
-    }
-    matrix_scan_user();
-}
