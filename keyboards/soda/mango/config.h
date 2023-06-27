405e8263b5190a3e1733fe51cec2ed3ffb39bcc9
Jeayo Keh <gezhaoyou@126.com>
2023-06-27 10:04:44 +0800

    Update keyboards/soda/mango/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/soda/mango/config.h
+++ keyboards/soda/mango/config.h
@@ -9,7 +9,6 @@
 #define RGB_MATRIX_KEYPRESSES
 
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
-#define RGB_MATRIX_DEFAULT_SPD 150 
 
 #define RGB_DISABLE_WHEN_USB_SUSPENDED //
 
