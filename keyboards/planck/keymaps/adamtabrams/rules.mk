ef305589e243c3f0b3b5aa59050df2516236104b
Adam Abrams <adam.abrams@optum.com>
2023-06-26 19:17:00 -0500

    try relying on rev6 default rules

--- keyboards/planck/keymaps/adamtabrams/rules.mk
+++ keyboards/planck/keymaps/adamtabrams/rules.mk
@@ -1,9 +1,9 @@
 LTO_ENABLE = yes
 
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-NKRO_ENABLE = yes
-RGBLIGHT_ENABLE = yes
+# MOUSEKEY_ENABLE = yes
+# EXTRAKEY_ENABLE = yes
+# NKRO_ENABLE = yes
+# RGBLIGHT_ENABLE = yes
 
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 COMMAND_ENABLE = no
