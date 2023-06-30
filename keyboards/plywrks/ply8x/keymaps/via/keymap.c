848af32a075ccfbe144c377f90fa000972a12390
Ramon Imbao <ramonimbao@gmail.com>
2023-06-30 16:38:43 +0800

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/plywrks/ply8x/keymaps/via/keymap.c
+++ keyboards/plywrks/ply8x/keymaps/via/keymap.c
@@ -16,11 +16,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef RGB_MATRIX_ENABLE
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (!((host_keyboard_led_state().scroll_lock))) {
+    led_t led_state = host_keyboard_led_state();
+    if (!led_state.scroll_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(1, 0, 0, 0);
     }
 
-    if (!((host_keyboard_led_state().caps_lock))) {
+    if (!led_state.caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(0, 0, 0, 0);
     }
 
