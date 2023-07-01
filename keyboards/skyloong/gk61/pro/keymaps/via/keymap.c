c924826a459029e1e38f16c10099214d8db9dd4e
skyloong <zhangyuanji@jikedingzhi.com>
2023-07-01 18:00:07 +0800

    rename key layouts

--- keyboards/skyloong/gk61/pro/keymaps/via/keymap.c
+++ keyboards/skyloong/gk61/pro/keymaps/via/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctl│GUI│Alt│   │Spc│Spc│Mut│   |Spc|Alt│App│Ctl│   │Mo3|
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
-    [_WIN] = LAYOUT_ansi_61(
+    [_WIN] = LAYOUT_all(
          KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,        KC_8,      KC_9,        KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,        KC_I,      KC_O,        KC_P,     KC_LBRC,   KC_RBRC,   KC_BSLS,
         KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,        KC_K,      KC_L,     KC_SCLN,     KC_QUOT,               KC_ENT,
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,     KC_MUTE,                KC_SPC,   KC_RALT,      KC_APP,     KC_RCTL,                MO(3)
     ),
 
-    [_WIN_LOCK] = LAYOUT_ansi_61(
+    [_WIN_LOCK] = LAYOUT_all(
         _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,     _______,   _______,     _______,     _______,   _______,   _______,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,     _______,   _______,     _______,     _______,   _______,   _______,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,     _______,   _______,     _______,     _______,              _______,
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    KC_NO,  _______,            _______,  _______,     _______,               _______,   _______,     _______,     _______,                MO(3)
     ),
 
-    [_MAC] = LAYOUT_ansi_61(
+    [_MAC] = LAYOUT_all(
         _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,     _______,   _______,     _______,     _______,   _______,   _______,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,     _______,   _______,     _______,     _______,   _______,   _______,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,     _______,   _______,     _______,     _______,              _______,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_LALT,  KC_LGUI,            _______,  _______,     _______,               _______,   KC_RGUI,     KC_RALT,     _______,                MO(3)
     ),
 
-    [_INDEX1] = LAYOUT_ansi_61(
+    [_INDEX1] = LAYOUT_all(
         KC_GRV ,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,       KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
         _______,  _______,    TO(0),    TO(2),  _______,  _______,     _______,  _______,  _______,  _______,  RGB_M_P,  RGB_RMOD, RGB_RMOD, RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,  _______,  _______,  RGB_SPD,  RGB_SPI,            _______,
