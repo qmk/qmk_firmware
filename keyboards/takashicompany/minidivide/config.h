22960cb2090f2b15f5c4016bf00daa3aac1976f6
takashicompany <t@kashi.company>
2023-07-01 16:39:33 +0900

    Delete config.h

--- keyboards/takashicompany/minidivide/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 takashicompany (@takashicompany)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4 }
-#define MATRIX_ROW_PINS_RIGHT { F4, F5, F6, F7 }
-#define MATRIX_COL_PINS_RIGHT { B4, E6, D7, C6, D4 }
