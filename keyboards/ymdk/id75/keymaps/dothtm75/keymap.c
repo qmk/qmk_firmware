920bb403b5687469dda6a59bcbf39e35943db24a
Mike Cramer <imanerd@me.com>
2023-07-01 23:27:11 -0500

    I don't have a 2u POS spacebar to spare for this board : (

--- keyboards/ymdk/id75/keymaps/dothtm75/keymap.c
+++ keyboards/ymdk/id75/keymaps/dothtm75/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          LGUI(KC_LCBR), LGUI(KC_RCBR), KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_LBRC,       KC_BSLS,
         KC_ESC,        KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          LGUI(KC_LBRC), LGUI(KC_RBRC), KC_H,          KC_J,          KC_K,          KC_L,          KC_SCLN,       KC_QUOT,       KC_ENT,
         KC_LSFT,       KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,          LGUI(KC_QUES), LGUI(KC_SLSH), KC_N,          KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,       KC_UP,         KC_RSFT,
-        MO(2),         KC_LCTL,       KC_LALT,       KC_LGUI,       LT(1,KC_SPC),  KC_SPC,        LGUI(KC_SPC),  KC_NO,         KC_SPC,        LT(1,KC_SPC),  KC_RGUI,       KC_RALT,       KC_LEFT,       KC_DOWN,       KC_RGHT
+        MO(2),         KC_LCTL,       KC_LALT,       KC_LGUI,       LT(1,KC_SPC),  KC_SPC,        LGUI(KC_SPC),  LGUI(KC_SPC),  KC_SPC,        LT(1,KC_SPC),  KC_RGUI,       KC_RALT,       KC_LEFT,       KC_DOWN,       KC_RGHT
     ),
 
     [1] = LAYOUT_ortho_5x15(
