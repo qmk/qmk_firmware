9c4ac686bc449a11ef8486af59959133287877ed
Deemen17 <deemen17@gmail.com>
2023-07-03 02:09:24 +0700

    Remove CORTEX_ENABLE_WFI_IDLE

--- keyboards/qneatandtidy/dx65/hotswap/rev1/rules.mk
+++ keyboards/qneatandtidy/dx65/hotswap/rev1/rules.mk
@@ -3,9 +3,5 @@
 # backlight effects.
 OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
 
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
 # Wear-levering driver
 WEAR_LEVELING_DRIVER = rp2040_flash
-
