96b7c0a95ecbe39fba5f301e71d495a7c5a7ad65
fauxpark <fauxpark@gmail.com>
2023-07-02 17:18:34 +1000

    Fix return type

--- keyboards/input_club/infinity60/led.c
+++ keyboards/input_club/infinity60/led.c
@@ -26,30 +26,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * both regular threads and ISRs, unlocked (during resume-from-sleep).
  * In particular, I2C functions (interrupt-driven) should NOT be called from here.
  */
-void led_update_user(led_t led_state) {
-    msg_t msg;
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if (res) {
+        msg_t msg;
 
-    if (led_state.num_lock) {
-        chSysUnconditionalLock();
-        msg=(1 << 8) | TOGGLE_NUM_LOCK;
-        chMBPostI(&led_mailbox, msg);
-        chSysUnconditionalUnlock();
-    } else {
-        chSysUnconditionalLock();
-        msg=(0 << 8) | TOGGLE_NUM_LOCK;
-        chMBPostI(&led_mailbox, msg);
-        chSysUnconditionalUnlock();
-    }
-    if (led_state.caps_lock) {
-        chSysUnconditionalLock();
-        msg=(1 << 8) | TOGGLE_CAPS_LOCK;
-        chMBPostI(&led_mailbox, msg);
-        chSysUnconditionalUnlock();
-    } else {
-        chSysUnconditionalLock();
-        msg=(0 << 8) | TOGGLE_CAPS_LOCK;
-        chMBPostI(&led_mailbox, msg);
-        chSysUnconditionalUnlock();
+        if (led_state.num_lock) {
+            chSysUnconditionalLock();
+            msg=(1 << 8) | TOGGLE_NUM_LOCK;
+            chMBPostI(&led_mailbox, msg);
+            chSysUnconditionalUnlock();
+        } else {
+            chSysUnconditionalLock();
+            msg=(0 << 8) | TOGGLE_NUM_LOCK;
+            chMBPostI(&led_mailbox, msg);
+            chSysUnconditionalUnlock();
+        }
+        if (led_state.caps_lock) {
+            chSysUnconditionalLock();
+            msg=(1 << 8) | TOGGLE_CAPS_LOCK;
+            chMBPostI(&led_mailbox, msg);
+            chSysUnconditionalUnlock();
+        } else {
+            chSysUnconditionalLock();
+            msg=(0 << 8) | TOGGLE_CAPS_LOCK;
+            chMBPostI(&led_mailbox, msg);
+            chSysUnconditionalUnlock();
+        }
     }
     return false;
 }
