c4f66e5d6e57d2100a8d3a08758075c470f04767
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2023-07-03 08:56:48 +1000

    [Core] RGB Matrix limit basic indicators to the last render (#21169)
    Co-authored-by: Joel Challis <git@zvecr.com>

--- quantum/rgb_matrix/rgb_matrix.c
+++ quantum/rgb_matrix/rgb_matrix.c
@@ -428,7 +428,10 @@ void rgb_matrix_task(void) {
         case RENDERING:
             rgb_task_render(effect);
             if (effect) {
-                rgb_matrix_indicators();
+                // Only run the basic indicators in the last render iteration (default there are 5 iterations)
+                if (rgb_effect_params.iter == RGB_MATRIX_LED_PROCESS_MAX_ITERATIONS) {
+                    rgb_matrix_indicators();
+                }
                 rgb_matrix_indicators_advanced(&rgb_effect_params);
             }
             break;
