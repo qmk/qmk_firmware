9ab16e62f75dc6228549ec0aba00ec0704bbc779
Ryan <fauxpark@gmail.com>
2023-07-03 04:35:41 +1000

    Get rid of `USB_LED_NUM_LOCK` (#21424)

--- quantum/led.h
+++ quantum/led.h
@@ -23,7 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* FIXME: Add doxygen comments here. */
 
 /* keyboard LEDs */
-#define USB_LED_NUM_LOCK 0
 #define USB_LED_CAPS_LOCK 1
 
 #ifdef __cplusplus
