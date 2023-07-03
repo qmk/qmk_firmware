131551d40175341b459104aef9734d9a7e6e0f53
Matthew Lyon <matthew@lyonheart.us>
2023-07-03 13:23:55 -0700

    try to get CI passing

--- users/mattly/mattly.c
+++ users/mattly/mattly.c
@@ -72,7 +72,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void keyboard_post_init_user(void) {
 #ifdef RGBLIGHT_ENABLE
-    rgblight_enable();
+    rgblight_enable_noeeprom();
     set_lights_default();
 #endif
 }
