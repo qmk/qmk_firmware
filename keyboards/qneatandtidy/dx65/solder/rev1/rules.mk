5011897b989e42f0b28b2bf944114cfa84f519d7
Pham Duc Minh <95753855+Deemen17@users.noreply.github.com>
2023-07-03 14:40:08 +0700

    Change to NO_SUSPEND_POWER_DOWN
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/qneatandtidy/dx65/solder/rev1/rules.mk
+++ keyboards/qneatandtidy/dx65/solder/rev1/rules.mk
@@ -1,7 +1,7 @@
 # Do not put the microcontroller into power saving mode
 # when we get USB suspend event. We want it to keep updating
 # backlight effects.
-OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
+NO_SUSPEND_POWER_DOWN = yes
 
 # Wear-levering driver
 WEAR_LEVELING_DRIVER = rp2040_flash
