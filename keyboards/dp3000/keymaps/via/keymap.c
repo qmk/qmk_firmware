fb952510f8b784f03dfa4bb2cd97af224b25c2ff
depermana12 <deddiapermana97@gmail.com>
2023-07-03 23:24:35 +0700

    Update encoder_map keymap.c

--- keyboards/dp3000/keymaps/via/keymap.c
+++ keyboards/dp3000/keymaps/via/keymap.c
@@ -48,10 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______
         )
         };
-
+#if defined (ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),   ENCODER_CCW_CW(RGB_HUD, RGB_HUI) },
     [1] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP),   ENCODER_CCW_CW(RGB_SAD, RGB_SAI) },
     [2] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI),   ENCODER_CCW_CW(RGB_SPD, RGB_SPI) },
     [3] = { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT)},
 };
+#endif
