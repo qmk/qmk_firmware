c9f1e180afb2cb7dbd66abd184333f0d6f5fb961
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2023-07-03 18:17:38 +0800

    Update cf81.c

--- keyboards/chosfox/cf81/cf81.c
+++ keyboards/chosfox/cf81/cf81.c
@@ -147,7 +147,7 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
     if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
         return false;
     }
-    if (host_keyboard_led_state()。caps_lock) {
+    if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(44, 255, 255, 255);
     }
     if (keymap_config.no_gui) {
