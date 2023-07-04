b1563205c9b6fbf86a0d613c871fa3328b7fafe7
Oliver Säfström <safstrom.oliver@gmail.com>
2023-07-04 16:59:41 +0200

    add kyria keymap

---
+++ keyboards/splitkb/kyria/keymaps/safstromo/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+CONVERT_TO=liatris		   # Convert from Elite-C to Liatris
+COMBO_ENABLE = yes
