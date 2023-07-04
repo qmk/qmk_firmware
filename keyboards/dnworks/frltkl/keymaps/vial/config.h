eae6c4559c8514f7b81ad259d7190968f444e95b
itsme-zeix <tanleyew2@gmail.com>
2023-07-04 23:15:58 +0800

    removed via json and vial files

--- keyboards/dnworks/frltkl/keymaps/vial/config.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xC3, 0x5E, 0x74, 0x91, 0x05, 0xDD, 0xD7, 0x3D}
-
-/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
-#define VIAL_INSECURE
