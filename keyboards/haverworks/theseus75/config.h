3dc0bdc1d92c983fbe27826eaf12699d57e835b8
Moritz <moritz.plattner@gmx.net>
2023-07-04 17:00:20 +0200

    switched from bitbang to PWM ws2812 driver

--- keyboards/haverworks/theseus75/config.h
+++ keyboards/haverworks/theseus75/config.h
@@ -33,23 +33,21 @@
 
 #define SPLIT_HAND_PIN B9               // The MCUs will probe a pin to determine if they are left or right side. high = left, low = right
 
-/* Underglow - TBD */
-/*
-#define WS2812_PWM_DRIVER PWMD3
-#define WS2812_PWM_CHANNEL 4
-#define WS2812_PWM_PAL_MODE 10
-#define WS2812_DMA_STREAM STM32_DMA1_STREAM2
-#define WS2812_DMA_CHANNEL 2
-*/
-
-#define RGBLIGHT_SLEEP
-#define WS2812_EXTERNAL_PULLUP
-
 // Not yet available in `info.json`
 #ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
-#    define RGB_MATRIX_LED_COUNT 53
-#    define RGB_MATRIX_SPLIT { 27, 26 }
-#    define SPLIT_TRANSPORT_MIRROR
-#    define NOP_FUDGE 0.4
+#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
+#   define RGB_MATRIX_LED_COUNT 53
+#   define RGB_MATRIX_SPLIT { 27, 26 }
+#   define SPLIT_TRANSPORT_MIRROR
+#   define NOP_FUDGE 0.4
+
+#   define RGBLIGHT_SLEEP
+
+#   define WS2812_EXTERNAL_PULLUP
+#   define WS2812_PWM_DRIVER PWMD3
+#   define WS2812_PWM_CHANNEL 4
+#   define WS2812_PWM_PAL_MODE 10
+#   define WS2812_DMA_STREAM STM32_DMA1_STREAM2
+#   define WS2812_DMA_CHANNEL 2
+#   define WS2812_DMAMUX_ID STM32_DMAMUX1_TIM3_UP
 #endif
