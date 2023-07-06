697a90c3e7bc4ff4185ede1451cb71510c14055f
Ross <rmontsinger@gmail.com>
2023-07-06 11:40:19 -0400

    blank space

--- keyboards/montsinger/rebound/rev5/keymaps/rossman360/keymap.c
+++ keyboards/montsinger/rebound/rev5/keymaps/rossman360/keymap.c
@@ -12,7 +12,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */#include QMK_KEYBOARD_H
+ */
+
+#include QMK_KEYBOARD_H
 #include "rossman360.h"
 
 #define PGMOD LT(_NUM, KC_PGDN)
