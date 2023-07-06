8bb1dd7645e8da8329f2dbfff838db74973b6915
Joel Challis <git@zvecr.com>
2023-07-06 02:55:18 +0100

    KEYMAP_OUTPUT -> INTERMEDIATE_OUTPUT

--- builddefs/xap.mk
+++ builddefs/xap.mk
@@ -24,21 +24,21 @@ ifneq ("$(wildcard $(USER_NAME)/xap.hjson)","")
 	XAP_FILES += $(USER_NAME)/xap.hjson
 endif
 
-$(KEYMAP_OUTPUT)/src/config_blob_gz.h: $(INFO_JSON_FILES)
+$(INTERMEDIATE_OUTPUT)/src/config_blob_gz.h: $(INFO_JSON_FILES)
 	@$(SILENT) || printf "$(MSG_GENERATING) $@" | $(AWK_CMD)
-	$(eval CMD=$(QMK_BIN) xap-generate-qmk-blob-h -o "$(KEYMAP_OUTPUT)/src/config_blob_gz.h" -kb $(KEYBOARD) -km $(KEYMAP))
+	$(eval CMD=$(QMK_BIN) xap-generate-qmk-blob-h -o "$(INTERMEDIATE_OUTPUT)/src/config_blob_gz.h" -kb $(KEYBOARD) -km $(KEYMAP))
 	@$(BUILD_CMD)
 
-$(KEYMAP_OUTPUT)/src/xap_generated.inl: $(XAP_FILES)
+$(INTERMEDIATE_OUTPUT)/src/xap_generated.inl: $(XAP_FILES)
 	@$(SILENT) || printf "$(MSG_GENERATING) $@" | $(AWK_CMD)
-	$(eval CMD=$(QMK_BIN) xap-generate-qmk-inc -o "$(KEYMAP_OUTPUT)/src/xap_generated.inl" -kb $(KEYBOARD) -km $(KEYMAP))
+	$(eval CMD=$(QMK_BIN) xap-generate-qmk-inc -o "$(INTERMEDIATE_OUTPUT)/src/xap_generated.inl" -kb $(KEYBOARD) -km $(KEYMAP))
 	@$(BUILD_CMD)
 
-$(KEYMAP_OUTPUT)/src/xap_generated.h: $(XAP_FILES)
+$(INTERMEDIATE_OUTPUT)/src/xap_generated.h: $(XAP_FILES)
 	@$(SILENT) || printf "$(MSG_GENERATING) $@" | $(AWK_CMD)
-	$(eval CMD=$(QMK_BIN) xap-generate-qmk-h -o "$(KEYMAP_OUTPUT)/src/xap_generated.h" -kb $(KEYBOARD) -km $(KEYMAP))
+	$(eval CMD=$(QMK_BIN) xap-generate-qmk-h -o "$(INTERMEDIATE_OUTPUT)/src/xap_generated.h" -kb $(KEYBOARD) -km $(KEYMAP))
 	@$(BUILD_CMD)
 
-generated-files: $(KEYMAP_OUTPUT)/src/config_blob_gz.h $(KEYMAP_OUTPUT)/src/xap_generated.inl $(KEYMAP_OUTPUT)/src/xap_generated.h
+generated-files: $(INTERMEDIATE_OUTPUT)/src/config_blob_gz.h $(INTERMEDIATE_OUTPUT)/src/xap_generated.inl $(INTERMEDIATE_OUTPUT)/src/xap_generated.h
 
-VPATH += $(KEYMAP_OUTPUT)/src
+VPATH += $(INTERMEDIATE_OUTPUT)/src
