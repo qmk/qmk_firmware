fa0f7186f66b94c3099b7db0df772cabaf987562
Kasper Flye <kasperflye@gmail.com>
2023-07-10 18:42:37 +0200

    add crkbd

---
+++ keyboards/crkbd/keymaps/cflye/keymap.c
@@ -0,0 +1,222 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "cflye.h"
+#include "features/achordion.h"
+#include <stdio.h>
+
+
+#define LAYOUT_crkbd_wrapper(...) LAYOUT(__VA_ARGS__)
+
+#define LAYOUT_crkbd_base(\
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
+     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+) \
+LAYOUT_crkbd_wrapper( \
+  KC_DEL,  K00, K01, K02, K03, K04,           K05, K06, K07, K08, K09, KC_BSPC, \
+  KC_LSFT, K10, K11, K12, K13, K14,           K15, K16, K17, K18, K19, KC_RSFT, \
+  KC_LCTL, K20, K21, K22, K23, K24,           K25, K26, K27, K28, K29, KC_RCTL, \
+                        K32, K33, K34,     K35, K36, K37 \
+  ) \
+
+#define LAYOUT_base_wrapper(...) LAYOUT_crkbd_base(__VA_ARGS__)
+
+#undef _________________THUMB_LEFT________________
+#undef _________________THUMB_RIGHT_______________
+#define _________________THUMB_LEFT________________     KC_NO,             KC_NO,           MO(_MOUSE),  LT(_MEDIA,KC_ESC),   LT(_NAV,KC_SPC)
+#define _________________THUMB_RIGHT_______________     LT(_SYM,KC_ENT),   LT(_NUM,KC_TAB), MO(_FUN),    KC_NO,               KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_base_wrapper(
+        _________________COLEMAK_L1_________________, _________________COLEMAK_R1_________________,
+        _________________COLEMAK_L2_________________, _________________COLEMAK_R2_________________,
+        _________________COLEMAK_L3_________________, _________________COLEMAK_R3_________________,
+        _________________THUMB_LEFT________________,  _________________THUMB_RIGHT_______________
+    ),
+    [_SYM] = LAYOUT_base_wrapper(
+        ___________________SYM_L1___________________, ___________________SYM_R1___________________,
+        ___________________SYM_L2___________________, ___________________SYM_R2___________________,
+        ___________________SYM_L3___________________, ___________________SYM_R3___________________,
+        ___________________BLANK___________________,  ___________________BLANK___________________
+    ),
+    [_NUM] = LAYOUT_base_wrapper(
+        ___________________NUM_L1___________________, ___________________NUM_R1___________________,
+        ___________________NUM_L2___________________, ___________________NUM_R2___________________,
+        ___________________NUM_L3___________________, ___________________NUM_R3___________________,
+        ___________________NUM_L4___________________,  ___________________BLANK___________________
+    ),
+    [_NAV] = LAYOUT_base_wrapper(
+        ___________________NAV_L1___________________, ___________________NAV_R1___________________,
+        ___________________NAV_L2___________________, ___________________NAV_R2___________________,
+        ___________________NAV_L3___________________, ___________________NAV_R3___________________,
+        ___________________NAV_L4___________________, ___________________NAV_R4___________________
+    ),
+    [_MEDIA] = LAYOUT_base_wrapper(
+        __________________MEDIA_L1__________________, __________________MEDIA_R1__________________,
+        __________________MEDIA_L2__________________, __________________MEDIA_R2__________________,
+        __________________MEDIA_L3__________________, __________________MEDIA_R3__________________,
+        __________________MEDIA_L4__________________, __________________MEDIA_R4__________________
+    ),
+    [_FUN] = LAYOUT_base_wrapper(
+        ___________________FUN_L1___________________, ___________________FUN_R1___________________,
+        ___________________FUN_L2___________________, ___________________FUN_R2___________________,
+        ___________________FUN_L3___________________, ___________________FUN_R3___________________,
+        ___________________FUN_L4___________________, ___________________FUN_R4___________________
+    ),
+    [_MOUSE] = LAYOUT_base_wrapper(
+        __________________MOUSE_L1__________________, __________________MOUSE_R1__________________,
+        __________________MOUSE_L2__________________, __________________MOUSE_R2__________________,
+        __________________MOUSE_L3__________________, __________________MOUSE_R3__________________,
+        __________________MOUSE_L4__________________, __________________MOUSE_R4__________________
+    ),
+};
+
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  uprintf("achordion_chord: %2u kc: 0x%04X, col: %2u, row: %2u, kc: 0x%04X, col: %2u, row: %2u, res %2u\n", MATRIX_ROWS, other_keycode, other_record->event.key.col, other_record->event.key.row, tap_hold_keycode, tap_hold_record->event.key.col, tap_hold_record->event.key.row, tap_hold_record->event.key.row % (MATRIX_ROWS / 2) >= 4);
+    return true;
+    // TODO: left right does not seem to function, it believes (e+a) is same hand
+  // Exceptionally consider the following chords as holds, even though they
+  // are on the same hand in Dvorak.
+  switch (tap_hold_keycode) {
+    case HOME_S:  // S + D and S + W.
+      if (other_keycode == KC_D || other_keycode == KC_W || other_keycode == KC_P) { return true; }
+      break;
+  }
+  
+  // Thumb keys
+  if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 3|| tap_hold_record->event.key.row % (MATRIX_ROWS / 2) >= 3 ) { return true; }
+
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, _NAV, _SYM, _FUN);
+    return state;
+}
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master()) {
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  }
+  return rotation;
+}
+
+#define L_BASE 0
+#define L_LOWER 2
+#define L_RAISE 4
+#define L_ADJUST 8
+
+void oled_render_layer_state(void) {
+    oled_write_P(PSTR("Layer: "), false);
+    switch (layer_state) {
+        case L_BASE:
+            oled_write_ln_P(PSTR("Default"), false);
+            break;
+        case L_LOWER:
+            oled_write_ln_P(PSTR("Lower"), false);
+            break;
+        case L_RAISE:
+            oled_write_ln_P(PSTR("Raise"), false);
+            break;
+        case L_ADJUST:
+        case L_ADJUST|L_LOWER:
+        case L_ADJUST|L_RAISE:
+        case L_ADJUST|L_LOWER|L_RAISE:
+            oled_write_ln_P(PSTR("Adjust"), false);
+            break;
+    }
+}
+
+
+char keylog_str[24] = {};
+
+const char code_to_name[60] = {
+    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
+    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+
+void set_keylog(uint16_t keycode, keyrecord_t *record) {
+  char name = ' ';
+    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
+        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
+  if (keycode < 60) {
+    name = code_to_name[keycode];
+  }
+
+  // update keylog
+  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
+           record->event.key.row, record->event.key.col,
+           keycode, name);
+}
+
+void oled_render_keylog(void) {
+    oled_write(keylog_str, false);
+}
+
+void render_bootmagic_status(bool status) {
+    /* Show Ctrl-Gui Swap options */
+    static const char PROGMEM logo[][2][3] = {
+        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
+        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
+    };
+    if (status) {
+        oled_write_ln_P(logo[0][0], false);
+        oled_write_ln_P(logo[0][1], false);
+    } else {
+        oled_write_ln_P(logo[1][0], false);
+        oled_write_ln_P(logo[1][1], false);
+    }
+}
+
+void oled_render_logo(void) {
+    static const char PROGMEM crkbd_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+        0};
+    oled_write_P(crkbd_logo, false);
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        oled_render_layer_state();
+        oled_render_keylog();
+    } else {
+        oled_render_logo();
+    }
+    return false;
+}
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    set_keylog(keycode, record);
+  }
+  return true;
+}
+#endif // OLED_ENABLE
---
+++ keyboards/crkbd/keymaps/cflye/rules.mk
@@ -0,0 +1,12 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+
+CONSOLE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+
+CONVERT_TO		= rp2040_ce
+BOARD 			= GENERIC_PROMICRO_RP2040
