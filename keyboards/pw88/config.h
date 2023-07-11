448e35ab755ebd7ab19ddc5e9059f997eb3271a1
smollchungusm <mpamuller@msn.com>
2023-07-11 20:36:07 +0200

    update config.h

--- keyboards/pw88/config.h
+++ keyboards/pw88/config.h
@@ -19,23 +19,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
-#define WD2812_BYTE_ORDER_RGB // Force the WS2812 implementation to use RGB byte order, by default the WS2812 implementation uses GRB byte order
-
-#undef RGBLED_NUM
-#define WS2812_DI_PIN GP0
-
-/* Define RGB */
-#define RGBLED_NUM 34
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#define RGB_VAL_STEP 12
-
-#define RGBLIGHT_LAYERS
