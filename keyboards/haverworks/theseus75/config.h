3baac8ca728930fd3204f375ebe3363979256ff7
Moritz <moritz.plattner@gmx.net>
2023-07-12 12:26:29 +0200

    fixed weird ghost inputs at powerup

--- keyboards/haverworks/theseus75/config.h
+++ keyboards/haverworks/theseus75/config.h
@@ -29,7 +29,7 @@
 //#define SELECT_SOFT_SERIAL_SPEED {0}    // Experimental, will make connection faster but may need testing.
 
 /* Set up various split configurations */
-//#define SPLIT_USB_DETECT                // The side that manages to establish a USB connection will be master. Disabled because default on ARM targets.
+#define SPLIT_USB_DETECT                // The side that manages to establish a USB connection will be master. Disabled because default on ARM targets.
 
 #define SPLIT_HAND_PIN B9               // The MCUs will probe a pin to determine if they are left or right side. high = left, low = right
 
--- keyboards/haverworks/theseus75/theseus75.c
+++ keyboards/haverworks/theseus75/theseus75.c
@@ -3,12 +3,40 @@
 
 #include "quantum.h"
 
-void keyboard_post_init_kb(void) {
+void keyboard_pre_init_kb(void) {
     PWR->CR3 |= PWR_CR3_UCPD_DBDIS;
-    // If the keyboard is master, turn on power to the split half, and to underglow LEDs
+}
+void keyboard_post_init_kb(void) {
+    // If the keyboard is master
     if (is_keyboard_master()) {
+        // Turn on power to the split half and to underglow LEDs
         setPinOutput(A15);
         writePinHigh(A15);
+
+        // Enable inputs used for current negotiation
+        setPinInput(F0);
+        setPinInput(F1);
+
+        // Not needed in this mode (always high-Z with pull-up on PCB if port controller is sink)
+        setPinInput(A14);
+    } else {
+        // Do not turn on power to the USB source port (let the port controller handle VBUS)
+        setPinOutput(A15);
+        //writePinHigh(A15);
+
+        // Disable the hub, route USB from split con to USB connector for downstream port
+        setPinOutput(A13);
+        writePinHigh(A13);
+
+        // Enable outputs used for current negotiation
+        setPinOutput(F0);
+        setPinOutput(F1);
+
+        // For now: 500 mA only
+        writePinLow(F0);
+        writePinLow(F1);
+
+        // Use ID pin to check if client is detected (if low: USB source port powered)
+        setPinInput(A14);
     };
-    keyboard_post_init_user();
 }
