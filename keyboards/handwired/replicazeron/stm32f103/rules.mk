c976aa5bcf1ee527b7493c6a896d8b2287bf61d7
9R <git@9-r.net>
2023-07-12 20:50:04 +0200

    remove CORTEX_ENABLE_WFI_IDLE from rules.mk as suggested by @drashna

--- keyboards/handwired/replicazeron/stm32f103/rules.mk
+++ keyboards/handwired/replicazeron/stm32f103/rules.mk
@@ -4,6 +4,3 @@ EXTRAKEY_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 NKRO_ENABLE = yes
 RGBLIGHT_ENABLE = yes
-
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
