9311c758026cda38847acfe64fd462f1481aeed9
sam <samkelly1290@gmail.com>
2023-07-12 19:42:00 +1000

    converted all tabs to 4-spaces as per code style guidelines

--- keyboards/handwired/bongo80/bongo80.c
+++ keyboards/handwired/bongo80/bongo80.c
@@ -34,32 +34,32 @@ const size_t frame_size = sizeof(idle_0);
 
 // Framesets for each typing state
 const struct frame_set no_caps[] = {
-	{.frames = idle_no_caps, .size = sizeof(idle_no_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
-	{.frames = slow_no_caps, .size = sizeof(slow_no_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
-	{.frames = med_no_caps,  .size = sizeof(med_no_caps)/sizeof(char*),  .frame_len = FRAME_LENGTH_MED},
-	{.frames = fast_no_caps, .size = sizeof(fast_no_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_FAST}
+    {.frames = idle_no_caps, .size = sizeof(idle_no_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
+    {.frames = slow_no_caps, .size = sizeof(slow_no_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
+    {.frames = med_no_caps,  .size = sizeof(med_no_caps)/sizeof(char*),  .frame_len = FRAME_LENGTH_MED},
+    {.frames = fast_no_caps, .size = sizeof(fast_no_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_FAST}
 };
 
 const struct frame_set caps[] = {
-	{.frames = idle_caps, .size = sizeof(idle_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
-	{.frames = slow_caps, .size = sizeof(slow_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
-	{.frames = med_caps,  .size = sizeof(med_caps)/sizeof(char*),  .frame_len = FRAME_LENGTH_MED},
-	{.frames = fast_caps, .size = sizeof(fast_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_FAST}
+    {.frames = idle_caps, .size = sizeof(idle_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
+    {.frames = slow_caps, .size = sizeof(slow_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_SLOW},
+    {.frames = med_caps,  .size = sizeof(med_caps)/sizeof(char*),  .frame_len = FRAME_LENGTH_MED},
+    {.frames = fast_caps, .size = sizeof(fast_caps)/sizeof(char*), .frame_len = FRAME_LENGTH_FAST}
 };
 
 const struct frame_set* frame_sets[] = {no_caps, caps};
 uint8_t curr_frame_index = 0;
+uint8_t frame_set_index = 0;
 
 static uint8_t curr_wpm = 0;
 led_t led_usb_state;
 bool run_oled = 1;
 uint32_t time = 0;
-uint8_t frame_set_index = 0;
 
 static void render_wpm(void) {
 
-	// Writes the WPM to the screen, and caps if enabled
-    oled_set_cursor(0, 7);
+    // Writes the WPM to the screen, and caps if enabled
+oled_set_cursor(0, 7);
     oled_write_P(PSTR("WPM:"), false);
     oled_write(get_u8_str(curr_wpm, ' '), false);
 
@@ -74,71 +74,71 @@ static void render_wpm(void) {
 static void render_bongocat(void) {
 
     oled_set_cursor(0, 0);
-	// Allows for frameset to change to caps state without waiting for end of frame set
-	struct frame_set curr_frame_set = frame_sets[led_usb_state.caps_lock][frame_set_index];
+    // Allows for frameset to change to caps state without waiting for end of frame set
+    struct frame_set curr_frame_set = frame_sets[led_usb_state.caps_lock][frame_set_index];
 
-	// Updates frame being displayed based on set interval
+    // Updates frame being displayed based on set interval
     if (timer_elapsed(time) > curr_frame_set.frame_len) {
 
         time = timer_read();
 
-		// Ensures a smoothly animated transition between the different states of the animation
-		if (curr_frame_index == curr_frame_set.size - 1) {
+        // Ensures a smoothly animated transition between the different states of the animation
+        if (curr_frame_index == curr_frame_set.size - 1) {
 
-			// Assigns frameset based on typing speed
-			if (curr_wpm <= IDLE_UPPER_BOUND) {
-				frame_set_index = IDLE;
+            // Assigns frameset based on typing speed
+            if (curr_wpm <= IDLE_UPPER_BOUND) {
+                frame_set_index = IDLE;
 
-			} else if (curr_wpm <= SLOW_UPPER_BOUND) {
-				frame_set_index = SLOW;
+            } else if (curr_wpm <= SLOW_UPPER_BOUND) {
+                frame_set_index = SLOW;
 
-			} else if (curr_wpm <= MED_UPPER_BOUND) {
-				frame_set_index = MED;
+            } else if (curr_wpm <= MED_UPPER_BOUND) {
+                frame_set_index = MED;
 
-			} else {
-				frame_set_index = FAST;
-			}
+            } else {
+                frame_set_index = FAST;
+            }
 
-			curr_frame_index = 0;
+            curr_frame_index = 0;
 
-		} else {
-        	curr_frame_index++;
-		}
+        } else {
+            curr_frame_index++;
+        }
 
-		oled_write_raw_P(curr_frame_set.frames[curr_frame_index], frame_size);
+        oled_write_raw_P(curr_frame_set.frames[curr_frame_index], frame_size);
     }
 }
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-	
+    
     if (!process_record_user(keycode, record)) {
         return false;
     }
 
-	switch (keycode) {
-		// Handles the keycode for turning on and off the oled screen
-		case KC_OLED_STATE:
-			if (record->event.pressed) {
-				run_oled = !run_oled;
-				oled_clear();
-			}
-			return false;
-	}
-	return true;
+    switch (keycode) {
+        // Handles the keycode for turning on and off the oled screen
+        case KC_OLED_STATE:
+            if (record->event.pressed) {
+                run_oled = !run_oled;
+                oled_clear();
+            }
+            return false;
+    }
+    return true;
 }
 
 bool oled_task_kb(void) {
-	
+
     if (!oled_task_user()) {
         return false;
     }
 
-	if (run_oled) {
-		curr_wpm = get_current_wpm();
-		led_usb_state = host_keyboard_led_state();
-		render_wpm();
-    	render_bongocat();
-	}
+    if (run_oled) {
+        curr_wpm = get_current_wpm();
+        led_usb_state = host_keyboard_led_state();
+        render_wpm();
+        render_bongocat();
+    }
     return false;
 }
 
