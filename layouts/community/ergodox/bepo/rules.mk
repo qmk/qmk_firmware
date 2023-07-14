25a6f8a4b9e5128ab288fa6723282a60cfadc4b9
Olivier <olivier@gid0.org>
2023-07-14 21:13:48 +0200

    Restore deleted keymap.

---
+++ layouts/community/ergodox/bepo/rules.mk
@@ -0,0 +1,7 @@
+# Having a file like this allows you to override Makefile definitions
+# for your own particular keymap
+
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+COMMAND_ENABLE   = no  # Commands for debug and configuration
+
+
