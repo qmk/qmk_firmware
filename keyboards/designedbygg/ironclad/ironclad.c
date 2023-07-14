a80e21daba39d2dbcec8a258d62c317c45028d44
dexter93 <d3xter93@gmail.com>
2023-07-14 13:21:19 +0300

    Add support for Designed by GG Ironclad (#315)
    * Add designedbyGG Ironclad
    
    support ICL-01 v1 and v2
    
    * linter gone wild here
    let's do something more.. readable
    
    * bump debounce time for ironlad to 10ms

---
+++ keyboards/designedbygg/ironclad/ironclad.c
@@ -0,0 +1,24 @@
+/* Copyright 2023 Dimitris Mantzouranis <d3xter93@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "quantum.h"
+#ifndef RGB_MATRIX_ENABLE
+void matrix_output_unselect_delay(uint8_t line, bool key_pressed) {
+    for (int i = 0; i < TIME_US2I(MATRIX_IO_DELAY); ++i) {
+        __asm__ volatile("" ::: "memory");
+    }
+}
+#endif
