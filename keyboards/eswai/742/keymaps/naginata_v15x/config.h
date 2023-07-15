047ae5a9a8c479e0a44c8f57967510c50d8a0112
eswai <sakamoto.yuki@gmail.com>
2023-07-15 09:26:34 +0900

    OLEDがスリープに入るように

--- keyboards/eswai/742/keymaps/naginata_v15x/config.h
+++ keyboards/eswai/742/keymaps/naginata_v15x/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define OLED_UPDATE_INTERVAL 100
-#define OLED_TIMEOUT 60000
+// #define OLED_UPDATE_INTERVAL 100
+// #define OLED_TIMEOUT 60000
 
 // #define NG_NO_HENSHU1
 // #define NG_NO_HENSHU2
