5b50e8b9825315d6153ec66b5b0fb537fe999b8a
Yizhen Liu <62583086+edwardslau@users.noreply.github.com>
2023-07-19 17:00:40 +0800

    Update think65v3.h

--- keyboards/gray_studio/think65v3/think65v3.h
+++ keyboards/gray_studio/think65v3/think65v3.h
@@ -24,7 +24,7 @@
     { k10, KC_NO, k12,   k13, k14,   k15, k16,   k17, k18, k19,   k1A, k1B, k1C,   k1D, k1E,   k1F }, \
     { k20, KC_NO, k22,   k23, k24,   k25, k26,   k27, k28, k29,   k2A, k2B, k2C,   k2D, KC_NO, KC_NO }, \
     { k30, k31,   k32,   k33, k34,   k35, k36,   k37, k38, k39,   k3A, k3B, KC_NO, k3D, k3E,   KC_NO }, \
-    { k40, k41,   k42, KC_NO,   KC_NO,KC_NO,KC_NO, k47,KC_NO, KC_NO,k4A, k4B, KC_NO, k4D, k4E,   k4F }, \
+    { k40, k41,   K42, KC_NO,   KC_NO,KC_NO,KC_NO, k47,KC_NO, KC_NO,k4A, k4B, KC_NO, k4D, k4E,   k4F }, \
 }
 
 #define LAYOUT_65_blocker( \
@@ -53,5 +53,5 @@
     { K10,   KC_NO, K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K1A,   K1B,   K1C,   K1D,   K1E,   K1F   }, \
     { K20,   KC_NO, K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A,   K2B,   K2C,   K2D,   KC_NO, KC_NO }, \
     { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   K3A,   K3B,   KC_NO, K3D,   K3E,   KC_NO }, \
-    { K40,   K41,   k42, KC_NO,   KC_NO, KC_NO, KC_NO, K47,   KC_NO, KC_NO, K4A,   K4B,   KC_NO, K4D,   K4E,   K4F   }, \
+    { K40,   K41,   K42, KC_NO,   KC_NO, KC_NO, KC_NO, K47,   KC_NO, KC_NO, K4A,   K4B,   KC_NO, K4D,   K4E,   K4F   }, \
 }
