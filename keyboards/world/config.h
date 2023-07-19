ba92215750a66a6c14efdb627fe40614f14fe033
anhdiepmmk <n08ni.dieppn@gmail.com>
2023-07-20 00:44:15 +0700

    feat: config rgb

--- keyboards/world/config.h
+++ keyboards/world/config.h
@@ -3,6 +3,20 @@
 
 #pragma once
 
+/* ws2812 RGB LED */
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
+#define RGBLED_NUM 2
+#define RGBLIGHT_LED_MAP { 0,  1 }
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
--- keyboards/world/keymaps/default/keymap.c
+++ keyboards/world/keymaps/default/keymap.c
@@ -1,24 +1,10 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
     [0] = LAYOUT(
-        KC_P7,   KC_P7,   KC_P7,   KC_7, KC_7,    KC_7, KC_7, KC_7, KC_7, KC_7,
-        KC_P7,   KC_P7,   KC_P7,   KC_7, KC_7,    KC_7, KC_7, KC_7, KC_7, KC_7,
-        KC_P7,   KC_P7,   KC_P7,   KC_7, KC_7,    KC_7, KC_7, KC_7, KC_7, KC_7,
-        KC_P7,   KC_P7,   KC_P7,   KC_7, KC_7,    KC_7, KC_7, KC_7, KC_7, KC_7
+        RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,   KC_7,    KC_7, KC_7, KC_7, KC_7, KC_7,
+        RGB_MODE_BREATHE,   RGB_MODE_FORWARD,   KC_P7,   KC_7,      KC_7,    KC_7, KC_7, KC_7, KC_7, KC_7,
+        RGB_MODE_SNAKE,   KC_P7,   KC_P7,   KC_7,      KC_7,    KC_7, KC_7, KC_7, KC_7, KC_7,
+        KC_P7,   KC_P7,   KC_P7,   KC_7,      QK_BOOT,    KC_7, KC_7, KC_7, KC_7, KC_7
     )
 };
