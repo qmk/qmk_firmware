991efd1907ffe4e24776da0fd17e7ded744a0cb1
mblex <matthias.blex@googlemail.com>
2023-07-21 20:00:08 +0200

    stuff

--- keyboards/splitkb/aurora/corne/keymaps/bonezer/rules.mk
+++ keyboards/splitkb/aurora/corne/keymaps/bonezer/rules.mk
@@ -1,7 +1,8 @@
 # Convert from Elite-C to standard RP2040
 CONVERT_TO=promicro_rp2040
 
-RGB_MATRIX_ENABLE = no
 RGBLIGHT_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
 CONSOLE_ENABLE = yes
 TAP_DANCE_ENABLE = no
+OLED_ENABLE = yes
