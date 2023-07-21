221557e3935ca4dc79694814472bdf89630bdd22
Yizhen Liu <62583086+edwardslau@users.noreply.github.com>
2023-07-21 09:48:01 +0800

    Update keyboards/gray_studio/think65v3/think65v3.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/gray_studio/think65v3/think65v3.c
+++ keyboards/gray_studio/think65v3/think65v3.c
@@ -8,11 +8,15 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer
 );
 
-void keyboard_post_init_user(void) {
+void keyboard_post_init_kb(void) {
     // Enable the LED layers
     rgblight_layers = my_rgb_layers;
+    keyboard_post_init_user();
 }
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(0, led_state.caps_lock);
-    return true;
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if (res) {
+        rgblight_set_layer_state(0, led_state.caps_lock);
+    }
+    return res;
 }
