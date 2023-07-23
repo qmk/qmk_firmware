934b56dbfd41341fe2ca98ef27b70cb8e6073bd2
ziptyze <mwcordier@gmail.com>
2023-07-22 20:29:00 -0400

    Update keyboards/1upkeyboards/1upslider8/keymaps/default/keymap.c
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/1upkeyboards/1upslider8/keymaps/default/keymap.c
+++ keyboards/1upkeyboards/1upslider8/keymaps/default/keymap.c
@@ -23,7 +23,7 @@
 #include "quantum/midi/midi_device.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_2x4(
+    [0] = LAYOUT(
                                    RGB_TOG,
         KC_1,     KC_2,     KC_3,     KC_4,
         KC_5,     KC_6,     KC_7,     KC_8
