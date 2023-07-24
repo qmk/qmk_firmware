88fd782ecee61eaf7ae95e2642ee6154b7d030a4
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-07-24 20:44:18 +0300

    Delete keyboards/darkproject/kd87b_ltd/keymaps/gaming_by_shvd3x directory

--- keyboards/darkproject/kd87b_ltd/keymaps/gaming_by_shvd3x/rules.mk
+++
@@ -1,6 +0,0 @@
-VIA_ENABLE = yes
-DEBOUNCE_TYPE = sym_eager_pk #If you have issues with double clicking open keymap's readme and follow the instructions
-
-OPT = 3
-
-SPACE_CADET_ENABLE = no #If you for some reason need this just remove this string
