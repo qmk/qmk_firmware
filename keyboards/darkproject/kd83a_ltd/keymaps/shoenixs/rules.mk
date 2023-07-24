84a2ea35331f1225882e621b1afaac51b720750e
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-07-24 20:29:10 +0300

    Delete keyboards/darkproject/kd83a_ltd/keymaps/shoenixs directory

--- keyboards/darkproject/kd83a_ltd/keymaps/shoenixs/rules.mk
+++
@@ -1,9 +0,0 @@
-VIA_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-DEBOUNCE_TYPE = sym_eager_pk
-
-OPT = 3
-
-SPACE_CADET_ENABLE = no #If you need this feature just remove this string
-
-MOUSEKEY_ENABLE = no
