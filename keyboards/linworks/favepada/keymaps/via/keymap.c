0907d18c090cd6998e68c4515094dd4a59a61f10
DUILCHOI <48977039+duilchoi@users.noreply.github.com>
2023-07-24 16:55:10 +0900

    Update keyboards/linworks/favepada/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/linworks/favepada/keymaps/via/keymap.c
+++ keyboards/linworks/favepada/keymaps/via/keymap.c
@@ -19,25 +19,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_numpad_6x4(
         MO(1),    KC_A,    KC_B,    KC_C,
         KC_NUM,   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,    KC_P5,   KC_P6,   
-        KC_P1,    KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,             KC_PDOT 
+        KC_P7,    KC_P8,   KC_P9,
+        KC_P4,    KC_P5,   KC_P6,   KC_PPLS,   
+        KC_P1,    KC_P2,   KC_P3, 
+        KC_P0,             KC_PDOT, KC_PENT 
     ),
     [1] = LAYOUT_numpad_6x4(
         KC_TRNS,  RGB_TOG, RGB_MOD, KC_D,
         KC_NUM,   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,    KC_P5,   KC_P6,   
-        KC_P1,    KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,             KC_PDOT 
+        KC_P7,    KC_P8,   KC_P9,
+        KC_P4,    KC_P5,   KC_P6,   KC_PPLS,   
+        KC_P1,    KC_P2,   KC_P3,   
+        KC_P0,             KC_PDOT, KC_PENT
     ),
     [2] = LAYOUT_numpad_6x4(
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
         KC_NUM,   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,    KC_P5,   KC_P6,   
-        KC_P1,    KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,             KC_PDOT 
+        KC_P7,    KC_P8,   KC_P9,
+        KC_P4,    KC_P5,   KC_P6,   KC_PPLS,   
+        KC_P1,    KC_P2,   KC_P3,   
+        KC_P0,             KC_PDOT, KC_PENT 
     )
 };
