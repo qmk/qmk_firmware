075cde7ba3ddae51b86caa180f25496ab08e4891
Yizhen Liu <62583086+edwardslau@users.noreply.github.com>
2023-07-25 21:48:15 +0800

    think65v3.h deleted, info.json updated

--- keyboards/gray_studio/think65v3/config.h
+++ keyboards/gray_studio/think65v3/config.h
@@ -1,17 +1,13 @@
 // Copyright 2023 Yizhen Liu (@edwardslau)
 // SPDX-License-Identifier: GPL-2.0
 #pragma once
-#define WS2812_DI_PIN B10
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
-#define RGBLED_NUM 6
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
-#define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-// /*== all animations enable ==*/
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
