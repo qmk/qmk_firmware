c76a07f8f5e0b3bb1d250ee7d361a8d759a81977
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-07-25 13:16:43 +0300

    Update keymap.c

--- keyboards/darkproject/kd87b_ltd/keymaps/default/keymap.c
+++ keyboards/darkproject/kd87b_ltd/keymaps/default/keymap.c
@@ -16,7 +16,6 @@
 
 #include QMK_KEYBOARD_H
 
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 enum custom_layers {
