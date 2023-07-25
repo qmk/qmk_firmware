29e83164e9a25ccdec9afbd813bf1e68681c08f4
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-07-25 22:09:27 +0300

    Update keyboards/darkproject/kd87b_ltd/keymaps/via/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/darkproject/kd87b_ltd/keymaps/via/keymap.c
+++ keyboards/darkproject/kd87b_ltd/keymaps/via/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,     TO(Win),  KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,       KC_LBRC,  KC_RBRC,  KC_BSLS,  RGB_MOD,  KC_END,   RGB_HUD,   
   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,    KC_QUOT,            KC_ENT,      
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     TO(Mac),  KC_COMM,  KC_DOT,   KC_SLSH,    KC_RSFT,            RGB_VAI,    
-  KC_LCTL,  GUI_TOG,  KC_LALT,                      KC_SPC,                       KC_RALT,  MO(Winfn),KC_APP,     KC_RCTL,  RGB_SPD,  RGB_VAD,  RGB_SPI),
+  KC_LCTL,  GUI_TOG,  KC_LALT,                      KC_SPC,                       KC_RALT,  _______, KC_APP,     KC_RCTL,  RGB_SPD,  RGB_VAD,  RGB_SPI),
 
 [Macfn] = LAYOUT_tkl_ansi(
   KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,   RGB_TOG,  KC_SCRL,  KC_PAUS, 
