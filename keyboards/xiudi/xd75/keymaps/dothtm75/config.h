25935b74ce6c258d2be9e793837301530cbb41cc
mechlovin <57231893+mechlovin@users.noreply.github.com>
2023-07-25 09:50:29 +0700

    Merge branch 'if88_rev2' of https://github.com/mechlovin/qmk_firmware into if88_rev2

--- keyboards/xiudi/xd75/keymaps/dothtm75/config.h
+++ keyboards/xiudi/xd75/keymaps/dothtm75/config.h
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:keyboards/xiudi/xd75/keymaps/dothtm75/config.h
 /* Copyright 2017 Mike Cramer
-========
-/* Copyright 2020 QMK
->>>>>>>> e0ecc772495b92032d36d918cfdb3c631bbdd078:keyboards/mechlovin/infinity88/rev3/mcuconf.h
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,22 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #pragma once
 
-<<<<<<<< HEAD:keyboards/xiudi/xd75/keymaps/dothtm75/config.h
 #define HOLD_ON_OTHER_KEY_PRESS
 
 #define MIDI_ADVANCED
-========
-#include_next <mcuconf.h>
-
-#undef STM32_PWM_USE_TIM4
-#define STM32_PWM_USE_TIM4 TRUE
-
-#undef STM32_SPI_USE_SPI2
-#define STM32_SPI_USE_SPI2 TRUE
-
-
-
->>>>>>>> e0ecc772495b92032d36d918cfdb3c631bbdd078:keyboards/mechlovin/infinity88/rev3/mcuconf.h
