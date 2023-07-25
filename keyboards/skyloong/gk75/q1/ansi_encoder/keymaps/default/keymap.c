db68be94f9afcffd1a3e371d13dff005a3186960
skyloong <zhangyuanji@jikedingzhi.com>
2023-07-25 09:43:58 +0800

    Some modified key codes are updated to comply with win11 system requirements.

--- keyboards/skyloong/gk75/q1/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/skyloong/gk75/q1/ansi_encoder/keymaps/default/keymap.c
@@ -6,7 +6,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_all(
-         KC_ESC,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,    KC_F6,      KC_F7,    KC_F8,       KC_F9,      KC_F10,              KC_F12,   KC_PSCR,
+         KC_ESC,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,    KC_F6,      KC_F7,    KC_F8,       KC_F9,      KC_F10,              KC_F12,   KC_MUTE,
          KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,       KC_8,     KC_9,        KC_0,     KC_MINS,   KC_EQL,   KC_BSPC,    KC_DEL,
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,       KC_I,     KC_O,        KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,   KC_PGUP,
         KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN,     KC_QUOT,              KC_ENT,   KC_PGDN,
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)), ENCODER_CCW_CW(LSG(KC_TAB),G(KC_TAB)) },
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)), ENCODER_CCW_CW(G(C(KC_LEFT)),G(C(KC_RGHT))) },
     [1] = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_SPD, RGB_SPI), ENCODER_CCW_CW(RGB_SAD, RGB_SAI) }
 };
 #endif
--- keyboards/skyloong/gk75/q1/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/skyloong/gk75/q1/ansi_encoder/keymaps/via/keymap.c
@@ -11,7 +11,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_WIN] =  LAYOUT_all(
-         KC_ESC,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,     KC_F6,     KC_F7,    KC_F8,           KC_F9,      KC_F10,   KC_F11,   KC_F12,    KC_PSCR,
+         KC_ESC,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,     KC_F6,     KC_F7,    KC_F8,           KC_F9,      KC_F10,   KC_F11,   KC_F12,    KC_MUTE,
          KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,       KC_8,     KC_9,            KC_0,     KC_MINS,   KC_EQL,   KC_BSPC,   KC_HOME,
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,       KC_I,     KC_O,            KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,    KC_DEL,
         KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,       KC_K,     KC_L,         KC_SCLN,     KC_QUOT,              KC_ENT,   KC_PGUP,
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
      [_MAC] =  LAYOUT_all(
-        _______,             KC_F14,   KC_F15, C(KC_UP),  KC_WSCH,   G(KC_SPC), LSG(KC_4),   KC_MPRV,  KC_MPLY,          KC_MNXT,      KC_MUTE,  KC_VOLD,  KC_VOLU,   KC_PSCR,
+        _______,             KC_F14,   KC_F15, C(KC_UP),  KC_WSCH,   G(KC_SPC), LSG(KC_4),   KC_MPRV,  KC_MPLY,          KC_MNXT,      KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,
          KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,      KC_7,       KC_8,     KC_9,            KC_0,      KC_MINS,   KC_EQL,   KC_BSPC,   KC_HOME,
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,      KC_U,       KC_I,     KC_O,            KC_P,      KC_LBRC,  KC_RBRC,   KC_BSLS,    KC_DEL,
         KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,      KC_J,       KC_K,     KC_L,         KC_SCLN,      KC_QUOT,              KC_ENT,   KC_PGUP,
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_WIN_INDEX] =  LAYOUT_all(
-        _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH, G(C(KC_S)), LCA(KC_A),   KC_MPRV,  KC_MPLY,    KC_MNXT,      KC_MUTE,  KC_VOLD,  KC_VOLU,   KC_PAUS,
+        _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH,  LSG(KC_S), LCA(KC_A),   KC_MPRV,  KC_MPLY,    KC_MNXT,      KC_MUTE,  KC_VOLD,  KC_VOLU,   KC_PAUS,
         _______,  _______,  _______,  _______,  _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,    RGB_TOG,     RGB_RMOD,  RGB_MOD,   KC_DEL,    KC_END,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,      _______,  _______,  _______,    KC_INS,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,      _______,            _______,   KC_PGDN,
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_WIN] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)), ENCODER_CCW_CW(LSG(KC_TAB),G(KC_TAB)) },
+    [_WIN] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)), ENCODER_CCW_CW(G(C(KC_LEFT)),G(C(KC_RGHT))) },
     [_MAC] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(LAG(KC_MINS), LAG(KC_EQL)), ENCODER_CCW_CW(C(KC_UP), C(KC_DOWN)) },
     [_WIN_INDEX] = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(RGB_SPD, RGB_SPI), ENCODER_CCW_CW(RGB_SAD, RGB_SAI) },
     [_MAC_INDEX] = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(RGB_SPD, RGB_SPI), ENCODER_CCW_CW(RGB_SAD, RGB_SAI) },
