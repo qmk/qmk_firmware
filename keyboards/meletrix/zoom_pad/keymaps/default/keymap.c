dcb662950f7ce8ec2730e1a86eaf94ef32a84a74
GG <zjjhgzh@gmail.com>
2023-07-26 16:54:38 +0800

    update

--- keyboards/meletrix/zoom_pad/keymaps/default/keymap.c
+++ keyboards/meletrix/zoom_pad/keymaps/default/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(
-        KC_ESC,  KC_DEL,  KC_HOME, KC_MUTE,
+        KC_ESC,  KC_DEL,  MO(1),   KC_MUTE,
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,   
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,
         KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_P0,            KC_PDOT
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, KC_MPLY,
         _______, _______, _______, _______,
         _______, _______, _______, _______,
-        _______, _______, _______, 
+        _______, _______, _______,
         _______, _______, _______, _______,
         _______,          _______
     ),
--- keyboards/meletrix/zoom_pad/keymaps/southpaw/keymap.c
+++ keyboards/meletrix/zoom_pad/keymaps/southpaw/keymap.c
@@ -25,18 +25,18 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_southpaw(
-        KC_MUTE, KC_HOME, KC_DEL,  KC_ESC,
+        KC_MUTE, MO(1),   KC_DEL,  KC_ESC,
         KC_PMNS, KC_PSLS, KC_PAST, KC_NUM,
-        KC_PPLS, KC_P7,   KC_P8,   KC_P9,   
+        KC_PPLS, KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,
-        KC_PENT, KC_P1,   KC_P2,   KC_P3,   
+        KC_PENT, KC_P1,   KC_P2,   KC_P3,
                  KC_P0,            KC_PDOT
     ),
     [1] = LAYOUT_southpaw(
-        KC_MPLY, _______, _______, _______, 
+        KC_MPLY, _______, _______, _______,
         _______, _______, _______, _______,
         _______, _______, _______, _______,
-        _______, _______, _______, 
+        _______, _______, _______,
         _______, _______, _______, _______,
                  _______,          _______
     ),
