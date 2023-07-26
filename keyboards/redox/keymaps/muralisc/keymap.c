c04d64fe86f0f7d9de9cd189f9563b19ac1ba795
Murali S <muralisc@gmail.com>
2023-07-26 22:18:14 +0100

    Apply suggestions from code review
    Co-authored-by: jack <0x6a73@protonmail.com>
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/redox/keymaps/muralisc/keymap.c
+++ keyboards/redox/keymaps/muralisc/keymap.c
@@ -4,16 +4,11 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _SYMB 1
-#define _NAV 2
-#define _ADJUST 3
-
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  SYMB,
-  NAV,
-  ADJUST,
+enum layers {
+    _QWERTY,
+    _SYMB,
+    _NAV,
+    _ADJUST,
 };
 
 // Shortcut to make keymap more readable
@@ -24,7 +19,6 @@ enum custom_keycodes {
 #define RAISE MO(_NAV)
 
 #define KC_NAGR LT(_NAV, KC_GRV)
-#define KC_NAMI LT(_NAV, KC_MINS)
 
 #define KC_ADEN LT(_ADJUST, KC_END)
 #define KC_ADPU LT(_ADJUST, KC_PGUP)
