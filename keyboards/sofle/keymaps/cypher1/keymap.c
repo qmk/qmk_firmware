44be86c6155d4e354a952c38493a8c03021e97ec
J Pratt <jp10010101010000@gmail.com>
2023-07-27 20:40:55 +1000

    Move slash and backslash to bottom row, clean up home/end/pgup/pgdn

--- keyboards/sofle/keymaps/cypher1/keymap.c
+++ keyboards/sofle/keymaps/cypher1/keymap.c
@@ -49,19 +49,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
   MT_LSHN,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  MT_RSHN,
-                      KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LGUI,   L1_SPC,  L2_BSPC,   L1_SPC,  KC_RCTL,  KC_RALT, TG(GAMES)
+                      KC_BSLS,  KC_LALT,  KC_LCTL,  KC_LGUI,   L1_SPC,  L2_BSPC,   L1_SPC,  KC_PGUP,  KC_PGDN, TG(GAMES)
 ),
 [GAMES] = LAYOUT(
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
   KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,   KC_ENT,
-                      KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LGUI,   KC_SPC,  KC_BSPC,   L1_SPC,  KC_RCTL,  KC_RALT, TG(GAMES)
+                      KC_BSLS,  KC_LALT,  KC_LCTL,  KC_LGUI,   KC_SPC,  KC_BSPC,   L1_SPC,  KC_PGUP,  KC_PGDN, TG(GAMES)
 ),
 [FUNCS] = LAYOUT(
   _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
   _______,  XXXXXXX,    KC_UP,  XXXXXXX,  KC_LBRC,  KC_RBRC,                      MS_W_DN,  MS_W_UP,  XXXXXXX,  XXXXXXX,  XXXXXXX,   KC_F12,
-  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,  KC_BSLS,                      MS_LEFT,  MS_DOWN,    MS_UP,  MS_RGHT,  XXXXXXX,  XXXXXXX,
+  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_HOME,   KC_END,                      MS_LEFT,  MS_DOWN,    MS_UP,  MS_RGHT,  XXXXXXX,  XXXXXXX,
   _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MINS,   KC_EQL,  _______,  MS_BTN1,  MS_BTN1,  MS_BTN3,  MS_BTN2,  XXXXXXX,  KC_BSLS,  _______,
                       _______,  _______,  _______,  _______,  _______,   KC_DEL,  _______,  _______,  _______, _______
 ),
