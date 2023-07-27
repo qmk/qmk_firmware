019d2a00032fe4e6e85bf82e42d3be7ba1d38006
J Pratt <jp10010101010000@gmail.com>
2023-07-27 20:22:13 +1000

    Use shortening CTRL to CTL

--- keyboards/sofle/keymaps/cypher1/keymap.c
+++ keyboards/sofle/keymaps/cypher1/keymap.c
@@ -49,14 +49,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
   MT_LSHN,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  MT_RSHN,
-                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   L1_SPC,  L2_BSPC,   L1_SPC, KC_RCTRL,  KC_RALT, TG(GAMES)
+                      KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LGUI,   L1_SPC,  L2_BSPC,   L1_SPC,  KC_RCTL,  KC_RALT, TG(GAMES)
 ),
 [GAMES] = LAYOUT(
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
   KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,   KC_ENT,
-                      KC_LGUI,  KC_LALT, KC_LCTRL,  KC_LGUI,   KC_SPC,  KC_BSPC,   L1_SPC, KC_RCTRL,  KC_RALT, TG(GAMES)
+                      KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LGUI,   KC_SPC,  KC_BSPC,   L1_SPC,  KC_RCTL,  KC_RALT, TG(GAMES)
 ),
 [FUNCS] = LAYOUT(
   _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
