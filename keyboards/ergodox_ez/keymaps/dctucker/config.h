77498d5f96c7e1c79f2b4d403d1d3151e0cb02c2
Casey Tucker <dctucker@github.com>
2023-07-28 09:41:03 -0700

    Update keyboards/ergodox_ez/keymaps/dctucker/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ergodox_ez/keymaps/dctucker/config.h
+++ keyboards/ergodox_ez/keymaps/dctucker/config.h
@@ -20,6 +20,8 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#pragma once
+
 #define PERMISSIVE_HOLD
 //#define TAPPING_FORCE_HOLD
 //#undef STRICT_LAYER_RELEASE // doesn't do anything
