7d1e88e57c282c078442e49efddcbc514268697a
fauxpark <fauxpark@gmail.com>
2023-07-29 16:24:24 +1000

    Move RGBLight animations to data driven, E

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -63,18 +63,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 #define LED_BRIGHTNESS_DEFAULT (LED_BRIGHTNESS_HI)
 
-/* ws2812 RGB LED */
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-
 // Pick one of the modes
 // Defaults to 15 mirror, for legacy behavior
 
