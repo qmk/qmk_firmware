46deac89fa3fe5d630e1e2707670bb740733d7f5
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-07-29 23:08:33 +0100

    move RGBLIGHT_EFFECT to info.json

--- keyboards/handwired/dmote/config.h
+++ keyboards/handwired/dmote/config.h
@@ -19,7 +19,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // WS2812 RGB LED, normally used to indicate keyboard state:
-#define RGBLIGHT_EFFECT_KNIGHT
 #define RGBLIGHT_EFFECT_KNIGHT_LENGTH 2
-#define RGBLIGHT_EFFECT_CHRISTMAS
 #define RGBLIGHT_EFFECT_CHRISTMAS_STEP 1
