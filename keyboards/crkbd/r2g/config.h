4199400399d02fbf2c98bcdba64602a185929533
fauxpark <fauxpark@gmail.com>
2023-07-29 16:24:07 +1000

    Move RGBLight animations to data driven, C

--- keyboards/crkbd/r2g/config.h
+++ keyboards/crkbd/r2g/config.h
@@ -19,20 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-
-#   define RGBLIGHT_EFFECT_BREATHING
-#   define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#   define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#   define RGBLIGHT_EFFECT_SNAKE
-#   define RGBLIGHT_EFFECT_KNIGHT
-#   define RGBLIGHT_EFFECT_CHRISTMAS
-#   define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#   define RGBLIGHT_EFFECT_RGB_TEST
-#   define RGBLIGHT_EFFECT_ALTERNATING
-#   define RGBLIGHT_EFFECT_TWINKLE
-#endif
-
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_MATRIX_LED_COUNT 54
 #    define RGB_MATRIX_SPLIT \
