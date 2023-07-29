ae4cdd08fa75d25311a35f24149648a5b06c15d3
fauxpark <fauxpark@gmail.com>
2023-07-29 16:29:47 +1000

    Move RGBLight animations to data driven, S

--- keyboards/silverbullet44/config.h
+++ keyboards/silverbullet44/config.h
@@ -29,15 +29,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_TYPING_HEATMAP
   #else
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-//  #define RGBLIGHT_EFFECT_KNIGHT
-//  #define RGBLIGHT_EFFECT_CHRISTMAS
-//  #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//  #define RGBLIGHT_EFFECT_RGB_TEST
-//  #define RGBLIGHT_EFFECT_ALTERNATING
 /*== customize breathing effect ==*/
   /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
     #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
