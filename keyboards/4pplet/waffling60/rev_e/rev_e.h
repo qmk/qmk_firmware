7e950b009337ee9554fc1a7f8d2db98e4e9c8506
4pplet <mail@4pplet.com>
2023-08-02 00:10:50 +0200

    Apply suggestions from code review
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/4pplet/waffling60/rev_e/rev_e.h
+++ keyboards/4pplet/waffling60/rev_e/rev_e.h
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
 
+#include "quantum.h"
+
 #define CAPS_LOCK_ENABLE true
 #define CAPS_LOCK_COLOR HSV_GREEN
-
-#include "quantum.h"
