a5c531418db374bc6d89d936bc00e0e2ca54b0a2
Richard Baptist <richard@remote.com>
2023-08-02 16:48:45 +0200

    Remove MOUSEKEY definition

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -87,15 +87,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 10
 #define TAPPING_TOGGLE 2
 
-#ifdef MOUSEKEY_ENABLE
-#    define MOUSEKEY_DELAY 0
-#    define MOUSEKEY_INTERVAL 16
-#    define MOUSEKEY_MAX_SPEED 6
-#    define MOUSEKEY_TIME_TO_MAX 36
-#    define MOUSEKEY_WHEEL_MAX_SPEED 4
-#    define MOUSEKEY_WHEEL_TIME_TO_MAX 100
-#endif
-
 #ifdef OLED_ENABLE
 #    define OLED_TIMEOUT 600000
 #    define OLED_FONT_H "glcdfont_multi.c"
