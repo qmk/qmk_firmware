e7c22e377b58a8855c108cbcd179bb22f07af59d
ClicketySplit <101202583+ClicketySplit@users.noreply.github.com>
2023-08-03 07:57:34 -0600

    Update keyboards/clickety_split/leeloo/rev2/rules.mk
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/clickety_split/leeloo/rev2/rules.mk
+++ keyboards/clickety_split/leeloo/rev2/rules.mk
@@ -1,3 +1,1 @@
 # Intentionally left blank.
-# See qmk_firmware\keyboards\clickety_split\leeloo\info.json and
-#     qmk_firmware\keyboards\clickety_split\leeloo\rev2\info.json for default settings.
