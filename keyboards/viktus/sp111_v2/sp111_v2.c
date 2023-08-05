c18b9f3d6f15e6fa5fc382b8b35535bd0d1432cd
Ele Thompson <ele.thompson12@gmail.com>
2023-08-04 22:57:32 -0700

    Removed via indicator code and moved more config into info.json

--- keyboards/viktus/sp111_v2/sp111_v2.c
+++ keyboards/viktus/sp111_v2/sp111_v2.c
@@ -1,7 +1,7 @@
 // Copyright 2023 BlindAssassin111 (@blindassassin111)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "sp111_v2.h"
+#include "quantum.h"
 
 void keyboard_pre_init_kb(void) {
     // enable built in pullups to avoid timeouts when right hand not connected
