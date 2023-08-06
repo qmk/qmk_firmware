1103dfae340d6a1a3864733946a1b75e3fe196af
zvecr <git@zvecr.com>
2023-08-06 02:40:47 +0100

    Ewww

--- keyboards/omnikeyish/config.h
+++ keyboards/omnikeyish/config.h
@@ -1,15 +1,15 @@
 #pragma once
 
-
 #define KEYBOARD_PCB_REV 11
 
 /* key matrix pins */
-#if KEYBOARD_PCB_REV == 10
-#define MATRIX_ROW_PINS { D2, D3, D4, D5, D6, D7 }
-#else
 #define MATRIX_ROW_PINS { D2, D3, D4, D5, E6, D7 }
-#endif
 #define MATRIX_COL_PINS { F0, F1, F2, F3, F4, F5, F6, F7, C7, C6, C5, C4, C3, C2, C1, C0, B0, B1, B2, B3, B4, B5, B6 }
+
+#if KEYBOARD_PCB_REV == 10
+#undef MATRIX_ROW_PINS
+#define MATRIX_ROW_PINS { D2, D3, D4, D5, D6, D7 }
+#endif
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
