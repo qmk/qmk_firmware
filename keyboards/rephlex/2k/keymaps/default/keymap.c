b1870b7f80e3f73ce5b9df8db36b1c1e3bccb858
RephlexZero <rephlexzero@protonmail.com>
2023-08-09 01:32:11 +0100

    Re-add default keymap to pass lint

---
+++ keyboards/rephlex/2k/keymaps/default/keymap.c
@@ -0,0 +1,15 @@
+/* Copyright 2023 RephlexZero (@RephlexZero)
+SPDX-License-Identifier: GPL-2.0-or-later */
+#include QMK_KEYBOARD_H
+#include "oled.h"
+
+/* This keymap does not work due to via dependance */
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┐
+     * │ Z │ X │
+     * └───┴───┘
+     */
+    [0] = LAYOUT_ortho_1x2(KC_Z, KC_X)
+};
