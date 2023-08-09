a39cb44781cc18eaf37025657d13a60fbcc05521
Daniel Schaefer <dhs@frame.work>
2023-08-09 11:41:56 +0800

    keyboards/framework: Clean up
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/framework/build_all.sh
+++
@@ -1,6 +0,0 @@
-set -e
-qmk compile -kb framework/ansi -km default
-qmk compile -kb framework/iso -km default
-qmk compile -kb framework/jis -km default
-qmk compile -kb framework/numpad -km default
-qmk compile -kb framework/macropad -km default
