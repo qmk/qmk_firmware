217d08111f36ac8b2d633436f5974fa83ce04f66
Ben Roe <ben@MacBookzPro-M2.lan>
2023-08-11 14:10:52 +0200

    migrate rule to info.json

--- keyboards/keycapsss/kimiko/rules.mk
+++ keyboards/keycapsss/kimiko/rules.mk
@@ -1,15 +1,5 @@
 # Build Options
 #   change yes to no to disable
 #
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = yes
 
-# LTO: Link Time Optimizations.
-# Reduce compiled size, but will automatically disable the legacy TMK Macros and Functions features.
-# This does not affect QMK Macros and Layers
-LTO_ENABLE = yes
+SPLIT_KEYBOARD = yes
