0a71dd384c57fc7618e2f9e8c370f7ef0a7d97b8
Controller Works <info@controller.works>
2023-08-10 22:28:24 -0400

    Removed RGB_MATRIX_VAL_STEP to avoid CI Build warn

--- keyboards/controllerworks/city42/config.h
+++ keyboards/controllerworks/city42/config.h
@@ -42,7 +42,7 @@
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
-#    define RGB_MATRIX_VAL_STEP 8
+// #    define RGB_MATRIX_VAL_STEP 8
 #    define RGB_MATRIX_SPD_STEP 10
 
 #    define ENABLE_RGB_MATRIX_ALPHAS_MODS
