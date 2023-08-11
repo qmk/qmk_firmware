812ffdaeac6b5682862b39fbc20295db878325cb
Filip Sund <filip.sund@bouvet.no>
2023-08-11 13:00:53 +0200

    Apply suggestions from code review

--- keyboards/pteron56/config.h
+++ keyboards/pteron56/config.h
@@ -14,35 +14,13 @@
 *You should have received a copy of the GNU General Public License
 *along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef CONFIG_H
-#define CONFIG_H
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x6D4B
-#define PRODUCT_ID      0x7056
-#define DEVICE_VER      0x0002
-#define MANUFACTURER    MooreKeys
-#define PRODUCT         Pteron56
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 12
-
-/* key matrix pins */
-#define MATRIX_ROW_PINS { D3, D2, F4, D4, B5 }
-#define MATRIX_COL_PINS { E6, B4, D7, C6, D0, D1, F6, F7, B1, B3, B6, B2 }
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION ROW2COL
+#pragma once
 
 /* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 3
+#ifdef BACKLIGHT_ENABLE
+#    define BACKLIGHT_LEVELS 3
 #endif
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -54,4 +32,3 @@
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
-#endif
