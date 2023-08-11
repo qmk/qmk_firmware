fc8361ae8cbb29465408471d68fdaf90044c6f74
Filip Sund <filip.sund@gmail.com>
2023-08-11 14:23:38 +0200

    Apply suggestions from code review
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/pteron56/keymaps/brant/config.h
+++ keyboards/pteron56/keymaps/brant/config.h
@@ -1,20 +1,5 @@
 #pragma once
 
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 12
-
-/* key matrix pins */
-#define MATRIX_ROW_PINS { D3, D2, F4, D4, B5 }
-#define MATRIX_COL_PINS { E6, B4, D7, C6, D0, D1, F6, F7, B1, B3, B6, B2 }
-
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION ROW2COL
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNE 5
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 
