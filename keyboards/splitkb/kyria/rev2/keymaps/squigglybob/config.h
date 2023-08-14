5772de9672fe938a7d2bdf9cbb8948be0fe42b78
squigglybob <squigglybob@users.noreply.github.com>
2023-08-14 16:03:00 +0100

    Update config.h to take out default rgb setting
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/splitkb/kyria/rev2/keymaps/squigglybob/config.h
+++ keyboards/splitkb/kyria/rev2/keymaps/squigglybob/config.h
@@ -18,7 +18,6 @@
 
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_ANIMATIONS
-#    define RGBLIGHT_HUE_STEP  8
 #    define RGBLIGHT_SAT_STEP  8
 #    define RGBLIGHT_VAL_STEP  8
 #    define RGBLIGHT_LIMIT_VAL 150
