0cc389e7ac6dde253dad8ad25dae7393e86af2d7
Joe Scotto <me@joescotto.com>
2023-08-14 08:02:36 -0400

    Update keyboards/handwired/scottokeebs/scottoslant/keymaps/scotto/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/scottokeebs/scottoslant/keymaps/scotto/keymap.c
+++ keyboards/handwired/scottokeebs/scottoslant/keymaps/scotto/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F11, KC_NO,  KC_NO,   QK_BOOT, KC_TRNS,        KC_NO, KC_NO,   KC_NO,   KC_NO,                     KC_F12,
                 KC_ESC, KC_LCTL, KC_LALT, LGUI_T(KC_SPC),        KC_TRNS, KC_TRNS, TD(TD_ESC_SPOTLIGHT_EMOJI)
     ),
-    [4] = LAYOUT_3x10_7(
+    [4] = LAYOUT(
         KC_Q,         KC_W,   KC_F,    KC_P,    KC_G,   KC_J, KC_L,          KC_U,          KC_Y,                    KC_BSPC,
         KC_A,         KC_R,   KC_S,    KC_T,    KC_D,   KC_H, KC_N,          KC_E,          KC_I,                    KC_O,
         LSFT_T(KC_Z), KC_X,   KC_C,    KC_V,    KC_B,   KC_K, KC_M,          KC_COMMA,      KC_DOT,                  RSFT_T(KC_SLSH),
