05afc2f1bdf37235a258adf14b67dc90db60d0f6
Joe Scotto <me@joescotto.com>
2023-08-14 08:03:07 -0400

    Update keyboards/handwired/scottokeebs/scottoslant/keymaps/default/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/scottokeebs/scottoslant/keymaps/default/keymap.c
+++ keyboards/handwired/scottokeebs/scottoslant/keymaps/default/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_NO,   KC_NO,   KC_NO,   MO(3),   KC_NO,   KC_NO,   KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
     ),
-    [3] = LAYOUT_3x10_7(
+    [3] = LAYOUT(
         KC_NO,  KC_NO,  KC_NO,    KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,
         KC_F1,  KC_F2,  KC_F3,    KC_F4,   KC_F5,   KC_F6, KC_F7,   KC_F8,   KC_F9, KC_F10,
         KC_F11, KC_NO,  KC_NO,    QK_BOOT, KC_TRNS, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_F12,
