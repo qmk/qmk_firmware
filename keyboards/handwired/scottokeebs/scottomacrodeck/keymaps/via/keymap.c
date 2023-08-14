84bedeb9dd2f1ca98ebd21f309c3fa96a5ebb2c2
Joe Scotto <me@joescotto.com>
2023-08-14 08:01:12 -0400

    Update keyboards/handwired/scottokeebs/scottomacrodeck/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/scottokeebs/scottomacrodeck/keymaps/via/keymap.c
+++ keyboards/handwired/scottokeebs/scottomacrodeck/keymaps/via/keymap.c
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_3x4(
+    [0] = LAYOUT(
         KC_1, KC_2, KC_3,
         KC_Q, KC_W, KC_E, KC_R,
         KC_A, KC_S, KC_D, KC_F
