304196fd2142d6e5f5620c03e40d313a214bd765
Joe Scotto <me@joescotto.com>
2023-08-14 08:03:02 -0400

    Update keyboards/handwired/scottokeebs/scottoslant/keymaps/scotto/keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/handwired/scottokeebs/scottoslant/keymaps/scotto/keymap.c
+++ keyboards/handwired/scottokeebs/scottoslant/keymaps/scotto/keymap.c
@@ -63,7 +63,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_3x10_7(
+    [0] = LAYOUT(
         KC_Q,         KC_W,   KC_F,    KC_P,    KC_G,           KC_J, KC_L,          KC_U,          KC_Y,                      KC_BSPC,
         KC_A,         KC_R,   KC_S,    KC_T,    KC_D,           KC_H, KC_N,          KC_E,          KC_I,                      KC_O,
         LSFT_T(KC_Z), KC_X,   KC_C,    KC_V,    KC_B,           KC_K, KC_M,          KC_COMMA,      KC_DOT,                    RSFT_T(KC_SLSH),
