db9b78026070e5b2bcb1df1a6b16ba0e055acfec
Daniel Schaefer <dhs@frame.work>
2023-08-16 02:31:33 +0800

    keyboards/framework: Remove non-ANSI keyboards
    Keep the initial PR simple.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- keyboards/framework/laptop16/macropad/rules.mk
+++
@@ -1,3 +0,0 @@
-# TODO: Wait for PR #21343 to hit master
-RGB_MATRIX_ENABLE = no
-RGB_MATRIX_DRIVER = IS31FL3743A
--- keyboards/framework/laptop16/numpad/numpad.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2022-2023 Framework Computer
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-enum _layers {
-  _NUMLOCK,
-  _FN
-};
