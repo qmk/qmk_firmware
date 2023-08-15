62ed33063ec6491b8ea3b8dff5a433f54b9341aa
TreasureTypes <73436366+TreasureTypes@users.noreply.github.com>
2023-08-15 10:37:36 +0200

    Update keyboards/treasure/type9s3/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/treasure/type9s3/config.h
+++ keyboards/treasure/type9s3/config.h
@@ -3,22 +3,6 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
 #define RGB_MATRIX_LED_COUNT 9
 
 #define RGB_MATRIX_DEFAULT_SPD 40  // Sets the default animation speed, if none has been set
