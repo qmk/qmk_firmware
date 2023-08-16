c44adeddcbf2d6a580bad5bdb139a081c7dd0f2f
vladkvit <vladkvit@outlook.com>
2023-08-16 12:37:42 -0400

    Initial updates to get keyboard specs to be data driven.

--- keyboards/orthograph/config.h
+++ keyboards/orthograph/config.h
@@ -15,8 +15,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x564B
 #define PRODUCT_ID      0x0602
