978ad6394e19b26e5cbf5398d9dfe6fc933fa9ed
RephlexZero <rephlexzero@protonmail.com>
2023-08-16 12:39:13 +0100

    Fix bootmagic

--- keyboards/rephlex/2k/2k.c
+++ keyboards/rephlex/2k/2k.c
@@ -9,7 +9,7 @@ SPDX-License-Identifier: GPL-2.0-or-later */
 
 extern pin_t matrix_pins[MATRIX_ROWS][MATRIX_COLS];
 void         bootmagic_lite(void) {
-    if (analogReadPin(matrix_pins[BOOTMAGIC_LITE_ROW][BOOTMAGIC_LITE_COLUMN] < 1350)) {
+    if (analogReadPin(matrix_pins[BOOTMAGIC_LITE_ROW][BOOTMAGIC_LITE_COLUMN]) < 1350) {
         bootloader_jump();
     }
 }
@@ -18,11 +18,11 @@ void         bootmagic_lite(void) {
 static uint8_t i = 0;
 void           housekeeping_task_user(void) {
     if (i == 0) {
-        uprintf("Mode:%d Actuation Point %d Press/Release sensitivity:%d/%d\n", g_config.mode, g_config.actuation_point, g_config.press_hysteresis, g_config.release_hysteresis);
+        uprintf("Mode:%d Actuation Point %d Press/Release sensitivity:%d/%d\n", g_config.mode, g_config.actuation_point, g_config.press_sensitivity, g_config.release_sensitivity);
         for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
             uprintf("\n");
             for (uint8_t j = 0; j < MATRIX_COLS; j++) {
-                uprintf("%d/%d", keys[i][j].value, analogReadPin(matrix_pins[i][j]));
+                uprintf("%d/%d ", keys[i][j].value, analogReadPin(matrix_pins[i][j]));
             }
             uprintf("\n");
         }
