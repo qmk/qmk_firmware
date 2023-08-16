c22634aaf562eb08d911c641d59a59c6c1959770
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2023-08-16 14:52:54 +0800

    Update keyboards/inland/v83p/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/inland/v83p/config.h
+++ keyboards/inland/v83p/config.h
@@ -2,11 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
 #define ENCODER_DEFAULT_POS 0x3
 
 /* DIP switch */
