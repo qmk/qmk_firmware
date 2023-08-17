4121c415efa9b1cbcb378a494e71ea34994b86b4
Filip Sund <filip.sund@bouvet.no>
2023-08-17 13:54:14 +0200

    Add license

--- keyboards/pteron56/keymaps/brant/config.h
+++ keyboards/pteron56/keymaps/brant/config.h
@@ -1,3 +1,6 @@
+// Copyright (C) 2023 @kraken-jokes
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
