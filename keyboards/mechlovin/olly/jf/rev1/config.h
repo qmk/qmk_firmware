8daeb0a20a86dc027f30bd89aef282502ff413b5
mechlovin <57231893+mechlovin@users.noreply.github.com>
2023-08-17 16:41:54 +0700

    add olly jf rev2

--- keyboards/mechlovin/olly/jf/rev1/config.h
+++ keyboards/mechlovin/olly/jf/rev1/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Mechlovin'
+Copyright 2021 mechlovin
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,7 +17,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
-/* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 19
+#define MATRIX_ROW_PINS { D5, D6, A5, A4, A3, A6}
---
+++ keyboards/mechlovin/olly/jf/rev1/rules.mk
@@ -0,0 +1,7 @@
+# Processor frequency
+F_CPU = 16000000
+
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+CUSTOM_MATRIX = lite
+
+SRC += matrix.c
