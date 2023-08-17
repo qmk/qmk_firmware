0f15062750c1e1787603d05c41d355efcf23f1d5
Jake Stewart <67481201+RephlexZero@users.noreply.github.com>
2023-08-17 13:58:40 +0100

    Update keyboards/rephlex/2k/matrix.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/rephlex/2k/matrix.c
+++ keyboards/rephlex/2k/matrix.c
@@ -4,7 +4,6 @@ SPDX-License-Identifier: GPL-2.0-or-later */
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
-#include "config.h"
 #include "print.h"
 #include "quantum.h"
 #include "analog.h"
