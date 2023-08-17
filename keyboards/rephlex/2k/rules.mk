deec18237c306fae1101c2d1604d445fe14b7c6e
Jake Stewart <67481201+RephlexZero@users.noreply.github.com>
2023-08-17 13:58:30 +0100

    Update keyboards/rephlex/2k/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/rephlex/2k/rules.mk
+++ keyboards/rephlex/2k/rules.mk
@@ -1,8 +1,6 @@
 # Path: keyboards/rephlex/2k/rules.mk
-SRC += analog.c
-SRC += matrix.c
-SRC += lut.c
-SRC += scanfunctions.c
+QUANTUM_LIB_SRC += analog.c
+SRC += matrix.c lut.c scanfunctions.c
 
 CUSTOM_MATRIX = lite
 
