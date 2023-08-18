2dbdaa4dd1b5e812e5f21517307730d1d24c70f2
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2023-08-18 11:31:22 +0800

    Update config.h

--- keyboards/monsgeek/m3/config.h
+++ keyboards/monsgeek/m3/config.h
@@ -48,7 +48,3 @@
 #define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_KEYPRESSES
-#define RGB_MATRIX_KEYRELEASES
-
-
-
