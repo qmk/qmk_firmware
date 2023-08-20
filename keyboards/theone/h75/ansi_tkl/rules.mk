d5857131b0ecf3018e4b095fa0a0e87e0216f519
Afan Persama <46205716+Persama@users.noreply.github.com>
2023-08-20 16:12:11 +0800

    Update keyboards/theone/h75/ansi_tkl/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/theone/h75/ansi_tkl/rules.mk
+++ keyboards/theone/h75/ansi_tkl/rules.mk
@@ -1,16 +1,1 @@
-BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes        # Mouse keys
-EXTRAKEY_ENABLE = yes        # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
-COMMAND_ENABLE = yes         # Commands for debug and configuration
-NKRO_ENABLE = yes            # USB Nkey Rollover
-AUDIO_ENABLE = no            # Audio output
-KEY_LOCK_ENABLE = yes
-ENCODER_ENABLE = no
-
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE = yes
-
-WS2812_DRIVER = bitbang
-  
+# This file intentionally left blank
