6280822b0fc8092136f543bebd0236752fdebfd6
J Pratt <jp10010101010000@gmail.com>
2023-08-20 12:36:54 +1000

    Clean up (split features from space saving attempts)

--- keyboards/sofle/keymaps/cypher1/rules.mk
+++ keyboards/sofle/keymaps/cypher1/rules.mk
@@ -1,17 +1,21 @@
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-OLED_ENABLE = yes
+
+# Features
+OLED_ENABLE = yes			# Enable OLED for WPM, status and pet
 OLED_DRIVER = SSD1306
-ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-CONSOLE_ENABLE = no         # Console for debug
-RGBLIGHT_ENABLE = no		# Turn of lighting to save space
-WPM_ENABLE = yes
+ENCODER_ENABLE = yes		# Enable the two rotary encoders
+ENCODER_MAP_ENABLE = yes	# Use the new method for mapping encoders
+WPM_ENABLE = yes			# Used to make the pet walk or run based on typing
 COMBO_ENABLE = yes			# Special keyboard combos without many many layers
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes           # Enable N-Key Rollover (for combos and gaming)
+
+# Save space
+CONSOLE_ENABLE = no         # Console for debug
+RGBLIGHT_ENABLE = no		# Turn of lighting to save space
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Disable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-VIA_ENABLE = no
+VIA_ENABLE = no				# Don't need via, I am happy writing qmk
 LTO_ENABLE = yes            # Save space
