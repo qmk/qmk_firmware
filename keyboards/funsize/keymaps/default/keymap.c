d0b9aa767e80f691ac62d2829b2bee05c8686c3a
D0hnut5 <wmmillar@gmail.com>
2023-08-22 15:28:17 -0500

    modified:   ../../info.json 	modified:   keymap.c 	modified:   ../../rules.mk

--- keyboards/funsize/keymaps/default/keymap.c
+++ keyboards/funsize/keymaps/default/keymap.c
@@ -19,25 +19,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
 	  
-    [0] = LAYOUT_split_3x5_3(
+    [0] = LAYOUT(
         KC_Q,  		KC_W,    	KC_E, 	   	KC_R,    	KC_T,                                  KC_Y,	   KC_U,	   KC_I,       KC_O,       KC_P,
         KC_A,    	KC_S,    	KC_D,   	KC_F,    	KC_G,                                  KC_H,	   KC_J,       KC_K,       KC_L,       KC_SCLN,
         KC_Z,    	KC_X,    	KC_C,    	KC_V,    	KC_B,								   KC_N,       KC_M,       KC_COMM,	   KC_DOT,     KC_SLSH,
-											KC_LGUI, 	KC_BSPC, 	KC_SPC,        KC_SPC,	   KC_ENT,     KC_RALT
+											KC_LGUI, 	MO(1), 	    KC_SPC,        KC_MUTE,	   KC_BSPC,     KC_SPC
     ),
-	[1] = LAYOUT_split_3x5_3(
+	[1] = LAYOUT(
+		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+											KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_LGUI,    KC_TRNS,    KC_TRNS
+    ),
+    [2] = LAYOUT(
 		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 											KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS
     ),
-    [2] = LAYOUT_split_3x5_3(
-		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-											KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS
-    ),
-    [3] = LAYOUT_split_3x5_3(
+    [3] = LAYOUT(
      	KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
 		KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
@@ -47,9 +47,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),			ENCODER_CCW_CW(KC_VOLD, KC_VOLU),			ENCODER_CCW_CW(KC_VOLD, KC_VOLU),			ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN),	ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN),	ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN),	ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),			ENCODER_CCW_CW(KC_TRNS, KC_TRNS),			ENCODER_CCW_CW(KC_TRNS, KC_TRNS),			ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),			ENCODER_CCW_CW(KC_TRNS, KC_TRNS),			ENCODER_CCW_CW(KC_TRNS, KC_TRNS),			ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),           ENCODER_CCW_CW(KC_VOLD, KC_VOLU),           ENCODER_CCW_CW(KC_VOLD, KC_VOLU),           ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
+    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
 #endif
+
+/*
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 2) {
+        // Mouse Scrolling
+        if (clockwise) {
+            tap_code(KC_WH_U);
+        } else {
+            tap_code(KC_WH_D);
+        }
+    }
+    return true;
+}
+#endif
+*/
