5c35e7d24a46c7061069fd7eabeefac8d844eefe
bomb <1304866351@qq.com>
2023-08-22 12:00:06 +0800

    [Keyboard]Add z70ultra_v1 to MelGeekk!

--- keyboards/melgeek/z70ultra_v1/keymaps/via/keymap.c
+++ keyboards/melgeek/z70ultra_v1/keymaps/via/keymap.c
@@ -15,21 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-
-enum custom_keycodes {
-#ifdef VIA_ENABLE
-    KC_MISS_CTRL = QK_KB_0,
-#else
-    KC_MISS_CTRL = SAFE_RANGE,
-#endif
-    KC_LHPAD
-};
-
-#define KC_MCTL KC_MISSION_CONTROL
-#define KC_LPAD KC_LAUNCHPAD
-#define KC_TASK LGUI(KC_TAB)
-#define KC_FLXP LGUI(KC_E)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker( /* Base */
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
@@ -60,25 +45,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,					 _______, 						     _______, _______,          _______, _______, _______ 
 	)
 };
-
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_MISS_CTRL:
-            if (record->event.pressed) {
-                host_consumer_send(0x29F);
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
-        case KC_LHPAD:
-            if (record->event.pressed) {
-                host_consumer_send(0x2A0);
-            } else {
-                host_consumer_send(0);
-            }
-            return false;  // Skip all further processing of this key
-        default:
-            return true;  // Process all other keycodes normally
-    }
-}
