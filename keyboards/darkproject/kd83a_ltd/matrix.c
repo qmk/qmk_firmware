7fd6f42b3a0ef5b0c46f2e1fc38610c2674ecdf4
SHVD3x <103226441+SHVD3x@users.noreply.github.com>
2023-08-22 22:22:21 +0300

    Update matrix.c

--- keyboards/darkproject/kd83a_ltd/matrix.c
+++ keyboards/darkproject/kd83a_ltd/matrix.c
@@ -23,6 +23,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define rows_bits   PAL_PORT_BIT(PAL_PAD(B0)) | PAL_PORT_BIT(PAL_PAD(B1)) | PAL_PORT_BIT(PAL_PAD(B2)) | PAL_PORT_BIT(PAL_PAD(B3)) | PAL_PORT_BIT(PAL_PAD(B4)) | PAL_PORT_BIT(PAL_PAD(B5)) | PAL_PORT_BIT(PAL_PAD(B6)) | PAL_PORT_BIT(PAL_PAD(B7)) | PAL_PORT_BIT(PAL_PAD(B8)) | PAL_PORT_BIT(PAL_PAD(B9)) | PAL_PORT_BIT(PAL_PAD(B10)) | PAL_PORT_BIT(PAL_PAD(B12))
 #define cols_bits   PAL_PORT_BIT(PAL_PAD(A0)) | PAL_PORT_BIT(PAL_PAD(A1)) | PAL_PORT_BIT(PAL_PAD(A2)) | PAL_PORT_BIT(PAL_PAD(A3)) | PAL_PORT_BIT(PAL_PAD(A4)) | PAL_PORT_BIT(PAL_PAD(A8)) | PAL_PORT_BIT(PAL_PAD(A9)) | PAL_PORT_BIT(PAL_PAD(A10))
 
+#ifndef MATRIX_DEBOUNCE
+#    define MATRIX_DEBOUNCE 5
+#endif
+
 /* matrix state(1:on, 0:off) */
 extern matrix_row_t raw_matrix[MATRIX_ROWS];  // raw values
 extern matrix_row_t matrix[MATRIX_ROWS];      // debounced values
@@ -36,30 +40,19 @@ static inline matrix_row_t read_cols(void) {
 }
 
 static inline void unselect_rows(void) {
-    ATOMIC_BLOCK_FORCEON {
     palSetPort(PAL_PORT(B0), rows_bits);
-    }
 }
 
 static inline void select_row(uint8_t row) {
-    if (row != 9)
-        ATOMIC_BLOCK_FORCEON {
-        palClearPort(PAL_PORT(B0), PAL_PORT_BIT(row));
-        }
-    else // Pin B12 was removed from matrix definition. This row can be scanned during scan of B9 row
-        ATOMIC_BLOCK_FORCEON {
-        palClearPort(PAL_PORT(B0), PAL_PORT_BIT(PAL_PAD(B9)) | PAL_PORT_BIT(PAL_PAD(B12)));
-        }
+    pal_lld_clearport(PAL_PORT(B0), PAL_PORT_BIT(row) | ((row == 9) ? 0b1000000000000 : 0));
 }
 
 // Only need to init the pins. Debounce / raw matrix are initialized already for us.
 void matrix_init_custom(void) {
     // initialize key pins
-    ATOMIC_BLOCK_FORCEON {
         palSetGroupMode(PAL_PORT(B0), rows_bits, 0U, (PAL_WB32_MODE_OUTPUT | PAL_WB32_OTYPE_PUSHPULL | PAL_WB32_OSPEED_HIGH | PAL_WB32_CURRENT_LEVEL3));
         palSetPort(PAL_PORT(B0), rows_bits);
         palSetGroupMode(PAL_PORT(A0), cols_bits, 0U, (PAL_WB32_MODE_INPUT | PAL_WB32_PUPDR_PULLUP | PAL_WB32_OSPEED_HIGH));
-    }
 }
 
 // Only need to scan the result into current_matrix, and return changed.
@@ -78,8 +71,10 @@ uint8_t matrix_scan_custom(matrix_row_t current_matrix[]) {
         changed |= (current_matrix[current_row] != cols);
         current_matrix[current_row] = cols;
         
-        while ((palReadPort(PAL_PORT(A0)) & 0b11100011111) != 0b11100011111)     // Wait for all Col signals to go HIGH
-            wait_cpuclock(1);
+        uint8_t stable_threshold = MATRIX_DEBOUNCE;
+
+        while (stable_threshold > 0)     // Wait for all Col signals to go HIGH
+            stable_threshold = (((palReadPort(PAL_PORT(A0)) & 0b11100011111) ^ 0b11100011111) == 0) ? (stable_threshold - 1) : MATRIX_DEBOUNCE;
 
     }
 
