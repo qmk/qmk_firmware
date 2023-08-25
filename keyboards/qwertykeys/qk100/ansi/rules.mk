2ae0f2c9b67230866fcc209ede6c434fad2ce3a5
owlab-git <keyboardpro303@gmail.com>
2023-08-25 15:47:00 +0800

    qk100

--- keyboards/qwertykeys/qk100/ansi/rules.mk
+++ keyboards/qwertykeys/qk100/ansi/rules.mk
@@ -1,5 +1,2 @@
-MCU_LDSCRIPT = STM32F103x8_0x08006000_bootloader
-DFU_ARGS = -d 1688:2220 -a 0 -s 0x08006000 
-DFU_SUFFIX_ARGS = -v 1688 -p 2220
-BOOTLOADER_TYPE = stm32duino
-OPT_DEFS += -DBOOTLOADER_STM32DUINO
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
