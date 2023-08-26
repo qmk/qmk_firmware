7dfd148d37a9e75fe7bad3e2ebe2c4aa84360891
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-08-26 02:04:26 +0100

    linting error corrections

--- keyboards/evyd13/plain60/keymaps/default/keymap.c
+++ keyboards/evyd13/plain60/keymaps/default/keymap.c
@@ -1,3 +1,6 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later 
+
 #include QMK_KEYBOARD_H
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
