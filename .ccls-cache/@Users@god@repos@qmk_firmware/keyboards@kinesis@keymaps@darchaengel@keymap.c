1ae77cac352e1797d0b18643517f444ae15d2d37
Archaengel <god11341258@gmail.com>
2023-08-26 18:17:26 -0700

    Remove ccls cache files and update keymap

--- .ccls-cache/@Users@god@repos@qmk_firmware/keyboards@kinesis@keymaps@darchaengel@keymap.c
+++
@@ -1,1 +0,0 @@
-// This is intentionally left blank.
--- users/archaengel/archaengel.c
+++ users/archaengel/archaengel.c
@@ -35,17 +35,17 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEF] = LAYOUT_archaengel(
-                KC_Q,     KC_W,   KC_E,     KC_R,   KC_T,      KC_Y,   KC_U, KC_I,    KC_O,   KC_P,
-                KC_A,     KC_S,   KC_D,     KC_F,   KC_G,      KC_H,   KC_J, KC_K,    KC_L,   KC_QUOT,
-                KC_Z,     KC_X,   KC_C,     KC_V,   KC_B,      KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                                            LA_NAV, KC_LSFT,   KC_SPC, LA_SYM
+                KC_Q,     KC_W,   KC_E,     KC_R,    KC_T,      KC_Y,   KC_U, KC_I,    KC_O,   KC_P,
+                KC_A,     KC_S,   KC_D,     KC_F,    KC_G,      KC_H,   KC_J, KC_K,    KC_L,   KC_QUOT,
+                KC_Z,     KC_X,   KC_C,     KC_V,    KC_B,      KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH,
+                                            KC_LSFT, LA_NAV,    LA_SYM, KC_SPC 
             ),
 
     [EXT] = LAYOUT_archaengel(
-                KC_Q,     KC_W,   KC_F,     KC_P,   KC_B,      KC_J,   KC_L, KC_U,    KC_Y,   KC_QUOT,
-                KC_A,     KC_R,   KC_S,     KC_T,   KC_G,      KC_M,   KC_N, KC_E,    KC_I,   KC_O,
-                KC_Z,     KC_X,   KC_C,     KC_D,   KC_V,      KC_K,   KC_H, KC_COMM, KC_DOT, KC_SLSH,
-                                            LA_NAV, KC_LSFT,   KC_SPC, LA_SYM
+                KC_Q,     KC_W,   KC_F,     KC_P,    KC_B,      KC_J,   KC_L, KC_U,    KC_Y,   KC_QUOT,
+                KC_A,     KC_R,   KC_S,     KC_T,    KC_G,      KC_M,   KC_N, KC_E,    KC_I,   KC_O,
+                KC_Z,     KC_X,   KC_C,     KC_D,    KC_V,      KC_K,   KC_H, KC_COMM, KC_DOT, KC_SLSH,
+                                            KC_LSFT, LA_NAV,    LA_SYM, KC_SPC 
             ),
 
     [SYM] = LAYOUT_archaengel(
