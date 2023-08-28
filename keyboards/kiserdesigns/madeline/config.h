a96658bcc182b83b5a54d0b6965c951248966e04
NCKiser <noahkiser95mygc@gmail.com>
2023-08-28 14:23:18 -0500

    Added kiserdesigns keyboard folder and Madeline keyboard

---
+++ keyboards/kiserdesigns/madeline/config.h
@@ -0,0 +1,53 @@
+ /* Copyright TJ Campie
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+#pragma once
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 10
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { GP5, GP4, GP12, GP7 }
+#define MATRIX_COL_PINS { GP25, GP26, GP27, GP28, GP29, GP9, GP0, GP1, GP2, GP3 }
+
+#define ENCODERS_PAD_A { GP13 }
+#define ENCODERS_PAD_B { GP15 }
+#define ENCODER_RESOLUTIONS { 4 }
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
+#define RP2040_FLASH_GENERIC_03H
+
+#endif
