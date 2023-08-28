6a2b3c073b37fce68032cfbba5d56acfe625cd3e
NCKiser <noahkiser95mygc@gmail.com>
2023-08-28 15:25:00 -0500

    PR checklist: copyright, moved more to info.json, etc

--- keyboards/kiserdesigns/madeline/config.h
+++ keyboards/kiserdesigns/madeline/config.h
@@ -1,4 +1,6 @@
- /* This program is free software: you can redistribute it and/or modify
+ /* Copyright 2023 Noah Kiser (NCKiser)
+  *
+  * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation, either version 2 of the License, or
   * (at your option) any later version.
@@ -13,19 +15,7 @@
   */
 #pragma once
 
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 10
-
-#define MATRIX_ROW_PINS { GP5, GP4, GP12, GP7 }
-#define MATRIX_COL_PINS { GP25, GP26, GP27, GP28, GP29, GP9, GP0, GP1, GP2, GP3 }
-
-#define ENCODERS_PAD_A { GP13 }
-#define ENCODERS_PAD_B { GP15 }
-#define ENCODER_RESOLUTIONS { 4 }
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
--- keyboards/kiserdesigns/madeline/madeline.h
+++ keyboards/kiserdesigns/madeline/madeline.h
@@ -1,4 +1,6 @@
- /* This program is free software: you can redistribute it and/or modify
+ /* Copyright 2023 Noah Kiser (NCKiser)
+  *
+  * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
   * the Free Software Foundation, either version 2 of the License, or
   * (at your option) any later version.
@@ -14,18 +16,3 @@
 #pragma once
 
 #include "quantum.h"
-
-#define XXX KC_NO
-
-#define LAYOUT( \
-    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, \
-    K30, K31,      K33,           K36, K37,      K39  \
-) \
-{ \
-    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09  }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19  }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29  }, \
-    { K30, K31, XXX, K33, XXX, XXX, K36, K37, XXX, K39  }  \
-}
