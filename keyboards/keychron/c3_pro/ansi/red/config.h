be9868c6f8c4a72df4be5fe3aad1f1f91d0175c9
lalalademaxiya1 <2831039915@qq.com>
2023-08-29 11:04:00 +0800

    Added c3 pro

---
+++ keyboards/keychron/c3_pro/ansi/red/config.h
@@ -0,0 +1,39 @@
+/* Copyright 2023 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef LED_MATRIX_ENABLE
+/* RGB Matrix Driver Configuration */
+#    define DRIVER_COUNT 1
+#    define DRIVER_ADDR_1 0b1110100
+
+/* RGB Matrix Configuration */
+#    define LED_MATRIX_LED_COUNT (99) // Here are twelve leds for side lights
+
+/* Use first 7 channels of LED driver */
+#    define PHASE_CHANNEL MSKPHASE_7CHANNEL
+
+/* Set LED driver current */
+#    define CKLED2001_CURRENT_TUNE \
+        { 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50, 0x50 }
+
+/* turn off effects when suspended */
+#    define LED_DISABLE_WHEN_USB_SUSPENDED
+
+/* Enable Reactive Animation */
+#    define LED_MATRIX_KEYPRESSES
+#endif
---
+++ keyboards/keychron/c3_pro/readme.md
@@ -0,0 +1,21 @@
+# Keychron C3 Pro
+
+![Keychron C3 Pro](https://i.imgur.com/79sp3PH.jpg)
+
+A customizable 87% keyboard.
+
+* Keyboard Maintainer: [Keychron](https://github.com/keychron)
+* Hardware Supported: Keychron C3 Pro
+* Hardware Availability:[Keychron](https://github.com/keychron)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make keychron/c3_pro/ansi/red:default
+
+Flashing example for this keyboard:
+
+    make keychron/c3_pro/ansi/red:default:flash
+
+**Reset Key**: Hold down the key located at *K00*, commonly programmed as *Esc* while plugging in the keyboard.
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
