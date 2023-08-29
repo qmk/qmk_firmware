b8c22b8ee924fd8fc3a63633c12be32507564886
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-08-29 10:54:57 +0100

    m - s

--- keyboards/omkbd/runner3680/4x8/keymaps/default/keymap.c
+++ keyboards/omkbd/runner3680/4x8/keymaps/default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |  F9  |  F10 | Ctrl | GUI  | Alt  | EISU |  Del | Space|      | Enter| Bksp | EISU |  ESC |Adjust| Left | Down | Right|
      * `-------------------------------------------------------'      `-------------------------------------------------------'
      */
-  [_QWERTY] = LAYOUT(
+  [_QWERTY] = LAYOUT_ortho_4x16(
       KC_F3,   KC_F4,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       KC_F5,   KC_F6,   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,
       KC_F7,   KC_F8,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, KC_UP,   KC_RSFT,
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
      * `-------------------------------------------------------'      `-------------------------------------------------------'
      */
-  [_ADJUST] =  LAYOUT(
+  [_ADJUST] =  LAYOUT_ortho_4x16(
       _______, _______, _______, RGBRST,  QK_BOOT,   _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
