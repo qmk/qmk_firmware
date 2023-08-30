b993d4c3373256fe0020f18899618634f4aa2bff
Pablo Martínez <58857054+elpekenin@users.noreply.github.com>
2023-08-30 09:25:54 +0200

    Update version after merge

--- quantum/qmk_version.h
+++ quantum/qmk_version.h
@@ -4,10 +4,10 @@
 #pragma once
 
 #define QMK_MAJOR 0
-#define QMK_MINOR 21
-#define QMK_PATCH 7
+#define QMK_MINOR 22
+#define QMK_PATCH 1
 
 // number that can be used to order all versions, 0x prefix because otherwise leading zeros would get this number interpreted as an octal literal
 // reserved 2/3/3 digits for each part of the version, so that it fits on 4 bytes
 // example: "#if __QMK__ == 0x00020005" (for 0.20.5), note how it is '20' and not '14' (values in base 10)
-#define __QMK__ 0x00021007
+#define __QMK__ 0x00022001
