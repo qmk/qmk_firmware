b9e41718f90fd5476cc2a87dffdb7393c163f0c1
Ben Roe <ben@MacBookzPro-M2.lan>
2023-08-30 11:53:54 +0200

    Add Tri layer feature

--- keyboards/keycapsss/kimiko/rev2/keymaps/default/keymap.c
+++ keyboards/keycapsss/kimiko/rev2/keymaps/default/keymap.c
@@ -26,7 +26,6 @@ enum layers {
     _ADJUST,
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERTY
  * ,--------------------------------------------.                                   ,----------------------------------------------.
@@ -46,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
     KC_LSFT, KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LCTL, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN, KC_LBRC,  KC_RBRC, KC_RPRN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-                             KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,   KC_ENT,  RAISE,   KC_BSPC, KC_RGUI, KC_RALT
+                             KC_LCTL, KC_LGUI, KC_LALT, TL_LOWR, KC_SPC,   KC_ENT,  TL_UPPR, KC_BSPC, KC_RGUI, KC_RALT
 ),
 /* LOWER
 * QWERTY
