fc6e18353250d82928f6f0cca76bc7ea7a072401
Less/Rikki <86894501+lesshonor@users.noreply.github.com>
2023-08-29 23:55:26 -0400

    style: correct formatting
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/tominabox1/le_chiffre/post_config.h
+++ keyboards/tominabox1/le_chiffre/post_config.h
@@ -4,11 +4,11 @@
 #pragma once
 
 #if defined(OLED_ENABLE)
-    #if !defined(OLED_UPDATE_INTERVAL)
-    #define OLED_UPDATE_INTERVAL 5
-    #endif
+#    if !defined(OLED_UPDATE_INTERVAL)
+#        define OLED_UPDATE_INTERVAL 5
+#    endif
 
-    #if !defined(OLED_SECTION_BREAK)
-    #define OLED_SECTION_BREAK "-----"
-    #endif
+#    if !defined(OLED_SECTION_BREAK)
+#        define OLED_SECTION_BREAK "-----"
+#    endif
 #endif
