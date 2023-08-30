d620233c29b5b7b41eb6a5f3023b5b7c92533111
owlab-git <keyboardpro303@gmail.com>
2023-08-30 20:03:30 +0800

    format-c files

--- keyboards/qwertykeys/qk100/ansi/config.h
+++ keyboards/qwertykeys/qk100/ansi/config.h
@@ -29,7 +29,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_EFFECT_BREATHE_MAX    60
 
 /* RGB matrix */
-#ifdef RGB_MATRIX_ENABLE
 #define I2C1_CLOCK_SPEED 400000  
 #define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2  
 #    define RGB_MATRIX_DISABLE_KEYCODES 
@@ -42,4 +41,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define DRIVER_ADDR_1                 0b0110000
 #    define DRIVER_COUNT                  1
 #    define RGB_MATRIX_LED_COUNT          101 
-#endif
--- keyboards/qwertykeys/qk100/solder/solder.c
+++ keyboards/qwertykeys/qk100/solder/solder.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright 2023 Qwertykeys
 
 This program is free software: you can redistribute it and/or modify
@@ -16,28 +16,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "quantum.h"
 
-const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 3, HSV_WHITE}      
-);
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 3, HSV_WHITE});
 
+const rgblight_segment_t PROGMEM my_numlock_layer[] = RGBLIGHT_LAYER_SEGMENTS({3, 3, HSV_WHITE});
 
-const rgblight_segment_t PROGMEM my_numlock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {3, 3, HSV_WHITE}      
-);
-
-
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_capslock_layer,
-    my_numlock_layer
-);
-
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(my_capslock_layer, my_numlock_layer);
 
 void keyboard_post_init_kb(void) {
     rgblight_layers = my_rgb_layers;
     keyboard_post_init_user();
 }
 
-bool led_update_kb(led_t led_state) {  
+bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res) {
         rgblight_set_layer_state(0, led_state.caps_lock);
