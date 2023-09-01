25eb800c53ac49322f2912bad662a133f030ad62
Cloud Chagnon <me@cloudchagnon.xyz>
2023-09-01 10:47:12 -0600

    Removed unneeded tri layer custom keycodes
    Now using the built-in functionality!

--- keyboards/handwired/split_cloud/keymaps/default/keymap.c
+++ keyboards/handwired/split_cloud/keymaps/default/keymap.c
@@ -34,8 +34,6 @@ enum custom_keycodes {
     COLEMAK,
     DVORAK,
     PLOVER,
-    LOWER,
-    RAISE,
     EXT_PLV
 };
 
@@ -58,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_ortho_5x12(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_DEL ,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_RCTL, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL ,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_RCTL, TL_LOWR, KC_SPC, KC_SPC, TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
     /* Colemak
@@ -79,11 +77,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_COLEMAK] = LAYOUT_ortho_5x12(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,  KC_G,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_DEL ,
-        KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,  KC_D,   KC_H,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_K,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_RCTL, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL ,
+        KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_RCTL, TL_LOWR, KC_SPC, KC_SPC, TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
     /* Dvorak
@@ -100,11 +98,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_DVORAK] = LAYOUT_ortho_5x12(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,    KC_0,  KC_BSPC,
-        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,  KC_Y,  KC_F,    KC_G,  KC_C,    KC_R,    KC_L,  KC_DEL,
-        KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,  KC_I,  KC_D,    KC_H,  KC_T,    KC_N,    KC_S,  KC_SLSH,
-        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,  KC_B,    KC_M,  KC_W,    KC_V,    KC_Z,  KC_ENT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_RCTL, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,  KC_F,    KC_G,    KC_C,    KC_R,    KC_L,  KC_DEL,
+        KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,  KC_D,    KC_H,    KC_T,    KC_N,    KC_S,  KC_SLSH,
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,  KC_ENT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_RCTL, TL_LOWR, KC_SPC, KC_SPC, TL_UPPR, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
     ),
 
     /* Lower
@@ -211,24 +209,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case DVORAK:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_DVORAK);
-            }
-            return false;
-        case LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
             }
             return false;
         case PLOVER:
