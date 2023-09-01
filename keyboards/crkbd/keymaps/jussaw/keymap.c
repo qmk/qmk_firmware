5f3d597438e74e81b4b29b11f5cec379bd7f31bb
Justin Sawyer <jtsaw36@gmail.com>
2023-09-01 16:08:47 -0600

    Update format

--- keyboards/crkbd/keymaps/jussaw/keymap.c
+++ keyboards/crkbd/keymaps/jussaw/keymap.c
@@ -35,11 +35,17 @@ enum custom_keycodes {
     UPPER,
     ADJUST,
     GAME,
+    DF_QWRT,
+    DF_CMDH,
+    TG_GAME,
 };
 
 #define LOWER MO(_LOWER)
 #define UPPER MO(_UPPER)
 #define ADJUST MO(_ADJUST)
+#define DF_QWRT DF(_QWERTY)
+#define DF_CMDH DF(_COLEMAKDH)
+#define TG_GAME TG(_GAME)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -129,9 +135,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                            `--------------------------'  `--------------------------'
    */
   [_ADJUST] = LAYOUT_split_3x6_3(
-      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,TG(_GAME),        KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX,
-      DF(_QWERTY),XXXXXXX,XXXXXXX, KC_BTN2, KC_BTN1, XXXXXXX,       KC_VOLU, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
-      DF(_COLEMAKDH), XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN5, XXXXXXX,  KC_VOLD, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG_GAME,    KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX,
+      DF_QWRT, XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN1, XXXXXXX,    KC_VOLU, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
+      DF_QWRT, XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN5, XXXXXXX,    KC_VOLD, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
                                  XXXXXXX, KC_TRNS, XXXXXXX,    XXXXXXX, KC_TRNS, XXXXXXX
   ),
 
@@ -148,9 +154,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                            `--------------------------'  `--------------------------'
    */
   [_GAME] = LAYOUT_split_3x6_3(
-         KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+         KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
          KC_G,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
          KC_B,  KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                                  KC_ESC, KC_LALT, KC_SPC,     XXXXXXX, XXXXXXX,    TG(_GAME)
+                                  KC_ESC, KC_LALT,  KC_SPC,    XXXXXXX, XXXXXXX, TG_GAME
   ),
 };
