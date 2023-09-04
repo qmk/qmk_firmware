5ea6d2656ff37d48080a52fa017f4217aae3a5b3
Ryan <fauxpark@gmail.com>
2023-09-04 12:14:14 +1000

    Add `mousekey.h` include to `quantum.h` (#21897)

--- quantum/quantum.h
+++ quantum/quantum.h
@@ -209,6 +209,10 @@ extern layer_state_t layer_state;
 #    include "pointing_device.h"
 #endif
 
+#ifdef MOUSEKEY_ENABLE
+#    include "mousekey.h"
+#endif
+
 #ifdef CAPS_WORD_ENABLE
 #    include "caps_word.h"
 #    include "process_caps_word.h"
