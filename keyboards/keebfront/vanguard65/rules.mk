db3b81d5f311b38623f77ab93bda96cbe6847370
Felix Jen <fjen@felixjen.com>
2023-09-05 23:27:19 -0500

    Update keyboards/keebfront/vanguard65/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keebfront/vanguard65/rules.mk
+++ keyboards/keebfront/vanguard65/rules.mk
@@ -1,7 +1,2 @@
 # Processor frequency
 F_CPU = 8000000
-# Build Options
-#   change yes to no to disable
-#LTO_ENABLE = yes
-#RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-#MIDI_ENABLE = yes          # Enable MIDI
