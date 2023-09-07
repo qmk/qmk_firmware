fe0e0b6efa1da08c5a99da2e8b9293a9d8aaae11
ptillemans <pti@snamellit.com>
2023-09-07 13:29:38 +0200

    routine maintenance on ptillemans keymaps
    - add capslock to ok60
    - add f11 and f12 and fix rgb control on redox

--- keyboards/redox/keymaps/ptillemans/config.h
+++ keyboards/redox/keymaps/ptillemans/config.h
@@ -38,3 +38,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true
+#define RGB_DISABLE_TIMEOUT 900000 // 15 minutes
