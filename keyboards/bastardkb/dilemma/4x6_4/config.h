73086b72e247ddff1a3a2e41dd1beac4ad09ac4b
casuanoob <gms.rulz@hotmail.com>
2023-09-09 16:38:32 +1000

    4x6_4 Change RGB matrix centre

--- keyboards/bastardkb/dilemma/4x6_4/config.h
+++ keyboards/bastardkb/dilemma/4x6_4/config.h
@@ -49,6 +49,7 @@
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 
 // Startup values.
+#    define RGB_MATRIX_CENTER { 112, 28 }
 #    define RGB_MATRIX_DEFAULT_HUE 0
 #    define RGB_MATRIX_DEFAULT_SAT 255
 #    define RGB_MATRIX_DEFAULT_VAL 64
