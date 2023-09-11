1a36b7fd3822eeb6caacf32ae37f3381a18b5a45
Gosku <gosku26@gmail.com>
2023-09-11 21:27:01 +1000

    Replace NumBlq numbers by normal number keys
    Replace the keycodes to type numbers in the keymap to use the normal number keycodes instead of the numblq ones since there are some instances (during boot) where the NumBlq is disabled
    
    The only side effect of this in practice is that those keys now type a symbol when in combination with
    shift

--- keyboards/keebio/iris/keymaps/gosku/keymap.c
+++ keyboards/keebio/iris/keymaps/gosku/keymap.c
@@ -55,13 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC,  KC_P7,   KC_P8,   KC_P9,  _______, KC_TRNS,
+     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC,   KC_7,    KC_8,    KC_9,  _______, KC_TRNS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_GRV, KC_AMPR, KC_PAST, KC_PMNS, KC_UNDS, KC_GRV,                             _______, KC_P4,   KC_P5,   KC_P6,   KC_PLUS,  KC_PMNS,
+      KC_GRV, KC_AMPR, KC_PAST, KC_PMNS, KC_UNDS, KC_GRV,                             _______,  KC_4,    KC_5,    KC_6,   KC_PLUS,  KC_PMNS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_LT,   KC_GT,   KC_PLUS, KC_EQL,  KC_DQUO,                             KC_RBRC,  KC_P1,   KC_P2,  KC_P3,  KC_PAST, KC_SLSH,
+     _______, KC_LT,   KC_GT,   KC_PLUS, KC_EQL,  KC_DQUO,                             KC_RBRC,  KC_1,    KC_2,    KC_3,  KC_PAST, KC_SLSH,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_PDOT, KC_TRNS,  KC_P0,   KC_TRNS, KC_MPRV, KC_MNXT,
+     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_PDOT, KC_TRNS,  KC_0,    KC_TRNS, KC_MPRV, KC_MNXT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,  KC_TRNS, KC_TRNS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
