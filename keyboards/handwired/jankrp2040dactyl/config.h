1af4b340bb198eccb4c8b59090c1392f4467de22
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2023-09-11 08:29:34 +0300

    First Version

---
+++ keyboards/handwired/jankrp2040dactyl/config.h
@@ -0,0 +1,6 @@
+// Copyright 2023 Ethan Perry (@wheres-perry)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+#define GRAVE_ESC_SHIFT_OVERRIDE //
---
+++ keyboards/handwired/jankrp2040dactyl/rules.mk
@@ -0,0 +1,2 @@
+SERIAL_DRIVER = vendor
+BOOTMAGIC_ENABLE = yes
