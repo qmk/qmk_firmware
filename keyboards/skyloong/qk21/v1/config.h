3aae13ce18c8c4dc286afaa4b12f89393bb2a37f
skyloong <92424712+JZ-Skyloong@users.noreply.github.com>
2023-09-11 10:32:12 +0800

    Update keyboards/skyloong/qk21/v1/config.h
    OK,Thanks.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/skyloong/qk21/v1/config.h
+++ keyboards/skyloong/qk21/v1/config.h
@@ -6,12 +6,7 @@
 /* Enable num-lock LED */
 #define NUM_LOCK_INDEX 13
 
-#ifdef RGB_MATRIX_ENABLE
 #define RGB_MATRIX_LED_COUNT 21
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS  //Enable frame effects
 #define RGB_MATRIX_KEYPRESSES  //Reacts to keypress
 #define RGB_DISABLE_WHEN_USB_SUSPENDED   //turn off effects when suspended
-#endif
-
-
-
