90a2933c2c026f9718c2025eee9907fe15199527
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-09-11 21:33:37 +0100

    further focus

--- layouts/community/60_iso/unxmaal/config.h
+++ layouts/community/60_iso/unxmaal/config.h
@@ -17,7 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef KEYBOARD_gh60
+#ifdef KEYBOARD_gh60_revc
 #    define WS2812_DI_PIN F4
 #    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
