4b6be27127c508fa1c86d95f4388c8dd138eaa43
TW59420 <40019210+TW59420@users.noreply.github.com>
2023-09-12 11:53:07 +0800

    Update keyboards/mwstudio/cover65/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/mwstudio/cover65/keymaps/via/keymap.c
+++ keyboards/mwstudio/cover65/keymaps/via/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPACE,                            KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
-	[1] = LAYOUT(
+    [1] = LAYOUT(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, RGB_HUI,
         RGB_TOG, _______, _______, _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUD,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, RGB_SAI,
