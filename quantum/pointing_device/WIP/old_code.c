e423fbf96c30a14f49888cfa567b5f486e69ab73
Alabastard-64 <snipac+github@gmail.com>
2023-09-14 13:31:33 -0600

    final cleanup

--- quantum/pointing_device/WIP/old_code.c
+++
@@ -1,5 +0,0 @@
-
-    if (pointing_mode.id >= POINTING_MODE_MAP_START && pointing_mode.id <= (POINTING_MODE_MAP_START + POINTING_MODE_MAP_COUNT - 1)) {
-        pointing_tap_codes(POINTING_MODE_MAP(pointing_mode.id - POINTING_MODE_MAP_START));
-        return mouse_report;
-    }
