43143dd1af381276b562f196018362b703c3b2e3
Andrew Kannan <andrew.kannan@gmail.com>
2023-09-14 13:43:46 -0400

    Remove safe range as they are unnecessary

--- keyboards/cannonkeys/is0gr/keymaps/via/keymap.c
+++ keyboards/cannonkeys/is0gr/keymaps/via/keymap.c
@@ -16,11 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_ENT
