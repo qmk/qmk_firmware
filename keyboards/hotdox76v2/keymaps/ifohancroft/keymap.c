72083429e82ac9dd4bab5a50b0104f3e4d45fe2c
IFo Hancroft <contact@ifohancroft.com>
2023-09-16 00:40:14 +0300

    Moved PgUp and PgDown to Default Layer

--- keyboards/hotdox76v2/keymaps/ifohancroft/keymap.c
+++ keyboards/hotdox76v2/keymaps/ifohancroft/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |---------+---------+---------+---------+---------+---------+---------|                             |---------+---------+---------+---------+---------+---------+---------|
     * | Tab     | Q       | W       | E       | R       | T       | [       |                             | ]       | Y       | U       | I       | O       | P       | \       |
     * |---------+---------+---------+---------+---------+---------+---------|                             |---------+---------+---------+---------+---------+---------+---------|
-    * | Esc     | A       | S       | D       | F       | G       |         |                             |         | H       | J       | K       | L       | ;       | '       |
+    * | Esc     | A       | S       | D       | F       | G       | PageUp  |                             | PageDn  | H       | J       | K       | L       | ;       | '       |
     * |---------+---------+---------+---------+---------+---------+---------*                             *---------+---------+---------+---------+---------+---------+---------|
     * | Shift   | Z       | X       | C       | V       | B       |                                                 | N       | M       | ,       | .       | /       | Shift   |
     * |---------+---------+---------+---------+---------+---------*                                                 *---------+---------+---------+---------+---------+---------|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_MINS,                                KC_EQL,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRC,                                KC_RBRC,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
         KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                       KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_NO,                                  KC_NO,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_PGUP,                                KC_PGDN,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
         KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    OSL(1),                                                                         TT(1),    KC_NO,    KC_RALT,  KC_RGUI,  KC_RCTL,
 
                                                                     KC_NO,    KC_NO,              KC_NO,    KC_NO,
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *                                                   *---------+---------+---------|         |---------+---------+---------*
     *                                                   |         |         |         |         |         |         |         |
     *                                                   |         | Enter   |---------|         |---------|         |         |
-    *                                                   | PgUp    |         |         |         |         |         | PgDn    |
+    *                                                   |         |         |         |         |         |         |         |
     *                                                   *---------+---------+---------*         *---------+---------+---------*
     */
 
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                                     _______,  _______,            _______,  _______,
                                                                               _______,            _______,
-                                                          KC_PGUP,  KC_PENT,  _______,            _______,  _______,  KC_PGDN   
+                                                          _______,  KC_PENT,  _______,            _______,  _______,  _______   
     ),
 
     /* Empty Layer */
