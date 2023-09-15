a146cfedbab8d8c5815c88ef88f2fd834f72df3e
numToStr <vikasraj1911@gmail.com>
2023-09-15 13:12:32 +0530

    keymap update

--- keyboards/keebit/coral/keymaps/numToStr/keymap.c
+++ keyboards/keebit/coral/keymaps/numToStr/keymap.c
@@ -23,8 +23,11 @@
 #define L_ROT TD(TD_MNXT_MPREV)
 #define R_ROT TD(TD_MPLY_MUTE)
 
+// Custom Keys
+#define KK_COPY LCTL(KC_C)
+#define KK_PASTE RCTL(KC_V)
 
-// ###### TAP DANCE ######
+// ###### Tap-Dance ######
 
 enum TapDance {
     TD_MNXT_MPREV,
@@ -39,7 +42,7 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_MPLY_MUTE] = ACTION_TAP_DANCE_DOUBLE(KC_MPLY, KC_MUTE),
 };
 
-// ###### LAYERS ######
+// ###### Layers ######
 
 enum Layers {
     BaseLayer,
@@ -48,12 +51,7 @@ enum Layers {
 };
 
 enum CustomKeycodes {
-    KC_LOREM = SAFE_RANGE,
-    KC_PRVWD,
-    KC_NXTWD,
-    KC_LSTRT,
-    KC_LEND,
-    KC_DLINE
+    KK_LOREM = SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -62,52 +60,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,   KC_T,           /**/        KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,
         KC_GRV,  GUI_A, ALT_S,   SFT_D,   CTL_F,  KC_G,           /**/        KC_H,    CTL_J,   SFT_K,    ALT_L,   GUI_SCLN, KC_QUOTE,
         KC_BSLS, KC_Z,  KC_X,    KC_C,    KC_V,   KC_B,           /**/        KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLASH, KC_RBRC,
-                        _______, KC_PSCR, KC_ESC, L_THUMB, L_ROT, /**/ R_ROT, R_THUMB, KC_BSPC, KC_DEL,   _______
+                        KK_COPY, KC_PSCR, KC_ESC, L_THUMB, L_ROT, /**/ R_ROT, R_THUMB, KC_BSPC, KC_DEL,   KK_PASTE
     ),
     [NavLayer] = LAYOUT(
         _______, _______, _______, _______, _______, _______,          /**/          KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD, KC_VOLU,
-        _______, _______, _______, _______, _______, _______,          /**/          KC_BRID, KC_BRIU, _______, _______,  _______, _______,
+        _______, _______, _______, _______, _______, _______,          /**/          KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_BRID, KC_BRIU,
         _______, _______, _______, _______, _______, _______,          /**/          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______,  _______, SS_LOREM,
+        _______, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______,  _______, KK_LOREM,
                           _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______
     ),
     [FnLayer] = LAYOUT(
-        KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            /**/          _______, _______, _______, _______, _______, _______,
-        KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           /**/          _______, _______, _______, _______, _______, _______,
-        _______,  _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
-        SS_LOREM, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
-                           _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            /**/          _______, _______, _______, _______, _______, _______,
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           /**/          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
+                          _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______
     )
 };
 
-// #if defined(ENCODER_MAP_ENABLE)
-// const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-//     [BaseLayer] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-// };
-// #endif
-
-// ###### OVERRIDES ######
-
-// Shift Left is Home
-const key_override_t left_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LEFT, KC_HOME);
-// Shift Right is End
-const key_override_t right_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RIGHT, KC_END);
-// Shift Up is PageUp
-const key_override_t up_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_UP, KC_PGUP);
-// Shift Down is PageDown
-const key_override_t down_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOWN, KC_PGDN);
-
-// This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &left_key_override,
-    &right_key_override,
-    &up_key_override,
-    &down_key_override,
-    NULL // Null terminate the array of overrides!
-};
-
-
-// ###### COMBOS ######
+// ###### Combos ######
 
 enum combos {
     CAPS_COMBO,
@@ -121,4 +92,32 @@ const uint16_t PROGMEM caps_combo[] = {L_THUMB, R_THUMB, COMBO_END};
 
 combo_t key_combos[] = {
     [CAPS_COMBO] = COMBO(caps_combo, QK_CAPS_WORD_TOGGLE),
+};
+
+// ###### Encoders #######
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [BaseLayer] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [NavLayer] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+    [FnLayer] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+};
+#endif
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KK_LOREM:
+            if (record->event.pressed) {
+                SEND_STRING("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae nibh neque. Etiam sed enim ac ipsum maximus rhoncus id et dui. Nam mi orci, rutrum in ipsum eget, hendrerit aliquam nulla. Maecenas ac nisl nibh. Integer erat mi, molestie vitae aliquet ut, faucibus eget sem. Suspendisse vel lacus interdum, consectetur nisi vitae, vulputate purus. Etiam ut aliquam turpis, id pellentesque ligula. Curabitur semper nisi in facilisis vehicula. Pellentesque felis risus, maximus blandit erat vel, congue sagittis mi. Etiam aliquam arcu sed neque semper laoreet. Sed id posuere libero, sed volutpat sapien.");
+            }
+            return false;
+        // case QK_CAPS_WORD_TOGGLE:
+        //     if (record->event.pressed) {
+        //         rgblight_setrgb_at(RGB_WHITE, 0);
+        //     }
+        //     return true;
+        default:
+            return true;
+    }
 };
