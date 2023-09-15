4e084d8d2348c04b45d1570e280335d60cb582c2
IFo Hancroft <contact@ifohancroft.com>
2023-09-16 00:26:35 +0300

    Unmapped NK_TOGG
    Unmapped NK_TOGG since I have enabled Command anyway, so no need to use a key for NK_TOGG.

--- keyboards/hotdox76v2/keymaps/ifohancroft/keymap.c
+++ keyboards/hotdox76v2/keymaps/ifohancroft/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NUM,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F11,                                 KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   _______,
         _______,  KC_PPLS,  KC_P7,    KC_P8,    KC_P9,    KC_PAST,  _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  KC_PMNS,  KC_P4,    KC_P5,    KC_P6,    KC_PSLS,                                                    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,
-        _______,  KC_P0,    KC_P1,    KC_P2,    KC_P3,    KC_PDOT,  _______,                                _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,
+        _______,  KC_P0,    KC_P1,    KC_P2,    KC_P3,    KC_PDOT,  _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,                                                                        _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,
 
                                                                     _______,  _______,            _______,  _______,
