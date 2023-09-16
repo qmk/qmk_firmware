a5adb5546858fec949a18763a3452bfbeb9f66ee
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-09-16 23:45:33 +0100

    @noroadsleft corrections 23SEP16

--- keyboards/free_willy/keymaps/colemak/keymap.c
+++ keyboards/free_willy/keymaps/colemak/keymap.c
@@ -32,21 +32,21 @@ enum combo_events {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_split_3x5_3(
+  [_BASE] = LAYOUT(
     KC_Q,   KC_W,   KC_F,    KC_P,       KC_B,    KC_J,     KC_L,       KC_U,    KC_Y,    KC_BSPC,
     KC_A,   KC_R,   KC_S,    KC_T,       KC_G,    KC_M,     KC_N,       KC_E,    KC_I,    KC_O,
     KC_X,   KC_C,   KC_D,    KC_V,       KC_Z,    KC_K,     KC_H,       KC_COMM, KC_DOT,  KC_SLSH,
                     KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI , KC_SYM_SPC, MO(_NAV)
   ),
 
-  [_SYM] = LAYOUT_split_3x5_3(
+  [_SYM] = LAYOUT(
     KC_1,       KC_2,     KC_3,      KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
     KC_EXLM,    KC_AT,    KC_HASH,   KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_EQUAL, KC_MINS,
     KC_BSLS,    KC_SCLN,   KC_LCBR,   KC_LBRC,  KC_LPRN,  KC_RPRN,  KC_RBRC,  KC_RCBR,  KC_QUOT,  KC_GRV,
                           KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-  [_NAV] = LAYOUT_split_3x5_3(
+  [_NAV] = LAYOUT(
     QK_BOOT,  KC_NO,    KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,
     KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,
     KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_NO,
