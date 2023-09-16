ca0039a6dc295e7f2e73c98f8bb137afd672ef78
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-09-16 08:44:35 +0100

    revert cxt_studio

--- keyboards/cxt_studio/keymaps/via/keymap.c
+++ keyboards/cxt_studio/keymaps/via/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_ortho_4x4(
+	[0] = LAYOUT(
         KC_ESC, KC_F11, KC_NO, KC_MSTP,
         KC_NO, KC_NO, KC_MRWD, KC_MFFD,
         KC_NO, KC_MPLY, KC_MPLY, KC_MNXT,
