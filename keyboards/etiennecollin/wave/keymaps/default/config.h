dd9a4c1fff75ffaa9d9af3299728a98ad05eb209
Etienne Collin <collin.etienne.contact@gmail.com>
2023-09-16 23:34:50 -0400

    Created the default keymap

---
+++ keyboards/etiennecollin/wave/keymaps/default/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Activate caps word by pressing Left Shift + Right Shift
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat
+#define QUICK_TAP_TERM 0
+
+// Maximum time between taps of tap dances
+#define TAPPING_TERM 175
+
+// Perform hold action if pressing a dual-role key, tapping another key and
+// releasing the dual-role key withing tapping term
+#define PERMISSIVE_HOLD
+
+// Perform hold action if pressing a dual-role key, pressing another key,
+// releasing the dual-role key and releasing the other key withing tapping term
+#define HOLD_ON_OTHER_KEY_PRESS
