3ad98dd5f7c208106b27dc464952ce0ee9efd1b5
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2023-09-17 12:10:47 -0600

    Update keyboards/handwired/jankrp2040dactyl/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/handwired/jankrp2040dactyl/config.h
+++ keyboards/handwired/jankrp2040dactyl/config.h
@@ -1,19 +1,5 @@
-/* Copyright 2023 Ethan Perry (@wheres-perry)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2023 Ethan Perry (@wheres-perry)
+// SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
-#define GRAVE_ESC_SHIFT_OVERRIDE //
+
+#define GRAVE_ESC_SHIFT_OVERRIDE
