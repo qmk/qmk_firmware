7052b2237aac0b438c8a42acca07e5d96752370e
jussaw <jtsaw36@gmail.com>
2023-09-18 02:32:56 -0600

    Update mod tap options

--- keyboards/crkbd/keymaps/jussaw/config.h
+++ keyboards/crkbd/keymaps/jussaw/config.h
@@ -29,7 +29,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Mod Tap */
 #define TAP_CODE_DELAY 50
+#define TAPPING_TERM 200
 #define QUICK_TAP_TERM 0
+#define PERMISSIVE_HOLD_PER_KEY
+#define TAPPING_FORCE_HOLD
 
 /* Mouse key speed and acceleration. */
 #undef MOUSEKEY_DELAY
