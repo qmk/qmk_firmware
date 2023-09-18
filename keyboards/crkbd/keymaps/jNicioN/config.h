1b5b6d58a2ff018c380ca52893cb53d800409812
Jonathan Nicio <jonathan.nicio@banregio.com>
2023-09-18 12:31:29 -0600

    feat: Elimina definiciones pasadas y corrige código comentado

--- keyboards/crkbd/keymaps/jNicioN/config.h
+++ keyboards/crkbd/keymaps/jNicioN/config.h
@@ -20,22 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
-//#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-
-//#define TAPPING_FORCE_HOLD
-//#define TAPPING_TERM 100
-
-
 #define EE_HANDS
 #define SPLIT_USB_DETECT
 
-#define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 
 #if defined(TAPPING_TERM)
@@ -56,34 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define OLED_KEY_TIMEOUT 30000
 //#   define KEY_LOG_ENABLE
 #endif
-
-//#ifdef OLED_DRIVER_ENABLE
-//  #define OLED_DISPLAY_128X64
-//  #define OLED_TIMEOUT 120000
-//#endif
-
-//#ifdef RGBLIGHT_ENABLE
-//    #define RGBLIGHT_EFFECT_BREATHING
-//    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//    #define RGBLIGHT_EFFECT_SNAKE
-//    #define RGBLIGHT_EFFECT_KNIGHT
-//    #define RGBLIGHT_EFFECT_CHRISTMAS
-//    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//    #define RGBLIGHT_EFFECT_RGB_TEST
-//    #define RGBLIGHT_EFFECT_ALTERNATING
-//    #define RGBLIGHT_EFFECT_TWINKLE
-//    #define RGBLIGHT_LIMIT_VAL 120
-//    #define RGBLIGHT_HUE_STEP 10
-//    #define RGBLIGHT_SAT_STEP 17
-//    #define RGBLIGHT_VAL_STEP 17
-//    #define RGBLIGHT_SLEEP
-//    
-//#endif
-
-//#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
-
-//#define TAPPING_TERM 200
 
 #if defined(RGB_MATRIX_ENABLE)
 #    define RGB_MATRIX_KEYPRESSES               // reacts to keypresses
