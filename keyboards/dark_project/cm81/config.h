7ec650c589350243e635fc5bd6b0f4d01bc556b8
jonylee1986 <2532487686@qq.com>
2023-09-18 12:36:40 +0800

    [keyboard]add dark_project/cm81 keyboard.

---
+++ keyboards/dark_project/cm81/config.h
@@ -0,0 +1,41 @@
+// Copyright 2023 jonylee@hfd (@jonylee1897)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RGB_TRIGGER_ON_KEYDOWN
+/* Force NKRO on boot up regardless of the setting saved in the EEPROM (uncomment to enable it) */
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* SPI Config for spi flash*/
+#define SPI_DRIVER SPIDQ
+#define SPI_SCK_PIN B3
+#define SPI_MOSI_PIN B5
+#define SPI_MISO_PIN B4
+#define SPI_MOSI_PAL_MODE 5
+
+#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
+#define WEAR_LEVELING_BACKING_SIZE (8 * 1024)
+
+/* I2C Config for LED Driver */
+#define DRIVER_COUNT 2
+#define DRIVER_ADDR_1 0b1110100
+#define DRIVER_ADDR_2 0b1110111
+#define I2C1_SDA_PIN B7
+#define I2C1_SCL_PIN B6
+#define I2C1_SCL_PAL_MODE 4
+#define I2C1_OPMODE OPMODE_I2C
+#define I2C1_CLOCK_SPEED 400000 /* 400000 */
+
+#define DRIVER_1_LED_TOTAL 60
+#define DRIVER_2_LED_TOTAL 21
+#define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL + 10)
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
---
+++ keyboards/dark_project/cm81/rules.mk
@@ -0,0 +1,7 @@
+# Build Options
+#   change yes to no to disable
+#
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = spi_flash
+#RGB_MATRIX_CUSTOM_KB = yes #Add turnoff LED
+
