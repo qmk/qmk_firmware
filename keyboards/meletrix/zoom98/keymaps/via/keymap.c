75d8577afc8ab3df15e0c055d23e0b06d1974cc1
spbgzh <72414103+spbgzh@users.noreply.github.com>
2023-09-19 10:26:01 +0300

    Update keyboards/meletrix/zoom98/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/meletrix/zoom98/keymaps/via/keymap.c
+++ keyboards/meletrix/zoom98/keymaps/via/keymap.c
@@ -43,14 +43,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] ={
     ),
 };
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (host_keyboard_led_state().caps_lock) {
-        for (uint8_t i = led_min; i < led_max; i++) {
-            if (g_led_config.flags[i] & LED_FLAG_MODIFIER) {
-                rgb_matrix_set_color(i, RGB_BLUE);
-            }
-        }
-    }
-    return false;
-}
-
