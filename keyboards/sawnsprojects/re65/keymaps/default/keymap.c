7d7838c9dad59ac0f0deb5182d42508614b754f5
Sắn <59417802+MaiTheSan@users.noreply.github.com>
2023-09-20 05:59:47 +0700

    Update keyboards/sawnsprojects/re65/keymaps/default/keymap.c
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/sawnsprojects/re65/keymaps/default/keymap.c
+++ keyboards/sawnsprojects/re65/keymaps/default/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * │Shift   │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│ Shift│ Up│End│
  * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬─┬───┼───┼───┤
  * │Ctrl│ Opt│ Cmd│          Space         │Cmd │FnPy│ │Lef│Dow│Rig│
- * └────┴────┴────┴────────────────────────┴────┴────┴─┴───┴───┴───┘
+ * └────┴────┴────┴────────────────────────┴────┴────┘ └───┴───┴───┘
  */
-[_BASE] = LAYOUT(
+[_BASE] = LAYOUT_65_ansi_blocker(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_HOME,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * │        │LYR│Thm│   │   │RST│   │Mke│Prv│Nxt│Ply│      │PgU│Mut│
  * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬─┬───┼───┼───┤
  * │    │    │    │                        │    │    │ │Hom│PgD│End│
- * └────┴────┴────┴────────────────────────┴────┴────┴─┴───┴───┴───┘
+ * └────┴────┴────┴────────────────────────┴────┴────┘ └───┴───┴───┘
  */
-[_FN1] = LAYOUT(
+[_FN1] = LAYOUT_65_ansi_blocker(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F13,  KC_F14,  KC_F15,  _______, KC_VOLU,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLD,
