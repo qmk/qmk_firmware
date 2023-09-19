642e59ea0df5c3a69aeb11782e03fbc2074c474f
jack <0x6a73@protonmail.com>
2023-09-19 17:07:15 -0600

    Update keyboards/era/klein/keymaps/via/keymap.c

--- keyboards/era/klein/keymaps/via/keymap.c
+++ keyboards/era/klein/keymaps/via/keymap.c
@@ -20,3 +20,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   KC_LSFT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                   KC_TRNS,            KC_TRNS,            KC_TRNS,  KC_TRNS,                      KC_TRNS,            KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS
     )
+};
