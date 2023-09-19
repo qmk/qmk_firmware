83f475126e53e5c644f1ae894324bb4934fe4aa2
spbgzh <72414103+spbgzh@users.noreply.github.com>
2023-09-19 13:11:38 +0300

    Update keyboards/meletrix/zoom98/zoom98.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/meletrix/zoom98/zoom98.c
+++ keyboards/meletrix/zoom98/zoom98.c
@@ -14,7 +14,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+#include "quantum.h"
+
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+        return false;
+    }
     if (host_keyboard_led_state().caps_lock) {
         for (uint8_t i = led_min; i < led_max; i++) {
             if (g_led_config.flags[i] & LED_FLAG_MODIFIER) {
