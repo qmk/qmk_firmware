f497d88d16d0b04c943645f42d5b5449b49084ff
Etienne Collin <collin.etienne.media@gmail.com>
2023-09-19 11:09:10 -0400

    Removed unnecessary #undef
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/etiennecollin/wave/keymaps/etiennecollin/config.h
+++ keyboards/etiennecollin/wave/keymaps/etiennecollin/config.h
@@ -34,15 +34,10 @@
 #define HOLD_ON_OTHER_KEY_PRESS
 
 // Mouse key speed and acceleration.
-#undef MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY          0
-#undef MOUSEKEY_INTERVAL
 #define MOUSEKEY_INTERVAL       16
-#undef MOUSEKEY_WHEEL_DELAY
 #define MOUSEKEY_WHEEL_DELAY    0
-#undef MOUSEKEY_MAX_SPEED
 #define MOUSEKEY_MAX_SPEED      6
-#undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX    64
 
 // Thumb Combos
