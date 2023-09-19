7387d89bee3a44de670ca47d1a9d9ec403126d2f
Sắn <otaxumi@gmail.com>
2023-09-20 03:57:40 +0700

    add support for Okayu

---
+++ keyboards/sawnsprojects/okayu/stm32f072/chconf.h
@@ -0,0 +1,12 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define CH_CFG_ST_FREQUENCY 10000
+
+#define CH_CFG_OPTIMIZE_SPEED FALSE
+
+#define CH_CFG_USE_CONDVARS_TIMEOUT FALSE
+
+#include_next <chconf.h>
