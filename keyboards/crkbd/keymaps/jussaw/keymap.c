5fe71a4201b88ac5bcbd6bfb9a45e496e6dc09d2
Justin Sawyer <jtsaw36@gmail.com>
2023-09-20 00:32:06 -0600

    Add hyper key to all layers

--- keyboards/crkbd/keymaps/jussaw/keymap.c
+++ keyboards/crkbd/keymaps/jussaw/keymap.c
@@ -74,14 +74,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
    * |  CAPS  |    Z   |    X   |    C   |    V   |    B   |  |    N   |    M   |    ,   |    .   |    /   | Enter  |
    * `--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------'
-   *                            |        | Lower  | Space  |  |   Bksp | Upper  |        |
+   *                            |  Hyper | Lower  | Space  |  |  Bksp  | Upper  |        |
    *                            `--------------------------'  `--------------------------'
    */
   [_QWERTY] = LAYOUT_split_3x6_3(
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
       KC_ESC,   A_LGUI,  S_LALT,  D_LCTL,  F_LSFT,    KC_G,       KC_H,  J_RSFT,  K_RCTL,  L_RALT, SC_RGUI, KC_QUOT,
       KC_CAPS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                                 XXXXXXX,   LOWER,  KC_SPC,    KC_BSPC,   UPPER, XXXXXXX
+                                 KC_HYPR,   LOWER,  KC_SPC,    KC_BSPC,   UPPER, XXXXXXX
   ),
 
   /* Lower
@@ -92,14 +92,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
    * |        |        |        |        |        |        |  |    ~   |    _   |    +   |    {   |    }   |   |    |
    * `--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------'
-   *                            |        | Trans  | Space  |  |  Bksp  | Adjust |        |
+   *                            |  Hyper | Trans  | Space  |  |  Bksp  | Adjust |        |
    *                            `--------------------------'  `--------------------------'
    */
   [_LOWER] = LAYOUT_split_3x6_3(
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,
        KC_ESC, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,     KC_GRV,MIN_RSFT,EQL_RCTL,LBC_RALT,RBC_RGUI, KC_BSLS,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                                 XXXXXXX, KC_TRNS,  KC_SPC,    KC_BSPC,  ADJUST, XXXXXXX
+                                 KC_HYPR, KC_TRNS,  KC_SPC,    KC_BSPC,  ADJUST, XXXXXXX
   ),
 
   /* Upper
@@ -110,14 +110,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * +--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
    * |   F12  |   F6   |   F7   |   F8   |   F9   |  F10   |  |PrntScrn|  Home  |Pg Down | Pg Up  |  End   | Enter  |
    * `--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------'
-   *                            |        | Adjust | Space  |  |  Bksp  | Trans  |        |
+   *                            |  Hyper | Adjust | Space  |  |  Bksp  | Trans  |        |
    *                            `--------------------------'  `--------------------------'
    */
   [_UPPER] = LAYOUT_split_3x6_3(
        KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
        KC_F11, F1_LGUI, F2_LALT, F3_LCTL, F4_LSFT,   KC_F5,     KC_INS, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_BSLS,
        KC_F12,   KC_F5,   KC_F7,   KC_F8,   KC_F9,  KC_F10,    KC_PSCR, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_ENT,
-                                 XXXXXXX,  ADJUST,  KC_SPC,    KC_BSPC, KC_TRNS, XXXXXXX
+                                 KC_HYPR,  ADJUST,  KC_SPC,    KC_BSPC, KC_TRNS, XXXXXXX
   ),
 
   /* Adjust
@@ -128,14 +128,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
    * |        |        |        | Mouse4 | Mouse5 |        |  |  Vol-  | MsWl L | MsWl D | MsWl U | MsWl R |        |
    * `--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------'
-   *                            |        | Lower  |        |  |        | Upper  |        |
+   *                            |  Hyper | Lower  |        |  |        | Upper  |        |
    *                            `--------------------------'  `--------------------------'
    */
   [_ADJUST] = LAYOUT_split_3x6_3(
       QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG_GAME,     KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX,
       DF_QWRT, XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN1, XXXXXXX,     KC_VOLU, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
       XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN4, KC_BTN5, XXXXXXX,     KC_VOLD, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
-                                 XXXXXXX, KC_TRNS, XXXXXXX,     XXXXXXX, KC_TRNS, XXXXXXX
+                                 KC_HYPR, KC_TRNS, XXXXXXX,     XXXXXXX, KC_TRNS, XXXXXXX
   ),
 
   /* Game
@@ -153,6 +153,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
          KC_G,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
          KC_B,  KC_LCTL,   KC_Z,    KC_X,    KC_C,    KC_V,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                                   KC_ESC, KC_LALT, KC_SPC,     XXXXXXX, XXXXXXX, TG_GAME
+                                  KC_ESC, KC_LALT,  KC_SPC,     XXXXXXX, XXXXXXX, TG_GAME
   ),
 };
