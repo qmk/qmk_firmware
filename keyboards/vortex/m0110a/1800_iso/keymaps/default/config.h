c6968d1104eed481a5d34790c16383f6b5ab02f3
gskygithub <106651989+gskygithub@users.noreply.github.com>
2023-09-20 09:02:17 +0800

    update file

--- keyboards/vortex/m0110a/1800_iso/keymaps/default/config.h
+++ keyboards/vortex/m0110a/1800_iso/keymaps/default/config.h
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
