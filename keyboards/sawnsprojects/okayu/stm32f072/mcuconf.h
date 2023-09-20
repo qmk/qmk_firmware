46f3e6985d816869877e2649c4b8673f8fa4720f
Sắn <59417802+MaiTheSan@users.noreply.github.com>
2023-09-20 07:26:59 +0700

    Update keyboards/sawnsprojects/okayu/stm32f072/mcuconf.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/sawnsprojects/okayu/stm32f072/mcuconf.h
+++ keyboards/sawnsprojects/okayu/stm32f072/mcuconf.h
@@ -4,11 +4,5 @@
 
 #include_next <mcuconf.h>
 
-#undef STM32_ADC_USE_ADC1
-#define STM32_ADC_USE_ADC1 TRUE
-
-#undef STM32_PWM_USE_TIM3
-#define STM32_PWM_USE_TIM3 TRUE
-
 #undef STM32_SPI_USE_SPI1
 #define STM32_SPI_USE_SPI1 TRUE
