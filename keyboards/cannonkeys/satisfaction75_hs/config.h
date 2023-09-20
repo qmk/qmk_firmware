ec18aff116484960f50e7b3aba5e22f1ae25907f
Andrew Kannan <andrew.kannan@gmail.com>
2023-09-19 23:28:35 -0400

    Some final cleanup

--- keyboards/cannonkeys/satisfaction75_hs/config.h
+++ keyboards/cannonkeys/satisfaction75_hs/config.h
@@ -1,19 +1,5 @@
-/*
-Copyright 2015 Jun Wako <wakojun@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2023 Andrew Kannan (@awkannan)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
@@ -24,9 +10,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define STM32_LSECLK 32768
 
 #define ENCODER_RESOLUTION 2
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
 
 // I2C config
 #define I2C_DRIVER I2CD1
@@ -59,19 +42,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // VIA lighting is handled by the keyboard-level code
 #define VIA_CUSTOM_LIGHTING_ENABLE
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
