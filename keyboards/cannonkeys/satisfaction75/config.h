70c3f613c58bb8e6a22a5a9168f0aaa2ea558afd
Andrew Kannan <andrew.kannan@gmail.com>
2023-09-19 23:32:57 -0400

    remove blank layers on rev2 via keymap

--- keyboards/cannonkeys/satisfaction75/config.h
+++ keyboards/cannonkeys/satisfaction75/config.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-/* Ensure we jump to bootloader if the RESET keycode was pressed */
+/* Ensure we jump to bootloader if the QK_BOOT keycode was pressed */
 #define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
 
 /* LSE clock */
