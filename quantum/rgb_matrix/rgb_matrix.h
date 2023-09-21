5b86c86bd88796b759e4245f7cf6ba76b63c24f9
Ryan <fauxpark@gmail.com>
2023-09-21 12:16:17 +1000

    LED/RGB Matrix: prefix driver defines (#22088)

--- quantum/rgb_matrix/rgb_matrix.h
+++ quantum/rgb_matrix/rgb_matrix.h
@@ -24,25 +24,25 @@
 #include "color.h"
 #include "keyboard.h"
 
-#if defined(IS31FL3218)
+#if defined(RGB_MATRIX_IS31FL3218)
 #    include "is31fl3218.h"
-#elif defined(IS31FL3731)
+#elif defined(RGB_MATRIX_IS31FL3731)
 #    include "is31fl3731.h"
-#elif defined(IS31FL3733)
+#elif defined(RGB_MATRIX_IS31FL3733)
 #    include "is31fl3733.h"
-#elif defined(IS31FL3736)
+#elif defined(RGB_MATRIX_IS31FL3736)
 #    include "is31fl3736.h"
-#elif defined(IS31FL3737)
+#elif defined(RGB_MATRIX_IS31FL3737)
 #    include "is31fl3737.h"
-#elif defined(IS31FL3741)
+#elif defined(RGB_MATRIX_IS31FL3741)
 #    include "is31fl3741.h"
 #elif defined(IS31FLCOMMON)
 #    include "is31flcommon.h"
-#elif defined(CKLED2001)
+#elif defined(RGB_MATRIX_CKLED2001)
 #    include "ckled2001.h"
-#elif defined(AW20216S)
+#elif defined(RGB_MATRIX_AW20216S)
 #    include "aw20216s.h"
-#elif defined(WS2812)
+#elif defined(RGB_MATRIX_WS2812)
 #    include "ws2812.h"
 #endif
 
