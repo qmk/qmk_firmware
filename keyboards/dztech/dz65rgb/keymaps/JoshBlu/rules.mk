38f2c23f41276940ff15e44bd7165dc1959ca587
JoshBlu <>
2023-09-20 20:14:39 -0500

    Fuxed typo in comments. Disabled Space Cadet

--- keyboards/dztech/dz65rgb/keymaps/JoshBlu/rules.mk
+++ keyboards/dztech/dz65rgb/keymaps/JoshBlu/rules.mk
@@ -1,6 +1,8 @@
 CAPS_WORD_ENABLE = yes
 VIA_ENABLE = yes
-LTO_ENABLE = yes	#Recuse the size of the firmware through link time optimization
+LTO_ENABLE = yes	#Reduce the size of the firmware through link time optimization
 #Following rules to reduce size of firmware size
 GRAVE_ESC_ENABLE = no
 MUSIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+
