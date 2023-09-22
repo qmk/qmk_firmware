7910b7e3d6fa7daf61f2dacdeb6f61f40a2c3052
“yuezp” <yuezpchn@126.com>
2023-09-22 23:08:20 +0800

    Accept the amendment

--- keyboards/split67/rules.mk
+++ keyboards/split67/rules.mk
@@ -1,16 +1,1 @@
-# Build Options
-#   change yes to no to disable
-#
-# Wildcard to allow APM32 MCU
-DFU_SUFFIX_ARGS = -p FFFF -v FFFF
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes         # Commands for debug and configuration
-NKRO_ENABLE = yes            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-LTO_ENABLE = yes
-SPLIT_KEYBOARD = yes
+# This file intentionally left blank
