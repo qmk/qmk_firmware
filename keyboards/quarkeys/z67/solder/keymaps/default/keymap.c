ed67d894c29806332d889527e094357fa7a76d85
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-09-23 23:41:22 +0100

    post-merge fixes

--- keyboards/quarkeys/z67/solder/keymaps/default/keymap.c
+++ keyboards/quarkeys/z67/solder/keymaps/default/keymap.c
@@ -28,7 +28,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * | Ctrl   |  Win   |  ALT   |                  SPACE                   | ALT  | Win  | Ctrl | Left | Down | Rght |
      * `--------+--------+--------+------------------------------------------+------+------+------+------+------+------'
      */
-
     [0] = LAYOUT_all(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_TILD, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
@@ -49,7 +48,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |        |        |        |                    0                     |      |      |      |      |      |      |
      * `--------+--------+--------+------------------------------------------+------+------+------+------+------+------'
      */
-
     [1] = LAYOUT_all(
         QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_BSLS, KC_7,    KC_8,    KC_9,    RGB_TOG, RGB_MOD, RGB_VAI, KC_TRNS,          KC_TRNS,
