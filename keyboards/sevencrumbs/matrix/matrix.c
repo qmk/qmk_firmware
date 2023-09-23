c237a550ab74337c3e017bc540eb82d722336773
Chris <chris@zoid.com.au>
2023-09-23 22:37:10 +1000

    Convert indentation to spaces

--- keyboards/sevencrumbs/matrix/matrix.c
+++ keyboards/sevencrumbs/matrix/matrix.c
@@ -5,23 +5,23 @@
 #include "rgb_matrix_direct.h"
 
 void bootmagic_lite(void) {
-	matrix_scan();
-	wait_ms(10); // Must be equal or greater than the debounce time
-	matrix_scan();
+    matrix_scan();
+    wait_ms(10); // Must be equal or greater than the debounce time
+    matrix_scan();
 
-	if (matrix_get_row(0) == 3 && matrix_get_row(1) == 3) {
-		eeconfig_disable();
-		bootloader_jump();
-	}
+    if (matrix_get_row(0) == 3 && matrix_get_row(1) == 3) {
+        eeconfig_disable();
+        bootloader_jump();
+    }
 }
 
 #ifdef RGB_MATRIX_ENABLE
 
 const rgb_matrix_driver_t rgb_matrix_driver = {
-	.init = rgb_matrix_direct_init,
-	.set_color = rgb_matrix_direct_set_color,
-	.set_color_all = rgb_matrix_direct_set_color_all,
-	.flush = rgb_matrix_direct_flush
+    .init = rgb_matrix_direct_init,
+    .set_color = rgb_matrix_direct_set_color,
+    .set_color_all = rgb_matrix_direct_set_color_all,
+    .flush = rgb_matrix_direct_flush
 };
 
 #endif
--- keyboards/sevencrumbs/matrix/rgb_matrix_direct.c
+++ keyboards/sevencrumbs/matrix/rgb_matrix_direct.c
@@ -12,45 +12,45 @@ static PWMDriver *drivers[] = {&PWMD2, &PWMD3, &PWMD4, &PWMD5, &PWMD6, &PWMD7};
 static uint8_t buffer[CHANNEL_COUNT];
 
 void rgb_matrix_direct_init(void) {
-	PWMConfig config = {
-		.frequency = 2000 * 255, // Ensure this is never below the system clock divided by 256
-		.period = 254, // Must be set to 1 less than the maximum value for toggle-free output at 100%
-		.channels = {
-			{.mode = PWM_OUTPUT_ACTIVE_HIGH},
-			{.mode = PWM_OUTPUT_ACTIVE_HIGH}
-		}
-	};
+    PWMConfig config = {
+        .frequency = 2000 * 255, // Ensure this is never below the system clock divided by 256
+        .period = 254, // Must be set to 1 less than the maximum value for toggle-free output at 100%
+        .channels = {
+            {.mode = PWM_OUTPUT_ACTIVE_HIGH},
+            {.mode = PWM_OUTPUT_ACTIVE_HIGH}
+        }
+    };
 
-	for (int channel = 0; channel < CHANNEL_COUNT; channel++) {
-		PWMDriver *driver = drivers[channel >> 1];
-		int output = channel & 1;
+    for (int channel = 0; channel < CHANNEL_COUNT; channel++) {
+        PWMDriver *driver = drivers[channel >> 1];
+        int output = channel & 1;
 
-		if (output == 0) {
-			pwmStart(driver, &config);
-		}
+        if (output == 0) {
+            pwmStart(driver, &config);
+        }
 
-		pwmEnableChannel(driver, output, 0);
-		palSetPadMode(0, channel + CHANNEL_OFFSET, PAL_MODE_ALTERNATE_PWM);
-	}
+        pwmEnableChannel(driver, output, 0);
+        palSetPadMode(0, channel + CHANNEL_OFFSET, PAL_MODE_ALTERNATE_PWM);
+    }
 }
 
 void rgb_matrix_direct_set_color(int index, uint8_t red, uint8_t green, uint8_t blue) {
-	index *= COMPONENT_COUNT;
+    index *= COMPONENT_COUNT;
 
-	buffer[index] = red;
-	buffer[index + 1] = green;
-	buffer[index + 2] = blue;
+    buffer[index] = red;
+    buffer[index + 1] = green;
+    buffer[index + 2] = blue;
 }
 
 void rgb_matrix_direct_set_color_all(uint8_t red, uint8_t green, uint8_t blue) {
-	for (int index = 0; index < RGB_MATRIX_LED_COUNT; index++) {
-		rgb_matrix_direct_set_color(index, red, green, blue);
-	}
+    for (int index = 0; index < RGB_MATRIX_LED_COUNT; index++) {
+        rgb_matrix_direct_set_color(index, red, green, blue);
+    }
 }
 
 void rgb_matrix_direct_flush(void) {
-	for (int channel = 0; channel < CHANNEL_COUNT; channel++) {
-		PWMDriver *driver = drivers[channel >> 1];
-		pwmEnableChannel(driver, channel & 1, buffer[channel]);
-	}
+    for (int channel = 0; channel < CHANNEL_COUNT; channel++) {
+        PWMDriver *driver = drivers[channel >> 1];
+        pwmEnableChannel(driver, channel & 1, buffer[channel]);
+    }
 }
