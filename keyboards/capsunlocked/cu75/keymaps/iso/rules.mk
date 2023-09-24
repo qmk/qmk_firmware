6424dadd3785d83ebbe17861ed9f9b495b8ba6ae
Ryan <fauxpark@gmail.com>
2023-09-24 12:32:20 +1000

    Remove custom ISSI lighting code (#22073)
    * Remove CU75 custom lighting
    
    * Remove LFK78 custom lighting
    
    * Remove LFK87 custom lighting
    
    * Remove LFKPad custom lighting
    
    * Remove Mini1800 custom lighting
    
    * Remove SMK65 custom lighting
    
    * Remove LFK65-HS custom lighting
    
    * Remove LFKeyboards custom lighting
    
    * Remove Meira custom lighting

--- keyboards/capsunlocked/cu75/keymaps/iso/rules.mk
+++ keyboards/capsunlocked/cu75/keymaps/iso/rules.mk
@@ -4,10 +4,5 @@ EXTRAKEY_ENABLE = yes           # Audio control and System control(+450)
 CONSOLE_ENABLE = yes            # Console for debug(+400)
 COMMAND_ENABLE = yes            # Commands for debug and configuration
 NKRO_ENABLE = yes
-# BACKLIGHT_ENABLE = no           # Disable keyboard backlight functionality
 AUDIO_ENABLE = no               # Audio output
-RGBLIGHT_ENABLE = no          	# Disable RGB underlight
-SLEEP_LED_ENABLE = yes
-
-ISSI_ENABLE = no               # If the I2C pullup resistors aren't install this must be disabled
 WATCHDOG_ENABLE = yes           # Resets keyboard if matrix_scan isn't run every 250ms
