eec663a993547548f33f2f5b44d640511443a928
Matthew J. Wolf <matthew.wolf@speciosus.net>
2023-09-24 19:51:22 -0400

    F62: Does the CI build like "HAPTIC_DRIVER = solenoid" in lower case?

--- keyboards/model_f_labs/f62/wcass/rules.mk
+++ keyboards/model_f_labs/f62/wcass/rules.mk
@@ -1,7 +1,7 @@
 # Build Options
 #   change yes to no to disable
 #
-HAPTIC_ENABLE = yes
-HAPTIC_DRIVER = SOLENOID
+#HAPTIC_ENABLE = yes
+HAPTIC_DRIVER = solenoid
 CUSTOM_MATRIX=lite
 SRC += ../xwhatsit_capsense_matrix.c ../xwhatsit_util_comm.c
