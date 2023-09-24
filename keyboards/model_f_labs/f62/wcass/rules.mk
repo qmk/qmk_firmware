c95d99264b089f7482119f2eb0e9c2a65db3590b
Matthew J. Wolf <matthew.wolf@speciosus.net>
2023-09-24 19:42:07 -0400

    F62: Move Haptic enable to rules.mk

--- keyboards/model_f_labs/f62/wcass/rules.mk
+++ keyboards/model_f_labs/f62/wcass/rules.mk
@@ -1,7 +1,7 @@
 # Build Options
 #   change yes to no to disable
 #
-#HAPTIC_ENABLE = yes
+HAPTIC_ENABLE = yes
 HAPTIC_DRIVER = SOLENOID
 CUSTOM_MATRIX=lite
 SRC += ../xwhatsit_capsense_matrix.c ../xwhatsit_util_comm.c
