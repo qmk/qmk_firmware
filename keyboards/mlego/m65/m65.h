764e30a6113ce6e97f053631fa34d942ae6fab4d
alin m elena <alinm.elena@gmail.com>
2023-09-25 11:49:46 +0100

    rebase  and fix

--- keyboards/mlego/m65/m65.h
+++ keyboards/mlego/m65/m65.h
@@ -31,7 +31,7 @@ void                             set_default_rgb_layers(layer_state_t);
 void toggle_leds(void);
 void set_led_toggle(const uint8_t, const bool);
 
-static inline void init_lwr_rse_led(void) {
+static inline void init_lwr_rse_led(const bool toggle_lwr, const bool toggle_rse) {
 #ifdef LED_LWR_PIN
     setPinOutput(LED_LWR_PIN);
     writePin(LED_LWR_PIN, toggle_lwr);
