622b3a48f533b57c2f51d623a23c22eae2741f30
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2023-09-26 18:57:37 -0300

    Fix default firmware

--- keyboards/dark/magnum_ergo_1/keymaps/default/keymap.c
+++ keyboards/dark/magnum_ergo_1/keymaps/default/keymap.c
@@ -30,5 +30,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
              KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,                       KC_TRNS,          KC_TRNS,                            KC_TRNS
 )
-)
 };
