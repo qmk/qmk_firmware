f3cafc13660d16efd59cd3af7e6450fbb0e7820a
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2023-09-25 23:46:44 -0300

    Fix license headers

--- keyboards/dark/magnum_ergo_1/halconf.h
+++ keyboards/dark/magnum_ergo_1/halconf.h
@@ -1,4 +1,4 @@
-/* Copyright 2023 Gondolindrim <gondolindrim@acheronproject3 Gondolindrim <gondolindrim@acheronproject3 Gondolindrim <gondolindrim@acheronproject.com>
+/* Copyright 2023 Gondolindrim <gondolindrim@acheronproject.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
