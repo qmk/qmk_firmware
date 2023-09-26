ab30f8d88157e8e43e45fe1262ff14b9f004249d
Etienne Collin <collin.etienne.contact@gmail.com>
2023-09-25 22:10:00 -0400

    Fixed mod activation on rolling

--- keyboards/etiennecollin/wave/keymaps/etiennecollin/config.h
+++ keyboards/etiennecollin/wave/keymaps/etiennecollin/config.h
@@ -19,19 +19,15 @@
 // Activate caps word by pressing Left Shift + Right Shift
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat
-#define QUICK_TAP_TERM 0
-
 // Maximum time between taps of tap dances
 #define TAPPING_TERM 175
+
+// Max time between taps to prevent hold function and hold auto-repeat
+#define QUICK_TAP_TERM 100
 
 // Perform hold action if pressing a dual-role key, tapping another key and
 // releasing the dual-role key withing tapping term
 #define PERMISSIVE_HOLD
-
-// Perform hold action if pressing a dual-role key, pressing another key,
-// releasing the dual-role key and releasing the other key withing tapping term
-#define HOLD_ON_OTHER_KEY_PRESS
 
 // Mouse key speed and acceleration.
 #define MOUSEKEY_DELAY          0
