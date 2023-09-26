f2cac78b3826027d0b718d4cf5b6f5b2a8bf0eab
michael-mml <mml.liu@mail.utoronto.ca>
2023-09-25 17:19:40 -0700

    Add michael-mml GMMK Pro keymaps

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/michael-mml/def.h
@@ -0,0 +1,123 @@
+/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+   Copyright 2023 michael-mml
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+enum LAYER_NAME_TO_LAYER_INDEX_MAP {
+    WIN,   // defaults to 0 and increments by 1
+    MAC,   // is equal to 1
+    DEBUG  // is equal to 2
+};
+
+enum LED_INDEX_TO_KEY_MAP {
+    ESC,
+    TILDE,
+    TAB,
+    CAPS,
+    LEFT_SHFT,
+    LEFT_CTRL,
+    FUNCTION1,
+    ONE,
+    Q,
+    A,
+    Z,
+    LEFT_WIN,
+    FUNCTION2,
+    TWO,
+    W,
+    S,
+    X,
+    LEFT_ALT,
+    FUNCTION3,
+    THREE,
+    E,
+    D,
+    C,
+    FUNCTION4,
+    FOUR,
+    R,
+    FUNCTION,
+    V,
+    FUNCTION5,
+    FIVE,
+    T,
+    G,
+    B,
+    SPACE,
+    FUNCTION6,
+    SIX,
+    Y,
+    H,
+    N,
+    FUNCTION7,
+    SEVEN,
+    U,
+    J,
+    M,
+    FUNCTION8,
+    EIGHT,
+    I,
+    K,
+    COMMA_LEFT_BRACKET,
+    RIGHT_ALT,
+    FUNCTION9,
+    NINE,
+    O,
+    L,
+    PERIOD_RIGHT_BRACKET,
+    FN,
+    FUNCTION10,
+    ZERO,
+    P,
+    SEMICOLON_COLON,
+    FORWARD_SLASH_QUESTION_MARK,
+    FUNCTION11,
+    DASH_UNDERSCORE,
+    LEFT_SQUARE_BRACKET_CURLY_BRACKET,
+    APOSTROPHE_DOUBLE_QUOTE,
+    RIGHT_CTRL,
+    FUNCTION12,
+    SIDE_LED_LEFT_0,
+    SIDE_LED_RIGHT_0,
+    PRINT_SCREEN,
+    SIDE_LED_LEFT_1,
+    SIDE_LED_RIGHT_1,
+    DELETE,
+    SIDE_LED_LEFT_2,
+    SIDE_LED_RIGHT_2,
+    PAGE_UP,
+    SIDE_LED_LEFT_3,
+    SIDE_LED_RIGHT_3,
+    EQUALS_PLUS,
+    RIGHT_ARROW,
+    SIDE_LED_LEFT_4,
+    SIDE_LED_RIGHT_4,
+    END_KEY,
+    SIDE_LED_LEFT_5,
+    SIDE_LED_RIGHT_5,
+    BACKSPACE,
+    PAGE_DOWN,
+    SIDE_LED_LEFT_6,
+    SIDE_LED_RIGHT_6,
+    RIGHT_SQUARE_BRACKET_CURLY_BRACKET,
+    RIGHT_SHIFT,
+    SIDE_LED_LEFT_7,
+    SIDE_LED_RIGHT_7,
+    BACKSLASH_PIPE,
+    UP_ARROW,
+    LEFT_ARROW,
+    ENTER,
+    DOWN_ARROW
+};
