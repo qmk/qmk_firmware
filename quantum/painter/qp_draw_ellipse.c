6e5e5cff129e0f926f4b133520c34a354be61c56
elpekenin <elpekenin@elpekenin.dev>
2023-09-28 17:06:58 +0200

    It was just an overflow after all, fix it

--- quantum/painter/qp_draw_ellipse.c
+++ quantum/painter/qp_draw_ellipse.c
@@ -1,13 +1,10 @@
 // Copyright 2021 Paul Cotter (@gr1mr3aver)
 // Copyright 2021 Nick Brassel (@tzarc)
-// Copyright 2023 Pablo Martinez (@elpekenin)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "qp_internal.h"
 #include "qp_comms.h"
 #include "qp_draw.h"
-
-#include <lib/lib8tion/lib8tion.h>
 
 // Utilize 4-way symmetry to draw an ellipse
 static bool qp_ellipse_helper_impl(painter_device_t device, uint16_t centerx, uint16_t centery, uint16_t offsetx, uint16_t offsety, bool filled) {
@@ -65,11 +62,18 @@ static bool qp_ellipse_helper_impl(painter_device_t device, uint16_t centerx, ui
 bool qp_ellipse(painter_device_t device, uint16_t x, uint16_t y, uint16_t sizex, uint16_t sizey, uint8_t hue, uint8_t sat, uint8_t val, bool filled) {
     qp_dprintf("qp_ellipse: entry\n");
     painter_driver_t *driver = (painter_driver_t *)device;
-
     if (!driver || !driver->validate_ok) {
         qp_dprintf("qp_ellipse: fail (validation_ok == false)\n");
         return false;
     }
+
+    int32_t aa = ((int32_t)sizex) * ((int32_t)sizex);
+    int32_t bb = ((int32_t)sizey) * ((int32_t)sizey);
+    int32_t fa = 4 * aa;
+    int32_t fb = 4 * bb;
+
+    int16_t dx = 0;
+    int16_t dy = ((int16_t)sizey);
 
     qp_internal_fill_pixdata(device, QP_MAX(sizex, sizey), hue, sat, val);
 
@@ -79,23 +83,31 @@ bool qp_ellipse(painter_device_t device, uint16_t x, uint16_t y, uint16_t sizex,
     }
 
     bool ret = true;
-    // parametric equation of the ellipse:
-    //    x = centerx + sizex * cos(t)
-    //    y = centery + sizey * sin(t)
-    // where t: [0, 2pi) rads, 90deg is enough due to 4-way symmetry
-    for (uint8_t degrees = 0; degrees < 90; ++degrees) {
-        // scale for lib8tion's input range
-        uint16_t angle = degrees * UINT16_MAX / 360;
-
-        // cos16 and sin16 return [-U16MAX/2, U16MAX/2)
-        // compute with equations and divide back into [0, 1) range
-        uint16_t offsetx = cos16(angle) * sizex / (UINT16_MAX / 2);
-        uint16_t offsety = sin16(angle) * sizey / (UINT16_MAX / 2);
-
-        if (!qp_ellipse_helper_impl(device, x, y, offsetx, offsety, filled)) {
+    for (int32_t delta = (2 * bb) + (aa * (1 - (2 * sizey))); bb * dx <= aa * dy; dx++) {
+        if (!qp_ellipse_helper_impl(device, x, y, dx, dy, filled)) {
             ret = false;
             break;
         }
+        if (delta >= 0) {
+            delta += fa * (1 - dy);
+            dy--;
+        }
+        delta += bb * (4 * dx + 6);
+    }
+
+    dx = sizex;
+    dy = 0;
+
+    for (int32_t delta = (2 * aa) + (bb * (1 - (2 * sizex))); aa * dy <= bb * dx; dy++) {
+        if (!qp_ellipse_helper_impl(device, x, y, dx, dy, filled)) {
+            ret = false;
+            break;
+        }
+        if (delta >= 0) {
+            delta += fb * (1 - dx);
+            dx--;
+        }
+        delta += aa * (4 * dy + 6);
     }
 
     qp_dprintf("qp_ellipse: %s\n", ret ? "ok" : "fail");
