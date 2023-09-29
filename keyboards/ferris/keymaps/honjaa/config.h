5dbb60454045f31dea4aa7be2d637b524127fe14
Håkon Oftedal Njaa <hakon.oftedal.njaa@basefarm.com>
2023-09-29 22:17:02 +0200

    increased tapping term from 150 to 175

--- keyboards/ferris/keymaps/honjaa/config.h
+++ keyboards/ferris/keymaps/honjaa/config.h
@@ -99,7 +99,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 150
+#define TAPPING_TERM 175
 #define PERMISSIVE_HOLD
 //#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
