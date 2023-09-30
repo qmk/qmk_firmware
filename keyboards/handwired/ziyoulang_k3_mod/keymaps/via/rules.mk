aff4f96b5a7bbca8c0f652af71e04f00e892616e
Coom <1655787+coomstoolbox@users.noreply.github.com>
2023-09-30 20:12:27 +0900

    Update keyboards/handwired/ziyoulang_k3_mod/keymaps/via/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/ziyoulang_k3_mod/keymaps/via/rules.mk
+++ keyboards/handwired/ziyoulang_k3_mod/keymaps/via/rules.mk
@@ -1,5 +1,3 @@
 VIA_ENABLE = yes
-# Blue Pill STM32F103C6T6 which only has 22KB of available space.
-# In order to reduce the size of Firmware, disabling mousekey and via LTO were necessary.
 MOUSEKEY_ENABLE = no
 LTO_ENABLE = yes
