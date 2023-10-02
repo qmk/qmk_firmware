5e6343cd3184d5a8bf75894dd40765dfaf3452d9
Persama <46205716+Persama@users.noreply.github.com>
2023-10-02 22:10:25 +0800

    Update keyboards/nuphy/air75_v2/ansi/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/nuphy/air75_v2/ansi/config.h
+++ keyboards/nuphy/air75_v2/ansi/config.h
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-#define EECONFIG_USER_DATA_SIZE 8 // This is the size of the EEPROM for the custom VIA-specific data
 
 #define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
 
