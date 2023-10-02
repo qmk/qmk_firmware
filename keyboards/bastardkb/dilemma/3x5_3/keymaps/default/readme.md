cb9ede0fed9fcb826d3b454c8efd4dd73c9e28af
casuanoob <gms.rulz@hotmail.com>
2023-10-03 00:28:06 +1100

    3x5_3 Apply changes for data-driven update
    mirror the changes requested by reviewers in the Dilemma Max PR
    move rgb animation enabling to info.json
    remove redefinitions of default rgbmatrix values
    move rules.mk <feature>_enable to info.json

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/default/readme.md
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/default/readme.md
@@ -1,3 +1,1 @@
 # Dilemma 3x5+3 default keymap
-
-> :bulb: Have a look at the [`via` keymap](../via) for a more feature-rich layout.
--- keyboards/bastardkb/dilemma/3x5_3/rules.mk
+++ keyboards/bastardkb/dilemma/3x5_3/rules.mk
@@ -1,18 +1,4 @@
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-MAGIC_ENABLE = yes			# Magic keycodes
-TRI_LAYER_ENABLE = yes		# Tri Layers
-
-AUDIO_SUPPORTED = no        # Audio is not supported
-RGBLIGHT_SUPPORTED = no     # RGB underglow is supported, but not enabled by default
-
 SERIAL_DRIVER = vendor
 
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
-
-# RP2040-specific options
-PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
