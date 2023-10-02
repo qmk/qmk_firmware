0aea2f8292cc8759adc32bde1930fe58aa8714ac
Zach DeCook <zachdecook@librem.one>
2023-10-01 20:35:32 -0400

    hexboard: compile with oled support...

--- keyboards/hexboard/keymaps/keyboard/keymap.c
+++ keyboards/hexboard/keymaps/keyboard/keymap.c
@@ -137,3 +137,33 @@ _______,_______,_______,_______,_______,KC_PGDN,KC_END
 
 
 };
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("ADJ\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return false;
+}
+#endif
