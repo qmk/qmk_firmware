783fc2a938a7258843174068c34a928bde6d2184
Coom <kazunori.takahashi.g@gmail.com>
2023-10-05 02:03:11 +0900

    viaoled added.

---
+++ keyboards/handwired/ziyoulang_k3_mod/keymaps/viaoled/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 Coom (@coomstoolbox)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define I2C1_SCL_PIN B10
+#define I2C1_SDA_PIN B11
+#define I2C_DRIVER I2CD2
