3cecb06353cb2ceccf58aaac1d4e961cabe4a766
Yizhen Liu <62583086+edwardslau@users.noreply.github.com>
2023-10-05 16:18:34 -0600

    Update keyboards/gray_studio/bd60/bd60.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/gray_studio/bd60/bd60.c
+++ keyboards/gray_studio/bd60/bd60.c
@@ -8,11 +8,16 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer
 );
 
-void keyboard_post_init_user(void) {
+void keyboard_post_init_kb(void) {
     // Enable the LED layers
     rgblight_layers = my_rgb_layers;
+    
+    keyboard_post_init_user();
 }
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(0, led_state.caps_lock);
+
+bool led_update_kb(led_t led_state) {
+    if (led_update_user(led_state)) {
+        rgblight_set_layer_state(0, led_state.caps_lock);
+    }
     return true;
 }
