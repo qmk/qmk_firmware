372a32a428be7b8ecc4156517b328c09b656c4e0
Troglodyte <tix.richard@protonmail.com>
2023-10-05 14:11:39 +0200

    added romeo40 keymap and folder

--- keyboards/coseyfannitutti/romeo/rules.mk
+++ keyboards/coseyfannitutti/romeo/rules.mk
@@ -10,3 +10,4 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+COMBO_ENABLE = yes
