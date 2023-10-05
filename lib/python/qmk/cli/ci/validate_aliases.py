05b88e1c2f661ed9296f04ef8b4accd6bb93a0d1
Nick Brassel <nick@tzarc.org>
2023-10-06 07:33:10 +1100

    Best-effort attempt at fixing up aliases.

--- lib/python/qmk/cli/ci/validate_aliases.py
+++ lib/python/qmk/cli/ci/validate_aliases.py
@@ -19,9 +19,14 @@ def _safe_keyboard_folder(target):
 @cli.subcommand('Validates the list of keyboard aliases.', hidden=True)
 def ci_validate_aliases(cli):
     aliases = json_load(Path('data/mappings/keyboard_aliases.hjson'))
+
+    success = True
     for alias in aliases.keys():
         if alias == 'bm16a':
             pass
         target = aliases[alias].get('target', None)
         if not target or not (QMK_FIRMWARE / 'keyboards' / target).exists() or not resolve_keyboard(target) or not _safe_keyboard_folder(target):
             cli.log.error(f'Keyboard alias {alias} has a target that doesn\'t exist: {target}')
+            success = False
+
+    return success
