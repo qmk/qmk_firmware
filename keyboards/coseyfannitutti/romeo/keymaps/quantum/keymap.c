b67643e2f70d0c1a01eb5586f6bffc26b9d6a3ac
Troglodyte <tix.richard@protonmail.com>
2023-10-06 10:26:25 +0200

    Alternate solution to combo implemented

--- keyboards/coseyfannitutti/romeo/keymaps/quantum/keymap.c
+++ keyboards/coseyfannitutti/romeo/keymaps/quantum/keymap.c
@@ -6,10 +6,27 @@
  * ctrl alt delete combo fix
  */
 
-const uint16_t PROGMEM ctrlaltdelete[] = {KC_LCTL, KC_LALT, KC_DOWN, COMBO_END};
-combo_t key_combos[] = {
-    COMBO(ctrlaltdelete, KC_LCTL(KC_LALT, KC_DEL))
+enum combo_events {
+	CTRL_ALT_DELETE,
 };
+
+const uint16_t PROGMEM ctrlaltdelete[] = {KC_LCTL, KC_LALT, KC_DOWN, COMBO_END};
+
+combo_t key_combos[] = {
+	[CTRL_ALT_DELETE] = COMBO_ACTION (ctrlaltdelete)
+};
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  switch(combo_index) {
+	case CTRL_ALT_DELETE:
+		if (pressed) {
+			tap_code16(KC_LCTL);
+			tap_code16(KC_LALT);
+			tap_code16(KC_DEL);
+			}
+			break;
+		}
+	}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_all(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, LM(1,MOD_LSFT), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT, KC_LSFT, KC_SLSH, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_LCTL, KC_LGUI, KC_LALT, LT(1,KC_SPC), LT(1,KC_SPC), LT(1,KC_SPC), KC_RALT, KC_LEFT, KC_DOWN),
