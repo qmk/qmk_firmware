ddf62c452f5cdb552bcbe125cde27fd6169acd8c
Nicolas Fabre <nicolas.fabre.21@outlook.fr>
2023-10-07 11:23:04 +0200

    Update keyboards/ciaanh/kanagawa/kanagawa.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ciaanh/kanagawa/kanagawa.c
+++ keyboards/ciaanh/kanagawa/kanagawa.c
@@ -49,9 +49,6 @@ bool oled_task_kb(void) {
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
     oled_write_ln_P(led_state.caps_lock ? PSTR("CAPLOCK") : PSTR("       "), false);
-    // oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    // oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    // oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
 
     oled_advance_page(true);
 
