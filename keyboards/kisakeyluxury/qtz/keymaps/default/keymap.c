971acaf4295a8bfdc7cc072264a090159d9b93f5
kisakey-luxury <kisakey.luxury@gmail.com>
2023-10-07 11:10:06 -0700

    updated requirements for PR

--- keyboards/kisakeyluxury/qtz/keymaps/default/keymap.c
+++ keyboards/kisakeyluxury/qtz/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ enum combo_events {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_default(
+  [_BASE] = LAYOUT(
     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
     KC_A,  KC_S,  KC_D,  KC_F,   KC_G,   KC_H,  KC_J,   KC_K,  KC_L, KC_ENT,
     KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM, KC_DOT,
