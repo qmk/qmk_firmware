e220a39399e1a0e2247b0024229194f2a29f9d75
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-10-08 22:31:00 +0100

    Add rules.mk for features & convert-to setting

---
+++ keyboards/splitkb/aurora/sweep/keymaps/agrski/rules.mk
@@ -0,0 +1,13 @@
+CONVERT_TO = liatris
+
+LTO_ENABLE          = yes
+MOUSEKEY_ENABLE     = yes
+EXTRAKEY_ENABLE     = yes
+RGBLIGHT_ENABLE     = yes
+LEADER_ENABLE       = yes
+SEND_STRING_ENABLE  = yes
+
+OLED_ENABLE             = no
+AUTO_SHIFT_ENABLE       = no
+AUTO_SHIFT_MODIFIERS    = no
+UNICODE_ENABLE          = no
