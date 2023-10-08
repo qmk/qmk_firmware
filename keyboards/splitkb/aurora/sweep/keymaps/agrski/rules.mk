c84dfad331cdd86ef509ebe5e6e8d0741eb17651
Alex Rakowski <20504869+agrski@users.noreply.github.com>
2023-10-08 23:28:54 +0100

    Disable RGBLIGHT and enable RGB_MATRIX in rules.mk

--- keyboards/splitkb/aurora/sweep/keymaps/agrski/rules.mk
+++ keyboards/splitkb/aurora/sweep/keymaps/agrski/rules.mk
@@ -3,10 +3,11 @@ CONVERT_TO = liatris
 LTO_ENABLE          = yes
 MOUSEKEY_ENABLE     = yes
 EXTRAKEY_ENABLE     = yes
-RGBLIGHT_ENABLE     = yes
 LEADER_ENABLE       = yes
 SEND_STRING_ENABLE  = yes
+RGB_MATRIX_ENABLE   = yes
 
+RGBLIGHT_ENABLE         = no
 OLED_ENABLE             = no
 AUTO_SHIFT_ENABLE       = no
 AUTO_SHIFT_MODIFIERS    = no
