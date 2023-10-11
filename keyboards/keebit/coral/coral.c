f39a4b616445996b712f8890e8cde94bcdc33f60
numToStr <vikasraj1911@gmail.com>
2023-10-11 19:57:57 +0530

    resolve pr review comments

--- keyboards/keebit/coral/coral.c
+++ keyboards/keebit/coral/coral.c
@@ -1,7 +1,7 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include QMK_KEYBOARD_H
+#include "quantum.h"
 
 // ===== LED/RGB INDICATOR =====
 enum RGB_LAYERS {
@@ -23,16 +23,20 @@ const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
    [RGBL_CAPSW] =  capsword_layer
 );
 
-bool led_update_user(led_t led_state) {
+bool led_update_kb(led_t led_state) {
+    if (!led_update_user(led_state)) {
+        return false;
+    }
     rgblight_set_layer_state(RGBL_CAPSL, led_state.caps_lock);
     return true;
 }
 
-void caps_word_set_user(bool active) {
+__attribute__((weak)) void caps_word_set_user(bool active) {
     rgblight_set_layer_state(RGBL_CAPSW, active);
 }
 
-void keyboard_post_init_user(void) {
+void keyboard_post_init_kb(void) {
     // Enable the LED layers
     rgblight_layers = rgb_layers;
+    keyboard_post_init_user();
 }
