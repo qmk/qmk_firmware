16b7f64eaf11dbd83cd2008ca847e804098f381d
dcpedit <dcpedit@hotmail.com>
2023-10-12 16:41:51 -0700

    fix defines

--- keyboards/dcpedit/midevil/config.h
+++ keyboards/dcpedit/midevil/config.h
@@ -8,8 +8,11 @@
 #define LCD_CS_PIN GP17   // chip select
 #define LCD_RST_PIN GP14  // reset
 #define LCD_LED_PIN GP15  // backlight
-#define LCD_SDA_PIN GP19  // SDI,MOSI data input
-#define LCD_SCK_PIN GP18  // clock
+
+/* SPI pins */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP18
+#define SPI_MOSI_PIN GP19
 
 #define QUANTUM_PAINTER_DISPLAY_TIMEOUT 0 // milliseconds
 #define QUANTUM_PAINTER_SUPPORTS_256_PALETTE TRUE
