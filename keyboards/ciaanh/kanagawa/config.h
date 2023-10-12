e112122451f37d8d699c47d07b4787b4eb8d5930
Nicolas Fabre <nicolas.fabre.21@outlook.fr>
2023-10-12 08:40:26 +0200

    Update keyboards/ciaanh/kanagawa/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/ciaanh/kanagawa/config.h
+++ keyboards/ciaanh/kanagawa/config.h
@@ -15,9 +15,6 @@
  */
 #pragma once
 
-#ifdef OLED_ENABLE
-/* I2C Conf */
-#    define I2C_DRIVER I2CD0
-#    define I2C1_SDA_PIN GP0
-#    define I2C1_SCL_PIN GP1
-#endif
+#define I2C_DRIVER I2CD0
+#define I2C1_SDA_PIN GP0
+#define I2C1_SCL_PIN GP1
