cd084cd03402756158c590aeaf5f7c4236e7dcaa
adophoxia <100170946+adophoxia@users.noreply.github.com>
2023-10-12 17:51:24 -0700

    Update keyboards/keychron/q5/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/keychron/q5/config.h
+++ keyboards/keychron/q5/config.h
@@ -44,9 +44,8 @@
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
 
 /* Encoder Configuration*/
-#ifdef ENCODER_ENABLE
-    #define ENCODER_DEFAULT_POS 0x3
-#endif
+#define ENCODER_DEFAULT_POS 0x3
+
 
 /* EEPROM Driver Configuration */
 #define WEAR_LEVELING_LOGICAL_SIZE 2048
