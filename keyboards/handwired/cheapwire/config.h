ad17030bab9cd9246f8a988bf93692b77b4aab16
cocolizo945 <81886929+cocolizo945@users.noreply.github.com>
2023-10-13 22:23:33 -0600

    adding the cheapwire

---
+++ keyboards/handwired/cheapwire/config.h
@@ -0,0 +1,20 @@
+// Copyright 2023 Eduardo Mendez (@cocolizo945)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+#define TAPPING_TERM 135
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM_PER_KEY
