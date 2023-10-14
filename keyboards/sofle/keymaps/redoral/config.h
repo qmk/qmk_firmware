7e1c60ff4c85e184beb2855a3a9b81d32becc3f6
Red Oral <redmunozoral@gmail.com>
2023-10-13 18:24:19 -0700

    Doc update, added back RGB support

--- keyboards/sofle/keymaps/redoral/config.h
+++ keyboards/sofle/keymaps/redoral/config.h
@@ -9,4 +9,30 @@
   
 #pragma once
 
-
+#if defined(KEYBOARD_sofle_rev1)
+// Add RGB underglow and top facing lighting
+#    define WS2812_DI_PIN D3
+#    define RGBLED_NUM 72
+#    define RGBLED_SPLIT \
+        { 36, 36 }
+#    ifdef RGB_MATRIX_ENABLE
+#        define RGB_MATRIX_LED_COUNT RGBLED_NUM
+#        define RGB_MATRIX_SPLIT RGBLED_SPLIT
+#        define SPLIT_TRANSPORT_MIRROR
+#    else
+#        define RGBLIGHT_EFFECT_BREATHING
+#        define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#        define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#        define RGBLIGHT_EFFECT_SNAKE
+#        define RGBLIGHT_EFFECT_KNIGHT
+#        define RGBLIGHT_EFFECT_CHRISTMAS
+#        define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#        define RGBLIGHT_EFFECT_RGB_TEST
+#        define RGBLIGHT_EFFECT_ALTERNATING
+#        define RGBLIGHT_EFFECT_TWINKLE
+#        define RGBLIGHT_LIMIT_VAL 120
+#        define RGBLIGHT_HUE_STEP 10
+#        define RGBLIGHT_SAT_STEP 17
+#        define RGBLIGHT_VAL_STEP 17
+#    endif
+#endif
