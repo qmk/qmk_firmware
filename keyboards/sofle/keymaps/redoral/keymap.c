3bc267318e3732fa7cd24593ff0090bd13841e54
Red Oral <redmunozoral@gmail.com>
2023-10-13 18:50:01 -0700

    Added middle mouse button on second layer

--- keyboards/sofle/keymaps/redoral/keymap.c
+++ keyboards/sofle/keymaps/redoral/keymap.c
@@ -32,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [0] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_MUTE,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_GRV,
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,     KC_9,   KC_0,     KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,     KC_BSPC,
+  KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_MUTE,KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_GRV,
                  KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC,      KC_ENT,  MO(1), KC_EQL, KC_RALT, KC_BSLS
 ),
 
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5,                       KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
   KC_TRNS, KC_TRNS, KC_UP,    KC_TRNS,  KC_TRNS, KC_TRNS,                     KC_INS,   KC_DEL,   KC_MS_U,  KC_LBRC,  KC_RBRC,  KC_F12,
   KC_TRNS, KC_LEFT, KC_DOWN,  KC_RGHT,  KC_TRNS, KC_TRNS,                     KC_BTN1,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2,  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_PGUP,  KC_PGDN,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_PGUP,  KC_PGDN,  KC_BTN3,  KC_TRNS,  KC_TRNS,  KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
