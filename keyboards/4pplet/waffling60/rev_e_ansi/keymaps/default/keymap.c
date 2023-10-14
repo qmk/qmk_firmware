b44d144500350e71780e71ac762213eec01789b2
4pplet <mail@4pplet.com>
2023-10-14 12:04:00 +0200

    update copyright text and keyboard name

--- keyboards/4pplet/waffling60/rev_e_ansi/keymaps/default/keymap.c
+++ keyboards/4pplet/waffling60/rev_e_ansi/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Stefan Sundin "4pplet" <4pplet@protonmail.com>
+Copyright 2023 Stefan Sundin "4pplet" <mail@4pplet.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
