f788141a03e59cef48eaa848d74265ace067c677
Daniel Reibl <janos.daniel.reibl@protonmail.com>
2023-10-15 12:21:05 +0200

    Add missing license headers

--- keyboards/handwired/riblee_split/halconf.h
+++ keyboards/handwired/riblee_split/halconf.h
@@ -1,3 +1,6 @@
+// Copyright 2023 Daniel Reibl (@riblee)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 #define HAL_USE_SERIAL TRUE
--- keyboards/handwired/riblee_split/mcuconf.h
+++ keyboards/handwired/riblee_split/mcuconf.h
@@ -1,3 +1,6 @@
+// Copyright 2023 Daniel Reibl (@riblee)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include_next <mcuconf.h>
 
 #undef STM32_SERIAL_USE_USART1
