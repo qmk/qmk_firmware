005bba1d3268ce80671e7ba00d4c2b9eaf2583cf
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2023-10-16 15:05:25 +0200

    Added wpm.

--- keyboards/lily58/keymaps/dvorak_prog/config.h
+++ keyboards/lily58/keymaps/dvorak_prog/config.h
@@ -22,8 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
