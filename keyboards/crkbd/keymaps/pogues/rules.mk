4a856d242ee723776dc832b6a483101c27e949ca
Paul Roe <20790515+pogues@users.noreply.github.com>
2023-10-19 23:48:26 +0100

    forcing RGB_MATRIX_ENABLE false for now

--- keyboards/crkbd/keymaps/pogues/rules.mk
+++ keyboards/crkbd/keymaps/pogues/rules.mk
@@ -1,5 +1,7 @@
 MOUSEKEY_ENABLE = yes     # Mouse keys
 RGBLIGHT_ENABLE = yes     # Enable WS2812 RGB underlight.
+# TODO should we switch from rgblight to rgbmatrix
+RGB_MATRIX_ENABLE = no     # Enable WS2812 RGB underlight.
 OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 
