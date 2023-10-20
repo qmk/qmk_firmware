867d2576c157bb79ba31434e1d52b4d82fa5d30f
zvecr <git@zvecr.com>
2023-10-20 22:16:41 +0100

    Remove console out endpoint

--- tmk_core/protocol/chibios/usb_main.h
+++ tmk_core/protocol/chibios/usb_main.h
@@ -57,7 +57,4 @@ void usb_event_queue_task(void);
 /* Putchar over the USB console */
 int8_t sendchar(uint8_t c);
 
-/* Flush output (send everything immediately) */
-void console_flush_output(void);
-
 #endif /* CONSOLE_ENABLE */
