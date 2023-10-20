5fafd79f9e5f71f84854ec7f7fffcd7662f66c92
pablin.123.ra@gmail.com <pablin.123.ra@gmail.com>
2023-10-20 10:18:35 -0600

    BigKnob: Invert colours + allow configuration of LCD params

--- keyboards/jpe230/big_knob/big_knob.c
+++ keyboards/jpe230/big_knob/big_knob.c
@@ -3,67 +3,10 @@
 
 #include "qp.h"
 #include "qp_comms.h"
-#include "qp_st7735.h"
 #include "qp_st77xx_opcodes.h"
-#include "qp_st7735_opcodes.h"
 #include "gfx/logo.qgf.h"
 
 painter_device_t lcd;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Quantum Painter Init
-// Using custom init to invert the colors of the display
-
-static inline void st7735_automatic_viewport_offsets(painter_device_t device, painter_rotation_t rotation) {
-    painter_driver_t *driver = (painter_driver_t *)device;
-
-    // clang-format off
-    const struct {
-        uint16_t offset_x;
-        uint16_t offset_y;
-    } rotation_offsets_80x160[] = {
-        [QP_ROTATION_0]   = { .offset_x = 26, .offset_y = 26 },
-        [QP_ROTATION_90]  = { .offset_x =  1, .offset_y =  1 },
-        [QP_ROTATION_180] = { .offset_x = 26, .offset_y =  1 },
-        [QP_ROTATION_270] = { .offset_x =  1, .offset_y = 26 },
-    };
-    // clang-format on
-
-    if (driver->panel_width == 80 && driver->panel_height == 160) {
-        driver->offset_x = rotation_offsets_80x160[rotation].offset_x;
-        driver->offset_y = rotation_offsets_80x160[rotation].offset_y;
-    }
-}
-
-bool qp_st7735_init(painter_device_t device, painter_rotation_t rotation) {
-    // clang-format off
-    const uint8_t st7735_init_sequence[] = {
-        // Command,                 Delay, N, Data[N]
-        ST77XX_CMD_RESET,            120,  0,
-        ST77XX_CMD_SLEEP_OFF,          5,  0,
-        ST77XX_SET_PIX_FMT,            0,  1, 0x55,
-        ST77XX_CMD_INVERT_ON,          0,  0,
-        ST77XX_CMD_NORMAL_ON,          0,  0,
-        ST77XX_CMD_DISPLAY_ON,        20,  0
-    };
-    // clang-format on
-    qp_comms_bulk_command_sequence(device, st7735_init_sequence, sizeof(st7735_init_sequence));
-
-    // Configure the rotation (i.e. the ordering and direction of memory writes in GRAM)
-    const uint8_t madctl[] = {
-        [QP_ROTATION_0]   = ST77XX_MADCTL_BGR,
-        [QP_ROTATION_90]  = ST77XX_MADCTL_BGR | ST77XX_MADCTL_MX | ST77XX_MADCTL_MV,
-        [QP_ROTATION_180] = ST77XX_MADCTL_BGR | ST77XX_MADCTL_MX | ST77XX_MADCTL_MY,
-        [QP_ROTATION_270] = ST77XX_MADCTL_BGR | ST77XX_MADCTL_MV | ST77XX_MADCTL_MY,
-    };
-    qp_comms_command_databyte(device, ST77XX_SET_MADCTL, madctl[rotation]);
-
-    #ifndef ST7735_NO_AUTOMATIC_VIEWPORT_OFFSETS
-        st7735_automatic_viewport_offsets(device, rotation);
-    #endif // ST7735_NO_AUTOMATIC_VIEWPORT_OFFSETS
-
-    return true;
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Init board:
@@ -72,15 +15,23 @@ bool qp_st7735_init(painter_device_t device, painter_rotation_t rotation) {
 void keyboard_post_init_kb(void) {    
     backlight_set(BACKLIGHT_DEFAULT_LEVEL);
 
-    wait_ms(150);
+    wait_ms(LCD_WAIT_TIME);
 
     // Initialise the LCD
-    lcd = qp_st7735_make_spi_device(80, 160, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 4, 0);
-    qp_init(lcd, QP_ROTATION_270);
+    lcd = qp_st7735_make_spi_device(LCD_HEIGHT, LCD_WIDTH, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, LCD_SPI_DIVISOR, 0);
+    qp_init(lcd, LCD_ROTATION);
+
+    // Invert Colour
+    qp_comms_start(lcd);
+    qp_comms_command(lcd, ST77XX_CMD_INVERT_ON);
+    qp_comms_stop(lcd);
+
+    // Apply Offset
+    qp_set_viewport_offsets(lcd, LCD_OFFSET_X, LCD_OFFSET_Y);
     
     // Turn on the LCD and clear the display
     qp_power(lcd, true);
-    qp_rect(lcd, 0, 0, 160, 80, HSV_BLACK, true);
+    qp_rect(lcd, 0, 0, LCD_WIDTH, LCD_HEIGHT, HSV_BLACK, true);
 
     // Show logo
     painter_image_handle_t logo_image = qp_load_image_mem(gfx_logo);
