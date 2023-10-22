17cfbb7562bf5d4a9fe6986769333747892cac93
Dasky <32983009+daskygit@users.noreply.github.com>
2023-10-22 12:12:05 +0100

    forgot to stage

--- quantum/pointing_device/pointing_device_drivers.c
+++ quantum/pointing_device/pointing_device_drivers.c
@@ -122,9 +122,9 @@ static i2c_status_t azoteq_iqs5xx_init_status = 1;
 void azoteq_iqs5xx_init(void) {
     i2c_init();
     azoteq_iqs5xx_wake();
-    if (azoteq_iqs5xx_get_product() != UNKNOWN) {
+    if (azoteq_iqs5xx_get_product() != AZOTEQ_IQS5XX_UNKNOWN) {
         azoteq_iqs5xx_setup_resolution();
-        azoteq_iqs5xx_init_status = azoteq_iqs5xx_set_report_rate(AZOTEQ_IQS5XX_REPORT_RATE, ACTIVE, false);
+        azoteq_iqs5xx_init_status = azoteq_iqs5xx_set_report_rate(AZOTEQ_IQS5XX_REPORT_RATE, AZOTEQ_IQS5XX_ACTIVE, false);
         azoteq_iqs5xx_init_status |= azoteq_iqs5xx_set_event_mode(AZOTEQ_IQS5XX_EVENT_MODE, false);
         azoteq_iqs5xx_init_status |= azoteq_iqs5xx_set_gesture_config(true);
         wait_ms(AZOTEQ_IQS5XX_REPORT_RATE + 1);
