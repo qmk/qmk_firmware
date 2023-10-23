bace9fc80b80c3af7c621d0fbfdd13c5075b7a6d
elpekenin <elpekenin@elpekenin.dev>
2023-10-23 21:51:32 +0200

    Missing header

--- drivers/painter/eink_panel/qp_eink_panel.c
+++ drivers/painter/eink_panel/qp_eink_panel.c
@@ -1,6 +1,8 @@
 // Copyright 2023 Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <stdlib.h> // abs
+
 #include "qp_comms.h"
 #include "qp_draw.h"
 #include "qp_eink_panel.h"
