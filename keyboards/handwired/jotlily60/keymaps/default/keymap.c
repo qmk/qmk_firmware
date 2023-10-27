dd2b2425dc9393747088a022b3f3222d8244e9cd
jotix <jujodeve@gmail.com>
2023-10-27 00:32:34 -0300

    default keymap

--- keyboards/handwired/jotlily60/keymaps/default/keymap.c
+++ keyboards/handwired/jotlily60/keymaps/default/keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                 |  lalt | lsuper| lctrl |   fn  | space | space |   fn  |  home |  end  |  del  |
  *                 +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  */
-[_QWERTY] = LAYOUT_jotlily60 (
-    KC_EQL, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MNS,
+[_QWERTY] = LAYOUT (
+    KC_EQL, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
     KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
     KC_TAB, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
     KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,KC_RBRC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
@@ -48,11 +48,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                 |       |       |       |       |       |       |       |  PgDn |  PgUp |       |
  *                 +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  */
-[_FN] = LAYOUT_jotlily60 (
-    KC_EQL, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,  XXXXXXX,XXXXXXX,KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MNS,
-    KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,  XXXXXXX,XXXXXXX,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
-    KC_TAB, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,  XXXXXXX,XXXXXXX,KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-    KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  KC_LBRC,KC_RBRC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
-    XXXXXXX,XXXXXXX,KC_LCTL,KC_LGUI,KC_LALT,FNLAYER,KC_SPC,KC_SPC, FNLAYER,KC_RALT,KC_DEL, KC_RCTL,XXXXXXX,XXXXXXX
-),
-
+[_FN] = LAYOUT (
+    KC_TILD,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,
+    _______,KC_F11, KC_F12, _______,_______,_______,                _______,_______,KC_PSCR,KC_SCRL,KC_PAUS,_______,
+    _______,KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,                KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,KC_QUOT,
+    _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,KC_BSLS,KC_PIPE,_______,_______,_______,_______,_______,_______,
+                    _______,_______,_______,_______,_______,_______,_______,KC_PGDN,KC_PGUP,_______
+)
+};
+  
