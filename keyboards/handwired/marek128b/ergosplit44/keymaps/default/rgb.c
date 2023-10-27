026bbad3cc5bad8c85b1328b76eb64ac56d52d2b
Marek128b <58476072+Marek128b@users.noreply.github.com>
2023-10-27 17:00:24 +0200

    Update keyboards/handwired/marek128b/ergosplit44/keymaps/default/rgb.c
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/handwired/marek128b/ergosplit44/keymaps/default/rgb.c
+++ keyboards/handwired/marek128b/ergosplit44/keymaps/default/rgb.c
@@ -2,8 +2,10 @@
 
 #include "quantum.h"
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) 
-{
+bool rgb_matrix_indicators_kb(void) {
+    if (!rgb_matrix_indicators_user()) {
+        return false;
+    }
     if (host_keyboard_led_state().caps_lock) 
 	{
         rgb_matrix_set_color(0, RGB_RED);
