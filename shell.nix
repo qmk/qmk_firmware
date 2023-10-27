ef92221cd2faacdba7048706307b0f05e1e95b8e
adisbladis <adisbladis@gmail.com>
2023-10-27 16:29:48 +1300

    nix: parse requirements-dev.txt to construct development shell
    This completely removes the poetry environment & poetry2nix from qmk
    and instead parses requirements-dev.txt and constructs an
    environment completely with packages from nixpkgs.
    
    That means that there is no more need to manually keep pyproject.toml
    in util/nix up to date and no need to do any dependency locking.

--- shell.nix
+++ shell.nix
@@ -1,35 +1,17 @@
 let
   # We specify sources via Niv: use "niv update nixpkgs" to update nixpkgs, for example.
   sources = import ./util/nix/sources.nix { };
-
-  # `tomlkit` >= 0.8.0 is required to build `jsonschema` >= 4.11.0 (older
-  # version do not support some valid TOML syntax: sdispater/tomlkit#148).  The
-  # updated `tomlkit` must be used by `makeRemoveSpecialDependenciesHook`
-  # inside `poetry2nix`, therefore just providing the updated version through
-  # our `nix/pyproject.toml` does not work, and using an overlay is required.
-  pythonOverlay = final: prev: {
-    python3 = prev.python3.override {
-      packageOverrides = self: super: {
-        tomlkit = super.tomlkit.overridePythonAttrs(old: rec {
-          version = "0.11.4";
-          src = super.fetchPypi {
-            inherit (old) pname;
-            inherit version;
-            sha256 = "sha256-MjWpAQ+uVDI+cnw6wG+3IHUv5mNbNCbjedrsYPvUSoM=";
-          };
-        });
-      };
-    };
-  };
 in
 # However, if you want to override Niv's inputs, this will let you do that.
-{ pkgs ? import sources.nixpkgs { overlays = [ pythonOverlay ]; }
-, poetry2nix ? pkgs.callPackage (import sources.poetry2nix) { }
+{ pkgs ? import sources.nixpkgs { }
+, pyproject-nix ? pkgs.callPackage (import sources."pyproject.nix") { }
 , avr ? true
 , arm ? true
 , teensy ? true }:
-with pkgs;
+
 let
+  inherit (pkgs) pkgsCross mkShell lib;
+
   avrlibc = pkgsCross.avr.libcCross;
 
   avr_incflags = [
@@ -42,33 +24,45 @@ let
     "-L${avrlibc}/avr/lib/avr51"
   ];
 
-  # Builds the python env based on nix/pyproject.toml and
-  # nix/poetry.lock Use the "poetry update --lock", "poetry add
-  # --lock" etc. in the nix folder to adjust the contents of those
-  # files if the requirements*.txt files change
-  pythonEnv = poetry2nix.mkPoetryEnv {
-    projectDir = ./util/nix;
-    overrides = poetry2nix.overrides.withDefaults (self: super: {
-      pillow = super.pillow.overridePythonAttrs(old: {
-        # Use preConfigure from nixpkgs to fix library detection issues and
-        # impurities which can break the build process; this also requires
-        # adding propagatedBuildInputs and buildInputs from the same source.
-        propagatedBuildInputs = (old.buildInputs or []) ++ pkgs.python3.pkgs.pillow.propagatedBuildInputs;
-        buildInputs = (old.buildInputs or []) ++ pkgs.python3.pkgs.pillow.buildInputs;
-        preConfigure = (old.preConfigure or "") + pkgs.python3.pkgs.pillow.preConfigure;
-      });
-      qmk = super.qmk.overridePythonAttrs(old: {
-        # Allow QMK CLI to run "qmk" as a subprocess (the wrapper changes
-        # $PATH and breaks these invocations).
-        dontWrapPythonPrograms = true;
-      });
-    });
-  };
+  pythonEnv = let
+    python = pkgs.python3.override {
+      packageOverrides = self: super: {
+        # QMK isn't exposed as a Python module in nixpkgs, only as a top-level package.
+        # Convert it to a Python module and add it to the Python set so it can be used in the withPackages call.
+        qmk = ((self.toPythonModule pkgs.qmk).override { python3 = self.python; }).overrideAttrs(old: {
+          # Allow QMK CLI to run "qmk" as a subprocess (the wrapper changes
+          # $PATH and breaks these invocations).
+          dontWrapPythonPrograms = true;
+        });
+      };
+    };
+
+    project = pyproject-nix.lib.project.loadRequirementsTxt {
+      requirements = ./requirements-dev.txt;
+    };
+  in
+    # Assert that versions from nixpkgs matches what's described in requirements.txt
+    assert project.validators.validateVersionConstraints { inherit python; } == { }; (
+      # Render requirements.txt into a Python withPackages environment
+      python.withPackages (project.renderers.withPackages {
+        inherit python;
+        extraPackages = ps: [ ps.qmk ];
+      })
+    );
+
 in
 mkShell {
   name = "qmk-firmware";
 
-  buildInputs = [ clang-tools dfu-programmer dfu-util diffutils git pythonEnv niv ]
+  buildInputs = with pkgs; [
+    clang-tools
+    dfu-programmer
+    dfu-util
+    diffutils
+    git
+    pythonEnv
+    niv
+  ]
     ++ lib.optional avr [
       pkgsCross.avr.buildPackages.binutils
       pkgsCross.avr.buildPackages.gcc8
