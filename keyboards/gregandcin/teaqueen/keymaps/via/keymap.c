5349d757a998cfde18ec6f062347c3c9be1e1250
gregandcin <gregandcinbusiness@gmail.com>
2023-10-28 19:29:38 -0400

    Make sure keymap files conform with C standards

--- keyboards/gregandcin/teaqueen/keymaps/via/keymap.c
+++ keyboards/gregandcin/teaqueen/keymaps/via/keymap.c
@@ -7,16 +7,15 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _SYMB 1
-#define _NAV 2
-#define _ADJUST 3
+enum custom_layer {
+    _QWERTY,
+    _SYMB,
+    _NAV,
+    _ADJUST
+};
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  SYMB,
-  NAV,
-  ADJUST,
+  QWERTY = SAFE_RANGE
 };
 
 // Shortcut to make keymap more readable
@@ -31,14 +30,14 @@ enum custom_keycodes {
 #define KC_ADEN LT(_ADJUST, KC_END)
 #define KC_ADPU LT(_ADJUST, KC_PGUP)
 
-  
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
 		KC_ESC,  KC_1,    KC_2,  KC_3,    KC_4,  KC_5,   KC_6,                    KC_7,  KC_8,   KC_9,  KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-		KC_TAB,           KC_Q,  KC_W,    KC_E,  KC_R,   KC_T,                    KC_Y,  KC_U,   KC_I,  KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
-		KC_CAPS,          KC_A,  KC_S,    KC_D,  KC_F,   KC_G,                    KC_H,  KC_J,   KC_K,  KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, 
-		KC_LSFT,          KC_Z,  KC_X,    KC_C,  KC_V,   KC_B,                    MO(1), KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(2), 
+		KC_TAB,           KC_Q,  KC_W,    KC_E,  KC_R,   KC_T,                    KC_Y,  KC_U,   KC_I,  KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+		KC_CAPS,          KC_A,  KC_S,    KC_D,  KC_F,   KC_G,                    KC_H,  KC_J,   KC_K,  KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+		KC_LSFT,          KC_Z,  KC_X,    KC_C,  KC_V,   KC_B,                    MO(1), KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(2),
 		KC_LCTL, KC_LGUI,        KC_LALT,        KC_SPC, MO(1),                          KC_SPC,        KC_RALT,                   KC_RGUI, KC_RCTL),
 
 	[_SYMB] = LAYOUT(
