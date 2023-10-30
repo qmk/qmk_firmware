5f3cd581ead8a20c1c05e62ebce0d4b02d7b99c6
ai03 <admin@ai03.com>
2023-10-31 02:54:08 +0900

    Apply suggestions from code review
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/ai03/duet/config.h
+++ keyboards/ai03/duet/config.h
@@ -1,6 +1,6 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 1
 #define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 128
