eea9128535704da437e503d458d616cb943ac6d6
jotix <jujodeve@gmail.com>
2023-10-31 19:31:34 -0300

    fn layer

--- keyboards/handwired/jotlily60/keymaps/default/keymap.c
+++ keyboards/handwired/jotlily60/keymaps/default/keymap.c
@@ -10,8 +10,6 @@
 #define _QWERTY   0
 #define _FN       1
 
-#define FNLAYER MO(_FN)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -32,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
     KC_TAB, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
     KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,KC_RBRC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
-                    KC_LALT,KC_LGUI,KC_LCTL,FNLAYER,KC_SPC, KC_SPC, FNLAYER,KC_HOME,KC_END, KC_DEL
+                    KC_LALT,KC_LGUI,KC_LCTL,MO(_FN),KC_SPC, KC_SPC, MO(_FN),KC_HOME,KC_END, KC_DEL
 ),
 
 /* FN
@@ -56,4 +54,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     _______,_______,_______,_______,_______,_______,_______,KC_PGDN,KC_PGUP,_______
 )
 };
-  
+
