f613315e4883b9c5c8d7875826d1bedeadf1064b
John Barbero Unenge <git@lsrkttn.com>
2023-11-01 00:15:44 +0100

    [Keyboard] Add new keyboard SNES Macropad
    This change adds support for keyboard SNES Macropad. It's a 3x4
    macropad that features a SNES port, and exposes an additional 12
    "keys" through the connected controller buttons.
    
    There's hardware support for more exotic controllers (D1 and IO
    pins from the SNES port are connected to the RPi), but as of right
    now there's no software support.

---
+++ keyboards/snes_macropad/post_config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 John Barbero Unenge (@jbarberu)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#ifdef RGBLIGHT_ENABLE
+#    ifndef RGBLIGHT_LIMIT_VAL
+#        define RGBLIGHT_LIMIT_VAL 80
+#    endif
+#endif
