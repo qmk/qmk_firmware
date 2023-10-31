66862775f8d3cea0ea40315735dc2feca5ec02bb
anhdiepmmk <diep.pham@your.rentals>
2023-10-31 11:21:47 +0700

    feat(3w6/keymaps/anhdiepmmk): hold m to open f layer

--- keyboards/3w6/rev2/keymaps/anhdiepmmk/keymap.c
+++ keyboards/3w6/rev2/keymaps/anhdiepmmk/keymap.c
@@ -68,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * BASE
   */
   [_BASE] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    HO_A,    HO_S,    HO_D,    HO_F,    KC_G,         KC_H,    HO_J,    HO_K,    HO_L,    HO_SCLN,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                      TH_MED,  TH_NAV,  TH_MOU,       TH_SYM,  TH_NUM,  TH_FUN
+    KC_Q, KC_W, KC_E  , KC_R  , KC_T  ,      KC_Y  , KC_U         , KC_I   , KC_O  , KC_P,
+    HO_A, HO_S, HO_D  , HO_F  , KC_G  ,      KC_H  , HO_J         , HO_K   , HO_L  , HO_SCLN,
+    KC_Z, KC_X, KC_C  , KC_V  , KC_B  ,      KC_N  , LT(FUN, KC_M), KC_COMM, KC_DOT, KC_SLSH,
+                TH_MED, TH_NAV, TH_MOU,      TH_SYM, TH_NUM       , TH_FUN
   ),
 
   /* 
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PAGE_UP  , KC_HOME, KC_UP  , KC_END  , _______,      _______, KC_HOME, _______, KC_END  , KC_PAGE_UP  ,
     KC_PAGE_DOWN, KC_LEFT, KC_DOWN, KC_RIGHT, _______,      KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, KC_PAGE_DOWN,
     _______     , _______, _______, _______ , _______,      _______, _______, _______, _______ , _______     ,
-                      _______, _______ , _______,      _______, _______, _______
+                           _______, _______ , _______,      _______, _______, _______
   ),
   /* 
   * NUM
