1f72c1c3b10b5f8c1b4d5013076efbd57df4a496
ai03 <admin@ai03.com>
2023-11-01 09:46:22 +0900

    Update keyboards/ai03/duet/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/ai03/duet/keymaps/default/keymap.c
+++ keyboards/ai03/duet/keymaps/default/keymap.c
@@ -3,8 +3,6 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-	[0] = LAYOUT(
 	[0] = LAYOUT(
         KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,
         KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,
