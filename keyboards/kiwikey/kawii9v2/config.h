1f065d36dcf317df2013d8ead21371a51e7c12e8
KiwiKey <kiwikey.vn@gmail.com>
2023-11-02 17:18:10 +0700

    Update config.h

--- keyboards/kiwikey/kawii9v2/config.h
+++ keyboards/kiwikey/kawii9v2/config.h
@@ -14,6 +14,5 @@
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
-// Set default RGB Matrix mode
-#    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_UP_DOWN
+#    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_UP_DOWN // Set default RGB Matrix mode
 #endif
