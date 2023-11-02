0f6ab7c3a047a6b49cc8e7b01fb3cc79f7a1a6bf
jotix <jujodeve@gmail.com>
2023-11-02 11:52:35 -0300

    default keymap

--- keyboards/handwired/jotlily60/keymaps/default/keymap.c
+++ keyboards/handwired/jotlily60/keymaps/default/keymap.c
@@ -9,10 +9,23 @@
 
 #define _QWERTY   0
 #define _FN       1
+#define _ADJUST   2
 
-#define SPC_FN  LT(_FN, KC_SPC)
-#define EN_LSFT LSFT_T(KC_ENT)
-#define EN_RSFT RSFT_T(KC_ENT)
+#define EN_LSFT LSFT_T(KC_ENT)  // Tap: Enter - Hold: Lesft Shift
+#define F13_LCT LCTL_T(KC_F13)  // Tap: F13   - Hold: Left Control
+#define F14_LSG LSG_T(KC_F14)   // Tap: F14   - Hold: Left Shift+Super
+#define F15_LAL LALT_T(KC_F15)  // Tap: F15   - Hold: Left Alt
+#define DEL_LGU LGUI_T(KC_DEL)  // Tap: Del   - Hold: Left Super
+#define SPC_FN  LT(_FN, KC_SPC) // Tap: Space - Hold: FN
+#define DEL_RGU LGUI_T(KC_DEL)  // Tap: Del   - Hold: Right Super
+#define F16_RAL RALT_T(KC_F16)  // Tap: F16   - Hold: Right Alt
+#define F17_RSG RSG_T(KC_F17)   // Tap: F17   - Hold: Right Shift+Super
+#define F18_RCT RCTL_T(KC_F18)  // Tap: F18   - Hold: Right Control
+#define EN_RSFT RSFT_T(KC_ENT)  // Tap: Enter = Hold: Right Shift
+
+#define ADJUST MO(ADJUST)
+
+#define SFT_TAB LSFT(TAB) // Left Shift + Tab
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -24,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
  * |  tab  |   A   |   S   |   D   |   F   |   G   |               |   H   |   J   |   K   |   L   |   ;   |   '   |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * |lsh_ent|   Z   |   X   |   C   |   V   |   B   |   [   |   ]   |   N   |   M   |   ,   |   .   |   /   |rsh_ent|
+ * |en_lsft|   Z   |   X   |   C   |   V   |   B   |   [   |   ]   |   N   |   M   |   ,   |   .   |   /   |en_rsft|
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- *                 | lctrl | lsuper| lalt  |  del  | fn_spc| fn_spc|  del  |  ralt |  rgui | rctl  |
+ *                 |f13_lct|f14_lgu|f15_lal|del_lsg| spc_fn| spc_fn|del_rsg|f16_ral|f17_rgu|f18_rct|
  *                 +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  */
 [_QWERTY] = LAYOUT (
@@ -34,28 +47,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
     KC_TAB, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
     EN_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,KC_RBRC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,EN_RSFT,
-                    KC_LCTL,KC_LGUI,KC_LALT,KC_DEL ,SPC_FN, SPC_FN, KC_DEL, KC_RALT,KC_RGUI,KC_RCTL
+                    F13_LCT,F14_LSG,F15_LAL,DEL_LGU,SPC_FN, SPC_FN, DEL_RGU,F16_RAL,F17_RGU,F17_RCT
 ),
 
 /* FN
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
  * |  F12  |   F1  |   F2  |   F3  |   F4  |   F5  |               |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
- * |       | Btn1  |MouseUp| Btn2  | WhUp  | WhLeft|               |  Ins  |  Caps | PScr  |  Slck | Pause |       |
+ * |adjust | Btn1  |MouseUp| Btn2  | WhUp  | WhLeft|               |  Ins  |  Caps | PScr  |  Slck | Pause |adjust |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
- * |       |MouseL |MouseDn|MouseR | WhDown| WhRght|               |  Left |  Down |   Up  | Right |   |   |   `   |
+ * |lst_tab|MouseL |MouseDn|MouseR | WhDown| WhRght|               |  Left |  Down |   Up  | Right |   |   |   `   |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * |       | Acc0  | Acc1  | Acc2  | Vol-  | Mute  | Vol+  |   ~   |  Home |  End  | PgUp  | PgDn  |   \   |       |
+ * | caps  | Acc0  | Acc1  | Acc2  | Vol-  | Mute  | Vol+  |   ~   |  Home |  End  | PgUp  | PgDn  |   \   | caps  |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- *                 |       |       |       |       |       |       |       |       | Menu  |       |
+ *                 |       |       |       |       |       |       |       |       |       |       |
  *                 +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  */
 [_FN] = LAYOUT (
     KC_F12, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
-    _______,KC_BTN1,KC_MS_U,KC_BTN2,KC_WH_U,AS_TOG,                _______,_______,KC_PSCR,KC_SCRL,KC_PAUS,_______,
-    _______,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,KC_WH_R,                KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_PIPE,KC_GRV,
-    _______,KC_ACL0,KC_ACL1,KC_ACL2,KC_VOLD,KC_MUTE,KC_VOLU,KC_TILD,KC_HOME,KC_END, KC_PGUP,KC_PGDN,KC_BSLS,_______,
-                    _______,_______,_______,_______,_______,_______,_______,_______,KC_APP,_______
-)
+    ADJUST, KC_BTN1,KC_MS_U,KC_BTN2,KC_WH_U,DM_PLY1,                KC_INS, KC_APP, KC_PSCR,KC_SCRL,KC_PAUS,ADJUST,
+    LST_TAB,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,DM_PLY2,                KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_PIPE,KC_GRV,
+    KC_CAPS,KC_ACL0,KC_ACL1,KC_ACL2,KC_VOLD,KC_MUTE,KC_VOLU,KC_TILD,KC_HOME,KC_END, KC_PGUP,KC_PGDN,KC_BSLS,KC_CAPS,
+                    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______
+),
+
+[_ADJUST] = LAYOUT (
+    _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
+    _______,QK_BOOT,_______,_______,_______,DM_REC1,                _______,_______,_______,_______,_______,_______,
+    _______,_______,_______,DB_TOGG,_______,DM_REC2,                _______,_______,_______,_______,_______,_______,
+    AS_TOGG,AS_UP,  AS_DOWN,AS_RPT, _______,DM_RSTP,_______,_______,_______,_______,_______,_______,_______,_______,
+                    DT_UP,  DT_DOWN,DT_PRNT,_______,_______,_______,_______,_______,_______,_______
+),
 };
 
