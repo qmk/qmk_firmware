212a29f301a894de105ab6f53c4f0650cace823c
Marek128b <58476072+Marek128b@users.noreply.github.com>
2023-11-02 10:10:46 +0000

    deleted rgb.h rgb.c moved to keymap.c

--- keyboards/handwired/marek128b/ergosplit44/keymaps/default/keymap.c
+++ keyboards/handwired/marek128b/ergosplit44/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "rgb.h"
+#include "quantum.h"
 
 enum layer_names {
     _QWERTY,
@@ -53,3 +53,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______,    _______,    _______,    _______,                _______,    _______,    _______,    _______
         ),
 };
+
+
+bool rgb_matrix_indicators_kb(void) {
+    if (!rgb_matrix_indicators_user()) {
+        return false;
+    }
+    if (host_keyboard_led_state().caps_lock) 
+	{
+        rgb_matrix_set_color(0, RGB_RED);
+    }
+	
+	if (host_keyboard_led_state().num_lock) 
+	{
+        rgb_matrix_set_color(1, RGB_RED);
+    }
+
+	if (host_keyboard_led_state().scroll_lock) 
+	{
+        rgb_matrix_set_color(2, RGB_RED);
+    }
+	
+	switch (get_highest_layer(layer_state))
+	{
+        case 1: 
+            rgb_matrix_set_color(3, RGB_CYAN);
+        break;
+        case 2: 
+            rgb_matrix_set_color(3, RGB_MAGENTA);
+        break;
+        case 3:
+            rgb_matrix_set_color(3, RGB_YELLOW);
+        break;
+    } 
+    return true;
+}
--- keyboards/handwired/marek128b/ergosplit44/rgb.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2023 @Marek128b
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-bool rgb_matrix_indicators_kb(void); //show lock states and layer states with rgb
