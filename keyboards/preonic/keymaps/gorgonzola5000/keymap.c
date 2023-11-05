d210eba8b7591c26f8b659725f47b6eb62785398
gorgonzola5000 <gorgonzola5000@outlook.com>
2023-11-05 18:38:03 +0100

    added backspace+shift = delete macro

--- keyboards/preonic/keymaps/gorgonzola5000/keymap.c
+++ keyboards/preonic/keymaps/gorgonzola5000/keymap.c
@@ -11,40 +11,53 @@ enum layer_names {
 	_F_KEYS
 };
 
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//     switch (keycode) {
-//     case RELEASE:
-//         if (record->event.pressed) {
-// 			uint8_t mods = 0;
-// 			if ((mods = get_oneshot_mods()) && !has_oneshot_mods_timed_out()) {
-// 				clear_oneshot_mods();
-// 				unregister_mods(mods);
-// 				layer_clear();
-// 			}
-// 			if ((mods = get_oneshot_locked_mods())) {
-// 				clear_oneshot_locked_mods();
-// 				unregister_mods(mods);
-// 				layer_clear();
-// 			}
-//         }
-		
-//         break;
-//     }
-// 	return true;
-// };
+uint8_t mod_state;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == RELEASE && record->event.pressed) {
-        uint8_t mods = 0;
-        if ((mods = get_oneshot_mods()) && !has_oneshot_mods_timed_out()) {
-            clear_oneshot_mods();
-			unregister_mods(mods);
-        }
-        if ((mods = get_oneshot_locked_mods())) {
-            clear_oneshot_locked_mods();
-            unregister_mods(mods);
-        }
-		layer_clear();
-    }
+	mod_state = get_mods();
+	switch(keycode){
+		case RELEASE: // disable OSMs and switch to default layer
+			if (record->event.pressed) {
+				uint8_t mods = 0;
+				if ((mods = get_oneshot_mods()) && !has_oneshot_mods_timed_out()) {
+					clear_oneshot_mods();
+					unregister_mods(mods);
+				}
+				if ((mods = get_oneshot_locked_mods())) {
+					clear_oneshot_locked_mods();
+					unregister_mods(mods);
+				}
+				layer_clear();
+			}
+		case KC_BSPC: // backspace + shift = delete
+			{
+			// Initialize a boolean variable that keeps track
+			// of the delete key status: registered or not?
+			static bool delkey_registered;
+			if (record->event.pressed) {
+				// Detect the activation of either shift keys
+				if (mod_state & MOD_MASK_SHIFT) {
+					// First temporarily canceling both shifts so that
+					// shift isn't applied to the KC_DEL keycode
+					del_mods(MOD_MASK_SHIFT);
+					register_code(KC_DEL);
+					// Update the boolean variable to reflect the status of KC_DEL
+					delkey_registered = true;
+					// Reapplying modifier state so that the held shift key(s)
+					// still work even after having tapped the Backspace/Delete key.
+					set_mods(mod_state);
+					return false;
+				}
+				} else { // on release of KC_BSPC
+					// In case KC_DEL is still being sent even after the release of KC_BSPC
+					if (delkey_registered) {
+						unregister_code(KC_DEL);
+						delkey_registered = false;
+						return false;
+					}
+				}
+			}
+		}
     return true;
 }
 
