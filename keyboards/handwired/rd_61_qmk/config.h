6ad84f15adfce6c2604f62b5345a5a475667e2af
Abhijithabhi <abhiabhijith367@gmail.com>
2023-11-06 17:47:47 +0530

    Update keyboards/handwired/rd_61_qmk/config.h
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/handwired/rd_61_qmk/config.h
+++ keyboards/handwired/rd_61_qmk/config.h
@@ -8,12 +8,4 @@
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
-/* prevent stuck modifiers */
-// #define PREVENT_STUCK_MODIFIERS
-
 #endif
