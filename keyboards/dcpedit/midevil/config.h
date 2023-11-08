cc43236e3078902c55f9e36f06e035e047e72421
dcpedit <dcpedit@hotmail.com>
2023-11-08 15:19:17 -0800

    Allow backlight feature to control LCD brightness

--- keyboards/dcpedit/midevil/config.h
+++ keyboards/dcpedit/midevil/config.h
@@ -7,7 +7,11 @@
 #define LCD_DC_PIN GP16   // data/command
 #define LCD_CS_PIN GP17   // chip select
 #define LCD_RST_PIN GP14  // reset
-#define LCD_LED_PIN GP15  // backlight
+#define BACKLIGHT_PIN GP15
+#define BACKLIGHT_PWM_DRIVER PWMD7
+#define BACKLIGHT_PWM_CHANNEL RP2040_PWM_CHANNEL_B
+#define BACKLIGHT_LEVELS 5
+#define BACKLIGHT_DEFAULT_LEVEL 4
 
 /* SPI pins */
 #define SPI_DRIVER SPID0
