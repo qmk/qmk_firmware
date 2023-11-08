50193d35689682173b668e9c398c3038282a50eb
jotix <jujodeve@gmail.com>
2023-11-07 21:07:23 -0300

    default layout

--- keyboards/handwired/jotlily60/keymaps/default/keymap.c
+++ keyboards/handwired/jotlily60/keymaps/default/keymap.c
@@ -20,40 +20,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
  * |   =   |   1   |   2   |   3   |   4   |   5   |               |   6   |   7   |   8   |   9   |   0   |   -   |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
- * |  esc  |   Q   |   W   |   E   |   R   |   T   |               |   Y   |   U   |   I   |   O   |   P   |   \   |
+ * |  esc  |   Q   |   W   |   E   |   R   |   T   |               |   Y   |   U   |   I   |   O   |   P   |  tab  |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
- * |  tab  |   A   |   S   |   D   |   F   |   G   |               |   H   |   J   |   K   |   L   |   ;   |   '   |
+ * | caps  |   A   |   S   |   D   |   F   |   G   |               |   H   |   J   |   K   |   L   |   ;   |   '   |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * | shift |   Z   |   X   |   C   |   V   |   B   |   [   |   ]   |   N   |   M   |   ,   |   .   |   /   | enter |
+ * | shift |   Z   |   X   |   C   |   V   |   B   |   {   |   }   |   N   |   M   |   ,   |   .   |   /   | enter |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- *                 | super |  alt  |  ctrl | space |   fn  |   fn  | space |  del  |  ralt | bspc  | 
+ *                 |  alt  | super |  ctrl | space |   fn  |   fn  | bspc  |  del  |  meh  |  ralt |
  *                 +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  */
 [_QWERTY] = LAYOUT (
     KC_EQL, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
-    KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,
-    KC_TAB, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-    KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,KC_RBRC,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
-                    KC_LGUI,KC_LALT,KC_LCTL,KC_SPC, FN,     FN,     KC_SPC, KC_DEL, KC_RALT,KC_BSPC
+    KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_TAB,
+    KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
+    KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LCBR,KC_RCBR,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
+                    KC_LALT,KC_LGUI,KC_LCTL,KC_SPC, FN,     FN,     KC_BSPC,KC_DEL, KC_MEH, KC_RALT
 ),
 
 /* +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
- * |   +   |   F1  |   F2  |   F3  |   F4  |   F5  |               |   F6  |   F7  |   F8  |   F9  |  F10  |   _   |
+ * |   +   |   !   |   @   |   #   |   $   |   %   |               |   ^   |   &   |   *   |   (   |   )   |   _   |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
- * | adjust|  F11  |  F12  |  F13  |  F14  |  F15  |               |  F16  |  F17  |  F18  |  F19  |  F20  |   |   |
+ * |adjust |   F1  |   F2  |   F3  |   F4  |   F5  |               |   F6  |   F7  |   F8  |   F9  |  F10  |       |
  * +-------+-------+-------+-------+-------+-------+               +-------+-------+-------+-------+-------+-------+
- * |  caps |  vol- |  mute |  vol+ | macro1| macro2|               |  Left |  Down |   Up  | Right |  pscr |  grv  |
+ * |       |  F11  |  F12  |  F13  |  F14  |  F15  |               |  left |  down |   up  | right |   |   |   "   |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
- * |       |  prev |  play |  next | mrec1 | mrec2 | rstp  |   ~   |  Home |  End  | PgUp  | PgDn  |  ins  |       |
+ * |       |  vol- |  mute |  vol+ |   `   |   ~   |   [   |   ]   |  home |  end  | pgup  | pgdn  |   \   |       |
  * +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  *                 |       |       |       |       |       |       |       |       |       |       |
  *                 +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
  */
 [_FN] = LAYOUT (
-    KC_PLUS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_UNDS,
-    ADJUST, KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                 KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_PIPE,  
-    KC_CAPS,KC_VOLD,KC_MUTE,KC_VOLU,DM_PLY1,DM_PLY2,                KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_PSCR,KC_GRV,
-    _______,KC_MPRV,KC_MPLY,KC_MNXT,DM_REC1,DM_REC2,DM_RSTP,KC_TILD,KC_HOME,KC_END, KC_PGUP,KC_PGDN,KC_INS, _______, 
+    KC_PLUS,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_UNDS,
+    ADJUST, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,  
+    _______,KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                 KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_BSLS,KC_DQUO,
+    _______,KC_VOLD,KC_MUTE,KC_VOLU,KC_GRV, KC_TILD,KC_LBRC,KC_RBRC,KC_HOME,KC_END, KC_PGUP,KC_PGDN,KC_PIPE,_______, 
                     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______
 ),
 
