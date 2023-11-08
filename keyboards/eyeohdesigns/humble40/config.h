908f27f4e7e86b0ec85989cd75ae37592a0507ca
honorless <86894501+lesshonor@users.noreply.github.com>
2023-11-08 16:59:44 -0500

    refactor: eyeohdesigns/humble40

--- keyboards/eyeohdesigns/humble40/config.h
+++ keyboards/eyeohdesigns/humble40/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 sekigon-gonnoc
+Copyright 2022 Eye Oh Designs
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,47 +17,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
-#define DEVICE_VER   0x0001
-#define MANUFACTURER eye oh designs
-#define PRODUCT      humble40
-
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 12
-
-#define DEBUG_MATRIX_SCAN_RATE
-#define DEBUG_ACTION
-
-#define MATRIX_ROW_PINS { 29, 20, 10, 12 }
-#define MATRIX_COL_PINS { 19, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
-#define UNUSED_PINS
-
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-#define BACKLIGHT_PIN 28
-#define BACKLIGHT_LEVELS 3
-//#define BACKLIGHT_BREATHING
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-
-
-/* disable these deprecated features by default */
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
-
-/* Bootmagic Lite key configuration */
-//#define BOOTMAGIC_LITE_ROW 0
-//#define BOOTMAGIC_LITE_COLUMN 0
+#define BACKLIGHT_PWM_DRIVER PWMD6
+#define BACKLIGHT_PWM_CHANNEL RP2040_PWM_CHANNEL_A
