d7f2df8bfa0bbb1c99f5e4d9d303541d86945e77
zvecr <git@zvecr.com>
2023-11-10 06:10:59 +0000

    Remove unnecessary driver counts

--- keyboards/fallacy/config.h
+++ keyboards/fallacy/config.h
@@ -19,7 +19,6 @@
  * Using the default defines here, but using a custom implementation
  */
 #define IS31FL3731_I2C_ADDRESS_1 IS31FL3731_I2C_ADDRESS_GND
-#define IS31FL3731_DRIVER_COUNT 1
 #define IS31FL3731_LED_COUNT 3
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
