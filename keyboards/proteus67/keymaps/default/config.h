36e3df4d72b14383e972c35038c50974f9b35444
gzowski <flexyjerkov@gmail.com>
2023-11-10 21:36:09 +0000

    Update keyboards/proteus67/keymaps/default/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/proteus67/keymaps/default/config.h
+++ keyboards/proteus67/keymaps/default/config.h
@@ -1,23 +1,6 @@
-/*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-#define RGB_DISABLE_TIMEOUT 900000
-#define RGB_DISABLE_WHEN_USB_SUSPENDED 1
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_MAX_LAYERS 3
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
-#define ENCODER_DIRECTION_FLIP
