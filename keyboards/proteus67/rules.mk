b7520ce5b7add183fec9d16f72cd938a7f1b04b4
gzowski <flexyjerkov@gmail.com>
2023-11-10 21:37:10 +0000

    Update keyboards/proteus67/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/proteus67/rules.mk
+++ keyboards/proteus67/rules.mk
@@ -1,5 +1,1 @@
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-COMMAND_ENABLE = yes
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-BOOTMAGIC_ENABLE = yes
+# This file intentionally left blank
