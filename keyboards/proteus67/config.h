209390b6021003055ad1cbae430ceaa6e4180fc3
gzowski <flexyjerkov@gmail.com>
2023-11-10 13:53:29 +0000

    Add files via upload

--- keyboards/proteus67/config.h
+++ keyboards/proteus67/config.h
@@ -21,10 +21,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ENCODER_DIRECTION_FLIP
 #define ENCODER_MAP_KEY_DELAY 10
 #define ENCODER_DEFAULT_POS 0x3
-#define RGB_DISABLE_TIMEOUT 900000 // milliseconds
-#define RGB_DISABLE_WHEN_USB_SUSPENDED 1
-#define RGBLIGHT_LAYERS
-#define RGBLIGHT_MAX_LAYERS 3
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
-#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
