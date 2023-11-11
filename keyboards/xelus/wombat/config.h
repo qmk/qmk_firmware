88e4aaa7e3efadc2af6b47ff0ede3eb4ec6c1b67
Xelus22 <preyas22@gmail.com>
2023-11-11 16:42:13 +1100

    fix up

--- keyboards/xelus/wombat/config.h
+++ keyboards/xelus/wombat/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 Harrison Chan (Xelus)
+/* Copyright 2023 Harrison Chan (Xelus)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,27 +16,8 @@
 
 #pragma once
 
-/* key matrix size */
-#define MATRIX_ROWS 18
-#define MATRIX_COLS 14
-
-#define MATRIX_COL_PINS { C11, C12, D0, D1, B10, E15, E14, E13, E12, E11, A13, A1,  C5, C4 }
-#define MATRIX_ROW_PINS { B6,  B5,  B3, B4, D6,  D7,  D4,  D5,  D2,  D3,  A15, C10, A3, A2, B1, B0, B2, E7 }
-#define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE    5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-// Encoders
-#define ENCODERS_PAD_A { B11, B13, B15, D9,  D11, D13, D15, C7, C9, A9,  B7, B9, E1, E3, E5, C13, C15, H1, C1, C3 }
-#define ENCODERS_PAD_B { B12, B14, D8,  D10, D12, D14, C6,  C8, A8, A10, B8, E0, E2, E4, E6, C14, H0,  C0, C2, A0 }
-#define ENCODER_RESOLUTION 4
-
+// SPI EEPROM left in because I couldn't get it working
+// So using Wear-leveling for now
 // SPI Setup
 #define SPI_DRIVER        SPID1
 #define SPI_SCK_PIN       A5
@@ -54,7 +35,6 @@
 #define EXTERNAL_EEPROM_ADDRESS_SIZE         3
 
 // Dynamic EEPROM
-#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 32767
 
 // Force NKRO
 #define FORCE_NKRO
--- keyboards/xelus/wombat/wombat.c
+++ keyboards/xelus/wombat/wombat.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Harrison Chan (Xelus)
+/* Copyright 2023 Harrison Chan (Xelus)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,12 +14,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "wombat.h"
-
 void matrix_io_delay(void) { __asm__ volatile("nop\nnop\nnop\n"); }
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_keyboard=true;
-}
