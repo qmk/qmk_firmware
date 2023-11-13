cfc8541f068a4ad72a08f24251f3d24d674a5dfc
mechlovin <57231893+mechlovin@users.noreply.github.com>
2023-11-13 08:24:35 +0700

    Update keyboards/mechlovin/zed1800/saber/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/mechlovin/zed1800/saber/keymaps/via/keymap.c
+++ keyboards/mechlovin/zed1800/saber/keymaps/via/keymap.c
@@ -26,29 +26,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                             KC_UP,      KC_P1,   KC_P2,   KC_P3,             //16
         KC_LCTL, KC_LGUI, KC_LALT,                                     KC_SPC,                             KC_RALT, KC_RCTL,           KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,   KC_PDOT, KC_PENT   //12 
     ),
-    [1] = LAYOUT_ansi(
-        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //18
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,             //16
-        KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS,                            KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS   //12 
-    ),
-    [2] = LAYOUT_ansi(
-        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //18
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,             //16
-        KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS,                            KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS   //12 
-    ),
-    [3] = LAYOUT_ansi(
-        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //18
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //19
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,             //16
-        KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS,                            KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS   //12 
-    )
 
 };
