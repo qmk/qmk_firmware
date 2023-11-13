40d9d0d8d0ec7fed85b8e8023d7cbd471762f6e7
Joao Silva <joaosilvatm@hotmail.com>
2023-11-13 00:53:38 +0000

    Copied files from sofle, needs edit
    Copied files from a similar keyboard so I could get the structure, still need to edit to my specific keyboard.

--- keyboards/ghost54/rules.mk
+++ keyboards/ghost54/rules.mk
@@ -1,1 +1,5 @@
 SERIAL_DRIVER = vendor
+#CAPS_WORD_ENABLE = yes #https://docs.qmk.fm/#/feature_caps_word
+COMBO_ENABLE = yes #https://docs.qmk.fm/#/feature_combo
+ENCODER_ENABLE = yes #https://docs.qmk.fm/#/feature_encoders
+OLED_ENABLE = yes
