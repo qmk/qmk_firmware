8f4091c994648292526c734ce2758ad4504afc1e
Nicola Lombardi <lombardi.nicola23@gmail.com>
2023-11-14 17:16:27 +0100

    added numbers layer, some refactoring, increased debounce, volume up down

--- keyboards/xiudi/xd60/keymaps/nicola_lombardi/keymap.c
+++ keyboards/xiudi/xd60/keymaps/nicola_lombardi/keymap.c
@@ -20,6 +20,7 @@
 #define L_ARROWS_SYMBOLS 2
 #define L_EXTRA 3
 #define L_GAMING_FN 4
+#define L_NUMBERS 5
 
 // TAP DANCES
 enum {
@@ -145,22 +146,22 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_ACCENTED_U] = ACTION_TAP_DANCE_FN(accented_letter_u),
 
     // Double tap shift to get caps. Both sides
-    [TD_LEFT_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-    [TD_RIGHT_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
+    // [TD_LEFT_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    // [TD_RIGHT_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // 0: Base Layer
-    LAYOUT_split_spacebar(
-        KC_ESC,   KC_1,     KC_2,      KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,     KC_0,     KC_NO,    KC_NO,   KC_BSPC,
+    [L_BASE] = LAYOUT_split_spacebar(
+        KC_ESC,   KC_1,     KC_2,      KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,     KC_0,     KC_VOLD,    KC_VOLU,   KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,      KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,     KC_P,     KC_NO,    KC_NO,   KC_BSLS,
         KC_CAPS,  KC_A,     KC_S,      KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,   KC_ENT,
         KC_LSFT,  KC_NO,    KC_Z,      KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,   KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LGUI,  KC_LALT,       MO(L_ARROWS_SYMBOLS),   KC_SPC,      KC_RCTL,  MO(L_EXTRA),  KC_DEL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     // 1: Gaming layer
-    LAYOUT_split_spacebar(
+    [L_GAMING] = LAYOUT_split_spacebar(
         KC_ESC,  KC_1,    KC_2,     KC_3,  KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,     KC_E,  KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,
         KC_CAPS, KC_A,    KC_S,     KC_D,  KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,  KC_NO,   KC_ENT,
@@ -168,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,         KC_SPC,    MO(L_GAMING_FN),     KC_RCTL, KC_RALT,   KC_DEL,  KC_LEFT, KC_DOWN,  KC_TGRT),
 
     // 2: Function Layer
-    LAYOUT_split_spacebar(
+    [L_ARROWS_SYMBOLS] = LAYOUT_split_spacebar(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC,
         KC_TAB, KC_RBRC, KC_RCBR, KC_LCBR, KC_LBRC, KC_F12, KC_NO, KC_HOME, KC_UP, KC_END, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_CAPS, KC_CSL, KC_NO, KC_EQL, KC_MINS, KC_PLUS, KC_UNDS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_MINS, KC_EQL, KC_NO, KC_ENT,
@@ -176,7 +177,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,       KC_TRNS, KC_SPC,         KC_RCTL, KC_NO,  KC_DEL,  KC_LEFT, KC_DOWN, KC_TGRT),
 
     // 3: Extra functions layer and accented letters
-    LAYOUT_split_spacebar(
+    [L_EXTRA] = LAYOUT_split_spacebar(
         QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_EACC, KC_NO, KC_NO, KC_NO, KC_UACC, KC_IACC, KC_OACC, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_AACC, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO,
@@ -185,20 +186,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     // 4: Function Layer but for gaming mode (no brackets, equal, dash, etc.)
-    LAYOUT_split_spacebar(
+    [L_GAMING_FN] = LAYOUT_split_spacebar(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,     KC_E,  KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,
         KC_CAPS, KC_A,    KC_S,     KC_D,  KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,  KC_NO,   KC_ENT,
         KC_LSFT, KC_NO,   KC_Z,     KC_X,  KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT, KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT,       KC_TRNS, KC_SPC,         KC_RCTL, KC_NO,  KC_DEL,  KC_LEFT, KC_DOWN, KC_TGRT),
 
-    // 5: Minecraft 
-    LAYOUT_split_spacebar(
-        KC_ESC,  KC_1,    KC_2,     KC_F3,  KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,     KC_E,  KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,
-        KC_CAPS, KC_A,    KC_S,     KC_D,  KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,  KC_NO,   KC_ENT,
-        KC_LSFT, KC_NO,   KC_Z,     KC_X,  KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT, KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT,         KC_SPC,    MO(L_GAMING_FN),     KC_RCTL, KC_RALT,   KC_DEL,  KC_LEFT, KC_DOWN,  KC_TGRT),
+    // 5: top row numbers
+    [L_NUMBERS] = LAYOUT_split_spacebar(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_UACC, KC_IACC, KC_OACC, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO,         KC_NO, KC_TRNS,  KC_NO,  KC_NO, KC_NO, KC_NO),
 
-    
+
 };
--- keyboards/xiudi/xd60/rev3/config.h
+++ keyboards/xiudi/xd60/rev3/config.h
@@ -45,7 +45,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define DIODE_DIRECTION COL2ROW
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
+#define DEBOUNCE 7
 
 /* RGB Underglow
  * F6 PIN for XD60v3 that has pre-soldered LEDs
