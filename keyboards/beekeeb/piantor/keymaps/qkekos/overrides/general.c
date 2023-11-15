d739eaa0a8778ced641ca3269b6814a7d0dcc496
QKekos <q.kekos.q@gmail.com>
2023-11-15 08:29:40 +0200

    fixed NUM only override

--- keyboards/beekeeb/piantor/keymaps/qkekos/overrides/general.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/overrides/general.c
@@ -9,6 +9,6 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &ko_make_basic(MOD_MASK_SHIFT, KC_F2, HK_RLD),
     &ko_make_basic(MOD_MASK_SHIFT, KC_CAPS, HK_GSRC),
 
-    &ko_make_with_layers(MOD_MASK_SHIFT, KC_ENT, KC_ENT, ~NUM),
+    &ko_make_with_layers(MOD_MASK_SHIFT, KC_ENT, KC_ENT, 1 << NUM),
     NULL
 };
