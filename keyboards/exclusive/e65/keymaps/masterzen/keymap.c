4c9ed3288d8b2d20cc2132588582bae1d912f492
Duncan Sutherland <dunk2k_2000@hotmail.com>
2023-11-17 10:23:06 +0000

    reversions based on #22485

--- keyboards/exclusive/e65/keymaps/masterzen/keymap.c
+++ keyboards/exclusive/e65/keymaps/masterzen/keymap.c
@@ -31,7 +31,7 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [BASE] = LAYOUT_65_ansi_blocker_split_bs(
+    [BASE] = LAYOUT_65_ansi_blocker_splitbs(
         KC_ESC, KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, LT(_ADJUST,KC_INS),
         KC_TAB, KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,
 
@@ -39,14 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT,KC_SLSH,         KC_RSFT, KC_UP, KC_DEL,
         KC_LCTL, KC_LGUI, KC_LALT,                  KC_SPC,                      KC_RALT, KC_RCTL,  KC_LEFT, KC_DOWN,KC_RGHT),
 
-    [OSX] = LAYOUT_65_ansi_blocker_split_bs(
+    [OSX] = LAYOUT_65_ansi_blocker_splitbs(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_LCTL, KC_LALT, KC_LGUI,                  _______,                             KC_LGUI, KC_RALT, _______, _______, _______),
 
-    [_ADJUST] = LAYOUT_65_ansi_blocker_split_bs(
+    [_ADJUST] = LAYOUT_65_ansi_blocker_splitbs(
         QK_BOOT, LAY_LIN, LAY_OSX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, MO(_ADJUST),
         BL_TOGG, BL_DOWN,BL_UP,  KC_LGUI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, _______,
