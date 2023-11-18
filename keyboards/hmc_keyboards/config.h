66b58bf2f4562e37172d9a09a79eacf59aeda9b1
Hiiwwy <180374520@qq.com>
2023-11-18 22:24:30 +0800

    提交hmc_keyboards文件夹

---
+++ keyboards/hmc_keyboards/config.h
@@ -0,0 +1,141 @@
+
+
+#pragma once
+/* Includes ------------------------------------------------------------------*/
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID                       0x0903  /* <ldq> */
+#define PRODUCT_ID                      0x0220  /* <cjl> */
+#define DEVICE_VER                      0x0001
+#define MANUFACTURER                    HMC
+#define PRODUCT                         Enter80_for HMC
+#define DESCRIPTION                     Enter80_for HMC
+#define SERIAL_NUMBER                   00000000/* <序列号> */
+#define BLE_KEYBOARD_NAME               "Enter80"
+/* #define BLE_KEYBOARD_NAME            "43 980 RGB" */
+
+
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 17
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS {               \
+                        LINE_ROW0,      \
+                        LINE_ROW1,      \
+                        LINE_ROW2,      \
+                        LINE_ROW3,      \
+                        LINE_ROW4,      \
+                        LINE_ROW5       \
+                        }
+
+#define MATRIX_COL_PINS {               \
+                        LINE_COL0,      \
+                        LINE_COL1,      \
+                        LINE_COL2,      \
+                        LINE_COL3,      \
+                        LINE_COL4,      \
+                        LINE_COL5,      \
+                        LINE_COL6,      \
+                        LINE_COL7,      \
+                        LINE_COL8,      \
+                        LINE_COL9,      \
+                        LINE_COL10,     \
+                        LINE_COL11,     \
+                        LINE_COL12,     \
+                        LINE_COL13,     \
+                        LINE_COL14,     \
+                        LINE_COL15,     \
+                        LINE_COL16      \
+                        }
+#define UNUSED_PINS
+
+        /**<   二极管方向  */
+#define DIODE_DIRECTION COL2ROW
+
+        /**<   去抖延时  */
+#define DEBOUNCING_DELAY 5
+
+        /**<   宏数量  */
+#define DYNAMIC_KEYMAP_MACRO_COUNT 15
+        /**<   Flash模拟eeprom  */
+#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR  5120-1
+
+        /**<   VIA 按键层数量  */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+//#define VIA_EEPROM_CUSTOM_CONFIG_SIZE 100
+
+#define VIA_CUSTOM_LIGHTING_ENABLE
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+#define RGB_DI_PIN           LINE_WS2812
+#define RGBLED_TOTAL         2    //RGB灯的总数
+#define RGBLED_KEY_LED       1       //轴灯的数量
+#define DRIVER_LED_TOTAL     RGBLED_TOTAL
+#define RGB_BOTTOM_LED_NUM   (RGBLED_TOTAL-RGBLED_KEY_LED)
+#define RGBLED_NUM           (RGB_BOTTOM_LED_NUM)
+
+
+#define RGBLIGHT_ANIMATIONS
+#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#define RGB_MATRIX_LED_FLUSH_LIMIT     16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 160 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#define RGB_MATRIX_STARTUP_HUE        100 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_SAT        255 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_STARTUP_VAL        100 // Sets the default brightness value, if none has been set
+#define RGB_MATRIX_STARTUP_SPD        127 // Sets the default animation speed, if none has been set
+
+
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_BREATHE_MAX     255
+#define RGBLIGHT_EFFECT_KNIGHT_LENGTH   8
+#define RGBLIGHT_EFFECT_SNAKE_LENGTH    8
+#define RGBLIGHT_EFFECT_TWINKLE_LIFE    100
+#define RGBLIGHT_DISABLE_KEYCODES
+#define RGBLIGHT_EFFECT_TWINKLE
+#define RGBLIGHT_LIMIT_VAL              180
+
+
+/* <RGB指示灯设置> */
+#ifdef  LG_Indicator_Enable
+   #define  CAPSLK_Indicator  0     /* <大小写灯的序号> */
+   #define   NUMLK_Indicator  NO_LED  /* <NUMLK指示灯的序号> */
+   #define   SCRLK_Indicator  NO_LED 
+   #define   KANA_Indicator   NO_LED
+   #define COMPOSE_Indicator  NO_LED
+#endif
+
+
+#ifdef VIA_CUSTOM_LIGHTING_ENABLE
+/* <音乐律动相关参数设定> */
+    #define RGB_LED_COL_MAX  19
+    #define RGB_LED_ROW_MAX  6
+#endif
+
+
+//#define NO_USB_STARTUP_CHECK
+
---
+++ keyboards/hmc_keyboards/rules.mk
@@ -0,0 +1,35 @@
+# MCU name
+MCU = STM32F103
+BOARD = LG_Keyboard
+
+# Build Options
+#   comment out to disable the options.
+#
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+BOOTMAGIC_ENABLE    = yes      # Virtual DIP switch configuration
+MOUSEKEY_ENABLE     = yes      # Mouse keys
+EXTRAKEY_ENABLE     = yes      # Audio control and System control
+CONSOLE_ENABLE      = no      # Console for debug
+COMMAND_ENABLE      = yes      # Commands for debug and configuration
+NKRO_ENABLE         = yes      # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+MIDI_ENABLE         = no       # MIDI controls
+AUDIO_ENABLE        = no       # Audio output on port C6
+UNICODE_ENABLE      = no       # Unicode
+BACKLIGHT_ENABLE    = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE     = yes      # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE   = WS2812
+
+API_SYSEX_ENABLE    = no
+SLEEP_LED_ENABLE    = no       # Breathing sleep LED during USB suspend
+OLED_DRIVER_ENABLE  = no
+ALLOW_WARNINGS      = yes
+LG_Indicator_Enable = yes
+RGB_LG              = yes
+#WS2812_DRIVER       = pwm
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+# OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+
