836cdcd57b5168de9bce360103079dd3e1ce8cc3
Dominik Tarnowski <dom@tdom.dev>
2023-11-18 16:55:48 +0000

    updates

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -51,14 +51,14 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
   ALT_TAB,         KC_1,               KC_2,                 KC_3,                KC_4,                  KC_5,    KC_BRID,                   KC_BRIU,        KC_6,                  KC_7,                  KC_8,                      KC_9,                 KC_0,                   ALT_TAB2,
-  KC_TAB,          KC_Q,               KC_W,                 KC_F,                KC_P,                  KC_B,    KC_SLASH,                  KC_LGUI,        KC_J,                  KC_L,                  KC_U,                      KC_Y,                 KC_BACKSLASH,           KC_BSPC,
+  KC_TAB,          KC_Q,               KC_W,                 KC_F,                KC_P,                  KC_B,    KC_SLASH,                  KC_UP,          KC_J,                  KC_L,                  KC_U,                      KC_Y,                 KC_BACKSLASH,           KC_BSPC,
   KC_ESC,          KC_A,               KC_R,                 KC_S,                KC_T,                  KC_G,                                               KC_M,                  KC_N,                  KC_E,                      KC_I,                 KC_O,                   KC_ENT,
-  KC_LSFT,         KC_Z,               KC_X,                 KC_C,                KC_D,                  KC_V,    KC_GRAVE,                  ALL_T(KC_QUOT), KC_K,                  KC_H,                  KC_COMMA,                  KC_DOT,               KC_MINUS,      KC_RSFT,
-  KC_BTN2,         KC_TRNS,            KC_LALT,              KC_LGUI,             KC_LCTL,                                                                                          KC_LCTL,               KC_LGUI,                   KC_LCTL,              TG(SYMB),               KC_TRNS,
+  KC_LSFT,         KC_Z,               KC_X,                 KC_C,                KC_D,                  KC_V,    KC_GRAVE,                  KC_DOWN,        KC_K,                  KC_H,                  KC_COMMA,                  KC_DOT,               KC_MINUS,      KC_RSFT,
+  KC_BTN2,         LCTL(KC_B),         KC_LEFT,              KC_BTN2,             KC_BTN1,                                                                                          MO(SYMB),              KC_LGUI,                   KC_RIGHT,             TG(SYMB),               KC_TRNS,
 
-                                                         KC_BTN1,     KC_BTN2,                         KC_MPRV, KC_MNXT,
+                                                         LGUI(KC_D),  LGUI(KC_E),                      KC_MPRV, KC_MNXT,
                                                                       KC_HOME,                         KC_VOLU,
-                                                 KC_SPC, LCTL(KC_B),  KC_LGUI,                         KC_VOLD, KC_LSFT, MO(SYMB)
+                                                 KC_LCTL, KC_LGUI,    KC_LALT,                         KC_VOLD, KC_LSFT, KC_SPC
 ),
 
 [SYMB] = LAYOUT_ergodox_pretty(
