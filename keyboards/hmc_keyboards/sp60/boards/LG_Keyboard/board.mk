e92294bc6420d3ced796ae221ebbc32fcf753aa2
Hiiwwy <180374520@qq.com>
2023-11-19 00:27:29 +0800

    更改

--- keyboards/hmc_keyboards/sp60/boards/LG_Keyboard/board.mk
+++
@@ -1,8 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/boards/LG_Keyboard/board.c
-
-# Required include directories
-BOARDINC = $(BOARD_PATH)/boards/LG_Keyboard
-
-ALLCSRC += $(BOARDSRC)
-ALLINC  += $(BOARDINC)
