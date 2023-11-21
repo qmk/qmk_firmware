c96f6a0a874ae807da0a77f838b6f971a4717b2c
joliverMI <qmk@Desktop2023>
2023-11-21 12:36:55 -0500

    moved a generalized color layer script to the origin rather than using it individually in each keymap.

---
+++ color_user_layer_settings.c
@@ -0,0 +1,43 @@
+#ifndef LAYER_LIGHT_MODE
+#define LAYER_LIGHT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+#endif
+
+void keyboard_post_init_user(void) {
+  rgblight_sethsv(C_QWERTY);
+  rgblight_mode(LAYER_LIGHT_MODE);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+   uint8_t layer = biton32(state);
+   switch (layer) {
+      case _QWERTY:
+         rgblight_sethsv(C_QWERTY);
+         break;
+      #ifdef C_RECORE
+      case _RECORE:
+         rgblight_sethsv(C_RECORE);
+         break;
+      #endif
+      #ifdef C_DIABLO
+      case _DIABLO:
+         rgblight_sethsv(C_DIABLO);
+         break;
+      #endif
+      #ifdef C_TEMPRTY
+      case _TEMPRTY:         
+         rgblight_sethsv(C_TEMPRTY);
+         break;
+      #endif
+      #ifdef C_SYMB
+      case _SYMB:         
+         rgblight_sethsv(C_SYMB);
+         break;
+      #endif
+      #ifdef C_NAV
+      case _NAV:         
+         rgblight_sethsv(C_NAV);
+         break;
+      #endif
+   }
+   return state;
+}
