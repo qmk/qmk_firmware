27893b918a011b103a63b3f8771cc077228ef2e5
joliverMI <qmk@Desktop2023>
2023-11-21 11:44:47 -0500

    Updated lambo's keyboard with boost options on the z keyboard.

--- keyboards/recorev3/keymaps/via/user_settings.c
+++ keyboards/recorev3/keymaps/via/user_settings.c
@@ -7,36 +7,11 @@
 #define BOOSTED_REPEAT_ENABLED
 #define REPEAT_DELAY 130
 #define REPEAT_TERM 6
-#define BOOSTED_REPEAT_DELAY 150
-#define BOOSTED_REPEAT_TERM 20
-#define FAST_REPEAT_KEYS    KC_P, \
-                            KC_EQL, \
-                            KC_RBRC, \
-                            KC_BSLS, \
-                            KC_BSPC, \
-                            KC_LBRC, \
-                            KC_SCLN, \
-                            KC_SLSH, \
-                            KC_QUOT, \
-                            KC_COMM, \
-                            KC_F, \
-                            KC_EQL, \
-                            KC_LBRC, \
-                            KC_BSLS, \
-                            KC_BSPC, \
-                            KC_G, \
-                            KC_V, \
-                            KC_SLSH, \
-                            KC_B, \
-                            KC_PGUP, \
-                            KC_C, \
-                            KC_PAST, \
-                            KC_X, \
-                            KC_T
+#define BOOSTED_REPEAT_DELAY 130
+#define BOOSTED_REPEAT_TERM 15
 #define FAST_REPEAT_KEY_COUNT 24
-#define BOOSTED_REPEAT_KEYS KC_Z, \
-                            KC_G
-#define BOOSTED_REPEAT_KEY_COUNT 2
+#define BOOSTED_REPEAT_KEYS KC_BSPC
+#define BOOSTED_REPEAT_KEY_COUNT 1
 
 #define FAST_REPEAT_LAYERS _RECORE
 #define FAST_REPEAT_LAYER_COUNT 1
