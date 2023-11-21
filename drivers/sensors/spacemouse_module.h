5398f49064edeaa4a9b5e8e93bc4c9cd87aea071
Drashna Jael're <drashna@live.com>
2023-11-20 16:57:19 -0800

    [Pointing] Add space mouse module uart support

---
+++ drivers/sensors/spacemouse_module.h
@@ -0,0 +1,48 @@
+// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// Datasheet UART settings specify:
+// - 38400 baud
+// - 8 data bits
+// - 1 stop bit
+// - no parity
+// - 100/s data rate
+
+#ifndef SPACEMOUSE_BAUD_RATE
+#    define SPACEMOUSE_BAUD_RATE 38400
+#endif
+
+#define SPACEMOUSE_AXIS_COUNT 6
+
+#define SPACEMOUSE_LENGTH_HEADER 1
+#define SPACEMOUSE_LENGTH_DATA (2 * SPACEMOUSE_AXIS_COUNT)
+#define SPACEMOUSE_LENGTH_CHECKSUM 2
+#define SPACEMOUSE_LENGTH_FOOTER 1
+#define SPACEMOUSE_LENGTH_PACKET (SPACEMOUSE_LENGTH_HEADER + SPACEMOUSE_LENGTH_DATA + SPACEMOUSE_LENGTH_CHECKSUM + SPACEMOUSE_LENGTH_FOOTER)
+
+enum spacemouse_commands {
+    SPACEMOUSE_CMD_REQUEST_DATA            = 0xAC,
+    SPACEMOUSE_CMD_SET_ZERO_POSITION       = 0xAD,
+    SPACEMOUSE_CMD_AUTO_DATA_ON            = 0xAE,
+    SPACEMOUSE_CMD_AUTO_DATA_OFF           = 0xAF,
+    SPACEMOUSE_CMD_END                     = 0x8D,
+    SPACEMOUSE_DATA_REQUEST_START          = 0x96,
+};
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+    int16_t a;
+    int16_t b;
+    int16_t c;
+} spacemouse_data_t;
+
+bool spacemouse_send_command(uint8_t cmd);
+bool spacemouse_init(void);
+spacemouse_data_t spacemouse_get_data(void);
