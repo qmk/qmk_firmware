b2e1cd998347ed2277a04e3c3b7a71165472a49f
Kasper Flye <kasperflye@gmail.com>
2023-11-22 21:15:45 +0100

    Move windows key on gmmk pro

--- keyboards/gmmk/pro/rev1/iso/keymaps/cflye/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/cflye/keymap.c
@@ -31,12 +31,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
 ) \
 LAYOUT_gmmk_pro_iso_wrapper(\
-    KC_ESC,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   QK_BOOT,           KC_MPLY,\
+    KC_ESC,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   QK_BOOT,          KC_MPLY,\
     KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,          KC_DEL,\
     KC_TAB,     K00,     K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09,     KC_LBRC, KC_RBRC,                    KC_PGUP,\
-    KC_LCTL,    K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19,     KC_QUOT, KC_NUHS,  KC_ENT,           KC_PGDN,\
-    KC_LSFT,    KC_NUBS, K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,               KC_RSFT, KC_UP,   KC_END,\
-    KC_LGUI,    K32,     LT(_NAV,KC_ESC),                KC_SPC,                       K36, K37,                   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT\
+    KC_LSFT,    K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19,     KC_QUOT, KC_NUHS,  KC_ENT,           KC_PGDN,\
+    KC_LCTL,    KC_NUBS, K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,               KC_RSFT, KC_UP,   KC_END,\
+    KC_LCTL,    KC_LGUI, LT(_NAV,KC_ESC),                KC_SPC,                       K36, K37,                   KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT\
 )
 
 #define LAYOUT_gmmk_pro_iso_gaming(\
@@ -46,12 +46,12 @@ LAYOUT_gmmk_pro_iso_wrapper(\
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
 ) \
 LAYOUT_gmmk_pro_iso_wrapper(\
-    KC_ESC,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TOG_WS,      KC_MPLY,\
-    KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXX,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC,     KC_DEL,\
+    KC_ESC,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  QK_BOOT,     KC_MPLY,\
+    KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,     KC_DEL,\
     KC_TAB,     K00,     K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09,     KC_AA,   XXX,                  KC_PGUP,\
-    KC_LCTL,    K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     KC_AE,   KC_OE,   KC_NUHS,     KC_ENT,  KC_PGDN,\
-    KC_LSFT,    KC_NUBS, K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,     KC_LSFT,     KC_UP,   KC_END,\
-    KC_LGUI,    K32,     KC_LALT,                         KC_SPC,                       K36, K37, KC_RCTL,             KC_LEFT, KC_DOWN, KC_RGHT\
+    KC_LSFT,    K10,     K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     KC_AE,   KC_OE,   KC_NUHS,     KC_ENT,  KC_PGDN,\
+    KC_LCTL,    KC_NUBS, K20,     K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,     KC_LSFT,     KC_UP,   KC_END,\
+    KC_LCTL,    KC_LGUI, KC_LALT,                         KC_SPC,                       K36, K37, KC_RCTL,             KC_LEFT, KC_DOWN, KC_RGHT\
 )
 
 #define LAYOUT_base_wrapper(...) LAYOUT_gmmk_pro_iso_base(__VA_ARGS__)
