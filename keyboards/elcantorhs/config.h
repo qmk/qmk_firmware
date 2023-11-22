4802011dfa16354fbd546220a46f0fdd688b9f7c
azhizhinov <azhizhinov@mail.ru>
2023-11-22 21:15:48 +0300

    Update keyboards/elcantorhs/config.h
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/elcantorhs/config.h
+++ keyboards/elcantorhs/config.h
@@ -16,19 +16,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
