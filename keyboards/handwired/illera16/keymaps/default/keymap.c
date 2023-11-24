0e42403b1cda4fa3f2c609f854dbe4333e08910b
villeramassana <132305075+villeramassana@users.noreply.github.com>
2023-11-24 23:20:46 +0100

    Update keymap.c

--- keyboards/handwired/illera16/keymaps/default/keymap.c
+++ keyboards/handwired/illera16/keymaps/default/keymap.c
@@ -1,6 +1,3 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 
 #define _BASE 0 
@@ -11,7 +8,10 @@
 layer_state_t layer_state_set_user(layer_state_t state);
 
 void keyboard_post_init_user(void) {
-    rgblight_enable_noeeprom();
+    // rgblight_enable() inicia el control RGB
+    // rgblight_sethsv() permite configurar el color HSV
+    rgblight_enable();
+    rgblight_sethsv(HSV_BLUE); // Color predeterminado al inicio
     layer_state_set_user(layer_state);
 }
 
@@ -19,79 +19,31 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = biton32(state);
     switch (layer) {    
         case _CAPA1:
-            rgblight_sethsv_noeeprom(HSV_GREEN);
+            rgblight_sethsv(HSV_GREEN);
             break;
 
         case _CAPA2:
-            rgblight_sethsv_noeeprom(HSV_RED);
+            rgblight_sethsv(HSV_RED);
             break;
 
         case _CAPA3:
-            rgblight_sethsv_noeeprom(HSV_PINK);
+            rgblight_sethsv(HSV_PINK);
             break;
 
         default:
-            rgblight_sethsv_noeeprom(HSV_BLUE); // MI COLOR
+            rgblight_sethsv(HSV_BLUE); // Color predeterminado si no está en una capa específica
             break;
     }
     
     return state;
 }
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │c1 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 7 │ 8 │ 9 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ 0 │
-     * └───┴───┴───┴───┘
-     */
-    
-    [0] = LAYOUT_ortho_4x4( /* CAPAS BASE */
-        KC_DEL, TG(1),  _______,   KC_SLASH,
-        KC_7,   KC_8,   KC_9,      KC_PAST,
-        KC_4,   KC_5,   KC_6,      KC_PMNS,
-        KC_1,   KC_2,   KC_3,      KC_0
-    ),
-    [1] = LAYOUT_ortho_4x4( /* CAPA1 */
-        KC_1,     TG(2),   TG(1),   KC_1,
-        KC_1,     KC_1,   KC_1,   KC_1,
-        KC_1,     KC_1,   KC_1,   KC_1,
-        KC_1,     KC_1,   KC_1,   KC_1
-    ),
-    [2] = LAYOUT_ortho_4x4( /* CAPA2 */
-        KC_2,      TG(3),   TG(2),   KC_2,
-        KC_2,       KC_2,   KC_2,   KC_2,
-        KC_2,       KC_2,   KC_2,   KC_2,
-        KC_2,       KC_2,   KC_2,   KC_2
-    ),
-    [3] = LAYOUT_ortho_4x4( /* CAPA3 */
-        KC_3,      KC_NO,    TG(3),   KC_3,
-        KC_3,       KC_3,     KC_3,   KC_3,
-        KC_3,       KC_3,     KC_3,   KC_3,
-        KC_3,       KC_3,     KC_3,   KC_3
-    ),
-
-
+    /* ... Tu disposición de teclas sigue aquí ... */
 };
-/*
- * ROTARY ENCODER
- */
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),         ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [1] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),         ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [2] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),         ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [3] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),         ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
+    /* ... Tu configuración de encoders sigue aquí ... */
 };
-
 #endif
-
-
-
