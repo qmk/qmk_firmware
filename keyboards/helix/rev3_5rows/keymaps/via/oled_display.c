cb1d0bc4116640f95b7cc332d1bb9ca8140b61aa
MakotoKurauchi <pluis@me.com>
2023-11-24 13:54:58 +0900

    Update keyboards/helix/rev3_5rows/keymaps/via/oled_display.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/helix/rev3_5rows/keymaps/via/oled_display.c
+++ keyboards/helix/rev3_5rows/keymaps/via/oled_display.c
@@ -71,7 +71,6 @@ void render_status(void) {
   led_t led_state = host_keyboard_led_state();
   oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
   oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-//  oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
 }
 
 
