084a0256b9aa914a16c400499b7951cbbea3bf7f
villeramassana <132305075+villeramassana@users.noreply.github.com>
2023-11-24 23:25:31 +0100

    Update keymap.c

--- keyboards/handwired/illera16/keymaps/default/keymap.c
+++ keyboards/handwired/illera16/keymaps/default/keymap.c
@@ -8,10 +8,8 @@
 layer_state_t layer_state_set_user(layer_state_t state);
 
 void keyboard_post_init_user(void) {
-    // rgblight_enable() inicia el control RGB
-    // rgblight_sethsv() permite configurar el color HSV
-    rgblight_enable();
-    rgblight_sethsv(HSV_BLUE); // Color predeterminado al inicio
+    rgblight_enable();  // Habilita el control del LED RGB
+    rgblight_sethsv(HSV_BLUE); // Establece el color azul como predeterminado al inicio
     layer_state_set_user(layer_state);
 }
 
@@ -31,7 +29,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             break;
 
         default:
-            rgblight_sethsv(HSV_BLUE); // Color predeterminado si no está en una capa específica
+            rgblight_sethsv(HSV_BLUE); // Establece el color azul si no está en una capa específica
             break;
     }
     
@@ -39,11 +37,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* ... Tu disposición de teclas sigue aquí ... */
+    /* Tu disposición de teclas sigue aquí */
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    /* ... Tu configuración de encoders sigue aquí ... */
+    /* Tu configuración de encoders sigue aquí */
 };
 #endif
