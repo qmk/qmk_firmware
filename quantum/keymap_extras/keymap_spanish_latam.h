167c914f40195eb5c37a3e72be50b7700faf9cda
Juan Díaz <3674588+JuanoD@users.noreply.github.com>
2023-11-25 12:42:15 -0500

    Add latam spanish headers

---
+++ quantum/keymap_extras/keymap_spanish_latam.h
@@ -0,0 +1,85 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#include "keycodes.h"
+// clang-format off
+
+// Aliases
+#define LA_PIPE KC_GRV  // |
+#define LA_1    KC_1    // 1
+#define LA_2    KC_2    // 2
+#define LA_3    KC_3    // 3
+#define LA_4    KC_4    // 4
+#define LA_5    KC_5    // 5
+#define LA_6    KC_6    // 6
+#define LA_7    KC_7    // 7
+#define LA_8    KC_8    // 8
+#define LA_9    KC_9    // 9
+#define LA_0    KC_0    // 0
+#define LA_QUOT KC_MINS // '
+#define LA_IQUE KC_EQL  // ¿
+#define LA_Q    KC_Q    // Q
+#define LA_W    KC_W    // W
+#define LA_E    KC_E    // E
+#define LA_R    KC_R    // R
+#define LA_T    KC_T    // T
+#define LA_Y    KC_Y    // Y
+#define LA_U    KC_U    // U
+#define LA_I    KC_I    // I
+#define LA_O    KC_O    // O
+#define LA_P    KC_P    // P
+#define LA_ACUT  KC_LBRC // ´ (dead)
+#define LA_PLUS KC_RBRC // +
+#define LA_A    KC_A    // A
+#define LA_S    KC_S    // S
+#define LA_D    KC_D    // D
+#define LA_F    KC_F    // F
+#define LA_G    KC_G    // G
+#define LA_H    KC_H    // H
+#define LA_J    KC_J    // J
+#define LA_K    KC_K    // K
+#define LA_L    KC_L    // L
+#define LA_NTIL KC_SCLN // Ñ
+#define LA_LCBR KC_QUOT // {
+#define LA_RCBR KC_NUHS // }
+#define LA_LABK KC_NUBS // <
+#define LA_Z    KC_Z    // Z
+#define LA_X    KC_X    // X
+#define LA_C    KC_C    // C
+#define LA_V    KC_V    // V
+#define LA_B    KC_B    // B
+#define LA_N    KC_N    // N
+#define LA_M    KC_M    // M
+#define LA_COMM KC_COMM // ,
+#define LA_DOT  KC_DOT  // .
+#define LA_MINS KC_SLSH // -
+#define LA_MORD S(LA_PIPE) // |
+#define LA_EXLM S(LA_1)    // !
+#define LA_DQUO S(LA_2)    // "
+#define LA_NUMB S(LA_3)    // #
+#define LA_DLR  S(LA_4)    // $
+#define LA_PERC S(LA_5)    // %
+#define LA_AMPR S(LA_6)    // &
+#define LA_SLSH S(LA_7)    // /
+#define LA_LPRN S(LA_8)    // (
+#define LA_RPRN S(LA_9)    // )
+#define LA_EQL  S(LA_0)    // =
+#define LA_QUES S(LA_QUOT) // ?
+#define LA_IEXL S(LA_IQUE) // ¡
+#define LA_DIAE S(LA_ACUT)  // ¨ (dead)
+#define LA_ASTR S(LA_PLUS) // *
+#define LA_LBRC S(LA_LCBR) // [
+#define LA_RBRC S(LA_RCBR) // ]
+#define LA_RABK S(LA_LABK) // >
+#define LA_SCLN S(LA_COMM) // ;
+#define LA_COLN S(LA_DOT)  // :
+#define LA_UNDS S(LA_MINS) // _
+#define LA_AT ALGR(LA_Q) // @
+#define LA_EUR ALGR(LA_E) // €
+#define LA_NOT ALGR(LA_PIPE) // ¬
+#define LA_BSLS ALGR(LA_PIPE) // (backslash)
+#define LA_TILD ALGR(LA_PLUS) // ~
+#define LA_CIRC ALGR(LA_LCBR) // ^
+#define LA_GRAV ALGR(LA_RCBR) // `
+
---
+++ quantum/keymap_extras/sendstring_spanish_latam.h
@@ -0,0 +1,120 @@
+/* Copyright 2023 Juan David Díaz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Sendstring lookup tables for Latam Spanish layouts
+
+#pragma once
+
+#include "send_string.h"
+#include "keymap_spanish_latam.h"
+
+// clang-format off
+
+const uint8_t ascii_to_shift_lut[16] PROGMEM = {
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+
+    KCLUT_ENTRY(0, 1, 1, 0, 1, 1, 1, 0),
+    KCLUT_ENTRY(1, 1, 1, 0, 0, 0, 0, 1),
+    KCLUT_ENTRY(0, 0, 1, 1, 0, 1, 1, 0),
+    KCLUT_ENTRY(0, 0, 1, 1, 0, 1, 1, 1),
+    KCLUT_ENTRY(0, 1, 1, 1, 1, 1, 1, 1),
+    KCLUT_ENTRY(1, 1, 1, 1, 1, 1, 1, 1),
+    KCLUT_ENTRY(1, 1, 1, 1, 1, 1, 1, 1),
+    KCLUT_ENTRY(1, 1, 1, 1, 0, 1, 0, 1),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0)
+};
+
+const uint8_t ascii_to_altgr_lut[16] PROGMEM = {
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(1, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 1, 0, 1, 0),
+    KCLUT_ENTRY(1, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 1, 0)
+};
+
+const uint8_t ascii_to_dead_lut[16] PROGMEM = {
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 1, 0),
+    KCLUT_ENTRY(1, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0)
+};
+
+const uint8_t ascii_to_keycode_lut[128] PROGMEM = {
+    // NUL   SOH      STX      ETX      EOT      ENQ      ACK      BEL
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    // BS    TAB      LF       VT       FF       CR       SO       SI
+    KC_BSPC, KC_TAB,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    // DLE   DC1      DC2      DC3      DC4      NAK      SYN      ETB
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    // CAN   EM       SUB      ESC      FS       GS       RS       US
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+
+    //       !        "        #        $        %        &        '
+    KC_SPC,  LA_1,    LA_2,    LA_3,    LA_4,    LA_5,    LA_6,    LA_QUOT,
+    // (     )        *        +        ,        -        .        /
+    LA_8,    LA_9,    LA_PLUS, LA_PLUS, LA_COMM, LA_MINS, LA_DOT,  LA_7,
+    // 0     1        2        3        4        5        6        7
+    LA_0,    LA_1,    LA_2,    LA_3,    LA_4,    LA_5,    LA_6,    LA_7,
+    // 8     9        :        ;        <        =        >        ?
+    LA_8,    LA_9,    LA_DOT,  LA_COMM, LA_LABK, LA_0,    LA_LABK, LA_QUOT,
+    // @     A        B        C        D        E        F        G
+    LA_Q,    LA_A,    LA_B,    LA_C,    LA_D,    LA_E,    LA_F,    LA_G,
+    // H     I        J        K        L        M        N        O
+    LA_H,    LA_I,    LA_J,    LA_K,    LA_L,    LA_M,    LA_N,    LA_O,
+    // P     Q        R        S        T        U        V        W
+    LA_P,    LA_Q,    LA_R,    LA_S,    LA_T,    LA_U,    LA_V,    LA_W,
+    // X     Y        Z        [        \        ]        ^        _
+    LA_X,    LA_Y,    LA_Z,    LA_LCBR, LA_QUOT, LA_RCBR, LA_LCBR, LA_MINS,
+    // `     a        b        c        d        e        f        g
+    LA_RCBR, LA_A,    LA_B,    LA_C,    LA_D,    LA_E,    LA_F,    LA_G,
+    // h     i        j        k        l        m        n        o
+    LA_H,    LA_I,    LA_J,    LA_K,    LA_L,    LA_M,    LA_N,    LA_O,
+    // p     q        r        s        t        u        v        w
+    LA_P,    LA_Q,    LA_R,    LA_S,    LA_T,    LA_U,    LA_V,    LA_W,
+    // x     y        z        {        |        }        ~        DEL
+    LA_X,    LA_Y,    LA_Z,    LA_LCBR, LA_PIPE, LA_RCBR, LA_PLUS, KC_DEL
+};
