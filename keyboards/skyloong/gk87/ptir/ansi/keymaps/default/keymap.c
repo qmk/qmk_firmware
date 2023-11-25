5c95808bedfd1cdde5d7055f121289e2f06ac1b1
skyloong <zhangyuanji@jikedingzhi.com>
2023-11-25 14:33:06 +0800

    Modified keymaps

--- keyboards/skyloong/gk87/ptir/ansi/keymaps/default/keymap.c
+++ keyboards/skyloong/gk87/ptir/ansi/keymaps/default/keymap.c
@@ -11,11 +11,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,       KC_I,     KC_O,      KC_P,   KC_LBRC,  KC_RBRC,      KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,       KC_K,     KC_L,   KC_SCLN,   KC_QUOT,                 KC_ENT,
         KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,    KC_COMM,   KC_DOT,   KC_SLSH,                           KC_RSFT,               KC_UP,
-        KC_LCTL,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,                 KC_SPC,             KC_RALT,   KC_MENU,   KC_RCTL,                  MO(1),   KC_LEFT,  KC_DOWN,    KC_RGHT
+        KC_LCTL,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,                 KC_SPC,             KC_RALT,     MO(1),   KC_MENU,                KC_RCTL,   KC_LEFT,  KC_DOWN,    KC_RGHT
     ),
 
     [1] = LAYOUT_all(
-        _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH,  G(C(KC_S)), LSG(KC_S),   KC_MPRV,  KC_MPLY,   KC_MNXT,   KC_MUTE,  KC_VOLD,     KC_VOLU,    KC_PSCR,  KC_SCRL,   KC_PAUS,
+        _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH,  G(C(KC_S)),   KC_SLEP,   KC_MPRV,  KC_MPLY,   KC_MNXT,   KC_MUTE,  KC_VOLD,     KC_VOLU,    KC_PSCR,  KC_SCRL,   KC_PAUS,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,   RGB_TOG,  RGB_RMOD,  RGB_MOD,      KC_DEL,     KC_INS,  KC_HOME,   KC_PGUP,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,   _______,   _______,  _______,     _______,     KC_DEL,   KC_END,   KC_PGDN,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,   _______,   _______,               _______,
