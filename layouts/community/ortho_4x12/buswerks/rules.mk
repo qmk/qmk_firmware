9083d40cb424f49fce399fb747eea76032351c29
zvecr <git@zvecr.com>
2023-11-26 16:00:01 +0000

    Remove remaining community layout keymaps

--- layouts/community/ortho_4x12/buswerks/rules.mk
+++
@@ -1,9 +0,0 @@
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE   = yes       # Mouse keys(+4700)
-EXTRAKEY_ENABLE   = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE    = no         # Console for debug(+400)
-COMMAND_ENABLE    = no        # Commands for debug and configuration
-TAP_DANCE_ENABLE  = no
-AUDIO_ENABLE      = yes
-
-MACROS_ENABLED    = no
