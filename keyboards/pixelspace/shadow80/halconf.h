2a7b5d83c496b46c168fe72379f5f83c858c36b6
PixelSpaceStudio <106432218+PixelSpaceStudio@users.noreply.github.com>
2023-11-27 11:48:01 +0800

    Update halconf.h

--- keyboards/pixelspace/shadow80/halconf.h
+++ keyboards/pixelspace/shadow80/halconf.h
@@ -13,8 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-
 #pragma once
 
 #define HAL_USE_PWM TRUE
