0d0010b61ef8ce28727bc8abc6a7de9fe5d5db48
PixelSpaceStudio <106432218+PixelSpaceStudio@users.noreply.github.com>
2023-11-27 11:36:27 +0800

    Update chconf.h

--- keyboards/pixelspace/shadow80/chconf.h
+++ keyboards/pixelspace/shadow80/chconf.h
@@ -13,12 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-
-
 #pragma once
-
 #define CH_CFG_ST_TIMEDELTA 0
-
 #define CH_CFG_USE_CONDVARS_TIMEOUT FALSE
 #include_next <chconf.h>
