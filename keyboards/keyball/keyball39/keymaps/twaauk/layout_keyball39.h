7ba0aa203592113e01e96e85ddb7668271c1aa7e
this was a auk <this.was.a.auk@gmail.com>
2023-11-28 21:58:41 +0900

    add twaauk's keymaps for keyball39 and keyball44.

---
+++ keyboards/keyball/keyball39/keymaps/twaauk/layout_keyball39.h
@@ -0,0 +1,16 @@
+#ifndef	LAYOUT_KEYBALL39_H
+#define LAYOUT_KEYBALL39_H
+#include "keyball39.h"
+
+#define LAYER_LAYOUT(\
+	L00, L01, L02, L03, L04, L05,           R05, R04, R03, R02, R01, R00, \
+	L10, L11, L12, L13, L14, L15,           R15, R14, R13, R12, R11, R10, \
+	L20, L21, L22, L23, L24, L25,           R25, R24, R23, R22, R21, R20, \
+	L30, L31, L32, L33, L34, L35, L36, R36, R35, R34, R33, R32, R31, R30, \
+	L40, L41, L42, L43, L44, L45, L46, R46, R45, R44, R43, R42, R41, R40) \
+	LAYOUT_universal( \
+		L11, L12, L13, L14, L15,           R15, R14, R13, R12, R11, \
+		L21, L22, L23, L24, L25,           R25, R24, R23, R22, R21, \
+		L31, L32, L33, L34, L35,           R35, R34, R33, R32, R31, \
+		L42, L44, L44, L45, L46, R46, R45, R44, R43, R42, R41, R40)
+#endif
