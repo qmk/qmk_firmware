e35c6df99b81ae0a2e90adaa1cbcd0ba56c0d7a9
QKekos <q.kekos.q@gmail.com>
2023-11-28 09:59:56 +0200

    refactored magic switches usage

--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
@@ -24,10 +24,10 @@ void process_magic_key(uint16_t keycode, uint8_t mods) {
                 magic_case(KC_U, "sic");
             );
 
-            double_magic_case(KC_U, GLT_S, "e");
-            double_magic_case(CLT_A, KC_L, "r");
-            double_magic_case(LT_I, SMT_N, "form");
-            double_magic_case(ALT_E, KC_X, "ample");
+            double_magic_case(CLT_A, KC_L,  "r");
+            double_magic_case(KC_U,  GLT_S, "e");
+            double_magic_case(LT_I,  SMT_N, "form");
+            double_magic_case(ALT_E, KC_X,  "ample");
         );
 
         triple_magic_case(GLT_S, KC_Y, GLT_S, "tem");
@@ -35,56 +35,56 @@ void process_magic_key(uint16_t keycode, uint8_t mods) {
 
     switch (last_queue_key) {
         double_magic_switch(KC_M,
-            magic_case(KC_P, "l");
+            magic_case(KC_P,  "l");
             magic_case(CLT_A, "o");
         );
 
         double_magic_switch(KC_O,
-            magic_case(KC_C, "k");
+            magic_case(KC_C,  "k");
             magic_case(ALT_T, "her");
         );
 
-        double_magic_case(LT_I, KC_C, "k");
-        double_magic_case(KC_U, KC_C, "k");
-        double_magic_case(KC_C, KC_Y, "c");
-        double_magic_case(KC_W, ALT_E, "b");
-        double_magic_case(CLT_A, KC_C, "k");
-        double_magic_case(KC_V, LT_I, "ew");
-        double_magic_case(KC_L, LT_I, "st");
-        double_magic_case(ALT_E, KC_C, "k");
-        double_magic_case(GLT_S, KC_O, "me");
-        double_magic_case(SMT_D, CLT_A, "y");
-        double_magic_case(KC_B, ALT_E, "en");
-        double_magic_case(KC_P, ALT_E, "ople");
+        double_magic_case(LT_I,   KC_C,  "k");
+        double_magic_case(KC_U,   KC_C,  "k");
+        double_magic_case(KC_C,   KC_Y,  "c");
+        double_magic_case(KC_W,   ALT_E, "b");
+        double_magic_case(CLT_A,  KC_C,  "k");
+        double_magic_case(ALT_E,  KC_C,  "k");
+        double_magic_case(SMT_D,  CLT_A, "y");
+        double_magic_case(GLT_S,  KC_O,  "me");
+        double_magic_case(KC_V,   LT_I,  "ew");
+        double_magic_case(KC_L,   LT_I,  "st");
+        double_magic_case(KC_B,   ALT_E, "en");
         double_magic_case(TH_NUM, ALT_E, ".g.");
+        double_magic_case(KC_P,   ALT_E, "ople");
     }
 
     switch (keycode) {
-        magic_case(KC_L, "k");
-        magic_case(KC_C, "y");
-        magic_case(KC_P, "y");
-        magic_case(KC_Y, "p");
-        magic_case(KC_K, "s");
-        magic_case(KC_G, "y");
-        magic_case(KC_O, "a");
-        magic_case(KC_U, "e");
-        magic_case(KC_X, "t");
-        magic_case(GLT_S, "k");
-        magic_case(CLT_R, "l");
-        magic_case(SMT_D, "y");
-        magic_case(ALT_E, "u");
-        magic_case(LT_I, "on");
-        magic_case(KC_V, "er");
-        magic_case(KC_Q, "ue");
-        magic_case(KC_M, "ent");
-        magic_case(KC_J, "ust");
-        magic_case(KC_Z, "one");
-        magic_case(SMT_N, "ion");
-        magic_case(KC_W, "hich");
-        magic_case(KC_F, "irst");
+        magic_case(KC_L,   "k");
+        magic_case(KC_C,   "y");
+        magic_case(KC_P,   "y");
+        magic_case(KC_Y,   "p");
+        magic_case(KC_K,   "s");
+        magic_case(KC_G,   "y");
+        magic_case(KC_O,   "a");
+        magic_case(KC_U,   "e");
+        magic_case(KC_X,   "t");
+        magic_case(GLT_S,  "k");
+        magic_case(CLT_R,  "l");
+        magic_case(SMT_D,  "y");
+        magic_case(ALT_E,  "u");
+        magic_case(LT_I,   "on");
+        magic_case(KC_V,   "er");
+        magic_case(KC_Q,   "ue");
+        magic_case(KC_M,   "ent");
+        magic_case(KC_J,   "ust");
+        magic_case(KC_Z,   "one");
+        magic_case(SMT_N,  "ion");
         magic_case(TH_NUM, "the");
-        magic_case(ALT_T, "ment");
-        magic_case(KC_B, "efore");
+        magic_case(ALT_T,  "ment");
+        magic_case(KC_W,   "hich");
+        magic_case(KC_F,   "irst");
+        magic_case(KC_B,   "efore");
         remember_magic_case(CLT_A, "b", KC_B);
     }
 }
--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/rep_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/rep_key.c
@@ -7,20 +7,20 @@ void process_rep_key(uint16_t keycode, uint8_t mods) {
     }
 
     switch (last_queue_key) {
-        double_magic_case(CLT_A, KC_B, "out");
-        double_magic_case(KC_F, CLT_R, "om");
-        double_magic_case(KC_L, CLT_A, "st");
+        double_magic_case(KC_F,  CLT_R, "om");
+        double_magic_case(KC_L,  CLT_A, "st");
+        double_magic_case(CLT_A, KC_B,  "out");
     }
 
     switch (keycode) {
-        magic_case(KC_X, "es");
-        magic_case(LT_I, "ng");
-        magic_case(KC_Y, "ou");
-        magic_case(KC_Q, "ui");
-        magic_case(KC_J, "oin");
-        magic_case(KC_W, "ould");
+        magic_case(KC_X,   "es");
+        magic_case(LT_I,   "ng");
+        magic_case(KC_Y,   "ou");
+        magic_case(KC_Q,   "ui");
+        magic_case(KC_J,   "oin");
         magic_case(TH_NUM, "for");
-        magic_case(KC_B, "ecause");
+        magic_case(KC_W,   "ould");
+        magic_case(KC_B,   "ecause");
         default: tap_code16(keycode);
     }
 }
