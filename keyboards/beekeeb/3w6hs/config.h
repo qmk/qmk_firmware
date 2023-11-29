e196d1ec3b59a1406d80023832fd38a6a7f8b4d5
Leo Lou <louyuhong@gmail.com>
2023-11-29 18:32:00 +0800

    Update keyboards/beekeeb/3w6hs/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/beekeeb/3w6hs/config.h
+++ keyboards/beekeeb/3w6hs/config.h
@@ -11,20 +11,8 @@
 #define MATRIX_ROWS_PER_SIDE (MATRIX_ROWS / 2)
 #define MATRIX_COLS_PER_SIDE (MATRIX_COLS / 2)
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
 #define MATRIX_ROW_PINS_L { GP7, GP8, GP9, GP10}
 #define MATRIX_COL_PINS_L { GP11, GP12, GP13, GP14, GP15 }
 
 #define MATRIX_ROW_PINS_R { P10, P11, P12, P05 }
 #define MATRIX_COL_PINS_R { P06, P13, P14, P01, P00 }
-
-#define DIODE_DIRECTION COL2ROW
