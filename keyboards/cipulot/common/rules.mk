7e909e2ce967808c1dbb436b0a43c9cb4f8cbbbb
Cipulot <40441626+Cipulot@users.noreply.github.com>
2023-11-29 17:50:06 +0100

    Moved common rules in `common/rules.mk`
    Moved shared bits of the `rules.mk` in a common file.

---
+++ keyboards/cipulot/common/rules.mk
@@ -0,0 +1,3 @@
+CUSTOM_MATRIX = lite
+ANALOG_DRIVER_REQUIRED = yes
+SRC += common/matrix.c common/ec_board.c common/ec_switch_matrix.c
--- keyboards/cipulot/ec_prox/jis/rules.mk
+++ keyboards/cipulot/ec_prox/jis/rules.mk
@@ -1,4 +1,2 @@
-CUSTOM_MATRIX = lite
-ANALOG_DRIVER_REQUIRED = yes
-SRC += keyboards/cipulot/common/matrix.c keyboards/cipulot/common/ec_board.c keyboards/cipulot/common/ec_switch_matrix.c
+include keyboards/cipulot/common/rules.mk
 OPT = 2
