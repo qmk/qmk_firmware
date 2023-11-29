73b8a65aca028b01a08ae52b41b9836854c16b8d
Ryan Neff <ryan.ray.neff@fluke.com>
2023-11-28 16:17:04 -0800

    Moved most of rules.mk to info.json Removed bootmagic

--- keyboards/sofle_pico/rules.mk
+++ keyboards/sofle_pico/rules.mk
@@ -4,17 +4,3 @@ SERIAL_DRIVER = vendor
 # @todo: Move to json
 # OLED_DRIVER = SSD1306
 OLED_TRANSPORT = i2c
-# Encoder Support
-ENCODER_ENABLE = yes
-# Use Enocoder Mapping
-ENCODER_MAP_ENABLE = yes
-# Turn on tap dance:
-TAP_DANCE_ENABLE = no
-# Enables RGB Lighting Effects
-RGB_MATRIX_ENABLE = yes
-# Dynamic Tapping Term:
-DYNAMIC_TAPPING_TERM_ENABLE = yes
-
-OLED_ENABLE = yes
-# OLED_DRIVER = SSD1306
-WPM_ENABLE = yes
