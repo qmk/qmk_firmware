6d2fc1d94b1dc0201fee82260653815e1dcc805f
Cipulot <40441626+Cipulot@users.noreply.github.com>
2023-11-29 18:03:18 +0100

    Revert "Moved common rules in `common/rules.mk`"
    This reverts commit 2238e95719c56e3f5fb240e3329fae0f720e5e28.

--- keyboards/cipulot/common/rules.mk
+++
@@ -1,3 +0,0 @@
-CUSTOM_MATRIX = lite
-ANALOG_DRIVER_REQUIRED = yes
-SRC += common/matrix.c common/ec_board.c common/ec_switch_matrix.c
--- keyboards/cipulot/ec_60/rules.mk
+++ keyboards/cipulot/ec_60/rules.mk
@@ -1,2 +1,4 @@
-include keyboards/cipulot/common/rules.mk
+CUSTOM_MATRIX = lite
+ANALOG_DRIVER_REQUIRED = yes
+SRC += keyboards/cipulot/common/matrix.c keyboards/cipulot/common/ec_board.c keyboards/cipulot/common/ec_switch_matrix.c
 OPT = 3
