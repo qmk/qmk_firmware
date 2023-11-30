35f9f7191595a0ac22dc606c8837fa91136b316b
adophoxia <andyao1528@gmail.com>
2023-11-29 19:08:02 -0800

    Update Q6

--- keyboards/keychron/q6/ansi_encoder/config.h
+++ keyboards/keychron/q6/ansi_encoder/config.h
@@ -26,15 +26,10 @@
 #define CLR_REG_VAL 0x3FF
 
 /* RGB Matrix Configuration */
-#define DRIVER_1_LED_TOTAL 60
-#define DRIVER_2_LED_TOTAL 48
-#define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
-
-/* Encoder Configuration */
-#define ENCODER_DEFAULT_POS 0x3
+#define RGB_MATRIX_LED_COUNT 108
 
 /* Enable caps-lock LED*/
 #define CAPS_LOCK_LED_INDEX 61
 
 /* Enable NUM_LOCK_LED_INDEX */
-// #define NUM_LOCK_LED_INDEX 37
+#define NUM_LOCK_LED_INDEX 37
--- keyboards/keychron/q6/iso/config.h
+++ keyboards/keychron/q6/iso/config.h
@@ -25,12 +25,10 @@
 #define NO_PIN_OFFSET 1
 
 /* RGB Matrix Configuration */
-#define DRIVER_1_LED_TOTAL 60
-#define DRIVER_2_LED_TOTAL 49
-#define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
+#define RGB_MATRIX_LED_COUNT 109
 
 /* Enable caps-lock LED*/
 #define CAPS_LOCK_LED_INDEX 60
 
 /* Enable NUM_LOCK_LED_INDEX */
-// #define NUM_LOCK_LED_INDEX 37
+#define NUM_LOCK_LED_INDEX 37
