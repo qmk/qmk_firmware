18fab4f746dd17e18a3c2d992eb40bca7ff57b98
Tom Barnes <tom@mechboards.co.uk>
2023-12-01 23:07:39 +0000

    tidy for PR

--- keyboards/helix/rev3_5rows/rules.mk
+++ keyboards/helix/rev3_5rows/rules.mk
@@ -1,7 +1,7 @@
 EXTRAKEY_ENABLE = yes    # Audio control and System control
-RGBLIGHT_ENABLE = no   # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes   # Enable keyboard RGB underglow
 SPLIT_KEYBOARD = yes
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 OLED_ENABLE = yes
 ENCODER_ENABLE = yes
 DIP_SWITCH_ENABLE = no
