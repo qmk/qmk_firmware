8fbd48703a7c80172d7e81d25dfaa6c6ef6c246e
DmNosachev <quartz64@gmail.com>
2023-12-03 10:44:44 +0300

    revert changes to the wrong file

--- keyboards/kin80/keymaps/quartz64/keymap.c
+++ keyboards/kin80/keymaps/quartz64/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2023 DmNosachev
+/* Copyright 2022 DmNosachev
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
