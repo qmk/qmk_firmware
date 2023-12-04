5286b6c2623796a4b6da0f9c754633f1879e7a15
Wolf Van Herreweghe <wolfvanh@gmail.com>
2023-12-04 10:21:59 +0100

    Update keyboards/mode/m60s/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mode/m60s/config.h
+++ keyboards/mode/m60s/config.h
@@ -7,7 +7,6 @@
 
 #define WS2812_PWM_DRIVER   PWMD4
 #define WS2812_PWM_CHANNEL  4
-#define WS2812_PWM_PAL_MODE 2
 #define WS2812_DMA_STREAM   STM32_DMA1_STREAM6  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
 #define WS2812_DMA_CHANNEL  2                   // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
 #define WS2812_PWM_TARGET_PERIOD 800000
