4e1c4c3ad4bae7db0dea9377e0901d0fdee8db40
fauxpark <fauxpark@gmail.com>
2023-12-08 14:03:48 +1100

    LED/RGB Matrix: add header for drivers

--- quantum/led_matrix/led_matrix.h
+++ quantum/led_matrix/led_matrix.h
@@ -23,31 +23,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "led_matrix_types.h"
+#include "led_matrix_drivers.h"
 #include "keyboard.h"
-
-#if defined(LED_MATRIX_IS31FL3218)
-#    include "is31fl3218-simple.h"
-#elif defined(LED_MATRIX_IS31FL3731)
-#    include "is31fl3731-simple.h"
-#endif
-#ifdef LED_MATRIX_IS31FL3733
-#    include "is31fl3733-simple.h"
-#endif
-#ifdef LED_MATRIX_IS31FL3736
-#    include "is31fl3736-simple.h"
-#endif
-#ifdef LED_MATRIX_IS31FL3737
-#    include "is31fl3737-simple.h"
-#endif
-#ifdef LED_MATRIX_IS31FL3741
-#    include "is31fl3741-simple.h"
-#endif
-#if defined(IS31FLCOMMON)
-#    include "is31flcommon.h"
-#endif
-#ifdef LED_MATRIX_SNLED27351
-#    include "snled27351-simple.h"
-#endif
 
 #ifndef LED_MATRIX_TIMEOUT
 #    define LED_MATRIX_TIMEOUT 0
@@ -193,18 +170,6 @@ led_flags_t led_matrix_get_flags(void);
 void        led_matrix_set_flags(led_flags_t flags);
 void        led_matrix_set_flags_noeeprom(led_flags_t flags);
 
-typedef struct {
-    /* Perform any initialisation required for the other driver functions to work. */
-    void (*init)(void);
-
-    /* Set the brightness of a single LED in the buffer. */
-    void (*set_value)(int index, uint8_t value);
-    /* Set the brightness of all LEDS on the keyboard in the buffer. */
-    void (*set_value_all)(uint8_t value);
-    /* Flush any buffered changes to the hardware. */
-    void (*flush)(void);
-} led_matrix_driver_t;
-
 static inline bool led_matrix_check_finished_leds(uint8_t led_idx) {
 #if defined(LED_MATRIX_SPLIT)
     if (is_keyboard_left()) {
@@ -216,8 +181,6 @@ static inline bool led_matrix_check_finished_leds(uint8_t led_idx) {
     return led_idx < LED_MATRIX_LED_COUNT;
 #endif
 }
-
-extern const led_matrix_driver_t led_matrix_driver;
 
 extern led_eeconfig_t led_matrix_eeconfig;
 
