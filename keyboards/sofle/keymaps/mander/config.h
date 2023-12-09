38b40b4f0e174c8fc06e3751a971ed000b2a54b0
mander <git@mander.io>
2023-12-09 21:38:31 +0100

    moved rules to correct place

--- keyboards/sofle/keymaps/mander/config.h
+++ keyboards/sofle/keymaps/mander/config.h
@@ -1,6 +1,4 @@
 #pragma once
-#define ENCODER_ENABLE true
-#define OLED_ENABLE true
 
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
