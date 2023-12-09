e7d3ea89c660f0939ca6586682691b2e22b442ce
mander <git@mander.io>
2023-12-09 21:30:22 +0100

    updated my keymap

---
+++ keyboards/sofle/keymaps/mander/config.h
@@ -0,0 +1,8 @@
+#pragma once
+#define ENCODER_ENABLE true
+#define OLED_ENABLE true
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
