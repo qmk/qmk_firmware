d2418eec520381146fa09c409a8ab2d1e3cb8701
TDC <64564678+kthorpe88@users.noreply.github.com>
2023-12-09 13:38:46 -0700

    Update tdcleft104.h

--- keyboards/tdcleft104/tdcleft104.h
+++ keyboards/tdcleft104/tdcleft104.h
@@ -5,7 +5,7 @@
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 
-// T his program is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
