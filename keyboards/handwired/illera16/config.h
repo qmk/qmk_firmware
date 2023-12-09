97b00ba2a430b0e027b444b0e8518a4773632d45
villeramassana <132305075+villeramassana@users.noreply.github.com>
2023-12-09 15:52:21 +0100

    Update config.h

--- keyboards/handwired/illera16/config.h
+++ keyboards/handwired/illera16/config.h
@@ -1,31 +1,3 @@
 // Copyright 2023 y (@villeramassana)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#pragma once
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
-#define WS2812_DI_PIN D0
-#define RGBLED_NUM 6
-#define RGBLIGHT_LAYERS
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
-#define ENCODERS_PAD_A { B5, B4}
-#define ENCODERS_PAD_B { B6, B2}
-#define ENCODER_RESOLUTION 2
