20b438e86c72e319a642d57cf5d75b803d6c4164
Michael Ridley <litridl@gmail.com>
2023-12-11 17:18:38 +0600

    Lit Voyager v1 keymap implementation

---
+++ keyboards/voyager/keymaps/lit/rules.mk
@@ -0,0 +1,11 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+ORYX_ENABLE = yes
+RGB_MATRIX_CUSTOM_KB = yes
+TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = no
+CAPS_WORD_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes
+SRC = matrix.c
