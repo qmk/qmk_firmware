9289df3da818030975b87c99b6573d895cad5819
casuanoob <96005765+casuanoob@users.noreply.github.com>
2023-12-12 20:22:59 +1100

    Bastard Keyboards: Add support for Dilemma v2 (3x5+3) (#22185)
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Ryan <fauxpark@gmail.com>
    Co-authored-by: Charly Delay <charly@delay.gg>

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/via/config.h
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/via/config.h
@@ -1,5 +1,6 @@
 /**
- * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+ * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+ * Copyright 2023 casuanoob <casuanoob@hotmail.com> (@casuanoob)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,10 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
-#include_next <mcuconf.h>
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
-#undef RP_SPI_USE_SPI0
-#define RP_SPI_USE_SPI0 TRUE
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
