6e5a6e9863e9adf0f482b6d4fd4d93890c2b322a
azhizhinov <azhizhinov@mail.ru>
2023-12-12 09:13:40 +0300

    Update keyboards/elcantorhs/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/elcantorhs/config.h
+++ keyboards/elcantorhs/config.h
@@ -3,6 +3,6 @@
 
 #pragma once
 
-#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
-#define SERIAL_USART_TX_PIN B6     // USART TX pin
-#define SERIAL_USART_RX_PIN B7     // USART RX pin
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN B6
+#define SERIAL_USART_RX_PIN B7
