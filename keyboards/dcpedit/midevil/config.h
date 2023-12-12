f09f469c98dd60efcd85d6e0076ef19a3b2b021f
dcpedit <dcpedit@hotmail.com>
2023-12-12 10:18:28 -0800

    Move backlight settings to info.json

--- keyboards/dcpedit/midevil/config.h
+++ keyboards/dcpedit/midevil/config.h
@@ -7,11 +7,8 @@
 #define LCD_DC_PIN GP16   // data/command
 #define LCD_CS_PIN GP17   // chip select
 #define LCD_RST_PIN GP14  // reset
-#define BACKLIGHT_PIN GP15
 #define BACKLIGHT_PWM_DRIVER PWMD7
 #define BACKLIGHT_PWM_CHANNEL RP2040_PWM_CHANNEL_B
-#define BACKLIGHT_LEVELS 5
-#define BACKLIGHT_DEFAULT_LEVEL 4
 
 /* SPI pins */
 #define SPI_DRIVER SPID0
