64074bee0df6c1a7491d08b55ad0e23e26b19ccb
Isaac Rex <isaaclrex@gmail.com>
2023-12-13 05:59:17 -0600

    moved RGB Matrix settings from config.h to info.json

--- keyboards/nifty_numpad/config.h
+++ keyboards/nifty_numpad/config.h
@@ -4,15 +4,3 @@
 #pragma once
 
 #define EECONFIG_KB_DATA_SIZE 11
-
-// RGB Matrix Parameters
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR // Sets the default mode, if none has been set
-#define RGB_MATRIX_DEFAULT_SPD 100 // Sets the default animation speed, if none has been set
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
-
-#define RGB_MATRIX_DEFAULT_HUE 127 // Sets the default hue value, if none has been set
-#define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
-#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
-
-// #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-// #define RGB_MATRIX_KEYPRESSES
