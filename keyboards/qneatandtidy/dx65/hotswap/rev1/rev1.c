c952bcadb5d0f45a0b685c9f51f8c8055332fe6f
Pham Duc Minh <95753855+Deemen17@users.noreply.github.com>
2023-12-14 01:17:41 +0700

    Update keyboards/qneatandtidy/dx65/hotswap/rev1/rev1.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/qneatandtidy/dx65/hotswap/rev1/rev1.c
+++ keyboards/qneatandtidy/dx65/hotswap/rev1/rev1.c
@@ -94,9 +94,9 @@ const is31_led PROGMEM g_is31_leds[RGB_MATRIX_LED_COUNT] = {
 };
 
 bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
-	if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
-	    return false;
-	}
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+        return false;
+    }
     if (host_keyboard_led_state().caps_lock) {
         for (uint8_t i = led_min; i < led_max; i++) {
             if (g_led_config.flags[i] & LED_FLAG_INDICATOR) {
