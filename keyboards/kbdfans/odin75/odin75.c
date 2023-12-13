e9c51e9444155ec32c58571f4244580be2dd7ecb
adophoxia <100170946+adophoxia@users.noreply.github.com>
2023-12-13 15:41:35 -0800

    Update keyboards/kbdfans/odin75/odin75.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/kbdfans/odin75/odin75.c
+++ keyboards/kbdfans/odin75/odin75.c
@@ -18,18 +18,18 @@
 #include "lib/bongocat.h"
 #ifdef OLED_ENABLE
 bool oled_task_kb(void) {
-	if (!oled_task_user()) { return false; }
-	led_t led_state = host_keyboard_led_state();
-	render_bongocat();
-	oled_set_cursor(14, 0); // sets cursor to (column, row) using charactar spacing (4 rows on 128x32 screen, anything more will overflow back to the top)
-	oled_write_P(PSTR("WPM:"), false);
-	oled_write(get_u8_str(get_current_wpm(), '0'), false); // writes wpm on top right corner of string
-	oled_set_cursor(0, 5);
-	oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "), false);
-	oled_set_cursor(0, 6);
-	oled_write_P(led_state.num_lock ? PSTR("NUM") : PSTR("    "), false);
-	oled_set_cursor(0, 7);
-	oled_write_P(led_state.scroll_lock ? PSTR("SCRL") : PSTR("    "), false);
-	return false;
+    if (!oled_task_user()) { return false; }
+    led_t led_state = host_keyboard_led_state();
+    render_bongocat();
+    oled_set_cursor(14, 0); // sets cursor to (column, row) using charactar spacing (4 rows on 128x32 screen, anything more will overflow back to the top)
+    oled_write_P(PSTR("WPM:"), false);
+    oled_write(get_u8_str(get_current_wpm(), '0'), false); // writes wpm on top right corner of string
+    oled_set_cursor(0, 5);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "), false);
+    oled_set_cursor(0, 6);
+    oled_write_P(led_state.num_lock ? PSTR("NUM") : PSTR("    "), false);
+    oled_set_cursor(0, 7);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCRL") : PSTR("    "), false);
+    return false;
 }
 #endif
