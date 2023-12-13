062cd998e8aba6a87dab235d2c42e820528a2f62
Wolf <contact@vwolf.be>
2023-12-13 04:24:28 +0100

    Fix spacings in Config file

--- keyboards/mode/m60s/config.h
+++ keyboards/mode/m60s/config.h
@@ -5,8 +5,8 @@
 
 #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
 
-#define WS2812_PWM_DRIVER   PWMD4
-#define WS2812_PWM_CHANNEL  4
-#define WS2812_DMA_STREAM   STM32_DMA1_STREAM6  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
-#define WS2812_DMA_CHANNEL  2                   // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#define WS2812_PWM_DRIVER PWMD4
+#define WS2812_PWM_CHANNEL 4
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM6
+#define WS2812_DMA_CHANNEL 2
 #define WS2812_PWM_TARGET_PERIOD 800000
