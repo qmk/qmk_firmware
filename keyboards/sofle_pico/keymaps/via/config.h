c8fd1299cdd21b4bef150ad3f8233f9290e1188d
Ryan Neff <ryan.ray.neff@fluke.com>
2023-12-14 14:53:09 -0800

    Default keymap matches legacy sofle Added via support (not tested yet)

--- keyboards/sofle_pico/keymaps/via/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Ryan Neff (@JellyTitan)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define POINTING_DEVICE_ROTATION_90
-#define SPLIT_POINTING_ENABLE
-#define POINTING_DEVICE_RIGHT
-#define POINTING_DEVICE_TASK_THROTTLE_MS 8
