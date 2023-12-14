c5f0adae708da80f0f90d9f81ceff9a060641523
Florian Pichler <fp@ylk.gd>
2023-12-14 11:30:57 +0100

    Fix rp2040_ce audio setup
    - halconf.h is required to enable PWM inside the HAL
    - mcuconf.h is required to select the correct PWM channel for GP12
    - rules.mk to define the audio driver class used by
    - config.h to configure the driver and and channel

--- keyboards/0xcb/newhorizons/rp2040_ce/config.h
+++ keyboards/0xcb/newhorizons/rp2040_ce/config.h
@@ -5,6 +5,9 @@
 
 #ifdef AUDIO_ENABLE
 #    define AUDIO_PIN GP12
+#    define AUDIO_PWM_DRIVER PWMD6
+#    define AUDIO_PWM_CHANNEL RP2040_PWM_CHANNEL_A
 #    define AUDIO_CLICKY
 #endif
+
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led which blinks when entering the bootloader
