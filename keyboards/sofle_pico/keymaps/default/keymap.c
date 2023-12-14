461432b6fc91bc6b441837662ba5299ef8a833e2
Ryan Neff <ryan.ray.neff@fluke.com>
2023-12-14 14:59:44 -0800

    Removed tapdance from default config and keymap

--- keyboards/sofle_pico/keymaps/default/keymap.c
+++ keyboards/sofle_pico/keymaps/default/keymap.c
@@ -22,31 +22,11 @@ enum custom_keycodes {
     KC_DLINE
 };
 
-/**
- * Tap Dance declarations
- */
-enum tapdances {
-    _TD_FIVE_ENTER,
-};
-
-/* Tapdance */
-#define TD5ENT TD(_TD_FIVE_ENTER)
-
-/**
- * Tap Dance definitions
- *
- * To use this in the configurator, enter the name 'TD5ENT' in the "Any" key.
- */
-tap_dance_action_t tap_dance_actions[] = {
-    /* Tap once for 5, twice for Enter. */
-    [_TD_FIVE_ENTER] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_ENT),
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |TD5ENT|                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -60,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    TD5ENT,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
--- keyboards/sofle_pico/keymaps/jellytitan/rules.mk
+++ keyboards/sofle_pico/keymaps/jellytitan/rules.mk
@@ -1,4 +1,4 @@
 #
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pimoroni_trackball
-VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
