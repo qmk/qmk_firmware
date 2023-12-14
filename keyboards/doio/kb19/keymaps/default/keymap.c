c9b74efb149d56107354d9fd96822845ba6e0ebb
DOIO2022 <116554792+DOIO2022@users.noreply.github.com>
2023-12-14 08:20:21 +0800

    Update keyboards/doio/kb19/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/doio/kb19/keymaps/default/keymap.c
+++ keyboards/doio/kb19/keymaps/default/keymap.c
@@ -19,10 +19,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-		KC_NUM, KC_PSLS, KC_PAST, KC_PMNS, RGB_MOD,
-		KC_P7,  KC_P8,   KC_P9,   KC_PPLS, RGB_TOG,
-		KC_P4,  KC_P5,   KC_P6,
-		KC_P1,  KC_P2,   KC_P3,   KC_PENT,
+        KC_NUM, KC_PSLS, KC_PAST, KC_PMNS, RGB_MOD,
+        KC_P7,  KC_P8,   KC_P9,   KC_PPLS, RGB_TOG,
+        KC_P4,  KC_P5,   KC_P6,
+        KC_P1,  KC_P2,   KC_P3,   KC_PENT,
         KC_P0,  KC_PDOT, KC_MUTE
   )
 };
