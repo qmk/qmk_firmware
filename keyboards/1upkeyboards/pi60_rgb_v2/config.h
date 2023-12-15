b29fec3f08f3620b50bb14b01f0f32186723eba1
ziptyze <mwcordier@gmail.com>
2023-12-14 21:46:28 -0500

    Update keyboards/1upkeyboards/pi60_rgb_v2/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/1upkeyboards/pi60_rgb_v2/config.h
+++ keyboards/1upkeyboards/pi60_rgb_v2/config.h
@@ -16,10 +16,7 @@
 
 #pragma once
 
-#define RGB_MATRIX_LED_COUNT 104
-#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 #define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
