2c20bc5d1f259891cc10bb01196d77620cb72e1b
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2023-12-14 19:27:59 -0500

    NCC Updates and via Support plus more clean up

---
+++ keyboards/themadnoodle/ncc1701kb/v2new/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 Jesse Leventhal (@The=Mad-Noodle)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*RGB Defaults*/
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
+#define RGBLIGHT_DEFAULT_SPD 3
