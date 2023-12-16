2e5fad73cfede8b1ea140392c0434996c6d06bea
Pierre BAUDEMONT <pierre.baudemont@gmail.com>
2023-12-16 18:31:18 +0100

    nibelheims' keymap for lily58

---
+++ keyboards/lily58/keymaps/nibelheims/README.md
@@ -0,0 +1,11 @@
+# Nibelheims' keymap
+Meant for the rev1 lily58 with Elite-Pi controllers and OLED displays.
+Use the default keymap with the following changes:
+- OLED enabled
+- VIA enabled
+- NKRO enabled
+- BOOTMAGIC_LITE enabled, on ESC and \`
+- vertical content on left screen with WPM counter, layers, mod keys and caps lock status
+- keylogger disabled, it's not fun having your passwords displayed...
+- caps lock on lower-lshift
+
---
+++ keyboards/lily58/keymaps/nibelheims/rules.mk
@@ -0,0 +1,19 @@
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+OLED_ENABLE= yes     # OLED display
+VIA_ENABLE = yes
+WPM_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+
+CONVERT_TO=elite_pi
+
+# If you want to change the display of OLED, you need to change here
+SRC += ./lib/logo_reader.c
