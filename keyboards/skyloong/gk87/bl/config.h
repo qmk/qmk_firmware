74e66492f19fbe4fdc03d42325b97a56c14415a7
JIKEDINGZHI <52481939+NaturalZh@users.noreply.github.com>
2023-12-16 09:50:47 +0800

    Update keyboards/skyloong/gk87/bl/config.h
    OK, I see.
    
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/skyloong/gk87/bl/config.h
+++ keyboards/skyloong/gk87/bl/config.h
@@ -3,11 +3,5 @@
 
 #pragma once
 
-#define LED_CAPS_LOCK_PIN C15
-#define LED_SCROLL_LOCK_PIN C14
 #define MAC_PIN C13
-#define LED_PIN_ON_STATE 0
-
-#define BACKLIGHT_DEFAULT_ON 1
-
 #define ENCODER_MAP_KEY_DELAY 20
