516c5ca4406c44a9d016706a971cec0633288d47
Patricio Poo <patopoo@gmail.com>
2023-12-18 19:38:09 -0300

    se agrega layout español latino americano. new layout for latin american spanish

--- keyboards/crkbd/keymaps/ppoo/config.h
+++ keyboards/crkbd/keymaps/ppoo/config.h
@@ -16,7 +16,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 #pragma once
