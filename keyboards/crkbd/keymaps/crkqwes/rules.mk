370c9493d043406597803f87184422e35862a37d
Arnold Saltmiris <arnoldsalt@hotmail.com>
2023-12-18 18:18:38 +0000

    Adding Corne back in

---
+++ keyboards/crkbd/keymaps/crkqwes/rules.mk
@@ -0,0 +1,6 @@
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = yes
+MOUSEKEY_ENABLE    = no
+NKRO_ENABLE        = yes
+OLED_ENABLE        = yes
+LTO_ENABLE         = yes
---
+++ keyboards/crkbd/keymaps/rjhilgefort/rules.mk
@@ -0,0 +1,7 @@
+# For Elite-C
+BOOTLOADER = atmel-dfu
+
+# https://www.reddit.com/r/olkb/comments/9pyc0u/qmk_media_keys_are_not_working/
+EXTRAKEY_ENABLE = yes
+
+OLED_ENABLE = yes
