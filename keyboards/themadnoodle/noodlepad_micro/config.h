9ae636d857f220fae1a5f24d4bddad9d883fbe07
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2023-12-18 18:47:54 -0500

    Update keyboards/themadnoodle/noodlepad_micro/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/themadnoodle/noodlepad_micro/config.h
+++ keyboards/themadnoodle/noodlepad_micro/config.h
@@ -3,27 +3,4 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
-
-/* RGB BackLight */
-#define WS2812_PIO_USE_PIO1
-#define WS2812_DI_PIN GP29
- 
-/*RGB Defaults*/
 #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL + 5
-/* See info.json for more RGB settings*/
