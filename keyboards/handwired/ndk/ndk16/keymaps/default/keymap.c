2d8ffc284411db9379577cd7c5aaa55d0ffa02ab
Kevin <khoa.kevin@gmail.com>
2023-12-17 23:48:47 -0500

    update to keymap

--- keyboards/handwired/ndk/ndk16/keymaps/default/keymap.c
+++ keyboards/handwired/ndk/ndk16/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───────┴───┴───┘
      */
     [0] = LAYOUT_numpad_5x5(
-        KC_MUTE, KC_COMM, KC_PSLS, KC_PAST,  KC_PMNS,
+        KC_MUTE, KC_NUM, KC_PSLS, KC_PAST,  KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,
         KC_P1,   KC_P2,   KC_P3, 
