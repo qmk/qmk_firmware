94577f91afdf38e5e09ea3cd0f767d0bab245105
gkeyboard <giggox_man@hotmail.com>
2023-12-18 21:48:38 +0700

    Update keyboards/gkeyboard/greatpad/keymaps/via/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/gkeyboard/greatpad/keymaps/via/keymap.c
+++ keyboards/gkeyboard/greatpad/keymaps/via/keymap.c
@@ -10,70 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P7,    KC_P8,    KC_P9,      KC_PSLS,    KC_NUM,
         KC_P4,    KC_P5,    KC_P6,      KC_PAST,    KC_PGUP,
         KC_P1,    KC_P2,    KC_P3,      KC_PMNS,    KC_PGDN,
-        KC_P0,    KC_PDOT,  KC_PENT,    KC_PPLS,    TO(1)
-    ),
-    [1] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(2)
-    ),
-    [2] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(3)
-    ),
-    [3] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(4)
-    ),
-    [4] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(5)
-    ),
-    [5] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(6)
-    ),
-    [6] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(7)
-    ),
-    [7] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(8)
-    ),
-    [8] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(9)
-    ),
-    [9] = LAYOUT(
-        KC_TRNS,                                    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    TO(0)
+        KC_P0,    KC_PDOT,  KC_PENT,    KC_PPLS,    _______
     )
 };
 
