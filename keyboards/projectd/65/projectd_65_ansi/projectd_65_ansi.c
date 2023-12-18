097345407b3f6c72f8a427c1ffdd59b28877a484
Ryan <fauxpark@gmail.com>
2023-12-19 07:13:08 +1100

    Update keyboard LED driver configs (#22638)

--- keyboards/projectd/65/projectd_65_ansi/projectd_65_ansi.c
+++ keyboards/projectd/65/projectd_65_ansi/projectd_65_ansi.c
@@ -18,7 +18,7 @@
 
 #ifdef RGB_MATRIX_ENABLE
 
-const aw20216s_led_t g_aw20216s_leds[RGB_MATRIX_LED_COUNT] = {
+const aw20216s_led_t PROGMEM g_aw20216s_leds[AW20216S_LED_COUNT] = {
 /* Refer to AW20216S manual for these locations
  *   driver
  *   |     R location
