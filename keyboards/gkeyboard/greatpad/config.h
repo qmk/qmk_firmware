c733059c6b2bc855d1ccc6c7d9787e8d24e7f04a
gkeyboard <giggox_man@hotmail.com>
2023-12-18 21:46:19 +0700

    Update keyboards/gkeyboard/greatpad/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/gkeyboard/greatpad/config.h
+++ keyboards/gkeyboard/greatpad/config.h
@@ -9,11 +9,3 @@
 #define I2C_DRIVER I2CD0
 #define OLED_BRIGHTNESS 128
 #define OLED_FONT_H  "lib/glcdfont.c"
-
-#define ENCODER_DIRECTION_FLIP          // If your encoder’s clockwise directions are incorrect, you can swap the A & B pad definitions
-
-#define RGB_MATRIX_LED_COUNT 28
-
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define RGB_MATRIX_KEYPRESSES           // reacts to keypresses
-#define RGB_DISABLE_WHEN_USB_SUSPENDED  // turn off effects when suspended
