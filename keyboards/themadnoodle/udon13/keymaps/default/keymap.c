a883f1ad5269f248aecfd4bbb881490435ae7cf1
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2023-12-19 16:23:56 -0500

    Update keymap.c

--- keyboards/themadnoodle/udon13/keymaps/default/keymap.c
+++ keyboards/themadnoodle/udon13/keymaps/default/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /*Layer 0*/
     [0] = LAYOUT(
-  	LT(1, KC_MUTE), 
+  	 	KC_MUTE,
 		KC_MPLY, KC_MPRV, KC_MNXT, KC_MSEL, 
 		KC_UNDO, KC_CALC, KC_MAIL, KC_MYCM, 
 		KC_COPY, KC_CUT, KC_PSTE, TO(3)
