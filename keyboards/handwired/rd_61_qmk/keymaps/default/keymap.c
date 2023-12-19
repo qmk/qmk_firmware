c141bfcf902eb7252e62bc146915148313d4dcef
Abhijithabhi <abhiabhijith367@gmail.com>
2023-12-19 19:02:51 +0530

    Update keymap.c

--- keyboards/handwired/rd_61_qmk/keymaps/default/keymap.c
+++ keyboards/handwired/rd_61_qmk/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DB_TOGG, QK_RBT,  QK_BOOT, QK_MAKE,
             KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-            KC_TRNS,           KC_TRNS,      KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS, DF(3),   DF(2),   DF(1),   DF(0),  RGB_VAI,          KC_TRNS,
+            KC_TRNS,           KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAI,          KC_TRNS,
             KC_TRNS,  KC_TRNS, KC_TRNS,                                     KC_TRNS,                            RGB_MOD, KC_TRNS, RGB_HUI, RGB_SAI
   )
 };
