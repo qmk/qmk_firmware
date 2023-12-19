b6713b9c38213076e6360d94260fddcfa96cf7c9
Abhijithabhi <abhiabhijith367@gmail.com>
2023-12-20 00:46:06 +0530

    Update keyboards/handwired/rd_61_qmk/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/handwired/rd_61_qmk/config.h
+++ keyboards/handwired/rd_61_qmk/config.h
@@ -2,11 +2,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
 /* QK_MAKE support*/
 #define ENABLE_COMPILE_KEYCODE
