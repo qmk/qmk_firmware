600b625b9d28c079a3e055951e8f4e15fe2422a2
Drashna Jael're <drashna@live.com>
2023-12-20 15:25:04 -0800

    Update adafruit macropad to use shutdown pin

--- keyboards/adafruit/macropad/macropad.c
+++ keyboards/adafruit/macropad/macropad.c
@@ -40,28 +40,3 @@ led_config_t g_led_config = { {
 } };
 
 #endif
-
-#ifdef AUDIO_ENABLE
-void keyboard_pre_init_kb(void) {
-    // ensure pin is set and enabled pre-audio init
-    setPinOutput(SPEAKER_SHUTDOWN);
-    writePinHigh(SPEAKER_SHUTDOWN);
-    keyboard_pre_init_user();
-}
-
-void keyboard_post_init_kb(void) {
-    // set pin based on active status
-    writePin(SPEAKER_SHUTDOWN, audio_is_on());
-    keyboard_post_init_user();
-}
-
-void audio_on_user(void) {
-    writePinHigh(SPEAKER_SHUTDOWN);
-}
-
-void audio_off_user(void) {
-    // needs a delay or it runs right after play note.
-    wait_ms(200);
-    writePinLow(SPEAKER_SHUTDOWN);
-}
-#endif
