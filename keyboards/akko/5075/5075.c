f8ead3aa4b67d1852895896cb2522723ac8d8dbc
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2023-12-20 09:24:07 +0800

    Update 5075.c

--- keyboards/akko/5075/5075.c
+++ keyboards/akko/5075/5075.c
@@ -189,7 +189,6 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
                     layer_off(WIN_W);
                 }
                 keymap_config.no_gui = 0;
-                eeconfig_update_keymap(keymap_config.raw);
             } else {
                 if (default_layer_state & (1<<MAC_W)) {
                     set_single_persistent_default_layer(WIN_W);
@@ -199,8 +198,6 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
                     layer_off(MAC_W);
                 }
             }
-            writePin(LED_MAC_OS_PIN,  default_layer_state & ((1<<MAC_B)|(1<<MAC_W)));
-            writePin(LED_WIN_LOCK_PIN, keymap_config.no_gui);
         default:
             break;
     }
