09cb5453ac788730dea5e02a3b1e7342736c2445
Vertex-kb <102476474+Vertex-kb@users.noreply.github.com>
2023-12-22 14:44:21 +0800

    Update keyboards/vertex/cycle8/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/vertex/cycle8/config.h
+++ keyboards/vertex/cycle8/config.h
@@ -5,14 +5,3 @@
 
 #define WS2812_SPI_DRIVER SPID2
 #define WS2812_SPI_MOSI_PAL_MODE 5
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
