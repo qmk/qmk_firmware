79d64b0b15081bbdae02f0b94b96b0a2fe7ba2e3
Zach DeCook <zachdecook@librem.one>
2023-12-23 10:53:27 -0500

    get rid of custom effect

--- keyboards/hexboard/1_1/rules.mk
+++ keyboards/hexboard/1_1/rules.mk
@@ -1,6 +1,4 @@
 SRC += matrix.c
 CUSTOM_MATRIX = lite
 
-RGB_MATRIX_CUSTOM_KB = yes
-
 AUDIO_DRIVER = pwm_hardware
--- keyboards/hexboard/hexboard.c
+++ keyboards/hexboard/hexboard.c
@@ -3,14 +3,6 @@
 
 #include "quantum.h"
 
-#ifdef RGB_MATRIX_ENABLE
-void keyboard_post_init_kb(void) {
-    // Start them OFF
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_my_off_effect);
-    keyboard_post_init_user();
-}
-#endif
-
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
 #ifdef AUDIO_ENABLE
