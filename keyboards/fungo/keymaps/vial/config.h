09b01612d5119dc3148323b624b1f3b559257cb7
bojiguard <107382014+bojiguard@users.noreply.github.com>
2023-12-27 15:22:37 +0900

    Delete keyboards/fungo/keymaps/vial directory
    vial is no use in qmk

--- keyboards/fungo/keymaps/vial/config.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#define VIAL_KEYBOARD_UID {0xD1, 0xAB, 0x55, 0xD9, 0x6D, 0x23, 0x19, 0x43}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 6 }
-#define VIAL_UNLOCK_COMBO_COLS { 6, 0 }
-
