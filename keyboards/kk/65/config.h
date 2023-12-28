51d73f0c4a71e4ecdd7ef72ebab10bdcd4c8dae1
Apricity-Projects <1koryhicks@gmail.com>
2023-12-27 22:09:33 -0800

    adding my keyboard

---
+++ keyboards/kk/65/config.h
@@ -0,0 +1,22 @@
+// Copyright 2023 Kory Hicks (@apricity-spam)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define ENABLE_COMPILE_KEYCODE
+#define TAPPING_TOGGLE 2
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
