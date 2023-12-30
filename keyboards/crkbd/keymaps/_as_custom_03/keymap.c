fb6c93edf00fd02f45747ab8c72ff83f7d9bce16
Arnold Saltmiris <arnoldsalt@hotmail.com>
2023-12-30 18:22:01 +0000

    Added something

--- keyboards/crkbd/keymaps/_as_custom_03/keymap.c
+++ keyboards/crkbd/keymaps/_as_custom_03/keymap.c
@@ -1,45 +1,34 @@
 /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-#include "config.h" 
+#include "config.h"
+
+extern keymap_config_t keymap_config;
 
 enum layers {
     _QWERTY,
     _NUMP,
     _LOWER,
     _FUNC,
-    _RGB,
+    _CRGB
 };
+
+enum custom_layers {
+    QWERTY = SAFE_RANGE,
+    NUMP,
+    LOWER,
+    FUNC,
+    CRGB,
+};
+
 
 uint8_t mod_state;
 
-/* CHAT GPT Code*/
-// Define custom keycodes
-enum custom_keycodes {
-    RGB_TOGGLE = SAFE_RANGE,
-};
-
-
-/* CHAT GPT Code END*/
-
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT_split_3x6_3(
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T			             ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSPC ,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -47,20 +36,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_LCTL ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B  		                 ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_ESC  ,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_LGUI ,MO(1)   ,KC_SPC     ,KC_ENT  ,MO(2)   ,MO(3)
+                                           KC_LGUI ,MO(NUMP),KC_SPC     ,KC_ENT  ,MO(LOWER),MO(FUNC)
                                         //`--------------------------'  `--------------------------'
 
         ),
 
     [_NUMP] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-           KC_TAB  ,XXXXXXX ,KC_UP   ,XXXXXXX ,XXXXXXX ,KC_NO	                     ,KC_P7   ,KC_P8   ,KC_P9   ,XXXXXXX ,XXXXXXX ,KC_BSPC ,
+           KC_TAB  ,KC_NO   ,KC_UP   ,KC_NO   ,KC_NO   ,KC_NO	                     ,KC_P7   ,KC_P8   ,KC_P9   ,KC_NO ,KC_NO ,KC_BSPC ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           KC_LSFT ,KC_LEFT ,KC_DOWN ,KC_RGHT ,XXXXXXX ,KC_NO  		                 ,KC_P4   ,KC_P5   ,KC_P6   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+           KC_LSFT ,KC_LEFT ,KC_DOWN ,KC_RGHT ,KC_NO   ,KC_NO  		                 ,KC_P4   ,KC_P5   ,KC_P6   ,KC_NO ,KC_NO ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           KC_LCTL ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_NO			             ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PDOT ,XXXXXXX ,XXXXXXX ,
+           KC_LCTL ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO			             ,KC_P1   ,KC_P2   ,KC_P3   ,KC_PDOT ,KC_NO ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                               KC_LGUI ,_______ ,KC_SPC     ,KC_ENT  ,KC_P0   ,MO(4)
+                                               KC_LGUI ,_______ ,KC_SPC     ,KC_ENT  ,KC_P0   ,MO(CRGB)
                                             //`--------------------------'  `--------------------------'
         ),
 
@@ -68,35 +57,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY] = LAYOUT
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
            KC_TAB  ,KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC		                 ,KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,KC_BSPC ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           KC_LSFT ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_NO			             ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_LBRC ,KC_RBRC ,XXXXXXX ,
+           KC_LSFT ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO			             ,KC_NO  ,KC_NO    ,KC_NO   ,KC_LBRC ,KC_RBRC ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           KC_LCTL ,XXXXXXX ,KC_PSLS ,XXXXXXX ,KC_HASH ,KC_UNDS   		             ,KC_PMNS ,KC_PLUS ,KC_EQL  ,KC_LT   ,KC_GT   ,XXXXXXX ,
+           KC_LCTL ,KC_NO ,KC_PSLS ,KC_NO ,KC_HASH ,KC_UNDS   		             ,KC_PMNS ,KC_PLUS ,KC_EQL  ,KC_LT   ,KC_GT   ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                               KC_LGUI ,XXXXXXX   ,KC_SPC     ,KC_ENT  ,_______ ,XXXXXXX
+                                               KC_LGUI ,KC_NO   ,KC_SPC     ,KC_ENT  ,_______ ,KC_NO
                                             //`--------------------------'  `--------------------------'
         ),
 
     [_FUNC] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-           XXXXXXX ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5                        ,KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,XXXXXXX ,
+           KC_NO ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5                        ,KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           XXXXXXX ,KC_F11  ,KC_F12  ,XXXXXXX ,XXXXXXX ,XXXXXXX                      ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+           KC_NO ,KC_F11  ,KC_F12  ,KC_NO ,KC_NO ,KC_NO                      ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX                      ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+           KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO                      ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                               KC_LGUI ,XXXXXXX ,KC_SPC     ,KC_ENT  ,XXXXXXX ,_______
+                                               KC_LGUI ,KC_NO ,KC_SPC     ,KC_ENT  ,KC_NO ,_______
                                             //`--------------------------'  `--------------------------'
         ),
 
-    [_RGB] = LAYOUT_split_3x6_3(
+    [_CRGB] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-           QK_BOOT ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX                      ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+           QK_BOOT ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO                      ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           RGB_TOG ,RGB_HUI ,RGB_SAI ,RGB_VAI ,XXXXXXX ,XXXXXXX                      ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+           RGB_TOG ,RGB_HUI ,RGB_SAI ,RGB_VAI ,KC_NO ,KC_NO                      ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           RGB_MOD ,RGB_HUD ,RGB_SAD ,RGB_VAD ,XXXXXXX, XXXXXXX                      ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+           RGB_MOD ,RGB_HUD ,RGB_SAD ,RGB_VAD ,KC_NO, KC_NO                      ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                               KC_LGUI ,_______ ,KC_SPC     ,KC_ENT  ,_______ ,XXXXXXX
+                                               KC_LGUI ,_______ ,KC_SPC     ,KC_ENT  ,_______ ,KC_NO
                                             //`--------------------------'  `--------------------------'
         )};
 
@@ -255,21 +244,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     #endif 
                      
     switch (keycode) {
-        case _QWERTY:       /*_QWERTY*/
-            print("_QWERTY \n ");
-            break;
-        case 21025:         /*_NUMP*/
-            print("_NUMP \n ");
-            break;
-        case 21026:         /*_LOWER*/
+        case QWERTY:       /*_QWERTY*/
+            // if (record->event.pressed) {
+            //     layer_on(_QWERTY);
+            // } else {
+            //     layer_off(_QWERTY);
+            // }
+            // print("_QWERTY \n ");
+            // return false;
+
+        case 21025:         /*_NUMP  21025*/
+            print("_NUMP \n "); 
+
+
+                 #ifdef RGB_MATRIX_ENABLE
+                    if (record->event.pressed) {
+                        rgb_matrix_enable();
+                    }
+                #endif
+      break;
+    
+        case 21026:         /*_LOWER  21026*/
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+            } else {
+                layer_off(_LOWER);
+            }
             print("_LOWER \n ");
+            return false;
             break;
-        case 21027:         /*_FUNC*/
-            print("_FUNC \n ");
+
+        case 21027:         /*_FUNC  21027*/
             break;
-        case 21028:         /*_RGB*/
-            print("_RGB \n ");
+        case 21027:         /*_RGB  21028*/
             break;
+
     }
     return true;
 };
@@ -320,17 +329,3 @@ bool oled_task_user(void) {
 }
 
 // Define a function to set RGB for a specific key when the RGB layer is active
-
-
-// bool rgb_matrix_indicators_user(void) {
-// #ifdef RGB_MATRIX_ENABLE
-//     if (layer_state_is(_NUMP)) {
-//         // Set RGB for a specific key (e.g., KC_A) when the RGB layer is active
-//         rgb_matrix_set_color(1, 0xFF, 0x00, 0x00); // Red color for key KC_A
-//     } else {
-//         // Set default RGB for the key when the RGB layer is not active
-//         rgb_matrix_set_color(1, 0x00, 0x00, 0x00); // Turn off RGB for key KC_A
-//     }
-//     return false;
-// }
-// #endif
