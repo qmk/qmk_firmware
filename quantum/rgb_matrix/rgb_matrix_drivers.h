dd5f014996c2baf48bd2c444d86b94850cdef3c4
Ryan <fauxpark@gmail.com>
2024-01-01 12:40:35 +1100

    LED drivers: extract IS31FL3742A from IS31COMMON (#22620)

--- quantum/rgb_matrix/rgb_matrix_drivers.h
+++ quantum/rgb_matrix/rgb_matrix_drivers.h
@@ -19,6 +19,8 @@
 #    include "is31fl3737.h"
 #elif defined(RGB_MATRIX_IS31FL3741)
 #    include "is31fl3741.h"
+#elif defined(RGB_MATRIX_IS31FL3742A)
+#    include "is31fl3742a.h"
 #elif defined(IS31FLCOMMON)
 #    include "is31flcommon.h"
 #elif defined(RGB_MATRIX_SNLED27351)
