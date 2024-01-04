817aedd6a53905ea2ee2a8679ba6443e53b16f68
Ishikawa <r.ishikawa@cartahd.com>
2024-01-04 23:55:17 +0900

    fix

--- keyboards/keyboardio/atreus/keymaps/ryu955/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/ryu955/keymap.c
@@ -26,8 +26,8 @@ enum custom_keycodes {
 #define MONUP LCTL(KC_UP)
 #define MONRGHT LCTL(KC_RGHT)
 #define MONCH LCTL(KC_SCLN)
-#define LT_REISU LT(_RS, KC_LANG2) // Eisū (英数) on macOS
-#define LT_LKANA LT(_LW, KC_LANG1) // Kana (かな) on macOS
+#define LT_REISU LT(_RS, KC_LNG2) // Eisū (英数) on macOS
+#define LT_LKANA LT(_LW, KC_LNG1) // Kana (かな) on macOS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QW] = LAYOUT( /* Qwerty */
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
             KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
             _______, KC_LBRC, KC_LCBR, KC_PLUS, _______, KC_LCBR, KC_RCBR, _______, KC_EQL,  KC_RCBR, KC_RBRC, _______,
-            _______, _______, _______, _______, _______, SSC,     SL,      _______, KC_DEL,  _______, _______, RESET),
+            _______, _______, _______, _______, _______, SSC,     SL,      _______, KC_DEL,  _______, _______, RTSEN),
 
     [_LW] = LAYOUT( /* [> LOWER <] */
             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
