81398f9fc1e09c486ca466477a3b517536711e42
Richard Dawe <richdawe@gmail.com>
2024-01-06 22:42:56 +0000

    Rework F-keys; add media keys; tweak number row to add colon and dot

--- keyboards/kiserdesigns/monorail2/keymaps/richdawe/keymap.c
+++ keyboards/kiserdesigns/monorail2/keymaps/richdawe/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *    ,-----+-----+-----+-----+-----+-----.
    *    |     |     |     |     |     |     |
    * ,--+-----+-----+-----+-----+-----+-----------------------------------------.
-   * |     |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |        |
+   * |  :  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |    .   |
    * |--------------------------------------------------------------------------+
    * |      |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |   \   |
    * |--------------------------------------------------------------------------+
@@ -53,31 +53,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |     |      |               |             |      |Home |PgDn | End |
    * `--------------------------------------------------------------------------'
    */
-  _______, _______, _______, _______, _______, _______, \
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0),    KC_BSLS, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______, \
-  _______, _______, _______, _______,      _______,          _______,     _______, KC_HOME, KC_PGDN, KC_END
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  KC_COLN, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DOT,  \
+  XXXXXXX, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_BSLS, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, _______, \
+  _______, _______, _______, _______,      _______,          _______,     XXXXXXX, KC_HOME, KC_PGDN, KC_END
 ),
 [2] = LAYOUT( \
-/* F-keys
+/* F-keys and media
    *    ,-----+-----+-----+-----+-----+-----.
    *    |     |     |     |     |     |     |
    * ,--+-----+-----+-----+-----+-----+-----------------------------------------.
-   * |     |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |     |     |     |     |        |
+   * | F12 |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 |  F11   |
    * |--------------------------------------------------------------------------+
-   * |      |  F6 |  F7 |  F8 |  F9 | F10 | F12 |     |     |     |     |       |
+   * |      |     |     |     |     |     |     |     |     |     |     |       |
    * |--------------------------------------------------------------------------+
-   * |        |     |     |     |     |     |     |     |     |     |     |     |
+   * |        |     |     |     |     |     |     |     |     |Mute |VolUp|     |
    * |--------------------------------------------------------------------------+
-   * |      |     |      |               |             |      |     |     |     |
+   * |      |     |      |               | Play/Pause  |      |Prev |VolDn|Next |
    * `--------------------------------------------------------------------------'
    */
-  _______, _______, _______,  _______, _______, _______, \
-  _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, _______, \
-  _______, KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, \
-  _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______,  _______,      _______,          _______,     _______, _______, _______, _______
+  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, \
+  KC_F12,  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLU, _______, \
+  _______, _______, _______,  _______,      _______,          KC_MPLY,     XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT
 ),
 [3] = LAYOUT( \
 /* RGB and boot
@@ -93,11 +93,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Boot |     |      |               |             |      |     |     |     |
    * `--------------------------------------------------------------------------'
    */
-  _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, _______, \
-  QK_BOOT, _______, _______, _______,      _______,          _______,     _______, _______, _______, _______
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, XXXXXXX, \
+  QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,          XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 };
 
