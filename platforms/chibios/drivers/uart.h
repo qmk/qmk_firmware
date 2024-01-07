095a01529d01a186666d5c67abf0fd503d0ba2b0
Stefan Kerkmann <karlk90@pm.me>
2024-01-07 17:36:11 +0100

    chibios: uart: add SIO driver and RP2040 compatibility
    Signed-off-by: Stefan Kerkmann <karlk90@pm.me>
    Co-authored-by: Sergey Vlasov <sigprof@gmail.com>

--- platforms/chibios/drivers/uart.h
+++ platforms/chibios/drivers/uart.h
@@ -1,18 +1,7 @@
-/* Copyright 2021
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+// Copyright 2024 Stefan Kerkmann
+// Copyright 2021 QMK
+// Copyright 2024 Stefan Kerkmann
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
@@ -25,7 +14,11 @@
 #include "chibios_config.h"
 
 #ifndef UART_DRIVER
-#    define UART_DRIVER SD1
+#    if (HAL_USE_SERIAL == TRUE)
+#        define UART_DRIVER SD1
+#    elif (HAL_USE_SIO == TRUE)
+#        define UART_DRIVER SIOD1
+#    endif
 #endif
 
 #ifndef UART_TX_PIN
@@ -103,14 +96,54 @@
 #    define UART_ATFLCT 0
 #endif
 
+/**
+ * @brief Initialize the UART driver. This function must be called only once,
+ * before any of the below functions can be called.
+ *
+ * @param baud The baud rate to transmit and receive at. This may depend on the
+ * device you are communicating with. Common values are 1200, 2400, 4800, 9600,
+ * 19200, 38400, 57600, and 115200.
+ */
 void uart_init(uint32_t baud);
 
+/**
+ * @brief Transmit a single byte.
+ *
+ * @param data The byte to transmit.
+ */
 void uart_write(uint8_t data);
 
+/**
+ * @brief Receive a single byte.
+ *
+ * @return uint8_t The byte read from the receive buffer. This function will
+ * block if the buffer is empty (ie. no data to read).
+ */
 uint8_t uart_read(void);
 
+/**
+ * @brief Transmit multiple bytes.
+ *
+ * @param data A pointer to the data to write from.
+ * @param length The number of bytes to write. Take care not to overrun the
+ * length of `data`.
+ */
 void uart_transmit(const uint8_t *data, uint16_t length);
 
+/**
+ * @brief Receive multiple bytes.
+ *
+ * @param data A pointer to the buffer to read into.
+ * @param length The number of bytes to read. Take care not to overrun the
+ * length of `data`.
+ */
 void uart_receive(uint8_t *data, uint16_t length);
 
+/**
+ * @brief Return whether the receive buffer contains data. Call this function
+ * to determine if `uart_read()` will return data immediately.
+ *
+ * @return true If there is data available to read.
+ * @return false If there is no data available to read.
+ */
 bool uart_available(void);
---
+++ platforms/chibios/drivers/uart_sio.c
@@ -0,0 +1,77 @@
+// Copyright 2024 Stefan Kerkmann (@KarlK90)
+// Copyright 2021 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "uart.h"
+
+#if defined(MCU_RP)
+// 38400 baud, 8 data bits, 1 stop bit, no parity, no flow control
+static SIOConfig sioConfig = {
+    .baud      = SIO_DEFAULT_BITRATE,
+    .UARTLCR_H = (UART_UARTLCR_H_WLEN_8BITS | UART_UARTLCR_H_FEN),
+    .UARTCR    = 0U,
+    .UARTIFLS  = (UART_UARTIFLS_RXIFLSEL_1_8F | UART_UARTIFLS_TXIFLSEL_1_8E),
+    .UARTDMACR = 0U,
+};
+#else
+static SIOConfig sioConfig = {
+    .baud  = SIO_DEFAULT_BITRATE,
+#    if defined(MCU_STM32) && defined(CHIBIOS_HAL_USARTv3)
+    .presc = USART_PRESC1,
+#    endif
+    .cr1   = UART_CR1,
+    .cr2   = UART_CR2,
+    .cr3   = UART_CR3,
+};
+#endif
+
+void uart_init(uint32_t baud) {
+    static bool is_initialised = false;
+
+    if (is_initialised) {
+        return;
+    }
+    is_initialised = true;
+
+    sioConfig.baud = baud;
+
+#if defined(USE_GPIOV1)
+    palSetLineMode(UART_TX_PIN, UART_TX_PAL_MODE);
+    palSetLineMode(UART_RX_PIN, UART_RX_PAL_MODE);
+#else
+    palSetLineMode(UART_TX_PIN, PAL_MODE_ALTERNATE(UART_TX_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST);
+    palSetLineMode(UART_RX_PIN, PAL_MODE_ALTERNATE(UART_RX_PAL_MODE) | PAL_OUTPUT_TYPE_PUSHPULL | PAL_OUTPUT_SPEED_HIGHEST);
+#endif
+
+    sioStart(&UART_DRIVER, &sioConfig);
+}
+
+void uart_write(uint8_t data) {
+    chnPutTimeout(&UART_DRIVER, data, TIME_INFINITE);
+}
+
+uint8_t uart_read(void) {
+    msg_t result = chnGetTimeout(&UART_DRIVER, TIME_INFINITE);
+
+    if (sioHasRXErrorsX(&UART_DRIVER)) {
+        sioGetAndClearErrors(&UART_DRIVER);
+    }
+
+    return (uint8_t)result;
+}
+
+void uart_transmit(const uint8_t *data, uint16_t length) {
+    chnWrite(&UART_DRIVER, data, length);
+}
+
+void uart_receive(uint8_t *data, uint16_t length) {
+    chnRead(&UART_DRIVER, data, length);
+
+    if (sioHasRXErrorsX(&UART_DRIVER)) {
+        sioGetAndClearErrors(&UART_DRIVER);
+    }
+}
+
+bool uart_available() {
+    return !sioIsRXEmptyX(&UART_DRIVER);
+}
