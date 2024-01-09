45304d8154bb79f7b79a954c955672885b8f2a54
mechlovin <57231893+mechlovin@users.noreply.github.com>
2024-01-09 18:40:04 +0700

    Update keyboards/mechlovin/infinity875/rev1/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechlovin/infinity875/rev1/config.h
+++ keyboards/mechlovin/infinity875/rev1/config.h
@@ -17,6 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 17
+
+#define MATRIX_ROW_PINS { D5, D2, D4, D3, D0, D1 }
+
+#define DIODE_DIRECTION ROW2COL
+
 #define RGB_MATRIX_LED_COUNT 42
 #define RGB_MATRIX_SLEEP // turn off effects when suspended
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
