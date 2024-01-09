4d805e26dec9da0d50beb40c824d3669a2de175d
Dominik Tarnowski <dom@tdom.dev>
2024-01-09 21:19:26 +0000

    updates, more emacs centric

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -23,9 +23,6 @@ const uint16_t PROGMEM combo_dqt[] = {KC_X, KC_C, COMBO_END};
 bool is_alt_tab_active = false;
 uint16_t alt_tab_timer = 0;
 
-bool is_alt_tab_active2 = false;
-uint16_t alt_tab_timer2 = 0;
-
 combo_t key_combos[] = {
     COMBO(combo_btn1l, KC_BTN1),
     COMBO(combo_btn2l, KC_BTN2),
@@ -34,11 +31,7 @@ combo_t key_combos[] = {
     COMBO(combo_play, KC_MPLY),
     COMBO(combo_tab, KC_TAB),
     COMBO(combo_enter, KC_ENTER),
-    COMBO(combo_esc, KC_ESC),
     COMBO(combo_rctl, OSM(MOD_LCTL)),
-    COMBO(combo_semi, KC_SEMICOLON),
-    COMBO(combo_colon, KC_COLON),
-    COMBO(combo_backtick, KC_GRAVE),
     COMBO(combo_dqt, KC_DQT),
 };
 
@@ -54,26 +47,26 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
-  ALT_TAB,         KC_1,               KC_2,                 KC_3,                KC_4,                  KC_5,    KC_EQL,                   LCTL(KC_H),     KC_6,                  KC_7,                  KC_8,                      KC_9,                 KC_0,                   KC_DELETE,
-  KC_TAB,          KC_Q,               KC_W,                 KC_F,                KC_P,                  KC_B,    KC_SLASH,                  KC_UP,          KC_J,                  KC_L,                  KC_U,                      KC_Y,                 KC_BACKSLASH,           KC_BSPC,
+  ALT_TAB,         KC_1,               KC_2,                 KC_3,                KC_4,                  KC_5,    KC_TILD,                   LCTL(KC_H),     KC_6,                  KC_7,                  KC_8,                      KC_9,                 KC_0,                   KC_DELETE,
+  KC_TAB,          KC_Q,               KC_W,                 KC_F,                KC_P,                  KC_B,    KC_SLASH,                  KC_DQT,         KC_J,                  KC_L,                  KC_U,                      KC_Y,                 KC_BACKSLASH,           KC_BSPC,
   KC_ESC,          KC_A,               KC_R,                 KC_S,                KC_T,                  KC_G,                                               KC_M,                  KC_N,                  KC_E,                      KC_I,                 KC_O,                   KC_ENT,
-  KC_LSFT,         KC_Z,               KC_X,                 KC_C,                KC_D,                  KC_V,    KC_GRAVE,                  KC_DOWN,        KC_K,                  KC_H,                  KC_COMMA,                  KC_DOT,               KC_MINUS,               KC_RSFT,
-  MO(SYMB),        KC_LCBR,            KC_RCBR,              KC_BTN2,             MO(SYMB),                                                                                         KC_BTN1,               KC_BTN2,                   KC_RIGHT,             KC_RCBR,                KC_LALT,
-                                                             LCTL(KC_C),          LCTL(KC_V),                                                KC_MPRV,        LCTL(KC_S),
-                                                                                  KC_HOME,                                                   KC_VOLU,
-                                                     KC_SPC, KC_LALT,             LALT(KC_X),                                                LCTL(KC_B),     LALT,            KC_LCTL
+  KC_LSFT,         KC_Z,               KC_X,                 KC_C,                KC_D,                  KC_V,    KC_GRAVE,                  KC_COLON,       KC_K,                  KC_H,                  KC_COMMA,                  KC_DOT,               KC_MINUS,               KC_RSFT,
+  KC_TRNS,         KC_LCBR,            KC_LBRC,              KC_SEMICOLON,        MO(SYMB),                                                                                         KC_BTN1,               KC_LGUI,                   KC_RBRC,              KC_RCBR,                KC_TRNS,
+                                                             KC_LT,               KC_TRNS,                                                   KC_VOLU,        KC_GT,
+                                                                                  KC_HOME,                                                   KC_VOLD,
+                                                     KC_SPC, KC_LALT,             LALT(KC_X),                                                KC_QUOT,        KC_LALT,               KC_LCTL
 ),
 
 [SYMB] = LAYOUT_ergodox_pretty(
-  // left hand`
-  VRSN,    KC_F1,    KC_F2,    KC_F3,   KC_F4,    KC_F5,   KC_TRNS,          KC_TRNS, KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_F11,
-  KC_TRNS, KC_TRNS,  KC_PGUP,  KC_UP,   KC_PGDN,  KC_TRNS, KC_MINUS,         KC_HOME, KC_HOME,   KC_PGDN,  KC_PGUP,  KC_END,  KC_TRNS, KC_F12,
-  KC_TRNS, KC_EQL,   KC_LEFT,  KC_DOWN, KC_RIGHT, KC_TRNS,                  KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_PLUS, KC_DEL,
-  KC_TRNS, KC_PLUS,  KC_TRNS,  KC_LBRC, KC_RBRC,  KC_TILD, KC_TRNS,     KC_TRNS,      KC_AMPR, KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS,
-  EE_CLR,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-                                               KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
-                                                        KC_TRNS,     KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
+  // left hand
+  VRSN,     KC_F1,      KC_F2,      KC_F3,   KC_F4,    KC_F5,   KC_TRNS,         KC_TRNS, KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  KC_QUOTE, KC_TRNS,    KC_PGUP,    KC_UP,   KC_PGDN,  KC_EQL,  KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
+  KC_DQT,   KC_HOME,    KC_LEFT,    KC_DOWN, KC_RIGHT, KC_END,                   KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS,  LGUI(KC_E), LGUI(KC_D), KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  EE_CLR,   KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+                                            KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS,
+                                                      KC_TRNS,     KC_TRNS,
+                                    KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
 ),
 };
 // clang-format on
@@ -98,15 +91,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void matrix_scan_user(void) { // The very important timer.
   if (is_alt_tab_active) {
-    if (timer_elapsed(alt_tab_timer) > 600) {
+    if (timer_elapsed(alt_tab_timer) > 400) {
       unregister_code(KC_LGUI);
       is_alt_tab_active = false;
-    }
-  }
-  if (is_alt_tab_active2) {
-    if (timer_elapsed(alt_tab_timer2) > 600) {
-      unregister_code(KC_LGUI);
-      is_alt_tab_active2 = false;
     }
   }
 }
