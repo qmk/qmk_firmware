5bfed6b1624a11572dd8300cde50b82e21a3c844
astro <yuleiz@gmail.com>
2024-01-10 22:56:48 +0800

    try pass the code lint

--- keyboards/neson_design/810e/is31fl3236.h
+++ keyboards/neson_design/810e/is31fl3236.h
@@ -1,4 +1,4 @@
- /**
+/**
  * Copyright 2023 astro
  *
  * This program is free software: you can redistribute it and/or modify
--- keyboards/neson_design/810e/is31fl3729.h
+++ keyboards/neson_design/810e/is31fl3729.h
@@ -1,5 +1,4 @@
-
- /**
+/**
  * Copyright 2023 astro
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include <stdbool.h>
 
 #include "is31fl3236.h"
