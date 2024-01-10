2c8c5b954aa2dc50142cc44a013e9e30d34ceffe
Duncan Sutherland <dunk2k_2000@hotmail.com>
2024-01-10 00:19:29 +0000

    update keymaps for dz60

--- keyboards/dz60/keymaps/iso_de_arrow_0x544d/keymap.c
+++ keyboards/dz60/keymaps/iso_de_arrow_0x544d/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | LCtl  | LGUI  | LAlt  |            Space                  |RAlt| MO(1)| Lft | Dwn | Rgh |
    * `-----------------------------------------------------------------------------------------'
    */
-    [_BL] = LAYOUT_60_iso_arrow_one_bksp(
+    [_BL] = LAYOUT_60_iso_arrow(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,         KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,         KC_RBRC,
         MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,         KC_BSLS, KC_ENT,
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |       |       |       |                                   |     |Trans| MPrv|Vol_D| MNxt|
    * `-----------------------------------------------------------------------------------------'
    */
-    [_FN1] = LAYOUT_60_iso_arrow_one_bksp(
+    [_FN1] = LAYOUT_60_iso_arrow(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,          KC_F12,  KC_DEL,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |       |       |       |                                   |     |     | Home|PGDN | End |
    * `-----------------------------------------------------------------------------------------'
    */
-    [_FN2] = LAYOUT_60_iso_arrow_one_bksp(
+    [_FN2] = LAYOUT_60_iso_arrow(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,          KC_F12,  KC_DEL,
         KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   QK_BOOT,   KC_NO,           KC_NO,
         KC_TRNS, KC_MRWD, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,
