8aee367024a4e448bf341bb6f3b572539ef37b27
QKekos <q.kekos.q@gmail.com>
2024-01-11 22:28:01 +0200

    `ve*` -> `very`

--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
@@ -65,6 +65,11 @@ void process_magic_key(void) {
             magic_case(CLT_A, "y");
         );
 
+        double_magic_switch(KC_V,
+            magic_case(LT_I,  "ew");
+            magic_case(ALT_E, "ry");
+        );
+
         double_magic_case(LT_I,   KC_C,  "k");
         double_magic_case(KC_U,   KC_C,  "k");
         double_magic_case(KC_C,   KC_Y,  "c");
@@ -72,7 +77,6 @@ void process_magic_key(void) {
         double_magic_case(ALT_E,  KC_C,  "k");
         double_magic_case(SMT_D,  CLT_A, "y");
         double_magic_case(GLT_S,  KC_O,  "me");
-        double_magic_case(KC_V,   LT_I,  "ew");
         double_magic_case(KC_L,   LT_I,  "st");
         double_magic_case(KC_B,   ALT_E, "en");
         double_magic_case(TH_NUM, ALT_E, ".g.");
