38616392d996caf3427e8b58426594ae80da7aa1
wilba <wilba@wilba.tech>
2024-01-11 15:03:51 +1100

    Initial version

---
+++ keyboards/wilba_tech/wt65_h2/config.h
@@ -0,0 +1,13 @@
+// Copyright 2024 Jason Williams (@wilba)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed
+#define DEBOUNCE 5
+
+// Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
+#define LOCKING_SUPPORT_ENABLE
+
+// Locking resynchronize hack
+#define LOCKING_RESYNC_ENABLE
