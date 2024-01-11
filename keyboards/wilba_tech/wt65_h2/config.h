7d96b1892ef37a29885a56ab163757a55bdfdddb
wilba <wilba@wilba.tech>
2024-01-11 15:23:24 +1100

    requested changes

--- keyboards/wilba_tech/wt65_h2/config.h
+++ keyboards/wilba_tech/wt65_h2/config.h
@@ -3,9 +3,6 @@
 
 #pragma once
 
-// Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed
-#define DEBOUNCE 5
-
 // Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
 #define LOCKING_SUPPORT_ENABLE
 
