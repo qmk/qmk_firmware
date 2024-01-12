af34f29524945a81df34f58aae50ddc2be085d51
mechlovin <57231893+mechlovin@users.noreply.github.com>
2024-01-12 13:51:01 +0700

    Update keyboards/mechlovin/olly/jf/rev2/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/mechlovin/olly/jf/rev2/config.h
+++ keyboards/mechlovin/olly/jf/rev2/config.h
@@ -17,21 +17,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// This is a 7-bit address, that gets left-shifted and bit 0
-// set to 0 for write, 1 for read (as per I2C protocol)
-// The address will vary depending on your wiring:
-// 00 AD <-> GND
-// 01 AD <-> SCL
-// 10 AD <-> SDA
-// 11 AD <-> VCC
-// ADDR represents A1:A0 of the 7-bit address.
-// The result is: 0b11101(ADDR)
-#define LED_DRIVER_ADDR_1 IS31FL3731_I2C_ADDRESS_SDA
+#define IS31FL3731_I2C_ADDRESS_1 IS31FL3731_I2C_ADDRESS_SDA
+
 #define I2C_DRIVER I2CD2
 #define I2C1_SCL_PIN B10
 #define I2C1_SDA_PIN B11
-#define LED_DRIVER_COUNT 1
-#define LED_MATRIX_LED_COUNT 114
+
 #define LED_MATRIX_KEYPRESSES // reacts to keypresses
 #define LED_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 #define LED_MATRIX_FRAMEBUFFER_EFFECTS // enable framebuffer effects
