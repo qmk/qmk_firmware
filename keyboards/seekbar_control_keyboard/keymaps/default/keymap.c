05338e6f7188ca77bdde624c4bb880c0076e0aeb
Kashiuchi Sotaro <sotaro0416@gmail.com>
2024-01-12 21:06:10 +0900

    create keymap.c

--- keyboards/seekbar_control_keyboard/keymaps/default/keymap.c
+++ keyboards/seekbar_control_keyboard/keymaps/default/keymap.c
@@ -5,20 +5,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+     * ┌───┬───┬───┐
+     * │ A │ B │ C │
+     * ├───┼───┼───┤
+     * │ D │ E │ F │
+     * └───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    [0] = LAYOUT_ortho_3x2(
+        KC_A,   KC_B,   KC_C,
+        KC_D,   KC_E,   KC_F
     )
 };
