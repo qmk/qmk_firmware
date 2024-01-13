13862cc77340d25bfd8d3803ddca64d28da5223b
Dominik Tarnowski <dom@tdom.dev>
2024-01-13 13:03:21 +0000

    we use emacs now

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode . ((compile-command . "qmk compile -kb ergodox_ez -km default"))))
