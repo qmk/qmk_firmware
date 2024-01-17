e9c749d152b29cf552ab97e470bb9695699daa00
Tommi Pääkkö <tommi.paakko@gmail.com>
2024-01-17 17:22:41 +0200

    Fixed username to be correct one

--- keyboards/atreus62/keymaps/via/keymap.c
+++ keyboards/atreus62/keymaps/via/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2024 Tommi Pääkkö (@glen_f)
+/* Copyright 2024 Tommi Pääkkö (@Glenf)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
