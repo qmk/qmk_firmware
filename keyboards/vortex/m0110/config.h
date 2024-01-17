20934a3705b05fb4899f77cafca1c71567453edd
gskygithub <106651989+gskygithub@users.noreply.github.com>
2024-01-17 19:38:02 +0800

    Delete config.h file

--- keyboards/vortex/m0110/config.h
+++ keyboards/vortex/m0110/config.h
@@ -16,6 +16,11 @@
 
 #pragma once
 
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
 /* External spi flash */
 #define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN B14
 #define WEAR_LEVELING_BACKING_SIZE (8 * 1024)
