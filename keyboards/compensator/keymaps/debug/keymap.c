73209e60336f0a2bde1a73f9eab311d6056e5c6e
Logan <lrfoster03@gmail.com>
2024-01-17 18:07:37 -0800

    Addressed Firmware Changes

--- keyboards/compensator/keymaps/debug/keymap.c
+++ keyboards/compensator/keymaps/debug/keymap.c
@@ -20,7 +20,7 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( /* Base -> XT:Num:Nav:Alphas:Nav:Num:Xt*/
+    [0] = LAYOUT( /* Base -> XT:Num:Nav:Alphas:Nav:Num:Xt*/ 
         KC_F1,  KC_F2,     KC_PPLS,  KC_P7, KC_P8, KC_P9,          KC_INS,  KC_HOME, KC_PGUP,          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,                KC_INS,  KC_HOME, KC_PGUP,          KC_P7,   KC_P8,   KC_P9,   KC_PPLS,     KC_F1,  KC_F2,
         KC_F3,  KC_F4,     KC_PMNS,  KC_P4, KC_P5, KC_P6,          KC_DEL,  KC_END,  KC_PGDN,          KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                          KC_DEL,  KC_END,  KC_PGDN,          KC_P4,   KC_P5,   KC_P6,   KC_PPLS,     KC_F3,  KC_F4,
         KC_F5,  KC_F6,     KC_PMNS,  KC_P1, KC_P2, KC_P3,          KC_SPC,  KC_UP,   KC_SPC,           KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),                  KC_SPC,  KC_UP, KC_SPC,             KC_P1,   KC_P2,   KC_P3,   KC_PMNS,     KC_F5,  KC_F6,
@@ -39,3 +39,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+};
