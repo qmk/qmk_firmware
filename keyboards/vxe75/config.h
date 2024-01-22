8eec908cdaa9c4332a62d4db96ccae4c6e0ed84f
gskygithub <106651989+gskygithub@users.noreply.github.com>
2024-01-22 10:12:18 +0800

    Update keyboards/vxe75/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/vxe75/config.h
+++ keyboards/vxe75/config.h
@@ -12,13 +12,6 @@
 /* Flash */
 #define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN B14
 
-/* LED */
-#define LED_PIN_ON_STATE 0
-#define LED_SCROLL_LOCK_PIN C0
-#define LED_WIN_LOCK_PIN C1
-#define LED_CAPS_LOCK_PIN C2
-#define LED_NUM_LOCK_PIN C3
-
 /* RGB Driver */
 #define DRIVER_1_CS A15
 #define DRIVER_2_CS B15
