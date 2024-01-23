018529b1535431cd499f7611edd0d49382f976de
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2024-01-23 09:53:35 +0800

    Update keyboards/steed/smk82_pro/keymaps/via/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/steed/smk82_pro/keymaps/via/keymap.c
+++ keyboards/steed/smk82_pro/keymaps/via/keymap.c
@@ -58,7 +58,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [WIN_B]  = {ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
     [WIN_FN] = {ENCODER_CCW_CW(RGB_SAI, RGB_SAD)},
     [MAC_B]  = {ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
