1717617a17e4a5c06dafba508a7839ee025d0ed4
smollchungusm <mpamuller@msn.com>
2024-01-27 00:37:50 +0100

    init pr

---
+++ keyboards/rose75/config.h
@@ -0,0 +1,14 @@
+// Copyright 2023 Matthijs Muller (@Smollchungus)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// RGB indicators
+
+#define WS2812_PIO_USE_PPIO1
+#define RGB_MATRIX_LED_COUNT 2
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 210
+
+//Force NKRO on startup
+#define FORCE_NKRO
