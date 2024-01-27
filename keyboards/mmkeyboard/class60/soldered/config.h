4938e1e476796bf45191eff1be1b9f1b68846ded
Lex Brugman <lex@probaa.nl>
2024-01-27 16:41:09 +0100

    Added support for the MMkeyboard Class60

---
+++ keyboards/mmkeyboard/class60/soldered/config.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define DIODE_DIRECTION COL2ROW
+#define MATRIX_HAS_GHOST
+
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
+
+#define USB_POLLING_INTERVAL_MS 1
+#define SOLENOID_PIN B3
+#define HAPTIC_ENABLE_PIN A15
+#define SOLENOID_DEFAULT_DWELL 4
+#define SOLENOID_MIN_DWELL 4
+#define SOLENOID_MAX_DWELL 45
+#define HAPTIC_OFF_IN_LOW_POWER 1
+#define NO_HAPTIC_MOD
+
+#define LED_PIN_ON_STATE 0
+#define LED_NUM_LOCK_PIN B8
+#define LED_CAPS_LOCK_PIN B9
+#define LED_SCROLL_LOCK_PIN B11
+
+#define AUDIO_CLICKY
+#define AUDIO_PIN A6
+#define AUDIO_PWM_DRIVER PWMD3
+#define AUDIO_PWM_CHANNEL 1
+#define AUDIO_STATE_TIMER GPTD4
+#define AUDIO_PIN_ALT_AS_NEGATIVE
+#define AUDIO_PIN_ALT A13
+#undef AUDIO_VOICES
+
+#define AUDIO_DAC_SAMPLE_MAX 4095U
+
+#ifdef AUDIO_ENABLE
+#define DEFAULT_LAYER_SONGS { \
+                              SONG(CAPS_LOCK_ON_SOUND), \
+                              SONG(CAPS_LOCK_OFF_SOUND), \
+                              SONG(NUM_LOCK_ON_SOUND), \
+                              SONG(NUM_LOCK_OFF_SOUND), \
+                              SONG(SCROLL_LOCK_ON_SOUND), \
+                              SONG(SCROLL_LOCK_OFF_SOUND) \
+                            }
+#endif
---
+++ keyboards/mmkeyboard/class60/soldered/keymaps/via/rules.mk
@@ -0,0 +1,3 @@
+VIA_ENABLE = yes            # Enable support for VIA
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
