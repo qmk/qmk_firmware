df107f8e78fa6762266943fda24b818d89438829
ArturR0k3r <artur2003s.ass@gmail.com>
2024-01-30 12:33:32 +0200

    KeyaPad1

---
+++ keyboards/.KeyaPad/keymaps/keya/keymap.c
@@ -0,0 +1,8 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_GRV, KC_1, KC_2, KC_TAB, KC_Q, KC_W),
+
+    [1] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+};
