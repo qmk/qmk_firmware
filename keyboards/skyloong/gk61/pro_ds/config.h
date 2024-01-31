5f4352743e49b512dc7d5bce9dd8a98c6cdbd38a
JIKEDINGZHI <52481939+NaturalZh@users.noreply.github.com>
2024-01-31 14:17:49 +0800

    Update keyboards/skyloong/gk61/pro_ds/config.h
    OK, Thank you.
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/skyloong/gk61/pro_ds/config.h
+++ keyboards/skyloong/gk61/pro_ds/config.h
@@ -3,17 +3,10 @@
 
 #pragma once
 
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_LED_COUNT 64
-#    define CAPS_LOCK_INDEX 28
-#    define WIN_MOD_INDEX 16
-#    define MAC_MOD_INDEX 17
-#    define SDB A4
+#define CAPS_LOCK_INDEX 28
+#define WIN_MOD_INDEX 16
+#define MAC_MOD_INDEX 17
+#define SDB A4
 
-#    define RGB_DISABLE_WHEN_USB_SUSPENDED
-#    define RGB_MATRIX_KEYPRESSES
-#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-
-#    define AW20216S_CS_PIN_1 B12
-#    define AW20216S_EN_PIN_1 SDB
-#endif // RGB_MATRIX_ENABLE
+#define AW20216S_CS_PIN_1 B12
+#define AW20216S_EN_PIN_1 SDB
