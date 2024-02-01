779eadfa6ee61668f4b96e734e1f892fceba354c
leep-frog <gleeper49@gmail.com>
2024-02-01 12:38:02 -0500

    update name

--- tests/housekeeping/test.mk
+++ tests/housekeeping/test.mk
@@ -1,4 +1,4 @@
-# Copyright 2024 Greg Leeper
+# Copyright 2024 leep-frog
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
