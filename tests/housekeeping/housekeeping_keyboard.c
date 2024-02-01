032d97f5e8d88dd2ee7f80310f345a1755d7f7ac
leep-frog <gleeper49@gmail.com>
2024-02-01 12:31:48 -0500

    Add housekeeping execution to unit tests

---
+++ tests/housekeeping/housekeeping_keyboard.c
@@ -0,0 +1,38 @@
+/* Copyright 2024 Greg Leeper
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "quantum.h"
+#include "housekeeping_keyboard.h"
+
+// clang-format off
+
+bool do_housekeeping = false;
+
+void housekeeping_task_user(void) {
+    if (do_housekeeping) {
+        do_housekeeping = false;
+        tap_code16(KC_I);
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == KC_H && !record->event.pressed) {
+        do_housekeeping = true;
+    }
+    return true;
+}
+
+// clang-format on
---
+++ tests/housekeeping/test_housekeeping.cpp
@@ -0,0 +1,54 @@
+/* Copyright 2024 Greg Leeper
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "keyboard_report_util.hpp"
+#include "keycode.h"
+#include "test_common.hpp"
+#include "action_tapping.h"
+#include "test_keymap_key.hpp"
+#include "housekeeping_keyboard.h"
+
+using testing::_;
+using testing::InSequence;
+
+class Housekeeping : public TestFixture {};
+
+TEST_F(Housekeeping, Works) {
+    TestDriver driver;
+    InSequence s;
+    auto       key_a = KeymapKey{0, 1, 0, KC_A};
+    auto       key_h = KeymapKey{0, 2, 0, KC_H};
+
+    set_keymap({key_a, key_h});
+
+    // Key that doesn't affect housekeeping
+    key_a.press();
+    EXPECT_REPORT(driver, (KC_A));
+    run_one_scan_loop();
+    key_a.release();
+    EXPECT_EMPTY_REPORT(driver);
+    run_one_scan_loop();
+
+    // Key that does affect housekeeping
+    key_h.press();
+    EXPECT_REPORT(driver, (KC_H));
+    run_one_scan_loop();
+    key_h.release();
+    EXPECT_EMPTY_REPORT(driver);
+    EXPECT_REPORT(driver, (KC_I));
+    EXPECT_EMPTY_REPORT(driver);
+    run_one_scan_loop();
+}
