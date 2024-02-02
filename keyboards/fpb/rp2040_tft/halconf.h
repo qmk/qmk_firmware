1fdc2e292492ceea111a9c775fc0aead2412cab9
Fernando Birra <fpb@fct.unl.pt>
2024-02-02 21:01:45 +0000

    Added one key pico based keyboard to test GC9107 driver

---
+++ keyboards/fpb/rp2040_tft/halconf.h
@@ -0,0 +1,15 @@
+// Copyright 2022 Stefan Kerkmann
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define HAL_USE_I2C TRUE
+#define HAL_USE_PWM TRUE
+#define HAL_USE_ADC TRUE
+
+#define HAL_USE_SPI TRUE
+//#define SPI_USE_WAIT TRUE
+//#define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
+
+#include_next <halconf.h>
+
