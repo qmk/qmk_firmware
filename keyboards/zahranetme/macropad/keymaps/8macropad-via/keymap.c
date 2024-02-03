be6017393e615b9dc468df805211adc9c24f895b
ZahraNetMe <101709005+zahranetme@users.noreply.github.com>
2024-02-03 22:23:34 +0700

    8macropad

--- keyboards/zahranetme/macropad/keymaps/8macropad-via/keymap.c
+++ keyboards/zahranetme/macropad/keymaps/8macropad-via/keymap.c
@@ -16,52 +16,66 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer_names { _MAIN, _FN1, _FN2, _FN3 };
+enum layer_names {
+  _MAIN,
+  _FN1,
+  _FN2,
+  _FN3
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
+  /*
+      | Knob 1: Vol Dn/Up |       | Knob 2: Page Dn/Up |
+      |  KC_NO            | KC_NO | KC_NO              |
+      |  KC_NO            | KC_NO | KC_NO              |
+      |  KC_NO            | KC_NO | KC_NO              |
+  */
+
+  [_MAIN] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+  /*
         | Knob 1: Vol Dn/Up |       | Knob 2: Page Dn/Up |
         |  KC_NO            | KC_NO | KC_NO              |
         |  KC_NO            | KC_NO | KC_NO              |
         |  KC_NO            | KC_NO | KC_NO              |
     */
 
-    [_MAIN] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-     /*
-        | Knob 1: Vol Dn/Up |       | Knob 2: Page Dn/Up |
-        |  KC_NO            | KC_NO | KC_NO              |
-        |  KC_NO            | KC_NO | KC_NO              |
-        |  KC_NO            | KC_NO | KC_NO              |
-    */
+  [_FN1] = LAYOUT(KC_NO, KC_NO, KC_NO, _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+  /*
+      | Knob 1: Vol Dn/Up |       | Knob 2: Page Dn/Up |
+      |  KC_NO            | KC_NO | KC_NO              |
+      |  KC_NO            | KC_NO | KC_NO              |
+      |  KC_NO            | KC_NO | KC_NO              |
+  */
 
+  [_FN2] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______),
+  /*
+      | Knob 1: Vol Dn/Up |       | Knob 2: Page Dn/Up |
+      |  KC_NO            | KC_NO | KC_NO              |
+      |  KC_NO            | KC_NO | KC_NO              |
+      |  KC_NO            | KC_NO | KC_NO              |
+  */
 
-    [_FN1] = LAYOUT(KC_NO, KC_NO, KC_NO, _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    /*
-        | Knob 1: Vol Dn/Up |       | Knob 2: Page Dn/Up |
-        |  KC_NO            | KC_NO | KC_NO              |
-        |  KC_NO            | KC_NO | KC_NO              |
-        |  KC_NO            | KC_NO | KC_NO              |
-    */
-
-
-    [_FN2] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______),
-    /*
-        | Knob 1: Vol Dn/Up |       | Knob 2: Page Dn/Up |
-        |  KC_NO            | KC_NO | KC_NO              |
-        |  KC_NO            | KC_NO | KC_NO              |
-        |  KC_NO            | KC_NO | KC_NO              |
-    */
-
-
-    [_FN3] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______)
+  [_FN3] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______)
 
 };
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_MAIN] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    [_FN1]  = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    [_FN2]  = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(_______, _______)},
-    [_FN3]  = {ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)},
+  [_MAIN] = {
+    ENCODER_CCW_CW(KC_NO, KC_NO),
+    ENCODER_CCW_CW(KC_NO, KC_NO)
+  },
+  [_FN1] = {
+    ENCODER_CCW_CW(KC_NO, KC_NO),
+    ENCODER_CCW_CW(KC_NO, KC_NO)
+  },
+  [_FN2] = {
+    ENCODER_CCW_CW(KC_NO, KC_NO),
+    ENCODER_CCW_CW(_______, _______)
+  },
+  [_FN3] = {
+    ENCODER_CCW_CW(_______, _______),
+    ENCODER_CCW_CW(_______, _______)
+  },
 };
 #endif
