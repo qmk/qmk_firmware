d38f4e71e37d458dd8e350337b3f33e919bfc0bd
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2024-02-03 20:13:23 -0300

    Fix Caps Lock indication

---
+++ keyboards/noxary/valhalla_v2/valhalla_v2.c
@@ -0,0 +1,25 @@
+/* Copyright 2023 Gondolindrim <gondolindrim@acheronproject.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "quantum.h"
+#include "backlight.h"
+extern backlight_config_t backlight_config;
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    (led_state.caps_lock && backlight_config.enable) ? backlight_set(backlight_config.level) : backlight_set(0);
+    return res;
+}
