154c0e5e23c0ab97d816671a8e6d6b8840044f7d
JIKEDINGZHI <52481939+NaturalZh@users.noreply.github.com>
2024-02-04 13:21:08 +0800

    Update keyboards/skyloong/gk980/bl/matrix.c
    OK.
    
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/skyloong/gk980/bl/matrix.c
+++ keyboards/skyloong/gk980/bl/matrix.c
@@ -12,10 +12,6 @@
 #define HC595_SH_PIN A5
 #define HC595_DS_PIN A7
 
-/* matrix state(1:on, 0:off) */
-extern matrix_row_t raw_matrix[MATRIX_ROWS]; // raw values
-extern matrix_row_t matrix[MATRIX_ROWS];     // debounced values
-
 pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
 
