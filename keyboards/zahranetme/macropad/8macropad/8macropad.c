9dc08a8a4ee8a62a0f3e67ee32d53b99662ca40e
ZahraNetMe <101709005+zahranetme@users.noreply.github.com>
2024-02-05 13:01:14 +0700

    Update

--- keyboards/zahranetme/macropad/8macropad/8macropad.c
+++ keyboards/zahranetme/macropad/8macropad/8macropad.c
@@ -34,7 +34,7 @@ void eeconfig_init_kb(void) {
 }
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-    if (!encoder_update_user(index, clockwise)) {
+    if (!encoder_update_kb(index, clockwise)) {
         return false;
     }
     if (index == 0) {
