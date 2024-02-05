84efc000bc2c29fdb6b022c65e00dfbed1e5e5d8
Joao Silva <joaosilvatm@hotmail.com>
2024-02-05 22:55:39 +0000

    Changed rules and added portuguese call

--- keyboards/ghost54/rules.mk
+++ keyboards/ghost54/rules.mk
@@ -3,3 +3,11 @@ SERIAL_DRIVER = vendor
 COMBO_ENABLE = yes #https://docs.qmk.fm/#/feature_combo
 ENCODER_ENABLE = yes #https://docs.qmk.fm/#/feature_encoders
 OLED_ENABLE = yes
+
+SWAP_HANDS_ENABLE = yes
+SPLIT_KEYBOARD = yes
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = yes
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
