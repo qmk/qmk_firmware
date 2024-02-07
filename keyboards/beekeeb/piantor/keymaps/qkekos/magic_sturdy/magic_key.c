8cc72ea213d29c586f4941bfaa887063b7f8803f
QKekos <q.kekos.q@gmail.com>
2024-02-07 23:40:31 +0200

    `hil*` -> `hilarious`

--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
@@ -79,6 +79,7 @@ void process_magic_key(void) {
         triple_magic_case(KC_J, KC_U, KC_D, "g");
         triple_magic_case(KC_L, KC_E, KC_D, "ge");
         triple_magic_case(KC_O, KC_B, KC_V, "ious");
+        triple_magic_case(KC_H, KC_I, KC_L, "arious");
     }
 
     switch (queue(-2)) {
