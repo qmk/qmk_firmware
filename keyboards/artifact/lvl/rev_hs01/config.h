ab7805d2629a62f41dc8bbc05aab9643c3645002
yiancar <yiangosyiangou@cytanet.com.cy>
2024-02-08 22:22:41 +0000

    Update keyboards/artifact/lvl/rev_hs01/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/artifact/lvl/rev_hs01/config.h
+++ keyboards/artifact/lvl/rev_hs01/config.h
@@ -19,8 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* RGB Light */
 #define WS2812_PWM_DRIVER PWMD1
-#define WS2812_PWM_CHANNEL 2
-#define WS2812_PWM_PAL_MODE 2
 #define WS2812_DMA_STREAM STM32_DMA1_STREAM5
 #define WS2812_DMA_CHANNEL 5  
 
