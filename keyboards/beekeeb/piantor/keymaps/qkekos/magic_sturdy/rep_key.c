dfed910ac3a87dfb1d2df1821be3c803d72f325a
QKekos <q.kekos.q@gmail.com>
2024-02-10 13:50:04 +0200

    `cou@` -> `course`

--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/rep_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/rep_key.c
@@ -25,8 +25,12 @@ void process_rep_key(void) {
             double_magic_case(KC_M, KC_P, "ossible");
         );
 
+        triple_magic_switch(KC_C,
+            double_magic_case(KC_O, KC_U, "rse");
+            double_magic_case(KC_A, KC_P, "able");
+        );
+
         triple_magic_case(KC_SPC, KC_A, KC_B, "out");
-        triple_magic_case(KC_C,   KC_A, KC_P, "able");
         triple_magic_case(KC_P,   KC_R, KC_O, "gram");
         triple_magic_case(KC_H,   KC_O, KC_W, "ever");
         triple_magic_case(KC_U,   KC_N, KC_I, "versity");
