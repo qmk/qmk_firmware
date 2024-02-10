8b983139cd069ac62e29db5f10212257fa28d896
QKekos <q.kekos.q@gmail.com>
2024-02-10 02:52:16 +0200

    `pro@` -> `program`

--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/rep_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/rep_key.c
@@ -21,6 +21,7 @@ void process_rep_key(void) {
 
         triple_magic_case(KC_SPC, KC_A, KC_B, "out");
         triple_magic_case(KC_C,   KC_A, KC_P, "able");
+        triple_magic_case(KC_P,   KC_R, KC_O, "gram");
         triple_magic_case(KC_U,   KC_N, KC_I, "versity");
         triple_magic_case(KC_I,   KC_M, KC_P, "ossible");
     }
