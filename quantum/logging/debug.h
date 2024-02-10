bc6cedc7fa648a8b1ee1c4c45bebdfeb6626f157
zvecr <git@zvecr.com>
2024-02-10 07:46:17 +0000

    atsam bleh

--- quantum/logging/debug.h
+++ quantum/logging/debug.h
@@ -17,8 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#ifndef PROTOCOL_ARM_ATSAM
+#    include <stdio.h>
+#endif
+
 #include <stdbool.h>
-#include <stdio.h>
 #include "print.h"
 
 #ifdef __cplusplus
