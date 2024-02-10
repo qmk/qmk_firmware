65ae33709b274c54fe6c661db952193553de316a
QKekos <q.kekos.q@gmail.com>
2024-02-11 00:52:53 +0200

    `te*` -> `tement`

--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
@@ -186,11 +186,15 @@ void process_magic_key(void) {
             magic_case(KC_O, "ub");
         );
 
+        double_magic_switch(KC_T,
+            magic_case(KC_E, "ment");
+            magic_case(KC_H, "rough");
+        );
+
         double_magic_case(KC_I, KC_C, "k");
         double_magic_case(KC_E, KC_C, "k");
         double_magic_case(KC_B, KC_E, "en");
         double_magic_case(KC_L, KC_I, "st");
-        double_magic_case(KC_T, KC_H, "rough");
     }
 
     switch (queue(-1)) {
