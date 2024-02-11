584537ffd1b9e47dd6ce24e2d2752d713df08b82
QKekos <q.kekos.q@gmail.com>
2024-02-11 22:24:33 +0200

    fixed lower layer mo

--- keyboards/beekeeb/piantor/keymaps/qkekos/general/lower_layer.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/general/lower_layer.c
@@ -3,7 +3,7 @@
 
 layer_state_t old_layer_state;
 
-bool lower_layer_mo(int target_layer, keyrecord_t *record) {
+enum pr_response lower_layer_mo(int target_layer, keyrecord_t *record) {
     if (record->event.pressed) {
         old_layer_state = layer_state;
         layer_state = (layer_state & ((1 << (target_layer + 1)) - 1)) | (1 << target_layer);
@@ -11,7 +11,7 @@ bool lower_layer_mo(int target_layer, keyrecord_t *record) {
         layer_state = old_layer_state;
     }
 
-    return true;
+    return false;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
--- keyboards/beekeeb/piantor/keymaps/qkekos/general/lower_layer.h
+++ keyboards/beekeeb/piantor/keymaps/qkekos/general/lower_layer.h
@@ -2,5 +2,5 @@
 #pragma once
 #include "../__init__.h"
 
-bool lower_layer_mo(int target_layer, keyrecord_t *record);
+enum pr_response lower_layer_mo(int target_layer, keyrecord_t *record);
 layer_state_t layer_state_set_user(layer_state_t state);
