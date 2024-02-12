3dff8c58039b603a0e806e2087648bc68d55c183
QKekos <q.kekos.q@gmail.com>
2024-02-12 04:35:52 +0200

    `remac*` -> `y`

--- keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/magic_sturdy/magic_key.c
@@ -30,6 +30,7 @@ void process_magic_key(void) {
 
     switch(queue(-5)) {
         quintuple_magic_case(KC_O, KC_C, KC_R, KC_A, KC_C, "y");
+        quintuple_magic_case(KC_R, KC_E, KC_M, KC_A, KC_C, "y");
         quintuple_magic_case(KC_N, KC_E, KC_V, KC_E, KC_R, "theless");
     }
 
