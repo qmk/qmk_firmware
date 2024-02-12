0199de5442da0ae2917b23f0caf58a1fa3ff78cf
Freather <freatherstudio@gmail.com>
2024-02-13 02:54:23 +0800

    adding haven60 firmware
    adding haven60 firmware

---
+++ keyboards/ah/haven60/config.h
@@ -0,0 +1,21 @@
+// Copyright 2023 Freather (@Freather)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+    #define RGBLIGHT_LAYERS 2
+    #define RGBLIGHT_DEFAULT_VAL 125
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
