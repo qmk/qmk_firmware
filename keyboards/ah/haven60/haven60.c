34a36a873973688cf144c55d98373ebc8ed860db
Freather <72902384+CMMS-Freather@users.noreply.github.com>
2024-02-13 21:15:16 +0800

    Update keyboards/ah/haven60/haven60.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/ah/haven60/haven60.c
+++ keyboards/ah/haven60/haven60.c
@@ -37,7 +37,7 @@ void keyboard_post_init_kb(void){
 }
 
 bool led_update_kb(led_t led_state){
-    bool res = led_update_user(state);
+    bool res = led_update_user(led_state);
     if (res) {
         rgblight_set_layer_state(0, led_state.caps_lock);
         rgblight_set_layer_state(1, !led_state.caps_lock);
