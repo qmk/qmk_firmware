8f50357a4abc94aafd166137106bc27028d8d42e
spacehangover <51838046+spacehangover@users.noreply.github.com>
2024-02-12 22:21:31 -0300

    added new keyboards wwa

--- keyboards/handwired/wwa/soyuz/config.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 spacehangover (@spacehangover)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define POT1_PIN GP27
-#define POT2_PIN GP26
-#define POT3_PIN GP28
--- keyboards/handwired/wwa/soyuz/keymaps/default/keymap.c
+++ keyboards/handwired/wwa/soyuz/keymaps/default/keymap.c
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "analog.h"
-#include "qmk_midi.h"
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -13,43 +11,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,   KC_F23,    KC_F24 
     )
 };
-
-uint8_t divisor = 0;
-
-
-void slider(void) {
-
-    static int previousPotValue = 0;
-    int currentPotValue = analogReadPin(POT1_PIN);
-    int threshold = 25;  
-
-    if (abs(currentPotValue - previousPotValue) > threshold) {
-        midi_send_cc(&midi_device, 2, 0x0A, 0x7F - (currentPotValue >> 3));
-        previousPotValue = currentPotValue;
-    }
-
-
-    static int previousPotValue2 = 0;
-    int currentPotValue2 = analogReadPin(POT2_PIN);
-
-
-    if (abs(currentPotValue2 - previousPotValue2) > threshold) {
-        midi_send_cc(&midi_device, 3, 0x0B, 0x7F - (currentPotValue2 >> 3));
-        previousPotValue2 = currentPotValue2;
-    }
-
-
-    static int previousPotValue3 = 0;
-    int currentPotValue3 = analogReadPin(POT3_PIN);
-
-
-    if (abs(currentPotValue3 - previousPotValue3) > threshold) {
-        midi_send_cc(&midi_device, 4, 0x0C, 0x7F - (currentPotValue3 >> 3));
-        previousPotValue3 = currentPotValue3;
-    }
-    
-}
-
-void matrix_scan_user(void) {
-    slider();
-}
