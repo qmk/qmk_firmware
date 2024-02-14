0d2f648fd7192a3fd27d228072358d1b2348d617
Freather <72902384+CMMS-Freather@users.noreply.github.com>
2024-02-14 08:28:33 +0800

    Update keyboards/ah/haven60/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/ah/haven60/keymaps/default/keymap.c
+++ keyboards/ah/haven60/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, 
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                     KC_RALT, MO(1),  KC_RCTL 
     ),
-    [1] = LAYOUT(
+    [1] = LAYOUT_60_ansi_tsangan(
         KC_GRV,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11, KC_F12,     KC_TRNS,
         KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_UP,  KC_TRNS,    KC_TRNS,
         KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_LEFT,    KC_RIGHT,           KC_TRNS,
