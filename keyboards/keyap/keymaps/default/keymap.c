ce39023fb5e1fffa16ed795e8eeaccb19bbb41f5
Artur <117598386+ArturR0k3r@users.noreply.github.com>
2024-02-15 10:13:56 +0200

    Update keyboards/keyap/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/keyap/keymaps/default/keymap.c
+++ keyboards/keyap/keymaps/default/keymap.c
@@ -19,10 +19,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_G, KC_H, KC_I
     )
 };
-
-#ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP)}
-};
-#endif
-// #endif
