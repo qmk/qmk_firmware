386edb1fcbbaa02a0013efd372b59cb684e041d7
jack <0x6a73@protonmail.com>
2024-02-16 16:33:32 -0700

    Apply suggestions from code review

--- keyboards/chlx/ppr_merro60/config.h
+++ keyboards/chlx/ppr_merro60/config.h
@@ -17,10 +17,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
 /* VIA related config */
 #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
