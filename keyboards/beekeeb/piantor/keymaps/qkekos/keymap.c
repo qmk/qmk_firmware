d4131124349b6d9cd276f2d10c68b1759585db94
QKekos <q.kekos.q@gmail.com>
2024-02-19 05:54:57 +0200

    added a bunch of `c_p|p_c`, `ex` fixes, `special, specific`, `undoable`, `eventually` and `nice` + enabled hard training mode by default

--- keyboards/beekeeb/piantor/keymaps/qkekos/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/keymap.c
@@ -28,6 +28,7 @@ void keyboard_pre_init_user(void) {
     transaction_register_rpc(USER_SYNC_A, user_sync_a_slave_handler);
     lang_independant_init();
     enable_magic_training();
+    enable_hard_training();
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
