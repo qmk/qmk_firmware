6808d857dd1a95bff82bc99c37a5e64b2a6cc8d6
zlabkeeb <160311066+zlabkeeb@users.noreply.github.com>
2024-02-19 07:16:59 +0700

    Update keyboards/zlabkeeb/6pad/keymaps/via/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/zlabkeeb/6pad/keymaps/via/keymap.c
+++ keyboards/zlabkeeb/6pad/keymaps/via/keymap.c
@@ -71,25 +71,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_1, KC_2, KC_3,
         KC_4, KC_5, KC_6
-        ),
-    [1] = LAYOUT(
-        KC_A, KC_B, KC_C,
-        KC_D, KC_E, KC_F
-        ),
-    [2] = LAYOUT(
-        _______, _______, _______,
-        _______, _______, _______
-        ),
-    [3] = LAYOUT(
-        _______, _______, _______,
-        _______, _______, _______
         )
         };
 #if defined (ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP)},
-    [2] = { ENCODER_CCW_CW(_______, _______)},
-    [3] = { ENCODER_CCW_CW(_______, _______)},
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
 };
 #endif
