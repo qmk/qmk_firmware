83b6e77437693fed9df7414ec5b3e255440ebacd
Artur <117598386+ArturR0k3r@users.noreply.github.com>
2024-02-19 17:25:16 +0200

    Update keyboards/keyap/keymaps/via/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/keyap/keymaps/via/keymap.c
+++ keyboards/keyap/keymaps/via/keymap.c
@@ -21,9 +21,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            KC_G, KC_H, KC_I)
     };
 
-//const uint16_t PROGMEM encoder_map[][1][2] = {[0] = {{KC_C, KC_D}}};
-
-
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = {ENCODER_CCW_CW(KC_C, KC_D)}
 };
