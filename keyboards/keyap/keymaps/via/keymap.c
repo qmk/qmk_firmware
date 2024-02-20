f9f8a572f4a70569caa81e17d4676fe693bc1ccd
ArturR0k3r <artur2003s.ass@gmail.com>
2024-02-20 10:00:26 +0200

    lold`git commit -m "via: Update keymap" `

--- keyboards/keyap/keymaps/via/keymap.c
+++ keyboards/keyap/keymaps/via/keymap.c
@@ -21,14 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            KC_G, KC_H, KC_I)
     };
 
-<<<<<<< HEAD
 const uint16_t PROGMEM encoder_map[][1][2] = {[0] = {{KC_C, KC_D}}};
-=======
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = {ENCODER_CCW_CW(KC_C, KC_D)}
-};
-
->>>>>>> 3af10181d54820c969975b950c11e9699097bb72
 
 void keyboard_pre_init_user(void) {
     setPinOutput(LED1); // initialize B0 for LED
