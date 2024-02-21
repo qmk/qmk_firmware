8cd844f67738dfd7f9438d8a782e28cc5b01ccc0
jiaxin96 <572673807@qq.com>
2024-02-21 15:28:16 +0800

    add t80 lite init

---
+++ keyboards/yandrstudio/transition_lite/mcuconf.h
@@ -0,0 +1,12 @@
+// Copyright 2024 Y&R-Biu (@jiaxin96)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_PWM_USE_TIM2
+#define STM32_PWM_USE_TIM2 TRUE
+
+#undef STM32_PLLXTPRE
+#define STM32_PLLXTPRE STM32_PLLXTPRE_DIV2
