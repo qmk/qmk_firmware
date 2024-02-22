f461a4d6e2b05b7d75c48fd38bbe87b27ec3d843
galile0 <134774462+galile0-designs@users.noreply.github.com>
2024-02-22 20:25:46 +0100

    Update keyboards/galile0/glyphkbd_v2/rules.mk
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/galile0/glyphkbd_v2/rules.mk
+++ keyboards/galile0/glyphkbd_v2/rules.mk
@@ -1,11 +1,1 @@
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = yes    
-CONSOLE_ENABLE = no  
-COMMAND_ENABLE = no   
-NKRO_ENABLE = yes
-BACKLIGHT_ENABLE = no
-AUDIO_ENABLE = no
+# This file intentionally left blank
