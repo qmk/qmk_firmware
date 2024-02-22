506f7cc03e147ed050eecad106cd7782963b7d78
galile0 <134774462+galile0-designs@users.noreply.github.com>
2024-02-22 18:10:38 +0100

    Add files via upload

--- keyboards/galile0/glyphkbd_v2/rules.mk
+++ keyboards/galile0/glyphkbd_v2/rules.mk
@@ -1,11 +1,11 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no        # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
+BOOTMAGIC_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes    
+CONSOLE_ENABLE = no  
+COMMAND_ENABLE = no   
+NKRO_ENABLE = yes
+BACKLIGHT_ENABLE = no
+AUDIO_ENABLE = no
