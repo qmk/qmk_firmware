fc5da1d6c9981561569b5e69b0ae0831932f5a18
Chad Skeeters <chad.skeeters@gmail.com>
2024-02-25 01:22:24 -0600

    Adds pragma once to slice65/board.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/pizzakeyboards/slice65/board.h
+++ keyboards/pizzakeyboards/slice65/board.h
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#pragma once
 
 #define STM32_HSECLK 16000000
 #include_next <board.h>
