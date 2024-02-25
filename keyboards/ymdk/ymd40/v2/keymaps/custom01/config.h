a10d7e3486282e4bc27fd4c6762bbf2bb774b0cc
Santiago <intxausti@gmail.com>
2024-02-25 11:45:25 +0100

    Enabling compile keycode

--- keyboards/ymdk/ymd40/v2/keymaps/custom01/config.h
+++ keyboards/ymdk/ymd40/v2/keymaps/custom01/config.h
@@ -1,12 +1,8 @@
 #define TAPPING_TERM 200
-//#define IGNORE_MOD_TAP_INTERRUPT
-//#define RETRO_TAPPING
-//#define RETRO_TAPPING_PER_KEY
 #define LEADER_TIMEOUT 1000
 #define TAPPING_TOGGLE 2
-// Tapping this number of times holds the key until tapped once again.
 #define ONESHOT_TAP_TOGGLE 3
-// Time (in ms) before the one shot key is released
+
 #define ONESHOT_TIMEOUT 2000
 
 #define MOUSEKEY_DELAY                              300
@@ -17,4 +13,6 @@
 #define MOUSEKEY_WHEEL_MAX_TIME_TO_MAX              40
 
 #define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_WINCOMPOSE
+
+#define ENABLE_COMPILE_KEYCODE
 
