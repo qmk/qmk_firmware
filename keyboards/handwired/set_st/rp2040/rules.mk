7508d4727d0e169c80b0964c50cf9eeb54823c2c
Stas <stas@pda.in.ua>
2024-02-25 22:43:42 +0200

    fixes

--- keyboards/handwired/set_st/rp2040/rules.mk
+++ keyboards/handwired/set_st/rp2040/rules.mk
@@ -1,12 +1,6 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes      # Mouse keys
-CONSOLE_ENABLE = yes       # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = yes          # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
