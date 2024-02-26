ed791972e1bf812c432655de31e3cf92f3bcf397
Cipulot <40441626+Cipulot@users.noreply.github.com>
2024-02-26 03:05:10 +0100

    Cipulot refactoring (#22368)
    Co-authored-by: Drashna Jaelre <drashna@live.com>
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/cipulot/common/matrix.c
+++ keyboards/cipulot/common/matrix.c
@@ -2,7 +2,7 @@
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -17,28 +17,26 @@
 #include "ec_switch_matrix.h"
 #include "matrix.h"
 
-/* matrix state(1:on, 0:off) */
 extern matrix_row_t raw_matrix[MATRIX_ROWS]; // raw values
 extern matrix_row_t matrix[MATRIX_ROWS];     // debounced values
 
+// Custom matrix init function
 void matrix_init_custom(void) {
-    // Default values, overwritten by VIA if enabled later
-    ecsm_config.ecsm_actuation_threshold = DEFAULT_ACTUATION_LEVEL;
-    ecsm_config.ecsm_release_threshold   = DEFAULT_RELEASE_LEVEL;
+    // Initialize EC
+    ec_init();
 
-    ecsm_init(&ecsm_config);
+    // Get the noise floor at boot
+    ec_noise_floor();
 }
 
+// Custom matrix scan function
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
-    bool updated = ecsm_matrix_scan(current_matrix);
+    bool updated = ec_matrix_scan(current_matrix);
 
-// RAW matrix values on console
-#ifdef CONSOLE_ENABLE
-    static int cnt = 0;
-    if (cnt++ == 350) {
-        cnt = 0;
-        ecsm_print_matrix();
-    }
-#endif
     return updated;
+}
+
+// Bootmagic overriden to avoid conflicts with EC
+void bootmagic_scan(void) {
+    ;
 }
