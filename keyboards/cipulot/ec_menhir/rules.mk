0692ac3caddbe242971aadab02012271a5ce7073
Cipulot <40441626+Cipulot@users.noreply.github.com>
2024-02-28 20:47:07 +0100

    Suggestion applied

--- keyboards/cipulot/ec_menhir/rules.mk
+++ keyboards/cipulot/ec_menhir/rules.mk
@@ -1,3 +1,4 @@
 CUSTOM_MATRIX = lite
 ANALOG_DRIVER_REQUIRED = yes
-SRC += keyboards/cipulot/common/matrix.c keyboards/cipulot/common/ec_board.c keyboards/cipulot/common/ec_switch_matrix.c
+VPATH += keyboards/cipulot/common
+SRC += matrix.c ec_board.c ec_switch_matrix.c
