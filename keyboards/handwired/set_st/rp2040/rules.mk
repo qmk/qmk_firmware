301bba5cb542cff56d2a686110b7150b6a613164
Stas <stas@pda.in.ua>
2024-02-28 18:00:47 +0200

    refactoring

--- keyboards/handwired/set_st/rp2040/rules.mk
+++ keyboards/handwired/set_st/rp2040/rules.mk
@@ -1,11 +1,1 @@
-# Build Options
-#   change yes to no to disable
-#
-OS_DETECTION_ENABLE = yes
-RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
-ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-RGB_MATRIX_ENABLE = yes
-OLED_ENABLE = yes
-OLED_DRIVER = ssd1306
-OLED_TRANSPORT = i2c
+# This file intentionally left blank
