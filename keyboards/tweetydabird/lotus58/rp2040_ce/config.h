4c420305e16486505c29266224659716ae53a1af
Markus Knutsson <markus.knutsson@tweety.se>
2024-03-01 17:34:55 +0100

    Apply suggestions from code review
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/tweetydabird/lotus58/rp2040_ce/config.h
+++ keyboards/tweetydabird/lotus58/rp2040_ce/config.h
@@ -1,7 +1,7 @@
 // Copyright 2024 Markus Knutsson (@TweetyDaBird)
 // SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
 
-#define SERIAL_USART_TX_PIN GP1     // The GPIO pin that is used split communication.
 #define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
 
 #define I2C_DRIVER I2CD1
