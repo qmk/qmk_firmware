3ed7eee8ec20fbe8dbe49d4d45b19b9a30573b87
TweetyDaBird <markus.knutsson@tweety.se>
2024-03-01 09:22:51 +0100

    Added rp2040ce

---
+++ keyboards/tweetydabird/lotus58/rp2040_ce/config.h
@@ -0,0 +1,6 @@
+#define SERIAL_USART_TX_PIN GP1     // The GPIO pin that is used split communication.
+#define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
