8b996f6d6dd4962a8ca05f2a13330043db9e610a
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2024-03-02 19:36:49 -0300

    Remove I2C1 def from \'mcuconf\'

--- keyboards/typek/mcuconf.h
+++ keyboards/typek/mcuconf.h
@@ -31,9 +31,6 @@
 #define STM32_PPRE1                         STM32_PPRE1_DIV2
 #define STM32_PPRE2                         STM32_PPRE2_DIV1
 
-#undef STM32_I2C_USE_I2C1
-#define STM32_I2C_USE_I2C1 TRUE
-
 #undef STM32_PWM_USE_ADVANCED
 #define STM32_PWM_USE_ADVANCED TRUE
 
