28b74462ef53860f4fb22f905c30382e82e94352
Cipulot <40441626+Cipulot@users.noreply.github.com>
2024-03-03 18:31:01 +0100

    Fix Keymap and eeprom support added

--- keyboards/cipulot/ec_typek/keymaps/via/keymap.c
+++ keyboards/cipulot/ec_typek/keymaps/via/keymap.c
@@ -23,14 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PGUP,  KC_TAB,   KC_Q,  KC_W,  KC_E,     KC_R,    KC_T,              KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,
         KC_PGDN,  KC_CAPS,  KC_A,  KC_S,  KC_D,     KC_F,    KC_G,              KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENTER,
         KC_LGUI,  KC_LSFT,  KC_Z,  KC_X,  KC_C,     KC_V,    KC_B,              KC_B,     KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   MO(1),
-        KC_LCTL,                          KC_LALT,  KC_SPC,  KC_LGUI,           KC_RGUI,  KC_SPC,  KC_RALT,                                           KC_RCTL
+        KC_LCTL,                          KC_LALT,  KC_SPC,  KC_LGUI,           KC_RGUI,  KC_SPC,  KC_RALT,                                           MO(1)
     ),
     [1] = LAYOUT(
-        _______,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,    _______,  _______,
-        KC_HOME,  _______,  _______,  KC_UP,    _______,  _______,  _______,            _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUSE,  KC_UP,     _______,  _______,
-        KC_END,   _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,  _______,            _______,  _______,  _______,  _______,  KC_LEFT,   KC_RIGHT,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______,  KC_DOWN,    _______,  _______,
-        _______,                                _______,  _______,  _______,            _______,  _______,  _______,                                            MO(2)
+        _______,  _______,  KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   _______,  _______,
+        KC_HOME,  _______,  _______,  KC_UP,    _______,   _______,  _______,            _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUSE,  _______,  _______,  _______,
+        KC_END,   _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  _______,  _______,            _______,  _______,  _______,  _______,  _______,   _______,  _______,
+        _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,
+        _______,                                _______,   _______,  _______,            _______,  _______,  MO(2),                                             _______
     ),
     [2] = LAYOUT(
         QK_BOOT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
