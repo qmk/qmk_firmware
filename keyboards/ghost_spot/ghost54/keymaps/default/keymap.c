c165aa593354fb9c1a02d3bde7240e979dafa945
Joao Silva <joaosilvatm@hotmail.com>
2024-03-03 22:22:28 +0000

    Able to move between layers and added mod tap for home row keys

--- keyboards/ghost_spot/ghost54/keymaps/default/keymap.c
+++ keyboards/ghost_spot/ghost54/keymaps/default/keymap.c
@@ -25,17 +25,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|WIN+TAB|    |  Play |------+------+------+------+------+------|
  * | Ctrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |  º   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                          | Enter|Space | /TG(1)  /       \OSL(2)\  |Bcksp | Del  |
+ *                          | Enter|Space | /TO(1)  /       \ MO(2)\  |Bcksp | Del  |
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
 
 [0] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
+  MT(QK_BOOT, KC_ESC),KC_1,KC_2,KC_3, KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    PT_ACUT,
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    PT_CCED, PT_TILD,
+  KC_LSFT,LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,      KC_H,LSFT_T(KC_J),LCTL_TKC_K),LALT_T(KC_L),LGUI_T(PT_CCED), PT_TILD,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M,    PT_COMM, PT_DOT,  KC_MINS, PT_MORD,
-                    KC_ENT,  KC_SPC,  TG(1),   LGUI(KC_TAB),             KC_MPLY, OSL(2),  KC_BSPC, KC_DEL
+                    KC_ENT,  KC_SPC,  TO(1),   LGUI(KC_TAB),             KC_MPLY, MO(2),   KC_BSPC, KC_DEL
 ),
 /*
  * NUMPAD + Navigation
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|WIN+TAB|    |  Play |------+------+------+------+------+------|
  * | Ctrl |      |   1  |   2  |   3  |   .  |-------|    |-------|      | HOME | INSRT|  END |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                          | Enter|Space | /TG(0)  /       \OSL(2)\  |Bcksp | Del  |
+ *                          | Enter|Space | /TO(0)  /       \ MO(2)\  |Bcksp | Del  |
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   PT_EURO,KC_P7,   KC_P8,   KC_P9,   KC_MINS,                  _______,  KC_PGUP, KC_UP,    KC_PGDN, _______, _______,
   KC_LSFT,  KC_P0,  KC_P4,   KC_P5,   KC_P6,   KC_PLUS,                  _______,  KC_LEFT, KC_DOWN,  KC_RIGHT,_______, _______,
   KC_LCTL,  _______,KC_P1,   KC_P2,   KC_P3,   PT_DOT,                   _______,  KC_HOME, KC_INSERT,KC_END,  _______, _______,
-                    KC_ENT,  KC_SPC,  TG(0),   LGUI(KC_TAB),             KC_MPLY,  OSL(2),  KC_BSPC,  KC_DEL
+                    KC_ENT,  KC_SPC,  TO(0),   LGUI(KC_TAB),             KC_MPLY,  MO(2),   KC_BSPC,  KC_DEL
 ),
 /* Symbols
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|WIN+TAB|    |  Play |------+------+------+------+------+------|
  * | Ctrl |      |      |   -  |   "  |  {   |-------|    |-------|   }  |   '  |   _  |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                          | Enter|Space | /TG(1)  /       \ TG(0)\  |Bcksp | Del  |
+ *                          | Enter|Space | /TO(1)  /       \ TO(0)\  |Bcksp | Del  |
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,    _______,   _______,PT_LDAQ, PT_BSLS, ALGR(PT_8),            ALGR(PT_9),S(PT_7),    S(PT_LDAQ), ALGR(PT_PLUS),_______,   _______,
   KC_LSFT,   S(PT_3),ALGR(PT_2),PT_LABK, S(PT_1), S(PT_8),               S(PT_9),   S(PT_QUOT), S(PT_LABK), S(PT_0),      S(PT_BSLS),_______,
   KC_LCTL,   _______,   _______,KC_MINS, S(PT_2), ALGR(PT_7),            ALGR(PT_0),PT_QUOT,    S(PT_MINS), _______,      _______,   _______,
-                     KC_ENT,    KC_SPC,  TG(1),   LGUI(KC_TAB),          KC_MPLY,   TG(0),      KC_BSPC,    KC_DEL
+                     KC_ENT,    KC_SPC,  TO(1),   LGUI(KC_TAB),          KC_MPLY,   TO(0),      KC_BSPC,    KC_DEL
 ),
 /* F Keys
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|WIN+TAB|    |  Play |------+------+------+------+------+------|
  * | Ctrl |      |  F1  |  F2  |  F3  |      |-------|    |-------|      |  F13 |  F14 |  F15 |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                          | Enter|Space | /TG(1)  /       \OSL(2)\  |Bcksp | Del  |
+ *                          | Enter|Space | /TO(1)  /       \ TO(0)\  |Bcksp | Del  |
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
@@ -100,7 +100,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  _______,   KC_F7,  KC_F8,  KC_F9, _______,              _______, KC_F19, KC_F20, KC_F21, _______, _______,
   KC_LSFT, _______,   KC_F4,  KC_F5,  KC_F6, _______,              _______, KC_F16, KC_F17, KC_F18, _______, _______,
   KC_LCTL, _______,   KC_F1,  KC_F2,  KC_F3, _______,              _______, KC_F13, KC_F14, KC_F15, _______, _______,
-                   KC_ENT, KC_SPC, TG(1),  LGUI(KC_TAB),         KC_MPLY, OSL(2), KC_BSPC, KC_DEL
+                      KC_ENT, KC_SPC, TO(1),  LGUI(KC_TAB),        KC_MPLY, TO(0),  KC_BSPC,KC_DEL
 )
+};
+
+
+// Combos
+
+const uint16_t PROGMEM layer0to3[] = {TO(1), MO(2), COMBO_END};
+const uint16_t PROGMEM layer1to3[] = {TO(0), MO(2), COMBO_END};
+const uint16_t PROGMEM layer2to3[] = {TO(1), TO(0), COMBO_END};
+combo_t key_combos[] = {
+    COMBO(layer0to3, TO(3)),
+    COMBO(layer1to3, TO(3)),
+    COMBO(layer2to3, TO(3)),
 };
 
