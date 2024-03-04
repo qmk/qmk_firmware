59a8c0cc152b7320009f8183feb79b786a29cb2e
alber-Zheng <160847224+alber-Zheng@users.noreply.github.com>
2024-03-04 14:16:41 +0800

    Update as84.c

--- keyboards/smart/smart84/as84.c
+++ keyboards/smart/smart84/as84.c
@@ -1,4 +1,4 @@
-/* Copyright 2023 Albert
+/* Copyright 2024 albert-zheng
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
