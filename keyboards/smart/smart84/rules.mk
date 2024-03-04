90b58e6b16b814c931c30421d94b8cd209cdca37
alber-Zheng <160847224+alber-Zheng@users.noreply.github.com>
2024-03-04 17:30:09 +0800

    Update rules.mk

--- keyboards/smart/smart84/rules.mk
+++ keyboards/smart/smart84/rules.mk
@@ -1,5 +1,3 @@
-
-# QUANTUM_LIB_SRC += uart.c
 MCU_LDSCRIPT = STM32F103xB
 
 DEFERRED_EXEC_ENABLE = yes
@@ -7,14 +5,9 @@ DEFERRED_EXEC_ENABLE = yes
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-#OLED_ENABLE = yes
-#OLED_TRANSPORT = spi
 
 ENCODER_MAP_ENABLE = yes
-# ENCODER_ENABLE = yes
-
 DYNAMIC_KEYMAP_ENABLE = yes
-
 
 SRC +=   analog.c
 
