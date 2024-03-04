5508341743872ec6940c8a5f72a24acfa4e52715
alber-Zheng <160847224+alber-Zheng@users.noreply.github.com>
2024-03-04 17:28:32 +0800

    Update config.h

--- keyboards/smart/smart84/config.h
+++ keyboards/smart/smart84/config.h
@@ -18,17 +18,12 @@
 #pragma once
 
 
-
-
 #define DRIVER_COUNT 1
 
 #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-//#define RGB_MATRIX_KEYRELEASES         // reacts to keyreleases (instead of keypresses)
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS // enable framebuffer effects
 #define RGB_DISABLE_WHEN_USB_SUSPENDED // USB待机时候停止灯效
 #define RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
-//#define RGB_MATRIX_TYPING_HEATMAP_SLIM
-//#define RGBLIGHT_SLEEP
 #define RGB_MATRIX_TIMEOUT 0 //#define RGBLIGHT_DISABLE_KEYCODES
 
 
@@ -48,10 +43,6 @@
 #define FORCE_NKRO // Force NKRO on by default
 
 /*DEBOUNCE TIME ms, default 5*/
-//#define DEBOUNCE 10
 #define ENCODER_RESOLUTION 4
 
-//#define WS2812_EXTERNAL_PULLUP
 
-
-//#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
