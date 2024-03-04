d766dd933acd107b44bb463f34d4ab9697eea0a3
alber-Zheng <160847224+alber-Zheng@users.noreply.github.com>
2024-03-04 14:31:25 +0800

    Update keymap.c

--- keyboards/smart/smart84/keymaps/via/keymap.c
+++ keyboards/smart/smart84/keymaps/via/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 owlab
+Copyright 2024 albert-zheng
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
