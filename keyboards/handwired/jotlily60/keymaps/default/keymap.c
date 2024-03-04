61a557031f799847a45b83b5cb37a1bb6766d1ca
jotix <69703151+jotix@users.noreply.github.com>
2024-03-04 16:14:21 -0300

    Update keyboards/handwired/jotlily60/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/handwired/jotlily60/keymaps/default/keymap.c
+++ keyboards/handwired/jotlily60/keymaps/default/keymap.c
@@ -1,12 +1,7 @@
-// This is the default layout for the handwired/jotanck keyboard
+// Copyright 2024 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
 #define _QWERTY 0
 #define _FN     1
 #define _ADJUST 2
