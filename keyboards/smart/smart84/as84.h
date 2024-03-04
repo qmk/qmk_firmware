a439905edc4dcc9ae656c666f97f0bef4156d5f1
alber-Zheng <160847224+alber-Zheng@users.noreply.github.com>
2024-03-04 17:27:46 +0800

    Update as84.h

--- keyboards/smart/smart84/as84.h
+++ keyboards/smart/smart84/as84.h
@@ -18,12 +18,9 @@
 
 #include "quantum.h"
 
-/* Indicator LEDs Config  */
-// #define CAPSLOCK_LED_PIN A15
 
 enum custom_keycodes {
 #ifdef VIA_ENABLE
-//    KC_USB = QK_USER,
     KC_USB = QK_KB_0,
 #else
     KC_USB = SAFE_RANGE,
