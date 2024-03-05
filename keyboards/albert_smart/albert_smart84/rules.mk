2b2e83cc3db4bc168c02c3040014248cf6c93df7
alber-Zheng <albert.zhenghn@gmail.com>
2024-03-05 15:57:10 +0800

    Requires Formatting

--- keyboards/albert_smart/albert_smart84/rules.mk
+++ keyboards/albert_smart/albert_smart84/rules.mk
@@ -6,7 +6,7 @@ DEFERRED_EXEC_ENABLE = yes
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
-ENCODER_MAP_ENABLE = yes
+
 DYNAMIC_KEYMAP_ENABLE = yes
 
 SRC +=   analog.c
