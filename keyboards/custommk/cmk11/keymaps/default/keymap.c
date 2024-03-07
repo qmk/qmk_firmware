66657b460862c8dfa460f5b289848c34781b2e68
David Hoelscher <infinityis@users.noreply.github.com>
2024-03-07 16:25:00 -0600

    correcting default keymap

--- keyboards/custommk/cmk11/keymaps/default/keymap.c
+++ keyboards/custommk/cmk11/keymaps/default/keymap.c
@@ -6,6 +6,6 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_C,    KC_V,   KC_VOLU, KC_VOLD, KC_DEL,
-        KC_LCTL, KC_PGUP, KC_SPC, KC_PGDN, MO(1)
+        KC_LCTL, KC_PGUP, KC_SPC, KC_PGDN, KC_ENT
     )
 };
