d5a89c06a6b5263e419bb1c785dd09abdbda6a72
108360205 <55587305+108360205@users.noreply.github.com>
2024-03-07 19:06:54 +0800

    Update keymap_common.h

--- quantum/keymap_common.h
+++ quantum/keymap_common.h
@@ -6,5 +6,9 @@
 #include <stdint.h>
 #include "keyboard.h"
 
+#ifdef ES_INCLUDE_INFO_CONFIG_FILE
+#include "info_config.h"
+#endif
+
 // translates key to keycode
 uint16_t keymap_key_to_keycode(uint8_t layer, keypos_t key);
