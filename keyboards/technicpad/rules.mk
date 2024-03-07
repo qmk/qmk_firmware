e16b956f831b935de3fbcbf9d827ba813416ed16
Niclas Hirschfeld <niclas@wonderland.io>
2024-03-07 18:18:39 +0100

    added TechnicPad

---
+++ keyboards/technicpad/rules.mk
@@ -0,0 +1,4 @@
+BOOTMAGIC_ENABLE 	= yes      	# Enable Bootmagic Lite
+MOUSEKEY_ENABLE 	= yes       # Mouse keys
+EXTRAKEY_ENABLE 	= yes       # Audio control and System control
+LTO_ENABLE 			= yes
