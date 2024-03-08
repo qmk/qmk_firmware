c38154e625421cb8945e6efe7fbd931d2268a8ff
Ramon Imbao <ramonimbao@gmail.com>
2024-03-08 10:47:58 +0800

    Apply suggestions from code review
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/abatskeyboardclub/newjeans/hotswap/config.h
+++ keyboards/abatskeyboardclub/newjeans/hotswap/config.h
@@ -12,22 +12,3 @@
 #define WS2812_SPI_SCK_PIN B13
 #define WS2812_SPI_SCK_PAL_MODE 0
 #define WS2812_EXTERNAL_PULLUP
-#define RGB_MATRIX_LED_COUNT 91
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#define RGB_MATRIX_KEYPRESSES
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
