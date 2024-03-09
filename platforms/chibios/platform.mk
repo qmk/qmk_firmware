940604b8658b5e374915a796aad46ae853a37b99
Nick Brassel <nick@tzarc.org>
2024-03-09 21:39:48 +1100

    Review comments, confirmed with hardware.

--- platforms/chibios/platform.mk
+++ platforms/chibios/platform.mk
@@ -405,7 +405,7 @@ ifeq ($(strip $(MCU)), risc-v)
 
     # Deal with different arch revisions and gcc renaming them
     ifneq ($(shell echo 'int main() { asm("csrc 0x300,8"); return 0; }' | $(TOOLCHAIN)gcc $(MCUFLAGS) $(TOOLCHAIN_CFLAGS) -x c -o /dev/null - 2>/dev/null >/dev/null; echo $$?),0)
-        MCUFLAGS = -march=$(MCU_ARCH)_zicsr_zifencei \
+        MCUFLAGS = -march=$(MCU_ARCH)_zicsr \
                    -mabi=$(MCU_ABI) \
                    -mcmodel=$(MCU_CMODEL) \
                    -mstrict-align
