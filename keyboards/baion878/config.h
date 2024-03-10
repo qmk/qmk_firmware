6957036b06760b3bbef1dd8d64c8d124e1888489
ojthetiny <ba6sti6@gmail.com>
2024-03-10 12:44:15 +0200

    add suggestions from code review

--- keyboards/baion878/config.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 ojthetiny
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-// Ensure we jump to bootloader if the QK_BOOT keycode was pressed
-#define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
