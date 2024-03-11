9a7e73713e552b0782420d41b399c083fa776468
alber-Zheng <160847224+alber-Zheng@users.noreply.github.com>
2024-03-11 16:00:51 +0800

    Update keyboards/albert_smart/albert_smart84/rules.mk
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/albert_smart/albert_smart84/rules.mk
+++ keyboards/albert_smart/albert_smart84/rules.mk
@@ -3,9 +3,6 @@ MCU_LDSCRIPT = STM32F103xB
 DEFERRED_EXEC_ENABLE = yes
 
 
-# Enter lower-power sleep mode when on the ChibiOS idle thread
-OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
-
 
 DYNAMIC_KEYMAP_ENABLE = yes
 
