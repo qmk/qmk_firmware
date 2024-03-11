1764bf07260f1aadef75e67dacf8dbce80a5474c
skyloong <92424712+JZ-Skyloong@users.noreply.github.com>
2024-03-11 16:21:35 +0800

    Update keyboards/skyloong/gk75/q1/config.h
    OK, Thank you.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/skyloong/gk75/q1/config.h
+++ keyboards/skyloong/gk75/q1/config.h
@@ -18,7 +18,5 @@
  * For full list of effects, see:
  * https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
  */
-#define RGB_MATRIX_KEYPRESSES
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CUSTOM_solid_green
 #define RGB_MATRIX_DEFAULT_HUE 0
