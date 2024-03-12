51cae5521d57ebe42839f1031ea1dcae8638823a
Nicholas Taormina <nick.taormina1223@gmail.com>
2024-03-12 19:28:21 -0400

    Update keyboards/nt_engineering/macropad/macropad.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/nt_engineering/macropad/macropad.c
+++ keyboards/nt_engineering/macropad/macropad.c
@@ -55,13 +55,3 @@ void matrix_scan_kb(void) {
         }
     }
 }
-
-// Handles rotary encoder
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
-    return true;
-}
