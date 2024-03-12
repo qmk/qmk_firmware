875a9fd8ee427b455ccc5f495a509c4fec266ce3
Sanma <sandikamaulana@gmail.com>
2024-03-12 14:30:38 +0700

    Add Nanggong Keyboard
    Add Nanggong Keyboard

---
+++ keyboards/nanggong/keymaps/via/rules.mk
@@ -0,0 +1,12 @@
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+CONSOLE_ENABLE = no        # Console for debug
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+AUDIO_ENABLE = no          # Audio output
+ENCODER_ENABLE = no
+VIA_ENABLE = yes
+LTO_ENABLE = yes
