4afd1d328efa91fb8a6aabf0cdc6bc319e01258d
gskygithub <106651989+gskygithub@users.noreply.github.com>
2024-03-13 14:25:00 +0800

    Resolved requested changes

--- keyboards/vxe75/readme.md
+++ keyboards/vxe75/readme.md
@@ -1,6 +1,6 @@
 # VXE75
 
-* Keyboard Maintainer:
+* Keyboard Maintainer: Gsky
 * Hardware Supported: VXE75 PCB
 
 Make example for this keyboard (after setting up your build environment):
@@ -11,8 +11,6 @@ Flashing example for this keyboard:
 
     make vxe75:default:flash
 
-See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
-
 ## Bootloader
 
 Enter the bootloader in 3 ways:
@@ -21,3 +19,4 @@ Enter the bootloader in 3 ways:
 * **Physical reset button**: Briefly press the button on the back of the PCB - some may have pads you must short instead
 * **Keycode in layout**: Press the key mapped to `QK_BOOT` if it is available
 
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
--- keyboards/vxe75/vxe75.c
+++ keyboards/vxe75/vxe75.c
@@ -16,97 +16,97 @@
 
 #include QMK_KEYBOARD_H
 
-const aw_led g_aw_leds[RGB_MATRIX_LED_COUNT] = {
+const aw20216s_led_t PROGMEM g_aw20216s_leds[AW20216S_LED_COUNT] = {
 /* Refer to IS31 manual for these locations
  *   driver
  *   |  R location
  *   |  |          G location
  *   |  |          |          B location
  *   |  |          |          | */
-    {0, CS1_SW1,  CS2_SW1,  CS3_SW1   },  // 0
-    {0, CS1_SW2,  CS2_SW2,  CS3_SW2   },  // 1
-    {0, CS1_SW3,  CS2_SW3,  CS3_SW3   },  // 2
-    {0, CS1_SW4,  CS2_SW4,  CS3_SW4   },  // 3
-    {0, CS1_SW5,  CS2_SW5,  CS3_SW5   },  // 4
-    {0, CS1_SW6,  CS2_SW6,  CS3_SW6   },  // 5
-    {0, CS1_SW7,  CS2_SW7,  CS3_SW7   },  // 6
-    {0, CS1_SW8,  CS2_SW8,  CS3_SW8   },  // 7
-    {0, CS1_SW9,  CS2_SW9,  CS3_SW9   },  // 8
-    {0, CS1_SW10, CS2_SW10, CS3_SW10  },  // 9
-    {0, CS1_SW11, CS2_SW11, CS3_SW11  },  // 10
-    {0, CS1_SW12, CS2_SW12, CS3_SW12  },  // 11
-    {1, CS1_SW1,  CS2_SW1,  CS3_SW1   },  // 12
-    {1, CS1_SW2,  CS2_SW2,  CS3_SW2   },  // 13
+    {0, SW1_CS1,  SW1_CS2,  SW1_CS3   },  // 0
+    {0, SW2_CS1,  SW2_CS2,  SW2_CS3   },  // 1
+    {0, SW3_CS1,  SW3_CS2,  SW3_CS3   },  // 2
+    {0, SW4_CS1,  SW4_CS2,  SW4_CS3   },  // 3
+    {0, SW5_CS1,  SW5_CS2,  SW5_CS3   },  // 4
+    {0, SW6_CS1,  SW6_CS2,  SW6_CS3   },  // 5
+    {0, SW7_CS1,  SW7_CS2,  SW7_CS3   },  // 6
+    {0, SW8_CS1,  SW8_CS2,  SW8_CS3   },  // 7
+    {0, SW9_CS1,  SW9_CS2,  SW9_CS3   },  // 8
+    {0, SW10_CS1, SW10_CS2, SW10_CS3  },  // 9
+    {0, SW11_CS1, SW11_CS2, SW11_CS3  },  // 10
+    {0, SW12_CS1, SW12_CS2, SW12_CS3  },  // 11
+    {1, SW1_CS1,  SW1_CS2,  SW1_CS3   },  // 12
+    {1, SW2_CS1,  SW2_CS2,  SW2_CS3   },  // 13
 
-    {0, CS4_SW1,  CS5_SW1,  CS6_SW1   },  // 14
-    {0, CS4_SW2,  CS5_SW2,  CS6_SW2   },  // 15
-    {0, CS4_SW3,  CS5_SW3,  CS6_SW3   },  // 16
-    {0, CS4_SW4,  CS5_SW4,  CS6_SW4   },  // 17
-    {0, CS4_SW5,  CS5_SW5,  CS6_SW5   },  // 18
-    {0, CS4_SW6,  CS5_SW6,  CS6_SW6   },  // 19
-    {0, CS4_SW7,  CS5_SW7,  CS6_SW7   },  // 20
-    {0, CS4_SW8,  CS5_SW8,  CS6_SW8   },  // 21
-    {0, CS4_SW9,  CS5_SW9,  CS6_SW9   },  // 22
-    {0, CS4_SW10, CS5_SW10, CS6_SW10  },  // 23
-    {0, CS4_SW11, CS5_SW11, CS6_SW11  },  // 24
-    {0, CS4_SW12, CS5_SW12, CS6_SW12  },  // 25
-    {1, CS1_SW5,  CS2_SW5,  CS3_SW5   },  // 26
-    {1, CS1_SW7,  CS2_SW7,  CS3_SW7   },  // 27
-    {1, CS4_SW3,  CS5_SW3,  CS6_SW3   },  // 28
+    {0, SW1_CS4,  SW1_CS5,  SW1_CS6   },  // 14
+    {0, SW2_CS4,  SW2_CS5,  SW2_CS6   },  // 15
+    {0, SW3_CS4,  SW3_CS5,  SW3_CS6   },  // 16
+    {0, SW4_CS4,  SW4_CS5,  SW4_CS6   },  // 17
+    {0, SW5_CS4,  SW5_CS5,  SW5_CS6   },  // 18
+    {0, SW6_CS4,  SW6_CS5,  SW6_CS6   },  // 19
+    {0, SW7_CS4,  SW7_CS5,  SW7_CS6   },  // 20
+    {0, SW8_CS4,  SW8_CS5,  SW8_CS6   },  // 21
+    {0, SW9_CS4,  SW9_CS5,  SW9_CS6   },  // 22
+    {0, SW10_CS4, SW10_CS5, SW10_CS6  },  // 23
+    {0, SW11_CS4, SW11_CS5, SW11_CS6  },  // 24
+    {0, SW12_CS4, SW12_CS5, SW12_CS6  },  // 25
+    {1, SW5_CS1,  SW5_CS2,  SW5_CS3   },  // 26
+    {1, SW7_CS1,  SW7_CS2,  SW7_CS3   },  // 27
+    {1, SW3_CS4,  SW3_CS5,  SW3_CS6   },  // 28
 
-    {0, CS7_SW1,  CS8_SW1,  CS9_SW1   },  // 29
-    {0, CS7_SW2,  CS8_SW2,  CS9_SW2   },  // 30
-    {0, CS7_SW3,  CS8_SW3,  CS9_SW3   },  // 31
-    {0, CS7_SW4,  CS8_SW4,  CS9_SW4   },  // 32
-    {0, CS7_SW5,  CS8_SW5,  CS9_SW5   },  // 33
-    {0, CS7_SW6,  CS8_SW6,  CS9_SW6   },  // 34
-    {0, CS7_SW7,  CS8_SW7,  CS9_SW7   },  // 35
-    {0, CS7_SW8,  CS8_SW8,  CS9_SW8   },  // 36
-    {0, CS7_SW9,  CS8_SW9,  CS9_SW9   },  // 37
-    {0, CS7_SW10, CS8_SW10, CS9_SW10  },  // 38
-    {0, CS7_SW11, CS8_SW11, CS9_SW11  },  // 39
-    {0, CS7_SW12, CS8_SW12, CS9_SW12  },  // 40
-    {1, CS1_SW8,  CS2_SW8,  CS3_SW8   },  // 41
-    {1, CS1_SW9,  CS2_SW9,  CS3_SW9   },  // 42
-    {1, CS4_SW4,  CS5_SW4,  CS6_SW4   },  // 43
+    {0, SW1_CS7,  SW1_CS8,  SW1_CS9   },  // 29
+    {0, SW2_CS7,  SW2_CS8,  SW2_CS9   },  // 30
+    {0, SW3_CS7,  SW3_CS8,  SW3_CS9   },  // 31
+    {0, SW4_CS7,  SW4_CS8,  SW4_CS9   },  // 32
+    {0, SW5_CS7,  SW5_CS8,  SW5_CS9   },  // 33
+    {0, SW6_CS7,  SW6_CS8,  SW6_CS9   },  // 34
+    {0, SW7_CS7,  SW7_CS8,  SW7_CS9   },  // 35
+    {0, SW8_CS7,  SW8_CS8,  SW8_CS9   },  // 36
+    {0, SW9_CS7,  SW9_CS8,  SW9_CS9   },  // 37
+    {0, SW10_CS7, SW10_CS8, SW10_CS9  },  // 38
+    {0, SW11_CS7, SW11_CS8, SW11_CS9  },  // 39
+    {0, SW12_CS7, SW12_CS8, SW12_CS9  },  // 40
+    {1, SW8_CS1,  SW8_CS2,  SW8_CS3   },  // 41
+    {1, SW9_CS1,  SW9_CS2,  SW9_CS3   },  // 42
+    {1, SW4_CS4,  SW4_CS5,  SW4_CS6   },  // 43
 
 
-    {0, CS10_SW1, CS11_SW1, CS12_SW1  },  // 44
-    {0, CS10_SW2, CS11_SW2, CS12_SW2  },  // 45
-    {0, CS10_SW3, CS11_SW3, CS12_SW3  },  // 46
-    {0, CS10_SW4, CS11_SW4, CS12_SW4  },  // 47
-    {0, CS10_SW5, CS11_SW5, CS12_SW5  },  // 48
-    {0, CS10_SW6, CS11_SW6, CS12_SW6  },  // 49
-    {0, CS10_SW7, CS11_SW7, CS12_SW7  },  // 50
-    {0, CS10_SW8, CS11_SW8, CS12_SW8  },  // 51
-    {0, CS10_SW9, CS11_SW9, CS12_SW9  },  // 52
-    {0, CS10_SW10,CS11_SW10,CS12_SW10 },  // 53
-    {0, CS10_SW11,CS11_SW11,CS12_SW11 },  // 54
-    {0, CS10_SW12,CS11_SW12,CS12_SW12 },  // 55
-    {1, CS1_SW11, CS2_SW11, CS3_SW11  },  // 56
-    {1, CS4_SW7,  CS5_SW7,  CS6_SW7   },  // 57
+    {0, SW1_CS10, SW1_CS11, SW1_CS12  },  // 44
+    {0, SW2_CS10, SW2_CS11, SW2_CS12  },  // 45
+    {0, SW3_CS10, SW3_CS11, SW3_CS12  },  // 46
+    {0, SW4_CS10, SW4_CS11, SW4_CS12  },  // 47
+    {0, SW5_CS10, SW5_CS11, SW5_CS12  },  // 48
+    {0, SW6_CS10, SW6_CS11, SW6_CS12  },  // 49
+    {0, SW7_CS10, SW7_CS11, SW7_CS12  },  // 50
+    {0, SW8_CS10, SW8_CS11, SW8_CS12  },  // 51
+    {0, SW9_CS10, SW9_CS11, SW9_CS12  },  // 52
+    {0, SW10_CS10,SW10_CS11,SW10_CS12 },  // 53
+    {0, SW11_CS10,SW11_CS11,SW11_CS12 },  // 54
+    {0, SW12_CS10,SW12_CS11,SW12_CS12 },  // 55
+    {1, SW11_CS1, SW11_CS2, SW11_CS3  },  // 56
+    {1, SW7_CS4,  SW7_CS5,  SW7_CS6   },  // 57
 
-    {0, CS13_SW1, CS14_SW1, CS15_SW1  },  // 58
-    {0, CS13_SW2, CS14_SW2, CS15_SW2  },  // 59
-    {0, CS13_SW3, CS14_SW3, CS15_SW3  },  // 60
-    {0, CS13_SW4, CS14_SW4, CS15_SW4  },  // 61
-    {0, CS13_SW5, CS14_SW5, CS15_SW5  },  // 62
-    {0, CS13_SW6, CS14_SW6, CS15_SW6  },  // 63
-    {0, CS13_SW7, CS14_SW7, CS15_SW7  },  // 64
-    {0, CS13_SW8, CS14_SW8, CS15_SW8  },  // 65
-    {0, CS13_SW9, CS14_SW9, CS15_SW9  },  // 66
-    {0, CS13_SW10,CS14_SW10,CS15_SW10 },  // 67
-    {0, CS13_SW11,CS14_SW11,CS15_SW11 },  // 68
-    {1, CS4_SW8,  CS5_SW8,  CS6_SW8   },  // 69
-    {1, CS4_SW9,  CS5_SW9,  CS6_SW9   },  // 70
+    {0, SW1_CS13, SW1_CS14, SW1_CS15  },  // 58
+    {0, SW2_CS13, SW2_CS14, SW2_CS15  },  // 59
+    {0, SW3_CS13, SW3_CS14, SW3_CS15  },  // 60
+    {0, SW4_CS13, SW4_CS14, SW4_CS15  },  // 61
+    {0, SW5_CS13, SW5_CS14, SW5_CS15  },  // 62
+    {0, SW6_CS13, SW6_CS14, SW6_CS15  },  // 63
+    {0, SW7_CS13, SW7_CS14, SW7_CS15  },  // 64
+    {0, SW8_CS13, SW8_CS14, SW8_CS15  },  // 65
+    {0, SW9_CS13, SW9_CS14, SW9_CS15  },  // 66
+    {0, SW10_CS13,SW10_CS14,SW10_CS15 },  // 67
+    {0, SW11_CS13,SW11_CS14,SW11_CS15 },  // 68
+    {1, SW8_CS4,  SW8_CS5,  SW8_CS6   },  // 69
+    {1, SW9_CS4,  SW9_CS5,  SW9_CS6   },  // 70
 
-    {0, CS16_SW1, CS17_SW1,  CS18_SW1 },  // 71
-    {0, CS16_SW2, CS17_SW2,  CS18_SW2 },  // 72
-    {0, CS16_SW3, CS17_SW3,  CS18_SW3 },  // 73
-    {0, CS16_SW6, CS17_SW6,  CS18_SW6 },  // 74
-    {0, CS16_SW9, CS17_SW9,  CS18_SW9 },  // 75
-    {0, CS16_SW10,CS17_SW10, CS18_SW10},  // 76
-    {1, CS4_SW10 ,CS5_SW10,  CS6_SW10 },  // 77
-    {1, CS4_SW11, CS5_SW11,  CS6_SW11 },  // 78
-    {1, CS10_SW5, CS11_SW5,  CS12_SW5 },  // 79
+    {0, SW1_CS16, SW1_CS17,  SW1_CS18 },  // 71
+    {0, SW2_CS16, SW2_CS17,  SW2_CS18 },  // 72
+    {0, SW3_CS16, SW3_CS17,  SW3_CS18 },  // 73
+    {0, SW6_CS16, SW6_CS17,  SW6_CS18 },  // 74
+    {0, SW9_CS16, SW9_CS17,  SW9_CS18 },  // 75
+    {0, SW10_CS16,SW10_CS17, SW10_CS18},  // 76
+    {1, SW10_CS4, SW10_CS5,  SW10_CS6 },  // 77
+    {1, SW11_CS4, SW11_CS5,  SW11_CS6 },  // 78
+    {1, SW5_CS10, SW5_CS11,  SW5_CS12 },  // 79
 };
