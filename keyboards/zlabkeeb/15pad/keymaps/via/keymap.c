7dfa161ac3719bea4a5964d6eeca7b98005348a9
Veriyans <162705394+veriyaniot@users.noreply.github.com>
2024-03-13 11:51:50 +0700

    Update

--- keyboards/zlabkeeb/15pad/keymaps/via/keymap.c
+++ keyboards/zlabkeeb/15pad/keymaps/via/keymap.c
@@ -17,49 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-
-uint16_t underglow_timer = 0;
-bool underglow_on = true;
-uint8_t prev_rgb_mode; // variable to store the previous RGB mode
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    prev_rgb_mode = rgblight_get_mode(); // store the current RGB mode before changing it
-    switch (get_highest_layer(state)) {
-    case 0:
-        rgblight_setrgb (0x00,  0x00, 0xFF); // LED blue for layer 0
-        underglow_timer = timer_read();
-        underglow_on = false;
-        break;
-    case 1:
-        rgblight_setrgb (0xFF,  0x00, 0x00); // LED red for layer 1
-        underglow_timer = timer_read();
-        underglow_on = false;
-        break;
-    case 2:
-        rgblight_setrgb (0xFF,  0xFF, 0x00); // LED yellow for layer 2
-        underglow_timer = timer_read();
-        underglow_on = false;
-        break;
-    case 3:
-        rgblight_setrgb (0x00,  0xFF, 0x00); // LED green for layer 3
-        underglow_timer = timer_read();
-        underglow_on = false;
-        break;
-    default: //  for any other layers, or the default layer
-        rgblight_setrgb (0x00,  0x00, 0x00);
-        underglow_timer = timer_read();
-        underglow_on = false;
-        break;
-    }
-  return state;
-}
-
-void matrix_scan_user(void) {
-    if (!underglow_on && timer_elapsed(underglow_timer) > 3000) { // 3000ms = 3 seconds
-        rgblight_mode_noeeprom(prev_rgb_mode); // restore the previous RGB mode
-        underglow_on = true;
-    }
-}
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /*
      * ┌─────────┌─────────┐         ┌─────────┐
@@ -84,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_5, KC_6, KC_7, KC_8,
         KC_LEFT, KC_RIGHT, KC_DOWN, KC_UP
         )
-        };
+};
 #if defined (ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)},
