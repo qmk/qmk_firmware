7e7d62b0a7343f1b0ee25b6fc52a57b95306abbd
Veriyans <162705394+veriyaniot@users.noreply.github.com>
2024-03-13 11:52:24 +0700

    Update

--- keyboards/zlabkeeb/15pad/keymaps/default/keymap.c
+++ keyboards/zlabkeeb/15pad/keymaps/default/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_5, KC_6, KC_7, KC_8,
         KC_LEFT, KC_RIGHT, KC_DOWN, KC_UP
         )
-        };
+};
 #if defined (ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)},
