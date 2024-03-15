96259d3b74de451f67f31a22781388b10f105cd3
DOIO2022 <116554792+DOIO2022@users.noreply.github.com>
2024-03-15 17:10:32 +0800

    Update keymap.c

--- keyboards/doio/kb3x/keymaps/default/keymap.c
+++ keyboards/doio/kb3x/keymaps/default/keymap.c
@@ -28,36 +28,36 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_LAY0] = LAYOUT(
-		   TO(1),    RGB_MOD,    RGB_TOG,    RGB_HUI,    RGB_VAD,    RGB_VAI,    
-		          KC_MPLY,                             KC_UP,                              KC_UP,
-  KC_MNXT,    KC_TRNS,    KC_MPRV,     A(KC_LEFT), KC_TRNS, A(KC_RIGHT),     KC_RIGHT, KC_TRNS, KC_LEFT,
-              KC_MPLY,                             KC_DOWN,                            KC_DOWN),
-  [_LAY1] = LAYOUT(
-		   TO(2),    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    
-		          KC_WH_U,                             S(KC_RBRC),                         C(KC_L),
-  C(KC_WH_D),  KC_TRNS,  C(KC_WH_U),   KC_DOT,     KC_TRNS, KC_COMM,         C(KC_B),  KC_TRNS, C(KC_U),
-              KC_WH_D,                             S(KC_LBRC),                         C(KC_M)),
-  [_LAY2] = LAYOUT(
-		   TO(3),    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    
-		          KC_B,                                KC_UP,                              S(KC_I),
-  KC_P,       KC_TRNS,   KC_H,         KC_RIGHT,   KC_TRNS,  KC_LEFT,        KC_O,    KC_TRNS,  KC_I,
-              KC_Y,                                KC_DOWN,                            S(KC_O)),
-  [_LAY3] = LAYOUT(
-		   TO(4),    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    
-		          KC_TRNS,                             KC_TRNS,                           KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS,
-              KC_TRNS,                             KC_TRNS,                           KC_TRNS),
-  [_LAY4] = LAYOUT(
-		   TO(5),    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    
-		          KC_TRNS,                             KC_TRNS,                           KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS,
-              KC_TRNS,                             KC_TRNS,                           KC_TRNS),
-  [_LAY5] = LAYOUT(
-		   TO(0),    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    
-		          KC_TRNS,                             KC_TRNS,                           KC_TRNS,
-  KC_TRNS,    KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS,
-              KC_TRNS,                             KC_TRNS,                           KC_TRNS),
+    [_LAY0] = LAYOUT(
+            TO(1),      RGB_MOD,        RGB_TOG,        RGB_HUI,        RGB_VAD,        RGB_VAI,    
+                KC_MPLY,                                    KC_UP,                                      KC_UP,
+    KC_MNXT,    KC_TRNS,    KC_MPRV,        A(KC_LEFT),     KC_TRNS,    A(KC_RIGHT),        KC_RIGHT,   KC_TRNS,    KC_LEFT,
+                KC_MPLY,                                    C_DOWN,                                     KC_DOWN),
+    [_LAY1] = LAYOUT(
+            TO(2),      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,    
+                KC_WH_U,                                    S(KC_RBRC),                                 C(KC_L),
+    C(KC_WH_D), KC_TRNS,    C(KC_WH_U),     KC_DOT,         KC_TRNS,    KC_COMM,            C(KC_B),    KC_TRNS,    C(KC_U),
+                KC_WH_D,                                    S(KC_LBRC),                                 C(KC_M)),
+    [_LAY2] = LAYOUT(
+            TO(3),      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,    
+                KC_B,                                       KC_UP,                                      S(KC_I),
+    KC_P,       KC_TRNS,    KC_H,           KC_RIGHT,       KC_TRNS,    KC_LEFT,            KC_O,       KC_TRNS,    KC_I,
+                KC_Y,                                       KC_DOWN,                                    S(KC_O)),
+    [_LAY3] = LAYOUT(
+            TO(4),      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,    
+                KC_TRNS,                                    KC_TRNS,                                    KC_TRNS,
+  KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_TRNS,        KC_TRNS,    KC_TRNS,            KC_TRNS,    KC_TRNS,    KC_TRNS,
+                KC_TRNS,                                    KC_TRNS,                                    KC_TRNS),
+    [_LAY4] = LAYOUT(
+            TO(5),      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,   
+                KC_TRNS,                                    KC_TRNS,                                    KC_TRNS,
+  KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_TRNS,        KC_TRNS,    KC_TRNS,            KC_TRNS,    KC_TRNS,    KC_TRNS,
+                KC_TRNS,                                    KC_TRNS,                                    KC_TRNS),
+    [_LAY5] = LAYOUT(
+		   TO(0),      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS, 
+                KC_TRNS,                                    KC_TRNS,                                    KC_TRNS,
+  KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_TRNS,        KC_TRNS,    KC_TRNS,            KC_TRNS,    KC_TRNS,    KC_TRNS,
+                KC_TRNS,                                    KC_TRNS,                                    KC_TRNS),
 
 };
 
