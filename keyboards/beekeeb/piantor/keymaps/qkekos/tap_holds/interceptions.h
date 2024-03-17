b581caf0a167cde1679397648444c6af228baa1a
QKekos <q.kekos.q@gmail.com>
2024-03-17 21:04:36 +0200

    interceptions refactor

--- keyboards/beekeeb/piantor/keymaps/qkekos/tap_holds/interceptions.h
+++ keyboards/beekeeb/piantor/keymaps/qkekos/tap_holds/interceptions.h
@@ -2,6 +2,8 @@
 #pragma once
 #include "../__init__.h"
 
+int interceptions_pr(uint16_t, keyrecord_t*);
+
 #define intercept_tap(body) \
     if (record->tap.count && record->event.pressed) { \
         body; \
