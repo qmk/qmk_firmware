34a46ad603aced6a05f4a8afbdfc4558f61203c2
era <73109780+eerraa@users.noreply.github.com>
2024-03-17 11:36:42 +0900

    Update keyboards/era/sirind/tomak/tomak.c
    Co-authored-by: Less/Rikki <86894501+lesshonor@users.noreply.github.com>

--- keyboards/era/sirind/tomak/tomak.c
+++ keyboards/era/sirind/tomak/tomak.c
@@ -153,7 +153,7 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max)
         }
     }
 
-    return false;
+    return true;
 }
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
