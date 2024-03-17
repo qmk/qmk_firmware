69fadff15656350c0a1e381c37e19d116a66553e
QKekos <q.kekos.q@gmail.com>
2024-03-17 20:26:55 +0200

    interceptions refactor

--- keyboards/beekeeb/piantor/keymaps/qkekos/tap_holds/interceptions.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/tap_holds/interceptions.c
@@ -1,33 +1,19 @@
 
 #include "../__init__.h"
 
-int proceed_custom_tap(uint16_t keycode, keyrecord_t *record) {
-    if (record->tap.count && record->event.pressed) {
-        tap_code16(keycode);
-        return false;
-    }
-
-    return PR_IGNORE;
-}
-
 int interceptions_pr(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case TH_FUNC:
-            if (record->tap.count && record->event.pressed) {
-                SEND_STRING("'s");
-                return false;
-            }
-
-            return PR_IGNORE;
+            intercept_tap(SEND_STRING("'s"));
 
         case TH_LNUM:
             if (!record->tap.count) return lower_layer_mo(NUM, record);
             return PR_IGNORE;
 
-        case HR_RMLN: return proceed_custom_tap(HK_RMLN, record);
-        case HR_SAVE: return proceed_custom_tap(HK_SAVE, record);
-        case HR_CPLN: return proceed_custom_tap(HK_CPLN, record);
-        case HR_SRCH: return proceed_custom_tap(HK_SRCH, record);
+        case HR_RMLN: intercept_tap(tap_code16(HK_RMLN));
+        case HR_SAVE: intercept_tap(tap_code16(HK_SAVE));
+        case HR_CPLN: intercept_tap(tap_code16(HK_CPLN));
+        case HR_SRCH: intercept_tap(tap_code16(HK_SRCH));
     }
 
     return PR_IGNORE;
--- keyboards/beekeeb/piantor/keymaps/qkekos/tap_holds/interceptions.h
+++ keyboards/beekeeb/piantor/keymaps/qkekos/tap_holds/interceptions.h
@@ -2,4 +2,16 @@
 #pragma once
 #include "../__init__.h"
 
-int interceptions_pr(uint16_t, keyrecord_t*);
+#define intercept_tap(body) \
+    if (record->tap.count && record->event.pressed) { \
+        body; \
+        return false; \
+    } \
+    return PR_IGNORE;
+
+#define intercept_hold(body) \
+    if (!record->tap.count && record->event.pressed) { \
+        body; \
+        return false; \
+    } \
+    return PR_IGNORE;
