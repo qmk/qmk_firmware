e3e71309dea16789086e9b01bafcb4202ec61eaa
Veriyans <162705394+veriyaniot@users.noreply.github.com>
2024-03-20 21:59:01 +0700

    Update keyboards/zlabkeeb/15pad/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/zlabkeeb/15pad/keymaps/default/keymap.c
+++ keyboards/zlabkeeb/15pad/keymaps/default/keymap.c
@@ -30,21 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├─────────┼─────────┬─────────┼─────────┤
      */
     [0] = LAYOUT(
-        KC_MUTE, KC_MPLY, TG(1),
+        KC_MUTE, KC_MPLY, KC_TRNS,
         KC_1, KC_2, KC_3, KC_4,
         KC_5, KC_6, KC_7, KC_8,
         KC_9, KC_0, KC_ENT, KC_DEL
-        ),
-    [1] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         )
 };
 #if defined (ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)},
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)}
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_DOWN, KC_UP)}
 };
 #endif
