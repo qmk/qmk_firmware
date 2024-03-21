273883c53d6ea57e4100081936f3cabc1057e7cc
QKekos <q.kekos.q@gmail.com>
2024-03-21 03:16:01 +0200

    made language swapper rely on companion app

--- keyboards/beekeeb/piantor/keymaps/qkekos/localization/language_swapper.c
+++ keyboards/beekeeb/piantor/keymaps/qkekos/localization/language_swapper.c
@@ -4,10 +4,26 @@
 enum languages current_lang = ENG;
 enum language_modes language_mode = TRIPLE;
 
-void cycle_language(int swaps_count, enum languages new_lang) {
+char *get_language_str(enum languages language) {
+    switch (language) {
+        case ENG:
+            return "en";
+
+        case RU:
+            return "ru";
+
+        case UA:
+            return "ua";
+
+        default:
+            return "en";
+    }
+}
+
+void swap_to(enum languages new_lang) {
     bool do_invert = current_lang == ENG || new_lang == ENG;
 
-    multi_tap(A(KC_LSFT), swaps_count);
+    uprintf("qmk_language_swap: %s\n", get_language_str(new_lang));
     current_lang = new_lang;
 
     if (do_invert) {
@@ -17,13 +33,13 @@ void cycle_language(int swaps_count, enum languages new_lang) {
     }
 }
 
-void tri_eng_tap_action(void)   { cycle_language(1, RU);  }
-void tri_ru_tap_action(void)    { cycle_language(2, ENG); }
-void tri_ua_tap_action(void)    { cycle_language(1, ENG); }
+void tri_eng_tap_action(void)   { swap_to(RU);  }
+void tri_ru_tap_action(void)    { swap_to(ENG); }
+void tri_ua_tap_action(void)    { swap_to(ENG); }
 
-void tri_eng_hold_action(void)  { cycle_language(2, UA);  }
-void tri_ru_hold_action(void)   { cycle_language(1, UA);  }
-void tri_ua_hold_action(void)   { cycle_language(2, RU);  }
+void tri_eng_hold_action(void)  { swap_to(UA);  }
+void tri_ru_hold_action(void)   { swap_to(UA);  }
+void tri_ua_hold_action(void)   { swap_to(RU);  }
 
 void double_ru_tap_action(void) { tri_ua_tap_action(); }
 
