b1d816c5845df87d512106b5dbef1ef16f5b36fa
Adam Coleman <gephorian@gmail.com>
2024-03-24 17:47:03 -0500

    Fix a key

--- keyboards/ergodox_extended/keymaps/default/keymap.c
+++ keyboards/ergodox_extended/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_LEFT, KC_DOWN, KC_RGHT,
 
         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,
-        KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_DEL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
