7ddc137954db5d27740644cce0bc6dea65689168
Adam Coleman <gephorian@gmail.com>
2024-03-24 20:11:28 -0500

    Move user settings into "features"
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/ergodox_extended/rules.mk
+++ keyboards/ergodox_extended/rules.mk
@@ -1,12 +1,3 @@
-BOOTMAGIC_ENABLE = no   # Enable Bootmagic Lite
-MOUSEKEY_ENABLE  = no   # Mouse keys
-EXTRAKEY_ENABLE  = yes  # Audio control and System control
-CONSOLE_ENABLE   = yes  # Console for debug
-COMMAND_ENABLE   = no   # Commands for debug and configuration
-CUSTOM_MATRIX    = yes  # Custom matrix file for the ErgoDox Extended
-NKRO_ENABLE      = yes  # Enable N-Key Rollover
-UNICODE_ENABLE   = yes  # Unicode
-
-# project specific files
+CUSTOM_MATRIX = yes
 SRC += matrix.c
 I2C_DRIVER_REQUIRED = yes
