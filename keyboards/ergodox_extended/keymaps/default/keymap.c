0f4e5fd4844f002c91d9feafbd9e294c410a8f04
Adam Coleman <gephorian@gmail.com>
2024-03-24 22:39:40 -0500

    Un-stack the keymap

--- keyboards/ergodox_extended/keymaps/default/keymap.c
+++ keyboards/ergodox_extended/keymaps/default/keymap.c
@@ -24,24 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                              └───┴───┴───┴───┘ └───┴───┴───┴───┘
      */
     [0] = LAYOUT_ergodox(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_NO,   KC_LALT,
-                                            KC_HOME, KC_DEL,  KC_PGUP,
-                                   KC_SPC,  KC_END,  KC_UP,   KC_PGDN,
-                                            KC_LEFT, KC_DOWN, KC_RGHT,
-
-        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,
-        KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_DEL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_QUOT, KC_LBRC, KC_RBRC, KC_RGUI, KC_RCTL,
-        KC_PGUP, KC_INS,  KC_PAUS,
-        KC_PGDN, KC_UP,   KC_PSCR, KC_SPC,
-        KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,    KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,    KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,     KC_DEL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_NO,   KC_LALT,                                        KC_QUOT, KC_LBRC, KC_RBRC, KC_RGUI, KC_RCTL,
+                                            KC_HOME, KC_DEL,  KC_PGUP,    KC_PGUP, KC_INS,  KC_PAUS,
+                                   KC_SPC,  KC_END,  KC_UP,   KC_PGDN,    KC_PGDN, KC_UP,   KC_PSCR, KC_SPC,
+                                            KC_LEFT, KC_DOWN, KC_RGHT,    KC_LEFT, KC_DOWN, KC_RGHT
     )
 };
