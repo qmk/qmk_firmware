0fd81c9e3f2971b2aa8ec56925b61b6cc0e3e880
mechlovin <57231893+mechlovin@users.noreply.github.com>
2024-03-27 10:51:07 +0700

    add support for if87 rgb rev2

--- keyboards/mechlovin/infinity87/rules.mk
+++ keyboards/mechlovin/infinity87/rules.mk
@@ -12,4 +12,4 @@ BACKLIGHT_ENABLE  = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-DEFAULT_FOLDER = mechlovin/infinity87/rgb_rev1
+DEFAULT_FOLDER = mechlovin/infinity87/rgb/rev1
