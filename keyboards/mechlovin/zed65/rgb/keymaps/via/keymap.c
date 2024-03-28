c9e78a1ccaaee0ac9de2543ed0a8c68e92ed9d71
mechlovin <57231893+mechlovin@users.noreply.github.com>
2024-03-28 09:34:44 +0700

    Update keyboards/mechlovin/zed65/rgb/keymaps/via/keymap.c
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/mechlovin/zed65/rgb/keymaps/via/keymap.c
+++ keyboards/mechlovin/zed65/rgb/keymaps/via/keymap.c
@@ -26,4 +26,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     )
 };
-
