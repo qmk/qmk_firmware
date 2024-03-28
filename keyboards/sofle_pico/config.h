0a210d52076ef844ec858a7abc8f1ebe938403e5
Ryan Neff <ryan.ray.neff@fluke.com>
2024-03-27 20:17:01 -0700

    Removes extended layer count, and extra rgb config from keyboard level
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/sofle_pico/config.h
+++ keyboards/sofle_pico/config.h
@@ -29,13 +29,4 @@
 
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
-/* If you're setting colors per key, this is required */
-#    define SPLIT_LAYER_STATE_ENABLE
-/* Turns off RGB effects when there is no longer a USB connection */
-#    define RGB_DISABLE_WHEN_USB_SUSPENDED
-#endif
-
-/* Allows for more than the default 4 layers in VIA. */
-#ifndef DYNAMIC_KEYMAP_LAYER_COUNT
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #endif
