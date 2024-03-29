c6544591f78e944099cd7db69572201e72e36f15
JIKEDINGZHI <52481939+NaturalZh@users.noreply.github.com>
2024-03-29 19:54:25 +0800

    Update keyboards/skyloong/gk61/pro_ds/config.h
    OK. Thanks.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/skyloong/gk61/pro_ds/config.h
+++ keyboards/skyloong/gk61/pro_ds/config.h
@@ -6,7 +6,7 @@
 #define CAPS_LOCK_INDEX 28
 #define WIN_MOD_INDEX 16
 #define MAC_MOD_INDEX 17
-#define SDB A4
+#define AW20216S_SDB_PIN A4
 #define TAPPING_TERM_PER_KEY
 
 #define AW20216S_CS_PIN_1 B12
