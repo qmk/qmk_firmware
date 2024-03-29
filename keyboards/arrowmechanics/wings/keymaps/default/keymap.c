fda56ced85597e21043d4d0327b1b3b3c2ec52c9
arrowmechanics <filip@arrowmechanics.com>
2024-03-29 07:28:12 +0100

    add 1 key - as suggested by beta-testers

--- keyboards/arrowmechanics/wings/keymaps/default/keymap.c
+++ keyboards/arrowmechanics/wings/keymaps/default/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_VOLD, KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   RGB_MOD,        KC_BTN1,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,       KC_BSPC,
         KC_MPLY,       KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   C(KC_C),        C(KC_V),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,
         KC_MNXT,      KC_CAPS,     KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   C(KC_Z),        C(KC_Y),  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,       KC_ENT,
-        KC_MPRV,      KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_ENT,         KC_ENT,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,      KC_RSFT,       KC_UP,
+        KC_MPRV,      KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_ENT,         KC_ENT,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_UP,    KC_HOME,
         KC_LCTL, KC_NO,   KC_NO,            KC_LCTL, KC_LCMD, KC_LALT,      KC_SPC,                  KC_SPC,       KC_RALT, KC_RCMD, KC_RCTL,          KC_LEFT,  KC_DOWN,  KC_RGHT
     )
 };
