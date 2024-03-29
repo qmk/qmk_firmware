a4cb0d8a283a991a3a0fee8f1bacfb5729dd0925
JIKEDINGZHI <52481939+NaturalZh@users.noreply.github.com>
2024-03-29 15:16:38 +0800

    Update keyboards/skyloong/gk980/bl/bl.c
    OK,Thanks.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/skyloong/gk980/bl/bl.c
+++ keyboards/skyloong/gk980/bl/bl.c
@@ -10,10 +10,10 @@ void suspend_power_down_kb() {
 void suspend_wakeup_init_kb(void) {
     switch (get_highest_layer(layer_state)) {
     case 0:
-        writePinLow(MAC_PIN);
+        gpio_write_pin_low(MAC_PIN);
         break;
     case 1:
-        writePinHigh(MAC_PIN);
+        gpio_write_pin_high(MAC_PIN);
         break;
     }
     suspend_wakeup_init_user();
