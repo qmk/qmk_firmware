18722ba3bf5eff6e830d2f2d41b15ff3d742e200
JIKEDINGZHI <52481939+NaturalZh@users.noreply.github.com>
2024-03-29 15:17:49 +0800

    Update keyboards/skyloong/gk980/bl/ansi/keymaps/via/keymap.c
    OK, Thank you very much.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/skyloong/gk980/bl/ansi/keymaps/via/keymap.c
+++ keyboards/skyloong/gk980/bl/ansi/keymaps/via/keymap.c
@@ -59,10 +59,10 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
     case 0:
-        writePinLow(MAC_PIN);
+        gpio_write_pin_low(MAC_PIN);
         break;
     case 1:
-        writePinHigh(MAC_PIN);
+        gpio_write_pin_high(MAC_PIN);
         break;
     }
   return state;
