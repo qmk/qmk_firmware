82588a6e4b5bf1b7d6c1b810aea05f0718c15586
NaturalZh <zhouziran@jikedingzhi.com>
2024-03-29 15:44:45 +0800

    update skyloong/gk980/bl/matrix.c

--- keyboards/skyloong/gk980/bl/matrix.c
+++ keyboards/skyloong/gk980/bl/matrix.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 //Matrix read rows set col:DIODE_DIRECTION == ROW2COL
 
-#include "matrix.h"
+#include "quantum.h"
 
 #define ReadDelayTime 120
 #define ClockTime 15
