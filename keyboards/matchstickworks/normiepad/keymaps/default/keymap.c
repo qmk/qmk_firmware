6d9f1260e2200f6ab547cfb893b0c27f953dcfdb
Logan Butler <8854426+MatchstickWorks@users.noreply.github.com>
2024-03-29 14:18:53 -0700

    Update keyboards/matchstickworks/normiepad/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/matchstickworks/normiepad/keymaps/default/keymap.c
+++ keyboards/matchstickworks/normiepad/keymaps/default/keymap.c
@@ -8,10 +8,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_BSPC, KC_EQL,  LSFT(KC_9),LSFT(KC_0),
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,            KC_PDOT
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,
+        KC_P0,            KC_PDOT, KC_PENT
     ),
 
 };
