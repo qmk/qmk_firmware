57a17c39e405b233d7379403eef6ac35405e6535
Logan Butler <8854426+MatchstickWorks@users.noreply.github.com>
2024-03-29 19:56:41 -0700

    Update keyboards/matchstickworks/normiepad/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/matchstickworks/normiepad/keymaps/via/keymap.c
+++ keyboards/matchstickworks/normiepad/keymaps/via/keymap.c
@@ -5,7 +5,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT(
+    [0] = LAYOUT_numpad_6x4(
         KC_BSPC, KC_EQL,  LSFT(KC_9),LSFT(KC_0),
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,
