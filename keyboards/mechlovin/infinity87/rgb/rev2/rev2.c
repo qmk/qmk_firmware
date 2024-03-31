3e2fbf8584dd0d4cebf6654a364138951bce4cd5
mechlovin <57231893+mechlovin@users.noreply.github.com>
2024-03-31 09:08:28 +0700

    move features to info.json

--- keyboards/mechlovin/infinity87/rgb/rev2/rev2.c
+++ keyboards/mechlovin/infinity87/rgb/rev2/rev2.c
@@ -1,4 +1,4 @@
-/* Copyright 2024 Mechlovin'
+/* Copyright 2024 Mechlovin' Studio
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- keyboards/mechlovin/infinity87/rgb/rev2/rules.mk
+++ keyboards/mechlovin/infinity87/rgb/rev2/rules.mk
@@ -1,2 +1,5 @@
 # Wildcard to allow APM32 MCU
 DFU_SUFFIX_ARGS = -p FFFF -v FFFF
+
+# Configure for 128K flash
+MCU_LDSCRIPT = STM32F103xB
