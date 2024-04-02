c51c3144ca68299623662c9f9b3df82fb24100f0
Sam Boynton <samlingx@gmail.com>
2024-04-01 23:58:04 -0700

    Remove a bunch of unneeded things to free up space

--- keyboards/splitkb/kyria/keymaps/sam_custom/rules.mk
+++ keyboards/splitkb/kyria/keymaps/sam_custom/rules.mk
@@ -1,7 +1,17 @@
+# LEDs
 OLED_ENABLE = yes
-ENCODER_ENABLE = no       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 
+# Disable some stuff we don't need
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
+
+# Behaviors
 CAPS_WORD_ENABLE = yes
 TAP_DANCE_ENABLE = yes
