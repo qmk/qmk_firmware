6a6ca80aa87451998d3891df23acf437883626ea
Tommaso Visconti <tommaso.visconti@gmail.com>
2024-04-03 00:27:27 +0200

    invert right cols

--- keyboards/handwired/ximi/keymaps/default/keymap.c
+++ keyboards/handwired/ximi/keymaps/default/keymap.c
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     )
 // };
 
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
-};
+// void keyboard_post_init_user(void) {
+//   // Customise these values to desired behaviour
+//   debug_enable=true;
+//   debug_matrix=true;
+//   debug_keyboard=true;
+//   //debug_mouse=true;
+// };
