b0348446955822b240c3c5e0eeb712e434efc341
NaturalZh <zhouziran@jikedingzhi.com>
2024-04-02 10:16:33 +0800

    Update skyloong/gk980/bl .

--- keyboards/skyloong/gk980/bl/ansi/keymaps/via/keymap.c
+++ keyboards/skyloong/gk980/bl/ansi/keymaps/via/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_WIN_INDEX] = LAYOUT_all(
         _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH,  G(C(KC_S)),   KC_SLEP,   KC_MPRV,  KC_MPLY,         KC_MNXT,   KC_MUTE,  KC_VOLD,     KC_VOLU,  G(KC_SPC),  KC_INS,  KC_HOME,  KC_END,  KC_PAUS,
-        _______,  _______,  _______,  _______,   _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,         BL_TOGG,   BL_BRTG,   BL_OFF,      KC_DEL,              KC_NUM,  KC_PSLS, KC_PAST,  KC_PMNS,
+        _______,  _______,  _______,  _______,   _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,         BL_TOGG,   BL_BRTG,  BL_BRTG,      KC_DEL,              KC_NUM,  KC_PSLS, KC_PAST,  KC_PMNS,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,   _______,  _______,     _______,               KC_P7,    KC_P8,   KC_P9,  KC_PPLS,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,   _______,               _______,               KC_P4,    KC_P5,   KC_P6,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,                          _______,    BL_UP,     KC_P1,    KC_P2,   KC_P3,  KC_PENT,
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MAC_INDEX] = LAYOUT_all(
         _______,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,     KC_F6,     KC_F7,    KC_F8,          KC_F9,     KC_F10,   KC_F11,   KC_F12,   G(KC_SPC),     KC_INS,  KC_HOME,  KC_END, KC_PAUS,
-        _______,  _______,  _______,  _______,  _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,        BL_TOGG,    BL_BRTG,   BL_OFF,   KC_DEL,                  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        _______,  _______,  _______,  _______,  _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,        BL_TOGG,    BL_BRTG,  BL_BRTG,   KC_DEL,                  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,    _______,  _______,  _______,                   KC_P7,    KC_P8,   KC_P9, KC_PPLS,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,    _______,            _______,                   KC_P4,    KC_P5,   KC_P6,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,                        _______,       BL_UP,      KC_P1,    KC_P2,   KC_P3, KC_PENT,
@@ -56,14 +56,20 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+
+  switch (get_highest_layer(state)) {
     case 0:
         gpio_write_pin_low(MAC_PIN);
         break;
     case 1:
         gpio_write_pin_high(MAC_PIN);
         break;
+    default:
+        gpio_write_pin_low(MAC_PIN);
+
     }
+
   return state;
 }
+
--- keyboards/skyloong/gk980/bl/bl.c
+++ keyboards/skyloong/gk980/bl/bl.c
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "quantum.h"
 
+_Bool en_breath = 0;
+uint8_t breath_level = 0;
+
 void suspend_power_down_kb() {
     gpio_write_pin_low(MAC_PIN);
     suspend_power_down_user();
@@ -30,6 +33,46 @@ bool shutdown_kb(bool jump_to_bootloader) {
         gpio_write_pin_low(MAC_PIN);
     }
     return true;
+}
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_user(keycode, record)) {
+        return false;
+    }
+    switch (keycode) {
+     case BL_TOGG:
+      if (record->event.pressed) {
+        if (is_backlight_breathing()){
+           breath_level = get_backlight_level();
+            en_breath = 1;
+           backlight_disable_breathing();
+           backlight_level(0);
+            return false;
+        } else if (en_breath){
+            backlight_level(breath_level);
+            backlight_enable_breathing();
+            en_breath = 0;
+            return false;
+        } else {
+            return true;
+        }
+      }
+
+     case TO(0):
+      if (record->event.pressed) {
+       set_single_persistent_default_layer(0);  //Save default layer 0 to eeprom
+      }
+      return true; // continue all further processing of this key
+
+     case TO(1):
+      if (record->event.pressed) {
+       set_single_persistent_default_layer(1);  //Save default layer 1 to eeprom
+      }
+      return true; // continue all further processing of this key
+
+     default:
+      return true;
+    }
 }
 
 void board_init(void) {
