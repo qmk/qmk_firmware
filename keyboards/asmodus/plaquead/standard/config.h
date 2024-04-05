32b308861f8f63651c265359ffff07268d630fe8
Sắn <59417802+MaiTheSan@users.noreply.github.com>
2024-04-05 23:06:48 +0700

    Update keyboards/asmodus/plaquead/standard/config.h
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/asmodus/plaquead/standard/config.h
+++ keyboards/asmodus/plaquead/standard/config.h
@@ -16,5 +16,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
-
 #define RP2040_FLASH_GENERIC_03H
