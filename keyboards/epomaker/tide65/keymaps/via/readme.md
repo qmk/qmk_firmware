cc144983cce78f3be8a9eb27b4999dfedeb8a0a6
岩弈 <oncwnuItq3UWm0BoCNj7YclFsdYs@git.weixin.qq.com>
2024-04-08 14:02:25 +0800

    update

---
+++ keyboards/epomaker/tide65/keymaps/via/readme.md
@@ -0,0 +1,3 @@
+# EPOMAKER TIDE 65 VIA Firmware
+
+This keymap is to enable the use of VIA on the EPOMAKER TIDE 65 Keyboard.
--- keyboards/epomaker/tide65/rules.mk
+++ keyboards/epomaker/tide65/rules.mk
@@ -1,27 +1,14 @@
-# MCU name
-MCU = WB32FQ95
-
-# Bootloader selection
-BOOTLOADER = wb32-dfu
 
 # Build Options
 #   change yes to no to disable
 #
-KEYBOARD_SHARED_EP = no
-BOOTMAGIC_ENABLE = no          # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes          # Mouse keys
-EXTRAKEY_ENABLE = yes          # Audio control and System control
-NKRO_ENABLE = yes              # Enable N-Key Rollover
-CONSOLE_ENABLE = no            # Console for debug
-COMMAND_ENABLE = no            # Commands for debug and configuration
-BACKLIGHT_ENABLE = no          # Enable keyboard backlight functionality
-AUDIO_ENABLE = no              # Audio output
-ENCODER_ENABLE = yes           # Encoder
-RGBLIGHT_ENABLE =  no          # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE = yes        # RGB matrix
-HAPTIC_ENABLE = no             # Haptic
-
-EEPROM_DRIVER = wear_leveling
-WEAR_LEVELING_DRIVER = spi_flash
-
-RGB_MATRIX_DRIVER = WS2812
+BOOTMAGIC_ENABLE = yes          # Enable Bootmagic Lite
+MOUSEKEY_ENABLE  = yes          # Mouse keys
+EXTRAKEY_ENABLE  = yes          # Audio control and System control
+CONSOLE_ENABLE   = no           # Console for debug
+COMMAND_ENABLE   = no           # Commands for debug and configuration
+NKRO_ENABLE      = yes          # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no           # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE  = no           # Enable keyboard RGB underglow
+HAPTIC_ENABLE    = no           # Haptic
+AUDIO_ENABLE     = no           # Audio output
