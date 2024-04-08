3abcc04661ce35e8711087421016ac3fb9be9390
DUILCHOI <48977039+duilchoi@users.noreply.github.com>
2024-04-08 17:28:32 +0900

    Update keyboards/linworks/fave87h/config.h
    thanks!
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/linworks/fave87h/config.h
+++ keyboards/linworks/fave87h/config.h
@@ -18,7 +18,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* Define RGB */
-#define RGB_MATRIX_LED_COUNT 127
+#define RGB_MATRIX_LED_COUNT 87
 
 #ifdef RGB_MATRIX_ENABLE
 
