13e0af104d93c452d1dbdc9eb9a9fcfe6d1e8134
Raido Oras <oras.raido@gmail.com>
2024-04-09 01:36:09 +0300

    update keymap

--- keyboards/splitkb/kyria/keymaps/raidora/config.h
+++ keyboards/splitkb/kyria/keymaps/raidora/config.h
@@ -1,44 +1,4 @@
-/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
-
-#ifdef RGBLIGHT_ENABLE
-//#    define RGBLIGHT_ANIMATIONS
-#    define RGBLIGHT_HUE_STEP  8
-#    define RGBLIGHT_SAT_STEP  8
-#    define RGBLIGHT_VAL_STEP  8
-#    define RGBLIGHT_LIMIT_VAL 150
-#endif
-
-#define PERMISSIVE_HOLD
-#define HOLD_ON_OTHER_KEY_PRESS
-
-#define ENCODER_RESOLUTION 2
-
-#define OLED_FONT_H "keyboards/splitkb/kyria/keymaps/raidora/glcdfont.c"
-
-#define OLED_UPDATE_INTERVAL 100
-
-#define FORCE_NKRO
-
-#undef TAPPING_TERM
-#define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
-
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 // #define SPLIT_USB_DETECT
 // #define NO_USB_STARTUP_CHECK
@@ -51,9 +11,9 @@
 #define NO_ACTION_FUNCTION
 #define DISABLE_LEADER
 
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+#define FORCE_NKRO
+#define TAPPING_TERM 300
+
+#define ENCODER_RESOLUTION 2
