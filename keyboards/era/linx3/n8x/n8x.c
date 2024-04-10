776e9447f5b76398f95b6036dba002266526675b
era <phyojinbin@gmail.com>
2024-04-10 21:50:44 +0900

    Removed code related to VIA
    Removed code related to VIA

--- keyboards/era/linx3/n8x/n8x.c
+++ keyboards/era/linx3/n8x/n8x.c
@@ -1,176 +1,46 @@
 // Copyright 2024 Hyojin Bak (@eerraa)
 // SPDX-License-Identifier: GPL-2.0-or-later
-#ifdef VIA_ENABLE
 
 #include "n8x.h"
 #include "quantum.h"
 #include "eeprom.h"
 #include "backlight.h"
 
-static bool countstart = false;
-static uint32_t blink_timer = 0;
+my_config_t g_my_config;
 backlight_config_t backlight_config;
 
-blink_settings_config g_config = {
-    .backlight_bright = 10,
-    .backlight_effect = 0,
-    .blink_speed = 2,
-    .breathing_period = 5
-};
+static bool countstart = false;
+static uint32_t blink_timer = 0;
 
-void keyboard_post_init_kb(void) {
-	
-    backlight_level(g_config.backlight_bright);
-    breathing_period_set(g_config.breathing_period);
-    if (g_config.backlight_effect == 1) breathing_enable();
-    keyboard_post_init_user();
+static void read_my_config_from_eeprom(my_config_t* config) {
+    config->raw = eeconfig_read_kb() & 0xffffff;
 }
 
-void values_load(void)
-{
-    eeprom_read_block( &g_config, ((void*)VIA_EEPROM_CUSTOM_CONFIG_ADDR), sizeof(g_config) );
+static void write_my_config_to_eeprom(my_config_t* config) {
+    eeconfig_update_kb(config->raw);
 }
 
-void values_save(void)
-{
-    eeprom_update_block( &g_config, ((void*)VIA_EEPROM_CUSTOM_CONFIG_ADDR), sizeof(g_config) );
-} 
-
-void via_init_kb(void)
-{
-    if (via_eeprom_is_valid()) {
-        values_load();
-    } else    {
-        values_save();
-    }
+void matrix_init_kb(void) {
+	read_my_config_from_eeprom(&g_my_config);
+    breathing_period_set(g_my_config.breathing_period);
+    matrix_init_user();
 }
 
-void custom_config_get_value( uint8_t *data )
-{
-    // data = [ value_id, value_data ]
-    uint8_t *value_id   = &(data[0]);
-    uint8_t *value_data = &(data[1]);
-
-    switch ( *value_id )
-    {
-        case id_custom_backlight_brightness:
-        {
-            *value_data = g_config.backlight_bright;
-            break;
-        }
-        case id_custom_backlight_effect:
-        {
-            *value_data = g_config.backlight_effect;
-            break;
-        }
-        case id_custom_breathing_period:
-        {
-            *value_data = g_config.breathing_period;
-            break;
-        }
-        case id_custom_blink_speed:
-        {
-            *value_data = g_config.blink_speed;
-            break;
-        }
-    }
-}
-
-void custom_config_set_value( uint8_t *data )
-{
-    // data = [ value_id, value_data ]
-    uint8_t *value_id   = &(data[0]);
-    uint8_t *value_data = &(data[1]);
-
-    switch ( *value_id )
-    {
-        case id_custom_backlight_brightness:
-        {
-            g_config.backlight_bright = *value_data;
-            backlight_level(g_config.backlight_bright);
-            break;
-        }
-        case id_custom_backlight_effect:
-        {
-            g_config.backlight_effect = *value_data;
-            switch (g_config.backlight_effect) {
-                case 0:
-                    breathing_disable();
-                    break;
-                case 1:
-                    breathing_enable();
-                    break;
-                case 2:
-                    breathing_disable();
-                    backlight_set(g_config.backlight_bright);
-                    break;
-                case 3:
-                    breathing_disable();
-                    backlight_set(0);
-                    break;
-            }
-            break;
-        }
-        case id_custom_breathing_period:
-        {
-            g_config.breathing_period = *value_data;
-            breathing_period_set(g_config.breathing_period);
-            break;
-        }
-        case id_custom_blink_speed:
-        {
-            g_config.blink_speed = *value_data;
-            break;
-        }
-    }
-}
-
-void via_custom_value_command_kb(uint8_t *data, uint8_t length)
-{
-    // data = [ command_id, channel_id, value_id, value_data ]
-    uint8_t *command_id        = &(data[0]);
-    uint8_t *channel_id        = &(data[1]);
-    uint8_t *value_id_and_data = &(data[2]);
-
-    if ( *channel_id == id_custom_channel ) {
-        switch ( *command_id )
-        {
-            case id_custom_set_value:
-            {
-                custom_config_set_value(value_id_and_data);
-                break;
-            }
-            case id_custom_get_value:
-            {
-                custom_config_get_value(value_id_and_data);
-                break;
-            }
-            case id_custom_save:
-            {
-                values_save();
-                break;
-            }
-            default:
-            {
-                // Unhandled message.
-                *command_id = id_unhandled;
-                break;
-            }
-        }
-        return;
-    }
-
-    // Return the unhandled state
-    *command_id = id_unhandled;
-
+void eeconfig_init_kb(void) {
+    g_my_config.raw = 0;
+    g_my_config.breathing_period = 5;
+    g_my_config.blink_value = 0;
+    g_my_config.blink_speed = 3;
+    write_my_config_to_eeprom(&g_my_config);
+    eeconfig_init_user();
 }
 
 void housekeeping_task_kb(void){
 	if (countstart) {
-		if (timer_elapsed32(blink_timer) > (g_config.blink_speed * 20)) {
-            if (g_config.backlight_effect == 2) {
-                backlight_set(g_config.backlight_bright);
-            } else if (g_config.backlight_effect == 3) {
+		if (timer_elapsed32(blink_timer) > (g_my_config.blink_speed * 20)) {
+            if (g_my_config.blink_value == 1) {
+                backlight_set(backlight_config.level);
+            } else if (g_my_config.blink_value == 2) {
                 backlight_set(0);
             }
 			countstart = false;
@@ -180,25 +50,58 @@ void housekeeping_task_kb(void){
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
-        switch (g_config.backlight_effect) {
-            case 2:
+        switch (g_my_config.blink_value) {
+            case 1:
                 if (!countstart) {
                     blink_timer = timer_read32();
                     backlight_set(0);
                     countstart = true;
                 }
                 break;
-            case 3:
+            case 2:
                 if (!countstart) {
                     blink_timer = timer_read32();
-                    backlight_set(g_config.backlight_bright);
+                    backlight_set(backlight_config.level);
                     countstart = true;
                 }
                 break;
         }
+        switch(keycode) {
+            case BR_UP:
+                if (g_my_config.breathing_period < 10) {
+                    g_my_config.breathing_period++;
+                }
+                breathing_period_set(g_my_config.breathing_period);
+                write_my_config_to_eeprom(&g_my_config);
+                return false;
+            case BR_DOWN:
+                if (g_my_config.breathing_period > 1) {
+                    g_my_config.breathing_period--;
+                }
+                breathing_period_set(g_my_config.breathing_period);
+                write_my_config_to_eeprom(&g_my_config);
+                return false;
+            case BK_OUT:
+                g_my_config.blink_value = (g_my_config.blink_value == 1) ? 0 : 1;
+                write_my_config_to_eeprom(&g_my_config);
+                return false;
+            case BK_IN:
+                g_my_config.blink_value = (g_my_config.blink_value == 2) ? 0 : 2;
+                write_my_config_to_eeprom(&g_my_config);
+                return false;
+            case BK_UP:
+                if (g_my_config.blink_speed < 10) {
+                    g_my_config.blink_speed++;
+                }
+                write_my_config_to_eeprom(&g_my_config);
+                return false;
+            case BK_DOWN:
+                if (g_my_config.blink_speed > 1) {
+                    g_my_config.blink_speed--;
+                }
+                write_my_config_to_eeprom(&g_my_config);
+                return false;
+        }
     }
-    
     return process_record_user(keycode, record);
 }
-
-#endif
--- keyboards/era/linx3/n8x/n8x.h
+++ keyboards/era/linx3/n8x/n8x.h
@@ -3,27 +3,25 @@
 
 #pragma once
 
-#ifdef VIA_ENABLE
 #include "quantum.h"
 
-// struct to save things
-typedef struct {
-    uint8_t backlight_bright;                 // 1 byte
-    uint8_t backlight_effect;                 // 1 byte
-    uint8_t breathing_period;                 // 1 byte
-    uint8_t blink_speed;                      // 1 byte
-} blink_settings_config;                      // total 4 bytes
-
-// via value id declaration
-enum n87s_custom_value_id {
-    id_custom_backlight_brightness = 0,
-    id_custom_backlight_effect,
-    id_custom_breathing_period,
-    id_custom_blink_speed
+enum my_keycodes {
+  BR_UP = QK_KB_0,
+  BR_DOWN,
+  BK_OUT,
+  BK_IN,
+  BK_UP,
+  BK_DOWN
 };
 
-// function declaration
-void indicator_config_set_value( uint8_t *data );
-void indicator_config_get_value( uint8_t *data );
-void indicator_config_save ( void );
-#endif
+typedef union {
+    uint32_t raw;
+    struct {
+        uint8_t    breathing_period :4;
+        uint8_t    blink_value      :2;
+        uint8_t    blink_speed      :4;
+        
+    };
+} my_config_t;
+
+extern my_config_t g_my_config;
