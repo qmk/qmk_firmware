6d769ab4879e73580e2665ca447703f4b85c47cd
Duncan Sutherland <dunk2k_2000@hotmail.com>
2024-04-12 21:54:37 +0100

    migrated

--- keyboards/omkbd/runner3680/5x6/keymaps/default/keymap.c
+++ keyboards/omkbd/runner3680/5x6/keymaps/default/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * | Ctrl | GUI  | Alt  | EISU |  Del | Space|      | Enter| Bksp | EISU |  ESC |Adjust| Left |
      * `-----------------------------------------'      `-----------------------------------------'
      */
-  [_QWERTY] = LAYOUT(
+  [_QWERTY] = LAYOUT_ortho_5x12(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
       KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------'      `-----------------------------------------'
      */
-  [_ADJUST] =  LAYOUT(
+  [_ADJUST] =  LAYOUT_ortho_5x12(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
       _______, RGBRST,  QK_BOOT,   _______, _______, _______,    _______, _______, _______, _______, _______, _______,
       _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______,    _______, _______, _______, _______, _______, _______,
