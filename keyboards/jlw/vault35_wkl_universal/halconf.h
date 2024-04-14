343c6618ead6f9caec2d8f77afd25967da2431ca
josh-l-wang <josh@jlwang.org>
2024-04-14 10:00:23 -0400

    updated halconf

--- keyboards/jlw/vault35_wkl_universal/halconf.h
+++ keyboards/jlw/vault35_wkl_universal/halconf.h
@@ -1,5 +1,16 @@
+<<<<<<< Updated upstream
 #pragma once
 
 //#define HAL_USE_PWM TRUE
 
 #include_next <halconf.h>
+=======
+// Copyright 2024 jlw
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define HAL_USE_PWM TRUE
+
+#include_next <halconf.h>
+>>>>>>> Stashed changes
