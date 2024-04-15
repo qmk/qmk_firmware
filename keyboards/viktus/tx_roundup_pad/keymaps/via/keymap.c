11c6b8654c2267eed100cb3de4cd135bd980bd07
blindassassin111 <38090555+blindassassin111@users.noreply.github.com>
2024-04-15 00:26:40 -0500

    Rename layout
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/viktus/tx_roundup_pad/keymaps/via/keymap.c
+++ keyboards/viktus/tx_roundup_pad/keymaps/via/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_all(
+    [0] = LAYOUT(
         KC_F1,  KC_F2,   KC_F3,   KC_F4,
         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,  KC_P8,   KC_P9,   KC_PPLS,
