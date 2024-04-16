baa6000ed33f1f190f43bcd993f84f3b057e952d
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-04-16 16:36:21 -0700

    Data-Driven Keyboard Conversions: H, Part 3 (#23530)

--- keyboards/handwired/uthol/rev3/rules.mk
+++ keyboards/handwired/uthol/rev3/rules.mk
@@ -1,11 +1,1 @@
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes
-NKRO_ENABLE = yes
 KEYBOARD_SHARED_EP = yes
-OLED_ENABLE = yes
-WPM_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-ENCODER_ENABLE = yes
-RGBLIGHT_ENABLE = yes
