41bb17ab1a0c8f3bc0537839fb86a00d6560c42c
era <phyojinbin@gmail.com>
2024-04-18 19:09:47 +0900

    Modified tomak
    Modified tomak

--- keyboards/era/sirind/tomak/tomak.c
+++ keyboards/era/sirind/tomak/tomak.c
@@ -8,92 +8,6 @@
 
 tomak_config_t g_tomak_config;
 
-static void read_tomak_config_from_eeprom(tomak_config_t* config) {
-    config->raw = eeconfig_read_kb() & 0xffffffff;
-}
-
-static void write_tomak_config_to_eeprom(tomak_config_t* config) {
-    eeconfig_update_kb(config->raw);
-}
-
-uint8_t increment( uint8_t value, uint8_t step, uint8_t min, uint8_t max )
-{
-    int16_t new_value = value;
-    new_value += step;
-    return MIN( MAX( new_value, min ), max );
-}
-
-uint8_t decrement( uint8_t value, uint8_t step, uint8_t min, uint8_t max )
-{
-    int16_t new_value = value;
-    new_value -= step;
-    return MIN( MAX( new_value, min ), max );
-}
-
-void indicator_operation_toggle(void)
-{
-    g_tomak_config.indicator_toggle = g_tomak_config.indicator_toggle ? false : true;
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void indicator_override_toggle(void)
-{
-    g_tomak_config.indicator_override = g_tomak_config.indicator_override ? false : true;
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void indicator_brightness_increase(void)
-{
-    g_tomak_config.indicator_hsv.v = increment( g_tomak_config.indicator_hsv.v, 8, 0, 255 );
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void indicator_brightness_decrease(void)
-{
-    g_tomak_config.indicator_hsv.v = decrement( g_tomak_config.indicator_hsv.v, 8, 0, 255 );
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void indicator_hue_increase(void)
-{
-    g_tomak_config.indicator_hsv.h = increment( g_tomak_config.indicator_hsv.h, 8, 0, 255 );
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void indicator_hue_decrease(void)
-{
-    g_tomak_config.indicator_hsv.h = decrement( g_tomak_config.indicator_hsv.h, 8, 0, 255 );
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void indicator_sat_increase(void)
-{
-    g_tomak_config.indicator_hsv.s = increment( g_tomak_config.indicator_hsv.s, 8, 0, 255 );
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void indicator_sat_decrease(void)
-{
-    g_tomak_config.indicator_hsv.s = decrement( g_tomak_config.indicator_hsv.s, 8, 0, 255 );
-    write_tomak_config_to_eeprom(&g_tomak_config);
-}
-
-void eeconfig_init_kb(void) {
-    g_tomak_config.raw = 0;
-    g_tomak_config.indicator_toggle = true;
-    g_tomak_config.indicator_override = false;
-    g_tomak_config.indicator_hsv.h = 0;
-    g_tomak_config.indicator_hsv.s = 0;
-    g_tomak_config.indicator_hsv.v = 255;
-    write_tomak_config_to_eeprom(&g_tomak_config);
-    eeconfig_init_user();
-}
-
-void matrix_init_kb(void) {
-    read_tomak_config_from_eeprom(&g_tomak_config);
-    matrix_init_user();
-}
-
 #ifdef TOMAK_CONFIG_SYNC
 void tomak_config_sync_handler(uint8_t initiator2target_buffer_size, const void* initiator2target_buffer, uint8_t target2initiator_buffer_size, void* target2initiator_buffer) {
     if (initiator2target_buffer_size == sizeof(g_tomak_config)) {
@@ -136,6 +50,44 @@ void housekeeping_task_kb(void) {
 }
 #endif
 
+static void read_tomak_config_from_eeprom(tomak_config_t* config) {
+    config->raw = eeconfig_read_kb() & 0xffffffff;
+}
+
+static void write_tomak_config_to_eeprom(tomak_config_t* config) {
+    eeconfig_update_kb(config->raw);
+}
+
+uint8_t increment( uint8_t value, uint8_t step, uint8_t min, uint8_t max )
+{
+    int16_t new_value = value;
+    new_value += step;
+    return MIN( MAX( new_value, min ), max );
+}
+
+uint8_t decrement( uint8_t value, uint8_t step, uint8_t min, uint8_t max )
+{
+    int16_t new_value = value;
+    new_value -= step;
+    return MIN( MAX( new_value, min ), max );
+}
+
+void eeconfig_init_kb(void) {
+    g_tomak_config.raw = 0;
+    g_tomak_config.indicator_toggle = true;
+    g_tomak_config.indicator_override = false;
+    g_tomak_config.indicator_hsv.h = 0;
+    g_tomak_config.indicator_hsv.s = 0;
+    g_tomak_config.indicator_hsv.v = 255;
+    write_tomak_config_to_eeprom(&g_tomak_config);
+    eeconfig_init_user();
+}
+
+void matrix_init_kb(void) {
+    read_tomak_config_from_eeprom(&g_tomak_config);
+    matrix_init_user();
+}
+
 bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max)
 {
     if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
@@ -164,56 +116,64 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         case IN_TOGG:
         {
             if (record->event.pressed) {
-                indicator_operation_toggle();
+                g_tomak_config.indicator_toggle = g_tomak_config.indicator_toggle ? false : true;
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
         case IN_OVER:
         {
             if (record->event.pressed) {
-                indicator_override_toggle();
+                g_tomak_config.indicator_override = g_tomak_config.indicator_override ? false : true;
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
         case IN_BRI:
         {
             if (record->event.pressed) {
-                indicator_brightness_increase();
+                g_tomak_config.indicator_hsv.v = increment( g_tomak_config.indicator_hsv.v, 8, 0, 255 );
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
         case IN_BRD:
         {
             if (record->event.pressed) {
-                indicator_brightness_decrease();
+                g_tomak_config.indicator_hsv.v = decrement( g_tomak_config.indicator_hsv.v, 8, 0, 255 );
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
         case IN_HUEI:
         {
             if (record->event.pressed) {
-                indicator_hue_increase();
+                g_tomak_config.indicator_hsv.h = increment( g_tomak_config.indicator_hsv.h, 8, 0, 255 );
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
         case IN_HUED:
         {
             if (record->event.pressed) {
-                indicator_hue_decrease();
+                g_tomak_config.indicator_hsv.h = decrement( g_tomak_config.indicator_hsv.h, 8, 0, 255 );
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
         case IN_SATI:
         {
             if (record->event.pressed) {
-                indicator_sat_increase();
+                g_tomak_config.indicator_hsv.s = increment( g_tomak_config.indicator_hsv.s, 8, 0, 255 );
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
         case IN_SATD:
         {
             if (record->event.pressed) {
-                indicator_sat_decrease();
+                g_tomak_config.indicator_hsv.s = decrement( g_tomak_config.indicator_hsv.s, 8, 0, 255 );
+                write_tomak_config_to_eeprom(&g_tomak_config);
             }
             return false;
         }
