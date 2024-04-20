fecef4b63e1e3aeecc47ce0b1dd1d80c61fac867
Ivan Gromov <38141348+key10iq@users.noreply.github.com>
2024-04-20 18:19:31 +0400

    Update keymap.c

--- keyboards/keyten/lisa/keymaps/default/keymap.c
+++ keyboards/keyten/lisa/keymaps/default/keymap.c
@@ -34,18 +34,18 @@ void matrix_init_user(void) {
 
 //layer_state_t layer_state_set_user(layer_state_t state) {
 //    switch (get_highest_layer(state)) {
-//	case 1:
-//        writePinHigh(B2);
-//        break;	
-//    case 2:
-//        writePinHigh(B2);
-//        break;
-//	case 3:
-//        writePinHigh(B2);
-//        break;	
-//    default:
-//        writePinLow(B2);
-//        break;
+//        case 1:
+//		    writePinHigh(B2);
+//		    break;	
+//        case 2:
+//		    writePinHigh(B2);
+//		    break;
+//        case 3:
+//		    writePinHigh(B2);
+//		    break;	
+//        default:
+//		    writePinLow(B2);
+//		    break;
 //    }
 //    return state;
 //}
