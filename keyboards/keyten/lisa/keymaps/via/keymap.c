1e7f74e4ed6a0f5d2b7f0cb82b12dc0c1e48e648
Ivan Gromov <38141348+key10iq@users.noreply.github.com>
2024-04-21 04:15:13 +0400

    Update keyboards/keyten/lisa/keymaps/via/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/keyten/lisa/keymaps/via/keymap.c
+++ keyboards/keyten/lisa/keymaps/via/keymap.c
@@ -25,27 +25,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS, KC_TRNS
 	)
 };
-
-void matrix_init_user(void) {
-    setPinOutput(B2);
-//    writePinLow(B2);
-    writePinHigh(B2);
-}
-
-//layer_state_t layer_state_set_user(layer_state_t state) {
-//    switch (get_highest_layer(state)) {
-//        case 1:
-//		    writePinHigh(B2);
-//		    break;	
-//        case 2:
-//		    writePinHigh(B2);
-//		    break;
-//        case 3:
-//		    writePinHigh(B2);
-//		    break;	
-//        default:
-//		    writePinLow(B2);
-//        break;
-//    }
-//    return state;
-//}
