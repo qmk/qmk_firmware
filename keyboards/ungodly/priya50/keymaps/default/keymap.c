1e530985293500e945b94995440a6d0727fd403a
Luis Godinez <beantek@gmail.com>
2024-04-20 20:09:39 -0700

    Overhaul config json

---
+++ keyboards/ungodly/priya50/keymaps/default/keymap.c
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[0] = LAYOUT(
+		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+		KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+		KC_LCTL, KC_LALT, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  KC_RGUI, KC_RALT, KC_RCTL, KC_APP
+	),
+
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0]   =  { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD) },
+    // [1]   =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
+    // [2] =  { ENCODER_CCW_CW(KC_VOLD,  KC_VOLU) },
+};
+#endif
