9939c86037257ddb736a90f3d3d77b9f3af2143d
Simon <68034180+rarepotato8de@users.noreply.github.com>
2024-04-23 22:28:32 +0200

    Update keyboards/rarepotato8de/3x3macropad/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/rarepotato8de/3x3macropad/keymaps/default/keymap.c
+++ keyboards/rarepotato8de/3x3macropad/keymaps/default/keymap.c
@@ -24,17 +24,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    }
-    return false;
-}
-
 #ifdef OLED_ENABLE
 static void render_bongo1(void) {
   static const char PROGMEM data[] = {
