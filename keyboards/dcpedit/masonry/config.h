fda73e848b5fc34957bc395753609e7676040428
dcpedit <dcpedit@hotmail.com>
2024-04-23 23:08:49 -0700

    Modify keymap

---
+++ keyboards/dcpedit/masonry/config.h
@@ -0,0 +1,8 @@
+// Copyright 2024 Ming-Gih Lam (@dcpedit)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+/* Hold tap config */
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 300
+#define PERMISSIVE_HOLD
