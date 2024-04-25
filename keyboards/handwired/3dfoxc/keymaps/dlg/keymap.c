d636500bfb3c322d530ccbdda37f87fe5d27a0ca
Goodrich David L <goodrichdavidl@johndeere.com>
2024-04-25 08:44:06 -0500

    pc default, both backspace

--- keyboards/handwired/3dfoxc/keymaps/dlg/keymap.c
+++ keyboards/handwired/3dfoxc/keymaps/dlg/keymap.c
@@ -19,15 +19,15 @@
 enum custom_layers {
     _BL,
     _FL,
-    _MAC,
+    _PC,
     _LA,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Keymap _BL: (Base Layer) Default Layer
+  /* Keymap _BL: (Base Layer) Default Layer (Mac)
    * ,---------------------------------------------------------------.
-   * |~` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |Bks|???|Esc|
+   * |~` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |Bks|Bks|Esc|
    * |---------------------------------------------------------------|
    * |Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \  |Del|
    * |---------------------------------------------------------------|
@@ -39,16 +39,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `--------------------------------------------------''-----------'
    */
     [_BL] = LAYOUT(
-       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, XXXXXXX, KC_ESC,
+       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_ESC,
        KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL,
        KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_PGUP,
        KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,     KC_UP,   KC_PGDN,
-       KC_LCTL,    KC_LGUI,    LM(_LA, MOD_LALT),               KC_SPC,                          KC_LALT,    MO(_FL),       KC_LEFT, KC_DOWN, KC_RGHT
+       KC_LCTL,    KC_LALT,    KC_LGUI,                          KC_SPC,                          KC_RGUI,    MO(_FL),       KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
   /* Keymap _FL: Function Layer
    * ,---------------------------------------------------------------.
-   * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del|RST|   |
+   * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|RST|RST|   |
    * |---------------------------------------------------------------|
    * |     |   |   |   |   |   |   |   |mac|   |PSc|Br-|Br+|     |Ins|
    * |---------------------------------------------------------------|
@@ -60,14 +60,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `--------------------------------------------------''-----------'
    */
     [_FL] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  QK_BOOT, _______,
-        _______,      _______, _______, _______, _______, _______, _______, _______, TG(_MAC),_______, KC_PSCR, KC_BRID, KC_BRIU, _______,     KC_INS,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  QK_BOOT, QK_BOOT, _______,
+        _______,      _______, _______, _______, _______, _______, _______, _______, TG(_PC),_______, KC_PSCR, KC_BRID, KC_BRIU, _______,     KC_INS,
         _______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,          KC_HOME,
         _______,               _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,     KC_PGUP, KC_END,
         _______,    _______,    _______,                         _______,                         _______,    _______,       KC_HOME, KC_PGDN, KC_END
     ),
 
-   /* Keymap _MAC: Mac Layer
+   /* Keymap _PC: PC Layer
    * ,---------------------------------------------------------------.
    * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
    * |---------------------------------------------------------------|
@@ -80,12 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Alt |Win |                         |Win |    ||   |   |   |
    * `--------------------------------------------------''-----------'
    */
-   [_MAC] = LAYOUT(
+   [_PC] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______,               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______,
-        KC_LCTL,    KC_LALT,    KC_LGUI,                         _______,                         KC_RGUI,    _______,       _______, _______, _______
+        KC_LCTL,    KC_LGUI,    LM(_LA, MOD_LALT),             _______,                         KC_LALT,    _______,       _______, _______, _______
     ),
 
   /* Keymap _LA: Left Alt Layer - LALT-4 maps to LALT-F4 so I can quit apps
