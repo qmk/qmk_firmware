5163c3f57aa719145c9d39a4d230df62a8ed47ec
zvecr <git@zvecr.com>
2024-04-25 06:51:31 +0100

    Migrate build target markers to keyboard.json

--- keyboards/matrix/m12og/rev1/rules.mk
+++ keyboards/matrix/m12og/rev1/rules.mk
@@ -1,12 +1,6 @@
-# MCU name
-MCU = STM32F103
-
+# custom bootloader
 MCU_LDSCRIPT = m12og_v1
-
 BOARD = m12og_v1
-
-# Bootloader selection
-BOOTLOADER = custom
 
 CUSTOM_MATRIX = lite
 SRC += matrix.c
