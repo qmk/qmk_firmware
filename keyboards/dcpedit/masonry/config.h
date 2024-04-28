a7cf162eb2ef7dcce71e563dcd9f0f40374c95f6
dcpedit <dcpedit@hotmail.com>
2024-04-27 21:09:44 -0700

    remove config.h

--- keyboards/dcpedit/masonry/config.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 Ming-Gih Lam (@dcpedit)
-// SPDX-License-Identifier: GPL-2.0-or-later
-#pragma once
-
-/* Hold tap config */
-#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 300
-#define PERMISSIVE_HOLD
