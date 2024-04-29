31e8121dcec736c3c17ce6dd6e694c178aa96fd3
chalex <alejandrelee+magbogbro@gmail.com>
2024-04-29 06:55:22 -0400

    move indicator LED info from config.h to info.json

--- keyboards/gh80_3000/config.h
+++ keyboards/gh80_3000/config.h
@@ -1,3 +1,20 @@
+/*
+Copyright 2024 Alexander Lee <chlx.bsmt@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
@@ -6,7 +23,7 @@
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-#define LED_NUM_LOCK_PIN B5
-#define LED_CAPS_LOCK_PIN B6
-#define LED_SCROLL_LOCK_PIN B7
-#define LED_PIN_ON_STATE 0
+// #define LED_NUM_LOCK_PIN B5
+// #define LED_CAPS_LOCK_PIN B6
+// #define LED_SCROLL_LOCK_PIN B7
+// #define LED_PIN_ON_STATE 0
