7e8c87ebd184647d756fe89f3dc22a9931455e81
chalex <alejandrelee+magbogbro@gmail.com>
2024-04-29 06:22:10 -0400

    enable indicator LED functionality

--- keyboards/gh80_3000/config.h
+++ keyboards/gh80_3000/config.h
@@ -5,3 +5,8 @@
 
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+#define LED_NUM_LOCK_PIN B5
+#define LED_CAPS_LOCK_PIN B6
+#define LED_SCROLL_LOCK_PIN B7
+#define LED_PIN_ON_STATE 0
