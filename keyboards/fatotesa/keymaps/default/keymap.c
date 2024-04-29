22100c3576db757a4e9dc68cbd2c6b901d9613ed
Stefan Gluszek <stefang@kahoot.com>
2024-04-29 08:11:21 +0200

    Use One Shot Modifiers for shifts

--- keyboards/fatotesa/keymaps/default/keymap.c
+++ keyboards/fatotesa/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                                        KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BACKSPACE,
         LT(1, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T,                        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_ENTER,
         OSM(MOD_LCTL), KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_BACKSLASH,
-        KC_LEFT_SHIFT, KC_LEFT_ANGLE_BRACKET, KC_Z, KC_X, KC_C, KC_V, KC_B,                    KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RIGHT_SHIFT,
+        OSM(MOD_LSFT), KC_LEFT_ANGLE_BRACKET, KC_Z, KC_X, KC_C, KC_V, KC_B,                    KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, OSM(MOD_RSFT),
         CW_TOGG, KC_LWIN, KC_LEFT_ALT, KC_BACKSPACE, OSM(MOD_LALT),                                 KC_SPACE, OSM(MOD_RALT), KC_RIGHT_CTRL, KC_NO, KC_NO
         ),
     [1] = LAYOUT(
