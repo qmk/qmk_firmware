6e1c7b06eed99e21002738c849f342ca94839322
Stefan Gluszek <stefang@kahoot.com>
2024-04-29 15:22:30 +0200

    Use One Shot Modifier for the Windows key

--- keyboards/fatotesa/keymaps/default/keymap.c
+++ keyboards/fatotesa/keymaps/default/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(1, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T,                        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_ENTER,
         OSM(MOD_LCTL), KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_BACKSLASH,
         OSM(MOD_LSFT), KC_LEFT_ANGLE_BRACKET, KC_Z, KC_X, KC_C, KC_V, KC_B,                    KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, OSM(MOD_RSFT),
-        CW_TOGG, KC_LWIN, KC_LEFT_ALT, KC_BACKSPACE, OSM(MOD_LALT),                                 KC_SPACE, OSM(MOD_RALT), KC_RIGHT_CTRL, KC_NO, KC_NO
+        CW_TOGG, OSM(MOD_LGUI), KC_LEFT_ALT, KC_BACKSPACE, OSM(MOD_LALT),                                 KC_SPACE, OSM(MOD_RALT), KC_RIGHT_CTRL, KC_NO, KC_NO
         ),
     [1] = LAYOUT(
         _______, _______, _______, _______, _______,                                                                 KC_PSCR, _______, _______, _______,
