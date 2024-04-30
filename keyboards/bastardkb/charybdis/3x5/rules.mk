cdc0623e2f50f060aedbbdfeb0aae44ccfb49128
burkfers <finrod09@users.noreply.github.com>
2024-04-30 09:34:40 +0200

    charybdis/3x5: dd conversion pt2

--- keyboards/bastardkb/charybdis/3x5/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/rules.mk
@@ -4,11 +4,9 @@
 AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
-RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix functionality
 
 SPLIT_KEYBOARD = yes
 
-POINTING_DEVICE_ENABLE = yes # Enable trackball
 POINTING_DEVICE_DRIVER = pmw3360
 
 SERIAL_DRIVER = vendor
