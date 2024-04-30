d43b0b711d0a820259f45fcb968de8fa00b50d61
blindassassin111 <38090555+blindassassin111@users.noreply.github.com>
2024-04-30 00:44:02 -0500

    Update keyboards/viktus/tx_roundup_pad/keymaps/via/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/viktus/tx_roundup_pad/keymaps/via/keymap.c
+++ keyboards/viktus/tx_roundup_pad/keymaps/via/keymap.c
@@ -17,11 +17,11 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT_numpad_6x4(
         KC_F1,  KC_F2,   KC_F3,   KC_F4,
         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,  KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,  KC_P5,   KC_P6,
+        KC_P7,  KC_P8,   KC_P9,
+        KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
         KC_P1,  KC_P2,   KC_P3,
         KC_P0,           KC_PDOT, KC_PENT
     )
