c1c63e56f33ca05b2cdb504c29c47d6927d6990a
devTayls <taylor.pine@pdq.com>
2024-05-01 09:27:47 -0600

    chore(config): make up commit saving before I edit more

---
+++ keyboards/keebio/iris/keymaps/devtayls/config.h
@@ -0,0 +1,2 @@
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
