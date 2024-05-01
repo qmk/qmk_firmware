fd35cb750f0cbcaac2592bc00dd744aa1e0f419f
burkfers <finrod09@users.noreply.github.com>
2024-05-01 09:22:40 +0200

    charybdis/4x6: remove unneccessary disabling of audio support

--- keyboards/bastardkb/charybdis/4x6/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/rules.mk
@@ -1,7 +1,6 @@
 # Build Options
 #   change yes to no to disable
 #
-AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
 
