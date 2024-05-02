9fd4f7a0a32fc859eba57c20c5c91267952eb4a3
burkfers <finrod09@users.noreply.github.com>
2024-05-02 08:36:26 +0200

    skeletyl: move split to DD, purge unnecessary build options

--- keyboards/bastardkb/skeletyl/rules.mk
+++ keyboards/bastardkb/skeletyl/rules.mk
@@ -1,9 +1,1 @@
-# Build Options
-#   change yes to no to disable
-#
-RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
-RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
-
-SPLIT_KEYBOARD = yes
-
 SERIAL_DRIVER = vendor
