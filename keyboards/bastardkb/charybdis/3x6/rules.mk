00c35c02b7c516d1ce6f0f8dc5e2db56ec57de09
burkfers <finrod09@users.noreply.github.com>
2024-05-02 08:35:27 +0200

    charybdis/3x6: move split to DD, purge unnecessary build options

--- keyboards/bastardkb/charybdis/3x6/rules.mk
+++ keyboards/bastardkb/charybdis/3x6/rules.mk
@@ -1,11 +1,3 @@
-# Build Options
-#   change yes to no to disable
-#
-RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
-RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
-
-SPLIT_KEYBOARD = yes
-
 POINTING_DEVICE_DRIVER = pmw3360
 
 SERIAL_DRIVER = vendor
