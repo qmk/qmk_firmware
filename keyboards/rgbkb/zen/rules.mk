bbb8559c39f4f56c3875e0b60513439e6fb774fc
zvecr <git@zvecr.com>
2024-05-05 08:12:33 +0100

    Tidy rgbkb/zen

--- keyboards/rgbkb/zen/rules.mk
+++ keyboards/rgbkb/zen/rules.mk
@@ -1,13 +1,1 @@
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight
-
 DEFAULT_FOLDER = rgbkb/zen/rev2
