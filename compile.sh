ab7e9b64b18b09a033ff15fa4cebe858042d935a
Evgeny K <3193631@gmail.com>
2024-05-05 22:40:41 +0200

    ci: update compile.sh, action to add default km builds to release

--- compile.sh
+++ compile.sh
@@ -9,11 +9,33 @@ echo "      Version tag: $TAG       "
 echo "=============================="
 
 
-echo "Compiling for wired"
-sed -i 's/WORK_MODE THREE_MODE/WORK_MODE USB_MODE/' keyboards/nuphy/gem80/ansi/config.h
-make clean
-qmk compile --compiledb -j 0 -e TARGET="gem80_wired_$TAG" -kb nuphy/gem80/ansi -km via
+echo ""
+echo ""
+echo "===== VIA variant ====="
+echo ""
 
-echo "Compiling for three-mode"
+echo "Switch to wired variant"
+sed -i 's/WORK_MODE THREE_MODE/WORK_MODE USB_MODE/' keyboards/nuphy/gem80/ansi/config.h
+echo ""
+echo ""
+
+echo "[via] Compiling for wired"
+make clean
+qmk compile --compiledb -j 0 -e TARGET="gem80_wired_via_$TAG" -kb nuphy/gem80/ansi -km via
+
+echo "[default] Compiling for wired"
+make clean
+qmk compile --compiledb -j 0 -e TARGET="gem80_wired_default_$TAG" -kb nuphy/gem80/ansi -km default
+
+echo "Switch to wired variant"
 sed -i 's/WORK_MODE USB_MODE/WORK_MODE THREE_MODE/' keyboards/nuphy/gem80/ansi/config.h
-qmk compile --compiledb -j 0 -e TARGET="gem80_threemode_$TAG" -kb nuphy/gem80/ansi -km via
+echo ""
+echo ""
+
+echo "[via] Compiling for three-mode"
+make clean
+qmk compile --compiledb -j 0 -e TARGET="gem80_threemode_via_$TAG" -kb nuphy/gem80/ansi -km via
+
+echo "[via] Compiling for three-mode"
+make clean
+qmk compile --compiledb -j 0 -e TARGET="gem80_threemode_default_$TAG" -kb nuphy/gem80/ansi -km default
