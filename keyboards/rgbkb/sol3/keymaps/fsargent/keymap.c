fad0a28f6fe8ad56c52f83f52dbc97887854316f
Felix Sargent <fsargent@recruitbot.com>
2024-05-07 09:05:52 -0700

    Delete key more accessible.

--- keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/fsargent/keymap.c
@@ -71,7 +71,7 @@ enum sol_keycodes {
 
 #define LTHUMB0		CMD_T(KC_BSPC)
 #define LTHUMB0WIN	CTL_T(KC_BSPC)
-#define LTHUMB1		MT(MOD_LCTL, KC_ENT)
+#define LTHUMB1		MT(MOD_LCTL, KC_DEL)
 #define LTHUMB2		MT(MOD_LALT, KC_TAB)
 #define RTHUMB0		KC_SPC
 #define RTHUMB2		KC_ENT
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS]	= {
 	*/
 
 	[_MAC]	= LAYOUT(
-	KC_GRV,	KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_ESC,						KC_NO,		KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_MINS,
+	KC_GRV,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_ESC,						KC_NO,		KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_DEL,
 	KC_TAB,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		TO(_MAC),					KC_NO,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_BSLS,
 	MEHESC,		LCTL_T(KC_A),	LALT_T(KC_S),	LGUI_T(KC_D),	LSFT_T(KC_F),	KC_G,		TO(_WIN),					KC_F11,		KC_H,		MT_J,		MT_K,		MT_L,		MT_SEMI,	KC_QUOT,
 	KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		TO(_GAME),					KC_F12,		KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,
