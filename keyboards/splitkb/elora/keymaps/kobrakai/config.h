d13271012485129317b1ac11a19409f0eb0507d9
Nick Brown <nickbrown.dev@gmail.com>
2024-05-08 15:42:16 +0100

    Updated Elora Keymap and config
    Re-added GAMING layer as best I can remember it

--- keyboards/splitkb/elora/keymaps/kobrakai/config.h
+++ keyboards/splitkb/elora/keymaps/kobrakai/config.h
@@ -21,3 +21,22 @@
 
 // Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed
 #define DEBOUNCE 5
+
+
+// RGB Animations
+// Sleep when computer turned off
+#define RGBLIGHT_SLEEP
+// Split Keyboard support
+#define RGBLIGHT_SPLIT
+// Default RGB Mode
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+// Default Brightness
+#define RGBLIGHT_DEFAULT_VAL 128
+// Default On/Off
+#define RGBLIGHT_DEFAULT_ON true
+// Enable animations
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
