aa7872554e79aa908edf04a8ca4793294bfdd083
tadashi <sanada@cytoronix.com>
2024-05-09 22:08:19 +0900

    Fixed lint error.

--- keyboards/tercy/rev004/config.h
+++ keyboards/tercy/rev004/config.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define LAYOUT( \
+#define LAYOUT2( \
  K00, K01, K02, K03, K04, K05, K40, K41, K42, K43, K44, K45,\
  K10, K11, K12, K13, K14, K15, K50, K51, K52, K53, K54, K55,\
  K20, K21, K22, K23, K24, K25, K60, K61, K62, K63, K64, K65,\
