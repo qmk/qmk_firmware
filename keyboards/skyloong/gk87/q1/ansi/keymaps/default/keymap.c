bb0cab451e4f35135a729595571842572eba9e6a
NaturalZh <zhouziran@jikedingzhi.com>
2024-05-09 16:30:35 +0800

    Create gk87 q1 keyboard

---
+++ keyboards/skyloong/gk87/q1/ansi/keymaps/default/keymap.c
@@ -0,0 +1,89 @@
+//  Copyright 2023 NaturalZh (@NaturalZh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+enum layer_names {
+    _WIN,
+    _MAC,
+    _WIN_INDEX,
+    _MAC_INDEX
+};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [_WIN] =  LAYOUT_all(
+         KC_ESC,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,     KC_F6,     KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_F11,       KC_F12,    KC_PSCR,  KC_SCRL,  KC_PAUS,                     KC_MUTE,   RGB_TOG,
+         KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,     KC_7,       KC_8,     KC_9,      KC_0,   KC_MINS,   KC_EQL,      KC_BSPC,     KC_INS,  KC_HOME,  KC_PGUP,  KC_NUM,  KC_PSLS,  KC_PAST,   KC_PMNS,
+         KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,     KC_U,       KC_I,     KC_O,      KC_P,   KC_LBRC,  KC_RBRC,      KC_BSLS,     KC_DEL,   KC_END,  KC_PGDN,   KC_P7,    KC_P8,    KC_P9,   KC_PPLS,
+        KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,     KC_J,       KC_K,     KC_L,   KC_SCLN,   KC_QUOT,                 KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,
+        KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,     KC_M,    KC_COMM,   KC_DOT,   KC_SLSH,                           KC_RSFT,                KC_UP,             KC_P1,    KC_P2,    KC_P3,   KC_PENT,
+        KC_LCTL,  KC_LGUI,  KC_LALT,             KC_SPC,   KC_SPC,                 KC_SPC,             KC_RALT,     MO(_WIN_INDEX),   KC_APP,        KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,       KC_P0,         KC_PDOT
+    ),
+
+     [_MAC] =  LAYOUT_all(
+         KC_ESC,             KC_F14,   KC_F15, C(KC_UP),  G(KC_D),   G(KC_SPC), LAG(KC_EJCT),    KC_MPRV,  KC_MPLY,        KC_MNXT,   KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_PSCR,  KC_SCRL,  KC_PAUS,                     KC_MUTE,   RGB_TOG,
+         KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,        KC_6,         KC_7,       KC_8,     KC_9,           KC_0,   KC_MINS,   KC_EQL,  KC_BSPC,     KC_INS,  KC_HOME,  KC_PGUP,  KC_NUM,  KC_PSLS,  KC_PAST,   KC_PMNS,
+         KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,        KC_Y,         KC_U,       KC_I,     KC_O,           KC_P,   KC_LBRC,  KC_RBRC,  KC_BSLS,     KC_DEL,   KC_END,  KC_PGDN,   KC_P7,    KC_P8,    KC_P9,   KC_PPLS,
+        KC_CAPS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,        KC_H,         KC_J,       KC_K,     KC_L,        KC_SCLN,   KC_QUOT,             KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,
+        KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,        KC_N,         KC_M,    KC_COMM,   KC_DOT,        KC_SLSH,                       KC_RSFT,                KC_UP,             KC_P1,    KC_P2,    KC_P3,   KC_PENT,
+        KC_LCTL,  KC_LALT,  KC_LGUI,             KC_SPC,   KC_SPC,                    KC_SPC,              KC_RCMD, MO(_MAC_INDEX),   KC_RALT,            KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,        KC_P0,        KC_PDOT
+    ),
+
+    [_WIN_INDEX] =  LAYOUT_all(
+        _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH,  G(C(KC_S)),   KC_SLEP,   KC_MPRV,  KC_MPLY,         KC_MNXT,   KC_MUTE,  KC_VOLD,     KC_VOLU,   G(KC_SPC), RCS(KC_SPC), G(RCS(KC_4)),                      KC_MUTE, RGB_TOG,
+        _______,  _______,  _______,  _______,   _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,         RGB_TOG,  RGB_RMOD,  RGB_MOD,      KC_DEL,      KC_INS,     KC_HOME,      KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,   _______,  _______,     _______,      KC_DEL,      KC_END,      KC_PGDN,     KC_P7,    KC_P8,   KC_P9, KC_PPLS,
+        _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,   _______,               _______,                                             KC_P4,    KC_P5,   KC_P6,
+        _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,                          _______,                  RGB_VAI,                   KC_P1,    KC_P2,   KC_P3, KC_PENT,
+        KC_LCTL,  KC_LGUI,  KC_LALT,             _______,  _______,                _______,             KC_RALT,         _______,    KC_APP,               _______,     RGB_SPD,     RGB_VAD,      RGB_SPI,          KC_P0,       KC_PDOT
+    ),
+
+    [_MAC_INDEX] = LAYOUT_all(
+        _______,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,     KC_F6,     KC_F7,    KC_F8,          KC_F9,     KC_F10,   KC_F11,   KC_F12,   G(KC_SPC), RCS(KC_SPC), G(RCS(KC_4)),                      KC_MUTE, RGB_TOG,
+        _______,  _______,  _______,  _______,  _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,        RGB_TOG,   RGB_RMOD,  RGB_MOD,   KC_DEL,      KC_INS,     KC_HOME,      KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,    _______,  _______,  _______,      KC_DEL,      KC_END,      KC_PGDN,     KC_P7,    KC_P8,   KC_P9, KC_PPLS,
+        _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,    _______,            _______,                                             KC_P4,    KC_P5,   KC_P6,
+        _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,                        _______,                  RGB_VAI,                   KC_P1,    KC_P2,   KC_P3, KC_PENT,
+        KC_LCTL,  KC_LALT,  KC_LGUI,            _______,  _______,                _______,             KC_RCMD,        _______,    KC_RALT,            _______,     RGB_SPD,     RGB_VAD,      RGB_SPI,          KC_P0,       KC_PDOT
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_WIN] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL))},
+    [_MAC] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(G(KC_MINS), G(KC_EQL))},
+    [_WIN_INDEX] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL))},
+    [_MAC_INDEX] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(G(KC_MINS), G(KC_EQL))}
+};
+#endif
+
+#if defined(DIP_SWITCH_MAP_ENABLE)
+const uint16_t PROGMEM dip_switch_map[NUM_DIP_SWITCHES][NUM_DIP_STATES] = {
+    DIP_SWITCH_OFF_ON(TO(0), TO(1)),
+};
+#endif
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+
+   switch (get_highest_layer(layer_state)) {
+      case 2:{
+        RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 255, 255, 255);
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 0, 0, 0);
+         }
+      } break;
+      case 3:{
+        RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 255, 255, 255);
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 0, 0, 0);
+         }
+      } break;
+
+      default:{
+         if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(WIN_MOD_INDEX, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(MAC_MOD_INDEX, 0, 0, 0);
+         }
+      }
+    }
+    return true;
+}
---
+++ keyboards/skyloong/gk87/q1/q1.c
@@ -0,0 +1,294 @@
+//  Copyright 2023 NaturalZh (@NaturalZh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#include "quantum.h"
+
+#define HC595_ST_PIN B5  //74HC595 storage register clock input
+#define HC595_SH_PIN B4  //74HC595 shift register clock input
+#define HC595_DS B3   // 74HC595 serial data input
+#include "led_hc595.c"
+uint8_t IND = 0;  //buffer of LED Display
+
+int FN_ON = 0;
+_Bool WIN_LOCK = 0;
+
+#if defined(RGB_MATRIX_ENABLE)  /*&& defined(CAPS_LOCK_INDEX)*/
+const aw20216s_led_t PROGMEM g_aw20216s_leds[AW20216S_LED_COUNT] = {
+/* Each AW20216 channel is controlled by a register at some offset between 0x00
+ * and 0xD7 inclusive.
+ * See drivers/awinic/aw20216.h for the mapping between register offsets and
+ * driver pin locations.
+ *   driver
+ *   |  R location
+ *   |  |          G location
+ *   |  |          |          B location
+ *   |  |          |          | */
+    {0, SW1_CS1,   SW1_CS2,   SW1_CS3},  //ESC
+    {0, SW3_CS1,   SW3_CS2,   SW3_CS3},  //F1
+    {0, SW4_CS1,   SW4_CS2,   SW4_CS3},
+    {0, SW5_CS1,   SW5_CS2,   SW5_CS3},  //F3
+    {0, SW6_CS1,   SW6_CS2,   SW6_CS3},
+    {0, SW7_CS1,   SW7_CS2,   SW7_CS3},  //F5
+    {0, SW8_CS1,   SW8_CS2,   SW8_CS3},
+    {0, SW9_CS1,   SW9_CS2,   SW9_CS3},  //F7
+    {0, SW10_CS1,  SW10_CS2,  SW10_CS3},
+    {0, SW11_CS1,  SW11_CS2,  SW11_CS3},  //F9
+    {0, SW12_CS1,  SW12_CS2,  SW12_CS3},  //F10
+    {0, SW2_CS10,  SW2_CS11,  SW2_CS12},
+    {0, SW3_CS10,  SW3_CS11,  SW3_CS12},  //F12
+    //encoder no led
+
+    {0, SW1_CS4,   SW1_CS5,   SW1_CS6},   //`~
+    {0, SW2_CS4,   SW2_CS5,   SW2_CS6},   //1
+    {0, SW3_CS4,   SW3_CS5,   SW3_CS6},   //2
+    {0, SW4_CS4,   SW4_CS5,   SW4_CS6},
+    {0, SW5_CS4,   SW5_CS5,   SW5_CS6},   //4
+    {0, SW6_CS4,   SW6_CS5,   SW6_CS6},
+    {0, SW7_CS4,   SW7_CS5,   SW7_CS6},   //6
+    {0, SW8_CS4,   SW8_CS5,   SW8_CS6},
+    {0, SW9_CS4,   SW9_CS5,   SW9_CS6},   //8
+    {0, SW10_CS4,  SW10_CS5,  SW10_CS6},
+    {0, SW11_CS4,  SW11_CS5,  SW11_CS6},  //0
+    {0, SW12_CS4,  SW12_CS5,  SW12_CS6},  //-_
+    {0, SW2_CS13,  CS14_SW2,  CS15_SW2},  //=+
+    {0, SW3_CS13,  SW3_CS14,  SW3_CS15},  //BS
+    {0, SW4_CS13,  SW4_CS14,  SW4_CS15},  //INS
+    {0, SW5_CS13,  SW5_CS14,  SW5_CS15},  //Home
+    {0, SW6_CS13,  SW6_CS14,  SW6_CS15},  //PgUp
+
+    {0, SW1_CS7,   SW1_CS8,   SW1_CS9},   //Tab
+    {0, SW2_CS7,   SW2_CS8,   SW2_CS9},   //Q
+    {0, SW3_CS7,   SW3_CS8,   SW3_CS9},   //W
+    {0, SW4_CS7,   SW4_CS8,   SW4_CS9},   //E
+    {0, SW5_CS7,   SW5_CS8,   SW5_CS9},   //R
+    {0, SW6_CS7,   SW6_CS8,   SW6_CS9},   //T
+    {0, SW7_CS7,   SW7_CS8,   SW7_CS9},   //Y
+    {0, SW8_CS7,   SW8_CS8,   SW8_CS9},   //U
+    {0, SW9_CS7,   SW9_CS8,   SW9_CS9},   //I
+    {0, SW10_CS7,  SW10_CS8,  SW10_CS9},   //O
+    {0, SW11_CS7,  SW11_CS8,  SW11_CS9},   //P
+    {0, SW12_CS7,  SW12_CS8,  SW12_CS9},   //[
+    {0, SW2_CS16,  SW2_CS17,  SW2_CS18},   //]
+    {0, SW3_CS16,  SW3_CS17,  SW3_CS18},   //|
+    {0, SW4_CS16,  SW4_CS17,  SW4_CS18},   //DEL
+    {0, SW5_CS16,  SW5_CS17,  SW5_CS18},   //END
+    {0, SW6_CS16,  SW6_CS17,  SW6_CS18},   //PgDn
+
+    {1, SW1_CS1,   SW1_CS2,   SW1_CS3},   //Cap
+    {1, SW2_CS1,   SW2_CS2,   SW2_CS3},   //A
+    {1, SW3_CS1,   SW3_CS2,   SW3_CS3},   //S
+    {1, SW4_CS1,   SW4_CS2,   SW4_CS3},   //D
+    {1, SW5_CS1,   SW5_CS2,   SW5_CS3},   //F
+    {1, SW6_CS1,   SW6_CS2,   SW6_CS3},   //G
+    {1, SW7_CS1,   SW7_CS2,   SW7_CS3},   //H
+    {1, SW8_CS1,   SW8_CS2,   SW8_CS3},   //J
+    {1, SW9_CS1,   SW9_CS2,   SW9_CS3},   //K
+    {1, SW10_CS1,  SW10_CS2,  SW10_CS3},   //L
+    {1, SW11_CS1,  SW11_CS2,  SW11_CS3},   //;
+    {1, SW12_CS1,  SW12_CS2,  SW12_CS3},   //.
+    {1, SW2_CS10,  SW2_CS11,  SW2_CS12},   //Ent
+
+    {1, SW1_CS4,   SW1_CS5,   SW1_CS6},   //Shift
+    {1, SW2_CS4,   SW2_CS5,   SW2_CS6},   //Z
+    {1, SW3_CS4,   SW3_CS5,   SW3_CS6},   //X
+    {1, SW4_CS4,   SW4_CS5,   SW4_CS6},   //C
+    {1, SW5_CS4,   SW5_CS5,   SW5_CS6},   //V
+    {1, SW6_CS4,   SW6_CS5,   SW6_CS6},   //B
+    {1, SW7_CS4,   SW7_CS5,   SW7_CS6},   //N
+    {1, SW8_CS4,   SW8_CS5,   SW8_CS6},   //M
+    {1, SW9_CS4,   SW9_CS5,   SW9_CS6},   //,
+    {1, SW10_CS4,  SW10_CS5,  SW10_CS6},   //.
+    {1, SW11_CS4,  SW11_CS5,  SW11_CS6},   //?
+    {1, SW3_CS10,  SW3_CS11,  SW3_CS12},   //Shift
+    {1, SW4_CS13,  SW4_CS14,  SW4_CS15},   //Up
+
+    {1, SW1_CS7,   SW1_CS8,   SW1_CS9},   //Ctrl
+    {1, SW2_CS7,   SW2_CS8,   SW2_CS9},   //Com
+    {1, SW3_CS7,   SW3_CS8,   SW3_CS9},   //Alt
+    {1, SW4_CS7,   SW4_CS8,   SW4_CS9},   //SPAC1
+    {1, SW6_CS7,   SW6_CS8,   SW6_CS9},   //SPAC
+    {1, SW8_CS7,   SW8_CS8,   SW8_CS9},   //SPAC2
+    {1, SW10_CS7,  SW10_CS8,  SW10_CS9},   //Alt
+    {1, SW11_CS7,  SW11_CS8,  SW11_CS9},   //Fn
+    {1, SW12_CS7,  SW12_CS8,  SW12_CS9},   //Menu
+    {1, SW2_CS16,  SW2_CS17,  SW2_CS18},   //Ctrl
+    {1, SW3_CS16,  SW3_CS17,  SW3_CS18},   //Left
+    {1, SW4_CS16,  SW4_CS17,  SW4_CS18},   //Down
+    {1, SW5_CS16,  SW5_CS17,  SW5_CS18},   //Right
+};
+#endif
+
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_user(keycode, record)) {
+        return false;
+    }
+    switch (keycode) {
+#  ifdef RGB_MATRIX_ENABLE
+    case RGB_TOG:
+         if (record->event.pressed) {
+            switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        rgb_matrix_set_flags(LED_FLAG_NONE);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                    } break;
+                    default: {
+                        rgb_matrix_set_flags(LED_FLAG_ALL);
+                    } break;
+            }
+        }
+        return false;
+    case RGB_VAI:
+       rgb_matrix_set_flags(LED_FLAG_ALL);
+       return true;
+#    endif
+
+    case MO(1):
+      if (record->event.pressed) {
+       FN_ON = 1;
+      } else {
+       FN_ON = 0;
+      }
+      return true;
+
+    case MO(2):
+      if (record->event.pressed) {
+       FN_ON = 1;
+      } else {
+       FN_ON = 0;
+      }
+      return true;
+
+    case MO(3):
+      if (record->event.pressed) {
+       FN_ON = 1;
+      } else {
+       FN_ON = 0;
+      }
+      return true;
+
+    case KC_LGUI:
+      if (FN_ON){
+          if ( record->event.pressed){
+             WIN_LOCK = !WIN_LOCK ; //change win lock state
+            }
+          if (!WIN_LOCK) {
+             IND = IND & (~WINLK_ON);   //Close win lock display
+             s_serial_to_parallel(IND);
+             writePinHigh(WIN_LOCK_PIN);
+             return false; //windows key locked do nothing
+            }
+          s_serial_to_parallel(IND);
+        }
+      if (WIN_LOCK) {
+             IND = IND | WINLK_ON;   //Open win lock display
+             s_serial_to_parallel(IND);
+             writePinLow(WIN_LOCK_PIN);
+             return false; //windows key locked do nothing
+            }
+      return true;  // continue all further processing of this key
+
+    case TO(0):
+       set_single_persistent_default_layer(0);
+       return true;
+
+    case TO(1):
+       set_single_persistent_default_layer(1);
+       return true;
+
+    default:
+      return true;
+    }
+   s_serial_to_parallel(IND);
+}
+
+void suspend_power_down_kb() {
+    writePinHigh(WIN_LOCK_PIN);
+    s_serial_to_parallel(0);
+    suspend_power_down_user();
+}
+
+void suspend_wakeup_init_kb() {
+    s_serial_to_parallel(IND);
+    suspend_wakeup_init_user();
+}
+
+bool shutdown_kb(bool jump_to_bootloader) {
+    writePinHigh(WIN_LOCK_PIN);
+    s_serial_to_parallel(0);
+    return true;
+}
+
+layer_state_t default_layer_state_set_kb(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case 0:
+        //switch to win layer display
+        IND = IND & (~MAC_ON);
+        IND = IND | WIN_ON;
+        break;
+    case 1:
+        //switch to mac layer display
+        IND = IND & (~WIN_ON);
+        IND = IND | MAC_ON;
+        break;
+    }
+    s_serial_to_parallel(IND);
+  return state;
+}
+
+
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+        return false;
+    }
+
+    //caps lock display
+    if (host_keyboard_led_state().caps_lock) {
+       // RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_INDEX, 255, 255, 255);
+        IND = IND | CAPS_ON;
+    } else {
+        /*
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_INDEX, 0, 0, 0);
+        }
+        */
+        IND = IND & (~CAPS_ON);
+    }
+    //number lock display
+    if (host_keyboard_led_state().num_lock) {
+       // RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_INDEX, 255, 255, 255);
+        IND = IND | NUM_ON;
+    } else {
+        /*
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_INDEX, 0, 0, 0);
+        }
+        */
+        IND = IND & (~NUM_ON);
+    }
+    //scroll lock display
+    if (host_keyboard_led_state().scroll_lock) {
+       // RGB_MATRIX_INDICATOR_SET_COLOR(SCR_LOCK_INDEX, 255, 255, 255);
+        IND = IND | SCR_ON;
+    } else {
+        /*
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(SCR_LOCK_INDEX, 0, 0, 0);
+        }
+        */
+        IND = IND & (~SCR_ON);
+    }
+
+    s_serial_to_parallel(IND);
+    return true;
+}
+
+
+void board_init(void) {
+    // JTAG-DP Disabled and SW-DP Disabled
+    AFIO->MAPR = (AFIO->MAPR & ~AFIO_MAPR_SWJ_CFG_Msk) | AFIO_MAPR_SWJ_CFG_DISABLE;
+    setPinOutput(WIN_LOCK_PIN);
+    writePinHigh(WIN_LOCK_PIN);
+    s_serial_to_parallel(0xFF);
+    IND = SKYLOONG;
+}
+
