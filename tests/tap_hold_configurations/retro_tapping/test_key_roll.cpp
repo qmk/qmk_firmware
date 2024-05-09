90826d88b3f8ec42bbbabcfe62717292cef7ac0c
john <JohnLRigoni@gmail.com>
2024-05-09 03:34:25 -0500

    fix copyright

--- tests/tap_hold_configurations/retro_tapping/test_key_roll.cpp
+++ tests/tap_hold_configurations/retro_tapping/test_key_roll.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2023 Vladislav Kucheriavykh
+/* Copyright 2024 John Rigoni
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
