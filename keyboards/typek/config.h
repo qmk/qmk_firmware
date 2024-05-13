2934573eb1e360253ef8bf69acb62c6ae2d9c08c
Álvaro A. Volpato <alvaro.augusto.volpato@gmail.com>
2024-05-12 23:17:04 -0300

    Update keyboards/typek/config.h
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/typek/config.h
+++ keyboards/typek/config.h
@@ -15,6 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#pragma once
+
 #define WS2812_PWM_COMPLEMENTARY_OUTPUT
 #define WS2812_PWM_DRIVER PWMD1
 #define WS2812_PWM_CHANNEL 3
