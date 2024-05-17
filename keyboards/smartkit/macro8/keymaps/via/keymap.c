81564e3c6532f9ad15877e9d3dadc8c0cb3f6581
Mark <58395926+M4rkoV2@users.noreply.github.com>
2024-05-17 12:13:25 +0200

    smartkit keyboards

---
+++ keyboards/smartkit/macro8/keymaps/via/keymap.c
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+		KC_0, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8),
+
+	  [1] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+    [2] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+    
+};
+
+
+
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+  [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+  [1] = { ENCODER_CCW_CW(_______, _______) },
+  [2] = { ENCODER_CCW_CW(_______, _______) }
+};
+#endif
+
