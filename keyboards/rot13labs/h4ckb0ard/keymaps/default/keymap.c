30a5aa077d88495c30d58895c0399a9a267aa186
c0ldbru <dennisp98@gmail.com>
2024-05-18 13:10:17 -0400

    Update keyboards/rot13labs/h4ckb0ard/keymaps/default/keymap.c
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/rot13labs/h4ckb0ard/keymaps/default/keymap.c
+++ keyboards/rot13labs/h4ckb0ard/keymaps/default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,     KC_9,     KC_0,    KC_DEL,
       KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,     KC_L,              KC_ENT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,   KC_RCTL,            KC_UP,   DF(3),
-      KC_LCTL, KC_LGUI, KC_LALT,          MO(3),          KC_SPC,         KC_RALT,  KC_LEFT,  KC_DOWN, KC_RIGHT),
+      KC_LCTL, KC_LGUI, _______,          MO(3),          KC_SPC,         KC_RALT,  KC_LEFT,  KC_DOWN, KC_RIGHT),
 
   /* SPC - RGB control layer */
   [3] = LAYOUT(
