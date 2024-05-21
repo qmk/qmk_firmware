43d1f5ed22ae881bd7ab61d094b5f932e618567a
Sinisha <stojchevski0@gmail.com>
2024-05-21 09:37:06 +0200

    Update keyboards/phnx/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/phnx/keymaps/default/keymap.c
+++ keyboards/phnx/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                 │   │   │   │     │   │   │   │
      *                 └───┴───┴───┘     └───┴───┴───┘
      */
-    [0] = LAYOUT_qwerty( // base layer
+    [0] = LAYOUT( // base layer
          KC_TILDE,      KC_1,   KC_2,   KC_3,   KC_4,   KC_5,         KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_PSCR,
         KC_LPRN,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,         KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_RPRN,
         KC_LT,  LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,         KC_H,   RSFT_T(KC_J),   RCTL_T(KC_K),   ALGR_T(KC_L),   RGUI_T(KC_SEMICOLON),     KC_GT,
