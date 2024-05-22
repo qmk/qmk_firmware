17b37cd5dd5d865cc0428bca181acfe53d56f7b3
fauxpark <fauxpark@gmail.com>
2024-05-22 19:27:45 +1000

    Some more that were missed

--- keyboards/reedskeebs/alish40/keymaps/via/keymap.c
+++ keyboards/reedskeebs/alish40/keymaps/via/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] =  { ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  },
+    [1] =  { ENCODER_CCW_CW(UG_SATD, UG_SATU)  },
     [2] =  { ENCODER_CCW_CW(UG_SPDD, UG_SPDU)  },
     [3] =  { ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) }
 };
