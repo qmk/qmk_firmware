e659c3dae9c50293a1483190db07601a83b73fef
Ryan <fauxpark@gmail.com>
2024-05-24 04:21:39 +1000

    Remove some useless code from keymaps (#23787)

--- keyboards/hs60/v2/iso/keymaps/win_osx_dual/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/win_osx_dual/keymap.c
@@ -69,16 +69,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-void matrix_init_user(void) {
-  //user initialization
-}
-
-
-void matrix_scan_user(void) {
-  //user matrix
-}
-
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
