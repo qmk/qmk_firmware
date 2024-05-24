eee2ba98628e47393d6b40312d1117304800a217
Sinisha <stojchevski0@gmail.com>
2024-05-24 18:37:11 +0200

    changed to KC_TRNS instead of KC_NO on each active layer
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/phnx/keymaps/default/keymap.c
+++ keyboards/phnx/keymaps/default/keymap.c
@@ -29,41 +29,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_AGIN,   KC_PASTE,   KC_COPY,   KC_CUT,   KC_UNDO,             KC_NO,
         KC_NO,  KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   KC_NO,         KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,   KC_RIGHT,     KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_INS,   KC_HOME,   KC_PAGE_DOWN,   KC_PAGE_UP,   KC_END,    KC_NO,
-                                   KC_NO, KC_NO, KC_NO,         KC_ENTER, KC_BACKSPACE, KC_DELETE
+                                   KC_NO, KC_TRNS, KC_NO,         KC_ENTER, KC_BACKSPACE, KC_DELETE
     ),
     [2] = LAYOUT( // function layer
         QK_BOOT,      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,             KC_NO,
         KC_NO,     KC_F12,   KC_F7,   KC_F8,   KC_F9,   KC_PSCR,         KC_NO,   KC_NO,   KC_NO,   KC_F11,   KC_F12,             KC_NO,
         KC_NO,     KC_F11,   KC_F4,   KC_F5,   KC_F6,   KC_SCRL,         KC_NO,   KC_RSFT,   KC_RCTL,   KC_RALT,   KC_RGUI,     KC_NO,
         KC_NO,     KC_F10,   KC_F1,   KC_F2,   KC_F3,   KC_PAUS,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
-                                         KC_APP, KC_SPC, KC_TAB,         KC_NO, KC_NO, KC_NO
+                                         KC_APP, KC_SPC, KC_TAB,         KC_NO, KC_NO, KC_TRNS
     ),
     [3] = LAYOUT( // number layer
         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,     KC_LBRC,   KC_7,   KC_8,   KC_9,   KC_RCBR,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,     KC_SCLN,   KC_4,   KC_5,   KC_6,   KC_EQL,         KC_NO,   KC_RSFT,   KC_RCTL,   KC_RALT,   KC_RGUI,     KC_NO,
         KC_NO,     KC_COMM,   KC_1,   KC_2,   KC_3,   KC_BSLS,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
-                                         KC_DOT, KC_0, KC_MINUS,         KC_NO, KC_NO, KC_NO
+                                         KC_DOT, KC_0, KC_MINUS,         KC_NO, KC_TRNS, KC_NO
     ),
     [4] = LAYOUT( // symbol layer
         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,     KC_LCBR,   KC_AMPR,   KC_ASTR,   KC_LPRN,   KC_RCBR,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,     KC_COLN,   KC_DLR,   KC_PERC,   KC_CIRC,   KC_PLUS,         KC_NO,   KC_RSFT,   KC_RCTL,   KC_RALT,   KC_RGUI,     KC_NO,
         KC_NO,     KC_TILD,   KC_EXLM,   KC_AT,   KC_HASH,   KC_PIPE,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
-                                         KC_LPRN, KC_RPRN, KC_UNDS,         KC_NO, KC_NO, KC_NO
+                                         KC_LPRN, KC_RPRN, KC_UNDS,         KC_TRNS, KC_NO, KC_NO
     ),
     [5] = LAYOUT( // media layer
         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         RGB_TOG,   RGB_MOD,   RGB_HUI,   RGB_SAI,   RGB_VAI,             KC_NO,
         KC_NO,     KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   KC_NO,         KC_NO,   KC_MPRV,   KC_VOLD,   KC_VOLU,   KC_MNXT,     KC_NO,
         KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
-                                         KC_NO, KC_NO, KC_NO,         KC_MSTP, KC_MPLY, KC_MUTE
+                                         KC_TRNS, KC_NO, KC_NO,         KC_MSTP, KC_MPLY, KC_MUTE
     ),
     [6] = LAYOUT( // mouse layer
         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_AGIN,   KC_PASTE,   KC_COPY,   KC_CUT,   KC_UNDO,             KC_NO,
         KC_NO,  KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   KC_NO,         KC_NO,   KC_MS_L,   KC_MS_D,   KC_MS_U,   KC_MS_R,     KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_WH_L,   KC_WH_D,   KC_WH_U,   KC_WH_R,    KC_NO,
-                                   KC_NO, KC_NO, KC_NO,         KC_BTN2, KC_BTN1, KC_BTN3
+                                   KC_NO, KC_NO, KC_TRNS,         KC_BTN2, KC_BTN1, KC_BTN3
     )
 };
