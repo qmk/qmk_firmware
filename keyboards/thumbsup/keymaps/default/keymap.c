4bb076e3b7c4004ef0b3df3c9deda0fb289b1c8c
ak66666 <32971450+ak66666@users.noreply.github.com>
2024-05-28 15:25:18 -0400

    Update keyboards/thumbsup/keymaps/default/keymap.c
    As per PR review suggestions.
    
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/thumbsup/keymaps/default/keymap.c
+++ keyboards/thumbsup/keymaps/default/keymap.c
@@ -53,11 +53,11 @@ Single hit:  |		       |      |Space |		|Space |      |                 |
 	     +------------------------------> SPACE SPACE <-----------------------------+
 
  */
-[_QWERTY] = LAYOUT_ortho_4x12( \
-  KC_ESC,                  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,  \
-  LT(_EXTRARIGHT,KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_PENT , \
-  KC_LCTL,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,  \
-                  	   KC_SPC,  KC_LGUI, KC_LALT, KC_LSFT, CURSORLEFT, CURSORRGHT, KC_RSFT, KC_RALT, KC_APP, KC_SPC             \
+[_QWERTY] = LAYOUT_ortho_4x12(
+  KC_ESC,                  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+  LT(_EXTRARIGHT,KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_PENT,
+  KC_LCTL,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
+                  	   KC_SPC,  KC_LGUI, KC_LALT, KC_LSFT, CURSORLEFT, CURSORRGHT, KC_RSFT, KC_RALT, KC_APP, KC_SPC
 ),
 
 
