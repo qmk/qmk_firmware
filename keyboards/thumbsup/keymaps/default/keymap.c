d525cdf067d3f5188c5492d233e3d863d15a88e2
ak66666 <32971450+ak66666@users.noreply.github.com>
2024-05-28 15:25:47 -0400

    Update keyboards/thumbsup/keymaps/default/keymap.c
    As per PR review suggestions.
    
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/thumbsup/keymaps/default/keymap.c
+++ keyboards/thumbsup/keymaps/default/keymap.c
@@ -127,12 +127,11 @@ without separate thumbcluster.
  *               |      |      |      |   X  |       |      |      |      |
  *               `-------------------------------------------------------'
  */
-[_CURSORLEFT] = LAYOUT_ortho_4x12( \
-  _______, KC_BRK,          KC_HOME, KC_UP,   KC_END,  KC_PGUP,       KC_DEL,  KC_WH_U,      KC_MS_U,      KC_WH_D,      KC_GRV,  KC_DEL , \
-  KC_ENT,  LCTL(KC_A),      KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,       XXXXXXX, KC_MS_L,      KC_MS_D,      KC_MS_R,      XXXXXXX, _______, \
-  _______, XXXXXXX,         KC_BTN2, KC_BTN3, KC_BTN1, KC_INS,        XXXXXXX, LCTL(KC_DEL), LCTL(KC_INS), LSFT(KC_INS), XXXXXXX, _______, \
-	   _______,	    _______, _______, _______, _______,       _______, _______, _______, _______, _______           \
-
+[_CURSORLEFT] = LAYOUT_ortho_4x12(
+  _______, KC_BRK,          KC_HOME, KC_UP,   KC_END,  KC_PGUP,       KC_DEL,  KC_WH_U,      KC_MS_U,      KC_WH_D,      KC_GRV,  KC_DEL,
+  KC_ENT,  LCTL(KC_A),      KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,       XXXXXXX, KC_MS_L,      KC_MS_D,      KC_MS_R,      XXXXXXX, _______,
+  _______, XXXXXXX,         KC_BTN2, KC_BTN3, KC_BTN1, KC_INS,        XXXXXXX, LCTL(KC_DEL), LCTL(KC_INS), LSFT(KC_INS), XXXXXXX, _______,
+	   _______,	    _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 
 
