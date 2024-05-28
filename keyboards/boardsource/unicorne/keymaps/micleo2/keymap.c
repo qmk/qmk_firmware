ff33ee75d56d3586d806e918838c1db2cc19375a
mike <programmermike1@gmail.com>
2024-05-28 00:31:21 -0700

    add new layer

--- keyboards/boardsource/unicorne/keymaps/micleo2/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/micleo2/keymap.c
@@ -10,38 +10,60 @@
 // mk2kWjrf<t_ý>a`kWkvehyjPwkr <t_ý>aj
 
 enum layers {
-  _QWERTY,
+  _BASE,
   _SYM,
-  _NAVSYS,
+  _NAV,
+  _SYS,
   _NUM,
 };
 
+#define Q _BASE
+#define M _SYM
+#define N _NAV
+#define Y _SYS
+#define U _NUM
+
+// This denotes the key you used to enter into the layer.
+// E for entry.
+#define ___E___ _______
+// Take a full screenshot on mac
+#define FSCRNSHT G(S(3))
+// Take a partial screenshot on mac
+#define SCRNSHT G(S(4))
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_QWERTY] = LAYOUT_split_3x6_3(
-    CW_TOGG,       KC_Q,          KC_W,         KC_E,          KC_R,          KC_T,                 KC_Y,          KC_U,          KC_I,         KC_O,          KC_P,          KC_DEL,
-    KC_ENT,        KC_A,          LT(3, KC_S),  LALT_T(KC_D),  LT(2, KC_F),   KC_G,                 KC_H,          KC_J,          KC_K,         KC_L,          KC_SCLN,       KC_BTN1,
+[_BASE] = LAYOUT_split_3x6_3(
+    OSM(MOD_LSFT), KC_Q,          KC_W,         KC_E,          KC_R,          KC_T,                 KC_Y,          KC_U,          KC_I,         KC_O,          KC_P,          KC_DEL,
+    KC_ENT,        KC_A,          LT(U, KC_S),  LALT_T(KC_D),  LT(N, KC_F),   KC_G,                 KC_H,          KC_J,          KC_K,         KC_L,          KC_SCLN,       OSL(Y),
     QK_REP,        KC_Z,          KC_X,         KC_C,          KC_V,          KC_B,                 KC_N,          KC_M,          KC_COMM,      KC_DOT,        KC_COLN,       C(G(KC_Q)),
-                                                CTL_T(KC_ESC), GUI_T(KC_SPC), SFT_T(KC_TAB),        TG(2),         SFT_T(KC_BSPC),OSL(1)
+                                                CTL_T(KC_ESC), GUI_T(KC_SPC), SFT_T(KC_TAB),        KC_ENT,        SFT_T(KC_BSPC),OSL(M)
 ),
 
 [_SYM] = LAYOUT_split_3x6_3(
     _______,       _______,       KC_LT,        KC_GT,         KC_ASTR,       KC_GRV,               _______,       KC_AMPR,       KC_LCBR,      KC_RCBR,       KC_PERC,       _______,
     _______,       KC_QUES,       KC_MINS,      KC_DQUO,       KC_QUOT,       KC_TILD,              KC_BSLS,       KC_PLUS,       KC_LPRN,      KC_RPRN,       KC_UNDS,       _______,
     _______,       KC_HASH,       _______,      KC_EQL,        KC_EXLM,       KC_CIRC,              KC_DLR,        KC_PIPE,       KC_LBRC,      KC_RBRC,       KC_AT,         _______,
-                                                KC_SLSH,       _______,       _______,              _______,       _______,       _______
+                                                KC_SLSH,       _______,       _______,              _______,       _______,       ___E___
 ),
 
-[_NAVSYS] = LAYOUT_split_3x6_3(
-    QK_BOOT,       _______,       _______,      _______,       _______,       _______,              _______,       C(KC_TAB),     KC_TAB,       LSFT(KC_TAB),  C(S(KC_TAB)),  _______,
-    _______,       KC_MUTE,       KC_VOLD,      KC_VOLU,       _______,       _______,              KC_LEFT,       KC_DOWN,       KC_UP,        KC_RGHT,       _______,       _______,
-    _______,       _______,       KC_MPRV,      KC_MNXT,       _______,       _______,              _______,       _______,       C(KC_LEFT),   C(KC_RGHT),    _______,       _______,
+[_NAV] = LAYOUT_split_3x6_3(
+    _______,       _______,       _______,      _______,       _______,       _______,              _______,       C(KC_TAB),     KC_TAB,       LSFT(KC_TAB),  C(S(KC_TAB)),  _______,
+    _______,       _______,       _______,      _______,       ___E___,       _______,              KC_LEFT,       KC_DOWN,       KC_UP,        KC_RGHT,       _______,       _______,
+    _______,       _______,       _______,      _______,       _______,       _______,              _______,       _______,       C(KC_LEFT),   C(KC_RGHT),    _______,       _______,
                                                 _______,       _______,       _______,              _______,       KC_ENT,        _______
+),
+
+[_SYS] = LAYOUT_split_3x6_3(
+    CW_TOGG,       _______,       FSCRNSHT,     SCRNSHT,       _______,       _______,              _______,       KC_F7,         KC_F8,        KC_F9,         KC_F12,        _______,
+    _______,       KC_MS_BTN1,    KC_MPLY,      KC_VOLU,       KC_VOLD,       _______,              _______,       KC_F4,         KC_F5,        KC_F6,         KC_F11,        ___E___,
+    QK_BOOT,       _______,       KC_MNXT,      KC_MPRV,       KC_MUTE,       _______,              _______,       KC_F1,         KC_F2,        KC_F3,         KC_F10,        QK_BOOT,
+                                                _______,       _______,       _______,              _______,       _______,       _______
 ),
 
 [_NUM] = LAYOUT_split_3x6_3(
     _______,       _______,       _______,      _______,       _______,       _______,              _______,       KC_7,          KC_8,         KC_9,          _______,       _______,
-    _______,       _______,       _______,      _______,       _______,       _______,              _______,       KC_4,          KC_5,         KC_6,          _______,       _______,
+    _______,       _______,       ___E___,      _______,       _______,       _______,              _______,       KC_4,          KC_5,         KC_6,          _______,       _______,
     _______,       _______,       _______,      _______,       _______,       _______,              _______,       KC_1,          KC_2,         KC_3,          _______,       _______,
                                                 _______,       _______,       _______,              _______,       _______,       KC_0
 )
@@ -98,13 +120,16 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                 continue;
             }
             switch (layer) {
-                case _QWERTY:
+                case _BASE:
                     rgb_matrix_set_color(index, BASE_COL);
                     break;
                 case _SYM:
                     rgb_matrix_set_color(index, RGB_PURPLE);
                     break;
-                case _NAVSYS:
+                case _NAV:
+                    rgb_matrix_set_color(index, RGB_BLUE);
+                    break;
+                case _SYS:
                     rgb_matrix_set_color(index, RGB_RED);
                     break;
                 case _NUM:
@@ -129,7 +154,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     const uint8_t cur_layer = get_highest_layer(layer_state);
-    if (cur_layer == _NAVSYS) {
+    if (cur_layer == _NAV) {
         // Convert mouse movement to scrolling on nav layer.
         mouse_report.h = mouse_report.x;
         mouse_report.v = -mouse_report.y / 4;
@@ -190,7 +215,7 @@ bool shutdown_user(bool jump_to_bootloader) {
 uint32_t frame_toggle_timer = 0;
 uint8_t  render_f1          = 0;
 // Delay to wait after kb inactivity to begin taunt animation.
-const uint32_t TAUNT_WAIT_MS = 30 * 1000;
+const uint32_t TAUNT_WAIT_MS = 50 * 1000;
 // Length of the taunt animation.
 const uint32_t TAUNT_LEN_MS = 60 * 4 * 1000;
 // Length of a single frame of the taunt animation.
@@ -206,21 +231,27 @@ bool oled_task_user() {
 
     const uint8_t  cur_layer    = get_highest_layer(layer_state);
     const uint32_t idle_time_ms = last_matrix_activity_elapsed();
-    if (idle_time_ms > TAUNT_WAIT_MS && cur_layer == _QWERTY) {
+    if (idle_time_ms > TAUNT_WAIT_MS && cur_layer == _BASE) {
         render_downtaunt(idle_time_ms);
     } else {
         switch (cur_layer) {
-            case _QWERTY:
+            case _BASE:
                 oled_write_raw(gw_idle, sizeof(gw_idle));
                 break;
             case _SYM:
                 oled_write_raw(gw_diver, sizeof(gw_diver));
                 break;
-            case _NAVSYS:
+            case _NAV:
                 oled_write_raw(gw_parachute, sizeof(gw_parachute));
+                break;
+            case _SYS:
+                oled_write_raw(gw_bell, sizeof(gw_bell));
                 break;
             case _NUM:
                 oled_write_raw(gw_key, sizeof(gw_key));
+                break;
+            default:
+                oled_write_raw(gw_idle, sizeof(gw_idle));
                 break;
         }
         should_sleep = false;
