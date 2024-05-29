bfb977fb0b6b697db23ec71c4a875a1721b86099
Cian Hughes <chughes000@gmail.com>
2024-05-29 16:33:04 +0100

    Added tap dancing improvements
    Added tap dancing. Used it to put shift and
    capslock on a single key and add a tap danced parens key

--- keyboards/splitkb/kyria/keymaps/my_kyria_layout/keymap.c
+++ keyboards/splitkb/kyria/keymaps/my_kyria_layout/keymap.c
@@ -39,8 +39,24 @@ enum layers {
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
 // produces the key `tap` when tapped (i.e. pressed and released).
 
-// clang-format off
+// Modified from: https://docs.qmk.fm/#/feature_tap_dance?id=simple-example
+// Tap Dance declarations
+enum {
+    TD_SFT_CPS,
+    TD_PARENS,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Press once for shift, twice for Caps Lock
+    [TD_SFT_CPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    // Press once for (, twice for )
+    [TD_PARENS] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+};
+
+// Add tap dance item to your keymap in place of a keycode
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 /*
  * Base Layer: QWERTY
  *
@@ -49,17 +65,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  | Tab  |  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? |  Bksp  |
+ * |Sft/Cps |   Z  |   X  |   C  |   V  |   B  | [ {  | Tab  |  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? |  Bksp  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |CapsLk| LGUI | LAlt/| Space| Nav  |  | Sym  | Space|  LCtl| LAlt |  \ | |
+ *                        |  ()  | LGUI | LAlt/| Space| Nav  |  | Sym  | Space|  LCtl| LAlt |  \ | |
  *                        |      |      | Enter|      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_TAB,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSPC,
-                                KC_CAPS, KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_LCTL, KC_LALT, KC_BSLS
+      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
+      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
+TD(TD_SFT_CPS), KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_TAB,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSPC,
+                           TD(TD_PARENS), KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_LCTL, KC_LALT, KC_BSLS
     ),
 
 /*
--- keyboards/splitkb/kyria/rev3/rules.mk
+++ keyboards/splitkb/kyria/rev3/rules.mk
@@ -1,2 +1,3 @@
 # Although no rules are defined,
 # presence of this file is required for QMK to compile it.
+TAP_DANCE_ENABLE = yes
