aad0ff6860cd3cd2145b2b15085242b488abf00d
sje-mse <sje.txt@gmail.com>
2024-05-30 08:31:19 -0400

    standardize comments

--- keyboards/ibm/model_m/yacobo/yacobo.c
+++ keyboards/ibm/model_m/yacobo/yacobo.c
@@ -1,7 +1,5 @@
 /* Copyright 2024 Stephen Edwards
  *
- * Adapted from the Model H Project by Jonathan Hawthorn.
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -14,6 +12,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Adapted from the Model H Project by Jonathan Hawthorn.
  */
 
 #include "quantum.h"
