4ec8592f96f8caaf3f497a2b186a857f9242de95
ak66666 <32971450+ak66666@users.noreply.github.com>
2024-05-29 22:25:07 -0400

    v9: Fixed the tri-layer switch as per the review recommendation.

--- keyboards/thumbsup/keymaps/default/keymap.c
+++ keyboards/thumbsup/keymaps/default/keymap.c
@@ -55,7 +55,7 @@ Single hit:  |		       |      |Space |		|Space |      |                 |
   KC_ESC,                  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
   LT(_EXTRARIGHT,KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_PENT,
   KC_LCTL,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
-                  	   KC_SPC,  KC_LGUI, KC_LALT, KC_LSFT, LT(_CURSORLEFT,KC_SPC), LT(_CURSORRGHT,KC_SPC), KC_RSFT, KC_RALT, KC_APP, KC_SPC
+                  	   KC_SPC,  KC_LGUI, KC_LALT, KC_LSFT, LT(TL_LOWR,KC_SPC), LT(TL_UPPR ,KC_SPC), KC_RSFT, KC_RALT, KC_APP, KC_SPC
 ),
 
 
@@ -173,7 +173,3 @@ without separate thumbcluster.
 )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _CURSORLEFT, _CURSORRGHT, _NUMBERS);
-}
-                                                                            
