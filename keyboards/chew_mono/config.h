23d35967dd4ea6c274c7441f4bad36721bce5c17
flo <f@linguenheld.fr>
2024-05-31 17:38:38 +0200

    Add config reset

--- keyboards/chew_mono/config.h
+++ keyboards/chew_mono/config.h
@@ -1,20 +1,8 @@
-// Copyright 2024 Florent Linguenheld (@flinguenheld)
+// Copyright 2024 Florent (@FLinguenheld)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+/* Flash */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET              // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // In ms in which the double tap can occur
