94a6bec9d284c39f19db3380c659f096420041b3
Lynton Ivins <lynton.ivins@proton.me>
2024-06-01 15:39:21 +1200

    edit

--- keyboards/acmac/keymaps/default/keymap.c
+++ keyboards/acmac/keymaps/default/keymap.c
@@ -7,7 +7,7 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-
+// entirely and just use numbers.
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
