e5dc1baa7559cf6a141cb9ea46be647d98c47358
Norpix <dudek.g@outlook.com>
2024-06-01 15:19:35 +0200

    Added new keyboard model [Skyloong GK61 pro_ds] with separate spacebar

---
+++ keyboards/skyloong/gk61/pro_ds/config.h
@@ -0,0 +1,13 @@
+// Copyright 2023 linlin012 (@linlin012)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define CAPS_LOCK_INDEX 28
+#define WIN_MOD_INDEX 16
+#define MAC_MOD_INDEX 17
+#define AW20216S_SDB_PIN A4
+#define TAPPING_TERM_PER_KEY
+
+#define AW20216S_CS_PIN_1 B12
+#define AW20216S_EN_PIN_1 A4
