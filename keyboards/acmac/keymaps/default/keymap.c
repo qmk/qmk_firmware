15c00fff7650b822748e0b6dea4bc4028179e99c
Lynton Ivins <lynton.ivins@proton.me>
2024-06-01 15:54:23 +1200

    EDIT

--- keyboards/acmac/keymaps/default/keymap.c
+++ keyboards/acmac/keymaps/default/keymap.c
@@ -27,12 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘└───┴───┴───┴───┘
      */
     [0] = LAYOUT(
-         KC_ESC,  KC_A, KC_B, KC_C,   KC_V,    KC_W,     KC_X,      KC_Y,
-         KC_DEL,  KC_D, KC_E, KC_F,   KC_Z,   KC_P0,  KC_PAST,   KC_PSLS,
-         KC_G,   KC_H, KC_I, KC_J,   KC_P7,   KC_P8,    KC_P9,    KC_PMNS,
-         KC_K,   KC_L, KC_M, KC_N,   KC_P4,   KC_P5,    KC_P6,    KC_PPLS,
-         KC_O,   KC_P, KC_Q, KC_R,   KC_P1,   KC_P2,    KC_P3,
-         MO(1),  KC_S, KC_T, KC_U,   KC_P0,   KC_PDOT, KC_COMM,   KC_PENT
+         KC_ESC, KC_A, KC_B, KC_C, KC_V, KC_W, KC_X, KC_Y,
+         KC_DEL, KC_D, KC_E, KC_F, KC_Z, KC_P0, KC_PAST, KC_PSLS,
+         KC_G, KC_H, KC_I, KC_J, KC_P7, KC_P8, KC_P9, KC_PMNS,
+         KC_K, KC_L, KC_M, KC_N, KC_P4, KC_P5, KC_P6, KC_PPLS,
+         KC_O, KC_P, KC_Q, KC_R, KC_P1, KC_P2, KC_P3,
+         MO(1), KC_S, KC_T, KC_U, KC_P0, KC_PDOT, KC_COMM, KC_PENT
     ),
 
     [1] = LAYOUT(
