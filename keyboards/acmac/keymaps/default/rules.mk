04d54c41a93634beb8597f009e32ddc3f74fb78f
Lynton Ivins <lynton.ivins@proton.me>
2024-06-03 09:16:30 +1200

    edit file strrructure

---
+++ keyboards/acmac/keymaps/default/rules.mk
@@ -0,0 +1,8 @@
+# This file intentionally left blank
+
+RGBLIGHT_ENABLE = yes
+RGBLIGHT_DRIVER = WS2812
+
+#  WS2812_DRIVER_REQUIRED = yes  -may not be required as could be included in above command
+
+
