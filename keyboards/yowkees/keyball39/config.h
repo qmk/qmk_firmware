8e92cd27cf93e4610ff48de893962aec98169583
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-06-03 23:26:38 -0700

    Convert `config.h` to data-driven

--- keyboards/yowkees/keyball39/config.h
+++ keyboards/yowkees/keyball39/config.h
@@ -19,18 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Key matrix parameters
-#define MATRIX_ROWS         (4 * 2)  // split keyboard
-#define MATRIX_COLS         6
-#define MATRIX_ROW_PINS     { F4, F5, F6, F7 }
-#define MATRIX_COL_PINS     { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_MASKED
-#define DEBOUNCE            5
-#define DIODE_DIRECTION     COL2ROW
 
 // Split parameters
-#define SOFT_SERIAL_PIN         D2
-#define SPLIT_HAND_MATRIX_GRID  F6, B5
-#define SPLIT_USB_DETECT
 #ifdef OLED_ENABLE
 #    define SPLIT_OLED_ENABLE
 #endif
@@ -43,7 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_TRANSACTION_IDS_KB KEYBALL_GET_INFO, KEYBALL_GET_MOTION, KEYBALL_SET_CPI
 
 // RGB LED settings
-#define WS2812_DI_PIN       D3
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLED_NUM      48
 #    define RGBLED_SPLIT    { 24, 24 }  // (24 + 22)
