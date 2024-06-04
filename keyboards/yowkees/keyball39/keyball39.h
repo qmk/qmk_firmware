3ffd5f9a410b780ac89b5ac9c3212bab0f70880e
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-06-03 19:08:28 -0700

    Convert `LAYOUT_right_ball` to data-driven

--- keyboards/yowkees/keyball39/keyball39.h
+++ keyboards/yowkees/keyball39/keyball39.h
@@ -23,22 +23,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // clang-format off
 
-#define LAYOUT_right_ball( \
-    L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
-    L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
-    L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
-    L30, L31, L32, L33, L34, L35,    R35, R34,                R30  \
-    ) \
-    { \
-        {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
-        {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
-        {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
-        {   L30,   L31,   L32,   L33,   L34,   L35 }, \
-        {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
-        {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
-        {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
-        {   R30, KC_NO, KC_NO, KC_NO,   R34,   R35 }, \
-    }
 
 
 #define LAYOUT_dual_ball( \
