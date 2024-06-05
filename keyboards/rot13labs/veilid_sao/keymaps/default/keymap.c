7e7764639753e024b61971661b6dfde8fd9d86a3
c0ldbru <dennisp98@gmail.com>
2024-06-04 20:27:19 -0400

    Update keyboards/rot13labs/veilid_sao/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/rot13labs/veilid_sao/keymaps/default/keymap.c
+++ keyboards/rot13labs/veilid_sao/keymaps/default/keymap.c
@@ -38,5 +38,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_sao_1x1(QK_MACRO_0),
+  [0] = LAYOUT_ortho_1x1(QK_MACRO_0),
 };
