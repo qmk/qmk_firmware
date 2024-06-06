e672b6b901f6fdcbb9cadc296822997fdafb61a3
Blake <blake@ghost7.com>
2024-06-06 22:12:33 +0100

    Update keyboards/draytronics/elise_v2/config.h
    Co-authored-by: jack <0x6a73@protonmail.com>

--- keyboards/draytronics/elise_v2/config.h
+++ keyboards/draytronics/elise_v2/config.h
@@ -1,4 +1,6 @@
 //KVM Specific tweaks to prevent key repeat on wake from sleep.
 //Please comment out and recompile if you do not wish to use them.
+#pragma once
+
 #define USB_POLLING_INTERVAL_MS 8
 #define USB_SUSPEND_WAKEUP_DELAY 3000
