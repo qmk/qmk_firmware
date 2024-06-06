003afd15e5591179f061d4492758aaceae781ee4
Blake Drayson <blake@ghost7.com>
2024-06-06 13:15:30 +0100

    Added missing licence headers.

--- keyboards/draytronics/daisy_v2/config.h
+++ keyboards/draytronics/daisy_v2/config.h
@@ -1,3 +1,28 @@
+/*Copyright 2024 Blake Drayson / Draytronics
+
+Contact info@draytronics.co.uk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This code is inspired by and adapted from the code used in the Printed Pad by Noah Beidelman (@noahbei)
+
+It also references the concept and some pixel frames (the arasaka logo) form Aleks (@aleksbrgt)
+
+Other pixel images are either created by me or use the resources of Velency Grpahics https://www.valencygraphics.com/cyberpunk-2077
+
+No claim for licence or ownership is made for any logos or similarities to logos or brand names.
+*/
 #pragma once
 
 #define I2C_DRIVER I2CD1
