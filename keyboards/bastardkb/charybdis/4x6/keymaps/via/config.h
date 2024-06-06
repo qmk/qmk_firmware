4cec46654c66e08404f55d578f7b88903dff49eb
burkfers <finrod09@users.noreply.github.com>
2024-06-06 16:16:33 +0200

    charybdis/4x6: remove redundant layer count setting

--- keyboards/bastardkb/charybdis/4x6/keymaps/via/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/via/config.h
@@ -16,11 +16,6 @@
  */
 #pragma once
 
-#ifdef VIA_ENABLE
-/* VIA configuration. */
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#endif // VIA_ENABLE
-
 #ifndef __arm__
 /* Disable unused features. */
 #    define NO_ACTION_ONESHOT
