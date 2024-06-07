b509ea2966c37a7a65e1d237132cc6433ad6b799
ai03 <admin@ai03.com>
2024-06-07 17:44:46 +0900

    Add misc things from PR checklist

--- keyboards/ai03/altair/altair.h
+++
@@ -1,5 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#include "quantum.h"
--- keyboards/ai03/altair/config.h
+++ keyboards/ai03/altair/config.h
@@ -1,3 +1,4 @@
+/* Copyright 2024 ai03 Design Studio */
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #pragma once
