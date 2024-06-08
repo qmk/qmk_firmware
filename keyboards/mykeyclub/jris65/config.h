43b736bb2387915eda920c3946cc4a9353f1b7e0
Robert Kleemann <robert@kleemann.org>
2024-06-08 10:16:52 -0700

    first compiled, flashed, and tested version of jris65

---
+++ keyboards/mykeyclub/jris65/config.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// RSK: it looks like QMK no longer requires this
+//#include "config_common.h"
+
+
+// /* USB Device descriptor parameter */
+// #define VENDOR_ID       0x5A4C
+// #define PRODUCT_ID      0x4953
+// #define DEVICE_VER      0x0001
+// #define MANUFACTURER    ZL
+// #define PRODUCT         IRIS65
+// #define DESCRIPTION     IRIS65
+
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 16
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { F7, F6, F5, B2, F4 }
+#define MATRIX_COL_PINS { B6, C6, C7, F1, F0, E6, B0, B1, B3, B7, D2, D1, D0, D7, B4, B5 }
+
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+
+
+
+
+
+#ifdef VIA_ENABLE
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#endif
---
+++ keyboards/mykeyclub/jris65/jris65.c
@@ -0,0 +1,1 @@
+#include "jris65.h"
