f0d88104ec8de1b868e091a65a232a25936d914c
Robert Kleemann <robert@kleemann.org>
2024-06-09 08:20:36 -0700

    moved some settings from c/h files to json

--- keyboards/mykeyclub/jris65/config.h
+++ keyboards/mykeyclub/jris65/config.h
@@ -9,15 +9,15 @@
 // #define DESCRIPTION     IRIS65
 
 /* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 16
+//#define MATRIX_ROWS 5
+//#define MATRIX_COLS 16
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { F7, F6, F5, B2, F4 }
-#define MATRIX_COL_PINS { B6, C6, C7, F1, F0, E6, B0, B1, B3, B7, D2, D1, D0, D7, B4, B5 }
+//#define MATRIX_ROW_PINS { F7, F6, F5, B2, F4 }
+//#define MATRIX_COL_PINS { B6, C6, C7, F1, F0, E6, B0, B1, B3, B7, D2, D1, D0, D7, B4, B5 }
 
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
+//#define BOOTMAGIC_LITE_ROW 0
+//#define BOOTMAGIC_LITE_COLUMN 0
 
 #ifdef VIA_ENABLE
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
