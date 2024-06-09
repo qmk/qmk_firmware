54d0cf7f491356351624b5c3d3e85761d3f77da1
Krzysztof Smosna <krzysieksmosna@gmail.com>
2024-06-09 19:17:28 +0200

    Added partial keyboard configuration using OLED and knob. Max brightness restored to 255

--- keyboards/skdevices/adler/mcuconf.h
+++ keyboards/skdevices/adler/mcuconf.h
@@ -7,3 +7,17 @@
 
 #undef STM32_I2C_USE_I2C1
 #define STM32_I2C_USE_I2C1 TRUE
+
+#undef STM32_HSECLK
+#undef STM32_PLLSRC
+#undef STM32_PLLM_VALUE
+#undef STM32_PLLN_VALUE
+#undef STM32_PLLP_VALUE
+#undef STM32_PLLQ_VALUE
+
+#define STM32_HSECLK            16000000U
+#define STM32_PLLSRC            STM32_PLLSRC_HSE
+#define STM32_PLLM_VALUE        16
+#define STM32_PLLN_VALUE        336
+#define STM32_PLLP_VALUE        4
+#define STM32_PLLQ_VALUE        7
