da83e45f37c2bd01e71d6aecf43a2b1fdc94c7be
k-jun <keijun091221@gmail.com>
2024-06-11 03:19:22 +0900

    feat(snowdrop43): reverse scroll

--- keyboards/snowdrop43/keymaps/default/keymap.c
+++ keyboards/snowdrop43/keymaps/default/keymap.c
@@ -47,8 +47,8 @@ enum custom_keycodes {
 bool set_scrolling = false;
 
 // Modify these values to adjust the scrolling speed
-#define SCROLL_DIVISOR_H 8.0
-#define SCROLL_DIVISOR_V 8.0
+#define SCROLL_DIVISOR_H -8.0
+#define SCROLL_DIVISOR_V -8.0
 
 // Variables to store accumulated scroll values
 float scroll_accumulated_h = 0;
