bfb939f8211a3a85732e09bb4871298e7545af7c
k-jun <keijun091221@gmail.com>
2024-06-11 02:28:01 +0900

    feat(snowdrop43): KC_PMNS to KC_MINS

--- keyboards/snowdrop43/keymaps/default/keymap.c
+++ keyboards/snowdrop43/keymaps/default/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,
                           KC_BSPC, KC_SPC, MO(2),    KC_LCMD,
 
-        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_PMNS,
+        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_MINS,
         KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOTE,
         KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,
         KC_RCMD, MO(1),   KC_ENT
