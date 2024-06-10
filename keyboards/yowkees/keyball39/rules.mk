93bf88482912f4ea903f071d92bd4bc970af0032
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-06-10 01:29:33 -0700

    Use `SPI_DRIVER_REQUIRED = yes` (per drashna)

--- keyboards/yowkees/keyball39/rules.mk
+++ keyboards/yowkees/keyball39/rules.mk
@@ -1,7 +1,7 @@
 # Optical sensor driver for trackball.
 POINTING_DEVICE_DRIVER = custom
 SRC += drivers/pmw3360/pmw3360.c
-QUANTUM_LIB_SRC += spi_master.c # Optical sensor use SPI to communicate
+SPI_DRIVER_REQUIRED = yes
 
 SRC += lib/oledkit/oledkit.c    # OLED utility for Keyball series.
 
