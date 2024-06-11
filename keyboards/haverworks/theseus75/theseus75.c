7d31530bb5fd01ae475438ac9d87454e4a9d5bdc
Alex Havermale <alex.havermale@icloud.com>
2024-06-11 17:19:06 -0400

    sync kb state between master and slave, set USBPD pins accordingly

--- keyboards/haverworks/theseus75/theseus75.c
+++ keyboards/haverworks/theseus75/theseus75.c
@@ -18,6 +18,7 @@
 #include "transactions.h"
 #include <stdio.h>
 #include "print.h"
+#include "split_util.h"
 #include "usbpd.h"
 
 typedef struct _kb_state_t {
@@ -40,6 +41,14 @@ const char* usbpd_str(usbpd_allowance_t allowance) {
     }
 }
 
+void kb_state_slave_handler(uint8_t m2s_size, const void* m2s_buffer, uint8_t s2m_size, void* s2m_buffer) {
+    if (m2s_size == sizeof(kb_state_t)) {
+        memcpy(&kb_state, m2s_buffer, sizeof(kb_state_t));
+    } else {
+        printf("Unexpected response in slave handler\n"); // TODO: add split debug logging
+    }
+}
+
 void keyboard_pre_init_kb(void) {
     // Disable the PD peripheral in pre-init because its pins are being used in the matrix:
     PWR->CR3 |= PWR_CR3_UCPD_DBDIS;
@@ -53,6 +62,9 @@ void keyboard_pre_init_kb(void) {
 }
 
 void keyboard_post_init_kb(void) {
+    // Register keyboard state transaction
+    transaction_register_rpc(RPC_ID_KB_STATE, kb_state_slave_handler);
+
     // Set default state values
     kb_state.allowance = USBPD_500MA;
 
@@ -99,6 +111,7 @@ void housekeeping_task_kb(void) {
     // Call the corresponding _user() function (see https://docs.qmk.fm/#/custom_quantum_functions)
     housekeeping_task_user();
 
+    // Update any shared kb state to send to slave:
     static uint32_t last_usbpd_allowance_check_time = 0;
     if (timer_elapsed32(last_usbpd_allowance_check_time) > USBPD_ALLOWANCE_CHECK_INTERVAL) {
         // On master side: check USBPD_1_PIN and USBPD_2_PIN to determine current negotiated with host
@@ -133,4 +146,68 @@ void housekeeping_task_kb(void) {
         };
         last_usbpd_allowance_check_time = timer_read32();
     };
+
+    // Sync state from master to slave:
+    if (is_keyboard_master() && is_transport_connected()) {
+        bool              needs_sync = false;
+        static uint32_t   last_kb_state_sync_time;
+        static kb_state_t last_kb_state;
+
+        // Check if the state values are different
+        if (memcmp(&kb_state, &last_kb_state, sizeof(kb_state_t))) {
+            needs_sync = true;
+            memcpy(&last_kb_state, &kb_state, sizeof(kb_state_t));
+        }
+
+        // Sync state every so often regardless
+        if (timer_elapsed32(last_kb_state_sync_time) > KB_STATE_SYNC_INTERVAL) {
+            needs_sync = true;
+        }
+
+        if (needs_sync) {
+            bool did_sync = transaction_rpc_send(RPC_ID_KB_STATE, sizeof(kb_state_t), &kb_state);
+            if (did_sync) {
+                dprintf("Synced to slave\n");
+                last_kb_state_sync_time = timer_read32();
+            } else {
+                dprintf("Failed to sync state\n");
+            }
+        }
+    }
+
+    // Update the USBPD output pins on slave half whenever allowance has changed:
+    if (!is_keyboard_master()) {
+        static usbpd_allowance_t last_allowance;
+
+        if (last_allowance != kb_state.allowance) {
+            last_allowance = kb_state.allowance;
+
+            printf("Setting USB-PD output to %s (%s-powered)\n", usbpd_str(kb_state.allowance), kb_state.allowance == USBPD_500MA ? "bus" : "self");
+
+            switch (kb_state.allowance) {
+                default:
+                case USBPD_500MA:
+                    // Set USBPD output to 500 mA:
+                    gpio_write_pin_high(USBPD_1_PIN);
+                    gpio_write_pin_high(USBPD_2_PIN);
+                    // Indicate hub is bus-powered and devices should not try to connect or fast charge:
+                    gpio_write_pin_low(BUS_B_PIN);
+                    break;
+                case USBPD_1500MA:
+                    // Set USBPD output to 500 mA:
+                    gpio_write_pin_high(USBPD_1_PIN);
+                    gpio_write_pin_high(USBPD_2_PIN);
+                    // Indicate hub is self-powered and devices can try to connect or fast charge:
+                    gpio_write_pin_high(BUS_B_PIN);
+                    break;
+                case USBPD_3000MA:
+                    // Set USBPD output to 1500 mA:
+                    gpio_write_pin_low(USBPD_1_PIN);
+                    gpio_write_pin_high(USBPD_2_PIN);
+                    // Indicate hub is self-powered and devices can try to connect or fast charge:
+                    gpio_write_pin_high(BUS_B_PIN);
+                    break;
+            }
+        }
+    }
 }
