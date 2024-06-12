75f7fadf2b60607cee0b32441ab9022fc09d6f41
k-jun <keijun091221@gmail.com>
2024-06-12 23:58:39 +0900

    feat(snowdrop43): keymaps siwtch mode

--- keyboards/snowdrop43/keymaps/default/keymap.c
+++ keyboards/snowdrop43/keymaps/default/keymap.c
@@ -3,25 +3,24 @@
 
 #include QMK_KEYBOARD_H
 
-enum my_keycode{
-  SCROLL = SAFE_RANGE,
-  NORMAL,
-  ACCEL,
-  CPI_UP,
-  CPI_DW,
+enum my_keycode {
+    SCROLL = SAFE_RANGE,
+    SWITCH,
+    CPI_UP,
+    CPI_DW,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,
-        KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,
-                          KC_BSPC, KC_SPC, MO(2),    KC_LCMD,
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,
+        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,
+                          KC_BSPC, KC_SPC , MO(2)  , KC_LCMD,
 
-        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_MINS,
-        KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOTE,
-        KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,
-        KC_RCMD, MO(1),   KC_ENT
+        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
+        KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+        KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+        KC_RCMD, MO(1)  , KC_ENT
     ),
     [1] = LAYOUT(
         _______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, _______,
@@ -31,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         _______, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, _______,
         KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_ESC , _______,
-        _______, _______, _______, _______, _______, _______,
-                 _______, _______, _______
+        _______, CPI_UP , CPI_DW , SWITCH , _______, _______,
+        _______, _______, _______
     ),
     [2] = LAYOUT(
         _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,
@@ -42,12 +41,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_CIRC, KC_AMPR, KC_ASTR, KC_PEQL, KC_PPLS, _______,
         KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
-        _______, CPI_UP , CPI_DW , NORMAL , ACCEL  , _______,
-                 _______, _______, _______
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______
     )
 };
 
-bool set_scrolling = false;
+
+int8_t cpi       = 40;
+bool   scrolling = false;
+bool   is_normal = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint8_t  addr     = 0x14;
+    uint8_t  data_n[] = {0x90, 0x00};
+    uint8_t  data_a[] = {0x91, 0x00};
+    uint16_t timeout  = 100;
+    switch (keycode) {
+        case SWITCH:
+            if (record->event.pressed) {
+                if (is_normal) {
+                    i2c_transmit(addr, data_a, 2, timeout);
+                    pimoroni_trackball_set_cpi(128 * cpi);
+                    is_normal = false;
+                } else {
+                    i2c_transmit(addr, data_n, 2, timeout);
+                    pimoroni_trackball_set_cpi(128 * 125);
+                    is_normal = true;
+                }
+            }
+            break;
+        case CPI_UP:
+            if (record->event.pressed) {
+                cpi += 5;
+                pimoroni_trackball_set_cpi(128 * cpi);
+            }
+            break;
+        case CPI_DW:
+            if (record->event.pressed) {
+                cpi -= 5;
+                pimoroni_trackball_set_cpi(128 * cpi);
+            }
+            break;
+        case SCROLL:
+            if (record->event.pressed) {
+                i2c_transmit(addr, data_n, 2, timeout);
+                pimoroni_trackball_set_cpi(128 * 125);
+            } else {
+                if (!is_normal) {
+                    i2c_transmit(addr, data_a, 2, timeout);
+                    pimoroni_trackball_set_cpi(128 * cpi);
+                }
+            }
+            scrolling = record->event.pressed;
+            break;
+        default:
+            break;
+    }
+    return true;
+}
+
+void pointing_device_init_kb(void) {
+    uint8_t  addr     = 0x14;
+    uint8_t  data_a[] = {0x91, 0x00};
+    uint16_t timeout  = 100;
+    i2c_transmit(addr, data_a, 2, timeout);
+    pimoroni_trackball_set_cpi(128 * cpi);
+}
 
 // Modify these values to adjust the scrolling speed
 #define SCROLL_DIVISOR_H -8.0
@@ -56,13 +115,11 @@ bool set_scrolling = false;
 // Variables to store accumulated scroll values
 float scroll_accumulated_h = 0;
 float scroll_accumulated_v = 0;
-int8_t cpi = 40;
-
 
 // Function to handle mouse reports and perform drag scrolling
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     // Check if drag scrolling is active
-    if (set_scrolling) {
+    if (scrolling) {
         // Calculate and accumulate scroll values based on mouse movement and divisors
         scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
         scroll_accumulated_v += (float)mouse_report.y / SCROLL_DIVISOR_V;
@@ -81,49 +138,3 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     }
     return mouse_report;
 }
-
-bool is_normal = true;
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  uint8_t addr = 0x14;
-  uint8_t data_n[] = {0x90, 0x00};
-  uint8_t data_a[] = {0x91, 0x00};
-  uint16_t timeout = 100;
-  switch (keycode) {
-    case ACCEL:
-      i2c_transmit (addr, data_a, 2, timeout);
-      pimoroni_trackball_set_cpi(128*cpi);
-      break;
-    case NORMAL:
-      i2c_transmit (addr, data_n, 2, timeout);
-      pimoroni_trackball_set_cpi(128*125*2);
-      break;
-    case CPI_UP:
-      cpi += 5;
-      pimoroni_trackball_set_cpi(128*cpi);
-      break;
-    case CPI_DW:
-      cpi -= 5;
-      pimoroni_trackball_set_cpi(128*cpi);
-      break;
-    case SCROLL:
-        set_scrolling = record->event.pressed;
-        break;
-    default:
-        break;
-      break;
-  }
-  return true;
-}
-
-// void pointing_device_init_kb(void) {
-//     uint8_t addr = 0x14;
-//     uint8_t data[] = {0x91, 0x00};
-//     uint16_t timeout = 100;
-//     i2c_status_t status;
-//     pimoroni_trackball_set_cpi(128*cpi);
-//     status = i2c_transmit (addr, data, 2, timeout);
-//     if (status != 0) {
-//         return;
-//     }
-// }
