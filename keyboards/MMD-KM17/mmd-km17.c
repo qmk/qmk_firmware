4b11b8220b3a91ccb3700cc942a9c1aad4de8af0
Smartmmd <mmd@i-chip.cn>
2024-06-14 17:46:47 +0800

    Create mmd-km17.c

---
+++ keyboards/MMD-KM17/mmd-km17.c
@@ -0,0 +1,33 @@
+/* Copyright 2022 Jacky
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "quantum.h"
+
+
+void keyboard_pre_init_kb(void) {
+    setPinOutput(A8);
+    writePinHigh(A8);
+}
+
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+    return false;
+}
+
+
