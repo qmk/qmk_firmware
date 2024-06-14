d8542e80d6cba43aeff99a15cf5662479fba711e
Blake Drayson <blake@ghost7.com>
2024-06-14 21:41:13 +0100

    Fix for key repeat on wake from sleep, RBG sleep tweak, readme update

---
+++ keyboards/draytronics/elise_v2/config.h
@@ -0,0 +1,23 @@
+/*Copyright 2021 Blake Drayson / Draytronics
+
+Contact info@draytronics.co.uk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//Fix to prevent key repeat on wake from sleep when using a KVM / non direct usb connection.
+#pragma once
+
+#define USB_POLLING_INTERVAL_MS 8
+#define USB_SUSPEND_WAKEUP_DELAY 3000
