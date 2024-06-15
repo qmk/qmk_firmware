0623f66c1aa5a1f3521302aba9bb6e411482cb29
axolotl-being <nora.and.the.moon@gmail.com>
2024-06-15 12:52:32 -0500

    tide75 personal config

---
+++ keyboards/epomaker/tide75/keymaps/personal_v1/config.h
@@ -0,0 +1,7 @@
+#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+
+#define FORCE_NKRO
+
+#define TAPPING_TOGGLE 3
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4 //max amount for this keyboard
