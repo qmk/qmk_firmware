cf566c1ccb0255d5ca026278c59cda5aa51beaa5
Aggynium <thefalsesix@gmail.com>
2024-06-17 12:51:58 +0700

    Update keymap.c

--- keyboards/dz60/keymaps/60_plus_arrows/keymap.c
+++ keyboards/dz60/keymaps/60_plus_arrows/keymap.c
@@ -1,50 +1,69 @@
 #include QMK_KEYBOARD_H
 
-#define ______ KC_TRNS
+enum layer_names {
+    _BASE, // Default Layer
+    _FN // Fn Layer
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-/* Qwerty
- * ,-----------------------------------------------------------------------------------------.
- * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
- * |-----------------------------------------------------------------------------------------+
- * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
- * |-----------------------------------------------------------------------------------------+
- * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
- * |-----------------------------------------------------------------------------------------+
- * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | FN  |
- * |-----------------------------------------------------------------------------------------+
- * | Ctrl |  Alt  |  Cmd  |              Space                | Cmd | RAlt |  L  |  D  |  R  |
- * `-----------------------------------------------------------------------------------------'
- */
-
-  LAYOUT_directional(
-      KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, MO(1),
-      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
-      ),
-
-/* FN Layer
- * ,-----------------------------------------------------------------------------------------.
- * | Esc | BL- | BL+ | BL  |     |     |     |QK_BOOT|     |     |     |     |     |           |
- * |-----------------------------------------------------------------------------------------+
- * |        |RBGM |     |     |     |     |     |     |     |     |     | Val+ | Val- |RBGTOG|
- * |-----------------------------------------------------------------------------------------+
- * |         | Hue+| Hue-| Sat+| Sat-|     |     |     |     |     |     |     |             |
- * |-----------------------------------------------------------------------------------------+
- * |           |     |     |     |     |     |     |     |     |     |     |     | Ctrl|     |
- * |-----------------------------------------------------------------------------------------+
- * |      |       |       |                                   |     |      |     |     |     |
- * `-----------------------------------------------------------------------------------------'
- */
-
-  LAYOUT_directional(
-      KC_ESC, BL_TOGG, BL_STEP, BL_DOWN,BL_UP,  ______, ______, QK_BOOT, ______, ______, ______, ______, ______, ______, ______,
-      ______, RGB_MOD, ______, ______, ______, ______, ______, ______, ______, ______, ______, RGB_VAI, RGB_VAD, RGB_TOG,
-      ______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, ______, ______, ______, ______, ______, ______, ______,
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_RCTL, ______,
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______
-      ),
+    // Default Layer
+    [_BASE] = LAYOUT_60_ansi_arrow(
+        KC_GESC, KC_1,    KC_2, KC_3,    KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
+        KC_TAB,           KC_Q, KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_CAPS,          KC_A, KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT,          KC_Z, KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_RSFT, KC_UP,   KC_SLSH,
+        KC_LCTL, KC_LGUI,       KC_LALT,             KC_SPC,                      KC_RALT, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    // Fn Layer
+    [_FN] = LAYOUT_60_ansi_arrow(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
+        KC_BRIU,          _______, _______, KC_END,  RESET,   _______, _______, _______, KC_INS,  _______, KC_PSCR, _______, _______, KC_EJCT,
+        KC_BRID,          _______, KC_SLCK, _______, _______, _______, KC_HOME, _______, _______, _______, _______, _______, KC_MUTE,
+        KC_VOLU,          _______, _______, KC_CALC, _______, _______, KC_NLCK, KC_MAIL, _______, _______,          KC_PAUS, KC_PGUP, _______,
+        KC_VOLD, KC_MRWD,          KC_MFFD,                   KC_MPLY,                            KC_MSTP, _______, KC_MPRV, KC_PGDN, KC_MNXT
+    )
 };
+
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_setrgb(100, 255, 100);
+}
+
+// RGB Lighting/LED/Underglow as Caps Lock, Num Lock, Scroll Lock, and Layer Indicator
+void update_led(void) {
+    // Num Lock Indicator
+    if (host_keyboard_led_state().num_lock) {
+        rgblight_setrgb(225, 8, 0);
+    }
+    // Scroll Lock Indicator
+    if (host_keyboard_led_state().scroll_lock) {
+        rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 2);
+    }
+}
+
+void led_set_user(uint8_t usb_led) {
+    // Caps Lock Indicator
+    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+        writePinLow(B2);
+        rgblight_setrgb(255, 110, 0);
+    }
+    // Layer Indicator
+    else {
+        switch (get_highest_layer(layer_state)) {
+            // Fn Layer Indicator
+            case _FN:
+                rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 1);
+                break;
+            // Default Layer Indicator
+            case _BASE:
+                rgblight_setrgb(100, 255, 100);
+                break;
+        }
+        update_led();
+    }
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    update_led();
+    return state;
+}
