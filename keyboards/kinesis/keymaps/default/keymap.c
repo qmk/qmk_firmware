6398cf78b31726fd0a26f83c00305e259a46ba0d
Saul Goodman <teoh@users.noreply.github.com>
2024-06-18 14:10:54 -0700

    kinesis keymap

--- keyboards/kinesis/keymaps/default/keymap.c
+++ keyboards/kinesis/keymaps/default/keymap.c
@@ -34,13 +34,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_ESC, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
-           KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
+           KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
            KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
                    KC_GRV ,KC_INS ,KC_LEFT,KC_RGHT,
 			   KC_LCTL,KC_LALT,
                                     KC_HOME,
-                           KC_BSPC,KC_DEL ,KC_END ,
-    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SCRL  ,KC_PAUS, KC_NO, QK_BOOT,
+                           KC_LGUI,KC_ENT,KC_END ,
+    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SCRL  ,KC_PAUS, KC_A, QK_BOOT,
 	KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
 	KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
 	KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
@@ -48,6 +48,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
            KC_RGUI,KC_RCTL,
            KC_PGUP,
-           KC_PGDN,KC_ENTER ,KC_SPC
+           KC_PGDN,KC_BSPC, KC_SPC
     )
 };
--- keyboards/kinesis/kinesis.h
+++ keyboards/kinesis/kinesis.h
@@ -14,6 +14,8 @@
 #    include "kint41.h"
 #elif defined(KEYBOARD_kinesis_kintlc)
 #    include "kintlc.h"
+#elif defined(KEYBOARD_kinesis_elite_c)
+#    include "elite_c.h"
 #endif
 
 #include "quantum.h"
