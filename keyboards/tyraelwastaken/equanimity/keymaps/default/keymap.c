ebf4e1f5f3c98ed809a74c45bb67cc61d25eee89
TyraelWasTaken <96232590+TyraelWasTaken@users.noreply.github.com>
2024-06-20 21:07:38 +0100

    Update keymap.c

--- keyboards/tyraelwastaken/equanimity/keymaps/default/keymap.c
+++ keyboards/tyraelwastaken/equanimity/keymaps/default/keymap.c
@@ -30,6 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef ENCODER_MAP_ENABLE
     const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
         [_BL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-        [_FL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+        [_FL] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     };
 #endif
