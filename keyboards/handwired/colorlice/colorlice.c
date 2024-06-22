46a13b1d88586d26ceed49a7111498b92eb0b1fe
zvecr <git@zvecr.com>
2024-06-22 20:43:49 +0100

    Migrate led_update_kb implementations to DD

--- keyboards/handwired/colorlice/colorlice.c
+++ keyboards/handwired/colorlice/colorlice.c
@@ -51,18 +51,3 @@ void suspend_wakeup_init_kb(void)
     suspend_wakeup_init_user();
 }
 #endif
-
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if(res) {
-        // gpio_write_pin sets the pin high for 1 and low for 0.
-        // In this example the pins are inverted, setting
-        // it low/0 turns it on, and high/1 turns the LED off.
-        // This behavior depends on whether the LED is between the pin
-        // and VCC or the pin and GND.
-        gpio_write_pin(B2, !led_state.num_lock);
-        gpio_write_pin(C6, !led_state.caps_lock);
-        gpio_write_pin(B7, !led_state.scroll_lock);
-    }
-    return res;
-}
