df3e0e2c9b3aa5f955e9e916772e5d93fd2007fd
zvecr <git@zvecr.com>
2024-06-22 08:25:53 +0100

    Migrate led_update_kb implementations to DD

--- keyboards/hineybush/h87a/h87a.c
+++ keyboards/hineybush/h87a/h87a.c
@@ -15,22 +15,6 @@
  */
 #include "quantum.h"
 
-void matrix_init_kb(void) {
-	// put your keyboard start-up code here
-	// runs once when the firmware starts up
-   	gpio_set_pin_output(D5);
-    gpio_set_pin_output(E6);
-	matrix_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-    if(led_update_user(led_state)) {
-        gpio_write_pin(D5, !led_state.caps_lock);
-        gpio_write_pin(E6, !led_state.scroll_lock);
-    }
-    return true;
-}
-
 void eeconfig_init_kb(void) {  // EEPROM is getting reset!
   rgblight_enable(); // Enable RGB by default
   rgblight_sethsv(0, 255, 128);  // Set default HSV - red hue, full saturation, medium brightness
