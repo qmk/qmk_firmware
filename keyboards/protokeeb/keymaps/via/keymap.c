d1c29a4f6be279da9738a2bc71d4d40550f74c53
atechofficials <atechofficials@gmail.com>
2024-06-25 04:40:53 +0530

    Removed the tri-layer functionality from via keymap. Also, removed the LAYOUT and LAYOUT_midi layouts from the keyboard.json file as requested by QMK member in pull request

--- keyboards/protokeeb/keymaps/via/keymap.c
+++ keyboards/protokeeb/keymaps/via/keymap.c
@@ -26,14 +26,13 @@ enum protokeeb_keymap_layers {
 };
 
 #define LOWER LT(LAYER_LOWER, KC_KP_0)
-#define RAISE LT(DYNAMIC_LAYER1, KC_KP_ENTER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[LAYER_BASE] = LAYOUT_ortho_4x4( /* Base */
 	  KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH,
 	  KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK,
 	  KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_MINUS,
-	  LOWER, KC_KP_DOT, RAISE, KC_KP_PLUS
+	  LOWER, KC_KP_DOT, KC_KP_ENTER, KC_KP_PLUS
 	),
 
 	[LAYER_LOWER] = LAYOUT_ortho_4x4( /* RGB Control */
