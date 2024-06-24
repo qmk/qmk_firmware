58c210201342353da814ebe12d67dbf0c64c2230
atechofficials <atechofficials@gmail.com>
2024-06-25 05:18:19 +0530

    Removed the unused SPI driver configuration in mcuconf.h file as requested by QMK member in pull request

--- keyboards/protokeeb/rev1/mcuconf.h
+++ keyboards/protokeeb/rev1/mcuconf.h
@@ -24,10 +24,3 @@
 
 #undef RP_I2C_USE_I2C1
 #define RP_I2C_USE_I2C1 FALSE
-
-/* RP2040 SPI Driver Selection */
-#undef RP_SPI_USE_SPI0
-#define RP_SPI_USE_SPI0 TRUE
-
-#undef RP_SPI_USE_SPI1
-#define RP_SPI_USE_SPI1 FALSE
