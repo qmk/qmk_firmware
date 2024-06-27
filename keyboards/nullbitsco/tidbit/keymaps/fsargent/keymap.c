8b8127e9d1d218525acb7a93914869030b92120e
Felix Sargent <felix.sargent@gmail.com>
2024-06-27 09:11:01 -0700

    Update tidbit.

--- keyboards/nullbitsco/tidbit/keymaps/fsargent/keymap.c
+++ keyboards/nullbitsco/tidbit/keymaps/fsargent/keymap.c
@@ -6,7 +6,7 @@
 #define _NUM	0
 #define _NAV	1
 #define _WNAV	2
-#define _FN		3
+#define _FN	3
 #define _FN2	4
 #define _MUSIC	5
 #define _RGB	6
@@ -20,55 +20,55 @@ enum sol_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// Base layer (numpad)
 	[_NUM] = LAYOUT(
-							KC_ESC,			KC_BSPC,	KC_EQL, \
-	KC_KP_SLASH,			KC_KP_7,		KC_KP_8,	KC_KP_9,\
-	KC_KP_ASTERISK,			KC_KP_4,		KC_KP_5,	KC_KP_6,\
-	KC_KP_MINUS,			KC_KP_1,		KC_KP_2,	KC_KP_3,\
+				KC_ESC,			KC_BSPC,	KC_EQL, \
+	KC_KP_SLASH,		KC_KP_7,		KC_KP_8,	KC_KP_9,\
+	KC_KP_ASTERISK,		KC_KP_4,		KC_KP_5,	KC_KP_6,\
+	KC_KP_MINUS,		KC_KP_1,		KC_KP_2,	KC_KP_3,\
 	LT(_NAV,KC_KP_PLUS),	KC_KP_ENTER,	KC_KP_0,	KC_KP_DOT\
 	),
 	[_NAV] = LAYOUT(
-						KC_ESC,		G(KC_Z),	S(G(KC_Z)),\
-	KC_BSPC,			A(KC_LEFT),	KC_UP,		A(KC_RIGHT),\
-	G(KC_X),			KC_LEFT,	KC_DOWN,	KC_RIGHT,\
-	G(KC_C),			S(KC_TAB),	SELECT,		KC_TAB,\
+				KC_ESC,		G(KC_Z),	S(G(KC_Z)),\
+	KC_BSPC,		A(KC_LEFT),	KC_UP,		A(KC_RIGHT),\
+	G(KC_X),		KC_LEFT,	KC_DOWN,	KC_RIGHT,\
+	G(KC_C),		S(KC_TAB),	SELECT,		KC_TAB,\
 	LT(_NUM, G(KC_V)),	KC_ENT,		KC_SPC,		KC_BSPC
 	),
 	[_WNAV] = LAYOUT(
-						KC_ESC,		C(KC_Z),	S(C(KC_Z)),\
-	KC_BSPC,			C(KC_LEFT),	KC_UP,		C(KC_RIGHT),\
-	C(KC_X),			KC_LEFT,	KC_DOWN,	KC_RIGHT,\
-	C(KC_C),			S(KC_TAB),	SELECT,		KC_TAB,\
+				KC_ESC,		C(KC_Z),	S(C(KC_Z)),\
+	KC_BSPC,		C(KC_LEFT),	KC_UP,		C(KC_RIGHT),\
+	C(KC_X),		KC_LEFT,	KC_DOWN,	KC_RIGHT,\
+	C(KC_C),		S(KC_TAB),	SELECT,		KC_TAB,\
 	LT(_NUM, C(KC_V)),	KC_ENT,		KC_SPC,		KC_BSPC
 	),
 	[_FN] = LAYOUT(
-					KC_F10,			KC_F11,		KC_F12, \
-	KC_F13,			KC_F7,			KC_F8,		KC_F9,\
-	KC_F14,			KC_F4,			KC_F5,		KC_F6,\
-	KC_F15,			KC_F1,			KC_F2,		KC_F3,\
+				KC_F10,		KC_F11,		KC_F12, \
+	KC_F13,			KC_F7,		KC_F8,		KC_F9,\
+	KC_F14,			KC_F4,		KC_F5,		KC_F6,\
+	KC_F15,			KC_F1,		KC_F2,		KC_F3,\
 	KC_F16,			KC_KP_ENTER,	KC_F10,		KC_ESC\
 	),
 	[_FN2] = LAYOUT(
-					KC_F22,			KC_F23,		KC_F24, \
+				KC_F22,			KC_F23,		KC_F24,\
 	KC_NO,			KC_F19,			KC_F20,		KC_F21,\
 	KC_ESC,			KC_F16,			KC_F17,		KC_F18,\
-	KC_KP_ENTER,	KC_F13,			KC_F14,		KC_F15,\
-	KC_NO,			KC_KP_ENTER,	KC_F22,		KC_ESC\
+	KC_KP_ENTER,		KC_F13,			KC_F14,		KC_F15,\
+	KC_NO,			KC_KP_ENTER,		KC_F22,		KC_ESC\
 	),
 
 	[_MUSIC] = LAYOUT(
-					KC_NO,		KC_MUTE,	KC_NO, \
+				KC_NO,		KC_MUTE,	KC_NO, \
 	KC_MSEL,		KC_NO,		KC_VOLU,	KC_NO,\
 	KC_ESC,			KC_MPRV,	KC_MPLY,	KC_MNXT,\
-	KC_KP_ENTER,	KC_NO,		KC_VOLD,	KC_NO,\
+	KC_KP_ENTER,		KC_NO,		KC_VOLD,	KC_NO,\
 	KC_MUTE,		KC_KP_ENTER,	KC_LCTL,	KC_LGUI\
 	),
 	// Function layer (numpad)
 	[_RGB] = LAYOUT(
 				KC_NO,		RGB_TOG,	RGB_TOG,
-	RGB_MOD,	RGB_MOD,	RGB_RMOD,	RGB_RMOD,
-	RGB_HUI,	RGB_HUI,	RGB_HUD,	RGB_HUD,
-	RGB_SAI,	RGB_SAI,	RGB_SAD,	RGB_SAD,
-	RGB_VAI,	RGB_VAI,	RGB_VAD,	RGB_VAD
+	RGB_MOD,		RGB_MOD,	RGB_RMOD,	RGB_RMOD,
+	RGB_HUI,		RGB_HUI,	RGB_HUD,	RGB_HUD,
+	RGB_SAI,		RGB_SAI,	RGB_SAD,	RGB_SAD,
+	RGB_VAI,		RGB_VAI,	RGB_VAD,	RGB_VAD
 	)
 };
 
@@ -226,77 +226,77 @@ void oled_task_user(void) {
 #endif
 
 
-void matrix_init_user(void) {
-	matrix_init_remote_kb();
-	register_code(KC_NLCK);
-}
+// void matrix_init_user(void) {
+// 	matrix_init_remote_kb();
+// 	register_code(KC_NLCK);
+// }
 
-void matrix_scan_user(void) {
-	matrix_scan_remote_kb();
-}
+// void matrix_scan_user(void) {
+// 	matrix_scan_remote_kb();
+// }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	switch (keycode)
-	{
-		case SELECT:
-			if (record->event.pressed) {
-				SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_LEFT) SS_DOWN(X_LSFT) SS_TAP(X_RIGHT) SS_UP(X_LSFT) SS_UP(X_LALT));
-			}
-			return false;	// Skip all further processing of this key
-		default:
-			break;
-	}
-	return true;
-}
-
-
-void led_set_kb(uint8_t usb_led) {
-	if (usb_led & (1<<USB_LED_NUM_LOCK))
-		set_bitc_LED(LED_DIM);
-	else
-		set_bitc_LED(LED_OFF);
-}
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+// 	switch (keycode)
+// 	{
+// 		case SELECT:
+// 			if (record->event.pressed) {
+// 				SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_LEFT) SS_DOWN(X_LSFT) SS_TAP(X_RIGHT) SS_UP(X_LSFT) SS_UP(X_LALT));
+// 			}
+// 			return false;	// Skip all further processing of this key
+// 		default:
+// 			break;
+// 	}
+// 	return true;
+// }
 
 
+// void led_set_kb(uint8_t usb_led) {
+// 	if (usb_led & (1<<USB_LED_NUM_LOCK))
+// 		set_bitc_LED(LED_DIM);
+// 	else
+// 		set_bitc_LED(LED_OFF);
+// }
 
 
-enum combo_events {
-	RST,
-	TAB,
-	SHIFT_TAB,
-	COMBO_LENGTH
-};
-uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
-
-const uint16_t PROGMEM reset_combo[] = {KC_KP_ENTER, LT(_NAV,KC_KP_PLUS), KC_KP_0, KC_KP_DOT, COMBO_END};
-const uint16_t PROGMEM tab_combo[] = {LT(_NAV,KC_KP_PLUS), KC_KP_MINUS, COMBO_END};
-const uint16_t PROGMEM shift_tab_combo[] = {KC_KP_3, KC_KP_DOT, COMBO_END};
 
 
-combo_t key_combos[] = {
-	[TAB] = COMBO_ACTION(tab_combo),
-	[SHIFT_TAB] = COMBO_ACTION(shift_tab_combo),
-	[RST] = COMBO_ACTION(reset_combo),
-};
+// enum combo_events {
+// 	RST,
+// 	TAB,
+// 	SHIFT_TAB,
+// 	COMBO_LENGTH
+// };
+// uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
 
-void process_combo_event(uint16_t combo_index, bool pressed) {
-	switch(combo_index) {
-		case RST:
-			if (pressed) {
-				set_bitc_LED(LED_DIM);
-				rgblight_disable_noeeprom();
-				bootloader_jump(); //jump to bootloader
-			}
-			break;
-		case TAB:
-			if (pressed) {
-				tap_code16(KC_TAB);
-			}
-			break;
-		case SHIFT_TAB:
-			if (pressed) {
-				tap_code16(S(KC_TAB));
-			}
-			break;
-	}
-}
+// const uint16_t PROGMEM reset_combo[] = {KC_KP_ENTER, LT(_NAV,KC_KP_PLUS), KC_KP_0, KC_KP_DOT, COMBO_END};
+// const uint16_t PROGMEM tab_combo[] = {LT(_NAV,KC_KP_PLUS), KC_KP_MINUS, COMBO_END};
+// const uint16_t PROGMEM shift_tab_combo[] = {KC_KP_3, KC_KP_DOT, COMBO_END};
+
+
+// combo_t key_combos[] = {
+// 	[TAB] = COMBO_ACTION(tab_combo),
+// 	[SHIFT_TAB] = COMBO_ACTION(shift_tab_combo),
+// 	[RST] = COMBO_ACTION(reset_combo),
+// };
+
+// void process_combo_event(uint16_t combo_index, bool pressed) {
+// 	switch(combo_index) {
+// 		case RST:
+// 			if (pressed) {
+// 				set_bitc_LED(LED_DIM);
+// 				rgblight_disable_noeeprom();
+// 				bootloader_jump(); //jump to bootloader
+// 			}
+// 			break;
+// 		case TAB:
+// 			if (pressed) {
+// 				tap_code16(KC_TAB);
+// 			}
+// 			break;
+// 		case SHIFT_TAB:
+// 			if (pressed) {
+// 				tap_code16(S(KC_TAB));
+// 			}
+// 			break;
+// 	}
+// }
