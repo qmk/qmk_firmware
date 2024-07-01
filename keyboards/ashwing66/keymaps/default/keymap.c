8a5029f7fdf4d889af6ba7bbbee6afda3c5c3df7
James Gzowski <flexyjerkov@gmail.com>
2024-07-01 20:39:22 +0100

    Update keymap.c

--- keyboards/ashwing66/keymaps/default/keymap.c
+++ keyboards/ashwing66/keymaps/default/keymap.c
@@ -10,25 +10,25 @@ enum custom_layer {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LAYERA] = LAYOUT_5x16(
-        KC_ESC , KC_1  		,KC_2   ,KC_3   , KC_4  , KC_5  ,  			 	 	 KC_6   , KC_7    	, KC_8    , KC_9   , KC_0   , KC_GRV ,
-        KC_TAB , KC_Q  		,KC_W   ,KC_E   , KC_R  , KC_T  , 			 		 KC_Y   , KC_U  	, KC_I    , KC_O   , KC_P   ,KC_MINUS,
-        KC_LCTL, KC_A  		,KC_S   ,KC_D   , KC_F  , KC_G  ,  		 			 KC_J   , KC_K    	, KC_L    , KC_SCLN,KC_QUOT ,KC_QUOT ,
-        KC_LSFT, KC_Z  		,KC_X   ,KC_C   , KC_V  , KC_B  , KC_LBRC,       KC_MUTE,	KC_RBRC, KC_N   , KC_M    	, KC_COMM , KC_DOT , KC_SLSH, KC_RSFT,
-        KC_NO  , MO(_LAYERB) 	,KC_LGUI,KC_LALT,KC_SPC , KC_LALT, KC_SPC , KC_SPC, 	KC_ENT, KC_ENT , KC_BSPC, MO(_LAYERC)   , KC_INS  , KC_DEL , KC_DEL , KC_DEL
+        KC_ESC , KC_1  		,KC_2   ,KC_3   , KC_4  , KC_5  ,  			 	 	 KC_6   , KC_7    	, KC_8    , KC_9   , KC_0    , KC_GRV ,
+        KC_TAB , KC_Q  		,KC_W   ,KC_F   , KC_P  , KC_G  , 			 		 KC_J   , KC_L  	, KC_U    , KC_Y   , KC_SCLN ,KC_MINUS,
+        KC_LCTL, KC_A  		,KC_R   ,KC_S   , KC_T  , KC_D  ,  		 			 KC_H   , KC_N    	, KC_E    , KC_I   , KC_O    ,KC_QUOT ,
+        KC_LSFT, KC_Z  		,KC_X   ,KC_C   , KC_V  , KC_B  , KC_LBRC,       KC_MUTE,	KC_RBRC, KC_K   , KC_M    	, KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
+        KC_BSLS  , MO(_LAYERB) 	,KC_LGUI,KC_LALT,KC_SPC , KC_LALT, KC_SPC , KC_PGDN, 	KC_PGUP, KC_ENT , KC_BSPC, MO(_LAYERC)   , KC_INS  , KC_DEL , KC_DEL  , KC_SLSH
 ),
     [_LAYERB] = LAYOUT_5x16(
-        KC_ESC , KC_1           ,KC_2   ,KC_3   , KC_4  , KC_5  ,                                        KC_6   , KC_7          , KC_8    , KC_9   , KC_0   , KC_GRV ,
-        KC_TAB , KC_Q           ,KC_W   ,KC_E   , KC_R  , KC_T  ,                                        KC_Y   , KC_U          , KC_I    , KC_O   , KC_P   ,KC_MINUS,
-        KC_LCTL, KC_A           ,KC_S   ,KC_D   , KC_F  , KC_G  ,                                        KC_J   , KC_K          , KC_L    , KC_SCLN,KC_QUOT ,KC_QUOT ,
-        KC_LSFT, KC_Z           ,KC_X   ,KC_C   , KC_V  , KC_B  , KC_LBRC,       KC_MUTE,       KC_RBRC, KC_N   , KC_M          , KC_COMM , KC_DOT , KC_SLSH, KC_RSFT,
-        KC_NO  , MO(_LAYERB)    ,KC_LGUI,KC_LALT,KC_SPC , KC_LALT, KC_SPC , KC_SPC,     KC_ENT, KC_ENT , KC_BSPC, MO(_LAYERC)   , KC_INS  , KC_DEL , KC_DEL , KC_DEL
+        KC_ESC , KC_1           ,KC_2   ,KC_3   , KC_4  , KC_5  ,                                        KC_6   , KC_7          , KC_8    , KC_9   , KC_0    , KC_GRV ,
+        KC_TAB , KC_Q           ,KC_W   ,KC_F   , KC_P  , KC_G  ,                                        KC_J   , KC_L          , KC_U    , KC_Y   , KC_SCLN ,KC_MINUS,
+        KC_LCTL, KC_A           ,KC_R   ,KC_S   , KC_T  , KC_D  ,                                        KC_H   , KC_N          , KC_E    , KC_I   , KC_O    ,KC_QUOT ,
+        KC_LSFT, KC_Z           ,KC_X   ,KC_C   , KC_V  , KC_B  , KC_LBRC,       KC_MUTE,       KC_RBRC, KC_K   , KC_M          , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
+        KC_BSLS  , MO(_LAYERB)  ,KC_LGUI,KC_LALT,KC_SPC , KC_LALT, KC_SPC , KC_PGDN,    KC_PGUP, KC_ENT , KC_BSPC, MO(_LAYERC)   , KC_INS  , KC_DEL , KC_DEL  , KC_SLSH
 ),
     [_LAYERC] = LAYOUT_5x16(
-        KC_ESC , KC_1           ,KC_2   ,KC_3   , KC_4  , KC_5  ,                                        KC_6   , KC_7          , KC_8    , KC_9   , KC_0   , KC_GRV ,
-        KC_TAB , KC_Q           ,KC_W   ,KC_E   , KC_R  , KC_T  ,                                        KC_Y   , KC_U          , KC_I    , KC_O   , KC_P   ,KC_MINUS,
-        KC_LCTL, KC_A           ,KC_S   ,KC_D   , KC_F  , KC_G  ,                                        KC_J   , KC_K          , KC_L    , KC_SCLN,KC_QUOT ,KC_QUOT ,
-        KC_LSFT, KC_Z           ,KC_X   ,KC_C   , KC_V  , KC_B  , KC_LBRC,       KC_MUTE,       KC_RBRC, KC_N   , KC_M          , KC_COMM , KC_DOT , KC_SLSH, KC_RSFT,
-        KC_NO  , MO(_LAYERB)    ,KC_LGUI,KC_LALT,KC_SPC , KC_LALT, KC_SPC , KC_SPC,     KC_ENT, KC_ENT , KC_BSPC, MO(_LAYERC)   , KC_INS  , KC_DEL , KC_DEL , KC_DEL
+        KC_ESC , KC_1           ,KC_2   ,KC_3   , KC_4  , KC_5  ,                                        KC_6   , KC_7          , KC_8    , KC_9   , KC_0    , KC_GRV ,
+        KC_TAB , KC_Q           ,KC_W   ,KC_F   , KC_P  , KC_G  ,                                        KC_J   , KC_L          , KC_U    , KC_Y   , KC_SCLN ,KC_MINUS,
+        KC_LCTL, KC_A           ,KC_R   ,KC_S   , KC_T  , KC_D  ,                                        KC_H   , KC_N          , KC_E    , KC_I   , KC_O    ,KC_QUOT ,
+        KC_LSFT, KC_Z           ,KC_X   ,KC_C   , KC_V  , KC_B  , KC_LBRC,       KC_MUTE,       KC_RBRC, KC_K   , KC_M          , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
+        KC_BSLS  , MO(_LAYERB)  ,KC_LGUI,KC_LALT,KC_SPC , KC_LALT, KC_SPC , KC_PGDN,    KC_PGUP, KC_ENT , KC_BSPC, MO(_LAYERC)   , KC_INS  , KC_DEL , KC_DEL  , KC_SLSH
 )
 };
 
