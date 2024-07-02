8b29617b2e10600a2bb984371d7115250546ddb5
James Gzowski <flexyjerkov@gmail.com>
2024-07-02 11:04:11 +0100

    02-Jul-11:04:08

--- keyboards/ashwing66/config.h
+++ keyboards/ashwing66/config.h
@@ -2,5 +2,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-#   define ENCODER_MAP_KEY_DELAY 10
-#   define ENCODER_DEFAULT_POS 0x3
+#define ENCODER_MAP_KEY_DELAY 10
+#define ENCODER_DEFAULT_POS 0x3
