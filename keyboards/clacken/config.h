ae90f8fb9b478d90e1f5e96aa70675940d85be7f
Carter Pollan <carter.pollan@gmail.com>
2024-07-06 21:21:15 -0500

    updated Clacken

--- keyboards/clacken/config.h
+++ keyboards/clacken/config.h
@@ -16,15 +16,19 @@
 #define SERIAL_USART_TX_PIN GP6
 #define SERIAL_USART_RX_PIN GP29
 
+
+#define PERMISSIVE_HOLD
+
+
 /* Right keyboard kin definitions */
-#define MATRIX_COL_PINS_RIGHT { GP4, GP3, GP28, GP27, GP26 }
-#define MATRIX_ROW_PINS_RIGHT { GP2, GP1, GP0, GP7 }
+#define MATRIX_COL_PINS_RIGHT { F5, F6, F7, B1, B3 }
+#define MATRIX_ROW_PINS_RIGHT { C6, D4, D0, B1 }
 
 /* LED definitions */
 #define WS2812_DI_PIN GP12
-#define RGBLED_NUM 2
-#define RGBLED_SPLIT {1, 1}
-#define RGBLIGHT_LAYERS
+// #define RGBLED_NUM 2
+// #define RGBLED_SPLIT {1, 1}
+// #define RGBLIGHT_LAYERS
 #define SPLIT_LAYER_STATE_ENABLE
 #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
 // #define SPLIT_TRANSPORT_MIRROR
--- keyboards/clacken/keymaps/default/keymap.c
+++ keyboards/clacken/keymaps/default/keymap.c
@@ -26,16 +26,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   └───┘   └───┘
      */
     [_ALPHA_QWERTY] = LAYOUT_clack(
-        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,               KC_Y,           KC_U,           KC_I,           KC_O,           KC_P, 
-        CTL_T(KC_A),    SFT_T(KC_S),    ALT_T(KC_D),    KC_F,           LWIN_T(KC_G),       RWIN_T(KC_H),   KC_J,           RALT_T(KC_K),   RSFT_T(KC_L),   RCTL_T(KC_SCLN), 
-        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,               KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH, 
+        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,               KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,
+        CTL_T(KC_A),    SFT_T(KC_S),    ALT_T(KC_D),    KC_F,           LWIN_T(KC_G),       RWIN_T(KC_H),   KC_J,           RALT_T(KC_K),   RSFT_T(KC_L),   RCTL_T(KC_SCLN),
+        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,               KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,
                                         MO(_NUMBER),    KC_SPC,         KC_BSPC,            KC_DEL,         KC_ENTER,       MO(_FUNCTION)),
 
-    /* 
+    /*
      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
      * │F1 │F2 │F3 │F4 │F5 │       │F6 │F7 │F8 │F9 │F10│
      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-     * │ A │ S │ D │ F │ G │       │PUp│PDw│UP │End│F11│
+     * │CTL│SFT│ALT│TAB│GUI│       │PUp│PDw│UP │End│F11│
      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
      * │PSC│Cap│   │   │   │       │PDw│LFT│DWN│RIT│F12│
      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   └───┘   └───┘
      */
     [_FUNCTION]     = LAYOUT_clack(
-        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,              KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10, 
-        KC_LCTL,        KC_LSFT,        KC_LALT,        KC_TAB,         KC_LGUI,            KC_PAGE_UP,     KC_HOME,        KC_UP,          KC_END,         KC_F11, 
-        KC_PSCR,        KC_CAPS,        XXXXXXX,        XXXXXXX,        XXXXXXX,            KC_PAGE_DOWN,   KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_F12, 
+        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,              KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,
+        KC_LCTL,        KC_LSFT,        KC_LALT,        KC_TAB,         KC_LGUI,            KC_PAGE_UP,     KC_HOME,        KC_UP,          KC_END,         KC_F11,
+        KC_PSCR,        KC_CAPS,        XXXXXXX,        XXXXXXX,        XXXXXXX,            KC_PAGE_DOWN,   KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_F12,
                                         _______,        _______,        _______,            _______,        _______,        _______),
     /*
      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
@@ -65,16 +65,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   └───┘   └───┘
      */
     [_NUMBER]       = LAYOUT_clack(
-        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,               KC_6,           KC_7,           KC_8,           KC_9,           KC_0, 
-        KC_LCTL,        KC_LSFT,        KC_LALT,        KC_TAB,         KC_LGUI,            XXXXXXX,        KC_GRV,         KC_QUOT,        KC_LBRC,        KC_RBRC, 
-        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,            XXXXXXX,        KC_MINS,        KC_EQL,         KC_BSLS,        KC_SLSH, 
+        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,               KC_6,           KC_7,           KC_8,           KC_9,           KC_0,
+        KC_LCTL,        KC_LSFT,        KC_LALT,        KC_TAB,         KC_LGUI,            XXXXXXX,        KC_GRV,         KC_QUOT,        KC_LBRC,        KC_RBRC,
+        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,            XXXXXXX,        KC_MINS,        KC_EQL,         KC_BSLS,        KC_SLSH,
                                         _______,        _______,        _______,            _______,        KC_RSFT,        _______),
     [_CTL]          = LAYOUT_clack(
-    KC_KB_VOLUME_UP,    KC_MPRV,        KC_MPLY,        KC_MNXT,        XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_BRIU, 
-    KC_KB_VOLUME_DOWN,  XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_BRID, 
-    KC_KB_MUTE,         XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX, 
+    KC_KB_VOLUME_UP,    KC_MPRV,        KC_MPLY,        KC_MNXT,        XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_BRIU,
+    KC_KB_VOLUME_DOWN,  XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        KC_BRID,
+    KC_KB_MUTE,         XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,            XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
                                         _______,        _______,        _______,            _______,        _______,        _______)
-    }; 
+    };
 
 const uint16_t PROGMEM layers_combo[] = {MO(_NUMBER), MO(_FUNCTION), COMBO_END};
 const uint16_t PROGMEM undo[] = {CTL_T(KC_A), KC_Z, COMBO_END};
