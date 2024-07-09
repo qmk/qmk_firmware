6684dcbaab650f7b7308f04d7076e1479fb14560
Drashna Jael're <drashna@live.com>
2024-07-09 15:26:23 -0700

    Remove unneeded update interval and moved split pin to dd

--- keyboards/crkbd/rev4_mini/config.h
+++ keyboards/crkbd/rev4_mini/config.h
@@ -20,9 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP12
 #define SERIAL_USART_RX_PIN GP13
-#define SPLIT_HAND_PIN GP21
 
 #define I2C_DRIVER I2CD1
-#define OLED_UPDATE_INTERVAL 50
 #define I2C1_SDA_PIN GP6
 #define I2C1_SCL_PIN GP7
