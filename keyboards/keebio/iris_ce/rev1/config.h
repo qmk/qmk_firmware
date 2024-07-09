20e3e6f799df922951022a36eebc82e26bd74946
FlamingPumpkin <82876933+Myghalloween@users.noreply.github.com>
2024-07-09 23:01:34 +0200

    Update config.h

--- keyboards/keebio/iris_ce/rev1/config.h
+++ keyboards/keebio/iris_ce/rev1/config.h
@@ -14,3 +14,21 @@
 #define I2C_DRIVER I2CD2
 #define I2C1_SDA_PIN GP10
 #define I2C1_SCL_PIN GP11
+
+/ Additional options
+
+// #define SPLIT_LAYER_STATE_ENABLE // ensures the current layer state is available on the slave when using the QMK-provided split transport
+// #define SPLIT_LED_STATE_ENABLE // ensures the current host indicator state (caps/num/scroll) is available on the slave when using the QMK-provided split transport
+
+// Additional RGB matrix options
+
+#define RGB_MATRIX_TIMEOUT 300000 // number of milliseconds to wait until rgb automatically turns off
+#define RGB_MATRIX_SLEEP // turn off effects when suspended
+// #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+// #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 120
+// #define RGB_MATRIX_DEFAULT_ON true // Sets the default enabled state, if none has been set
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR // Sets the default mode, if none has been set
+// #define RGB_MATRIX_DEFAULT_HUE 127 // Sets the default hue value, if none has been set
+// #define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
+// #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+// #define RGB_MATRIX_DEFAULT_SPD 127 // Sets the default animation speed, if none has been set 
