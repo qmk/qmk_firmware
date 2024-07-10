41fbc685b3007636a798ceefaa55488f8101ff35
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-07-09 19:45:18 -0700

    Enable PMW33XX SROM upload for default keymap

--- keyboards/yowkees/keyball39/keymaps/default/config.h
+++ keyboards/yowkees/keyball39/keymaps/default/config.h
@@ -20,6 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define PMW33XX_UPLOAD_SROM
+
 #ifdef RGBLIGHT_ENABLE
 //#    define RGBLIGHT_EFFECT_BREATHING
 //#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
