b696d78df509664c9298d48ee46d837d3093ad24
Mrinal Singh Tak <89794960+atechofficials@users.noreply.github.com>
2024-07-11 01:56:55 +0530

    Update keyboards/protokeeb/keymaps/default/rules.mk
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/protokeeb/keymaps/default/rules.mk
+++ keyboards/protokeeb/keymaps/default/rules.mk
@@ -1,8 +1,2 @@
-# Build Options Protokeeb rev1
-# Change yes to no to disable features
-
-# Rotary Encoder Keymap support
 ENCODER_MAP_ENABLE = yes
-
-# Enable Tri Layer support
 TRI_LAYER_ENABLE = yes
