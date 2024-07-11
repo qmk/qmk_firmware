5d7f01de054c9a69d192e91e34a2187b78c4f991
falsonix <greenemachine124@gmail.com>
2024-07-10 21:25:15 -0600

    add proper keymap

--- keyboards/falsonix/fx19/keymaps/default/keymap.c
+++ keyboards/falsonix/fx19/keymaps/default/keymap.c
@@ -5,20 +5,23 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │   │
-     * ├───┼───┼───┤ + │
-     * │ 4 │ 5 │ 6 │   │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │   │
-     * ├───┴───┼───┤Ent│
-     * │   0   │ . │   │
-     * └───────┴───┴───┘
-     */
-    [0] = LAYOUT_numpad_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_P0,            KC_PDOT
+    *┌───┬───┬───┬───┐
+    * Num│ / │ * │ - │
+    *├───┼───┼───┼───┤
+    *│ 7 │ 8 │ 9 │ + │
+    *├───┼───┼───┤   │
+    *│ 4 │ 5 │ 6 │   │
+    *├───┼───┼───┤───┤
+    *│ 1 │ 2 │ 3 │   │
+    *├───┼───┼───┤Ent│
+    *│Win│ 0 │ . │   │
+    *┴───┴───┴───┴───┘
+    */
+    [0] = LAYOUT_numpad_4x5(
+        KC_LNUM,   KC_PSLS,   KC_PAST,   KC_PMNS,
+        KC_P7,     KC_P8,     KC_P9,     KC_PPLS,
+        KC_P4,     KC_P5,     KC_P6,   
+        KC_P1,     KC_P2,     KC_P3,     KC_PENT,  
+        KC_RWIN,   KC_P0,     KC_PDOT
     )
 };
