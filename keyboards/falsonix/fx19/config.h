26da65ef9bc1c7525ca0b8a1f9d4aef9b7f3051b
falsonix <greenemachine124@gmail.com>
2024-07-11 12:47:49 -0600

    add config for RGB and bootloader

---
+++ keyboards/falsonix/fx19/config.h
@@ -0,0 +1,5 @@
+// Written by Falsonix
+// Original on 7/11/2024
+
+#define WS2812_DI_PIN F7 // Define pin for RGB lighting
+#define RGBLIGHT_DEFAULT_ON // Set RGB lighting on by default
---
+++ keyboards/falsonix/fx19/rules.mk
@@ -0,0 +1,7 @@
+# Written by Falsonix
+# Original on 7/11/2024
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+RGBLIGHT_ENABLE = yes # Enable RGB lighting
