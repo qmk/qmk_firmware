3a4956fd6d89432ff6922750afc3b32a1ba7be37
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2024-07-13 00:42:38 -0700

    Update config.h

--- keyboards/atset/at2/config.h
+++ keyboards/atset/at2/config.h
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright 2024 Atset (@anubhavd7) */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #pragma once
-#define TAPPING_TERM 175
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
