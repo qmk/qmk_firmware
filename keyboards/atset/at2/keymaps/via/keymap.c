27cb1053ab72b5c97d9ed7db76226abdcc6ad4a9
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2024-07-13 01:43:23 -0700

    Update keymap.c

--- keyboards/atset/at2/keymaps/via/keymap.c
+++ keyboards/atset/at2/keymaps/via/keymap.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H 
-VIA_ENABLE = yes
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_MUTE, KC_MPLY
