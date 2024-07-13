6dd97984a3d86df7cc8dca5bc3e0f51a02f64290
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2024-07-13 00:41:45 -0700

    Update keymap.c

--- keyboards/atset/at2/keymaps/via/keymap.c
+++ keyboards/atset/at2/keymaps/via/keymap.c
@@ -1,19 +1,12 @@
-/* Copyright 2024 Atset (@anubhavd7) */
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include QMK_KEYBOARD_H
-
-enum {
-    _L0,
-    _L1
-} keyboard_layers;
+#include QMK_KEYBOARD_H 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_L0] = LAYOUT_ortho_1x1(
+    [0] = LAYOUT(
         KC_MUTE, KC_MPLY
     ),
-    [_L1] = LAYOUT_ortho_1x1(
+    [1] = LAYOUT(
         KC_A, KC_B
     )
 };
-
