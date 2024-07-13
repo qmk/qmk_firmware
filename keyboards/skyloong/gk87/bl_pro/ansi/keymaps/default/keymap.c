22e59a56a91cef2d2f67d04dd980fed1e5c574ea
NaturalZh <zhouziran@jikedingzhi.com>
2024-07-13 17:23:23 +0800

    update skyloong/gk87/bl_pro/ansi

--- keyboards/skyloong/gk87/bl_pro/ansi/keymaps/default/keymap.c
+++ keyboards/skyloong/gk87/bl_pro/ansi/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_WIN_INDEX] =  LAYOUT_all(
         _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH, G(C(KC_S)),   KC_SLEP,   KC_MPRV,  KC_MPLY,    KC_MNXT,   KC_MUTE,  KC_VOLD,     KC_VOLU,    KC_PSCR,
-        _______,  _______,  _______,  _______,  _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,    BL_TOGG,   BL_BRTG,  BL_BRTG,      KC_DEL,     KC_INS,  KC_HOME,   KC_PGUP,
+        _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    BL_TOGG,   BL_BRTG,  BL_BRTG,      KC_DEL,     KC_INS,  KC_HOME,   KC_PGUP,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,   _______,  _______,     _______,     KC_DEL,   KC_END,   KC_PGDN,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,   _______,               _______,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,                          _______,                BL_UP,
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MAC_INDEX] = LAYOUT_all(
         _______,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,     KC_F6,     KC_F7,    KC_F8,      KC_F9,       KC_F10,   KC_F11,   KC_F12,    KC_PSCR,
-        _______,  _______,  _______,  _______,  _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,    BL_TOGG,      BL_BRTG,  BL_BRTG,   KC_DEL,     KC_INS,  KC_HOME,   KC_PGUP,
+        _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    BL_TOGG,      BL_BRTG,  BL_BRTG,   KC_DEL,     KC_INS,  KC_HOME,   KC_PGUP,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,      _______,  _______,  _______,     KC_DEL,   KC_END,   KC_PGDN,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,      _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,    _______,                          _______,                BL_UP,
@@ -57,7 +57,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 #if defined(DIP_SWITCH_MAP_ENABLE)
 const uint16_t PROGMEM dip_switch_map[NUM_DIP_SWITCHES][NUM_DIP_STATES] = {
-    DIP_SWITCH_OFF_ON(TO(0), TO(1)),
+    DIP_SWITCH_OFF_ON(DF(0), DF(1)),
 };
 #endif
 
