100fd07eb50bca4f3e1da2e21d4b74456c8965d4
VinayKomaravolu <vinay.komaravolu@gmail.com>
2024-07-13 02:02:21 -0500

    Adds TRI_LAYER_ENABLE to keymaps/default

--- keyboards/varys/keymaps/default/keymap.c
+++ keyboards/varys/keymaps/default/keymap.c
@@ -4,14 +4,11 @@
 #include QMK_KEYBOARD_H
 
 enum layer_number {
-    _QWERTY = 0,
-    _RAISE,
+    _QWERTY,
     _LOWER,
+    _RAISE,
     _MISC,
 };
-
-#define L_RAISE MO(_RAISE)
-#define L_LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default keyboard settings
@@ -33,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
     KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_WHOM,    KC_AUDIO_MUTE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                               KC_LALT, L_LOWER,KC_LGUI, KC_SPC,   KC_ENT, KC_BSPC,  L_RAISE, KC_RALT
+                               KC_LALT, TL_LOWR,KC_LGUI, KC_SPC,   KC_ENT, KC_BSPC,  TL_UPPR , KC_RALT
     ),
     [_LOWER] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
@@ -57,11 +54,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                _______, _______, _______, _______, _______,  _______, _______, _______ 
     )
 };
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    state = update_tri_layer_state(state, _RAISE, _LOWER, _MISC);
-    return state;
-}
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
