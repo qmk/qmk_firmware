070bac68de08d2bed50dd281fc7304a209312b9c
William Brookes <wbrookes66@gmail.com>
2024-07-18 10:22:31 +1000

    Swap adjust & L ctrl

--- keyboards/contra/keymaps/brookesy66/keymap.c
+++ keyboards/contra/keymaps/brookesy66/keymap.c
@@ -32,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Adjst| Ctrl | Gui  | Alt  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+   * | Ctrl | Adjst| Gui  | Alt  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
    * `-----------------------------------------------------------------------------------'
    */
   [_BASE] = LAYOUT_planck_mit(
     KC_ESC,      KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,    KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,      KC_A,    KC_S,    KC_D,    KC_F,       KC_G,    KC_H,    KC_J,         KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_N,    KC_M,         KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    MO(_ADJUST), KC_LCTL, KC_LGUI, KC_LALT, MO(_LOWER),     KC_SPC,       MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, MO(_ADJUST), KC_LGUI, KC_LALT, MO(_LOWER),     KC_SPC,       MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   /* Lower
