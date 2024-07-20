b62d809f75f00378a3b36116117df07b014548d9
Dasky <32983009+daskygit@users.noreply.github.com>
2024-07-20 22:27:24 +0100

    remove commented MASTER_LEFT

--- keyboards/handwired/dactyl_minidox/keymaps/via/config.h
+++ keyboards/handwired/dactyl_minidox/keymaps/via/config.h
@@ -17,7 +17,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* Select hand configuration */
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
-
 #define EE_HANDS
