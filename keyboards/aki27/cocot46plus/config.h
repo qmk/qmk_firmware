c26c021c46e0283adb52d00fe638ed5bb7973109
Mark Stosberg <mark@stosberg.com>
2024-07-23 10:13:16 -0400

    WIP: getting rid of CONVERT_TO
    Encoder and lights are currently not working correctly.

--- keyboards/aki27/cocot46plus/config.h
+++ keyboards/aki27/cocot46plus/config.h
@@ -18,17 +18,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // These do not seem to have keyboard.json support yet.
-#define ADNS5050_SCLK_PIN         B2
-#define ADNS5050_SDIO_PIN         B4
-#define ADNS5050_CS_PIN           B5
+#define ADNS5050_SCLK_PIN         23U
+#define ADNS5050_SDIO_PIN         8U
+#define ADNS5050_CS_PIN           9U
+
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
 
 /* key matrix size */
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 6
-
-// wiring of each half
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, NO_PIN }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
 
 #define POINTING_DEVICE_ROTATION_180
 #define OLED_FONT_H "keyboards/aki27/cocot46plus/glcdfont.c"
