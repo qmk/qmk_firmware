fd2a82051fdc4a77764b52ffcd5d0f1d2e1076d9
NaturalZh <zhouziran@jikedingzhi.com>
2024-07-24 16:59:51 +0800

    update skyloong/gk980/q1/ansi/via/keymap.c for layer switch

--- keyboards/skyloong/gk980/q1/ansi/keymaps/via/keymap.c
+++ keyboards/skyloong/gk980/q1/ansi/keymaps/via/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_WIN_INDEX] =  LAYOUT_all(
         _______,             KC_F14,   KC_F15, G(KC_TAB),  KC_WSCH,  G(C(KC_S)),   KC_SLEP,   KC_MPRV,  KC_MPLY,         KC_MNXT,   KC_MUTE,  KC_VOLD,     KC_VOLU,   G(KC_SPC),      KC_INS,      KC_HOME,  KC_END, KC_PAUSE,
-        _______,  _______,  _______,  _______,   _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,         RGB_TOG,  RGB_RMOD,  RGB_MOD,      KC_DEL,                   KC_NUM,      KC_PSLS, KC_PAST,  KC_PMNS,
+        _______,  _______,  _______,  _______,   _______,  _______,    DF(_WIN),  DF(_MAC),   _______,  _______,         RGB_TOG,  RGB_RMOD,  RGB_MOD,      KC_DEL,                   KC_NUM,      KC_PSLS, KC_PAST,  KC_PMNS,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,   _______,  _______,     _______,                    KC_P7,        KC_P8,   KC_P9,  KC_PPLS,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,   _______,               _______,                    KC_P4,        KC_P5,   KC_P6,
         _______,  _______,  _______,  _______,   _______,  _______,     _______,   _______,   _______,  _______,         _______,                          _______,  RGB_VAI,          KC_P1,        KC_P2,   KC_P3,  KC_PENT,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MAC_INDEX] = LAYOUT_all(
         _______,              KC_F1,    KC_F2,    KC_F3,    KC_F4,       KC_F5,     KC_F6,     KC_F7,    KC_F8,          KC_F9,     KC_F10,   KC_F11,   KC_F12,   G(KC_SPC),      KC_INS,      KC_HOME,  KC_END, KC_PAUSE,
-        _______,  _______,  _______,  _______,  _______,  _______,    TO(_WIN),  TO(_MAC),   _______,  _______,        RGB_TOG,   RGB_RMOD,  RGB_MOD,   KC_DEL,                   KC_NUM,      KC_PSLS, KC_PAST,  KC_PMNS,
+        _______,  _______,  _______,  _______,  _______,  _______,    DF(_WIN),  DF(_MAC),   _______,  _______,        RGB_TOG,   RGB_RMOD,  RGB_MOD,   KC_DEL,                   KC_NUM,      KC_PSLS, KC_PAST,  KC_PMNS,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,    _______,  _______,  _______,                    KC_P7,        KC_P8,   KC_P9,  KC_PPLS,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,    _______,            _______,                    KC_P4,        KC_P5,   KC_P6,
         _______,  _______,  _______,  _______,  _______,  _______,     _______,   _______,   _______,  _______,        _______,                        _______,  RGB_VAI,          KC_P1,        KC_P2,   KC_P3,  KC_PENT,
@@ -133,7 +133,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
        rgb_matrix_set_flags(LED_FLAG_ALL);
        return true;
 #    endif
-     case TO(0):
+     case DF(0):
       if (record->event.pressed) {
        L_WIN = 1;
        set_single_persistent_default_layer(0); // Save default layer 0 to eeprom
@@ -150,7 +150,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return true; // continue all further processing of this key
 
-     case TO(1):
+     case DF(1):
       if (record->event.pressed) {
        L_MAC = 1;
        set_single_persistent_default_layer(1);  //Save default layer 1 to eeprom
