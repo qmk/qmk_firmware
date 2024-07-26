29f51aeccec0bb8d8a9a24f4cc5fc7b1fdf5a961
falsonix <greenemachine124@gmail.com>
2024-07-26 17:12:40 -0600

    fix stuff once and for all

--- keyboards/falsonix/fx19/rules.mk
+++
@@ -1,8 +0,0 @@
-# Written by Falsonix
-# Original on 7/11/2024
-
-# Bootloader selection
-BOOTLOADER = atmel-dfu
-
-RGBLIGHT_ENABLE = yes # enable RGB lighting
-RGBLIGHT_DRIVER = ws2812 # specify led type
