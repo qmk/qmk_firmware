aa470f75e2d5e2a0cc1e4ebb9e585fb3345a0ce3
mike <programmermike1@gmail.com>
2024-07-28 01:36:37 -0700

    encoder adjusts kb brightness

--- keyboards/boardsource/lulu/keymaps/micleo2/keymap.c
+++ keyboards/boardsource/lulu/keymaps/micleo2/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_BOOT,       KC_1,          KC_2,         KC_3,          KC_4,          KC_5,                                        KC_6,          KC_7,          KC_8,         KC_9,          KC_0,          QK_BOOT,
   TG(_PLV),      KC_Q,          KC_W,         KC_E,          KC_R,          KC_T,                                        KC_Y,          KC_U,          KC_I,         KC_O,          KC_P,          KC_DEL,
   PWR_SFT,       KC_A,          LT(U, KC_S),  ALT_T(KC_D),   LT(N, KC_F),   KC_G,                                        KC_H,          KC_J,          KC_K,         KC_L,          KC_SCLN,       OSL(Y),
-  TG(_BLN),      KC_Z,          KC_X,         KC_C,          KC_V,          KC_B,          KC_LBRC,       KC_RBRC,       KC_N,          KC_M,          KC_COMM,      KC_DOT,        KC_COLN,       C(G(KC_Q)),
+  TG(_BLN),      KC_Z,          KC_X,         KC_C,          KC_V,          KC_B,          RGB_VAI,       RGB_VAD,       KC_N,          KC_M,          KC_COMM,      KC_DOT,        KC_COLN,       C(G(KC_Q)),
                                 _______,      CTL_T(KC_ESC), GUI_T(KC_SPC), HYPR_T(KC_ENT),                              KB_VLEAD,      SFT_T(KC_BSPC),OSL(M),       TG(_GME)
 ),
 
