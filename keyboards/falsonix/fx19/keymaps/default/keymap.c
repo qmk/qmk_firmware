ffe6fb2ec6feb0a88fa34d85be01f9f4a5a6d6cb
Christian Greene <95236755+falsonix@users.noreply.github.com>
2024-07-30 15:47:47 -0600

    Update keyboards/falsonix/fx19/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/falsonix/fx19/keymaps/default/keymap.c
+++ keyboards/falsonix/fx19/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *│Win│ 0 │ . │   │
     *┴───┴───┴───┴───┘
     */
-    [0] = LAYOUT_numpad_4x5(
+    [0] = LAYOUT(
         KC_LNUM,   KC_PSLS,   KC_PAST,   KC_PMNS,
         KC_P7,     KC_P8,     KC_P9,     
         KC_P4,     KC_P5,     KC_P6,     KC_PPLS,
