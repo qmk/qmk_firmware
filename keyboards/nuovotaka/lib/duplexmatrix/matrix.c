ce729661f661102138f8628b47f62ddced75c992
Takahiro Natsume <tkn.infomail@gmail.com>
2024-08-10 14:43:01 +0900

    Update matrix

--- keyboards/nuovotaka/lib/duplexmatrix/matrix.c
+++ keyboards/nuovotaka/lib/duplexmatrix/matrix.c
@@ -13,6 +13,8 @@
 
 #define MATRIXSIZE_PER_HAND (ROWS_PER_HAND * sizeof(matrix_row_t))
 
+#define MATRIX_INPUT_PRESSED_STATE 0
+
 static pin_t row_pins[PINNUM_ROW] = MATRIX_ROW_PINS;
 static pin_t col_pins[PINNUM_COL] = MATRIX_COL_PINS;
 
@@ -22,11 +24,11 @@ static inline void set_pin_input(pin_t pin) {
     }
 }
 
-static void set_pins_input(pin_t *pins, uint8_t n)
-{
-    for (uint8_t i = 0; i < n; i++)
-    {
-        set_pin_input(pins[i]);
+static void set_pins_input(pin_t* pins, uint8_t n) {
+    for (uint8_t i = 0; i < n; i++) {
+        if (pins[i] != NO_PIN) {
+            set_pin_input(pins[i]);
+        }
     }
 }
 
@@ -38,46 +40,55 @@ static inline void set_pin_output(pin_t pin) {
 }
 
 static inline bool read_pin(pin_t pin) {
-    return gpio_read_pin(pin);
+    if (pin != NO_PIN) {
+        return (gpio_read_pin(pin) == MATRIX_INPUT_PRESSED_STATE) ? 0 : 1;
+    } else {
+        return 1;
+    }
 }
 
-__attribute__((weak)) void duplex_scan_post_kb(matrix_row_t out_matrix[]) {}
+__attribute__((weak)) void duplex_scan_post_kb(matrix_row_t current_matrix[]) {}
 
-static void duplex_scan(matrix_row_t out_matrix[]) {
+static void duplex_scan(matrix_row_t current_matrix[]) {
     // scan column to row
-    for (uint8_t row = 0; row < PINNUM_ROW; row++)
-    {
+    for (uint8_t row = 0; row < PINNUM_ROW; row++) {
+        if (row_pins[row] != NO_PIN) {
         set_pin_output(row_pins[row]);
+        }
         matrix_output_select_delay();
         for (uint8_t col = 0; col < PINNUM_COL; col++) {
             if (!read_pin(col_pins[col])) {
-                out_matrix[row] |= 1 << col;
+                current_matrix[row] |= 1 << col;
             }
         }
+        if (row_pins[row] != NO_PIN) {
         set_pin_input(row_pins[row]);
+        }
         matrix_output_unselect_delay(row, false);
     }
 
     // scan row to column.
-    for (uint8_t col = 0; col < PINNUM_COL; col++)
-    {
+    matrix_row_t shifter = ((matrix_row_t)1) << PINNUM_COL;
+    for (uint8_t col = 0; col < PINNUM_COL; col++, shifter <<= 1) {
+        if (col_pins[col] != NO_PIN) {
         set_pin_output(col_pins[col]);
+        }
         matrix_output_select_delay();
-        matrix_row_t shifter = ((matrix_row_t)1) << (col + PINNUM_COL);
         for (uint8_t row = 0; row < PINNUM_ROW; row++) {
             if (!read_pin(row_pins[row])) {
-                out_matrix[row] |= shifter;
+                current_matrix[row] |= shifter;
             }
         }
+        if (col_pins[col] != NO_PIN) {
         set_pin_input(col_pins[col]);
+        }
         matrix_output_unselect_delay(col, false);
     }
 
-    duplex_scan_post_kb(out_matrix);
+    duplex_scan_post_kb(current_matrix);
 }
 
-void matrix_init_custom(void)
-{
+void matrix_init_custom(void) {
     set_pins_input(col_pins, PINNUM_COL);
     set_pins_input(row_pins, PINNUM_ROW);
 }
