2f7ba0d1d445910570246ff32143a503cbb49488
Sandy Macdonald <sandyjmacdonald@MacBook-Air-de-Sandy.local>
2024-08-13 23:11:29 +0100

    Added layer with RGB LED function keys.

--- keyboards/keybow_2040/keymaps/default/keymap.c
+++ keyboards/keybow_2040/keymaps/default/keymap.c
@@ -3,6 +3,13 @@
 
 #include QMK_KEYBOARD_H
 
+enum layer {
+    LAYER_0,
+    LAYER_1,
+};
+
+#define KC_LYR1     LT(LAYER_1, KC_P0) 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
@@ -15,4 +22,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ 0 │ . │Ent│ + │
      * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x4(KC_P7, KC_P8, KC_P9, KC_PSLS, KC_P4, KC_P5, KC_P6, KC_PAST, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_P0, KC_PDOT, KC_PENT, KC_PPLS)};
+    [LAYER_0] = LAYOUT_ortho_4x4(KC_P7, KC_P8, KC_P9, KC_PSLS, KC_P4, KC_P5, KC_P6, KC_PAST, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_LYR1, KC_PDOT, KC_PENT, KC_PPLS),
+    [LAYER_1] = LAYOUT_ortho_4x4(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_MOD, RGB_RMOD)};
