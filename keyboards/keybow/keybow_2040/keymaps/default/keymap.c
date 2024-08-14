181b5cc5a6d43590f23a21b4b0b3c47b7e4401ec
sandyjmacdonald <sandyjmacdonald@gmail.com>
2024-08-14 13:15:06 +0100

    Adding boot button as RGB modifier.

--- keyboards/keybow/keybow_2040/keymaps/default/keymap.c
+++ keyboards/keybow/keybow_2040/keymaps/default/keymap.c
@@ -16,13 +16,9 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer {
-    LAYER_0,
-    LAYER_1,
-};
-
-#define KC_LYR1     LT(LAYER_1, KC_P0) 
+#define KC_LYR1 LT(1, KC_P0)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [LAYER_0] = LAYOUT_ortho_4x4( KC_P7, KC_P8, KC_P9, KC_PSLS, KC_P4, KC_P5, KC_P6, KC_PAST, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_LYR1, KC_PDOT, KC_PENT, KC_PPLS ),
-    [LAYER_1] = LAYOUT_ortho_4x4( _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAD, RGB_SAI, RGB_HUD, RGB_HUI, _______, RGB_TOG, RGB_RMOD, RGB_MOD )};
+    [0] = LAYOUT( KC_P7, KC_P8, KC_P9, KC_PSLS, KC_P4, KC_P5, KC_P6, KC_PAST, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_LYR1, KC_PDOT, KC_PENT, KC_PPLS, RGB_TOG ),
+    [1] = LAYOUT( _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAD, RGB_SAI, RGB_HUD, RGB_HUI, _______, RGB_TOG, RGB_RMOD, RGB_MOD, _______ )
+};
