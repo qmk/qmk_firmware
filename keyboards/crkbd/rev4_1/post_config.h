2d1854c12d669ce989bf2191985a87169a0e9217
Drashna Jael're <drashna@live.com>
2024-08-16 21:37:21 -0700

    Additional config fixes

--- keyboards/crkbd/rev4_1/post_config.h
+++ keyboards/crkbd/rev4_1/post_config.h
@@ -6,3 +6,6 @@
 #ifndef OLED_FONT_H
 #  define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
 #endif
+#ifndef OLED_UPDATE_INTERVAL
+#    define OLED_UPDATE_INTERVAL 50
+#endif // OLED_UPDATE_INTERVAL
