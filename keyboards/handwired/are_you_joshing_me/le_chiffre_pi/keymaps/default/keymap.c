78548e0c217454b7027b038577206e1843d593b1
joshua kaunert <4586402+jkaunert@users.noreply.github.com>
2024-08-19 09:25:25 -0500

    updates layers 1&2

--- keyboards/handwired/are_you_joshing_me/le_chiffre_pi/keymaps/default/keymap.c
+++ keyboards/handwired/are_you_joshing_me/le_chiffre_pi/keymaps/default/keymap.c
@@ -27,11 +27,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1,             KC_2,             KC_3,             KC_4,             KC_5,                               KC_6,     KC_7,             KC_8,             KC_9,            KC_0,
         KC_EXLM,          KC_AT,            KC_HASH,          KC_DLR,           KC_PERC,                            KC_CIRC,  KC_AMPR,          KC_ASTR,          KC_MINS,         KC_EQL,
         KC_BSLS,          KC_UNDS,          KC_LCBR,          KC_LBRC,          KC_LPRN,                            KC_RPRN,  KC_RBRC,          KC_RCBR,          KC_QUOTE,        KC_GRV,
-                                                              KC_TRNS,          KC_TRNS,                            KC_TRNS,  KC_TRNS
+                                                              KC_LSFT,          KC_TRNS,                            KC_TRNS,  KC_TRNS
     ),
     [2] = LAYOUT_split_3x5_2(
         QK_BOOT,          KC_NO,            AG_NORM,          AG_SWAP,          DB_TOGG,                            KC_ESC,   KC_PGDN,          KC_PGUP,          KC_END,          KC_SCLN,
-        RGB_TOG,          RGB_HUI,          RGB_SAI,          RGB_VAI,          KC_TAB,                             KC_LEFT,  KC_RGHT,          KC_UP,            KC_DOWN,         KC_BSPC,
+        RGB_TOG,          RGB_HUI,          RGB_SAI,          RGB_VAI,          KC_TAB,                             KC_LEFT,  KC_DOWN,          KC_UP,            KC_RGHT,         KC_BSPC,
         RGB_MOD,          RGB_HUD,          RGB_SAD,          RGB_VAD,          KC_GRV,                             KC_MINS,  KC_QUOTE,         KC_COMM,          KC_DOT,          KC_BSLS,
                                                               KC_LSFT,          KC_TRNS,                            KC_TRNS,  KC_TRNS
     ),
