411e7516d3b9329153e4a722f8491c1e151c0cca
mkass420 <xazgaz50@gmail.com>
2024-08-24 00:34:08 +0300

    rawhid in new file, fixed rgb matrix, still haven't added effects

--- keyboards/pcilabs/pcilabs_common/pcilabs_common.c
+++ keyboards/pcilabs/pcilabs_common/pcilabs_common.c
@@ -14,7 +14,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include "eeprom.h"
 #include "scanfunctions.h"
 #include "print.h"
-#include "raw_hid.h"
+#include "rawhid.h"
 #include "lut.h"
 
 extern const key_config_t key_config_default;
@@ -132,41 +132,6 @@ void keyboard_post_init_kb(void) {
     keyboard_post_init_user();
 }
 
-void raw_hid_receive(uint8_t *data, uint8_t length) {
-    switch (data[0]) {
-        case 2:;
-            /* new report structure: id, row_num, row_low, row_high, switch_type, mode, actuation_point_low, actuation_point_high, press_sensitivity, release_sensitivity, press_hysteresis, release_hysteresis */
-            uint8_t      row        = data[1];
-            uint16_t     row_val    = (uint16_t)data[2] | (uint16_t)data[3] << 8;
-            key_config_t new_config = {.switch_type = data[4], .mode = data[5], .actuation_point = (uint16_t)data[6] | (uint16_t)data[7] << 8, .press_sensitivity = data[8], .release_sensitivity = data[9], .press_hysteresis = data[10], .release_hysteresis = data[11]};
-            for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-                if ((row_val >> col) & 1) {
-                    memcpy(keys[row][col].config_raw, new_config.config_raw, EECONFIG_KEY_DATA_SIZE);
-                }
-            }
-            lut_init();
-            values_save();
-            get_sensor_offsets();
-            break;
-        case 3:;
-            /* new rebind structure: id, [layer, row, col, keycode], ..., 69 {finish marker} */
-            for (int i = 1; i < length && data[i] != 69; i++) {
-                uint8_t  layer   = data[i];
-                uint8_t  row     = data[i + 1];
-                uint8_t  col     = data[i + 2];
-                uint16_t keycode = data[i + 3];
-                dynamic_keymap_set_keycode(layer, row, col, keycode);
-            }
-            break;
-        case 4:;
-            bootloader_jump();
-            break;
-
-        case 5:;
-            eeconfig_init();
-            reset_keyboard();
-    }
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // If console is enabled, it will print the matrix position and status of each key pressed
--- keyboards/pcilabs/pcilabs_common/pcilabs_common.mk
+++ keyboards/pcilabs/pcilabs_common/pcilabs_common.mk
@@ -5,4 +5,5 @@ SRC += \
 	pcilabs_common/scanfunctions.c \
 	pcilabs_common/custom_analog.c \
 	pcilabs_common/pcilabs_common.c \
+	pcilabs_common/rawhid.c \
 	pcilabs_common/matrix_analog.c
