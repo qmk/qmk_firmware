65a17f25ee7710befea53db57eeaf9878cf2a934
Timass60 <118626023+Timass60@users.noreply.github.com>
2024-08-24 23:09:19 +0300

    Update keymap.c

--- keyboards/pcilabs/tmtng60_moon/keymaps/default/keymap.c
+++ keyboards/pcilabs/tmtng60_moon/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
         for (uint8_t i = led_min; i < led_max; i++) {
-            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
+            if (g_led_config.flags[i] & LED_FLAG_INDICATOR) {
                 rgb_matrix_set_color(i, RGB_WHITE);
             }
         }
