94cb0f5d52458491f59f90cd79d15c4f00a251c2
Moses Hoyt <mohoyt@users.noreply.github.com>
2024-08-25 19:13:39 +0200

    Update keyboards/sthlmkb/storre/matrix.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/sthlmkb/storre/matrix.c
+++ keyboards/sthlmkb/storre/matrix.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "quantum.h"
+#include "matrix.h"
 
 #define COL_SHIFTER ((uint32_t)1)
 
