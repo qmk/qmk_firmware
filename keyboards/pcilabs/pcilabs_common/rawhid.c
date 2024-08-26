b1a8339fe71708f116f386062f1a06acb85b4075
mkass420 <xazgaz50@gmail.com>
2024-08-26 17:38:08 +0300

    reports from kb to software, TODO: sync rgb when keycodes are used

--- keyboards/pcilabs/pcilabs_common/rawhid.c
+++ keyboards/pcilabs/pcilabs_common/rawhid.c
@@ -6,12 +6,108 @@
 #include <stdint.h>
 #include <string.h>
 #include "config.h"
+#include "color.h"
+#include "rgb_matrix.h"
 #include "raw_hid.h"
+
+void raw_hid_send_rgb_settings(void){
+    uint8_t data[32] = {0};
+    data[0] = RECIEVE_RGB_SETTINGS;
+    data[1] = rgb_matrix_get_hue();
+    data[2] = rgb_matrix_get_sat();
+    data[3] = rgb_matrix_get_val();
+    data[4] = rgb_matrix_get_mode();
+    data[5] = rgb_matrix_get_speed();
+    raw_hid_send(data, 32);
+}
+
+void raw_hid_send_key_rebinds(void){
+    /* new rebind structure: id, [layer, row, col, keycode], ..., 69 {finish marker} */
+    uint8_t data[32] = {0};
+    data[0] = RECIEVE_KEY_REBIND;
+    uint8_t data_idx = 1;
+    for (uint8_t layer = 0; layer < DYNAMIC_KEYMAP_LAYER_COUNT; layer++){
+        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+            for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+                uint8_t keycode = dynamic_keymap_get_keycode(layer, row, col); // TODO: make it 16 bit when software will be ready
+                if(data_idx + 4 >= 31) {
+                    data[data_idx] = 69;
+                    raw_hid_send(data, 32);
+                    data_idx = 1;
+                    memset(&data[1], 0, 31);
+                }
+                uint8_t rebind[4] = {layer, row, col, keycode};
+                memcpy(&data[data_idx], rebind, 4);
+                data_idx += 4;
+            }
+        }
+    }
+    if(data[data_idx] != 0){
+        data[data_idx + 1] = 69;
+        raw_hid_send(data, 32);
+    }
+}
+
+void raw_hid_send_key_config(uint8_t row_num, uint16_t row_val, key_config_t* key_config){
+    /* new report structure:
+        id, row_num, row_low, row_high, switch_type, mode,
+        actuation_point_low, actuation_point_high, press_sensitivity,
+        release_sensitivity, press_hysteresis, release_hysteresis
+    */
+    uint8_t data[32] = {0};
+    data[0] = RECIEVE_KEY_CONFIG;
+    data[1] = row_num;
+    data[2] = (uint8_t) row_val & 255;
+    data[3] = (uint8_t) row_val >> 8;
+    memcpy(&data[4], key_config, EECONFIG_KEY_DATA_SIZE);
+    raw_hid_send(data, 32);
+}
+
+void raw_hid_send_all_key_configs(void) {
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++){
+        key_config_t unique_configs[MATRIX_COLS];
+        uint8_t unique_count = 0;
+
+        for (key_config_t* key_config_ptr = (key_config_t*)&keys[row][0].config_raw; key_config_ptr <= (key_config_t*)&keys[row][MATRIX_COLS - 1].config_raw; key_config_ptr++) {
+            bool is_unique = true;
+            for (uint8_t i = 0; i < unique_count; i++){
+                if (memcmp(key_config_ptr, &unique_configs[i], EECONFIG_KEY_DATA_SIZE) == 0){
+                    is_unique = false;
+                    break;
+                }
+            }
+            if (is_unique == true){
+                unique_configs[unique_count] = *key_config_ptr;
+                unique_count++;
+            }
+        }
+
+        for (key_config_t* key_config_ptr = &unique_configs[0]; key_config_ptr <= &unique_configs[unique_count - 1]; key_config_ptr++){
+            uint16_t row_val = 0;
+            for (uint8_t col = 0; col < MATRIX_COLS; col++){
+                if (memcmp(key_config_ptr, &keys[row][col].config_raw, EECONFIG_KEY_DATA_SIZE) == 0) {
+                    row_val |= 1 << col;
+                }
+            }
+            raw_hid_send_key_config(row, row_val, key_config_ptr);
+        }
+    }
+}
 
 void raw_hid_receive(uint8_t *data, uint8_t length) {
     report_id_t report_id = data[0];
     switch (report_id) {
         case SEND_KEYBOARD_DATA:;
+            break;
+        case RECIEVE_RGB_SETTINGS:;
+            uint8_t hue   = data[1];
+            uint8_t sat   = data[2];
+            uint8_t val   = data[3];
+            uint8_t mode  = data[4];
+            uint8_t speed = data[5];
+            rgb_matrix_sethsv(hue, sat, val);
+            rgb_matrix_mode(mode);
+            rgb_matrix_set_speed(speed);
             break;
         case RECIEVE_KEY_CONFIG:;
             /* new report structure:
@@ -52,9 +148,18 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
         case BOOTLOADER_JUMP:;
             bootloader_jump();
             break;
-
         case EEPROM_RESET:;
             eeconfig_init();
             reset_keyboard();
+            break;
+        case SEND_RGB_SETTINGS:;
+            raw_hid_send_rgb_settings();
+            break;
+        case SEND_KEY_CONFIG:;
+            raw_hid_send_all_key_configs();
+            break;
+        case SEND_KEY_REBIND:;
+            raw_hid_send_key_rebinds();
+            break;
     }
 }
--- keyboards/pcilabs/pcilabs_common/rawhid.h
+++ keyboards/pcilabs/pcilabs_common/rawhid.h
@@ -1,13 +1,22 @@
 #pragma once
+#include "analogkeys.h"
 #include "stdint.h"
 
 typedef enum {
-    SEND_KEYBOARD_DATA = 1,
+    SEND_KEYBOARD_DATA,
+    RECIEVE_RGB_SETTINGS,
     RECIEVE_KEY_CONFIG,
     RECIEVE_KEY_REBIND,
     BOOTLOADER_JUMP,
     EEPROM_RESET,
-
+    SEND_RGB_SETTINGS,
+    SEND_KEY_CONFIG,
+    SEND_KEY_REBIND
 } report_id_t;
+
+void raw_hid_send_rgb_settings(void);
+void raw_hid_send_key_rebinds(void);
+void raw_hid_send_key_config(uint8_t, uint16_t, key_config_t*);
+void raw_hid_send_all_key_configs(void);
 
 void raw_hid_receive(uint8_t *, uint8_t);
