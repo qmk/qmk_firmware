3df799de92b74ca024f7c1dffaf71491448c1b6f
tnargy <clsscmstng@gmail.com>
2024-08-26 18:15:20 +0000

    added oled into keymap

--- keyboards/crkbd/keymaps/gg/keymap.c
+++ keyboards/crkbd/keymaps/gg/keymap.c
@@ -16,6 +16,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ADJUST] = LAYOUT_split_3x6_3(QK_BOOT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_PAST, KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO, KC_P4, KC_P5, KC_P6, KC_PMNS, KC_PPLS, TG(1), KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_P0, KC_P1, KC_P2, KC_P3, KC_PDOT, KC_NO, KC_ENT, KC_SPC, KC_TRNS, KC_TRNS, KC_SPC, KC_ENT)
 };
 
+#ifdef OLED_ENABLE
+// Rotate OLED
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+	return OLED_ROTATION_90;
+}
+
+// Draw to OLED
+bool oled_task_user() {
+	oled_set_cursor(0,1);
+
+	// oled_write("Hello World!", false);
+	switch (get_highest_layer(layer_state)) {
+		case _DEFAULT :
+			oled_write("Default", false);
+			break;
+		case _GAMING :
+			oled_write("Gaming", false);
+			break;
+		case _NUMBER :
+			oled_write("Numbers", false);
+			break;
+		case _SYMBOL :
+			oled_write("Symbols", false);
+			break;
+		case _ADJUST :
+			oled_write("Adjust", false);
+			break;
+	}
+	oled_set_cursor(0,2);
+	led_t led_state = host_keyboard_led_state();
+	oled_write(led_state.caps_lock ? PSTR("Caps Lock: On") : PSTR("Caps Lock: Off"), false);
+
+	return false;
+}
+#endif
+
 enum combos { 
   J_K_ESC,
   
