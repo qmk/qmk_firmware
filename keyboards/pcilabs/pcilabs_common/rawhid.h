45ade9a8080ca9b47198c238879fbed4cb5d2eac
Timass60 <118626023+Timass60@users.noreply.github.com>
2024-08-26 20:59:22 +0300

    Update rawhid.h

--- keyboards/pcilabs/pcilabs_common/rawhid.h
+++ keyboards/pcilabs/pcilabs_common/rawhid.h
@@ -8,10 +8,10 @@ typedef enum {
     RECIEVE_KEY_CONFIG,
     RECIEVE_KEY_REBIND,
     BOOTLOADER_JUMP,
-    EEPROM_RESET,
-    SEND_RGB_SETTINGS,
-    SEND_KEY_CONFIG,
-    SEND_KEY_REBIND
+    EEPROM_RESET
+    //SEND_RGB_SETTINGS,
+    //SEND_KEY_CONFIG,
+    //SEND_KEY_REBIND
 } report_id_t;
 
 void raw_hid_send_rgb_settings(void);
