91c42764ebd79b5c9dd8f9468d4a2daeec32e295
Idan Kamara <i@idank.me>
2024-08-26 20:44:09 +0300

    keyball: enable extra key

--- keyboards/keyball/keyball44/rules.mk
+++ keyboards/keyball/keyball44/rules.mk
@@ -3,7 +3,7 @@
 
 # Build Options
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
