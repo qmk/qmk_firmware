ddbb4452557321e19dcb3a8b718ade6e5909ca09
Tepphhra <73445393+Tepphhra@users.noreply.github.com>
2024-08-29 03:01:35 +0300

    Add files via upload

---
+++ keyboards/tepphhra/rlycoolmacropad/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 4
+
+#define DIODE_DIRECTION COL2ROW
+
+#define MATRIX_ROW_PINS { B6, E6, B4, B5 }
+#define MATRIX_COL_PINS { C6, D4, D0, D1 }
