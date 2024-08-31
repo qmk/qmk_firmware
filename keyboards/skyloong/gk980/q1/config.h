8fa865a6b6ba4ac56beb5070fc1ae953d968b903
NaturalZh <zhouziran@jikedingzhi.com>
2024-08-31 17:02:59 +0800

    Delete skyloong/gk980/q1/ansi/keymaps/via

--- keyboards/skyloong/gk980/q1/config.h
+++ keyboards/skyloong/gk980/q1/config.h
@@ -5,6 +5,7 @@
 
 #define MAC_PIN C13
 #define DEBOUNCE 10
+#define WIN_LOCK_INDEX 75
 
 #ifdef ENCODER_ENABLE
 #    define ENCODER_MAP_KEY_DELAY 20
@@ -16,12 +17,6 @@
 // Hardware enable lines may be connected to the same pin
 #    define AW20216S_EN_PIN_1 B5
 #    define AW20216S_EN_PIN_2 B5
-
-#    define NUM_LOCK_INDEX 31
-#    define WIN_MOD_INDEX 23
-#    define MAC_MOD_INDEX 24
-#    define WIN_LOCK_INDEX 75
-#    define SCR_LOCK_INDEX 14
 #    define RGB_MATRIX_KEYPRESSES
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #endif // RGB_MATRIX_ENABLE
