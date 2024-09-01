072e2b0224db403b1d94834b4bdadf0fff177e74
takashicompany <t@kashi.company>
2024-09-01 15:28:03 +0900

    rotary encoder

--- keyboards/takashicompany/jourkey/keymaps/default/keymap.c
+++ keyboards/takashicompany/jourkey/keymaps/default/keymap.c
@@ -16,9 +16,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_1, KC_2, KC_3, KC_4, KC_5,
+        KC_6, KC_7, KC_8, KC_9,
+        KC_0, KC_A, KC_B, KC_C
     )
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =   { ENCODER_CCW_CW(KC_X, KC_Y) }
+};
+#endif
