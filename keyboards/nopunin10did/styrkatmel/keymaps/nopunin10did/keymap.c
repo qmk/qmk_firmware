bd950b041a8e13e70d55c41cb214fae859e93c2e
NoPunIn10Did <w.alex.ronke@gmail.com>
2024-09-04 15:53:08 -0400

    Styrka Atmel fixes to compile and readme

--- keyboards/nopunin10did/styrkatmel/keymaps/nopunin10did/keymap.c
+++ keyboards/nopunin10did/styrkatmel/keymaps/nopunin10did/keymap.c
@@ -18,7 +18,7 @@
 
 #define DB_CONT KC_F5
 #define DB_STOP LSFT(KC_F5)
-#define DETACH  LCTL(KC_F5)
+#define DB_DTCH  LCTL(KC_F5)
 #define STEP_IN KC_F11
 #define STEPOUT LSFT(KC_F11)
 #define STEPOVR KC_F10
@@ -34,15 +34,15 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[0] = LAYOUT_default( 
+[0] = LAYOUT( 
     DB_STOP, DB_CONT, QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC, KC_DEL,
-    STEP_IN, DETACH,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
+    STEP_IN, DB_DTCH, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
     STEPOUT, UNXCOPY, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,          KC_ENT,  KC_PGDN,
     STEPOVR, UNXPAST, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LSA_DEL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   FN_CALC,
     KC_INS,  SCRNSHT, KC_RCTL, GUI_F16, ALT_GRV,                            KC_SPC,                             RALTHOM, RGUIEND,          KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-[1] = LAYOUT_default( 
+[1] = LAYOUT( 
     EE_CLR,  KC_NO,   QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_TRNS, KC_TRNS,
     KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS,                   KC_TRNS,
     KC_NO,   KC_NO,   KC_CAPS, KC_TRNS, KC_SCRL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SCRL, KC_TRNS, KC_TRNS, KC_INT3,          KC_TRNS, KC_TRNS,
