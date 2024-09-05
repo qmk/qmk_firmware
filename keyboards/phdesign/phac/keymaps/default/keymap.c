fb10ea00ffaead48a3864474c8fe25fdfa2324b7
nonameCCC <79012391+nonameCCC@users.noreply.github.com>
2024-09-05 17:32:45 +0800

    Detail Improvement

--- keyboards/phdesign/phac/keymaps/default/keymap.c
+++ keyboards/phdesign/phac/keymaps/default/keymap.c
@@ -3,9 +3,33 @@
 
 #include QMK_KEYBOARD_H
 
+// Helpful defines
+#define GRAVE_MODS  (MOD_BIT(KC_LSFT)|MOD_BIT(KC_RSFT)|MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)|MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
+
+enum layer_names {
+    _BL,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_P1,   KC_P2,   KC_P3,   KC_P4,
-        KC_P5,   KC_P6,   KC_P7
+    [_BL] = LAYOUT(
+        KC_S,   KC_D,   KC_K,   KC_L,
+        KC_V,   KC_B,   KC_N
     )
 };
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(QK_MOUSE_CURSOR_UP);
+        } else {
+            tap_code(QK_MOUSE_CURSOR_DOWN);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(QK_MOUSE_CURSOR_LEFT);
+        } else {
+            tap_code(QK_MOUSE_CURSOR_RIGHT);
+        }
+    }
+    return false;
+}
