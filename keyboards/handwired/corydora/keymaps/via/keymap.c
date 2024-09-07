c94275f572d3fe0902e43dac511940302dd1f69a
Balu Babu <balub997@gmail.com>
2024-09-07 06:58:38 +0530

    feat: added via support to CoryDora

--- keyboards/handwired/corydora/keymaps/via/keymap.c
+++ keyboards/handwired/corydora/keymaps/via/keymap.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "timer.h"
+#include "timer.h" 
 
 #define NUM_LAYERS 4           // Constant for the number of layers
 #define DEBOUNCE_TIME 200      // Debounce time in milliseconds
@@ -11,10 +11,10 @@ uint8_t current_layer = 0;     // Tracks the current layer
 uint16_t last_switch_time = 0; // Tracks the last time the layer was switched
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_3x3(KC_3, KC_2, KC_1, KC_6, KC_5, KC_4, KC_9, KC_8, KC_7),
-    [1] = LAYOUT_ortho_3x3(KC_C, KC_B, KC_A, KC_F, KC_E, KC_D, KC_I, KC_H, KC_G),
-    [2] = LAYOUT_ortho_3x3(KC_3, KC_2, KC_1, KC_6, KC_5, KC_4, KC_9, KC_8, KC_7),
-    [3] = LAYOUT_ortho_3x3(KC_C, KC_B, KC_A, KC_F, KC_E, KC_D, KC_I, KC_H, KC_G),
+    [0] = LAYOUT_ortho_3x3(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, LT(1, KC_9)),
+    [1] = LAYOUT_ortho_3x3(KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, LT(0, KC_I)),
+    [2] = LAYOUT_ortho_3x3(KC_A, KC_B, KC_1, KC_D, KC_E, KC_F, KC_G, KC_H, LT(0, KC_I)),
+    [3] = LAYOUT_ortho_3x3(KC_A, KC_B, KC_2, KC_D, KC_E, KC_F, KC_G, KC_H, LT(0, KC_I)),
 };
 
 // OLED
--- keyboards/handwired/corydora/rules.mk
+++ keyboards/handwired/corydora/rules.mk
@@ -3,3 +3,7 @@ OLED_ENABLE = yes
 LTO_ENABLE = yes
 ENCODER_ENABLE = yes
 DIP_SWITCH_ENABLE = yes
+VIA_ENABLE = yes
+
+
+
