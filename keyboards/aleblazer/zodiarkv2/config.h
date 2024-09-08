198438544efea041740fd5ba8c6615629f5e680b
Aleblazer <60912320+Aleblazer@users.noreply.github.com>
2024-09-08 01:10:18 -0500

    Initial work
    Pending new keyboard.json RGB setup.

---
+++ keyboards/aleblazer/zodiarkv2/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
+
+/* RP2040- and hardware-specific config */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
