3ea84b53c2fdef72f4a0d29f6f9e71692b57a1c1
Lefuneste83 <fragueulerauque@gmail.com>
2024-09-08 09:36:07 +0200

    Change review #3

--- keyboards/electronlab/klor/config.h
+++ keyboards/electronlab/klor/config.h
@@ -63,10 +63,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUDIO_DAC_SAMPLE_MAX 4095U
 #define AUDIO_VOICE_DEFAULT butts_fader
 #ifdef AUDIO_ENABLE     
-#    define KLOR_SOUND W__NOTE(_DS0), W__NOTE(_DS1), H__NOTE(_DS2), H__NOTE(_DS3), Q__NOTE(_DS4), Q__NOTE(_DS5), E__NOTE(_DS6), E__NOTE(_DS7), S__NOTE(_DS8), Q__NOTE(_GS0)
-#    define STARTUP_SONG SONG(KLOR_SOUND)
-#    define BYE_SOUND H__NOTE(_DS4), H__NOTE(_DS3), W__NOTE(_DS1)
-#    define GOODBYE_SONG SONG(BYE_SOUND)
+#    define STARTUP_SONG SONG(W__NOTE(_DS0), W__NOTE(_DS1), H__NOTE(_DS2), H__NOTE(_DS3), Q__NOTE(_DS4), Q__NOTE(_DS5), E__NOTE(_DS6), E__NOTE(_DS7), S__NOTE(_DS8), Q__NOTE(_GS0))
+#    define GOODBYE_SONG SONG(H__NOTE(_DS4), H__NOTE(_DS3), W__NOTE(_DS1))
 #    define DEFAULT_LAYER_SONGS \
         { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND) }
 #endif 
