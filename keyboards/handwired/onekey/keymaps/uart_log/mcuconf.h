87034673b7bd1d99b5065cb1bd946d34a7aed8d2
Stefan Kerkmann <karlk90@pm.me>
2024-09-10 18:40:34 +0200

    onekey: uart_log: remove assumption about stm32 platform

--- keyboards/handwired/onekey/keymaps/uart_log/mcuconf.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-#pragma once
-
-#include_next <mcuconf.h>
-
-#undef STM32_SERIAL_USE_USART1
-#define STM32_SERIAL_USE_USART1 TRUE
