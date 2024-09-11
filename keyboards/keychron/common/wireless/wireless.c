99e516fd622f88da00c378296812b0b766e43501
lokher <lokher@gmail.com>
2024-09-11 17:27:07 +0800

    Fixed TAP_HOLD_CAPS_DELAY doens't work with wireless mode

--- keyboards/keychron/common/wireless/wireless.c
+++ keyboards/keychron/common/wireless/wireless.c
@@ -361,6 +361,9 @@ void wireless_send_keyboard(report_keyboard_t *report) {
             report_buffer.type = REPORT_TYPE_KB;
             memcpy(&report_buffer.keyboard, report, sizeof(report_keyboard_t));
             report_buffer_enqueue(&report_buffer);
+
+            if (empty)
+                report_buffer_task();
 #else
             wireless_transport.send_keyboard(&report->mods);
 #endif
@@ -378,10 +381,15 @@ void wireless_send_nkro(report_nkro_t *report) {
     if (wireless_state == WT_CONNECTED || (wireless_state == WT_PARING && pincodeEntry)) {
         if (wireless_transport.send_nkro) {
 #ifndef DISABLE_REPORT_BUFFER
+            bool empty = report_buffer_is_empty();
+
             report_buffer_t report_buffer;
             report_buffer.type = REPORT_TYPE_NKRO;
             memcpy(&report_buffer.nkro, report, sizeof(report_nkro_t));
             report_buffer_enqueue(&report_buffer);
+
+            if (empty)
+                report_buffer_task();
 #else
             wireless_transport.send_nkro(&report->mods);
 #endif
