272b5074944bf4bbd52348485d4e697661df7703
takashicompany <t@kashi.company>
2024-09-12 15:17:33 +0900

    fix keymap

--- keyboards/takashicompany/jourkey/keymaps/default/keymap.c
+++ keyboards/takashicompany/jourkey/keymaps/default/keymap.c
@@ -4,26 +4,15 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
     [0] = LAYOUT(
-        KC_1, KC_2, KC_3, KC_4, KC_5,
-        KC_6, KC_7, KC_8, KC_9,
-        KC_0, KC_A, KC_B, KC_C
+        KC_ESC, KC_1, KC_2, KC_3, KC_4,
+        KC_TAB, KC_Q, KC_W, KC_ENT,
+        KC_LCTL, KC_A, KC_S, KC_RSFT
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] =   { ENCODER_CCW_CW(KC_X, KC_Y) }
+    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) }
 };
 #endif
