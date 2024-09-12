d561d735796db4b2bc28ec03ab910c6115b6d22b
Jim Sangwine <jim@sangwine.net>
2024-09-12 21:54:04 +0100

    Update kyria layout

--- keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
+++ keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  | Nav  |  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Ctrl  | LAlt | LGUI | Bksp | Enter|  | Sym  | Space| RGUI | AltGr| Menu |
+ *                        |Ctrl  | LAlt | LGUI | Bksp | Enter|  | Sym  | Space| Shift| AltGr| Menu |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U   ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_TAB  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J   ,  KC_K ,   KC_L ,KC_SCLN,KC_QUOTE,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, NAV   ,     FKEYS  , KC_RBRC, KC_N,   KC_M   ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                               KC_LCTL , KC_LALT, KC_LGUI, KC_BSPC, KC_ENT,     SYM    , KC_SPC , KC_RGUI,KC_RALT, KC_APP
+                               KC_LCTL , KC_LALT, KC_LGUI, KC_BSPC, KC_ENT,     SYM    , KC_SPC , KC_RSFT,KC_RALT, KC_APP
     ),
 
 /*
