qmk/qmk_firmware

commit 380e0c9cad72ac29f858bef85c8b8eb35b6931f0

Userspace: add support for adding environment variables during build (#22887)

lib/python/qmk/search.py
@@ -1,11 +1,13 @@
 """Functions for searching through QMK keyboards and keymaps.
 """
+from dataclasses import dataclass
 import contextlib
 import functools
 import fnmatch
+import json
 import logging
 import re
-from typing import Callable, List, Optional, Tuple
+from typing import Callable, Dict, List, Optional, Tuple, Union
 from dotty_dict import dotty, Dotty
 from milc import cli
 
@@ -15,7 +17,32 @@ from qmk.keyboard import list_keyboards, keyboard_folder
 from qmk.keymap import list_keymaps, locate_keymap
 from qmk.build_targets import KeyboardKeymapBuildTarget, BuildTarget
 
-TargetInfo = Tuple[str, str, dict]
+
+@dataclass
+class KeyboardKeymapDesc:
+    keyboard: str
+    keymap: str
+    data: dict = None
+    extra_args: dict = None
+
+    def __hash__(self) -> int:
+        return self.keyboard.__hash__() ^ self.keymap.__hash__() ^ json.dumps(self.extra_args, sort_keys=True).__hash__()
+
+    def __lt__(self, other) -> bool:
+        return (self.keyboard, self.keymap, json.dumps(self.extra_args, sort_keys=True)) < (other.keyboard, other.keymap, json.dumps(other.extra_args, sort_keys=True))
+
+    def load_data(self):
+        data = keymap_json(self.keyboard, self.keymap)
+        self.data = data.to_dict() if isinstance(data, Dotty) else data
+
+    @property
+    def dotty(self) -> Dotty:
+        return dotty(self.data) if self.data is not None else None
+
+    def to_build_target(self) -> KeyboardKeymapBuildTarget:
+        target = KeyboardKeymapBuildTarget(keyboard=self.keyboard, keymap=self.keymap, json=self.data)
+        target.extra_args = self.extra_args
+        return target
 
 
 # by using a class for filters, we dont need to worry about capturing values
@@ -36,7 +63,7 @@ class FilterFunction:
     value: Optional[str]
 
     func_name: str
-    apply: Callable[[TargetInfo], bool]
+    apply: Callable[[KeyboardKeymapDesc], bool]
 
     def __init__(self, key, value):
         self.key = key
@@ -46,33 +73,29 @@ class FilterFunction:
 class Exists(FilterFunction):
     func_name = "exists"
 
-    def apply(self, target_info: TargetInfo) -> bool:
-        _kb, _km, info = target_info
-        return self.key in info
+    def apply(self, target_info: KeyboardKeymapDesc) -> bool:
+        return self.key in target_info.data
 
 
 class Absent(FilterFunction):
     func_name = "absent"
 
-    def apply(self, target_info: TargetInfo) -> bool:
-        _kb, _km, info = target_info
-        return self.key not in info
+    def apply(self, target_info: KeyboardKeymapDesc) -> bool:
+        return self.key not in target_info.data
 
 
 class Length(FilterFunction):
     func_name = "length"
 
-    def apply(self, target_info: TargetInfo) -> bool:
-        _kb, _km, info = target_info
-        return (self.key in info and len(info[self.key]) == int(self.value))
+    def apply(self, target_info: KeyboardKeymapDesc) -> bool:
+        return (self.key in target_info.data and len(target_info.data[self.key]) == int(self.value))
 
 
 class Contains(FilterFunction):
     func_name = "contains"
 
-    def apply(self, target_info: TargetInfo) -> bool:
-        _kb, _km, info = target_info
-        return (self.key in info and self.value in info[self.key])
+    def apply(self, target_info: KeyboardKeymapDesc) -> bool:
+        return (self.key in target_info.data and self.value in target_info.data[self.key])
 
 
 def _get_filter_class(func_name: str, key: str, value: str) -> Optional[FilterFunction]:
@@ -109,12 +132,12 @@ def ignore_logging():
     _set_log_level(old)
 
 
-def _all_keymaps(keyboard):
-    """Returns a list of tuples of (keyboard, keymap) for all keymaps for the given keyboard.
+def _all_keymaps(keyboard) -> List[KeyboardKeymapDesc]:
+    """Returns a list of KeyboardKeymapDesc for all keymaps for the given keyboard.
     """
     with ignore_logging():
         keyboard = keyboard_folder(keyboard)
-        return [(keyboard, keymap) for keymap in list_keymaps(keyboard)]
+        return [KeyboardKeymapDesc(keyboard, keymap) for keymap in list_keymaps(keyboard)]
 
 
 def _keymap_exists(keyboard, keymap):
@@ -124,85 +147,91 @@ def _keymap_exists(keyboard, keymap):
         return keyboard if locate_keymap(keyboard, keymap) is not None else None
 
 
-def _load_keymap_info(target: Tuple[str, str]) -> TargetInfo:
-    """Returns a tuple of (keyboard, keymap, info.json) for the given keyboard/keymap combination.
+def _load_keymap_info(target: KeyboardKeymapDesc) -> KeyboardKeymapDesc:
+    """Ensures a KeyboardKeymapDesc has its data loaded.
     """
-    kb, km = target
     with ignore_logging():
-        return (kb, km, keymap_json(kb, km))
+        target.load_data()  # Ensure we load the data first
+        return target
 
 
-def expand_make_targets(targets: List[str]) -> List[Tuple[str, str]]:
-    """Expand a list of make targets into a list of (keyboard, keymap) tuples.
+def expand_make_targets(targets: List[Union[str, Tuple[str, Dict[str, str]]]]) -> List[KeyboardKeymapDesc]:
+    """Expand a list of make targets into a list of KeyboardKeymapDesc.
 
     Caters for 'all' in either keyboard or keymap, or both.
     """
     split_targets = []
     for target in targets:
-        split_target = target.split(':')
+        extra_args = None
+        if isinstance(target, tuple):
+            split_target = target[0].split(':')
+            extra_args = target[1]
+        else:
+            split_target = target.split(':')
         if len(split_target) != 2:
             cli.log.error(f"Invalid build target: {target}")
             return []
-        split_targets.append((split_target[0], split_target[1]))
+        split_targets.append(KeyboardKeymapDesc(split_target[0], split_target[1], extra_args=extra_args))
     return expand_keymap_targets(split_targets)
 
 
-def _expand_keymap_target(keyboard: str, keymap: str, all_keyboards: List[str] = None) -> List[Tuple[str, str]]:
-    """Expand a keyboard input and keymap input into a list of (keyboard, keymap) tuples.
+def _expand_keymap_target(target: KeyboardKeymapDesc, all_keyboards: List[str] = None) -> List[KeyboardKeymapDesc]:
+    """Expand a keyboard input and keymap input into a list of KeyboardKeymapDesc.
 
     Caters for 'all' in either keyboard or keymap, or both.
     """
     if all_keyboards is None:
         all_keyboards = list_keyboards()
 
-    if keyboard == 'all':
-        if keymap == 'all':
+    if target.keyboard == 'all':
+        if target.keymap == 'all':
             cli.log.info('Retrieving list of all keyboards and keymaps...')
             targets = []
             for kb in parallel_map(_all_keymaps, all_keyboards):
                 targets.extend(kb)
+            for t in targets:
+                t.extra_args = target.extra_args
             return targets
         else:
-            cli.log.info(f'Retrieving list of keyboards with keymap "{keymap}"...')
-            keyboard_filter = functools.partial(_keymap_exists, keymap=keymap)
-            return [(kb, keymap) for kb in filter(lambda e: e is not None, parallel_map(keyboard_filter, all_keyboards))]
+            cli.log.info(f'Retrieving list of keyboards with keymap "{target.keymap}"...')
+            keyboard_filter = functools.partial(_keymap_exists, keymap=target.keymap)
+            return [KeyboardKeymapDesc(kb, target.keymap, extra_args=target.extra_args) for kb in filter(lambda e: e is not None, parallel_map(keyboard_filter, all_keyboards))]
     else:
-        if keymap == 'all':
-            cli.log.info(f'Retrieving list of keymaps for keyboard "{keyboard}"...')
-            return _all_keymaps(keyboard)
+        if target.keymap == 'all':
+            cli.log.info(f'Retrieving list of keymaps for keyboard "{target.keyboard}"...')
+            targets = _all_keymaps(target.keyboard)
+            for t in targets:
+                t.extra_args = target.extra_args
+            return targets
         else:
-            return [(keyboard, keymap)]
+            return [target]
 
 
-def expand_keymap_targets(targets: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
-    """Expand a list of (keyboard, keymap) tuples inclusive of 'all', into a list of explicit (keyboard, keymap) tuples.
+def expand_keymap_targets(targets: List[KeyboardKeymapDesc]) -> List[KeyboardKeymapDesc]:
+    """Expand a list of KeyboardKeymapDesc inclusive of 'all', into a list of explicit KeyboardKeymapDesc.
     """
     overall_targets = []
     all_keyboards = list_keyboards()
     for target in targets:
-        overall_targets.extend(_expand_keymap_target(target[0], target[1], all_keyboards))
+        overall_targets.extend(_expand_keymap_target(target, all_keyboards))
     return list(sorted(set(overall_targets)))
 
 
-def _construct_build_target_kb_km(e):
-    return KeyboardKeymapBuildTarget(keyboard=e[0], keymap=e[1])
+def _construct_build_target(e: KeyboardKeymapDesc):
+    return e.to_build_target()
 
 
-def _construct_build_target_kb_km_json(e):
-    return KeyboardKeymapBuildTarget(keyboard=e[0], keymap=e[1], json=e[2])
-
-
-def _filter_keymap_targets(target_list: List[Tuple[str, str]], filters: List[str] = []) -> List[BuildTarget]:
-    """Filter a list of (keyboard, keymap) tuples based on the supplied filters.
+def _filter_keymap_targets(target_list: List[KeyboardKeymapDesc], filters: List[str] = []) -> List[KeyboardKeymapDesc]:
+    """Filter a list of KeyboardKeymapDesc based on the supplied filters.
 
     Optionally includes the values of the queried info.json keys.
     """
     if len(filters) == 0:
         cli.log.info('Preparing target list...')
-        targets = list(set(parallel_map(_construct_build_target_kb_km, target_list)))
+        targets = target_list
     else:
         cli.log.info('Parsing data for all matching keyboard/keymap combinations...')
-        valid_keymaps = [(e[0], e[1], dotty(e[2])) for e in parallel_map(_load_keymap_info, target_list)]
+        valid_targets = parallel_map(_load_keymap_info, target_list)
 
         function_re = re.compile(r'^(?P<function>[a-zA-Z]+)\((?P<key>[a-zA-Z0-9_\.]+)(,\s*(?P<value>[^#]+))?\)$')
         equals_re = re.compile(r'^(?P<key>[a-zA-Z0-9_\.]+)\s*=\s*(?P<value>[^#]+)$')
@@ -220,7 +249,7 @@ def _filter_keymap_targets(target_list: List[Tuple[str, str]], filters: List[str
                 if filter_class is None:
                     cli.log.warning(f'Unrecognized filter expression: {function_match.group(0)}')
                     continue
-                valid_keymaps = filter(filter_class.apply, valid_keymaps)
+                valid_targets = filter(filter_class.apply, valid_targets)
 
                 value_str = f", {{fg_cyan}}{value}{{fg_reset}}" if value is not None else ""
                 cli.log.info(f'Filtering on condition: {{fg_green}}{func_name}{{fg_reset}}({{fg_cyan}}{key}{{fg_reset}}{value_str})...')
@@ -234,32 +263,42 @@ def _filter_keymap_targets(target_list: List[Tuple[str, str]], filters: List[str
                     expr = fnmatch.translate(v)
                     rule = re.compile(f'^{expr}$', re.IGNORECASE)
 
-                    def f(e):
-                        lhs = e[2].get(k)
+                    def f(e: KeyboardKeymapDesc):
+                        lhs = e.dotty.get(k)
                         lhs = str(False if lhs is None else lhs)
                         return rule.search(lhs) is not None
 
                     return f
 
-                valid_keymaps = filter(_make_filter(key, value), valid_keymaps)
+                valid_targets = filter(_make_filter(key, value), valid_targets)
             else:
                 cli.log.warning(f'Unrecognized filter expression: {filter_expr}')
                 continue
 
         cli.log.info('Preparing target list...')
-        valid_keymaps = [(e[0], e[1], e[2].to_dict() if isinstance(e[2], Dotty) else e[2]) for e in valid_keymaps]  # need to convert dotty_dict back to dict because it doesn't survive parallelisation
-        targets = list(set(parallel_map(_construct_build_target_kb_km_json, list(valid_keymaps))))
+        targets = list(sorted(set(valid_targets)))
 
     return targets
 
 
-def search_keymap_targets(targets: List[Tuple[str, str]] = [('all', 'default')], filters: List[str] = []) -> List[BuildTarget]:
+def search_keymap_targets(targets: List[Union[Tuple[str, str], Tuple[str, str, Dict[str, str]]]] = [('all', 'default')], filters: List[str] = []) -> List[BuildTarget]:
     """Search for build targets matching the supplied criteria.
     """
-    return _filter_keymap_targets(expand_keymap_targets(targets), filters)
+    def _make_desc(e):
+        if len(e) == 3:
+            return KeyboardKeymapDesc(keyboard=e[0], keymap=e[1], extra_args=e[2])
+        else:
+            return KeyboardKeymapDesc(keyboard=e[0], keymap=e[1])
+
+    targets = map(_make_desc, targets)
+    targets = _filter_keymap_targets(expand_keymap_targets(targets), filters)
+    targets = list(set(parallel_map(_construct_build_target, list(targets))))
+    return sorted(targets)
 
 
-def search_make_targets(targets: List[str], filters: List[str] = []) -> List[BuildTarget]:
+def search_make_targets(targets: List[Union[str, Tuple[str, Dict[str, str]]]], filters: List[str] = []) -> List[BuildTarget]:
     """Search for build targets matching the supplied criteria.
     """
-    return _filter_keymap_targets(expand_make_targets(targets), filters)
+    targets = _filter_keymap_targets(expand_make_targets(targets), filters)
+    targets = list(set(parallel_map(_construct_build_target, list(targets))))
+    return sorted(targets)

builddefs/build_keyboard.mk
@@ -34,10 +34,16 @@ ifeq ($(strip $(DUMP_CI_METADATA)),yes)
 endif
 
 # Force expansion
-TARGET := $(TARGET)
+override TARGET := $(TARGET)
+$(info TARGET=$(TARGET))
 
 ifneq ($(FORCE_LAYOUT),)
-    TARGET := $(TARGET)_$(FORCE_LAYOUT)
+    override TARGET := $(TARGET)_$(FORCE_LAYOUT)
+    $(info TARGET=$(TARGET))
+endif
+ifneq ($(CONVERT_TO),)
+    override TARGET := $(TARGET)_$(CONVERT_TO)
+    $(info TARGET=$(TARGET))
 endif
 
 # Object files and generated keymap directory
@@ -57,9 +63,6 @@ endif
 ifdef SKIP_GIT
 VERSION_H_FLAGS += --skip-git
 endif
-
-# Generate the board's version.h file.
-$(shell $(QMK_BIN) generate-version-h $(VERSION_H_FLAGS) -q -o $(INTERMEDIATE_OUTPUT)/src/version.h)
 
 # Determine which subfolders exist.
 KEYBOARD_FOLDER_PATH_1 := $(KEYBOARD)
@@ -217,6 +220,9 @@ generated-files: $(INTERMEDIATE_OUTPUT)/src/config.h $(INTERMEDIATE_OUTPUT)/src/
 endif
 
 include $(BUILDDEFS_PATH)/converters.mk
+
+# Generate the board's version.h file.
+$(shell $(QMK_BIN) generate-version-h $(VERSION_H_FLAGS) -q -o $(INTERMEDIATE_OUTPUT)/src/version.h)
 
 MCU_ORIG := $(MCU)
 include $(wildcard $(PLATFORM_PATH)/*/mcu_selection.mk)

builddefs/converters.mk
@@ -32,9 +32,6 @@ ifneq ($(CONVERT_TO),)
 
     PLATFORM_KEY = $(shell echo $(CONVERTER) | cut -d "/" -f2)
 
-    # force setting as value can be from environment
-    override TARGET := $(TARGET)_$(CONVERT_TO)
-
     # Configure any defaults
     OPT_DEFS += -DCONVERT_TO_$(shell echo $(CONVERT_TO) | tr '[:lower:]' '[:upper:]')
     OPT_DEFS += -DCONVERTER_TARGET=\"$(CONVERT_TO)\"

data/schemas/definitions.jsonschema
@@ -16,12 +16,6 @@
         "type": "object",
         "additionalProperties": {"type": "boolean"}
     },
-    "build_target": {
-        "oneOf": [
-            {"$ref": "#/keyboard_keymap_tuple"},
-            {"$ref": "#/json_file_path"}
-        ]
-    },
     "filename": {
         "type": "string",
         "minLength": 1,
@@ -53,6 +47,19 @@
             {"$ref": "#/keyboard"},
             {"$ref": "#/filename"}
         ],
+        "minItems": 2,
+        "maxItems": 2,
+        "unevaluatedItems": false
+    },
+    "keyboard_keymap_env": {
+        "type": "array",
+        "prefixItems": [
+            {"$ref": "#/keyboard"},
+            {"$ref": "#/filename"},
+            {"$ref": "#/kvp_object"}
+        ],
+        "minItems": 3,
+        "maxItems": 3,
         "unevaluatedItems": false
     },
     "keycode": {
@@ -86,6 +93,10 @@
         "minLength": 2,
         "maxLength": 7,
         "pattern": "^[A-Z][A-Zs_0-9]*$"
+    },
+    "kvp_object": {
+        "type": "object",
+        "additionalProperties": {"type": "string"}
     },
     "layout_macro": {
         "oneOf": [

data/schemas/user_repo_v1_1.jsonschema
@@ -1,12 +1,13 @@
 {
     "$schema": "https://json-schema.org/draft/2020-12/schema#",
-    "$id": "qmk.user_repo.v1",
+    "$id": "qmk.user_repo.v1_1",
     "title": "User Repository Information",
     "type": "object",
     "definitions": {
         "build_target": {
             "oneOf": [
                 {"$ref": "qmk.definitions.v1#/keyboard_keymap_tuple"},
+                {"$ref": "qmk.definitions.v1#/keyboard_keymap_env"},
                 {"$ref": "qmk.definitions.v1#/json_file_path"}
             ]
         },
@@ -18,7 +19,7 @@
     "properties": {
         "userspace_version": {
             "type": "string",
-            "enum": ["1.0"]
+            "enum": ["1.1"]
         },
         "build_targets": {
             "type": "array",

lib/python/qmk/build_targets.py
@@ -1,8 +1,8 @@
-# Copyright 2023 Nick Brassel (@tzarc)
+# Copyright 2023-2024 Nick Brassel (@tzarc)
 # SPDX-License-Identifier: GPL-2.0-or-later
 import json
 import shutil
-from typing import List, Union
+from typing import Dict, List, Union
 from pathlib import Path
 from dotty_dict import dotty, Dotty
 from milc import cli
@@ -13,6 +13,9 @@ from qmk.info import keymap_json
 from qmk.keymap import locate_keymap
 from qmk.path import is_under_qmk_firmware, is_under_qmk_userspace
 
+# These must be kept in the order in which they're applied to $(TARGET) in the makefiles in order to ensure consistency.
+TARGET_FILENAME_MODIFIERS = ['FORCE_LAYOUT', 'CONVERT_TO']
+
 
 class BuildTarget:
     def __init__(self, keyboard: str, keymap: str, json: Union[dict, Dotty] = None):
@@ -22,24 +25,24 @@ class BuildTarget:
         self._parallel = 1
         self._clean = False
         self._compiledb = False
-        self._target = f'{self._keyboard_safe}_{self.keymap}'
-        self._intermediate_output = Path(f'{INTERMEDIATE_OUTPUT_PREFIX}{self._target}')
-        self._generated_files_path = self._intermediate_output / 'src'
+        self._extra_args = {}
         self._json = json.to_dict() if isinstance(json, Dotty) else json
 
     def __str__(self):
         return f'{self.keyboard}:{self.keymap}'
 
     def __repr__(self):
+        if len(self._extra_args.items()) > 0:
+            return f'BuildTarget(keyboard={self.keyboard}, keymap={self.keymap}, extra_args={json.dumps(self._extra_args, sort_keys=True)})'
         return f'BuildTarget(keyboard={self.keyboard}, keymap={self.keymap})'
+
+    def __lt__(self, __value: object) -> bool:
+        return self.__repr__() < __value.__repr__()
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, BuildTarget):
             return False
         return self.__repr__() == __value.__repr__()
-
-    def __ne__(self, __value: object) -> bool:
-        return not self.__eq__(__value)
 
     def __hash__(self) -> int:
         return self.__repr__().__hash__()
@@ -72,7 +75,34 @@ class BuildTarget:
     def dotty(self) -> Dotty:
         return dotty(self.json)
 
-    def _common_make_args(self, dry_run: bool = False, build_target: str = None):
+    @property
+    def extra_args(self) -> Dict[str, str]:
+        return {k: v for k, v in self._extra_args.items()}
+
+    @extra_args.setter
+    def extra_args(self, ex_args: Dict[str, str]):
+        if ex_args is not None and isinstance(ex_args, dict):
+            self._extra_args = {k: v for k, v in ex_args.items()}
+
+    def target_name(self, **env_vars) -> str:
+        # Work out the intended target name
+        target = f'{self._keyboard_safe}_{self.keymap}'
+        vars = self._all_vars(**env_vars)
+        for modifier in TARGET_FILENAME_MODIFIERS:
+            if modifier in vars:
+                target += f"_{vars[modifier]}"
+        return target
+
+    def _all_vars(self, **env_vars) -> Dict[str, str]:
+        vars = {k: v for k, v in env_vars.items()}
+        for k, v in self._extra_args.items():
+            vars[k] = v
+        return vars
+
+    def _intermediate_output(self, **env_vars) -> Path:
+        return Path(f'{INTERMEDIATE_OUTPUT_PREFIX}{self.target_name(**env_vars)}')
+
+    def _common_make_args(self, dry_run: bool = False, build_target: str = None, **env_vars):
         compile_args = [
             find_make(),
             *get_make_parallel_args(self._parallel),
@@ -98,13 +128,16 @@ class BuildTarget:
             f'KEYBOARD={self.keyboard}',
             f'KEYMAP={self.keymap}',
             f'KEYBOARD_FILESAFE={self._keyboard_safe}',
-            f'TARGET={self._target}',
-            f'INTERMEDIATE_OUTPUT={self._intermediate_output}',
+            f'TARGET={self._keyboard_safe}_{self.keymap}',  # don't use self.target_name() here, it's rebuilt on the makefile side
             f'VERBOSE={verbose}',
             f'COLOR={color}',
             'SILENT=false',
             'QMK_BIN="qmk"',
         ])
+
+        vars = self._all_vars(**env_vars)
+        for k, v in vars.items():
+            compile_args.append(f'{k}={v}')
 
         return compile_args
 
@@ -150,6 +183,8 @@ class KeyboardKeymapBuildTarget(BuildTarget):
         super().__init__(keyboard=keyboard, keymap=keymap, json=json)
 
     def __repr__(self):
+        if len(self._extra_args.items()) > 0:
+            return f'KeyboardKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap}, extra_args={self._extra_args})'
         return f'KeyboardKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap})'
 
     def _load_json(self):
@@ -159,15 +194,13 @@ class KeyboardKeymapBuildTarget(BuildTarget):
         pass
 
     def compile_command(self, build_target: str = None, dry_run: bool = False, **env_vars) -> List[str]:
-        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target)
-
-        for key, value in env_vars.items():
-            compile_args.append(f'{key}={value}')
+        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target, **env_vars)
 
         # Need to override the keymap path if the keymap is a userspace directory.
         # This also ensures keyboard aliases as per `keyboard_aliases.hjson` still work if the userspace has the keymap
         # in an equivalent historical location.
-        keymap_location = locate_keymap(self.keyboard, self.keymap)
+        vars = self._all_vars(**env_vars)
+        keymap_location = locate_keymap(self.keyboard, self.keymap, force_layout=vars.get('FORCE_LAYOUT'))
         if is_under_qmk_userspace(keymap_location) and not is_under_qmk_firmware(keymap_location):
             keymap_directory = keymap_location.parent
             compile_args.extend([
@@ -196,47 +229,51 @@ class JsonKeymapBuildTarget(BuildTarget):
 
         super().__init__(keyboard=json['keyboard'], keymap=json['keymap'], json=json)
 
-        self._keymap_json = self._generated_files_path / 'keymap.json'
-
     def __repr__(self):
+        if len(self._extra_args.items()) > 0:
+            return f'JsonKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap}, path={self.json_path}, extra_args={self._extra_args})'
         return f'JsonKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap}, path={self.json_path})'
 
     def _load_json(self):
         pass  # Already loaded in constructor
 
     def prepare_build(self, build_target: str = None, dry_run: bool = False, **env_vars) -> None:
+        intermediate_output = self._intermediate_output(**env_vars)
+        generated_files_path = intermediate_output / 'src'
+        keymap_json = generated_files_path / 'keymap.json'
+
         if self._clean:
-            if self._intermediate_output.exists():
-                shutil.rmtree(self._intermediate_output)
+            if intermediate_output.exists():
+                shutil.rmtree(intermediate_output)
 
         # begin with making the deepest folder in the tree
-        self._generated_files_path.mkdir(exist_ok=True, parents=True)
+        generated_files_path.mkdir(exist_ok=True, parents=True)
 
         # Compare minified to ensure consistent comparison
         new_content = json.dumps(self.json, separators=(',', ':'))
-        if self._keymap_json.exists():
-            old_content = json.dumps(json.loads(self._keymap_json.read_text(encoding='utf-8')), separators=(',', ':'))
+        if keymap_json.exists():
+            old_content = json.dumps(json.loads(keymap_json.read_text(encoding='utf-8')), separators=(',', ':'))
             if old_content == new_content:
                 new_content = None
 
         # Write the keymap.json file if different so timestamps are only updated
         # if the content changes -- running `make` won't treat it as modified.
         if new_content:
-            self._keymap_json.write_text(new_content, encoding='utf-8')
+            keymap_json.write_text(new_content, encoding='utf-8')
 
     def compile_command(self, build_target: str = None, dry_run: bool = False, **env_vars) -> List[str]:
-        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target)
+        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target, **env_vars)
+        intermediate_output = self._intermediate_output(**env_vars)
+        generated_files_path = intermediate_output / 'src'
+        keymap_json = generated_files_path / 'keymap.json'
         compile_args.extend([
-            f'MAIN_KEYMAP_PATH_1={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_2={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_3={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_4={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_5={self._intermediate_output}',
-            f'KEYMAP_JSON={self._keymap_json}',
-            f'KEYMAP_PATH={self._generated_files_path}',
+            f'MAIN_KEYMAP_PATH_1={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_2={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_3={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_4={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_5={intermediate_output}',
+            f'KEYMAP_JSON={keymap_json}',
+            f'KEYMAP_PATH={generated_files_path}',
         ])
-
-        for key, value in env_vars.items():
-            compile_args.append(f'{key}={value}')
 
         return compile_args

lib/python/qmk/cli/format/json.py
@@ -18,10 +18,17 @@ def _detect_json_format(file, json_data):
     """
     json_encoder = None
     try:
-        validate(json_data, 'qmk.user_repo.v1')
+        validate(json_data, 'qmk.user_repo.v1_1')
         json_encoder = UserspaceJSONEncoder
     except ValidationError:
         pass
+
+    if json_encoder is None:
+        try:
+            validate(json_data, 'qmk.user_repo.v1')
+            json_encoder = UserspaceJSONEncoder
+        except ValidationError:
+            pass
 
     if json_encoder is None:
         try:

commit bdb07ca2ac8cc5ee46ba29d31f3533c8ae1048ce

`qmk find`: Fix handling of keys with dots in filter functions
Filters like `-f "exists(rgb_matrix.split_count)"` broke after #22887,
because a plain dict was used instead of the `dotty` wrapper, therefore
only keys with a single component worked.  Fix the implementation of
filtering functions to use the `dotty` wrapper properly, so that keys
with dots could be used in functions again.

lib/python/qmk/search.py
@@ -74,28 +74,30 @@ class Exists(FilterFunction):
     func_name = "exists"
 
     def apply(self, target_info: KeyboardKeymapDesc) -> bool:
-        return self.key in target_info.data
+        return self.key in target_info.dotty
 
 
 class Absent(FilterFunction):
     func_name = "absent"
 
     def apply(self, target_info: KeyboardKeymapDesc) -> bool:
-        return self.key not in target_info.data
+        return self.key not in target_info.dotty
 
 
 class Length(FilterFunction):
     func_name = "length"
 
     def apply(self, target_info: KeyboardKeymapDesc) -> bool:
-        return (self.key in target_info.data and len(target_info.data[self.key]) == int(self.value))
+        info_dotty = target_info.dotty
+        return (self.key in info_dotty and len(info_dotty[self.key]) == int(self.value))
 
 
 class Contains(FilterFunction):
     func_name = "contains"
 
     def apply(self, target_info: KeyboardKeymapDesc) -> bool:
-        return (self.key in target_info.data and self.value in target_info.data[self.key])
+        info_dotty = target_info.dotty
+        return (self.key in info_dotty and self.value in info_dotty[self.key])
 
 
 def _get_filter_class(func_name: str, key: str, value: str) -> Optional[FilterFunction]: