qmk/qmk_firmware

commit 380e0c9cad72ac29f858bef85c8b8eb35b6931f0

Userspace: add support for adding environment variables during build (#22887)

builddefs/build_keyboard.mk
@@ -34,10 +34,16 @@ ifeq ($(strip $(DUMP_CI_METADATA)),yes)
 endif
 
 # Force expansion
-TARGET := $(TARGET)
+override TARGET := $(TARGET)
+$(info TARGET=$(TARGET))
 
 ifneq ($(FORCE_LAYOUT),)
-    TARGET := $(TARGET)_$(FORCE_LAYOUT)
+    override TARGET := $(TARGET)_$(FORCE_LAYOUT)
+    $(info TARGET=$(TARGET))
+endif
+ifneq ($(CONVERT_TO),)
+    override TARGET := $(TARGET)_$(CONVERT_TO)
+    $(info TARGET=$(TARGET))
 endif
 
 # Object files and generated keymap directory
@@ -57,9 +63,6 @@ endif
 ifdef SKIP_GIT
 VERSION_H_FLAGS += --skip-git
 endif
-
-# Generate the board's version.h file.
-$(shell $(QMK_BIN) generate-version-h $(VERSION_H_FLAGS) -q -o $(INTERMEDIATE_OUTPUT)/src/version.h)
 
 # Determine which subfolders exist.
 KEYBOARD_FOLDER_PATH_1 := $(KEYBOARD)
@@ -217,6 +220,9 @@ generated-files: $(INTERMEDIATE_OUTPUT)/src/config.h $(INTERMEDIATE_OUTPUT)/src/
 endif
 
 include $(BUILDDEFS_PATH)/converters.mk
+
+# Generate the board's version.h file.
+$(shell $(QMK_BIN) generate-version-h $(VERSION_H_FLAGS) -q -o $(INTERMEDIATE_OUTPUT)/src/version.h)
 
 MCU_ORIG := $(MCU)
 include $(wildcard $(PLATFORM_PATH)/*/mcu_selection.mk)

builddefs/converters.mk
@@ -32,9 +32,6 @@ ifneq ($(CONVERT_TO),)
 
     PLATFORM_KEY = $(shell echo $(CONVERTER) | cut -d "/" -f2)
 
-    # force setting as value can be from environment
-    override TARGET := $(TARGET)_$(CONVERT_TO)
-
     # Configure any defaults
     OPT_DEFS += -DCONVERT_TO_$(shell echo $(CONVERT_TO) | tr '[:lower:]' '[:upper:]')
     OPT_DEFS += -DCONVERTER_TARGET=\"$(CONVERT_TO)\"

data/schemas/definitions.jsonschema
@@ -16,12 +16,6 @@
         "type": "object",
         "additionalProperties": {"type": "boolean"}
     },
-    "build_target": {
-        "oneOf": [
-            {"$ref": "#/keyboard_keymap_tuple"},
-            {"$ref": "#/json_file_path"}
-        ]
-    },
     "filename": {
         "type": "string",
         "minLength": 1,
@@ -53,6 +47,19 @@
             {"$ref": "#/keyboard"},
             {"$ref": "#/filename"}
         ],
+        "minItems": 2,
+        "maxItems": 2,
+        "unevaluatedItems": false
+    },
+    "keyboard_keymap_env": {
+        "type": "array",
+        "prefixItems": [
+            {"$ref": "#/keyboard"},
+            {"$ref": "#/filename"},
+            {"$ref": "#/kvp_object"}
+        ],
+        "minItems": 3,
+        "maxItems": 3,
         "unevaluatedItems": false
     },
     "keycode": {
@@ -86,6 +93,10 @@
         "minLength": 2,
         "maxLength": 7,
         "pattern": "^[A-Z][A-Zs_0-9]*$"
+    },
+    "kvp_object": {
+        "type": "object",
+        "additionalProperties": {"type": "string"}
     },
     "layout_macro": {
         "oneOf": [

data/schemas/user_repo_v1_1.jsonschema
@@ -1,12 +1,13 @@
 {
     "$schema": "https://json-schema.org/draft/2020-12/schema#",
-    "$id": "qmk.user_repo.v1",
+    "$id": "qmk.user_repo.v1_1",
     "title": "User Repository Information",
     "type": "object",
     "definitions": {
         "build_target": {
             "oneOf": [
                 {"$ref": "qmk.definitions.v1#/keyboard_keymap_tuple"},
+                {"$ref": "qmk.definitions.v1#/keyboard_keymap_env"},
                 {"$ref": "qmk.definitions.v1#/json_file_path"}
             ]
         },
@@ -18,7 +19,7 @@
     "properties": {
         "userspace_version": {
             "type": "string",
-            "enum": ["1.0"]
+            "enum": ["1.1"]
         },
         "build_targets": {
             "type": "array",

lib/python/qmk/build_targets.py
@@ -1,8 +1,8 @@
-# Copyright 2023 Nick Brassel (@tzarc)
+# Copyright 2023-2024 Nick Brassel (@tzarc)
 # SPDX-License-Identifier: GPL-2.0-or-later
 import json
 import shutil
-from typing import List, Union
+from typing import Dict, List, Union
 from pathlib import Path
 from dotty_dict import dotty, Dotty
 from milc import cli
@@ -13,6 +13,9 @@ from qmk.info import keymap_json
 from qmk.keymap import locate_keymap
 from qmk.path import is_under_qmk_firmware, is_under_qmk_userspace
 
+# These must be kept in the order in which they're applied to $(TARGET) in the makefiles in order to ensure consistency.
+TARGET_FILENAME_MODIFIERS = ['FORCE_LAYOUT', 'CONVERT_TO']
+
 
 class BuildTarget:
     def __init__(self, keyboard: str, keymap: str, json: Union[dict, Dotty] = None):
@@ -22,24 +25,24 @@ class BuildTarget:
         self._parallel = 1
         self._clean = False
         self._compiledb = False
-        self._target = f'{self._keyboard_safe}_{self.keymap}'
-        self._intermediate_output = Path(f'{INTERMEDIATE_OUTPUT_PREFIX}{self._target}')
-        self._generated_files_path = self._intermediate_output / 'src'
+        self._extra_args = {}
         self._json = json.to_dict() if isinstance(json, Dotty) else json
 
     def __str__(self):
         return f'{self.keyboard}:{self.keymap}'
 
     def __repr__(self):
+        if len(self._extra_args.items()) > 0:
+            return f'BuildTarget(keyboard={self.keyboard}, keymap={self.keymap}, extra_args={json.dumps(self._extra_args, sort_keys=True)})'
         return f'BuildTarget(keyboard={self.keyboard}, keymap={self.keymap})'
+
+    def __lt__(self, __value: object) -> bool:
+        return self.__repr__() < __value.__repr__()
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, BuildTarget):
             return False
         return self.__repr__() == __value.__repr__()
-
-    def __ne__(self, __value: object) -> bool:
-        return not self.__eq__(__value)
 
     def __hash__(self) -> int:
         return self.__repr__().__hash__()
@@ -72,7 +75,34 @@ class BuildTarget:
     def dotty(self) -> Dotty:
         return dotty(self.json)
 
-    def _common_make_args(self, dry_run: bool = False, build_target: str = None):
+    @property
+    def extra_args(self) -> Dict[str, str]:
+        return {k: v for k, v in self._extra_args.items()}
+
+    @extra_args.setter
+    def extra_args(self, ex_args: Dict[str, str]):
+        if ex_args is not None and isinstance(ex_args, dict):
+            self._extra_args = {k: v for k, v in ex_args.items()}
+
+    def target_name(self, **env_vars) -> str:
+        # Work out the intended target name
+        target = f'{self._keyboard_safe}_{self.keymap}'
+        vars = self._all_vars(**env_vars)
+        for modifier in TARGET_FILENAME_MODIFIERS:
+            if modifier in vars:
+                target += f"_{vars[modifier]}"
+        return target
+
+    def _all_vars(self, **env_vars) -> Dict[str, str]:
+        vars = {k: v for k, v in env_vars.items()}
+        for k, v in self._extra_args.items():
+            vars[k] = v
+        return vars
+
+    def _intermediate_output(self, **env_vars) -> Path:
+        return Path(f'{INTERMEDIATE_OUTPUT_PREFIX}{self.target_name(**env_vars)}')
+
+    def _common_make_args(self, dry_run: bool = False, build_target: str = None, **env_vars):
         compile_args = [
             find_make(),
             *get_make_parallel_args(self._parallel),
@@ -98,13 +128,16 @@ class BuildTarget:
             f'KEYBOARD={self.keyboard}',
             f'KEYMAP={self.keymap}',
             f'KEYBOARD_FILESAFE={self._keyboard_safe}',
-            f'TARGET={self._target}',
-            f'INTERMEDIATE_OUTPUT={self._intermediate_output}',
+            f'TARGET={self._keyboard_safe}_{self.keymap}',  # don't use self.target_name() here, it's rebuilt on the makefile side
             f'VERBOSE={verbose}',
             f'COLOR={color}',
             'SILENT=false',
             'QMK_BIN="qmk"',
         ])
+
+        vars = self._all_vars(**env_vars)
+        for k, v in vars.items():
+            compile_args.append(f'{k}={v}')
 
         return compile_args
 
@@ -150,6 +183,8 @@ class KeyboardKeymapBuildTarget(BuildTarget):
         super().__init__(keyboard=keyboard, keymap=keymap, json=json)
 
     def __repr__(self):
+        if len(self._extra_args.items()) > 0:
+            return f'KeyboardKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap}, extra_args={self._extra_args})'
         return f'KeyboardKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap})'
 
     def _load_json(self):
@@ -159,15 +194,13 @@ class KeyboardKeymapBuildTarget(BuildTarget):
         pass
 
     def compile_command(self, build_target: str = None, dry_run: bool = False, **env_vars) -> List[str]:
-        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target)
-
-        for key, value in env_vars.items():
-            compile_args.append(f'{key}={value}')
+        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target, **env_vars)
 
         # Need to override the keymap path if the keymap is a userspace directory.
         # This also ensures keyboard aliases as per `keyboard_aliases.hjson` still work if the userspace has the keymap
         # in an equivalent historical location.
-        keymap_location = locate_keymap(self.keyboard, self.keymap)
+        vars = self._all_vars(**env_vars)
+        keymap_location = locate_keymap(self.keyboard, self.keymap, force_layout=vars.get('FORCE_LAYOUT'))
         if is_under_qmk_userspace(keymap_location) and not is_under_qmk_firmware(keymap_location):
             keymap_directory = keymap_location.parent
             compile_args.extend([
@@ -196,47 +229,51 @@ class JsonKeymapBuildTarget(BuildTarget):
 
         super().__init__(keyboard=json['keyboard'], keymap=json['keymap'], json=json)
 
-        self._keymap_json = self._generated_files_path / 'keymap.json'
-
     def __repr__(self):
+        if len(self._extra_args.items()) > 0:
+            return f'JsonKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap}, path={self.json_path}, extra_args={self._extra_args})'
         return f'JsonKeymapTarget(keyboard={self.keyboard}, keymap={self.keymap}, path={self.json_path})'
 
     def _load_json(self):
         pass  # Already loaded in constructor
 
     def prepare_build(self, build_target: str = None, dry_run: bool = False, **env_vars) -> None:
+        intermediate_output = self._intermediate_output(**env_vars)
+        generated_files_path = intermediate_output / 'src'
+        keymap_json = generated_files_path / 'keymap.json'
+
         if self._clean:
-            if self._intermediate_output.exists():
-                shutil.rmtree(self._intermediate_output)
+            if intermediate_output.exists():
+                shutil.rmtree(intermediate_output)
 
         # begin with making the deepest folder in the tree
-        self._generated_files_path.mkdir(exist_ok=True, parents=True)
+        generated_files_path.mkdir(exist_ok=True, parents=True)
 
         # Compare minified to ensure consistent comparison
         new_content = json.dumps(self.json, separators=(',', ':'))
-        if self._keymap_json.exists():
-            old_content = json.dumps(json.loads(self._keymap_json.read_text(encoding='utf-8')), separators=(',', ':'))
+        if keymap_json.exists():
+            old_content = json.dumps(json.loads(keymap_json.read_text(encoding='utf-8')), separators=(',', ':'))
             if old_content == new_content:
                 new_content = None
 
         # Write the keymap.json file if different so timestamps are only updated
         # if the content changes -- running `make` won't treat it as modified.
         if new_content:
-            self._keymap_json.write_text(new_content, encoding='utf-8')
+            keymap_json.write_text(new_content, encoding='utf-8')
 
     def compile_command(self, build_target: str = None, dry_run: bool = False, **env_vars) -> List[str]:
-        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target)
+        compile_args = self._common_make_args(dry_run=dry_run, build_target=build_target, **env_vars)
+        intermediate_output = self._intermediate_output(**env_vars)
+        generated_files_path = intermediate_output / 'src'
+        keymap_json = generated_files_path / 'keymap.json'
         compile_args.extend([
-            f'MAIN_KEYMAP_PATH_1={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_2={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_3={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_4={self._intermediate_output}',
-            f'MAIN_KEYMAP_PATH_5={self._intermediate_output}',
-            f'KEYMAP_JSON={self._keymap_json}',
-            f'KEYMAP_PATH={self._generated_files_path}',
+            f'MAIN_KEYMAP_PATH_1={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_2={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_3={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_4={intermediate_output}',
+            f'MAIN_KEYMAP_PATH_5={intermediate_output}',
+            f'KEYMAP_JSON={keymap_json}',
+            f'KEYMAP_PATH={generated_files_path}',
         ])
-
-        for key, value in env_vars.items():
-            compile_args.append(f'{key}={value}')
 
         return compile_args

lib/python/qmk/cli/format/json.py
@@ -18,10 +18,17 @@ def _detect_json_format(file, json_data):
     """
     json_encoder = None
     try:
-        validate(json_data, 'qmk.user_repo.v1')
+        validate(json_data, 'qmk.user_repo.v1_1')
         json_encoder = UserspaceJSONEncoder
     except ValidationError:
         pass
+
+    if json_encoder is None:
+        try:
+            validate(json_data, 'qmk.user_repo.v1')
+            json_encoder = UserspaceJSONEncoder
+        except ValidationError:
+            pass
 
     if json_encoder is None:
         try:

lib/python/qmk/cli/mass_compile.py
@@ -7,6 +7,7 @@ from typing import List
 from pathlib import Path
 from subprocess import DEVNULL
 from milc import cli
+import shlex
 
 from qmk.constants import QMK_FIRMWARE
 from qmk.commands import find_make, get_make_parallel_args, build_environment
@@ -26,7 +27,8 @@ def mass_compile_targets(targets: List[BuildTarget], clean: bool, dry_run: bool,
     if dry_run:
         cli.log.info('Compilation targets:')
         for target in sorted(targets, key=lambda t: (t.keyboard, t.keymap)):
-            cli.log.info(f"{{fg_cyan}}qmk compile -kb {target.keyboard} -km {target.keymap}{{fg_reset}}")
+            extra_args = ' '.join([f"-e {shlex.quote(f'{k}={v}')}" for k, v in target.extra_args.items()])
+            cli.log.info(f"{{fg_cyan}}qmk compile -kb {target.keyboard} -km {target.keymap} {extra_args}{{fg_reset}}")
     else:
         if clean:
             cli.run([make_cmd, 'clean'], capture_output=False, stdin=DEVNULL)
@@ -36,18 +38,26 @@ def mass_compile_targets(targets: List[BuildTarget], clean: bool, dry_run: bool,
             for target in sorted(targets, key=lambda t: (t.keyboard, t.keymap)):
                 keyboard_name = target.keyboard
                 keymap_name = target.keymap
+                keyboard_safe = keyboard_name.replace('/', '_')
+                target_filename = target.target_name(**env)
                 target.configure(parallel=1)  # We ignore parallelism on a per-build basis as we defer to the parent make invocation
                 target.prepare_build(**env)  # If we've got json targets, allow them to write out any extra info to .build before we kick off `make`
                 command = target.compile_command(**env)
                 command[0] = '+@$(MAKE)'  # Override the make so that we can use jobserver to handle parallelism
-                keyboard_safe = keyboard_name.replace('/', '_')
+                extra_args = '_'.join([f"{k}_{v}" for k, v in target.extra_args.items()])
                 build_log = f"{QMK_FIRMWARE}/.build/build.log.{os.getpid()}.{keyboard_safe}.{keymap_name}"
                 failed_log = f"{QMK_FIRMWARE}/.build/failed.log.{os.getpid()}.{keyboard_safe}.{keymap_name}"
+                target_suffix = ''
+                if len(extra_args) > 0:
+                    build_log += f".{extra_args}"
+                    failed_log += f".{extra_args}"
+                    target_suffix = f"_{extra_args}"
                 # yapf: disable
                 f.write(
                     f"""\
-all: {keyboard_safe}_{keymap_name}_binary
-{keyboard_safe}_{keymap_name}_binary:
+.PHONY: {target_filename}{target_suffix}_binary
+all: {target_filename}{target_suffix}_binary
+{target_filename}{target_suffix}_binary:
 	@rm -f "{build_log}" || true
 	@echo "Compiling QMK Firmware for target: '{keyboard_name}:{keymap_name}'..." >>"{build_log}"
 	{' '.join(command)} \\
@@ -65,9 +75,9 @@ all: {keyboard_safe}_{keymap_name}_binary
                     # yapf: disable
                     f.write(
                         f"""\
-	@rm -rf "{QMK_FIRMWARE}/.build/{keyboard_safe}_{keymap_name}.elf" 2>/dev/null || true
-	@rm -rf "{QMK_FIRMWARE}/.build/{keyboard_safe}_{keymap_name}.map" 2>/dev/null || true
-	@rm -rf "{QMK_FIRMWARE}/.build/obj_{keyboard_safe}_{keymap_name}" || true
+	@rm -rf "{QMK_FIRMWARE}/.build/{target_filename}.elf" 2>/dev/null || true
+	@rm -rf "{QMK_FIRMWARE}/.build/{target_filename}.map" 2>/dev/null || true
+	@rm -rf "{QMK_FIRMWARE}/.build/obj_{target_filename}" || true
 """# noqa
                     )
                     # yapf: enable

lib/python/qmk/cli/userspace/add.py
@@ -1,8 +1,9 @@
-# Copyright 2023 Nick Brassel (@tzarc)
+# Copyright 2023-2024 Nick Brassel (@tzarc)
 # SPDX-License-Identifier: GPL-2.0-or-later
 from pathlib import Path
 from milc import cli
 
+from qmk.commands import parse_env_vars
 from qmk.constants import QMK_USERSPACE, HAS_QMK_USERSPACE
 from qmk.keyboard import keyboard_completer, keyboard_folder_or_all
 from qmk.keymap import keymap_completer, is_keymap_target
@@ -12,11 +13,14 @@ from qmk.userspace import UserspaceDefs
 @cli.argument('builds', nargs='*', arg_only=True, help="List of builds in form <keyboard>:<keymap>, or path to a keymap JSON file.")
 @cli.argument('-kb', '--keyboard', type=keyboard_folder_or_all, completer=keyboard_completer, help='The keyboard to build a firmware for. Ignored when a configurator export is supplied.')
 @cli.argument('-km', '--keymap', completer=keymap_completer, help='The keymap to build a firmware for. Ignored when a configurator export is supplied.')
+@cli.argument('-e', '--env', arg_only=True, action='append', default=[], help="Extra variables to set during build. May be passed multiple times.")
 @cli.subcommand('Adds a build target to userspace `qmk.json`.')
 def userspace_add(cli):
     if not HAS_QMK_USERSPACE:
         cli.log.error('Could not determine QMK userspace location. Please run `qmk doctor` or `qmk userspace-doctor` to diagnose.')
         return False
+
+    build_env = None if len(cli.args.env) == 0 else parse_env_vars(cli.args.env)
 
     userspace = UserspaceDefs(QMK_USERSPACE / 'qmk.json')
 
@@ -44,8 +48,8 @@ def userspace_add(cli):
             cli.config.new_keymap.keyboard = cli.args.keyboard
             cli.config.new_keymap.keymap = cli.args.keymap
             if new_keymap(cli) is not False:
-                userspace.add_target(keyboard=cli.args.keyboard, keymap=cli.args.keymap)
+                userspace.add_target(keyboard=cli.args.keyboard, keymap=cli.args.keymap, build_env=build_env)
         else:
-            userspace.add_target(keyboard=cli.args.keyboard, keymap=cli.args.keymap)
+            userspace.add_target(keyboard=cli.args.keyboard, keymap=cli.args.keymap, build_env=build_env)
 
     return userspace.save()

commit 262c116ed352a5cae41bcd95ffab5e5f7fe17c34

`qmk find`: Add tests
Add tests which should have caught the breakage from #22887.

lib/python/qmk/tests/test_cli_commands.py
@@ -347,3 +347,68 @@ def test_format_json_keymap_auto():
     result = check_subcommand('format-json', '--format', 'auto', 'lib/python/qmk/tests/minimal_keymap.json')
     check_returncode(result)
     assert result.stdout == '{\n    "keyboard": "handwired/pytest/basic",\n    "keymap": "test",\n    "layers": [\n        ["KC_A"]\n    ],\n    "layout": "LAYOUT_ortho_1x1",\n    "version": 1\n}\n'
+
+
+def test_find_exists():
+    result = check_subcommand('find', '-f', 'exists(rgb_matrix.split_count)', '-p', 'rgb_matrix.split_count')
+    check_returncode(result)
+    values = [s for s in result.stdout.splitlines() if 'rgb_matrix.split_count=' in s]
+    assert len(values) > 0
+    for s in values:
+        assert '=None' not in s
+        assert '=[' in s
+
+
+def test_find_absent():
+    result = check_subcommand('find', '-f', 'absent(rgb_matrix.split_count)', '-p', 'rgb_matrix.split_count')
+    check_returncode(result)
+    values = [s for s in result.stdout.splitlines() if 'rgb_matrix.split_count=' in s]
+    assert len(values) > 0
+    for s in values:
+        assert '=None' in s
+        assert '=[' not in s
+
+
+def test_find_length():
+    result = check_subcommand('find', '-f', 'length(matrix_pins.cols, 6)', '-p', 'matrix_pins.cols')
+    check_returncode(result)
+    values = [s for s in result.stdout.splitlines() if 'matrix_pins.cols=' in s]
+    assert len(values) > 0
+    for s in values:
+        assert s.count(',') == 5
+
+
+def test_find_contains():
+    result = check_subcommand('find', '-f', 'contains(matrix_pins.cols, B1)', '-p', 'matrix_pins.cols')
+    check_returncode(result)
+    values = [s for s in result.stdout.splitlines() if 'matrix_pins.cols=' in s]
+    assert len(values) > 0
+    for s in values:
+        assert "'B1'" in s
+
+
+def test_find_multiple_conditions():
+    # this is intended to match at least 'crkbd/rev1'
+    result = check_subcommand(
+        'find', '-f', 'exists(rgb_matrix.split_count)', '-f', 'contains(matrix_pins.cols, B1)', '-f', 'length(matrix_pins.cols, 6)', '-f', 'absent(eeprom.driver)', '-f', 'ws2812.pin=D3', '-p', 'rgb_matrix.split_count', '-p', 'matrix_pins.cols', '-p',
+        'eeprom.driver', '-p', 'ws2812.pin'
+    )
+    check_returncode(result)
+    rgb_matrix_split_count_values = [s for s in result.stdout.splitlines() if 'rgb_matrix.split_count=' in s]
+    assert len(rgb_matrix_split_count_values) > 0
+    for s in rgb_matrix_split_count_values:
+        assert '=None' not in s
+        assert '=[' in s
+    matrix_pins_cols_values = [s for s in result.stdout.splitlines() if 'matrix_pins.cols=' in s]
+    assert len(matrix_pins_cols_values) > 0
+    for s in matrix_pins_cols_values:
+        assert s.count(',') == 5
+        assert "'B1'" in s
+    eeprom_driver_values = [s for s in result.stdout.splitlines() if 'eeprom.driver=' in s]
+    assert len(eeprom_driver_values) > 0
+    for s in eeprom_driver_values:
+        assert '=None' in s
+    ws2812_pin_values = [s for s in result.stdout.splitlines() if 'ws2812.pin=' in s]
+    assert len(ws2812_pin_values) > 0
+    for s in ws2812_pin_values:
+        assert '=D3' in s