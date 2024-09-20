ea234945935cbfe9825807c360bc7149fdc205ad
--get <95505981+ekamtaneja@users.noreply.github.com>
2024-09-20 13:01:54 -0700

    Finalized led animations

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/ekamtaneja/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/ekamtaneja/keymap.c
@@ -27,39 +27,36 @@ td_state_t cur_dance(tap_dance_state_t *state);
 
 void fn_finished(tap_dance_state_t *state, void *user_data);
 void fn_reset(tap_dance_state_t *state, void *user_data);
-   
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    for (uint8_t i = led_min; i < led_max; i++) {
-        switch(get_highest_layer(layer_state|default_layer_state)) {
-            case _DEF:
-                rgb_matrix_mode(RGB_MATRIX_CYCLE_ALL);
-                break;
-            case _GAM:
-                rgb_matrix_mode(RGB_MATRIX_RAINBOW_MOVING_CHEVRON);
-                break;
-            case _ENG:
-                rgb_matrix_mode(RGB_MATRIX_CYCLE_OUT_IN_DUAL);
-                break;
-            case _FN:
-                rgb_matrix_mode(RGB_MATRIX_PIXEL_FRACTAL);
-                break;
-            case _NUM:
-                rgb_matrix_mode(RGB_MATRIX_JELLYBEAN_RAINDROPS);
-                break;
-            default:
-                rgb_matrix_mode(RGB_MATRIX_CYCLE_ALL);
-                break;
-        }
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case _NUM:
+            rgb_matrix_mode(RGB_MATRIX_PIXEL_RAIN);
+            break;
+        case _FN:
+            rgb_matrix_mode(RGB_MATRIX_JELLYBEAN_RAINDROPS);
+            break;
+        case _ENG:
+            rgb_matrix_mode(RGB_MATRIX_RIVERFLOW);
+            break;
+        case _GAM:
+            rgb_matrix_mode(RGB_MATRIX_MULTISPLASH);
+            break;
+        case _DEF:
+            rgb_matrix_mode(RGB_MATRIX_CYCLE_ALL);
+            break;
+        default:
+            rgb_matrix_mode(RGB_MATRIX_CYCLE_ALL);
+            break;
     }
-    return false;
+    return state;
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEF] = LAYOUT_65_ansi_blocker( /* Base */
 		QK_GESC,    KC_1,    KC_2,          KC_3,          KC_4,          KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_TRNS,
 		KC_TAB,     KC_Q,    KC_W,          KC_E,          KC_R,          KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
-		TD(FN_NUM), KC_A,    KC_S,          KC_D,          KC_F,          KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  DF(_ENG),
+		TD(FN_NUM), KC_A,    KC_S,          KC_D,          KC_F,          KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  TG(_ENG),
 		KC_LSFT,    KC_Z,    KC_X,          KC_C,          KC_V,          KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   TG(_GAM),
 		KC_LCTL,    KC_LGUI, KC_LALT,                                     KC_SPC,                    KC_CAPS,          KC_RALT, KC_LEFT,          KC_DOWN, KC_RIGHT
 	),
@@ -73,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ENG] = LAYOUT_65_ansi_blocker( /* Engrammer */
 		QK_GESC,    KC_1,    KC_2,          KC_3,          KC_4,          KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC, KC_TRNS,
 		KC_TAB,     KC_B,    KC_Y,          KC_O,          KC_U,          KC_QUOT, KC_SCLN, KC_L,    KC_D,    KC_W,    KC_V,    KC_Z,    KC_EQL,  KC_BSLS, KC_TRNS,
-		TD(FN_NUM), KC_C,    KC_I,          KC_E,          KC_A,          KC_COMM, KC_DOT,  KC_H,    KC_T,    KC_S,    KC_N,    KC_Q,             KC_ENT,  DF(_DEF),
+		TD(FN_NUM), KC_C,    KC_I,          KC_E,          KC_A,          KC_COMM, KC_DOT,  KC_H,    KC_T,    KC_S,    KC_N,    KC_Q,             KC_ENT,  TG(_ENG),
 		KC_LSFT,    KC_G,    KC_X,          KC_J,          KC_K,          KC_MINS, KC_SLSH, KC_R,    KC_M,    KC_F,    KC_P,    KC_RSFT,          KC_UP,   TG(_GAM),
 		KC_LCTL,    KC_LGUI, KC_LALT,                                     KC_SPC,                    KC_CAPS,          KC_RALT, KC_LEFT,          KC_DOWN, KC_RIGHT
 	),
