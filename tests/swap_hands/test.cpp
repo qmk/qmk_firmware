50d71be7e0c5a07c0ee6ac6cf246975c87d8bf03
Wojciech Jasiński <ilikeheaps@gmail.com>
2024-09-21 19:45:34 +0200

    Add a test combining OSM with SH

--- tests/swap_hands/test.cpp
+++ tests/swap_hands/test.cpp
@@ -10,7 +10,7 @@ class SHParametrizedTestFixture : public ::testing::WithParamInterface<std::pair
 
 // XXX hand_swap_config[row][column] -> column, row
 const keypos_t hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
-  {{0, 0}, {2, 0}, {1, 0}}
+  {{0, 0}, {2, 0}, {1, 0}, {3, 0}}
 };
 
 TEST_F(SH, SwapHandsTapLongHoldSwaps) {
@@ -62,3 +62,33 @@ TEST_F(SH, SwapHandsTapShortHoldSwaps) {
     EXPECT_EMPTY_REPORT(driver);
     keyboard_task();
 }
+
+TEST_F(SH, SwapHandsAfterOSM) {
+    TestDriver driver;
+    auto       key_sh_t = KeymapKey(0, 0, 0, SH_T(KC_SPACE));
+    auto       key_a = KeymapKey(0, 1, 0, KC_A);
+    auto       key_b = KeymapKey(0, 2, 0, KC_B);
+    auto       key_m = KeymapKey(0, 3, 0, OSM(MOD_LSFT));
+
+    set_keymap({key_sh_t, key_a, key_b, key_m});
+
+    debug_enable = true;
+    debug_keyboard = true;
+
+    key_m.press();
+    idle_for(1);
+    key_m.release();
+    idle_for(1);
+    key_sh_t.press();
+    idle_for(1);
+    key_a.press();
+    EXPECT_REPORT(driver, (key_b.report_code, KC_LEFT_SHIFT));
+    idle_for(1);
+    key_sh_t.release();
+    idle_for(1);
+    key_a.release();
+
+    EXPECT_EMPTY_REPORT(driver);
+    keyboard_task();
+}
+
