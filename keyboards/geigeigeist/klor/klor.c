2d3e9b97db303989d5aee05fed74d0a66d3b44db
Victor <VictorN076@gmail.com>
2024-09-24 13:51:45 -0500

    Missed copyright in one file. added

--- keyboards/geigeigeist/klor/klor.c
+++ keyboards/geigeigeist/klor/klor.c
@@ -1,4 +1,4 @@
- /* Copyright 2022 GEIST (@geigeigeist)
+/* Copyright 2022 GEIST (@geigeigeist)
  * Copyright 2024 t4corun (@t4corun)
  *
  * This program is free software: you can redistribute it and/or modify
