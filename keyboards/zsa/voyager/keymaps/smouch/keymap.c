71b54e7c8e747c218811f20e9c226a1277f96407
RN820503 <roderick.smith@me.com>
2024-09-24 13:21:41 +1000

    Deleted keycodes.h

--- keyboards/zsa/voyager/keymaps/smouch/keymap.c
+++ keyboards/zsa/voyager/keymaps/smouch/keymap.c
@@ -6,8 +6,7 @@ enum layer_names {
     _DEF,
     _NUM,
     _SYM,
-    _NAV,
-    _SNTH
+    _NAV
 };
 
 enum custom_keycodes {
@@ -43,7 +42,7 @@ enum custom_keycodes {
 #define LN3 KC_NO
 #define LN2 KC_TAB
 #define LN1 KC_NO
-#define LN0 TO(_SNTH)
+#define LN0 CW_TOGG
 
 #define RN0 TG(_NUM)
 #define RN1 KC_LPRN
@@ -186,20 +185,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                          _______, _______, C(KC_LEFT), C(KC_RIGHT)
   ),
 
-  /*  Base (alpha) Layer SNTH
-    ⎋           ⇥        tg              (   ⌦   )   vol↓↑
-    `   x   p   d⌃   m   q           =   y   o⌃  u   .  \
-    z   s   n⌥  t⌘   h⇧  v           g   c⇧  a⌘  e⌥  i  q
-        f   b   k    l   j      	 z   w   '   /   ,
-                         r   ⌫   ⏎   ␣
-  */
-  [_SNTH] = LAYOUT_voyager(
-    _______, _______,        _______,      _______,      _______,      TO(_DEF), 					_______, _______,      _______,      _______,      _______,        _______,
-    _______, KC_X,           KC_P,         LCTL_T(KC_D), KC_M,         KC_Q,     					KC_EQL,  KC_Y,         RCTL_T(KC_O), KC_U,         KC_DOT,         _______,
-    _______, LT(_SYM, KC_S), LALT_T(KC_N), LGUI_T(KC_T), LSFT_T(KC_H), KC_V,     					KC_G,    RSFT_T(KC_C), RGUI_T(KC_A), RALT_T(KC_E), LT(_SYM, KC_I), _______,
-    ___x___, KC_F,           KC_B,         KC_K,         KC_L,         KC_J,     					KC_Z,    KC_W,         KC_QUOT,      KC_SLSH,      KC_COMM,        ___x___,
-                                                                       _______,  _______,  _______, _______
-  ),
 };
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
