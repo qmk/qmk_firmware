efe846f2c5fade44e482309d8a34c1b919fdc009
Pham Duc Minh <95753855+Deemen17@users.noreply.github.com>
2024-09-27 00:07:47 +0700

    Fix keymap de80

--- keyboards/deemen17/de80/keymaps/default/keymap.c
+++ keyboards/deemen17/de80/keymaps/default/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, RGB_SAD, RGB_HUD,
         KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_TOG,
         KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      RGB_VAI,
-        KC_TRNS,   KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            RGB_RMOD, RGB_VAD, RGB_MOD
+        KC_TRNS,   KC_TRNS, KC_TRNS,                            KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS,            RGB_RMOD, RGB_VAD, RGB_MOD
     )
 
 };
