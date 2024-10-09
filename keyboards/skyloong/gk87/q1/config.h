7ecb950612bb32b0b6cebf5e87470e8c211707a9
JIKEDINGZHI <52481939+NaturalZh@users.noreply.github.com>
2024-10-09 15:54:47 +0800

    Update keyboards/skyloong/gk87/q1/config.h
    OK. I agree with you.
    
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/skyloong/gk87/q1/config.h
+++ keyboards/skyloong/gk87/q1/config.h
@@ -5,22 +5,16 @@
 
 #define WIN_LOCK_PIN C13
 
-#ifdef ENCODER_ENABLE
-#    define ENCODER_MAP_KEY_DELAY 10
-#endif
+#define ENCODER_MAP_KEY_DELAY 10
 
-#ifdef RGB_MATRIX_ENABLE
-#    define AW20216S_CS_PIN_1 B12
-#    define AW20216S_CS_PIN_2 B11
+#define AW20216S_CS_PIN_1 B12
+#define AW20216S_CS_PIN_2 B11
 // Hardware enable lines may be connected to the same pin
-#    define AW20216S_EN_PIN B10
+#define AW20216S_EN_PIN B10
 
-#    define NUM_LOCK_INDEX 33
-#    define WIN_MOD_INDEX 22
-#    define MAC_MOD_INDEX 23
-#    define WIN_LOCK_INDEX 93
-#    define CAPS_LOCK_INDEX 58
-#    define SCR_LOCK_INDEX 14
-#endif // RGB_MATRIX_ENABLE
-
-
+#define NUM_LOCK_INDEX 33
+#define WIN_MOD_INDEX 22
+#define MAC_MOD_INDEX 23
+#define WIN_LOCK_INDEX 93
+#define CAPS_LOCK_INDEX 58
+#define SCR_LOCK_INDEX 14
