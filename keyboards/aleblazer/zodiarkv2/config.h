e206cec561b709226ed1d7dfe4ed6aae99b2290d
Aleblazer <60912320+Aleblazer@users.noreply.github.com>
2024-10-13 22:05:34 -0500

    keyboard.json and handiness fixes

--- keyboards/aleblazer/zodiarkv2/config.h
+++ keyboards/aleblazer/zodiarkv2/config.h
@@ -3,13 +3,17 @@
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
-#define SERIAL_USART_DRIVER SIOD0
 
-#define SPLIT_HAND_PIN GP25
+#define SPI_DRIVER SPID1
+#define SPI_SCK_PIN GP14
+#define SPI_MOSI_PIN GP15
+#define SPI_MISO_PIN GP12
 
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP2
 #define I2C1_SCL_PIN GP3
+
+#define SPLIT_TRANSPORT_MIRROR
 
 /* RP2040- and hardware-specific config */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
--- keyboards/aleblazer/zodiarkv2/mcuconf.h
+++ keyboards/aleblazer/zodiarkv2/mcuconf.h
@@ -5,5 +5,8 @@
 #undef RP_I2C_USE_I2C1
 #define RP_I2C_USE_I2C1 TRUE
 
+#undef RP_SPI_USE_SPI1
+#define RP_SPI_USE_SPI1 TRUE
+
 #undef RP_SIO_USE_UART0
 #define RP_SIO_USE_UART0 TRUE
