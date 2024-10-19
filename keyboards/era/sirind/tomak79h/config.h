c2d07d5b3da208b0b7308ff710932fc51e047271
era <phyojinbin@gmail.com>
2024-10-20 01:56:30 +0900

    Add Tomak79H
    Add Tomak79H

---
+++ keyboards/era/sirind/tomak79h/config.h
@@ -0,0 +1,19 @@
+// Copyright 2024 Hyojin Bak (@eerraa)
+// SPDX-License-Identifier: GPL-2.0-or-later
+ 
+#pragma once
+
+/* Split configuration */
+#define USB_VBUS_PIN GP19
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+#define SERIAL_USART_PIN_SWAP
+
+/* Sync configuration */
+#define TOMAK_CONFIG_SYNC
+#define SPLIT_TRANSACTION_IDS_KB RPC_ID_KB_CONFIG_SYNC
+
+/* Reset */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
