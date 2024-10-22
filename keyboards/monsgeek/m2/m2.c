b99eed7551daefdb43685755ba1d42ca303dffb1
jonylee@hfd <108282809+jonylee1986@users.noreply.github.com>
2024-10-22 13:55:54 +0800

    Update keyboards/monsgeek/m2/m2.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/monsgeek/m2/m2.c
+++ keyboards/monsgeek/m2/m2.c
@@ -140,17 +140,6 @@ void led_init_ports(void){
     gpio_write_pin_low(LED_WIN_LOCK_PIN);
 }
 
-bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if(res) {
-        // writePin sets the pin high for 1 and low for 0.
-        // In this example the pins are inverted, setting
-        // it low/0 turns it on, and high/1 turns the LED off.
-        // This behavior depends on whether the LED is between the pin
-        // and VCC or the pin and GND.
-        gpio_write_pin(LED_NUM_LOCK_PIN, led_state.num_lock);
-        gpio_write_pin(LED_CAPS_LOCK_PIN, led_state.caps_lock);
-        gpio_write_pin(LED_WIN_LOCK_PIN, keymap_config.no_gui);
- }
-    return res;
+void housekeeping_task_kb(void) {
+    gpio_write_pin(LED_WIN_LOCK_PIN, keymap_config.no_gui);
 }
