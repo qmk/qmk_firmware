ac42e1646f019e091c11d8b52bdec3b03e10b87d
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-10-25 09:16:00 +0800

    Refactor Starlight matrix effect
    Guard main loop with `RGB_MATRIX_USE_LIMITS` to avoid breaking `rgb_matrix_indicators_advanced_user()` function.

--- quantum/rgb_matrix/animations/starlight_anim.h
+++ quantum/rgb_matrix/animations/starlight_anim.h
@@ -3,6 +3,7 @@ RGB_MATRIX_EFFECT(STARLIGHT)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 void set_starlight_color(uint8_t i, effect_params_t* params) {
+    if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) return;
     uint16_t time = scale16by8(g_rgb_timer, rgb_matrix_config.speed / 8);
     hsv_t    hsv  = rgb_matrix_config.hsv;
     hsv.v         = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
@@ -11,18 +12,18 @@ void set_starlight_color(uint8_t i, effect_params_t* params) {
 }
 
 bool STARLIGHT(effect_params_t* params) {
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
     if (!params->init) {
+        // Change one LED every tick, make sure speed is not 0
         if (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 5)) % 5 == 0) {
             uint8_t rand_led = random8_max(RGB_MATRIX_LED_COUNT);
             set_starlight_color(rand_led, params);
         }
-        return false;
-    }
-
-    RGB_MATRIX_USE_LIMITS(led_min, led_max);
-    for (uint8_t i = led_min; i < led_max; i++) {
-        RGB_MATRIX_TEST_LED_FLAGS();
-        set_starlight_color(i, params);
+    } else {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            RGB_MATRIX_TEST_LED_FLAGS();
+            set_starlight_color(i, params);
+        }
     }
     return rgb_matrix_check_finished_leds(led_max);
 }
