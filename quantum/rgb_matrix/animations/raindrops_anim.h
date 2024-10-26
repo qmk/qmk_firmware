1596033bc4e0d0767ae5da6740951b796f995e7a
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-10-26 19:53:44 +0800

    Update Raindrops effect to respect LED range limits
    Improved effect to update LED index within `led_min` and `led_max` limits

--- quantum/rgb_matrix/animations/raindrops_anim.h
+++ quantum/rgb_matrix/animations/raindrops_anim.h
@@ -2,34 +2,39 @@
 RGB_MATRIX_EFFECT(RAINDROPS)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static void raindrops_set_color(int i, effect_params_t* params) {
-    if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) return;
-    hsv_t hsv = {0, rgb_matrix_config.hsv.s, rgb_matrix_config.hsv.v};
-
+static rgb_t raindrops_set_color(hsv_t hsv) {
     // Take the shortest path between hues
-    int16_t deltaH = ((rgb_matrix_config.hsv.h + 180) % 360 - rgb_matrix_config.hsv.h) / 4;
+    int16_t deltaH = ((hsv.h + 180) % 360 - hsv.h) / 4;
     if (deltaH > 127) {
         deltaH -= 256;
     } else if (deltaH < -127) {
         deltaH += 256;
     }
 
-    hsv.h     = rgb_matrix_config.hsv.h + (deltaH * (random8() & 0x03));
-    rgb_t rgb = rgb_matrix_hsv_to_rgb(hsv);
-    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+    hsv.h += (deltaH * random8_max(3));
+    return rgb_matrix_hsv_to_rgb(hsv);
 }
 
 bool RAINDROPS(effect_params_t* params) {
+    static uint16_t idx = 0;
+    static rgb_t    rgb = {0};
+
+    if (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 16)) % 10 == 0) {
+        idx = random8_max(RGB_MATRIX_LED_COUNT);
+        rgb = raindrops_set_color(rgb_matrix_config.hsv);
+    }
+
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
-    if (!params->init) {
-        // Change one LED every tick, make sure speed is not 0
-        if (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 16)) % 10 == 0) {
-            raindrops_set_color(random8_max(RGB_MATRIX_LED_COUNT), params);
+    if (params->init) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            RGB_MATRIX_TEST_LED_FLAGS();
+            rgb = raindrops_set_color(rgb_matrix_config.hsv);
+            rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
         }
-    } else {
-        for (int i = led_min; i < led_max; i++) {
-            raindrops_set_color(i, params);
-        }
+    }
+    // Change one LED every tick
+    else if (led_min <= idx && idx < led_max && HAS_ANY_FLAGS(g_led_config.flags[idx], params->flags)) {
+        rgb_matrix_set_color(idx, rgb.r, rgb.g, rgb.b);
     }
     return rgb_matrix_check_finished_leds(led_max);
 }
