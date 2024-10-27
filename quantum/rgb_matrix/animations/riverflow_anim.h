b0a67fbd0392b36378f89ca69c29a2d321fde1c6
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-10-27 12:37:14 +0800

    Change math function name
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- quantum/rgb_matrix/animations/riverflow_anim.h
+++ quantum/rgb_matrix/animations/riverflow_anim.h
@@ -4,14 +4,14 @@ RGB_MATRIX_EFFECT(RIVERFLOW)
 
 // inspired by @PleasureTek's Massdrop Alt LED animation
 
-hsv_t RIVERFLOW_maths(hsv_t hsv, uint8_t i, uint8_t time) {
+hsv_t RIVERFLOW_math(hsv_t hsv, uint8_t i, uint8_t time) {
     time  = scale16by8(g_rgb_timer + (i * 315), rgb_matrix_config.speed / 8);
     hsv.v = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
     return hsv;
 }
 
 bool RIVERFLOW(effect_params_t* params) {
-    return effect_runner_i(params, &RIVERFLOW_maths);
+    return effect_runner_i(params, &RIVERFLOW_math);
 }
 
 #    endif // RGB_MATRIX_CUSTOM_EFFECT_IMPLS
