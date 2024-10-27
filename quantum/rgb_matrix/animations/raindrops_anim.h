2f07445e9ad1fed4586bb5efa2c3c08d033c9288
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-10-27 18:30:50 +0800

    Remove static RGB variable
    Set index out of range after trigger

--- quantum/rgb_matrix/animations/raindrops_anim.h
+++ quantum/rgb_matrix/animations/raindrops_anim.h
@@ -2,7 +2,10 @@
 RGB_MATRIX_EFFECT(RAINDROPS)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static rgb_t raindrops_set_color(hsv_t hsv) {
+static void raindrops_set_color(uint8_t i, effect_params_t* params) {
+    if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) return;
+    hsv_t hsv = rgb_matrix_config.hsv;
+
     // Take the shortest path between hues
     int16_t deltaH = ((hsv.h + 180) % 360 - hsv.h) / 4;
     if (deltaH > 127) {
@@ -12,29 +15,28 @@ static rgb_t raindrops_set_color(hsv_t hsv) {
     }
 
     hsv.h += (deltaH * random8_max(3));
-    return rgb_matrix_hsv_to_rgb(hsv);
+    rgb_t rgb = rgb_matrix_hsv_to_rgb(hsv);
+    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
 }
 
 bool RAINDROPS(effect_params_t* params) {
-    static uint16_t idx = 0;
-    static rgb_t    rgb = {0};
+    static uint16_t index = RGB_MATRIX_LED_COUNT + 1;
 
-    if (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 16)) % 10 == 0) {
-        idx = random8_max(RGB_MATRIX_LED_COUNT);
-        rgb = raindrops_set_color(rgb_matrix_config.hsv);
+    // Periodic trigger for LED change
+    if ((params->iter == 0) && (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 16)) % 10 == 0)) {
+        index = random8_max(RGB_MATRIX_LED_COUNT);
     }
 
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
     if (params->init) {
         for (uint8_t i = led_min; i < led_max; i++) {
-            RGB_MATRIX_TEST_LED_FLAGS();
-            rgb = raindrops_set_color(rgb_matrix_config.hsv);
-            rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+            raindrops_set_color(i, params);
         }
     }
-    // Change one LED every tick
-    else if (led_min <= idx && idx < led_max && HAS_ANY_FLAGS(g_led_config.flags[idx], params->flags)) {
-        rgb_matrix_set_color(idx, rgb.r, rgb.g, rgb.b);
+    // Change LED once and set index out of range till next trigger
+    else if (led_min <= index && index < led_max) {
+        raindrops_set_color(index, params);
+        index = RGB_MATRIX_LED_COUNT + 1;
     }
     return rgb_matrix_check_finished_leds(led_max);
 }
