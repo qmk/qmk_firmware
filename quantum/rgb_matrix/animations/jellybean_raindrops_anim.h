40279659f0de8bcdeabf53e411b0b70cc780a4c0
フィルターペーパー <76888457+filterpaper@users.noreply.github.com>
2024-10-28 07:19:24 +0800

    Remove static RGB variable
    Set index out of range after trigger

--- quantum/rgb_matrix/animations/jellybean_raindrops_anim.h
+++ quantum/rgb_matrix/animations/jellybean_raindrops_anim.h
@@ -2,31 +2,32 @@
 RGB_MATRIX_EFFECT(JELLYBEAN_RAINDROPS)
 #    ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static rgb_t jellybean_raindrops_set_color(void) {
+static void jellybean_raindrops_set_color(uint8_t i, effect_params_t* params) {
+    if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) return;
+
     hsv_t hsv = {random8(), random8_min_max(127, 255), rgb_matrix_config.hsv.v};
-    return rgb_matrix_hsv_to_rgb(hsv);
+    rgb_t rgb = rgb_matrix_hsv_to_rgb(hsv);
+    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
 }
 
 bool JELLYBEAN_RAINDROPS(effect_params_t* params) {
-    static uint16_t idx = 0;
-    static rgb_t    rgb = {0};
+    static uint16_t index = RGB_MATRIX_LED_COUNT + 1;
 
-    if (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 16)) % 5 == 0) {
-        idx = random8_max(RGB_MATRIX_LED_COUNT);
-        rgb = jellybean_raindrops_set_color();
+    // Periodic trigger for LED change
+    if ((params->iter == 0) && (scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 16)) % 5 == 0)) {
+        index = random8_max(RGB_MATRIX_LED_COUNT);
     }
 
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
     if (params->init) {
         for (uint8_t i = led_min; i < led_max; i++) {
-            RGB_MATRIX_TEST_LED_FLAGS();
-            rgb = jellybean_raindrops_set_color();
-            rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+            jellybean_raindrops_set_color(i, params);
         }
     }
-    // Change one LED every tick
-    else if (led_min <= idx && idx < led_max && HAS_ANY_FLAGS(g_led_config.flags[idx], params->flags)) {
-        rgb_matrix_set_color(idx, rgb.r, rgb.g, rgb.b);
+    // Change LED once and set index out of range till next trigger
+    else if (led_min <= index && index < led_max) {
+        jellybean_raindrops_set_color(index, params);
+        index = RGB_MATRIX_LED_COUNT + 1;
     }
     return rgb_matrix_check_finished_leds(led_max);
 }
