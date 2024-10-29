ade99d7b434f6bca6b95c84d0f16957414f9251d
VeyPatch <126267034+VeyPatch@users.noreply.github.com>
2024-10-29 15:52:19 +0100

    Add update user

--- keyboards/splitkb/halcyon/elora/rev2/rev2.c
+++ keyboards/splitkb/halcyon/elora/rev2/rev2.c
@@ -5,7 +5,9 @@
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
-
+    if (!encoder_update_user(index, clockwise)) {
+        return false;
+    }
     if (index == 0) {
         // Volume control
         if (clockwise) {
