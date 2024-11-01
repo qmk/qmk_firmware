3c75854da0ef3c71255cf052d92f2b9b4a8b8271
Akshay <akshay7924@gmail.com>
2024-11-01 15:38:31 +0530

    fixed matrix pins and layout

--- keyboards/shorty/keymaps/default/keymap.c
+++ keyboards/shorty/keymaps/default/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P7,   KC_P8,   KC_P9,    
         KC_P4,   KC_P5,   KC_P6,     
         KC_P1,   KC_P2,   KC_P3,  
-        KC_A,    KC_NO,   KC_NO, // encoder button1
-        KC_D,    KC_NO,   KC_NO  // encoder button2
+        KC_A,    // encoder button1
+        KC_D     // encoder button2
     )
 };
