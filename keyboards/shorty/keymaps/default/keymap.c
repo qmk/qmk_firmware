8e10697d65b4f071ea4c1717d5a3e838119c73ff
Akshay <akshay7924@gmail.com>
2024-11-03 23:07:50 +0530

    updated keymap with fixing PCB issues

--- keyboards/shorty/keymaps/default/keymap.c
+++ keyboards/shorty/keymaps/default/keymap.c
@@ -4,15 +4,13 @@
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MS_WH_DOWN,KC_MS_WH_UP)  }
-    //                  Encoder 1                                     Encoder 2
 };
 #endif
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
+        KC_A,             KC_D,    
         KC_P7,   KC_P8,   KC_P9,    
         KC_P4,   KC_P5,   KC_P6,     
-        KC_P1,   KC_P2,   KC_P3,  
-        KC_A,    // encoder button1
-        KC_D     // encoder button2
+        KC_P1,   KC_P2,   KC_P3  
     )
 };
