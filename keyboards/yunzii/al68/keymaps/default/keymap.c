7d2053fa8d57cb564303eb766b40fda04a3e9c5a
yunzii keyboard <381957792@qq.com>
2024-11-05 11:58:48 +0800

    Update keyboards/yunzii/al68/keymaps/default/keymap.c
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/yunzii/al68/keymaps/default/keymap.c
+++ keyboards/yunzii/al68/keymaps/default/keymap.c
@@ -39,5 +39,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    _______,     _______,                           _______,                           _______,    _______,                            RGB_SPD,    RGB_VAD,    RGB_SPI 
     )
 };
-                        
-
