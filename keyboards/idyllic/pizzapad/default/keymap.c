d28e8e1284e713599d8deb05eae152069d927926
zykrah <23428162+zykrah@users.noreply.github.com>
2024-11-06 23:00:15 +1100

    Updated files to conform to PR checklist

---
+++ keyboards/idyllic/pizzapad/default/keymap.c
@@ -0,0 +1,20 @@
+// Copyright 2024 Zykrah (@zykrah)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[0] = LAYOUT(
+		KC_P7,   KC_P8,   KC_P9,
+		KC_P4,   KC_P5,   KC_P6,
+		KC_P1,   KC_P2,   KC_P3
+	),
+
+	[1] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS
+	),
+
+};
