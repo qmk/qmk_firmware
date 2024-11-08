b70c1fe078d5da6e9d82db932fee484953a24188
Andrew Ostroumov <a.ostroumov@pin-up.tech>
2024-11-08 17:02:50 +0100

    Change masterness

--- keyboards/bastardkb/charybdis/3x6/v2/splinky_3/config.h
+++ keyboards/bastardkb/charybdis/3x6/v2/splinky_3/config.h
@@ -18,7 +18,7 @@
 #pragma once
 
 /* Handedness. */
-#define MASTER_LEFT
+#define MASTER_RIGHT
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
