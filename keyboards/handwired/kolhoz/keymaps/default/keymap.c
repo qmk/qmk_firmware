e33b3c98794567465d1cec98cfaae009c6372285
Vitali Fomine <vf_info@mail.ru>
2024-11-09 11:20:06 +0300

    Add print screen key

--- keyboards/handwired/kolhoz/keymaps/default/keymap.c
+++ keyboards/handwired/kolhoz/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,     KC_TRNS,  TO(LDEF),    KC_NO,       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
     ),
     [LNUM] = LAYOUT_split_2_3x5_4_3(
-        KC_NO,          KC_F1,         KC_F2,         KC_F3,          KC_F4,       KC_NO,                                               KC_0,         KC_1,          KC_2,          KC_3,         KC_NO,       KC_TRNS,
+        KC_NO,          KC_F1,         KC_F2,         KC_F3,          KC_F4,     KC_PSCR,                                               KC_0,         KC_1,          KC_2,          KC_3,         KC_NO,       KC_TRNS,
         KC_NO,          KC_F5,         KC_F6,         KC_F7,          KC_F8,       KC_NO,                                              KC_NO,         KC_4,          KC_5,          KC_6,         KC_NO,       KC_BSPC,
                         KC_F9,        KC_F10,        KC_F11,         KC_F12,       KC_NO,                                              KC_NO,         KC_7,          KC_8,          KC_9,         KC_NO,
                       KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,     KC_TRNS,  TO(LDEF),    KC_NO,       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
