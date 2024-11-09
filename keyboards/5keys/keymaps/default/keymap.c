ccf9e9319a23659a0fd17cd40c6b204ea3f7ef17
mikiya418 <64995478+mikiya418@users.noreply.github.com>
2024-11-09 19:54:40 +0900

    Add keyboard 5keys

--- keyboards/5keys/keymaps/default/keymap.c
+++ keyboards/5keys/keymaps/default/keymap.c
@@ -4,9 +4,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
         KC_WBAK,   KC_WFWD,   LSG_T(KC_S),   G(KC_L),  MO(1) 
-    ),
-
-    [1] = LAYOUT(
-        KC_BRIU,   KC_BRID,   KC_VOLU,   KC_VOLD,  _______
     )
 };
