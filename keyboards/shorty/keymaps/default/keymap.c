983a6b0cc402f5d03786f87f681acc2a98e053fa
Akshay <akshay7924@gmail.com>
2024-11-12 14:34:54 +0530

    added layer switcher logic

--- keyboards/shorty/keymaps/default/keymap.c
+++ keyboards/shorty/keymaps/default/keymap.c
@@ -4,15 +4,57 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_A,             KC_D,    
-        KC_P7,   KC_P8,   KC_P9,    
-        KC_P4,   KC_P5,   KC_P6,     
-        KC_P1,   KC_P2,   KC_P3  
+        LT(0,KC_NO),   KC_LEFT_GUI,    
+        KC_7,   KC_8,   KC_9,    
+        KC_4,   KC_5,   KC_6,     
+        KC_1,   KC_2,   KC_3  
+    ),
+
+    [1] = LAYOUT(
+        LT(0,KC_NO),   KC_LEFT_GUI,    
+        KC_A,   KC_B,   KC_C,
+        KC_D,   KC_E,   KC_F,
+        KC_G,   KC_H,   KC_I
+    ),
+
+    [2] = LAYOUT(
+        LT(0,KC_NO),   KC_LEFT_GUI,    
+        KC_J,   KC_K,   KC_L,
+        KC_M,   KC_N,   KC_O,
+        KC_P,   KC_Q,   KC_R
+    ),
+
+    [3] = LAYOUT(
+        LT(0,KC_NO),   KC_ENT,    
+        KC_S,   KC_T,   KC_U,
+        KC_V,   KC_W,   KC_X,
+        KC_Y,   KC_Z,   KC_ENT
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)  }
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)  },
+    [1] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)  },
+    [2] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)  },
+    [3] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)  }
 };
 #endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case LT(0, KC_NO):
+            if (record->event.pressed) {
+                // on tap
+                if (record->tap.count) {
+                    if (get_highest_layer(layer_state) >= 3) {
+                        layer_clear(); 
+                    } else { 
+                        layer_move(get_highest_layer(layer_state) + 1);
+                    }
+                }
+            }
+            return false;
+    }
+    return true;
+}
