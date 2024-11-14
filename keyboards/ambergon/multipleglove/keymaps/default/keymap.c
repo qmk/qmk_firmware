f6043420839875611690e5b1d4f3a8cd8cf9dbbd
ambergon <sc.foxgon@gmail.com>
2024-11-14 23:24:47 +0900

    [fix]
    DELETE ./config.h
    DELETE ./rules.mk
    FIX    ./readme.md
    FIX    ./keymaps/default/rules.mk
        remove ENCODER ENABLE
    FIX    ./keyboard.json
        add encoder true
        add layer 16
    FIX    ./keymaps/default/keymap.c
    
    thank @zvecr!

--- keyboards/ambergon/multipleglove/keymaps/default/keymap.c
+++ keyboards/ambergon/multipleglove/keymaps/default/keymap.c
@@ -3,118 +3,8 @@
 
 #include QMK_KEYBOARD_H
 
-#ifdef CONSOLE_ENABLE
-#include <print.h>
-#endif 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // KC_NO : 何もしない。
     [0] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [1] = LAYOUT(
-        KC_B  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [2] = LAYOUT(
-        KC_C  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [3] = LAYOUT(
-        KC_D  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [4] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [5] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [6] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [7] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [8] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [9] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [10] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [11] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [12] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [13] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [14] = LAYOUT(
-        KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
-        KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
-        KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
-        KC_NO ,   KC_NO ,   KC_NO ,   KC_NO ,
-        KC_Q  ,   KC_R
-    ),
-    [15] = LAYOUT(
         KC_A  ,   KC_B  ,   KC_C  ,   KC_D  ,
         KC_E  ,   KC_F  ,   KC_G  ,   KC_H  ,
         KC_I  ,   KC_J  ,   KC_K  ,   KC_L  ,
@@ -123,45 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-
-
-// rules.mkで有効化された関数
-// ここで各ロータリーエンコーダの挙動を指定する。
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    // レイヤー数
-    // エンコーダー数
-    // 配列の先頭のエンコーダから指定する。
-    // ENCODER_CCW_CW( 回転時のキー , 逆転時のキー ) ,
-    [0]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [1]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [2]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [3]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [4]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [5]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [6]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [7]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [8]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [9]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [10] = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [11] = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [12] = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [13] = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [14] = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) },
-    [15] = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) }
+    [0]  = { ENCODER_CCW_CW( KC_P , KC_Q ) , ENCODER_CCW_CW( KC_R , KC_S ) , ENCODER_CCW_CW( KC_T , KC_U ) }
 };
 #endif
-
-
-/* #include <print.h> */
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
-/*     print( "test\n" ); */
-/* #ifdef CONSOLE_ENABLE */
-/*     uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed); */
-/* #endif */ 
-/*     // falseならキー処理をしない。 */
-/*     return true; */
-/* } */
 
 static int myOldLayer = 0;
 void matrix_scan_user(void) {
@@ -173,40 +29,4 @@ void matrix_scan_user(void) {
         layer_move( myLayer );
         myOldLayer = myLayer;
     }
-    /* uprintf("now layer myLayer %d\n" , myLayer ); */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- keyboards/ambergon/multipleglove/readme.md
+++ keyboards/ambergon/multipleglove/readme.md
@@ -1,20 +1,29 @@
-# ambergon/MultipleGlove
+# MultipleGlove
 
-![ambergon/MultipleGlove](imgur.com image replace me!)
+![MultipleGlove](https://github.com/ambergon/MultipeGlove/blob/master/img/1.jpg)
+![MultipleGlove](https://github.com/ambergon/MultipeGlove/blob/master/img/2.jpg)
+![MultipleGlove](https://github.com/ambergon/MultipeGlove/blob/master/img/6.jpg)
 
-*A short description of the keyboard/project*
+This keyboard was strongly influenced by the text editor Vim and the Keyball61 by Hakugin Labs.
+The emphasis is on the home position of the left hand and access to the three rotary encoders without moving the hand significantly.
+this keyboard specialize in 0-15 layer switching.
+
 
 * Keyboard Maintainer: [ATSUTO_NAKAYAMA](https://github.com/ambergon)
-* Hardware Supported: *The PCBs, controllers supported*
-* Hardware Availability: *Links to where you can find this hardware*
+* Hardware Supported: Pro Micro
+* Hardware Availability: [Here](https://github.com/ambergon/MultipeGlove/tree/master#%E3%82%AD%E3%83%83%E3%83%88%E3%81%AE%E8%B2%A9%E5%A3%B2%E5%A0%B4%E6%89%80)
 
 Make example for this keyboard (after setting up your build environment):
 
-    make ambergon/MultipleGlove:default
+## build hex file
+```.sh
+make ambergon/MultipleGlove:default
+```
 
-Flashing example for this keyboard:
-
-    make ambergon/MultipleGlove:default:flash
+## flash hex file
+```.sh
+make ambergon/MultipleGlove:default:flash
+```
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
 
