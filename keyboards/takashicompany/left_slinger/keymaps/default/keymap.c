27e232c8774cd2884974273d677c0fc6441b4061
takashicompany <t@kashi.company>
2024-11-17 01:51:27 +0900

    rotary encoder

--- keyboards/takashicompany/left_slinger/keymaps/default/keymap.c
+++ keyboards/takashicompany/left_slinger/keymaps/default/keymap.c
@@ -23,3 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_F, KC_G, KC_H
     )
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =   { ENCODER_CCW_CW(KC_1, KC_2) }
+};
+#endif
