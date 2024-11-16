768f01e4f68c519cdb2c97a7a7193c9db2e4b2aa
Christian C. Berclaz <christian.berclaz@mac.com>
2024-11-16 18:38:50 +0100

    Update keyboards/zlant_xl/keymaps/default/keymap.c
    Co-authored-by: jack <jack@pngu.org>

--- keyboards/zlant_xl/keymaps/default/keymap.c
+++ keyboards/zlant_xl/keymaps/default/keymap.c
@@ -16,8 +16,10 @@
 
 #include QMK_KEYBOARD_H
 
-#define BASE 0
-#define FN 1
+enum layers {
+    BASE,
+    FN
+};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
