d29b667c136d6d135ce7aee2a59b7d1f8896a952
Christian C. Berclaz <christian.berclaz@mac.com>
2024-11-16 14:38:03 +0100

    docs(zlant_xl): add readme.md, updated keymap.c with correct year

--- keyboards/zlant_xl/keymaps/default/keymap.c
+++ keyboards/zlant_xl/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 Christian C. Berclaz
+/* Copyright 2024 Christian C. Berclaz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
