5abe82bb8072ad591b592028f246d97afbde3b75
Vitali Fomine <vf_info@mail.ru>
2024-11-19 20:32:14 +0300

    Update keymap

--- keyboards/handwired/kolhoz/keymaps/default/keymap.c
+++ keyboards/handwired/kolhoz/keymaps/default/keymap.c
@@ -21,19 +21,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,       KC_TILDE,        KC_GRV,         KC_NO,       KC_SLASH,     KC_BSLS,                                            KC_PEQL,      KC_LBRC,       KC_RBRC,       KC_QUES,         KC_NO,       KC_TRNS,
         KC_NO,          KC_AT,       KC_HASH,     KC_DOLLAR,     KC_PERCENT,     KC_CIRC,                                            KC_AMPR,      KC_ASTR,       KC_LPRN,       KC_RPRN,      KC_COLON,       KC_TRNS,
                    KC_EXCLAIM, KC_UNDERSCORE,      KC_MINUS,        KC_PLUS,     KC_PIPE,                                            KC_LCBR,      KC_RCBR,         KC_LT,         KC_GT,       KC_DQUO,
-                      KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,     KC_TRNS,  TO(LDEF),    KC_NO,       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
+                      KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,       KC_NO,     KC_NO,    KC_NO,       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
     ),
     [LNUM] = LAYOUT_split_2_3x5_4_3(
         KC_NO,          KC_F1,         KC_F2,         KC_F3,          KC_F4,     KC_PSCR,                                              KC_NO,         KC_1,          KC_2,          KC_3,         KC_NO,       KC_TRNS,
         KC_NO,          KC_F5,         KC_F6,         KC_F7,          KC_F8,     CW_TOGG,                                              KC_NO,         KC_4,          KC_5,          KC_6,          KC_0,       KC_TRNS,
                         KC_F9,        KC_F10,        KC_F11,         KC_F12,       KC_NO,                                              KC_NO,         KC_7,          KC_8,          KC_9,         KC_NO,
-                      KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,     KC_TRNS,  TO(LDEF),    KC_NO,       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
+                      KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,       KC_NO,     KC_NO,    KC_NO,       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
     ),
     [LNAV] = LAYOUT_split_2_3x5_4_3(
         KC_NO,          KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,                                            KC_HOME,      KC_PGDN,       KC_PGUP,        KC_END,        KC_DEL,       KC_TRNS,
         KC_NO,          KC_NO,       MS_BTN2,       MS_BTN3,       MS_BTN1,        KC_NO,                                            KC_LEFT,      KC_DOWN,         KC_UP,      KC_RIGHT,         KC_NO,       KC_TRNS,
                         KC_NO,         KC_NO,         KC_NO,         KC_NO,        KC_NO,                                      RCTL(KC_LEFT),        KC_NO,         KC_NO, RCTL(KC_RIGHT),        KC_NO,
-                        KC_NO,         KC_NO,         KC_NO,         KC_NO,      KC_TRNS,  TO(LDEF), TO(LDEF),       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
+                        KC_NO,         KC_NO,         KC_NO,         KC_NO,     TO(LDEF),  TO(LDEF), TO(LDEF),       KC_NO,   KC_NO,   KC_NO,      KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS
     )
 };
 
