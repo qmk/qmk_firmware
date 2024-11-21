35dd9f7794b5124ddbe6973ca52c0384629c4095
Can Baytok <ctiomega.1@gmail.com>
2024-11-21 15:14:41 +0100

    Update keyboards/handwired/cans12erv2/keymaps/default/keymap.c
    Co-authored-by: jack <jack@pngu.org>

--- keyboards/handwired/cans12erv2/keymaps/default/keymap.c
+++ keyboards/handwired/cans12erv2/keymaps/default/keymap.c
@@ -2,15 +2,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
-#define _KEYPAD 0
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_KEYPAD] = LAYOUT(
+    [0] = LAYOUT(
         KC_A, KC_B, KC_C, KC_D,
         KC_E, KC_F, KC_G, KC_H,
         KC_I, KC_J, KC_K, KC_L
