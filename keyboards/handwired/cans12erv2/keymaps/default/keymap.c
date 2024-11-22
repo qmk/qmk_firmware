77c33ca329b1965b6b5f0648cafe1175f1f46d3e
Can Baytok <baytok@dydocon.com>
2024-11-22 13:27:00 +0100

    added keymap_german header

--- keyboards/handwired/cans12erv2/keymaps/default/keymap.c
+++ keyboards/handwired/cans12erv2/keymaps/default/keymap.c
@@ -1,6 +1,7 @@
 // Copyright 2024 Can Baytok (https://github.com/canbaytok)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
+#include "keymap_german.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
