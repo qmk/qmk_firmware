e45ba6bbe1e6fd261ce79353c67d777a62271bd1
Ryan <fauxpark@gmail.com>
2024-11-24 11:49:59 +1100

    Update keyboards/moky/moky67/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/moky/moky67/keymaps/default/keymap.c
+++ keyboards/moky/moky67/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
     [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
-    [1] = { ENCODER_CCW_CW(RM_PREV,  M_NEXT)},
+    [1] = { ENCODER_CCW_CW(RM_PREV,  RM_NEXT)},
     [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
     [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
 };
