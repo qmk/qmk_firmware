a5fccd220fc2e192e3d53b2024946cdd2848644b
Marco Pelegrini <mpelegrini@microsoft.com>
2024-11-24 20:22:41 -0800

    Move to RGB Matrix

--- keyboards/marcopad/keymaps/backlit/keymap.c
+++ keyboards/marcopad/keymaps/backlit/keymap.c
@@ -21,8 +21,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM backlight_next[] = {KC_P7, KC_P8, COMBO_END};
-const uint16_t PROGMEM backlight_toggle[] = {KC_P8, KC_P9, COMBO_END};
+const uint16_t PROGMEM backlight_toggle[] = {KC_P7, KC_P9, COMBO_END};
+const uint16_t PROGMEM hue_up[] = {KC_P7, KC_P4, COMBO_END};
+const uint16_t PROGMEM hue_down[] = {KC_P7, KC_P1, COMBO_END};
+const uint16_t PROGMEM sat_up[] = {KC_P7, KC_P5, COMBO_END};
+const uint16_t PROGMEM sat_down[] = {KC_P7, KC_P2, COMBO_END};
+const uint16_t PROGMEM value_up[] = {KC_P7, KC_P6, COMBO_END};
+const uint16_t PROGMEM value_down[] = {KC_P7, KC_P3, COMBO_END};
+const uint16_t PROGMEM speed_up[] = {KC_P8, KC_P4, COMBO_END};
+const uint16_t PROGMEM speed_down[] = {KC_P8, KC_P1, COMBO_END};
 combo_t key_combos[] = {
-    COMBO(backlight_next, UG_NEXT),
-    COMBO(backlight_toggle, UG_TOGG)
+    COMBO(backlight_next, RGB_MODE_FORWARD),
+    COMBO(backlight_toggle, RGB_TOG),
+    COMBO(hue_up, RGB_HUI),
+    COMBO(hue_down, RGB_HUD),
+    COMBO(sat_up, RGB_SAI),
+    COMBO(sat_down, RGB_SAD),
+    COMBO(value_up, RGB_VAI),
+    COMBO(value_down, RGB_VAD),
+    COMBO(speed_up, RGB_SPI),
+    COMBO(speed_down, RGB_SPD)
 };
--- keyboards/marcopad/keymaps/backlit/rules.mk
+++ keyboards/marcopad/keymaps/backlit/rules.mk
@@ -1,5 +1,4 @@
 # Copyright (c) 2022 Marco Pelegrini <marco@pelegrini.ca>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-RGBLIGHT_ENABLE = yes
 COMBO_ENABLE = yes
