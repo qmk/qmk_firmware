614eeaae8a828739792613c1270a7d417aa3211b
Stephen Onnen <stephen.onnen@gmail.com>
2024-11-27 19:17:53 -0600

    Update keyboards/onnenon/hotdog_pad/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/onnenon/hotdog_pad/keymaps/default/keymap.c
+++ keyboards/onnenon/hotdog_pad/keymaps/default/keymap.c
@@ -12,9 +12,3 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = {ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
 };
 #endif
-
-// Enable the power pin for the Xiao Seeed rp2040 onboard NeoPixel
-void matrix_init_kb(void) {
-    gpio_set_pin_output(GP11);
-    gpio_write_pin_high(GP11);
-}
