6f7f670306dc802bfa2bcf891969d782418cc8d4
Stephen Onnen <stephen.onnen@gmail.com>
2024-11-27 19:16:56 -0600

    Update keyboards/onnenon/hotdog_pad/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/onnenon/hotdog_pad/keymaps/default/keymap.c
+++ keyboards/onnenon/hotdog_pad/keymaps/default/keymap.c
@@ -3,7 +3,9 @@
 
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_A, KC_B, KC_C, KC_D, KC_MUTE)};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(KC_A, KC_B, KC_C, KC_D, KC_MUTE)
+};
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
