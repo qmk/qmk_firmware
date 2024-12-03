269ea335bb295804b5c840140177bb3721442f59
Sắn <59417802+MaiTheSan@users.noreply.github.com>
2024-12-03 08:13:36 +0700

    Update keymap.c

--- keyboards/salane/starryfrl/keymaps/default/keymap.c
+++ keyboards/salane/starryfrl/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-z/* Copyright 2023 SawnsProjects
+/* Copyright 2023 SawnsProjects
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
