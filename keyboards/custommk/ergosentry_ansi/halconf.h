80f10c6497387dda842202a3d62e2a84c8707b71
David Hoelscher <infinityis@users.noreply.github.com>
2024-12-03 21:01:57 -0600

    adding ergosentry ansi

---
+++ keyboards/custommk/ergosentry_ansi/halconf.h
@@ -0,0 +1,18 @@
+// Copyright 2025 customMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define HAL_USE_PWM TRUE
+
+#define HAL_USE_SPI TRUE
+
+#define HAL_USE_GPT TRUE
+
+#define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
+
+#define SERIAL_BUFFERS_SIZE 256
+
+#define SPI_USE_WAIT TRUE
+
+#include_next <halconf.h>
