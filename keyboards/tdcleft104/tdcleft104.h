ce87d1ce9f8c7a39ef09d3cedc09fcf16fb26d83
TDC <64564678+kthorpe88@users.noreply.github.com>
2024-12-05 17:50:43 -0500

    Update tdcleft104.h

--- keyboards/tdcleft104/tdcleft104.h
+++ keyboards/tdcleft104/tdcleft104.h
@@ -1,4 +1,4 @@
-// Copyright 2022 TDC
+// Copyright 2024 TDC
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
