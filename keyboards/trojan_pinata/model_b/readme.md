80c05a298261aa709928ef29bc768d91d01cdea5
Brian Hill <hillbr18@gmail.com>
2024-12-05 23:19:42 -0500

    PR changes for Model-B

--- keyboards/trojan_pinata/model_b/readme.md
+++ keyboards/trojan_pinata/model_b/readme.md
@@ -19,10 +19,6 @@ Flashing example for this keyboard:
 
     make trojan_pinata/model-b/rev0:default:flash
 
-Compile example:
-
-    qmk compile -kb trojan_pinata/model_b/rev0 -km default
-
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
 
 ## Bootloader
--- keyboards/trojan_pinata/model_b/rev0/config.h
+++ keyboards/trojan_pinata/model_b/rev0/config.h
@@ -3,25 +3,12 @@
 
 #pragma once
 
-
-#define DEBUG_MATRIX_SCAN_RATE
-
-//#define QMK_WAITING_TEST_BUSY_PIN GP8
-//#define QMK_WAITING_TEST_YIELD_PIN GP9
-
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 
 // settings for the oled keyboard demo with Adafruit 0.91" OLED display on the Stemma QT port
-#define OLED_DISPLAY_128X32
 #define I2C_DRIVER I2CD0
 #define I2C1_SDA_PIN GP0
 #define I2C1_SCL_PIN GP1
 
-#define OLED_BRIGHTNESS 255 // you can reduce the brightness, but not make it brighter!
 #define OLED_TIMEOUT 3000000 // 5 minutes before oled turns off
-
-#define ENCODER_A_PINS { GP27, GP26 }
-
-#define ENCODER_RESOLUTION 4
