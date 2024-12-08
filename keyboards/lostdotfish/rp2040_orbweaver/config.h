a22ecbba4914615c3987b0f71fa60b7457f48ee1
Lostdotfish <davidharley@gmail.com>
2024-12-08 16:44:58 +0000

    Update keyboards/lostdotfish/rp2040_orbweaver/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/lostdotfish/rp2040_orbweaver/config.h
+++ keyboards/lostdotfish/rp2040_orbweaver/config.h
@@ -20,10 +20,3 @@
 #define I2C1_SCL_PIN GP13
 #define IS31FL3731_I2C_ADDRESS_1 IS31FL3731_I2C_ADDRESS_GND
 #define I2C1_CLOCK_SPEED 100000
-#define RGB_MATRIX_TIMEOUT 1000000 // number of milliseconds to wait until rgb automatically turns off
-#define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-#define RGB_MATRIX_DEFAULT_VAL 200
-#define RGB_MATRIX_DEFAULT_ON true // Sets the default enabled state, if none has been set
-#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
-#define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
-#define RGB_MATRIX_DEFAULT_SPD 127 // Sets the default animation speed, if none has been set
