0ec696c7a7f6e17b8e154b0409794275f58a5ccb
Feags <41646528+Feags@users.noreply.github.com>
2024-12-07 18:31:58 -0600

    Adding layer access to Promenade RP24S
    Adding layer access to layers 1 and 2

--- keyboards/kradoindustries/promenade_rp24s/keymaps/default/keymap.c
+++ keyboards/kradoindustries/promenade_rp24s/keymaps/default/keymap.c
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [3] = LAYOUT( /* Layer Three */
 
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-  _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_BSLS,
+  _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, DF(0)  , DF(1)  , _______, _______, _______, _______, KC_PSCR, KC_BSLS,
   _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, KC_QUOT,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, KC_CAPS,
+  _______, _______, _______, _______, _______, _______, DF(2)  , _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, KC_CAPS,
   _______, _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______, QK_BOOT
   )
 
