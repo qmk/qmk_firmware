52492d319304f6baae768ec6c9bbaf91206bb647
Bertrand Le roy <bertrandleroy@gmail.com>
2024-12-09 23:48:43 -0800

    DecenTKL main keyboard

---
+++ keyboards/decent/tkl/keymaps/default/config.h
@@ -0,0 +1,14 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 6
+
+#define I2C1_SDA_PIN GP26
+#define I2C1_SCL_PIN GP27
+
+#define RGB_MATRIX_LED_COUNT 82
+#define RGB_MATRIX_CENTER {77, 32}
+#define OLED_FONT_H "atari-font.c"
