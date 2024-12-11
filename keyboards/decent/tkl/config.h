81d27b732e804939a31e482e505046ececc96d38
Bertrand Le roy <bertrandleroy@gmail.com>
2024-12-10 17:06:11 -0800

    Address code review comments.

--- keyboards/decent/tkl/config.h
+++ keyboards/decent/tkl/config.h
@@ -3,12 +3,7 @@
 
 #pragma once
 
-#define BOOTMAGIC_ROW 0
-#define BOOTMAGIC_COLUMN 6
-
 #define I2C1_SDA_PIN GP26
 #define I2C1_SCL_PIN GP27
 
-#define RGB_MATRIX_LED_COUNT 82
-#define RGB_MATRIX_CENTER {77, 32}
 #define OLED_FONT_H "atari-font.c"
