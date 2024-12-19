43917bfc3638ebde58bc8c2b6b8cbc477343b646
arcmags <c.magyar.ec@gmail.com>
2024-12-18 21:50:30 -0600

    missing LAYOUTS added to novelkeys/nk87*/rules.mk

--- keyboards/novelkeys/nk87/rules.mk
+++ keyboards/novelkeys/nk87/rules.mk
@@ -11,3 +11,5 @@ SRC =	keyboards/wilba_tech/wt_main.c \
 		keyboards/wilba_tech/wt_rgb_backlight.c \
 		drivers/led/issi/is31fl3733.c \
 		quantum/color.c
+
+LAYOUTS = tkl_f13_ansi_tsangan
--- keyboards/novelkeys/nk87b/rules.mk
+++ keyboards/novelkeys/nk87b/rules.mk
@@ -1,2 +1,4 @@
 # Wildcard to allow APM32 MCU
 DFU_SUFFIX_ARGS = -v FFFF -p FFFF
+
+LAYOUTS = tkl_f13_ansi_tsangan
