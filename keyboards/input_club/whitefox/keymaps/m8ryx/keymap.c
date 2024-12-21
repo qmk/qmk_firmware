a675bd6d90b43fa0660a49ee904e868b22fcd9f4
Rick Rezinas <rick@rezinas.com>
2024-12-21 14:15:38 -0800

    qwerty mode

--- keyboards/input_club/whitefox/keymaps/m8ryx/keymap.c
+++ keyboards/input_club/whitefox/keymaps/m8ryx/keymap.c
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _BASE 0
 #define _FN 1
 #define _NOHRMOD 3
+#define _QWERTY 4
 
 
 
@@ -42,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_LBRC,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_SLSH, KC_BSPC, KC_DEL,
         MEH_T(KC_ESC), LGUI_T(KC_A),    LALT_T(KC_R),    LCTL_T(KC_S),    LSFT_T(KC_T),    HYPR_T(KC_G),    KC_RBRC,    HYPR_T(KC_M),    LSFT_T(KC_N),    LCTL_T(KC_E),    LALT_T(KC_I), LGUI_T(KC_O), MEH_T(KC_ENT),  KC_PGUP,
         KC_LSFT, KC_X, KC_C,    KC_D,    KC_V,    KC_Z,    KC_BSLS, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_RSFT, KC_UP,   KC_PGDN,
-        MO(1), KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        MO(1), KC_LALT, KC_LGUI,                            KC_SPC,                    TO(_QWERTY), KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     /*
      * ┌───┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐───┬───┌───┐
@@ -71,5 +72,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,       KC_X,    KC_C,    KC_D,    KC_V,    KC_Z,    KC_BSLS,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_RSFT, KC_UP,                     KC_PGDN,
         MO(1),         KC_LALT, KC_LGUI,                            KC_SPC,                       KC_RGUI, KC_RALT,                   KC_LEFT, KC_DOWN, KC_RGHT
 
+    ),
+    [_QWERTY] = LAYOUT_65_ansi_blocker_split_bs(
+        KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV, KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_QUOT, KC_EQL, TO(_NOHRMOD),
+        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_SCLN,KC_SLSH, KC_BSPC,    KC_DEL,
+        MEH_T(KC_ESC),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,  KC_RBRC, KC_H,   KC_J,   KC_K,   KC_L,   KC_NUHS,KC_ENT,      KC_PGUP,
+        KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  KC_BKLS,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_RSFT,     KC_UP,  KC_PGDN,
+        KC_LCTL,KC_LGUI,KC_LALT,               KC_SPC,           KC_RALT,TO(_BASE),  KC_RCTL,     KC_LEFT,KC_DOWN,KC_RGHT
     )
 };
+
