4e67f9db0e62d379e2645cae554598c82ead28f5
Philip <68480429+iliorik@users.noreply.github.com>
2024-12-25 19:52:11 +0300

    I rewrote the layout so that the halves are not stacked.

--- keyboards/handwired/dactyl_manuform/5x8/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x8/keymaps/default/keymap.c
@@ -40,24 +40,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x8(
-        // left hand
-        KC_ESC,    KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
-        DM_RSTP,   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   QK_BOOT,
-        DM_REC1,   KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_F7,
-        DM_REC2,   KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-                   KC_LCTL, KC_LGUI, KC_LALT, KC_INS,
-				    TT(1), KC_SPC,
-                                    KC_ESC, KC_ENT,
-                                    KC_DEL, KC_F2,
-        // right hand
-                          KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_BSPC,
-                          KC_F8,   KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,
-                          KC_F9,   KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,   KC_ENT,
-                                   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   KC_RCTL,
-                                            KC_UP,    KC_DOWN, KC_LEFT,  KC_RGHT,
-             KC_BSPC, KC_SPC,
-             KC_HOME, KC_END,
-             KC_PGUP, KC_PGDN
+        KC_ESC,    KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,          KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,   KC_BSPC,
+        DM_RSTP,   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   QK_BOOT,       KC_F8,   KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,
+        DM_REC1,   KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_F7,         KC_F9,   KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,   KC_ENT,
+        DM_REC2,   KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,                  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,   KC_RCTL,
+                   KC_LCTL, KC_LGUI, KC_LALT, KC_INS,                                                  KC_UP,    KC_DOWN, KC_LEFT,  KC_RGHT,
+				                              TT(1), KC_SPC,                                  KC_BSPC, KC_SPC,
+                                                     KC_ESC, KC_ENT,                 KC_HOME, KC_END,
+                                                     KC_DEL, KC_F2,                  KC_PGUP, KC_PGDN
     ),
 /* Base (right to left on hold button)
      * ,-------------------------------------------------------,   ,-------------------------------------------------------,
@@ -80,23 +70,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_RIGHT2LEFT] = LAYOUT_5x8(
-        // left hand
-        KC_BSPC,   KC_BSLS,  KC_EQL,    KC_MINS,    KC_0,   KC_9,   KC_8,   KC_7,
-        KC_RBRC,   KC_LBRC,  KC_P,    KC_O,    KC_I,   KC_U,   KC_Y,   QK_BOOT,
-        DM_PLY1,   KC_QUOT, KC_SCLN,    KC_L,    KC_K,   KC_J,   KC_H,   KC_PSCR,
-        DM_PLY2,   OSM(MOD_LSFT), KC_SLSH,    KC_DOT,    KC_COMM,   KC_M,    KC_N,
-                   OSM(MOD_LCTL), OSM(MOD_LALT), KC_DEL, KC_BSPC,
-                                    KC_NO, KC_SPC,
-                                    KC_ESC, KC_ENT,
-                                    KC_DEL, KC_F2,
-        // right hand
-                          KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,  KC_NO,   KC_NO,   KC_BSPC,
-                          KC_NO,   KC_NO,    KC_NO,    KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,
-                          KC_NO,   KC_LEFT,    KC_DOWN,    KC_UP,     KC_RIGHT,     KC_SCLN,  KC_NO,   KC_ENT,
-                                   KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO,  KC_NO,   KC_NO,
-                                            KC_NO,    KC_NO, KC_NO,  KC_NO,
-             KC_BSPC, KC_SPC,
-             KC_HOME, KC_END,
-             KC_PGUP, KC_PGDN
+        KC_BSPC,   KC_BSLS,  KC_EQL,    KC_MINS, KC_0,   KC_9,   KC_8,   KC_7,         KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_BSPC,
+        KC_RBRC,   KC_LBRC,  KC_P,      KC_O,    KC_I,   KC_U,   KC_Y,   QK_BOOT,      KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,
+        DM_PLY1,   KC_QUOT,  KC_SCLN,   KC_L,    KC_K,   KC_J,   KC_H,   KC_PSCR,      KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT,   KC_SCLN, KC_NO,   KC_ENT,
+        DM_PLY2,  OSM(MOD_LSFT), KC_SLSH, KC_DOT, KC_COMM, KC_M, KC_N,                 KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,      KC_NO,   KC_NO,
+                   OSM(MOD_LCTL), OSM(MOD_LALT), KC_DEL, KC_BSPC,                                          KC_NO,    KC_NO,   KC_NO,      KC_NO,
+                                                         KC_NO,  KC_SPC,                          KC_BSPC, KC_SPC,
+                                                                 KC_ESC, KC_ENT,       KC_HOME,   KC_END,
+                                                                 KC_DEL, KC_F2,        KC_PGUP,   KC_PGDN
     ),
 };
