a86741e81456aba1383a198fb5b5c84ca992e3cf
Blake <blake@ghost7.com>
2024-12-26 16:41:38 +0000

    Update keyboards/draytronics/daisy_v2/daisy_v2.c
    Co-authored-by: jack <jack@pngu.org>

--- keyboards/draytronics/daisy_v2/daisy_v2.c
+++ keyboards/draytronics/daisy_v2/daisy_v2.c
@@ -14,19 +14,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-This code is inspired by and adapted from the code used in the Printed Pad by Noah Beidelman (@noahbei)
-
-It also references the concept of glitching animations from Aleks (@aleksbrgt)
-
-The pixel graphics used here are from a combination of sources;
-
-1. Layer indicators are created by myself and free to use by anyone.
-2. "Revengeday", "Cyber Cafe", "Cortex Implant" logos are used with kind permission of OBEY THE SYSTEM.
-   A collective of Fediverse instances and creatives. https://git.cyberwa.re/obey-the-system.
-   They are licenced as Non-Commercial and for use by members of the network, with attribution.
-3. Key press indicator graphics were commissioned for this project and were designed by the
-   amazing https://corteximplant.com/@jadedtwin / https://www.jadedtwin.com/
 */
 #include "quantum.h"
 
