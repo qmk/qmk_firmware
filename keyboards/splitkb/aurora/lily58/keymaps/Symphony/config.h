ec9dfc6ae9469221082559e5bc17f63254241e3a
Travis <travisredden@outlook.com>
2024-12-27 16:57:58 -0600

    added rgb matrix WIP

--- keyboards/splitkb/aurora/lily58/keymaps/Symphony/config.h
+++ keyboards/splitkb/aurora/lily58/keymaps/Symphony/config.h
@@ -6,8 +6,6 @@
 // RGB Matrix configuration
 #define RGB_DI_PIN D3                 // Pin connected to RGB
 #define DRIVER_LED_TOTAL 58           // Total number of LEDs
-#define RGB_MATRIX_SPLIT              // Enable split keyboard RGB support
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // Limit brightness
 #define RGB_MATRIX_KEYPRESSES         // Enable reactive keypress effects
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS // Enable framebuffer effects
 
