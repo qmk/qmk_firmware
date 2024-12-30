3f106a3e7ab37e200b1315a02f4baf20f210b174
Drashna Jael're <drashna@live.com>
2024-12-29 21:23:29 -0800

    Add WeAct STM32F405RG controller support to Tractyl Manuform

---
+++ keyboards/handwired/tractyl_manuform/5x6_right/f405/f405.c
@@ -0,0 +1,47 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tractyl_manuform.h"
+
+#ifdef USB_VBUS_PIN
+bool usb_vbus_state(void) {
+    gpio_set_pin_input_low(USB_VBUS_PIN);
+    wait_us(5);
+    return gpio_read_pin(USB_VBUS_PIN);
+}
+#endif
+
+void user_button_init(void) {
+    // Pin needs to be configured as input low
+#ifdef USER_BUTTON_PIN
+    gpio_set_pin_input_low(USER_BUTTON_PIN);
+#endif // USER_BUTTON_PIN
+}
+
+bool check_user_button_state(void) {
+    gpio_write_pin(DEBUG_LED_PIN, is_keyboard_master());
+#ifdef USER_BUTTON_PIN
+    return gpio_read_pin(USER_BUTTON_PIN);
+#endif // USER_BUTTON_PIN
+    return false;
+}
+
+void board_init(void) {
+    // unset improper SPI pins
+    gpio_set_pin_input(A5);
+    gpio_set_pin_input(A6);
+    gpio_set_pin_input(A7);
+}
