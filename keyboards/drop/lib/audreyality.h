9e0c3ae95ed4e715c259aa81e8122eb132712623
✨ Audrey ✨ <audrey@audreyality.com>
2025-01-05 15:27:19 -0500

    add LED matrix controls

--- keyboards/drop/lib/audreyality.h
+++ keyboards/drop/lib/audreyality.h
@@ -20,27 +20,12 @@ enum layer_names {
     _DFL_WINDOWS,
     _XTL_BASE,
     _XTL_FUNCTION,
+    _XTL_LED_MATRIX,
     _MDL_HELLDIVERS,
     // `AUD_TTL_CONTROL` has global navigation; it should override all layers
     _TTL_CONTROL
     // NB: max layer is 15
 };
-
-// layer control
-#define DF_MCOS DF(_DFL_MACOS)
-#define DF_WIND DF(_DFL_WINDOWS)
-#define OS_FUNL OSM(_XTL_FUNCTION)
-#define OS_CTLL OSM(_TTL_CONTROL)
-#define TG_NUMP XXXXXXX
-#define TG_PROG XXXXXXX
-#define TG_LEDM XXXXXXX
-#define TG_HELL TG(_MDL_HELLDIVERS)
-
-// these will be important for later
-#define ANIRSVP KC_TRNS
-#define XTLRSVP KC_TRNS
-#define FNLRSVP KC_TRNS
-#define MDLRSVP KC_TRNS
 
 enum custom_keycodes {
     // macOS Spotlight key
@@ -52,3 +37,19 @@ enum custom_keycodes {
     // return to the default layer
     E_X_I_T
 };
+
+// layer control
+#define DF_MCOS DF(_DFL_MACOS)
+#define DF_WIND DF(_DFL_WINDOWS)
+#define OS_FUNL OSM(_XTL_FUNCTION)
+#define OS_CTLL OSM(_TTL_CONTROL)
+#define TG_NUMP XXXXXXX
+#define TG_PROG XXXXXXX
+#define TG_LEDM TG(_XTL_LED_MATRIX)
+#define TG_HELL TG(_MDL_HELLDIVERS)
+
+// these will be important for later
+#define ANIRSVP KC_TRNS
+#define XTLRSVP KC_TRNS
+#define FNLRSVP KC_TRNS
+#define MDLRSVP KC_TRNS
