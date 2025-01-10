f9e249de938a26cdcc8566f4e6e82224285f336d
mizma <omoikane@path-works.net>
2025-01-10 15:51:50 +0900

    Add community layout "ortho_5x14"
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/mzmkb/slimdash/rev1/keymaps/default/keymap.c
+++ keyboards/mzmkb/slimdash/rev1/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | RM_DN| RM_UP| GAMEM|  GUI | Lower| Space| LSFT |                    | RCTL | Enter| Raise| HOME |  END |PageUp|PageDn|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
-    [_QWERTY] = LAYOUT(
+    [_QWERTY] = LAYOUT_ortho_5x14(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,            KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,            KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,            KC_INS,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      | NOOP |      |      |      |                    |      |      |      |   ←  |   ↓  |   ↑  |   →  |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
-    [_GAMEMODE] = LAYOUT(
+    [_GAMEMODE] = LAYOUT_ortho_5x14(
         _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |                    |      | PENT |      |   0  |   .  |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
-    [_LOWER] = LAYOUT(
+    [_LOWER] = LAYOUT_ortho_5x14(
         KC_GRV,  _______, _______, _______, _______, KC_MUTE, KC_VOLD,            KC_VOLU, _______, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, _______,
         _______, _______, _______, _______, RM_TOGG, _______, KC_BRID,            KC_BRIU, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
         KC_CAPS, _______, _______, RM_HUED, RM_HUEU, _______, RM_PREV,            RM_NEXT, _______, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, _______,
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
-    [_RAISE] = LAYOUT(
+    [_RAISE] = LAYOUT_ortho_5x14(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,            _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______, _______, _______, KC_WH_L, KC_WH_R, _______, _______,            _______, _______, _______, KC_PAUS, KC_SCRL, KC_PSCR, KC_F12,
         KC_CAPS, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_WH_U, _______,            _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
