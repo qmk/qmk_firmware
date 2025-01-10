4fa0346089be63fede11d45e1de27716f3edbbea
vinniefranco <vince@freshivore.net>
2025-01-10 13:38:14 -0600

    Final

--- keyboards/gggw/crosses/3x5/keymaps/default/keymap.c
+++ keyboards/gggw/crosses/3x5/keymaps/default/keymap.c
@@ -33,7 +33,6 @@ enum my_keycodes {
     DRAG_SCROLL,
 };
 
-
 enum combos {
     WY_TAB,
     DH_CAPS,
@@ -60,6 +59,8 @@ const uint16_t PROGMEM gm_combo[]     = {KC_G, KC_M, COMBO_END};
 const uint16_t PROGMEM comd_combo[]   = {KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM zdot_combo[]   = {KC_Z, KC_DOT, COMBO_END};
 
+// clang-format off
+//
 combo_t key_combos[] = {
     [WY_TAB] = COMBO(wy_combo, KC_TAB),
     [DH_CAPS] = COMBO(dh_combo, CW_TOGG),
@@ -79,20 +80,20 @@ combo_t key_combos[] = {
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌───────────┬───────────┬───────────┬───────────┬────────────┐                      ┌──────┬───────────┬───────────┬───────────┬───────────┐
-//    │     q     │     w     │     f     │     p     │     b      │                      │  j   │     l     │     u     │     y     │     ;     │
-//    ├───────────┼───────────┼───────────┼───────────┼────────────┤                      ├──────┼───────────┼───────────┼───────────┼───────────┤
-//    │ LGUI_T(a) │ LALT_T(r) │ LCTL_T(s) │ LSFT_T(t) │     g      │                      │  m   │ RSFT_T(n) │ RCTL_T(e) │ RALT_T(i) │ RGUI_T(o) │
-//    ├───────────┼───────────┼───────────┼───────────┼────────────┤                      ├──────┼───────────┼───────────┼───────────┼───────────┤
-//    │     z     │     x     │     c     │     d     │     v      │                      │  k   │     h     │     ,     │     .     │ LT(3, /)  │
-//    └───────────┴───────────┴───────────┼───────────┼────────────┼─────┐   ┌────────────┼──────┼───────────┼───────────┴───────────┴───────────┘
-//                                        │   MO(5)   │ LT(2, esc) │ spc │   │ LT(1, ent) │ bspc │   MO(4)   │
-//                                        └───────────┴────────────┴─────┘   └────────────┴──────┴───────────┘
+//    ┌───────────┬───────────┬───────────┬───────────┬────────────┐                      ┌──────┬────────────┬───────────┬───────────┬───────────┐
+//    │     q     │     w     │     f     │     p     │     b      │                      │  j   │     l      │     u     │     y     │     ;     │
+//    ├───────────┼───────────┼───────────┼───────────┼────────────┤                      ├──────┼────────────┼───────────┼───────────┼───────────┤
+//    │ LGUI_T(a) │ LALT_T(r) │ LCTL_T(s) │ LSFT_T(t) │     g      │                      │  m   │ RSFT_T(n)  │ RCTL_T(e) │ RALT_T(i) │ RGUI_T(o) │
+//    ├───────────┼───────────┼───────────┼───────────┼────────────┤                      ├──────┼────────────┼───────────┼───────────┼───────────┤
+//    │     z     │     x     │     c     │     d     │     v      │                      │  k   │     h      │     ,     │     .     │ LT(3, /)  │
+//    └───────────┴───────────┴───────────┼───────────┼────────────┼─────┐   ┌────────────┼──────┼────────────┼───────────┴───────────┴───────────┘
+//                                        │   MO(5)   │ LT(2, esc) │ spc │   │ LT(1, ent) │ bspc │ LT(4, del) │
+//                                        └───────────┴────────────┴─────┘   └────────────┴──────┴────────────┘
 [_BASE] = LAYOUT_default(
-  KC_Q         , KC_W         , KC_F         , KC_P         , KC_B          ,                              KC_J    , KC_L         , KC_U         , KC_Y         , KC_SCLN       ,
-  LGUI_T(KC_A) , LALT_T(KC_R) , LCTL_T(KC_S) , LSFT_T(KC_T) , KC_G          ,                              KC_M    , RSFT_T(KC_N) , RCTL_T(KC_E) , RALT_T(KC_I) , RGUI_T(KC_O)  ,
-  KC_Z         , KC_X         , KC_C         , KC_D         , KC_V          ,                              KC_K    , KC_H         , KC_COMM      , KC_DOT       , LT(3, KC_SLSH),
-                                               MO(5)        , LT(2, KC_ESC) , KC_SPC ,     LT(1, KC_ENT) , KC_BSPC , MO(4)
+  KC_Q         , KC_W         , KC_F         , KC_P         , KC_B          ,                              KC_J    , KC_L          , KC_U         , KC_Y         , KC_SCLN       ,
+  LGUI_T(KC_A) , LALT_T(KC_R) , LCTL_T(KC_S) , LSFT_T(KC_T) , KC_G          ,                              KC_M    , RSFT_T(KC_N)  , RCTL_T(KC_E) , RALT_T(KC_I) , RGUI_T(KC_O)  ,
+  KC_Z         , KC_X         , KC_C         , KC_D         , KC_V          ,                              KC_K    , KC_H          , KC_COMM      , KC_DOT       , LT(3, KC_SLSH),
+                                               MO(5)        , LT(2, KC_ESC) , KC_SPC ,     LT(1, KC_ENT) , KC_BSPC , LT(4, KC_DEL)
 ),
 
 //    ┌──────┬───┬───┬───────────┬──────┐               ┌──────┬───────────┬───────────┬──────┬─────┐
@@ -175,6 +176,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 KC_TRNS     , KC_NO , KC_NO ,     KC_NO , KC_NO  , KC_NO
 )
 };
+
+// clang-format on
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!process_achordion(keycode, record)) {
@@ -284,16 +287,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
-        0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
-        0x90, 0x91, 0x92, 0x93, 0x94, 0xA0, 0xA1, 0xA2,
-        0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA,
-        0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2,
-        0xB3, 0xB4, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5,
-        0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD,
-        0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
+    static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00};
 
     oled_write_P(qmk_logo, false);
 }
@@ -301,13 +295,8 @@ static void render_logo(void) {
 bool oled_task_user(void) {
     render_logo();
 
-    const char *layer_names[] = {
-        [ _BASE ] = ">> BASE ",
-        [ _NUM ] = ">> PROG ",
-        [ _NAV ] = ">> NAVI ",
-        [ _MEDIA ] = ">>> MEDIA ",
-        [ _FUNC ] = ">>> FUNC ",
-        [ _MOUS ] = ">>> MOUSE ",
+    const char* layer_names[] = {
+        [_BASE] = ">> BASE ", [_NUM] = ">> PROG ", [_NAV] = ">> NAVI ", [_MEDIA] = ">>> MEDIA ", [_FUNC] = ">>> FUNC ", [_MOUS] = ">>> MOUSE ",
     };
 
     oled_write_P(PSTR(layer_names[get_highest_layer(layer_state)]), false);
