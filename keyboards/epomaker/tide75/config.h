d049ed4bddd7f34faad3f045084e94d83e1ab61b
JD Davis <mxjerrett@gmail.com>
2025-01-11 21:56:02 -0600

    chore: added pragma

--- keyboards/epomaker/tide75/config.h
+++ keyboards/epomaker/tide75/config.h
@@ -1,5 +1,7 @@
 // Copyright 2024 Your Name (@JerrettDavis)
 // SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
 #define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
 #define RGB_MATRIX_CENTER { 112, 32 }
 #define ENCODER_RESOLUTION 2
