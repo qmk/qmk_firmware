13e199b804c6419a3511bc0a9aa70e5bedd66cd5
JD Davis <mxjerrett@gmail.com>
2025-01-11 21:54:36 -0600

    chore: added license to config.

--- keyboards/epomaker/tide75/config.h
+++ keyboards/epomaker/tide75/config.h
@@ -1,3 +1,5 @@
+// Copyright 2024 Your Name (@JerrettDavis)
+// SPDX-License-Identifier: GPL-2.0-or-later
 #define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
 #define RGB_MATRIX_CENTER { 112, 32 }
 #define ENCODER_RESOLUTION 2
