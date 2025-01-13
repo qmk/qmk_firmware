2894f10b4e0315a81dccbe322ca1e3361ef84e61
NaturalZh <zhouziran@jikedingzhi.com>
2025-01-13 14:06:05 +0800

    modified skyloong/gk104/q1/q1.c

--- keyboards/skyloong/gk104/q1/q1.c
+++ keyboards/skyloong/gk104/q1/q1.c
@@ -250,7 +250,7 @@ layer_state_t default_layer_state_set_kb(layer_state_t state) {
         break;
     }
     s_serial_to_parallel(IND);
-  return state;
+   return default_layer_state_set_user(state);
 }
 
 
