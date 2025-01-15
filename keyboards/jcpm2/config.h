b7de9259f233bac18dae5afdf12d19c019025c01
Jeremy Cook <jscook55@gmail.com>
2025-01-15 08:19:37 -0500

    Update keyboards/jcpm2/config.h
    Co-authored-by: jack <jack@pngu.org>

--- keyboards/jcpm2/config.h
+++ keyboards/jcpm2/config.h
@@ -17,11 +17,5 @@
 #pragma once
 
 // I2C Configuration
-#define I2C_DRIVER I2CD1
-#define I2C1_SCL_PIN D0
-#define I2C1_SDA_PIN D1
-
-// OLED Configuration
 #define OLED_DISPLAY_128X64
-#define OLED_DISPLAY_ADDRESS 0x3C
 #define OLED_TIMEOUT 2000000
