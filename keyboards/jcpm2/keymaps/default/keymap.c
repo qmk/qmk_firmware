f777b4178d2d6612785f2cf942b9e0f35d35bc44
Jeremy Cook <jscook55@gmail.com>
2025-01-15 21:46:04 -0500

    Update keyboards/jcpm2/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/jcpm2/keymaps/default/keymap.c
+++ keyboards/jcpm2/keymaps/default/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-#ifdef ENCODER_MAP_ENABLE //defined in rules.mk
+#ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_LAYER0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }, // default
     [_LAYER0_MOD] = { ENCODER_CCW_CW(_______, _______) }, // default MOD
