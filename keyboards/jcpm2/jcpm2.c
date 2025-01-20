08464506e5d5919112dbfe2dfe274f167889483f
Jeremy Cook <jscook55@gmail.com>
2025-01-20 13:20:41 -0500

    Update keyboards/jcpm2/jcpm2.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/jcpm2/jcpm2.c
+++ keyboards/jcpm2/jcpm2.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- #include "quantum.h"
+#include "quantum.h"
  
 #ifdef OLED_ENABLE
 static uint32_t logo_timer = 0;
