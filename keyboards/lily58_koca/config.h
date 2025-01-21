764a59bb6b83df599be283368389ff8a662ced97
oxnh <77563973+Hardware7253@users.noreply.github.com>
2025-01-21 23:30:16 +1300

    add license headers

--- keyboards/lily58_koca/config.h
+++ keyboards/lily58_koca/config.h
@@ -1,3 +1,5 @@
+/* Copyright 2025 oxnh */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN C10    // USART TX pin
 #define SERIAL_USART_RX_PIN C11    // USART RX pin
--- keyboards/lily58_koca/keymaps/default/keymap.c
+++ keyboards/lily58_koca/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright 2025 oxnh */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H
 
