6b2c4aae84ab80cb697f5ab48a9938a805260200
Joy Lee <joylee.lc@foxmail.com>
2025-01-23 06:42:03 +0800

    Update keymap.c
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/sharkoon/skiller_sgk50_s4/keymaps/default/keymap.c
+++ keyboards/sharkoon/skiller_sgk50_s4/keymaps/default/keymap.c
@@ -13,10 +13,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [1] = LAYOUT_60_iso(
-         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-         _______, _______, _______, _______, _______,  _______, KC_INS,  _______,  KC_UP,    KC_PAUS, KC_PGUP, KC_HOME, KC_PSCR,
-         _______, _______, _______, _______, _______,  _______, KC_SCRL, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN, KC_END,  RM_VALU, _______,
-         _______, _______, _______, _______, _______,  _______, _______, _______,  KC_MUTE,  KC_VOLD, KC_VOLU, RM_VALD,          _______,
-         _______, GU_TOGG, _______,                             _______,                              _______, _______, RM_HUEU, RM_NEXT
-    ),
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, _______, _______, _______, _______,  _______, KC_INS,  _______,  KC_UP,    KC_PAUS, KC_PGUP, KC_HOME, KC_PSCR,
+        _______, _______, _______, _______, _______,  _______, KC_SCRL, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_PGDN, KC_END,  RM_VALU, _______,
+        _______, _______, _______, _______, _______,  _______, _______, _______,  KC_MUTE,  KC_VOLD, KC_VOLU, RM_VALD,          _______,
+        _______, GU_TOGG, _______,                             _______,                              _______, _______, RM_HUEU, RM_NEXT
+    )
 };
