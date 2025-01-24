591d5970863996df836bfabb8d7c05c2b751ea6b
dnlnm <danielnaim5543@gmail.com>
2025-01-25 01:24:10 +0800

    Fixed number of layers for rgblight

--- keyboards/dnlnm/cloak/keymaps/default/keymap.c
+++ keyboards/dnlnm/cloak/keymaps/default/keymap.c
@@ -41,17 +41,12 @@ const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {4, 2, HSV_BLUE}
 );
-// Light LEDs 8 & 9 in blue when keyboard layer 3 is active
-const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {8, 2, HSV_BLUE}
-);
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer,
     my_layer1_layer,    // Overrides caps lock layer
-    my_layer2_layer,    // Overrides other layers
-    my_layer3_layer     // Overrides other layers
+    my_layer2_layer    // Overrides other layers
 );
 
 void keyboard_post_init_user(void) {
@@ -67,7 +62,6 @@ bool led_update_user(led_t led_state) {
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(1, layer_state_cmp(state, 1));
     rgblight_set_layer_state(2, layer_state_cmp(state, 2));
-    rgblight_set_layer_state(3, layer_state_cmp(state, 3));
     return state;
 }
 
