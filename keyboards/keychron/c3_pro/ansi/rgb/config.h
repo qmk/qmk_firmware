6d48ffcfbc88637ddf9161b2bc55117e3c699484
adophoxia <100170946+adophoxia@users.noreply.github.com>
2025-01-27 18:35:14 -0800

    Apply suggestions from code review
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/keychron/c3_pro/ansi/rgb/config.h
+++ keyboards/keychron/c3_pro/ansi/rgb/config.h
@@ -21,13 +21,9 @@
 #define LED_WIN_OS_PIN A7
 #define LED_OS_PIN_ON_STATE 1
 
-#ifdef RGB_MATRIX_ENABLE
 /* RGB Matrix Driver Configuration */
 #define SNLED27351_I2C_ADDRESS_1 SNLED27351_I2C_ADDRESS_VDDIO
 #define SNLED27351_I2C_ADDRESS_2 SNLED27351_I2C_ADDRESS_GND
 
 /* Set LED driver current */
-#define SNLED27351_CURRENT_TUNE \
-    { 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30 }
-#endif
-
+#define SNLED27351_CURRENT_TUNE { 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30 }
