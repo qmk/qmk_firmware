eac4b189482668697547567e0cf9f9e707e1e9c1
Topher Ludlow <topherludlow@protonmail.com>
2025-02-04 13:10:24 -0800

    Changed keymap

--- keyboards/topher/tophactyl/keymaps/topher/keymap.c
+++ keyboards/topher/tophactyl/keymaps/topher/keymap.c
@@ -10,12 +10,7 @@ enum layers {
 };
 enum tophactyl_keycodes {
     QW = SAFE_RANGE,
-    XCOPY, XPASTE,
-    TabLeft, TabRight,
-    STabLeft, STabRight,
-    TTY1,
-    TTY2,
-    TTY3,
+    AltTab,
 };
 
 enum tap_dance_keycodes {
@@ -52,21 +47,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOW] = LAYOUT(
     TD(_1_WS1), TD(_2_WS2), TD(_3_WS3), KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
     QW, _______, KC_LCBR, KC_RCBR, KC_COLN, KC_GRV, KC_MINS, KC_PLUS, KC_PIPE, KC_DEL,
-    _______, _______, XCOPY, XPASTE, _______, _______, KC_QUOT, _______, _______, KC_BSLS,
+    _______, _______, _______, _______, _______, _______, KC_QUOT, _______, _______, KC_BSLS,
     _______, _______, _______, GAME, _______, _______ 
     ),
 
     [_LOWER] = LAYOUT(
     TD(EXLM_MWS1), TD(AT_MWS3), TD(HASH_MWS3), KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, 
     KC_ESC, _______, KC_LBRC, KC_RBRC, KC_SCLN, _______, KC_UNDS, KC_EQL, _______, _______, 
-    _______, _______, _______, _______, _______, TabLeft, KC_PGDN, KC_PGUP, TabRight, _______, 
+    _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, 
     _______, _______, _______, _______, _______, _______ 
     ),
 
     [_LOWEST] = LAYOUT(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, 
-    KC_F11, KC_F12, TTY1, TTY2, TTY3, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, 
-    _______, _______, _______, _______, _______, STabLeft, _______, _______, STabRight, _______, 
+    KC_F11, KC_F12, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
     _______, QK_RBT, QK_BOOT, _______, _______, _______ 
     ),
 
@@ -80,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_GAME] = LAYOUT(
     KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_BSPC,
-    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, _______, KC_TAB, KC_ESC, 
-    		      KC_COMM, KC_DOT, KC_SLSH, GAME, KC_RSFT, KC_ENT
+    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_RSFT, AltTab, KC_ESC, 
+    		      KC_COMM, KC_DOT, KC_SLSH, GAME, KC_SPC, KC_ENT
     ),
 };
 
@@ -100,17 +95,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     }
     return update_tri_layer_state(state, _LOW, _LOWER, _LOWEST);
 }
-
-
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     switch(get_highest_layer(state)) {
-//         case _LOW:
-//         case _LOWER:
-//             auto_mouse_trigger_reset(true);
-//             break;
-//     }
-//     return state;
-// }
 
 void pointing_device_init_user(void) {
     set_auto_mouse_layer(_MOUSE);
@@ -149,7 +133,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [_QWERTY] = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
     [_LOW] = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
-    [_LOWER] = { ENCODER_CCW_CW(STabRight, STabLeft) },
+    [_LOWER] = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
     [_LOWEST] = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
     [_MOUSE] = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
     [_GAME] = { ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
@@ -253,88 +237,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             unregister_code(KC_LSFT);
         }
         break;      
-        case XCOPY:
-        if (record->event.pressed)
-        {
-            register_code(KC_LCTL);
-            register_code(KC_LSFT);
-            tap_code(KC_C);
-            unregister_code(KC_LCTL);
-            unregister_code(KC_LSFT);
-        }
-        break;
-        case XPASTE:
-        if (record->event.pressed)
-        {
-            register_code(KC_LSFT);
-            tap_code(KC_INS);
-            unregister_code(KC_LSFT);
-        }
-        break;
-        case TabLeft:
-        if (record->event.pressed)
-        {
-            register_code(KC_LCTL);
-            tap_code(KC_PGUP);
-            unregister_code(KC_LCTL);
-        }
-        break;
-        case TabRight:
-        if (record->event.pressed)
-        {
-            register_code(KC_LCTL);
-            tap_code(KC_PGDN);
-            unregister_code(KC_LCTL);
-        }
-        break;
-        case STabLeft:
-        if (record->event.pressed)
-        {
-            register_code(KC_LSFT);
-            register_code(KC_LCTL);
-            tap_code(KC_PGUP);
-            unregister_code(KC_LSFT);
-            unregister_code(KC_LCTL);
-        }
-        break;
-        case STabRight:
-        if (record->event.pressed)
-        {
-            register_code(KC_LSFT);
-            register_code(KC_LCTL);
-            tap_code(KC_PGDN);
-            unregister_code(KC_LSFT);
-            unregister_code(KC_LCTL);
-        }
-        break;
-        case TTY1:
+        case AltTab:
         if (record->event.pressed)
         {
             register_code(KC_LALT);
-            register_code(KC_LCTL);
-            tap_code(KC_F1);
+            tap_code(KC_TAB);
             unregister_code(KC_LALT);
-            unregister_code(KC_LCTL);
-        }
-        break;
-        case TTY2:
-        if (record->event.pressed)
-        {
-            register_code(KC_LALT);
-            register_code(KC_LCTL);
-            tap_code(KC_F2);
-            unregister_code(KC_LALT);
-            unregister_code(KC_LCTL);
-        }
-        break;
-        case TTY3:
-        if (record->event.pressed)
-        {
-            register_code(KC_LALT);
-            register_code(KC_LCTL);
-            tap_code(KC_F3);
-            unregister_code(KC_LALT);
-            unregister_code(KC_LCTL);
         }
         break;
         }
