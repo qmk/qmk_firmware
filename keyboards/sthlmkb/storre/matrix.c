5ce3c457aad0abccb03688e053393fa8a1e633aa
Moses Hoyt <mohoyt@users.noreply.github.com>
2025-02-05 18:03:50 +0100

    Update matrix.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/sthlmkb/storre/matrix.c
+++ keyboards/sthlmkb/storre/matrix.c
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "wait.h"
 #include "matrix.h"
 
 #define COL_SHIFTER ((uint32_t)1)
