0c8ffadd4840762bd8a402729154aa44facad372
Joel Beckmeyer <joel@beckmeyer.us>
2025-02-07 11:14:16 -0500

    add info.json

--- keyboards/anne_pro/config.h
+++ keyboards/anne_pro/config.h
@@ -17,58 +17,8 @@
 #pragma once
 
 
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 14
-
-#define MATRIX_ROW_PINS { B9, B8, B7, B6, A0 }
-#define MATRIX_COL_PINS { A5, A6, A7, B0, B1, B12, B13, B14, A8, A9, A15, B3, B4, B5 }
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
-
 /*
  * Timeout after which the backlight of the keyboard is disabled if no keypresses are received.
  * If this feature is not needed you can set the timeout to UINT32_MAX, this is more than 100 years.
  */
 #define BACKLIGHT_TIMEOUT 60
-
-/*
- * Force NKRO
- *
- * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
- * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
- * makefile for this to work.)
- *
- * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
- * until the next keyboard reset.
- *
- * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
- * fully operational during normal computer usage.
- *
- * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
- * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
- * bootmagic, NKRO mode will always be enabled until it is toggled again during a
- * power-up.
- *
- */
-//#define FORCE_NKRO
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
--- keyboards/anne_pro/rules.mk
+++ keyboards/anne_pro/rules.mk
@@ -29,13 +29,5 @@ SRC += uart_tx_ringbuf.c anne_pro_lighting.c anne_pro_bluetooth.c
 # Required for functionality with Bluetooth
 NO_USB_STARTUP_CHECK = yes
 
-# Build Options
-#   comment out to disable the options.
-#
-MOUSEKEY_ENABLE = no  # Mouse keys
-EXTRAKEY_ENABLE = yes # Audio control and System control
-NKRO_ENABLE = yes     # USB Nkey Rollover
-EEPROM_DRIVER = transient
-
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
