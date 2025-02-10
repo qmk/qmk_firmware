37f048ceb0e8bb9c0cce86f7e13d7086506e51c5
MyBestFriendMe <codylmcfadden@gmail.com>
2025-02-10 14:22:30 -0600

    keymap rebuild

--- keyboards/handwired/dactyl_cc/5x7_Trackball/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_cc/5x7_Trackball/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_CUT,
         KC_TAB,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_CALC,
         KC_CAPS,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-        TG(1),   KC_LCTL, KC_LEFT, KC_RGHT,KC_LALT,
+        TG(1),     KC_GRV,  KC_LEFT, KC_RGHT,KC_LALT,
                                     KC_LCTL, KC_HOME,
                                     KC_END,
                                     KC_DEL, KC_BSPC, KC_LSFT,
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_CALC, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,
                           KC_COPY, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
                           KC_PSTE, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                                   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  MO(2),
+                                   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  MO(1),
                                             KC_ESC,  KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
              KC_RSFT, KC_SPC, KC_ENT,
              KC_LGUI, MS_BTN3, DRAG_SCROLL, MS_BTN2, MS_BTN1
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT(
         // left hand
-        _______,   KC_F1,     KC_F2,      KC_F3,    KC_F4,     KC_F5,    KC_F6,
+        KC_F1,     KC_F2,     KC_F3,      KC_F4,    KC_F5,     KC_F6,    _______,
         _______,   _______,   _______,    KC_UP,    _______,   _______,  _______,
         _______,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   _______,  QK_BOOT,
         _______,   _______,   _______,   _______,   _______,   _______,
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______, _______,
                                     _______,
         // right hand
-                          KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,
-                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
-                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
-                                     _______,   _______,   _______,   _______,   _______,   _______,
-                                                _______,   _______,   _______,   _______,   _______,
+                          _______,   KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,
+                          _______,   _______,   _______,   _______,   _______,   _______,   RGB_MOD,
+                          _______,   _______,   _______,   _______,   _______,   _______,   RGB_RMOD,
+                                     _______,   _______,   _______,   _______,   _______,   MO(2),
+                                                _______,   _______,   _______,   _______,   RGB_TOG,
              _______, _______, _______,
 
              _______, _______, _______, _______, _______
@@ -76,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______, _______,
                                     _______,
         // right hand
-                          _______,   _______,   KC_NUM,    KC_PSLS,   KC_PAST,   KC_PMNS,   RGB_MOD,
-                          _______,   _______,   KC_P7,     KC_P8,     KC_P9,     KC_PPLS,   RGB_RMOD,
+                          _______,   _______,   KC_NUM,    KC_PSLS,   KC_PAST,   KC_PMNS,   _______,
+                          _______,   _______,   KC_P7,     KC_P8,     KC_P9,     KC_PPLS,   _______,
                           _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PPLS,   _______,
-                                     _______,   KC_P1,     KC_P2,     KC_P3,     KC_PENT,   MO(3),
-                                                KC_P0,     KC_P0,     KC_PDOT,   KC_PENT,   RGB_TOG,
+                                     _______,   KC_P1,     KC_P2,     KC_P3,     KC_PENT,   _______,
+                                                KC_P0,     KC_P0,     KC_PDOT,   KC_PENT,   _______,
              _______, KC_P0,   KC_PENT,
 
              _______, _______, _______, _______, _______
