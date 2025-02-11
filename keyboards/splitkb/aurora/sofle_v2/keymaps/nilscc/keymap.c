67457f0b6e923df864d0e5cf89fe95fca9bc5d6a
Nils <mail@nils.cc>
2025-02-11 21:45:09 +0100

    use colemak-dh mod

--- keyboards/splitkb/aurora/sofle_v2/keymaps/nilscc/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/nilscc/keymap.c
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_COLEMAK] = LAYOUT(
         KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
-        KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_P,       KC_G,                               KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_DEL,
-        KC_GRV,     KC_A,       KC_R,       KC_S,       KC_T,       KC_D,                               KC_H,       KC_N,       KC_E,       KC_I,       KC_O,       KC_QUOT,
-        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       XXXXXXX,    XXXXXXX,    KC_K,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
+        KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,                               KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_DEL,
+        KC_GRV,     KC_A,       KC_R,       KC_S,       KC_T,       KC_G,                               KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_QUOT,
+        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_D,       KC_V,       XXXXXXX,    XXXXXXX,    KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
                                 KC_LGUI,    KC_LALT,    KC_LCTL,    LOWER,      KC_ENT,     KC_SPC,     RAISE,      KC_RCTL,    KC_RALT,    KC_RGUI
     ),
 
