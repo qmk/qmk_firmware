13bb1297d19213711444ea77eb11e7c620d1e7ff
Trần Thanh Sơn <son.tt1@teko.vn>
2025-02-17 17:52:07 +0700

    Update firmware s7ventyr2 handle indicator led

--- keyboards/trnthsn/s7ventyr2/config.h
+++ keyboards/trnthsn/s7ventyr2/config.h
@@ -21,3 +21,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define WS2812_SPI_MOSI_PAL_MODE 0
 #define WS2812_SPI_SCK_PAL_MODE 0
 #define WS2812_SPI_SCK_PIN B13
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
