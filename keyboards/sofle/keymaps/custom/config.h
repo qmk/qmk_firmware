1dfe1796bda86891b9db0aa8a0c3d8600316dd69
brewster <brewstermackenzie@protonmail.com>
2025-02-18 15:16:30 +0000

    merged custom sofle keyboard layout from my old GitHub profile

---
+++ keyboards/sofle/keymaps/custom/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define SPLIT_LAYER_STATE_ENABLE
+
+#define SPLIT_LED_STATE_ENABLE
+
+#define SPLIT_MODS_ENABLE
+
+#define ENCODER_DIRECTION_FLIP
+
+#define ENCODER_RESOLUTIONS { 4, 1 }
---
+++ keyboards/sofle/keymaps/custom/readme.md
@@ -0,0 +1,7 @@
+Custom Sofle keyboard layout
+
+- disabled left OLED and use right side only
+- added page up/down to right pot
+- flipped right pot for more natural scrolling
+- moved position of pipe, tilde, hash to feel more like standard UK layout
+- moved backslash and NUBS (non-US black slash)
