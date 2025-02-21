d2c7446ec621a21aa715be214fa6937ac3b0dcf6
anubhav dhiman <104914147+anubhavd7@users.noreply.github.com>
2025-02-21 12:21:42 +0530

    Add files via upload

---
+++ keyboards/atset/at2/keymaps/default/via/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* Because we use vibl, this has to be defined here instead of under keymaps/vial/config.h */
+#define VIAL_KEYBOARD_UID {0x88, 0x38, 0x5C, 0x6F, 0xEA, 0xED, 0x55, 0xC2}                                                                      
