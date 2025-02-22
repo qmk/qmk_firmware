3cdfd5ea8ecc0922b1de0d454e380b4190281957
Nils <mail@nils.cc>
2025-02-22 14:36:02 +0100

    Fix indention

--- keyboards/splitkb/aurora/sofle_v2/keymaps/nilscc/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/nilscc/keymap.c
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     Q,          Y,          O,          U,          KC_MINS,                            J,          G,          N,          W,          K,          KC_DEL,
         KC_GRV,     H,          I,          LSFT_T(E),  LCTL_T(A),  KC_DOT,                             P,          RCTL_T(D),  RSFT_T(R),  S,          L,          KC_SLSH,
         KC_LSFT,    Z,          X,          KC_QUOT,    KC_COMM,    KC_SCLN,    XXXXXXX,    XXXXXXX,    B,          C,          M,          F,          V,          KC_RSFT,
-                                KC_LGUI,    KC_LALT,    KC_TAB,     NUM_SPC,    QK_REP,     SYM_T,      NAV_BS,    KC_DEL,     KC_RALT,    KC_RGUI
+                                KC_LGUI,    KC_LALT,    KC_TAB,     NUM_SPC,    QK_REP,     SYM_T,      NAV_BS,     KC_DEL,     KC_RALT,    KC_RGUI
     ),
 
     //
