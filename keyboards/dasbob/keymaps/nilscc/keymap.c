50461880b3997a586f5937555418fd4a083f1d6e
Nils <mail@nils.cc>
2025-02-22 14:44:59 +0100

    stuff

--- keyboards/dasbob/keymaps/nilscc/keymap.c
+++ keyboards/dasbob/keymaps/nilscc/keymap.c
@@ -20,10 +20,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ALPHA_NORDRASSIL] = LAYOUT_split_3x5_3(
         KC_Q,   KC_Y,           KC_O,           KC_U,           KC_MINS,                                KC_J,       KC_G,           KC_N,           KC_W,           KC_K,
-        KC_H,   RALT_T(KC_I),   LSFT_T(KC_E),   LCTL_T(KC_A),   KC_DOT,                                 KC_P,       LCTL_T(KC_D),   LSFT_T(KC_R),   RALT_T(KC_S),   KC_L,
+        KC_H,   KC_I,           LSFT_T(KC_E),   LCTL_T(KC_A),   KC_DOT,                                 KC_P,       LCTL_T(KC_D),   LSFT_T(KC_R),   RALT_T(KC_S),   KC_L,
         KC_Z,   KC_X,           KC_QUOT,        KC_COMM,        KC_SCLN,                                KC_B,       KC_C,           KC_M,           KC_F,           KC_V,
-                        LCTL_T(KC_ESC), LT(_NUM,KC_SPC), QK_REP,                             LT(_SYM, KC_T), LT(_NAV, KC_BSPC), LALT_T(KC_DEL)
+                        RALT_T(KC_ESC), LT(_NUM,KC_SPC), QK_REP,                             LT(_SYM, KC_T), LT(_NAV, KC_BSPC), LALT_T(KC_DEL)
     ),
+
+    // default layers
 
     [_ALPHA_QWERTY] = LAYOUT_split_3x5_3(
         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                                  KC_Y,       KC_U,           KC_I,           KC_O,           KC_P,
@@ -42,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // special layers
 
     [_SYM] = LAYOUT_split_3x5_3(
-        KC_GRV , KC_CIRC,   KC_AT,  KC_DLR, KC_TILD,                                KC_AMPR, KC_EXLM, KC_PIPE, KC_UNDS, KC_HASH,
-        KC_SLSH, KC_LBRC, KC_LCBR, KC_LPRN,  KC_EQL,                                KC_ASTR, KC_RPRN, KC_RCBR, KC_RBRC, KC_BSLS,
-        _______, KC_QUES, KC_PLUS, KC_PERC, XXXXXXX,                                XXXXXXX, XXXXXXX, KC_MINS, XXXXXXX, _______,
+        KC_GRV , KC_CIRC, KC_AT,   KC_DLR,  KC_TILD,                                KC_AMPR, KC_EXLM, KC_PIPE, KC_UNDS, KC_HASH,
+        KC_SLSH, KC_LBRC, KC_LCBR, KC_LPRN, KC_EQL,                                 KC_ASTR, KC_RPRN, KC_RCBR, KC_RBRC, KC_BSLS,
+        XXXXXXX, KC_QUES, KC_PLUS, KC_PERC, XXXXXXX,                                XXXXXXX, XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,
                                         XXXXXXX, KC_TAB, KC_ENT,        _______, XXXXXXX, XXXXXXX
     ),
 
@@ -52,13 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                KC_CAPS, KC_NUM,  XXXXXXX, XXXXXXX, XXXXXXX,
         KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, XXXXXXX,                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
-                                        XXXXXXX, XXXXXXX, _______,      XXXXXXX, MO(_CFG), XXXXXXX
+                                        _______, _______, _______,      XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
     [_NUM] = LAYOUT_split_3x5_3(
-        XXXXXXX,  KC_F9, KC_F10, KC_F11, KC_F12,                                    KC_PPLS,  KC_P7,  KC_P8,  KC_P9, KC_PSLS,
-        XXXXXXX,  KC_F5,  KC_F6,  KC_F7,  KC_F8,                                    KC_P0,  KC_P4,  KC_P5,  KC_P6, KC_PDOT,
-        XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F4,                                    KC_PMNS,  KC_P1,  KC_P2,  KC_P3, KC_PAST,
+        XXXXXXX,  KC_F9,  KC_F10, KC_F11, KC_F12,                                   KC_PPLS,  KC_P7,  KC_P8,  KC_P9,  KC_PSLS,
+        XXXXXXX,  KC_F5,  KC_F6,  KC_F7,  KC_F8,                                    KC_P0,    KC_P4,  KC_P5,  KC_P6,  KC_PDOT,
+        XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F4,                                    KC_PMNS,  KC_P1,  KC_P2,  KC_P3,  KC_PAST,
                                         XXXXXXX, _______, XXXXXXX,      KC_PEQL, KC_PENT, XXXXXXX
     ),
 
