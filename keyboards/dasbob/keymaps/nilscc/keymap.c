0d4fa358dbad9a117de28bb54ea7914e4c41eba0
Nils <mail@nils.cc>
2025-02-24 18:27:13 +0100

    nav/num updates

--- keyboards/dasbob/keymaps/nilscc/keymap.c
+++ keyboards/dasbob/keymaps/nilscc/keymap.c
@@ -52,17 +52,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
-        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                KC_CAPS, KC_TAB,  KC_LSFT, KC_LALT, XXXXXXX,
-        KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, XXXXXXX,                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
+        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                KC_GRV,  KC_TAB,  KC_LSFT, KC_LALT, XXXXXXX,
+        KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, XXXXXXX,                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT, KC_LGUI,                                KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
                                         _______, _______, _______,      XXXXXXX, _______, _______
     ),
 
     [_NUM] = LAYOUT_split_3x5_3(
-        KC_F9,  KC_F10, KC_F11, KC_F12, XXXXXXX,                                    KC_NUM,   KC_P7,  KC_P8,  KC_P9,  KC_PSLS,
-        KC_F5,  KC_F6,  KC_F7,  KC_F8,  XXXXXXX,                                    KC_PPLS,  KC_P4,  KC_P5,  KC_P6,  KC_PDOT,
-        KC_F1,  KC_F2,  KC_F3,  KC_F4,  XXXXXXX,                                    KC_PMNS,  KC_P1,  KC_P2,  KC_P3,  KC_PAST,
-                                        XXXXXXX, _______, XXXXXXX,        KC_PENT,  KC_PEQL,  KC_P0
+        KC_F9,  KC_F10, KC_F11, KC_F12, KC_CAPS,                                    KC_NUM,   KC_7,   KC_8,   KC_9,   KC_PSLS,
+        KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_PSCR,                                    KC_PPLS,  KC_4,   KC_5,   KC_6,   KC_PDOT,
+        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_LGUI,                                    KC_PMNS,  KC_1,   KC_2,   KC_3,   KC_PAST,
+                                        XXXXXXX, _______, XXXXXXX,        KC_PENT,  KC_PEQL,  KC_0
     ),
 
     [_CFG] = LAYOUT_split_3x5_3(
