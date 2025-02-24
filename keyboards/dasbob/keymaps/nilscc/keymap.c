ff2699b7415d3f5311c138dc19b252f980ae53ac
Nils <mail@nils.cc>
2025-02-24 15:47:06 +0100

    Enable permissive hold and some layout adjustments

--- keyboards/dasbob/keymaps/nilscc/keymap.c
+++ keyboards/dasbob/keymaps/nilscc/keymap.c
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+
 enum layers {
     _ALPHA_NORDRASSIL,
     _ALPHA_QWERTY,
@@ -46,22 +47,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYM] = LAYOUT_split_3x5_3(
         KC_GRV , KC_CIRC, KC_AT,   KC_DLR,  KC_TILD,                                KC_AMPR, KC_EXLM, KC_PIPE, KC_UNDS, KC_HASH,
         KC_SLSH, KC_LBRC, KC_LCBR, KC_LPRN, KC_EQL,                                 KC_ASTR, KC_RPRN, KC_RCBR, KC_RBRC, KC_BSLS,
-        XXXXXXX, KC_QUES, KC_PLUS, KC_PERC, XXXXXXX,                                XXXXXXX, XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_QUES, KC_PLUS, KC_PERC, XXXXXXX,                                XXXXXXX, KC_COLN, KC_MINS, XXXXXXX, XXXXXXX,
                                         XXXXXXX, KC_TAB, KC_ENT,        _______, _______, _______
     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
-        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                KC_CAPS, KC_NUM,  XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                KC_CAPS, KC_TAB,  KC_LSFT, KC_LALT, XXXXXXX,
         KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, XXXXXXX,                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
                                         _______, _______, _______,      XXXXXXX, _______, _______
     ),
 
     [_NUM] = LAYOUT_split_3x5_3(
-        XXXXXXX,  KC_F9,  KC_F10, KC_F11, KC_F12,                                   KC_PPLS,  KC_P7,  KC_P8,  KC_P9,  KC_PSLS,
-        XXXXXXX,  KC_F5,  KC_F6,  KC_F7,  KC_F8,                                    KC_P0,    KC_P4,  KC_P5,  KC_P6,  KC_PDOT,
-        XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F4,                                    KC_PMNS,  KC_P1,  KC_P2,  KC_P3,  KC_PAST,
-                                        XXXXXXX, _______, XXXXXXX,      KC_PEQL, KC_PENT, XXXXXXX
+        KC_F9,  KC_F10, KC_F11, KC_F12, XXXXXXX,                                    KC_NUM,   KC_P7,  KC_P8,  KC_P9,  KC_PSLS,
+        KC_F5,  KC_F6,  KC_F7,  KC_F8,  XXXXXXX,                                    KC_PPLS,  KC_P4,  KC_P5,  KC_P6,  KC_PDOT,
+        KC_F1,  KC_F2,  KC_F3,  KC_F4,  XXXXXXX,                                    KC_PMNS,  KC_P1,  KC_P2,  KC_P3,  KC_PAST,
+                                        XXXXXXX, _______, XXXXXXX,        KC_PENT,  KC_PEQL,  KC_P0
     ),
 
     [_CFG] = LAYOUT_split_3x5_3(
