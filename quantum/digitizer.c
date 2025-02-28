fe2418e7d6aa91e50d5ec2b996927be8b278fde6
George Norton <george_norton_uk@hotmail.com>
2025-02-28 15:15:20 +0000

    include string.h, the missing include causes a few CI test targets to fail to build.

--- quantum/digitizer.c
+++ quantum/digitizer.c
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <string.h>
 #include "digitizer.h"
 #include "digitizer_mouse_fallback.h"
 #include "debug.h"
