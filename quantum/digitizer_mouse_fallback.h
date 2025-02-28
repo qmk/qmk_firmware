a39ccfa990bb3dbafd990907777a321c66d7db22
George Norton <george_norton_uk@hotmail.com>
2025-02-28 13:31:35 +0000

    Fix lint issues.

--- quantum/digitizer_mouse_fallback.h
+++ quantum/digitizer_mouse_fallback.h
@@ -5,7 +5,7 @@
 #    include "pointing_device.h"
 
 const pointing_device_driver_t digitizer_pointing_device_driver;
-extern bool digitizer_send_mouse_reports;
+extern bool                    digitizer_send_mouse_reports;
 
 __attribute__((weak)) void digitizer_update_mouse_report(report_digitizer_t *report);
 __attribute__((weak)) bool digitizer_update_gesture_state(void);
