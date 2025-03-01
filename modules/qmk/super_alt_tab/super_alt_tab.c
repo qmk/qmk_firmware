01bf1602fa9a3d41465c0936ced8804de3292e1d
Drashna Jael're <drashna@live.com>
2025-03-01 12:15:15 -0800

    Make module more configurable

--- modules/qmk/super_alt_tab/super_alt_tab.c
+++ modules/qmk/super_alt_tab/super_alt_tab.c
@@ -10,6 +10,16 @@ ASSERT_COMMUNITY_MODULES_MIN_API_VERSION(1, 0, 0);
 static bool     is_alt_tab_active = false; // ADD this near the beginning of keymap.c
 static uint16_t alt_tab_timer     = 0;     // we will be using them soon.
 
+#ifndef COMMUNITY_MODULE_SUPER_ALT_TAB_TIMEOUT
+#    define COMMUNITY_MODULE_SUPER_ALT_TAB_TIMEOUT 1000
+#endif // COMMUNITY_MODULE_SUPER_ALT_TAB_TIMEOUT
+#ifndef COMMUNITY_MODULE_SUPER_ALT_TAB_MODIFIER
+#    define COMMUNITY_MODULE_SUPER_ALT_TAB_MODIFIER MOD_LALT
+#endif // COMMUNITY_MODULE_SUPER_ALT_TAB_MODIFIER
+#ifndef COMMUNITY_MODULE_SUPER_ALT_TAB_KEY
+#    define COMMUNITY_MODULE_SUPER_ALT_TAB_KEY KC_TAB
+#endif // COMMUNITY_MODULE_SUPER_ALT_TAB_KEY
+
 bool process_record_super_alt_tab(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_super_alt_tab_kb(keycode, record)) {
         return false;
@@ -20,12 +30,12 @@ bool process_record_super_alt_tab(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 if (!is_alt_tab_active) {
                     is_alt_tab_active = true;
-                    register_code(KC_LALT);
+                    register_mods(COMMUNITY_MODULE_SUPER_ALT_TAB_MODIFIER);
                 }
                 alt_tab_timer = timer_read();
-                register_code(KC_TAB);
+                register_code(COMMUNITY_MODULE_SUPER_ALT_TAB_KEY);
             } else {
-                unregister_code(KC_TAB);
+                unregister_code(COMMUNITY_MODULE_SUPER_ALT_TAB_KEY);
             }
             break;
     }
@@ -34,8 +44,8 @@ bool process_record_super_alt_tab(uint16_t keycode, keyrecord_t *record) {
 
 void housekeeping_task_super_alt_tab(void) {
     if (is_alt_tab_active) {
-        if (timer_elapsed(alt_tab_timer) > 1000) {
-            unregister_code(KC_LALT);
+        if (timer_elapsed(alt_tab_timer) > COMMUNITY_MODULE_SUPER_ALT_TAB_TIMEOUT) {
+            unregister_mods(COMMUNITY_MODULE_SUPER_ALT_TAB_MODIFIER);
             is_alt_tab_active = false;
         }
     }
