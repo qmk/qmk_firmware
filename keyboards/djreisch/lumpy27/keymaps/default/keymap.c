fa929cf8f5572b9a553eb66bc7d3cf8a529a8abd
djreisch <danieljreisch@gmail.com>
2025-03-02 02:08:45 -0800

    updated layout name

--- keyboards/djreisch/lumpy27/keymaps/default/keymap.c
+++ keyboards/djreisch/lumpy27/keymaps/default/keymap.c
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   |      |      |/       /
                                   `---------------------'
     */
-    [0] = LAYOUT_all(KC_ESC,  KC_1,  KC_2, KC_3, KC_4, KC_5,
-	                 KC_TAB,  KC_G,  KC_Q, KC_W, KC_E, KC_R,
-                     KC_LSFT, KC_A,  KC_S, KC_D, KC_F,
-                     KC_LCTL, KC_B,  KC_Z, KC_X, KC_C, KC_V,
-                     KC_LALT, MO(1), KC_SPACE),
+    [0] = LAYOUT(KC_ESC,  KC_1,  KC_2, KC_3, KC_4, KC_5,
+	             KC_TAB,  KC_G,  KC_Q, KC_W, KC_E, KC_R,
+                 KC_LSFT, KC_A,  KC_S, KC_D, KC_F,
+                 KC_LCTL, KC_B,  KC_Z, KC_X, KC_C, KC_V,
+                 KC_LALT, MO(1), KC_SPACE),
 
     /*
          Alt
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   |      |      |/       /
                                   `---------------------'
     */
-    [1] = LAYOUT_all(KC_ESC, KC_0, KC_9, KC_8, KC_7, KC_6,
-                     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
-                     KC_LSFT, KC_A, KC_S, KC_Y, KC_H,
-                     KC_LCTL, MACRO_GG, KC_Z, KC_X, KC_C, KC_M,
-                     KC_LALT, KC_TRNS, KC_ENTER)
+    [1] = LAYOUT(KC_ESC, KC_0, KC_9, KC_8, KC_7, KC_6,
+                 KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
+                 KC_LSFT, KC_A, KC_S, KC_Y, KC_H,
+                 KC_LCTL, MACRO_GG, KC_Z, KC_X, KC_C, KC_M,
+                 KC_LALT, KC_TRNS, KC_ENTER)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
