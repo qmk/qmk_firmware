9af623764c1f9ab2d7fec219b99e47fc58f7dc63
Marek Schmitt <62243375+mawaeg@users.noreply.github.com>
2025-03-02 20:13:55 +0100

    Apply suggestions from code review
    Co-authored-by: jack <jack@pngu.org>

--- keyboards/mawaeg/sharkropad/keymaps/default/keymap.c
+++ keyboards/mawaeg/sharkropad/keymaps/default/keymap.c
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴────────────┘
      */
         [0] = LAYOUT(
-                             RGB_MOD,
-        KC_P7, KC_P8, KC_P9, RGB_TOG,
+                             RM_NEXT,
+        KC_P7, KC_P8, KC_P9, RM_TOGG,
         KC_P4, KC_P5, KC_P6, KC_NUM,
         KC_P1, KC_P2, KC_P3, KC_P0
     )
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(RGB_SPD, RGB_SPI)},
+    [0] = { ENCODER_CCW_CW(RM_SPDD, RM_SPDU)},
 };
 #endif
 
