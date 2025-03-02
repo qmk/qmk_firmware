aa754da13d0b9630b4e2a8d80b652dbb999d7b55
mawaeg <62243375+mawaeg@users.noreply.github.com>
2025-03-02 20:18:51 +0100

    Use PWM driver for ws2812

--- keyboards/mawaeg/sharkropad/chconf.h
+++ keyboards/mawaeg/sharkropad/chconf.h
@@ -16,13 +16,7 @@
 
 #pragma once
 
-#include_next <mcuconf.h>
+#define CH_CFG_ST_RESOLUTION 16
+#define CH_CFG_ST_FREQUENCY 10000
 
-#undef STM32_I2C_USE_I2C1
-#define STM32_I2C_USE_I2C1 TRUE
-
-#undef STM32_PWM_USE_TIM2
-#define STM32_PWM_USE_TIM2 TRUE
-
-#undef STM32_ST_USE_TIMER
-#define STM32_ST_USE_TIMER 15
+#include_next <chconf.h>
