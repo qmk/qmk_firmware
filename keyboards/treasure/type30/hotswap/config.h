a584c57e6fbd62929c30b1ac8f37795f400b11be
TreasureTypes <73436366+TreasureTypes@users.noreply.github.com>
2025-03-05 10:29:58 +0100

    Update config.h

--- keyboards/treasure/type30/hotswap/config.h
+++ keyboards/treasure/type30/hotswap/config.h
@@ -3,24 +3,15 @@
 
 #pragma once
 
-/* COMBO FUNCTION */
-#define COMBO_TERM 150        // how quickly all combo keys must be pressed in succession to trigger
-//#define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
-//#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
-
 /* disable action features */
-
 
 #define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
 
 #define RGB_MATRIX_LED_COUNT 36
-#define WS2812_DI_PIN B4
 #define DRIVER_LED_TOTAL 36
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
 
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT // Sets the default mode, if none has been set
-#define RGB_MATRIX_DEFAULT_SPD 40 // Sets the default animation speed, if none has been set
 
 //#define RGB_MATRIX_LED_FLUSH_LIMIT 32 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness) _ Default is 16
 //#define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 6 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness) - Default is 5
