e9a8b041065285b72d6b4ca3034f0439ce8365df
Nils <mail@nils.cc>
2025-03-05 09:08:42 +0100

    add fnc layer

--- keyboards/dasbob/keymaps/nilscc/keymap.c
+++ keyboards/dasbob/keymaps/nilscc/keymap.c
@@ -12,7 +12,7 @@ enum layers {
     _SYM,
     _NAV,
     _NUM,
-    _CFG,
+    _FNC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ALPHA_NORDRASSIL] = LAYOUT_split_3x5_3(
         KC_Q,   KC_Y,           KC_O,           KC_U,           KC_MINS,                                KC_J,       KC_G,           KC_N,           KC_W,           KC_K,
-        KC_H,   KC_I,           LSFT_T(KC_E),   LCTL_T(KC_A),   KC_DOT,                                 KC_P,       LCTL_T(KC_D),   LSFT_T(KC_R),   RALT_T(KC_S),   KC_L,
+        KC_H,   LALT_T(KC_I),   LSFT_T(KC_E),   LCTL_T(KC_A),   KC_DOT,                                 KC_P,       LCTL_T(KC_D),   LSFT_T(KC_R),   RALT_T(KC_S),   KC_L,
         KC_Z,   KC_X,           KC_QUOT,        KC_COMM,        KC_SCLN,                                KC_B,       KC_C,           KC_M,           KC_F,           KC_V,
-                        RALT_T(KC_ESC), LT(_NUM,KC_SPC), QK_REP,                             LT(_SYM, KC_T), LT(_NAV, KC_BSPC), LALT_T(KC_DEL)
+                              LT(_FNC,KC_ESC), LT(_NUM,KC_SPC), QK_REP,                             LT(_SYM, KC_T), LT(_NAV, KC_BSPC), LALT_T(KC_DEL)
     ),
 
     // [_ALPHA_QWERTY] = LAYOUT_split_3x5_3(
@@ -52,24 +52,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
-        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                KC_GRV,  KC_TAB,  KC_LSFT, KC_LALT, XXXXXXX,
+        XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5),
         KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, XXXXXXX,                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT, KC_LGUI,                                KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
                                         _______, _______, _______,      XXXXXXX, _______, _______
     ),
 
+    // number layer with modifiers and ?! keys to avoid conflicts with TAB on
+    // symbols layer when typing a fast space after a sentence
     [_NUM] = LAYOUT_split_3x5_3(
-        KC_F9,  KC_F10, KC_F11, KC_F12, KC_CAPS,                                    KC_NUM,   KC_7,   KC_8,   KC_9,   KC_PSLS,
-        KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_PSCR,                                    KC_PPLS,  KC_4,   KC_5,   KC_6,   KC_PDOT,
-        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_LGUI,                                    KC_PMNS,  KC_1,   KC_2,   KC_3,   KC_PAST,
-                                        XXXXXXX, _______, _______,      KC_PENT, KC_0, KC_LALT
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM,                                 KC_PEQL,  KC_7,   KC_8,   KC_9,   KC_PSLS,
+        KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,                                KC_PPLS,  KC_4,   KC_5,   KC_6,   KC_PCMM,
+        XXXXXXX, XXXXXXX, KC_QUES, KC_EXLM, XXXXXXX,                                KC_PMNS,  KC_1,   KC_2,   KC_3,   KC_PAST,
+                                        XXXXXXX, _______, _______,      KC_PENT, KC_0, XXXXXXX
     ),
 
-    // [_CFG] = LAYOUT_split_3x5_3(
-    //     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                XXXXXXX, XXXXXXX, XXXXXXX,DF(_ALPHA_QWERTY), DF(_ALPHA_COLEMAK),
-    //     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    //     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    //                                      XXXXXXX, _______, XXXXXXX,     XXXXXXX, _______, XXXXXXX
-    // )
+    // function layer, with alt-tab and umlauts on left side
+    [_FNC] = LAYOUT_split_3x5_3(
+        KC_LALT, KC_TAB,    ALGR(KC_O), ALGR(KC_U), KC_CAPS,                                XXXXXXX, KC_F9,  KC_F10, KC_F11, KC_F12,
+        XXXXXXX, S(KC_TAB), ALGR(KC_E), ALGR(KC_A), KC_PSCR,                                XXXXXXX, KC_F5,  KC_F6,  KC_F7,  KC_F8,
+        XXXXXXX, KC_GRV,    XXXXXXX,    ALGR(KC_S), KC_LGUI,                                XXXXXXX, KC_F1,  KC_F2,  KC_F3,  KC_F4,
+                                         XXXXXXX, _______, XXXXXXX,     XXXXXXX, _______, XXXXXXX
+    )
     // clang-format on
 };
