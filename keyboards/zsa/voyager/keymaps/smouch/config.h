aa35bda9ddd9ad6e216b601f0c3f9c964a867c1d
RN820503 <roderick.smith@me.com>
2025-03-08 13:20:33 +1100

    Regular update

--- keyboards/zsa/voyager/keymaps/smouch/config.h
+++ keyboards/zsa/voyager/keymaps/smouch/config.h
@@ -8,19 +8,16 @@
 // #define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
 // #define COMBO_HOLD_TERM 200  // how long at least one of the combo keys must be held to trigger
 #define COMBO_ONLY_FROM_LAYER 0 // combos mapped to base layer only
-#define COMBO_STRICT_TIMER
+#define COMBO_STRICT_TIMER      // full combo must be pressed within the combo_term (less misfires)
+// #define COMBO_COUNT 19
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 // When idle, turn off Caps Word after 10 seconds.
 #define CAPS_WORD_IDLE_TIMEOUT 10000
 
-// #undef RGB_MATRIX_TIMEOUT
-// #define RGB_MATRIX_TIMEOUT 300000
-
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define SERIAL_NUMBER "RmgQl/B5Rbd"
 #define LAYER_STATE_8BIT
-// #define COMBO_COUNT 19
 
 // Configure the global tapping term (default: 200ms)
 #undef TAPPING_TERM // if not default
@@ -33,6 +30,7 @@
 // Apply the modifier on keys that are tapped during a short hold of a modtap
 #define PERMISSIVE_HOLD_PER_KEY
 
+// Opposite hands rule for HRM. Prevents misfires on same hand rolls.
 #define CHORDAL_HOLD
 
 // Don't apply custom shift keys when any non-shift mod is held.
@@ -41,5 +39,7 @@
 /* Default to Mac hotkeys for Select_Word */
 #define SELECT_WORD_OS_MAC
 
+// #undef RGB_MATRIX_TIMEOUT
+// #define RGB_MATRIX_TIMEOUT 300000
 // #define RGB_MATRIX_STARTUP_SPD 60
 
