97f3baaf7ce2c9482cc656e71201cd9baf7b952a
RN820503 <roderick.smith@me.com>
2025-03-08 15:41:31 +1100

    Rearranged punc to -.'=/

--- keyboards/zsa/voyager/keymaps/smouch/keymap.c
+++ keyboards/zsa/voyager/keymaps/smouch/keymap.c
@@ -29,7 +29,7 @@ enum custom_keycodes {
 
 /*  Hands Down Promethium (canonical)
     ⎋   3   2@  1$   0#  4           7   6   5   9   8  CW
-    ⇥   v   w   g2   m   j           /   .:  =2  -   '  `
+    ⇥   v   w   g2   m   j           -   .:  '2  =   /  `
     z   s⌃  n⌥  t3   h⌘  k           ,;  a⌘  e1  i⌥  c⌃ q
     tg  f   p   d    l   x      	 ;   u   o   y   b  \
                          r⇧  ⌫   ⏎   ␣⇧
@@ -58,11 +58,11 @@ enum custom_keycodes {
 #define LT1 KC_M
 #define LT0 KC_J
 
-#define RT0 KC_SLSH
+#define RT0 KC_MINS
 #define RT1 KC_DOT
-#define RT2 LT(_SYM, KC_EQL)
-#define RT3 KC_MINS
-#define RT4 KC_QUOT
+#define RT2 LT(_SYM, KC_QUOT)
+#define RT3 KC_EQL
+#define RT4 KC_SLSH
 #define RT5 KC_GRV
 
 // middle row
@@ -113,7 +113,7 @@ static uint16_t keyhold_timer; // for handling Qu combo
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*  Hands Down Promethium
     ⎋   3   2@  1$   0#  4           7   6   5   9   8  CW
-    ⇥   v   w   g2   m   j           /   .:  =2  -   '  `
+    ⇥   v   w   g2   m   j           -   .:  '2  =   /  `
     z   s⌃  n⌥  t3   h⌘  k           ,;  a⌘  e1  i⌥  c⌃ q
     tg  f   p   d    l   x      	 ;   u   o   y   b  \
                          r⇧   ⌫   ⏎   ␣⇧
