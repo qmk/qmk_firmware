55d948b76c5e1ed5bd326eca1910b1a31336cc1c
Richard Dawe <richdawe@gmail.com>
2025-03-10 22:28:47 +0000

    ut472: richdawe2 keymap: fix lower row in L1

--- keyboards/keyhive/ut472/keymaps/richdawe2/keymap.c
+++ keyboards/keyhive/ut472/keymaps/richdawe2/keymap.c
@@ -46,13 +46,13 @@ LAYOUT(
    * |-------------------------------------------------------------------------+
    * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  | Up  |Sh/En|
    * |-------------------------------------------------------------------------+
-   * |  \  |Ctrl | Gui | Alt |  L1  |   Space   |  L1  |/ Alt| Left|Down |Right|
+   * |  \  |Ctrl | Gui | Alt |Spc/L1|   Space   |Spc/L1|/ Alt| Left|Down |Right|
    * `-------------------------------------------------------------------------'
    */
   LT3_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LT2_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SC_SENT,
-  KC_BSLS, KC_LCTL, KC_LGUI, KC_LALT, MO(1),      KC_SPC,        MO(1),   MT_LASL, KC_LEFT, KC_DOWN, KC_RGHT
+  KC_BSLS, KC_LCTL, KC_LGUI, KC_LALT, LT1_SPC,    KC_SPC,        LT1_SPC, MT_LASL, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 
@@ -70,7 +70,7 @@ LAYOUT(
    */
   KC_DOT,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_COLN, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_BSLS,
-  _______, KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, KC_LBRC, KC_RBRC, KC_PGUP, _______,
+  _______, KC_BSLS, KC_TILD, KC_GRV,  KC_PIPE, KC_MINS, S(KC_1), KC_EQL,  KC_LBRC, KC_RBRC, KC_PGUP, _______,
   _______, _______, _______, _______, _______,     _______,      _______, _______, KC_HOME, KC_PGDN, KC_END
 ),
 
