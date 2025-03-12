cef8b6b0aa14f6c0e2c9f39b22dceca2d15e122e
HorrorTroll <sonicvipduc@gmail.com>
2025-03-12 15:41:18 +0700

    Allow LVGL onekey keymap to be able compile for other board

--- keyboards/handwired/onekey/at_start_f415/config.h
+++ keyboards/handwired/onekey/at_start_f415/config.h
@@ -9,6 +9,15 @@
 #define BACKLIGHT_PWM_DRIVER PWMD5
 #define BACKLIGHT_PWM_CHANNEL 1
 
+#define LCD_RST_PIN A2
+#define LCD_DC_PIN A3
+#define LCD_CS_PIN A4
+
+#define SPI_DRIVER SPID1
+#define SPI_SCK_PIN A5
+#define SPI_MOSI_PIN A7
+#define SPI_MISO_PIN A6
+
 #define SOLENOID_PIN B12
 #define SOLENOID_PINS { B12, B13, B14, B15 }
 #define SOLENOID_PINS_ACTIVE_STATE { high, high, low }
