f5b882f95810ca7e4cd4e4fed74272e2ba6b2434
Grigory Avdyushin <avdyushin.g@gmail.com>
2025-03-13 18:23:51 +0100

    Show layer info on the display

--- keyboards/handwired/franky36/keymaps/default/keymap.c
+++ keyboards/handwired/franky36/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY = 0,
+    _BASE = 0,
     _LOWER,
     _RAISE,
     _NAV,
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *             │CTL│CMD│SPC│ │ENT│CMD│OPT│
      *             └───┴───┴───┘ └───┴───┴───┘
      */
-    [_QWERTY] = LAYOUT_split_3x5_3(
+    [_BASE] = LAYOUT_split_3x5_3(
         KC_Q,     KC_W,    KC_E,       KC_R,       KC_T,       KC_Y,        KC_U,       KC_I,    KC_O,    KC_P,
         KC_NAV_A, KC_S,    KC_D,       KC_F,       KC_G,       KC_H,        KC_J,       KC_K,    KC_L,    KC_SCLN,
         KC_SFT_Z, KC_X,    KC_C,       KC_V,       KC_B,       KC_N,        KC_M,       KC_COMM, KC_DOT,  KC_SFT_SL,
@@ -123,22 +123,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef OLED_ENABLE
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
-}
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_0;
+    return OLED_ROTATION_270;
+}
+
+static void render_mod_status(uint8_t modifiers) {
+    oled_write_P(PSTR("MODS:"), false);
+    oled_write_P(PSTR("S"), (modifiers & MOD_MASK_SHIFT));
+    oled_write_P(PSTR("C"), (modifiers & MOD_MASK_CTRL));
+    oled_write_P(PSTR("A"), (modifiers & MOD_MASK_ALT));
+    oled_write_ln_P(PSTR("G"), (modifiers & MOD_MASK_GUI));
+    oled_write_ln_P(PSTR(" "), false);
+}
+
+static void render_layer_state(void) {
+    oled_write_ln_P("LAYER", false);
+    oled_write_P("BASE ", layer_state_is(_BASE));
+    oled_write_P("LOWER", layer_state_is(_LOWER));
+    oled_write_P("RAISE", layer_state_is(_RAISE));
+    oled_write_P("NAV  ", layer_state_is(_NAV));
+    oled_write_ln_P(PSTR(" "), false);
 }
 
 bool oled_task_user(void) {
-    render_logo();
+    render_layer_state();
+    render_mod_status(get_mods()|get_oneshot_mods());
     return false;
 }
 #endif
