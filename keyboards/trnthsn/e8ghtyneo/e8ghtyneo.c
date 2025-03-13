e5759cf9075f5a7f30f98dda556dfcbb9cd7a1d1
Thanh Son Tran <62438883+trnthsn@users.noreply.github.com>
2025-03-14 00:12:17 +0700

    Apply suggestions from code review
    Co-authored-by: jack <jack@pngu.org>

--- keyboards/trnthsn/e8ghtyneo/e8ghtyneo.c
+++ keyboards/trnthsn/e8ghtyneo/e8ghtyneo.c
@@ -1,20 +1,24 @@
-#ifdef RGBLIGHT_ENABLE
-#include "rgblight.h"
+// Copyright 2025 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 #include "quantum.h"
-#endif
 
+#ifdef RGBLIGHT_ENABLE
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
   {0,4, HSV_GREEN}
-);     
+);
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
   my_capslock_layer
 );
 
-void keyboard_post_init_user(void) {
-  // Enable the LED layers
+void keyboard_post_init_kb(void) {
   rgblight_layers = my_rgb_layers;
+  keyboard_post_init_user();
 }
-bool led_update_user(led_t led_state) {
-  rgblight_set_layer_state(0, led_state.caps_lock);
-  return true;
+bool led_update_kb(led_t led_state) {
+  bool res = led_update_user(led_state);
+  if (res) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+  }
+  return res;
 }
+#endif
