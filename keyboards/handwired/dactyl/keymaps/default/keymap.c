55170f59d0033602f27b130292976a74c0dc8fe6
Duncan Sutherland <dunk2k_2000@hotmail.com>
2025-03-14 23:23:48 +0000

    #23930 adjustments

--- keyboards/handwired/dactyl/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl/keymaps/default/keymap.c
@@ -6,7 +6,7 @@
 enum layers {
     _QWERTY,
     _LOWER,
-    _RAISE
+    _RAISE,
 };
 
 #define RAISE MO(_RAISE)
@@ -22,7 +22,7 @@ enum layers {
 #define LT2_COL LT(_RAISE, KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT(
+    [_QWERTY] = LAYOUT_modern(
         KC_EQL,  KC_1,    KC_2,     KC_3,    KC_4,    KC_5,                                             KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_DEL,  KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                                             KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_BSPC, KC_A,    KC_S,     KC_D,    KC_F,    KC_G,                                             KC_H,   KC_J,  KC_K,    KC_L,    LT2_COL, LG_QUOT,
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_SPC, KC_BSPC,  KC_HOME,      KC_PGUP, KC_TAB,  KC_ENT,
                                                                         KC_END,       KC_PGDN
     ),
-    [_LOWER] = LAYOUT(
+    [_LOWER] = LAYOUT_modern(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                             KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,
         _______, KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_PIPE,                                           KC_UP,   KC_P7,   KC_P8,   KC_P9, KC_PAST, KC_F12,
         _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                            KC_DOWN, KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                      _______, _______, _______,       _______, _______, _______,
                                                                        _______,       _______
     ),
-    [_RAISE] = LAYOUT(
+    [_RAISE] = LAYOUT_modern(
         _______, _______, _______, _______, _______, _______,                                           _______, _______, _______, _______, _______, _______,
         _______, _______, _______, KC_MS_U, _______, _______,                                           _______, _______, _______, _______, _______, _______,
         _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,                                           _______, _______, _______, _______, _______, KC_MPLY,
