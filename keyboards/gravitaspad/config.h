42cdb8d327e84fc81214315a9576f9900411fabb
deverdum <108906642+deverdum@users.noreply.github.com>
2025-03-19 00:09:47 +0300

    Delete keyboards/gravitaspad directory

--- keyboards/gravitaspad/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2025 Egor Zakharov @deverdum
-// SPDX-License-Identifier: GPL-3.0-or-later
-#pragma once
-#define ENCODERS_PAD_A { GP6 }
-#define ENCODERS_PAD_B { GP5 }
-#define ENCODER_RESOLUTION 4
-#define I2C1_SCL_PIN GP1
-#define I2C1_SDA_PIN GP0
-#define I2C_DRIVER I2CD0
