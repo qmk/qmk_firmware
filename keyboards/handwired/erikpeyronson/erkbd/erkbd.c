ecbd721aa0791adbebb2186efac37e14db7cc5b7
Erik Peyronson <erik.peyronson@gmail.com>
2025-03-18 08:37:22 +0100

    Add handwired/erikpeyronson/erkbd
    A 3d printed handwired 44 key double encoder keyboard with 128x64 oleds

---
+++ keyboards/handwired/erikpeyronson/erkbd/erkbd.c
@@ -0,0 +1,2 @@
+// Copyright 2025 Erik Peyronson @erikpeyronson
+// SPDX-License-Identifier: GPL-3.0-or-later
---
+++ keyboards/handwired/erikpeyronson/erkbd/erkbd.h
@@ -0,0 +1,5 @@
+// Copyright 2025 Erik Peyronson @erikpeyronson
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+#include "quantum.h"
