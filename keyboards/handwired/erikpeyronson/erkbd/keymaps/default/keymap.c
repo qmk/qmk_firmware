6937d15440776b4ac44a75dc0a5acc9a88dcf59a
Erik Peyronson <erikpeyronson@gmail.com>
2025-03-19 17:40:16 +0100

    Apply suggestions from code review
    Co-authored-by: jack <jack@pngu.org>

--- keyboards/handwired/erikpeyronson/erkbd/keymaps/default/keymap.c
+++ keyboards/handwired/erikpeyronson/erkbd/keymaps/default/keymap.c
@@ -13,15 +13,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-void keyboard_post_init_user(void) {
-    // Put initialization code here
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // Put keymap specific processing here
-    return true;
-}
-
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {[0] = {ENCODER_CCW_CW(KC_UP, KC_DOWN), ENCODER_CCW_CW(KC_RIGHT, KC_LEFT)}};
 #endif
