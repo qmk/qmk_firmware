58f71e67056b4a4f98c4bae169c805f21874eac6
mechlovin <57231893+mechlovin@users.noreply.github.com>
2025-03-24 08:13:30 +0700

    Update keyboards/mechlovin/zed65/rev1/rev1.h
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- keyboards/mechlovin/zed65/rev1/rev1.h
+++ keyboards/mechlovin/zed65/rev1/rev1.h
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 #include "rgblight.h"
-#include "via.h"
 #include "eeconfig.h"
 
 enum via_rgblight_value {
