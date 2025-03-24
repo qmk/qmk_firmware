927d4abc72a267bad06079d91073894994d3a43b
takashicompany <t@kashi.company>
2025-03-24 11:06:37 +0900

    add comment

--- keyboards/takashicompany/left_slinger/left_slinger.h
+++ keyboards/takashicompany/left_slinger/left_slinger.h
@@ -6,17 +6,17 @@
 #include "quantum.h"
 
 enum custom_keycodes_kb {
-    MY_BTN1 = SAFE_RANGE,
-    MY_BTN2,
-    MY_BTN3,
-    MY_SCRL,	// Scroll
-    MY_CLKI,	// Click time increase
-    MY_CLKD,	// Click time decrease
-    MY_SCDV,	// Scroll vertical
-    MY_SCDH,	// Scroll horizontal
-    MY_JSDI,	// Joy stick movement divisor increase
-    MY_JSDD,	// Joy stick movement divisor decrease
-    MY_STGE,    // Setting End
-    MY_JSKD,    // Joy stick key default
-    MY_JSKE,    // Joy stick key enable
+    MY_BTN1 = SAFE_RANGE,   // User 0
+    MY_BTN2,                // User 1
+    MY_BTN3,                // User 2
+    MY_SCRL,                // User 3 : Scroll
+    MY_CLKI,                // User 4 : Click time increase
+    MY_CLKD,                // User 5 : Click time decrease
+    MY_SCDV,                // User 6 : Scroll vertical
+    MY_SCDH,                // User 7 : Scroll horizontal
+    MY_JSDI,                // User 8 : Joy stick movement divisor increase
+    MY_JSDD,                // User 9 : Joy stick movement divisor decrease
+    MY_STGE,                // User 10 : Setting End
+    MY_JSKD,                // User 11 : Joy stick key default
+    MY_JSKE,                // User 12 : Joy stick key enable
 };
