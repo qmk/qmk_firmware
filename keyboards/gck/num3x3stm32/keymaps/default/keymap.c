75ce8cdf844e584f775a22146f221007ec332436
Giliy666 <gilitjuk@gmail.com>
2025-03-26 16:34:38 +0300

    My first keyboard
    My first keyboard

---
+++ keyboards/gck/num3x3stm32/keymaps/default/keymap.c
@@ -0,0 +1,28 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │   │
+     * ├───┼───┼───┤ + │
+     * │ 4 │ 5 │ 6 │   │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │   │
+     * ├───┴───┼───┤Ent│
+     * │   0   │ . │   │
+     * └───────┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   MO(1)),
+
+    [1] = LAYOUT(
+        UG_TOGG,  UG_PREV,  UG_NEXT,
+        UG_HUEU,  UG_SATU,  UG_VALU,
+        KC_LSFT,  UG_SPDU,   KC_P)    
+
+};
