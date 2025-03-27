f0a65f79552cecc94316e46b49ef60867c65a2d6
Evaldas Kulpis <kulpis@pm.me>
2025-03-27 20:10:47 +0000

    updated ferris for linux

--- keyboards/ferris/keymaps/sweet-linux/config.h
+++ keyboards/ferris/keymaps/sweet-linux/config.h
@@ -1,5 +1,5 @@
 #pragma once
-#define MASTER_LEFT
+#define MASTER_RIGTH
 #define USB_DEVICE_DESCRIPTOR_STRING       "Ferris Sweep RP2040"
 #define USB_MANUFACTURER                   "DPB"
 #define USB_MAX_POWER_CONSUMPTION          500  // Set higher for better boot stability
@@ -10,14 +10,14 @@
 #define TAPPING_FORCE_HOLD
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 #define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define ONESHOT_TIMEOUT 3000  /* Time (in ms) before the one shot key is released */
 
-// Add these settings to improve split keyboard detection
-#define USB_POLLING_INTERVAL_MS 1  // Default is 1, increase for better detection
-#define USB_SUSPEND_WAKEUP_DELAY 0
-#define FORCE_NKRO  // Force N-key rollover which can help with detection 
-#define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 2000  // Wait 2 seconds for split detection
+// // Add these settings to improve split keyboard detection
+// #define USB_POLLING_INTERVAL_MS 1  // Default is 1, increase for better detection
+// #define USB_SUSPEND_WAKEUP_DELAY 0
+// #define FORCE_NKRO  // Force N-key rollover which can help with detection 
+// #define SPLIT_USB_DETECT
+// #define SPLIT_USB_TIMEOUT 2000  // Wait 2 seconds for split detection
 
 // Remove problematic pin definitions that caused compiler errors
 // #define SPLIT_HAND_PIN B0  // Not using pin for split detection
@@ -25,7 +25,7 @@
 // #define USB_DETECT_PIN GP29  // This would cause an error too
 
 // Instead, use proper ChibiOS/QMK pin naming
-#define SPLIT_TRANSPORT_MIRROR  // Mirror state between sides
+// #define SPLIT_TRANSPORT_MIRROR  // Mirror state between sides
 
 // Add a simple wait at startup
 #define KEYBOARD_INIT_DELAY 1000  // Wait 500ms at startup
