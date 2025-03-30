c0132d8595cc889e238253f94fa4b35d9a29132a
Vincent Franco <vince@freshivore.net>
2025-03-30 15:51:42 -0500

    Lint fixup?

--- keyboards/gggw/crosses/3x5/keymaps/stock/keymap.c
+++ keyboards/gggw/crosses/3x5/keymaps/stock/keymap.c
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └───────────┴───────────┴───────────┼───────────┼────────────┼─────┐   ┌────────────┼──────┼────────────┼───────────┴───────────┴───────────┘
 //                                        │   MO(5)   │ LT(2, esc) │ spc │   │ LT(1, ent) │ bspc │ LT(4, del) │
 //                                        └───────────┴────────────┴─────┘   └────────────┴──────┴────────────┘
-[_BASE] = LAYOUT(
+[_BASE] = LAYOUT_3x5(
   KC_Q         , KC_W         , KC_F         , KC_P         , KC_B          ,                               KC_J          , KC_L          , KC_U         , KC_Y         , KC_SCLN       ,
   LGUI_T(KC_A) , LALT_T(KC_R) , LCTL_T(KC_S) , LSFT_T(KC_T) , KC_G          ,                               KC_M          , RSFT_T(KC_N)  , RCTL_T(KC_E) , RALT_T(KC_I) , RGUI_T(KC_O)  ,
   KC_Z         , KC_X         , KC_C         , KC_D         , KC_V          ,                               KC_K          , KC_H          , KC_COMM      , KC_DOT       , LT(3, KC_SLSH),
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └──────┴───┴───┼───────────┼──────┼─────┐   ┌─────┼──────┼───────────┼───────────┴──────┴─────┘
 //                   │           │  0   │     │   │     │      │           │
 //                   └───────────┴──────┴─────┘   └─────┴──────┴───────────┘
-[_NUM] = LAYOUT(
+[_NUM] = LAYOUT_3x5(
   S(KC_8) , KC_7 , KC_8 , KC_9         , KC_SLSH   ,                         KC_BSLS , S(KC_9)         , S(KC_0)         , S(KC_BSLS) , KC_TRNS,
   KC_MINS , KC_4 , KC_5 , LSFT_T(KC_6) , S(KC_EQL) ,                         S(KC_5) , RSFT_T(KC_LBRC) , RCTL_T(KC_RBRC) , S(KC_SCLN) , KC_SCLN,
   KC_GRV  , KC_1 , KC_2 , KC_3         , KC_EQL    ,                         KC_LBRC , KC_RBRC         , KC_TRNS         , KC_TRNS    , KC_TRNS,
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └──────┴──────┴──────┼───────┼────┼────┐   ┌────┼────┼──────┼──────┴──────┴──────┘
 //                         │  no   │ no │ no │   │ no │ no │  no  │
 //                         └───────┴────┴────┘   └────┴────┴──────┘
-[_NAV] = LAYOUT(
+[_NAV] = LAYOUT_3x5(
   KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,                     KC_NO , KC_NO   , KC_NO   , KC_NO   , KC_NO  ,
   KC_LGUI , KC_LALT , KC_LCTL , KC_LSFT , KC_NO ,                     KC_NO , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT,
   KC_NO   , KC_NO   , KC_COPY , KC_PSTE , KC_NO ,                     KC_NO , KC_HOME , KC_END  , KC_PGDN , KC_PGUP,
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └──────┴──────┴──────┼──────┼──────┼────┐   ┌────┼────┼────┼────┴────┴────┘
 //                         │  no  │  no  │ no │   │ no │ no │ no │
 //                         └──────┴──────┴────┘   └────┴────┴────┘
-[_MEDIA] = LAYOUT(
+[_MEDIA] = LAYOUT_3x5(
   KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,                     KC_NO , KC_NO , KC_NO , KC_NO , KC_NO,
   KC_MPRV , KC_VOLD , KC_MPLY , KC_VOLU , KC_MNXT ,                     KC_NO , KC_NO , KC_NO , KC_NO , KC_NO,
   KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,                     KC_NO , KC_NO , KC_NO , KC_NO , KC_NO,
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └─────┴─────┴─────┼─────┼─────┼─────┐   ┌─────┼─────┼──────┼──────┴──────┴──────┘
 //                      │     │     │     │   │     │     │      │
 //                      └─────┴─────┴─────┘   └─────┴─────┴──────┘
-[_FUNC] = LAYOUT(
+[_FUNC] = LAYOUT_3x5(
   KC_F11 , KC_F12 , KC_F13 , KC_F14  , KC_F15  ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
   KC_F6  , KC_F7  , KC_F8  , KC_F9   , KC_F10  ,                         KC_TRNS , KC_RSFT , KC_RCTL , KC_RALT , KC_RGUI,
   KC_F1  , KC_F2  , KC_F3  , KC_F4   , KC_F5   ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    └──────┴─────────┴─────────┼─────────────┼────┼────┐   ┌────┼────────┼─────────┼─────────┴──────────┴─────────┘
 //                               │             │ no │ no │   │ no │   no   │   no    │
 //                               └─────────────┴────┴────┘   └────┴────────┴─────────┘
-[_MOUS] = LAYOUT(
+[_MOUS] = LAYOUT_3x5(
   KC_NO   , KC_NO , KC_NO   , KC_NO       , KC_NO ,                     EE_CLR , MSE_INC , MSE_DEC , KC_NO    , KC_NO  ,
   KC_NO   , MS_BTN3 , MS_BTN2 , MS_BTN1     , KC_NO ,                     KC_NO  , KC_RSFT , KC_RCTL , KC_RALT  , KC_RGUI,
   KC_NO   , KC_NO   , KC_NO   , DRAG_SCROLL , KC_NO ,                     KC_NO  , KC_NO   , KC_NO   , MSE_TOGG , DB_TOGG,
