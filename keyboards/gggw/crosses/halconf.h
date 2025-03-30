c639ba2b78685ef03c8a60b4b08d4c8d7efb7d81
Vincent Franco <vince@freshivore.net>
2025-03-30 00:15:11 -0500

    QMK Linting

--- keyboards/gggw/crosses/halconf.h
+++ keyboards/gggw/crosses/halconf.h
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 Vincent Franco <me@vincentfranco.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #pragma once
 
 #define HAL_USE_I2C TRUE
@@ -6,4 +22,4 @@
 #define SPI_USE_WAIT TRUE
 #define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
 
-#include_next "halconf.h"
+#include_next <halconf.h>
