d925e4c3284a23567832649ed4bd31d74d095602
Drashna Jael're <drashna@live.com>
2025-04-06 16:01:41 -0700

    Revert missed changes for pointing combined config

--- quantum/pointing_device/pointing_device.c
+++ quantum/pointing_device/pointing_device.c
@@ -309,7 +309,7 @@ __attribute__((weak)) bool pointing_device_task(void) {
         local_mouse_report  = pointing_device_adjust_by_defines_right(local_mouse_report);
         shared_mouse_report = pointing_device_adjust_by_defines(shared_mouse_report);
     }
-    local_mouse_report = is_keyboard_left() ? pointing_device_task_combined(local_mouse_report, shared_mouse_report) : pointing_device_task_combined(shared_mouse_report, local_mouse_report);
+    local_mouse_report = is_keyboard_left() ? pointing_device_task_combined_kb(local_mouse_report, shared_mouse_report) : pointing_device_task_combined_kb(shared_mouse_report, local_mouse_report);
 #else
     local_mouse_report = pointing_device_adjust_by_defines(local_mouse_report);
 #endif
@@ -491,21 +491,6 @@ report_mouse_t pointing_device_adjust_by_defines_right(report_mouse_t mouse_repo
     mouse_report.y = -mouse_report.y;
 #    endif
     return mouse_report;
-}
-
-/**
- * @brief Weak function allowing for keyboard level mouse report modification
- *
- * Takes 2 report_mouse_t structs allowing individual modification of sides at keyboard level then returns pointing_device_task_combined_user.
- *
- * NOTE: Only available when using SPLIT_POINTING_ENABLE and POINTING_DEVICE_COMBINED
- *
- * @param[in] left_report report_mouse_t
- * @param[in] right_report report_mouse_t
- * @return pointing_device_task_combined_user(left_report, right_report) by default
- */
-report_mouse_t pointing_device_task_combined(report_mouse_t left_report, report_mouse_t right_report) {
-    return pointing_device_task_combined_kb(left_report, right_report);
 }
 
 /**
