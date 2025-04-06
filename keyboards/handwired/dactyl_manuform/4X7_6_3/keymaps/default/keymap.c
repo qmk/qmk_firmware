69da09e7876dab59368da9812fefafe470081c5b
Antonio Giaquinto <antoniogiaquinto93@hotmail.it>
2025-04-06 15:57:08 +0200

    minor change due to pull request fail

--- keyboards/handwired/dactyl_manuform/4X7_6_3/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4X7_6_3/keymaps/default/keymap.c
@@ -36,20 +36,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  KC_MINS,
     KC_TAB,  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                      KC_LBRC, KC_RBRC,  KC_RBRC,                                                        KC_PLUS, KC_EQL
+             KC_LBRC, KC_RBRC,  KC_RBRC,                                                                 KC_PLUS, KC_EQL,  KC_EQL
     ),
 
     [_LOWER] = LAYOUT(
     KC_ESC,  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  KC_MINS,
     KC_TAB,  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                      KC_LBRC, KC_RBRC,  KC_RBRC,                                                        KC_PLUS, KC_EQL
+             KC_LBRC, KC_RBRC,  KC_RBRC,                                                                 KC_PLUS, KC_EQL,  KC_EQL
     ),
 
     [_RAISE] = LAYOUT(
     KC_ESC,  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,  KC_MINS,
     KC_TAB,  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                      KC_LBRC, KC_RBRC,  KC_RBRC,                                                        KC_PLUS, KC_EQL
+             KC_LBRC, KC_RBRC,  KC_RBRC,                                                                 KC_PLUS, KC_EQL,  KC_EQL
     )
 };
