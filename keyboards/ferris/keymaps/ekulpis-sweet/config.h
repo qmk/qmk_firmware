9f11e788bc57d9bd362c59ff62491445b2860eb8
Evaldas Kulpis <kulpis@pm.me>
2025-04-09 20:34:15 +0100

    trying to fix ferris sweep boot

--- keyboards/ferris/keymaps/ekulpis-sweet/config.h
+++ keyboards/ferris/keymaps/ekulpis-sweet/config.h
@@ -1,6 +1,6 @@
 #pragma once
-#define MASTER_LEFT
-
+//#define MASTER_LEFT
+#define EE_HANDS
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 175
 #define HOLD_ON_OTHER_KEY_PRESS
@@ -8,3 +8,5 @@
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 #define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define USB_POLLING_INTERVAL_MS 1
+#define DEBOUNCE 5
