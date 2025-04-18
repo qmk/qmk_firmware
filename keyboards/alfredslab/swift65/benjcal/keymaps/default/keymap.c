6913c990decdfd7ca7b9b95d23e0f3e2770ebaf1
Benjamin Calderon <benj.calderon@gmail.com>
2025-04-17 20:23:08 -0400

    colon

--- keyboards/alfredslab/swift65/benjcal/keymaps/default/keymap.c
+++ keyboards/alfredslab/swift65/benjcal/keymaps/default/keymap.c
@@ -16,6 +16,8 @@
 
 #include QMK_KEYBOARD_H
 
+#define _KC_CLN LSFT(KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,             KC_PGUP,
@@ -27,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,              _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_GRV,           _______,             KC_PGDN,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _KC_CLN, KC_GRV,           _______,             KC_PGDN,
         QK_BOOT,          _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TILD, _______, KC_VOLU,
         _______, _______, _______,                            _______,                   _______, _______,          KC_HOME, KC_VOLD, KC_END
     ),
