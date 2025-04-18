f2166565b2bec8b022dacfb951a2916db745383b
LucaFrankfurt <111198815+LucaFrankfurt@users.noreply.github.com>
2025-04-18 14:32:37 +0200

    Update keyboards/handwired/bad_numpad/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/bad_numpad/keymaps/default/keymap.c
+++ keyboards/handwired/bad_numpad/keymaps/default/keymap.c
@@ -4,15 +4,6 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 1 │ 2 │ 3 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 7 │ 8 │ 9 │ - │
-     * ├───┼───┼───┼───┤
-     */
     [0] = LAYOUT(
         KC_A, KC_B, KC_C, KC_D,
         KC_D, KC_E, KC_F, KC_G,
