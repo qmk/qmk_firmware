0f6926f21ccad154f034d5d84280dea53b3f0d7a
no <66252845+iH0118@users.noreply.github.com>
2025-04-22 16:53:56 +0200

    Apply suggestions from code review
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/0118/blacktip/config.h
+++ keyboards/0118/blacktip/config.h
@@ -3,7 +3,6 @@
 
 #pragma once
 
-//#define WS2812_POWER_ENABLE_PIN             A5
 #define WS2812_EXTERNAL_PULLUP
 #define WS2812_PWM_DRIVER                   PWMD2
 #define WS2812_PWM_CHANNEL                  1
@@ -11,11 +10,9 @@
 #define WS2812_PWM_DMA_STREAM	            STM32_DMA1_STREAM2
 #define WS2812_PWM_DMA_CHANNEL              8
 
-#define RGB_MATRIX_DEFAULT_MODE             RGB_MATRIX_MULTISPLASH
 #define RGB_MATRIX_TYPING_HEATMAP_SPREAD    25
 
 #define USB_VBUS_PIN                        A9
-#define SPLIT_TRANSPORT_MIRROR
 #define EE_HANDS
 
 #define STM32_ONBOARD_EEPROM_SIZE           (6*1024)
--- keyboards/0118/blacktip/halconf.h
+++ keyboards/0118/blacktip/halconf.h
@@ -3,8 +3,6 @@
 
 #pragma once
 
-//#define SERIAL_BUFFERS_SIZE 256
-
 #define HAL_USE_PWM         TRUE
 #define HAL_USE_SERIAL      TRUE
 
