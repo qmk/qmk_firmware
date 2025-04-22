4046f22b4a0fe179845fa11c6472c40dbc298a17
Rick Rezinas <rick@rezinas.com>
2025-04-21 19:46:08 -0700

    added homerow to sym layer and qwerty

--- keyboards/ai03/altair_x/keymaps/m8ryx/keymap.c
+++ keyboards/ai03/altair_x/keymaps/m8ryx/keymap.c
@@ -14,7 +14,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-        LT(MO(_QWERTY), KC_TAB),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    QK_LAYER_LOCK, QK_LAYER_LOCK, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_SLSH,
+        LT(MO(_QWERTY), KC_TAB),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    XXXXXXX, XXXXXXX, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_SLSH,
         MEH_T(KC_ESC), LGUI_T(KC_A), LALT_T(KC_R),LCTL_T(KC_S),LSFT_T(KC_T), HYPR_T(KC_G),    XXXXXXX, XXXXXXX, HYPR_T(KC_M), RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
         KC_LSFT, KC_X,    KC_C,    KC_D,    KC_V,    KC_Z, XXXXXXX, XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                    KC_LGUI, MO(_FN), KC_BSPC, LT(MO(_SYM), KC_SPC), LT(MO(_SYM), KC_SPC), KC_ENT, MO(_FN),  KC_DEL
@@ -32,9 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______
-    )
+    ),
+
     [_QWERTY] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    QK_LAYER_LOCK, QK_LAYER_LOCK, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         MEH_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                    KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_TAB,  KC_DEL
