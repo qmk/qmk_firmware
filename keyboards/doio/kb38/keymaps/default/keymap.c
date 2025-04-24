eadefc5093350783b32776850b73c8bf51f49d80
clownfish-og <177758267+clownfish-og@users.noreply.github.com>
2025-04-24 15:47:46 -0400

    fixup keymap and layout

--- keyboards/doio/kb38/keymaps/default/keymap.c
+++ keyboards/doio/kb38/keymaps/default/keymap.c
@@ -17,52 +17,52 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY,
-    _LAYERTWO
+    _BASE,
+    _FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* ┌───┐   ┌───────┐┌───┬───┬───┐┌───┬───┬───┐
-     * │ESC│   │KC_BSPC││F1 │F2 │F3 ││F4 │F5 │F6 │
+     * │Fn │   │KC_BSPC││F1 │F2 │F3 ││F4 │F5 │F6 │
      * └───┘   └───────┘└───┴───┴───┘└───┴───┴───┘
      * ┌───┬───┬───┬───┐┌───┬───┬───┐
-     * │NUM│ / │ * │ - ││PAS│SCR│PSC│
+     * │NUM│ / │ * │ - ││PSC│SCR│PAS│
      * ├───┼───┼───┼───┤├───┼───┼───┤┌───┐   ┌───┐
      * │ 7 │ 8 │ 9 │   ││INS│HOM│PGU││HOM│   │END│
      * ├───┼───┼───┤ + │├───┼───┼───┤├───┴───┴───┤
-     * │ 4 │ 5 │ 6 │   ││END│DEL│PGD││           │
+     * │ 4 │ 5 │ 6 │   ││DEL│END│PGD││           │
      * ├───┼───┼───┼───┤└───┼───┼───┘│           │
-     * │ 1 │ 2 │ 3 │ E │    │UP │    │     B     │
+     * │ 1 │ 2 │ 3 │ E │    │UP │    │           │
      * ├───┴───┼───┤ N │┌───┼───┼───┐│           │
      * │   0   │DEL│ T ││LFT│DWN│RHT││           │
      * └───────┴───┴───┘└───┴───┴───┘└───────────┘
      */
-    [_QWERTY] = LAYOUT(
-        MO(1),            KC_BSPC,             RM_PREV,  RM_TOGG, RM_NEXT,    KC_F1,   KC_F2,   KC_F3,
-        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,    KC_PAUS,  KC_SCRL, KC_PSCR,
+    [_BASE] = LAYOUT(
+        MO(_FN),          KC_BSPC,             KC_F1,    KC_F2,   KC_F3,      KC_F4,   KC_F5,   KC_F6,
+        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,    KC_PSCR,  KC_SCRL, KC_PAUS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_INS,   KC_HOME, KC_PGUP,    KC_HOME,          KC_END,
-        KC_P4,   KC_P5,   KC_P6,               KC_END,   KC_DEL,  KC_PGDN,
-        KC_P1,   KC_P2,   KC_P3,   KC_PENT,              KC_UP,                        KC_B,
+        KC_P4,   KC_P5,   KC_P6,               KC_DEL,   KC_END,  KC_PGDN,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,              KC_UP,                        KC_NO,
         KC_P0,            KC_PDOT,             KC_LEFT,  KC_DOWN, KC_RGHT
     ),
-    [_LAYERTWO] = LAYOUT(
-        _______,          KC_BSPC,             RM_PREV,  RM_TOGG, RM_NEXT,    KC_A,    QK_RBT,  QK_BOOT,
-        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,    KC_PAUS,  KC_SCRL, KC_PSCR,
-        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_INS,   KC_HOME, KC_PGUP,    KC_HOME,          KC_END,
-        KC_P4,   KC_P5,   KC_P6,               KC_END,   KC_DEL,  KC_PGDN,
-        KC_P1,   KC_P2,   KC_P3,   KC_PENT,              KC_UP,                        KC_B,
-        KC_P0,            KC_PDOT,             KC_LEFT,  KC_DOWN, KC_RGHT
+    [_FN] = LAYOUT(
+        _______,          RM_TOGG,             _______,  RM_SATU, RM_SATD,    RM_PREV, RM_NEXT, _______,
+        _______, _______, _______, _______,    _______,  _______, _______,
+        _______, _______, _______, _______,    _______,  _______, _______,    RM_SPDD,          RM_SPDU,
+        _______, _______, _______,             _______,  _______, _______,
+        _______, _______, _______, _______,              _______,                      KC_NO,
+        _______,          _______,             _______,  _______, _______
     )
 };
 
 /*NOTE FOR PERSON MODIFYING KEYMAP
-The large knob press is mapped as KC_B, despite it not having one.
+The large knob press is mapped as KC_NO, despite it not having one.
 I'm not quite sure why, but the only reason it can't be clicked is because the potentiometer is different.
 If you were to replace it with one that can be clicked, it would work. I shorted it and it does work.*/
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+    [_BASE] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(MS_WHLU, MS_WHLD), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_FN]   = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
