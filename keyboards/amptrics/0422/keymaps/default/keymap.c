24461b75adcc1d1d56b8ed1b613dc612542d35a3
Vinod Chowl <vchowl@outlook.com>
2025-04-27 21:43:59 +0530

    Amptrics 422 keyboard, review comments, removed eeprom emulation.

--- keyboards/amptrics/0422/keymaps/default/keymap.c
+++ keyboards/amptrics/0422/keymaps/default/keymap.c
@@ -29,48 +29,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┘
      */
     [0] = LAYOUT(
-        LT(0, KC_NO),
+        TO(1),
         KC_7,   KC_8,   KC_9,
         KC_4,   KC_5,   KC_6,
         KC_1,   KC_2,   KC_3
     ),
 
     [1] = LAYOUT(
-        LT(0, KC_NO),
+        TO(2),
         KC_A,   KC_B,   KC_C,
         KC_D,   KC_E,   KC_F,
         KC_G,   KC_H,   KC_I
     ),
 
     [2] = LAYOUT(
-        LT(0, KC_NO),
+        TO(3),
         KC_J,   KC_K,   KC_L,
         KC_M,   KC_N,   KC_O,
         KC_P,   KC_Q,   KC_R
     ),
 
     [3] = LAYOUT(
-        LT(0, KC_NO),
+        TO(0),
         KC_S,   KC_T,   KC_U,
         KC_V,   KC_W,   KC_X,
         KC_Y,   KC_Z,   KC_ENT
     )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t * record) {
-    switch (keycode) {
-        case LT(0, KC_NO):
-            if (record -> event.pressed) {
-                // Move to next layer on tap
-                uint8_t current_layer = get_highest_layer(layer_state);
-                if (current_layer < 3) {
-                    layer_move(current_layer + 1);
-                } else {
-                    layer_move(0); // Wrap around to first layer
-                }
-                return false; // Prevent further processing of this key
-            }
-            break;
-    }
-    return true; // Allow other keycodes to be processed normally
-}
