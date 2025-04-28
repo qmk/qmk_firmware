c327c161fff1720bc8841912e3f5ef16afc49036
Martijn Boers <martijn@plebian.nl>
2025-04-28 13:34:22 +0200

    Init macos config

--- keyboards/kradoindustries/promenade/keymaps/martijn/config.h
+++ keyboards/kradoindustries/promenade/keymaps/martijn/config.h
@@ -1,17 +1,16 @@
 #define TAPPING_TERM 200
 
-#define COPY        LCTL(KC_C)
-#define PASTE       LCTL(KC_V)
+#define COPY        LCMD(KC_C)
+#define PASTE       LCMD(KC_V)
 #define T_PREV      LCTL(KC_PGUP)
 #define T_NEXT      LCTL(KC_PGDN)
-#define N_TAB       LCTL(KC_T)
-#define W_QUIT      LALT(KC_F4)
+#define N_TAB       LCMD(KC_T)
+#define W_QUIT      LCMD(KC_Q)
 #define D_SUGGEST   LSFT(KC_DEL)
-
-#define ALL       LCTL(KC_A)
-
-#define SWITCH    LALT(KC_ESC)
-#define TERMINAL  RGUI(KC_T)
+#define LOCK        LGUI(LCTL(KC_Q))
+#define ALL         LCMD(KC_A)
+#define SWITCH      LALT(KC_ESC)
+#define TERMINAL    RGUI(KC_T)
 
 #define OSUP      LCTL(LALT(KC_UP))
 #define OSDOWN    LCTL(LALT(KC_DOWN))
