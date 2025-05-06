580e447d9f1beefaeffbd939e47eaa4c18806385
Nick Brassel <nick@tzarc.org>
2025-05-06 22:31:31 +1000

    Simpify.

--- builddefs/support.mk
+++ builddefs/support.mk
@@ -3,5 +3,5 @@ cc-option = $(shell \
 	if { echo 'int main(){return 0;}' | $(CC) $(1) $(3) -o /dev/null -x c /dev/null >/dev/null 2>&1; }; \
 	then echo "$(1)"; else echo "$(2)"; fi)
 
-# Helper to pass comma character to make functions
+# Helper to pass comma character to make functions (use with `$(,)` to pass in `$(call ...)` arguments)
 , := ,
