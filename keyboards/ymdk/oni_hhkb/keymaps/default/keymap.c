0c2b302654cfcfe3906005e7a25fc85daa434e04
Christian C. Berclaz <christian.berclaz@mac.com>
2025-05-06 21:40:13 +0200

    Update keyboards/ymdk/oni_hhkb/keymaps/default/keymap.c
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/ymdk/oni_hhkb/keymaps/default/keymap.c
+++ keyboards/ymdk/oni_hhkb/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI, KC_LALT,                                        KC_SPC,                                          KC_RALT, KC_RGUI, KC_RCTL
     ),
 
-    [1] = LAYOUT_60_tsangan_hhkb(
+    [1] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  _______, QK_BOOT,
         _______, _______, RM_TOGG, RM_VALU, RM_SPDU, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, RM_VALD, RM_SPDD, _______, _______, _______, _______, _______, _______, _______,               _______,
