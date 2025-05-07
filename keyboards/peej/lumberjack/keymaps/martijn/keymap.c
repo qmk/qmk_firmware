28f3969a40d5384104c7d95c2f18e92c9b1b5144
Martijn Boers <martijn@plebian.nl>
2025-05-07 11:37:49 +0200

    Nix + Direnv 💕

--- keyboards/peej/lumberjack/keymaps/martijn/keymap.c
+++ keyboards/peej/lumberjack/keymaps/martijn/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_BASE] = LAYOUT_ortho_5x12( \
   KC_ESC,   KC_1,    KC_2,       KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,       KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC1_O,       KC_P,    KC_BSLS, \
+  KC_TAB,   KC_Q,    KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,    KC_BSLS, \
   POKER,    KC_A,    KC_S,       KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_SCLN, KC_ENT, \
   SC_LSPO,  KC_Z,    KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,     KC_SLSH, SC_RSPC, \
   KC_LCTL,  EXTR,    KC_LGUI,    KC_LALT, PROG,    PROG,    KC_SPC,  KC_SPC,  KC_RALT, UM(THUMB),  KC_PSCR, KC_LCTL
@@ -88,14 +88,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_POKER] = LAYOUT_ortho_5x12( \
   CW_TOGG, _______,          _______, _______, _______,  _______,   _______,   _______,   _______,   PUSH_2_TALK, _______,   KC_DEL, \
   _______, KC_MPRV,          KC_MPLY, KC_MNXT, KC_F5,    N_TAB,     KC_PAUSE,  T_PREV,    KC_UP,     T_NEXT,      KC_ENT,    W_QUIT, \
-  _______, TD(LAYER_SWITCH), KC_VOLD, KC_VOLU, KC_MUTE,  _______,   KC_HOME,   KC_LEFT,   KC_DOWN,   KC_RGHT,     KC_BSPC,   KC_DEL, \
+  _______, TD(TAP_SHFT),     KC_VOLD, KC_VOLU, KC_MUTE,  _______,   KC_HOME,   KC_LEFT,   KC_DOWN,   KC_RGHT,     KC_BSPC,   KC_DEL, \
   _______, _______,          _______, COPY,    PASTE,    _______,   KC_END,    _______,   E_BACK,    E_FORWARD,   ALL,       C_HOME, \
   _______, _______,          _______, _______, _______,  _______,   KC_SPC,    _______,    _______,  _______,     _______,   _______
 ),
 
 [_PROGRAM] = LAYOUT_ortho_5x12( \
   KC_F12,    KC_F1,   KC_F2,   KC_F3,              KC_F4,             KC_F5,    KC_F6,     KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11, \
-  SWITCH,    KC_GRV,  KC_DLR,  KC_TILD,            KC_ASTERISK,       TERMINAL, _______,   _______,  KC_LBRC,  KC_RBRC, _______,  _______, \
+  SWITCH,    KC_GRV,  KC_DLR,  KC_TILD,            KC_ASTERISK,       KC_CIRC,  _______,   _______,  KC_LBRC,  KC_RBRC, _______,  _______, \
   _______,   KC_PLUS, KC_UNDS, KC_MINS,            KC_EQL,            OSDOWN,   OSUP,      KC_DQUO,  KC_LCBR,  KC_RCBR, KC_QUOT,  _______, \
   KC_LSFT,   _______, _______, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP,  _______,  _______,   _______,  _______,  _______, _______,  _______, \
   _______,   _______, _______, _______,            _______,           _______,  _______,   _______,  _______,  _______, _______,  _______
