e4ae5ef3f2a2cca0a19788f5291ca7eea445bd43
David Hoelscher <infinityis@users.noreply.github.com>
2025-05-07 16:22:21 -0500

    incorporating changes from drashna

--- keyboards/custommk/ergosentry_ansi/ergosentry_ansi.c
+++ keyboards/custommk/ergosentry_ansi/ergosentry_ansi.c
@@ -2,19 +2,19 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <stdbool.h>
-#include "keyboards/custommk/cmk_cm6646.h"
+#include "ergosentry_ansi.h"
 
 typedef union {
     uint32_t raw;
     struct {
         bool usb_audio_enabled;
     };
-} user_config_t;
+} keyboard_config_t;
 
-user_config_t user_config;
+keyboard_config_t keyboard_config;
 
 void update_kb_eeprom(void) {
-    eeconfig_update_kb(user_config.raw);
+    eeconfig_update_kb(keyboard_config.raw);
 }
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
@@ -23,29 +23,30 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 }
 
 void keyboard_post_init_kb(void) {
-    user_config.raw = eeconfig_read_kb();
-    cmk_cmk6646_init(user_config.usb_audio_enabled);
+    keyboard_config.raw = eeconfig_read_kb();
+    cmk_cmk6646_init(keyboard_config.usb_audio_enabled);
     keyboard_post_init_user();
 }
 
 void housekeeping_task_kb(void) {
     bool usb_audio_enabled;
     usb_audio_enabled = cmk_cmk6646_housekeeping_task();
-    if (usb_audio_enabled != user_config.usb_audio_enabled) {
-        user_config.usb_audio_enabled = usb_audio_enabled;
+    if (usb_audio_enabled != keyboard_config.usb_audio_enabled) {
+        keyboard_config.usb_audio_enabled = usb_audio_enabled;
         update_kb_eeprom();
     }
 }
 
-void eeconfig_init_kb() {
+void eeconfig_init_kb(void) {
     //enable nkro by default (not forced)
     keymap_config.raw = eeconfig_read_keymap();
     keymap_config.nkro = 1;
     eeconfig_update_keymap(keymap_config.raw);
 
-    //enable audio by default (not forced)
-    user_config.raw = eeconfig_read_kb();
-    user_config.usb_audio_enabled = 1;
-    eeconfig_update_kb(user_config.raw);
+    //enable USB audio by default (not forced)
+    keyboard_config.raw = eeconfig_read_kb();
+    keyboard_config.usb_audio_enabled = 1;
+    eeconfig_update_kb(keyboard_config.raw);
 
+    eeconfig_init_user();
 }
