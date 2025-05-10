f5cfa68738f662a7013668135d9dfca1d1b09920
Pascal Getreuer <50221757+getreuer@users.noreply.github.com>
2025-05-10 11:45:47 -0700

    Update keyboards/handwired/onekey/keymaps/cm_flow_rgb_matrix_effect/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/onekey/keymaps/cm_flow_rgb_matrix_effect/config.h
+++ keyboards/handwired/onekey/keymaps/cm_flow_rgb_matrix_effect/config.h
@@ -11,5 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#pragma once
 
 #define SNLED27351_I2C_ADDRESS_1 SNLED27351_I2C_ADDRESS_GND
