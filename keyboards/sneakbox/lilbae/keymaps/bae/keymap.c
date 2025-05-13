c7ad010a2f974c798d11eed34dce2feb9c8177e9
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2025-05-13 11:35:19 -0500

    Update keyboards/sneakbox/lilbae/keymaps/bae/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/sneakbox/lilbae/keymaps/bae/keymap.c
+++ keyboards/sneakbox/lilbae/keymaps/bae/keymap.c
@@ -19,8 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE,
-    _FN
+    _BASE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
