b72034c6cbd395936dbee198b0c86ffd59a2521d
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2025-05-13 09:28:23 -0500

    Update keyboards/sneakbox/lilbae/keymaps/ansi_enter/keymap.c
    Co-authored-by: ClownFish <177758267+clownfish-og@users.noreply.github.com>

--- keyboards/sneakbox/lilbae/keymaps/ansi_enter/keymap.c
+++ keyboards/sneakbox/lilbae/keymaps/ansi_enter/keymap.c
@@ -32,10 +32,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_TRNS,
               KC_TRNS),
 };
-
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_BASE] = { ENCODER_CCW_CW(KC_1, KC_2)},
-    [_FN]   = { ENCODER_CCW_CW(KC_1, KC_2)}
-};
-#endif
