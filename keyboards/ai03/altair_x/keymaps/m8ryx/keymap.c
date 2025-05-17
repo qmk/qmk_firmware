e8f17c3bc12f12689f7367e48da55b687c47a706
Rick Rezinas <rick@rezinas.com>
2025-05-17 16:45:11 -0700

    introducing term adjustment

--- keyboards/ai03/altair_x/keymaps/m8ryx/keymap.c
+++ keyboards/ai03/altair_x/keymaps/m8ryx/keymap.c
@@ -13,8 +13,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-        LT(MO(_QWERTY), KC_TAB),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    XXXXXXX, XXXXXXX, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_SLSH,
-        MEH_T(KC_ESC), LGUI_T(KC_A), LALT_T(KC_R),LCTL_T(KC_S),LSFT_T(KC_T), HYPR_T(KC_G),    XXXXXXX, XXXXXXX, HYPR_T(KC_M), RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
+        LT(MO(_QWERTY), KC_TAB),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    DT_UP, DT_PRINT, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_SLSH,
+        MEH_T(KC_ESC), LGUI_T(KC_A), LALT_T(KC_R),LCTL_T(KC_S),LSFT_T(KC_T), HYPR_T(KC_G),    DT_DOWN, XXXXXXX, HYPR_T(KC_M), RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
         KC_LSFT, KC_X,    KC_C,    KC_D,    KC_V,    KC_Z, QK_MAKE, QK_BOOT, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                                    KC_LGUI, MO(_FN), KC_BSPC, LT(MO(_SYM), KC_SPC), LT(MO(_SYM), KC_SPC), KC_ENT, MO(_FN),  KC_DEL
     ),
@@ -41,4 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
+    [_VI] = LAYOUT(
+        _______, _______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______,
+        _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, _______, KC_H, KC_J, KC_K, KC_L, _______, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______
 };
