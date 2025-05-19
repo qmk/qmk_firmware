ab91c29c9222b8943f293f4da0fe27317cb4cf4f
Stefan Kerkmann <karlk90@pm.me>
2025-05-19 07:30:33 +0200

    chibios: boards: stm32g0b1xb: use ST_NUCLEO64_G0B1RE board as base
    Signed-off-by: Stefan Kerkmann <karlk90@pm.me>

--- platforms/chibios/boards/GENERIC_STM32_G0B1XB/board/board.mk
+++ platforms/chibios/boards/GENERIC_STM32_G0B1XB/board/board.mk
@@ -1,8 +1,11 @@
 # List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/board/board.c
+BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO64_G0B1RE/board.c
+
+# Extra files
+BOARDSRC += $(BOARD_PATH)/board/extra.c
 
 # Required include directories
-BOARDINC = $(BOARD_PATH)/board
+BOARDINC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO64_G0B1RE
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)
