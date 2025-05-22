00581fab744ff3635ed56b4cfa6c861a72ed7f77
etiennecollin <collin.etienne.contact@gmail.com>
2025-05-22 02:59:43 -0400

    Fixed right hand modifiers

--- keyboards/etiennecollin/wave/keymaps/default/keymap.c
+++ keyboards/etiennecollin/wave/keymaps/default/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                 --------+-------+--------                                                          --------+-------+--------
     KC_Q,               KC_W,               KC_F,               KC_P,               KC_B,               KC_J,               KC_L,               KC_U,               KC_Y,               KC_QUOT,
     KC_A,               KC_R,               KC_S,               KC_T,               KC_G,               KC_M,               KC_N,               KC_E,               KC_I,               KC_O,
-    LGUI_T(KC_Z),       LALT_T(KC_X),       LCTL_T(KC_C),       LSFT_T(KC_D),       KC_V,               KC_K,               LSFT_T(KC_H),       LCTL_T(KC_COMM),    LALT_T(KC_DOT),     LGUI_T(KC_SLSH),
+    LGUI_T(KC_Z),       LALT_T(KC_X),       LCTL_T(KC_C),       LSFT_T(KC_D),       KC_V,               KC_K,               RSFT_T(KC_H),       RCTL_T(KC_COMM),    RALT_T(KC_DOT),     RGUI_T(KC_SLSH),
                                             LT(MED, KC_ESC),    LT(NAV, KC_SPC),    LT(MOS, KC_TAB),    LT(SYM, KC_ENT),    LT(NUM, KC_BSPC),    LT(FUN, KC_DEL)
   ),
   [QWE] = LAYOUT_split_3x5_3(
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                 --------+-------+--------                                                          --------+-------+--------
     KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,               KC_Y,               KC_U,               KC_I,               KC_O,               KC_P,
     KC_A,               KC_S,               KC_D,               KC_F,               KC_G,               KC_H,               KC_J,               KC_K,               KC_L,               KC_QUOT,
-    LGUI_T(KC_Z),       LALT_T(KC_X),       LCTL_T(KC_C),       LSFT_T(KC_V),       KC_B,               KC_N,               LSFT_T(KC_M),       LCTL_T(KC_COMM),    LALT_T(KC_DOT),     LGUI_T(KC_SLSH),
+    LGUI_T(KC_Z),       LALT_T(KC_X),       LCTL_T(KC_C),       LSFT_T(KC_V),       KC_B,               KC_N,               RSFT_T(KC_M),       RCTL_T(KC_COMM),    RALT_T(KC_DOT),     RGUI_T(KC_SLSH),
                                             LT(MED, KC_ESC),    LT(NAV, KC_SPC),    LT(MOS, KC_TAB),    LT(SYM, KC_ENT),    LT(NUM, KC_BSPC),    LT(FUN, KC_DEL)
   ),
   [GAM] = LAYOUT_split_3x5_3(
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                 --------+-------+--------                                                          --------+-------+--------
     KC_PIPE,            KC_AMPR,            KC_ASTR,            KC_LPRN,            KC_LCBR,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
     KC_COLN,            KC_DLR,             KC_PERC,            KC_CIRC,            KC_PLUS,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
-    KC_TILD,            KC_EXLM,            KC_AT,              KC_HASH,            KC_UNDS,            XXXXXXX,            KC_LSFT,            KC_LCTL,            KC_LALT,            KC_LGUI,
+    KC_TILD,            KC_EXLM,            KC_AT,              KC_HASH,            KC_UNDS,            XXXXXXX,            KC_RSFT,            KC_RCTL,            KC_RALT,            KC_RGUI,
                                             KC_LPRN,            KC_RPRN,            KC_RCBR,            _______,            XXXXXXX,            XXXXXXX
   ),
   [NUM] = LAYOUT_split_3x5_3(
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                 --------+-------+--------                                                          --------+-------+--------
     KC_BSLS,            KC_7,               KC_8,               KC_9,               KC_LBRC,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
     KC_SCLN,            KC_4,               KC_5,               KC_6,               KC_EQL,             XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
-    KC_GRV,             KC_1,               KC_2,               KC_3,               KC_MINS,            XXXXXXX,            KC_LSFT,            KC_LCTL,            KC_LALT,            KC_LGUI,
+    KC_GRV,             KC_1,               KC_2,               KC_3,               KC_MINS,            XXXXXXX,            KC_RSFT,            KC_RCTL,            KC_RALT,            KC_RGUI,
                                             KC_DOT,             KC_0,               KC_RBRC,            XXXXXXX,            _______,            XXXXXXX
   ),
   [FUN] = LAYOUT_split_3x5_3(
@@ -162,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                 --------+-------+--------                                                          --------+-------+--------
     KC_PSCR,             KC_F7,              KC_F8,              KC_F9,             KC_F12,             XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
     KC_PAUS,             KC_F4,              KC_F5,              KC_F6,             KC_F11,             XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
-    KC_SCRL,             KC_F1,              KC_F2,              KC_F3,             KC_F10,             XXXXXXX,            KC_LSFT,            KC_LCTL,            KC_LALT,            KC_LGUI,
+    KC_SCRL,             KC_F1,              KC_F2,              KC_F3,             KC_F10,             XXXXXXX,            KC_RSFT,            KC_RCTL,            KC_RALT,            KC_RGUI,
                                              KC_APP,             KC_SPC,            KC_TAB,             XXXXXXX,            XXXXXXX,            _______
   ),
   [SYS] = LAYOUT_split_3x5_3(
