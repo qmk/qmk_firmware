4806990037882b43b35657d6a5073016954b9357
etiennecollin <collin.etienne.contact@gmail.com>
2025-05-22 03:50:20 -0400

    Formatting of config.h

--- keyboards/etiennecollin/wave/keymaps/default/config.h
+++ keyboards/etiennecollin/wave/keymaps/default/config.h
@@ -36,9 +36,9 @@
 // Mouse key speed and acceleration.
 #define MOUSEKEY_DELAY 0
 #define MOUSEKEY_INTERVAL 16
-#define MOUSEKEY_WHEEL_DELAY 0
 #define MOUSEKEY_MAX_SPEED 6
 #define MOUSEKEY_TIME_TO_MAX 64
+#define MOUSEKEY_WHEEL_DELAY 0
 
 // Thumb Combos
 #define COMBO_COUNT 2
