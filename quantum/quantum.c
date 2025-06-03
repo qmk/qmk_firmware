f096e5a3f3af404e6f752185260f183d3ecd503e
Joel Challis <git@zvecr.com>
2025-06-03 23:44:46 +0100

    Relocate remaining `process_record_quantum` keycodes (#25328)

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -15,6 +15,7 @@
  */
 
 #include "quantum.h"
+#include "process_quantum.h"
 
 #ifdef BACKLIGHT_ENABLE
 #    include "process_backlight.h"
@@ -82,6 +83,10 @@
 
 #ifdef LAYER_LOCK_ENABLE
 #    include "process_layer_lock.h"
+#endif
+
+#ifndef NO_ACTION_ONESHOT
+#    include "process_oneshot.h"
 #endif
 
 #ifdef AUDIO_ENABLE
@@ -439,83 +444,11 @@ bool process_record_quantum(keyrecord_t *record) {
 #ifdef CONNECTION_ENABLE
             process_connection(keycode, record) &&
 #endif
-            true)) {
-        return false;
-    }
-
-    if (record->event.pressed) {
-        switch (keycode) {
-#ifndef NO_RESET
-            case QK_BOOTLOADER:
-                reset_keyboard();
-                return false;
-            case QK_REBOOT:
-                soft_reset_keyboard();
-                return false;
-#endif
-#ifndef NO_DEBUG
-            case QK_DEBUG_TOGGLE:
-                debug_enable ^= 1;
-                if (debug_enable) {
-                    print("DEBUG: enabled.\n");
-                } else {
-                    print("DEBUG: disabled.\n");
-                }
-                return false;
-#endif
-            case QK_CLEAR_EEPROM:
-#ifdef NO_RESET
-                eeconfig_init();
-#else
-                eeconfig_disable();
-                soft_reset_keyboard();
-#endif
-                return false;
-#ifdef VELOCIKEY_ENABLE
-            case QK_VELOCIKEY_TOGGLE:
-                velocikey_toggle();
-                return false;
-#endif
 #ifndef NO_ACTION_ONESHOT
-            case QK_ONE_SHOT_TOGGLE:
-                oneshot_toggle();
-                return false;
-            case QK_ONE_SHOT_ON:
-                oneshot_enable();
-                return false;
-            case QK_ONE_SHOT_OFF:
-                oneshot_disable();
-                return false;
+            process_oneshot(keycode, record) &&
 #endif
-#ifdef ENABLE_COMPILE_KEYCODE
-            case QK_MAKE: // Compiles the firmware, and adds the flash command based on keyboard bootloader
-            {
-#    ifdef NO_ACTION_ONESHOT
-                const uint8_t temp_mod = mod_config(get_mods());
-#    else
-                const uint8_t temp_mod = mod_config(get_mods() | get_oneshot_mods());
-                clear_oneshot_mods();
-#    endif
-                clear_mods();
-
-                SEND_STRING_DELAY("qmk", TAP_CODE_DELAY);
-                if (temp_mod & MOD_MASK_SHIFT) { // if shift is held, flash rather than compile
-                    SEND_STRING_DELAY(" flash ", TAP_CODE_DELAY);
-                } else {
-                    SEND_STRING_DELAY(" compile ", TAP_CODE_DELAY);
-                }
-#    if defined(CONVERTER_ENABLED)
-                SEND_STRING_DELAY("-kb " QMK_KEYBOARD " -km " QMK_KEYMAP " -e CONVERT_TO=" CONVERTER_TARGET SS_TAP(X_ENTER), TAP_CODE_DELAY);
-#    else
-                SEND_STRING_DELAY("-kb " QMK_KEYBOARD " -km " QMK_KEYMAP SS_TAP(X_ENTER), TAP_CODE_DELAY);
-#    endif
-                if (temp_mod & MOD_MASK_SHIFT && temp_mod & MOD_MASK_CTRL) {
-                    reset_keyboard();
-                }
-                return false;
-            }
-#endif
-        }
+            process_quantum(keycode, record))) {
+        return false;
     }
 
     return process_action_kb(record);
