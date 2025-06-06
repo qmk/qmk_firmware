8c3587207a15ae183eefe1582cdd5595a3756bdb
jacksaxi <jacksaxi2008@gmail.com>
2025-06-06 12:44:12 +0300

    Update rules.mk

--- keyboards/mectecpad/rules.mk
+++ keyboards/mectecpad/rules.mk
@@ -11,3 +11,22 @@ LTO_ENABLE = yes         # Enable Link Time Optimization for smaller firmware
 EXTRAFLAGS += -Os        # Optimize for size
 
 VIA_ENABLE = yes          # Enable VIA support
+
+#/*
+#* rules.mk
+#*
+#* Copyright 2025 Iakovos Sachinidhs isachinidhs@gmail.com
+#*
+#* This program is free software: you can redistribute it and/or modify
+#* it under the terms of the GNU General Public License as published by
+#* the Free Software Foundation, either version 2 of the License, or
+#* (at your option) any later version.
+#*
+#* This program is distributed in the hope that it will be useful,
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#* GNU General Public License for more details.
+#*
+#* You should have received a copy of the GNU General Public License
+#* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#*/
