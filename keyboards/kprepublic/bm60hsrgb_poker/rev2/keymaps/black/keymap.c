032a63559c48d319103e42ba3ee0a18fb635924e
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2025-06-08 00:27:02 +0200

    feat(): remove keylogger from poker

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/black/keymap.c
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/black/keymap.c
@@ -17,11 +17,6 @@
 #include QMK_KEYBOARD_H
 
 #include "eeprom.h"
-#include "print.h"
-#include "keylogger.h"
-
-#define TD_LANG TD(QU_LANG)
-#define TD_LAYR TD(QU_LAYR)
 
 #define LT_SPC LT(LY_NAV, KC_SPC)
 #define MO_CTRL MO(LY_CTRL)
@@ -29,49 +24,32 @@
 #define LCTL_SPC C(KC_SPC)
 
 enum {
-    QU_REPL = SAFE_RANGE,
-    QU_RAND,
+    QU_RAND = SAFE_RANGE
 };
 
 enum {
-    QU_LANG,
-    QU_LAYR,
-};
-
-enum {
-    LY_BASE,
+    LY_BASE = 0,
     LY_NAV,
     LY_CTRL,
-};
-
-void on_lang_finished(tap_dance_state_t *state, void *user_data);
-void on_lang_reset(tap_dance_state_t *state, void *user_data);
-
-void on_layer_finished(tap_dance_state_t *state, void *user_data);
-void on_layer_reset(tap_dance_state_t *state, void *user_data);
-
-tap_dance_action_t tap_dance_actions[] = {
-    [QU_LANG] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, on_lang_finished, on_lang_reset),
-    [QU_LAYR] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, on_layer_finished, on_layer_reset),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LY_BASE] = LAYOUT_60_ansi(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT ,
         SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   SC_RSPC,
-        KC_LCTL, KC_LALT, KC_LGUI,                            LT_SPC,                             TD_LANG, _______, KC_RGUI, KC_RCTL
+        KC_LCTL, KC_LALT, KC_LGUI,                            LT_SPC,                             KC_RGUI, MO_CTRL, KC_RALT, KC_RCTL
     ),
     [LY_NAV] = LAYOUT_60_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
-        _______, _______, _______,                            _______,                            _______, MO_CTRL, _______, _______
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
     [LY_CTRL] = LAYOUT_60_ansi(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QU_RAND,
+        QK_BOOT, EE_CLR , QK_RBT , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QU_RAND,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
@@ -79,31 +57,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-keylogger_t keylogger;
-
-void keyboard_pre_init_user(void) {
-    debug_enable = true;
-    keylogger_initialize(&keylogger);
-}
 
 void keyboard_post_init_user(void) {
+#ifdef RGB_MATRIX_ENABLE
     rgb_matrix_enable_noeeprom();
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_TYPING_HEATMAP);
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_DEFAULT_MODE);
+#endif /* RGB_MATRIX_ENABLE */
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case QU_REPL:
-            if (record->event.pressed) {
-                for (uint8_t i = 0; i < keylogger.written; i++) {
-                    //                    uprintf("i: %d\n", i);
-                    uint8_t pos = (keylogger_cycle_pos(&keylogger) + i) % KEY_LOGGER_BUF_SIZE;
-                    //                    uprintf("pos: %d\n", pos);
-                    uint16_t kc = keylogger.keycodes[pos];
-                    tap_code_delay(kc, 80);
-                }
-            }
-            return false;
         case QU_RAND:
             if (record->event.pressed) {
                 for (uint8_t i = 0; i < 16; i++) {
@@ -117,42 +80,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QU_REPL ... QU_RAND:
-            return;
-        default:
-            if (record->event.pressed) {
-                //                uprintf("initial pos: %d written: %d\n", keylogger.pos, keylogger.written);
-                keylogger_record_keycode(&keylogger, keycode);
-                //                uprintf("record pos: %d written: %d\n", keylogger.pos, keylogger.written);
-            }
-    }
-}
-
-void on_lang_finished(tap_dance_state_t *state, void *user_data) {
-    register_code16(KC_LCTL);
-    tap_code16(KC_SPC);
-}
-
-void on_lang_reset(tap_dance_state_t *state, void *user_data) {
-    unregister_code16(KC_LCTL);
-}
-
-void on_layer_finished(tap_dance_state_t *state, void *user_data) {
-    if (state->count > 1) {
-        layer_invert(LY_NAV);
-    } else {
-        layer_on(LY_NAV);
-    }
-}
-
-void on_layer_reset(tap_dance_state_t *state, void *user_data) {
-    if (state->count < 2) {
-        layer_off(LY_NAV);
-    }
-}
-
+#ifdef RGB_MATRIX_ENABLE
 bool rgb_matrix_indicators_user(void) {
     switch (get_highest_layer(layer_state | default_layer_state)) {
         case LY_NAV:
@@ -176,10 +104,11 @@ bool rgb_matrix_indicators_user(void) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-        case 0:
+        case LY_BASE:
             rgb_matrix_mode_noeeprom(RGB_MATRIX_TYPING_HEATMAP);
             break;
     }
 
     return state;
 }
+#endif /* RGB_MATRIX_ENABLE */
--- keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/black/rules.mk
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/black/rules.mk
@@ -1,5 +1,5 @@
 MOUSEKEY_ENABLE  = no        # Mouse keys
 EXTRAKEY_ENABLE  = no        # Audio control and System control
 CONSOLE_ENABLE   = no        # Console for debug
-TAP_DANCE_ENABLE = yes       # Tap dance a single key can do different things
-SRC += keylogger.c
+TAP_DANCE_ENABLE = no       # Tap dance a single key can do different things
+RGB_MATRIX_ENABLE = yes
