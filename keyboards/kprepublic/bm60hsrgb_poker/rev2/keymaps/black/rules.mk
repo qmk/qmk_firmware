b2b1143c7ed4cec39551be33fae58a7f2526d069
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2025-06-10 09:41:04 +0200

    Disable RGB_MATRIX_ENABLE

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/black/rules.mk
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/black/rules.mk
@@ -2,4 +2,4 @@ MOUSEKEY_ENABLE  = no        # Mouse keys
 EXTRAKEY_ENABLE  = no        # Audio control and System control
 CONSOLE_ENABLE   = no        # Console for debug
 TAP_DANCE_ENABLE = no       # Tap dance a single key can do different things
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
