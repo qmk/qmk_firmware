a0cb6202c115d939a868b176ed3521e1fe663822
jacksaxi <jacksaxi2008@gmail.com>
2025-06-13 22:09:24 +0300

    Update keyboards/mectechpad/config.h
    Co-authored-by: ClownFish <177758267+clownfish-og@users.noreply.github.com>

--- keyboards/mectechpad/config.h
+++ keyboards/mectechpad/config.h
@@ -1,10 +1,6 @@
 // Copyright 2025 Jack Sachinidhs (@jacksaxi)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
-/* Key matrix size */
-#define MATRIX_ROWS 3
-#define MATRIX_COLS 3
-
 /* LED pin definitions */
 #define LED_PIN_LAYER1 GP12
 #define LED_PIN_LAYER2 GP11
