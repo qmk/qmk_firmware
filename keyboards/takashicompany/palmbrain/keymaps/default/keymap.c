3dc3b629e11bab830a5610224613138203621961
takashicompany <t@kashi.company>
2025-06-17 19:53:22 +0900

    Update keyboards/takashicompany/palmbrain/keymaps/default/keymap.c
    Co-authored-by: Duncan Sutherland <dunk2k_2000@hotmail.com>

--- keyboards/takashicompany/palmbrain/keymaps/default/keymap.c
+++ keyboards/takashicompany/palmbrain/keymaps/default/keymap.c
@@ -5,7 +5,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT(
+    [0] = LAYOUT_split_3x5_2(
         LT(4, KC_Q), KC_W, KC_E, LT(3, KC_R), KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A, KC_S, LT(4, KC_D), KC_F, KC_G, KC_H, KC_J, LT(3, KC_K), KC_L, KC_ENT,
         LSFT_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LCTL_T(KC_DOT), KC_BSPC,
