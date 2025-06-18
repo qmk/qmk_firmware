1ee762e125c88b23c6a06fb115bf6f0b16ab92d2
jacksaxi <jacksaxi2008@gmail.com>
2025-06-18 15:33:57 +0300

    Update keyboards/mectechpad/mectechpad.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/mectechpad/mectechpad.c
+++ keyboards/mectechpad/mectechpad.c
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "quantum.h"
 
-// Initialize LED pins
-void matrix_init_kb() {
+void keyboard_post_init_kb(void) {
+    // Initialize LED pins
     gpio_set_pin_output(LED_PIN_LAYER_0);
     gpio_write_pin_low(LED_PIN_LAYER_0);
     gpio_set_pin_output(LED_PIN_LAYER_1);
@@ -12,7 +12,10 @@ void matrix_init_kb() {
     gpio_write_pin_low(LED_PIN_LAYER_2);
     gpio_set_pin_output(LED_PIN_LAYER_3);
     gpio_write_pin_low(LED_PIN_LAYER_3);
+
+    keyboard_post_init_user();
 }
+
 // Update LEDs based on the current layer
 void housekeeping_task_kb(void) {
     gpio_write_pin(LED_PIN_LAYER_0, (get_highest_layer(layer_state) == 0));
