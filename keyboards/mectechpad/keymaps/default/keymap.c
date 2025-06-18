c467da28b995c3d1ee0fd79267bc340a189b9fc4
jacksaxi <jacksaxi2008@gmail.com>
2025-06-18 12:29:43 +0300

    Update keyboards/mectechpad/keymaps/default/keymap.c
    Co-authored-by: ClownFish <177758267+clownfish-og@users.noreply.github.com>

--- keyboards/mectechpad/keymaps/default/keymap.c
+++ keyboards/mectechpad/keymaps/default/keymap.c
@@ -21,7 +21,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_R, KC_T, KC_Y,
         KC_U, KC_I, KC_O
     ),
-    [_LAYER3] = LAYOUT(
+    [_LAYER2] = LAYOUT(
+        TO(_LAYER3),
         KC_A, KC_S, KC_D,
         KC_F, KC_G, KC_H,
         KC_J, KC_K, KC_L
