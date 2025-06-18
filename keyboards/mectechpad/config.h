cb9f8e049809e452cef71c4c733cb9cb291f8752
jacksaxi <jacksaxi2008@gmail.com>
2025-06-18 12:28:30 +0300

    Update keyboards/mectechpad/config.h
    Co-authored-by: ClownFish <177758267+clownfish-og@users.noreply.github.com>

--- keyboards/mectechpad/config.h
+++ keyboards/mectechpad/config.h
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 /* LED pin definitions */
-#define LED_PIN_LAYER1 GP12
-#define LED_PIN_LAYER2 GP11
-#define LED_PIN_LAYER3 GP10
-#define LED_PIN_LAYER4 GP9
+#define LED_PIN_LAYER_0 GP12
+#define LED_PIN_LAYER_1 GP11
+#define LED_PIN_LAYER_2 GP10
+#define LED_PIN_LAYER_3 GP9
 
 /* Pin for the layer change button */
 #define LAYER_CHANGE_PIN GP13
