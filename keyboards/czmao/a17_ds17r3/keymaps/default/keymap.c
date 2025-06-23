62b99da18d8897e0cf29d8fd96046cc4835b5780
Micah <45813290+micahyy@users.noreply.github.com>
2025-06-23 23:30:15 +0800

    update keymap

--- keyboards/czmao/a17_ds17r3/keymaps/default/keymap.c
+++ keyboards/czmao/a17_ds17r3/keymaps/default/keymap.c
@@ -15,16 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
-// enum custom_keycodes {
-//   QWERTY = SAFE_RANGE,
-//   LOWER,
-//   RAISE
-// };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_numpad_5x4(
 //1    2        3        4        
