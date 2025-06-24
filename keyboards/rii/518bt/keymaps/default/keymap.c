6e5fe5f77c1e51af204c23232f38e75a934a7b01
Tim Eastwood <tim.eastwood@hotmail.com>
2025-06-24 13:34:38 +1000

    remove diag config

--- keyboards/rii/518bt/keymaps/default/keymap.c
+++ keyboards/rii/518bt/keymaps/default/keymap.c
@@ -16,14 +16,6 @@
 #include QMK_KEYBOARD_H
 #define CTALDEL LCTL(LALT(KC_DEL))
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  // If console is enabled, it will print the matrix position and status of each key pressed
-#ifdef CONSOLE_ENABLE
-    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
-#endif 
-  return true;
-}
-
 /* Membrane labels - base
  *        .----------------------------------------------------------------------------.
  *        |light | home |  GUI | MUTE | VOL- | VOL+ | Prev | Pause| Next |    Bksp     |
