c951c6dd9f2c64575743531cf59afafbbaebc157
wholteza <zackarias@montell.se>
2025-06-26 22:22:11 +0200

    wip getting serial to work

---
+++ keyboards/5x6_split/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN GP0     // USART TX pin
+#define SERIAL_USART_RX_PIN GP1     // USART RX pin
+
+#define SERIAL_DEBUG
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25 // Specify a optional status led by GPIO number which blinks when entering the bootloader
