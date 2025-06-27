ff09b5e4f983b297b3d4138e68d2a432d748bead
kevin takano <kevin.takano@gmail.com>
2025-06-27 14:26:20 -0300

    Update keymap.c

--- keyboards/crkbd/keymaps/kevin/keymap.c
+++ keyboards/crkbd/keymaps/kevin/keymap.c
@@ -281,11 +281,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYM] = LAYOUT_split_3x5_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-      KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_LCTL, KC_RCTL, KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN,
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,      KC_F8,    KC_F9,    KC_F10, KC_F11, KC_F12,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-      KC_QUES, KC_UNDS, KC_COLN, KC_EQL, KC_GRV,   KC_LALT, KC_RALT, KC_MINS,  KC_LBRC,  KC_LCBR, KC_RCBR, KC_RBRC,
+      KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_GRV,   KC_LALT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
-      KC_SCLN, KC_LT, KC_GT, KC_PLUS,   KC_TILD,                      KC_UNDS, KC_PLUS,  KC_LPRN, KC_RPRN,  KC_PIPE,
+LCTL(KC_GRV), LALT(KC_GRV), LGUI(KC_GRV), LSFT(KC_GRV),KC_TILD,       KC_UNDS, KC_PLUS,  KC_LPRN, KC_RPRN,  KC_PIPE,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
                                 KC_LGUI, KC_SPC,  KC_SPC,   KC_ENT, KC_BSPC, KC_LSFT
                                       //`--------------------------'  `--------------------------'
