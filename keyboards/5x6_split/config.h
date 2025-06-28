80aa9a9faca1267a04e2ed32878be0e39023274b
wholteza <zackarias@montell.se>
2025-06-28 13:29:20 +0200

    wip try using half duplex

--- keyboards/5x6_split/config.h
+++ keyboards/5x6_split/config.h
@@ -1,11 +1,14 @@
 #pragma once
 
-#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
-#define SERIAL_USART_TX_PIN GP0     // USART TX pin
-#define SERIAL_USART_RX_PIN GP1     // USART RX pin
+//#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN GP2     // USART TX pin
+//#define SERIAL_USART_RX_PIN GP1     // USART RX pin
 
 #define SERIAL_DEBUG
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 3000
