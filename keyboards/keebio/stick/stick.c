578496aad5b42d47798295f109be35520d957f45
zvecr <git@zvecr.com>
2025-06-28 02:55:03 +0100

    Migrate some eeconfig_init_kb to config

--- keyboards/keebio/stick/stick.c
+++ keyboards/keebio/stick/stick.c
@@ -17,23 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-void eeconfig_init_kb(void) {
-#ifdef RGBLIGHT_ENABLE
-    rgblight_enable();             // Enable RGB by default
-    rgblight_sethsv(0, 255, 255);  // Set default HSV - red hue, full saturation, full brightness
-#    ifdef RGBLIGHT_EFFECT_RAINDOWN_SWIRL
-    rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL + 2);  // set to RGB_RAINBOW_SWIRL by default
-#    endif
-#endif
-
-#ifdef RGB_MATRIX_ENABLE
-    rgb_matrix_enable();  // Enable RGB by default
-#endif
-
-    eeconfig_update_kb(0);
-    eeconfig_init_user();
-}
-
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
     if (index == 0) {
