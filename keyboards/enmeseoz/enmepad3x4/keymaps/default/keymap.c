4910dbd00b1b05694d780e39904007227f94cc17
bkchqaeh <bkchqaeh@gmail.com>
2025-06-30 12:37:19 +0300

    Requested changes

--- keyboards/enmeseoz/enmepad3x4/keymaps/default/keymap.c
+++ keyboards/enmeseoz/enmepad3x4/keymaps/default/keymap.c
@@ -19,16 +19,6 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     LAYOUT_3x4(
-        KC_0,  KC_1,    KC_4,    KC_7, 
-        KC_ENT,  KC_2,    KC_5,    KC_8, 
-        MO(1),  KC_3,    KC_6,    KC_9
-    ),
-    LAYOUT_3x4(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    LAYOUT_3x4(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
