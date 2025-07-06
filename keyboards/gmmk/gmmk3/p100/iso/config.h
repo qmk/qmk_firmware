983239aebe95e998044cc514df571202e9faa413
James Guillochon <guillochon@gmail.com>
2025-07-06 12:35:00 -0700

    cleanup

--- keyboards/gmmk/gmmk3/p100/iso/config.h
+++ keyboards/gmmk/gmmk3/p100/iso/config.h
@@ -16,25 +16,5 @@
 
 #pragma once
 
-/* External spi flash */
-#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C4
-
-#define USB_SUSPEND_WAKEUP_DELAY 1000
-
-/* SPI Config for LED Driver */
-#define SPI_DRIVER SPIDQ
-#define SPI_SCK_PIN A5
-#define SPI_SCK_PAL_MODE 5
-#define SPI_MOSI_PIN A7
-#define SPI_MOSI_PAL_MODE 5
-#define SPI_MISO_PIN A6
-#define SPI_MISO_PAL_MODE 5
-
-#define AW20216S_CS_PIN_1 C5
-#define AW20216S_CS_PIN_2 B11
-#define AW20216S_EN_PIN   C13
-
-#define DRIVER_1_LED_TOTAL (67)
-#define MATRIX_ROWS 17
-
-#define DIP_SWITCH_PINS { A13 }
+#define DRIVER_2_LED_TOTAL (57)  // ISO has one more LED than ANSI
+#define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
