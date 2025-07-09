1614ce7a925a84ae8cbd6a97884cb50b142f1713
togkey86 <togkey86@gmail.com>
2025-07-09 09:25:48 -0700

    Initial Source for Pad Pocket

---
+++ keyboards/togkey/pad_pocket/keymaps/default/keymap.c
@@ -0,0 +1,10 @@
+// Copyright 2025 TogKey
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP
+    )
+};
