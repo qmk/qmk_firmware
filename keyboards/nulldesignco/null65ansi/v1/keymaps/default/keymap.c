93d327b6f708c54e8c8edf2e25ce1845b084be7b
Brandon <brandon.abr@hotmail.com>
2025-07-10 06:06:44 -0600

    License, layout and RGB indicator review
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/nulldesignco/null65ansi/v1/keymaps/default/keymap.c
+++ keyboards/nulldesignco/null65ansi/v1/keymaps/default/keymap.c
@@ -1,10 +1,11 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+// Copyright 2025 quark-works
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT(
+    [0] = LAYOUT_65_ansi(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
@@ -13,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+bool rgb_matrix_indicators_user(void) {
     if (host_keyboard_led_state().caps_lock) {
-    	RGB_MATRIX_INDICATOR_SET_COLOR(24, 255, 0, 0);
+    	rgb_matrix_set_color(24, 255, 0, 0);
     }
     return false;
 }
