631b670a3b59f11bbadbac060a27a7656c057872
Benjamin Krammel <bk@internetx.de>
2025-07-10 15:42:01 +0200

    Refactor adelholtz keymap and update config for Iris CE
    - Add .clang-format for consistent code style - Refactor tap dance and
    magic key logic in keymap.c - Remove PERMISSIVE_HOLD and adjust tap
    terms in config.h - Add palettefx module to keyboard.json configuration

--- keyboards/keebio/iris_ce/keymaps/adelholtz/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/adelholtz/keymap.c
@@ -22,10 +22,10 @@ enum custom_keycodes {
 
 #define MACLCURLY ALGR(DE_8)
 #define MACRCURLY ALGR(DE_9)
-#define HRM_GUI_D MT(MOD_LGUI,DE_D)
-#define HRM_ALT_S MT(MOD_LALT | MOD_RALT,DE_S)
-#define HRM_GUI_H MT(MOD_LGUI,DE_H)
-#define HRM_CTRL_E MT(MOD_LCTL,DE_E)
+#define HRM_GUI_D MT(MOD_LGUI, DE_D)
+#define HRM_ALT_S MT(MOD_LALT | MOD_RALT, DE_S)
+#define HRM_GUI_H MT(MOD_LGUI, DE_H)
+#define HRM_CTRL_E MT(MOD_LCTL, DE_E)
 #define HRL_BRACKETS_T LT(_BRACKETS, DE_T)
 #define HRL_BRACKETS_N LT(_BRACKETS, DE_N)
 #define TRL_MEDIA_L LT(_MEDIA, DE_L)
@@ -33,54 +33,49 @@ enum custom_keycodes {
 
 // Tap Dance Hold configuration
 typedef struct {
-    uint16_t tap;
-    uint16_t hold;
-    uint16_t held;
+  uint16_t tap;
+  uint16_t hold;
+  uint16_t held;
 } tap_dance_tap_hold_t;
 
 void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+  tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
 
-    if (state->count == 1 && state->pressed
-        #ifndef PERMISSIVE_HOLD
-                    && !state->interrupted
-        #endif
-    )
-    {
-        register_code16(tap_hold->hold);
-        tap_hold->held = tap_hold->hold;
-    }else if (state->count == 2){
-   	register_code16(tap_hold->tap);
-	register_code16(tap_hold->tap);
-        tap_hold->held = tap_hold->tap;
-
-	}else {
-        register_code16(tap_hold->tap);
-        tap_hold->held = tap_hold->tap;
+  if (state->pressed) {
+    if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+        && !state->interrupted
+#endif
+    ) {
+      register_code16(tap_hold->hold);
+      tap_hold->held = tap_hold->hold;
     }
+  } else {
+    register_code16(tap_hold->tap);
+    tap_hold->held = tap_hold->tap;
+  }
 }
 
 void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+  tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
 
-    if (tap_hold->held) {
-        unregister_code16(tap_hold->held);
-        tap_hold->held = 0;
-    }
+  if (tap_hold->held) {
+    unregister_code16(tap_hold->held);
+    tap_hold->held = 0;
+  }
 }
 
-#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold)                                        \
-    {                                                                               \
-        .fn        = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, \
-        .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}),               \
-    }
-
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold)                               \
+  {                                                                        \
+      .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, \
+      .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}),        \
+  }
 
 enum {
-    SPACE_TAB,
-    TDH_U_UE,
-    TDH_O_OE,
-    TDH_A_AE,
+  SPACE_TAB,
+  TDH_U_UE,
+  TDH_O_OE,
+  TDH_A_AE,
 };
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
@@ -90,32 +85,33 @@ tap_dance_action_t tap_dance_actions[] = {
     [TDH_O_OE] = ACTION_TAP_DANCE_TAP_HOLD(DE_O, DE_ODIA),
     [TDH_A_AE] = ACTION_TAP_DANCE_TAP_HOLD(DE_A, DE_ADIA),
 };
-static void process_tap_dance_hold(uint16_t keycode, keyrecord_t* record) {
-    tap_dance_action_t *action;
+static void process_tap_dance_hold(uint16_t keycode, keyrecord_t *record) {
+  tap_dance_action_t *action;
 
-    action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
-    if (!record->event.pressed && action->state.count && !action->state.finished) {
-        tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
-        tap_code16(tap_hold->tap);
-    }
+  action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+  if (!record->event.pressed && action->state.count &&
+      !action->state.finished) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+    tap_code16(tap_hold->tap);
+  }
 }
 
 enum custom_layers {
-     _COLEMAK,
-     _GAME,
-     _NAVIGATION,
-     _BRACKETS,
-     _EXTRA,
-     _MEDIA,
+  _COLEMAK,
+  _GAME,
+  _NAVIGATION,
+  _BRACKETS,
+  _EXTRA,
+  _MEDIA,
 };
-
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
      QK_GESC, KC_1, KC_2,  KC_3,      KC_4,           KC_5,                        KC_6,      KC_7, KC_8,      KC_9,       KC_0,   KC_BSPC,
-     KC_TAB,  DE_Q, DE_W,  DE_F,      TRL_MEDIA_P,    DE_G,                        DE_J,      TRL_MEDIA_L,     TD(TDH_U_UE),       DE_Y,   XXXXXXX, XXXXXXX,
-     KC_LCTL, TD(TDH_A_AE), DE_R,  HRM_ALT_S, HRL_BRACKETS_T, HRM_GUI_D,                   HRM_GUI_H, HRL_BRACKETS_N,  HRM_CTRL_E, DE_I,   TD(TDH_O_OE),   XXXXXXX,
+     KC_TAB,  DE_Q, DE_W,  DE_F,      TRL_MEDIA_P,    DE_G,                        DE_J,      TRL_MEDIA_L,     DE_U,       DE_Y,   XXXXXXX, XXXXXXX,
+     KC_LCTL, DE_A, DE_R,  HRM_ALT_S, HRL_BRACKETS_T, HRM_GUI_D,                   HRM_GUI_H, HRL_BRACKETS_N,  HRM_CTRL_E, DE_I,   DE_O,   KC_ENTER,
      KC_LSFT, DE_Z, DE_X,  DE_C,      DE_V, DE_B,               KC_HOME, KC_END,   DE_K,      DE_M, DE_COMM,   KC_DOT, KC_SLSH, KC_CAPS,
-                            LMAGIC, MO(_NAVIGATION), TD(SPACE_TAB), OSM(MOD_LSFT), LT(_EXTRA,KC_ENTER), RMAGIC
+                            LMAGIC, MO(_NAVIGATION), TD(SPACE_TAB), OSM(MOD_LSFT), MO(_EXTRA), RMAGIC
   ),
   [_GAME] = LAYOUT(
      QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,                  KC_6, KC_7, KC_8, KC_9,   KC_0,    KC_BSPC,
@@ -162,140 +158,150 @@ const char PROGMEM chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] =
         'L', 'L', 'L', 'L', 'L', 'L','*','*','R', 'R', 'R', 'R', 'R', 'R',
                        '*', '*', '*',        '*', '*', '*'
     );
+// clang-format on
+
 const uint16_t PROGMEM activate_game_layer[] = {DE_Q, DE_W, DE_Y, COMBO_END};
-const uint16_t PROGMEM next_song[] = {DE_N, MT(MOD_LCTL,DE_E), COMBO_END};
+const uint16_t PROGMEM next_song[] = {DE_N, MT(MOD_LCTL, DE_E), COMBO_END};
 const uint16_t PROGMEM play_stop[] = {DE_M, DE_P, COMBO_END};
-combo_t key_combos[] = {
-    COMBO(activate_game_layer, TO(1)),
-    COMBO(next_song, KC_MEDIA_NEXT_TRACK),
-    COMBO(play_stop, KC_MEDIA_PLAY_PAUSE)
-};
+combo_t key_combos[] = {COMBO(activate_game_layer, TO(1)),
+                        COMBO(next_song, KC_MEDIA_NEXT_TRACK),
+                        COMBO(play_stop, KC_MEDIA_PLAY_PAUSE)};
 
-bool remember_last_key_user(uint16_t keycode, keyrecord_t* record,
-                            uint8_t* remembered_mods) {
-	switch (keycode) {
-		case CW_TOGG:
-        case KC_ESC:
-        case KC_BSPC:
-        case KC_DEL:
+bool remember_last_key_user(uint16_t keycode, keyrecord_t *record,
+                            uint8_t *remembered_mods) {
+  switch (keycode) {
+    case CW_TOGG:
+    case KC_ESC:
+    case KC_BSPC:
+    case KC_DEL:
 
-        case LMAGIC:
-        case RMAGIC:
-            return false;  // Magic keys will ignore the above keycodes.
-    }
-    return true;  // Other keys can be repeated.
+    case LMAGIC:
+    case RMAGIC:
+      return false;  // Magic keys will ignore the above keycodes.
+  }
+  return true;  // Other keys can be repeated.
 }
 
 // An enhanced version of SEND_STRING: the last key is set such that if
 // the Repeat Key is pressed next, it produces `repeat_keycode`.
 #define MAGIC_STRING(str, repeat_keycode) \
-		magic_send_string_P(PSTR(str), (repeat_keycode))
+  magic_send_string_P(PSTR(str), (repeat_keycode))
 
-static void magic_send_string_P(const char* str, uint16_t repeat_keycode) {
+static void magic_send_string_P(const char *str, uint16_t repeat_keycode) {
   send_string_with_delay_P(str, TAP_CODE_DELAY);  // Send the string.
-  set_last_keycode(repeat_keycode); // 2024-03-09 Disabled sending of string for mag-rep / rep-mag consistency.
-
+  set_last_keycode(repeat_keycode);  // 2024-03-09 Disabled sending of string
+                                     // for mag-rep / rep-mag consistency.
 }
 
+static void process_left_magic(uint16_t keycode,
+                               uint8_t mods) {  // LMAGIC definitions
+  switch (keycode) {
+    case TRL_MEDIA_L: {
+      MAGIC_STRING("l", KC_NO);
+    } break;
+    case KC_M: {
+      MAGIC_STRING("m", KC_NO);
+    } break;
+    case DE_O: {
+      MAGIC_STRING("o", KC_NO);
+    } break;
+    case DE_U: {
+      MAGIC_STRING("u", KC_NO);
+    } break;
 
-static void process_left_magic(uint16_t keycode, uint8_t mods) { // LMAGIC definitions
-    switch (keycode) {
-        case  TD(TDH_A_AE): { MAGIC_STRING("a", 		KC_NO); } break;
-    	case  KC_B: { MAGIC_STRING("ecause",	KC_SPC); } break;
-    	case  KC_C: { MAGIC_STRING("an",		KC_NO); } break;
-    	case  KC_E: { MAGIC_STRING("e",			KC_NO); } break;
-    	case  KC_G: { MAGIC_STRING("eneral", 	KC_NO); } break;
-    	case  KC_I: { MAGIC_STRING("e", 		KC_NO); } break;
-    	case  KC_J: { MAGIC_STRING("ust",		KC_NO); } break;
-    	case  KC_L: { MAGIC_STRING("l", 		KC_NO); } break;
-    	case  KC_M: { MAGIC_STRING("ent",		KC_NO); } break;
-    	case  TD(TDH_O_OE): { MAGIC_STRING("o", 		KC_NO); } break;
-    	case  KC_P: { MAGIC_STRING("retty",    	KC_NO); } break;
-    	case  KC_Q: { MAGIC_STRING("q", 		KC_NO); } break;
-        case  KC_T: { MAGIC_STRING("t", 	    KC_NO); } break;
-       	case  TD(TDH_U_UE): { MAGIC_STRING("u", 		KC_NO); } break;
-    	case  KC_W: { MAGIC_STRING("hich",		KC_NO); } break;
-    	case  KC_Y: { MAGIC_STRING("ou", 		KC_NO); } break;
-    	case  KC_Z: { MAGIC_STRING("z", 		KC_NO); } break;
-
-    	case KC_COMM: { MAGIC_STRING(" and",    KC_NO); } break;
-    }
+    case MACRCURLY: {
+      SEND_STRING(SS_RALT("8"));
+    } break;
+  }
 }
 
-static void process_right_magic(uint16_t keycode, uint8_t mods) { // RMAGIC definitions
-    switch (keycode) {
-       	case  TD(TDH_A_AE): { MAGIC_STRING("a", 		KC_NO); } break;
-    	case  KC_B: { MAGIC_STRING("b", 		KC_NO); } break;
-    	case  KC_C: { MAGIC_STRING("c", 		KC_NO); } break;
-    	case  KC_E: { MAGIC_STRING("e", 		KC_NO); } break;
-    	case  KC_F: { MAGIC_STRING("f", 		KC_NO); } break;
-    	case  KC_G: { MAGIC_STRING("g", 		KC_NO); } break;
-    	case  KC_I: { MAGIC_STRING("on",     	KC_NO); } break;
-    	case  KC_J: { MAGIC_STRING("j", 		KC_NO); } break;
-    	case  KC_K: { MAGIC_STRING("k", 		KC_NO); } break;
-    	case  KC_L: { MAGIC_STRING("l", 		KC_NO); } break;
-    	case  KC_M: { MAGIC_STRING("m", 		KC_NO); } break;
-    	case  KC_N: { MAGIC_STRING("n", 		KC_NO); } break;
-    	case  KC_P: { MAGIC_STRING("p", 		KC_NO); } break;
-    	case  KC_R: { MAGIC_STRING("r", 	    KC_NO); } break;
-    	case  KC_S: { MAGIC_STRING("s", 		KC_NO); } break;
-    	case  DE_T: { MAGIC_STRING("t", 		KC_NO); } break;
-       	case  DE_W: { MAGIC_STRING("w", 		KC_NO); } break;
-    	case  KC_Y: { MAGIC_STRING("eah",    	KC_NO); } break;
+static void process_right_magic(uint16_t keycode,
+                                uint8_t mods) {  // RMAGIC definitions
+  switch (keycode) {
+    case DE_A: {
+      MAGIC_STRING("a", KC_NO);
+    } break;
+    case KC_B: {
+      MAGIC_STRING("b", KC_NO);
+    } break;
+    case TRL_MEDIA_P: {
+      MAGIC_STRING("p", KC_NO);
+    } break;
+    case KC_R: {
+      MAGIC_STRING("r", KC_NO);
+    } break;
+    case HRM_ALT_S: {
+      MAGIC_STRING("s", KC_NO);
+    } break;
+    case HRL_BRACKETS_T: {
+      MAGIC_STRING("t", KC_NO);
+    } break;
+    case DE_W: {
+      MAGIC_STRING("w", KC_NO);
+    } break;
 
-    	case  KC_COMM: { MAGIC_STRING(" but",    KC_NO); } break;
-    	case  LT(_BRACKETS,KC_SPC): { MAGIC_STRING("the", 	KC_NO); } break;
-
-        case MACLCURLY: { SEND_STRING(SS_RALT("9")); } break;
-    }
+    case MACLCURLY: {
+      SEND_STRING(SS_RALT("9"));
+    } break;
+  }
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-    // case _NAVIGATION:
-    //     rgb_matrix_mode (RGB_MATRIX_SPLASH);
-    //     break;
-    // case _BRACKETS:
-    //     rgb_matrix_mode (RGB_MATRIX_BAND_SAT);
-    //     break;
-    // case _EXTRA:
-    //     rgb_matrix_mode (RGB_MATRIX_BREATHING);
-    //     break;
-    case _GAME:
-        rgb_matrix_mode (RGB_MATRIX_SOLID_COLOR);
-        break;
-    default: //  for any other layers, or the default layer
-        rgb_matrix_mode (RGB_MATRIX_SOLID_REACTIVE_WIDE);
-        //rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_PALETTEFX_FLOW);
-        break;
-    }
-  return state;
-}
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   switch (get_highest_layer(state)) {
+//     // case _NAVIGATION:
+//     //     rgb_matrix_mode (RGB_MATRIX_SPLASH);
+//     //     break;
+//     // case _BRACKETS:
+//     //     rgb_matrix_mode (RGB_MATRIX_BAND_SAT);
+//     //     break;
+//     // case _EXTRA:
+//     //     rgb_matrix_mode (RGB_MATRIX_BREATHING);
+//     //     break;
+//     case _GAME:
+//       rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+//       break;
+//     default:  //  for any other layers, or the default layer
+//       rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE_WIDE);
+//       // rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_PALETTEFX_FLOW);
+//       break;
+//   }
+//   return state;
+// }
 
 // custom keystroke main logic
-bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-  if (record->event.pressed){
-  	switch (keycode) {
-       	 	case LMAGIC: { process_left_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
-        	case RMAGIC: { process_right_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
-            case TD(TDH_A_AE):
-            case TD(TDH_O_OE):
-            case TD(TDH_U_UE): {process_tap_dance_hold(keycode, record);}
-	}
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case LMAGIC: {
+        process_left_magic(get_last_keycode(), get_last_mods());
+        set_last_keycode(KC_SPC);
+      }
+        return false;
+      case RMAGIC: {
+        process_right_magic(get_last_keycode(), get_last_mods());
+        set_last_keycode(KC_SPC);
+      }
+        return false;
+      case TD(TDH_A_AE):
+      case TD(TDH_O_OE):
+      case TD(TDH_U_UE): {
+        process_tap_dance_hold(keycode, record);
+      }
+    }
   }
 
   return true;
 }
 
-
 void keyboard_post_init_user(void) {
-     rgb_matrix_mode (RGB_MATRIX_SOLID_REACTIVE_WIDE);
+  rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE_WIDE);
+  rgb_matrix_sethsv(191, 255, 255);
 }
 // void keyboard_post_init_user(void) {
 //   // Set the effect.
 //   rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_PALETTEFX_REACTIVE);
 //   // Set the palette and maximize saturation and brightness.
-//   uint8_t palette_index = PALETTEFX_AFTERBURN;  // Set Carnival palette.
+//   uint8_t palette_index = PALETTEFX_AFTERBURN;
 //   rgb_matrix_sethsv_noeeprom(RGB_MATRIX_HUE_STEP * palette_index, 255, 255);
 //   // Set speed to default.
 //   rgb_matrix_set_speed_noeeprom(128);
