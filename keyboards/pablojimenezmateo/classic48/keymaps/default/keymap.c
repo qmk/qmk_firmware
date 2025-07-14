748ddb4f9f3babe6cc970b75da9bb899d29b4752
Pablo Jimenez Mateo <pablojimenezmateo@gmail.com>
2025-07-14 16:52:12 +0200

    fix: remove old layer reference

--- keyboards/pablojimenezmateo/classic48/keymaps/default/keymap.c
+++ keyboards/pablojimenezmateo/classic48/keymaps/default/keymap.c
@@ -88,10 +88,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V), KC_NO,   KC_NO,   KC_NO,             MS_WHLD,       MS_WHLU,       KC_NO,         KC_NO,         KC_NO,  KC_BRID
     ),
     [_GAMING] = LAYOUT(
-        KC_TAB,       KC_Q,  KC_W,  KC_E,          KC_R,   KC_T,                   KC_Y,  KC_U, KC_I,     KC_O,   KC_P,    ES_ACUT,
-        KC_LEFT_CTRL, KC_A,  KC_S,  KC_D,          KC_F,   KC_G,                   KC_H,  KC_J, KC_K,     KC_L,   ES_NTIL, KC_NO,
-        KC_ESCAPE,    KC_Z,  KC_X,  KC_C,          KC_V,   KC_B,                   KC_N,  KC_M, KC_COMMA, KC_DOT, ES_SLSH, KC_NO,
-        TG(_GAMING),  KC_NO, KC_NO, KC_LEFT_SHIFT, KC_SPC, MO(_GAMING_NUM),        KC_NO, KC_1, KC_2,     KC_3,   KC_4,    KC_5
+        KC_TAB,       KC_Q,  KC_W,  KC_E,          KC_R,   KC_T,         KC_Y,  KC_U, KC_I,     KC_O,   KC_P,    ES_ACUT,
+        KC_LEFT_CTRL, KC_A,  KC_S,  KC_D,          KC_F,   KC_G,         KC_H,  KC_J, KC_K,     KC_L,   ES_NTIL, KC_NO,
+        KC_ESCAPE,    KC_Z,  KC_X,  KC_C,          KC_V,   KC_B,         KC_N,  KC_M, KC_COMMA, KC_DOT, ES_SLSH, KC_NO,
+        TG(_GAMING),  KC_NO, KC_NO, KC_LEFT_SHIFT, KC_SPC, KC_NO,        KC_NO, KC_1, KC_2,     KC_3,   KC_4,    KC_5
     )
 };
 
