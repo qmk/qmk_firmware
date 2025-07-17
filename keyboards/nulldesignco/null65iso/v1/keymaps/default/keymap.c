ebf9b64c16cb236a40f81bc23a18a99962168e19
Brandon <brandon.abr@hotmail.com>
2025-07-17 10:25:35 -0600

    Removed unnecessary comma
    Co-authored-by: Jack Sangdahl <jack@pngu.org>

--- keyboards/nulldesignco/null65iso/v1/keymaps/default/keymap.c
+++ keyboards/nulldesignco/null65iso/v1/keymaps/default/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGUP,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, KC_APP,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
-    ),
+    )
 };
 
 bool rgb_matrix_indicators_user(void) {
