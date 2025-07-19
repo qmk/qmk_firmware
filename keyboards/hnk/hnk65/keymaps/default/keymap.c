54867cf5805e6ed4b970e0fed3980247ca22a3c9
Alexis St-Pierre Cadieux <alexis@Alexiss-Mac-mini.local>
2025-07-19 13:35:40 -0400

    fixing the layout macro and keymap

--- keyboards/hnk/hnk65/keymaps/default/keymap.c
+++ keyboards/hnk/hnk65/keymaps/default/keymap.c
@@ -21,11 +21,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [0] = LAYOUT_65_ansi(
-        KC_HOME,  KC_BSPC,    KC_EQL,    KC_MINS,    KC_0,    KC_9,    KC_8,    KC_7,    KC_6,    KC_5,    KC_4,    KC_3, KC_2,  KC_1, KC_ESC,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        KC_PGDN, KC_ENT,    KC_QUOT,    KC_SCLN,  KC_L,  KC_K,    KC_J,    KC_H,    KC_G,    KC_F,    KC_D,    KC_S, KC_A,            KC_CAPS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END,
-        KC_RGHT, KC_DOWN, KC_LEFT,KC_RCTL, MO(1), KC_RALT,           KC_SPC,                                   KC_LALT,  KC_LGUI,     KC_LCTL           
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                              KC_RALT, MO(1),   KC_RCTL, KC_LEFT,KC_DOWN, KC_RGHT
     ),
 
 
@@ -44,12 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
 
-
     [1] = LAYOUT_65_ansi(
-        KC_DEL,  KC_INS,   KC_F12,  KC_F11,  KC_F10,  KC_F9,   KC_F8,   KC_F7,   KC_F6,  KC_F5,   KC_F4,  KC_F3, KC_F2,   KC_F1,   KC_GRV,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,   KC_INS,   KC_DEL,
         _______, _______, KC_VOLU, _______, _______, _______, _______, KC_SCRL, _______, KC_7,    KC_8,    KC_9,   KC_BSLS,  RM_SATU,  RM_HUEU,
-        RM_HUED, RM_SATD, KC_6, KC_5, KC_4, _______, _______, _______, _______, _______, KC_PAUS,    KC_VOLD,             KC_MUTE,  _______,
+        _______, KC_MUTE, KC_VOLD, KC_PAUS, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,             RM_SATD,  RM_HUED,
         _______, KC_PIPE, _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,             RM_VALU,  RM_TOGG,
-        RM_NEXT, RM_VALD, RM_PREV, RM_SPDU,  _______, RM_SPDD,          KC_PSCR,                _______, _______,  _______
+        _______, _______, _______,                            KC_PSCR,                   RM_SPDD, _______, RM_SPDU, RM_PREV, RM_VALD,  RM_NEXT
     )
 };
