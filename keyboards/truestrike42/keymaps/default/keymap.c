bcf141d5154bfb6573f52b965e3ac7377c0bb94e
Choi Byungyoon <byungyoonc@gmail.com>
2025-07-20 14:38:23 +0900

    Update default keymap to use tri layer

--- keyboards/truestrike42/keymaps/default/keymap.c
+++ keyboards/truestrike42/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LALT, MO(1),   KC_LSFT,          KC_SPC,  MO(2),  KC_RALT
+                                            KC_LALT, TL_LOWR, KC_LSFT,          KC_SPC,  TL_UPPR, KC_RALT
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_CAPS,                            KC_NUM,  KC_7,    KC_8,    KC_9,    KC_PMNS, KC_DEL,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,                            _______, KC_4,    KC_5,    KC_6,    KC_PPLS, _______,
         _______, KC_ESC,  _______, _______, _______, KC_LGUI,                            _______, KC_1,    KC_2,    KC_3,    KC_PDOT, _______,
-                                            _______, _______, _______,          _______, MO(3),   _______
+                                            _______, _______, _______,          _______, _______, _______
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_PERC, _______,                            _______, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,  KC_DEL,
         _______, KC_AMPR, KC_ASTR, KC_DLR,  _______, _______,                            _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______,
         _______, _______, KC_CIRC, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-                                            _______, MO(3),   _______,          _______, _______, _______
+                                            _______, _______, _______,          _______, _______, _______
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
