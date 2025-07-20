9d64de92fa8efde187dd2bb4f3bc124e6bc5f44f
Choi Byungyoon <byungyoonc@gmail.com>
2025-07-20 14:26:39 +0900

    Do not add via_he.c to SRC unless VIA_ENABLE is specified

--- keyboards/truestrike42/rules.mk
+++ keyboards/truestrike42/rules.mk
@@ -1,5 +1,9 @@
-SRC += matrix.c he_switch_matrix.c via_he.c graphics/display.c graphics/quinquefive.qff.c
+SRC += matrix.c he_switch_matrix.c graphics/display.c graphics/quinquefive.qff.c
 CUSTOM_MATRIX = lite
 ANALOG_DRIVER_REQUIRED = yes
 QUANTUM_PAINTER_ENABLE = yes
 QUANTUM_PAINTER_DRIVERS += sh1106_i2c
+
+ifeq ($(strip $(VIA_ENABLE)), yes)
+    SRC += via_he.c
+endif
