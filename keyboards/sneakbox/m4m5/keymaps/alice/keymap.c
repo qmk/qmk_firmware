f2d5bfe41e18ba853f86e9846df9f5f4affb64b8
SneakboxKB <67670271+SneakboxKB@users.noreply.github.com>
2025-07-20 23:45:27 -0500

    Added boot keycode to Layer 1

--- keyboards/sneakbox/m4m5/keymaps/alice/keymap.c
+++ keyboards/sneakbox/m4m5/keymaps/alice/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT_alice(
         KC_TRNS,   KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,       KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,    KC_TRNS,    
         KC_TRNS,   KC_TRNS,   KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   UG_PREV,   UG_NEXT,    UG_TOGG,
-        KC_TRNS,   KC_TRNS,   KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    UG_VALD,   UG_VALU,    KC_TRNS,
+        QK_BOOT,   KC_TRNS,   KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    UG_VALD,   UG_VALU,    KC_TRNS,
                    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    UG_SATD,   UG_SATU,    KC_TRNS,    UG_HUEU,
                    KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS,                                    KC_TRNS),
 };
