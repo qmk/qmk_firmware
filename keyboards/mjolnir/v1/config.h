15298108ee8812419d0675629dfc37cf54d2ce4d
MartinL <martin.liivak@gmail.com>
2025-07-27 09:52:10 +0300

    Add split timeout

--- keyboards/mjolnir/v1/config.h
+++ keyboards/mjolnir/v1/config.h
@@ -4,3 +4,7 @@
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN GP0    // USART TX pin
 #define SERIAL_USART_RX_PIN GP1    // USART RX pin
+
+// possibly fix KB  not working at boot time
+#define SPLIT_USB_TIMEOUT 10000
+#define SPLIT_USB_TIMEOUT_POLL 10
