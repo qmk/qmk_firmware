69149b14c7abc55f4c7a2389688adb51e9045be7
Mike Hamrah <m@hamrah.com>
2025-07-30 18:24:05 -0400

    fix wpm

--- keyboards/sofle_choc/keymaps/mhamrah/keymap.c
+++ keyboards/sofle_choc/keymaps/mhamrah/keymap.c
@@ -66,7 +66,7 @@ void cycle_custom_rgb_mode(bool forward) {
     } else {
         current_custom_mode_index = (current_custom_mode_index + CUSTOM_RGB_MODES_COUNT - 1) % CUSTOM_RGB_MODES_COUNT;
     }
-    
+
     if (custom_rgb_modes[current_custom_mode_index] == 0) {
         rgb_matrix_disable();
     } else {
@@ -106,30 +106,30 @@ void oled_write_bitmap(const uint8_t *bitmap, uint8_t start_col, uint8_t start_r
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_MAC_DEFAULT] = 
-        LAYOUT( 
-    KC_GRV,     KC_1,   KC_2, KC_3, KC_4,           KC_5,                   KC_6,   KC_7,           KC_8, KC_9, KC_0,       KC_MINUS, 
-    KC_TAB,     KC_Q,   KC_W, KC_E, KC_R,           KC_T,                   KC_Y,   KC_U,           KC_I, KC_O, KC_P,       KC_EQUAL, 
-    KC_RCTL,    KC_A,   KC_S, KC_D, KC_F,   KC_G,                   KC_H,   KC_J,   KC_K, KC_L, KC_SCLN,    KC_QUOT, 
-    KC_LSFT,    KC_B,   KC_Z, KC_X, KC_C,   KC_V,  KC_MUTE,     KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-                RGB_CYCLE, LSA_T(KC_TAB), LCA_T(KC_ESC), MO(1), LCMD_T(KC_SPC),     KC_SPC, KC_ENT, SCMD_T(KC_TAB), MO(2), MOD_MEH  
+    [_MAC_DEFAULT] =
+        LAYOUT(
+    KC_GRV,     KC_1,   KC_2, KC_3, KC_4,           KC_5,                   KC_6,   KC_7,           KC_8, KC_9, KC_0,       KC_MINUS,
+    KC_TAB,     KC_Q,   KC_W, KC_E, KC_R,           KC_T,                   KC_Y,   KC_U,           KC_I, KC_O, KC_P,       KC_EQUAL,
+    KC_RCTL,    KC_A,   KC_S, KC_D, KC_F,   KC_G,                   KC_H,   KC_J,   KC_K, KC_L, KC_SCLN,    KC_QUOT,
+    KC_LSFT,    KC_B,   KC_Z, KC_X, KC_C,   KC_V,  KC_MUTE,     KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                RGB_CYCLE, LSA_T(KC_TAB), LCA_T(KC_ESC), MO(1), LCMD_T(KC_SPC),     KC_SPC, KC_ENT, SCMD_T(KC_TAB), MO(2), MOD_MEH
             ),
-            
-    [_MAC_SYMBOLS] = 
-        LAYOUT( 
-    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS,                   KC_TRNS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_PAGE_DOWN, KC_PAGE_UP, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSLS, 
-    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+
+    [_MAC_SYMBOLS] =
+        LAYOUT(
+    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS,                   KC_TRNS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_PAGE_DOWN, KC_PAGE_UP, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_BSPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
             ),
-                                                              
-    [_MAC_UTIL] = 
+
+    [_MAC_UTIL] =
         LAYOUT(
-    RGB_TOG,    KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  QK_BOOT, 
-    RGB_CYCLE,  RGB_HUI, RGB_SAI,   RGB_VAI, RGB_SPI, KC_F11,                       KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR, 
+    RGB_TOG,    KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  QK_BOOT,
+    RGB_CYCLE,  RGB_HUI, RGB_SAI,   RGB_VAI, RGB_SPI, KC_F11,                       KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR,
     RGB_CYCLE_REV, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG,        KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG,        KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
             )
 
@@ -170,10 +170,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // OLED
 #ifdef OLED_ENABLE
- 
+
 // Rotate OLED
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_270; 
+    return OLED_ROTATION_270;
 }
 
 // Draw to OLED
@@ -181,8 +181,10 @@ bool oled_task_user() {
     if (is_keyboard_master()) {
         // Master side - WPM and layer info
         oled_set_cursor(0, 0);
-        sprintf(rgb_str, "WPM:%03d", get_current_wpm());
+        sprintf(rgb_str, "WC:%03d", get_current_wpm());
         oled_write(rgb_str, false);
+
+        oled_set_cursor(0, 1);
 
         // Draw layer bitmap above layer name
         uint8_t current_layer = get_highest_layer(layer_state);
@@ -215,8 +217,8 @@ bool oled_task_user() {
     } else {
         // Slave side - uptime and activity
         oled_set_cursor(0, 0);
-        oled_write_P(PSTR("UPTIME"), false);
-        
+        oled_write_P(PSTR("UP"), false);
+
         oled_set_cursor(0, 1);
         // Show timer in seconds (simple uptime counter)
         static uint32_t start_time = 0;
@@ -224,10 +226,10 @@ bool oled_task_user() {
         uint32_t uptime_sec = (timer_read32() - start_time) / 1000;
         sprintf(rgb_str, "%02d:%02d", (int)(uptime_sec / 60), (int)(uptime_sec % 60));
         oled_write(rgb_str, false);
-        
+
         oled_set_cursor(0, 2);
-        oled_write_P(PSTR("KEYLOG"), false);
-        
+        oled_write_P(PSTR("KEYS"), false);
+
         oled_set_cursor(0, 3);
         // Show total key press count
         sprintf(rgb_str, "%04d", key_count);
