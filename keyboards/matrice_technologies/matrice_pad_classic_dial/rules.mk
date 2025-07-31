a739d8e4a64ece7b62ddde34ba0ebaf3c8507c7a
Griffin Puc <griffinpuc@outlook.com>
2025-07-31 14:05:31 -0400

    remove dial artifcats

--- keyboards/matrice_technologies/matrice_pad_classic_dial/rules.mk
+++
@@ -1,7 +0,0 @@
-# keyboards/matrice_technologies/matrice_pad_classic_dial/rules.mk
-BOOTLOADER = caterina
-ENCODER_ENABLE = yes
-CONSOLE_ENABLE = yes
-ENCODER_BUTTONS_ENABLE  = yes
-EXTRAKEY_ENABLE = yes
-DIRECT_PIN_ENABLE = yes
