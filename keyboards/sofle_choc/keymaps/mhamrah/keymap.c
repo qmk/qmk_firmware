ffb5208488fee74d633b1c44ddc76cd7eb5181f5
Mike Hamrah <m@hamrah.com>
2025-07-31 10:53:17 -0400

    better animation control

--- keyboards/sofle_choc/keymaps/mhamrah/keymap.c
+++ keyboards/sofle_choc/keymaps/mhamrah/keymap.c
@@ -15,9 +15,26 @@
  */
 #include QMK_KEYBOARD_H
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "rgb_matrix_user.h"
 
 char rgb_str[10];
+
+// Get RGB mode name for enabled animations only
+const char* get_rgb_mode_name(uint8_t mode) {
+    switch(mode) {
+        case 1: return "Static";
+        case 2: return "AlphaMods";
+        case 14: return "CycleUpDn";
+        case 15: return "CycleOut";
+        case 18: return "Pinwheel";
+        case 20: return "DualBeam";
+        case 22: return "RainbowPin";
+        case 25: return "HueBreathe";
+        default: return "Unknown";
+    }
+}
 
 // Custom 8x8 layer bitmaps (each array is 8 bytes for 8x8 pixels)
 static const uint8_t PROGMEM layer_main_bitmap[] = {
@@ -38,57 +55,7 @@ enum layer_names {
     _MAC_UTIL
 };
 
-enum custom_keycodes {
-    RGB_CYCLE = SAFE_RANGE,
-    RGB_CYCLE_REV
-};
 
-// Custom RGB modes array
-const uint8_t custom_rgb_modes[] = {
-    0,   // Off
-    2,   // ALPHAS_MODS
-    14,  // CYCLE_UP_DOWN
-    15,  // CYCLE_OUT_IN
-    18,  // CYCLE_PINWHEEL
-    20,  // DUAL_BEACON
-    22,  // RAINBOW_PINWHEELS
-    25   // HUE_BREATHING
-};
-
-#define CUSTOM_RGB_MODES_COUNT (sizeof(custom_rgb_modes) / sizeof(custom_rgb_modes[0]))
-
-uint8_t current_custom_mode_index = 6; // Start with RAINBOW_PINWHEELS (index 6)
-
-// Custom RGB mode cycling function
-void cycle_custom_rgb_mode(bool forward) {
-    if (forward) {
-        current_custom_mode_index = (current_custom_mode_index + 1) % CUSTOM_RGB_MODES_COUNT;
-    } else {
-        current_custom_mode_index = (current_custom_mode_index + CUSTOM_RGB_MODES_COUNT - 1) % CUSTOM_RGB_MODES_COUNT;
-    }
-
-    if (custom_rgb_modes[current_custom_mode_index] == 0) {
-        rgb_matrix_disable();
-    } else {
-        rgb_matrix_enable();
-        rgb_matrix_mode(custom_rgb_modes[current_custom_mode_index]);
-    }
-}
-
-// Get RGB mode name
-const char* get_rgb_mode_name(uint8_t mode) {
-    switch(mode) {
-        case 0: return "Off";
-        case 2: return "AlphaMods";
-        case 14: return "CycleUpDn";
-        case 15: return "CycleOut";
-        case 18: return "Pinwheel";
-        case 20: return "DualBeam";
-        case 22: return "RainbowPin";
-        case 25: return "HueBreathe";
-        default: return "Unknown";
-    }
-}
 
 // Function to draw 8x8 bitmap by setting individual pixels
 void oled_write_bitmap(const uint8_t *bitmap, uint8_t start_col, uint8_t start_row, bool invert) {
@@ -112,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,   KC_W, KC_E, KC_R,           KC_T,                   KC_Y,   KC_U,           KC_I, KC_O, KC_P,       KC_EQUAL,
     KC_RCTL,    KC_A,   KC_S, KC_D, KC_F,   KC_G,                   KC_H,   KC_J,   KC_K, KC_L, KC_SCLN,    KC_QUOT,
     KC_LSFT,    KC_B,   KC_Z, KC_X, KC_C,   KC_V,  KC_MUTE,     KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                RGB_CYCLE, LSA_T(KC_TAB), LCA_T(KC_ESC), MO(1), LCMD_T(KC_SPC),     KC_SPC, KC_ENT, SCMD_T(KC_TAB), MO(2), MOD_MEH
+                RGB_MOD, LSA_T(KC_TAB), LCA_T(KC_ESC), MO(1), LCMD_T(KC_SPC),     KC_SPC, KC_ENT, SCMD_T(KC_TAB), MO(2), MOD_MEH
             ),
 
     [_MAC_SYMBOLS] =
@@ -127,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC_UTIL] =
         LAYOUT(
     RGB_TOG,    KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  QK_BOOT,
-    RGB_CYCLE,  RGB_HUI, RGB_SAI,   RGB_VAI, RGB_SPI, KC_F11,                       KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR,
-    RGB_CYCLE_REV, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    RGB_MOD,  RGB_HUI, RGB_SAI,   RGB_VAI, RGB_SPI, KC_F11,                       KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR,
+    RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG,        KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
             )
@@ -139,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PAGE_DOWN, KC_PAGE_UP)},
     {ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    {ENCODER_CCW_CW(RGB_CYCLE_REV, RGB_CYCLE), ENCODER_CCW_CW(RGB_HUD, RGB_HUI)},
+    {ENCODER_CCW_CW(RGB_RMOD, RGB_MOD), ENCODER_CCW_CW(RGB_HUD, RGB_HUI)},
 };
 #endif
 
@@ -148,24 +115,14 @@ static uint16_t key_count = 0;
 
 // Handle custom keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case RGB_CYCLE:
-            if (record->event.pressed) {
-                cycle_custom_rgb_mode(true);
-            }
-            return false;  // Don't count RGB keys for WPM or key counter
-        case RGB_CYCLE_REV:
-            if (record->event.pressed) {
-                cycle_custom_rgb_mode(false);
-            }
-            return false;  // Don't count RGB keys for WPM or key counter
-        default:
-            // Increment key counter and allow WPM tracking for regular keys
-            if (record->event.pressed) {
-                key_count++;
-            }
-            return true;  // This allows QMK's built-in WPM system to process the keypress
+    // Increment key counter for all keys except RGB control keys
+    if (record->event.pressed) {
+        // Don't count RGB control keys for key counter
+        if (!(keycode >= RGB_TOG && keycode <= RGB_MODE_TWINKLE)) {
+            key_count++;
+        }
     }
+    return true;  // Allow QMK to process all keycodes normally
 }
 
 // OLED
