f44e2e73aa5157b6d110ed446a949b9b2f904539
Aahil Irfan <aahilirfan2011@gmail.com>
2025-08-01 02:21:27 +0500

    Create CYBR-X.c

---
+++ keyboards/aahil/CYBR-X/CYBR-X.c
@@ -0,0 +1,26 @@
+#include "CYBR-X.h"
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    switch(index) {
+        case 0:
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
+            break;
+        case 1:
+            if (clockwise) {
+                tap_code(KC_MEDIA_NEXT_TRACK);
+            } else {
+                tap_code(KC_MEDIA_PREV_TRACK);
+            }
+            break;
+    }
+    return true;
+}
+
+bool oled_task_user(void) {
+    oled_write_P(PSTR("CYBR-X: Enhanced Edition\n"), false);
+    return false;
+}
