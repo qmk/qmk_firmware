7a92d3f3384bb22e2ed701ba4ad1342e09723b6c
Milos Lalic <losmi66@hotmail.rs>
2025-08-01 19:56:31 +0200

    update v3

--- keyboards/ziz0gaming19/c13x/keymaps/default/keymap.c
+++ keyboards/ziz0gaming19/c13x/keymaps/default/keymap.c
@@ -26,28 +26,28 @@ enum layers {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_10x4(
-        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,
-        KC_Z, KC_X, KC_C, KC_V, KC_B, TO(1), KC_N, KC_M, KC_SLSH, KC_TAB,
-        KC_LCTL, KC_LALT, KC_SPC, MO(_FN), KC_LGUI, KC_RSFT
+    [_BASE] = LAYOUT(
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,             KC_L,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TO(1),   KC_N,    KC_M,    KC_SLSH, KC_TAB,
+        KC_LCTL,          KC_LALT, KC_SPC,                    MO(_FN), KC_LGUI,          KC_RSFT
     ),
-    [_LAYER2] = LAYOUT_10x4(
-        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(2), KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_LCTL, KC_LALT, KC_SPC, MO(_FN), KC_LGUI, KC_RSFT
+    [_LAYER2] = LAYOUT(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,            KC_F9,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TO(2),   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LCTL,          KC_LALT, KC_SPC,                    MO(_FN), KC_LGUI,          KC_RSFT
     ),
-    [_LAYER3] = LAYOUT_10x4(
-        KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_INS, KC_HOME, KC_PGUP, KC_NO, QK_RBT,
-        KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_DEL, KC_END, KC_PGDN, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_LCTL, KC_LALT, KC_SPC, MO(_FN), KC_LGUI, KC_RSFT
+    [_LAYER3] = LAYOUT(
+        XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  KC_HOME, KC_PGUP, XXXXXXX, QK_RBT,
+        KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_DEL,  KC_END,  KC_PGDN,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TO(0),   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LCTL,          KC_LALT, KC_SPC,                    MO(_FN), KC_LGUI,          KC_RSFT
     ),
-    [_FN] = LAYOUT_10x4(
-        KC_ESC, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_PSCR, KC_NO, KC_MINS, KC_EQL, KC_BSPC,
-        KC_TAB, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO, KC_NO, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_CAPS, KC_LBRC, KC_RBRC, KC_NUBS, KC_GRV, KC_NO, KC_COMM, KC_DOT, KC_SLSH, KC_TAB,
-        KC_LCTL, KC_LALT, KC_SPC, KC_TRNS, KC_LGUI, KC_RSFT
+    [_FN] = LAYOUT(
+        KC_ESC,  KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, KC_PSCR, XXXXXXX, KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,  KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX, KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_CAPS, KC_LBRC, KC_RBRC, KC_NUBS, KC_GRV,  XXXXXXX, KC_COMM, KC_DOT,  KC_SLSH, KC_TAB,
+        KC_LCTL,          KC_LALT, KC_SPC,                    _______, KC_LGUI,          KC_RSFT
     )
 };
--- keyboards/ziz0gaming19/c13x/readme.md
+++ keyboards/ziz0gaming19/c13x/readme.md
@@ -1,12 +1,27 @@
-# C13X
+# c13x
 
-![Nayeon](https://imgur.com/a/LhhLknI)
+![c13x](https://imgur.com/a/LhhLknI)
 
-The C-13X is a cute 35% QAZ/Alice-like keyboard, designed by flurpyflurples, but updated by ziz0gaming19 to use ProMicro controller instead.
+*The C-13X is a cute 35% QAZ/Alice-like keyboard, designed by flurpyflurples, but updated by ziz0gaming19 to use ProMicro controller instead.*
 
-* Keyboard Maintainer: [flurpyflurples](https://github.com/flurples/)
+* Keyboard Maintainer: [milos](https://github.com/miloslalic19)
 * Hardware Supported: ProMicro
-    
-To get to the bootloader, simply press the reset button located at the back of the PCB. You can then flash new firmware onto it.
+* Hardware Availability: *[Aliexpress](https://www.aliexpress.com/item/1005006654641959.html)*
+
+Make example for this keyboard (after setting up your build environment):
+
+    make c13x:default
+
+Flashing example for this keyboard:
+
+    make c13x:default:flash
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+## Bootloader
+
+Enter the bootloader in 3 ways:
+
+* **Bootmagic reset**: Hold down the key at (0,0) in the matrix (usually the top left key or Escape) and plug in the keyboard
+* **Physical reset button**: Briefly press the button on the back of the PCB - some may have pads you must short instead
+* **Keycode in layout**: Press the key mapped to `QK_BOOT` if it is available
