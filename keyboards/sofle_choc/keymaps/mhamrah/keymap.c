9daa13554b9ad59cc7d460b66f27c9814cfef65a
Mike Hamrah <m@hamrah.com>
2025-08-01 17:13:04 -0400

    switch control and shift

--- keyboards/sofle_choc/keymaps/mhamrah/keymap.c
+++ keyboards/sofle_choc/keymaps/mhamrah/keymap.c
@@ -159,9 +159,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LAYOUT(
     KC_GRV,     KC_1,   KC_2, KC_3, KC_4,           KC_5,                   KC_6,   KC_7,           KC_8, KC_9, KC_0,       KC_MINUS,
     KC_TAB,     KC_Q,   KC_W, KC_E, KC_R,           KC_T,                   KC_Y,   KC_U,           KC_I, KC_O, KC_P,       KC_EQUAL,
-    KC_RCTL,    KC_A,   KC_S, KC_D, KC_F,   KC_G,                   KC_H,   KC_J,   KC_K, KC_L, KC_SCLN,    KC_QUOT,
-    KC_LSFT,    KC_B,   KC_Z, KC_X, KC_C,   KC_V,  KC_MUTE,     KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                RGB_MOD, LSA_T(KC_TAB), LCA_T(KC_ESC), MO(1), MT(MOD_LGUI | MOD_LSFT, KC_SPC),     KC_SPC, KC_ENT, SCMD_T(KC_TAB), MO(2), SCMD(KC_SPC)
+    KC_LSFT,    KC_A,   KC_S, KC_D, KC_F,   KC_G,                   KC_H,   KC_J,   KC_K, KC_L, KC_SCLN,    KC_QUOT,
+    KC_RCTL,    KC_B,   KC_Z, KC_X, KC_C,   KC_V,  KC_MUTE,     KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                RGB_MOD, LSA_T(KC_TAB), LCA_T(KC_ESC), MO(1), LCMD_T(KC_SPC),     KC_SPC, KC_ENT, SCMD_T(KC_TAB), MO(2), HOMEROW
             ),
 
     [_MAC_SYMBOLS] =
