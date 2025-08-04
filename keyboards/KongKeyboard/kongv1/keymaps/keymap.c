c312d24090b01f9d02325a1d4b0d3886a5be74c1
Darwin <darwingeo05@gmail.com>
2025-08-04 12:39:59 -0500

    rectificado

--- keyboards/KongKeyboard/kongv1/keymaps/keymap.c
+++ keyboards/KongKeyboard/kongv1/keymaps/keymap.c
@@ -1,15 +1,11 @@
-
 #include QMK_KEYBOARD_H
-
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_5x5(
-        KC_1, KC_2, KC_3, KC_4, KC_5,
-        KC_6, KC_7, KC_8, KC_9, KC_0,
-        KC_Q, KC_W, KC_E, KC_R, KC_T,
-        KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  
-    )
+[0] = LAYOUT_ortho_5x5(
+KC_1, KC_2, KC_3, KC_4, KC_5,
+KC_6, KC_7, KC_8, KC_9, KC_0,
+KC_Q, KC_W, KC_E, KC_R, KC_T,
+KC_A, KC_S, KC_D, KC_F, KC_G,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  
+)
 };
 
