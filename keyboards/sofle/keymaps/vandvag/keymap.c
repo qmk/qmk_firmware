99af02a9b0a75f8eb146300b19f95307751fc938
vandvag <vandvag@gmail.com>
2025-08-07 10:28:38 +0300

    [SOFLE]: Minor changes - Disabled rotary encoder button - Changed position of ctrl and alt buttons

--- keyboards/sofle/keymaps/vandvag/keymap.c
+++ keyboards/sofle/keymaps/vandvag/keymap.c
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
 KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
 KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-KC_LCTL,  GUI_A, ALT_S, SFT_D, CTL_F, KC_G,                            KC_H,    CTL_J,   SFT_K,   ALT_L,   GUI_SCLN,  KC_QUOT,
-KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTL, TL_LOWR, KC_ENT,      KC_SPC,  TL_UPPR, KC_RCTL, KC_RALT, KC_RGUI
+KC_LCTL,  GUI_A, ALT_S, SFT_D, CTL_F, KC_G,                            KC_H,    CTL_J,   SFT_K,   ALT_L,    GUI_SCLN,  KC_QUOT,
+KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                 KC_LGUI,KC_LCTL,KC_LALT, TL_LOWR, KC_ENT,      KC_SPC,  TL_UPPR, KC_RALT, KC_RCTL, KC_RGUI
 ),
 /*
  * COLEMAK
