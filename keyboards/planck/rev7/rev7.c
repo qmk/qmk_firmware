055ece854b6207e0d47c46f7ee4eabd427bc0b13
Jack Sangdahl <jack@pngu.org>
2025-08-11 16:40:24 +0200

    Migrate keyboards P

--- keyboards/planck/rev7/rev7.c
+++ keyboards/planck/rev7/rev7.c
@@ -15,34 +15,6 @@
  */
 #include "quantum.h"
 
-#ifdef RGB_MATRIX_ENABLE
-// clang-format off
-led_config_t g_led_config = { {
-  // Key Matrix to LED Index
-  { NO_LED, 6,      NO_LED, NO_LED, 5,      NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, 0      },
-  { NO_LED, 7,      NO_LED, NO_LED, 2,      NO_LED },
-  { NO_LED, 4,      NO_LED, NO_LED, 3,      NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, 1,      NO_LED, NO_LED, 8,      NO_LED },
-}, {
-  // LED Index to Physical Position
-  {112, 39}, {148, 60}, {206, 53}, {206, 3}, {150, 3}, {74, 3}, {18, 3}, {18, 54}, {77, 60}
-}, {
-  // LED Index to Flag
-  LED_FLAG_ALL, LED_FLAG_ALL, LED_FLAG_ALL, LED_FLAG_ALL, LED_FLAG_ALL,
-  LED_FLAG_ALL, LED_FLAG_ALL, LED_FLAG_ALL, LED_FLAG_ALL
-} };
-
-// LED physical location index
-//     6 5 4 3
-//        0
-//     7 8 1 2
-
-#endif
-
 #ifdef SWAP_HANDS_ENABLE
 __attribute__ ((weak))
 const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,7 +22,7 @@ const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
     {{5, 5}, {4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}},
     {{5, 6}, {4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6}},
     {{5, 3}, {4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}},
-    
+
     {{5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
     {{5, 1}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}},
     {{5, 2}, {4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}},
