9cf07d0a4c8bef924cdc25955dbb29d6489a8951
vandvag <vandvag@gmail.com>
2025-08-13 21:22:20 +0300

    [SOFLE]: Added number layer

--- keyboards/sofle/keymaps/vandvag/keymap.c
+++ keyboards/sofle/keymaps/vandvag/keymap.c
@@ -32,6 +32,7 @@ enum sofle_layers {
     _SYM,
     _NAV,
     _FN,
+	_NUM,
     _ADJUST,
 };
 
@@ -57,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |SYM   | /Enter  /       \Space \  |NAV   |(FN, RAlt)| RClt | RGUI |
- *            |      |      |      |      |/       /         \      \ |      |          |      |      |
- *            `----------------------------------'           '------''---------------------------'
+ *     | LGUI | LAlt | (NUM, LAlt) |SYM   | /Enter  /       \Space \  |NAV   |(FN, RAlt)| RClt | RGUI |
+ *     |      |      |             |      |/       /         \      \ |      |          |      |      |
+ *     `------------------------------------------'           '------''---------------------------'
  */
 
 [_QWERTY] = LAYOUT(
@@ -67,8 +68,9 @@ KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    
 KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
 KC_LCTL,  GUI_A, ALT_S, SFT_D, CTL_F, KC_G,                            KC_H,    CTL_J,   SFT_K,   ALT_L,    GUI_SCLN,  KC_QUOT,
 KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-KC_LGUI,KC_LCTL,KC_LALT, MO(_SYM), KC_ENT,      KC_SPC,  MO(_NAV), LT(_FN, KC_RALT), KC_RCTL, KC_RGUI
+KC_LGUI,KC_LCTL,LT(_NUM, KC_LALT), MO(_SYM), KC_ENT,      KC_SPC,  MO(_NAV), LT(_FN, KC_RALT), KC_RCTL, KC_RGUI
 ),
+
 /*
  * COLEMAK
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -80,9 +82,9 @@ KC_LGUI,KC_LCTL,KC_LALT, MO(_SYM), KC_ENT,      KC_SPC,  MO(_NAV), LT(_FN, KC_RA
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |SYM   | /Enter  /       \Space \  |NAV   | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'           '------''---------------------------'
+ *     | LGUI | LAlt | (NUM, LAlt) |SYM   | /Enter  /       \Space \  |NAV   |(FN, RAlt)| RClt | RGUI |
+ *     |      |      |             |      |/       /         \      \ |      |          |      |      |
+ *     `------------------------------------------'           '------''---------------------------'
  */
 
 [_COLEMAK] = LAYOUT(
@@ -90,8 +92,9 @@ KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,          /**/  KC_6,    KC_7,
 KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,          /**/  KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
 KC_LCTL,  GUI_A,  ALT_R,   SFT_S,   CTL_T,   KC_D,          /**/  KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
 KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE, /**/  XXXXXXX, KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-KC_LGUI,KC_LCTL,KC_LALT, MO(_SYM), KC_ENT,                  /**/  KC_SPC,  MO(_NAV), LT(_FN, KC_RALT), KC_RCTL, KC_RGUI
+KC_LGUI,KC_LCTL,LT(_NUM, KC_LALT), MO(_SYM), KC_ENT,        /**/  KC_SPC,  MO(_NAV), LT(_FN, KC_RALT), KC_RCTL, KC_RGUI
 ),
+
 /* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | F12  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
@@ -113,6 +116,7 @@ KC_TRNS, S(KC_1), S(KC_MINS), S(KC_SCLN), KC_EQL, S(KC_4),       /**/ S(KC_2), S
 KC_TRNS, S(KC_5), KC_SLSH, S(KC_8), S(KC_EQL), KC_BSLS, KC_TRNS, /**/ KC_TRNS, KC_SLSH, KC_MINS, S(KC_COMM), S(KC_DOT), KC_BSLS, KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
+
 /* NAVIGATION
  * ,----------------------------------------.                        ,-----------------------------------------.
  * |      |      |       |        |      |     |                     |      |      |      |      |      |      |
@@ -134,6 +138,7 @@ _______, _______, MS_LEFT, MS_DOWN, MS_RGHT,  KC_CAPS,         /**/ KC_F9, KC_LE
 _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_PSCR, _______, /**/ _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______,
 _______, _______, _______, _______, _______,                   /**/ _______, _______, _______, _______, _______
 ),
+
 /* FUNCTION
  * ,----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -155,6 +160,29 @@ _______, KC_F11, KC_F4, KC_F5, KC_F6, _______,          /**/ _______, KC_RCTL, K
 _______, KC_F10, KC_F1, KC_F2, KC_F3, _______, _______, /**/ _______, _______, _______, _______, _______, _______, _______,
 _______, _______, _______, _______, _______,            /**/ _______, _______, _______, _______, _______
 ),
+
+/* NUMBER
+ * ,----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      | F12  |  F7  |  F8  |  F9  |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      | F11  |  F4  |  F5  |  F6  |      |-------.    ,-------|      | RCTL | RSFT | RALT | RGUI |      |
+ * |------+------+------+------+------+------|  MUTE  |   |       |------+------+------+------+------+------|
+ * |     |  F10  |  F1  |  F2  |  F3  |    |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |NAV   | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+[_NUM] = LAYOUT(
+_______, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
+_______, _______, _______, _______, _______, _______,          /**/          _______,    KC_7,    KC_8,    KC_9, _______, _______,
+_______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCLT, _______,          /**/          _______,    KC_4,    KC_5,    KC_6, _______, _______,
+_______, _______, _______, _______, _______, _______, _______, /**/ _______, _______,    KC_1,    KC_2,    KC_3, _______, _______,
+_______, _______, _______, _______, _______,                   /**/ _______, _______, _______, _______, _______
+),
+
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -260,6 +288,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef OLED_ENABLED
 static void print_status_narrow(void) {
+
     oled_write_P(PSTR("\n\n"), false);
     switch (get_highest_layer(layer_state)) {
         case 0:
@@ -272,6 +301,7 @@ static void print_status_narrow(void) {
             oled_write_P(PSTR("Mod\n"), false);
             break;
     }
+
     oled_write_P(PSTR("\n\n"), false);
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
@@ -294,6 +324,7 @@ static void print_status_narrow(void) {
         default:
             oled_write_ln_P(PSTR("Undef\n"), false);
     }
+
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
