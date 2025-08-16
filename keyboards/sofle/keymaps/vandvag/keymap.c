7b19b213069eb5d7be0a1e21f629f1f860c89d99
vandvag <vandvag@gmail.com>
2025-08-16 14:55:35 +0300

    [SOFLE]: Removed ESC from base layer.

--- keyboards/sofle/keymaps/vandvag/keymap.c
+++ keyboards/sofle/keymaps/vandvag/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
  * ,-----------------------------------------.                     ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                     |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |      |   1  |   2  |   3  |   4  |   5  |                     |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                     |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          /**/             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+ XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          /**/             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
   KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          /**/             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
  KC_LCTL,   GUI_A,   ALT_S,   SFT_D,   CTL_F,    KC_G,          /**/             KC_H,   CTL_J,   SFT_K,   ALT_L,GUI_SCLN, KC_QUOT,
  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX, /**/ XXXXXXX,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
