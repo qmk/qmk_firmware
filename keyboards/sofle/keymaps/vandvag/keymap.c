ff6766a6c6e0843665a369670c8c1554fde13d55
vandvag <vandvag@gmail.com>
2025-08-16 13:05:28 +0300

    [SOFLE]: Fixes for base layers
    - Enter is now shift when held, enter when tapped
    - RShift is replaced with ESC

--- keyboards/sofle/keymaps/vandvag/keymap.c
+++ keyboards/sofle/keymaps/vandvag/keymap.c
@@ -25,7 +25,11 @@
 #define ALT_I RALT_T(KC_I)
 #define GUI_O RGUI_T(KC_O)
 
+#define SFT_ENT LSFT_T(KC_ENT)
 
+// LAYER SWITCHING
+#define NUM_ALT LT(_NUM, KC_LALT)
+#define FN_ALT LT(_FN, KC_RALT)
 
 enum sofle_layers {
     _QWERTY,
@@ -48,40 +52,40 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | LCTL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *     | LGUI | LAlt | (NUM, LAlt) |SYM   | /Enter  /       \Space \  |NAV   |(FN, RAlt)| RClt | RGUI |
+ * ,-----------------------------------------.                     ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                     |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                     |   Y  |   U  |   I  |   O  |   P  | Bspc |
+ * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
+ * | LCTL |   A  |   S  |   D  |   F  |   G  |--------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|  MUTE  |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |--------|    |-------|   N  |   M  |   ,  |   .  |   /  | ESC  |
+ * `-----------------------------------------/        /     \      \-----------------------------------------'
+ *     | LGUI | LAlt | (NUM, LAlt) |SYM   | /SFT(ENT)/       \Space \  |NAV   |(FN, RAlt)| RClt | RGUI |
  *     |      |      |             |      |/       /         \      \ |      |          |      |      |
  *     `------------------------------------------'           '------''---------------------------'
  */
 
 [_QWERTY] = LAYOUT(
-KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-KC_LCTL,  GUI_A, ALT_S, SFT_D, CTL_F, KC_G,                            KC_H,    CTL_J,   SFT_K,   ALT_L,    GUI_SCLN,  KC_QUOT,
-KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-KC_LGUI,KC_LCTL,LT(_NUM, KC_LALT), MO(_SYM), KC_ENT,      KC_SPC,  MO(_NAV), LT(_FN, KC_RALT), KC_RCTL, KC_RGUI
+  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          /**/             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          /**/             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+ KC_LCTL,   GUI_A,   ALT_S,   SFT_D,   CTL_F,    KC_G,          /**/             KC_H,   CTL_J,   SFT_K,   ALT_L,GUI_SCLN, KC_QUOT,
+ KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX, /**/ XXXXXXX,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+		           KC_LGUI, KC_LCTL, NUM_ALT,MO(_SYM), SFT_ENT, /**/  KC_SPC,MO(_NAV),  FN_ALT,  KC_RCTL,KC_RGUI
 ),
 
 /*
  * COLEMAK
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   G  |                    |   J  |   L  |   U  |   Y  |   ;  | Bspc |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | LCTL |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *     | LGUI | LAlt | (NUM, LAlt) |SYM   | /Enter  /       \Space \  |NAV   |(FN, RAlt)| RClt | RGUI |
+ * ,-----------------------------------------.                     ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                     |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |                     |   J  |   L  |   U  |   Y  |   ;  | Bspc |
+ * |------+------+------+------+------+------|                     |------+------+------+------+------+------|
+ * | LCTL |   A  |   R  |   S  |   T  |   D  |--------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
+ * |------+------+------+------+------+------|  MUTE  |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |--------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/        /     \      \-----------------------------------------'
+ *     | LGUI | LAlt | (NUM, LAlt) |SYM   | /SFT(ENT)/       \Space \  |NAV   |(FN, RAlt)| RClt | RGUI |
  *     |      |      |             |      |/       /         \      \ |      |          |      |      |
  *     `------------------------------------------'           '------''---------------------------'
  */
@@ -90,8 +94,8 @@ KC_LGUI,KC_LCTL,LT(_NUM, KC_LALT), MO(_SYM), KC_ENT,      KC_SPC,  MO(_NAV), LT(
 KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,          /**/  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
 KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,          /**/  KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
 KC_LCTL,  GUI_A,  ALT_R,   SFT_S,   CTL_TH,   KC_D,         /**/  KC_H,   CTL_N,   SFT_E,   ALT_I,   GUI_O,  KC_QUOT,
-KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE, /**/  XXXXXXX, KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-KC_LGUI,KC_LCTL,LT(_NUM, KC_LALT), MO(_SYM), KC_ENT,        /**/  KC_SPC,  MO(_NAV), LT(_FN, KC_RALT), KC_RCTL, KC_RGUI
+KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE, /**/  XXXXXXX, KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_ESC,
+		           KC_LGUI, KC_LCTL, NUM_ALT,MO(_SYM), SFT_ENT, /**/  KC_SPC,MO(_NAV),  FN_ALT,  KC_RCTL,KC_RGUI
 ),
 
 /* SYMBOLS
