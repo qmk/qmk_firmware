b9d051c00bc72a2fae503990273f2c9c4e7b76de
vandvag <vandvag@gmail.com>
2025-08-16 14:53:59 +0300

    [SOFLE]: Added more options to config.h

--- keyboards/sofle/keymaps/vandvag/config.h
+++ keyboards/sofle/keymaps/vandvag/config.h
@@ -3,8 +3,23 @@
 
 #pragma once
 
+#define MASTER_LEFT
+
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
+
+#define TAPPING_TERM_PER_KEY
+
+// See https://configure.zsa.io/moonlander/layouts/bn3qp/latest/config/mouse
+// for an explanation.
+#define MOUSEKEY_INTERVAL 20
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_MAX_SPEED 8
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_WHEEL_INTERVAL 100
+#define MOUSEKEY_WHEEL_MAX_SPEED 40
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+#define MOUSEKEY_WHEEL_DELAY 400
