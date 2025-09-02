19a6c90aacfcbb9b141514346bd97bcc925238ac
Freather <2247435595@qq.com>
2025-09-02 21:37:56 +0800

    Add Electra 80 PCB
    Add Electra 80 PCB

---
+++ keyboards/pluskbd/electra80/config.h
@@ -0,0 +1,17 @@
+// Copyright 2025 PluskbID
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keyboards/pluskbd/electra80/electra80.c
@@ -0,0 +1,58 @@
+/* Copyright 2025 PluskbID
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "quantum.h"
+
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 1, HSV_WHITE}
+);
+
+const rgblight_segment_t PROGMEM my_capsoff_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 1, HSV_BLACK}
+);
+
+const rgblight_segment_t PROGMEM my_scrlk_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 1,  HSV_WHITE}
+);
+
+const rgblight_segment_t PROGMEM my_scrlkoff_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 1,  HSV_BLACK}
+);
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer,
+    my_capsoff_layer,
+    my_scrlk_layer,
+    my_scrlkoff_layer
+);
+
+
+void keyboard_post_init_kb(void){
+    rgblight_layers = my_rgb_layers;
+}
+
+
+bool led_update_kb(led_t led_state){
+    bool res = led_update_user(led_state);
+    if (res) {
+        rgblight_set_layer_state(0, led_state.caps_lock);
+        rgblight_set_layer_state(1, !led_state.caps_lock);
+        rgblight_set_layer_state(2, led_state.scroll_lock);
+        rgblight_set_layer_state(3, !led_state.scroll_lock);
+    }
+    return res;
+}
