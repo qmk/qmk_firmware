7174058a111e20723e08f147c64ce0d36bea8f91
Philip <68480429+iliorik@users.noreply.github.com>
2025-09-07 23:03:47 +0300

    Add files via upload

---
+++ keyboards/handwired/dactyl_manuform/5x8/config.h
@@ -0,0 +1,17 @@
+// Включение OLED
+// #define OLED_DISPLAY_128X64
+// #define OLED_TIMEOUT 30000 // таймаут в миллисекундах
+#define OLED_DISPLAY_WIDTH 128
+#define OLED_DISPLAY_HEIGHT 32
+#define OLED_COLUMN_OFFSET 0 // Shift output to the right this many pixels
+
+// Высокий шрифт
+#define OLED_FONT_H "lib/glcdfont.c"
+#define OLED_FONT_START 0
+#define OLED_FONT_END 223
+#define OLED_FONT_WIDTH 6
+#define OLED_FONT_HEIGHT 12
+
+// Пины подключения
+// #define OLED_SDA_PIN G1
+// #define OLED_SCL_PIN G0
