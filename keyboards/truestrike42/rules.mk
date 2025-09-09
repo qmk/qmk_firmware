99c929a8b26a12b7ac90c012e56f3dd717a380a9
Choi Byungyoon <byungyoonc@gmail.com>
2025-09-09 16:37:07 +0900

    Remove VIA related code (to be added to VIA userspace repo)

--- keyboards/truestrike42/rules.mk
+++ keyboards/truestrike42/rules.mk
@@ -3,7 +3,3 @@ CUSTOM_MATRIX = lite
 ANALOG_DRIVER_REQUIRED = yes
 QUANTUM_PAINTER_ENABLE = yes
 QUANTUM_PAINTER_DRIVERS += sh1106_i2c
-
-ifeq ($(strip $(VIA_ENABLE)), yes)
-    SRC += via_he.c
-endif
