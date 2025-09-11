54a923ade5a566252e742616c815d271ce0f93ae
ec965 <enoch965@gmail.com>
2025-09-11 01:57:35 -0700

    fix numpad_5x4 layout and add ortho_5x4 layout

--- keyboards/enochchau/dropout/keymaps/default/keymap.c
+++ keyboards/enochchau/dropout/keymaps/default/keymap.c
@@ -11,29 +11,30 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_BASE] = LAYOUT_numpad_5x4(
+    [_BASE] = LAYOUT_ortho_5x4(
         LT(1, KC_NUM), KC_PSLS, KC_PAST, KC_MPLY,
         KC_P7, KC_P8, KC_P9, KC_PMNS,
         KC_P4, KC_P5, KC_P6, KC_PPLS,
-        KC_P1, KC_P2, KC_P3, KC_PENT,
-        KC_P0, KC_PDOT
+        KC_P1, KC_P2, KC_P3, _______,
+        KC_P0, _______, KC_PDOT, KC_PENT
     ),
 
-    [_LIGHT] = LAYOUT_numpad_5x4(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, BL_UP, BL_DOWN, BL_TOGG,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
-        KC_TRNS, KC_TRNS
+    [_LIGHT] = LAYOUT_ortho_5x4(
+        _______, _______, _______, _______,
+        _______, BL_UP, BL_DOWN, BL_TOGG,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, QK_BOOT
     ),
 
-    [_EMPTY] = LAYOUT_numpad_5x4( // empty layer
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS
-    )};
+    [_EMPTY] = LAYOUT_ortho_5x4( // empty layer
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        _______, _______, _______, _______
+    )
+};
 
 bool led_update_user(led_t led_state) {
     return true;
@@ -42,7 +43,7 @@ bool led_update_user(led_t led_state) {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_BASE]  = {ENCODER_CCW_CW(KC_VOLU, KC_VOLD)},
-    [_LIGHT] = {ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
-    [_EMPTY] = {ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
+    [_LIGHT] = {ENCODER_CCW_CW(_______, _______)},
+    [_EMPTY] = {ENCODER_CCW_CW(_______, _______)},
 };
 #endif
