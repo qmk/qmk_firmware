6353b5b8627ce8034723fcaa932d9e17f2027bee
ec965 <enoch965@gmail.com>
2025-09-11 11:13:00 -0700

    setup numlock to copy BACKLIGHT_CAPS_LOCK

--- keyboards/enochchau/dropout/dropout.c
+++ keyboards/enochchau/dropout/dropout.c
@@ -3,29 +3,31 @@
 
 #include "quantum.h"
 
-void led_init_kb(void) {
-    // numlock led is wired like so:
-    // 5V -> led -> pin
-    setPinOutput(LED_NUM_LOCK_PIN); // numlock led
-    writePinHigh(LED_NUM_LOCK_PIN);
+// Mimic handle_backlight_caps_lock to use num lock indicator for backlight
+void handle_backlight_num_lock(led_t led_state) {
+    // Use backlight as num_lock indicator
+    uint8_t bl_toggle_lvl     = 0;
+    bool    backlight_enabled = is_backlight_enabled();
+
+    if (led_state.num_lock && !backlight_enabled) {
+        // Turning num_lock ON and backlight is disabled in config
+        // Toggling backlight to the brightest level
+        bl_toggle_lvl = BACKLIGHT_LEVELS;
+    } else if (!led_state.num_lock && backlight_enabled) {
+        // Turning num_lock OFF and backlight is enabled in config
+        // Toggling backlight and restoring config level
+        bl_toggle_lvl = get_backlight_level();
+    }
+
+    backlight_set(bl_toggle_lvl);
 }
 
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res) {
-        writePin(LED_NUM_LOCK_PIN, led_state.num_lock);
-        // enable or disable the backlight based on num lock state
 #ifdef BACKLIGHT_ENABLE
-        if (!led_state.num_lock) {
-            backlight_enable();
-        } else {
-            backlight_disable();
-        }
+        handle_backlight_num_lock(led_state);
 #endif
     }
     return res;
-}
-
-void matrix_init_kb(void) {
-    led_init_kb();
 }
