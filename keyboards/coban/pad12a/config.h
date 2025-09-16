f3f8b1901f4f01bbb086f8fb7d9e4bab1e139bc4
Jack Sangdahl <jack@pngu.org>
2025-09-16 15:44:52 -0600

    Migrate keyboards C

--- keyboards/coban/pad12a/config.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2025 RyanDam (https://github.com/RyanDam)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
