04f780ef095f27731c2f93ef1167ebf46a2613b4
Jack Sangdahl <jack@pngu.org>
2025-09-16 16:57:32 -0600

    Migrate keyboards E

--- keyboards/era/sirind/tomak79h/config.h
+++ keyboards/era/sirind/tomak79h/config.h
@@ -1,6 +1,6 @@
 // Copyright 2024 Hyojin Bak (@eerraa)
 // SPDX-License-Identifier: GPL-2.0-or-later
- 
+
 #pragma once
 
 /* Split configuration */
@@ -9,7 +9,10 @@
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
 #define SERIAL_USART_PIN_SWAP
+<<<<<<< HEAD
 
 /* Reset */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+=======
+>>>>>>> 33252b93ee (Migrate keyboards E)
