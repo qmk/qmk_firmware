2bdd2b88ac7b0248b4a45cebf0cb36403a40a653
Jack Sangdahl <jack@pngu.org>
2025-09-16 22:02:46 -0600

    Migrate keyboards M

--- keyboards/mzmkb/slimdash/rev1/config.h
+++ keyboards/mzmkb/slimdash/rev1/config.h
@@ -16,13 +16,8 @@
 
 #pragma once
 
-// Make it easier to enter the bootloader
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
-
 // UART1, communication between the two halves
 #define SERIAL_USART_FULL_DUPLEX // Enable full duplex operation mode
 #define SERIAL_USART_TX_PIN GP17 // USART TX pin
 #define SERIAL_USART_RX_PIN GP18 // USART RX pin
 #define SERIAL_PIO_USE_PIO1      // Use PIO1 as PIO0 is used for WS2812 driver
-
