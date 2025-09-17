edbd5bfa4e28f71a263d6aa252c30c94dd253c95
Jack Sangdahl <jack@pngu.org>
2025-09-16 22:14:35 -0600

    Migrate keyboards T

--- keyboards/trojan_pinata/model_b/rev0/config.h
+++ keyboards/trojan_pinata/model_b/rev0/config.h
@@ -3,9 +3,6 @@
 
 #pragma once
 
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
-
 // settings for the oled keyboard demo with Adafruit 0.91" OLED display on the Stemma QT port
 #define I2C_DRIVER I2CD0
 #define I2C1_SDA_PIN GP0
