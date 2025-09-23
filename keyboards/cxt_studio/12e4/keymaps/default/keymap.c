584b1d4d7b35df4aec23265ba0bfa9ae6904569a
Mari <198762422+a-mimi-r@users.noreply.github.com>
2025-09-22 23:54:02 -0300

    Update keymap.c

--- keyboards/cxt_studio/12e4/keymaps/default/keymap.c
+++ keyboards/cxt_studio/12e4/keymaps/default/keymap.c
@@ -20,8 +20,9 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     // Light up only the selected LED if it's within the current render range
     if (index_to_light != NO_LED && index_to_light >= led_min && index_to_light < led_max) {
-        rgb_matrix_set_color(index_to_light, RGB_GREEN); // You can choose any color
+        rgb_matrix_set_color(index_to_light, RGB_GREEN);
     }
 
     return false;  // Let other lighting effects (if any) still apply
 }
+
