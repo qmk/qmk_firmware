51fc885a8d77794b8c55f7a05ccfbd4aff5ca2a4
diegorodriguezv <diegorodriguezv@gmail.com>
2025-09-23 23:00:52 -0500

    Add delete to the default keymap
    It commonly deletes items in graphical applications.

--- keyboards/szrkbd/szr35/keymaps/default/keymap.c
+++ keyboards/szrkbd/szr35/keymaps/default/keymap.c
@@ -6,6 +6,7 @@
 // switch easily between the 36-key keyboard and a laptop keyboard. The goal is to minimize the
 // learning curve by avoiding key relocation and maintaining our hard-earned muscle memory.
 
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
 
 enum layers {
@@ -23,6 +24,7 @@ enum layers {
 #define Z_SFT   LSFT_T(KC_Z)
 #define SLS_SFT RSFT_T(KC_SLSH)
 #define BSL_SFT RSFT_T(KC_BSLS)
+#define DEL_ALT LALT_T(KC_DEL)
 #define TAB_GUI LGUI_T(KC_TAB)
 #define ESC_CTL LCTL_T(KC_ESC)
 
@@ -38,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     Z_SFT,   KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  SLS_SFT,
-                      KC_LALT, TAB_GUI, ESC_CTL,      KC_SPC,  ENT_LY1, BSP_LY2
+                      DEL_ALT, TAB_GUI, ESC_CTL,      KC_SPC,  ENT_LY1, BSP_LY2
   ),
 
   [_LAYER_1] = LAYOUT_split_3x5_3(
