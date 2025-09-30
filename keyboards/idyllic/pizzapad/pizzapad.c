0ab1e66c65f77a669dc76c348877f82531bfddb5
harky <23428162+zykrah@users.noreply.github.com>
2025-09-30 12:02:54 +1000

    Remove config.h file

--- keyboards/idyllic/pizzapad/pizzapad.c
+++ keyboards/idyllic/pizzapad/pizzapad.c
@@ -1,7 +1,20 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+Copyright 2025 Zykrah
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "quantum.h"
+
+#define ENCODER_PIN_C GP27
 
 void keyboard_pre_init_kb(void) {
     // Set encoder pin C low
