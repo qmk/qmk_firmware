ed3f466c20a27c332ef60d198f5248321b6f30fa
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2025-10-02 19:19:37 +0200

    first attempt at sl-tog

---
+++ keyboards/cheapino/keymaps/sl-tog/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// qmk configurator seems to use these shortcuts that are otherwise not defined
+#define OS_LSFT OSM(MOD_LSFT)
+#define OS_LALT OSM(MOD_LALT)
+#define OS_LGUI OSM(MOD_LGUI)
+#define OS_LCTL OSM(MOD_LCTL)
+#define OS_RSFT OSM(MOD_RSFT)
+#define OS_RALT OSM(MOD_RALT)
+#define OS_RGUI OSM(MOD_RGUI)
+#define OS_RCTL OSM(MOD_RCTL)
+
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
+
