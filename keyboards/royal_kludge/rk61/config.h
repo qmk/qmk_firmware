e713a3c7fd2ab5623c502c649b0bf6b70281f688
DEREFERENC3D <DEREFERENC3D@protonmail.com>
2025-10-02 18:14:26 +0200

    rk61/config.h: Remove useless RGB matrix-related defines and comment
    On top of 2 out of the 3 being commented out, RGB_MATRIX_KEYPRESSES and
    RGB_MATRIX_FRAMEBUFFER_EFFECTS are already set in keyboard.json.

--- keyboards/royal_kludge/rk61/config.h
+++ keyboards/royal_kludge/rk61/config.h
@@ -1,11 +1,3 @@
-/* Enable CapsLcok LED*/
 #define CAPS_LOCK_LED_INDEX 40
 
 #define VIA_EEPROM_ALLOW_RESET TRUE
-
-/* RGB Matrix */
-#ifdef RGB_MATRIX_ENABLE
-//#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-//#   define RGB_MATRIX_LED_FLUSH_LIMIT 26
-#endif
