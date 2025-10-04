3a57a3a43b9dfacf41cafcba4a04720835909204
John Wilmes <jw@johnwilmes.name>
2025-10-04 06:12:13 -0500

    add test for additional combo config flags

---
+++ tests/combo/combo_flags/config.h
@@ -0,0 +1,13 @@
+// Copyright 2025 @johnwilmes
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "test_common.h"
+
+#define COMBO_BUFFER_LENGTH 2
+#define EXTRA_EXTRA_LONG_COMBOS
+#define COMBO_COMPRESSED
+#define COMBO_MUST_HOLD_MODS
+#define COMBO_MUST_PRESS_IN_ORDER
+#define COMBO_ONLY_FROM_LAYER 0
