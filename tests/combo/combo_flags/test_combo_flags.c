f54530f6d1eb51c64e0f4bc623818fd786a0e554
John Wilmes <jw@johnwilmes.name>
2025-10-05 14:42:34 -0500

    lint

--- tests/combo/combo_flags/test_combo_flags.c
+++ tests/combo/combo_flags/test_combo_flags.c
@@ -5,7 +5,7 @@
 
 enum combos { big, small };
 
-uint16_t const big_combo[] = {KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J, KC_K, KC_L, KC_M, KC_N, KC_O, KC_P, KC_Q, KC_R, COMBO_END};
+uint16_t const big_combo[]   = {KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J, KC_K, KC_L, KC_M, KC_N, KC_O, KC_P, KC_Q, KC_R, COMBO_END};
 uint16_t const small_combo[] = {KC_A, KC_D, COMBO_END};
 
 // clang-format off
