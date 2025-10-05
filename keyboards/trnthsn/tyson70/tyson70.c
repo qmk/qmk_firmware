9bb5ea41b52aac8c7758b5c1ad7c81c0e23174ff
Thanh Son Tran <62438883+trnthsn@users.noreply.github.com>
2025-10-05 19:06:55 +0700

    Apply suggestions from code review
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/trnthsn/tyson70/tyson70.c
+++ keyboards/trnthsn/tyson70/tyson70.c
@@ -1,7 +1,6 @@
 // Copyright 2025 Tyson.Keebs
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "rgblight.h"
 #include "quantum.h"
 
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
@@ -11,11 +10,17 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer
 );
 
-void keyboard_post_init_user(void) {
+void keyboard_post_init_kb(void) {
     // Enable the LED layers
     rgblight_layers = my_rgb_layers;
+
+    keyboard_post_init_user();
 }
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(0, led_state.caps_lock);
-    return true;
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if (res) {
+        rgblight_set_layer_state(0, led_state.caps_lock);
+    }
+    return false;
 }
