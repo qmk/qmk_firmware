f414f5a7eb60d6554d38d84d725f396b79d7df3b
Logan Butler <8854426+MatchstickWorks@users.noreply.github.com>
2025-10-06 13:53:43 -0700

    Cleanup

--- keyboards/matchstickworks/southpad/rev2/keymaps/reverse/keymap.c
+++ keyboards/matchstickworks/southpad/rev2/keymaps/reverse/keymap.c
@@ -3,14 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_reverse(
