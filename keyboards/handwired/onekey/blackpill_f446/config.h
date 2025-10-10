a7551ffaccce064156c627e3cf9853e09fa84aef
vermilion00 <106870608+vermilion00@users.noreply.github.com>
2025-10-11 00:54:49 +0200

    Added onekey example keyboard

---
+++ keyboards/handwired/onekey/blackpill_f446/config.h
@@ -0,0 +1,22 @@
+/* Copyright 2025 QMK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define ADC_PIN A5
+
+#define SOLENOID_PINS { B12, B13, B14, B15 }
+#define SOLENOID_PINS_ACTIVE_STATE { high, high, low }
