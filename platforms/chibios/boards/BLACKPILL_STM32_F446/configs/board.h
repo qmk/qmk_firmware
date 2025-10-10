42ae747f1c209fea5ebc252e965678af6aa1e362
vermilion00 <106870608+vermilion00@users.noreply.github.com>
2025-10-10 20:58:10 +0200

    Support for WeAct STM32F446 Blackpill/Core Board

---
+++ platforms/chibios/boards/BLACKPILL_STM32_F446/configs/board.h
@@ -0,0 +1,29 @@
+/* Copyright 2020 Nick Brassel (tzarc)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+// On-board active low key
+#ifndef BOARD_USER_KEY
+#   define BOARD_USER_KEY C13
+#endif
+// On-board LED
+#ifndef BOARD_USER_LED
+#   define BOARD_USER_LED B2
+#endif
+
+#include_next <board.h>
+
+#undef STM32_HSE_BYPASS
