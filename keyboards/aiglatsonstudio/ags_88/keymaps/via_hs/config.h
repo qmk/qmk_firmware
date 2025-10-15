87cb8762fc30f7e81548d671917691ad06282b24
patipan36991 <wuaywuay@hotmail.com>
2025-10-15 19:13:16 +0800

    Add aiglatsonstudio/ags_88 keyboard

---
+++ keyboards/aiglatsonstudio/ags_88/keymaps/via_hs/config.h
@@ -0,0 +1,8 @@
+// Copyright 2024 kindlestar (@kindlestar)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Override keyboard name for HS version
+#undef PRODUCT
+#define PRODUCT "AGS_88H"
---
+++ keyboards/aiglatsonstudio/ags_88/keymaps/via_sd/config.h
@@ -0,0 +1,12 @@
+// Copyright 2024 kindlestar (@kindlestar)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Override keyboard name for SD version
+#undef PRODUCT
+#define PRODUCT "AGS_88S"
+
+// Override Product ID for SD version to distinguish from HS version
+#undef PRODUCT_ID
+#define PRODUCT_ID 0x0012
