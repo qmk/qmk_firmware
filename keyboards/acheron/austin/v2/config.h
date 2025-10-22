ed5044c6a8d1dfed763295e767783d9ebcd349f3
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2025-10-21 22:23:44 -0300

    V2 firm fixes

--- keyboards/acheron/austin/v2/config.h
+++ keyboards/acheron/austin/v2/config.h
@@ -27,11 +27,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BACKLIGHT_PWM_DRIVER  PWMD3
 #define BACKLIGHT_PWM_CHANNEL 4
 #define BAKCLIGHT_PAL_MODE    2
-#define BREATHING_PERIOD      10
-#define BACKLIGHT_DEFAULT_LEVEL	10
-
-#define WEAR_LEVELING_LOGICAL_SIZE 2048
-#define WEAR_LEVELING_BACKING_SIZE 4096
 
 #define CAPS_INDICATOR_PIN C8
 #define INDICATOR_1_PIN D2 // RIGHT INDICATOR
--- keyboards/acheron/austin/v2/halconf.h
+++ keyboards/acheron/austin/v2/halconf.h
@@ -17,6 +17,5 @@
 #pragma once
 
 #define HAL_USE_PWM TRUE
-#define HAL_USE_PAL TRUE
 
 #include_next <halconf.h>
