1b2c3574a2ad160b8d472a6671f8c2b568ce1de7
yiancar <yiangosyiangou@cytanet.com.cy>
2025-10-23 14:52:16 +0100

    Update keymap.c

--- keyboards/yiancardesigns/hyper7/v4/keymaps/default/keymap.c
+++ keyboards/yiancardesigns/hyper7/v4/keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 /* Copyright 2023 Yiancar-Designs, Bit-Shifter
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,8 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// Copyright 2023 Yiancar-Designs, Bit-Shifter
-// SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #include QMK_KEYBOARD_H
 
 enum my_keycodes {
