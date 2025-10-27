fa0708e3cb5a9e31b1fe9d68ef9614158c9648cc
Félix <59839401+v3lmx@users.noreply.github.com>
2025-10-27 16:44:39 +0100

    remove layer 3 in default keymap
    Co-authored-by: Jack Sangdahl <jack@pngu.org>

--- keyboards/cornifi/keymaps/default/keymap.c
+++ keyboards/cornifi/keymaps/default/keymap.c
@@ -15,13 +15,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   _______,        _______,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,        _______,    KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,XXXXXXX,
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-                                KC_LGUI,_______,KC_SPC,         KC_ENT,     MO(3),  KC_RALT
+                                KC_LGUI,_______,KC_SPC,         KC_ENT,     XXXXXXX,  KC_RALT
   ),
     [2] = LAYOUT_split_3x5_3_ex2(
         KC_EXLM, KC_AT,KC_HASH, KC_DLR, KC_PERC,_______,        _______,    KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,        _______,    KC_MINS,KC_EQL, KC_LBRC,KC_RBRC,KC_BSLS,
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                            KC_UNDS,KC_PLUS,KC_LCBR,KC_RCBR,KC_PIPE,
-                                KC_LGUI,MO(3),  KC_SPC,         KC_ENT,     _______,KC_RALT
-  ),
+                                KC_LGUI,XXXXXXX,  KC_SPC,         KC_ENT,     _______,KC_RALT
+  )
     // clang-format on
 };
