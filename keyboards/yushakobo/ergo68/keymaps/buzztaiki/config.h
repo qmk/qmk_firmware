aa13d015a5550237e0796a54e6b39fbc6c9cc53d
Taiki Sugawara <buzz.taiki@gmail.com>
2025-11-10 11:22:30 +0900

    use HOLD_ON_OTHER_KEY_PRESS

--- keyboards/yushakobo/ergo68/keymaps/buzztaiki/config.h
+++ keyboards/yushakobo/ergo68/keymaps/buzztaiki/config.h
@@ -1,5 +1,6 @@
 #pragma once
 
-// work around for tab and hold
-#define PERMISSIVE_HOLD
-#define RETRO_TAPPING
+// https://docs.qmk.fm/tap_hold
+// https://www.reddit.com/r/ErgoMechKeyboards/comments/1bt3nf5/hold_keys_like_hrm_space_significantly_reduce_my/
+// 全部のキーじゃなくて、親指系だけ有効がよいかも。
+#define HOLD_ON_OTHER_KEY_PRESS
