f7139bae3c107119a84818fa92a26cdd65458cc4
Richard Tix <rtix@hu-c-031.cinemo-de.internal>
2025-11-18 11:21:54 +0100

    Finalized romeo 40 layout

--- keyboards/coseyfannitutti/romeo/keymaps/Troglodyte/keymap.c
+++ keyboards/coseyfannitutti/romeo/keymaps/Troglodyte/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KC_TAB,         KC_Q,    KC_W,    KC_E,      KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P,      KC_BSPC,
 	LM(1,MOD_LSFT), KC_A,    KC_S,    KC_D,      KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,            KC_ENT,
-	KC_LSFT,        KC_SLSH, KC_Y,    KC_X,      KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSFT,
+	KC_LSFT,        KC_MINS, KC_Y,    KC_X,      KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
 	KC_LCTL,        KC_LGUI, KC_LALT, LT(1,KC_SPC), LT(2,KC_SPC), KC_SPC, KC_RALT, LT(2,KC_DEL), KC_RCTL ),
 
 [1] = LAYOUT_all(
