4a19133806a592344ce51cbf4176ac5b6da12dc2
andrew morton <drewish@katherinehouse.com>
2025-11-18 11:26:34 -0700

    Enhance the default keymap

--- keyboards/cbkbd/acai/keymaps/default/keymap.c
+++ keyboards/cbkbd/acai/keymaps/default/keymap.c
@@ -6,10 +6,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,   KC_Y,   KC_U,           KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,          KC_G,   KC_H,   KC_J,           KC_K,    KC_L,    KC_ENTER,
-        KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,   KC_N,   KC_M,           KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LCTL, KC_LGUI, KC_LALT, LT(1,KC_BSPC), KC_DEL, KC_ENT, LT(2,KC_SPACE), KC_RALT, KC_RGUI, KC_RCTL
+        KC_Q,         KC_W,    KC_E,    KC_R,          KC_T,   KC_Y,   KC_U,           KC_I,    KC_O,    KC_P,
+        KC_A,         KC_S,    KC_D,    KC_F,          KC_G,   KC_H,   KC_J,           KC_K,    KC_L,    KC_ENTER,
+        LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,          KC_B,   KC_N,   KC_M,           KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
+        KC_LCTL,      KC_LGUI, KC_LALT, LT(1,KC_BSPC), KC_DEL, KC_ENT, LT(2,KC_SPACE), KC_RALT, KC_RGUI, KC_RCTL
     ),
 
     [1] = LAYOUT(
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [2] = LAYOUT(
-        QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        UG_TOGG, UG_NEXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
