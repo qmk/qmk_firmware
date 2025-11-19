5d9d80ccdf546fa8cbf271e647a6fae8e23c3cca
Carlos de Paula <me@carlosedp.com>
2025-11-19 16:54:51 -0300

    Cleanup some warnings

--- keyboards/epomaker/tide_alice66/config.h
+++ keyboards/epomaker/tide_alice66/config.h
@@ -3,16 +3,16 @@
 
 #pragma once
 
-#define USB_POWER_EN_PIN                    B1 // USB ENABLE pin
-#define LED_POWER_EN_PIN                    A5 // LED ENABLE pin
-#define HS_BAT_CABLE_PIN                    A7 // USB insertion detection pin
-#define HS_LED_BOOSTING_PIN                 C4 // LED BOOSTING,Power boost pin
+#define USB_POWER_EN_PIN B1    // USB ENABLE pin
+#define LED_POWER_EN_PIN A5    // LED ENABLE pin
+#define HS_BAT_CABLE_PIN A7    // USB insertion detection pin
+#define HS_LED_BOOSTING_PIN C4 // LED BOOSTING,Power boost pin
 
-#define BAT_FULL_PIN                        A15
-#define BAT_FULL_STATE                      1
+#define BAT_FULL_PIN A15
+#define BAT_FULL_STATE 1
 
-#define HS_RGB_INDICATOR_COUNT              69
-#define HS_RGB_BAT_COUNT                    1
+#define HS_RGB_INDICATOR_COUNT 69
+#define HS_RGB_BAT_COUNT 1
 
 /* Device Name Definition */
 #define MD_BT1_NAME "Tide Alice-1"
@@ -46,16 +46,16 @@
 #define RGB_MATRIX_BAT_INDEX_MAP {53, 54, 55, 56, 57, 58, 59, 60, 61, 62}
 
 /* Status Indicator Lamp */
-#define HS_MATRIX_BLINK_INDEX_BAT           5
-#define HS_RGB_INDEX_CAPS                   23
-#define HS_RGB_INDEX_WIN_LOCK               1
-#define HS_RGB_BLINK_INDEX_VAI              6
-#define HS_RGB_BLINK_INDEX_VAD              7
-#define HS_RGB_BLINK_INDEX_SPI              8
-#define HS_RGB_BLINK_INDEX_SPD              10
+#define HS_MATRIX_BLINK_INDEX_BAT 5
+#define HS_RGB_INDEX_CAPS 23
+#define HS_RGB_INDEX_WIN_LOCK 1
+#define HS_RGB_BLINK_INDEX_VAI 6
+#define HS_RGB_BLINK_INDEX_VAD 7
+#define HS_RGB_BLINK_INDEX_SPI 8
+#define HS_RGB_BLINK_INDEX_SPD 10
 
-#define HS_RGB_BLINK_INDEX_WIN              24
-#define HS_RGB_BLINK_INDEX_MAC              25
+#define HS_RGB_BLINK_INDEX_WIN 24
+#define HS_RGB_BLINK_INDEX_MAC 25
 
 /* UART */
 #define SERIAL_DRIVER SD1
@@ -69,18 +69,18 @@
 #define UART_TX_PAL_MODE 7
 
 /* Encoder */
-#define ENCODER_MAP_KEY_DELAY               1
-#define ENCODER_RESOLUTION                  2
+#define ENCODER_MAP_KEY_DELAY 1
+#define ENCODER_RESOLUTION 2
 
 /* SPI */
-#define SPI_DRIVER                          SPIDQ
-#define SPI_SCK_PIN                         B3
-#define SPI_MOSI_PIN                        B5
-#define SPI_MISO_PIN                        B4
+#define SPI_DRIVER SPIDQ
+#define SPI_SCK_PIN B3
+#define SPI_MOSI_PIN B5
+#define SPI_MISO_PIN B4
 
 /* Flash */
 #define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
-#define WEAR_LEVELING_LOGICAL_SIZE          (WEAR_LEVELING_BACKING_SIZE / 2)
+#define WEAR_LEVELING_LOGICAL_SIZE (WEAR_LEVELING_BACKING_SIZE / 2)
 
 /* RGB Matrix */
 #define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
@@ -91,28 +91,20 @@
 #define RGBLED_NUM 8
 #define RGBLIGHT_CUSTOM_DRIVER
 #define RGBLIGHT_DISABLE_KEYCODES
-#define BREATHING_VAL           40
-#define RAINBOW_MOOD            40
-#define LED_TYPE                RGB
+#define BREATHING_VAL 40
+#define RAINBOW_MOOD 40
+#define LED_TYPE RGB
 #define RGBLIGHT_LED_MAP {0, 1, 2, 3, 4, 5, 6, 7}
 
 /* WS2812 */
-#define WS2812_SPI_DRIVER       SPIDM2
-#define WS2812_SPI_DIVISOR      32
+#define WS2812_SPI_DRIVER SPIDM2
+#define WS2812_SPI_DIVISOR 32
 
 /* rgb_record */
 #define ENABLE_RGB_MATRIX_RGBR_PLAY
-#define RGBREC_CHANNEL_NUM         4
+#define RGBREC_CHANNEL_NUM 4
 #define EECONFIG_CONFINFO_USE_SIZE (4 + 16)
-#define EECONFIG_RGBREC_USE_SIZE   (RGBREC_CHANNEL_NUM * MATRIX_ROWS * MATRIX_COLS * 2)
-#define EECONFIG_USER_DATA_SIZE    (EECONFIG_RGBREC_USE_SIZE + EECONFIG_CONFINFO_USE_SIZE)
-#define RGBREC_EECONFIG_ADDR       (uint8_t *)(EECONFIG_USER_DATABLOCK)
-#define CONFINFO_EECONFIG_ADDR     (uint32_t *)((uint32_t)RGBREC_EECONFIG_ADDR + (uint32_t)EECONFIG_RGBREC_USE_SIZE)
-
-/* Matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
-
-/* Matrix pin configuration */
-#define MATRIX_ROW_PINS { A1, A2, A3, A4, C13 }
-#define MATRIX_COL_PINS { C0, C1, C2, C3, A6, B10, B11, B12, B13, B14, A10, C6, C7, C8, C9 }
+#define EECONFIG_RGBREC_USE_SIZE (RGBREC_CHANNEL_NUM * MATRIX_ROWS * MATRIX_COLS * 2)
+#define EECONFIG_USER_DATA_SIZE (EECONFIG_RGBREC_USE_SIZE + EECONFIG_CONFINFO_USE_SIZE)
+#define RGBREC_EECONFIG_ADDR (uint8_t *)(EECONFIG_USER_DATABLOCK)
+#define CONFINFO_EECONFIG_ADDR (uint32_t *)((uint32_t)RGBREC_EECONFIG_ADDR + (uint32_t)EECONFIG_RGBREC_USE_SIZE)
