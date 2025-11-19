27e892f9e3b1e4bc028b9c3cbfad1431a7239d32
galican <2831039915@qq.com>
2025-11-19 15:16:32 +0800

    Updated mistel/airone_pro/jis.

--- keyboards/mistel/airone_pro/jis/config.h
+++ keyboards/mistel/airone_pro/jis/config.h
@@ -16,18 +16,8 @@
 
 #pragma once
 
-/* I2C Config for LED Driver */
-#define SNLED27351_I2C_ADDRESS_1 SNLED27351_I2C_ADDRESS_GND
-#define SNLED27351_I2C_ADDRESS_2 SNLED27351_I2C_ADDRESS_VDDIO
+/* Enable CapsLock LED */
+#define CAPS_LOCK_LED_INDEX 72
 
-#define I2C1_OPMODE OPMODE_I2C
-#define I2C1_CLOCK_SPEED 400000
-
-/* SPI Config for spi flash*/
-#define SPI_DRIVER SPIDQ
-#define SPI_SCK_PIN B3
-#define SPI_MOSI_PIN B5
-#define SPI_MISO_PIN B4
-#define SPI_MOSI_PAL_MODE 5
-
-#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
+/* Enable GUI Lock LED */
+#define GUI_LOCK_LED_INDEX 61
