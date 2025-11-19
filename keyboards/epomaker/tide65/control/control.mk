a09309fd65c5551ee8838ed9a3bd94ff3ac9c0a1
Carlos de Paula <me@carlosedp.com>
2025-11-19 14:53:21 -0300

    Add Epomaker Tide65 keyboard

---
+++ keyboards/epomaker/tide65/control/control.mk
@@ -0,0 +1,1 @@
+SRC += control/control.c
---
+++ keyboards/epomaker/tide65/rgb_record/rgb_rgblight.h
@@ -0,0 +1,6 @@
+// Copyright 2024 SDK (@sdk66)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "rgb_matrix.h"
+
+bool rgb_matrix_indicators_advanced_rgblight(uint8_t led_min, uint8_t led_max);
