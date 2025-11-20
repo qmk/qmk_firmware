66d38b3df147e989687f52725e9b9e9685c015dd
andrew morton <drewish@katherinehouse.com>
2025-11-19 21:58:19 -0700

    Fix keymap indenting

--- keyboards/cbkbd/coffeevan/keymaps/default/keymap.c
+++ keyboards/cbkbd/coffeevan/keymaps/default/keymap.c
@@ -4,7 +4,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 
+    /* Minisub style split-space
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬──────┐
      * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Bspc │
      * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬─────┤
@@ -14,14 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───┼───┼───┼───┤
      * │Ctrl|Spr│ Al │ Spacebar |Spacebar│Meta│Alt│Spr│Ctl│
      * └────┴───┴────┴──────────┴────────┴────┴───┴───┴───┴
-     */
-    [0] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,       
-        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,      
-        KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, 
-        KC_LCTL,   KC_LGUI, KC_LALT,   KC_SPC,                 KC_SPC,KC_SPC,              KC_RGUI,   KC_RALT, KC_RGUI, KC_RCTL  
-    )
-    /* 
+     *
+     * JetVan style 6.25u
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬──────┐
      * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Bspc │
      * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬─────┤
@@ -32,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl|Spr│ Al │        Spacebar        │Alt│Spr│Ctl│
      * └────┴───┴────┴────────────────────────┴───┴───┴───┴
      */
-    // [1] = LAYOUT_solid_space(
-    //     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,       
-    //     KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,      
-    //     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, 
-    //     KC_LCTL,   KC_LGUI, KC_LALT,   KC_SPC,                                                 KC_RALT, KC_RGUI, KC_RCTL  
-    // )
+    [0] = LAYOUT(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           KC_SPC,  KC_SPC,           KC_RGUI, KC_RALT, KC_RGUI, KC_RCTL
+    )
 };
