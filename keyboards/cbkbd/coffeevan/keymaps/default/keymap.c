2cf089c4f683a4621fcbb1af003ab3c576726559
andrew morton <drewish@katherinehouse.com>
2025-11-20 10:12:23 -0700

    Move layout diagrams to matrix_diagram.md

--- keyboards/cbkbd/coffeevan/keymaps/default/keymap.c
+++ keyboards/cbkbd/coffeevan/keymaps/default/keymap.c
@@ -4,28 +4,6 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Minisub style split-space
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬──────┐
-     * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Bspc │
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬─────┤
-     * │ Tb │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; | Ent │
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬───┤
-     * │ Shft │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │Sft│
-     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───┼───┼───┼───┤
-     * │Ctrl|Spr│ Al │ Spacebar |Spacebar│Meta│Alt│Spr│Ctl│
-     * └────┴───┴────┴──────────┴────────┴────┴───┴───┴───┴
-     *
-     * JetVan style 6.25u
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬──────┐
-     * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Bspc │
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬─────┤
-     * │ Tb │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; | Ent │
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬───┤
-     * │ Shft │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │Sft│
-     * ├────┬─┴─┬─┴──┬┴───┴───┴───┴───┴───┴───┼───┼───┼───┤
-     * │Ctrl|Spr│ Al │        Spacebar        │Alt│Spr│Ctl│
-     * └────┴───┴────┴────────────────────────┴───┴───┴───┴
-     */
     [0] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
