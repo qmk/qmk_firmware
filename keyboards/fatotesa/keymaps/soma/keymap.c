75b27ba8e11ca13b736141d5b4d876b8e90ee3f8
Stefan Gluszek <stefang@kahoot.com>
2025-11-21 10:04:59 +0100

    Update fatotesa keyboard config.

--- keyboards/fatotesa/keymaps/soma/keymap.c
+++ keyboards/fatotesa/keymaps/soma/keymap.c
@@ -18,15 +18,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_ENTER,
         OSM(MOD_LCTL), KC_A, KC_S, KC_D, KC_F, KC_G,                                             KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_BACKSLASH,
         OSM(MOD_LSFT), KC_LEFT_ANGLE_BRACKET, KC_Z, KC_X, KC_C, KC_V, KC_B,                              KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, OSM(MOD_RSFT),
-        CW_TOGG, OSM(MOD_LGUI), KC_LEFT_ALT, MO(1), OSM(MOD_LALT),                        KC_SPACE, OSM(MOD_RALT), KC_RIGHT_CTRL, KC_NO, KC_NO
+        MO(1), OSM(MOD_LCTL), KC_F12, OSM(MOD_HYPR), OSM(MOD_LGUI),                        MT(MOD_HYPR, KC_SPACE), OSM(MOD_RALT), KC_RIGHT_CTRL, KC_NO, KC_NO
     ),
 
     [1] = LAYOUT(
         QK_REBOOT, _______, _______, _______, _______,                                                                         KC_PSCR, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,  _______,
-        KC_TAB,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                               _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                                     KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, _______, _______, _______,
         _______, _______, _______,  _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,                                                                KC_PGDN, _______, _______, _______, _______
+        _______, _______, _______, _______, _______,                                                                CW_TOGG, _______, _______, _______, _______
     )
 };
