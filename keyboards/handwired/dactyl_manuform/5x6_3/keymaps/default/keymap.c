80efffb77d7d307ceaef4da57004246afa0846bf
Julian Yap <julianokyap@gmail.com>
2025-11-21 13:39:13 -0800

    Update keymap

--- keyboards/handwired/dactyl_manuform/5x6_3/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_3/keymaps/default/keymap.c
@@ -10,16 +10,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_5x6_3(
           KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
           KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLS,
-          KC_LCTL, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+          KC_LGUI, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
           KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                            KC_LEFT,KC_RGHT,                                                       KC_UP,   KC_DOWN,
-                           KC_BSPC,KC_LGUI,KC_LALT,                                               TL_LOWR,KC_ENTER,KC_SPC),
+                           KC_BSPC,KC_LCTL,KC_LALT,                                               TL_LOWR,KC_ENTER,KC_SPC),
 
   [1] = LAYOUT_5x6_3(
           KC_TILD, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,  KC_EQL,
-          __,     KC_CAPS,     __,     __,     __,     __,                        KC_VOLD,KC_VOLU,KC_MUTE,     __,KC_PSCR,      __,
+          KC_CAPS,     __,     __,     __,     __,     __,                        KC_VOLD,KC_VOLU,KC_MUTE,     __,KC_PSCR,      __,
           __,          __,     __,     __,     __,     __,                             __,     __,     __,     __,     __,      __,
           __,          __,     __,     __,     __,     __,                             __,     __,KC_LBRC,KC_RBRC,     __,      __,
                            KC_HOME,KC_END,                                                        KC_PGUP,KC_PGDN,
-                           KC_DEL,     __,KC_LCTL,                                                     __,     __,     __),
+                           KC_DEL,     __,     __,                                                     __,     __,     __),
  };
