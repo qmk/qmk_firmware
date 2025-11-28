6bccd1998d846360e9a110733b4538353489e602
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2025-11-28 09:59:47 +0100

    Add Soldered Macro Pad

---
+++ keyboards/soldered/macro_pad/keymaps/default/keymap.c
@@ -0,0 +1,37 @@
+/* Copyright 2025 Soldered Electronics <hello@soldered.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+// Layer 0: media + utilities
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_MPRV,     // Previous Track
+        KC_MNXT,     // Next Track
+        KC_MUTE,     // Mute / Unmute - encoder button
+        KC_MPLY,     // Play / Pause
+        KC_CALC,     // Launch Calculator
+        KC_MAIL      // Launch Email client
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CW(KC_VOLD, KC_VOLU)  },
+};
+#endif
+
+
