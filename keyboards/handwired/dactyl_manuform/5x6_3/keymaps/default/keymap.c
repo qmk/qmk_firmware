2e847eec86c6d58111966ff768a10442e9a97aa1
Julian Yap <julianokyap@gmail.com>
2025-12-11 00:58:05 -0800

    Update keymap

--- keyboards/handwired/dactyl_manuform/5x6_3/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_3/keymaps/default/keymap.c
@@ -10,8 +10,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLS,
           KC_LGUI, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
           KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,
-                           KC_LEFT,KC_RGHT,                                                       KC_UP,   KC_DOWN,
-                           KC_BSPC,KC_LCTL,KC_LALT,                                               TL_LOWR,KC_ENT,KC_SPC),
+                           KC_LEFT,KC_RGHT,                                                       KC_UP,  KC_DOWN,
+                           KC_BSPC,KC_LCTL,KC_LALT,                                               KC_ENT ,KC_SPC,TL_LOWR),
 
   [1] = LAYOUT_5x6_3(
           KC_GRV , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_EQL ,
