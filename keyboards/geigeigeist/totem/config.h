9f1db6a4b555f2e9be774bc47fba34aaac823f25
Jose M Zelezarof <git@zelezarof.ca>
2025-12-14 14:54:18 -0500

    Code cleanup

--- keyboards/geigeigeist/totem/config.h
+++ keyboards/geigeigeist/totem/config.h
@@ -2,27 +2,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 5
-
 /* https://docs.qmk.fm/drivers/serial#usart-full-duplex */
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
 #define SERIAL_USART_PIN_SWAP
-
-/* https://docs.qmk.fm/features/split_keyboard#handedness-by-eeprom */
-#define EE_HANDS
-
-/* https://docs.qmk.fm/tap_hold#tapping-term */
-#define TAPPING_TERM 250
-
-/* https://docs.qmk.fm/tap_hold#flow-tap */
-#define FLOW_TAP_TERM 150
-
-/* https://docs.qmk.fm/tap_hold#chordal-hold */
-#define CHORDAL_HOLD
-#define HOLD_ON_OTHER_KEY_PRESS
 
 /* https://docs.qmk.fm/platformdev_rp2040#double-tap */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
--- keyboards/geigeigeist/totem/keymaps/default/rules.mk
+++ keyboards/geigeigeist/totem/keymaps/default/rules.mk
@@ -1,8 +1,2 @@
-# https://docs.qmk.fm/features/auto_shift#how-do-i-enable-auto-shift
-AUTO_SHIFT_ENABLE = no
-
 # https://docs.qmk.fm/features/caps_word#caps-word
 CAPS_WORD_ENABLE = yes
-
-# https://docs.qmk.fm/features/repeat_key#repeat-key
-REPEAT_KEY_ENABLE = yes
