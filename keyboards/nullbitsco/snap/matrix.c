f39d822e57fdb041cecc04f64d588a409b9bcac3
zvecr <git@zvecr.com>
2025-12-20 19:40:46 +0000

    fix

--- keyboards/nullbitsco/snap/matrix.c
+++ keyboards/nullbitsco/snap/matrix.c
@@ -13,8 +13,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "matrix.h"
 #include <string.h>
+#include "matrix.h"
+#include "keyboard.h"
 #include "split_util.h"
 #include "wait.h"
 
