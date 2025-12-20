d00821785928c9f4305cd0d1014fedc0e6071507
JakeEMuller <44328624+JakeEMuller@users.noreply.github.com>
2025-12-20 00:29:37 -0500

    update default map for flash

--- keyboards/handwired/blackmuller/flash/keymaps/default/keymap.c
+++ keyboards/handwired/blackmuller/flash/keymaps/default/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,       KC_F5,   KC_F6,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,
         KC_TAB,           KC_HOME, KC_UP,   KC_END,  KC_PGUP,     KC_T,
-        KC_CAPS,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDWN,     KC_G,
+        KC_CAPS,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,     KC_G,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
         KC_LCTL, KC_LGUI,          KC_LALT,          LAYER_SYNC,  KC_DEL
     )
