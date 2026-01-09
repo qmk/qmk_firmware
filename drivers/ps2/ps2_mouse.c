c802a20604b366e02910c584946b9ec62c44bfe3
Klesh Wong <klesh@qq.com>
2026-01-09 16:14:33 +0800

    fix(input): Resolve lost mouse clicks in PS/2 streaming mode

--- drivers/ps2/ps2_mouse.c
+++ drivers/ps2/ps2_mouse.c
@@ -101,8 +101,7 @@ void ps2_mouse_task(void) {
 #    endif
     } else {
         if (debug_mouse) print("ps2_mouse: fail to get mouse packet\n");
-        /* return here to avoid updating the mouse button state */
-        return;
+        /* do NOT returning here or it would cause #24455 #24599  */
     }
 #endif
 
