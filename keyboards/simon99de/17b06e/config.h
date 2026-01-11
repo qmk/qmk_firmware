51caa3f505b93d47df8b461013669bcc50671543
Fabian Felix Selbach <ffs2309.fs@gmail.com>
2026-01-11 19:13:23 +0100

    Apply suggestions from code review
    Mostly formatting changes
    
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/simon99de/17b06e/config.h
+++ keyboards/simon99de/17b06e/config.h
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
+
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP10
 #define I2C1_SCL_PIN GP11
--- keyboards/simon99de/17b06e/rgb/keymaps/default_simon99de/config.h
+++ keyboards/simon99de/17b06e/rgb/keymaps/default_simon99de/config.h
@@ -1,3 +1,5 @@
 // Copyright 2025 FabiClawZ (@FFS2309)
 // SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
 #define MIDI_ADVANCED
