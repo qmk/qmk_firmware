c633fc174cba031ee60d8c75cd131c2e95e0599b
ivndbt <81021475+ivndbt@users.noreply.github.com>
2026-02-01 17:37:15 +0100

    added obiwanstenobit keyboard

---
+++ keyboards/ivndbt/obiwanstenobit/rev1/keymaps/default/keymap.c
@@ -0,0 +1,97 @@
+/* Copyright 2026 ivndbt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Layers declarations
+enum layers {
+    _PLOVER,
+    _LOWER,
+    _RAISE,
+    _CONTROL,
+    _QWERTY
+};
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define CONTROL MO(_CONTROL)
+#define QWERTY TG(_QWERTY)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Plover layer
+ *
+ * | Lower |  S  |  T  |  P  |  H  |  *  |   |  *  |  F  |  P  |  L  |  T  |  D  |
+ * | Raise |  S  |  K  |  W  |  R  |  *  |   |  *  |  R  |  B  |  G  |  S  |  Z  |
+ *                                                                              
+ *                     |  #  |  A  |  O  |   |  E  |  U  |  #  |  
+ */
+[_PLOVER] = LAYOUT(
+    LOWER,  STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1,    STN_ST3, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
+    RAISE,  STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,    STN_ST4, STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
+                            STN_N1, STN_A,  STN_O,      STN_E,   STN_U,  STN_N2
+  ),
+
+/* Lower layer
+ *
+ * |       |     |     |     |     |     |   |     |     |     |     |     |     |
+ * |Control|     |     |     |     |     |   |     |     |     |     |     |     |
+ *                                                                              
+ *                     |     |     |     |   |     |     |     |  
+ */
+[_LOWER] = LAYOUT(
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    CONTROL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                                  _______,  _______,  _______,  _______,  _______,  _______
+  ),
+
+/* Raise layer
+ *
+ * |Control|     |     |     |     |     |   |     |     |     |     |     |     |
+ * |       |     |     |     |     |     |   |     |     |     |     |     |     |
+ *                                                                              
+ *                     |     |     |     |   |     |     |     | 
+ */
+[_RAISE] = LAYOUT(
+    CONTROL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                                  _______,  _______,  _______,  _______,  _______,  _______
+  ),
+
+/* Control layer
+ *
+ * |       |     |     |     |     |Btldr|   |     |     |     |     |     |Qwerty|
+ * |Control|     |     |     |     |Rboot|   |     |     |     |     |     |      |
+ *                                                                              
+ *                     |     |     |     |   |     |     |     | 
+ */
+[_CONTROL] = LAYOUT(
+    _______,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QK_BOOT,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QWERTY,
+    _______,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QK_RBT,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                                    XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX
+  ),
+
+/* Qwerty layer
+ *
+ * |Space |  Q  |  W  |  E  |  R  |  T  |   |  Y  |  U  |  I  |  O  |  P  |Qwerty|
+ * |Bckspc|  A  |  S  |  D  |  F  |  G  |   |  H  |  J  |  K  |  L  |  Z  |Enter |
+ *                                                                              
+ *                    |  X  |  C  |  V  |   |  B  |  N  |  M  | 
+ */
+[_QWERTY] = LAYOUT(
+    KC_BSPC,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, QWERTY,
+    KC_SPC,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_Z, KC_ENT,
+                          KC_X, KC_C, KC_V, KC_B, KC_N, KC_M
+  )
+};
---
+++ keyboards/ivndbt/obiwanstenobit/rev1/readme.md
@@ -0,0 +1,9 @@
+# ObiWanStenobit rev1
+
+This is the first revision of the ObiWanStenobit.\
+For an in-depth look at its features, please visit the [project repository](https://github.com/ivndbt/obiwanstenobit).
+
+## Usage
+
+ObiWanStenobit is meant to be use with [Plover](https://opensteno.org/plover/) via the GeminiPR steno protocol.\
+To get started, make sure to follow the setup guide described in the [Plover Wiki](https://plover.wiki/index.php/Setting_up_a_hobbyist_writer_with_Plover) for hobbyist writers.
