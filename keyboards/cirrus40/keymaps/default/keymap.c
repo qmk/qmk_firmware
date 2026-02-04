2ec6df80e54365af7eb6026a73081553f5f89beb
Jakob Linke <36006+schuay@users.noreply.github.com>
2026-02-04 08:25:02 +0100

    Address comments

--- keyboards/cirrus40/keymaps/default/keymap.c
+++ keyboards/cirrus40/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,          KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,
         HRM_A,    HRM_R,    HRM_S,    HRM_T,    KC_G,          KC_M,     HRM_N,    HRM_E,    HRM_I,    HRM_O,
         KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,          KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_UNDS,
-                  QK_LLCK,  BTN_ESC,  NAV_SPC,  NM2_TAB,       FUN_ENT,  SYM_BSP,  NUM_DEL,  QK_LLCK
+                  _______,  BTN_ESC,  NAV_SPC,  NM2_TAB,       FUN_ENT,  SYM_BSP,  NUM_DEL,  _______
     ),
 
     [kOneHand] = LAYOUT(
@@ -50,23 +50,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [kNavigation] = LAYOUT(
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,       KC_PGUP,  _______,  _______,  _______,  XXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,       KC_PGUP,  _______,  _______,  _______,  XXXXXXX,
         KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX,       KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,
         _______,  _______,  _______,  _______,  XXXXXXX,       _______,  KC_HOME,  _______,  _______,  KC_END,
                   _______,  _______,  _______,  _______,       KC_ENT,   KC_BSPC,  KC_DEL,   _______
     ),
 
     [kNumbers] = LAYOUT(
-        QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,       XXXXXXX,  KC_7,     KC_8,     KC_9,     XXX,
+        QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,       XXXXXXX,  KC_7,     KC_8,     KC_9,     XXXXXXX,
         KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX,       S(KC_G),  KC_1,     KC_2,     KC_3,     KC_0,
         QK_RBT,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,       XXXXXXX,  KC_4,     KC_5,     KC_6,     _______,
                   _______,  _______,  _______,  _______,       KC_ENT,   KC_BSPC,  KC_DEL,   _______
     ),
 
     [kFnKeys] = LAYOUT(
-        KC_F10,   KC_F9,    KC_F8,    KC_F7,    XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXX,
+        KC_F10,   KC_F9,    KC_F8,    KC_F7,    XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
         KC_F11,   KC_F3,    KC_F2,    KC_F1,    XXXXXXX,       XXXXXXX,  KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LGUI,
-        KC_F12,   KC_F6,    KC_F5,    KC_F4,    XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXX,
+        KC_F12,   KC_F6,    KC_F5,    KC_F4,    XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
                   _______,  KC_ESC,   KC_SPC,   KC_TAB,        _______,  _______,  _______,  _______
     ),
 
