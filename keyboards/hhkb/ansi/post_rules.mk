1b7082b4a6ca79b6d5098208265707d3930bef32
uruz7 <z13857231871@gmail.com>
2026-02-05 18:52:31 +0900

    Recover RN-42 Bluetooth support for HHKB keyboard
    - Implement battery management with new battery.h and battery.c files.
    - Create main.c to initialize hardware and manage USB/Bluetooth tasks.
    - Add RN-42 communication functions in rn42.c and rn42.h.
    - Implement RN-42 task handling in rn42_task.c and rn42_task.h.
    - Introduce serial communication functions in serial.h and serial_uart.c.
    - Add software UART implementation in suart.S and suart.h.
    - Ensure compatibility with NKRO and power management features.

---
+++ keyboards/hhkb/ansi/post_rules.mk
@@ -0,0 +1,14 @@
+# RN42 Bluetooth Support
+# add HHKB_RN42_ENABLE = yes to rules.mk to enable
+
+ifeq ($(strip $(HHKB_RN42_ENABLE)), yes)
+    OPT_DEFS += -DHHKB_RN42_ENABLE
+    RN42_DIR = keyboards/hhkb/rn42
+    SRC += $(RN42_DIR)/serial_uart.c \
+           $(RN42_DIR)/suart.S \
+           $(RN42_DIR)/rn42.c \
+           $(RN42_DIR)/rn42_task.c \
+           $(RN42_DIR)/battery.c
+    VPATH += $(RN42_DIR)
+    EXTRAINCDIRS += $(RN42_DIR)
+endif
