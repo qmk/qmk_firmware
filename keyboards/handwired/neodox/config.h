6c3d3074b3cbf1f451e3c5de8f27055c59bc6667
Matteo Dal Molin <matteodalmo@gmail.com>
2026-02-13 18:48:16 +0100

    remove duplicated data between json and rules.mk/config.h

--- keyboards/handwired/neodox/config.h
+++ keyboards/handwired/neodox/config.h
@@ -3,8 +3,6 @@
 
 #pragma once
 
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
 
 
 /* Handness */
--- keyboards/handwired/neodox/rules.mk
+++ keyboards/handwired/neodox/rules.mk
@@ -1,19 +1,9 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System controliouy
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 BACKLIGHT_DRIVER = pwm
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-WPM_ENABLE = yes
-SPLIT_KEYBOARD = yes
-QUANTUM_PAINTER_ENABLE = yes
 SERIAL_DRIVER = vendor
 WS2812_DRIVER = vendor
 QUANTUM_PAINTER_DRIVERS = gc9a01_spi
