79015fa307d7a1755e182f372962bb7970ee962f
MatteoDM <matteodalmo@gmail.com>
2026-02-13 18:20:16 +0100

    Update keyboards/handwired/neodox/config.h
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/neodox/config.h
+++ keyboards/handwired/neodox/config.h
@@ -30,9 +30,3 @@
 
 #define BACKLIGHT_PWM_DRIVER PWMD6
 #define BACKLIGHT_PWM_CHANNEL RP2040_PWM_CHANNEL_A
-
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
