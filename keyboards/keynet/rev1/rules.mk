85d31004e232e4eba25222c6a12ae50df72d1b03
Arthur <arturf12515@gmail.com>
2026-02-15 21:02:29 +0400

    Add keyboard keynet

---
+++ keyboards/keynet/rev1/rules.mk
@@ -0,0 +1,23 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+# CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+# RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+SERIAL_DRIVER = bitbang
+ENCODER_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+# ENCODER_MAP_ENABLE = yes
+
+# STENO_ENABLE = yes
+# STENO_PROTOCOL = geminipr
+NKRO_ENABLE = yes	            # Enable N-Key Rollover
+VIRTSER_ENABLE = yes
+FORCE_NKRO = yes
+KEYBOARD_SHARED_EP = yes
+
+SWAP_HANDS_ENABLE = yes
