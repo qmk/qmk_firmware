89cf2ce900141c585d3a3eaf915b806ae8af9ab1
Assem Mohamed <126217978+ToYoNiX@users.noreply.github.com>
2026-02-26 13:48:14 +0200

    Adding encoder map and updating keymap.

--- keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
+++ keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
@@ -1,3 +1,6 @@
+// Copyright 2026 ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 // Home Row Mods - custom modifier order
@@ -11,10 +14,7 @@
 #define HM_L LCTL_T(KC_L)           // Ctrl
 #define HM_SCLN RSFT_T(KC_SCLN)     // Shift
 
-// Tap-Hold Keys
-#define TC_L0 LT(MO(1), KC_ESC)
-#define TC_R0 LT(MO(4), KC_ENT)
-
+// Keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* BASE Layer: QWERTY */
     [0] = LAYOUT_split_3x5_3(
@@ -22,47 +22,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             /*B0*/            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         HM_A,    HM_S,    HM_D,    HM_F,    KC_G,             /*A5*/            KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             /*A4*/            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          TC_L0,   KC_SPC,  MO(3),            /*A3*/            TC_R0,   KC_BSPC, MO(2)
+                          KC_ESC,  KC_SPC,  MO(1),            /*A3*/            MO(2),   KC_BSPC, KC_ENT
     ),
     [1] = LAYOUT_split_3x5_3(
-        KC_ESC,  KC_F3,   KC_F6,   KC_F9,   KC_F12,                             KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,
-        KC_TAB,  KC_F2,   KC_F5,   KC_F8,   KC_F11,                             KC_NO,   KC_NO,   KC_4,    KC_5,    KC_6,
-        KC_DEL,  KC_F1,   KC_F4,   KC_F7,   KC_F10,                             KC_NO,   KC_0,    KC_1,    KC_2,    KC_3,
+        KC_NO,   KC_7,    KC_8,    KC_9,    KC_0,                               KC_GRV,  KC_QUOT, KC_MINS, KC_EQL,  KC_NO,
+        KC_LSFT, KC_4,    KC_5,    KC_6,    KC_PSCR,                            KC_HOME, KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT,
+        KC_NO,   KC_1,    KC_2,    KC_3,    KC_DEL,                             KC_END,  KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
                           _______, _______, _______,                            _______, _______, _______
     ),
     [2] = LAYOUT_split_3x5_3(
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-        KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE,                            KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_TILD, KC_GRV,  KC_QUOT, KC_DQUO, KC_COLN,                            KC_SCLN, KC_LT,   _______,   _______, _______,
+        QK_BOOT,  KC_F7,  KC_F8,   KC_F9,   KC_F10,                             TO(3),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,    KC_F4,  KC_F5,   KC_F6,   KC_F11,                             KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        QK_RBT,   KC_F1,  KC_F2,   KC_F3,   KC_F12,                             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                           _______, _______, _______,                            _______, _______, _______
-    ), 
+    ),
+    // Gaming Layers
     [3] = LAYOUT_split_3x5_3(
-        QK_BOOT, KC_NO,   RESET,   KC_MPRV, KC_MNXT,                            KC_MPLY, KC_MSTP, KC_MUTE, KC_PSCR, KC_NUM,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   MS_LEFT, MS_DOWN, MS_UP  , MS_RGHT,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_BRID, KC_BRIU, KC_NO,
-                          _______, _______, _______,                            MS_BTN1, MS_BTN2, _______
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                               TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                          KC_ESC,  KC_SPC,  LT(MO(4), KC_LGUI),                 KC_NO,   KC_NO,   KC_NO
     ),
     [4] = LAYOUT_split_3x5_3(
-        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-        KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                          _______, _______, _______,                            _______, _______, _______
+        KC_NO,    KC_7,   KC_8,    KC_9,    KC_0,                               TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_M,     KC_4,   KC_5,    KC_6,    KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_B,     KC_1,   KC_2,    KC_3,    KC_F3,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                          _______, _______, _______,                            KC_NO,   KC_NO,   KC_NO
     )
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
-    if (clockwise) {
-        tap_code(KC_VOLU);
-    } else {
-        tap_code(KC_VOLD);
-    }
-  } else if (index == 1) { /* Second encoder */
-    if (clockwise) {
-        tap_code(MS_WHLU);
-    } else {
-        tap_code(MS_WHLD);
-    }
-  }
-  return false;
-}
+// Encoders
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)  },
+    [1] =   { ENCODER_CCW_CW(KC_NO,   KC_NO),   ENCODER_CCW_CW(KC_BRIU, KC_BRID)  },
+    [2] =   { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_NO, KC_NO)      },
+    [3] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_NO, KC_NO)      },
+    [4] =   { ENCODER_CCW_CW(KC_NO,   KC_NO),   ENCODER_CCW_CW(KC_NO, KC_NO)      },
+    //                  Encoder 1                                     Encoder 2
+};
+#endif
