7e0092594682f2097eb2b36d12d7c01f79455bf7
Assem Mohamed <126217978+ToYoNiX@users.noreply.github.com>
2026-02-26 14:07:48 +0200

    Update keymap.c

--- keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
+++ keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_ESC,  KC_SPC,  MO(1),            /*A3*/            MO(2),   KC_BSPC, KC_ENT
     ),
     [1] = LAYOUT_split_3x5_3(
-        KC_NO,   KC_7,    KC_8,    KC_9,    KC_0,                               KC_GRV,  KC_QUOT, KC_MINS, KC_EQL,  KC_NO,
-        KC_LSFT, KC_4,    KC_5,    KC_6,    KC_PSCR,                            KC_HOME, KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT,
-        KC_NO,   KC_1,    KC_2,    KC_3,    KC_DEL,                             KC_END,  KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
+        KC_TAB,   KC_7,    KC_8,    KC_9,    KC_0,                               KC_GRV,  KC_QUOT, KC_MINS, KC_EQL,  KC_NO,
+        KC_LSFT,  KC_4,    KC_5,    KC_6,    KC_PSCR,                            KC_HOME, KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT,
+        KC_LGUI,  KC_1,    KC_2,    KC_3,    KC_DEL,                             KC_END,  KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
                           _______, _______, _______,                            _______, _______, _______
     ),
     [2] = LAYOUT_split_3x5_3(
