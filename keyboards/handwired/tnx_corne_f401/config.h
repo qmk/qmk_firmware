48aeda3ea0190eb103165f94331741362b105574
Assem Mohamed <126217978+ToYoNiX@users.noreply.github.com>
2026-02-26 10:40:34 +0200

    Update keyboards/handwired/tnx_corne_f401/config.h - Removing Tap dance config and mouse config since My TD config is the default and I stopped using the mouse anyway.
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/handwired/tnx_corne_f401/config.h
+++ keyboards/handwired/tnx_corne_f401/config.h
@@ -11,18 +11,3 @@
 #define SERIAL_USART_TX_PIN      B6
 #define SERIAL_USART_RX_PIN      B7
 
-// Tap dance for HRM 
-#define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
-
-// Mouse key speed and acceleration 
-#undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY          0
-#undef MOUSEKEY_INTERVAL
-#define MOUSEKEY_INTERVAL       16
-#undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY    0
-#undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED      6
-#undef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX    64
