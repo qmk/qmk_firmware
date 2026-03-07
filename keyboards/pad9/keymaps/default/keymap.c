73297aed750a9bc87714a3cee3600c238b3b78a7
Hippo5412 <parkerlane003@gmail.com>
2026-03-07 14:25:35 -0600

    Fixed encoder layers.

--- keyboards/pad9/keymaps/default/keymap.c
+++ keyboards/pad9/keymaps/default/keymap.c
@@ -6,9 +6,6 @@
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [2] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  }
 };
 #endif
 
