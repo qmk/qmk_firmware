3db6ca18f389799fda34f6dcb62bf803a3a56fa1
Garth <garthbenson@gmail.com>
2026-03-07 00:50:34 -0600

    First commit#

---
+++ keyboards/keebio/iris/keymaps/Garth_42/rules.mk
@@ -0,0 +1,4 @@
+# Please set the following according to the selection of the hardware implementation option.
+# RGBLED_OPTION_TYPE = backlight   ## none, backlight or underglow
+
+TAP_DANCE_ENABLE = yes
