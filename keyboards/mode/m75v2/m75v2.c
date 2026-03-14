8e641f578d801fed2e63c1a8104185ffe9e8696e
Álvaro A. Volpato <alvaro.augusto.volpato@gmail.com>
2026-03-14 14:40:07 -0300

    Move color operation to get_hsv and color type
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/mode/m75v2/m75v2.c
+++ keyboards/mode/m75v2/m75v2.c
@@ -25,12 +25,9 @@ void keyboard_post_init_kb(void) {
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res) {
-        uint8_t h = rgblight_get_hue();
-        uint8_t s = rgblight_get_sat();
-        uint8_t v = rgblight_get_val();
-
-        led_state.caps_lock ? rgblight_sethsv_at(h,s,v, 37) : rgblight_sethsv_at(HSV_OFF, 37); 
-        led_state.caps_lock ? rgblight_sethsv_at(h,s,v, 38) : rgblight_sethsv_at(HSV_OFF, 38); 
+        hsv_t current_color = rgblight_get_hsv();
+        led_state.caps_lock ? rgblight_sethsv_at(current_color.h, current_color.s, current_color.v, 37) : rgblight_sethsv_at(HSV_OFF, 37); 
+        led_state.caps_lock ? rgblight_sethsv_at(current_color.h, current_color.s, current_color.v, 38) : rgblight_sethsv_at(HSV_OFF, 38); 
     }   
     return res;
 }
