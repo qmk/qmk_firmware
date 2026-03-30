13d71c30433eaa4cb5c321fd6dfa5087808cc1bb
Dominic <59139317+DoomishGuy@users.noreply.github.com>
2026-03-30 15:39:37 +1100

    Apply suggestions from code review
    Co-authored-by: Jack Sangdahl <jack@pngu.org>

--- keyboards/alatus/keymaps/default/keymap.c
+++ keyboards/alatus/keymaps/default/keymap.c
@@ -1,18 +1,5 @@
-/* Copyright 2026 Dominic Yeap-Holliday <DoomishGuy>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+// Copyright 2026 Dominic Yeap-Holliday <DoomishGuy>
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
@@ -22,20 +9,6 @@ enum layers {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * QWERTY Layer
-     *
-     * .-----------------------------------.    .--------------------------------.
-     * |  Q  |   W  |   E  |   R   |   T   |    |  Y  |   U  |   I  |   O  |  P  |
-     * |-----+------+------+-------+-------|    |-----+------+------+------+-----|
-     * |  A  |   S  |   D  |   F   |   G   |    |  H  |   J  |   K  |   L  | ; : |
-     * |-----+------+------+-------+-------|    |-----+------+------+------+-----|
-     * |  Z  |   X  |   C  |   V   |   B   |    |  N  |   M  |  , < |  . > | / ? |
-     * '-----+------+------+-------+-------|    |-----+------+------+------+-----'
-     *       | LCTL | LGUI | Space | Enter |    | Tab | BSPC | RSFT | RALT |
-     *       '-----------------------------'    '--------------------------'
-     */
-
     [_QWERTY] = LAYOUT(
         KC_Q, KC_W,    KC_E,    KC_R,     KC_T,        KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,
         KC_A, KC_S,    KC_D,    KC_F,     KC_G,        KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,
