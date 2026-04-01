6d2006b52640162f7d23e739b0f00674bc683ae7
Assem Mohamed <126217978+ToYoNiX@users.noreply.github.com>
2026-04-01 11:11:51 +0200

    removing the hardcoded number on encoders in the encoders map.
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
+++ keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Encoders
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(MS_WHLD, MS_WHLU)  },
     [1] =   { ENCODER_CCW_CW(KC_NO,   KC_NO),   ENCODER_CCW_CW(KC_BRIU, KC_BRID)  },
     [2] =   { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_NO, KC_NO)      },
