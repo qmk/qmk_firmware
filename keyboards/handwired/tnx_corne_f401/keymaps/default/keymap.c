27792ae3435fad870c13d090c003e3fdb0f01013
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2026-04-01 11:20:32 +0200

    fixing the invalid keymab.

--- keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
+++ keyboards/handwired/tnx_corne_f401/keymaps/default/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                               TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                          KC_ESC,  KC_SPC,  LT(MO(4), KC_LGUI),                 KC_NO,   KC_NO,   KC_NO
+                          KC_ESC,  KC_SPC,  LT(4, KC_LGUI),                 KC_NO,   KC_NO,   KC_NO
     ),
     [4] = LAYOUT_split_3x5_3(
         KC_NO,    KC_7,   KC_8,    KC_9,    KC_0,                               TO(0),   KC_NO,   KC_NO,   KC_NO,   KC_NO,
