c52c12fef7ec557ebc63426acc17bf764817483d
Idle Builds <cvance84@hotmail.com>
2026-04-11 20:16:39 -0500

    Add Idle Builds Dinkey 32/30 and Dinkey 34 keyboards

---
+++ keyboards/idlebuilds/dinkey34/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Idle Builds (@IdleBuilds)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define EE_HANDS
+
+#define OLED_DISPLAY_128X32
+#define SPLIT_MODS_ENABLE
