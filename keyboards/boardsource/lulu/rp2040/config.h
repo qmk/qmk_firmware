00b3b1ab78f2efe06e3fdede01ad6df9e8557a5a
chikulla <3042031+chikulla@users.noreply.github.com>
2026-04-15 18:38:34 +0900

    adjust ENCODER_RESOLUTION

--- keyboards/boardsource/lulu/rp2040/config.h
+++ keyboards/boardsource/lulu/rp2040/config.h
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
+#define ENCODER_RESOLUTION 2
+
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
