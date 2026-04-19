6a03d9b0ca33e1eeb3e409162780d08ae500ee20
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2026-04-19 12:32:39 +0200

    add link65 keyboard

---
+++ keyboards/owlab/link65/hotswap/rules.mk
@@ -0,0 +1,10 @@
+MCU_LDSCRIPT = STM32F103x8_0x08006000_bootloader
+BOARD = STM32_F103_STM32DUINO
+# dfu start address for download firmware: 0x08006000
+DFU_ARGS = -d 1688:2220 -a 0 -s 0x08006000
+# PID
+DFU_SUFFIX_ARGS = -v 1688 -p 2220
+
+# Enter lower-power sleep mode when on the chibios idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+OPT_DEFS += -DBOOTLOADER_STM32DUINO
