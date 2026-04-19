48d22041088bde2dd9b269536fa892c73ac9fd01
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2026-04-19 12:43:26 +0200

    some cleanup

--- keyboards/owlab/link65/hotswap/hotswap.c
+++ keyboards/owlab/link65/hotswap/hotswap.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright 2022 Owlab
 
 This program is free software: you can redistribute it and/or modify
--- keyboards/owlab/link65/hotswap/rules.mk
+++ keyboards/owlab/link65/hotswap/rules.mk
@@ -1,6 +1,5 @@
 MCU_LDSCRIPT = STM32F103x8_0x08006000_bootloader
 BOARD = STM32_F103_STM32DUINO
-# dfu start address for download firmware: 0x08006000
 DFU_ARGS = -d 1688:2220 -a 0 -s 0x08006000
 # PID
 DFU_SUFFIX_ARGS = -v 1688 -p 2220
