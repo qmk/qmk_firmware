0edf52f0cdab2b7f4fe5c40f5b16c448cc75389d
Feags <41646528+Feags@users.noreply.github.com>
2026-04-25 10:51:05 -0500

    Adding Promenade RP24HS
    Adding keyboard.info, default keymap, readme, config, rules.

---
+++ keyboards/kradoindustries/promenade_rp24hs/config.h
@@ -0,0 +1,13 @@
+/* Copyright 2026 Matt F
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ */
+#pragma once
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+#define USB_SUSPEND_WAKEUP_DELAY 200
