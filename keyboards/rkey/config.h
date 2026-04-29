c12d7eab1ed1842afd1bff583c24be488a3441a0
rkey-device <rkey.device@gmail.com>
2026-04-29 13:57:28 +0700

    WIP

--- keyboards/rkey/config.h
+++ keyboards/rkey/config.h
@@ -1,33 +1,39 @@
 #pragma once
 
 // Timing for SK6812
- // #define WS2812_T1H 650
-#define WS2812_T1H 600
-//// #define WS2812_T1L 600
-#define WS2812_T0H 300
-//// #define WS2812_T0L 900
-//// #define WS2812_TIMING 1250
-#define WS2812_TRST_US 80
-#define WS2812_EXTERNAL_PULLUP
 
-//#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_GRB
+#undef WS2812_TRST_US
+#define WS2812_TRST_US 300 
+
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
+
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_GRB
 
 /* Underglow */
 #define WS2812_SPI_DRIVER           SPID1
 #define WS2812_SPI_MOSI_PAL_MODE    5
 // #define WS2812_SPI_SCK_PIN          A5
 // #define WS2812_SPI_SCK_PAL_MODE     5
-// #define WS2812_SPI_DIVISOR          16
+#undef WS2812_SPI_DIVISOR
+#define WS2812_SPI_DIVISOR          32
 #define TRANSIENT_EEPROM_SIZE       1024
-
-#define AUDIO_PIN A4
-
-#ifdef AUDIO_ENABLE
-# define STARTUP_SONG SONG(STARTUP_SOUND)
 
 /* Включаем поддержку хранения настроек */
 #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
 /* Если памяти мало, можно ограничить количество слоев для VIA (по умолчанию 4) */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
+#define VIA_RGB_MATRIX_ENABLE
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS // Нужно для красивых эффектов
+#define RGB_MATRIX_KEYPRESSES        // Нужно для реактивных эффектов
 
+#define RGBLED_NUM 14 // Укажите общее число диодов на плате
+#define DRIVER_LED_TOTAL RGBLED_NUM
+
+#define AUDIO_PIN A4
+
+#ifdef AUDIO_ENABLE
+# define STARTUP_SONG SONG(STARTUP_SOUND)
 #endif
+
+#define AUDIO_VOICES
+#define AUDIO_VOICE_DEFAULT something
--- keyboards/rkey/halconf.h
+++ keyboards/rkey/halconf.h
@@ -18,6 +18,8 @@
 
 #define HAL_USE_SPI TRUE
 
+#define HAL_USE_DMA TRUE
+// #define HAL_USE_PWM TRUE
 #define HAL_USE_DAC TRUE
 #define HAL_USE_GPT TRUE
 
