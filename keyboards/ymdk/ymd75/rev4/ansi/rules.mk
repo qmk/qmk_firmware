f0be087384e6698cc3dc43a5e1e022e7e7a2f925
Steffen Olszewski <steffen.o.dev@gmail.com>
2026-05-10 16:52:18 +0200

    Convert to Data Driven Configuration
    The conversion does fully replicate the current settings with the only exceptions
    of changing the manufacturer and version to be in line with the previous revisions
    and updating the maintainer.

--- keyboards/ymdk/ymd75/rev4/ansi/rules.mk
+++ keyboards/ymdk/ymd75/rev4/ansi/rules.mk
@@ -1,25 +1,2 @@
-# MCU name
-MCU = STM32F401
-
-# Bootloader selection
-# BOOTLOADER = stm32duino
-
-BOOTLOADER = tinyuf2
-
 CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
 QUANTUM_SRC += matrix.c
-
-#
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
-WS2812_DRIVER = pwm
-# Enter lower-power sleep mode when on the ChibiOS idle thread
