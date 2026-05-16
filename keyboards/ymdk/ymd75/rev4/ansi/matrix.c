b073600308f7e50151afd2392424f50b905b0a92
Steffen Olszewski <steffen.o.dev@gmail.com>
2026-05-16 05:11:54 +0200

    Simplify custom matrix code
    Remove unused code paths.

--- keyboards/ymdk/ymd75/rev4/ansi/matrix.c
+++ keyboards/ymdk/ymd75/rev4/ansi/matrix.c
@@ -3,110 +3,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "matrix.h"
 
-#ifdef DIRECT_PINS
-static pin_t direct_pins[MATRIX_ROWS][MATRIX_COLS] = DIRECT_PINS;
-#elif (DIODE_DIRECTION == ROW2COL) || (DIODE_DIRECTION == COL2ROW)
 static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
-#endif
-
-#ifdef DIRECT_PINS
-
-static void init_pins(void) {
-    for (int row = 0; row < MATRIX_ROWS; row++) {
-        for (int col = 0; col < MATRIX_COLS; col++) {
-            pin_t pin = direct_pins[row][col];
-            if (pin != NO_PIN) {
-                gpio_set_pin_input_high(pin);
-            }
-        }
-    }
-}
-
-static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
-    // Start with a clear matrix row
-    matrix_row_t current_row_value = 0;
-
-    for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
-        pin_t pin = direct_pins[current_row][col_index];
-        if (pin != NO_PIN) {
-            current_row_value |= gpio_read_pin(pin) ? 0 : (MATRIX_ROW_SHIFTER << col_index);
-        }
-    }
-
-    // If the row has changed, store the row and return the changed flag.
-    if (current_matrix[current_row] != current_row_value) {
-        current_matrix[current_row] = current_row_value;
-        return true;
-    }
-
-    return false;
-}
-
-#elif defined(DIODE_DIRECTION)
-#    if (DIODE_DIRECTION == COL2ROW)
-
-static void select_col(uint8_t col) {
-    palSetLine(col_pins[col]);
-}
-
-static void unselect_col(uint8_t col) {
-    palClearLine(col_pins[col]);
-}
-
-static void unselect_rows(void) {
-    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
-        palSetLineMode(row_pins[x], PAL_MODE_INPUT_PULLDOWN);
-    }
-}
-
-static void init_pins(void) {
-    unselect_rows();
-    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
-        palSetLineMode(col_pins[x], PAL_MODE_OUTPUT_PUSHPULL);
-    }
-    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
-        palClearLine(col_pins[x]);
-    }
-}
-
-static bool read_rows_on_col(matrix_row_t current_matrix[], uint8_t current_col) {
-    bool matrix_changed = false;
-
-    // Select col
-    select_col(current_col);
-    matrix_output_select_delay();
-
-    // For each row...
-    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
-        // Store last value of row prior to reading
-        matrix_row_t last_row_value    = current_matrix[row_index];
-        matrix_row_t current_row_value = last_row_value;
-
-        // Check row pin state
-        if (gpio_read_pin(row_pins[row_index]) == 1) {
-            // Pin HI, set col bit
-            current_row_value |= (MATRIX_ROW_SHIFTER << current_col);
-        } else {
-            // Pin LO, clear col bit
-            current_row_value &= ~(MATRIX_ROW_SHIFTER << current_col);
-        }
-
-        // Determine if the matrix changed state
-        if ((last_row_value != current_row_value)) {
-            matrix_changed |= true;
-            current_matrix[row_index] = current_row_value;
-        }
-    }
-
-    // Unselect col
-    unselect_col(current_col);
-    matrix_output_unselect_delay(current_col, matrix_changed); // wait for all Row signals to go HIGH
-
-    return matrix_changed;
-}
-
-#    elif (DIODE_DIRECTION == ROW2COL)
 
 static void select_row(uint8_t row) {
     palSetLine(row_pins[row]);
@@ -162,13 +60,6 @@ static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
     return false;
 }
 
-#    else
-#        error DIODE_DIRECTION must be one of COL2ROW or ROW2COL!
-#    endif
-#else
-#    error DIODE_DIRECTION is not defined!
-#endif
-
 void matrix_init_custom(void) {
     // initialize key pins
     init_pins();
@@ -177,17 +68,10 @@ void matrix_init_custom(void) {
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     bool changed = false;
 
-#if defined(DIRECT_PINS) || (DIODE_DIRECTION == COL2ROW)
-    // Set col, read rows
-    for (uint8_t current_col = 0; current_col < MATRIX_COLS; current_col++) {
-        changed |= read_rows_on_col(current_matrix, current_col);
-    }
-#elif (DIODE_DIRECTION == ROW2COL)
     // Set row, read cols
     for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
         changed |= read_cols_on_row(current_matrix, current_row);
     }
-#endif
 
     return changed;
 }
