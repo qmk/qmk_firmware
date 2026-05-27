79bd3d935d485451bcfb2359b00e885204290f31
Timo <76815855+tstrube@users.noreply.github.com>
2026-05-27 20:30:45 +0200

    added mouse functions as 3rd mode for joystick. added usb wakeup delay

--- keyboards/tsc/tiwaz/keymaps/default/keymap.c
+++ keyboards/tsc/tiwaz/keymaps/default/keymap.c
@@ -10,16 +10,17 @@
 #include "transactions.h"
 
 // Joysticks
-#define EEPROM_ADDR_JOY_LEFT      0
-#define EEPROM_ADDR_JOY_RIGHT     1
-#define LED_INDEX_JOY_MODE_LEFT   5
-#define LED_INDEX_JOY_MODE_RIGHT 49
-#define JOY_POLLING_INTERVAL     10
+#define EEPROM_ADDR_JOY_LEFT       0
+#define EEPROM_ADDR_JOY_RIGHT      1
+#define LED_INDEX_JOY_MODE_LEFT    5
+#define LED_INDEX_JOY_MODE_RIGHT  49
+#define JOY_POLLING_INTERVAL      10
+#define MOUSE_BUTTON_HOLD_MS     500
 
-#define JOY_ADC_SHIFT           2
-#define JOY_CENTER            127
-#define JOY_DIGITAL_ON         50
-#define JOY_DIGITAL_OFF        40
+#define JOY_ADC_SHIFT              2
+#define JOY_CENTER               127
+#define JOY_DIGITAL_ON            50
+#define JOY_DIGITAL_OFF           40
 
 #define LJOY_UP_POS    (keypos_t){0, 5}
 #define LJOY_DOWN_POS  (keypos_t){1, 5}
@@ -40,10 +41,17 @@
 typedef union {
     uint8_t raw;
     struct {
-        bool joystick_digital_left :1;
-        bool joystick_digital_right :1;
+        uint8_t joystick_mode_left :2;  // 0=digital, 1=analog, 2=mouse
+        uint8_t joystick_mode_right :2; // 0=digital, 1=analog, 2=mouse
     };
 } user_config_t;
+
+// Joystick modes
+enum joystick_mode {
+    JOY_MODE_DIGITAL = 0,
+    JOY_MODE_ANALOG = 1,
+    JOY_MODE_MOUSE = 2
+};
 
 typedef struct {
     bool up;
@@ -60,9 +68,19 @@ typedef struct {
 } joystick_adc_t;
 
 user_config_t user_config = {
-    .joystick_digital_left = true,
-    .joystick_digital_right = true
+    .joystick_mode_left = JOY_MODE_DIGITAL,
+    .joystick_mode_right = JOY_MODE_DIGITAL
 };
+
+// Button hold tracking for mouse mode
+typedef struct {
+    uint32_t press_time;
+    bool button_registered;  // Track if we've registered a mouse button
+    bool motion_detected;
+} button_hold_t;
+
+button_hold_t local_button_hold = {0, false, false};
+button_hold_t remote_button_hold = {0, false, false};
 
 joystick_state_t local_joystick_state = {
     .up = false,
@@ -114,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,KC_PSLS,
         RM_NEXT,RM_SPDU,RM_HUEU,RM_SATU,RM_VALU,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_P4  ,KC_P5  ,KC_P6  ,KC_PPLS,KC_PAST,
         RM_PREV,RM_SPDD,RM_HUED,RM_SATD,RM_VALD,XXXXXXX,XXXXXXX,MD_JOYL,XXXXXXX,XXXXXXX,MD_JOYR,XXXXXXX,XXXXXXX,KC_P1  ,KC_P2  ,KC_P3  ,KC_PENT,KC_PAST,
-        KC_AGIN,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX        ,XXXXXXX,XXXXXXX        ,XXXXXXX,XXXXXXX,TG(1)  ,KC_P0  ,KC_COMM,KC_PENT,KC_NUM ,
+        KC_AGIN,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX        ,XXXXXXX,XXXXXXX        ,XXXXXXX,XXXXXXX,TG(1)  ,KC_P0  ,KC_PDOT,KC_PENT,KC_NUM ,
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX
     )
 };
@@ -128,12 +146,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
             case MD_JOYL:
-                user_config.joystick_digital_left = !user_config.joystick_digital_left;
+                user_config.joystick_mode_left = (user_config.joystick_mode_left + 1) % 3;
                 save_user_eeprom();
                 return false;
 
             case MD_JOYR:
-                user_config.joystick_digital_right = !user_config.joystick_digital_right;
+                user_config.joystick_mode_right = (user_config.joystick_mode_right + 1) % 3;
                 save_user_eeprom();
                 return false;
         }
@@ -141,27 +159,45 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-void set_status_led(uint8_t i, bool digital) {
-    hsv_t hsv = digital ? (hsv_t){HSV_BLUE} : (hsv_t){HSV_YELLOW};
-    int brightness = rgb_matrix_get_val();
-    if (hsv.v > brightness) hsv.v = brightness;
-    rgb_t rgb = hsv_to_rgb(hsv);
-    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-}
 
 bool rgb_matrix_indicators_user(void) {
     if (get_highest_layer(layer_state) == 1) {
-        set_status_led(LED_INDEX_JOY_MODE_LEFT, is_keyboard_left() ? user_config.joystick_digital_left : user_config.joystick_digital_right);
-        set_status_led(LED_INDEX_JOY_MODE_RIGHT, is_keyboard_left() ? user_config.joystick_digital_left : user_config.joystick_digital_right);
+        uint8_t mode_left = is_keyboard_left() ? user_config.joystick_mode_left : user_config.joystick_mode_right;
+        uint8_t mode_right = is_keyboard_left() ? user_config.joystick_mode_left : user_config.joystick_mode_right;
+        
+        hsv_t hsv_left, hsv_right;
+        
+        // Set LED colors based on mode: 0=digital(blue), 1=analog(yellow), 2=mouse(red)
+        switch(mode_left) {
+            case JOY_MODE_MOUSE: hsv_left = (hsv_t){HSV_RED}; break;
+            case JOY_MODE_ANALOG: hsv_left = (hsv_t){HSV_YELLOW}; break;
+            default: hsv_left = (hsv_t){HSV_BLUE}; break;
+        }
+        
+        switch(mode_right) {
+            case JOY_MODE_MOUSE: hsv_right = (hsv_t){HSV_RED}; break;
+            case JOY_MODE_ANALOG: hsv_right = (hsv_t){HSV_YELLOW}; break;
+            default: hsv_right = (hsv_t){HSV_BLUE}; break;
+        }
+        
+        int brightness = rgb_matrix_get_val();
+        if (hsv_left.v > brightness) hsv_left.v = brightness;
+        if (hsv_right.v > brightness) hsv_right.v = brightness;
+        
+        rgb_t rgb_left = hsv_to_rgb(hsv_left);
+        rgb_t rgb_right = hsv_to_rgb(hsv_right);
+        
+        rgb_matrix_set_color(LED_INDEX_JOY_MODE_LEFT, rgb_left.r, rgb_left.g, rgb_left.b);
+        rgb_matrix_set_color(LED_INDEX_JOY_MODE_RIGHT, rgb_right.r, rgb_right.g, rgb_right.b);
     }
     return false;
 }
 
-static void handle_joystick(bool digital_mode, joystick_adc_t* adc, joystick_state_t* state, int js_btn,
-                            keypos_t up, keypos_t down, keypos_t left, keypos_t right, keypos_t btn) {
+static void handle_joystick(uint8_t mode, joystick_adc_t* adc, joystick_state_t* state, button_hold_t* btn_hold,
+                            int js_btn, keypos_t up, keypos_t down, keypos_t left, keypos_t right, keypos_t btn) {
     layer_state_t layer = get_highest_layer(layer_state | default_layer_state);
 
-    if (digital_mode) { // DIGITAL mode
+    if (mode == JOY_MODE_DIGITAL) { // DIGITAL mode
         int8_t sx = (int8_t)adc->x;
         int8_t sy = (int8_t)adc->y;
 
@@ -210,13 +246,103 @@ static void handle_joystick(bool digital_mode, joystick_adc_t* adc, joystick_sta
             state->btn = false;
         }
 
-    } else { // ANALOG mode
+    } else if (mode == JOY_MODE_ANALOG) { // ANALOG mode
         if (adc->btn && !state->btn) {
             register_joystick_button(js_btn);
             state->btn = true;
         } else if (!adc->btn && state->btn) {
             unregister_joystick_button(js_btn);
             state->btn = false;
+        }
+
+    } else if (mode == JOY_MODE_MOUSE) { // MOUSE mode
+        // Map analog values to mouse movement keycodes
+        int8_t sx = (int8_t)adc->x;
+        int8_t sy = (int8_t)adc->y;
+        
+        // Detect motion while button is held
+        if (state->btn && !btn_hold->motion_detected) {
+            if (sx < -JOY_DIGITAL_ON || sx > JOY_DIGITAL_ON || 
+                sy < -JOY_DIGITAL_ON || sy > JOY_DIGITAL_ON) {
+                btn_hold->motion_detected = true;
+            }
+        }
+        
+        // Handle X-axis (LEFT/RIGHT)
+        if (sx < -JOY_DIGITAL_ON && !state->left) {
+            register_code(MS_LEFT);
+            state->left = true;
+        } else if (sx > -JOY_DIGITAL_OFF && state->left) {
+            unregister_code(MS_LEFT);
+            state->left = false;
+        }
+        
+        if (sx > JOY_DIGITAL_ON && !state->right) {
+            register_code(MS_RGHT);
+            state->right = true;
+        } else if (sx < JOY_DIGITAL_OFF && state->right) {
+            unregister_code(MS_RGHT);
+            state->right = false;
+        }
+        
+        // Handle Y-axis (UP/DOWN)
+        if (sy < -JOY_DIGITAL_ON && !state->up) {
+            register_code(MS_UP);
+            state->up = true;
+        } else if (sy > -JOY_DIGITAL_OFF && state->up) {
+            unregister_code(MS_UP);
+            state->up = false;
+        }
+        
+        if (sy > JOY_DIGITAL_ON && !state->down) {
+            register_code(MS_DOWN);
+            state->down = true;
+        } else if (sy < JOY_DIGITAL_OFF && state->down) {
+            unregister_code(MS_DOWN);
+            state->down = false;
+        }
+
+        // Button handling with hold detection and motion lockout
+        if (adc->btn && !state->btn) {
+            // Button pressed - don't register yet, wait to see if there's motion or timeout
+            btn_hold->press_time = timer_read32();
+            btn_hold->button_registered = false;
+            btn_hold->motion_detected = false;
+            state->btn = true;
+        } else if (!adc->btn && state->btn) {
+            // Button released
+            if (btn_hold->button_registered) {
+                // A button was registered, unregister it
+                if (btn_hold->motion_detected) {
+                    unregister_code(MS_BTN1);  // Was left-click
+                } else {
+                    unregister_code(MS_BTN2);  // Was right-click
+                }
+            } else {
+                // Button was released before any button was registered (quick click)
+                // Treat as left-click
+                register_code(MS_BTN1);
+                unregister_code(MS_BTN1);
+            }
+            state->btn = false;
+            btn_hold->button_registered = false;
+            btn_hold->motion_detected = false;
+        } else if (adc->btn && state->btn) {
+            // Button held - decide which button to register based on motion or timeout
+            if (!btn_hold->button_registered) {
+                // Check for motion
+                if (sx < -JOY_DIGITAL_ON || sx > JOY_DIGITAL_ON || 
+                    sy < -JOY_DIGITAL_ON || sy > JOY_DIGITAL_ON) {
+                    // Motion detected - register left-click
+                    btn_hold->motion_detected = true;
+                    register_code(MS_BTN1);
+                    btn_hold->button_registered = true;
+                } else if (timer_elapsed32(btn_hold->press_time) >= MOUSE_BUTTON_HOLD_MS) {
+                    // 500ms timeout without motion - register right-click
+                    register_code(MS_BTN2);
+                    btn_hold->button_registered = true;
+                }
+            }
         }
     }
 }
@@ -264,23 +390,27 @@ void housekeeping_task_user(void) {
         };
 
         if (is_keyboard_left()) {
-            handle_joystick(user_config.joystick_digital_left, &local_joystick_axis, &local_joystick_state,
+            handle_joystick(user_config.joystick_mode_left, &local_joystick_axis, &local_joystick_state, &local_button_hold,
                 JS_0, LJOY_UP_POS, LJOY_DOWN_POS, LJOY_LEFT_POS, LJOY_RIGHT_POS, LJOY_BTN_POS);
-            handle_joystick(user_config.joystick_digital_right, &remote_joystick_axis, &remote_joystick_state,
+            handle_joystick(user_config.joystick_mode_right, &remote_joystick_axis, &remote_joystick_state, &remote_button_hold,
                 JS_1, RJOY_UP_POS, RJOY_DOWN_POS, RJOY_LEFT_POS, RJOY_RIGHT_POS, RJOY_BTN_POS);
-            joystick_set_axis(0, user_config.joystick_digital_left  ? 0 : local_joystick_axis.x);
-            joystick_set_axis(1, user_config.joystick_digital_left  ? 0 : local_joystick_axis.y);
-            joystick_set_axis(2, user_config.joystick_digital_right ? 0 : remote_joystick_axis.x);
-            joystick_set_axis(3, user_config.joystick_digital_right ? 0 : remote_joystick_axis.y);
+            
+            // Set joystick axes only for analog mode
+            joystick_set_axis(0, user_config.joystick_mode_left == JOY_MODE_ANALOG ? local_joystick_axis.x : 0);
+            joystick_set_axis(1, user_config.joystick_mode_left == JOY_MODE_ANALOG ? local_joystick_axis.y : 0);
+            joystick_set_axis(2, user_config.joystick_mode_right == JOY_MODE_ANALOG ? remote_joystick_axis.x : 0);
+            joystick_set_axis(3, user_config.joystick_mode_right == JOY_MODE_ANALOG ? remote_joystick_axis.y : 0);
         } else {
-            handle_joystick(user_config.joystick_digital_left, &remote_joystick_axis, &remote_joystick_state,
+            handle_joystick(user_config.joystick_mode_left, &remote_joystick_axis, &remote_joystick_state, &remote_button_hold,
                 JS_0, LJOY_UP_POS, LJOY_DOWN_POS, LJOY_LEFT_POS, LJOY_RIGHT_POS, LJOY_BTN_POS);
-            handle_joystick(user_config.joystick_digital_right, &local_joystick_axis, &local_joystick_state,
+            handle_joystick(user_config.joystick_mode_right, &local_joystick_axis, &local_joystick_state, &local_button_hold,
                 JS_1, RJOY_UP_POS, RJOY_DOWN_POS, RJOY_LEFT_POS, RJOY_RIGHT_POS, RJOY_BTN_POS);
-            joystick_set_axis(0, user_config.joystick_digital_left  ? 0 : remote_joystick_axis.x);
-            joystick_set_axis(1, user_config.joystick_digital_left  ? 0 : remote_joystick_axis.y);
-            joystick_set_axis(2, user_config.joystick_digital_right ? 0 : local_joystick_axis.x);
-            joystick_set_axis(3, user_config.joystick_digital_right ? 0 : local_joystick_axis.y);
+            
+            // Set joystick axes only for analog mode
+            joystick_set_axis(0, user_config.joystick_mode_left == JOY_MODE_ANALOG ? remote_joystick_axis.x : 0);
+            joystick_set_axis(1, user_config.joystick_mode_left == JOY_MODE_ANALOG ? remote_joystick_axis.y : 0);
+            joystick_set_axis(2, user_config.joystick_mode_right == JOY_MODE_ANALOG ? local_joystick_axis.x : 0);
+            joystick_set_axis(3, user_config.joystick_mode_right == JOY_MODE_ANALOG ? local_joystick_axis.y : 0);
         }
     } else {
         remote_joystick_axis = (joystick_adc_t){
