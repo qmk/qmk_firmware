f358495f10331e641a25ea895e0ab49f035eaa99
santiago garcia <santyg81855@gmail.com>
2026-06-18 23:17:16 -0400

    changed default keymap to use qwerty

--- keyboards/santy65v2/keymaps/default/keymap.c
+++ keyboards/santy65v2/keymaps/default/keymap.c
@@ -6,12 +6,12 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_F15, KC_ESC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, G(KC_BSLS), A(KC_END), KC_TRNS, 
-        KC_F16, KC_TAB, KC_SCLN, KC_COMMA, KC_DOT, KC_P, KC_Y, 		 		   		KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSLS, KC_LBRC, KC_RBRC, KC_TRNS, 
-        KC_F17, KC_BSPC, KC_A, KC_O, KC_E, KC_U, KC_I, 		 						KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, KC_ENT, KC_TRNS, 
-        KC_F18, KC_RSFT, KC_QUOTE, KC_Q, KC_J, KC_K, KC_X, 						KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, KC_UP, 
-        KC_TRNS, KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE, KC_SPACE, KC_TRNS,		KC_SPACE, KC_SPACE, KC_MPLY, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RIGHT
-    )
+	    KC_F15, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,                         KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_TRNS, 
+	    KC_F16, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS, 
+	    KC_F17, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,                              KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_TRNS, 
+	    KC_F18, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                              KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, 
+	    KC_TRNS, KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE, KC_SPACE, KC_TRNS,		        KC_SPACE, KC_SPACE, KC_MPLY, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RIGHT
+	)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
