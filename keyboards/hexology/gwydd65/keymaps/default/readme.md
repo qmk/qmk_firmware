c581850b35f87e026c5ee6631fe7dda05e61709e
Aeonstrife <j.v.jeoffer@gmail.com>
2026-07-05 23:38:17 +0530

    Add Hexology Gwydd65
    Add support for the Hexology Gwydd65 keyboard.

--- keyboards/hexology/gwydd65/keymaps/default/readme.md
+++ keyboards/hexology/gwydd65/keymaps/default/readme.md
@@ -1,3 +1,3 @@
-# Default Potato65 Layout
+# Default Gwydd65 Layout
 
-This is the default layout for the Potato65. Largely based on the Tada68 layout.
+This is the default layout for the Hexology Gwydd65.
--- keyboards/hexology/gwydd65/rules.mk
+++ keyboards/hexology/gwydd65/rules.mk
@@ -1,6 +1,1 @@
 BOARD = GENERIC_RP_RP2040
-
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-WS2812_DRIVER = vendor
