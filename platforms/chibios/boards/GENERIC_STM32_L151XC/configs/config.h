2eb4694ff4778483c4f986515b3cd3979259d4e7
Harrison <contact@htv04.com>
2026-07-12 17:52:21 -0400

    Add STM32L151xC support

---
+++ platforms/chibios/boards/GENERIC_STM32_L151XC/configs/config.h
@@ -0,0 +1,7 @@
+// Copyright 2026 htv04
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#ifndef EARLY_INIT_PERFORM_BOOTLOADER_JUMP
+#    define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
+#endif
