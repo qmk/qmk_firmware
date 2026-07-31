46c8e6e1aa3eea6cf27e360e3592f1599ed984cc
heerelectronics <heerelectronicx@gmail.com>
2026-07-31 13:03:22 +0530

    Update keyboards/heer_s1/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/heer_s1/keymaps/default/keymap.c
+++ keyboards/heer_s1/keymaps/default/keymap.c
@@ -37,7 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_DEL,     KC_HOME,    KC_END,     TO(L_SYS)
     ),
 
-    [L_SYS] = LAYOUT_ortho_4x4(
+    [L_SYS] = LAYOUT(
+        TO(L_BASE), TO(L_BASE),
         KC_F1,   KC_F2,   KC_F3,   KC_F4,
         KC_F5,   KC_F6,   KC_F7,   KC_F8,
         KC_F9,   KC_F10,  KC_F11,  KC_F12,
