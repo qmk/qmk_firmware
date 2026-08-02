d82123597a53bd2b0db93bb68358865648333ba3
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-08-01 21:54:30 -0500

    Update keyboards/corepad_technologies_inc/corepad_v1/keymaps/vial/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/corepad_technologies_inc/corepad_v1/keymaps/vial/keymap.c
+++ keyboards/corepad_technologies_inc/corepad_v1/keymaps/vial/keymap.c
@@ -17,47 +17,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MSTP
 ),
 
-[1] = LAYOUT(
-    KC_TRNS,
-    KC_TRNS,
-    KC_TRNS
-),
-
-[2] = LAYOUT(
-    KC_TRNS,
-    KC_TRNS,
-    KC_TRNS
-),
-
-[3] = LAYOUT(
-    KC_TRNS,
-    KC_TRNS,
-    KC_TRNS
-),
-
-[4] = LAYOUT(
-    KC_TRNS,
-    KC_TRNS,
-    KC_TRNS
-),
-
-[5] = LAYOUT(
-    KC_TRNS,
-    KC_TRNS,
-    KC_TRNS
-),
-
-[6] = LAYOUT(
-    KC_TRNS,
-    KC_TRNS,
-    KC_TRNS
-),
-
-[7] = LAYOUT(
-    KC_TRNS,
-    KC_TRNS,
-    KC_TRNS
-)
 
 };
 
