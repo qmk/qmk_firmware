135d0e04a8da4c23a9f8efc71f4bf3aacc9893e9
Drashna Jael're <drashna@drashna.net>
2026-08-02 17:03:51 -0700

    Wrap dict better

--- quantum/autocorrect.c
+++ quantum/autocorrect.c
@@ -85,7 +85,7 @@ void autocorrect_dict_cycle(bool forward) {
     if (forward) {
         autocorrect_config.current_dict = (autocorrect_config.current_dict + 1) % number_dicts;
     } else {
-        autocorrect_config.current_dict = (autocorrect_config.current_dict - 1) % number_dicts;
+        autocorrect_config.current_dict = autocorrect_config.current_dict ? (autocorrect_config.current_dict - 1) : (number_dicts - 1);
     }
     eeconfig_update_autocorrect(&autocorrect_config);
     autocorrect_init_dict();
