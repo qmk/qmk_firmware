a754b52d2b012ac25e12bd0831e775455fc6416a
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-08-06 12:35:39 -0500

    Update keyboards/corepad_technologies_inc/corepad_v1/config.h
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/corepad_technologies_inc/corepad_v1/config.h
+++ keyboards/corepad_technologies_inc/corepad_v1/config.h
@@ -18,24 +18,4 @@
 #define I2C1_SDA_PIN GP6
 #define I2C1_SCL_PIN GP7
 
-#define I2C1_SDA_PAL_MODE 3
-#define I2C1_SCL_PAL_MODE 3
-
-#define OLED_DISPLAY_ADDRESS 0x3C
 #define OLED_UPDATE_INTERVAL 20
-// #define OLED_TIMEOUT 60000
-
-/* --------------------------------------------------------------------------
- * WS2812 RGB LED
- *
- * Data  = GP12
- * Enable = GP11 (enabled in keyboard_pre_init_kb(), see corepad_v1.c)
- * -------------------------------------------------------------------------- */
-
-/* --------------------------------------------------------------------------
- * Direct Matrix
- *
- * SW1 = GP3
- * SW2 = GP4
- * SW3 = GP2
- * -------------------------------------------------------------------------- */
