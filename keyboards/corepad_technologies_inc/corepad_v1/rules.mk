fdd731bd637aeff0d7d82bad3082a0e23828b259
Albert Phu <127133346+albertphu07@users.noreply.github.com>
2026-08-06 09:14:20 -0500

    Add files via upload

--- keyboards/corepad_technologies_inc/corepad_v1/rules.mk
+++ keyboards/corepad_technologies_inc/corepad_v1/rules.mk
@@ -4,7 +4,5 @@
 # "features" in keyboard.json - don't redeclare them here, it just
 # produces "specified in both" warnings.
 MOUSEKEY_ENABLE = no
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
 
 LTO_ENABLE = yes
