0645d453911bf1a3f2d078fb39ac6449f25cd762
Torsten Schmitz <git@torsten.dev>
2026-08-08 19:41:28 +0200

    Use enums. with custom extract/generate's
    Made magic values proper enums requiring custom extraction/generation
    logic to make the uppercase convention work with the lowercase info.json
    excpectation.
    
    Since it makes sense to write ANSI,ISO etc. uppercase this makes them
    partially case insensitive by allowing full upper and fully lowercased
    variants in the schema. Json schema does not currently offer a simple
    clean way to allow case insensitive string enum's, sadly.

--- lib/python/qmk/cli/generate/config_h.py
+++ lib/python/qmk/cli/generate/config_h.py
@@ -169,16 +169,27 @@ def generate_led_animations_config(feature, led_feature_json, config_h_lines, en
     for animation in led_feature_json.get('animations', {}):
         config_h_lines.append(generate_flag(f'{enable_prefix}{animation.upper()}', led_feature_json['animations'][animation]))
 
-def generate_extended_attributes_config(extended_attrs, config_h_lines: list[str], postfix=''):
+
+def generate_extended_attributes_config(extended_attrs, config_h_lines: list[str]):
     if len(extended_attrs) > 0 and 'enabled' not in extended_attrs:
         config_h_lines.append(generate_define('EXTENDED_ATTRIBUTES_ENABLE'))
 
     if 'layout' in extended_attrs:
         layout = extended_attrs['layout']
-        if isinstance(layout, int) or layout.startswith("0x"):
+        if isinstance(layout, int) or layout.startswith("0"):
+            if isinstance(layout, str):
+                layout = int(layout, base=0)
+            if layout not in range(1, 256):
+                cli.log.error(f"keyboard vendor layout is specified but invalid, {layout} is outside the range(1,256)")
             config_h_lines.append(generate_define('KEYBOARD_EXT_ATTR_VENDOR_LAYOUT', layout))
         else:
-            config_h_lines.append(generate_define('KEYBOARD_EXT_ATTR_PHYSICAL_LAYOUT', layout))
+            config_h_lines.append(generate_define('KEYBOARD_EXT_ATTR_PHYSICAL_LAYOUT', layout.upper()))
+    # uppercase keys used in enums
+    if 'form_factor' in extended_attrs:
+        config_h_lines.append(generate_define('KEYBOARD_EXT_ATTR_FORM_FACTOR', extended_attrs['form_factor'].upper()))
+    if 'key_travel' in extended_attrs:
+        config_h_lines.append(generate_define('KEYBOARD_EXT_ATTR_KEY_TYPE', extended_attrs['key_travel'].upper()))
+
 
 def generate_stenography_config(kb_info_json, config_h_lines):
     """Generate the config.h lines for stenography."""
