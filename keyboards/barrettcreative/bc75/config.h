f40be891efd438dec91907b148f7a77e5e6b4497
gregandcin <gregandcinbusiness@gmail.com>
2026-08-15 17:52:46 -0400

    [Keyboard] Add Barrett Creative BC-75

---
+++ keyboards/barrettcreative/bc75/config.h
@@ -0,0 +1,11 @@
+// Copyright 2024-2026 DailyDesk, gregnadinc <gregandcinbusiness@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#undef I2C_DRIVER
+#define I2C_DRIVER I2CD0
+#undef I2C1_SCL_PIN
+#define I2C1_SCL_PIN GP1
+#undef I2C1_SDA_PIN
+#define I2C1_SDA_PIN GP0
---
+++ keyboards/barrettcreative/bc75/mcuconf.h
@@ -0,0 +1,12 @@
+// Copyright 2024-2026 DailyDesk, gregnadinc <gregandcinbusiness@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef RP_I2C_USE_I2C1
+#define RP_I2C_USE_I2C1 FALSE
+
+#undef RP_I2C_USE_I2C0
+#define RP_I2C_USE_I2C0 TRUE
