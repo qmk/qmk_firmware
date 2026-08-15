42db88e4a4f3005bf67ad8f469591275e134d162
gregandcin <gregandcinbusiness@gmail.com>
2026-08-15 19:04:15 -0400

    fix: copyright names

--- keyboards/barrettcreative/bc75/halconf.h
+++ keyboards/barrettcreative/bc75/halconf.h
@@ -1,4 +1,4 @@
-// Copyright 2024-2026 DailyDesk, gregandcin <gregandcinbusiness@gmail.com>
+// Copyright 2024-2026 DeskDaily (@DeskDaily), gregandcin <gregandcinbusiness@gmail.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
