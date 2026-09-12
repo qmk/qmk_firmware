de5af28ef5734134e5fcdf2666ebb66ba4d5e120
andrew morton <drewish@katherinehouse.com>
2026-09-11 21:21:16 -0600

    Add support for JLW's Le Chiffre 36

---
+++ keyboards/jlw/le_chiffre_36/keymaps/default/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// Comment these out if you don't want front indicators leds
+#define RGB_MODS_INDICATOR_ENABLE
+#define RGB_LAYER_INDICATOR_ENABLE
+
+#define RGB_INDICATOR_BRIGHTNESS 8
+#define RGB_TRIGGER_ON_KEYDOWN
+
+#define NO_AUTO_SHIFT_ALPHA
