c4dcb32491163834bd99a6d5930bc10c6969981e
gejigeji-com <28683785+gejigeji-com@users.noreply.github.com>
2026-09-19 07:10:36 +0900

    Update keyboards/gejigeji/gejipad_8s4re/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/gejigeji/gejipad_8s4re/keymaps/default/keymap.c
+++ keyboards/gejigeji/gejipad_8s4re/keymaps/default/keymap.c
@@ -4,8 +4,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        /* SW6   SW7   SW8   SW9   SW10  SW11  SW12  SW13 */
-        KC_1,    KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8
     )
 };
 
