5903997c29522889ed41c2ccc748852883f0129b
andrew morton <drewish@katherinehouse.com>
2026-09-19 09:18:56 -0600

    Move the RBG status indicators from keymap to keyboard

--- keyboards/jlw/le_chiffre_36/keymaps/default/keymap.c
+++ keyboards/jlw/le_chiffre_36/keymaps/default/keymap.c
@@ -14,10 +14,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_3thumb(
-         KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,             KC_MUTE,  KC_J,   KC_L,   KC_U,    KC_Y,   KC_QUOT,
-         HM_A,   HM_R,   HM_S,   KC_T,   KC_G,                       KC_M,   KC_N,   HM_E,    HM_I,   HM_O,
-         KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,                       KC_K,   KC_H,   KC_COMM, KC_DOT, KC_SLSH,
-                LT(3, KC_TAB), KC_RALT, SFT_T(KC_BSPC),          LT(1, KC_SPC), KC_LGUI, LT(2,KC_ENT)
+         KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_MUTE,  KC_J,   KC_L,   KC_U,    KC_Y,   KC_QUOT,
+         HM_A,     HM_R,     HM_S,     KC_T,     KC_G,               KC_M,   KC_N,   HM_E,    HM_I,   HM_O,
+         KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,               KC_K,   KC_H,   KC_COMM, KC_DOT, KC_SLSH,
+                 LT(3, KC_TAB), KC_RALT, SFT_T(KC_BSPC),             LT(1, KC_SPC), KC_LGUI, LT(2,KC_ENT)
     ),
 
     [1] = LAYOUT_3thumb(
@@ -71,52 +71,3 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
         COMBO(combo_del, KC_DEL),
     };
 #endif
-
-// RGB settings for indicator lights
-// Layer and Mods indicator
-#define LED_CENTER_TOP 1
-#define LED_CENTER_BOTTOM 0
-
-#define LAYER_R layer_colors[layer][0] *  RGB_INDICATOR_BRIGHTNESS / 255
-#define LAYER_G layer_colors[layer][1] *  RGB_INDICATOR_BRIGHTNESS / 255
-#define LAYER_B layer_colors[layer][2] *  RGB_INDICATOR_BRIGHTNESS / 255
-
-#define MODS_ACTIVE(mods) \
-    ((get_mods()|get_oneshot_mods()) & MOD_MASK_##mods ? RGB_INDICATOR_BRIGHTNESS:0)
-#define SHIFT_ACTIVE (get_mods() & MOD_MASK_SHIFT ? RGB_INDICATOR_BRIGHTNESS/4:0)
-#define MODS_R MODS_ACTIVE(CTRL) + SHIFT_ACTIVE
-#define MODS_G MODS_ACTIVE(GUI) + SHIFT_ACTIVE
-#define MODS_B MODS_ACTIVE(ALT) + SHIFT_ACTIVE
-
-const uint8_t PROGMEM layer_colors[][3] = {
-    {RGB_OFF},
-    {RGB_RED},
-    {RGB_GREEN},
-    {RGB_BLUE},
-    {RGB_YELLOW},
-    {RGB_PURPLE},
-    {RGB_PINK},
-    {RGB_TEAL}
-};
-
-void set_rgb_matrix_indicators(uint8_t led_min, uint8_t led_max) {
-    #if defined(RGB_LAYER_INDICATOR_ENABLE)
-    int layer = get_highest_layer(layer_state|default_layer_state);
-    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, LAYER_R, LAYER_G, LAYER_B);
-    /* uprintf("layer RGB: (%u, %u, %u)\n", LAYER_R, LAYER_G, LAYER_B); */
-    #else
-    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, 0, 0, 0);
-    #endif
-
-    #if defined(RGB_MODS_INDICATOR_ENABLE)
-    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, MODS_R, MODS_G, MODS_B);
-    /* uprintf("mod RGB: (%u, %u, %u)\n", MODS_R, MODS_G, MODS_B); */
-    #else
-    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, 0, 0, 0);
-    #endif
-}
-
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    set_rgb_matrix_indicators(led_min, led_max);
-    return false;
-}
---
+++ keyboards/jlw/le_chiffre_36/le_chiffre_36.c
@@ -0,0 +1,57 @@
+// Copyright 2023 jlw
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+// RGB settings for indicator lights
+// Layer and Mods indicator
+#define LED_CENTER_TOP 1
+#define LED_CENTER_BOTTOM 0
+
+#define LAYER_R layer_colors[layer][0] *  RGB_INDICATOR_BRIGHTNESS / 255
+#define LAYER_G layer_colors[layer][1] *  RGB_INDICATOR_BRIGHTNESS / 255
+#define LAYER_B layer_colors[layer][2] *  RGB_INDICATOR_BRIGHTNESS / 255
+
+#define MODS_ACTIVE(mods) \
+    ((get_mods()|get_oneshot_mods()) & MOD_MASK_##mods ? RGB_INDICATOR_BRIGHTNESS:0)
+#define SHIFT_ACTIVE (get_mods() & MOD_MASK_SHIFT ? RGB_INDICATOR_BRIGHTNESS/4:0)
+#define MODS_R MODS_ACTIVE(CTRL) + SHIFT_ACTIVE
+#define MODS_G MODS_ACTIVE(GUI) + SHIFT_ACTIVE
+#define MODS_B MODS_ACTIVE(ALT) + SHIFT_ACTIVE
+
+const uint8_t PROGMEM layer_colors[][3] = {
+    {RGB_OFF},
+    {RGB_RED},
+    {RGB_GREEN},
+    {RGB_BLUE},
+    {RGB_YELLOW},
+    {RGB_PURPLE},
+    {RGB_PINK},
+    {RGB_TEAL}
+};
+
+void set_rgb_matrix_indicators(uint8_t led_min, uint8_t led_max) {
+    #if defined(RGB_LAYER_INDICATOR_ENABLE)
+    int layer = get_highest_layer(layer_state|default_layer_state);
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, LAYER_R, LAYER_G, LAYER_B);
+    /* uprintf("layer RGB: (%u, %u, %u)\n", LAYER_R, LAYER_G, LAYER_B); */
+    #else
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_TOP, 0, 0, 0);
+    #endif
+
+    #if defined(RGB_MODS_INDICATOR_ENABLE)
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, MODS_R, MODS_G, MODS_B);
+    /* uprintf("mod RGB: (%u, %u, %u)\n", MODS_R, MODS_G, MODS_B); */
+    #else
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_CENTER_BOTTOM, 0, 0, 0);
+    #endif
+}
+
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+        return false;
+    }
+
+    set_rgb_matrix_indicators(led_min, led_max);
+    return true;
+}
