617e5f4f9661161e4b284c8e198f359ae3aefde9
Iris <ivan.tulskuy@gmail.com>
2026-09-19 02:52:36 +0200

    Update keyboards/wd66/keymaps/default/keymap.c
    Co-authored-by: Joel Challis <git@zvecr.com>

--- keyboards/wd66/keymaps/default/keymap.c
+++ keyboards/wd66/keymaps/default/keymap.c
@@ -10,7 +10,7 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_BL] = LAYOUT_65_ansi_blocker(
+    [_BL] = LAYOUT_65_ansi(
         QK_GRAVE_ESCAPE, KC_1,        KC_2,        KC_3,        KC_4,          KC_5,          KC_6,          KC_7,          KC_8,          KC_9,          KC_0,          KC_MINUS,    KC_EQUAL,         KC_BACKSPACE,    KC_INSERT,
         KC_TAB,          KC_Q,        KC_W,        KC_E,        KC_R,          KC_T,          KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,    KC_DELETE,
         KC_CAPS_LOCK,    KC_A,        KC_S,        KC_D,        KC_F,          KC_G,          KC_H,          KC_J,          KC_K,          KC_L,          KC_SEMICOLON,  KC_QUOTE,         KC_ENTER,                            KC_PAGE_UP,
